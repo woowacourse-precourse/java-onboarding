9f68c61bcaef23f6c192ebb445162c44643bf96c
sunwng <sunwgna@gmail.com>
2022-10-26 17:12:20 +0900

    [problem_2] 필요 기능 구현, 테스트 성공

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,12 +5,40 @@ public class Problem2 {
     public static boolean check = true;
 
     public static String decodeString(String strIn) {
-
+        String output = "";
+        String[] strSplit = strIn.split("");
+        Queue<String> queue = new LinkedList<>(Arrays.asList(strSplit));
+        String prevStr = queue.poll();
+        String curStr;
+        while (!queue.isEmpty()) {
+            curStr = queue.poll();
+            if (curStr.equals(prevStr)) {
+                String temp = prevStr;
+                if (!queue.isEmpty()) {
+                    prevStr = queue.poll();
+                }
+                if (queue.isEmpty() && !temp.equals(prevStr)) output = output.concat(prevStr);
+                continue;
+            }
+            output = output.concat(prevStr);
+            if (queue.isEmpty()) {
+                output = output.concat(curStr);
+            } else {
+                prevStr = curStr;
+            }
+        }
+        return output;
     }
 
     public static String solution(String cryptogram) {
         String answer = cryptogram;
-
+        int checkNum;
+        while (check) {
+            check = false;
+            checkNum = answer.length();
+            answer = decodeString(answer);
+            if (answer.length() != checkNum) check = true;
+        }
 
         return answer;
     }
