405de81f4c959bf7aa9f7e6123fb3c485366a84c
seokjin <mm8678@g.yju.ac.kr>
2022-10-26 22:35:51 +0900

    feat(Problem3.java): 각 자리 수를 비교하여 result에 갯수만큼 증가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,7 +4,14 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            System.out.print(i);
+            int temp = i;
+            while (temp > 0) {
+                int temp2 = temp % 10;
+                if (temp2 != 0 && temp2 % 3 == 0) {
+                    answer++;
+                }
+                temp /= 10;
+            }
         }
 
         return answer;
