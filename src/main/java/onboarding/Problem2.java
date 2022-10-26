b93c172b557df39288e233cfefadc6f6836d8b3b
eastsage <dpwk8@naver.com>
2022-10-27 03:06:55 +0900

    feat: [problem2] 연속된 문자가 있는지 판별하는 함수 구현
    hasDuplicate 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,12 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    static int hasDuplicate(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1))
+                return i;
+        }
+        return -1;
+    }
 }
