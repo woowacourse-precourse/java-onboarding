8fd7ab7222e7e4c16760ee6856766a181ff94c15
woosung1223 <dntjd991223@naver.com>
2022-10-26 22:07:41 +0900

    [문제 1] 최대값을 계산하는 getMaxValue 함수 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (validationCheck(pobi, crong)) {
+            int pobi_max = getMaxValue(pobi);
+            int crong_max = getMaxValue(crong);
+            int answer = Integer.MAX_VALUE;
+            return answer;
+        }
+        return -1;
     }
 
     public static boolean validationCheck(List<Integer> user_1, List<Integer> user_2) {
@@ -25,5 +30,27 @@ class Problem1 {
             return false;
         }
         return true;
+    }
+
+    public static int getMaxValue(List<Integer> user) {
+        String left = Integer.toString(user.get(0));
+        String right = Integer.toString(user.get(1));
+        int biggest = -1;
+        int[] temp = {0, 1, 0, 1};
+        for (int i = 0; i < left.length(); i++) {
+            temp[0] += (left.charAt(i) - '0');
+            temp[1] *= (left.charAt(i) - '0');
+        }
+        for (int i = 0; i < right.length(); i++) {
+            temp[2] += (right.charAt(i) - '0');
+            temp[3] *= (right.charAt(i) - '0');
+        }
+
+        for (int elem : temp) {
+            if (elem > biggest) {
+                biggest = elem;
+            }
+        }
+        return biggest;
     }
 }
