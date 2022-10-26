d118a59e1f217b9c0c49372246dfcdb2a2ae5841
kevstevie <jianna442@gmail.com>
2022-10-26 18:47:20 +0900

    fix: lower upper case

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,7 +8,7 @@ public class Problem4 {
         String answer = "";
         Map<Character, Character> alphabets = new HashMap<>();
         for (int i = 0; i < 26; i++) {
-            alphabets.put( (char)(i + 'a'), (char)('z' - i));
+            alphabets.put( (char)('a' + 'i'), (char)('z' - i));
         }
         char[] chars = word.toCharArray();
         for (char c : chars) {
@@ -18,10 +18,10 @@ public class Problem4 {
     }
     static char changeFrogWord(Map<Character, Character> map, char ch){
         if (ch >= 'a' && ch <= 'z') {
-            return Character.toUpperCase(map.get(ch));
+            return map.get(ch);
         }
         if (ch >= 'A' && ch <= 'Z') {
-            return map.get(Character.toLowerCase(ch));
+            return Character.toUpperCase(map.get(Character.toLowerCase(ch)));
         }
         return ch;
     }
