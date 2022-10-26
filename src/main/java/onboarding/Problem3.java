4d66d46e995431dc47a67364f77acfd01fdb870d
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 19:58:12 +0900

    문자열에서 3,6,9 외를 제거하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,9 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    //문자열에서 3, 6, 9가 아닌 숫자를 제거하는 함수
+    private static String get369(String number){
+        return number.replaceAll("[1245780]","");
+    }
 }
