98cbf4fb88a28d6b00ed9e6b241be5b3d3a127a8
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-10-26 19:24:15 +0900

    연속 중복 문자 제거 Problem2 완료
    Stack을 사용하여 문자 비교 후 제거 로직 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,7 +3,9 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
+
         /*
         주어진 문자 cryptogram 에  대해 연속하는 중복 문자를 제거하는 기능 구현
         조건 - cryptogram 의 길이는 1이상 1000이하이다, 알파벳 소문자로만 이루어져 있다.
@@ -11,11 +13,28 @@ public class Problem2 {
         2. cryptogram 에 CharAt으로 하나씩 접근, 값을 비교하여 제거
         3. 문자 내 연속 중복 문자가 없을때까지 반복실행
          */
+        Stack<Character> characterStack = new Stack<>();
+        char temp = ' ';
+        for (Character data : cryptogram.toCharArray()) {
+            if(!characterStack.isEmpty() && characterStack.peek().equals(data)){
+                temp = characterStack.peek();
+                characterStack.pop();
+            }else if(data.equals(temp)){
+                continue;
+            }else{
+                characterStack.push(data);
+            }
+        }
 
-       
-        String answer = "answer";
+        String answer = "";
+        if(characterStack.isEmpty()){
+            return answer;
+        }
+
+        for (Character character : characterStack) {
+            answer+=character;
+        }
         return answer;
     }
-
 
 }
