c39beec99f88b21a9dccefc75f798aa82671f074
jthugg <jthug@naver.com>
2022-10-27 00:51:54 +0900

    2번 문제 테스트케이스 통과

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,14 +9,18 @@ public class Problem2 {
 
         int hasRm = 1; // 0이 아니면 지워야할 문자열이 있음, 루프 진입을 위해 1로 초기화.
         String str = ""; // 중복 문자열을 담을 변수
+
         while(hasRm != 0) {
             hasRm = 0; // 루프 진입 후 1 -> 0
+
             for(int i = 0; i < cryptogram.length(); i++) {
                 char ch = cryptogram.charAt(i);
                 str = ch + "";
+
                 for(int j = i + 1; j < cryptogram.length() && ch == cryptogram.charAt(j); j++) { // i 번째 인덱스와 다음 오는 문자들이 중복되는 경우, 중복아니면 패스
                     str += cryptogram.charAt(j);
                 }
+
                 if(str.length() > 1) { // str 길이가 1 보다 크면 중복되는게 있음
                     hasRm = 1; // hasRM이 0이 아니므로 기능 수행 후 루프 재진입 예정
                     cryptogram = cryptogram.replaceFirst(str, ""); // 지워야 할 문자열 하나만 지우기. 다른것도 지워버리면 문제 발생. ex) axxbxxxc -> abxc
@@ -24,6 +28,7 @@ public class Problem2 {
                 }
             }
         }
+
         return cryptogram;
     }
 }
