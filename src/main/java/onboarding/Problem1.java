7375ccf9abf993eaa4f55ccb22cbd30883cffb19
kingseungwooya <ksw_0505@nate.com>
2022-10-27 01:52:41 +0900

    comment: add comment

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,13 +8,14 @@ import java.util.stream.Stream;
 /**
  * Programs should be written for people to read, and only incidectally for machine to execute
  */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         try{
             int pobiMax = getMax(pobi);
             int crongMax = getMax(crong);
-            switch (String.valueOf(pobiMax).compareTo(String.valueOf(crongMax))){
+            switch (Integer.compare(pobiMax,crongMax)){
                 case 1 :
                     return 1;
                 case -1 :
@@ -29,6 +30,10 @@ class Problem1 {
         return answer;
     }
 
+    /**
+     * 사용자의 나올 수 있는 경우의 수 중 최댓값 구하는 함수
+     **/
+
     private static int getMax(List<Integer> pages) throws Exception {
         int leftPage = pages.get(0);
         int rightPage = pages.get(1);
@@ -40,6 +45,10 @@ class Problem1 {
         return max;
     }
 
+    /**
+     * 최대값을 구하기 위한 계산함수
+     **/
+
     private static int getMaxCalculation(int page){
         int addition = getSum(getNumberOfDigits(page));
         int multiplication = getMultiple(getNumberOfDigits(page));
@@ -47,16 +56,28 @@ class Problem1 {
         return max;
     }
 
+    /**
+     * 입력된 숫자의 각 자리수별 숫자를 반환하는 함수
+     **/
+
     private static int[] getNumberOfDigits(int page){
         int[] numberOfDigits = Stream.of(String.valueOf(page).split(""))
                 .mapToInt(Integer::parseInt).toArray();
         return numberOfDigits;
     }
 
+    /**
+     * 자릿수의 덧셈 결과를 반환하는 함수
+     **/
+
     private static int getSum(int[] numList){
         int sum = IntStream.of(numList).sum();
         return sum;
     }
+
+    /**
+     * 자릿수의 곱셈 결과를 반환하는 함수
+     **/
 
     private static int getMultiple(int[] numList){
         int multiple = Arrays.stream(numList)
