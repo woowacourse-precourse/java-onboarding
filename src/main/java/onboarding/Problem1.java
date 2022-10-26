c0cb8dc1302d21c9dae8432d164d46a1cca04015
Kim kyunghee <kkh2415@naver.com>
2022-10-26 22:14:13 +0900

    add : #Problem1-핵심 기능 max() 메서드 구현 1. 왼쪽 페이지의 가장 큰 수와 오른쪽 페이지 가장 큰 수를 비교하여 가장 큰 값을 리턴하는 max() 메서드 구현 2. Integer를 int[] 배열로 반환하는 toIntArray() 구현 3. int[] 배열의 합을 구하는 getSumNumber() 구현 4. int[] 배열의 곱을 구하는 getMulNumber() 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
@@ -8,7 +9,13 @@ class Problem1 {
             return -1;
         }
 
-        return 0;
+        int pobiMaxNum = max(pobi);
+        int crongMaxNum = max(crong);
+
+        if (pobiMaxNum == crongMaxNum) {
+            return 0;
+        }
+        return pobiMaxNum > crongMaxNum ? 1 : 2;
     }
 
     private static boolean validatePage(List<Integer> pages) {
@@ -31,5 +38,35 @@ class Problem1 {
         final int LAST_PAGE_NUM = 400;
 
         return first == FIRST_PAGE_NUM || second == LAST_PAGE_NUM;
+    }
+
+    private static int max(List<Integer> pages) {
+        final int FIRST_PAGE = 0;
+        final int SECOND_PAGE = 1;
+
+        int[] eachNumberInFirstPage = toIntArray(pages.get(FIRST_PAGE));
+        int[] eachNumberInSecondPage = toIntArray(pages.get(SECOND_PAGE));
+
+        int firstNum = Math.max(getSumNumber(eachNumberInFirstPage), getMulNumber(eachNumberInFirstPage));
+        int secondNum = Math.max(getSumNumber(eachNumberInSecondPage), getMulNumber(eachNumberInSecondPage));
+
+        return Math.max(firstNum, secondNum);
+    }
+
+    private static int[] toIntArray(Integer page) {
+        return Arrays.stream(String.valueOf(page).split(""))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+    }
+
+    private static int getSumNumber(int[] eachNumberInPage) {
+        return Arrays.stream(eachNumberInPage)
+                .max()
+                .orElse(0);
+    }
+
+    private static int getMulNumber(int[] eachNumberInPage) {
+        return Arrays.stream(eachNumberInPage)
+                .reduce(1, (f, s) -> f * s);
     }
 }
