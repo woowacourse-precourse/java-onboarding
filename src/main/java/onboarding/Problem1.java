358e4d93c66a098e5b555c9cbd30bbb89a0f6ed7
skck0226 <skck0226@ajou.ac.kr>
2022-10-26 21:55:04 +0900

    [refactor] Problem1 변수명,함수 리팩토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,41 +11,30 @@ class Problem1 {
             return -1;
         }
 
-        String pobi_str = pobi.get(0).toString() + pobi.get(1).toString();
-        String crong_str = crong.get(0).toString() + crong.get(1).toString();
+        String pobiNumStr = pobi.get(0).toString() + pobi.get(1).toString();
+        String crongNumStr = crong.get(0).toString() + crong.get(1).toString();
 
-        int pobi_max = getPobiMax(pobi_str);
-        int crong_max = getCrongMax(crong_str);
+        int pobiMax = getMaxNum(pobiNumStr);
+        int crongMax = getMaxNum(crongNumStr);
 
-        answer = (pobi_max < crong_max) ? 2 : ((pobi_max == crong_max) ? 0 : 1);
-        return answer;
+        answer = Integer.compare(pobiMax,crongMax);
+        return (answer > -1) ? answer : 2;
     }
 
-    private static int getCrongMax(String crong_str) {
-        int crong_sum = Stream.of(crong_str.split(""))
+    private static int getMaxNum(String numStr) {
+        int sum = Stream.of(numStr.split(""))
                 .mapToInt(Integer::parseInt)
                 .sum();
-        int crong_multi = Stream.of(crong_str.split(""))
+        int multiply = Stream.of(numStr.split(""))
                 .mapToInt(Integer::parseInt)
                 .reduce(1, (x, y) -> x * y);
-        return Math.max(crong_sum, crong_multi);
-    }
-
-    private static int getPobiMax(String pobi_str) {
-        int pobi_sum = Stream.of(pobi_str.split(""))
-                .mapToInt(Integer::parseInt)
-                .sum();
-        int pobi_multi = Stream.of(pobi_str.split(""))
-                .mapToInt(Integer::parseInt)
-                .reduce(1, (x, y) -> x * y);
-        return Math.max(pobi_sum, pobi_multi);
+        return Math.max(sum, multiply);
     }
 
     private static boolean isExceptionCase(List<Integer> pobi, List<Integer> crong) {
         if (pobi.get(1) - pobi.get(0) != 1) return true;
         if (crong.get(1) - crong.get(0) != 1) return true;
-        if (pobi.get(0) == 1 || crong.get(1) == 400) return true;
-        return false;
+        return pobi.get(0) == 1 || crong.get(1) == 400;
     }
 
 }
