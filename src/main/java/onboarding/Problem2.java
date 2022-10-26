d08d985674e9f8820487af0f8cd4319b25267b7c
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 19:35:57 +0900

    중복 제거 함수 정규식 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,12 +3,13 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
-        return answer;
+        return cryptogram;
     }
 
     //중복 제거 함수
     private static String deduplication(String cryptogram){
-        String changeString = cryptogram.replaceAll("[a-z]{2,}","");
+        String changeString = cryptogram.replaceAll("(([a-zA-Z0-9])\\2{1,})","");
+        
         //원래 문자와 같다면 FALSE 문자 반환
         if(cryptogram.equals(changeString)){
             return "FALSE";
