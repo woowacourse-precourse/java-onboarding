9d7dba0b8f923039ad08f3988dcc6df9f494766e
seokjin <mm8678@g.yju.ac.kr>
2022-10-26 18:39:32 +0900

    feat(Problem1.java): 기능 일부 구현 - 페이지의 번호의 각 자리 숫자의 합 또는 곱 구하기 - 구해진 합 또는 곱 중 큰 값을 구하기 - 큰 값에서 한 번 더 큰 값 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,32 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+
+        Integer pobiMax = pobi.stream()
+                .map(Problem1::getMaxPage)
+                .max(Integer::compareTo)
+                .orElse(-1);
+
+		Integer crongMax = crong.stream()
+				.map(Problem1::getMaxPage)
+				.max(Integer::compareTo)
+				.orElse(-1);
+
         return answer;
-    }
+	}
+
+	private static int getMaxPage(Integer page) {
+		int sum = 0;
+		int mul = 1;
+		int temp;
+		while (page > 0) {
+			temp = page % 10;
+			page /= 10;
+			sum += temp;
+			mul *= temp;
+		}
+		return Math.max(sum, mul);
+	}
 }
