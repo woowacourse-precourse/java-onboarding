3e86a918f29c47473f3e389ccdb1e874e7cc62ff
Kloong1 <dkdlsmsm98@gmail.com>
2022-10-27 03:20:39 +0900

    problem2 - removing consecutive duplicate alphabet

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder stringBuilder = new StringBuilder();
+     
+
+        return cryptogram;
+    }
+
+    private static String removeConsecutiveDuplicateAlpha(String cryptogram) {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean duplicateFlag = false;
+
+        for (int idx = 0; idx < cryptogram.length() - 1; idx++) {
+            if (cryptogram.charAt(idx) == cryptogram.charAt(idx + 1)) {
+                duplicateFlag = true;
+            } else if (duplicateFlag) {
+                duplicateFlag = false;
+            } else {
+                stringBuilder.append(cryptogram.charAt(idx));
+            }
+        }
+
+        if (!duplicateFlag) {
+            stringBuilder.append(cryptogram.charAt(cryptogram.length() - 1));
+        }
+
+        return stringBuilder.toString();
     }
 }
