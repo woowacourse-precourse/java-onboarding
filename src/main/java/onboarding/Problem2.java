afe57f6b4aab9f12cf6ee72d196f5ae9a3ffc587
cmong0516 <cmong0516@gmail.com>
2022-10-26 23:30:47 +0900

    2022.10.26 Problem 2 first

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,42 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        StringBuilder sb = new StringBuilder();
 
+        ArrayList<Integer> overlapIndex = findOverlapIndex(cryptogram);
+        remove(cryptogram,overlapIndex);
+        cryptogram = remove(cryptogram,overlapIndex);
+        if (overlapIndex.isEmpty()) {
+            return cryptogram;
+        }
+
+
+        return solution(cryptogram);
+    }
+
+    static ArrayList<Integer> findOverlapIndex(String cryptogram) {
+        ArrayList<Integer> list = new ArrayList<>();
         char[] chars = cryptogram.toCharArray();
-        char c = chars[0];
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i - 1] == chars[i]) {
+                list.add(i-1);
+                list.add(i);
+            }
+        }
+        return list;
+    }
 
+    static String remove(String cryptogram,ArrayList<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(cryptogram);
 
-        return String.valueOf(sb);
+        for (int i = 0; i < list.size(); i++) {
+            sb.delete(list.get(0), list.get(1));
+            cryptogram= String.valueOf(sb);
+        }
+
+        return cryptogram;
     }
 }
