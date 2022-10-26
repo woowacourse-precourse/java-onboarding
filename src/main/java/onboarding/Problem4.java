83b2cea7f671fd803ff453b5db75484f52aeb262
backFox <jorealbusiness@gmail.com>
2022-10-26 18:56:16 +0900

    feat(problem4): 문제 4 알고리즘 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import static java.lang.Character.*;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder stringBuilder = new StringBuilder();
+        word.chars().forEach(ascii -> stringBuilder.append(reverse(ascii)));
+
+        return stringBuilder.toString();
+    }
+
+    private static char reverse(int ascii) {
+        if (isLowerCase(ascii)) {
+            return reverseLowerCase(ascii);
+        } else if (isUpperCase(ascii)) {
+            return reverseUpperCase(ascii);
+        }
+        return (char)ascii;
+    }
+
+    private static char reverseUpperCase(int ascii) {
+        return (char)(90 - (ascii - 65));
+    }
+
+    private static char reverseLowerCase(int ascii) {
+        return (char)(122 - (ascii - 97));
     }
 }
