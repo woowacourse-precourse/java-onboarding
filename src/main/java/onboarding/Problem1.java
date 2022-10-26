85b232604ebdb3a3b47e79b023ef8006dfd79da5
swhyeon98 <smreo75@gmail.com>
2022-10-26 23:39:38 +0900

    점수 판별 기능 추가 - 점수 판별 기능 추가 및 구현한 기능 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = 0;
+        int pobiMax = checkLeftRight(pobi.get(0),pobi.get(1));
+        int crongMax = checkLeftRight(crong.get(0),crong.get(1));
+        int answer = checkScore(pobiMax,crongMax);
 
         return answer;
     }
@@ -28,5 +30,19 @@ class Problem1 {
         int right = getMaxPage(rightPage);
 
         return Math.max(left,right);
+    }
+
+    private static int checkScore(int pobiScore, int crongScore){
+        int answer = -1;
+
+        if (pobiScore>crongScore) {
+            answer = 1;
+        } else if (pobiScore<crongScore){
+            answer = 2;
+        } else {
+            answer = 0;
+        }
+
+        return answer;
     }
 }
