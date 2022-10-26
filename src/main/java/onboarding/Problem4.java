a32612bb0afdeb4e1e985e1ae542d0d10900a260
young2866 <62212610+young2866@users.noreply.github.com>
2022-10-26 22:30:36 +0900

    Problem4 기능 목록 및 기능1 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,49 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
+
         return answer;
+    }
+
+    /**
+     * 기능 1
+     * String을 청개구리 사전으로 변환 하는 기능
+     */
+
+    public static String translateString(String word) {
+        String translatedString = "";
+
+        for(int i = 0; i < word.length(); i++)
+        {
+            if (checkAlphabet(word.charAt(i))) {
+                translatedString += translateChar(word.charAt(i));
+            }else
+            {
+                translatedString += word.charAt(i);
+            }
+        }
+
+        return translatedString;
+    }
+
+    /**
+     * 기능 2
+     * 알파벳인지 아닌지를 구분하는 기능
+     */
+    public static boolean checkAlphabet(char ch) {
+
+        return true;
+    }
+
+    /**
+     * 기능 3
+     * 알파벳 하나를 변환 하는 기능
+     */
+    public static char translateChar(char ch) {
+        char translatedCharacter = ' ';
+
+        return translatedCharacter;
     }
 }
