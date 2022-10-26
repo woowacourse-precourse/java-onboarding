69ce123819a9ede76cf7d5b0f030de9e0a1419e4
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-27 00:00:12 +0900

    feat(problem1): solution method작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,13 @@
 package onboarding;
 
+import onboarding.problem1.BookGame;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        BookGame bookGame = new BookGame(pobi, crong);
+        int answer = bookGame.getPlayResult();
         return answer;
     }
 }
