d3c631f3f35c495d2fdf6afc388aab388382aab1
K-Diger <kitten.diger@gmail.com>
2022-10-26 20:05:25 +0900

    Week1 : Problem2 Solved - 암호문 List<String> 으로 치환 후 - List 크기만큼 반복하며 인덱스 값을 두 개씩 비교하며 중복되는 값 제거하는 로직

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,52 @@
 package onboarding;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 중복된 문자열 검사
+// 어렵다...
 public class Problem2 {
+
+    public static String overlapRemover(String cryptogram) {
+        int cryptogramLength = cryptogram.length();
+        List<String> stringToList = new ArrayList<>(cryptogramLength);
+
+        // 입력 암호문 String 리스트로 치환
+        for (int i = 0; i < cryptogramLength; i++) {
+            stringToList.add(String.valueOf(cryptogram.charAt(i)));
+        }
+
+        int i = 0;
+
+        while (i < stringToList.size() - 1) {
+            String lowIndexValue = stringToList.get(i);
+            String highIndexValue = stringToList.get(i + 1);
+
+            if (lowIndexValue.equals(highIndexValue)) {
+                stringToList.remove(i);
+                stringToList.remove(i);
+                i = 0;
+            }
+            else if (!lowIndexValue.equals(highIndexValue)) {
+                i ++;
+            }
+        }
+
+        // 암호 해독 후 문자열이 비어있으면 빈 문자열 반환
+        if (stringToList.size() == 0) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String element : stringToList) {
+            result.append(element);
+        }
+
+        return result.toString();
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return overlapRemover(cryptogram);
     }
 }
