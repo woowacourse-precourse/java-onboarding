ec529d8aae5657b4c63dada608aaf67d1dd9a7a9
Kayoung <rkdkdudjd@gmail.com>
2022-10-27 01:45:18 +0900

    2번: 순차적으로 순회하며 문자열 수정하기
    - 덱의 마지막 문자와 같은 문자일경우 삽입
    - 같지 않은 문자일 경우 덱에서 겹치는 문자열 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!deque.isEmpty() && deque.peekLast() != cryptogram.charAt(i)) {
+                // TODO: 덱에서 앞의 중복 문자열 지우기
+            }
+            deque.addLast(cryptogram.charAt(i));
+        }
+        // TODO: 마지막 문자 한번 더 처리
+
+        StringBuilder answer = new StringBuilder();
+        while (!deque.isEmpty()) {
+            answer.append(deque.pollFirst());
+        }
+        return answer.toString();
+
     }
 }
