2d147eeb030281e8f32ab2f05bb1e8910d89b3bb
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-26 18:04:38 +0900

    feat: 문자를 ArrayList를 변환 하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        //암호문 리스트로 변환
+        List<String> cryptogramList = wordToList(cryptogram);
         return answer;
+    }
+    public static List wordToList(String word){
+        return new ArrayList<>(Arrays.asList(word.split("")));
     }
 }
