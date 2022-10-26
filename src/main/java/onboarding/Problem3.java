d018b12d6d93c40df10318a3416365a0cbb36614
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 04:46:44 +0900

    Problem3 solve update

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int num;
+        int tmp;
+
+        // count 3, 6, 9
+        for(int i=1; i<= number; i++){
+            num = i;
+            while(num > 0){
+                tmp = num % 10;
+                if((tmp == 3) || (tmp == 6) || (tmp == 9))
+                    answer++;
+                num /= 10;
+            }
+        }
         return answer;
     }
 }
