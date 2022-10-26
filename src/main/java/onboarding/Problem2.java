bd493f16e2ac32ebefdc67078f1d2e25b8e4868e
greeng00se <thegreengoose7@gmail.com>
2022-10-26 22:39:18 +0900

    feat(Problem2): 중복문자를 제거한 문자열을 반환하는 로직 추가
    - 반환하는 문자열을 생성하는 부분은 StringBuilder 사용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,45 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+
 public class Problem2 {
+    private static final int MIN_DECRYPT_LENGTH = 2;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String result;
+
+        while (true) {
+            result = decrypt(cryptogram);
+            if (cryptogram.equals(result)) {
+                break;
+            }
+            cryptogram = result;
+        }
+        return result;
+    }
+
+    private static String decrypt(String cryptogram) {
+        if (cryptogram.length() < MIN_DECRYPT_LENGTH) {
+            return cryptogram;
+        }
+
+        ArrayDeque<Character> queue = new ArrayDeque<>();
+        for (char c : cryptogram.toCharArray()) {
+            if (queue.isEmpty() || queue.getLast() != c) {
+                queue.addLast(c);
+            } else {
+                queue.pollLast();
+            }
+        }
+
+        return queueToString(queue);
+    }
+
+    private static String queueToString(ArrayDeque<Character> queue) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : queue) {
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
