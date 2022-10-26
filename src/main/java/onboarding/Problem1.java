987980e621ca07424200c21dabeee3cf8051b995
main <jang0929@naver.com>
2022-10-26 20:16:26 +0900

    Problem 1 기능 사항 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,7 +1,13 @@
 package onboarding;
 
 import java.util.List;
-
+/**
+ * 기능 사항
+ * 1. 에러 체크
+ * 2. 자릿수 리스트로 변환하는 함수
+ * 3. 자릿수 별 합과 곱 중 큰 값을 리턴하는 함수
+ * 4. 기능 요구사항의 연산을 실행하는 함수
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
