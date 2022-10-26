b36f93e30d299828800053f4bfdf14aa7a17bc22
hyeongcheolkim <tori1753@naver.com>
2022-10-26 19:44:25 +0900

    problem2 first commit

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decode(cryptogram);
+    }
+
+    private static String decode(String cryptogram){
+        var s = new Stack<Character>();
+        for(var c : cryptogram.toCharArray()){
+            if(!s.empty() && s.peek().equals(c))
+                s.pop();
+            else
+                s.push(c);
+        }
+        return s.stream().map(String::valueOf).collect(Collectors.joining());
     }
 }
