513f5dd22184b4a65cc5339d7ee5d8380f373ba4
ycopp9 <aciees95@gmail.com>
2022-10-26 16:23:09 +0900

    feat: add the function sumOfDigits() that returns the sum of all digits (of a number)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,21 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    int sumOfDigits(int number) {
+        int sum = 0;
+        while(number > 0) {
+            sum += number%10;
+            sum /= 10;
+        }
+        return sum;
+    }
+    int mulOfDigits(int number) {
+        int result = 1;
+        String str = String.valueOf(number);
+        String[] strs = str.split(" ");
+        for(int i = 0; i < strs.length; i++) {
+            result *= Integer.parseInt(strs[i]);
+        }
+        return result;
+    }
 }
