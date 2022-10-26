e40f6c579463cfcc4ec22d15b2b7eb24bc2bb1bc
bsh1603 <84119508+bsh1603@users.noreply.github.com>
2022-10-26 18:57:31 +0900

    refactor: Refactoring problem(2) 메서드 추가
    depth를 최대 2로 맞추기 위하여 메서드 추가 및 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -18,28 +18,37 @@ public class Problem2 {
         return answer;
     }
 
+    // 중복 문자를 제거하기 위한 함수
+    // stack에 매개변수 str의 문자들을 집어넣으며 조건에 맞게 stack을 구성한다.
     static Stack<Character> duplicate_remove(String str){
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < str.length(); i++) {
             int stack_len = stack.size();
+            // stack에 아무것도 들어있지 않으면 그냥 추가해준다.
             if(stack_len == 0){
                 stack.add(str.charAt(i));
             }
-
+            // stack에 문자가 들어있으면 비교해서 stack에서 뺄건지 추가할건지 결정한다.
             else{
-                if(stack.elementAt(stack_len-1).equals(str.charAt(i))){
-                    stack.pop();
-                }
-                else{
-                    stack.add(str.charAt(i));
-                }
+                add_or_pop(stack, str.charAt(i));
             }
         }
 
         return stack;
     }
 
+    // stack의 맨 위와 문자를 비교하여 stack에 뺄건지 추가할건지 결정하는 함수
+    static void add_or_pop(Stack<Character> stack, Character c){
+        if(stack.elementAt(stack.size()-1).equals(c)){
+            stack.pop();
+        }
+        else{
+            stack.add(c);
+        }
+    }
+
+    // 조건에 맞게 구성된 stack에서 문자를 하나씩 추가해 답을 리턴하는 함수
     static String solve(String str){
         Stack<Character> stack = duplicate_remove(str);
 
