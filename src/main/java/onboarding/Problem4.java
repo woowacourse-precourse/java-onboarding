14ce7e7657fac898853b89f6574d62dc92e83911
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 20:18:12 +0900

    feat(Problem4.java): initDictionary() 사전 초기화

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,18 @@
 package onboarding;
 
+import java.util.Map;
+
 public class Problem4 {
+    public static void initDictionary(Map<Character, Character> dictionary){
+        char upperCase='A';
+        char lowerCase='a';
+
+        for (int i = 0; i < 26; i++) {
+            dictionary.put((char)(upperCase+i), (char)(upperCase+25-i));
+            dictionary.put((char)(lowerCase+i), (char)(lowerCase+25-i));
+        }
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
