faab704a109a13447bffa345fd9c4690dd057b2d
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 21:27:46 +0900

    feat(Problem6.java): detectDuplication() 2글자씩 잘라서 중복 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
+    public static void detectDuplication(List<String> form, Map<String, String> tokens, Set<String> answer){
+        String email = form.get(0);
+        String nickname = form.get(1);
+
+        if(nickname.length()==1)
+            return;
+
+        for(int i=0; i<nickname.length()-1; i++){
+            String token=nickname.substring(i, i+2);
+
+            if (tokens.containsKey(token)) {
+                answer.add(tokens.get(token));
+                answer.add(email);
+                continue;
+            }
+
+            tokens.put(token, email);
+        }
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
