1e4895025a642ec08874d8dbbd80cf1fb96f5a6b
nswon <swnam0729@gmail.com>
2022-10-26 22:08:12 +0900

    feat(sumEachNumber): 페이지 번호의 각 숫자 합 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    private static int sumEachNumber(int pageNumber) {
+        int sum = 0;
+        String strPageNumber = String.valueOf(pageNumber);
+        for(int i=0; i<strPageNumber.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(strPageNumber.charAt(i)));
+        }
+
+        return sum;
     }
 }
