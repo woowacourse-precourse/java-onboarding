49c642b350c9e9171cddcbd4789b564c3dc85486
bradbrad97 <taehyun0121@naver.com>
2022-10-27 00:49:11 +0900

    승리자 선정 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,42 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int getBiggerNum(int pageNum){
+        int addedNum = 0; //각 자릿수 덧셈
+        int multipliedNum = 1; //각 자릿수 곱셈
+        int mod;
+
+        while(pageNum != 0){
+            mod = pageNum % 10;
+            addedNum = addedNum + mod;
+            multipliedNum = multipliedNum * mod;
+            pageNum = pageNum / 10;
+        }
+
+        return Math.max(addedNum, multipliedNum);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiLeftPageNum = pobi.get(0);
+        int pobiRightPageNum = pobi.get(1);
+
+        int crongLeftPageNum = crong.get(0);
+        int crongRightPageNum = crong.get(1);
+
+        int pobiMaxNum = Math.max(getBiggerNum(pobiLeftPageNum), getBiggerNum(pobiRightPageNum));
+        int crongMaxNum = Math.max(getBiggerNum(crongLeftPageNum), getBiggerNum(crongRightPageNum));
+
+        //무승부 0 포비승리 1 크롱승리 2
+        if (pobiMaxNum == crongMaxNum){ //무승부
+            return 0;
+        }
+
+        else if (pobiMaxNum > crongMaxNum){ //포비 승리
+            return 1;
+        }
+
+        else { //크롱 승리
+            return 2;
+        }
     }
 }
