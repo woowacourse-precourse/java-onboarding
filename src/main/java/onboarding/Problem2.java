ebeddfc14b49818f211c2e3cba51e0e062369d78
cmong0516 <cmong0516@gmail.com>
2022-10-27 00:46:32 +0900

    2022.10.27 Problem 3 first

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,13 +6,11 @@ public class Problem2 {
     public static String solution(String cryptogram) {
 
         ArrayList<Integer> overlapIndex = findOverlapIndex(cryptogram);
-        remove(cryptogram,overlapIndex);
-        cryptogram = remove(cryptogram,overlapIndex);
+        remove(cryptogram, overlapIndex);
+        cryptogram = remove(cryptogram, overlapIndex);
         if (overlapIndex.isEmpty()) {
             return cryptogram;
         }
-
-
         return solution(cryptogram);
     }
 
@@ -21,20 +19,20 @@ public class Problem2 {
         char[] chars = cryptogram.toCharArray();
         for (int i = 1; i < chars.length; i++) {
             if (chars[i - 1] == chars[i]) {
-                list.add(i-1);
+                list.add(i - 1);
                 list.add(i);
             }
         }
         return list;
     }
 
-    static String remove(String cryptogram,ArrayList<Integer> list) {
+    static String remove(String cryptogram, ArrayList<Integer> list) {
         StringBuilder sb = new StringBuilder();
         sb.append(cryptogram);
 
         for (int i = 0; i < list.size(); i++) {
             sb.delete(list.get(0), list.get(1));
-            cryptogram= String.valueOf(sb);
+            cryptogram = String.valueOf(sb);
         }
 
         return cryptogram;
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 3; i <=number ; i++) {
+            String temp = String.valueOf(i);
+            answer += count(temp);
+        }
+
+        return answer;
+    }
+
+    static int count(String num) {
+        int answer = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(i) == '3' || num.charAt(i) == '6' || num.charAt(i) == '9') {
+                answer++;
+            }
+        }
         return answer;
     }
 }
