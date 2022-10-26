07c8f0e027ad8b388ed4dd1ba816bd6a9ce319be
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-26 20:56:16 +0900

    문제 1 - 기능 1 : 올바른 페이지 번호가 주어지지 않는 경우 판별 (-1인 경우 판별)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,19 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 오른쪽 페이지의 번호는 언제나 왼쪽 페이지의 번호보다 1만큼 크다.
+        // 따라서 그런 경우가 아니라면 -1을 반환한다.
+        if (pobi.get(0) + 1 != pobi.get(1)) return -1;
+        if (crong.get(0) + 1 != crong.get(1)) return -1;
+
+        // 왼쪽 페이지는 홀수고 오른쪽 페이지는 짝수여야 한다.
+        // 그런 경우가 아니라면 -1을 반환한다.
+        if (pobi.get(0) % 2 == 0 || pobi.get(1) % 2 == 1) return -1;
+        if (crong.get(0) % 2 == 0 || crong.get(1) % 2 == 1) return -1;
+
+
+
         return answer;
     }
 }
