e69a48e8e8d83420e47ebccdd5bb00793257c9bd
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-26 19:50:58 +0900

    feat : Add a function that multiply each digit number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,7 @@ class Problem1 {
         return answer;
     }
 
+    //각 자리를 더하는 함수
     public static int getSum(int num) {
         int sum = 0;
         while (num < 10) {
@@ -16,5 +17,16 @@ class Problem1 {
         }
         sum += num;
         return sum;
+    }
+
+    //각 자리를 곱하는 함수
+    public static int getMultiply(int num) {
+        int multiply = 1;
+        while (num < 10) {
+            multiply *= (num % 10);
+            num /= 10;
+        }
+        multiply *= num;
+        return multiply;
     }
 }
