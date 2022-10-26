735dd29092f7def6808b53a64d3ad6e69f6157ff
KimSangHyeok <tkdgur0906@naver.com>
2022-10-27 01:23:11 +0900

    Feat(PROBLEM1): 주어진 페이지의 각 자리 숫자의 합을 구하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static int getPagePlaceSum(int page){
+        int pagePlaceSum = 0;
+        while(page > 0){
+            pagePlaceSum = pagePlaceSum + page % 10;
+            page = page / 10;
+        }
+        return pagePlaceSum;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
