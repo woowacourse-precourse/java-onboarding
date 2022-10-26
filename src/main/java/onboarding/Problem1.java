9da736a3b2cfda64caecd5b4949bd672077c0b3f
wonjunYou <youwjune@gmail.com>
2022-10-27 00:02:15 +0900

    feat : (Problem1) 펼친 페이지 정보를 저장하고 검증하는 Pages 클래스 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,38 +8,58 @@ class Problem1 {
     public static final int CRONG_WIN = 2;
     public static final int EXCEPTIONS_OCCURED = -1;
 
-    public static final int SIZE_OF_PAGES = 2;
-    public static final int FIRST_PAGE_NUMBER = 1;
-    public static final int LAST_PAGE_NUMBER = 400;
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        if (!validateAllPages(pobi, crong)) {
+        try {
+            Pages pobiPages = Pages.of(pobi);
+            Pages crongPages = Pages.of(crong);
+        } catch (IllegalArgumentException e) {
             return EXCEPTIONS_OCCURED;
         }
 
         return answer;
     }
 
-    public static boolean validateAllPages(List<Integer> pobi, List<Integer> crong) {
-        return (validate(pobi) && validate(crong));
-    }
+    static class Pages {
+        private static final int SIZE_OF_PAGES = 2;
 
-    //무슨 에러인지 찾아볼 것.
-    private static boolean validate(List<Integer> pages) {
-        return (validateLength(pages) && validateConsecutivePages(pages) && validateNotFirstOrLastPages(pages));
-    }
+        private static final int FIRST_PAGE_NUMBER = 1;
+        private static final int LAST_PAGE_NUMBER = 400;
 
-    private static boolean validateLength(List<Integer> pages) {
-        return (pages.size() == SIZE_OF_PAGES);
-    }
+        private final List<Integer> pages;
 
-    private static boolean validateConsecutivePages(List<Integer> pages) {
-        return (pages.get(0) % 2 == 1 && pages.get(0) + 1 == pages.get(1));
-    }
+        private Pages(List<Integer> pages) {
+            this.pages = pages;
+        }
 
-    private static boolean validateNotFirstOrLastPages(List<Integer> pages){
-        return (pages.get(0) != FIRST_PAGE_NUMBER && pages.get(1) != LAST_PAGE_NUMBER);
+        public static Pages of(List<Integer> pages) {
+            validate(pages);
+            return new Pages(pages);
+        }
+
+        private static void validate(List<Integer> pages) {
+            validateListSize(pages);
+            validateContinuousPages(pages);
+            validateFirstOrLastPages(pages);
+        }
+
+        private static void validateListSize(List<Integer> pages) {
+            if (pages.size() != SIZE_OF_PAGES) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        private static void validateContinuousPages(List<Integer> pages) {
+            if (pages.get(0) % 2 == 0 || pages.get(0) + 1 != pages.get(1)) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        private static void validateFirstOrLastPages(List<Integer> pages) {
+            if (pages.get(0) == FIRST_PAGE_NUMBER || pages.get(1) == LAST_PAGE_NUMBER) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
