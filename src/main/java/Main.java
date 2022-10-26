9e1df99102165cfd0e86afcc05bbcde4e6f71222
ma-an-jong <alswhd1113@gmail.com>
2022-10-26 20:34:56 +0900

    problem1 passed test

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import onboarding.Problem1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Problem1.solution(Arrays.asList(97,98) ,Arrays.asList(197,198)));
+        System.out.println(Problem1.solution(Arrays.asList(131,132) ,Arrays.asList(211,212)));
+        System.out.println(Problem1.solution(Arrays.asList(99,102) ,Arrays.asList(211,212)));
+    }
+}
--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,96 @@ package onboarding;
 
 import java.util.List;
 
-class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+public class Problem1 {
+    private static final int POBI_WIN_VALUE = 1;
+    private static final int CRONG_WIN_VALUE = 2;
+    private static final int DRAW_VALUE = 0;
+    private static final int EXCEPTION_VALUE = -1;
+    private static final int INPUT_SIZE = 2;
+    private static final int LEFT_BOUND= 0;
+    private static final int RIGHT_BOUND = 401;
+    private static final int DIVISION_VALUE = 10;
+
+    private static boolean checkValidation(List<Integer> pages)
+    {
+        if(pages.size() != INPUT_SIZE)
+        {
+            return false;
+        }
+
+        Integer leftPageNumber = getLeftPage(pages);
+        Integer rightPageNumber = getRightPage(pages);
+
+        return leftPageNumber == (rightPageNumber - 1)
+                && leftPageNumber > LEFT_BOUND
+                && rightPageNumber < RIGHT_BOUND;
     }
+
+    private static Integer getLeftPage(List<Integer> inputList)
+    {
+        return inputList.get(0);
+    }
+
+    private static Integer getRightPage(List<Integer> inputList)
+    {
+        return inputList.get(1);
+    }
+    private static int getValueByPage(Integer page)
+    {
+        int nowDigit = page % DIVISION_VALUE;
+        int nowValue = page / DIVISION_VALUE;
+
+        int sumValue = nowDigit;
+        int multiplicationValue = nowDigit;
+
+        while(nowValue != 0)
+        {
+            nowDigit = nowValue % DIVISION_VALUE;
+
+            sumValue += nowDigit;
+            multiplicationValue *= nowDigit;
+
+            nowValue /= DIVISION_VALUE;
+        }
+
+        return Math.max(multiplicationValue, sumValue);
+    }
+
+    public static int getGameValue(List<Integer> pages)
+    {
+        int leftPage = getLeftPage(pages);
+        int rightPage = getRightPage(pages);
+
+        return Math.max(getValueByPage(leftPage),getValueByPage(rightPage));
+    }
+
+    public static int solution(List<Integer> pobi, List<Integer> crong)
+    {
+        if(checkValidation(pobi) && checkValidation(crong))
+        {
+            int pobiGameValue = getGameValue(pobi);
+            int crongGameValue = getGameValue(crong);
+            int returnValue;
+
+            if(pobiGameValue > crongGameValue)
+            {
+                returnValue = POBI_WIN_VALUE;
+            }
+            else if(pobiGameValue < crongGameValue)
+            {
+                returnValue = CRONG_WIN_VALUE;
+            }
+            else
+            {
+                returnValue = DRAW_VALUE;
+            }
+
+            return returnValue;
+        }
+
+        return EXCEPTION_VALUE;
+    }
+
+
+
 }
