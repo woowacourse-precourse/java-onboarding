12089974d980e4da69cba7eb394c1bc62123b2b5
WonjunS <seowj0710@gmail.com>
2022-10-27 07:14:26 +1100

    Problem 3 Solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem3 {
+    static int answer = 0;
+
     public static int solution(int number) {
-        int answer = 0;
+        answer = pro(number);
+
         return answer;
+    }
+
+    static int pro(int number) {
+        int count = 0;
+
+        for(int i = 1; i <= number; i++) {
+            String s = String.valueOf(i);
+            count += count(s);
+        }
+
+        return count;
+    }
+
+    static int count(String number) {
+        int count = 0;
+
+        for(int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            if(c == '3' || c == '6' || c == '9') count++;
+        }
+
+        return count;
     }
 }
