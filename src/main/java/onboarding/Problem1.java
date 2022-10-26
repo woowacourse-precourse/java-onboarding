b6c3763a6d741b69a16c7d9f3af716471c58b717
ghchoi <ghchoi0427@gmail.com>
2022-10-26 20:12:39 +0900

    feat: 페이지 수를 통해 가장 높은 점수를 반환하는 메서드 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,28 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public int findMaxFromSinglePage(int page) {
+        int temp = page;
+        int add = 0;
+        int multi = 1;
+
+        while (temp > 0) {
+            add += temp % 10;
+            temp /= 10;
+        }
+
+        temp = page;
+
+        while (temp > 0) {
+            multi *= temp % 10;
+            temp /= 10;
+        }
+
+        return getMax(add, multi);
+    }
+
+    public int getMax(int a, int b) {
+        return Math.max(a, b);
+    }
 }
