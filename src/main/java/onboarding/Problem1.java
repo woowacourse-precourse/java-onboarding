adbe4adeaf22cb6fee18fdba1ad722c1ce5b2601
kimJaeHyeon <kmr2644@gmail.com>
2022-10-26 18:39:29 +0900

    feat: implement method for calculating result

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,12 +5,26 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         if (!(isValidPage(pobi) && isValidPage(crong))) {
             return -1;
         }
         int pobiResult = calculatePage(pobi);
         int crongResult = calculatePage(crong);
+
+        answer = getResult(pobiResult, crongResult);
+
         return answer;
+    }
+
+    private static int getResult(int pobiResult, int crongResult) {
+        if (pobiResult > crongResult) {
+            return 1;
+        }
+        if (crongResult > pobiResult) {
+            return 2;
+        }
+        return 0;
     }
 
     private static int calculatePage(List<Integer> pageList) {
