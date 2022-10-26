2de68ab7d36fc59cc935ed50fa3cd74c4fe0cd37
0chil <0@chll.it>
2022-10-27 00:30:37 +0900

    feat(problem1): 페이지는 연속적이어야 한다

--- src/main/java/onboarding/problem1/Pages.java
+++ src/main/java/onboarding/problem1/Pages.java
@@ -1,5 +1,7 @@
 package onboarding.problem1;
 
+import static java.lang.Math.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +10,8 @@ public class Pages {
 	private final List<Page> pages;
 
 	public Pages(List<Integer> pageNumbers) {
+		validateConsecutive(pageNumbers);
+
 		List<Page> pages = new ArrayList<>();
 		for (Integer pageNumber : pageNumbers) {
 			pages.add(new Page(pageNumber));
@@ -21,5 +25,12 @@ public class Pages {
 			maximum = Math.max(maximum, page.calculateScore());
 		}
 		return maximum;
+	}
+
+	private void validateConsecutive(List<Integer> pageNumbers) {
+		int diff = abs(pageNumbers.get(0) - pageNumbers.get(1));
+		if (diff != 1) {
+			throw new IllegalArgumentException("페이지가 연속적이지 않습니다");
+		}
 	}
 }
--- src/test/java/onboarding/problem1/PagesTest.java
+++ src/test/java/onboarding/problem1/PagesTest.java
@@ -1,8 +1,9 @@
 package onboarding.problem1;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,14 @@ public class PagesTest {
 		List<Integer> pageNumbers = List.of(97, 98);
 		Pages pages = new Pages(pageNumbers);
 
-		Assertions.assertThat(pages.calculateScore()).isEqualTo(72);
+		assertThat(pages.calculateScore()).isEqualTo(72);
+	}
+
+	@Test
+	@DisplayName("페이지는 이어져야 한다")
+	void pages_are_consecutive() {
+		List<Integer> pageNumbers = List.of(97, 99);
+
+		assertThatIllegalArgumentException().isThrownBy(() -> new Pages(pageNumbers));
 	}
 }
