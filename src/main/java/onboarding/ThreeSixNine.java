8b06d7ad5c89e1f18999ea7e64c59c497de090f3
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-26 21:30:21 +0900

    feat(problem3): count 369

--- src/main/java/onboarding/ThreeSixNine.java
+++ src/main/java/onboarding/ThreeSixNine.java
@@ -3,6 +3,7 @@ package onboarding;
 public class ThreeSixNine {
 
 	private final int number;
+	private int clap = 0;
 
 	public ThreeSixNine(int number) {
 		this.number = number;
@@ -18,7 +19,14 @@ public class ThreeSixNine {
 	private void countClap(int number) {
 		while (number > 0) {
 			int digit = number % 10;
+			if (is369(digit)) {
+				clap += 1;
+			}
 			number /= 10;
 		}
+	}
+
+	private boolean is369(int number) {
+		return number == 3 || number == 6 || number == 9;
 	}
 }
