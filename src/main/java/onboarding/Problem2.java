03ab70c8456e6b7bca7a89804e692aecbfcfb4c1
SEONMl <sm000117@naver.com>
2022-10-26 22:42:33 +0900

    feat: Problem2 Cryptogram class 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem2 {
+
+    static class Cryptogram {
+        private String origin;
+        private String duplication;
+
+        public Cryptogram(String origin) {
+            this.origin = origin;
+        }
+
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
