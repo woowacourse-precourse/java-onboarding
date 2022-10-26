f97e4a7f6c2f39c744a21a63c89ab4477826b5ab
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 19:40:01 +0900

    solution 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,14 +2,25 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = cryptogram;
+        while(true){
+            //중복 제거 함수 결과
+            String ret = deduplication(cryptogram);
+
+            //중복 제거 함수 결과가 FALSE라면 break
+            if(ret.equals("FALSE")){
+                break;
+            }
+            
+            //중복 제거가 됐다면 문자열 덮어쓰기
+            cryptogram = ret;
+        }
         return cryptogram;
     }
 
     //중복 제거 함수
     private static String deduplication(String cryptogram){
         String changeString = cryptogram.replaceAll("(([a-zA-Z0-9])\\2{1,})","");
-        
+
         //원래 문자와 같다면 FALSE 문자 반환
         if(cryptogram.equals(changeString)){
             return "FALSE";
