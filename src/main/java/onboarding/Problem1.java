cf3537ddb0e56ed1aea89cbc7ad72e77d1fb991d
dongwoo <shp05179@gmail.com>
2022-10-26 23:33:57 +0900

    페이지가 있는 List를 받아서 각자릿수의 합과 곱중 더 큰값을 리턴하는 함수 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+    }
+
+    static int getMax(List<Integer> player) {
+        int maximum_value = 0;
+        List<String> playerString = List.of(Integer.toString(player.get(0)), Integer.toString(player.get(1)));
+        for (int i = 0; i<2; i++) {
+            int sum = 0;
+            int mul = 1;
+            String now_num = playerString.get(i);
+            int digits = now_num.length();
+            for (int j=0; j<digits; j++) {
+                int n = Integer.parseInt(String.valueOf(now_num.charAt(j)));
+                sum += n;
+                mul *= n;
+            }
+            if (Math.max(sum, mul)>maximum_value) {
+                maximum_value = Math.max(sum, mul);
+            }
+        }
+        return maximum_value;
     }
 }
