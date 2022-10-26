86069e0bce8ce92dd65cc5fb25ab06623033e902
backFox <jorealbusiness@gmail.com>
2022-10-26 18:25:39 +0900

    feat(problem3): 문제 3 알고리즘 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int totalClap = 0;
+        for (int count = 1; count <= number; count++) {
+            totalClap += countThreeSixNine(count);
+        }
+        return totalClap;
+    }
+
+    private static int countThreeSixNine(Integer count) {
+        String countString = count.toString();
+        return countString.length() - countString
+            .replace("3", "")
+            .replace("6", "")
+            .replace("9", "")
+            .length();
     }
 }
