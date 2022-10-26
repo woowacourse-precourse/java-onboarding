024e297e6a75ae7246bb4bed48b4eded8f087aa0
bjk1649 <qhwns1649@naver.com>
2022-10-26 23:37:54 +0900

    feat: 점수를 계산하는 클래스 안에 입력값의 각 자리 숫자  더해주는 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -27,3 +27,15 @@ class Validation{
                 || input.get(0) <= MIN_PAGE);
     }
 }
+
+class ScoreController{
+    private static int sumDigits(int num){
+        int result = 0;
+
+        while (num != 0) {
+            result += num%10;
+            num /= 10;
+        }
+        return result;
+    }
+}
