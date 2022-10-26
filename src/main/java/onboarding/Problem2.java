f3c488cda3b2010940bfabff98d4a065873d5364
jmj005 <jmj00005@naver.com>
2022-10-26 21:55:55 +0900

    feat: 중복 문자 제거 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,8 +15,30 @@ public class Problem2 {
         return 0;
     }
 
+    private static String removeOverlap(String word) {
+        if (checkOverlap(word) == 0)
+            return word;
+        if (word.length() == 2 && checkOverlap(word) == 1)
+            return "";
+
+        int i, k = 0;
+        char[] tmp = word.toCharArray();
+        for (i = 1; i < word.length(); i ++) {
+            if (word.charAt(i - 1) == word.charAt(i)) {
+                while (i < word.length() && word.charAt(i - 1) == word.charAt(i))
+                    i++;
+            } else
+                tmp[k++] = word.charAt(i - 1);
+        }
+        tmp[k++] = tmp[i - 1];
+        word = new String(tmp).substring(0, k);
+
+        return removeOverlap(word);
+    }
+
     public static String solution(String cryptogram) {
         String answer = "";
+        answer = removeOverlap(cryptogram);
         return answer;
     }
 }
