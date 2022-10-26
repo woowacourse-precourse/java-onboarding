b3a95f841869766bdbdf002f9d7146a3206e5bbf
Woong99 <dndry1072@naver.com>
2022-10-26 22:17:04 +0900

    feat: 문제 2 - 연속하는 중복 문자를 찾아 인덱스를 반환하는 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,13 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static int getContinuousIndex(String text) {
+        for (int i = 0; i < text.length() - 1; i++) {
+            if (text.charAt(i) == text.charAt(i + 1)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
