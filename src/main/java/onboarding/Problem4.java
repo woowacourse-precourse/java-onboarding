79d9bc5b3d71bb87d9c14432f7f5cc236c2db74b
seungmin-park <tmddudals369@naver.com>
2022-10-26 23:26:53 +0900

    1주차 과제 4번

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.StringTokenizer;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder sb = new StringBuilder(word);
+        for (int i =0; i < sb.length(); i++){
+            if (Character.isUpperCase(sb.charAt(i))){
+                sb.setCharAt(i, (char)(90 - (sb.charAt(i) - 65)));
+            }else if (Character.isLowerCase(sb.charAt(i))){
+                sb.setCharAt(i, (char)(122 - (sb.charAt(i) - 97)));
+            }
+        }
+
+        answer = sb.toString();
         return answer;
     }
 }
