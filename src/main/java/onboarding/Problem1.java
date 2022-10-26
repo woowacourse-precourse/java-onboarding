c5a57726e13b54f6b169a024792e38334221f7c5
kimJaeHyeon <kmr2644@gmail.com>
2022-10-26 18:25:56 +0900

    feat: implement method for page calculation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,7 +8,30 @@ class Problem1 {
         if (!(isValidPage(pobi) && isValidPage(crong))) {
             return -1;
         }
+        int pobiResult = calculatePage(pobi);
+        int crongResult = calculatePage(crong);
         return answer;
+    }
+
+    private static int calculatePage(List<Integer> pageList) {
+        return Math.max(
+                multiplePage(pageList.get(0)),
+                plusPage(pageList.get(1))
+        );
+    }
+
+    private static int multiplePage(int page) {
+        if (page < 10) {
+            return page;
+        }
+        return (page % 10) * (page / 10);
+    }
+
+    private static int plusPage(int page) {
+        if (page < 10) {
+            return page;
+        }
+        return (page % 10) + (page / 10);
     }
 
     private static boolean isValidPage(List<Integer> pageList) {
