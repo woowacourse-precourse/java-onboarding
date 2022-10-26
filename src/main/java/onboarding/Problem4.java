3a2f23c17634e9f785dffde382872fcd5f43c436
JaehongDev <println@kakao.com>
2022-10-27 02:04:30 +0900

    docs (Problem4) : 문제4 기능 구현 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import onboarding.problem4.ContraryCharacter;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answerBuilder = new StringBuilder();
+        for(Character character : word.toCharArray()){
+            answerBuilder.append(new ContraryCharacter(character).character());
+        }
+        return answerBuilder.toString();
     }
 }
