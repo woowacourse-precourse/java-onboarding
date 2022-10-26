629183b2ba6d8a1108d87f1964a353f68effd670
Kayoung <rkdkdudjd@gmail.com>
2022-10-27 00:42:59 +0900

    1번: solution 메서드
    포비와 크롱의 최댓값을 비교하는 로직

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiMax = getMaxValueFromList(pobi);
+        int crongMax = getMaxValueFromList(crong);
+        if (pobiMax > crongMax) {
+            return 1;
+        } else if (pobiMax < crongMax) {
+            return 2;
+        }
+        return 0;
     }
 }
