d945c28c392ff2f4177f2ed07e1667cd5aa45774
nswon <swnam0729@gmail.com>
2022-10-27 02:15:54 +0900

    refactor(pageNumberException): 페이지 번호 검증 메서드에서 내부 클래스로 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (validatePageNumber(pobi) == Constance.EXCEPTION_MESSAGE || validatePageNumber(crong) == Constance.EXCEPTION_MESSAGE) {
+        if (PageNumberException.validates(pobi) == Constance.EXCEPTION_MESSAGE ||
+                PageNumberException.validates(crong) == Constance.EXCEPTION_MESSAGE) {
             return Constance.EXCEPTION_MESSAGE;
         }
 
@@ -61,48 +62,53 @@ class Problem1 {
         return Math.max(number1, number2);
     }
 
-    private static int validatePageNumber(List<Integer> user) {
-        int leftPageNumber = user.get(0);
-        int rightPageNumber = user.get(1);
-        if (!isPageOdd(leftPageNumber) ||
-                isPageOdd(rightPageNumber) ||
-                isFirstPage(leftPageNumber) ||
-                isEndPage(rightPageNumber) ||
-                isPageSizeLackOrOverflow(leftPageNumber) ||
-                isPageSizeLackOrOverflow(rightPageNumber) ||
-                !isPageContinue(leftPageNumber, rightPageNumber)) {
-            return Constance.EXCEPTION_MESSAGE;
-        }
-
-        return 0;
-    }
-
-    private static boolean isPageOdd(int pageNumber) {
-        return pageNumber % 2 != 0;
-    }
-
-    private static boolean isFirstPage(int pageNumber) {
-        return pageNumber == Constance.START_PAGE_NUMBER || pageNumber == Constance.START_PAGE_NUMBER + 1;
-    }
-
-    private static boolean isEndPage(int pageNumber) {
-        return pageNumber == Constance.END_PAGE_NUMBER - 1 || pageNumber == Constance.END_PAGE_NUMBER;
-    }
-
-    private static boolean isPageSizeLackOrOverflow(int pageNumber) {
-        return pageNumber < Constance.START_PAGE_NUMBER || pageNumber > Constance.END_PAGE_NUMBER;
-    }
-
-    private static boolean isPageContinue(int leftPageNumber, int rightPageNumber) {
-        return leftPageNumber+1 == rightPageNumber;
-    }
-
-    private final class Constance {
+    private static final class Constance {
         private static final int START_PAGE_NUMBER = 1;
         private static final int END_PAGE_NUMBER = 400;
         private static final int WINNER_USER1_MESSAGE = 1;
         private static final int WINNER_USER2_MESSAGE = 2;
         private static final int SCORE_SAME_MESSAGE = 0;
         private static final int EXCEPTION_MESSAGE = -1;
+    }
+
+    public static class PageNumberException {
+
+        private PageNumberException() {}
+
+        public static int validates(List<Integer> user) {
+            int leftPageNumber = user.get(0);
+            int rightPageNumber = user.get(1);
+            if (!isPageOdd(leftPageNumber) ||
+                    isPageOdd(rightPageNumber) ||
+                    isFirstPage(leftPageNumber) ||
+                    isEndPage(rightPageNumber) ||
+                    isPageSizeLackOrOverflow(leftPageNumber) ||
+                    isPageSizeLackOrOverflow(rightPageNumber) ||
+                    !isPageContinue(leftPageNumber, rightPageNumber)) {
+                return Constance.EXCEPTION_MESSAGE;
+            }
+
+            return 0;
+        }
+
+        private static boolean isPageOdd(int pageNumber) {
+            return pageNumber % 2 != 0;
+        }
+
+        private static boolean isFirstPage(int pageNumber) {
+            return pageNumber == Constance.START_PAGE_NUMBER || pageNumber == Constance.START_PAGE_NUMBER + 1;
+        }
+
+        private static boolean isEndPage(int pageNumber) {
+            return pageNumber == Constance.END_PAGE_NUMBER - 1 || pageNumber == Constance.END_PAGE_NUMBER;
+        }
+
+        private static boolean isPageSizeLackOrOverflow(int pageNumber) {
+            return pageNumber < Constance.START_PAGE_NUMBER || pageNumber > Constance.END_PAGE_NUMBER;
+        }
+
+        private static boolean isPageContinue(int leftPageNumber, int rightPageNumber) {
+            return leftPageNumber+1 == rightPageNumber;
+        }
     }
 }
