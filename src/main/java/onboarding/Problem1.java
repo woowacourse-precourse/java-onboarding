44e9fe80c37894bde36fab0857fa1cc0f96609da
yg0585 <yg0585@naver.com>
2022-10-26 19:13:43 +0900

    세 번째 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,7 @@ import java.util.List;
 //------기능------
 //1. 곱하기
 //2. 더하기
+//3. 곱하기 더하기 중 더 큰 수 구하기
 //3. max
 
 class Problem1 {
@@ -32,8 +33,15 @@ class Problem1 {
         return num;
     }
 
+    private int pickNum(int pageNum) {
+        return Math.max(addNum(pageNum), mulNum(pageNum));
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiNum, crongNum;
+
+        pobiNum = Math.max();
         return answer;
     }
 }
