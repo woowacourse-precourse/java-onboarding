ba1aac0dd496528173fc2cb7880ae59e8357b4b6
aksmf1442 <aksmf1442@naver.com>
2022-10-27 01:22:11 +0900

    style: Problem3 불필요한 주석 제거

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,18 +3,6 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * goal: 369게임을 할 때 1~number까지 몇 번의 손뼉을 치는지 리턴
- */
-
-/**
- * 문제 풀이 순서
- * 1. 먼저 list에 박수를 쳐야 할 떄인 3, 6, 9를 초기화 해준다.
- * 2. 박수는 3부터 치기 때문에 3~number까지 for문을 돌려준다.
- * 3. for문을 돌면서 현재 num을 String으로 변환해주고 3, 6, 9가 몇 개 포함되어 있는지 확인한다.
- * 4. for문을 돌면서 박수친 횟수를 모두 더해주면 결과가 나온다.
- */
-
 public class Problem3 {
     static List<Character> clapList = new ArrayList<>();
 
