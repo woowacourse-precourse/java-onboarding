341a2a56592e9de8562470b1e540f633fd056a72
K-jun98 <alap_u@naver.com>
2022-10-26 22:05:25 +0900

    feat - 숫자의 3,6,9 포함 찾기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 0; i <= number; i++) {
+            answer += getClapCount(i);
+        }
         return answer;
+    }
+
+
+    private static int getClapCount(int number) {
+        int clapCount = 0;
+        String num = String.valueOf(number);
+        for (int i = 0; i < num.length(); i++) {
+            char value = num.charAt(i);
+            if (value == '3' || value == '6' || value == '9') {
+                clapCount++;
+            }
+        }
+        return clapCount;
     }
 }
