bc77a93c630814e4a52cb87c23ebf3a2f71fe960
jeomxon <jeomxon@gmail.com>
2022-10-26 23:02:54 +0900

    feat: validate input pages

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,26 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!isCorrectInput(pobi.get(0), pobi.get(1)) || !isCorrectInput(crong.get(0), crong.get(1))) {
+            return -1;
+        }
+
+
+
         return answer;
     }
+
+    public static boolean isCorrectInput(int numOfFirstPage, int numOfSecondPage) {
+
+        if (numOfFirstPage < 1 || numOfSecondPage > 400) {
+            return false;
+        }
+
+        if (((numOfFirstPage + 1) != numOfSecondPage) || numOfFirstPage % 2 != 1 || numOfSecondPage % 2 != 0) {
+            return false;
+        }
+        return true;
+    }
+
 }
