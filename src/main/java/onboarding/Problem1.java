6873cf9a07e8e63b8ebe80a5294ddadf670eba45
5nam <ohnam90@gmail.com>
2022-10-26 22:46:55 +0900

    feat: Add function to change page digit to int array

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,18 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    // int -> int[] 변환 후 리턴
+    private static int[] changeArray(int input) {
+        int[] digits = Stream.of(String.valueOf(input).split(""))
+                .mapToInt(Integer::parseInt).toArray();
+        return digits;
     }
 }
