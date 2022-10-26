50523afc09947e94c16a01a70f484d905f15b6a4
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-26 21:56:13 +0900

    feat : 숫자가 주어졌을 때 자리수를 확인하는 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,20 +9,35 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
+        int answer = 0;
+
         int pobiFirstNumber = pobi.get(0);
         int pobiSecondNumber = pobi.get(1);
+
         int crongFirstNumber = pobi.get(0);
         int crongSecondNumber = pobi.get(1);
 
         if (pobiFirstNumber + 1 != pobiSecondNumber || crongFirstNumber + 1 != crongSecondNumber){
             pageSequence = false;
-            return -1;
-        } else{
-
-
-        int answer = Integer.MAX_VALUE;
-        return answer;
+            answer = -1;
         }
+
+        int pobiMaxValue = 0;
+        int crongMaxValue = 0;
+
+        // 자리수 확인
+
+
+
+
+        answer = Integer.MAX_VALUE;
+        return answer;
     }
+
+    private static int numberLength(int number){
+        int length = (int)(Math.log10(number)+1);
+        return length;
+    }
+
 
 }
