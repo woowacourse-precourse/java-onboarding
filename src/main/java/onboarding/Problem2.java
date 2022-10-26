3e690e6ce464c8d03ea1dfe1a40a78034c9f3913
kevstevie <jianna442@gmail.com>
2022-10-26 17:34:23 +0900

    fix: stack to deque

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,27 +1,28 @@
 package onboarding;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
         char[] chars = cryptogram.toCharArray();
-        char tmp = ' ';
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> chDeque = new LinkedList<>();
+
         for(char ch : chars){
-            if(!stack.isEmpty()){
-                checkContinuous(stack, ch);
+            if(!chDeque.isEmpty()){
+                checkContinuous(chDeque, ch);
                 continue;
             }
-            stack.push(ch);
+            chDeque.addLast(ch);
         }
-
         return answer;
     }
 
-    static void checkContinuous(Stack<Character> stack, char ch){
-        if(stack.peek() == ch){
-            stack.pop();
+    static void checkContinuous(Deque<Character> chDeque, char ch){
+        if(chDeque.getLast() == ch){
+            chDeque.removeLast();
         }
     }
 }
