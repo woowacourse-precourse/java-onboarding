469878a64abc1b9b1f038ef39b99d4dd61794c33
hum02 <ehdgma02@gmail.com>
2022-10-26 16:45:38 +0900

    feat:P2 해독기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,41 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        String deciphered = decipher(cryptogram);
+
+        while(true){
+             String curDeciphered = decipher(deciphered);
+
+             if(deciphered.equals( curDeciphered )){
+                 answer = deciphered;
+                 break;
+             }else{
+                 deciphered = curDeciphered;
+             }
+        }
+
         return answer;
+    }
+
+    private static String decipher(String cryptogram){
+        StringBuilder sb = new StringBuilder();
+
+        char[] cryptoChars = cryptogram.toCharArray();
+
+        for(int i=1; i<cryptogram.length(); i++){
+            if(cryptoChars[i] == cryptoChars[i-1]){
+                cryptoChars[i-1] = '0';
+                cryptoChars[i] = '0';
+            }
+        }
+
+        for(char c :cryptoChars){
+            if(c != '0'){
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 }
