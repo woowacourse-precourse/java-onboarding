98be3bfc18e2f6e3f897a343fd312634030eb273
Kid-Chang <pch3310@gmail.com>
2022-10-26 21:39:29 +0900

    feat: solved problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        if(number > 10000) return -1;
+        for(int i = 0; i<=number; i++){
+            answer += count3(i);
+        }
         return answer;
+    }
+
+    /**
+     * 파라미터로 들어오는 숫자에 3이 몇번 들어갔는지 카운트
+     * @param value
+     * @return
+     */
+    public static int count3(int value){
+        int count = 0;
+        char[] num = Integer.toString(value).toCharArray();
+
+        for(int i=0; i<num.length; i++) {
+            if (num[i] == '3' || num[i] == '6' || num[i] == '9') count++;
+        }
+        return count;
     }
 }
