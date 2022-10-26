4a03c7a5a1bcea462fcfda2a9f897c96271fe6ef
junhei Lee <jun2ee22@naver.com>
2022-10-26 23:21:17 +0900

    feat: 두 수중 큰 수 반환하는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,18 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static int compare(int num1, int num2) {
+        int bigNum = 0;
+
+        if (num1 > num2) {
+            bigNum = num1;
+        } else {
+            bigNum = num2;
+        }
+        return bigNum;
+    }
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
