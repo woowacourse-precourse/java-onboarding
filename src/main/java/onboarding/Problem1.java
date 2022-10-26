cd76ba683154140524679458912e6642cfe9e195
Namyunsuk <yunsuk0328@naver.com>
2022-10-26 21:10:16 +0900

    feat(calculate): 페이지 번호를 사용한 계산 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,25 @@ package onboarding;
 
 import java.util.List;
 
+class calculate{
+    public static int sumOfPageNumbers(int pageNumber){
+        int sum=0;
+        while(pageNumber>0){
+            sum+=pageNumber%10;
+            pageNumber/=10;
+        }
+        return sum;
+    }
+    public static int mulOfPageNumbers(int pageNumber){
+        int mul=1;
+        while(pageNumber>0){
+            mul*=pageNumber%10;
+            pageNumber/=10;
+        }
+        return mul;
+    }
+}
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
