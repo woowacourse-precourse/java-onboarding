832e91408aacc12accb99d0b3b60159974256f57
GwansuShin <happysun7080@naver.com>
2022-10-27 00:23:17 +0900

    feat(Problem2): 최초 구현
    제한사항 검사
    [재귀]암호해독(중복 제거, 해독 완료 검사)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,95 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        if (!validateInput(cryptogram)) {
+            return cryptogram;
+        }
+        return decodeCryptogram(cryptogram);
+    }
+
+    private static boolean validateInput(String cryptogram) {
+        return Validation.validate(cryptogram);
+    }
+
+    private static String decodeCryptogram(String cryptogram) {
+        if (isDecoded(cryptogram)) {
+            return cryptogram;
+        }
+        cryptogram = deleteDuplication(cryptogram);
+        return decodeCryptogram(cryptogram);
+    }
+
+    private static String deleteDuplication(String cryptogram) {
+        List<Character> newCharacterList = new ArrayList<>();
+        List<Character> characterList = convertStringToCharacterList(cryptogram);
+        int index = 0;
+
+        newCharacterList.add(characterList.get(0));
+        characterList.remove(0);
+        for (char character : characterList) {
+            if (newCharacterList.get(index) == character) {
+                newCharacterList.remove(index--);
+                continue;
+            }
+            newCharacterList.add(character);
+            index++;
+        }
+        return convertCharacterListToString(newCharacterList);
+    }
+
+    private static boolean isDecoded(String cryptogram) {
+        char previousCharacter = 0;
+        for (char character : convertStringToCharacterList(cryptogram)) {
+            if (previousCharacter == character) {
+                return false;
+            }
+            previousCharacter = character;
+        }
+        return true;
+    }
+
+    private static List<Character> convertStringToCharacterList(String string) {
+        return string
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+    }
+
+    private static String convertCharacterListToString(List<Character> characterList) {
+        return characterList
+                .stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+
+    static abstract class Validation {
+
+        private static final int MIN_LENGTH = 1;
+        private static final int MAX_LENGTH = 1000;
+        private static final String LOWER_ALPHABET_REGEX = "^[a-z]*$";
+
+        public static boolean validate(String cryptogram) {
+            return validateNotNull(cryptogram)
+                    && validateSize(cryptogram)
+                    && validateLowerAlphabet(cryptogram);
+        }
+
+        private static boolean validateNotNull(String cryptogram) {
+            return cryptogram != null;
+        }
+
+        private static boolean validateSize(String cryptogram) {
+            return cryptogram.length() >= MIN_LENGTH
+                    && cryptogram.length() <= MAX_LENGTH;
+        }
+
+        private static boolean validateLowerAlphabet(String cryptogram) {
+            return cryptogram.matches(LOWER_ALPHABET_REGEX);
+        }
     }
 }
