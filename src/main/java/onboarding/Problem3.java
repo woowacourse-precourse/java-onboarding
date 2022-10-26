64654aaa07e91b3f27b1e8855ce96f5e2e6b5862
yj2980 <adore2100@naver.com>
2022-10-26 19:50:58 +0900

    feat : 중복을 체크하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,9 +8,24 @@ public class Problem3 {
         return answer;
     }
 
+    public static int checkDuplicaion(String strNum)
+    {
+        int check = 30;
+        int duplicaionCnt = 0;
+        
+        if (Integer.parseInt(strNum) >= check)
+        {
+            for (int i = check; i < strNum.length(); i++)
+            {
+                if (strNum.charAt(i) == '3' || strNum.charAt(i) == '6' || strNum.charAt(i) == '9')
+                    duplicaionCnt++;
+            }
+        }
+        return duplicaionCnt;
+    }
     public static int checkNum(int number)
     {
-        String strNum;
+        String strNum = null;
         int cnt = 0;
 
         for (int i = 1; i <= number; i++)
@@ -19,6 +34,7 @@ public class Problem3 {
             if (strNum.contains("3") || strNum.contains("6") || strNum.contains("9"))
                 cnt++;
         }
-        return cnt;
+        cnt += checkDuplicaion(strNum);
+        return cnt ;
     }
 }
