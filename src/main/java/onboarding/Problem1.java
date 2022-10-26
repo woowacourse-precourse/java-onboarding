0e779aa359f3b4c45e1a42fa70ac69c658d77444
ensk26 <tjensk26@gmail.com>
2022-10-27 01:35:00 +0900

    feat: 페이지 번호의 각 자릿수 연산 후 최댓값을 반환하는 기능 추가 ,problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,9 +9,17 @@ class Problem1 {
         return answer;
     }
 
-    //페이지 번호의 각 자리수를 모두 더하기, 모두 빼기 한 가장 큰 수 반환
+    //페이지 번호의 각 자릿수를 모두 더하기, 모두 빼기 한 가장 큰 수 반환
     static int getMaxPageOperationValue(int num) {
-        return -1;
+        int sum = 0, mul = 1;
+
+        while (num > 0) {
+            sum += num % 10;
+            mul *= num % 10;
+            num /= 10;
+        }
+
+        return Math.max(sum, mul);
     }
 
     //잘못된 페이지인지 확인
