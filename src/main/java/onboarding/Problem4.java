b4664cb8f1d66894ed8d7b29d00f915d3293c103
mckim123 <minchan06@gmail.com>
2022-10-27 03:43:02 +0900

    문제 4 기능 목록 구성 및 기능 1 구현(문제 4 완료)
    문제 4의 기능 목록을 작성하고 기능을 모두 구현하였습니다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i=0; i<word.length();i++){
+            int ascii_num = word.charAt(i);
+            if (65 <= ascii_num && ascii_num <= 90) {
+                ascii_num = 155 - ascii_num;
+            } else if (97 <= ascii_num && ascii_num <= 122) {
+                ascii_num = 219 - ascii_num;
+            }
+            answer += (char) ascii_num;
+        }
         return answer;
     }
 }
