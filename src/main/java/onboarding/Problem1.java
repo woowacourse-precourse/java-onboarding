899c8961f64fe31edf559683761d3dcd7c2b37d5
YE-EUN <musiclove529@naver.com>
2022-10-26 18:44:23 +0900

    feature : add checkValid() - 예외사항 처리 - checkIsValidPages() : 올바른 페이지 번호인지 확인(왼쪽 페이지 번호가 홀수이고, 오른쪽 페이지 번호가 짝수이고, 각 페이지 번호가 1~400 사이인지) - checkPagesDiff() : 왼쪽 페이지 번호와 오른쪽 페이지 번호의 차가 1인지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,8 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    static final int TOTAL_PAGES = 400;
+    static final int START_PAGE = 1;
+    static final int END_PAGE = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
@@ -11,12 +12,41 @@ class Problem1 {
         int pobiScore = getScore(pobi);
         int crongScore = getScore(crong);
 
+        if(!(checkValid(pobi) && checkValid(crong))) return -1;
+
         if(pobiScore > crongScore) answer = 1;
         else if(pobiScore < crongScore) answer = 2;
         else if(pobiScore == crongScore) answer = 0;
-        else answer = -1;
 
         return answer;
+    }
+
+    public static boolean checkValid(List<Integer> pageNums){
+        int leftPage = pageNums.get(0);
+        int rightPage = pageNums.get(1);
+
+        if(checkIsValidPages(leftPage, rightPage) && checkPagesDiff(leftPage, rightPage)) return true;
+
+        return false;
+    }
+
+    public static boolean checkIsValidPages(int leftPage, int rightPage){
+
+        if(leftPage%2!=1) return false;
+        if(rightPage%2!=0) return false;
+
+        if(leftPage < START_PAGE || leftPage >= END_PAGE) return false;
+        if(rightPage <= START_PAGE || rightPage > END_PAGE) return false;
+
+        return true;
+    }
+
+    public static boolean checkPagesDiff(int leftPage, int rightPage){
+        int diff = Math.abs(leftPage - rightPage);
+
+        if(diff == 1) return true;
+
+        return false;
     }
 
     public static int getScore(List<Integer> pageNums){
@@ -24,13 +54,10 @@ class Problem1 {
         int leftPage = pageNums.get(0);
         int rightPage = pageNums.get(1);
 
-        //왼쪽 페이지에서 얻어지는 가장 큰 수 구하기
         int leftScore = getBiggerNum(leftPage);
 
-        //오른쪽 페이지에서 얻어지는 가장 큰 수 구하기
         int rightScore = getBiggerNum(rightPage);
 
-        //최종 점수 선택
         score = Math.max(leftScore, rightScore);
 
         return score;
