8bc0bcdf96a901d95715cb89f2c952905912b51e
K-Diger <kitten.diger@gmail.com>
2022-10-26 20:06:29 +0900

    Week1 : Problem3 Solved - 입력받은 숫자만큼 반복하여 각 반복마다 3, 6, 9 가 있는지 판별 후 Count 하는 로직의 메서드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+
+// 뭔가 하드코딩한 느낌이나서 다시 살펴보면 좋을 것 같음
 public class Problem3 {
+
+    public static int countClap(int number) {
+        int result = 0;
+
+        for (int i = 1; i < number + 1; i++) {
+
+            String currentValue = String.valueOf(i);
+            int currentValueLength = currentValue.length();
+
+            for (int j = 0; j < currentValueLength; j++) {
+                if (currentValue.charAt(j) == '3') {
+                    result += 1;
+                } else if (currentValue.charAt(j) == '6') {
+                    result += 1;
+                } else if (currentValue.charAt(j) == '9') {
+                    result += 1;
+                }
+            }
+        }
+        return result;
+    }
+
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return countClap(number);
     }
 }
