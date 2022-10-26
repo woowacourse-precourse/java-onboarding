886de68bc13392ef4feec2aeb935725d827ecb72
hodomaroo <enghemdwkd@gmail.com>
2022-10-26 22:51:17 +0900

    problem7의 명세 목록 정리 완료

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,9 +3,22 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ *  🚀 기능 명세
+    사용자의 정보를 저장할 객체 (이름 / 친구 관계 / 점수)
+    * 이름을 반환하는 메서드 (getName : String)
+    * 친구 목록을 반환하는 메서드 (getFriends : Set)
+
+    * 점수를 반환하는 메서드 (getScore : int)
+    * 점수를 변경하는 메서드 (setScore : int)
+
+    추천 친구를 구할 메서드(getRecommnededFriends)
+
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
 }
+
