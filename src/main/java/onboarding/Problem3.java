c86400ac15451f97c6eae47d6293b886f59034fc
박준용 <jo101044@naver.com>
2022-10-26 20:35:25 +0900

    문제3 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // 1 부터 number 까지 확인
+        for (int i = 1; i <= number; i++) {
+            String str = String.valueOf(i);
+
+            for (int j = 0; j < str.length(); j++) {
+                int clap = str.charAt(j) - '0';
+
+                // 3 6 9가 포함될 시 answer+1
+                if(clap != 0 && clap%3 == 0) answer++;
+            }
+        }
+
         return answer;
     }
 }
