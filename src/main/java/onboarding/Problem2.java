430b35c9c27451536d53a60c7bc569be548f60aa
whdcks420 <whdcks420@gmail.com>
2022-10-27 05:00:35 +0900

    feat: PROBLEM2 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+
+        char[] chars = cryptogram.toCharArray();
+        for (Character aChar : chars) {
+            duplicateCheck(stack, aChar);
+        }
+
+        return stack.stream().map(String::valueOf).collect(Collectors.joining());
     }
+
+    private static void duplicateCheck(Stack<Character> stack, Character aChar) {
+        if (stack.isEmpty()) {
+            stack.push(aChar);
+            return;
+        }
+        if (stack.peek().equals(aChar)) {
+            stack.pop();
+            return;
+        }
+        stack.push(aChar);
+    }
+
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> list = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        for (Integer integer : list) {
+            answer.add(money / integer);
+            money %= integer;
+        }
         return answer;
     }
 }
