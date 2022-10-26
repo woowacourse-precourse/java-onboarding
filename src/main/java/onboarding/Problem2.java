331ec72da9cb3015816d40eb6f0894bd1767dacb
Laeng <Laeng@users.noreply.github.com>
2022-10-26 19:59:34 +0900

    feat(problem2): 문자열 분할 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static boolean validateCryptogram(String cryptogram) {
+        int lengthCryptogram = cryptogram.length();
+
+        // 문자열 길이가 1 이상 ~ 1000 이하인지 확인
+        if (lengthCryptogram < 1 || lengthCryptogram > 1000) {
+            return false;
+        }
+
+        // 문자열이 알파벳 소문자인지 확인
+        // TODO
+
+        return true;
+    }
+
+    private static String[] splitCryptogram(String cryptogram) {
+        return cryptogram.split("");
     }
 }
