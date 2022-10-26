47fa1f9107343a9cf62e8e80a5610aec2479264f
JunH <tjdtls690@gmail.com>
2022-10-26 20:00:30 +0900

    :sparkles: feat : 문자열 전체 변환하기 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,17 +1,14 @@
 package onboarding;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Problem4 {
     private static final List<Character> LOWERCASE_LETTERS = lowercaseLettersInit();
     private static final List<Character> UPPERCASE_LETTERS = uppercaseLettersInit();
-    
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
     
     private static List<Character> lowercaseLettersInit() {
         return IntStream.rangeClosed(0, 25)
@@ -23,6 +20,32 @@ public class Problem4 {
         return IntStream.rangeClosed(0, 25)
                 .mapToObj(number -> (char) (90 - number))
                 .collect(Collectors.toList());
+    }
+    
+    public static String solution(String word) {
+        String answer = "";
+    
+        final char[] chars = word.toCharArray();
+        for (int index = 0; index < chars.length; index++) {
+            if (isAlphabet(chars[index])) {
+                chars[index] = toConvertAlphabet(chars[index]);
+            }
+        }
+        
+        return answer;
+    }
+    
+    private static char toConvertAlphabet(final char aChar) {
+        if (Character.isUpperCase(aChar)) {
+            return toConvertUpperCase(aChar);
+        }
+        
+        return toConvertLowerCase(aChar);
+    }
+    
+    private static boolean isAlphabet(final char aChar) {
+        final Matcher matcher = Pattern.compile("[a-z]|[A-Z]").matcher(String.valueOf(aChar));
+        return matcher.matches();
     }
     
     private static Character toConvertLowerCase(Character character) {
