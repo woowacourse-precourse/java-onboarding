ba506d6aab1f4aa0ec8f75c0c9704c26d43d0fc4
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-26 19:52:40 +0900

    Feat: 구현 Problem2 toEliminateDuplicate 함수 - 현재 문장의 중복을 제거하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+
+	private static String toEliminateDuplicate(String givenString) {
+		StringBuilder eliminateDuplicateString = new StringBuilder();
+		char compareString = givenString.charAt(0);
+		int isDuplicated = 0;
+
+		for (int i = 1; i < givenString.length(); i++) {
+			if (givenString.charAt(i) != compareString) {
+				if (isDuplicated == 0) {
+					compareString = givenString.charAt(i);
+					eliminateDuplicateString.append(compareString);
+				} else {
+					isDuplicated = 0;
+					continue;
+				}
+			} else if (givenString.charAt(i) == compareString) {
+				isDuplicated = 1;
+			}
+		}
+		if (isDuplicated == 0) {
+			eliminateDuplicateString.append(compareString);
+		}
+
+		return eliminateDuplicateString.toString();
+	}
+
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+		return answer;
+	}
 }
