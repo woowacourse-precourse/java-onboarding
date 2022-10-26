b110f13b9201d1f57913afa053a716ed1911618f
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 20:03:15 +0900

    fix(Problem3.java): char형 문자와 int형 정수를 비교하던 코드 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,7 @@ package onboarding;
 public class Problem3 {
     public static int count369(int n){
         return (int)String.valueOf(n).chars()
-                .filter(o -> o==3 || o==6 || o==9)
+                .filter(o -> o=='3' || o=='6' || o=='9')
                 .count();
     }
 
