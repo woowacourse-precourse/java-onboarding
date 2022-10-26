744a8c5734df6753afed631d3353cee99138fbd8
main <jang0929@naver.com>
2022-10-26 20:24:29 +0900

    Problem 2 바꿀값이 있는지 확인하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,18 @@ package onboarding;
  * 2. 바꿀 값이 있을 경우 바꾸는 함수
  */
 public class Problem2 {
+    /**
+     * 바꿀 값이 있는지 확인하는 함수
+     * @param s
+     * @return 바꿀값이 시작되는 index를 return
+     */
+    static int checkPossible(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) return i;
+        }
+        return -1;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
