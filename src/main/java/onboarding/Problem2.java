120625a75120de9c9d00f5527ecfcd2e9017238f
hyun6ik <fkanwm@naver.com>
2022-10-26 20:03:47 +0900

    feat(16) : 기능 요구 사항에 필요한 값들 초기화

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        final char[] chars = cryptogram.toCharArray();
+        final StringBuilder newCryptogram = new StringBuilder();
+
+        char duplicateChar = ' ';
+        boolean hasNotDuplicate = false;
+
+
+
+
+        System.out.println(newCryptogram);
+        return newCryptogram.toString();
+    }
+
+    public static void main(String[] args) {
+        Problem2.solution("browoanoommnaon");
     }
 }
