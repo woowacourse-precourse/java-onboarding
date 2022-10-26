cea44211a1f73de6364b1911505ccab5974d61d1
kevstevie <jianna442@gmail.com>
2022-10-26 16:41:26 +0900

    fix: chooseMultiSum 탐색범위

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if(checkException(pobi) || checkException(crong)){
-            return 0;
+            return -1;
         }
         int pobiNum = getMax(pobi);
         int crongNum = getMax(crong);
@@ -48,17 +48,16 @@ class Problem1 {
         int multi = 1;
 
         int tmp = page;
-        for (int i = 0; i < len - 1; i++) {
+        for (int i = 0; i < len; i++) {
             sum += tmp % 10;
             tmp /= 10;
         }
 
         tmp = page;
-        for (int i = 0; i < len - 1; i++) {
+        for (int i = 0; i < len; i++) {
             multi *= tmp % 10;
             tmp /= 10;
         }
-
         return Math.max(sum, multi);
     }
 }
