ff3d2fbe5b71f22fde5d75c941a090ed3e37a592
Kim kyunghee <kkh2415@naver.com>
2022-10-27 00:15:45 +0900

    add : #Problem3-1 ~ number 범위의 int[] 배열 생성 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * a. 1 ~ number 범위의 int[] 배열 생성 기능 구현
+ * b. 1 ~ number 범위의 각 각의 숫자에서 박수 횟수 계산하는 기능 구현
+ * */
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        int[] numbers = IntStream.rangeClosed(1, number).toArray();
+
+        return count;
     }
 }
