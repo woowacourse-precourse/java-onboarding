ad1a98ffbe9b93e4c3d03c3c9ba67630cb439c97
1eejisoo <jisu991012@gmail.com>
2022-10-27 05:31:13 +0900

    [Problem 3] Feat: 1 ~ number의 수 나열을 문자열로 만드는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,31 @@
-
 package onboarding;
 
 public class Problem3 {
+    /***
+     *
+     * 기능1. 1 ~ number까지의 수 나열을 문자열로 만드는 기능 -> numberSequenceToString()
+     * 기능2. 기능2의 합을 구하는 기능 -> getClapCount()
+     *
+     * 기능2 구현을 위한 함수 - 문자열에서 특정 문자의 횟수를 세는 함수 -> count()
+     */
+
     public static int solution(int number) {
         int answer = 0;
+        String routine = "";
+
+        routine = numberSequenceToString(number);
+        // answer = getClapCount(routine);
+
         return answer;
     }
+
+    // 기능 1
+    public static String numberSequenceToString(int number) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= number; i++)  sb.append(i);
+
+        return sb.toString();
+    }
+
 }
