6c50ec8c581f3bf5176addf7098e911d94999aff
kevstevie <jianna442@gmail.com>
2022-10-26 17:47:27 +0900

    fix: remove checkContinuous add to solution

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,21 +10,15 @@ public class Problem2 {
         Deque<Character> chDeque = new LinkedList<>();
 
         for(char ch : chars){
-            if(!chDeque.isEmpty()){
-                checkContinuous(chDeque, ch);
+            if(!chDeque.isEmpty() && chDeque.getLast() == ch){
+                chDeque.removeLast();
                 continue;
             }
             chDeque.addLast(ch);
         }
         return charToString(chDeque);
     }
-
-    static void checkContinuous(Deque<Character> chDeque, char ch){
-        if(chDeque.getLast() == ch){
-            chDeque.removeLast();
-        }
-    }
-
+    
     static String charToString(Deque<Character> chDeque){
         String result = "";
         while (!chDeque.isEmpty()) {
