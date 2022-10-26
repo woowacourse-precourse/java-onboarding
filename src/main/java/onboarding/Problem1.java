9c7db59ef301235f319ea8a95da0b12cae5b60e6
sangho-jeon <jeonsangho1587@gmail.com>
2022-10-26 16:34:40 +0900

    feat      : problem1 validation check added.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static final int PAGE_MIN = 1;
+    static final int PAGE_MAX = 399;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static boolean isValid(List<Integer> page) {
+        int frontPage = page.get(0);
+        int endPage = page.get(1);
+        if((PAGE_MIN < frontPage && frontPage < PAGE_MAX) && frontPage % 2 == 1){
+            return endPage - frontPage == 1;
+        }
+        return false;
     }
 
     public static int getScore(int pageNumber) {
