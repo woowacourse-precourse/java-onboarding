a4e1223e9af36032a187b750901f9c83c9b31bbd
K-jun98 <alap_u@naver.com>
2022-10-26 17:36:59 +0900

    feat - 결과를 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int DRAW = 0;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int answer = getWinner(pobi, crong);
+
+        if (pageValidation(pobi) && pageValidation(crong)) {
+            if (answer == 0) {
+                return DRAW;
+            } else if (answer > 0) {
+                return POBI_WIN;
+            } else if (answer < 0) {
+                return CRONG_WIN;
+            }
+        }
+        return EXCEPTION;
     }
 
     private static int numberAdd(int number) {
