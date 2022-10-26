ad3f575b34ba34a1a9df2b222a7a7c0751cb3079
alstjr706 <alstjr706@naver.com>
2022-10-27 04:14:07 +0900

    feat : (페이지 번호 게임) calculation 구현

--- src/main/java/onboarding/problem1/BookCalculation.java
+++ src/main/java/onboarding/problem1/BookCalculation.java
@@ -1,12 +1,31 @@
 package onboarding.problem1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookCalculation {
 
-    public int calculation(List<Integer> player) {
+    public List<Integer> calculation(List<Integer> player) {
 
-        return 0;
+        int addition = 0;
+        int multiplication = 0;
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int number : player) {
+
+            int first = number / 100;
+            int second = (number / 10) % 10;
+            int third = number % 10;
+
+            addition = first + second + third;
+            multiplication = first * second * third;
+
+            result.add(addition);
+            result.add(multiplication);
+        }
+
+        return result;
     }
 
     public int maxNumber(List<Integer> player) {
