80e3d0d88e8b8b7f9a1f99b0ec85842a8bd872a1
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-10-26 22:43:39 +0900

    problem1: 예외처리 함수(errorHandle)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
     static int getBig(int x) {
@@ -29,6 +30,12 @@ class Problem1 {
         else {
             return 0;
         }
+    }
+    static boolean errorHandle(int left, int right) {
+        if (left < 1 || left > 400 || right < 1 || right > 400 || right - left != 1) {
+            return false;
+        }
+        return true;
     }
 //    public static void main(String[] args){
 //        System.out.println(getWinner(10,1));
