9c4f6a48bccf7f8b9a785c30b799d062dbef9e93
akskflwn <akskflwn@gmail.com>
2022-10-27 01:46:33 +0900

    (feat) problem2 기능 구현 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,42 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer = cryptogram;
+
+        while (true) {
+            String extractedString = extract(answer);
+            if (extractedString.equals(answer)) break;
+            answer = extractedString;
+        }
         return answer;
     }
+
+    private static String extract(String cryptogram) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (isDuplicated(cryptogram,i)) {
+                continue;
+            }
+            if (isDuplicated(cryptogram, i)) {
+                continue;
+            }
+            sb.append(cryptogram.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    private static boolean isDuplicated(String cryptogram, int i) {
+        if (i != 0 && cryptogram.charAt(i - 1) == cryptogram.charAt(i)) {
+            return true;
+        }
+        if (i != cryptogram.length() - 1 && cryptogram.charAt(i + 1) == cryptogram.charAt(i)) {
+            return true;
+        }
+        return false;
+    }
+
 }
