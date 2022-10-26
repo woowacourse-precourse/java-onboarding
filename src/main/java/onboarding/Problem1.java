501840ebda36a9b6e3e43bc84d6024e1e415cafc
CHAYEONIL <jeay123@naver.com>
2022-10-26 22:22:57 +0900

    문제 1 기능 구현내용 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
     }
 }
