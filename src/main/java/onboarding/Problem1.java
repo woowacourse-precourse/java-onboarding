327c8b98622a0bc7794ba32dd5bab3fca2324ce3
jeongjihun <wlgns2234@naver.com>
2022-10-27 03:01:30 +0900

    Feat: 페이지 번호 각 자리 숫자의 합 반환 기능추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int SumPageNum(int pageNumber) {
+        int ret = 0;
+        char c;
+        String pageNumberStr = Integer.toString(pageNumber);
+
+        for (int i = 0; i < pageNumberStr.length(); i++) {
+            c = pageNumberStr.charAt(i);
+            ret += Character.getNumericValue(c);
+        }
+
+        return ret;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
