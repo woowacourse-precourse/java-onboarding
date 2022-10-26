f7793b5f95434df0456b101118cbdfa1a3c1e9d2
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 21:00:07 +0900

    알파벳을 사전에 맞게 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,14 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+    public static void main(String[] args) {
+
+    }
     public static String solution(String word) {
-        String answer;
-
-
-        return word;
+        StringBuilder sb = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            c = convertFrogAlphabet(c);
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
-    public static boolean isAlphabet(String str) {
-        return str != null && str.matches("^[a-zA-Z]*$");
+    public static char convertFrogAlphabet(char c) {
+        if (!Character.isLetter(c)) {
+            return c;
+        }
+        if (Character.isLowerCase(c)) {
+            return (char)('z' - (c - 'a'));
+        }
+        if (Character.isUpperCase(c)) {
+            return (char)('Z' - (c - 'A'));
+        }
+        return c;
     }
 }
