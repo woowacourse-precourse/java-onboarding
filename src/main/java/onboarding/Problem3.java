8f48fd9905d247f5ebb90651ac85e9d2e1798b60
sunwng <sunwgna@gmail.com>
2022-10-26 17:41:58 +0900

    [problem_3] 기능 구현, 테스트 성공

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int clapCheck(int numIn) {
+        int output = 0;
+
+        String numStr = Integer.toString(numIn);
+        int curNum;
+        for (int i = 0; i < numStr.length(); i++) {
+            curNum = numStr.charAt(i) - '0';
+            if (curNum != 0) {
+                if ((curNum % 3) == 0) output++;
+            }
+        }
+        return output;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += clapCheck(i);
+        }
         return answer;
     }
 }
