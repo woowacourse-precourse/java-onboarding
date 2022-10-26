6c7fd01af8cc54d871f8c9ddb1f48587ab9425a2
Amyhds <dasomhan524@gmail.com>
2022-10-26 23:29:05 +0900

    feat: Add check error function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,24 @@
 package onboarding;
 
 import java.util.List;
+import java.lang.Math;
 
 class Problem1 {
+    //check page
+    public static boolean errChk(List<Integer> lst){
+        if (lst.get(1) - lst.get(0) != 1){
+            return true;
+        }
+        else if (lst.contains(1) || lst.contains(2) || lst.contains(399) || lst.contains(400)){
+            return true;
+        }
+        return false;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (errChk(pobi) || errChk(crong)){
+            return -1;
+        }
         return answer;
     }
 }
