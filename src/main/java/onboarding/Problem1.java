458cccb804f53706a9998ae1209a9c1ce5138eab
yg0585 <yg0585@naver.com>
2022-10-26 19:06:37 +0900

    첫 번째 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,7 +2,24 @@ package onboarding;
 
 import java.util.List;
 
+//------기능------
+//1. 곱하기
+//2. 더하기
+//3. max
+
 class Problem1 {
+    private int addNum(int pageNum)    {
+        int num = 0;
+
+        while (pageNum > 0)
+        {
+            num += pageNum % 10;
+            pageNum /= 10;
+        }
+
+        return num;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
