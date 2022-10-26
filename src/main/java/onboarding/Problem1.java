bfa926e8eb3370ef1bf94ea8b3a0e8f405c66605
Park shinyeong <syngh503@gmail.com>
2022-10-26 22:00:54 +0900

    Feat/각 자리수의 합 구하기 각 자리수의 합을 구하는 getTotal 함수를 구현하였습니다. 각 자리수가 담긴 리스트를 입력값으로 받습니다. 각 자리수 합인 정수값을 리턴합니다.
    Relate to: #1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,15 +16,23 @@ class Problem1 {
 
     // 각 자리수를 구한다.
     public static List<Integer> getEachDigit(int page) {
-        List<Integer> pageNum = new ArrayList<>();
+        List<Integer> pageDigitList = new ArrayList<>();
         while(page > 0){
-            pageNum.add(page % 10);
+            pageDigitList.add(page % 10);
             page /= 10;
         }
-        return pageNum;
+        return pageDigitList;
     }
 
     // 각 자리수의 합을 구한다.
+    public static int getTotal(List<Integer> pageDigitList) {
+        int answer = 0;
+        for (int i = 0; i < pageDigitList.size(); i++) {
+            answer += pageDigitList.get(i);
+        }
+        return answer;
+    }
+
 
 
     // 구한 값 중 최댓값을 구한다.
