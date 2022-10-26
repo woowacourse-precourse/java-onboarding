d175b7288a170ca4af3125c992834f03621ffa4f
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-10-26 22:11:06 +0900

    각 페이지의 자리수 합 구하는 함수
    왼쪽, 오른쪽 페이지의 자리수를 더한 후 더 큰 숫자 반환해주는 sum_page 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -22,5 +22,20 @@ class Problem1 {
         }
         return 0;
     }
+
+    public static int sum_page(List<Integer> page) { // 왼쪽 오른쪽 페이지의 각 자리 숫자를 더한 후 더 큰 숫자 반환
+        int sum_left = 0, sum_right = 0;
+        int left_page = page.get(0);
+        int right_page = page.get(1);
+
+        while(left_page > 0 || right_page > 0) {
+            sum_left += left_page % 10;
+            sum_right += right_page % 10;
+            left_page /= 10;
+            right_page /= 10;
+        }
+        if(sum_left > sum_right) return sum_left;
+        else return sum_right;
+    }
 }
 
