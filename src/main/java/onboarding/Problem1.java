6e631ea69797696680b9d7accd3ee1eb7569baa6
ch200203 <ch200205@naver.com>
2022-10-27 00:02:29 +0900

    1.예외 상황처리 1-1 : 시작면이 나오거나 마지막면이 나오는 경우 1-2 : 페이지가 서로 인접해있지 않은 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 예외의 경우 처리
+        if(!validChk(pobi) || !validChk(crong)) return answer = -1;
+
         return answer;
+    }
+    /**
+     * 예외처리 함수
+     * @param list
+     * @return {boolean}
+     */
+    public static boolean validChk(List<Integer> list) {
+        if(list.get(1) - list.get(0) > 1) return false;
+        else if(list.contains(400) || list.contains(1)) return false;
+        return true;
     }
 }
