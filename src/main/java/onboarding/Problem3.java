96efdf9c7060ba7e19ae98733fb3dfdaaacd8e98
JeonghoPark <pjhpjh0805@naver.com>
2022-10-27 06:51:04 +0900

    feat: 3번 박수 치는 횟수 카운트 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+
         int answer = 0;
+        for(int i=1; i<=number; i++) {
+            answer += countClap(i);
+        }
         return answer;
+    }
+
+    private static int countClap(int number) {
+        String s = String.valueOf(number);
+        int total = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '3' || s.charAt(i) == '6' || s.charAt(i) == '9') {
+                total++;
+            }
+        }
+        return total;
     }
 }
