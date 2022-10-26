a3dc6df0136873ec0def96c166e14fd93ff04e92
kimJaeHyeon <kmr2644@gmail.com>
2022-10-26 17:53:24 +0900

    feat: implement method for pagelist validation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,16 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!(isValidPage(pobi) && isValidPage(crong))) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static boolean isValidPage(List<Integer> pageList) {
+        Integer first = pageList.get(0);
+        Integer second = pageList.get(1);
+        return (first > 1 && second < 400) &&
+                (first % 2 == 0 && second - 1 == first);
     }
 }
