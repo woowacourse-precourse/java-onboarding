b81f11ffd88edcd088ba7fe6a716f5a152ccbc1a
JaehongDev <println@kakao.com>
2022-10-27 00:25:28 +0900

    feature (Problem2) : 문제 2번 기능 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,9 @@
 package onboarding;
 
+import onboarding.problem2.PlainText;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return PlainText.decodeCipherText(cryptogram);
     }
 }
