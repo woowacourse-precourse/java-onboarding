02763319715751ce3e9382a3e09b3cced6beeee0
young2866 <62212610+young2866@users.noreply.github.com>
2022-10-27 01:48:03 +0900

    Problem6 기능 2 구현 및 기능 4 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
@@ -31,9 +32,41 @@ public class Problem6 {
      * HashMap을 통해서 중복된 닉네임을 작성한 이메일 목록을 찾는 기능
      */
 
+    public static List<String> findDuplicateNickname(List<List<String>> forms) {
+
+        HashMap<String, Integer> listOfAll = new HashMap<>(); // 두 글자로 분리된 이름과 인덱스를 저장할 해쉬맵
+        List<String> listOfEmail = new ArrayList<>(); // 이메일을 저장할 리스트
+
+        for(int i = 0; i < forms.size(); i++)
+        {
+            List<String> separatedNickname = separateNickname(forms.get(i).get(1)); // 두 글자로 분리된 닉네임 리스트
+
+            for(int j = 0; j < separatedNickname.size(); j++)
+            {
+                String currentNickname = separatedNickname.get(j);
+                if(listOfAll.containsKey(currentNickname))
+                {
+                    listOfEmail.add(forms.get(i).get(0)); //
+                    listOfEmail.add(forms.get(listOfAll.get(currentNickname)).get(0));
+                }else
+                {
+                    listOfAll.put(currentNickname, i);
+                }
+            }
+        }
+
+        return listOfEmail;
+    }
+
+
     /**
      * 기능 3
      * 이메일 목록을 오름차순으로 정렬
+     */
+
+    /**
+     * 기능 4
+     * 이메일 목록의 중복 제거
      */
 
 }
