4bf980b78f20c9cf25e1e0f8bfaa93247358370b
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-27 00:38:56 +0900

    feat : 배달이가 좋아하는 369게임

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            String str = Integer.toString(i);
+            for (int j = 0; j < str.length(); j++) {
+            	char check = str.charAt(j);
+                if (check == '3' || check == '6' || check == '9') {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
