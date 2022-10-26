acd7e9255f6efe3b3b09b0445923175893b325a9
sangho-jeon <jeonsangho1587@gmail.com>
2022-10-26 16:00:25 +0900

    feat      : problem1 score 계산 기능 추가.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,19 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int getScore(int pageNumber) {
+        int score = 0;
+        int pageMult = 1;
+        int pageSum = 0;
+        while (pageNumber > 0) {
+            int digit = pageNumber % 10;
+            pageMult *= digit;
+            pageSum += digit;
+            pageNumber = pageNumber / 10;
+        }
+        score = Math.max(pageMult, pageSum);
+        return score;
+    }
+
 }
