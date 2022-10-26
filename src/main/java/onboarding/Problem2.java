437809242953693ec33eb67743a75fd936928485
JH-TT <leejongho9803@gmail.com>
2022-10-27 02:08:14 +0900

    2번 : Charactor배열에 알파벳 넣기, 빼기 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+
+    // code 배열 상태에 따른 알파벳 넣을지 말지.
+    static List<Character> updateCode(List<Character> code, char alpha) {
+        if(code.size() != 0 && code.get(code.size()-1) == alpha) {
+            code.remove(code.size()-1);
+        } else {
+            code.add(alpha);
+        }
+        return code;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        List<Character> code = new ArrayList<>();
+        for(char alpha : cryptogram.toCharArray()) {
+            code = updateCode(code, alpha);
+        }
+        return code;
     }
 }
