9fe2eb56c87c39dd05c5bb6e370e999f8f5565c4
mikekang47 <gyeong0308@gmail.com>
2022-10-26 22:24:18 +0900

    Docs: describe requirement by comment, Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+// 기능 요구사항
+// 1. 책을 임의로 펼친다.
+// 2, 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+// 3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+// 4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+// 5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+// 6. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
