1113854c2fe4e28d67a9cc1e8910c9615dfd250b
main <jang0929@naver.com>
2022-10-26 20:17:56 +0900

    Problem 1 에러 체크 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,27 @@ import java.util.List;
  * 4. 기능 요구사항의 연산을 실행하는 함수
  */
 class Problem1 {
+
+
+    /**
+     * 1. 에러 체크
+     * @param list : 에러가 있는지 체크할 list
+     * @return error가 있을 경우 true, 없을경우 false
+     */
+
+    static boolean checkError(List<Integer> list) {
+        if (list.get(0) == null || list.get(1) == null)
+            return true;
+        if (!(((list.get(0) & 1) == 1) && ((list.get(1) & 1) == 0))) {
+            return true;
+        }
+        if (!(list.get(0) <= 400 && list.get(1) <= 400 && list.get(0) >= 1 && list.get(1) >= 1)) {
+            return true;
+        }
+        return list.get(1) - list.get(0) != 1;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
