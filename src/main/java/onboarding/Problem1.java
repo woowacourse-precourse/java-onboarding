5e5d38df9f55539ade30682f659cdc812f490656
kimensoo <ekclstkfka5@gmail.com>
2022-10-27 01:15:39 +0900

    feature : 구현 기능 목록 명세 추가 구현 할 메서드들의 명세 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,31 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private final int POBI_WIN = 1;
+    private final int CRONG_WIN = 2;
+    private final int DRAW = 0;
+    private final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public int sumPageNum(int pageNum) {
+        int sum = 0;
+        return sum;
+    }
+
+    public int multiplyPageNum(int pageNum) {
+        int sum = 0;
+        return sum;
+    }
+
+    public int getWhoWin(int pobiNum, int croingNum) {
+        return -1;
+    }
+
+    public boolean isValid(List<Integer> pobi, List<Integer> crong) {
+        return true;
     }
 }
