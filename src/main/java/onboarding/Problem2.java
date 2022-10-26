0cf70d9135a9a9833f1ebb409b78aae84d964b78
Miseong Kim <miseongk@naver.com>
2022-10-27 04:45:59 +0900

    feat: add remove duplicated characters function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,43 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        int previousLength = 0;
+
+        // 연속된 중복 문자들이 없을때 까지 반복
+        while (answer.length() > 0) {
+            previousLength = answer.length();
+            answer = removeDuplication(answer);
+            if (answer.length() == previousLength) {
+                break;
+            }
+        }
+
         return answer;
+    }
+
+    // 연속하는 중복 문자 삭제하는 함수
+    static String removeDuplication(String cryptogram) {
+        String result = "";
+        int current = 0;
+        char currentChar;
+
+        while (current < cryptogram.length()) {
+            if (current == cryptogram.length() - 1 ||
+                    cryptogram.charAt(current) != cryptogram.charAt(current + 1)) {
+                result += cryptogram.charAt(current);
+                current++;
+            } else {
+                currentChar = cryptogram.charAt(current);
+                while (currentChar == cryptogram.charAt(current)) {
+                    current++;
+                    if (current == cryptogram.length()) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
