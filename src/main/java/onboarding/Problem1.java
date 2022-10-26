4d880af8f93c8f2e24a06d99ec19af4d1585f8dd
BaeDongHyeon <sasd730@gmail.com>
2022-10-26 21:25:00 +0900

    feat: 더하거나 곱한 값 중 더 큰 값을 구함

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,27 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        int pobiLeftMax = getMaxValue(pobi.get(0));
+        int pobiRightMax = getMaxValue(pobi.get(1));
+        int crongLeftMax = getMaxValue(crong.get(0));
+        int crongRightMax = getMaxValue(crong.get(1));
+
+        return -1;
+    }
+
+    private static int getMaxValue(int page) {
+        int sum = 0;
+        int mul = 1;
+        int seatNum;
+
+        while (page > 0) {
+            seatNum = page % 10;
+            page /= 10;
+            sum += seatNum;
+            mul *= seatNum;
+        }
+
+        return Math.max(sum, mul);
     }
 }
