d8fc612d9aae55bda6a3dfac7775f1b495956767
SeokyungKim <kskyung0624@gmail.com>
2022-10-26 17:28:12 +0900

    문제 2 - 문제 해결 흐름 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/**
+ * 1. redo를 false 초기화하고 한글자씩 돌며 이전 글자를 캐시에 저장한다.
+ * 2. 캐시와 이번 글자가 같으면 플래그를 truee 두고 현재 인덱스를 저장한다.
+ * 3. 캐시와 현재 글자가 다르다면 인덱스에 표기된 글자들을 제거하고 인덱스를 초기화한다.
+ * 4. 마저 돈다.
+ * 5-1. 문자열이 끝까지 돌았을 때 redo가 false라면 종료하고 현재 문자열을 반환한다.
+ * 5-2. redo가 true라면 함수를 재실행한다.
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
