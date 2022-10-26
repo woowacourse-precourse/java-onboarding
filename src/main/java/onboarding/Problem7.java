7bca5b4d16542d88a53735ec2e137ff88b8bb58f
young2866 <62212610+young2866@users.noreply.github.com>
2022-10-27 02:58:40 +0900

    Problem7 기능2 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -17,8 +15,7 @@ public class Problem7 {
     public static List<String> makeFriendList(String user, List<List<String>> friends) {
         List<String> friendsOfUser = new ArrayList<>();
 
-        for(int i = 0; i < friends.size(); i++)
-        {
+        for (int i = 0; i < friends.size(); i++) {
             if (friends.get(i).get(0).equals(user)) {
                 friendsOfUser.add(friends.get(i).get(1));
             }
@@ -32,15 +29,44 @@ public class Problem7 {
 
     /**
      * 기능 2
-     * 사용자와 함께 아는 친구를 가진 사람과 점수 리스트 생성
+     * 사용자와 함께 아는 친구를 가진 사람과 점수 Map 생성
      */
+    public static HashMap<String, Integer> makeRecommendList(String user, List<String> friendsOfUser,
+                                                             List<List<String>> friends) {
+
+        HashMap<String, Integer> recommendNameWithScoreMap = new HashMap<>();
+
+        for (int i = 0; i < friendsOfUser.size(); i++) {
+            for (int j = 0; j < friends.size(); j++) {
+                if (friends.get(j).get(0).equals(friendsOfUser.get(i)) && !friends.get(j).get(1).equals(user)) {
+                    if (recommendNameWithScoreMap.containsKey(friends.get(j).get(1))) {
+                        recommendNameWithScoreMap.put(friends.get(j).get(1), recommendNameWithScoreMap.get(friends.get(j).get(1)) + 10);
+                    } else {
+                        recommendNameWithScoreMap.put(friends.get(j).get(1), 10);
+                    }
+                }
+                if (friends.get(j).get(1).equals(friendsOfUser.get(i)) && !friends.get(j).get(0).equals(user)) {
+                    if (recommendNameWithScoreMap.containsKey(friends.get(j).get(0))) {
+                        recommendNameWithScoreMap.put(friends.get(j).get(0), recommendNameWithScoreMap.get(friends.get(j).get(1)) + 10);
+                    } else {
+                        recommendNameWithScoreMap.put(friends.get(j).get(0), 10);
+                    }
+                }
+            }
+        }
+
+        return recommendNameWithScoreMap;
+    }
 
     /**
      * 기능 2에 만든 리스트에 방문자 리스트 및 점수 추가
      */
 
+
+
     /**
      * 기능 4
      * 점수가 높은 순서대로 정렬
      */
+
 }
