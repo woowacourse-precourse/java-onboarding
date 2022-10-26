3ba37476198109482a95d6db2796e7a603a25281
JongDo737 <jongmin373@gmail.com>
2022-10-26 16:55:34 +0900

    problem3 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=0; i<=number; i++) {
+            int temp = i;
+            int length = (temp+"").length();
+            for(int j=0; j< length; j++){
+                int tempNum = temp%10 ;
+                if(tempNum ==3 || tempNum == 6 || tempNum ==9 ) {
+                    answer ++;
+                }
+                temp /= 10;
+            }
+        }
         return answer;
     }
 }
