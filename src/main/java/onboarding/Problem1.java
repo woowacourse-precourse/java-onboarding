f0cc0ecc28e474446cdf60faee75a24ed52079a7
lecocococo <chemwang13@naver.com>
2022-10-27 00:40:38 +0900

    Feat: Problem1의 각 페이지 최대값 구하기(각자리 더하기, 각 자리 곱하기, 최대값 반환)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,46 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+	public static final int FIRST_PAGE = 1;
+	public static final int LAST_PAGE = 400;
+    static class PageCalculator {
+		int leftPage;
+		int rightPage;
+
+		public PageCalculator(int leftPage, int rightPage) {
+			this.leftPage = leftPage;
+			this.rightPage = rightPage;
+		}
+
+		public int calculatePlus(int pageNumber) {
+			int sumResult = 0;
+			while (pageNumber != 0) {
+				sumResult += pageNumber % 10;
+				pageNumber /= 10;
+			}
+			return sumResult;
+		}
+
+		public int calculateMultiply(int pageNumber) {
+			int multipleResult = 1;
+			while (pageNumber != 0) {
+				multipleResult *= pageNumber % 10;
+				pageNumber /= 10;
+			}
+			return multipleResult;
+		}
+
+		public int calculateMax(int pageNumber) {
+
+			return Math.max(calculatePlus(pageNumber), calculateMultiply(pageNumber));
+		}
+
+		public int returnResult() {
+
+			return Math.max(calculateMax(leftPage), calculateMax(rightPage));
+		}
+	}
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
