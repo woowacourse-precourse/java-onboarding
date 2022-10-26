5f9a6171a0ed7ececbdde7819fa0f6399a83d859
lecocococo <chemwang13@naver.com>
2022-10-27 04:21:36 +0900

    Feat: Problem2의 정규식 패턴 생성기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,16 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem2 {
+	static Pattern pattern;
+
+	public static void createRegexPattern() {
+		String regex = "([a-z])\\1+";
+		pattern = Pattern.compile(regex);
+	}
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
