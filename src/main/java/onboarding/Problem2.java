4f5bf251dace77e98000f848f95698d926beacd2
jini_11 <jieun21124@naver.com>
2022-10-26 23:39:53 +0900

    feat: 문제2 중복 문자 찾는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,13 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    public static int findSame(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
