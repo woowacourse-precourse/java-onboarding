802a849e6bfdc63be1cd5df555e92e1639164dea
K-jun98 <alap_u@naver.com>
2022-10-26 21:30:52 +0900

    feat - 문자열의 중복되는 문자의 인덱스를 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return "";
     }
+
+
+    private static int isOverlap(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
