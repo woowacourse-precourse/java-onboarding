8e4192d3b00789baa3979dc96673b7c9676f0427
hyun6ik <fkanwm@naver.com>
2022-10-26 20:41:16 +0900

    feat(19) : 무한 루프안에서 중복된 문자를 찾는 로직 개발

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,20 +3,53 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
 
-        final char[] chars = cryptogram.toCharArray();
-        final StringBuilder newCryptogram = new StringBuilder();
-
-        char duplicateChar = ' ';
+        char[] chars = cryptogram.toCharArray();
         boolean hasNotDuplicate = false;
+        String answer = "";
 
+        while (!hasNotDuplicate) {
+            final StringBuilder newCryptogram = new StringBuilder();
+            char duplicateChar = ' ';
+            hasNotDuplicate = true;
+            for (int i = 0; i < chars.length; i++) {
+                final char character = chars[i];
+                if (i == 0) {
+                    if (chars[i] != chars[i + 1]) {
+                        newCryptogram.append(character);
+                    }
+                    continue;
+                }
 
+                if (i == chars.length - 1) {
+                    if (character != chars[i - 1] && character != duplicateChar) {
+                        newCryptogram.append(character);
+                    }
+                    continue;
+                }
 
+                if (character == chars[i + 1]) {
+                    duplicateChar = character;
+                    hasNotDuplicate = false;
+                    continue;
+                }
+                if (character != chars[i + 1]) {
+                    if (chars[i] != duplicateChar) {
+                        newCryptogram.append(character);
+                        duplicateChar = ' ';
+                    }
+                }
+            }
+            chars = newCryptogram.toString().toCharArray();
+            if (hasNotDuplicate) {
+                answer = newCryptogram.toString();
+            }
+        }
 
-        System.out.println(newCryptogram);
-        return newCryptogram.toString();
+        System.out.println(answer);
+        return answer;
     }
 
     public static void main(String[] args) {
-        Problem2.solution("browoanoommnaon");
+        Problem2.solution("zyelleyz");
     }
 }
