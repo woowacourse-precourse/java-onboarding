ea436115c51070dda744a391614aa46d8b260d15
kang jihyeong <her4386@gmail.com>
2022-10-26 21:17:43 +0900

    Add / Problem2 요구사항 개발 / 테스트 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        StringBuilder cryptoBuilder = new StringBuilder(cryptogram);
+        boolean loop = true;
+
+        while(loop){
+            loop = false;
+            for(int i=0; i<cryptoBuilder.length()-1; i++){
+                if(cryptoBuilder.charAt(i) == cryptoBuilder.charAt(i+1)){
+                    cryptoBuilder.delete(i, i+2);
+                    loop = true;
+                    break;
+                }
+            }
+        }
+
+        answer = cryptoBuilder.toString();
+
         return answer;
     }
 }
