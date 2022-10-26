c4a09a4eb1e37a3cc4569b2063e96f3b50ac17bd
elizabethgim <yujingim@kookmin.ac.kr>
2022-10-26 20:32:39 +0900

    Add: Problem1 main testcase 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,12 +3,28 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static boolean check(List<Integer> list){
+        int first = list.get(0);
+        int second = list.get(1);
+        if(second != first+1){
+            return true;
+        }
+        return false;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(check(pobi) || check(crong)){
+            return -1;
+        }
+
         return answer;
     }
 
     public static void main(String args[]){
         System.out.println("안녕하세요");
+        List<Integer> pobi = List.of(99, 102);
+        List<Integer> crong = List.of(211, 212);
+        System.out.println(solution(pobi, crong));
     }
 }
