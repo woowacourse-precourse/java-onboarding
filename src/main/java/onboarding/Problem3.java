82e25f5022a961393584be638114f279013490bb
eunbii0213 <eunbii0213@naver.com>
2022-10-26 23:54:53 +0900

    feat: (Problem3) memoization 초기값 설정한다(index 0,1,2)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+    public static int[] memoization;
+
     public static int solution(int number) {
         int answer = 0;
+
+        //index 1부터 시작
+        memoization = new int[10001];
+        memoization[0] = 0;
+        memoization[1] = 0;
+        memoization[2] = 0;
+
         return answer;
     }
 }
