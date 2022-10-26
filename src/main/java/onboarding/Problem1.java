9b36e5e58c170394c9512b30c7ce0ba9454cfe2a
qkrdmswl <eunji303@naver.com>
2022-10-27 01:34:24 +0900

    [feat] problem1: 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,24 +6,30 @@ import java.util.stream.Stream;
 
 class Problem1 {
 
-    // 어떤 값을 낼지 결정하는 함수
-    public static int maxComparator(List<Integer> list) {
-        int sum = 0;
-        int mul = 0;
+    /** 기능 명세
+     * 1. 승부에 낼 값 결정하기
+     *     - 왼쪽과 오른쪽의 페이지 번호로 만들 수 있는 가장 큰 값을 찾아 선택
+     * 2. 승부 결과 출력
+     * 3. 예외 처리
+     *     - 왼쪽, 오른쪽 페이지가 서로 연속된 숫자가 아닌 경우
+     *     - 시작면이나 마지막 면이 나온 경우
+     */
+
+    // 기능 명세 1번에 해당하는 함수
+    public static int findMax(List<Integer> pageList) {
         int max = 0;
-        int previous = list.get(0)-1;
-        for (int var : list) {
-            if (previous+1 == var) {
-                int[] arrNum = Stream.of(String.valueOf(var).split("")).mapToInt(Integer::parseInt).toArray();
-                sum = Arrays.stream(arrNum).sum();
-                mul = multipleCalculator(arrNum);
+        int leftPage = pageList.get(0)-1;
+        for (int currentPage : pageList) {
+            if (leftPage+1 == currentPage) {
+                int[] pageNumToArr = Stream.of(String.valueOf(currentPage).split("")).mapToInt(Integer::parseInt).toArray();
+                int sum = Arrays.stream(pageNumToArr).sum();
+                int mul = multipleCalculator(pageNumToArr);  // 각 자리 곱셈은 별도의 함수로 분리하여 계산
 
                 if (sum > mul) { max = sum; }
-                else { max = mul; }
+                else           { max = mul; }
 
-                previous = var;
-            }
-            else { max = -1; break; }
+                leftPage = currentPage;
+            } else { max = -1; break; }
         }
         return max;
     }
@@ -32,17 +38,15 @@ class Problem1 {
     // 각 자리 곱셈은 따로 계산하기 위해 별도의 함수 생성
     public static int multipleCalculator(int[] arr) {
         int mul = 1;
-        for (int var : arr) {
-            mul *= var;
-        }
+        for (int var : arr) { mul *= var; }
         return mul;
     }
 
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = -1;
-        int pobiAnswer = maxComparator(pobi);
-        int crongAnswer = maxComparator(crong);
+        int pobiAnswer = findMax(pobi);
+        int crongAnswer = findMax(crong);
 
         if (pobiAnswer == -1 || crongAnswer == -1 ) { answer = -1; }
         else {
@@ -61,7 +65,6 @@ class Problem1 {
                     break;
             }
         }
-
         return answer;
     }
 }
