61b6fc730741136f780944ea7366c1a6407471f9
hsj4436 <hsj4436@naver.com>
2022-10-26 19:17:05 +0900

    feature/Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i < number + 1; i++){
+            int target = i;
+            while(target != 0){
+                if(target % 10 == 3 || target % 10 == 6 || target % 10 == 9){
+                    answer++;
+                }
+                target /= 10;
+            }
+        }
         return answer;
     }
 }
