995c42e5e32863df79612323fcee3b01b6a77219
Damgom <wisewish1@gmail.com>
2022-10-26 19:04:52 +0900

    FEAT: select higher value

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static int higherValue(int num) {
+        String number = String.valueOf(num);
+        int sum = 0;
+        int multiple = 1;
+        for(int i = 0; i < number.length(); i++) {
+            sum += number.charAt(i)-'0';
+            multiple *= number.charAt(i)-'0';
+        }
+        return Math.max(sum, multiple);
+    }
 }
