d466e36e72228137899feeea21680df8fc20722b
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-26 17:32:35 +0900

    docs(Problem1): 구현할 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+// <기능 목록>
+// 1. 예외상황인지 각각 판별 (해당되면 return -1)
+// 결과 예외상황: 왼쪽페이지!=홀수, 오른쪽페이지!=짝수, 오른쪽페이지-왼쪽페이지!=1
+// 2. 각각의 max num을 구함
+// 3. 점수 비교
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
