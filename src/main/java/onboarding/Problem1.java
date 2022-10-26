ec7ddd5c22b82bd4c9ee9e0748c9e801bdd6b767
jini_11 <jieun21124@naver.com>
2022-10-26 21:27:26 +0900

    feat: 문제1 입력 예외처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,26 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (checkError(pobi) || checkError(crong)) {
+            return -1;
+        }
         return answer;
+    }
+
+    public static boolean checkError(List<Integer> inputList) {
+        if (inputList.get(0) == null || inputList.get(1) == null || inputList.size() != 2) {
+            return true;
+        }
+        if (inputList.get(0) < 1 || inputList.get(0) > 400 || inputList.get(1) < 1 || inputList.get(1) > 400) {
+            return true;
+        }
+        if (inputList.get(0) % 2 == 0 || inputList.get(1) % 2 == 1) {
+            return true;
+        }
+        if (inputList.get(0) + 1 != inputList.get(1)) {
+            return true;
+        }
+        return false;
     }
 }
