a29cc1b044b68a44d17a717f865b3a8534f4dd91
KimJaeSung <behappy0916@naver.com>
2022-10-26 22:48:39 +0900

    java-onboarding-1-큰 수와 작은수를  비교하는 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,13 +17,28 @@ class Problem1 {
         }
         return n;
     }
+    static int compareNumber(int n1, int n2){
+        return (n1 > n2) ? n1 : n2;
+    }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         if(pobi.get(0) != pobi.get(1) -1 || crong.get(0) != crong.get(1)-1){
             return -1;
         }
+        String pobiNum1 = String.valueOf(pobi.get(0));
+        String pobiNum2 = String.valueOf(pobi.get(1));
 
+        String crongNum1 = String.valueOf(crong.get(0));
+        String crongNum2 = String.valueOf(crong.get(1));
+
+        int BigSumNumPobi = compareNumber(sumStringToInteger(pobiNum1),sumStringToInteger(pobiNum2));
+        int BigMulNumPobi = compareNumber(mulStringToInteger(pobiNum1),mulStringToInteger(pobiNum2));
+        int BigNumPobi = compareNumber(BigMulNumPobi,BigSumNumPobi);
+
+        int BigSumNumCrong = compareNumber(sumStringToInteger(crongNum1),sumStringToInteger(crongNum2));
+        int BigMulNumCrong = compareNumber(mulStringToInteger(crongNum1),mulStringToInteger(crongNum2));
+        int BigNumCrong = compareNumber(BigMulNumCrong,BigSumNumCrong);
 
         return answer;
     }
