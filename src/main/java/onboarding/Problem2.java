9f8eb0e6942e9194cbffa87b97ec2d2cf30cc4b5
aossuper7 <aossuper7@naver.com>
2022-10-26 21:18:37 +0900

    feat: 문제2 스택 중복검사 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        char[] answer = cryptogram.toCharArray();
+
+    }
+
+    public static boolean isNotEmptyAndEquals(Stack<Character> stack, char answer) {
+        return !stack.empty() && stack.peek().equals(answer);
     }
 }
