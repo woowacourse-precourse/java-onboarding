e0ac16b548df8333514be433b3985d2747239e04
Namyunsuk <yunsuk0328@naver.com>
2022-10-26 22:43:27 +0900

    feat(validationSamethings): 문자열에 연속한 문자가 있는지 판별하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+
+class validationSamethings{
+    public static boolean validation(ArrayList<String> list){
+        String pivot;
+        for(int i=0;i<list.size()-1;i++){
+            pivot=list.get(i);
+            if(pivot.equals(list.get(i+1))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
