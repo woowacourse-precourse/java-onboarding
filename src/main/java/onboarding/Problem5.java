db6d3a20db3d4ba908fc55b3aa4e39343e1e72f3
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-26 18:16:02 +0900

    feat: 화폐의 단위 갯수 구하는 기능 구현, 문제5 해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = CashCount(money);
+        return answer;
+    }
+    public static List<Integer> CashCount(Integer money){
+        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < moneyList.length; i++) {
+            Integer temp = money / moneyList[i];
+            answer.add(temp);
+            money = money % moneyList[i];
+        }
         return answer;
     }
 }
