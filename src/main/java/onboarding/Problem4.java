c360bdf76cbf80e2424d0992f795a0c05cbdfa6f
jaehyeon <jhjh0022@naver.com>
2022-10-27 02:08:37 +0900

    ã다시 잘 생각해보자

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
+    }
+
+    public static String change(char[] j){
+        char[] a = {'A','B','C'};
+        for(int i= 25; i>=1; i-=2){
+
+        }
     }
 }
