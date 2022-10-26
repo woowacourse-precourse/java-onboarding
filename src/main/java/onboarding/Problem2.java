c835684d9d62677ff0665e5926c7a06dfc96a70e
jkde7721 <jkde7721@naver.com>
2022-10-26 19:54:28 +0900

    [문제 2] 기능 요구 사항 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,25 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return getDecryptedResult(cryptogram);
+    }
+
+    private static String getDecryptedResult(String cryptogram) {
+        StringBuilder decryptedResultBuilder = new StringBuilder();
+        for (int index = 0; index < cryptogram.length();) {
+            if (index == cryptogram.length() - 1 || isDifferentFromNext(cryptogram, index)) {
+                decryptedResultBuilder.append(cryptogram.charAt(index));
+                index += 1;
+            }
+            else index += 2;
+        }
+
+        String decryptedResult = decryptedResultBuilder.toString();
+        if (decryptedResult.equals(cryptogram)) return decryptedResult;
+        return getDecryptedResult(decryptedResult);
+    }
+
+    private static boolean isDifferentFromNext(String cryptogram, int index) {
+        return cryptogram.charAt(index) != cryptogram.charAt(index + 1);
     }
 }
