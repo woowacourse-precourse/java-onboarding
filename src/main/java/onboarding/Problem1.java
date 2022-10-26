a38c1e71d39bb650ddf147d84b9c1ba7e7aa7373
drunkenhw <drunkenhw@gmail.com>
2022-10-27 00:38:01 +0900

    refactor(Problem1) : 객체의 책임 분리를 위한 내부 클래스 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,16 +8,15 @@ class Problem1 {
     private static final int CRONG_WIN = 2;
     private static final int DRAW = 0;
     private static final int EXCEPTION = -1;
-    private static final int LEFT_PAGE = 0;
-    private static final int RIGHT_PAGE = 1;
+
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (isError(pobi) || isError(crong)) {
-            return EXCEPTION;
-        }
 
-        int pobiMaxResult = getMaxLeftAndRight(pobi);
-        int crongMaxResult = getMaxLeftAndRight(crong);
+        Pages pobiPages = new Pages(pobi);
+        Pages crongPages = new Pages(crong);
+
+        int pobiMaxResult = pobiPages.getMaxLeftAndRight();
+        int crongMaxResult = crongPages.getMaxLeftAndRight();
 
         if (pobiMaxResult > crongMaxResult) {
             return POBI_WIN;
@@ -29,61 +28,78 @@ class Problem1 {
         return DRAW;
     }
 
-    private static boolean isError(List<Integer> pages) {
-        if (pages.get(LEFT_PAGE) == null || pages.get(RIGHT_PAGE) == null) {
-            return true;
-        }
-        return getRightPageNum(pages) - getLeftPageNum(pages) != 1;
-    }
+    static class Pages {
+        private static final int LEFT_PAGE = 0;
+        private static final int RIGHT_PAGE = 1;
+        private final List<Integer> pages;
 
-    private static int getMaxLeftAndRight(List<Integer> pages) {
-        int leftPageNum = getLeftPageNum(pages);
-        int rightPageNum = getRightPageNum(pages);
-
-        List<Integer> leftPageDigit = splitToDigitList(leftPageNum);
-        List<Integer> rightPageDigit = splitToDigitList(rightPageNum);
-
-        int leftPageMax = getMaxSumAndMultiply(leftPageDigit);
-        int rightPageMax = getMaxSumAndMultiply(rightPageDigit);
-
-        return Math.max(leftPageMax, rightPageMax);
-
-    }
-
-
-    private static int getLeftPageNum(List<Integer> pages) {
-        return pages.get(LEFT_PAGE);
-    }
-
-    private static int getRightPageNum(List<Integer> pages) {
-        return pages.get(RIGHT_PAGE);
-    }
-
-    private static int getMaxSumAndMultiply(List<Integer> digitList) {
-
-        return Integer.max(getDigitSum(digitList), getDigitMultiply(digitList));
-    }
-
-    private static List<Integer> splitToDigitList(int number) {
-
-        List<Integer> digitList = new ArrayList<>();
-        while (number != 0) {
-            digitList.add(number % 10);
-            number /= 10;
+        public Pages(List<Integer> pages) {
+            validateIsNotNull(pages);
+            validateIsContinuous(pages);
+            this.pages = pages;
         }
 
-        return digitList;
-    }
+        private void validateIsNotNull(List<Integer> pages) {
+            if (pages.get(LEFT_PAGE) == null || pages.get(RIGHT_PAGE) == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+        private void validateIsContinuous(List<Integer> pages) {
+            if (pages.get(RIGHT_PAGE) - pages.get(LEFT_PAGE) != 1) {
+                throw new IllegalArgumentException();
+            }
+        }
 
-    private static int getDigitSum(List<Integer> digitList) {
+        private int getMaxLeftAndRight() {
+            int leftPageNum = getLeftPageNum(this.pages);
+            int rightPageNum = getRightPageNum(this.pages);
 
-        return digitList.stream().reduce(0, Integer::sum);
+            List<Integer> leftPageDigit = splitToDigitList(leftPageNum);
+            List<Integer> rightPageDigit = splitToDigitList(rightPageNum);
 
-    }
+            int leftPageMax = getMaxSumAndMultiply(leftPageDigit);
+            int rightPageMax = getMaxSumAndMultiply(rightPageDigit);
 
-    private static int getDigitMultiply(List<Integer> digitList) {
+            return Math.max(leftPageMax, rightPageMax);
 
-        return digitList.stream().reduce(1, (x, y) -> x * y);
+        }
+
+
+        private int getLeftPageNum(List<Integer> pages) {
+            return pages.get(LEFT_PAGE);
+        }
+
+        private int getRightPageNum(List<Integer> pages) {
+            return pages.get(RIGHT_PAGE);
+        }
+
+        private int getMaxSumAndMultiply(List<Integer> digitList) {
+
+            return Integer.max(getDigitSum(digitList), getDigitMultiply(digitList));
+        }
+
+        private List<Integer> splitToDigitList(int number) {
+
+            List<Integer> digitList = new ArrayList<>();
+            while (number != 0) {
+                digitList.add(number % 10);
+                number /= 10;
+            }
+
+            return digitList;
+        }
+
+        private int getDigitSum(List<Integer> digitList) {
+
+            return digitList.stream().reduce(0, Integer::sum);
+
+        }
+
+        private int getDigitMultiply(List<Integer> digitList) {
+
+            return digitList.stream().reduce(1, (x, y) -> x * y);
+
+        }
 
     }
 
