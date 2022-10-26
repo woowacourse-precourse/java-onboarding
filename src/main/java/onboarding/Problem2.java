d6ef52daa741da97820cf8ff9f81ae0a634a22e3
WonjunS <seowj0710@gmail.com>
2022-10-27 07:07:46 +1100

    Problem 2 Solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = pro(cryptogram);
+
         return answer;
+    }
+
+    static String pro(String cryptogram) {
+        boolean[] isDuplicated = new boolean[cryptogram.length()];  // 연속으로 중복되는 문자의 인덱스를 기록
+        String s = "";
+
+        for(int i = 1; i < cryptogram.length(); i++) {
+            char curr = cryptogram.charAt(i);       // 이전 문자열
+            char prev = cryptogram.charAt(i - 1);   // 현재 문자열
+            if(curr != prev) continue;              // 서로 다른 경우
+            else isDuplicated[i] = true;    isDuplicated[i - 1] = true;
+        }
+
+        for(int i = 0; i < cryptogram.length(); i++) {
+            if(isDuplicated[i]) continue;       // 중복 문자일 경우
+            else {
+                char c = cryptogram.charAt(i);
+                s += String.valueOf(c);         // 중복되지 않은 문자일 경우
+            }
+        }
+
+        if(s.length() == cryptogram.length()) return s; // input 의 길이와 output 의 길이가 같으면 더 이상 해독팔 필요가 없다
+        return pro(s);  // 재귀 호출
     }
 }
