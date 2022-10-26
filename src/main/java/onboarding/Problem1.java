88b68f00752848af31da99b55fe009463b30e97a
elizabethgim <yujingim@kookmin.ac.kr>
2022-10-26 21:01:36 +0900

    Add: 포비와 크롱 점수 만들기 및 비교

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -33,6 +33,31 @@ class Problem1 {
             return -1;
         }
 
+        int pobiMax = 0, psum, pmul;
+        for(int p:pobi){
+            psum = getSumOfEachNumber(p);
+            pobiMax = Math.max(pobiMax, psum);
+            pmul = getMultipleOfEachNumber(p);
+            pobiMax = Math.max(pobiMax, pmul);
+        }
+
+        int crongMax = 0, csum, cmul;
+        for(int c:crong){
+            csum = getSumOfEachNumber(c);
+            crongMax = Math.max(crongMax, csum);
+            cmul = getMultipleOfEachNumber(c);
+            crongMax = Math.max(crongMax, cmul);
+        }
+
+        System.out.println("포비: "+pobiMax+" 크롱: "+crongMax);
+
+        if(pobiMax == crongMax){
+            answer = 0;
+        } else if(pobiMax > crongMax){
+            answer = 1;
+        } else{
+            answer = 2;
+        }
         return answer;
     }
 
@@ -40,6 +65,19 @@ class Problem1 {
         System.out.println("안녕하세요");
         List<Integer> pobi = List.of(99, 102);
         List<Integer> crong = List.of(211, 212);
-        System.out.println(solution(pobi, crong));
+        System.out.println(solution(pobi, crong)); // -1
+
+        pobi = List.of(131, 132);
+        crong = List.of(211, 212);
+        System.out.println(solution(pobi, crong)); // 1
+
+        pobi = List.of(97, 98);
+        crong = List.of(197, 198);
+        System.out.println(solution(pobi, crong)); // 0
+
+        pobi = List.of(11, 12);
+        crong = List.of(9, 10);
+        System.out.println(solution(pobi, crong)); // 2
+
     }
 }
