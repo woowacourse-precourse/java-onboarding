15b04fc44cbaa23ceac967409d38ed86e48f4aaf
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-26 17:17:08 +0900

    Feat: 구현 Problem1 함수 maxValue - 두 수가 주어질 때 비교하여 큰 수 반환하는 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	private static int maxValue(int value1, int value2) {
+		if (value1 >= value2) {
+			return value1;
+		}
+		return value2;
+	}
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+
+		return answer;
+	}
 }
