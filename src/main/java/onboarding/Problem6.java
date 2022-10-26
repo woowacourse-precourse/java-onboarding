6e313124e96ba6d05d7933330cfd94eef7945c49
JunH <tjdtls690@gmail.com>
2022-10-26 22:55:24 +0900

    :sparkles: feat : 차례대로 연속된 두 글자를 저장하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String, String> map = new HashMap<>();
+        
+        for (List<String> form : forms) {
+            Map<String, String> tmp = new HashMap<>();
+            final String crewName = form.get(1);
+            final String email = form.get(0);
+            
+            for (int index = 0; index < crewName.length() - 1; index++) {
+                tmp.put(crewName.substring(index, index + 2), email);
+            }
+            
+            map.putAll(tmp);
+        }
+    
+        System.out.println(map.keySet());
+        
         return answer;
     }
     
