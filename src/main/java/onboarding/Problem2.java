9a643c075f182ed33bf67e1a48dfe0625033f504
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-26 23:17:12 +0900

    feat : 연속하는 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (stack.isEmpty()) {
+            	stack.push(cryptogram.charAt(i));
+            } else {
+                if (stack.peek() == cryptogram.charAt(i)) {
+                	stack.pop();
+                } else {
+                	stack.push(cryptogram.charAt(i));
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            answer = stack.pop() + answer;
+        }
         return answer;
     }
 }
