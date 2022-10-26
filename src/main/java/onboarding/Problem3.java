8d6762c74fcad90b75d24100d8ea12b7b2ebdf92
dev_kong <ahdjd5@gmail.com>
2022-10-26 18:36:07 +0900

    feat(Problem3): solve problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,31 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        if (number > 3) {
+            answer = getHandClap(number);
+        }
+        System.out.println(answer);
         return answer;
+    }
+
+    public static int getHandClap(int number) {
+        int totalCount = 0;
+        for (int i = 3; i < number + 1; i++) {
+            totalCount += getCount(i);
+        }
+        return totalCount;
+    }
+
+    public static int getCount(int number) {
+        int count = 0;
+        while (number > 0) {
+            int target = number % 10;
+            if (target != 0 && target % 3 == 0) {
+                count += 1;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
