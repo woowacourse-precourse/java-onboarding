dc9529aa10655d58e463a8e5335f0b62ff953cf0
jinny-l <jinny.l.dev@gmail.com>
2022-10-27 01:21:40 +0900

    [refactor] notContain_3_6_9() 메소드의 반환 타입을 String에서 char로 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,18 +7,12 @@ public class Problem3 {
     }
 
 
+
+
     // 입력받은 문자열에 "3" "6" '9"가 포함되어 있는지 확인하는 메소드
-    private static boolean notContain_3_6_9(String num) {
+    private static boolean notContain_3_6_9(char c) {
 
-        for (int i = 0; i < num.length(); i++) {
-            char c = num.charAt(i);
-            String str = Character.toString(c);
-
-            if (str.equals("3") || str.equals("6") || str.equals("9")) {
-                return true;
-            }
-        }
-        return false;
+        String num = Character.toString(c);
+        return num.equals("3") || num.equals("6") || num.equals("9");
     }
-
 }
