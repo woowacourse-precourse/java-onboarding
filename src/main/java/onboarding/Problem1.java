684ad72f4571aa14cbbd7d97a6345b5e23fb16f1
bjk1649 <qhwns1649@naver.com>
2022-10-26 23:36:12 +0900

    feat: 입력을 검증하는 클래스, 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,27 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+    public static int solution(List<Integer> pobi, List<Integer> crong){
+    int answer = Integer.MAX_VALUE;
+
+    if (!Validation.isValidateInput(pobi) || !Validation.isValidateInput(crong)) {
+        return -1;
+    }
+
+    return answer;
+}
+
+}
+class Validation{
+    static final int LIST_LENGTH = 2;
+    static final int MAX_PAGE = 400;
+    static final int MIN_PAGE = 1;
+
+    static boolean isValidateInput(List<Integer> input){
+
+        return !(input.size() != LIST_LENGTH
+                || input.get(1) - input.get(0) != 1
+                || input.get(1) >= MAX_PAGE
+                || input.get(0) <= MIN_PAGE);
     }
 }
