eda34cd3a32f1cf3ae376125a559fdba8e416e48
eunbii0213 <eunbii0213@naver.com>
2022-10-26 19:19:46 +0900

    feat: (Problem1) 왼쪽 page와 오른쪽 page중에 더 큰 수를 반환한다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,9 +7,15 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiMax = 0;
+        int crongMax = 0;
+
 
         if (isException(pobi) || isException(crong)) {
             answer = EXCEPTION;
+        } else {
+            pobiMax = calculatePageMax(pobi);
+            crongMax = calculatePageMax(crong);
         }
 
         return answer;
@@ -28,6 +34,18 @@ class Problem1 {
             }
         }
         return false;
+    }
+
+    private static int calculatePageMax(List<Integer> pageList) {
+        int max = 0;
+
+        for (Integer pageNumber : pageList) {
+            int pageNumberToInt = pageNumber.intValue();
+            if (max < seperateOneNumber(pageNumberToInt)) {
+                max = seperateOneNumber(pageNumberToInt);
+            }
+        }
+        return max;
     }
 
     private static int seperateOneNumber(int pageNumberToInt) {
