359d4f3ebdf3f07f6193bfe54e1326924443778a
bomin1996 <bomin199628@gmail.com>
2022-10-27 01:08:59 +0900

    문제2번구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.*;
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        boolean count = true;
+        String[] array = cryptogram.split("");
+        List<String> list = Arrays.asList(array);
+        while (count){
+            count = false;
+            List<String> cur_string = new ArrayList<>();
+            cur_string.addAll(list);
+            for (int i = 0; list.size() -1>i;i++){
+                if (list.get(i).equals(list.get(i+1))){
+                    cur_string.remove(i+1);
+                    cur_string.remove(i);
+                    list = cur_string;
+                    count = true;
+                    continue;
+                }
+            }
+            list = cur_string;
+        }
+        StringBuilder bo = new StringBuilder();
+        for (String s : list){
+            bo.append(s);
+        }
+        answer = bo.toString();
         return answer;
     }
 }
