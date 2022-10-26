8bd75b97bc2f08802bc2c14e0a876d88eb43a872
jinny-l <jinny.l.dev@gmail.com>
2022-10-27 01:12:55 +0900

    [feat] 입력받은 문자열에 "3" "6" "9"가 포함되어 있는지 확인하는 메소드 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,20 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+
+    // 입력받은 문자열에 "3" "6" '9"가 포함되어 있는지 확인하는 메소드
+    private static boolean notContain_3_6_9(String num) {
+
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            String str = Character.toString(c);
+
+            if (str.equals("3") || str.equals("6") || str.equals("9")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
