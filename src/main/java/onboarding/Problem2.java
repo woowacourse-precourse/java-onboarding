80b4eac16f9f96f47cba890ebfea70a9594b3a06
hstla <ree7243@gmail.com>
2022-10-26 21:26:00 +0900

    문제2 feat: 리스트 문자 반환, solution 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,4 @@
 package onboarding;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,28 +12,29 @@ public class Problem2 {
         return list;
     }
 
+    public static String listToString (List<Character> list) {
+        String answer = "";
+        for (int i =0; i < list.size(); i ++) {
+            answer += list.get(i);
+        }
+        return answer;
+    }
 
-
-    public static List<Character> deleteSame(List<Character> list) {
+    public static void deleteSame(List<Character> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             if (list.get(i) == list.get(i + 1)) {
                 list.remove(i);
                 list.remove(i);
             }
         }
-        return list;
     }
 
     public static String solution(String cryptogram) {
-        String answer = "";
+        String answer;
         List<Character> list = stringToList(cryptogram);
-        System.out.println("list = " + list);
-
-        while (true) {
-            deleteSame(list);
-            break;
-        }
-        System.out.println("list = " + list);
+        int size = list.size();
+        for (int i = 0; i < size / 2; i++) deleteSame(list);
+        answer = listToString(list);
         return answer;
     }
 }
