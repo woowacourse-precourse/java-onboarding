1d221582372326b76c8ea7c5f49cca1f07108266
LEE-sh1673 <lsh901673@gmail.com>
2022-10-26 23:32:53 +0900

    feat($common): 페이지 숫자 순서 검증 기능 추가

--- src/main/java/onboarding/pagenumbergame/ValidationUtils.java
+++ src/main/java/onboarding/pagenumbergame/ValidationUtils.java
@@ -1,5 +1,7 @@
 package onboarding.pagenumbergame;
 
+import java.util.List;
+
 public class ValidationUtils {
 
 	private static final int MIN_PAGE_NUMBER = 1;
@@ -9,5 +11,12 @@ public class ValidationUtils {
 	public static boolean validateNumber(final int pageNumber) {
 		return MIN_PAGE_NUMBER < pageNumber
 			&& pageNumber < MAX_PAGE_NUMBER;
+	}
+
+	public static boolean validatePageOrder(final List<Integer> pageNumbers) {
+		int leftNumber = pageNumbers.get(0);
+		int rightNumber = pageNumbers.get(1);
+		return leftNumber < rightNumber
+			&& leftNumber + 1 == rightNumber;
 	}
 }
--- src/test/java/onboarding/pagenumbergame/ValidationUtilsTest.java
+++ src/test/java/onboarding/pagenumbergame/ValidationUtilsTest.java
@@ -2,6 +2,8 @@ package onboarding.pagenumbergame;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,17 @@ public class ValidationUtilsTest {
 		assertThat(ValidationUtils.validateNumber(400)).isFalse();
 		assertThat(ValidationUtils.validateNumber(2)).isTrue();
 		assertThat(ValidationUtils.validateNumber(5)).isTrue();
+	}
+
+	@DisplayName("페이지 숫자 순서 검증")
+	@Test
+	void 페이지_숫자_순서_검증() {
+		assertThat(ValidationUtils.validatePageOrder(List.of(99, 102))).isFalse();
+		assertThat(ValidationUtils.validatePageOrder(List.of(100, 99))).isFalse();
+		assertThat(ValidationUtils.validatePageOrder(List.of(100, 100))).isFalse();
+
+		assertThat(ValidationUtils.validatePageOrder(List.of(99, 100))).isTrue();
+		assertThat(ValidationUtils.validatePageOrder(List.of(197, 198))).isTrue();
+		assertThat(ValidationUtils.validatePageOrder(List.of(211, 212))).isTrue();
 	}
 }
