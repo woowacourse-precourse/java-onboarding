3d18171e086d6f7b0b85ac7c6ddb06e9d1df240d
JHS <wnghdtjr129@naver.com>
2022-10-26 21:25:41 +0900

    doc: 기능 구현사항 정리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,11 @@ package onboarding;
 
 import java.util.List;
 
+/** TODO
+ * 1. 입력 값이 옳게 들어왔는지 검증 - 페이지 두개의 값의 차 1, 홀수 짝수페이지 검증
+ * 2. 각 페이지의 자리수를 더한 값 곱한 값 중 큰수 구하기
+ * 3. 승자 비교
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
