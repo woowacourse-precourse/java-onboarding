e719ada4b9eaf70e9859664b7d13697fc3a5c045
은지 <midzy0228@gmail.com>
2022-10-27 00:33:59 +0900

    Add remark Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,15 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        // 구현해야할 기능
+        /*
+        * (pobi[0]%2!=0 && pobi[1]%2==0 && crong[0]%2!=0 && crong[1]%2==0) false ==> -1
+        * name[0]이 홀수, name[1]이 짝수가 아니라면 -1 이 사항은 상관안써도 될듯
+        * 점수가 1<= 400 범위 밖이라면 -1
+        * 각 자리수 더해서 max 값 찾음
+        * 비교후 포비가 이긴다면 1, 크롱이 이긴다면 2, 비긴다면 0
+        * */
+        //System.out.println(pobi);
         return answer;
     }
 }
