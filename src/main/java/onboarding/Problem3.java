53c1d5020201f6332ecffd67e0690ce439502e7a
kevstevie <jianna442@gmail.com>
2022-10-26 17:55:23 +0900

    feat: add countingClap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return countingClap(number);
+    }
+
+    static int countingClap(int num){
+        int count = 0;
+        while(num>0){
+            if(num % 10 % 3 == 0 && num % 10 != 0){
+                count++;
+            }
+            num /= 10;
+        }
+        return count;
     }
 }
