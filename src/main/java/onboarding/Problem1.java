e5aaeac51c99afaffdab4de50c176e4543f89817
jeomxon <jeomxon@gmail.com>
2022-10-26 23:09:43 +0900

    feat: find max score of page

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        //페이지를 입력받아 페이지의 최대 점수를 반환하는 함수(더하기, 곱하기 비교)
+        //왼쪽, 오른쪽 페이지의 점수를 받아 최대점수를 반환하는 함수
+
         int answer = Integer.MAX_VALUE;
 
         if (!isCorrectInput(pobi.get(0), pobi.get(1)) || !isCorrectInput(crong.get(0), crong.get(1))) {
             return -1;
         }
-
-
 
         return answer;
     }
@@ -27,4 +28,17 @@ class Problem1 {
         return true;
     }
 
+    public static int findMaxOfPage(int numOfPage) {
+        String numOfStringPage = Integer.toString(numOfPage);
+        int sumOfPage = 0;
+        int productOfPage = 1;
+
+        for (int i = 0; i < numOfStringPage.length(); i++) {
+            int strToInt = Integer.parseInt(numOfStringPage.substring(i, i + 1));
+            sumOfPage += strToInt;
+            productOfPage *= strToInt;
+        }
+
+        return Math.max(sumOfPage, productOfPage);
+    }
 }
