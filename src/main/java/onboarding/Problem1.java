05ec460f3511dd84b48dfe280cd3261fbd5d1abe
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-26 17:23:38 +0900

    Docs: 문제 1 구현 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,6 +10,12 @@ class Problem1 {
 		return value2;
 	}
 
+	private static int bigPageResult(List<Integer> person) {
+		int leftPage = person.get(0);
+		int rightPage = person.get(1);
+		
+	}
+
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int answer = Integer.MAX_VALUE;
 
