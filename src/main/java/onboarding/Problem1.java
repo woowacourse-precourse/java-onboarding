698c651ad5a4912ea7426083b2695b62eeb5dd4f
woosung1223 <dntjd991223@naver.com>
2022-10-26 22:05:22 +0900

    [문제 1] 예외처리 기능 담당 validationCheck 함수 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,23 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static boolean validationCheck(List<Integer> user_1, List<Integer> user_2) {
+        if (user_1.contains(1) || user_1.contains(400) ||
+                user_2.contains(1) || user_2.contains(400)) {
+            return false;
+        } else if (user_1.get(0) < 1 || user_1.get(1) > 400 ||
+                user_2.get(0) < 1 || user_2.get(1) > 400) {
+            return false;
+        } else if (user_1.get(1) != user_1.get(0) + 1 ||
+                user_2.get(1) != user_2.get(0) + 1) {
+            return false;
+        } else if (user_1.get(0) % 2 != 1 ||
+                user_2.get(0) % 2 != 1) {
+            return false;
+        } else if (user_1.size() != 2 || user_2.size() != 2) {
+            return false;
+        }
+        return true;
+    }
 }
