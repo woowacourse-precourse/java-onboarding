52ad9a4f439520fe6a71d03408464bd5c7f991a5
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 20:08:36 +0900

    청개구리 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,19 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    
+    //주어진 문자와 반대되는 문자열 반환하는 함수
+    private static Character changeWord(Character c){
+        //소문자인 경우
+        if(c>='a' && c<='z'){
+            return (char)(('z'-c)+'a');
+        }
+        //대문자인 경우
+        else if(c>='A' && c<='Z'){
+            return (char)(('Z'-c)+'A');
+        }
+        else{
+            return c;
+        }
+    }
 }
