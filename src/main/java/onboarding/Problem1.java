42419e57fd3b252b4342b29eb6b691033b614f11
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 18:29:18 +0900

    번호에 대한 각 자리 숫자들의 합이나 곱 중 최대값 구하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,15 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    //번호의 각 자리 숫자들의 합이나 곱 중 가장 큰값을 구하는 함수
+    private static int getNumberMax(int num){
+        int sum = 0, multi = 1;
+        while(num != 0){
+            sum += num%10;
+            multi *= num%10;
+            num /= 10;
+        }
+        return Math.max(sum, multi);
+    }
 }
