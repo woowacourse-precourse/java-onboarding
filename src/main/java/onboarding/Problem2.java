863f022ed7a89f7491fba3ba666aefbe4a6233b2
jthugg <jthug@naver.com>
2022-10-27 00:33:32 +0900

    init problem2 & chk todo list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        //1. 중복체크 기능
+        //2. 지워야 할 문자열
+        //3. 루프내에서 중복 문자열을 삭제할때마다 체크
+        //4. 더이상 지울 문자열이 없으면 리턴
+        
+        return cryptogram;
     }
 }
