570920cce0a89e713bb6c71b54577e13aed36e49
ma-an-jong <alswhd1113@gmail.com>
2022-10-26 21:23:37 +0900

    problem2 passed test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,19 @@
 import onboarding.Problem1;
+import onboarding.Problem2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Problem1.solution(Arrays.asList(97,98) ,Arrays.asList(197,198)));
-        System.out.println(Problem1.solution(Arrays.asList(131,132) ,Arrays.asList(211,212)));
-        System.out.println(Problem1.solution(Arrays.asList(99,102) ,Arrays.asList(211,212)));
+//        System.out.println(Problem1.solution(Arrays.asList(97,98) ,Arrays.asList(197,198)));
+//        System.out.println(Problem1.solution(Arrays.asList(131,132) ,Arrays.asList(211,212)));
+//        System.out.println(Problem1.solution(Arrays.asList(99,102) ,Arrays.asList(211,212)));
+
+        String cryptogram = "browoanoommnaon";
+        String result = "brown";
+        System.out.println(Problem2.solution(cryptogram));
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,91 @@
 package onboarding;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+class Decoder
+{
+    private int itemIndex;
+    private int emptyIndex;
+    Stack<Character>[] stacks;
+
+    public Decoder(String cryptogram)
+    {
+        stacks = new Stack[]{new Stack<Character>(),  new Stack<Character>()};
+
+        itemIndex = 0;
+        emptyIndex = 1;
+
+        for(int i = 0 ; i < cryptogram.length();i++)
+        {
+            stacks[itemIndex].add(cryptogram.charAt(i));
+        }
+    }
+
+    public boolean removeDuplicatedCharacter()
+    {
+        boolean flag = false;
+
+        char c = '\u0000';
+
+        while(!stacks[itemIndex].isEmpty())
+        {
+
+            if(stacks[itemIndex].peek().equals(c))
+            {
+                stacks[itemIndex].pop();
+                stacks[emptyIndex].pop();
+                flag = true;
+            }
+            else
+            {
+                c = stacks[itemIndex].pop();
+                stacks[emptyIndex].push(c);
+            }
+
+        }
+
+        swapIndex();
+
+        return flag;
+    }
+
+    private void swapIndex()
+    {
+        int tmp = itemIndex;
+        itemIndex = emptyIndex;
+        emptyIndex = tmp;
+    }
+
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        if(itemIndex == 0)
+        {
+            for(char c : stacks[itemIndex])
+            {
+                sb.append(c);
+            }
+        }
+        else
+        {
+            while(!stacks[itemIndex].isEmpty())
+            {
+                sb.append(stacks[itemIndex].pop());
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+}
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Decoder decodeQueue = new Decoder(cryptogram);
+        while(decodeQueue.removeDuplicatedCharacter());
+        return decodeQueue.toString();
     }
 }
