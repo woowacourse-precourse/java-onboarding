c31f15a71230a6c22fea963733850407ea0e46b1
greeng00se <thegreengoose7@gmail.com>
2022-10-26 16:51:53 +0900

    feat(Problem1): 올바른 입력 값인지 검증하는 로직 추가
    - isValidRange: 페이지가 문제의 요구사항 범위 내에 있는지 확인
    - isPageContinuous: 페이지가 연속되는지 확인
    - isLeftPage, isRightPage: 페이지의 왼쪽은 홀수, 오른쪽은 짝수인지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,59 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int LEFT_PAGE_INDEX = 0;
+    private static final int RIGHT_PAGE_INDEX = 1;
+
+    private static final int MIN_PAGE_LENGTH = 1;
+    private static final int MAX_PAGE_LENGTH = 400;
+
+    private static final int RESULT_EXCEPTION = -1;
+    private static final int RESULT_DRAW = 0;
+    private static final int RESULT_POBI_WIN = 1;
+    private static final int RESULT_CRONG_WIN = 2;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!isValidPages(pobi) || !isValidPages(crong)) {
+            return RESULT_EXCEPTION;
+        }
+
         return answer;
+    }
+
+    private static boolean isValidPages(List<Integer> pages) {
+        Integer leftPage = pages.get(LEFT_PAGE_INDEX);
+        Integer rightPage = pages.get(RIGHT_PAGE_INDEX);
+
+        if (!isValidRange(leftPage) || !isValidRange(rightPage)) {
+            return false;
+        }
+
+        if (!isPageContinuous(leftPage, rightPage)) {
+            return false;
+        }
+
+        if (!isLeftPage(leftPage) || !isRightPage(rightPage)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isValidRange(Integer page) {
+        return MIN_PAGE_LENGTH <= page && page <= MAX_PAGE_LENGTH;
+    }
+
+    private static boolean isPageContinuous(Integer leftPage, Integer rightPage) {
+        return leftPage + 1 == rightPage;
+    }
+
+    private static boolean isLeftPage(Integer page) {
+        return page % 2 == 1;
+    }
+
+    private static boolean isRightPage(Integer page) {
+        return page % 2 == 0;
     }
 }
