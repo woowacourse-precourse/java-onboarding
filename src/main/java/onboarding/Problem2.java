51d795cad924bd1c42131c37047e868601df4a87
mckim123 <minchan06@gmail.com>
2022-10-27 03:19:30 +0900

    문제 2 기능 목록 구성 및 기능 1 구현(문제 2 완료)
    문제 2의 기능 목록을 작성하고 기능을 모두 구현하였습니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        Stack<String> stack = new Stack<>();
+        int i = 0;
+        int N = cryptogram.length();
+
+        while (i < N){
+            String next = Character.toString(cryptogram.charAt(i));
+            if (stack.empty()) {
+                stack.push(next);
+                i += 1;
+            } else if (stack.peek().equals(next)){
+                stack.pop();
+                i += 1;
+            } else {
+                stack.push(next);
+                i += 1;
+            }
+        }
+        String answer = "";
+        for (String s:stack){
+            answer = answer + s;
+        }
         return answer;
     }
 }
