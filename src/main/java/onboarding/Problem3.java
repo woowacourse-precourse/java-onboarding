a712f655b3ba89fc357e18b4b28a29e5de5dee47
SEONMl <sm000117@naver.com>
2022-10-26 22:51:06 +0900

    feat: Problem3 ClapCount class 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    static final int MAX_SIZE = 10001;
+
+    static class ClapCount {
+        private int[] count = new int[MAX_SIZE];
+        private int target;
+
+        public ClapCount(int target) {
+            this.target = target;
+        }
+    }
+
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
