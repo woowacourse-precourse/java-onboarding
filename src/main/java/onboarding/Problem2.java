1bc1a0d83943089f62f35a5c2c6dd7cc02080ff5
SH <gram753@naver.com>
2022-10-27 01:01:26 +0900

    feat: Problem2 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+
+            if (!stack.isEmpty() && stack.peek() == cryptogram.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.add(cryptogram.charAt(i));
+            }
+        }
+
+        return stack.stream().map(String::valueOf).collect(Collectors.joining());
     }
+
 }
