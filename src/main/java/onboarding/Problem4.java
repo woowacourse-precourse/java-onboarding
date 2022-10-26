e07d8e400a255fa0a9d4599963e6e4a134490806
sangil <gsl0505@naver.com>
2022-10-26 19:53:56 +0900

    feat: 조건에 맞는 문자열 변형 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
 	public static String solution(String word) {
 		String answer = "";
+		for (char ch : word.toCharArray()) {
+			answer += converter(ch);
+		}
 		return answer;
+	}
+
+	private static char converter(char alphabet) {
+		if ('a' <= alphabet && 'z' >= alphabet) {
+			alphabet = (char)('z' + 'a' - alphabet);
+		} else if ('A' <= alphabet && 'Z' >= alphabet) {
+			alphabet = (char)('Z' + 'A' - alphabet);
+		} else {
+			return alphabet;
+		}
+		return alphabet;
 	}
 }
