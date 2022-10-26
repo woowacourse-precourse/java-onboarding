1af4ff2a9e1a35b1c3e6d9341db831b07c36b917
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 19:42:54 +0900

    feat(Problem2.java): removeDuplication() 연속된 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,19 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeDuplication(String s){
+        StringBuilder sb=new StringBuilder(s);
+
+        for(int i=0; i<sb.length()-1; i++){
+            if(sb.charAt(i)==sb.charAt(i+1)){
+                sb.delete(i,i+2);
+                i--;
+            }
+        }
+
+        return sb.toString();
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
