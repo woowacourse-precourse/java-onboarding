30a2d97bcd6a8da82f79763e6577a75166f90b01
JUHA <khabh@naver.com>
2022-10-27 08:36:14 +0900

    Add(Problem1) : 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /*
+    (1) 전달인자(페이지 정보)에 대한 예외 처리
+        1. 페이지 존재 여부 확인 (1-400 사이의 값)
+        2. 서로 연결된 페이지가 맞는지 확인 (ex: [102, 103], [6, 7])
+   (2) 페이지 세자리수의 곱과 페이지 세자리수의 합 중 더 큰 값을 점수로 저장
+   (3) 두 사람의 점수를 비교해서 게임의 결과 return
+   */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
