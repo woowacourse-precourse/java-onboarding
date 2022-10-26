ea3ecc26d0ef8563c2b32bfeca975e2d6fca770b
youngkyunIm <yee0230@naver.com>
2022-10-26 23:26:53 +0900

    feat(Problem2) : add removeReduplication
    문자열을 반복하며 중복된 문자들을 삭제하는 기능을 한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = removeReduplication(cryptogram);
         return answer;
+    }
+
+    private static String removeReduplication(String cryptogram) {
+        int startIndex = 0;
+        int lastIndex = 0;
+        String startString;
+        String lastString;
+
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                if (startIndex == 0) {
+                    startIndex = i;
+                }
+                lastIndex = i + 1;
+            }
+        }
+        startString = cryptogram.substring(0, startIndex);
+        // 중복 다음 값의 index 부터 시작이라 +1
+        lastString = cryptogram.substring(lastIndex + 1);
+        return startString + lastString;
     }
 }
