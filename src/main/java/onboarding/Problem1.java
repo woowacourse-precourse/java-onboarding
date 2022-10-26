d84ce2a3812e48c1f4ab736e697cb83251334483
there <there@example.com>
2022-10-26 18:10:34 +0900

    Problem1 기능 목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,21 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int leftPagePobi = pobi.get(0);
+        int rightPagePobi = pobi.get(1);
+        int leftPageCrong = crong.get(0);
+        int rightPageCrong = crong.get(1);
+
+        /*예외사항 발생시 처리*/
+
+
+        /*포비와 크롱의 왼쪽, 오른쪽 페이지 중에 가장 큰 값 구하기*/
+
+
+        /*포비와 크롱 대결 승패 결정*/
+
+
         return answer;
     }
+
 }
