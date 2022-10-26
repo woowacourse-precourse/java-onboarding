11da6523ff787bf51f6f013f919bced82dd78baf
sangwon <swchoi1997@naver.com>
2022-10-27 08:58:08 +0900

    feat,test : 1이상 1,000,000 범위 확인

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] MONEY_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
+        checkValidRange(money);
         List<Integer> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    private static void checkValidRange(int money) {
+        if (money < 1 || money > 1000000) {
+            throw new IllegalArgumentException();
+        }
     }
 }
