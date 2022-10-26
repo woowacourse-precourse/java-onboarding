e737a4f258adf964f4f6fab61128e1b2415da8ab
이한솔 <hansol8701@naver.com>
2022-10-27 00:47:45 +0900

    feat: 예외 처리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+
+    public boolean validation(String cryptogram) {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            return false;
+        }
+        else if (!cryptogram.equals(cryptogram.toLowerCase())){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+
         return answer;
     }
 }
