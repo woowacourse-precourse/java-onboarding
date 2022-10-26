19c497a206bdf921198380cafc33f8dc5f6de798
sangil <gsl0505@naver.com>
2022-10-26 16:31:06 +0900

    feat: 각 자리 수 더하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+    private static final int NUMBER_OF_DIGIT_DIVISION = 10;
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+
+	private static int digitAdd(Integer page) {
+            int addValue = 0;
+            while(page!=0){
+                addValue += page % NUMBER_OF_DIGIT_DIVISION;
+                page /= NUMBER_OF_DIGIT_DIVISION;
+            }
+            return addValue;
+	}
+
 }
