a6e960c1fe6f36d4ab563798a12934e73d27914e
KMuSeung <peter0757@nvaer.com>
2022-10-26 23:02:30 +0900

    feat : Problem2에 중복제거 메서드 removeDuplicateLetters 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,12 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    public String removeDuplicateLetters(String str) {
+        String removedStr = str;
+        for(char i = 'a'; i <= 'z'; i++) {
+            removedStr = removedStr.replaceAll(i + "{2,}", " ");
+        }
+        removedStr = removedStr.replaceAll(" ", "");
+        return removedStr;
+    }
 }
