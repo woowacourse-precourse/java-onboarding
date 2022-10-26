5df40212597b2ccc68632e1ad9bfdadd06207720
2jun0 <soo28819@gmail.com>
2022-10-27 03:32:13 +0900

    Feat: 문제 2번 - 중복 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: cryptogram.toCharArray()) {
+            if (stack.peek() == c)
+                stack.pop();
+            else
+                stack.push(c);
+        }
+
+        String result = "";
+
+        return result;
     }
 }
---
+++ src/main/java/onboarding/problem2/usecase.md
@@ -0,0 +1,7 @@
+# Problem 2 기능 목록
+- `cryptogram`의 중복제거
+- 중복제거된 `cryptogram` 반환
+
+## 제한사항 예외처리
+- `cryptogram`의 길이가 1이상 1000이하가 아니면 예외 발동
+- `cryptogram`의 모든 문자가 알파벳 소문자로만 이루어져 있지 않으면 예외 발동
