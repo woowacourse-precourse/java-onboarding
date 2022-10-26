b9c5714b77573733635440a452d5a954bff0b5b7
leewoojin <zmfpdl64@naver.com>
2022-10-26 17:49:41 +0900

    문제1번 이우진 제출합니다.
    문제: 정수의 각 자라의 값들을 더하거나 곱한값의 최댓값
    
    해결방법:
    1. 더한값을 구하는 함수와 곱한값을 구하는 함수를 구현
    1.1. 정수를 10으로 나눈 나머지를 모두 더해서 각 자리 값을 구한다.
    1.2. 곱한값도 더한값과 비슷하나 초기값이 0이기에 처음일 때 첫번째 값을 대입한다.
    
    2. 앞에서 구한 더한값, 곱한값들을 모두 비교하여 최댓값을 출력한다.
    2.1. 단 입력 조건이 1페이지 초과, 미만이 되면 안되기 때문에 해당 조건에 해당되면 -1로 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,64 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(Math.abs(pobi.get(0) - pobi.get(1)) != 1 || Math.abs(crong.get(0) - crong.get(1)) != 1) return -1;
+        int pobi_max = Math.max(sum_max(pobi), mul_max(pobi));
+        int crong_max = Math.max(sum_max(crong), mul_max(crong));
+        if(pobi_max > crong_max) return 1;
+        else if(pobi_max == crong_max) return 0;
+        else return 2;
+    }
+
+
+    public static int mul_max(List<Integer> pages) {
+        int max = 0;
+        int[] sum = {0, 0};
+        for(int i = 0; i < pages.size(); i++){
+            int num = pages.get(i);
+            int re = 0;
+            while(num > 0) {
+                if(re == 0) re = num % 10;
+                else re *= num % 10;
+                num /= 10;
+
+            }
+            sum[i] = re;
+        }
+        return Math.max(sum[0], sum[1]);
+    }
+
+    public static int sum_max(List<Integer> pages){
+        int max = 0;
+        int[] sum = {0, 0};
+        for(int i = 0; i < pages.size(); i++){
+            int num = pages.get(i);
+            int re = 0;
+            while(num > 0){
+                re += num % 10;
+                num /= 10;
+            }
+            sum[i] = re;
+        }
+        return Math.max(sum[0], sum[1]);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> pobi = new ArrayList<>();
+//        pobi.add(97);
+//        pobi.add(98);
+        pobi.add(132);
+        pobi.add(133);
+        List<Integer> crong = new ArrayList<>();
+//        crong.add(197);
+//        crong.add(198);
+        crong.add(211);
+        crong.add(212);
+
+        System.out.println(solution(pobi, crong));
+
     }
 }
