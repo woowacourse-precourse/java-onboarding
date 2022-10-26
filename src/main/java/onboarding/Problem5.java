6499f2eae57df20c3ac90729ef45e6d90a15f9b8
hodomaroo <enghemdwkd@gmail.com>
2022-10-26 21:50:35 +0900

    problem5 구현 완료. 추후 코딩 컨벤션에 따라 코드 리팩토링 예정입니다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ *  🚀 기능 명세
+    입력으로 주어진 금액에 대응되는 각 금액권의 개수 배열을 반환하는 메서드(getNumberOfEachMonetaryUnit)
+
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> answer = new ArrayList<Integer>();
+        return getNumberOfEachMonetaryUnit(money, answer);
+    }
+
+    public static List<Integer> getNumberOfEachMonetaryUnit(int money, List<Integer> monetaryList){
+        List<Integer> monetaryUnitList = List.of(50000,10000,5000,1000,500,100,50,10,1);
+
+        for(int i = 0 ; i < monetaryUnitList.size() ; i++){
+            monetaryList.add(money / monetaryUnitList.get(i));
+            money %= monetaryUnitList.get(i);
+        }
+
+        return monetaryList;
     }
 }
