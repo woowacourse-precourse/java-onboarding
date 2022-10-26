bcce933a37b802730f873e18b040fd0afd3c2a5e
skydreamer21 <skydreamer210@gmail.com>
2022-10-26 18:48:49 +0900

    [Feature] Problem2 - 암호 해독 기능 구현
    함수명 : decrypt

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,9 +5,40 @@
 
 package onboarding;
 
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String decrypt(String cryptogram) {
+        char[] cryptogramCharArray = cryptogram.toCharArray();
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : cryptogramCharArray) {
+            if (!stack.isEmpty() && stack.peekLast() == c) {
+                stack.pollLast();
+                continue;
+            }
+            stack.add(c);
+        }
+
+        return stackToString(stack);
+    }
+
+    private static String stackToString(Deque<Character> stack) {
+        int decryptedLength = stack.size();
+        char[] decryptedCharArray = new char[decryptedLength];
+
+        int index = 0;
+        while (!stack.isEmpty()) {
+            decryptedCharArray[index++] = stack.poll();
+        }
+
+        return String.valueOf(decryptedCharArray);
     }
 }
