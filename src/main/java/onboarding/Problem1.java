c0f698ef69000efc2e6435785cf8c275ecc04b4c
yeonhee <ydol2@naver.com>
2022-10-26 23:37:58 +0900

    [1번] feat : 예외처리 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,18 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /**
+     *예외처리 함수
+     */
+    private static int exception(List<Integer> person){
+        if( person.get(0)%2==0 || person.get(1)%2==1){
+            return -1;
+        } else if (person.get(1) - person.get(0) != 1){
+            return -1;
+        } else{
+            return 0;
+        }
+    }
+
 }
