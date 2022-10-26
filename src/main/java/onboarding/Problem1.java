3cd415ac6f967b00b0d769f853fa76e03b65a3bb
whibaek <jibaek0513@gmail.com>
2022-10-27 01:07:29 +0900

    P1 Add getValue Method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -14,6 +14,19 @@ class Problem1 {
         return false;
     }
 
+    public static int getValue(int val){
+        int addSum = 0;
+        int mulSum = 1;
+
+        while(val > 0){
+            int temp = val % 10;
+            addSum += temp;
+            mulSum *= temp;
+            val /= 10;
+        }
+        return Math.max(addSum, mulSum);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
