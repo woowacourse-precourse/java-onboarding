38712869129fbf689b0205576f3cdd50164e84c8
DoooongDong <rhkswls98@kakao.com>
2022-10-26 23:01:49 +0900

    [Problem1] 시작 면이나 마지막 면이 나오면 예외사항으로 간주하고 -1을 return 한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,11 @@ class Problem1 {
         if(Math.abs(pobi.get(0) - pobi.get(1)) != 1 || Math.abs(crong.get(0) - crong.get(1)) != 1) {
             return -1;
         }
+        //시작 면이나 마지막 면이 나오면 예외 발생
+        if(pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(0) == 399 || crong.get(0) == 399){
+            return -1;
+        }
+
         return answer;
     }
 }
