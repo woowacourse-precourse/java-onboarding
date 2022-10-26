20d4123660bc659f3a3f16c5e8ac3f312d771e28
sangil <gsl0505@naver.com>
2022-10-26 19:02:54 +0900

    feat: 중복이 있을 경우 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,18 +5,33 @@ public class Problem2 {
 
 	public static String solution(String cryptogram) {
 		String answer = cryptogram;
+		int answerLength;
+		do {
+			answerLength = answer.length();
+			answer = removeString(answer);
+
+		} while(answerLength != answer.length() && answer.length()!=0);
+
 		return answer;
 	}
 
-	public static String removeString(String answer) {
-		char checkChar = answer.charAt(CHECK_INIT);
-		StringBuilder removeString = new StringBuilder();
-		for (int i = 1; i < answer.length(); i++) {
-			if (answer.charAt(i) != checkChar) {
-				removeString.append(answer.charAt(i));
+	private static String removeString(String answer) {
+		StringBuilder plainIndex = new StringBuilder();
+		char checkText = answer.charAt(CHECK_INIT);
+		for(int i =1; i<answer.length(); i++) {
+			if(checkText != answer.charAt(i)) {
+				checkText = answer.charAt(i);
+				plainIndex.append(answer.charAt(i-1));
+			} else {
+				while(i<answer.length() && checkText == answer.charAt(i)){
+					i++;
+				}
 			}
 		}
-        return removeString.toString();
+		if(plainIndex.toString().length()>0) {
+			return plainIndex.append(answer.charAt(answer.length() - 1)).toString();
+		}
+		return plainIndex.toString();
 	}
 
 }
