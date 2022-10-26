f0ea859bb0f83391f55c3ec92a55636a1a7384af
DoooongDong <rhkswls98@kakao.com>
2022-10-26 23:51:55 +0900

    [Problem1] 각 자리의 합을 구하는 sum 함수와 각 자리의 곱을 구하는 mul 함수를 작성하고 포비와 크롱의 점수를 각각 구해 둘을 비교하여 승자를 구한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    //각 자리 수를 모두 합한 값을 반환
+    public static int sum(int num){
+        int ret = 0;
+        int tmp = num;
+        while(tmp != 0){
+            ret += tmp % 10;
+            tmp /= 10;
+        }
+        return ret;
+    }
+    //각 자리 수를 모두 곱한 값을 반환
+    public static int mul(int num){
+        int ret = 1;
+        int tmp = num;
+        while(tmp != 0){
+            ret *= tmp % 10;
+            tmp /= 10;
+        }
+        return ret;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         //양면의 페이지 수 차가 1이 아니면 예외 발생
@@ -14,6 +34,23 @@ class Problem1 {
             return -1;
         }
 
+        //왼쪽, 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구함.
+        int pScore = Math.max(
+                Math.max(sum(pobi.get(0)), mul(pobi.get(0))),
+                Math.max(sum(pobi.get(1)), mul(pobi.get(1)))
+        );
+        int cScore = Math.max(
+                Math.max(sum(crong.get(0)), mul(crong.get(0))),
+                Math.max(sum(crong.get(1)), mul(crong.get(1)))
+        );
+
+        if(pScore > cScore){
+            answer = 1;
+        }else if(cScore > pScore){
+            answer = 2;
+        }else {
+            answer = 0;
+        }
         return answer;
     }
 }
