ca4ccc014328ed0d42f4bde3ede1c6f327e38a5a
KTH-96 <taehyun960@gmail.com>
2022-10-26 19:26:55 +0900

    feat(Problem1): 양쪽 페이지중 가장 큰값을 구하는 기능 구현(조건 2,3의 식으로 구한다.)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -14,8 +14,34 @@ class Problem1 {
      * 2. 페이지가 붙어있지 않은경우 ex) [99, 102]
      * 3. 페이지 순서가 [홀, 짝]이 아닌경우
      */
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int pobiMaxNum = findMaxNum(pobi);
+		int crongMaxNum = findMaxNum(crong);
+
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+
+	private static int findMaxNum(List<Integer> pageList) {
+		int maxValue = Integer.MIN_VALUE;
+		for (Integer page : pageList) {
+			if (page < 10) {
+				maxValue = Math.max(maxValue, page);
+			} else if (page < 100) {
+				int units = page % 10;
+				int tens = page / 10;
+
+				int bigNum = Math.max(units + tens, units * tens);
+				maxValue = Math.max(maxValue, bigNum);
+			} else {
+				int units = page % 10;
+				int tens = (page / 10) % 10;
+				int hundreds = page / 100;
+
+				int bigNum = Math.max(units + tens + hundreds, units * tens * hundreds);
+				maxValue = Math.max(maxValue, bigNum);
+			}
+		}
+		return maxValue;
+	}
 }
