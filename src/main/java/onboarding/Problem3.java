0386baa503a9ef82a63af5bc635ba51235e0c193
whdcks420 <whdcks420@gmail.com>
2022-10-27 00:06:37 +0900

    feat: PROBLEM3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,14 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return (int) IntStream.rangeClosed(1, number)
+                .mapToObj(String::valueOf)
+                .map(i -> i.split(""))
+                .flatMap(j -> Arrays.stream(j).filter(k -> k.equals("3") || k.equals("6") || k.equals("9")))
+                .count();
     }
 }
