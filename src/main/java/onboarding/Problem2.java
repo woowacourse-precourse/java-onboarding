5391f5801b1992708370e5e2d9491e8bcb797f8a
이한솔 <hansol8701@naver.com>
2022-10-27 00:53:39 +0900

    feat: 연속하는 중복 문자 세기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,16 +5,29 @@ public class Problem2 {
     public boolean validation(String cryptogram) {
         if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
             return false;
-        }
-        else if (!cryptogram.equals(cryptogram.toLowerCase())){
+        } else if (!cryptogram.equals(cryptogram.toLowerCase())) {
             return false;
-        }else {
+        } else {
             return true;
         }
     }
 
+    public static int countDuplicatedNeighbor(char[] cryptogram, int start) {
+        int cnt = 1;
+        for (int i = start; i < cryptogram.length - 1; i++) {
+            if (cryptogram[i] == cryptogram[i+1]){
+                cnt++;
+            }
+            else{
+                break;
+            }
+        }
+        return cnt;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+        char[] charArr = cryptogram.toCharArray();
 
 
         return answer;
