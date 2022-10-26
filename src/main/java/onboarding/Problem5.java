9e4942847f15124455940581a7e22703ee6eb95c
K-Diger <kitten.diger@gmail.com>
2022-10-26 20:45:43 +0900

    Week1 : Problem5 Solved - static 변수 purse 로 변환할 수 있는 화폐단위 셋팅 - 결과값 리스트에 현재 가진 총금액을 화폐단위로 나눈 몫을 추가 - 몫에 대한 나머지를 갱신하여 가진 화폐의 갯수만큼 반복하는 메서드 divideMoney 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    // [오만원, 만원, 오천원, 천원, 오백원, 백원, 오십원, 십원, 일원]
+    static List<Integer> purse = new ArrayList<>() {{
+        add(50000);
+        add(10000);
+        add(5000);
+        add(1000);
+        add(500);
+        add(100);
+        add(50);
+        add(10);
+        add(1);
+    }};
+
+    public static List<Integer> divideMoney(int money) {
+        int remainMount = money;
+
+        List<Integer> result = new ArrayList<>(purse.size());
+
+        for (int i = 0; i < purse.size(); i++) {
+            // 결과 값 배열에 각 인덱스를 넣음
+            result.add(remainMount / purse.get(i));
+            // 배열에 넣은 후 나머지 현재 남은 돈에 반영
+            remainMount %= purse.get(i);
+        }
+        return result;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return divideMoney(money);
     }
 }
