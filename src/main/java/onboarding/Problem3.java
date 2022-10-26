22a13f370effd7316ec004c893f774b7468065d1
sangwon <swchoi1997@naver.com>
2022-10-26 23:28:47 +0900

    feat : 3, 6, 9가 들어가는 숫자에 박수 처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
-
+/**
+ * 1. 유효범위 숫자 확인
+ * 2. 숫자를 String으로 변환 후 각 자리수 별로 나누기
+ */
 public class Problem3 {
+
+    public static final String SPLIT_POINT = "";
     public static int solution(int number) {
+        checkValidRange(number);
         int answer = 0;
+        int result = 0;
+        for(int i = 1; i <= number ; i ++ ){
+            String[] check369 = String.valueOf(i).split(SPLIT_POINT);
+            for (String step : check369) {
+                if (step.equals("3") || step.equals("6") || step.equals("9")) {
+                    result += 1;
+                }
+            }
+        }
+
+        answer = result;
         return answer;
     }
+
+    private static void checkValidRange(int number) {
+        if (number > 10000 || number <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
