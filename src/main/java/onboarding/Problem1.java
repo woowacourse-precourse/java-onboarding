9d788090bb289f343684b4396acca34f43ac1a7e
yoondgu <doyou2ing@naver.com>
2022-10-26 20:31:14 +0900

    feat(Problem1):Implement method to get score of pages

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -54,6 +54,16 @@ class Problem1 {
    private static int getScore(List<Integer> pages) {
 	   int score = 0;
 	   
+	   for (Integer page : pages) {
+		   // TODO 입력값이 잘못될 일 없으면 필요없는 if문. 고민해볼 것
+		   if (page != null) {
+			   // 페이지 번호를 꺼내서 각 자리 수의 합, 각 자리 수의 곱 중 큰 값을 택한다.
+			   int pageValue = Math.max(sumNumbersOfDigits(page), multiplyNumbersOfDigits(page));
+			   // 최종적으로 왼쪽 페이지 번호의 값, 오른쪽 페이지 번호의 값 중 더 큰 값이 score에 저장된다.
+			   score = Math.max(pageValue, score);
+		   }
+	   }
+	   
 	   return score;
    }
 }
