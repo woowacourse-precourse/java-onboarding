8415064b0c58220e94a18ae53a35dfe5aae56448
juno-junho <ssmm0205@naver.com>
2022-10-26 22:26:11 +0900

    feat: solve problem 1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,9 +4,21 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        // 예외 거르고
+        if (pobi.size() !=2 || crong.size()!=2) return -1;
+        if (pobi.get(1) < pobi.get(0) || crong.get(1) < crong.get(0)) return -1;
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) return -1;
+        if (pobi.get(0) == 1 || crong.get(1) == 400) return -1;
+        // pobi, crong 모두 왼쪽 숫자, 오른쪽 숫자 각각 곱하거나 더해서 큰수 고른다
+        // 둘중 큰수
+        // 둘 비교해서 값 반환.
+        int pobiNum = highestNum(pobi);
+        int crongNum = highestNum(crong);
 
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (pobiNum > crongNum) return 1;
+        if (pobiNum < crongNum) return 2;
+        if (pobiNum == crongNum) return 0;
+        return -1;
     }
 
     private static int highestNum(List<Integer> pobi) {
