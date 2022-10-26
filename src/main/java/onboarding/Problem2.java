54056753fd18510e3898a3a2c8fe86f57b30ecc0
DongwookKim0823 <dwkim0823@naver.com>
2022-10-26 22:27:07 +0900

    feat: (Problem2) find duplicated character index

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,41 @@
 package onboarding;
 
+import java.util.HashSet;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        //String preCryptogram = cryptogram;
+        deleteDuplicatedCharacter(cryptogram);
+
+
         String answer = "answer";
         return answer;
+    }
+
+    private static void deleteDuplicatedCharacter(String cryptogram) {
+
+        StringBuilder cryptogramStringBuilder = new StringBuilder(cryptogram);
+
+        while (cryptogramStringBuilder.length() != 0) {
+
+            HashSet<Integer> duplicatedCharacterSets;
+
+            duplicatedCharacterSets = findDuplicatedCharacterIndex(cryptogramStringBuilder);
+
+        }
+    }
+
+    private static HashSet<Integer> findDuplicatedCharacterIndex(StringBuilder cryptogramStringBuilder) {
+        HashSet<Integer> duplicatedCharacterSets = new HashSet<Integer>();
+
+        for (int i = 1; i < cryptogramStringBuilder.length(); i++) {
+            if (cryptogramStringBuilder.charAt(i-1) != cryptogramStringBuilder.charAt(i)) continue;
+
+            duplicatedCharacterSets.add(i);
+            duplicatedCharacterSets.add(i-1);
+        }
+
+        return duplicatedCharacterSets;
     }
 }
