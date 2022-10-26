131f76415a98e391e6f7c3b987f277ee3ecdc4bb
hodomaroo <enghemdwkd@gmail.com>
2022-10-26 23:05:19 +0900

    problem7의 명세를 수정하고, memberRepository 클래스 구현하였습니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,13 +1,10 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /*
  *  🚀 기능 명세
-    사용자의 정보를 저장할 객체 (이름 / 친구 관계 / 점수)
+    사용자의 정보를 저장할 객체 (이름 / 친구 관계 / 점수) (Member)
     * 이름을 반환하는 메서드 (getName : String)
     * 친구 목록을 반환하는 메서드 (getFriendSet : Set)
     * 친구를 추가할 메서드 (addFriend)
@@ -15,13 +12,38 @@ import java.util.Set;
     * 점수를 반환하는 메서드 (getScore : int)
     * 점수를 변경하는 메서드 (setScore : int)
 
-    추천 친구를 구할 메서드(getRecommnededFriends)
+    멤버 목록을 저장할 객체 (멤버 목록) (MemberRepository)
+    *
+    * 추천 친구를 구할 메서드(getRecommnededFriends)
+    * 해당 이름을 가지는 객체를 반환하는 메서드 (getMember : Member)
+        -> 만약 목록에 그런 이름을 가지는 멤버 객체가 존재하지 않는 경우 새로 생성해서 반환
 
+    * 전체 멤버 목록을 반환하는 메서드 (getAllMembers)
  */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        for(List<String> friendShip : friends){
+
+        }
+
         return answer;
+    }
+}
+
+class MemberRepository{
+    Map<String,Member> memberMap = new HashMap<String,Member>();
+
+    public Member getMember(String name){
+        if(memberMap.get(name) == null){
+            memberMap.put(name, new Member(name));
+        }
+        return memberMap.get(name);
+    }
+
+    public List<Member> getMemberList(){
+        return new ArrayList<Member>(memberMap.values());
     }
 }
 
