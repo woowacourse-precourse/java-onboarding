2072a7c69ac62efa67abab8fb1932e065fb0e827
kevstevie <jianna442@gmail.com>
2022-10-26 17:27:43 +0900

    feat: add checkContinuous

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,10 +6,22 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
         char[] chars = cryptogram.toCharArray();
+        char tmp = ' ';
         Stack<Character> stack = new Stack<>();
         for(char ch : chars){
+            if(!stack.isEmpty()){
+                checkContinuous(stack, ch);
+                continue;
+            }
             stack.push(ch);
         }
+
         return answer;
+    }
+
+    static void checkContinuous(Stack<Character> stack, char ch){
+        if(stack.peek() == ch){
+            stack.pop();
+        }
     }
 }
