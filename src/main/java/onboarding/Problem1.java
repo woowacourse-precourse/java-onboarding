807bd8ad6283aed4d84c78071d99d2b3a3308790
yoondgu <doyou2ing@naver.com>
2022-10-26 19:45:23 +0900

    feat(Problem1): declare features to make

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,46 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+    	// 0, 1, 2, -1은 모두 의미를 가지는 값이므로 로직 상 반환될 수 없는 값으로 초기화한다.
         int answer = Integer.MAX_VALUE;
+
+        // TODO getScore 메소드를 이용해 pobi, crong의 점수를 얻는다.
+        // TODO pobi, crong의 점수를 비교해 반환할 값을 answer에 담는다.
+        
         return answer;
     }
+    
+
+    /**
+     * 주어진 정수를 이루는 각 자리 수의 합을 반환한다.
+     * @param number
+     * @return 각 자리 수의 합
+     */
+    private static int sumNumbersOfDigits(int number) {
+    	int result = 0;
+    	
+    	return result;
+    }
+    
+    /**
+     * 주어진 정수를 이루는 각 자리 수의 곱을 반환한다.
+     * @param number
+     * @return 각 자리 수의 곱
+     */
+    private static int multiplyNumbersOfDigits(int number) {
+    	int result = 1;
+    	
+    	return result;
+    }
+    
+   /**
+    * 주어진 각 페이지로 계산한 값을 비교해 정한 점수를 반환한다.
+    * @param pages
+    * @return
+    */
+   private static int getScore(List<Integer> pages) {
+	   int score = 0;
+	   
+	   return score;
+   }
 }
