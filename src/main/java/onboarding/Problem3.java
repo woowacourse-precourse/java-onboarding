14cc327e9de61828408c8b024a799e26e43d0ddc
Hayoon <hy97@sju.ac.kr>
2022-10-26 22:37:19 +0900

    fix: 입력받은 숫자 369 포함 횟수 카운팅 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,20 +2,23 @@ package onboarding;
 
 public class Problem3 {
 
-    static boolean clappingHands(int number) {
+    static int clappingHands(int number) {
+
+        int count = 0;
+
         while(number != 0) {
             int val = number % 10;
             if (val == 3 || val == 6 || val == 9)
-                return true;
-
+                count++;
             number /= 10;
             }
-        return false;
+
+        return count;
     }
     public static int solution(int number) {
         int answer = 0;
         for(int i = 1; i <= number; i++)
-            if(clappingHands(i)) answer++;
+            answer += clappingHands(i);
 
         return answer;
     }
