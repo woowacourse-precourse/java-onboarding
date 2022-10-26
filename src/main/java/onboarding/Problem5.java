ab5bfd81ffa1625d1e45b21b98e8d77ad3343ffe
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-26 22:18:34 +0900

    add : (Problem5) 제수를 계산하여 결과 도출
    제수의 성질을 이용하여 계산을 하고, 해당 제수들로 결과를 도출한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,37 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        /*
+        * 나누는 금액이 1/2와 1/5 순서대로 줄어들고 있다.
+        * 몫을 리스트에 저장하고, 나머지를 다음 계산을 위한 돈으로 할당한다.
+        * */
+        int divide = 100_000;
+        for(int i=0; i<8; i++){
+            divide = (i%2==0) ? divide/2 : divide/5;
+
+            int[] qar = quotientAndRemainder(money, divide);
+            answer.add(qar[0]);
+            money = qar[1];
+        }
+
+        /*
+        * 1원은 나눌 필요 없이 바로 추가한다.
+        * */
+        answer.add(money);
+
         return answer;
     }
 
+    /*
+    * qar[0] : 몫을 저장한다.
+    * qar[1] : 나머지를 저장한다.
+    * */
     private static int[] quotientAndRemainder(int num, int divide){
         int[] qar = new int[2];
         qar[0] = num/divide;
