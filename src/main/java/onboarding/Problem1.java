859658f1756578433a439403894db7e56f32e551
abbd2 <ajkety00@gmail.com>
2022-10-26 20:32:40 +0900

    기능별로 메소드 틀 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,32 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = -1;
+
         return answer;
+    }
+
+    // 게임에 참여하는 개인의 페이지 리스트를 매개변수로 받으면 각 자릿수를 계산 후 최대값을 리턴하는 메소드
+    public static int pageCal(List<Integer> pageLst){
+        int maxNum = 0;
+
+        int fstPage = 0;
+        int sndPage = 0;
+
+        return maxNum;
+    }
+
+    // 페이지를 매개변수로 받으면 해당 페이지의 자릿수를 모두 더한 값을 리턴
+    public static int pageSum(int page) {
+        int sum = 0;
+
+        return sum;
+    }
+
+    // 페이지를 매개변수로 받으면 해당 페이지의 자릿수를 모두 곱한 값을 리턴
+    public static int pageMul(int page) {
+        int mul = 0;
+
+        return mul;
     }
 }
