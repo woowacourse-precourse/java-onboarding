3f5a4aed82561104543fc44dd5c209c18303c0ab
Kangbin <hz0819@naver.com>
2022-10-26 18:21:14 +0900

    1 Slove

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
 public class Problem2 {
+    public static String Calculator(String Strdata) {
+        String ReturnString = Strdata.substring(0, 1);
+        int Check = 0;
+        for (int i=1; i<Strdata.length();i+=1) {
+            if (ReturnString.charAt(ReturnString.length() - 1) == Strdata.charAt(i)) {
+                Check=1;
+            }
+            else {
+                if (Check==1) {
+                    ReturnString = ReturnString.substring(0, ReturnString.length() - 1);
+                    Check=0;
+                }
+                ReturnString += Strdata.charAt(i);
+            }
+        }
+        if (Check==1) {
+            ReturnString = ReturnString.substring(0, ReturnString.length() - 1);
+        }
+
+        return ReturnString;
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String CompareValue;
+        while (true) {
+            CompareValue = Calculator(cryptogram);
+            if (cryptogram.equals(CompareValue) || CompareValue.isEmpty()) break;
+            cryptogram = CompareValue;
+        }
+        String answer = CompareValue;
         return answer;
     }
+
 }
