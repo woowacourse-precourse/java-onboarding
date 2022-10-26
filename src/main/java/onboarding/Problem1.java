cd6347fa16d537751f456702a1508e51c8a72931
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-10-26 18:01:33 +0900

    Problem1 구현 기능 목록
    기능 요구사항 분석 및 구현 기능 목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        /*
+        조건 0 : 페이지 수의 범위는 1~400 이다.
+        조건 1 : 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수 번호
+        조건 2 : 모든 페이지에는 번호가 적혀있다
+        조건 2-1 :
+        왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+        오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+        위의 과정에서 가장 큰 수를 본인의 점수로 한다.
+        조건 3 : 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return
+         */
+
+        //유효성 확인 메서드 구현
+
+
+        // 최댓값 만들기 메서드 구현
+
+        // 최댓값 비교 구현
+
         return answer;
     }
+
 }
