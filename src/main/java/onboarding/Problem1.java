3cda6e1e59cc36ed94523beb607b6162a3c07010
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-26 17:07:22 +0900

    feat : 점수 계산 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,67 +1,128 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
 
-    private static final int MIN_PAGE = 1;
-    private static final int MAX_PAGE = 400;
+	private static final int MIN_PAGE = 1;
+	private static final int MAX_PAGE = 400;
 
-    private static final int LEFT_PAGE_INDEX = 0;
-    private static final int RIGHT_PAGE_INDEX = 1;
+	private static final int LEFT_PAGE_INDEX = 0;
+	private static final int RIGHT_PAGE_INDEX = 1;
 
-    private static final int EXCEPTION_CASE = -1;
+	private static final int EXCEPTION_CODE = -1;
+	private static final int POBI_WIN_CODE = 1;
+	private static final int CRONG_WIN_CODE = 2;
+	private static final int DRAW_CODE = 0;
 
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
 
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+		if (isValidPages(pobi) && isValidPages(crong)) {
+			int pobiScore = getScore(pobi);
+			int crongScore = getScore(pobi);
 
-        if (isValidPages(pobi) && isValidPages(crong)) {
+			answer = getResultCode(pobiScore, crongScore);
+		} else {
+			answer = EXCEPTION_CODE;
+		}
 
-            answer = 0;
-        } else {
-            answer = EXCEPTION_CASE;
-        }
+		return answer;
+	}
 
-        return answer;
-    }
+	public static boolean isValidPages(List<Integer> pages) {
+		Integer leftPage = pages.get(LEFT_PAGE_INDEX);
+		Integer rightPage = pages.get(RIGHT_PAGE_INDEX);
 
-    private static boolean isValidPages(List<Integer> pages) {
-        Integer leftPage = pages.get(LEFT_PAGE_INDEX);
-        Integer rightPage = pages.get(RIGHT_PAGE_INDEX);
+		if ((isOutOfRange(leftPage) || isOutOfRange(rightPage))) {
+			return false;
+		}
 
-        if ((isOutOfRange(leftPage) || isOutOfRange(rightPage))) {
-            return false;
-        }
+		if (!isOdd(leftPage) || !isEven(rightPage)) {
+			return false;
+		}
 
-        if (!isOdd(leftPage) || !isEven(rightPage)) {
-            return false;
-        }
+		if (!isContinuousNumber(leftPage, rightPage)) {
+			return false;
+		}
 
-        if (!isContinuousNumber(leftPage, rightPage)) {
-            return false;
-        }
+		return true;
+	}
 
-        return true;
-    }
+	public static int getScore(List<Integer> pages) {
+		Integer leftPage = pages.get(LEFT_PAGE_INDEX);
+		Integer rightPage = pages.get(RIGHT_PAGE_INDEX);
 
-    private static boolean isOutOfRange(int page) {
-        if (page < MIN_PAGE || page > MAX_PAGE) {
-            return true;
-        }
+		int maxNumberOfLeftPage = getMaxNumberByPage(leftPage);
+		int maxNumberOfRightPage = getMaxNumberByPage(rightPage);
 
-        return false;
-    }
+		if (maxNumberOfLeftPage > maxNumberOfRightPage) {
+			return maxNumberOfLeftPage;
+		}
 
-    private static boolean isOdd(int page) {
-        return page % 2 == 1;
-    }
+		return maxNumberOfRightPage;
+	}
 
-    private static boolean isEven(int page) {
-        return page % 2 == 0;
-    }
+	private static boolean isOutOfRange(int page) {
+		if (page < MIN_PAGE || page > MAX_PAGE) {
+			return true;
+		}
 
-    private static boolean isContinuousNumber(int a, int b) {
-        return b - a == 1;
-    }
+		return false;
+	}
+
+	private static boolean isOdd(int page) {
+		return page % 2 == 1;
+	}
+
+	private static boolean isEven(int page) {
+		return page % 2 == 0;
+	}
+
+	private static boolean isContinuousNumber(int a, int b) {
+		return b - a == 1;
+	}
+
+	private static int getMaxNumberByPage(int page) {
+		List<Integer> pageEach = separateInteger(page);
+
+		int sum = getSum(pageEach);
+		int multiples = getMultiples(pageEach);
+
+		if (sum > multiples) {
+			return sum;
+		}
+
+		return multiples;
+	}
+
+	private static List<Integer> separateInteger(int target) {
+		List<Integer> separated = new ArrayList<>();
+
+		while (target != 0) {
+			int singleNumber = target % 10;
+			separated.add(singleNumber);
+
+			target /= 10;
+		}
+
+		return separated;
+	}
+
+	private static int getSum(List<Integer> integers) {
+		return integers.stream()
+			.mapToInt(i -> i)
+			.sum();
+	}
+
+	private static int getMultiples(List<Integer> integers) {
+		int result = 1;
+		for (Integer integer : integers) {
+			result *= integer;
+		}
+
+		return result;
+	}
+
 }
