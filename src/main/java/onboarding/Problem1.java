35494bdc1f362ce548d576b64155c2ff823e7551
HiiWee <wpdlzhf159@gmail.com>
2022-10-26 21:44:01 +0900

    Feat problem1: 필요 상수들 정의

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static final int FIRST_PAGE = 1;
+    static final int LAST_PAGE = 400;
+    static final int LEFT_PAGE = 0;
+    static final int RIGHT_PAGE = 1;
+
+    static final int EXCEPTION = -1;
+    static final int WIN_POBI = 1;
+    static final int WIN_CRONG = 2;
+    static final int DRAW = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
