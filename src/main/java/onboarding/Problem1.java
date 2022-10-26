d0e4478fd65a3a56b62e084f0c15b10841957aeb
youngkyunIm <yee0230@naver.com>
2022-10-26 21:50:50 +0900

    feat(Problem1) : add sum 숫자 각 자릿값끼리 더하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,7 +6,7 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         if (!checkPage(pobi) || !checkPage(crong)) {
-            return -1;
+            answer = -1;
         }
         return answer;
     }
@@ -15,5 +15,15 @@ class Problem1 {
         if (list.size() != 2) return false;
         if (Math.abs(list.get(1) - list.get(0)) != 1) return false;
         return true;
+    }
+
+    public static int getSum(int value){
+        int sum = 0;
+        while (value > 0) {
+            int n = value % 10;
+            sum += n;
+            value /= 10;
+        }
+        return sum;
     }
 }
