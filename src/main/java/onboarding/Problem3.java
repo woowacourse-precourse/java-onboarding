d2aa4678dba912969919e6d4e22033b51f92aa33
mckim123 <minchan06@gmail.com>
2022-10-27 03:37:39 +0900

    문제 3 기능 목록 구성 및 기능 1 구현(문제 3 완료)
    문제 3의 기능 목록을 작성하고 기능을 모두 구현하였습니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int exponent = 0;
+        int left = 0;
+        while (number > 2){
+            int remainder = number % 10;
+            number /= 10;
+            answer += (number * 3) * Math.pow(10, exponent);
+            if (remainder >= 3){
+                answer += (remainder - 1)/3 * Math.pow(10, exponent);
+                if (remainder % 3 == 0){
+                    answer += left + 1;
+                }
+            }
+            left += remainder * Math.pow(10, exponent);
+            exponent += 1;
+        }
         return answer;
     }
 }
