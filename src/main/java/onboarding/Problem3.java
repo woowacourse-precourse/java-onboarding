4226c316f6ef5877212d61c78cb6c7b7c7f1d443
2jun0 <soo28819@gmail.com>
2022-10-27 03:40:38 +0900

    Feat: 문제 3번 - 3,6,9의 개수를 구하는 메서드 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int get369Count(int number) {
+        int cnt = 0;
+
+        while (number > 0) {
+            int currDigit = number % 10;
+            if (currDigit % 3 == 0) cnt++;
+            number /= 10;
+        }
+
+        return cnt;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
