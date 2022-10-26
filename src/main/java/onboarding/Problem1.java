bcaeb8061132ca529dcdc38e877ed274ab3e2e2f
hyun-seung <ardimento22@naver.com>
2022-10-26 21:43:03 +0900

    [FEAT] 올바른 범위인지 확인한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,31 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static final int FIRST_PAGE = 1;
+    public static final int LAST_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(!verificationCheck(pobi) || !verificationCheck(crong)) {
+            return -1;
+        }
+        
         return answer;
+    }
+
+    public static boolean verificationCheck(List<Integer> checkList) {
+        for (Integer check : checkList) {
+            if (check < FIRST_PAGE || check > LAST_PAGE) {
+                return false;
+            }
+        }
+
+        if (checkList.get(0) + 1 != checkList.get(1)) {
+            return false;
+        }
+
+        return true;
     }
 }
