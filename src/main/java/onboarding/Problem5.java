2643b848889bc48018f0f746ecbc499648d93405
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-27 00:28:01 +0900

    feat : 화폐단위 정보 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,33 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        Map<Integer, Integer> moneyUnitMap = getMoneyUnitMap();
+
+
         return answer;
+    }
+
+    private static Map<Integer, Integer> getMoneyUnitMap() {
+        Map<Integer, Integer> moneyUnitMap = new LinkedHashMap<>();
+
+        moneyUnitMap.put(50000, 0);
+        moneyUnitMap.put(10000, 0);
+        moneyUnitMap.put(5000, 0);
+        moneyUnitMap.put(1000, 0);
+        moneyUnitMap.put(500, 0);
+        moneyUnitMap.put(100, 0);
+        moneyUnitMap.put(10, 0);
+        moneyUnitMap.put(1, 0);
+
+        return moneyUnitMap;
     }
 }
