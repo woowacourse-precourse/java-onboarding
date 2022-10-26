7e6423db0c886e6276326933ee5306425546ac04
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 19:57:48 +0900

    feat(Problem3.java): count369() 숫자에서 3, 6, 9의 갯수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem3 {
+    public static int count369(int n){
+        return (int)String.valueOf(n).chars()
+                .filter(o -> o==3 || o==6 || o==9)
+                .count();
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
