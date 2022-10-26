cc59299071512eaa928cfe37cea696131b9b5b90
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-26 17:51:52 +0900

    feat : 문자열 중복 판단 및 지우는 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char letter = cryptogram.charAt(i);
+            if (stack.empty()) {
+                stack.add(letter);
+                continue;
+            }
+
+            boolean isDuplicated = false;
+            Character lastLetter = stack.peek();
+            while (cryptogram.charAt(i) == lastLetter) {
+                isDuplicated = true;
+                i += 1;
+            }
+
+            if (isDuplicated) {
+                stack.pop();
+                i -= 1;
+            } else {
+                stack.add(cryptogram.charAt(i));
+            }
+        }
+
+
         return answer;
     }
 }
