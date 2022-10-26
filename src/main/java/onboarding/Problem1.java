6a4d661000e0d19d5be81190889690e31bf8a64c
swhyeon98 <smreo75@gmail.com>
2022-10-26 20:44:14 +0900

    구현 목록 변경 및 일부 기능 구현 - README에 작성한 Problem1 기능 구현 목록을 PROBLEM1.md로 이동 - 페이지 번호의 각 자리 합, 곱을 구한 후 더 큰 수를 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+
         return answer;
+    }
+
+    private static int maxPage(int page){
+        int sum = 0;
+        int mul = 1;
+        while (page>0){
+            sum += page%10;
+            mul *= page%10;
+            page /= 10;
+        }
+        return Math.max(sum,mul);
     }
 }
