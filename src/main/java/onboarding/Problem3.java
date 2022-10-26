484c0fdb0d060dad3a619351fbefbf1a0986f079
jinny-l <jinny.l.dev@gmail.com>
2022-10-27 02:00:55 +0900

    [refactor] notContain_3_6_9() 메소드 구현 코드 간결화

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
@@ -8,11 +9,9 @@ public class Problem3 {
 
 
 
-
-    // 입력받은 문자열에 "3" "6" '9"가 포함되어 있는지 확인하는 메소드
-    private static boolean notContain_3_6_9(char c) {
-
-        String num = Character.toString(c);
-        return num.equals("3") || num.equals("6") || num.equals("9");
+    // 입력받은 문자에 '3' '6' '9'가 포함되어 있는지 확인하는 메소드
+    private static boolean Contain_3_6_9(char c) {
+        return (c == '3' || c == '6' || c == '9');
     }
+
 }
