dfeabe94eac3ce826c797a6996493e6fcc2b724f
JeonghoPark <pjhpjh0805@naver.com>
2022-10-27 06:58:22 +0900

    feat: 4번 대문자 소문자 정보가 담긴 char 배열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem4 {
+
+    static char[] upperCaseList = new char[26];
+    static char[] lowerCaseList = new char[26];
     public static String solution(String word) {
         String answer = "";
+        for (int i=0; i<26; i++) {
+            upperCaseList[i] = (char)(i+65);
+            lowerCaseList[i] = (char)(i+97);
+        }
         return answer;
     }
 }
