9777dc11af55a16a176eb6afe206fc0b16d51d7f
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-26 17:27:53 +0900

    Feat: 구현 Problem1 함수 sumEachInt - 각 자릿수를 더해서 반환 하는 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,10 +10,19 @@ class Problem1 {
 		return value2;
 	}
 
+	private static int sumEachInt(int value) {
+		int sumInt = 0;
+		while (value > 0) {
+			sumInt += value % 10;
+			value /= 10;
+		}
+		return sumInt;
+	}
+
 	private static int bigPageResult(List<Integer> person) {
 		int leftPage = person.get(0);
 		int rightPage = person.get(1);
-		
+
 	}
 
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
