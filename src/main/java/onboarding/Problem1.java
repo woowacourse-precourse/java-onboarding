af936a09ec17c579b094d7496df4d940acec9cc1
박준용 <jo101044@naver.com>
2022-10-26 19:58:51 +0900

    문제1 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,52 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        //왼쪽 페이지가 짝수일 경우 -1 리턴
+        if(pobi.get(0)%2 != 1) return -1;
+        if(crong.get(0)%2 != 1) return -1;
+
+        //오른쪽 페이지가 홀수일 경우 -1 리턴
+        if(pobi.get(1)%2 != 0) return -1;
+        if(crong.get(1)%2 != 0) return -1;
+
+        //페이지 사이가 비어있을 경우 -1 리턴
+        if(pobi.get(0)+1 != pobi.get(1)) return -1;
+        if(crong.get(0)+1 != crong.get(1)) return -1;
+
+        int pobiScore = 0;
+        int crongScore = 0;
+
+        for (int i = 0; i < pobi.size(); i++) {
+            //pobi의 점수 구하기
+            String pobiPage = String.valueOf(pobi.get(i));
+            pobiScore = Math.max(pobiScore,getMaxNum(pobiPage));
+
+            //crong의 점수 구하기
+            String crongPage = String.valueOf(crong.get(i));
+            crongScore = Math.max(crongScore,getMaxNum(crongPage));
+        }
+
+        //pobi의 점수가 큰 경우 리턴 1
+        if(pobiScore>crongScore) return 1;
+
+        //crong의 점수가 큰 경우 리턴 2
+        if(crongScore>pobiScore) return 2;
+
+        //같을 경우 리턴 0
+        return 0;
     }
+
+    // 더하기와 곱하기 중 큰 값을 출력하는 메소드
+    private static int getMaxNum(String page) {
+        int plusNum = page.charAt(0)-'0';
+        int multipleNum = page.charAt(0)-'0';
+
+        for (int i = 1; i < page.length(); i++) {
+            plusNum += page.charAt(i) - '0';
+            multipleNum *= page.charAt(i) - '0';
+        }
+
+        return Math.max(plusNum, multipleNum);
+    }
+
 }
