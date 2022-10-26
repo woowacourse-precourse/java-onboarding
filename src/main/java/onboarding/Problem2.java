f8d3b608b4f797cae1dae169a257b751a2540311
dongwoo <shp05179@gmail.com>
2022-10-26 23:37:36 +0900

    연속된 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (true) {
+            int wordLength = cryptogram.length();
+            char now = cryptogram.charAt(0);
+            String Aftercryptogram = String.valueOf(now);
+            int stack = 0;
+            for (int i = 1; i<wordLength; i++) {
+                if (now != cryptogram.charAt(i)) {
+                    Aftercryptogram += cryptogram.charAt(i);
+                    now = cryptogram.charAt(i);
+                    stack = 0;
+                } else {
+                    if (stack==0) {
+                        Aftercryptogram = Aftercryptogram.substring(0, Aftercryptogram.length() - 1);
+                        stack+=1;
+                    }
+                }
+            }
+            System.out.println(Aftercryptogram);
+            if (Aftercryptogram.equals(cryptogram)) break;
+            cryptogram = Aftercryptogram;
+            if (cryptogram.length()==0) break;
+        }
+        return cryptogram;
     }
 }
