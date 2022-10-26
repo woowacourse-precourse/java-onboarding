bb76ee8d474a8bd2328e620d0f543e0b2d08a482
ywj9811 <ywj9811@naver.com>
2022-10-27 04:26:01 +0900

    자체 테스트케이스 제거

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -50,13 +50,6 @@ class ApplicationTest {
             String result = "";
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
         }
-
-        @Test
-        void case3() {
-            String cryptogram = "aabbccde";
-            String result = "de";
-            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
-        }
     }
 
     @Nested
