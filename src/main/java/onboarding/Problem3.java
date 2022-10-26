aeb4c67021a96500d592a73aa95a3eeb603fd4a8
jkde7721 <jkde7721@naver.com>
2022-10-26 20:29:37 +0900

    [문제 3] 기능 요구 사항 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return getTotalClapCount(number);
+    }
+
+    private static int getTotalClapCount(int number) {
+        int totalClapCount = 0;
+        for (int i = 1; i <= number; i++) {
+            totalClapCount += getClapCount(i);
+        }
+        return totalClapCount;
+    }
+
+    private static int getClapCount(int number) {
+        int clapCount = 0;
+        while (number > 0) {
+            if (isMultipleOfThree(number % 10)) clapCount++;
+            number /= 10;
+        }
+        return clapCount;
+    }
+
+    private static boolean isMultipleOfThree(int digit) {
+        return digit != 0 && digit % 3 == 0;
     }
 }
