2fe4870feec512f916d4997f564948651ae4dc9a
AMVVLA <blackcube222@gmail.com>
2022-10-26 22:50:07 +0900

    구현해야 할 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,21 @@ package onboarding;
 
 import java.util.List;
 
+/*
+* 기능목록
+*
+* 1. 에러 처리 기능
+* 2. 각 자리 숫자를 구하는 기능
+* 3. 페이지 각 자리수 더해서 반환하는 기능
+* 4. 페이지 각 자리수 곱해서 반환하는 기능
+* 5. 3번과 4번 반환값에서 최댓값을 판별하는 기능
+* 5. pobi와 crong의 최댓값을 비교하여 승자 판별하는 기능
+*
+* */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+
     }
 }
