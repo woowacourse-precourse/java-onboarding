bbeeca9d42947edfd09d30f9393424cacf320c43
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-27 00:12:16 +0900

    feat : 변환하여 출력

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,12 +5,17 @@ import java.util.Map;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
 
         Map<Character, Character> treeFrogDictionary = getTreeFrogDictionary();
 
+        char[] chars = word.toCharArray();
 
-        return answer;
+        StringBuilder answerString = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            answerString.append(treeFrogDictionary.getOrDefault(chars[i], chars[i]));
+        }
+
+        return answerString.toString();
     }
 
     private static Map<Character, Character> getTreeFrogDictionary() {
