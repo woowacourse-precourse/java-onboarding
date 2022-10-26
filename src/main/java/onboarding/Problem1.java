56584ba00c5beb541c66bc1ea37fb2e8d4b394a5
JHS <wnghdtjr129@naver.com>
2022-10-26 21:32:26 +0900

    feat: 입력 페이지 값에서 최대값 구하기 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,10 +12,44 @@ class Problem1 {
         if (validationPages(pobi) || validationPages(crong)) {
             return -1;
         }
+        int pobiMaxValue = getMaxValue(pobi);
+        int crongMaxValue = getMaxValue(crong);
+
+        
         return 0;
     }
 
     private static boolean validationPages(List<Integer> pages) {
         return (pages.get(0) + 1 != pages.get(1)) || pages.get(0) % 2 != 1 || pages.get(1) % 2 != 0;
     }
+
+    private static int getMaxValue(List<Integer> pages) {
+        int max = 0;
+        for (int page : pages) {
+            String[] p = String.valueOf(page).split("");
+            max = Math.max(max, getMaxBetweenSumAndMul(p));
+        }
+        return max;
+    }
+
+    private static int getMaxBetweenSumAndMul(String[] page) {
+        return Math.max(getSum(page), getMul(page));
+    }
+
+    private static int getSum(String[] page) {
+        int sum = 0;
+        for (String s : page) {
+            sum += Integer.parseInt(s);
+        }
+        return sum;
+    }
+
+    private static int getMul(String[] page) {
+        int mul = 1;
+        for (String s : page) {
+            mul *= Integer.parseInt(s);
+        }
+        return mul;
+    }
+
 }
