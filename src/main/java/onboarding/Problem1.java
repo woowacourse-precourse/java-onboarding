8950e51b637d3321de9c2306ffd28b911f79983e
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-26 16:51:52 +0900

    MAKE Function List

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+            int answer = Integer.MAX_VALUE;
+            return answer;
+    }
+
+    private static boolean check(List<Integer> list) {
+
+    }
+
+    private static int calcalateMax(List<Integer> list) {
+    }
+    private static int numSum(int i){
+
+    }
+    private static int numMulti(int i){
+
     }
 }
---
+++ src/test/java/onboarding/Problem1Test.java
@@ -0,0 +1,10 @@
+package onboarding;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem1Test {
+    public static void main(String[] args) {
+        Problem1 pro = new Problem1();
+
+    }
+}
