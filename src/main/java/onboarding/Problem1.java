9fac7b1a4691ba3645d320e06a5e9e77e8cde8b6
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 16:57:01 +0900

    feat(Problem1.java): validPages() 유효한 페이지인지 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean validPages(List<Integer> pages){
+        int left = pages.get(0);
+        int right = pages.get(0);
+
+        if(left<1 || 400<left || right<1 || 400<right)
+            return false;
+
+        if(right!=left+1)
+            return false;
+
+        if(left==1 || right==400)
+            return false;
+
+        if(left%2==0 || right%2==1)
+            return false;
+
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
