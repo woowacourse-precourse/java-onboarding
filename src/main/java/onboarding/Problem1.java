f6e1339c1e37db35699cec162bb3f6e42986aab5
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-26 20:46:43 +0900

    feat : 제한 사항 관련 -1을 리턴하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,27 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+
+    private static boolean pageSequence;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        int pobiSize = pobi.size();
+        int crongSize = crong.size();
+
+        int pobiFirstNumber = pobi.get(0);
+        int pobiSecondNumber = pobi.get(1);
+
+        if (pobiSize !=2 || crongSize !=2){
+            return -1;
+        } else if (pobiFirstNumber + 1 != pobiSecondNumber){
+            pageSequence = true;
+            return -1;
+        } else{
         int answer = Integer.MAX_VALUE;
         return answer;
+        }
     }
+
 }
