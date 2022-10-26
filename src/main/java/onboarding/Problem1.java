e9f7daa3c36d9d49039056234790b30053dc9f83
mckim123 <minchan06@gmail.com>
2022-10-27 02:49:36 +0900

    문제 1 기능 목록 구성 및 기능 1 구현
    문제 1의 기능 목록을 구성하여 PROBLEM1.md에 작성하고, 기능 1을 구현하였습니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobi_score = max(pobi);
+        int crong_score = max(crong);
+
+        if (pobi_score == -1 || crong_score == -1) {
+            return -1;
+           }
     }
+    static int max(List<Integer> pages) {
+        int page1 = pages.get(0).intValue();
+        int page2 = pages.get(1).intValue();
+        if (page1 % 2 == 0 || page1 < 3 || page1 > 397 || page1 + 1 != page2) {
+            return -1;
+            }
+        }
 }
