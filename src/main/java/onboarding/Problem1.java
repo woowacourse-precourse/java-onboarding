a51a8e6447d2f26df78ce1d1a0a3d9f734959d23
d0ri123 <lsh81550@gmail.com>
2022-10-26 17:43:11 +0900

    Exception case를 판단하는 기능 정의

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,42 +8,42 @@ class Problem1 {
         return (b-a)==1 && (a %= 2) == 1 && (b %= 2) == 0;
     }
 
-    public static int help(int a, int b){ //a는 왼쪽 페이지, b는 오른쪽 페이지
-        int[] arr1 = Stream.of(String.valueOf(a).split("")).mapToInt(Integer::parseInt).toArray();
-        int[] arr2 = Stream.of(String.valueOf(b).split("")).mapToInt(Integer::parseInt).toArray();
-
-        int result_left = getResult(arr1);
-        int result_right = getResult(arr2);
-
-        return Math.max(result_left, result_right);
-    }
-
-    private static int getResult(int[] arr) {
-        int res1 = 0, res2 = 1;
-        for(int num: arr){
-            res1 += num;
-            res2 *= num;
-        }
-        return Math.max(res1, res2);
-    }
-
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        Integer[] pobi_arr = pobi.toArray(new Integer[2]);
-        Integer[] crong_arr = crong.toArray(new Integer[2]);
-
-        Integer left_p = pobi_arr[0];
-        Integer right_p = pobi_arr[1];
-
-        Integer left_c = crong_arr[0];
-        Integer right_c = crong_arr[1];
-
-        if(!(isEx(left_p, right_p) && isEx(left_c, right_c))) return -1;
-
-        int max_pobi = help(left_p, right_p);
-        int max_crong = help(left_c, right_c);
-
-        if(max_pobi == max_crong) return 0;
-        else if(max_pobi > max_crong) return 1;
-        else return 2;
-    }
+//    public static int help(int a, int b){
+//        int[] arr1 = Stream.of(String.valueOf(a).split("")).mapToInt(Integer::parseInt).toArray();
+//        int[] arr2 = Stream.of(String.valueOf(b).split("")).mapToInt(Integer::parseInt).toArray();
+//
+//        int result_left = getResult(arr1);
+//        int result_right = getResult(arr2);
+//
+//        return Math.max(result_left, result_right);
+//    }
+//
+//    private static int getResult(int[] arr) {
+//        int res1 = 0, res2 = 1;
+//        for(int num: arr){
+//            res1 += num;
+//            res2 *= num;
+//        }
+//        return Math.max(res1, res2);
+//    }
+//
+//    public static int solution(List<Integer> pobi, List<Integer> crong) {
+//        Integer[] pobi_arr = pobi.toArray(new Integer[2]);
+//        Integer[] crong_arr = crong.toArray(new Integer[2]);
+//
+//        Integer left_p = pobi_arr[0];
+//        Integer right_p = pobi_arr[1];
+//
+//        Integer left_c = crong_arr[0];
+//        Integer right_c = crong_arr[1];
+//
+//        if(!(isEx(left_p, right_p) && isEx(left_c, right_c))) return -1;
+//
+//        int max_pobi = help(left_p, right_p);
+//        int max_crong = help(left_c, right_c);
+//
+//        if(max_pobi == max_crong) return 0;
+//        else if(max_pobi > max_crong) return 1;
+//        else return 2;
+//    }
 }
