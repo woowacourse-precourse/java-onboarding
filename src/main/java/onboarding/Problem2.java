7ab63c72e8779fb6859df6e9e7e1ffa4a1b2049f
Jeeseob <jeeseob5761@gmail.com>
2022-10-27 08:44:57 +0900

    Requirements-list: problem2 - 기능 목록 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,39 @@
 package onboarding;
 
 public class Problem2 {
+    /**
+     * @Problem: 연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 메서드를 완성하라.
+     * @Example
+     * "browoanoommnaon"
+     * "browoannaon"
+     * "browoaaon"
+     * "browoon"
+     * "brown"
+     */
+
+
+    /**
+     * @Method: deleteContinuousCharacter
+     * 문자열을 입력받아 중복된 문제를 삭제한다.
+     * TODO: 문자열에서 앞뒤 값이 같은 index의 경우, return할 문자열에 추가하지 않는다.
+     */
+
+
+    /**
+     * @Method: decodeCryptogram
+     * deleteContinuousCharacter를 반복하여, 입려받은 cryptogram을 복호화 한다.
+     * TODO: 입력한 문자열과, return된 문자열의 길이를 비교하여, 반복 여불을 결정한다.
+     * TODO: 중복되는 문자열이 없을 때 까지 반복한다.
+     */
+
+
+    /**
+     * 5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+     * @Method: Solution
+     * 임의의 문자열 cryptogram이 매개변수로 주어질 때, 연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 메서드를 완성하라.
+     * TODO: 복호화된 문자열을 return한다.
+     */
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
