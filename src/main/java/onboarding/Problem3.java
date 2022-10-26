fc5b5a3c70710948b8a3d844a449c49093390378
ym2629 <dydals2629@naver.com>
2022-10-26 14:25:19 +0900

    problem3 try

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return solve(number);
+    }
+    
+    static int solve(int num) {
+    	int res = 0;
+    	for(int i = 0; i <= num; i++) {
+    		char[] ch = Integer.toString(i).toCharArray();
+    	
+    		for(int j = 0; j < ch.length; i++) {
+    			if(ch[j]=='3'||ch[j]=='6'||ch[j]=='9') {
+    				res+=1;
+    			}
+    		}
+    	}
+    	return res;
     }
 }
