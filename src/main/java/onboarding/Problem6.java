bac0a0b52fea2e40b4ad442a0e1a4785235f766a
main <jang0929@naver.com>
2022-10-26 20:47:21 +0900

    Problem6 두 자씩 map에 저정하는 함수 구현 및 중복된 이름일 경우 set에 저장하는 함수 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -41,13 +41,33 @@ public class Problem6 {
 
     /**
      * 2. 중복된 이름일 경우 set에 저장하는 함수
-     * idx1, idx2를 answer set에 저장하는 함수
+     * idx 를 answer set에 저장하는 함수
      */
-    static void addAnswer(int idx1, int idx2){
-        answerSet.add(idx1);
-        answerSet.add(idx2);
+    static void addAnswer(int idx){
+        answerSet.add(idx);
 
     }
+
+    /**
+     * 3. 두 자씩 map에 저장하는 함수
+     * @param s 유저의 닉네임
+     * @param idx 해당 유저의 인덱스
+     */
+    static void calc(String s, Integer idx) {
+        for (int i = 0; i + 1 < s.length(); i++) {
+            if (nicknameMap.containsKey(s.substring(i, i + 2))) {
+                if (!Objects.equals(nicknameMap.get(s.substring(i, i + 2)), idx)) {
+                    addAnswer(idx);
+                    addAnswer(nicknameMap.get(s.substring(i,i+2)));
+                    answerSet.add(idx);
+                    answerSet.add(nicknameMap.get(s.substring(i, i + 2)));
+                }
+            } else {
+                nicknameMap.put(s.substring(i, i + 2), idx);
+            }
+        }
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
