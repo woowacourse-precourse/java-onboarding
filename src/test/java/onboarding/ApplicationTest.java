14f8213d5aad240cbc0cf9d6563bdbc9cb45a019
ywj9811 <ywj9811@naver.com>
2022-10-27 04:25:15 +0900

    cryptogram으로 들어온 값들을 나누어서 배열에 담고 순서대로 현재값과 다음값을 비교해서 같으면 잘라 내도록 작업하도록 하자 원하는 데이터를 입력하면 duplicated 함수를 재귀식으로 동작하게 되면 return 을 타고 쭉 들어가서 마지막에 연속된 중복값이 나오지 않는 순간 해당 값을 return 하게 된다.

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -50,6 +50,13 @@ class ApplicationTest {
             String result = "";
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
         }
+
+        @Test
+        void case3() {
+            String cryptogram = "aabbccde";
+            String result = "de";
+            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
     }
 
     @Nested
