ca58b6ccd36ddc2b34f1ace40b3336b757f6b954
JunH <tjdtls690@gmail.com>
2022-10-26 19:19:40 +0900

    :sparkles: feat : 1부터 입력된 숫자까지 증가하면서 3,6,9 개수 더하기 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,17 +1,23 @@
 package onboarding;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return IntStream.rangeClosed(1, number)
+                .map(Problem3::countOfThreeSixNine)
+                .reduce(0, Integer::sum);
     }
     
     private static int countOfThreeSixNine(int number) {
-        return (int) Arrays.stream(String.valueOf(number).split(""))
+        return (int) Arrays.stream(split(number))
                 .mapToInt(Integer::parseInt)
-                .filter(eachDigit -> eachDigit % 3 == 0)
+                .filter(eachDigit -> eachDigit != 0 && eachDigit % 3 == 0)
                 .count();
+    }
+    
+    private static String[] split(final int number) {
+        return String.valueOf(number).split("");
     }
 }
