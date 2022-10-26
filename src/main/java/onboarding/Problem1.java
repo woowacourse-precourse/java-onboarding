4b7afea78c13c6164abae28ced785dfe951daf70
apptie <appti@naver.com>
2022-10-26 20:42:26 +0900

    feat: 문제 1 페이지 범위를 검증하는 validatePageRange 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean validatePageRange(Integer pageNumber) {
+        if (!(3 <= pageNumber && pageNumber <= 398)) {
+            return false;
+        }
+        return true;
     }
 }
