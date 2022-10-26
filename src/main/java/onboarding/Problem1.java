76d692ed904cbfaf246ff7d3ab39f76a91c02579
01192mg <01192mg@gmail.com>
2022-10-26 16:54:20 +0900

    feat: Problem1 검증 기능 구현
    입력된 페이지가 올바른지 확인하기 위해 validatePages 메소드로 검증을 함
    검증 시 올바르지 않은 페이지가 입력됐을 경우 IllegalArgumentException 예외를 던진다.
    요구사항에서 예외 사항이 발생하면 -1을 리턴해야 하므로 예외를 잡아서 -1로 리턴해준다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,27 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        try {
+            validatePages(pobi);
+            validatePages(crong);
+        } catch (IllegalArgumentException e) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static void validatePages(List<Integer> pages) {
+        if (pages.size() != 2) {
+            throw new IllegalArgumentException("책 페이지는 두 쪽이 입력되어야 합니다.");
+        }
+
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        if (leftPage < 1 || leftPage > 400 || leftPage % 2 == 0 || rightPage < 1 || rightPage > 400 || rightPage % 2 == 1) {
+            throw new IllegalArgumentException("올바른 페이지 번호가 아닙니다.");
+        }
+        if (rightPage - leftPage != 1) {
+            throw new IllegalArgumentException("연속된 페이지 번호를 입력해주세요.");
+        }
     }
 }
