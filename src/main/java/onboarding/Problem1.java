637d872fe6a86db5587bcf55d613f34feeb9dc51
kkunh97@naver.com <kkunh97@naver.com>
2022-10-26 20:05:33 +0900

    1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,18 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Scanner;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요.");
+        number = scan.nextInt();
+        for (int i = 1; i < number; i++) {
+            String str1 = String.valueOf(i);
+            if (str1.contains("3")) {
+                answer++;
+            } else if (str1.contains("6")) {
+                answer++;
+            } else if (str1.contains("9")) {
+                answer++;
+            }
+        }
         return answer;
     }
 }
