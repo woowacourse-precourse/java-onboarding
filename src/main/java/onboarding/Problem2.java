4f19e40335a22c73f85e402e6e01df872e87340f
TaemHam <tk4003950@gmail.com>
2022-10-27 00:13:23 +0900

    Feat: Implement solution

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
+    private static boolean isValidInput(String cryptogram) {
+        return false;
+    }
+
+    private static String decrypt(String cryptogram) {
+        return "";
+    }
+
+    /*
+     * Problem2의 솔루션 코드입니다.
+     *
+     * @param 해독에 사용될 문자열 입니다.
+     * @return 중복글자가 제거된 문자열입니다.
+     * */
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        if (isValidInput(cryptogram) == false) {
+            return "Error: Wrong input";
+        }
+
+        String answer = decrypt(cryptogram);
         return answer;
     }
 }
