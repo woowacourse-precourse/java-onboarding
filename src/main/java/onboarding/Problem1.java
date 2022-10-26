660318ba2169a2ef7f72d481444e95bf3a5c8ec6
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 18:49:43 +0900

    solution 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,27 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+        int pobiNumber = -1, crongNumber = -1;
+
+        //페이지 오류
+        if(pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)){
+            return -1;
+        }
+
+        pobiNumber = Math.max(getNumberMax(pobi.get(0)), getNumberMax(pobi.get(1)));
+        crongNumber = Math.max(getNumberMax(crong.get(0)), getNumberMax(crong.get(1)));
+
+        if(pobiNumber > crongNumber){
+            answer = 1;
+        }
+        else if(pobiNumber < crongNumber){
+            answer = 2;
+        }
+        else if(pobiNumber == crongNumber){
+            answer = 0;
+        }
+
         return answer;
     }
 
