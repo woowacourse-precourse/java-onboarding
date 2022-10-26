07fca3cd992815b57bcbc2561996eb5789e6d208
jaehee329 <jaehee329@yonsei.ac.kr>
2022-10-26 19:52:13 +0900

    feat: 펼친 페이지 배열에서 최대 점수 계산 로직 추가
    리스트에서 최대 점수를 계산하는 getScore()메서드 추가
    getMaxDigitSum() 메서드와 getMaxDigitProduct() 메서드의 결과를 비교해 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,61 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        checkException(pobi);
+        checkException(crong);
+        answer = getWinner(pobi, crong);
         return answer;
+    }
+
+    public static void checkException(List<Integer> pageList) {
+    }
+
+    public static int getWinner(List<Integer> pobi, List<Integer> crong) {
+        int pobiScore = getScore(pobi);
+        int crongScore = getScore(crong);
+
+        if (pobiScore == crongScore) {
+            return 0;
+        } else if (pobiScore > crongScore) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public static int getScore(List<Integer> pageList) {
+        int add = getMaxDigitSum(pageList);
+        int mult = getMaxDigitProduct(pageList);
+        return Math.max(add, mult);
+    }
+
+    public static int getMaxDigitSum(List<Integer> pageList) {
+        int leftDigitSum = getDigitSum(pageList.get(0));
+        int rightDigitSum = getDigitSum(pageList.get(1));
+        return Math.max(leftDigitSum, rightDigitSum);
+    }
+
+    public static int getDigitSum(int page) {
+        int digitSum = 0;
+        while (page >= 1) {
+            digitSum += page % 10;
+            page /= 10;
+        }
+        return digitSum;
+    }
+
+    public static int getMaxDigitProduct(List<Integer> pageList) {
+        int leftDigitProduct = getDigitProduct(pageList.get(0));
+        int rightDigitProduct = getDigitProduct(pageList.get(1));
+        return Math.max(leftDigitProduct, rightDigitProduct);
+    }
+
+    public static int getDigitProduct(int page) {
+        int digitProduct = 1;
+        while (page >= 1) {
+            digitProduct *= page % 10;
+            page /= 10;
+        }
+        return digitProduct;
     }
 }
---
+++ src/test/java/onboarding/Problem1Test.java
@@ -0,0 +1,72 @@
+package onboarding;
+
+import static onboarding.Problem1.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.*;
+
+class Problem1Test {
+
+	@Nested
+	class getMaxDigitTest {
+
+		@Test
+		@DisplayName("정수의 각 자리 수 합을 반환해야 한다")
+		public void getMaxDigitSumTest() {
+			// given
+			List<Integer> pageList = new ArrayList<>();
+			pageList.add(123);
+			pageList.add(124);
+
+			// when
+			int maxDigitSum = getMaxDigitSum(pageList);
+
+			// then
+			assertThat(maxDigitSum).isEqualTo(1+2+4);
+		}
+
+		@Test
+		@DisplayName("정수의 각 자리 수 곱을 반환해야 한다")
+		public void getMaxDigitProductTest() {
+			// given
+			List<Integer> pageList = new ArrayList<>();
+			pageList.add(129);
+			pageList.add(130);
+
+			// when
+			int maxDigitProduct = getMaxDigitProduct(pageList);
+
+			// then
+			assertThat(maxDigitProduct).isEqualTo(1*2*9);
+		}
+	}
+
+	@Nested
+	class getScoreTest {
+
+		@Test
+		@DisplayName("곱한 값이 더 큰 경우 곱한 값을 반환해야 한다")
+		public void getScoreTest1() {
+			List<Integer> pageList = new ArrayList<>();
+			pageList.add(357);
+			pageList.add(358);
+
+			int score = getScore(pageList);
+			assertThat(score).isEqualTo(120);
+		}
+		@Test
+		@DisplayName("더한 값이 더 큰 경우 더한 값을 반환해야 한다")
+		public void getScoreTest2() {
+			List<Integer> pageList = new ArrayList<>();
+			pageList.add(19);
+			pageList.add(20);
+
+			int score = getScore(pageList);
+			assertThat(score).isEqualTo(10);
+		}
+	}
+}
