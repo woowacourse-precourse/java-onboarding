4d7eb7334436f5b1301b095c01f193a0005dde05
JH-TT <leejongho9803@gmail.com>
2022-10-27 02:31:01 +0900

    3번 : 현재 숫자에 3,6,9가 총 몇개 들어있는지 확인하는 메서드 구현.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+
+    // 3, 6, 9가 몇개 들어있는지
+    static int countThreeSixNine(int num) {
+        int cnt = 0;
+        char[] eachDigit = String.valueOf(num).toCharArray();
+        for(char digit : eachDigit) {
+            if(digit == '3' || digit == '6' || digit == '9') {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number;i++) {
+            answer += countThreeSixNine(i);
+        }
         return answer;
     }
 }
