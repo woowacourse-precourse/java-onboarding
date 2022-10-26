6db58cf6a235968a263855bd2d43ab76a4056917
Kid-Chang <pch3310@gmail.com>
2022-10-26 21:38:37 +0900

    feat: solved problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int duplicate=1;
+        do{
+            char[] answer = cryptogram.toCharArray();
+            duplicate = 0;
+            for(int i=0; i<cryptogram.length()-1; i++){
+                if(answer[i] == answer[i+1]){
+                    answer[i] = ' ';
+                    answer[i+1] = ' ';
+                    duplicate=1;
+                }
+            }
+            cryptogram = new String(answer).replace(" ", "");
+        }
+        while(duplicate == 1);
+        return String.valueOf(cryptogram);
     }
 }
