6c932d1cb31c558597bb273f19336470b2095779
YE-EUN <musiclove529@naver.com>
2022-10-27 02:10:15 +0900

    feature : add findDuplicatedChar - 연속하는 중복 문자 찾기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
     }
+
+    public static boolean[] findDuplicatedChars(String cryptogram){
+        boolean[] isDuplicated = new boolean[cryptogram.length()];
+        int i = 0;
+        for(int j = 1;j<cryptogram.length();i++,j++){
+            char currentChar = cryptogram.charAt(i);
+            char nextChar = cryptogram.charAt(j);
+            if(currentChar==nextChar){
+                isDuplicated[i] = true;
+                isDuplicated[j] = true;
+            }
+        }
+        return isDuplicated;
+    }
+
 }
