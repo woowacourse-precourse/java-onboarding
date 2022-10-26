3b0e11e1145c3b72be7c94a4d68b4940c1fa9635
eunbii0213 <eunbii0213@naver.com>
2022-10-26 19:14:29 +0900

    feat: (Problem1) 예외상황 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,31 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (isException(pobi) || isException(crong)) {
+            answer = EXCEPTION;
+        }
+
         return answer;
     }
+
+    private static boolean isException(List<Integer> pageList) {
+        if (pageList.get(1) - pageList.get(0) > 1) {
+            return true;
+        } else if (pageList.get(1) < pageList.get(0)) {
+            return true;
+        }
+
+        for (Integer pageNumber : pageList) {
+            if (pageNumber > 200) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
