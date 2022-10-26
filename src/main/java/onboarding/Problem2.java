5d21179531fa58416457f7b5049f860f9b93a830
Plastic_hong <101045399+plastichong@users.noreply.github.com>
2022-10-26 23:41:48 +0900

    1. 문자열 내에서 연속으로 등장하는 문자를 찾기
    getDuplicatedStr 함수 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+
+//        - 1. 문자열 내에서 연속으로 등장하는 문자를 찾기
+//        - 2. 찾은 문자를 입력받은 문자열에서 제거한 뒤, 다시 1번 수행
+//        - 3. 1번에서 연속으로 등장하는 문자를 찾을 수 없을 때 까지 1~2 반복
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+
         String answer = "answer";
         return answer;
+
+
     }
+    public static String getDuplicatedStr(String str) {
+
+        for (int i = 0; i < str.length() - 1; i++) {
+
+            if (str.charAt(i) == str.charAt(i + 1)) {
+
+                String duplicatedStr = String.valueOf(str.charAt(i));
+                duplicatedStr += duplicatedStr;
+
+                return duplicatedStr;
+            }
+
+        }
+        return "";
+    }
+
 }
