db237524e3c54536cb25631755b6498d4ac9fce3
aksmf1442 <aksmf1442@naver.com>
2022-10-26 20:14:06 +0900

    feat: Problem4 ->  문제 풀이 순서 및 문제 목표 주석 달기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+/**
+ * goal: word가 주어질 때 청개구리 사전을 참고하여 반대로 변환한 값을 리턴
+ */
+
+/**
+ * 문제 풀이 순서
+ * 1. map에 청개구리 사전을 a~z까지 등록한다.
+ * 2. 주어진 word의 글자를 하나씩 확인하면서 청개구리 사전 map을 통해 문자열을 반환한다.
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
 }
