2addd1b24153deeee2a26dadd524e291772cb271
KimJaeSung <behappy0916@naver.com>
2022-10-26 22:47:48 +0900

    java-onboarding-1-각 자리 숫자 덧셈 곱셈 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static int sumStringToInteger(String n1){
+        int n = 0;
+        for(int i=0; i<n1.length(); i++){
+            n += n1.charAt(i) - '0';
+        }
+        return n;
+    }
+    static int mulStringToInteger(String n1){
+        int n = 1;
+        for(int i=0; i<n1.length(); i++){
+            n *= n1.charAt(i) - '0';
+        }
+        return n;
+    }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
