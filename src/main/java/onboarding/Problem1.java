affd0013af5de98097f4322c4189161951be49e2
K-jun98 <alap_u@naver.com>
2022-10-26 17:31:12 +0900

    feat - 각 자리수 덧셈

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,13 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static int numberAdd(int number) {
+        int result = 0;
+        while (number != 0) {
+            result += number % 10;
+            number /= 10;
+        }
+        return result;
+    }
 }
