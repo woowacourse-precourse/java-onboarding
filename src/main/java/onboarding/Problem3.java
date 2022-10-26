89eb89b2d68790f5bb022730150cd0c5043b1ee4
main <jang0929@naver.com>
2022-10-26 20:29:58 +0900

    Problem3 369 리턴 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,23 @@ package onboarding;
  * 1. 해당 수의 369의 개수를 리턴하는 함수
  */
 public class Problem3 {
+
+    /**
+     * 해당 수의 369를 리턴하는 함수 
+     * @param number
+     * @return 369 개수 리턴
+     */
+    static int count(int number){
+        int res = 0;
+        while(number != 0){
+            int tmp = number%10;
+            if(tmp == 3 || tmp == 6 || tmp == 9)
+                res++;
+            number/=10;
+        }
+        return res;
+
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
