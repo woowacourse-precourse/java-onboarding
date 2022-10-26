bc19b967c4e428d8a89a45e79cfa61c7a062bd88
MoonJeWoong <munjin0201@naver.com>
2022-10-27 01:18:51 +0900

    [1번 문제] feat: solution 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,35 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static boolean exceptionFlag = false;
+
+    public void checkException(List<Integer> pages){
+
+    }
+
+    public int calculateMaximumAddOrMultiply(int pageNum){
+        int result = 0;
+        return result;
+    }
+
+    public int calculateScore(List<Integer> pages){
+        int result = 0;
+        return result;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Problem1 T = new Problem1();
+
+        T.checkException(pobi);
+        T.checkException(crong);
+        if(exceptionFlag) return -1;
+
+        int pobiScore = T.calculateScore(pobi);
+        int crongScore = T.calculateScore(crong);
+
+        if(pobiScore>crongScore) return 1;
+        if(pobiScore<crongScore) return 2;
+        return 0;
     }
 }
