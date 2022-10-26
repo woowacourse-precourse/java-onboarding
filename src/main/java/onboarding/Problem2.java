abb4296bc0462724f4e4b4c233c187e51b0b106d
JunH <tjdtls690@gmail.com>
2022-10-26 18:00:15 +0900

    :art: refactor : 메서드 기능 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,22 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        for (int i = 0; i < cryptogram.length() - 1; i++) {
-            if (isEqualsTwoString(cryptogram.charAt(i), cryptogram.charAt(i + 1))) {
-                cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + 2);
-                
-                if (i == 0) {
-                    i--;
-                } else {
-                    i -= 2;
-                }
+        for (int index = 0; index < cryptogram.length() - 1; index++) {
+            if (isEqualsTwoChar(cryptogram.charAt(index), cryptogram.charAt(index + 1))) {
+                cryptogram = removeTwoEqualsChar(cryptogram, index);
+                index = revertIndex(index);
             }
         }
         
         return cryptogram;
     }
     
-    private static boolean isEqualsTwoString(final char firstChar, final char secondChar) {
+    private static int revertIndex(int index) {
+        if (index == 0) {
+            return index - 1;
+        } else {
+            return index - 2;
+        }
+    }
+    
+    private static String removeTwoEqualsChar(String cryptogram, final int index) {
+        return cryptogram.substring(0, index) + cryptogram.substring(index + 2);
+    }
+    
+    private static boolean isEqualsTwoChar(final char firstChar, final char secondChar) {
         return firstChar == secondChar;
     }
 }
