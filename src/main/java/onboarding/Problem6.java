213eae5870749bd558c7b70460fd8b1d3b4ecba6
yj2980 <adore2100@naver.com>
2022-10-26 23:31:15 +0900

    fect : 닉네임만 저장하는 함수 추가 savaUser(List<List<String>> forms)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,12 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    static int email = 0;
+    static int name = 1;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
+        System.out.println(forms.get(0).get(0));
+        userCheck(savaUser(forms));
         return answer;
     }
 
+    //닉네임만 저장하는 함수
+    public static List<String> savaUser(List<List<String>> forms)
+    {
+        List<String> userList = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++)
+            userList.add(forms.get(i).get(name));
+
+        return userList;
+    }
     //닉네임이 같은 유저가 있는지 확인
     public static void userCheck(List<String> nickname)
     {
