d55f58d6956fa313885bc871d679bf65303200a3
KimDongho <hkim4410@naver.com>
2022-10-26 21:55:20 +0900

    feat: Problem 1. getScore, makeBiggerNum 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,4 +13,23 @@ class Problem1 {
             return true;
         return false;
     }
+
+    public static int getScore(int left, int right) {
+        int leftScore = makeBiggerNum(left);
+        int rightScore = makeBiggerNum(right);
+
+        return Math.max(leftScore, rightScore);
+    }
+
+    public static int makeBiggerNum(int num) {
+        int sum = 0, mul = 1, max = Integer.MIN_VALUE;
+
+        char[] numArr = String.valueOf(num).toCharArray();
+        for(int i = 0; i < numArr.length; i++) {
+            sum += (int)numArr[i];
+            mul *= (int)numArr[i];
+        }
+
+        return Math.max(sum, mul);
+    }
 }
