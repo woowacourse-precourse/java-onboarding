56ee014a022e65b1648777cda231bd8a2392028b
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-10-26 16:29:01 +0900

    2번문제 해결 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer= new StringBuilder();
+        List<Character>list = new ArrayList<>();
+        for(int i=0;i<cryptogram.length();i++){
+            list.add(cryptogram.charAt(i));
+        }
+        while(true){
+            int idx=0;
+            int cnt=0;
+            while(cnt<list.size()-1){
+
+                if(list.get(cnt)==list.get(cnt+1)){
+                    list.remove(cnt);
+                    list.remove(cnt);
+                    idx=1;
+                }
+            cnt++;
+            }
+            if(idx==0) break;
+        }
+        for(int i=0;i<list.size();i++) answer.append(list.get(i));
+        return answer.toString();
     }
 }
