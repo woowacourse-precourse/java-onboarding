62543c55c4cef98ac0dee3d977c4e0ce385fef45
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-26 21:50:23 +0900

    feat: 예외 처리를 통해 잘못된 입력일 경우 -1을 출력. 모든 기능 요구사항 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,26 +9,32 @@ import java.util.List;
  * [o] 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
  * [o] 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
  * [o] 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
- * [ ] 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
- * [ ] 포비와 크롱이 펼친 페이지가 들어있는 리스트/배열 pobi와 crong이 주어질 때, 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return 하도록 solution 메서드를 완성하라.
+ * [o] 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+ * [o] 포비와 크롱이 펼친 페이지가 들어있는 리스트/배열 pobi와 crong이 주어질 때, 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return 하도록 solution 메서드를 완성하라.
  */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        try {
+            if (!isRandomPage(pobi) || isFirstAndLastPage(pobi) || !isRandomPage(crong) || isFirstAndLastPage(crong)) {
+                throw new Exception();
+            }
+            int answer = Integer.MAX_VALUE;
 
-        int pobiMax = getMax(pobi);
-        int crongMax = getMax(crong);
+            int pobiMax = getMax(pobi);
+            int crongMax = getMax(crong);
 
-        if (pobiMax > crongMax) {
-            answer = 1;
-        } else if (pobiMax < crongMax) {
-            answer = 2;
-        } else if (pobiMax == crongMax) {
-            answer = 0;
+            if (pobiMax > crongMax) {
+                answer = 1;
+            } else if (pobiMax < crongMax) {
+                answer = 2;
+            } else if (pobiMax == crongMax) {
+                answer = 0;
+            }
+
+            return answer;
+        } catch (Exception e) {
+            return -1;
         }
-
-        return answer;
-
     }
 
     public static int getSumNumberOfDigits(int num) {
