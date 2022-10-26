6f4b7f74bd4dafce477fd10fc14cf97936945c60
CHAYEONIL <jeay123@naver.com>
2022-10-27 03:26:52 +0900

    문제 2 주요기능 구현(앞,뒤 비교 동일 문자 삭제, 앞,뒤 동일문자 없을 때까지 반복)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 
 1. [문제 1](./docs/TODO_PROBLEM1.md)
-2. [문제 2](./docs/PROBLEM2.md)
+2. [문제 2](./docs/TODO_PROBLEM2.md)
 3. [문제 3](./docs/PROBLEM3.md)
 4. [문제 4](./docs/PROBLEM4.md)
 5. [문제 5](./docs/PROBLEM5.md)
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String result = "";
+        boolean check = true;
+        // 문자열을 리스트로 변환
+        String[] input_array = cryptogram.split("");
+        List<String> values = Arrays.asList(input_array);
+
+        // 앞,뒤 동일문자 없을 때까지 반복
+        while (check) {
+            check = false;
+            // R_String 리스트로 동일문자 삭제 작업
+            List<String> R_String = new ArrayList();
+            R_String.addAll(values);
+
+            // 앞,뒤 비교하여 동일한 문자 삭제
+            for(int i = 0; i < values.size() -1; i++) {
+                if (values.get(i).equals(values.get(i + 1))) {
+                    R_String.remove(i + 1);
+                    R_String.remove(i);
+                    values = R_String;
+                    check = true;
+                    continue;
+                }
+            }
+        }
+
+        // 리스트를 문자열로 변환
+        result = String.join("", values);
+        return result;
     }
 }
