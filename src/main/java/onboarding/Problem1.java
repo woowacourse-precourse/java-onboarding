be6aaabb35e9d853f5893d1833da427b888167f4
bjk1649 <qhwns1649@naver.com>
2022-10-26 23:39:25 +0900

    feat: 두 숫자 중 최대값 찾아주는 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -50,4 +50,11 @@ class ScoreController{
         return result;
     }
 
+    private static int findMax(int num1,int num2){
+        if (num1 >= num2) {
+            return num1;
+        }
+        return num2;
+    }
+
 }
