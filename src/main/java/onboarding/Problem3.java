872e2bdbac0ef731a11235d14217c235312e5c91
jinny-l <jinny.l.dev@gmail.com>
2022-10-27 02:03:26 +0900

    [feat] 입력받은 문자열에 "3" "6" "9"가 포함되어 있는 개수를 반환하는 메소드 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,16 @@ public class Problem3 {
         return answer;
     }
 
-
+    // 입력받은 문자열에 "3" "6" "9" 가 포함되어 있는 개수를 반환하는 메소드
+    private static int count_3_6_9(String num) {
+        int count = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if (Contain_3_6_9(num.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
 
     // 입력받은 문자에 '3' '6' '9'가 포함되어 있는지 확인하는 메소드
     private static boolean Contain_3_6_9(char c) {
