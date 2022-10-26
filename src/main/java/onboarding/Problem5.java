f062051f8e0fc1a2d75c9ee73de8216988df107f
seungmin-park <tmddudals369@naver.com>
2022-10-26 23:27:04 +0900

    1주차 과제 5번

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = new ArrayList<>();
+        int[] changes = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+        for(int change : changes){
+            answer.add(money/change);
+            money %= change;
+        }
         return answer;
     }
 }
