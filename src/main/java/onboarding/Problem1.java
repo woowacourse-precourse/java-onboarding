a974c892afd7786a3f2015ff01283ee8ca2553f2
ddubidubap <park62000@gmail.com>
2022-10-27 01:18:39 +0900

    PROBLEM1 예외사항 확인 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,6 +10,21 @@ class Problem1 {
         3. solution (왼쪽·오른쪽 비교, 승자 선정)
     */
 
+    // 예외사항 확인 함수
+    private static boolean correctGiven(List<Integer> given) {
+        // 연속적인 수가 주어졌는지 확인
+        if (given.get(0) + 1 != given.get(1))
+            return false;
+
+        // 첫 면, 마지막 면이 주어졌는지 확인
+        for (int i : given) {
+            if ((i == 1) || (i == 400))
+                return false;
+        }
+
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
