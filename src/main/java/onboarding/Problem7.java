c5970f17686b2620abf850f259f845e34f06a4bf
yj2980 <adore2100@naver.com>
2022-10-27 02:41:08 +0900

    feat : 방문 횟수만큼 점수를 부여하는 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Problem7 {
+    static int frendPoint = 10;
+    static int visitPoint = 1;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
@@ -13,15 +15,34 @@ public class Problem7 {
 
     public static Map<String, Integer> findknowFriendsPoint(List<String> knowFriendsList)
     {
-        int point = 0;
-        Map<String, Integer> knowFriends = new HashMap();
+        int point = frendPoint;
+        String key;
+
+        Map<String, Integer> knowFriendsMap = new HashMap();
 
         for (int i = 0; i < knowFriendsList.size(); i++)
         {
-            if (knowFriends.containsKey(knowFriendsList.get(i)))
-                point += 10;
-            knowFriends.put(knowFriendsList.get(i), point);
+            key = knowFriendsList.get(i);
+            if (knowFriendsMap.containsKey(key))
+                point = knowFriendsMap.get(key) + frendPoint;
+            knowFriendsMap.put(knowFriendsList.get(i), point);
+            point = frendPoint;
         }
-        return knowFriends;
+        return knowFriendsMap;
+    }
+
+    public static void findVisitedPoint(Map<String, Integer> knowFriendsMap, List<String> visitors)
+    {
+        int point = visitPoint;
+
+        for (int i = 0; i < visitors.size(); i++)
+        {
+            String key = visitors.get(i);
+            if (knowFriendsMap.containsKey(key))
+            {
+                point = knowFriendsMap.get(key) + visitPoint;
+                knowFriendsMap.put(visitors.get(i), point);
+            }
+        }
     }
 }
