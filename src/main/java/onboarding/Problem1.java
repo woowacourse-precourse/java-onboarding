8f057c31c2a056dbf2cf2f32bdbcdf6204726b1e
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-26 21:46:47 +0900

    문제 1 예외상황 추가 - 왼쪽 항상 홀수, 주어진 list의 size는 항상 2

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,15 +6,19 @@ import java.util.stream.Stream;
 class Problem1 {
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 
+		// 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
 		int pobiLeft = getMaxValue(getDigitArray(pobi, "left"));
 		int crongLeft = getMaxValue(getDigitArray(crong, "left"));
 
+		// 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
 		int pobiRight = getMaxValue(getDigitArray(pobi, "right"));
 		int crongRight = getMaxValue(getDigitArray(crong, "right"));
 
+		//2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
 		int pobiMaxValue = Math.max(pobiLeft, pobiRight);
 		int crongMaxValue = Math.max(crongLeft, crongRight);
 
+		//점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
 		if (isException(pobi) && isException(crong)) {
 			if (pobiMaxValue == crongMaxValue) {
 				return 0;
@@ -25,7 +29,7 @@ class Problem1 {
 	}
 
 	static boolean isException(List<Integer> list) {
-		return list.get(0) == list.get(1) - 1;
+		return list.get(0) == list.get(1) - 1 && list.get(0) % 2 == 1 && list.size() == 2;
 	}
 
 	static int[] getDigitArray(List<Integer> list, String side) {
