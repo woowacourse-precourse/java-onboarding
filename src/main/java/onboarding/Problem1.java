2a387bcff0cad6ed487f95fab7abdd49572038f6
syeonbot <asdf748596123@gmail.com>
2022-10-27 01:11:43 +0900

    feat: problem1 제한 사항 validation 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,12 +8,80 @@ class Problem1 {
     public static final int POBI_WIN = 1;
     public static final int CRONG_WIN = 2;
     public static final int DRAW = 0;
+    public static final int PAGE_SIZE = 2;
+    public static final int FIRST_PAGE = 1;
+    public static final int LAST_PAGE = 400;
+    public static final int PAGE_DIFF = 1;
+    public static final int ERROR = -1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        try {
+            validatePages(pobi);
+            validatePages(crong);
+        } catch (IllegalArgumentException e) {
+            return ERROR;
+        }
+
         int pobiMaxScore = getMaxScore(pobi);
         int crongMaxScore = getMaxScore(crong);
 
         return getAnswer(pobiMaxScore, crongMaxScore);
+    }
+    private static void validatePages(List<Integer> pages) {
+        validateListSize(pages);
+        validateLeftPage(getLeftPageNumber(pages));
+        validateRightPage(getRightPageNumber(pages));
+        validatePageDifference(pages);
+    }
+
+    private static void validateLeftPage(int pageNumber) {
+        validateRange(pageNumber);
+        validateOdd(pageNumber);
+    }
+
+    private static void validateRightPage(int pageNumber) {
+        validateRange(pageNumber);
+        validateEven(pageNumber);
+    }
+
+    private static void validatePageDifference(List<Integer> pages) {
+        int pageDifference = getRightPageNumber(pages) - getLeftPageNumber(pages);
+
+        if (pageDifference != PAGE_DIFF) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateEven(int pageNumber) {
+        if (isOdd(pageNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateOdd(int pageNumber) {
+        if (isEven(pageNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isOdd(int n) {
+        return !isEven(n);
+    }
+
+    private static boolean isEven(int n) {
+        return (n % 2) == 0;
+    }
+
+    private static void validateRange(int pageNumber) {
+        if (pageNumber < FIRST_PAGE || LAST_PAGE < pageNumber) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateListSize(List<Integer> pages) {
+        if (pages.size() != PAGE_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
     private static int getAnswer(int pobiMaxScore, int crongMaxScore) {
         if (pobiMaxScore > crongMaxScore) {
