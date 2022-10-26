94e739e3e6fbdca8d738d8f78d4a6488e94662a9
ddubidubap <park62000@gmail.com>
2022-10-27 01:11:17 +0900

    PROBLEM1 기능 요구 사항 목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /*
+        기능 요구 사항 목록
+        1. 예외사항 확인 함수
+        2. 자릿수 합과 곱 중 큰 값을 반환하는 함수
+        3. solution (왼쪽·오른쪽 비교, 승자 선정)
+    */
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
