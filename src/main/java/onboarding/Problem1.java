5b2ee7dfe4c5d7b19e883ba7e069352f13ba51e9
JunH <tjdtls690@gmail.com>
2022-10-26 17:11:59 +0900

    :sparkles: feat : 왼쪽 홀수, 오른쪽 짝수가 아닌 경우 예외처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,8 +5,27 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (isPlayersCorrectInput(Arrays.asList(pobi, crong))) {
+            return -1;
+        }
+        
         int result = Integer.compare(getMaxNumber(pobi), getMaxNumber(crong));
         return result >= 0 ? result : 2;
+    }
+    
+    private static boolean isPlayersCorrectInput(final List<List<Integer>> players) {
+        return players.stream()
+                .anyMatch(Problem1::isNotCorrectInputNumbers);
+    }
+    
+    private static boolean isNotCorrectInputNumbers(final List<Integer> numbers) {
+        int leftNumber = numbers.get(0);
+        int rightNumber = numbers.get(1);
+        return isNotCorrectEvenAndOdd(leftNumber, rightNumber);
+    }
+    
+    private static boolean isNotCorrectEvenAndOdd(final int leftNumber, final int rightNumber) {
+        return leftNumber % 2 != 1 || rightNumber % 2 != 0;
     }
     
     private static int getMaxNumber(final List<Integer> numbers) {
