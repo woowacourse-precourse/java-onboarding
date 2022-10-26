b858e6dfd12bfdc5b668460512e82557487ffa50
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 19:15:19 +0900

    앞, 뒤를 비교해 같은 문자일 시 삭제하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        List<String> splitCryptogram = new ArrayList<>(List.of(cryptogram.split("")));
+        int idx = 1;
+        while (splitCryptogram.size() != 0) {
+            if (isDuplicate(splitCryptogram, idx)) {
+                removeDuplicate(splitCryptogram, idx);
+                idx = 1;
+                continue;
+            }
+            idx++;
+        }
+        return String.join("", splitCryptogram);
+    }
+    public static boolean isDuplicate(List<String> str, int idx) {
+        return str.get(idx-1).equals(str.get(idx));
+    }
+
+    public static void removeDuplicate(List<String> strList, int idx) {
+        strList.remove(idx);
+        strList.remove(idx-1);
     }
 }
