e589c2ad2cfae8790e904bd5c88172d9c94d88ba
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 17:47:11 +0900

    결과값으로 나온 두 수를 비교하여 결과를 출력하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,12 +6,17 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobiScore;
         int crongScore;
-        int answer;
 
         pobiScore = Math.max(howManyScore(pobi.get(0)), howManyScore(pobi.get(1)));
         crongScore = Math.max(howManyScore(crong.get(0)), howManyScore(crong.get(1)));
 
-        return -1;
+        return Result(pobiScore, crongScore);
+    }
+
+    public static int Result(int pobiScore, int crongScore) {
+        if (pobiScore > crongScore) return 1;
+        if (pobiScore < crongScore) return 2;
+        return 0;
     }
 
     public static int howManyScore(int num) {
