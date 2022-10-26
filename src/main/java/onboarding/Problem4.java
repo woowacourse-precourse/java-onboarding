8a6153e918fcb9812b2e56a67b8c42b63cbd2d23
Hayoon <hy97@sju.ac.kr>
2022-10-26 23:13:25 +0900

    feat: HashMap으로 대/소문자 각각<Key, Value> 값 설정 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    static class ConvertAlphabet {
+        HashMap<Character, Character> alphabet = new HashMap<Character, Character>();
+
+        public ConvertAlphabet() {
+            char upperAlphabetSerial = 65;
+            char lowerAlphabetSerial = 97;
+
+            for (int i = 0; i < 26; i++) {
+                alphabet.put(upperAlphabetSerial, (char) (90 - i));
+                alphabet.put(lowerAlphabetSerial, (char) (122 - i));
+
+                upperAlphabetSerial++;
+                lowerAlphabetSerial++;
+            }
+        }
+    }
     public static String solution(String word) {
+        ConvertAlphabet convertAlphabet = new ConvertAlphabet();
+        StringBuilder stringBuilder = new StringBuilder(word);
+
         String answer = "";
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String sample = "I love you";
+
+        System.out.println(solution(sample));
     }
 }
