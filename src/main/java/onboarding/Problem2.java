532f5c59a908fe4d9bc0a2f8418e82999f5f259d
01192mg <01192mg@gmail.com>
2022-10-26 18:15:28 +0900

    feat: Problem2 기능 구현
    입력된 문자열을 하나씩 스택에 넣어준다. 이때 스택이 비어있을 땐 문자를 바로 넣어준다. 비어있지 않으면 스택에 있는 마지막 문자와 현재 문자를 비교해서 같은 경우 마지막 문자를 꺼내고 다를 경우 현재 문자를 스택에 추가해준다.
    * StringBuilder 사용 이유
    스택에 있는 문자를 하나씩 꺼내서 `String` 에 하나씩 더해도 되지만, 문자는 불변 객체기 때문에 문자를 더할 때마다 새로운 `String` 객체가 생성된다. 객체 생성은 비용이 크기 때문에 성능저하가 우려됨. 따라서 반복적으로 문자를 연산하는 경우 StringBuilder 또는 StringBuffer가 성능이 좋기 때문에 사용했다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> alphabets = new Stack<>();
+        for (char c : cryptogram.toCharArray()) {
+            if (alphabets.empty()) {
+                alphabets.push(c);
+                continue;
+            }
+            Character before = alphabets.peek();
+            if (before == c) {
+                alphabets.pop();
+            } else {
+                alphabets.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        alphabets.forEach(sb::append);
+        return sb.toString();
     }
 }
