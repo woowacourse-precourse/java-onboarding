95490a5b6960aac09cec070b537e018d9972597e
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 18:26:06 +0900

    문제 1 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,11 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobiScore;
         int crongScore;
 
-
+        if (exceptionCheck(pobi, crong)) return -1;
 
         pobiScore = Math.max(howManyScore(pobi.get(0)), howManyScore(pobi.get(1)));
         crongScore = Math.max(howManyScore(crong.get(0)), howManyScore(crong.get(1)));
@@ -17,7 +18,7 @@ class Problem1 {
 
     public static boolean exceptionCheck(List<Integer> pobi, List<Integer> crong) {
         if (pobi.get(0) == 0 || crong.get(0) == 0 || pobi.get(1) == 401 || crong.get(1) == 401) return true;
-        if (pobi.get(0) % 2 != 1 || pobi.get(1) % 2 != 0 || crong.get(0) % 2 != 1 || crong.get(0) % 2 != 0) return true;
+        if (pobi.get(0) % 2 != 1 || pobi.get(1) % 2 != 0 || crong.get(0) % 2 != 1 || crong.get(1) % 2 != 0) return true;
         return pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1);
     }
 
@@ -33,14 +34,14 @@ class Problem1 {
 
     public static int plusDigits(int num) {
         if (num >= 100) {
-            return num / 100 + (num % 100 + 1)/10 - 1 + num % 10;
+            return num / 100 + ((num % 100) / 10) + num % 10;
         }
         return num / 10 + num % 10;
     }
 
     public static int multiFlyDigits(int num) {
         if (num >= 100) {
-            return num / 100 * ((num % 100 + 1)/10 - 1) * num % 10;
+            return (num / 100) * ((num % 100) / 10) * (num % 10);
         }
         return (num / 10) * (num % 10);
     }
