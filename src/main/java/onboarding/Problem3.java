e87d4f1185f317a0fc7196f004cd49f794c1d947
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-26 21:11:00 +0900

    add : (Problem1) 3 포함 여부 확인
    숫자 3이 포함되어 있는지 확인함

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=0; i<number+1; i++){
+            char[] intToChar = String.valueOf(i).toCharArray();
+            answer += containThree(intToChar);
+        }
+
         return answer;
+    }
+
+    private static int containThree(char[] num){
+        int cnt = 0;
+        for(int i=0; i<num.length; i++){
+            if(num[i] == '3') cnt++;
+        }
+        return cnt;
     }
 }
