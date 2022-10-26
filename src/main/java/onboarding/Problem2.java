449e40137e62dd65d198ceac97cb2bf090ee13bf
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-26 20:52:14 +0900

    add : (Problem1) 문자 중복 제거
    현재 위치 i와 다음 위치인 i+1의 문자를 비교하여 같은지 확인하여 중복을 제거한다. 중복 문자가 존재하지 않는 경우 결과를 반환한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,37 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        /*
+        * 2연속으로 중복되는 문자가 있는지 확인하여 제거한다.
+        * 없으면(길이 변화가 없다면), 반복을 그만한다.
+        * */
+        while (true){
+            String tmp = decryption(cryptogram);
+            if(cryptogram.length() == tmp.length()) break;
+            else cryptogram = tmp;
+        }
+
+        return cryptogram;
+    }
+
+    /*
+    * 현재 위치 i와 다음 위치인 i+1의 문자를 비교하여 같은지 확인한다.
+    * 같으면 i와 i+1을 제외한 문자를 subString으로 제거한다.
+    * */
+    private static String decryption(String str) {
+        char[] strToChar = str.toCharArray();
+        int n = strToChar.length;
+
+        String left=str;
+        String right="";
+        for(int i=0; i<n-1; i++) {
+            if(strToChar[i] == strToChar[i+1]){
+                left = str.substring(0, i);
+                if(i+2<n) right = str.substring(i+2, n);
+                break;
+            }
+        }
+        return left + right;
     }
 }
