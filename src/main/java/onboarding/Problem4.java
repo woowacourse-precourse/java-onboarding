7bcb7d4c139000d0014f9952cf440978f523efdd
Kuyho Chung <kchung1995@gmail.com>
2022-10-26 23:46:21 +0900

    온보딩 - 문제 4 - reverseAlphabet 내용 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+
+    /**
+     * 입력받은 알파벳의 reverse를 리턴한다.
+     * @param input
+     * @return
+     */
+    private static char reverseAlphabet(char input) {
+        // 소문자인 경우
+        if ('a' <= input && input <= 'z') {
+            return (char) (input - 'z' + 'a');
+        }
+        // 대문자인 경우
+        if ('A' <= input && input <= 'Z') {
+            return (char) (input - 'Z' + 'A');
+        }
+        // 공백인 경우
+        return input;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
