c0f5674f8a09c68bbd2f8b3df127ae33af9b7331
code-tree <dltjdqls1200@naver.com>
2022-10-26 22:03:36 +0900

    feat : Check for invalid input

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,33 @@ package onboarding;
 
 import java.util.List;
 
-class Problem1 {
+public class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (isInvalidPage(pobi) || isInvalidPage(crong)) {
+            return -1;
+        }
+
+        return 1;
+    }
+
+    private static boolean isInvalidPage(List<Integer> openPage) {
+        if (openPage.size() != 2) {
+            return true;
+        }
+
+        int leftPage = openPage.get(0);
+        int rightPage = openPage.get(1);
+
+        if (leftPage == 1 || rightPage == 400) {
+            return true;
+        }
+        if (leftPage % 2 == 0 || rightPage % 2 == 1) {
+            return true;
+        }
+        if (rightPage - leftPage != 1) {
+            return true;
+        }
+
+        return false;
     }
 }
