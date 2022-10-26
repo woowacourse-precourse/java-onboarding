4ad8363263605927c261dffdb18a55b544dc9e5b
Hayoon <hy97@sju.ac.kr>
2022-10-26 22:35:14 +0900

    feat: 입력받은 숫자 369 검증 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+
+    static boolean clappingHands(int number) {
+        while(number != 0) {
+            int val = number % 10;
+            if (val == 3 || val == 6 || val == 9)
+                return true;
+
+            number /= 10;
+            }
+        return false;
+    }
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++)
+            if(clappingHands(i)) answer++;
+
         return answer;
     }
 }
