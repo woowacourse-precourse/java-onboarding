bb6a10cca087549c554a5f5f8bcde18821943eb1
myungsikyoon <yms8795@gmail.com>
2022-10-27 00:08:48 +0900

    docs(problem1) : 기능 정리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /*
+    * 기능 정리
+    * 1. 각 user에 대한 page error 검사(홀짝 확인, 1~400 범위 확인, page 차이 1인 것 확인)
+    * 2. user의 페이지에 대해 자리수 분리 후, 분리한 각 자리 list 반환
+    * 3. 반환된 list를 가지고 각 자리 덧셈, 곱셈한 값에 대해 대소 관계 비교 후, 더 큰 값 반환하며 user의 값 결정.
+    * 4. 이렇게 결정된 두 user의 값을 비교하여 최종 answer값 return
+    * */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
