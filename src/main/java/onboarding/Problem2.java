82bb846e7fd4f665ce675dfd8612e94e24942200
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-26 22:47:42 +0900

    feat(Problem2): 암호문 해독 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,8 +10,35 @@ package onboarding;
 //  반복문 break 조건 : 연속하는 중복문자가 없었을 때, string length < 2
 
 public class Problem2 {
+    static boolean isRemoved = false;  // decrypthion이 실행되는 동안 중복문자를 지웠는지 확인
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    // 1. 암호문 해독
+    public static String decryption(String cipher) {
+        isRemoved = false;
+
+        for (int i = 0; i < cipher.length() - 1; i++) {
+            // 1-a. cipher(n) == cipher(n+1)일 때까지 탐색
+            if (cipher.charAt(i) == cipher.charAt(i + 1)) {
+                isRemoved = true;   // 제거된 중복문장 있음
+                int j = 2;
+
+                // 1-b. cipher(n) != cipher(n+m)일 때까지 탐색
+                for (j = 2; j < cipher.length(); j++) {
+                    if (cipher.charAt(i) != cipher.charAt(i + j)) {
+                        break;
+                    }
+                }
+
+                // 1-c. cipher(n) ~ cipher(n+m-1)을 제거한 문장으로 cipher 초기화
+                cipher = cipher.substring(0, i) + cipher.substring(i + j);
+            }
+            // 1-d. cipher(n+1)부터 다시 탐색 시작 -> 문장이 끝날때까지
+        }
+        return cipher;
     }
 }
