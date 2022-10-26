dc1db1b0589542bb4dc40d9b9a5b61c75b27cdc8
young2866 <62212610+young2866@users.noreply.github.com>
2022-10-26 18:49:57 +0900

    Problem1 기능2 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -24,6 +24,37 @@ class Problem1 {
     왼쪽 페이지 숫자 배열과 오른쪽 페이지 숫자 배열이 주어졌을때, 최댓값을 구하는 기능
     * */
 
+    public static int findMaxValue(int[] leftNum, int[] rightNum)
+    {
+        int maxValue = 0; // 각 자리 수를 더했을 때, 최솟값이 1이므로 0으로 초기화
+        for(int i = 0; i < 2; i++) // 배열이 2개 이므로 2번반복
+        {
+            int addNum = 0; // 각 자리 수를 더할 변수
+            int mulNum = 1; // 각 자리 수를 곱할 변수
+            if(i == 0)
+            {
+                for(int j = 0; j < leftNum.length; j++)
+                {
+                    addNum += leftNum[j];
+                    mulNum += leftNum[j];
+                }
+
+                maxValue = Math.max(addNum, mulNum); // left 배열이 먼저 실행되므로 maxValue는 따로 비교하지 않아도 된다.
+            }else
+            {
+                for(int j = 0; j < rightNum.length; j++)
+                {
+                    addNum += leftNum[j];
+                    mulNum += leftNum[j];
+                }
+
+                maxValue = Math.max(maxValue, Math.max(addNum, mulNum));
+            }
+        }
+
+        return maxValue;
+    }
+
     /*
     기능 3
     점수를 비교해 승자 값을 리턴하는 기능
