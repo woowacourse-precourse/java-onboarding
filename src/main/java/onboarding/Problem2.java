bf85933377d050a66167a860600d7dc2e38f290b
K-jun98 <alap_u@naver.com>
2022-10-26 21:43:17 +0900

    feat - 중복되는 문자를 뺀 문자열을 다시 만들어 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        return "";
+        return removeLetter(cryptogram);
     }
 
 
@@ -15,4 +15,21 @@ public class Problem2 {
         return -1;
     }
 
+    private static String removeLetter(String cryptogram) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = isOverlap(cryptogram);
+
+        if (index == -1) {
+            return cryptogram;
+        }
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (index == i) {
+                i++;
+            } else if (index != i) {
+                stringBuilder.append(cryptogram.charAt(i));
+            }
+        }
+        return removeLetter(String.valueOf(stringBuilder));
+    }
 }
