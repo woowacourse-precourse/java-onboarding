5662e8831a2ab181a1d04362fb04cb862469ebac
01192mg <01192mg@gmail.com>
2022-10-27 01:40:51 +0900

    refactor: Problem4 기능 단위로 메소드 분리
    기능 목록에 맞춰서 메소드로 분리하여 의미가 명확히 나타나게 변경함.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,17 +3,22 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
-        for (char c : word.toCharArray()) {
-            if (!Character.isAlphabetic(c)) {
-                answer.append(c);
-                continue;
-            }
-            if (Character.isUpperCase(c)) {
-                answer.append((char) ('Z' - c + 'A'));
-            } else {
-                answer.append((char) ('z' - c + 'a'));
-            }
-        }
+        word.chars().map(c -> convertToFrogDictionary((char) c))
+                .forEach(answer::append);
         return answer.toString();
+    }
+
+    private static char convertToFrogDictionary(char c) {
+        if (isNotContainsFrogDictionary(c)) {
+            return c;
+        }
+        if (Character.isUpperCase(c)) {
+            return (char) ('Z' - c + 'A');
+        }
+        return (char) ('z' - c + 'a');
+    }
+
+    private static boolean isNotContainsFrogDictionary(char c) {
+        return !Character.isAlphabetic(c);
     }
 }
