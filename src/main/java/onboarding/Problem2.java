0a588cfdb3b48b9a57abf80ea95bc1614717b7ab
young2866 <62212610+young2866@users.noreply.github.com>
2022-10-26 20:49:38 +0900

    Problem2 기능1 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,35 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
     }
+
+    /*
+    기능 1
+    문자열에서 중복을 제거 하는 기능
+    */
+    public static String eraseDuplicateCharacter(String str)
+    {
+        if(str.length() == 1 || str.length() == 0)
+            return str;
+
+        String ErasedDuplicateCharacter = "";
+
+        if(str.charAt(0) != str.charAt(1))
+            ErasedDuplicateCharacter += str.charAt(0);
+        for(int i = 1; i < str.length() - 1; i++)
+        {
+            if(str.charAt(i) != str.charAt(i - 1) && str.charAt(i) != str.charAt(i + 1))
+            {
+                ErasedDuplicateCharacter += str.charAt(i);
+            }
+        }
+        if(str.charAt(str.length() - 1) != str.charAt(str.length() - 2))
+            ErasedDuplicateCharacter += str.charAt(str.length() - 1);
+
+        return ErasedDuplicateCharacter;
+    }
+
+
 }
