e82cb8ec31d7f55f956a4402dd99b1c4fad4ee36
yj2980 <adore2100@naver.com>
2022-10-26 18:37:21 +0900

    feat : String을 List로 변환하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+		createList(cryptogram);
+
         return answer;
     }
+
+	//String을 List로 변환하는 함수
+	public static List<Character> createList(String word)
+	{
+		List<Character> wordList = new ArrayList<>();
+
+		for (int i = 0; i < word.length(); i++)
+			wordList.add(word.charAt(i));
+
+		return wordList;
+	}
+
+	//중복하는 단어를 제거
+	public static String remove(String cryptogram)
+	{
+
+	}
 }
