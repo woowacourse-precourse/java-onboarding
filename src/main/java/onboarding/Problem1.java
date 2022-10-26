9c66df787a18d0fa5804384acfa2cf6c26f85d20
JaehongDev <println@kakao.com>
2022-10-26 20:45:03 +0900

    change (Problem) : 1번 구현완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import onboarding.problem1.CompareResult;
+import onboarding.problem1.Player;
+
 import java.util.List;
 
 class Problem1 {
+    private static final Integer ERROR_NUMBER = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        try {
+            return new CompareResult().compare(new Player(pobi), new Player(crong));
+        } catch (Exception e) {
+            return ERROR_NUMBER;
+        }
     }
 }
