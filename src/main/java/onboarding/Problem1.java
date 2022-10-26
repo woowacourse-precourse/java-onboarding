4f3e65cd35544a1360b0430fc0555ca7b0cb56af
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-26 21:13:50 +0900

    feat : Add a function that choose sum or multiply

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+
     }
 
     //각 자리를 더하는 함수
@@ -31,27 +31,42 @@ class Problem1 {
     }
 
     //두 숫자 크기를 비교하여 큰 값을 반환하는 함수
-    public static int getBigNumber(int sum, int multiply){
-        if(sum==multiply || sum>multiply) return sum;
+    public static int getBigNumber(int sum, int multiply) {
+        if (sum == multiply || sum > multiply) return sum;
         return multiply;
     }
 
     //승자를 가리는 함수
-    public static int getWinner(int pobi, int crong){
-        if(pobi==crong) return 0;
-        if(pobi>crong) return 1;
+    public static int getWinner(int pobi, int crong) {
+        if (pobi == crong) return 0;
+        if (pobi > crong) return 1;
         return 2;
     }
 
     //예외상황을 판별하는 함수
-    public static int isException(List<Integer> list){
+    public static boolean isException(List<Integer> list) {
 
         //연속된 두 페이지가 아니거나 왼쪽페이지 쪽수가 오른쪽 페이지 쪽수보다 큰 경우
-        if(list.indexOf(1)-list.indexOf(0)!=1) return -1;
+        if (list.indexOf(1) - list.indexOf(0) != 1) return true;
 
         //시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다는 조건에 해당하는 예외상황
-        if(list.indexOf(0)==1||list.indexOf(1)==400) return -1;
+        if (list.indexOf(0) == 1 || list.indexOf(1) == 400) return true;
 
-        return 1;
+        return false;
+    }
+
+    //각 자리 합과 곱 중 더 큰 값을 골라주는 함수
+    public static int getSumOrMultiply(int left, int right){
+        int leftSum = getSum(left);
+        int leftMultiply = getMultiply(left);
+        int leftBigNum = getBigNumber(leftSum, leftMultiply);
+
+        int rightSum = getSum(right);
+        int rightMultiply = getMultiply(right);
+        int rightBigNum = getBigNumber(rightSum, rightMultiply);
+
+        int bigNum = getBigNumber(leftBigNum, rightBigNum);
+
+        return bigNum;
     }
 }
