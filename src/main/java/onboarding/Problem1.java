4a5f05e6f8330202ea9f86c484e384a43d7dbdff
kmularise <kmula222@gmail.com>
2022-10-26 22:05:06 +0900

    ex01 각자릿수 더하는 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static int addValue(int num){
+        int answer = 0;
+        answer += num % 10;
+        while(num / 10 > 0){
+            num = num / 10;
+            answer += num % 10;
+        }
+        return answer;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //int pobiMax = addValue(pobi.get(0));
+        //int crongMax = addValue(pobi.get(0));
+        //System.out.println("pobi[max]"+pobi.get(0));
+        //System.out.println("crong[max]"+crong.get(0)+"\n\n\n\n\n");
+       //System.out.println("answer" + addValue(pobi.get(0)));
+       //System.out.println("crong" + addValue(crong.get(0)));
+        //return answer;
+
         return answer;
     }
 }
