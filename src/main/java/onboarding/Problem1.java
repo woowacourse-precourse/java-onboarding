1ff2eec9a890b677383b626e96a5022a21287355
limjae <limjae96@gmail.com>
2022-10-26 19:01:09 +0900

    feat: 문제 1 solution 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import onboarding.problem1.PageGame;
+import onboarding.problem1.PageScore;
+import onboarding.problem1.impl.PageGameV1;
+import onboarding.problem1.impl.PageScoreV1;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        PageScore pobiScore = new PageScoreV1(pobi);
+        PageScore crongScore = new PageScoreV1(crong);
+        PageGame game = new PageGameV1(pobiScore, crongScore);
+        return game.getResult();
     }
 }
