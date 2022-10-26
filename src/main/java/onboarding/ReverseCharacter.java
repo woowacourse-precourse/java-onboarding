c647217a186b1fa752401f13748163866077e4ff
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-27 03:56:51 +0900

    feat(problem4): reverse upper case

--- src/main/java/onboarding/ReverseCharacter.java
+++ src/main/java/onboarding/ReverseCharacter.java
@@ -3,6 +3,7 @@ package onboarding;
 public class ReverseCharacter {
 
 	private static final int BLANK = 32;
+	private static final int UPPER = 65 + 90;
 
 	private final char[] chars;
 
@@ -12,11 +13,14 @@ public class ReverseCharacter {
 
 	public String run() {
 		String answer = "";
+
 		for (char c : chars) {
 			int ascii = (int)c;
 
 			if (ascii == BLANK) {
 				answer += " ";
+			} else if (Character.isUpperCase(ascii)) {
+				answer += Character.toString(UPPER - ascii);
 			}
 
 		}
