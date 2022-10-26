b35263f25e384f3b7384c72d428b9d1b538df544
Laeng <Laeng@users.noreply.github.com>
2022-10-26 21:42:39 +0900

    feat(problem2): 중복문자 제거 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Problem2 {
@@ -9,6 +9,11 @@ public class Problem2 {
         return answer;
     }
 
+    /**
+     * 올바른 암호문이 입력되었는지 검증합니다.
+     * @param cryptogram 암호문
+     * @return 올바른 암호문이라면 TRUE 반환
+     */
     private static boolean validateCryptogram(String cryptogram) {
         int lengthCryptogram = cryptogram.length();
 
@@ -25,7 +30,37 @@ public class Problem2 {
         return true;
     }
 
-    private static String[] splitCryptogram(String cryptogram) {
-        return cryptogram.split("");
+    /**
+     * 중복 문자를 제거합니다.
+     * @param cryptogram 암호문
+     * @return 제거된 문자열
+     */
+    private static String removeDuplicateString(String cryptogram) {
+        int lengthCryptogram = cryptogram.length();
+        List<String> splitCryptogram = splitCryptogram(cryptogram);
+
+        // 문자열 순차 읽기
+        for (int i = 0; i < lengthCryptogram; i++) {
+            // 지난 문자와 현재 문자가 같은지 확인하기.
+            if (i > 0 && Objects.equals(splitCryptogram.get(i - 1), splitCryptogram.get(i))) {
+                //중복 문자 제거
+                splitCryptogram.remove(i - 1);
+                splitCryptogram.remove(i);
+
+                lengthCryptogram -= 2;
+                i -= 2;
+            }
+        }
+
+        return String.join("",splitCryptogram);
+    }
+
+    /**
+     * 암호문을 분할합니다.
+     * @param cryptogram 암호문
+     * @return 분할된 암호문
+     */
+    private static List<String> splitCryptogram(String cryptogram) {
+        return Arrays.asList(cryptogram.split(""));
     }
 }
