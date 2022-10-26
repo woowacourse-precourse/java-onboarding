555bfad8eed65d84372fe827fad9015eb9645e63
hoin <apicad0307@gmail.com>
2022-10-26 23:10:53 +0900

    ãfeat: 요구사항3 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -19,8 +19,4 @@ public class Problem2 {
         }
         return sb.toString();
     }
-
-    public static void main(String[] args) {
-        System.out.println(solution("zyelleyz"));
-    }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem3 {
+    static int claps(int num){
+
+        int cnt = 0;
+        while (num > 0) {
+            int tmp = num % 10;
+            if(tmp == 3 || tmp == 6 || tmp ==9) cnt++;
+            num /= 10;
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
+
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            answer += claps(i);
+        }
         return answer;
     }
 }
