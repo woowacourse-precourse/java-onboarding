77923fd94739ee446cc056129da246202e0d3506
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-26 17:56:15 +0900

    feat: 왼쪽, 오른쪽 페이지 순서대로 아닐 시 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,24 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //예외처리
+        if (CheckException(pobi)) {
+            return -1;
+        }
+        if (CheckException(crong)) {
+            return -1;
+        }
+
         return answer;
     }
+
+    public static Boolean CheckException(List<Integer> Page){
+        if (Page.get(0) + 1 != Page.get(1)) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
