f053d4e93dc4fa114b79eef88fbe11f2b26a6d2e
이한솔 <hansol8701@naver.com>
2022-10-27 00:31:29 +0900

    feat: validation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,15 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static void openBook(List<Integer> pages) {
-        int randNum;
-        for (int i = 0; i <= 1; i++) {
-            randNum = pages.get(i);
-            while (randNum == 1 || randNum == 400) {
-                randNum = (int) (Math.random() * (399 - 2 + 1) + 2);
-            }
-            pages.set(i, randNum);
+    public static boolean validation(List<Integer> pages) {
+        int pageOdd = pages.get(0);
+        int pageEven = pages.get(1);
+        if (pages.size() != 2) {
+            return false;
         }
+        if (pageOdd % 2 != 1 || pageEven % 2 != 0 || pageOdd + 1 != pageEven) {
+            return false;
+        }
+        if (pageOdd <= 1 || pageOdd > 400 || pageEven < 1 || pageEven >= 400){
+            return false;
+        }
+        return true;
     }
 
     public static int calcNumber(Integer page) {
@@ -25,16 +29,32 @@ class Problem1 {
         return Math.max(mult, sum);
     }
 
-    public static int getScore(List<Integer> pages){
+    public static int getScore(List<Integer> pages) {
         return Math.max(calcNumber(pages.get(0)), calcNumber(pages.get(1)));
     }
 
+    public static int chooseWinner(List<Integer> pages1, List<Integer> pages2) {
+        int score1 = getScore(pages1), score2 = getScore(pages2);
+        try {
+            if (!validation(pages1) || !validation(pages2)){
+                return -1;
+            }
+            if (score1 > score2) {
+                return 1;
+            } else if (score1 < score2) {
+                return 2;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer;
 
-        openBook(pobi);
-        openBook(crong);
-
+        answer = chooseWinner(pobi, crong);
 
         return answer;
     }
