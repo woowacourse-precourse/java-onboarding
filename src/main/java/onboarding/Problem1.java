96c678ebdd7a0cafb935642cff4dab15537d3fde
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-10-26 21:37:42 +0900

    [feat] Problem1: 페이지 번호 각 자리 수 덧셈, 곱셈 기능추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,21 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    private static int pageMultiply(Integer page) {
+        int multiply = 1;
+        while(page > 0) {
+            multiply *= page % 10;
+            page /= 10;
+        }
+        return multiply;
+    }
+
+    private static int pagePlus(Integer page) {
+        int plus = 0;
+        while(page > 0) {
+            plus += page % 10;
+            page /= 10;
+        }
+        return plus;
+    }
 }
