8d0d0b108db9369fab53d45d47fc1d9dbcc75423
meunrud <y3a0522@gmail.com>
2022-10-27 01:11:23 +0900

    포비와 크롱 배열에 랜덤으로 홀수, 짝수 넣기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        pobi = new ArrayList<>();
+        crong = new ArrayList<>();
+
+        pobi.add(randomPage());
+        pobi.add(randomPage()+1);
+
+        crong.add(randomPage());
+        crong.add(randomPage()+1);
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static Integer randomPage(){
+        Random random = new Random();
+        Integer randomPage  = 0;
+        randomPage = 2*random.nextInt(401)+1;
+        return randomPage;
     }
 }
