613fc8f5c33a6f88bdc70dc0e6ddf966f72e6508
JeonghoPark <pjhpjh0805@naver.com>
2022-10-27 07:23:30 +0900

    feat: 5번 금액이 큰 순서대로 잔돈 반환 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = new ArrayList<>();
+        int[] typeOfMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        countChanges(typeOfMoney, answer, money);
+
         return answer;
+    }
+
+    private static void countChanges(int[] typeOfMoney, List<Integer> answer, int money) {
+        int balance = money;
+        for (int i=0; i<typeOfMoney.length; i++) {
+            int count = balance/typeOfMoney[i];
+            if (count != 0) {
+                balance -= count*typeOfMoney[i];
+                answer.add(count);
+                continue;
+            }
+            answer.add(0);
+        }
     }
 }
