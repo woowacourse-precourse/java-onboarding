f596a6e0c886ee7030865df69d2f1b01259b0fd8
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-10-26 21:19:49 +0900

    데이터의 유효성 검사
    시작 면이나 마지막 면이 나오지 않았는가?
    데이터가 연속적으로 주어졌는가?
    
    예외사항을 확인하기 위한 코드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,22 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(validation(pobi) == -1 || validation(crong) == -1) {
+            return -1;
+        }
+
         return answer;
     }
+
+    public static int validation(List<Integer> page) {
+        if(page.get(0) == 1 || page.get(1) == 400) {
+            return -1;
+        }
+        if(page.get(0) + 1 != page.get(1)) {
+            return -1;
+        }
+        return 0;
+    }
 }
+
