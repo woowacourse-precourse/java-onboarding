e83461ab19aa16966644400deb8d83bcedab2f9a
ykmxxi <ykmxxi97@gmail.com>
2022-10-26 22:54:12 +0900

    feat: (Problem1)게임 결과를 얻는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int DRAW = 0;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int EXCEPTIONS = -1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
@@ -45,5 +49,20 @@ class Problem1 {
         int score = 0;
         score = Math.max(getLeftPageMaximum(pageList), getRightPageMaximum(pageList));
         return score;
+    }
+
+    private static int getGameResult(List<Integer> pobi, List<Integer> crong) {
+        int pobiScore = getScore(pobi);
+        int crongScore = getScore(crong);
+        if (pobiScore > crongScore) {
+            return POBI_WIN;
+        }
+        if (pobiScore < crongScore) {
+            return CRONG_WIN;
+        }
+        if (pobiScore == crongScore) {
+            return DRAW;
+        }
+        return -1;
     }
 }
