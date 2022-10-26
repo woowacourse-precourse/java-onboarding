058647131b5568e64ec94fb1b1e1306960d2f41b
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 20:25:49 +0900

    화폐 단위로 최대 받을수 있는 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    //단위 배열
+    private static int[] units = {50000,10000,5000,1000,500,100,50,10,1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    //해당 단위로 최대 받을수 있는 크기 구하는 함수
+    private static int getCount(int money, int idx){
+        return money / units[idx];
     }
 }
