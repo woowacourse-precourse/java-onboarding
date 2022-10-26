de1ce21ba83dbbeb9ef2d4f832d7dc0c1c25f5c9
main <jang0929@naver.com>
2022-10-26 20:42:09 +0900

    Problem6 User class 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,6 +8,31 @@ import java.util.List;
  * 3. 두 자씩 map에 저장하는 함수
  */
 public class Problem6 {
+
+
+    /**
+     * 1. User Class 구현
+     */
+    static class User {
+
+        private String nickname;
+        private String email;
+
+        public String getNickname() {
+            return nickname;
+        }
+
+
+        public String getEmail() {
+            return email;
+        }
+
+        public User(String nickname, String email) {
+            this.nickname = nickname;
+            this.email = email;
+
+        }
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
