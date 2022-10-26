798f6f8d479e49d7c8d1865c303056296c5ca1b1
HwangSeokJin <ghkd3703@gmail.com>
2022-10-26 20:00:51 +0900

    숫자에 포함된 3, 6, 9 개수를 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,17 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int count(int num){
+        int result = 0;
+        while (num > 0){
+            int current = num % 10;
+
+            if (current == 3 || current == 6 || current == 9){
+                result += 1;
+            }
+            num /= 10;
+        }
+        return result;
+    }
 }
