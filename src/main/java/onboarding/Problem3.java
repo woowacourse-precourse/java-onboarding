16733f073979c4e63e60f55c33751ab50e10d5d6
skydreamer21 <skydreamer210@gmail.com>
2022-10-26 23:13:58 +0900

    [Feature] Problem3 - 1부터 10,000까지 10의 거듭제곱 단위로 박수 횟수 카운트 구현
    함수명 : getCountArray
    구한 카운트를 배열에 저장

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,9 +6,35 @@
 
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
+    static int[] count = new int[5];
+    static final int MAX_EXP = 4;
+    static final int TEN = 10;
+    static final int NOT_VISITED = -1;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static void getCountArray() {
+        initCountArray();
+        getClapCountOfExp10(MAX_EXP);
+    }
+
+    public static int getClapCountOfExp10 (int exp) {
+        if (count[exp] != NOT_VISITED) {
+            return count[exp];
+        }
+
+        count[exp] = getClapCountOfExp10(exp - 1) * TEN + (int)Math.pow(10,exp - 1) * 3;
+        return count[exp];
+    }
+
+    public static void initCountArray() {
+        Arrays.fill(count, NOT_VISITED);
+        count[0] = 0;
     }
 }
