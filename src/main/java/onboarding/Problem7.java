201c50b5451762ed35aa0a53927d75ac5b50856e
main <jang0929@naver.com>
2022-10-26 20:55:27 +0900

    Problem 7 Solution

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -39,6 +39,7 @@ public class Problem7 {
 
     /**
      * 3. 친구 관계일 경우 10점을 추가하는 함수
+     *
      * @param user
      */
 
@@ -60,14 +61,15 @@ public class Problem7 {
 
     /**
      * 4. 방문자일 경우 1점을 추가하는 함수
+     *
      * @param visitors
      */
 
     static void calcVisitors(List<String> visitors) {
         visitors.forEach(s -> {
-            if(points.containsKey(s)){
+            if (points.containsKey(s)) {
                 points.put(s, points.get(s) + 1);
-            }else{
+            } else {
                 points.put(s, 1);
             }
         });
@@ -92,5 +94,29 @@ public class Problem7 {
             friendsHashMap.get(f.get(0)).add(f.get(1));
             friendsHashMap.get(f.get(1)).add(f.get(0));
         });
+        calcFriends(user);
+        calcVisitors(visitors);
+
+        List<RecFriend> recFriendList = new ArrayList<>();
+        for (String name : points.keySet()) {
+            if (points.get(name) == 0) continue;
+            if (friendsHashMap.get(user).contains(name)) continue;
+            recFriendList.add(new RecFriend(name, points.get(name)));
+        }
+
+
+        Collections.sort(recFriendList, (a, b) -> {
+
+            if (!a.getScore().equals(b.getScore())) {
+                return -Integer.compare(a.getScore(), b.getScore());
+            }
+            return String.CASE_INSENSITIVE_ORDER.compare(a.getName(), b.getName());
+        });
+
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < Integer.min(recFriendList.size(), 5); i++) {
+            answer.add(recFriendList.get(i).name);
+        }
+        return answer;
     }
 }
