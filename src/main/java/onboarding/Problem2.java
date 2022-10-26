e8896d465b97357d966fe48302e8a451fba17a01
yg0585 <yg0585@naver.com>
2022-10-26 20:16:29 +0900

    구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+//-----기능-----
+//1. 중복 문자열 찾기
+//2. 해당 문자열 제거
+//3. 위 기능 반복
+
 public class Problem2 {
+
+    private static int dupStr(String s)
+    {
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (s.charAt(i) == s.charAt(i + 1))
+                return i;
+        }
+        return -1;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int idx;
+        while (true)
+        {
+            idx = dupStr(cryptogram);
+            if (idx == -1)
+                break;
+            cryptogram = cryptogram.substring(idx, idx + 1);
+        }
+        return cryptogram;
     }
 }
