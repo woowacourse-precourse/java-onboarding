7716a28cb269b3afbdf2ce9025002dc088e46a00
kevstevie <jianna442@gmail.com>
2022-10-26 18:37:10 +0900

    feat: add changrFrogWords

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,6 +10,16 @@ public class Problem4 {
         for (int i = 0; i < 26; i++) {
             alphabets.put( (char)(i + 'a'), (char)('z' - i));
         }
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            answer += Character.toString(changeFrogWord(alphabets, c));
+        }
         return answer;
+    }
+    static char changeFrogWord(Map<Character, Character> map, char ch){
+        if(map.containsKey(ch)){
+            return map.get(ch);
+        }
+        return ch;
     }
 }
