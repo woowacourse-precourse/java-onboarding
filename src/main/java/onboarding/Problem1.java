e6874368bde9b65f02b60ba501443bca16eb565a
Park Sungchul <try615@naver.com>
2022-10-26 19:44:56 +0900

    [feat] Problem1.java : 페이지를 각 자리 숫자로 분해하여 배열 저장

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,14 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
+    static int LEN_NUMBER_OF_DIGITS = 3;
+
     public static int solution(List<Integer> pobi, List<Integer> 현crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 
@@ -26,6 +30,14 @@ class Problem1 {
 
     private static int compareScore(int[] arr) {
         return Math.max(operMulti(arr), operPlus(arr));
+    }
+
+    private static int[] intToArr(int page) {
+        int[] arr = new int[LEN_NUMBER_OF_DIGITS];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (page % (int) Math.pow(10, i + 1)) / (int) Math.pow(10, i);
+        }
+        return arr;
     }
 
 }
