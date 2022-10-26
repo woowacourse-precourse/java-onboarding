ddd1409bfb0d6d5495cc7df3f3cdab443ec99ec9
sangho-jeon <jeonsangho1587@gmail.com>
2022-10-26 16:49:37 +0900

    feat      : problem1 add game decision method.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,15 +5,37 @@ import java.util.List;
 class Problem1 {
     static final int PAGE_MIN = 1;
     static final int PAGE_MAX = 399;
+    static final int POBI_WIN_CASE = 1;
+    static final int CRONG_WIN_CASE = 2;
+    static final int DRAW_CASE = 0;
+    static final int ERROR = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        if (isValid(pobi) && isValid(crong)) {
+            int pobiMaxScore = Math.max(getScore(pobi.get(0)), getScore(pobi.get(1)));
+            int crongMaxScore = Math.max(getScore(crong.get(0)), getScore(crong.get(1)));
+            return game(pobiMaxScore, crongMaxScore);
+        }
+        return ERROR;
+    }
+
+    public static int game(int pobiScore, int crongScore){
+        int flag = crongScore - pobiScore;
+        if(flag > 0){
+            return CRONG_WIN_CASE;
+        }
+        else if(flag < 0){
+            return POBI_WIN_CASE;
+        }
+        else
+            return DRAW_CASE;
     }
 
     public static boolean isValid(List<Integer> page) {
         int frontPage = page.get(0);
         int endPage = page.get(1);
-        if((PAGE_MIN < frontPage && frontPage < PAGE_MAX) && frontPage % 2 == 1){
+        if ((PAGE_MIN < frontPage && frontPage < PAGE_MAX) && frontPage % 2 == 1) {
             return endPage - frontPage == 1;
         }
         return false;
