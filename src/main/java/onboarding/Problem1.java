e68eff74a0ae705b1c6af47ada6913e985358cc8
Woong99 <dndry1072@naver.com>
2022-10-26 16:20:20 +0900

    feat: 문제 1 - 숫자의 각 자릿수를 곱하는 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,4 +17,12 @@ class Problem1 {
         return sum;
     }
 
+    private static int multiplyNumber(Integer number) {
+        int result = 1;
+        while (number > 0) {
+            result *= number % 10;
+            number /= 10;
+        }
+        return result;
+    }
 }
