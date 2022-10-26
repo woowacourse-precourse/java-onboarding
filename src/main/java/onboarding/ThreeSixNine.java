72b34b499934dc3f9359b1ad5d0a6098e88c560a
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-26 21:22:03 +0900

    feat(problem3): set project structure

---
+++ src/main/java/onboarding/ThreeSixNine.java
@@ -0,0 +1,14 @@
+package onboarding;
+
+public class ThreeSixNine {
+
+	private final int number;
+
+	public ThreeSixNine(int number) {
+		this.number = number;
+	}
+
+	public int run() {
+		return 0;
+	}
+}
