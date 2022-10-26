4e0cffe7eb8cf29a9fc5207ea8a53dec8ae33332
Namyunsuk <yunsuk0328@naver.com>
2022-10-27 01:00:47 +0900

    feat(buildList): 1 부터number 범위의 리스트를 초기화하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Problem3 {
+    public static List<Integer> buildList(int number){
+        List<Integer> list = new ArrayList<Integer>(number);
+        IntStream.rangeClosed(1, number).forEach(i -> list.add(i));
+        return list;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
