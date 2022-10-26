11ccf889b2816e36a36c6f4e2c851ef8815e86d7
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-26 21:41:06 +0900

    기능요구사항 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    // 기능요구사항
+    // TODO 예외를 처리하는 함수
+    // TODO 큰 값을 리턴하는 함수
+    // TODO pobi와 crong의 값을 비교하는 함수
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
