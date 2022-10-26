18e7cf87acdda0a3c3bab34aa86746305da79c24
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-26 20:43:58 +0900

    Feat: 구현 Problem3 count369함수 - 해당 숫자에 있는 3,6,9의 갯수를 합해서 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	private static int count369(int number) {
+		int count = 0;
+		while (number > 0) {
+			int units = number % 10;
+			if (units % 3 == 0 && units != 0) {
+				count += 1;
+			}
+			number /= 10;
+		}
+		return count;
+	}
+
+	public static int solution(int number) {
+		int answer = 0;
+		return answer;
+	}
 }
