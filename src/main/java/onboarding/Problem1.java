2fcd5bd1316c8f1ce4ed98e87093afda115f658d
noah.lee <rhkd2612@naver.com>
2022-10-27 05:50:08 +0900

    feat: getScoreOnPage method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -22,12 +22,27 @@ class Problem1 {
             return -1;
         }
 
+        int pobiHighScore = getScoreOnPage(pobi);
+        int crongHighScore = getScoreOnPage(crong);
+
+        if(pobiHighScore > crongHighScore)
+            answer = 1;
+        if(pobiHighScore < crongHighScore)
+            answer = 2;
+
         return answer;
     }
 
     private static void validatePage(final List<Integer> pages){
         if(pages.get(0) + 1 != pages.get(1))
             throw new IllegalArgumentException("연속되지 않은 페이지입니다.");
+    }
+
+    private static int getScoreOnPage(final List<Integer> pages){
+        int left = Math.max(plusPage(pages.get(0)),timesPage(pages.get(0)));
+        int right = Math.max(plusPage(pages.get(1)),timesPage(pages.get(1)));
+
+        return Math.max(left, right);
     }
 
     private static int plusPage(int page){
