bb91dab962a435f92ec75dd01c6197ecd9c00d99
JeonghoPark <pjhpjh0805@naver.com>
2022-10-27 08:38:08 +0900

    feat: 6번 입력 email과 name 리스트 생성 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+//        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        // 사용자 이름을 기반으로 2글자씩 substring정보를 담는 이중 list 생성
+        List<String> emailList = makeEmailList(forms);
+        List<String> nameList = makeNameList(forms);
+
+
         return answer;
+    }
+
+    private static List<String> makeNameList(List<List<String>> forms) {
+        List<String> nameList = new ArrayList<>();
+        for (List<String> form : forms) {
+            String name = form.get(1);
+            nameList.add(name);
+        }
+        return nameList;
+    }
+
+    private static List<String> makeEmailList(List<List<String>> forms) {
+        List<String> emailList = new ArrayList<>();
+        for (List<String> form : forms) {
+            String emailAddress = form.get(0);
+            emailList.add(emailAddress);
+        }
+        return emailList;
     }
 }
