c5b8bedc009cfa35bd63eba64af639ca9440d570
eastsage <dpwk8@naver.com>
2022-10-26 23:09:03 +0900

    refactor: solution내 조건문을 isError 함수로 분리
    solution 함수가 보기 어렵다고 생각되어 함수 내 조건문을 isError 함수로 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,48 +4,46 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        int incorrect = -1;
+        int answer = Integer.MIN_VALUE;
 
-        //잘못된 입력
-        if (pobi.get(0) == 1 || pobi.get(1) == 400 || crong.get(0) == 1 || crong.get(1) == 400) {
-            // 시작 면이나 마지막 면이 나오지 않도록 한다. ex) 1,2 / 399, 400
-            return incorrect;
-        } else if (pobi.get(0) % 2 == 0 || crong.get(0) % 2 == 0) {
-            // 왼쪽 페이지는 홀수이다.
-            return incorrect;
-        } else if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
-            // 리스트는 연속된 숫자이다.
-            return incorrect;
-        }
+        if (isError(pobi) || isError(crong))
+            return -1;
 
-        //조건 통과시 실행
-        int pobiScore = getScore(pobi);
-        int crongScore = getScore(crong);
-        if (pobiScore > crongScore) { // 포비 승 return 1
-            answer = 1;
-        } else if (pobiScore == crongScore) { //무승부 return 0
+        if (getScore(pobi) == getScore(crong))
             answer = 0;
-        } else answer = 2; // 크롱 승 return 2
+        else if (getScore(pobi) > getScore(crong))
+            answer = 1;
+        else if (getScore(pobi) < getScore(crong))
+            answer = 2;
 
         return answer;
     }
 
-    public static int getScore(List<Integer> list) {
+    static boolean isError(List<Integer> list) {
+        if (list.get(0) == 1 || list.get(1) == 400)
+            return true;
+        if (list.get(0) % 2 == 0)
+            return true;
+        if (list.get(1) - list.get(0) != 1)
+            return true;
+        return false;
+    }
+
+    static int getScore(List<Integer> list) {
         int max = Integer.MIN_VALUE;
 
         for (Integer quotient : list) { //list iter
             int remainder = 0; //나머지
             int sum = 0; //합
-            int multi = 1; //곱
+            int multiply = 1; //곱
             while (quotient != 0) {
                 remainder = quotient % 10;
                 quotient = quotient / 10;
                 sum += remainder; //자릿수의 합
-                multi = multi * remainder; // 자릿수의 곱
+                multiply = multiply * remainder; // 자릿수의 곱
             }
             max = Math.max(max, sum);
-            max = Math.max(max, multi);
+            max = Math.max(max, multiply);
         }
 
         return max;
