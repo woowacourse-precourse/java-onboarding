a7cd181bd07196daf067cc88d9f9605975b7df52
junho lee <junho1273@gmail.com>
2022-10-26 23:50:28 +0900

    Problem1 기능목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,14 @@ package onboarding;
 
 import java.util.List;
 
+// 기능목록
+// 1. 예외처리
+// 2. 자리수의 합과 곱 중 최댓값 구하기
+// 3. 크롱과 포비의 최댓값 비교하기
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
 }
