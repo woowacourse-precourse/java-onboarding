29f53e243bac72091fe28f9df962bfe19a3f0708
cpot5620 <=cpot5620@naver.com>
2022-10-27 00:23:20 +0900

    Implement checkInvalidInput()
    If user's input is invalid, the method return false.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
         return answer;
+    }
+
+    public static boolean checkInvalidInput(List<Integer> list){
+        int leftPage = list.get(0);
+        int rightPage = list.get(1);
+
+        if(rightPage - leftPage != 1)
+            return false;
+
+        if (leftPage%2 != 1)
+            return false;
+
+        if (rightPage %2 != 0)
+            return false;
+
+        return true;
     }
 }
