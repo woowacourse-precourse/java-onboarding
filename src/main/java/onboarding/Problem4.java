c7e26371747ca35673cd1502466ad4096a8c1e29
jkde7721 <jkde7721@naver.com>
2022-10-26 21:17:36 +0900

    [문제 4] 기능 요구 사항 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return convertToFrogWord(word);
+    }
+
+    private static String convertToFrogWord(String word) {
+        StringBuilder frogWordBuilder = new StringBuilder();
+        for(int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (isAlphabet(ch)) frogWordBuilder.append(convertToFrogChar(ch));
+            else frogWordBuilder.append(ch);
+        }
+        return frogWordBuilder.toString();
+    }
+
+    private static boolean isAlphabet(char ch) {
+        return ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z';
+    }
+
+    private static char convertToFrogChar(char ch) {
+        char firstAlphabet = isCapital(ch) ? 'A' : 'a';
+        char lastAlphabet = isCapital(ch) ? 'Z' : 'z';
+        return (char) (firstAlphabet + lastAlphabet - ch);
+    }
+
+    private static boolean isCapital(char ch) {
+        return ch >= 'A' && ch <= 'Z';
     }
 }
