37931b339701e54ce6b1dcdb801315df656e9501
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-26 16:46:28 +0900

    Problem1 - 값 추출 및 비교

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,48 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Collections.max;
+
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        List<Integer> calResultListByPobi = getResult(pobi);
+        List<Integer> calResultListByCrong = getResult(crong);
+        int answer = max(calResultListByPobi).compareTo(max(calResultListByCrong));
         return answer;
     }
+
+    private static List<Integer> getResult(List<Integer> participant) {
+        List<Integer> calResultList = new ArrayList<>();
+
+        for (Integer integer : participant) {
+            List<Integer> singleNumbers = getSingleNumbers(integer);
+            doCalculate(singleNumbers, calResultList);
+        }
+        return calResultList;
+    }
+
+    private static List<Integer> getSingleNumbers(Integer integer) {
+        List<Integer> valueList = new ArrayList<>();
+        int loof = integer.toString().length();
+        for (int i = 0; i < loof; i++) {
+            valueList.add(integer % 10);
+            integer /= 10;
+        }
+        return valueList;
+    }
+
+    private static void doCalculate(List<Integer> valueList, List<Integer> calResultList) {
+        int all_Plus = 0;
+        int all_multiply = 1;
+        for (Integer values : valueList) {
+            all_Plus += values;
+            all_multiply *= values;
+        }
+        calResultList.add(all_Plus);
+        calResultList.add(all_multiply);
+    }
+
 }
