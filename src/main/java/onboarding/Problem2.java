3860250237f04d34b43159a729f43f5d954c1189
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-10-26 20:05:48 +0900

    feat(Problem2) Todo List 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import onboarding.problem2.*;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
---
+++ src/test/java/onboarding/Problem2Test.java
@@ -0,0 +1,4 @@
+package onboarding;
+
+public class Problem2Test {
+}
