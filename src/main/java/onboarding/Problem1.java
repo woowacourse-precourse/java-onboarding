d133cb4ca4f7757dffca0cde6121e42033af98c6
woogi <dnrlwo11@gmail.com>
2022-10-26 21:13:24 +0900

    problem1 문제 분석

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        // 시작면과 마지막면일 경우 에러처리
+        // 각 자리 수를 더하는 메서드
+        // 각 자리 수를 곱하는 메서드
+        // 가장 큰 값을 리턴
         int answer = Integer.MAX_VALUE;
         return answer;
     }
