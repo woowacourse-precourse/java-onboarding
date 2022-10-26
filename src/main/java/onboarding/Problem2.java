d5384216bc7716b144a6761cee9c70850a0817da
jaehyeon <jhjh0022@naver.com>
2022-10-27 00:33:48 +0900

    다시 êê³í생각해서 풀어보자

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,19 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    public static String decoder(String phar){
+        String sub ="";
+        String dec = phar;
+
+        for(int i=0; i<phar.length()-1; i++ ){
+
+                if(phar.charAt(i) == phar.charAt(i+1)){
+                    sub += phar.charAt(i);
+                    sub += phar.charAt(i+1);
+                    dec.strip(sub);
+                    sub ="";
+                }
+            }
+        }
+    }
 }
