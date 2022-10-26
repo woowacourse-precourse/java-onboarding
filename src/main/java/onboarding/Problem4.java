663c77a8dfbb5f814c729b28d2abfe93765e5a84
dongwoo <shp05179@gmail.com>
2022-10-26 23:39:06 +0900

    ascii 코드 기반 알파벳 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i=0; i<word.length(); i++) {
+            int ascii_val = (int)word.charAt(i);
+            int new_alphabet = ascii_val;
+            if ((65<= ascii_val && ascii_val <=90)) {
+                new_alphabet = 155-ascii_val;
+            } else if ((97<= ascii_val && ascii_val <=122)) {
+                new_alphabet = 219-ascii_val;
+            }
+            answer += String.valueOf((char)new_alphabet);
+        }
         return answer;
     }
 }
