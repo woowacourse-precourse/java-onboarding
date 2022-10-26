358fee7452543094b38c1c249219722057bf9a45
TaemHam <tk4003950@gmail.com>
2022-10-27 00:21:26 +0900

    Feat: Implement hasValidLength

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class Problem2 {
 
     /*
-     * 문자열이 유효한 입력인지 확인하는 메서드입니다.
+     * 문자열이 유효한 입력인지 확인합니다.
      *
      * @param 입력으로 주어진 문자열입니다.
      * @return 문자열이 유효하면 참, 아니면 거짓을 반환합니다.
@@ -14,11 +14,20 @@ public class Problem2 {
         boolean valid =
             hasValidLength(cryptogram) &&
             isLowerCase(cryptogram);
-        return false;
+        return valid;
     }
 
+    /*
+     * 문자열의 길이가 1~1000인지 확인합니다.
+     *
+     * @param 입력으로 주어진 문자열입니다.
+     * @return 길이가 1~1000 사이라면 참, 아니면 거짓을 반환합니다.
+     * */
     private static boolean hasValidLength(String cryptogram) {
-        return false;
+        boolean valid =
+            cryptogram.length() > 0 &&
+            cryptogram.length() <= 1000;
+        return valid;
     }
 
     private static boolean isLowerCase(String cryptogram) {
