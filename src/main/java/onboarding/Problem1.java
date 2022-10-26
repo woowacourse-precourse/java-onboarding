216e88213f7c68b3d1955b3abf70973cdf8104b0
Jeeseob <jeeseob5761@gmail.com>
2022-10-26 23:38:11 +0900

    REFACTORING: problem1 - 자릿수 연산 함수 stream으로 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 class Problem1 {
 
-    /**
-     * @Method: Solution
-     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (checkPageException(pobi) && checkPageException(crong)) {
             // 각 점수 계산
@@ -21,12 +18,6 @@ class Problem1 {
         return -1;
     }
 
-    /**
-     * 1. 책을 임의로 펼친다.
-     * 6. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
-     * @Method: checkPageException
-     * 페이지 자체에 에러가 존재하는지 확인한다.
-     */
     private static boolean checkPageException(List<Integer> pages) {
         int right = pages.get(1);
         // 오른쪽 페이지가 2 < right < 400 인지 확인
@@ -41,15 +32,8 @@ class Problem1 {
         return false;
     }
 
-
-    /**
-     * 4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
-     * @Method: calcPoint
-     * 본인의 점수를 구한다.
-     */
     private static int calcPoint(List<Integer> pages) {
         int point = 0;
-        // 오른쪽 왼쪽 페이지 각각 계산한다.
         for(Integer page: pages) {
             List<Integer> digits = digitPage(page);
             int addPoint = addDigit(digits);
@@ -60,17 +44,6 @@ class Problem1 {
         return point;
     }
 
-
-    /**
-     * 2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
-     * 3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
-     */
-
-
-    /**
-     * @Method: digitPage
-     * 페이지 번호를 자릿수 별로 나눈다.
-     */
     private static List<Integer> digitPage(Integer num) {
         List<Integer> digits = new ArrayList<>();
         // 각 페이지 번호를 받아 자릿수 별로 나눈다.
@@ -78,44 +51,17 @@ class Problem1 {
             digits.add(num % 10);
             num /= 10;
         }
-        // List 형태로 return
         return digits;
     }
 
-
-    /**
-     * @Method: addDigit
-     * 각 자릿수를 기반으로 더한 값을 구한다.
-     */
     private static int addDigit(List<Integer> digits) {
-        int answer = 0;
-        // 각 자릿수를 더한다.
-        for(Integer digit : digits) {
-            answer += digit;
-        }
-        return answer;
+        return digits.stream().mapToInt(Integer::intValue).sum();
     }
 
-
-    /**
-     * @Method: mutiplyDigit
-     * 각 자릿수를 기반으로 곱한값을 구한다.
-     */
     private static int mutiplyDigit(List<Integer> digits) {
-        int answer = 1;
-        // 각 자릿수를 곱한다.
-        for(Integer digit : digits) {
-            answer *= digit;
-        }
-        return answer;
+        return digits.stream().reduce(1, (a, b) -> a * b);
     }
 
-
-    /**
-     * 5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
-     * @Method: calcWinner
-     * 점수를 비교하여, 알맞은 값을 return한다.
-     */
     private static int calcWinner(int poingOfPobi, int pointOfCrong) {
         // 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0을 return
         if (poingOfPobi > pointOfCrong) {
