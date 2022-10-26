22b0b096529db4dd9aea857d9fd6546057006d41
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 22:09:09 +0900

    feat(Problem7.java): initGraph() 전체 친구 관계 그래프 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static Map<String, LinkedList<String>> initGraph(List<List<String>> friends){
+        Map<String, LinkedList<String>> graph=new HashMap<>();
+        friends.forEach(o->{
+            String person1 = o.get(0);
+            String person2 = o.get(1);
+
+            LinkedList<String> graph1 = graph.getOrDefault(person1, new LinkedList<>());
+            LinkedList<String> graph2 = graph.getOrDefault(person2, new LinkedList<>());
+
+            graph1.add(person2);
+            graph2.add(person1);
+
+            graph.put(person1, graph1);
+            graph.put(person2, graph2);
+        });
+
+        return graph;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
