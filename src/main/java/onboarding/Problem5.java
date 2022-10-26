682f1c00ca4df3edde7bd2712f24b66dc375a5bb
aksmf1442 <aksmf1442@naver.com>
2022-10-27 02:48:13 +0900

    feat: Problem5(완료) 각 단위별 화페의 개수를 구하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,14 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    static List<Integer> moneyPerUnitCounts = new ArrayList<>();
-
     public static List<Integer> solution(int money) {
-
-        return moneyPerUnitCounts;
+        List<Integer> monetaryUnitCounts = new ArrayList<>();
+        findMonetaryUnitCounts(monetaryUnitCounts, money);
+        return monetaryUnitCounts;
     }
 
-    private static int giveChange(int money, int moneyPerUnit) {
-        return money % moneyPerUnit;
+    private static void findMonetaryUnitCounts(
+        List<Integer> monetaryUnitCounts, int money) {
+        int[] monetaryUnits = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int monetaryUnit : monetaryUnits) {
+            int monetaryUnitCount = countMonetaryUnit(money, monetaryUnit);
+            money = giveChange(money, monetaryUnit);
+            monetaryUnitCounts.add(monetaryUnitCount);
+        }
+    }
+
+    private static int countMonetaryUnit(int money, int monetaryUnit) {
+        return money / monetaryUnit;
+    }
+
+    private static int giveChange(int money, int monetaryUnit) {
+        return money % monetaryUnit;
     }
 }
