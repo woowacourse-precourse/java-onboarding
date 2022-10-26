3ba1393d982738e1360b2c0f49a83f7ac1c06907
yj2980 <adore2100@naver.com>
2022-10-27 02:27:58 +0900

    feat : 사용자와 함께 하는 친구 수만큼 점수 부여하는 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static Map<String, Integer> findknowFriendsPoint(List<String> knowFriendsList)
+    {
+        int point = 0;
+        Map<String, Integer> knowFriends = new HashMap();
+
+        for (int i = 0; i < knowFriendsList.size(); i++)
+        {
+            if (knowFriends.containsKey(knowFriendsList.get(i)))
+                point += 10;
+            knowFriends.put(knowFriendsList.get(i), point);
+        }
+        return knowFriends;
     }
 }
