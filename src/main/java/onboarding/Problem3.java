1ee902b68fc028e3b6d1d1cae54e9b71f7c622b3
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 20:06:56 +0900

    feat(Problem3.java): 범위 내 모든 숫자의 3, 6, 9 갯수합 출력

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,7 +8,10 @@ public class Problem3 {
     }
 
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int sum=0;
+        for(int i=1; i<=number; i++)
+            sum+=count369(i);
+
+        return sum;
     }
 }
