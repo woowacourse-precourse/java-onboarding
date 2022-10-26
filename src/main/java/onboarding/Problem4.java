9a6d43526e0b5ee1854c43465c099cb25850a967
starwook <star.wookk@gmail.com>
2022-10-26 23:26:37 +0900

    add return char method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,18 +2,27 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = reString(word);
         return answer;
     }
     public static String reString(String s){
         String str = "";
         for(int i=0;i<s.length();i++) {
-            str += changeChar(s.charAt(i));
+            str += reChar(s.charAt(i));
         }
         return str;
+    }
+    public static char reChar(char c){
+        if( c==' '){
+            return c;
+        }
+        else{
+            return changeChar(c);
+        }
     }
     public static char changeChar(char c){
 
         return c;
     }
+
 }
