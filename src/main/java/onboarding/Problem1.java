c42621facaca18acc08ce3eb9c621fa964c32b96
ensk26 <tjensk26@gmail.com>
2022-10-27 01:25:04 +0900

    docs: 기능 목록 구성 ,problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    //페이지 번호의 각 자리수를 모두 더하기, 모두 빼기 한 가장 큰 수 반환
+    static int getMaxPageOperationValue(int num) {
+        return -1;
+    }
+
+    //잘못된 페이지인지 확인
+    static boolean isWrongPage(int leftPage, int rightPage) {
+        return true;
     }
 }
