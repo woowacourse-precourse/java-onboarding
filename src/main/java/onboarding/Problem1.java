4d1a1a90402ccb0988ee4861b1d1ceed5ef7c595
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-26 20:30:25 +0900

    feat :: 왼쪽 페이지 번호의 각 자리 숫자를 더한 값과 곱한 값을 각각 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    public static void main(String[] args) {
+        List<Integer> pobi = new ArrayList<>();
+        pobi.add(97);
+        pobi.add(98);
+
+        List<Integer> crong = new ArrayList<>();
+        crong.add(197);
+        crong.add(198);
+
+        System.out.println(solution(pobi, crong));
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int max = 0;
+
+        for (int page : pobi) {
+            int sum = 0;
+            int multi = 1;
+
+            while (page != 0) {
+                int result = page % 10;
+                sum += result;
+                multi *= result;
+                page /= 10;
+            }
+
+        }
+
+        return 0;
     }
 }
