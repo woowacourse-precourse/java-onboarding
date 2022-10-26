fcbfad44cf16c4bed9fabef2f670bbb6a435075f
kevstevie <jianna442@gmail.com>
2022-10-26 18:33:31 +0900

    feat: add alphabets Map

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        Map<Character, Character> alphabets = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            alphabets.put( (char)(i + 'a'), (char)('z' - i));
+        }
         return answer;
     }
 }
