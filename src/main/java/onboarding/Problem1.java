ca1a85cb85bbb2e8943a4358f13edb6c09abe9d7
ym2629 <dydals2629@naver.com>
2022-10-26 14:14:02 +0900

    Problem1 modify and Probelm2 solve

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,10 +5,11 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        answer = selectRes(pobi,crong);
         return answer;
     }
     
-    int digSum(int num){
+    static int digSum(int num){
     	int res = 0;
     	while (num > 0){
     		res += num % 10;
@@ -17,7 +18,7 @@ class Problem1 {
     	return res;
     }
     
-    int digMul(int num) {
+    static int digMul(int num) {
     	int res = 1;
     	while (num > 0)
     	{
@@ -27,16 +28,33 @@ class Problem1 {
     	return res;
     }
     
-    int selectNum(List<Integer> arr) {
+    static int selectNum(List<Integer> arr) {
     	return Math.max(Math.max(digSum(arr.get(0)),digMul(arr.get(0))),Math.max(digSum(arr.get(1)),digMul(arr.get(1))));
     }
     
-    boolean checkErr(List<Integer> arr) {
+    static boolean checkErr(List<Integer> arr) {
     	if (arr.get(0) > arr.get(1) || arr.get(0) < 0 || arr.get(1) > 400 || arr.get(1) - arr.get(0) != 1){
     		return false;
     	}else {
     		return true;
     	}
     }
-    
+    public static int selectRes(List<Integer> arr1, List<Integer> arr2) {
+    	if (checkErr(arr1) && checkErr(arr2)){
+    		int pobiN = selectNum(arr1);
+        	int crongN = selectNum(arr2);
+        	
+        	if(pobiN == crongN) {
+        		return 0;
+        	}
+        	else if(pobiN > crongN){
+        		return 1;
+        	}
+        	else {
+        		return 2;
+        	}
+    	}else {
+    		return -1;
+    	}
+    }
 }
