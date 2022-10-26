e21498328ea4174b356225e86b31ddfce9f95e9a
Park shinyeong <syngh503@gmail.com>
2022-10-26 21:57:25 +0900

    Feat/각 자리수 구하기 각 자리수를 구하는 getEachDigit 함수를 구현하였습니다. 페이지 값을 받아서 정수 리스트를 리턴합니다.
    Relate to: #1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
+
+
+
         return answer;
     }
+
+    // 각 자리수를 구한다.
+    public static List<Integer> getEachDigit(int page) {
+        List<Integer> pageNum = new ArrayList<>();
+        while(page > 0){
+            pageNum.add(page % 10);
+            page /= 10;
+        }
+        return pageNum;
+    }
+
+    // 각 자리수의 합을 구한다.
+
+
+    // 구한 값 중 최댓값을 구한다.
 }
