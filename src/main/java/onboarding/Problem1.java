d2329747485c7b949863e2d9f902ce15e8729c15
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-26 21:49:45 +0900

    예외처리메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,13 @@ class Problem1 {
 
     // 기능요구사항
     // TODO 예외를 처리하는 함수
+    static boolean errorCheck(List<Integer> error) {
+        if (error.get(0) == null || error.get(1) == null) return true;
+        if (!((error.get(0) <=400 && error.get(0) >= 1) || (error.get(1) <=400 && error.get(1) >= 1))) return true;
+        if (error.get(1) - error.get(0) != 1) return true;
+        return false;
+    }
+
     // TODO 큰 값을 리턴하는 함수
     // TODO pobi와 crong의 값을 비교하는 함수
 
