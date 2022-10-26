d3db316900a72f968151516d7357453bf4846d75
main <jang0929@naver.com>
2022-10-26 20:32:00 +0900

    Problem4 해당 문자를 변환하는 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,6 +9,25 @@ package onboarding;
 public class Problem4 {
 
 
+    static String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static String lower = "abcdefghijklmnopqrstuvwxyz";
+
+    /**
+     * 1. 해당 문자를 변환하는 함수
+     * @param x
+     * @return 해당되는 문자열로 변환
+     */
+    static char translate(char x) {
+        char res = x;
+        if (Character.isLowerCase(x)) {
+            res = lower.charAt(25 - x + 'a');
+        } else if (Character.isUpperCase(x)) {
+            res = upper.charAt(25 - x + 'A');
+        }
+        return res;
+    }
+
+
     public static String solution(String word) {
         String answer = "";
         return answer;
