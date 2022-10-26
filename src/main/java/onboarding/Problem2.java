bd39a3beb4dd08f25126580d4fd0a4a83ae906cd
aksmf1442 <aksmf1442@naver.com>
2022-10-26 18:46:57 +0900

    feat: Problem2 ->  문제 풀이 순서 및 문제 목표 주석 달기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,17 @@
 package onboarding;
 
+/**
+ * goal: 중복되는 문자열을 제거하여 암호를 해독하고 이를 리턴
+ */
+
+/**
+ * 문제 풀이 순서
+ * 1. 먼저 중복된 단어의 인덱스를 찾아서 set에 넣어준다.
+ * 2. set에 포함된 인덱스를 반대로 정렬하고 해당 인덱스를 통해서 cryptogram의 중복 문자열을 지워준다.
+ * 3. 중복 문자열이 나오면 재귀적으로 메서드를 반복하여 다시 중복이 있는지 확인한다.
+ * 4. 중복 문자열이 없다면 해당 cryptogram을 리턴해준다.
+ */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
