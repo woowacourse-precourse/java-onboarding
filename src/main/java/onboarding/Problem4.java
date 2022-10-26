0566cc6b9802744cc0898355bf72f977abe0d0ab
K-jun98 <alap_u@naver.com>
2022-10-26 23:34:23 +0900

    feat - 문자변환, 단어 변환 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return changeWord(word);
+    }
+
+
+    public static String changeWord(String word) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char ch : word.toCharArray()) {
+            stringBuilder.append(convert(ch));
+        }
+        return String.valueOf(stringBuilder);
+    }
+
+    private static char convert(char ch) {
+        if (ch >= 'a' && 'z' >= ch) {
+            return (char) (97 + (122 - ch));
+        } else if (ch >= 'A' && 'Z' >= ch) {
+            return (char) (65 + (90 - ch));
+        }
+        return ch;
     }
 }
