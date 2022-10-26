91a53a927879022020d6507351850c4cd47fd1d3
jini_11 <jieun21124@naver.com>
2022-10-26 23:50:38 +0900

    feat: 문제2 중복 문자를 삭제하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,7 +3,29 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        answer = decodingText(cryptogram);
+
         return answer;
+    }
+
+    public static String decodingText(String cryptogram) {
+        while (true) {
+            int start = findSame(cryptogram);
+            int end = start;
+
+            if (start == -1) {
+                return cryptogram;
+            }
+
+            for (end = start + 1; end < cryptogram.length(); end++) {
+                if (cryptogram.charAt(start) != cryptogram.charAt(end)) {
+                    break;
+                }
+            }
+
+            cryptogram = cryptogram.substring(0, start) + cryptogram.substring(end, cryptogram.length());
+        }
     }
 
     public static int findSame(String cryptogram) {
