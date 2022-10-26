a8bdde91bbfafe37152ef63ba95b5fb5db0812e1
yj2980 <adore2100@naver.com>
2022-10-26 21:00:24 +0900

    feat : 자리수를 구해 리스트에 저장하는 함수 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,22 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    //자리수 구분해 리스트에 넣음
+    public static List<Integer> digit(int money)
+    {
+        List<Integer> digitNumber = new ArrayList<>();
+
+        while (money >= 0)
+        {
+            digitNumber.get(money % 10);
+            money /= 10;
+        }
+        return digitNumber;
+    }
+    public static void divisionMoney(int money)
+    {
+
     }
 }
