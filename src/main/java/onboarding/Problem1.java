29f118a3bd67ed12de81cd8def202a34429b60f8
Kuyho Chung <kchung1995@gmail.com>
2022-10-26 23:30:54 +0900

    온보딩 - 문제 1 - isException 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    /**
+     * 두 페이지 수가 연속되었는지 확인한다.
+     * @param input
+     * @return
+     */
+    private static boolean isException(List<Integer> input) {
+        if (input.get(0) % 2 != 1 || input.get(1) % 2 != 0) return true;
+        if (input.get(1) - input.get(0) != 1) return true;
+        if (input.get(0) <= 1 || input.get(1) >= 400) return true;
+        return false;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
