9c5002b67d4d620508a86faa074e9fdbfc2d9636
yj2980 <adore2100@naver.com>
2022-10-26 23:22:39 +0900

    fect : 닉네임이 같은 유저가 있는지 확인하고 저장하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,7 +11,24 @@ public class Problem6 {
     }
 
     //닉네임이 같은 유저가 있는지 확인
- 
+    public static void userCheck(List<String> nickname)
+    {
+        List<Integer> userMemo = new ArrayList<>();
+
+        for (int i = 0; i < nickname.size() - 1; i++)
+        {
+            //한 단어라도 포함되어 있는 경우
+            if (nickname.get(i).contains(nickname.get(i + 1)))
+            {
+                //중복인 경우
+                if (isDuplicate(nickname.get(i), nickname.get(i + 1)))
+                {
+                    userMemo.add(i);
+                    userMemo.add(i + 1);
+                }
+            }
+        }
+    }
 
     //연속적으로 닉네임이 포함되어 있는지 확인
     public static boolean isDuplicate(String first, String second)
