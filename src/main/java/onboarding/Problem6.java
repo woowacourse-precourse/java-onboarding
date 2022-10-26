65b8f7af18fa28e5b53540b74b64bfade530e126
hum02 <ehdgma02@gmail.com>
2022-10-26 23:25:24 +0900

    feat:P6 같은 연속 두글자 포함한닉네임 찾기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Set<String> twoChars = new HashSet<>();
+        Set<String> duplicated = new HashSet<>();
+
+        for(List<String> info:forms) {
+            String nickName = info.get(1);
+
+            for(String twoChar :twoChars) {
+                if(nickName.contains(twoChar)) {
+                    duplicated.add(twoChar);
+                }
+            }
+
+            for(int i=0; i<nickName.length()-1; i++) {
+                twoChars.add( nickName.substring(i,i+2) );
+            }
+        }
+
+        for(List<String> info:forms) {
+            for(String dupString: duplicated) {
+                if (info.get(1).contains(dupString)) {
+                    answer.add(info.get(0));
+                }
+            }
+        }
+
+        Collections.sort(answer);
         return answer;
     }
 }
