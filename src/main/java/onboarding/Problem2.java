2f43cc85100315a4a0e4238f6db83049ffb284f7
jonghun <jonghuncu@gmail.com>
2022-10-26 18:26:35 +0900

    Feat : 문제 2번 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,41 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        answer = overlap(cryptogram);
         return answer;
+    }
+
+    public static String overlap(String s) {
+        Stack<String> st = new Stack<>();
+        ArrayList<Character> sLst = new ArrayList<>();
+
+
+        for(int i = 0; i < s.length(); i++) {
+            char currentCharacter = s.charAt(i);//getting the character at current index
+            sLst.add(currentCharacter);//adding the character to the list
+        }
+        for(int i=0; i<s.length(); i++) {
+            if(st.size() == 0) {
+                st.push(String.valueOf(sLst.get(i)));
+            } else {
+                String c1 = st.peek();
+                Character c11 = c1.charAt(0);
+                Character c2 = sLst.get(i);
+                if(c11 != c2) {
+                    st.push(String.valueOf(sLst.get(i)));
+                }
+                else {
+                    st.pop();
+                }
+            }
+
+        }
+
+        return String.join("", st);
     }
 }
