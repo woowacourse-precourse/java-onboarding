683a5a1f4543f8c57206b67a03c9462eebd6e2ec
ensk26 <tjensk26@gmail.com>
2022-10-27 03:05:17 +0900

    docs: 기능 목록 구성, problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
         String answer = "answer";
         return answer;
     }
+
+    //중복 문자 제거
+    static String getRemoveDuplicateWord(String word) {
+
+        StringBuffer resultWord = new StringBuffer();
+        return resultWord.toString();
+    }
+
 }
