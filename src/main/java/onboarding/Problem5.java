2f0b1cbcdc5238d0731307bc6e7acfcfccc2479c
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-26 22:05:10 +0900

    Feat: 구현 Problem5 moneyCount함수 구현 - 가진 돈이 돈 단위에 대해 몇 개 나오는지 구하는 함수

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+	private static int moneyCount(int myMoney, int moneyType) {
+		return myMoney / moneyType;
+	}
+
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = Collections.emptyList();
+		int[] moneyType = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+		return answer;
+	}
 }
