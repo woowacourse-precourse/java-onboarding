5d33dcc9527b0982a4f457eaa1950ab86d8c5f81
Kayoung <rkdkdudjd@gmail.com>
2022-10-27 02:09:58 +0900

    3번: solution 메서드
    - 1부터 number까지 for loop 순회
    - 일의 자리부터 3, 6. 9가 나오는 횟수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 3; i <= number; i++) {
+            int j = i;
+            do {
+                switch (j % 10) {
+                    case 3:
+                    case 6:
+                    case 9:
+                        answer++;
+                }
+                j /= 10;
+            } while (j != 0);
+        }
         return answer;
     }
 }
