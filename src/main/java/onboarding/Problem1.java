f66d9e33f62720aa70e36b5c19c88a60a1d1c538
chj <bluedog129@gmail.com>
2022-10-27 01:53:54 +0900

    페이지 연산 처리 기능(각 자릿수 plus,mul)구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static int plusPlaceValue(int page) {
+        int result = 0;
+        while(page > 0) {
+            result += page % 10;
+            page /= 10;
+        }
+
+        return result;
+    }
+    private static int multiplyPlaceValue(int page) {
+        int result = 1;
+        while (page > 0) {
+            result *= page % 10;
+            page /= 10;
+        }
+        return result;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
