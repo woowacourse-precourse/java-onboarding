f973d5aa245c653f79c753b7cda94209237277f4
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-27 00:49:03 +0900

    feat : 반환되는 화폐 개수 저장 및 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Map;
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
 
         final int[] koreanMoneyUnits = new int[] {
             50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
