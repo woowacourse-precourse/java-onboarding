74950625a7c68a2f1cc1540582603a076905b653
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 17:31:41 +0900

    페이지 번호의 자릿수를 곱하거나 더하여 큰 수를 정하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,30 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiLeftScore;
+        int crongLeftScore;
+        int pobiRightScore;
+        int crongRightScore;
+        int answer;
+
+        return -1;
+    }
+
+    public int howManyScore(int num) {
+        return Math.max(plusDigits(num), multiFlyDigits(num));
+    }
+
+    public int plusDigits(int num) {
+        if (num >= 100) {
+            return num / 100 + (num % 100 + 1)/10 - 1 + num % 10;
+        }
+        return num / 10 + num % 10;
+    }
+
+    public int multiFlyDigits(int num) {
+        if (num >= 100) {
+            return num / 100 * ((num % 100 + 1)/10 - 1) * num % 10;
+        }
+        return (num / 10) * (num % 10);
     }
 }
