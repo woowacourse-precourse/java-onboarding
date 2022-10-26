a8a9027450c84c0c803133efed8859a943b21d60
seokjin <mm8678@g.yju.ac.kr>
2022-10-26 22:45:46 +0900

    refactor(Problem3.java): 로직 메서드로 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,19 +1,23 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            int temp = i;
-            while (temp > 0) {
-                int temp2 = temp % 10;
-                if (temp2 != 0 && temp2 % 3 == 0) {
-                    answer++;
-                }
-                temp /= 10;
-            }
-        }
+	public static int solution(int number) {
+		int answer = 0;
+		for (int i = 1; i <= number; i++) {
+			answer += get369(i);
+		}
+		return answer;
+	}
 
-        return answer;
-    }
+	public static int get369(int number) {
+		int result = 0;
+		while (number > 0) {
+			int temp = number % 10;
+			if (temp != 0 && temp % 3 == 0) {
+				result++;
+			}
+			number /= 10;
+		}
+		return result;
+	}
 }
