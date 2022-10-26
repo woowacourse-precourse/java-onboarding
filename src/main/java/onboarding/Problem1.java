786eabe3b3daf783ba350d31d1be2d88e76fdbfb
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-27 00:25:13 +0900

    [문제1] - 제공된 페이지가 유효한지 판단하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean validator(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        return leftPage > 0 && leftPage < 400 && rightPage > 1 && rightPage <= 400 && rightPage - leftPage == 1;
+    }
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
