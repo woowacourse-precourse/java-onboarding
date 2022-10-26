ea34835ec4a03ff306c1a27654416538c4fedd95
jjmin9797 <jjmin9797@naver.com>
2022-10-26 23:42:21 +0900

    Test Push

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.List;
 
+//test
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
