73269b525c4daaed28f48a30510212ca522ff2cf
박준용 <jo101044@naver.com>
2022-10-26 21:00:17 +0900

    문제4 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        // 대문자 'A' 와 'Z' 의 아스키코드를 더한다.
+        char Upper = 'A'+'Z';
+        // 소문자 'a' 와 'z' 의 아스키코드를 더한다.
+        char lower = 'a'+'z';
+
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i);
+
+            //문자를 바꾸어 준다.
+            if ('A' <= c && c <= 'Z') sb.append((char)(Upper - c));
+            else if ('a' <= c && c <= 'z') sb.append((char)(lower - c));
+            else sb.append(c);
+        }
+
+        return sb.toString();
     }
 }
