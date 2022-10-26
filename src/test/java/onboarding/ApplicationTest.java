9664b284f1ed8fec6bb7b8f018ed2b04f0c0c649
JunH <tjdtls690@gmail.com>
2022-10-26 17:01:22 +0900

    :white_check_mark: test : 테스트 케이스 작성

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -17,7 +17,7 @@ class ApplicationTest {
             int result = 0;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
-
+    
         @Test
         void case2() {
             List<Integer> pobi = List.of(131, 132);
@@ -25,12 +25,76 @@ class ApplicationTest {
             int result = 1;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
-
+    
         @Test
         void case3() {
             List<Integer> pobi = List.of(99, 102);
             List<Integer> crong = List.of(211, 212);
             int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case4() {
+            List<Integer> pobi = List.of(99, 98);
+            List<Integer> crong = List.of(211, 212);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case5() {
+            List<Integer> pobi = List.of(98, 99);
+            List<Integer> crong = List.of(211, 212);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case6() {
+            List<Integer> pobi = List.of(211, 212);
+            List<Integer> crong = List.of(99, 102);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case7() {
+            List<Integer> pobi = List.of(211, 212);
+            List<Integer> crong = List.of(99, 98);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case8() {
+            List<Integer> pobi = List.of(211, 212);
+            List<Integer> crong = List.of(98, 99);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case9() {
+            List<Integer> pobi = List.of(1, 2);
+            List<Integer> crong = List.of(11, 12);
+            int result = 2;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case10() {
+            List<Integer> pobi = List.of(11, 12);
+            List<Integer> crong = List.of(11, 12);
+            int result = 0;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    
+        @Test
+        void case11() {
+            List<Integer> pobi = List.of(1, 2);
+            List<Integer> crong = List.of(399, 400);
+            int result = 2;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
     }
