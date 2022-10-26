c2572ea09198b0b643f24c1715b7ae02e227f0b5
JunH <tjdtls690@gmail.com>
2022-10-26 17:03:06 +0900

    :sparkles: feat : 해당 숫자의 자릿수 합 구하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+    
+    private static int sum(final int number) {
+        return Arrays.stream(split(number))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+    
+    private static String[] split(final int number) {
+        return String.valueOf(number).split("");
     }
 }
