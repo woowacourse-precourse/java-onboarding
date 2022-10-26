0954fe6e673ab0b14430e273285768d3b8f9cc91
이한솔 <hansol8701@naver.com>
2022-10-26 23:46:02 +0900

    feat: 책 펼치기 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,22 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static void openBook(List<Integer> pages) {
+        int randNum;
+        for (int i = 0; i <= 1; i++) {
+            randNum = pages.get(i);
+            while (randNum == 1 || randNum == 400) {
+                randNum = (int) (Math.random() * (399 - 2 + 1) + 2);
+            }
+            pages.set(i, randNum);
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        openBook(pobi);
+        openBook(crong);
         return answer;
     }
 }
