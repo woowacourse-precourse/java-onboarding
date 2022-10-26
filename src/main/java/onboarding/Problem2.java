e8db19de0f61ef70621863a38256ff2a5954fb99
sunwng <sunwgna@gmail.com>
2022-10-26 16:46:02 +0900

    [problem_2] 필요 기능, 예외 사항 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
+import java.util.*;
 
 public class Problem2 {
+    public static boolean check = true;
+
+    public static String decodeString(String strIn) {
+
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+
+
         return answer;
     }
 }
