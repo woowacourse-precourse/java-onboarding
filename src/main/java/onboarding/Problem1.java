42bfa84308f6f01c58f358d5022f7fa368596724
rhqudco <rhqudco1204@naver.com>
2022-10-26 21:08:00 +0900

    FEAT : Pobi And Crong index Compare

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,7 @@ import java.util.Random;
 * 1. 결과 비교를 위한 변수 생성.
 * 2. pobi와 crong의 index에 접근해서 index 값 잘라서 더하기, 곱하기 후 1번에서 생성한 변수에 저장.
 * 3. 값 비교 후 answer에 요구 사항 조건에 따라 값 저장.
+* 4. 예외 사항
 * */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -26,6 +27,34 @@ class Problem1 {
         int crongCompareLeft = 0;
         int crongCompareRight = 0;
         int crongResult = 0;
+
+        for (int i = 0; i < pobi.size(); i++) {
+            String getPobiIndex = Integer.toString(pobi.get(i));
+            for (int j = 0; j < getPobiIndex.length(); j++) {
+                int pobiTemp = Character.getNumericValue(getPobiIndex.charAt(j));
+                if (i == 0) {
+                    pobiSumLeft += pobiTemp;
+                    pobiMulLeft *= pobiTemp;
+                } else {
+                    pobiSumRight += pobiTemp;
+                    pobiMulRight *= pobiTemp;
+                }
+            }
+        }
+
+        for (int i = 0; i < crong.size(); i++) {
+            String getCrongIndex = Integer.toString(crong.get(i));
+            for (int j = 0; j < getCrongIndex.length(); j++) {
+                int crongTemp = Character.getNumericValue(getCrongIndex.charAt(j));
+                if (i == 0) {
+                    crongSumLeft += crongTemp;
+                    crongMulLeft *= crongTemp;
+                } else {
+                    crongSumRight += crongTemp;
+                    crongMulRight *= crongTemp;
+                }
+            }
+        }
 
         int answer = Integer.MAX_VALUE;
 
