15a6d911db3f2b8039b7b8af255a315985f36a11
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-27 00:00:15 +0900

    feat(Problem1): 예외상황 판별 기능 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,8 +8,8 @@ import java.util.List;
 //  a. 왼쪽페이지!=홀수
 //  b. 오른쪽페이지!=짝수
 //  c. 오른쪽페이지-왼쪽페이지!=1
-//  d. 페이지<1 (a,b,c 조건때문에 해당될 수 없음)
-//  e. 페이지>400
+//  d. 페이지<1 (위의 조건때문에 해당될 수 없음)
+//  e. 페이지>400 (위의 조건때문에 left>400에만 해당돼도 예외)
 // 2. 각각의 max num 구함 (각자의 점수)
 // 3. 점수 비교
 
@@ -45,21 +45,30 @@ class Problem1 {
         return answer;
     }
 
-    // 예외상황 판별
+    // 1. 예외상황 판별
     public static boolean isExcption(int left, int right) {
+        // 1-a. 왼쪽페이지!=홀수
         if (left % 2 != 1) {
             return true;
         }
-        if (right % 2 != 0) {
+        // 1-b. 오른쪽페이지!=짝수
+        else if (right % 2 != 0) {
             return true;
         }
-        if (right - left != 1) {
+        // 1-c. 오른쪽페이지-왼쪽페이지!=1
+        else if (right - left != 1) {
             return true;
         }
-        return false;
+        // 1-d. 페이지<1 (위의 조건때문에 해당될 수 없음)
+        // 1-e. 페이지>400 (위의 조건때문에 left>400에만 해당돼도 예외)
+        else if (left > 400) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    // max num 구함
+    // 2. max num 구함 (각자의 점수)
     public static int findMaxNum(int left, int right) {
         int maxL = 0;   //left max
         int maxR = 0;   //right max
