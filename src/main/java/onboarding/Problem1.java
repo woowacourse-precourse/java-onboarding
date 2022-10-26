cece7dbefbaf53b38a5f93c6eb628c2634f2d762
meunrud <y3a0522@gmail.com>
2022-10-27 02:03:17 +0900

    페이지 번호의 각 자리 숫자 더하기(sum), 곱하기(multi) 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,23 +7,42 @@ import java.util.Random;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        pobi = new ArrayList<>();
-        crong = new ArrayList<>();
+        Integer[] page  = {randomNum(),randomNum()+1};
+        pobi = new ArrayList<>(Arrays.asList(page));
+        crong = new ArrayList<>(Arrays.asList(page));
 
-        pobi.add(randomPage());
-        pobi.add(randomPage()+1);
-
-        crong.add(randomPage());
-        crong.add(randomPage()+1);
 
         int answer = Integer.MAX_VALUE;
         return answer;
     }
 
-    public static Integer randomPage(){
+    public static Integer randomNum(){
         Random random = new Random();
-        Integer randomPage  = 0;
-        randomPage = 2*random.nextInt(401)+1;
-        return randomPage;
+        Integer randomNum  = 0;
+        randomNum = 2*random.nextInt(401)+1;
+        return randomNum;
     }
+
+    public static int sum(Integer a){
+        int sum = 0;
+        while(a != 0){
+           sum += a%10;
+           a /= 10;
+        }return sum;
+    }
+
+    public static int multi(Integer a){
+        String str = String.valueOf(a);
+        int [] num = new int[str.length()];
+        Integer multi = 0;
+        for(int i = 0; i<str.length(); i++){
+            for(int k = 0; k<str.length(); k++){
+                String b = str.substring(i,i+1);
+               num [k]= Integer.parseInt(b);
+               multi = num[k] * num[k+1];
+            }
+        }return multi;
+    }
+
+
 }
