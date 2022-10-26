0cfc1f86c0a826526ce5a70ded3c16b8ca49a94e
KTH-96 <taehyun960@gmail.com>
2022-10-26 18:29:07 +0900

    comment(Problem1): 기능 구현 목록 정리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    /**
+     * 기능 목록 작성
+     * 1. 양쪽 페이지중 가장 큰값을 구하는 기능 (조건 2, 3의 식으로 구한다.)
+     * 2. 1기능으로 찾은 값을 비교해 승자에 맞는 값을 리턴하는 기능
+     * 3. 예외상황 조건에 부합하면 -1리턴 하는 기능
+     * 예외사항 조건정리
+     * 1. 페이지가 [1, 2] 또는 [399, 400]
+     * 2. 페이지가 붙어있지 않은경우 ex) [99, 102]
+     * 3. 페이지 순서가 [홀, 짝]이 아닌경우
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
