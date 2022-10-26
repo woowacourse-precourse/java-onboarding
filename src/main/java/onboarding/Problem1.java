64fdd6a90a1103f9e36c49617923de3fdebed0b8
bomin1996 <bomin199628@gmail.com>
2022-10-27 00:37:47 +0900

    문제 1번 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -14,11 +14,18 @@ class Problem1 {
         if(pobi_R-1==pobi_L && corng_R-1==crong_L){
             pobi_num=slove(pobi_L,pobi_R);
             crong_num=slove(crong_L,corng_R);
+
+            System.out.println(pobi_num);
+            System.out.println(crong_num);
+
             if(pobi_num>crong_num){
                 answer = 1;
             }
-            if (crong_num>pobi_num){
+            else if (crong_num>pobi_num){
                 answer = 2;
+            }
+            else if (crong_num==pobi_num){
+                answer = 0;
             }
         }
         else answer=-1;
@@ -27,24 +34,28 @@ class Problem1 {
     public static int slove(int Lnum, int Rnum) {
         int sum = 0,pro = 0,answer=0;
         int Lsum =0,Rsum=0,Lpro=1,Rpro=1;
+        int LLnum =Lnum;
+        int RRnum =Rnum;
+
         while (Lnum>0){
-            Lsum += Lnum &10;
+            Lsum += Lnum %10;
             Lnum /= 10;
         }
         while (Rnum>0){
-            Rsum += Rnum &10;
+            Rsum += Rnum %10;
             Rnum /= 10;
         }
         if (Lsum>Rsum)sum=Lsum;
         if (Rsum>Lsum)sum=Rsum;
 
-        while (Lnum>0){
-            Lpro *= Lnum &10;
-            Lnum /= 10;
+
+        while (LLnum>0){
+            Lpro *= LLnum %10;
+            LLnum /= 10;
         }
-        while (Rnum>0){
-            Rpro *= Rnum &10;
-            Rnum /= 10;
+        while (RRnum>0){
+            Rpro *= RRnum %10;
+            RRnum /= 10;
         }
         if (Lpro>Rpro)sum=Lpro;
         if (Rpro>Lpro)sum=Rpro;
