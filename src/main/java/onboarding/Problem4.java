ae06d1a4c176deb55c2852cf07317e2a8301ee25
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-27 00:08:03 +0900

    feat : 청개구리 사전 등록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        Map<Character, Character> treeFrogDictionary = getTreeFrogDictionary();
+
+
         return answer;
+    }
+
+    private static Map<Character, Character> getTreeFrogDictionary() {
+        Map<Character, Character> treeFrogDictionary = new HashMap<>();
+        char from = 'A';
+        char to = 'Z';
+        for (int i = 0; i < 26; i++) {
+            treeFrogDictionary.put((char)(from + i), (char)(to - i));
+        }
+
+        from = 'a';
+        to = 'z';
+        for (int i = 0; i < 26; i++) {
+            treeFrogDictionary.put((char)(from + i), (char)(to - i));
+        }
+
+        return treeFrogDictionary;
     }
 }
