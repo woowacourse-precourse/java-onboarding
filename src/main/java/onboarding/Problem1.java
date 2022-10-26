707c61a502ce5ccb72c8f3d270e162576d40a43c
이한솔 <hansol8701@naver.com>
2022-10-26 23:55:12 +0900

    feat: 페이지 번호 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -14,11 +14,24 @@ class Problem1 {
         }
     }
 
+    public static int calcNumber(Integer page) {
+        String strPage = String.valueOf(page);
+        int mult = 1, sum = 0;
+
+        for (int i = 0; i < strPage.length(); i++) {
+            mult *= strPage.charAt(i) - '0';
+            sum += strPage.charAt(i) - '0';
+        }
+        return Math.max(mult, sum);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
         openBook(pobi);
         openBook(crong);
+
+
         return answer;
     }
 }
