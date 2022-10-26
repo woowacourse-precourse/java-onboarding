dd088d0a24d3e784f9edf08684d0e0b16b0c6f0e
0chil <0@chll.it>
2022-10-27 00:08:51 +0900

    feat(problem1): 페이지 점수를 계산할 수 있다

--- src/main/java/onboarding/problem1/Page.java
+++ src/main/java/onboarding/problem1/Page.java
@@ -7,4 +7,10 @@ public class Page {
 	public Page(int pageNumber) {
 		this.pageNumber = new Digits(pageNumber);
 	}
+
+	public int calculateScore() {
+		int multiplyScore = pageNumber.multiplyAllDigits();
+		int addScore = pageNumber.addAllDigits();
+		return Math.max(multiplyScore, addScore);
+	}
 }
--- src/test/java/onboarding/problem1/PageTest.java
+++ src/test/java/onboarding/problem1/PageTest.java
@@ -1,5 +1,16 @@
 package onboarding.problem1;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class PageTest {
 
+	@Test
+	@DisplayName("점수를 가져올 수 있다")
+	void calculate_score() {
+		Page page = new Page(124);
+
+		Assertions.assertThat(page.calculateScore()).isEqualTo(8);
+	}
 }
