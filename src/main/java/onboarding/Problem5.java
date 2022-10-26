79c5d924c61e04f8f18c0104d57efd9dfa5a95f8
hyeongcheolkim <tori1753@naver.com>
2022-10-26 20:34:45 +0900

    problem5 first commit

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> unit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        var ret = new ArrayList<Integer>();
+        for (var u : unit) {
+            ret.add(money / u);
+            money %= u;
+        }
+        return ret;
     }
 }
