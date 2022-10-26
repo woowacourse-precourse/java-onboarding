6b22e220136934c5ea745556bc52f20d05f4ab58
Eundms <dms011221@gmail.com>
2022-10-26 22:25:12 +0900

    add(Problem2)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,33 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        StringBuilder answer=new StringBuilder(cryptogram);
+
+        while(true){ //step 0~?
+            int start=0;
+            int current=0;
+            StringBuilder stepAnswer = new StringBuilder();
+            while(true){ //step i
+                if(current>=answer.length()-1){
+                    break;
+                }
+                if(answer.charAt(current)==answer.charAt(current+1)){
+                    stepAnswer.append(answer.substring(start,current));
+                    current+=1;
+                    start=current+1;
+                }
+                current+=1;
+            }
+            if(start!=answer.length()){
+                stepAnswer.append(answer.substring(start,answer.length()));
+            }
+            if(answer.toString().equals(stepAnswer.toString())){ // 업데이트가 없다
+                break;
+            }
+            answer=stepAnswer;
+        }
+
+        return String.valueOf(answer);
     }
 }
--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
