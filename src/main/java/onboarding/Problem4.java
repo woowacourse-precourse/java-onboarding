e73a764e07a2163b2a6714ddb805c82ad8e89f11
JunH <tjdtls690@gmail.com>
2022-10-26 19:45:37 +0900

    :sparkles: feat : 대문자 변환 전용 배열 초기화하기 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,10 +6,17 @@ import java.util.stream.IntStream;
 
 public class Problem4 {
     private static final List<Character> LOWERCASE_LETTERS = lowercaseLettersInit();
+    private static final List<Character> UPPERCASE_LETTERS = uppercaseLettersInit();
     
     public static List<Character> lowercaseLettersInit() {
         return IntStream.rangeClosed(0, 25)
                 .mapToObj(number -> (char) (122 - number))
+                .collect(Collectors.toList());
+    }
+    
+    private static List<Character> uppercaseLettersInit() {
+        return IntStream.rangeClosed(0, 25)
+                .mapToObj(number -> (char) (90 - number))
                 .collect(Collectors.toList());
     }
     
