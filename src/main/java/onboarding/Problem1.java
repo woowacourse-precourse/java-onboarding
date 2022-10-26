4a59110010bdd434cb2f7b9b82d8118bc7b067c9
ch200203 <ch200205@naver.com>
2022-10-27 00:05:14 +0900

    2. 포비와 크롱의 점수를 계산 2-1 : 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다. 2-2 : 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다. 2-3 : 둘 중 더 큰수를 점수로 선택한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,16 +9,37 @@ class Problem1 {
         // 예외의 경우 처리
         if(!validChk(pobi) || !validChk(crong)) return answer = -1;
 
+        // 포비와 크롱의 점수 계산
+        int pobiPoint = Math.max(solution(pobi.get(0)), solution(pobi.get(1)));
+        int crongPoint = Math.max(solution(crong.get(0)), solution(crong.get(1)));
+
         return answer;
     }
+
+    /**
+     * 조건에 따라 더 큰 점수를 리턴
+     * @param pageNum
+     * @return Math.max(합, 곱셈)
+     */
+    public static int solution(int pageNum) {
+        String page = String.valueOf(pageNum);
+        int sum = 0, multi = 1;
+        for (char c : page.toCharArray()) {
+            int num = Integer.parseInt(String.valueOf(c));
+            sum += num;
+            multi *= num;
+        }
+
+        return Math.max(sum, multi);
+    }
+
     /**
      * 예외처리 함수
      * @param list
      * @return {boolean}
      */
     public static boolean validChk(List<Integer> list) {
-        if(list.get(1) - list.get(0) > 1) return false;
-        else if(list.contains(400) || list.contains(1)) return false;
+        if(list.get(1) - list.get(0) > 1 || list.contains(400) || list.contains(1)) return false;
         return true;
     }
 }
