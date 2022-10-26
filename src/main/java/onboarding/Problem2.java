0e87a73a255d2afc241d4f1e575c8fa58f84422a
hstla <ree7243@gmail.com>
2022-10-26 20:55:57 +0900

    문제2 feat: 문자 리스트 변환, 같은 문자 삭제 메서드 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+
+    public static List<Character> stringToList (String str) {
+        List<Character> list = new ArrayList<>();
+        for (char c : str.toCharArray()) {
+            list.add(c);
+        }
+        return list;
+    }
+
+
+
+    public static List<Character> deleteSame(List<Character> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) == list.get(i + 1)) {
+                list.remove(i);
+                list.remove(i);
+            }
+        }
+        return list;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        List<Character> list = stringToList(cryptogram);
+        System.out.println("list = " + list);
+
+        while (true) {
+            deleteSame(list);
+            break;
+        }
+        System.out.println("list = " + list);
         return answer;
     }
 }
