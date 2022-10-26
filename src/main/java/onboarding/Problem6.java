0d86825ef42ff9776f0f2663387c091a05acefaa
main <jang0929@naver.com>
2022-10-26 20:41:29 +0900

    Problem6 기능 사항 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,15 +1,12 @@
 package onboarding;
 
 import java.util.List;
-
-
 /**
  * 기능 사항
  * 1. nickname과 email을 포함한 User Class 구현
  * 2. 중복된 이름일 경우 set에 저장하는 함수
  * 3. 두 자씩 map에 저장하는 함수
  */
-
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
