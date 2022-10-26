0a5316ce7d214c227f6e6c0bc3ceea2b51b5dc9a
Kayoung <rkdkdudjd@gmail.com>
2022-10-27 00:54:17 +0900

    1번: validateInput 메서드
    입력값을 검증하는 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,14 +4,37 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int pobiMax = getMaxValueFromList(pobi);
-        int crongMax = getMaxValueFromList(crong);
-        if (pobiMax > crongMax) {
-            return 1;
-        } else if (pobiMax < crongMax) {
-            return 2;
+        try {
+            validateInput(pobi, crong);
+            int pobiMax = getMaxValueFromList(pobi);
+            int crongMax = getMaxValueFromList(crong);
+            if (pobiMax > crongMax) {
+                return 1;
+            } else if (pobiMax < crongMax) {
+                return 2;
+            }
+            return 0;
+        } catch (Exception e) {
+            return -1;
         }
-        return 0;
+    }
+
+    public static void validateInput(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.size() != 2 || crong.size() != 2) {
+            throw new IllegalArgumentException("Invalid input size");
+        }
+
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
+
+        boolean validatePobi = pobiLeft <= 399 && pobiLeft % 2 == 1 && pobiRight == pobiLeft + 1;
+        boolean validateCrong = crongLeft <= 399 && crongLeft % 2 == 1 && crongRight == crongLeft + 1;
+
+        if (!validatePobi || !validateCrong) {
+            throw new IllegalArgumentException("Invalid input value");
+        }
     }
 
     public static int getMaxValueFromList(List<Integer> intList) {
