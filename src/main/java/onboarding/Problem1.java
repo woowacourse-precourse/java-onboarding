aa5842c54c2bf4dca3f1061d4abcb8e4e8920193
kimminsu <xjfcnfw3@naver.com>
2022-10-26 22:26:54 +0900

    feat(문제1): 승부의 결과를 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,12 @@
 package onboarding;
 
 import java.util.List;
+import onboarding.problem1.Comparator;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        answer = Comparator.compareUsers(pobi, crong);
         return answer;
     }
 }
