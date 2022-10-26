b1b4236b970d4bfbc977100ae3ed16a5a45001e1
H4R1B0 <qkrguswns25@naver.com>
2022-10-26 19:01:25 +0900

    중복 제거 결과 반환할 클래스 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
         return answer;
+    }
+
+    //중복 제거 함수
+
+    //중복 제거 결과를 반환할 클래스
+    class result{
+        private boolean changed;
+        private String str;
+
+        public result(boolean changed, String str){
+            this.changed = changed;
+            this.str = str;
+        }
     }
 }
