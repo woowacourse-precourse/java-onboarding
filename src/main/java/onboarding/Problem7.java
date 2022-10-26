f59e242b6d192da73fb545dfbc3deeaecec81161
yj2980 <adore2100@naver.com>
2022-10-27 03:28:31 +0900

    fix : findVisitedPoint 함수 추가 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -11,21 +11,38 @@ public class Problem7 {
     static int frendPoint = 10;
     static int visitPoint = 1;
     static int newFriends = 1;
+    static int recommendMax = 5;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+
+        findRecommendFriends(findVisitedPoint(findKnowFriendsPoint(savaFriendsList(friends, user)), visitors));
         return answer;
     }
 
-    public static void findRecommendFrinds(Map<String, Integer> friendsMap)
+    public static List<String> findRecommendFriends(Map<String, Integer> friendsMap)
     {
+        int cnt = 0;
+        List<String> recommendFriendsList = new ArrayList<>();
         Integer [] friendsPointArray = new Integer[friendsMap.size()];
 
         for (int i = 0; i < friendsMap.size(); i++)
+        {
             friendsPointArray[i] = friendsMap.get(i);
+            System.out.println(friendsPointArray[i]);
+        }
         Arrays.sort(friendsPointArray, Collections.reverseOrder());
 
+        for (int i = 0; i < friendsPointArray.length; i++)
+        {
+            if (friendsPointArray[i] == 0 || cnt == recommendMax)
+                break;
+            recommendFriendsList.add(getKey(friendsMap, friendsPointArray[i]));
+            cnt++;
+        }
 
+        return recommendFriendsList;
     }
 
     // hashmap에 value 로 key 찾기
@@ -38,9 +55,9 @@ public class Problem7 {
         }
         return null;
     }
-    
+
     //유저와 알고있는 친구 목록 구하기기
-   public static List<String> savaFirendsList(List<List<String>> friends, String user)
+   public static List<String> savaFriendsList(List<List<String>> friends, String user)
     {
         boolean userCheck = false;
         List<String> friendList = new ArrayList<>();
@@ -58,7 +75,7 @@ public class Problem7 {
             return null;
         return friendList;
     }
-    public static Map<String, Integer> findknowFriendsPoint(List<String> knowFriendsList)
+    public static Map<String, Integer> findKnowFriendsPoint(List<String> knowFriendsList)
     {
         int point = frendPoint;
         String key;
@@ -73,26 +90,30 @@ public class Problem7 {
             knowFriendsMap.put(knowFriendsList.get(i), point);
             point = frendPoint;
         }
+
         return knowFriendsMap;
     }
     public static Map<String, Integer> findVisitedPoint(Map<String, Integer> knowFriendsMap, List<String> visitors)
     {
         int point = visitPoint;
 
+        System.out.println("방문 : " + knowFriendsMap);
         for (int i = 0; i < visitors.size(); i++)
         {
             String key = visitors.get(i);
-            //null인 경우
-            if (knowFriendsMap == null)
-            {
 
-            }
             if (knowFriendsMap.containsKey(key))
             {
                 point = knowFriendsMap.get(key) + visitPoint;
                 knowFriendsMap.put(visitors.get(i), point);
             }
+            else if (!knowFriendsMap.containsKey(key))
+            {
+                knowFriendsMap.put(visitors.get(i), point);
+            }
         }
+
+        System.out.println("방문 후 : " + knowFriendsMap);
 
         return knowFriendsMap;
     }
