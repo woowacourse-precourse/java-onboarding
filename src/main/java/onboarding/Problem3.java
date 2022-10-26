deff94fa8c5c56139a047c1d116cb2b34d2fcbc4
yj2980 <adore2100@naver.com>
2022-10-26 19:43:01 +0900

    feat : 숫자가 포함되는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        answer = checkNum(number);
         return answer;
+    }
+
+    public static int checkNum(int number)
+    {
+        String strNum;
+        int cnt = 0;
+
+        for (int i = 1; i <= number; i++)
+        {
+            strNum = Integer.toString(i);
+            if (strNum.contains("3") || strNum.contains("6") || strNum.contains("9"))
+                cnt++;
+        }
+        return cnt;
     }
 }
