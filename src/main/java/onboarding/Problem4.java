7fe1dc074c00c64551745eafd2a406de2b500bdb
woosung1223 <dntjd991223@naver.com>
2022-10-26 23:58:17 +0900

    [문제 4] 입력 예외처리 기능 담당 checkValidation 함수 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,19 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    public static boolean checkValidation(String word) {
+        if (word.length() > 1000 || word.length() < 1) {
+            return false;
+        }
+        return true;
+    }
+
 }
+
+
+/*
+1. 입력 조건에 맞지 않는 경우 예외처리 기능
+2. 알파벳을 변환하는 기능
+- (예외) 알파벳 외의 문자는 변환하지 않음
+3. word을 변환하는 기능
+* */
