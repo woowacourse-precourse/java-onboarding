f161007f58948410e2b92327f28e43b49b72f396
hsj4436 <hsj4436@naver.com>
2022-10-26 22:23:20 +0900

    feature/Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        Map<Character, Character> dict = new HashMap<Character, Character>();
+        initializeDict(dict);
+
+        for(int i = 0; i < word.length(); i++){
+            if(word.charAt(i) == ' '){
+                answer += ' ';
+            }else{
+                answer += dict.get(word.charAt(i));
+            }
+        }
         return answer;
+    }
+
+    static void initializeDict(Map<Character, Character> dict){
+        for(char ch = 'A'; ch <= 'Z'; ch++){
+            dict.put(ch, (char) ('Z' + ('A' - ch)));
+        }
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            dict.put(ch, (char) ('z' + ('a' - ch)));
+        }
     }
 }
