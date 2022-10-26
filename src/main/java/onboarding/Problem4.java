29955b9fe380fe0823078d681fa3c119a9a9ca62
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-26 21:16:45 +0900

    Feat: 구현 Problem4 문자열을 읽으면서 알파뱃이면 조건에 맞게 바꿔준다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		StringBuilder answer = new StringBuilder();
+		for (int i = 0; i < word.length(); i++) {
+			if ('A' <= word.charAt(i) && word.charAt(i) <= 'Z') {
+				answer.append((char)('Z' - word.charAt(i) + 'A'));
+			} else if ('a' <= word.charAt(i) && word.charAt(i) <= 'z') {
+				answer.append((char)('z' - word.charAt(i) + 'a'));
+			} else {
+				answer.append(word.charAt(i));
+			}
+		}
+		return answer.toString();
+	}
 }
