95a8ae136385c28db9f320ac6b1bfe6789632ab0
aossuper7 <aossuper7@naver.com>
2022-10-26 22:02:42 +0900

    feat: 문제2 스택 값 출력

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,15 +9,22 @@ public class Problem2 {
         char[] answer = cryptogram.toCharArray();
 
         for (int i = 0; i < answer.length; i++) {
-            if (isNotEmptyAndEquals(stack, answer[i])) {
+            if (isNotEmptyAndPeekEquals(stack, answer[i])) {
                 stack.pop();
             } else {
                 stack.push(answer[i]);
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        //stack 값 출력
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
     }
 
-    public static boolean isNotEmptyAndEquals(Stack<Character> stack, char answer) {
+    public static boolean isNotEmptyAndPeekEquals(Stack<Character> stack, char answer) {
         return !stack.empty() && stack.peek().equals(answer);
     }
 }
