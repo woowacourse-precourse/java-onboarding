57c2ff91e6d67651e043990ff6bd4c48b2916fb1
sangil <gsl0505@naver.com>
2022-10-26 20:52:26 +0900

    feat: 중복되는 이름 찾기 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
-		List<String> answer = List.of("answer");
-		return answer;
+		Map<String, Integer> duplicatedNameCount = new HashMap<>();
+		for (List<String> form : forms) {
+			for (int nameIndex = 0; nameIndex < form.get(1).length() - 1; nameIndex++) {
+				String duplicatedName = form.get(1).substring(nameIndex, nameIndex + 2);
+				if (!duplicatedNameCount.containsKey(duplicatedName)) {
+					duplicatedNameCount.put(duplicatedName, 1);
+				} else {
+					duplicatedNameCount.put(duplicatedName, duplicatedNameCount.get(duplicatedName) + 1);
+				}
+			}
+		}
+		return Collections.emptyList();
 	}
 }
