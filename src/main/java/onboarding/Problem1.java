f54a13ebc2ea14a4177f31187735fa2c3b6e07d5
Kim kyunghee <kkh2415@naver.com>
2022-10-26 22:02:01 +0900

    add : #Problem1-예외처리 기능 추가 1. 전체적인 유효성 검사 템플릿 기능을 하는 validatePage() 메서드 추가 2. 펼친 페이지가 순차적인 페이지인지 검사하는 isSequentialPage() 메서드 추가 3. 펼친 페이지가 첫 장인지 마지막 장인지 검사하는 isFirstOrLastPage() 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,32 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!validatePage(pobi) || !validatePage(crong)) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private static boolean validatePage(List<Integer> pages) {
+        final int FIRST_PAGE = 0;
+        final int SECOND_PAGE = 1;
+
+        boolean result = isSequentialPage(pages.get(FIRST_PAGE), pages.get(SECOND_PAGE));
+        if (isFirstOrLastPage(pages.get(FIRST_PAGE), pages.get(SECOND_PAGE))) {
+            result = false;
+        }
+        return result;
+    }
+
+    private static boolean isSequentialPage(int first, int last) {
+        return (last - 1) == first;
+    }
+
+    private static boolean isFirstOrLastPage(int first, int second) {
+        final int FIRST_PAGE_NUM = 1;
+        final int LAST_PAGE_NUM = 400;
+
+        return first == FIRST_PAGE_NUM || second == LAST_PAGE_NUM;
     }
 }
