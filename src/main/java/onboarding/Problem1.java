9de06a0ceba84b04b28d2492ca180ff4b57274df
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-26 16:42:02 +0900

    docs: add function list to problem 01

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * Problem 01 기능 목록
+ * - 올바른 입력값 확인
+ * - 펼친 페이지에서의 최댓값 계산
+ * - 포비와 크롱 중에서 승자를 결정
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
