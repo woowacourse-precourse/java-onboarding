5319fc9b691ca0dcf9046afe028fa9a8f2e23aa0
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-27 01:27:20 +0900

    Feat: 구현 Problem6 isVlidInput 함수 구현 - 닉네임이 1글자 이상 20글자 미만 판단 - 이메일이 @email.com으로 끝나는지 판다 - 이메일이 11글자 이상 20글자 미만 판단

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	private static final String DOMAIN = "@email.com";
+
+	public static boolean isValidInput(String email, String nickName) {
+		if (11 > email.length() || email.length() >= 20) {
+			return false;
+		} else if (1 > nickName.length() || nickName.length() >= 20) {
+			return false;
+		} else if (!email.endsWith(DOMAIN)) {
+			return false;
+		}
+		return true;
+	}
+
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = new ArrayList<>();
+		return answer;
+	}
 }
