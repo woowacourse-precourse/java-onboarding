ca89ca085c0461a024adca1f3c292cafdeb9f53a
23Yong <1239824@naver.com>
2022-10-26 19:44:15 +0900

    [feat] 중복 문자를 제거
    - Stack을 사용해 문자를 제거하도록 한다.
    - Stack의 최상단 문자와 현재 문자가 동일하면 제거하도록 한다.
    - 그 외의 경우 Stack에 넣도록 한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> stack = new Stack<>();
+        int len = cryptogram.length();
+        for (int i = 0; i < len; i++) {
+            char ch = cryptogram.charAt(i);
+
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop();
+            } else {
+                stack.add(ch);
+            }
+        }
+
         return answer;
     }
 }
