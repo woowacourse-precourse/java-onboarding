772447280a69eb4fa871e86e78cead811b3755d0
somsom13 <somin1113@naver.com>
2022-10-26 23:07:19 +0900

    feat(문제1): 예외처리 - 연속 페이지, 홀짝 순서

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,22 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int ERROR_CODE=-1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(!isInputValid(pobi)||!isInputValid(crong)){
+            return ERROR_CODE;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static Boolean isInputValid(List<Integer> person){
+        int leftPage=person.get(0);
+        int rightPage=person.get(1);
+        if(isPageRangeInBook(leftPage,rightPage)&&isPageInRow(leftPage,rightPage)&&isOddEven(leftPage,rightPage)) {
+            return true;
+        }
+        return false;
     }
 
     private static Boolean isPageRangeInBook(int leftPage, int rightPage) {
@@ -17,4 +30,19 @@ class Problem1 {
         }
         return true;
     }
+
+    private static Boolean isPageInRow(int leftPage, int rightPage){
+        if(leftPage+1==rightPage){
+            return true;
+        }
+        return false;
+    }
+
+    private static Boolean isOddEven(int leftPage, int rightPage){
+        if(leftPage%2==0||rightPage%2==1){
+            return false;
+        }
+        return true;
+    }
+
 }
