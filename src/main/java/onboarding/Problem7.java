298f0fefff7f602538168f69835fb033e21ad1b7
main <jang0929@naver.com>
2022-10-26 20:51:21 +0900

    Problem7 RecFriend 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 기능 사항
@@ -11,6 +13,32 @@ import java.util.List;
  * 4. 방문자일 경우 1점을 추가하는 함수
  */
 public class Problem7 {
+
+
+    static HashMap<String, Set<String>> friendsHashMap = new HashMap<>();
+    static HashMap<String, Integer> points = new HashMap<>();
+
+    /**
+     * 1. 추천할 친구 RecFriend class
+     */
+    static class RecFriend {
+        private String name;
+        private Integer score;
+
+        RecFriend(String name, Integer score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Integer getScore() {
+            return score;
+        }
+
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
