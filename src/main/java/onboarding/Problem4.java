872ddd21e9621c4d15b32b6f91532a5be829f568
seokjin <mm8678@g.yju.ac.kr>
2022-10-26 23:45:50 +0900

    feat(Problem4.java): 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		char[] chars = word.toCharArray();
+
+		for (int i = 0; i < chars.length; i++) {
+			char temp = chars[i];
+			if (temp >= 'A' && temp <= 'Z') {
+				temp = (char) ('Z' - (temp - 'A'));
+			} else if (temp >= 'a' && temp <= 'z') {
+				temp = (char) ('z' - (temp - 'a'));
+			}
+			chars[i] = temp;
+		}
+		return String.valueOf(chars);
+	}
 }
