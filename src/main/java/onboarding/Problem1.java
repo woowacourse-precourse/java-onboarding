4c65cfd8bb11af0c5c963b4917d1b25fc9b519ef
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 17:44:06 +0900

    홀수, 짝수 페이지 점수 중 가장 큰 수를 고르는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,27 +4,28 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int pobiLeftScore;
-        int crongLeftScore;
-        int pobiRightScore;
-        int crongRightScore;
+        int pobiScore;
+        int crongScore;
         int answer;
+
+        pobiScore = Math.max(howManyScore(pobi.get(0)), howManyScore(pobi.get(1)));
+        crongScore = Math.max(howManyScore(crong.get(0)), howManyScore(crong.get(1)));
 
         return -1;
     }
 
-    public int howManyScore(int num) {
+    public static int howManyScore(int num) {
         return Math.max(plusDigits(num), multiFlyDigits(num));
     }
 
-    public int plusDigits(int num) {
+    public static int plusDigits(int num) {
         if (num >= 100) {
             return num / 100 + (num % 100 + 1)/10 - 1 + num % 10;
         }
         return num / 10 + num % 10;
     }
 
-    public int multiFlyDigits(int num) {
+    public static int multiFlyDigits(int num) {
         if (num >= 100) {
             return num / 100 * ((num % 100 + 1)/10 - 1) * num % 10;
         }
