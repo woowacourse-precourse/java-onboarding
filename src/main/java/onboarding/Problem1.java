e70cff77f344f0dd8ccce78e7f9566c48a9c50c2
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-26 21:36:55 +0900

    feat: 랜덤 페이지임을 확인하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 
 /**
- * [ ] 책을 임의로 펼친다.
+ * [o] 책을 임의로 펼친다.
  * [o] 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
  * [o] 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
  * [o] 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
@@ -15,18 +15,11 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int pobiMax = 0;
-        int crongMax = 0;
 
-        for(Integer page:pobi) {
-            pobiMax = Math.max(Math.max(getSumNumberOfDigits(page), getMultipleNumberOfDigits(page)),pobiMax);
-        }
+        int pobiMax = getMax(pobi);
+        int crongMax = getMax(crong);
 
-        for(Integer page:crong) {
-            crongMax = Math.max(Math.max(getSumNumberOfDigits(page), getMultipleNumberOfDigits(page)),crongMax);
-        }
-
-        if(pobiMax > crongMax) {
+        if (pobiMax > crongMax) {
             answer = 1;
         } else if (pobiMax < crongMax) {
             answer = 2;
@@ -35,6 +28,7 @@ class Problem1 {
         }
 
         return answer;
+
     }
 
     public static int getSumNumberOfDigits(int num) {
@@ -57,5 +51,22 @@ class Problem1 {
         }
 
         return multiple;
+    }
+
+    public static int getMax(List<Integer> pages) {
+        int max = 0;
+        for (Integer page : pages) {
+            max = Math.max(Math.max(getSumNumberOfDigits(page), getMultipleNumberOfDigits(page)), max);
+        }
+
+        return max;
+    }
+
+    public static boolean isRandomPage(List<Integer> pages) {
+        if (pages.get(1) - pages.get(0) == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
