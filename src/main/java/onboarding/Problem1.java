768c0db3868a43ca506c39aa0ac1903e0e9a968d
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-10-26 22:32:25 +0900

    feat: Problem 1, Add valid function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if((isValidInput(pobi) && isValidInput(crong)) == false){
+            return -1;
+        }
+
+        return 0;
+    }
+
+    public static boolean isValidInput(List<Integer> input){
+        if((input.get(1) - input.get(0)) == 1){
+            return true;
+        }
+        return false;
     }
 }
