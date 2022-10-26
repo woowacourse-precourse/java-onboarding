a129597d0c38324ac363788f6f860d78c0e237d9
DoooongDong <rhkswls98@kakao.com>
2022-10-26 22:35:51 +0900

    [Problem1] 책을 펼쳤을 때 양면에 있는 페이지 수의 차가 1이 아닌 경우 예외사항으로 간주하고 -1을 return 한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,10 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        //양면의 페이지 수 차가 1이 아니면 예외 발생
+        if(Math.abs(pobi.get(0) - pobi.get(1)) != 1 || Math.abs(crong.get(0) - crong.get(1)) != 1) {
+            return -1;
+        }
         return answer;
     }
 }
