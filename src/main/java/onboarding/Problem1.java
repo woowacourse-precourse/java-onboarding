b9c34e43fefa1f0f2e54ba82abae8a0aa1993ed6
ECHO <eunchan1001@gmail.com>
2022-10-26 18:19:35 +0900

    feat(Problem1): add methods of getting sum and product from digits

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static int getSumOfDigits(char[] digits) {
+        int sum = 0;
+        for (int i : digits) {
+            i -= 48;
+            sum += i;
+        }
+        return sum;
+    }
+
+    static int getProductOfDigits(char[] digits) {
+        int product = 1;
+        for (int i : digits) {
+            i -= 48;
+            product *= i;
+        }
+        return product;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
