c353dc9fd47a9b0c53b9a07f1bba7226631e78d6
Junho Choi <ajchoi0928@gmail.com>
2022-10-26 17:30:56 +0900

    feat: 기능 1 구현 전반적으로 수정에 닫혀 있고 확장에 열려있도록 고려 ** 주요 내용 ** - 사용되는 상수형 값들을 명시적으로 관리 - 클라이언트가 사용하는 solution 코드는 간결하게 작성 - 입력에 대한 예외 검사 메소드 구현 - 다양한 연산 방법에 대해 확장할 수 있되, 수정은 닫혀있도록 calculateEachDigit 메소드 구현  - 각 메소드의 책임과 역할이 명확하게 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,78 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int FIRST_PAGE = 1;
+    private static final int LAST_PAGE = 400;
+    private static final int NEXT_PAGE_INC = 1;
+    private static final int LEFT_PARAM_WIN_SCORE = 1;
+    private static final int RIGHT_PARAM_WIN_SCORE = 2;
+    private static final int DRAW_SCORE = 0;
+    private static final int EXCEPTION_CODE = -1;
+
+    /* 추상화를 이용해 다양한 계산 방식으로 확장할 수 있도록 */
+    static abstract class DigitsCalculator {
+        abstract int calculate(char[] digits);
+    }
+
+    static class DigitsSum extends DigitsCalculator {
+        @Override
+        int calculate(char[] digits) {
+            int result = 0;
+            for (char digit : digits) {
+                result += digit - '0';
+            }
+            return result;
+        }
+    }
+
+    static class DigitMultiply extends DigitsCalculator {
+        @Override
+        int calculate(char[] digits) {
+            int result = 1;
+            for (char digit : digits) {
+                result *= digit - '0';
+            }
+            return result;
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        /* 입력 예외 검증 */
+        if (!validateInput(pobi) || !validateInput(crong))
+            return EXCEPTION_CODE;
+        return compare(getScore(pobi), getScore(crong));
+    }
+
+    static boolean validateInput(List<Integer> list) {
+        boolean validList = !list.isEmpty() && list.size() <= 2;
+        boolean pageBoundary = list.get(0) > FIRST_PAGE && list.get(1) < LAST_PAGE;
+        boolean startWithOdd = list.get(0) % 2 == 1;
+        boolean pagesInRow = (list.get(0) + NEXT_PAGE_INC) == list.get(1);
+        return validList && pageBoundary && startWithOdd && pagesInRow;
+    }
+
+    static int calculateEachDigit(int number, DigitsCalculator rule) {
+        char[] digits = numToDigits(number);
+        return rule.calculate(digits);
+    }
+
+    // 왼쪽/오른쪽 페이지 번호에 대한 합의 결과 곱의 결과 중 MAX인 값을 반환
+    static int getScore(List<Integer> list) {
+        int mostMax = 0;
+        for (Integer page : list) {
+            int comp = Math.max(calculateEachDigit(page, new DigitsSum()), calculateEachDigit(page, new DigitMultiply()));
+            if (mostMax < comp)
+                mostMax = comp;
+        }
+        return mostMax;
+    }
+
+    static char[] numToDigits(int number) {
+        return String.valueOf(number).toCharArray();
+    }
+
+    static int compare(int x, int y) {
+        return x > y ? LEFT_PARAM_WIN_SCORE : x == y ? DRAW_SCORE : RIGHT_PARAM_WIN_SCORE;
     }
 }
