a30e2374e2cf51f5b292c9bf44e484d46266f1c4
main <jang0929@naver.com>
2022-10-26 20:31:14 +0900

    Problem4 기능 사항 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,14 @@
 package onboarding;
 
+
+/**
+ * 기능 사항
+ * 1. 해당 문자를 변환하는 함수
+ *
+ */
 public class Problem4 {
+
+
     public static String solution(String word) {
         String answer = "";
         return answer;
