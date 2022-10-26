9b7413fad45aaa89927e508673df79a4f44c749f
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-26 21:23:08 +0900

    add : (Problem3) 결과 도출
    tsn 문자 배열과 1부터의 반복문을 통해 정답을 도출하여 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        char[] tsn = {'3', '6', '9'};
+        for(int i=1; i<number+1; i++){
+            char[] numChar = String.valueOf(i).toCharArray();
+            for(int j=0; j<tsn.length; j++){
+                answer += containChar(numChar, tsn[j]);
+            }
+        }
+
         return answer;
     }
 
