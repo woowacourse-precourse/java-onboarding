ace76c146948d5ff3665e4c6ec2739fa852385a7
juno-junho <ssmm0205@naver.com>
2022-10-26 22:17:21 +0900

    feat: make method for calculating heighest number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,31 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int highestNum(List<Integer> pobi) {
+        int leftNum = pobi.get(0);
+        int rightNum = pobi.get(1);
+
+        int leftNumSum = 0;
+        int leftNumMul = 0;
+        while (leftNum != 0) {
+            leftNumSum += leftNum % 10;
+            leftNumMul *= leftNum % 10;
+            leftNum /= 10;
+        }
+        int rightNumSum = 0;
+        int rightNumMul = 0;
+        while (rightNum != 0) {
+            rightNumSum += rightNum % 10;
+            rightNumMul *= rightNum % 10;
+            rightNum /= 10;
+        }
+        int highestLeftNum = leftNumMul > leftNumSum ? leftNumMul : leftNumSum;
+        int highestRightNum = rightNumMul > rightNumSum ? rightNumMul : rightNumSum;
+        return highestLeftNum > highestRightNum ? highestLeftNum : highestRightNum;
     }
 }
