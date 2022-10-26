86682238d7a8f6c732c31bd138948f79f50101e1
K-jun98 <alap_u@naver.com>
2022-10-26 23:37:21 +0900

    test - 변환 테스트 코드 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -85,6 +85,13 @@ class ApplicationTest {
             String result = "R olev blf";
             assertThat(Problem4.solution(word)).isEqualTo(result);
         }
+
+        @Test
+        void case2() {
+            String word = "abcxyz , ABCXYZ";
+            String result = "zyxcba , ZYXCBA";
+            assertThat(Problem4.solution(word)).isEqualTo(result);
+        }
     }
 
     @Nested
