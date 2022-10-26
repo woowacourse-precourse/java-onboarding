86497876b5ec7a42ac5071206a12d0a9ed7c8d8a
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-10-27 00:34:36 +0900

    Gradle 오류인듯 계속 0만 출력됨

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        List<String> cryptogramList = Arrays.asList(cryptogram);
+
+
+        for(int i = 0 ; i < cryptogram.length()-1 ; i++){
+            if (cryptogramList.get(i) == cryptogramList.get(i+1)){
+                cryptogramList.remove(i);
+                cryptogramList.remove(i+1);
+                cryptogramList.add(i," ");
+            }
+            cryptogramList.remove(" ");
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String cryptogram = "browoanoommnaon";
+        System.out.println(solution(cryptogram));
     }
 }
