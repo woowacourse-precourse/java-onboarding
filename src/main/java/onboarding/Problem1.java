0c747143cb0045d9b16047a2e790a73100e4d1c9
sangil <gsl0505@naver.com>
2022-10-26 16:44:37 +0900

    feat: 페이지 중 가장 큰 수 구하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 class Problem1 {
     private static final int NUMBER_OF_DIGIT_DIVISION = 10;
+    private static final int LEFT_PAGE = 0;
+    private static final int RIGHT_PAGE = 1;
 
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int answer = Integer.MAX_VALUE;
@@ -28,4 +30,11 @@ class Problem1 {
         return multipleValue;
     }
 
+    private static int pageMax(List<Integer> pages) {
+        int leftMaxValue = Math.max(digitAdd(pages.get(LEFT_PAGE)),
+            digitMultiple(pages.get(LEFT_PAGE)));
+        int rightMaxValue = Math.max(digitAdd(pages.get(RIGHT_PAGE)),
+            digitMultiple(pages.get(RIGHT_PAGE)));
+        return Math.max(leftMaxValue,rightMaxValue);
+    }
 }
