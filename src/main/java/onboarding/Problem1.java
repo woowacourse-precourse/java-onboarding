4bd05d5362d0535ae7da0eab9978ba059f80564e
jjmin9797 <jjmin9797@naver.com>
2022-10-27 01:14:23 +0900

    [Problem-1][기능구현][checkForException] 입력 예외 처리 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,25 +1,42 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> test1 = new ArrayList<Integer>(Arrays.asList(1,2));
+        ArrayList<Integer> test2 = new ArrayList<Integer>(Arrays.asList(4,6));
+        System.out.println(checkForException(test2,test1));
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
 
 
+
+
     /**
      * [예외 발생 확인 기능]
      * - 입력받은 페이지가 1~400사이인지 확인한다.
      * - 입력받은 페이지의 차가 1인지 확인한다.
-     * - 입력받 데이터의 길이가 2인지 확인한다.
+     * - 입력받은 데이터의 길이가 2인지 확인한다.
      * - 입력 받은 페이지에 예외가 발생하지 않는다면 True를 반환한다.
      */
-    public boolean checkForExceptions(List<Integer> pobi, List<Integer> crong) {
-        boolean returnValue = false;
-        return returnValue;
+    public static boolean checkForException(List<Integer> pobi, List<Integer> crong) {
+        if ((pobi.size() == 2 && crong.size() == 2) &&
+                (pobi.get(1)-pobi.get(0) == 1 && crong.get(1)-crong.get(0) == 1) &&
+                (0 < pobi.get(0) && pobi.get(0) <= 400  && 0 < pobi.get(1) && pobi.get(1) <= 400) &&
+                (0 < crong.get(0) && crong.get(0) <= 400  && 0 < crong.get(1) && crong.get(1) <= 400)) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
 
