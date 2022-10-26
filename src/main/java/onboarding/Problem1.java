a0fd87f7e3c60f0f31aa8e83bea03b6c68300e5d
jjongwa <troas96@naver.com>
2022-10-27 01:24:47 +0900

    문제1-페이지 올바른지 확인하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+
 class Problem1 {
+     // 올바른 페이지 입력받았는지 check
+     // 왼쪽 페이지가 홀수고 오른쪽 페이지가 왼쪽 페이지 숫자 +1 이면 통과
+     static boolean checkPage(List<Integer> book){
+        if(book.get(0) %2 == 1 && book.get(1) == book.get(0) + 1){
+            return true;
+        }
+        return false;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(!(checkPage(pobi) && checkPage(crong))){ // 페이지 올바른지 check
+            return -1;
+        }
+
+
+
         return answer;
     }
 }
