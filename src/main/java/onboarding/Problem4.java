94c66aba37aea5f4ffa9569e24aa3a7e1c9c4c16
jeomxon <jeomxon@gmail.com>
2022-10-27 00:48:52 +0900

    feat(Problem4): check it is alphabet

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        //알파벳인지 확인하는 함수
+        //반대로 변환하기
+        for (int i = 0; i < word.length(); i++) {
+
+            if (isAlphabet(word.charAt(i))) {
+
+            }
+        }
+        return "";
+    }
+
+    public static boolean isAlphabet(char alphabet) {
+
+        if (alphabet >= 'a' && alphabet <= 'z') {
+            return true;
+        } else if (alphabet >= 'A' && alphabet <= 'Z') {
+            return true;
+        }
+        return false;
     }
 }
