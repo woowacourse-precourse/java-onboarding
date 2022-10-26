52f5fa70f64c64e1b1e8f821940f0dc8ee2ac762
apptie <appti@naver.com>
2022-10-26 21:02:50 +0900

    feat: 문제 1 페이지 검증 전체를 수행하는 validatePage 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,15 @@ class Problem1 {
         return answer;
     }
 
+    private static boolean validatePage(List<Integer> player) {
+        Integer leftPageNumber = player.get(0);
+        Integer rightPageNumber = player.get(1);
+
+        return validatePageRange(leftPageNumber) && validatePageRange(rightPageNumber)
+            && validateRightPage(rightPageNumber) && validateLeftPage(leftPageNumber)
+            && validateBetweenLeftAndRightPage(leftPageNumber, rightPageNumber);
+    }
+
     private static boolean validatePageRange(Integer pageNumber) {
         if (!(3 <= pageNumber && pageNumber <= 398)) {
             return false;
