c2017cc25a7ffc78e81cbfaed97062081417c3bb
areyouhun <areyouhun@gmail.com>
2022-10-27 00:58:13 +0900

    refactor(prob1): add final method to check existing validators

--- src/main/java/onboarding/feature1/PageValidator.java
+++ src/main/java/onboarding/feature1/PageValidator.java
@@ -3,6 +3,12 @@ package onboarding.feature1;
 import static onboarding.feature1.Constants.*;
 
 public class PageValidator {
+    public static boolean isValid (PageNumber pageNumber) {
+        if (!isConsecutive(pageNumber) || isFirstOrLast(pageNumber)) {
+            return false;
+        }
+        return true;
+    }
     public static boolean isConsecutive (PageNumber pageNumber) {
         if (pageNumber.getRightPage() - pageNumber.getLeftPage() == 1) {
             return true;
