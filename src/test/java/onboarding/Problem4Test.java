f88fba500ef62697c7171a7cede72954af508ff4
sangwon <swchoi1997@naver.com>
2022-10-27 00:34:02 +0900

    test : 단어리스트를 아스키코드값이 담긴 리스트로 변경 테스트

--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +18,19 @@ class Problem4Test {
 		assertThrows(IllegalArgumentException.class, () ->
 			Problem4.checkValidRange(0)
 		);
+	}
+
+	@Test
+	void 아스키코드로변경(){
+		String val = "I Love You";
+		List<Integer> result = Arrays.asList(73, 32, 76, 111, 118, 101, 32, 89, 111, 117);
+		// 73 32 76 111 118 101 32 89 111 117
+
+		assertThat(Problem4.turnWordIntoAscii(val.split(""))).isEqualTo(result);
+		val = "Choi Sang Won";
+		List<Integer> Choi_Sang_Won = Arrays.asList(67, 104, 111, 105, 32, 83, 97, 110, 103, 32, 87, 111, 110);
+
+		assertThat(Problem4.turnWordIntoAscii(val.split(""))).isEqualTo(Choi_Sang_Won);
+
 	}
 }
