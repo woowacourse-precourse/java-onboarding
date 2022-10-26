24c98a32d55ef403e32cf172b3bf9717be1a11b5
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-26 20:36:48 +0900

    feat(Problem5.java): 각 금액권으로 나누고 최종 결과 출력

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> dividors= Arrays.asList(
+                50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        ArrayList<Integer> answer=new ArrayList<>();
+        for(int i:dividors){
+            if(i>money)
+                answer.add(0);
+            else{
+                answer.add(money/i);
+                money%=i;
+            }
+        }
+
         return answer;
     }
 }
