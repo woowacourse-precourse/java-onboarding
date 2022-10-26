ddb6c2280ffea63588961b214765185eda9026c9
jaehyeon <jhjh0022@naver.com>
2022-10-27 02:07:56 +0900

    일단구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int result = check(number);
+
+        return result;
+    }
+
+    public static int check(int num){
+
+        int sum = 0;
+
+        for(int i=1; i<=num; i++){
+            String s = Integer.toString(i);
+            for(int j=0; j<s.length(); j++){
+                if((int) s.charAt(j) ==3 | (int)s.charAt(j) == 6 | (int)s.charAt(j) ==9) {
+                    sum+=1;
+                }
+            }
+
+        }
+        return sum;
     }
 }
