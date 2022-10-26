7cc68d6c603015a31daaa34a255d1cd93badb92f
Youngho <cyh6099@gmail.com>
2022-10-26 18:49:26 +0900

    feat : 문제1 - 한 명의 입력 값 중 각 자리 수 별로 더할 경우 최대값 도출

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    static int findSumMaximumValue(List<Integer> values) {
+        int sumResult = 0;
+        // 입력 값이 유효한 경우, 모든 자리 합의 경우 짝수 페이지가 무조건 크다.
+        String stringValue = values.get(1).toString();
+        for (int i = 0; i < stringValue.length(); i++) {
+            sumResult += Integer.parseInt(String.valueOf(stringValue.charAt(i)));
+        }
+        return sumResult;
+    }
 }
