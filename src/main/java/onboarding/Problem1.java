df505b48e0d95693be22c4ba872546290ede91bb
noah.lee <rhkd2612@naver.com>
2022-10-27 05:59:04 +0900

    docs: problem1 methods

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,15 @@ package onboarding;
 
 import java.util.List;
 
+/*
+* # 기능 정리
+* [O] 입력 검증
+* [O] 페이지 덧셈 메소드
+* [O] 페이지 곱셈 메소드
+* [O] 각 페이지 계산치 최고값 계산
+* [O] solution 작성
+* [O] 테스트 확인
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
