2c958031ee5ecc2c4215ac321a748fd6c3e20979
JunH <tjdtls690@gmail.com>
2022-10-26 22:36:50 +0900

    :sparkles: feat : 연속된 두 글자가 이미 존재하는지 확인하기 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,15 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    
+    public static boolean isExistTwoConsecutiveLetters(final Map<String, String> map, final String twoConsecutiveLetters) {
+        return map.containsKey(twoConsecutiveLetters);
     }
 }
