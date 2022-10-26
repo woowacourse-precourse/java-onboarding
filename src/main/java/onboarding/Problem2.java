961edccf8a95238b3fd1f59414aa04d75c3ad4cd
jeomxon <jeomxon@gmail.com>
2022-10-26 23:37:43 +0900

    feat: check it has repeated word

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        //중복이 있는지 체크하는 함수
+        //중복이 있으면 제거하는 함수
+
+        while (isRepeated(cryptogram)) {
+
+        }
+        
+
+        return "";
     }
+
+    public static boolean isRepeated(String word) {
+
+        for (int i = 0; i < word.length()-1; i++) {
+
+            if (word.substring(i, i + 1).equals(word.substring(i + 1, i + 2))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
