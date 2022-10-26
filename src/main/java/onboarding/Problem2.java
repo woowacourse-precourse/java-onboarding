3e27be0a7516e7cdb83a0d6f98b4e7910fb4242b
backFox <jorealbusiness@gmail.com>
2022-10-26 17:22:37 +0900

    feat(problem2): 문제 2 알고리즘 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return removeDuplicated(cryptogram);
+    }
+
+    public static String removeDuplicated(String target) {
+        String result = target.replaceAll("(.)\\1+", "");
+        if (!target.equals(result)) {
+            result = removeDuplicated(result);
+        }
+        return result;
     }
 }
