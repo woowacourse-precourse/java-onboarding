faa0634f78b4b4dbbad488ed6eccaa465a7cc790
jinny-l <jinny.l.dev@gmail.com>
2022-10-27 02:07:58 +0900

    [feat] 1부터 입력받은 숫자까지 "3" "6" "9"가 포함되어 있는 개수를 반환하는 메소드 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,15 @@ package onboarding;
 
 public class Problem3 {
 
+    // 1부터 입력받은 숫자까지 "3" "6" "9"가 포함되어 있는 개수를 반환하는 메소드
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String num = String.valueOf(i);
+            answer += count_3_6_9(num);
+        }
+
         return answer;
     }
 
