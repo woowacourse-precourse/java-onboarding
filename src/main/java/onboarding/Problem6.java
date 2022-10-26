7fe959e985137659b45ba5ea251e97382689f447
01192mg <01192mg@gmail.com>
2022-10-26 21:41:49 +0900

    feat: Problem6 닉네임을 2글자 토큰으로 변환하는 기능 구현
    '감자감자감자'처럼 반복되는 문자열의 경우 중복된 토큰을 생성하므로 `Set` 을 사용해서 중복을 제거해 주었다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            nicknameToToken(nickname);
+        }
         return answer;
+    }
+
+    private static Set<String> nicknameToToken(String nickname) {
+        Set<String> tokens = new HashSet<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String nicknameToken = nickname.substring(i, i + 2);
+            tokens.add(nicknameToken);
+        }
+        return tokens;
     }
 }
