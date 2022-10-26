8041e9790236631dc63a013b7445d94a4e4d6955
youngkyunIm <yee0230@naver.com>
2022-10-26 21:30:09 +0900

    feat(Problem1) : add check page
    정상적인 페이지인지 확인, 페이지는 2쪽 -> list.size == 2, 양쪽 페이지는 1 페이지 차이.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,15 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!checkPage(pobi) || !checkPage(crong)) {
+            return -1;
+        }
         return answer;
+    }
+
+    public static boolean checkPage(List<Integer> list) {
+        if (list.size() != 2) return false;
+        if (Math.abs(list.get(1) - list.get(0)) != 1) return false;
+        return true;
     }
 }
