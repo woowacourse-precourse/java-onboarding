6cfca36f2e3a76029b9e3287e8e31caf990556e3
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-27 00:26:46 +0900

    [문제1] - 페이지 계산 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,26 @@ class Problem1 {
 
         return leftPage > 0 && leftPage < 400 && rightPage > 1 && rightPage <= 400 && rightPage - leftPage == 1;
     }
+
+    public static int calcScore(List<Integer> pages) {
+        int[] scores = { 0, 0 };
+        for (int i = 0; i < pages.size(); i++) {
+            int page = pages.get(i);
+
+            int plusScore = 0, multiScore = 1;
+            while (page != 0) {
+                int score = page % 10;
+                plusScore += score;
+                multiScore *= score;
+
+                page /= 10;
+            }
+
+            scores[i] = Math.max(plusScore, multiScore);
+        }
+
+        return Math.max(scores[0], scores[1]);
+    }
     
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
