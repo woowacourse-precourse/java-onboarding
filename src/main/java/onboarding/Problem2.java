d9b9f8974c89a213f84c9597026731050c23425e
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-26 18:05:29 +0900

    feat: 중복된 문자 제거 기능 구현, 문제2 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,9 +9,30 @@ public class Problem2 {
         String answer = "answer";
         //암호문 리스트로 변환
         List<String> cryptogramList = wordToList(cryptogram);
+        answer = DeleteCoupleWord(cryptogramList);
         return answer;
     }
     public static List wordToList(String word){
         return new ArrayList<>(Arrays.asList(word.split("")));
+    }
+
+    public static String DeleteCoupleWord(List<String> cryptogramList){
+        while (true) {
+            int check = 0;
+            for (int i = 1; i < cryptogramList.size(); i++) {
+                //리스트 순회 하면서 연속된 문자 제거
+                if (cryptogramList.get(i).equals(cryptogramList.get(i - 1)) ) {
+                    cryptogramList.remove(i);
+                    cryptogramList.remove(i-1);
+                    check = 1;
+                    i-=2;
+                }
+            }
+            //연속된 문자가 없으면 종료
+            if (check == 0) {
+                break;
+            }
+        }
+        return String.join("", cryptogramList);
     }
 }
