39cb4f687263d886d91a41819d320e926d2dbdee
great-park <cksgh1735@naver.com>
2022-10-27 02:29:52 +0900

    feat(Problem1): refactoring solution - 역할 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    private static int pobiWin = 1;
+    private static int crongWin = 2;
+    private static int draw = 0;
+    private static int exception = -1;
+    private static List<Integer> startPage = List.of(1,2);
+    private static List<Integer> endPage = List.of(399,400);
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(validatePageNumbers(List.of(pobi, crong))){
+            return exception;
+        }// 이제 pobi, crong을 신뢰할 수 있다.
+
+        int pobiMaxValue = calculateMaxValue(pobi);
+        int crongMaxValue = calculateMaxValue(crong);
+
+        if (pobiMaxValue < crongMaxValue){
+            return crongWin;
+        }
+        if (crongMaxValue < pobiMaxValue){
+            return pobiWin;
+        }
+        return draw;
     }
 }
