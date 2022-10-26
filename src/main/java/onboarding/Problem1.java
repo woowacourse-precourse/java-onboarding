484cff07b01935cb6f6a0749dba125fee0735408
HwangSeokJin <ghkd3703@gmail.com>
2022-10-26 19:24:38 +0900

    포비와 크롱의 최대 점수 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiLeftMax = Integer.max(addAllNumbers(pobi.get(0)), multiplyAllNumbers(pobi.get(0)));
+        int pobiRightMax = Integer.max(addAllNumbers(pobi.get(1)), multiplyAllNumbers(pobi.get(1)));
+        int pobiScore = Integer.max(pobiLeftMax, pobiRightMax);
+
+        int crongLeftMax = Integer.max(addAllNumbers(crong.get(0)), multiplyAllNumbers(crong.get(0)));
+        int crongRightMax = Integer.max(addAllNumbers(crong.get(1)), multiplyAllNumbers(crong.get(1)));
+        int crongScore = Integer.max(crongLeftMax, crongRightMax);
+
+
+
         return answer;
     }
     private static int addAllNumbers(int page){
