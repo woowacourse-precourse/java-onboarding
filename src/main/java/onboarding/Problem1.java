6c7ee8652931b26f16794d56393968ac91ff3295
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-26 21:19:44 +0900

    feat: 점수를 비교해 가장 높은 사람이 게임의 승자를 가리는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,7 +8,7 @@ import java.util.List;
  * [o] 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
  * [o] 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
  * [o] 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
- * [ ] 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+ * [o] 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
  * [ ] 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
  * [ ] 포비와 크롱이 펼친 페이지가 들어있는 리스트/배열 pobi와 crong이 주어질 때, 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return 하도록 solution 메서드를 완성하라.
  */
@@ -26,6 +26,14 @@ class Problem1 {
             crongMax = Math.max(Math.max(getSumNumberOfDigits(page), getMultipleNumberOfDigits(page)),crongMax);
         }
 
+        if(pobiMax > crongMax) {
+            answer = 1;
+        } else if (pobiMax < crongMax) {
+            answer = 2;
+        } else if (pobiMax == crongMax) {
+            answer = 0;
+        }
+
         return answer;
     }
 
@@ -41,7 +49,7 @@ class Problem1 {
     }
 
     public static int getMultipleNumberOfDigits(int num) {
-        int multiple = 0;
+        int multiple = 1;
         String s = Integer.toString(num);
 
         for (int i = 0; i < s.length(); i++) {
