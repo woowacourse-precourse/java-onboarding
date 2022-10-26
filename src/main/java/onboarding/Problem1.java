fab51a97a45255c5df130c5b874bae3ea167a342
ym2629 <dydals2629@naver.com>
2022-10-26 12:24:41 +0900

    digSum and digMul
    return sum of digit and multiply of digit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,23 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    
+    int digSum(int num){
+    	int res = 0;
+    	while (num > 0){
+    		res += num % 10;
+    		num = num / 10;
+    	}
+    	return res;
+    }
+    
+    int digMul(int num) {
+    	int res = 1;
+    	while (num > 0)
+    	{
+    		res *= num % 10;
+    		num = num / 10;
+    	}
+    	return res;
+    }
 }
