bd8748e70e111e75d1517a15425c3aacce8c15e9
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-26 21:19:12 +0900

    add : (Problem3) 3,6,9 확인
    tsn에 3,6,9 문자가 올 것이기 때문에, 해당 파라미터를 통해 몇 개가 포함되어 있는지 확인하여 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int containChar(char[] num, char tsn){
+        int cnt = 0;
+        for(int i=0; i<num.length; i++){
+            if(num[i] == tsn) cnt++;
+        }
+        return cnt;
+    }
 }
