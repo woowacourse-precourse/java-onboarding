607ee7d7fb047dc93338aa8bcdbb142b44ada82a
JunH <tjdtls690@gmail.com>
2022-10-26 19:08:46 +0900

    :sparkles: feat : 숫자의 각 자리수 중 3,6,9인 수의 개수 구하기 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+    
+    private static int countOfThreeSixNine(int number) {
+        return (int) Arrays.stream(String.valueOf(number).split(""))
+                .mapToInt(Integer::parseInt)
+                .filter(eachDigit -> eachDigit % 3 == 0)
+                .count();
     }
 }
