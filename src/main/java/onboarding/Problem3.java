80d31090546f5986dc4b3413187ea231c9aed829
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 19:41:23 +0900

    기능 구현 목록 수정 및 숫자에 3, 6, 9가 들어가는지 체크하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,11 +4,15 @@ public class Problem3 {
     public static int solution(int number) {
         int answer;
         for (int i = 1; i < number; i++) {
-             i+=1;
+            if (containThreeSixNine(i)) {
+            }
         }
 
         return 1;
     }
 
-    pu
+    public static boolean containThreeSixNine(int num) {
+        String str = Integer.toString(num);
+        return str.contains("3") || str.contains("6") || str.contains("9");
+    }
 }
