ddc1c21d4884030a27b16622f4811ebcd4c735e6
Kim kyunghee <kkh2415@naver.com>
2022-10-26 23:47:59 +0900

    add : #Problem2-중복되지 않은 문자열만 저장하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * a. 중복되지 않은 문자열만 저장하는 기능 구현
+ *  a-1. 연속으로 중복되었는지 검사하는 기능 구현
+ * */
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decipherCryptogram(cryptogram);
+    }
+
+    private static String decipherCryptogram(String cryptogram) {
+        List<String> decipheringCryptogram = new ArrayList<>();
+
+        for (String s : cryptogram.split("")) {
+            int size = decipheringCryptogram.size();
+
+            if (isDuplication(decipheringCryptogram, s)) {
+                decipheringCryptogram.remove(size - 1);
+                continue;
+            }
+
+            decipheringCryptogram.add(s);
+        }
+
+        return String.join("", decipheringCryptogram);
+    }
+
+    private static boolean isDuplication(List<String> decipheringCryptogram, String s) {
+        int size = decipheringCryptogram.size();
+
+        if (size == 0) {
+            return false;
+        }
+
+        return decipheringCryptogram.get(size - 1).equals(s);
     }
 }
