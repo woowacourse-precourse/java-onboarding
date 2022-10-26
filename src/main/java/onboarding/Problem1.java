daa8919e2941e0a52fb6361f16113d50f281cf38
hstla <ree7243@gmail.com>
2022-10-26 17:13:14 +0900

    문제1 덧셈, 곱셈 비교하는 기능 구현 및 예외처리 기능 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,11 +3,41 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public int inputOfException (List<Integer> pobi,List<Integer> crong) {
         int input = 0;
-        if (pobi.get(1) != pobi.get(0) + 1) input = -1;
-        if (crong.get(1) != crong.get(0) + 1) input = -1;
+        if (pobi.size() == 2 && crong.size() == 2) return -1;
+        if (pobi.get(0) == 0 && crong.get(0) == 0) return -1;
+        if (pobi.get(1) != pobi.get(0) + 1) return  -1;
+        if (crong.get(1) != crong.get(0) + 1) return  -1;
         return input;
+    }
+
+    public int sumNumber (int i) {
+        int sum = 0;
+        while (i > 0) {
+            sum += i % 10;
+            i /= 10;
+        }
+        return sum;
+    }
+
+    public int multNumber (int i) {
+        int multi = 1;
+        while (i > 0) {
+            multi *= i % 10;
+            i /= 10;
+        }
+        return multi;
+    }
+
+    public int leftNum (int left) {
+        return Math.max(sumNumber(left),multNumber(left));
+    }
+
+    // leftNum이랑 같은 함수지만 기능 요구 사항에 맞게 구현
+    public int rightNum (int right) {
+        return Math.max(sumNumber(right),multNumber(right));
     }
 
 
@@ -15,8 +45,6 @@ class Problem1 {
         Problem1 p1 = new Problem1();
         int answer = Integer.MAX_VALUE;
         if (p1.inputOfException(pobi, crong) == -1) return -1;
-
-
 
         return answer;
     }
