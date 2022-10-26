9ada6869fada38ac67a1d6e0ed15a804b93a0d0c
23Yong <1239824@naver.com>
2022-10-27 00:04:16 +0900

    [feat] 주어진 숫자까지 박수의 수 더하기
    - 이때 clap 함수는 주어진 수에 박수를 쳐줄 것이라고 기대

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+        for (int num = 3; num <= number; num++) {
+            answer += clap(num);
+        }
+
         return answer;
+    }
+
+    private static int clap(int number) {
+        return 0;
     }
 }
