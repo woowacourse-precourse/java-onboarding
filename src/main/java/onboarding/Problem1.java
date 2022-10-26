c4ad59a67abb4935669367e1e0c574142360ebf8
MinSungKim <timkim01@naver.com>
2022-10-26 17:25:08 +0900

    feat : Problem1 예외처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,63 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+
+
         return answer;
     }
+
+}
+class Person{
+    private int leftPage;
+    private int rightPage;
+    private int score;
+
+    Person(List<Integer> pages){
+        this.leftPage = pages.get(0);
+        this.rightPage = pages.get(1);
+    }
+    public int getLeftPage() {
+        return leftPage;
+    }
+
+    public int getRightPage() {
+        return rightPage;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
+
+class ExceptionHandling{
+    private int leftPage;
+    private int rightPage;
+
+    ExceptionHandling(Person person){
+        this.leftPage = person.getLeftPage();
+        this.rightPage = person.getRightPage();
+    }
+
+    private boolean isConsecutivePages() {
+        if (rightPage - leftPage == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isInRange(){
+        if(leftPage > 0 && leftPage <= 400 && rightPage > 0 && rightPage <= 400){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isRightPageFormat(){
+        if(leftPage % 2 == 1 && rightPage % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
 }
