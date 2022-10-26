e3f3eaf3f7c41287a868a92e2403a892db2004a9
Fizioo0102 <a27313797@gmail.com>
2022-10-26 23:58:00 +0900

    book_range_check() 함수를 작성. book_range_check()함수는 책의 쪽수 값이 올바른 값인지 아닌지  boolean 값을 반환해주는 함수이다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,16 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    static boolean book_range_check(int left, int right) {
+        if(left<0 || right<0 || left>400 || right >400){
+            return false;
+        }
+        if(left%2 != 1 || right%2 != 0) {
+            return false;
+        }
+        if(left+1 != right){
+            return false;
+        }
+        return true;
+    }
 }
