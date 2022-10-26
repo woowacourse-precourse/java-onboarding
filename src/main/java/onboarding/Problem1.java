4ab9478be1b91a64b9a9a3b1e279e8f4ef0cb09b
dongwoo <shp05179@gmail.com>
2022-10-26 23:35:09 +0900

    페이지 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(1)-pobi.get(0)!=1 || crong.get(1)-crong.get(0)!=1) {
+            return -1;
+        }
 
+        return 0;
     }
 
     static int getMax(List<Integer> player) {
