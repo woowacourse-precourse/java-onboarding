d3d7f44fcba4c9d51569d1c5bede2511797eadf9
squirMM <jsm6616@naver.com>
2022-10-27 02:50:01 +0900

    :rocket: feat(Problem4) makeFun, fin Test, put Comment

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        // 공백으로 단어 구분
+        String[] strings = word.split(" ");
+        for(int i=0;i<strings.length;i++){
+            strings[i]=changeAlpha(strings[i]);
+        }
+        // join 함수를 통해 배열 전환했던 문자열을 통합
+        return String.join(" ",strings);
+    }
+    public static String changeAlpha (String w){
+        char[] chars=w.toCharArray();
+        // casting 으로 intToChar or charToInt 가능
+        int upper=(int)'A'+(int)'Z';
+        int lower=(int)'a'+(int)'z';
+        for(int i=0; i<chars.length; i++){
+            // 대소문자 판별
+            if(Character.isUpperCase(chars[i]))chars[i]=(char)(upper-(int)chars[i]);
+            else{
+                chars[i]=(char)(lower-(int)chars[i]);
+            }
+        }
+        // char 형 배열을 string 으로 변환
+        return String.valueOf(chars);
     }
 }
