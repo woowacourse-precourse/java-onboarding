9ee9bed8ddfcb96b6855a2f38c5e41d59cc62e02
KimJaeSung <behappy0916@naver.com>
2022-10-26 22:46:00 +0900

    java-onboarding-1-예외사항 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,14 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(pobi.get(0) != pobi.get(1) -1 || crong.get(0) != crong.get(1)-1){
+            return -1;
+        }
+
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        while(true){
+
+        }
         return answer;
     }
 }
