553f54a1a735d18e7aa79f20a21cd828dabd9167
K-jun98 <alap_u@naver.com>
2022-10-27 00:05:29 +0900

    feat - 돈의 단위로 나누고 리스트에 저장하여 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static List<Integer> moneyUnit = Arrays.asList(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getUnitCount(money);
+    }
+
+    private static List<Integer> getUnitCount(int money) {
+        List<Integer> unitCount = new ArrayList<>();
+        for (int i = 0; i < moneyUnit.size(); i++) {
+            unitCount.add(money / moneyUnit.get(i));
+            money %= moneyUnit.get(i);
+        }
+        return unitCount;
     }
 }
