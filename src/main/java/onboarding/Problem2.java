8b63b0714127ddb9b38400eb628a202702e92f88
code-tree <dltjdqls1200@naver.com>
2022-10-26 23:09:26 +0900

    feat(Problem2): Decrypto the password

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = getDecrypto(cryptogram);
+
         return answer;
+    }
+
+    private static String getDecrypto(String cryptogram) {
+        Stack<Character> cryptor = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (char crypto : cryptogram.toCharArray()) {
+            if (!cryptor.isEmpty() && cryptor.peek() == crypto) {
+                cryptor.pop();
+                continue;
+            }
+
+            cryptor.push(crypto);
+        }
+
+        cryptor.forEach(result::append);
+
+        return result.toString();
     }
 }
