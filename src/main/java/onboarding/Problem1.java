b71c9d219e0fdbdcd996182f728ce9625e778790
whibaek <jibaek0513@gmail.com>
2022-10-27 01:05:21 +0900

    P1 Method for Exception Situation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static boolean checkException(int n1, int n2){
+        if(n1%2 == 1 && n2%2 == 0) {
+            if (n2 - n1 == 1){
+                if( (n1 != 1 && n2 != 2) && (n1 != 399 && n2 != 400) )
+                    return true;
+            }
+        }
+        return false;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
