a0388437f17e4887c44b37510299b71b170882c8
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 19:51:14 +0900

    숫자에 3,6,9 가 들어갈 때 3, 6, 9의 개수 만큼 손뼉을 친 수를 증가시키는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,18 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer;
-        for (int i = 1; i < number; i++) {
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
             if (containThreeSixNine(i)) {
+                answer += countThreeSixNine(i);
             }
         }
-
-        return 1;
+        return answer;
     }
 
     public static boolean containThreeSixNine(int num) {
         String str = Integer.toString(num);
         return str.contains("3") || str.contains("6") || str.contains("9");
+    }
+
+    public static int countThreeSixNine(int num) {
+        String[] str = Integer.toString(num).split("");
+        return (int)Arrays.stream(str).filter(i -> i.equals("3") || i.equals("6") || i.equals("9")).count();
     }
 }
