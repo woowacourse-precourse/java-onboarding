d384190d0e2be357cf5f85947b6bb648eed7000e
5uhwann <5uhwann27@gmail.com>
2022-10-26 18:25:08 +0900

    feat: PROBLEM1 - 기능1 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,18 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+  public static int solution(List<Integer> pobi, List<Integer> crong) {
+    int answer = Integer.MAX_VALUE;
+    return answer;
+  }
+
+  private static int plusNum(int num) {
+    int returnNum = 0;
+    String pageNum = String.valueOf(num);
+    for (int i = 0; i < pageNum.length(); i++) {
+      returnNum += pageNum.charAt(i) - '0';
     }
+    return returnNum;
+  }
 }
