88a501d98f37c7cdcf7b125edd378ede660fbeb4
starwook <star.wookk@gmail.com>
2022-10-26 20:56:18 +0900

    add return clap method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+    public static int reClap(int num){
+        int count = 0;
+        for(int i=1;i<=num;i++){
+            count += count369(i);
+        }
+        return count;
+    }
+    public static int count369(int num){
+        int count =0;
+
+        return count;
     }
 }
