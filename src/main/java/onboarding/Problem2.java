ef4020aa9d60c6e71436adb3edf78013af1634af
main <jang0929@naver.com>
2022-10-26 20:27:49 +0900

    Problem 2 바꿀 값이 있을 경우 바꾸는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,7 @@ package onboarding;
  */
 public class Problem2 {
     /**
-     * 바꿀 값이 있는지 확인하는 함수
+     * 1. 바꿀 값이 있는지 확인하는 함수
      * @param s
      * @return 바꿀값이 시작되는 index를 return
      */
@@ -17,8 +17,27 @@ public class Problem2 {
         return -1;
     }
 
+    /**
+     * 2. 바꿀 값이 있을 경우 바꾸는 함수
+     * @param cryptogram
+     * @return 바꿔진 값을 재귀로 리턴 만약 바꿀값이 없을 경우 String 반환
+     */
+
+    static String calc(String cryptogram){
+        int res = checkPossible(cryptogram);
+        if(res == -1) return cryptogram;
+        int end = res;
+        for(; end< cryptogram.length(); end++){
+            if(cryptogram.charAt(res) == cryptogram.charAt(end)) continue;
+            break;
+        }
+        cryptogram = cryptogram.substring(0, res) + cryptogram.substring(end);
+        return calc(cryptogram);
+
+    }
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
     }
 }
