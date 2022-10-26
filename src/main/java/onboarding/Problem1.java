db055905941bbdf56eb7f140239de8ba48342746
baexxbin <soo3886@gmail.com>
2022-10-26 22:18:03 +0900

    Feat: "getScore 함수"
    가장 큰 페이지 결과값 반환 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int getScore(List<Integer> pageList) {
+		int max = Integer.MIN_VALUE;
+		int[] numList;
+		int add;
+		int mul;
+
+		for (int page : pageList) {
+			String number = String.valueOf(page);
+			numList = Stream.of(number.split("")).mapToInt(Integer::parseInt).toArray();
+			add = Arrays.stream(numList).sum();
+			mul = Arrays.stream(numList).parallel().reduce(0, (a, b) -> a * b);
+			max = Math.max(Math.max(add, mul), max);
+		}
+		return max;
+	}
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
 }
