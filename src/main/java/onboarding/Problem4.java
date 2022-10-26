3dde33cab7d6267899fe3200d5386165ed68ab6e
2jun0 <soo28819@gmail.com>
2022-10-27 04:05:26 +0900

    Feat: 문제 4번 - 청개구리 문자 메서드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,22 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static char turnToTreeFrogChar(char c) {
+
+        char srtAlpha;
+        if (Character.isUpperCase(c)) {
+            srtAlpha = 'A';
+        } else {
+            srtAlpha = 'a';
+        }
+
+        int alphaIdx = c - srtAlpha;
+
+        char treeFrogChar = (char)(srtAlpha + (25 - alphaIdx));
+        return treeFrogChar;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
