bf9421084e6feb9ab4328f90b760925470b49bd2
Gaby <gaby@Gabyui-MacBookPro.local>
2022-10-26 23:07:15 +0900

    Problem 2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        StringBuilder crypt = new StringBuilder(cryptogram);
+        while (true) {
+            boolean dup = false;
+            for (int i = 0; i < crypt.length(); ++i) {
+                int tmp = 1;
+                while (i + tmp < crypt.length() && crypt.charAt(i) == crypt.charAt(i + tmp))
+                    tmp++;
+                if (tmp > 1) {
+                    dup = true;
+                    for (int j = 1; j <= tmp; ++j)
+                        crypt.deleteCharAt(i);
+                }
+            }
+            if (!dup)
+                break;
+        }
+        String answer = crypt.toString();
+
         return answer;
     }
 }
