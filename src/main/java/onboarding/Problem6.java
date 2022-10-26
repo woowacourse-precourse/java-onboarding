880082025257e9ac542decdb40eca2df6ad0ee27
JunH <tjdtls690@gmail.com>
2022-10-26 23:22:20 +0900

    :art: refactor : 메서드 기능 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,35 +5,43 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Set<String> emails = new HashSet<>();
-        Map<String, String> map = new HashMap<>();
-        
-        for (List<String> form : forms) {
-            Map<String, String> tmp = new HashMap<>();
-            final String crewName = form.get(1);
-            final String email = form.get(0);
-            
-            for (int index = 0; index < crewName.length() - 1; index++) {
-                final String consecutiveLetters = crewName.substring(index, index + 2);
-                
-                if (map.containsKey(consecutiveLetters)) {
-                    emails.add(email);
-                    emails.add(map.get(consecutiveLetters));
-                }
-                
-                tmp.put(consecutiveLetters, email);
-            }
-            
-            map.putAll(tmp);
-        }
+        Set<String> emails = duplicateNicknameEmails(forms);
     
-        System.out.println(emails);
         return emails.stream()
                 .sorted()
                 .collect(Collectors.toList());
     }
     
-    public static boolean isExistTwoConsecutiveLetters(final Map<String, String> map, final String twoConsecutiveLetters) {
+    private static Set<String> duplicateNicknameEmails(final List<List<String>> forms) {
+        Set<String> emails = new HashSet<>();
+        Map<String, String> map = new HashMap<>();
+        
+        for (List<String> form : forms) {
+            addConsecutiveLettersOfEachCrew(emails, map, form);
+        }
+        return emails;
+    }
+    
+    private static void addConsecutiveLettersOfEachCrew(final Set<String> emails, final Map<String, String> map, final List<String> form) {
+        Map<String, String> tmp = new HashMap<>();
+        final String crewName = form.get(1);
+        final String email = form.get(0);
+        
+        for (int index = 0; index < crewName.length() - 1; index++) {
+            final String consecutiveLetters = crewName.substring(index, index + 2);
+            
+            if (isExistTwoConsecutiveLetters(map, consecutiveLetters)) {
+                emails.add(email);
+                emails.add(map.get(consecutiveLetters));
+            }
+            
+            tmp.put(consecutiveLetters, email);
+        }
+        
+        map.putAll(tmp);
+    }
+    
+    private static boolean isExistTwoConsecutiveLetters(final Map<String, String> map, final String twoConsecutiveLetters) {
         return map.containsKey(twoConsecutiveLetters);
     }
 }
