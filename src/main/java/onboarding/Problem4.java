9f62aec2bb8800ee46a9dd58b7f73bc59cce210a
yj2980 <adore2100@naver.com>
2022-10-26 20:50:24 +0900

    feat : 알파벳을 String으로 반환하는 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,10 +4,18 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
+        answer = returnAlpha(word);
 
         return answer;
     }
-    
+    public static String returnAlpha(String word)
+    {
+        String reverseStr = "";
+        for (int i = 0; i < word.length(); i++)
+            reverseStr += reverseAlpha(word.charAt(i));
+
+        return reverseStr;
+    }
     //알파벳을 반대로 변경
     public static char reverseAlpha(char alpha)
     {
@@ -16,11 +24,17 @@ public class Problem4 {
 
         //대문자일 경우
         if (Character.isUpperCase(alpha))
+        {
             minus = alpha - 'A';
+            result = (char)(alpha + 25 - (minus * 2));
+        }
         else if (Character.isLowerCase(alpha))
+        {
             minus = alpha - 'a';
-
-        result = (char)(25 - (minus * 2));
+            result = (char)(alpha + 25 - (minus * 2));
+        }
+        else
+            result = alpha;
         return result;
     }
 }
