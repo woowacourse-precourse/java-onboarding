582b176c2bc0a4a902792a87d7e888c0a2b49551
Kayoung <rkdkdudjd@gmail.com>
2022-10-27 02:37:33 +0900

    4번: getOppositeLetter 메서드
    인자로 들어온 알파벳의 반대 글자를 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,4 +8,16 @@ public class Problem4 {
         }
         return answer.toString();
     }
+
+    private static char getOppositeLetter(char c) {
+        if (!Character.isAlphabetic(c)) {
+            return c;
+        }
+        if (Character.isLowerCase(c)) {
+            int diff = 'z' - c;
+            return (char)('a' + diff);
+        }
+        int diff = 'Z' - c;
+        return (char)('A' + diff);
+    }
 }
