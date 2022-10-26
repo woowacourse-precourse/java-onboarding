e5ee86a2010dbedaeef9551ecafde5e9f30d3dc0
aksmf1442 <aksmf1442@naver.com>
2022-10-27 02:28:59 +0900

    feat: Problem5 각 단위별 화폐의 개수를 구하고 남은 돈을 반환해주는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    static List<Integer> moneyPerUnitCounts = new ArrayList<>();
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        return moneyPerUnitCounts;
+    }
+
+    private static int giveChange(int money, int moneyPerUnit) {
+        return money % moneyPerUnit;
     }
 }
