39ff111edbcda49d09b35391255fada5ab12e4be
noah.lee <rhkd2612@naver.com>
2022-10-27 06:10:10 +0900

    docs: problem2 methods

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+/*
+ * # 기능 정리
+ * [X] 연속하는 중복문자 검증
+ * [X] 제거된 문자열 반환 메서드
+ * [X] 1~2 기능 반복 메서드
+ * [X] solution 작성
+ * [X] 테스트 확인
+ */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
