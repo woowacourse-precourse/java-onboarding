b0213929b3a4629c3b38f4f465c24d28394e0419
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-26 20:15:35 +0900

    init: Problem1 기능 요구 사항 정리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,15 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * [ ] 책을 임의로 펼친다.
+ * [ ] 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+ * [ ] 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+ * [ ] 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+ * [ ] 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+ * [ ] 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+ * [ ] 포비와 크롱이 펼친 페이지가 들어있는 리스트/배열 pobi와 crong이 주어질 때, 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return 하도록 solution 메서드를 완성하라.
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
