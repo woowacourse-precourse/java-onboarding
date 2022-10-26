8ea57f8d8c66e6e75257d478a967e3eb4107c287
main <jang0929@naver.com>
2022-10-26 20:34:18 +0900

    Problem5 기능 사항 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 사항
+ * 1. 지폐및 코인 개수를 리턴하는 함수
+ */
 public class Problem5 {
+
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
     }
 }
