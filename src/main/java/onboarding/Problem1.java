4da58c9cbd2c500ac21afb0c471f7c6817168100
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 02:47:56 +0900

    Problem1 solution update

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,43 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    //test commit
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int left;
+        int right;
+        //exception check
+        if(pobi.get(0)+1 != pobi.get(1) || crong.get(0)+1 != crong.get(1)) return -1;
+
+        //find max value
+        left = Math.max(plusBookNum(pobi.get(0)), multiBookNum(pobi.get(0)));
+        right = Math.max(plusBookNum(pobi.get(1)), multiBookNum(pobi.get(1)));
+        int pobi_max = Math.max(left, right);
+        left = Math.max(plusBookNum(crong.get(0)), multiBookNum(crong.get(0)));
+        right = Math.max(plusBookNum(crong.get(1)), multiBookNum(crong.get(1)));
+        int crong_max = Math.max(left, right);
+
+        if(pobi_max > crong_max) answer = 1;
+        else if(pobi_max < crong_max) answer = 2;
+        else if(pobi_max == crong_max) answer = 0;
         return answer;
+    }
+
+    public static int plusBookNum(int num){
+        int sum=0;
+        while(num > 0) {
+            sum+= num %10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static int multiBookNum(int num){
+        int sum=1;
+        while(num > 0) {
+            sum*= num %10;
+            num /= 10;
+        }
+        return sum;
     }
 }
