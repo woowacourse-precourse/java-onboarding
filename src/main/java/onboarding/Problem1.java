d081b15ca897e7802fa70898e627ce3a7c30562c
ddubidubap <park62000@gmail.com>
2022-10-27 01:31:45 +0900

    PROBLEM1 solution 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -42,7 +42,32 @@ class Problem1 {
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        // 페이지 변수 설정
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
+        // 예외사항 확인
+        if (!correctGiven(pobi) || !correctGiven(crong))
+            return -1;
+
+        // 포비의 왼쪽 오른쪽 중 큰 값
+        int pobiLeft = sumOrProduct(pobiLeftPage);
+        int pobiRight = sumOrProduct(pobiRightPage);
+        int pobiResult = Math.max(pobiLeft, pobiRight);
+
+        // 크롱의 왼쪽 오른쪽 중 큰 값
+        int crongLeft = sumOrProduct(crongLeftPage);
+        int crongRight = sumOrProduct(crongRightPage);
+        int crongResult = Math.max(crongLeft, crongRight);
+
+        // 승자 선정
+        if (pobiResult > crongResult)
+            return 1;   // 포비 승
+        else if (pobiResult < crongResult)
+            return 2;   // 크롱 승
+        else
+            return 0;   // 무승부
     }
 }
