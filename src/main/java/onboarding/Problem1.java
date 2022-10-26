18ab1c8497a310f066a8c2be47b4e066a49130cc
iamjooon2 <iamjooon2@gmail.com>
2022-10-27 00:41:39 +0900

    feat: page 유효성 검사 함수 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,28 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!validatePageConsecutive(pobi) || !validatePageConsecutive(crong)) {
+            return -1;
+        }
+        if (!validatePageRange(pobi) || !validatePageRange(crong)) {
+            return -1;
+        }
+        if (!validatePageSequence(pobi) || !validatePageSequence(crong)) {
+            return -1;
+        }
+        return 1;
     }
+
+    private static boolean validatePageSequence(List<Integer> list) {
+        return list.get(1) > list.get(0);
+    }
+
+    private static boolean validatePageConsecutive(List<Integer> list) {
+        return list.get(1) - list.get(0) == 1;
+    }
+
+    private static boolean validatePageRange(List<Integer> list) {
+        return list.get(0) > 1 ||  list.get(1) < 400;
+    }
+
 }
