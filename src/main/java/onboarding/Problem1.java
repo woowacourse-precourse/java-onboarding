7726665e6fb98db439c65f15f5077c7c6ae6b9ad
1eejisoo <jisu991012@gmail.com>
2022-10-26 23:40:32 +0900

    [Problem 1] Feat: 각 자리 수를 모두 더하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,15 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    // 페이지 번호의 각 자리 숫자를 모두 더하는 기능을 하는 함수
+    public static int getSumPageNum(Integer pageNum){
+        int sum = 0;
+        while (pageNum > 0) {
+            sum += pageNum % 10;
+            pageNum /= 10;
+        }
+        return sum;
+    }
+
 }
