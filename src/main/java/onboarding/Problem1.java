5740a1e823ad305058922e12ff1f5cb94f59d01c
Kayoung <rkdkdudjd@gmail.com>
2022-10-27 00:45:38 +0900

    1번: getMaxValueFromList 메서드
    입력된 리스트의 수로 계산하여 가장 큰 값을 반환하는 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,4 +13,22 @@ class Problem1 {
         }
         return 0;
     }
+
+    public static int getMaxValueFromList(List<Integer> intList) {
+        int first = intList.get(0);
+        int second = intList.get(1);
+        return Math.max(getMaxValueFromNumber(first), getMaxValueFromNumber(second));
+    }
+
+    public static int getMaxValueFromNumber(int number) {
+        int sum = 0, product = 1;
+        do {
+            int lsn = number % 10;
+            sum += lsn;
+            product *= lsn;
+            number = number / 10;
+        } while (number != 0);
+        return Math.max(sum, product);
+    }
+
 }
