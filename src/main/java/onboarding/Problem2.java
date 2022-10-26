728d1828b607f5db5c925c471e22791ed8e71e90
main <jang0929@naver.com>
2022-10-26 20:23:48 +0900

    Problem 2 기능 사항 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/** 기능 사항
+ * 1. 바꿀 값이 있는지 확인하는 함수
+ * 2. 바꿀 값이 있을 경우 바꾸는 함수
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
