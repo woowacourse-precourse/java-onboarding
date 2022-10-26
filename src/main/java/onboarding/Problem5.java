c0022f91a78951bf12b1ff32485681129383b825
JunH <tjdtls690@gmail.com>
2022-10-26 20:25:58 +0900

    :sparkles: feat : 오만원 권 개수 구하기 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        
+        money = numberOfCurrency(money, 50000);
+        
         return answer;
+    }
+    
+    private static int numberOfCurrency(final int currentTotalMoney, final int monetaryUnit) {
+        return currentTotalMoney / monetaryUnit;
     }
 }
