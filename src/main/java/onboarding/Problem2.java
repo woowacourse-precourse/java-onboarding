e35016dccda33fa1250124fd40ae1fafd3f987b1
hoin <apicad0307@gmail.com>
2022-10-26 23:00:51 +0900

    feat: 요구사항2 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    static Stack<Character> stack = new Stack<>();
+    static StringBuilder sb = new StringBuilder();
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String answer = "";
+        stack.push(cryptogram.charAt(0));
+        for(int i=1; i<cryptogram.length(); i++){
+            if(stack.peek() == cryptogram.charAt(i)) stack.pop();
+            else{
+                stack.push(cryptogram.charAt(i));
+            }
+        }
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("zyelleyz"));
     }
 }
