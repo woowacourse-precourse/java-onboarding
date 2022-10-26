9ecea6a92e33d25b98d9eb0ba92fafc5bf87cd47
23Yong <1239824@naver.com>
2022-10-26 19:10:04 +0900

    [feat] 예외사항 처리
    예외 사항
    1. Null을 고려
    2. 시작면이나 마지막 페이지를 펼치지 않도록 한다. (시작면이나 마지막 페이지가 의미하는 바를 0페이지, 401페이지로 간주)
    3. 왼쪽 페이지 번호 + 1 는 오른쪽 페이지 번호와 같아야 한다.
    4. 1 <= 페이지 번호의 범위 <= 400
    5. 왼쪽 페이지 번호는 홀수, 오른쪽 페이지 번호는 짝수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,56 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int DRAW = 0;
+    private static final int EXCEPTION_CASE = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        
+        Integer pobiLeftPage = pobi.get(0);
+        Integer pobiRightPage = pobi.get(1);
+
+        Integer crongLeftPage = crong.get(0);
+        Integer crongRightPage = crong.get(1);
+
+        if (isExceptionCase(pobiLeftPage, pobiRightPage, crongLeftPage, crongRightPage)) {
+            return EXCEPTION_CASE;
+        }
+
         return answer;
+    }
+
+    private static boolean isExceptionCase(Integer pobiLeftPage, Integer pobiRightPage, Integer crongLeftPage, Integer crongRightPage) {
+        if (isNullPage(pobiLeftPage) || isNullPage(pobiRightPage) || isNullPage(crongLeftPage) || isNullPage(crongRightPage)) {
+            return true;
+        }
+        if (!isContinuousPage(pobiLeftPage, pobiRightPage) || !isContinuousPage(crongLeftPage, crongRightPage)) {
+            return true;
+        }
+        if (!isValidRangePage(pobiLeftPage, pobiRightPage) || !isValidRangePage(crongLeftPage, crongRightPage)) {
+            return true;
+        }
+        if (!isValidPage(pobiLeftPage, pobiRightPage) || !isValidPage(crongLeftPage, crongRightPage)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isNullPage(Integer page) {
+        return page == null;
+    }
+
+    private static boolean isContinuousPage(Integer leftPage, Integer rightPage) {
+        return rightPage.equals(leftPage + 1);
+    }
+
+    private static boolean isValidRangePage(Integer leftPage, Integer rightPage) {
+        return leftPage >= 1 && rightPage <= 400;
+    }
+
+    private static boolean isValidPage(Integer leftPage, Integer rightPage) {
+        return leftPage % 2 != 0 && rightPage % 2 == 0;
     }
 }
