87060bc5fd22e9beca7c7e029826bfe827e78f3e
JHS <wnghdtjr129@naver.com>
2022-10-26 21:30:28 +0900

    feat: 입력받은 페이지 값 검증 로직 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,7 +9,13 @@ import java.util.List;
  */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (validationPages(pobi) || validationPages(crong)) {
+            return -1;
+        }
+        return 0;
+    }
+
+    private static boolean validationPages(List<Integer> pages) {
+        return (pages.get(0) + 1 != pages.get(1)) || pages.get(0) % 2 != 1 || pages.get(1) % 2 != 0;
     }
 }
