04a1d5de0703009d51974aeb3104ea7c2d7577f4
ys200209 <lys200209@naver.com>
2022-10-26 22:51:11 +0900

    feat(문제1): 데이터 값 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,41 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        try {
+            validate(pobi);
+            validate(crong);
+        } catch (IllegalStateException ex) {
+            return -1;
+        }
+
         return answer;
+    }
+
+    private static void validate(List<Integer> pages) {
+        if (pages.size() != 2) {
+            throw new IllegalStateException("페이지는 총 2쪽이어야 합니다. 현재 페이지 수 : " + pages.size());
+        }
+
+        if (pages.get(0) % 2 != 1) {
+            throw new IllegalStateException("왼쪽 페이지가 홀수가 아닙니다. 왼쪽 페이지 : " + pages.get(0));
+        }
+
+        if (pages.get(1) % 2 != 0) {
+            throw new IllegalStateException("오른쪽 페이지가 짝수가 아닙니다. 오른쪽 페이지 : " + pages.get(1));
+        }
+
+        if (pages.get(1) <= pages.get(0) || pages.get(1) - pages.get(0) != 1) {
+            throw new IllegalStateException("페이지가 잘못되었습니다.");
+        }
+
+        for (Integer page : pages) {
+            if (page < 1 || page > 400) {
+                throw new IllegalStateException("페이지의 범위가 잘못되었습니다. 올바른 범위 : 1 ~ 400, 현재 페이지 : " + page);
+            }
+        }
     }
 }
