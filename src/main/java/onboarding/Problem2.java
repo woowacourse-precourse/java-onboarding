66c868d1ec3f46f9048b0a914f8cc549df845d2f
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-26 22:13:21 +0900

    docs(Problem2): 구현할 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+// <기능 목록>
+// 1. cipher(n) == cipher(n+1)일 때까지 탐색
+// 2. cipher(n) != cipher(n+m)일 때까지 탐색
+// 3. cipher(n) ~ cipher(n+m-1)을 제거한 문장으로 cipher 초기화
+// 4. cipher(n+1)부터 다시 탐색 시작 -> 문장이 끝날때까지
+// 5. 1번~4번 반복
+// 반복문 break 조건 : 연속하는 중복문자가 없을 때, string length가 2 미만
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
