8a3b2d56ec42eb4dd890c748f0234f2c834b3a5b
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-10-26 20:29:55 +0900

    Problem4 최종 완료
    불필요한 검증 삭제

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,28 +10,26 @@ public class Problem4 {
         Z	Y	X	W	V	U	T	S	R	Q	P	O	N	M	L	K	J	I	H	G	F	E	D	C	B	A
 
         구현 메서드
-        검증 1 :  word의 길이가 1이상 1000이하인가
-        검증 2 : word에 알파벳과 띄어쓰기 외 문자는 변환하지 않는다.
-        검증 3 : 대문자는 대문자로, 소문자는 소문자로 변환한다. >> 아스키코드로 판별
+        조건 1 :  word의 길이가 1이상 1000이하로 주어진다.
+        검증 1 : word에 알파벳과 띄어쓰기 외 문자는 변환하지 않는다.
+        검증 2 : 대문자는 대문자로, 소문자는 소문자로 변환한다. >> 아스키코드로 판별
          */
 
         String answer = "";
 
-        //검증1
-        if(word.length()>=1 && word.length()<=1000){
-
-            //검증 2,3
-            for (int i = 0; i < word.length(); i++) {
-                if(word.charAt(i)>='A' && word.charAt(i)<='Z'){
-                    answer+=(char)('A'+'Z'-(word.charAt(i)));
-                }else if(word.charAt(i)>='a' && word.charAt(i)<='z'){
-                    answer+=(char)('a'+'z'-word.charAt(i));
-                }else{
-                    answer+=word.charAt(i);
-                }
+        //검증 1,2
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                answer += (char) ('A' + 'Z' - (word.charAt(i)));
+            } else if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') {
+                answer += (char) ('a' + 'z' - word.charAt(i));
+            } else {
+                answer += word.charAt(i);
             }
         }
+
         return answer;
     }
-
 }
+
+
