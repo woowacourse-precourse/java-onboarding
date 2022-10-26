62760041bc8bcec66a4d36786668ee2f55c8c7fd
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-26 20:05:18 +0900

    add : (Problem1) 입력값 확인
    문제 파라미터의 입력이 올바른지 확인한다. 만일 올바르지 않다면 -1을 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        /*
+        * 설명 : 입력값이 올바르지 않는 경우 -1를 반환한다.
+        * */
+        if(!isPageRight(pobi) || !isPageRight(crong)) return -1;
+
+
         return answer;
+    }
+
+    private static boolean isPageRight(List<Integer> human){
+        int leftPage = human.get(0);
+        int rightPage = human.get(1);
+
+        return ((rightPage - leftPage) == 1);
     }
 }
