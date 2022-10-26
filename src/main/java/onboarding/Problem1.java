99fa68f580dece68b212e1aded68086a424db1ab
chordpli <chordplaylist@gmail.com>
2022-10-27 00:11:20 +0900

    feat(problem1) : 메소드 설계

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,58 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 왼쪽 페이지가 짝수거나, 페이지가 연결되지 않으면 -1 return;
+        // 포비의 수
+        // 왼쪽 페이지 번호의 계산값
+            // 덧셈
+            // 곱셈
+        // 오른쪽 페이지 번호의 계산값
+            // 덧셈
+            // 곱셈
+        // 큰 계산값 반환
+
+        // 크롱의 수
+        // 왼쪽 페이지 번호의 계산값
+            // 덧셈
+            // 곱셈
+        // 오른쪽 페이지 번호의 계산값
+            // 덧셈
+            // 곱셈
+        // 큰 계산값 반환
+
+        // 점수 비교
+        // 리턴
+
         return answer;
+    }
+
+    // pobi의 값과 crong의 값을 비교하여 결과를 리턴
+    public int compareNumber(List<Integer> pobi, List<Integer> crong) {
+        int result = Integer.MAX_VALUE;
+
+        return result;
+    }
+
+    // 덧셈과 곱셈 비교 후 큰 수 리턴
+    public int returnBigNum(List<Integer> people) {
+        // 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+        int result = Integer.MAX_VALUE;
+
+        return result;
+    }
+
+    // 각 자릿수를 더한 후 큰 수 반환
+    public int returnPlusNum(int page) {
+        int result = Integer.MAX_VALUE;
+
+        return result;
+    }
+
+    // 각 자릿수를 곱하기 큰 수 반환
+    public int returnMulipleNum(int page) {
+        int result = Integer.MAX_VALUE;
+
+        return result;
     }
 }
