f40aca3b3ed994f67595bdf11a4bd4beb2feed62
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-26 23:30:40 +0900

    [Problem 1] getMaxPoint를 통해 유저의 최댓값을 받아와 연산의 결과를 결정한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,25 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        answer = getGameResult(pobi, crong);
         return answer;
     }
+
+    private static int getGameResult(List<Integer> userA, List<Integer> userB){
+        int userAPoint = getMaxPoint(userA);
+        int userBPoint = getMaxPoint(userB);
+
+
+        if(userAPoint == -1 || userBPoint == -1){
+            return -1;
+        } else if (userAPoint>userBPoint) {
+            return 1;
+        } else if (userAPoint<userBPoint) {
+            return 2;
+        } else if (userAPoint == userBPoint) {
+            return 0;
+        }
+        return -1;
+    }
+
 }
