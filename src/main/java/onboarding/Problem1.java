8632b140ad07b63f1becc8ae6c8e0b62e2911ed6
YE-EUN <musiclove529@naver.com>
2022-10-26 17:13:57 +0900

    feature : add getScore() - Problem 1의 4번 기능 구현 - 점수 구하는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,28 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static final int TOTAL_PAGE_NUM = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int getScore(List<Integer> pageNums){
+        int score = 0;
+        int leftPage = pageNums.get(0);
+        int rightPage = pageNums.get(1);
+
+        //왼쪽 페이지 가장 큰 수 구하기
+        int leftScore = getBiggerNum(leftPage);
+
+        //오른쪽 페이지 가장 큰 수 구하기
+        int rightScore = getBiggerNum(rightPage);
+
+        //최종 점수 선택
+        score = Math.max(leftScore, rightScore);
+
+        return score;
+    }
+
 }
