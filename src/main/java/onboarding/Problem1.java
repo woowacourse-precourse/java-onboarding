24c4a68e64a19a64579098b8fdc2c4127e94b8b3
YE-EUN <musiclove529@naver.com>
2022-10-26 17:36:55 +0900

    feature : add getBiggerNum() - Problem 1의 2, 3번 기능 구현 - getSumOfEachDigit() : 각 자리 수의 합 구하는 함수 - getProductOfEachDigit() : 각 자리 수의 곱 구하는 함수 - 페이지 수의 각 자리 수의 합과 곱 중 큰 수를 구함

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,7 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    static final int TOTAL_PAGE_NUM = 400;
+    static final int TOTAL_PAGES = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
@@ -15,16 +15,47 @@ class Problem1 {
         int leftPage = pageNums.get(0);
         int rightPage = pageNums.get(1);
 
-        //왼쪽 페이지 가장 큰 수 구하기
+        //왼쪽 페이지에서 얻어지는 가장 큰 수 구하기
         int leftScore = getBiggerNum(leftPage);
 
-        //오른쪽 페이지 가장 큰 수 구하기
+        //오른쪽 페이지에서 얻어지는 가장 큰 수 구하기
         int rightScore = getBiggerNum(rightPage);
 
         //최종 점수 선택
         score = Math.max(leftScore, rightScore);
 
         return score;
+    }
+
+    public static int getBiggerNum(int page){
+        int biggerNum = 0;
+
+        int sumOfEachDigit = getSumOfEachDigit(page);
+        int productOfEachDigit = getProductOfEachDigit(page);
+
+        biggerNum = Math.max(sumOfEachDigit, productOfEachDigit);
+
+        return biggerNum;
+    }
+
+    public static int getSumOfEachDigit(int num){
+        int sum = 0;
+        while(num != 0){
+            int digit = num % 10;
+            sum += digit;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static int getProductOfEachDigit(int num){
+        int product = 1;
+        while(num != 0){
+            int digit = num % 10;
+            product *= digit;
+            num /= 10;
+        }
+        return product;
     }
 
 }
