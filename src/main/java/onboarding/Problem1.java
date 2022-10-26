1d89963403a6c97f09ee49cfa1d9c90abbaa74a1
meunrud <y3a0522@gmail.com>
2022-10-27 02:47:25 +0900

    4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,6 +11,19 @@ class Problem1 {
         pobi = new ArrayList<>(Arrays.asList(page));
         crong = new ArrayList<>(Arrays.asList(page));
 
+        int pobisum = sum(pobi.get(0), pobi.get(1));
+        int pobimulti = multi(pobi.get(0),pobi.get(1));
+        int crongsum = sum(crong.get(0),crong.get(0));
+        int crongmulti = multi(crong.get(0),crong.get(1));
+
+        int pobiMax = Math.max(pobisum,pobimulti);
+        int crongMax = Math.max(crongsum,crongmulti);
+
+
+
+
+
+
 
         int answer = Integer.MAX_VALUE;
         return answer;
@@ -19,29 +32,48 @@ class Problem1 {
     public static Integer randomNum(){
         Random random = new Random();
         Integer randomNum  = 0;
-        randomNum = 2*random.nextInt(401)+1;
+        randomNum = 2*random.nextInt(400)+3;
         return randomNum;
     }
 
-    public static int sum(Integer a){
-        int sum = 0;
-        while(a != 0){
-           sum += a%10;
-           a /= 10;
-        }return sum;
+    public static int sum(Integer a, Integer b){
+        int [] sum1 = new int [2];
+        int answer = 0;
+        while(a != 0 && b !=0){
+                 sum1[0]+= a%10;
+                 sum1[1]+= b%10;
+                 b /= 10;
+                 a /= 10;
+
+
+        }return answer = Math.max(sum1[0],sum1[1]);
     }
 
-    public static int multi(Integer a){
-        String str = String.valueOf(a);
-        int [] num = new int[str.length()];
-        Integer multi = 0;
-        for(int i = 0; i<str.length(); i++){
-            for(int k = 0; k<str.length(); k++){
-                String b = str.substring(i,i+1);
-               num [k]= Integer.parseInt(b);
-               multi = num[k] * num[k+1];
-            }
-        }return multi;
+    public static int multi(Integer a, Integer b){
+        int multi = 0;
+       if(a == 99){
+           int a1 = a/10%10;
+           int a2 = a/10;
+           int b1 = b/100%10;
+           int b2 = b/10%10;
+           int b3 = b/10;
+           multi = Math.max((a1*a2),(b1*b2*b3));
+       }else if (a<99){
+           int a1 = a/10%10;
+           int a2 = a/10;
+           int b1 = b/10%10;
+           int b2 = b/10;
+            multi = Math.max((a1*a2),(b1*b2));
+       }else if(a>100){
+           int a1 = a/10%10;
+           int a2 = a/10;
+           int a3 = a/100%10;
+           int b1 = b/100%10;
+           int b2 = b/10%10;
+           int b3 = b/10;
+           multi = Math.max((a1*a2*a3),(b1*b2*b3));
+       }
+       return multi;
     }
 
 
