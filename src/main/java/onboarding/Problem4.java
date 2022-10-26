ba393bc50a0df4806df7e8480a3472c58b4ec9b8
Laeng <Laeng@users.noreply.github.com>
2022-10-27 00:34:01 +0900

    docs(problem4): 기능 구현 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /**
+     * 문자열 검증
+     * @param word 문자열
+     * @return 검증 결과 값
+     */
+    private static boolean validateWord(String word) {
+        int lengthWord = word.length();
+        return (lengthWord > 0 && lengthWord <= 1000);
+    }
+
+
 }
