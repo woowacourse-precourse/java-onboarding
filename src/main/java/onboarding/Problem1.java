bd875a5ee48980d6624c09581c57a988e5b44fa8
ParkRootSeok <g_e_un@naver.com>
2022-10-26 21:35:50 +0900

    feat : 1. 임의로 페이지 펼치기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,89 +3,58 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.List;
 
+/* 기능 요구 사항 */
+// 1. 책을 임의로 펼친다.
+// 2. 각 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+// 3. 위 과정에서 가장 큰 수를 본인의 점수로 한다.
+// 4. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+// 5. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+// -> 필요한 기능 1. randomPage 2. calculateScore 3. compareScore
+
+
 class Problem1 {
 
-    /* 점수 계산 메소드 */
-    public static int calculateScore(List<Integer> page) {
-
-        int result, left, right;
-        List<Integer> score = new ArrayList<>();
-
-        // 페이지 별 점수 구하기 (0 : 좌 / 1 : 우)
-        for (int i = 0; i < 2; i++) {
-
-            List<Integer> digit = new ArrayList<>();
-
-            // 페이지 정보 읽기
-            int page_info = page.get(i);
-
-            // 페이지 파싱
-            while (page_info > 0) {
-                digit.add(page_info % 10);
-                page_info /= 10;
-            }
-
-            // 각 자릿수의 합과 곱 비교
-            score.add(compareScore(digit));
-
-        }
-
-        // 좌, 우 페이지 점수 비교 (0 : 좌 / 1 : 우)
-        left = score.get(0);
-        right = score.get(1);
-        result = left >= right ? left : right;
-
-        return result;
+    public static boolean isOdd(int num) {
+        if (num % 2 == 1) return true;
+        else return false;
     }
 
-    /* 각 자릿수 합과 곱 비교 메소드 */
-    public static int compareScore(List<Integer> digit) {
+    public static List<Integer> randomPage() {
 
-        int sum_score, mul_score;
+        List<Integer> page = new ArrayList<>();
+        int left, right;
 
-        sum_score = 0;
-        mul_score = 1;
+        // 왼쪽 페이지는 홀수
+        do {
+            left = (int)(Math.random() * 400);
+        } while (isOdd(left));
 
-        for (int n : digit) {
-            sum_score += n;
-            mul_score *= n;
-        }
+        // 페이지 번호가 순서대로 들어 있으므로 오른쪽 페이지 = 왼쪽 페이지 + 1
+        right = left + 1;
 
-        return sum_score >= mul_score ? sum_score : mul_score;
+        // 리스트에 추가
+        page.add(left);
+        page.add(right);
+
+        return page;
 
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
         int answer = Integer.MAX_VALUE;
-
-        /* 예외 처리 */
-        // 1. R - L != 1 일 경우 예외 처리
-        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
-            answer = -1;
-            return answer;
-        }
-
-        // 2. 첫 페이지(1) 또는 마지막 페이지(400)일 경우 예외 처리
-        if (pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(1) == 400 || crong.get(1) == 400) {
-            answer = -1;
-            return answer;
-        }
-
-        /* 점수 계산 및 비교 */
-        if (calculateScore(pobi) > calculateScore(crong)) {
-            // 포비 승
-            answer = 1;
-        } else if (calculateScore(pobi) < calculateScore(crong)) {
-            // 크롱 승
-            answer = 2;
-        } else {
-            // 무승부
-            answer = 0;
-        }
-
         return answer;
 
     }
+
+    public static void main(String[] args) {
+
+        /* 책을 임의로 펼친다 */
+        List<Integer> A = randomPage();
+        List<Integer> B = randomPage();
+
+
+    }
+
 
 }
