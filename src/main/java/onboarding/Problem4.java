b142d819e2789639054e84b06b07b7206c8f7d24
twknds <twknds@naver.com>
2022-10-26 23:16:47 +0900

    [4번 feature] : change메서드 대소문자 외 특별문자 구별 공식 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+    public static char[] change(String word){
+        char[] arr=word.toCharArray();
+        int cnt = 0;
+        while (cnt<arr.length){
+            if (arr[cnt]>=65 && arr[cnt]<=90){ //대문자면
+
+            }
+            else if (arr[cnt]>=97 && arr[cnt]<=122){ //소문자면
+
+            }
+            else{ //알파벳이 아니면
+
+            }
+        }
+        return arr;
+    }
     public static String solution(String word) {
         String answer = "";
+        change(word);
         return answer;
     }
 }
