2777a863e21a902f86db377d13fe4c05760a17e7
qkrdmswl <eunji303@naver.com>
2022-10-27 02:13:43 +0900

    [feat] problem2: solution 함수 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -39,7 +39,7 @@ public class Problem2 {
     }
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = decryptor(cryptogram);
         return answer;
     }
 }
