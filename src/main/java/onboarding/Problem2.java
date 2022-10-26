b463d66d831787284405adbfc7827133a57bb8e9
jmj005 <jmj00005@naver.com>
2022-10-26 21:54:17 +0900

    feat: 중복 문자 체크 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+/*
+1. 중복 문자 체크
+2. 중복 문자 제거
+ */
+
 public class Problem2 {
+
+    private static int checkOverlap(String word) {
+        for (int i = 1; i < word.length(); i ++) {
+            if (word.charAt(i - 1) == word.charAt(i))
+                return 1;
+        }
+        return 0;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
         return answer;
     }
 }
