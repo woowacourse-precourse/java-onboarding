6b9aaf262c57c1256f1e1da52ebb615e4ae12b3b
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-27 02:21:54 +0900

    [Problem1] 기능 목록 설계

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    /**
+     * 기능 목록
+     * 1. 페이지 번호의 각 자리 숫자 모두 더하기
+     * 2. 페이지 번호의 각 자리 숫자 모두 곱하기
+     * 3. 숫자 비교 후 큰 수 구하기
+     * 4. 점수 비교 후 게임 승자 결정
+     * 5. 예외 사항
+     */
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
