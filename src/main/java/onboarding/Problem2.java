67ee2d5b50f4b0bc21f49591bbd41089aefca7ab
HwangSeokJin <ghkd3703@gmail.com>
2022-10-26 19:53:44 +0900

    중복된 문자를 제거하며 스택에 저장

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
+import java.util.*;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: cryptogram.toCharArray()){
+            if (!stack.empty() && stack.peek().equals(c)){
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
         return answer;
     }
 }
