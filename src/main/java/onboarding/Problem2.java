f08bebb333cf7e6513c073b5071347df8d0a2359
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-27 01:33:41 +0900

    feat: 암호 해독기 추가 - 복호화 기능 추가 - 복호화된 메시지 출력 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,41 @@
 package onboarding;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+  static class Decryption {
+    private final Stack<Character> message = new Stack<>();
+
+    public void decrypt(Character c) {
+      try {
+        if (this.isSameCharacter(c)) {
+          message.pop();
+          return;
+        }
+        message.push(c);
+      } catch (EmptyStackException e) {
+        message.push(c);
+      }
     }
+
+    public String getMessage() {
+      return message.stream()
+              .map(Object::toString)
+              .collect(Collectors.joining());
+    }
+
+    private boolean isSameCharacter(Character c) {
+      return message.peek() == c;
+    }
+  }
+
+  public static String solution(String cryptogram) {
+    Decryption decryption = new Decryption();
+    for(char code: cryptogram.toCharArray()) {
+      decryption.decrypt(code);
+    }
+    return decryption.getMessage();
+  }
 }
