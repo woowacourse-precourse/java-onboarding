1bb704d7b5225911ed4b4f7934ba415a36f967df
thdwoqor <celebarte01@gmail.com>
2022-10-26 23:36:23 +0900

    add exception `1~400 outside range`

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -14,6 +14,11 @@ class Problem1 {
         return Math.max(add, multiply);
     }
 
+    public static boolean getRangeCheck(int num){
+        if (num<1 || num>400) return true;
+        else return false;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
@@ -22,15 +27,16 @@ class Problem1 {
         int crongL=crong.get(0).intValue();
         int crongR=crong.get(1).intValue();
 
-        if(Math.abs(pobiL-pobiR)!=1 || Math.abs(pobiL-pobiR)!=1) {
-            answer = -1;
-        }else {
-            int pobiMax = Math.max(getCompare(pobiL), getCompare(pobiR));
-            int crongMax = Math.max(getCompare(crongL), getCompare(crongR));
-            if (pobiMax > crongMax) answer=1;
-            else if (pobiMax < crongMax) answer=2;
-            else answer=0;
-        }
+        if (getRangeCheck(pobiL) || getRangeCheck(pobiR) || getRangeCheck(crongL) || getRangeCheck(crongR))
+            return -1;
+        if(Math.abs(pobiL-pobiR)!=1 || Math.abs(pobiL-pobiR)!=1)
+            return -1;
+
+        int pobiMax = Math.max(getCompare(pobiL), getCompare(pobiR));
+        int crongMax = Math.max(getCompare(crongL), getCompare(crongR));
+        if (pobiMax > crongMax) answer=1;
+        else if (pobiMax < crongMax) answer=2;
+        else answer=0;
         return answer;
     }
 }
