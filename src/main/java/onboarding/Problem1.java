80bdc9462d827e303bb2d7732dff5a838bf3a0d5
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-10-26 22:06:38 +0900

    문제 1번. 숫자의 각 자릿수를 더하는 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    //입력받은 숫자의 각 자릿수를 더하는 메서드
+    private static int sumNum(int num) {
+        int result = 0;
+        while (num > 0) {
+            result += num%10;
+            result /= 10;
+        }
+        return result;
+    }
 }
