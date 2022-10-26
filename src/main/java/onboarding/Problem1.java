e11e8827009ef59ecc7e164d84bd6d6d4a993c13
jin8073 <jin8073@gmail.com>
2022-10-26 22:35:20 +0900

    feat(problem1): 페이지 숫자 자릿수 별로 분리하여 리스트로 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+
+/** 구현 기능 목록
+ * 1. 페이지 숫자 자릿수 별로 분리하여 리스트로 반환
+ * 2. 분리된 숫자의 합 반환
+ * 3. 분리된 숫자의 곱 반환
+ * 4. 둘 중 더 큰 숫자 반환
+ * 5. 이긴 사람 정하기
+ * 6. 예외 처리
+ * */
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public List<Integer> getPageNumbers(int page) {
+        List<Integer> pageNumbers = new ArrayList<>();
+        while (page > 0) {
+            pageNumbers.add(page % 10);
+            page /= 10;
+        }
+        return pageNumbers;
     }
 }
