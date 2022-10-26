a4607ffd7983fb7b13da636d52db89d859290d1d
Park Sungchul <try615@naver.com>
2022-10-26 19:31:39 +0900

    [feat] Problem1.java : 각 자리 숫자의 곱셈 연산 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    public static int solution(List<Integer> pobi, List<Integer> 현crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static int operMulti(int[] arr) {
+        int result = 1;
+        for (int num : arr) {
+            result *= num;
+        }
+        return result;
+    }
+
 }
