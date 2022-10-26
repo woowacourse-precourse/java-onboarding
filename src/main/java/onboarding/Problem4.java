56e6b053f3d261c09d9f1029f5194c5b962e77d8
aksmf1442 <aksmf1442@naver.com>
2022-10-27 01:31:29 +0900

    style: Problem4 불필요한 주석 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,17 +1,7 @@
 package onboarding;
 
-/**
- * goal: word가 주어질 때 청개구리 사전을 참고하여 반대로 변환한 값을 리턴
- */
-
 import java.util.HashMap;
 import java.util.Map;
-
-/**
- * 문제 풀이 순서
- * 1. map에 청개구리 사전을 a~z까지 등록한다.
- * 2. 주어진 word의 글자를 하나씩 확인하면서 청개구리 사전 map을 통해 문자열을 반환한다.
- */
 
 public class Problem4 {
 
@@ -36,12 +26,10 @@ public class Problem4 {
     }
 
     private static void createTreeFrogMap() {
-        // 소문자 매핑
         char start = 'a';
         char end = 'z';
         mappingAlphabetToMap(start, end);
 
-        // 대문자 매핑
         start = 'A';
         end = 'Z';
         mappingAlphabetToMap(start, end);
