aa54cd7845c90c7969c23bcbfcd17364991ff11e
yj2980 <adore2100@naver.com>
2022-10-26 19:25:36 +0900

    fix : 중복하는 단어를 제거하는 함수 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,7 +8,7 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
-		remove(createList(cryptogram));
+		answer = listToString(remove(createList(cryptogram)));
         return answer;
     }
 
@@ -26,20 +26,34 @@ public class Problem2 {
 	//중복하는 단어를 제거
 	public static List<Character> remove(List<Character> duplication)
 	{
-		for (int i = 0; i < duplication.size(); i++)
+		for (int i = 0; i < duplication.size() - 1; i++)
 		{
-			if (i + 1 <= duplication.size())
+			System.out.println("hi");
+			if (duplication.get(i) == duplication.get(i + 1))
 			{
-				if (duplication.get(i) == duplication.get(i + 1))
-				{
-					duplication.remove(i);
-					duplication.remove(i + 1);
-				}
+				duplication.remove(i);
+				duplication.remove(i);
+				i--;
+			}
+
+			if (i + 1 == duplication.size() - 1)
+			{
+				if (!duplicaionCheck(duplication))
+					i = -1;
 			}
 		}
 		return duplication;
 	}
 
+	public static boolean duplicaionCheck(List<Character> list)
+	{
+		for (int i = 0; i < list.size() - 1; i++)
+		{
+			if (list.get(i) == list.get(i + 1))
+				return false;
+		}
+		return true;
+	}
 	public static String listToString(List<Character> list)
 	{
 		String str = "";
