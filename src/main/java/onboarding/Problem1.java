42a49878549f1714bb609b0e066781bb53117c68
01192mg <01192mg@gmail.com>
2022-10-26 17:22:52 +0900

    feat: Problem1 전체 기능 구현
    포비와 크롱의 점수를 구해서 알맞은 응답 결과(1, 2, 0, -1)를 반환하는 기능을 구현했다.
    * 스트림 사용 이유
    입력 크기가 작고 로직이 간단하므로 성능상 반복문이 더 좋지만, 반복문 대신 stream을 사용해서 코드가 간결하게 보여지는 게 더 중요하다고 생각해서 작성했다.
    * if else 대신 if 연속 사용 이유
    응답을 반환하는 answer 변수를 선언해서 각각의 if, if-else 문에 해당하는 값을 대입한 후 최종적으로 return 을 해도 되지만, 추가적인 비즈니스 로직이 없으므로 if 문을 통과하지 않으면 바로 return 을 해줌으로써 통과하지 않은 조건을 바로 머릿속에서 제외할 수 있어서 코드를 읽기 편하게 해준다고 생각해서 사용했다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,17 +1,26 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
         try {
             validatePages(pobi);
             validatePages(crong);
         } catch (IllegalArgumentException e) {
             return -1;
         }
-        return answer;
+
+        int pobiScore = scoreFrom(pobi);
+        int crongScore = scoreFrom(crong);
+        if (pobiScore == crongScore) {
+            return 0;
+        }
+        if (pobiScore > crongScore) {
+            return 1;
+        }
+        return 2;
     }
 
     private static void validatePages(List<Integer> pages) {
@@ -27,5 +36,34 @@ class Problem1 {
         if (rightPage - leftPage != 1) {
             throw new IllegalArgumentException("연속된 페이지 번호를 입력해주세요.");
         }
+    }
+
+    private static int scoreFrom(List<Integer> pages) {
+        return pages.stream()
+                .map(Problem1::findMaxScore)
+                .max(Integer::compareTo).orElse(-1);
+    }
+
+    private static int findMaxScore(Integer page) {
+        Integer[] digits = pageToDigitNumbers(page);
+        return Math.max(addNumbers(digits), multiplyNumbers(digits));
+    }
+
+    private static Integer[] pageToDigitNumbers(Integer page) {
+        int digit = String.valueOf(page).length();
+        Integer[] digitNumbers = new Integer[digit];
+        for (int i = 0; i < digit; i++) {
+            digitNumbers[i] = page % 10;
+            page /= 10;
+        }
+        return digitNumbers;
+    }
+
+    private static int multiplyNumbers(Integer[] digits) {
+        return Arrays.stream(digits).reduce(1, (integer, integer2) -> integer * integer2);
+    }
+
+    private static int addNumbers(Integer[] digits) {
+        return Arrays.stream(digits).reduce(0, Integer::sum);
     }
 }
