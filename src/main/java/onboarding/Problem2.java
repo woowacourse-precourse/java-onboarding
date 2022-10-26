3631769cc9c919f4be25f7b5671b1b76f8fc3ad4
kevstevie <jianna442@gmail.com>
2022-10-26 17:09:43 +0900

    feat: add cryptogram to stack

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        char[] chars = cryptogram.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for(char ch : chars){
+            stack.push(ch);
+        }
         return answer;
     }
 }
