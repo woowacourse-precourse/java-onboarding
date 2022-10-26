145d3fa76fbc50cb3afa9e68622bf505d9992650
01192mg <01192mg@gmail.com>
2022-10-26 23:25:26 +0900

    feat: Problem7 사용자와 함께 아는 친구의 수를 계산하는 기능 구현
    사용자와 이미 친구인 사람을 구해서 리스트에 추가하고 친구 관계를 순회하면서 친구 관계에 있는 사람이 리스트에 있는 경우 점수를 더해준다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, Integer> userToScore = new HashMap<>();
+        List<String> myFriends = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (friend.get(0).equals(user)) {
+                myFriends.add(friend.get(1));
+            } else if (friend.get(1).equals(user)) {
+                myFriends.add(friend.get(0));
+            }
+        }
+
+        for (List<String> friend : friends) {
+            if (friend.get(0).equals(user) || friend.get(1).equals(user)) {
+                continue;
+            }
+            if (myFriends.contains(friend.get(0))) {
+                userToScore.put(friend.get(1), userToScore.getOrDefault(friend.get(1), 0) + 10);
+            }
+            if (myFriends.contains(friend.get(1))) {
+                userToScore.put(friend.get(0), userToScore.getOrDefault(friend.get(0), 0) + 10);
+            }
+        }
         return answer;
     }
 }
