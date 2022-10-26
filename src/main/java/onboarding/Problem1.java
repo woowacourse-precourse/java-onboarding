c8fd22057d534dbe871bd352a9baf241b953756c
Damgom <wisewish1@gmail.com>
2022-10-26 19:06:09 +0900

    FEAT: compare to left page number & right page number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,10 +11,13 @@ class Problem1 {
         String number = String.valueOf(num);
         int sum = 0;
         int multiple = 1;
-        for(int i = 0; i < number.length(); i++) {
-            sum += number.charAt(i)-'0';
-            multiple *= number.charAt(i)-'0';
+        for (int i = 0; i < number.length(); i++) {
+            sum += number.charAt(i) - '0';
+            multiple *= number.charAt(i) - '0';
         }
         return Math.max(sum, multiple);
+    }
+    public static int maxValue(int leftNum, int rightNum) {
+        return Math.max(higherValue(leftNum), higherValue(rightNum));
     }
 }
