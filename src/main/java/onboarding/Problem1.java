eae9b4b35822852f7700be7fb0a681926c36e865
syeonbot <asdf748596123@gmail.com>
2022-10-27 01:08:34 +0900

    feat: problem1 왼쪽 최댓값과 오른쪽 최댓값 중 최댓값 찾기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,29 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static final int LEFT_PAGE = 0;
+    public static final int RIGHT_PAGE = 1;
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+    public static int getMaxScore(List<Integer> pages) {
+        int leftPageNumber = getLeftPageNumber(pages);
+        int leftPageNumberMax = findMaxOfMultiAndAdd(leftPageNumber);
+
+        int rightPageNumber = getRightPageNumber(pages);
+        int rightPageNumberMax = findMaxOfMultiAndAdd(rightPageNumber);
+
+        return Math.max(leftPageNumberMax, rightPageNumberMax);
+    }
+
+    private static int getRightPageNumber(List<Integer> pages) {
+        return pages.get(RIGHT_PAGE);
+    }
+
+    private static int getLeftPageNumber(List<Integer> pages) {
+        return pages.get(LEFT_PAGE);
     }
     private static int findMaxOfMultiAndAdd(int pageNum) {
         int sum = addElements(pageNum);
