c3168d31d8421c7af4306d4b0d6948a08b13851c
CHAYEONIL <jeay123@naver.com>
2022-10-26 22:28:46 +0900

    문제 1 더하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,4 +6,23 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
     }
+
+    // 더하기
+    private static int sum(List<Integer> value) {
+
+        int value1 = 0;
+        int value2 = 0;
+
+        String[] array1 = String.valueOf(value.get(0)).split("");
+        for (String val : array1) {
+            value1 = value1 + Integer.valueOf(val);
+        }
+
+        String[] array2 = String.valueOf(value.get(1)).split("");
+        for (String val : array2) {
+            value2 = value2 + Integer.valueOf(val);
+        }
+
+        return max(value1, value2);
+    }
 }
