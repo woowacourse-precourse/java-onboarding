adac99ccdd7f951c4399fc6c0bf2c36c73f24148
jeonghoon <shb03323@naver.com>
2022-10-27 01:43:33 +0900

    feat: solve problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = makeResult(cryptogram);
         return answer;
+    }
+
+    private static String makeResult(String cryptogram) {
+        int resultLength = cryptogram.length() + 1;
+        while (cryptogram.length() != resultLength) {
+            resultLength = cryptogram.length();
+            cryptogram = reduceCryptogram(cryptogram);
+        }
+        return cryptogram;
+    }
+
+    private static String reduceCryptogram(String cryptogram) {
+        String cloneCryptogram = "";
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (i != 0 && cryptogram.charAt(i - 1) == cryptogram.charAt(i)) {
+                continue;
+            } else if (i != cryptogram.length() - 1 && cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                continue;
+            }
+            cloneCryptogram += cryptogram.charAt(i);
+        }
+        return cloneCryptogram;
     }
 }
