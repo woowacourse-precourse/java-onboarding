3ef106e2e44566681ae14a43cc7aeb1f7ee57922
drunkenhw <drunkenhw@gmail.com>
2022-10-26 22:21:28 +0900

    feat(Problem1) : 페이지 번호를 받아 각 자리 숫자를 구한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static List<Integer> splitToDigitList(int number) {
+
+        List<Integer> result = new ArrayList<>();
+        while (number != 0) {
+            result.add(number % 10);
+            number /= 10;
+        }
+
+        return result;
+    }
+
 }
