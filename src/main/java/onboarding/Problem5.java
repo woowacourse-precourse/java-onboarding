0f27adaca29857307aaba87174168ad4e94435b0
iamminho <minho100227@kakao.com>
2022-10-26 20:05:51 +0900

    Problem5 feat(getMoneyConversion): money만큼 화폐의 갯수를 변환하여 반환하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,29 +1,31 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = getMoneyConversion(money);
         return answer;
     }
 
-    static int[] getMoneyConversion (int money) {
+    static List<Integer> getMoneyConversion (int money) {
         int[] currencyArr = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        int[] countArr = new int[] {0,0,0,0,0,0,0,0,0};
+        List<Integer> countList = Arrays.asList(0,0,0,0,0,0,0,0,0);
 
+        convertMoney(money, currencyArr, countList);
 
-
-
-        return countArr;
+        return countList;
     }
 
-    static void convertMoney(int money, int[] currencyArr, int[] countArr) {
+    static void convertMoney(int money, int[] currencyArr, List<Integer> countList) {
         int len = currencyArr.length;
+
         for (int i = 0; i < len; i++) {
             int count = getCount(money, currencyArr[i]);
-
+            money = deductMoney(money, currencyArr[i], count);
+            countList.set(i, count);
         }
     }
 
@@ -35,7 +37,13 @@ public class Problem5 {
         return 0;
     }
 
-    static int deductMoney(int money, int count) {
-        return money - count;
+    static int deductMoney(int money, int currency, int count) {
+        return money - count * currency;
+    }
+
+    public static void main(String[] args) {
+        int money = 50237;
+        List<Integer> list =getMoneyConversion(money);
+        list.stream().forEach(System.out::print);
     }
 }
