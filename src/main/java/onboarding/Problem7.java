4437c160bbbe318893f1b11e730c977f25acefd2
main <jang0929@naver.com>
2022-10-26 20:50:17 +0900

    Problem7 기능 사항 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 사항
+ * 1. 추천할 친구 class 인 RecFriend 구현
+ * 2. 친구 관계를 만드는 함수
+ * 3. 친구 관계일 경우 10점을 추가하는 함수
+ * 4. 방문자일 경우 1점을 추가하는 함수
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
