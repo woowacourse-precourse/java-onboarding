42876340c06b6e08399f92a71b14375a1dd1705c
jmj005 <jmj00005@naver.com>
2022-10-26 23:23:27 +0900

    feat: 손뼉 수 구하기 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+/*
+1. 손뼉 수 구하기
+ */
+
 public class Problem3 {
+
+    private static int countClap(int number) {
+        int tmp, sum = 0;
+        for (int i = 1; i <= number; i ++) {
+            tmp = i;
+            while (tmp > 0) {
+                if (tmp % 10 == 3 || tmp % 10 == 6 || tmp % 10 == 9)
+                    sum++;
+                tmp /= 10;
+            }
+        }
+        return sum;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        answer = countClap(number);
         return answer;
     }
 }
