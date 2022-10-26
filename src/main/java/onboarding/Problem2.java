350a41dbeaf488aaefdb3fdea146f4f8611c3dd9
chosunghyun18 <krismon@naver.com>
2022-10-27 01:39:29 +0900

    [2번] 기능 요구 사항 1 번 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
+    public static String solution(String cryptogram)
+    {
         String answer = "answer";
+        answer = checkCryptogram(cryptogram);
         return answer;
+    }
+    public static String checkCryptogram(String cryptogram)
+    {
+        String crypto = "";
+
+        return crypto;
     }
 }
