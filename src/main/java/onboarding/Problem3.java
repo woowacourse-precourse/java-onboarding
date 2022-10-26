7603261665585480b49142d2b1b4fc19b75e652a
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-26 19:31:19 +0900

    1부터 주어진 값까지 숫자를 증가시키는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int answer;
+        for (int i = 1; i < number; i++) {
+             i+=1;
+        }
+
+        return 1;
     }
 }
