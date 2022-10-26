ac33537b82bc417603730ef6e0920e017570bc7a
iamminho <minho100227@kakao.com>
2022-10-26 19:08:00 +0900

    Problem4 feat(upperCaseConversion): 대문자를 반대로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -19,4 +19,15 @@ public class Problem4 {
         }
         return false;
     }
+
+    static Character upperCaseConversion(char character) {
+        int move = character - 'A';
+        char result =  (char) ('Z' - move);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(upperCaseConversion('Z'));
+    }
 }
