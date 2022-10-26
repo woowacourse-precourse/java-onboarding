495667fbbf90192f698718aa89e7a01577e340a9
JunH <tjdtls690@gmail.com>
2022-10-26 20:34:17 +0900

    :sparkles: feat : 화폐 단위 배열 초기화하기 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,18 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        
-        money = numberOfCurrency(money, 50000);
-        
-        return answer;
+    private static final List<Integer> UNITS_OF_MONEY = unitsOfMoneyInit();
+    
+    private static List<Integer> unitsOfMoneyInit() {
+        return List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     }
     
-    private static int numberOfCurrency(final int currentTotalMoney, final int monetaryUnit) {
-        return currentTotalMoney / monetaryUnit;
+    public static List<Integer> solution(int money) {
+        List<Integer> answer = new ArrayList<>();
+        
+        return answer;
     }
 }
