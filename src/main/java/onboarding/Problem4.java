3052a9e831756450fe9daac8ff9c34980965a7b6
yj2980 <adore2100@naver.com>
2022-10-26 20:17:47 +0900

    feat : 알파벳을 배열에 저장하는 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,31 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
         return answer;
+    }
+
+    //알파벳 저장
+    public static char[] createAlphaArray(String isAlpha)
+    {
+        int alphabetCount  = 26;
+        char[] alphaArray = new char[alphabetCount];
+
+        for (int i = 0; i < alphaArray.length; i++)
+        {
+            if (isAlpha.equals("Upper"))
+                alphaArray[i] = (char)('A' + i);
+            else if (isAlpha.equals("Lower"))
+                alphaArray[i] = (char)('a' + i);
+        }
+        return alphaArray;
+    }
+    //알파벳을 반대로 변경
+    public static String reverseAlpha(char alpha)
+    {
+        //대문자일 경우
+        if (Character.isUpperCase(alpha))
+            alpha
     }
 }
