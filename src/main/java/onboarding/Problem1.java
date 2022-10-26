aa44af29a813ab4da20424281dbc00e02f1c3198
junho lee <junho1273@gmail.com>
2022-10-26 23:54:05 +0900

    Problem1 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        //예외처리
+        //왼쪽 페이지가 홀수가 아니면 리턴
+        if(pobi.get(0) % 2 == 0 || crong.get(0) % 2 == 0){
+            return  -1;
+        //오른쪽 페이지가 이상하면 리턴
+        } else if (pobi.get(1) != pobi.get(0) + 1 || crong.get(1) != crong.get(0) + 1) {
+            return  -1;
+        //시작면이나 마지막 면이 나오면 리턴
+        } else if(pobi.get(0) == 1 || pobi.get(1) == 400 || crong.get(0) == 1 || crong.get(1) == 400){
+            return  -1;
+        }
         return answer;
     }
 
