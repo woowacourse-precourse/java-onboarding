1757f2ff3a6a238662c0c9ca68a7ad4803833e1c
hyungkya <36350256+hyungkya@users.noreply.github.com>
2022-10-26 19:27:32 +0900

    feat(problem 3): added brute-force 369-clap counting method
    From 1 to given number, total number of claps is counted by checking every digits.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+    private final static List<Integer> clapTargets = List.of(3, 6, 9);
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int totalClaps = 0;
+        int currentNumber = 1;
+        while (currentNumber <= number) {
+            totalClaps += clapsOf(currentNumber);
+            currentNumber += 1;
+        }
+        return totalClaps;
+    }
+
+    private static int clapsOf(int number) {
+        int claps = 0;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            if (Problem3.clapTargets.contains(lastDigit)) {
+                claps += 1;
+            }
+            number /= 10;
+        }
+        return claps;
     }
 }
