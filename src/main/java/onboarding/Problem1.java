9b8602083b1fba922bd0f166646707b47e99affb
wonho-seo <sjdlrnen0507@naver.com>
2022-10-27 04:44:28 +0900

    [Problem1][wonho-seo] 제한사항을 제외하고 예외사항 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static boolean validPage(List<Integer> pages){
+        Integer leftPage = pages.get(0);
+        Integer rightPage = pages.get(1);
+        if (leftPage < 1 || rightPage > 400) // 주운 책보다 많은 페이지가 들어온 경우
+            return false;
+        if (leftPage != rightPage - 1 || leftPage == 1 || rightPage == 400) // 연속된 값이 아니거나 첫번째 페이지(1)이거나 마지막 페이지(400) 일경우
+            return false;
+        return true;
+    }
 }
