7abb768d2e8a166b1678b6c5dd6b18b3e293f5ed
phj1120 <hyeonj1998@naver.com>
2022-10-27 07:29:33 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,77 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram.toString();
+        while (isCryptogram(answer)) {
+            boolean[] repeatCheck = new boolean[answer.length()];
+            for (int i = 0; i < answer.length(); i++) {
+                if (isRepeat(answer, i)) {
+                    repeatCheck[i] = true;
+                }
+            }
+            StringBuffer sb = new StringBuffer(answer);
+            for (int i = repeatCheck.length-1; i >= 0; i--) {
+                if (repeatCheck[i]) {
+                    sb.deleteCharAt(i);
+                }
+            }
+            answer = sb.toString();
+        }
         return answer;
     }
+
+    public static boolean isCryptogram(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isRepeat(String str, int i) {
+        if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+            return true;
+        }
+        if (i > 0 && str.charAt(i) == str.charAt(i - 1)) {
+            return true;
+        }
+        return false;
+    }
 }
+
+class Problem2First {
+    public static String solution(String cryptogram) {
+        String answer = cryptogram.toString();
+        while (isCryptogram(answer)) {
+            boolean[] repeatCheck = new boolean[answer.length()];
+            for (int i = 0; i < answer.length(); i++) {
+                if (i < answer.length() - 1 && answer.charAt(i) == answer.charAt(i + 1)) {
+                    repeatCheck[i] = true;
+                }
+                if (i > 0 && answer.charAt(i) == answer.charAt(i - 1)) {
+                    repeatCheck[i] = true;
+                }
+            }
+            StringBuffer sb = new StringBuffer(answer);
+            for (int i = repeatCheck.length-1; i >= 0; i--) {
+                if (repeatCheck[i]) {
+                    sb.deleteCharAt(i);
+                }
+            }
+            answer = sb.toString();
+        }
+        return answer;
+    }
+
+    public static boolean isCryptogram(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
