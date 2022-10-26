b530831c5456423742eb993b6104fd5c07b87811
dhkim <akdlzmf1123@gmail.com>
2022-10-27 00:00:04 +0900

    feat: Problem1 예외 처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,89 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+    public static int solution(List<Integer> pobiList, List<Integer> crongList) {
+        try {
+            List<Page> pobiPages = asPages(pobiList);
+            List<Page> crongPages = asPages(crongList);
+
+            Player pobi = new Player(pobiPages);
+            Player crong = new Player(crongPages);
+
+            return pobi.battle(crong);
+        } catch (IllegalArgumentException e) {
+            return -1;
+        }
+    }
+
+    private static List<Page> asPages(List<Integer> list) {
+        List<Page> result = new ArrayList<>();
+        for (Integer page : list) {
+            result.add(new Page(page));
+        }
+        return result;
+    }
+
+    private static class Player {
+
+        private static final String ERR_PAGES_SIZE = "페이지는 2개만 입력하시오.";
+        private static final String ERR_PAGES_ORDER = "좌우 페이지 순서대로 입력하시오.";
+        private final List<Page> pages;
+
+        public Player(List<Page> pages) {
+            this.pages = Objects.requireNonNull(pages);
+            validatePages();
+        }
+
+        private void validatePages() {
+            validatePagesSize();
+            validatePagesOrder();
+        }
+
+        private void validatePagesSize() {
+            if (pages.size() != 2) throw new IllegalStateException(ERR_PAGES_SIZE);
+        }
+
+        private void validatePagesOrder() {
+            Page firstPage = pages.get(0), secondPage = pages.get(1);
+            if (!firstPage.isOdd() && firstPage.compareTo(secondPage) != -1)
+                throw new IllegalArgumentException(ERR_PAGES_ORDER);
+        }
+
+        public int battle(Player other) {
+            return 0;
+        }
+    }
+
+    private static class Page implements Comparable<Page> {
+        private static final int LOWER_BOUND = 1;
+        private static final int UPPER_BOUND = 400;
+        private static final String ERR_PAGE_BOUND = "페이지는 1 ~ 400까지의 값입니다.";
+
+        private Integer page;
+
+        public Page(Integer page) {
+            this.page = page;
+            validatePage();
+        }
+
+        private void validatePage() {
+            if (page < LOWER_BOUND || page > UPPER_BOUND)
+                throw new IllegalArgumentException(ERR_PAGE_BOUND);
+        }
+
+        public boolean isOdd() {
+            return page % 2 == 1;
+        }
+
+        @Override
+        public int compareTo(Page other) {
+            return this.page - other.page;
+        }
     }
 }
