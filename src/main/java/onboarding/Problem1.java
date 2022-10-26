6308d41d16bc636177ec11c136ecb4852ee597cc
aossuper7 <aossuper7@naver.com>
2022-10-26 19:29:54 +0900

    feat: 문제1 왼쪽, 오른쪽 페이지 차 구하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,18 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (checkPage(pobi) || checkPage(crong)) {
+            return -1;
+        }
+
+    }
+
+    public static boolean checkPage(List<Integer> page) {
+        if (page.get(0) - page.get(1) == -1) {
+            return false;
+        }
+        return true;
     }
 }
