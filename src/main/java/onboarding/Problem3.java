5416c4f0353d7b1f918bfe110e891b21811f70d4
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-10-26 19:27:08 +0900

    Problem3 구현 기능 목록
    369 게임 규칙 이해

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        //369 게임 구현
+        // 1부터 number까지의 숫자중 3,6,9의 갯수를 answer에 담아서 return하기
+        
+
         return answer;
     }
 }
