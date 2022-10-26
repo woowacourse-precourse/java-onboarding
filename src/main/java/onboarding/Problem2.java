66b23a7e27bfa602ff3dc94730e56d24310dbb26
ddubidubap <park62000@gmail.com>
2022-10-27 02:21:18 +0900

    PROBLEM2 기능 요구 사항 목록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem2 {
+    /*
+        기능 요구 사항 목록
+        1. 반복되는 문자의 인덱스를 찾는 함수
+        2. 반복되는 문자를 제거하는 함수
+        3. solution (반복문으로 문자 제거)
+    */
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
