11b73bf063ef71401e87533fea44ae51f5d237af
hyeongcheolkim <tori1753@naver.com>
2022-10-26 20:08:56 +0900

    problem3 first commit

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; ++i)
+            answer += String.valueOf(i).chars().filter(e -> e == '3' || e == '6' || e == '9').count();
         return answer;
     }
 }
