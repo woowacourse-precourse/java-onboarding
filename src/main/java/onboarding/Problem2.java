57ef62056056a06749bf2b008681d3109e4e9ea0
sangil <gsl0505@naver.com>
2022-10-26 17:35:25 +0900

    feat: 중복이 있을경우 지우는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	private static final int CHECK_INIT = 0;
+
+	public static String solution(String cryptogram) {
+		String answer = cryptogram;
+		return answer;
+	}
+
+	public static String removeString(String answer) {
+		char checkChar = answer.charAt(CHECK_INIT);
+		StringBuilder removeString = new StringBuilder();
+		for (int i = 1; i < answer.length(); i++) {
+			if (answer.charAt(i) != checkChar) {
+				removeString.append(answer.charAt(i));
+			}
+		}
+        return removeString.toString();
+	}
+
 }
