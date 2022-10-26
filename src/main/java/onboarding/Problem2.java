b6c4c44faae5be39a5a6c8055b82c9b3803f5bc1
kevstevie <jianna442@gmail.com>
2022-10-26 17:36:25 +0900

    feat: add deque to String

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,6 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "";
         char[] chars = cryptogram.toCharArray();
         Deque<Character> chDeque = new LinkedList<>();
 
@@ -17,12 +16,20 @@ public class Problem2 {
             }
             chDeque.addLast(ch);
         }
-        return answer;
+        return charToString(chDeque);
     }
 
     static void checkContinuous(Deque<Character> chDeque, char ch){
         if(chDeque.getLast() == ch){
             chDeque.removeLast();
         }
+    }
+
+    static String charToString(Deque<Character> chDeque){
+        String result = "";
+        while (!chDeque.isEmpty()) {
+            result += Character.toString(chDeque.removeFirst());
+        }
+        return result;
     }
 }
