d3d334334d39d66efe750cdae5de3e2b4f8021f6
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 20:18:27 +0900

    feat: convert를 사용해 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,9 @@
 package onboarding;
 
+import onboarding.problem4.ReverseConverter;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		return ReverseConverter.convert(word);
+	}
 }
