09f506bb8ee6bd64b2cafba8185b9d75de489a55
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-27 15:22:18 +0900

    feat(problem2): 반복되는 문자의 시작index 확인

--- src/main/java/onboarding/problem2/Decrypter.java
+++ src/main/java/onboarding/problem2/Decrypter.java
@@ -6,4 +6,16 @@ public class Decrypter {
         String result = "";
         return result;
     }
+
+    private static int findIndexOfRepeatedChar(String cryptogram){
+        int startingIndex = -1;
+        for(int i=0; i<cryptogram.length(); i++ ){
+            char temp = cryptogram.charAt(i);
+            if(temp == cryptogram.charAt(i+1)){
+                startingIndex = i;
+                return startingIndex;
+            }
+        }
+        return startingIndex;
+    }
 }
