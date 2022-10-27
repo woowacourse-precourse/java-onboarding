c165e65dc0926488c1e198923b50e1363a230f75
JuHyeokParkk <hyeok5596@naver.com>
2022-10-27 22:17:47 +0900

    feat(Problem1): 입력 값에 대한 유효성 체크 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,30 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    static void validateInput(List<Integer> bookPage) throws Exception {
+
+        if(bookPage == null || bookPage.size() != 2)
+            throw new Exception();
+
+        int leftPageNumber = bookPage.get(0);
+        int rightPageNumber = bookPage.get(1);
+
+        if(outOfPage(leftPageNumber) || outOfPage(rightPageNumber))
+            throw new Exception();
+
+        if(leftPageNumber % 2 != 1 || rightPageNumber % 2 != 0)
+            throw new Exception();
+
+        if(leftPageNumber > rightPageNumber)
+            throw new Exception();
+
+        if(rightPageNumber - 1 != leftPageNumber)
+            throw new Exception();
+
+    }
+
+    static boolean outOfPage(int page) {
+        return !(1 <= page && page <= 400);
+    }
 }
