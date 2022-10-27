b0213fd3c56c0b8aa6311c41a17c470414879da5
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-27 21:23:04 +0900

    feat : 10 미만의 숫자가 3, 6, 9 를 포함하는지 여부 확인 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int i = 0;
+        for (i = 0; i <= number; i++) {
+            String numberStr = String.valueOf(i);
+            if (numberStr.contains("3") || numberStr.contains("6") || numberStr.contains("9")) {
+                System.out.println(numberStr);
+            }
+        }
+
+        int answer = 3;
         return answer;
+
     }
 }
