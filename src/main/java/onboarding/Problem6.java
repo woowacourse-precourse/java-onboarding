97452f880a212a39988bd966a92d70ff257d285a
Hayoon <hy97@sju.ac.kr>
2022-10-27 16:18:27 +0900

    feat: 비교할 문자와 비교기준 문자 간 문자열 비교 isDupChar 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,35 +5,60 @@ import java.util.List;
 
 public class Problem6 {
     static class CheckList {
-        int [] checkName;
+        int[] checkName;
 
         public CheckList(List<List<String>> forms) {
             checkName = new int[forms.size()];
-            for(int i = 0; i < forms.size(); i++)
+            for (int i = 0; i < forms.size(); i++)
                 checkName[i] = 0;
         }
 
         int[] checkingList(List<List<String>> forms) {
             String compName = "";
+            int flag = 0;
             for (int i = 0; i < forms.size(); i++) {
-                if(checkName[i] == 0) {
+                if (checkName[i] == 0) {
                     compName = forms.get(i).get(1);
                 } else continue;
                 for (int j = 1; j < forms.size(); j++) {
-                    if (compName.equals(forms.get(j).get(1))
+                    if (isDupChar(forms, compName, j)
                             && checkName[j] == 0 && i != j) {
                         checkName[j] = 1;
+                        flag = 1;
                     }
                 }
+                if (flag == 1) checkName[i] = 1;
+                flag = 0;
             }
-            System.out.println(Arrays.toString(checkName));
+
             return checkName;
         }
+
+        private boolean isDupChar(List<List<String>> forms, String compName, int j) {
+            /***
+             * [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"],
+             * ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]
+             */
+            int firstIdx = 0;
+            int secondIdx = 0;
+            int sameCount = 0;
+            for (int m = 0; m < forms.get(j).get(1).length() - 1; m++) {
+                for (int n = 0; n < compName.length() - 1; n++) {
+                    String compNameSlicing = compName.substring(n, n + 2);
+                    String formsNameSlicing = forms.get(j).get(1).substring(m, m + 2);
+                    if (compNameSlicing.equals(formsNameSlicing))
+                        return true;
+                }
+            }
+            return false;
+        }
     }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         CheckList checkList = new CheckList(forms);
         int[] ints = checkList.checkingList(forms);
+        System.out.println(Arrays.toString(ints));
         return answer;
     }
 
@@ -49,3 +74,5 @@ public class Problem6 {
         solution(forms);
     }
 }
+
+
