8bc7283f626324fd4fb4d98bc6fc5039e6269c91
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-27 17:05:37 +0900

    refactor(problem2): Decrypter내부 문자열 처리를 StringBuilder로 변경

--- src/main/java/onboarding/problem2/Decrypter.java
+++ src/main/java/onboarding/problem2/Decrypter.java
@@ -3,27 +3,27 @@ package onboarding.problem2;
 public class Decrypter {
 
     public static String decrypt(String cryptogram){
-        String result = cryptogram;
-        while (isDecryptable(result)){
-            result = removeRepeatedChar(result);
+        StringBuilder resultBuilder = new StringBuilder(cryptogram);
+        while (isDecryptable(resultBuilder)){
+            resultBuilder = removeRepeatedChar(resultBuilder);
         }
-        return result;
+        return resultBuilder.toString();
     }
 
-    private static boolean isDecryptable(String cryptogram){
+    private static boolean isDecryptable(StringBuilder cryptogram){
         if(findIndexOfRepeatedChar(cryptogram) == -1){
             return false;
         }
         return true;
     }
 
-    private static String removeRepeatedChar(String cryptogram){
+    private static StringBuilder removeRepeatedChar(StringBuilder cryptogram){
         int startingIndex = findIndexOfRepeatedChar(cryptogram);
         int repeatedNum = findNumberOfRepeatedChar(cryptogram, startingIndex);
         return removeCharacters(cryptogram, startingIndex, repeatedNum);
     }
 
-    private static int findIndexOfRepeatedChar(String cryptogram){
+    private static int findIndexOfRepeatedChar(StringBuilder cryptogram){
         int startingIndex = -1;
         int index = 0;
         for(int i=0; i<cryptogram.length()-1; i++ ){
@@ -36,7 +36,7 @@ public class Decrypter {
         return startingIndex;
     }
 
-    private static int findNumberOfRepeatedChar(String cryptogram, int startingIndex){
+    private static int findNumberOfRepeatedChar(StringBuilder cryptogram, int startingIndex){
         int num = 1;
         final char repeatedChar = cryptogram.charAt(startingIndex);
         int index = startingIndex + 1;
@@ -50,14 +50,8 @@ public class Decrypter {
         return num;
     }
 
-    private static String removeCharacters(String cryptogram, int startingIndex, int charNum){
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(
-                cryptogram.substring(0, startingIndex)
-        );
-        stringBuilder.append(
-                cryptogram.substring(startingIndex+charNum)
-        );
-        return stringBuilder.toString();
+    private static StringBuilder removeCharacters(StringBuilder cryptogram, int startingIndex, int charNum){
+        cryptogram.delete(startingIndex, startingIndex+charNum);
+        return cryptogram;
     }
 }
