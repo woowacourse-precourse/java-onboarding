6406111c8c21e9319c578deae25de3fef6e3ac0d
HubCreator <chocomilk8604@gmail.com>
2022-10-27 13:14:22 +0900

    refactor(Problem4) : Util 클래스 삭제. 애플리케이션으로 메서드를 이동

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,14 +1,22 @@
 package onboarding;
 
-import utils.Problem4Util;
-
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            answer.append(Problem4Util.transform(ch));
+            answer.append(transform(ch));
         }
         return answer.toString();
+    }
+
+    public static char transform(char ch) {
+        if (Character.isLowerCase(ch)) {
+            return (char) ('z' - (ch - 'a'));
+
+        } else if (Character.isUpperCase(ch)) {
+            return (char) ('Z' - (ch - 'A'));
+        }
+        return ch;
     }
 }
--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -1,23 +1,10 @@
 package onboarding;
 
 import org.junit.jupiter.api.Test;
-import utils.Problem4Util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem4Test {
-    @Test
-    void charChageTest() {
-        assertThat(Problem4Util.transform('A')).isEqualTo('Z');
-        assertThat(Problem4Util.transform('a')).isEqualTo('z');
-        assertThat(Problem4Util.transform('B')).isEqualTo('Y');
-        assertThat(Problem4Util.transform('C')).isEqualTo('X');
-        assertThat(Problem4Util.transform('Y')).isEqualTo('B');
-        assertThat(Problem4Util.transform('Z')).isEqualTo('A');
-        assertThat(Problem4Util.transform('O')).isEqualTo('L');
-        assertThat(Problem4Util.transform('o')).isEqualTo('l');
-    }
-
     @Test
     void test() {
         assertThat((char)('Z' - ('A' - 'A'))).isEqualTo('Z');
