5688c14512389200086be4f8f0f14df467330c40
metorg <metrog950@gmail.com>
2022-10-27 14:29:31 +0900

    feat: 예외처리 메서드 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,11 +5,29 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobi_MaxVal=0;
+        int crong_MaxVal=0;
 
-        int pobi_MaxVal =Math.max(PageCheck(pobi.get(0)),PageCheck(pobi.get(1)));
-        int crong_MaxVal =Math.max(PageCheck(crong.get(0)),PageCheck(crong.get(1)));
+        if(isException(pobi.get(0),pobi.get(1))||isException(crong.get(0),crong.get(1))){
+            answer=-1;
+        }else {
+
+        }
 
         return answer;
+    }
+
+    static boolean isException(int leftPage ,int rightPage){
+        if(leftPage>rightPage){
+            return true;
+        }
+        else if(rightPage-leftPage!=1){
+            return true;
+        }
+        else if(leftPage<1||rightPage>400){
+            return true;
+        }
+        else return false;
     }
 
     static int PageCheck(int page){
