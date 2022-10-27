75425c510d73470e4a79febdad8d348fdeb86c57
LEE-sh1673 <lsh901673@gmail.com>
2022-10-27 21:52:38 +0900

    refactor(CryptogramDecoder): 메서드 구현 기능 추가
    - 기존 문자열 리터럴의 regex 표현을 상수 필드로 변경
    - CryptogramDecoder 의 isValidFormat 암호문 길이 검증 기능 추가.
    
    암호문이 올바른 포맷인지 검사하는 메서드이기 때문에 길이를 검사하는 로직이 포함된다고 판단하여 구현 부분을 수정함.

--- src/main/java/onboarding/cryptogram/CryptogramDecoder.java
+++ src/main/java/onboarding/cryptogram/CryptogramDecoder.java
@@ -6,12 +6,16 @@ public class CryptogramDecoder {
 
 	private static final int MIN_LENGTH = 1;
 
+	private static final String FORMAT_REGEX = "^[a-z]+$";
+
 	public static boolean validateLength(final String cryptogram) {
 		int length = cryptogram.length();
 		return MIN_LENGTH <= length && length <= MAX_LENGTH;
 	}
 
 	public static boolean isValidFormat(final String cryptogram) {
-		return cryptogram != null && cryptogram.matches("^[a-z]+$");
+		return cryptogram != null
+			&& validateLength(cryptogram)
+			&& cryptogram.matches(FORMAT_REGEX);
 	}
 }
