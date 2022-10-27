b6d9e9362a7420c171e82ef3aefb92ef1211f444
qpdh <74190329+qpdh@users.noreply.github.com>
2022-10-27 10:22:27 +0900

    Update Problem5.java
    Problem5 해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,48 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        // 50_000
+        answer.add(money / 50_000);
+        money %= 50_000;
+
+        // 10_000
+        answer.add(money / 10_000);
+        money %= 10_000;
+
+        // 5_000
+        answer.add(money / 5_000);
+        money %= 5_000;
+
+        // 1_000
+        answer.add(money / 1_000);
+        money %= 1_000;
+
+        // 500
+        answer.add(money / 500);
+        money %= 500;
+
+        // 100
+        answer.add(money / 100);
+        money %= 100;
+
+        // 50
+        answer.add(money / 50);
+        money %= 50;
+
+        // 10
+        answer.add(money / 10);
+        money %= 10;
+
+        // 1
+        answer.add(money);
+
         return answer;
     }
+
 }
