d31c0aa9f6078ee8f412c8ad277e47fdcd98011a
hyun6ik <fkanwm@naver.com>
2022-10-27 20:55:54 +0900

    feat(67) : [기능 7] : Friends 일급 컬레션 생성 및 메소드 구현

---
+++ src/main/java/onboarding/problem7/Friends.java
@@ -0,0 +1,34 @@
+package onboarding.problem7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Friends {
+
+    private List<Friend> friends = new ArrayList<>();
+
+    public Friends(List<Friend> friends) {
+        this.friends = friends;
+    }
+
+    public void add(Friend friend) {
+        validateFriendsSize();
+        if (friends.contains(friend)) {
+            throw new IllegalArgumentException("이미 동일한 친구 관계가 존재합니다.");
+        }
+        friends.add(friend);
+    }
+
+    public List<String> getFriendList() {
+        return friends.stream()
+                .map(Friend::getName)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validateFriendsSize() {
+        if (friends.size() > 100000) {
+            throw new IllegalStateException("Friends의 크기는 10000이하어야 합니다.");
+        }
+    }
+}
