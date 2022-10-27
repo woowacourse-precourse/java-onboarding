4b126286fbb79b6a3350adc5eaf3e5e6f807a31b
Kimhyeyun <cqqwer@gmail.com>
2022-10-27 17:08:02 +0900

    [Problem 1] MAKE CALCULATE MAX FUNCTION

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,42 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if(!check(pobi) || !check(crong)) return -1;
+
+        int pobiMax = calculateMax(pobi);
+        int crongMax = calculateMax(crong);
+
+        if(pobiMax > crongMax) return 1;
+        else if(pobiMax < crongMax) return 2;
+        else return 0;
+    }
+
+    private static int calculateMax(List<Integer> name) {
+        int left = name.get(0);
+        int right = name.get(1);
+
+        int sumMax = Math.max(calculateSum(left),calculateSum(right));
+        int multMax = Math.max(calculateMult(left),calculateMult(right));
+
+        return Math.max(sumMax, multMax);
+
+    }
+
+    private static int calculateMult(int num) {
+        int mult = 1;
+        while (num != 0) {
+            mult *= (num % 10);
+            num /= 10;
+        }
+        return mult;
+    }
+
+    private static int calculateSum(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum += (num % 10);
+            num /= 10;
+        }
+        return sum;
     }
 
     private static boolean check(List<Integer> name) {
