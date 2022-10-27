ef7aa1100033a867474bb88b24c62116589c4c2e
aksmf1442 <aksmf1442@naver.com>
2022-10-27 14:38:33 +0900

    feat: Problem6(완료) 같은 글자가 연속적으로 포함되는 닉네임이 있는지 찾는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,21 +1,55 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+    static Map<String, Integer> nicknameMap;
+    static Set<Integer> duplicateNicknameIdxSet;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        findDuplicateNickname(forms);
-        return answer;
+        nicknameMap = new HashMap<>();
+        duplicateNicknameIdxSet = new HashSet<>();
+        findDuplicateNicknames(forms);
+        return parseEmails(forms);
     }
 
-    private static void findDuplicateNickname(List<List<String>> forms) {
-        for (List<String> crewForm : forms) {
+    private static List<String> parseEmails(List<List<String>> forms) {
+        List<String> emails = new ArrayList<>();
+        for (Integer idx : duplicateNicknameIdxSet) {
+            String email = forms.get(idx).get(0);
+            emails.add(email);
+        }
+        return emails.stream().sorted().collect(Collectors.toList());
+    }
+
+    private static void findDuplicateNicknames(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> crewForm = forms.get(i);
             String email = crewForm.get(0);
             String nickname = crewForm.get(1);
             if (!validateEmail(email) || !validateNickname(nickname)) {
                 continue;
             }
+            findDuplicateNickname(nickname, i);
+        }
+    }
+
+    private static void findDuplicateNickname(String nickname, int idx) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String twoLetterNickname = nickname.substring(i, i + 2);
+            Integer nicknameIdx = nicknameMap.get(twoLetterNickname);
+            if (nicknameIdx == null) {
+                nicknameMap.put(twoLetterNickname, idx);
+                continue;
+            }
+            duplicateNicknameIdxSet.add(idx);
+            duplicateNicknameIdxSet.add(nicknameIdx);
         }
     }
 
@@ -32,7 +66,8 @@ public class Problem6 {
     }
 
     private static boolean validateNickname(String nickname) {
-        if (!nickname.matches("^[ㄱ-ㅎ가-힣]*$")) {
+        String expression = "^[ㄱ-ㅎ가-힣]*$";
+        if (!nickname.matches(expression)) {
             return false;
         }
 
