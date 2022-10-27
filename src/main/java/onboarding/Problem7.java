88561beba47274238fd214f0cbdd31bd1b75a24d
apptie <appti@naver.com>
2022-10-28 01:02:55 +0900

    feat: 문제 7 친구 관계 정보 friends에서 모든 회원의 친구 관계를 구하는 initFriendsRelation 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,6 +12,22 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static class FriendsRecommender {
+        private static Map<String, Account> accountInfoMap = new HashMap<>();
+
+        private static void initFriendsRelation(List<List<String>> friends) {
+            for (List<String> friend : friends) {
+                Account accountA = new Account(friend.get(0));
+                Account accountB = new Account(friend.get(1));
+
+                accountA.addFriend(accountB);
+                accountB.addFriend(accountA);
+                accountInfoMap.put(friend.get(0), accountA);
+                accountInfoMap.put(friend.get(1), accountB);
+            }
+        }
+    }
 }
 
 class Account {
