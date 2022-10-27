741224e342557526e913b847459117c0086b5b2a
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-27 23:15:25 +0900

    check pages are valid or not (right - left == 1)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        System.out.println(pobi.get(0).getClass().getName());
+        if (notInOnePage(pobi) || notInOnePage(crong)) return -1;
         int pobiMax = getMaxPage(pobi.get(0)) > getMaxPage(pobi.get(0)) ? getMaxPage(pobi.get(0)) : getMaxPage(pobi.get(0));
         int crongMax = getMaxPage(crong.get(0)) > getMaxPage(crong.get(0)) ? getMaxPage(crong.get(0)) : getMaxPage(crong.get(0));
+        if (pobiMax == crongMax) return 0;
+        else if (pobiMax > crongMax) return 1;
+        else return 2;
+    }
 
-        return answer;
+    public static boolean notInOnePage(List<Integer> intList) {
+        return (intList.get(1) - intList.get(0) != 1) ? true : false;
     }
 
     public static int getMaxPage(int page) {
