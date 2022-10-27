b8c361b7b268adb3bae52789022be7497a8e19ab
hong-sile <gurwns9325@naver.com>
2022-10-27 15:47:09 +0900

    feat : 청개구리 사전을 초기화해주는 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	private static HashMap<Character,Character> frogDic = new HashMap<>();
+	private static final char LOWER_FIRST_CHAR='a';
+	private static final char LOWER_MIDDLE_CHAR='m';
+	private static final char LOWER_END_CHAR='z';
+	private static final char UPPER_FIRST_CHAR='A';
+	private static final char UPPER_MIDDLE_CHAR='M';
+	private static final char UPPER_END_CHAR='Z';
+
+	public static String solution(String word) {
+		initFrogDic();
+		String answer = "";
+		return answer;
+	}
+
+	private static void initFrogDic(){
+		for(char ch = UPPER_FIRST_CHAR; ch<=UPPER_END_CHAR; ch++)
+			frogDic.put(ch,(char)(2*UPPER_MIDDLE_CHAR-ch+1));
+		for(char ch = LOWER_FIRST_CHAR; ch<=LOWER_END_CHAR; ch++)
+			frogDic.put(ch,(char)(2*LOWER_MIDDLE_CHAR-ch+1));
+	}
 }
