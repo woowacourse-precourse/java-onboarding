e5edbb6cb0f895291fc487d5b92c3f23100abb9e
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 17:27:10 +0900

    feat(P2): char arry를String으로 변환해주는 로직 구현

--- src/main/java/onboarding/problem2/StringConverter.java
+++ src/main/java/onboarding/problem2/StringConverter.java
@@ -1,4 +1,14 @@
 package onboarding.problem2;
 
 public class StringConverter {
+
+    public static String charArrToString(char[] chars) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char aChar : chars) {
+            stringBuilder.append(aChar);
+        }
+
+        return stringBuilder.toString();
+    }
 }
