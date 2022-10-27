0781e37e7e590d4ed659bb66f178013d966814cb
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-27 19:17:15 +0900

    feat: problem7 - user와 친구관계가 동일하면 10점 추가 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -14,6 +14,16 @@ public class Problem7 {
             memberList.put(friends.get(i).get(0), 0);
             memberList.put(friends.get(i).get(1), 0);
         }
+
+        for (int i = 0; i < friends.size(); i++) {
+            String friend1 = friends.get(i).get(0);
+            String friend2 = friends.get(i).get(1);
+
+            if(friend1.equals(user) || friend2.equals(user)){
+                continue;
+            }
+            memberList = checkUserFriendFriend(friend1, friend2, friendList, memberList);
+        }
         return answer;
     }
     //user의 친구목록을 작성하는 기능
@@ -25,5 +35,19 @@ public class Problem7 {
             friendList.add(friend1);
         }
         return friendList;
+    }
+
+    //user의 친구의 친구인지 확인, 맞으면 10점 추가하는 기능
+    public static HashMap<String, Integer> checkUserFriendFriend(String friend1, String friend2,
+                                                                 List<String> friendList, HashMap<String, Integer> memberList){
+        if(friendList.contains(friend1)){
+            Integer score = memberList.get(friend2);
+            memberList.replace(friend2, score + 10);
+        }
+        if(friendList.contains(friend2)){
+            Integer score = memberList.get(friend1);
+            memberList.replace(friend1, score + 10);
+        }
+        return memberList;
     }
 }
