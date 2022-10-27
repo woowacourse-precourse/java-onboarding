8e6a0c08023782fc92fb8516e53431e0aef216da
kariskan <zkfzpf56@naver.com>
2022-10-27 16:09:14 +0900

    docs: 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static final int LAST_CHARACTER_ASCII = 25;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            answer.append(getDictionaryCharacter(word.charAt(i)));
+        }
+
+        return answer.toString();
     }
+
+    public static char getDictionaryCharacter(char original) {
+        if (original == ' ') return original;
+        int base = getBaseCharacterASCII(original);
+        return (char) (LAST_CHARACTER_ASCII - (original - base) + base);
+    }
+
+    public static int getBaseCharacterASCII(char c) {
+        int base = 'a';
+        if (isUpper(c)) base = 'A';
+        return base;
+    }
+
+    public static boolean isUpper(char a) {
+        return a >= 'A' && a <= 'Z';
+    }
+
 }
