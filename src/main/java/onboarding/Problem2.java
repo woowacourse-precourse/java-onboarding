85ad7a65fe26d0c5b69fcff94a96ad1685ebf51e
yujeongkwon <yoojeongkwon@naver.com>
2022-10-28 02:06:43 +0900

    문제2 테스트 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String cryptogramOrigin;
+        int idx;
+        while(true) {
+            cryptogramOrigin = cryptogram;
+            for (int i = 0; i < cryptogram.length(); i++) {
+                idx = i + 1;
+                while (idx < cryptogram.length() && cryptogram.charAt(idx) == cryptogram.charAt(i)) {
+                    idx++;
+                }
+                if (idx - i > 1)
+                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(idx);
+            }
+
+            if (cryptogramOrigin.equals(cryptogram))
+                return cryptogram;
+        }
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int n, answer = 0;
+
+        for(int num= 1; num<=number; num++){
+            n = num;
+            while(n!=0){
+                if(n%10==3|| n%10==6|| n%10==9)
+                    answer++;
+                n /= 10;
+            }
+        }
         return answer;
     }
 }
