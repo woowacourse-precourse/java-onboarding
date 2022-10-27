c09113dfd388e77ef8ad63255486cd64b467fc35
JJongmen <pjy90123@gmail.com>
2022-10-28 00:23:51 +0900

    feat(Problem1): 각 자리 숫자를 모두 더하거나 모두 곱하고 더 큰 수를 반환하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,6 +11,28 @@ class Problem1 {
         return answer;
     }
 
+    private static int getPageScore(int page) {
+        return Math.max(sumDigits(page), productDigits(page));
+    }
+
+    private static int sumDigits(int page) {
+        int sum = 0;
+        String pageStr = Integer.toString(page);
+        for (int i = 0; i < pageStr.length(); i++) {
+            sum += pageStr.charAt(i) - '0';
+        }
+        return sum;
+    }
+
+    private static int productDigits(int page) {
+        int result = 1;
+        String pageStr = Integer.toString(page);
+        for (int i = 0; i < pageStr.length(); i++) {
+            result *= pageStr.charAt(i) - '0';
+        }
+        return result;
+    }
+
     private static boolean isBoundary(List<Integer> pages) {
         return 3 <= pages.get(0) && pages.get(1) <= 398;
     }
