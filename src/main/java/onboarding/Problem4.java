04fa66a30fc8c237029b9109a1cf4b9dde227936
rvlwldev <rvlwlasd77@gmail.com>
2022-10-27 18:12:43 +0900

    problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        ArrayList<String> upperList = new ArrayList<>();
+        ArrayList<String> lowerList = new ArrayList<>();
+
+        int upperLastChar = 90;
+        int lowerLastChar = 122;
+
+        for (int i = 0; i < 26; i++) {
+            upperList.add(String.valueOf((char) (upperLastChar - i)));
+            lowerList.add(String.valueOf((char) (lowerLastChar - i)));
+        }
+
+        for (char c : word.toCharArray()) {
+            int num = (int) c;
+
+            // 공백
+            if (num == 32) {
+                answer += " ";
+            }
+            // 대문자
+            else if (num < 91) {
+                answer += upperList.get(num - 65);
+            }
+            // 소문자
+            else if (num > 96) {
+                answer += lowerList.get(num - 97);
+            }
+
+        }
+
         return answer;
     }
 }
