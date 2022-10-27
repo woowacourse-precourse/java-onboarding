c222d5829f68025572a0f4bc4425211d515c865c
devmizz <garamminchan@gmail.com>
2022-10-27 21:53:48 +0900

    problem1: get point

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,11 +4,22 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int pobiPoint = getPoint(pobi);
+        int crongPoint = getPoint(crong);
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
 
-    private int addDigits(int num) {
+    private static int getPoint(List<Integer> person) {
+        return Math.max(getPagePoint(person.get(0)), getPagePoint(person.get(1)));
+    }
+
+    private static int getPagePoint(int page) {
+        return Math.max(addDigits(page), multipleDigits(page));
+    }
+
+    private static int addDigits(int num) {
         int sum = 0;
         while(num == 0) {
             sum += num % 10;
@@ -17,7 +28,7 @@ class Problem1 {
         return sum;
     }
 
-    private int multipleDigits(int num) {
+    private static int multipleDigits(int num) {
         int multiple = 1;
         while(num == 0) {
             multiple *= num % 10;
