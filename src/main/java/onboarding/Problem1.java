61727073755f1e7536d70526a0f39975db120446
bando99 <bando991225@gmail.com>
2022-10-27 10:49:04 +0900

    problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,103 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    public static int get_number(){
+        double random_num = Math.random(); //0~1.0까지의 수
+
+        int random_page = (int)(random_num * 10000 % 401); // 0~ 400
+
+        return random_page;
+
+    }
+    //public static int is_max = 0;
+    //public static int is_max_multi = 1;
+
+    //197 -> 19 -> 1
+
+    public static int max_sum(Integer page,Integer is_max){
+        if(page/10 >=1){
+            is_max += page%10;
+            return max_sum(page/10,is_max);
+        }
+        is_max += page % 10;
+        System.out.println("sum(197)"+is_max);
+        return is_max;
+    }
+
+    public static int max_multi(Integer page,Integer is_max){
+        if(page/10 >=1){
+            //System.out.println(page%10);
+            is_max *= page%10;
+            return max_multi(page/10,is_max);
+        }
+        is_max *= page % 10;
+        System.out.println("multi(197)"+is_max);
+        return is_max;
+    }
+
+    public static int max_page(Integer page){
+        int max_sum = max_sum(page,0);
+        int max_multi = max_multi(page,1);
+        System.out.println("max_page : "+Integer.max(max_sum,max_multi));
+        return Integer.max(max_sum,max_multi);
+    }
+    public static int winner(Integer a , Integer b){
+        if(a>b){
+            return 1;
+        }
+        if(a == b ){
+            return 0;
+        }
+        return 2;
+    }
+    public static boolean is_exception(List<Integer> page){
+        //이상한 데이터가 들어왔다면 false 를 반환(예외처리 -1)
+        if(page.get(0)%2 == 0 && page.get(0)+1 != page.get(1)){
+            return false;//올바른 data 들어왔을 때 1을 반환
+        }
+        //이상한 데이터가 들어왔다면 false 를 반환 (=6번 조건을 충족하지 않는다면.)
+        if(page.get(0) == 1 || page.get(1) == 400){
+            return false;
+        }
+
+        return true ; //이상한 data 들어왔을 떄 0을 반환
+    }
+    public static int solution(List<Integer> pobi, List<Integer> crong) { //play game
         int answer = Integer.MAX_VALUE;
+        int pobi_left_page = pobi.get(0);
+        int pobi_right_page = pobi.get(1);
+
+        int crong_left_page = crong.get(0);
+        int crong_right_page = crong.get(1);
+
+        if(is_exception(pobi) && is_exception(crong)){
+            //1. 책을 임의로 펼친다. 이미 solution의 argument로 들어왔음
+
+            //2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나 , 모두 곱해 가장 큰 수를 구한다.
+            int pobi_left = max_page(pobi_left_page);
+            int crong_left = max_page(crong_left_page);
+
+            //3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나 , 모두 곱해 가장 큰 수를 구한다.
+            int pobi_right = max_page(pobi_right_page);
+            int crong_right = max_page(crong_right_page);
+
+            //4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+            int pobi_result = Integer.max(pobi_left,pobi_right);
+            int crong_result = Integer.max(crong_left,crong_right);
+
+
+            System.out.println("포비 : " + pobi_result);
+            System.out.println("크롱 : " + crong_result);
+            //5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+            //포비 1 크롱 2 무승부 0 예외사항 -1(이미 처리)
+            answer = winner(pobi_result,crong_result);
+            //6. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+
+            System.out.println(answer);
+            return answer;
+        }
+        answer = -1;
+        System.out.println(answer);
         return answer;
     }
 }
