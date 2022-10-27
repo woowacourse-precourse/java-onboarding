14204f867e85c35010dcd5705358a5dba0a3751a
koo <rnqudgur@naver.com>
2022-10-27 22:48:37 +0900

    기능요구사항 2번 문제
    기능 분리
    1. 작게 순회하며 지우는 기능 분리(RepectDelete)
    2. 분리한 기능을 반복해서 사용하는 기능 지운 횟수로 통제(solution)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        Problem2 problem2 = new Problem2();
+        
+        if(cryptogram.length()==1) return cryptogram;
+        StringBuilder sbCryptogram = new StringBuilder(cryptogram);
+
+        while(problem2.RepectDelete(sbCryptogram)!=0) {
+        problem2.RepectDelete(sbCryptogram); 
+        }//while
+        
+        String answer = sbCryptogram.toString();
         return answer;
-    }
-}
+    }// solution
+    
+    private int RepectDelete(StringBuilder sbCryptogram) {
+    	int numberOfDelete = 0;
+    	for(int i=1; i<sbCryptogram.length();i++) {
+    		if(sbCryptogram.charAt(i-1)==sbCryptogram.charAt(i)) {
+    			sbCryptogram.delete(i-1, i+1);
+    			i=i-1;
+    			numberOfDelete++;
+    		}//if
+    	}//for
+    	return numberOfDelete;
+    }// RepectDelete
+}// class
