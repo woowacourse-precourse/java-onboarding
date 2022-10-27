dafc46752caf1a1e86dd80d8d45c7eaf0418d821
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-10-27 20:05:50 +0900

    Update Problem1.java
    day 1 misson one
    비교하는 코드와 페이지의 합과 곱의 최댓값을 비교하는 코드를 메소드로 만들어서 대입을 하였다. List.of 는 리스트의 변수 변환이 되지 않아서 고민을 많이 하였는데 메소드로 넣어서 만드니 코드가 깔끔해지고 효율도 좋아진 것 같다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,14 +6,9 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int pobis=0;
-        int crongs=0;
-        int[] plussum = new int[2];
-        int[] multsum = new int[2];
         int[] pobisum = new int[3];
         int[] crongsum = new int[3];
-        Arrays.fill(multsum,1);
-        if((pobi.get(0)+1)!=pobi.get(1) || (crong.get(0)+1)!=crong.get(1))
+        if((pobi.get(0)+1)!=pobi.get(1) || (crong.get(0)+1)!=crong.get(1)) //페이지가 이어지지않는 예외 상황
         {
             answer=-1;
         }
@@ -21,27 +16,14 @@ class Problem1 {
         {
             for(int i=0; i<2;i++)
             {
-                pobis =pobi.get(i);
-                while(pobis>0)
-                {
-                    plussum[i] += pobis % 10;
-                    multsum[i] *= pobis % 10;
-                    pobis /= 10;
-                }
-                pobisum[i]= Math.max(plussum[i], multsum[i]);
-                crongs=crong.get(i);
-                Arrays.fill(plussum,0);
-                Arrays.fill(multsum,1);
-                while(crongs>0)
-                {
-                    plussum[i] += crongs % 10;
-                    multsum[i] *= crongs % 10;
-                    crongs /= 10;
-                }
-                crongsum[i]= Math.max(plussum[i], multsum[i]);
+                pobisum[i]=maximum(pobi.get(i));
+
+                crongsum[i]=maximum((crong.get(i)));
             }
-            pobisum[2] = Math.max(pobisum[0], pobisum[1]);
-            crongsum[2]= Math.max(crongsum[0],crongsum[1]);
+            pobisum[2] = compare(pobisum[0], pobisum[1]);
+
+            crongsum[2]= compare(crongsum[0],crongsum[1]);
+
             if(pobisum[2]>crongsum[2])
             {
                 answer=1;
@@ -56,5 +38,28 @@ class Problem1 {
             }
         }
         return answer;
+    }
+    public static  int compare(int sum1, int sum2)//값을 비교하는 함수
+    {
+        int sum =0;
+        sum = Math.max(sum1,sum2);
+        return sum;
+    }
+
+    public static int maximum(int number) //페이지의 합 그리고 곱의 크기 대교 메소드
+    {
+        int sum = 0;
+        int multsum = 1;
+        int plussum = 0;
+
+        while(number>0)
+        {
+            plussum += number % 10;
+            multsum *= number % 10;
+            number /= 10;
+        }
+        sum= Math.max(plussum, multsum);
+
+        return sum;
     }
 }
