02beb9695ab97fac5c6cbc22d574c4501d2eb354
hyunsb <jhss0113@naver.com>
2022-10-28 00:03:56 +0900

    problem2 solve

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,10 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        while(true) {
+            String temp = "";
 
+            for (int i = 0; i < cryptogram.length(); i++) {
+                if (i == cryptogram.length() - 1) temp += cryptogram.charAt(i);
+                else {
+                    if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) i++;
+                    else temp += cryptogram.charAt(i);
+                }
+            }
 
-
-        return answer;
+            if (cryptogram.equals(temp)) return temp;
+            else cryptogram = temp;
+        }
     }
 }
