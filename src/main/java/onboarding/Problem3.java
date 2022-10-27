a3a92c84fc5b45b9eaa0d44c52208b94cb9cd005
jeein2222 <jiinbag057@gmail.com>
2022-10-27 21:39:13 +0900

    problem3 369게임 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int num=0;
+
+        for(int i=1;i<=number;i++){
+            int n=i;
+            while(n>0){
+                num=n%10;
+                if(num==3||num==6||num==9){
+                    answer++;
+                }
+                n/=10;
+            }
+        }
+
         return answer;
     }
 }
