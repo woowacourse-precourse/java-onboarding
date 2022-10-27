c12a7ece0ccefb3fbd520ebe703ee0eedf0d0674
hongminjeong <meanj@hongminjeongui-MacBookAir.local>
2022-10-27 14:27:24 +0900

    refactor: Problem1.java

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,43 +2,63 @@ package onboarding;
 
 import java.util.List;
 import java.util.TreeSet;
+import java.lang.IllegalArgumentException;
 
-class Problem1 {
-    public static int getMaxValue(List<Integer> list){
-        TreeSet<Integer> candidates = new TreeSet<>();
-        for(int i=0;i<2;i++){
-            String value = Integer.toString(list.get(i));
-            int sumTmp = 0;
-            int mulTmp = 1;
-            for(int j=0;j<value.length();j++){
-                int tmp = Integer.parseInt(value.substring(j,j+1));
-                sumTmp += tmp;
-                mulTmp *= tmp;
-            }
-            candidates.add(sumTmp);
-            candidates.add(mulTmp);
-        }
-        return candidates.last();
+class PageList{
+    private List<Integer> list;
+    public PageList(List<Integer> list){
+        validatePageRange(list);
+        validatePageDiff(list);
+        validatePageLeftRight(list);
+        this.list = list;
     }
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        // 1~400까지의 범위가 맞는가
-        // 시작면이나 마지막면이 나오도록 책을 펼치지 않는가
-        if(pobi.get(0)<=1 || pobi.get(1)>=400 || crong.get(0)<=1 || crong.get(1)>=400)
-            return -1;
-        // 양 숫자의 차이가 1인가(이때 항상 왼쪽<오른쪽)
-        if((pobi.get(1)-pobi.get(0)!=1)||(crong.get(1)-crong.get(0)!=1))
-            return -1;
-        // 왼쪽은 홀수, 오른쪽은 짝수가 맞는가
-        if(pobi.get(0)%2==0 || pobi.get(1)%2==1 || crong.get(0)%2==0 || crong.get(1)%2==1)
-            return -1;
+    public void validatePageRange(List<Integer> list){
+        if(list.get(0)<=1 || list.get(1)>=400)
+            throw new IllegalArgumentException("Invalid Page Range");
+    }
+    public void validatePageDiff(List<Integer> list){
+        if(list.get(1)-list.get(0)!=1)
+            throw new IllegalArgumentException("Invalid Page Difference");
+    }
+    public void validatePageLeftRight(List<Integer> list){
+        if(list.get(0)%2==0 || list.get(1)%2==1)
+            throw new IllegalArgumentException("Invalid Page Odd & Even");
+    }
+    public int getMaxValueOfOne(int item){
+        String value = Integer.toString(item);
+        int sumTmp = 0;
+        int mulTmp = 1;
+        for(int j=0;j<value.length();j++){
+            int tmp = Integer.parseInt(value.substring(j,j+1));
+            sumTmp += tmp;
+            mulTmp *= tmp;
+        }
+        return Math.max(sumTmp, mulTmp);
+    }
+    public int getMaxValueOfAll(){
+        return Math.max(getMaxValueOfOne(this.list.get(0)), getMaxValueOfOne(this.list.get(1)));
+    }
+}
+class Problem1 {
 
-        int pobiMax = getMaxValue(pobi);
-        int crongMax = getMaxValue(crong);
-        if(pobiMax==crongMax)
-            return 0;
-        else if (pobiMax>crongMax)
-            return 1;
-        else
-            return 2;
+    private static int answer;
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        try{
+            PageList pobiPageList = new PageList(pobi);
+            PageList crongPageList = new PageList(crong);
+
+            int pobiMax = pobiPageList.getMaxValueOfAll();
+            int crongMax = crongPageList.getMaxValueOfAll();
+
+            if(pobiMax==crongMax)
+                answer = 0;
+            else if (pobiMax>crongMax)
+                answer = 1;
+            else
+                answer = 2;
+        }catch(Exception e){
+            answer = -1;
+        }
+        return answer;
     }
 }
