f2c47c8b913981adba3b0efe15667baca0bcda18
metorg <metrog950@gmail.com>
2022-10-27 15:37:12 +0900

    feat: [Problem2] 같은 문자 제거 메서드 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,9 +15,14 @@ public class Problem2 {
     static String sameLetterCheck(){
         for(int i=1;i<sb.length();i++){
             if(sb.charAt(i-1)==sb.charAt(i)){
-
+                deleteLetter(i);
             }
         }
         return sb.toString();
+    }
+
+    static int deleteLetter(int index){
+        sb.delete(index-1,index+1);
+        return index;
     }
 }
