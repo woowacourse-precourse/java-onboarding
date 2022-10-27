52111f62ab588b52bb7b27c3c30692b8975d797e
geneaky <kaktus418@gmail.com>
2022-10-27 16:38:58 +0900

    [feat] solving problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import onboarding.problem2.Decryptor;
+import onboarding.problem2.StackCalculator;
+import onboarding.problem2.StringMainStack;
+import onboarding.problem2.StringStackCalculator;
+import onboarding.problem2.StringStackDecryptor;
+import onboarding.problem2.StringSubStack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringMainStack stringMainStack = new StringMainStack();
+        StringSubStack stringSubStack = new StringSubStack();
+        StackCalculator<StringMainStack, StringSubStack> stackCalculator = new StringStackCalculator();
+        Decryptor decryptor = new StringStackDecryptor(stringMainStack, stringSubStack, stackCalculator);
+
+        decryptor.insertCrypto(cryptogram);
+        return decryptor.decrypt();
     }
 }
