1727ee96b72c8cc8e7b973c09700252b58e2ff8b
song-wooseok <sws981202@gmail.com>
2022-10-27 15:24:59 +0900

    feat: 자주 사용하는 상수 추가

---
+++ src/main/java/onboarding/problem1/utils/Constants.java
@@ -0,0 +1,20 @@
+package onboarding.problem1.utils;
+
+public class Constants {
+    // 참가자 당 최대 베열 크기
+    public static final int MAX_ARRAY_LENGTH = 2;
+    //책의 첫 페이지
+    public static final int FRIST_BOOK_PAGE_NUMBER = 1;
+    //책의 마지막 페이지
+    public static final int LAST_BOOK_PAGE_NUMBER = 400;
+
+    // 포비의 우승 결과 값
+    public static final int POBI_WIN_RESULT = 1;
+    // 크롱의 우승 결과 값
+    public static final int CRONG_WIN_RESULT = 2;
+    // 무승부 결과 값
+    public static final int DRAW_RESULT = 0;
+    // 예외발생 시 결과 값
+    public static final int EXCEPT_RESULT = -1;
+
+}
