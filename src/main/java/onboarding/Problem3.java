412f858c322c3c0265fe30feb4a073813549c608
jeongjihun <wlgns2234@naver.com>
2022-10-27 16:52:02 +0900

    Feat: 손벽치는 횟수 반환하는 기능추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -15,8 +15,17 @@ public class Problem3 {
         return cnt;
     }
 
+    // num까지 손치는 횟수 반환
+    public static int countHitPalm(int num) {
+        int cnt = 0;
+        for (int i = 1; i < num  + 1; i++) {
+            cnt += countOneTwoThree(i);
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
+        int answer = countHitPalm(number);
         return answer;
     }
 }
