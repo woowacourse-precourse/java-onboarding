87b0cf05cece3e2482624fd7e21c2125e5d621c8
bomin1996 <bomin199628@gmail.com>
2022-10-27 17:22:40 +0900

    문제5. 5만원권 몇개 반환인지 구현 그후 반환된 금액은 money에서 차감 하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        answer.add(0, money / 50000);
+        money=money-((money/50000)*50000);//5만원
         return answer;
     }
 }
