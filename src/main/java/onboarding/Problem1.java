fd85c87b04136654fd9d9b4e4c3e909ae57b173c
Grooomit <tmd5972@gmail.com>
2022-10-28 08:39:59 +0900

    [Feat] Problem1 toList() 구현
    - toList()
    페이지 번호를 입력받아 각 자리 숫자의 값을 리스트로 반환하는 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+
+     /*
+     * 페이지 번호의 각 자리 숫자의 값을 리스트로 반환하는 함수
+     * parameter : int
+     * return : List<Integer>
+     * */
+    public static List<Integer> toList(int x) {
+        List<Integer> output = new ArrayList<>();
+        while(x != 0) {
+            output.add(x % 10);
+            x = x / 10;
+        }
+        return output;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
