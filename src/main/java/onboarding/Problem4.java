a628b46ce83752e9a02a7f0eb5e7004dd480f3d3
Junmin <junmin1623@naver.com>
2022-10-27 23:17:39 +0900

    problem 4 ver_1

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        if(word.length()<1||word.length()>1000) return "제한발생, 길이초과or미달";
+
+        //ascii 이용 A = 65 Z=90 이므로 합 155 이용
+        for(int i=0; i<word.length();i++){
+            char temp =word.charAt(i);
+            if (temp==32) answer+= " ";
+            else if (temp<91&&temp>64)answer+=(char) (155 - temp);
+            else if (temp<123&&temp>96) answer+= (char) (219 - temp);
+            else return "제한 발생, 알파벳 이외의 문자입니다";
+
+        }
         return answer;
     }
 }
