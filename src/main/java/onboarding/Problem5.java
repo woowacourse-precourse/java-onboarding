e328dd6af26a1b04c31afc74d7399e35950aad29
davejang <davejang98@naver.com>
2022-10-27 17:40:01 +0900

    feat: Problem5 implement
    온보딩 미션 문제5 기능 요구 사항 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,13 +8,22 @@ public class Problem5 {
     public static int moneyChange(int money, int unit) {
         int count = 0;
         if(money >= unit){
-            count = money / unit;
+            count = Math.floorDiv(money,unit);
         }
         return count;
     }
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        answer.add(moneyChange(money,50000));
+        int[] unitList = {50000,10000,5000,1000,500,100,50,10,1};
+        int currentMoney = money;
+        int countByUnit = 0;
+
+        for(int index = 0; index < unitList.length; index++){
+            countByUnit = moneyChange(currentMoney,unitList[index]);
+            answer.add(countByUnit);
+            currentMoney -= unitList[index] * countByUnit;
+        }
+
         return answer;
     }
 }
