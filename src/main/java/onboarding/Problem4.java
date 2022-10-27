e58ffa0fe0b87789bde853acb7ccbdc7f55b83ea
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-27 19:44:19 +0900

    feat: [Problem4] - 주어진 문장의 각 글자를 아스키코드를 통해 청개구리 사전 형식으로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,33 @@
 package onboarding;
 
+import org.mockito.stubbing.ValidableAnswer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            int ascii = word.toCharArray()[i];
+
+            if (Character.isLowerCase(ascii)) {
+                if (ascii + 25 > 122) {
+                    ascii = 122 - ((ascii + 25) % 122);
+                }
+            }
+
+            if (Character.isUpperCase(ascii)) {
+                if (ascii + 25 > 90) {
+                    ascii = 90 - ((ascii + 25) % 90);
+                }
+            }
+
+            list.add(ascii);
+        }
+
         String answer = "";
         return answer;
     }
