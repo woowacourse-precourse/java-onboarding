9ed5bfbc61f71e808d4999798f766b2c5592e078
dldbdud314 <dldbdud314@naver.com>
2022-10-27 16:52:22 +0900

    [feat]: 1.2. 점수 연산 및 승패 가리기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,12 +4,24 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (checkInput(pobi.get(0), pobi.get(1)) || checkInput(crong.get(0), crong.get(1)))
+
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
+
+        if (checkInput(pobiLeft, pobiRight) || checkInput(crongLeft, crongRight))
             return -1;
 
-        //todo: 점수 계산, 승부 발표
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiMaxScore = getMaxScore(List.of(add(pobiLeft), add(pobiRight), multiply(pobiLeft), multiply(pobiRight)));
+        int crongMaxScore = getMaxScore(List.of(add(crongLeft), add(crongRight), multiply(crongLeft), multiply(crongRight)));
+
+        if (pobiMaxScore > crongMaxScore)
+            return 1;
+        else if (pobiMaxScore < crongMaxScore)
+            return 2;
+        else
+            return 0;
     }
 
     private static boolean checkInput(int left, int right){
@@ -18,6 +30,33 @@ class Problem1 {
         if (left % 2 == 0 || right % 2 == 1)
             return true;
         return right - left != 1;
+    }
+
+    private static int add(int num){
+        int result = 0;
+        while (num > 0){
+            result += (num % 10);
+            num /= 10;
+        }
+        return result;
+    }
+
+    private static int multiply(int num){
+        int result = 1;
+        while (num > 0){
+            result *= (num % 10);
+            num /= 10;
+        }
+        return result;
+    }
+
+    private static int getMaxScore(List<Integer> scores){
+        int maxScore = Integer.MIN_VALUE;
+        for (Integer score : scores){
+            if (maxScore < score)
+                maxScore = score;
+        }
+        return maxScore;
     }
 
 }
