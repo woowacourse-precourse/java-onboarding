9d72f8dc7e646d9ac18215c960d94f4a1d006292
yxnxx <jh4rgh0@gmail.com>
2022-10-28 05:03:34 +0900

    feat: Add checking page validation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,12 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        answer = 0;
+        if(isPageInvalid(pobi) || isPageInvalid(crong)) answer = -1;
+
         return answer;
+    }
+    public static boolean isPageInvalid(List<Integer> array) {
+        return (array.get(0) < 1) || (array.get(0) > 400) || (array.get(1) < 1) || (array.get(1) > 400);
     }
 }
