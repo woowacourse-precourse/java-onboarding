48f16771f9d40fe9113e72dee43c63497f423e71
apptie <appti@naver.com>
2022-10-27 13:15:39 +0900

    refactor: 문제 5 매직 넘버를 상수로 치환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 import static onboarding.Problem5.MoneyCalculator.calculateMoneyCount;
+import static onboarding.Problem5.MoneyConst.FIFTY_THOUSAND_WON;
+import static onboarding.Problem5.MoneyConst.FIFTY_WON;
+import static onboarding.Problem5.MoneyConst.FIVE_HUNDRED_WON;
+import static onboarding.Problem5.MoneyConst.FIVE_THOUSAND_WON;
+import static onboarding.Problem5.MoneyConst.ONE_HUNDRED_WON;
+import static onboarding.Problem5.MoneyConst.ONE_THOUSAND_WON;
+import static onboarding.Problem5.MoneyConst.ONE_WON;
+import static onboarding.Problem5.MoneyConst.TEN_THOUSAND_WON;
+import static onboarding.Problem5.MoneyConst.TEN_WON;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,7 +24,8 @@ public class Problem5 {
     public static class MoneyCalculator {
 
         public static List<Integer> calculateMoneyCount(int money) {
-            int[] moneyTypes = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+            int[] moneyTypes = {FIFTY_THOUSAND_WON, TEN_THOUSAND_WON, FIVE_THOUSAND_WON,
+                ONE_THOUSAND_WON, FIVE_HUNDRED_WON, ONE_HUNDRED_WON, FIFTY_WON, TEN_WON, ONE_WON};
             List<Integer> result = new ArrayList<>();
 
             for (int i : moneyTypes) {
@@ -24,5 +34,18 @@ public class Problem5 {
             }
             return result;
         }
+    }
+
+    public abstract class MoneyConst {
+
+        public static final int FIFTY_THOUSAND_WON = 50000;
+        public static final int TEN_THOUSAND_WON = 10000;
+        public static final int FIVE_THOUSAND_WON = 5000;
+        public static final int ONE_THOUSAND_WON = 1000;
+        public static final int FIVE_HUNDRED_WON = 500;
+        public static final int ONE_HUNDRED_WON = 100;
+        public static final int FIFTY_WON = 50;
+        public static final int TEN_WON = 10;
+        public static final int ONE_WON = 1;
     }
 }
