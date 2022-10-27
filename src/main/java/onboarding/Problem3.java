bd40680a5e5d5de3a71e6d58886593e0329082be
kimJaeHyeon <kmr2644@gmail.com>
2022-10-27 11:26:38 +0900

    feat: implement method for count 369

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,12 +5,8 @@ import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
         List<Integer> separateList = separateToNumber(number);
-
-
-        return answer;
-
+        return (int)get369Count(separateList);
 
     }
     private static List<Integer> separateToNumber(int number) {
@@ -20,5 +16,14 @@ public class Problem3 {
             number = number / 10;
         }
         return separate;
+    }
+    private static long get369Count(List<Integer> separateList) {
+        return separateList.stream()
+                    .filter(Problem3::is369)
+                    .count();
+    }
+    private static boolean is369(Integer integer) {
+        return List.of(3, 6, 9)
+                   .contains(integer);
     }
 }
