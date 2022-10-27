4727dff3e5c779c4f321fb31a4378224ee333b0f
minhee020 <minhee020@naver.com>
2022-10-28 03:00:28 +0900

    Problem4 | 기능 구현 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/**
+ * 기능 구현
+ * 1. 소문자 알파벳 반대로, 대문자 알파벳 반대로 된 문자열 변수 생성.
+ * 2. 대문자인지 소문자인지 판별
+ * 3. 인덱스를 통해 알맞은 문자로 넣어주기
+ * 4. 알파벳이 아니라면 문자열에 넣기
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
