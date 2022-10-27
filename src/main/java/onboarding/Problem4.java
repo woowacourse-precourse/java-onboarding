dc651e680d6376b65d26c39dee464002be25db61
hstla <ree7243@gmail.com>
2022-10-27 10:58:34 +0900

    문제4 feat: 문자 변환 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 public class Problem4 {
+    public static HashMap<Character, Character> fullmap () {
+        HashMap<Character, Character> changeChar = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            changeChar.put((char) (65 + i), (char) (90 - i));
+        }
+        return changeChar;
+        }
+
+
     public static String solution(String word) {
         String answer = "";
         return answer;
