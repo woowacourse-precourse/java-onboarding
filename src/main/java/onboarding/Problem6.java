350410224abeba9c2eb79fd305582b37962e7e30
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-10-27 16:35:03 +0900

    feat : add problem6
    ### 구현방법
    모든 닉네임 패턴을 검사
    2글자 이상일때만 패턴으로 만든다.
    패턴과 이메일을 저장해두는 HashMap을 생성, 중복과 정렬을 함께 해주는 자료구조인 TreeSet을 통해 답안 email을 관리 하였다.
    패턴을 만든 후 만약 hashmap에 그 패턴이 존재하면, 그 패턴을 통해 email을 얻어와 현재 검사하는 formsEmail이랑 다르다면 다른 사람이 패턴에 걸리는 것이므로, 뽑아온 email과 formsEmail 모두 TreeSet에 넣는다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+
+	public static List<String> solution(List<List<String>> forms) {
+		TreeSet<String> emailSet = new TreeSet<>();
+		HashMap<String, String> map = new HashMap<>();
+		for (int i = 0; i < forms.size(); i++) {
+			String nickname = forms.get(i).get(1);
+
+			if (nickname.length() < 2) {
+				break;
+			}
+
+			for (int j = 0; j < nickname.length() - 1; j++) {
+				String key = nickname.substring(j, j + 2);
+				String formsEmail = forms.get(i).get(0);
+				if (map.containsKey(key)) {
+					final String email = map.get(key);
+					if (!email.equals(formsEmail)) {
+						emailSet.add(email);
+						emailSet.add(formsEmail);
+					}
+				}
+				map.put(key, formsEmail);
+			}
+
+		}
+		return new ArrayList<>(emailSet);
+	}
+
 }
