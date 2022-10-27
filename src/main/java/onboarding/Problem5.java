04781793629d525cd07fa9948db10069f53a14ec
Laeng <Laeng@users.noreply.github.com>
2022-10-27 19:30:33 +0900

    feat(problem5): 솔루션 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,8 +11,11 @@ public class Problem5 {
     };
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        if (!validateMoney(money)) {
+            return new LinkedList<>();
+        }
+
+        return classifyMoney(money);
     }
 
     /**
