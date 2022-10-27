c92157b685b5876b4ea51aeaf2c23a219cd6ea9f
안광민 <creativeduck0518@gmail.com>
2022-10-28 00:18:18 +0900

    hotfix(Problem 5): money 를 currenty의 나머지로 계산하는 부분의 순서 변경
    money를 currency의 나머지로 계산하기 이전에 먼저 currency의 값을 나누다보니 의도하지 않은 값이 발생했다.
    따라서 문장의 순서를 변경해서 문제를 해결했다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Problem5 {
     private static final int START_MONEY = 50_000;
 
+    public static void main(String[] args) {
+        solution(50_237);
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = new LinkedList<>();
         int currency = START_MONEY;
@@ -15,6 +19,7 @@ public class Problem5 {
         while(currency >= 10) {
             amount = money / currency;
             answer.add(amount);
+            money %= currency;
 
             if (isHalf) {
                 currency /= 5;
@@ -22,8 +27,6 @@ public class Problem5 {
             else {
                 currency /= 2;
             }
-
-            money %= currency;
             isHalf = !isHalf;
         }
 
