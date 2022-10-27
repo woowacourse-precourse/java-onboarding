a40459c56318ba02c9102a38a1f0433c57f7c428
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 01:59:39 +0900

    [Problem2] 중복된 문자 있는지 체크하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,20 @@ public class Problem2 {
      * 2. 중복된 문자 제거
      */
 
+    /**
+     * 1. 중복된 문자 있는지 체크
+     * @param cryptogram : 체크할 암호문
+     * @return : 중복된 문자가 있으면 해당 문자열 인덱스 값, 없으면 -1
+     */
+
+    static int checkDuplicate(String cryptogram) {
+        for(int i = 0; i < cryptogram.length() - 1; i++){
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) return i;
+        }
+
+        return -1;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
