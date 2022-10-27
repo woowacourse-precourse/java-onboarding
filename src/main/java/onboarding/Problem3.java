e7c375d1c64db94f13e64e7975d103f0308fc5fc
ycopp9 <aciees95@gmail.com>
2022-10-27 10:42:58 +0900

    feat: add the function toInts() that returns int array from String value

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int[] toInts(String value) {
+        int[] numbers = new int[value.length()];
+        for(int i = 0; i < value.length(); i++) {
+            numbers[i] = value.charAt(i) - '0';
+        }
+        return numbers;
+    }
 }
