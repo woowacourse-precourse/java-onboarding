f61e97fcdad82c068bd6110fe4bc2175a8300bb1
Hongsugyeong <olivia258@naver.com>
2022-10-27 20:17:57 +0900

    [problem2] 솔루션

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,8 +8,7 @@ public class Problem2 {
      * 스택을 문자열로 반환하는 함수
      */
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return remove(cryptogram);
     }
 
     /*
