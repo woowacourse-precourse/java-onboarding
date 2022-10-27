fd8c3979f967402afdaff65e11962cec729ceb95
Hyeonjae-K <href.hjk@gmail.com>
2022-10-27 23:52:39 +0900

    add: 기능 목록 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/* 기능 목록
+ * 숫자에 포함된 3, 6, 9의 개수 반환 기능
+ * 1부터 number까지 반복 기능
+ * */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
