a6489031fdc055387db54fa3f5a95b40bb4edd1d
KyungKyu Park <rudrb0505@gmail.com>
2022-10-27 11:52:35 +0900

    feat: 페이지의 짝수, 홀수 판단 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,7 +9,11 @@ class Problem1 {
 		int crongLeftPage = crong.get(0);
 		int crongRightPage= crong.get(1);
 
-		int answer = Integer.MAX_VALUE;
-		return answer;
+		int result = 0;
+		if (pobiLeftPage % 2 == 0 || pobiRightPage % 2 == 1 || crongLeftPage % 2 == 0 || crongRightPage % 2 == 1) {
+			result =  -1;
+		}
+		// int answer = Integer.MAX_VALUE;
+		return result;
 	}
 }
