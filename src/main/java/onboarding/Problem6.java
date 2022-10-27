4f172c99a5ee9de5c50a72c5b4add9fe297650f1
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-27 18:21:29 +0900

    feat: Problem6 - 같은 글자가 연속적으로 파함된 닉네임을 찾는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,17 +2,25 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         HashMap<String, Integer> userTwoLetterList = new HashMap<>();
+        Set<String> DuplicateNameList = new HashSet<>();
 
         List<String> answer = List.of("answer");
         for (List<String> form : forms) {
             makeTwoLetterList(form.get(1), userTwoLetterList);
         }
+        for (Map.Entry<String, Integer> entry : userTwoLetterList.entrySet()) {
+            makeDuplicateNameList(DuplicateNameList, entry);
+        }
+
+
 
         return answer;
     }
@@ -22,6 +30,12 @@ public class Problem6 {
         for (int i = 0; i < splitNickName.length-1; i++) {
             String userTwoLetter = splitNickName[i]+splitNickName[i+1];
             twoLetterList.put(userTwoLetter, twoLetterList.getOrDefault(userTwoLetter, 0) + 1);
+        }
+    }
+
+    public static void makeDuplicateNameList(Set<String> duplicateList, Map.Entry<String, Integer> entry) {
+        if (entry.getValue() > 1) {
+            duplicateList.add(entry.getKey());
         }
     }
 }
