d647eec64777a8d9728987499565180c6e519eab
SH <gram753@naver.com>
2022-10-27 22:27:41 +0900

    feat: Problem3 기능구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+
 	public static int solution(int number) {
+
 		int answer = 0;
+		for (int i = 1; i <= number; i++) {
+			answer += countClap(i);
+		}
 		return answer;
+	}
+
+	private static int countClap(int number) {
+		String string = String.valueOf(number);
+		int count = 0;
+		for (int i = 0; i < string.length(); i++) {
+			if (string.charAt(i) == '3' || string.charAt(i) == '6' || string.charAt(i) == '9') {
+				count++;
+			}
+		}
+		return count;
 	}
 }
