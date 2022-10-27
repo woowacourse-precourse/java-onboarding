d704ea61f0ea90763f76d41d40fd7479ae0d059d
YooByeongIk <quddlr96@naver.com>
2022-10-27 16:30:18 +0900

    [Problem2] feat : 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.Stack;
+
 /*
 ##기능 목록
 
@@ -7,24 +9,49 @@ package onboarding;
 2. 중복 제거
 3. 중복 문자가 없을 때까지 반복
 
-
  */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        char[] charArray = cryptogram.toCharArray();
-        while(isDuplicate(charArray)){
 
-        }
         return answer;
     }
 
-    public static boolean isDuplicate(char[] charArray) {
-        for(int i=1;i<charArray.length;i++){
-            if(charArray[i] == charArray[i-1]){
+    public static boolean isDuplicate(String str) {
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static String removeDuplicate(String str) {
+        Stack<Character> stack = new Stack<>();
+        boolean flag = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(str.charAt(i));
+            } else {
+                if (stack.peek() == str.charAt(i)) {
+                    flag = true;
+                } else {
+                    if (flag) {
+                        stack.pop();
+                        flag = false;
+                    }
+                    stack.push(str.charAt(i));
+                }
+                if (i == str.length() - 1 && flag) {
+                    stack.pop();
+                }
+            }
+        }
+        String result = "";
+        while (!stack.isEmpty()) {
+            result = stack.peek() + result;
+            stack.pop();
+        }
+        return result;
     }
 }
