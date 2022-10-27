7050767830a5797220a935d1141ec5575835883e
juhwang <juhong4930@naver.com>
2022-10-27 10:18:55 +0900

    chore(Problem3):game369Board rebuild and test Failed

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,16 +3,14 @@ package onboarding;
 import java.util.List;
 
 public class Problem3 {
+    private static final int[] GAME_369_BOARD = new int[10001];
     public static int solution(int number) {
-        game369BoardLoop();
+        game369Board();
         int answer = numberOnGame369Board(number);
         return answer;
     }
-    public static int[] game369Board(){
-        return new int[10001];
-    }
     public static int numberOnGame369Board(int number){
-        return game369Board()[number];
+        return GAME_369_BOARD[number];
     }
     public static String[] numToString(int num){
         return String.valueOf(num).split("(?<=.)");
@@ -21,14 +19,14 @@ public class Problem3 {
         List<String> numToString = List.of(numToString(num));
         return numToString.contains("3")||numToString.contains("6")||numToString.contains("9");
     }
-    public static void game369BoardLoop(){
-        for (int i = 3; i < 10001; i++) {
-            markClapNumber(i);
-        }
-    }
     public static void markClapNumber(int num){
         if(validCheck369(num)){
-            game369Board()[num] += game369Board()[num-1]+1;
+            GAME_369_BOARD[num] += GAME_369_BOARD[num-1]+1;
+        }
+    }
+    public static void game369Board(){
+        for (int i = 3; i < 10001; i++) {
+            markClapNumber(i);
         }
     }
 }
