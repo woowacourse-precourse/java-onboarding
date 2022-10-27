e8a204cf57912ad31a7e045c345764000c03282b
0112leesy <0112leesy@naver.com>
2022-10-27 21:31:37 +0900

    [Problem3] feat: is_clap_number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,10 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    static boolean is_clap_number(int number){
+        if(number == 3 || number == 6 || number == 9) return true;
+        return false;
+    }
+
 }
