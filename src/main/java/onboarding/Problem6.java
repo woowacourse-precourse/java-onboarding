a5ad1cbdbe44166f094e54c0f3646e7635c44318
suheonjoo <suheonj95@naver.com>
2022-10-27 20:53:37 +0900

    feat(Problem6): Problem6 메인 로직 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,41 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        int size = forms.size();
+        //사이즈가 1이하이면 그냥 리턴
+        if (size <= 1) {
+            return new ArrayList<>();
+        }
+
+        List<Member> memberRepository = new ArrayList<>();
+        Set<String> emailHashSet = new HashSet<>();
+
+        for (List<String> form : forms) {
+
+            if (memberRepository.size() == 0 ) {
+                memberRepository.add(new Member(form.get(1), form.get(0)));
+                continue;
+            }
+
+            for (Member member : memberRepository) {
+                if (hasConsecutiveString(form.get(1),member.name)) {
+                    emailHashSet.add(form.get(0));
+                    emailHashSet.add(member.email);
+                }
+            }
+            memberRepository.add(new Member(form.get(1), form.get(0)));
+        }
+
+        return emailHashSet.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     static class Member {
