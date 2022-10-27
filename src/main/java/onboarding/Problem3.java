1d9691f8465531a597b1d14cf02d9db08150f9dc
kokodak <kokodakadokok@gmail.com>
2022-10-27 15:51:03 +0900

    feat: [문제3] 손뼉을 총 몇 번 쳐야하는지 계산하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,7 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return getTotalClapCnt(number);
     }
 
     private static int getThreeSixNineCnt(int number) {
@@ -12,6 +11,16 @@ public class Problem3 {
 
         for (int i = 0; i < tmp.length(); i++) {
             if (tmp.charAt(i) == '3' || tmp.charAt(i) == '6' || tmp.charAt(i) == '9') ret++;
+        }
+
+        return ret;
+    }
+
+    private static int getTotalClapCnt(int number) {
+        int ret = 0;
+
+        for (int i = 1; i <= number; i++) {
+            ret += getThreeSixNineCnt(i);
         }
 
         return ret;
