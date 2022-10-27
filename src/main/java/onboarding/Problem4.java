f7f70d49acb26237793476c435d8a84ccad14970
Your Name <you@example.com>
2022-10-27 16:11:37 +0900

    Problem4 기능 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,11 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        /*문자 순회하면서 알파벳 찾기*/
+
+        /*문자가 대문자일 시 변환*/
+
+        /*문자가 소문자일 시 변환*/
         String answer = "";
         return answer;
     }
