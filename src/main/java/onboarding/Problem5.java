1f9baf5004623b8473e8830d0416077039218ca0
WonJeongYeon <wonjungyoun@naver.com>
2022-10-27 15:47:52 +0900

    20221027 Problem5.java 예외처리나 클린코드 없이 작동 기능만 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
+        int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int currencyCount;
+        int currentMoney = money;
+        for (int i = 0; i < currency.length; i++) {
+            currencyCount = currentMoney / currency[i];
+            answer.add(i, currencyCount);
+            currentMoney = currentMoney % currency[i];
+        }
         return answer;
     }
 }
