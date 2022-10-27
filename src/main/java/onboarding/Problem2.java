31cc7d925a14bcf3b22b2d68e52653bea0801e26
Reznov <reznov03048@gmail.com>
2022-10-27 23:31:12 +0900

    문자열에 삭제할 중복된 문자들이 있는지 확인하는 메소드 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,10 +5,33 @@ public class Problem2 {
         String answer = "answer";
 
         // 더 이상 중복된 문자가 없을 때 까지 반복해서 중복된 문자 삭제하기
+        while (true){
+            if(!beAbleToDelete(cryptogram)) break;
+
+
+        }
 
         // 결과 반환
 
 
         return answer;
     }
+
+    /**
+     * 문자열에 삭제할 중복된 문자들이 있는지 확인하는 메소드
+     * @param str
+     * @return
+     */
+    static boolean beAbleToDelete(String str){
+        boolean[] isDuplicated = new boolean[str.length()];
+
+        for(int i=0; i<str.length()-1; i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
