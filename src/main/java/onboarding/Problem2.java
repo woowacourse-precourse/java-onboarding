3609200bb467858eab9d0e8b7b34a01680c63e31
squareyun <squareyun@gmail.com>
2022-10-27 21:48:25 +0900

    style: 코드 라인 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,13 +3,15 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
-
         int startIdx = getOverlabStartIdx(answer);
+
         while (startIdx != -1) {
             int endIdx = getOverlabEndIdx(answer, startIdx);
+
             String substr1 = answer.substring(0, startIdx);
             String substr2 = answer.substring(endIdx);
             answer = substr1 + substr2;
+
             startIdx = getOverlabStartIdx(answer);
         }
 
@@ -17,18 +19,23 @@ public class Problem2 {
     }
 
     static int getOverlabStartIdx(String s) {
-        if (s.length() <= 1) return -1;
+        if (s.length() <= 1) {
+            return -1;
+        }
+
         for (int i = 0; i < s.length() - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 return i;
             }
         }
+
         return -1;
     }
 
     static int getOverlabEndIdx(String s, int overlabIdx) {
         char base = s.charAt(overlabIdx);
         int endIdx = overlabIdx + 1;
+
         for (int i = endIdx; i < s.length(); i++) {
             if (base == s.charAt(i)) {
                 endIdx += 1;
