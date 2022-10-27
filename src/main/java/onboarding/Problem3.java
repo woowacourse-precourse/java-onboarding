bb5797bc646b4215afcef6659ca66cd90ddeb440
ddubidubap <park62000@gmail.com>
2022-10-27 11:53:16 +0900

    PROBLEM3 기능 요구 사항 목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+    /*
+        기능 요구 사항 목록
+        1. 숫자 당 박수 카운터 함수
+        2. solution (반복문으로 문자 제거)
+    */
     public static int solution(int number) {
         int answer = 0;
         return answer;
