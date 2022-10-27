c0c1b609ccfbf1c1c7170ee1b0d2fd16cdde0e77
juhwang <juhong4930@naver.com>
2022-10-27 14:44:57 +0900

    feat(Problem5):adjusting money to convert into cash

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> MONEYLIST = List.of(50000,10000,5000,1000,500,100,50,10,1);
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+    private static int divideMoney(int money, int divMoney){
+        return Math.floorDiv(money,divMoney);
+    }
+    private static int moduloMoney(int money, int divMoney){
+        return Math.floorMod(money,divMoney);
     }
 }
