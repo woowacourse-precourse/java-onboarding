4398de1414eac79cd594ccd75988d9b990b49d37
MinjeongKong <kong1301@ajou.ac.kr>
2022-10-27 10:31:47 +0900

    Problem1 - add checkPage

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,8 +10,22 @@ import java.util.List;
  * 4. 승자 출력 메서드 getWinner
  */
 class Problem1 {
+    public static boolean checkPage(List<Integer> pages) {
+
+        if (pages.size() != 2) return false;
+
+        int left = pages.get(0);
+        int right = pages.get(1);
+
+        if ((left + 1) != right) return false;
+        if (left <= 1 || right >= 400) return false;
+
+        return true;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!checkPage(pobi) || !checkPage(crong)) return -1;
         return answer;
     }
+
 }
