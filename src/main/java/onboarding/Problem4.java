703b6d20b1d6f281afb24b7dfc5ea42bf29af26e
ycopp9 <aciees95@gmail.com>
2022-10-27 13:33:20 +0900

    feat: add the function convertAlphaInReverse() that return alphabet in reverse

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,13 @@ public class Problem4 {
         return answer;
     }
 
+    private static char convertAlphaInReverse(char c) {
+        if(isUpperCase(c)) {
+            return (char)('A' + ('Z' - c));
+        }
+        return (char)('a' + ('z'- c));
+    }
+
     private static boolean isAlpha(char c) {
         return isUpperCase(c) || isLowerCase(c);
     }
