4d14d4da9148b86516e66670c8bc3b8913e52e06
Jihoon9809 <dkdb521@naver.com>
2022-10-27 23:08:01 +0900

    이거 하는데 이틀 ㅜㅜ....

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,52 @@
 package onboarding;
 
+import static java.lang.Math.*;
+
 public class Problem4 {
+    public static char upperReplaceWorld(char word){
+        int replaceTarget = (char)word;
+
+        int aTozAsciiCodeRange = 25;
+        int upperAasciiCode = 65;
+        int upperZasciiCode = upperAasciiCode + aTozAsciiCodeRange;
+
+        int RangeOfChangeWorde = abs(upperAasciiCode - replaceTarget);
+        int reverseOfword = upperZasciiCode - RangeOfChangeWorde;
+        char reverseWord = (char)reverseOfword;
+
+        return reverseWord;
+    }
+
+    public static char lowerReplaceWorld(char word){
+        int replaceTarget = (char)word;
+        int aTozAsciiCodeRange = 25;
+        int lowerAasciiCode = 97;
+        int lowerZasciiCode = lowerAasciiCode + aTozAsciiCodeRange;
+
+        int RangeOfChangeWorde = abs(lowerAasciiCode - replaceTarget);
+        int reverseOfword = lowerZasciiCode - RangeOfChangeWorde;
+        char reverseWord = (char)reverseOfword;
+
+        return reverseWord;
+    }
+
     public static String solution(String word) {
         String answer = "";
+
+        for(int i=0;i< word.length();i++){
+            char replaceTarget = word.charAt(i);
+
+            if(Character.isUpperCase(replaceTarget)) answer += upperReplaceWorld(replaceTarget);
+            if(Character.isLowerCase(replaceTarget)) answer += lowerReplaceWorld(replaceTarget);
+            if(replaceTarget == ' ') answer += ' ';
+        }
+
         return answer;
     }
 }
+
+// word의 단어 한개를 검사한다 .
+// 대문자일 경우 대문자 호출 --> 치환메서드 호출
+// 소문자일 경우 소문자 호출 --> 치환메서드 호출
+// 대문자, 소문자 치환에서 중복되는 부분 메서드 구현.
+//반환.
