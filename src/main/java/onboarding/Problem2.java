6d77b2eca9eb303a06b57db9c7849684d5ffbbbb
Dabeen Jeong <hello70825@gmail.com>
2022-10-27 11:06:11 +0900

    feat: [Problem2] 올바른 입력이 들어왔는지 확인하는 기능을 추가하다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        if (checkValidation(answer)) {
+
+        } else {
+            answer = "-1";
+        }
+
         return answer;
+    }
+
+    private static boolean checkValidation(String inputString) {
+        int inputStringLength = inputString.length();
+
+        // 길이가 1 이상 400 이하여야 한다.
+        if (!(1 <= inputStringLength && inputStringLength <= 400)) {
+            return false;
+        }
+
+        // 알파벳 소문자만 들어 있어야 한다.
+        String pattern = "^[a-z]*$";
+        if (!Pattern.matches(pattern, inputString)) {
+            return false;
+        }
+
+        return true;
     }
 }
