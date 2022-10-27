a7c6f4eec2e3343c7ac2a7377e621668d7e598d8
cpot5620 <=cpot5620@naver.com>
2022-10-28 00:54:25 +0900

    Implement problem 3
    If number has 3, 6, or 9, count of them and print sum.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += numberOfClap(i);
+        }
+
         return answer;
+    }
+
+    public static int numberOfClap(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            count = (num % 10 == 3 || num % 10 == 6 || num % 10 == 9)
+                    ? (count + 1)
+                    : count;
+            num /= 10;
+        }
+
+        return count;
     }
 }
