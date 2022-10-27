9614760bc98d389021af5cf042a5abd071ba2955
kwYoohae <gocksdl7979@gmail.com>
2022-10-27 13:59:29 +0900

    feat(Problem2.java): 중복 문자가 삭제되어 유효한 값인지 확인하는 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,7 +7,19 @@ import java.util.Arrays;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        int cryptoLength = cryptogram.length();
+        String answer = cryptogram;
+        String removeCryptogram = cryptogram;
+
+        for (int i = 0; i < cryptoLength; i++) {
+            removeCryptogram = duplicateRemove(answer, i);
+            if (!answer.equals(removeCryptogram)) {
+                answer = removeCryptogram;
+                cryptoLength = answer.length();
+                i = -1; // 첫 번째 문장부터 다시 검사하기 위한 초기화 끝나면 i++이 되기 때문에 i = -1이어야함
+            }
+        }
+
         return answer;
     }
 
@@ -30,4 +42,5 @@ public class Problem2 {
 
         return String.join("", splitCryptogram);
     }
+
 }
