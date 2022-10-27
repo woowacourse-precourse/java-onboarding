f11f4082b0d0bdda8ca5f5d6cb51f4e766babe40
antaeho <an981022@naver.com>
2022-10-27 22:30:20 +0900

    기능 구현사항 목록 정리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,13 @@
 package onboarding;
 
 import java.util.List;
-
+/*
+1. 각 페이지 번호를 받아서 각 자리 숫자를 더하고, 곱한 숫자중 최댓값을 리턴 하는 함수 선언
+2. 예외처리 사항 return -1
+3. 1번 단계에서 얻은 값을 비교해 result 출력
+ */
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
