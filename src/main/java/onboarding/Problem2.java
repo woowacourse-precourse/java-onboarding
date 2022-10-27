d6926e994921cac346f1989c03d71c76c3f7227e
hyeongu <wancern@naver.com>
2022-10-28 00:54:45 +0900

    Problem2. 요구사항 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
-
+// 1. Deque을 이용하여 기능 구현
+// 2. Deque이 비어있을 경우 offer
+// 3. 새로운 문자와 Deque의 마지막 문자가 같을경우 poll
+// 4. 다를 경우 offer
+// 5. 최종적으로 남아있는 문자 반환
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
