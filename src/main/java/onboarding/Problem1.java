5b3ec9d7490dab801bffe9d20b905c7babb5d455
qdeu22 <qdeu22@gmail.com>
2022-10-27 16:51:23 +0900

    최대값 구하는 기능 커밋

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -26,9 +26,25 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        /*
+        * p_max : 포비의 최대값 변수
+        * c_max : 크롱의 최대값 변수
+        */
+        int p_max = Integer.MIN_VALUE;
+        int c_max = Integer.MIN_VALUE;
+
         /* 예외 처리 기능 */
         if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1))
             return -1;
+
+        for (int i = 0; i < 2; i++) {
+            int p = pobi.get(i);
+            int c = crong.get(i);
+
+            /* 최대값 구하는 기능 */
+            p_max = Math.max(p_max, Math.max(hap(p), mul(p)));
+            c_max = Math.max(c_max, Math.max(hap(c), mul(c)));
+        }
 
         return answer;
     }
