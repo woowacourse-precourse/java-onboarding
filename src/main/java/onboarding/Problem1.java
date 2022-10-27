63756ef0f27f1aa8388cc0422321e382c15deb2f
plusjob70 <plusjob70@gmail.com>
2022-10-27 22:20:51 +0900

    feat: 페이지 번호 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,43 @@
 package onboarding;
-
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        // pobi와 crong의 유효성 검사
+        if (!validate(pobi) || !validate(crong)) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    // 책 페이지의 유효성 검사
+    private static boolean validate(List<Integer> pages) {
+        int left;
+        int right;
+
+        // 두 페이지 중에 null이 존재하면 invalid
+        if ((pages.get(0) == null) || (pages.get(1) == null)) {
+            return false;
+        }
+
+        left = pages.get(0);
+        right = pages.get(1);
+
+        // 왼쪽 페이지가 짝수이거나 오른쪽 페이지가 홀수이면 invalid
+        if ((left % 2 == 0) || (right % 2 == 1)) {
+            return false;
+        }
+
+        // 책의 시작 면이거나 마지막 면이 나오면 invalid
+        if ((left == 1 || left == 399) && (right == 2 || right == 400)) {
+            return false;
+        }
+
+        // 오른쪽 페이지와 왼쪽 페이지의 차이가 1이 아니면 invalid
+        if (right - left != 1) {
+            return false;
+        }
+        return true;
     }
 }
