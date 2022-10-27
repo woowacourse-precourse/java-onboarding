b49391953cea9a7a52969f3a80a39d8d3b415fd7
bomin1996 <bomin199628@gmail.com>
2022-10-27 13:32:45 +0900

    문제3번 숫자에 369가 들어가면 박수치는 부분 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,14 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
+        while (number > 0) {
+            int countNumber = number % 10;
+            if (countNumber == 3 || countNumber == 6 || countNumber == 9) {
+                answer++;
+            }
+            number /= 10;
+        }
+        
         return answer;
     }
 }
