3b3f5d4cd5e4f64861b90d6468a4c510b185414a
saehim <saeilee.vxv@gmail.com>
2022-10-28 02:14:34 +0900

    Feat(Problem2): 중복 문자 없을 때까지 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while(deleteIdx(cryptogram).size()!=0){
+            cryptogram = replace(cryptogram);
+        }
+        return cryptogram;
+    }
+
     // 중복 문자 삭제
     private static String replace(String input) {
         List<Integer> delete = deleteIdx(input);
