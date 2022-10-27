bc2bd7791b44d813e62d59786ba4aeaf852dfda2
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-27 22:40:11 +0900

    feat: Problem_1 페이지를 점수로 바꾸는 기능 구현
    페이지의 자릿수의 각 숫자를 구하고
    모두 더한 값과 모두 곱한 값중 큰 값을 반환한다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,30 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static int toScore(int page){
+        int addedResult = 0;
+        int multipliedResult = 1;
+
+        for (int i=getMaxPlaceValue(page); i>0; i/=10){
+            int score = page / i;
+            page %= i;
+
+            addedResult += score;
+            multipliedResult *= score;
+        }
+
+        return Math.max(addedResult, multipliedResult);
+    }
+
+    private static int getMaxPlaceValue(int page){
+        int placeValue = (int)(Math.log10(page));
+        int result = 1;
+
+        for (int i=0; i<placeValue; i++){
+            result *= 10;
+        }
+
+        return result;
+    }
 }
