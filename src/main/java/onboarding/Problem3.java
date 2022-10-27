dc4456a8aeb4bf8ad11ad3d3649306d247922f8a
kokodak <kokodakadokok@gmail.com>
2022-10-27 15:46:14 +0900

    feat: [문제3] 숫자 안에 있는 3, 6, 9의 개수를 세는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int getThreeSixNineCnt(int number) {
+        String tmp = Integer.toString(number);
+        int ret = 0;
+
+        for (int i = 0; i < tmp.length(); i++) {
+            if (tmp.charAt(i) == '3' || tmp.charAt(i) == '6' || tmp.charAt(i) == '9') ret++;
+        }
+
+        return ret;
+    }
 }
