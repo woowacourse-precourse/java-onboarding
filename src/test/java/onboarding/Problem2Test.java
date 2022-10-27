51583d0410573a85e498eb79698f68b37736dab8
xi_jjun <rlawowns97@naver.com>
2022-10-27 17:56:23 +0900

    test(Problem2): 더 이상 연속되지 않을 때 까지 연속된 문자 지우는 기능 테스트

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -13,7 +13,18 @@ class Problem2Test {
 		// when : input 에서 같은 문자가 연속한 경우 빈 문자로 대체
 		String erasedString = Problem2.eraseSuccessiveCharacters(input);
 
-		// when : 연속하는 같은 문자들이 제거됐는지 확인
+		// then : 연속하는 같은 문자들이 제거됐는지 확인
 		assertThat(erasedString).isEqualTo(expected);
+	}
+
+	@ParameterizedTest(name = "''{0}'' -- erase until unchanged --> ''{1}''")
+	@CsvSource(value = {"a:a", "aabb:''", "abc:abc", "aabc:bc", "abaabac:c", "aabbbbdccdq:q",
+			"mmbnnbr:r", "abccabc:ababc", "ab:ab", "bbbbbb:''", "abbabba:''", "nbdbddn:nbdbn"}, delimiter = ':')
+	void stringEraseUtilUnchangedTest(String input, String expected) {
+		// when : input 에서 같은 문자가 더이상 연속하지 않을 때까지 빈 문자로 바꾸기
+		String output = Problem2.eraseSuccessiveCharactersUntilUnchanged(input);
+
+		// then : 연속하지 않을 때까지 빈 문자로 바뀌었는지 확인
+		assertThat(output).isEqualTo(expected);
 	}
 }
