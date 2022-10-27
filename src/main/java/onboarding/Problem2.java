eddeca1b6baf953aba4f2291158dc6d3551a667d
nswon <swnam0729@gmail.com>
2022-10-27 20:34:47 +0900

    refactor(Problem2) <deleteDuplicateChars>: 연속하는 중복된 문자 버그 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 public class Problem2 {
 
     private static Stack<Character> characterStack;
+    private static char rememberChar = '\0';
 
     public static String solution(String cryptogram) {
         initStack(cryptogram);
@@ -13,12 +14,18 @@ public class Problem2 {
 
     private static String deleteDuplicateChars(String cryptogram) {
         for (int i = 1; i < cryptogram.length(); i++) {
+            if(rememberChar == cryptogram.charAt(i)) {
+                continue;
+            }
+
             if (validateSameChar(characterStack.peek(), cryptogram.charAt(i))) {
+                rememberChar = characterStack.peek();
                 characterStack.pop();
                 continue;
             }
 
             characterStack.push(cryptogram.charAt(i));
+            rememberChar = '\0';
         }
 
         return CharsToString();
