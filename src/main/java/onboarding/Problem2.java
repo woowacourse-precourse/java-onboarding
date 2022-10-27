b91ed83584bbb013cc5935f9913f2eb80f9fd89b
ParkRootSeok <g_e_un@naver.com>
2022-10-27 22:10:03 +0900

    feat : Promblem2 - 중복 문자 유무 검색 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
 public class Problem2 {
+
+
+    public static boolean hasTextDuplication(StringBuilder crypto) {
+
+        int len = crypto.length();
+
+        for (int i = 0; i < len - 1; i++) {
+
+            // 현재 위치와 다음 위치의 문자가 같을 경우 중복
+            if (crypto.charAt(i) == crypto.charAt(i + 1)) {
+                    return true;
+            }
+        }
+
+        return false;
+
+    }
+
     public static String solution(String cryptogram) {
+
         String answer = "answer";
+        answer = removeTextDuplication(cryptogram);
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String result = solution("browoanoommnaon");
+        System.out.println(result);
     }
 }
