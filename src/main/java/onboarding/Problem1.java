97822a10ffd5659419979257511a9d8f97706c8d
lzlkolo <lzlko1209@gmail.com>
2022-10-27 10:25:22 +0900

    PROBLEM1.큰 수 구하기 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int pageNumCheck(List<Integer> list) {
@@ -14,6 +15,19 @@ class Problem1 {
             return -1;
         }
         return 0;
+    }
+
+    public static int getMax(int num) {
+        int max = Integer.MIN_VALUE;
+        int add = 0;
+        int mul = 1;
+        int[] arrNum = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+        for(int i=0; i<arrNum.length; i++) {
+            add += arrNum[i];
+            mul *= arrNum[i];
+            max = Math.max(add, mul);
+        }
+        return max;
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
