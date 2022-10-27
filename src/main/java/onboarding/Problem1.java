99bda6281268baaede0e5ffde194c0fc48cff1ad
dclxxi <dclxxi@kakao.com>
2022-10-27 17:50:07 +0900

    feat(문제1): 페이지 번호의 각 자리 숫자를 리스트로 반환하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    private static final char ZERO = '0';
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    public static List<Integer> getEachDigit(Integer page) {
+        List<Integer> digits = new ArrayList<>();
+        String pageString = page.toString();
+
+        for (int i = 0; i < pageString.length(); i++) {
+            digits.add(pageString.charAt(i) - ZERO);
+        }
+
+        return digits;
     }
 }
