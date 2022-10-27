2ff01f132870bd8e85191bdb4bbe5ecc4f7c160a
Hayoon <hy97@sju.ac.kr>
2022-10-27 14:25:59 +0900

    feat: 이름중복 검증 checkingList 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,25 +1,39 @@
 package onboarding;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem6 {
     static class CheckList {
         int [] checkName;
 
-        public CheckList(List<Integer> forms) {
+        public CheckList(List<List<String>> forms) {
             checkName = new int[forms.size()];
             for(int i = 0; i < forms.size(); i++)
                 checkName[i] = 0;
         }
-        void checkingList(List<List<String>> forms) {
-            String compName = forms.get(0).get(1);
+
+        int[] checkingList(List<List<String>> forms) {
+            String compName = "";
+            for (int i = 0; i < forms.size(); i++) {
+                if(checkName[i] == 0) {
+                    compName = forms.get(i).get(1);
+                } else continue;
+                for (int j = 1; j < forms.size(); j++) {
+                    if (compName.equals(forms.get(j).get(1))
+                            && checkName[j] == 0 && i != j) {
+                        checkName[j] = 1;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(checkName));
+            return checkName;
         }
     }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-
-
+        CheckList checkList = new CheckList(forms);
+        int[] ints = checkList.checkingList(forms);
         return answer;
     }
 
@@ -31,7 +45,7 @@ public class Problem6 {
                 List.of("mj@email.com", "엠제이"),
                 List.of("nowm@email.com", "이제엠")
         );
-//        System.out.println(forms.get(0).get(1));
+        System.out.println(forms.size());
         solution(forms);
     }
 }
