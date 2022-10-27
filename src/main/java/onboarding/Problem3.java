63c9f9286f52afe85951c7ef53ddea1a93139ed2
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-27 22:28:56 +0900

    problem3 구현기능 목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 구현 기능 목록
+ * 1. number를 인자로 받아 박수 cnt를 하는 기능 구현
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
