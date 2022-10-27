a0c09195a44c718b0aab68feafc063029ed1eb07
bomin1996 <bomin199628@gmail.com>
2022-10-27 17:25:08 +0900

    문제5. 5만원부터 일원까지 순서대로 차감하면서 배열에 추가후 answer반환 하는 기능. 5번문제 완료했습니다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,22 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         answer.add(0, money / 50000);
         money=money-((money/50000)*50000);//5만원
+        answer.add(1, money / 10000);
+        money=money-((money/10000)*10000);//만원
+        answer.add(2, money / 5000);
+        money=money-((money/5000)*5000);//5천원
+        answer.add(3, money / 1000);
+        money=money-((money/1000)*1000);//천원
+        answer.add(4, money /500);
+        money=money-((money/500)*500);//오백원
+        answer.add(5, money / 100);
+        money=money-((money/100)*100);//백원
+        answer.add(6, money / 50);
+        money=money-((money/50)*50);//오십원
+        answer.add(7, money / 10);
+        money=money-((money/10)*10);//십원
+        answer.add(8, money / 1);
+        money=money-((money/1)*1);//일원
         return answer;
     }
 }
