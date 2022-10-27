747c5681cbc529cc29485f5b2ca1ca7f215e5b31
song-wooseok <sws981202@gmail.com>
2022-10-27 16:37:08 +0900

    feat: solution 메서드 작성 및 1번 문제 테스트 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,11 @@
 package onboarding;
 
+import onboarding.problem1.controller.PageNumberGame;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = new PageNumberGame().startGame(pobi, crong).getGameResult();
         return answer;
     }
 }
