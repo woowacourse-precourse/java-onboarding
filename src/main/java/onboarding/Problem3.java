d5a3fc8d058564e01cb4a1722efd01186d34ee39
MIN-04 <kimji_0424@naver.com>
2022-10-27 17:09:20 +0900

    update Problem3 기능 요구사항 주석

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,12 @@
 package onboarding;
 
+
 public class Problem3 {
+    /**
+     * 기능 요구 사항
+     * 1. 숫자 3, 6, 9가 들어가는지 확인
+     * 2. 들어가면 손뼉 ( answer 값에 + 1)
+     */
     public static int solution(int number) {
         int answer = 0;
 
