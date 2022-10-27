195be28a70377ced1b3c7a96e055a634421eecd2
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-27 23:57:36 +0900

    feat : problem3 solve

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
 
-        int answer = 0;
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            String numberStr = String.valueOf(i);
+            count = (int) (count + countChar(numberStr, '3') + countChar(numberStr, '6') + countChar(numberStr, '9'));
+        }
+        int answer = count;
         return answer;
     }
 
