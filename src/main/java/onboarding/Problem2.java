3feab08387c0035b4100dca1d2e4dc3d1a8a5a87
asas6978 <asas6978@naver.com>
2022-10-27 22:56:03 +0900

    feat(problem2): 문제2 호출 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import static onboarding.problem2.Delete.executeDelete;
+import static onboarding.problem2.Validation.isOutOfRange;
+import static onboarding.problem2.Validation.checkValidForm;
+
 public class Problem2 {
+    public static final String INPUT_FORM_ERROR = "문자열 형식이 잘못되었습니다.";
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        if(isOutOfRange(cryptogram)) {
+            return INPUT_FORM_ERROR;
+        }
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            answer = checkValidForm(cryptogram.charAt(i));
+        }
+
+        if(INPUT_FORM_ERROR.equals(answer)) {
+            return INPUT_FORM_ERROR;
+        }
+
+        answer = executeDelete(cryptogram);
+
         return answer;
     }
 }
