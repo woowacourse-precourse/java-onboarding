1c0d4c1de64ea1b2818c510d2c4895cf9b578ce8
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-27 19:09:00 +0900

    feat(Problem2): 문자열 첫 번째, 마지막 문자인지 확인
    인덱스 범위 초가 오류를 방지하기 위함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
 public class Problem2 {
+    static boolean state = true;
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (state) {
+            state = false;
+
+            answer = getAnswerWithoutDuplicate(cryptogram);
+
+            cryptogram = answer;
+        }
+
         return answer;
+    }
+
+    private static String getAnswerWithoutDuplicate(String cryptogram) {
+        StringBuilder answer = new StringBuilder();
+        boolean isDuplicate;
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+
+            if (i == 0) {
+
+            } else if (i == cryptogram.length() - 1) {
+
+            } else {
+
+            }
+        }
+
+        return answer.toString();
     }
 }
