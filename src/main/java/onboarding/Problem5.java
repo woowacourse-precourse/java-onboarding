b3362306522bca47daba15505d5419b70fb14da0
taeyun-work <wmfajr1357@gmail.com>
2022-10-27 22:43:00 +0900

    [Refac] Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,20 +9,16 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         answer = new ArrayList<>(8);
-        answer = getCountBill(answer, money);
+        getCountBill(answer, money);
         return answer;
     }
 
-    public static List<Integer> getCountBill(List<Integer> answer, int money){
+    public static void getCountBill(List<Integer> answer, int money){
         List<Integer> billList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
-        List<Integer> res = answer;
         int tmp = money;
         for(int bill : billList) {
             answer.add(tmp / bill);
             tmp %= bill;
         }
-        return res;
     }
-
-
 }
