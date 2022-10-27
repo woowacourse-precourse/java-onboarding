53538710497a12b564e7fae262f2ae0bf77d3dff
jjongwa <troas96@naver.com>
2022-10-27 09:42:12 +0900

    문제2- stack 사용한 문자 중복 제거 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    static String decrypt(String crypt){
+        Stack<Character> stack = new Stack<>();
+        stack.add(crypt.charAt(0));
+        char top = stack.peek();
+        for(int i= 1; i < crypt.length(); i++) {
+            if (crypt.charAt(i) == stack.peek()) {
+                top = stack.peek();
+                stack.pop();
+            }
+            if (crypt.charAt(i) != top) {
+                stack.add(crypt.charAt(i));
+                top = stack.peek();
+            }
+        }
+        StringBuilder ret_value = new StringBuilder();
+        for (Character character : stack) {
+            ret_value.append(character.toString());
+        }
+        //System.out.println(ret_value);
+        return ret_value.toString();
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decrypt(cryptogram);
     }
 }
