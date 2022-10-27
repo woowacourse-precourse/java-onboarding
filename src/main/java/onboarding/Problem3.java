cf4bffbd9135e42480874942162dd1245ca62fad
Jeonwoojae <dnwo0719@icloud.com>
2022-10-27 11:11:38 +0900

    문제 3 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int quotitent;
+
+        for (int i=0; i<number;i++)
+        {
+            quotitent = i;
+
+            while (quotitent != 0){
+//            나머지가 3의 배수이면
+                if ((quotitent % 10) % 3 == 0)
+                {
+                    answer++;
+                    break;
+                }
+                quotitent /= 10;
+            }
+
+        }
+
         return answer;
     }
 }
