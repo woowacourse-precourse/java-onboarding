ca355d8ef091f926a202e49c28ef5ecac4cdaa4d
ParkSangCheon <pswlove38@naver.com>
2022-10-27 18:35:36 +0900

    난수 발생

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,21 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Random;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static int random(){
+        Random rd = new Random();
+        return rd.nextInt(400);
+    }
+    // 난수 발생
+    public static void main(String[] args){
+        System.out.println(random());
+    }
+    // 리스트 초기화
+    // 리스트 비교
 }
