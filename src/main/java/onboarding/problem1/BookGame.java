c1a13d091144c486ba3c8eaa4147862ff16be498
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 15:47:07 +0900

    feat(P1): 검증 로직 구현

--- src/main/java/onboarding/problem1/BookGame.java
+++ src/main/java/onboarding/problem1/BookGame.java
@@ -2,22 +2,32 @@ package onboarding.problem1;
 
 import java.util.List;
 
+import static onboarding.problem1.PageValidation.*;
+
 public class BookGame {
 
     private static final int FIRST_PAGE_WIN = 1;
     private static final int SECOND_PAGE_WIN = 2;
     private static final int DRAW = 0;
-    private static final int ILLEGAL_PAGE_VALUES = -1;
+    private static final int ILLEGAL_PAGES = -1;
 
     public static int doBookGame(List<Integer> firstPages, List<Integer> secondPages) {
         // 게임을 수행하는 메서드
+        if (!isValidPage(List.of(firstPages, secondPages))) {
+            return ILLEGAL_PAGES;
+        }
 
-        return -1;
+        return 1;
     }
 
     private static int getWinner(int firstPageResult, int secondPageResult) {
         // 페이지 계산 결과를 받아 우승자를 구한다.
 
         return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doBookGame(List.of(1, 2), List.of(3, 6)));
+        System.out.println(doBookGame(List.of(0, 2), List.of(3, 4)));
     }
 }
--- src/main/java/onboarding/problem1/PageValidation.java
+++ src/main/java/onboarding/problem1/PageValidation.java
@@ -2,18 +2,50 @@ package onboarding.problem1;
 
 import java.util.List;
 
+/**
+ * true : 검증 성공
+ * false  : 검증 실패
+ */
 public class PageValidation {
 
-    public static boolean isPageInvalid(List<Integer>... page) {
+    public static boolean isValidPage(List<List<Integer>> pagesList) {
         // page들을 받아 validation 합니다.
-        // page 개수
+        // pageList 중 하나라도 검증에 실패하면 false를 반환한다.
+        for (List<Integer> page : pagesList) {
+            if (!isValidSize(page.size())) {  // 페이지 개수 체크
+                return false;
+            }
+
+            if (!isValidRange(page) || !isVaildInterval(page)) {  // 잘못된 범위 혹은 간격일 경우
+                return false;
+            }
+        }
+
+        return true;  // 위 사항에 아무것도 해당되지 않으면 검증 성공.
+    }
+
+    private static boolean isValidSize(int size) {
+        final int DEFAULT_PAGE_SIZE = 2;
+        return size == DEFAULT_PAGE_SIZE;
+    }
+
+    private static boolean isValidRange(List<Integer> pages) {
+        // page의 범위를 validation 합니다.
+        for (Integer page : pages) {
+            if (page < 1 || page > 400) {
+                return false;
+            }
+        }
 
         return true;
     }
 
-    private static boolean isInvalidRange(int... page) {
-        // page의 범위를 validation 합니다.
-        return true;
+    private static boolean isVaildInterval(List<Integer> pages) {
+        // 이미 위에서 페이지 사이즈가 검증되었으므로, 2라고 확신해도 됨.
+        Integer firstPage = pages.get(0);
+        Integer secondPage = pages.get(1);
+
+        return secondPage - firstPage == 1;
     }
 
 }
