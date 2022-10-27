4fe2d51d9ea89e71ce8192387df077cf63a4a94b
sieun <sieun960513@naver.com>
2022-10-27 19:23:14 +0900

    [Problem5] : 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,13 +4,22 @@ import java.util.Collections;
 import java.util.List;
 
 /** 기능 목록
- * solutionLogic    : 메인 솔루션
- * checkValid       : 예외 처리
+ * countCoins   : 필요한 동전 개수 반환
+ * checkValid   : 예외 처리
  */
 
 public class Problem5 {
+    static int[] wallet = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static int countCoins(int num) {
+
+    }
+
+    public static boolean checkValid(int num) {
+
     }
 }
