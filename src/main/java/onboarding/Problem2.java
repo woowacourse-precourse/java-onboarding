16a3221c53b39ac9c0f46943b000744eb9d98847
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-27 10:42:16 +0900

    docs: function list of problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/**
+ * Problem 2 function list
+ * - decoding cryptogram
+ * - search duplication
+ * - treat duplication
+ * - treat non duplication
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
