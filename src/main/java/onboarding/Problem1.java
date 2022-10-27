a3508603b72c454334c82e6d6c60504d32c22583
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-27 23:25:44 +0900

    check if pages are in range of book

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,11 +6,16 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (notInOnePage(pobi) || notInOnePage(crong)) return -1;
         if (isOddAndEven(pobi) || isOddAndEven(crong)) return -1;
+        if (isInBookRange(pobi) || isInBookRange(crong)) return -1;
         int pobiMax = getMaxPage(pobi.get(0)) > getMaxPage(pobi.get(0)) ? getMaxPage(pobi.get(0)) : getMaxPage(pobi.get(0));
         int crongMax = getMaxPage(crong.get(0)) > getMaxPage(crong.get(0)) ? getMaxPage(crong.get(0)) : getMaxPage(crong.get(0));
         if (pobiMax == crongMax) return 0;
         else if (pobiMax > crongMax) return 1;
         else return 2;
+    }
+
+    public static boolean isInBookRange(List<Integer> intList) {
+        return (intList.get(0) < 3 || intList.get(1) > 398) ? true : false;
     }
 
     public static boolean isOddAndEven(List<Integer> intList) {
