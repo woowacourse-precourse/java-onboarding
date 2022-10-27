0933eecae48834117daceb3213d0e6d3e01da3bd
kjb95 <topmedia44@gmail.com>
2022-10-27 18:50:01 +0900

    feat: Problem1 - 포비와 크롱의 비정상적인 입력의 예외처리
    - 왼쪽 페이지 번호가 짝수인 경우
    - 오른쪽 페이지 번호가 홀수인 경우
    - (왼쪽 페이지 번호 + 1 != 오른쪽 페이지 번호)인 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,31 @@ import java.util.List;
 
 class Problem1 {
 
+    private static final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (isValidInput(pobi) || isValidInput(crong)) {
+            return EXCEPTION;
+        }
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean isValidInput(List<Integer> input) {
+        int leftPageNum = input.get(0);
+        int rightPageNum = input.get(1);
+
+        if (!isOdd(leftPageNum) || isOdd(rightPageNum) || leftPageNum + 1 != rightPageNum) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isOdd(int num) {
+        if (num % 2 == 1) {
+            return true;
+        }
+        return false;
     }
 }
