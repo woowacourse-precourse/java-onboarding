3dbaee9da361233371d9b82edcf4a618e9b8dc85
youngkyunIm <yee0230@naver.com>
2022-10-27 17:11:11 +0900

    fix(Problem4) : delete all method
    세부 기능 변경으로 인한 리셋

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,24 +3,6 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        char[] uppercase = makeUppercase();
-        char[] lowercase = makeLowercase();
         return answer;
-    }
-
-    public static char[] makeUppercase() {
-        char[] uppercase = new char[26];
-        for (int i = 0; i < 26; i++) {
-            uppercase[i] = (char) (i + 65);
-        }
-        return uppercase;
-    }
-
-    private static char[] makeLowercase() {
-        char[] lowercase = new char[26];
-        for (int i = 0; i < 26; i++) {
-            lowercase[i] = (char) (i + 97);
-        }
-        return lowercase;
     }
 }
