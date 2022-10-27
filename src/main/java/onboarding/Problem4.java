fed4b0e36c1b89bf8e441fc33ded571d6e6ef111
Beginner <dkstmdwn17@naver.com>
2022-10-27 22:01:02 +0900

    feet : P4 기능 요구사항과 청개구리 사전을 만드는 메서드를 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 기능 요구사항
+ * 1. 청개구리 사전을 구현한다.
+ * 2. 주어진 word를 반대로 변환해야 한다.
+ */
 public class Problem4 {
+    static Map<Character, Character> frogDictionary;
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        frogDictionary = new HashMap<>();
+        makingDictionary();
+        return answer.toString();
+    }
+
+    private static void makingDictionary() {
+        int tmp = 90;
+        for(int i=65;i<=90;i++){
+            frogDictionary.put((char) i, (char) tmp--);
+        }
     }
 }
