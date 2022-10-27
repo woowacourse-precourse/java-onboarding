069701e358b31cab5be8c47ca082d7c5796fc8ce
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-28 00:30:10 +0900

    Comment: Problem4 구현 기능목록

---
+++ src/main/java/onboarding/Problem4.java
@@ -0,0 +1,14 @@
+package onboarding;
+
+/**
+ * 구현 기능 목록
+ * 1. 문자열을 받아 역순으로 바꿔주는 기능
+ * 2. 치환한 문자열의 첫 글자를 대문자로 바꿔주기
+ * 3. result에 치환한 문자열을 담고 반환
+ */
+public class Problem4 {
+    public static String solution(String word) {
+        String answer = "";
+        return answer;
+    }
+}
