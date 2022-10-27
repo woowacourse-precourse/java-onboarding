1ee14127d37a02dc7d88863764eebf6a00181133
jeein2222 <jiinbag057@gmail.com>
2022-10-27 21:22:45 +0900

    problem2 중복문자 삭제하기 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        ArrayList<String> mylist=new ArrayList<>(List.of(cryptogram.split("")));
+
+        for(int i=0;i<mylist.size()-1;i++){
+            if((mylist.get(i)).equals(mylist.get(i+1))){
+                mylist.remove(i);
+                mylist.remove(i);
+                i=i-2;
+            }
+        }
+        for(String i:mylist){
+            answer+=i;
+        }
         return answer;
     }
 }
