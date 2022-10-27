fa436465c7563d95af02d5f6c34e7df9a11a71ae
jjongwa <troas96@naver.com>
2022-10-27 16:36:55 +0900

    문제6- 필요한 함수 틀 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,42 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    // 두 문자열 비교
+    static int checkString(String a, String b){
+
+        return 0;
+    }
+
+    // chkNum번 유저와 자신 뒤에 사람들 비교
+    static List<Integer> checkSimilar(List<Integer> S_List, int chkNum){
+
+        return S_List;
+    }
+
+    // 비슷한 닉네임 인덱스 모음 리스트 반환
+    static List<Integer> findSimilarList(List<List<String>> forms){
+        ArrayList<Integer> similarList = new ArrayList<>();
+        for(int i = 0; i < forms.size(); i++){
+            similarList.add(0);
+        }
+
+        for(int i = 0; i < forms.size(); i++){
+
+        }
+
+        return similarList;
+    }
+
+    // 인덱스 모음 리스트에 따른 이메일 리스트 반환
+    static List<String> returnEmail(List<Integer> similarList){
+        List<String> answer = List.of("answer");
+
+        return answer;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
