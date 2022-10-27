c64d8acf4403c66fe7e6ad19f195bb234e95af37
kang jihyeong <her4386@gmail.com>
2022-10-27 23:41:03 +0900

    function / Problem4 reverseSmall(), reverseBig() - aToz, AToZ 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,18 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static char reverseBig(char chr){
+        int offset = 'Z' - chr;
+
+        return (char)('A'+offset);
+    }
+
+    public static  char reverseSmall(char chr){
+        int offset = 'z' - chr;
+
+        return (char)('a'+offset);
+    }
+
+
 }
