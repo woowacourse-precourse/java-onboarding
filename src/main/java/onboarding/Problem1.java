698bb4c24f03e344eab02f8dd133721ac2dc254c
Albert <guang7226961@naver.com>
2022-10-27 10:42:47 +0900

    1. 페이지번호 유효성을 확인  - Problme1.checkPageNumber 메서드 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,30 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+	private final static int MINPAGE = 1;
+	private final static int MAXPAGE = 400;
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		if (!checkPageNumber(pobi) || !checkPageNumber(crong)) {
+			return -1;
+		}
+		return Integer.MAX_VALUE;
+	}
+
+	public static boolean checkPageNumber(List<Integer> pages) {
+		int limitPage = 2;
+		if (pages.size() != limitPage) {
+			return false;
+		}
+
+		if (pages.get(1) - pages.get(0) != 1) {
+			return false;
+		}
+
+		if (pages.get(0) <= MINPAGE || pages.get(1) >= MAXPAGE) {
+			return false;
+		}
+
+        return pages.get(0) % 2 == 1;
     }
 }
