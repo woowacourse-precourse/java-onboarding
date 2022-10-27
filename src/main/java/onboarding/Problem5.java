d811c0c419674610f5919853949767701c46d8fe
squirMM <jsm6616@naver.com>
2022-10-27 15:33:22 +0900

    :rocket: feat(Problem5) fin test

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> ans = new ArrayList<>();
+        for (int mt : moneyType){
+            if(money==0) ans.add(0);
+            else {
+                ans.add(Math.floorDiv(money, mt));
+                money = Math.floorMod(money, mt);
+            }
+        }
+        return ans;
     }
 }
