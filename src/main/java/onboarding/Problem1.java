6bbd16b808ba8257e105fced5f29da14ba7eb81b
yujaemin <woawoa0513@gmail.com>
2022-10-27 18:56:07 +0900

    feat: [문제 1] 점수 계산하기 기능 추가
    페이지 번호를 입력받아, 각 자리 숫자를 모두 더했을 떄와 곱했을 떄의 값중 더 큰 값을 구한다. 왼쪽 페이지와 오른쪽 페이지의 점수를 각각 계산하고, 그 중에서 큰 값이 최종 점수가 된다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,30 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        int pobiLeft = pobi.get(0), pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0), crongRight = crong.get(1);
+
+        int pobiScore = Math.max(getBigNumberByPage(pobiLeft), getBigNumberByPage(pobiRight));
+        int crongScore = Math.max(getBigNumberByPage(crongLeft), getBigNumberByPage(crongRight));
+
+        System.out.println(pobiScore);
+        System.out.println(crongScore);
+
+        return 0;
+    }
+
+    public static int getBigNumberByPage(int page) {
+        int addSum = 0, multiplySum = 1;
+
+        while (page > 0) {
+            int mod = page % 10;
+            addSum += mod;
+            multiplySum *= mod;
+            page /= 10;
+        }
+
+        int bigNumber = Math.max(addSum, multiplySum);
+        return bigNumber;
     }
 }
