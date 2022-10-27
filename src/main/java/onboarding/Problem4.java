b65ecacd1b567afa7a5919a1cecea03956f3214a
juhwang <juhong4930@naver.com>
2022-10-27 11:44:13 +0900

    feat(Problem4):defining spelling convert hashMap

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    private static final Map<Character, Character> SPELLING_CONVERT = defineSpellingConvert();
+    private static final int UPPERCASE_ASCII_START_NUMBER = 65;
+    private static final int LOWERCASE_ASCII_START_NUMBER = 97;
+    private static final int ALLCASE_SPELLING_TERM = 25;
     public static String solution(String word) {
-        String answer = "";
+        String answer = SPELLING_CONVERT.toString();
         return answer;
+    }
+    public static HashMap<Character,Character> defineSpellingConvert(){
+        Map map = new HashMap<Character,Character>();
+        for (int i = 0; i < ALLCASE_SPELLING_TERM+1; i++) {
+            map.put((char)(UPPERCASE_ASCII_START_NUMBER+i),(char)(ALLCASE_SPELLING_TERM+UPPERCASE_ASCII_START_NUMBER-i));
+            map.put((char)(LOWERCASE_ASCII_START_NUMBER+i),(char)(ALLCASE_SPELLING_TERM+LOWERCASE_ASCII_START_NUMBER-i));
+        }
+        return (HashMap<Character, Character>) map;
     }
 }
