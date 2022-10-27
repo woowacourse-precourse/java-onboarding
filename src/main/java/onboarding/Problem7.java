c4baf1a2c91a10ed4a9a983a75cc58a9f4c1479d
kevstevie <jianna442@gmail.com>
2022-10-28 02:53:35 +0900

    fix: fix Comparator to sort reverse

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -78,6 +78,6 @@ class SortFriendList implements Comparator<Map.Entry<String, Integer>>{
         if (o1.getValue() == o2.getValue()) {
             return o1.getKey().compareTo(o2.getKey());
         }
-        return o1.getValue().compareTo(o2.getValue());
+        return o2.getValue().compareTo(o1.getValue());
     }
 }
