fca7e19b0e1793893a91e91e2a95923cb2417fdd
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-27 13:20:35 +0900

    feat: [Problem2] 기능 요구사항 2(연속된 중복 문자열 제거) 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,9 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
@@ -13,7 +15,14 @@ public class Problem2 {
                 sames.add(i);
             }
         }
-        String answer = "answer";
+
+        sames.sort(Collections.reverseOrder());
+        for (Integer same : sames) {
+            collect.subList(same, same + 2).clear();
+        }
+
+
+        String answer = String.join("", collect);
         return answer;
     }
 }
