2da50b04c556ec8e865cbdac00277fcbf8b83f6b
qdeu22 <qdeu22@gmail.com>
2022-10-27 20:00:47 +0900

    키와 값을 가지는 map 생성 커밋

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,14 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        /* 키와 값을 가지는 map 생성 */
+        HashMap<Character, Character> m = new HashMap<>();
+
         return answer;
     }
 }
