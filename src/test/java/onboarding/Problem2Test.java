6c3f1c02931c544a603c456b806bccbd198af862
xi_jjun <rlawowns97@naver.com>
2022-10-27 21:43:18 +0900

    test(Problem2): 검증 로직 테스트

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -27,4 +27,36 @@ class Problem2Test {
 		// then : 연속하지 않을 때까지 빈 문자로 바뀌었는지 확인
 		assertThat(output).isEqualTo(expected);
 	}
+
+	@ParameterizedTest(name = "''{0}'' has valid length? ==> {1}")
+	@CsvSource(value = {"fndf:true", "a:true", "'':false"}, delimiter = ':')
+	void cryptogramLengthValidTest(String cryptogram, boolean expected) {
+		// when : 현재 들어온 암호의 길이가 1~1000 범위를 만족하는지 확인
+		boolean validationResult = Problem2.isValidLength(cryptogram);
+
+		// then : 유효한 길이라면 true, else false
+		assertThat(validationResult).isEqualTo(expected);
+	}
+
+	@ParameterizedTest(name = "''{0}'' has valid format? ==> {1}")
+	@CsvSource(value = {"fna:true", "ffNf:false", "f:true", "f4na:false", "fnn_n:false", "r=r:false",
+			"fja`geg:false", "'':false", "af ag:false"}, delimiter = ':')
+	void validateCryptogramAllLowerCaseCharacterTest(String cryptogram, boolean expected) {
+		// when : 들어온 암호가 모두 소문자 알파벳인지 확인할 때
+		boolean validationResult = Problem2.isLowerCaseCharacters(cryptogram);
+
+		// then : 모두 소문자 알파벳이라면 true, else false
+		assertThat(validationResult).isEqualTo(expected);
+	}
+
+	@ParameterizedTest(name = "validate test ''{0}'' ==> {1}")
+	@CsvSource(value = {"fna:true", "ffNf:false", "f:true", "f4na:false", "fnn_n:false", "r=r:false",
+			"fja`geg:false", "'':false", "af ag:false", "knldaf:true"}, delimiter = ':')
+	void validateCryptogramInputFormatTest(String cryptogram, boolean expected) {
+		// when : 들어온 암호가 유효한지 확인
+		boolean validationResult = Problem2.validateCryptogramFormat(cryptogram);
+
+		// then : 유효하다면 true, else false
+		assertThat(validationResult).isEqualTo(expected);
+	}
 }
