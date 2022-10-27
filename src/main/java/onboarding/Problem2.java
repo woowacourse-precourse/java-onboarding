4c50f61e12a11511b1a1dbae4ef6a34c858db9b6
Lee Sang Won <tkddnjs206@gmail.com>
2022-10-27 21:47:29 +0900

    solve2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,83 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem2 {
+
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        String tmp= "";
+        char[] c = cryptogram.toCharArray();
+        char first = c[0];
+        int cnt = 0;
+
+
+        int cou =1;
+        while(true)
+        {
+            tmp+=first;
+            System.out.println("c.length = " + c.length);
+            try {
+                for (int i = 1; i < c.length; i++) {
+                    if (first == c[i]) {
+                        cnt += 1;
+                        if (tmp.length() > 1)
+                            tmp = tmp.substring(0, tmp.length() - 1);
+                        else
+                            tmp = tmp.substring(0, 0);
+                    } else {
+                        tmp += c[i];
+                    }
+                    first = c[i];
+                }
+            }catch (Exception e){
+                System.out.println("e = " + e);
+                System.out.println("c len = " + c.length);
+                System.out.println("tmp1 = " + tmp);
+            }
+
+            if(cnt ==0 || tmp.equals("")) {
+                answer=tmp;
+                System.out.println("tsol mp = " + tmp);
+                break;
+            }
+            try {
+                System.out.println("tmp2 = " + tmp);
+                System.out.println("cnt = " + cnt);
+                c = tmp.toCharArray();
+                System.out.println("c = " + Arrays.toString(c));
+
+                cnt = 0;
+                first = c[0];
+                System.out.println("first = " + first);
+                tmp = "";
+                cou += 1;
+            }
+            catch (Exception e){
+                System.out.println("e = " + e);
+                System.out.println();
+            }
+        }
+
+
         return answer;
     }
-}
+
+    public static void main(String[] args) {
+        String c ="browoanoommnaon";
+
+//        System.out.println("solve    "+solution(c));
+
+        c ="zyelleyz";
+        solution(c);
+//
+//        c ="zyelleyz";
+//        c = c.substring(0, 0);
+//        System.out.println(c);
+//        for (int i = 1; i < 0; i++) {
+//        }
+        }
+    }
+
