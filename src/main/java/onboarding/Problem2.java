c2f53aeff1b40e697b3bff4b2274a4165c147e45
JJ <jeongjae1996@naver.com>
2022-10-28 00:24:37 +0900

    [Problem2] feat: 문자열의 연속하는 중복 문자를 삭제하는 기능을 구현했습니다.
    removeDuplicate 메서드를 통해 재귀적으로 문자열을 순회하며, 문자열이 비거나 더 이상 중복되는 문자가 없을 때까지 재귀적으로 실행됩니다. 재귀적으로 실행될 때마다 문자열 처음 부분부터 다시 순회하여, 중복되는 문자가 뒤에 몰려있을 경우 비효율적입니다. 이 부분을 index를 고정하는 등의 방식으로 리팩토링 예정입니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,49 @@
 package onboarding;
 
+/*
+문제 설명
+- 임의의 문자열의 연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 메서드를 완성해야 한다.
+*/
+
+/*
+기능 목록
+- 연속하는 중복 문자 삭제하는 기능 구현하기
+*/
+
 public class Problem2 {
+
+    public static String removeDuplicate(String str) {
+        // 문자열이 비었다면 빈 문자열 그대로를 반환한다.
+        if (str.isEmpty()) {return str;}
+
+        for (int i = 0; i < str.length(); i++) {
+            // 연속되는 중복 문자를 발견하면, 중복되는 부분을 제외하고 앞 부분의 문자열과 뒷 부분의 문자열을 concat해준다.
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                String front_str = str.substring(0, i);
+                // 중복된 부분이 문자열 제일 뒷부분일 수도 있으므로, 뒷 문자열을 빈 문자열로 일단 초기화해준다.
+                String back_str = "";
+
+                if (i + 2 < str.length()) {
+                    back_str = str.substring(i + 2);
+                }
+
+                str = front_str.concat(back_str);
+
+                break;
+            }
+            // 문자열 끝까지 순회했음에도 중복된 문자를 발견하지 못하면, 그 문자열을 반환해준다.
+            else if (i == str.length() - 1) {
+                return str;
+            }
+        }
+
+        // 연속되는 중복 문자를 다 제거할 때까지, 재귀적으로 찾아준다.
+        return removeDuplicate(str);
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        cryptogram = removeDuplicate(cryptogram);
+
+        return cryptogram;
     }
 }
