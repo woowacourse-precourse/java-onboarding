fd21e2edc15684d82825191f6c27450542725d2c
heesang <dlagmltkd12@naver.com>
2022-10-28 00:23:07 +0900

    feat(problem3): Problem3 클라이언트 코드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,9 @@
 package onboarding;
 
+import problem3.ThreeSixNineGame;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return ThreeSixNineGame.playGame(number);
     }
 }
