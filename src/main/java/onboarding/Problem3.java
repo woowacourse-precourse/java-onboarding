557ddda1365518486d97aee87d8478028b8b0dec
ouohoon <begreat@kakao.com>
2022-10-28 06:22:23 +0900

    3번 문제 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+    private static List<Integer> CLAP_NUMBERS = List.of(3, 6, 9);
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 0; i <= number; i++) {
+            answer += countClap(i);
+        }
         return answer;
     }
+
+    // 수의 각 자릿수를 검사해서 손뼉의 횟수를 리턴한다.
+    private static int countClap(int number) {
+        int result = 0;
+        while (number > 0) {
+            int n = number % 10;
+            if (CLAP_NUMBERS.contains(n)) {
+                result++;
+            }
+            number /= 10;
+        }
+        return result;
+    }
+
 }
