da66d04b4451ed3fc08c5844a2c080cb6016aed7
TaeMin <xoals3094@naver.com>
2022-10-28 06:25:22 +0900

    no.5 unit counting method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int rest_money;
     public static List<Integer> solution(int money) {
+        rest_money = money;
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+    public static int counting(int unit){
+        int count = rest_money / unit;
+        rest_money = rest_money % unit;
+        return count;
     }
 }
