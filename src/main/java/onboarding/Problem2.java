0c59f3160f730f3b215309bd8762e5bfdaae10f5
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-10-27 21:30:52 +0900

    solved problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+
+        boolean hasChanged = true;
+        while (hasChanged) {
+            System.out.println(answer);
+            StringBuilder sb = new StringBuilder();
+            int len = answer.length();
+            int cnt = 0;
+            hasChanged = false;
+
+            while (cnt < len-1) {
+                if (answer.charAt(cnt) == answer.charAt(cnt + 1)) {
+                    System.out.println(answer.charAt(cnt) + " 삭제");
+                    cnt += 2;
+                    hasChanged = true;
+                    continue;
+                }
+                sb.append(answer.charAt(cnt++));
+            }
+            if (cnt == len-1) sb.append(answer.charAt(cnt));
+            answer = sb.toString();
+        }
+
         return answer;
     }
+
 }
