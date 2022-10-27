053889340bebd4713300dd73c4d234abcbfa107b
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-28 08:49:46 +0900

    comment: add function list of problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * Problem 3 function list
+ * - Running game (search each number)
+ * - Count threefold nums of single number
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
