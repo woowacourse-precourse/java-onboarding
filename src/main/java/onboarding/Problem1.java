5bbefcd88eb1117433546a37bfc77f9d0dcce405
minhee020 <minhee020@naver.com>
2022-10-27 10:58:06 +0900

    최대값 추출 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,24 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private int extraction(List<Integer> pages){
+        int result;
+        int left = pages.get(0);
+        int right = pages.get(1);
+
+        // 더하기
+
+        // 곱하기
+
+        return result;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiScore = extraction(pobi);
+        int crongScore = extraction(crong);
+
+        answer = Math.max(pobiScore, crongScore);
         return answer;
     }
 }
