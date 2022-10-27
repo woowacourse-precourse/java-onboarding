8616710186bd65b01df637a8e590d0af44abf9a0
EUNJEONGMUN <munej26@gmail.com>
2022-10-28 00:44:21 +0900

    feat: 이메일과 이름을 key, value 값으로 저장하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
+    final static int EMAIL = 0;
+    final static int NAME = 1;
     public static List<String> solution(List<List<String>> forms) {
+        HashMap<String, String> contacts = new HashMap<>();
+        for (int i=0; i<forms.size(); i++) {
+            List<String> form = forms.get(i);
+            contacts.put(form.get(EMAIL),form.get(NAME));
+        }
+
         List<String> answer = List.of("answer");
         return answer;
     }
