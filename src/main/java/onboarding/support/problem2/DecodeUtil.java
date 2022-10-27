532d3940f239a380159517d24af6dfad0bb78562
kariskan <zkfzpf56@naver.com>
2022-10-27 21:20:04 +0900

    feat: 재귀를 통해 문자열의 중복을 제거하는 decode 함수 작성

--- src/main/java/onboarding/support/problem2/DecodeUtil.java
+++ src/main/java/onboarding/support/problem2/DecodeUtil.java
@@ -4,6 +4,25 @@ public class DecodeUtil {
 
     private static final int DUPLICATION_INTERVAL = 1;
 
+    public static String decode(String cryptogram) {
+        StringBuilder originalString = new StringBuilder(cryptogram);
+        StringBuilder decodedString = new StringBuilder(cryptogram);
+
+        for (int i = 0; i < originalString.length(); i++) {
+            int duplicatedIndex = i + 1;
+            duplicatedIndex = getDuplicatedIndex(originalString, i, duplicatedIndex);
+            if (deleteDuplication(decodedString, i, duplicatedIndex)) {
+                break;
+            }
+        }
+
+        if (isNotDecoded(originalString.toString(), decodedString.toString())) {
+            return originalString.toString();
+        }
+
+        return decode(originalString.toString());
+    }
+
     private static boolean deleteDuplication(StringBuilder decodedString, int i, int duplicatedIndex) {
         if (duplicatedIndex - i > DUPLICATION_INTERVAL) {
             decodedString.delete(i, duplicatedIndex);
@@ -17,5 +36,9 @@ public class DecodeUtil {
             duplicatedIndex++;
         }
         return duplicatedIndex;
+    }
+
+    private static boolean isNotDecoded(String originalString, String decodedString) {
+        return originalString.equals(decodedString);
     }
 }
