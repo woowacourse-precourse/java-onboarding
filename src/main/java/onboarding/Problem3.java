8c5b1c9fb52204d3f4ed678e404a6371b1ad135e
JinWoo Park <jinwuui@gmail.com>
2022-10-27 16:29:19 +0900

    feat: solve problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
+
         int answer = 0;
+
+        for (int now = 1; now <= number; now++) {
+            answer += getClapCount(now);
+        }
+
         return answer;
+    }
+
+    private static int getClapCount(int number) {
+
+        int count = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit % 3 == 0 && digit != 0) {
+                count++;
+            }
+            number /= 10;
+        }
+
+        return count;
     }
 }
