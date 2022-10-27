76cd191ddcb285f958ec26b5cad109205dee6566
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-27 17:03:40 +0900

    feat : 암호 길이 검증 기능 메소드 추가
    암호의 길이가 1 미만, 4000 초과면
    IlleagalArgumentException을 발생시키는 메소드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -18,4 +18,10 @@ public class Problem2 {
         String afterDecodeCryptogram = decodeString(beforeDecodeCryptogram);
         return beforeDecodeCryptogram.equals(afterDecodeCryptogram);
     }
+
+    public static void validateCryptogramLength(String cryptogram) {
+        if(cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            throw new IllegalArgumentException("암호는 길이 1 이상, 1000이하의 문자열입니다.");
+        }
+    }
 }
