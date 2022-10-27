e4c304dec512942a204b0d2f3ff36069ff0e481e
yeonhee <ydol2@naver.com>
2022-10-27 17:37:09 +0900

    [2번] feat : 연속된 문자를 제외하고 반환하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,23 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    /**
+     * 연속된 문자를 지워주는 함수
+     */
+    private static String delete_eq(String cryptogram){
+        int str_len = cryptogram.length();
+        char stand = ' ';
+        int stand_location;
+        String new_cryptogram = "";
+        for(int i=0;i<str_len-1;i++){
+            if (cryptogram.charAt(i)==cryptogram.charAt(i+1) || (stand == cryptogram.charAt(i) && stand_location == i)){
+                stand = cryptogram.charAt(i);
+                stand_location = i+1;
+            }else{
+                new_cryptogram=new_cryptogram + cryptogram.charAt(i);
+            }
+        }
+        return new_cryptogram;
+    }
 }
