bfe756d6ae77475bbc82351fcb12b76e7c98961d
KangSungBum <peter9706@naver.com>
2022-10-28 00:26:36 +0900

    feat: Problem5 기능 구현 완료
    convertIntoMoneyUnit 메소드 추가 : 입력받은 금액을 큰 단위부터 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        answer = convertIntoMoneyUnit(money);
+
         return answer;
+    }
+
+    private static List<Integer> convertIntoMoneyUnit(int money) {
+        List<Integer> moneyUnit = new ArrayList<>();
+
+        moneyUnit.add(money / 50000);
+        money %= 50000;
+        moneyUnit.add(money / 10000);
+        money %= 10000;
+        moneyUnit.add(money / 5000);
+        money %= 5000;
+        moneyUnit.add(money / 1000);
+        money %= 1000;
+        moneyUnit.add(money / 500);
+        money %= 500;
+        moneyUnit.add(money / 100);
+        money %= 100;
+        moneyUnit.add(money / 50);
+        money %= 50;
+        moneyUnit.add(money / 10);
+        money %= 10;
+        moneyUnit.add(money);
+
+        return moneyUnit;
     }
 }
