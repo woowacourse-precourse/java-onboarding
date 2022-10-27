127b29fc1eb44829ec78cd1a8cf64b77e8d56599
LEE-haeuk <rkwksmswhdk@naver.com>
2022-10-28 03:42:00 +0900

    프리코스 1주차 문제2 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = removeConsecutiveCharacters(cryptogram);
         return answer;
+    }
+
+    //연속된 소문자 처리 재귀적 메소드
+    public static String removeConsecutiveCharacters(String str){
+
+        //연속된 소문자 처리
+        String removedStr = str.replaceAll("([a-z])\\1+","");
+        if(str.length()==removedStr.length()) return str;
+        else return removeConsecutiveCharacters(removedStr);
+
     }
 }
