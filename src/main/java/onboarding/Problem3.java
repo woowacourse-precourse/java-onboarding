71ff7f89fba91b8563dabcc5b0234b29d4170408
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-27 19:17:00 +0900

    feat(Problem3): 1 ~ number까지 3, 6, 9가 포함되는 지 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 0; i <= number; i++) {
+            System.out.println(getCount369(i));
+        }
+
         return answer;
+    }
+
+    private static boolean getCount369(int number) {
+        int checkNumber;
+
+        while (number != 0) {
+            checkNumber = number % 10;
+
+            if (checkNumber == 3 || checkNumber == 6 || checkNumber == 9) {
+                return true;
+            }
+
+            number /= 10;
+        }
+
+        return false;
     }
 }
