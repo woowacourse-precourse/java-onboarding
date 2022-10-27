ea593b55c5958fa844f8ff8b7de08e7a1147e80e
chordpli <chordplaylist@gmail.com>
2022-10-27 22:10:15 +0900

    think(problem6) : 문제풀이 생각

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,16 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        // 닉네임 중 같은 글자가 연속적으로 포함 도리 경우 닉네임 사용 제한
+        // 같은 글자가 연속적으로 포함되는 닉네임 지원자의 이메일을 리턴.
+
+        // 반복문으로 전체 List 크기만큼 반복.
+        // 두글자씩 비교
+        // 탐색시간이 오래걸리지 않을까.
+
+
+
         return answer;
     }
 }
