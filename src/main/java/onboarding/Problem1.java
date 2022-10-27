ac92b9334c69ce33df00a8a77762880542c8c60c
UJ15 <dbwogml15@naver.com>
2022-10-27 09:51:22 +0900

    feat : pages validation check

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,25 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (isNotValidPages(pobi) || isNotValidPages(crong)) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private static boolean isNotValidPages(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        if (leftPage % 2 != 1 || rightPage % 2 != 0) {
+            return true;
+        }
+
+        if (rightPage - leftPage != 1 || rightPage > 400) {
+            return true;
+        }
+
+        return false;
     }
 }
