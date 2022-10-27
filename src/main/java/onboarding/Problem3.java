6058022214eaf7dba3b628765fa4877820245498
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-10-27 19:42:12 +0900

    feat: Problem 3, Add loop wrapping countClaps Function
    complete soulution of problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-
+        for(int i = 0; i <= number; i++){
+            answer += countClaps(i);
+        }
         return answer;
     }
 
