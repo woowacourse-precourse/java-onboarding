31e25d0bd219e078023f7541a1738c274c399400
kariskan <zkfzpf56@naver.com>
2022-10-27 15:31:36 +0900

    feat: 암호문 해독 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return getDecodedString(cryptogram);
+    }
+
+    public static String getDecodedString(String cryptogram) {
+        StringBuilder sb = new StringBuilder(cryptogram);
+
+        boolean isPresentDuplication = false;
+
+        for (int i = 0; i < sb.length(); i++) {
+            int duplicatedLastIndex = i;
+            while (duplicatedLastIndex < sb.length() && sb.charAt(duplicatedLastIndex) == sb.charAt(i)) {
+                duplicatedLastIndex++;
+            }
+
+            if (duplicatedLastIndex - i > 1) {
+                sb.delete(i, duplicatedLastIndex);
+                isPresentDuplication = true;
+            }
+        }
+
+        if (isPresentDuplication) return getDecodedString(sb.toString());
+        return sb.toString();
     }
 }
