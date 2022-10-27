55ef49edf5b8aea27c3afb738614463e1a762984
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-27 10:40:42 +0900

    [문제2] - 중복 문자의 시작과 종료 위치를 반환하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,21 @@
 package onboarding;
 
 public class Problem2 {
+    public static int[] findDupliIndex(StringBuilder s) {
+        int i, j;
+
+        for (i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1))
+                break;
+        }
+
+        j = i + 1;
+        while (j < s.length() && s.charAt(i) == s.charAt(j))
+            j++;
+
+        return new int[] { i, j };
+    }
+    
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
