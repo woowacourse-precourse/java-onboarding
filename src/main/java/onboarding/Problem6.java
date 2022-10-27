743bd5e6a8972707b5800bfc12fa11dc66e36c76
YE-EUN <musiclove529@naver.com>
2022-10-28 00:30:11 +0900

    feature : design problem6 structure - 문제 6 구조 잡기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        List<String> invalidNicknames = getInvalidNicknames(forms);
+        answer = getEmailsOfInvalidNickname(forms, invalidNicknames);
+
         return answer;
+    }
+
+    public static List<String> getInvalidNicknames(List<List<String>> forms){
+        List<String> invalidNickNames = new ArrayList<>();
+
+        return invalidNickNames;
+    }
+
+    public static Map<String, Integer> getStringsOfLength2Cnt(List<List<String>> forms){
+        Map<String, Integer> stringsOfLength2Cnt = new HashMap<>();
+
+        return stringsOfLength2Cnt;
+    }
+
+    public static List<String> getEmailsOfInvalidNickname(List<List<String>> forms, List<String> invalidNicknames){
+        List<String> emailsOfInvalidNickname = new ArrayList<>();
+
+        return emailsOfInvalidNickname;
     }
 }
