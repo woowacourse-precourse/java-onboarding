f48f8ff92adbfadb11e83649ea412241705fc6d5
MIN-04 <kimji_0424@naver.com>
2022-10-27 16:29:10 +0900

    add Problem3 solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i < number + 1; i++) {
+            answer += String.valueOf(i)
+                    .chars().filter(ch -> ch == '3' || ch == '6' || ch == '9')
+                    .count();
+        }
+
         return answer;
     }
 }
