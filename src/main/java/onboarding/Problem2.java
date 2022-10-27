096dc97dc42636b6901623a4d8acdd9f47bd6195
SH <gram753@naver.com>
2022-10-27 13:42:21 +0900

    style: reformat codes

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,23 +1,22 @@
 package onboarding;
 
-
 import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class Problem2 {
 
-    public static String solution(String cryptogram) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < cryptogram.length(); i++) {
+	public static String solution(String cryptogram) {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < cryptogram.length(); i++) {
 
-            if (!stack.isEmpty() && stack.peek() == cryptogram.charAt(i)) {
-                stack.pop();
-            } else {
-                stack.add(cryptogram.charAt(i));
-            }
-        }
+			if (!stack.isEmpty() && stack.peek() == cryptogram.charAt(i)) {
+				stack.pop();
+			} else {
+				stack.add(cryptogram.charAt(i));
+			}
+		}
 
-        return stack.stream().map(String::valueOf).collect(Collectors.joining());
-    }
+		return stack.stream().map(String::valueOf).collect(Collectors.joining());
+	}
 
 }
