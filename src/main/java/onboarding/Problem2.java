affc10f8e9021dfda10bb1453290bc27b740a9bf
gardenee <snnjjang@naver.com>
2022-10-27 14:21:34 +0900

    problem 2 first commit

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,39 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = delete(cryptogram);
+
         return answer;
+    }
+
+
+    public static String delete(String str) {
+        if (str.equals("")) {
+            return str;
+        }
+
+        int N = str.length();
+        String newStr = "";
+
+        char prev = str.charAt(0);
+        for (int i = 1; i < N; i++) {
+            char curr = str.charAt(i);
+            if (prev == curr) {
+                prev = ' ';
+            } else {
+                if (prev != ' ') {
+                    newStr += prev;
+                }
+                prev = curr;
+            }
+            if (i == N-1) {
+                newStr += prev;
+            }
+        }
+        if (N == newStr.length()) {
+            return newStr;
+        } else {
+            return delete(newStr);
+        }
     }
 }
