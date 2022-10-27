1b72c4e0144b35f2b2ae40134fa22ac257409240
squareyun <squareyun@gmail.com>
2022-10-27 19:29:35 +0900

    feat: 중복 문자 판별 함수

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
         return answer;
+    }
+
+    static boolean isOverlab(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.index(i) == cryptogram.index(i+1)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
