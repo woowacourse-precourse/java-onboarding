060361b2881a0b7a8f733a2fb6d19501b6d8224f
KimSangHyeok <tkdgur0906@naver.com>
2022-10-28 00:24:01 +0900

    Feat(PROBLEM3): 숫자에 3,6,9가 들어가는 개수 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+    public static int getNumCount369(int number){
+        int count = 0;
+        while(number > 0){
+            if(number % 10 == 3 || number % 10 == 6 || number % 10 == 9){
+                count++;
+            }
+            number = number / 10;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
