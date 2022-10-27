ff466a2c883d7b61974b6b674ddf30f62ee5a0c9
Heosejin98 <sejin3319@naver.com>
2022-10-28 01:05:03 +0900

    Feat : PROBLEM5 예외 처리 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,25 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    static final List<Integer> ERROR = Collections.emptyList();
+    static boolean Exception(int money) {
+        return (0 < money && money < 1000000) ? false : true;
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = List.of(1);
+
+        if (Exception(money))
+            return ERROR;
+
         return answer;
     }
+
+    public static void main(String argv[]) {
+
+        System.out.println(solution(0));
+        System.out.println(solution(1000001));
+        System.out.println(solution(100001));
+    }
+
 }
