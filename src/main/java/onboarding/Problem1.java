ae06fb84a11738a1e49be4b8067dcfddaf149b9f
JunginPark4364 <wjddlsee@gmail.com>
2022-10-28 02:32:26 +0900

    feat: check page if it is the first or last page
    - 첫 페이지 이거나 마지막 페이지인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,12 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    // 시작면 or 마지막면이 나오는 경우
+    public static boolean isFirstOrLast(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(1) == 400 || crong.get(1) == 400) {
+            return true;
+        }
+        return false;
+    }
 }
