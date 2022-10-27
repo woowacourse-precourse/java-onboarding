e9a78dab3867e2254a07bf9d229faf30ff9f1608
Jzakka <mouse4786@gmail.com>
2022-10-27 23:11:22 +0900

    Feat: "페이지의 최대값 기능 구현"

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import problem1.Calculator;
+import problem1.pageException;
+
 import java.util.List;
 
+import static java.lang.Math.max;
+
 class Problem1 {
+    private static final int SUM = 0;
+    private static final int MUL = 1;
+    private static Calculator calculator = new Calculator();
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int getMaxValue(List<Integer> pages) throws pageException {
+        checkException(pages.get(0), pages.get(1));
+
+        int maxVal = 0;
+        for (Integer integer : pages) {
+            maxVal = max(maxVal, max(calculator.digitCalculate(integer, SUM), calculator.digitCalculate(integer, MUL)));
+        }
+        return maxVal;
     }
 }
---
+++ src/main/java/problem1/Calculator.java
@@ -0,0 +1,13 @@
+package problem1;
+
+public class Calculator {
+    public int digitCalculate(Integer integer, final int operand) {
+        int result = operand; // SUM -> result = 0      MUL -> result = 1
+        while (integer > 0) {
+            int temp = integer % 10;
+            result = operand == 0 ? result + temp : result * temp;
+            integer /= 10;
+        }
+        return result;
+    }
+}
