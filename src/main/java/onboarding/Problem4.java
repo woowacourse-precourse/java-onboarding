c0c835443d182fb5b77354557cc43446fa0ce117
kokodak <kokodakadokok@gmail.com>
2022-10-27 16:23:16 +0900

    feat: [문제4] 알파벳인지 확인하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,14 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static int isAlphabet(char word) {
+        final int BIG_ALPHABET = 1;
+        final int SMALL_ALPHABET = 2;
+        final int NO_ALPHABET = 3;
+
+        if (word >= 'A' && word <= 'Z') return BIG_ALPHABET;
+        if (word >= 'a' && word <= 'z') return SMALL_ALPHABET;
+        return NO_ALPHABET;
+    }
 }
