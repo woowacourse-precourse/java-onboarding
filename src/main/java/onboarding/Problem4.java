88b6abf2bc86ec13465828405567e99a7305b15d
eunbii0213 <eunbii0213@naver.com>
2022-10-27 15:57:26 +0900

    feat: (Problem4) word를 한 글자씩 분리해서 list에 저장한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,13 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Problem4 {
     public static Map<Character, Character> upperCase;
     public static Map<Character, Character> lowerCase;
+    public static List<Character> motherWord;
+    public static final String UPPERCASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static final int indexStart = 0;
     public static final int indexEnd = 27;
     public static int upperCaseA = 65;
@@ -17,8 +19,23 @@ public class Problem4 {
         String answer = "";
 
         makeFrogDictionary();
+        reverseWord(word);
 
         return answer;
+    }
+
+    private static void reverseWord(String word) {
+        wordInList(word);
+        //isUpperCaseLetter();
+    }
+
+    private static void wordInList(String word) {
+        motherWord = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(word, UPPERCASE_ALPHABET + LOWERCASE_ALPHABET, true);
+
+        for (int index = indexStart; index < word.length(); index++) {
+            motherWord.add(st.nextToken().charAt(index));
+        }
     }
 
     public static void makeFrogDictionary() {
