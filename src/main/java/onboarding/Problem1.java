befcd32e21a1a88727cf3bf68d7c87fc13643ad5
lzlkolo <lzlko1209@gmail.com>
2022-10-27 10:21:47 +0900

    PROBLEM1.기능목록 및 예외사항 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int pageNumCheck(List<Integer> list) {
+        if(list.get(1)!=list.get(0)+1) {
+            return -1;
+        }
+        if(list.get(0)%2==0 || list.get(1)%2==1) {
+            return -1;
+        }
+        if(list.get(0)<=1 || list.get(1)>=400) {
+            return -1;
+        }
+        return 0;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
