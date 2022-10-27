a246387a05f0e899b975508dd991e3d88ab0a882
sangwon <swchoi1997@naver.com>
2022-10-27 23:18:58 +0900

    test : 테스트 케이스 추가

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class Problem2Test {
@@ -11,6 +12,43 @@ class Problem2Test {
 		assertThrows(IllegalArgumentException.class, () ->
 			Problem2.solution("")
 		);
+	}
 
+	@Test
+	void 테스트케이스_추가() {
+		String words = "hello";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("heo");
+		words = "aaaaaa";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("");
+		words = "browoanoommnaon";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("brown");
+		words = "aaabbbcccddd";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("");
+		words = "a";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("a");
+		words = "zyelleyz";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("");
+		words = "abcccddb";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("a");
+		words = "ababcdcdababcdcd";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("ababcdcdababcdcd");
+		words = "zyellezz";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("zy");
+		words = "zyelleyzz";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("");
+		words = "zzzzzzzzzzmmmmmz";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("z");
+		words = "ABAAAAABCBDDDDDDDABC";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("ACBABC");
+		words = "hellllleeeelo";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("helo");
+		words = "holellllleeeelo";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("h");
+		words = "aabbccddeeffggghhhiiiihhh";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("");
+		words = "DBAABDAB";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("AB");
+		words = "ABDAADBDAABB";
+		Assertions.assertThat(Problem2.solution(words)).isEqualTo("AD");
 	}
 }
