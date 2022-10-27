9a91c11f2a72c31ef7dea4a5de835a333ae1b342
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 12:28:23 +0900

    문제 1 : 기능 구현 사항 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ [기능 구현]
+ 1. 예외 사항
+ 2. 두 수를 비교하여 답을 내는 메소드
+ 3. 자릿수 별로 곱하고 더하는 메소드
+ **/
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
