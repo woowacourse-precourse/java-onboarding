55dea2164ecad4c162284df3e5c03372f976f372
ssssudan <kankgm0324@gmail.com>
2022-10-27 22:40:58 +0900

    problem1 요구사항 분석과 설계

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,10 +2,27 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 요구 사항
+ * 책을 임의로 펼친다.
+ * 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+ * 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+ * 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+ * 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+ * 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+ * 비와 크롱이 펼친 페이지가 들어있는 리스트/배열 pobi와 crong이 주어질 때,
+ * 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return 하도록 solution 메서드를 완성하라.
+ */
+
+/**
+ * 설계
+ * list 를 매개변수로 받아 가장 큰수를 return 하는 함수
+ * pobi 점수와 crong 점수를 매개변수로 받아 비교하여 최종 return 값을 결정하는 함수
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        System.out.println("commit test");
+        int answer = Integer.MAX_VALUE; // int 범위에서 최대값 2147483647
+
         return answer;
     }
 }
