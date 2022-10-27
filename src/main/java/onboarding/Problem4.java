e7bf2187151203571b13edd69c3f7f46903d6487
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 00:22:57 +0900

    feat : get ascii method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+
         String answer = "";
         return answer;
     }
+
+    private static int getAscii(Character character) {
+        int ascii = (int) character;
+        return ascii;
+    }
+
 }
