111efe0be3ebbc31432afaf804f4cabdd9cedc91
chanminmaxscaler <jcm04@naver.com>
2022-10-27 15:59:43 +0900

    feat(Problem1): checkException 과 그 하위 두 개의 메소드의 기능을 구현한다.
    checkException, checkPageNumberValidityException, checkOutOfRangeException 각각에 대한 기능을 구현한다.
    
    checkException은 두 메소드를 호출해 입력값의 예외를 확인하고 boolean 값을 반환한다.
    checkException이 호출하는 두 메소드는 아래와 같다.
    첫 번째 메소드 checkPageNumberValidityException은 책 페이지 번호의 타당성을 확인하고 boolean 값을 반환한다.
    두 번째 메소드 checkOutOfRangeException은 책 페이지 번호의 범위를 확인하고 boolean 값을 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,11 +3,21 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public boolean checkOutOfRangeException(List<Integer> list) {
+        return (list.get(0) > 1) && (list.get(1) < 400);
+    }
+
+   public boolean checkPageNumberValidityException(List<Integer> list) {
+       return (list.get(0) % 2 == 1) && (list.get(1) - list.get(0) == 1);
+   }
+
+    public boolean checkException(List<Integer> list) {
+        return checkOutOfRangeException(list) && checkPageNumberValidityException(list);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
-    }
-
-    public Object checkPageNumberValidityException(List<Integer> exceptionCase1) {
     }
 }
