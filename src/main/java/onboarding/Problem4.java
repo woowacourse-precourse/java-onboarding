09cec55e3f5a5ec9aa7bddb3dd2845ff27d27202
code-tree <dltjdqls1200@naver.com>
2022-10-27 15:23:43 +0900

    feat: Check for invalid input

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        if (word.length() < 1 || word.length() > 1000) {
+            return "false";
+        }
+
         return answer;
     }
 }
