0288571980489bc3704a60bb531b7201acf093ce
Amyhds <dasomhan524@gmail.com>
2022-10-27 12:12:36 +0900

    feat: Add clap function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    public static int clap(int n){
+        int cnt = 0;
+        for(int i = 1; i < n+1; i++){
+            int tmp = i;
+            while(tmp>0){
+                int x = tmp%10;
+                tmp /= 10;
+                if(x != 0 && x%3 == 0){
+                    cnt++;
+                }
+            }
+        }
+        return cnt
+    }
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return clap(number);
     }
 }
