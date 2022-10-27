29b0802ae032168e4bb053c4bfcb9c16642fadc3
baexxbin <soo3886@gmail.com>
2022-10-27 16:13:18 +0900

    Feat(PROBLEM2): "기능구현"
    solution함수의 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		Stack<String> stack = new Stack<>();
+		String[] cryptogramArray = cryptogram.split("");
+
+		for (String alphabet : cryptogramArray) {
+			if (!stack.isEmpty() && alphabet.equals(stack.peek())) {
+				stack.pop();
+				continue;
+			}
+			stack.push(alphabet);
+		}
+
+		return String.join("", stack);
+	}
 }
