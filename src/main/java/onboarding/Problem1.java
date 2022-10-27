8ac187b7357202440da1353d572b0bff5aa83089
chanwonlee <clw8679@naver.com>
2022-10-27 17:51:19 +0900

    FEAT: [Problem1] 입력받은 리스트 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,12 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    boolean check(List<Integer> list) {
+        //왼쪽 페이지 번호가 홀수 인지 확인
+        if(list.get(0)%2 == 0) return false;
+
+        //숫자가 연속적인지 확인
+        return (list.get(1) - list.get(0) == 1);
+    }
 }
