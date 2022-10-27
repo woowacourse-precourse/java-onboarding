63bcc376cc9409afef1e86b08acae56371643bfa
MinSungKim <timkim01@naver.com>
2022-10-27 20:33:49 +0900

    feat : Problem2 연속된 문자가 3개 이상인 경우 기능 추가구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,21 +8,30 @@ public class Problem2 {
         return answer;
     }
 
-    public static String decrypt(String cryptogram){
+    public static String decrypt(String cryptogram) {
         int length = cryptogram.length();
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < length; i++) {
             char ch = cryptogram.charAt(i);
-            if(!stack.isEmpty() && stack.peek() == ch){
+
+            if (!stack.isEmpty() && ch == stack.peek()) {
+                if (i < length - 1) {
+                    char nextCh = cryptogram.charAt(i + 1);
+
+                    if (ch == nextCh) {
+                        continue;
+                    }
+                }
                 stack.pop();
                 continue;
             }
+
             stack.add(ch);
         }
 
         StringBuilder sb = new StringBuilder();
-        for (Character ch : stack){
+        for (Character ch : stack) {
             sb.append(ch);
         }
 
