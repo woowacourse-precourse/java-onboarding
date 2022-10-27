5362a5f206b6d89a15982c562309952d46a64c8c
taeyun-work <wmfajr1357@gmail.com>
2022-10-27 22:37:17 +0900

    [Solved] Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -7,17 +8,21 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        getCountBill(answer, money);
+        answer = new ArrayList<>(8);
+        answer = getCountBill(answer, money);
         return answer;
     }
 
     public static List<Integer> getCountBill(List<Integer> answer, int money){
         List<Integer> billList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> res = answer;
         int tmp = money;
-        for(int bill : billList){
+        for(int bill : billList) {
             answer.add(tmp / bill);
             tmp %= bill;
         }
-        return answer;
+        return res;
     }
+
+
 }
