7819e69b3340903417b110cb3fe7a3e5191e507b
0112leesy <0112leesy@naver.com>
2022-10-27 22:31:39 +0900

    [Problem4] feat: change_word

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,19 @@ public class Problem4 {
         return answer;
     }
 
+    static char change_word(char input_character){
+        char changed_input = 0;
+        if(Character.isUpperCase(input_character)){
+            changed_input = 'Z';
+            changed_input -= (input_character - 'A');
+        }
+        else{
+            changed_input = 'z';
+            changed_input -= (input_character - 'a');
+        }
+        return changed_input;
+    }
+
     static boolean is_alphabet(char character){
         if(character >= 'A' && character <= 'Z') return true;
         if(character >= 'a' && character <= 'z') return true;
