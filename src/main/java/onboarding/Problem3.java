ce0539a42668668f6224148ba6f0659be4435e92
UJ15 <dbwogml15@naver.com>
2022-10-27 11:01:59 +0900

    feat : add clap count method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+    private static final List<Integer> clapNumbers = List.of(3, 6, 9);
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += getClapCount(i);
+        }
+
         return answer;
+    }
+
+    private static int getClapCount(int number) {
+        int clapCount = 0;
+
+        while (number > 0) {
+            if (clapNumbers.contains(number % 10)) {
+                clapCount++;
+            }
+            number /= 10;
+        }
+
+        return clapCount;
     }
 }
