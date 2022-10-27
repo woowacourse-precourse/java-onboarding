5afce9ac7da2fd84901eb29a4a79fecfcddff364
kwYoohae <gocksdl7979@gmail.com>
2022-10-27 13:41:52 +0900

    feat(Problem2.java): 중복 문자를 삭제하는 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import org.junit.platform.commons.util.StringUtils;
+import org.mockito.internal.util.StringUtil;
+
+import java.util.Arrays;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String duplicateRemove(String cryptogram, int index) {
+
+        String[] splitCryptogram = cryptogram.split("");
+        boolean isDuplicate = false;
+
+        for (int i = index + 1; i < splitCryptogram.length; i++) {
+            if (!splitCryptogram[i].equals(splitCryptogram[index])) {
+                break;
+            }
+            splitCryptogram[i] = "";
+            isDuplicate = true;
+        }
+
+        if (isDuplicate == true) {
+            splitCryptogram[index] = "";
+        }
+
+        return String.join("", splitCryptogram);
     }
 }
