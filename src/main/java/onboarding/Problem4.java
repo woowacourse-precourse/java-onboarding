a1e8ccdb8633efd027b03e0887817c618a6b8384
juhwang <juhong4930@naver.com>
2022-10-27 13:24:21 +0900

    feat(Problem4):creating reverse spell change

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,15 +9,25 @@ public class Problem4 {
     private static final int LOWERCASE_ASCII_START_NUMBER = 97;
     private static final int ALLCASE_SPELLING_TERM = 25;
     public static String solution(String word) {
-        String answer = SPELLING_CONVERT.toString();
+        String answer = "";
         return answer;
     }
-    public static HashMap<Character,Character> defineSpellingConvert(){
+
+    private static HashMap<Character,Character> defineSpellingConvert(){
         Map map = new HashMap<Character,Character>();
         for (int i = 0; i < ALLCASE_SPELLING_TERM+1; i++) {
             map.put((char)(UPPERCASE_ASCII_START_NUMBER+i),(char)(ALLCASE_SPELLING_TERM+UPPERCASE_ASCII_START_NUMBER-i));
             map.put((char)(LOWERCASE_ASCII_START_NUMBER+i),(char)(ALLCASE_SPELLING_TERM+LOWERCASE_ASCII_START_NUMBER-i));
         }
         return (HashMap<Character, Character>) map;
+    }
+    private static Character wordChange(Character wordCharacter){
+        return SPELLING_CONVERT.get(wordCharacter);
+    }
+    private static String wordLoop(char[] wordCharacters,String reverseWord){
+        for (char wordCharacter : wordCharacters) {
+            reverseWord += wordChange(wordCharacter);
+        }
+        return reverseWord;
     }
 }
