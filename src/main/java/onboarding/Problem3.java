0085e5befd6195f24132bed6dd72933919eeb7e5
minju412 <ln8847@ajou.ac.kr>
2022-10-27 10:11:57 +0900

    refactor: 리스트 제거 후 문자열 순회

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,28 +1,19 @@
 package onboarding;
 
-import java.util.ArrayList;
-
 public class Problem3 {
     public static int solution(int number) {
         int cnt = 0;
-        ArrayList<Character> charList = new ArrayList<Character>();
 
         for (int i=1; i<=number; i++) {
             String str = String.valueOf(i); // int -> string 변환
 
-            // str -> charList 변환
-            for (int j=0; j<str.length(); j++) {
-                charList.add(str.charAt(j));
-            }
-
             // 예를 들어, 36 이라는 숫자에는 박수 두 번
-            for (Character character : charList) {
-                if (character.equals('3') || character.equals('6') || character.equals('9')) {
+            for (int j=0; j<str.length(); j++) {
+                char c = str.charAt(j);
+                if (c == '3' || c == '6' || c == '9') {
                     cnt++;
                 }
             }
-
-            charList.clear();
         }
 
         return cnt;
