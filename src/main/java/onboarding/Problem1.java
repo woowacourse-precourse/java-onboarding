16c8b15fe9c370a40a4a42af6c6812c98a66fd4e
woosung1223 <dntjd991223@naver.com>
2022-10-27 20:29:20 +0900

    refactor(Problem1): validationCheck 함수 조건 함축, 의미 없는 상수 제거

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,11 +1,10 @@
 package onboarding;
 
 import java.util.List;
-import java.util.Arrays;
-import java.util.stream.Stream;
+
 class Problem1 {
-    private static final int START_PAGE_LIMIT = 1;
-    private static final int END_PAGE_LIMIT = 400;
+    private static final int LEFT_PAGE_LIMIT = 1;
+    private static final int RIGHT_PAGE_LIMIT = 400;
     private static final int PAGE_SIZE = 2;
     private static final int ONE_PAGE = 1;
     private static final char CHAR_ZERO = '0';
@@ -15,11 +14,10 @@ class Problem1 {
     private static final int EXCEPTION = -1;
     private static final int LEFT_PAGE = 0;
     private static final int RIGHT_PAGE = 1;
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
+
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (validationCheck(pobi, crong)) {
+        if (validationCheck(pobi) && validationCheck(crong)) {
             int pobi_max = getMaxValue(pobi);
             int crong_max = getMaxValue(crong);
             int winner = getWinner(pobi_max, crong_max);
@@ -27,27 +25,18 @@ class Problem1 {
         }
         return EXCEPTION;
     }
-    public static boolean validationCheck(List<Integer> user_1, List<Integer> user_2) {
-        int user1Left = user_1.get(LEFT_PAGE);
-        int user1Right = user_1.get(RIGHT_PAGE);
-        int user2Left = user_2.get(LEFT_PAGE);
-        int user2Right = user_2.get(RIGHT_PAGE);
-        int user1Size = user_1.size();
-        int user2Size = user_2.size();
+    public static boolean validationCheck(List<Integer> user) {
+        int leftPage = user.get(LEFT_PAGE);
+        int rightPage = user.get(RIGHT_PAGE);
+        int pageCount = user.size();
 
-        if (user_1.contains(START_PAGE_LIMIT) || user_1.contains(END_PAGE_LIMIT) ||
-                user_2.contains(START_PAGE_LIMIT) || user_2.contains(END_PAGE_LIMIT)) {
+        if (leftPage <= LEFT_PAGE_LIMIT || rightPage >= RIGHT_PAGE_LIMIT) {
             return false;
-        } else if (user1Left < START_PAGE_LIMIT || user1Right > END_PAGE_LIMIT ||
-                user2Left < START_PAGE_LIMIT || user2Right > END_PAGE_LIMIT) {
+        } else if (rightPage != leftPage + ONE_PAGE) {
             return false;
-        } else if (user1Right != user1Left + ONE_PAGE ||
-                user2Right != user2Left + ONE_PAGE) {
+        } else if (leftPage % PAGE_SIZE != 1) {
             return false;
-        } else if (user1Left % PAGE_SIZE != 1 ||
-                user2Left % PAGE_SIZE != 1) {
-            return false;
-        } else if (user1Size != PAGE_SIZE || user2Size != PAGE_SIZE) {
+        } else if (pageCount != PAGE_SIZE) {
             return false;
         }
         return true;
@@ -57,18 +46,18 @@ class Problem1 {
         String leftPage = Integer.toString(user.get(LEFT_PAGE));
         String rightPage = Integer.toString(user.get(RIGHT_PAGE));
 
-        int[] left = {ZERO, ONE};
-        int[] right = {ZERO, ONE};
-        for (int i = ZERO; i < leftPage.length(); i++) {
-            left[ZERO] += (leftPage.charAt(i) - CHAR_ZERO);
-            left[ONE] *= (leftPage.charAt(i) - CHAR_ZERO);
+        int[] left = {0, 1};
+        int[] right = {0, 1};
+        for (int i = 0; i < leftPage.length(); i++) {
+            left[0] += (leftPage.charAt(i) - CHAR_ZERO);
+            left[1] *= (leftPage.charAt(i) - CHAR_ZERO);
         }
-        for (int i = ZERO; i < rightPage.length(); i++) {
-            right[ZERO] += (rightPage.charAt(i) - CHAR_ZERO);
-            right[ONE] *= (rightPage.charAt(i) - CHAR_ZERO);
+        for (int i = 0; i < rightPage.length(); i++) {
+            right[0] += (rightPage.charAt(i) - CHAR_ZERO);
+            right[1] *= (rightPage.charAt(i) - CHAR_ZERO);
         }
-        int biggest = ZERO;
-        for (int i = ZERO; i < left.length; i++) {
+        int biggest = 0;
+        for (int i = 0; i < left.length; i++) {
             if (left[i] > biggest) {
                 biggest = left[i];
             }
@@ -77,6 +66,7 @@ class Problem1 {
             }
         }
         return biggest;
+
     }
     public static int getWinner(int user_1, int user_2) {
         if (user_1 > user_2) {
