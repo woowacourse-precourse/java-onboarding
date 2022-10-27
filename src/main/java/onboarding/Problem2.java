d75fd2275c57d47275d8b0776ecf180add27795d
splitube <voicesplit@daum.net>
2022-10-27 22:08:08 +0900

    2번 문제 -  Password_solver 클래스 기능 구현, 기능 추가
     * 1. save_char(char c) = 문자를 저장
     * 2. saved_char_exist() = 이전에 저장된 문자가 없는지 반환
     * 3. show_recent() = 최근에 저장된 문자를 조회하여 반환
     * 4. recent_has_same_char(char c) = 최근에 저장된 문자와 c가 같은 문자인지 비교하여 반환
     * 5. remove_recent() = 최근에 저장된 문자를 삭제
     * 6. get_recent() = 최근에 저장된 문자를 stack 에서 빼서 반환
     * 7. prepend_recent(StringBuilder sb) = sb의 제일 앞에 최근 저장된 문자를 넣는다.
     * 8. progress(char c) = 문자를 c를 받아서 암호해독과정 진행
     * 9. make_solved_to_string() = 해독이 끝난 암호를 문자열로 생성
     * 10. solve(String cryptogram) = 해독을 진행하고 해독이 끝난 암호를 문자열로 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,18 +1,90 @@
 package onboarding;
 
+import java.util.Stack;
+
 /**
  *
  * Password_solver
- * 1. save_character() = 문자를 스택에 저장
- * 2. is_prev_exist() = 중복인지 비교할 문자가 스택에 저장되어 있는지 반환
- * 3. is_duplicate() = 이전 문자와 현재 문자가 중복되는지 반환
- * 4. remove_duplicate() = 중복되는 이전 문자를 삭제
- * 5. show_solved() = 해독된 문자열을 리턴
+ * 1. save_char(char c) = 문자를 저장
+ * 2. saved_char_exist() = 이전에 저장된 문자가 없는지 반환
+ * 3. show_recent() = 최근에 저장된 문자를 조회하여 반환
+ * 4. recent_has_same_char(char c) = 최근에 저장된 문자와 c가 같은 문자인지 비교하여 반환
+ * 5. remove_recent() = 최근에 저장된 문자를 삭제
+ * 6. get_recent() = 최근에 저장된 문자를 stack 에서 빼서 반환
+ * 7. prepend_recent(StringBuilder sb) = sb의 제일 앞에 최근 저장된 문자를 넣는다.
+ * 8. progress(char c) = 문자를 c를 받아서 암호해독과정 진행
+ * 9. make_solved_to_string() = 해독이 끝난 암호를 문자열로 생성
+ * 10. solve(String cryptogram) = 해독을 진행하고 해독이 끝난 암호를 문자열로 반환
  *
  */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Password_solver ps = new Password_solver();
+        return ps.solve(cryptogram);
+    }
+}
+
+class Password_solver {
+    Stack<Character> stack;
+
+    Password_solver() {
+        stack = new Stack<>();
+    }
+
+    private void save_char(char c) {
+        stack.push(c);
+    }
+
+    private boolean saved_char_exist() {
+        return !stack.isEmpty();
+    }
+
+    private char show_recent() {
+        return stack.peek();
+    }
+
+    private boolean recent_has_same_char(char c) {
+        return show_recent() == c;
+    }
+
+    private void remove_recent() {
+        stack.pop();
+    }
+
+    private char get_recent() {
+        return stack.pop();
+    }
+
+    private void prepend_recent(StringBuilder sb) {
+        sb.insert(0, get_recent());
+    }
+
+    private void progress(char c) {
+        if (!saved_char_exist()) {
+            save_char(c);
+            return;
+        }
+        if (recent_has_same_char(c)) {
+            remove_recent();
+            return;
+        }
+        save_char(c);
+    }
+
+
+    public String make_solved_to_string() {
+        StringBuilder sb = new StringBuilder();
+        while (saved_char_exist()) {
+            prepend_recent(sb);
+        }
+        return sb.toString();
+    }
+
+    public String solve(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            progress(cryptogram.charAt(i));
+        }
+        return make_solved_to_string();
     }
 }
