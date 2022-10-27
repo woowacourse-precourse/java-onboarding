a82f32b69d816578b5f7ba945635e1d8e7668932
Eunkeee <eunkeeeee@gmail.com>
2022-10-27 16:42:15 +0900

    ✨feat: 예외상황 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,53 +1,63 @@
 package onboarding;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    public static <e> int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer = 0;
 
-        int[] pobiArr = pobi.stream().mapToInt(i->i).toArray();
-        int pobiMax = getMax(pobiArr);
+        try{
+            Exception e = new Exception("[왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있지 않음.");
+            int[] pobiArr = pobi.stream().mapToInt(i->i).toArray();
+            int[] crongArr = crong.stream().mapToInt(i->i).toArray();
 
-        int[] crongArr = crong.stream().mapToInt(i->i).toArray();
-        int crongMax = getMax(crongArr);
+            Boolean check1 = pobiArr[0] % 2 == 1 && pobiArr[1] % 2 == 0 && pobiArr[1] == pobiArr[0] + 1;
+            Boolean check2 = crongArr[0] % 2 == 1 && crongArr[1] % 2 == 0 && crongArr[1] == crongArr[0] + 1;
 
-        int answer = Integer.MAX_VALUE;
+            if(!check1 || !check2){
+                throw e;
+            }
 
-        if(pobiMax > crongMax){
-            answer = 1;
-            System.out.println("Pobi win");
-        }else if(pobiMax < crongMax){
-            answer = 2;
-            System.out.println("Crong win");
-        }else{
-            answer = 0;
-            System.out.println("무승부");
+            int pobiScore = getMax(pobiArr);
+            int crongScore = getMax(crongArr);
+
+            if(pobiScore > crongScore){
+                answer = 1;
+                System.out.println("Pobi win");
+            }else if(pobiScore < crongScore){
+                answer = 2;
+                System.out.println("Crong win");
+            }else{
+                answer = 0;
+                System.out.println("무승부");
+            }
+        } catch(Exception e) {
+            answer = -1;
         }
         return answer;
     }
 
-    private static int getMax(int[] pobiArr) {
-        int[] pobiResArr = new int[2];
-        for (int i = 0; i < pobiArr.length; i++){
-            int[] pobiNum = Stream.of(String.valueOf(pobiArr[i]).split("")).mapToInt(Integer::parseInt).toArray();
-            int pobiAdd = 0;
-            int pobiMult = 1;
-            for (int num: pobiNum){
-                pobiAdd += num;
-                pobiMult *= num;
+    private static int getMax(int[] arr) {
+        int[] resArr = new int[2];
+        for (int i = 0; i < arr.length; i++){
+            int[] numValue = Stream.of(String.valueOf(arr[i]).split("")).mapToInt(Integer::parseInt).toArray();
+            int addRes = 0;
+            int multRes = 1;
+            for (int num: numValue){
+                addRes += num;
+                multRes *= num;
             }
-            if(pobiAdd>pobiMult){
-                pobiResArr[i]=pobiAdd;
+            if(addRes>multRes){
+                resArr[i]=addRes;
             }else{
-                pobiResArr[i]=pobiMult;
+                resArr[i]=multRes;
             }
         }
-        int pobiMax = pobiResArr[0];
-        for(int i=1 ; i<pobiResArr.length ; i++) {
-            if(pobiResArr[i]>pobiMax) {
-                pobiMax = pobiResArr[i];
+        int pobiMax = resArr[0];
+        for(int i=1 ; i<resArr.length ; i++) {
+            if(resArr[i]>pobiMax) {
+                pobiMax = resArr[i];
             }
         }
         return pobiMax;
