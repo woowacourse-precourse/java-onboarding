ec3e0de9fbbe61aaebf009b9b537b8c824fefb5e
euncheol-kim <klmeuncheol@kakao.com>
2022-10-28 03:31:49 +0900

    Feat : PROBLEM1에 주어진 제한 사항과 숨겨진 제한 사항에 대한 코드를 구현했습니다.

--- src/main/java/service/problem1/Problem1Service.java
+++ src/main/java/service/problem1/Problem1Service.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class Problem1Service {
     // 승자 판별 메소드 -> player1 승리하면 1, player2 승리하면 2, 비기면 0, 예외상황인 경우 -1을 반환합니다.
     public Integer getWinner(List<Integer> player1Pages, List<Integer> player2pages){
-        return null;
+        // 제한 사항을 체크하고, 제한 사항에 걸린다면 -1을 리턴합니다.
+        if(isRestrict(player1Pages, player2pages)) return -1;
+        return isWinner(player1Pages, player2pages);
+
     }
 
     // Winner를 가립니다.
@@ -15,16 +18,36 @@ public class Problem1Service {
 
     // 모든 제한사항을 판별하는 메소드
     public boolean isRestrict(List<Integer> player1pages, List<Integer> player2pages){
-        return true;
+        boolean isRestrictPage = isRestrictPage(player1pages, player2pages);
+        boolean isWrongPageNumber = isWrongPageInputNumber(player1pages, player2pages);
+
+        return isRestrictPage || isWrongPageNumber;
     }
 
     // 나올수 없는 페이지 판별 메소드 -> [짝, 홀]인 경우, arr[0] + 1 == arr[1]이 아닌 경우 true를 반환합니다.
     public boolean isWrongPageInputNumber(List<Integer> player1pages, List<Integer> player2pages){
-        return true;
+        // page번호가 [홀, 짝]이 아닌 경우 true 반환
+        boolean test =  player1pages.get(0) % 2 != 1;
+        boolean test2 = player2pages.get(0) % 2 != 0;
+        if(player1pages.get(0) % 2 != 1 || player2pages.get(0) % 2 != 1) return true;
+        if(player1pages.get(1) % 2 != 0 || player2pages.get(1) % 2 != 0) return true;
+
+        // arr[0] + 1 == arr[1]이 아닌 경우 true 반환
+        if(player1pages.get(0) + 1 != player1pages.get(1)) return true;
+        if(player2pages.get(0) + 1 != player2pages.get(1)) return true;
+
+        return false;
     }
 
     // 제한된 페이지 판별 메소드 -> 음수 및 {1,2,399,400}페이지라면 true를 반환합니다.
     public boolean isRestrictPage(List<Integer> player1pages, List<Integer> player2pages){
+        for(int i = 0; i < player1pages.size(); i++){
+            boolean player1Page = (3 <= player1pages.get(i)) && (player1pages.get(i) <= 398);
+            boolean player2Page = (3 <= player2pages.get(i)) && (player2pages.get(i) <= 398);
+
+            if(!(player1Page && player2Page)) return true;
+        }
+
         return false;
     }
 }
