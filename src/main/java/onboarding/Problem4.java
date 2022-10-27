4d432e924c535df8bcdda2ad81ae6ce373967327
Namyunsuk <yunsuk0328@naver.com>
2022-10-27 17:18:05 +0900

    feat(validateString): 문자열의 상태 확인 및 예외처리 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,17 @@ class constant{
     static final String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static final String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
 }
-
+class validateString{
+    public static boolean isLower(String alphabet){
+        return alphabet.matches(alphabet.toLowerCase());
+    }
+    public static boolean isvalidLength(String word){
+        return (word.length()>=1)&&(word.length()<=1000);
+    }
+    public static boolean isAlphabet(String alphabet){
+        return alphabet.chars().allMatch(Character::isLetter);
+    }
+}
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
