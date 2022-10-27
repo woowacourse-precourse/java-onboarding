451788aac10c7dba476b1eb64f92ee498ec1efa5
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 14:43:52 +0900

    문제 5 : 풀이방법 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,9 @@ import java.util.*;
 
 /**
  * [풀이 방법]
- * 1. 지폐 배열 {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1} 을 선언한다.
- * 2. 주어진 money 에서 지폐 배열의 값을 순차대로 빼가며 answer 배열을 구성한다.
+ * 1. 초기 money_bill 50000원으로 설정,
+ * 2. money_bill /5 그리고 /2 가 번갈아 진행되기 때문에 이를 switch 해줄 boolean flag 설정,
+ * 3. 10원 -> 1원은 /10 이고 마지막 경우이기 때문에 따로 조건을 걸어줌
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
