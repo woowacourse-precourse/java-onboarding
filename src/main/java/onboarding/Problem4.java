2e78e3b7d18dd3578dba791eebfff34794e46c8a
Lee Sang Bin <rhkd2612@naver.com>
2022-10-27 18:42:44 +0900

    fix: remove transform case

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,28 +3,25 @@ package onboarding;
 /*
  * # 기능 정리
  * [O] 변환하는 문자인지 검증
- * [X] 대문자/소문자 변환 메서드
  * [O] 반대 문자 변환 메서드
- * [X] solution 작성
- * [X] 테스트 확인
+ * [O] solution 작성
+ * [O] 테스트 확인
  */
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuffer answer = new StringBuffer("");
 
         for(int i = 0; i < word.length(); i++){
             char currentLetter = word.charAt(i);
-            if(isLetter(currentLetter)) {
-                char transformChar = currentLetter;
-                transformChar = getOppositeLetter(currentLetter);
 
-                System.out.println(currentLetter);
-                System.out.println(transformChar);
-            }
+            if(isLetter(currentLetter))
+                currentLetter = getOppositeLetter(currentLetter);
+
+            answer.append(currentLetter);
         }
 
-        return answer;
+        return answer.toString();
     }
 
     private static boolean isLetter(char currentLetter){
