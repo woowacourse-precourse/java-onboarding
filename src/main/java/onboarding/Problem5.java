a08ca337018f25edc984e8f2eda3b022916f18af
hstla <ree7243@gmail.com>
2022-10-27 14:18:11 +0900

    문제5 feat: solution 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    static int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for (int won : unit) {
+            answer.add(money / won);
+            money = money % won;
+        }
         return answer;
     }
 }
