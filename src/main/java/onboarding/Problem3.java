86b2240b339eeb36a1ae1d26f2b8df30b75998c6
DongwookKim0823 <dwkim0823@naver.com>
2022-10-27 13:41:07 +0900

    feat: (Problem3) check if the condition is satisfied

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = checkEachValue(number);
+        return answer;
+    }
+
+    private static int checkEachValue(int number) {
+        for (int i = 1; i < number + 1; i++) {
+            isSatisfiedCondition(i);
+        }
+    }
+
+    private static int isSatisfiedCondition(int uncheckNumber) {
+
+        int answer = 0;
+
+        do{
+            int remainder = uncheckNumber % 10;
+            if(remainder == 3 || remainder == 6 || remainder == 9) answer += 1;
+            uncheckNumber /= 10;
+        } while (uncheckNumber != 0);
+
         return answer;
     }
 }
