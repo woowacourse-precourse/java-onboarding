dda16afc841479bafa93acc85ea6d8fbb376cf09
Kid-Chang <pch3310@gmail.com>
2022-10-28 03:44:23 +0900

    feat: remove psvm from problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,21 +3,6 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
-    public static void main(String[] args) {
-        String user = "mrko";
-        List<List<String>> friends = List.of(
-                List.of("donut", "andole"),
-                List.of("donut", "jun"),
-                List.of("donut", "mrko"),
-                List.of("shakevan", "andole"),
-                List.of("shakevan", "jun"),
-                List.of("shakevan", "mrko")
-        );
-        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
-        List<String> result = List.of("andole", "jun", "bedi");
-        System.out.println(solution(user, friends, visitors));
-        System.out.println(result);
-    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         List<String> nearFriends = new ArrayList<>();
