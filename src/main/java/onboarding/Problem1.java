f9f8ea6b9f2ac83dd5ed4fcddcbb77e18367baa1
Your Name <dbsdbxkrzz@naver.com>
2022-10-27 16:49:49 +0900

    feat: 메서드 기본 틀을 작성합니다.
    기능 목록의 요구사항에 맞추어 메서드의 기본 틀을 작성합니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,53 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+
+	public static boolean isInvalid(List<Integer> pobi, List<Integer> crong) {
+
+		return false;
+	}
+
+	public static boolean isNotInRange(List<Integer> pages) {
+
+		return false;
+	}
+
+	public static boolean isPageNumNotEven(int page) {
+
+		return false;
+	}
+
+	public static boolean isPageNumNotOdd(int page) {
+		return false;
+	}
+
+	public static boolean isInvalidPage(int leftPageNum, int rightPageNum) {
+		return false;
+	}
+
+	public static int calculateScore(List<Integer> pages) {
+		return 0;
+	}
+
+	public static int pageScoreAdd(int page) {
+		return 0;
+	}
+
+	public static int pageScoreMult(int page) {
+		return 0;
+	}
+
+	public static int findGreaterScore(int scoreAdd, int scoreMult) {
+		return 0;
+	}
+
+	public static int winnerCheck(int pobiScore, int crongScore) {
+
+		return 2;
+	}
 }
