ff39fffc24eb253ca4c234d6b2691bc2f5740a01
dclxxi <dclxxi@kakao.com>
2022-10-27 20:09:36 +0900

    feat(문제2): 문자열의 중복 문자를 삭제하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static Stack<Character> deduplication(String cryptogram) {
+        Stack<Character> characters = new Stack<>();
+        char[] chars = cryptogram.toCharArray();
+
+        char before = chars[0];
+        characters.push(before);
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            char now = chars[i];
+            if (before == now) {
+                characters.pop();
+            } else {
+                characters.push(now);
+            }
+
+            before = now;
+        }
+        
+        return characters;
     }
 }
