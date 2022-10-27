c3fe2d966fe25f4d21e498ae5118675558cd125a
zcae1020 <zcae1020@naver.com>
2022-10-28 00:18:34 +0900

    add: getMax method in Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,26 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    //리스트 안 숫자들의 getSum, getMultiple의 값들 중 최대값 구하기
+    private static int getMax(List<Integer> list){
+        int ret = 0;
+        int num;
+
+        for(int element : list) {
+            num = getSum(element);
+            if(ret < num)
+                ret = num;
+
+            num = getMultiple(element);
+            if(ret < num)
+                ret = num;
+        }
+
+        return ret;
     }
 
     //각 자리 숫자 더하기 값 구하기
