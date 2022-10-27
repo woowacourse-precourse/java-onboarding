ea7313c32c5d4215b801fa44db30bc7caf5c8218
alstjr706 <alstjr706@naver.com>
2022-10-28 02:30:11 +0900

    build : (369 게임) compare() 연산자 수정

--- src/main/java/onboarding/problem3/IterativeGenerator.java
+++ src/main/java/onboarding/problem3/IterativeGenerator.java
@@ -4,13 +4,13 @@ public class IterativeGenerator {
 
     public int typeConversion(int number) {
 
-        CheckNumbers checkNumbers = new CheckNumbers();
-
         int compare = 0;
+
+        CheckNumbers checkNumbers = new CheckNumbers();
 
         for (int numberIndex = 1; numberIndex <= number; numberIndex++) {
             String strNumber = String.valueOf(numberIndex);
-            compare = checkNumbers.compare(strNumber);
+            compare += checkNumbers.compare(strNumber);
         }
 
         return compare;
