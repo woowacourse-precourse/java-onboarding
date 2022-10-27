493936da7c5be3b947df0ff492d27eb302b9d344
yj2980 <gum0945@naver.com>
2022-10-27 20:18:22 +0900

    feat : 특정 사람과 친구인 사람 구하는 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,12 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    static final int firstFriend = 0;
+    static final int secondFriend = 1;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-
+        findUserFriends(user, friends);
         return answer;
+    }
+
+    //특정 사람과 친구인 사람 구하기
+    public static List<String> findUserFriends(String user, List<List<String>> friendsList) {
+        List<String> userFriendList = new ArrayList<>();
+        String friends = "";
+
+        for (int i = 0; i < friendsList.size(); i++) {
+            friends = friendsList.get(i).get(firstFriend);
+            if (user.equals(friendsList.get(i).get(firstFriend)))
+                userFriendList.add(friends);
+        }
+        return userFriendList;
     }
 }
