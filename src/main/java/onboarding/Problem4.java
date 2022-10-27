f1b6e761a48bf15e2374d3e0177a20f2a9503512
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-27 10:43:26 +0900

    docs(Problem4): 구현할 기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+// <기능 목록>
+// 1. 글자 변환하기
+//  a. 알파벳 외의 문자는 변환하지 않음
+//  b. 대문자와 소문자 구분
+//  c. 글자 변환
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
