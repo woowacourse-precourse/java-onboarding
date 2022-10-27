af893f685b76198fe2c89413b7d98ff0108f7c9d
splitube <voicesplit@daum.net>
2022-10-27 22:33:45 +0900

    3번 문제 - 기능 목록 완성
     Game369 객체
     * 1. how_many_target_exist(int num, int target) = num에 target이 몇개 존재하는지 반환
     * 2. get_369(int num) = num에 3,6,9가 몇개 존재하는지 반환
     * 3. get_total_count(int limit) = 1부터 limit까지 총 몇번의 박수를 쳐야하는지 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+
+/**
+ *
+ * Game369
+ * 1. how_many_target_exist(int num, int target) = num에 target이 몇개 존재하는지 반환
+ * 2. get_369(int num) = num에 3,6,9가 몇개 존재하는지 반환
+ * 3. get_total_count(int limit) = 1부터 limit까지 총 몇번의 박수를 쳐야하는지 반환
+ *
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
