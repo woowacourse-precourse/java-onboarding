19ff7863a9182e857d93a9ee93cc66453312dd0a
juhwang <juhong4930@naver.com>
2022-10-28 00:06:04 +0900

    feat(Problem6):to appear who will receive the alarm in boolean data

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,10 +6,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
-
+    private static boolean[] ALARM_SYSTEM = null;
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
+        fillAlarmSystem(forms.size());
         return answer;
+    }
+    private static void fillAlarmSystem(int formsSize){
+        ALARM_SYSTEM = new boolean[formsSize];
+        Arrays.fill(ALARM_SYSTEM,false);
     }
     private static List<List<String>> extractNickName(List<List<String>> forms){
         List<List<String>> nickNameList = new ArrayList<>();
