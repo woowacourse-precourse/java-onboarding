3d65a4903dc25661d6e767528baaf3fbd785bf70
dev-green-flamingo <devgflamingo@gmail.com>
2022-10-27 14:29:20 +0900

    feat: problem2 구현
    - 정규식으로 중복된 문자를 찾고 이를 빈 문자로 교체합니다.
       - 교체는 모든 문자가 아닌 중복된 문자 2개만 공백으로 교체합니다.
    - 빈 문자 교체는 정규식이 문자를 찾을 수 없을 때까지 수행합니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class Problem2 {
+    private static final Pattern pattern = Pattern.compile("([a-z])\\1");
+
+    private static String decrypt(String cryptogram) {
+        Matcher matcher = pattern.matcher(cryptogram);
+        while (matcher.find()) {
+            matcher.reset(matcher.replaceAll(""));
+        }
+        return matcher.replaceAll("");
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decrypt(cryptogram);
     }
 }
