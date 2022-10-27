6e6564509909309f452265064d05f04b455f24df
Heebeom Yang <takeny1998@gmail.com>
2022-10-27 15:43:35 +0900

    feat: solve problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,46 @@
 package onboarding;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Problem2 {
+    // 연속된 글자 중복을 제거하는 함수
+    // input : 문자열
+    // output: 연속이 제거된 문자열
+    private static String removeDuplicate(String str) {
+        String nextStr = "";
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length; i ++) {
+            char curt = chars[i];
+
+            // 맨 끝이 아닌 경우 다음 원소와 중복검사
+            if (i < (chars.length - 1))
+                if (curt == chars[i + 1])
+                    continue;
+
+            // 맨 앞이 아닌경우 이전 원소와 중복검사
+            if (i != 0)
+                if (curt == chars[i - 1])
+                    continue;
+
+            // 양 옆이 중복이 아닌경우 현재글자 추가
+            nextStr += curt;
+        }
+
+        return nextStr;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String str = cryptogram;
+
+        while (true) {
+            String nextStr = removeDuplicate(str);
+
+            if (str.length() == nextStr.length())
+                break;
+
+            str = nextStr;
+        }
+        return str;
     }
 }
