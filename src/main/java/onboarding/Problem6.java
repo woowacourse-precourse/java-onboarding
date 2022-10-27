ffa4d53a3492c362561b90393ad2a1c2022793de
aksmf1442 <aksmf1442@naver.com>
2022-10-27 14:10:35 +0900

    feat: Problem6 이메일, 닉네임 형식에 맞는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,41 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        findDuplicateNickname(forms);
         return answer;
+    }
+
+    private static void findDuplicateNickname(List<List<String>> forms) {
+        for (List<String> crewForm : forms) {
+            String email = crewForm.get(0);
+            String nickname = crewForm.get(1);
+            if (!validateEmail(email) || !validateNickname(nickname)) {
+                continue;
+            }
+        }
+    }
+
+    private static boolean validateEmail(String email) {
+        if (email.length() < 11 || email.length() >= 20) {
+            return false;
+        }
+
+        if (!email.contains("email.com")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean validateNickname(String nickname) {
+        if (!nickname.matches("^[ㄱ-ㅎ가-힣]*$")) {
+            return false;
+        }
+
+        if (nickname.length() < 1 || nickname.length() >= 20) {
+            return false;
+        }
+
+        return true;
     }
 }
