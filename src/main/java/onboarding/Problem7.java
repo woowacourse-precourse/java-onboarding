979d0421ec675dd738d247c7213814bec1bd39c7
juhwang <juhong4930@naver.com>
2022-10-28 07:17:59 +0900

    feat(Problem7):finding user's friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,20 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static List<String> filterFriendsOfUser(String user, List<List<String>> friends){
+        List<String> friendsOfUser = new ArrayList<>();
+        friends.forEach(relationTwoUsers -> {
+            if(relationTwoUsers.contains(user)){
+                friendsOfUser.add(relationTwoUsers.get(friendsIndex(user,relationTwoUsers)));
+            }
+        });
+        return friendsOfUser;
+    }
+    private static int friendsIndex(String user, List<String> relationTwoUsers) {
+        if(user.equals(relationTwoUsers.get(0))){
+            return 1;
+        }
+        return 0;
     }
 }
