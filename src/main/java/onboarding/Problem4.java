9e48b8a53019455db6719e10bca5c429c6c0fb19
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-27 12:11:12 +0900

    feat : 문제4-word를 청개구리 사전을 참고해 반대로 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        
+        String[] change = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"}; 
+        Map<String, String> match = new HashMap<>();
+        for (int i = 0; i < change.length; i++) {
+            match.put(Character.toString('A' + i), change[i]);  
+        }
+
+        String[] wordArr = word.split("");
+        for (String c : wordArr) {  
+            String reverse = match.get(c.toUpperCase());
+            if (reverse == null) {
+                reverse = " ";
+            }
+            if (!(Character.isUpperCase(c.charAt(0)))) {
+                reverse = reverse.toLowerCase();
+            }
+            answer += reverse;
+        }
         return answer;
     }
 }
