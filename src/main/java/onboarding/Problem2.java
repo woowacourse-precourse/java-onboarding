70ddbb9c553a9ebdeae6b9e6036167ac49d36ef7
JeonghoPark <pjhpjh0805@naver.com>
2022-10-27 18:37:07 +0900

    feat: 2번 stack에 입력과 동시에 중복 문자열 삭제 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        Stack<Character> stack = new Stack<>();
+        char lastDeleteAlphabet = ' ';
+        char[] alphabetArray = cryptogram.toCharArray();
+        for (char alphabet : alphabetArray) {
+            if (stack.isEmpty()) {
+                stack.push(alphabetArray[alphabet]);
+                continue;
+            }
+            char forefrontAlphabet = stack.peek();
+            if (alphabet == forefrontAlphabet) {
+                stack.pop();
+                lastDeleteAlphabet = alphabet;
+                continue;
+            }
+            if (alphabet == lastDeleteAlphabet) {
+                continue;
+            }
+            stack.push(alphabet);
+            lastDeleteAlphabet = ' ';
+        }
+
+
         return answer;
     }
 }
