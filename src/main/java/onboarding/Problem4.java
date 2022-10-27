5c8f02121b6c11707e3e3c2f433e63ff5b574865
whdcks420 <whdcks420@gmail.com>
2022-10-27 15:49:57 +0900

    feat: PROBLEM5 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.stream.Collectors;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return word.chars().map(Problem4::swapAlphabet).mapToObj(Character::toString).collect(Collectors.joining());
+    }
+
+    private static int swapAlphabet(int i) {
+        if (i > 96 && i < 123) {
+            return 219 - i;
+        }
+        if (i > 64 && i < 91) {
+            return 155 - i;
+        }
+        return i;
     }
 }
