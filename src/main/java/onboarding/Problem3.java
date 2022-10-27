8e275496a07632c05a424400090cdb00dfe63453
Jin409 <sualng123@gmail.com>
2022-10-28 02:28:52 +0900

    feat(problem3): 1부터 입력값까지 숫자의 범위 만드는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int[] numRange = getNumRange(number);
+        return 0; // 임시
+    }
+
+    private static int[] getNumRange(int number) {
+        return IntStream.rangeClosed(1, number).toArray();
     }
 }
