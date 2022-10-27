a22b8e38a457a6e53e28bb4ef690d514964acb87
bomin1996 <bomin199628@gmail.com>
2022-10-27 15:53:57 +0900

    문제4.알파벳이 아니면 그대로 문자에 추가하는 기능 추가, 문제 4번 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,12 +8,14 @@ public class Problem4 {
         for (int i =0;i<word.length();i++){
             char ascnum = word.charAt(i);
             if (ascnum>=65 && ascnum<=90){
-                answer = String.valueOf((char)(155-ascnum));
-            }//대문자에 속하면 반대되는 대문자로 변환
-            if (ascnum>=97 && ascnum<=122){
-                answer = String.valueOf((char)(219-ascnum));
-            }//소문자에속하면 반대되는 소문자로 변환
-            System.out.println("answer = " + answer);
+                answer = answer+String.valueOf((char)(155-ascnum));
+            }//대문자에 속하면 반대되는 대문자로 변환하여 추가
+            else if (ascnum>=97 && ascnum<=122){
+                answer = answer+String.valueOf((char)(219-ascnum));
+            }//소문자에속하면 반대되는 소문자로 변환하여 추가
+            else {
+                answer = answer+(char)ascnum;
+            }//알파벳이 아니면 그대로 문자 추가
         }
         return answer;
     }
