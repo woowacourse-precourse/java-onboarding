c85ad26c7f69672f0eef6c295fd45f2f69eceaef
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-27 19:12:12 +0900

    feat(Problem2): 앞, 뒤 문자와 비교 후 다르면 answer에 추가하기
    만약 현재 문자가 앞, 뒤 문자와 비교하였을 때 다르다면 answer 문자열에 추가한다.
    반복해 진행한 후 더 이상 중복 문자가 없다면 state를 false로 변경하여 반복문을 종료한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,7 +4,7 @@ public class Problem2 {
     static boolean state = true;
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
 
         while (state) {
             state = false;
@@ -22,16 +22,33 @@ public class Problem2 {
         boolean isDuplicate;
 
         for (int i = 0; i < cryptogram.length(); i++) {
+            char currentCharacter = cryptogram.charAt(i);
+            char frontCharacter;
+            char backCharacter;
 
             if (i == 0) {
-
+                backCharacter = cryptogram.charAt(i + 1);
+                isDuplicate = isDuplicateCharacter(currentCharacter, backCharacter);
             } else if (i == cryptogram.length() - 1) {
-
+                frontCharacter = cryptogram.charAt(i - 1);
+                isDuplicate = isDuplicateCharacter(currentCharacter, frontCharacter);
             } else {
+                frontCharacter = cryptogram.charAt(i - 1);
+                backCharacter = cryptogram.charAt(i + 1);
+                isDuplicate = isDuplicateCharacter(currentCharacter, frontCharacter) || isDuplicateCharacter(currentCharacter, backCharacter);
+            }
 
+            if (!isDuplicate) {
+                answer.append(cryptogram.charAt(i));
+            } else {
+                state = true;
             }
         }
 
         return answer.toString();
+    }
+
+    private static boolean isDuplicateCharacter(char character1, char character2) {
+        return character1 == character2;
     }
 }
