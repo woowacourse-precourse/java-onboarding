df5ae2f9da7fbb51e4c6f3030ff439d58c4e2cdd
phdljr <phdljr@naver.com>
2022-10-27 20:59:26 +0900

    Add Encrypted Alphabet arr.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem4 {
+    // A: 65, a: 97
+    static final char[] ENCRYPT_ALPHABET = new char[26];
     public static String solution(String word) {
-        String answer = "";
+        init();
+        String answer = encode(word);
         return answer;
     }
+
+    private static void init(){
+        for(int i=0;i<ENCRYPT_ALPHABET.length;i++){
+            ENCRYPT_ALPHABET[i] = (char)(90 - i);
+            System.out.println(ENCRYPT_ALPHABET[i]);
+        }
+    }
+
+    private static String encode(String word) {
+        return null;
+    }
+
+
 }
