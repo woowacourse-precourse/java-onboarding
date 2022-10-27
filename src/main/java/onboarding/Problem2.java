fcaf2d347c92d023edd095cee305bf731ce80804
WhalesBob <ndw8200@naver.com>
2022-10-27 20:27:59 +0900

    Complete to make Problem2(test complete)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder builder = new StringBuilder(cryptogram);
+
+        while(true){
+            int index = duplicateCharacterIndex(builder);
+            if(index == -1){
+                return builder.toString();
+            }else{
+                builder.delete(index-1,index+1);
+            }
+        }
+    }
+    static int duplicateCharacterIndex(StringBuilder str){
+        for(int i = 1; i < str.length(); i++){
+            if(str.charAt(i-1) == str.charAt(i)){
+                return i;
+            }
+        }
+        return -1;
     }
 }
