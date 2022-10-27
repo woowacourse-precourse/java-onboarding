e763c0a13aa50ef2b068b6d9ce26abf4ef90e784
2jun0 <soo28819@gmail.com>
2022-10-28 03:21:50 +0900

    Feat: 문제 7번 - 크루별 함께 아는 친구들의 수를 구하는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -37,6 +37,57 @@ public class Problem7 {
         return friendGraph;
     }
 
+    public static <T> HashMap<T, Integer> getCntOfTravelCaseBfs(Map<T, Set<T>> graph, T srtNode, int targetDist) {
+        // init cnfOfTravelCase
+        HashMap<T, Integer> cntOfTravelCase = new HashMap<>();
+        graph.keySet().forEach(node -> cntOfTravelCase.put(node, 0));
+
+        // 시작 노드부터의 거리
+        HashMap<T, Integer> dists = new HashMap<>();
+        graph.keySet().forEach(node -> dists.put(node, -1));
+        // node, visitedBitmask, depth
+        ArrayDeque<T> queue = new ArrayDeque<>();
+
+        // 최초 방문 노드
+        cntOfTravelCase.replace(srtNode, 1);
+        dists.put(srtNode, 0);
+        queue.addLast(srtNode);
+
+        while (!queue.isEmpty()) {
+            T node = queue.poll();
+            int dist = dists.get(node);
+            int curCntOfTravelCase = cntOfTravelCase.get(node);
+
+            if (dist >= targetDist)
+                continue;
+
+            for (T nxtNode: graph.get(node)) {
+                int nxtCntOfTravelCase = cntOfTravelCase.get(nxtNode) + curCntOfTravelCase;
+                cntOfTravelCase.replace(nxtNode, nxtCntOfTravelCase);
+
+                if (dists.get(nxtNode).equals(-1)) {
+                    dists.replace(nxtNode, dist+1);
+                    queue.addLast(nxtNode);
+                }
+            }
+        }
+
+        // target
+        HashMap<T, Integer> cntOfTravelCaseToTargetDist = new HashMap<>();
+        cntOfTravelCase.forEach((node, cnt) -> {
+            if (dists.get(node) == targetDist)
+                cntOfTravelCaseToTargetDist.put(node, cnt);
+        });
+
+        return cntOfTravelCaseToTargetDist;
+    }
+
+    public static Map<String, Integer> getCntOfSameFriendsWithMeByCrew(Map<String, Set<String>> friendGraph, String me) {
+        // 아래는 크루별 방문 회수
+        Map<String, Integer> cntOfSameFriendsWithMeByCrew = getCntOfTravelCaseBfs(friendGraph, me, 2);
+        return cntOfSameFriendsWithMeByCrew;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
