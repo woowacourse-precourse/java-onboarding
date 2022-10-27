c2f452a334404c7dde296d56a6d4b6b3dad1955e
eunbii0213 <eunbii0213@naver.com>
2022-10-27 20:39:31 +0900

    feat: (Problem6) 중복 검사할 이름을 담을 변수를 선언한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,14 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    public static String targetName;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        targetName = "";
+
+        //checkSameName();
+        //makeResultAscendingOrder();
         return answer;
     }
 }
