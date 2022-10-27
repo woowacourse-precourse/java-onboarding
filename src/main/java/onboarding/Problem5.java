a19a6fcd843a9e836973d36ec9116d2c36e4b96a
LHS-11 <ghktjq1119@naver.com>
2022-10-27 22:25:10 +0900

    Problem5 : 1. 상수 -> enum으로 리팩토링, 3. enum에 맞게 메서드 구성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,38 +6,40 @@ import java.util.List;
 
 public class Problem5 {
 
-    // 1. 5만원, 만원, 5천원, 천원, 오백원, 백원, 오십원, 십원, 일원 동전 상수 생성
-    public static final int FIFTY_THOUSAND_WON=50000;
-    public static final int TEN_THOUSAND_WON=10000;
-    public static final int FIVE_THOUSAND_WON=5000;
-    public static final int ONE_THOUSAND_WON=1000;
-    public static final int FIVE_HUNDRED_WON=500;
-    public static final int ONE_HUNDRED_WON=100;
-    public static final int FIFTY_WON=50;
-    public static final int TEN_WON=10;
-    public static final int ONE_WON=1;
+    // 1. 5만원, 만원, 5천원, 천원, 오백원, 백원, 오십원, 십원, 일원 동전 enum 으로 리팩토링
+    public enum Won{
+        FIFTY_THOUSAND_WON(50000),
+        TEN_THOUSAND_WON(10000),
+        FIVE_THOUSAND_WON(5000),
+        ONE_THOUSAND_WON(1000),
+        FIVE_HUNDRED_WON(500),
+        ONE_HUNDRED_WON(100),
+        FIFTY_WON(50),
+        TEN_WON(10),
+        ONE_WON(1);
+        final private int num;
+
+        private Won(int num){
+            this.num = num;
+        }
+    }
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = List.of(0,0,0,0,0,0,0,0,0);
 
         ArrayList<Integer> arrayList = new ArrayList<>(answer);
 
-
 //        3. 출금한 돈에서 지갑에 담은 돈 빼기 => 출금해야할 남아있는 돈 만들기
-        money -= FIFTY_THOUSAND_WON * getCoin(money, FIFTY_THOUSAND_WON, 0, arrayList);
-        money -= TEN_THOUSAND_WON * getCoin(money, TEN_THOUSAND_WON, 1, arrayList);
-        money -= FIVE_THOUSAND_WON * getCoin(money, FIVE_THOUSAND_WON, 2, arrayList);
-        money -= ONE_THOUSAND_WON * getCoin(money, ONE_THOUSAND_WON, 3, arrayList);
-        money -= FIVE_HUNDRED_WON * getCoin(money, FIVE_HUNDRED_WON, 4, arrayList);
-        money -= ONE_HUNDRED_WON * getCoin(money, ONE_HUNDRED_WON, 5, arrayList);
-        money -= FIFTY_WON * getCoin(money, FIFTY_WON, 6, arrayList);
-        money -= TEN_WON * getCoin(money, TEN_WON, 7, arrayList);
-        money -= ONE_WON * getCoin(money, ONE_WON, 8, arrayList);
+        int arrayIdx =0;
+        for (Won value : Won.values()) {
+            money -= value.num * getCoin(money, value.num, arrayIdx, arrayList);
+            arrayIdx++;
+        }
 
         return arrayList;
     }
 
-    //        2. 지갑에 최대한 담을 수 있는 갯수 구하기 (지갑이 최대한 가볍도록 큰 값의 지폐부터 담아야 함)
+//    2. 지갑에 최대한 담을 수 있는 갯수 구하기 (지갑이 최대한 가볍도록 큰 값의 지폐부터 담아야 함)
     private static int getCoin(int money, int won,int idx, ArrayList<Integer> arrayList) {
         int i=1000000/won;
         for(;i>=0;i--){
