5ce498019cd55e7869b5f08d007609fcce06a2d9
MIN-04 <kimji_0424@naver.com>
2022-10-27 17:22:49 +0900

    add Problem4 기능 1

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    /**
+     * 기능 목록
+     * 1. 청개구리 사전 배열 선언 (대문자용, 소문자용)
+     * 2. word를 한 글자씩 읽으며 소문자인지 대문자인지 판별
+     * 3. 대문자이면 대문자 청개구리 사전 참고, 소문자이면 소문자 청개구리 사전 참고 (index)
+     *      - 알파벳이 아닌 문자는 변환하지 않는다.
+     */
     public static String solution(String word) {
         String answer = "";
+
+        String dicUpper[] = {"Z", "Y", "X",	"W", "V", "U", "T", "S", "R", "Q", "P", "O", "N",
+                "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
+        String dicLower[] = {"z", "y", "x",	"w", "v", "u", "t", "s", "r", "q", "p", "o", "n",
+                "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"};
+
+
+
         return answer;
     }
 }
