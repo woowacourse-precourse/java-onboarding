de2ea5d1e1b649d54b2bae7ec9dacc13c5a628e7
abbd2 <ajkety00@gmail.com>
2022-10-28 00:32:49 +0900

    1번 인덱스부터 중복 검사 시작, 현재 인덱스와 한 칸 앞에 있는 인덱스 검사, 중복인 경우 제거, 인덱스 0번째와 1번째 제거 시 i를 1로 초기화, 아닌 경우에는 i를 -2 해줌으로써 제거한 인덱스 자리의 한 칸 앞에서 다시 검사 시작

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,7 +5,20 @@ public class Problem2 {
       StringBuilder sb = new StringBuilder(cryptogram);
 
       for(int i = 1; i < sb.length(); i++){
+          char fstWord = sb.charAt(i - 1);
+          char sndWord = sb.charAt(i);
+          if(fstWord == sndWord){
+              // 현재 인덱스의 문자와 한칸 뒤에있는 인덱스의 문자를 제거
+              sb.delete(i - 1, i + 1);
 
+              // 인덱스1(두번째 문자)에서 문자를 지운 경우에는 i를 1로 초기화
+              if(i <= 1){
+                  i = 1;
+              } else { // 문자를 지운 인덱스에서 2번 앞에있는 인덱스부터 다시 검색
+                  i = i - 2;
+              }
+
+          }
       }
       String answer = sb.toString();
         return answer;
