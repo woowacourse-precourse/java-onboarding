54efa153b595aa50c810653f3aae491648acce1e
elizabethgim <yujingim@kookmin.ac.kr>
2022-10-27 11:35:32 +0900

    Feat: Problem3 solution 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=0;i<number+1;i++){
+            int copy = i;
+            while(copy != 0){
+                if(copy % 10 == 3 || copy % 10 == 6 || copy % 10 == 9){
+                    answer++;
+                }
+                copy /= 10;
+            }
+        }
         return answer;
+    }
+
+    public static void main(String[] agrs){
+        System.out.println(solution(13));
+        System.out.println(solution(33));
+        System.out.println(solution(10000));
+        System.out.println(solution(1));
     }
 }
