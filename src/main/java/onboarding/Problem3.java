c18f374746bf7cfd56efb0491e45789a5637eea0
ycopp9 <aciees95@gmail.com>
2022-10-27 11:42:24 +0900

    feat: add the function getClapByDigit() that returns the total number of claps corresponding to the digits

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,9 +2,14 @@ package onboarding;
 
 public class Problem3 {
     private final static int[] MAX_CLAP_BY_DIGIT = {12000, 900, 60, 3, 0}; // a[n-1 - i] = 3 * (10 ^ (i-1)) + 10 * a[n-i - (i-1)]  (i >= 1)
+    private final static int[] MULTIPLES_OF_TEN_UNDER_I = {1000, 100, 10, 1, 0};
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int getClapByDigit(int i) {
+        return i * MAX_CLAP_BY_DIGIT[i] + getNumOfMulOfThreeLessThanValue(i) * MULTIPLES_OF_TEN_UNDER_I[i];
     }
 
     private static int getNumOfMulOfThreeLessThanValue(int value) {
