580874ad45bbb2c20eda7bcda883617e82565982
minhee020 <minhee020@naver.com>
2022-10-28 02:55:02 +0900

    Problem3 | 박수 세는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,8 +5,23 @@ package onboarding;
  * 1. 박수 세는 알고리즘
  */
 public class Problem3 {
+    private static int isClap(int number){
+        int tmp = 0;
+        int clapCount = 0;
+        while (number != 0){
+            tmp = number % 10;
+            if ((tmp % 3 == 0) && (tmp != 0)){ // tmp는 한 자리수이므로 3의 배수이기만 하면 박수를 치면 된다.
+                clapCount++;
+            }
+            number /= 10;
+        }
+        return clapCount;
+    }
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++){
+            answer += isClap(i);
+        }
         return answer;
     }
 }
