e4faafc9a2fcdf2928ab6cc0b6d5d1db30a58a04
whipbaek <jibaek0513@gmail.com>
2022-10-27 18:48:18 +0900

    P2 Add removeDuplication Method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
+    public static Stack<Character> removeDuplication(String cryptogram){
+        Stack<Character> st = new Stack<>();
+
+        for (int i=0; i<cryptogram.length(); i++) {
+            Character ch = cryptogram.charAt(i);
+
+            if(st.empty()) st.push(ch);
+            else{
+                if(st.peek().equals(ch)) st.pop();
+                else st.push(ch);
+            }
+        }
+
+        return st;
+    }
+
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        Stack<Character> st = removeDuplication(cryptogram);
+
+        return "";
     }
 }
