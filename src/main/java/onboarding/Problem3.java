07211115b18b44ca765f9c78f161a73d536b318d
iamjooon2 <iamjooon2@gmail.com>
2022-10-28 00:19:20 +0900

    feat: Problem3 - 문자열로 바뀐 숫자를 처음부터 끝까지 탐색하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,9 @@ public class Problem3 {
         int answer = 0;
         for (int i = 1; i <= number; i += 1) {
             sTemp = i + "";
+            for (int j = 0; j < sTemp.length(); j += 1) {
+
+            }
         }
         return answer;
     }
