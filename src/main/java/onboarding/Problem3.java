fe8eafef2d7378d022ba6047d311f5a46baf8fde
junhei Lee <jun2ee22@naver.com>
2022-10-27 18:20:30 +0900

    feat: 10으로 나누며 각 자리의 3,6,9 포함 개수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int count = 0;
+        int num = number;
+
+        while (number != 0) {
+
+            if (count != 0 && number / 10 == 0 && number % 3 == 0) {
+                //number가 한자리 수가 이니고 가장 높은 자리수가 3의 배수일 때
+                answer += (((number % 10) / 3) - 1) * Math.pow(10, count);
+                answer += num - (number * Math.pow(10, count)) + 1;
+            }else{
+                answer += ((number % 10 / 3)) * Math.pow(10, count);
+            }
+
+            answer += (number % 10) * 3 * Math.pow(10, count - 1) * count;
+            number/=10;
+            count++;
+        }
+
         return answer;
     }
 }
