4b14b7a429f1bff7fa435517bd869e6a27644d60
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-27 12:44:02 +0900

    refactor(Problem1.java): 점수 계산 로직을 findMaxSum()으로 분리, 결과값 final 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,65 +3,66 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int addAllNumbers(int page){
+    public static final int POBI_WIN = 1;
+    public static final int CRONG_WIN = 2;
+    public static final int DRAW = 0;
+
+    public static int addAllNumbers(int page) {
         return String.valueOf(page).chars()
                 .map(Character::getNumericValue)
                 .sum();
     }
 
-    public static int multiplyAllNumbers(int page){
-        int sum=1;
+    public static int multiplyAllNumbers(int page) {
+        int sum = 1;
 
-        for(char c:String.valueOf(page).toCharArray())
-            sum*=Character.getNumericValue(c);
+        for (char c : String.valueOf(page).toCharArray())
+            sum *= Character.getNumericValue(c);
 
         return sum;
     }
 
-    public static boolean validPages(List<Integer> pages){
+    public static boolean validPages(List<Integer> pages) {
         int left = pages.get(0);
         int right = pages.get(1);
 
-        if(left<1 || 400<left || right<1 || 400<right)
+        if (left < 1 || 400 < left || right < 1 || 400 < right)
             return false;
 
-        if(right!=left+1)
+        if (right != left + 1)
             return false;
 
-        if(left==1 || right==400)
+        if (left == 1 || right == 400)
             return false;
 
-        if(left%2==0 || right%2==1)
+        if (left % 2 == 0 || right % 2 == 1)
             return false;
 
         return true;
     }
 
+    public static int findMaxSum(List<Integer> pages) {
+        int left = pages.get(0);
+        int right = pages.get(1);
+
+        int leftMax = Math.max(addAllNumbers(left), multiplyAllNumbers(left));
+        int rightMax = Math.max(addAllNumbers(right), multiplyAllNumbers(right));
+
+        return Math.max(leftMax, rightMax);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(!validPages(pobi) || !validPages(crong))
+        if (!validPages(pobi) || !validPages(crong))
             return -1;
 
-        int pobiScore=0;
-        for(int i:pobi){
-            int addSum=addAllNumbers(i);
-            int mulSum=multiplyAllNumbers(i);
+        int pobiScore = findMaxSum(pobi);
+        int crongScore = findMaxSum(crong);
 
-            pobiScore = Math.max(pobiScore, Math.max(addSum, mulSum));
-        }
+        if (pobiScore > crongScore)
+            return POBI_WIN;
+        if (pobiScore < crongScore)
+            return CRONG_WIN;
 
-        int crongScore=0;
-        for(int i:crong){
-            int addSum=addAllNumbers(i);
-            int mulSum=multiplyAllNumbers(i);
-
-            crongScore = Math.max(crongScore, Math.max(addSum, mulSum));
-        }
-
-        if(pobiScore>crongScore)
-            return 1;
-        else if(pobiScore<crongScore)
-            return 2;
-        else
-            return 0;
+        return DRAW;
     }
 }
