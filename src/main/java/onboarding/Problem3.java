8e3a6cd3e6ef64423b353b721ce624ec81d0cc56
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-10-27 16:41:51 +0900

    refactor : 기능에 따른 메서드 분리
    - 범위 내의 문자 자릿수별로 쪼개기
    - 3/6/9 카운팅

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,13 +8,20 @@ public class Problem3 {
     public static int solution(int number) {
         List<Character> numList = new ArrayList<>();
         for (int i = 1; i < number + 1; i++) {
-            String numStr = String.valueOf(i);
-            char[] numArr = numStr.toCharArray();
+            char[] numArr = splitByDigits(i);
             for (char c : numArr) {
                 numList.add(c);
             }
         }
+        return calculateCount(numList);
+    }
 
+    private static char[] splitByDigits(int i) {
+        String numStr = String.valueOf(i);
+        return numStr.toCharArray();
+    }
+
+    private static int calculateCount(List<Character> numList) {
         int count = 0;
         count += Collections.frequency(numList, '3');
         count += Collections.frequency(numList, '6');
