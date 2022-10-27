b7eb44cb314fc7e2dbebb22280e6b6d668a77334
metorg <metrog950@gmail.com>
2022-10-27 15:53:16 +0900

    feat: [Problem2] 인덱스 예외처리 메서드 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,20 +2,21 @@ package onboarding;
 
 public class Problem2 {
 
-    public static StringBuilder sb = new StringBuilder();
+    public static StringBuilder sb;
 
     public static String solution(String cryptogram) {
-
         String answer = "answer";
-        sb.append(cryptogram);
+        sb= new StringBuilder(cryptogram);
+
         answer=sameLetterCheck();
 
         return answer;
     }
+
     static String sameLetterCheck(){
         for(int i=1;i<sb.length();i++){
             if(sb.charAt(i-1)==sb.charAt(i)){
-                deleteLetter(i);
+                i=deleteLetter(i);
             }
         }
         return sb.toString();
@@ -23,6 +24,11 @@ public class Problem2 {
 
     static int deleteLetter(int index){
         sb.delete(index-1,index+1);
-        return index;
+        return isException(index);
+    }
+
+    static int isException(int index){
+        if(index-2<0)return 0;
+        else return index-2;
     }
 }
