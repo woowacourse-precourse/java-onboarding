868744b2db2a5229f02367ba092ed31ceda4f62f
seheonCho <whtpgjs94@gmail.com>
2022-10-27 22:13:20 +0900

    feat: [문제1] 명시된 예외사항이 생길 수 있는지 체크하는 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
+
+    static final int LEFT_INDEX_IN_LIST = 0;
+    static final int RIGHT_INDEX_IN_LIST = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /**
+     * 결과로 -1 예외사항이 생길 수 있는지 체크하는 메서드
+     */
+    public static boolean isImpossiblePages(List<Integer> pages) {
+        if (pages.get(RIGHT_INDEX_IN_LIST) - pages.get(LEFT_INDEX_IN_LIST) == 1) {
+            return false;
+        }
+        return true;
+    }
+
 }
