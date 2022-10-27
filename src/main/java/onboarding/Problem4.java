79b704a378596cdbba3fbbfbf17682d875c37542
SEONMl <sm000117@naver.com>
2022-10-27 15:39:38 +0900

    feat: Problem4 solution 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -41,7 +41,7 @@ public class Problem4 {
         }
     }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Transformer transformer = new Transformer(word);
+        return transformer.transform();
     }
 }
