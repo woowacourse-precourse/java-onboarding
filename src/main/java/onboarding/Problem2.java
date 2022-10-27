41277c0fc76716fb598f85ef7d7e24e89251f5b3
MinjeongKong <kong1301@ajou.ac.kr>
2022-10-27 11:14:23 +0900

    Problem2 - initial commit

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ * [기능 요구사항 목록]
+ * 1. 중복 문자를 삭제한 결과를 저장하는 메서드 getMessage
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
