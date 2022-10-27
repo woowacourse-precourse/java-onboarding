8e8d285e1275542f50b1fe80dde9f8c2b75bfe65
Haechan Lee <dlgockssla@gmail.com>
2022-10-28 00:17:27 +0900

    prob4: 암호화 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+/**
+ * 구현 순서
+ * 1. word의 각 글자가 알파벳인지 확인
+ * 2. 알파벳을 반대로 변환
+ */
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = encrypt(word);
         return answer;
+    }
+
+    private static String encrypt(String plain) {
+        StringBuilder cypher = new StringBuilder();
+        for (int i = 0; i < plain.length(); i++) {
+            char c = plain.charAt(i);
+            if (c >= 'A' && c <= 'Z')
+                cypher.append((char) ('Z' - (c - 'A')));
+            else if (c >= 'a' && c <= 'z')
+                cypher.append((char) ('z' - (c - 'a')));
+            else
+                cypher.append(c);
+        }
+
+        return cypher.toString();
     }
 }
