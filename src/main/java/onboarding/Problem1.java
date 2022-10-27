ee1f7f7050c3cfb906ff18554556dd9b58ca3046
JunginPark4364 <wjddlsee@gmail.com>
2022-10-28 02:40:31 +0900

    feat: check page is consecutive
    - 연속된 페이지가 아닌지 확인하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,14 @@ class Problem1 {
         return answer;
     }
 
+    // 연속된 페이지가 아닌경우
+    public static boolean isNotConsecutive(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
+            return true;
+        }
+        return false;
+    }
+
     // 시작면 or 마지막면이 나오는 경우
     public static boolean isFirstOrLast(List<Integer> pobi, List<Integer> crong) {
         if (pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(1) == 400 || crong.get(1) == 400) {
