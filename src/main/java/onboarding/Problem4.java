46130b2c91ee540f6912e7ae6bb0c7f1c4790722
YooByeongIk <quddlr96@naver.com>
2022-10-27 17:19:23 +0900

    [Problem4] docs : 기능 목록 주석 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+import java.util.HashMap;
+/*
+##기능 목록
+
+1. 각 알파벳에 대응하는 값을 담은 HashMap 생성
+2. 해당 문자가 알파벳인지 확인
+3. String의 각 문자를 확인하며 변환 및 결과 생성
+
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
