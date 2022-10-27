a58448990650ff51ab29fc0eb9a59347b96c77b1
viyott <jackk0616@gmail.com>
2022-10-27 15:40:01 +0900

    p4 기능 목록 설정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+    /**
+     * 기능 목록
+     * 1. map에 알파벳과 그 역순을 저장
+     * 2. word를 한 글자씩 받아서 문자일 경우 대소문자를 구분하며 map으로 변환
+     */
     public static String solution(String word) {
         String answer = "";
         return answer;
