23f6bd2f6a7bbae9d04d25adec57e76155dcc140
code-tree <dltjdqls1200@naver.com>
2022-10-27 13:34:22 +0900

    feat: Check for invalid input

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        if (number < 1 || number > 10000) {
+            return -1;
+        }
+
+        
+
         return answer;
     }
 }
