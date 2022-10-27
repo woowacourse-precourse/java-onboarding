9c739d6cf68b5c74656f6c25317da9a63787bd33
jjmin9797 <jjmin9797@naver.com>
2022-10-27 19:26:59 +0900

    [Problem5][TEST][exchangeMoney]exchangeMoney 테스트 완료

--- src/test/java/onboarding/Problem5Test.java
+++ src/test/java/onboarding/Problem5Test.java
@@ -1,9 +1,18 @@
 package onboarding;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-@Disabled
+
+@DisplayName("[Problem5] 테스트")
 class Problem5Test {
+
+    @Test
+    @DisplayName("[Problem5][exchangeMoney] 화폐 갯수 구하기 테스트")
+    public void exchangeMoneyTest() {
+        assertEquals(100,Problem5.exchangeMoney(100,10001));
+    }
 
 }
