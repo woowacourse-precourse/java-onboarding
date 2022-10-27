cd7b90c1bdf9a159947451bacc0e6006624a8b7c
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-28 01:02:53 +0900

    feat(문제2): 입력받은 문자열 소문자로 구성되어 있는지 검증

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) throws Exception {
-        String answer = "answer";
-        answer = decode(cryptogram);
+        String answer = "";
+        List<Character> charList = new ArrayList<>();
+
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            throw new Exception("입력값의 길이가 올바르지 않습니다");
+        }
+        for(char each: cryptogram.toCharArray()) {
+            if(!Character.isLowerCase(each)) {
+                throw new Exception("소문자로만 구성되어 있지 않습니다");
+            }
+            charList.add(each);
+        }
+
+        answer = decode(charList);
         return answer;
     }
 
