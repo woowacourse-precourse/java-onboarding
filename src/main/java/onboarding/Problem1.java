ac1da9e032214933538dba53a780ca3d2f3bad8a
Jihoon9809 <dkdb521@naver.com>
2022-10-27 23:04:46 +0900

    first

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,38 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE; // 정수의 최대값.
+        int pobiSum=0, pobiMulti=1;
+        int crongSum=0, crongMulti =1;
+
+        if(pobi.get(0) != pobi.get(1)-1 || crong.get(0) != crong.get(1)-1) return -1;
+
+        for(int i =0;i<2;i++){
+            int pobiNow = pobi.get(i);
+            int crongNow = crong.get(i);
+
+            while(pobiNow !=0 && crongNow !=0) {
+                if (pobiNow != 0) {
+                    pobiSum += pobiNow % 10;
+                    pobiMulti *= pobiNow % 10;
+                    pobiNow = pobiNow / 10;
+                }
+
+                if (crongNow != 0) {
+                    crongSum += crongNow % 10;
+                    crongMulti *= crongNow % 10;
+                    crongNow = crongNow / 10;
+                }
+            }
+        }
+        if(Math.max(pobiSum,pobiMulti) > Math.max(crongSum,crongMulti)) answer = 1;
+        else if(Math.max(pobiSum,pobiMulti) < Math.max(crongSum,crongMulti)) answer = 2;
+        else answer = 0;
+
         return answer;
+        // 제발 올라가줘요 ㅠㅠ 선생님 제발요 ㅠㅠ
+        // 파일변경// 파일변경// 파일변경// 파일변경// 파일변경// 파일변경
     }
 }
