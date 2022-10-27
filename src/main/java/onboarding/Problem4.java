1ec16277cf726c3d14ddb2fac7fb1e649cdacf34
jini_11 <jieun21124@naver.com>
2022-10-27 13:43:08 +0900

    feat: 문제4 입력 예외처리 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        if (checkInput(word)) {
+            return "-1";
+        }
+
         return answer;
+    }
+
+    public static boolean checkInput(String word) {
+        if (word.length() < 1 || word.length() > 1000) {
+            return true;
+        }
+        return false;
     }
 }
