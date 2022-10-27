a5d3777c7ab71e8481b014c8e94d3fa229323090
EUNJEONGMUN <munej26@gmail.com>
2022-10-27 17:53:43 +0900

    feat: 문자열 중복 확인하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,15 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    private static boolean[] checkDuplicate(String cryptogram) {
+        boolean duplicates[] = new boolean[cryptogram.length()];
+
+        for (int i=0; i<cryptogram.length()-1; i++) {
+            if (cryptogram.charAt(i)==cryptogram.charAt(i+1)) {
+                duplicates[i] = true;
+                duplicates[i+1] = true;
+            }
+        }
+        return duplicates;
+    }
 }
