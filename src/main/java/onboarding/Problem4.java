97f9cff1b3d204fc7c62a8f959bd3314e3b17a41
EUNJEONGMUN <munej26@gmail.com>
2022-10-27 21:06:03 +0900

    feat: 반대 알파벳 구하기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
+    static final int UPPER_BASE = 155;
+    static final int LOWER_BASE = 219;
+    static final char START_LOWER = 'a';
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+    private static char convertAlphabet(char alphabet) {
+        if (alphabet == ' ') {
+            return alphabet;
+        }
+        if (alphabet-START_LOWER<0) {
+            return (char)(UPPER_BASE-alphabet);
+        }
+        return (char)(LOWER_BASE-alphabet);
     }
 }
