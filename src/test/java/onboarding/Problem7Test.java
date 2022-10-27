ebae58f98660e83890278d5c4b72b73416ebec7f
정현수 <wjdgustn15@naver.com>
2022-10-27 16:58:16 +0900

    test : add Timetest in Problem7Test
    * 시간 관련 테스트 추가

--- src/test/java/onboarding/Problem7Test.java
+++ src/test/java/onboarding/Problem7Test.java
@@ -2,8 +2,10 @@ package onboarding;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,4 +40,31 @@ class Problem7Test {
         List<String> result = List.of("abc");
         assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
     }
+    @Test
+    void maxValueTimeTest() {
+        long startTime = System.currentTimeMillis();
+        String user = "mrko";
+        List<List<String>> friends = new ArrayList<>(List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        ));
+        for (int i = 0; i < 995; i++) {
+            friends.add(new ArrayList<>(
+                    List.of((""+(char)i),""+(char)(996+i))));
+        }
+        List<String> visitors = Collections.emptyList();
+        List<String> result = List.of("andole", "jun");
+        assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+        long endTime =System.currentTimeMillis();
+        long time = (endTime-startTime)/1000;
+        assertThat(1L).isGreaterThan(time);
+
+
+    }
+
+
 }
