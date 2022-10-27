053f724e590ff747ef8ac9d41433d6887d025eba
viyott <jackk0616@gmail.com>
2022-10-27 13:47:40 +0900

    2 prev와 현재 글자가 같다면 temp 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,12 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-
+        String answer = "";
+        char prev = ' ';
         while(true) {
             String temp = cryptogram.substring(0, 1);
             for(int i = 1; i < cryptogram.length(); i++) {
+                // 2. 이전 글자와 현재 글자가 같다면 temp 뒤에 이미 들어간 문자도 잘라준다.
+                if (prev == cryptogram.charAt(i)) {
+                    temp = temp.substring(0, temp.length() - 1);
+                    continue;
+                }
+                // 1. for문을 돌면서 temp에 한 글자씩 붙일 껀데
                 temp += cryptogram.substring(i, i + 1);
+                prev = cryptogram.charAt(i);
             }
             cryptogram = temp;
             if()
