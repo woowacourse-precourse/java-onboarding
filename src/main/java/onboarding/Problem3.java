3558d826c82be6daedb47dcd6cce2541fa75942f
Park shinyeong <syngh503@gmail.com>
2022-10-27 17:14:11 +0900

    Problem3/Feat/각 자리수 구하기 기능 구현 각 자리수를 구하는 getEachDigit 함수를 구현하였습니다. 정수를 문자열로 바꾼 뒤, 다시 각 자리를 정수로 바꾸어 정수 배열에 추가하였습니다. Relate to: #3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+    public static int[] getEachDigit(int number) {
+        String strNum = Integer.toString(number);
+        int[] arrNum = new int[strNum.length()];
+        for (int i = 0; i < strNum.length(); i++) {
+            arrNum[i] = strNum.charAt(i) - '0';
+        }
+        return arrNum;
+    }
+
 }
