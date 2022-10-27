b0214a0d58e8aa0c84856aaf9fb444b8203a7af5
JJongmen <pjy90123@gmail.com>
2022-10-27 22:39:20 +0900

    feat(Problem1): 페이지의 범위 예외처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,15 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int EXCEPTION_CASE = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!isBoundary(pobi) || !isBoundary(crong)) return EXCEPTION_CASE;
         return answer;
+    }
+
+    private static boolean isBoundary(List<Integer> pages) {
+        return 3 <= pages.get(0) && pages.get(1) <= 398;
     }
 }
