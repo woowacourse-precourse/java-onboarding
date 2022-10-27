f49aaefb65e3764b660c5c2169c80e767cf62f9c
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 01:33:30 +0900

    feat: PROBLEM2 중복 문자 삭제 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,25 +4,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        Decoder decoder = new Decoder();
-        // return decoder.decode(cryptogram);
-        return null;
-    }
+	public static String solution(String cryptogram) {
+		Decoder decoder = new Decoder();
+		return decoder.decode(cryptogram);
+	}
 
-    static class Decoder {
+	static class Decoder {
 
-        private final static String REGEX = "(([a-z])\\2+)";
+		private final static String REGEX = "(([a-z])\\2+)";
 
-        private final Pattern pattern;
+		private final Pattern pattern;
 
-        Decoder() {
-            this.pattern = Pattern.compile(REGEX);
-        }
+		Decoder() {
+			this.pattern = Pattern.compile(REGEX);
+		}
 
-        private boolean hasDuplicates(String cryptogram) {
-            Matcher matcher = pattern.matcher(cryptogram);
-            return matcher.find();
-        }
-    }
+		public String decode(String cryptogram) {
+			while (hasDuplicates(cryptogram)) {
+				cryptogram = deleteDuplicates(cryptogram);
+			}
+			return cryptogram;
+		}
+
+		private boolean hasDuplicates(String cryptogram) {
+			Matcher matcher = pattern.matcher(cryptogram);
+			return matcher.find();
+		}
+
+		private String deleteDuplicates(String cryptogram) {
+			return cryptogram.replaceAll(REGEX, "");
+		}
+	}
 }
