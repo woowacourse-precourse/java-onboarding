02eeb8768275d99e34f07fde410a22183a85ae02
eunbii0213 <eunbii0213@naver.com>
2022-10-27 21:40:06 +0900

    feat: (Problem6) 타겟과 두 글자 이상 같은지 검사한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,16 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
-    public static String targetName;
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        targetName = "";
+        List<String> answer = new ArrayList<String>(findAnswer(forms));
 
-        //checkSameName();
-        //makeResultAscendingOrder();
         return answer;
+    }
+
+    public static Set<String> findAnswer(List<List<String>> forms) {
+        Set<String> answer = new HashSet<>();
+
+
+        for (int i = 0; i < forms.size() - 1; i++) {
+            String[] targetNameArr = forms.get(i).get(1).split("");
+            String compareArr = forms.get(i + 1).get(1);
+
+            findTwoSameLetter(forms, i, targetNameArr, compareArr, answer);
+        }
+        return answer;
+    }
+
+    public static void findTwoSameLetter(List<List<String>> forms, int i, String[] targetNameArr, String compareArr, Set<String> answer) {
+        for (int z = 0; z < targetNameArr.length - 1; z++) {
+            String targetTwoLetter = targetNameArr[z] + targetNameArr[z + 1];
+
+            if (compareArr.contains(targetTwoLetter)) {
+                break;
+            }
+        }
     }
 }
