aa626b7aabef2276aaae8602f983150a7791ec08
kokodak <kokodakadokok@gmail.com>
2022-10-27 14:00:18 +0900

    feat: [문제1] 페이지의 수를 이용한 최대 값 계산 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,30 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static int getMaxNumber(List<Integer> page) {
+        int leftPage = page.get(0);
+        int rightPage = page.get(1);
+        int leftPageMaxNumber = Math.max(getMaxNumberWithAdd(leftPage), getMaxNumberWithMultiply(leftPage));
+        int rightPageMaxNumber = Math.max(getMaxNumberWithAdd(rightPage), getMaxNumberWithMultiply(rightPage));
+        return Math.max(leftPageMaxNumber, rightPageMaxNumber);
+    }
+
+    private static int getMaxNumberWithAdd(int pageNumber) {
+        String tmp = Integer.toString(pageNumber);
+        int ret = 0;
+        for (int i = 0; i < tmp.length(); i++) {
+            ret += tmp.charAt(i) - '0';
+        }
+        return ret;
+    }
+
+    private static int getMaxNumberWithMultiply(int pageNumber) {
+        String tmp = Integer.toString(pageNumber);
+        int ret = 1;
+        for (int i = 0; i < tmp.length(); i++) {
+            ret *= tmp.charAt(i) - '0';
+        }
+        return ret;
+    }
 }
