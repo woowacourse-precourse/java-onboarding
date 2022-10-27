cc42eda826a8fe6d8fada675746dc8fddf0b6d3d
iamjooon2 <iamjooon2@gmail.com>
2022-10-28 00:51:15 +0900

    style: Problem3 - 변수명 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,15 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         String sTemp;
-        int result = 0;
+        int count = 0;
         for (int i = 1; i <= number; i += 1) {
             sTemp = i + "";
             for (int j = 0; j < sTemp.length(); j += 1) {
                 if (sTemp.charAt(j) == '3' || sTemp.charAt(j) == '6' || sTemp.charAt(j) == '9') {
-                    result += 1;
+                    count += 1;
                 }
             }
         }
-        return result;
+        return count;
     }
 }
