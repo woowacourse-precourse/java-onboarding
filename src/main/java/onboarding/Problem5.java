2dde689c155b2415c7ff26fa7fdcc42a55861d52
jini_11 <jieun21124@naver.com>
2022-10-27 14:36:55 +0900

    feat: 문제5 입력 예외처리 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,19 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        if (checkInput(money)) {
+            answer.add(-1);
+            return answer;
+        }
+
         return answer;
+    }
+
+    public static boolean checkInput(int money) {
+        if (money < 1 || money > 1000001) {
+            return true;
+        }
+        return false;
     }
 }
