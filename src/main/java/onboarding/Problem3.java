5dd45e00186818d82bffed335e071a6dd971593a
CHAYEONIL <jeay123@naver.com>
2022-10-27 18:31:53 +0900

    문제3 십의 자리수 3,6,9 판별 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 0; i < number + 1; i++) {
+            int ten_num = i / 10; // 십의 자리
+            int one_num = i % 10; // 일의 자리
+
+            // 십의 자리수 3,6,9 판별
+            if (ten_num == 3 || ten_num == 6 || ten_num == 9) {
+                answer += 1;
+            }
+        }
         return answer;
     }
 }
