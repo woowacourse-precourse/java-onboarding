7d2363fa0e5447576010411413fe909398a9fffa
cmong0516 <cmong0516@gmail.com>
2022-10-28 00:47:40 +0900

    2022.10.28 Problem all comment

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        // 요구사항 외의 값은 -1 을 반환.
         int answer = -1;
 
+        // pobi 와 crong 의 값을 체크.
         if (check(pobi) && check(crong)) {
+            // pobi 의 맥스값으로 int p 초기화
             int p = selectNum(pobi);
+            // crong 의 맥스값으로 int c 초기화
             int c = selectNum(crong);
 
             if (p > c) {
@@ -24,6 +28,8 @@ class Problem1 {
         return answer;
     }
 
+
+    // 왼쪽 페이지를 이용한 최대값과 오른쪽 페이지를 이용한 최대값을 비교하여 최대값을 반환.
     static int selectNum(List<Integer> x) {
         int result = 0;
 
@@ -35,6 +41,7 @@ class Problem1 {
         return result;
     }
 
+    // 모든수의 합과 곱 중 더 큰값을 반환.
     static int maxCase(int i) {
         String[] split = String.valueOf(i).split("");
         int sum = 0;
@@ -48,6 +55,7 @@ class Problem1 {
         return Math.max(sum, mul);
     }
 
+    // 입력된 페이지 값이 요구사항에 적합한 값인지 판별.
     static boolean check(List<Integer> x) {
         boolean answer = true;
         if (x.get(0) == 1 || x.get(0) == 399) {
