f20b398cc01e350ccd42f970a09c1edaada65d2d
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-27 10:37:17 +0900

    feat: Problem3 3,6,9 카운트해주는 메소드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,13 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    public static int count369(int number){
+        int count = 0;
+        while(number>0){
+            int tmp = number%10;
+            if(tmp == 3 || tmp ==6 || tmp==9)count++;
+            number/=10;
+        }
+        return count;
+    }
 }
---
+++ src/test/java/onboarding/Problem3Test.java
@@ -0,0 +1,12 @@
+package onboarding;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem3Test {
+    @Test
+    public void $NAME() throws Exception{
+        Problem3.solution(1);
+    }
+}
