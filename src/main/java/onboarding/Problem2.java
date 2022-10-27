3df806dfc0ba85e9b93891233b5f93f685c94f5e
limzzum <chfhddl1234@naver.com>
2022-10-27 15:04:22 +0900

    [문제 2] 중복 문자가 아닌 경우에만 StringBuilder sb 에 추가해준다
    중복되는 문자가 있으면 word 에 저장한다.
    그리고 처음에 현재 문자가 word와 같은 문자인지 비교하는 로직을 추가했다.
    중복문자가 아닌경우는 word를 다시 리셋해주고 answer에 추가해준다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,15 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        StringBuilder sb = new StringBuilder();
         cryptogram+= "?";
+        String word = "";
 
         for(int i=0; i<cryptogram.length(); i++){
+            if(String.valueOf(cryptogram.charAt(i)).equals(word))
+                continue;
             if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
-
+                word = String.valueOf(cryptogram.charAt(i));
             }else {
+                word ="";
+                sb.append(cryptogram.charAt(i));
             }
         }
-        String answer = "answer";
+        String answer = sb.toString();
         return answer;
     }
 }
