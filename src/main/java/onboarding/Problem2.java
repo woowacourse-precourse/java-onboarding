604ad697c1e6922984ac4d0135b4cab2cadf9128
ghkdgus29 <ghkdgus29@naver.com>
2022-10-27 15:33:08 +0900

    #2 중복문자가 있는지 찾고 중복문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String isDuplicate(String text) {
+        char fivot;
+        StringBuilder newText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            fivot = text.charAt(i);
+            Boolean duplicateFlag = false;
+            while (i < text.length() - 2 && fivot == text.charAt(i + 1)) {
+                i++;
+                duplicateFlag = true;
+            }
+            if (!duplicateFlag) {
+                newText.append(fivot);
+            }
+        }
+        return newText.toString();
     }
 }
---
+++ src/test/java/onboarding/Problem2Test.java
@@ -0,0 +1,16 @@
+package onboarding;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem2Test {
+
+    @Test
+    void isDuplicate() {
+        String result = Problem2.isDuplicate("brooorwnnk");
+
+        System.out.println("result = " + result);
+
+    }
+}
