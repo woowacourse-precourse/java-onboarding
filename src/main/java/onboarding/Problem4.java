1acd9e02fe69c8cfac3e30a2c831ce218c130f38
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-27 14:18:12 +0900

    feat:4번reverseString 대소문자 구분 후 변환하여 문자열에 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = reverseString(word, answer);
+        return answer;
+    }
+
+    private static String reverseString(String word, String answer) {
+        for(int i = 0; i< word.length(); i++){
+            char reverse = word.charAt(i);
+            if(reverse>='A' && reverse<='Z'){
+                answer +=(char)(('Z'-reverse) + 'A'); // 대문자 청개구리 사전 참고하여 ASCII 코드 활용
+            }
+            else if(reverse>='a' && reverse<='z'){
+                answer +=(char)(('z'-reverse) + 'a'); // 소문자 청개구리 사전 참고하여 ASCII 코드 활용
+            }
+
+        }
         return answer;
     }
 }
