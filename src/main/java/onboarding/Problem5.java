b887639aa66d07ca3d432d30206793b49e07ad82
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 14:33:50 +0900

    문제 5 : 풀이방법

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/**
+ * [풀이 방법]
+ * 1. 지폐 배열 {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1} 을 선언한다.
+ * 2. 주어진 money 에서 지폐 배열의 값을 순차대로 빼가며 answer 배열을 구성한다.
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
         return answer;
     }
 }
