e85ae1d72dd3388653414de92d64c796b7e84199
kariskan <zkfzpf56@naver.com>
2022-10-27 21:58:46 +0900

    feat: DictionaryConverter 클래스 및 멤버 변수 정의

---
+++ src/main/java/onboarding/support/problem4/DictionaryConverter.java
@@ -0,0 +1,14 @@
+package onboarding.support.problem4;
+
+public class DictionaryConverter {
+
+    private String convertedString;
+
+    public DictionaryConverter() {
+
+    }
+
+    public String getConvertedString() {
+        return convertedString;
+    }
+}
