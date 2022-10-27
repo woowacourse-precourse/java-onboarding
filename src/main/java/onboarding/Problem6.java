43c104190e636880fc567b036b9acf1914a70a4c
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-10-28 01:10:30 +0900

    문제 6 기능 1(각 멤버들의 이름을 연속된 두글자 씩 잘라서 map형태로 이메일과 함께 저장, 이미 있는 조합이라면 set에 이메일 추가 저장) 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Set<String> emails = new HashSet<>();
+        Map<String, String> combinationsOfNames = new HashMap<>();
+        for(int i = 0; i < forms.size(); ++i){
+            findDuplicatedNamePart(forms.get(i), emails, combinationsOfNames);
+        }
         return answer;
     }
+
+    public static void findDuplicatedNamePart(List<String> member, Set<String> emails,
+                                              Map<String, String> combinationsOfNames){
+        String email = member.get(0);
+        String name = member.get(1);
+        int nameLen = name.length();
+        if(nameLen == 1) return;
+        for(int i = 0; i < nameLen - 1; ++i){
+            String smallNamePart = String.valueOf(name.charAt(i) + name.charAt(i + 1));
+            if(combinationsOfNames.containsKey(smallNamePart)) {
+                emails.add(combinationsOfNames.get(smallNamePart));
+                emails.add(email);
+            } else if(!combinationsOfNames.containsKey(smallNamePart)) {
+                combinationsOfNames.put(smallNamePart, email);
+            }
+        }
+    }
+
 }
