126c61ad0b0fdb5f9957aaa12fe459971e4c139b
imsongj <imsongj@gmail.com>
2022-10-27 17:50:30 +0900

    feat(Problem3): 각 자리 숫자 계산 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,6 @@
 package onboarding;
 
 public class Problem3 {
-
     private static final int[] TARGET_NUMBERS = {3, 6, 9};
     private static final int MAX_DIGITS = 5;
     public static boolean isTargetNumber(int number){
@@ -12,17 +11,30 @@ public class Problem3 {
         }
         return false;
     }
+
     public static int countNumberOfClaps(int number){
         int count = 0;
-        int[] digits = new int[MAX_DIGITS];
-        //digits = getDigits(number);
-        for(int i = 0; i < MAX_DIGITS; i++){
+        int[] digits = getDigits(number);
+        for(int i = 0; i < digits.length; i++){
             if(isTargetNumber(digits[i])){
                 count++;
             }
         }
         return count;
     }
+
+    public static int[] getDigits(int number){
+        int[] digits = new int[MAX_DIGITS];
+        int position = MAX_DIGITS - 1;
+        while(position > 0){
+            int digit = number % 10;
+            digits[position] = digit;
+            position--;
+            number /= 10;
+        }
+        return digits;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         for(int i = 1; i <= number; i++){
