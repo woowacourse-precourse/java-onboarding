b958079358a1710fc70882a985b4941a20557271
apptie <appti@naver.com>
2022-10-27 11:09:00 +0900

    feat: 문제 3 박수를 쳐야 하는 숫자인지 확인하는 isClapped 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,8 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static boolean isClapped(int target) {
+        return target == 3 || target == 6 || target == 9;
+    }
 }
