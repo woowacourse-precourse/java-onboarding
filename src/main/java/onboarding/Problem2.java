b4ee03619aac056b80c431511f26a1036dbfbb78
splitube <voicesplit@daum.net>
2022-10-27 20:06:47 +0900

    2번 문제 - 기능 목록 완성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,15 @@
 package onboarding;
 
+/**
+ *
+ * Password_solver
+ * 1. save_character() = 문자를 스택에 저장
+ * 2. is_prev_exist() = 중복인지 비교할 문자가 스택에 저장되어 있는지 반환
+ * 3. is_duplicate() = 이전 문자와 현재 문자가 중복되는지 반환
+ * 4. remove_duplicate() = 중복되는 이전 문자를 삭제
+ * 5. show_solved() = 해독된 문자열을 리턴
+ *
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
