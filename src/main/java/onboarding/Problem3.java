43bf20af4b1e876ff8b8900ac3eec432fe3b4c04
csj <koreaxman@naver.com>
2022-10-27 13:51:27 +0900

    feat: Explore strings and find 3, 6, 9 to count

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,11 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int convertNumber;
+        return countThreeAndSixAndNine(number);
+    }
 
-        convertNumber = Integer.parseInt(convertCountingNumbersToString(number));
+    public static int countThreeAndSixAndNine(int number) {
+        int count = 0;
+        String convertString = convertCountingNumbersToString(number);
 
-        return convertNumber;
+        for (int i = 0; i < convertString.length(); i++) {
+            if (convertString.charAt(i) == '3'
+                    || convertString.charAt(i) == '6'
+                    || convertString.charAt(i) == '9') {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public static String convertCountingNumbersToString(int number) {
