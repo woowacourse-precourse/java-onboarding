d29c209522804748492f230b9c383c9209b7e6d2
JunH <tjdtls690@gmail.com>
2022-10-27 17:44:58 +0900

    :sparkles: feat(PROBLEM4) : 대문자 변환 전용 배열 초기화하기

--- src/main/java/onboarding/problem4/domain/AlphabetConverter.java
+++ src/main/java/onboarding/problem4/domain/AlphabetConverter.java
@@ -6,10 +6,17 @@ import java.util.stream.IntStream;
 
 public class AlphabetConverter {
     private static final List<Character> LOWERCASE_LETTERS = lowercaseLettersInit();
+    private static final List<Character> UPPERCASE_LETTERS = uppercaseLettersInit();
     
     private static List<Character> lowercaseLettersInit() {
         return IntStream.rangeClosed(0, 25)
                 .mapToObj(number -> (char) (122 - number))
+                .collect(Collectors.toList());
+    }
+    
+    private static List<Character> uppercaseLettersInit() {
+        return IntStream.rangeClosed(0, 25)
+                .mapToObj(number -> (char) (90 - number))
                 .collect(Collectors.toList());
     }
 }
