1e925f0e4790324c8272153e33a335fb3d574952
iamjooon2 <iamjooon2@gmail.com>
2022-10-27 19:04:22 +0900

    feat: Problem3 - 반복하는 숫자를 문자열로 변환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,9 +2,10 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        String sTemp;
         int answer = 0;
         for (int i = 1; i <= number; i += 1) {
-
+            sTemp = i + "";
         }
         return answer;
     }
