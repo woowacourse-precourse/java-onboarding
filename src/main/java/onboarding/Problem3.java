7376fe94bec7c35739aefabea1062ec80ad9bdfa
kjb95 <topmedia44@gmail.com>
2022-10-27 21:23:51 +0900

    feat: Problem3 - 해당 숫자에 손뼉을 몇번 치는지 계산하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+
+    private static final int THREE = 3;
+    private static final int SIX = 6;
+    private static final int NINE = 9;
 
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int computeClapCount(int num) {
+        List<Integer> only369 = List.of(THREE, SIX, NINE);
+
+        return (int) Arrays.stream(String.valueOf(num)
+                .split(""))
+            .filter(n -> only369.contains(n))
+            .count();
     }
 }
