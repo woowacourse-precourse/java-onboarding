8e9c5f8bd34b42fab62c4e64520a90f41adf6a54
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-27 16:13:21 +0900

    feat: 화폐 개수를 배열에 저장하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,19 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     private static final int[] MONEY_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
+        List<Integer> answer = new ArrayList<>();
         for (int u : MONEY_LIST) {
+            answer.add(exchangeMoney(money, u));
             money = divideMoney(money, u);
         }
-        List<Integer> answer = Collections.emptyList();
         return answer;
     }
 
     public static int divideMoney(int money, int unit) {
         return money % unit;
+    }
+
+    public static int exchangeMoney(int money, int unit) {
+        return money / unit;
     }
 }
