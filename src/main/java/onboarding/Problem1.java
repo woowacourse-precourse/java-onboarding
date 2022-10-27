7c159c6f30d49ac85e522a2658b746dc0b1df690
kokodak <kokodakadokok@gmail.com>
2022-10-27 14:23:15 +0900

    feat: [문제1] 페이지 검증 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,6 +12,7 @@ class Problem1 {
     private static int getMaxNumber(List<Integer> page) {
         int leftPage = page.get(0);
         int rightPage = page.get(1);
+        if (!validationCheck(leftPage, rightPage)) return -1;
         int leftPageMaxNumber = Math.max(getMaxNumberWithAdd(leftPage), getMaxNumberWithMultiply(leftPage));
         int rightPageMaxNumber = Math.max(getMaxNumberWithAdd(rightPage), getMaxNumberWithMultiply(rightPage));
         return Math.max(leftPageMaxNumber, rightPageMaxNumber);
@@ -36,8 +37,30 @@ class Problem1 {
     }
 
     private static int compareScore(int pobiScore, int crongScore) {
+        if (pobiScore == -1 || crongScore == -1) return -1;
         if (pobiScore > crongScore) return 1;
         else if (pobiScore < crongScore) return 2;
         else return 0;
+    }
+
+    private static boolean validationCheck(int leftPage, int rightPage) {
+        if (isOutOfBoundary(leftPage) || isOutOfBoundary(rightPage)) return false;
+        if (isLeftPageOdd(leftPage) && isRightPageEven(rightPage)) return true;
+        return false;
+    }
+
+    private static boolean isLeftPageOdd(int leftPage) {
+        if (leftPage % 2 == 0) return false;
+        return true;
+    }
+
+    private static boolean isRightPageEven(int rightPage) {
+        if (rightPage % 2 == 0) return true;
+        return false;
+    }
+
+    private static boolean isOutOfBoundary(int page) {
+        if (page < 1 || page > 400) return true;
+        return false;
     }
 }
