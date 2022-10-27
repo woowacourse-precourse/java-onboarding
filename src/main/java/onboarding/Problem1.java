84ce2bfe8dd77a29916f0c38d998e5d15107318e
DoooongDong <rhkswls98@kakao.com>
2022-10-27 23:38:23 +0900

    [Problem1] 예외 처리 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -26,9 +26,14 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         //양면의 페이지 수 차가 1이 아니면 예외 발생
-        if(Math.abs(pobi.get(0) - pobi.get(1)) != 1 || Math.abs(crong.get(0) - crong.get(1)) != 1) {
+        if(pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
             return -1;
         }
+        //1~400 페이지 이외에 번호가 들어오면 예외 발생
+        if(pobi.get(0) < 1 || pobi.get(1) > 400 || crong.get(0) < 1 || crong.get(1) > 400){
+            return -1;
+        }
+
         //시작 면이나 마지막 면이 나오면 예외 발생
         if(pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(0) == 399 || crong.get(0) == 399){
             return -1;
