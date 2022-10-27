cf2f121b65bf81cd2a7d44de566ec4136ed04c78
jiwon1217 <koagjw@gmail.com>
2022-10-28 02:52:29 +0900

    [FEATURE] Solve the Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char alphabet = word.charAt(i);
+            if (alphabet >= 'A' && alphabet <= 'Z') {
+                alphabet = (char) ('A' + 'Z' - alphabet);
+            } else if (alphabet >= 'a' && alphabet <= 'z') {
+                alphabet = (char) ('a' + 'z' - alphabet);
+            }
+            answer.append(alphabet);
+        }
+        return answer.toString();
     }
 }
