702db3635c48d8667c2ef24cb901eb4db95de9b8
ys200209 <lys200209@naver.com>
2022-10-27 15:54:16 +0900

    feat(문제3): 숫자에 3, 6, 9가 포함되는지 판별하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    private static boolean checkNumber(int number) {
+
+        if (number == 3 || number == 6 || number == 9) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
