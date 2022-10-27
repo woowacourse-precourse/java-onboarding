5f6c0262b6efb88afbe8dc2db4c35de1bd272340
junhei Lee <jun2ee22@naver.com>
2022-10-27 19:58:15 +0900

    feat: 알파벳 대문자 변환 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 public class Problem4 {
+
+    //대문자 변경
+    private static char changeUpper(char upper) {
+        int iUpper = (int) upper;
+        int iResult = 0;
+        iResult = 65 + (90 - iUpper);
+        return (char) iResult;
+    }
+    
     public static String solution(String word) {
         String answer = "";
         return answer;
