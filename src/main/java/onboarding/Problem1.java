5d21324e107edc09dd9b0ad03886dbb3ee678caf
wooseobee <dntjq0807@naver.com>
2022-10-28 00:57:37 +0900

    예외처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobi_left = pobi.get(0);
+        int pobi_right = pobi.get(1);
+
+        int crong_left = crong.get(0);
+        int crong_right = crong.get(1);
+
+        if (pobi_right - pobi_left != 1 || crong_right - crong_left != 1 || pobi_left <= 1 || pobi_right >= 400 || crong_left <= 1 || crong_right >= 400) {
+            return -1;
+        }
+
         return answer;
     }
 }
