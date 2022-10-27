a0d81fe24a45cfcadb38fa6cc25196b79f08a510
splitube <voicesplit@daum.net>
2022-10-27 18:36:13 +0900

    기능 목록 추가 (홀수 검증, 짝수 검증, 연속된 페이지 검증)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,20 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Game_starter
- * 1. open_book() = 책을 랜덤으로 펼친다.
- *
- * Validator
+ * Game_starter 클래스 ( XX )
+ * 1. open_book() = 책을 랜덤으로 펼친다. ( XX )
+ * <p>
+ * Validator 클래스
  * 1. is_proper_pages() = 알맞은 페이지 리스트가 들어왔는지 검증 ( 오류시 ERROR (-1) 반환 )
- *
- * Calculator
+ * 2. is_odd() = 홀수인지 검증
+ * 3. is_even() = 짝수인지 검증
+ * 4. is_continuous = 연속된 페이지 인지 검증
+ * <p>
+ * Calculator 클래스
  * 1. page_sum() = 현재 페이지의 모든 숫자를 더한 값 반환
  * 2. page_multiple() = 현재 페이지의 모든 숫자를 곱한 값 반환
  * 3. get_biggest() = 왼쪽 페이지의 합과 곱, 오른쪽 페이지의 합과 곱 중 제일 큰 값을 반환
- *
- * Judge
+ * <p>
+ * Judge 클래스
  * 1. show_result() = 결과를 반환한다 ( ERROR = -1, POBI = 1, CRONG = 2, DRAW = 0 )
  */
 class Problem1 {
