1a9d94ab29e90037199f204ddc999fb2822d2dbf
TaeMin <xoals3094@naver.com>
2022-10-28 04:01:52 +0900

    no.4 chung_frog_word translate method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,22 @@ public class Problem4 {
         return answer;
     }
 
+    public static String to_chung_flog_word(String word){
+        String chung_word = "";
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if (c == ' ') {
+                chung_word += ' ';
+                continue;
+            }
+            char chung_lang = chung_frog_lang(c);
+            if (chung_lang != 0)
+                chung_word += chung_lang;
+        }
+        System.out.println(chung_word);
+        return chung_word;
+    }
+
     public static char chung_frog_lang(char c){
         char chung_lang = 0; // 0이 리턴되면 예외값
         if(c >= 'a' && c <= 'z'){
