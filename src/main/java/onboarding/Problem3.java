6b382526eb180602d6fd51e8949cb343c53467a3
hstla <ree7243@gmail.com>
2022-10-27 10:28:09 +0900

    문제3 feat: 문자를 카운트하는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 public class Problem3 {
+    public static int countNum (int i, char findNum) {
+        int count = 0;
+        String c = String.valueOf(i);
+        for (char a : c.toCharArray()) {
+            if (a == findNum) count += 1;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
