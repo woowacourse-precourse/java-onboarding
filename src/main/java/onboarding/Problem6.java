47f4b17ddf8ece25680d2c1c32aad07cda4a9c9b
UJ15 <dbwogml15@naver.com>
2022-10-27 12:21:08 +0900

    feat : add create duplicate word dictionary for Problem6 with docs complete

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        final Map<String, List<String>> dictionary = initDictionary(new HashMap<>(), forms);
+
+        return Collections.emptyList();
+    }
+
+    private static Map<String, List<String>> initDictionary(
+            Map<String, List<String>> dictionary,
+            List<List<String>> forms
+    ) {
+        for (List<String> form : forms) {
+            for (int i = 0; i < form.get(1).length() - 1; i++) {
+                String word = form.get(1).substring(i, i + 2);
+
+                if (!dictionary.containsKey(word)) {
+                    dictionary.put(word, new ArrayList<>());
+                }
+
+                dictionary.get(word).add(form.get(0));
+            }
+        }
+
+        return dictionary;
     }
 }
