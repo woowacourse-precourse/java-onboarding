188d14a1cbaa2818274011d484b90394e9810a4b
조예람 <yrcho@joyelam-ui-MacBook-Pro.local>
2022-10-27 13:01:22 +0900

    Feat : decryption

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Deque<Character> dq = new LinkedList<>();
+        char[] arr = cryptogram.toCharArray();
+
+        for (int i = 0; i < arr.length;) {
+            if (dq.isEmpty() || dq.getLast()!=arr[i]) {
+                dq.add(arr[i]);
+                i++;
+                continue;
+            }
+            while (!dq.isEmpty() && dq.getLast()==arr[i]) {
+                dq.removeLast();
+                i++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!dq.isEmpty()) {
+            sb.append(dq.removeFirst());
+        }
+
+        return sb.toString();
     }
 }
