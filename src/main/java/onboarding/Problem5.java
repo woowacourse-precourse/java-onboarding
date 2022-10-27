9ea769850f7579bf669a376ab835315662ca1822
apptie <appti@naver.com>
2022-10-27 13:08:29 +0900

    feat: 문제 5 각 금액권이 몇 개 사용되었는지 계산하는 calculateMoneyCount 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import static onboarding.Problem5.MoneyCalculator.calculateMoneyCount;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return calculateMoneyCount(money);
+    }
+
+    public static class MoneyCalculator {
+
+        public static List<Integer> calculateMoneyCount(int money) {
+            int[] moneyTypes = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+            List<Integer> result = new ArrayList<>();
+
+            for (int i : moneyTypes) {
+                result.add(money / i);
+                money %= i;
+            }
+            return result;
+        }
     }
 }
