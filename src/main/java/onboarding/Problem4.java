69a69f46c8d9afe454b40e6719a24094c275a97a
juno-junho <ssmm0205@naver.com>
2022-10-27 23:02:15 +0900

    refactor(Problem4): refactor for clarity
    타입 에러를 줄이기 위해 알파벳을 상수로 선언 & 모호한 변수명 명확히 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,20 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-
-        StringBuilder sb = new StringBuilder(word.length());
+        StringBuilder convertedWord = new StringBuilder(word.length());
         for (char letter : word.toCharArray()) {
-            sb.append(convertWithDictionary(letter));
+            convertedWord.append(convertWithDictionary(letter));
         }
-        return sb.toString();
+        return convertedWord.toString();
     }
 
     static char convertWithDictionary(char letter) {
+        final char ALPHABET_UPPERCASE_START = 'A';
+        final char ALPHABET_UPPERCASE_END = 'Z';
+        final char ALPHABET_LOWERCASE_START = 'a';
+        final char ALPHABET_LOWERCASE_END = 'z';
+
         if (Character.isUpperCase(letter)) {
-            return (char) ('Z' - (letter - 'A'));
+            return (char) (ALPHABET_UPPERCASE_END - (letter - ALPHABET_UPPERCASE_START));
         }
         if (Character.isLowerCase(letter)) {
-            return (char) ('z' - (letter - 'a'));
+            return (char) (ALPHABET_LOWERCASE_END - (letter - ALPHABET_LOWERCASE_START));
         }
         return letter;
     }
