a15a19f8ca0c3143fea4bee05a25795dbc3199b2
junhei Lee <jun2ee22@naver.com>
2022-10-27 20:02:25 +0900

    feat: 대문자, 소문자, 그 외에 따라 변환 후 붙이기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -18,9 +18,23 @@ public class Problem4 {
         return (char) iResult;
     }
 
-    
+
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                //대문자면 대문자변환
+                answer += changeUpper(word.charAt(i));
+            } else if (Character.isLowerCase(word.charAt(i))) {
+                //소문자면 소문자변환
+                answer += changeLower(word.charAt(i));
+            } else {
+                //알파벳 외의 문자는 그대로 더하기
+                answer += word.charAt(i);
+            }
+        }
+
         return answer;
     }
 }
