595cc3b4939805fd3436ef622f89f1175c76260c
Eunkeee <eunkeeeee@gmail.com>
2022-10-27 23:55:46 +0900

    ✨[P2] feat: 중복 문자 삭제 (1 cycle)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,10 +14,30 @@ public class Problem2 {
             System.out.println(stringArr[i].equals(stringArr[i+1]));
 
             if(stringArr[i].equals(stringArr[i+1])){
-//                remove index
                 delIndexArr[i] += 1;
                 delIndexArr[i+1] += 1;}}
         System.out.println(Arrays.toString(delIndexArr));
+        // remove index from stringArr
+
+        // define the length of new string
+        int lossLen = 0;
+        for (int i =0; i<delIndexArr.length;i++){
+            if(delIndexArr[i]>0){
+                lossLen += 1;
+            }
+        }
+        System.out.println(lossLen); // 4
+        int newStringLen = stringArr.length - lossLen;
+        String[] newStringArr = new String[newStringLen];
+        int newStringIndex = 0;
+        for (int i =0; i<delIndexArr.length;i++){
+            if(delIndexArr[i]==0){
+                newStringArr[newStringIndex] = stringArr[i];
+                newStringIndex = newStringIndex + 1;
+            }}
+        System.out.println(Arrays.toString(newStringArr));
+
+
         String answer = "answer";
         return answer;
     }
