64e42eabd239115415ab87e7a607a4cb490120dd
ygreenb <nhj7911@gmail.com>
2022-10-28 04:49:46 +0900

    feat: Problem2 기능1 - 중복문자 인덱스 찾는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem2 {
+    private static final int NO_DUPLICATE = -1;
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static int findDuplicateIndex(String str){
+        for(int index=0; index<str.length()-1; index++){
+            if(str.charAt(index)==str.charAt(index+1)){
+                return index;
+            }
+        }
+        return NO_DUPLICATE;
     }
 }
