2b88b67c03481598b52ce14c359c5314d834444d
great-park <cksgh1735@naver.com>
2022-10-27 23:12:34 +0900

    feat(Problem2): 기본 구조 설계 및 기능 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
+    private static Stack<String> stack = new Stack<String>();
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        List<String> buffer = Arrays.asList(cryptogram.split("")); // ['b','r','w','p','p','w']
+        pushStack(buffer);
+        String answer = parseStack(stack);
         return answer;
     }
+
+
+    public static List<String> pushStack(List<String> buffer){
+        for(int i = 0; i < buffer.size(); i++){
+            push(buffer, i);
+        }
+        return buffer;
+    }
+
 }
