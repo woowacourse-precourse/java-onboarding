ec415281b3549aeda49654958e7e7c31d506f912
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 02:00:43 +0900

    [Problem2] 최종 결과값 출력

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -44,7 +44,7 @@ public class Problem2 {
     }
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeDuplicate(cryptogram);
         return answer;
     }
 }
