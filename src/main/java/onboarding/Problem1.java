175c3f0d650aa0b4c8e47ba7682f92f558db66fd
RumbleKAT <reki318@naver.com>
2022-10-27 22:58:39 +0900

    (feat) problem1 solved

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,62 @@
 package onboarding;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Calculator calculator = new Calculator();
+        int pobiNum = calculator.getMaxNumbers(pobi);
+        int crongNum = calculator.getMaxNumbers(crong);
+
+        if(pobiNum == -1 || crongNum == -1) return -1;
+        else if(pobiNum > crongNum) return 1;
+        else if(pobiNum < crongNum) return 2;
+        else return 0;
+    }
+
+}
+class Calculator {
+    public Calculator(){}
+    public boolean validateCheck(List<Integer> numArr){
+        return Math.abs(numArr.get(0) - numArr.get(1)) == 1;
+    }
+
+    public int getMaxNumbers(List<Integer> numArr){
+        if(!validateCheck(numArr)) return -1;
+        List<String> strArr = convertStrArr(numArr);
+        return Math.max(calcNums(strArr,"sum"), calcNums(strArr,"mul"));
+    }
+
+    public List<String> convertStrArr(List<Integer> numArr) {
+        return numArr.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> convertStrToNumArr(List<String> arr, int index){
+        List<Integer> list = new LinkedList<>();
+        for(char temp : arr.get(index).toCharArray()){
+            list.add(temp - '0');
+        }
+        return list;
+    }
+
+    public int sumNums(List<Integer> list){
+        return list.stream().reduce(0, Integer::sum);
+    }
+
+    public int mulNums(List<Integer> arr){
+        return arr.stream().reduce(1, (a, b) -> a * b);
+    }
+
+    public int calcNums(List<String> arr, String type) {
+        List<Integer> leftNum = convertStrToNumArr(arr,0);
+        List<Integer> rightNum = convertStrToNumArr(arr,1);
+        if(type.equals("sum")){
+            return Math.max(sumNums(leftNum),sumNums(rightNum));
+        }
+        return Math.max(mulNums(leftNum),mulNums(rightNum));
     }
 }
