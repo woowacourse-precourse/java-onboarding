fdc97c582c4d11935bf358f58f432598fc80ac54
bjk1649 <qhwns1649@naver.com>
2022-10-28 00:00:23 +0900

    feat: Problem3 숫자 하나에 3, 6, 9가 몇 번 들어가는지 찾아주는 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -11,4 +11,14 @@ class ThreeSixNine {
     private static boolean is369(int num){
         return (num == 3 || num == 6 || num == 9);
     }
+
+    private static int find369(int num){
+        int result = 0;
+        while (num!=0) {
+            result += is369(num % 10)? 1 : 0;
+            num /= 10;
+        }
+        return result;
+    }
+
 }
