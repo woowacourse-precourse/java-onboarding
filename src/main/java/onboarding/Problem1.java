5189388a707df447ea3896159440b0eedc0a4ddc
Hongsugyeong <olivia258@naver.com>
2022-10-27 19:28:30 +0900

    [problem1] 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /**
+     * 리스트의 문자열을 나눠서 배열에 저장하는 함수
+     * 배열에서 문자열을 더하거나 곱해서 나온 숫자 중 가장 큰 수를 뽑는 함수
+     * 가장 큰 수 끼리 비교하는 함수
+     * 예외사항
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
