0305d842fb6fe5bea888bfe61e4e70ce57deaff1
apptie <appti@naver.com>
2022-10-27 11:10:35 +0900

    feat: 문제 3 이미 박수의 횟수를 계산한 숫자인지 확인하는 isCached 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 public class Problem3 {
+    static int[] clapCountArr = new int[10001];
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
@@ -8,5 +10,9 @@ public class Problem3 {
 
     private static boolean isClapped(int target) {
         return target == 3 || target == 6 || target == 9;
+    }
+
+    private static boolean isCached(int target) {
+        return clapCountArr[target] > 0;
     }
 }
