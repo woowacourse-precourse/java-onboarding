8f49261a8bb07ad0eeaa4db123d28364742ba257
sonsoowon <yuwoo1018@gmail.com>
2022-10-27 13:27:24 +0900

    feat(P1): 점수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -26,6 +28,13 @@ class Problem1 {
         }
 
         return result;
+    }
+
+    static int getScore(List<Integer> pageNums){
+        // TODO: . 을 줄이는 방향으로 리팩토링
+        Stream<Integer> pageStream = pageNums.stream();
+        IntStream pageResult = pageStream.mapToInt(p -> Integer.max(addPageNum(p), mulPageNum(p)));
+        return pageResult.max().getAsInt();
     }
 
 }
