87a0836b9fcfe9754c9b6ec0d6d42a653ef7fdd2
sangil <gsl0505@naver.com>
2022-10-27 17:13:12 +0900

    feat: 점수 순으로 정렬하고 점수가 같을 시 이름순으로 정렬 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,6 +50,18 @@ public class Problem7 {
 		for (String visitor : visitors) {
 			userScore.put(visitor, userScore.get(visitor) + VISIT_SCORE);
 		}
+
+		List<Map.Entry<String, Integer>> recommandFriendList = new ArrayList<>(userScore.entrySet());
+		Collections.sort(recommandFriendList, new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				if (o1.getValue().equals(o2.getValue())) {
+					return o1.getKey().compareTo(o2.getKey());
+				}
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+
 		return new ArrayList<>();
 	}
 }
