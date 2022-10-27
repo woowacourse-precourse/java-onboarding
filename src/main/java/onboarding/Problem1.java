c0bb3adfdfaadb2e61d0fc38f7c03bbc8d776635
imsongj <imsongj@gmail.com>
2022-10-27 13:45:36 +0900

    feat(Problem1): 각 자리 숫자 덧셈 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,21 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int MIN_PAGE = 1;
+    private static final int MAX_PAGE = 400;
+    public static int addDigits(int num){
+        int sum = 0;
+        while(num > 0){
+            int digit = num % 10;
+            sum += digit;
+            num /= 10;
+        }
+        return sum;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 }
