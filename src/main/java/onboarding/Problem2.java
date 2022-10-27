24b9cb6dbbbc75a4c22e0f262eb04514cdf7ff81
Heosejin98 <sejin3319@naver.com>
2022-10-27 21:40:35 +0900

    Feat : PROBLEM2 연속 문자열 저장하는 함수 DuplicateStr 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 
+import java.util.Stack;
 
 public class Problem2 {
     static boolean Exception(String cryptogram) {
@@ -12,7 +13,6 @@ public class Problem2 {
         }
         return false;
     }
-
     static boolean CheckDuplicate(String str){
         char front_char;
         char back_char;
@@ -27,17 +27,36 @@ public class Problem2 {
         return  false;
     }
 
+    static Stack<String> DuplicateStr(String str){
+        String front_char;
+        String back_char;
+        Stack<String> result = new Stack<>();
+
+        for (int i = 0; i < str.length() - 1; i++){
+            front_char = "" + str.charAt(i);
+            back_char = "" + str.charAt(i + 1);
+
+            if (front_char.equals(back_char)) {
+                result.push(front_char + back_char);
+            }
+        }
+        return  result;
+    }
     public static String solution(String cryptogram) {
         String answer = "";
+        Stack<String> duplicate = new Stack<>();
 
         if (Exception(cryptogram))
             return answer;
+
+        while (CheckDuplicate(cryptogram)) {
+            duplicate = DuplicateStr(cryptogram);
+        }
 
         return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println(CheckDuplicate("asdwqezxc"));
-        System.out.println(CheckDuplicate("asdwqeqqbb"));
+
     }
 }
