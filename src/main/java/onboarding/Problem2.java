a0370ec32b82a6c1c88309e633b2eb5a735304b6
Yongwoo Choi <cyw7515@naver.com>
2022-10-27 22:16:09 +0900

    feat(problem2): 입력 stream 을 중복 문자가 제거된 스택으로 반환하는 함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> nonDuplicates = decryption(cryptogram);
+
         return answer;
     }
+
+    private static Stack<Character> decryption(String input) {
+        Stack<Character> result = new Stack<>();
+        int index = 0;
+
+        while (index < input.length()) {
+            if (result.isEmpty()) {
+                result.push(input.charAt(index));
+            }
+            else {
+                if (result.peek() == input.charAt(index)) {
+                    result.pop();
+                }
+                else {
+                    result.push(input.charAt(index));
+                }
+            }
+            index += 1;
+        }
+
+        return result;
+    }
+
+
 }
