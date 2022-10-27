5166e4f107a43e2dd441862c782115c51baee032
dev_kong <ahdjd5@gmail.com>
2022-10-27 16:51:54 +0900

    feat(Problem7): add exceptFriend method to except friend from visitors

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -8,11 +9,15 @@ public class Problem7 {
 
         Set<String> friendList = generateFriendList(friends);
         List<String> relatedUserList = generateRelatedUser(friends, user);
+        List<String> unknownVisitors = exceptFriend(visitors, friendList);
+//        Map<String,Integer> scoreMap = generateScoreMap();
+//        List<Map.Entry<String,Integer>> sortedScoreMap = sortScoreMap();
+//        answer = generateRecommedList();
 
         return answer;
     }
 
-    private static Set<String> generateFriendList(List<List<String>> friends){
+    private static Set<String> generateFriendList(List<List<String>> friends) {
         Set<String> friendList = new HashSet<>();
         for (List<String> friend : friends) {
             friendList.add(friend.get(0));
@@ -24,10 +29,23 @@ public class Problem7 {
         List<String> relatedUserList = new ArrayList<>();
         for (List<String> friend : friends) {
             String userName = friend.get(1);
-            if(!userName.equals(user)){
+            if (!userName.equals(user)) {
                 relatedUserList.add(userName);
             }
         }
         return relatedUserList;
     }
+
+    private static List<String> exceptFriend(List<String> visitors, Set<String> friendList) {
+        for (String friend : friendList) {
+            visitors = visitors.stream()
+                    .filter(v -> !v.equals(friend))
+                    .collect(Collectors.toList());
+        }
+        return visitors;
+    }
+
+//    private static Map<String,Integer> generateScoreMap(){
+//        
+//    }
 }
