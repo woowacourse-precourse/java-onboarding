0cf16a1c36ef61539006bec08fa3826c0ddedc16
drunkenhw <drunkenhw@gmail.com>
2022-10-28 02:07:47 +0900

    feat(Problem3) : 분리된 숫자가 3, 6, 9가 맞는지 확인한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,8 +8,22 @@ public class Problem3 {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
             List<Integer> digits = getDigitList(i);
+            answer += getClap(digits);
         }
         return answer;
+    }
+    private static int getClap(List<Integer> digits) {
+        int clap = 0;
+        for (Integer digit : digits) {
+            if (is369(digit)) {
+                clap++;
+            }
+        }
+        return clap;
+    }
+
+    private static boolean is369(Integer num) {
+        return num == 3 || num == 6 || num == 9;
     }
 
     private static List<Integer> getDigitList(int number) {
