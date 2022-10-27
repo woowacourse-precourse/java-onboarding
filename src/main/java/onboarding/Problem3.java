876c104df00c95850bd7ff81940ebca146a0fa37
korjun1993 <corojoon93@naver.com>
2022-10-27 20:28:43 +0900

    기능추가: Problem3 solution 추가
    1 ~ number 에 존재하는 모든 숫자에 대하여 3, 6, 9 가 몇번 등장하는지 세줘야하는 문제다. 만약 임의의 숫자 k에 대해 3, 6, 9 가 몇번 등장하는지 알려주는 테이블이 존재한다면, table[k + 1] = table[k] + (숫자 k + 1에 3,6,9의 등장 횟수) 일 것이다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int[] clap = new int[10001];
+
+        for (int i = 1; i <= 10000; i++) {
+            clap[i] = clap[i - 1] + getClapCount(i);
+        }
+        return clap[number];
+    }
+
+    /**
+     * @param number
+     * @return 숫자 number 에 포함된 3,6,9의 수
+     */
+    private static int getClapCount(int number) {
+        int share = 0, result = 0;
+        while (number > 0) {
+            share = number % 10;
+            if (share == 3 || share == 6 || share == 9) {
+                result++;
+            }
+            number /= 10;
+        }
+        return result;
     }
 }
