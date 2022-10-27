79758a4335c58c05da665a5bd732438f2e4a3c09
yeonji <view7186@naver.com>
2022-10-27 18:57:11 +0900

    Solving

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,21 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int bignum(int page) {
+        int sum = 0, mux = 1, tmp;
+        while(page > 0) {
+            tmp = page % 10;
+            sum += tmp;
+            mux *= tmp;
+        }
+        return sum > mux? sum : mux;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int ponum = Math.max(bignum(pobi.get(0)), bignum(pobi.get(1))), crnum = Math.max(bignum(pobi.get(0)), bignum(pobi.get(1)));
+        if(ponum > crnum) return 1;
+        if(crnum > ponum) return 2;
+        if(crnum == ponum) return 0;
+        return -1;
     }
 }
