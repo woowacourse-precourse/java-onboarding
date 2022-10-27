f20568101a29a5686e2c4e532734a90e31472886
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-10-27 11:41:29 +0900

    [feat] Problem2: 중복문자 확인 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
         return answer;
     }
+    private static boolean isDuplication(String[] array) {
+        Set<String> set = new HashSet<>(Arrays.asList(array));
+        return set.size() != array.length;
+    }
+
+
 }
