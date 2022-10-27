d297f53ec5bcca622a428448f36db25246c4f306
ParkRootSeok <g_e_un@naver.com>
2022-10-27 22:10:26 +0900

    feat : Promblem2 - 중복 문자 삭제 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,35 @@ package onboarding;
 
 public class Problem2 {
 
+    public static String removeTextDuplication(String cryptogram) {
+
+        String result;
+
+        /* String -> StringBuilder 형변환 */
+        StringBuilder crypto = new StringBuilder();
+        crypto.append(cryptogram);
+
+        while (hasTextDuplication(crypto)) {    // 중복 문자가 있다면 다시 반복
+
+            int len = crypto.length(); // 문자열 길이 갱신
+
+            for (int i = 0; i < len - 1; i++){
+
+                // 현재 인덱스와 다음 인덱스 중복 비교
+                if (crypto.charAt(i) == crypto.charAt(i + 1)) {
+                    crypto.delete(i, i + 2);    // 중복 구간 삭제(i <= interval < i + 2)
+                    break;
+                }
+
+            }
+        }
+
+        // 중복 제거된 문자열 저장 String 객체로 변환
+        result = crypto.toString();
+
+        return result;
+
+    }
 
     public static boolean hasTextDuplication(StringBuilder crypto) {
 
