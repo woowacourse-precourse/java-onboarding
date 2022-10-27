309add85ea13b0032a600552a8562d9856b3a2e7
kjb95 <topmedia44@gmail.com>
2022-10-27 21:40:40 +0900

    feat: Problem3 - 1부터 number 까지 손뼉을 친 개수의 총합 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,20 +2,24 @@ package onboarding;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Problem3 {
 
-    private static final int THREE = 3;
-    private static final int SIX = 6;
-    private static final int NINE = 9;
+    private static final String THREE = "3";
+    private static final String SIX = "6";
+    private static final String NINE = "9";
+    private static final int START_NUM = 1;
 
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return (int) IntStream.rangeClosed(START_NUM, number)
+            .map(Problem3::computeClapCount)
+            .reduce((clapCount1, clapCount2) -> clapCount1 + clapCount2)
+            .getAsInt();
     }
 
     private static int computeClapCount(int num) {
-        List<Integer> only369 = List.of(THREE, SIX, NINE);
+        List<String> only369 = List.of(THREE, SIX, NINE);
 
         return (int) Arrays.stream(String.valueOf(num)
                 .split(""))
