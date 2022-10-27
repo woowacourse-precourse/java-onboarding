5c4bed171b15550191642d56d850cbdee705da45
JunginPark4364 <wjddlsee@gmail.com>
2022-10-28 04:09:03 +0900

    feat: count 3, 6, 9
    - 주어진 숫자에서 3, 6, 9 몇번 사용되었는지 구하는 기능추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += get369Count(i);
+        }
+
         return answer;
+    }
+
+    public static int get369Count(int n) {
+        int cnt = 0;
+        while (n > 0) {
+            if ((n % 10 != 0) && (n % 10) % 3 == 0) cnt++;
+            n /= 10;
+        }
+        return cnt;
     }
 }
