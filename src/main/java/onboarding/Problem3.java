86e6c8c9cc76f841b3c65c8f1db68b4e394cd16d
KangSungBum <peter9706@naver.com>
2022-10-27 19:58:00 +0900

    feat: Problem3 기능 구현 완료
    countThreeSixNine 메소드 추가: 입력받은 num에 대한 3,6,9 포함 갯수 count

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i < number+1; i++) {
+            int targetNumber = i;
+            answer += countThreeSixNine(targetNumber);
+        }
+
         return answer;
+    }
+
+    private static int countThreeSixNine(int num) {
+        int count = 0;
+        String strNum = String.valueOf(num);
+
+        for (int i = 0; i < strNum.length(); i++) {
+            if (strNum.charAt(i) == '3' || strNum.charAt(i) == '6' || strNum.charAt(i) == '9') {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
