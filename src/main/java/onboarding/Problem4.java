cf5e932a2e4d090bfb173567a67b13077783ab9d
Park shinyeong <syngh503@gmail.com>
2022-10-27 18:05:49 +0900

    Problem4/Feat/영문자 체크기능 구현 각 문자를 매개변수로 받아 영문자인지 아닌지 확인하는 checkAlphabet 함수를 구현하였습니다. 영문자가 아니면 false, 영문자이면 true를 리턴합니다. Relate to: #4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static boolean checkAlphabet(char character) {
+        if(character < 65 || character > 90 || character < 97 || character > 122) {
+           return false;
+        }
+        return true;
     }
 }
