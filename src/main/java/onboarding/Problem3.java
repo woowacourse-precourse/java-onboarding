bd7ede510a1776903bd6b8eee701752cfa9af1e2
JHS <wnghdtjr129@naver.com>
2022-10-27 10:12:40 +0900

    feat: [문제3] 입력 받은 숫자까지 반복
    3,6,9 갯수를 세기 편하도록 문자열로 변환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 0; i <= number; i++) {
+            String currentNumber = String.valueOf(i);
+
+
+        }
         return answer;
     }
 }
