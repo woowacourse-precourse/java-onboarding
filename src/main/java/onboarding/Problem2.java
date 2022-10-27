92ad439b9dbd03c0f2f2e5f385f9e0f8e2b929c4
hong-sile <gurwns9325@naver.com>
2022-10-27 14:38:21 +0900

    feat : 연속하는 문자를 제거해 반환하는 메소드 추가, solution 메소드 완성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import onboarding.validatechecker.Problem2ValidateChecker;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	private static boolean duplicateFlag;
+
+	public static String solution(String cryptogram) {
+		Problem2ValidateChecker.isCryptogramValidate(cryptogram);
+		String answer = cryptogram;
+		duplicateFlag=true;
+
+		while(duplicateFlag) {
+			answer = removeDuplicateCharacter(answer);
+		}
+
+		return answer;
+	}
+
+	public static String removeDuplicateCharacter(String str){
+		duplicateFlag=false;
+		char[] charArray = str.toCharArray();
+		String result = "";
+		char preChar = '\0';
+
+		for (char ch : charArray) {
+			if(ch != preChar)
+				result += ch;
+			else {
+				duplicateFlag = true;
+				result=result.substring(0,result.length()-1);
+			}
+			preChar=ch;
+		}
+		return result;
+	}
 }
