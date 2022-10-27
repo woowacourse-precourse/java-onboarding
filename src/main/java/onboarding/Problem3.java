71a3eb404b53ba5787d6e8603bdbb144504861fc
hyun-seung <ardimento22@naver.com>
2022-10-27 12:04:29 +0900

    [FEAT] 1부터 주어진 number까지 반복한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,8 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
+        answer = startOneToNumber(number);
+
         return answer;
     }
 
@@ -12,12 +14,22 @@ public class Problem3 {
 
         while (targetNumber > 0) {
             int nowNumber = targetNumber % 10;
-            if(nowNumber == 3 || nowNumber == 6 || nowNumber == 9) {
+            if (nowNumber == 3 || nowNumber == 6 || nowNumber == 9) {
                 result++;
             }
             targetNumber /= 10;
         }
 
         return result;
+    }
+
+    public static int startOneToNumber(int number) {
+        int total = 0;
+
+        for (int i = 1; i <= number; i++) {
+            total += count(i);
+        }
+
+        return total;
     }
 }
