784b605c86071fd24c9b89faaa6a39962a652649
MinjeongKong <kong1301@ajou.ac.kr>
2022-10-27 09:52:01 +0900

    Problem1 - initial commit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * [기능 요구사항 목록]
+ * 1. 예외사항 처리 메서드 checkPage
+ * 2. 각 자리 숫자를 모두 더하는 메서드 addPage
+ * 3. 각 자리 숫자를 모두 곱하는 메서드 multiPage
+ * 4. 승자 출력 메서드 getWinner
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
