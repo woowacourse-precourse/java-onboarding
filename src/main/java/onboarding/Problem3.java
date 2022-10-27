9e1d91170b498e9a833b3bac0f44a09585aa8f3e
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-27 11:00:22 +0900

    [문제3] - 제공받은 한 자리 숫자가 3, 6, 9인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem3 {
+    public static boolean check369(int n) {
+        return n > 0 && n < 10 && n % 3 == 0;
+    }
+    
     public static int solution(int number) {
         int answer = 0;
         return answer;
