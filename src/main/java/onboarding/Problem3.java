6c99ad317e5a8a5609fd29c59a4b7af897b7e1ac
devmizz <garamminchan@gmail.com>
2022-10-28 02:15:35 +0900

    problem3: calculate clap count

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,19 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int getClap(int num) {
+        int cnt = 0;
+        int temp;
+
+        while(num != 0) {
+            temp = num % 10;
+            if(temp % 3 == 0) {
+                cnt++;
+            }
+            num /= 10;
+        }
+
+        return cnt;
+    }
 }
