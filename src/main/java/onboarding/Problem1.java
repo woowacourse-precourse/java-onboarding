bd31987c78848448534454d4408702a3d06a53ca
LHS-11 <ghktjq1119@naver.com>
2022-10-27 20:58:31 +0900

    Problem1 : 상수명 변경 (underscores 사용)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,8 +5,8 @@ import java.util.List;
 class Problem1 {
 
     private static final int EXCEPTION=-1;
-    private static final int POBIWIN=1;
-    private static final int CRONGWIN=2;
+    private static final int POBI_WIN =1;
+    private static final int CRONG_WIN =2;
     private static final int DRAW=0;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
@@ -58,9 +58,9 @@ class Problem1 {
     }
 
     private static int getAnswer(int answer, int pobiMax, int crongMax) {
-        if(pobiMax > crongMax) answer =POBIWIN;
+        if(pobiMax > crongMax) answer = POBI_WIN;
         if(pobiMax == crongMax) answer =DRAW;
-        if(pobiMax < crongMax) answer =CRONGWIN;
+        if(pobiMax < crongMax) answer = CRONG_WIN;
         return answer;
     }
 
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+
         return answer;
     }
 }
