5327ce451a0270e6bb7d9f23a657a82309b527b8
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-27 10:35:22 +0900

    feat: 숫자에 3, 6, 9 포함 개수 찾기 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+  public static int solution(int number) {
+    return 1;
+  }
+
+  public static int countIncludeGameNumber(int number) {
+    return (int) Stream.of(Integer.toString(number).split(""))
+            .mapToInt(Integer::parseInt)
+            .filter(n -> n == 3 | n == 6 || n == 9)
+            .count();
+  }
 }
