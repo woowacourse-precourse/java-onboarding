06fbfc084f6eb482ba1b9bffc2f8c30b03752cc0
jeujeon <1132jjw@naver.com>
2022-10-28 00:36:02 +0900

    feat: add calculate each score feat in Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,95 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static final int ERROR = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiScore;
+        int crongScore;
+
+        if (!isValidInput(pobi) || !isValidInput(crong)) {
+            answer = ERROR;
+            return answer;
+        }
+        pobiScore = calculateScore(pobi);
+        crongScore = calculateScore(crong);
         return answer;
+    }
+
+    private static int calculateScore(List<Integer> userInput) {
+        int leftPage;
+        int rightPage;
+        int sum;
+        int mul;
+
+        leftPage = userInput.get(0);
+        rightPage = userInput.get(1);
+        sum = getBiggerSum(leftPage, rightPage);
+        mul = getBiggerMultiply(leftPage, rightPage);
+        return sum > mul ? sum : mul;
+    }
+
+    private static int getBiggerSum(int leftPage, int rightPage) {
+        int leftSum = 0;
+        int rightSum = 0;
+
+        while (leftPage > 0) {
+            leftSum += leftPage % 10;
+            leftPage /= 10;
+        }
+        while (rightPage > 0) {
+            rightSum += rightPage % 10;
+            rightPage /= 10;
+        }
+        return leftSum > rightSum ? leftSum : rightSum;
+    }
+
+    private static int getBiggerMultiply(int leftPage, int rightPage) {
+        int leftMul = 1;
+        int rightMul = 1;
+
+        while (leftPage > 0) {
+            leftMul *= leftPage % 10;
+            leftPage /= 10;
+        }
+        while (rightPage > 0) {
+            rightMul *= rightPage % 10;
+            rightPage /= 10;
+        }
+        return leftMul > rightMul ? leftMul : rightMul;
+    }
+
+    private static boolean isValidInput(List<Integer> userInput) {
+        return (isNotNull(userInput) && isValidSize(userInput) && isConnectedPage(userInput)
+            && isOddEvenSequence(userInput) && isValidRange(userInput));
+    }
+
+    private static boolean isNotNull(List<Integer> userInput) {
+        return userInput != null;
+    }
+
+    private static boolean isValidSize(List<Integer> userInput) {
+        return userInput.size() == 2;
+    }
+
+    private static boolean isConnectedPage(List<Integer> userInput) {
+        return userInput.get(1) - userInput.get(0) == 1;
+    }
+
+    private static boolean isOddEvenSequence(List<Integer> userInput) {
+        return isOdd(userInput.get(0)) && isEven(userInput.get(1));
+    }
+
+    private static boolean isOdd(int num) {
+        return num % 2 == 1;
+    }
+
+    private static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    private static boolean isValidRange(List<Integer> userInput) {
+        return userInput.get(0) >= 1 && userInput.get(0) <= 400;
     }
 }
