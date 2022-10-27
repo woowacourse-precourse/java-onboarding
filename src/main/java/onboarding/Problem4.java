cb299130302a7d1c7d2cb95efa1cb331bbf79032
yoondgu <doyou2ing@naver.com>
2022-10-27 16:04:47 +0900

    feat(Problem4): implement isLowerCase, getSymmetricAlphabet methods

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,19 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    
+    private static boolean isLowerCase(char ch) {
+    	return (ch >= 97 && ch <= 122) ? true : false;
+    }
+
+    private static char getSymmetricAlphabet(char src) {
+    	int startValue = 65;
+    	int endValue = 90;
+    	// 주어진 문자가 소문자라면 기준 값을 변경한다.
+    	if (isLowerCase(src)) {
+    		startValue = 97;
+    		endValue = 122;
+    	}
+    	return (char) (endValue - (src - startValue));
+    }
 }
