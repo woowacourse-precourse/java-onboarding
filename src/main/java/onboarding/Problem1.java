df57b83249e7e363331126b60217eb5ca9d14b19
Heosejin98 <sejin3319@naver.com>
2022-10-27 16:41:35 +0900

    Feat: PROBLEM1 사용자가 펼친 페이지 자릿수 저장 클래스 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,37 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
+public class NanoPage{
 
+    public List<Integer> left_page;
+    public List<Integer> right_page;
+
+    public Page(List<Integer> people){
+        left_page = page_split(people.get(0));
+        right_page = page_split(people.get(1));
+    }
+
+    public List<Integer> page_split(int num){
+        List<Integer> result_page = new ArrayList<Integer>();
+
+        while(num > 0){
+            result_page.add(num % 10);
+            num /= 10;
+        }
+        return result_page;
+    }
+
+}
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        Page pobi_page = new NanoPage(pobi);
+        Page crong_page = new Page(crong);
+
+
         return answer;
     }
 }
+
