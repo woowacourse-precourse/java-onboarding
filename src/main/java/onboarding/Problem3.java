1c86bb3042cb44f29c0ffa07c40fb50ea2c68d2b
JDK <ehdrjf2062@gmail.com>
2022-10-27 18:56:47 +0900

    feat: 문제3 369게임

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+		
+		// 1부터 number까지
+		for(int i = 1; i <= number; i++){
+			String strNum = String.valueOf(i);
+
+			// 자릿수 확인
+			for(int j = 0 ; j < strNum.length(); j++){
+				char target = strNum.charAt(j);
+				if(target == '3' || target == '6' || target == '9'){
+					System.out.println("i = " + i);
+					answer++;
+				}
+			}
+		}
+		
         return answer;
     }
 }
