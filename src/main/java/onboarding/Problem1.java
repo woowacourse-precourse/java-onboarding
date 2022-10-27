dbac88dcc4477245d28eed00428a8ff3a0ad448a
hyunsb <jhss0113@naver.com>
2022-10-27 20:48:24 +0900

    problem1 success

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,25 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        int pobiMax = Math.max(findMaxNum(pobi.get(0)), findMaxNum(pobi.get(1)));
-        int crongMax = Math.max(findMaxNum(crong.get(0)), findMaxNum(crong.get(1)));
+        if(pobi.get(0) == pobi.get(1)-1 && crong.get(0) == crong.get(1)-1){
+            int pobiMax = Math.max(findMaxNum(pobi.get(0)), findMaxNum(pobi.get(1)));
+            int crongMax = Math.max(findMaxNum(crong.get(0)), findMaxNum(crong.get(1)));
 
-        System.out.println(pobiMax);
-        System.out.println(crongMax);
-
-        return Math.max(pobiMax, crongMax);
+            if(pobiMax == crongMax) return 0;
+            return (pobiMax > crongMax) ? 1 : 2;
+        }else{
+            return -1;
+        }
     }
 
     public static int findMaxNum(int n) {
-        int add = 0, mul = 0;
+        int add = n % 10;
+        int mul = n % 10;
+        n /= 10;
 
-        for (n > 10) {
+        while(n > 0) {
             int temp = n % 10;
-
             add += temp;
             mul *= temp;
-
             n /= 10;
         }
 
