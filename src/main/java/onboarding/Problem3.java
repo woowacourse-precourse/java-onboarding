5bd69bae5358080dd8c12234abe347b8ea5173b3
TaeMin <xoals3094@naver.com>
2022-10-28 03:27:06 +0900

    no.3 start_game method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,13 @@ public class Problem3 {
     public static int solution(int number) {
         return 1;
     }
+    public static int start_game(int number){
+        int count = 0;
+        for(int i = 1; i <= number; i++){
+            count += counting_369(i);
+        }
+        return count;
+    }
     public static int counting_369(int num){
         int count = 0;
         for(int n; num != 0; num /= 10){
