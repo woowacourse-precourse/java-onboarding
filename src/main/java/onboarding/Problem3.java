1a29ba423afa27ae746fb6c75fa0bdbd68e4d31c
bunsung92 <bunsung92@naver.com>
2022-10-28 00:28:01 +0900

    feat: add play game method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return playGame(number);
+    }
+
+    private static int playGame(int number) {
+        int result = 0;
+
+        for (int i = 1; i <= number; i++) {
+            result += countClap(i);
+        }
+
+        return result;
+    }
+
+    private static int countClap(int checkNum) {
+        return 0;
     }
 }
