42ec299b2ef585f994a5a3d93a94739566f65423
youhobin <ghdb132@naver.com>
2022-10-27 18:00:16 +0900

    feat(onboarding): 문자의 각 글자를 다음 글자와 비교해 중복이면 그 부분을 제외한 새로운 문자열을 만드는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        for (int i = cryptogram.length() - 1; i > 0; i--) {
+            char currChar = cryptogram.charAt(i);
+            char prevChar = cryptogram.charAt(i - 1);
+
+            if (currChar == prevChar) {
+                cryptogram = cryptogram.substring(0,i - 1) + cryptogram.substring(i + 1);
+            }
+        }
+        return cryptogram;
     }
 }
