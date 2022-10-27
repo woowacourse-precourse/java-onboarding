a2b084c6cad8f7c1033b7c1f76887b2ce9d98b2c
sieun <sieun960513@naver.com>
2022-10-27 17:47:10 +0900

    [Problem34] : 기능 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/** 기능 목록
+ * solutionLogic    : 메인 솔루션
+ * lowerChange      : 소문자 변환
+ * upperChange      : 대문자 변환
+ * checkValid       : 예외 처리
+ * checkWordLength  : 단어 길이 체크
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
