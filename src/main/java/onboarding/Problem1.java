a99a25dd1c7373975459c299db0a2bdc2ba04d33
KyungKyu Park <rudrb0505@gmail.com>
2022-10-27 14:05:39 +0900

    feat: 페이지의 각 자리수 더하기 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,10 +15,24 @@ class Problem1 {
 			return result;
 		}
 		result = checkException(crongLeftPage, crongRightPage);
+		if(result == -1) {
+			return result;
+		}
+
+		int pobiLeftSum = 0;
+		int pobiRightSum = 0;
+		int crongLeftSum = 0;
+		int crongRightSum = 0;
+
+		pobiLeftSum = calculateSum(pobiLeftPage, pobiLeftSum);
+		pobiRightSum = calculateSum(pobiRightPage, pobiRightSum);
+		crongLeftSum = calculateSum(crongLeftPage, crongLeftSum);
+		crongRightSum = calculateSum(crongRightPage, crongRightSum);
 
 		// int answer = Integer.MAX_VALUE;
 		return result;
 	}
+
 	private static int checkException(int LeftPage, int RightPage) {
 		if (LeftPage % 2 == 0 || RightPage % 2 == 1 ) {
 			return  -1;
@@ -30,5 +44,13 @@ class Problem1 {
 			return  -1;
 		}
 		return 0;
+	}
+
+	private static int calculateSum (int page, int sum) {
+		while (page > 0) {
+			sum += page % 10 ;
+			page /= 10;
+		}
+		return sum;
 	}
 }
