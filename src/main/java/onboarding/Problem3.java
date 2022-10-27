d0d8fba23bb6adb24fd6f2a8240281f93c5729b5
sieun <sieun960513@naver.com>
2022-10-27 17:02:17 +0900

    [Problem3] :(countClap) 수에 rule에 해당하는 숫자 갯수 반환 [신규]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 /** 기능 목록
  * solutionLogic    : 메인 솔루션
  * setGameRule      : 게임 룰에 맞는 숫자 저장
+ * isSatisfied      : 게임 룰에 맞는지 확인
  * countClap        : 해당 숫자 박수 갯수 반환
  * checkValid       : 예외 처리
  */
@@ -15,6 +16,8 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+        setGameRule();
+
         return answer;
     }
 
@@ -26,17 +29,27 @@ public class Problem3 {
         }
     }
 
+
     public static void setGameRule() {
         gameRule.add(3);
         gameRule.add(6);
         gameRule.add(9);
     }
 
+    public static int isSatisfied(int num) {
+        if (gameRule.contains(num % 10)) {
+            return 1;
+        }
+        return 0;
+    }
+
     public static int countClap(int num) {
         int count = 0;
         while (num != 0) {
-
+            count += isSatisfied(num);
+            num = num/10;
         }
+        return count;
     }
 
     public static boolean checkValid(int num) {
