b52a44c547f558df829281ba66d9180cc38ce8a1
안광민 <creativeduck0518@gmail.com>
2022-10-27 23:21:23 +0900

    feat(Problem 7): 친구 관계 Map 및 친구들의 점수 Map 에 항목을 추가하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void addFriend(Map<String, List<String>> friendMap, Map<String, Integer> friendScoreMap, String friendA, String friendB) {
+        if (!friendMap.containsKey(friendA)) {
+            List<String> friends = new LinkedList<>();
+            friends.add(friendB);
+
+            friendMap.put(friendA, friends);
+            friendScoreMap.put(friendA, 0);
+        }
+        else {
+            List<String> friends = friendMap.get(friendA);
+            friends.add(friendB);
+        }
     }
 }
