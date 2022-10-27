bd645f3f839e2c1795ac0bfc291b37dd351f0c79
jthugg <jthug@naver.com>
2022-10-27 22:05:13 +0900

    init problem5 & chk todo list

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        // 액면가가 큰 화폐 순으로 최대한 많이 담기
+        // 오만원권, 만원권, 오천원권, 천원권, 오백원동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 순
+        // 액면가 별 메서드 생성, 화폐 수량 리턴
+        // 리턴 받은 수량은 제출 할 답안 리스트에 추가
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
