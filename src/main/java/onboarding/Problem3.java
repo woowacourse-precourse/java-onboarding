7a9cc27edb7ae736e2a3f5545d5e1e3a6509dea5
kariskan <zkfzpf56@naver.com>
2022-10-27 15:41:50 +0900

    feat: getClapCount 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 3; i <= number; i++) {
+            answer += getClapCount(i);
+        }
+
         return answer;
+    }
+
+    public static int getClapCount(int number) {
+        int clap = 0;
+        while (number != 0) {
+            if (isMatch(number % 10)) {
+                clap++;
+            }
+            number /= 10;
+        }
+        return clap;
+    }
+
+    public static boolean isMatch(int digit) {
+        return true;
     }
 }
