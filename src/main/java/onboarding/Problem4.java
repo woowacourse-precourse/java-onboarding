7a384d3f95d26052fc9b506cb63a295ab26bbe39
jini_11 <jieun21124@naver.com>
2022-10-27 14:04:01 +0900

    feat: 문제4 사전을 참고해 반환하는 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+    private static char[] dictionary = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+
     public static String solution(String word) {
         String answer = "";
 
         if (checkInput(word)) {
             return "-1";
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            answer += changeAlphabet(word.charAt(i));
         }
 
         return answer;
@@ -26,5 +32,18 @@ public class Problem4 {
             return 1;
         }
         return 0;
+    }
+
+    private static char changeAlphabet(char alphabet) {
+        int check = checkUpperOrLower(alphabet);
+
+        if (check == 2) {
+            return dictionary[(int)alphabet - 65];
+        }
+        if (check == 1) {
+            return Character.toLowerCase(dictionary[(int)Character.toUpperCase(alphabet) - 65]);
+        }
+
+        return alphabet;
     }
 }
