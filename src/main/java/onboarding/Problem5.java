a85a9f82442f1847bfb3e5b1eb2d4a97bf9e2779
somsom13 <somin1113@naver.com>
2022-10-27 23:14:11 +0900

    feat(문제5): 금액별 인출 가능 요금 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static int[] amounts = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static int[] answerArr = new int[9];
+    private static int leftMoney;
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    private static int calculateWithdraw(int amount) {
+        int count = leftMoney/amount;
+        leftMoney = leftMoney%amount;
+        return count;
+    }
+
 }
