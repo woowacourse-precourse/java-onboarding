a1e3c2eb92701446f463ac8ad4f199597ba1eb00
areyouhun <areyouhun@gmail.com>
2022-10-27 18:51:12 +0900

    feat(prob2): return a parameter as it is if it has only one letter

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,10 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        if (cryptogram.length() == 1) {
+            return cryptogram;
+        }
+        String answer = cryptogram;
         return answer;
     }
 }
