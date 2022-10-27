c26d36168ab511fa8ff24d8294244699b5c17c93
Hongsugyeong <olivia258@naver.com>
2022-10-27 20:18:42 +0900

    [problem3] 기능 목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem3 {
+    /**
+     * 3,6,9 갯수 세는 함수
+     */
     public static int solution(int number) {
         int answer = 0;
         return answer;
