aeb6543dd47e3ec928e5f7b632219a5d093c4578
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-10-27 15:17:37 +0900

    문제 3번 <1번 기능 추가 완료>

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,19 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    //숫자 하나에서 3,6,9가 몇 번 포함되어 있는지 확인하는 메서드
+    private static int checkNum(int num) {
+        int cnt = 0;
+        while (num > 0) {
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                cnt++;
+                num /= 10;
+            }
+            if (num % 10 != 3 && num % 10 != 6 && num % 10 != 9) {
+                num /= 10;
+            }
+        }
+        return cnt;
+    }
 }
