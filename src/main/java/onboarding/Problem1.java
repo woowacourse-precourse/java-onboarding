532715200aad1d351d8890d7b909d57b022b7195
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-27 12:52:16 +0900

    feat:pobiMax,crongMax를통한최대값추출

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,16 +5,29 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int left_max_pobi=divideNum(pobi.get(0));
-        int right_max_pobi=divideNum(pobi.get(1));
+        int pobi_max = pobiMax(pobi);
+        int crong_max = crongMax(crong);
 
-        int left_max_crong=divideNum(crong.get(0));
-        int right_max_crong=divideNum(crong.get(1));
 
-        
 
         return answer;
     }
+
+    public static int pobiMax(List<Integer> pobi){  // pobi의 값중 가장 큰 값 찾기
+
+        int left_max=divideNum(pobi.get(0));
+        int right_max=divideNum(pobi.get(1));
+
+        return Math.max(left_max, right_max);
+    }
+
+    public static int crongMax(List<Integer> crong){  // crong의 값중 가장 큰 값 찾기
+        int left_max=divideNum(crong.get(0));
+        int right_max=divideNum(crong.get(1));
+
+        return Math.max(left_max, right_max);
+    }
+
 
     public static int divideNum(Integer x){ // 각 자릿수 별로 나누고, 더한것이 가장 큰지 곱한 것이 가장 큰지 비교
         int addSum=0;
