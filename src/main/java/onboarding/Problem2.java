41935dbb714f65321fb22e1adbfbcd7a4fcc9af3
junhei Lee <jun2ee22@naver.com>
2022-10-27 12:16:54 +0900

    feat: 반복되는 두 문자 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+
+    //두번 반복되는 문자 삭제
+    private static StringBuffer removeRepeat(StringBuffer cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                cryptogram.delete(i, i + 2);
+                break;
+            }
+        }
+        return cryptogram;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        StringBuffer strBCcryptogram = new StringBuffer(cryptogram);
+        
         return answer;
     }
 }
