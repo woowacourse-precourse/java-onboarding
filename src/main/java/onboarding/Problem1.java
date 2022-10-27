fe12201aa88034f0d1009591482caab1f5415539
swandevson <shai4956@gmail.com>
2022-10-28 02:25:07 +0900

    feat: 승자 판별 각 인원의 점수를 비교하여 승자를 판별한다. 단, 예외 페이지를 입력받은 경우에는 -1을 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -43,13 +43,36 @@ class Problem1 {
 
         return Math.max(leftRes, rightRes);
     }
+    public static int getWinner(List<Integer> pobi, List<Integer> crong) {
+        int draw = 0;
+        int pobiWin = 1;
+        int crongWin = 2;
 
+        int pobiScore = getPageResult(pobi);
+        int crongScore = getPageResult(crong);
+
+        // 예외 처리
+        if (pobiScore == -1 || crongScore == -1) {
+            return -1;
+        }
+
+        // 승자 처리
+        if (pobiScore > crongScore) {
+            return pobiWin;
+        }
+        else if (pobiScore < crongScore) {
+            return crongWin;
+        }
+        else {
+            return draw;
+        }
+    }
 
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-
+        answer = getWinner(pobi, crong);
 
         return answer;
     }
