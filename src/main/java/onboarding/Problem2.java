1d8061c50988f18915a561f41df4985cc8b2531d
WonJeongYeon <wonjungyoun@naver.com>
2022-10-27 13:35:42 +0900

    20221027 Problem2.java 기초공사 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,37 +7,35 @@ import java.util.List;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        answer = cryptogram.toLowerCase();
         String[] charList = answer.split("");
         List<String> charArrayList = new ArrayList<>(Arrays.asList(charList));
-        List<Integer> indexList = new ArrayList<>();
-        do {
-            indexList.clear();
-            System.out.println(indexList);
-            for (int i = 0; i < charArrayList.size(); i++) {
-                if (i != charArrayList.size()-1) {
-                    System.out.println(indexList);
-                    if (charArrayList.get(i).equals(charArrayList.get(i + 1))) {
-                        indexList.add(i);
-                        indexList.add(i + 1);
-                        System.out.println(indexList.get(0));
+        if (!cryptogram.equals(cryptogram.toLowerCase())) {
+            throw new IllegalStateException("문자열은 소문자로만 이루어져 있어야 합니다.");
+        } else if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            throw new IllegalStateException("문자열의 길이는 1 이상, 1000 이하여야 합니다.");
+        } else {
+            List<Integer> indexList = new ArrayList<>();
+            do {
+                indexList.clear();
+                for (int i = 0; i < charArrayList.size(); i++) {
+                    if (i != charArrayList.size() - 1) {
+                        if (charArrayList.get(i).equals(charArrayList.get(i + 1))) {
+                            indexList.add(i);
+                            indexList.add(i + 1);
+                        }
                     }
                 }
-            }
-            for (int i = indexList.size()-1; i >= 0; i--) {
-                charArrayList.remove(Integer.parseInt(indexList.get(i).toString()));
-                System.out.println(charArrayList);
-            }
+                for (int i = indexList.size() - 1; i >= 0; i--) {
+                    charArrayList.remove(Integer.parseInt(indexList.get(i).toString()));
+                }
+            } while (!indexList.isEmpty());
 
-        } while (!indexList.isEmpty());
+            answer = String.join("", charArrayList);
 
-        answer = String.join("", charArrayList);
-
-        System.out.println("     cryptogram          result     ");
-        System.out.println("\"" + cryptogram + "\"" +  "    " + "\"" + answer + "\"");
+            System.out.println("     cryptogram          result     ");
+            System.out.println("\"" + cryptogram + "\"" + "    " + "\"" + answer + "\"");
+        }
 
         return answer;
     }
-
-
 }
