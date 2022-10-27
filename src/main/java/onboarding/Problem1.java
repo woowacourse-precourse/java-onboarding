0070cc4b9e702bc70e1f3c6f56f20db0d539b378
JunginPark4364 <wjddlsee@gmail.com>
2022-10-28 02:47:57 +0900

    feat: get large page(left or right) and get large value(plus or multiply)
    - 한 페이지의 각 자리수 덧셈, 곱셈 중 더 큰값 구하는 기능추가
    - 왼쪽 페이지, 오른쪽 페이지 중 큰 값 구하는 기능추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -18,6 +18,26 @@ class Problem1 {
         return answer;
     }
 
+    // 왼쪽 페이지, 오른쪽 페이지 중 최댓값 반환
+    public static int getMax(String left, String right) {
+        int max = Math.max(getLargeSumMul(left), getLargeSumMul(right));
+
+        return max;
+    }
+
+    // 해당 페이지에서 각 자리수 합 or 곱 중 큰값 반환
+    public static int getLargeSumMul(String page) {
+        int sum = 0, mul = 1;
+
+        for (int i = 0; i < page.length(); i++) {
+            int n = page.charAt(i) - '0';
+            sum += n;
+            mul *= n;
+        }
+
+        return Math.max(sum, mul);
+    }
+
     // 입력된 페이지들이 조건에 맞는지 확인
     public static boolean isWrongInput(List<Integer> pobi, List<Integer> crong) {
         if (isFirstOrLast(pobi, crong) || isNotConsecutive(pobi, crong)) {
