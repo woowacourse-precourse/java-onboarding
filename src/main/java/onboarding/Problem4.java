52f1c1ad887c72ad95733ac6a00446c6882abd60
MIN-04 <kimji_0424@naver.com>
2022-10-27 17:30:09 +0900

    add Problem4 기능 2, 3 update Problem4 기능 1 배열선언 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,15 +9,23 @@ public class Problem4 {
      *      - 알파벳이 아닌 문자는 변환하지 않는다.
      */
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
 
-        String dicUpper[] = {"Z", "Y", "X",	"W", "V", "U", "T", "S", "R", "Q", "P", "O", "N",
+        String[] dicUpper = {"Z", "Y", "X",	"W", "V", "U", "T", "S", "R", "Q", "P", "O", "N",
                 "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
-        String dicLower[] = {"z", "y", "x",	"w", "v", "u", "t", "s", "r", "q", "p", "o", "n",
+        String[] dicLower = {"z", "y", "x",	"w", "v", "u", "t", "s", "r", "q", "p", "o", "n",
                 "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"};
 
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                answer.append(dicUpper[word.charAt(i) - 65]);
+            } else if (Character.isLowerCase(word.charAt(i))) {
+                answer.append(dicLower[word.charAt(i) - 97]);
+            } else {
+                answer.append(word.charAt(i));
+            }
+        }
 
-
-        return answer;
+        return answer.toString();
     }
 }
