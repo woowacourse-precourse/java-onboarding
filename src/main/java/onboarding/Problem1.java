1853b933bd3511de7f6a26825a64ea25e0f8b5e1
ygreenb <nhj7911@gmail.com>
2022-10-28 03:13:28 +0900

    feat: Problem1. 기능1. 페이지 번호로 구할 수 있는 가장 큰 수를 구하는 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,16 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int findMaxNum(String page){
+        int add = page.charAt(0)-'0';
+        int multiple = page.charAt(0)-'0';
+
+        for(int i=1;i<page.length();i++){
+            add += page.charAt(i)-'0';
+            multiple *= page.charAt(i)-'0';
+        }
+
+        return Math.max(add, multiple);
+    }
 }
