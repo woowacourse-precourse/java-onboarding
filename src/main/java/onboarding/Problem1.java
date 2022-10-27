0111bfa0c9cb143dbc5641d15833e6e9e5e94cfa
gomljo <gomljo0307@naver.com>
2022-10-28 00:11:59 +0900

    refactor(Problem1.java): 에러 처리 기능 구현을 위한 클래스 추가
    에러 처리를 담당하는 함수들을 모은 Exception 클래스 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,19 +2,23 @@ package onboarding;
 
 import java.util.List;
 
-class Problem1 {
-    public static final int CORRECT_LENGTH = 2;
-    public static final int LEFT_PAGE = 0;
-    public static final int RIGHT_PAGE = 1;
-    public static final int DIFFERENCE_BETWEEN_PAGES=1;
-    public static final int START_PAGE = 1;
-    public static final int FINAL_PAGE = 400;
-    public static Boolean isWrongLength(List<Integer> bookPages){
+class Exception{
+    public final int CORRECT_LENGTH = 2;
+    public final int LEFT_PAGE = 0;
+    public final int RIGHT_PAGE = 1;
+    public final int DIFFERENCE_BETWEEN_PAGES=1;
+    public final int START_PAGE = 1;
+    public final int FINAL_PAGE = 400;
+
+    public boolean isWrongLength(List<Integer> bookPages){
         return bookPages.size() != CORRECT_LENGTH;
     }
-    public static boolean isWrongPosition(List<Integer> bookPages){
+    public boolean isWrongPosition(List<Integer> bookPages){
         return (bookPages.get(LEFT_PAGE) % 2!=1)||(bookPages.get(RIGHT_PAGE)%2!=0);
     }
+}
+class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
