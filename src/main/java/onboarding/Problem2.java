7ad8f6e65fbb7d75cd1adf3c6132953eb0bbde27
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-27 16:43:02 +0900

    기능 구현목록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+
+import java.util.Stack;
+
+/**
+ * 기능 구현 목록
+ * 1) 중복을 제거하는 메서드 구현(Stack 이용)
+ * 2) 구현된 값을 answer에 추가하는 기능 구현
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
         return answer;
     }
+
 }
