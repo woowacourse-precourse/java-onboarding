96b7460fd058b57f3f4e92afc4cb1d8576508de6
jmj005 <jmj00005@naver.com>
2022-10-27 19:34:44 +0900

    feat: 형식 체크하기 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+
+/*
+1. 형식 체크하기
+2. 문자열 토큰 얻기
+3. 중복 확인하기
+4. 중복 추가하기
+5. 정렬하기
+ */
 
 public class Problem6 {
+
+    private static boolean isValidEmail(String email) {
+        if (email.matches(".*@email.com") && 10 < email.length() && email.length() < 20)
+            return true;
+        return false;
+    }
+
+    private static boolean isValidName(String name) {
+        if (name.matches("^[가-힣]*$") && 0 < name.length() && name.length() < 20)
+            return true;
+        return false;
+    }
+
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
