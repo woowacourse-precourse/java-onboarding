50cf13520725555dc4a044d9f64c99c1fd368bde
whipbaek <jibaek0513@gmail.com>
2022-10-27 19:42:16 +0900

    P4 Add map for crypto

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    private static Map<Character, Character> map = new HashMap<>() {
+        {
+            put('A','Z'); put('B', 'Y'); put('C', 'X'); put('D', 'W'); put('E', 'V'); put('F', 'U'); put('G', 'T'); put('H', 'S'); put('I', 'R'); put('J', 'Q');
+            put('K', 'P'); put('L', 'O'); put('M', 'N'); put('N', 'M'); put('O', 'L'); put('P', 'K'); put('Q', 'J'); put('R', 'I'); put('S', 'H'); put('T', 'G');
+            put('U', 'F'); put('V', 'E'); put('W', 'D'); put('X', 'C'); put('Y', 'B'); put('Z', 'A');
+        }
+    };
+    
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        return "";
     }
 }
