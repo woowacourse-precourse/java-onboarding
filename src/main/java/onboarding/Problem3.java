755294381b27f91ed2d686205cba28d8c23dbd63
viyott <jackk0616@gmail.com>
2022-10-27 14:07:38 +0900

    p3 count369 메소드 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,7 @@ public class Problem3 {
      * 기능 목록
      * 1. 특정 숫자에 대해서 손뼉을 치는 수를 구하는 메소드 구현
      * 2. number까지 for문을 돌면서 answer에 추합
+     * 3. 10으로 나눴을 때 0이되는 경우 제외
      */
     public static int solution(int number) {
         int answer = 0;
@@ -18,7 +19,10 @@ public class Problem3 {
     public static int count369(int n) {
         int cnt = 0;
         while(n > 0) {
-            if((n % 10) % 3 == 0) cnt++;
+            // 3. 10으로 나눴을 때 0이되는 경우 제외
+            if(n % 10 != 0) {
+                if((n % 10) % 3 == 0) cnt++;
+            }
             n = n / 10;
         }
         return cnt;
