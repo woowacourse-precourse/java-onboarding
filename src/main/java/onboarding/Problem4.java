2f3fe13088efb4d1557892eb08a6332ab221feb8
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-27 12:25:35 +0900

    [문제4] - 제공받은 한 문자를 청개구리 문자로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,21 @@ public class Problem4 {
     public static boolean isAlpha(char c) {
         return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
     }
+
+    public static char formatToFrogCharacter(char c) {
+        if (isAlpha(c)) {
+            int base;
+            
+            if (c >= 'a') 
+                base = 'a';
+            else
+                base = 'A';
+
+            return (char)((25 - (c - base)) % 26 + base); 
+        } else {
+            return c;
+        }
+    }
     
     public static String solution(String word) {
         String answer = "";
