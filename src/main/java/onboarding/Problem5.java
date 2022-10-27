9b2ffb81c978170f93bf347b3a9d32a1f458cab2
JHS <wnghdtjr129@naver.com>
2022-10-27 18:25:01 +0900

    feat(Problem5): [문제 5] 일치하는 인덱스에 저장 후 list 변환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,7 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int[] moneys = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] result = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
         int index = 0;
         int count = 0;
@@ -17,6 +18,13 @@ public class Problem5 {
                 count++;
                 continue;
             }
+            result[index++] = count;
+            count = 0;
+        }
+        result[index] = count;
+
+        for (int value : result) {
+            answer.add(value);
         }
         return answer;
     }
