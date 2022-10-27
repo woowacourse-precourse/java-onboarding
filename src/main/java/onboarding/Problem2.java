c32a6cd26ab894730a7929147efc730e07adff4f
abbd2 <ajkety00@gmail.com>
2022-10-28 00:15:39 +0900

    cryptogram의 수정을 용이하게 하기위해 StringBuildert형으로 변환하고, 중복된 문자를 검사하기 위해 반복문의 틀을 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+      StringBuilder sb = new StringBuilder(cryptogram);
+
+      for(int i = 1; i < sb.length(); i++){
+
+      }
+      String answer = sb.toString();
         return answer;
     }
 }
