87044584d40f79dc645835090a353f85c9cd3fbf
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 02:00:14 +0900

    [Problem2] 중복된 문자 제거하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -22,6 +22,27 @@ public class Problem2 {
         return -1;
     }
 
+    /**
+     * 2. 중복된 문자 제거
+     * @param cryptogram : 중복된 문자를 제거할 암호문
+     * @return : 재귀함수로 다른 중복된 문자 제거 함수 호출, 만약 제거할 것이 없다면 암호문 리턴
+     */
+    static String removeDuplicate(String cryptogram){
+        int checking = checkDuplicate(cryptogram);
+        if (checking == -1) return cryptogram;
+
+        int start = checking;
+        int end = start + 1;
+        for (; end < cryptogram.length(); end++){
+            if (cryptogram.charAt(start) == cryptogram.charAt(end)) continue;
+            else break;
+        }
+
+        cryptogram = cryptogram.substring(0, start) + cryptogram.substring(end);
+
+        return removeDuplicate(cryptogram);
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
