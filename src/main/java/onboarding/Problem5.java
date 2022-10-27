4a32538773077ff7493838da001287e88df8116b
Your Name <you@example.com>
2022-10-27 18:09:33 +0900

    금액이 큰 순서로 화폐 반환 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,16 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        int[] unitOfMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] unitOfMoneys = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+        int[] tmpAnswer = new int[unitOfMoneys.length];
 
         /*금액이 큰 순서대로 추가*/
-        
+        for (int i = 0; i < unitOfMoneys.length; ++i) {
+            int moneyReturned = money / unitOfMoneys[i];
+            if (moneyReturned > 0) {
+                tmpAnswer[i] = moneyReturned;
+                money -= moneyReturned * unitOfMoneys[i];
+            }
+        }
+
+        answer = Arrays.stream(tmpAnswer)
+                        .boxed()
+                        .collect(Collectors.toList());
         return answer;
     }
 }
