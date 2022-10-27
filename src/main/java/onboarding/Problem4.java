3469716bd604ce5de214257ecd2dbe256edea247
qdeu22 <qdeu22@gmail.com>
2022-10-27 20:01:21 +0900

    청개구리 사전 커밋

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,6 +9,14 @@ public class Problem4 {
         /* 키와 값을 가지는 map 생성 */
         HashMap<Character, Character> m = new HashMap<>();
 
+        /* 청개구리 사전 */
+        for (int i = 0; i < 26; i++) {
+            /* 대문자 입력 */
+            m.put((char) ('A' + i), (char) ('Z' - i));
+            /* 소문자 입력 */
+            m.put((char) ('a' + i), (char) ('z' - i));
+        }
+
         return answer;
     }
 }
