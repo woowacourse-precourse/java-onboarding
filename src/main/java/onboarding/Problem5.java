329573eb4da5a8ea5901119a773b509c5a3d7b27
YooByeongIk <quddlr96@naver.com>
2022-10-27 17:54:24 +0900

    [Problem5] docs : 기능 목록 주석 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+/*
+##기능 목록
 
+1. 단위 지폐 목록을 담은 List 생성
+2. 입력 값을 단위 지폐로 나눠 몫을 결과 List에 저장
+3. 걸과 반환
+
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
