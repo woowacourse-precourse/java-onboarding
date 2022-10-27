c394d2601d7123710c5c708244b79526261b45bf
Hongsugyeong <olivia258@naver.com>
2022-10-27 19:29:44 +0900

    [problem1] 리스트의 문자열을 나눠서 배열에 저장하는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
@@ -12,5 +13,17 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    /*
+     *리스트의 문자열을 나눠서 배열에 저장하는 함수
+     */
+    static List<Integer> splitList(int number) {
+        List<Integer> split = new ArrayList<>();
+        while(number != 0){
+            split.add(number % 10);
+            number = number/10;
+        }
+        return split;
     }
 }
