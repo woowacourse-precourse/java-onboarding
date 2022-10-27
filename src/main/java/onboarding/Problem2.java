87c581568c7647bcd8e8f3ae6a50d5eb946b6352
nswon <swnam0729@gmail.com>
2022-10-27 18:52:33 +0900

    feat(Problem2) <initStack>: 스택 초기화 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,12 +1,23 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> characterStack = initStack(cryptogram);
+
+
         return answer;
     }
 
     private static boolean validateSameChar(char char1, char char2) {
         return char1 == char2;
+    }
+
+    private static Stack<Character> initStack(String cryptogram) {
+        Stack<Character> characterStack = new Stack<>();
+        characterStack.push(cryptogram.charAt(0));
+        return characterStack;
     }
 }
