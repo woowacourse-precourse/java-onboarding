f79a8ddff97ac4e875c06eae010f168e296fa323
csj <koreaxman@naver.com>
2022-10-27 13:43:04 +0900

    feat: Create string as one to number entered

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int convertNumber;
+
+        convertNumber = Integer.parseInt(convertCountingNumbersToString(number));
+
+        return convertNumber;
+    }
+
+    public static String convertCountingNumbersToString(int number) {
+        String convertString = "";
+
+        for (int i = 1; i <= number; i++) {
+            convertString += i;
+        }
+
+        return convertString;
     }
 }
