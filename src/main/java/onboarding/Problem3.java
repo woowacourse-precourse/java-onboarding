d96b6ba1ca6af5be858fb0986f7b9c02327852d5
dev-green-flamingo <devgflamingo@gmail.com>
2022-10-27 16:02:16 +0900

    feat: problem3 구현
    - 1부터 number까지들의 숫자들을 순회합니다.
    - 순회하는 숫자의 각 자릿수들의 3의 배수 개수를 계산합니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem3 {
+    private static int clap(int number) {
+        int count = 0;
+        while (number > 0) {
+            int digit = (number % 10);
+            if (digit != 0 && digit % 3 == 0) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
+
+    private static int countClap(int number) {
+        int count = 0;
+        for (int i = number; i > 0; i--) {
+            count += clap(i);
+        }
+        return count;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return countClap(number);
     }
 }
