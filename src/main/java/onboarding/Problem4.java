b157c2906d61e35114e4ef863b93215676951a64
kang jihyeong <her4386@gmail.com>
2022-10-27 23:47:47 +0900

    Add / Problem3, Problem4 요구사항 개발 / 테스트 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i=0; i<word.length(); i++){
+            char chr = word.charAt(i);
+
+            if(chr >= 'a' && chr <= 'z')
+                stringBuilder.append(reverseSmall(chr));
+            else if(chr >= 'A' && chr <= 'Z')
+                stringBuilder.append(reverseBig(chr));
+            else
+                stringBuilder.append(chr);
+        }
+        answer = stringBuilder.toString();
+
         return answer;
     }
 
