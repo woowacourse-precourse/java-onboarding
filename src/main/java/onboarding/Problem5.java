b0da67489f433d05afeb56d33ebf9fd0eab11c2b
jthugg <jthug@naver.com>
2022-10-27 22:14:27 +0900

    todo list 수정
    todo list 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,8 +7,10 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         // 액면가가 큰 화폐 순으로 최대한 많이 담기
         // 오만원권, 만원권, 오천원권, 천원권, 오백원동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 순
-        // 액면가 별 메서드 생성, 화폐 수량 리턴
+        // 메서드가 카운트 값을 매개변수로 받아오면 카운트 숫자에 따른 액면가 조정
+        // 길이가 2인 int 배열을 리턴 0번 인덱스에는 화폐 수량, 1번 인덱스에는 화폐로 변환 뒤 남은 금액
         // 리턴 받은 수량은 제출 할 답안 리스트에 추가
+        // 리턴 받은 남은 금액은 money 변수에 다시 저장
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
