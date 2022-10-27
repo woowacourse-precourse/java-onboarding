367dc7aa362640df3513189ee2ce7045fd7b411d
hyun6ik <fkanwm@naver.com>
2022-10-27 15:50:03 +0900

    feat(24) : [기능 3] : Dy 배열 생성 및 점화식 구하기 위한 preProcess과정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+
+    static int[] Dy;
+    static int answer;
+
+    static void preProcess() {
+        Dy[1] = 0;
+        Dy[2] = 0;
+        Dy[3] = 1;
+        Dy[4] = 1;
+        Dy[5] = 1;
+        Dy[6] = 2;
+        Dy[7] = 2;
+        Dy[8] = 2;
+        Dy[9] = 3;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
+        Dy = new int[number + 1];
         return answer;
     }
 }
