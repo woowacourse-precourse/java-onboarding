b6bbfc376da59d8d59305611586ea8bf728a7612
bellljun3395 <195850@jnu.ac.kr>
2022-10-27 23:33:45 +0900

    feat : Input 클래스 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+}
+
+class Input {
+    static String inputCryptogram() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+
 }
