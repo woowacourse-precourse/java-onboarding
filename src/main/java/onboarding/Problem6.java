cfdce6f6be51852d3a751dc60e8128e8105f8387
안광민 <creativeduck0518@gmail.com>
2022-10-27 21:30:23 +0900

    feat(Problem 6): 두 닉네임에 중복 문자가 있는지 판별하는 기능  구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,18 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static boolean isDuplicate(String subject, String target) {
+        String word;
+
+        for (int i = 0; i < subject.length() - 1; i++) {
+            word = subject.substring(i, i + 2);
+
+            if (target.contains(word)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
