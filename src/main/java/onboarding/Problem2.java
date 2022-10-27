dd014bccb9b10c3000b0911bb4780cf71fce2b32
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-10-27 15:09:22 +0900

    feat : 중복 문자 찾기 & 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        List<Character> crypList = initList(cryptogram);
+        List<Character> tmpList = new ArrayList<>(crypList);
+
+        while (true) {
+            int size = crypList.size();
+            for (int i = 0; i < crypList.size() - 1; i++) {
+                if (crypList.get(i) == crypList.get(i + 1)) {
+                    tmpList.set(i, '*');
+                    tmpList.set(i + 1, '*');
+                }
+            }
+            tmpList.removeAll(Collections.singleton('*'));
+            crypList = new ArrayList<>(tmpList);
+            if (size == crypList.size()) break; // 변화가 없었다면 종료
+        }
+
+        return getAnswerStr(crypList);
     }
+
+    private static String getAnswerStr(List<Character> crypList) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < crypList.size(); i++) {
+            answer.append(crypList.get(i));
+        }
+        return answer.toString();
+    }
+
+    private static List<Character> initList(String str) {
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
+        }
+        return list;
+    }
+
 }
