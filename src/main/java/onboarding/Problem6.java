e4e460375ece0e1ee7d2574b707ff0251c4d9668
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-10-27 13:15:08 +0900

    6번문제 해결 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,71 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        /*
+        int cnt=0;
+        String pattern = "^[ㄱ-ㅎ가-힣]*$";
+        while(cnt<forms.size()){
+            String mail = forms.get(cnt).get(0);
+            String nickname = forms.get(cnt).get(1);
+            String [] tmp = mail.split("@");
+            if(!tmp[1].equals("email.com")||!Pattern.matches(pattern, nickname)) {
+                forms.remove(cnt);
+            }
+            else cnt++;
+        }
+        */
+
+        List<String> nickName = new ArrayList<>();
+
+        for(int i=0;i<forms.size();i++){
+            nickName.add(forms.get(i).get(1));
+        }
+
+        //nickName에서 서로 비교해야함
+        List<Integer>list = new ArrayList<>();
+
+        for (int i = 0; i < nickName.size() - 1; i++) {
+            if (list.contains(i)||nickName.get(i).length()==1) continue;
+            for (int j = i + 1; j < nickName.size(); j++) {
+                if(nickName.get(j).length()==1) continue;
+                if (nickName.get(i).length() == 2) {
+                    if (nickName.get(j).contains(nickName.get(i))) {
+                        if(!list.contains(i))
+                        list.add(i);
+                        if(!list.contains(j))
+                        list.add(j);
+                    }
+                } else {
+                    for (int k = 0; k < nickName.get(i).length() - 1; k++) {
+                        String idx = nickName.get(i).substring(k, k + 2);
+                        if (nickName.get(j).contains(idx)) {
+                            if(!list.contains(i))
+                            list.add(i);
+                            if(!list.contains(j))
+                            list.add(j);
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<list.size();i++){
+            answer.add(forms.get(list.get(i)).get(0));
+        }
+        Collections.sort(answer);
+
+
+
+
+
+
         return answer;
     }
 }
