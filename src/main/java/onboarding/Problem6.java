5fe7a3932395723d57f896b0034a01dd5600f9f1
HyunggiPark <phg9587@naver.com>
2022-10-27 23:39:59 +0900

    feat : 닉네임에서 연속적으로 연결된 문자를 뽑아내고 해당 문자가 어디에서 비롯된 닉네임인지 확인하기 위해서 duplicateSubNick 내부 클래스 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,5 +6,21 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    static class DuplicateSubNick {
+        String email;
+        String subStrNick;
+
+        public DuplicateSubNick(String email, String subStrNick) {
+            this.email = email;
+            this.subStrNick = subStrNick;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getSubStrNick() {
+            return subStrNick;
+        }
     }
 }
