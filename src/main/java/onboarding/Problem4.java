1e560527730ff30761a30ba50800d2c6537de7c7
d0ri123 <lsh81550@gmail.com>
2022-10-27 23:43:37 +0900

    입력받은 문자를 아스키코드를 통해 장난으로 바꾸는 fun 함수 기능 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = fun(word);
         return answer;
+    }
+
+    //TODO:fun함수 기능목록 정의(말 다듬기)
+    public static String fun(String word){
+        char[] arr = word.toCharArray();
+        String str = "";
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>=65 && arr[i]<=90){
+                str += (arr[i] = (char)(155-arr[i]));
+            }else if(arr[i]>=97 && arr[i]<=122){
+                str += (arr[i] = (char)(219-arr[i]));
+            }else if(arr[i]==32){
+                str+=" ";
+            }
+        }
+        return str;
     }
 }
