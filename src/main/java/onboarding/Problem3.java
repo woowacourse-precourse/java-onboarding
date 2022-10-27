024964ae9925e74b18c0eae7ac1e7a7a9eb89693
baexxbin <soo3886@gmail.com>
2022-10-28 02:23:33 +0900

    Feat(PROBLEM3): "각 숫자의 박수횟수 카운트 구현"
    countClap() 함수작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int countClap(int index) {
+		int count = 0;
+		while (index > 0) {
+			if (index % 10 == 3 || index % 10 == 6 || index % 10 == 9) {
+				count++;
+			}
+			index /= 10;
+		}
+		return count;
+	}
+
+	public static int solution(int number) {
+		int answer = 0;
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int number = 13;
+		System.out.println(solution(number));
+	}
 }
