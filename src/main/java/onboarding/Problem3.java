9dbc5aae118787f2a2f430ed197d5325363dcf7f
Taehwan2 <taehwansw@gmail.com>
2022-10-27 15:41:07 +0900

    test

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+       answer=(int) IntStream.rangeClosed(1,number).filter(i->i%10==3||i%10==6||i%10==9).count()+
+                (int)IntStream.rangeClosed(1,number).filter(i->i/10==3||i/10==6||i/10==9).count();
         return answer;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,23 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        int arr[] = {50000,10000,5000,1000,500,100,50,10,1};
         List<Integer> answer = Collections.emptyList();
+        int i=0;
+        while(money!=0){
+            answer.add(money/arr[i]);
+            money%=arr[i];
+            i++;
+        }
+
+        if(answer.size()!=arr.length){
+            for(int j=answer.size();j<arr.length;j++){
+                answer.add(0);
+            }
+        }
+
+
+
         return answer;
     }
 }
