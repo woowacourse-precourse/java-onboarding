b6e29978787aa4fd10bf2fca297bde6988e0f791
sugamypapa <sk000801@naver.com>
2022-10-27 16:13:02 +0900

    chore: 문제 5 아이디어 주석

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    // 처음 아이디어
+    // 금액/단위>0이라면 이 몫이 변환되는 개수
+    // 높은 단위부터 시작하여 개수가 구해지면 그 개수*금액만큼 빼서 계산을 돌릴 것!
+    
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
