66cb1a9f024d93deb0e4fbd32f90b2eda34702ee
Ilchul Kim <huj0159@gmail.com>
2022-10-28 04:55:42 +0900

    feat(Problem1.java) adding TwoPage check method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,12 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-
-
         return answer;
+    }
+
+    public static boolean isTwoPages(List<Integer> page){
+        if(page.size()!=2)
+            return false;
+        return true;
     }
 }
