2d0e672a9fa81a6fd7c67c4461cf09dccc34eb31
alstjr706 <alstjr706@naver.com>
2022-10-28 02:22:32 +0900

    feat : (369 게임) 369 반복 구현

--- src/main/java/onboarding/problem3/IterativeGenerator.java
+++ src/main/java/onboarding/problem3/IterativeGenerator.java
@@ -4,7 +4,15 @@ public class IterativeGenerator {
 
     public int typeConversion(int number) {
 
+        CheckNumbers checkNumbers = new CheckNumbers();
 
-        return 0;
+        int compare = 0;
+
+        for (int numberIndex = 1; numberIndex <= number; numberIndex++) {
+            String strNumber = String.valueOf(numberIndex);
+            compare = checkNumbers.compare(strNumber);
+        }
+
+        return compare;
     }
 }
