196b7bf013554f9a412055ff41da89129797fa77
jeongwle <dldl4568@naver.com>
2022-10-27 20:25:55 +0900

    [Feat] Problem3 제한사항 검증하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        if (!isValidate(number)) {
+            return -1;
+        }
         int answer = 0;
         return answer;
+    }
+
+    public static boolean isValidate(int number) {
+        return (number > 0 && number < 10001);
     }
 }
