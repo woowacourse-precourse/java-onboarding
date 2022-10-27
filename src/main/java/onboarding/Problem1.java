d37859c692c5455ee79d82723d70796e9f439370
HyunggiPark <phg9587@naver.com>
2022-10-27 16:34:09 +0900

    feat : pageList를 받아서 페이지가 정상적인 페이지인지 확인 후 왼쪽 페이지 와 오른쪽 페이지 중에 더 큰 수를 구하는 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,20 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    private static int getMaxNumByPageList(List<Integer> pageList) {
+        int leftPage = pageList.get(0);
+        int rightPage = pageList.get(1);
+
+        if ((rightPage == leftPage + 1) && isRightPageNum(leftPage) && isRightPageNum(rightPage)) {
+            return Math.max(getMaxNumByPage(leftPage), getMaxNumByPage(rightPage));
+        } else {
+            return -1;
+        }
+    }
+
+    private static boolean isRightPageNum(int page) {
+        return 1 <= page && page <= 400;
+    }
     private static int getMaxNumByPage(int page) {
         String pageStr = String.valueOf(page);
         int maxPlusNum = 0;
