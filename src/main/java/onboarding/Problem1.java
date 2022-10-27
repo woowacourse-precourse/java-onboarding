7138c0bead4c2f457d4c3f8f72807537134da6a3
hyowonhan <wain1719@gmail.com>
2022-10-27 22:55:21 +0900

    문제1 main, solution 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,11 +1,62 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
+
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        List<Integer> pobi_page = new ArrayList<Integer>();
+        List<Integer> crong_page = new ArrayList<Integer>();
         int answer = Integer.MAX_VALUE;
-        return answer;
+
+        if (exception_check(pobi) || exception_check(crong) == -1 {
+            answer = -1;
+        } else{
+            for (int i = 0; i < 2; i++) {
+                int sum = 0;
+                int mul = 1;
+                int num = pobi.get(i);
+                while (num != 0) {
+                    sum += num % 10;
+                    mul *= num % 10;
+                    num /= 10;
+                }
+                pobi_page.add(sum);
+                pobi_page.add(mul);
+            }
+
+            for (int i = 0; i < 2; i++) {
+                int sum = 0;
+                int mul = 1;
+                int num = crong.get(i);
+                while (num != 0) {
+                    sum += num % 10;
+                    mul *= num % 10;
+                    num /= 10;
+                }
+                crong_page.add(sum);
+                crong_page.add(mul);
+            }
+            answer = (Collections.max(pobi_page) > Collections.max(crong_page)) ? 1 : (Collections.max(pobi_page) == Collections.max(crong_page)) ? 0 : 2;
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> pobi = new ArrayList<Integer>();
+        List<Integer> crong= new ArrayList<Integer>();
+
+        Scanner sc = new Scanner(System.in);
+        pobi.add(sc.nextInt());
+        pobi.add(sc.nextInt());
+        crong.add(sc.nextInt());
+        crong.add(sc.nextInt());
+
+        System.out.println(solution(pobi, crong));
     }
 
 }
