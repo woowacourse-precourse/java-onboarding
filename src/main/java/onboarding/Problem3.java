95e94b1d93924d958fc06caa083f8717936f3944
noyes5 <noyes5@naver.com>
2022-10-28 05:10:09 +0900

    problem3 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i=1;i<=number;i++) {
+            StringBuilder sb = new StringBuilder(String.valueOf(i));
+
+            for (int j=0;j<sb.length();j++) {
+                if ((sb.charAt(j) -'0') % 3 == 0) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
