5b4660fe0dd3bf5380a96c352cc49cde978471e8
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-27 16:35:55 +0900

    feat: Problem6 중복된 닉네임의 forms 인덱스 값을 리스트로 반환해주는 메소드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,10 +7,14 @@ public class Problem6 {
     private static final int EMAIL=0;
     private static final int NICKNAME=1;
 
+    private static final boolean YES_DUPLICATE=true;
+    private static final boolean NO_DUPLICATE=false;
+
     public static List<String> solution(List<List<String>> forms) {
 
         Map<String,Integer> nickMap = new HashMap<>();
         mapSetting(forms, nickMap);
+        List<Integer> duplicateUserIndex = duplicateIndex(forms, nickMap);
         List<String> answer =List.of("answer");
         return answer;
     }
@@ -26,5 +30,23 @@ public class Problem6 {
             nickMap.put(tmp,nickMap.getOrDefault(tmp,0)+1);
         }
     }
+    private static List<Integer> duplicateIndex(List<List<String>> forms,Map<String, Integer> nickMap) {
+        List<Integer> duplicateIndex = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            String nowNick = forms.get(i).get(NICKNAME);
+            if (isDuplicate(nowNick, nickMap)) {
+                duplicateIndex.add(i);
+            }
+        }
+        return duplicateIndex;
+    }
+    private static boolean isDuplicate(String now, Map<String, Integer> nickMap){
+        for(int i=0;i<now.length()-1;i++){
+            String tmp = now.substring(i,i+2);
+            if(nickMap.get(tmp)>1)return YES_DUPLICATE;
+        }
+        return NO_DUPLICATE;
+    }
+
 
 }
