c4bb5f6889f26e9fd9fa48c834409389640af466
JHS <wnghdtjr129@naver.com>
2022-10-27 18:48:25 +0900

    feat(Problem6): [문제 6] 닉네임 중복시 결과값에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,16 +1,22 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     static List<String> answer = new ArrayList<>();
-
+    static Set<String> nicknames = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
         for (List<String> crew : forms) {
             if (cannotUser(crew.get(1))) {
-
+                answer.add(crew.get(0));
             }
+        }
+        List<String> firstCrew = forms.get(0);
+        if (cannotUser(firstCrew.get(1))) {
+            answer.add(firstCrew.get(0));
         }
         return answer;
     }
@@ -20,7 +26,10 @@ public class Problem6 {
         for (int i = 0; i < nickname.length() - 1; i++) {
             for (int j = i + 2; j < nickname.length(); j++) {
                 String subNickname = nickname.substring(i, j);
-
+                if (nicknames.contains(subNickname)) {
+                    result = true;
+                }
+                nicknames.add(nickname);
             }
         }
         return result;
