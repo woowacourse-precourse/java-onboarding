e30224a62e4baa49fbf09785fb5137b92e11f4b6
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-27 13:18:19 +0900

    refactor(Problem4.java): initDictionary() 파라미터 제거, 리턴값 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,24 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem4 {
-    public static void initDictionary(Map<Character, Character> dictionary){
-        char upperCase='A';
-        char lowerCase='a';
+    public static Map<Character, Character> initDictionary() {
+        Map<Character, Character> dictionary = new HashMap<>();
+
+        char upperCase = 'A';
+        char lowerCase = 'a';
 
         for (int i = 0; i < 26; i++) {
-            dictionary.put((char)(upperCase+i), (char)(upperCase+25-i));
-            dictionary.put((char)(lowerCase+i), (char)(lowerCase+25-i));
+            dictionary.put((char) (upperCase + i), (char) (upperCase + 25 - i));
+            dictionary.put((char) (lowerCase + i), (char) (lowerCase + 25 - i));
         }
+
+        return dictionary;
     }
 
     public static String solution(String word) {
-        Map<Character, Character> dictionary=new HashMap<>();
-        initDictionary(dictionary);
+        Map<Character, Character> dictionary = initDictionary();
 
-        StringBuilder sb=new StringBuilder(word);
-        for(int i=0; i<sb.length(); i++){
-            char next=sb.charAt(i);
-            if(dictionary.containsKey(next))
+        StringBuilder sb = new StringBuilder(word);
+        for (int i = 0; i < sb.length(); i++) {
+            char next = sb.charAt(i);
+
+            if (dictionary.containsKey(next))
                 sb.setCharAt(i, dictionary.get(next));
         }
 
