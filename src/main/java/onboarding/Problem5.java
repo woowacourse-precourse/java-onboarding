b45bc08c7ca3416f8048b3b18da9a63c03d1e81a
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-27 15:38:45 +0900

    feat: money를 오만 원권부터, 일원 동전까지 순차적으로 돈을 나누는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static int divideMoney(int money) {
+        money %= 50000;
+        money %= 10000;
+        money %= 5000;
+        money %= 1000;
+        money %= 500;
+        money %= 100;
+        money %= 50;
+        money %= 10;
+        money %= 1;
+        return money;
     }
 }
