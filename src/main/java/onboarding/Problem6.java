70a364ec1db009f868e3a2e36f7be3f02ea0ec9d
juhwang <juhong4930@naver.com>
2022-10-27 16:46:28 +0900

    feat(Problem6):extracting only nickName in forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    public static List<String> extractNickName(List<List<String>> forms){
+        List<String> nickNameList = new ArrayList<>();
+        forms.forEach(form -> nickNameList.add(form.get(1)));
+        return nickNameList;
     }
 }
