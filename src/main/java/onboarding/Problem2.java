c3221a4d98bbe787fec051585963f28f7a801f90
jaehyeon <jhjh0022@naver.com>
2022-10-27 21:10:29 +0900

    다시 생각

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,23 +1,46 @@
 package onboarding;
+import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        char[] ch = cryptogram.toCharArray();
+        while(check(ch)){
+            Stack<Character> decodeStack = dremove(ch);                       //?
+
+        }
         return answer;
     }
-    public static String decoder(String phar){
-        String sub ="";
-        String dec = phar;
 
-        for(int i=0; i<phar.length()-1; i++ ){
-
-                if(phar.charAt(i) == phar.charAt(i+1)){
-                    sub += phar.charAt(i);
-                    sub += phar.charAt(i+1);
-                    dec.strip(sub);
-                    sub ="";
-                }
+    public static boolean check(char[] num){                                  // 중복 있는지 없는지 체크
+        for(int i =0; i<num.length -1; i++){
+            if(num[i] == num[i+1]){
+                return true;
             }
         }
+        return false;
     }
+
+
+    public static String dremove(char[] nu) {
+        char decode = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : nu) {
+
+            if (decode != c) {
+                stack.push(c);
+                decode = c;
+            } else if (decode == stack.peek() ) {                      //stack.peek()  제일 상단에 있는 값을 빼지 않고 반환
+                stack.pop();
+            }
+
+        }
+        return stack;
+
+
+    }
+
+
+
+
 }
