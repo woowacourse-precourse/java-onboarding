d59ddff792eca1303ca12b85d85d7b9adeb245f5
2jun0 <soo28819@gmail.com>
2022-10-28 01:59:28 +0900

    Feat: 문제 7번 - 추천 점수 구하는 메서드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    public static int getRecommandScore(int cntOfSameFriendsWithMe, int cntOfVisit) {
+        return cntOfSameFriendsWithMe * 10 + cntOfVisit;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
