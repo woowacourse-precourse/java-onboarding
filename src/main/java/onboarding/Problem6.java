b0ffc091050b804aef10b1bf7253b143fb7999da
sieun <sieun960513@naver.com>
2022-10-27 21:22:26 +0900

    [Problem6] : 크루 크기 제한사항 체크 [신규]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,14 +4,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /** 기능 목록
- * solutionLogic        : 메인 솔루션
+ *
+ *
+ * [CreList Collection]
+ * addCrew              : 크루 추가
  * checkValid           : 예외 처리
  * checkCrewNums        : 크루 크기 유효성 체크
+ * checkEmail           : 이메일 유효성 체크
+ * checkNickName        : 닉네임 유효성 체크
+ * checkAlarmTarget     : 알림 보내줄 대상인지 체크
  * checkEmailForm       : 이메일 형식 유효성 체크
  * checkEmailLength     : 이메일 길이 유효성 체크
  * checkEmailDomain     : 이메일 도메인 체크
+ * checkEmailId         : 이메일 아이디 체크
  * checkNickNameType    : 닉네임 한글 타입 체크
  * checkNickNameLength  : 닉네임 길이 확인
  */
@@ -26,16 +34,25 @@ public class Problem6 {
         private final Map<String, String> crewList = new HashMap<>();
         private final List<String> alarm = new ArrayList<>();
 
-        private void addCrew() {
-
+        private boolean addCrew(List<String> form) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            if (checkValid(email, nickname)) {
+                crewList.put(email, nickname);
+            } else {
+                System.out.println("올바르지 않은 형식");
+                return false;
+            }
+            return checkCrewNums();
         }
 
-        private boolean checkValid() {
-            return
+        private boolean checkValid(String email, String nickname) {
+            return checkEmail(email) && checkNickName(nickname);
         }
 
         private boolean checkCrewNums() {
-
+            int crewSize = crewList.size();
+            return 1 <= crewSize && crewSize <= 10000;
         }
 
         private boolean checkEmail(String email) {
@@ -79,12 +96,13 @@ public class Problem6 {
             return true;
         }
 
-
         private boolean checkNickNameType(String nickname) {
-
+            return nickname.matches("^[가-힣]*$");
         }
-        private boolean checkNickNameLength(String nickname) {
 
+        private boolean checkNickNameLength(String nickname) {
+            int len = nickname.length();
+            return 1<= len && len < 20;
         }
     }
 }
