b31f63bead3e46b9794f636b0a034c32f46ea3f6
dev_kong <ahdjd5@gmail.com>
2022-10-27 11:37:14 +0900

    feat(Problem5): solve Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    public static final int[] MONEY_UNIT_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for (int unit : MONEY_UNIT_LIST) {
+            answer.add(money / unit);
+            money %= unit;
+        }
         return answer;
     }
 }
