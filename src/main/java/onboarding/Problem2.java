2618dff004cd565dace0ff05d3d9d2b1660962d5
Identityless <rlandwnd555@naver.com>
2022-10-27 22:12:24 +0900

    FEAT : 검사 기능 추상화한 인터페이스 Detector 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.List;
+
+/*
+*   구현 기능
+*   1. 중복 패턴 검사 기능
+*   2. 중복 패턴 제거 기능
+*/
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+}
+
+interface Detector {
+    List<Integer> location(String cryptogram);
 }
