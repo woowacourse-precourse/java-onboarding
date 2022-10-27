2da402650054108262b1b4a29acebd12551bbce9
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-10-27 11:43:02 +0900

    [feat] Problem2: 같은 문자를 특정 문자로 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Problem2 {
+
+    public static final String SPECIFIC_VALUE = "-";
+
     public static String solution(String cryptogram) {
         String answer = cryptogram;
         return answer;
@@ -14,5 +17,16 @@ public class Problem2 {
         return set.size() != array.length;
     }
 
+    private static void convertSpecificValue(String[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            String firstString = array[i];
+            String nextString = array[i + 1];
+
+            if(firstString.equals(nextString)) {
+                array[i] = SPECIFIC_VALUE;
+                array[i + 1] = SPECIFIC_VALUE;
+            }
+        }
+    }
 
 }
