de09ae3452ad31329d9eddce6178622ea71f8dc0
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-27 16:24:59 +0900

    Feat : checkException 예외사항인지 검사하는 메서드를 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,6 +10,7 @@ class Problem1 {
     * 3. 각 자리 숫자를 곱하는 메서드 mulValue
     * 4. 왼쪽 페이지와 오른쪽 페이지의 각 자리 숫자를 더하고 곱하고 비교하여 가장 큰수를 리턴하는 메서드 getMaxValue
     * 5. 점수를 비교하여 승자를 정하는 메서드 whoIsWinner
+    * 6. 예외사항인지 검사하는 메서드 checkException
     * */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
@@ -35,7 +36,14 @@ class PageNumGame {
     }
     // 각 자리 숫자를 곱하는 메서드
     int mulValue(int value) {
-        int res = (value % 10) * (value % 100 / 10) + (value / 100);
+        int res;
+        if(value >= 100) {
+            res = (value % 10) * (value % 100 / 10) * (value / 100);
+        } else if(value >= 10) {
+            res = (value % 10) * (value % 100 / 10);
+        } else {
+            res = (value % 10);
+        }
         return res;
     }
 
@@ -50,6 +58,10 @@ class PageNumGame {
     // 점수를 비교하여 승자를 정하는 메서드 whoIsWinner
     int whoIsWinner() {
         int answer = -1;
+
+        if(checkException())
+            return answer;
+
         int gamer1Point = getMaxValue(gamer1.get(0), gamer1.get(1));
         int gamer2Point = getMaxValue(gamer2.get(0), gamer2.get(1));
         if(gamer1Point > gamer2Point) {
@@ -60,6 +72,33 @@ class PageNumGame {
             answer = 0;
         }
         return answer;
+    }
+
+    // 예외 사항
+    boolean checkException(){
+        // 왼쪽 페이지가 홀수, 오른쪽 페이지가 짝수 인지 검증
+        if(gamer1.get(0) % 2 == 0 || gamer1.get(1) % 2 == 1) {
+            return true;
+        }
+        if(gamer2.get(0) % 2 == 0 || gamer2.get(1) % 2 == 1) {
+            return true;
+        }
+
+        // 인접한 페이지인지 검증
+        if(gamer1.get(1) - gamer1.get(0) != 1) {
+            return true;
+        }
+        if(gamer2.get(1) - gamer2.get(0) != 1) {
+            return true;
+        }
+
+        // 시작 면이나 마지막 면이 나오는지 검증
+        if(gamer1.get(0) == 1 || gamer1.get(1) == 400
+                || gamer2.get(0) == 1 || gamer2.get(1) == 400) {
+            return true;
+        }
+
+        return false;
     }
 
 }
