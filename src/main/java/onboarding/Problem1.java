75bbc0259cb9ff3cf54bf8604837190d4214157c
xxeol2 <jhj01177@naver.com>
2022-10-27 16:30:19 +0900

    feat(prob1): Problem1, GameController 클래스 연결

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+import onboarding.problem1.GameController;
+
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		GameController gameController = new GameController(pobi, crong);
+		int answer = gameController.playGame();
+		return answer;
+	}
 }
