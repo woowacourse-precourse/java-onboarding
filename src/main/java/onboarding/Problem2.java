8208d31ec2c5d50a84058171861f713b46ce4093
sieun <sieun960513@naver.com>
2022-10-27 16:39:24 +0900

    [Problem2] :(solution) 로직 완성 [신규]

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,13 +12,15 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-
-
-        return answer;
+        final String errorCase ="-1";
+        if (!checkValid(cryptogram)) {
+            return errorCase;
+        }
+        return stackToString(solutionLogic(cryptogram));
     }
 
-    public static Stack<Character> solutionLogic(char[] charArray) {
+    public static Stack<Character> solutionLogic(String s) {
+        char[] charArray = s.toCharArray();
         Stack<Character> stack = new Stack<>();
         for (char c : charArray) {
             if (stack.isEmpty() || stack.peek() != c) {
@@ -30,23 +32,26 @@ public class Problem2 {
         return stack;
     }
 
+    public static String stackToString(Stack<Character> stack) {
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+
     public static boolean checkValid(String s) {
         return checkLength(s) && checkLowerCase(s);
     }
 
     public static boolean checkLength(String s) {
         int len = s.length();
-        if (1 <= len && len <= 1000) {
-            return true;
-        }
-        return false;
+        return 1 <= len && len <= 1000;
     }
 
     public static boolean checkLowerCase(String s) {
-        if (s.equals(s.toLowerCase())) {
-            return true;
-        }
-        return false;
+        return s.equals(s.toLowerCase());
     }
 
 }
