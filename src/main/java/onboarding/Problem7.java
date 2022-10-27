b6408fc7c59c9dd51cb1b3bb8a5b523415553167
apptie <appti@naver.com>
2022-10-28 00:47:07 +0900

    feat: 문제 7 회원의 아이디, 친구 관계 정보, 추천 점수를 표현하는 Account 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+}
+
+class Account {
+    private final String id;
+    private final Set<Account> friendsRelation;
+    private int score;
+
+    public Account(String id) {
+        this.id = id;
+        this.friendsRelation = new HashSet<>();
+        this.score = 0;
     }
 }
