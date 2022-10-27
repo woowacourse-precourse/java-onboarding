69b57fccda2373bdc187b63970ebbdff9a0d70a9
Kimhyeyun <cqqwer@gmail.com>
2022-10-27 17:51:31 +0900

    [Problem 5] MAKE  큰 잔돈분터 반환하기 FUNCTION

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        int[] ans = new int[9];
+        while (money != 0) {
+            for (int i = 0; i < units.length; i++) {
+                if(money >= units[i]){
+                    ans[i] += (money / units[i]);
+                    money %= units[i];
+                }
+            }
+        }
+
+        List<Integer> answer = new ArrayList<Integer>(Arrays.stream(ans).boxed().collect(Collectors.toList()));
+
         return answer;
     }
 }
