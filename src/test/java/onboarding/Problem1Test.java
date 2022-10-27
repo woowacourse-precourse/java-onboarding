61a7f79eba3be59c15b1bb321bdc3c92f08e7a50
saehim <saeilee.vxv@gmail.com>
2022-10-27 19:43:29 +0900

    Test: 문제1 자리수 반환 함수

--- src/test/java/onboarding/Problem1Test.java
+++ src/test/java/onboarding/Problem1Test.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package onboarding;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class Problem1Test {
-  
+
+    @Test
+    void 자리수() {
+        List<Integer> list = Arrays.asList(7,9);
+        assertThat(jarisoo(97)).isEqualTo(list);
+    }
+    static List<Integer> jarisoo(int num) {
+        List<Integer> numArr = new ArrayList<Integer>();
+        while(num > 0){
+            numArr.add(num % 10);
+            num /= 10;
+        }
+        return numArr;
+    }
 }
