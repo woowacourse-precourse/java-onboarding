df3fe953c761c4006aae896e5b9dafd629ba1a95
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 14:09:53 +0900

    문제 3 : 기능 구현 사항 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ * [기능 구현]
+ * 1. 주어진 숫자의 각 자릿수의 수 중 3,6,9의 개수를 세는 메소드
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
