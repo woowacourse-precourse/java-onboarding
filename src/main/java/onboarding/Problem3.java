e77af2a408d424f86c125b57492a0f2368522b97
5nam <ohnam90@gmail.com>
2022-10-27 22:50:26 +0900

    feat: 각 숫자마다 3, 6, 9 가 몇 번 들어가는지 판단하고, 그 개수를 리턴하는 기능 추가 두 자리 이상 숫자일 때 1. 그 숫자를 분리하고 2. for 문으로 분리한 숫자에 3, 6, 9 가 몇 번 들어가는 카운트하고 그 개수 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -26,4 +26,24 @@ public class Problem3 {
 
         return digits;
     }
+
+    // 각 숫자마다 3, 6, 9 가 몇 번 들어가는지 판단하고, 그 개수를 리턴하는 함수
+    private static int judgeNumber(int input) {
+        // 한 자리수 숫자일 때 판단
+        if(input < 10 && (input == 3 || input == 6 || input == 9)) {
+            return 1;
+        }
+
+        // 두 자리 이상 숫자일 때 판단
+        int[] doubleDigits;
+        int count = 0;
+        doubleDigits = doubleSplit(input);
+        for(int i = 0; i<doubleDigits.length; i++) {
+            if(doubleDigits[i] == 3 || doubleDigits[i] == 6 || doubleDigits[i] == 9) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
