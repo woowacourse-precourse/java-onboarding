00561da94a54726c1e1ae846e752ad6d7df044d5
viyott <jackk0616@gmail.com>
2022-10-27 13:49:28 +0900

    2 처음 길이와 비교해서 다르면 while 탈출

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,8 @@ public class Problem2 {
         char prev = ' ';
         while(true) {
             String temp = cryptogram.substring(0, 1);
-            for(int i = 1; i < cryptogram.length(); i++) {
+            int len = cryptogram.length();
+            for(int i = 1; i < len; i++) {
                 // 2. 이전 글자와 현재 글자가 같다면 temp 뒤에 이미 들어간 문자도 잘라준다.
                 if (prev == cryptogram.charAt(i)) {
                     temp = temp.substring(0, temp.length() - 1);
@@ -17,7 +18,11 @@ public class Problem2 {
                 prev = cryptogram.charAt(i);
             }
             cryptogram = temp;
-            if()
+            // 3. 같은 글자가 다 지워져서 없거나 조작 전 후의 길이가 같으면 while 탈출
+            if(cryptogram.length() == 0 || len == cryptogram.length()) {
+                answer = cryptogram;
+                break;
+            }
         }
         return answer;
     }
