a7d08901aad374f59e4dc67cab42838081d25af7
Kimhyeyun <cqqwer@gmail.com>
2022-10-27 17:14:29 +0900

    [Problem 2] MAKE CHECK DUPLICATE FUNCTION

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,28 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        boolean isDuplicated = false;
+        while (true) {
+            for (int i = 0; i < cryptogram.length(); i++) {
+                int duplicateCount = checkDuplicate(cryptogram, i);
+            }
+            if(!isDuplicated) break;
+        }
+
         return answer;
+    }
+
+    private static int checkDuplicate(String cryptogram, int idx) {
+        char c = cryptogram.charAt(idx);
+        int cnt = 1;
+        for (int i = idx + 1; i < cryptogram.length(); i++) {
+            if(c == cryptogram.charAt(i)) cnt += 1;
+            else {
+                if(i == idx) cnt = -1;
+                break;
+            }
+        }
+        return cnt;
     }
 }
