d3b38013384ac7fb591be2c0b25de4063943ce5a
HyunggiPark <phg9587@naver.com>
2022-10-27 16:32:01 +0900

    feat : page 번호의 각 자리 숫자를 모두 더하거나, 모두 곱한 수 중 가장 큰 수를 구하는 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,5 +6,17 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    private static int getMaxNumByPage(int page) {
+        String pageStr = String.valueOf(page);
+        int maxPlusNum = 0;
+        int maxMultiplyNum = 1;
+        for (int i = 0; i < pageStr.length(); i++) {
+            int tempNum = pageStr.charAt(i) - '0';
+
+            maxPlusNum += tempNum;
+            maxMultiplyNum *= tempNum;
+        }
+
+        return Math.max(maxPlusNum, maxMultiplyNum);
     }
 }
