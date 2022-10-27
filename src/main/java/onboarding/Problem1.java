b058f519dce864f5500dad7f226f8cc26217164a
swhyeon98 <smreo75@gmail.com>
2022-10-28 02:21:09 +0900

    Problem1, Problem2 코드 서식 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,53 +4,53 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(errorCheck(pobi) || errorCheck(crong)) {
+        if (errorCheck(pobi) || errorCheck(crong)) {
             return -1;
         }
 
-        int pobiMax = compareLeftRight(pobi.get(0),pobi.get(1));
-        int crongMax = compareLeftRight(crong.get(0),crong.get(1));
-        int answer = getWinner(pobiMax,crongMax);
+        int pobiMax = compareLeftRight(pobi.get(0), pobi.get(1));
+        int crongMax = compareLeftRight(crong.get(0), crong.get(1));
+        int answer = getWinner(pobiMax, crongMax);
 
         return answer;
     }
 
-    private static boolean errorCheck (List<Integer> pages) {
+    private static boolean errorCheck(List<Integer> pages) {
         if (pages.get(0) == null || pages.get(1) == null) {
             return true;
         }
 
-        return pages.get(1)-pages.get(0) != 1;
+        return pages.get(1) - pages.get(0) != 1;
     }
 
-    private static int getMaxPage(int page){
+    private static int getMaxPage(int page) {
         int num = 0;
         int sum = 0;
         int mul = 1;
 
-        while (page > 0){
-            num = page%10;
+        while (page > 0) {
+            num = page % 10;
             sum += num;
             mul *= num;
             page /= 10;
         }
 
-        return Math.max(sum,mul);
+        return Math.max(sum, mul);
     }
 
-    private static int compareLeftRight(int leftPage, int rightPage){
+    private static int compareLeftRight(int leftPage, int rightPage) {
         int left = getMaxPage(leftPage);
         int right = getMaxPage(rightPage);
 
-        return Math.max(left,right);
+        return Math.max(left, right);
     }
 
-    private static int getWinner(int pobiScore, int crongScore){
+    private static int getWinner(int pobiScore, int crongScore) {
         if (pobiScore > crongScore) {
             return 1;
         }
 
-        if (pobiScore < crongScore){
+        if (pobiScore < crongScore) {
             return 2;
         }
 
