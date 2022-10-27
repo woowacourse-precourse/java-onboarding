7553fd7c850e353de1a936b5b553662b00cf0a26
dev_kong <ahdjd5@gmail.com>
2022-10-27 16:24:24 +0900

    feat(Problem7): add method generateFriendList to get friend list from friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Set<String> friendList = generateFriendList(friends);
+
         return answer;
+    }
+
+    private static Set<String> generateFriendList(List<List<String>> friends){
+        Set<String> friendList = new HashSet<>();
+        for (List<String> friend : friends) {
+            friendList.add(friend.get(0));
+        }
+        return friendList;
     }
 }
