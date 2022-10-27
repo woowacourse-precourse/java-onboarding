ae1cb8dad158c2bea35330d2b4b45a5038a9bf95
맹순영 <symaeng98@naver.com>
2022-10-27 19:45:01 +0900

    feat: 크롱 점수 계산 기능 구현
    크롱의 페이지 합, 곱을 통한 점수 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,6 +11,14 @@ class Problem1 {
         int pobiMul = pobi.get(0) * pobi.get(1);
         // 포비의 점수 계산
         int pobiScore = Math.max(pobiSum, pobiMul);
-        return 1;
+
+        // ----- 크롱의 점수 계산 -----
+        // 크롱의 페이지 합 계산
+        int crongSum = crong.get(0) + crong.get(1);
+        // 크롱의 페이지 곱 계산
+        int crongMul = crong.get(0) * crong.get(1);
+        // 크롱의 점수 계산
+        int crongScore = Math.max(crongSum, crongMul);
+        return 2;
     }
 }
