ca600c4988625ec6db417cc054fc28a0f9fca466
suheonjoo <suheonj95@naver.com>
2022-10-27 20:25:38 +0900

    feat(Problem2): 메인 클래스 로직 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder cryptogramStringBuilder = new StringBuilder(cryptogram);
+
+        while (true) {
+            int length = cryptogramStringBuilder.length();
+            checkConsecutiveString(cryptogramStringBuilder);
+            if (length == cryptogramStringBuilder.length()) {
+                break;
+            }
+        }
+
+        return cryptogramStringBuilder.toString();
     }
+
+    private static void checkConsecutiveString(StringBuilder stringBuilder) {
+        for (int i = 0; i < stringBuilder.length() - 1; i++) {
+            if (stringBuilder.charAt(i) == stringBuilder.charAt(i + 1)) {
+                stringBuilder.delete(i, i + 2);
+                break;
+            }
+        }
+    }
+
 }
