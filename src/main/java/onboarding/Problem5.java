3313075b6eb8435137226af26453bd0b3a41d15e
MIN-04 <kimji_0424@naver.com>
2022-10-27 21:01:01 +0900

    update Problem5 기능2 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,11 +10,16 @@ public class Problem5 {
      * 2. 금액을 배열 값으로 나눈 결과값인 몫을 결과 배열에 담기
      */
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new java.util.ArrayList<>(Collections.emptyList());
 
         // 1.
         int[] bank = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        // 2.
+        for (int i : bank) {
+            answer.add(money / i);
+            money %= i;
+        }
 
         return answer;
     }
