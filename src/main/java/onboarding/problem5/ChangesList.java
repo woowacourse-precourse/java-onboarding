b5450ec248beb24b01ece316c5cdcd7ac148839e
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 20:05:25 +0900

    feat(P5): 기능 구현을 위한 객체 생성

---
+++ src/main/java/onboarding/problem5/ChangesList.java
@@ -0,0 +1,13 @@
+package onboarding.problem5;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ChangesList {
+
+    private final List<Integer> changesList = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
+
+    public List<Integer> getChangesList() {
+        return changesList;
+    }
+}
---
+++ src/main/java/onboarding/problem5/ValidationMoney.java
@@ -0,0 +1,7 @@
+package onboarding.problem5;
+
+import java.util.List;
+
+public class ValidationMoney {
+
+}
