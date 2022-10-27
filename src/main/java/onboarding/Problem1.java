b0007c553275e5b1c23292131fc6b14b3a24f9ab
Eunkeee <eunkeeeee@gmail.com>
2022-10-27 14:13:23 +0900

    ✨feat: 포비의 최대값 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,16 +6,39 @@ import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int[] pobiValues = pobi.stream().mapToInt(i->i).toArray();
-        for (int pobiValue: pobiValues){
-            int[] arrNum = Stream.of(String.valueOf(pobiValue).split("")).mapToInt(Integer::parseInt).toArray();
-            System.out.println(Arrays.toString(arrNum));
+        int[] pobiResArr = new int[2];
+        int[] pobiArr = pobi.stream().mapToInt(i->i).toArray();
+        int index =0;
+        for (int pobiValue: pobiArr){
+            int[] pobiNum = Stream.of(String.valueOf(pobiValue).split("")).mapToInt(Integer::parseInt).toArray();
+            int pobiAdd = 0;
+            int pobiMult = 1;
+            for (int num: pobiNum){
+                pobiAdd += num;
+                pobiMult *= num;
+            }
+            if(pobiAdd>pobiMult){
+                pobiResArr[index]=pobiAdd;
+            }else{
+                pobiResArr[index]=pobiMult;
+            }
+            index = index+ 1;
         }
-        int[] crongValues = crong.stream().mapToInt(i->i).toArray();
-        for(int crongValue : crongValues){
-            int[] arrNum = Stream.of(String.valueOf(crongValue).split("")).mapToInt(Integer::parseInt).toArray();
-            System.out.println(Arrays.toString(arrNum));
+        System.out.println(Arrays.toString(pobiResArr));
+        int pobiMax = pobiResArr[0];
+        for(int i=1 ; i<pobiResArr.length ; i++) {
+            if(pobiResArr[i]>pobiMax) {
+                pobiMax = pobiResArr[i];
+            }
         }
+        System.out.println(pobiMax);
+
+
+//        int[] crongValues = crong.stream().mapToInt(i->i).toArray();
+//        for(int crongValue : crongValues){
+//            int[] arrNum = Stream.of(String.valueOf(crongValue).split("")).mapToInt(Integer::parseInt).toArray();
+//            System.out.println(Arrays.toString(arrNum));
+//        }
 
         int answer = Integer.MAX_VALUE;
         return answer;
