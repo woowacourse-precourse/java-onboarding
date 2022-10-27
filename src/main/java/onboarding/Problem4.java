46d1584b978106ac84f749e307c1d31cc22f7033
WonJeongYeon <wonjungyoun@naver.com>
2022-10-27 15:09:05 +0900

    20221027 Problem4.java 예외처리나 클린코드 없이 작동 기능만 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] wordArray = word.toCharArray();
+        char convertChar;
+
+        for (int i = 0; i<wordArray.length; i++) {
+            char c = wordArray[i];
+            if (Character.isAlphabetic(c)) {
+                if (Character.isLowerCase(c)) {
+                    convertChar = (char) ('z' - c + 'a');
+                    wordArray[i] = convertChar;
+
+                    System.out.println(c);
+                } else if (Character.isUpperCase(c)) {
+                    convertChar = (char) ('Z' - c + 'A');
+                    wordArray[i] = convertChar;
+                }
+            } else {
+                convertChar = c;
+            }
+
+        }
+        answer = new String(wordArray);
         return answer;
     }
 }
