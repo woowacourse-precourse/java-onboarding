f5ed19e76269fcff3d88e0b566b21bfd7f1f15e6
taekwon-dev <taekwon_dev@naver.com>
2022-10-27 16:47:33 +0900

    problem 2 - feat : 연결리스트 활용해 주어진 문자열 내 인접한 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        LinkedList<Character> list = new LinkedList<>();
+        char[] chars = cryptogram.toCharArray();
+        for (char c : chars) {
+            if (!list.isEmpty() && list.getLast() == c) {
+                list.removeLast();
+            } else {
+                list.addLast(c);
+            }
+        }
+        if (list.size() == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        while (list.size() != 0) {
+            sb.append(list.removeFirst());
+        }
+        return sb.toString();
     }
 }
