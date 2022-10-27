1c2bb0e4884393551bc469b9a386cfd817a6735b
YooByeongIk <quddlr96@naver.com>
2022-10-27 14:33:41 +0900

    [Problem1] docs : 기능 목록 주석 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,20 @@ package onboarding;
 
 import java.util.List;
 
+/*
+##기능 목록##
+
+1. 각 List의 index 0, 1이 각각 홀수, 짝수 인지 확인
+2. 각 List의 index 0, 1이 연속된 수 인지 확인
+3. 각 List의 index 0으로 만들 수 있는 최대 값 구하기
+4. 각 List의 index 1로 만들 수 있는 최대 값 구하기
+5. 각 List의 index 0, 1로 만들 수 있는 최대 값 구하기
+6. 최대 값 비교 후 결과 출력
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
 }
