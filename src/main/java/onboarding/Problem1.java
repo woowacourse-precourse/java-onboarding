c0f8dc5090599c5bf3afecdbf64a70e36b98920d
egoing <dudqk9696@naver.com>
2022-10-27 19:53:21 +0900

    페이지 더하기 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,18 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    static int addDigit(List<Integer> list) {
+        int result = 0;
+        for(int i=0; i<2; i++) {
+            int tmp = list.get(i);
+            int sum = 0;
+            while(tmp > 0) {
+                sum += tmp % 10;
+                tmp /= 10;
+            }
+            result = Math.max(result,sum);
+        }
+        return result;
+    }
 }
