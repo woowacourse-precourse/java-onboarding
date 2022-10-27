f0d37fc1f0e3034532a5519bcdda47a1d29f0b77
endlessmomo <soohyuk96@naver.com>
2022-10-27 21:08:11 +0900

    feat : Problem1 기능 구현
    getMax : 페이지 별 각 자릿수의 합/곱을 비교하여 최대값은 산출하는 기능
    getSum : 각 자릿수의 합을 구한다.
    getMulti : 각 자릿수의 곱을 구한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,15 +10,57 @@ class Problem1 {
             return -1;
         }
 
-        return 0;
+        int pobiMaxValue = getMax(pobi);
+        int crongMaxValue = getMax(crong);
+
+        if(pobiMaxValue == crongMaxValue) {
+            return 0;
+        }
+
+        return pobiMaxValue > crongMaxValue ? 1 : 2;
+    }
+
+    public static int getMax(List<Integer> pages) {
+        int maxValue = Integer.MIN_VALUE;
+
+        for(int page : pages){
+            int sumValue = getSum(page);
+            int multiValue = getMulti(page);
+
+            maxValue = Math.max(maxValue, Math.max(sumValue, multiValue));
+        }
+
+        return maxValue;
+    }
+
+    private static int getMulti(int page) {
+        int multiValue = 1;
+
+        while(page > 0){
+            multiValue *= page % 10;
+            page /= 10;
+        }
+
+        return multiValue;
+    }
+
+    private static int getSum(int page) {
+        int sumValue = 0;
+
+        while(page > 0){
+            sumValue += page % 10;
+            page /= 10;
+        }
+
+        return sumValue;
     }
 
     public static boolean validation(List<Integer> list){
         if(list.size() != 2 || list.get(0) <= 1 || list.get(1) >= 400
-            || list.get(0) % 2 != 1 || list.get(1) % 2 == 0) {
+                || list.get(0) % 2 != 1 || list.get(1) % 2 != 0) {
             return true;
         }
 
-        return list.get(1) - list.get(0) % 2 != 1;
+        return list.get(1) - list.get(0) != 1;
     }
 }
