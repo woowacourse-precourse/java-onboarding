19320b0f92d3480f524ffb939ba76fd9199056fd
JunginPark4364 <wjddlsee@gmail.com>
2022-10-28 04:23:17 +0900

    feat: change character
    - 글자 변환기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(changeAlphabet(word.charAt(i)));
+        }
+
+        answer = sb.toString();
         return answer;
+    }
+
+    // 글자 변환
+    public static char changeAlphabet(char ch) {
+        // 대문자인 경우
+        if (ch >= 'A' && ch <= 'Z') {
+            return (char) ('A' + 'Z' - ch);
+        }
+        // 소문자인 경우
+        if (ch >= 'a' && ch <= 'z') {
+            return (char) ('a' + 'z' - ch);
+        }
+        return ch;
     }
 }
