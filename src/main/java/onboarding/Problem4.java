10142ee06fa6d6ea945a767fa1388c7c6943eb03
jeonghoon <shb03323@naver.com>
2022-10-27 18:39:18 +0900

    feat: solve problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = reverseWord(word);
         return answer;
+    }
+
+    private static String reverseWord(String word) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c: word.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                stringBuilder.append((char)('Z' + 'A' - c));
+            } else if (c >= 'a' && c <= 'z') {
+                stringBuilder.append((char)('z' + 'a' - c));
+            } else if (c == ' ') {
+                stringBuilder.append(c);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
