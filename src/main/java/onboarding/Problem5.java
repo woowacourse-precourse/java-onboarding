6510e149c15c24291f90c90f65076d8c0b72cbdb
ycopp9 <aciees95@gmail.com>
2022-10-27 17:00:21 +0900

    feat: add the function change() that returns the remaining money after exchange

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private final static int[] UNIT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static int change(int i, int number, int money) {
+        return money - UNIT[i] * number;
     }
 }
