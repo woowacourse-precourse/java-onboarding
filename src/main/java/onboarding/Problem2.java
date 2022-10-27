01705e66af967243ff76071b3aebb0360972e3bf
juhee park <jouhi3322@gmail.com>
2022-10-28 00:51:42 +0900

    feat: Problem2 파일 기능목록 추가, 중복 문자 확인메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+
+/**
+ * 기능목록
+ * 1. 동일 문자 있는지 확인하는 메서드 구현, 동일 문자열가 있었다면 true를 리턴, 없었다면 false를 리턴
+ * 2. 동일 문자를 지우는 메서드 구현
+ * 3. 동일 문자를 반복해서 지우고 만약 없다면 answer 반환 구현
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = " ";
+
         return answer;
     }
+
+    //기능1 동일 문자 있는지 확인하는 메서드 구현, 동일 문자열가 있었다면 true를 리턴, 없었다면 false를 리턴
+    private boolean checkDuplicate(String input){
+        char[] inputArr = input.toCharArray();
+        for(int i=0;i<inputArr.length-1;i++){
+            if(inputArr[i] == inputArr[i+1]) return true;
+        }
+        return false;
+    }
+
 }
