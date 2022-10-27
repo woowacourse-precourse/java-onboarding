4ae78a73ed08408596c761f83ed8b8080458a052
famo1245 <kandallee38@gmail.com>
2022-10-28 01:46:21 +0900

    solved problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer;
+
         if ((pobi.get(0) + 1) != pobi.get(1) ||
             (pobi.get(0) % 2) != 1 ||
             (pobi.get(1) % 2 != 0) ||
             (crong.get(0) + 1) != crong.get(1) ||
             (crong.get(0) % 2) != 1 ||
             (crong.get(1) % 2) != 0) {
-            return -1;
+            answer = -1;
+            return answer;
         }
 
         int pobi_max = 0;
@@ -39,7 +42,6 @@ class Problem1 {
             crong_max = Math.max(crong_max, Math.max(sum, mul));
         }
 
-        int answer;
         if (pobi_max > crong_max)
             answer = 1;
         else if (pobi_max < crong_max)
