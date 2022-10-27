505bfd8cf2247f08171146daf72ede88703aee78
xi_jjun <rlawowns97@naver.com>
2022-10-27 17:24:20 +0900

    feat(Problem2): 연속되는 같은 문자열 지우는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem2 {
+    private static final String SUCCESSIVE_REGEX = "a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|" +
+            "j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}";
+    private static final Pattern SUCCESSIVE_PATTERN = Pattern.compile(SUCCESSIVE_REGEX);
+    private static final String BLANK = "";
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String eraseSuccessiveCharacters(String str) {
+        Matcher successiveStringMatcher = SUCCESSIVE_PATTERN.matcher(str);
+        return successiveStringMatcher.replaceAll(BLANK);
     }
 }
