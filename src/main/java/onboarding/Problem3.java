b9cc86ca5fa86b3882ab6b6e16a3ab75470428fe
qdeu22 <qdeu22@gmail.com>
2022-10-27 17:36:53 +0900

    number 값 까지 반복하는 for문 구현 커밋

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        /* number 값 까지 반복하는 for문 구현 */
+        for (int i = 3; i <= number; i++) {
+
+        }
+
         return answer;
     }
 }
