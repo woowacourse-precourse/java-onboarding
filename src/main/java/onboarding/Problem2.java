a6285363e3311ae09e18fd04d758981de234ee9d
lee <higgs95@naver.com>
2022-10-28 01:42:53 +0900

    fix(problem2): bug about the number of duplication

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,26 +3,35 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "";
 
-        Stack<Character> duplicateChecker = new Stack<>();
+        Stack<Character> newCryptogramChars = new Stack<>();
         char[] cryptogramChars = cryptogram.toCharArray();
 
+        char poppedChar = '\0';
+
         for(int index=0; index<cryptogramChars.length; index++) {
-            if (index==0 || duplicateChecker.empty()) {
-                duplicateChecker.push(cryptogramChars[index]);
-            }else if (duplicateChecker.peek() == cryptogramChars[index]) {
-                duplicateChecker.pop();
-            }else{
-                duplicateChecker.push(cryptogramChars[index]);
+            if (newCryptogramChars.empty()) {
+                newCryptogramChars.push(cryptogramChars[index]);
+                continue;
+            }
+
+            char prevChar = newCryptogramChars.peek();
+
+            if (prevChar == cryptogramChars[index]) {
+                poppedChar = newCryptogramChars.pop();
+            }else if (poppedChar == cryptogramChars[index]) {
+                continue;
+            }else {
+                newCryptogramChars.push(cryptogramChars[index]);
+                poppedChar = '\0';
             }
         }
 
-        if(!duplicateChecker.empty()) {
-            for(char cryptogramChar : duplicateChecker) {
-                answer += "" + cryptogramChar;
-            }
+        for(char cryptogramChar : newCryptogramChars) {
+            answer += "" + cryptogramChar;
         }
         return answer;
     }
