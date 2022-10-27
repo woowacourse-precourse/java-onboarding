7917e37a5a0d0d3baff6ba8c5fa8cc3094cf8c1e
jaehee329 <jaehee329@yonsei.ac.kr>
2022-10-27 19:12:24 +0900

    feat(Problem6): 닉네임을 두 글자씩 나눠 (두 글자, 이메일 리스트)쌍의 HashMap으로 저장 로직 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Set<String> duplicatedUserEmailSet = getDuplicatedUserEmailHashSet(forms);
         return answer;
+    }
+
+    public static HashSet<String> getDuplicatedUserEmailHashSet(List<List<String>> forms) {
+        HashMap<String, ArrayList<String>> stringEmailHashMap = getStringEmailHashMap(forms);
+        HashSet<String> duplicatedUserEmailHashSet = new HashSet<>();
+
+        return duplicatedUserEmailHashSet;
+    }
+
+    public static HashMap<String, ArrayList<String>> getStringEmailHashMap(List<List<String>> forms) {
+        HashMap<String, ArrayList<String>> stringEmailMap = new HashMap<>();
+        for (List<String> userInfo : forms) {
+            String email = userInfo.get(0);
+            String nickname = userInfo.get(1);
+            for (int i=0; i<nickname.length()-1; i++) {
+                String partial = nickname.substring(i, i+2);
+                if (!stringEmailMap.containsKey(partial)) {
+                    stringEmailMap.put(partial, new ArrayList<>(List.of(email)));
+                    continue;
+                }
+                ArrayList<String> emailList = stringEmailMap.get(partial);
+                emailList.add(email);
+            }
+        }
+        return stringEmailMap;
     }
 }
---
+++ src/test/java/onboarding/Problem6Test.java
@@ -0,0 +1,38 @@
+package onboarding;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Problem6Test {
+
+	@Test
+	void getStringEmailHashMapTest() {
+		// given
+		List<List<String>> forms = List.of(
+			List.of("jm@email.com", "제이엠"),
+			List.of("jason@email.com", "제이슨"),
+			List.of("woniee@email.com", "워니"),
+			List.of("mj@email.com", "엠제이"),
+			List.of("nowm@email.com", "이제엠")
+		);
+		
+		// when
+		HashMap<String, ArrayList<String>> stringEmailHashMap =  Problem6.getStringEmailHashMap(forms);
+		
+		// then
+		assertThat(stringEmailHashMap.size()).isEqualTo(7);
+		assertThat(stringEmailHashMap.get("제이").size()).isEqualTo(3);
+		assertThat(stringEmailHashMap.get("워니").size()).isEqualTo(1);
+		Assertions.assertTrue(stringEmailHashMap.get("제이").contains("jm@email.com"));
+		Assertions.assertTrue(stringEmailHashMap.get("제이").contains("jason@email.com"));
+		Assertions.assertTrue(stringEmailHashMap.get("제이").contains("mj@email.com"));
+	}
+
+}
