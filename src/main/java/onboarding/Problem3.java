6fddcf22f0b70c710fc02667f4ce641e403e0ad4
MoonJeWoong <munjin0201@naver.com>
2022-10-28 00:52:16 +0900

    [3번 문제] feat: solution 메소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    public int count369(int checkNum){
+        int cnt = 0;
+        return cnt;
+    }
     public static int solution(int number) {
+        Problem3 T = new Problem3();
         int answer = 0;
+        for(int checkNum=1; checkNum<=number; checkNum++){
+            answer+=T.count369(checkNum);
+        }
         return answer;
     }
 }
