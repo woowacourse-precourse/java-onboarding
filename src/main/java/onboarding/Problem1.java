fb0501d3516c58600a2283cda570125acdd50133
zcae1020 <zcae1020@naver.com>
2022-10-28 00:14:30 +0900

    add: getMultipe method in Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,12 +8,24 @@ class Problem1 {
         return answer;
     }
 
-    //각 자리 숫자 더하기 구하기
+    //각 자리 숫자 더하기 값 구하기
     private static int getSum(int num) {
         int ret = 0;
 
         while(num>0) {
             ret += num % 10;
+            num /= 10;
+        }
+
+        return ret;
+    }
+
+    //각 자리 숫자 곱하기 값 구하기
+    private  static int getMultiple(int num) {
+        int ret = 1;
+
+        while(num>0) {
+            ret *= num % 10;
             num /= 10;
         }
 
