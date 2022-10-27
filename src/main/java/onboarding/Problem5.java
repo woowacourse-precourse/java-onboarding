1c7dd9846e3786d5e0d0d50785ec78dd549c4d37
viyott <jackk0616@gmail.com>
2022-10-27 16:39:52 +0900

    p5 단위보다 money가 크면 money를 단위로 나눈 나머지를 뺀 수를 다시 단위로 나눠서 answer에 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -14,6 +14,14 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int[] krw = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int won : krw) {
+            if (money >= won) {
+                answer.add((money - money % won) / won);
+                money = money % won;
+            } else {
+                answer.add(0);
+            }
+        }
         return answer;
     }
 }
