6ea2515ef93185e46b029d0af3c95185f8713ecb
viyott <jackk0616@gmail.com>
2022-10-27 14:01:38 +0900

    p3 기능 목록 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+    /**
+     * 기능 목록
+     * 1. 특정 숫자에 대해서 손뼉을 치는 수를 구하는 메소드 구현
+     * 2. number까지 for문을 돌면서 answer에 추합
+     */
     public static int solution(int number) {
         int answer = 0;
         return answer;
