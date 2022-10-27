f4b2fea222d5534cb1df760579bc4230f7f72f6b
jhon3242 <rldotnr2@gmail.com>
2022-10-27 15:05:49 +0900

    [feat] 문제 7 User 클래스 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    class User{
+        final String name;
+        List<String> friends;
+        List<String> visitor;
+        int score;
+
+        public User(String name) {
+            this.name = name;
+            this.friends = new ArrayList<>();
+            this.visitor = new ArrayList<>();
+            this.score = 0;
+        }
+
+        public void addFriend(String name){
+            friends.add(name);
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
 }
