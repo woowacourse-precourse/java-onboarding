ffd81b5c016dd53a9da288a0ea954e7cf073f431
hyun6ik <fkanwm@naver.com>
2022-10-27 17:13:53 +0900

    feat(31) : [기능 4] : word 길이 검증 로직 개발

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+    static StringBuilder answer;
+
+    static void input() {
+        answer = new StringBuilder();
     }
+
+    private static void validateWord(String word) {
+        if (word.length() < 1 || word.length() > 1000) {
+            throw new IllegalArgumentException("word는 길이가 1 이상 1,000 이하인 문자열입니다.");
+        }
+    }
+
+    public static String solution(String word) {
+        validateWord(word);
+        return answer.toString();
+    }
+
+
 }
