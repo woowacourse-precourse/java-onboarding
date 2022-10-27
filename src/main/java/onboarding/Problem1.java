2eec81f153efb99516acd0314fc6fcb5f9cb5a8c
minje <alswp0910@naver.com>
2022-10-27 12:40:45 +0900

    기능 목록 생성 기능 1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,16 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int bigPage(int page1, int page2){
+        int answer = 0;
+        if(page1 > page2)
+            answer = page1;
+        else
+            answer = page2;
+
+        return answer;
+    }
+
+
 }
