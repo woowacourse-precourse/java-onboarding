2e3e286218a4b214ab02c9a8b1ed1eef078f7d23
hyun-seung <ardimento22@naver.com>
2022-10-27 23:21:47 +0900

    [FEAT] 주어진 닉네임을 두 글자씩 자르고, 해당 두 글자가 몇 번 중복되는지 체크한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+
+    public static final int EMAIL = 0;
+    public static final int NICKNAME = 1;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        HashMap<String, Integer> twoLetterHashMap = new HashMap<>();
+
+        for (List<String> form : forms) {
+            twoLetterHashMap = putTwoLetters(form, twoLetterHashMap);
+        }
+
         return answer;
+    }
+
+    public static HashMap<String, Integer> putTwoLetters(List<String> form, HashMap<String, Integer> twoLetterHashMap) {
+        String targetNickname = form.get(NICKNAME);
+
+        for (int i = 0; i < targetNickname.length() - 1; i++) {
+            String twoLetter = targetNickname.substring(i, i + 2);
+            twoLetterHashMap.put(twoLetter, twoLetterHashMap.getOrDefault(twoLetter, 0) + 1);
+        }
+
+        return twoLetterHashMap;
     }
 }
