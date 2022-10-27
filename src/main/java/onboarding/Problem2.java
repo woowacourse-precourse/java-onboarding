1198b4c75c8f66183b448182d3f7e77f9a751d41
minhee020 <minhee020@naver.com>
2022-10-27 12:26:15 +0900

    Problem2 | 기능 구현 사항

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+/**
+ * 기능 구현 사항
+ * 1. 연속된 문자열 삭제 함수
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        answer = deleteChar(cryptogram);
         return answer;
     }
 }
