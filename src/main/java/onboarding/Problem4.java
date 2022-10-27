1b53b08eef8334ead645720aaa73398ce4d54cc8
MuSeung <peter0757@nvaer.com>
2022-10-27 09:54:04 +0900

    feat : Problem4의 소문자변경 메소드 changeCapitalLetters 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,11 +6,21 @@ public class Problem4 {
         return answer;
     }
 
-    public static String changeSmallLetters(String word){
+    public static String changeSmallLetters(String word) {
         StringBuilder changeWord = new StringBuilder(word);
         for(int i = 0; i < word.length(); i++) {
             if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') {
                 char change = (char) ('a' + 'z' -  word.charAt(i));
+                changeWord.setCharAt(i, change);
+            }
+        }
+        return changeWord.toString();
+    }
+    public static String changeCapitalLetters(String word){
+        StringBuilder changeWord = new StringBuilder(word);
+        for(int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                char change = (char) ('A' + 'Z' -  word.charAt(i));
                 changeWord.setCharAt(i, change);
             }
         }
