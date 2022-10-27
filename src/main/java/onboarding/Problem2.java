c4e7be3056b2c00e18c97d574e0e20d718f047b2
leewoojin <zmfpdl64@naver.com>
2022-10-27 18:02:10 +0900

    문제2번 이우진 제출합니다.
    문제: 중복되는 문자들을 문자열에서 제거해야한다.
    
    해결방법:
    두가지의 해결방법을 생각했다.
    SOL1: 문자열들의 중복되는 문자열을 제외한 문자열들을 계속해서 이어붙히기를 한다.
    
    SOL2: Stack을 통해 중복되는 문자를 제거한다.
    
    Stack을 이용한 풀이를 선택했다. 왜냐하면 문자열을 자르고 이어붙히면 거기서 소요되는 시간이 많아지기 때문에 효율성에서 손실을 보기 때문이다.
    
    주요 핵심문제인
    중복되는 문자열 제거 방법에는 4가지 조건이 있습니다.
    1. 문자가 일치할 때
        1.1. 일치한 문자가 끝까지 도달했을 때
        1.2.  pass
    2. 문자가 일치하지 않을 때
        2.1. 문자가 그 전까지 일치했을 때
        2.2. 문자가 그 전에도 일치하지 않았을 때 pass
    
    3. while문 안에서 계속해서 돌아도 변경되지 않는다면 결과를 반환한다.
    
    결론적으로 중복되는 문자열을 제거할 수 있었습니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,47 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//해결방법
+// TODO: 채택 sol1. 스택을 이용한 중복문자 제거
+// sol2. 문자열 나누기를 이용한 중복문자 제거
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        List<String> stack = new ArrayList<>(Arrays.asList(cryptogram.split("")));
+
+        while(!stack.isEmpty()){
+            int size = stack.size();
+            for(int i = 0; i < stack.size(); i++){ //문자 str1 선택
+                String s1 = stack.get(i);
+                for(int j = i+1; j < stack.size(); j++){    //문자 str2 선택
+                    String s2 = stack.get(j);
+                    //문자가 일치할때
+                    if(s1.equals(s2)){
+                        if(j == stack.size()-1){    //끝일 때도 불구하고 일치한다면 문자열의 끝index까지 제거 해줘야한다.
+                            for(int k = i; k <= j; k++){
+                                stack.remove(i);
+                            }
+                        }
+                    }else{     //일치하지 않을 때
+                        if(i != j-1){
+                            for(int k = i; k < j; k++){
+                                stack.remove(i);
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            if(size == stack.size()){
+                break;
+            }
+        }
+        return String.join("", stack);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("zyelleyz"));
     }
 }
