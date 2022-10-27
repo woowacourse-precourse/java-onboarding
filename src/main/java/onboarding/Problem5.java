e6d023b7e6028ae81f31686bbfffe3067fa5e290
2jun0 <soo28819@gmail.com>
2022-10-27 18:38:06 +0900

    Feat: 문제 5번 - 지폐 돈을 입력으로 받고 개수를 반환하는 메서드 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,31 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    enum Bill {
+        FIFTY_THOUSAND(50000),
+        TEN_THOUSAND(10000),
+        FIVE_THOUSAND(5000),
+        ONE_THOUSAND(1000),
+        FIVE_HUNDRED(500),
+        ONE_HUNDRED(100),
+        FIFTY(50),
+        TEN(10),
+        ONE(1);
+
+        private final int value;
+        Bill(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public static int getCapableBillCount(Bill bill, int money) {
+        return money / bill.getValue();
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
