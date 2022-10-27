375b2ac1326c5da881f3b92395d975518fb09806
apptie <appti@naver.com>
2022-10-27 16:55:38 +0900

    feat: 문제 6 크루의 이메일과 닉네임을 표현하는 Crew 클래스 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,3 +8,22 @@ public class Problem6 {
         return answer;
     }
 }
+
+class Crew {
+
+    private String email;
+    private String nickname;
+
+    public Crew(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
