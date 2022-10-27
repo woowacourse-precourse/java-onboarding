6a4bd9e48abfc0d3148f740e296b6d1ddb6dbebc
이석환 <2018008886@hanyang.ac.kr>
2022-10-27 20:37:10 +0900

    feat: add Problem1's solution

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,46 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiResult, cronResult;
+
+        if(hasErrorOnInput(pobi) ||hasErrorOnInput(crong)) {
+            return -1;
+        }
+
+        pobiResult = calculateScore(pobi);
+        cronResult = calculateScore(crong);
+
+        if(pobiResult == cronResult) return 0;
+        else if(pobiResult > cronResult) return 1;
+        else return 2;
+    }
+
+    private static boolean hasErrorOnInput(List<Integer> input) {
+        int leftNum, rightNum;
+        boolean isValidate;
+
+        leftNum = input.get(0);
+        rightNum = input.get(1);
+
+        isValidate = (leftNum >= 3) && (leftNum <= 397)
+                && (leftNum % 2 == 1) && (leftNum + 1 == rightNum);
+
+        return !isValidate;
+    }
+
+    private static int calculateScore(List<Integer> input) {
+        int target = input.get(1);
+        int resultCandidate[] = {0, 1};
+
+        if(target % 10 == 0)
+            target = input.get(0);
+
+        while(target != 0) {
+            resultCandidate[0] += target % 10;
+            resultCandidate[1] *= target % 10;
+            target /= 10;
+        }
+
+        return Integer.max(resultCandidate[0], resultCandidate[1]);
     }
 }
