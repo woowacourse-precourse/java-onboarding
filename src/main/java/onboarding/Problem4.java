25cf90503128971a0d5904ab2a7616275759395d
0112leesy <0112leesy@naver.com>
2022-10-27 22:30:14 +0900

    [Problem4] feat: is_alphabet

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,11 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    static boolean is_alphabet(char character){
+        if(character >= 'A' && character <= 'Z') return true;
+        if(character >= 'a' && character <= 'z') return true;
+        return false;
+    }
+
 }
