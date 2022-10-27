cedad965de82997f9f38892bde8665c9fab8fd05
unknown <2021110806@dgu.ac.kr>
2022-10-27 17:11:12 +0900

    feat:problem3 howManyThree method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+
 public class Problem3 {
+    static int cnt=0;
+    public static void howManyThree(String number) {
+        for (int i=0; i<number.length(); i++) {
+            int iNumber= (int) Character.getNumericValue(number.charAt(i));
+            if (iNumber==3) {
+                cnt++;
+            }
+        }
+    }
     public static int solution(int number) {
-        int answer = 0;
+
         return answer;
     }
 }
