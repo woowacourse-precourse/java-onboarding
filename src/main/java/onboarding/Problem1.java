bd17e364840500ea5603af24f8d9b65663177e4b
juhee park <jouhi3322@gmail.com>
2022-10-27 23:54:43 +0900

    docs: problem1.java의 기능목록 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,16 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ *
+ * 기능목록
+ *  1. 페이지는 1~400 페이지 중에 포함되어야 하고, 시작면(1페이지)나 마지막면(400페이지)가 나오지 않아야 하며,
+ *  왼쪽이 홀수, 오른쪽이 짝수이고, 왼쪽과 오른쪽은 한페이지 차이가 나야한다. 이 외의 경우 예외상황 -1을 출력한다
+ *  2. 각 자릿수를 더하는 메서드, 각 자릿수의 곲을 구하는 메서드를 포함한다.
+ *  3. 포비와 크롱의 페이지에의 최대값을 구한다
+ *  4. 포비가 이기는 경우 1, 크롱이 이기면2, 비기면 0을 리턴한다.
+ *
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
