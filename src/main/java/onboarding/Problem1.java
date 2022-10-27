64b0ab06ccdb223afaa46f94a54eb63533ee28dc
yesjuhee <juhee0924@g.skku.edu>
2022-10-28 05:46:08 +0900

    [PROBLEM1] 예외사항 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,8 @@ class Problem1 {
         int scoreOfPobi = getScore(pobi.get(0), pobi.get(1));
         int scoreOfCrong = getScore(crong.get(0), crong.get(1));
 
+        if (isException(pobi.get(0), pobi.get(1))||isException(crong.get(0), crong.get(1))) return -1;
+
         if (scoreOfPobi > scoreOfCrong) return 1;
         else if (scoreOfPobi < scoreOfCrong) return 2;
         else return 0;
@@ -35,5 +37,14 @@ class Problem1 {
         int rightPageScore= getSum(rightPageNum)>getMultiply(rightPageNum) ? getSum(rightPageNum) : getMultiply(rightPageNum);
         int finalScore = leftPageScore>rightPageScore ? leftPageScore : rightPageScore;
         return finalScore;
+    }
+
+    public static boolean isException(int leftPageNum, int rightPageNum){
+        if (leftPageNum%2 != 1) return true;
+        if (leftPageNum < 1) return true;
+        if (rightPageNum%2 != 0) return true;
+        if (rightPageNum > 400) return true;
+        if (rightPageNum - leftPageNum != 1) return true;
+        return false;
     }
 }
