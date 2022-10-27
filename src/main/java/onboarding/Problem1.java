92ea6894f86801d80e89016e433fcbbed09a9402
QLoooad <ckcc0813@gmail.com>
2022-10-28 03:11:37 +0900

    페이지 호출과 동시 2~399제한

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,34 +1,38 @@
 package onboarding;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.StringTokenizer;
+
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //int pobiPage = Integer.parseInt(br.readLine());
-       // int crongPage = Integer.parseInt(br.readLine());
-
-        StringTokenizer pobi = new StringTokenizer(br.readLine(), " ,");
-        StringTokenizer crong = new StringTokenizer(br.readLine(), " ,");
-        int pobiR = Integer.parseInt(pobi.nextToken());
-        int pobiL = Integer.parseInt(pobi.nextToken());
-        int crongR = Integer.parseInt(crong.nextToken());
-        int crongL = Integer.parseInt(crong.nextToken());
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+	
+	public static int makePage() { //makePage와 동시 2~399까지 추출
+        int Page = (int) (Math.random() * (399-2))+2;
+        do {
+        	Page = (int) (Math.random() * (399-2))+2;
+        }while(Page <= 1 || Page >= 400); //조건 충족 시 리셋
+        return Page;
+	}
+    public static void main(String[] args){
+    	
+       int pobiPage = makePage();
+       int crongPage = makePage()+2;
+        
+        
+        
+        int pobiL;
+        int pobiR;
+        int crongL;
+        int crongR;
     }
 }
 
 
 /*
- * 랜덤번호 2~399
+ * 랜덤번호 2~399 **
  * 나온번호 올수짝수 구분 후 홀수 +1 짝수 -1
  * 나온 두 수 *페이지 번호의 각 자리 숫자를 모두 더함 || 모두 곱해 가장 큰 수를 구한다.
  * 더하거나 곱한 수 큰수로 비교
