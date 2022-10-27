108145f1fb3d854f4faada810624c3a6c593f89b
hyeongu <wancern@naver.com>
2022-10-28 00:23:46 +0900

    Problem1. 요구사항 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,7 +1,12 @@
 package onboarding;
 
 import java.util.List;
-
+/*
+* 1. 예외사항 체크 메소드 작성
+* 2. 최댓값 메소드 작성
+* 3. 점수 비교 메소드 작성
+* 4. 점수 계산 및 결과 산출
+* */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
