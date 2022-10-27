4a90531c7008d68b2ef81203fc63bdb817a0fef6
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-28 01:23:38 +0900

    feat: 구현한 기능을 호출 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,9 +2,10 @@ package onboarding;
 
 import java.util.List;
 
+import onboarding.problem6.Verifier;
+
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		return Verifier.extractDuplicateCrews(forms);
+	}
 }
