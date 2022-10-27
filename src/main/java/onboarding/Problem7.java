d1646a9e526905e2adccdc547e5506ea65505743
Your Name <you@example.com>
2022-10-28 03:09:02 +0900

    Problem7 기능 목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        /*사용자와 친구들 관계 구현*/
+
+        /*사용자와 함께 아는 친구의 수 점수 구하기*/
+        
+        /*사용자의 타임 라인에 방문한 횟수 점수 구하기*/
+
+        /*점수가 가장 높은 순으로 정렬, 점수가 같은 경우는 이름순으로 정렬*/
+
         return answer;
     }
 }
