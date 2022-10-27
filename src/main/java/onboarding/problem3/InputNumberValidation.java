46ef13b173690168818585a90eec7760b2475a7f
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 17:56:19 +0900

    feat(P3): 검증 로직 구현

--- src/main/java/onboarding/problem3/InputNumberValidation.java
+++ src/main/java/onboarding/problem3/InputNumberValidation.java
@@ -2,4 +2,12 @@ package onboarding.problem3;
 
 public class InputNumberValidation {
 
+    public static void isValidNumber(int number) {
+        int MAX_VALUE = 10_000;
+        int MIN_VALUE = 1;
+
+        if (number < MIN_VALUE || MAX_VALUE < number) {
+            throw new IllegalArgumentException("숫자는 1 이상 10,000 이하여야 합니다.");
+        }
+    }
 }
