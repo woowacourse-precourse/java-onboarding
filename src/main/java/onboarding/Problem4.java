8944e82f0577ede0eccc6917fdd1fa004e6007f6
Choi-JY <Choi-JY1107@github.com>
2022-10-28 01:04:36 +0900

    feat: 4번문제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        int upper = (int)'A' + (int)'Z';
+        int lower = (int)'a' + (int)'z';
         String answer = "";
+
+        for(char c: word.toCharArray()){
+            if(Character.isUpperCase(c)) answer += (char)(upper-(int)c);
+            else if(Character.isLowerCase(c)) answer += (char)(lower-(int)c);
+            else answer += c;
+        }
         return answer;
     }
 }
