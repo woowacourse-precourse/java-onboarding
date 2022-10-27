74050c389a758ba00d4bd28b1f7a1df6feb6cd35
Beginner <dkstmdwn17@naver.com>
2022-10-27 21:43:35 +0900

    feat : P3 기능 요구사항과, 3, 6, 9 의 개수를 세는 메서드를 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.List;
+
+/**
+ * 기능 요구사항
+ * 1. 3, 6, 9의 개수를 세는 메서드를 작성한다.
+ */
 public class Problem3 {
+    static int answer;
+    static List<Character> threeSixNine = List.of('3','6','9');
     public static int solution(int number) {
-        int answer = 0;
+        answer = countClap(number);
+        return answer;
+    }
+
+    public static int countClap(int number){
+        for(int i=1;i<=number;i++){
+            String str = String.valueOf(i);
+            for(int j=0;j<str.length();j++){
+                if(threeSixNine.contains(str.charAt(j))){
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
