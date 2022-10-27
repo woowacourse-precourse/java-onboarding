3a401f91c9e6d564c36b7e23b9c241b87ace48ae
lee <whddlsk123@naver.com>
2022-10-28 00:53:29 +0900

    Problem2 dequeToString() 기능구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,12 +7,15 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+
         return answer;
     }
 
     /**
      * 입력 받은 String을 deque에 뒤에서부터 한 문자씩 저장하며 연속된 중복값을 제거한다.
      * @param cryptogram
+     * @return Deque<Character>
      */
     public static Deque<Character> decoding(String cryptogram) {
         Deque<Character> deque = new LinkedList<>();
@@ -28,6 +31,22 @@ public class Problem2 {
         }
 
         return deque;
+    }
+
+    /**
+     * deque의 요소들을 String으로 반환
+     * @param deque
+     * @return String
+     */
+    public static String dequeToString(Deque<Character> deque) {
+        String s = "";
+
+        if(deque.isEmpty()) return s;
+        else {
+            while(!deque.isEmpty()) s += deque.pollFirst();
+        }
+
+        return s;
     }
 
 }
