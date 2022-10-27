8863fb0ea77da43bbf995a4c16905e8394d3f93f
MuSeung <peter0757@nvaer.com>
2022-10-27 09:50:55 +0900

    feat : Problem4의 소문자변경 메소드 changeSmallLetters 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,15 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static String changeSmallLetters(String word){
+        StringBuilder changeWord = new StringBuilder(word);
+        for(int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') {
+                char change = (char) ('a' + 'z' -  word.charAt(i));
+                changeWord.setCharAt(i, change);
+            }
+        }
+        return changeWord.toString();
+    }
 }
