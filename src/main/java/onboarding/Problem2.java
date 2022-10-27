23b786644a3fe4da68db57b10048bae1f54c8199
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-27 15:16:00 +0900

    [Problem 2] decodingCurrentString은 현재 string에서 중복이 있는지 확인하고 중복을 발견할 경우 removeDuplication에게 중복을 제거해달라 한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,6 +13,21 @@ public class Problem2 {
         while(!completeDecoding){
             completeDecoding=decodingCurrentString(decodedCrypto);
         }
+        return decodedCrypto.toString();
+    }
+
+    private static boolean decodingCurrentString(StringBuffer cryptogram){
+        StringBuffer afterDecode = cryptogram;
+        boolean result = true;
+        int checkPoint = 0;
+        while(checkPoint<afterDecode.length()-1){
+            if(afterDecode.charAt(checkPoint)==afterDecode.charAt(checkPoint+1)){
+                removeDuplication(afterDecode, checkPoint);
+                result=false;
+            }
+            checkPoint++;
+        }
+        return result;
     }
 
 }
