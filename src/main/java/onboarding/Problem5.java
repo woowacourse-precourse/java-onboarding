6ee5de26cc5302e6d8496c5bf337d9547fb05975
eunbii0213 <eunbii0213@naver.com>
2022-10-27 19:40:12 +0900

    feat: (Problem5) 화폐의 단위를 배열에 저장한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    public static int moneyForCalculate;
+    public static int[] unit = new int[9];
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+
+        initUnit();
+        //atm(money, answer);
+
         return answer;
+    }
+
+    private static void initUnit() {
+        unit[0] = 50000;
+        unit[1] = 10000;
+        unit[2] = 5000;
+        unit[3] = 1000;
+        unit[4] = 500;
+        unit[5] = 100;
+        unit[6] = 50;
+        unit[7] = 10;
+        unit[8] = 1;
     }
 }
