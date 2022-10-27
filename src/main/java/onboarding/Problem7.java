5c7361ec3f56d3755f827fb4433afcdd535b35be
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-27 19:16:08 +0900

    refactor: problem7 - insertFriendToListIfUserFriend 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,21 +6,23 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         List<String> friendList = new ArrayList<>();
-        HashSet<String> memberList = new HashSet<>();
+        HashMap<String, Integer> memberList = new HashMap<>();
 
         for (int i = 0; i < friends.size(); i++) {
-            friendList = insertFriendToListIfUserFriend(friends.get(i).get(0), user, friendList);
-            friendList = insertFriendToListIfUserFriend(friends.get(i).get(1), user, friendList);
+            friendList = insertFriendToListIfUserFriend(friends.get(i).get(0), friends.get(i).get(1), user, friendList);
             //전체 멤버 리스트 작성 기능
-            memberList.add(friends.get(i).get(0));
-            memberList.add(friends.get(i).get(1));
+            memberList.put(friends.get(i).get(0), 0);
+            memberList.put(friends.get(i).get(1), 0);
         }
         return answer;
     }
     //user의 친구목록을 작성하는 기능
-    public static List<String> insertFriendToListIfUserFriend(String name, String user, List<String>friendList){
-        if (name.equals(user)) {
-            friendList.add(name);
+    public static List<String> insertFriendToListIfUserFriend(String friend1, String friend2, String user, List<String>friendList){
+        if (friend1.equals(user)) {
+            friendList.add(friend2);
+        }
+        if (friend2.equals(user)) {
+            friendList.add(friend1);
         }
         return friendList;
     }
