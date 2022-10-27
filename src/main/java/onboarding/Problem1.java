f9b97c862b41a4f8a97a16c17acc7b8548668589
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-27 17:13:42 +0900

    refactor: PROBLEM1 PageComparator inner 클래스로 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,49 +16,49 @@ class Problem1 {
 			return -1;
 		}
 	}
-}
 
-class PageComparator {
+	static class PageComparator {
 
-	public boolean verifyPage(List<Integer> page) {
-		Integer left = page.get(0);
-		Integer right = page.get(1);
+		public boolean verifyPage(List<Integer> page) {
+			Integer left = page.get(0);
+			Integer right = page.get(1);
 
-		if (left % 2 == 1 && right == left + 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public Integer getMaxPageValue(List<Integer> page) {
-		Integer left = page.get(0);
-		Integer right = page.get(1);
-
-		return Math.max(getMaxValue(left), getMaxValue(right));
-	}
-
-	public Integer compare(Integer value1, Integer value2) {
-		if (value1 > value2) {
-			return 1;
-		} else if (value1 < value2) {
-			return 2;
-		} else {
-			return 0;
-		}
-	}
-
-	private Integer getMaxValue(Integer page) {
-		List<Integer> numberList = new ArrayList<>();
-
-		while (page > 0) {
-			numberList.add(page % 10);
-			page = page / 10;
+			if (left % 2 == 1 && right == left + 1) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 
-		Integer sumValue = numberList.stream().reduce(0, (sum, value) -> sum + value);
-		Integer multipleValue = numberList.stream().reduce(1, (sum, value) -> sum * value);
+		public Integer getMaxPageValue(List<Integer> page) {
+			Integer left = page.get(0);
+			Integer right = page.get(1);
 
-		return Math.max(sumValue, multipleValue);
+			return Math.max(getMaxValue(left), getMaxValue(right));
+		}
+
+		public Integer compare(Integer value1, Integer value2) {
+			if (value1 > value2) {
+				return 1;
+			} else if (value1 < value2) {
+				return 2;
+			} else {
+				return 0;
+			}
+		}
+
+		private Integer getMaxValue(Integer page) {
+			List<Integer> numberList = new ArrayList<>();
+
+			while (page > 0) {
+				numberList.add(page % 10);
+				page = page / 10;
+			}
+
+			Integer sumValue = numberList.stream().reduce(0, (sum, value) -> sum + value);
+			Integer multipleValue = numberList.stream().reduce(1, (sum, value) -> sum * value);
+
+			return Math.max(sumValue, multipleValue);
+		}
 	}
 }
