f5cf0a4d07c2b9098d1b693239917c2ef32631a3
eyeofsol <kingjihong@naver.com>
2022-10-27 19:16:32 +0900

    feat(문제4): 문자가 알파벳인지 검사하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,14 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static boolean isAlphabet(char c) {
+        if (c >= 65 && c <= 90) {
+            return true;
+        }
+        if (c >= 97 && c <= 122) {
+            return true;
+        }
+        return false;
+    }
 }
