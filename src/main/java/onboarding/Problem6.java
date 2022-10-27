1befe0a204783be5bf26455bc5536075120d142a
yujaemin <woawoa0513@gmail.com>
2022-10-27 16:39:03 +0900

    feat: [문제 6] 크루들이 신청한 닉네임 중 겹치는 문자열 찾기
    회원의 닉네임을 부분문자열로 자르고, 해당 부분문자열을 연속적으로 연속적으로 포함하는 크루가 있는지 찾는다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,41 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        List<String> nicknames = new ArrayList<>();
+        List<String> emails = new ArrayList<>();
+
+        for (List<String> crews : forms) {
+
+            String email = crews.get(0);
+            String nickname = crews.get(1);
+
+            nicknames.add(nickname);
+            emails.add(email);
+        }
+
+        System.out.println(getDuplicatedSubstring(nicknames));
+
         return answer;
+    }
+
+    public static String getDuplicatedSubstring(List<String> nicknames) {
+        for (int i = 0; i < nicknames.size() - 1; i++) {
+            String nickname = nicknames.get(i);
+            for (int idx = 0; idx < nickname.length() - 1; idx++) {
+                String substring = nickname.substring(idx, idx + 2);
+                for (int j = i + 1; j < nicknames.size(); j++) {
+                    String otherNickname = nicknames.get(j);
+
+                    if (otherNickname.contains(substring)) return substring;
+                }
+            }
+        }
+        return null;
     }
 }
