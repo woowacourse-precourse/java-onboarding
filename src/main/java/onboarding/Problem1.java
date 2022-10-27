cc675936d6261d9b58bc89cb7b5ebbf5cb6b7c43
ChoiYeonho0903 <903yh@naver.com>
2022-10-27 19:42:01 +0900

    Promblem1 : add 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = whoIsWin(pobi, crong);
         return answer;
+    }
+
+    public static int add(int page) {
+        int sum = 0;
+        while(page > 0) {
+            sum += page % 10;
+            page /= 10;
+        }
+        return sum;
     }
 }
