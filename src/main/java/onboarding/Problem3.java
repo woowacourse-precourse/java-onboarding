d2dac324cee1b76ba385c3052625c51c2c2c926d
JunH <tjdtls690@gmail.com>
2022-10-27 17:39:06 +0900

    :memo: docs(PROBLEM4) : 기능 구현 목록 작성(객체지향 설계로 다시 구현)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,9 +2,6 @@ package onboarding;
 
 import onboarding.problem3.domain.Numbers;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Problem3 {
     public static int solution(int number) {
         return new Numbers(number).sumCountOfMultipleOfThree();
