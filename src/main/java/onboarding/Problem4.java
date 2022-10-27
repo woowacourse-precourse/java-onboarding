83e118aa82bb40808578cd3ce52ef33d29595ab7
youngkyunIm <yee0230@naver.com>
2022-10-27 17:00:13 +0900

    feat(Problem4) : makeUppercase
    대문자 알파벳 배열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] uppercase = makeUppercase();
         return answer;
+    }
+
+    public static char[] makeUppercase() {
+        char[] uppercase = new char[26];
+        for (int i = 0; i < 26; i++) {
+            uppercase[i] = (char) (i + 65);
+        }
+        return uppercase;
     }
 }
