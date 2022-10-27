561a3efb460566bd18183c88a44ecbdca39299f2
davejang <davejang98@naver.com>
2022-10-27 15:28:30 +0900

    feat: Problem4 implement
    온보딩 미션 문제3 기능 요구 사항 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        HashMap<Character,Character> dictonary = new HashMap<>();
+        char[] chars = word.toCharArray();
+
+        int currentUpperValue = 'Z';
+        for(int ascii = 'A'; ascii <= 'Z'; ascii++) {
+            dictonary.put((char) ascii, (char) currentUpperValue);
+            currentUpperValue--;
+        }
+
+        int currentLowerValue = 'z';
+        for(int ascii = 'a'; ascii <= 'z'; ascii++){
+            dictonary.put((char)ascii,(char)currentLowerValue);
+            currentLowerValue--;
+        }
+
+        for(int index=0; index < chars.length; index++) {
+            if(dictonary.containsKey(chars[index]) == true){
+                answer = answer + dictonary.get(chars[index]);
+            }
+            if(dictonary.containsKey(chars[index]) == false){
+                answer = answer + chars[index];
+            }
+        }
+
         return answer;
     }
 }
