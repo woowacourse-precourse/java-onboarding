ab2f79a95ddc6941e6af90c6d78aa60e52f08cc0
limjae <limjae96@gmail.com>
2022-10-27 09:27:49 +0900

    feat: Problem4 - Add TreeFrog
    생성자 생성

---
+++ src/main/java/onboarding/problem4/TreeFrog.java
@@ -0,0 +1,14 @@
+package onboarding.problem4;
+
+public class TreeFrog implements Frog{
+    String momSays;
+
+    public TreeFrog(String momSays){
+        this.momSays = momSays;
+    }
+
+    @Override
+    public String repeatAfter() {
+        return null;
+    }
+}
