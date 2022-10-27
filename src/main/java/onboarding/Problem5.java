642aad7d7063c23e82d6598d91f9e4bac08e342c
shinyehwan <jghisjl007@naver.com>
2022-10-27 20:48:35 +0900

    Problem5 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<Integer> solution(int money) {
+		ArrayList<Integer> answer = new ArrayList<>();
+		int[] moneyPackage = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+		for (int i = 0; i < moneyPackage.length; i++) {
+			int result = money / moneyPackage[i];
+			answer.add(result);
+
+			money %= moneyPackage[i];
+		}
+
+		return answer;
+	}
 }
