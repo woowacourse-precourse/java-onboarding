3a2bf75df2014ed241de463d492e65af762d2b94
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-27 23:48:32 +0900

    feat : Add a function checking duplication

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
@@ -13,4 +15,17 @@ public class Problem2 {
         if (length < 2) return false;
         return true;
     }
+
+    //중복 여부를 체크하는 함수
+    public static boolean hasDuplicate(String cryptogram){
+        int length = cryptogram.length();
+        boolean isDuplicate = false;
+
+        for(int i=0;i<length-1;i++){
+            if(cryptogram.charAt(i)==cryptogram.charAt(i+1)) return true;
+        }
+
+        return false;
+    }
+
 }
