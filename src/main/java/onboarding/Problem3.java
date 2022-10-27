a2b0382f1c7b0b3e83e263db38f8fcef4fa5c701
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-10-27 17:44:58 +0900

    [feat] Problem3: 3,6,9 조건 확인 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+    private static final List<Integer> CLAP_NUMBER = List.of(3,6,9);
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+    private static boolean isContainClapNumber(int currentNumber) {
+        return CLAP_NUMBER.contains(currentNumber % 10);
+    }
+
 }
