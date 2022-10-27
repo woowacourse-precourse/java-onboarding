fb7d9aedc2d1a1fe0990b518debbd71f771dc8db
kokodak <kokodakadokok@gmail.com>
2022-10-27 17:23:47 +0900

    feat: [문제5] 각 지폐와 동전으로 변환할 수 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,44 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    private static boolean canConvertToFiftyThousand(int money) {
+        if (money / 50000 != 0) return true;
+        return false;
+    }
+
+    private static boolean canConvertToTenThousand(int money) {
+        if (money / 10000 != 0) return true;
+        return false;
+    }
+
+    private static boolean canConvertToFiveThousand(int money) {
+        if (money / 5000 != 0) return true;
+        return false;
+    }
+
+    private static boolean canConvertToOneThousand(int money) {
+        if (money / 1000 != 0) return true;
+        return false;
+    }
+
+    private static boolean canConvertToFiveHundred(int money) {
+        if (money / 500 != 0) return true;
+        return false;
+    }
+
+    private static boolean canConvertToOneHundred(int money) {
+        if (money / 100 != 0) return true;
+        return false;
+    }
+
+    private static boolean canConvertToFifty(int money) {
+        if (money / 50 != 0) return true;
+        return false;
+    }
+
+    private static boolean canConvertToTen(int money) {
+        if (money / 10 != 0) return true;
+        return false;
+    }
 }
