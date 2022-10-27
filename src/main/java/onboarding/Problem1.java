53fb58069f3f7c42a60553bc34edef8e395fb738
kimensoo <ekclstkfka5@gmail.com>
2022-10-28 00:05:28 +0900

    refactor: 코드 리팩토링
    공백, 괄호 등 기본 컨벤션 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,9 +10,9 @@ class Problem1 {
     private static final int PAGE_LENGTH = 2;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer;
 
-        if(!isValid(pobi, crong)) {
+        if (!isValid(pobi, crong)) {
             return answer = EXCEPTION;
         }
 
@@ -50,10 +50,10 @@ class Problem1 {
     }
 
     public static int getWhoWin(int pobiNum, int crongNum) {
-        if(pobiNum > crongNum) {
+        if (pobiNum > crongNum) {
             return POBI_WIN;
         }
-        if(pobiNum < crongNum) {
+        if (pobiNum < crongNum) {
             return CRONG_WIN;
         }
         return DRAW;
@@ -63,19 +63,19 @@ class Problem1 {
         for (int i = 0; i < PAGE_LENGTH; i++) {
             int tmpPobiPageNum = pobi.get(i);
             int tmpCrongPageNum = crong.get(i);
-            if(tmpPobiPageNum < 1 || tmpPobiPageNum > 400 || tmpCrongPageNum < 1 || tmpCrongPageNum > 400) {
+            if (tmpPobiPageNum < 1 || tmpPobiPageNum > 400 || tmpCrongPageNum < 1 || tmpCrongPageNum > 400) {
                 return false;
             }
         }
 
-        if(pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(1) == 400 || crong.get(1) == 400) {
+        if (pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(1) == 400 || crong.get(1) == 400) {
             return false;
         }
 
         int pobiPageGap = Math.abs(pobi.get(1) - pobi.get(0));
         int crongPageGap = Math.abs(crong.get(1) - crong.get(0));
 
-        if(pobiPageGap != 1 || crongPageGap != 1) {
+        if (pobiPageGap != 1 || crongPageGap != 1) {
             return false;
         }
         return true;
