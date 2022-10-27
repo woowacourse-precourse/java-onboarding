418a96c4f969dc2512c3b5cea6df76f2cca82aa5
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-27 11:06:19 +0900

    feat :: 숫자 각 자리 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    public static final List<String> include369array = List.of("3", "6", "9");
+
+    public static void main(String[] args) {
+        System.out.println(solution(33));
     }
+
+    public static int solution(int number) {
+        int cnt = 0;
+        for (int i = 1; i <= number; i++) {
+            cnt += separateEachDigit(i);
+        }
+
+        return cnt;
+    }
+
+    // 각 자리 숫자 분리
+    private static int separateEachDigit(int number) {
+        int cnt = 0;
+        while (number != 0) {
+            if (isInclude3or6or9(number % 10)) {
+                cnt++;
+            }
+            number /= 10;
+        }
+
+        return cnt;
+    }
+
 }
