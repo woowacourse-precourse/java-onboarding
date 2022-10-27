b49fd4fe71239e4c5dfe7cff5cae352b440d3924
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-28 00:11:14 +0900

    feat(문제2): 입력받은 문자열의 길이 검증

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
+    public static String solution(String cryptogram) throws Exception {
         String answer = "answer";
+        answer = decode(cryptogram);
         return answer;
+    }
+
+    private static String decode(String cryptogram) throws Exception {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            throw new Exception("입력값의 길이가 올바르지 않습니다");
+        }
+        return "아직 처리x";
     }
 }
