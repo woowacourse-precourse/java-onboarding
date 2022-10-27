4b7cd609d16195041899b9152eab57c8e016b630
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-10-28 03:15:06 +0900

    feat: 인접 중복문자 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,6 +15,8 @@ public class Problem2 {
             throw new IllegalArgumentException("문자열의 형태가 유효하지 않습니다");
         }
 
+        Stack<Character> unduplicatedCryptogram = removeDupAdjacentChar(cryptogram);
+
         return answer;
     }
 
@@ -27,5 +30,24 @@ public class Problem2 {
         }
 
         return true;
+    }
+
+    public static Stack<Character> removeDupAdjacentChar(String cryptogram) {
+        Stack<Character> storage = new Stack<>();
+        int idx = 0;
+
+        while (idx < cryptogram.length()) {
+            char charOfCryptogram = cryptogram.charAt(idx);
+
+            if (!storage.isEmpty() && storage.peek() == charOfCryptogram) {
+                storage.pop();
+            } else {
+                storage.push(charOfCryptogram);
+            }
+
+            idx++;
+        }
+
+        return storage;
     }
 }
