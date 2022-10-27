2326ed6658b59b06b0c5ce0d7438700ef9c04fec
KSH <tkdgns543@gmail.com>
2022-10-28 04:06:14 +0900

    feat(Problem1): 예외확인 클래스 생성
    클래스 생성자 코드 작성
    주어진 4개 페이지 int 타입으로 명시적 초기화

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,19 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private class ExceptionChecker{
+        int pageOne;
+        int pageTwo;
+        int pageThree;
+        int pageFour;
+
+        ExceptionChecker() {}
+        ExceptionChecker(List<Integer> pobi, List<Integer> crong) {
+            pageOne = pobi.get(0);
+            pageTwo = pobi.get(1);
+            pageThree = crong.get(0);
+            pageFour = crong.get(1);
+        }
+    }
 }
