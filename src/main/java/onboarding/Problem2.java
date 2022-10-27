fe5a404826d05fc8a2eb7cc4304767f631118fea
minje <alswp0910@naver.com>
2022-10-27 19:48:10 +0900

    문제2_기능1

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,17 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    //기능 1.
+    public static int index(String str){
+        int answer = -1;
+        char [] chars = str.toCharArray();
+        int charsLen = chars.length-1;
+        for (int i = 0; i < charsLen; i++) {
+            answer = i;
+            break;
+        }
+        return answer;
+    }
+
 }
