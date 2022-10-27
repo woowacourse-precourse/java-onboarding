a49396b6f3263406bba73717394326aac3fe8133
hyun6ik <fkanwm@naver.com>
2022-10-27 16:04:09 +0900

    feat(26) : [기능 3] : 기능 요구 사항 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,24 +1,34 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Problem3 {
 
     static int[] Dy;
-    static int answer;
 
     static void preProcess() {
         Dy[1] = 0;
-        Dy[2] = 0;
-        Dy[3] = 1;
-        Dy[4] = 1;
-        Dy[5] = 1;
-        Dy[6] = 2;
-        Dy[7] = 2;
-        Dy[8] = 2;
-        Dy[9] = 3;
+    }
+
+    static int[] getDigitArr(int number) {
+        return Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static int getClapCount(int[] digitArr) {
+        return Arrays.stream(digitArr)
+                .filter(digit -> digit == 3 || digit == 6 || digit == 9)
+                .reduce(0, (answer, count) -> answer + 1);
     }
 
     public static int solution(int number) {
         Dy = new int[number + 1];
-        return answer;
+
+        for (int i = 2; i <= number; i++) {
+            final int[] digitArr = getDigitArr(i);
+            Dy[i] = getClapCount(digitArr) + Dy[i - 1];
+        }
+
+        return Dy[number];
     }
 }
