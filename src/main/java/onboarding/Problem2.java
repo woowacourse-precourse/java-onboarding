b7229ff41b9dc357a79de7a7d4e799453ca0b7f3
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 01:18:35 +0900

    feat: PROBLEM2 중복 문자 확인 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Decoder decoder = new Decoder();
+        // return decoder.decode(cryptogram);
+        return null;
+    }
+
+    static class Decoder {
+
+        private final static String REGEX = "(([a-z])\\2+)";
+
+        private final Pattern pattern;
+
+        Decoder() {
+            this.pattern = Pattern.compile(REGEX);
+        }
+
+        private boolean hasDuplicates(String cryptogram) {
+            Matcher matcher = pattern.matcher(cryptogram);
+            return matcher.find();
+        }
     }
 }
