21eb4c620329bebe535e95086ca0d7e3d2350c93
devjordy14 <devjordy14@gmail.com>
2022-10-27 16:56:16 +0900

    did 2-1

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,12 @@ package onboarding;
 
 // todo
 // 1. 받은 매개변수까지 반복하는 반복문 만들기
-// 2.
+// 2. 각 자릿수에 3,6,9가 몇개 들어가는지 검사
+// 2-1. 숫자의 각 자릿수를 분해
+// 2-2. 분해한 각 자릿수가 3,6,9인지 검사
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
@@ -14,8 +19,25 @@ public class Problem3 {
 
         for(int i = 1; i <= 13; i++){
             System.out.println(i);
+            System.out.println(returnDigit(i));
         }
 
         return answer;
     }
+
+    public static List<Integer> returnDigit(int num){
+        ArrayList<Integer> arrNum = new ArrayList<>();
+        while(num > 0) {
+            arrNum.add(num %10);
+            num /= 10;
+        }
+        return arrNum;
+    }
+
+    public static int returnSumOfCondition(List<Integer>){
+        int sum = 0;
+
+        return sum;
+    }
 }
+
