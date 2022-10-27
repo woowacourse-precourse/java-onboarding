1bc3955ccb4686c8d73da41d4d785edc2753d5d2
jeongjihun <wlgns2234@naver.com>
2022-10-27 17:18:26 +0900

    Feat: 대문자를 받았을때 변환기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem4 {
+    // 대문자 65~90
+    // 소문자 97~122
+    // 공백문자 32
+
+    public static char changeUpper(char c) {
+        // 공백문자인 경우
+        if ((int)c == 32)
+            return c;
+
+        c = (char)(65 + 90 - (int)c);
+        return c;
+    }
+
     public static String solution(String word) {
         String answer = "";
+        char c;
+        for (int i = 0;  i < word.length(); i++) {
+            c = word.charAt(i);
+            // 대문자 인경우
+            if ((int)c < 91) {
+                c = changeUpper(c);
+                answer += c;
+            }
+        }
         return answer;
     }
 }
