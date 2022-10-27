37bd5d7c0e0bdc3f0929e2a2b5dc5de90d7347b1
ChoiYeonho0903 <903yh@naver.com>
2022-10-27 19:43:33 +0900

    Problem1 : calculateScore 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,12 @@ class Problem1 {
         return answer;
     }
 
+    public static int calculateScore(List<Integer> page) {
+        int leftSum = Math.max(multiply(page.get(0)), add(page.get(0)));
+        int rightSum = Math.max(multiply(page.get(1)), add(page.get(1)));
+        return Math.max(leftSum, rightSum);
+    }
+
     public static int multiply(int page) {
         int sum = 1;
         while(page > 0) {
