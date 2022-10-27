61015d3ffb00b3f9f7e01b58dcb97f1da2c7277e
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-27 11:10:14 +0900

    Feat: 구현 Problem6 닉네임의 두 글자씩 가져와서 갯수 count

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
 	private static final String DOMAIN = "@email.com";
@@ -19,6 +21,18 @@ public class Problem6 {
 
 	public static List<String> solution(List<List<String>> forms) {
 		List<String> answer = new ArrayList<>();
+		Map<String, Integer> twoWordNickNames = new HashMap<>();
+
+		for (int i = 0; i < forms.size(); i++) {
+			if (!isValidInput(forms.get(i).get(0), forms.get(i).get(1))) {
+				continue;
+			}
+			String nickName = forms.get(i).get(1);
+			for (int j = 0; j < nickName.length() - 1; j++) {
+				String twoWord = nickName.substring(j, j + 2);
+				twoWordNickNames.put(twoWord, twoWordNickNames.getOrDefault(twoWord, 0) + 1);
+			}
+		}
 		return answer;
 	}
 }
