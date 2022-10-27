e9deb0d9247bd3d7c09c00b699b1ae245ef6d8ad
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-10-27 10:12:46 +0900

    Create overlapCheckingStack: if stack's last element == input element => no push and pop last element, else => push element and repeat all charArray and return stack without duplicates

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
-
+import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
+        char [] splitString = cryptogram.toCharArray();
+        Stack<Character> noDuplicatesStack = overlapCheckStack(splitString);
         String answer = "answer";
         return answer;
+    }
+    private static Stack<Character> overlapCheckStack(char [] charArray){
+        Stack<Character> stack = new Stack<>();
+        for(char element :charArray){
+            char lastElement = stack.peek();
+            if (lastElement==element){
+                stack.pop();
+            }else{
+                stack.push(element);
+            }
+        }
+        return stack;
     }
 }
