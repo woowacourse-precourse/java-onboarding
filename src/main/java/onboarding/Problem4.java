b68b6bf1854636c19cb397aa3eefdb8d60bbc963
TaemHam <tk4003950@gmail.com>
2022-10-27 11:53:20 +0900

    Feat: Implement problem 4 main solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static boolean isValidInput(String word) {
+        return false;
+    }
+
+    private static String encryptString(String word) {
+        return "";
+    }
+
+    /*
+     * Problem4의 솔루션 코드입니다.
+     *
+     * @param 청개구리 문자열로 바꿀 문자열입니다.
+     * @return 청개구리 문자열로 바뀐 문자열입니다.
+     * */
     public static String solution(String word) {
-        String answer = "";
+        if (isValidInput(word) == false) {
+            return "Error: Wrong input";
+        }
+        String answer = encryptString(word);
         return answer;
     }
 }
