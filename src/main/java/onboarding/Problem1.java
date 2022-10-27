4882367f8e8be37e0ba45dc33e4f5abba0f34693
penrose15 <seongju99@naver.com>
2022-10-27 15:15:01 +0900

    '우테코 problem1, problem2 solved'

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,78 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    /*
+        1. 각각 리스트의 자리 수를 구한다
+            10씩 나누어 나머지를 구하거나
+            문자열로 쪼갠 후 구하거나 등등....
+
+        2. 싹다 곱하거나 싹다 더해서 그 중에 Max값을 구한다.
+
+        3. 포비와 크롱의 Max 값을 구해서 비교한다
+            3-1. 포비 > 크롱 : 1
+            3-2. 포비 < 크롱 : 2
+            3-3. 포비 == 크롱 : 0
+            3-4. 예외 사항 : -1
+                예외사항 예시 :
+                    a. 왼쪽이 짝수 || 오른쪽이 홀수
+                    b. 왼쪽 + 1 != 오른쪽인 경우
+                    c. 가장 마지막 면 혹은 시작면이 나오는 경우(리스트 크기가 1이하인 경우)
+                    d. 페이지를 잘라먹는 꼼수 등등....
+        * */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+//        int answer = Integer.MAX_VALUE;
+//        return answer;
+        if(!pageValidation(pobi) || !pageValidation(crong)){
+            return -1;
+        }
+
+        int pobiMax = comparePages(pobi);
+        int crongMax = comparePages(crong);
+
+        if(pobiMax > crongMax) return 1;
+        else if (pobiMax < crongMax) return 2;
+        else return 0;
+
     }
+
+    static int comparePages(List<Integer> player) {
+        int max = 0;
+        for (Integer page : player) {
+            int tmp = max(page);
+
+            if(max < tmp) max = tmp;
+        }
+        return max;
+    }
+    static int max(int page) {
+        List<Integer> pageDigit = new ArrayList<>();
+        while(page > 0) {
+            int num = page % 10;
+            pageDigit.add(num);
+
+            page /= 10;
+        }
+        int sum = 0; int multi = 1;
+        for (Integer integer : pageDigit) {
+            sum += integer;
+            multi *= integer;
+        }
+        return Math.max(sum, multi);
+    }
+
+    static boolean pageValidation(List<Integer> player) {
+        for (Integer i : player) if (i < 1 || i > 400) {
+            return false;
+        }
+        if(player.size() != 2) return false;
+
+        if (player.get(0) % 2 != 1 || player.get(1) % 2 !=0) {
+            return false;
+        }
+        return player.get(1) - player.get(0) == 1;
+    }
+
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,67 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Problem2 {
+    /*
+    1. for문을 돌려 start를 찾음
+    2. 만약 start가 존재한다면 for문을 돌려 end 를 찾음
+    3. 이후 start ~ end 까지를 삭제
+    4. 문자열에 연속되는 문자열이 없을때 까지 1 - 5를 반복한다.
+    * */
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        while(cryptogram.length() > 1) {
+            if(cryptogram.length() == 2) {
+                if(cryptogram.charAt(0) == cryptogram.charAt(1)) {
+                    cryptogram = ""; break;
+                } else break;
+            }
+
+            int start = -1;
+            for(int i = 0; i<cryptogram.length()-1; i++) {
+                if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                    start = i; break;
+                }
+            }
+
+            if(start != -1) {
+                int end = start;
+                for(int i = start+1; i<cryptogram.length(); i++) {
+                    if(cryptogram.charAt(start) != cryptogram.charAt(i)) {
+                        end = i; break;
+                    }
+                }
+
+                StringBuilder sb = new StringBuilder();
+                sb.append(cryptogram);
+                sb.replace(start, end, "");
+                cryptogram = sb.toString();
+            } else break;
+        }
+        return cryptogram;
+
+        //처음에 생각했던 풀이방법. 불필요한 중복이 많아 위의 코드로 변경
+/*        while(true) {
+            Set<Integer> chars = new HashSet<>();
+            for(int i = 0; i< cryptogram.length()-1; i++) {
+                if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                    chars.add(i);
+                    chars.add(i+1);
+                }
+            }
+            if(chars.size() == 0) break;
+            StringBuilder sb = new StringBuilder();
+            sb.append(cryptogram);
+            for(Integer i : chars) {
+                sb.replace(i,i+1," ");
+            }
+            cryptogram = sb.toString();
+            cryptogram = cryptogram.replaceAll(" ", "");
+        }
+        return cryptogram;*/
     }
 }
