e8003df32005e9a730090a323921741b640dd1cf
Kimhyeyun <cqqwer@gmail.com>
2022-10-27 17:36:42 +0900

    [Problem 4] MAKE TRANSLATE CHAR FUNCTION

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (char w : word.toCharArray()) {
+            if(w == ' ') answer += " ";
+            else answer += translate(w);
+        }
+
         return answer;
+    }
+
+    private static char translate(char w) {
+        char c;
+        if (97 <= w && w <= 122) {
+            c = (char) (122 - (w - 97));
+        } else {
+            c = (char) (90 - (w - 65));
+        }
+        return c;
     }
 }
