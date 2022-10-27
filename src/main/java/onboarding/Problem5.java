92a289691df1ac0c7ec27e44bbe25bb939552f95
aossuper7 <aossuper7@naver.com>
2022-10-27 18:13:10 +0900

    feat: 문제5 몫을 구해 리턴

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] divide = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < divide.length; i++) {
+            result.add(money / divide[i]);
+            money %= divide[i];
+        }
+        return result;
     }
 }
