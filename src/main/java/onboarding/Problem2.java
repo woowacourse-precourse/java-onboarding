bac8df2ac265afff338eb42b04abeadd713b3d4f
sugamypapa <sk000801@naver.com>
2022-10-27 14:40:20 +0900

    feat: 문제 2 아이디어 및 풀이 실패

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+    // 뭔가 정규표현식으로 연속된 문자 제거하는 방식으로 풀면 될듯하다
+
+    public static String isContinue() {
+        String findContinue = "\"";
+        for(int i = 97; i <= 122; i++) {
+            findContinue  +=  Character.toString((char) i) + "{2, }";
+            if(i<122) findContinue += "|";
+            else findContinue += "\"";
+        }
+        System.out.println(findContinue);
+        return findContinue;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while(cryptogram.matches(isContinue())) {
+            cryptogram.replaceAll(isContinue(), "");
+        }
+        return cryptogram;
     }
 }
