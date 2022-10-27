47570815ee94eef2df03f7d691b6281d8814d23e
Dongmin Kim <kimdongmin68@naver.com>
2022-10-27 16:33:15 +0900

    Feat: 예외사항 기능 추가 (PROBLEM1)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,21 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        final int EXCEPTION = -1;
         int answer = Integer.MAX_VALUE;
+
+        if(!isOk(pobi) || !isOk(crong)) return EXCEPTION;
+
         return answer;
+    }
+
+    public static boolean isOk(List<Integer> bookPages){
+        int leftPage = bookPages.get(0);
+        int rightPage = bookPages.get(1);
+
+        if(leftPage<1 || leftPage>400 || rightPage<1 || rightPage>400) return false;
+        if(rightPage - leftPage != 1) return false;
+
+        return true;
     }
 }
