3760f370b33e9a505d4a17220b1b305525829d5f
glaehfdl1654 <glaehfdl1654@naver.com>
2022-10-27 23:42:05 +0900

    [Problem1] isError function complete

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-class Problem1 {
+public class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
     }
+    public static boolean isError(List<Integer> pobi, List<Integer> crong) {
+        for (int i = 0; i <= 1; i++) {
+            if (!(0 < pobi.get(i) && pobi.get(i) <= 400)) {
+                return true;
+            }
+            if (!(0 < crong.get(i) && crong.get(i) <= 400)) {
+                return true;
+            }
+        }
+        if (!((pobi.get(0) + 1 == pobi.get(1)) && (crong.get(0) + 1 == crong.get(1)))) {
+            return true;
+        }
+        if (!((pobi.get(1) % 2 == 0) && (crong.get(1) % 2 == 0))) {
+            return true;
+        }
+        return false;
+    }
+
 }
