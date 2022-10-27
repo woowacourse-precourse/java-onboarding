4c7f43308d05c0b86cf3db55a18bf5f45517c0d5
sangwon <swchoi1997@naver.com>
2022-10-28 00:08:18 +0900

    feat : 2~n개 연속되는 문자의 중복을 제거할 수 있게 로직 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * 1. 문자 입력 범위 확인
  * 2. 조건 : 연속하는 중복 문자들을 삭제
@@ -10,13 +14,73 @@ package onboarding;
  *   -> 처음에는 스택을 사용하여 변경감지와 동시에 앞에 중복데이터를 삭제하는 형식으로 로직을 구성하였다.
  *   -> 제일 끝 데이터를 처리하는 과정에서 오류가 발생 / 홀수개 중복 데이터 입력시 한 개씩 남는 현상 발생
  *   -> 그래서 생각했던 방법이 투포인터 + 스택이었다.
- *   -> 시간복잡도 측면에서는 비효율적일지 모르겠지만, 일단 데이터가 1000개이기도하고.. 더 할 수있는 방법을 못찾겟다.
+ *   -> 시간복잡도 측면에서는 비효율적일지 모르겠지만, 일단 데이터가 1000개이기도하고.. 더 할 수있는 방법을 못찾겟다.(다음주 1주차 토론에서 한번 물어봐야겠다.)
  *
+ *   words = "zzzzzzzzzzmmmmmz";
+ *   문제를 풀다가 드는 의문점이다
+ *   위 문자의 경우 앞에서부터 중복을 체크하면 z가 남고
+ *   m부터 체크를하면 z만 남는다. 만약 최소한으로 줄이라면 두번째로 하는게 맞는데 일단... 이 부분도 고려해봐야겠다.
  */
 public class Problem2 {
     public static String solution(String cryptogram) {
         checkValidRange(cryptogram.length());
         String answer = "answer";
+
+        Stack<String> stack = new Stack<>();
+
+        int indexNumber = 0;
+        while (true) {
+            boolean checkChanged = false;
+            String[] splitWord = cryptogram.split("");
+
+            if (String.join("", stack).trim().equals(String.join("", splitWord).trim())) {
+                break; // 종료조건 1번 (스택에 들어가 있는 데이터의 조합과 원본 데이터의 조합이 같을때. 즉, 중복이 발생하지않고 모두 돌았을 때
+            }
+
+            if (stack.size() == 0 ) {
+                stack.add(splitWord[indexNumber]);
+                indexNumber += 1;
+                continue;
+            }
+
+            if (!stack.peek().equals(splitWord[indexNumber])) {
+                stack.add(splitWord[indexNumber]);
+                indexNumber += 1;
+                continue;
+            }
+
+            if (stack.peek().equals(splitWord[indexNumber])) {
+                int samePoint = indexNumber - 1;
+                if (samePoint < 0) samePoint = 0;
+                List<Integer> eraseList = new ArrayList<>();
+                for (int i = samePoint; i < splitWord.length; i++) {
+                    if (!splitWord[samePoint].equals(splitWord[i])) {
+                        break;
+                    }
+                    eraseList.add(i);
+                }
+
+                for (Integer eraseIndex : eraseList) {
+                    splitWord[eraseIndex] = "";
+                    checkChanged = true;
+                }
+            }
+
+            String changedWord = String.join("", splitWord).trim();
+            cryptogram = changedWord;
+
+            indexNumber = 0;
+            stack.clear();
+
+            if (!checkChanged) {
+                break;
+            }
+            if (cryptogram.length() == 0) {
+                break;
+            }
+        }
+        answer = cryptogram;
+
         return answer;
     }
 
