2dcc7daf2b59ed90c08a0474b0ae4cf88e557553
hong-sile <gurwns9325@naver.com>
2022-10-27 14:42:16 +0900

    feat : 숫자에 3,6,9가 포함되어 있는 개수를 반환하는 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import onboarding.validatechecker.Problem3ValidateChecker;
+
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	private static int[] gameResult = new int[10001];
+
+	public static int solution(int number) {
+		Problem3ValidateChecker.isNumberValidate(number);
+		calculateGameResult(number);
+		return gameResult[number];
+	}
+
+	private static void calculateGameResult(int number){
+		for(int i = 1; i <= number; i++){
+			gameResult[i]=gameResult[i-1]+check369InNumber(i);
+		}
+	}
+
+	private static int check369InNumber(int number){
+		int count369=0;
+		while(number!=0){
+			int digit=number%10;
+			if(digit == 3 || digit == 6 || digit == 9)
+				count369+=1;
+			number=number/10;
+		}
+		return count369;
+	}
 }
