674b32f416d0146871b7ea8b92a3fdb98a199325
mrbonk <mrbonk97@gmail.com>
2022-10-27 15:23:13 +0900

    1번 해결

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,50 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(pobi.get(0) % 2 == 0) return -1;
+        if(crong.get(0) % 2 == 0) return -1;
+        if(pobi.get(0) + 1 != pobi.get(1)) return -1;
+        if(crong.get(0) + 1 != crong.get(1)) return -1;
+
+        int [] left = new int [] {0,1,0,1};
+        int [] right = new int [] {0,1,0,1};
+
+        for(int i = pobi.get(0); i > 0; i = i / 10)
+        {
+            int cur = i % 10;
+            left[0] += cur;
+            left[1] *= cur;
+        }
+
+        for(int i = pobi.get(1); i > 0; i = i / 10)
+        {
+            int cur = i % 10;
+            left[2] += cur;
+            left[3] *= cur;
+        }
+
+        for(int i = crong.get(0); i > 0; i = i / 10)
+        {
+            int cur = i % 10;
+            right[0] += cur;
+            right[1] *= cur;
+        }
+
+        for(int i = crong.get(1); i > 0; i = i / 10)
+        {
+            int cur = i % 10;
+            right[2] += cur;
+            right[3] *= cur;
+        }
+        int left_max = left[0];
+        int right_max = right[0];
+
+        for(Integer e: left) if(e > left_max) left_max = e;
+        for(Integer e: right) if(e > right_max) right_max = e;
+
+        if(left_max > right_max) return 1;
+        if(left_max < right_max) return 2;
+        return 0;
+    
     }
 }
