894ffa1e6939086f543191a32c93996eb4058da3
meunrud <y3a0522@gmail.com>
2022-10-28 03:11:14 +0900

    5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer = 0;
+
         int pobiMaxsum = maxSum(pobi.get(0),pobi.get(1));
         int pobiMaxmulti = maxMulti(pobi.get(0),pobi.get(1));
         int pobiMaxNum = Math.max(pobiMaxsum,pobiMaxmulti);
@@ -11,6 +13,8 @@ class Problem1 {
         int crongMaxsum = maxSum(crong.get(0),crong.get(1));
         int crongMaxmulti = maxMulti(crong.get(0), crong.get(1));
         int crongMaxNum = Math.max(crongMaxsum,crongMaxmulti);
+
+        answer = winner(pobiMaxNum,crongMaxNum);
 
 
 
@@ -61,6 +65,21 @@ class Problem1 {
             multiRightpage *=Integer.parseInt(page.substring(i,i+1));
         }
         return multiRightpage;
+    }
+
+    public static int winner(int pobiMaxNum, int crongMaxNum){
+        int winner = 0;
+        if(pobiMaxNum > crongMaxNum){
+            winner = 1;
+        }
+        if(pobiMaxNum < crongMaxNum){
+            winner = 2;
+        }
+        if (pobiMaxNum == crongMaxNum){
+            winner = 0;
+        }
+        return winner;
+
     }
 
 
