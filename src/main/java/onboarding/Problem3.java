22f9e49090c74bb28f5122b7b445cea5bf7c8b4e
Hongsugyeong <olivia258@naver.com>
2022-10-27 20:22:46 +0900

    [problem3] 솔루션

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,8 +5,7 @@ public class Problem3 {
      * 3,6,9 갯수 세는 함수
      */
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return count(number);
     }
 
     /*
