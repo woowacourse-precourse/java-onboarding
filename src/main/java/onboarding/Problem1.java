9e736d351f326434e6b714df9c7081f4d41a1f98
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 13:48:42 +0900

    문제 1 : 예외사항 기능구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,17 @@ import java.util.List;
  3. 자릿수 별로 곱하고 더하는 메소드
  **/
 class Problem1 {
+
+    // 예외 사항
+    // 1. 들어온 숫자가 홀수/짝수가 아닐 때
+    // 2. 연속된 숫자가 아닐 때
+    private static boolean exception(List<Integer> pobi, List<Integer> crong) {
+        if ((pobi.get(1) - pobi.get(0) != 1) || (crong.get(1) - crong.get(0) != 1)) return true;
+        if ((pobi.get(0) % 2 != 1) || (crong.get(0) % 2 != 1)) return true;
+        if (pobi.get(0) == 1 ||  pobi.get(0) == 399 || crong.get(0) == 1 || crong.get(0) == 399) return true;
+        return false;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
