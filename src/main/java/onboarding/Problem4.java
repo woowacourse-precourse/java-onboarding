029db1c511ff99b6e182496f7cc2b5339809b8b7
이석환 <2018008886@hanyang.ac.kr>
2022-10-28 01:33:24 +0900

    feat: add Problem4's solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < word.length(); i++) {
+            answer.append(encryptChar(word.charAt(i)));
+        }
+        return answer.toString();
+    }
+
+    private static char encryptChar(char plainChar) {
+        if(plainChar >= 65 && plainChar <= 90)
+            return (char) ((90 - plainChar) + 65);
+        else if(plainChar >= 97 && plainChar <= 122)
+            return (char) ((122 - plainChar) + 97);
+        else return plainChar;
     }
 }
