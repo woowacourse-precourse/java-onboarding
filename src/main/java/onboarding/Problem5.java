e0ab66157138da5d18baadedbbaa5e7df6fffbd1
qkrdmswl <eunji303@naver.com>
2022-10-27 14:30:15 +0900

    [feat] 출금 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /**
+     * 1. 입력받은 돈의 액수를 가장 큰 금액을 시작으로 나누어 몫을 구한다.
+     * 2. 남은 금액(mod)을 1번의 나머지 금액으로 갱신한다.
+     * 3. 위 과정 반복
+     */
+
+    public static List<Integer> withDrawMoney(int money) {
+        List<Integer> result = new ArrayList<Integer>();
+        int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int mod = money;
+
+        for (int value: currency) {
+            result.add(mod / value);
+            mod = money % value;
+        }
+        return result;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = withDrawMoney(money);
         return answer;
     }
 }
