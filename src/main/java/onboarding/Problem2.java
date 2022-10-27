259f4b4576b463a64940c396f149227816f4741a
jeongjihun <wlgns2234@naver.com>
2022-10-27 15:29:47 +0900

    Feat: 중복문자 끝 인덱스 찾기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,20 @@
 package onboarding;
 
 public class Problem2 {
+    public static int findIndexRedundancy(String str, int i) {
+        int end = i + 1;
+        char std = str.charAt(i);
+
+        while (end < str.length()) {
+            if (std != str.charAt(end)) {
+                break;
+            }
+            end += 1;
+        }
+
+        return end - 1;
+    }
+
     public static String removeRedundancyChar(String str, int start, int end) {
         String ret = str.substring(0, start) + str.substring(end+1);
         return ret;
