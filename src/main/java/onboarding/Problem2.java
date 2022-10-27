10c8f006a9ad839c16cfa850ade63d674b798bae
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-27 12:08:21 +0900

    문제 2 - 문자열을 배열로 변환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,20 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+
+		// 문자열을 배열로 변환
+		char[] chars = cryptogram.toCharArray();
+		// 배열의 길이가 1보다 작을 때 리턴
+		// 배열 내 인접 중복 문자열 존재 여부 확인
+		// 인접한 중복 문자열 제거
+		// stack을 문자열로 변환
+		// 해독할때까지 재귀
+
+		String answer = "answer";
+		return answer;
+	}
 
 }
