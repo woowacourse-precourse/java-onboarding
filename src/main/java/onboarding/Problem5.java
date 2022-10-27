ff8917cc25a6561f6187166877aa0acd2edf4cd4
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 20:24:10 +0900

    feat(P5): 입력 값을 받아 출금액을 리스트로 반환하는 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
+import onboarding.problem5.Atm;
+import onboarding.problem5.ChangesList;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        Atm atm = new Atm(new ChangesList());
+        return atm.withdraw(money);
     }
 }
--- src/main/java/onboarding/problem5/Atm.java
+++ src/main/java/onboarding/problem5/Atm.java
@@ -1,4 +1,35 @@
 package onboarding.problem5;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static onboarding.problem5.ValidationMoney.*;
+
 public class Atm {
+
+    private final ChangesList changesList;
+
+    public Atm(ChangesList changesList) {
+        this.changesList = changesList;
+    }
+
+    public List<Integer> withdraw(int money) {
+        validateMoney(money);
+        return getChanges(money);
+    }
+
+    private List<Integer> getChanges(int money) {
+        ArrayList<Integer> amounts = new ArrayList<>();
+        List<Integer> changes = changesList.getChangesList();
+
+        for (Integer change : changes) {
+            int count = money / change;
+
+            amounts.add(count);
+            money -= (change * count);
+        }
+
+        return amounts;
+    }
+
 }
