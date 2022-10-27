d7ae4dd672564e4ed7eb4737415905ab4b7fbfe2
Namyunsuk <yunsuk0328@naver.com>
2022-10-27 17:08:14 +0900

    feat(constant): 상수 관리 클래스 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+class constant{
+    static final String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static final String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+}
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
