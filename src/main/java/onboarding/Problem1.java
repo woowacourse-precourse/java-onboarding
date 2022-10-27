307d42a03dc7c64c5364411f36ed3c8b0f603a92
HyunggiPark <phg9587@naver.com>
2022-10-27 16:36:19 +0900

    feat : getMaxNumByPageList 메서드를 통해서 pobi 와 crong 둘 중에 누가 더 큰 값을 가지는지 확인 후 return한다. 예외가 발생시에는 -1을 리턴한다.
    getMaxNumByPageList를 통해서 return 된 값이 -1 이라면 예외가 발생한 것이다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+    public static int solution(List<Integer> pobiPageList, List<Integer> crongPageList) {
+        Integer pobiMaxNum = getMaxNumByPageList(pobiPageList);
+        int crongMaxNum = getMaxNumByPageList(crongPageList);
+
+        if (pobiMaxNum == -1 || crongMaxNum == -1) {
+            return -1;
+        }
+
+        return pobiMaxNum.compareTo(crongMaxNum);
+    }
+
     private static int getMaxNumByPageList(List<Integer> pageList) {
         int leftPage = pageList.get(0);
         int rightPage = pageList.get(1);
@@ -20,6 +28,7 @@ class Problem1 {
     private static boolean isRightPageNum(int page) {
         return 1 <= page && page <= 400;
     }
+
     private static int getMaxNumByPage(int page) {
         String pageStr = String.valueOf(page);
         int maxPlusNum = 0;
