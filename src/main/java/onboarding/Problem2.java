67edab2f5d0d394e65f78242eac536c39ac8bac0
Lee Sang Bin <rhkd2612@naver.com>
2022-10-27 17:25:47 +0900

    feat: compressString method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,8 @@ package onboarding;
 
 /*
  * # 기능 정리
- * [X] 연속하는 중복문자 검증
- * [X] 제거된 문자열 반환 메서드
+ * [O] 연속하는 중복문자 검증
+ * [O] 제거된 문자열 반환 메서드
  * [X] 1~2 기능 반복 메서드
  * [X] solution 작성
  * [X] 테스트 확인
@@ -12,6 +12,22 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
+    }
+
+    private static StringBuffer compressString(final String str){
+        StringBuffer compressedString = new StringBuffer();
+
+        compressedString.append(str.charAt(0));
+        for(int i = 1; i < str.length(); i++){
+            char beforeChar = str.charAt(i - 1);
+            char currentChar = str.charAt(i);
+
+            if(beforeChar != currentChar)
+                compressedString.append(currentChar);
+        }
+
+        return compressedString;
     }
 }
