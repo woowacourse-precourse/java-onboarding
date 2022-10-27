a9094a465861c5b8dec29cb6362803d6e6a1c3e2
YE-EUN <musiclove529@naver.com>
2022-10-27 17:07:29 +0900

    feature : make structure - solution 구현 및 사용할 함수 구조 잡기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1;i<=number;i++){
+            ArrayList<Integer> digits = getDigits(number);
+            answer += countClap(digits);
+        }
+
         return answer;
     }
+
+    public static ArrayList<Integer> getDigits(int number){
+        ArrayList<Integer> digits = new ArrayList<>();
+
+        return digits;
+    }
+
+    public static int countClap(ArrayList<Integer> digits){
+        int count = 0;
+
+        return count;
+    }
+
 }
