a3b820900d7bbec5c5cc19843bc8c009ce6df1cf
5uhwann <5uhwann27@gmail.com>
2022-10-27 12:14:00 +0900

    feat: PROBLEM2 - 기능3 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+  public static String solution(String cryptogram) {
+    String answer = "answer";
+    return answer;
+  }
+
+  public static boolean isAllowedString(String cryptogram) throws IllegalStateException {
+    for (int i = 0; i < cryptogram.length(); i++) {
+      int temp = cryptogram.charAt(i) - 0;
+      if (temp < 97 || temp > 122) {
+        throw new IllegalStateException("소문자 알파벳으로 이루어진 문자열이 아닙니다.");
+      }
     }
+    return true;
+  }
 }
