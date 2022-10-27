bbf053220cbb1ac797a6e5d4f0a95d5f2aa3b8be
JunginPark4364 <wjddlsee@gmail.com>
2022-10-28 02:43:57 +0900

    feat: check input is wrong
    - 매개변수로 받은 pobi, crong 의 유효성 체크
    - 유효하지 않은 값이면 예외사항(-1) 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,27 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static final int EXCEPTION = -1;
+    public static final int POBI_WIN = 1;
+    public static final int CRONG_WIN = 2;
+    public static final int DRAW = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (isWrongInput(pobi, crong)) {
+            return EXCEPTION;
+        }
+
         return answer;
+    }
+
+    // 입력된 페이지들이 조건에 맞는지 확인
+    public static boolean isWrongInput(List<Integer> pobi, List<Integer> crong) {
+        if (isFirstOrLast(pobi, crong) || isNotConsecutive(pobi, crong)) {
+            return true;
+        }
+        return false;
     }
 
     // 연속된 페이지가 아닌경우
