2f0096f879a31779a6bcc1e7638b3423664e9239
KangSungBum <peter9706@naver.com>
2022-10-27 19:33:33 +0900

    feat: Problem2 기능 구현 완료
    removeRedundateSequence 메소드 추가 : 입력받은 문자열 중복문자 제거(substring, stringbuilder)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,56 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (true) {
+            String inputStr = cryptogram;
+
+            String outputStr = removeRedundantSequence(inputStr);
+
+            if (outputStr.equals("")) {
+                answer = "";
+                break;
+            }
+
+            if (inputStr.equals(outputStr)) {
+                answer = outputStr;
+                break;
+            }
+
+            cryptogram = outputStr;
+        }
+
         return answer;
+    }
+
+    private static String removeRedundantSequence(String crypto) {
+        StringBuilder sb = new StringBuilder();
+
+        if (crypto.charAt(0) == crypto.charAt(1)) {
+            if (crypto.length() == 2) {
+                return "";
+            }
+
+            sb.append(crypto.substring(2));
+
+            return sb.toString();
+        }
+
+        for (int i = 1; i < crypto.length() -2; i++) {
+            if (crypto.charAt(i) == crypto.charAt(i + 1)) {
+                sb.append(crypto, 0, i);
+                sb.append(crypto.substring(i + 2));
+
+                return sb.toString();
+            }
+        }
+
+        if (crypto.charAt(crypto.length() - 2) == crypto.charAt(crypto.length() - 1)) {
+            sb.append(crypto, 0, crypto.length() - 2);
+
+            return sb.toString();
+        }
+
+        return crypto;
     }
 }
