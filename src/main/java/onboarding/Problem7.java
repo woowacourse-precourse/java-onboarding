7883670970b3cc4b832ec00b5a067d901ac9739b
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-28 04:52:42 +0900

    feat(problem7): add friend point

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,15 +2,19 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
 
+	private static final int FRIEND_POINT = 10;
 	static List<String> friendName = new ArrayList<>();
+	static HashMap<String, Integer> recommendation = new HashMap<>();
 
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
 		findFriend(user, friends);
+		findFriendofFriend(user, friends);
 		List<String> answer = Collections.emptyList();
 		return answer;
 	}
@@ -23,5 +27,36 @@ public class Problem7 {
 				friendName.add(friend);
 			}
 		}
+	}
+
+	private static boolean isFriend(String friend) {
+		return friendName.contains(friend);
+	}
+
+	private static void findFriendofFriend(String user, List<List<String>> friends) {
+		for (List<String> relation : friends) {
+			if (relation.contains(user)) {
+				continue;
+			}
+
+			String friend1 = relation.get(0);
+			String friend2 = relation.get(1);
+
+			if (isFriend(friend1) ^ isFriend(friend2)) {
+				if (isFriend(friend1)) {
+					addFriendPoint(friend2);
+				} else {
+					addFriendPoint(friend1);
+				}
+			}
+		}
+	}
+
+	private static void addFriendPoint(String user) {
+		int score = FRIEND_POINT;
+		if (recommendation.containsKey(user)) {
+			score += recommendation.get(user);
+		}
+		recommendation.put(user, score);
 	}
 }
