2833eb8082f955d575609a4339cf1c3b35a17ffc
ParkRootSeok <g_e_un@naver.com>
2022-10-27 22:24:41 +0900

    Problem3 - 각 자릿수 분리 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,13 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
 
     public static int countClap(int number) {
 
     }
 
-    public static int parseNum(int number) {
+    public static List<Integer> parseNum(int number) {
 
+        List<Integer> digits = new ArrayList<>();
+
+        while (number > 0) {
+            digits.add(number % 10);
+            number /= 10;
+        }
+
+        return digits;
     }
 
     public static boolean compareNumWith369(int digit) {
