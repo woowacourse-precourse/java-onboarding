8f68f5611c27199ba39115e8322e49fd329b38d4
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-27 16:09:01 +0900

    feat : 그래프 구조 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,59 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
+
+    private static Map<String, LetterNode> letterNodeMap = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        for (List<String> infos : forms) {
+            String nickname = infos.get(1);
+
+            String[] letters = nickname.split("");
+            LetterNode prev = null;
+
+            for (String letter : letters) {
+                LetterNode letterNode =
+                    letterNodeMap.getOrDefault(letter, new LetterNode(letter));
+                letterNodeMap.put(letter, letterNode);
+                if (prev == null) {
+                    prev = letterNode;
+                    continue;
+                }
+
+                prev.addNext(letterNode);
+                prev = letterNode;
+            }
+        }
+
+
         return answer;
+    }
+}
+
+class LetterNode {
+    private String letter;
+    private Set<LetterNode> next = new HashSet<>();
+
+    public LetterNode(String letter) {
+        this.letter = letter;
+    }
+
+    public void addNext(LetterNode letterNode) {
+        this.next.add(letterNode);
+    }
+    public String getLetter() {
+        return letter;
+    }
+
+    public Set<LetterNode> getNext() {
+        return next;
     }
 }
