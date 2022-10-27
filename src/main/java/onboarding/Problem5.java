ff74412a5e30d297002b9df6e9bcb0825a2fc84b
Your Name <you@example.com>
2022-10-27 17:37:05 +0900

    Problem5 기능 목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,15 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] unitOfMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        /*금액이 큰 순서대로 추가*/
+        
         return answer;
     }
 }
