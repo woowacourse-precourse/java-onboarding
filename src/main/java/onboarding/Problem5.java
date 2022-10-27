421a3a477ef24da1d74cc18a0c9e8a853eb6397b
sunwng <sunwgna@gmail.com>
2022-10-27 17:37:04 +0900

    [problem_5] feature updated, test completed

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
+    static final int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    static final int totalLen = 9;
+    static List<Integer> result;
+
+    public static void transformer(int money) {
+        int quotient;
+        for (int i = 0; i < totalLen; i++) {
+            quotient = (int) money / currency[i];
+            result.add(quotient);
+            money -= quotient * currency[i];
+        }
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+//        List<Integer> answer = Collections.emptyList();
+        result = new ArrayList<>();
+        transformer(money);
+        return result;
     }
 }
