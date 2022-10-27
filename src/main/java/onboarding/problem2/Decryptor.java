f839d9f897d47046ef09b0748bc664173e87a751
situodys <situodys@gmail.com>
2022-10-27 19:15:07 +0900

    feat(문제2): 2개의 연속된 문자 존재 시 제거

--- src/main/java/onboarding/problem2/Decryptor.java
+++ src/main/java/onboarding/problem2/Decryptor.java
@@ -1,9 +1,38 @@
 package onboarding.problem2;
 
+import java.util.Stack;
+
 public class Decryptor {
-    String cryptogram=null;
+    String cryptogram;
+    Stack<Character> decryptor;
 
     public Decryptor(String cryptogram) {
         this.cryptogram = cryptogram;
+        decryptor = new Stack<>();
+    }
+
+    public String decode() {
+        for (int idx = 0; idx < cryptogram.length(); idx++) {
+            char nextCharacter = cryptogram.charAt(idx);
+            if (IsDecryptorThenPush(nextCharacter)) continue;
+            if (isDuplicate(nextCharacter)) {
+                decryptor.pop();
+            }
+        }
+        return "";
+    }
+
+    private boolean IsDecryptorThenPush(char nextCharacter) {
+        if (decryptor.empty()) {
+            decryptor.push(nextCharacter);
+            return true;
+        }
+        return false;
+    }
+    private boolean isDuplicate(char next){
+        if (decryptor.peek() == next) {
+            return true;
+        }
+        return false;
     }
 }
