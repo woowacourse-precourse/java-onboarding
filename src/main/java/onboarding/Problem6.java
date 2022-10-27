6d5bf5b1bfb4d59040cb14a8d888e16093b84b90
MuSeung <peter0757@nvaer.com>
2022-10-27 22:06:07 +0900

    feat : Problem6의 2글자씩 끊어서 체크하는 checkNickName과 받은 2글자가 usedNickName에 있는지, 있으면 email을 emailToBeSent에 추가하는 checkNickNamePart구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    public static void checkNickName(List<String> emailAndName){
+        HashMap<String, String> myUsedNickNamePart = new HashMap<>();
+        String myEmail = emailAndName.get(0);
+        String myNickName = emailAndName.get(1);
+        for(int i = 1; i < myNickName.length(); i++){
+            String myNickNamePart = "" + myNickName.charAt(i - 1) + myNickName.charAt(i);
+            checkNickNamePart(myNickNamePart, myEmail);
+            myUsedNickNamePart.put(myNickNamePart,myEmail);
+        }
+        usedNickNamePart.putAll(myUsedNickNamePart);
+    }
+
+    public static void checkNickNamePart(String myNickNamePart, String myEmail){
+        if(usedNickNamePart.containsKey(myNickNamePart)){
+            emailToBeSent.add(usedNickNamePart.get(myNickNamePart));
+            emailToBeSent.add(myEmail);
+        }
     }
 }
