c94fa73554a6de80b5e255c534f795d1b547f76e
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-27 14:57:40 +0900

    Test: Problem6 테스트케이스 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -124,6 +124,23 @@ class ApplicationTest {
 			List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
 			assertThat(Problem6.solution(forms)).isEqualTo(result);
 		}
+
+		@Test
+		void case2() {
+			List<List<String>> forms = List.of(
+				List.of("jm@email.com", "제이엠"),
+				List.of("jason@email.com", "제이슨"),
+				List.of("woniee@email.com", "워니"),
+				List.of("mj@email.com", "엠제이"),
+				List.of("nowm@email.com", "이제엠"),
+				List.of("jm@email.com", "제이엠"),
+				List.of("jason@email.com", "제이슨"),
+				List.of("jason@gmail.com", "제이슨"),
+				List.of("hiads@gmail.com", "제")
+			);
+			List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
+			assertThat(Problem6.solution(forms)).isEqualTo(result);
+		}
 	}
 
 	@Nested
