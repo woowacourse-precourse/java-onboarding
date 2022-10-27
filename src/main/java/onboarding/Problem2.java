2251d7204e4eaab353232966ca62e67e9bf842da
UJ15 <dbwogml15@naver.com>
2022-10-27 10:36:43 +0900

    feat : add removeDuplicate method for Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = removeDuplicate(cryptogram);
+
+        return "";
+    }
+
+    private static Stack<Character> removeDuplicate(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : cryptogram.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop();
+                continue;
+            }
+
+            stack.push(ch);
+        }
+
+        return stack;
     }
 }
