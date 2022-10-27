c6c9bc3ef5e290c716f36d1a9bffdd8909906851
jhon3242 <rldotnr2@gmail.com>
2022-10-27 15:13:12 +0900

    [feat] 문제 7 Map 생성 및 순회하면서 객체 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,9 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
-    class User{
+    static class User{
         final String name;
         List<String> friends;
         List<String> visitor;
@@ -21,10 +19,43 @@ public class Problem7 {
         public void addFriend(String name){
             friends.add(name);
         }
+
+        public void setVisitor(List<String> visitor) {
+            this.visitor = visitor;
+        }
     }
+
+    public static Map<String, User> repository = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
+        initFriend(friends);
+
+        User userInstance = repository.get(user);
+        userInstance.setVisitor(visitors);
+
         return answer;
+    }
+
+    private static void initFriend(List<List<String>> friends) {
+        for (List<String> friend : friends){
+            String friendA = friend.get(0);
+            String friendB = friend.get(1);
+
+            setFriendToMap(friendA, friendB);
+        }
+    }
+
+    private static void setFriendToMap(String friendA, String friendB) {
+        User tmpUser;
+
+        tmpUser = repository.getOrDefault(friendA, new User(friendA));
+        tmpUser.addFriend(friendB);
+        repository.put(friendA, tmpUser);
+
+        tmpUser = repository.getOrDefault(friendB, new User(friendB));
+        tmpUser.addFriend(friendA);
+        repository.put(friendB, tmpUser);
     }
 }
