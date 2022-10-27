1c55f9f3e6934187209540b4a7261bed5ce10fe0
viyott <jackk0616@gmail.com>
2022-10-27 16:38:00 +0900

    p5 기능 목록 설정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /**
+     * 기능 목록
+     * 1. 화폐 단위를 배열로 만들고
+     * 2. 단위보다 money가 크다면 money를 단위로 나눈 나머지를 뺀다
+     * 3. 그 수를 단위로 나눠서 나온 지폐 수를 answer에 저장
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
