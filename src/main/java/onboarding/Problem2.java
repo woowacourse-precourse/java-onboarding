fa079b9dbeb2ff602cf5ccad5d3c794cd653511e
woo-chang <clllickme@naver.com>
2022-10-27 17:13:37 +0900

    feat: 문자열에 대해 중복 문자 삭제한 문자열 추출 기능 (Problem2)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+  public static String solution(String cryptogram) {
+    Stack<Character> stack = new Stack<>();
+
+    int index = 0;
+    while (index < cryptogram.length()) {
+      if (stack.isEmpty() || !stack.peek().equals(cryptogram.charAt(index))) {
+        stack.push(cryptogram.charAt(index));
+        ++index;
+        continue;
+      }
+      while (index < cryptogram.length() && stack.peek().equals(cryptogram.charAt(index))) {
+        ++index;
+      }
+      stack.pop();
     }
+
+    return stack.stream().map(Object::toString).collect(Collectors.joining());
+  }
 }
