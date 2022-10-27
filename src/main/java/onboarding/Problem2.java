f46786563e9910ccbd70cc9af054c61e63d4e594
youhobin <ghdb132@naver.com>
2022-10-27 18:35:38 +0900

    refactor : 변수 이름 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,12 +2,12 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        for (int i = cryptogram.length() - 1; i > 0; i--) {
-            char currChar = cryptogram.charAt(i);
-            char prevChar = cryptogram.charAt(i - 1);
+        for (int index = cryptogram.length() - 1; index > 0; index--) {
+            char currChar = cryptogram.charAt(index);
+            char prevChar = cryptogram.charAt(index - 1);
 
             if (currChar == prevChar) {
-                cryptogram = cryptogram.substring(0,i - 1) + cryptogram.substring(i + 1);
+                cryptogram = cryptogram.substring(0, index - 1) + cryptogram.substring(index + 1);
             }
         }
         return cryptogram;
