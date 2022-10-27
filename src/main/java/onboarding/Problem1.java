212284273beb94bfe8d3546d05251703629dcd1f
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-27 23:23:05 +0900

    check if left is odd and right is even

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,11 +5,16 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (notInOnePage(pobi) || notInOnePage(crong)) return -1;
+        if (isOddAndEven(pobi) || isOddAndEven(crong)) return -1;
         int pobiMax = getMaxPage(pobi.get(0)) > getMaxPage(pobi.get(0)) ? getMaxPage(pobi.get(0)) : getMaxPage(pobi.get(0));
         int crongMax = getMaxPage(crong.get(0)) > getMaxPage(crong.get(0)) ? getMaxPage(crong.get(0)) : getMaxPage(crong.get(0));
         if (pobiMax == crongMax) return 0;
         else if (pobiMax > crongMax) return 1;
         else return 2;
+    }
+
+    public static boolean isOddAndEven(List<Integer> intList) {
+        return (intList.get(0) % 2 == 0 || intList.get(1) % 2 == 1) ? true : false;
     }
 
     public static boolean notInOnePage(List<Integer> intList) {
