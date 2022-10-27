23d9d19d66af20d4392c514a10fae2780f36a0da
김진호 <rlawlsgh1227@gmail.com>
2022-10-27 13:41:10 +0900

    feat: Complete problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,38 +7,43 @@ public class Problem2 {
     public static String solution(String cryptogram) {
 
         List<Integer> duplicateIndex = new ArrayList<>();
-
-        String DeleteDuplicate;
-
-        for(int i = 0; i < cryptogram.length() - 1; i++){
-            String nowString = cryptogram.substring(i, i + 1);
-            String nextString = cryptogram.substring(i + 1, i + 2);
-
-            if (nowString.equals(nextString)) {
-                duplicateIndex.add(i);
-                duplicateIndex.add(i + 1);
-            }
-        }
-
         StringBuffer sb = new StringBuffer();
         sb.append(cryptogram);
 
-        int forCount = 0;
+        do {
+            // 여러 번 진행하기 위해 초기화
+            duplicateIndex.clear();
 
-        for (Integer duplicateIndexNum : duplicateIndex) {
-            sb.deleteCharAt(duplicateIndexNum - forCount);
-            forCount++;
-        }
+            for(int i = 0; i < sb.length() - 1; i++) {
+                String nowString = sb.substring(i, i + 1);
+                String nextString = sb.substring(i + 1, i + 2);
 
-        System.out.println("sb = " + sb);
+                if (nowString.equals(nextString)) {
+                    duplicateIndex.add(i);
+                    duplicateIndex.add(i + 1);
+                }
+            }
 
-        return "";
+            int forCount = 0;
+
+            for (Integer duplicateIndexNum : duplicateIndex) {
+                sb.deleteCharAt(duplicateIndexNum - forCount);
+                forCount++;
+            }
+
+            forCount = 0;
+
+            System.out.println("sb = " + sb);
+        } while(!duplicateIndex.isEmpty());
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         String cryptogram = "browoanoommnaon";
 
         String solution = solution(cryptogram);
+        System.out.println("solution = " + solution);
     }
 }
 
