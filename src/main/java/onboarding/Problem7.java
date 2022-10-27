6fa68bfa9eba1933c2ecff511c89e6192dcacf00
K-jun98 <alap_u@naver.com>
2022-10-28 01:05:17 +0900

    feat - 유저와 친구인 사람을 조회한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static List<String> userFriends = new ArrayList<>();
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        getUserFriends(user, friends);
+
     }
+
+    private static void getUserFriends(String user, List<List<String>> friends) {
+        for (List<String> friendship : friends) {
+            if (friendship.get(0) == user) {
+                userFriends.add(friendship.get(1));
+            } else if (friendship.get(1) == user) {
+                userFriends.add(friendship.get(0));
+            }
+        }
+    }
+
 }
