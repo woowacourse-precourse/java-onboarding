210f26a3a2d1e1c0abc8b9f568dcd2ceed55aded
CMHyuk <alsgur9710@naver.com>
2022-10-27 22:35:46 +0900

    feat: 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    public static final int firstPage = 1;
+    public static final int LastPage = 400;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pLeftPage = pobi.get(0);
+        int pRightPage = pobi.get(1);
+
+        int cLeftPage = crong.get(0);
+        int cRightPage = crong.get(1);
+
+        if(validation(pLeftPage, pRightPage)) {
+            return -1;
+        }
+
+        if(validation(cLeftPage, cRightPage)) {
+            return -1;
+        }
+
+
         return answer;
+    }
+
+    private static boolean validation(int leftPage, int rightPage) {
+        return isLeftOddNumberAndRightEvenNumberPage(leftPage, rightPage) || isNextPage(leftPage, rightPage) || isFirstAndLastPage(leftPage, rightPage);
+    }
+
+    private static boolean isFirstAndLastPage(int leftPage, int rightPage) {
+        return leftPage == firstPage || rightPage == LastPage;
+    }
+
+    private static boolean isNextPage(int leftPage, int rightPage) {
+        return rightPage - leftPage != 1;
+    }
+
+    private static boolean isLeftOddNumberAndRightEvenNumberPage(int leftPage, int rightPage) {
+        return leftPage % 2 == 0 || rightPage % 2 != 0;
     }
 }
