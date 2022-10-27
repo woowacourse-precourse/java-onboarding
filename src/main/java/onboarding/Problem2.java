30bd5f887b6924de616316701a43ffd00ae5c89a
Limjongho <whdgh9873@icloud.com>
2022-10-27 14:33:36 +0900

    feat(Problem2): 현재 LinkedList 가 비어있는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,14 @@
 package onboarding;
 
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private boolean isEmpty(LinkedList<Character> deduplicationLetters) {
+        return deduplicationLetters.isEmpty();
     }
 }
