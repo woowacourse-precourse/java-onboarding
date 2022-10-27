b2d56c3d847abd47650ee437fce23eac33d7cd47
davejang <davejang98@naver.com>
2022-10-27 17:55:54 +0900

    refactor: variable name modify
    코드 변수명 오타 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,26 +6,26 @@ import java.util.HashSet;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        HashMap<Character,Character> dictonary = new HashMap<>();
+        HashMap<Character,Character> dictionary = new HashMap<>();
         char[] chars = word.toCharArray();
 
         int currentUpperValue = 'Z';
         for(int ascii = 'A'; ascii <= 'Z'; ascii++) {
-            dictonary.put((char) ascii, (char) currentUpperValue);
+            dictionary.put((char) ascii, (char) currentUpperValue);
             currentUpperValue--;
         }
 
         int currentLowerValue = 'z';
         for(int ascii = 'a'; ascii <= 'z'; ascii++){
-            dictonary.put((char)ascii,(char)currentLowerValue);
+            dictionary.put((char)ascii,(char)currentLowerValue);
             currentLowerValue--;
         }
 
         for(int index=0; index < chars.length; index++) {
-            if(dictonary.containsKey(chars[index]) == true){
-                answer = answer + dictonary.get(chars[index]);
+            if(dictionary.containsKey(chars[index]) == true){
+                answer = answer + dictionary.get(chars[index]);
             }
-            if(dictonary.containsKey(chars[index]) == false){
+            if(dictionary.containsKey(chars[index]) == false){
                 answer = answer + chars[index];
             }
         }
