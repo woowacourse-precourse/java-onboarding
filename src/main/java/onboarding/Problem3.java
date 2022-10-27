e6e63419f67d22ab09dfaa68be307409c32ff23a
honey170 <63653475+hingjae@users.noreply.github.com>
2022-10-28 01:46:36 +0900

    problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import onboarding.problem3.Calculator;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = 0, tmp;
+        Calculator calculator = new Calculator();
+        for(int i = 1; i <= number; i++){
+            tmp = calculator.func(i);
+            answer += tmp;
+        }
         return answer;
     }
 }
