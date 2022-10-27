ce36aa895de56328cf77beadf2090da64bf87e19
0112leesy <0112leesy@naver.com>
2022-10-27 21:02:45 +0900

    [Problem2] feat: string_to_stack

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
+    static Stack<Character> string_to_stack(String input){
+        Stack<Character> input_stack = new Stack<Character>();
+        for(int i=0; i<input.length(); i++){
+            input_stack.push(input.charAt(i));
+        }
+        return input_stack;
+    }
+
 }
