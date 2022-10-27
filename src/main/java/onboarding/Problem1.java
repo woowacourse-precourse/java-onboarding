6f59b66fc9be758020772f21e2f1a9048587d577
situodys <situodys@gmail.com>
2022-10-27 16:19:22 +0900

    feat(문제1): 입력으로부터 포비,크롱 Pages 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,26 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        Pages pobiPages = new Pages(pobi);
+        Pages crongPages = new Pages(crong);
+
         return answer;
     }
+
+    public static class Pages {
+        private static final int LEFT_INDEX = 0;
+        private static final int RIGHT_INDEX = 1;
+
+        private int leftNumber;
+        private int rightNumber;
+
+        public Pages(List<Integer> indexes) {
+            this.leftNumber=indexes.get(LEFT_INDEX);
+            this.rightNumber=indexes.get(RIGHT_INDEX);
+        }
+
+
+    }
+
 }
