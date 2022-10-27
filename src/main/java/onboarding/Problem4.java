919efb2a2a6462f851e24200c90707c4260beabc
Youngho <cyh6099@gmail.com>
2022-10-28 02:34:12 +0900

    feat(Problem4): 문자인지 확인하는 함수 구현, 대문자 소문자에 따라 대응하는 문자 변환 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,15 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static boolean checkAlpha(char letter) {
+        return Character.isAlphabetic(letter) ;
+    }
+
+    public static int changeCharacter(char letter) {
+        if (Character.isUpperCase(letter)) {
+            return 25 - letter + 'A' * 2;
+        }
+        return 25 - letter + 'a' * 2;
+    }
 }
