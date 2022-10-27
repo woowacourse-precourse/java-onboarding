9173eda2a18cddc16a90a8b432f8e31b56595624
eugene1204 <s1061001@naver.com>
2022-10-27 16:45:08 +0900

    p1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,10 +6,6 @@ import java.util.List;
 class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        // 1. 페이지가 연속적인가 -> 아닐경우 return -1
-        // 2. 오른쪽 왼쪽 계산해주는 메소드
-        // 3. 나의 큰수 결정하는 메소드
-        // 4. 포비랑 크롱을 비교하는 메소드 -> return 값을 정하는 메소드 : solution
         if(checkPage(pobi) && checkPage(crong)){
             if(calMax(pobi)>calMax(crong)) return 1;
             else if(calMax(pobi)<calMax(crong)) return 2;
@@ -24,9 +20,6 @@ class Problem1 {
     }
 
     public static int calMax(List<Integer> list){ // 왼쪽 오른쪽 페이지 계산해주는 메소드
-        // 왼쪽 페이지의 덧셈 곱셈을  -> 큰수결정
-        // 오른쪽 페이지의 덧셈 곱셈을  -> 큰수결정
-        // 왼 vs 오 큰수 결정 해서 반환
         int leftNum = Math.max(calPlus(list.get(0)), calMulti((list.get(0))));
         int rigthNum = Math.max(calPlus(list.get(1)), calMulti((list.get(1))));
         int max = Math.max(leftNum,rigthNum);
