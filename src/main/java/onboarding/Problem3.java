84a7b31d55e975668269da49b7e5a07ec0240008
Minjune <minjune8506@gmail.com>
2022-10-27 19:34:51 +0900

    feat(Problem3): 3,6,9 개수를 찾는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int count(int number) {
+        int cnt = 0;
+        while (number != 0) {
+            int temp = number % 10;
+            if (temp != 0 && temp % 3 == 0) {
+                cnt++;
+            }
+            number /= 10;
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 3; i <= number; i++) {
+            answer += count(i);
+        }
         return answer;
     }
 }
