d12dbcc4c7bfbcb777f174c677c4aad18dd6d0ea
suheonjoo <suheonj95@naver.com>
2022-10-27 20:32:54 +0900

    feat(Problem4): Problem4 메인 로직 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,28 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            char wordChar = word.charAt(i);
+            if (!(('a' <= wordChar && wordChar <= 'z') || ('A' <= wordChar && wordChar <= 'Z'))) {
+                answer += wordChar;
+                continue;
+            }
+            String reversedString = getReversedString(wordChar);
+            answer += reversedString;
+        }
+
         return answer;
     }
+
+    private static String getReversedString(char ch) {
+        char reversedString;
+        if (ch >= 97) {
+            reversedString = (char) (25 - (ch - 'a') + 'a');
+            return reversedString + "";
+        }
+        reversedString = (char) (25 - (ch - 'A') + 'A');
+        return reversedString + "";
+
+    }
+
 }
