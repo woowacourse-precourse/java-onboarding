72a7aa06f247f364cbc752352c21777c23981e68
Heosejin98 <sejin3319@naver.com>
2022-10-27 21:11:39 +0900

    Feat : PROBLEM2 예외 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
+    static boolean exception(String cryptogram) {
+        if (cryptogram.length() < 1 || 1000 <cryptogram.length())
+            return true;
+        for (char c : cryptogram.toCharArray()) {
+            if (!(Character.isLowerCase(c)))
+                return true;
+        }
+        return false;
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        if(exception(cryptogram))
+            return answer;
         return answer;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
