74afcab3a129483e048a7b8112b754b2585e0395
rhqudco <rhqudco1204@naver.com>
2022-10-27 11:37:07 +0900

    FEAT : Create Clapcount Function, DOCS : Requirement Analysis

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
-
+/*
+* 1. 박수 카운트 체크하는 함수 생성
+* 2. 조건(1 ~ 10,000)에 따라 1과 2가 들어오면 바로 0 리턴
+* 3. 반복문을 따라서 몫과, 나머지를 통해 3, 6, 9를 구함.
+* 4. 1부터 solution 파라미터 number까지 반복.
+* */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static int clapCount(int num) {
+
     }
 }
