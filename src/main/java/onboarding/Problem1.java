d1742f443efeabe3063d94de24c68a2957c45a17
chj <bluedog129@gmail.com>
2022-10-27 15:40:09 +0900

    problem1 연산결과 비교 구현(left, right)(pobi, crong)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,7 +9,6 @@ class Problem1 {
             result += page % 10;
             page /= 10;
         }
-
         return result;
     }
     private static int multiplyPlaceValue(int page) {
@@ -20,8 +19,45 @@ class Problem1 {
         }
         return result;
     }
+    private static int compareValue(int page) {
+        int plusValue = plusPlaceValue(page);
+        int multiplyValue = multiplyPlaceValue(page);
+
+        return Math.max(plusValue, multiplyValue);
+    }
+    private static int getMaxPage(int maxValue, int page) {
+        if (maxValue < page) {
+            maxValue = page;
+        }
+        return maxValue;
+    }
+    private static int getMaxValue(List<Integer> pages){
+        int maxValue = 0;
+        for (int page : pages) {
+            int value = compareValue(page);
+            maxValue = getMaxPage(maxValue, value);
+        }
+        return maxValue;
+    }
+    private static int getResult(int pobiMax, int crongMax) {
+        if (pobiMax == crongMax) {
+            return 0;
+        } else if (pobiMax > crongMax) {
+            return 1;
+        } else if (pobiMax < crongMax) {
+            return 2;
+        }
+        return -1;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int result = 0;
+        int pobiMax = 0;
+        int crongMax = 0;
+
+        pobiMax = getMaxValue(pobi);
+        crongMax = getMaxValue(crong);
+        result = getResult(pobiMax, crongMax);
+
+        return result;
     }
 }
