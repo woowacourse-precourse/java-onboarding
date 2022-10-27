4003fa3a98fdc051390681e640afa0a2b0cf73ec
yoondgu <doyou2ing@naver.com>
2022-10-27 14:43:12 +0900

    feat(Problem3): implement get369removedString method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+//        System.out.println(get369removedString(String.valueOf(number)));
         return answer;
     }
+    
+    private static String get369removedString(String numToString) {
+    	String result = "";
+    	result = numToString.replaceAll("3", "")
+    			.replaceAll("6", "")
+    			.replaceAll("9", "");
+    	return result;
+    }
+    
 }
