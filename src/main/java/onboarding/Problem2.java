9efd94f8e9532527c28b46ce388ef6e81342217b
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-10-28 02:13:42 +0900

    Feat : 문제 2 기능 구현 기능 2.1.1 구현 및 테스트 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,57 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+
+    private static List<Character> chars;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        chars = getChars(cryptogram);
+
+        removeDuplicates(chars);
+
+        StringBuilder answer = new StringBuilder();
+        for (Character c : chars)
+            answer.append(c);
+
+        return answer.toString();
+    }
+
+    /**
+     * 2.1.1
+     * 문자열에서 문자 리스트 get
+     */
+    public static List<Character> getChars(String word) {
+        chars = new ArrayList<>();
+
+        for (int i = 0; i < word.length(); i++)
+            chars.add(word.charAt(i));
+
+        return chars;
+    }
+
+    /**
+     * 2.1.2
+     * 문자 리스트에서 중복 문자 제거
+     */
+    public static void removeDuplicates(List<Character> characters) {
+        int index = characters.size() - 1;
+
+        while (index > 0) {
+            if (characters.get(index-1) == characters.get(index)) {
+
+                while (index > 0 && characters.get(index-1) == characters.get(index))
+                    characters.remove(index--);
+
+                characters.remove(index);
+                index = characters.size() - 1;
+                continue;
+            }
+            index--;
+        }
+
     }
 }
