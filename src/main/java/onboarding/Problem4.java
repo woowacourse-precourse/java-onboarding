9995c8ae1ac028648687c422e0e4634dc469f811
Heosejin98 <sejin3319@naver.com>
2022-10-28 00:01:06 +0900

    Feat : PROBLEM4 아스키코드 배열 초기화 함수 InitAscii 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Problem4 {
+    static ArrayList<Integer> InitAscii() {
+        ArrayList<Integer> result = new ArrayList<>(
+                Collections.nCopies(256, -1)
+        );
+        for (int i = 0; i < 26; i++) {
+            result.set('A'+i, 'A'+25-i);
+            result.set('a'+i, 'a'+25-i);
+        }
+        return result;
+    }
+
     public static String solution(String word) {
         String answer = "";
+        ArrayList<Integer> ascii_array;
+
+        ascii_array = InitAscii();
+
         return answer;
+    }
+
+    public static void main(String argv[]){
+
+        solution("asdasd");
     }
 }
