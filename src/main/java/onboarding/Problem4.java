2e71ce6d63ac170c3aa2194551d71eb380ae4e34
elizabethgim <yujingim@kookmin.ac.kr>
2022-10-27 11:39:40 +0900

    Add: Problem4 main 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,8 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static void main(String[] args){
+        System.out.println(solution("I love you")); // "R olev blf"
+    }
 }
