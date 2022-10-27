238a8e9744302bea9415ffbe0931da61a51e273d
skck0226 <skck0226@ajou.ac.kr>
2022-10-27 10:24:37 +0900

    [feat] Problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,33 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (true) {
+            String decoded;
+            if(cryptogram.length() < 2) decoded = cryptogram;
+            else decoded = decodeCryptogram(cryptogram);
+            if (cryptogram.equals(decoded)) break;
+            cryptogram = decoded;
+        }
+        return cryptogram;
+    }
+
+    private static String decodeCryptogram(String cryptogram) {
+        String newCrypt = "";
+        char prev = ' ';
+
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            char now = cryptogram.charAt(i);
+            char next = cryptogram.charAt(i + 1);
+            if (now != prev && now != next) {
+                newCrypt += now;
+            }
+            prev = now;
+        }
+
+        if (cryptogram.charAt(cryptogram.length() - 1) != cryptogram.charAt(cryptogram.length() - 2)) {
+            newCrypt += cryptogram.charAt(cryptogram.length() - 1);
+        }
+        return newCrypt;
     }
 }
+
