23a234e94f172c849ded4a648c7f5660eea8c83f
shinyehwan <jghisjl007@naver.com>
2022-10-27 20:16:47 +0900

    Problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = 0;
+
+		for (int i = 1; i <= number; i++) {
+			if (i < 10) {
+				if (i % 3 == 0) {
+					answer += 1;
+				}
+			} else {
+				String numberToString = Integer.toString(i);
+				String[] splitNumber = numberToString.split("");
+
+				for (int k = 0; k < splitNumber.length; k++) {
+					if (splitNumber[k].contains("3") || splitNumber[k].contains("6") || splitNumber[k].contains("9")) {
+						answer += 1;
+					}
+				}
+			}
+		}
+
+		return answer;
+	}
 }
