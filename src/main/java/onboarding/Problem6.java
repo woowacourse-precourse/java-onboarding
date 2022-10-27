72b5039ce556683f38abeb4103a41d406a120d20
박준용 <jo101044@naver.com>
2022-10-27 20:18:47 +0900

    문제6 리스트 생성 메소드 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,28 +4,34 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Set<String> set = new HashSet<>();
 
-        for (int i = 0; i < forms.size(); i++) {
-
-            String str = forms.get(i).get(1);
-            
-            for (int j = 1; j < str.length(); j++) {
-                
-                //비교할 두 글자 불러오기
-                String subNickName = str.substring(j-1,j+1);
-                
-                //중복시 이메일 추가
-                if(!validateNickName(subNickName, i, forms)) set.add(forms.get(i).get(0));
-            }
-        }
-
-        List<String> answer = new ArrayList<>(set);
+        List<String> answer = duplicateNickNameList(forms);
 
         //오름차순 정렬
         Collections.sort(answer);
 
         return answer;
+    }
+
+    // 중복 리스트 생성 메소드
+    private static List<String> duplicateNickNameList(List<List<String>> forms) {
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+
+            String str = forms.get(i).get(1);
+
+            for (int j = 0; j < str.length()-1; j++) {
+
+                //비교할 두 글자 불러오기
+                String subNickName = str.substring(j,j+2);
+
+                //중복시 이메일 추가
+                if(!validateNickName(subNickName, i, forms)) set.add(forms.get(i).get(0));
+            }
+        }
+
+        return new ArrayList<>(set);
     }
 
     //중복 확인 메소드
