412b836a2c7f7b9b3db338338f20341f15ec4c60
2jun0 <soo28819@gmail.com>
2022-10-28 02:15:08 +0900

    Feat: 문제 7번 - 친구 그래프를 생성하는 메서드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -19,6 +19,24 @@ public class Problem7 {
         return allCrews;
     }
 
+    public static Map<String, Set<String>> createFriendGraph(Set<String> crews, List<List<String>> friendRelations) {
+        HashMap<String, Set<String>> friendGraph = new HashMap<>();
+
+        // init graph
+        crews.forEach(crew -> friendGraph.put(crew, new HashSet<>()));
+
+        // add line in graph
+        friendRelations.forEach(friendRelation -> {
+            String crew1 = friendRelation.get(0);
+            String crew2 = friendRelation.get(1);
+
+            friendGraph.get(crew1).add(crew2);
+            friendGraph.get(crew2).add(crew1);
+        });
+
+        return friendGraph;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
