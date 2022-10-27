180c012a32a5f9b132ecdbd9b89866b25092a7c0
jthugg <jthug@naver.com>
2022-10-27 14:21:54 +0900

    init problem3 & chk todo list

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,13 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        // 손뼉 카운트
+        // 반복문 루프로? or 자릿수 별 수열 합?
+        // 반복문
+        // 1. 1부터 number 까지 진행하는 루프
+        // 2. 루프별 해당하는 숫자 자릿수 분리
+        // 3. 각 자릿수에 3, 6, 9가 있는지 체크
+        // 4. 있다면 손뼉 카운트
         int answer = 0;
         return answer;
     }
