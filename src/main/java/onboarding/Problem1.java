f6a5ee0ef04d7917528b0e934c749724ae94cc2d
Jueun <jueun000101@gmail.com>
2022-10-27 20:49:48 +0900

    docs: 1번 기능 목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 책 페이지가 유효하게 입력되었는지 확인하는 함수
+ * 2. 한 사람의 가장 큰 점수를 계산하는 함수
+ * 3. 승자 결과를 반환하는 함수
+ * 4. 1-3의 기능을 활용해 문제에서 원하는 출력을 리턴하는 함수
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
