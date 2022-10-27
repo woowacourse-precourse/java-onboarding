c07f2be7c6755d389a7ab46ea54dc9b746611d5c
saehim <saeilee.vxv@gmail.com>
2022-10-27 19:47:50 +0900

    Feat: 자리수 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,5 +6,15 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    // 각 자리수 배열로 출력
+    static List<Integer> jarisoo(int num) {
+        List<Integer> numArr = new ArrayList<Integer>();
+        while(num > 0){
+            numArr.add(num % 10);
+            num /= 10;
+        }
+        return numArr;
+    }
+
     }
 }
