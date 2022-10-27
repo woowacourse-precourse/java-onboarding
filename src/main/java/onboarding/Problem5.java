20f346974b3f80a739d75b5fc8d07e01b35f6b4d
jjmin9797 <jjmin9797@naver.com>
2022-10-27 19:20:26 +0900

    [Problem5][기능요구사항] 문제 해결 위한 기능 정의

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,26 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    /**
+     * [입력받은 돈의 단위로 최대 화폐의 갯수 구하기]
+     * - 입력받은 돈의 최대 화폐 갯수를 반환한다.
+     * @param unitOfMoney 돈 단위
+     * @param money 남은 돈
+     * @return 화폐의 갯수
+     */
+    public static int exchangeMoney(int unitOfMoney, int money) {
+        return 1;
+    }
+
+
+    /**
+     * [각 화폐 단위의 모든 갯수 구하기]
+     * - 각 화폐 단위의 모든 갯수를 구해 반환한다.
+     * @param money
+     * @return 각 화폐의 모든 갯수.
+     */
+    public static List<Integer> exchangeAllMoney(int money){
+        return Collections.emptyList();
+    }
 }
