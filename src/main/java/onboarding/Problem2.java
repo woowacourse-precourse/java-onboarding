a567b80315fe810bd68a5eb0f31b8feb4d6dbf31
Kuyho Chung <kchung1995@gmail.com>
2022-10-27 23:44:40 +0900

    온보딩 - 문제 2 - solution 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -36,7 +36,6 @@ public class Problem2 {
     }
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decryptString(cryptogram);
     }
 }
