1c9e65057826c51811771e2359e39ce189312053
heesang <dlagmltkd12@naver.com>
2022-10-27 23:22:02 +0900

    feat(problem3): 한 숫자가 주어질 때 3,6,9 개수 카운트

---
+++ src/main/java/problem3/ThreeSixNineGame.java
@@ -0,0 +1,34 @@
+package problem3;
+
+public class ThreeSixNineGame {
+
+    private static final int DIVISION_CRITERION = 10;
+    private static final int ROOF_CRITERION = 0;
+    private static final int MULTIPLE_OF_THREE = 3;
+    private static final int MULTIPLE_CRITERION = 0;
+    private static int INIT_COUNT_ZERO = 0;
+
+    public static int isThreeSixNineNumber(int number) {
+        int totalCount = INIT_COUNT_ZERO;
+
+        while (number != ROOF_CRITERION) {
+            totalCount += countThreeSixNine(number);
+            number /= DIVISION_CRITERION;
+        }
+
+        return totalCount;
+    }
+
+    private static int countThreeSixNine(int number) {
+        int count = INIT_COUNT_ZERO;
+
+        if (isThreeSixNineDigitNumber(number % DIVISION_CRITERION)) {
+            count++;
+        }
+        return count;
+    }
+
+    private static boolean isThreeSixNineDigitNumber(int number) {
+        return number % MULTIPLE_OF_THREE == MULTIPLE_CRITERION;
+    }
+}
---
+++ src/test/java/problem3/ThreeSixNineGameTest.java
@@ -0,0 +1,17 @@
+package problem3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ThreeSixNineGameTest {
+
+    @Test
+    void 숫자가_주어질때_369의개수_카운트() {
+        int count1 = ThreeSixNineGame.isThreeSixNineNumber(153);
+        int count2 = ThreeSixNineGame.isThreeSixNineNumber(133);
+
+        assertThat(count1).isEqualTo(1);
+        assertThat(count2).isEqualTo(2);
+    }
+}
