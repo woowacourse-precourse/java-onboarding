9b8454516e1592e42f1f9057f8196c596674036e
BaeDongHyeon <sasd730@gmail.com>
2022-10-27 15:03:37 +0900

    refactor: 컬렉션의 stream을 메소드로 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,17 +9,17 @@ class Problem1 {
             return -1;
         }
 
-        Integer pobiMax = pobi.stream()
-                .map(Problem1::getMaxValue)
-                .max(Integer::compareTo)
-                .orElse(-1);
-
-        Integer crongMax = crong.stream()
-                .map(Problem1::getMaxValue)
-                .max(Integer::compareTo)
-                .orElse(-1);
+        Integer pobiMax = getScore(pobi);
+        Integer crongMax = getScore(crong);
 
         return getWinner(pobiMax, crongMax);
+    }
+
+    private static Integer getScore(List<Integer> user) {
+        return user.stream()
+                .map(Problem1::getMaxValue)
+                .max(Integer::compareTo)
+                .orElse(-1);
     }
 
     private static boolean errorCheck(List<Integer> pobi, List<Integer> crong) {
