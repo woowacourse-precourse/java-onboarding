99e41f746820de18ab30a0fadfe33f5b46c06fe6
somsom13 <somin1113@naver.com>
2022-10-27 21:25:21 +0900

    feat(문제4): 역순 알파벳 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int NO_ALPHABET = 0;
+    private static final int LOWER_CASE = -1;
+    private static final int UPPER_CASE = 1;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    private static char reverseAlphabet(char alphabet, int alphabetCase) {
+        double mid = calculateMidAlphabet(alphabetCase);
+        double distanceFromMid = mid - alphabet;
+        return (char)(mid + distanceFromMid);
+    }
+
+    private static double calculateMidAlphabet(int alphabetCase) {
+        if(alphabetCase==UPPER_CASE) {
+            return ((double)'A'+'Z')/2;
+        }
+        return ((double)'a'+'z')/2;
+    }
+
 }
