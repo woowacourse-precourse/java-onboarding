dcb94acc68e2827fad1ab339c250af9779a93748
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-28 00:14:40 +0900

    Problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        while(number >= 0) {
+            for (int i = 1; i <= number; i++) {
+                String num_str = String.valueOf(i);
+                if(num_str.indexOf("3") >= 0 || num_str.indexOf("6") >= 0 || num_str.indexOf("9") >= 0) {
+                    answer += 1;
+                }
+            }
+            number = number / 10;
+
+            if(number <= 0) {
+                break;
+            }
+        }
+
+
+
         return answer;
     }
 }
