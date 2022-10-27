5df4dba07e1e972ec40f72939cc5c5a05c94c919
gomljo <gomljo0307@naver.com>
2022-10-27 15:17:53 +0900

    refactor(Problem1.java): 제한사항 1번 구현(#8) 함수 이름 수정
    코드 내 함수의 이름이 isCorrectLength로 명명헸으나 입력된 배열이 올바른 길이가 아닌 경우 리턴 값에 대한 부정을 취해야하여 가독성이 떨어질 것으로 판단하고 함수 이름을 isWrongLength로 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,7 +9,7 @@ class Problem1 {
     public static final int DIFFERENCE_BETWEEN_PAGES=1;
     public static final int START_PAGE = 1;
     public static final int FINAL_PAGE = 400;
-    public static Boolean isCorrectLength(List<Integer> bookPages){
+    public static Boolean isWrongLength(List<Integer> bookPages){
         return bookPages.size() != CORRECT_LENGTH;
     }
     public static boolean isCorrectPosition(List<Integer> bookPages){
