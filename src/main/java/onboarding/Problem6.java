8739de54142b6ac45b86dc9bf16d6a75b66a4136
Taewoon Kim <xodns98@gmail.com>
2022-10-27 16:15:35 +0900

    문제 6 구현 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    private static final int MAX_CREW_COUNT = 10000;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>(MAX_CREW_COUNT);
+
+        for (int i = 0; i < forms.size() - 1; ++i) {
+            String baseEmail = forms.get(i).get(0);
+            String baseNickName = forms.get(i).get(1);
+
+            for (int j = i + 1; j < forms.size(); ++j) {
+                String compareEmail = forms.get(j).get(0);
+                String compareNickName = forms.get(j).get(1);
+
+                if (isRestrictedNickName(baseNickName, compareNickName)) {
+                    if (!answer.contains(compareEmail)) {
+                        answer.add(compareEmail);
+                    }
+
+                    if (!answer.contains(baseEmail)) {
+                        answer.add(baseEmail);
+                    }
+                }
+            }
+
+        }
+
         return answer;
+    }
+
+    private static boolean isRestrictedNickName(String baseNickname, String compareNickName) {
+        for (int i = 0; i < compareNickName.length() - 1; ++i) {
+            String checkNickName = compareNickName.substring(i, i + 2);
+            if (baseNickname.contains(checkNickName)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
