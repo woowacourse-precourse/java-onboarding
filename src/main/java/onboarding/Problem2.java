628e6b194f8a7dbe61f4e839f7a3b54edcd59947
swhyeon98 <smreo75@gmail.com>
2022-10-28 02:21:09 +0900

    코드 서식 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,12 +3,12 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         int k = 0;
-        int length = cryptogram.length()-1;
+        int length = cryptogram.length() - 1;
 
-        for (int i = 0; i<length; i++) {
-            k = i+1;
-            if(cryptogram.charAt(i) == cryptogram.charAt(k)) {
-                cryptogram = cryptogram.substring(0,i) + cryptogram.substring(k+1);
+        for (int i = 0; i < length; i++) {
+            k = i + 1;
+            if (cryptogram.charAt(i) == cryptogram.charAt(k)) {
+                cryptogram = cryptogram.substring(0, i) + cryptogram.substring(k + 1);
                 break;
             }
         }
