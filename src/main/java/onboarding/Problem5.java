69598cc688bca364641427b981193a5e96e2e495
kevstevie <jianna442@gmail.com>
2022-10-27 17:57:25 +0900

    feat: add changeMoney function

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,16 +1,21 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return changeMoney(money);
     }
 
     static int[] changes = {50000, 10000,  5000, 1000, 500, 100, 50, 10, 1};
 
-    
+    static List changeMoney(int money){
+        List<Integer> changelist = new ArrayList<>();
+        for (int i = 0; i < changes.length; i++) {
+            changelist.add(money / changes[i]);
+            money %= changes[i];
+        }
+        return changelist;
+    }
 }
