70dff610e2b818691cc21c8c91330af6c267ed58
JeonghoPark <pjhpjh0805@naver.com>
2022-10-27 17:42:52 +0900

    feat: 7번 점수 순서에 따른 정렬 및 상위 5 return 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,21 +1,58 @@
 package onboarding;
 
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = new ArrayList<>();
         List<String> userAndFriendList = getFriendList(friends, user);
         userAndFriendList.add(user);
 
         HashMap<String, Integer> strangerScoreMap = getStrangerScoreMap(userAndFriendList, friends);
         calculateVisitorScore(visitors, userAndFriendList, strangerScoreMap);
 
+        List<String> answer = makeAnswerList(strangerScoreMap);
         return answer;
+    }
+
+    private static List<String> makeAnswerList(HashMap<String, Integer> strangerScoreMap) {
+        List<String> answerList = new ArrayList<>(strangerScoreMap.keySet());
+        Collections.sort(answerList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (strangerScoreMap.get(o1) > strangerScoreMap.get(o2)) {
+                    return -1;
+                }
+                else if (strangerScoreMap.get(o1) < strangerScoreMap.get(o2)) {
+                    return 1;
+                }
+                else {
+                    return o1.compareTo(o2);
+                }
+            }
+        });
+        removeZeroInList(answerList, strangerScoreMap);
+        if (answerList.size() > 5) {
+            pickTopFiveScoreMember(answerList);
+        }
+        return answerList;
+    }
+
+    private static void pickTopFiveScoreMember(List<String> answerList) {
+        for (int i=answerList.size()-1 ; i>4; i--) {
+            answerList.remove(i);
+        }
+    }
+
+    private static void removeZeroInList(List<String> answerList, HashMap<String, Integer> strangerScoreMap) {
+        for (int i=answerList.size()-1; i>=0; i--) {
+            String key = answerList.get(i);
+            int score = strangerScoreMap.get(key);
+            if (score == 0) {
+                answerList.remove(i);
+                continue;
+            }
+            break;
+        }
     }
 
     private static void calculateVisitorScore(List<String> visitors, List<String> userAndFriendList, HashMap<String, Integer> strangerScoreMap) {
@@ -53,7 +90,6 @@ public class Problem7 {
         strangerScoreMap.put(firstFriend, strangerScoreMap.getOrDefault(firstFriend, 0));
         strangerScoreMap.put(secondFriend, strangerScoreMap.getOrDefault(secondFriend, 0));
     }
-
 
     private static List <String> getFriendList(List<List<String>> friends, String user) {
         List<String> friendList = new ArrayList<>();
