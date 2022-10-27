e01894f044b58fdf56276e9864082e591959db05
ymkim97 <rladudaud97@naver.com>
2022-10-28 01:19:15 +0900

    feat: add fuction for exception of page length

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,16 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public int raisePageLengthException(List<Integer> pobi, List<Integer> crong) {
+        int pobiLength = pobi.get(1) - pobi.get(0);
+        int crongLength = crong.get(1) - crong.get(0);
+
+        if (pobiLength != 1 || crongLength != 1) {
+            return 1;
+        }
+        
+        return 0;
+    }
+
 }
