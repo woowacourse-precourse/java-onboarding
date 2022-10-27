93934fdf8ebfc529abe4c7ccbd2b826e02c8eda7
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-27 14:48:54 +0900

    feat:5번 list에 화폐단위별로 주입

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> money_unit = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100,50, 10, 1));// 돈 단위별로 list에 넣어놓기
+
+
         return answer;
     }
 }
