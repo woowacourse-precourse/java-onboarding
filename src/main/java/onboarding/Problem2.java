50529c070173044569557a810572633c7a03b869
squareyun <squareyun@gmail.com>
2022-10-27 20:28:33 +0900

    feat: 중복 문자 인덱스 번호 찾기 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,16 +2,30 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "";
+        String answer = cryptogram;
         return answer;
     }
 
-    static boolean isOverlab(String cryptogram) {
-        for (int i = 0; i < cryptogram.length() - 1; i++) {
-            if (cryptogram.index(i) == cryptogram.index(i+1)) {
-                return true;
+    static int getOverlabStartIdx(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                return i;
             }
         }
-        return false;
+        return -1;
+    }
+
+    static String getOverlabEndIdx(String s, int overlabIdx) {
+        int endIdx = overlabIdx + 1;
+        for (int i = overlabIdx + 2; i < s.length() - 1; i++) {
+            if (i > s.length() - 1)
+                return endIdx;
+
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                endIdx += 1;
+            }
+        }
+
+        return endIdx;
     }
 }
