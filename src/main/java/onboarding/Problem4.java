d4ec1d6ed2ea24a8cd8a0ba4fea16f9090fe17e2
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 00:26:57 +0900

    feat : get reversed alphabet method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,7 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
 
+
         String answer = "";
         return answer;
     }
@@ -10,6 +11,12 @@ public class Problem4 {
     private static int getAscii(Character character) {
         int ascii = (int) character;
         return ascii;
+    }
+    private static Character getReversedAlphabet(int ascii){
+        int reversedAscii = 155 - ascii;
+        Character reversedAlphabet = (char) reversedAscii;
+
+        return reversedAlphabet;
     }
 
 }
