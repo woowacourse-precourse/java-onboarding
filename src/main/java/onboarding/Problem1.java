a4564f8a1d52f810d7366f21d93ef149107f359e
minhee020 <minhee020@naver.com>
2022-10-27 11:19:33 +0900

    max 추출 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,24 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    private int extraction(List<Integer> pages){
+    private static int extraction(int page){
         int result;
-        int left = pages.get(0);
-        int right = pages.get(1);
-
-        // 더하기
-
-        // 곱하기
-
+        int add = 0;
+        int mul = 1;
+        // 더하기, 곱하기
+        while (page != 0){
+            add += page % 10;
+            mul *= page % 10;
+            page /= 10;
+        }
+        result = Math.max(add, mul);
         return result;
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        int pobiScore = extraction(pobi);
-        int crongScore = extraction(crong);
+        int pobiScore = Math.max(extraction(pobi.get(0)), extraction(pobi.get(1)));
+        int crongScore = Math.max(extraction(crong.get(0)), extraction(crong.get(1)));
 
-        answer = Math.max(pobiScore, crongScore);
         return answer;
     }
 }
