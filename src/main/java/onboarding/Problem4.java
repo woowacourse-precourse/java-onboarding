d5b772c58e6ab508643e013cf873ed2809c1d158
kokodak <kokodakadokok@gmail.com>
2022-10-27 16:43:47 +0900

    feat: [문제4] 청개구리 사전을 이용한 알파벳 변환 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,4 +15,13 @@ public class Problem4 {
         if (word >= 'a' && word <= 'z') return SMALL_ALPHABET;
         return NO_ALPHABET;
     }
+
+    private static char convertAlphabet(char word) {
+        final int BIG_ALPHABET = 1;
+        final int SMALL_ALPHABET = 2;
+
+        if (isAlphabet(word) == BIG_ALPHABET) return (char)('A' + 'Z' - word);
+        if (isAlphabet(word) == SMALL_ALPHABET) return (char)('a' + 'z' - word);
+        return word;
+    }
 }
