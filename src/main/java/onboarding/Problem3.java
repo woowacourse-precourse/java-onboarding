511195d175b06f1aa38df1cb02e4e47fc8c841b6
abbd2 <ajkety00@gmail.com>
2022-10-28 01:11:45 +0900

    문제3: for each 문을 이용하여 chArr을 하나씩 검사, 3 또는 6 또는 9와 같다면 answer을 1증가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,12 @@ public class Problem3 {
         for(int i = 3; i <= number; i++){
             String str = Integer.toString(i);
             char[] numChArr = str.toCharArray();
-            
+
+            for(char chNum : chArr){
+                if(chNum == '3' || chNum == '6' || chNum == '9'){
+                    answer++;
+                } // end if
+            } // end for
         } // end for
 
         return answer;
