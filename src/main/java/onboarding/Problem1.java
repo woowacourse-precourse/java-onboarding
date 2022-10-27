bfcff24fe240f2f8437293dd42151118087a0143
Eunkeee <eunkeeeee@gmail.com>
2022-10-27 12:41:36 +0900

    ✨feat: 각 자리 숫자 자르기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,9 +1,22 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int[] pobiValues = pobi.stream().mapToInt(i->i).toArray();
+        for (int pobiValue: pobiValues){
+            int[] arrNum = Stream.of(String.valueOf(pobiValue).split("")).mapToInt(Integer::parseInt).toArray();
+            System.out.println(Arrays.toString(arrNum));
+        }
+        int[] crongValues = crong.stream().mapToInt(i->i).toArray();
+        for(int crongValue : crongValues){
+            int[] arrNum = Stream.of(String.valueOf(crongValue).split("")).mapToInt(Integer::parseInt).toArray();
+            System.out.println(Arrays.toString(arrNum));
+        }
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
