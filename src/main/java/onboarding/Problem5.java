e187161d249868db6d9a02664061a27c50515213
davejang <davejang98@naver.com>
2022-10-27 17:14:31 +0900

    fead: Problem5 moneyChange method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int moneyChange(int money, int unit) {
+        int count = 0;
+        if(money >= unit){
+            count = money / unit;
+        }
+        return count;
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        answer.add(moneyChange(money,50000));
         return answer;
     }
 }
