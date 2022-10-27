43ba2ea97e6497cdef4e4a5a181ba379811e1d75
iamjooon2 <iamjooon2@gmail.com>
2022-10-28 00:42:12 +0900

    feat: Problem3 - 결과값에 1 추가 및 출력하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,13 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         String sTemp;
-        int answer = 0;
+        int result = 0;
         for (int i = 1; i <= number; i += 1) {
             sTemp = i + "";
             for (int j = 0; j < sTemp.length(); j += 1) {
-                if (sTemp.charAt(j) == '3' || sTemp.charAt(j) == '6' || sTemp.charAt(j) == '9') { }
+                if (sTemp.charAt(j) == '3' || sTemp.charAt(j) == '6' || sTemp.charAt(j) == '9') {
+                    result += 1;
+                }
             }
         }
-        return answer;
+        return result;
     }
 }
