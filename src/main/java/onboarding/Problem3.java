0e64eeed51cd6f5f12961a426a869c56851bdd93
Your Name <you@example.com>
2022-10-27 16:02:24 +0900

    손뼉 치기 횟수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,10 +5,22 @@ public class Problem3 {
         int answer = 0;
 
         /*숫자 하나씩 대기*/
+        for (int i = 1; i <= number; ++i) {
+            int clap = i;
+            /*숫자 3,6,9 보일 시 손뼉 치기*/
+            answer = numberOfClap(clap, answer);
+        }
 
-        /*숫자 3,6,9 보일 시 손뼉 치기*/
+        return answer;
+    }
 
-
+    private static int numberOfClap(int clap, int answer) {
+        while (clap != 0) {
+            if (clap % 10 == 3 || clap % 10 == 6 || clap % 10 == 9) {
+                ++answer;
+            }
+            clap /= 10;
+        }
         return answer;
     }
 }
