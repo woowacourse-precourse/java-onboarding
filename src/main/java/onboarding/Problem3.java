4f8798aa5016104fb26fe4fa5a0e9d06808f21d9
jeongjihun <wlgns2234@naver.com>
2022-10-27 16:06:16 +0900

    Feat: 3,6,9포함 여부 반환기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+    public static boolean isOneTwoThree(int num) {
+        String str = Integer.toString(num);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '3')
+                return true;
+            if (str.charAt(i) == '6')
+                return true;
+            if (str.charAt(i) == '9')
+                return true;
+        }
+        return false;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
