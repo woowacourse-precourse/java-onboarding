4828c31d05b23b0ae51ed79875970b01e8dc0970
jini_11 <jieun21124@naver.com>
2022-10-27 13:13:04 +0900

    feat: 문제3 3,6,9를 찾는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,31 @@
 package onboarding;
 
 public class Problem3 {
+    static final int THREE = 3;
+    static final int SIX = 6;
+    static final int NINE = 9;
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += find369(i);
+        }
+
         return answer;
+    }
+
+    public static  int find369(int number) {
+        int count = 0;
+
+        while (number != 0) {
+            int remainder = number % 10;
+            if (remainder == THREE || remainder == SIX || remainder == NINE) {
+                count++;
+            }
+            number /= 10;
+        }
+
+        return count;
     }
 }
