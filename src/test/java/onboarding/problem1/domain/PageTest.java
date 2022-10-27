b44cf210f37787af7e4f57fb7571e4855e0e6e88
JunH <tjdtls690@gmail.com>
2022-10-27 14:27:55 +0900

    :white_check_mark: test : 단위 테스트 없는 메서드들 단위 테스트 추가

--- src/test/java/onboarding/problem1/domain/PageTest.java
+++ src/test/java/onboarding/problem1/domain/PageTest.java
@@ -1,22 +1,59 @@
 package onboarding.problem1.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class PageTest {
+    private Page page;
+    
+    @BeforeEach
+    void setUp() {
+        page = new Page(236);
+    }
+    
     @Test
     @DisplayName("숫자의 각 자릿수 합 구하기")
     void sumOfDigits() {
-        Page page = new Page(236);
         assertThat(page.sumOfDigits()).isEqualTo(11);
     }
     
     @Test
     @DisplayName("숫자의 각 자릿수 곱하기 구하기")
     void multiplyOfDigits() {
-        Page page = new Page(236);
         assertThat(page.multiplyOfDigits()).isEqualTo(36);
+    }
+    
+    @Test
+    @DisplayName("홀수가 아닌지 확인")
+    void isNotOdd() {
+        assertThat(page.isNotOdd()).isTrue();
+    }
+    
+    @Test
+    @DisplayName("짝수가 아닌지 확인")
+    void isNotEven() {
+        assertThat(page.isNotEven()).isFalse();
+    }
+    
+    @Test
+    @DisplayName("두 페이지 차이가 1이 아닌지 확인")
+    void isDifferenceNotOne() {
+        assertAll(
+                () -> assertThat(page.isDifferenceNotOne(new Page(238))).isTrue(),
+                () -> assertThat(page.isDifferenceNotOne(new Page(237))).isFalse()
+        );
+    }
+    
+    @Test
+    @DisplayName("왼쪽 페이지가 더 큰지 확인")
+    void isBigger() {
+        assertAll(
+                () -> assertThat(page.isBigger(new Page(235))).isTrue(),
+                () -> assertThat(page.isBigger(new Page(237))).isFalse()
+        );
     }
 }
--- src/test/java/onboarding/problem1/domain/PlayersTest.java
+++ src/test/java/onboarding/problem1/domain/PlayersTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class PlayersTest {
     @Test
@@ -27,5 +28,32 @@ public class PlayersTest {
     void draw() {
         Players players = new Players(Arrays.asList(157, 158), Arrays.asList(57, 58));
         assertThat(players.playResult()).isEqualTo(0);
+    }
+    
+    @Test
+    @DisplayName("짝수 홀수가 안맞는 경우")
+    void isNotCorrectEvenAndOddNumbers() {
+        assertAll(
+                () -> assertThat(new Players(Arrays.asList(158, 159), Arrays.asList(57, 58)).playResult()).isEqualTo(-1),
+                () -> assertThat(new Players(Arrays.asList(157, 158), Arrays.asList(58, 59)).playResult()).isEqualTo(-1)
+        );
+    }
+    
+    @Test
+    @DisplayName("차이가 1이 아닌 경우")
+    void isDifferenceNotOne() {
+        assertAll(
+                () -> assertThat(new Players(Arrays.asList(157, 160), Arrays.asList(57, 58)).playResult()).isEqualTo(-1),
+                () -> assertThat(new Players(Arrays.asList(157, 158), Arrays.asList(57, 59)).playResult()).isEqualTo(-1)
+        );
+    }
+    
+    @Test
+    @DisplayName("왼쪽 수가 오른쪽 수보다 더 큰 경우")
+    void isLeftBigger() {
+        assertAll(
+                () -> assertThat(new Players(Arrays.asList(161, 160), Arrays.asList(57, 58)).playResult()).isEqualTo(-1),
+                () -> assertThat(new Players(Arrays.asList(157, 158), Arrays.asList(59, 58)).playResult()).isEqualTo(-1)
+        );
     }
 }
