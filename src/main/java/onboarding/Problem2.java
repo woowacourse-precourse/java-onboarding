8f5f5db8a3d8e1e9352d5d1dd08320d55e117b32
hyeongu <wancern@naver.com>
2022-10-28 00:57:14 +0900

    Problem2. 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,9 +4,27 @@ package onboarding;
 // 3. 새로운 문자와 Deque의 마지막 문자가 같을경우 poll
 // 4. 다를 경우 offer
 // 5. 최종적으로 남아있는 문자 반환
+import java.util.*;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Deque<Character> dq = new ArrayDeque<>();
+
+        for(int i = 0; i<cryptogram.length();i++){
+            char c = cryptogram.charAt(i);
+
+            if(dq.isEmpty()){
+                dq.offerLast(c);
+            }
+            else{
+                if(dq.peekLast() == c){
+                    dq.pollLast();
+                }
+                else{
+                    dq.offerLast(c);
+                }
+            }
+        }
         return answer;
     }
 }
