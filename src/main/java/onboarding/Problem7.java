84aaaf9034c2bf8ac79a4ffaa8727902142ea658
suheonjoo <suheonj95@naver.com>
2022-10-27 20:41:32 +0900

    feat(Problem7): 친구목록 그래프 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    static class FriendGraph {
+        private Map<String, List<String>> friendMap = new HashMap<>();
+
+        public void addFriend(String user, String friend) {
+            if (!friendMap.containsKey(user)) {
+                friendMap.put(user,new ArrayList<>());
+            }
+            friendMap.get(user).add(friend);
+        }
+
+        public List<String> getFriendList(String user) {
+            return friendMap.get(user);
+        }
+
+        public Iterator<String> getIteratorFriendGraph(){
+            return friendMap.keySet().iterator();
+        }
+
+    }
+
 }
