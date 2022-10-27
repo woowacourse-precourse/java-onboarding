e2d52348cccd24565dbbdfdff6fe9242140e9a82
hyun6ik <fkanwm@naver.com>
2022-10-27 18:13:42 +0900

    feat(35) : [기능 5] : money를 검증한다 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    private static void validateMoney(int money) {
+        if (money < 1 || money > 1000000) {
+            throw new IllegalArgumentException("money는 1 이상 1,000,000 이하인 자연수입니다.");
+        }
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        validateMoney(money);
+        return new ArrayList<>();
     }
 }
