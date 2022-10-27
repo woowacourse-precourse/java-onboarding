5710d9e1865c141655ddc1708d7dc778890c64e1
LEE-sh1673 <lsh901673@gmail.com>
2022-10-27 19:23:28 +0900

    feat(Problem1): 문제1 solution 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+import onboarding.pagenumbergame.Page;
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Page pobiPage = new Page(pobi);
+        Page crongPage = new Page(crong);
+        return pobiPage.compare(crongPage);
     }
 }
