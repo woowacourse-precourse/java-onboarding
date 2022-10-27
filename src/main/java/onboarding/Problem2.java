8d4b041c099c4677b69bb070797971e10e7f70f8
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-28 01:03:26 +0900

    feat(문제2): 문자열 해독을 재귀로 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -22,10 +22,33 @@ public class Problem2 {
         return answer;
     }
 
-    private static String decode(String cryptogram) throws Exception {
-        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
-            throw new Exception("입력값의 길이가 올바르지 않습니다");
+    private static String decode(List<Character> cryptogram) throws Exception {
+        List<Character> answerList = new ArrayList<>();
+
+        Character prev = null;
+        for(Character each : cryptogram) {
+            if (prev == null) {
+                answerList.add(each);
+                prev = each;
+                continue;
+            }
+            if (prev == each) {
+                if(answerList.get(answerList.size()-1) == prev) {
+                    answerList.remove(answerList.size()-1);
+                }
+                continue;
+            }
+            answerList.add(each);
+            prev = each;
         }
-        return "아직 처리x";
+
+        if(cryptogram.size() != answerList.size()) {
+            return decode(answerList);
+        }
+        String answer = "";
+        for(Character each : answerList) {
+            answer += Character.toString(each);
+        }
+        return answer;
     }
 }
