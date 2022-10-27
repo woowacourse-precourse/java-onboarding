f60394e6941c9210720a324fd9bebda25180d1b7
Dasol Jung <greteljds0251@gmail.com>
2022-10-27 22:44:12 +0900

    feat: 1-2. Method for multiplying digits

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,8 +7,8 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         /*
         필요한 기능
-        1. 각 자리별 숫자를 모두 더하는 함수
-        2. 각 자리별 숫자를 모두 곱하는 함수
+        1. 각 자리별 숫자를 모두 더하는 메소드
+        2. 각 자리별 숫자를 모두 곱하는 메소드
         3. 결과 비교를 위한 변수 하나씩
          */
 
@@ -19,6 +19,14 @@ class Problem1 {
         int sum = 0;
         for (; a > 0; a /= 10) {
             sum += a % 10;
+        }
+        return sum;
+    }
+
+    public static int multNumber(int a) {
+        int sum = 1;
+        for (; a > 0; a /= 10) {
+            sum *= (a % 10);
         }
         return sum;
     }
