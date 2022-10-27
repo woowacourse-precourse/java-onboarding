29bc1dd6149c645fc8cf4bde6b556577f5fb4676
shpark30 <seohyeonpark30.gmail.com>
2022-10-27 22:40:13 +0900

    feat(Problem1.java): 자리수별 숫자를 곱하는 기능추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -21,4 +21,18 @@ class Problem1 {
         }
         return sum;
     }
+
+    static int multiplyDigits(int digits) {
+        // 숫자 digits가 주어졌을 때 자리수별 숫자(digit)의 곱을 return
+        // 정수 digits를 10으로 나누었을 때 나머지(digit)를 차례로 곱한다.
+        int mul = 1;
+
+        while(digits!=0) {
+            // digits를 10으로 나눈 나머지(digit)를 mul에 곱한다.
+            mul *= digits%10;
+            // digits를 10으로 나눈 볷을 digits에 저장한다.
+            digits /= 10;
+        }
+        return mul;
+    }
 }
