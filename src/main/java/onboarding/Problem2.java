ad4d93732d6ed54ad9514af0bbcbcfc70e52dfed
MinjeongKong <kong1301@ajou.ac.kr>
2022-10-27 12:41:37 +0900

    Problem2 - add getMessage

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,8 +5,35 @@ package onboarding;
  * 1. 중복 문자를 삭제한 결과를 저장하는 메서드 getMessage
  */
 public class Problem2 {
+
+    public static String getMessage(String str, boolean flag) {
+        if (flag) {
+            return str;
+        }
+        String msg = "";
+        str += " ";
+        flag = true;
+
+        if (str.length() > 1) {
+            if (str.charAt(0) == str.charAt(1)) {
+                flag = false;
+            } else {
+                msg += str.charAt(0);
+            }
+        }
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                flag = false;
+                continue;
+            }
+            if (str.charAt(i) != str.charAt(i - 1)) {
+                msg += str.charAt(i);
+            }
+        }
+        return getMessage(msg, flag);
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return getMessage(cryptogram, false);
     }
 }
