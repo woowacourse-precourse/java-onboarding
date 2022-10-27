bac4ab1e2ea715a379ca07742a23e7ab31625d34
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 14:11:37 +0900

    문제 3 : solution 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -19,6 +19,11 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += doClap(i);
+        }
+
         return answer;
     }
 }
