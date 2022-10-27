7a8e497a63bed417e46229154519914ec361ecb5
Your Name <you@example.com>
2022-10-27 15:42:28 +0900

    Problem3 기능 목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        /*숫자 하나씩 대기*/
+
+        /*숫자 3,6,9 보일 시 손뼉 치기*/
+
+
         return answer;
     }
 }
