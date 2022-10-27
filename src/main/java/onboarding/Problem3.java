43796cc6e009645e5606c2b6f0c5925b19ece167
youngkyunIm <yee0230@naver.com>
2022-10-27 15:36:46 +0900

    feat(Problem3) : 3의 배수 확인 및 카운트

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,7 +4,17 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
+            answer += hasThreeTimes(i);
         }
         return answer;
+    }
+
+    public static int hasThreeTimes(int number) {
+        String str = String.valueOf(number);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) % 3 == 0 && str.charAt(i) != '0') count++;
+        }
+        return count;
     }
 }
