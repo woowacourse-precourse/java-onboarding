92c98727d944ac51ce0a974700d40e5c74680ea7
greeng00se <thegreengoose7@gmail.com>
2022-10-27 16:59:43 +0900

    feat(Problem4): 청개구리 사전을 생성하는 메서드 추가
    - A -> Z, Z -> A
    - a -> z, z -> a 와 같은 형식을 띈다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    private static final HashMap<Character, Character> frogMap = new HashMap<>();
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        initializeFrogMap();
+        return "";
+    }
+
+    private static void initializeFrogMap() {
+        char upperCaseLetterA = 'A';
+        char upperCaseLetterZ = 'Z';
+        char lowerCaseLetterA = 'a';
+        char lowerCaseLetterZ = 'z';
+
+        for (int i = 0; i < 26; i++) {
+            frogMap.put(upperCaseLetterA++, upperCaseLetterZ--);
+            frogMap.put(lowerCaseLetterA++, lowerCaseLetterZ--);
+        }
     }
 }
