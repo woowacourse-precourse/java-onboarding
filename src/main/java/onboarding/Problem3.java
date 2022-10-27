bb995c8cecb0b951ad0f69a4e5e46375d69b6da4
Jin409 <sualng123@gmail.com>
2022-10-28 02:54:06 +0900

    feat(problem3): 각 숫자에 3,6,9가 몇번 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,10 @@
 package onboarding;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Problem3 {
+    private static final List<Integer> conditionNums = List.of(3, 6, 9);
 
     public static int solution(int number) {
         int[] numRange = getNumRange(number);
@@ -11,5 +13,16 @@ public class Problem3 {
 
     private static int[] getNumRange(int number) {
         return IntStream.rangeClosed(1, number).toArray();
+    }
+
+    private static int getCnt(int number) {
+        int cnt = 0;
+        while (number != 0) {
+            if (conditionNums.contains(number % 10)) {
+                cnt++;
+            }
+            number /= 10;
+        }
+        return cnt;
     }
 }
