0e2f60ddae3c36aad4b2dd5b28f27ccc95f1f89a
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-27 11:08:12 +0900

    feat :: 3, 6, 9가 포함된 수 만큼 cnt를 증가시키고 cnt 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -18,7 +18,7 @@ public class Problem3 {
         return cnt;
     }
 
-    // 각 자리 숫자 분리
+    // 숫자의 각 자리 분리
     private static int separateEachDigit(int number) {
         int cnt = 0;
         while (number != 0) {
@@ -31,4 +31,9 @@ public class Problem3 {
         return cnt;
     }
 
+    // 3, 6, 9가 포함된 숫자인가
+    private static boolean isInclude3or6or9(int number) {
+        return include369array.stream()
+                .anyMatch(n -> String.valueOf(number).contains(n));
+    }
 }
