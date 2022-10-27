d155ad9237801e6a23776dfe56439b36167fbc92
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-27 15:20:07 +0900

    369 포함여부 확인 및 반복문 추가, 테스트 통과

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,18 +1,32 @@
 package onboarding;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-
 public class Problem3 {
 	public static int solution(int number) {
-		// 숫자를 배열로 바꾼다.
-		int[]numArray = Problem1.getDigitArray(number);
-
-		// 배열이 3,6,9로 나누어지는 지 확인한다.
-		// 나누어진다면 result 1을 더해준다.
-		// result 리턴한다.
 		int answer = 0;
+
+		for (int i = 1; i <= number; i++) {
+			// 숫자를 배열로 바꾼다.
+			int[] numArray = Problem1.getDigitArray(i);
+			answer += get369Number(numArray);
+		}
+
 		return answer;
 	}
+
+	static int get369Number(int[] array) {
+		int sum = 0;
+
+		for (int j : array) {
+			// 배열이 3,6,9로 나누어지는 지 확인한다.
+			sum += is369(j) ? 1 : 0;
+			// 나누어진다면 sum + 1 한다.
+		}
+
+		return sum;
+	}
+
+	static boolean is369(int j) {
+		return (j == 3) || (j == 6) || (j == 9);
+	}
+
 }
