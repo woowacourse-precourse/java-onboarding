81ced6294b3dd7308dc594f3694f35add19dd7bb
choyeram <choyeram8@gmail.com>
2022-10-27 13:44:41 +0900

    Feat : change word

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            if (c >= 'a' && c <= 'z') {
+                arr[i] = (char) ('a' + 'z' - c);
+            } else if (c >= 'A' && c <= 'Z') {
+                arr[i] = (char) ('A' + 'Z' - c);
+            }
+        }
+        return String.valueOf(arr);
     }
 }
