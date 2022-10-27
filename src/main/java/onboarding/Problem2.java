21a78ed951fcb1d1b61be14d3006a45d8be07545
YooByeongIk <quddlr96@naver.com>
2022-10-27 15:20:39 +0900

    [Problem2] docs : 기능 목록 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+/*
+##기능 목록
+
+1. 중복 문자가 있는지 확인
+2. 중복 제거
+3. 중복 문자가 없을 때까지 반복
+
+
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
 }
