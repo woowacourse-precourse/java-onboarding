c3b1b53245ca72d57f138c9bfb0e8f20bc6da0c0
slbin-park <smpts00@naver.com>
2022-10-27 21:58:47 +0900

    feat : 숫자안에 3,6,9 의 개수를 구하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    public static int countClap(int number){
+        String strnumber = Integer.toString(number);
+        int cnt = 0;
+        for(int i=0;i<strnumber.length();i++){
+            String curnumber = String.valueOf(strnumber.charAt(i));
+            if(Integer.parseInt(curnumber) %3 ==0){
+                cnt+=1;
+            }
+        }
+        return cnt;
+    }
 }
