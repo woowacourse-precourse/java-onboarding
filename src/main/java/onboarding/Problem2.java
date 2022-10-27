d67c3ff570ad74cc8a2d26cef2eb402e70c65571
drunkenhw <drunkenhw@gmail.com>
2022-10-27 15:28:15 +0900

    feat(Problem2) : 연속하는 중복 문자를 찾는다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int duplicateLetterIndex = findDuplicateLetterIndex(cryptogram);
         return answer;
+    }
+
+    private static int findDuplicateLetterIndex(String word) {
+        for (int index = 0; index < word.length() - 1; index++) {
+            char leftLetter = word.charAt(index);
+            char rightLetter = word.charAt(index + 1);
+            if (leftLetter == rightLetter) {
+                return index;
+            }
+        }
+        return -1;
     }
 }
