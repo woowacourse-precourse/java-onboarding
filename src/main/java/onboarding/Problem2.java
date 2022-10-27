02c60327171252d786a0618c7bb6495e2f695da0
woosung1223 <dntjd991223@naver.com>
2022-10-27 22:31:39 +0900

    refactor(Problem2): 보초를 사용하는 방법으로 doCompress 구현부 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,10 +4,10 @@ import java.util.regex.Pattern;
 
 public class Problem2 {
     static final String EXCEPTION = "[ERROR]";
-    static final String REGEX = "[a-z]*$";
+    static final String VALIDATION_REGEX = "[a-z]*$";
     static final int LENGTH_UPPERBOUND = 1000;
     static final int LENGTH_LOWERBOUND = 1;
-
+    static final char SENTINEL = '#';
     public static String solution(String cryptogram) {
         if (validationCheck(cryptogram)) {
             while (needToCompress(cryptogram)) {
@@ -21,7 +21,7 @@ public class Problem2 {
     public static boolean validationCheck(String cryptogram) {
         if (cryptogram.length() > LENGTH_UPPERBOUND || cryptogram.length() < LENGTH_LOWERBOUND) {
             return false;
-        } else if (!Pattern.matches(REGEX, cryptogram)) {
+        } else if (!Pattern.matches(VALIDATION_REGEX, cryptogram)) {
             return false;
         }
         return true;
@@ -36,22 +36,17 @@ public class Problem2 {
     }
 
     public static String doCompress(String cryptogram) {
-        String newCryptogram = "";
+        String newCryptogram = SENTINEL + cryptogram + SENTINEL;
+        String result = "";
+        for (int i = 1; i < newCryptogram.length() - 1; i++) {
+            char leftValue = newCryptogram.charAt(i-1);
+            char middleValue = newCryptogram.charAt(i);
+            char rightValue = newCryptogram.charAt(i+1);
 
-        for (int i = 0; i < cryptogram.length(); i++) {
-            if (i == 0) {
-                if (cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
-                    newCryptogram += cryptogram.charAt(i);
-                }
-            } else if (i == cryptogram.length() - 1) {
-                if (cryptogram.charAt(i) != cryptogram.charAt(i - 1)) {
-                    newCryptogram += cryptogram.charAt(i);
-                }
-            } else if (cryptogram.charAt(i) != cryptogram.charAt(i - 1) &&
-                    cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
-                newCryptogram += cryptogram.charAt(i);
+            if (leftValue != middleValue && middleValue != rightValue) {
+                result += middleValue;
             }
         }
-        return newCryptogram;
+        return result;
     }
 }
