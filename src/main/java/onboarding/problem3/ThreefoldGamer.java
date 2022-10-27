2609b70e954cd52adc448a1c0ac895cf1784a573
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-28 08:55:24 +0900

    feat: create gamer class for threefold game

---
+++ src/main/java/onboarding/problem3/ThreefoldGamer.java
@@ -0,0 +1,17 @@
+package onboarding.problem3;
+
+/**
+ * Gamer playing threefold game
+ */
+public class ThreefoldGamer {
+
+    private final int NUM;
+
+    /**
+     * Constructor with number
+     * @param number last number of game
+     */
+    public ThreefoldGamer(int number) {
+        this.NUM = number;
+    }
+}
