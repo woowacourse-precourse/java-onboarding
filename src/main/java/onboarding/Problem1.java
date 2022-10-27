305936c1270f7a8175827f03ed4f4068b72800ee
ckyeon <qsky8520@gmail.com>
2022-10-27 17:27:53 +0900

    Add Result enum to Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,7 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
@@ -16,6 +17,19 @@ class Problem1 {
 
         PageDirection(int index) {
             this.index = index;
+        }
+    }
+
+    private enum Result {
+        POBI(1),
+        CRONG(2),
+        DRAW(0),
+        EXCEPTION(-1);
+
+        private final int number;
+
+        Result(int number) {
+            this.number = number;
         }
     }
 }
