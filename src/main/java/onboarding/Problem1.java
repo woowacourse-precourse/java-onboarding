8db4243cd35eba3b45a210ebea5ce1ed41982bec
Yongwoo Choi <cyw7515@naver.com>
2022-10-27 20:42:21 +0900

    feat(problem1): 타당한 입력인지 확인하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,15 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (validate(pobi) || validate(crong)) return -1;
+
         return answer;
+    }
+
+    private static boolean validate(List<Integer> pages) {
+        if (pages.get(1) - pages.get(0) > 1) return true;
+        if (pages.get(1) == 400 || pages.get(0) == 1) return true;
+
+        return false;
     }
 }
