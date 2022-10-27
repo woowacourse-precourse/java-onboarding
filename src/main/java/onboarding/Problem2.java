d2d8d37356a64b27c042cca37ec3ec636abce378
sinsu <sinsu19012@gmail.com>
2022-10-27 20:42:47 +0900

    ✨feat: Problem2구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,46 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String str=cryptogram;
+        while(true){
+            String str_rm=same_remove(str);
+            if(str_rm.equals(str)){
+                break;
+            }
+            str=str_rm;
+        }
+        return str;
     }
+
+    public static String same_remove(String s){
+        List<Character> chars=new ArrayList<>();
+        for(char ch:s.toCharArray()){
+            int size =chars.size()-1;
+            if(chars.isEmpty()){
+                chars.add(ch);
+                continue;
+            }
+            if (chars.get(size).equals(ch)) {
+                chars.remove(size);
+                continue;
+            }
+            chars.add(ch);
+
+        }
+        return convertCharListToString(chars);
+    }
+    public static String convertCharListToString(List<Character> characterList){
+        StringBuilder sb=new StringBuilder();
+        for (Character ch:characterList){
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+
 }
