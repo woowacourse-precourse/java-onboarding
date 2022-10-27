979fe8f1b2b850e93f72bc43c1d6482c63493d33
wwwndus <simock22@gmail.com>
2022-10-27 16:16:56 +0900

    feat: 배열의 길이만큼 반복문을 진행하며 3,6,9를 포함하였는지 판별

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,6 +9,12 @@ public class Problem3 {
 
             if (strNumber.contains("3") || strNumber.contains("6") || strNumber.contains("9")) {
                 String[] strNum = strNumber.split("");
+
+                for (int j = 0; j < strNum.length; j++) {
+                    if (strNum[j].equals("3") || strNum[j].equals("6") || strNum[j].equals("9")) {
+                        answer += 1;
+                    }
+                }
             }
         }
         return answer;
@@ -19,7 +25,7 @@ public class Problem3 {
 // 33	14
 
 // 입력 값까지 for문 진행
-// 수를 Strgin형태로 변환 후 3,6,9를 포함하였다면 문자열 배열로 변환
+// number를 String형태로 변환 후 3,6,9를 포함하였다면 문자열 배열로 변환
 // 배열의 길이만큼 반복문을 진행하며 3,6,9를 포함하였는지 판별
 // -> 포함하였다면 answer += 1
 // answer 값 출력
