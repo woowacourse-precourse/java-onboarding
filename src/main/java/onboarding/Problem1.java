23d488bd3674cfde2c318f5997b76c1c3b6b4c1e
lee <higgs95@naver.com>
2022-10-27 16:14:44 +0900

    feat(problem1): validate input data

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,19 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static boolean validate(List<Integer> pages){
+        if(pages.contains(1) || pages.contains(2)) {
+            return false;
+        }else if (pages.contains(399) || pages.contains(400)) {
+            return false;
+        }else if (pages.get(0) < 1 || pages.get(1) > 400 ) {
+            return false;
+        }else if (pages.get(1) - pages.get(0) != 1) {
+            return false;
+        }else if (pages.get(0) % 2 != 1 || pages.get(1) % 2 != 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
