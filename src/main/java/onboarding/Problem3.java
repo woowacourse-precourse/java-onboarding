451911401a0d283a2c6cb90ac2c6235c9b3b7573
jeonghoon <shb03323@naver.com>
2022-10-28 00:57:37 +0900

    refactor: change problem3 to a more readable solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,33 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        int cloneNumber = number;
-        int placeValue = 1;
-
-        while (cloneNumber != 0) {
-            answer += count369(placeValue, number);
-            cloneNumber /= 10;
-            placeValue *= 10;
+        for (int i = 1; i <= number; i++) {
+            answer += count369(i);
         }
         return answer;
     }
 
-    private static int count369(int placeValue, int number) {
-        int dividend = placeValue * 10;
-        int count = number / dividend * 3 * placeValue;
-        int remainder = number % placeValue;
-        int additionalNumber = (number % dividend) / placeValue;
-
-        if (additionalNumber == 9) {
-            count += placeValue * 2 + remainder + 1;
-        } else if (additionalNumber > 6) {
-            count += placeValue * 2;
-        } else if (additionalNumber == 6) {
-            count += placeValue * 1 + remainder + 1;
-        } else if (additionalNumber > 3) {
-            count += placeValue * 1;
-        } else if (additionalNumber == 3) {
-            count += remainder + 1;
+    private static int count369(int number) {
+        int count = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+            if (remainder == 3 || remainder == 6 || remainder == 9) {
+                count += 1;
+            }
+            number /= 10;
         }
         return count;
     }
