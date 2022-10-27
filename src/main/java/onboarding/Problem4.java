acee7a760451203c38b3022a52732fb5fa9cd9f8
jthugg <jthug@naver.com>
2022-10-27 21:51:30 +0900

    feat. 변환 기능 구현
    replace 메서드 보다 StringBuilder 사용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,6 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        // todo list
         // 알파벳 치트시트
         String A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String Z = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
@@ -13,19 +12,18 @@ public class Problem4 {
 
         // 대소문자 구분 변환 루프
         for(int i = 0; i < word.length(); i++) {
-            // 알파벳인지 아닌지 체크
             char ch = word.charAt(i);
             boolean upper = ch >= 'A' && ch <= 'Z';
             boolean lower = ch >= 'a' && ch <='z';
-            // 알파벳 변환만 구현하면 됨
-            if(upper) {
 
+            if(upper) { // 대문자인지 체크
+                sb.append(Z.charAt(A.indexOf(ch)));
             }
-            if(lower) {
-
+            if(lower) { // 소문자인지 체크
+                sb.append(z.charAt(a.indexOf(ch)));
             }
-            if(!upper && !lower) {
-
+            if(!upper && !lower) { // 대문자 소문자 둘 다 아닌경우, 알파벳이 아님
+                sb.append(ch);
             }
         }
 
