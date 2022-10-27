916cb59466bf7a3c7443c2502281aa03ccad7253
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-27 23:55:59 +0900

    feat : counting method change with implement

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,37 +1,17 @@
 package onboarding;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Problem3 {
     public static int solution(int number) {
 
-
-        int count = 0;
-        for (int i = 1; i <= number; i++) {
-
-            String numberStr = String.valueOf(i);
-            if (numberStr.contains("3") || numberStr.contains("6") || numberStr.contains("9")) {
-                count++;
-
-                if (numberStr.length() >= 2) {
-                    Pattern patternRegex = setRegex();
-                    Matcher patternMatcher = patternRegex.matcher(numberStr);
-                    if (patternMatcher.find()) {
-                        count++;
-                    }
-                }
-            }
-        }
-
-        int answer = count;
+        int answer = 0;
         return answer;
 
     }
 
-    private static Pattern setRegex() {
-        String regex = "[369]{2}";
-        Pattern patternRegex = Pattern.compile(regex);
-        return patternRegex;
+    public static long countChar(String numberStr, char threeSixNine) {
+        return numberStr.chars()
+                .filter(character -> character == threeSixNine)
+                .count();
     }
+
 }
