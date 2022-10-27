05a8be819129e8b60a3991fc4f493cf3ad49150d
ashlovesliitea <skyrider1811@gmail.com>
2022-10-27 21:55:35 +0900

    feature: 기능 3번 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int num = i;
+            while (num > 0) {
+                int left = num % 10;
+                if (left == 3 || left == 6 || left == 9) {
+                    answer += 1;
+                }
+                num /= 10;
+            }
+        }
+
         return answer;
     }
 }
