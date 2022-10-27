e75ea7f0f174ac38b18336162542b463e9d91c53
Kim kyunghee <kkh2415@naver.com>
2022-10-27 17:49:48 +0900

    fix : #Problem2 - 리팩토링 진행 배열을 반복하는 메서드와 리스트에 추가하는 메서드 기능 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,41 +3,45 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
- * a. 중복되지 않은 문자열만 저장하는 기능 구현
- *  a-1. 연속으로 중복되었는지 검사하는 기능 구현
+ * a. 배열 요소 반복하면서 리스트에 요소 추가하는 메서드 구현 - decipherCryptogram()
+ * b. 해당 요소가 중복된 요소인지 확인하고 중복이면 삭제하고 아니면 리스트에 추가하는 메서드 구현 - add()
+ * c. 해당 요소가 중복된 문자인지 확인하는 메서드 기능 구현 - isDuplication()
  * */
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-        return decipherCryptogram(cryptogram);
+        return decipherCryptogram(cryptogram.split(""));
     }
 
-    private static String decipherCryptogram(String cryptogram) {
-        List<String> decipheringCryptogram = new ArrayList<>();
+    private static String decipherCryptogram(String[] cryptograms) {
+        List<String> plainText = new ArrayList<>();
 
-        for (String s : cryptogram.split("")) {
-            int size = decipheringCryptogram.size();
-
-            if (isDuplication(decipheringCryptogram, s)) {
-                decipheringCryptogram.remove(size - 1);
-                continue;
-            }
-
-            decipheringCryptogram.add(s);
+        for (String s : cryptograms) {
+            add(plainText, s);
         }
 
-        return String.join("", decipheringCryptogram);
+        return String.join("", plainText);
     }
 
-    private static boolean isDuplication(List<String> decipheringCryptogram, String s) {
-        int size = decipheringCryptogram.size();
+    private static void add(List<String> plainText, String s) {
+        int size = plainText.size();
+
+        if (isDuplication(plainText, s)) {
+            plainText.remove(size - 1);
+            return ;
+        }
+
+        plainText.add(s);
+    }
+
+    private static boolean isDuplication(List<String> plainText, String s) {
+        int size = plainText.size();
 
         if (size == 0) {
             return false;
         }
 
-        return decipheringCryptogram.get(size - 1).equals(s);
+        return plainText.get(size - 1).equals(s);
     }
 }
