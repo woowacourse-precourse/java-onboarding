deaf42dfc9ea198951cd301c8a06ff510af49609
K-jun98 <alap_u@naver.com>
2022-10-27 20:16:54 +0900

    feat - 크루의 이름의 토큰을 저장된 리스트에서 조회후 2이상 이라면 중복 리스트에 이메일을 추가한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,6 +8,7 @@ public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
         crewToMap(forms);
+        return getDuplicateNames(forms);
     }
 
     private static void crewToMap(List<List<String>> forms) {
@@ -23,6 +24,20 @@ public class Problem6 {
             String nameToken = name.substring(i, i + 2);
             nameTokens.put(nameToken, nameTokens.getOrDefault(nameToken, 0) + 1);
         }
+    }
+
+    private static List<String> getDuplicateNames(List<List<String>> forms) {
+        List<String> duplicateName = new ArrayList<>();
+        for (List<String> id : forms) {
+            String name = id.get(1);
+            for (int i = 0; i < name.length() - 1; i++) {
+                if (nameTokens.get(name.substring(i, i + 2)) > 1) {
+                    duplicateName.add(crews.get(name));
+                    break;
+                }
+            }
+        }
+        return duplicateName;
     }
 
 }
