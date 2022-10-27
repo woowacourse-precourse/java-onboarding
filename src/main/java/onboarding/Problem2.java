a6e43fa7c75a38947b009136dd246b34d3f9668f
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-27 13:12:42 +0900

    feat: [Problem2] 기능 요구사항 1 추가(임의의 문자열에서 연속하는 문자들을 확인하는 기능)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,13 +6,11 @@ import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        boolean done = false;
-        List<String> list = new ArrayList(Arrays.asList(cryptogram.split("")));
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(0) == list.get(1)) {
-                list.remove(i + 1);
-                list.remove(i);
+        List<String> collect = Arrays.stream(cryptogram.split("")).collect(Collectors.toList());
+        List<Integer> sames = new ArrayList<>();
+        for (int i = 0; i < collect.size()-1; i++) {
+            if (collect.get(i).equals(collect.get(i + 1))) {
+                sames.add(i);
             }
         }
         String answer = "answer";
