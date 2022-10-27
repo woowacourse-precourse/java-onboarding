68bb6de5b8a28f194221db55dbb4b3a72829ba6c
ddubidubap <park62000@gmail.com>
2022-10-27 21:57:28 +0900

    PROBLEM4 기능 요구 사항 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+    /*
+        기능 요구 사항 목록
+        1. 각 알파벳 아스키의 반대되는 아스키를 반환하는 함수
+        2. solution (반대되는 문자들을 문자열로 합침)
+    */
+
     public static String solution(String word) {
         String answer = "";
         return answer;
