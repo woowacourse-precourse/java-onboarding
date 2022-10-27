cde9e212882ce3a2a566a81b7085f37559e05382
limjae <limjae96@gmail.com>
2022-10-27 10:12:02 +0900

    feat: Problem4 - Implement TreeFrog convert logic
    청개구리 구현체 변환 로직 추가 테스트 완료

--- src/main/java/onboarding/problem4/TreeFrog.java
+++ src/main/java/onboarding/problem4/TreeFrog.java
@@ -1,14 +1,35 @@
 package onboarding.problem4;
 
-public class TreeFrog implements Frog{
+public class TreeFrog implements Frog {
     String momSays;
 
-    public TreeFrog(String momSays){
+    public TreeFrog(String momSays) {
         this.momSays = momSays;
     }
 
     @Override
     public String repeatAfter() {
-        return null;
+        String[] spellings = momSays.split("");
+        for (int index = 0; index < spellings.length; index++) {
+            char spelling = spellings[index].charAt(0);
+            spellings[index] = convert(spelling);
+        }
+        return String.join("", spellings);
+    }
+
+    /**
+     * @param spelling 은 1글자 char이다.
+     * @return 청개구리 사전에 의해 변환된 char을 다시 String으로 변환하여 반환
+     */
+    private String convert(char spelling) {
+        // lower case
+        if ('a' <= spelling && spelling <= 'z') {
+            spelling = (char) ('a' + ('z' - spelling));
+        }
+        //upper case
+        else if ('A' <= spelling && spelling <= 'Z') {
+            spelling = (char) ('A' + ('Z' - spelling));
+        }
+        return String.valueOf(spelling);
     }
 }
