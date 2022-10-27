9e16244912337dee5670191733d414f8ad2d7fd4
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 20:06:01 +0900

    feat(P5): 검증 로직 구현

--- src/main/java/onboarding/problem5/ValidationMoney.java
+++ src/main/java/onboarding/problem5/ValidationMoney.java
@@ -4,4 +4,12 @@ import java.util.List;
 
 public class ValidationMoney {
 
+    public static void validateMoney(int money) {
+        final int MIN_VALUE = 1;
+        final int MAX_VALUE = 1_000_000;
+
+        if (money < MIN_VALUE || MAX_VALUE < money) {
+            throw new IllegalArgumentException("금액은 1원 이상 100만원 이하여야 합니다.");
+        }
+    }
 }
