71e3f9d2e8520879231ba2055fbc741ce296ac47
code-tree <dltjdqls1200@naver.com>
2022-10-27 17:38:55 +0900

    feat: Add monetary unit array

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] monetaryUnit = new int[] {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
 
         if (money < 1 || money > 1_000_000) {
-            return answer;
+            return Collections.emptyList();
         }
+        
 
         return answer;
     }
