c6de0bdd552f85dac80f827f6e070c51ef017de1
bjk1649 <qhwns1649@naver.com>
2022-10-27 23:59:35 +0900

    test: Problem3 테스트 케이스 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -95,23 +95,23 @@ class ApplicationTest {
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
         }
     }
-//
-//    @Nested
-//    class Problem3Test {
-//        @Test
-//        void case1() {
-//            int number = 13;
-//            int result = 4;
-//            assertThat(Problem3.solution(number)).isEqualTo(result);
-//        }
-//
-//        @Test
-//        void case2() {
-//            int number = 33;
-//            int result = 14;
-//            assertThat(Problem3.solution(number)).isEqualTo(result);
-//        }
-//    }
+
+    @Nested
+    class Problem3Test {
+        @Test
+        void case1() {
+            int number = 13;
+            int result = 4;
+            assertThat(Problem3.solution(number)).isEqualTo(result);
+        }
+
+        @Test
+        void case2() {
+            int number = 33;
+            int result = 14;
+            assertThat(Problem3.solution(number)).isEqualTo(result);
+        }
+    }
 //
 //    @Nested
 //    class Problem4Test {
