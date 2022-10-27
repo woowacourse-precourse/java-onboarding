bfb1d270b8dc0269dfb4971020ca3063aece11c2
박준용 <jo101044@naver.com>
2022-10-27 14:57:12 +0900

    문제1 예외처리 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,33 +1,52 @@
 package onboarding;
 
+
 import java.util.List;
 
 class Problem1 {
+
+    // 예외 처리 메소드
+    public static void exceptions(List<Integer> pobi, List<Integer> crong) throws RuntimeException {
+        if(pobi.get(0)%2 != 1 || crong.get(0)%2 != 1) {
+            throw new RuntimeException("왼쪽 페이지는 홀수만 입력 가능합니다.");
+        }
+
+        if(pobi.get(1)%2 != 0 || crong.get(1)%2 != 0) {
+            throw new RuntimeException("오른쪽 페이지는 짝수만 입력 가능합니다.");
+        }
+
+        if(pobi.get(0)+1 != pobi.get(1) || crong.get(0)+1 != crong.get(1)){
+            throw new RuntimeException("페이지 사이가 비어있습니다.");
+        }
+    }
+    
+    // 더하기와 곱하기 중 큰 값을 출력하는 메소드
+    private static int getMaxNum(String page) {
+        int plusNum = page.charAt(0)-'0';
+        int multipleNum = page.charAt(0)-'0';
+
+        for (int i = 1; i < page.length(); i++) {
+            plusNum += page.charAt(i) - '0';
+            multipleNum *= page.charAt(i) - '0';
+        }
+
+        return Math.max(plusNum, multipleNum);
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        //시작 페이지가 나올 시 -1 리턴
-        if(pobi.get(0) == 1) return -1;
-        if(crong.get(0) == 1) return -1;
 
-        //마지막 페이지가 나올 시 -1 리턴
-        if(pobi.get(1) == 400) return -1;
-        if(crong.get(1) == 400) return -1;
-
-        //왼쪽 페이지가 짝수일 경우 -1 리턴
-        if(pobi.get(0)%2 != 1) return -1;
-        if(crong.get(0)%2 != 1) return -1;
-
-        //오른쪽 페이지가 홀수일 경우 -1 리턴
-        if(pobi.get(1)%2 != 0) return -1;
-        if(crong.get(1)%2 != 0) return -1;
-
-        //페이지 사이가 비어있을 경우 -1 리턴
-        if(pobi.get(0)+1 != pobi.get(1)) return -1;
-        if(crong.get(0)+1 != crong.get(1)) return -1;
+        //예외 발생시 -1 리턴
+        try {
+            exceptions(pobi,crong);
+        }catch (RuntimeException e){
+            return -1;
+        }
 
         int pobiScore = 0;
         int crongScore = 0;
 
-        for (int i = 0; i < pobi.size(); i++) {
+        for (int i = 0; i < 2; i++) {
             //pobi의 점수 구하기
             String pobiPage = String.valueOf(pobi.get(i));
             pobiScore = Math.max(pobiScore,getMaxNum(pobiPage));
@@ -45,19 +64,6 @@ class Problem1 {
 
         //같을 경우 리턴 0
         return 0;
-    }
-
-    // 더하기와 곱하기 중 큰 값을 출력하는 메소드
-    private static int getMaxNum(String page) {
-        int plusNum = page.charAt(0)-'0';
-        int multipleNum = page.charAt(0)-'0';
-
-        for (int i = 1; i < page.length(); i++) {
-            plusNum += page.charAt(i) - '0';
-            multipleNum *= page.charAt(i) - '0';
-        }
-
-        return Math.max(plusNum, multipleNum);
     }
 
 }
