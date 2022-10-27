ccd9c4d340113789963a71da3ec612274b5fa5d1
wwwndus <simock22@gmail.com>
2022-10-27 13:37:08 +0900

    feat: 리스트의 남은 문자열을 합쳐서 출력

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -29,6 +29,8 @@ public class Problem2 {
             }
         }
 
+        answer = String.join("", strList);
+
         return answer;
     }
 }
@@ -39,4 +41,4 @@ public class Problem2 {
 // 입력 문자열을 분리하여 배열로 저장한 후 리스트로 변환
 // while문을 진행하여 연속적으로 중복되는 문자열은 삭제
 //  -> 모두 중복되어 문자열이 없거나 더이상 중복되는 문자열이 없을 경우 반복문 빠져나옴
-// 리스트의 문자열을 합쳐서 출력
+// 리스트의 남은 문자열을 합쳐서 출력 
