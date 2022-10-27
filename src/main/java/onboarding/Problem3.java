6546eb5949d2fb654be73d35cc97ee9092c5b69c
youhobin <ghdb132@naver.com>
2022-10-27 19:08:30 +0900

    feat(onboarding) : num을 1부터 제시되는 number까지 1씩 더하기를 반복하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem3 {
+    static int result;
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        result = 0;
+        for (int num = 1; num <= number; num++) {
+            check369(num);
+        }
+
+        return result;
     }
 }
