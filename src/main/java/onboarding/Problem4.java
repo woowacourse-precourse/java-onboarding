ec006eaa472faa958f4c1aa0a3acb2ff73269098
안광민 <creativeduck0518@gmail.com>
2022-10-27 21:35:25 +0900

    refactor(Problem 4): 상수 클래스 내부에 배치하는 것으로 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int NOT_ALPHABET = 0;
+    private static final int LOWERCASE = 1;
+    private static final int UPPERCASE = 2;
+
     public static String solution(String word) {
         String answer = "";
         StringBuilder stringBuilder = new StringBuilder(word);
@@ -12,7 +16,7 @@ public class Problem4 {
             letter = stringBuilder.charAt(i);
             alphabetType = makeAlphabetType(letter);
 
-            if (alphabetType != Constants.NOT_ALPHABET) {
+            if (alphabetType != NOT_ALPHABET) {
                 convertedLetter = convert(letter, alphabetType);
                 stringBuilder.setCharAt(i, convertedLetter);
             }
@@ -24,16 +28,16 @@ public class Problem4 {
 
     private static int makeAlphabetType(char letter) {
         if (letter >= 'a' && letter <= 'z') {
-            return Constants.LOWERCASE;
+            return LOWERCASE;
         }
         if (letter >= 'A' && letter <= 'Z') {
-            return Constants.UPPERCASE;
+            return UPPERCASE;
         }
-        return Constants.NOT_ALPHABET;
+        return NOT_ALPHABET;
     }
 
     private static char convert(char letter, int alphabetType) {
-        if (alphabetType == Constants.LOWERCASE) {
+        if (alphabetType == LOWERCASE) {
             return (char) ('a' + 'z' - letter);
         }
         return (char) ('A' + 'Z' - letter);
