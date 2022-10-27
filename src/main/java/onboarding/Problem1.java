8dfba610f57797dc824bcf262c361ba4e3adee8d
Kimhyeyun <cqqwer@gmail.com>
2022-10-27 17:04:33 +0900

    [Problem 1] MAKE EXCEPTION FUNCTION

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(!check(pobi) || !check(crong)) return -1;
+    }
+
+    private static boolean check(List<Integer> name) {
+        if (name.get(1) - name.get(0) != 1) return false;
+        if(name.get(0) % 2 != 1 || name.get(1) % 2 != 0) return false;
+        return true;
     }
 }
