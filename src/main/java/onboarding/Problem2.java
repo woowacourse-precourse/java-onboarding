2dc31aad00fab9de5cdda714c9e6761ad4f950e4
yujaemin <woawoa0513@gmail.com>
2022-10-27 19:14:40 +0900

    feat: [문제 2] 중복 문자 삭제 기능 추가
    문자열을 문자 단위로 쪼개서 바로 직전의 문자와 같은 문자인지 확인한 뒤, 중복되는 문자라면 삭제한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (Character c : cryptogram.toCharArray()) {
+            if (stack.isEmpty()) stack.push(c);
+            else {
+                if (stack.peek() != c) stack.push(c);
+                else stack.pop();
+            }
+        }
+
+        for (Character c : stack) {
+            answer.append(c);
+        }
+
+        return answer.toString();
     }
 }
