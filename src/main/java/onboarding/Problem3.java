b1714205955a3b7ed2c5ebd575f3c7c53044d495
jthugg <jthug@naver.com>
2022-10-27 15:07:36 +0900

    feat.각 자릿수 체크, 카운트 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,12 +14,19 @@ public class Problem3 {
         for(int i = 1; i <= number; i++) { // 1 ~ number까지 순환 하는 루프
             int tmp1 = i; // number가 저장될 변수
             int tmp2 = 0; // 각각의 자릿수가 저장될 변수
+
             for(; tmp1 != 0;) { // 자릿수 분리, 1의 자리 부터 체크
                 tmp2 = tmp1 % 10;
+
+                if(tmp2 == 3 || tmp2 == 6 || tmp2 == 9) { // 분리된 자릿수가 3, 6, 9중 하나에 해당하면 카운트 변수 증가
+                    cnt++;
+                }
+
                 tmp1 /= 10;
             }
         }
 
         return cnt;
+        // 불필요한 연산 줄여야...
     }
 }
