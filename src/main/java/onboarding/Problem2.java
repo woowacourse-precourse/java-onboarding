26941d626adf1275442c2c49037e1b9e79108016
Hongsugyeong <olivia258@naver.com>
2022-10-27 20:15:53 +0900

    [problem2] 스택을 문자열로 반환하는 함수

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     /**
      * 스택 이용해서 문자 제거하는 함수
@@ -8,5 +10,20 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    /*
+     *스택을 문자열로 반환하는 함수
+     */
+    static String dump(Stack<Character> ptr) {
+        String result = "";
+        if(ptr.size() <= 0 ){
+            return "";
+        }else{
+            for(int i = 0 ; i < ptr.size() ; i++) {
+                result = result+ptr.get(i);
+            }
+        }
+        return result;
     }
 }
