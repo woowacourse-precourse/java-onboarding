bc3571db5908128bd0e22f40d4725614cadd1720
kevstevie <jianna442@gmail.com>
2022-10-27 20:38:15 +0900

    feat: add makingFriendsList

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        Map<String, Set<String>> friendsList = new HashMap<>();
+
+        for (int i = 0; i < friends.size(); i++) {
+            makeList(friendsList, friends.get(i).get(0), friends.get(i).get(1));
+        }
+    }
+
+    static void makeList(Map<String, Set<String>> map, String name1, String name2){
+        if (map.containsKey(name1)){
+            map.get(name1).add(name2);
+        } else {
+            map.put(name1, Set.of(name2));
+        }
     }
 }
