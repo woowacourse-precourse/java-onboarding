3bee07c7268420133a63ebacf80d69c88e90012b
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-10-27 20:26:18 +0900

    기능 1(입력된 문자열의 연속으로 중복된 문자 삭제) 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        boolean isChanged;
+        do {
+            if(cryptogram.length() <= 1) break;
+            deleteDuplicatedChars(cryptogram);
+        } while(true);
         return answer;
+    }
+
+    public static String deleteDuplicatedChars(String cryptogram){
+        StringBuilder sb = new StringBuilder();
+        int cryptogramLen = cryptogram.length();
+        if(cryptogram.charAt(0) != cryptogram.charAt(1))
+            sb.append(cryptogram.charAt(0));
+        for(int i = 1; i < cryptogramLen - 1; ++i){
+            char curChar = cryptogram.charAt(i);
+            char leftChar = cryptogram.charAt(i - 1);
+            char rightChar = cryptogram.charAt(i + 1);
+            if(curChar != leftChar && curChar != rightChar)
+                sb.append(curChar);
+        }
+        return sb.toString();
     }
 }
