356faec38bc8c9ed58ec3190b53667922d55306f
whale0928 <rlagusrl928@gmail.com>
2022-10-27 18:45:23 +0900

    3번 문제 해결
    내가 아싸라 369 게임을 모르는게 치명적이였던 문제.
    3으로 나눠 반환하는거 아냐? 하고 생각했다.
    문제도 제대로 안읽고 , 게임도 몰랐던 문제
    
    for를 반복하며 1부터 number까지 반복한다
    이 때 따로 선언한 메서드를 활용해 3 6 9 가 포함되 있을 경우 증가 연산을 통해 값을 누적한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,7 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
