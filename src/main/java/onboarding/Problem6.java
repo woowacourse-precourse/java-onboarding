0ab3a9311b6dbd34e5294f6225ef1b9ab5efd7ee
Park shinyeong <syngh503@gmail.com>
2022-10-27 23:40:45 +0900

    Problem6/Feat/이메일 형식 체크 구현 이메일 형식을 체크하는 checkEmail 함수를 구현하였습니다. 이메일 길이와 이메일 도메인이 인지 확인합니다. 사용가능한 이메일인 경우 true를 사용할 수 없으면 false를 리턴합니다. Relate to: #6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,23 @@
 package onboarding;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static boolean checkEmail(String email) {
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@email.com";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        if(m.matches() && email.length() >= 11 && email.length() <= 20) {
+            return true;
+        }
+        return false;
     }
 
 }
