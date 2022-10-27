4d90ca571924e12ebe24eaa72dbbd61a26ac51d5
JunH <tjdtls690@gmail.com>
2022-10-27 17:35:02 +0900

    :sparkles: feat(PROBLEM3) : 도메인 객체 이용해서 결과 반환하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,23 +1,12 @@
 package onboarding;
 
+import onboarding.problem3.domain.Numbers;
+
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Problem3 {
     public static int solution(int number) {
-        return IntStream.rangeClosed(1, number)
-                .map(Problem3::countOfThreeSixNine)
-                .reduce(0, Integer::sum);
-    }
-    
-    private static int countOfThreeSixNine(int number) {
-        return (int) Arrays.stream(split(number))
-                .mapToInt(Integer::parseInt)
-                .filter(eachDigit -> eachDigit != 0 && eachDigit % 3 == 0)
-                .count();
-    }
-    
-    private static String[] split(final int number) {
-        return String.valueOf(number).split("");
+        return new Numbers(number).sumCountOfMultipleOfThree();
     }
 }
