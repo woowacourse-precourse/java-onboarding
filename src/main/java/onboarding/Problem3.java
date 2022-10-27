aa084d61221a09d1ca9b183c86b4f1c0fbab80a7
lee <higgs95@naver.com>
2022-10-27 20:25:56 +0900

    feat(problem3): count claps

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int num=1; num <= number; num++) {
+            answer += calculClaps(num);
+        }
         return answer;
     }
+
+    public static int calculClaps(int number) {
+        int claps=0;
+
+        while(number!= 0) {
+            int num = number % 10;
+            number /= 10;
+
+            if (num == 3 || num == 6 || num == 9) {
+                claps++;
+            }
+        }
+
+        return claps;
+    }
+
 }
