84f189aeb169fdff02fdb43b3f58fabdea4ff98b
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-27 18:55:50 +0900

    feat(Problem1): 결과 별 상수 초기화

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private final static int EXCEPTIONS_NUMBER = -1;
+    private static final int DRAW_NUMBER = 0;
+    private static final int WIN_POBI_NUMBER = 1;
+    private static final int WIN_CRONG_NUMBER = 2;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
         return answer;
     }
 }
