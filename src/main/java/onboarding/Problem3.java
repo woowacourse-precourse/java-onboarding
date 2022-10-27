6e78fd616860ca271a2c00775ebbb6fcf57658ac
KimJaeSung <behappy0916@naver.com>
2022-10-27 16:06:37 +0900

    java-onboarding-2-구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,7 +12,10 @@ public class Problem3 {
     }
     public static int solution(int number) {
         int answer = 0;
-
+        for(int i=1; i<number+1; i++){
+            String n = Integer.toString(i);
+            answer += countNumber(n,'3','6','9');
+        }
         return answer;
     }
 }
