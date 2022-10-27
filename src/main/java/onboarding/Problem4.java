7f02079182034df46eb358ac85c04c6be8124e65
ddubidubap <park62000@gmail.com>
2022-10-27 22:39:09 +0900

    PROBLEM4 solution 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -23,6 +23,14 @@ public class Problem4 {
 
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            int reverse = reverseAscii(letter);
+            char reverseChar = (char) reverse;
+            answer += reverseChar;
+        }
+
         return answer;
     }
 }
