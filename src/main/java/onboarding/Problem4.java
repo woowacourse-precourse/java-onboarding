1c986c368154fec9263967b8349680e968b2bf92
SEONMl <sm000117@naver.com>
2022-10-27 15:35:17 +0900

    feat: Problem4 청개구리 사전 class 생성 및 단어 변환 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+
+    static class AbnormalDictionary {
+
+        private char findWord(char c) {
+            if(!(('a'<=c && c<='z') || ('A'<=c && c<='Z'))) {
+                return c;
+            }
+
+            int index = 'a'+'z';
+            if('A'<=c && c<='Z') {
+                index = 'A'+'Z';
+            }
+
+            char res = (char)(index - c);
+            return res;
+        }
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
