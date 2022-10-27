672028f9e9529c6f499bf2d64b0b9ad49310a476
devmizz <garamminchan@gmail.com>
2022-10-27 21:31:56 +0900

    problem1: calculating method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,22 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private int addDigits(int num) {
+        int sum = 0;
+        while(num == 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    private int multipleDigits(int num) {
+        int multiple = 1;
+        while(num == 0) {
+            multiple *= num % 10;
+            num /= 10;
+        }
+        return multiple;
+    }
 }
