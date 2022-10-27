9ae27b78e6bc5fc9976a476b1c30d311dd365876
Ridealist <cjsn92@gmail.com>
2022-10-27 23:52:44 +0900

    feat: Calculator 클래스 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,3 +8,44 @@ class Problem1 {
         return answer;
     }
 }
+
+
+class Calculator {
+    List<Integer> pages;
+
+    Calculator(List<Integer> pages) {
+        this.pages = pages;
+    }
+
+    int returnScore(){
+        int page1_score = getScore(pages.get(0));
+        int page2_score = getScore(pages.get(1));
+        return Integer.max(page1_score, page2_score);
+    }
+    int getScore(int page) {
+        int max_page = Integer.max(getSum(page), getMulti(page));
+        return max_page;
+    }
+
+    int getSum(int page) {
+        String str_page = String.valueOf(page);
+        int sum = 0;
+        for(int i=0; i<str_page.length(); i++) {
+            char c = str_page.charAt(i);
+            sum += c;
+        }
+        return sum;
+    }
+
+    int getMulti(int page) {
+        String str_page = String.valueOf(page);
+        int multi = 1;
+        for(int i=0; i<str_page.length(); i++) {
+            char c = str_page.charAt(i);
+            multi *= c;
+        }
+        return multi;
+    }
+}
+
+
