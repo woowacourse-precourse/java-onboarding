c118f804b311aaea3244c05e3d783e23ff78f642
yewon-Noh <yewo2nn16@gmail.com>
2022-10-27 23:47:24 +0900

    Problem1 : 페이지 번호의 연속 여부 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+
 import java.util.List;
 
 class Problem1 {
@@ -7,4 +8,18 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /**
+     * 페이지 번호의 연속 여부 확인
+     * @param page
+     * @return int
+     */
+    public static int checkPage(List<Integer> page) {
+        int result = -1;
+        if ( page.get(0) + 1 == page.get(1) ) {
+            result = 1;
+        }
+        return result;
+    }
+
 }
