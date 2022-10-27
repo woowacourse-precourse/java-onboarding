b2f2b70cc60d5e5a0389492cc6f5be6d7f6dcc53
kariskan <zkfzpf56@naver.com>
2022-10-27 21:49:36 +0900

    feat: getClapCount 함수 정의

--- src/main/java/onboarding/support/problem3/GameUtil.java
+++ src/main/java/onboarding/support/problem3/GameUtil.java
@@ -3,8 +3,23 @@ package onboarding.support.problem3;
 public class GameUtil {
 
     private int score;
+    private static final int MOD = 10;
 
     public GameUtil(int number) {
+        for (int i = 1; i <= number; i++) {
+            this.score += getClapCount(number);
+        }
+    }
+
+    public static int getClapCount(int number) {
+        int clap = 0;
+        while (number != 0) {
+            if (canGetPoint(number % MOD)) {
+                clap++;
+            }
+            number /= MOD;
+        }
+        return clap;
     }
 
     public static boolean canGetPoint(int digit) {
