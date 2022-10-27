9318c42d21d16147514de89e73844b0bffb64cbd
Kangbin <hz0819@naver.com>
2022-10-27 15:50:06 +0900

    4 Slove

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i=0; i<word.length();i++) {
+            char tmpChar = word.charAt(i);
+            if ((int)tmpChar>=97 && (int)tmpChar<=122) {
+                tmpChar = (char) (122 - (int) tmpChar + 97);
+            } else if ((int)tmpChar>=65 && (int)tmpChar<=90) {
+                tmpChar = (char) (90 - (int) tmpChar + 65);
+            }
+            answer+=tmpChar;
+        }
         return answer;
     }
 }
