ea8b965ab9773ac92289481fd451d93c18a59e73
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-10-27 20:59:10 +0900

    Probelm 3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,31 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            String numberString = Integer.toString(i);
+            for (int j = 0; j < numberString.length(); j++) {
+                char check = numberString.charAt(j);
+                if(isNumber_369(check)){
+                    sum++;
+                }
+            }
+        }
+        int answer = sum;
         return answer;
+    }
+
+    public static boolean isNumber_369(char check) {
+        boolean isExist;
+        switch (check) {
+            case '3':
+            case '6':
+            case '9':
+                isExist = true;
+                break;
+            default:
+                isExist = false;
+        }
+        return isExist;
     }
 }
