a631e728e9839553f64220491772c2c8c6d945fd
khsrla9806 <kimhunsope@gmail.com>
2022-10-27 20:38:40 +0900

    feat(Problem3): 문제 3번 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+    public static int clapCount(int num) {
+        int count = 0;
+        List<Integer> numList = new ArrayList<>();
+        numList.add(num % 10); // 1의 자릿수
+        numList.add(num % 100 / 10); // 10의 자릿수
+        numList.add(num % 1000 / 100); // 100의 자릿수
+        numList.add(num / 1000); // 1000의 자릿수
+
+        for(Integer number : numList) {
+            switch(number) {
+                case 3:
+                case 6:
+                case 9:
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i < number + 1; i++) {
+            answer += clapCount(i);
+        }
+
         return answer;
     }
 }
