2c92fb820fad46720ae881f5c00e8e0f21c78295
nswon <swnam0729@gmail.com>
2022-10-27 19:58:25 +0900

    refactor(Problem2) <deleteDuplicateChars>: 필요없는 메서드 제거 후 개선

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,12 +3,15 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
+
+    private static Stack<Character> characterStack;
+
     public static String solution(String cryptogram) {
-        Stack<Character> decodedCryptogram = deleteDuplicateChars(initStack(cryptogram), cryptogram);
-        return result(decodedCryptogram);
+        initStack(cryptogram);
+        return deleteDuplicateChars(cryptogram);
     }
 
-    private static Stack<Character> deleteDuplicateChars(Stack<Character> characterStack, String cryptogram) {
+    private static String deleteDuplicateChars(String cryptogram) {
         for(int i=1; i<cryptogram.length(); i++) {
             if(validateSameChar(characterStack.peek(), cryptogram.charAt(i))) {
                 characterStack.pop();
@@ -18,27 +21,26 @@ public class Problem2 {
             characterStack.push(cryptogram.charAt(i));
         }
 
-        return characterStack;
+        return result();
     }
 
     private static boolean validateSameChar(char char1, char char2) {
         return char1 == char2;
     }
 
-    private static Stack<Character> initStack(String cryptogram) {
-        Stack<Character> characterStack = new Stack<>();
+    private static void initStack(String cryptogram) {
+        characterStack = new Stack<>();
         characterStack.push(cryptogram.charAt(0));
-        return characterStack;
     }
 
-    private static String result(Stack<Character> decodedCryptogram) {
+    private static String result() {
         StringBuilder sb = new StringBuilder();
 
-        if(decodedCryptogram.empty()) {
+        if(characterStack.empty()) {
             return "";
         }
 
-        decodedCryptogram.forEach(sb::append);
+        characterStack.forEach(sb::append);
         return sb.toString();
     }
 }
