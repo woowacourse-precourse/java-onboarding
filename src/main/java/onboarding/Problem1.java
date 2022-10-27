2e99a242f04537df13f113c859cfa3da4c2fb788
wooseobee <dntjq0807@naver.com>
2022-10-28 01:00:46 +0900

    숫자를 더한 것과 곱한 것의 크기를 비교해 더 큰 수 계산하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,6 +16,25 @@ class Problem1 {
             return -1;
         }
 
+        int pobi_result;
+        int crong_result;
+        int pobi_left_result = calculate(pobi_left, checkLength(pobi_left));
+        int pobi_right_result = calculate(pobi_right, checkLength(pobi_right));
+        int crong_left_result = calculate(crong_left, checkLength(crong_left));
+        int crong_right_result = calculate(crong_right, checkLength(crong_right));
+
+        if (pobi_left_result < pobi_right_result) {
+            pobi_result = pobi_right_result;
+        } else {
+            pobi_result = pobi_left_result;
+        }
+
+        if (crong_left_result < crong_right_result) {
+            crong_result = crong_right_result;
+        } else {
+            crong_result = crong_left_result;
+        }
+
         return answer;
     }
     public static int checkLength(int num) {
@@ -25,5 +44,18 @@ class Problem1 {
             i /= 10;
         }
         return cnt;
+    }
+    public static int calculate(int num, int length) {
+        int add = 0, multiply = 1;
+        for (int i = 0; i < length; i++) {
+            add += num % 10;
+            multiply *= num % 10;
+            num /= 10;
+        }
+        if (add > multiply) {
+            return add;
+        } else {
+            return multiply;
+        }
     }
 }
