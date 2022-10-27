197185ffc3576a0009e6ba0e729f37765c3e8357
juno-junho <ssmm0205@naver.com>
2022-10-27 23:26:40 +0900

    refactor(Problem5): refactor solution method
    1. 명확한 변수명과 유지보수 위해 화폐 단위를 상수로 변경
    2. index 변수의 명확성을 위해 wonListIndex로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> account = new ArrayList<>();
-        List<Integer> won = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
-        int index = 0;
-        for (int i = 0; i < won.size(); i++) {
-            account.add(money / won.get(index));
-            money %= won.get(index++);
+        List<Integer> wallet = new ArrayList<>();
+        final List<Integer> WON_LIST = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
+        int wonListIndex = 0;
+        for (int i = 0; i < WON_LIST.size(); i++) {
+            wallet.add(money / WON_LIST.get(wonListIndex));
+            money %= WON_LIST.get(wonListIndex++);
         }
-        return account;
+        return wallet;
     }
 }
