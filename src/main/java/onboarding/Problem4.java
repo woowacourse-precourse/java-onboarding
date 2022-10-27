79de7511286c4456a48ca354784e6f6518714b1f
Reznov <reznov03048@gmail.com>
2022-10-28 00:32:38 +0900

    문자열을 변환하는 메소드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,16 +1,41 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
         // 입력받은 문자열을 어절 단위로 자름
-        
+        String[] chunk = word.split(" ");
         // 변환하기
+        for(int i=0; i<chunk.length; i++){
+            String str = chunk[i];
+            answer += changeChunk(str);
+            answer += " ";
+        }
 
         // 변환된 문자열 만들기
 
-        return answer;
+        return answer.substring(0, answer.length()-1);
+    }
+
+    /**
+     * 청개구리 사전 기준으로 변환된 문자열을 반환하는 메소드
+     * @param chunk
+     * @return 변환된 문자열을 반환
+     */
+    static String changeChunk(String chunk){
+        String result = "";
+
+        for(int i=0; i<chunk.length(); i++){
+            char c = chunk.charAt(i);
+            result += changeChar(c);
+        }
+
+        return result;
     }
 
     /**
