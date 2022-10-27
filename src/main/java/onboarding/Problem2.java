4cde86ace149bd5db19f7d372f85673d66c0890b
jeongwle <dldl4568@naver.com>
2022-10-27 20:14:05 +0900

    [Feat] Problem2 중복된 문자열 제거하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,16 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    private static final Stack<Integer> start = new Stack<>();
+    private static final Stack<Integer> end = new Stack<>();
     public static String solution(String cryptogram) {
         // 오류 시 error 반환은 스스로 판단하여 구현한 것.
         if (!isValidate(cryptogram)) {
             return "error";
         }
-        String answer = "answer";
-        return answer;
+        return removeDuplicated(cryptogram);
     }
 
     /**
@@ -34,5 +37,42 @@ public class Problem2 {
 
     private static boolean isLowerCase(char ch) {
         return (ch >= 'a' && ch <= 'z');
+    }
+
+    /**
+     * 중복되는 문자열 찾아서 제거
+     */
+    public static String removeDuplicated(String cryptogram) {
+        String result = cryptogram;
+        while (start.size() != 0 || findRange(result)) {
+            String substring = result.substring(start.pop(), end.pop());
+            result = result.replace(substring, "");
+        }
+        return result;
+    }
+
+    public static boolean findRange(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char ch = cryptogram.charAt(i);
+            int endTemp = findEnd(cryptogram, i, ch);
+            if (i != endTemp - 1) {
+                pushStack(i, endTemp);
+                i = endTemp;
+            }
+        }
+        return (start.size() != 0);
+    }
+
+    private static int findEnd(String cryptogram, int index, char ch) {
+        char[] toCharArray = cryptogram.toCharArray();
+        while (index < toCharArray.length && toCharArray[index] == ch) {
+            index++;
+        }
+        return index;
+    }
+
+    private static void pushStack(int startTemp, int endTemp) {
+        start.push(startTemp);
+        end.push(endTemp);
     }
 }
