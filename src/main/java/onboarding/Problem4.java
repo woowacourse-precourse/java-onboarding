6cd225c4817ff216aced359a51d6cf86c08b2bb9
gardenee <snnjjang@naver.com>
2022-10-27 18:56:47 +0900

    problem 4 first commit

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] charArr = word.toCharArray();
+
+        for (char w: charArr) {
+            if (w == ' ') {
+                answer += " ";
+                continue;
+            }
+            if (w - 'a' < 0) {
+                answer += (char)('A' + 'Z' - w);
+            } else {
+                answer += (char)('a' + 'z' - w);
+            }
+        }
+
         return answer;
     }
 }
