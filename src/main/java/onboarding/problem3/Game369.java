d88e5501f8ac289e8ee05f128a1e4fbf2abcc83e
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 17:55:33 +0900

    feat: 박수를 쳐야 할 숫자 추가

---
+++ src/main/java/onboarding/problem3/Game369.java
@@ -0,0 +1,9 @@
+package onboarding.problem3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Game369 {
+	public static final List<String> GAME_TARGET = Arrays.asList("3", "6", "9");
+	
+}
