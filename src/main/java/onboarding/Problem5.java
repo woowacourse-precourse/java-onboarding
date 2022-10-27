12a6bd37018d13cfb7beec25f39c184aa098606f
ys200209 <lys200209@naver.com>
2022-10-28 00:04:33 +0900

    feat(문제5): 각 화폐 단위 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            int divMoney = getDivMoney(i, 50000);
+
+        }
+
         return answer;
     }
+
+    private static int getDivMoney(int depth, int money) {
+
+        if (depth == 8) {
+            return 1;
+        }
+
+        for (int i = 0; i < depth; i++) {
+
+            if (i % 2 == 0) {
+                money /= 5;
+            } else {
+                money /= 2;
+            }
+
+        }
+
+        return money;
+    }
+
 }
