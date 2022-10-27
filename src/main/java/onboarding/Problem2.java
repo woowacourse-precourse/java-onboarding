a9e62ea867f9f477ff975e738bc3dc4424b3f891
squareyun <squareyun@gmail.com>
2022-10-27 21:43:27 +0900

    feat: 중복된 문자 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,10 +3,21 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
+
+        int startIdx = getOverlabStartIdx(answer);
+        while (startIdx != -1) {
+            int endIdx = getOverlabEndIdx(answer, startIdx);
+            String substr1 = answer.substring(0, startIdx);
+            String substr2 = answer.substring(endIdx);
+            answer = substr1 + substr2;
+            startIdx = getOverlabStartIdx(answer);
+        }
+
         return answer;
     }
 
     static int getOverlabStartIdx(String s) {
+        if (s.length() <= 1) return -1;
         for (int i = 0; i < s.length() - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 return i;
@@ -15,14 +26,14 @@ public class Problem2 {
         return -1;
     }
 
-    static String getOverlabEndIdx(String s, int overlabIdx) {
+    static int getOverlabEndIdx(String s, int overlabIdx) {
+        char base = s.charAt(overlabIdx);
         int endIdx = overlabIdx + 1;
-        for (int i = overlabIdx + 2; i < s.length() - 1; i++) {
-            if (i > s.length() - 1)
-                return endIdx;
-
-            if (s.charAt(i) == s.charAt(i + 1)) {
+        for (int i = endIdx; i < s.length(); i++) {
+            if (base == s.charAt(i)) {
                 endIdx += 1;
+            } else {
+                return endIdx;
             }
         }
 
