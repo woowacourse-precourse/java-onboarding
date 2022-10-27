64e21b10b1c4c494948cb54466b60414c80136dd
맹순영 <symaeng98@naver.com>
2022-10-27 19:54:01 +0900

    fix: list 에서의 get 반복 호출 부분 개선

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,21 +4,27 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
+
         // ----- 포비의 점수 계산 -----
         // 포비의 페이지 합 계산
-        int pobiSum = pobi.get(0) + pobi.get(1);
+        int pobiSum = pobiLeft + pobiRight;
         // 포비의 페이지 곱 계산
-        int pobiMul = pobi.get(0) * pobi.get(1);
+        int pobiMul = pobiLeft * pobiRight;
         // 포비의 점수 계산
         int pobiScore = Math.max(pobiSum, pobiMul);
 
         // ----- 크롱의 점수 계산 -----
         // 크롱의 페이지 합 계산
-        int crongSum = crong.get(0) + crong.get(1);
+        int crongSum = crongLeft + crongRight;
         // 크롱의 페이지 곱 계산
-        int crongMul = crong.get(0) * crong.get(1);
+        int crongMul = crongLeft * crongRight;
         // 크롱의 점수 계산
         int crongScore = Math.max(crongSum, crongMul);
+
         return 2;
     }
 }
