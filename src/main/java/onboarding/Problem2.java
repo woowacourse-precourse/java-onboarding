5f13083381fd60c88033ab9abb60132842e88f3e
situodys <situodys@gmail.com>
2022-10-27 18:57:45 +0900

    feat(문제2): Decryptor 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import onboarding.problem2.Decryptor;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Decryptor decryptor = new Decryptor(cryptogram);
         return answer;
     }
 }
---
+++ src/main/java/onboarding/problem2/Decryptor.java
@@ -0,0 +1,9 @@
+package onboarding.problem2;
+
+public class Decryptor {
+    String cryptogram=null;
+
+    public Decryptor(String cryptogram) {
+        this.cryptogram = cryptogram;
+    }
+}
