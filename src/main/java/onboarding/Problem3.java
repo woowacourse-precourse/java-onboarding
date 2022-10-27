e76b77d59bf36ca8438948a2bdfcb2a8d84fddb4
code-tree <dltjdqls1200@naver.com>
2022-10-27 15:10:28 +0900

    feat: Count clabs from 1 to number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,15 +1,36 @@
 package onboarding;
 
+import java.util.Set;
+
 public class Problem3 {
+    static final Set<Integer> clabNumber = Set.of(3, 6, 9);
+
     public static int solution(int number) {
-        int answer = 0;
+        int clabCount = 0;
+        int n = 1;
 
         if (number < 1 || number > 10000) {
             return -1;
         }
 
-        
+        while (n <= number) {
+            clabCount += clab(n);
+            n++;
+        }
 
-        return answer;
+        return clabCount;
+    }
+
+    private static int clab(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            if (clabNumber.contains(n % 10)) {
+                count++;
+            }
+            n /= 10;
+        }
+
+        return count;
     }
 }
