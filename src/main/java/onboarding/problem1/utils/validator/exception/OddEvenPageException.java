7ffdabb8dfa4ab01b1a9e9c9d3bff1982bb49862
song-wooseok <sws981202@gmail.com>
2022-10-27 15:49:48 +0900

    feat: [예외사항 2] List 의 홀/짝 예외처리 구현

---
+++ src/main/java/onboarding/problem1/utils/validator/exception/OddEvenPageException.java
@@ -0,0 +1,7 @@
+package onboarding.problem1.utils.validator.exception;
+
+public class OddEvenPageException extends Exception{
+    public OddEvenPageException(String msg){
+        super(msg);
+    }
+}
