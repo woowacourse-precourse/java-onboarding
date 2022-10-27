38b3b5843dff36722f1dca8e77395d0a85d8a3e0
twknds <twknds@naver.com>
2022-10-27 14:38:50 +0900

    [4번 feature] : change메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,21 +5,24 @@ public class Problem4 {
         char[] arr=word.toCharArray();
         int cnt = 0;
         while (cnt<arr.length){
-            if (arr[cnt]>=65 && arr[cnt]<=90){ //대문자면
-
+            if ((int)arr[cnt]>=65 && (int)arr[cnt]<=90){ //대문자면
+                arr[cnt]= (char) (155-(int)arr[cnt]);
+                cnt+=1;
             }
-            else if (arr[cnt]>=97 && arr[cnt]<=122){ //소문자면
-
+            else if ((int)arr[cnt]>=97 && (int)arr[cnt]<=122){ //소문자면
+                arr[cnt]= (char) (219-(int)arr[cnt]);
+                cnt+=1;
             }
             else{ //알파벳이 아니면
-
+                cnt++;
             }
         }
         return arr;
     }
     public static String solution(String word) {
         String answer = "";
-        change(word);
+        char[] temp = change(word);
+        answer = String.valueOf(temp);
         return answer;
     }
 }
