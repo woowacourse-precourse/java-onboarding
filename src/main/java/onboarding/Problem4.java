73cc690e20b7719ab2b4a1e0fccdbd51a8ea91a4
tjddnr7760 <tjddnr7760@naver.com>
2022-10-27 17:13:53 +0900

    <4번 - 구현> 단어목록 char배열 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        Problem4 problem = new Problem4();
         String answer = "";
+
+        char[] stringarray = word.toCharArray();
+
+        for(int i=0; i<stringarray.length; i++) {
+            stringarray[i] = problem.reserveAlphabet(stringarray[i]);
+        }
+
         return answer;
+    }
+
+    public char reserveAlphabet(char alphabet) {
+        char result = 32;
+        return result;
     }
 }
