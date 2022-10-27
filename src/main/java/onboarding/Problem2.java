de779ad5affe3e31f2e1837207e9745a1fb2add0
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-27 18:11:19 +0900

    problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> stack=new Stack<Character>();
+        stack.push(cryptogram.charAt(0));
+        for(int i=1;i<cryptogram.length();i++){
+            char x=cryptogram.charAt(i);
+            if(stack.peek()==x)
+                stack.pop();
+            else
+                stack.push(x);
+        }
+        for(char c:stack)
+            answer+=c;
         return answer;
     }
 }
--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -8,16 +8,15 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest {
-    @Nested
-    class Problem1Test {
-        @Test
-        void case1() {
-            List<Integer> pobi = List.of(97, 98);
-            List<Integer> crong = List.of(197, 198);
-            int result = 0;
-            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
-        }
-
+//    @Nested
+//    class Problem1Test {
+//        @Test
+//        void case1() {
+//            List<Integer> pobi = List.of(97, 98);
+//            List<Integer> crong = List.of(197, 198);
+//            int result = 0;
+//            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+//        }
 //        @Test
 //        void case2() {
 //            List<Integer> pobi = List.of(131, 132);
@@ -35,23 +34,23 @@ class ApplicationTest {
 //        }
 //    }
 //
-//    @Nested
-//    class Problem2Test {
-//        @Test
-//        void case1() {
-//            String cryptogram = "browoanoommnaon";
-//            String result = "brown";
-//            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
-//        }
-//
-//        @Test
-//        void case2() {
-//            String cryptogram = "zyelleyz";
-//            String result = "";
-//            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
-//        }
-//    }
-//
+    @Nested
+    class Problem2Test {
+        @Test
+        void case1() {
+            String cryptogram = "browoanoommnaon";
+            String result = "brown";
+            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
+
+        @Test
+        void case2() {
+            String cryptogram = "zyelleyz";
+            String result = "";
+            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
+    }
+
 //    @Nested
 //    class Problem3Test {
 //        @Test
@@ -129,5 +128,5 @@ class ApplicationTest {
 //            List<String> result = List.of("andole", "jun", "bedi");
 //            assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
 //        }
-   }
+//}
 }
