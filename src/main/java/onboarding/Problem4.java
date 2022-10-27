39fa804ef9c88056dcbb53134c7a15771b728582
ninaaano <tvxqinak@naver.com>
2022-10-28 00:23:13 +0900

    221028 00:22 prob4 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(ch > 'a' && ch < 'z'){
+                ch = (char) ('z'-(ch-'a'));
+            }
+            if(ch > 'A' && ch < 'Z'){
+                ch = (char) ('Z'-(ch-'A'));
+            }
+            answer += ch;
+        }
         return answer;
     }
 }
