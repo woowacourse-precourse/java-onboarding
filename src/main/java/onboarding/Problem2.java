8af0c591fff275aeb18837c4452ca9290eddf311
limzzum <chfhddl1234@naver.com>
2022-10-27 15:35:31 +0900

    StringBuilder sb -> String answer로 변경
    중복이 없을 때까지 반복해준다.
    -> 중복 제거 전 문자열 길이와 중복 제거 후 문자열 길이 비교

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,21 +2,34 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         cryptogram+= "?";
         String word = "";
 
-        for(int i=0; i<cryptogram.length(); i++){
-            if(String.valueOf(cryptogram.charAt(i)).equals(word))
-                continue;
-            if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
-                word = String.valueOf(cryptogram.charAt(i));
-            }else {
-                word ="";
-                sb.append(cryptogram.charAt(i));
+        while (true) {
+            for (int i = 0; i < cryptogram.length()-1; i++) {
+                if (String.valueOf(cryptogram.charAt(i)).equals(word))
+                    continue;
+
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    word = String.valueOf(cryptogram.charAt(i));
+                }
+                else {
+                    word = "";
+                    answer += String.valueOf(cryptogram.charAt(i));
+
+                }
+            }
+
+            if(cryptogram.length()-1 == answer.length())
+                break;
+            else {
+                System.out.println("현재 + " +answer);
+                cryptogram = answer+"?";
+                answer="";
             }
         }
-        String answer = sb.toString();
+
         return answer;
     }
 }
