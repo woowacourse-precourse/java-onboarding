9abd94e3e9ffdcaf3fc8849881839320c1874700
euncheol-kim <klmeuncheol@kakao.com>
2022-10-28 03:19:42 +0900

    Feat : Add global variable of Problem1Service type. and Modify solution Method contents.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,12 @@
 package onboarding;
 
+import service.problem1.Problem1Service;
 import java.util.List;
 
 class Problem1 {
+    private static final Problem1Service service = new Problem1Service();
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        return service.getWinner(pobi, crong);
     }
 }
