be881732d3bb2ff8199827dd832b97074fab8b88
YooByeongIk <quddlr96@naver.com>
2022-10-27 16:47:00 +0900

    [Problem3] feat : 1부터 해당 숫자까지 누적 박수 횟수 계산 및 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,13 +2,16 @@ package onboarding;
 /*
 ##기능 목록
 
-1. 특정 숫자가 3 or 6 or 9를 포함하는 개수 확인
+1. 특정 숫자의 3 or 6 or 9를 포함하는 개수 계산
 2. 1부터 해당 숫자까지 누적 박수 횟수 계산 및 반환
 
  */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1;i<=number;i++){
+            answer += countNumber(i);
+        }
         return answer;
     }
 
