02176fceb36a81c4f55d95c3b3814482bbc89c8b
limjae <limjae96@gmail.com>
2022-10-27 10:42:02 +0900

    refactor: Problem4 - Change momSay access modifier

--- src/main/java/onboarding/problem4/TreeFrog.java
+++ src/main/java/onboarding/problem4/TreeFrog.java
@@ -1,7 +1,7 @@
 package onboarding.problem4;
 
 public class TreeFrog implements Frog {
-    String momSays;
+    private final String momSays;
 
     public TreeFrog(String momSays) {
         this.momSays = momSays;
