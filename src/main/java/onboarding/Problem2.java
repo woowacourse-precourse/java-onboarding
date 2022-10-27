8faa215a52198c2211f05c52a86fc0e9c608a54d
KTH-96 <taehyun960@gmail.com>
2022-10-28 02:50:11 +0900

    comment(Problem2): 기능구현 목록 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+
+	/**
+	 * 기능 목록 작성
+	 * 1. 문자열의 길이가 1일때 바로 리턴해주는 기능
+	 * 2. 중복문자를 제거하는 기능(중복이 2개 이상일 경우도 처리)
+	 * 3. 데큐에 남은 문자 문자열로 만드는 기능
+	 */
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+		return answer;
+	}
 }
