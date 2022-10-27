fd196c9bf27cea05fbc2128656612dc3bea50474
kimJaeHyeon <kmr2644@gmail.com>
2022-10-27 18:58:04 +0900

    feat: implement method to create words map

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        return null;
     }
+
+    private static Map<String,String> createAlphabetMap() {
+        Map<String , String > map = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            String alpa = Character.toString('a' + i);
+            String reverse = Character.toString('z' - i);
+            map.put(alpa,reverse);
+        }
+        return map;
+    }
+
 }
