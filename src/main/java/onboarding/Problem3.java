50fdb71bd7ccb72d467825632a8e49a1c248bb1b
JHS <wnghdtjr129@naver.com>
2022-10-27 10:19:48 +0900

    refactor: [문제3] 기능 단위 메소드 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 /** TODO
  * 1. 해당 숫자까지 반복
  * 2. 각 숫자마다 3,6,9 포함 갯수 세기
@@ -10,13 +12,18 @@ public class Problem3 {
 
         for (int i = 0; i <= number; i++) {
             String currentNumber = String.valueOf(i);
-
-            for (int j = 0; j < currentNumber.length(); j++) {
-                if (currentNumber.charAt(j) == '3' || currentNumber.charAt(j) == '6' || currentNumber.charAt(j) == '9') {
-                    answer++;
-                }
-            }
+            answer += getClapCount(currentNumber);
         }
         return answer;
+    }
+
+    private static int getClapCount(String currentNumber) {
+        return (int) IntStream.range(0, currentNumber.length())
+                .filter(index -> containThreeSixNine(currentNumber, index))
+                .count();
+    }
+
+    private static boolean containThreeSixNine(String currentNumber, int index) {
+        return currentNumber.charAt(index) == '3' || currentNumber.charAt(index) == '6' || currentNumber.charAt(index) == '9';
     }
 }
