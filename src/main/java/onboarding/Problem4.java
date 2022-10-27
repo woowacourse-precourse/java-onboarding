d65da3c8d5800d4a1c7087ffb4cb88cd29856c33
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-10-27 23:57:32 +0900

    feat: Problem 4, Add charEncoder function
    charEncoder using type casting

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static char charEncoder(char letter){
+        char encodedletter = letter;
+        int asciiIndex = letter;
+        if(asciiIndex >= 'a' && asciiIndex <= 'z'){
+            encodedletter = (char) (Math.abs(25-(asciiIndex - 'a')) + 'a');
+        }
+        if(asciiIndex >= 'A' && asciiIndex <= 'Z'){
+            encodedletter =  (char) (Math.abs(25-(asciiIndex - 'A'))+'A');
+        }
+
+
+        return encodedletter;
+
     }
 }
