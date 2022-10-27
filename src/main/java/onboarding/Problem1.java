b916ea41463eb66475f94c15623507f1538c3b34
KyungKyu Park <rudrb0505@gmail.com>
2022-10-27 11:33:03 +0900

    feat: 페이지 숫자를 변수에 저장

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int pobiLeftPage = pobi.get(0);
+		int pobiRightPage = pobi.get(1);
+		int crongLeftPage = crong.get(0);
+		int crongRightPage= crong.get(1);
+
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
 }
