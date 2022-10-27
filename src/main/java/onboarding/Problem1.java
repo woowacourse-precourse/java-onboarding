82fdf1684c543e3824dc1f2256130fe4d03b0c15
MIN-04 <kimji_0424@naver.com>
2022-10-27 17:07:10 +0900

    update Problem1 기능 요구사항 주석

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,15 +3,29 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /**
+     * 기능 요구 사항
+     * 1. pobi와 crong의 페이지가 연속된 값으로 들어오는지 확인한다. (예외 경우 확인)
+     *      - 예외의 경우 return -1
+     * 2. 페이지 합, 페이지 곱에서 가장 큰 값을 구한다.
+     *      - 페이지 합, 페이지 곱을 구하는 함수를 만든다.
+     *      - 이 함수를 사용하여 큰 값을 구하는 함수를 만든다.
+     * 3. pobi의 큰 값과 crong의 큰 값을 비교한다.
+     *      - pobi가 클 경우 return 1
+     *      - crong이 클 경우 return 2
+     *      - 무승부면 return 0
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
-        // 예외 경우
+        // 1. 예외 경우
         if (pobi.get(1) - pobi.get(0) != 1) return -1;
         else if(crong.get(1) - crong.get(0) != 1)  return -1;
 
+        // 2. 가장 큰 합 구하기
         int pobiMaxScore = findMaxScore(pobi);
         int crongMaxScore = findMaxScore(crong);
 
+        // 3. pobi와 crong의 값 비교
         if (pobiMaxScore > crongMaxScore) return 1;
         else if (pobiMaxScore < crongMaxScore) return 2;
         else return 0;
