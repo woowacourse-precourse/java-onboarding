30d2528ceaa3025a453366ccab899cfb0bbff5eb
unknown <2021110806@dgu.ac.kr>
2022-10-27 17:20:46 +0900

    fix:add condition to problem3 howManyThree method/feat:problem3 solution method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,13 +6,18 @@ public class Problem3 {
     public static void howManyThree(String number) {
         for (int i=0; i<number.length(); i++) {
             int iNumber= (int) Character.getNumericValue(number.charAt(i));
-            if (iNumber==3) {
+            if (iNumber!=0&&iNumber==3) {
                 cnt++;
             }
         }
     }
     public static int solution(int number) {
+        int n=33;
+        int answer=0;
 
-        return answer;
+        for (int i=1; i<=n; i++) {
+            howManyThree(i);
+        }
+        return cnt;
     }
 }
