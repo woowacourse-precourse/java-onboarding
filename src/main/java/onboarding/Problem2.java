f10830eb30b18b884d9590ef0d28b88d6d371a96
metorg <metrog950@gmail.com>
2022-10-27 15:31:38 +0900

    feat: [Problem2] 같은 문자 체크 메서드생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static StringBuilder sb = new StringBuilder();
+
     public static String solution(String cryptogram) {
+
         String answer = "answer";
+        sb.append(cryptogram);
+        answer=sameLetterCheck();
+
         return answer;
+    }
+    static String sameLetterCheck(){
+        for(int i=1;i<sb.length();i++){
+            if(sb.charAt(i-1)==sb.charAt(i)){
+
+            }
+        }
+        return sb.toString();
     }
 }
