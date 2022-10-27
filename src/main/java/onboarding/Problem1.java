cd1b1c7a6b61d3435609eb30375693354166ac03
gomljo <gomljo0307@naver.com>
2022-10-27 14:04:05 +0900

    feat: 제한사항 1번 구현(#8)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static final int CORRECT_LENGTH = 2;
+    public static final int NUMBER_OF_PLAYER = 2;
+    public static final int DIFFERENCE_BETWEEN_PAGES=1;
+    public static final int START_PAGE = 1;
+    public static final int FINAL_PAGE = 400;
+    public static Boolean isCorrectLength(List<Integer> bookPages){
+        return bookPages.size() != CORRECT_LENGTH;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(Problem1.isCorrectLength(pobi)&Problem1.isCorrectLength(crong)){
+            return -1;
+        }
         return answer;
     }
 }
