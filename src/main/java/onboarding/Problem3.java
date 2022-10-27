8585451ffeb2ad272d8efb61f4036013f5905af7
juno-junho <ssmm0205@naver.com>
2022-10-27 22:32:48 +0900

    feat(Problem3): create method for clap count
    테스트 케이스 순서 설정을 통한 3, 6, 9 박수 테스트

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clapCount = 0;
+        for (int i = 1; i <= number; i++) {
+            int targetNumber = i;
+            while (targetNumber > 0) {
+                if (targetNumber % 10 == 3 || targetNumber % 10 == 6 || targetNumber % 10 == 9) {
+                    clapCount++;
+                }
+                targetNumber /= 10;
+            }
+        }
+        return clapCount;
     }
 }
