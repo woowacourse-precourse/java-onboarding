8103780ce2e55ba3bcbf026004ddf67d44daf3fb
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 20:33:59 +0900

    feat: 화폐 가치를 담은 리스트 상수 추가

---
+++ src/main/java/onboarding/problem5/Changer.java
@@ -0,0 +1,9 @@
+package onboarding.problem5;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Changer {
+	public static final List<Integer> CURRENCY = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+	
+}
