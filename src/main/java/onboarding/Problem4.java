a0eb398a52064d8b4b23e321e8db4644ff3dbcd4
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 00:34:31 +0900

    refactor : distinguish lower and upper case

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,8 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
 
+//        System.out.println(getReversedAlphabet(getAscii('o')));
+//        System.out.println((char) 96);
 
         String answer = "";
         return answer;
@@ -12,8 +14,16 @@ public class Problem4 {
         int ascii = (int) character;
         return ascii;
     }
-    private static Character getReversedAlphabet(int ascii){
-        int reversedAscii = 155 - ascii;
+
+    private static Character getReversedAlphabet(int ascii) {
+
+        int reversedAscii = 0;
+
+        if (ascii >= 65 && ascii <= 90) {
+            reversedAscii = 155 - ascii;
+        } else if (ascii >= 97 && ascii <= 122) {
+            reversedAscii = 219 - ascii;
+        }
         Character reversedAlphabet = (char) reversedAscii;
 
         return reversedAlphabet;
