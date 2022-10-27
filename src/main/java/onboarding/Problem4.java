f77438316608056d2240d4f400bbe0b67dd7e857
somsom13 <somin1113@naver.com>
2022-10-27 21:33:08 +0900

    feat(문제4): 알파벳 범위 체크, 변환 결과 반영

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,16 +4,28 @@ public class Problem4 {
     private static final int NO_ALPHABET = 0;
     private static final int LOWER_CASE = -1;
     private static final int UPPER_CASE = 1;
+    private static StringBuilder sb;
 
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
 
+    private static void transferAlphabet(char alphabet) {
+        int charRange = checkCharRange(alphabet);
+        char result = alphabet;
+        if(charRange != NO_ALPHABET) {
+            result = reverseAlphabet(alphabet, charRange);
+        }
+        sb.append(result);
+        return;
+    }
+
     private static char reverseAlphabet(char alphabet, int alphabetCase) {
         double mid = calculateMidAlphabet(alphabetCase);
         double distanceFromMid = mid - alphabet;
-        return (char)(mid + distanceFromMid);
+        char reversed =  (char)(mid + distanceFromMid);
+        return toUpperLowerCase(alphabetCase, reversed);
     }
 
     private static double calculateMidAlphabet(int alphabetCase) {
@@ -21,6 +33,24 @@ public class Problem4 {
             return ((double)'A'+'Z')/2;
         }
         return ((double)'a'+'z')/2;
+    }
+
+    private static char toUpperLowerCase(int alphabetCase, char alphabet) {
+        if(alphabetCase==UPPER_CASE) {
+            return Character.toLowerCase(alphabet);
+        }
+        return Character.toUpperCase(alphabet);
+    }
+
+
+    private static int checkCharRange(char nowChar) {
+        if (nowChar>='A' && nowChar<='Z') {
+            return UPPER_CASE;
+        }
+        if(nowChar >= 'a' && nowChar <= 'z') {
+            return LOWER_CASE;
+        }
+        return NO_ALPHABET;
     }
 
 }
