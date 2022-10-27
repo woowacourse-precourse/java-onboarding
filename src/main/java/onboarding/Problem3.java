fd3f3be94226b96c1fb00783f346a48bd759500d
limzzum <chfhddl1234@naver.com>
2022-10-27 15:49:27 +0900

    숫자에 3,6,9 가 포함된 갯수를 반환하는 기능구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+
         int answer = 0;
         return answer;
+    }
+    static public int count369(int num){
+        int count =0;
+        while (num>0){
+            if(num%10==3|| num%10==6|| num%10==9){
+                count += 1;
+            }
+            num /= 10;
+        }
+        return count;
     }
 }
