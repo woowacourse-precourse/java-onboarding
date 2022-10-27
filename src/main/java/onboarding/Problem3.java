667bcd26c1352d72ee306801f32d71a1be8d5956
keon0711 <fnc6488@gmail.com>
2022-10-28 00:14:32 +0900

    [Problem3] 각 자리수 중 3,6,9가 몇개인지 세는 기능 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,6 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
     }
 
     private static int check369(int digit) {
@@ -15,13 +13,13 @@ public class Problem3 {
     }
 
     private static int count369(int number) {
-        int count369 = 0;
+        int numberOf369 = 0;
         int digit = 0;
         while (number > 0) {
             digit = number % 10;
-            count369 += check369(digit);
+            numberOf369 += check369(digit);
             number /= 10;
         }
-        return count369;
+        return numberOf369;
     }
 }
