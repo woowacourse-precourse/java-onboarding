88dbbfdd6ad9a5da9018175722e1df887d702c82
Heosejin98 <sejin3319@naver.com>
2022-10-28 01:16:33 +0900

    Feat : PROBLEM5 돈 액수 검사 getMoneyType 함수 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,11 +9,34 @@ public class Problem5 {
     static boolean Exception(int money) {
         return (0 < money && money < 1000000) ? false : true;
     }
+
+    static int getMoneyType(int money) {
+        if (money >= 50000)
+            return 50000;
+        else if (money >= 10000)
+            return 10000;
+        else if (money >= 5000)
+            return 5000;
+        else if (money >= 1000)
+            return 1000;
+        else if (money >= 500)
+            return 500;
+        else if (money >= 100)
+            return 100;
+        else if (money >= 50)
+            return 50;
+        else if (money >= 10)
+            return 10;
+        else if (money >= 1)
+            return 1;
+        return 0;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = List.of(1);
 
         if (Exception(money))
             return ERROR;
+
 
         return answer;
     }
@@ -22,7 +45,7 @@ public class Problem5 {
 
         System.out.println(solution(0));
         System.out.println(solution(1000001));
-        System.out.println(solution(100001));
+        System.out.println(getMoneyType(5354));
     }
 
 }
