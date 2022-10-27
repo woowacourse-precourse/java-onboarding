b8f677d5a709f5f6968e9b996a900bfd0f1ebd53
imsongj <imsongj@gmail.com>
2022-10-27 17:30:10 +0900

    feat(Problem3): 수가 타겟 수 인지 확인 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+
+    private static final int[] TARGET_NUMBERS = {3, 6, 9};
+    public static boolean isTargetNumber(int number){
+        for(int i = 0; i < TARGET_NUMBERS.length; i++){
+            if(number == TARGET_NUMBERS[i]){
+                return true;
+            }
+        }
+        return false;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
