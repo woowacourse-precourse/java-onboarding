a3c6f9b33fa93a53d21d90fb8688b06f1fd95be1
suheonjoo <suheonj95@naver.com>
2022-10-27 20:16:40 +0900

    feat(Problem1):  페이지 번호를 받으면, 기능 요구사항에 따른 최대값 반환 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,21 @@ class Problem1 {
         return answer;
     }
 
+    private static int getMaxDataByOnePage(String page) {
+
+        int pageLength = page.length();
+
+        int rightAdd = 0;
+        int rightMultiply = 1;
+        for (int i = 0; i < pageLength; i++) {
+            int number = Integer.parseInt(page.charAt(i) + "");
+            rightAdd += number;
+            rightMultiply *= number;
+        }
+
+        return Math.max(rightAdd, rightMultiply);
+    }
+
     private static boolean checkException(List<Integer> list) {
         if (list.get(0) + 1 != list.get(1)) {
             return false;
