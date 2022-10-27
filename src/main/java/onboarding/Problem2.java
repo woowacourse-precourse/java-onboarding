646efb5eef947c9d3cfb8717616043ee963e08b2
Jin409 <sualng123@gmail.com>
2022-10-28 02:04:00 +0900

    feat(problem2): 암호 해독 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decrypt(cryptogram);
+    }
+
+    private static String decrypt(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = cryptogram.toCharArray();
+
+        for (char c : chars) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
+        return stack.toString();
     }
 }
+
