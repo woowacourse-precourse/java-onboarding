4c22e59ed7311677079a63d83f22fe72e42a9d54
jipyo0106 <jg020106@naver.com>
2022-10-27 15:11:38 +0900

    예외 검출 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int LEFT_PAGE = 0;
+    private static final int RIGHT_PAGE = 1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (pobi.get(LEFT_PAGE) - pobi.get(RIGHT_PAGE) != -1 || crong.get(LEFT_PAGE) - crong.get(RIGHT_PAGE) != -1) {
+            return -1;
+        }
+        return 0;
     }
 }
