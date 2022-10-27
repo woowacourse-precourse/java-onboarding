98d7cf8dddb4ef1b573c89e1eaf5d86a23fa9fa1
Eunkeee <eunkeeeee@gmail.com>
2022-10-28 00:48:49 +0900

    ✨[P3] feat: 결과 도출

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int currentNum =1; currentNum<=number; currentNum++){
+            // 1 ... number
+            String numToStr = Integer.toString(currentNum);
+            String[] numToStrArr = numToStr.split("");
+            int clap = 0;
+            for(String str: numToStrArr){
+                int stringToInt = Integer.parseInt(str);
+                if(stringToInt % 3 ==0 && stringToInt != 0){
+                    clap++;
+                }
+            }
+        answer += clap;
+
+        }
         return answer;
     }
 }
