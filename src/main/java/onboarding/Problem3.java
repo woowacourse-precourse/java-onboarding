7a1ea31f879cec3f978ca98ec70085e69dbb76f3
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-10-27 16:38:10 +0900

    feat : number 범위 내의 문자 자릿수별로 쪼개기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        List<Character> numList = new ArrayList<>();
+        for (int i = 1; i < number + 1; i++) {
+            String numStr = String.valueOf(i);
+            char[] numArr = numStr.toCharArray();
+            for (char c : numArr) {
+                numList.add(c);
+            }
+        }
+
+        int count = 0;
+        return count;
     }
 }
