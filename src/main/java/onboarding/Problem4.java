bb4d3cf167aafd9e53c62cb178d0737bd16ff229
young2866 <62212610+young2866@users.noreply.github.com>
2022-10-27 18:06:52 +0900

    [Fix]Problem4 주석 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,12 +15,13 @@ public class Problem4 {
 	 */
 
 	public static String translateString(String word) {
-		String translatedString = "";
+		String translatedString = "";  // 변환된 문자를 추가할 문자열
 
 		for (int i = 0; i < word.length(); i++) {
-			if (checkAlphabet(word.charAt(i))) {
+			if (checkAlphabet(word.charAt(i))) { // 변환할 문자열이 알파벳이 맞는지 검증
 				translatedString += translateChar(word.charAt(i));
-			} else {
+			}
+			if (!checkAlphabet(word.charAt(i))) { // 알파벳이 아니면
 				translatedString += word.charAt(i);
 			}
 		}
@@ -33,22 +34,24 @@ public class Problem4 {
 	 */
 	public static boolean checkAlphabet(char ch) {
 
-        if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
-            return true;
-        }
+		if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) { // 아스키 코드에서 65~90은 대문자, 97~122가 소문자
+			return true;
+		}
 
 		return false;
 	}
 
 	/**
 	 * 기능 3 알파벳 하나를 변환 하는 기능
+	 * 'A' + 25 = 'Z' , 'Z' - 25 = 'A'
+	 * 따라서 청개구리 변환 공식은 parameter + (25 - '알파벳의 순서 값' * 2)
 	 */
 	public static char translateChar(char ch) {
 		char translatedCharacter = ' ';
 
 		if (checkUpperOrLower(ch)) // 대문자 인 경우
 		{
-			int add = ch - 'A';
+			int add = ch - 'A'; // 몇번째 알파벳인지 구한 값
 			translatedCharacter = (char) (ch + (25 - add * 2));
 			return translatedCharacter;
 		}
@@ -62,10 +65,10 @@ public class Problem4 {
 	 * 기능 4 알파벳 대문자 소문자 구분 하는 기능 true : 대문자 , false : 소문자
 	 */
 	public static boolean checkUpperOrLower(char ch) {
-        if (ch >= 65 && ch <= 90) // 대문자일 경우,
-        {
-            return true;
-        }
+		if (ch >= 65 && ch <= 90) // 대문자일 경우,
+		{
+			return true;
+		}
 
 		return false;
 	}
