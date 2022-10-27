f75881e5b1df66c792fb57162fe480d0b00835e3
sieun <sieun960513@naver.com>
2022-10-27 14:01:23 +0900

    기능 정리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+
+/** 기능 목록
+ * method 1 : 각 자리 숫자 합 반환
+ * method 2 : 각 자리 숫자 곱 반환
+ * method 3 : 곱과 합 중 가장 큰 값 반환
+ * method 4 : 숫자를 각 자리의 수의 배열로 반환
+ * method 5 : 예외 상황 체크
+ */
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int numSum(int num) {
+
+    }
+
+    public static int numMulti(int num) {
+
+    }
+
+    public static int maxResult(int num){
+
+    }
+
+    public static List<Integer> numToDigits(int num){
+
+    }
+
+    public static boolean checkValid() { // 각 배열의 길이, 페이지 번호 확인
+
+    }
+
 }
