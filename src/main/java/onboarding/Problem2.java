1a7b50467ded2d605ac79cb7db3faf1ae703c330
ysng_ysng <yeasung67@gmail.com>
2022-10-28 03:23:28 +0900

    feat: fix feature 2, create new class 'DuplicatedRange' and implement feature 3.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,14 +5,6 @@ import onboarding.crytogramDecoding.CryptogramDecoding;
 public class Problem2 {
 	public static String solution(String cryptogram) {
 		CryptogramDecoding cryptoObject = new CryptogramDecoding();
-		cryptoObject.decode(cryptogram);
-		String answer = "";
-		return answer;
-	}
-
-	public static void main(String[] args) {
-		String result;
-		result = solution("browoanoommnaon");
-		System.out.println(result);
+		return cryptoObject.decode(cryptogram);
 	}
 }
