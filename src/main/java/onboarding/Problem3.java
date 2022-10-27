a97bd27846155c46914f1dd24d5a7dce9ac758eb
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-27 22:39:29 +0900

    [Problem 3] 입력 받은 숫자의 유효성을 검사하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        if(!checkValidation(number)){
+            return -1;
+        }
         return answer;
     }
+
+    private static boolean checkValidation(int number){
+        if(1<number && number<10000){
+            return true;
+        }
+        return false;
+    }
+
 }
