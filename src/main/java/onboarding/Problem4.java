a536dbf0a7654e81eb3a5e074cb01e1e114c12a1
HyunggiPark <phg9587@naver.com>
2022-10-27 22:26:43 +0900

    feat : 대문자 , 소문자 경우를 나눠서 그에 맞춰서 값을 변환하여 사용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char tempChar = word.charAt(i);
+
+            if (Character.isUpperCase(tempChar)) {
+                int diffVal = tempChar - 'A';
+                sb.append((char) ('Z' - diffVal));
+            } else if (Character.isLowerCase(tempChar)) {
+                int diffVal = tempChar - 'a';
+                sb.append((char) ('z' - diffVal));
+            } else {
+                sb.append(tempChar);
+            }
+        }
+
+        return sb.toString();
     }
 }
