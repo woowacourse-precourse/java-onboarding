0eddbf702b781ebd6242ffc647f44a64de4cc74c
recicoco068 <gkapf838@gmail.com>
2022-10-27 21:52:18 +0900

    feat : Problem2 문자열 내 중복 문자를 제거한 문자열을 반환하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,19 @@ public class Problem2 {
         return answer;
     }
 
+    public static String removeDuplication(String str) {
+        String result = "";
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) != str.charAt(i + 1))
+                result += String.valueOf(str.charAt(i));
+            else
+                i += duplicationCount(str, i);
+        }
+        if (str.length() > 1 && str.charAt(str.length() - 2) != str.charAt(str.length() - 1))
+            result += String.valueOf(str.charAt(str.length() - 1));
+        return result;
+    }
+
     public static int duplicationCount(String str, int start) {
         int cnt = 0;
         while (start < str.length() - 1 && str.charAt(start) == str.charAt(start + 1)) {
