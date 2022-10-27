ab6dfaa16e548a975a1cdfa606c4963ba638ed2c
wonho-seo <sjdlrnen0507@naver.com>
2022-10-27 21:36:56 +0900

    [onBoarding-back-Problem4-01][wonho-seo] 청개구리 변환 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,8 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] wordCharArray = word.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char wordChar : wordCharArray){
+            if (Character.isLowerCase(wordChar))
+                sb.append(getChangeLower(wordChar));
+            else if (Character.isUpperCase(wordChar))
+                sb.append(getChangeUpper(wordChar));
+            else
+                sb.append(wordChar);
+        }
+        return sb.toString();
     }
 
     private static char getChangeLower(char c){
