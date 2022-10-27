595fe53bdac3e0fc7a94eb6e2801559dd1613c4e
khj1 <dhepd8849@gmail.com>
2022-10-27 10:15:04 +0900

    feat(problem1): 페이지의 각 자리 숫자들을 구하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static int[] getEachDigits(int page) {
+        return Arrays.stream(String.valueOf(page).split(""))
+            .mapToInt(Integer::parseInt)
+            .toArray();
     }
 }
