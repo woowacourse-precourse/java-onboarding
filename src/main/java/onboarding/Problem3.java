db63b364e0b748bc50fadf4f8cefbafa82c9fb24
limjimin <dkdldsla0000@naver.com>
2022-10-28 02:20:08 +0900

    Feat(Problem3): 3, 6, 9 판별 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    private static final String THREE = "3";
+    private static final String SIX = "6";
+    private static final String NINE = "9";
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static boolean isThreeSixNine(String s) {
+        return s.equals(THREE) || s.equals(SIX) || s.equals(NINE);
     }
 }
