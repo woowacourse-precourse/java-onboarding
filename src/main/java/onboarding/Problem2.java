1bee20420ac6f060d66cb8e9c7d65b3e40e2e75c
alstjr706 <alstjr706@naver.com>
2022-10-27 20:04:33 +0900

    feat : (암호문 문제) Problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import onboarding.problem2.Determine;
+import onboarding.problem2.StackGenerator;
+
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
 
+        StackGenerator generator = new StackGenerator();
+        Stack<String> passwordStack = generator.createStack(cryptogram);
 
-
+        Determine determine = new Determine();
+        String answer = determine.compare(passwordStack);
 
         return answer;
     }
