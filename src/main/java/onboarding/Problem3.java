a838d0c3ca9f31b8f12e96e383aa4f66b18fb883
kimJaeHyeon <kmr2644@gmail.com>
2022-10-27 11:20:30 +0900

    feat: implement method for separate to number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        List<Integer> separateList = separateToNumber(number);
+
+
         return answer;
+
+
+    }
+    private static List<Integer> separateToNumber(int number) {
+        List<Integer> separate = new ArrayList<>();
+        while (number > 0) {
+            separate.add(number % 10);
+            number = number / 10;
+        }
+        return separate;
     }
 }
