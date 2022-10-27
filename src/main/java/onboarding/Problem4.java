43ea20507d7672402029bc8994223c7707ff0798
KimSangHyeok <tkdgur0906@naver.com>
2022-10-28 00:58:35 +0900

    Feat(PROBLEM4): 알파벳을 조건에 맞게 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+    public static char changeAlphabetReverse(char ch){
+        return (char)((char)155 - ch);
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
