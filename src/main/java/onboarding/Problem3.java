4da3e7d3d22e30170ee6a697522ad3243b4f79e4
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-27 13:44:37 +0900

    feat:3번 기본틀 구성및 judge 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            answer+=judge(i); // judge를 통하여 짝 몇번 할 지 계산
+        }
         return answer;
+    }
+
+    public static int judge(int i){
+        int sum=0;
+
+        return sum;
     }
 }
