95988cdc1f6e3f3691481a0219478a8d3ec36610
nswon <swnam0729@gmail.com>
2022-10-27 18:58:29 +0900

    feat(Problem2) <deleteDuplicateChars>: 중복 문자들을 삭제시키는 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,10 +5,19 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        Stack<Character> characterStack = initStack(cryptogram);
-
+        Stack<Character> decodedCryptogram = deleteDuplicateChars(initStack(cryptogram), cryptogram);
 
         return answer;
+    }
+
+    private static Stack<Character> deleteDuplicateChars(Stack<Character> characterStack, String cryptogram) {
+        for(int i=0; i<cryptogram.length(); i++) {
+            if(validateSameChar(characterStack.peek(), cryptogram.charAt(i))) {
+                characterStack.pop();
+            }
+        }
+
+        return characterStack;
     }
 
     private static boolean validateSameChar(char char1, char char2) {
