5e4d1fdfeaa8759b06d289f05dc62c272b8858c7
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-27 23:08:40 +0900

    get max number of page

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,23 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        System.out.println(pobi.get(0).getClass().getName());
+        int pobiMax = getMaxPage(pobi.get(0)) > getMaxPage(pobi.get(0)) ? getMaxPage(pobi.get(0)) : getMaxPage(pobi.get(0));
+        int crongMax = getMaxPage(crong.get(0)) > getMaxPage(crong.get(0)) ? getMaxPage(crong.get(0)) : getMaxPage(crong.get(0));
+
         return answer;
+    }
+
+    public static int getMaxPage(int page) {
+        int plus = 0;
+        int multi = 1;
+        String pageNums = Integer.toString(page);
+
+        for(char pageNum : pageNums.toCharArray()) {
+            int num = Character.getNumericValue(pageNum);
+            plus += num;
+            multi *= num;
+        }
+        return (plus > multi) ? plus : multi;
     }
 }
