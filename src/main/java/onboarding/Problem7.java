64b956b8b55bf31d52b7f93a1e25130310d5f4f5
choyeram <choyeram8@gmail.com>
2022-10-27 18:49:58 +0900

    Feat : show recommended friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,50 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        HashMap<String, List<String>> relationMap = new HashMap<>();
+
+        for (List<String> relation : friends) {
+            if (relationMap.get(relation.get(0)) == null) {
+                relationMap.put(relation.get(0), new ArrayList<>());
+            }
+            if (relationMap.get(relation.get(1)) == null) {
+                relationMap.put(relation.get(1), new ArrayList<>());
+            }
+        }
+        for (List<String> relation : friends) {
+            relationMap.get(relation.get(0)).add(relation.get(1));
+            relationMap.get(relation.get(1)).add(relation.get(0));
+        }
+
+        List<String> myFriends = relationMap.get(user);
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+
+        for (String name : relationMap.keySet()) {
+            if (name.equals(user) || myFriends.contains(name)) continue;
+            
+            List<String> relation = relationMap.get(name);
+            for (String theirName : relation) {
+                if (myFriends.contains(theirName)) {
+                    scoreMap.put(name, scoreMap.getOrDefault(name, 0)+10);
+                }
+            }
+        }
+
+        for (String name : visitors) {
+            if (!myFriends.contains(name)) {
+                scoreMap.put(name, scoreMap.getOrDefault(name, 0)+1);
+            }
+        }
+
+        return scoreMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(Map.Entry::getKey)
+                .limit(5)
+                .collect(Collectors.toList());
     }
 }
+
