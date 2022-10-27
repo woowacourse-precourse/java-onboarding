21ce87bececc3596fb3398cfbef17bfaba1eb33a
LHS-11 <ghktjq1119@naver.com>
2022-10-27 17:22:45 +0900

    Problem3 : 1. 1~number 까지 몇 개의 3,6,9가 나오는지 체크

--- src/main/java/onboarding/Main.java
+++ src/main/java/onboarding/Main.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        String s = "aaaaaaab";
-        String ss = Problem2.solution(s);
-        System.out.println("ss = " + ss);
+//        String s = "aaaaaaab";
+//        String ss = Problem2.solution(s);
+//        System.out.println("ss = " + ss);
+
+        int sss = Problem3.solution(13);
+        System.out.println("args = " + sss);
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int threeNum=0;
+        int sixNum=0;
+        int nineNum=0;
+
+        // 1. 1~number 까지 몇 개의 3,6,9가 나오는지 체크
+        for (int i = 1; i <= number; i++) {
+            String s = String.valueOf(i);
+            threeNum+=countChar(s,'3');
+            sixNum+=countChar(s,'6');
+            nineNum+=countChar(s,'9');
+        }
+        // 2. 나온 3,6,9 의 갯수만큼 더하기
+
+
         return answer;
+    }
+
+    public static int countChar(String str,char ch){
+        return (int) str.chars()
+                .filter(c->c==ch)
+                .count();
     }
 }
