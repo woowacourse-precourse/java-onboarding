ab034b393084df9ffb7b03ab4003f13cbacf1902
limzzum <chfhddl1234@naver.com>
2022-10-27 13:49:14 +0900

    [문제 1] 왼쪽 오른쪽 페이지의 번호에 대해 plus, multiply 함수 실행하여 가장 큰 점수를 각자의 점수로 할당. 두 명의 점수 비교하여 리턴값 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,17 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int pobiScore= 0;
+        int crongScore= 0;
 
-        int answer = 0;
+        for(int i=0; i<2; i++){
+            int num = pobi.get(i);
+            pobiScore = pobiScore>plus(num)? pobiScore: plus(num)>multiply(num)? plus(num) :multiply(num);
+            num = crong.get(i);
+            crongScore = crongScore > plus(num)? crongScore: plus(num)>multiply(num)? plus(num) :multiply(num);
+        }
+
+        int answer = pobiScore>crongScore? 1: pobiScore==crongScore? 0:2;
         return answer;
     }
     static public int plus(int num){
