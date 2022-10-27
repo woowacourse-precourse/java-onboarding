ebe8552671978b48a1ffc8ad95ecf015de6176ea
Hyeonjae-K <href.hjk@gmail.com>
2022-10-27 18:02:52 +0900

    feat: 예외 처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,6 +15,32 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        Problem1 problem1 = new Problem1();
+
+        if (problem1.isExcept(pobi) || problem1.isExcept(crong)) {
+            answer = -1;
+        }
+
         return answer;
+    }
+
+    /**
+     * 페이지 번호에 예외가 있는 경우 true를 반환합니다.
+     *
+     * @param pages [왼쪽 페이지 번호, 오른쪽 페이지 번호]
+     * @return 예외가 있는 경우 true, 그렇지 않으면 false 반환*/
+    boolean isExcept(List<Integer> pages) {
+        int left = pages.get(0);
+        int right = pages.get(1);
+
+        if (left <= 1 || right >= 400) {
+            return true;
+        } else if (left % 2 != 1 || right % 2 != 0) {
+            return true;
+        } else if (left + 1 != right) {
+            return true;
+        }
+
+        return false;
     }
 }
