887cc2d836747029e1f673706fdd7afd9b647486
WhalesBob <ndw8200@naver.com>
2022-10-27 20:28:28 +0900

    Complete to make Problem3(test complete)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        for(int i = 1; i <= number; i++){
+            count += clap(i);
+        }
+
+        return count;
+    }
+    static int clap(int number){
+        int count = 0;
+        String[] array = Integer.toString(number).split("");
+        for(int i = 0; i < array.length; i++){
+            if(haveToClap(array[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean haveToClap(String numberString){
+        String[] compare = {"3","6","9"};
+        for(int i = 0; i < 3; i++){
+            if(numberString.equals(compare[i])){
+                return true;
+            }
+        }
+        return false;
     }
 }
