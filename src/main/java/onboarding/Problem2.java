0a1bd682a53e289af4e12eb0900571a23391976b
ys200209 <lys200209@naver.com>
2022-10-27 15:02:04 +0900

    feat(문제2): 문자열 내에 중복 문자가 존재하는지 여부

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,8 +4,27 @@ public class Problem2 {
 
     public static String solution(String cryptogram) {
 
+        while(isDuplicate(cryptogram)) {
+
+            break;
+        }
 
         return cryptogram;
+    }
+
+    private static boolean isDuplicate(String answer) {
+
+        if (answer.length() <= 1) {
+            return false;
+        }
+
+        for (int i = 0; i < answer.length()-1; i++) {
+            if (checkDuplicate(answer.charAt(i), answer.charAt(i+1))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private static boolean checkDuplicate(char first, char second) {
