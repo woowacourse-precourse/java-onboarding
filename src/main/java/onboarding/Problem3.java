413000045cf614d5a5ee76fa093ef1dbe6afc296
whipbaek <jibaek0513@gmail.com>
2022-10-27 19:07:48 +0900

    P3 solution clear

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,6 +14,7 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++) answer += getCount(i);
         return answer;
     }
 }
