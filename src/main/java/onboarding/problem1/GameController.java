7edb118ec12c4b89b7fbcc3d62fe54c8a8f7d699
xxeol2 <jhj01177@naver.com>
2022-10-27 16:58:45 +0900

    refactor(prob1): 인스턴스 변수 -> 지역 변수

--- src/main/java/onboarding/problem1/GameController.java
+++ src/main/java/onboarding/problem1/GameController.java
@@ -9,9 +9,6 @@ public class GameController {
 	private final List<Integer> pobi;
 	private final List<Integer> crong;
 	private static final int lastPage = 400;
-	private PageValidChecker pageValidChecker;
-	private ScoreCalculator scoreCalculator;
-	private WinnerSelector winnerSelector;
 
 	public GameController(List<Integer> pobi, List<Integer> crong) {
 		this.pobi = pobi;
@@ -19,17 +16,17 @@ public class GameController {
 	}
 
 	public int playGame() {
-		pageValidChecker = new PageValidChecker(lastPage);
+		PageValidChecker pageValidChecker = new PageValidChecker(lastPage);
 		List<List<Integer>> users = Arrays.asList(pobi, crong);
-		if (!users.stream().allMatch(u -> pageValidChecker.checkValid(u))) {
+		if (!users.stream().allMatch(pageValidChecker::checkValid)) {
 			return ERROR.getCode();
 		}
 
-		scoreCalculator = new ScoreCalculator();
+		ScoreCalculator scoreCalculator = new ScoreCalculator();
 		int pobiScore = scoreCalculator.getScore(pobi);
 		int crongScore = scoreCalculator.getScore(crong);
 
-		winnerSelector = new WinnerSelector();
+		WinnerSelector winnerSelector = new WinnerSelector();
 		ResultType winner = winnerSelector.getWinner(pobiScore, crongScore);
 
 		return winner.getCode();
