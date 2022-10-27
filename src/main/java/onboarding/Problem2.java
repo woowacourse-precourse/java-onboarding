01806b2d35a091493a0e62515ee3bcb1e3520337
davejang <davejang98@naver.com>
2022-10-27 13:20:12 +0900

    feat: Problem3 implement
    온보딩 미션 문제2 기능 요구 사항 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
         return answer;
     }
 }
