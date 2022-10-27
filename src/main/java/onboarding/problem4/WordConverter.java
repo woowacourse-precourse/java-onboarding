adbeddaad58a6e18b59b1c3a956f987a553b8668
Sehbeom <j3beom@gmail.com>
2022-10-28 05:32:47 +0900

    feat(Problem4): WordConverter클래스 및 멤버필드 생성
    WordConverter 클래스 추가
    멤버 상수 및 enum 추가

---
+++ src/main/java/onboarding/problem4/WordConverter.java
@@ -0,0 +1,12 @@
+package onboarding.problem4;
+
+public class WordConverter {
+	public static final int ASCII_OF_LOWERCASE_A = 97;
+	public static final int ASCII_OF_LOWERCASE_Z = 122;
+	public static final int ASCII_OF_UPPERCASE_A = 65;
+	public static final int ASCII_OF_UPPERCASE_Z = 90;
+
+	private enum CharacterCategory {
+		LOWERCASE, UPPERCASE, NOT_ALPHABET
+	}
+}
