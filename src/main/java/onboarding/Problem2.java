cd51cfe1b85fa578a8a70ba50ab933305f0d780d
myungsikyoon <yms8795@gmail.com>
2022-10-27 22:11:47 +0900

    feat(problem2) : Implement removeDuplicates

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,13 +9,32 @@ public class Problem2 {
     * 1,2를 중복이 제거 될 때 까지 반복됨.
     * */
 
+    //1. 기능1
     static int isDuplicates(String str){
-        for(int i=0; i<str.length(); i++){
+        for(int i=0; i<str.length()-1; i++){
             if(str.charAt(i) == str.charAt(i+1)) return i;
         }
-        return 0;
+        return -1; // 0번 idx를 사용함으로 -1로 반환
     }
 
+    //2. 기능2
+    static String removeDuplicates(String cryptogram){
+
+        int start = isDuplicates(cryptogram);
+
+        if(start == -1){ // 비교연산자 써야함.
+            return cryptogram;
+        }
+
+        int end = start+1;
+
+        for(; end<cryptogram.length(); end++){
+            if(cryptogram.charAt(start) != cryptogram.charAt(end)) break;
+        }
+
+        String newCryptogram = cryptogram.substring(0,start) + cryptogram.substring(end);
+        return removeDuplicates(newCryptogram);
+    }
 
     public static String solution(String cryptogram) {
         return removeDuplicates(cryptogram);
