c88bc02220c0c66cc1969f5bafae9d0cd84ec4f6
Hongsugyeong <olivia258@naver.com>
2022-10-27 20:21:05 +0900

    [problem3] 3,6,9 갯수 세는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,4 +8,22 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /*
+     *3,6,9 갯수 세는 함수
+     */
+    static int count(int number){
+        int answer = 0;
+        for(int i=1;i<=number;i++) {
+            int num = i;
+            while (num != 0) {
+                int result = num % 10;
+                num = num/10;
+                if (result>0 && result % 3 == 0) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
 }
