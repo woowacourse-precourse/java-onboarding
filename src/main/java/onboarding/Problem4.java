1276358588f5c5ae4b760ad33f8ebb0132d794cb
23Yong <1239824@naver.com>
2022-10-27 10:14:14 +0900

    [feat] 변환된 문자열 반환
    - 문자열이 주어지면 이를 뒤집어 반환하도록 구현
    - converToReverseCharacter 함수는 주어진 문자를 뒤집을 것으로 기대

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return makeReverseString(word);
+    }
+
+    private static String makeReverseString(String word) {
+        StringBuilder ret = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            ret.append(convertToReverseCharacter(word.charAt(i)));
+        }
+
+        return ret.toString();
+    }
+
+    private static char convertToReverseCharacter(char ch) {
+        return ch;
     }
 }
