38541d41bece05d1f63649cabfe36f9e919d709c
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 01:58:06 +0900

    [Problem2] 기능 목록 설계

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem2 {
+
+    /**
+     * 기능 목록
+     * 1. 중복된 문자 있는지 체크
+     * 2. 중복된 문자 제거
+     */
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
