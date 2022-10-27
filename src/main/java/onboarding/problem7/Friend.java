3531a621a62b0d1011b365e8af5876bea5c561e7
hyun6ik <fkanwm@naver.com>
2022-10-27 20:35:28 +0900

    feat(58) : [기능 7] : Friend 클래스 생성 및 구현

--- src/main/java/onboarding/problem7/Friend.java
+++ src/main/java/onboarding/problem7/Friend.java
@@ -21,6 +21,17 @@ public class Friend {
     }
 
     private void validateName(String name) {
+        validateNameLength(name);
+        validateNameIsLowerCase(name);
+    }
+
+    private void validateNameIsLowerCase(String name) {
+        if (!name.equals(name.toLowerCase())) {
+            throw new IllegalArgumentException("아이디는 소문자이어야 합니다.");
+        }
+    }
+
+    private void validateNameLength(String name) {
         if (name.length() < 1 || name.length() > 30) {
             throw new IllegalArgumentException("아이디는 길이가 1 이상 30 이하인 문자열이어야 합니다.");
         }
