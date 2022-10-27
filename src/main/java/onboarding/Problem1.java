506ad53309b5a53a2c247a2b1a0b35ce487d6362
Jzakka <mouse4786@gmail.com>
2022-10-27 23:29:11 +0900

    문제1 해결

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,6 +16,16 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        try {
+            int pobiMax = getMaxValue(pobi);
+            int crongMax = getMaxValue(crong);
+
+            answer = judge(pobiMax, crongMax);
+        } catch (pageException e) {
+            answer = -1;
+        }
+
         return answer;
     }
 
