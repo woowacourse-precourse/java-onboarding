09282a7a9221063bbf7ba1c13f41ec759455bfec
kariskan <zkfzpf56@naver.com>
2022-10-27 22:08:41 +0900

    refactor: 정의한 클래스를 이용한 solution 함수 리팩토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,33 +1,12 @@
 package onboarding;
 
+import onboarding.support.problem4.Constants;
+import onboarding.support.problem4.DictionaryConverter;
+
 public class Problem4 {
 
-    public static final int LAST_CHARACTER_ASCII = 25;
-
     public static String solution(String word) {
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = 0; i < word.length(); i++) {
-            answer.append(getDictionaryCharacter(word.charAt(i)));
-        }
-
-        return answer.toString();
+        DictionaryConverter dictionaryConverter = new DictionaryConverter(word);
+        return dictionaryConverter.getConvertedString();
     }
-
-    public static char getDictionaryCharacter(char original) {
-        if (original == ' ') return original;
-        int base = getBaseCharacterASCII(original);
-        return (char) (LAST_CHARACTER_ASCII - (original - base) + base);
-    }
-
-    public static int getBaseCharacterASCII(char c) {
-        int base = 'a';
-        if (isUpper(c)) base = 'A';
-        return base;
-    }
-
-    public static boolean isUpper(char a) {
-        return a >= 'A' && a <= 'Z';
-    }
-
 }
