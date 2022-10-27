f108ed759decd7d9dd03c39594e611ac9c545573
keon0711 <fnc6488@gmail.com>
2022-10-28 00:07:46 +0900

    [Problem3] 숫자가 3,6,9 중 하나인지 판별하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static boolean check369(int number) {
+        if (number == 3) return true;
+        if (number == 6) return true;
+        if (number == 9) return true;
+
+        return false;
+    }
 }
