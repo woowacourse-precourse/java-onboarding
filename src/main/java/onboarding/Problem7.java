3a7ac76d3e7347f693960eb6cac37d6ee3666865
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-27 18:53:48 +0900

    feat: problem7 - user의 친구 리스트를 만드는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> friendList = new ArrayList<>();
+
+        for (int i = 0; i < friends.size(); i++) {
+            friendList = insertFriendToListIfUserFriend(friends.get(i).get(0), user, friendList);
+            friendList = insertFriendToListIfUserFriend(friends.get(i).get(1), user, friendList);
+        }
         return answer;
+    }
+    //user의 친구목록을 작성하는 기능
+    public static List<String> insertFriendToListIfUserFriend(String name, String user, List<String>friendList){
+        if (name.equals(user)) {
+            friendList.add(name);
+        }
+        return friendList;
     }
 }
