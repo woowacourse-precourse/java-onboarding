ec0dbbcd71c467c24cf6484f6df7ac5333829713
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-10-27 14:48:21 +0900

    feat : add problem2
    ### 구현방법
    연속된 길이의 문자를 세서 길이가 2가 될경우 연속으로 판단 후, substring을 사용하여 연속된 문자열 제거하는 방식으로 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+
+	public static String solution(String cryptogram) {
+		char tmp = cryptogram.charAt(0);
+		int continuationLength = 0;
+		for (int i = 0; i < cryptogram.length(); i++) {
+			if (tmp == cryptogram.charAt(i)) {
+				continuationLength++;
+			} else {
+				tmp = cryptogram.charAt(i);
+				continuationLength = 1;
+			}
+			if (continuationLength == 2) {
+				int start = i - continuationLength + 1;
+				cryptogram = cryptogram.substring(0, start) + cryptogram.substring(i + 1);
+				continuationLength = 0;
+				i = -1;
+			}
+		}
+		return cryptogram;
+	}
+
 }
