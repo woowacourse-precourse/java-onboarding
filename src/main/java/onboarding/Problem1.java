0afd58aadbbbb5cbd1de1ef48a07041a61d5a10b
¤knj <krkoknj@gmail.com>
2022-10-28 00:17:39 +0900

    포비와 크롱의 각자리 숫자를 List에 넣어 반환 받는다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,7 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 1. 첫번째 페이지 혹은 마지막 페이지가 매개변수로 들어오면 -1을 리턴한다.
+ * 2. 포비와 크롱의 각자리 숫자를 구한다.
+ * 3. 포비와 크롱의 각자리 숫자를 더하거나 곱하여 큰수를 구한다.
+ * 4. 포비와 크롱의 큰 수끼리 비교한다.
+ * 5. 포비의 숫자가 크면 1을 리턴한다.
+ * 6. 크롱의 숫자가 크면 2를 리턴한다.
+ * 7. 무승부면 0을 리턴한다.
+ * 8. 예외사항은 -1을 리턴한다.
+ */
 class Problem1 {
 
     private static final int FIRST_PAGE = 1;
@@ -13,7 +24,24 @@ class Problem1 {
             return -1;
         }
 
+        List<Integer> pobiEachNumbers = getEachDigitNumber(pobi);
+        List<Integer> crongEachNumbers = getEachDigitNumber(crong);
+
         return answer;
+    }
+
+    private static List<Integer> getEachDigitNumber(List<Integer> getEach) {
+        int num;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < getEach.size(); i++) {
+            num = getEach.get(i);
+            while (num > 0) {
+                list.add(num % 10);
+                num /= 10;
+            }
+        }
+
+        return list;
     }
 
     public static boolean validate(List<Integer> checkList) {
