ab18ce731c46873cc8117ffbb013d1869938ab5f
TaeMin <xoals3094@naver.com>
2022-10-28 06:35:01 +0900

    no.5 check_unit_count method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,6 +11,18 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    public static List<Integer> check_unit_count(){
+        int[] money_unit = {50000,10000,5000,1000,500,100,50,10,1};
+        List<Integer> unit_count = new ArrayList<Integer>();
+        for(int i = 0; i < money_unit.length; i++){
+            int unit = money_unit[i];
+            int count = counting(unit);
+            unit_count.add(count);
+        }
+        return unit_count;
+    }
+
     public static int counting(int unit){
         int count = rest_money / unit;
         rest_money = rest_money % unit;
