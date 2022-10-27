4bf1875e2d1a135951dc77af34741452767c7a5a
kariskan <zkfzpf56@naver.com>
2022-10-27 21:48:07 +0900

    feat: GameUtil 클래스 멤버 변수 및 getter 정의

---
+++ src/main/java/onboarding/support/problem3/GameUtil.java
@@ -0,0 +1,13 @@
+package onboarding.support.problem3;
+
+public class GameUtil {
+
+    private int score;
+
+    public GameUtil(int number) {
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
