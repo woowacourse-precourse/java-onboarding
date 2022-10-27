86e9e92cdeabfaf23aadb035b9e6ccfd37631dc4
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-27 14:59:49 +0900

    C:/Program Files/Git/, %연산을 활용 기능 추가 후 정답 List에 주입

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,6 +10,13 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         List<Integer> money_unit = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100,50, 10, 1));// 돈 단위별로 list에 넣어놓기
 
+        for(int i=0; i<money_unit.size(); i++){ //반복문 진행시 각 돈 단위에 맞게 처리
+
+            int unit=money_unit.get(i);
+            answer.add(money / unit);
+            money = money%unit;
+
+        }
 
         return answer;
     }
