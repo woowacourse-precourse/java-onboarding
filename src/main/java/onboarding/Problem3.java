506782ce6bbd695621181109bdb7ae60884a210f
wlsh44 <k87913j@gmail.com>
2022-10-27 14:24:59 +0900

    feat: solution 추가 [문제3]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int num = 1; num <= number; num++) {
+            String numStr = String.valueOf(num);
+
+            answer += count369(numStr);
+        }
         return answer;
+    }
+
+    private static int count369(String numStr) {
+        int cnt = 0;
+        for (int i = 0; i < numStr.length(); i++) {
+            if (numStr.charAt(i) == '3' ||
+                    numStr.charAt(i) == '6' ||
+                    numStr.charAt(i) == '9') {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
