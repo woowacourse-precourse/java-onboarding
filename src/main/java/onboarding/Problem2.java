fff67d738e72bac00be8e36dfa55f940da71293f
greeng00se <thegreengoose7@gmail.com>
2022-10-27 19:28:49 +0900

    fix(Problem2): 연속되는 중복문자가 3개 이상 존재하는 경우도 해독 가능하도록 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem2 {
     private static final int MIN_DECRYPT_LENGTH = 2;
@@ -23,19 +25,31 @@ public class Problem2 {
             return cryptogram;
         }
 
+        List<Character> result = new ArrayList<>();
         ArrayDeque<Character> queue = new ArrayDeque<>();
+
         for (char c : cryptogram.toCharArray()) {
-            if (queue.isEmpty() || queue.getLast() != c) {
+            if (queue.isEmpty() || queue.getLast() == c) {
                 queue.addLast(c);
-            } else {
-                queue.pollLast();
+                continue;
             }
+
+            if (queue.size() == 1) {
+                result.add(queue.getLast());
+            }
+
+            queue.clear();
+            queue.addLast(c);
         }
 
-        return queueToString(queue);
+        if (queue.size() == 1) {
+            result.add(queue.getLast());
+        }
+
+        return queueToString(result);
     }
 
-    private static String queueToString(ArrayDeque<Character> queue) {
+    private static String queueToString(List<Character> queue) {
         StringBuilder sb = new StringBuilder();
         for (char c : queue) {
             sb.append(c);
