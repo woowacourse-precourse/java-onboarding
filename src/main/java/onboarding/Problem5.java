4dc62bf0eb7729ee8e300bf6b7ac95e8c3b79583
jaehyeon <jhjh0022@naver.com>
2022-10-27 23:38:07 +0900

    일단구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int coin[] = {1, 10, 50, 100, 500, 1000, 5000, 10000, 50000};
+        int idx = coin.length -1;
+        while (money > 0){
+            answer.add(money/ coin[idx]);
+            money %= coin[idx];
+            idx -= 1;
+        }
         return answer;
     }
+
 }
+
