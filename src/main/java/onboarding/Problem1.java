8acb45bfd57c46991a0666e702238fe6d26fcaec
hyeongu <wancern@naver.com>
2022-10-28 00:30:21 +0900

    Problem1. 예외사항 메소드 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 /*
 * 1. 예외사항 체크 메소드 작성
 * 2. 최댓값 메소드 작성
@@ -11,5 +11,19 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    // 예외사항 체크 메소드( 예외사항일때 true 리턴 )
+    public static boolean exception(List<Integer> pobi, List<Integer>crong){
+        if(pobi.get(0) % 2 != 1 || crong.get(0) % 2 != 1){
+            return true;
+        }
+        if(pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)){
+            return true;
+        }
+        if(pobi.get(0) == 1 || pobi.get(1) == 400 || crong.get(0) == 1 || crong.get(1) == 400 ){
+            return true;
+        }
+        return false;
     }
 }
