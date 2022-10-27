000df74adb377e1dd872a449714f5e2a7e68375e
WonJeongYeon <wonjungyoun@naver.com>
2022-10-27 14:12:14 +0900

    20221027 Problem3.java 기초공사 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,29 +1,29 @@
 package onboarding;
 
-import java.util.stream.IntStream;
-
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        int[] gameNumbers = {3, 6, 9};
-        for (int i = 1; i <= number; i++) {
-            String numStr = Integer.toString(i);
-            System.out.println(numStr + "o");
-            int currentNum = 0;
-            for (int j = 0; j < numStr.length(); j++) {
-                currentNum = numStr.charAt(j) - '0';
-                System.out.println(numStr.length());
-                System.out.println(numStr.charAt(j));
-                System.out.println(currentNum);
-                for (int k = 0; k < 3; k++) {
-                    if (currentNum == gameNumbers[k]) {
-                        answer++;
+        if (number < 1 || number > 10000) {
+            throw new IllegalStateException("숫자는 1 이상, 10,000 이하여야 합니다.");
+        } else {
+            int[] gameNumbers = {3, 6, 9};
+            for (int i = 1; i <= number; i++) {
+                String numStr = Integer.toString(i);
+                int currentNum;
+                for (int j = 0; j < numStr.length(); j++) {
+                    currentNum = numStr.charAt(j) - '0';
+                    for (int k = 0; k < 3; k++) {
+                        if (currentNum == gameNumbers[k]) {
+                            answer++;
+                        }
                     }
                 }
             }
-        }
+            System.out.println("number result");
+            System.out.println(number + "  " + answer);
 
-        return answer;
+            return answer;
+        }
     }
 }
