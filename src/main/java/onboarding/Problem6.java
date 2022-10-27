4ce2093df925d5095a49cae4a2703ce05c4dd518
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-27 18:55:06 +0900

    feat: Problem6 - 중복 닉네임을 가진 회원을 찾아 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,20 +8,23 @@ import java.util.Map;
 import java.util.Set;
 
 public class Problem6 {
+    public static void main(String[] args) {
+
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         HashMap<String, Integer> userTwoLetterList = new HashMap<>();
         Set<String> DuplicateNameList = new HashSet<>();
-
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         for (List<String> form : forms) {
             makeTwoLetterList(form.get(1), userTwoLetterList);
         }
         for (Map.Entry<String, Integer> entry : userTwoLetterList.entrySet()) {
             makeDuplicateNameList(DuplicateNameList, entry);
         }
-
-
-
+        for (List<String> form : forms) {
+            makeWarningList(answer, DuplicateNameList, form);
+        }
         return answer;
     }
 
@@ -32,10 +35,16 @@ public class Problem6 {
             twoLetterList.put(userTwoLetter, twoLetterList.getOrDefault(userTwoLetter, 0) + 1);
         }
     }
-
     public static void makeDuplicateNameList(Set<String> duplicateList, Map.Entry<String, Integer> entry) {
         if (entry.getValue() > 1) {
             duplicateList.add(entry.getKey());
+        }
+    }
+    public static void makeWarningList(List<String> warningList, Set<String> duplicateList, List<String> form) {
+        for (String s : duplicateList) {
+            if (form.get(1).contains(s)) {
+                warningList.add(form.get(0));
+            }
         }
     }
 }
