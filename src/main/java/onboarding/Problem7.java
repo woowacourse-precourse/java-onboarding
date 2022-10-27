6d86efaa8d17c386a10a828072d82fb5a5131da4
kevstevie <jianna442@gmail.com>
2022-10-28 01:31:52 +0900

    feat: add getFriendScore

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import javax.print.attribute.HashPrintServiceAttributeSet;
 import java.util.*;
 
 public class Problem7 {
@@ -11,6 +12,12 @@ public class Problem7 {
         for (int i = 0; i < friends.size(); i++) {
             makeConnections(friendsList, friends.get(i).get(0), friends.get(i).get(1));
         }
+
+        Set<String> userFriendSet = friendsList.get(user);
+
+        Map scores = getFriendScore(friendsList, userFriendSet);
+
+        
     }
 
     static void makeConnections(Map<String, Set<String>> map, String name1, String name2){
@@ -22,5 +29,18 @@ public class Problem7 {
             set.add(name2);
         }
         map.put(name1, set);
+    }
+
+    static Map getFriendScore(Map<String, Set<String>> friendList, Set<String> userFriends){
+        Map<String, Integer> result = new HashMap<>();
+
+        for (String str : userFriends) {
+            Iterator<String> it = friendList.get(str).iterator();
+            while(it.hasNext()){
+                String name = it.next();
+                result.put(str, result.getOrDefault(str, 0) + 10);
+            }
+        }
+        return result;
     }
 }
