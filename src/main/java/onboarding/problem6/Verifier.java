551f1c229fb0fd49709d80b14e960c9fd1e3e8d5
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-28 01:23:25 +0900

    feat: 중복되는 이름이 있다면 그 Crew의 email을 정렬해서 반환하는 기능

--- src/main/java/onboarding/problem6/Verifier.java
+++ src/main/java/onboarding/problem6/Verifier.java
@@ -1,7 +1,9 @@
 package onboarding.problem6;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Verifier {
 	private static boolean isDuplicate(Crew crewOne, Crew crewTwo) {
@@ -16,5 +18,21 @@ public class Verifier {
 			return true;
 		}
 		return false;
+	}
+
+	public static List<String> extractDuplicateCrews(List<List<String>> forms) {
+		Set<String> result = new LinkedHashSet<>();
+		List<Crew> crews = Crew.createCrews(forms);
+
+		for (Crew crewOne : crews) {
+			for (Crew crewTwo : crews) {
+				if (isDuplicate(crewOne, crewTwo)) {
+					result.add(crewOne.getEmail());
+					break;
+				}
+			}
+		}
+
+		return result.stream().sorted().collect(Collectors.toList());
 	}
 }
