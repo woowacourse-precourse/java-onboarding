0774f16879f2aaa423845dd13c2266ea8716b200
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-10-28 00:15:56 +0900

    feat: 예외 처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int EXCEPTION = -1;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int TIE_GAME = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!isvalidPages(pobi) || !isvalidPages(crong)) {
+            return EXCEPTION;
+        }
+
         return answer;
+    }
+
+    public static boolean isvalidPages(List<Integer> pages) {
+        if (pages.size() != 2 || pages.get(0) == 0 || pages.get(1) == 400 || pages.get(0) % 2 != 1 || pages.get(1) % 2 != 0 || pages.get(0) + 1 != pages.get(1)) {
+            return false;
+        }
+
+        return true;
     }
 }
