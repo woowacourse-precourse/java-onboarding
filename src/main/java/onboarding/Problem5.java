46106398ccd60f0eb7fb826a0be50c57d9cb024d
jiwon1217 <koagjw@gmail.com>
2022-10-28 02:52:45 +0900

    [FEATURE] Solve the Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> coins = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+
+        for (int i = 0; i < coins.size(); i++) {
+            if (money >= coins.get(i)) {
+                answer.add(i, money / coins.get(i));
+                money = money % coins.get(i);
+            } else {
+                answer.add(i, 0);
+            }
+        }
         return answer;
     }
 }
