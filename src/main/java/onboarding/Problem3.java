1eced72ecfa58f02eccd6ad4c6d5714f88eb6bb5
sieun <sieun960513@naver.com>
2022-10-27 16:45:38 +0900

    [Problem3] :(checkValid) 제한사항 점검 [신규]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+/** 기능 목록
+ * 
+ * checkValid : 예외 처리
+ */
+
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static boolean checkValid(int num) {
+        return 1 <= num && num <= 10000;
     }
 }
