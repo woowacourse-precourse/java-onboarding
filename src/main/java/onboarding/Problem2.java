060f80a651b3f36ed68300c754d9e558b4b84a0d
TaeMin <xoals3094@naver.com>
2022-10-28 00:04:26 +0900

    no.2 delete continuous method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,12 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    public static String delete_continuous(String str, char c){
+        int index = str.indexOf(c);
+        while(index == 0){
+            str = str.substring(1);
+            index = str.indexOf(c);
+        }
+        return str;
+    }
 }
