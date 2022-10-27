864ef7cfcc6f7873103dead729da1664b7e9c72f
JHS <wnghdtjr129@naver.com>
2022-10-27 10:08:40 +0900

    refactor: [문제2] 기능 단위 메소드 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,30 +9,39 @@ package onboarding;
  */
 public class Problem2 {
     public static String solution(String cryptogram) {
+        String answer = cryptogram;
         boolean decoding = true;
 
         while (decoding) {
             decoding = false;
 
-            for (int i = 0; i < cryptogram.length() - 1; i++) {
-                char currentChar = cryptogram.charAt(i);
+            for (int start = 0; start < answer.length() - 1; start++) {
+                char currentChar = answer.charAt(start);
 
-                int count = 0;
-                for (int j = i + 1; j < cryptogram.length(); j++) {
-                    char nextChar = cryptogram.charAt(j);
-                    if (currentChar != nextChar) {
-                        break;
-                    }
-                    count = j - i + 1;
-                }
-                cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + count);
+                int count = getDuplicatedCharCount(start, answer, currentChar);
+                answer = removeDuplicateChar(answer, start, count);
 
                 if (count > 0) {
                     decoding = true;
-                    i--;
+                    start--;
                 }
             }
         }
-        return cryptogram;
+        return answer;
+    }
+
+    private static int getDuplicatedCharCount(int start, String cryptogram, char currentChar) {
+        int count = 0;
+        for (int end = start + 1; end < cryptogram.length(); end++) {
+            if (currentChar != cryptogram.charAt(end)) {
+                break;
+            }
+            count = end - start + 1;
+        }
+        return count;
+    }
+
+    private static String removeDuplicateChar(String cryptogram, int start, int count) {
+        return cryptogram.substring(0, start) + cryptogram.substring(start + count);
     }
 }
