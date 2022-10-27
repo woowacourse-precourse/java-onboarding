75ce9976578e3dd977b9431d239e80c2db57cea3
WonJeongYeon <wonjungyoun@naver.com>
2022-10-27 13:12:38 +0900

    20221027 Problem2.java 예외처리나 클린코드 없이 작동 기능만 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = cryptogram.toLowerCase();
+        String[] charList = answer.split("");
+        List<String> charArrayList = new ArrayList<>(Arrays.asList(charList));
+        List<Integer> indexList = new ArrayList<>();
+        do {
+            indexList.clear();
+            System.out.println(indexList);
+            for (int i = 0; i < charArrayList.size(); i++) {
+                if (i != charArrayList.size()-1) {
+                    System.out.println(indexList);
+                    if (charArrayList.get(i).equals(charArrayList.get(i + 1))) {
+                        indexList.add(i);
+                        indexList.add(i + 1);
+                        System.out.println(indexList.get(0));
+                    }
+                }
+            }
+            for (int i = indexList.size()-1; i >= 0; i--) {
+                charArrayList.remove(Integer.parseInt(indexList.get(i).toString()));
+                System.out.println(charArrayList);
+            }
+
+        } while (!indexList.isEmpty());
+
+        answer = String.join("", charArrayList);
+
+        System.out.println("     cryptogram          result     ");
+        System.out.println("\"" + cryptogram + "\"" +  "    " + "\"" + answer + "\"");
+
         return answer;
     }
+
+
 }
