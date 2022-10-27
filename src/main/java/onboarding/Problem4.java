af607a5ba697e3d16adc38540c23e33b9030c50e
jeein2222 <jiinbag057@gmail.com>
2022-10-27 21:57:40 +0900

    problem4 청개구리 문제 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] mylist=word.toCharArray();
+        int n=0;
+
+        for(char c:mylist){
+            n=(int)c;
+            if(n>=65 && n<=90){
+                c=(char)(155-n);
+            }else if(n>=97 && n<=122){
+                c=(char)(219-n);
+            }
+            answer+=c;
+        }
+
         return answer;
     }
 }
