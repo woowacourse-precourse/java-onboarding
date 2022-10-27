178e88b5e95271f28f3def4cd93c15c8ba7af1b0
myungsikyoon <yms8795@gmail.com>
2022-10-27 21:42:19 +0900

    feat(problem2) : Implement isDuplicates

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,7 +9,14 @@ public class Problem2 {
     * 1,2를 중복이 제거 될 때 까지 반복됨.
     * */
 
-    
+    static int isDuplicates(String str){
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == str.charAt(i+1)) return i;
+        }
+        return 0;
+    }
+
+
     public static String solution(String cryptogram) {
         return removeDuplicates(cryptogram);
     }
