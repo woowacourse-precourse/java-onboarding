30252830edadd9676fb559e89f137140beb2a2a0
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 17:56:00 +0900

    feat: 문자열을 분리하여 박수를 세는 기능 추가

--- src/main/java/onboarding/problem3/Game369.java
+++ src/main/java/onboarding/problem3/Game369.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class Game369 {
 	public static final List<String> GAME_TARGET = Arrays.asList("3", "6", "9");
-	
+
+	private static int countClap(String number) {
+		int result = 0;
+
+		String[] splitNumber = number.split("");
+		for (String num : splitNumber) {
+			if (GAME_TARGET.contains(num)) {
+				result++;
+			}
+		}
+		return result;
+	}
+
 }
