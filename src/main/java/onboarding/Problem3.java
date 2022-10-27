b8efe5e87b58076283da8980bec1261ef740dd14
안광민 <creativeduck0518@gmail.com>
2022-10-27 15:12:06 +0900

    feat(Problem 3): 숫자가 주어지면 3,6,9개수만큼 손뼉 개수 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,21 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int makeClapCount(int number) {
+        if (number <= 0)
+            return 0;
+
+        int count = 0;
+        switch (number % 10) {
+            case 3:
+            case 6:
+            case 9:
+                ++count;
+                break;
+            default:
+                break;
+        }
+        return count + makeClapCount(number / 10);
+    }
 }
