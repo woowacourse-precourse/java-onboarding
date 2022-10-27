267b107973252d2059f3ac2b575013260102d1b9
안광민 <creativeduck0518@gmail.com>
2022-10-27 16:00:18 +0900

    feat(Problem 4): 문제 4 최종 구현

--- src/main/java/onboarding/Constants.java
+++ src/main/java/onboarding/Constants.java
@@ -5,4 +5,8 @@ public final class Constants {
     public static final int DRAW = 0;
     public static final int WIN_POBI = 1;
     public static final int WIN_CRONG = 2;
+
+    public static final int NOT_ALPHABET = 0;
+    public static final int LOWERCASE = 1;
+    public static final int UPPERCASE = 2;
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder stringBuilder = new StringBuilder(word);
+        int alphabetType;
+        char letter;
+        char convertedLetter;
+
+        for(int i=0; i<stringBuilder.length(); i++) {
+            letter = stringBuilder.charAt(i);
+            alphabetType = makeAlphabetType(letter);
+
+            if (alphabetType != Constants.NOT_ALPHABET) {
+                convertedLetter = convert(letter, alphabetType);
+                stringBuilder.setCharAt(i, convertedLetter);
+            }
+        }
+
+        answer = stringBuilder.toString();
         return answer;
     }
 
