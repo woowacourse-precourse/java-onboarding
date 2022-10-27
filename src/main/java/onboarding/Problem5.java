138b5f29a8c32c465ccd096c44aa67a6754567e2
Park shinyeong <syngh503@gmail.com>
2022-10-27 21:45:01 +0900

    Problem5/Feat/화폐 개수 구하는 기능 구현 화폐 개수를 구하는 기능을 구현하였습니다. Relate to:#5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] moneyList = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for(int coin : moneyList) {
+            int numberOfMoney= money / coin;
+            answer.add(numberOfMoney);
+            money = money % coin;
+            if(money <= 0) break;
+        }
         return answer;
     }
 }
