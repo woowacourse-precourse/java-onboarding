d58d4dfe5dfeb7d37419dfdb59aea2aadb4af995
jjongwa <troas96@naver.com>
2022-10-27 14:28:27 +0900

    문제5- 거스름돈 count 함수 작성 및 예제 확인 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+    static int change(int m, int div_m){
+        return m/div_m;
+    }
+
+    static List<Integer> withdrow(int money){
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
+        answer.add(change(money, 50000));
+        money %= 50000;
+        answer.add(change(money, 10000));
+        money %= 10000;
+        answer.add(change(money, 5000));
+        money %= 5000;
+        answer.add(change(money, 1000));
+        money %= 1000;
+        answer.add(change(money, 500));
+        money %= 500;
+        answer.add(change(money, 100));
+        money %= 100;
+        answer.add(change(money, 50));
+        money %= 50;
+        answer.add(change(money, 10));
+        money %= 10;
+        answer.add(change(money, 1));
+//        System.out.println(answer);
         return answer;
+    }
+
+    public static List<Integer> solution(int money) {
+        return withdrow(money);
     }
 }
