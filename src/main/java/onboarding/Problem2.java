9bd64594d9a613359f1455a816c979c9d4f192cd
jaehee329 <jaehee329@yonsei.ac.kr>
2022-10-27 10:56:06 +0900

    feat(Problem2): 기능 목록 정리 및 재귀함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,29 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = decrypto(cryptogram);
         return answer;
+    }
+
+    public static String decrypto(String string) {
+        String copy = string;
+        for (int i=0; i<string.length()-1; i++) {
+            if (isCharsDuplicatedInGivenIdx(string, i)) {
+                string = getCharsDeletedInGivenIdx(string, i);
+            }
+        }
+        if (string.equals(copy)) {
+            return string;
+        }
+        string = decrypto(string);
+        return string;
+    }
+
+    public static boolean isCharsDuplicatedInGivenIdx(String string, int idx) {
+        return true;
+    }
+
+    public static String getCharsDeletedInGivenIdx(String string, int idx) {
+        return string;
     }
 }
