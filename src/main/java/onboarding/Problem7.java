2cf51506f900809bee2eaa77d79c2749a11e13d0
aksmf1442 <aksmf1442@naver.com>
2022-10-27 16:48:37 +0900

    feat: Problem7 서로 친구인 유저들간 매핑 및 결과에서 제외되어야 하는 유저를 등록하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,41 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
+    static Map<String, Integer> excludedUserMap;
+    static Map<String, List<String>> friendMap;
+    static Map<String, Integer> acquaintanceScoreMap;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        excludedUserMap = new HashMap<>();
+        friendMap = new HashMap<>();
+        acquaintanceScoreMap = new HashMap<>();
+        initFriendMap(friends);
+        initExcludeUserMap(user);
+        return null;
     }
+
+    private static void initExcludeUserMap(String user) {
+        excludedUserMap.put(user, 1);
+        for (String friend : friendMap.get(user)) {
+            excludedUserMap.put(friend, 1);
+        }
+    }
+
+    private static void initFriendMap(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+            friendMap.computeIfAbsent(user1, k -> new ArrayList<>());
+            friendMap.computeIfAbsent(user2, k -> new ArrayList<>());
+            friendMap.get(user1).add(user2);
+            friendMap.get(user2).add(user1);
+
+        }
+    }
+
 }
