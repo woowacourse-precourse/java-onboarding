4ce0f8c5f22d7af2dae3bae0628e520a11bd4a55
JunH <tjdtls690@gmail.com>
2022-10-27 19:11:34 +0900

    :art: refactor(PROBLEM4) : 도메인 객체 이용해서 결과 값 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,61 +1,9 @@
 package onboarding;
 
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import onboarding.problem4.domain.Sentence;
 
 public class Problem4 {
-    private static final List<Character> LOWERCASE_LETTERS = lowercaseLettersInit();
-    private static final List<Character> UPPERCASE_LETTERS = uppercaseLettersInit();
-    
-    private static List<Character> lowercaseLettersInit() {
-        return IntStream.rangeClosed(0, 25)
-                .mapToObj(number -> (char) (122 - number))
-                .collect(Collectors.toList());
-    }
-    
-    private static List<Character> uppercaseLettersInit() {
-        return IntStream.rangeClosed(0, 25)
-                .mapToObj(number -> (char) (90 - number))
-                .collect(Collectors.toList());
-    }
-    
     public static String solution(String word) {
-        final char[] chars = word.toCharArray();
-        
-        for (int index = 0; index < chars.length; index++) {
-            convertWord(chars, index);
-        }
-        
-        return String.valueOf(chars);
-    }
-    
-    private static void convertWord(final char[] chars, final int index) {
-        if (isAlphabet(chars[index])) {
-            chars[index] = convertAlphabet(chars[index]);
-        }
-    }
-    
-    private static char convertAlphabet(final char aChar) {
-        if (Character.isUpperCase(aChar)) {
-            return toConvertUpperCase(aChar);
-        }
-        
-        return toConvertLowerCase(aChar);
-    }
-    
-    private static boolean isAlphabet(final char aChar) {
-        final Matcher matcher = Pattern.compile("[a-z]|[A-Z]").matcher(String.valueOf(aChar));
-        return matcher.matches();
-    }
-    
-    private static Character toConvertLowerCase(Character character) {
-        return LOWERCASE_LETTERS.get(character - 'a');
-    }
-    
-    private static Character toConvertUpperCase(Character character) {
-        return UPPERCASE_LETTERS.get(character - 'A');
+        return new Sentence(word).convert();
     }
 }
---
+++ src/test/java/onboarding/problem4/domain/AlphabetConvertorTest.java
@@ -0,0 +1,18 @@
+package onboarding.problem4.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlphabetConvertorTest {
+    @Test
+    @DisplayName("알파벳 변환")
+    void convertAlphabet() {
+        assertAll(
+                () -> assertThat(AlphabetConvertor.convertAlphabet('a')).isEqualTo('z'),
+                () -> assertThat(AlphabetConvertor.convertAlphabet('A')).isEqualTo('Z')
+        );
+    }
+}
