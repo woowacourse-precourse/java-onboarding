8ac3e6f0aea1c602bb295676f29d49a577941d67
Hyeonjae-K <href.hjk@gmail.com>
2022-10-27 21:24:17 +0900

    feat: 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/* 기능 목록
+ * 1. 중복된 문자들을 삭제하는 기능
+ * 2. 중복되지 않은 값들로 업데이트하는 기능
+ * 3. 중복된 값이 모두 없어질 때 까지 반복하는 기능
+ * */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
