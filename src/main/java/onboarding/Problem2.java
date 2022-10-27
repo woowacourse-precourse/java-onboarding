59869f9a7e62ddd9bb854e745f87c0776214530c
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-27 17:49:57 +0900

    init: Problem2 기능 요구사항 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/**
+ * [ ] "browoanoommnaon"
+ * [ ] "browoannaon"
+ * [ ] "browoaaon"
+ * [ ] "browoon"
+ * [ ] "brown"
+ * [ ] 임의의 문자열 cryptogram이 매개변수로 주어질 때, 연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 메서드를 완성하라.
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
