e62445dae2af4bc22a057f3d98442e76a01c4f70
whale0928 <rlagusrl928@gmail.com>
2022-10-27 23:42:13 +0900

    4번 문제 해결중

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Problem1 {
 
@@ -20,52 +22,47 @@ class Problem1 {
 
           , 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0,
         */
-        int pobiMaxNum=0;
-        int crongMaxNum=0;
 
-        if(!pobi.isEmpty()) pobiMaxNum = temp(pobi);
-        else                crongMaxNum =temp(crong);
-
-        System.out.println(pobiMaxNum);
-        System.out.println(crongMaxNum);
-        return 0;
-    }
-
-    /**포비 혹은 크롱의 리스트를 받아 매직 후 최대 값을 반환하는 메서드
-     *
-     * @param list
-     * @return
-     */
-    public static int temp(List<Integer> list){
-        String left = String.valueOf(list.get(0));
-        String right = String.valueOf(list.get(1));
-        int lMax = 1;
-        int rMax = 1;
-
-        for (int i = 0; i <left.length(); i++) {
-            lMax += magic( Character.getNumericValue(left.charAt(i)),lMax);
+        int pobisNumber;
+        int crongsNumber;
+        try {
+            pobisNumber = findMaxValue(pobi);
+            crongsNumber = findMaxValue(crong);
+            System.out.println("pobisNumber = " + pobisNumber);
+            System.out.println("crongsNumber = " + crongsNumber);
+        } catch (Exception e) {
+            return -1;
         }
-
-        for (int i = 0; i <right.length(); i++) {
-            rMax += magic(Character.getNumericValue(right.charAt(i)),rMax);
-        }
-        return Math.max(lMax, rMax);
+        return pobisNumber > crongsNumber?1:pobisNumber < crongsNumber?2:0;
     }
 
-    /**두 방식중 큰 수를 구하기
-     *
-     * @param n
-     * @param i
-     * @return
-     */
-    public static int magic(int n , int i){
-        return Math.max((n + i), (n * i));
+    public static int findMaxValue(List<Integer>list)throws Exception{
+        List<Integer> left =Arrays.stream(String.valueOf(list.get(0)).split("")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> right =Arrays.stream(String.valueOf(list.get(1)).split("")).map(Integer::parseInt).collect(Collectors.toList());
+
+//        원래라면 재귀를 쓰겟지만 문제에서 400 페이지 미만이라는 조건을 주었기 때문에 각각의 경우를 만든다.
+//        재귀를 이용 할 수 있을 것 같다!!
+//        int leftMax = recursive(list,"",0);
+//        int rightMax = recursive(right,"",0);
+//        포기 게속 하면 찾을거 같은데 더 이상 시간을 투자하게 되면 힘들듯.
+
+        int leftMax = 1;
+        int rightMax = 1;
+
+        for (Integer i : left) leftMax = Math.max((leftMax * i), (leftMax + i));
+        for (Integer i : right) rightMax = Math.max((rightMax * i), (rightMax + i));
+
+        return Math.max(leftMax,rightMax);
     }
 
-    public static void main(String[] args) {
-        List<Integer> pobi = List.of(97, 98);
-        List<Integer> crong = List.of(197, 198);
-        int i = solution(pobi,crong);
-    }
+    public static int recursive(List<Integer> list,String simpol,int result){
 
+        result = Integer.parseInt( simpol+list.get(0));
+        list.remove(0);
+        //최종 값까지 추출한 다음 list가 비었을 경우 누적된 결과값을 가져간다.
+        if(list.isEmpty()) return result;
+
+        return Math.max(recursive(list, "+",result), recursive(list, "*",result));
+    }
 }
+
