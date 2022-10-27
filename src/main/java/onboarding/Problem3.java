126dec83dca7ffa968b7bca4276c5a6ac0a68e9c
somsom13 <somin1113@naver.com>
2022-10-27 19:28:04 +0900

    feat(문제3): 자릿수별 3,6,9 개수 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+    public static String numberString;
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int count369InDigitNumber(int digitNumber) {
+        if(digitNumber>=9) {
+            return 3;
+        }
+        if(digitNumber>=6) {
+            return 2;
+        }
+        if(digitNumber>=3){
+            return 1;
+        }
+        return 0;
     }
 }
