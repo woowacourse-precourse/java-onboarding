b4c6213309c69c43a94300d04c4f250a8f03c021
jjmin9797 <jjmin9797@naver.com>
2022-10-27 15:44:12 +0900

    [Problem3][기능요구사항]문제 해결 위한 기능 정의

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,25 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * [숫자 하나 당 박수 구하기]
+     * - 입력으로 들어온 숫자에 몇 번의 박수를 처야하는지 반환
+     * @param number
+     * @return clap
+     */
+    public static int getClapPerNumber(int number){
+        return 1;
+    }
+
+    /**
+     * [모든 박수 구하기]
+     * - 1부터 입력으로 들어온 number까지 총 몇번의 박수를 처야하는지 반환
+     * @param number
+     * @return Applause
+     */
+    public static int getApplause(int number){
+        return 1;
+    }
+
 }
