7c51ca536664f70d09423c0501de933058a80676
JDK <ehdrjf2062@gmail.com>
2022-10-27 18:37:44 +0900

    feat: 문제2 cryptogram

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+		
+		while (true) {
+			boolean flag = true;
+			for (int i = 0; i < cryptogram.length()-1; i++) {
+				if(cryptogram.charAt(i) == cryptogram.charAt(i+1)){
+					String beforeStr = cryptogram.substring(0, i);
+					String afterStr = cryptogram.substring(i + 2);
+					
+					cryptogram = beforeStr + afterStr;
+					i--;
+					
+					flag = false;
+				}
+			}
+			if(flag){
+				break;
+			}
+		}
+		
+		answer = cryptogram;
+		
+		return answer;
+	}
 }
