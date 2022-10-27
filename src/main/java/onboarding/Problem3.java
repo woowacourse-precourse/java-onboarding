37d5bf48e239cab47fa0610ced9d528d4324d776
Limjongho <whdgh9873@icloud.com>
2022-10-27 15:45:42 +0900

    feat(Problem3): 제시된 숫자 이하의 모든 수를 포함하는 리스트를 반환하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static List<Integer> getIntegersUnderNumber(int number) {
+        List<Integer> integersUnderNumber = new ArrayList<>();
+        for (int i = 1; i <= number; i++) {
+            integersUnderNumber.add(i);
+        }
+        return integersUnderNumber;
     }
 }
