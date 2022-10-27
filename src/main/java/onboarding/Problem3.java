51d738bf4146d3ac7138a2918314492d45f64492
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-27 23:04:30 +0900

    문제 3 버그, 기능 목록 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,18 +1,21 @@
 package onboarding;
 
 public class Problem3 {
-    public static boolean contain3or6or9(int number) {
+    public static int contain3or6or9(int number) {
+        int result = 0;
         String stringNumber = String.valueOf(number);
         for (int i = 0; i < stringNumber.length(); i++) {
-            int digit = stringNumber.charAt(i);
-            if (digit == 3 || digit == 6 || digit == 9) return true;
+            int digit = stringNumber.charAt(i) - '0';
+
+            if (digit == 3 || digit == 6 || digit == 9) result++;
         }
-        return false;
+        return result;
     }
+
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            if (contain3or6or9(i)) answer++;
+            answer += contain3or6or9(i);
         }
         return answer;
     }
