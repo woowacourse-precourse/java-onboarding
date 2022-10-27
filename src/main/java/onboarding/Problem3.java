b9ffb5d89710a6df26fb05890a6dfa31a5570ee9
hong-sile <gurwns9325@naver.com>
2022-10-27 14:46:28 +0900

    feat : 입력값까지의 게임결과를 계산하는 메소드 추가, solution 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,9 @@ import onboarding.validatechecker.Problem3ValidateChecker;
 
 public class Problem3 {
 	private static int[] gameResult = new int[10001];
+	public static final int GAME_NUMBER3=3;
+	public static final int GAME_NUMBER6=6;
+	public static final int GAME_NUMBER9=9;
 
 	public static int solution(int number) {
 		Problem3ValidateChecker.isNumberValidate(number);
@@ -21,7 +24,8 @@ public class Problem3 {
 		int count369=0;
 		while(number!=0){
 			int digit=number%10;
-			if(digit == 3 || digit == 6 || digit == 9)
+			if(digit == GAME_NUMBER3 || digit == GAME_NUMBER6
+				|| digit == GAME_NUMBER9)
 				count369+=1;
 			number=number/10;
 		}
