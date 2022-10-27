6b52bf0c65b294cd498b2d3a4eea2feb2412c940
ouohoon <begreat@kakao.com>
2022-10-28 06:22:10 +0900

    2번 문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decode(cryptogram);
+    }
+
+    // deque의 last에서 중복 문자를 제거해서 암호문을 해독한다.
+    private static String decode(String cryptogram) {
+        Deque<Character> deque = new ArrayDeque<>();
+        deque.addLast(cryptogram.charAt(0));
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            Character last = deque.peekLast();
+            Character current = cryptogram.charAt(i);
+            if (current.equals(last)) {
+                deque.removeLast();
+                continue;
+            }
+            deque.addLast(current);
+        }
+
+        return deque.stream().map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
