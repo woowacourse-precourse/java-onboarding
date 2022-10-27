20df2a97b9669c4f1f2909ce4c3b58b01e301d25
ygreenb <nhj7911@gmail.com>
2022-10-28 05:27:14 +0900

    feat: Problem3.1 3,6,9의 개수만큼 손뼉을 치는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int ClapBy369(int number){
+        int cnt = 0;
+        String str = String.valueOf(number);
+        for(char c : str.toCharArray()){
+            if((c == '3') || (c == '6') || (c == '9')){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
