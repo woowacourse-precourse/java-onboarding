6bd522e8dc5e4209ea928e741e08f57197e02ada
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-27 22:54:22 +0900

    문제 3 : 기능 1 - 어떤 숫자가 매개변수로 주어질 때 그 숫자가 3, 6, 9를 포함하는지 확인하는 메소드.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,14 @@
 package onboarding;
 
 public class Problem3 {
+    public static boolean contain3or6or9(int number) {
+        String stringNumber = String.valueOf(number);
+        for (int i = 0; i < stringNumber.length(); i++) {
+            int digit = stringNumber.charAt(i);
+            if (digit == 3 || digit == 6 || digit == 9) return true;
+        }
+        return false;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
