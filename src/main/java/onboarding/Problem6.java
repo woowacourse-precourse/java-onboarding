6612ab18573fc9f654e90e27207ad1c998d68bf5
kdkdhoho <hkim4410@naver.com>
2022-10-27 13:33:03 +0900

    feat: Problem6 Crew 클래스 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,53 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
-public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+class Crew {
+    private String email;
+    private String nickname;
+    private boolean isDuplicate;
+    private Set<String> tokens = new HashSet(); // 개인이 가지고 있는 tokens
+
+    public Crew(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+        isDuplicate = false;
+        makeTokens(this.nickname);
+    }
+
+    private void makeTokens(String nickname) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String token = nickname.substring(i, i+2);
+            this.tokens.add(token);
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public boolean getIsDuplicate() {
+        return isDuplicate;
+    }
+
+    public Set<String> getTokens() {
+        return tokens;
+    }
+
+    public void setIsDuplicateTrue() {
+        this.isDuplicate = true;
     }
 }
+
+public class Problem6 {
+
+    public static List<String> solution(List<List<String>> forms) {
+
+    }
+}
+
+
