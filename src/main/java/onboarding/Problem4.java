da08c8125535216d1c167f4f128d9b6ef1882f79
Hongsugyeong <olivia258@naver.com>
2022-10-27 20:25:31 +0900

    [problem4] 기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+    /**
+     * 소문자 ,대문자 문자 전환 함수
+     * 문자열 전환 함수
+     */
     public static String solution(String word) {
         String answer = "";
         return answer;
