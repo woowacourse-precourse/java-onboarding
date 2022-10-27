12fc37612d54cf5b70e1c2cfbe3a395d155848bc
junho lee <junho1273@gmail.com>
2022-10-27 22:45:27 +0900

    problem2 기능목록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+//기능 목록
+// 1. 인접한 중복 문자 찾고 소문자 알파벳이 아닌 특수문자로 치환.
+// 2. 치환되지 않은 문자열 합치기
+// 3. 1번과 2번을 치환되지 않을때까지 반복
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        return answer;
+        return  answer;
     }
 }
