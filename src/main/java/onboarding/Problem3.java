44e65610ee1699e2f1bbf61237ecc1c4abc62e8f
Lee Sang Bin <rhkd2612@naver.com>
2022-10-27 18:13:59 +0900

    docs: problem3 methods

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/*
+ * # 기능 정리
+ * [X] 목표한 숫자까지 순회 메서드
+ * [X] 각 숫자 별 결과값 확인 메서드
+ * [X] solution 작성
+ * [X] 테스트 확인
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
