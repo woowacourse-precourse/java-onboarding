f9b47c3a0b095f7b1f36511b60f7b9e04060b696
ParkRootSeok <g_e_un@naver.com>
2022-10-27 22:21:53 +0900

    Problem3 - 필요한 Method 정의

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int countClap(int number) {
+
+    }
+
+    public static int parseNum(int number) {
+
+    }
+
+    public static boolean compareNumWith369(int digit) {
+
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
     }
+
+
 }
