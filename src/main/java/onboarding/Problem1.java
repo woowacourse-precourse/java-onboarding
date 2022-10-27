6b7aa050dfe9947841ba320a93a06592e418b6f6
Hyeonjae-K <href.hjk@gmail.com>
2022-10-27 17:57:12 +0900

    feat: 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,16 @@ package onboarding;
 
 import java.util.List;
 
+/* 기능 목록
+ * 1. 예외 처리 기능
+ *   - 페이지 범위는 1 ~ 400
+ *   - 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수
+ *   - 왼쪽과 오른쪽 페이지는 연속적 ([99, 102] X)
+ *   - 시작 면이나 마지막 면은 펼치지 않음
+ * 2. 페이지의 각 자리 합과 곱을 계산하여 최댓값을 반환하는 기능
+ * 3. 페이지를 비교하여 결과를 반환하는 기능
+ * */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
