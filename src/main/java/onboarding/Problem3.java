f7bdcdc76f2f0617ad02a586bea5671c3351fdff
minje <alswp0910@naver.com>
2022-10-27 21:23:00 +0900

    문제3_기능1_구현완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+
+    //기능 1.
     public static int solution(int number) {
         int answer = 0;
+        int num = number;
+        int [] divNum = {1000, 100, 10, 1};
+        for (int i = 0; i <= num; i++) {
+            int su = i;
+            for (int j = 0; j < 4; j++) {
+                int mok = su/divNum[j];
+                su %= divNum[j];
+                if(mok != 0 && mok%3 == 0){
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
