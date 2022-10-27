2d8e6254c555f7ef4e2ae4937e98d8fb96eb3df8
juno-junho <ssmm0205@naver.com>
2022-10-27 23:43:52 +0900

    refactor(Problem6): change variable name
    1. 불필요한 index 변수 삭제
    2. nicknameList가 아닌 자료형이 Set이므로 nicknameSet으로 변경
    3. 변수 substring의 명확성을 높히기 위해 consecutiveLetter로 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> result = new ArrayList<>();
+
+        Map<String, String> crew = new HashMap<>();
+
+        for (List<String> form : forms) {
+            for (int i = 0; i < form.get(1).length() - 1; i++) {
+                crew.put(form.get(1), form.get(0));
+            }
+        }
+        Set<String> nicknameSet = crew.keySet();
+        nicknameSet.forEach(i -> {
+            for (int j = 0; j < i.length() - 1; j++) {
+                String consecutiveLetter = i.substring(j, j + 2);
+                nicknameSet.forEach(k -> {
+                    if (!k.equals(i) && k.contains(consecutiveLetter)) result.add(crew.get(k));});
+            }
+        });
+        return result.stream().distinct().sorted().collect(Collectors.toList());
     }
 }
