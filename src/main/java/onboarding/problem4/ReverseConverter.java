9d0363c9fe15fd6f42f67720166353e909bb39e0
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 20:16:47 +0900

    feat: 거꾸로 된 알파벳 리스트 상수 추가

--- src/main/java/onboarding/problem4/ReverseConverter.java
+++ src/main/java/onboarding/problem4/ReverseConverter.java
@@ -1,5 +1,6 @@
 package onboarding.problem4;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,4 +9,8 @@ public class ReverseConverter {
 	public static final List<Character> dict = IntStream.rangeClosed('A', 'Z')
 		.mapToObj(c -> (char)c)
 		.collect(Collectors.toList());
+	public static final List<Character> reverseDict = dict.stream()
+		.sorted(Comparator.reverseOrder())
+		.collect(Collectors.toList());
+	
 }
