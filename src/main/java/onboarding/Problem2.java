ac8d7f26c88cb811f159ba88eaef0faf07225b88
Kimhyeyun <cqqwer@gmail.com>
2022-10-27 17:23:26 +0900

    [Problem 2] MAKE DELETE DUPLICATE CHARACTERS FUNCTION

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,26 +2,39 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
 
-        boolean isDuplicated = false;
         while (true) {
+            boolean isDuplicated = false;
             for (int i = 0; i < cryptogram.length(); i++) {
                 int duplicateCount = checkDuplicate(cryptogram, i);
+                if (duplicateCount == -1 || duplicateCount == 0) {
+                    continue;
+                } else {
+                    cryptogram = deleteDuplicate(cryptogram, i, duplicateCount);
+                    isDuplicated = true;
+                    break;
+                }
             }
             if(!isDuplicated) break;
         }
 
-        return answer;
+        return cryptogram;
+    }
+
+    private static String deleteDuplicate(String cryptogram, int idx, int count) {
+        String c = cryptogram.substring(0, idx);
+        c += cryptogram.substring(idx + count + 1, cryptogram.length());
+
+        return c;
     }
 
     private static int checkDuplicate(String cryptogram, int idx) {
         char c = cryptogram.charAt(idx);
-        int cnt = 1;
+        int cnt = 0;
         for (int i = idx + 1; i < cryptogram.length(); i++) {
             if(c == cryptogram.charAt(i)) cnt += 1;
             else {
-                if(i == idx) cnt = -1;
+                if(i == idx + 1) cnt = -1;
                 break;
             }
         }
