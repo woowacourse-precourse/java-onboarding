0840d9e56582b5e068529da0a7607ebe12a0f2b4
cpot5620 <=cpot5620@naver.com>
2022-10-27 21:09:25 +0900

    Make Crpytogram class and method checking dupcliated string
    - isDuplicate() returns whether the string has duplicated word to client.
    - checkDuplicate() can be called in class. This change 'duplicated' variable.

---
+++ src/main/java/onboarding/Cryptogram.java
@@ -0,0 +1,27 @@
+package onboarding;
+
+public class Cryptogram {
+    private String value;
+    private boolean duplicated;
+
+    public Cryptogram(String value) {
+        this.value = value;
+        duplicated = true;
+
+        checkDuplicate();
+    }
+
+    public boolean isDuplicate() {
+        return duplicated;
+    }
+
+    private void checkDuplicate() {
+        int i = 0;
+
+        while (i < value.length() - 1) {
+            i = (value.charAt(i) == value.charAt(i + 1)) ? Integer.MAX_VALUE : i + 1;
+        }
+
+        duplicated = Integer.MAX_VALUE == i ? true : false;
+    }
+}
