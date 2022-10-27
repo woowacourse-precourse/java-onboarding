a61dcefc566060f91e32a1724692efd876db72df
kariskan <zkfzpf56@naver.com>
2022-10-27 21:20:51 +0900

    refactor: DecodeUtil 클래스를 이용한 solution 함수 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,28 +1,10 @@
 package onboarding;
 
+import onboarding.support.problem2.DecodeUtil;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        return getDecodedString(cryptogram);
-    }
-
-    public static String getDecodedString(String cryptogram) {
-        StringBuilder sb = new StringBuilder(cryptogram);
-
-        boolean isPresentDuplication = false;
-
-        for (int i = 0; i < sb.length(); i++) {
-            int duplicatedLastIndex = i;
-            while (duplicatedLastIndex < sb.length() && sb.charAt(duplicatedLastIndex) == sb.charAt(i)) {
-                duplicatedLastIndex++;
-            }
-
-            if (duplicatedLastIndex - i > 1) {
-                sb.delete(i, duplicatedLastIndex);
-                isPresentDuplication = true;
-            }
-        }
-
-        if (isPresentDuplication) return getDecodedString(sb.toString());
-        return sb.toString();
+        DecodeUtil decodeUtil = new DecodeUtil(cryptogram);
+        return decodeUtil.getDecodedString();
     }
 }
