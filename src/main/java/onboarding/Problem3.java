7065c429aa4c491cb910b25543b01ff6e7580eba
juhwang <juhong4930@naver.com>
2022-10-27 09:46:14 +0900

    feat(Problem3):checking 3,6,9 in game369Board

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
@@ -11,4 +13,15 @@ public class Problem3 {
     public static int numberOnGame369Board(int number){
         return game369Board()[number];
     }
+    public static String[] numToString(int num){
+        return String.valueOf(num).split("(?<=.)");
+    }
+    public static boolean validCheck369(int num){
+        CharSequence numToString = (CharSequence) List.of(numToString(num));
+        if("3".contains(numToString) || "6".contains(numToString) || "9".contains(numToString)){
+            return true;
+        }
+        return false;
+    }
+
 }
