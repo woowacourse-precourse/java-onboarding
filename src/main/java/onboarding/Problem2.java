25a143a667262e251677838d920670dd1e6a80b9
Hongsugyeong <olivia258@naver.com>
2022-10-27 19:58:03 +0900

    [problem2] 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem2 {
+    /**
+     * 스택 이용해서 문자 제거하는 함수
+     * 스택을 문자열로 반환하는 함수
+     */
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
