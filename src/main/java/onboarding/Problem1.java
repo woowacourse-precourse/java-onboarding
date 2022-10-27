ef1d92d1539509a4dc64c91240a27fc174dcdd35
seongha <11pi885@gmail.com>
2022-10-28 00:52:35 +0900

    기능목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,21 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    //한 페이지 번호의 각 자리 숫자를 모두 더하는 함수
+
+    //한 페이지 번호의 각 자리 숫자를 모두 곱하는 함수
+
+    //한명의 최대 점수를 구하는 함수
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //예외처리 (페이지가 연속하지 않는경우)
+
+        //포비와 크롱의 점수를 비교하여 answer을 결정
+
         return answer;
     }
 }
