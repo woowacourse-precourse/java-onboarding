36eba4fef7b737deaf63c48dfebfa2f649541251
juhwang <juhong4930@naver.com>
2022-10-27 09:25:19 +0900

    feat(Problem3):finding number on game369Board

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,4 +8,7 @@ public class Problem3 {
     public static int[] game369Board(){
         return new int[10001];
     }
+    public static int numberOnGame369Board(int number){
+        return game369Board()[number];
+    }
 }
