65d8694ac64d5b8525044a447ac8250d89a09645
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-10-27 15:18:47 +0900

    포비, 크롱 각 페이지의 더하기, 곱하기 계산 후 최대값 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,30 @@ class Problem1 {
         if (pobi.get(LEFT_PAGE) - pobi.get(RIGHT_PAGE) != -1 || crong.get(LEFT_PAGE) - crong.get(RIGHT_PAGE) != -1) {
             return -1;
         }
+
+        int pobiHighValue = calc(pobi.get(LEFT_PAGE).toString(), pobi.get(RIGHT_PAGE).toString()),
+                crongHighValue = calc(crong.get(LEFT_PAGE).toString(), crong.get(RIGHT_PAGE).toString());
+
         return 0;
     }
+
+    private static int calc(String leftPage, String rightPage) {
+        int leftPlus = 0, rightPlus = 0, leftMultiple = 1, rightMultiple = 1;
+
+        for (int i = 0; i < leftPage.length(); i++) {
+            // left page plus and multiplication
+            leftPlus += leftPage.charAt(i) - '0';
+            leftMultiple *= leftPage.charAt(i) - '0';
+
+            // right page plus and multiplication
+            rightPlus += rightPage.charAt(i) - '0';
+            rightMultiple *= rightPage.charAt(i) - '0';
+        }
+
+        int plusValue = Math.max(leftPlus, rightPlus),
+                multipleValue = Math.max(leftMultiple, rightMultiple);
+
+        return Math.max(plusValue, multipleValue);
+    }
+
 }
