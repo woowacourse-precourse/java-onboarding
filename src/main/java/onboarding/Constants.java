ed635a7550ba9aded5bfe32ba68105c30af37907
안광민 <creativeduck0518@gmail.com>
2022-10-27 19:30:53 +0900

    feat(Problem 5): 금액을 전달하면 각 동전 개수 배열을 만드는 기능 구현 및 문제 5 최종 구현

--- src/main/java/onboarding/Constants.java
+++ src/main/java/onboarding/Constants.java
@@ -9,4 +9,6 @@ public final class Constants {
     public static final int NOT_ALPHABET = 0;
     public static final int LOWERCASE = 1;
     public static final int UPPERCASE = 2;
+
+    public static final int START_MONEY = 50_000;
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new LinkedList<>();
+        int currency = Constants.START_MONEY;
+        int amount = 0;
+        boolean isHalf = true;
+
+        while(currency >= 10) {
+            amount = money / currency;
+            answer.add(amount);
+
+            if (isHalf) {
+                currency /= 5;
+            }
+            else {
+                currency /= 2;
+            }
+
+            money %= currency;
+            isHalf = !isHalf;
+        }
+
+        answer.add(money);
+
         return answer;
     }
 }
