d9e69921809b4e111efe3441fed9eeb48ef5f534
kjb95 <topmedia44@gmail.com>
2022-10-27 20:45:22 +0900

    feat: Problem2 - 연속하는 중복 문자열이 존재한다면, 중복 문자에 해당하는 인덱스를 반환하는 메소드 생성하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,19 @@ package onboarding;
 
 public class Problem2 {
 
+    private static final int NONE_DUPLICATE = -1;
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static int findDuplicateStrIndex(String str) {
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                return i - 1;
+            }
+        }
+        return NONE_DUPLICATE;
     }
 }
