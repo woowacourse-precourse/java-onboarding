4993357677490e168eb3d9bfe13e44005714c552
choyoungwoo <choyoungwoo@naver.com>
2022-10-27 14:31:25 +0900

    Implement Problem3 Solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,34 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return count_clab(number);
+    }
+
+    static int count_clab(int num)
+    {
+        int i = 0;
+        int count = 0;
+        while(true)
+        {
+            if(i > num)
+                break;
+            count = count + count_three_six_nine(i);
+            i ++;
+        }
+        return count;
+    }
+    static int count_three_six_nine(int i)
+    {
+        int count = 0;
+        while(true)
+        {
+            if(i == 0)
+                break;
+            int tmp_digit = i % 10;
+            if(tmp_digit == 3 || tmp_digit == 6 || tmp_digit == 9)
+                count ++;
+            i = i / 10;
+        }
+        return count;
     }
 }
