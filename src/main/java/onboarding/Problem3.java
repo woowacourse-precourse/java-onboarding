588057fc94b46a60a0af9c39d6c47ebb78c63a0f
yujaemin <woawoa0513@gmail.com>
2022-10-27 19:28:47 +0900

    feat: [문제 3] 특정 숫자의 박수(3,6,9) 횟수 구하는 함수 추가
    숫자를 입력받아, 369 게임에서 해당 숫자의 박수 횟수를 반환하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        System.out.println(count369(313));
+
         return answer;
+    }
+
+    public static int count369(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            int mod = num % 10;
+            if (mod == 3 || mod == 6 || mod == 9) count++;
+            num /= 10;
+        }
+
+        return count;
     }
 }
