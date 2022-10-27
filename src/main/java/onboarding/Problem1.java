11160c253d9cd933e1efb879bfa0a3f5b7dcd407
situodys <situodys@gmail.com>
2022-10-27 17:08:21 +0900

    feat(문제1): 페이지 번호 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,12 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        Pages pobiPages = new Pages(pobi);
-        Pages crongPages = new Pages(crong);
+        try {
+            Pages pobiPages = new Pages(pobi);
+            Pages crongPages = new Pages(crong);
+        } catch (IllegalArgumentException e) {
+            return -1;
+        }
 
         return answer;
     }
@@ -19,9 +23,39 @@ class Problem1 {
         private int leftNumber;
         private int rightNumber;
 
+        private void validate(List<Integer> indexes) throws IllegalArgumentException {
+            hasNullValue(indexes);
+            int leftNo = indexes.get(LEFT_INDEX);
+            int rightNo = indexes.get(RIGHT_INDEX);
+
+            isContinuousNumbers(leftNo, rightNo);
+            isInProperRange(leftNo, rightNo);
+        }
+
+        private void hasNullValue(List<Integer> indexes) {
+            for (Integer idx : indexes) {
+                if(idx == null){
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+
+        private void isContinuousNumbers(int leftNumber, int rightNumber) {
+            if (rightNumber - leftNumber != 1)
+                throw new IllegalArgumentException();
+        }
+
+        private void isInProperRange(int leftNumber, int rightNumber) {
+            if (1 < leftNumber && leftNumber < rightNumber && rightNumber < 400) {
+                return;
+            }
+            throw new IllegalArgumentException();
+        }
+
         public Pages(List<Integer> indexes) {
-            this.leftNumber=indexes.get(LEFT_INDEX);
-            this.rightNumber=indexes.get(RIGHT_INDEX);
+            validate(indexes);
+            this.leftNumber = indexes.get(LEFT_INDEX);
+            this.rightNumber = indexes.get(RIGHT_INDEX);
         }
 
 
