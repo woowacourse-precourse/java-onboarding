fc05eb2152cde727dd9b2c961f76ab52d228c9e2
csj <koreaxman@naver.com>
2022-10-27 16:47:54 +0900

    feat: Divide alphabets by uppercase and lowercase

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return replaceAlphabet(word);
+    }
+
+    public static String replaceAlphabet(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if ((word.charAt(i) >= 97) && (word.charAt(i) <= 122)) {
+                word = replaceSmallAlphabetReverse(word, i);
+            }
+            if ((word.charAt(i) >= 65) && (word.charAt(i) <= 90)) {
+                word = replaceBigAlphabetReverse(word, i);
+            }
+        }
+
+        return word;
+    }
+
+    private static String replaceSmallAlphabetReverse(String word, int i) {
+        return "";
+    }
+
+
+    private static String replaceBigAlphabetReverse(String word, int i) {
+        return "";
     }
 }
