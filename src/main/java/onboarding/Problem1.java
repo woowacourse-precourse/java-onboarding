a6301a8cf59c78bf879f0e52a5f5fdca86e5f862
Your Name <dbsdbxkrzz@naver.com>
2022-10-27 20:34:29 +0900

    feat: 유효성 검사 메서드를 구현합니다.
    isInvalid 메서드의 페이지 번호가 범위(1 ~ 400)안에 있는지 검사하는 isNotInRange메서드를 구현합니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,17 +6,23 @@ class Problem1 {
 
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int answer = Integer.MAX_VALUE;
+		if (isInvalid(pobi, crong)) {
+			return -1;
+		}
 		return answer;
 	}
 
 	public static boolean isInvalid(List<Integer> pobi, List<Integer> crong) {
-
+		if (isNotInRange(pobi) || isNotInRange(crong)) {
+			return true;
+		}
 		return false;
 	}
 
 	public static boolean isNotInRange(List<Integer> pages) {
-
-		return false;
+		int leftPageNum = pages.get(0);
+		int rightPageNum = pages.get(1);
+		return leftPageNum < 1 || leftPageNum > 400 || rightPageNum < 1 || rightPageNum > 400;
 	}
 
 	public static boolean isPageNumNotEven(int page) {
