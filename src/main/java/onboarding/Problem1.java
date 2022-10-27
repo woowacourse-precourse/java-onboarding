47eefaa95b429752fa5441e2a96e844a5737444e
TaeMin <xoals3094@naver.com>
2022-10-27 16:25:40 +0900

    page num TO score method / add PROBLEM1.md

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,19 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public int make_score(int page_num){
+        int sum = 0;
+        int mul = 1;
+        for(; page_num > 0; page_num /= 10){
+            int num = page_num % 10;
+            sum += num;
+            mul *= num;
+        }
+        if (sum > mul){
+            return sum;
+        }
+        else{
+            return mul;
+        }
+    }
 }
