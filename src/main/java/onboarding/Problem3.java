c6c988e75201bc2f260b3fc455c29e9f73d493b9
rvlwldev <rvlwlasd77@gmail.com>
2022-10-27 18:12:33 +0900

    problem3 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += getClaps(i);
+        }
+
         return answer;
     }
+
+    private static int getClaps(int x) {
+        String str = String.valueOf(x);
+        String fix = str.replace("3", "")
+                .replace("6", "")
+                .replace("9", "");
+
+        return str.length() - fix.length();
+    }
+
 }
