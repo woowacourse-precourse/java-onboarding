8f2da6433f19f6f9835b9834192aaad902c2a2a8
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-10-27 16:07:46 +0900

    feat : [problem2] removDuplication()메소드 작성 좋은 아이디어가 떠오르지 않아 부르트 포스로 모든 문자열을 검사하였고, 문자열이 변경될 때 마다 다시 처음부터 검사하도록 설계하였다. 지금 이 방법보다 더 깔끔하게 정리하는 방법이 분명 있을테니 다시 리펙토링 해보아야겠다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,36 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return removDuplication(cryptogram);
+    }
+
+    public static String removDuplication(String str){
+
+        while(true) {
+            // 중복된 문자열이 있는 확인하는 변수
+            int duplicatedChek = 0;
+
+            for (int strIndex = 0; strIndex < str.length() - 1; strIndex++) {
+                // 연속으로 중복된 index를 찾으면
+                if (str.charAt(strIndex) == str.charAt(strIndex + 1)) {
+                    int strDuplicatiedIndex = strIndex + 1;
+                    // 어디까지 연속인지 검사
+                    while (strDuplicatiedIndex < str.length() && str.charAt(strIndex) == str.charAt(strDuplicatiedIndex)) {
+                        strDuplicatiedIndex++;
+                    }
+                    strDuplicatiedIndex--;
+
+                    // 연속된 문자열 제거
+                    str = str.substring(0, strIndex) + str.substring(strDuplicatiedIndex + 1);
+                    // 문자열이 바뀌였으므로 처음부터 다시 검사
+                    duplicatedChek = 1;
+                    break;
+                }
+            }
+            // 바뀐 문자열이 없으므로 검사 중지
+            if (duplicatedChek == 0) break;
+        }
+
+        return str;
     }
 }
