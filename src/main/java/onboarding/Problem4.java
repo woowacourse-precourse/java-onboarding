322b6e5008c410e8ef26762ed4e607d5ea72b99a
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-10-27 21:13:01 +0900

    기능 1(각 문자가 알파벳인지 확인한다) 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int wordLen = word.length();
+        for(int i = 0; i < wordLen; ++i){
+            char curChar = word.charAt(i);
+            if(Character.isLetter(curChar)) {
+
+            }
+        }
         return answer;
     }
 }
