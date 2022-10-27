48feed21b1ff51024e0544c56497595e1581b2c6
5nam <ohnam90@gmail.com>
2022-10-27 22:44:01 +0900

    feat: 입력받은 숫자 n 을 배열에 1 ~ n 까지 나열하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    // number 을 배열에 1 ~ number 까지 나열하는 함수
+    private static int[] intToArray(int number) {
+        int[] output = new int[number];
+
+        for(int i = 0; i<number; i++) {
+            output[i] = i+1;
+        }
+
+        return output;
+    }
 }
