c8b2da3b13d8d32a030aaf60934d318d77ed621b
jaehee329 <jaehee329@yonsei.ac.kr>
2022-10-27 11:39:52 +0900

    test(Problem2): Problem2의 통합 테스트 구현

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -5,12 +5,47 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class Problem2Test {
+
+	@Nested
+	class IntegrationTest {
+
+		@Test
+		@DisplayName("browoanoommnaon을 brown으로 줄여야 한다")
+		void integrationTest1() {
+			String string = "browoanoommnaon";
+
+			assertThat(solution(string)).isEqualTo("brown");
+		}
+
+		@Test
+		@DisplayName("zyelleyz을 빈 문자열로 줄여야 한다")
+		void integrationTest2() {
+			String string = "zyelleyz";
+
+			assertThat(solution(string)).isEqualTo("");
+		}
+
+		@Test
+		@DisplayName("aabbccd를 d로 줄여야 한다")
+		void integrationTest3() {
+			String string = "aabbccd";
+
+			assertThat(solution(string)).isEqualTo("d");
+		}
+
+		@Test
+		@DisplayName("bgfaafgecddce를 b로 줄여야 한다")
+		void integrationTest4() {
+			String string = "bgfaafgecddce";
+
+			assertThat(solution(string)).isEqualTo("b");
+		}
+	}
 
 	@Test
 	@DisplayName("주어진 인덱스와 다음 인덱스의 문자가 중복일 때만 참을 반환해야 한다")
