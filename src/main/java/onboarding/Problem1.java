12400189b321cb94f634bb9f399e91a0bc18bfc6
0112leesy <0112leesy@naver.com>
2022-10-27 20:28:49 +0900

    feat: invalid

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,16 @@ class Problem1 {
         return answer;
     }
 
+    static boolean invalid(List<Integer> list){
+        int left = list.get(0);
+        int right = list.get(1);
+
+        if(left+1 != right) return true;
+        if(left%2 == 0) return true;
+        if(out_of_bound(left) || out_of_bound(right)) return true;
+        return false;
+    }
+
     static boolean out_of_bound(int number){
         if(number >= 1 && number <= 400) return false;
         return true;
