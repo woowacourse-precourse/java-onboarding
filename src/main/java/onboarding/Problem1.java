50233571dcbff6c87c292978071a210d84aa44d9
parkmuhyeun <pjhg410@gmail.com>
2022-10-27 22:27:02 +0900

    feat: [Problem1] solution 로직 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import problem1.CompareScore;
+import problem1.Page;
 import java.util.List;
 
 class Problem1 {
+    private static final CompareScore compareScore = new CompareScore();
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Page pobiPage = new Page(pobi.get(0), pobi.get(1));
+        Page crongPage = new Page(crong.get(0), crong.get(1));
+
+        if (!pobiPage.checkPage() || !crongPage.checkPage())
+            return -1;
+
+        return compareScore.compare(pobiPage.getMaxValue(), crongPage.getMaxValue());
     }
 }
