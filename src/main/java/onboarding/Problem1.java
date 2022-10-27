d7d297aedd01a330373ae41b832b07a602e57793
hyunsb <jhss0113@naver.com>
2022-10-27 21:03:51 +0900

    problem1 add exception

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        // 첫 페이지 혹은 마지막 페이지 혹은 범위를 벗어나는 페이지를 선택했을 시 에러처리
+        if(Math.min(pobi.get(0), crong.get(0))<=0 && Math.max(pobi.get(1), crong.get(1))>=400){
+            return -1;
+        }
+
+        // 연속되는 페이지를 선택하였을 시 로직 실행
         if(pobi.get(0) == pobi.get(1)-1 && crong.get(0) == crong.get(1)-1){
             int pobiMax = Math.max(findMaxNum(pobi.get(0)), findMaxNum(pobi.get(1)));
             int crongMax = Math.max(findMaxNum(crong.get(0)), findMaxNum(crong.get(1)));
