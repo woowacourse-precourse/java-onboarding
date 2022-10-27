5c4d6793ec6276764c438254f6966e1d79d00487
Cho Changhun <cvcvcx@naver.com>
2022-10-27 16:21:42 +0900

    7번문제 방문자중 내 친구가 아닌 사람 거르는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 //A와 B는 친구
 //입력된 유저와 친구는 아니지만,
@@ -30,12 +31,43 @@ public class Problem7 {
             }
             if(friends.get(i).indexOf(user)==1) {
                 result.add(friends.get(i).get(0));
-                continue;
             }
 
         }
         System.out.println(result);
         return result;
 
+    }
+//입력된 방문자 목록과 횟수를 구함 친구 제외시킴
+    public static HashMap<String,Integer> visitorsNotMyFriendPoints(List<String> visitors,List<String> myFriends){
+        List<String> visitorsNotMyFriend = new ArrayList<String>();
+        for (int i = 0; i < visitors.size(); i++) {
+            for (int j = 0; j < myFriends.size(); j++) {
+                if(visitors.get(i).indexOf(myFriends.get(j))<0){
+                    visitorsNotMyFriend.add(visitors.get(i));
+                }
+            }
+        }
+
+        HashMap<String,Integer> result = new HashMap<String,Integer>();
+        for(int i =0 ;i<visitors.size(); i++){
+            Integer prevVisitNum = result.get(visitors.get(i));
+            if (prevVisitNum!=null){
+                result.put(visitors.get(i),prevVisitNum+1);
+                continue;
+            }
+            result.put(visitors.get(i),1);
+        }
+    }
+    //방문자중 내 친구가 아닌 사람을 찾는 함수
+    public static List<String> visitorsNotMyFriend(List<String> visitors,List<String> myFriends){
+        List<String> visitorsNotMyFriend = new ArrayList<String>();
+        for (int i = 0; i < visitors.size(); i++) {
+            for (int j = 0; j < myFriends.size(); j++) {
+                if(visitors.get(i).indexOf(myFriends.get(j))<0){
+                    visitorsNotMyFriend.add(visitors.get(i));
+                }
+            }
+        }
     }
 }
