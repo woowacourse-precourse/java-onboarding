ef68e57b2df6dde66feb33094bc215c57591db02
2jun0 <soo28819@gmail.com>
2022-10-27 19:15:05 +0900

    Feat: 문제 6번 - forms을 이메일:닉네임 형식의 맵으로 변환하는 메서드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+
+    public static Map<String, String> createNicknameMap(List<List<String>> forms) {
+        HashMap<String, String> nicknameMap = new HashMap<>();
+
+        for (List<String> emailNickname: forms) {
+            String email = emailNickname.get(0);
+            String nickname = emailNickname.get(1);
+
+            nicknameMap.put(email, nickname);
+        }
+
+        return nicknameMap;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
