27c28d220edbe17b5c5e2dfb79cd26b1bda7dd49
asas6978 <asas6978@naver.com>
2022-10-27 22:54:08 +0900

    feat(problem2): 중복 문자 삭제 기능 구현

---
+++ src/main/java/onboarding/problem2/Delete.java
@@ -0,0 +1,63 @@
+package onboarding.problem2;
+
+public class Delete {
+    public static String executeDelete(String cryptogram) {
+        int index = 0;
+
+        while(isAbleCompare(cryptogram) && isExistDuplication(cryptogram)) {
+            DeleteResult deleteResult = deleteDuplication(cryptogram, index);
+
+            cryptogram = deleteResult.cryptogram;
+            index = (index + deleteResult.index) % (cryptogram.length() - 1);
+        }
+
+        if(cryptogram.length() == 2 && (cryptogram.charAt(0) == cryptogram.charAt(1))) {
+            cryptogram = "";
+        }
+
+        return cryptogram;
+    }
+
+    private static boolean isAbleCompare(String cryptogram) {
+        return cryptogram.length() > 2;
+    }
+
+    private static boolean isExistDuplication(String cryptogram) {
+        int index = 0;
+        boolean result = false;
+
+        while(index < cryptogram.length() - 1) {
+            result = (result || (cryptogram.charAt(index) == cryptogram.charAt(index + 1)));
+            index++;
+        }
+
+        return result;
+    }
+
+    private static DeleteResult deleteDuplication(String cryptogram, int index) {
+        String originalCryptogram = cryptogram;
+        int duplicateIndex = index;
+
+        while((index < cryptogram.length()) &&
+                (cryptogram.charAt(index) == cryptogram.charAt(duplicateIndex + 1))) {
+            duplicateIndex++;
+        }
+
+        if(duplicateIndex != index) {
+            cryptogram = cryptogram.substring(0, index) + cryptogram.substring(duplicateIndex + 1);
+            return new DeleteResult(cryptogram, 0);
+        }
+
+        return new DeleteResult(originalCryptogram, 1);
+    }
+
+    private static class DeleteResult {
+        private String cryptogram;
+        private int index;
+
+        public DeleteResult(String cryptogram, int index) {
+            this.cryptogram = cryptogram;
+            this.index = index;
+        }
+    }
+}
