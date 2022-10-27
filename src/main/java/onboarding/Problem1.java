793d794caf767ac2525b9e578b43a4dc0dd2bd32
kokodak <kokodakadokok@gmail.com>
2022-10-27 14:27:38 +0900

    refactor: [문제1] 가독성을 위한 줄바꿈 추가 및 else 문 제거

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,41 +6,49 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobiScore = getMaxNumber(pobi);
         int crongScore = getMaxNumber(crong);
+
         return compareScore(pobiScore, crongScore);
     }
 
     private static int getMaxNumber(List<Integer> page) {
         int leftPage = page.get(0);
         int rightPage = page.get(1);
+
         if (!validationCheck(leftPage, rightPage)) return -1;
+
         int leftPageMaxNumber = Math.max(getMaxNumberWithAdd(leftPage), getMaxNumberWithMultiply(leftPage));
         int rightPageMaxNumber = Math.max(getMaxNumberWithAdd(rightPage), getMaxNumberWithMultiply(rightPage));
+
         return Math.max(leftPageMaxNumber, rightPageMaxNumber);
     }
 
     private static int getMaxNumberWithAdd(int pageNumber) {
         String tmp = Integer.toString(pageNumber);
         int ret = 0;
+
         for (int i = 0; i < tmp.length(); i++) {
             ret += tmp.charAt(i) - '0';
         }
+
         return ret;
     }
 
     private static int getMaxNumberWithMultiply(int pageNumber) {
         String tmp = Integer.toString(pageNumber);
         int ret = 1;
+
         for (int i = 0; i < tmp.length(); i++) {
             ret *= tmp.charAt(i) - '0';
         }
+
         return ret;
     }
 
     private static int compareScore(int pobiScore, int crongScore) {
         if (pobiScore == -1 || crongScore == -1) return -1;
         if (pobiScore > crongScore) return 1;
-        else if (pobiScore < crongScore) return 2;
-        else return 0;
+        if (pobiScore < crongScore) return 2;
+        return 0;
     }
 
     private static boolean validationCheck(int leftPage, int rightPage) {
