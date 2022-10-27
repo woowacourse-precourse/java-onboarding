c199c4eeca21f74a300504a83c60fdc8647bcbf6
¤knj <krkoknj@gmail.com>
2022-10-27 23:42:28 +0900

    첫번째 페이지나 마지막 페이지가 매개변수로 들어오면 -1을 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int FIRST_PAGE = 1;
+    private static final int LAST_PAGE = 400;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (validate(pobi) || validate(crong)) {
+            return -1;
+        }
+
         return answer;
+    }
+
+    public static boolean validate(List<Integer> checkList) {
+        return checkList.contains(FIRST_PAGE) || checkList.contains(LAST_PAGE);
     }
 }
