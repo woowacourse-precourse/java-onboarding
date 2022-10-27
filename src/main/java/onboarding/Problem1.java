50d40385babcd07b5f4e9779dabdae0ee81c3113
WonJeongYeon <wonjungyoun@naver.com>
2022-10-27 10:51:37 +0900

    20221027 Problem1.java 예외처리 및 중복 코드 제외할 함수 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,61 +6,65 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        int pobi_result = 0;
-        for (int i = 0; i<2; i++) {
-            System.out.println(pobi.get(i));
-            String pobiStr = Integer.toString(pobi.get(i));
-            int[] pobi_numList = new int[pobiStr.length()];
-            int pobi_sum = 0;
-            int pobi_mul = 1;
-            for (int j = 0; j<pobiStr.length(); j++) {
-                pobi_numList[j] = pobiStr.charAt(j) - '0';
-                pobi_sum += pobi_numList[j];
-                pobi_mul *= pobi_numList[j];
-                if (pobi_result < Math.max(pobi_mul, pobi_sum)) {
-                    pobi_result = Math.max(pobi_mul, pobi_sum);
+        try {
+            if (pobi.isEmpty() || crong.isEmpty()) {
+                throw new IllegalStateException("입력된 값이 존재하지 않습니다.");
+            } else if (pobi.get(0) % 2 == 0 || crong.get(0) % 2 == 0) {
+                throw new IllegalStateException("왼쪽 페이지 번호는 홀수여야 합니다.");
+            } else if (pobi.size() != 2 || crong.size() != 2) {
+                throw new IllegalStateException("페이지 번호는 좌, 우로 총 2개여야 합니다.");
+            } else if (Math.abs(pobi.get(1) - pobi.get(0)) != 1 || Math.abs(crong.get(1) - crong.get(0)) != 1) {
+                throw new IllegalStateException("페이지 번호는 연속되어야 합니다.");
+            } else if (pobi.get(0) < 1 || pobi.get(0) > 400 || crong.get(0) < 1 || crong.get(0) > 400) {
+                throw new IllegalStateException("페이지 범위는 0~400입니다.");
+            } else {
+
+                int pobi_result = calMaximum(pobi);
+                int crong_result = calMaximum(crong);
+
+                if (pobi_result > crong_result) {
+                    answer = 1;
+                } else if (pobi_result < crong_result) {
+                    answer = 2;
+                } else if (pobi_result == crong_result) {
+                    answer = 0;
+                } else {
+                    answer = -1;
                 }
+
+                System.out.println(pobi_result);
+                System.out.println(crong_result);
+
             }
 
-        }
-
-        int crong_result = 0;
-        for (int i = 0; i<2; i++) {
-            System.out.println(crong.get(i));
-            String crongStr = Integer.toString(crong.get(i));
-            int[] crong_numList = new int[crongStr.length()];
-            int crong_sum = 0;
-            int crong_mul = 1;
-            for (int j = 0; j<crongStr.length(); j++) {
-                crong_numList[j] = crongStr.charAt(j) - '0';
-                crong_sum += crong_numList[j];
-                crong_mul *= crong_numList[j];
-                if (crong_result < Math.max(crong_mul, crong_sum)) {
-                    crong_result = Math.max(crong_mul, crong_sum);
-                }
-            }
-
-        }
-
-        if (pobi_result > crong_result) {
-            answer = 1;
-        } else if (pobi_result < crong_result) {
-            answer = 2;
-        } else if (pobi_result == crong_result) {
-            answer = 0;
-        } else {
+        } catch (IllegalStateException e) {
             answer = -1;
         }
-
-        System.out.println(pobi_result);
-        System.out.println(crong_result);
         System.out.println(answer);
         System.out.println("pobi crong result");
-        System.out.println(pobi + " " + crong + " " +  answer);
+        System.out.println(pobi + " " + crong + " " + answer);
         return answer;
     }
 
+    public static int calMaximum(List<Integer> friend) {
+        int result = 0;
+        for (int i = 0; i<2; i++) {
+            System.out.println(friend.get(i));
+            String numStr = Integer.toString(friend.get(i));
+            int[] numList = new int[numStr.length()];
+            int sum = 0;
+            int mul = 1;
+            for (int j = 0; j<numStr.length(); j++) {
+                numList[j] = numStr.charAt(j) - '0';
+                sum += numList[j];
+                mul *= numList[j];
+                if (result < Math.max(mul, sum)) {
+                    result = Math.max(mul, sum);
+                }
+            }
 
-
+        }
+        return result;
+    }
 
 }
