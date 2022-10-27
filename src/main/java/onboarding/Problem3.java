ed52579dd306e9a05bb47f99706e52ebcb9dabca
lzlkolo <lzlko1209@gmail.com>
2022-10-27 20:09:40 +0900

    PROBLEM3.기능목록 및 예외사항 처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+    static String numCheck(int number) {
+        String exception="";
+        if(number < 1 || number > 1000) {
+            exception = "범위 오류";
+        }
+        return exception;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        numCheck(number);
         return answer;
     }
 }
