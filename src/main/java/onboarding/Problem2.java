25366593435c93c453cddd7b32f1c379e44a624c
KimJaeSung <behappy0916@naver.com>
2022-10-27 18:03:23 +0900

    java-onboarding-2-문자열 중복을 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,7 +3,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 public class Problem2 {
-
+    static void removeOverLap(List data){
+        while(true){
+            boolean overlap = false;
+            for(int i=0; i<data.size() -1; i++){
+                if(data.get(i) == data.get(i+1)){
+                    data.remove(i);
+                    data.remove(i);
+                    overlap = true;
+                }
+            }
+            if(overlap == false){
+                break;
+            }
+        }
+    }
     public static String solution(String cryptogram) {
         String answer = "";
         List<Character> crypcryptogramList = new ArrayList<>();
