3b81128c72bd11775772a4d37fc4235c1c85b9a8
eastsage <dpwk8@naver.com>
2022-10-27 19:20:27 +0900

    feat: [problem4] 해독문을 구하는 함수와 솔루션 함수 구현
    암호문에서 평문으로 해독해주는 getPlainWord 함수 구현 및 solution 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return getPlainWord(word);
+    }
+
+    static String getPlainWord(String word) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : word.toCharArray()) {
+
+            if (Character.isUpperCase(c)) {
+                sb.append((char) (155 - c));
+            } else if (Character.isLowerCase(c)) {
+                sb.append((char) (219 - c));
+            } else sb.append(c);
+        }
+        return sb.toString();
     }
 }
