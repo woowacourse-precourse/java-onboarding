aa8a3c8af04f0b300b15a535a0e1df95d2582eaf
tjddnr7760 <tjddnr7760@naver.com>
2022-10-27 18:06:37 +0900

    <5번 - 구현> 링크드 리스트 추가, 각 금액별 나누고 리스트 추가 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,21 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.LinkedList;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = new LinkedList<Integer>();
+
+        int[] moneyarray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i=0; i<moneyarray.length; i++) {
+            int quotient = money/moneyarray[i];
+            answer.add(quotient);
+            money = money - moneyarray[i] * quotient;
+        }
+
         return answer;
     }
 }
