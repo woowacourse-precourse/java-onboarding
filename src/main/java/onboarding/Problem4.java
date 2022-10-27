5aa7576ca6f8418337552cbb4b2ffbbfae4831da
Kid-Chang <pch3310@gmail.com>
2022-10-27 14:23:25 +0900

    feat: solved problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Problem4 {
+
+    public static void main(String[] args) {
+        String word = "I love you";
+        String result = "R olev blf";
+        System.out.println(solution(word));
+    }
     public static String solution(String word) {
-        String answer = "";
+        String answer;
+        if (word.length()==0 || word.length()>1000){
+            return "";
+        }
+        answer = reverseString(word);
+
         return answer;
+    }
+
+    /**
+     * 청개구리의 방식대로 A->z, b->Y 가 출력되도록 하는 함수
+     * @param word
+     * @return
+     */
+    public static String reverseString(String word){
+        StringBuilder reverseWord = new StringBuilder();
+
+        for(char c: word.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                reverseWord.append((char) ('Z' - c + 'A'));
+            } else if (c >= 'a' && c <= 'z') {
+                reverseWord.append((char) ('z' - c + 'a'));
+            }
+            else{
+                reverseWord.append(c);
+            }
+        }
+
+        return reverseWord.toString();
     }
 }
