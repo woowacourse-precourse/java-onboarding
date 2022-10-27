e373466fcc72cc9107555ff0b9ca41da362c0f39
Park shinyeong <syngh503@gmail.com>
2022-10-28 02:06:23 +0900

    Problem6/Feat/닉네임에서 두 글자씩 빼내오는 기능 구현 닉넥임에서 두글자씩 빼내 HashMap에 삽입하는 getNickname 함수를 구현하였습니다. 해시맵과 닉네임 문자열, 닉네임이 들어있는 인덱스를 매개변수로 입력받습니다. 닉네임 해시맵을 리턴합니다.
    Relate to: #6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,15 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem6 {
+    private HashMap<String, Integer> nicknameMap = new HashMap<>();
+    private TreeSet<String> alertList = new TreeSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
@@ -18,6 +23,19 @@ public class Problem6 {
             return true;
         }
         return false;
+    }
+
+    public static HashMap<String, Integer> getNickname(HashMap<String, Integer> nicknameMap, String nickname, Integer idx) {
+        for (int i = 0; i < nickname.length() - 1; i++){
+            char nicknameChar1 = nickname.charAt(i);
+            char nicknameChar2 = nickname.charAt(i + 1);
+
+            String words = String.valueOf(nicknameChar1) + String.valueOf(nicknameChar2);
+            if (nicknameMap.containsKey(words) == false) {
+                nicknameMap.put(words, idx);
+                continue;
+            }
+        }
     }
 
 }
