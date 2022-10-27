c6b3c0e895a21a1190c945911edd3bdb3f16bde7
YooByeongIk <quddlr96@naver.com>
2022-10-27 15:21:59 +0900

    [Problem2] feat : 중복 문자 여부 체크

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,7 +12,19 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        char[] charArray = cryptogram.toCharArray();
+        while(isDuplicate(charArray)){
+
+        }
         return answer;
     }
 
+    public static boolean isDuplicate(char[] charArray) {
+        for(int i=1;i<charArray.length;i++){
+            if(charArray[i] == charArray[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
 }
