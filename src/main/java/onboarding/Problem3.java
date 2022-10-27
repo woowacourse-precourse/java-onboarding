2cdd5402ece9bf0a394aa10640ba8f31571dc6fa
0112leesy <0112leesy@naver.com>
2022-10-27 21:32:24 +0900

    [Problem3] feat: get_number_of_clap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,17 @@ public class Problem3 {
         return answer;
     }
 
+    static int get_number_of_clap(int number){
+        int number_of_clap = 0;
+        String number_to_string = Integer.toString(number);
+        for(int i=0; i<number_to_string.length(); i++){
+            if(is_clap_number(number_to_string.charAt(i) - '0')){
+                number_of_clap++;
+            }
+        }
+        return number_of_clap;
+    }
+
     static boolean is_clap_number(int number){
         if(number == 3 || number == 6 || number == 9) return true;
         return false;
