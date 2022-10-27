b701e31c1014089c71e1b68cd6497cc383ad2fba
ycopp9 <aciees95@gmail.com>
2022-10-27 11:09:55 +0900

    feat: add the function getMaxClapByDigit() that returns the maximum number of claps by digits.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,14 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    private final static int[] MAX_CLAP_BY_DIGIT = {12000, 900, 60, 3, 0}; // a[n-1 - i] = 3 * (10 ^ (i-1)) + 10 * a[n-i - (i-1)]  (i >= 1)
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
-
+    private static int getMaxClapByDigit(int i) {
+        return MAX_CLAP_BY_DIGIT[i];
+    }
     private static String zeroPadding(int number) {
         return String.format("%05d", number);
     }
+
     private static int[] toInts(String value) {
         int[] numbers = new int[value.length()];
         for(int i = 0; i < value.length(); i++) {
