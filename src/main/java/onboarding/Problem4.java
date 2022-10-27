2efee53c79c99d93141fdf10da74970af159e97a
junhei Lee <jun2ee22@naver.com>
2022-10-27 19:59:30 +0900

    feat: 알파벳 소문자 변환 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,6 +9,15 @@ public class Problem4 {
         iResult = 65 + (90 - iUpper);
         return (char) iResult;
     }
+
+    //소문자 변경
+    private static char changeLower(char lower) {
+        int iLower = (int) lower;
+        int iResult = 0;
+        iResult = 97 + (122 - iLower);
+        return (char) iResult;
+    }
+
     
     public static String solution(String word) {
         String answer = "";
