7904e519bc90b1099b4fc67db10b0e830ebe6709
limjimin <dkdldsla0000@naver.com>
2022-10-28 02:00:50 +0900

    Feat(Problem2): 사이즈가 1인 경우, 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,12 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         Deque<String> queue = new ArrayDeque<>(List.of(cryptogram.split("")));
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            if (queueSize == 1) {
+                break;
+            }
+        }
         return answer;
     }
 }
