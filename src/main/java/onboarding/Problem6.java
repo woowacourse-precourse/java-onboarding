ad513177d300b91a5e4741c26646f4584964a6c2
K-jun98 <alap_u@naver.com>
2022-10-27 20:07:33 +0900

    feat - 크루의 이름과 이메일을 저장하고 이름의 토큰 수를 카운트한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    private static Map<String, String> crews = new HashMap<>();
+    private static Map<String, Integer> nameTokens = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        crewToMap(forms);
     }
+
+    private static void crewToMap(List<List<String>> forms) {
+        for (List<String> id : forms) {
+            String name = id.get(1);
+            crews.put(name, id.get(0));
+            sliceName(name);
+        }
+    }
+
+    private static void sliceName(String name) {
+        for (int i = 0; i < name.length() - 1; i++) {
+            String nameToken = name.substring(i, i + 2);
+            nameTokens.put(nameToken, nameTokens.getOrDefault(nameToken, 0) + 1);
+        }
+    }
+
 }
