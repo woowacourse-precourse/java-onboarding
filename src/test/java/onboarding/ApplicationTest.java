65151d8f98a9caa39ac0fd9e982f31101e5e9b60
wlsh44 <k87913j@gmail.com>
2022-10-27 21:28:40 +0900

    test: 테스트 코드 추가 [문제4]

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -105,6 +105,13 @@ class ApplicationTest {
             String result = "R olev blf";
             assertThat(Problem4.solution(word)).isEqualTo(result);
         }
+
+        @Test
+        void case2() {
+            String word = "";
+            String result = "";
+            assertThat(Problem4.solution(word)).isEqualTo(result);
+        }
     }
 
     @Nested
