99f48f78ca36d16a3541313a7641d1857a87bbb6
sieun <sieun960513@naver.com>
2022-10-27 18:06:13 +0900

    [Problem4] : 오류 수정 [수정]

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,7 @@ package onboarding;
  * solutionLogic    : 메인 솔루션
  * classifyChar    : 대문자, 소문자, 일반문자 판별
  * lowerChange      : 소문자 변환
- * upperChange      : 대문자 변
+ * upperChange      : 대문자 변환
  * checkValid       : 예외 처리
  * checkWordLength  : 단어 길이 체크
  */
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,11 +5,7 @@ import java.util.List;
 
 /** 기능 목록
  * solutionLogic    : 메인 솔루션
- * solutionLogic    : 대문자, 소문자, 일반문자 판별
- * lowerChange      : 소문자 변환
- * upperChange      : 대문자 변
  * checkValid       : 예외 처리
- * checkWordLength  : 단어 길이 체크
  */
 
 public class Problem5 {
