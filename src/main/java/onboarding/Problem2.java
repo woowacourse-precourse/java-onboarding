2665039a221ad2ccd0081346c2df1b617bfd533b
chj <bluedog129@gmail.com>
2022-10-28 01:16:18 +0900

    연속하는 중복 문자 stack으로 분별 후 중복하지 않는 문자만 담게 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String result = "";
+        char temp = ' ';
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : cryptogram.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            }
+            else if (temp == c){
+                continue;
+            }
+            else if (stack.peek() != c) {
+                stack.add(c);
+            }
+            temp = c;
+        }
+        return result;
     }
 }
