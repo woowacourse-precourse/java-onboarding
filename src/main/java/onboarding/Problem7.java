b6c1f332d97c61bf972f7f3453397a52005fda54
apptie <appti@naver.com>
2022-10-28 00:55:59 +0900

    feat: 문제 7 회원의 친구 관계를 더하는 addFriend 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,10 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Problem7 {
@@ -21,5 +23,9 @@ class Account {
         this.id = id;
         this.friendsRelation = new HashSet<>();
         this.score = 0;
+    }
+
+    public void addFriend(Account friend) {
+        friendsRelation.add(friend);
     }
 }
