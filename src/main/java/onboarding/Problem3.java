31b23eb72ec42915c8e76782ee55def60e837e5d
swandevson <shai4956@gmail.com>
2022-10-28 04:19:41 +0900

    fix: P3 3, 6, 9 체크 수정#1
    10의 배수도 카운트가 증가하는 경우가 있어 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+    public static int chckClap(int n) {
+        int cnt = 0;
+
+        while(n > 0) {
+            if (n%10 != 0 && (n%10) % 3 == 0) {  // 3, 6, 9 모두 3의 배수임
+                cnt++;
+            }
+            n /= 10;
+        }
+
+        return cnt;
+    }
+
+
     public static int solution(int number) {
         int answer = 0;
+
+
+
         return answer;
     }
 }
