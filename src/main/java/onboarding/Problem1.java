98384238228f02d75951572dd667d14c34157c59
TaeMin <xoals3094@naver.com>
2022-10-27 16:26:16 +0900

    compare score method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,18 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public int compare_score(List<Integer> score_list){
+        int max = 0;
+        for(int i = 0; i < score_list.size(); i++){
+            int score = make_score(score_list.get(i));
+            if(score > max){
+                max = score;
+            }
+        }
+        return max;
+    }
+
     public int make_score(int page_num){
         int sum = 0;
         int mul = 1;
