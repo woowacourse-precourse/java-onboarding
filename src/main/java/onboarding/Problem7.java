be0d5ee5944a5650ccfb7b92a01aa93f318c67c8
HwangSeokJin <ghkd3703@gmail.com>
2022-10-28 03:20:15 +0900

    타임 라인 방문 횟수만큼 추천 점수를 매기는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,9 +8,9 @@ import java.util.HashSet;
 
 public class Problem7 {
     private static Map<String, HashSet<String>> graph = new HashMap<>();
+    private static Map<String, Integer> scores = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        Map<String, Integer> scores = new HashMap<>();
         graph.put(user, new HashSet<>());
 
         for (List<String> idList: friends){
@@ -24,18 +24,28 @@ public class Problem7 {
             graph.get(idB).add(idA);
         }
 
-        addScoreByMutualFriend(scores, user);
+        addScoreByMutualFriend(user);
+        addScoreByVisits(visitors, user);
 
         return answer;
     }
 
-    private static void addScoreByMutualFriend(Map<String, Integer> scores, String user){
+    private static void addScoreByMutualFriend(String user){
         for (String friend: graph.get(user)){
             for (String name: graph.get(friend)){
                 if (name.equals(user)) continue;
                 if (graph.get(user).contains(name)) continue;
+
                 scores.put(name, scores.getOrDefault(name, 0) + 10);
             }
+        }
+    }
+
+    private static void addScoreByVisits(List<String> visitors, String user){
+        for (String visitor: visitors){
+            if (graph.get(user).contains(visitor)) continue;
+
+            scores.put(visitor, scores.getOrDefault(visitor, 0) + 1);
         }
     }
 }
