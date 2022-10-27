b1f7bc0acf698b6e67ba83d1211b3045d83c1e44
qdeu22 <qdeu22@gmail.com>
2022-10-27 16:11:25 +0900

    예외처리 기능 커밋

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,11 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        /* 예외 처리 기능 */
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1))
+            return -1;
+
         return answer;
     }
 }
