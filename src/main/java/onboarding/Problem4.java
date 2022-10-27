4b412a32ef9a35a2d0809f71e51217841a921ec6
juno-junho <ssmm0205@naver.com>
2022-10-27 22:54:02 +0900

    feat(Problem4): create solution method
    청개구리 사전을 참고해 매개변수 word를 반대로 변환하는 solution 메소드 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder(word.length());
+        for (char letter : word.toCharArray()) {
+            if (Character.isUpperCase(letter)) {
+                sb.append((char) ('Z' - (letter - 'A')));
+                continue;
+            }
+            if (Character.isLowerCase(letter)) {
+                sb.append((char) ('z' - (letter - 'a')));
+                continue;
+            }
+            sb.append(letter);
+        }
+        return sb.toString();
     }
 }
