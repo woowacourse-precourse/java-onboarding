7532809c15499dcf4de2ac44b05111c80df821f6
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-27 14:52:27 +0900

    feat(problem2): 요구사항 설정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,12 @@
 package onboarding;
 
+/**
+ * > 요구사항 </br>
+ * 1. 인접한 중복 문자열 삭제(stack을 이용해 매번 순회할 때 마다 중복여부에 따라 추가 /삭제 결정 </br>
+ * 2. 알파벳은 소문자로만 이루어져 있음
+ */
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
