9c57fb34764bd02f8a3c096cd0dbc0139dc1df62
jthugg <jthug@naver.com>
2022-10-27 21:29:11 +0900

    feat. 치트시트, 루프 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,10 +4,22 @@ public class Problem4 {
     public static String solution(String word) {
         // todo list
         // 알파벳 치트시트
-        // 대소문자 구분 변환 루프
-        // 알파벳 변환만 구현하면 됨
-        // 알파벳인지 아닌지 체크하는 기능
+        String A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String Z = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        String a = "abcdefghijklmnopqrstuvwxyz";
+        String z = "zyxwvutsrqpomnlkjihgfedcba";
+
         StringBuilder sb = new StringBuilder();
+
+        // 대소문자 구분 변환 루프
+        for(int i = 0; i < A.length(); i++) {
+            // 알파벳인지 아닌지 체크
+            char ch = word.charAt(i);
+            boolean bool = (ch >= 'a' && ch <='z') || (ch >= 'A' && ch >= 'Z');
+            // 알파벳 변환만 구현하면 됨
+
+        }
+
         return sb.toString();
     }
 }
