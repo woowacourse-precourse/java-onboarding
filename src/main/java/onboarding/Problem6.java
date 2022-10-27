22b7f33d4ed8d9b585738fe80fef6954ad3410af
code-tree <dltjdqls1200@naver.com>
2022-10-27 19:56:30 +0900

    feat: Check for invalid input

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        if (forms.size() < 1 || forms.size() > 10000) {
+            return Collections.emptyList();
+        }
+        
         return answer;
     }
 }
