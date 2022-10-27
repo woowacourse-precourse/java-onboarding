10f0de9648708d9410c1cd71942a6721a83ddaf2
eastsage <dpwk8@naver.com>
2022-10-28 02:22:18 +0900

    feat: [problem6] forms 에서 중복된 부분을 찾아서 리스트로 반환하는 함수 구현
    findDuplicatedWord 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static List<String> findDuplicatedWord(List<List<String>> forms) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> duplicatedWord = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String crewName = form.get(1);
+            for (int j = 0; j < crewName.length() - 1; j++) {
+                String subName = crewName.substring(j, j + 2);
+                map.put(subName, map.getOrDefault(subName, 0) + 1);
+            }
+        }
+
+        for (String s : map.keySet()) {
+            if (map.get(s) > 1)
+                duplicatedWord.add(s);
+        }
+        return duplicatedWord;
     }
 }
