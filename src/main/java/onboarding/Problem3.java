025bbb93aba09d4a6462855d025d5ce9d5a0c5d6
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-27 17:29:59 +0900

    feat: [Problem3] - 요구사항 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1 ; i <= number ; i++) {
+            answer += Stream.of(String.valueOf(i).split(""))
+                    .mapToInt(Integer::parseInt)
+                    .filter(num -> num == 3 || num == 6 || num == 9)
+                    .count();
+        }
+
         return answer;
     }
 }
