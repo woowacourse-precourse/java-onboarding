90773f92bee8942b6f9a767ed189195c2240833f
greeng00se <thegreengoose7@gmail.com>
2022-10-28 00:22:37 +0900

    feat(Problem6): 제한사항에 만족하는 이메일 도메인인지 검증하는 메서드 추가
    - email.com 도메인인지 확인

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    private static final int DOMAIN_INDEX = 1;
+    private static final String VALID_DOMAIN = "email.com";
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static boolean isValidDomain(String email) {
+        String[] separatedEmail = email.split("@");
+        return separatedEmail[DOMAIN_INDEX].equals(VALID_DOMAIN);
     }
 }
