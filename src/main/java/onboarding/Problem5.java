3d5c74e6937d2a10527ae9f70425bbcd866033af
code-tree <dltjdqls1200@naver.com>
2022-10-27 17:21:36 +0900

    feat: Check for invalid input

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        if (money < 1 || money > 1_000_000) {
+            return answer;
+        }
+
         return answer;
     }
 }
