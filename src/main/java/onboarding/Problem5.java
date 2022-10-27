f7142c351f09912463d26a9fa47ab759c14088a4
choyeram <choyeram8@gmail.com>
2022-10-27 13:56:03 +0900

    Feat : calculate money

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] values = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+
+        for (int value : values) {
+            answer.add(money / value);
+            money = money % value;
+        }
+
         return answer;
     }
 }
