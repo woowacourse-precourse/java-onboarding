3a0dc1fe94f9e84dca65714f1fd5b22477cea8dc
jeongjihun <wlgns2234@naver.com>
2022-10-27 21:15:24 +0900

    Feat: forms를 받았때 {제이엠:이메일} 형태의 Map을 반환하는기능추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+    public static Map<String, String> makeHashMap(List<List<String>> forms) {
+        Map<String, String> hash = new HashMap<>();
+        for(int i = 0; i < forms.size(); i++) {
+            List<String> data = forms.get(i);
+            hash.put(data.get(1), data.get(0));
+        }
+        return hash;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        //해시 테이블 생성
+        Map<String, String> hash = makeHashMap(forms);
+
+
+
         return answer;
     }
 }
