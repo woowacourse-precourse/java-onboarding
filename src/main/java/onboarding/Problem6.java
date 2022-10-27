a4a66eb6d2a1d1eb85786d0f9eba642fc11f5951
Your Name <you@example.com>
2022-10-27 20:30:11 +0900

    Problem6 기능 목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        /*두 글자 이상 문자 분리*/
+
+        /*중복 있을 시 중복된 닉네임의 이메일 저장*/
+        
+        /*이메일 중복 제거*/
+
         return answer;
     }
 }
