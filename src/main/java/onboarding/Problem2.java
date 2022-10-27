4b891ee880b440739a652fd9e18341f421a3e1d2
limjimin <dkdldsla0000@naver.com>
2022-10-28 01:59:58 +0900

    Feat(Problem2): 입력받은 문자열 Deque로 변환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Deque<String> queue = new ArrayDeque<>(List.of(cryptogram.split("")));
         return answer;
     }
 }
