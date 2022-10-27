38bf1963011e14c08a676173d4cb6a8292d608cc
정현수 <wjdgustn15@naver.com>
2022-10-27 16:51:36 +0900

    refactor : refactor Problem7
    *로직 관련 부분 메소드로 재정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,6 +6,61 @@ import java.util.stream.Collectors;
 public class Problem7 {
     static Map<String,List<String>> friendMap;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        initFriendRelation(friends);
+        Map<String, Integer> visitorScore = getVisitorScore(user, visitors);
+        Queue<Score> scores = getSCorePriorityQueue(visitorScore);
+        return getAnswerList(scores);
+    }
+
+    private static List<String> getAnswerList(Queue<Score> scores) {
+        List<String> answer =new ArrayList<>();
+        while(!scores.isEmpty()&&answer.size()!=5){
+            answer.add(scores.poll().name);
+        }
+        return answer;
+    }
+
+    private static Queue<Score> getSCorePriorityQueue(Map<String, Integer> visitorScore) {
+        Queue<Score> scores = new PriorityQueue<>();
+        scores.addAll(visitorScore.keySet().stream().map(x->new Score(x, visitorScore.get(x))).collect(Collectors.toList()));
+        return scores;
+    }
+
+    private static Map<String, Integer> getVisitorScore(String user, List<String> visitors) {
+        Map<String, Integer> visitorScore = calculateFriendFriend(user);
+        calculateVisitor(visitors, visitorScore);
+        removeUserFriend(user, visitorScore);
+        removeUser(user, visitorScore);
+        return visitorScore;
+    }
+    private static Map<String, Integer> calculateFriendFriend(String user) {
+        Map<String,Integer> visitorScore = new HashMap<>();
+        for (String friend : friendMap.get(user)) {
+            for (String friendFriend : friendMap.getOrDefault(friend,new ArrayList<>())) {
+                visitorScore.merge(friendFriend,10,Integer::sum);
+            }
+        }
+        return visitorScore;
+    }
+    private static void calculateVisitor(List<String> visitors, Map<String, Integer> visitorScore) {
+        for (String visitor : visitors) {
+            visitorScore.merge(visitor,1,Integer::sum);
+        }
+    }
+
+
+    private static void removeUserFriend(String user, Map<String, Integer> visitorScore) {
+        for (String friend : friendMap.get(user)) {
+            removeUser(friend, visitorScore);
+        }
+    }
+
+    private static void removeUser(String user, Map<String, Integer> visitorScore) {
+        visitorScore.remove(user);
+    }
+
+
+    private static void initFriendRelation(List<List<String>> friends) {
         friendMap = new HashMap<>();
         for (List<String> friend : friends) {
             friendMap.putIfAbsent(friend.get(0),new ArrayList<>());
@@ -14,29 +69,6 @@ public class Problem7 {
             friendMap.get(friend.get(0)).add(friend.get(1));
             friendMap.get(friend.get(1)).add(friend.get(0));
         }
-
-        Map<String,Integer> visitorScore = new HashMap<>();
-        for (String friend : friendMap.get(user)) {
-            for (String friendFriend : friendMap.getOrDefault(friend,new ArrayList<>())) {
-                visitorScore.merge(friendFriend,10,Integer::sum);
-            }
-        }
-        for (String visitor : visitors) {
-            visitorScore.merge(visitor,1,Integer::sum);
-        }
-
-        for (String friend : friendMap.get(user)) {
-            visitorScore.remove(friend);
-        }
-        visitorScore.remove(user);
-
-        Queue<Score> scores = new PriorityQueue<>();
-        scores.addAll(visitorScore.keySet().stream().map(x->new Score(x, visitorScore.get(x))).collect(Collectors.toList()));
-        List<String> answer =new ArrayList<>();
-        while(!scores.isEmpty()&&answer.size()!=5){
-            answer.add(scores.poll().name);
-        }
-        return answer;
     }
 }
 class Score implements Comparable<Score>{
