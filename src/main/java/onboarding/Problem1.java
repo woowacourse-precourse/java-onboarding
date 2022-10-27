a0aec90576f719649cbf36be515753514231c987
gomljo <gomljo0307@naver.com>
2022-10-27 14:22:42 +0900

    feat: 제한사항 2번 구현(#9)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static final int CORRECT_LENGTH = 2;
-    public static final int NUMBER_OF_PLAYER = 2;
+    public static final int LEFT_PAGE = 0;
+    public static final int RIGHT_PAGE = 1;
     public static final int DIFFERENCE_BETWEEN_PAGES=1;
     public static final int START_PAGE = 1;
     public static final int FINAL_PAGE = 400;
     public static Boolean isCorrectLength(List<Integer> bookPages){
         return bookPages.size() != CORRECT_LENGTH;
     }
+    public static boolean isCorrectPosition(List<Integer> bookPages){
+        return (bookPages.get(LEFT_PAGE) % 2!=1)||(bookPages.get(RIGHT_PAGE)%2!=0);
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-
-        if(Problem1.isCorrectLength(pobi)&Problem1.isCorrectLength(crong)){
-            return -1;
-        }
         return answer;
     }
 }
