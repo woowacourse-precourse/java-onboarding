0095d2182e6cf76c44837e4305956f49498838cd
ddubidubap <park62000@gmail.com>
2022-10-27 21:59:46 +0900

    PROBLEM4 반대 아스키 반환 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,6 +7,20 @@ public class Problem4 {
         2. solution (반대되는 문자들을 문자열로 합침)
     */
 
+    // 반대 아스키 반환 함수
+    private static int reverseAscii(int ascii) {
+        // 대문자 알파벳의 반대 아스키 반환
+        if (ascii >= 65 && ascii <= 90)
+            return 155 - ascii;
+
+        // 소문자 알파벳의 반대 아스키 반환
+        if (ascii >= 97 && ascii <= 122)
+            return 219 - ascii;
+
+        // 그 외의 문자는 그대로 반환
+        return ascii;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
