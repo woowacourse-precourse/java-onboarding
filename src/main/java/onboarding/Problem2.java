0a39a2b252421be6c0b8921b597ea7cb245d1a41
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-27 22:18:09 +0900

    기능요구사항 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,13 @@
 package onboarding;
 
+import org.intellij.lang.annotations.RegExp;
+
 public class Problem2 {
+
+    // 기능 요구 사항
+    // TODO 중복 문자의 시작 인덱스와 마지막 인덱스를 리턴하는 메서드
+    // TODO 중복 문자를 삭제하는 메서드
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
