83fb3c5bbd6bf188951d0b43f5459631f19caf80
swandevson <shai4956@gmail.com>
2022-10-28 02:07:23 +0900

    feat: 페이지 확인 및 예외처리
    pobi 또는 crong의 페이지를 확인하고 예외상황일 경우 -1 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
 import java.util.List;
+import java.lang.Math;
+
 
 class Problem1 {
+
+    public static int getPageResult(List<Integer> pageList) {
+        int leftPage = pageList.get(0);
+        int rightPage = pageList.get(1);
+
+        if (leftPage <= 1 || rightPage >= 400) {
+            return -1;
+        }
+        if (rightPage - leftPage != 1) {
+            return -1;
+        }
+
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
+
         return answer;
     }
 }
