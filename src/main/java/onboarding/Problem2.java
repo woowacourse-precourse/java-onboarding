eef367cb8ef75dec4f5cc8598c32bb9d3590b964
lee <higgs95@naver.com>
2022-10-27 19:57:19 +0900

    feat(problem2): remove duplicate characters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        Stack<Character> duplicateChecker = new Stack<>();
+        char[] cryptogramChars = cryptogram.toCharArray();
+
+        for(int index=0; index<cryptogramChars.length; index++) {
+            if (index==0 || duplicateChecker.empty()) {
+                duplicateChecker.push(cryptogramChars[index]);
+            }else if (duplicateChecker.peek() == cryptogramChars[index]) {
+                duplicateChecker.pop();
+            }else{
+                duplicateChecker.push(cryptogramChars[index]);
+            }
+        }
+
+        if(!duplicateChecker.empty()) {
+            for(char cryptogramChar : duplicateChecker) {
+                answer += "" + cryptogramChar;
+            }
+        }
         return answer;
     }
 }
