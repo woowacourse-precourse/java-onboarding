3add9abe96ff6470ae5237d64508a1ae9a6a505a
korjun1993 <corojoon93@naver.com>
2022-10-27 20:24:03 +0900

    기능추가: Problem2 solution 추가
    i번째 문자의 좌우를 살피면서 연속으로 중복된 문자가 오는지 검사한다. 중복일 경우 해당 문자를 제거한다. 이와 같은 동작을 하는 함수 decode()를 작성하였다. 문자열 s에 대해 decode(s)의 결과값이 s와 다르다면 계속 반복한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,38 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String decryption = null; // 연속으로 중복된 문자를 제거한 결과
+
+        // 중복을 제거한 결과가 이전과 다르면 계속 진행한다.
+        while (!(decryption = decode(cryptogram)).equals(cryptogram)) {
+            cryptogram = decryption;
+        }
+
+        return decryption;
+    }
+
+    /**
+     * @param cryptogram 중복문자가 포함된 문자열
+     * @return 중복된 문자가 있다면 이를 제거한 결과를 반환한다.
+     */
+    public static String decode(String cryptogram) {
+        if (cryptogram == null || cryptogram.equals(null)) {
+            return null;
+        }
+
+        int size = cryptogram.length();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+
+            // i번째 문자열의 좌우로 같은 문자열이 오는지 확인한다.
+            if (i + 1 < size && cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                continue;
+            }
+            if (i - 1 >= 0 && cryptogram.charAt(i) == cryptogram.charAt(i - 1)) {
+                continue;
+            }
+            result.append(cryptogram.charAt(i));
+        }
+        return result.toString();
     }
 }
