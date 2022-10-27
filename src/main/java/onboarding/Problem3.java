e067c8cda21a659d4b17925b23ae1f2eaf96fdb9
aossuper7 <aossuper7@naver.com>
2022-10-27 14:22:41 +0900

    feat: 문제3 number 각 자리에 3,6,9 숫자 있는지 판단 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,4 +8,15 @@ public class Problem3 {
         }
 
     }
+
+    public static int count(String number) {
+        int hap = 0;
+        for (int i = 0; i < number.length(); i++) {
+            char ch = number.charAt(i);
+            if (ch == '3' || ch == '6' || ch == '9') {
+                hap += 1;
+            }
+        }
+        return hap;
+    }
 }
