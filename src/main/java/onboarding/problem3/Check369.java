3731a26439e25d2854e606ca3f9212e04960adae
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 18:08:56 +0900

    feat(P3): 3,6,9를 체크하고 회수를 세어주는 로직 구현

--- src/main/java/onboarding/problem3/Check369.java
+++ src/main/java/onboarding/problem3/Check369.java
@@ -2,5 +2,33 @@ package onboarding.problem3;
 
 public class Check369 {
 
+    public int check369(String number) {
+        int result = 0;
+        for (int i = 1; i < number.length(); i++) {
+
+            if (contains369(number)) {
+                result += count369(number);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean contains369(String number) {
+        return number.contains("3") || number.contains("6") || number.contains("9");
+    }
+
+    private int count369(String number) {
+        int count = 0;
+        for (int i = 0; i < number.length(); i++) {
+            int num = number.charAt(i) - '0';
+
+            if (num == 3 || num == 6 || num == 9) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 
 }
--- src/main/java/onboarding/problem3/ThreeSixNineGame.java
+++ src/main/java/onboarding/problem3/ThreeSixNineGame.java
@@ -2,6 +2,12 @@ package onboarding.problem3;
 
 public class ThreeSixNineGame {
 
+    private final Check369 check369;
+
+    public ThreeSixNineGame(Check369 check369) {
+        this.check369 = check369;
+    }
+
     public int do369(int number) {
 
         return 0;
