efe8c158b6d0931e4f1ea33936f8c085380281f7
GwansuShin <happysun7080@naver.com>
2022-10-27 17:04:25 +0900

    feat(Problem5): 최초 구현
    제한사항 검사
    [재귀]변환 금액 계산(단위 금액 당 누계, money 감산)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,55 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Problem5 {
+
+    private static final List<Integer> moneyUnitList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    private static final String MONEY_KEY = "MONEY";
+    private static final String COUNT_KEY = "COUNT";
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        if (!validateInput(money)) {
+            return null;
+        }
+        return calculateMoney(money);
+    }
+
+    private static boolean validateInput(int money) {
+        return Validation.validate(money);
+    }
+
+    private static List<Integer> calculateMoney(int money) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (Integer moneyUnit : moneyUnitList) {
+            Map<String, Integer> result = calculateMoneyUnitCount(money, moneyUnit, 0);
+            money = result.get(MONEY_KEY);
+            answer.add(result.get(COUNT_KEY));
+        }
         return answer;
+    }
+
+    private static Map<String, Integer> calculateMoneyUnitCount(int money, int moneyUnit, int count) {
+        if (money < moneyUnit) {
+            return Map.of(MONEY_KEY, money, COUNT_KEY, count);
+        }
+        return calculateMoneyUnitCount(money - moneyUnit, moneyUnit, ++count);
+    }
+
+    static abstract class Validation {
+        private static final int MIN_VALUE = 1;
+        private static final int MAX_VALUE = 1000000;
+
+        public static boolean validate(int money) {
+            return validateRange(money);
+        }
+
+        private static boolean validateRange(int money) {
+            return money >= MIN_VALUE && money <= MAX_VALUE;
+        }
     }
 }
