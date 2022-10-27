58db1d011e75c95c18dcd502e89d7c779d080593
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-10-27 22:53:47 +0900

    add: problem1 풀 풀이

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,68 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static int addDigit(int n) {
+        int result = 0;
+        while (n > 0) {
+            result += n % 10;
+            n /= 10;
+        }
+        return (result);
+    }
+
+    private static int multiDigit(int n) {
+        int result = 1;
+        while (n > 0) {
+            result *= n % 10;
+            n /= 10;
+        }
+        return (result);
+    }
+
+    private static int maxPage(List<Integer> pages) {
+        int max = Integer.MIN_VALUE;
+        for (int p : pages) {
+            int addSum = addDigit(p);
+            int multiSum = multiDigit(p);
+            if (max < addSum) {
+                max = addSum;
+            }
+            if (max < multiSum) {
+                max = multiSum;
+            }
+        }
+        return (max);
+    }
+
+    private static boolean isValidInput(List<Integer> page) {
+        if (page.get(1) - page.get(0) != 1) {
+            return (false);
+        }
+        for (int p : page) {
+            if (p < -1 || p > 400) {
+                return (false);
+            }
+        }
+        return (true);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int max_pobi = maxPage(pobi);
+        int max_crong = maxPage(crong);
+
+        if (!isValidInput(pobi) || !isValidInput(crong)) {
+            return (-1);
+        }
+        if (max_pobi > max_crong) {
+            answer = 1;
+        }
+        else if (max_crong < max_pobi) {
+            answer = 2;
+        }
+        else {
+            answer = 0;
+        }
         return answer;
     }
 }
