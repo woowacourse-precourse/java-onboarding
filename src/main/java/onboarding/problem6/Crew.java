953d820b9cc8833080d88a13b7353eaaa4226b39
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-28 01:16:29 +0900

    feat: form과 forms를 받아 Crew 또는 Crew 리스트를 만드는 기능

--- src/main/java/onboarding/problem6/Crew.java
+++ src/main/java/onboarding/problem6/Crew.java
@@ -2,14 +2,23 @@ package onboarding.problem6;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Crew {
 	private final String email;
 	private final Set<String> splitNames;
 
-	public Crew(List<String> form) {
+	private Crew(List<String> form) {
 		this.email = form.get(0);
 		this.splitNames = Splitter.splitTwoChar(form.get(1));
+	}
+
+	public static Crew createFrom(List<String> form) {
+		return new Crew(form);
+	}
+
+	public static List<Crew> createCrews(List<List<String>> forms) {
+		return forms.stream().map(Crew::createFrom).collect(Collectors.toList());
 	}
 
 	public String getEmail() {
