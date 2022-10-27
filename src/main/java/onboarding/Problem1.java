1918644c700df8401a48684be862fba48788956d
nakalic <nakalic28@gmail.com>
2022-10-27 21:46:28 +0900

    feat(Problem1): 값 검증하는 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+
+		// 값 검증
+		if (validateNum(pobi) == -1 || validateNum(crong) == -1)
+			return -1;
+
+		return answer;
+	}
+
+	// 올바르면 1, 이상하면 -1을 return
+	private static int validateNum(List<Integer> lst) {
+		if (lst.get(0) < 1 || lst.get(1) > 400)
+			return -1;
+		if (lst.get(0) % 2 == 0 || lst.get(1) % 2 == 1)
+			return -1;
+		if (lst.get(0) + 1 != lst.get(1))
+			return -1;
+
+		return 1;
+	}
 }
