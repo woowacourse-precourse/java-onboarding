3ecbb05c939fd9442c616de71014dc6e128ae797
great-park <cksgh1735@naver.com>
2022-10-27 23:30:52 +0900

    fix(Problem2): 주석 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class Problem2 {
     private static Stack<String> stack = new Stack<String>();
+
     public static String solution(String cryptogram) {
-        List<String> buffer = Arrays.asList(cryptogram.split("")); // ['b','r','w','p','p','w']
+        List<String> buffer = Arrays.asList(cryptogram.split(""));
         pushStack(buffer);
         String answer = parseStackToString(stack);
         return answer;
     }
-
 
     public static List<String> pushStack(List<String> buffer){
         for(int i = 0; i < buffer.size(); i++){
