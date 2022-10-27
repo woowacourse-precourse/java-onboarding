d07a15d3ef254abacaa38639f48687f27c436c54
drunkenhw <drunkenhw@gmail.com>
2022-10-28 00:18:10 +0900

    feat(Problem3) : 숫자를 자릿수 별로 분리한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-
+            List<Integer> digits = getDigitList(i);
         }
         return answer;
+    }
+
+    private static List<Integer> getDigitList(int number) {
+        List<Integer> digitList = new ArrayList<>();
+        while (number != 0) {
+            digitList.add(number % 10);
+            number /= 10;
+        }
+        return digitList;
     }
 }
