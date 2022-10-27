26253ebed0139e88d6fea7040b5500a2164b250a
csj <koreaxman@naver.com>
2022-10-27 17:38:36 +0900

    feat: Divide from large numbers in order

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return divideMoneySequential(money);
+    }
+
+    public static List<Integer> divideMoneySequential(int money) {
+        List<Integer> moneyList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> moneyCount = new ArrayList<>();
+
+        for (Integer integer : moneyList) {
+            int sum = 0;
+
+            if (money >= integer) {
+                sum = money / integer;
+                money %= integer;
+            }
+            moneyCount.add(sum);
+        }
+        return moneyCount;
     }
 }
