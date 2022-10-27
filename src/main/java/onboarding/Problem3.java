12afdf37720c7ebafa25121d0adee30aa5573a42
sonsoowon <yuwoo1018@gmail.com>
2022-10-27 17:16:25 +0900

    feat(P3): 숫자의 손뼉 횟수 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    static int countClap(Integer num){
+        String[] numTokens = num.toString().split("");
+        Stream<String> tokenStream = Arrays.stream(numTokens);
+        Stream<String> clapTokens = tokenStream.filter(t -> ((t == "3") || (t == "6") || (t == "9")));
+
+        return (int) clapTokens.count();
     }
 }
