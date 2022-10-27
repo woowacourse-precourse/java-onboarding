c8c7b07e49f23f644cc666afbb183941be3fbba7
saehim <saeilee.vxv@gmail.com>
2022-10-28 02:13:54 +0900

    Feat(Problem2): 삭제할 문자 인덱스 반환 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,5 +4,13 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    // 삭제할 문자 인덱스 반환
+    private static List<Integer> deleteIdx(String input){
+        char[] chars = input.toCharArray();
+        List<Integer> delete = new ArrayList<>();
+        for (int i=1; i<chars.length; i++){
+            if (chars[i-1]==chars[i]) delete.add(i);
+        }
+        return delete;
     }
 }
