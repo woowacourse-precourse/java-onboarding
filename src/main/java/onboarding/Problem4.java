37fa357591ec8bed688dd13571920060b417d865
Park shinyeong <syngh503@gmail.com>
2022-10-27 20:06:17 +0900

    Problem4/Refactor/영문자 변환함수 리팩토링 영어 소문자, 대문자 따로 나뉘어져있던 함수를 changeLetter 함수 하나로 통합했습니다. checkAlphabet 조건도 수정했습니다. Relate to: #4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,27 +1,26 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
 
-    public static boolean checkAlphabet(char character) {
-        if(character < 65 || character > 90 || character < 97 || character > 122) {
-           return false;
+    public static boolean checkAlphabet(int asciiNum) {
+        if (asciiNum < 65 || (asciiNum > 90  && asciiNum < 97) || asciiNum > 122) {
+            return false;
         }
         return true;
     }
 
-    public static char changeLowercaseLetter(char c) {
-        int TOTAL = (int)'a' + (int)'z';
-        int asciiNum = (int)c;
-        char answer = (char)(TOTAL - asciiNum);
-        return answer;
+    public static char changeLetter(int asciiNum) {
+        int TOTAL = 0;
+        if (asciiNum <= 90) {
+            TOTAL = (int)'A' + (int)'Z';
+        }
+        if(asciiNum >= 97){
+            TOTAL = (int)'a' + (int)'z';
+        }
+        return (char)(TOTAL - asciiNum);
     }
-
-
 }
