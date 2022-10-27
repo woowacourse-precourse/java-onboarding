76a7f61b500c092ac0c1599b06abaae5c17c1070
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 16:48:04 +0900

    문제 7 : 각자의 친구 목록을 자료구조 hashmap을 사용해 저장하는 메소드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,6 +10,22 @@ import java.util.*;
  * 4. 점수를 저장하는 map 을 value 기준으로 내림차순 정렬하는 메소드 -> stream 사용 treemap 을 사용했기 때문에 key 는 자동 오름차순 정렬
  **/
 public class Problem7 {
+    private static Map<String, ArrayList> map_friends = new HashMap<>();
+
+    // 각자의 친구 목록을 자료구조 hashmap 을 사용해 저장해주는 메소드
+    private static void makeFriendsMap(List<List<String>> friends) {
+        for (List<String> list : friends) {
+            ArrayList A_List = map_friends.getOrDefault(list.get(0), new ArrayList());
+            ArrayList B_List = map_friends.getOrDefault(list.get(1), new ArrayList());
+
+            A_List.add(list.get(1));
+            B_List.add(list.get(0));
+
+            map_friends.put(list.get(0), A_List);
+            map_friends.put(list.get(1), B_List);
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         return answer;
