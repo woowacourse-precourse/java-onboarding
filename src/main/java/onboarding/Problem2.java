f261d8b0719d22e096c9f25e6636614c94f92dcf
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-27 21:49:55 +0900

    feat : Add a function checking cryptogram length

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    //문자열 길이가 2 이상인지 체크하는 함수
+    public static boolean isLengthLong(String cryptogram) {
+        int length = cryptogram.length();
+        if (length < 2) return false;
+        return true;
     }
 }
