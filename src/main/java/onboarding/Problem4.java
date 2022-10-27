2e90543676e2a32fd9ec080800ef593c57c8e26f
jjongwa <troas96@naver.com>
2022-10-27 13:46:37 +0900

    문제4- 문자 변환 함수 작성 및 예제 확인 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+    static String frog(String word){
+        StringBuilder rev_w = new StringBuilder();
+        for(int i = 0; i < word.length(); i++){
+            if((int)word.charAt(i) >=65 && (int)word.charAt(i) <= 90){  // 대문자
+                rev_w.append((char) (155 - (int) word.charAt(i)));
+            }
+            else if((int)word.charAt(i) >=97 && (int)word.charAt(i) <= 122){  // 소문자
+                rev_w.append((char) (219 - (int) word.charAt(i)));
+            }
+            else{
+                rev_w.append(word.charAt(i));
+            }
+        }
+        return rev_w.toString();
+    }
+
+    public static String solution(String word){
+        return frog(word);
     }
 }
