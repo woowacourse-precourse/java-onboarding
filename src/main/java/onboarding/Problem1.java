9ef0be515de1b1303efd7ad6e19ef09e2cb6398e
yjlee0235 <yjlee0235@naver.com>
2022-10-27 17:18:56 +0900

    feat: 입력된 페이지 유효성 검사 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import onboarding.problem1.PageValidator;
+
 import java.util.List;
 
 class Problem1 {
+
+    public static final int EXCEPTION_SITUATION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        PageValidator pageValidator = new PageValidator();
+        try {
+            pageValidator.validatePages(pobi);
+            pageValidator.validatePages(crong);
+        } catch (IllegalArgumentException e) {
+            return EXCEPTION_SITUATION;
+        }
         return answer;
     }
 }
