5fc9a3c0758c65a0aedc4d08639b0ab13408bf00
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-27 13:24:23 +0900

    [Problem 2] getDecodingCryptogram은 decodingCurrentString을 통해 현재 String이 decoding해야 하는지 알 수 있고 그것을 바탕으로 모든 decoding이 끝날 때 까지 decodingCurrentString을 호출한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer=getDecodingCryptogram(cryptogram);
         return answer;
     }
+
+    private static String getDecodingCryptogram(String cryptogram){
+        boolean completeDecoding = false;
+        StringBuffer decodedCrypto = new StringBuffer(cryptogram);
+        while(!completeDecoding){
+            completeDecoding=decodingCurrentString(decodedCrypto);
+        }
+    }
+
 }
