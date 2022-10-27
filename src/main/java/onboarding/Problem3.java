a02f43623e3c67d0340141891b19200d11a1c0eb
kdkdhoho <hkim4410@naver.com>
2022-10-27 10:12:32 +0900

    refactor: Problem3 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -13,15 +13,21 @@ public class Problem3 {
 
     public static int count369(int number) {
         int result = 0;
-
         String numStr = String.valueOf(number);
+
         for (int i = 0; i < numStr.length(); i++) {
-            char ch = numStr.charAt(i);
-            if((int)ch == 3 || (int)ch == 6 || (int)ch == 9) {
+            String tmpStr = Character.toString(numStr.charAt(i));
+            int checkNum = Integer.parseInt(tmpStr);
+            if(is369(checkNum))
                 result++;
-            }
         }
 
         return result;
+    }
+
+    public static boolean is369(int checkNum) {
+        if(checkNum == 3 || checkNum == 6 || checkNum == 9)
+            return true;
+        return false;
     }
 }
