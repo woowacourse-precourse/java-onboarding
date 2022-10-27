270bd985c5dec61f5b3dfcd9501d5d541e96decc
Kangbin <hz0819@naver.com>
2022-10-27 16:42:30 +0900

    5 Slove

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int Change = 100000;
+        int Switch = 2;
+        do  {
+            Change = (int)Change/Switch;
+            Switch = (Switch==5) ? 2 : 5;
+            if (Change==5) Change=1;
+            answer.add((int)money/Change);
+            money = money%Change;
+        }while(Change!=1);
         return answer;
     }
 }
