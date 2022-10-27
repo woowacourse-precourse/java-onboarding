2562a66e5c031ab7d7bb37664b1bb84c387f9fdc
suheonjoo <suheonj95@naver.com>
2022-10-27 20:40:23 +0900

    feat(Problem6): Problem6 메인 로직 구현 및 연속된 문자를 가지고 있는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -18,4 +18,20 @@ public class Problem6 {
         }
     }
 
+    private static boolean hasConsecutiveString(String base, String target) {
+
+        int baseLength = base.length();
+        int targetLength = target.length();
+
+        for (int i = 0; i < baseLength - 1; i++) {
+            for (int j = 0; j < targetLength - 1; j++) {
+                if (base.charAt(i) == target.charAt(j)) {
+                    if (base.charAt(i + 1) == target.charAt(j + 1)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
