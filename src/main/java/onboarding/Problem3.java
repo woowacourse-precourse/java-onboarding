a429c70b869d3558ffd2bc02c9cc80cec2c76244
Heosejin98 <sejin3319@naver.com>
2022-10-27 22:45:55 +0900

    Feat : PROBLEM3 예외처리 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+
+    static boolean Exception(int number) {
+        return (0 < number && number <= 10000) ? false : true;
+    }
     public static int solution(int number) {
         int answer = 0;
-        return answer;
+
+        if (Exception(number))
+            return answer;
+        else
+            return 1;
+
+    }
+
+    public static void main(String argv[]){
+        System.out.println(solution(10001));
+        System.out.println(solution(0));
+        System.out.println(solution(10000));
+        System.out.println(solution(1));
     }
 }
