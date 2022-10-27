e7f1921c38b15356e13538ea04682c45e795052e
minju412 <ln8847@ajou.ac.kr>
2022-10-27 10:07:15 +0900

    solve prob 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int cnt = 0;
+        ArrayList<Character> charList = new ArrayList<Character>();
+
+        for (int i=1; i<=number; i++) {
+            String str = String.valueOf(i); // int -> string 변환
+
+            // str -> charList 변환
+            for (int j=0; j<str.length(); j++) {
+                charList.add(str.charAt(j));
+            }
+
+            // 예를 들어, 36 이라는 숫자에는 박수 두 번
+            for (Character character : charList) {
+                if (character.equals('3') || character.equals('6') || character.equals('9')) {
+                    cnt++;
+                }
+            }
+
+            charList.clear();
+        }
+
+        return cnt;
     }
 }
