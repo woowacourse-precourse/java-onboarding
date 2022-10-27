e92fe6a2be4f547b948d533181f852b14c155b98
Reznov <reznov03048@gmail.com>
2022-10-28 00:19:30 +0900

    문자를 변환하는 메소드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,11 +4,25 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
-        // 청개구리 사전 만들기
-
+        // 입력받은 문자열을 어절 단위로 자름
+        
         // 변환하기
 
+        // 변환된 문자열 만들기
 
         return answer;
+    }
+
+    /**
+     * 청개구리 사전 기준으로 문자 변환하는 메소드
+     * @param c
+     * @return 변환된 문자를 반환
+     */
+    static char changeChar(char c){
+        int asci = (int)c;
+        int changeNum = 155 - asci;
+        char result = (char) changeNum;
+
+        return result;
     }
 }
