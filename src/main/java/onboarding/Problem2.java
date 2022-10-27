cc7c41cd9c04be390b765a562d1d9b3565dafad0
eugene1204 <s1061001@naver.com>
2022-10-27 20:15:31 +0900

    p4

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,43 +1,57 @@
 package onboarding;
 
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 //import java.util.LinkedListList;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        // 1. 글자 분리하기
-        // 2. 중복을 따로 확인할 List 만들기
-        // 3. ...
-        String[] cry = cryptogram.split("");
-        List<String> crylist = new LinkedList<>();
+        // 제한사항 길이, 소문자
+        // 중복이 없을때 까지 반복해서 중복문자 제거하기
 
-        for (int a =0; a<cry.length; a++)  crylist.add(cry[a]);
+        // 1. cryptogram을 list로 문자열 분리하기
+        // 2. 문자열 분리후 길이 확인, 소문자확인
+        // 3. list를 하나 더 만들어서 계속해서 저장하기
 
-        List<Integer> list = new LinkedList<>();
-
+        List<String> lastList = crypList(cryptogram); // browoanoommnaon
         while (true) {
-            String last = "";
-            for (int i = 0; i < crylist.size(); i++) {
-                if (last.equals(crylist.get(i))) {
-                    list.add(i - 1);
-                    list.add(i);
-                } //browoanoommnaon
-                last = crylist.get(i);
-            }
-
-            if(list.size() == 0) break;
-
-            for(int j =0; j<list.size(); j++){
-                crylist.remove(list.get(j));
-            }
-
-            list.clear();
-
-
+            List<String> newList = cryp(lastList); //browoannaon -> browoaaon -> browoon -> brown
+            if(lastList.size() == newList.size()) break;
+            lastList.clear();
+            lastList = newList; //browoannaon
         }
+        
+        String text = String.join((CharSequence) lastList,",");
 
-        return crylist.toString();
+        return text;
+
+    }
+
+    public static List<String> crypList(String s){
+        String[] arr = s.split("");
+        List<String> list = new ArrayList<>();
+        for(int i =0; i<arr.length;i++) list.add(arr[i]);
+        return list;
+
+    }
+
+    public static List<String> cryp(List<String> list){
+        List<String> lastList = list;
+        List<String> newList = new ArrayList<>();
+        if(lastList.size()== newList.size()) return lastList;
+        String last =lastList.get(0);
+
+        for (int i =1; i<lastList.size(); i++){
+            if(!last.equals(lastList.get(i))) newList.add(lastList.get(i-1));
+            last= lastList.get(i);
+        }
+        newList.add(lastList.get(-1));
+
+
+        return  newList;
 
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        // 65 66 67 68 69 70 71 72 73 74 75 ..
+        // 90 89 88
+        // 155, 대문자
+
+        // 97 98 99 100 101 102 103
+        // 122 121 120
+        // 219, 소문자
+
+        // 1 2 3 4 5 6 7 8 9 10
+        // 10 9 8 7 6 5 4 3 2 1
+
+        // 대문자 인가 소문자인가
+        // 맞는 숫자 출력해주기
+        String newWord ="";
+        for (int i =0; i<word.length(); i++){
+            int num = Integer.valueOf(word.charAt(i));
+            if(word.charAt(i)==' ') newWord +=" ";
+            if(num >64 && num <91) newWord += String.valueOf(Math.abs(155-num));
+            if(num >97 && num <122) newWord += String.valueOf(Math.abs(219-num));
+
+        }
+
+        return newWord;
     }
 }
