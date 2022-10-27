569b9edcafa21b6bbb84c7dae8f733ce531f7b83
yujaemin <woawoa0513@gmail.com>
2022-10-27 18:58:10 +0900

    feat: [문제 1] 문제 조건에 따른 예외 처리 기능 추가
    다음 상황에 대해 예외로 처리한다.
    1. 시작 면이나 마자믹 면을 펼쳤을 떄
    2. 왼쪽 페이지의 번호와 오르쪽 페이지의 번호가 연속되지 않을 떄

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,13 @@ class Problem1 {
         int pobiLeft = pobi.get(0), pobiRight = pobi.get(1);
         int crongLeft = crong.get(0), crongRight = crong.get(1);
 
+        if (pobiLeft == 1 || crongLeft == 1 || pobiRight == 400 || crongRight == 400) {
+            return -1;
+        }
+        if (pobiRight - pobiLeft != 1 || crongRight - crongLeft != 1) {
+            return -1;
+        }
+
         int pobiScore = Math.max(getBigNumberByPage(pobiLeft), getBigNumberByPage(pobiRight));
         int crongScore = Math.max(getBigNumberByPage(crongLeft), getBigNumberByPage(crongRight));
 
