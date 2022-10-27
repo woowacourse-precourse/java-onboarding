db95783bb0340edf7bbd0ecb49075f861e46db91
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 09:52:04 +0900

    feat: 숫자의 각 자리수를 더하는 로직 추가

--- src/main/java/onboarding/problem1/PageNumberGame.java
+++ src/main/java/onboarding/problem1/PageNumberGame.java
@@ -1,8 +1,17 @@
 package onboarding.problem1;
 
+import java.util.Arrays;
+
 public class PageNumberGame {
 	public static final Integer POBI_WIN = 1;
 	public static final Integer CRONG_WIN = 2;
 	public static final Integer TIE_MATCH = 0;
 	public static final Integer EXCEPTION = -1;
+
+	private static Integer addEachNumber(Integer number) {
+		return Arrays.stream(number.toString().split(""))
+			.mapToInt(Integer::parseInt)
+			.sum();
+	}
+	
 }
