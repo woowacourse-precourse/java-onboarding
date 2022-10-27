6589f1e25d8ae06903aa3a3ef7da52c53f56ae27
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-10-27 15:46:18 +0900

    refactor : '중복 문자 찾기' 로직 메서드 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,32 +6,20 @@ import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-
         List<Character> crypList = initList(cryptogram);
         List<Character> tmpList = new ArrayList<>(crypList);
 
         while (true) {
             int size = crypList.size();
-            for (int i = 0; i < crypList.size() - 1; i++) {
-                if (crypList.get(i) == crypList.get(i + 1)) {
-                    tmpList.set(i, '*');
-                    tmpList.set(i + 1, '*');
-                }
-            }
+            findDuplicateChar(crypList, tmpList);
+
             tmpList.removeAll(Collections.singleton('*'));
             crypList = new ArrayList<>(tmpList);
+
             if (size == crypList.size()) break; // 변화가 없었다면 종료
         }
 
         return getAnswerStr(crypList);
-    }
-
-    private static String getAnswerStr(List<Character> crypList) {
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < crypList.size(); i++) {
-            answer.append(crypList.get(i));
-        }
-        return answer.toString();
     }
 
     private static List<Character> initList(String str) {
@@ -40,6 +28,23 @@ public class Problem2 {
             list.add(str.charAt(i));
         }
         return list;
+    }
+
+    private static void findDuplicateChar(List<Character> crypList, List<Character> tmpList) {
+        for (int i = 0; i < crypList.size() - 1; i++) {
+            if (crypList.get(i) == crypList.get(i + 1)) {
+                tmpList.set(i, '*');
+                tmpList.set(i + 1, '*');
+            }
+        }
+    }
+
+    private static String getAnswerStr(List<Character> crypList) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < crypList.size(); i++) {
+            answer.append(crypList.get(i));
+        }
+        return answer.toString();
     }
 
 }
