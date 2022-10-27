17d81fd412f679dc4eaf93e32dc61aa6a7e514c1
endlessmomo <soohyuk96@naver.com>
2022-10-27 20:19:12 +0900

    Problem1 데이터 검증 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,21 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int answer = Integer.MIN_VALUE;
+
+        if(validation(pobi) || validation(crong)){
+            return -1;
+        }
+
+        return 0;
+    }
+
+    public static boolean validation(List<Integer> list){
+        if(list.size() != 2 || list.get(0) <= 1 || list.get(1) >= 400
+            || list.get(0) % 2 != 1 || list.get(1) % 2 == 0) {
+            return true;
+        }
+
+        return list.get(1) - list.get(0) % 2 != 1;
     }
 }
