0c75bea3ab96c238ad19502c210ba9475746306e
jhon3242 <rldotnr2@gmail.com>
2022-10-27 15:59:31 +0900

    [feat] 문제 7 같은 친구 있으면 점수 추가 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,19 +1,28 @@
 package onboarding;
 
+import org.mockito.internal.util.collections.Sets;
+
 import java.util.*;
 
 public class Problem7 {
     static class User{
-        final String name;
-        List<String> friends;
-        List<String> visitor;
-        int score;
+        private final String name;
+        private List<String> friends;
+        private List<String> visitor;
+        private int score;
 
         public User(String name) {
             this.name = name;
             this.friends = new ArrayList<>();
             this.visitor = new ArrayList<>();
             this.score = 0;
+        }
+
+        public boolean isFriend(String name){
+            if (friends.contains(name)){
+                return true;
+            }
+            return false;
         }
 
         public void addFriend(String name){
@@ -23,20 +32,56 @@ public class Problem7 {
         public void setVisitor(List<String> visitor) {
             this.visitor = visitor;
         }
+
+        public List<String> getFriends() {
+            return friends;
+        }
+
     }
 
     public static Map<String, User> repository = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, Integer> newFriendMap = new HashMap<>();
 
         initFriend(friends);
 
+
         User userInstance = repository.get(user);
-        userInstance.setVisitor(visitors);
+        userInstance.setVisitor(visitors); // init visitor
+
+        setSameFriendScore(user, newFriendMap);
+
 
         return answer;
     }
+
+    private static void setVisitorScore(String user, Map<String, Integer> newFriendMap){
+
+    }
+
+    private static void setSameFriendScore(String user, Map<String, Integer> newFriendMap) {
+        User userInstance = repository.get(user);
+        repository.forEach((name, userObj) -> {
+            if (!name.equals(user) && !userObj.isFriend(name)){
+                newFriendMap.put(name, getSameFriendCount(userInstance, userObj) * 10);
+            }
+        });
+    }
+
+    private static int getSameFriendCount(User userA, User userB){
+        List<String> userAFriends = userA.getFriends();
+        List<String> userBFriends = userB.getFriends();
+        List<String> mergeFriends = new ArrayList<>();
+
+        mergeFriends.addAll(userAFriends);
+        mergeFriends.addAll(userBFriends);
+        Set<String> nameSet = new HashSet<>(mergeFriends);
+        return mergeFriends.size() - nameSet.size();
+    }
+
+    private static setFriendScore()
 
     private static void initFriend(List<List<String>> friends) {
         for (List<String> friend : friends){
