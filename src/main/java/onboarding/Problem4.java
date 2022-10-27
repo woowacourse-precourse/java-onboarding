7e9d63a001b065141bf34196145c2654cd0031b1
lecocococo <chemwang13@naver.com>
2022-10-28 01:17:44 +0900

    Feat: Problem4의 반대로된 알파벳을 저장하는 dictionary 생성 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+	public static Map<Character, Character> createFrogDictionary() {
+		Map<Character, Character> frogDictionary = new HashMap<>();
+		int tempNumber = 25;
+
+		for(int i = 65; i < 91; i++) {
+			frogDictionary.put((char)i, (char)(i + tempNumber));
+			frogDictionary.put((char)(i + 32), (char)(i + 32 + tempNumber));
+			tempNumber -= 2;
+		}
+		return frogDictionary;
+	}
+
     public static String solution(String word) {
         String answer = "";
-        return answer;
+		return answer;
     }
 }
