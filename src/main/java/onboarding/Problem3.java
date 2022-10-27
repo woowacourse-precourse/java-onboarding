68ffb79254a9e9de67d019f1b7003cb2ed2c6070
Taewoon Kim <xodns98@gmail.com>
2022-10-27 15:12:12 +0900

    문제 3 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; ++i) {
+            answer += getThreeSixNineRecursive(i);
+        }
         return answer;
+    }
+
+    private static int getThreeSixNineRecursive(int num) {
+        if (num == 0) {
+            return 0;
+        }
+
+        int value = num % 10;
+        int result = 0;
+
+        if (value == 3 || value == 6 || value == 9) {
+            result = 1;
+        }
+
+        num /= 10;
+
+        return result + getThreeSixNineRecursive(num);
     }
 }
