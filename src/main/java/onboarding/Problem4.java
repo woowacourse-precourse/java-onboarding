3dfbdf16bb047e77e64c203a498ecc3c0f59e3b1
juno-junho <ssmm0205@naver.com>
2022-10-27 22:55:33 +0900

    feat(Problem4): create convertWithDictionary method
    청개구리 사전을 참고해 반대로 변환해 주는 convertWithDictionary 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,18 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+
         StringBuilder sb = new StringBuilder(word.length());
         for (char letter : word.toCharArray()) {
-            if (Character.isUpperCase(letter)) {
-                sb.append((char) ('Z' - (letter - 'A')));
-                continue;
-            }
-            if (Character.isLowerCase(letter)) {
-                sb.append((char) ('z' - (letter - 'a')));
-                continue;
-            }
-            sb.append(letter);
+            sb.append(convertWithDictionary(letter));
         }
         return sb.toString();
+    }
+
+    static char convertWithDictionary(char letter) {
+        if (Character.isUpperCase(letter)) {
+            return (char) ('Z' - (letter - 'A'));
+        }
+        if (Character.isLowerCase(letter)) {
+            return (char) ('z' - (letter - 'a'));
+        }
+        return letter;
     }
 }
