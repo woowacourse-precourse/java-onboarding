8e5fc6fb2b88848c4ccf245f960a38674d7e4e71
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-27 22:16:20 +0900

    Modify: Problem1의 메서드 목록 주석으로 추가
    - -1을 리턴하는 예외 시에 고려해야 할 사항 추가
    - solution 메서드의 flow 작성
    - 게임참여자의 점수를 반환하는 private static int getScore(List<Integer> pages) 함수 선언 후 flow 주석 작성
        solution 메서드 내부에서만 사용하므로 private static으로 선언
    - 책 페이지의 각 자리수는 서로 합이나 곱으로 사용되므로 순서가 중요하지 않고, 각 자리수를 개별 숫자로 다룰 수 있어야 하므로, 책 페이지의 각 자리수를 분리해주는 private static List<Integer> pageNumToList(int pageNum) 함수 프로토타입 선언
    - List<Integer>로 전달되는 책 페이지의 유효성을 검사하는 private static boolean isException(List<Integer> pages) 함수 프로토타입 선언
    TODO: isException() 내부에서 각 예외 사항을 체크할 함수를 따로 정의해야 하는지 고민해볼 것
    
    Fixes: Porblem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,58 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    /*
+    두 게임 참여자의 pages를 List<Integer>로 받아 승자를 return하는 메서드
+    - 예외 사항
+        1. pages List의 요소 수는 2개
+        2. pages List의 Integer item은 연속된 두 수
+        3. pages의 두 수 중 왼쪽의 수는 항상 홀수, 오른쪽의 수는 항상 짝수
+        4. pages 경우의 수에 시작 면(1, 2)과 마지막 면(399, 400)은 포함X
+        5. 따라서 pages List item의 범위는 3~398
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 예외 사항 체크. boolean isException(List<Integer> pages)
+
+        // pobi의 점수 구하기. int getScore(List<Integer> pages)
+        // crong의 점수 구하기. int getScore(List<Integer> pages)
+
+        // pobi와 crong의 점수 비교
+         // pobi win
+         // crong win
+         // 무승부 발생
+
         return answer;
     }
+
+    /*
+    게임 참여자의 점수를 계산하는 메서드
+    List<Integer> 객체를 매개변수로 받음.
+    두 요소(숫자)의 각 자리 합과 각 자리 곱을 계산하여 리스트에 저장
+    해당 리스트의 요소 중 가장 큰 숫자를 반환
+     */
+    private static int getScore(List<Integer> pages) {
+        int score = null;
+
+        // 각 페이지 숫자의 자리 합과 곱(4개)을 저장할 배열 scores 선언
+        // pageList 순회하며 배열 scores에 요소의 합과 곱을 저장. List<Integer> pageNumToList(int pageNum)
+        // 배열 scores 요소 중 가장 큰 값을 score에 저장
+
+        return score
+    }
+    
+    /*
+    특정 페이지 번호의 각 자리 숫자를 리스트에 저장하여 반환하는 메서드
+    각 숫자를 더하거나 곱하므로 리스트 배치 순서를 고려하지 않아도 됨
+     */
+    private static List<Integer> pageNumToList(int pageNum);
+
+    /*
+    매개변수로 받은 List<Integer>의 예외를 체크하는 메서드
+    예외 존재 시 true, 존재하지 않을 시 false
+     */
+    private static boolean isException(List<Integer> pages);
+
 }
