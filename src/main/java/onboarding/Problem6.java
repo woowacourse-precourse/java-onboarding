ce5997cc343ab33c47e392753b95ffd204f90f23
JHS <wnghdtjr129@naver.com>
2022-10-27 19:00:23 +0900

    feat(Problem6): [문제 6] 결과값 오름차순 정렬

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,35 +1,35 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     static List<String> answer = new ArrayList<>();
     static Set<String> nicknames = new HashSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
         for (List<String> crew : forms) {
-            if (cannotUser(crew.get(1))) {
-                answer.add(crew.get(0));
-            }
+            addAnswer(crew);
         }
-        List<String> firstCrew = forms.get(0);
-        if (cannotUser(firstCrew.get(1))) {
-            answer.add(firstCrew.get(0));
-        }
+        addAnswer(forms.get(0));
+        answer.sort(Comparator.comparing(o -> o.substring(0, o.indexOf("@"))));
         return answer;
+    }
+
+    private static void addAnswer(List<String> crew) {
+        if (cannotUser(crew.get(1))) {
+            answer.add(crew.get(0));
+        }
     }
 
     private static boolean cannotUser(String nickname) {
         boolean result = false;
         for (int i = 0; i < nickname.length() - 1; i++) {
-            for (int j = i + 2; j < nickname.length(); j++) {
+            for (int j = i + 2; j <= nickname.length(); j++) {
                 String subNickname = nickname.substring(i, j);
                 if (nicknames.contains(subNickname)) {
                     result = true;
                 }
-                nicknames.add(nickname);
+                nicknames.add(subNickname);
             }
         }
         return result;
