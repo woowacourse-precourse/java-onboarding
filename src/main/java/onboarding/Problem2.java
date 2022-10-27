20a8380ac05c02cf192065aad5ff5cd77cbd1012
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-27 12:16:32 +0900

    문제 2 - 배열 내 인접 중복 문자열 존재 여부 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,12 +12,24 @@ public class Problem2 {
 			return cryptogram;
 		}
 		// 배열 내 인접 중복 문자열 존재 여부 확인
+		if(!isDuplicate(chars)) {
+			return cryptogram;
+		}
 		// 인접한 중복 문자열 제거
 		// stack을 문자열로 변환
 		// 해독할때까지 재귀
 
 		String answer = "answer";
 		return answer;
+	}
+	static boolean isDuplicate(char[] arr) {
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] == arr[i + 1]) {
+				return true;
+			}
+		}
+		return false;
 	}
 	static boolean isLengthEnough(char[] arr) {
 		return arr.length < 2;
