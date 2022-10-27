8cdfffece0b986475543c4e89cb73d6aee3fee09
seokjin <mm8678@g.yju.ac.kr>
2022-10-28 02:02:05 +0900

    feat(Problem7.java): visitors 배열의 아이디에 대한 점수를 계산하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -17,6 +17,15 @@ public class Problem7 {
 				recommendFriends.put(friend.get(1), 10);
 			}
 		}
+
+		for (String visitor : visitors) {
+			if (recommendFriends.containsKey(visitor)) {
+				recommendFriends.put(visitor, recommendFriends.get(visitor) + 1);
+			} else {
+				recommendFriends.put(visitor, 1);
+			}
+		}
+
 		System.out.println(recommendFriends);
 
 		return Collections.emptyList();
