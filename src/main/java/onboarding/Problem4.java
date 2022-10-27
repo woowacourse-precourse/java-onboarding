bc803063d994cf6df445ddf999ab6268dd618798
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 14:23:28 +0900

    문제 4 : 풀이방법

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/**
+ * [풀이 방법]
+ * 대문자와 소문자의 경우에 따라 아스키코드 값을 조절하여 변경해준다.
+ * - 대문자 일 경우 -> 'Z'에서 현재 단어를 빼준 뒤, 'A'+1 즉 65만큼 더해준다
+ * - 소문자 일 경우 -> 'z'에서 현재 단어를 빼준 뒤, 'a'+1 즉 97만큼 더해준다
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
