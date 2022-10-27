f64f68d2512549cb535ba18ae9e038d4c8158726
UJ15 <dbwogml15@naver.com>
2022-10-27 13:15:02 +0900

    feat : add friend list map create method Problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String, List<String>> friendsMap;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        friendsMap = new HashMap<>();
+
+        friends.forEach(Problem7::addFriendList);
+
+
+        return Collections.emptyList();
+    }
+
+    private static void addFriendList(List<String> friend) {
+        int flag = -1;
+
+        for (int i = 0 ; i < 2 ; i++) {
+            if (!friendsMap.containsKey(friend.get(i))) {
+                friendsMap.put(friend.get(i), new ArrayList<>());
+            }
+
+            flag *= -1;
+            friendsMap.get(friend.get(i)).add(friend.get(i + flag));
+        }
     }
 }
