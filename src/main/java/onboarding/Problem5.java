a58f8ae4a8148db3ac1e943375500da9361de03e
H4R1B0 <qkrguswns25@naver.com>
2022-10-27 21:14:47 +0900

    solution 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,8 +9,13 @@ public class Problem5 {
     private static int[] units = {50000,10000,5000,1000,500,100,50,10,1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(); //List 초기화
 
+        for(int i=0;i<units.length;i++){
+            int count = getCount(money, i);
+            answer.add(count);
+            money -= count*units[i];
+        }
         return answer;
     }
 
