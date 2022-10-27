9c8d08127c4e3540eeed458db15bc57f3709b4cd
5nam <ohnam90@gmail.com>
2022-10-27 22:24:34 +0900

    feat: String 을 ArrayList 로 변환하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    // string -> arrayList 로 변환
+    private static ArrayList<String> changeString(String input) {
+        String[] strArr = input.split("");
+        ArrayList<String> output = new ArrayList<String>(Arrays.asList(strArr));
+        return output;
     }
 }
