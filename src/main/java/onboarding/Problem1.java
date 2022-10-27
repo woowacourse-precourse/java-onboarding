9a14e189940c3632125b3c216793b89bb21c0c83
맹순영 <symaeng98@naver.com>
2022-10-27 19:54:20 +0900

    feat: 유효성 확인 및 결과 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,11 @@ class Problem1 {
         int crongLeft = crong.get(0);
         int crongRight = crong.get(1);
 
+        // ----- 유효성 확인 -----
+        if(pobiRight-pobiLeft != 1 || crongRight-crongLeft != 1){
+            return -1;
+        }
+
         // ----- 포비의 점수 계산 -----
         // 포비의 페이지 합 계산
         int pobiSum = pobiLeft + pobiRight;
@@ -25,6 +30,12 @@ class Problem1 {
         // 크롱의 점수 계산
         int crongScore = Math.max(crongSum, crongMul);
 
+        // ----- 결과 계산 -----
+        // 포비와 크롱의 점수가 같다면,
+        if (pobiScore == crongScore) return 0;
+        // 포비의 점수가 더 크다면,
+        if (pobiScore > crongScore) return 1;
+        // 포비의 점수가 더 작다면,
         return 2;
     }
 }
