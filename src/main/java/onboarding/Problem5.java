e7afc6c961e54483b5574db22786ea1c0434898e
greeng00se <thegreengoose7@gmail.com>
2022-10-27 18:10:15 +0900

    feat(Problem5): 반환해야할 지폐/동전을 표현하는 enum 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return new ArrayList<>();
+    }
+
+    enum Money {
+        _50000_WON(50000),
+        _10000_WON(10000),
+        _5000_WON(5000),
+        _1000_WON(1000),
+        _500_WON(500),
+        _100_WON(100),
+        _50_WON(50),
+        _10_WON(10),
+        _1_WON(1);
+
+        int value;
+
+        Money(int value) {
+            this.value = value;
+        }
     }
 }
