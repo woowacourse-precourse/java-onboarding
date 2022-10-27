6e8a09633515801a68c38b246c92b11b70fd9b02
HwangSeokJin <ghkd3703@gmail.com>
2022-10-28 02:01:44 +0900

    중복된 글자가 있는 닉네임을 신청한 크루를 찾는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,39 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Problem6 {
     private static HashMap<Character, HashSet<Character>> nameMap = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> validCrewInfo = new LinkedList<>(); // info = name/email
+
+        for (List<String> crew: forms){
+            String email = crew.get(0), name = crew.get(1);
+            String dSentence = getDuplicateSentenceOrBlank(name);
+
+            if (dSentence != ""){
+                break;
+            }
+
+            validCrewInfo.add(name + "/" + email);
+        }
         return answer;
+    }
+
+    private static List<String> getInvalidCrews(List<String> validCrewInfo, String dSentence){
+        List<String> invalidCrews = new ArrayList<>();
+        for (String info: validCrewInfo){
+            String name = info.split("/")[0];
+            if (name.contains(dSentence)){
+                invalidCrews.add(info);
+            }
+        }
+        return invalidCrews;
     }
 
     private static String getDuplicateSentenceOrBlank(String name) {
