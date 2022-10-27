9469e47e767df74b9db5421c4c706a0c24fb5225
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-27 14:53:17 +0900

    Comment : modify method name 기능 목록 주석 4번의 메서드 명을 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,7 +8,7 @@ class Problem1 {
     * 1. 각 자리 숫자를 더하는 메서드 sumValue
     * 2. 각 자리 숫자를 곱하는 메서드 mulValue
     * 3. 왼쪽 페이지와 오른쪽 페이지의 각 자리 숫자를 더하고 곱하고 비교하여 가장 큰수를 리턴하는 메서드 getMaxValue
-    * 4. 점수를 비교하여 승자를 정하는 메서드 compare
+    * 4. 점수를 비교하여 승자를 정하는 메서드 whoIsWinner
     * */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
