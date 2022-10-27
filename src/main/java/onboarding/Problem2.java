9a05d0e7b5f802a5e9a8f03c05a6ca0e5d5cee60
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-27 14:31:38 +0900

    feat: [Problem2] - 요구사항들을 메소드로 분리 및 while문 무한루프가 아닌 재귀적 메소드를 통해 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,30 +13,34 @@ public class Problem2 {
         List<String> splitString = Arrays.stream(cryptogram.split("")).collect(Collectors.toList());
         List<Integer> indexOfDuplicateWords = new ArrayList<>();
 
-        while (true) {
-
-            for (int i = 0; i < splitString.size() - 1; i++) {
-                if (splitString.get(i).equals(splitString.get(i + 1))) {
-                    indexOfDuplicateWords.add(i);
-                }
-            }
-
-            if (indexOfDuplicateWords.size() == 0) {
-                break;
-            }
-
-            indexOfDuplicateWords.sort(Collections.reverseOrder());
-
-            for (Integer idx : indexOfDuplicateWords) {
-                splitString.subList(idx, idx + 2).clear();
-            }
-            indexOfDuplicateWords.clear();
-        }
+        getDuplicateWords(splitString, indexOfDuplicateWords);
+        removeDuplicateWords(splitString, indexOfDuplicateWords);
 
         return String.join("", splitString);
-
     }
 
+    private static void getDuplicateWords(List<String> splitString, List<Integer> indexOfDuplicateWords) {
+        for (int i = 0; i < splitString.size() - 1; i++) {
+            if (splitString.get(i).equals(splitString.get(i + 1))) {
+                indexOfDuplicateWords.add(i);
+            }
+        }
+    }
 
+    private static void removeDuplicateWords(List<String> splitString, List<Integer> indexOfDuplicateWords) {
+        if (indexOfDuplicateWords.size() == 0) {
+            return;
+        }
+        indexOfDuplicateWords.sort(Collections.reverseOrder());
+
+        for (Integer idx : indexOfDuplicateWords) {
+            splitString.subList(idx, idx + 2).clear();
+        }
+        indexOfDuplicateWords.clear();
+        
+        getDuplicateWords(splitString, indexOfDuplicateWords);
+        removeDuplicateWords(splitString, indexOfDuplicateWords);
+
+    }
 
 }
