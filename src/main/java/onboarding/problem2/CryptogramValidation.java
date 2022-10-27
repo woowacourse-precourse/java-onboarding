a9bb8e4000fa4b4d54bd0b806ba3a11df8b7ecfb
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 17:05:04 +0900

    feat(P2): 검증 로직 구현

--- src/main/java/onboarding/problem2/CryptogramValidation.java
+++ src/main/java/onboarding/problem2/CryptogramValidation.java
@@ -1,4 +1,21 @@
 package onboarding.problem2;
 
 public class CryptogramValidation {
+
+    public static boolean isValidCryptogram(String cryptogram) {
+        return isValidLength(cryptogram) || isOnlyContainsLowerCase(cryptogram);
+    }
+
+    private static boolean isValidLength(String cryptogram) {
+        final int MAX_VALUE = 1000;
+        final int MIN_VALUE = 1;
+        int length = cryptogram.length();
+
+        return MIN_VALUE <= length && length <= MAX_VALUE;
+
+    }
+
+    private static boolean isOnlyContainsLowerCase(String cryptogram) {
+        return cryptogram.matches("^[a-z]*$");  // 소문자만 있을 때 true를 반환하는 정규식이라고 한다.
+    }
 }
