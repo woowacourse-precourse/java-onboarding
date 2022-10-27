1c79902c86481b671ec79e46557b6e1aa6eb9ca1
metorg <metrog950@gmail.com>
2022-10-27 10:16:06 +0900

    feat: 왼쪽 페이지 체크 메서드 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,10 +2,25 @@ package onboarding;
 
 import java.util.List;
 
+
+
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        System.out.println("git test");
         return answer;
     }
+    
+    static int leftPageCheck(int page){
+        int MAX_PAGE_ADD =0;
+        int MAX_PAGE_MUL =1;
+
+        while(page!=0){
+            MAX_PAGE_ADD+=page%10;
+            MAX_PAGE_MUL*=page%10;
+            page/=10;
+        }
+        return Math.max(MAX_PAGE_ADD,MAX_PAGE_MUL);
+    }
+
 }
