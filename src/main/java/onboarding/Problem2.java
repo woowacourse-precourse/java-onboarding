32601fe0a50021933758cb1d96eb307a99332574
leesky <leehaneul0623@gmail.com>
2022-10-27 23:10:28 +0900

    feat: 스택 활용, 암호화 해독 솔루션 구현(문제 2)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < cryptogram.length(); i++) {
+            char now = cryptogram.charAt(i);
+            if(stack.isEmpty()) stack.push(now);
+            else {
+                char top = stack.pop();
+                if(top == now) continue;
+                stack.push(top);
+                stack.push(now);
+            }
+        }
+
+        Iterator<Character> iterator = stack.listIterator();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while(iterator.hasNext()) {
+            stringBuilder.append(iterator.next());
+        }
+        return stringBuilder.toString();
     }
 }
