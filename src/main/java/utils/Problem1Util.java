77dc24e461b9506a5cf19bab250e0638bf1f1ee5
HubCreator <chocomilk8604@gmail.com>
2022-10-27 10:51:09 +0900

    feat(Problem1) : 두 숫자에 대한 유효성 로직 추가 및 결과값 반환 로직 구현

--- src/main/java/utils/Problem1Util.java
+++ src/main/java/utils/Problem1Util.java
@@ -4,11 +4,25 @@ import java.util.List;
 
 public class Problem1Util {
     public static int getMaxBetweenSumAndMultiply(List<Integer> nums) {
+        if (!isValid(nums)) return -1;
         int result = Integer.MIN_VALUE;
         for (Integer num : nums) {
             result = isMax(result, num);
         }
         return result;
+    }
+
+    private static boolean isValid(List<Integer> nums) {
+        if (nums.size() != 2 && nums.get(1) - nums.get(0) != 1) {
+            return false; // input되는 숫자가 두 개이면서, 연속된 두 숫자인가
+        }
+        for (int i = 0; i < 2; i++) {
+            if (nums.get(i) % 2 != 1 - i) {
+                return false; // 해당 숫자가 [홀수, 짝수]인가
+            }
+        }
+
+        return true;
     }
 
     private static int isMax(int result, Integer num) {
@@ -22,5 +36,11 @@ public class Problem1Util {
         result = Math.max(result, sum);
         result = Math.max(result, mult);
         return result;
+    }
+
+    public static int whoIsWinner(int pobiResult, int crongResult) {
+        if (pobiResult > crongResult) return 1;
+        else if (pobiResult < crongResult) return -1;
+        return 0;
     }
 }
--- src/test/java/onboarding/Problem1Test.java
+++ src/test/java/onboarding/Problem1Test.java
@@ -16,4 +16,21 @@ class Problem1Test {
         int result = Problem1Util.getMaxBetweenSumAndMultiply(nums);
         assertThat(result).isEqualTo(4);
     }
+
+    @Test
+    void whoIsWinnerTest() {
+        List<Integer> pobi = List.of(97, 98);
+        List<Integer> crong = List.of(197, 198);
+        int pobiResult = Problem1Util.getMaxBetweenSumAndMultiply(pobi);
+        int crongResult = Problem1Util.getMaxBetweenSumAndMultiply(crong);
+        int result = Problem1Util.whoIsWinner(pobiResult, crongResult);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void ex_unValidNum() {
+        List<Integer> pobi = List.of(98, 99);
+        int result = Problem1Util.getMaxBetweenSumAndMultiply(pobi);
+        assertThat(result).isEqualTo(-1);
+    }
 }
