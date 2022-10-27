d64a54a12bb066b4d4fd8bf98e231a52544be0ae
cookienc <kim76744@naver.com>
2022-10-27 22:03:49 +0900

    feat(Problem1): Problem1 예외 처리 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,30 +6,63 @@ class Problem1 {
 
 	public static final int HUNDRED = 100;
 	public static final int SUM_START = 0;
-    public static final int MULTIPLY_START = 1;
+	public static final int MULTIPLY_START = 1;
 	public static final int LEFT_PAGE = 0;
 	public static final int RIGHT_PAGE = 1;
+	public static final int NORMAL_BOOK_SIZE = 2;
 	public static final int TEN = 10;
 
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+		if (!checkBook(pobi, crong)) {
+			return -1;
+		}
 
 		int pobiMaxNumber = getMaxNumber(pobi);
 		int crongMaxNumber = getMaxNumber(crong);
 
-        if (pobiMaxNumber > crongMaxNumber) {
-            return 1;
-        }
+		if (pobiMaxNumber > crongMaxNumber) {
+			return 1;
+		}
 
-        if (pobiMaxNumber < crongMaxNumber) {
-            return 2;
-        }
+		if (pobiMaxNumber < crongMaxNumber) {
+			return 2;
+		}
 
-        return 0;
+		return 0;
+	}
+
+	private static boolean checkBook(List<Integer> pobi, List<Integer> crong) {
+		return isRightBook(pobi) && isRightBook(crong);
+	}
+
+	private static boolean isRightBook(List<Integer> book) {
+		return isRightSize(book) && isRightOrder(book);
+	}
+
+	private static boolean isRightSize(List<Integer> book) {
+		return book.size() == NORMAL_BOOK_SIZE &&
+				0 <= leftPage(book) &&
+				rightPage(book) <= 400;
+	}
+
+	private static Integer rightPage(List<Integer> book) {
+		return book.get(RIGHT_PAGE);
+	}
+
+	private static Integer leftPage(List<Integer> book) {
+		return book.get(LEFT_PAGE);
+	}
+
+	private static boolean isRightOrder(List<Integer> book) {
+		return leftPage(book) < rightPage(book) &&
+				leftPage(book) % 2 == 1 &&
+				rightPage(book) % 2 == 0;
 	}
 
 	private static int getMaxNumber(List<Integer> book) {
-		int leftPageMax = getMaxValue(book.get(LEFT_PAGE));
-		int rightPageMax = getMaxValue(book.get(RIGHT_PAGE));
+		int leftPageMax = getMaxValue(leftPage(book));
+		int rightPageMax = getMaxValue(rightPage(book));
 
 		return Math.max(leftPageMax, rightPageMax);
 	}
