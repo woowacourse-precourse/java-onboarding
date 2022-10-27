26b73eb69fd30a5a02cfa5cb7893a19033f827a8
hyun-seung <ardimento22@naver.com>
2022-10-27 16:29:34 +0900

    [FEAT] 돈의 단위를 저장한다. (큰 순서부터)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    public static final int[] moneyUnitList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        
+
         return answer;
     }
 }
