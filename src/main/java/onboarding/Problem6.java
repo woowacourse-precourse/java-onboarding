c8723bfa597f8d8bbf271f84218028c739b4dfec
안광민 <creativeduck0518@gmail.com>
2022-10-27 21:31:02 +0900

    feat(Problem 6): 크루 이름에 해당하는 인덱스 배열에서 각 크루 이름에 중복 문자가 있는지 여부를 나타내는 boolean 배열 만들어주는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    private static final int EMAIL = 0;
+    private static final int NICKNAME = 1;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
@@ -20,5 +23,27 @@ public class Problem6 {
         }
 
         return false;
+    }
+
+    private static boolean[] makeDuplicatedIndexes(List<List<String>> forms) {
+        boolean[] duplicatedIndexes = new boolean[forms.size()];
+
+        for (int i = 0; i < forms.size(); i++) {
+            String subject = forms.get(i).get(NICKNAME);
+
+            for (int j = 0; j < forms.size(); j++) {
+                String target = forms.get(j).get(NICKNAME);
+
+                if (target.equals(subject))
+                    continue;
+
+                if (isDuplicate(subject, target)) {
+                    duplicatedIndexes[i] = true;
+                    duplicatedIndexes[j] = true;
+                }
+            }
+        }
+
+        return duplicatedIndexes;
     }
 }
