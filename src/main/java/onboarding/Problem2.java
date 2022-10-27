ed7817f84c6c902b5d2c0bd294c73a7231fdd716
kokodak <kokodakadokok@gmail.com>
2022-10-27 14:59:30 +0900

    feat: [문제2] 삭제할 중복 문자가 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,14 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static boolean canMoreDelete(String cryptogram) {
+        if (cryptogram.length() == 0 || cryptogram.length() == 1) return false;
+
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) return true;
+        }
+
+        return false;
+    }
 }
