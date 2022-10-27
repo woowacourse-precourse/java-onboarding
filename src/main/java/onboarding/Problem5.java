b3333415fe7dd4e2a4accc979454ffc508a98206
Beginner <dkstmdwn17@naver.com>
2022-10-27 22:32:48 +0900

    feet : P5 기능 요구사항과 50000~1까지 단위를 정의하는 부분 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+/**
+ * 기능 요구사항
+ * 1. 50000 ~ 1까지 단위를 정의해야 한다.
+ * 2. 주어진 돈을 각 각의 단위로 몇 개씩 변환되는지 알 수 있어야 한다.
+ */
 public class Problem5 {
+    static int [] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
         return answer;
     }
 }
