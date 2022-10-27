c1c4a363d4efa5587d4e4aeb5bc43e4f8160e829
keon0711 <fnc6488@gmail.com>
2022-10-27 22:23:50 +0900

    [Problem2] 해당 문자가 중복문자가 아닌지를 확인하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return "a";
     }
+
+    private static boolean isSolitary(String cryptogram, int index) {
+        if (index == 0)
+            return cryptogram.charAt(index) != cryptogram.charAt(index + 1);
+        if (index == cryptogram.length() - 1)
+            return cryptogram.charAt(index) != cryptogram.charAt(index - 1);
+        if (cryptogram.charAt(index) != cryptogram.charAt(index-1)
+                && cryptogram.charAt(index) != cryptogram.charAt(index + 1))
+            return true;
+        return false;
+    }
+
 }
