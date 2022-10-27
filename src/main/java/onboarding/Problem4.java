4569b1544d4865d3c536d093ecbdc022a3905560
ygreenb <nhj7911@gmail.com>
2022-10-28 06:30:06 +0900

    feat: Problem4.1 알파벳 청개구리 사전 기반으로 변환해 return

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return reverseAlphabet(word);
+    }
+
+    public static String reverseAlphabet(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if('A' < c && c < 'M'){
+                c = (char)('Z' - (c  - 'A'));
+            }else if('N' < c && c < 'Z'){
+                c = (char)('A'+('Z' - c));
+            }else if('a' < c && c < 'm'){
+                c = (char)('z' - (c  - 'a'));
+            }else if('n' < c && c < 'z'){
+                c = (char)('a'+('z' - c));
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
