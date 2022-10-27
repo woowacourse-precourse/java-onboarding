57be8c3582d279c19135c208938f2f6611a52736
juhwang <juhong4930@naver.com>
2022-10-27 23:59:42 +0900

    feat(Problem6):cutting two words from nickName

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,16 +1,27 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
     }
-    public static List<String> extractNickName(List<List<String>> forms){
-        List<String> nickNameList = new ArrayList<>();
-        forms.forEach(form -> nickNameList.add(form.get(1)));
+    private static List<List<String>> extractNickName(List<List<String>> forms){
+        List<List<String>> nickNameList = new ArrayList<>();
+        forms.forEach(form -> nickNameList.add(cutNickName(form.get(1))));
         return nickNameList;
     }
+    private static List<String> cutNickName(String nickName){
+        List<String> TwoNickNameString = new ArrayList<>();
+        for (int i = 0; i < nickName.length()-1; i++) {
+            TwoNickNameString.add(nickName.substring(i,i+2));
+        }
+        return TwoNickNameString;
+    }
+
 }
