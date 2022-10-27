6c92cceebc8a0f22be45153dd1eb4e44b44f3a06
woo-chang <clllickme@naver.com>
2022-10-27 18:48:39 +0900

    feat: 주어진 숫자까지 게임 숫자가 포함된 개수 추출 기능 (Problem3)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,49 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+  enum GameNumber {
+    THREE(3), SIX(6), NINE(9);
+
+    GameNumber(int value) {
+      this.value = value;
     }
+
+    private final int value;
+
+    public int getValue() {
+      return this.value;
+    }
+  }
+
+  public static int solution(int number) {
+    return getGameNumberCount(number);
+  }
+
+  private static int getGameNumberCount(int number) {
+    int targetPosLeftNumber = 1;
+    int targetPosRightNumber, targetPosNumber;
+    int digit = 1;
+
+    int result = 0;
+    while (targetPosLeftNumber != 0) {
+      targetPosLeftNumber = (number / digit) / 10;
+      targetPosRightNumber = number % digit;
+      targetPosNumber = (number / digit) % 10;
+
+      for (GameNumber gameNumber : GameNumber.values()) {
+        if (targetPosNumber > gameNumber.getValue()) {
+          result += (targetPosLeftNumber + 1) * digit;
+        } else if (targetPosNumber < gameNumber.getValue()) {
+          result += targetPosLeftNumber * digit;
+        } else {
+          result += targetPosLeftNumber * digit + (targetPosRightNumber + 1);
+        }
+      }
+
+      digit *= 10;
+    }
+    return result;
+  }
+
 }
