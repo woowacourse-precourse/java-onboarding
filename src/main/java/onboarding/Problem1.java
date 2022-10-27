5b0ab482b7c95c10db38218f37d326219223b003
sen <seeunchoi99@gmail.com>
2022-10-27 13:18:58 +0900

    💡 Problem1 기능 목록 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 입력값이 유효한 지 검사하는 함수
+ * 2. 페이지 번호의 각 자리 숫자를 연산 해 가장 큰 수를 구하는 함수
+ * 3. 본인의 점수(2번의 결과값들 중 가장 큰 수)를 구하는 함수
+ */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
