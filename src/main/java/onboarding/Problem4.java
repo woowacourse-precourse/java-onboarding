3ef6227aad4568f380f4dcb5b6516246ea3e82d0
apptie <appti@naver.com>
2022-10-27 12:30:54 +0900

    feat: 문제 4 문자열을 변환하는 calculateWord 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,8 +7,28 @@ public class Problem4 {
         return answer;
     }
 
+    private static String calculateWord(String word) {
+        char[] chars = word.toCharArray();
+        char standardA;
+        char standardZ;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (!isAlphabet(chars[i])) {
+                continue;
+            }
+            standardA = 'a';
+            standardZ = 'z';
+            if (isUpperCase(chars[i])) {
+                standardA = 'A';
+                standardZ = 'Z';
+            }
+            chars[i] = calculateCharacter(standardA, standardZ, chars[i]);
+        }
+        return String.valueOf(chars);
+    }
+
     private static char calculateCharacter(char standardA, char standardZ, char target) {
-        return (char)(standardA + (standardZ - target));
+        return (char) (standardA + (standardZ - target));
     }
 
     private static boolean isAlphabet(char target) {
