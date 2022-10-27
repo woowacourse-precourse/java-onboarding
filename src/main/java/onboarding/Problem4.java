a51bee6f8168f538b55f31a7b8da253fccb134bd
TaeMin <xoals3094@naver.com>
2022-10-28 03:52:13 +0900

    no.4 chung_frog_language translate method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static char chung_frog_lang(char c){
+        char chung_lang = 0; // 0이 리턴되면 예외값
+        if(c >= 'a' && c <= 'z'){
+            chung_lang = (char)('z' - c + 'a');
+        }
+        else if(c >= 'A' && c <= 'Z'){
+            chung_lang = (char)('Z' - c + 'A');
+        }
+
+        return chung_lang;
+    }
 }
