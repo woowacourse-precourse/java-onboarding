88a02d3e6dc379ca4188d966e599622c7c2db7ac
EUNJEONGMUN <munej26@gmail.com>
2022-10-27 16:36:24 +0900

    feat: 페이지 번호 각 자리 수 모두 더하기 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
+    static final int LEFT = 0;
+    static final int RIGHT = 1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int getPlus(Integer pageNumber) {
+        int value = 0;
+        while(pageNumber>0) {
+            value += pageNumber%10;
+            pageNumber /= 10;
+        }
+        return value;
     }
 }
