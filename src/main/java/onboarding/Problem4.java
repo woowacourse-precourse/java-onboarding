baee5e6d63ae73d3017317d1f0f4eaf151243b7b
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-10-27 12:39:00 +0900

    refactor(Problem4) : 알파벳 유효성 검사 추가, 기능 구현 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,6 @@ import onboarding.domain.problem4.Frog;
 public class Problem4 {
     public static String solution(String word) {
         Frog frog = new Frog(word);
-        return answer;
+        return frog.say();
     }
 }
