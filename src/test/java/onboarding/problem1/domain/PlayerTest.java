6668737cb3080225ee84ff3db7fe7885b07c918a
JunH <tjdtls690@gmail.com>
2022-10-27 12:05:41 +0900

    :art: refactor : 필요없는 Player 클래스 삭제

--- src/test/java/onboarding/problem1/domain/PlayerTest.java
+++ src/test/java/onboarding/problem1/domain/PlayerTest.java
@@ -7,9 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest {
     @Test
-    @DisplayName("플레이어의 가장 큰 더하기 숫자 구하기")
-    void getMaxSumOfPlayer() {
+    @DisplayName("가장 큰 더하기 숫자 구하기")
+    void getMaxSum() {
         Player player = new Player(237, 238);
-        assertThat(player.getMaxSumOfPlayer()).isEqualTo(13);
+        assertThat(player.getMaxSum()).isEqualTo(13);
+    }
+    
+    @Test
+    @DisplayName("가장 큰 곱하기 숫자 구하기")
+    void getMaxMultiply() {
+        Player player = new Player(237, 238);
+        assertThat(player.getMaxMultiply()).isEqualTo(48);
     }
 }
