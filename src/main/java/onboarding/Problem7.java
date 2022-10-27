b8b1c4224dd595c29686b39f85a9943e1ac00439
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-27 23:55:05 +0900

    feat: user 친구들 조회 - 구현 목록 최신화

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+  static final int FRIEND_SCORE = 10;
+  static final int VISIT_SCORE = 1;
+
+  public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+    List<String> friendsByUser = findFriendsByUser(user, friends);
+    return List.of("");
+  }
+
+
+  private static List<String> findFriendsByUser(String user, List<List<String>> friends) {
+    List<String> userFriends = new ArrayList<>();
+    for (List<String> friendList : friends) {
+      if (!friendList.contains(user)) {
+        continue;
+      }
+      String friend = friendList.stream()
+              .filter(name -> name != user)
+              .findFirst()
+              .get();
+      userFriends.add(friend);
     }
+    return userFriends;
+  }
 }
