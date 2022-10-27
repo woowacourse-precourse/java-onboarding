926f5acbf58ab27f43ef3d32180fe022dfabd28b
Minjune <minjune8506@gmail.com>
2022-10-27 18:58:43 +0900

    feat(Problem2): Problem2 solution 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static int getDuplicateStartPoint(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int start = getDuplicateStartPoint(cryptogram);
+        if (start < 0) {
+            return cryptogram; // 중복된 부분이 없으면 종료
+        }
+        int end = start;
+        while (end < cryptogram.length()) {
+            if (cryptogram.charAt(start) == cryptogram.charAt(end)) {
+                end++;
+                continue;
+            }
+            break;
+        }
+        String temp = cryptogram.substring(0, start) + cryptogram.substring(end);
+        return solution(temp);
     }
 }
