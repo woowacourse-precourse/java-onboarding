163ddd664d14b1d27bd18640a60171c7fa1a8ec4
dev_kong <ahdjd5@gmail.com>
2022-10-27 13:27:06 +0900

    feat(Problem6): add takeApartNikcname method to take apart nickname

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,46 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            String nickName = forms.get(i).get(1);
+            List<String> partOfNicknameList = takeApartNickname(nickName);
+            boolean confusing = checkConfusing(partOfNicknameList, i, forms);
+            if (confusing) {
+                set.add(forms.get(i).get(0));
+            }
+        }
+        List<String> answer = new ArrayList<>(set);
+        Collections.sort(answer);
         return answer;
+    }
+
+    private static List<String> takeApartNickname(String nickname) {
+        List<String> partOfNicknameList = new ArrayList<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            partOfNicknameList.add(nickname.substring(i, i + 2));
+        }
+        return partOfNicknameList;
+    }
+
+    private static boolean checkConfusing(List<String> partList, int curIndex, List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            if (i == curIndex) {
+                continue;
+            }
+
+            String nickname = forms.get(i).get(1);
+            // TODO : indent 2 넘지 않도록 함수 분리
+            for (String part : partList) {
+                if (nickname.contains(part)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
