cd38bb04ad7c7659c334b13dfb04ff903063ed7c
hyeongu <wancern@naver.com>
2022-10-28 01:48:50 +0900

    Problem4. 결과 산출

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,11 +5,17 @@ package onboarding;
 * */
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i<word.length(); i++){
+            char ch = convert(word.charAt(i));
+            sb.append(ch);
+        }
+        return sb.toString();
     }
+
     // 아스키코드 값을 이용하여 반대되는 문자를 구함
-    static char uppercase(char ch){
+    static char convert(char ch){
         if(ch >= 'A' && ch <= 'Z'){
             return (char)('Z' - ch + 'A');
         }
