ac213264a3bf1043492154d6828064720bbba44f
recicoco068 <gkapf838@gmail.com>
2022-10-27 19:23:48 +0900

    feat : Problem1 입력값 유효성 검사 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (validation(pobi) || !validation(crong))
+            return -1;
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static boolean validation(List<Integer> pages) {
+        Integer left = pages.get(0);
+        Integer right = pages.get(1);
+        // 연속된 값이 아니거나, 짝수, 홀수 순인 경우 false
+        if (left + 1 != right || left % 2 == 0)
+            return false;
+        // 왼쪽이 3 ~ 397인 경우만 true
+        return left > 1 && left < 399;
     }
 }
