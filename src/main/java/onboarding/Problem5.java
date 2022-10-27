dbfcbbb2691ba1ade83121db314418ca4929474b
ddubidubap <park62000@gmail.com>
2022-10-28 00:23:34 +0900

    PROBLEM5 기능 요구 사항 목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /*
+        기능 요구 사항 목록
+        1. 변환할 돈의 개수와 나머지를 반환하는 함수
+        2. solution (변환할 돈을 차례대로 입력)
+    */
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
