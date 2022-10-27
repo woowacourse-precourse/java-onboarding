7074e98132e797dd715c307e75d5ffa8df358f3d
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-27 12:57:24 +0900

    feat:exception예외처리기능추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,9 +8,35 @@ class Problem1 {
         int pobi_max = pobiMax(pobi);
         int crong_max = crongMax(crong);
 
+        if(!(exception(crong) && exception(pobi))){
+            answer=-1;
+            return answer;
+        }
 
+        if(pobi_max>crong_max){
+            answer=1;
+        }
+        else if(crong_max<pobi_max){
+            answer=2;
+        }
+        else if (pobi_max == crong_max){
+            answer=0;
+        }
 
         return answer;
+
+    }
+
+    public static boolean exception(List <Integer> judge){ // 예외 처리 기능
+        int left = judge.get(0);
+        int right = judge.get(1);
+        if(Math.abs(right-left)>1){ // 입력으로 연속된 페이지가 아닐 경우 예외 처리
+            return false;
+        }
+        else if(right== 400 || left == 1){ // 펼쳤을때 첫 페이지, 끝 페이지 나올 경우 예외 처리
+            return false;
+        }
+        return true;
     }
 
     public static int pobiMax(List<Integer> pobi){  // pobi의 값중 가장 큰 값 찾기
