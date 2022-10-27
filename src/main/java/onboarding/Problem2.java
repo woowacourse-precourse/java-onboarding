1fbcaf68e19c89356b9efb1ed297ef4ffb268412
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-28 02:23:58 +0900

    feat: Problem_2 연속된 문자를 삭제하는 기능 구현
    다른 문자가 나올 때 까지 문자를 스택에 push하고
    길이가 1을 넘으면 빈 스택을 재할당
    길이가 1이면 리스트에 add

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,59 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Decoder decoder = new Decoder(cryptogram);
+        return  decoder.decode();
+    }
+}
+
+class Decoder {
+    private List<Character> cryptogram;
+
+    Decoder(String cryptogram){
+        this.cryptogram = cryptogram.chars()
+                .mapToObj(ch -> (char)ch)
+                .collect(Collectors.toList());
+    }
+
+    String decode(){
+        while(deleteOverlappingChar()){continue;}
+
+        return cryptogram.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+
+    boolean deleteOverlappingChar(){
+
+        if (cryptogram.size() <= 1){ return false; }
+
+        List<Character> tempCryptogram = new ArrayList<>();
+        Stack<Character> charStack = new Stack<>();
+        charStack.push(cryptogram.get(0));
+
+        for (int i=1; i<cryptogram.size(); i++){
+            if (charStack.peek() != cryptogram.get(i) & charStack.size() == 1){
+                tempCryptogram.add(charStack.pop());
+            } else if (charStack.peek() != cryptogram.get(i)) {
+                charStack = new Stack<>();
+            }
+
+            charStack.push(cryptogram.get(i));
+        }
+
+        if (charStack.size() == 1){
+            tempCryptogram.add(charStack.pop());
+        }
+
+        boolean isDeleted = cryptogram.size() != tempCryptogram.size();
+        cryptogram = tempCryptogram;
+
+        return isDeleted;
     }
 }
