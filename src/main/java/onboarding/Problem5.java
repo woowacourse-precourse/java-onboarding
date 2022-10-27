4e7ff24e28d214f6322b34fabbc9275d102a56c9
sieun <sieun960513@naver.com>
2022-10-27 18:05:28 +0900

    [Problem4] : 오류 수정 [수정]

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/** 기능 목록
+ * solutionLogic    : 메인 솔루션
+ * solutionLogic    : 대문자, 소문자, 일반문자 판별
+ * lowerChange      : 소문자 변환
+ * upperChange      : 대문자 변
+ * checkValid       : 예외 처리
+ * checkWordLength  : 단어 길이 체크
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
