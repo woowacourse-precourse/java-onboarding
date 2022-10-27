810ed918775778d02604c3a266f7b34af4f5b0b1
egoing <dudqk9696@naver.com>
2022-10-28 00:01:59 +0900

    문제 2 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+
+        for(int i=0; i<cryptogram.length(); i++){
+            left.push(cryptogram.charAt(i));
+        }
+
+        while(!left.isEmpty()) {
+            if(right.isEmpty() || left.peek() != right.peek()) right.push(left.peek());
+            else if(left.peek() == right.peek()){
+                right.pop();
+            }
+            left.pop();
+        }
+
+        int rightsize = right.size();
+        for(int i=0; i<rightsize; i++) {
+            answer += right.pop();
+        }
+
         return answer;
     }
+
+
+
 }
