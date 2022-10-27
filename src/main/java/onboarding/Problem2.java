ecd226c0ee2b9386453582bcf21a147df21a7ed8
HyunggiPark <phg9587@naver.com>
2022-10-27 21:54:57 +0900

    feat : stack 자료구조를 통해서 중복되는 문자를 확인할 수 있다. 그리고 계속해서 중복을 제거해야 하므로 재귀를 사용함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return deduplication(cryptogram);
+    }
+
+    private static String deduplication(String cryptogram) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char tempChar = cryptogram.charAt(i);
+
+            if (stack.empty()) {
+                stack.push(tempChar);
+            } else if (stack.peek() == tempChar) {
+                stack.pop();
+            } else {
+                stack.push(tempChar);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Character character : stack) {
+            sb.append(character);
+        }
+
+        if (cryptogram.length() == stack.size()) {
+            return cryptogram;
+        }
+
+        return deduplication(sb.toString());
     }
 }
