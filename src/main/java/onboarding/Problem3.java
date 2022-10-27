27176a4e352b48becb543df7e8d2be328f7d4622
EUNJEONGMUN <munej26@gmail.com>
2022-10-27 18:10:08 +0900

    feat: 숫자에 들어있는 3,6,9의 개수 세기 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem3 {
+    private static final Set<Integer> numbers369 = new HashSet<>(Arrays.asList(3, 6, 9));
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int count396(int num) {
+        int count = 0;
+        int remainder;
+        while (num>0) {
+            remainder = num%10;
+            if (numbers369.contains(remainder)) {
+                count++;
+            }
+            num /= 10;
+        }
+        return count;
     }
 }
