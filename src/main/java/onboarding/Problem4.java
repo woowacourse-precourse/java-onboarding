3f64f55da9851f62055ccbdd2ee17707ae4b60bb
bjk1649 <qhwns1649@naver.com>
2022-10-28 00:36:44 +0900

    feat; 문자가 대문자인지 검사하는 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,3 +6,14 @@ public class Problem4 {
         return answer;
     }
 }
+
+class AlphabetController{
+    static final int UPPER_START_INDEX = 65;
+    static final int UPPER_END_INDEX = 90;
+    static final int LOWER_START_INDEX = 97;
+    static final int LOWER_END_INDEX = 122;
+
+    private static boolean isUpperAlpha(char alpha){
+        return ((int)alpha >= UPPER_START_INDEX && (int)alpha <= UPPER_END_INDEX);
+    }
+}
