45fd74d5082dedc623576f3bb8633be041e097b1
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-27 22:28:42 +0900

    기능 요구사항 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,16 @@ public class Problem2 {
 
     // 기능 요구 사항
     // TODO 중복 문자의 시작 인덱스와 마지막 인덱스를 리턴하는 메서드
+    public static int getIndex(String cryptogram) {
+
+        return 0;
+    }
+
     // TODO 중복 문자를 삭제하는 메서드
+    public static String deleteCryptogram (String cryptogram) {
+
+        return null;
+    }
 
     public static String solution(String cryptogram) {
         String answer = "answer";
