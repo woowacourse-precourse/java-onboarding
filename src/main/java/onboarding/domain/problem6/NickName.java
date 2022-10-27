979f278c142bcf65bee52f6e0c75d5074f7643ca
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-10-28 05:54:41 +0900

    refactor(Problem6) 중복 체크 기능 수정

--- src/main/java/onboarding/domain/problem6/NickName.java
+++ src/main/java/onboarding/domain/problem6/NickName.java
@@ -1,14 +1,9 @@
 package onboarding.domain.problem6;
 
 public class NickName {
-    private String nickname;
-    public NickName(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public boolean hasOverlap(NickName other) {
+    public static boolean hasOverlap(String nickname, String otherName) {
         for (int i=0; i<nickname.length()-1;i++)
-            if (other.nickname.contains(nickname.substring(i,i+2)))
+            if (otherName.contains(nickname.substring(i,i+2)))
                 return true;
         return false;
     }
--- src/test/java/onboarding/domain/Problem6Test.java
+++ src/test/java/onboarding/domain/Problem6Test.java
@@ -3,27 +3,23 @@ package onboarding.domain;
 import onboarding.domain.problem6.NickName;
 import org.junit.jupiter.api.Test;
 
+import static onboarding.domain.problem6.NickName.hasOverlap;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Problem6Test {
-    @Test
-    public void overlapTest1() {
-        NickName jm = new NickName("제이엠");
-        assertThat(jm.hasOverlap(new NickName("제이슨"))).isTrue();
+    @ParameterizedTest
+    @CsvSource({"제이엠,엠제이","제이엠,제이슨","제이슨,엠제이"})
+    public void overlapTest(String nickname1, String nickname2) {
+        assertThat(hasOverlap(nickname1,nickname2)).isTrue();
     }
-    @Test
-    public void overlapTest2() {
-        NickName jm = new NickName("제이엠");
-        assertThat(jm.hasOverlap(new NickName("워니"))).isFalse();
-    }
-    @Test
-    public void overlapTest3() {
-        NickName jm = new NickName("제이엠");
-        assertThat(jm.hasOverlap(new NickName("엠제이"))).isTrue();
-    }
-    @Test
-    public void overlapTest4() {
-        NickName jm = new NickName("이제엠");
-        assertThat(jm.hasOverlap(new NickName("엠제이"))).isFalse();
+    @ParameterizedTest
+    @CsvSource({"워니,이제엠","워니,제이슨","이제엠,엠제이"})
+    public void overlapTest2(String nickname1, String nickname2) {
+        assertThat(hasOverlap(nickname1,nickname2)).isFalse();
     }
 }
