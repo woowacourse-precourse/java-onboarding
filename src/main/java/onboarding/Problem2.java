b165d1e99e0309f6d5a58c99b75654285763f083
abbd2 <ajkety00@gmail.com>
2022-10-28 00:33:34 +0900

    주석 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,13 +8,13 @@ public class Problem2 {
           char fstWord = sb.charAt(i - 1);
           char sndWord = sb.charAt(i);
           if(fstWord == sndWord){
-              // 현재 인덱스의 문자와 한칸 뒤에있는 인덱스의 문자를 제거
+              // 현재 인덱스의 문자와 한 칸 뒤에 있는 인덱스의 문자를 제거
               sb.delete(i - 1, i + 1);
 
               // 인덱스1(두번째 문자)에서 문자를 지운 경우에는 i를 1로 초기화
               if(i <= 1){
                   i = 1;
-              } else { // 문자를 지운 인덱스에서 2번 앞에있는 인덱스부터 다시 검색
+              } else { // 문자를 지운 인덱스에서 2번 앞에 있는 인덱스부터 다시 검색
                   i = i - 2;
               }
 
