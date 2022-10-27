a912bb8546df5140b073374ec75f82f5a90e62d1
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 09:51:19 +0900

    feat: 게임 결과에 대한 상수 추가

---
+++ src/main/java/onboarding/problem1/PageNumberGame.java
@@ -0,0 +1,8 @@
+package onboarding.problem1;
+
+public class PageNumberGame {
+	public static final Integer POBI_WIN = 1;
+	public static final Integer CRONG_WIN = 2;
+	public static final Integer TIE_MATCH = 0;
+	public static final Integer EXCEPTION = -1;
+}
