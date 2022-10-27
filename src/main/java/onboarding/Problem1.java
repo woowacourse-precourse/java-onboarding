a3a993b4da8c76ec893106bacf46a1bfc748f951
YuHayeon <youha0930@gmail.com>
2022-10-28 00:36:17 +0900

    feat(Problem1): 입력이 올바르지 않는 경우 예외처리
    - 각각의 숫자가 1 미만이거나 400 초과인 수가 있을 경우
    - 배열의 길이가 2가 아닌 경우
    - 홀수페이지, 짝수페이지(==홀수페이지+1)의 순서가 아닌 경우
    - 시작면을 펼친 경우
    - 마지막면을 펼친 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,62 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+
+	static class InputValidator {
+		public static boolean isRightPagesInput(List<Integer> pobi, List<Integer> crong) {
+			if (isRightPage(pobi) && isRightPage(crong)) {
+				return true;
+			}
+			return false;
+		}
+
+		public static boolean isRightPage(List<Integer> pages) {
+			if (isRightPagesLength(pages) && isRightPageRange(pages) && isRightPagesOrder(pages) && !isFirstPage(pages)
+				&& !isLastPage(pages)) {
+				return true;
+			}
+			return false;
+		}
+
+		private static boolean isRightPagesLength(List<Integer> pages) {
+			if (pages.size() != 2) {
+				return false;
+			}
+			return true;
+		}
+
+		private static boolean isRightPageRange(List<Integer> pages) {
+			for (int page : pages) {
+				if (page < 1 || page > 400) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private static boolean isRightPagesOrder(List<Integer> pages) {
+			if (pages.get(0) % 2 == 1 && pages.get(1) % 2 == 0 && pages.get(1) - pages.get(0) == 1) {
+				return true;
+			}
+			return false;
+		}
+
+		private static boolean isFirstPage(List<Integer> pages) {
+			if (pages.get(0) == 1 && pages.get(1) == 2) {
+				return true;
+			}
+			return false;
+		}
+
+		private static boolean isLastPage(List<Integer> pages) {
+			if (pages.get(0) == 399 && pages.get(1) == 400) {
+				return true;
+			}
+			return false;
+		}
+	}
 }
