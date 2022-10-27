33fc8f70a1357dd85379c7f1569429547c42324d
song-wooseok <sws981202@gmail.com>
2022-10-27 15:52:26 +0900

    feat: [예외사항 3] 책 페이지 짝 예외처리 구현

---
+++ src/main/java/onboarding/problem1/utils/validator/exception/PagePairException.java
@@ -0,0 +1,8 @@
+package onboarding.problem1.utils.validator.exception;
+
+public class PagePairException extends Exception{
+    public PagePairException(String msg){
+        super(msg);
+    }
+}
+
