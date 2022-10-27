6382f4b3e33fd417ff67b7b54790d205dec8e774
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-27 14:51:22 +0900

    feat: Problem5 돈의 단위 반환해주는 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer =Collections.emptyList();
         return answer;
     }
+    private static List<Integer> moneyType(){
+        List<Integer> moneyTypeList = new ArrayList<>();
+        moneyTypeList.add(50000);
+        moneyTypeList.add(10000);
+        moneyTypeList.add(5000);
+        moneyTypeList.add(1000);
+        moneyTypeList.add(500);
+        moneyTypeList.add(100);
+        moneyTypeList.add(50);
+        moneyTypeList.add(10);
+        moneyTypeList.add(1);
+        return moneyTypeList;
+    }
+
 }
