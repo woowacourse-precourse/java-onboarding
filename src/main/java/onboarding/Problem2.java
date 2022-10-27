4b7e53d1b42f55302af643363b05f423d7ccc740
Kim woo jong <skekdnwhd@gmail.com>
2022-10-28 02:32:11 +0900

    해독과 제한사항 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,13 +5,13 @@ import java.util.regex.Pattern;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000 || !cryptogram.matches("^[a-z]*$"))
+            return cryptogram;
+
         return decoder(cryptogram);
     }
 
     private static String decoder(String cryptogram) {
-        if (cryptogram.length() < 1 || cryptogram.length() > 1000 || !cryptogram.matches("^[a-z]*$"))
-            return cryptogram;
-
         String answer =  cryptogram.replaceAll("([a-z])\\1","");
 
         if (answer.matches("^[a-z]*([a-z])\\1[a-z]*$"))
