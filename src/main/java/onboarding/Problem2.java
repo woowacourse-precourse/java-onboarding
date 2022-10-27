d5b4b70c9a75ac84d1dc25f9c2679338736a7207
hyun-seung <ardimento22@naver.com>
2022-10-27 09:29:06 +0900

    [FEAT] 처음 중복되는 위치를 구한다. (중복되는 것이 없다면 불가능한 수를 return)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
+    }
+
+    public static int getRepetitionStartPoint(String target) {
+        for (int i = 0; i < target.length() - 1; i++) {
+            if (target.charAt(i) == target.charAt(i + 1)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
