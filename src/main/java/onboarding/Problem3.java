d32161f861ae1c745a7db4916cc5280f0bd9ac0b
limecats <limecats0331@gmail.com>
2022-10-28 04:10:37 +0900

    feat: [추가] 입력받은 숫자까지 369의 갯수를 확인하는 메소드
    모든 기능을 구현하여 입력받은 숫자까지 369의 갯수를
    반환하는 메소드를 구현했다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += count369InNumber(i);
+        }
         return answer;
     }
 
