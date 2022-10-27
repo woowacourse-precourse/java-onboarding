52abb13e18ccfb420f7ecbbb93a481984deb8ec0
Beginner <dkstmdwn17@naver.com>
2022-10-27 23:16:09 +0900

    feet : P6 기능 요구사항을 작성하고, 크루들의 닉네임 중 같은 글자가 연속적으로 포함될 경우 닉네임 사용이 제한되고 해당하는 유저의 이메일을 추출하는 메서드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,54 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * 기능 요구사항
+ * 1. 크루들의 닉네임 중 같은 글자가 연속적으로 포함될 경우 닉네임 사용을 제한해야 한다.
+ * 2. 닉네임 사용을 제한당한 크루들의 이메일을 오름차순으로 정렬해야 한다.
+ */
 public class Problem6 {
+    static boolean[] c;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        c = new boolean[forms.size()];
+        limitDuplication(forms, answer);
+
         return answer;
     }
+
+    private static void limitDuplication(List<List<String>> forms, List<String> answer) {
+        for(int i = 0; i< forms.size(); i++){
+            if(c[i]) continue;
+            List<String> str = forms.get(i);
+            String nickName = str.get(1);
+
+            for(int j=0;j<nickName.length();j++){
+                for(int k = i+1; k< forms.size(); k++){
+                    int count = 0;
+                    String nickName2 = forms.get(k).get(1);
+                    for(int z=0;z<nickName2.length();z++){
+                        if(nickName.charAt(j)==nickName2.charAt(z)){
+                            if(j+1<nickName.length()&&z+1<nickName2.length()){
+                                if(nickName.charAt(j+1)==nickName2.charAt(z+1)){
+                                    count++;
+                                    if(!answer.contains(forms.get(i).get(0))){
+                                        answer.add(forms.get(i).get(0));
+                                        c[i] = true;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if(count>0){
+                        answer.add(forms.get(k).get(0));
+                        c[k] = true;
+                    }
+                }
+            }
+
+        }
+    }
+
 }
