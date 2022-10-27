8fe1ced9aa39c37d2558508097bbc6e76703a0e5
minhee020 <minhee020@naver.com>
2022-10-27 12:26:46 +0900

    Problem2 | 연속된 문자 삭제 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,16 @@ package onboarding;
  * 1. 연속된 문자열 삭제 함수
  */
 public class Problem2 {
+    private static String deleteChar(String cryptogram){
+        String result = "";
+        for (int i = 0; i < cryptogram.length(); i++){
+            if ((i != 0) && (cryptogram.charAt(i) == cryptogram.charAt(i-1))) {
+                continue;
+            }
+            result += cryptogram.charAt(i);
+        }
+        return result;
+    }
     public static String solution(String cryptogram) {
         String answer = cryptogram;
         answer = deleteChar(cryptogram);
