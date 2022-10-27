8650e3c866bad6127a9b7166993fe22d3cfefbe6
jiwon1217 <koagjw@gmail.com>
2022-10-28 02:52:16 +0900

    [FEATURE] Solve the Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+    public static final List<Integer> game369 = new ArrayList<>(Arrays.asList(3, 6, 9));
+
+    public static Integer countClap(int number) {
+        int count = 0;
+
+        while (number != 0) {
+            if (game369.contains(number % 10)) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += countClap(i);
+        }
         return answer;
     }
 }
