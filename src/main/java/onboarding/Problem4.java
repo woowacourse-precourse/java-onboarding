88ce633cf011211b0915f4290a233eed1b35bf58
Namyunsuk <yunsuk0328@naver.com>
2022-10-28 00:40:02 +0900

    feat(frog): 문자열을 청개구리 사전에 따라 변환해주는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 class constant{
     static final String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static final String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    static final int length = 26;
 }
 class validateString{
     public static boolean isLower(String alphabet){
@@ -13,6 +20,33 @@ class validateString{
     }
     public static boolean isAlphabet(String alphabet){
         return alphabet.chars().allMatch(Character::isLetter);
+    }
+}
+class frog{
+    private int index;
+    private int changedIndex;
+    public void setIndex(int index){
+        this.index = index;
+    }
+    public void setChangedIndex(int index){
+        this.changedIndex = constant.length-index-1;
+    }
+    public static String changeCase(String word){
+        List<String> list = Arrays.asList(word.split(""));
+        word = list.stream()
+                .map(i->(i==i.toLowerCase())?i.toUpperCase():i.toLowerCase())
+                .collect(Collectors.joining());
+        return word;
+    }
+    public String changeWord(String alphabet){
+        if(validateString.isLower(alphabet)){
+            setIndex(constant.lowerAlphabet.indexOf(alphabet));
+            setChangedIndex(index);
+            return constant.lowerAlphabet.substring(changedIndex,changedIndex+1);
+        }
+        setIndex(constant.upperAlphabet.indexOf(alphabet));
+        setChangedIndex(index);
+        return constant.upperAlphabet.substring(changedIndex,changedIndex+1);
     }
 }
 public class Problem4 {
