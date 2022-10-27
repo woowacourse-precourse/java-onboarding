3b9d830624df6407fb40ce067baa474d08451424
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-27 15:38:28 +0900

    Problem1(설계): 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,73 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /**
+     * 참여자 두 명의 페이지 번호 리스트들을 받아 게임 결과를 반환
+     * @param pages1 : 첫 번째 참여자의 page list
+     * @param pages2 : 두 번째 참여자의 page list
+     * @return 첫 번째 참여자가 이긴다면 1, 두 번째 참여자가 이긴다면 2, 무승부라면 0, 예외사항은 -1
+     */
+    private int getResult(List<Integer> pages1, List<Integer> pages2){
+
+    }
+
+    /**
+     * 게임 입력값의 유효성을 검사
+     * @param pages1 : 첫 번째 참여자의 page list
+     * @param pages2 : 두 번째 참여자의 page list
+     */
+    private void checkParameterValidation(List<Integer> pages1, List<Integer> pages2){
+
+    }
+
+    /**
+     * 숫자 리스트를 받아 각 자리의 한 자리 숫자 리스트를 반환하는 함수
+     * @param numList
+     * @return 숫자 리스트의 각 자리 수 리스트
+     */
+    private List<Integer> getDigitList(List<Integer> numList){
+
+    }
+
+    /**
+     * 두 개의 페이지 번호를 받아 게임 점수를 반환
+     * @param pages : 책을 펼쳤을 때 나온 두 개의 페이지 번호
+     * @return 페이지 번호를 더한 값과 페이지 번호를 곱한 값 중 더 큰 값
+     */
+    private int getScore(List<Integer> pages){
+
+    }
+
+    /**
+     * 두 개의 숫자 중 더 큰 수를 반환
+     * @param num1 : 첫 번째 숫자
+     * @param num2 : 두 번째 숫자
+     * @return 두 숫자 중 더 큰 수
+     */
+    private int getLargerNumber(int num1, int num2){
+
+    }
+
+    /**
+     * 숫자 리스트를 받아 곱을 반환하는 함수
+     * @param numList : 숫자 리스트
+     * @return 숫자 리스트의 곱
+     */
+    private int getMultiplicationOfList(List<Integer> numList){
+
+    }
+
+    /**
+     * 숫자 리스트를 받아 합을 반환하는 함수
+     * @param numList : 숫자 리스트
+     * @return 숫자 리스트의 합
+     */
+    private int getSumOfList(List<Integer> numList) {
+
+    }
+
+
+
+
 }
