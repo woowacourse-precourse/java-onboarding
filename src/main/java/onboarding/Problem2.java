4dccf3b1caa273b58339edd5a9afc8d059f21534
noyes5 <noyes5@naver.com>
2022-10-28 05:05:56 +0900

    style : modify spacing Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,13 +3,12 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         StringBuilder sb = new StringBuilder(cryptogram);
 
-        for(int i=1;i<sb.length();i++){
-
+        for (int i=1;i<sb.length();i++) {
             if (sb.charAt(i-1) == sb.charAt(i)) {
                 sb.delete(i-1, i + 1);
                 i -= 2;
             }
-            if(i<0) {
+            if (i<0) {
                 i = 0;
             }
         }
