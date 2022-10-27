2275e9682e57df6aaffcd519c390ceff33f3617b
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-27 10:54:50 +0900

    feat: 청개구리 사전 구현 - 문자열 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+  static class GreenFrogDictionary {
+    public static char translate(Character character) {
+      if (character >= 'A' & character <= 'Z') {
+        return (char) ('Z' - (character - 'A'));
+      }
+      if (character >= 'a' & character <= 'z') {
+        return (char) ('z' - (character - 'a'));
+      }
+      return character;
     }
+  }
+
+  public static String solution(String word) {
+    return "";
+  }
 }
