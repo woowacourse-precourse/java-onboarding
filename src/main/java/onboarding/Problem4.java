92f010b900e3b23f8c754b7e6755e14ef0a6a872
devjordy14 <devjordy14@gmail.com>
2022-10-27 17:20:17 +0900

    todo 1.find some rule from condition

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+// 길이가 1 이상 1000 이하인 문자열을 받는다.
+// 알파벳 외의 문자는 변환하지 않고, 대문자는 대문자로 소문자는 소문자로 변환한다.
+// 문자열의 각 문자를 a -> z로, b -> y로, ... , Z -> A로 변환 후 반환
+
+// todo
+// 1. 각 문자를 변환하는 과정에서 규칙성 찾아보기
+// 1-2. 문자를 숫자로 변환해보기
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
