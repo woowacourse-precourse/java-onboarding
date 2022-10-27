8c2fa49be579fc6f4709e639ed8dd4d7d49cbdbd
CHAYEONIL <jeay123@naver.com>
2022-10-27 22:17:29 +0900

    문제4 해시맵에 청개구리 언어 저장

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 1. [문제 1](./docs/TODO_PROBLEM1.md)
 2. [문제 2](./docs/TODO_PROBLEM2.md)
-3. [문제 3](./docs/PROBLEM3.md)
-4. [문제 4](./docs/PROBLEM4.md)
+3. [문제 3](./docs/TODO_PROBLEM3.md)
+4. [문제 4](./docs/TODO_PROBLEM4.md)
 5. [문제 5](./docs/PROBLEM5.md)
 6. [문제 6](./docs/PROBLEM6.md)
 7. [문제 7](./docs/PROBLEM7.md)
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String[] basic_word;
+        // 청개구리 언어
+        String[] change_word = {"Z", "Y", "X", "W", "V", "U", "T", "S",  "R", "Q", "P", "O", "N", "M", "L",
+                "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
+
+        basic_word = word.split("");
+
+        // 해시맵에 청개구리 언어 저장
+        HashMap<String, String> convert = new HashMap<>();
+
+        for(int i = 0; i < 26; i++ ){ // 알파벳 26개
+            convert.put(String.valueOf((char) ('A' + i)), change_word[i]);
+        }
         return answer;
     }
 }
