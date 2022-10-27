52fa9f7124b63daf57bf95f104fcdd4d8ca215ed
KyungKyu Park <rudrb0505@gmail.com>
2022-10-27 11:57:38 +0900

    feat: 왼쪽과 오른쪽 페이지의 크기 비교

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,11 +8,16 @@ class Problem1 {
 		int pobiRightPage = pobi.get(1);
 		int crongLeftPage = crong.get(0);
 		int crongRightPage= crong.get(1);
-
 		int result = 0;
+
 		if (pobiLeftPage % 2 == 0 || pobiRightPage % 2 == 1 || crongLeftPage % 2 == 0 || crongRightPage % 2 == 1) {
 			result =  -1;
 		}
+
+		if(pobiLeftPage > pobiRightPage || crongLeftPage > crongRightPage) {
+			result = -1;
+		}
+
 		// int answer = Integer.MAX_VALUE;
 		return result;
 	}
