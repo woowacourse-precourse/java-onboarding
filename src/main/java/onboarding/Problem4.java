e4a7ec62d7f4ad9f2f5a84be810e8107ee1832c9
juhee park <jouhi3322@gmail.com>
2022-10-28 01:53:13 +0900

    feat: Problem4 전체 문자열에 대해 알파벳인경우 반대로 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,22 +8,42 @@ package onboarding;
  */
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder changedStr = new StringBuilder();
 
-        return answer;
+        //기능3. 전체 문자열에 대해 알파벳이라면 바꿔주고 문자가 아니면 바꾸지 않도록 구현
+        for (char c : word.toCharArray()) {
+            char changedAlphabet = getCharacter(c);
+            changedStr.append(changedAlphabet);
+        }
+
+        return changedStr.toString();
     }
+
+    private static Character getCharacter(char c) {
+        char changedChar = c;
+
+        if (Character.isLowerCase(c)) {
+            changedChar = getLowerCaseCharacter(c);
+        }
+        else if (Character.isUpperCase(c)) {
+            changedChar = getUpperCaseCharacter(c);
+        }
+        //else 인 경우는 알파벳인 아닌 경우이므로 그래도 리턴
+        return changedChar;
+    }
+
 
     //기능1. 소문자 알파벳을 반대로 바꿔주는 메서드
     private static Character getLowerCaseCharacter(char c) {
         int dif = c - 'a';
-        char changedAlphabet = (char) ('z' - dif);
-        return changedAlphabet;
+        return (char) ('z' - dif);
     }
 
     //기능2. 대문자 알파벳을 반대로 바꿔주는 메서드
     private static Character getUpperCaseCharacter(char c) {
         int dif = c - 'A';
-        char changedAlphabet = (char) ('Z' - dif);
-        return changedAlphabet;
+        return (char) ('Z' - dif);
     }
+
+
 }
