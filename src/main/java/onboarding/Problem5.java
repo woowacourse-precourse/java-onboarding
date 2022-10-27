23b5aa3d8a9e4d629d5c36ac30d061dc6e4ca23d
honey170 <63653475+hingjae@users.noreply.github.com>
2022-10-28 02:31:35 +0900

    problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import onboarding.problem5.Calculator;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = new ArrayList<>();
+        Calculator calculator = new Calculator();
+        answer = calculator.func(answer, money);
         return answer;
     }
 }
