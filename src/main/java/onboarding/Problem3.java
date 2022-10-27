b00d9ee654d5cd5b244c85c56b3df4bcd56fcb1a
Miseong Kim <miseongk@naver.com>
2022-10-27 15:52:48 +0900

    feat: add game 369

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return game369(number);
+    }
+
+    static int game369(int number) {
+        int count = 0;
+        int j;
+
+        for (int i = 1; i <= number; i++) {
+            j = i;
+            while (j > 0) {
+                if (j % 10 == 3 || j % 10 == 6 || j % 10 == 9) {
+                    count++;
+                }
+                j /= 10;
+            }
+        }
+
+        return count;
     }
 }
