fb9b64b90aa7b1ffe839555b27bde4f37c9d0292
limzzum <chfhddl1234@naver.com>
2022-10-27 13:10:13 +0900

    각 자리 숫자를 더하는 함수 -> plus함수 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+
+        int answer = 0;
         return answer;
+    }
+    static public int plus(int num){
+        int sum= 0;
+        while(num>0){
+            sum += num%10;
+            num/= 10;
+        }
+        return sum;
     }
 }
