3bb7a34961705526edd3d9601a010358564f19b1
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-10-27 10:12:33 +0900

    problem1 페이지번호 분리하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int leftpage = pobi.get(0);
+
+
+
+
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static ArrayList<Integer> sperater(int page) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        while(page > 0) {
+            numbers.add(page % 10);
+            page /= 10;
+        }
+        return numbers;
+    }
 }
+
