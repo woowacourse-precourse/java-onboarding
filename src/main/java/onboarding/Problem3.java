1c7f79d150b203a423f570fdfd16b23dd81f97b5
c4fiber <qudcjf153@gmail.com>
2022-10-28 05:03:54 +0900

    solve(prob3) 문제 풀이 완료
    풀이를 우선시 하기 위해 우선 brute force 방식을 선택함.
    countNumbOf369 함수가 static으로 선언되어 비효율적일 수 있음. 추후에 수정필요.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1; i<=number; i++) {
+            answer += countNumOf369(i);
+        }
+
         return answer;
+    }
+
+    public static int countNumOf369(int target) {
+        int count = 0;
+        while(target > 0) {
+            if (target % 10 == 3 || target % 10 == 0 || target % 10 == 9) {
+                count++;
+            }
+            target /= 10;
+        }
+
+        return count;
     }
 }
