80118e78f43333e946427eb96f64e569fbd476c9
Kid-Chang <pch3310@gmail.com>
2022-10-28 02:24:41 +0900

    feat: problem7, Add findMyFriends func

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,53 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        System.out.println(solution(user, friends, visitors));
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        // 1. 내 친구 찾기.
+        List<String> myFriends = findMyFriends(user, friends);
+        System.out.println(myFriends);
+        // 2. 내 친구와 아는(=함께 아는 친구) 찾기.
+        // 3. 함께 아는 친구에 대한 점수 부여.
+
+
+        // 4. 방문자에게 점수 부여하기.
+        // 5. 점수별로 정렬하기
+
         return answer;
     }
+
+    public static List<String> findMyFriends(String user, List<List<String>> friends){
+        List<String> myFriends = new ArrayList<>();
+
+        for(List<String> friend: friends){
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+            if(friend1.equals(user))
+                myFriends.add(friend2);
+            if(friend2.equals(user))
+                myFriends.add(friend1);
+        }
+
+        return myFriends;
+    }
+
+
 }
