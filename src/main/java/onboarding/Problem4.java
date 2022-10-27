ed7ec238f8d0bdf574cd14777a56212b3ba1c9cc
Miseong Kim <miseongk@naver.com>
2022-10-27 16:29:51 +0900

    feat: add convert string function

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,8 +2,22 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return convertString(word);
+    }
+
+    // 문자열을 변환하는 함수
+    static String convertString(String word) {
+        String convertString = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            if (isAlphabet(word.charAt(i))) {
+                convertString += convertChar(word.charAt(i));
+            } else {
+                convertString += word.charAt(i);
+            }
+        }
+
+        return convertString;
     }
 
     // 문자 하나를 반대로 변환하는 함수
