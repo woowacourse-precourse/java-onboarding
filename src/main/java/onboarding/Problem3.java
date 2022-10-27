f4e76bde31410ca0d45d7714662ecbe56957f280
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-27 17:57:47 +0900

    feat: 게임을 호출하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,9 @@
 package onboarding;
 
+import onboarding.problem3.Game369;
+
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		return Game369.start(number);
+	}
 }
