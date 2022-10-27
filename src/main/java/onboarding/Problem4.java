08b1f6183b6f6a84f6420d965796aeb6b55a891c
hyun-seung <ardimento22@naver.com>
2022-10-27 13:45:46 +0900

    [FEAT] 대문자인 경우 청개구리 사전에 의거하여 변환한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static final Character UPPER_FIRST_CHARACTER = 'A';
+    public static final Character UPPER_LAST_CHARACTER = 'Z';
+
     public static String solution(String word) {
         String answer = "";
         return answer;
@@ -12,5 +16,10 @@ public class Problem4 {
 
     public static boolean checkUppder(Character target) {
         return Character.isLowerCase(target);
+    }
+
+    public static Character changeCharUpperCase(Character target) {
+        int diff = target - UPPER_FIRST_CHARACTER;
+        return (char)(UPPER_LAST_CHARACTER - diff);
     }
 }
