18caf03f90487b19e5bdd13d647c4475efb1b498
ygreenb <nhj7911@gmail.com>
2022-10-28 03:46:08 +0900

    refactor: Problem1. 코드 컨벤션 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(isPage(pobi)==false||isPage(crong)==false) return -1;
+        if((isPage(pobi)==false) || (isPage(crong)==false)) return -1;
 
         int pobiScore =0;
         int crongScore = 0;
 
         for(int i=0;i<pobi.size();i++){
             String pobiPage = String.valueOf(pobi.get(i));
-            pobiScore = Math.max(pobiScore,findMaxNum(pobiPage));
+            pobiScore = Math.max(pobiScore, findMaxNum(pobiPage));
 
             String crongPage = String.valueOf(crong.get(i));
-            crongScore = Math.max(crongScore,findMaxNum(crongPage));
+            crongScore = Math.max(crongScore, findMaxNum(crongPage));
         }
 
         return findWinner(pobiScore, crongScore);
@@ -26,7 +26,7 @@ class Problem1 {
         if (leftPage%2 == 0) {
             return false;
         }
-        return (rightPage-leftPage==1)?true:false;
+        return ((rightPage-leftPage==1) ? true : false);
     }
 
     public static int findMaxNum(String page){
