d4a84d0062e495833499fc61b9ba0c2590eedfb2
hyeongu <wancern@naver.com>
2022-10-28 02:21:45 +0900

    Problem6. 요구사항 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,9 @@
 package onboarding;
-
+/*
+* 1. 닉네임으로 만들 수 있는 2글자의 문자를 모두 리스트에 저장
+* 2. 중복되는 문자열은 따로 리스트에 저장
+* 3. 중복되는 문자열과 닉네임을 비교하여 중복되는 닉네임 추출
+*/
 import java.util.List;
 
 public class Problem6 {
