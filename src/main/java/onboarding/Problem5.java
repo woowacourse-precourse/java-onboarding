59daa6a36a37839a106a4ec3672f15760111275a
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-27 11:42:24 +0900

    feat: ATM 인출 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
   static class ATM {
-    private static final List<Integer> unit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    private static final List<Integer> units = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
     public List<Integer> withdraw(int money) {
-      return List.of(1);
+      List<Integer> result = new ArrayList<>();
+      for (Integer unit : units) {
+        result.add(money / unit);
+        money %= unit;
+      }
+      return result;
     }
   }
 
