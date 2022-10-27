1dd2d3b331a11043613be62083b1c6990954bc73
kariskan <zkfzpf56@naver.com>
2022-10-27 21:19:11 +0900

    feat: 중복된 문자열을 제거하는 함수 작성

--- src/main/java/onboarding/support/problem2/DecodeUtil.java
+++ src/main/java/onboarding/support/problem2/DecodeUtil.java
@@ -2,6 +2,16 @@ package onboarding.support.problem2;
 
 public class DecodeUtil {
 
+    private static final int DUPLICATION_INTERVAL = 1;
+
+    private static boolean deleteDuplication(StringBuilder decodedString, int i, int duplicatedIndex) {
+        if (duplicatedIndex - i > DUPLICATION_INTERVAL) {
+            decodedString.delete(i, duplicatedIndex);
+            return true;
+        }
+        return false;
+    }
+
     private static int getDuplicatedIndex(StringBuilder originalString, int i, int duplicatedIndex) {
         while (duplicatedIndex < originalString.length() && originalString.charAt(i) == originalString.charAt(duplicatedIndex)) {
             duplicatedIndex++;
