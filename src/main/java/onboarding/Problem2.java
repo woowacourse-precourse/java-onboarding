bac34024cf22ee112ad9b61adfdd24b94f774e0c
lzlkolo <lzlko1209@gmail.com>
2022-10-27 19:51:03 +0900

    PROBLEM2.기능목록 및 예외사항 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    static String check(String cryptogram) {
+        String exception="";
+        if(cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            exception = "암호문 길이 오류";
+        }
+        for(char c : cryptogram.toCharArray()) {
+            if(!(c >= 97 && c <= 122)) {
+                exception = "암호 오류";
+            }
+        }
+        return exception;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        check(cryptogram);
         return answer;
     }
 }
+
+
