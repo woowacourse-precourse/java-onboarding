0e4cf865f2c8ba63cadd15ccca94f6fd915e6ea1
taekwon-dev <taekwon_dev@naver.com>
2022-10-27 14:35:02 +0900

    problem 1 - feat : 포비, 크롱의 점수 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,36 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int pobiPoint = getPoint(pobi);
+        int crongPoint = getPoint(crong);
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int getPoint(List<Integer> pageNums) {
+        int left = pageNums.get(0);
+        int right = pageNums.get(1);
+
+        int plus = 0;
+        int multiply = 1;
+        while (left > 0) {
+            int digit = left % 10;
+            plus += digit;
+            multiply *= digit;
+            left /= 10;
+        }
+        int max = Math.max(plus, multiply);
+        plus = 0;
+        multiply = 1;
+
+        while (right > 0) {
+            int digit = right % 10;
+            plus += digit;
+            multiply *= digit;
+            right /= 10;
+        }
+        max = Math.max(max, Math.max(plus, multiply));
+        return max;
     }
 }
