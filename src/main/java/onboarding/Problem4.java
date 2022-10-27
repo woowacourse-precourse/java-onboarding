518cfea57e39ab0af5466f8982eb29ec2ea8d6b6
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-27 12:06:49 +0900

    [문제4] - 제공받은 문자가 영문자인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+    public static boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+    
     public static String solution(String word) {
         String answer = "";
         return answer;
