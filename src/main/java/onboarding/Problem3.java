836f46334a1e6406882270dc29d417f930d227e5
Lee Sang Bin <rhkd2612@naver.com>
2022-10-27 18:19:51 +0900

    feat: calculate clapCount

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,10 +2,10 @@ package onboarding;
 
 /*
  * # 기능 정리
- * [X] 목표한 숫자까지 순회 메서드
- * [X] 각 숫자 별 결과값 확인 메서드
- * [X] solution 작성
- * [X] 테스트 확인
+ * [O] 목표한 숫자까지 순회 메서드
+ * [O] 각 숫자 별 결과값 확인 메서드
+ * [O] solution 작성
+ * [O] 테스트 확인
  */
 
 public class Problem3 {
@@ -17,8 +17,21 @@ public class Problem3 {
     private static int getAnswer(int number) {
         int calculateAnswer = 0;
         for(int i = 3; i <= number; i++){
-            //calculateAnswer += clapOnNumber(i);
+            calculateAnswer += clapOnNumber(i);
         }
         return calculateAnswer;
+    }
+
+    private static int clapOnNumber(int i) {
+        int clapCount = 0;
+
+        while(i > 0){
+            int lastNumber = i % 10; i /= 10;
+
+            if(lastNumber != 0 && lastNumber % 3 == 0)
+                clapCount++;
+        }
+
+        return clapCount;
     }
 }
