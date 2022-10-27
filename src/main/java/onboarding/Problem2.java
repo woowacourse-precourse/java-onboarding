637da78a8772ee9893434b88c010e66d9209576e
recicoco068 <gkapf838@gmail.com>
2022-10-27 21:51:08 +0900

    feat : Problem2 연속하는 중복 문자 개수 세는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        if (cryptogram.length() == 1)
+            return cryptogram;
         String answer = "answer";
         return answer;
+    }
+
+    public static int duplicationCount(String str, int start) {
+        int cnt = 0;
+        while (start < str.length() - 1 && str.charAt(start) == str.charAt(start + 1)) {
+            cnt += 1;
+            start += 1;
+        }
+        return cnt;
     }
 }
