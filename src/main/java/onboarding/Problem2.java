4bd06156317acd6d3abf246d447bbf18899a8b79
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-10-27 14:01:23 +0900

    문제 2번 <1번 기능 추가 완료>

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,39 +1,35 @@
 package onboarding;
 
-import java.util.Arrays;
-
 public class Problem2 {
-    public static void main(String[] args) {
-        String cryptogram = "browoanoommnaon";
-        System.out.println("암호를 해독하면 다음과 같습니다 : " + solution(cryptogram));
-    }
     public static String solution(String cryptogram) {
         String answer = "";
+        String[] answers = cryptogram.split(""); // 각 문자를 비교, 확인하기 위해 쪼개어 배열에 담아준다.
 
-//        반복문 사용하기
-        String[] chars = cryptogram.split("");
-        System.out.println(Arrays.toString(chars));
-
-        if (cryptogram.length() == 0) {
-            answer = "";
-            return answer;
-        }
-
-        for (int i = 1; i < cryptogram.length(); i++){
-            if (chars[i-1].equals(chars[i])) {
-                chars[i-1] = "";
-                chars[i] = "";
+        //인접한 문자열이 서로 같다면 빈 값을 넣어주고, 다르다면 앞의 글자를 넣어준다.
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (answers[i-1].equals(answers[i])) {
+                answers[i-1] = "";
+                answers[i] = "";
                 answer += "";
             }
-
-            if (!chars[i-1].equals(chars[i])) {
-                answer += chars[i-1];
+            if (!answers[i-1].equals(answers[i])) {
+                answer += answers[i-1];
             }
-
+            //적어준 조건상, 맨 마지막 글자는 잘리기 때문에 이를 붙여준다.
             if (i == cryptogram.length()-1) {
-                answer += chars[i];
+                answer += answers[i];
             }
-            answer = solution(answer);
+        }
+
+        // 위 과정을 걸쳐 나온 문자를 재귀적으로 함수를 호출해 반복하여 확인해본다.
+        String[] results = answer.split("");
+        if (answer.length() == 2 && results[0].equals(results[1])) {
+            answer = "";
+        }
+        for (int i = 1; i < answer.length()-1; i++) {
+            if (results[i-1].equals(results[i])) {
+                answer = solution(answer);
+            }
         }
         return answer;
     }
