2f164237cffd300aeda3e96b46189250a6be1851
정현수 <wjdgustn15@naver.com>
2022-10-27 15:59:36 +0900

    feat : solve Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        return answer;
+        Map<String,List<Integer>> nickNameIdx = new HashMap<>();
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> form = forms.get(i);
+            String nickName = form.get(1);
+            for (int j = 0; j <nickName.length()-1 ; j++) {
+                String partNickName = nickName.substring(j,j+2);
+                nickNameIdx.putIfAbsent(partNickName,new ArrayList<>());
+                nickNameIdx.get(partNickName).add(i);
+            }
+        }
+        Set<String> idSet = new HashSet<>();
+        for (List<Integer> value : nickNameIdx.values()) {
+            if(value.size()<2)
+                continue;
+            idSet.addAll(value.stream().map(x->forms.get(x).get(0)).collect(Collectors.toSet()));
+        }
+        return idSet.stream().sorted().collect(Collectors.toList());
     }
 }
