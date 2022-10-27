6832d68e1bbe8dc74e773ba00d8d4cf471b0ba4f
SeolYoungKim <nasur4da@gmail.com>
2022-10-27 17:23:23 +0900

    feat(P2): 중복값을 제거해주는 로직 구현

--- src/main/java/onboarding/problem2/Decrypt.java
+++ src/main/java/onboarding/problem2/Decrypt.java
@@ -2,5 +2,5 @@ package onboarding.problem2;
 
 public class Decrypt {
 
-    
+    private DuplicateProcessor duplicateProcessor;
 }
--- src/main/java/onboarding/problem2/DuplicateProcessor.java
+++ src/main/java/onboarding/problem2/DuplicateProcessor.java
@@ -1,4 +1,52 @@
 package onboarding.problem2;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class DuplicateProcessor {
+
+    private final Deque<Character> stack = new ArrayDeque<>();
+    private boolean flag = true;
+
+    public char[] checkDuplication(char[] cryptoCharArr) {
+        int duplicationCount = 0;
+
+        for (char cryptoChar : cryptoCharArr) {
+            if (stack.isEmpty()) {
+                stack.push(cryptoChar);
+                continue;
+            }
+
+            if (stack.peek() == cryptoChar) {
+                stack.pop();
+                duplicationCount++;
+                continue;
+            }
+
+            stack.push(cryptoChar);
+        }
+
+        return getDecryptedCharArray(stack, cryptoCharArr, duplicationCount);
+    }
+
+    private char[] getDecryptedCharArray(Deque<Character> stack, char[] cryptoCharArr,
+                                         int duplicationCount) {
+        if (duplicationCount == 0) {
+            flag = false;
+            return cryptoCharArr;
+        }
+
+        int size = stack.size();
+        char[] result = new char[size];
+
+        for (int i = 0; i < size; i++) {
+            if (stack.isEmpty()) {
+                break;
+            }
+
+            result[i] = stack.poll();
+        }
+
+        return result;
+    }
 }
