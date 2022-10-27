4d91b766c48f9a24ed7aeff8e41156a6bc1a429a
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-27 10:45:17 +0900

    feat: treat single duplication

--- src/main/java/onboarding/problem2/Decoder.java
+++ src/main/java/onboarding/problem2/Decoder.java
@@ -18,4 +18,17 @@ public class Decoder {
     public Decoder(String cryptogram) {
         builder = new StringBuilder(cryptogram);
     }
+
+    /**
+     * Method for treating single duplication
+     * @param index index of current index
+     */
+    private void treatDuplication(int index) {
+        from = index;
+        duplicate = true;
+        if (index == 0) {
+            builder.delete(from, to);
+            length = 0;
+        }
+    }
 }
