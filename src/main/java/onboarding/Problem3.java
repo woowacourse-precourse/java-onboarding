bbfd3724d2fdc90cf78e7b8c1ae9147c8d16121a
Junmin <junmin1623@naver.com>
2022-10-27 22:56:54 +0900

    problem 3 ver_1

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        //제한조건
+        if(number<1||number>10000) return -1;
+        int sum=0;
+        for(int j=1;j<=number;j++)
+        {
+            String n = Integer.toString(j);
+            for(int i=0;i<n.length();i++){
+                int sol1 = Integer.parseInt(n.substring(i, i + 1));
+                if(sol1 %3==0 && sol1!=0) sum++;
+            }
+        }
+
+        int answer = sum;
         return answer;
     }
 }
