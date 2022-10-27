f68981697a93208459f9e7d6ad65d66ae1e6d535
Reznov <reznov03048@gmail.com>
2022-10-27 21:51:02 +0900

    기본적인 요구사항을 solution 메소드에 주석으로 표시

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        // 더 이상 중복된 문자가 없을 때 까지 반복해서 중복된 문자 삭제하기
+
+        // 결과 반환
+
+
         return answer;
     }
 }
