3e3b80eb786a7ebf370a0430ea831989eae41271
devjordy14 <devjordy14@gmail.com>
2022-10-27 16:12:59 +0900

    did todo 1

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+// 1 이상 10000 이하인 자연수를 받음
+// 각각의 숫자의 자릿수를 분해해서 3,6,9의 갯수를 세야함
+// 갯수의 총합을 return 해야함
+
+// todo
+// 1. 받은 매개변수까지 반복하는 반복문 만들기
+// 2.
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= 13; i++){
+            System.out.println(i);
+        }
+
         return answer;
     }
 }
