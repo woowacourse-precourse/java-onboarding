29e0197e7a8edba9379bb052aea7a21babcbd032
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-27 22:34:29 +0900

    feat : 3,6,9 handle repeating numbers

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,19 +1,40 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem3 {
     public static int solution(int number) {
+
+
+
+
         int count = 0;
         for (int i = 1; i <= number; i++) {
 
             String numberStr = String.valueOf(i);
             if (numberStr.contains("3") || numberStr.contains("6") || numberStr.contains("9")) {
                 count++;
-                System.out.println(count);
+
+                if ( numberStr.length() >= 2 ) {
+                    Pattern patternRegex = setRegex();
+                    Matcher patternMatcher = patternRegex.matcher(numberStr);
+                    if ( patternMatcher.find()){
+                        System.out.println(numberStr);
+                        count++;
+                    }
+                }
             }
         }
 
         int answer = count;
         return answer;
 
+    }
+
+    private static Pattern setRegex() {
+        String regex = "[369]{2}";
+        Pattern patternRegex = Pattern.compile(regex);
+        return patternRegex;
     }
 }
