4191d17485e417a913104bf36c5fb79f5d8a8ac0
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-10-27 19:47:17 +0900

    기능 목록 1번(주어진 페이지 번호에 문제가 있는지 확인) 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,15 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(isProblem(pobi) || isProblem(crong)) answer = -1;
         return answer;
+    }
+
+    public static boolean isProblem(List<Integer> list){
+        int leftPage = list.get(0);
+        int rightPage = list.get(1);
+        if(rightPage - leftPage != 1) return true;
+        if(leftPage % 2 == 0 || rightPage % 2 == 1) return true;
+        return false;
     }
 }
