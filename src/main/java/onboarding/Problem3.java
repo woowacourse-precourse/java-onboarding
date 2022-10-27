90e0dc4ab9f6728efe271ddb8e50357ab96318b6
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-27 10:36:38 +0900

    feat(Problem3): 손뼉 횟수 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,23 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = clapHand(number);
         return answer;
+    }
+
+    // 1. 손뼉 횟수 구하기
+    // number가 큰 수가 아니기 때문에 이중반복문 사용
+    public static int clapHand(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            int n = i;
+            while (n > 0) {
+                if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) {
+                    sum++;
+                }
+                n /= 10;
+            }
+        }
+        return sum;
     }
 }
