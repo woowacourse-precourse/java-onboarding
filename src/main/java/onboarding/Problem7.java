d19bfc21adf7f4623b582e993d72e4f1d956fe68
2jun0 <soo28819@gmail.com>
2022-10-28 02:12:34 +0900

    Feat: 문제 7번 - 모든 크루를 구하는 메서드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,12 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
 
     public static int getRecommendScore(int cntOfSameFriendsWithMe, int cntOfVisit) {
         return cntOfSameFriendsWithMe * 10 + cntOfVisit;
+    }
+
+    public static Set<String> getAllCrews(List<List<String>> friendRelations, List<String> visitors) {
+        HashSet<String> allCrews = new HashSet<>();
+
+        // 관계에 있는 모든 크루들 저장
+        friendRelations.forEach(friendRelation -> allCrews.addAll(friendRelation));
+        // 관계가 없는 크루도 있으니 방문한 크루들도 저장
+        visitors.forEach(visitor -> allCrews.add(visitor));
+
+        return allCrews;
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
