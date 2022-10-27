e66a1376a39c66d31a5a766a078c84f6e60e28d1
Park shinyeong <syngh503@gmail.com>
2022-10-27 16:56:59 +0900

    Problem2/Feat/중복 문자 제거 함수 중복 문자를 제거하는 함수 removeDuplication 함수를 생성하였습니다. 2개로 연속된 문자는 잘 제거합니다 . 그러나 3개 이상의 연속되고 중복된 문자는 제거하지 않습니다. 이 부분은 나중에 추가해주어야 할 것 같습니다.
    Relate to: #2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
+import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeDuplication(cryptogram);
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static String removeDuplication(String cryptogram) {
+        Stack<Character> cryptoStack = new Stack<>();
+        cryptoStack.push(cryptogram.charAt(0));
+        cryptogram = cryptogram.substring(1);
+        for (char c: cryptogram.toCharArray()) {
+            if (cryptoStack.peek() != c) {
+                cryptoStack.push(c);
+            } else {
+                cryptoStack.pop();
+            }
+        }
+
+        String answer = "";
+        while(cryptoStack.isEmpty() == false) {
+            answer = cryptoStack.pop() + answer;
+        }
         return answer;
     }
 }
