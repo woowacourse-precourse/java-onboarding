ef4cd0e53dacffe35544feae8ccdc4c03941470c
ParkRootSeok <g_e_un@naver.com>
2022-10-27 22:30:56 +0900

    Problem3 - 박수 치는 횟수 계산 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,26 @@ public class Problem3 {
 
     public static int countClap(int number) {
 
+        List<Integer> digits = new ArrayList<>();
+        int result;
+
+        result = 0;
+        for (int i = 1; i <= number; i++) {
+
+            digits = parseNum(i);   // 숫자 분리
+
+            for (int n : digits) {  // 분리된 숫자 3, 6, 9 비교
+
+                if (compareNumWith369(n)) { // 비교 후 참이면 카운트 증가
+                    result++;
+                }
+
+            }
+
+        }
+
+        return result;
+
     }
 
     public static List<Integer> parseNum(int number) {
@@ -32,11 +52,12 @@ public class Problem3 {
         }
 
         return false;
+
     }
 
     public static int solution(int number) {
         int answer = 0;
-
+        answer = countClap(number);
         return answer;
     }
 
