cfc09a906aa2c5fad28fb982444619a9d736884e
Lee Sang Bin <rhkd2612@naver.com>
2022-10-27 18:22:32 +0900

    docs: problem4 methods

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/*
+ * # 기능 정리
+ * [X] 대문자/소문자 변환 메서드
+ * [X] 반대 문자 변환 메서드
+ * [X] solution 작성
+ * [X] 테스트 확인
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
