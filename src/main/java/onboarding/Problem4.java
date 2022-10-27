c72641ad1f3b4a7c5ce95c97db4e6973975cba77
ys200209 <lys200209@naver.com>
2022-10-27 18:16:46 +0900

    feat(문제4): 알파벳이 대문자인지 소문자인지 구분한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
+         String answer = "";
+
         return answer;
     }
+
+    private static boolean isUpperCase(char spelling) {
+
+        if (spelling - 'A' >= 0 && spelling - 'A' < 26) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private static boolean isLowerCase(char spelling) {
+
+        if (spelling - 'a' >= 0 && spelling - 'a' < 26) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
