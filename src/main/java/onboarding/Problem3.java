a136fd8fb98b6e6ca1951db53efd815f0311b59f
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-27 22:55:10 +0900

    문제 3 : 기능 2 - 1부터 number까지 순회하는 기능.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -11,6 +11,9 @@ public class Problem3 {
     }
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (contain3or6or9(i)) answer++;
+        }
         return answer;
     }
 }
