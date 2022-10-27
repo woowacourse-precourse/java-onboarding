a1c155c875e74908c0ca57e900a13527dac31d9e
devjordy14 <devjordy14@gmail.com>
2022-10-27 17:05:46 +0900

    did todo 2-2, 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,6 +9,7 @@ package onboarding;
 // 2. 각 자릿수에 3,6,9가 몇개 들어가는지 검사
 // 2-1. 숫자의 각 자릿수를 분해
 // 2-2. 분해한 각 자릿수가 3,6,9인지 검사
+// 3. 조건에 맞는 수의 갯수를 더한 후 return
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +18,10 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        for(int i = 1; i <= 13; i++){
-            System.out.println(i);
-            System.out.println(returnDigit(i));
+        for(int i = 1; i <= number; i++){
+            answer += returnSumOfCondition(returnDigit(i));
         }
-
+        System.out.println(answer);
         return answer;
     }
 
@@ -34,9 +34,14 @@ public class Problem3 {
         return arrNum;
     }
 
-    public static int returnSumOfCondition(List<Integer>){
+    public static int returnSumOfCondition(List<Integer> digitList){
         int sum = 0;
 
+        for(Integer i : digitList) {
+            // condition
+            if(i == 3 || i == 6 || i == 9)
+                sum += 1;
+        }
         return sum;
     }
 }
