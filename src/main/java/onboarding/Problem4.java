aeaf1f2ab80193aa4185e37e7b7320de4e5ca6f1
cpot5620 <=cpot5620@naver.com>
2022-10-28 01:38:21 +0900

    Implement Word class and solve problem4
    - Word class is responsible for cheking lower or upper word and conversion.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import java.sql.Wrapper;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Word answer = new Word(word);
+
+        return answer.conversion();
     }
 }
