b3760a3b568744a99d72a3df311d82644c693a61
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-27 21:02:51 +0900

    feat(problem4): 문자 종류 검사

--- src/main/java/onboarding/problem4/ReverseConverter.java
+++ src/main/java/onboarding/problem4/ReverseConverter.java
@@ -1,2 +1,21 @@
-package onboarding.problem4;public class ReverseConverter {
+package onboarding.problem4;
+
+public class ReverseConverter {
+
+    public static final int TYPE_NONE = 0;
+    public static final int TYPE_UPPER_CASE = 1;
+    public static final int TYPE_LOWER_CASE = 2;
+
+
+
+    private static int checkCharType(char ch){
+        if(Character.isUpperCase(ch)){
+            return TYPE_UPPER_CASE;
+        }
+        if(Character.isLowerCase(ch)){
+            return TYPE_LOWER_CASE;
+        }
+        return TYPE_NONE;
+    }
+
 }
