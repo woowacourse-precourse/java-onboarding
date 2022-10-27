12f1c632c62cd063f0122a352b248921107082d3
choyeram <choyeram8@gmail.com>
2022-10-27 17:02:37 +0900

    Feat : find duplicated names

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> wordsMap = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String name = form.get(1);
+            for (int i = 0; i < name.length()-1; i++) {
+                String subName = name.substring(i, i+2);
+                wordsMap.put(subName, wordsMap.getOrDefault(subName, 0)+1);
+            }
+        }
+        for (List<String> form : forms) {
+            String name = form.get(1);
+            for (int i = 0; i < name.length()-1; i++) {
+                String subName = name.substring(i, i+2);
+                if (wordsMap.get(subName)-1 > 0) {
+                    answer.add(form.get(0));
+                    break;
+                }
+            }
+        }
+
+        return answer.stream().distinct().sorted().collect(Collectors.toList());
     }
 }
