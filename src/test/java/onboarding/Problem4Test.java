2a40852ea27fc108c2c2117296869d815c1b1d1a
jjmin9797 <jjmin9797@naver.com>
2022-10-27 17:11:29 +0900

    [Problem4][Test][characterOneConversion] 문자 변환 테스트 진행 및 완료

--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -17,4 +17,10 @@ class Problem4Test {
         assertEquals(true,problem4.upperCaseDivision("A".charAt(0)));
     }
 
+    @Test
+    @DisplayName("[Problem][characterOneConversion] 문자 변환 테스트")
+    public void characterOneConversionTest() {
+        assertEquals("o".charAt(0),problem4.characterOneConversion("l".charAt(0),false));
+    }
+
 }
