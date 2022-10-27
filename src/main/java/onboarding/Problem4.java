f52924b4aa81ebc49146d013aeff32e62ed9f101
Minjune <minjune8506@gmail.com>
2022-10-27 20:01:16 +0900

    feat(Problem4): 알파벳, 대소문자 구분하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static char translate(char c) {
+        if (Character.isUpperCase(c)) {
+            return c; // 대문자 번역
+        }
+        return c; // 소문자 번역
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (Character.isAlphabetic(c)) {
+                answer.append(translate(c));
+                continue;
+            }
+            answer.append(c);
+        }
+        return answer.toString();
     }
 }
