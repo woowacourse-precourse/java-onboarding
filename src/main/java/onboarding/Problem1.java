8e42916f43b05673f0ef7fa1ccd89d601117c2d9
yesjuhee <juhee0924@g.skku.edu>
2022-10-28 05:16:59 +0900

    [PROBLEM1] 각 자리수의 합 구하는 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,17 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static int getSum (int num){
+        int sum = 0;
+        while(num != 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
