94d7949c66b8c24893a6f1e5ccb498c5242ca240
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-27 16:32:50 +0900

    feat: Problem6 닉네임HashMap에 두자리씩 저장해주는 메소드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    private static final int EMAIL=0;
+    private static final int NICKNAME=1;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        Map<String,Integer> nickMap = new HashMap<>();
+        mapSetting(forms, nickMap);
+        List<String> answer =List.of("answer");
         return answer;
     }
+    private static void mapSetting(List<List<String>> forms, Map<String, Integer> nickMap) {
+        for(int i=0;i<forms.size();i++){
+            String nowNick = forms.get(i).get(NICKNAME);
+            mapAdd(nickMap, nowNick);
+        }
+    }
+    private static void mapAdd(Map<String,Integer> nickMap, String nowNick) {
+        for(int i=0;i<nowNick.length()-1;i++){
+            String tmp = nowNick.substring(i,i+2);
+            nickMap.put(tmp,nickMap.getOrDefault(tmp,0)+1);
+        }
+    }
+
 }
