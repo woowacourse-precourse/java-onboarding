fc186f852d6db5b2a32a3ab5f582c74ce4027bda
Isu Shin <isu010614@naver.com>
2022-10-27 23:03:16 +0900

    [Feat] Add exception checking function
     LeftPage의 숫자가 RighPage의 숫자보다 1 작은 경우만 true로 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,24 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
+        if (!isException(pobiLeftPage, pobiRightPage)
+                || !isException(crongLeftPage, crongRightPage)) {
+            return -1;
+        }
+
         return answer;
+    }
+
+    public static boolean isException(int leftPage, int rightPage) {
+        if (rightPage - leftPage == 1) {
+            return true;
+        }
+        return false;
     }
 }
