d133daf36b7913b7e7a5d054feda6f23558d0b15
song-wooseok <sws981202@gmail.com>
2022-10-27 15:56:56 +0900

    feat: [기능 1] 숫자를 각 자리별로 잘라서 저장하는 기능 구현

---
+++ src/main/java/onboarding/problem1/service/ScoreService.java
@@ -0,0 +1,21 @@
+package onboarding.problem1.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScoreService {
+    //해당 함수는 Integer 받아 한 개씩 잘라서 ArrayList 에 쌓은 후 반환
+    public static ArrayList<Integer> integerToArrayList(Integer value){
+        ArrayList<Integer> slicedIntList = new ArrayList<Integer>();
+        int number = value.intValue();
+
+        while (number > 0){
+            slicedIntList.add(number % 10);
+            number /= 10;
+        }
+        /*
+         * number = 123 -> slicedList = [3,2,1]
+         * */
+        return slicedIntList;
+    }
+}
