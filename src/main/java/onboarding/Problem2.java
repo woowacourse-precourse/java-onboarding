a3cc51b8aa196bcbaecb80300ea09848f2d42b4b
jeongjihun <wlgns2234@naver.com>
2022-10-27 14:33:15 +0900

    Feat: 중복되는 문자열 제거한 문자열 반환기능추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeRedundancyChar(String str, int start, int end) {
+        String ret = str.substring(0, start) + str.substring(end+1);
+        return ret;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
