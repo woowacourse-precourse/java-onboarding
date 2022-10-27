9f2fe336dc0accd92e1387e1ff7b7be60f0a6ab9
meunrud <y3a0522@gmail.com>
2022-10-28 03:00:18 +0900

    2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다. 3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,53 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int Maxsum(Integer leftPage, Integer rightPage){
+        int Maxsum = Math.max(sumLeftPage(leftPage),sumRightpage(rightPage));
+
+        return Maxsum;
+    }
+
+    public static int sumLeftPage(Integer pageNum){
+        int sumLeftPage = 0;
+        while (pageNum != 0){
+            sumLeftPage += pageNum%10;
+            pageNum /= 10;
+        }return  sumLeftPage;
+    }
+
+    public static int sumRightpage(Integer pageNum){
+        int sumRightPage = 0;
+        while (pageNum != 0){
+            sumRightPage += pageNum%10;
+            pageNum /= 10;
+        }return  sumRightPage;
+    }
+
+    public static int maxMulti(Integer leftPage, Integer rightPage){
+        int maxMulti = Math.max(multiLeftpage(leftPage),multiRightpage(rightPage));
+        return maxMulti;
+    }
+
+    public static int multiLeftpage(Integer pageNum){
+        int multiLeftpage = 0;
+        String page = String.valueOf(pageNum);
+
+        for(int i = 0; i<page.length();i++){
+            multiLeftpage *=Integer.parseInt(page.substring(i,i+1));
+        }
+        return multiLeftpage;
+    }
+    public static int multiRightpage(Integer pageNum){
+        int multiRightpage = 0;
+        String page = String.valueOf(pageNum);
+
+        for(int i = 0; i<page.length();i++){
+            multiRightpage *=Integer.parseInt(page.substring(i,i+1));
+        }
+        return multiRightpage;
+    }
+
+
+
 }
