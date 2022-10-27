e838a77aea257bfc21bd8f3c7abe7dbdee0b6e84
YSEO <1park4170@gmail.com>
2022-10-27 13:10:24 +0900

    3rd problem

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i =1 ; i <= number; i++) {
+
+            String s = Integer.toString(i);
+            answer += countChar(s, '3');
+            answer += countChar(s, '6');
+            answer += countChar(s, '9');
+        }
+
         return answer;
+    }
+
+    private static long countChar(String str, char ch) {
+        return str.chars()
+            .filter(c -> c == ch)
+            .count();
     }
 }
