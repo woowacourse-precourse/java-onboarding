85d4a3dccf09139995dd28c15b5e2543211e8bb3
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-27 11:03:51 +0900

    돈 단위 리스트 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Problem5 {
   static class ATM {
+    private static final List<Integer> unit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
     public List<Integer> withdraw(int money) {
       return List.of(1);
