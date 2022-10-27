163a6dd7233be28f9e0c8fa9a5ea7ebde6a63bd0
맹순영 <symaeng98@naver.com>
2022-10-27 15:58:39 +0900

    feat: 포비 점수 계산 기능 구현
    포비의 페이지 합, 곱을 통한 점수 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        // ----- 포비의 점수 계산 -----
+        // 포비의 페이지 합 계산
+        int pobiSum = pobi.get(0) + pobi.get(1);
+        // 포비의 페이지 곱 계산
+        int pobiMul = pobi.get(0) * pobi.get(1);
+        // 포비의 점수 계산
+        int pobiScore = Math.max(pobiSum, pobiMul);
+        return 1;
     }
 }
