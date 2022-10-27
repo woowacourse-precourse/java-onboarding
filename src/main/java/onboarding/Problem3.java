f04db8f28286101e8b20a668265692ccd6a92c45
Choi-JY <Choi-JY1107@github.com>
2022-10-28 00:27:47 +0900

    feat: 3번문제 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = number/10 * 3 + number%10/3;
+        int div = 10;
+
+        while(div <= number){
+            int rotate = number/div;
+            answer += rotate/10*3*div;
+            answer += (rotate-1)%10/3 * div;
+            if(rotate%10%3 == 0) answer += number%div + 1;
+            div*=10;
+        }
+
         return answer;
     }
 }
