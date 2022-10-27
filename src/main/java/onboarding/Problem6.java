7d71797412e455547307fc44ef11ce895461c128
YSEO <1park4170@gmail.com>
2022-10-27 17:01:40 +0900

    6th problem

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,42 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        Set<String> patterns = new HashSet<>();
+        Set<String> Emails = new HashSet<>();
+
+        for (List form : forms) {
+            String nick = (String) form.get(1);
+            String email = (String) form.get(0);
+
+            checkSub(nick, email, patterns, Emails);
+        }
+
+        checkSub(forms.get(0).get(1), forms.get(0).get(0), patterns, Emails);
+
+        List<String> list = new ArrayList<>(Emails);
+        list.sort(Comparator.naturalOrder());
+
+        answer = list;
+
         return answer;
+    }
+
+    private static void checkSub(String nick, String email, Set patterns, Set Emails) {
+        for (int i = 0; i < nick.length(); i++) {
+            for (int j = i+2; j <= nick.length(); j++) {
+                String sub = nick.substring(i,j);
+
+                if (patterns.contains(sub)) {
+                    Emails.add(email);
+                } else {
+                    patterns.add(sub);
+                }
+            }
+        }
     }
 }
