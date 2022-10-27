39e1e21ebb5b0b934b7babef9118f2b6de4d3db0
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-28 01:12:23 +0900

    Feat: 구현 Problem7 visitors에 있는 사람은 갯수당 1점 얻는것 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.Set;
 
 public class Problem7 {
+	private final int MAX_PERSON_SHOW = 5;
+
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 		List<String> answer = Collections.emptyList();
 		Set<String> userFriends = new HashSet<>();
@@ -24,13 +26,18 @@ public class Problem7 {
 		for (int i = 0; i < friends.size(); i++) {
 			String idA = friends.get(i).get(0);
 			String idB = friends.get(i).get(1);
-			if (userFriends.contains(idA)) {
+			if (userFriends.contains(idA) && !userFriends.contains(idB)) {
 				friendScore.put(idB, friendScore.getOrDefault(idB, 0) + 10);
-			} else if (userFriends.contains(idB)) {
+			} else if (userFriends.contains(idB) && !userFriends.contains(idA)) {
 				friendScore.put(idA, friendScore.getOrDefault(idA, 0) + 10);
 			}
 		}
-		
+		for (int i = 0; i < visitors.size(); i++) {
+			String visitor = visitors.get(i);
+			if (!userFriends.contains(visitor)) {
+				friendScore.put(visitor, friendScore.getOrDefault(visitor, 0) + 1);
+			}
+		}
 		return answer;
 	}
 }
