309cd7c721390969c620d5207ee78c5c17fa99b1
code-tree <dltjdqls1200@naver.com>
2022-10-27 18:04:27 +0900

    feat: Create withdraw service

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,19 +3,30 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Problem5 {
+    static final int[] monetaryUnit = new int[] {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-        int[] monetaryUnit = new int[] {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+        List<Integer> exchangeResult = new ArrayList<>();
 
         if (money < 1 || money > 1_000_000) {
             return Collections.emptyList();
         }
-        
 
-        return answer;
+        withdrawMoney(exchangeResult, money);
+
+        return exchangeResult;
+    }
+
+    private static void withdrawMoney(List<Integer> exchangeResult, int money) {
+        int monetaryCount;
+
+        for (int i = 0; i < monetaryUnit.length; i++) {
+            monetaryCount = money / monetaryUnit[i];
+
+            exchangeResult.add(monetaryCount);
+            money -= (monetaryCount * monetaryUnit[i]);
+        }
     }
 }
