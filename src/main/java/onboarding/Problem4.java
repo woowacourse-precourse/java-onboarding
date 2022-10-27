84175205ea0b8b73c0478b3926054bd569444157
minju412 <ln8847@ajou.ac.kr>
2022-10-27 10:37:50 +0900

    solve prob 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] charArr = new char[word.length()];
+        int tmp=0;
+
+        for (int i=0; i<word.length(); i++) {
+            charArr[i] = word.charAt(i);
+        }
+
+        for (int i=0; i<word.length(); i++) {
+            if (charArr[i] > 'a' && charArr[i] < 'z') {
+                tmp = charArr[i] - 96; // 앞에서부터 몇 번째 캐릭터인지 -> tmp
+                charArr[i] = (char)(123 - tmp); // 뒤에서부터 tmp 번째 캐릭터를 대입
+            } else if (charArr[i] > 'A' && charArr[i] < 'Z') {
+                tmp = charArr[i] - 64;
+                charArr[i] = (char)(91 - tmp);
+            }
+        }
+
+        return new String(charArr);
     }
 }
