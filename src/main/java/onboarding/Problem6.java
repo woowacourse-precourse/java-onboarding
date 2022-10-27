5a569f3c2db6e7454d232f5d3d58215737cc5aa6
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-27 18:03:51 +0900

    refactor: Problem6 - 변수명 수정
    - makeTwoLetterList의 변수명 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,24 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         HashMap<String, Integer> userTwoLetterList = new HashMap<>();
+
         List<String> answer = List.of("answer");
         for (List<String> form : forms) {
-            makeUserTwoLetterList(form.get(1), userTwoLetterList);
+            makeTwoLetterList(form.get(1), userTwoLetterList);
         }
 
         return answer;
     }
 
-    public static void makeUserTwoLetterList(String nickName, HashMap<String, Integer> userTwoLetterList) {
+    public static void makeTwoLetterList(String nickName, HashMap<String, Integer> twoLetterList) {
         String[] splitNickName = nickName.split("");
         for (int i = 0; i < splitNickName.length-1; i++) {
             String userTwoLetter = splitNickName[i]+splitNickName[i+1];
-            userTwoLetterList.put(userTwoLetter, userTwoLetterList.getOrDefault(userTwoLetter, 0) + 1);
+            twoLetterList.put(userTwoLetter, twoLetterList.getOrDefault(userTwoLetter, 0) + 1);
         }
     }
 }
