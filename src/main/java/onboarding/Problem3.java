ead7221dc75dd3c207401fa6c2956527501cd917
lecocococo <chemwang13@naver.com>
2022-10-27 22:55:29 +0900

    Feat: Problem3의 주어진 숫자가 3,6,9인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,14 @@
 package onboarding;
 
 public class Problem3 {
+
+	public static boolean check369(char num) {
+		if (num == '3'||num == '6'||num == '9') {
+			return true;
+		}
+		return false;
+	}
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
