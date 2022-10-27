2c6dc2096c31352a078627a142e118bf73e79a75
dooboocookie <bkh3030@gmail.com>
2022-10-27 12:58:59 +0900

    5번 문제 풀이

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        //제일 큰 단위부터 체크해야 조건에 맞음
+        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        int count;
+        for (int i = 0; i < unit.length; i++) {
+            // 현재 금액권으로 해결할 수 있는 돈
+            count = money / unit[i];
+            answer.add(count);
+            // 나머지 금액
+            money = money % unit[i];
+        }
         return answer;
     }
 }
