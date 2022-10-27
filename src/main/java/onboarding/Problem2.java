8cf17b0ea690a32230145bdad5c6a2b45b567361
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-27 10:16:21 +0900

    feat: Problem2 연속으로 중복된 인덱스 반환 메소드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem2 {
+    private static final int NO_DUPLICATE = -1;
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+    private static int duplicateIndex(String checkDuplicate) {
+        for(int index=0;index<checkDuplicate.length()-1;index++){
+            if(checkDuplicate.charAt(index)==checkDuplicate.charAt(index+1))return index;
+        }
+        return NO_DUPLICATE;
     }
 }
