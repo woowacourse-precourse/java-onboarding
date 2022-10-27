8b5fad429394d9f7b7ef3b2f05aba0a8378e30cb
김현석 <mrbonk97@gmail.com>
2022-10-28 01:13:37 +0900

    2번 문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        StringBuilder sb = new StringBuilder();
+        Deque<Character> word = new ArrayDeque<>();
+
+        for(Character e: cryptogram.toCharArray())
+        {
+            if(!word.isEmpty() && word.peekLast() == e) { word.pollLast(); continue; }
+            word.addLast(e);
+        }
+
+        for(Character e: word) sb.append(e);
 
 
+        return sb.toString();
 
-        String answer = "answer";
-        return answer;
     }
 }
