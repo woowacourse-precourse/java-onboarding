7a748781c60464094bbfe518d79b85fa4a674d35
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-10-27 10:29:51 +0900

    problem1 페이지번호 더하기,곱하기 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,9 +6,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int leftpage = pobi.get(0);
-
-
-
+        sperater(leftpage);
 
 
         int answer = Integer.MAX_VALUE;
@@ -17,11 +15,26 @@ class Problem1 {
 
     public static ArrayList<Integer> sperater(int page) {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        while(page > 0) {
+        while (page > 0) {
             numbers.add(page % 10);
             page /= 10;
         }
         return numbers;
     }
-}
 
+    public static int add(ArrayList<Integer> numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            sum += numbers.get(i);
+        }
+        return sum;
+    }
+
+    public static int multiply(ArrayList<Integer> numbers) {
+        int sum = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            sum *= numbers.get(i);
+        }
+        return sum;
+    }
+}
