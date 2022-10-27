28a81b53bfb4a0abc5221ffa70624614f3bb5774
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-27 16:06:39 +0900

    문제 4 - 문자열을 배열로 변환, 상수 배열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	static final String[] UPPER_CASE_ARR = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
+	static final String[] LOWER_CASE_ARR = {"z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"};
+	public static String solution(String word) {
+
+		//문자열을 배열로 변환
+		char[] arr = word.toCharArray();
+		String answer = "";
+		return answer;
+	}
+
 }
