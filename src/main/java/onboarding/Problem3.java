dc7020c346fbe9e0fadb2bccccb44abc979ac064
HyunggiPark <phg9587@naver.com>
2022-10-27 22:10:26 +0900

    feat : for loop를 통해서 모든 수에 대해서 접근하고 모든 수에 대해서 몇번 박수를 칠 수 있는지 세어서 answer에 더함

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += countClapNum(i);
+        }
+
         return answer;
+    }
+
+    private static int countClapNum(int number) {
+        String numberStr = String.valueOf(number);
+
+        int rtVal = 0;
+
+        for (int i = 0; i < numberStr.length(); i++) {
+            int c = numberStr.charAt(i) - '0';
+
+            if (c != 0 && c % 3 == 0) {
+                rtVal++;
+            }
+        }
+
+        return rtVal;
     }
 }
