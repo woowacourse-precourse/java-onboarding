04ff9dfc29322fe2a37d9f3f46986f05ad8d84d2
RumbleKAT <reki318@naver.com>
2022-10-27 23:00:01 +0900

    (feat) problem2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        char [] temp = cryptogram.toCharArray();
+        boolean isConverting = true;
+
+        while(isConverting){
+            boolean check = false;
+            Stack<Character> stack = new Stack<>();
+
+            for(char c : temp){
+                if(!stack.isEmpty() && stack.peek() == c){
+                    stack.pop();
+                    check = true;
+                }else
+                    stack.push(c);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(Character c : stack){
+                sb.append(c);
+            }
+            answer = sb.toString();
+            temp = sb.toString().toCharArray();
+            isConverting = check;
+        }
+
         return answer;
     }
 }
