da08b677d73bb34ff65c1c48832d7a99adc76b3f
ycopp9 <aciees95@gmail.com>
2022-10-27 19:41:45 +0900

    feat: add the function addEmailSet() that adds the emails with some matching nicknames to set

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
+    private static Map<String,List<String>> nicknameMap = new HashMap<>();
+    private static Set<String> emailSet = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static void addEmailSet(List<String> emailList) {
+        for(String email : emailList) {
+            emailSet.add(email);
+        }
+    }
+
+
+
+
+
 }
