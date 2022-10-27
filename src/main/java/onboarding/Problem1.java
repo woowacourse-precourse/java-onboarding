b489503bb3c36d71ef9d2f5e631be22ff1ee7b9e
fakerdeft <whakswp123@naver.com>
2022-10-27 23:19:28 +0900

    Problem1.java clear

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,143 +6,113 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        int p1 = pobi.get(0); //포비 왼쪽 페이지
-        int p2 = pobi.get(1); //포비 오른쪽 페이지
-        int c1 = crong.get(0); //크롱 왼쪽 페이지
-        int c2 = crong.get(1); //크롱 오른쪽 페이지
-        int n1, n2, n3, sum, mul, lNum, rNum, pNum, cNum = 0;
+        int pobiLeftNum = pobi.get(0); //포비 왼쪽 페이지
+        int pobiRightNum = pobi.get(1); //포비 오른쪽 페이지
+        int crongLeftNum = crong.get(0); //크롱 왼쪽 페이지
+        int crongRightNum = crong.get(1); //크롱 오른쪽 페이지
 
-        if (p1 == (p2 - 1) && (p1>1&&c1>1) && (400>p2&&400>c2) ) {
-            //연속 페이지 and 왼쪽 페이지 1초과 and 오른쪽 페이지 400미만인 경우
-
-            ////포비
-            if (p1>= 100) {//포비 왼쪽 페이지가 100이상
-                n1 = p1 / 100;
-                n2 = (p1 % 100) / 10;
-                n3 = p1 % 10;
-                sum = n1 + n2 + n3;
-                mul = n1 * n2 * n3;
-                if (sum >= mul) {//합이 큰 경우
-                    lNum = sum;
-                } else {//곱이 큰 경우
-                    lNum = mul;
-                }
-            } else if (p1 >= 10) {//두자리 수인 경우
-                n1 = p1 / 10;
-                n2 = p1 % 10;
-                sum = n1 + n2;
-                mul = n1 * n2;
-                if (sum >= mul) {//합이 큰 경우
-                    lNum = sum;
-                } else {//곱이 큰 경우
-                    lNum = mul;
-                }
-            } else {//한자리인 경우
-                lNum = p1;
-            }
-
-            if (p2 >= 100) {//포비 오른쪽 페이지가 100이상
-                n1 = p2 / 100;
-                n2 = (p2 % 100) / 10;
-                n3 = p2 % 10;
-                sum = n1 + n2 + n3;
-                mul = n1 * n2 * n3;
-                if (sum >= mul) {//합이 큰 경우
-                    rNum = sum;
-                } else {//곱이 큰 경우
-                    rNum = mul;
-                }
-            } else if (p2 >= 10) {//두자리 수인 경우
-                n1 = p2 / 10;
-                n2 = p2 % 10;
-                sum = n1 + n2;
-                mul = n1 * n2;
-                if (sum >= mul) {//합이 큰 경우
-                    rNum = sum;
-                } else {//곱이 큰 경우
-                    rNum = mul;
-                }
-            } else {//한자리인 경우
-                rNum = p2;
-            }
-
-            if (lNum > rNum) {
-                pNum = lNum;
-            } else {
-                pNum = rNum;
-            }
-
-
-            ////크롱
-            if (c1 >= 100) {//크롱 왼쪽 페이지가 100이상
-                n1 = c1 / 100;
-                n2 = (c1 % 100) / 10;
-                n3 = c1 % 10;
-                sum = n1 + n2 + n3;
-                mul = n1 * n2 * n3;
-                if (sum >= mul) {//합이 큰 경우
-                    lNum = sum;
-                } else {//곱이 큰 경우
-                    lNum = mul;
-                }
-            } else if (c1 >= 10) {//두자리 수인 경우
-                n1 = c1 / 10;
-                n2 = c1 % 10;
-                sum = n1 + n2;
-                mul = n1 * n2;
-                if (sum >= mul) {//합이 큰 경우
-                    lNum = sum;
-                } else {//곱이 큰 경우
-                    lNum = mul;
-                }
-            } else {//한자리 수인 경우
-                lNum = c1;
-            }
-
-            if (c2 >= 100) {//크롱 오른쪽 페이지가 100이상
-                n1 = c2 / 100;
-                n2 = (c2 % 100) / 10;
-                n3 = c2 % 10;
-                sum = n1 + n2 + n3;
-                mul = n1 * n2 * n3;
-                if (sum >= mul) {//합이 큰 경우
-                    rNum = sum;
-                } else {//곱이 큰 경우
-                    rNum = mul;
-                }
-            } else if (c2 >= 10) {//두자리 수인 경우
-                n1 = c2 / 10;
-                n2 = c2 % 10;
-                sum = n1 + n2;
-                mul = n1 * n2;
-                if (sum >= mul) {//합이 큰 경우
-                    rNum = sum;
-                } else {//곱이 큰 경우
-                    rNum = mul;
-                }
-            } else {//한자리 수인 경우
-                rNum = c2;
-            }
-
-            if (lNum > rNum) {
-                cNum = lNum;
-            } else {
-                cNum = rNum;
-            }
-
-            if (pNum > cNum) {//포비>크롱
-                answer = 1;
-            } else if (pNum < cNum) {//포비<크롱
-                answer = 2;
-            } else {//포비=크롱
-                answer = 0;
-            }
-
-        }else {//불연속 페이지 or 왼쪽 페이지 1이하 or 오른쪽 페이지 400이상인 경우
-            answer=-1;
-        }
+        answer=pageCheck(pobiLeftNum,pobiRightNum,crongLeftNum,crongRightNum);
 
         return answer;
 
     }//public static int solution
+
+    public static int pageCheck(int pobiLeftNum,int pobiRightNum
+                                ,int crongLeftNum,int crongRightNum){
+        int result = 0;
+        int pLeftWinNum = 0;
+        int pRightWinNum = 0;
+        int cLeftWinNum = 0;
+        int cRightWinNum = 0;
+        int pobiNum = 0;
+        int crongNum = 0;
+
+        if (pobiLeftNum == pobiRightNum-1
+            && crongLeftNum == crongRightNum-1
+            && pobiLeftNum > 1
+            && crongLeftNum > 1
+            && 400 > pobiRightNum
+            && 400 > crongRightNum
+            && pobiLeftNum%2 == 1
+            && crongLeftNum%2 == 1
+            && pobiRightNum%2 == 0
+            && crongRightNum%2 == 0) {
+
+            pLeftWinNum = pageCompare(pageSum(pobiLeftNum),pageSum(pobiLeftNum));
+            pRightWinNum = pageCompare(pageSum(pobiRightNum),pageMul(pobiRightNum));
+
+            cLeftWinNum = pageCompare(pageSum(crongLeftNum),pageMul(crongRightNum));
+            cRightWinNum = pageCompare(pageSum(crongRightNum),pageMul(crongRightNum));
+
+            pobiNum = pageCompare(pLeftWinNum,pRightWinNum);
+            crongNum = pageCompare(cLeftWinNum,cRightWinNum);
+
+            result = pageMatch(pobiNum,crongNum);
+
+            return result;
+        }
+        return -1;
+    }// public static int pageCheck
+
+    public static int pageSum(int page){
+        int page1 = 0;
+        int page2 = 0;
+        int page3 = 0;
+        int sum = 0;
+
+        if(page>=10){
+            page1 = page/100;
+            page2 = (page%100)/10;
+            page3 = page%10;
+            sum = page1 + page2 + page3;
+            return sum;
+        }
+        sum = page;
+        return sum;
+    }//public static int sum
+
+    public static int pageMul(int page){
+        int page1 = 0;
+        int page2 = 0;
+        int page3 = 0;
+        int mul = 0;
+
+        page1 = page/100;
+        page2 = (page%100)/10;
+        page3 = page%10;
+
+        if(page1 != 0) {
+            mul = page1 * page2 * page3;
+            return mul;
+        }
+        if(page2 != 0) {
+            mul = page2 * page3;
+            return mul;
+        }
+        mul = page;
+        return mul;
+    }//public static int mul
+
+    public static int pageCompare(int num1,int num2){
+        int winNum = 0;
+
+        if(num1>num2){
+            winNum = num1;
+            return winNum;
+        }
+        winNum = num2;
+        return winNum;
+    }//public static int pageCompare
+
+    public static int pageMatch(int pobiNum,int crongNum){
+        if (pobiNum > crongNum) {//포비>크롱
+            return 1;
+        }
+        if (pobiNum < crongNum) {//포비<크롱
+            return 2;
+        }
+        //포비=크롱
+        return 0;
+    }//public static int pageMatch
+
 }//class Problem1
