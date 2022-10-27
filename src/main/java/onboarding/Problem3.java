8ab8f8af498f2512a3806202c0123586ea45cf7d
whipbaek <jibaek0513@gmail.com>
2022-10-27 19:05:59 +0900

    P3 Add getCount Method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int getCount(int val){
+        int count = 0;
+        while(val > 0){
+            int temp = val % 10;
+            if( (temp == 3) || (temp == 6) || (temp == 9)) count+=1;
+            val /= 10;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
