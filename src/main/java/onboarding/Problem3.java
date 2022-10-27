2688fc844b84637a9e3073e1262fe0cbe8a70bf4
apptie <appti@naver.com>
2022-10-27 11:51:41 +0900

    refactor: 문제3 박수 계산 메소드를 ClapCalculator 클래스로 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,7 @@ package onboarding;
 import static onboarding.Problem3.CalculateClapCount.CALCULATE_REMAINDER_VALUE;
 import static onboarding.Problem3.CalculateClapCount.DECIMAL_UNIT_VALUE;
 import static onboarding.Problem3.ClapCacheConst.CHECK_CACHE_VALUE;
+import static onboarding.Problem3.ClapCalculator.calculateTotalClapCount;
 import static onboarding.Problem3.ClapNumberConst.CLAP_MAX_VALUE;
 import static onboarding.Problem3.ClapNumberConst.CLAP_NINE;
 import static onboarding.Problem3.ClapNumberConst.CLAP_SIX;
@@ -19,45 +20,48 @@ public class Problem3 {
         return calculateTotalClapCount(number);
     }
 
-    private static int calculateTotalClapCount(int number) {
-        int totalClapCount = 0;
+    public static class ClapCalculator {
 
-        for (int i = CLAP_START_NUMBER; i <= number; i++) {
-            totalClapCount += calculateTargetClapCount(i);
-        }
+        public static int calculateTotalClapCount(int number) {
+            int totalClapCount = 0;
 
-        return totalClapCount;
-    }
-
-    private static int calculateTargetClapCount(int target) {
-        int clapCount = 0;
-
-        while (target != CALCULATE_REMAINDER_VALUE) {
-            int targetDigit = target % DECIMAL_UNIT_VALUE;
-            target /= DECIMAL_UNIT_VALUE;
-
-            if (isCached(targetDigit)) {
-                clapCount += clapCountArr[targetDigit];
-                continue;
+            for (int i = CLAP_START_NUMBER; i <= number; i++) {
+                totalClapCount += calculateTargetClapCount(i);
             }
-            clapCount += calculateTargetDigitClapCount(targetDigit);
+
+            return totalClapCount;
         }
-        return clapCount;
-    }
 
-    private static int calculateTargetDigitClapCount(int target) {
-        if (isClapped(target)) {
-            return clapCountArr[target] = clapCountArr[target] + CLAP_VALUE;
+        private static int calculateTargetClapCount(int target) {
+            int clapCount = 0;
+
+            while (target != CALCULATE_REMAINDER_VALUE) {
+                int targetDigit = target % DECIMAL_UNIT_VALUE;
+                target /= DECIMAL_UNIT_VALUE;
+
+                if (isCached(targetDigit)) {
+                    clapCount += clapCountArr[targetDigit];
+                    continue;
+                }
+                clapCount += calculateTargetDigitClapCount(targetDigit);
+            }
+            return clapCount;
         }
-        return NOT_CLAP_VALUE;
-    }
 
-    private static boolean isClapped(int target) {
-        return target == CLAP_THREE || target == CLAP_SIX || target == CLAP_NINE;
-    }
+        private static int calculateTargetDigitClapCount(int target) {
+            if (isClapped(target)) {
+                return clapCountArr[target] = clapCountArr[target] + CLAP_VALUE;
+            }
+            return NOT_CLAP_VALUE;
+        }
 
-    private static boolean isCached(int target) {
-        return clapCountArr[target] > CHECK_CACHE_VALUE;
+        private static boolean isClapped(int target) {
+            return target == CLAP_THREE || target == CLAP_SIX || target == CLAP_NINE;
+        }
+
+        private static boolean isCached(int target) {
+            return clapCountArr[target] > CHECK_CACHE_VALUE;
+        }
     }
 
     public abstract class ClapNumberConst {
