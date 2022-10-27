367595681031c598a645f8a744ba30f77916c74d
LHS-11 <ghktjq1119@naver.com>
2022-10-27 22:21:03 +0900

    Problem5 : 3. 남아있는 돈 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -22,13 +22,22 @@ public class Problem5 {
 
         ArrayList<Integer> arrayList = new ArrayList<>(answer);
 
-//        2. 지갑에 최대한 담을 수 있는 갯수 구하기 (지갑이 최대한 가볍도록 큰 값의 지폐부터 담아야 함)
 
 //        3. 출금한 돈에서 지갑에 담은 돈 빼기 => 출금해야할 남아있는 돈 만들기
+        money -= FIFTY_THOUSAND_WON * getCoin(money, FIFTY_THOUSAND_WON, 0, arrayList);
+        money -= TEN_THOUSAND_WON * getCoin(money, TEN_THOUSAND_WON, 1, arrayList);
+        money -= FIVE_THOUSAND_WON * getCoin(money, FIVE_THOUSAND_WON, 2, arrayList);
+        money -= ONE_THOUSAND_WON * getCoin(money, ONE_THOUSAND_WON, 3, arrayList);
+        money -= FIVE_HUNDRED_WON * getCoin(money, FIVE_HUNDRED_WON, 4, arrayList);
+        money -= ONE_HUNDRED_WON * getCoin(money, ONE_HUNDRED_WON, 5, arrayList);
+        money -= FIFTY_WON * getCoin(money, FIFTY_WON, 6, arrayList);
+        money -= TEN_WON * getCoin(money, TEN_WON, 7, arrayList);
+        money -= ONE_WON * getCoin(money, ONE_WON, 8, arrayList);
 
         return arrayList;
     }
 
+    //        2. 지갑에 최대한 담을 수 있는 갯수 구하기 (지갑이 최대한 가볍도록 큰 값의 지폐부터 담아야 함)
     private static int getCoin(int money, int won,int idx, ArrayList<Integer> arrayList) {
         int i=1000000/won;
         for(;i>=0;i--){
