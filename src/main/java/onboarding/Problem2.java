ff00620eebb2e06447cb955acc73207ab3cf5c32
Choi-JY <Choi-JY1107@github.com>
2022-10-27 23:44:45 +0900

    feat: 2번문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+
+import java.util.Arrays;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        boolean[] eraser = new boolean[1010];
+        boolean flag = true;
+        String answer = cryptogram;
+
+        while(flag){
+            int stringSize = answer.length();
+            String temp = "";
+
+            for(int i=0; i<stringSize; ++i)
+                if(eraser[i] == false) temp += answer.charAt(i);
+            answer = temp;
+            if(answer.isEmpty()) break;
+
+            Arrays.fill(eraser, false);
+            char preString = answer.charAt(0);
+            flag = false;
+            stringSize = answer.length();
+            for(int i=1; i<stringSize; ++i){
+                if(Character.compare(preString, answer.charAt(i)) == 0){
+                    eraser[i-1] = true;
+                    eraser[i] = true;
+                    flag = true;
+                }
+                preString=answer.charAt(i);
+            }
+
+        }
+
         return answer;
     }
 }
