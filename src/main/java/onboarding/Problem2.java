24bbd0f7848e1d3f6beef1daad88a08a130c9517
swandevson <shai4956@gmail.com>
2022-10-28 03:39:50 +0900

    feat: P2 암호 해독 기능
    해독 전과 해독 후의 암호가 같을 때 까지 해독한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static void decrypt(String cryptogram) {
+
+    }
+
+    public static String solveCrypto(String cryptogram) {
+        String decryptogram = cryptogram;
+
+        while(!decryptogram.equals(cryptogram)) {
+            decryptogram = cryptogram;
+            decrypt(cryptogram);
+        }
+
+        return decryptogram;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        solveCrypto(cryptogram);
+
+        return cryptogram;
     }
 }
