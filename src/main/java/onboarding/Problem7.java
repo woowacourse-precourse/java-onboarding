d2993432e3b2834154d9af9cf14a1eedd9eeb702
HwangSeokJin <ghkd3703@gmail.com>
2022-10-28 03:11:31 +0900

    사용자와 함께 아는 친구의 수 만큼 추천 점수를 매기는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,13 +2,15 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Problem7 {
+    private static Map<String, HashSet<String>> graph = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        HashMap<String, HashSet<String>> graph = new HashMap<>();
+        Map<String, Integer> scores = new HashMap<>();
         graph.put(user, new HashSet<>());
 
         for (List<String> idList: friends){
@@ -21,6 +23,19 @@ public class Problem7 {
             graph.get(idA).add(idB);
             graph.get(idB).add(idA);
         }
+
+        addScoreByMutualFriend(scores, user);
+
         return answer;
+    }
+
+    private static void addScoreByMutualFriend(Map<String, Integer> scores, String user){
+        for (String friend: graph.get(user)){
+            for (String name: graph.get(friend)){
+                if (name.equals(user)) continue;
+                if (graph.get(user).contains(name)) continue;
+                scores.put(name, scores.getOrDefault(name, 0) + 10);
+            }
+        }
     }
 }
