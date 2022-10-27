7445a9fb4beb37c1f704ec98ba7bff04634f3338
iamjooon2 <iamjooon2@gmail.com>
2022-10-28 00:54:28 +0900

    refactor: Problem3 - 369가 존재하는지 확인하는 함수 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,14 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        String sTemp;
         int count = 0;
         for (int i = 1; i <= number; i += 1) {
-            sTemp = i + "";
-            for (int j = 0; j < sTemp.length(); j += 1) {
-                if (sTemp.charAt(j) == '3' || sTemp.charAt(j) == '6' || sTemp.charAt(j) == '9') {
-                    count += 1;
-                }
+            count = check369Exists(i, count);
+        }
+        return count;
+    }
+    public static int check369Exists(int number, int count) {
+        String sTemp = number + "";
+        for (int j = 0; j < sTemp.length(); j += 1) {
+            if (sTemp.charAt(j) == '3' || sTemp.charAt(j) == '6' || sTemp.charAt(j) == '9') {
+                count += 1;
             }
         }
         return count;
