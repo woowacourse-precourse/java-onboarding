efc49c165cb95dccb17cf7772d9aead8e3c9e7e2
KimTaeKang57 <zizoen123@gmail.com>
2022-10-27 14:05:17 +0900

    문제 2 : 풀이방법

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+import java.util.*;
+
+/**
+ * [풀이방법]
+ * queue 에 넣으면서 확인하고 같으면 poll, 다르면 offer
+ **/
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
