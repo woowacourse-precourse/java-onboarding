a2ea3e9753fe0405fc5d0a29ad262e841ac46783
MoonJeWoong <munjin0201@naver.com>
2022-10-27 22:42:24 +0900

    [2번 문제] docs: 1차 요구사항 체크리스트 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem2 {
+    //앞 문자를 기억하고 있다가 같은 문자가 바로 나오면 삭제 후 전 문자를 다시 기억
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
