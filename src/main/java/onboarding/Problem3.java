e61225d397067e8d3d24105690c8fbcd1af1b9fc
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-10-27 19:38:39 +0900

    feat: problem 3, countClaps Function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    public static int countClaps(int number){
+        int claps = 0;
+        while(number > 0){
+            int digit = number%10;
+            if(digit == 3 || digit == 6 || digit == 9){
+                claps += 1;
+            }
+            number = number/10;
+        }
+        return claps;
     }
 }
