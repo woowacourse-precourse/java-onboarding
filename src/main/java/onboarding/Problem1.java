e7ab1ebe6588db9aa21d6611f4692d24ee6a8160
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-10-27 13:51:15 +0900

    feat : [problem1] validatePages()메소드 작성 (기능 요구사항 6) 예외사항 이므로 exception을 발생시켜 -1을 return하도록 작성 제한사항에 pobi와 crong의 길이는 2, pobi와 crong에는 [왼쪽 페이지 번호, 오른쪽 페이지 번호] 순서라고 명시되어 있지만, 최대한 정확하고 자세하게 예외를 잡고싶은 마음에 내가 생각하는 모든 경우의 예외사항을 입력하였다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,14 @@ class Problem1 {
     //pobi와 crong의 길이는 2이다.
     //pobi와 crong에는 [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있다.
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        //유효성 체크
+        try{
+            validatePages(pobi);
+            validatePages(crong);
+        } catch (IllegalArgumentException e){
+            return -1;
+        }
+
         // 왼쪽, 오른쪽 페이지로 만들 수 있는 숫자 만들기
         int pobiLeftMakeNum = makeNum(pobi.get(0));
         int pobiRightMakeNum = makeNum(pobi.get(1));
@@ -17,7 +25,7 @@ class Problem1 {
         // 만든 수 중 가장 큰 수 찾기
         int pobiMaxNum = returnMaxNum(pobiLeftMakeNum,pobiRightMakeNum);
         int crongMaxNum = returnMaxNum(crongLeftMakeNum,crongRightMakeNum);
-        
+
         // 가장 큰 수를 찾은 것 중 비교하여 결과값 return
         return compareNums(pobiMaxNum,crongMaxNum);
     }
@@ -56,4 +64,41 @@ class Problem1 {
         else return 2;
     }
 
+    public static void validatePages(List<Integer> page){
+        int leftPage = page.get(0);
+        int rightPage = page.get(1);
+
+        // 페이지개수가 2가 아닐 때
+        if(page.size() != 2){
+            throw new IllegalArgumentException("두 페이지를 입력해주세요!");
+        }
+
+        //왼쪽은 홀수, 오른쪽은 짝수 페이지가 아닐 때
+        if(leftPage % 2 != 1 && rightPage % 2 != 0){
+            throw new IllegalArgumentException("왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수로 입력해주세요!");
+        }
+
+        //오른쪽 짝수 페이지가 왼쪽 홀수 페이지의 다음 페이지가 아닐 때
+        if(rightPage - leftPage != 1){
+            throw new IllegalArgumentException("연속된 페이지를 입력해주세요!");
+        }
+
+        //페이지가 시작 면이나 마지막 면일 때
+        if(leftPage == 1){
+            throw new IllegalArgumentException("페이지가 시작면 입니다!");
+        }
+
+        if(rightPage == 400){
+            throw new IllegalArgumentException("페이지가 마지막면 입니다.!");
+        }
+
+        //페이지 범위가 1~400 페이지가 아닐 때
+        if(leftPage<1 || leftPage>399){
+            throw new IllegalArgumentException("페이지 범위를 1~400로 지정해주세요!");
+        }
+
+        if(rightPage <2 || rightPage > 400){
+            throw new IllegalArgumentException("페이지 범위를 1~400로 지정해주세요!");
+        }
+    }
 }
