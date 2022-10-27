ce4339fe65e0d54a7a88d79b5d399196701fee0a
asas6978 <asas6978@naver.com>
2022-10-27 19:31:08 +0900

    feat(problem1): Validation 기능 구현

---
+++ src/main/java/onboarding/problem1/Validation.java
@@ -0,0 +1,44 @@
+package onboarding.problem1;
+
+import java.util.List;
+
+public class Validation {
+    public static final int START_RIGHT_PAGE = 2;
+    public static final int END_RIGHT_PAGE = 400;
+
+    public static boolean isValidPage(List<Integer> list) {
+        boolean result = true;
+        Integer left = list.get(0);
+        Integer right = list.get(1);
+
+        for (Integer integer : list) {
+            result = (isPositiveNumber(integer) && !isPageBothEnds(integer));
+        }
+
+        if(!isOdd(left) || !isEven(right) || !isContinuousNumber(left, right)) {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public static boolean isPositiveNumber(Integer integer) {
+        return integer > 0;
+    }
+
+    private static boolean isPageBothEnds(Integer integer) {
+        return integer <= START_RIGHT_PAGE || integer >= END_RIGHT_PAGE;
+    }
+
+    public static boolean isOdd(Integer integer) {
+        return integer % 2 != 0;
+    }
+
+    public static boolean isEven(Integer integer) {
+        return integer % 2 == 0;
+    }
+
+    public static boolean isContinuousNumber(Integer left, Integer right) {
+        return left == right - 1;
+    }
+}
