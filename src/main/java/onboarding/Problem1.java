948546018ba62212889459fe640e65f9d4a2b4e9
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-27 14:13:08 +0900

    feat : 각 자리 숫자의 합과 곱을 구하는 getScore()메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,22 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+        int pobiScore = 0;
+        int crongScore = 0;
+        
+        int getScore(int n) {
+            int sum = 0;
+            int multiply = 1;
+
+            while (n != 0) {
+                sum += n % 10;
+                multiply *= n % 10;;
+                n /= 10;
+            }
+
         return answer;
+        
     }
+}
 }
