2a289991712fe718a85192ad107f0ae8960b8b29
ycopp9 <aciees95@gmail.com>
2022-10-27 17:10:53 +0900

    feat: (problem5) modify the function solution() that meets the requirements

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     private final static int[] UNIT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < UNIT.length; i++) {
+            answer.add(getNumOfBillsOrCoins(i, money));
+            money = change(i, answer.get(i), money);
+        }
         return answer;
     }
     private static int getNumOfBillsOrCoins(int i, int money) {
