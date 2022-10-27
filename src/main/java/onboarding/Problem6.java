e19caea14ea3cb07821a570102137b709a45b0cf
JHS <wnghdtjr129@naver.com>
2022-10-27 18:45:04 +0900

    feat(Problem6): [문제 6] 2~최대 길이까지 닉네임 분할

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    static List<String> answer = new ArrayList<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        for (List<String> crew : forms) {
+            if (cannotUser(crew.get(1))) {
+
+            }
+        }
         return answer;
+    }
+
+    private static boolean cannotUser(String nickname) {
+        boolean result = false;
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            for (int j = i + 2; j < nickname.length(); j++) {
+                String subNickname = nickname.substring(i, j);
+
+            }
+        }
+        return result;
     }
 }
