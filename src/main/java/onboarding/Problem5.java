4d304084d23747479bdf18e8c25d02670cc8324f
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-27 14:52:23 +0900

    feat: Problem5 단위별로 잔액 나눠주는 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,10 +6,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-
+    private static final int EMPTY = 0;
     public static List<Integer> solution(int money) {
-        List<Integer> answer =Collections.emptyList();
+        List<Integer> answer =changeAsBig(moneyType(),money);
         return answer;
+    }
+    private static List<Integer> changeAsBig(List<Integer> moneyType, int money) {
+        int length = moneyType.size();
+        List<Integer> change = new ArrayList<>();
+        for(int index =0;index<length;index++){
+            int nowType = moneyType.get(index);
+            if(nowType>money)change.add(EMPTY);
+            if(nowType<=money) {
+                change.add(money / nowType);
+                money = money % nowType;
+            }
+        }
+        return change;
     }
     private static List<Integer> moneyType(){
         List<Integer> moneyTypeList = new ArrayList<>();
