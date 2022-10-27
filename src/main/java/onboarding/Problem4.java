fa120cbd6f79a7552f788cd164a60b6926ac260c
jthugg <jthug@naver.com>
2022-10-27 21:17:40 +0900

    init problem4 & chk todo list

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        // todo list
+        // 알파벳 치트시트
+        // 대소문자 구분 변환 루프
+        // 알파벳 변환만 구현하면 됨
+        // 알파벳인지 아닌지 체크하는 기능
+        StringBuilder sb = new StringBuilder();
+        return sb.toString();
     }
 }
