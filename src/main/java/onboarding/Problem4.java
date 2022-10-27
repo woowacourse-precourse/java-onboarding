cf735c08468b628846fb8d91f5bd6f204f7bf51b
ys200209 <lys200209@naver.com>
2022-10-27 18:17:48 +0900

    feat(문제4): 문자가 알파벳인지 아닌지 구분한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,29 @@ public class Problem4 {
     public static String solution(String word) {
          String answer = "";
 
+        for (int i = 0; i < word.length(); i++) {
+
+            if (isSpelling(word.charAt(i))) {
+
+            } else {
+
+            }
+
+        }
+
         return answer;
+    }
+
+    private static boolean isSpelling(char spelling) {
+
+        if (isUpperCase(spelling)) {
+            return true;
+        } else if (isLowerCase(spelling)) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     private static boolean isUpperCase(char spelling) {
