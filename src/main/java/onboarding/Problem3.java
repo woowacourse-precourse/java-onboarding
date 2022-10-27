673c027bd726c1ce2ab247b345d7d06b21b6d2f7
hyun-seung <ardimento22@naver.com>
2022-10-27 11:47:20 +0900

    [FEAT] 주어진 숫자에 3,6,9가 들어간 갯수를 구한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    public static int count(int targetNumber) {
+        int result = 0;
+
+        while (targetNumber > 0) {
+            int nowNumber = targetNumber % 10;
+            if(nowNumber == 3 || nowNumber == 6 || nowNumber == 9) {
+                result++;
+            }
+            targetNumber /= 10;
+        }
+
+        return result;
     }
 }
