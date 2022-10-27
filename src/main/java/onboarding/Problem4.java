d6ce3c3cc56bd9484f1e902e22103401fb7ffd82
WhalesBob <ndw8200@naver.com>
2022-10-27 20:28:49 +0900

    Complete to make Problem4(test complete)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        char[] wordArray = word.toCharArray();
+
+        for(int i = 0; i < word.length(); i++){
+            byte asciiCode =  (byte)wordArray[i];
+            if(asciiCode >= 65 && asciiCode <= 90){
+                answer.append(convertBigLetter(wordArray[i]));
+            }else if(asciiCode >= 97 && asciiCode <= 122){
+                answer.append(convertSmallLetter(wordArray[i]));
+            }else{
+                answer.append(wordArray[i]);
+            }
+        }
+        return answer.toString();
+    }
+    static char convertBigLetter(char c){
+        int convertAscii = 90 - ((byte)c - 65);
+        return (char)convertAscii;
+    }
+    static char convertSmallLetter(char c){
+        int convertAscii = 122 - ((byte)c - 97);
+        return (char)convertAscii;
     }
 }
