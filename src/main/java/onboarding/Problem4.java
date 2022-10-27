4b9b69f23797fc648ad21990ba983af1e9439752
SEONMl <sm000117@naver.com>
2022-10-27 15:36:07 +0900

    feat: Problem4 문장을 변환할 Transformer class 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,20 @@ package onboarding;
 
 public class Problem4 {
 
+    static class Transformer {
+        private Problem4.AbnormalDictionary dictionary = new Problem4.AbnormalDictionary();
+        private String sentence;
+        private String result = "";
+
+        public Transformer(String sentence) {
+            if(sentence.length()<1 || sentence.length()>1000) {
+                throw new RuntimeException("word의 길이가 올바르지 않습니다");
+            }
+            this.sentence = sentence;
+        }
+    }
+
+
     static class AbnormalDictionary {
 
         private char findWord(char c) {
