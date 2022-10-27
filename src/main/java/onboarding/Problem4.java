aaca56a45fbe99d1ac29a98a88f716b7319aae7c
Miseong Kim <miseongk@naver.com>
2022-10-27 16:28:51 +0900

    feat: add convertChar function

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,19 @@ public class Problem4 {
         return answer;
     }
 
+    // 문자 하나를 반대로 변환하는 함수
+    static char convertChar(char c) {
+        int ascii;
+
+        if (Character.isUpperCase(c)) {
+            ascii = 'Z' - (c - 'A');
+        } else {
+            ascii = 'z' - (c - 'a');
+        }
+
+        return (char)ascii;
+    }
+
     // 알파벳인지 확인하는 함수
     static boolean isAlphabet(char c) {
         if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
