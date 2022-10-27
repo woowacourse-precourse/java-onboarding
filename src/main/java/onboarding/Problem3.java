b9bd51615f1b34038ca7f22469fa6d3617ea90b4
Jaehyun Hwang <nuyh@nuyh.local>
2022-10-27 12:52:29 +0900

    refactor(Problem3.java): IntStream 사용으로 가독성 향상

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,17 +1,17 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
-    public static int count369(int n){
-        return (int)String.valueOf(n).chars()
-                .filter(o -> o=='3' || o=='6' || o=='9')
+    public static int count369(int n) {
+        return (int) String.valueOf(n).chars()
+                .filter(o -> o == '3' || o == '6' || o == '9')
                 .count();
     }
 
     public static int solution(int number) {
-        int sum=0;
-        for(int i=1; i<=number; i++)
-            sum+=count369(i);
-
-        return sum;
+        return IntStream.range(1, number + 1)
+                .map(Problem3::count369)
+                .sum();
     }
 }
