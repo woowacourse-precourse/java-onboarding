e2bbed0da68ccd0cc14194d1e0f1d5ff7676a786
raycho <raycho521@yonsei.ac.kr>
2022-10-27 14:00:27 +0900

    problem5 try1 2022.10.27

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> moneyBag = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        answer = new ArrayList<>();
+
+        for(int price : moneyBag){
+            answer.add(money/price);
+            money = money % price;
+        }
+
         return answer;
     }
 }
