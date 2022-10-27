78cce05383525d57359718bab193b57f899e66d9
kevstevie <jianna442@gmail.com>
2022-10-27 20:53:10 +0900

    feat: add makeUsersFriendsList

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,15 +8,33 @@ public class Problem7 {
         Map<String, Set<String>> friendsList = new HashMap<>();
 
         for (int i = 0; i < friends.size(); i++) {
-            makeList(friendsList, friends.get(i).get(0), friends.get(i).get(1));
+            makeConnections(friendsList, friends.get(i).get(0), friends.get(i).get(1));
         }
+
+        Set<String> usersFriends = makeUsersFriendsList(user, friendsList);
+
+        
+
     }
 
-    static void makeList(Map<String, Set<String>> map, String name1, String name2){
+    static void makeConnections(Map<String, Set<String>> map, String name1, String name2){
         if (map.containsKey(name1)){
             map.get(name1).add(name2);
         } else {
             map.put(name1, Set.of(name2));
         }
+    }
+
+    static Set<String> makeUsersFriendsList(String user, Map<String, Set<String>> map){
+        Set<String> result = new HashSet<>();
+
+        Set<String> keySet = map.keySet();
+
+        for(String str : keySet){
+            if (map.get(user).contains(str)){
+                result.add(str);
+            }
+        }
+        return result;
     }
 }
