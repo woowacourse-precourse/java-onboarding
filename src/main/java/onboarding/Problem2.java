4863c3c5561e95a4fcb7c207a3fc6884aff93ede
limzzum <chfhddl1234@naver.com>
2022-10-27 14:54:48 +0900

    암호문의 문자들을 비교하며 현재 문자가 다음 문자와 같은지 비교한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        cryptogram+= "?";
+
+        for(int i=0; i<cryptogram.length(); i++){
+            if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+
+            }else {
+            }
+        }
         String answer = "answer";
         return answer;
     }
