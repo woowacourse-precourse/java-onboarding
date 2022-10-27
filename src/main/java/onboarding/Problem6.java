1baba37d94c0f76c70f8142895d6b5ed967172c5
sieun <sieun960513@naver.com>
2022-10-27 20:36:31 +0900

    [Problem6] : 기능목록 [신규]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+/** 기능 목록
+ * solutionLogic        : 메인 솔루션
+ * checkValid           : 예외 처리
+ * checkCrewNums        : 크루 크기 유효성 체크
+ * checkEmailForm       : 이메일 형식 유효성 체크
+ * checkEmailLength     : 이메일 길이 유효성 체크
+ * checkEmailDomain     : 이메일 도메인 체크
+ * checkNickNameType    : 닉네임 한글 타입 체크
+ * checkNickNameLength  : 닉네임 길이 확인
+ */
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+}
+
+class CrewList{
+    Map<String, String> crewList = new HashMap<>();
+
+
+
+
 }
