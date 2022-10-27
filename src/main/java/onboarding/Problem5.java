c42c34d89f57bfb5342a4269172771473a2f325d
hyun6ik <fkanwm@naver.com>
2022-10-27 18:15:15 +0900

    feat(37) : [기능 5] : 정답 리스트 생성 및 화폐배열 초기화 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,6 +5,14 @@ import java.util.List;
 
 public class Problem5 {
 
+    static List<Integer> answer;
+    static int[] moneyArr;
+
+    static void input() {
+        answer = new ArrayList<>();
+        moneyArr = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    }
+
     private static void validateMoney(int money) {
         if (money < 1 || money > 1000000) {
             throw new IllegalArgumentException("money는 1 이상 1,000,000 이하인 자연수입니다.");
@@ -13,6 +21,7 @@ public class Problem5 {
 
     public static List<Integer> solution(int money) {
         validateMoney(money);
+        input();
         return new ArrayList<>();
     }
 }
