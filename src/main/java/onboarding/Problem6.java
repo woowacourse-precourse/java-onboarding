469aa0edc163518336ca9ff5b1cb90278a9317db
junhei Lee <jun2ee22@naver.com>
2022-10-28 02:37:19 +0900

    feat: 두 글자씩 문자를 나누는 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+
+    //두 글자씩 문자 나누기
+    private static List<String> divideByTwoChars(String nickNames) {
+        List<String> eachTwoChars = new ArrayList<>();
+
+        for (int i = 0; i < nickNames.length() - 1; i++) {
+            eachTwoChars.add("" + nickNames.charAt(i) + nickNames.charAt(i + 1));
+        }
+
+        return eachTwoChars;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
+
         List<String> answer = List.of("answer");
+
+        List<List<String>> indexTwoChars = new ArrayList<>();
+        List<String> allTwoChars = new ArrayList<>();
+
+        String nickname;
+
+        
         return answer;
     }
 }
