065a520e82b3977c303452671ea076b94a892709
meunrud <y3a0522@gmail.com>
2022-10-28 02:08:27 +0900

    초기화

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,12 +1,10 @@
 package onboarding;
 
+import java.util.List;
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-
-        return;
+        int answer = Integer.MAX_VALUE;
+        return answer;
     }
-
-
-
-
 }
