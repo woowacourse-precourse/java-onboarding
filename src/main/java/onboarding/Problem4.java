342e62989fa17bbdb1c44981b279e8d2f48e8b98
apptie <appti@naver.com>
2022-10-27 12:20:11 +0900

    feat: 문제 4 문자가 대문자인지 확인하는 isUpperCase 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,13 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static boolean isUpperCase(char target) {
+        return 'A' <= target && target <= 'Z';
     }
 }
