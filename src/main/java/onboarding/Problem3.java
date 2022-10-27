d9b4afeb480d3bfe02d348b2b45c957cdae2bb5e
apptie <appti@naver.com>
2022-10-27 11:43:25 +0900

    feat: 문제 3 주어진 숫자가 박수를 치는지 계산하는 calculateTargetDigitClapCount 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,35 @@
 package onboarding;
 
 public class Problem3 {
+
     static int[] clapCountArr = new int[10001];
 
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int calculateTargetClapCount(int target) {
+        int clapCount = 0;
+
+        while (target != 0) {
+            int targetDigit = target % 10;
+            target /= 10;
+
+            if (isCached(targetDigit)) {
+                clapCount += clapCountArr[targetDigit];
+                continue;
+            }
+            clapCount += calculateTargetDigitClapCount(targetDigit);
+        }
+        return clapCount;
+    }
+
+    private static int calculateTargetDigitClapCount(int target) {
+        if (isClapped(target)) {
+            return ++clapCountArr[target];
+        }
+        return 0;
     }
 
     private static boolean isClapped(int target) {
