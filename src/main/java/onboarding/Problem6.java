3b71bc2c31a72c2ecd45315d1df44bcc01e40304
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-27 17:47:36 +0900

    feat: Problem6 - 각 닉네임에서 연속하는 문자 두 개를 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        HashMap<String, Integer> userTwoLetterList = new HashMap<>();
         List<String> answer = List.of("answer");
+        for (List<String> form : forms) {
+            makeUserTwoLetterList(form.get(1), userTwoLetterList);
+        }
+
         return answer;
+    }
+
+    public static void makeUserTwoLetterList(String nickName, HashMap<String, Integer> userTwoLetterList) {
+        String[] splitNickName = nickName.split("");
+        for (int i = 0; i < splitNickName.length-1; i++) {
+            String userTwoLetter = splitNickName[i]+splitNickName[i+1];
+            userTwoLetterList.put(userTwoLetter, userTwoLetterList.getOrDefault(userTwoLetter, 0) + 1);
+        }
     }
 }
