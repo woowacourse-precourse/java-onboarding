0a5c964142c369a118e3704166b425764026e8bc
Reznov <reznov03048@gmail.com>
2022-10-28 00:05:51 +0900

    기본적인 요구사항을 solution 메소드에 주석으로 표시

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        // 청개구리 사전 만들기
+
+        // 변환하기
+
+
         return answer;
     }
 }
