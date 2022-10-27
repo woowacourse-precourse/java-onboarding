dd6bbf8e711f21842b2349263178e4c7eeca1f23
code-tree <dltjdqls1200@naver.com>
2022-10-27 17:12:36 +0900

    feat: Make frog dictionary

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,12 +2,30 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
 
         if (word.length() < 1 || word.length() > 1000) {
             return "false";
         }
 
-        return answer;
+        return convertFrogDictionary(word);
+    }
+
+    private static String convertFrogDictionary(String word) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char alphabet : word.toCharArray()) {
+            char understand = alphabet;
+
+            if (Character.isLowerCase(alphabet)) {
+                understand = (char) ('z' - (alphabet - 'a'));
+            }
+            if (Character.isUpperCase(alphabet)) {
+                understand = (char) ('Z' - (alphabet - 'A'));
+            }
+
+            sb.append(understand);
+        }
+
+        return sb.toString();
     }
 }
