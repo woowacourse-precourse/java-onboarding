40cb5cb880d8e60cd1bb522efbf770f80112d729
xxeol2 <jhj01177@naver.com>
2022-10-28 13:53:55 +0900

    feat(prob3): 카운팅 합산 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,7 @@ package onboarding;
 public class Problem3 {
 	public static int solution(int number) {
 		int answer = 0;
+		answer = sumCounts(number);
 		return answer;
 	}
 
@@ -13,6 +14,14 @@ public class Problem3 {
 				count += 1;
 			}
 			number /= 10;
+		}
+		return count;
+	}
+
+	public static int sumCounts(int number) {
+		int count = 0;
+		for (int i = 1; i <= number; i++) {
+			count += countSpecial(i);
 		}
 		return count;
 	}
