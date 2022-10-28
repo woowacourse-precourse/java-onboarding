f97bea389896d24ad9ce8a3c4e455e8f01f0af71
한은영 <oneeqzero@gmail.com>
2022-10-28 11:50:50 +0900

    feat: problem3 3,6,9 개수 카운트 메소드 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 public class Problem3 {
+    public static int clapNum(int num){
+        String str = ""+num;
+        int ret = 0;
+        for(int i = 0; i<str.length(); i++){
+            char tmp = str.charAt(i);
+            if(tmp == '3' || tmp == '6' || tmp == '9') ret++;
+        }
+        return ret;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
