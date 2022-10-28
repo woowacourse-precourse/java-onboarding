26b8500b7478c842c1313b244b5e6cc91beb5a3b
5nam <ohnam90@gmail.com>
2022-10-29 00:42:57 +0900

    feat: 규칙에 맞게 최소한의 화폐로 반환해주는 기능 추가 money 와 list 를 매개변수로 받아와서 큰 화폐 순으로 나누어 보고, 나누어지면 반환하는 list 에 set 해주는 것

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,6 +10,60 @@ public class Problem5 {
         return answer;
     }
 
+    // 규칙에 맞게 돈을 반환하는 함수
+    private static List<Integer> moneyChange(int money, List<Integer> list) {
+
+        int change = 0;
+
+        if(money/50000 != 0) {
+            change = money/50000;
+            list.set(0, change);
+            money = money%50000;
+        }
+        if(money/10000 != 0) {
+            change = money/10000;
+            list.set(1, change);
+            money = money%10000;
+        }
+        if(money/5000 != 0) {
+            change = money/5000;
+            list.set(2, change);
+            money = money%5000;
+        }
+        if(money/1000 != 0) {
+            change = money/1000;
+            list.set(3, change);
+            money = money%1000;
+        }
+        if(money/500 != 0) {
+            change = money/500;
+            list.set(4, change);
+            money = money%500;
+        }
+        if(money/100 != 0) {
+            change = money/100;
+            list.set(5, change);
+            money = money%100;
+        }
+        if(money/50 != 0) {
+            change = money/50;
+            list.set(6, change);
+            money = money%50;
+        }
+        if(money/10 != 0) {
+            change = money/10;
+            list.set(7, change);
+            money = money%10;
+        }
+        if(money/1 != 0) {
+            change = money / 1;
+            list.set(8, change);
+            money = money % 1;
+        }
+
+        return list;
+    }
+
     // 리스트 0 으로 초기화
     private static List<Integer> initList(List<Integer> list) {
         list = new ArrayList<>();
