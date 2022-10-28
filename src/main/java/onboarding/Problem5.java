9c2c5641d994c738e0865d73a016a1d6eb122f81
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-28 20:48:08 +0900

    [Problem 5] 숫자의 유효성 검사

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        if(!checkValidation(money)){
+            return null;
+        }
         return answer;
+    }
+
+    private static boolean checkValidation(int money){
+        if(1<=money && money<=1000000){
+            return true;
+        }
+        return false;
     }
 }
