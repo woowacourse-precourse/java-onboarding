8aebde8651bcd5e5929296efb4ac8a4c68b4f14c
kimminsu <xjfcnfw3@naver.com>
2022-10-28 20:16:08 +0900

    feat(문제2): 문자열을 복호화하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import onboarding.problem2.Decryption;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = Decryption.decrypt(cryptogram);
         return answer;
     }
 }
--- src/main/java/onboarding/problem2/Decryption.java
+++ src/main/java/onboarding/problem2/Decryption.java
@@ -4,10 +4,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Decryption {
-    public static final String REPEAT_PATTERN = "([a-z])\\1+";
+    public static final String REPEAT_PATTERN_REGEXP = "([a-z])\\1+";
+
+    public static String decrypt(String cryptogram) {
+        boolean decryptionComplete = false;
+        Pattern repeatPattern = Pattern.compile(REPEAT_PATTERN_REGEXP);
+        while (!decryptionComplete){
+            cryptogram = removeRepeatedCharacters(cryptogram);
+            decryptionComplete = hasNotRepeatedCharacters(cryptogram, repeatPattern);
+        }
+        return cryptogram;
+    }
 
     private static String removeRepeatedCharacters(String cryptogram) {
-        return cryptogram.replaceAll(REPEAT_PATTERN, "");
+        return cryptogram.replaceAll(REPEAT_PATTERN_REGEXP, "");
     }
 
     private static boolean hasNotRepeatedCharacters(String cryptogram, Pattern pattern) {
