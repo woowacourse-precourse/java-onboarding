5da7e97273d451e193da10392de9bf79b20a73b8
qdeu22 <qdeu22@gmail.com>
2022-10-28 12:08:52 +0900

    forms 리스트에 닉네임에서 2자리 이상의 단어 찾는 기능 커밋

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        /* ArrayList를 이용하여 2자리 이상 단어 담기 */
+        List<String> ar = new ArrayList<>();
+
+        /* forms 리스트에 닉네임에서 2자리 이상의 단어 찾는 기능 */
+        for (int i = 0; i < forms.size(); i++) {
+
+            String nickname = forms.get(i).get(1);
+
+            for (int j = 0; j < nickname.length(); j++) {
+                for (int k = j + 1; k < nickname.length(); k++) {
+                    /* substring을 이용하여 2자리 이상 단어 추출 */
+                    String s = nickname.substring(j, k + 1);
+                    if (!ar.contains(s)) ar.add(s);
+                }
+            }
+
+        }
         return answer;
     }
 }
