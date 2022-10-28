15432f97e0f96f54bacd0dee881b84526b2ccc0d
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2022-10-29 01:16:04 +0900

    [문제3] 3,6,9 일 때 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -13,9 +13,20 @@ public class Problem3 {
         divide[4]=number%division[4];
     }
 
+    public static int threesixnine(int number){
+        if(number<4){
+            return 0;
+        }else if(number<7){
+            return 1;
+        }else{
+            return 2;
+        }
+    }
+
     public static void singlefigure(){
         count=divide[4]/3;
     }
+
 
     public static int solution(int number) {
         int answer = 0;
