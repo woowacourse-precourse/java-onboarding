f7ac4cd8c96acb0d056fd3037533cd177ae4ca1b
unknown <jeon98@soongsil.ac.kr>
2022-10-28 15:33:35 +0900

    solved #3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i<=number ; i++){
+            String s = String.valueOf(i);
+            int len = s.length();
+            for(int j = 0 ; j<len;j++){
+                if(s.charAt(j)=='3' | s.charAt(j)=='6' |s.charAt(j)=='9'){
+                    answer += 1;
+                }
+            }
+        }
         return answer;
     }
 }
