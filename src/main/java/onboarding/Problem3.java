1e922790411bd4b6385cb1d2157447d65809da04
KyungKyu Park <rudrb0505@gmail.com>
2022-10-28 12:26:06 +0900

    feat(Problem3): 3,6,9일 때 손뼉을 치는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,11 +3,14 @@ package onboarding;
 public class Problem3 {
 	public static int solution(int number) {
 		int count = 0;
+		int clapCount = 0;
+
 		for (count = 1; count <= number; count++) {
-			System.out.println(count);
+			if (count % 10 == 3 || count % 10 == 6 || count % 10 == 9) {
+				clapCount++;
+			}
 		}
 
-		int answer = 0;
-		return answer;
+		return clapCount;
 	}
 }
