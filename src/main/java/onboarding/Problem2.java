28facae6cab3aed6c46f386b3dd054ed7baefca1
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-28 21:24:44 +0900

    for문에서 문자열을 벗어나는 오류를 예외처리함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,10 +4,24 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String result = "";
         for(int i =0; i<cryptogram.length(); i++){
-            if(cryptogram.charAt(i-1)!=cryptogram.charAt(i) && cryptogram.charAt(i)!=cryptogram.charAt(i+1) ){
-                result += cryptogram.charAt(i);
+            try{
+                if(cryptogram.charAt(i-1)!=cryptogram.charAt(i) && cryptogram.charAt(i)!=cryptogram.charAt(i+1) ){
+                    result += cryptogram.charAt(i);
+                }
             }
-        }   
+            catch(StringIndexOutOfBoundsException e){
+                if(i==0){
+                    if(cryptogram.charAt(i)!=cryptogram.charAt(i+1)){
+                        result += cryptogram.charAt(i);
+                    }
+                }
+                if(i==cryptogram.length()-1){
+                    if(cryptogram.charAt(i-1)!=cryptogram.charAt(i)){
+                        result += cryptogram.charAt(i);
+                    }
+                }
+            }
+        }       
         
         String answer = "answer";
         return answer;
