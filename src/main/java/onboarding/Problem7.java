cb2e781c54b92a30f92113378f5bce97b72d2f3a
kokodak <kokodakadokok@gmail.com>
2022-10-28 16:29:36 +0900

    feat: [문제7] SNS를 사용하는 모든 유저들의 목록 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> getAllUsersName(String user, List<List<String>> friends, List<String> visitors) {
+        Set<String> allUsersNameSet = new LinkedHashSet<>();
+        allUsersNameSet.add(user);
+
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> friend = friends.get(i);
+            allUsersNameSet.add(friend.get(0));
+            allUsersNameSet.add(friend.get(1));
+        }
+
+        for (int i = 0; i < visitors.size(); i++) {
+            allUsersNameSet.add(visitors.get(i));
+        }
+
+        List<String> allUserNameList = new ArrayList<>(allUsersNameSet);
+
+        return allUserNameList;
     }
 }
