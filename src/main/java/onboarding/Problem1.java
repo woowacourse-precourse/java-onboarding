66f90cba90ae9a78bc80d7089bee4f84f79fb72c
jh868 <2weeks9947@naver.com>
2022-10-28 23:28:24 +0900

    fin

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,46 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        //int answer = Integer.MAX_VALUE;
+        if(pobi.size() !=2 || crong.size() !=2) return -1;
+        if(pobi.get(0) != pobi.get(1)-1 || crong.get(0) != crong.get(1)-1) return -1;
+
+        if(score(pobi)>score(crong)){
+            return 1;
+        } else if (score(pobi)<score(crong)) {
+            return 2;
+        } else if (score(pobi) == score(crong)) {
+            return 0;
+        }
+
+        return -1;
+    }
+
+    public static int score(List<Integer> pages){
+        int max_score = 0;
+        int page_num;
+        int sum;
+        int mul;
+
+        for(int i =0; i < 2; i++){
+            page_num = pages.get(i);
+            sum = 0;
+            mul = 1;
+            while(page_num>0){
+                sum += page_num%10;
+                mul *= page_num%10;
+                page_num /= 10;
+            }
+            if(sum < mul){
+                if(max_score< mul){
+                    max_score = mul;
+                }
+            } else {
+                if(max_score<sum){
+                    max_score = sum;
+                }
+            }
+        }
+        return max_score;
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,35 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String tmp = cryptogram;
+        while(!tmp.equals(crypto(tmp))){
+            tmp = crypto(tmp);
+        }
+        return tmp;
+    }
+    public static String crypto(String cryptogram){
+        if(cryptogram.length() == 0) return "";
+        if(cryptogram.length() == 1){
+            return cryptogram;
+        }else if(cryptogram.length() == 2){
+            if(cryptogram.charAt(0) == cryptogram.charAt(1)){
+                return "";
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < cryptogram.length(); i++){
+            if(cryptogram.charAt(i) == cryptogram.charAt(i-1)){
+                i++;
+            }else{
+                sb.append(cryptogram.charAt(i-1));
+            }
+        }
+        if(cryptogram.charAt(cryptogram.length()-1) != cryptogram.charAt(cryptogram.length()-2)){
+            sb.append(cryptogram.charAt(cryptogram.length()-1));
+        }
+
+        return sb.toString();
     }
 }
