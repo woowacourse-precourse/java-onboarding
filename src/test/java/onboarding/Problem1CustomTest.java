185c72e18dfa65ab9e18cc6efaa94fbbcd03187a
math0917 <math0917@naver.com>
2022-10-28 16:32:34 +0900

    test : 길이가 2인지 확인하는 validation test code을 작성

---
+++ src/test/java/onboarding/Problem1CustomTest.java
@@ -0,0 +1,42 @@
+package onboarding;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+/*
+*
+* - pobi와 crong의 input이 길이가 2인지 판단한다.
+* - pobi와 crong의 input의 원소들이 페이지의 최소값인 1과 최대값인 400사이인지 확인한다.
+* - pobi와 crong의 input이 각각 연속된 숫자 리스트인지 확인한다. ex) [97, 98] -> (O), [98,100] -> (X)
+* - pobi와 crong의 input이 각각 [홀수, 짝수] 쌍인지 확인한다.
+* - pobi와 crong의 input이 [홀수, 짝수]인 쌍에서 짝수값이 홀수 값보다 1 큰지 확인한다.
+ */
+public class Problem1CustomTest {
+    @Nested
+    class Problem1Test {
+        @Test
+        void 길이가2보다큰경우테스트() {
+            List<Integer> pobi = List.of(97, 98, 100);
+            List<Integer> crong = List.of(197, 198, 201);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+        @Test
+        void 길이가2보다작은경우테스트() {
+            List<Integer> pobi = List.of(97);
+            List<Integer> crong = List.of(197);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+        @Test
+        void 길이가2인경우테스트() {
+            List<Integer> pobi = List.of(97, 98);
+            List<Integer> crong = List.of(197, 198);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+    }
+}
