abf68460a619b102b8395a5087fe19cae770c49e
es <mascoeskim@gmail.com>
2022-10-28 17:19:12 +0900

    Problem6/1.초기세팅

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,14 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        forms = new ArrayList<>(forms);
+        ArrayList<String> separateName = new ArrayList<>();
+
+
+
         List<String> answer = List.of("answer");
         return answer;
     }
