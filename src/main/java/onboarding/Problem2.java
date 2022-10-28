f61c95aaa0426ef45d0c39e2b823c23391cbd825
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 00:59:20 +0900

    Feat : add input validation check function to Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,20 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static boolean validCheck(String cryptogram) {
+        char[] charArray = cryptogram.toCharArray();
+        if (cryptogram.length() > 1000 || cryptogram.length() < 1) {
+            return false;
+        }
+        for (char c : charArray) {
+            if (Character.isUpperCase(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
