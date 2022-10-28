33516ede528bec97f543c358524aab5bdf653fef
Polite-Jehu <*****@***.com>
2022-10-29 02:22:04 +0900

    기능 7 완료 반환 : 추천 리스트 =============== Friends 클래스 name 친구이름 friendList 친구의 친구들 visitCount 친구가 user한테 방문한 횟수 recommendPoint 추천 포인트 solveRecommendPoint 메소드 : 친구가 user랑 친구면 0포인트를 반환 그렇지 않으면 user의 친구 리스트와 비교해 10포인트 더하고 visitCount를 더해 recommendPoint를 만든다. compareTo : recommendPoint가 클수록 먼저 오게 해두는 메소드
    솔루션 메소드
    answer : 추천 리스트
    allList : 인원 전체 리스트
    첫번째주석:
    allList에 Friends를 생성하여 넣는다.
    친구관계리스트에서 Friends를 생성하거나 찾고 각자의 친구 리스트에 넣는다.
    
    두번째주석:
    방문자를 처리한다.
    visitor리스트에서 Friends를 생성하거나 찾고 방문횟수를 visitPlus로 늘린다.
    
    세번째주석:
    user객체를 allList에서 찾고
    user객체와 user의 친구리스트를 모든 Friends에게 넘겨 RecommendPoints를 알아서 찾아오게 만든다.
    이후 maybeAnswer을 정렬하고
    5개 안쪽으로 추천 친구를 뽑아 answer에 넣는다.
    answer를 반환한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,150 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+
+class Friends implements Comparable<Friends> {
+    public Friends(String n) {
+        name=n;
+    }
+
+    String name = "";
+    HashSet<String> friendList = new HashSet<>();
+    int visitCount = 0;
+    int recommendPoint = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void inputFriend(String st) {
+        friendList.add(st);
+    }
+
+    public void visitPlus() {
+        visitCount++;
+    }
+
+    public HashSet<String> getFriendList()
+    {
+        return friendList;
+    }
+    public void solveRecommendPoint(String user, HashSet<String> userFriendList){
+        boolean isUserFriend = false;
+        for(String st:friendList)
+        {
+            if(st.equals(user))
+                isUserFriend=true;
+        }
+        for(String myFriend : friendList)
+        {
+            for(String userFriend:userFriendList)
+            {
+                if(myFriend.equals(userFriend))
+                    recommendPoint+=10;
+            }
+        }
+        recommendPoint+=visitCount;
+        if(isUserFriend)
+        {
+            recommendPoint=0;
+        }
+    }
+    public int getRecommendPoint()
+    {
+        return recommendPoint;
+    }
+    @Override
+    public int compareTo(Friends o) {
+        if(this.recommendPoint>o.recommendPoint)
+            return -1;
+        return 1;
+    }
+}
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        ArrayList<Friends> allList = new ArrayList<>();
+
+        //#1
+        allList.add(new Friends(user));
+        for(List<String> ls:friends)
+        {
+            boolean exist1 = false;
+            boolean exist2 = false;
+            for(Friends f:allList)
+            {
+                if(f.getName().equals(ls.get(0)))
+                {
+                    f.inputFriend(ls.get(1));
+                    exist1 = true;
+                }
+                if(f.getName().equals(ls.get(1))) {
+                    f.inputFriend(ls.get(0));
+                    exist2 = true;
+                }
+            }
+            if(!exist1)
+            {
+                Friends newFriend1 = new Friends(ls.get(0));
+                newFriend1.inputFriend(ls.get(1));
+                allList.add(newFriend1);
+            }
+            if(!exist2)
+            {
+                Friends newFriend2 = new Friends(ls.get(1));
+                newFriend2.inputFriend(ls.get(0));
+                allList.add(newFriend2);
+            }
+        }
+
+        //#2
+        for(String st:visitors)
+        {
+            boolean exist3 = false;
+            for(Friends f:allList) {
+                if(f.getName().equals(st))
+                {
+                    exist3=true;
+                    f.visitPlus();
+                }
+            }
+            if(!exist3)
+            {
+                Friends newFriend3 = new Friends(st);
+                newFriend3.visitPlus();
+                allList.add(newFriend3);
+            }
+        }
+
+        //#3
+        Friends master=null;
+        for(Friends f:allList)
+        {
+            if(f.getName().equals(user))
+            {
+                master = f;
+                break;
+            }
+        }
+        for(Friends f:allList)
+        {
+            f.solveRecommendPoint(user,master.getFriendList());
+        }
+        ArrayList<Friends> maybeAnswer = new ArrayList<>(allList);
+        maybeAnswer.remove(master);
+        Collections.sort(maybeAnswer);
+        int countRecommend=0;
+        for(Friends f:maybeAnswer)
+        {
+            if(f.getRecommendPoint()>0&&countRecommend<5)
+            {
+                answer.add(f.getName());
+            }
+            countRecommend++;
+        }
         return answer;
     }
+
 }
