92f2af016e1078eac8fc8056cf505b606a5200af
YooDongSeon <inko51366@naver.com>
2022-10-28 17:43:30 +0900

    기능목록 작성 후 커밋

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem2 {
+    /*  기능구현
+        
+        1. 중복된 변수를 삭제하기 위해 param을 StringBulider(sb)에 담아준다.
+        2. for문을 돌며 sb를 i, i + 1을 반복해서 비교 (중복비교)
+        3. 맞으면 문자 두개(중복된 문자) 제거 후 인덱스 감소
+        4. sb를 answer 옮김
+     */
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
     }
 }
