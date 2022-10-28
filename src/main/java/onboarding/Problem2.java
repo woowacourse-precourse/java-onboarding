f7cb57b2eb387e04252610156a920f6a276d7cd8
KSH <tkdgns543@gmail.com>
2022-10-28 22:20:11 +0900

    refactor(Problem2): 문자열 2개로 압축하는 메서드 구조 변경
    매개변수 target의 자료형을 String에서 char로 변경
    반복되는 문자열을 repeat 메서드로 생성하도록 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,11 +7,14 @@ public class Problem2 {
     }
 
     /* 3 이상 연속된 target 문자열을 2개까지 줄이는 메서드 */
-    private static String compressToSizeTwo(String cryptogram, String target) {
+    private static String compressToSizeTwo(String cryptogram, char target) {
         String result=cryptogram;
-        String tmp=target+target+target;
+        String tmp=Character.toString(target);
+        String start=tmp.repeat(3);
+        String end=tmp.repeat(2);
+
         while (result.contains(tmp)) {
-            result=result.replace(tmp,target+target);
+            result=result.replace(start,end);
         }
 
         return result;
