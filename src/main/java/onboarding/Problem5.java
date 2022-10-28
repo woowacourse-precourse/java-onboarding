bb2cd7a070eeee1661c8d3f96bbfcb0b43529d78
juno-junho <ssmm0205@naver.com>
2022-10-29 07:01:06 +0900

    feat(Problem5): add moneyChanger method
    기능 분리를 위해서 돈을 화폐단위로 맞게 큰 금액 부터 거슬러 주도록
    moneyChanger(List<Integer> currencyUnitList, int money) 메소드 분리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> wallet = new ArrayList<>();
         final List<Integer> WON_LIST = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
-        for (Integer integer : WON_LIST) {
-            wallet.add(money / integer);
-            money %= integer;
+        return moneyChanger(WON_LIST, money);
+    }
+
+    static List<Integer> moneyChanger(List<Integer> currencyUnitList, int money) {
+        List<Integer> changedResult = new ArrayList<>();
+        for (Integer currencyUnit : currencyUnitList) {
+            changedResult.add(money / currencyUnit);
+            money %= currencyUnit;
         }
-        return wallet;
+        return changedResult;
     }
 }
