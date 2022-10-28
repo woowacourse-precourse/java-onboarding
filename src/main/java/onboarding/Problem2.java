aaf1d00027b8958b1337f5d91120f704c43ce6a1
son <shhmun02@naver.com>
2022-10-28 17:24:09 +0900

    Problem2_req_complete

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,9 +13,9 @@ public class Problem2 {
         {
             answer = "";
             sw = 0;
+            //1. 연속하는 중복 문자 찾기
             for(int i = 0; i < s.length(); i++)
             {
-                //1. 연속하는 중복 문자 찾기
                 if(i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1))
                 {
                     sw = 1;
@@ -29,10 +29,13 @@ public class Problem2 {
                 //2. 연속하는 중복 문자 제거
                 answer += s.charAt(i);
             }
-            //4. 연속하는 중복 문자들을 삭제한 결과를 return
+            s = answer;
+
+            //3. 1~2를 연속하는 중복문자가 없을 때 까지 반복
             if(sw == 0)
                 break;
         }
+        //4. 연속하는 중복 문자들을 삭제한 결과를 return
         return answer;
     }
 }
