c0765887c6f626f0b68a208273f94683c328db2f
xxeol2 <jhj01177@naver.com>
2022-10-28 14:25:47 +0900

    feat(prob4): 한 문자를 청개구리 사전을 참고해 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		String answer = "";
+		return answer;
+	}
+
+	public static char changeChar(char c) {
+		boolean isLowerCase = (c >= 'a');
+		if (isLowerCase) {
+			return (char)('a' + 'z' - c);
+		}
+		return (char)('A' + 'Z' - c);
+	}
 }
