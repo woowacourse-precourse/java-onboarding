38cb51de4f5b4ad49d4f2ebeaf8e5641074fd026
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-29 02:11:41 +0900

    [Problem2] feat: getSStringBuffer 구현
    - cryptogram을 쉽게 다루기 위해 crytogram와 길이, 값이 일치한 StringBuffer 생성해 return하는 메서드 정의 및 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        // crytogram와 길이, 값이 일치한 StringBuffer 생성
+        StringBuffer sb = getSStringBuffer(cryptogram);
+
+        //
+
         return answer;
+    }
+
+    // String 매개변수와 길이, 값이 일치한 StringBuffer 반환
+    private static StringBuffer getSStringBuffer(String cryptogarm) {
+        StringBuffer sb = new StringBuffer(cryptogram.length());
+        sb.append(cryptogram);
+        return sb;
     }
 }
