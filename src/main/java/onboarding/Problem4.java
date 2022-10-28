c88c7324e70ed206b63142bc3e4144a6c6c4dc83
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 16:43:12 +0900

    feat: [4번] 문자를 반대로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,13 @@ public class Problem4 {
 
     /** 문자를 반대로 변환하는 함수 */
     private static char replaceChar(char ch) {
-        return Character.MAX_VALUE;
+        if(ch >= 'a' && ch <= 'z') {
+            return (char)('z' - (ch - 'a'));
+        } else if(ch >= 'A' && ch <= 'Z') {
+            return (char)('Z' - (ch - 'A'));
+        } else {
+            return ch;
+        }
     }
 
     public static String solution(String word) {
