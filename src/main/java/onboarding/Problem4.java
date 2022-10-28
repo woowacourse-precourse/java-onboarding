ce267df2b10218d6b3cc63f838daeed50c21488f
nswon <swnam0729@gmail.com>
2022-10-29 01:19:28 +0900

    feat(Problem4): DTO 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -18,4 +18,16 @@ public class Problem4 {
     private static String toString(char character, char lastChar, char firstChar) {
         return String.valueOf((char) lastChar - character + firstChar);
     }
+
+    private static class CharDto {
+        private char firstChar;
+        private char nowChar;
+        private char lastChar;
+
+        public CharDto(char firstChar, char nowChar, char lastChar) {
+            this.firstChar = firstChar;
+            this.nowChar = nowChar;
+            this.lastChar = lastChar;
+        }
+    }
 }
