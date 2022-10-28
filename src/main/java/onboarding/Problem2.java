1191a0a1e9e5c1f9f05380350ecacc135319abc8
Dabeen Jeong <hello70825@gmail.com>
2022-10-28 14:51:18 +0900

    feat: [Problem2] 연속하는 중복된 문자가 존재하면 삭제해주는 기능을 추가하다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
@@ -41,5 +43,32 @@ public class Problem2 {
         int result = inputIntStream.reduce(0, (a, b) -> a == -1 ? a : (a == b) ? -1 : b);
 
         return result == -1;
+    }
+
+    private static String getAmendString(String inputString) {
+        List<Character> resultList = new ArrayList<>();
+        String resultString = null;
+        Character nowChar = null;
+        Integer nowSize = null;
+
+        for(int i = 0; i < inputString.length(); i++) {
+            nowSize = resultList.size();
+            nowChar = inputString.charAt(i);
+            try {
+                if (resultList.get(nowSize - 1).equals(nowChar)) {
+                    resultList.remove(nowSize - 1);
+                } else {
+                    resultList.add(nowChar);
+                }
+            } catch (Exception e) {
+                resultList.add(nowChar);
+            }
+        }
+
+        resultString = resultList.stream()
+                        .map(String::valueOf)
+                        .reduce("", String::concat);
+
+        return resultString;
     }
 }
