7fd78734f36b81c1d9a67eef1f1e0eedfe7c6c07
Hojinida <opqrs1103@gmail.com>
2022-10-29 01:29:02 +0900

    feat: 페이지 번호를 입력했을 때 덧셈 곱셈 중 어느 값이 더 큰지 비교해 반환하는 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     private static final int DRAW=0;
@@ -13,7 +14,16 @@ class Problem1 {
             return EXCEPTION;
         return answer;
     }
-
+    public static int calculator(Integer num){
+        //입력받은 페이지 번호를 계산을 위해 정수 배열로 변환
+        int[] nums= Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+        int sum=0,mul=1;
+        for(int n:nums){
+            sum+=n;
+            mul*=n;
+        }
+        return Math.max(sum, mul);
+    }
     public static boolean exceptionHandler(List<Integer> pobi, List<Integer> crong){
         if(rangeMatch(pobi) || rangeMatch(crong))
             return true;
