34850196d24be7fcc3c60b40e63f52fb9480b2ea
swonny <wonny921@gmail.com>
2022-10-28 18:43:39 +0900

    feat: '페이지 각 자리수 더하기 연산' 기능 구현
    기능 구현 중 각 자리수를 분리하는 작업이 필요하다는 것을 알게 되었다. 기능 목록에서 수정이 필요하다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    public static int add(List<Integer> arr) {
+        int length = arr.size();
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            System.out.println(i);
+            sum += arr.get(i);
+        }
+        return sum;
+    }
+    public static int solution(List<Integer> pobi, List<Integer> crong){
+        int result = add(pobi);
+        System.out.println(result);
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(List.of(97, 98), List.of(197, 198));
     }
 }
