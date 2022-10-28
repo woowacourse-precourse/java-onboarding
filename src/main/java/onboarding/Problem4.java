9cc0bb5c8eac53e685a4a5ff3133730d7fac1efc
metorg <metrog950@gmail.com>
2022-10-28 13:40:18 +0900

    feat: [Problem4] 문자 판별 메서드 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static final int lowerCase=1;
+    public static final int upperCase=2;
+    public static final int etc=3;
+    public static StringBuilder sb;
+
     public static String solution(String word) {
+
         String answer = "";
         return answer;
+    }
+    static int letterCheck(char a){
+        if('a'<=a&&a<='z')return lowerCase;
+        else if('A'<=a&&a<='Z')return upperCase;
+        else return etc;
     }
 }
