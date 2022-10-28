0aec7fcd6d2de42dd49ad4fc722aa56e742bb1da
seongha <11pi885@gmail.com>
2022-10-29 02:04:57 +0900

    feat: 기능목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,13 @@
 package onboarding;
 
 public class Problem2 {
+
+    //현재 주어진 문자열에서 중복된 문자들 제거하는 함수
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        //함수호출
         return answer;
     }
 }
