fb2da3901d3aca6ed9b07f0fde8658f37eca06d5
yangjiyoung <0112jyoung@naver.com>
2022-10-28 10:51:50 +0900

    Problem 3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+    public static int count369(int number){
+        int count=0;
+        while(number!=0){
+            int tmp=number%10;
+            if(tmp==3 || tmp==6 || tmp==9)
+                count++;
+            number/=10;
+        }
+        return count;
+    }
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=3;i<=number;i++)
+            answer+=count369(i);
+
         return answer;
     }
 }
