05d22b82e1e7ef338401f958bdacad73b1b78d79
woosung1223 <dntjd991223@naver.com>
2022-10-28 15:50:01 +0900

    feat(Problem6): inputValidation 함수 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,8 @@ package onboarding;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Problem6 {
     static final HashMap<String, String> hm = new HashMap<>();
@@ -12,11 +14,18 @@ public class Problem6 {
         return answer;
     }
     public static boolean validationCheck(List<List<String>> forms) {
-        if (inputValidaiton(forms) && contentValidation(forms)) {
+        if (inputValidation(forms) && contentValidation(forms)) {
             return true;
         }
         return false;
     }
+    public static boolean inputValidation(List<List<String>> forms) {
+        if (forms.size() < 1 || forms.size() > 10000) {
+            return false;
+        }
+        return true;
+    }
+
     public static void splitLexim(List<String> repository, String name) {
         for (int j = 0; j < name.length()-1; j++) {
             repository.add(name.substring(j, j+2));
