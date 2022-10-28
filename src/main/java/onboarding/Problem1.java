c668447a8594ebda79703d4751eda6ac5adc25a7
hoonn9 <hun9935@gmail.com>
2022-10-28 14:58:42 +0900

    Add number to digit array method (Problem1)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    private static List<Integer> toDigitList(Integer num) {
+        return Arrays.stream(num.toString()
+                .split("")).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
