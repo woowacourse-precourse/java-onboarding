fa37fe4f37556b7d845c8d4feb412062786364d2
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 22:29:13 +0900

    refactor: Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,16 +1,16 @@
 package onboarding;
 
 public class Problem4 {
-    private static Character lowerChange(char ch){
+    private static Character lowerChange(char change){
         // a to z, b to x... 모두 두 문자의 아스키코드 값을 더하면 219가 된다.
         // 따라서 219에서 현재 문자의 아스키코드 값을 뺴면 변환 후 문자의 아스키코드 값이 된다.
-        return (char)(219 - ch);
+        return (char)(219 - change);
     }
 
-    private static Character upperChange(char ch){
+    private static Character upperChange(char change){
         // A to Z, B to X... 모두 두 문자의 아스키코드 값을 더하면 155가 된다.
         // 따라서 155에서 현재 문자의 아스키코드 값을 뺴면 변환 후 문자의 아스키코드 값이 된다.
-        return (char)(155 - ch);
+        return (char)(155 - change);
     }
 
     public static String solution(String word) {
