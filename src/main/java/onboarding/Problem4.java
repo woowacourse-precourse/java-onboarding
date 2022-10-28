978e39d7b8312d807440006e1fa18247536982c7
yeonhee <ydol2@naver.com>
2022-10-29 02:30:39 +0900

    [4번] feat : solution 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i=0;i<word.length();i++){
+            answer+=conversion(word.charAt(i));
+        }
         return answer;
     }
 
