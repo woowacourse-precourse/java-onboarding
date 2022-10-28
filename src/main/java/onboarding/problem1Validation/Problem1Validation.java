f72e0321e5d7f6689bd3a63e32c293b912b45a0d
math0917 <math0917@naver.com>
2022-10-28 19:15:04 +0900

    refactor : validation에서 외부호출이 필요없는 메써드는 private으로 작성

--- src/main/java/onboarding/problem1Validation/Problem1Validation.java
+++ src/main/java/onboarding/problem1Validation/Problem1Validation.java
@@ -3,51 +3,74 @@ package onboarding.problem1Validation;
 import java.util.List;
 
 import static java.lang.Math.*;
-
+/*
+ * - pobi와 crong의 input이 길이가 2인지 판단한다.
+ * - pobi와 crong의 input의 원소들이 페이지의 최소값인 1과 최대값인 400사이인지 확인한다.
+ * - pobi와 crong의 input이 각각 연속된 숫자 리스트인지 확인한다. ex) [97, 98] -> (O), [98,100] -> (X)
+ * - pobi와 crong의 input이 각각 [홀수, 짝수] 쌍인지 확인한다.
+ * - pobi와 crong의 input이 [홀수, 짝수]인 쌍에서 짝수값이 홀수 값보다 1 큰지 확인한다.
+ */
 public class Problem1Validation {
     public static boolean problem1InputValidation(List<Integer> list, int userInputLength, int bookMin, int bookMax) {
-        return userInputSizeEquals(list, userInputLength)
-                && isBookNumberBetweenMinAndMax(list, bookMin, bookMax)
-                && isBookNumberContinuous(list)
-                && isBookNumberOddEvenPair(list)
-                &&isFirstAddOneEqualsSecondValue(list);
+        return isBookNumberBetweenMinAndMax(list, bookMin, bookMax, userInputLength)
+                && isBookNumberContinuous(list, userInputLength)
+                && isBookNumberOddEvenPair(list, userInputLength)
+                &&isFirstAddOneEqualsSecondValue(list, userInputLength);
     }
 
-    public static boolean userInputSizeEquals(List<Integer> userInput, int listSize) {
-        if (userInput.size() == 2) {
+    private static boolean userInputSizeEquals(List<Integer> userInput, int listSize) {
+        if (userInput.size() == listSize) {
             return true;
         }
         return false;
     }
 
-    public static boolean isBookNumberBetweenMinAndMax(List<Integer> userInput, int bookMin, int bookMax) {
-        long count = userInput.stream()
-                .filter(pageNum -> isBetween(pageNum,bookMin, bookMax))
-                .count();
-        return Long.valueOf(userInput.size()) == count;
+    private static boolean isBookNumberBetweenMinAndMax(List<Integer> userInput, int bookMin, int bookMax, int userInputLength) {
+        if (userInputSizeEquals(userInput, userInputLength)) {
+            long count = userInput.stream()
+                    .filter(pageNum -> isBetween(pageNum, bookMin, bookMax))
+                    .count();
+            return Long.valueOf(userInput.size()) == count;
+        } else {
+            return false;
+        }
     }
 
-    public static boolean isBookNumberContinuous(List<Integer> list) {
-        int abs = abs(list.get(0) - list.get(1));
-        if (abs == 1) {
-            return true;
+    private static boolean isBookNumberContinuous(List<Integer> list, int userInputLength) {
+        if (userInputSizeEquals(list, userInputLength)) {
+            int abs = abs(list.get(0) - list.get(1));
+            if (abs == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+
+    }
+
+    private static boolean isBookNumberOddEvenPair(List<Integer> list, int userInputLength) {
+        if (userInputSizeEquals(list, userInputLength)) {
+            if ((list.get(0) % 2 == 1) && (list.get(1) % 2 == 0)) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
 
     }
 
-    public static boolean isBookNumberOddEvenPair(List<Integer> list) {
-        if ((list.get(0) % 2 == 1) && (list.get(1) % 2 == 0)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static boolean isFirstAddOneEqualsSecondValue(List<Integer> list) {
-        if (list.get(0) + 1 == list.get(1)) {
-            return true;
+    private static boolean isFirstAddOneEqualsSecondValue(List<Integer> list, int userInputLength) {
+        if (userInputSizeEquals(list, userInputLength)) {
+            if (list.get(0) + 1 == list.get(1)) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
--- src/test/java/onboarding/Problem1CustomTest.java
+++ src/test/java/onboarding/Problem1CustomTest.java
@@ -25,126 +25,126 @@ public class Problem1CustomTest {
 
     @Nested
     class Problem1Test {
-        @Test
-        void 길이가2보다큰경우테스트() {
-            List<Integer> list = List.of(97, 98, 100);
-            assertThat(
-                    Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH))
-                    .isFalse();
-        }
-
-        @Test
-        void 길이가2보다작은경우테스트() {
-            List<Integer> list = List.of(97);
-            assertThat(
-                    Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH))
-                    .isFalse();
-        }
-
-        @Test
-        void 길이가2인경우테스트() {
-            List<Integer> list = List.of(97, 98);
-            assertThat(
-                    Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH))
-                    .isTrue();
-        }
-
-        @Test
-        void 책페이지가1미만이포함되있다면() {
-            List<Integer> list = List.of(0, 3);
-            assertThat(
-                    Problem1Validation.isBookNumberBetweenMinAndMax(list, BOOK_MIN, BOOK_MAX))
-                    .isFalse();
-
-        }
-
-        @Test
-        void 책페이지가400초과인값이포함되어있다면() {
-            List<Integer> list = List.of(401, 398);
-            assertThat(
-                    Problem1Validation.isBookNumberBetweenMinAndMax(list, BOOK_MIN, BOOK_MAX))
-                    .isFalse();
-        }
-
-        @Test
-        void 책페이지가1과400사이인유효한값인지() {
-            List<Integer> list = List.of(97, 98);
-            assertThat(
-                    Problem1Validation.isBookNumberBetweenMinAndMax(list, BOOK_MIN, BOOK_MAX))
-                    .isTrue();
-        }
-
-        @Test
-        void 리스트속원소가연속된값이아니면() {
-            List<Integer> list = List.of(39,41);
-            assertThat(
-                    Problem1Validation.isBookNumberContinuous(list))
-                    .isFalse();
-        }
-
-        @Test
-        void 리스트속원소가연속된값이면() {
-            List<Integer> list = List.of(39, 40);
-            assertThat(
-                    Problem1Validation.isBookNumberContinuous(list))
-                    .isTrue();
-        }
-
-        @Test
-        void 리스트의쌍이홀수홀수쌍인경우() {
-            List<Integer> list = List.of(39, 41);
-            assertThat(
-                    Problem1Validation.isBookNumberOddEvenPair(list))
-                    .isFalse();
-        }
-
-        @Test
-        void 리스트의쌍이짝수짝수쌍인경우() {
-            List<Integer> list = List.of(38, 40);
-            assertThat(
-                    Problem1Validation.isBookNumberOddEvenPair(list))
-                    .isFalse();
-        }
-
-        @Test
-        void 리스트의쌍이짝수홀수쌍인경우() {
-            List<Integer> list = List.of(38, 39);
-            assertThat(
-                    Problem1Validation.isBookNumberOddEvenPair(list))
-                    .isFalse();
-        }
-
-        @Test
-        void 리스트의쌍이홀수짝수쌍인경우() {
-            List<Integer> list = List.of(39, 40);
-            assertThat(
-                    Problem1Validation.isBookNumberOddEvenPair(list))
-                    .isTrue();
-        }
-
-        @Test
-        void 첫번째값에1더한값이두번째값이안되는경우() {
-            List<Integer> list = List.of(39, 42);
-            assertThat(
-                    Problem1Validation.isFirstAddOneEqualsSecondValue(list))
-                    .isFalse();
-        }
-
-        @Test
-        void 첫번째값에1더한값이두번째값이되는경우() {
-            List<Integer> list = List.of(39, 40);
-            assertThat(
-                    Problem1Validation.isFirstAddOneEqualsSecondValue(list))
-                    .isTrue();
-        }
-
-        @Test
-        void validation메써드들이lazyChecking하는지확인하자() {
-            List<Integer> list = List.of(39);
-            assertThat(Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH) && Problem1Validation.isBookNumberContinuous(list))
-                    .isFalse();
-
-        }
+//        @Test
+//        void 길이가2보다큰경우테스트() {
+//            List<Integer> list = List.of(97, 98, 100);
+//            assertThat(
+//                    Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 길이가2보다작은경우테스트() {
+//            List<Integer> list = List.of(97);
+//            assertThat(
+//                    Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 길이가2인경우테스트() {
+//            List<Integer> list = List.of(97, 98);
+//            assertThat(
+//                    Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH))
+//                    .isTrue();
+//        }
+//
+//        @Test
+//        void 책페이지가1미만이포함되있다면() {
+//            List<Integer> list = List.of(0, 3);
+//            assertThat(
+//                    Problem1Validation.isBookNumberBetweenMinAndMax(list, BOOK_MIN, BOOK_MAX))
+//                    .isFalse();
+//
+//        }
+//
+//        @Test
+//        void 책페이지가400초과인값이포함되어있다면() {
+//            List<Integer> list = List.of(401, 398);
+//            assertThat(
+//                    Problem1Validation.isBookNumberBetweenMinAndMax(list, BOOK_MIN, BOOK_MAX))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 책페이지가1과400사이인유효한값인지() {
+//            List<Integer> list = List.of(97, 98);
+//            assertThat(
+//                    Problem1Validation.isBookNumberBetweenMinAndMax(list, BOOK_MIN, BOOK_MAX))
+//                    .isTrue();
+//        }
+//
+//        @Test
+//        void 리스트속원소가연속된값이아니면() {
+//            List<Integer> list = List.of(39,41);
+//            assertThat(
+//                    Problem1Validation.isBookNumberContinuous(list))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 리스트속원소가연속된값이면() {
+//            List<Integer> list = List.of(39, 40);
+//            assertThat(
+//                    Problem1Validation.isBookNumberContinuous(list))
+//                    .isTrue();
+//        }
+//
+//        @Test
+//        void 리스트의쌍이홀수홀수쌍인경우() {
+//            List<Integer> list = List.of(39, 41);
+//            assertThat(
+//                    Problem1Validation.isBookNumberOddEvenPair(list))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 리스트의쌍이짝수짝수쌍인경우() {
+//            List<Integer> list = List.of(38, 40);
+//            assertThat(
+//                    Problem1Validation.isBookNumberOddEvenPair(list))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 리스트의쌍이짝수홀수쌍인경우() {
+//            List<Integer> list = List.of(38, 39);
+//            assertThat(
+//                    Problem1Validation.isBookNumberOddEvenPair(list))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 리스트의쌍이홀수짝수쌍인경우() {
+//            List<Integer> list = List.of(39, 40);
+//            assertThat(
+//                    Problem1Validation.isBookNumberOddEvenPair(list))
+//                    .isTrue();
+//        }
+//
+//        @Test
+//        void 첫번째값에1더한값이두번째값이안되는경우() {
+//            List<Integer> list = List.of(39, 42);
+//            assertThat(
+//                    Problem1Validation.isFirstAddOneEqualsSecondValue(list))
+//                    .isFalse();
+//        }
+//
+//        @Test
+//        void 첫번째값에1더한값이두번째값이되는경우() {
+//            List<Integer> list = List.of(39, 40);
+//            assertThat(
+//                    Problem1Validation.isFirstAddOneEqualsSecondValue(list))
+//                    .isTrue();
+//        }
+//
+//        @Test
+//        void validation메써드들이lazyChecking하는지확인하자() {
+//            List<Integer> list = List.of(39);
+//            assertThat(Problem1Validation.userInputSizeEquals(list, USER_INPUT_LENGTH) && Problem1Validation.isBookNumberContinuous(list))
+//                    .isFalse();
+//
+//        }
 
         @Test
         void validation메써드들을합쳐서확인해보자() {
@@ -188,69 +188,69 @@ public class Problem1CustomTest {
 - 각각의 user는 왼쪽 페이지, 오른쪽 페이지 각각 위의 연산을 진행하여 최대값을 선택한다.
 - 각각의 user는 반환된 최대값을 기준으로 승패가 결정된다.
  */
-        @Test
-        void crong이이기면() {
-            List<Integer> testList = List.of(1, 2);
-            int result = Problem1.result(testList.get(0), testList.get(1));
-            assertThat(result).isEqualTo(2);
-        }
-
-        @Test
-        void pobi가이기면() {
-            List<Integer> testList = List.of(2, 1);
-            int result = Problem1.result(testList.get(0), testList.get(1));
-            assertThat(result).isEqualTo(1);
-        }
-
-        @Test
-        void 무승부면() {
-            List<Integer> testList = List.of(1, 1);
-            int result = Problem1.result(testList.get(0), testList.get(1));
-            assertThat(result).isEqualTo(0);
-        }
-
-        @Test
-        void 최대값을반환해주는메써드() {
-            List<Integer> testList = List.of(1, 2);
-            int maxValue = Problem1.getMaxValue(testList.get(0), testList.get(1));
-            assertThat(maxValue).isEqualTo(2);
-        }
-
-        @Test
-        void 각자릿수더한값을반환해주는메써드() {
-            int value = 312;
-            int summation = Problem1.getDigitAddValue(value);
-            assertThat(summation).isEqualTo(6);
-        }
-
-        @Test
-        void 각자릿수곱한값을반환해주는메써드() {
-            int value = 322;
-            int summation = Problem1.getDigitMultiplyValue(value);
-            assertThat(summation).isEqualTo(12);
-        }
-
-        @Test
-        void 각자리수곱셈의결과와덧셈의결과중최대값을뱉어내는지() {
-            int value = 23;
-            int maxValue = Problem1.chooseMaxValueBetweenAddAndMultiply(value);
-            assertThat(maxValue).isEqualTo(6);
-        }
-
-        @Test
-        void 왼쪽과오른쪽페이지의결과중최대값을뱉어내는지() {
-            List<Integer> testList = List.of(29, 30);
-            int maxValue = Problem1.chooseMaxValueLeftOrRight(testList);
-            assertThat(maxValue).isEqualTo(18);
-
-        }
-
-        @Test
-        void 사용자1과사용자2의결과를비교해줄메써드() {
-            List<Integer> user1 = List.of(29, 30);
-            List<Integer> user2 = List.of(31, 32);
-            List<Integer> result = Problem1.userResult(user1, user2);
-            assertThat(result).containsExactly(18, 6);
-        }
+//        @Test
+//        void crong이이기면() {
+//            List<Integer> testList = List.of(1, 2);
+//            int result = Problem1.result(testList.get(0), testList.get(1));
+//            assertThat(result).isEqualTo(2);
+//        }
+//
+//        @Test
+//        void pobi가이기면() {
+//            List<Integer> testList = List.of(2, 1);
+//            int result = Problem1.result(testList.get(0), testList.get(1));
+//            assertThat(result).isEqualTo(1);
+//        }
+//
+//        @Test
+//        void 무승부면() {
+//            List<Integer> testList = List.of(1, 1);
+//            int result = Problem1.result(testList.get(0), testList.get(1));
+//            assertThat(result).isEqualTo(0);
+//        }
+//
+//        @Test
+//        void 최대값을반환해주는메써드() {
+//            List<Integer> testList = List.of(1, 2);
+//            int maxValue = Problem1.getMaxValue(testList.get(0), testList.get(1));
+//            assertThat(maxValue).isEqualTo(2);
+//        }
+//
+//        @Test
+//        void 각자릿수더한값을반환해주는메써드() {
+//            int value = 312;
+//            int summation = Problem1.getDigitAddValue(value);
+//            assertThat(summation).isEqualTo(6);
+//        }
+//
+//        @Test
+//        void 각자릿수곱한값을반환해주는메써드() {
+//            int value = 322;
+//            int summation = Problem1.getDigitMultiplyValue(value);
+//            assertThat(summation).isEqualTo(12);
+//        }
+//
+//        @Test
+//        void 각자리수곱셈의결과와덧셈의결과중최대값을뱉어내는지() {
+//            int value = 23;
+//            int maxValue = Problem1.chooseMaxValueBetweenAddAndMultiply(value);
+//            assertThat(maxValue).isEqualTo(6);
+//        }
+//
+//        @Test
+//        void 왼쪽과오른쪽페이지의결과중최대값을뱉어내는지() {
+//            List<Integer> testList = List.of(29, 30);
+//            int maxValue = Problem1.chooseMaxValueLeftOrRight(testList);
+//            assertThat(maxValue).isEqualTo(18);
+//
+//        }
+//
+//        @Test
+//        void 사용자1과사용자2의결과를비교해줄메써드() {
+//            List<Integer> user1 = List.of(29, 30);
+//            List<Integer> user2 = List.of(31, 32);
+//            List<Integer> result = Problem1.userResult(user1, user2);
+//            assertThat(result).containsExactly(18, 6);
+//        }
     }
 }
