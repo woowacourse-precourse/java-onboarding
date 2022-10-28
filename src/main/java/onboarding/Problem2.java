7db1eda7d11ca3a667b2536565c6da418e9cbec1
democracyKim <107744617+juspian@users.noreply.github.com>
2022-10-28 19:57:13 +0900

    solution function complete

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import onboarding.solution2.*;
+import java.util.Stack;
+
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        TypeChanger change = new TypeChanger();
+        RedundantChecker checker = new RedundantChecker();
+        char[] str = change.stringToCharArray(cryptogram);
+        Stack<Character> result = checker.removeRedundancy(str);
+        String answer = change.stackToString(result);
         return answer;
     }
 }
