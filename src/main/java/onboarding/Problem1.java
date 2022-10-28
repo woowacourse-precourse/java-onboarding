ff7beea90492c485b356afd8b26132ac10adad74
homecoder98 <homecoder98@gmail.com>
2022-10-28 21:44:29 +0900

    Feat : Calculate score

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiScore = getScore(pobi.get(1));
+        int crongScore = getScore(crong.get(1));
+        if (pobiScore == crongScore) return 0;
+        return (pobiScore < crongScore)? 2 : 1;
+    }
+
+    static int getScore(int i) {
+        int j = 0, k = 1;
+        while (i > 0) {
+            int d = i % 10;
+            j += d;
+            k *= d;
+            i /= 10;
+        }
+        return (j > k)?j : k;
     }
 }
