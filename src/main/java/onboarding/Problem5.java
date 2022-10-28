0b21f7c889143bf498994b919db77862601923ba
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 16:59:06 +0900

    [Problem5] 기능 목록 설계

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    /**
+     * 기능 목록
+     * 1. 큰 단위로 화폐 교환
+     */
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
