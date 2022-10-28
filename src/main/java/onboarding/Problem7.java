716a82507158f14b844edf3b9185573a2e7aed47
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-28 15:44:08 +0900

    feat:7번 추천친구 목록중 제거해야하는 친구 분별 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,9 +12,14 @@ public class Problem7 {
 
         List<String> user_friend= new ArrayList<>();
         user_friend=map.get(user);
-        Map<String, Integer> friend_score = new HashMap<>();
+        Map<String, Integer> friend_score = new HashMap<>(); // (map (이름 : 점수))
 
         userFriendDelete(map, user_friend); // 사용자 친구는 항목에서 제거 (추천할 필요 없으니)
+
+        friendScorePut(visitors, map, user_friend, friend_score); // friendscore 부분에다가 넣어주기.
+
+        removeNoResult(user, friend_score, visitors, user_friend); // 추천친구 목록에 들어가면 안되는 친구 분별하고 제외하는 과정
+
 
 
 
@@ -50,6 +55,56 @@ public class Problem7 {
 
             map.get(friend1).add(friend2);
             map.get(friend2).add(friend1); // 친구1,2,3 추가 (양방향적으로)
+        }
+    }
+
+    private static void friendScorePut(List<String> visitors, Map<String, List<String>> map, List<String> user_friend, Map<String, Integer> friend_score) {
+        for(String s : map.keySet()){
+            friend_score.put(s, 0);
+        }
+
+        for(String s : visitors){
+            friend_score.put(s,0);
+        }
+
+        for(String s : visitors){
+            int a= friend_score.get(s)+1;
+            friend_score.put(s, a);
+        }
+
+        for(String s : map.keySet()){ // friend_score에 넣는 부분
+            for(int i = 0; i< user_friend.size(); i++){
+                for(int j = 0; j< map.get(s).size(); j++){
+                    if(user_friend.get(i).equals(map.get(s).get(j))){
+                        int a= friend_score.get(s)+10;
+                        friend_score.put(s, a);
+                    }
+                }
+            }
+        }
+    }
+
+
+    private static void removeNoResult(String user, Map<String, Integer> friend_score, List<String> visitors, List<String> user_friend) {
+        if(friend_score.containsKey(user)){
+            friend_score.remove(user); // 사용자와 친구인 경우 제거
+        }
+        for(String s : visitors){ // 방문자 제거
+            if(user_friend.contains(s)){
+                friend_score.remove(s);
+            }
+        }
+
+        List<String> removed = new ArrayList<>();
+
+        for(String s: friend_score.keySet()){
+            if(friend_score.get(s)==0){
+                removed.add(s); //0점인 경우도 제거하기위한 제거리스트
+            }
+        }
+
+        for(int i=0; i<removed.size(); i++){
+            friend_score.remove(removed.get(i)); // 0점인 친구들 제거
         }
     }
 }
