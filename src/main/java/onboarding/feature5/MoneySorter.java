517e3748a5b2149fa5f294b5cc5347f5443ebfe2
areyouhun <areyouhun@gmail.com>
2022-10-29 04:42:09 +0900

    feat(prob5): add field and constructor to money sorter class

--- src/main/java/onboarding/feature5/MoneySorter.java
+++ src/main/java/onboarding/feature5/MoneySorter.java
@@ -1,8 +1,21 @@
 package onboarding.feature5;
 
+import static onboarding.feature5.Constants.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MoneySorter {
+    private int money = 0;
+    private List<Integer> billsSorted;
+    private int numberOfBill = 0;
+
+    public MoneySorter(int money) {
+        this.money = money;
+        for (int i = 0; i < CURRENCY_UNIT.size(); i++) {
+            billsSorted.add(numberOfBill);
+        }
+    }
     public static void increaseNumberOfBill(List<Integer> billsSorted, Integer increasement) {
         for (int i = 0; i < billsSorted.size(); i++) {
             Integer currentNumberOfBill = billsSorted.get(i);
