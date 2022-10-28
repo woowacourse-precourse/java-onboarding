83c4da7ba74e9a775ede136e9a5a97ccc1c2224c
syeonbot <asdf748596123@gmail.com>
2022-10-29 04:03:50 +0900

    feat: problem3 validate 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    public static final int MINIMUM_STRING_LENGTH = 1;
+    public static final int MAXIMUM_STRING_LENGTH = 10000;
     public static int solution(int number) {
+        validateRange(number);
         int answer = 0;
         return answer;
+    }
+    public static void validateRange(int number) {
+        if (number < MINIMUM_STRING_LENGTH || MAXIMUM_STRING_LENGTH < number) {
+            throw new IllegalArgumentException();
+        }
     }
 }
