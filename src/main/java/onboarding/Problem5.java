7514f441e19c380ce7b97a8dcce1fa4898d6839f
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-28 10:08:01 +0900

    문제 5 -나눠줄 액수 리스트화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+		// 나눠줄 액수 리스트화 하기,
+	static final int[] moneyArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+	public static List<Integer> solution(int money) {
+		// 돈을 받고
+		// 몫은 리스트에 입력,
+		// 나머지는 업데이트
+		List<Integer> answer = new ArrayList<Integer>();
+		return answer;
+	}
+
 }
