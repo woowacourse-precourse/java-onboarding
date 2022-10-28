fb83cb237810c3f27f3dabe001a6860fb727caec
sosow0212 <sosow0212@naver.com>
2022-10-28 16:10:00 +0900

    feat: Problem3 - 3,6,9에 해당하는 상수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem3 {
+    static final char THREE = '3';
+    static final char SIX = '6';
+    static final char NINE = '9';
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
