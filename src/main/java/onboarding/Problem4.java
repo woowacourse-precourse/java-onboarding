f4f3162ba18b5c5d4039ea1759da6a70de11acb8
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-10-28 23:28:14 +0900

    Problem4 기능 목록 1 : 사전을 배열로 저장한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
+
         String answer = "";
+
+        List<Character> upperDictionary = List.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M');
+        List<Character> lowerDictionary = List.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm');
+        List<Character> upperDictionary2 = List.of('Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N');
+        List<Character> lowerDictionary2 = List.of('z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n');
+
+
         return answer;
     }
 }
