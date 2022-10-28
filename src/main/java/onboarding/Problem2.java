20e0c68f75087baca9105cbf90dd50057d3dea63
cookienc <kim76744@naver.com>
2022-10-28 13:17:36 +0900

    feat(Problem2): Problem2 validate추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,8 +5,14 @@ public class Problem2 {
     public static final char ALPHABET_START = 'a';
     public static final char ALPHABET_END = 'z';
     public static final String DUPLICATE = "{2,}";
+    public static final String PLEASE_ENTER_RIGHT_CRYPTOGRAM = "정확한 암호문을 입력해주세요.";
 
     public static String solution(String cryptogram) {
+
+        if (validateCryptogram(cryptogram)) {
+            throw new IllegalArgumentException(PLEASE_ENTER_RIGHT_CRYPTOGRAM);
+        }
+
         while (true) {
             int originLength = cryptogram.length();
             cryptogram = removeDuplicateAlphabet(cryptogram);
@@ -17,6 +23,29 @@ public class Problem2 {
         }
 
         return cryptogram;
+    }
+
+    private static boolean validateCryptogram(String cryptogram) {
+        return validateLength(cryptogram) || validateAlphabet(cryptogram);
+    }
+
+    private static boolean validateLength(String cryptogram) {
+        return cryptogram.length() < 1 || cryptogram.length() > 1000;
+    }
+
+    private static boolean validateAlphabet(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char alphabet = cryptogram.charAt(i);
+
+            if (isCapitalLetter(alphabet)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isCapitalLetter(char alphabet) {
+        return alphabet >= 'A' && alphabet <= 'Z';
     }
 
     private static String removeDuplicateAlphabet(String cryptogram) {
