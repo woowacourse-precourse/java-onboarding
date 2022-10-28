01f3158ee5f310ecce8e6fe57e5a424d620a0793
math0917 <math0917@naver.com>
2022-10-29 02:01:51 +0900

    feat : 1 ~ userInput까지 박수친 횟수를 반환하는 전반적인 메써드 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import onboarding.problem3.Problem3Validation;
+
 public class Problem3 {
+    private static final int MIN_INPUT = 1;
+    private static final int MAX_INPUT = 10000;
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        if (Problem3Validation.problem3InputValidation(number, MIN_INPUT, MAX_INPUT)) {
+            int clapHandsCount = 0;
+            for (int i = 1; i <= number; i++) {
+                clapHandsCount += thisTurnClapCounts(i);
+            }
+            return clapHandsCount;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int thisTurnClapCounts(int number) {
+        return 0;
     }
 }
