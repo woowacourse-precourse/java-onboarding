4e0ab64914f002e2e504824d2c397db65313cd49
woogi <dnrlwo11@gmail.com>
2022-10-28 23:14:46 +0900

    [problem5] 돈의 액수보다 화폐단위보다 적으면 0을 리턴

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,13 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         int[] kind_of_money = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int remain_money = money;
+        for (int won: kind_of_money) {
+            if (won > remain_money) {
+                answer.add(0);
+                continue;
+            }
+        }
         return answer;
     }
 }
