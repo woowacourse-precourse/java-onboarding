d962ecc66ebca5f4fbf139d3e4a75075f41350d6
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-10-28 15:24:43 +0900

    feat: 입력 예외처리 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -37,25 +37,29 @@ class Problem1 {
     }
 
     private static boolean isValidInput(List<Integer> page) {
-        if (page.get(1) - page.get(0) != 1) {
+        int left = page.get(0);
+        int right = page.get(1);
+
+        if(left < 2 || left > 399 ||
+                    right < 2 || right > 399)
+            return (false);
+        if (left % 2 == 0 || right % 2 == 1 || right - left != 1) {
             return (false);
         }
-        for (int p : page) {
-            if (p < -1 || p > 400) {
-                return (false);
-            }
-        }
+
         return (true);
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int max_pobi = maxPage(pobi);
-        int max_crong = maxPage(crong);
 
         if (!isValidInput(pobi) || !isValidInput(crong)) {
             return (-1);
         }
+
+        int max_pobi = maxPage(pobi);
+        int max_crong = maxPage(crong);
+
         if (max_pobi > max_crong) {
             answer = 1;
         }
