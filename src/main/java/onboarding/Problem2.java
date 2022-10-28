5c90112b75bd049ba95b91502f0ccd0a82e0164a
Seung-Won-Kim <hesjs5@skuniv.ac.kr>
2022-10-28 18:21:44 +0900

    Feat(Problem2): 배열 안에 연속적으로 중복된 문자가 있는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
@@ -13,5 +15,20 @@ public class Problem2 {
         String[] splitCryptogram = cryptogram.split("");
 
         return Arrays.asList(splitCryptogram);
+    }
+
+    static Set<Integer> getDuplicatedCharIndex(List<String> list) {
+
+        Set<Integer> duplicatedCharIndex = new HashSet<>();
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            // 중복된 문자의 인덱스 찾기
+            if (list.get(i).equals(list.get(i + 1))) {
+                duplicatedCharIndex.add(i);
+                duplicatedCharIndex.add(i + 1);
+            }
+        }
+
+        return duplicatedCharIndex;
     }
 }
--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -3,6 +3,7 @@ package onboarding;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +22,34 @@ class Problem2Test {
         //then
         assertThat(cryptogramIngredients1).containsExactly("b", "r", "o", "w", "o", "a", "n", "o", "o", "m", "m", "n", "a", "o", "n");
         assertThat(cryptogramIngredients2).containsExactly("z", "y", "e", "l", "l", "e", "y", "z");
+    }
+
+    @Test
+    public void getDuplicatedCharIndexTest() {
+        //given
+        String cryptogram1 = "browoanoommnaon";
+        String cryptogram2 = "browoannaon";
+        String cryptogram3 = "browoaaon";
+        String cryptogram4 = "browoon";
+        String cryptogram5 = "brown";
+        List<String> cryptogramIngredients1 = Problem2.getCryptogramIngredients(cryptogram1);
+        List<String> cryptogramIngredients2 = Problem2.getCryptogramIngredients(cryptogram2);
+        List<String> cryptogramIngredients3 = Problem2.getCryptogramIngredients(cryptogram3);
+        List<String> cryptogramIngredients4 = Problem2.getCryptogramIngredients(cryptogram4);
+        List<String> cryptogramIngredients5 = Problem2.getCryptogramIngredients(cryptogram5);
+
+        //when
+        Set<Integer> duplicatedCharIndex1 = Problem2.getDuplicatedCharIndex(cryptogramIngredients1);
+        Set<Integer> duplicatedCharIndex2 = Problem2.getDuplicatedCharIndex(cryptogramIngredients2);
+        Set<Integer> duplicatedCharIndex3 = Problem2.getDuplicatedCharIndex(cryptogramIngredients3);
+        Set<Integer> duplicatedCharIndex4 = Problem2.getDuplicatedCharIndex(cryptogramIngredients4);
+        Set<Integer> duplicatedCharIndex5 = Problem2.getDuplicatedCharIndex(cryptogramIngredients5);
+
+        //then
+        assertThat(duplicatedCharIndex1).containsExactly(7, 8, 9, 10);
+        assertThat(duplicatedCharIndex2).containsExactly(6, 7);
+        assertThat(duplicatedCharIndex3).containsExactly(5, 6);
+        assertThat(duplicatedCharIndex4).containsExactly(4, 5);
+        assertThat(duplicatedCharIndex5).isEmpty();
     }
 }
