484aa424dea54c653f74995cf99d3efc66b3402e
backFox <jorealbusiness@gmail.com>
2022-10-28 19:49:46 +0900

    refactor(problem4): 단어를 뒤집는 핵심 비즈니스 로직을 별도의 서비스 레이어로 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,33 +1,10 @@
 package onboarding;
 
-import onboarding.problem4.BlueFrogValidator;
+import onboarding.problem4.BlueFrogService;
 
-import static java.lang.Character.*;
 
 public class Problem4 {
     public static String solution(String word) {
-        BlueFrogValidator.validate(word);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        word.chars().forEach(ascii -> stringBuilder.append(reverse(ascii)));
-
-        return stringBuilder.toString();
-    }
-
-    private static char reverse(int ascii) {
-        if (isLowerCase(ascii)) {
-            return reverseLowerCase(ascii);
-        } else if (isUpperCase(ascii)) {
-            return reverseUpperCase(ascii);
-        }
-        return (char)ascii;
-    }
-
-    private static char reverseUpperCase(int ascii) {
-        return (char)(90 - (ascii - 65));
-    }
-
-    private static char reverseLowerCase(int ascii) {
-        return (char)(122 - (ascii - 97));
+        return BlueFrogService.reverseMotherSaying(word);
     }
 }
---
+++ src/main/java/onboarding/problem4/BlueFrogService.java
@@ -0,0 +1,34 @@
+package onboarding.problem4;
+
+public class BlueFrogService {
+    public static String reverseMotherSaying(String sentence) {
+        BlueFrogValidator.validate(sentence);
+        return reverseSentence(sentence);
+    }
+
+    private static String reverseSentence(String word) {
+        StringBuilder resultBuilder = new StringBuilder();
+        char[] spells = word.toCharArray();
+        for (char spell : spells) {
+            resultBuilder.append(reverseSpell(spell));
+        }
+        return resultBuilder.toString();
+    }
+
+    private static char reverseSpell(char spell) {
+        if (Character.isLowerCase(spell)) {
+            return reverseLowerCase(spell);
+        } else if (Character.isUpperCase(spell)) {
+            return reverseUpperCase(spell);
+        }
+        return spell;
+    }
+
+    private static char reverseUpperCase(char asciiCode) {
+        return (char)('Z' - (asciiCode - 'A'));
+    }
+
+    private static char reverseLowerCase(char asciiCode) {
+        return (char)('z' - (asciiCode - 'a'));
+    }
+}
