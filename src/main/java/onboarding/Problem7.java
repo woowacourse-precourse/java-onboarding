f35018edd68defa3e097fda4a3bfb07f0966410c
Kimhyeyun <cqqwer@gmail.com>
2022-10-28 20:25:42 +0900

    [Problem 7] MAKE 친구 관계 그래프 FUNCTION

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, List<String>> friendsMap = makeFriendsMap(friends, user);
+    }
+
+    private static Map<String, List<String>> makeFriendsMap(List<List<String>> friends, String user) {
+        Map<String, List<String>> tmp = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            tmp.computeIfAbsent(friend.get(0), s -> new ArrayList<>()).add(friend.get(1));
+            tmp.computeIfAbsent(friend.get(1), s -> new ArrayList<>()).add(friend.get(0));
+        }
+        return tmp;
     }
 }
