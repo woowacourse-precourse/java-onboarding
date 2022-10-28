ece365b105a3bd6a10d27a399b145d8e37a6c9c7
JunJI97 <bigwkddo@gmail.com>
2022-10-29 04:22:15 +0900

    [문제해결] 문제 4번 풀이

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i=0; i < word.length(); i++) {
+            char temp = word.charAt(i);
+
+            if(Character.isUpperCase(temp)) { // 대문자의 경우
+                answer += (char)(155 - temp);
+            } else if (Character.isLowerCase(temp)){ // 소문자의 경우
+                answer += (char)(219 - temp);
+            } else { // 그 외 문자의 경우
+                answer += temp;
+                System.out.println(temp);
+            }
+        }
+
         return answer;
     }
 }
