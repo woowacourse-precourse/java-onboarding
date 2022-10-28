e59f146c81cab462e8d8e55869487748992f18db
Hojinida <opqrs1103@gmail.com>
2022-10-29 03:17:53 +0900

    feat: 문제 3번 10의자리 이상의 정수를 입력 시 각 자리수로 나누어서 int[]로 반환해주는 converter 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,15 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static int[] converter(int num){
+        return Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
     }
 
     public static boolean check369(int num){
