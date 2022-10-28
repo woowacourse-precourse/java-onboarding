c5ade02ca4f76432278dd900914f144fa85ed00d
jwp345 <jwp135@naver.com>
2022-10-28 19:01:27 +0900

    problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+//        String answer = "";
+        Map<Character, Character> dict = new HashMap<>();
+
+        char curr = 'Z';
+        for(char ch = 'A'; ch <= 'Z'; ch++) {
+            dict.put(ch, curr);
+            dict.put((char)(ch - 'A' + 'a'), (char)(curr - 'A' + 'a'));
+            curr--;
+        }
+
+        StringBuilder temp = new StringBuilder();
+        for(char ch : word.toCharArray()) {
+            if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+                temp.append(dict.get(ch));
+            }
+            else{
+                temp.append(ch);
+            }
+        }
+
+        return temp.toString();
     }
 }
