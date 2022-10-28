e5d599f2b7e631b0431860f3c439341059167163
stk346 <stk346@gamil.com>
2022-10-28 13:14:56 +0900

    feat. 주어진 숫자의 자릿수 덧셈, 곱 기능 구현 완료.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,28 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private int doPlus(int num) {
+        String stringInt = String.valueOf(num);
+        int sum = 0;
+        for (int i=0; i< stringInt.length(); i++) {
+            String convertedNum = String.valueOf(stringInt.charAt(i));
+            sum += Integer.parseInt(convertedNum);
+        }
+        return sum;
+    }
+
+    private int doMultiply(int num) {
+        String stringInt = String.valueOf(num);
+        int multiply = 1;
+        for (int i=0; i< stringInt.length(); i++) {
+            String convertedNum = String.valueOf(stringInt.charAt(i));
+            multiply *= Integer.parseInt(convertedNum);
+        }
+        return multiply;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
