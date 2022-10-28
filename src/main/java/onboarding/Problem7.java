3f9e9eaf37a8b72d9519edea30554c64fc11ab22
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-29 01:15:14 +0900

    [Problem7] 기능 목록 설계

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    /**
+     * 기능 목록
+     * 1. user의 친구목록
+     * 2. friends와 visitors에 있는 친구 정리
+     * 3. user와 함께 아는 친구의 수 점수 계산
+     * 4. user의 타임 라인에 방문한 친구 점수 계산
+     * 5. 추천인 5명 뽑기
+     * 6. 이미 친구인 사이 삭제
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
