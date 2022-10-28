587117ce7addaaeb270dd06c9f7da11f82fd0302
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-29 01:03:26 +0900

    feat: Problem5 돈의액수를 담은 배열 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,11 +7,12 @@ import java.util.*;
  * 1. greedy를 이용해 최적의 해를 찾도록 함
  * 2. 돈의 액수를 나열한 배열을 생성한다. {50000, 10000 … 10, 1}
  * 2. 돈의 액수를 담은 배열을 순회하면서, 변환된 개수를 list에 담는다.
- * 3. ist를 반환한다.
+ * 3. list를 반환한다.
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List <Integer> result = new ArrayList<>();
+        List <Integer> amount = List.of(50000, 10000, 5000, 1000, 500, 100 ,50, 10, 1);
+        return result;
     }
 }
