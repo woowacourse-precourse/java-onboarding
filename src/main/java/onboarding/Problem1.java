ea315da6f89f90638d85da3f0e931e42fc9c7cd2
meunrud <y3a0522@gmail.com>
2022-10-29 02:33:09 +0900

    최종본

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,54 +1,54 @@
 package onboarding;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
-class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = 0;
 
+class Problem1 {
+   public static final int LISTSIZE = 2;
+   public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer = 0;
         try {
-            exception(pobi.get(1),pobi.get(0));
-            exception(crong.get(1),crong.get(0));
-            int pobiMaxNum = maxNumber(pobi.get(0),pobi.get(1));
-            int crongMaxNum = maxNumber(crong.get(0), crong.get(1));
+            exception(pobi);
+            exception(crong);
+            int pobiMaxNum = maxNumber(pobi);
+            int crongMaxNum = maxNumber(crong);
 
             answer = winner(pobiMaxNum,crongMaxNum);
-        }catch (IOException e){
+        }catch (IOException | RuntimeException e){
             answer = -1;
         }
 
-        return answer;
+       return answer;
     }
 
-    public static int maxNumber(Integer leftPage, Integer rightPage){
-        int maxNumber = 0;
-        int maxMulti = Math.max(multiLeftpage(leftPage),multiRightpage(rightPage));
-        int maxSum = Math.max(sumLeftPage(leftPage),sumRightpage(rightPage));
+    public static int maxNumber(List<Integer>someone){
+        int MaxsumPageNumber = maxSumPageNumber(someone);
+        int MaxmultiPageNumber = maxmultipage(someone);
 
-        return maxNumber = Math.max(maxMulti,maxSum);
-    }
-
-
-    public static int sumLeftPage(Integer pageNum){
-        int sumLeftPage = 0;
-        while (pageNum != 0){
-            sumLeftPage += pageNum%10;
-            pageNum /= 10;
-        }return  sumLeftPage;
+        return Math.max(MaxsumPageNumber,MaxmultiPageNumber);
     }
 
 
+    public static int maxSumPageNumber(List<Integer>someone){
+        int leftPageSum = 0;
+        int leftpage = someone.get(0);
+            while(leftpage != 0) {
+                leftPageSum += leftpage % 10;
+                leftpage /= 10;
+            }
+        int rightPageSum = 0;
+        int rightpage = someone.get(1);
+            while(rightpage != 0) {
+                rightPageSum += rightpage % 10;
+                rightpage /= 10;
+            }
+        return Math.max(leftPageSum,rightPageSum);
+   }
 
-    public static int sumRightpage(Integer pageNum){
-        int sumRightPage = 0;
-        while (pageNum != 0){
-            sumRightPage += pageNum%10;
-            pageNum /= 10;
-        }return  sumRightPage;
-    }
-
+   public static int maxmultipage(List<Integer>someone){
+       return Math.max(multiLeftpage(someone.get(0)), multiRightpage(someone.get(1)));
+   }
 
     public static int multiLeftpage(Integer pageNum){
         int multiLeftpage = 1;
@@ -82,22 +82,24 @@ class Problem1 {
         int winner = 0;
         if(pobiMaxNum > crongMaxNum){
             winner = 1;
-        }else if(pobiMaxNum < crongMaxNum){
+        }
+        if(pobiMaxNum < crongMaxNum){
             winner = 2;
-        }else if (pobiMaxNum == crongMaxNum){
-            winner = 0;
         }
         return winner;
 
     }
 
-    public static void exception(Integer leftPageNum, Integer rightPageNum) throws IOException{
-        int exception = 0;
-        if(rightPageNum - leftPageNum != 1){
+    public static void exception(List<Integer>someone) throws IOException{
+        for(int i = 0; i<LISTSIZE; i++){
+        if((someone.get(1) - someone.get(0)) != 1){
             throw new IOException();
         }
+        if(someone.get(0)==1 || someone.get(1)==400 ){
+            throw new IOException();
+        }
+        }
+
     }
-
-
 
 }
