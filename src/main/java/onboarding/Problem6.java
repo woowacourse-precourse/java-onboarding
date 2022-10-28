3d4c4db1cedcaf456c68276b4ae27aa75afcdac9
antaeho <an981022@naver.com>
2022-10-28 14:40:38 +0900

    Problem6 기능구현사항 목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,12 @@
 package onboarding;
 
 import java.util.List;
-
+/*
+- 기능 구현사항 목록 -
+1. email과 nickname을 가지는 class 생성
+2. 2글자 단위로 잘라서
+3. 중복된다면 set에 저장
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
