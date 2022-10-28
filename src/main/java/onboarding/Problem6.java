9bca99ae423ec768e13d24a9613c5d6ef5c08d1e
kjb95 <topmedia44@gmail.com>
2022-10-28 16:35:48 +0900

    feat: Problem6 - 키는 닉네임, 값은 닉네임이 가지는 두 글자 경우의 수의 문자열 리스트를 가지는 맵 구하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class Problem6 {
@@ -9,12 +11,55 @@ public class Problem6 {
     private static final int TWO = 2;
 
     public static List<String> solution(List<List<String>> forms) {
+        Map<String, List<String>> nicknameToTwoLetter = new HashMap();
+        Map<String, String> nicknameToEmail = new HashMap<>();
+        List<String> answer = new ArrayList<>();
 
-        List<String> answer = List.of("answer");
+        forms.forEach(man -> {
+            String email = man.get(0);
+            String nickname = man.get(1);
+            List<String> twoLetters = computeTwoLetters(nickname);
+
+            nicknameToTwoLetter.put(nickname, twoLetters);
+        });
+
+        //
+        //        forms.forEach(man -> {
+        //            String nickname = man.get(1);
+        //            List<String> twoLetters = computeTwoLetters(nickname);
+        //
+        //            nicknameTwoLetter.put(nickname, twoLetters);
+        //            nicknameToEmail.put(nickname, man.get(0));
+        //        });
+        //
+        //        IntStream.range(0, forms.size())
+        //            .map(i -> forms.size() - i - 1)
+        //            .forEach(i -> {
+        //                String email = forms.get(i)
+        //                    .get(0);
+        //                String nickname = forms.get(i)
+        //                    .get(1);
+        //                System.out.println("email : " + email);
+        //                System.out.println("nickname : " + nickname);
+        //
+        //                forms.remove(i);
+        //                List<String> nicknames = nicknameTwoLetter.get(nickname)
+        //                    .stream()
+        //                    .filter(twoLetter -> forms.stream()
+        //                        .filter(form -> nicknameTwoLetter.get(form.get(1))
+        //                            .contains(twoLetter))
+        //                        .count() != 0)
+        //                    .collect(Collectors.toList());
+        //                if (nicknames.isEmpty()) {
+        //                    return ;
+        //                }
+        //                answer.add(email);
+        //                nicknames.stream().map(name -> nicknameToEmail.get(name)).forEach(em -> answer.add(em));
+        //            });
         return answer;
     }
 
-    private static List<String> computeTwoLetter(String input) {
+    private static List<String> computeTwoLetters(String input) {
         List<String> twoLetter = new ArrayList<>();
 
         IntStream.range(0, input.length() - TWO)
