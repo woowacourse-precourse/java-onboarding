60fddb45fc6e9faff73bac5f3881049179017c25
sangil <gsl0505@naver.com>
2022-10-28 15:40:16 +0900

    refactor(PROBLEM3): indent 감소와 이해하기 쉬운 메소드로 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,28 +1,22 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
-	private static final int NUMBER_OF_DIGIT_DIVISION = 10;
-	private static final int[] CLAP_NUMBERS = new int[] {3, 6, 9};
+	private static final String NUMBER_OF_DIGIT_DIVISION = "";
+	private static final String CLAP_NUMBER = "[369]";
 
 	public static int solution(int number) {
-		int answer = 0;
-		for (int i = 0; i <= number; i++) {
-			answer += isClap(i);
-		}
-		return answer;
-	}
-
-	private static int isClap(int number) {
 		int clapCount = 0;
-		while (number != 0) {
-			for (int i = 0; i < CLAP_NUMBERS.length; i++) {
-				if (number % NUMBER_OF_DIGIT_DIVISION == CLAP_NUMBERS[i]) {
-					System.out.println(number);
-					clapCount++;
-				}
-			}
-			number /= NUMBER_OF_DIGIT_DIVISION;
+		for (int i = 0; i <= number; i++) {
+			clapCount += isClap(String.valueOf(i));
 		}
 		return clapCount;
+	}
+
+	private static int isClap(String number) {
+		return (int)Arrays.stream(number.split(NUMBER_OF_DIGIT_DIVISION))
+			.filter(digit -> digit.matches(CLAP_NUMBER))
+			.count();
 	}
 }
