3e6ea803a6ba84fab2bbcbc993bc7f45b32ac4f2
seonghoo1217 <seonghoo1217@naver.com>
2022-10-28 16:04:54 +0900

    feat : Left Page number must be Even and Right Page number must be Odd if is not throw Exception

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -18,6 +18,11 @@ class Problem1 {
         int right = list.get(1);
 
         checkPages(left,right);
+        if (isEven(left)&&isOdd(right)){
+
+        }else {
+            throw new Exception("홀수 짝수가 지켜지지 않음");
+        }
     }
 
     public static void checkLength(List<Integer> pobi,List<Integer> crong) throws Exception{
@@ -26,4 +31,15 @@ class Problem1 {
     public static void checkPages(int left,int right) throws Exception {
         if (right-left!=1) throw new Exception ("페이지는 1차이어야 합니다.");
     }
+
+    public static boolean isEven(int page){
+        if (page%2==0) return true;
+        else return false;
+    }
+
+    public static boolean isOdd(int page){
+        if (!isEven(page)) return true;
+        else return false;
+    }
+
 }
