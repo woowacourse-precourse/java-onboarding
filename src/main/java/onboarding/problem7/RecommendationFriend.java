b2228d06cab82948aff049f8ddea0acf6706a86f
Heosejin98 <sejin3319@naver.com>
2022-10-29 07:14:23 +0900

    Feat : PROBLEM7 RecommendationFriend 클래스 추가
    추천 친구 id, score 저장

---
+++ src/main/java/onboarding/problem7/RecommendationFriend.java
@@ -0,0 +1,14 @@
+package onboarding.problem7;
+
+
+public class RecommendationFriend{
+
+    public int score;
+    public String id;
+
+    public RecommendationFriend(String id, int score) {
+        this.id = id;
+        this.score = score;
+    }
+
+}
