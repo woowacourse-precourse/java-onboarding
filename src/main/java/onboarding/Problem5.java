1ec353b8a1a5bdd0c730d1db5eaf531c2a8b618f
Lee Min Woo <lmw7414@naver.com>
2022-10-28 23:06:40 +0900

    problem5 - 기능 요소 설계
    5만원부터 시작하여 나눈 몫을 리스트에 차례대로 담는다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/** 구현해야할 기능
+ * [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+ * 5만원부터 시작하여 나눈 몫을 리스트에 차례대로 담는다.
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
