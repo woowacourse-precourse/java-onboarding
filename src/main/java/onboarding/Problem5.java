770d2ff03d742532f2c9e6fd10751729b3045dbd
nakalic <nakalic28@gmail.com>
2022-10-29 01:14:07 +0900

    feat(Problem5): solution 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+	static final List<Integer> typeOfMoney = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+	public static List<Integer> solution(int money) {
+
+		List<Integer> answer = new ArrayList<>();
+
+		for (int a : typeOfMoney) {
+			answer.add(withdrawMoney(money, a));
+			money = updateMoney(money, a);
+		}
+		return answer;
     }
 
 	private static int withdrawMoney(int money, int won) {
