bb86bdfb71e80f2efc5126aecebf9bb72b653f86
eunbii0213 <eunbii0213@naver.com>
2022-10-29 04:07:00 +0900

    refactor: (Problem2) refactoring

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,52 +6,69 @@ import java.util.StringTokenizer;
 
 public class Problem2 {
     public static final String ALPHABET_DELIMITER = "abcdefghijklmnopqrstuvwxyz";
-    public static List<String> list;
+    public static final int START_INDEX = 0;
+    public static final int INIT_SAME_LETTER_COUNT = 1;
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        list = new LinkedList<String>();
+        List<String> letterList = new LinkedList<>();
 
-        addLettersInList(cryptogram);
-        traverseList(list);
-        answer = listToString(list);
+        separateLetters(letterList, cryptogram);
+        decryption(letterList);
+        String answer = listToString(letterList);
 
         return answer;
     }
 
-    private static String listToString(List<String> list) {
+    public static String listToString(List<String> list) {
         String answer = "";
-        for(String alphabet : list){
-            answer+=alphabet;
+        for (String alphabet : list) {
+            answer += alphabet;
         }
         return answer;
     }
 
-    private static boolean isSameLetter(int i) {
-        if (list.get(i).equals(list.get(i + 1))) {
-            removeSameLetter(i);
+    public static boolean isSameLetter(List<String> letterList, int index, int compareIndex) {
+        if (letterList.get(index).equals(letterList.get(compareIndex))) {
             return true;
         }
         return false;
     }
 
-    private static void removeSameLetter(int i) {
-        list.remove(i + 1);
-        list.remove(i);
+    public static void removeSameLetter(List<String> letterList, int index, int sameLetterCount) {
+        for (int removeIndex = START_INDEX; removeIndex < sameLetterCount; removeIndex++) {
+            letterList.remove(index);
+        }
     }
 
-    private static void traverseList(List<String> list) {
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (isSameLetter(i)) {
-                i = -1;
+    public static void decryption(List<String> letterList) {
+        for (int index = START_INDEX; index < letterList.size() - 1; index++) {
+            int sameLetterCount = countSameLetter(letterList, INIT_SAME_LETTER_COUNT, index);
+
+            if (sameLetterCount != INIT_SAME_LETTER_COUNT) {
+                removeSameLetter(letterList, index, sameLetterCount);
+                index = -INIT_SAME_LETTER_COUNT;
             }
         }
     }
 
-    private static void addLettersInList(String cryptogram) {
-        StringTokenizer st = new StringTokenizer(cryptogram, ALPHABET_DELIMITER,true);
+    public static int countSameLetter(List<String> letterList, int sameLetterCount, int index) {
+        int compareIndex = index + 1;
+
+        while (compareIndex < letterList.size() && isSameLetter(letterList, index, compareIndex)) {
+            sameLetterCount++;
+            compareIndex++;
+        }
+        return sameLetterCount;
+    }
+
+    public static void separateLetters(List<String> letterList, String cryptogram) {
+        StringTokenizer st = new StringTokenizer(cryptogram, ALPHABET_DELIMITER, true);
+        addLettersInList(letterList, st);
+    }
+
+    public static void addLettersInList(List<String> letterList, StringTokenizer st) {
         while (st.hasMoreTokens()) {
-            list.add(st.nextToken());
+            letterList.add(st.nextToken());
         }
     }
 }
