7259ceb57e2fed3de8204416595acf7660df9f6c
ywj9811 <ywj9811@naver.com>
2022-10-29 00:14:11 +0900

    배열의 크기만큼 반복을 하며 나온 몫을 리스트에 추가를 해주면 50000 부터 1까지 순서대로 저장이 되게 된다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,11 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int moneyList[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i = 0; i < moneyList.length; i++) {
+            answer.add(money/moneyList[i]);
+            money %= moneyList[i];
+        }
         return answer;
     }
 }
