9f86bfb82d90c5677c743ca16892fb8be88bdd62
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-10-29 03:33:30 +0900

    Problem5 예외처리 money의 크기 1이상 1,000,000이하 예외처리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public  static  void catchException(int num){
+        assert (num>=1);
+        assert (num<1000000);
+    }
     public static List<Integer> solution(int money) {
+        catchException(money);
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
