32345cae2e3861a8b41890e32daac5cf2bdf9246
HamSeungChan <head0618@naver.com>
2022-10-28 17:47:06 +0900

    Refactor(Problem1): 변수명 함수명 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,29 +4,29 @@ import java.util.List;
 
 class Problem1 {
 
-  private static final int pobi_Win = 1;
-  private static final int crong_Win = 2;
-  private static final int draw = 0;
-  private static final int exception = -1;
-  private static final int LeftPage = 0;
-  private static final int RightPage = 1;
-  private static final int FirstPage = 0;
-  private static final int LastPage = 400;
+  private static final int POBI_WIN = 1;
+  private static final int CRONG_WIN = 2;
+  private static final int DRAW = 0;
+  private static final int EXCEPTION = -1;
+  private static final int LEFT_PAGE = 0;
+  private static final int RIGHT_PAGE = 1;
+  private static final int FIRST_PAGE = 0;
+  private static final int LAST_PAGE = 400;
 
   public static int solution(List<Integer> pobi, List<Integer> crong) {
     int answer = Integer.MAX_VALUE;
 
-    if(findPageException(pobi,crong)) return exception;
+    if(findPageException(pobi,crong)) return EXCEPTION;
 
     return answer = Winner(pobi,crong);
   }
 
   private static boolean findPageException(List<Integer> pobi, List<Integer> crong) {
-    if (pobi.get(LeftPage) < FirstPage || pobi.get(RightPage) > LastPage
-            || crong.get(LeftPage) < FirstPage || crong.get(RightPage) > LastPage) {
+    if (pobi.get(LEFT_PAGE) < FIRST_PAGE || pobi.get(RIGHT_PAGE) > LAST_PAGE
+            || crong.get(LEFT_PAGE) < FIRST_PAGE || crong.get(RIGHT_PAGE) > LAST_PAGE) {
       return true;
     }
-    if ((pobi.get(RightPage) - pobi.get(LeftPage)) != 1 || (crong.get(RightPage) - crong.get(LeftPage)) != 1) {
+    if ((pobi.get(RIGHT_PAGE) - pobi.get(LEFT_PAGE)) != 1 || (crong.get(RIGHT_PAGE) - crong.get(LEFT_PAGE)) != 1) {
       return true;
     }
     return false;
@@ -38,24 +38,27 @@ class Problem1 {
     int crong_Score = CompareScore(crong);
     int result = Integer.MAX_VALUE;
 
-    if(pobi_Score>crong_Score) result = pobi_Win;
-    else if(pobi_Score==crong_Score) result = draw;
-    else result = crong_Win;
+    if(pobi_Score>crong_Score)
+      result = POBI_WIN;
+    else if(pobi_Score==crong_Score)
+      result = DRAW;
+    else
+      result = CRONG_WIN;
 
     return result;
   }
 
   private static int CompareScore(List<Integer> page){
-    int LeftPage_Score = PageBestScore(page.get(LeftPage));
-    int RightPage_Score = PageBestScore(page.get(RightPage));
+    int LeftPage_Score = getPageBestScore(page.get(LEFT_PAGE));
+    int RightPage_Score = getPageBestScore(page.get(RIGHT_PAGE));
     return Math.max(LeftPage_Score,RightPage_Score);
   }
 
-  private static int PageBestScore(int bookpage){
-    return Math.max(Plus_Score(bookpage),Multiply_Score(bookpage));
+  private static int getPageBestScore(int bookpage){
+    return Math.max(getPlusScore(bookpage),getMultiplyScore(bookpage));
   }
 
-  private static int Plus_Score(int bookpage){
+  private static int getPlusScore(int bookpage){
     int plusScore = 0;
     while(bookpage !=0) {
       plusScore += bookpage % 10;
@@ -64,7 +67,7 @@ class Problem1 {
     return plusScore;
   }
 
-  private static int Multiply_Score(int bookpage) {
+  private static int getMultiplyScore(int bookpage) {
     int multiplyScore = 1;
     while(bookpage !=0) {
       multiplyScore *= bookpage % 10;
