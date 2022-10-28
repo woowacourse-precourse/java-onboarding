44e7ca688d82f9c5eda0cee577e016b90048e35c
Sieun Park <psieun0606@gmail.com>
2022-10-28 23:09:03 +0900

    feat: problem1 예외사항 처리 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,28 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static Boolean handleException(int leftPage, int rightPage) {   // 예외 사항 처리
+        if (rightPage - leftPage > 1) {             // 연속된 페이지가 아닐 경우
+            return true;
+        }
+        if (leftPage == 1 || rightPage == 400) {    // 시작면(1, 2)나 마지막 면(399, 400)이 포함되어 있을 경우
+            return true;
+        }
+        return false;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 포비와 크롱의 예외 확인
+        Boolean isExceptionPobi = handleException(pobi.get(0), pobi.get(1));
+        Boolean isExceptionCrong = handleException(crong.get(0), crong.get(1));
+
+
+
+
         return answer;
     }
+
 }
