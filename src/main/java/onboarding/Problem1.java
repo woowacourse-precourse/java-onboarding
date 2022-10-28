457f84a421e2aba77d4a53be31e24875851f4904
sohyundoh <lyny123@naver.com>
2022-10-28 22:07:30 +0900

    Problem 1 제한 사항 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,7 +1,11 @@
 package onboarding;
 
 import java.util.List;
-
+/*
+제한 사항
+    pobi와 crong의 길이는 2이다.
+    pobi와 crong에는 [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있다.
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
