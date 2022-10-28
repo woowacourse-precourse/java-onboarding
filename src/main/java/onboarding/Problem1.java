ff371b27a73582297b3ead6767f21b06636ec155
dydtmd6801 <dydtmd6801@naver.com>
2022-10-28 16:07:38 +0900

    feat : 페이지가 연속되지 않으면 예외처리 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        answer = pageNumberException(pobi, crong);
+        if(answer == -1){
+            return answer;
+        }
         return answer;
+    }
+
+    private static int pageNumberException(List<Integer> pobi, List<Integer> crong) {
+        if((pobi.get(0)+1 != pobi.get(1)) || (crong.get(0)+1 != crong.get(1))){
+            return -1;
+        }
+        return 0;
     }
 }
