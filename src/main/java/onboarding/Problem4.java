e0ae4201ef9c52e79837b8173b9b0f66605f15cf
sohyundoh <lyny123@naver.com>
2022-10-29 07:37:12 +0900

    Probelm 4 제한 사항 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+/*
+제한사항
+word는 길이가 1 이상 1,000 이하인 문자열이다.
+알파벳 외의 문자는 변환하지 않는다.
+알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
