71d3a9aa6f467e4c5d662b1ae2a71faf044183a1
alstjr706 <alstjr706@naver.com>
2022-10-28 17:13:53 +0900

    feat : (청개구리 문제) 사전 생성 구현

---
+++ src/main/java/onboarding/problem4/DictionaryGenerator.java
@@ -0,0 +1,23 @@
+package onboarding.problem4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DictionaryGenerator {
+
+    // createDictionary
+    public Map<Character, Character> createDictionary(String word) {
+
+        HashMap<Character, Character> characterMap = new HashMap<>();
+
+        int asciiLast = 90;
+
+        for(int ascii = 65; ascii <= 90; ascii++) {
+            characterMap.put((char) ascii, (char) asciiLast--);
+        }
+
+        return characterMap;
+    }
+
+
+}
