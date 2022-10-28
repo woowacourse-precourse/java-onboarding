5d38f458a9aa92d76190297fc1c86e92b9bd6811
ymkim97 <rladudaud97@naver.com>
2022-10-28 22:43:41 +0900

    feat: add function boundary exception

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        if (throwBoundaryException(number)) {
+            return -1;
+        }
+
         return answer;
+    }
+
+    public static boolean throwBoundaryException(int number) {
+
+        if (number < 1 || number > 10000) {
+            return true;
+        }
+
+        return false;
     }
 }
