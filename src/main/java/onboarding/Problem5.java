032e2e54d0e2093615898f6049769944b1a52edd
CHAYEONIL <jeay123@naver.com>
2022-10-28 18:26:10 +0900

    문제5 주요기능 큰 금액별로 나누고 넘기기 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] check_money = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        //큰 금액별로 나누고 나머지 넘기기
+        for(int i = 0; i < check_money.length; i++) {
+            if (i == 0) {
+                answer.add(money / check_money[i]);
+            } else {
+                answer.add((money % check_money[i - 1]) / check_money[i]);
+           }
+        }
         return answer;
     }
 }
