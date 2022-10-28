f53ab88fd98e422763515540485247c2ebfe52bb
joohyeongKim <wnguddl96@knou.ac.kr>
2022-10-28 16:01:21 +0900

    [refactoring] else 제거

---
+++ src/main/java/onboarding/problem1/ExceptionHandler.java
@@ -0,0 +1,19 @@
+package onboarding.problem1;
+
+import java.util.List;
+
+// else 제거
+public class ExceptionHandler {
+    public static boolean runtimeException(List<Integer> pageByPlayer) {
+        //given
+        int leftNumber = pageByPlayer.get(0);
+        int rightNumber = pageByPlayer.get(1);
+
+        //when
+        if (leftNumber < 1){return true;}
+        if (rightNumber >= 400){return true;}
+        if (rightNumber - leftNumber > 1) {return true;}
+
+        return false;
+    }
+}
