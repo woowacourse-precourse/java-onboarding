98882acd534c91796f9decf102fecf54a903573a
matt <matt1235@naver.com>
2022-10-28 12:47:45 +0900

    feat: Problem3 (기능 목록1 - 인자의 유효성 검사) 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * 입력값의 범위 지정, 오류 처리*/
+    private static void check(int number) {
+        if(number < 1 || number > 10000)
+            throw new IllegalArgumentException("숫자의 범위를 확인하세요");
+    }
+
 }
