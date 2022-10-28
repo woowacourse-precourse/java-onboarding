4a5900793977cbc83d2511b6fa52f3f64f20b25a
chesthyeon <niw927@naver.com>
2022-10-29 02:23:54 +0900

    문제 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = 0, temp,tempM; // 초기화
+        for (int i = 1; i < number + 1; i++){ // 1부터 입력 숫자까지
+            temp = i; // 차례대로 값을 넣어줌
+            while (temp > 0){ // 0이 될때 까지
+                tempM = temp % 10; // 1의 자리수
+                if (tempM == 3 || tempM == 6 || tempM == 9) answer += 1; // 3,6,9 라면 +1
+                temp /= 10; // 10으로 나눠줌
+            }
+        }
         return answer;
     }
 }
