18df5a847923e137fdebb8f4d084dbc65d674e3d
win9 <krrr8@naver.com>
2022-10-28 23:13:11 +0900

    [기능5번] valid메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static int[]moneyUnits = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        if (!isValid(money)){
+            return new ArrayList<>();   // 조건이 맞지 않으면 빈 리스트를 반환.
+        }
+
         return answer;
     }
+
+    public static boolean isValid(int money){
+        return 1 >= money && 1001000000 >= money;
+    }
+
 }
