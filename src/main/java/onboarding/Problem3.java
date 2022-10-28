816e8602773d9807c4f3d08e4403daaea0e8aed5
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 15:51:02 +0900

    [Problem3] 기능 목록 설계

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+
+    /**
+     * 기능 목록
+     * 1. int형 -> string형으로 변환
+     * 2. 해당 숫자 3, 6, 9의 개수 찾기
+     */
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
