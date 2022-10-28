5d9ffdbe454d36bf15e026930b304e179b65a94c
chanminmaxscaler <jcm04@naver.com>
2022-10-28 22:17:39 +0900

    feat(Problem3): play369Game, solution 메소드의 기능을 구현한다
    play369Game 메소드는 다음과 같이 기능한다.
    endNumber를 입력받으면 1부터 endNumber까지 반복하여 숫자가 포함하는 3, 6, 9의 개수를 센다.
    3, 6, 9의 개수를 반환한다.
    
    solution 메소드는 다음과 같이 기능한다.
    number를 입력받으면 play369Game에 전달하고 박수의 총 횟수를 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,10 +1,6 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
 
     public int countTargetCharacterInNumber(char targetCharacter, int number) {
         String stringOfNumber = Integer.toString(number);
@@ -12,5 +8,23 @@ public class Problem3 {
             return (int)stringOfNumber.chars().filter(x -> x == targetCharacter).count();
         }
         return 0;
+    }
+
+    public int play369Game(int endNumber) {
+        int count = 0;
+        for (int i = 1; i <= endNumber; i++) {
+            count += countTargetCharacterInNumber('3', i);
+            count += countTargetCharacterInNumber('6', i);
+            count += countTargetCharacterInNumber('9', i);
+        }
+        return count;
+    }
+
+    public static int solution(int number) {
+        int numberOfClaps = 0;
+        Problem3 problem3 = new Problem3();
+
+        numberOfClaps = problem3.play369Game(number);
+        return numberOfClaps;
     }
 }
