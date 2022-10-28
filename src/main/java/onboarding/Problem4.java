f38f4887a7dabd8c26a61cf19e8523d22a79a991
jihong <clara5091@naver.com>
2022-10-28 20:37:07 +0900

    problem4-제한사항 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+    public static void checkWord(String word){
+        if(word.length()>1000||word.length()<1)
+            throw new IllegalArgumentException("올바른 범위의 입력값이 아닙니다.");
+    }
     public static String solution(String word) {
+        checkWord(word);
         String answer = "";
         return answer;
     }
