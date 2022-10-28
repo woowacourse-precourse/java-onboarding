3a18f8a242dbaccdcba0feb1224724ce414dec2f
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-28 16:49:54 +0900

    221028 Feat: P1 - 기능3 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,11 +17,36 @@ class Problem1 {
         if(pRightPage - pLeftPage != 1 || cRightPage - cLeftPage != 1)
             return -1;
 
+        int pMax = Math.max(getSum(pRightPage), Math.max(getSum(pLeftPage),
+                Math.max(getMultiple(pRightPage), getMultiple(pLeftPage))));
 
+        int cMax = Math.max(getSum(cRightPage), Math.max(getSum(cLeftPage),
+                Math.max(getMultiple(cRightPage), getMultiple(cLeftPage))));
 
         //
         return answer;
     }
 
+    public static int getSum(int num) {
+        int sum = 0;
+
+        while(num / 10 > 0){
+            sum += (num % 10);
+            num = num / 10;
+        }
+
+        return sum;
+    }
+
+    public static int getMultiple(int num) {
+        int mul = 1;
+
+        while(num / 10 > 0){
+            mul *= (num % 10);
+            num = num / 10;
+        }
+
+        return mul;
+    }
 
 }
