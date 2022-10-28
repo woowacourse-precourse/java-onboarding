58ba06e621e970ee38b21d540a0d02b91136bf37
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-10-28 14:00:49 +0900

    refactor : [problem4] reverseAlphabet(), makeReverse() 매소드 작성으로 기능 분리 ascii 코드를 이용해 알파벳을 뒤집였다. 함수명을 조금 더 직관적이고 명확하게 바꿀 수 있을지 더 고민해보아야겠다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,24 +3,36 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        int ascii;
-        int reverse;
 
         for(char tempWord : word.toCharArray()){
-            if(Character.isUpperCase(tempWord)){ // 대문자 이면 ascii코드 시작은 65
-                ascii = 65;
-            } else if (Character.isLowerCase(tempWord)) { //소문자 이면 ascii코드 시작은 97
-                ascii = 97;
-            } else{ //대문자와 소문자 이외의 문자이면 그대로 (공백 또는 숫자 또는 특수 문자)
-                answer += tempWord;
-                continue;
-            }
-
-            // 알파벳 개수가 26개 이므로 a에서 25를 더해 z를 만든 후 현재 알파벳과 a의 차이만큼 빼서 반대로 변환한다.
-            reverse = (ascii + 25) - Math.abs(ascii - (int)tempWord);
-            answer += (char)reverse;
+            answer += reverseAlphabet(tempWord);
         }
 
         return answer;
+    }
+
+    // 알파벳을 뒤집기 위한 조건을 계산하고 뒤집어서 반환하는 함수
+    public static char reverseAlphabet(char alphabet){
+        int ascii;
+
+        if(Character.isUpperCase(alphabet)){ // 대문자 이면 ascii코드 시작은 65
+            ascii = 65;
+        } else if (Character.isLowerCase(alphabet)) { //소문자 이면 ascii코드 시작은 97
+            ascii = 97;
+        } else{ //대문자와 소문자 이외의 문자이면 그대로 반환 (공백 또는 숫자 또는 특수 문자)
+            return alphabet;
+        }
+
+        return makeReverse(ascii,(int) alphabet);
+    }
+
+    //알파벳을 뒤집는 행동을 하는 함수
+    public static char makeReverse(int ascii, int alphabet){
+        // 알파벳 개수가 26개 이므로 a에서 25를 더해 z를 만든 후 현재 알파벳과 a의 차이만큼 빼서 반대로 변환한다.
+        return (char) ((ascii + 25) - Math.abs(ascii - (int)alphabet));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("a b c1d"));
     }
 }
