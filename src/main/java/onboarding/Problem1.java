94dba58f3b39346205edf56f494bde0c0fc7635b
juspian <107744617+juspian@users.noreply.github.com>
2022-10-28 13:26:34 +0900

    main solution

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,19 @@
 package onboarding;
 
 import java.util.List;
+import onboarding.solution1.*;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        ScoreCalculator score = new ScoreCalculator();
+        IsException exception = new IsException();
+        CompareScore compare = new CompareScore();
+        if (exception.isException(pobi) == 0 && exception.isException(crong) == 0){
+            int resPobi = score.findScore(pobi);
+            int resCrong = score.findScore(crong);
+            int answer = compare.compareRes(resPobi, resCrong);
+            return answer;
+        }
+        return -1;
     }
 }
