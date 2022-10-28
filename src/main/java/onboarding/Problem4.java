e1b7849af29b7d7834d3eaf8386358661ca627a1
chj <bluedog129@gmail.com>
2022-10-28 15:34:26 +0900

    [P4] 인자로 받은 문자열 서치 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    private static char conversionUpperLower(char c) {
+        char result = 0;
+
+        return result;
+    }
+    private static char conversionAscii(char c) {
+        char result = 0;
+
+        return result;
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] wordArr = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            wordArr[i] = conversionAscii(word.charAt(i));
+            wordArr[i] = conversionUpperLower(word.charAt(i));
+        }
+        word = String.valueOf(wordArr);
+        return word;
     }
 }
