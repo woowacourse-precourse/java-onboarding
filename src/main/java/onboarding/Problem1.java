9d070f85ee585892c20937131c775b910f992b5d
yangjiyoung <0112jyoung@naver.com>
2022-10-28 10:37:27 +0900

    Problem 1 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,50 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static int pobiMax = 0;
+    static int crongMax = 0;
+
+    public static int getMax(int number) {
+        int arrMax[]={0,0};
+        for (int i = 0; i < 2; i++) {
+            String tmp = Integer.toString(number + 1);
+            String[] array = tmp.split("");
+
+            int plusSum = 0, mulSum = 1;
+
+            for (int j = 0; j < array.length; j++) {
+                plusSum += Integer.parseInt(array[j]);
+                mulSum *= Integer.parseInt(array[j]);
+            }
+            if(plusSum>mulSum)
+                arrMax[i]=plusSum;
+            else
+                arrMax[i]=mulSum;
+        }
+        if(arrMax[0]>arrMax[1])
+            return arrMax[0];
+        return arrMax[1];
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(Math.abs(pobi.get(0)-pobi.get(1))==1 && Math.abs(crong.get(0)-crong.get(1))==1){
+            pobiMax = getMax(pobi.get(0));
+            crongMax = getMax(crong.get(0));
+
+            if(pobiMax>crongMax)
+                answer=1;
+            else if(pobiMax<crongMax)
+                answer=2;
+            else if(pobiMax==crongMax)
+                answer=0;
+        }else
+            answer=-1;
+
+        //System.out.println("pobimax: "+pobiMax+", crongMax:"+crongMax+", answer: "+answer);
+
         return answer;
     }
 }
