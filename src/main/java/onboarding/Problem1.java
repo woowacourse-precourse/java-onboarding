6e542e7308ae3f1873b581c20c2d6866d90abb05
chesthyeon <niw927@naver.com>
2022-10-28 16:42:32 +0900

    문제1 , 문제 2 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,4 @@
 package onboarding;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
@@ -16,25 +12,25 @@ class Problem1 {
         pMax = Math.max(maxMulAdd(pobi.get(0)),maxMulAdd(pobi.get(1))); // 포비의 최댓값
         cMax = Math.max(maxMulAdd(crong.get(0)),maxMulAdd(crong.get(1))); // 크롱의 최댓값
 
-        if (cMax == pMax) answer = 0;
-        else answer = (pMax > cMax) ? 1 : 2;
+        if (cMax == pMax) answer = 0; // 값이 같다면 0
+        else answer = (pMax > cMax) ? 1 : 2; // 포비가 크면 1 크롱이 크면 2
 
-        return answer;
+        return answer; //
     }
 
-    static int maxMulAdd(int num){
-        int add = 0, mul = 1;
-        while (num > 0){
-            add += num % 10;
-            mul *= num % 10;
-            num /= 10;
+    static int maxMulAdd(int num){ // 모든 자리수를 더한 값과 곱합 값을 비교하는 함수
+        int add = 0, mul = 1; // 변수 초기화
+        while (num > 0){ // 넘이 0일 때까지
+            add += num % 10; // 1의 자리를 더 해줌
+            mul *= num % 10; // 1의 자리 곱 해줌
+            num /= 10; // 넘 값을 줄인다
         }
-        return Math.max(add, mul);
+        return Math.max(add, mul); // 더 큰 값
     }
     static public boolean exception(List<Integer> array){
-      if (array.size() != 2) return true; // 리스트 사이즈가 2인지
-      if (array.get(0) < 0 || array.get(1) > 401) return true; //페이지는 1~400
-      if (array.get(0) % 2 == 0 || array.get(1) % 2 == 1) return true; // 왼쪽은 홀수, 오른쪽은 짝수
+        if (array.size() != 2) return true; // 리스트 사이즈가 2인지
+        if (array.get(0) < 0 || array.get(1) > 400) return true; //페이지는 1~400
+        if (array.get(0) % 2 == 0 || array.get(1) % 2 == 1) return true; // 왼쪽은 홀수, 오른쪽은 짝수
         return array.get(1) - array.get(0) != 1; //오른쪽 페이지 - 왼쪽 페이지 = 1
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder(); //정답 출력
+        Stack<Character> stack = new Stack<>(); //스택 선언
+        stack.add(cryptogram.charAt(0));// 스택에 첫 문자를 담아 놓는다
+        for (int i = 1; i < cryptogram.length(); i++){ // 두번쨰 문자부터 시작한다.
+            if (stack.peek() == cryptogram.charAt(i)) stack.pop(); // 스택의 마지막 값과 값다면 제거
+            else stack.push(cryptogram.charAt(i)); // 아니라면 push()
+        }
+        while (!stack.isEmpty()){ // 스택이 텅 빌때까지
+            answer.insert(0, stack.pop()); // 스트링 빌더에 스택 값을 넣는다.
+        }
+        return answer.toString();
     }
 }
