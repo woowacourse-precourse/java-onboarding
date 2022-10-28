e95a6dc181dcd6ba07945836b850ac539f90c708
sinryuji <siler2474@gmail.com>
2022-10-28 14:50:13 +0900

    Feat: Problem1 add getMul method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,9 +1,9 @@
 package onboarding;
 /*
     기능 목록
-    1. 각 자리수를 더하는 기능
-    2. 각 자리수를 곱하는 기능
-    3. 두 페이지의 각 자릿수에 1, 2를 적용한 후 Max값을 구하는 기능
+    1. 각 자리수를 더하는 기능 -> getSum
+    2. 각 자리수를 곱하는 기능 -> getMul
+    3. 두 페이지의 각 자릿수에 1, 2를 적용한 후 Max값을 구하는 기능 -> getMax
  */
 import java.util.List;
 
@@ -16,9 +16,18 @@ class Problem1 {
     private static int getSum(int page) {
         int ret = 0;
 
-        while(page > 0)
-        {
+        while(page > 0) {
             ret += page % 10;
+            page /= 10;
+        }
+        return ret;
+    }
+
+    private static int getMul(int page) {
+        int ret = 1;
+
+        while(page > 0) {
+            ret *= page % 10;
             page /= 10;
         }
         return ret;
