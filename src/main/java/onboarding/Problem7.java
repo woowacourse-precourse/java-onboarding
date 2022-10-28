fa4281f61305492df54d03fba271189723fcc253
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-28 15:34:21 +0900

    feat: Problem7 - 유저의 친구들 목록을 만드는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Set<String> userFriends = new HashSet<>();
+        for (List<String> friend : friends) {
+            makeUserFriendList(userFriends ,friend, user);
+        }
+
         return answer;
+    }
+
+    public static void makeUserFriendList(Set<String> friendSet, List<String> friendship, String user) {
+        if (friendship.get(0).equals(user)) {
+            friendSet.add(friendship.get(1));
+        }
+        if (friendship.get(1).equals(user)) {
+            friendSet.add(friendship.get(0));
+        }
     }
 }
