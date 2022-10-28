1599f78382ff058f4252870869e351d53057d8f0
chesthyeon <niw927@naver.com>
2022-10-29 02:39:31 +0900

    문제 4 g해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,15 +1,16 @@
 package onboarding;
 
 public class Problem4 {
-    public static void main(String[] args) {
-        System.out.println(solution("ii"));
-    }
     public static String solution(String word) {
-        String answer = "";
-        for (int i = 0; i < word.length(); i++){
-
+        StringBuilder answer = new StringBuilder(); //문자열 저장을 위한 스트링 빌더
+        for (int i = 0; i < word.length(); i++){ // 각 문자열 확인
+           answer.append(reverseAlpa(word.charAt(i))); // 뒤집어서 추가
         }
-        return answer;
+        return answer.toString();
     }
-
+    public static char reverseAlpa(char alpha){ // 각 문자열을 뒤집어서 추가 하는 함수
+        if (alpha >= 'a' && alpha <= 'z') return (char) ('z' - alpha + 'a'); // 소문자라면 소문자로
+        if (alpha >= 'A' && alpha <= 'Z') return (char) ('Z' - alpha + 'A'); // 대문자라면 대문자로
+        return alpha;
+    }
 }
