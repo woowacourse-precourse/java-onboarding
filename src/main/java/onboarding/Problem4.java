f36f2cd21cd227002ea70a322c8f251b0cbaed6f
Kim woo jong <skekdnwhd@gmail.com>
2022-10-28 16:14:49 +0900

    기능 서술 및 제한사항 서술

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+/**
+ * 기능 : 1. 엄마 말씀 word가 매개변수로 주어지면 청개구리 사전을 참고하여 반대로 return
+ * 제한 사항 : 1. word의 길이가 1이상 1000이하이다.
+ *          2. 알파벳 이외의 문자는 변환하지 않는다.
+ *          3. 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
