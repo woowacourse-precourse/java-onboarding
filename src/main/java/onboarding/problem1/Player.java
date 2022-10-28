d72c41052d0f7b6ab28dc291d9e6820d1a006afe
apptie <appti@naver.com>
2022-10-28 18:19:57 +0900

    feat: 문제 1 플레이어가 선택한 페이지와 점수를 표현하는 Player 추가

---
+++ src/main/java/onboarding/problem1/Player.java
@@ -0,0 +1,51 @@
+package onboarding.problem1;
+
+import static onboarding.problem1.consts.CalculateGameScoreConst.ADD_SCORE_DEFAULT_VALUE;
+import static onboarding.problem1.consts.CalculateGameScoreConst.CALCULATE_REMAINDER_VALUE;
+import static onboarding.problem1.consts.CalculateGameScoreConst.DECIMAL_UNIT_VALUE;
+import static onboarding.problem1.consts.CalculateGameScoreConst.MULTIPLY_SCORE_DEFAULT_VALUE;
+
+public class Player {
+
+    private final int leftPageNumber;
+    private final int rightPageNumber;
+    private final int playerScore;
+
+    public Player(int leftPageNumber, int rightPageNumber) {
+        this.leftPageNumber = leftPageNumber;
+        this.rightPageNumber = rightPageNumber;
+        playerScore = initPlayerScore();
+    }
+
+    private int initPlayerScore() {
+        return Math.max(calculatePageScore(leftPageNumber), calculatePageScore(rightPageNumber));
+    }
+
+    private int calculatePageScore(int pageNumber) {
+        return Math.max(calculateAddPageScore(pageNumber), calculateMultiplyPageScore(pageNumber));
+    }
+
+    private int calculateAddPageScore(int pageNumber) {
+        int addScore = ADD_SCORE_DEFAULT_VALUE;
+
+        while (pageNumber != CALCULATE_REMAINDER_VALUE) {
+            addScore += pageNumber % DECIMAL_UNIT_VALUE;
+            pageNumber /= DECIMAL_UNIT_VALUE;
+        }
+        return addScore;
+    }
+
+    private int calculateMultiplyPageScore(int pageNumber) {
+        int multiplyScore = MULTIPLY_SCORE_DEFAULT_VALUE;
+
+        while (pageNumber != CALCULATE_REMAINDER_VALUE) {
+            multiplyScore *= pageNumber % DECIMAL_UNIT_VALUE;
+            pageNumber /= DECIMAL_UNIT_VALUE;
+        }
+        return multiplyScore;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+}
