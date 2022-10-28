069e66c28edc6058d0e7434f76c683016029c9f4
sdcodebase <sdcodebase@gmail.com>
2022-10-28 20:49:07 +0900

    feat: Problem2 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<String> stack = new Stack<>();
+
+        String[] alphabets = cryptogram.split("");
+        String sameAlphabet = "";
+        for (String alphabet : alphabets) {
+            if (stack.isEmpty()) {
+                stack.push(alphabet);
+                sameAlphabet = "";
+                continue;
+            }
+
+            if (sameAlphabet.equals(alphabet)) {
+                continue;
+            }
+
+            String peek = stack.peek();
+            if (alphabet.equals(peek)) {
+                stack.pop();
+                sameAlphabet = peek;
+                continue;
+            }
+
+            stack.add(alphabet);
+        }
+
+        return String.join("", stack);
     }
 }
