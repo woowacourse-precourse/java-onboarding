ee84d3fa0a8457ccad7b305692d5e2944adc227e
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-10-28 13:00:10 +0900

    misson3-1 problem solving
    misson3-1
    problem solving

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,31 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        answer = count(number);
+
+
         return answer;
     }
+
+    public static int count(int number)
+    {
+        int counts=0;
+        int j=0;
+        for(int i =1;i<=number;i++)
+        {
+            j=i;
+            while(j>0)
+            {
+                if(j%10== 3 || j%10== 6 || j%10== 9 )
+                    counts++;
+
+                j/=10;
+
+            }
+        }
+
+        return counts;
+    }
+
 }
