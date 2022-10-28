f6e583375728de3d763af004451919ddf9083fdd
Dasol Jung <greteljds0251@gmail.com>
2022-10-28 23:15:57 +0900

    feat: 2-2. Repeat until no duplicate characters exist

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,20 +3,39 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int count = 1;
+        while (count != 0) {
+            cryptogram = chackSameChar(cryptogram);
+            count = countSameChar(cryptogram);
+        }
+        answer = cryptogram;
 
         return answer;
     }
 
     // 중복 문자 확인, 제거
-    public static int chackSameChar(String cryptogram) {
-        char c = NULL;
+    public static String chackSameChar(String cryptogram) {
+        char c = '\0';
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (c == cryptogram.charAt(i)) {
+                c = cryptogram.charAt(i);
+                cryptogram = cryptogram.substring(0, i - 1) + cryptogram.substring(i + 1);
+            } else {
+                c = cryptogram.charAt(i);
+            }
+        }
+        return cryptogram;
+    }
+
+    public static int countSameChar(String cryptogram) {
+        char c = '\0';
         int count = 0;
         for (int i = 0; i < cryptogram.length(); i++) {
             if (c == cryptogram.charAt(i)) {
-                cryptogram = cryptogram.substring(0, i-1) + cryptogram.substring(i);
                 count++;
+            } else {
+                c = cryptogram.charAt(i);
             }
-            c = cryptogram.charAt(i);
         }
         return count;
     }
@@ -24,8 +43,6 @@ public class Problem2 {
 
 /*
 기능
-1. 중복 문자 저장
-2. 중복 문자 제거
-3. 반복
-4. 중복 문자 없을 시 스탑
+1. 문자 저장 및 연속한 중복 문자 제거
+2. 연속하여 중복하는 문자 없을 때까지 반복
  */
