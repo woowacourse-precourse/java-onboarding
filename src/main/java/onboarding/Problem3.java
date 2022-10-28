578139121c62c40e657ed5eb628a34ee11beb600
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-10-28 21:08:19 +0900

    Problem3 기능 목록 2 : 순회하는 해당 숫자에 3,6,9가 들어가는지 판별한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,15 @@ public class Problem3 {
 
         for(int i = 1 ; i < number+1 ; i++) {
 
-            
+            int nowNumber = i;
+
+            while (nowNumber != 0) {
+                if (nowNumber % 10 == 3 || nowNumber % 10 == 6 || nowNumber % 10 == 9) {
+
+                }
+                nowNumber /= 10;
+            }
+
         }
         return answer;
     }
