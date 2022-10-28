362b726cd74372f5b4dae88b06e367865f6cb922
syeonbot <asdf748596123@gmail.com>
2022-10-29 05:18:22 +0900

    feat: problem4 알파벳이 키, 반대 알파벳이 값인 Map 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,17 +1,34 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static final int MINIMUM_STRING_LENGTH = 1;
     public static final int MAXIMUM_STRING_LENGTH = 1000;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
         validateRange(word);
+        Map<Character, Character> dictionary = generateDictionary();
     }
 
     public static void validateRange(String word) {
         if (word.length() < MINIMUM_STRING_LENGTH || MAXIMUM_STRING_LENGTH < word.length()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static Map<Character, Character> generateDictionary() {
+        Map<Character, Character> dictionary = new HashMap<>();
+        int maxIndex = ALPHABET.length() - 1;
+        String alphabetUpperCase = ALPHABET.toUpperCase();
+
+        for (int i = 0; i < ALPHABET.length(); i++) {
+            dictionary.put(ALPHABET.charAt(i), ALPHABET.charAt(maxIndex - i));
+            dictionary.put(alphabetUpperCase.charAt(i), alphabetUpperCase.charAt(maxIndex - i));
+        }
+
+        return dictionary;
     }
 }
