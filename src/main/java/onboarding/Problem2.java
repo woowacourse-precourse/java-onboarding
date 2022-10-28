b1f4b070af7bc774ad3cc673aa2ac9dfddd574a9
KSH <tkdgns543@gmail.com>
2022-10-28 21:57:26 +0900

    feat(Problem2): 3개 이상 연속된 같은 문자열을 2개까지 줄이는 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,15 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    /* 3 이상 연속된 target 문자열을 2개까지 줄이는 메서드 */
+    private static String compressToSizeTwo(String cryptogram, String target) {
+        String result=cryptogram;
+        String tmp=target+target+target;
+        while (result.contains(tmp)) {
+            result=result.replace(tmp,target+target);
+        }
+
+        return result;
+    }
 }
