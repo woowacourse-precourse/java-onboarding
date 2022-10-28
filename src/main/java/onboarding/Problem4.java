d199554877a7d059e702173395fe10a0d4b660c1
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 14:27:46 +0900

    feat: PROBLEM4 알파벳 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,11 +2,38 @@ package onboarding;
 
 public class Problem4 {
 	public static String solution(String word) {
-		String answer = "";
-		return answer;
+		Frog frog = new Frog();
+		// return frog.translate(word);
+		return null;
 	}
 
 	static class Frog {
+
+		private final char[] reversedLowerAlphabetArray;
+		private final char[] reversedHigherAlphabetArray;
+
+		Frog() {
+			char[] reversedLowerAlphabetArray = new char[26];
+			char[] reversedHigherAlphabetArray = new char[26];
+
+			for (int i = 0; i < 26; i++) {
+				reversedLowerAlphabetArray[i] = (char)(122 - i);
+				reversedHigherAlphabetArray[i] = (char)(90 - i);
+			}
+			this.reversedLowerAlphabetArray = reversedLowerAlphabetArray;
+			this.reversedHigherAlphabetArray = reversedHigherAlphabetArray;
+		}
+
+		private char reverse(char c) {
+			if (isLowerAlphabet(c)) {
+				int index = c - 97;
+				return reversedLowerAlphabetArray[index];
+			} else if (isHigherAlphabet(c)) {
+				int index = c - 65;
+				return reversedHigherAlphabetArray[index];
+			}
+			return c;
+		}
 
 		private boolean isLowerAlphabet(char c) {
 			return Character.isAlphabetic(c) && Character.isLowerCase(c);
