6b1d5c35efd112d9aefe36674dde13076266765e
leewoojin <zmfpdl64@naver.com>
2022-10-28 14:23:54 +0900

    문제4번 이우진 제출합니다.
    문제: 개구리 사전은 거꾸로 표기되어 있는데 이 내용을 번역하세요
    
    해결방법:
    SOL1: 개구리가 정의한 사전을 등록해서 문자열이 들어오면 문자 하나씩 번역하여 반환한다.
    
    1. 개구리 사전을 아스키코드를 이용해 소문자 대문자 사전으로 만든다.
    2. 입력받은 문자를 한문자씩 개구리 사전에 입력하여 결과를 반환한다. 단 문자가 사전에 없을 때는 문자 그대로 반환한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+//Sol1: 해시맵을 이용한 사전 방식
+//주의 사항 소문자는 소문자로 대문자는 대문자로
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        Map<Character, Character> map = new HashMap<>();
+        for(int i = 0; i <= 26; i++){
+            map.put( (char)('a'+i), (char)('z'-i)); //개구리 사전을 만든다. 단 소문자와 대문자 둘다 만들어준다.
+            map.put((char)('A'+i), (char)('Z'-i));
+        }
+        for(int i = 0; i < word.length(); i++){ //문자를 받아 사전에 입력하면 사전에 정의한 값이 출력되며 없을 때는 문자 그대로 반환한다.
+            Character c = word.charAt(i);
+            answer += map.getOrDefault(c, c);
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("I love you"));
+
     }
 }
