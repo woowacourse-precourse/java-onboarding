8398da4f557407b602f7ac9bb2f02595eef81a70
dclxxi <dclxxi@kakao.com>
2022-10-28 20:57:51 +0900

    feat(문제4): 알파벳의 소문자/대문자 여부를 확인하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static final char FIRST_LOWER = 'a';
+    public static final char LAST_LOWER = 'z';
+    public static final char FIRST_UPPER = 'A';
+    public static final char LAST_UPPER = 'Z';
+
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+    public static boolean isLower(char alphabet) {
+        return alphabet >= FIRST_LOWER && alphabet <= LAST_LOWER;
+    }
+
+    public static boolean isUpper(char alphabet) {
+        return alphabet >= FIRST_UPPER && alphabet <= LAST_UPPER;
     }
 }
