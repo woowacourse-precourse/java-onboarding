fbfe4deb52e09a887b56b7635844456a0f46c653
hyunsb <jhss0113@naver.com>
2022-10-28 15:24:06 +0900

    ploblem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] code = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P',
+                'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+
+        for(int i=0; i < word.length(); i++){
+            char tmp = word.charAt(i);
+
+            // 대문자인 경우
+            if( (65 <= tmp) && (tmp <= 90)){
+                answer += code[tmp-65];
+            } // 소문자인 경우
+            else if ((97 <= tmp) && (tmp <= 122)) {
+                answer += Character.toString(code[tmp-97]).toLowerCase();
+            } // 그 외의 경우
+            else {
+                answer += tmp;
+            }
+        }
+
         return answer;
     }
 }
