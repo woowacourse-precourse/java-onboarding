ac71a12f693f98a83f6e176062c9a2afe8cc61a3
sosow0212 <sosow0212@naver.com>
2022-10-28 17:41:52 +0900

    feat: Problem7 - 점수 관련 상수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class Problem7 {
     private static final Map<String, Integer> userAndScoreMap = new HashMap<>();
     private static final Set<String> myFriendsSet = new HashSet<>();
+    private static final int ACQUAINTANCE_POINT = 10;
+    private static final int VISITOR_POINT = 1;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
