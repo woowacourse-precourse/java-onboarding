4ba28ceb42fd71bb0d1413add56c848542d7c48f
yumyeonghan <mgtmh991013@gmail.com>
2022-10-29 03:45:07 +0900

    Problem4 Solving

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,53 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem4 {
+
+    public static String changeWord(String word) {
+
+        String alpabat = "";
+
+        for (int i = 0; i < 26; i++) {
+            alpabat += (char) ('a' + i);
+        }
+
+        String alpabatC = "";
+
+        for (int i = 0; i < 26; i++) {
+            alpabatC += (char) ('A' + i);
+        }
+
+        String alpabatR = "";
+
+        for (int i = 0; i < 26; i++) {
+            alpabatR += (char) ('z' - i);
+        }
+
+        String alpabatCR = "";
+
+        for (int i = 0; i < 26; i++) {
+            alpabatCR += (char) ('Z' - i);
+        }
+
+        if (alpabat.contains(word)) {
+            return String.valueOf(alpabatR.charAt(alpabat.indexOf(word)));
+        }
+
+        if (alpabatC.contains(word)) {
+            return String.valueOf(alpabatCR.charAt(alpabatC.indexOf(word)));
+        }
+
+        return " ";
+    }
+
     public static String solution(String word) {
         String answer = "";
+        List<String> splitWord = List.of(word.split(""));
+        answer = splitWord.stream().map(x -> changeWord(x)).collect(Collectors.joining());
+
         return answer;
     }
+
 }
