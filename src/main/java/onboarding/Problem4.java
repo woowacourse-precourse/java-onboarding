40384cfb83bc5d611bb73576e1651401f2cea06f
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-10-28 16:27:20 +0900

    feat: problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.Locale;
+
 public class Problem4 {
+
+    private static char getKey(char c) {
+        String keys = "zyxwvutsrqponmlkjihgfedcba";
+
+        if (Character.isUpperCase(c)) {
+            return (keys.toUpperCase().charAt(c - 'A'));
+        }
+        return (keys.charAt(c - 'a'));
+    }
+
+    private static String encrypt(String word) {
+        String result = "";
+
+        for (char w : word.toCharArray()) {
+            if (Character.isAlphabetic(w)) {
+                result += getKey(w);
+            }
+            else {
+                result += w;
+            }
+        }
+        return (result);
+    }
+
     public static String solution(String word) {
-        String answer = "";
+        String answer = encrypt(word);
         return answer;
     }
 }
