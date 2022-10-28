4ea171cd95e6266fb31d70a98e14b5a1a49a63c5
23Yong <1239824@naver.com>
2022-10-28 15:43:30 +0900

    [feat] 중복된 닉네임의 이메일 목록 얻기
    - isDuplicated 함수는 파라미터로 주어진 닉네임이 중복되었는지 확인 할 것으로 기대
    - 중복 처리를 위해 Set 을 사용

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,51 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
+    private static int totalCrewNumber;
+    private static List<String> nicknames;
+    private static List<String> emails;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        initVariables(forms);
+
+        return null;
+    }
+
+    private static Set<String> findDuplicatedCrewEmails() {
+        Set<String> ret = new HashSet<>();
+        for (int oriCrewNum = 0; oriCrewNum < totalCrewNumber; oriCrewNum++) {
+            String ori = nicknames.get(oriCrewNum);
+
+            for (int cmpCrewNum = oriCrewNum + 1; cmpCrewNum < totalCrewNumber; cmpCrewNum++) {
+                if (isDuplicated(ori, nicknames.get(cmpCrewNum))) {
+                    ret.add(emails.get(oriCrewNum));
+                    ret.add(emails.get(cmpCrewNum));
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    private static boolean isDuplicated(String ori, String cmp) {
+        return true;
+    }
+
+    private static void initVariables(List<List<String>> forms) {
+        totalCrewNumber = forms.size();
+        nicknames = new ArrayList<>();
+        emails = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            emails.add(email);
+            nicknames.add(nickname);
+        }
     }
 }
