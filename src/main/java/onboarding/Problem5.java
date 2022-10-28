3581314784864f985835c8ba14b26738d361f251
yjlee0235 <yjlee0235@naver.com>
2022-10-28 16:40:48 +0900

    feat: 입력한 금액에 대한 큰 금액 위주로 거스름돈을 반환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    static final int[] MONETARY_UNITS = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getChange(money);
+    }
+
+    private static List<Integer> getChange(int money) {
+        List<Integer> result = new ArrayList<>();
+        int targetMoney = money;
+        for (int i = 0; i < MONETARY_UNITS.length; i++) {
+            int unitNum = 0;
+            if (targetMoney >= MONETARY_UNITS[i]) {
+                unitNum = targetMoney / MONETARY_UNITS[i];
+                targetMoney -= unitNum * MONETARY_UNITS[i];
+            }
+            result.add(unitNum);
+        }
+        return result;
     }
 }
