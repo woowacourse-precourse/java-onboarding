8ffb3db1809fdeff40377f78d770eedfd27dbdfe
gnoesnooj <306yyy@naver.com>
2022-10-28 19:20:00 +0800

    test : PROBLEM3 테스트코드 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -118,6 +118,13 @@ class ApplicationTest {
             int result = 14;
             assertThat(Problem3.solution(number)).isEqualTo(result);
         }
+
+        @Test
+        void case3() {
+            int number = 330000000;
+            int result = 0;
+            assertThat(Problem3.solution(number)).isEqualTo(result);
+        }
     }
 
     @Nested
