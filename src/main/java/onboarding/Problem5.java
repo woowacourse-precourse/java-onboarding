e005dbfdb65e21fd3b61f8f4848916c134c867a8
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 09:50:13 +0900

    feat : problem 5 solve

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,21 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
 
-        List<Integer> answer = Collections.emptyList();
+        List answer = new ArrayList();
         int moneyCount = 0;
         int wonUnit[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        for (int i = 0; i < 9; i++){
+        for (int i = 0; i < 9; i++) {
             moneyCount = money / wonUnit[i];
             money = money % wonUnit[i];
-//            answer.add(moneyCount);
-            System.out.printf("%d %d %d \n", i, moneyCount, money);
+            answer.add(moneyCount);
         }
+
         return answer;
     }
 }
