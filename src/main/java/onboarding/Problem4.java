14298f0f856079f91e920ff3c46eb63cc59a7f66
metorg <metrog950@gmail.com>
2022-10-28 13:50:53 +0900

    feat: [Problem4] 배열 순회 메서드 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,11 +10,30 @@ public class Problem4 {
     public static String solution(String word) {
 
         String answer = "";
+        letterRound(word);
         return answer;
     }
+
     static int letterCheck(char a){
         if('a'<=a&&a<='z')return lowerCase;
         else if('A'<=a&&a<='Z')return upperCase;
         else return etc;
+    }
+
+    static String letterRound(String word){
+        sb=new StringBuilder();
+        
+        for(int i=0;i<word.length();i++){
+            if(letterCheck(word.charAt(i))==lowerCase){
+
+            }
+            else if(letterCheck(word.charAt(i))==upperCase){
+
+            }
+            else{
+                sb.append(word.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
