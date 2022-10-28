50a1cd2e43998052c217225f687fe523e849aed0
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-29 03:13:49 +0900

    refactor(problem3): change to inner class

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,28 @@
 package onboarding;
 
 public class Problem3 {
+
+	private static int clap;
+
 	public static int solution(int number) {
-		ThreeSixNine threeSixNine = new ThreeSixNine(number);
-		int answer = threeSixNine.run();
-		return answer;
+		clap = 0;
+		for (int i = 1; i < number + 1; i++) {
+			countClap(i);
+		}
+		return clap;
+	}
+
+	private static void countClap(int number) {
+		while (number > 0) {
+			int digit = number % 10;
+			if (is369(digit)) {
+				clap += 1;
+			}
+			number /= 10;
+		}
+	}
+
+	private static boolean is369(int number) {
+		return number == 3 || number == 6 || number == 9;
 	}
 }
