67d091aa53f4110fce5ddb9de24c3874da90c865
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-28 22:26:53 +0900

    feat(Problem5): 큰 금액의 화폐부터 반환 개수 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,7 +8,13 @@ public class Problem5 {
     private static final int[] CURRENCIES = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
+
+        for (int currency : CURRENCIES) {
+            answer.add(money / currency);
+            money %= currency;
+        }
+
         return answer;
     }
 }
