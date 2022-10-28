c87b898d72fd588a5fb8823af1ef0e23250aec5e
seoeunbae <enyoupark@gmail.com>
2022-10-28 20:59:46 +0900

    [3] < feature > 제한 사항 처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,8 @@ public class Problem3 {
     public static int solution(int number) {
         int clap = 0;
 
+        validateCase(number);
+
         for(int i=1 ; i <= number ; i++){
             String stringNumber = Integer.valueOf(i).toString();
             System.out.println(stringNumber);
@@ -21,5 +23,9 @@ public class Problem3 {
             }
         }
         return clap;
+    }
+
+    public static void validateCase(int number){
+        if(number < 1 || number > 10000) throw new RuntimeException("숫자의 범위가 적합하지 않습니다.");
     }
 }
