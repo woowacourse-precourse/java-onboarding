3b502f6b7ea90b303aab6424e38a326e2fc24002
JJongmen <pjy90123@gmail.com>
2022-10-28 20:25:41 +0900

    feat(Problem5): 매 순간 받을 수 있는 최대 금액을 받고 배열에 저장한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int[] moneys = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] wallet = new int[9];
+        for (int i = 0; i < 9; i++) {
+            if (money < moneys[i]) continue;
+            wallet[i] += money / moneys[i];
+            money %= moneys[i];
+        }
         return answer;
     }
 }
