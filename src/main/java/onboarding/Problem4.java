a8c1b42c2665284afe78f2df4da007ac6caf7399
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-10-29 04:02:27 +0900

    feat: problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] letterList = new char[]{
+            'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R',
+            'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I',
+            'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'
+        };
+
+        for(int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+
+            if(c>='A' && c<='Z'){
+                answer+=letterList[c-'A'];
+            }
+            else if (c>='a' && c<='z'){
+                answer+=(char)((letterList[c-'a'])+32);
+            }
+            else answer+=c;
+        }
+
         return answer;
     }
 }
