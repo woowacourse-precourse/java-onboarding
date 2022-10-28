d4d2e5571574cb68426367febad560c371e9cdfa
wonseokjee <wsji9404@gmail.com>
2022-10-28 13:42:22 +0900

    221028 prblm3-1 1부터 더한 다음 숫자배열 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,7 +10,20 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
-//  1부터 number만큼 문자로 더한 문자열 생성후 숫자배열로 변환하는 메서드
 
-//  3,6,9 개수 세는 메서드
+    //  1부터 number만큼 문자로 더한 문자열 생성후 한 단위의 숫자배열로 변환하는 메서드
+    public static ArrayList<Integer> intToLst(int number) {
+        String lstToStr = "";
+        for (int i = 0; i < number; i++) {
+            lstToStr += Integer.toString(i);
+        }
+        ArrayList<Integer> strToNumlist = new ArrayList<>();
+        for (String sepNum : lstToStr.split("")) {
+            strToNumlist.add(Integer.parseInt(sepNum));
+        }
+        return strToNumlist;
+    }
+
+    //  3,6,9 개수 세는 메서드
+
 }
