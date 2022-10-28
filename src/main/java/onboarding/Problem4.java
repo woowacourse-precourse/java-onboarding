8f91feea5cfc3d0613234cd01df37ee3c69e87ec
KSH <tkdgns543@gmail.com>
2022-10-29 00:38:14 +0900

    style(Problem4): 두 대소문자 치환 메서드의 +와 - 앞뒤로 공백 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -13,12 +13,12 @@ public class Problem4 {
 
     /* 주어진 소문자를 기능 구현 조건대로 치환하는 메서드 */
     private static char changeLowerCase(char c) {
-        return (char)('z'+'a'-c);
+        return (char)('z' + 'a' - c);
     }
 
     /* 주어진 대문자를 기능 구현 조건대로 치환하는 메서드 */
     private static char changeUpperCase(char c) {
-        return (char)('Z'+'A'-c);
+        return (char)('Z' + 'A' - c);
     }
 
     /* 대문자인지, 소문자인지, 모두 아닌지 구분해서 결과 리턴하는 메서드 */
