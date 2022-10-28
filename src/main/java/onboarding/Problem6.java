1de00a67df39b33e706de9fc8e85e2878af900c1
leesky <leehaneul0623@gmail.com>
2022-10-28 23:20:44 +0900

    feat: form 유효성 검사 기능 구현(문제 6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -22,7 +22,13 @@ public class Problem6 {
      * @return 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 {@code List}
      *
      */
+
+    private static final String[] emailDomains = {"email.com"};
+    private static List<List<String>> badForms = new ArrayList<>();
+
     public static List<String> solution(List<List<String>> forms) {
+        if(!validation("crew_limit", forms.size())) return new ArrayList<>();
+
         List<String> answer = new ArrayList<>(findOverlapUsers(getHash(forms)));
         answer.sort(Comparator.naturalOrder());
         return answer;
@@ -36,12 +42,16 @@ public class Problem6 {
         return set;
     }
 
-
     private static HashMap<String, ? extends List<String>> getHash(List<List<String>> forms) {
         HashMap<String, ArrayList<String>> map = new HashMap<>();
         for(List<String> form : forms) {
             String nickName = form.get(1);
             String email = form.get(0);
+
+            if(!(validation("email", email)) || !(validation("nick_name", nickName))) {
+                badForms.add(form);
+                continue;
+            }
 
             if(nickName.length() < 2) {
                 ArrayList<String> emails = map.getOrDefault(nickName, new ArrayList<>());
@@ -63,5 +73,37 @@ public class Problem6 {
             }
         }
         return map;
+    }
+
+    private static boolean validation(String type, Object target){
+        switch (type) {
+            case "crew_limit":
+                int crewNumber = ((Number)target).intValue();
+                if(!(0 < crewNumber && crewNumber < 10001)) return false;
+                break;
+
+            case "nick_name":
+                int nameLength = ((String)target).length();
+                if(!(0 < nameLength && nameLength < 20)) return false;
+                if(!((String)target).matches("^[가-힣]*$")) return false;
+                break;
+
+            case "email":
+                int mailLength = ((String)target).length();
+                if(!(10 < mailLength && mailLength < 20)) return false;
+                if(!domainCheck((String)target)) return false;
+                break;
+
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean domainCheck(String email) {
+        for (String domain : emailDomains) {
+            if(!(email.matches("[a-z0-9]+@" + domain))) return false;
+        }
+        return true;
     }
 }
