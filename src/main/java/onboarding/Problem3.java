e861c7b611edd9826d884b034419aac635dce515
5uhwann <5uhwann27@gmail.com>
2022-10-28 15:15:52 +0900

    feat: PROBLEM3 - 기능1 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,17 +1,35 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
 
   private static final int MAX_NUMBER = 10000;
 
   public static int solution(int number) {
-    int answer = 0;
+    int answer = clapCount(number);
     return answer;
+  }
+
+  public static int clapCount(int number) {
+    int count = 0;
+    for (int i = 1; i <= number; i++) {
+      count += eachNumClapCount(i);
+    }
+    return count;
+  }
+
+  private static int eachNumClapCount(int number) {
+    String[] splitNumber = String.valueOf(number).split("");
+    long count = Arrays.stream(splitNumber)
+        .filter(s -> s.equals("3") || s.equals("6") || s.equals("9"))
+        .count();
+    return Long.valueOf(count).intValue();
   }
 
   public static boolean isAllowedNumber(int number) throws IllegalStateException {
     if (number < 1 || number > MAX_NUMBER) {
-      throw  new IllegalStateException("1 ~ 10000 사이의 숫자가 아닙니다.");
+      throw new IllegalStateException("1 ~ 10000 사이의 숫자가 아닙니다.");
     }
     return true;
   }
