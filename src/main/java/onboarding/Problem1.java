bdf1e964ddffb1322886f82584dd0a038505feb6
son <shhmun02@naver.com>
2022-10-28 16:34:25 +0900

    problem1_requirements1_complete

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,33 @@
 package onboarding;
 
 import java.util.List;
-
+//1.책을 임의로 펼쳤을 때 옳은 값이 들어왔는지 확인한다.
+//2.왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+//3.오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+//4.2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+//5.점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+//6.시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+//7.포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return 하도록 solution 메서드를 완성한다.
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        //1.책을 임의로 펼쳤을 때 옳은 값이 들어왔는지 확인한다.
+        //왼쪽 페이지와 오른쪽 페이지의 숫자가 1차이 나는지 확인
+        if(pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1)
+            return -1;
+        //책의 처음이나 마지막 부분을 펼치지 않았는지 확인
+        if(pobi.get(0) == 1 || pobi.get(1) == 400 || crong.get(0) == 1 || crong.get(1) == 400)
+            return -1;
+        //책을 펼쳤을 때 왼쪽은 홀수 오른쪽은 짝수 인지 확인
+        for(int i = 0; i < 2; i++)
+        {
+            if(i == 0)
+                if(pobi.get(i) % 2 == 0 || crong.get(i) % 2 == 0)
+                    return -1;
+            else if(i == 1)
+                if(pobi.get(i) % 2 != 0 || crong.get(i) % 2 != 0)
+                    return -1;
+        }
         return answer;
     }
 }
