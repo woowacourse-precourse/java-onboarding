e6222b64da6cfc4ffdf278ff477608989db27309
KNH <nh2004@naver.com>
2022-10-29 01:31:34 +0900

    add : problem2 solution
    문제2 풀이

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+
 public class Problem2 {
+    public static String removeDuplicate(String word){
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        while (i < word.length()-1) {
+            if (word.charAt(i) == word.charAt(i + 1))
+                i+=2;
+            else res.append(word.charAt(i++));
+        }
+        if (res.length() > 0) {
+            if (word.charAt(word.length() - 1) != res.charAt(res.length() - 1))
+                res.append(word.charAt(word.length() - 1));
+            else res.replace(res.length() - 1, res.length(), "");
+        }
+        return res.toString();
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (! cryptogram.equals(removeDuplicate(cryptogram))) {
+            cryptogram = removeDuplicate(cryptogram);
+        }
+        return cryptogram;
     }
 }
