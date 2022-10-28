9ed6135e7c2e0c80188ec33fece2b3f218215845
Limjongho <whdgh9873@icloud.com>
2022-10-28 22:15:49 +0900

    feat(Problem5): 문제 5번 solution method 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,8 +7,18 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> unitsList = new ArrayList<>();
+        List<Integer> moneyUnitList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        int remainder = money;
+
+        for (Integer moneyUnit : moneyUnitList) {
+            int units = getUnits(remainder, moneyUnit);
+            remainder -= withdraw(units, moneyUnit);
+
+            unitsList.add(units);
+        }
+
+        return unitsList;
     }
 
     private static int getUnits(int remainder, int moneyUnit) {
@@ -19,5 +29,4 @@ public class Problem5 {
         int withdrawnMoney = units * moneyUnit;
         return withdrawnMoney;
     }
-
 }
