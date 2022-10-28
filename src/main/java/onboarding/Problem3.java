28348cef588645e688e7149da7730a8081b6ca99
bradbrad97 <taehyun0121@naver.com>
2022-10-28 18:53:45 +0900

    problem3: 손뼉 쳐야 하는 횟수 세는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem3 {
+    public static int get369Count(int number){
+        int mod;
+        int count = 0;
+
+        while(number != 0){
+            mod = number % 10;
+            if (mod == 3 || mod == 6 || mod == 9){
+                ++count;
+            }
+            number = number / 10;
+        }
+
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int n = 1; n<=number; n++){
+            answer += get369Count(n);
+        }
+
         return answer;
     }
 }
