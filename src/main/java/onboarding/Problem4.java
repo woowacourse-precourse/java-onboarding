4e6113e63457ffd0a12b3d142c77e931388cf195
sinryuji <siler2474@gmail.com>
2022-10-28 17:20:01 +0900

    Feat: Problem4 isAlpha method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
-
+/*
+    기능 목록
+    1. 대응하는 알파벳을 구하는 기능
+    2. 알파벳인지 확인하는 기능
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static boolean isAlpha(char ch) {
+        return ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122));
     }
 }
