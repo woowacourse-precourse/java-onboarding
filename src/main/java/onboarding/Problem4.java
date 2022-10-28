de398b093cd8fa7b3e6a47822c3a7dec8a4d1434
Hojinida <opqrs1103@gmail.com>
2022-10-29 04:08:02 +0900

    feat: 문제 4번 대문자, 소문자 별로 변환하는 값을 저장해 놓는 메서드 setConvert 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    private static final HashMap<Character, Character> upperConvert=new HashMap<>();
+    private static final HashMap<Character, Character> lowerConvert=new HashMap<>();
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        setConvert();
+        return null;
+    }
+
+    public static void setConvert(){
+        for(int i=0;i<=25;i++){
+            upperConvert.put((char)(i+65),(char)(90-i));
+            lowerConvert.put((char)(i+97),(char)(122-i));
+        }
     }
 }
