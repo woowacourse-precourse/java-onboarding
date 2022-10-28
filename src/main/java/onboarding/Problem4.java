0874ddf52ad4a28551b82ec02003d712bb99a212
chj <bluedog129@gmail.com>
2022-10-28 16:42:26 +0900

    [P4] 인자로 받은 문자 아스키코드상 미러로 변환 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,24 +1,27 @@
 package onboarding;
 
 public class Problem4 {
-    private static char conversionUpperLower(char c) {
+    private static char convertUpperLower(char c) {
         if (Character.isUpperCase(c)) {
             c = Character.toLowerCase(c);
-        }else if (Character.isLowerCase(c)) {
+        } else if (Character.isLowerCase(c)) {
             c = Character.toUpperCase(c);
         }
         return c;
     }
-    private static char conversionAscii(char c) {
-        char result = 0;
-
-        return result;
+    private static char convertAscii(char c) {
+        if ('a' <= c && 'z' >= c) {
+            c = (char)('z' - (c - 'a'));
+        } else if ('A' <= c && 'Z' >= c) {
+            c = (char)('Z' - (c - 'A'));
+        }
+        return c;
     }
     public static String solution(String word) {
         char[] wordArr = word.toCharArray();
         for (int i = 0; i < word.length(); i++) {
-            wordArr[i] = conversionAscii(word.charAt(i));
-            wordArr[i] = conversionUpperLower(word.charAt(i));
+            wordArr[i] = convertAscii(wordArr[i]);
+            wordArr[i] = convertUpperLower(wordArr[i]);
         }
         word = String.valueOf(wordArr);
         return word;
