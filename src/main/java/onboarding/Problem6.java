182cb04fc6308d6e11f56e32b85939b96c0ff3c5
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-29 04:34:56 +0900

    update problem6 email check method

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static boolean isEmail(String email){
+        boolean validation = false;
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+
+        if(m.matches() &&  11 <= email.length() && email.length() < 20){
+            if (email.split("@")[1].equals("email.com")) validation = true;
+        }
+        return validation;
     }
 }
