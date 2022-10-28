222716a691703cd5b31f566723f301ddb8247971
June <68291395+june-777@users.noreply.github.com>
2022-10-28 16:57:52 +0900

    feat: 중복문자 찾기 및 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,44 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+    	
+    	char[] cryptogramArr = cryptogram.toCharArray();
+    	findDuplicateChars(cryptogramArr);
+    	
+        String answer = "";
+        
+        
+        
         return answer;
     }
+    public static void findDuplicateChars(char[] cryptogramArr) {
+    	Stack<int[]> pastCharAndIdxes = new Stack<int[]>();
+    	
+    	for(int i=0; i<cryptogramArr.length; i++) {
+    		char cur = cryptogramArr[i];
+    		
+    		if(pastCharAndIdxes.isEmpty()) {
+    			pastCharAndIdxes.add(new int[] {cur, i});
+    		} else {
+    			int[] pastCharAndIdx = pastCharAndIdxes.peek();
+    			char pastChar = (char)pastCharAndIdx[0];
+    			int pastCharIdx = pastCharAndIdx[1];
+    			
+    			if(pastChar == cur) {
+    				pastCharAndIdxes.pop();
+    				removeDuplicateChars(cryptogramArr, pastCharIdx, i);
+    			} else {
+    				pastCharAndIdxes.add(new int[] {cur, i});
+    			}
+    		}
+    	}
+    }
+    public static void removeDuplicateChars(char[] cryptogramArr, int pastIdx, int curIdx) {
+    	cryptogramArr[pastIdx] = '0';
+    	cryptogramArr[curIdx] = '0';
+    }
+    
 }
