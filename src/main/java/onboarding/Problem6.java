bb64bf3b19246380391d946b688cdb0f2c22f4cf
seokjin <rnrn8678@naver.com>
2022-10-28 11:33:02 +0900

    feat(Problem6.java): User의 닉네임을 문자열로 추출하여 블랙리스트에 등록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,31 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
+		Set<String> blacklist = new HashSet<>();
 		List<User> userList = forms.stream()
 				.map(User::new)
 				.collect(Collectors.toList());
+
+		for (User user : userList) {
+			String nickname = user.nickname;
+			for (int i = 1; i < nickname.length(); i++) {
+				int ptr1 = 0;
+				int ptr2 = i + 1;
+				while (ptr2 <= nickname.length()) {
+					blacklist.add(nickname.substring(ptr1, ptr2));
+					ptr1++;
+					ptr2++;
+				}
+			}
+		}
+		System.out.println(blacklist);
 
 		return Collections.emptyList();
 	}
