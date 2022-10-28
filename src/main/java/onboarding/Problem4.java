a166ccb255f01183fb14f6e22df9e4b56ec35e69
nakalic <nakalic28@gmail.com>
2022-10-28 19:04:27 +0900

    feat(Problem4): 알파벳 검증 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,12 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		String answer = "";
+		return answer;
+	}
+
+	private static boolean isAlphabet(char c) {
+		return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z');
+	}
 }
