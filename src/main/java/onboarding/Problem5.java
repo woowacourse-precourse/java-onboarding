0a2c86e420c6985ceb92b298c1a90872f3cc196d
ksh-beginner <ohk9134@naver.com>
2022-10-28 22:03:09 +0900

    feat : 화폐 단위별 개수 맵을 초기화하는 기능 메소드 추가
    Key - 화폐 단위, Value - 가진 갯수인 MoneyMap을 초기화하는 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    public static Map<Integer, Integer> initMoneyMap() {
+        Map<Integer, Integer> moneyMap = new LinkedHashMap<>();
+        moneyMap.put(50000, 0);
+        moneyMap.put(10000, 0);
+        moneyMap.put(5000, 0);
+        moneyMap.put(1000, 0);
+        moneyMap.put(500, 0);
+        moneyMap.put(100, 0);
+        moneyMap.put(50, 0);
+        moneyMap.put(10, 0);
+        moneyMap.put(1, 0);
+        return moneyMap;
+    }
+
 }
