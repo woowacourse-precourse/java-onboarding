a99a8cc9d3934140e81019e3ed32e4294f7f8cf2
49ehyeon42 <49ehyeon42@gmail.com>
2022-10-28 13:55:03 +0900

    Add Problem3.java

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int total = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int temp = i;
+            while (temp > 1) {
+                if (temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9) {
+                    total++;
+                }
+                temp /= 10;
+            }
+        }
+
+        return total;
     }
 }
