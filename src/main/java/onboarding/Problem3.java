6801b417b0479c2cdb6b45896dfdaef6b245c248
sosow0212 <sosow0212@naver.com>
2022-10-28 16:11:53 +0900

    feat: Problem3 - 들어온 수를 기준으로 박수를 몇 번 쳐야하는지 알려주는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,9 +10,19 @@ public class Problem3 {
         return answer;
     }
 
+    public static int getClapCountByStrNumber(String numberStr) {
+        int count = 0;
+        for (int i = 0; i < numberStr.length(); i++) {
+            char numTypeOfChar = numberStr.charAt(i);
+            if (isClapTiming(numTypeOfChar))
+                count++;
+        }
+        return count;
+    }
+
     public static boolean isClapTiming(char numTypeOfChar) {
         boolean isClapTiming = false;
-        if(numTypeOfChar == THREE || numTypeOfChar == SIX || numTypeOfChar == NINE)
+        if (numTypeOfChar == THREE || numTypeOfChar == SIX || numTypeOfChar == NINE)
             isClapTiming = true;
         return isClapTiming;
     }
