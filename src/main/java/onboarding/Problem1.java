2c0fa4056703e1f42447f250bf54e0e370f1e5e4
chaos0103 <lyt1228@naver.com>
2022-10-29 01:07:16 +0900

    feat(Problem1): 페이지 예외를 검증해주는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,46 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int MIN_PAGE = 1;
+    private static final int MAX_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!(isValidation(pobi) && isValidation(crong))) {
+            return -1;
+        }
+
         return answer;
+    }
+
+    private static boolean isValidation(List<Integer> page) {
+        int leftPage = page.get(0);
+        int rightPage = page.get(1);
+        if (isOutOfRange(leftPage) || isOutOfRange(rightPage)) {
+            return false;
+        } else if (isNotOddNumber(leftPage)) {
+            return false;
+        } else if (isNotEvenNumber(rightPage)) {
+            return false;
+        }
+        return checkPageOrder(leftPage, rightPage);
+    }
+
+    private static boolean isOutOfRange(int pageNumber) {
+        return pageNumber < MIN_PAGE || MAX_PAGE < pageNumber;
+    }
+
+    private static boolean isNotOddNumber(int pageNumber) {
+        return pageNumber % 2 == 1;
+    }
+
+    private static boolean isNotEvenNumber(int pageNumber) {
+        return pageNumber % 2 == 0;
+    }
+
+    private static boolean checkPageOrder(int leftPage, int rightPage) {
+        return (rightPage - leftPage) == 1;
     }
 }
