86018ea7055c7ce283c12cc9b8fc512814099e64
Identityless <rlandwnd555@naver.com>
2022-10-28 18:32:46 +0900

    FEAT : 알파벳을 대응하는 뒤집힌 알파벳으로 변환하는 AlphaSwaper 클래스의 converseAlpha() 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,8 +5,8 @@ import java.util.stream.Stream;
 /*
  *  구현 기능
  *  1. 문자열을 각 토큰으로 분할하는 기능
- *  2. 각 토큰이 알파벳인지 검사하는 기능
- *  3. 알파벳을 대응하는 뒤집힌 알파벳으로 변환하는 기능
+ *  2. 알파벳을 대응하는 뒤집힌 알파벳으로 변환하는 기능
+ *  3. 뒤집힌 알바벳을 다시 문장으로 만들어 봔환하는 기능
  */
 public class Problem4 {
     public static String solution(String word) {
@@ -27,3 +27,23 @@ class StringTokenizer {
     }
 }
 
+class AlphaSwaper {
+    private Stream token;
+    public AlphaSwaper(Stream token){
+        this.token = token;
+    }
+
+
+    public char converseAlpha(char alpha) {
+        if(!Character.isAlphabetic(alpha)) {
+            return alpha;
+        }
+        if(Character.isUpperCase(alpha)) {
+            return (char) ('Z' - alpha + 'A');
+        }
+        else {
+            return (char) ('z' - alpha + 'a');
+        }
+    }
+
+}
