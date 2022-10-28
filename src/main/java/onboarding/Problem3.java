59f081f1304622147201b7fa9c0ea682cc4659ce
matt <matt1235@naver.com>
2022-10-28 13:05:21 +0900

    feat: Problem3 (기능 목록2 - 3,6,9의 개수를 세는 메서드) 기능 구현 기능 요구 사항을 만족하는 메서드 - 3, 6, 9의 갯수를 세서 반환하는 메서드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -13,4 +13,16 @@ public class Problem3 {
             throw new IllegalArgumentException("숫자의 범위를 확인하세요");
     }
 
+    /**
+     * 3,6,9의 개수를 세는 함수*/
+    private static int count369(int number, int answer) {
+        for(int i = 1; i<= number; i++){
+            char[] charArr = Integer.toString(i).toCharArray();
+            for (char c : charArr) {
+                if(c=='3' || c=='6' || c=='9') answer++;
+            }
+        }
+        return answer;
+    }
+
 }
