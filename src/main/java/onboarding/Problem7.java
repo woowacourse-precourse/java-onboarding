8547d20bfc19bd6a4488caa3878879c26221f891
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-28 14:28:27 +0900

    feat: Problem7 friends 내에서 user의 친구를 HashSet에 담아 반환해주는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static Set<String> findUserFriend(String user, List<List<String>> friends) {
+        Set<String> friend = new HashSet<>();
+        //friend.add(user);
+        for(int i=0;i<friends.size();i++){
+            String leftPerson = friends.get(i).get(0);
+            String rightPerson = friends.get(i).get(1);
+            if(leftPerson.equals(user))friend.add(rightPerson);
+            if(rightPerson.equals(user))friend.add(leftPerson);
+        }
+        return friend;
     }
 }
