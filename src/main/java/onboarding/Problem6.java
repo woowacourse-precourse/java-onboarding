7b23be25e27809e15d0b67960461ba969af5c723
Plastic_hong <101045399+plastichong@users.noreply.github.com>
2022-10-29 02:03:40 +0900

    problem6 1.중복 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+
+        Map<String,String> validMap = new HashMap<>();
+
+        List<String> emailList = new ArrayList<>();
+
+        for (int i = 0; i <forms.size() ; i++) {
+            List<String> member = forms.get(i);
+
+            String email = member.get(0);
+            String nickName = member.get(1);
+
+
+            for (int j = 0; j <nickName.length()-1 ; j++) {
+                String minDuplicatePart = nickName.charAt(j)+""+nickName.charAt(j+1);
+
+                validMap.put(minDuplicatePart,email);
+
+                if(validMap.containsKey(minDuplicatePart)){
+                    emailList.add(validMap.get(minDuplicatePart));
+                }
+
+            }
+
+        }
+
         return answer;
     }
 }
