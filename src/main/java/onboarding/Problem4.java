248c9d888d5d9cef0a8c5b9ed7bb56cf21e527fe
egoing <dudqk9696@naver.com>
2022-10-28 09:59:58 +0900

    feat : 아스키 코드를 이용한 문자 변환 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,36 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = changeString(word);
         return answer;
+    }
+    static String changeString(String tmp) {
+        String result = "";
+        for(int i=0; i<tmp.length(); i++) {
+            char c = tmp.charAt(i);
+            int rem = 0;
+            if(Character.isUpperCase(c)) {
+                if(c <= 'M') {
+                    rem = 65 + ('Z' - c);
+                }
+                else if(c > 'M') {
+                    rem = 90 - (c - 'A');
+                }
+                result += (char)(rem);
+            }
+            else if(Character.isLowerCase(c)) {
+                if(c <= 'm') {
+                    rem = 97 + ('z' - c);
+                }
+                else if(c > 'M') {
+                    rem = 122 - (c - 'a');
+                }
+                result += (char)(rem);
+            }
+            else if(c == ' ') {
+                result += c;
+            }
+        }
+        return result;
     }
 }
