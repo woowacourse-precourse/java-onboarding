aab20205bcfefc841e4325473e1dd7edfff3a677
woosung1223 <dntjd991223@naver.com>
2022-10-28 11:40:34 +0900

    feat(Problem5): 입력 예외처리 담당 validationCheck 함수 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final int MONEY_LOWERBOUND = 1;
+    static final int MONEY_UPPERBOUND = 1_000_000;
+    static final List<Integer> MONEY_LIST = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    public static boolean ValidationCheck(int money) {
+        if (MONEY_LOWERBOUND <= money && money <= MONEY_UPPERBOUND) {
+            return true;
+        }
+        return false;
+    }
 }
+
+
