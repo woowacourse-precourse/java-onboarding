50637f7ea2c328c2715d69ceb1e51b2f9ece0924
homecoder98 <homecoder98@gmail.com>
2022-10-28 23:59:20 +0900

    Feat : Count three,six,nine from number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,14 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    static int countThreeSixNine(int i) {
+        int cnt = 0;
+        while (i > 0) {
+            int num = i % 10;
+            if (num == 3 || num == 6 || num == 9) cnt++;
+            i /= 10;
+        }
+        return cnt;
+    }
 }
