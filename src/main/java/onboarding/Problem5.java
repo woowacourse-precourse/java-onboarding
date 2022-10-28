3abf9178c9f3e92fbb56dbddfa95f07f2e53fa50
ywj9811 <ywj9811@naver.com>
2022-10-29 00:11:29 +0900

    ArrayList로 바꾸어 준비를 한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+
         return answer;
     }
 }
+/**
+  가장 큰단위 화폐를 위주로 돈을 넣어 준다.
+  오만, 만, 오천, 천, 오백, 백, 오십, 십, 일원 순서대로 리스트로 반환
+ */
