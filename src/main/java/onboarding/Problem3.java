f2e3a6c91691344db88db2d5b401eac9cde2c66e
juno-junho <ssmm0205@naver.com>
2022-10-29 07:10:27 +0900

    feat(Problem3): add method
    countingClapCount 메소드, countingNumberThreeSixNine 메소드 생성을 통해 기능 세분화

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,15 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        return countingClapCount(number);
+    }
+
+    static int countingClapCount(int endNum) {
         int clapCount = 0;
-        for (int i = 1; i <= number; i++) {
-            int targetNumber = i;
-            while (targetNumber > 0) {
-                if(isThreeSixNine(targetNumber)) clapCount++;
-                targetNumber /= 10;
-            }
+        for (int eachNum = 0; eachNum < endNum; eachNum++) {
+            clapCount += countingNumberThreeSixNine(eachNum);
         }
         return clapCount;
+    }
+
+    static int countingNumberThreeSixNine(int number) {
+        int countedNumber = 0;
+        while (number > 0) {
+            if (isThreeSixNine(number)) countedNumber++;
+            number /= 10;
+        }
+        return countedNumber;
     }
 
     static boolean isThreeSixNine(int number) {
