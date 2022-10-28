7eb5ba77132af98149998a839613d1202821ba8c
woosung1223 <dntjd991223@naver.com>
2022-10-28 22:31:31 +0900

    feat(Problem7): visitor관련 함수 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,20 +8,28 @@ public class Problem7 {
     private static int forID = 0;
     private static int FRIEND_WEIGHT = 10;
     private static int VISITOR_WEIGHT = 1;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = getSolution(user, friends, visitors);
         return answer;
+    }
+    
+    public static void initializeWeight() {
+        for (int i = 0; i < uniqueIDs.size(); i++) {
+            weight.put(i, 0);
+        }
     }
     public static void addWeight(List<String> list, int WEIGHT) {
         for (int i = 0; i < list.size(); i++) {
             String user = list.get(i);
-            int updatedValue = weight.get(user) + WEIGHT;
-            int uniqueID = uniqueIDs.get(user);
-            weight.put(uniqueID, updatedValue);
+            int userID = uniqueIDs.get(user);
+            int updatedValue = weight.get(userID) + WEIGHT;
+            weight.put(userID, updatedValue);
         }
     }
+
     public static List<String> getFriendsList(List<List<String>> friends, String user) {
-        List<String> friendsList = Collections.emptyList();
+        List<String> friendsList = new ArrayList<>();
         for (int i = 0; i < friends.size(); i++) {
             List<String> relation = friends.get(i);
             String friend1 = relation.get(0);
@@ -35,10 +43,16 @@ public class Problem7 {
         }
         return friendsList;
     }
-    public static void setDistinctID(List<String> friends) { // getDistinctFriends 이후 호출됨
+    public static void setDistinctID(List<String> friends, List<String> visitors) { // getDistinctFriends 이후 호출됨
         for (int i = 0; i < friends.size(); i++) {
             String name = friends.get(i);
             uniqueIDs.put(name, forID++);
+        }
+        for (int i = 0; i < visitors.size(); i++) {
+            String name = visitors.get(i);
+            if (!uniqueIDs.containsKey(name)) {
+                uniqueIDs.put(name, forID++);
+            }
         }
     }
     public static List<String> getDistinctFriends(List<List<String>> friends) {
@@ -51,4 +65,11 @@ public class Problem7 {
         List<String> distinctList = new ArrayList<>(distinctSet);
         return distinctList;
     }
+
+    public static List<String> getDistinctVisitors(List<String> visitors) {
+        Set<String> distinctSet = new HashSet<>(visitors);
+        List<String> distinctList = new ArrayList<>(distinctSet);
+        return distinctList;
+    }
+
 }
