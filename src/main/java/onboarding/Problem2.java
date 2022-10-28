1cfd39b869f4c5429e224560778fcdd3cceaf168
김태용 <amico741@naver.com>
2022-10-28 20:17:46 +0900

    feat: 중복되는 문자열 제거하는 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,36 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decrypt(cryptogram);
+    }
+
+    public static String decrypt(String crpytogram) {
+        StringBuilder newString = new StringBuilder();
+        boolean check;
+        do {
+            if (crpytogram.length() == 0) break;
+            newString.append(crpytogram.charAt(0));
+            int start = 0;
+            int end = 0;
+            check = false;
+            for (int i = 0; i < crpytogram.length(); i++) {
+                end += 1;
+                int length = newString.length();
+                if (end < crpytogram.length()) {
+                    if (crpytogram.charAt(start) != crpytogram.charAt(end)) {
+                        start = end;
+                        newString.append(crpytogram.charAt(end));
+                    } else if (length > 0) {
+                        check = true;
+                        if(newString.charAt(newString.length() - 1) == crpytogram.charAt(end)){
+                            newString.deleteCharAt(newString.length() - 1);
+                        }
+                    }
+                }
+            }
+            crpytogram = String.valueOf(newString);
+            newString.delete(0,newString.length());
+        } while (check);
+        return crpytogram;
     }
 }
