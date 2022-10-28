a0c6aa703188b14b247a9ac405482996141a88bf
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 12:13:04 +0900

    feat: PROBLEM3 3, 6, 9 카운트 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = 0;
+		return answer;
+	}
+
+	static class Counter {
+
+		public int count369(int number) {
+			List<Integer> numberList = new ArrayList<>();
+			int result = 0;
+
+			while (number > 0) {
+				numberList.add(number % 10);
+				number = number / 10;
+			}
+
+			for (int n : numberList) {
+				if (n > 0 && n % 3 == 0) {
+					result += 1;
+				}
+			}
+			return result;
+		}
+	}
 }
