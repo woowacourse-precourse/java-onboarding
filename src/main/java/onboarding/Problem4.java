b4a5e9aa5d375f8864e4c42f79364cb05189c700
Hyeonjae-K <href.hjk@gmail.com>
2022-10-28 09:53:17 +0900

    feat: 알파벳 변환 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-/*기능 목록
+/* 기능 목록
  * 알파벳을 반대로 변환해 주는 기능
  * 문자열을 반복하는 기능
  * */
@@ -9,5 +9,25 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    /**
+     * 문자를 변환하여 반환한다.
+     *
+     * @param letter 변환할 문자
+     * @return 알파벳을 변환하여 반환, 알파벳이 아닐 경우 변환하지 않고 반환
+     */
+    char reverse(char letter) {
+        char res;
+
+        if ('A' <= letter && letter <= 'Z') {
+            res = (char) ('Z' - letter + 'A');
+        } else if ('a' <= letter && letter <= 'z') {
+            res = (char) ('z' - letter + 'a');
+        } else {
+            res = letter;
+        }
+
+        return res;
     }
 }
