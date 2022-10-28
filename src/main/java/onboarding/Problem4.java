9a36e0d18604a7a403fe41c8dd0c628d23c57d45
jeongwle <dldl4568@naver.com>
2022-10-28 14:07:10 +0900

    [Feat] Problem4 word의 길이를 검증하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        if (!isValidate(word)) {
+            return "error";
+        }
         String answer = "";
         return answer;
+    }
+
+    /**
+     * word의 길이를 검증하는 기능
+     */
+    public static boolean isValidate(String word) {
+        int length = word.length();
+        return (length > 0 && length < 1001);
     }
 }
