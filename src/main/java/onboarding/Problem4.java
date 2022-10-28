1c51197cca394ce09495fb5f0e86b53dcf1e4f28
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-28 17:23:13 +0900

    feat: Problem_4 문자의 케이스를 확인해서 변환하는 기능 구현
    문자의 케이스에 따라서
    아스키 코드 값의 범위를 정하고
    약속된 값으로 연산

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        char[] chars = word.toCharArray();
+        char[] result = new char[word.length()];
+
+        for (int i=0; i<word.length(); i++){
+            result[i] = toDictionaryWord(chars[i]);
+        }
+        return new String(result);
     }
+
+
+    public static char toDictionaryWord(char ch){
+        final char[] UPPER = new char[]{'A', 'Z'};
+        final char[] LOWER = new char[]{'a', 'z'};
+
+        int dictWord = ch;
+        if (Character.isUpperCase(ch)) {
+            dictWord = getDictAsc(UPPER, ch);
+        } else if (Character.isLowerCase(ch)) {
+            dictWord = getDictAsc(LOWER, ch);
+        }
+
+        return (char) dictWord;
+    }
+
+    private static int getDictAsc(char[] ascRange, char originalWord){
+        char start = ascRange[0];
+        char end = ascRange[1];
+
+        int ascDistance = originalWord - start;
+        return end - ascDistance;
+    }
+
+
+
+
 }
