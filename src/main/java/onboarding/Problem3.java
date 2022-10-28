cf0668034121c7d8686b2f5f376816fd63fdb1f4
Plastic_hong <zakkslash18@naver.com>
2022-10-28 11:55:24 +0900

    problem3 1.StringBuffer 객체 생성과 대입

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,11 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < number ; i++) {
+            sb.append(i+1);
+        }
 
         return answer;
     }
