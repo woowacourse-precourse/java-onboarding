7699d93436822ef50e58da7422c4b251a60a8d91
Isu Shin <isu010614@naver.com>
2022-10-28 20:07:50 +0900

    fix: Fix errors and change method and variable names
    error - sum/multiple digits methods were wrong

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,31 +5,31 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int pobiLeftPage = pobi.get(0);
-        int pobiRightPage = pobi.get(1);
-        int crongLeftPage = crong.get(0);
-        int crongRightPage = crong.get(1);
+        final int pobiLeftPage = pobi.get(0);
+        final int pobiRightPage = pobi.get(1);
+        final int crongLeftPage = crong.get(0);
+        final int crongRightPage = crong.get(1);
 
         if (!isException(pobiLeftPage, pobiRightPage)
                 || !isException(crongLeftPage, crongRightPage)) {
             return -1;
         }
 
-        int pobiScore = findMaxScore(pobiLeftPage, pobiRightPage);
-        int crongScore = findMaxScore(crongLeftPage, crongRightPage);
+        int pobiScore = maxScore(pobiLeftPage, pobiRightPage);
+        int crongScore = maxScore(crongLeftPage, crongRightPage);
         answer = compareScore(pobiScore, crongScore);
 
         return answer;
     }
 
-    public static int plusDigits(int number) {
-        int plusNumber = 0;
+    public static int sumDigits(int number) {
+        int sumNumber = 0;
 
         while (number > 0) {
-            plusNumber += number % 10;
+            sumNumber += number % 10;
             number /= 10;
         }
-        return number;
+        return sumNumber;
     }
 
     public static int multipleDigits(int number) {
@@ -39,14 +39,14 @@ class Problem1 {
             multipleNumber *= number % 10;
             number /= 10;
         }
-        return number;
+        return multipleNumber;
     }
 
-    public static int comparePlusNumber(int leftPage, int rightPage) {
-        if (plusDigits(leftPage) - plusDigits(rightPage) >= 0) {
-            return plusDigits(leftPage);
+    public static int compareSumNumber(int leftPage, int rightPage) {
+        if (sumDigits(leftPage) - sumDigits(rightPage) >= 0) {
+            return sumDigits(leftPage);
         }
-        return plusDigits(rightPage);
+        return sumDigits(rightPage);
     }
 
     public static int compareMultipleNumber(int leftPage, int rightPage) {
@@ -56,12 +56,12 @@ class Problem1 {
         return multipleDigits(rightPage);
     }
 
-    public static int findMaxScore(int leftPage, int rightPage) {
+    public static int maxScore(int leftPage, int rightPage) {
         if (compareMultipleNumber(leftPage, rightPage)
-                - comparePlusNumber(leftPage, rightPage) >= 0) {
+                - compareSumNumber(leftPage, rightPage) >= 0) {
             return compareMultipleNumber(leftPage, rightPage);
         }
-        return comparePlusNumber(leftPage, rightPage);
+        return compareSumNumber(leftPage, rightPage);
     }
 
     public static int compareScore(int pobiScore, int crongScore) {
@@ -71,18 +71,18 @@ class Problem1 {
     }
 
     public static boolean isException(int leftPage, int rightPage) {
+        // 시작 면이나 마지막 면을 포함하여 페이지를 벗어난 곳 펼친 경우
+        if (leftPage <= 1 || rightPage >= 400) {
+            return false;
+        }
+
         //연속된 페이지가 아닌 경우
         if (rightPage - leftPage != 1) {
             return false;
         }
 
         // 왼쪽 페이지가 홀수, 오른쪽 페이지가 짝수가 아닌 경우
-        if ((leftPage - 1) / 2 != 0 || rightPage / 2 != 0) {
-            return false;
-        }
-
-        // 시작 면이나 마지막 면을 포함하여 페이지를 벗어난 곳 펼친 경우
-        if (leftPage <= 1 || rightPage >= 400) {
+        if (leftPage % 2 == 0 || rightPage % 2 != 0) {
             return false;
         }
         return true;
