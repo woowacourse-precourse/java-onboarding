7e6ac1f4a825e8e9ddc4939bf3d4d010982ff09f
ymkim97 <rladudaud97@naver.com>
2022-10-28 23:23:16 +0900

    feat: add function count number of clap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,20 +3,39 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int count;
 
         if (throwBoundaryException(number)) {
             return -1;
+        }
+
+        for (count = 1; count <= number; count++) {
+            answer += countClapNumber(count);
         }
 
         return answer;
     }
 
     public static boolean throwBoundaryException(int number) {
-
         if (number < 1 || number > 10000) {
             return true;
         }
 
         return false;
+    }
+
+    public static int countClapNumber(int number) {
+        int countClap = 0;
+        int compare;
+
+        while (number > 0) {
+            compare = number % 10;
+            if (compare == 3 || compare == 6 || compare == 9) {
+                countClap += 1;
+            }
+            number /= 10;
+        }
+
+        return countClap;
     }
 }
