f8741490ff6a0202ccd5c7242fdd990c236d0d89
sen <seeunchoi99@gmail.com>
2022-10-28 19:30:24 +0900

    🎨 Problem2.deleteDuplication 추가에 따른 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,18 +9,10 @@ import java.util.*;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        Stack<String> stack = new Stack<>();
-        String[] strings = cryptogram.split("");
+        final String[] strings = cryptogram.split("");
 
-        for (String string : strings) {
-            if (!stack.isEmpty() && stack.peek().equals(string)) {
-                stack.pop();
-            } else {
-                stack.add(string);
-            }
-        }
-
-        return stack.stream().reduce("", (x, y) -> x + y);
+        return deleteDuplication(strings).stream()
+                .reduce("", (x, y) -> x + y);
     }
 
     /**
