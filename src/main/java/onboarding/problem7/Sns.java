dbe2e605a00420c3553a4c020ba78be569a25803
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-28 14:08:12 +0900

    feat: 친구 목록을 통해 SNS 가입 사용자 리스트에 추가하는 기능

---
+++ src/main/java/onboarding/problem7/Sns.java
@@ -0,0 +1,16 @@
+package onboarding.problem7;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Sns {
+	public static final HashMap<String, User> registeredUsers = new HashMap<>();
+
+	private static void addUserFromRelation(List<List<String>> relations) {
+		for (List<String> relation : relations) {
+			for (String userName : relation) {
+				registeredUsers.putIfAbsent(userName, new User(userName));
+			}
+		}
+	}
+}
