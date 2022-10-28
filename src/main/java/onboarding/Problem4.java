ef6982dd8666cdfedd001321e1269580629304e2
ymkim97 <rladudaud97@naver.com>
2022-10-28 23:58:54 +0900

    feat: add fuction distinguish alphabet

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,11 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-
         String[] stringArray = word.split(" ");
 
         if (throwBoundaryException(word)) {
             return "failed with boundary exception";
+        }
+
+        for (String eachWord : stringArray) {
+
         }
 
         return ;
@@ -17,6 +20,15 @@ public class Problem4 {
         if (word.length() < 1 || word.length() > 1000) {
             return true;
         }
+
         return false;
     }
+
+    public static boolean isAlphabet(char word) {
+        if (word < 65 || word > 122 || (word > 90 && word < 97)) {
+            return true;
+        }
+        return false;
+    }
+
 }
