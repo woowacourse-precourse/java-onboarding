7035e5f86702fc0185a4f556905ae4c5c565161f
lecocococo <chemwang13@naver.com>
2022-10-29 00:45:59 +0900

    Feat: Problem6의 crew의 닉네임으로 2글자의 토큰들로 만드는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
+	public static Map<String, Integer> nicknameTokenizer(String crewNickname){
+		Map<String, Integer> nicknameTokens = new HashMap<>();
+		String nicknameToken = "";
+
+		for(int i = 0; i < crewNickname.length()-1; i++) {
+			nicknameToken = crewNickname.substring(i,i + 2);
+			nicknameTokens.put(nicknameToken, 1);
+		}
+
+		return nicknameTokens;
+	}
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+		List<String> answer = List.of("answer");
         return answer;
     }
 }
