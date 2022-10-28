1f2e599cf9dc586c6d981dd13f844526c6b792fe
2jun0 <soo28819@gmail.com>
2022-10-28 22:57:49 +0900

    Fix: 문제 1번 - page예외처리 1<=page<=399 수정

--- src/main/java/onboarding/problem1/Pages.java
+++ src/main/java/onboarding/problem1/Pages.java
@@ -6,7 +6,7 @@ public class Pages {
 
     public Pages(int page1, int page2) {
         // Assert args
-        if (!(page1 >= 1 && page1 <= 309)) {
+        if (!(page1 >= 1 && page1 <= 399)) {
             throw new IllegalArgumentException("페이지 값은 다음과 같은 조건을 만족해야 합니다 : 1 <= page1 <= 309");
         } else if (!(page1%2 == 1 && page2 == page1+1)) {
             throw new IllegalArgumentException("페이지 값은 다음과 같은 조건을 만족해야 합니다 : page1 = 홀수, page2 = page1 + 1");
