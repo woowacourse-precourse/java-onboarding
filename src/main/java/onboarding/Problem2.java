61bdd4111ed16bd3c3d4e15b5c7c75b48109c4bb
Jeongthingamajig <netpetet@naver.com>
2022-10-29 00:15:29 +0900

    7번 정렬 하지 않은 채로 풀이 2번 제약조건 생각 해놓음

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,18 +3,22 @@ package onboarding;
 /*
  * decoder에서 해독 방식 로직만 가져와서 체크 할 수 있도록 리팩토링
  * return crypto 생각하기
- *
+ * 지우는 순서도 생각하기 aabbba a먼저 -> bbba -> a가 남음 b 먼저 -> 전부 소거
+ * i부터 i+1이 아닌 같으면 전부 지우는가도 생각을 해보기
+ * 40번줄 target + 2가 아닌 target + 같은 문자의 길이
+ * result output << 이거 nullpointexception 날 수도 있음 안에 값 넣어줘야함
 */
 public class Problem2 {
     public static String solution(String cryptogram) {
 
         String result = decoder(cryptogram);
 
+        System.out.println(result);
         return result;
     }
 
     public static String decoder(String crypto){ //암호해독기
-        String output = "";
+        String output;
 
         for(int check = 0; check < crypto.length() - 1; check++) { // 중복 있는지 체크
             if (crypto.charAt(check) == crypto.charAt(check + 1)) {
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,49 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+ * 여기서 친구 사이를 알아 놓는다
+ * 친구가 아닌 사람을 배열에 모아 놓는다
+ * 그 사람들의 점수를 know_with_user_score 함수에 넣어서 점수 알아오고
+ * visitor 점수를 알기 위해 visitor_score 함수에 넣고 온다
+ */
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        Map<String, Integer> score_board = new HashMap<>(friends.size()); // 점수 저장
+        List<List<String>> friends_friends = new ArrayList<>(); //친구들의 친구들
+
+        // 친구의 친구들을 구함
+        for(List<String> friends_list : friends){
+            if(friends_list.contains(user)){
+                friends_friends.add(friends_list); //user도 포함
+            }
+        }
+
+        // 친구의 친구들 중에서 추천 점수 +10 해줄 사람들을 구함
+        for(List<String> recommend_friend : friends_friends){
+            for(int i = 0; i < recommend_friend.size(); i++){
+                if(!recommend_friend.contains(user) && !recommend_friend.contains(recommend_friend.get(i))){
+                    score_board.put(recommend_friend.get(i), score_board.get("recommend_friend.get(i))" + 10));
+                }
+            }
+        }
+
+        for(String s : visitors){
+            score_board.put(s, score_board.get(s) + 1);
+        }
+
+        System.out.println(score_board);
+
+        List<String> KeyList = new ArrayList<>(score_board.keySet());
+        System.out.println(KeyList);
+
+        return KeyList;
+
     }
+
 }
