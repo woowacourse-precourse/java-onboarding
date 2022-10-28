2f0a271b2fb4665bf8fd8ef4172d5cb8d366e3c0
baekeunsun <eunsun5460@gmail.com>
2022-10-28 23:59:01 +0900

    [문제2] feats: input 예외 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,29 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        return answer;
+        if(CheckCryptogram(cryptogram)){
+
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static boolean CheckCryptogram(String str){
+        if(isLower(str) && isInRange(str) )return true;
+        return false;
+    }
+
+    private static boolean isLower(String str){
+        char[] charArray = str.toCharArray();
+
+        for(int i = 0; i < charArray.length; i++){
+            if( !Character.isUpperCase(charArray[i]))
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean isInRange(String str){
+        if(str.length() > 0 && str.length() < 1001) return true;
+        return false;
     }
 }
