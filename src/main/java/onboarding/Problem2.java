7229b7032441288c86b041535ce9187feb338953
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-10-28 13:54:37 +0900

    Problem2 기능 목록 2 : 결과값을 저장할 수 있도록 LinkedList를 준비한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,13 @@
 package onboarding;
 
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
         char[] characterArray = cryptogram.toCharArray();
+        LinkedList<Character> characterList = new LinkedList<>();
 
         return answer;
     }
