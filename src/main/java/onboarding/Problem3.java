e858cfe6ac8b487af5beae05b7648947e784283e
khj1 <dhepd8849@gmail.com>
2022-10-28 16:08:36 +0900

    feat(problem3): 3, 6, 9가 포함된 횟수를 계산하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    public static final int NO_CLAP = 0;
+
+    public static int solution(int number) {;
+        return calculateClapCount(number);
+    }
+
+    private static int calculateClapCount(int number) {
+        int clapCount = NO_CLAP;
+        for (int i = 1; i <= number; i++) {
+            clapCount += find369(i);
+        }
+        return clapCount;
+    }
+
+    private static int find369(int number) {
+        return 0;
     }
 }
