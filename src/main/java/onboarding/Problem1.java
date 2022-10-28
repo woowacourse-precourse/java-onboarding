ebcafb5df39e704b425c82828c8ab84941c6a546
ywj9811 <ywj9811@naver.com>
2022-10-28 17:53:48 +0900

    포비 최대값 크롱 최대값 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,51 +9,9 @@ class Problem1 {
         int pobiMax;
         int crongMax;
 
-        pobiMax = (getPlus(pobi) > getMul(pobi)) ? getPlus(pobi) : getMul(pobi);
-        crongMax = (getPlus(crong) > getMul(crong) ? getPlus(crong) : getMul(crong));
-
-        answer = getMax(pobiMax, crongMax);
-
-        if((pobi.get(1) - pobi.get(0) != 1) || (crong.get(1) - crong.get(0) != 1))
-            answer = -1;
-
         return answer;
     }
-
-    static int getPlus(List<Integer> list) {
-        int listPlusLeft, listPlusRight;
-
-        if (list.get(0)/10 == 0) {
-            return list.get(1);
-        } else if (list.get(0)/100 == 0) {
-            listPlusLeft = (list.get(0) % 10) + (list.get(0) / 10);
-            listPlusRight = (list.get(1) % 10) + (list.get(1) / 10);
-        } else {
-            listPlusLeft = (list.get(0) / 100) + ((list.get(0) % 100) / 10) + (list.get(0) % 10);
-            listPlusRight = (list.get(1) / 100) + ((list.get(1) % 100) / 10) + (list.get(1) % 10);
-        }
-
-        return listPlusLeft > listPlusRight ? listPlusLeft : listPlusRight;
-    }
-    static int getMul(List<Integer> list) {
-        int listMulLeft, listMulRight;
-
-        if (list.get(0)/10 == 0) {
-            return list.get(1);
-        } else if (list.get(0)/100 == 0) {
-            listMulLeft = (list.get(0) % 10) * (list.get(0) / 10);
-            listMulRight = (list.get(1) % 10) * (list.get(1) / 10);
-        } else {
-            listMulLeft = (list.get(0) / 100) * ((list.get(0) % 100) / 10) * (list.get(0) % 10);
-            listMulRight = (list.get(1) / 100) * ((list.get(1) % 100) / 10) * (list.get(1) % 10);
-        }
-
-        return listMulLeft > listMulRight ? listMulLeft : listMulRight;
-    }
-
-    static int getMax(int pobi, int crong) {
-        return (pobi == crong) ? 0 : (pobi > crong) ? 1 : 2;
-    }
+}
     /**
      * 왼쪽 페이지의 번호 각자리를 더하거나 곱해서 가장 큰 수
      * 오른쪽 마찬가지
@@ -88,4 +46,3 @@ class Problem1 {
      * 결국 나온 코드는 위와 같은 코드인데 이보다 더 클린하게 만들 수 있지 않을까 싶으며
      * 모범적인 코드를 본다면 보고 배우는 시간을 가져볼까 합니다.
      */
-}
