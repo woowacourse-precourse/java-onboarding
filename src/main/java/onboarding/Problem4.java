cd5f4c83484bef3cc60d537162374afcf0a312fc
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-28 17:27:07 +0900

    feat: Problem_4 변환된 문자열을 생성하는 기능 구현
    연산된 문자배열을 문자열로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,18 +4,21 @@ import java.util.*;
 
 public class Problem4 {
     public static String solution(String word) {
+        return getDictWord(word);
+    }
+
+    private static String getDictWord(String word){
 
         char[] chars = word.toCharArray();
         char[] result = new char[word.length()];
 
         for (int i=0; i<word.length(); i++){
-            result[i] = toDictionaryWord(chars[i]);
+            result[i] = toDictChar(chars[i]);
         }
         return new String(result);
     }
 
-
-    public static char toDictionaryWord(char ch){
+    public static char toDictChar(char ch){
         final char[] UPPER = new char[]{'A', 'Z'};
         final char[] LOWER = new char[]{'a', 'z'};
 
