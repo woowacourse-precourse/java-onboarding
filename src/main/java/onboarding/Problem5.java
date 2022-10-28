c144f8ba6a1af5f7d629816a51347ed31006165c
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-28 12:58:27 +0900

    refactor: [Problem5] - 주석 대신 리스트 이름 변경을 통해 의미 명확화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,9 +9,9 @@ public class Problem5 {
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        List<Integer> moneyUnits = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> moneyUnitsToExchange = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
-        for (Integer moneyUnit : moneyUnits) {
+        for (Integer moneyUnit : moneyUnitsToExchange) {
             money = exchangeMoney(money, moneyUnit, answer);
         }
         return answer;
