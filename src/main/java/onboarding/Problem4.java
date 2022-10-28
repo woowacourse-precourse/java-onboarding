930071fc6511207084e97977148ab5f630d8435a
sen <seeunchoi99@gmail.com>
2022-10-28 16:55:23 +0900

    ✨ Problem4.toOpposite 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,7 +7,22 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        return toOpposite(word.toCharArray());
+    }
+
+    private static String toOpposite(char[] chars) {
         String answer = "";
+
+        for (char ch : chars) {
+            if (Character.isUpperCase(ch)) {
+                answer += (char)('Z' - (ch - 'A'));
+            } else if (Character.isLowerCase(ch)) {
+                answer += (char)('z' - (ch - 'a'));
+            } else {
+                answer += ch;
+            }
+        }
+
         return answer;
     }
 }
