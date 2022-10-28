a1fa22d8c5814b02f8665394ae5e1145eb4f904d
antaeho <an981022@naver.com>
2022-10-28 21:20:23 +0900

    Problem7 기능 구현사항 목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,7 +2,12 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+/*
+- 기능 구현사항 목록 -
+1. 친구 목록 해쉬맵 만들기
+2. user 와 이미 친구인 사람 목록 만들기 (나중에 해쉬맵에서 제거할 목록)
+3. 점수 측정 -> value 값으로 정렬후 5명으로 자르기
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
