fff0a826932c5a435c051db9c3fea50526d99f44
hectick <chyeon1223@cau.ac.kr>
2022-10-29 02:38:46 +0900

    feat : [Problem6] 중복 닉네임을 작성한 지원자의 메일 리스트를 반환하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,12 +1,9 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
         HashMap<String, String> map = new HashMap<>(); //닉네임 중복 체크를 위해 닉네임의 파편들을 이메일주소와 함께 저장
         HashSet<String> set = new HashSet<>(); //중복되지 않도록 이메일을 저장
 
@@ -32,7 +29,8 @@ public class Problem6 {
             }
         }
         //set을 list로 변경하여 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 메일 목록을 반환하는 기능
-
+        List<String> answer = new ArrayList<>(set); //set -> list
+        Collections.sort(answer);
         return answer;
     }
 }
