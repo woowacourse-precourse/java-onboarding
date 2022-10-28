189e16a872d2e612407f16c7e38d8c53636712e8
khj1 <dhepd8849@gmail.com>
2022-10-28 15:40:22 +0900

    refactor(problem2): 함수를 기능 단위로 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,30 +12,66 @@ public class Problem2 {
         if (cryptogram.equals(EMPTY_STRING)) {
             return cryptogram;
         }
-        String refinedCryptogram = removeDuplicateCharacters(cryptogram);
+        String refinedCryptogram = removeDuplicates(cryptogram);
         if (cryptogram.equals(refinedCryptogram)) {
             return cryptogram;
         }
         return decrypt(refinedCryptogram);
     }
 
-    private static String removeDuplicateCharacters(String cryptogram) {
-        char[] cryptogramChars = cryptogram.toCharArray();
+    private static String removeDuplicates(String cryptogram) {
         String refinedCryptogram = EMPTY_STRING;
-        if (cryptogram.length() == ONE_LETTER) {
+        if (isOneLetter(cryptogram)) {
             refinedCryptogram = cryptogram;
         }
-        if (cryptogram.length() > ONE_LETTER && cryptogramChars[0] != cryptogramChars[1]) {
-            refinedCryptogram += cryptogramChars[0];
-        }
-        for (int i = 1; i < cryptogram.length() - 1; i++) {
-            if (cryptogramChars[i] != cryptogramChars[i + 1] && cryptogramChars[i] != cryptogramChars[i - 1]) {
-                refinedCryptogram += cryptogramChars[i];
-            }
-        }
-        if (cryptogram.length() > ONE_LETTER && cryptogramChars[cryptogram.length() - 1] != cryptogramChars[cryptogram.length() - 2]) {
-            refinedCryptogram += cryptogramChars[cryptogram.length() - 1];
+        if (isMoreThanTwoLetter(cryptogram)) {
+            refinedCryptogram = removeDuplicatesOfMoreThanTwoLetters(cryptogram, refinedCryptogram);
         }
         return refinedCryptogram;
+    }
+
+    private static String removeDuplicatesOfMoreThanTwoLetters(String cryptogram, String refinedCryptogram) {
+        char[] cryptogramChars = cryptogram.toCharArray();
+        refinedCryptogram += checkDuplicatesOfFirstLetter(cryptogramChars);
+        refinedCryptogram = removeDuplicatesOfMiddleLetters(cryptogram, refinedCryptogram, cryptogramChars);
+        refinedCryptogram += checkDuplicatesOfLastLetter(cryptogram, cryptogramChars);
+
+        return refinedCryptogram;
+    }
+
+    private static String checkDuplicatesOfLastLetter(String cryptogram, char[] cryptogramChars) {
+        if (cryptogramChars[cryptogram.length() - 1] != cryptogramChars[cryptogram.length() - 2]) {
+            return Character.toString(cryptogramChars[cryptogram.length() - 1]);
+        }
+        return EMPTY_STRING;
+    }
+
+    private static String removeDuplicatesOfMiddleLetters(String cryptogram, String refinedCryptogram, char[] cryptogramChars) {
+        for (int i = 1; i < cryptogram.length() - 1; i++) {
+            refinedCryptogram += checkDuplicatesOfMiddleLetters(cryptogramChars, i);
+        }
+        return refinedCryptogram;
+    }
+
+    private static String checkDuplicatesOfMiddleLetters(char[] cryptogramChars, int i) {
+        if (cryptogramChars[i] != cryptogramChars[i + 1] && cryptogramChars[i] != cryptogramChars[i - 1]) {
+            return Character.toString(cryptogramChars[i]);
+        }
+        return EMPTY_STRING;
+    }
+
+    private static String checkDuplicatesOfFirstLetter(char[] cryptogramChars) {
+        if (cryptogramChars[0] != cryptogramChars[1]) {
+            return Character.toString(cryptogramChars[0]);
+        }
+        return EMPTY_STRING;
+    }
+
+    private static boolean isMoreThanTwoLetter(String cryptogram) {
+        return cryptogram.length() > ONE_LETTER;
+    }
+
+    private static boolean isOneLetter(String cryptogram) {
+        return cryptogram.length() == ONE_LETTER;
     }
 }
