0ed72363cfcf2ef240543d224f5002d3919ac014
kwYoohae <gocksdl7979@gmail.com>
2022-10-28 10:28:09 +0900

    feat(Problem3.java): 369게임에서 3,6,9가 몇개인지 확인하는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += countTargetNumber(i);
+        }
+
         return answer;
+    }
+
+    public static long countTargetNumber(int number) {
+        String[] splitNumber = Integer.toString(number).split("");
+
+        return Arrays.stream(splitNumber).filter(eachNumber ->
+                eachNumber.equals("3") || eachNumber.equals("6") || eachNumber.equals("9")
+        ).count();
     }
 }
