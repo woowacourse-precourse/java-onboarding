dbbd04a7f42352552d0901eef2f7e0efe0ba0a10
KSH <tkdgns543@gmail.com>
2022-10-28 23:43:36 +0900

    feat(Problem3): solution에 반복문으로 1부터 주어진 number까지 3,6,9의 합을 구하는 기능 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer +=countThreeNumbers(i);
+        }
+
         return answer;
     }
 
