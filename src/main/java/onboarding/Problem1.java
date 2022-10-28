c826ba1ffca77cd1bcd3205438ffa737a6c9c9f9
great-park <cksgh1735@naver.com>
2022-10-29 02:48:51 +0900

    fix(Problem1): 상수 -> final 접근 제어자 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    private static int pobiWin = 1;
-    private static int crongWin = 2;
-    private static int draw = 0;
-    private static int exception = -1;
+    private static final int pobiWin = 1;
+    private static final int crongWin = 2;
+    private static final int draw = 0;
+    private static final int exception = -1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if(validatePageNumbers(List.of(pobi, crong))){
