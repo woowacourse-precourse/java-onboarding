219311aac29a80b4299fe0c649fd91e15a6b71ea
Eundms <dms011221@gmail.com>
2022-10-28 11:13:08 +0900

    add(Problem7-TODO)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        //TODO: network 만들기
+
+        //TODO: 'user 친구' 리스트
+        //TODO: user와 친구가 아닌 'user 친구의 친구' 리스트 만들기
+
+        //TODO: score 높은 순서대로 정렬 + 최대 5명
+
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
 }
