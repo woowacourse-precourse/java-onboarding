e0aaf2c25f5e515a3e81a6d69336fb6a5b8f2406
sen <seeunchoi99@gmail.com>
2022-10-28 14:10:52 +0900

    💡 Problem3 기능 목록 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 현재 숫자에 따른 손뼉치는 횟수 계산
+ * 2. 전체 손뼉치는 횟수 계산
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
