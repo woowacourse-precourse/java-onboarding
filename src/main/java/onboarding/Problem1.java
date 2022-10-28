11e48f416848a38afca8cbae16233241a72ae8e2
seonghoo1217 <seonghoo1217@naver.com>
2022-10-28 15:17:11 +0900

    feat : List length not match 2 Exception

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+        try{
+            checkLength(pobi,crong);
+
+        } catch (Exception e){
+            answer=-1;
+        }
         return answer;
     }
+
+
+    public static void checkLength(List<Integer> pobi,List<Integer> crong) throws Exception{
+        if (pobi.size()!=2||crong.size()!=2) throw new Exception("페이지 불일치");
+    }
+
 }
