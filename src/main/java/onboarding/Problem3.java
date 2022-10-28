39fbe40b734a81d77747dc4574a52fd9eea5c614
KTH-96 <taehyun960@gmail.com>
2022-10-28 17:29:28 +0900

    feat(Problem3): 주어진 자연수까지 박수를 몇번 쳤는지 계산하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,20 @@ public class Problem3 {
 	 */
 	public static int solution(int number) {
 		int answer = 0;
+		for (int i = 1; i <= number; i++) {
+			answer += getClapCount(i);
+		}
 		return answer;
+	}
+
+	private static int getClapCount(int num) {
+		int count = 0;
+		while (num != 0) {
+			if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+				count++;
+			}
+			num /= 10;
+		}
+		return count;
 	}
 }
