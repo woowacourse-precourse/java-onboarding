6347dc5e0057f5ccd2541de6c958c077c412b5c4
mywnajsldkf <mywnajsldkf@gmail.com>
2022-10-28 23:43:41 +0900

    feat : 유효한 페이지 리스트인지 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!isValidPage(pobi) || !isValidPage(crong)) {
+            answer = -1;
+        }
+
         return answer;
+    }
+
+    private static boolean isValidPage(List<Integer> page) {
+        if (page.get(1) - page.get(0) != 1) {
+            return false;
+        }
+        if (page.get(0) % 2 == 0) {
+            return false;
+        }
+        return !page.contains(1) || !page.contains(400);
     }
 }
