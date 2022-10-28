2baf273384340b3ab5b7b4eee01d56e0e3e60919
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-10-29 01:42:26 +0900

    Problem3 예외처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+    public  static  void catchException(int num){
+        assert (num>=1);
+        assert (num<=10000);
+    }
+
     public static int solution(int number) {
+        catchException(number);
         int answer = 0;
         return answer;
     }
