ae0b48283a0fdae1e6faaae8ca5288c40baeecd6
KSH <tkdgns543@gmail.com>
2022-10-29 00:23:40 +0900

    feat(Problem4): 주어진 소문자를 기능 구현 조건대로 치환하는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,9 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /* 주어진 소문자를 기능 구현 조건대로 치환하는 메서드 */
+    private static char lowerCaseReplace(char c) {
+        return (char)('z'+'a'-c);
+    }
 }
