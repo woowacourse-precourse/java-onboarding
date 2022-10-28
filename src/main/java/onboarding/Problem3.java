b1dec67a20f61ea6ed8eff4507ecbc2a6db87c03
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 16:29:35 +0900

    feat: [3번] 입력받은 숫자까지 게임을 진행하며 박수를 쳐야하는 횟수의 총합을 구하는 기능 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -38,6 +38,12 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            List<Integer> digitList = splitDigit(i);
+
+            answer += getClapCount(digitList);
+        }
+
         return answer;
     }
 }
