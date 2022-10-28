58a219b74bd503b908e03438189a50636316a081
MIN-04 <kimji_0424@naver.com>
2022-10-28 14:28:50 +0900

    add Problem2 기능 구현 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem2 {
+    /**
+     * 기능 구현
+     * 1. 입력받은 문자열을 배열로 만든다.
+     * 2. 배열을 돌며 연속한 문자들은 ' '로 치환한다.
+     * 3. 배열을 문자열로 붙인 다음, ' '은 없앤 결과값을 리턴한다.
+     */
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
