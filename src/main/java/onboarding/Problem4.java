d5bd8ac8df76f7816abcf41fe1090db2d567634b
Soomin Lim <augustth511@gmail.com>
2022-10-28 23:47:19 +0900

    기능 추가: 요구사항에 맞게 문자를 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (Character.isAlphabetic(ch))
+                sb.append(reverseChar(ch));
+            else
+                sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 요구사항에 맞게 문자 ch를 변환하여 변환된 문자를 리턴
+     */
+    public static char reverseChar(char ch) {
+        if (Character.isUpperCase(ch))
+            return (char)('A' + 25 - (ch - 'A'));
+        else
+            return (char)('a' + 25 - (ch - 'a'));
     }
 }
