e915608ad8acf65bd7025de1bcdd334328bf334b
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-28 10:47:37 +0900

    문제 6 - 리스트 중복 제거 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		// 중복 여부 판단 문자열 선정 (이중 for문)
+		// 중복 여부 확인 (contain 함수)
+		// 중복된 닉네임의 이메일을 리스트에 추가
+		// 리스트 오름차순 정렬  (sort 함수)
+		// 리스트 중복 제거 (set 함수 이용)
+		List<String> answer = List.of("answer");
+		return answer;
+	}
+
+	static List<String> removeListDuplication(List<String> list) {
+		return list.stream().distinct().collect(Collectors.toList());
+	}
 }
