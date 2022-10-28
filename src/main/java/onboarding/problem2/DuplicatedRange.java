bf5bbcaca482f8ed606f058c42dfb74afebab674
ysng_ysng <yeasung67@gmail.com>
2022-10-28 12:49:18 +0900

    modified: problem1, problem2 class file directory modified

---
+++ src/main/java/onboarding/problem2/DuplicatedRange.java
@@ -0,0 +1,11 @@
+package onboarding.problem2;
+
+public class DuplicatedRange {
+	public int start;
+	public int end;
+
+	public DuplicatedRange(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+}
