8aaf658c48d3a877c902825e7f3be190e0f2bae3
yxnxx <jh4rgh0@gmail.com>
2022-10-28 17:07:39 +0900

    feat: add checking page continuation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,11 +6,14 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         answer = 0;
-        if(isPageInvalid(pobi) || isPageInvalid(crong)) answer = -1;
-
+        if(isPageInvalid(pobi) || isPageInvalid(crong)) return -1;
+        if (isPageNotContinuous(pobi) || isPageNotContinuous(crong)) return -1;
         return answer;
     }
     public static boolean isPageInvalid(List<Integer> array) {
         return (array.get(0) < 1) || (array.get(0) > 400) || (array.get(1) < 1) || (array.get(1) > 400);
+    }
+    public static boolean isPageNotContinuous(List<Integer> array) {
+        return array.get(0) != array.get(1) -1;
     }
 }
