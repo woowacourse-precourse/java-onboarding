47022af32f519465d81abc73c19aa75bbc5a3dc0
chopinballadeno4 <qo98333@naver.com>
2022-10-28 17:01:38 +0900

    problem 1, function 1 compose cal method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,27 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int calBigNum(int num) {
+        if(num / 10 == 0) {
+            // 한자리 일 때
+            return num;
+        } else if(num / 100 == 0) {
+            // 두자리 일 때
+            int sumNumber = num % 10 + num / 10;
+            int mulNumber = (num % 10) * (num / 10);
+            return Math.max(sumNumber, mulNumber);
+        } else {
+            // 세자리 일 때
+            int sumNumber = num % 10 + num / 10 + num / 100;
+            int mulNumber = (num % 10) * (num / 10) * (num / 100);
+            return Math.max(sumNumber, mulNumber);
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
         return answer;
     }
 }
