ab3192a9669ea7009c4e688d91e2acdc813f9016
jihong <clara5091@naver.com>
2022-10-28 20:22:37 +0900

    problem3-자릿수별로 분리 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,15 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Problem3 {
+    public static List<Integer> splitNumber(int number){
+        return Stream.of(String.valueOf(number).split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
