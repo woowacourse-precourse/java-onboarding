799dca5da00d6d981eb051a2a6dfef2992bf89dc
junho lee <junho1273@gmail.com>
2022-10-28 20:05:53 +0900

    Problem 6 기능목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
+
+//기능목록
+//1.해쉬맵에 모든 닉네임에서 연속되는 두 글자 씩을 잘라 키를 지정하고 값으로 1을 넣는다. 만약 키값이 이미 있다면 1을 더한다.
+//2.해쉬맵의 키값으로 반복문을 돌아 키의 값이 1을 넘어선다면 입력값으로 반복문을 돌아 해쉬맵의 키와 같은 이름을 가진 배열의 이메일을 정답에 집어넣는다
+//3.정렬한다. 끝!
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        ArrayList<String> answer = new ArrayList<>();
+
         return answer;
     }
 }
