3c2f5b8e61ee31bf2ca99f56a08ae569b0726713
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 17:56:45 +0900

    feat: PROBLEM6 이메일 형식 분리 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static class NicknameFilter {
+
+        private static final String EMAIL_FORM = "@email.com";
+
+        private String emailParser(String email) {
+            return email.replaceAll(EMAIL_FORM, "");
+        }
     }
 }
