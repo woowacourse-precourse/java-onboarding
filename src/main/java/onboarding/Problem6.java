02f871bdbdaf1f964b42591629d0471d4850cb51
greeng00se <thegreengoose7@gmail.com>
2022-10-28 15:22:12 +0900

    feat(Problem6): 허용된 이메일 도메인이 아닌경우 아래 로직에 포함하지 않는 로직 추가
    - 이메일을 @ 기준으로 분리한다.
    - 이메일을 검증한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,18 +1,39 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
-    private static final int DOMAIN_INDEX = 1;
     private static final String VALID_DOMAIN = "email.com";
+    private static final String EMAIL_AT_SIGN = "@";
+
+    private static final int EMAIL_INDEX = 0;
+    private static final int DOMAIN_INDEX = 1;
+    private static final int NICKNAME_INDEX = 1;
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        HashMap<String, String> separatedNameToEmail = new HashMap<>();
+        HashSet<String> result = new HashSet<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(EMAIL_INDEX);
+            String nickname = form.get(NICKNAME_INDEX);
+            if (!isValidDomain(email)) {
+                continue;
+            }
+        }
+
+        return List.of();
     }
 
-    public static boolean isValidDomain(String email) {
-        String[] separatedEmail = email.split("@");
-        return separatedEmail[DOMAIN_INDEX].equals(VALID_DOMAIN);
+    private static boolean isValidDomain(String email) {
+        String domain = splitEmail(email)[DOMAIN_INDEX];
+        return domain.equals(VALID_DOMAIN);
+    }
+
+    private static String[] splitEmail(String email) {
+        return email.split(EMAIL_AT_SIGN);
     }
 }
