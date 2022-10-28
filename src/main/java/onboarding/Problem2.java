067907a052b43c66b120ce79c8a4e7a73775b4bc
wonseokjee <wsji9404@gmail.com>
2022-10-28 11:47:58 +0900

    221027 prblm2 - 1

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+//      cryptogram 받아서 단일 문자 list에 넣기
+        ArrayList<String> lst = new ArrayList<String>();
+
+        for (String s : cryptogram.split("")) {
+            lst.add(s);
+        }
+        String answer = sameLang(lst);;
         return answer;
     }
+
 }
