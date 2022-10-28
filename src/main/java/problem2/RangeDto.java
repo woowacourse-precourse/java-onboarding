fa5fd1fd22aa6c260affa43be0825386a6fd2964
parkmuhyeun <pjhg410@gmail.com>
2022-10-28 19:36:40 +0900

    feat: [Problem2] 연속된 중복 문자 한번에 제거

---
+++ src/main/java/problem2/RangeDto.java
@@ -0,0 +1,19 @@
+package problem2;
+
+public class RangeDto {
+    private int start;
+    private int end;
+
+    public RangeDto(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
