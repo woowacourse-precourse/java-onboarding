17811b45e0d0a08706d1673c378c433d92b3cace
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-28 17:11:40 +0900

    Problem6

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
 
         final int[] typeOfMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
@@ -21,12 +20,8 @@ public class Problem5 {
         //큰 돈부터 작은 돈까지 차례대로 계산한다.
         for(int i = 0; i < typeOfMoney.length; i++) {
             int cnt = money / typeOfMoney[i];
-            if(cnt != 0) {
-                answer.add(cnt);
-                money -= cnt*typeOfMoney[i];
-            }else {
-                answer.add(cnt);
-            }
+            answer.add(cnt);
+            money %= typeOfMoney[i];
         }
     }
 }
