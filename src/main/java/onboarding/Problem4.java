f744afd1d1e5a1792f5f39a61c14b28efb92f5aa
koo <rnqudgur@naver.com>
2022-10-29 02:28:41 +0900

    기능요구사항 4번 문제
    기능 분리
    1. 알파벳을 한글자씩 재조합해주는 기능 (solution)
    2. 분리되서 들어오는 알파벳을 청개구리 사전으로 변역해주는 기능 (frogTranslate)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        Problem4 problem4 = new Problem4();
+        
+        for(char alphabet : word.toCharArray()) {
+        	answer += problem4.frogTranslate(alphabet);
+        }//for
+        
         return answer;
-    }
-}
+    }// solution
+    
+    private char frogTranslate(char alphabet) {
+    	if( 'A'<= alphabet && alphabet <='M') return (char)('Z'+('A'-alphabet));
+    	if( 'N'<= alphabet && alphabet <='Z') return (char)('A'+('Z'-alphabet));
+    	if( 'a'<= alphabet && alphabet <='m') return (char)('z'+('a'-alphabet));
+    	if( 'n'<= alphabet && alphabet <='z') return (char)('a'+('z'-alphabet));
+    	return alphabet;
+    }// frogTranslate
+}// end class
