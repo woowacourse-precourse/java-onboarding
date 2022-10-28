cecd71f19927100a08704961b836babdedc25394
Soomin Lim <augustth511@gmail.com>
2022-10-28 18:32:58 +0900

    기능 추가: 연속하는 중복 문자들을 삭제하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        StringBuilder cryptogramSb = new StringBuilder(cryptogram);
+
+        int i = 0, j = 1;
+        for (; j < cryptogramSb.length(); ) {
+            if (cryptogramSb.charAt(i) != cryptogramSb.charAt(j)) {
+                answer = cryptogramSb.substring(0, i+1);
+                i++;
+                j++;
+            }
+            else {
+                cryptogramSb.deleteCharAt(j);
+                cryptogramSb.deleteCharAt(i);
+                i--;
+                j = i + 1;
+            }
+        }
+
+        answer = cryptogramSb.substring(0, i+1);
+
         return answer;
     }
 }
