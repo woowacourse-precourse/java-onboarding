5a8448bb6e02d4772018ac96e5e19899bb563d7f
seoeunbae <enyoupark@gmail.com>
2022-10-28 20:58:01 +0900

    [3] < feature > 369 게임 메소드 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clap = 0;
+
+        for(int i=1 ; i <= number ; i++){
+            String stringNumber = Integer.valueOf(i).toString();
+            System.out.println(stringNumber);
+            if( stringNumber.contains("3") || stringNumber.contains("6") || stringNumber.contains("9") ){
+                String[] splits = stringNumber.split("");
+
+                for( String split : splits ){
+                    System.out.println("split : " + split);
+                    if( split.equals("3") || split.equals("6") || split.equals("9") ){
+                        clap++;
+                    }
+                    System.out.println(" : " + clap);
+                }
+            }
+        }
+        return clap;
     }
 }
