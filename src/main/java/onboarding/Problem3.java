2c94abe2979f9f1acf9ac7aa5ee72368f04c6499
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-28 17:53:28 +0900

    Problem3 기능요구조건 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+
+    // TODO 입력받은 수의 3,6,9 개수를 반환하는 메서드
+    public static int getCountNum(int num) {
+
+        return 0;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
