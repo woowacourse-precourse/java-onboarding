468d0738268779669871665acab93a800e4ddcbd
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 20:33:58 +0900

    feat: Add getRecommendPoint in Problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     private static List<String> createFriendList(String user, List<List<String>> friends){
@@ -16,6 +14,23 @@ public class Problem7 {
         }
 
         return friendList;
+    }
+
+    private static HashMap<String, Integer> getRecommendPoint(List<String> friendList, List<String> visitorList){
+        HashMap<String, Integer> pointMap = new HashMap<>();
+
+        for(int i = 0; i < friendList.size(); i++){
+            pointMap.put(friendList.get(i), 10);
+        }
+
+        for(int i = 0; i < visitorList.size(); i++){
+            if(pointMap.containsKey(visitorList.get(i)))
+                pointMap.put(visitorList.get(i), pointMap.get(visitorList.get(i)) + 1);
+            else
+                pointMap.put(visitorList.get(i), 1);
+        }
+
+        return pointMap;
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
