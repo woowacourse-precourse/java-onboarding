0ccea3bfb75865646272700410765f55542c16a1
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-10-28 21:24:03 +0900

    feat: [Prob 1] 페이지 번호의 각 자리 숫자의 합 계산 인수로 전달 받은 페이지 번호의 각 자리 숫자의 합을 계산하는 addEachDigitOfPageNum 함수를 구현합니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public int addEachDigitOfPageNum(int pageNum){
+        int sumOfEachDigit=0;
+        while(pageNum>0){
+            sumOfEachDigit+=pageNum%10;
+            pageNum/=10;
+        }
+        return sumOfEachDigit;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
