0a18b2f4906971a33c3fe8d8b3ae2f16871e63fb
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2022-10-28 18:49:02 +0900

    [문제1] 주어진 페이지가 연속된 수인지 확인하는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean isSuccessivePage(List<Integer> pagenumber){
+        if (pagenumber.get(1) - pagenumber.get(0)!=1){
+            return false;
+        }else{
+            return true;
+        }
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(isSuccessivePage(pobi)==false||isSuccessivePage(crong)==false){
+            answer=-1;
+        }
+
+
         return answer;
     }
 }
