28220b20d5faa2a905528d7200beebf3741b5e1a
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-28 09:49:44 +0900

    [문제5] - 제공된 화폐 단위로 거스름 돈 반환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int[] changeMoney(int unit, int money) {
+        return new int[] { money / unit, money % unit };
+    }
+    
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
