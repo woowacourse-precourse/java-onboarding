a3584fe1b4e1415eaf70562d46176f2133c233f3
egoing <dudqk9696@naver.com>
2022-10-28 23:34:14 +0900

    feat : 이름안에 중복 값들이 있는지 구분해주는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,6 +11,7 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+    
     static void inputNames(String form) {
         Set<String> set = new HashSet<>();
         for(int i=0; i<form.length()-1; i++) {
@@ -22,6 +23,7 @@ public class Problem6 {
             checkNames(strings);
         }
     }
+
     static void checkNames(String tmp) {
         if(names.get(tmp) == null) {
             names.put(tmp,"1");
@@ -29,6 +31,20 @@ public class Problem6 {
         else {
             names.put(tmp,"2");
         }
+    }
+
+    static boolean overlapNames(String form) {
+        boolean result = false;
+
+        for(int i=0; i<form.length()-1; i++) {
+            String rem="";
+            rem = rem + form.charAt(i) + form.charAt(i+1);
+            if(names.get(rem).equals("2")) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
 }
