92e75bdc1172afaab5ee9192cdf57cd0273307cc
chaos0103 <lyt1228@naver.com>
2022-10-29 01:48:37 +0900

    feat(Problem3): 숫자에 3, 6, 9 갯수를 카운트하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,28 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int getClapCount(int number) {
+        int count = 0;
+        while (number != 0) {
+            int digitNumber = number % 10;
+            if (isThree(digitNumber) || isSix(digitNumber) || isNine(digitNumber)) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
+
+    private static boolean isThree(int digitNumber) {
+        return digitNumber == 3;
+    }
+
+    private static boolean isSix(int digitNumber) {
+        return digitNumber == 6;
+    }
+
+    private static boolean isNine(int digitNumber) {
+        return digitNumber == 9;
+    }
 }
