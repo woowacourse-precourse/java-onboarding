e17007f617faefba31fe0063d42bc71432d9def4
jeujeon <1132jjw@naver.com>
2022-10-28 19:39:17 +0900

    feat: add Input Exception in Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+    static final int MIN_LENGTH = 1;
+    static final int MAX_LENGTH = 1000;
     public static String solution(String word) {
         String answer = "";
+        if (isValidInput(word)) {
+            answer = "error";
+            return answer;
+        }
         return answer;
+    }
+
+    private static boolean isValidInput(String word) {
+        return (isNotNull(word) && isValidRange(word));
+    }
+
+    private static boolean isValidRange(String word) {
+        return word.length() >= MIN_LENGTH && word.length() <= MAX_LENGTH;
+    }
+
+    private static boolean isNotNull(String word) {
+        return word != null;
     }
 }
