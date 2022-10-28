69c3bc9f60b90ebfa82672de4d4fec71ae286413
jeongwle <dldl4568@naver.com>
2022-10-28 15:45:59 +0900

    [Feat] Problem5 완료  - 큰 금액을 우선으로 지갑에 돈을 담는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,11 +7,13 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         if (!isValidate(money)) {
-            ArrayList<Integer> error = new ArrayList<>();
-            error.add(-1);
-            return error;
+            return Collections.emptyList();
         }
-        List<Integer> answer = Collections.emptyList();
+
+        int[] unit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        List<Integer> answer = new ArrayList<>();
+        getAnswer(money, unit, 0, answer);
         return answer;
     }
 
@@ -20,5 +22,16 @@ public class Problem5 {
      */
     public static boolean isValidate(int money) {
         return (money > 0 && money < 1_000_001);
+    }
+
+    /**
+     * 큰 돈 위주로 지갑에 담는 기능
+     */
+    public static void getAnswer(int money, int[] unit, int idx, List<Integer> answer) {
+        if (idx == unit.length) {
+            return;
+        }
+        answer.add(money / unit[idx]);
+        getAnswer(money % unit[idx], unit, idx + 1, answer);
     }
 }
