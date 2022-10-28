b14cfb164cceaf8dff01e898527ef74f47418ba4
Dongmin Kim <kimdongmin68@naver.com>
2022-10-28 17:16:41 +0900

    feat: Add split nickname method

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer;
+        Set<String> splitName;
+        Set<String> splitNameAll = new HashSet<>();
+        Set<String> getDuplicateEmail = new HashSet<>();
+
+        for(List<String> trainee : forms){
+            String email = trainee.get(0);
+            String name = trainee.get(1);
+
+            splitName = getSplitName(name);
+
+        }
+
+        answer = new ArrayList<>(getDuplicateEmail);
+        Collections.sort(answer);
         return answer;
+    }
+
+    public static Set<String> getSplitName(String name){
+        Set<String> splitWords = new HashSet<>();
+        for (int i=0;i<name.length()-1;i++){
+            String splitWord = name.substring(i,i+2);
+            splitWords.add(splitWord);
+        }
+        return splitWords;
     }
 }
