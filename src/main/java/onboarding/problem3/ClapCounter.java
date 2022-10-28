0b65c9b86f90d339da3df1522e3653823f5cf8e9
situodys <situodys@gmail.com>
2022-10-28 11:24:12 +0900

    feat(문제3): 숫자가 3 또는 6 또는 9인지 확인

---
+++ src/main/java/onboarding/problem3/ClapCounter.java
@@ -0,0 +1,23 @@
+package onboarding.problem3;
+
+public class ClapCounter {
+    private static final int CONSTANT_THREE = 3;
+    private static final int CONSTANT_SIX = 6;
+    private static final int CONSTANT_NINE = 9;
+
+    private int clapCount;
+    private int maxRangeValue;
+
+    public ClapCounter(int maxRangeValue) {
+        this.clapCount=0;
+        this.maxRangeValue = maxRangeValue;
+    }
+
+    public int count() {
+        return 0;
+    }
+    
+    private boolean isSatisfy369Rules(int digit) {
+        return digit == CONSTANT_THREE || digit == CONSTANT_SIX || digit == CONSTANT_NINE;
+    }
+}
