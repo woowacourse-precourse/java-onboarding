3a25e45de5acff9403d759f307aaf0e25e8b6d3d
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-10-28 21:28:28 +0900

    feat: [Prob 1] 페이지 번호의 각 자리 숫자의 곱 계산 인수로 전달 받은 페이지 번호의 각 자리 숫자의 곱을 계산하는 multiplyEachDigitOfPageNum 함수를 구현합니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,6 +11,14 @@ class Problem1 {
         }
         return sumOfEachDigit;
     }
+    public int multiplyEachDigitOfPageNum(int pageNum){
+        int productOfEachDigit=1;
+        while(pageNum>0){
+            productOfEachDigit*=pageNum%10;
+            pageNum/=10;
+        }
+        return productOfEachDigit;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
