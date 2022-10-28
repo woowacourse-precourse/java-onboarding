6f5c7cb031f79c3176a81d8da87588bc1f1c8593
jthugg <jthug@naver.com>
2022-10-29 02:33:34 +0900

    feat. 프로그램 구조 구축
    프로그램 구조 구축 및 필요한 변수, 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -12,7 +13,36 @@ public class Problem7 {
         // 5. 가장 높은 점수 인덱스를 제출 할 리스트에 입력한다
         // 6. 이름 순서대로 제출 할 리스트에 입력하고 친구의 친구 리스트와 점수 리스트에서 해당 인덱스를 삭제한다
         // 7. 5-6을 반복하고 가장 높은 점수가 0이거나 더이상 인덱스가 없을 경우 루프를 탈출하고 리턴
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> friendsList = findMyFriends(user, friends);
+        List<String> stepFriendsList = findStepFriends(user, friends, friendsList);
+        List<Integer> scr = new ArrayList<>();
+
+        friendsList = findMyFriends(user, friends);
+        stepFriendsList = findStepFriends(user, friends, friendsList);
+        scr = cntScore(user, friends, visitors, stepFriendsList);
+
+
+        return sort(stepFriendsList, scr);
+    }
+
+    private static List<String> findMyFriends(String user, List<List<String>> friends) {
+        List<String> friendsList = new ArrayList<>();
+        return friendsList;
+    }
+
+    private static List<String> findStepFriends(String user, List<List<String>> friends, List<String> friendsList) {
+        List<String> stepFriendsList = new ArrayList<>();
+        return stepFriendsList;
+    }
+
+    private static List<Integer> cntScore(String user,List<List<String>>friends, List<String> visitors, List<String> stepFriendsList) {
+        List<Integer> scr = new ArrayList<>();
+        return scr;
+    }
+
+    private static List<String> sort(List<String> stepFriendsList, List<Integer> scr) {
+        List<String> answer = new ArrayList<>();
         return answer;
     }
 }
