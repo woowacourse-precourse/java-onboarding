d648c4f946033bf757f62b2322c55f394b623431
LHS-11 <ghktjq1119@naver.com>
2022-10-28 16:20:45 +0900

    Problem7 : 1-2 예외 체크 (friends 길이 체크)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import onboarding.problem7.NotMatchingFriendSize;
 import onboarding.problem7.NotMatchingUserLength;
 
 import java.util.Collections;
@@ -9,20 +10,33 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
+        int userLength = user.length();
+        int friendsSize = friends.size();
+
         //1. 예외사항
         //- user 길이 1 이상 30 이하인지 체크
-        if(isNotMatchingUserLength(user.length())){
+        if(isNotMatchingUserLength(userLength)){
             throw new NotMatchingUserLength("사용자의 길이가 맞지 않습니다.");
         }
 
         //- friends 길이 1 이상 10000 이하인지 체크
+        if(isNotMatchingFriendsSize(friendsSize)){
+            throw new NotMatchingFriendSize("친구 관계 정보의 길이가 맞지 않습니다.");
+        }
+
         //- friends 의 각 원소의 길이가 2인지 체크
+
+
         //- 아이디 길이 1 이상 30 이하인지 체크
         //- visitors 길이 0 이상 10000 이하인지 체크
         //- 사용자 아이디 소문자인지 체크
 
 
         return answer;
+    }
+
+    private static boolean isNotMatchingFriendsSize(int size) {
+        return size<1 || size>10000;
     }
 
     private static boolean isNotMatchingUserLength(int length) {
