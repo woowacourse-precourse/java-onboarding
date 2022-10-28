e727d04be9a55d186183250d9d9126cfeffa2e5a
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-29 01:16:17 +0900

    [Problem7] user의 친구목록 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
@@ -14,6 +16,28 @@ public class Problem7 {
      * 5. 추천인 5명 뽑기
      * 6. 이미 친구인 사이 삭제
      */
+
+    /**
+     * 1. user의 친구목록
+     * @param user : user 아이디
+     * @param friends : 친구 관계
+     * @return : user의 친구목록
+     */
+    static List<String> findUserFriends(String user, List<List<String>> friends){
+        List<String> userFriends = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            String id1 = friend.get(0);
+            String id2 = friend.get(1);
+
+            // 친구 관계에 user 아이디가 존재하면 user와 친구 관계
+            if (id1.equals(user)) userFriends.add(id2);
+            if (id2.equals(user)) userFriends.add(id1);
+        }
+
+        return userFriends;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
