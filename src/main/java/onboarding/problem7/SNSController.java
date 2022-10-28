a691e4a8686e16c4d1b502385e2f6726b1afce16
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-29 01:59:37 +0900

    refactor(problem7): RecommendPoint 를 upper class로 전환

--- src/main/java/onboarding/problem7/SNSController.java
+++ src/main/java/onboarding/problem7/SNSController.java
@@ -132,35 +132,4 @@ public class SNSController {
         users.put(userName, user);
     }
 
-    private class RecommendPoint implements Comparable<RecommendPoint>{
-
-        private final String name;
-        private int point;
-
-        public RecommendPoint(String name){
-            this.name = name;
-            point = 0;
-        }
-
-        public void addPoints(int point){
-            this.point += point;
-        }
-
-        public int getPoint(){
-            return point;
-        }
-
-        public String getName(){
-            return name;
-        }
-
-        @Override
-        public int compareTo(RecommendPoint o) {
-            if(this.point == o.point){
-                return this.name.compareTo(o.getName());
-            }
-            return -(this.point - o.getPoint());
-        }
-    }
-
 }
