777c97844878367de521de3668c1c31e48d02a82
phobyjun <phobyjun@gmail.com>
2022-10-28 21:22:35 +0900

    [PROBLEM2] ADD: FEATURE1. 중복 문자 유무 및 시작 및 끝 지점 확인 함수

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static List<Integer> reapeatedNum(String cryptogram) {
+        // 중복 문자 체크를 위한 변수 선언
+        int hasRepeatedNum = -1;
+        int beginRepeatedNum = -1;
+        int endRepeatedNum = -1;
+
+        // 중복 문자 유무 확인
+        boolean checkFirst = true;
+
+        char beforeChar = cryptogram.charAt(0);
+        for (int i = 1; i < cryptogram.length(); i++) {
+            char nowChar = cryptogram.charAt(i);
+
+            if (nowChar == beforeChar && checkFirst) {
+                // 중복 문자 처음 발견 시 시작 지점 저장
+                hasRepeatedNum = 1;
+                beginRepeatedNum = i - 1;
+                checkFirst = false;
+            } else if ((nowChar != beforeChar) && !checkFirst) {
+                // 중복 문자 발견 및 연결된 문자 다를 시 끝 지점 저장
+                endRepeatedNum = i - 1;
+                break;
+            } else if ((i == cryptogram.length() - 1) && !checkFirst) {
+                // 중복 문자 발견 및 문자열 끝까지 탐색 완료 시 끝 지점 저장
+                endRepeatedNum = i;
+                break;
+            }
+            beforeChar = nowChar;
+        }
+
+        return List.of(hasRepeatedNum, beginRepeatedNum, endRepeatedNum);
     }
 }
