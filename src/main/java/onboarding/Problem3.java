532217941cfd1933b2def92549b3bdba26ed0771
recicoco068 <gkapf838@gmail.com>
2022-10-28 21:44:49 +0900

    feat : Problem3 각 자리수에 3, 6, 9가 몇 개인지 세는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int count369(int num) {
+        int cnt = 0;
+        while (num > 0) {
+            int n = num % 10;
+            if (n == 3 || n == 6 || n == 9)
+                cnt++;
+            num /= 10;
+        }
+        return cnt;
+    }
 }
