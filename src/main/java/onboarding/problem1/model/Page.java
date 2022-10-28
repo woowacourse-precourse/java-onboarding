fab8c4743a613b7d578d6db36ec465d1b2641ef0
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-28 17:18:14 +0900

    feat : 모델의 필드 정의

--- src/main/java/onboarding/problem1/model/Page.java
+++ src/main/java/onboarding/problem1/model/Page.java
@@ -1,4 +1,13 @@
 package onboarding.problem1.model;
 
 public class Page {
+	private int page;
+
+	public Page(int page) {
+		this.page = page;
+	}
+
+	public int getPage() {
+		return page;
+	}
 }
--- src/main/java/onboarding/problem1/model/Player.java
+++ src/main/java/onboarding/problem1/model/Player.java
@@ -1,4 +1,15 @@
 package onboarding.problem1.model;
 
 public class Player {
+
+	private Pages pages;
+	private Score score;
+
+	public Pages getPages() {
+		return pages;
+	}
+
+	public Score getScore() {
+		return score;
+	}
 }
