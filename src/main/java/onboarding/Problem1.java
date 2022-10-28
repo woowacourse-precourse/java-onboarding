275c864041604f6d26887717d67155e9f7a44630
Choi-JJunho <junho5336@gmail.com>
2022-10-28 21:55:59 +0900

    docs(문제1): 기능목록 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,15 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 📚 기능 목록
+ *
+ * 1. 숫자의 합을 구하는 기능 : `getPageSum()`
+ * 2. 숫자의 곱을 구하는 기능 : `getPageMultiple()`
+ * 3. 점수를 비교하는 기능 : `getMaxScore()`
+ * 4. 유저의 점수를 구하는 기능 : `getUserScore()`
+ * 5. Solution
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
