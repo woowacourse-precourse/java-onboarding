260af1062cc8f69adc372b62ceaf5006ec2e96f6
Jaeyeop-Jung <jjy5349@naver.com>
2022-10-28 14:21:17 +0900

    feat: SNS를 초기화하고, 친구 추천을 받는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
 
+import onboarding.problem7.Recommender;
+import onboarding.problem7.Sns;
+
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		Sns.init(friends, visitors);
+		return Recommender.recommendByUserName(user, visitors);
+	}
 }
