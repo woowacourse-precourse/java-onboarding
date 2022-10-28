35ff6bf9b47959589b8a5f36bd55e23a3879d468
ihn <ch200205@naver.com>
2022-10-28 13:58:03 +0900

    Problem2 암호문에서 중복이 되는 문자가 없도록 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+
+        // 암호문에서 중복되는 문자가 없도록 삭제한다.
+        for (char c : cryptogram.toCharArray()) {
+            if(!stack.isEmpty()) {
+                char prev = stack.peek();
+                if(prev == c) {
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(c);
+        }
+
         return answer;
     }
+
 }
