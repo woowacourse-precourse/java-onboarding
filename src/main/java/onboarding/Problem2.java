81741ea55953eecbba3ddd07204bf6f3bd60c278
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-29 00:40:43 +0900

    [feat] Problem2 암호문 해독

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,10 +5,11 @@ import java.util.List;
 public class Problem2 {
     public static String deleteRepeation(String crypt){
         String deleteCrypt = crypt;
-
+        if (crypt.equals("") || crypt.charAt(0) == crypt.charAt(1)) return "";
         int i = 0;
         StringBuilder temp = new StringBuilder();
         while(i<deleteCrypt.length()){
+
             if((i+2) < deleteCrypt.length() && deleteCrypt.charAt(i) == deleteCrypt.charAt(i+1))
             {
                 i += 2;
@@ -25,7 +26,14 @@ public class Problem2 {
 
     public static String solution(String cryptogram) {
         String answer = "answer";
-
+        String deleteCrypt  = deleteRepeation(cryptogram);
+        while(true)
+        {
+            if(deleteCrypt.equals(cryptogram)) break;
+            cryptogram = deleteCrypt;
+            deleteCrypt = deleteRepeation(cryptogram);
+        }
+        answer = cryptogram;
         return answer;
     }
 
