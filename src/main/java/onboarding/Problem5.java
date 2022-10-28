25e1352a77dc80ae6e93aa1b8d066335ec1a1ea9
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 16:50:07 +0900

    feat: [5번] 주어진 금액의 화폐 구성 구하기 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    /** 주어진 금액의 화폐 구성을 반환하는 함수 */
+    private static List<Integer> getCureencyCombination(int money) {
+        return Collections.emptyList();
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
