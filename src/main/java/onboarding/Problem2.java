0a67832c110a068879ad8a18afd0d6b3bacedf87
stk346 <stk346@gamil.com>
2022-10-28 15:15:11 +0900

    feat. 중복된 문자 찾아서 제거하는 메소드 구현 완료.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,27 @@
 package onboarding;
 
 public class Problem2 {
+
+    private static String findRepeatedChar(String cryptogram) {
+        boolean flag = false;
+        int lidx = 0;
+        int ridx = 0;
+        for (int i=1; i<=cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i-1)) {
+                char repeatedChar = cryptogram.charAt(i);
+                lidx = i-1;
+                flag = true;
+            if ((flag) & (cryptogram.charAt(i) != cryptogram.charAt(i-1))) {
+                ridx = i;
+                break;
+                }
+            }
+        }
+        if (flag) {
+            cryptogram = cryptogram.substring(lidx, ridx);
+        }
+        return cryptogram;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
