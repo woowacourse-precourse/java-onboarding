1e26a624ee1e71fa1ef1118b51c1748bddd7cd27
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-28 17:14:54 +0900

    221028 Feat: P1 - 기능4 추가, 테스트 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static final int pobiWin = 1;
+    public static final int crongWin = 2;
+    public static final int draw = 0;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
@@ -17,20 +21,30 @@ class Problem1 {
         if(pRightPage - pLeftPage != 1 || cRightPage - cLeftPage != 1)
             return -1;
 
+        //3.각 페이지 자릿수의 합과 곱을 구한다.
         int pMax = Math.max(getSum(pRightPage), Math.max(getSum(pLeftPage),
                 Math.max(getMultiple(pRightPage), getMultiple(pLeftPage))));
 
         int cMax = Math.max(getSum(cRightPage), Math.max(getSum(cLeftPage),
                 Math.max(getMultiple(cRightPage), getMultiple(cLeftPage))));
 
-        //
+        //4.최댓값을 비교하고 게임 결과를 리턴한다.
+        if(pMax > cMax)
+            answer = pobiWin;
+
+        if(pMax < cMax)
+            answer = crongWin;
+
+        if(pMax == cMax)
+            answer = draw;
+
         return answer;
     }
 
     public static int getSum(int num) {
         int sum = 0;
 
-        while(num / 10 > 0){
+        while(num > 0){
             sum += (num % 10);
             num = num / 10;
         }
@@ -41,7 +55,7 @@ class Problem1 {
     public static int getMultiple(int num) {
         int mul = 1;
 
-        while(num / 10 > 0){
+        while(num > 0){
             mul *= (num % 10);
             num = num / 10;
         }
