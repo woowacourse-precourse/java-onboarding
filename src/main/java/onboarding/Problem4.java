e8bab866570ca2ff99de6c46d2bb49d20ff3d0a0
seoeunbae <enyoupark@gmail.com>
2022-10-28 21:31:25 +0900

    [4] < feature > 제한 사항 처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,8 +5,10 @@ public class Problem4 {
 
     public static String solution(String word) {
         String answer = "";
-
         char[] words = word.toCharArray();
+
+        validateCase(word);
+
         for(int i=0 ; i < words.length ; i++){
             if( (byte) words[i] == 32){  //공백 유지
                 continue;
@@ -20,5 +22,9 @@ public class Problem4 {
         answer = String.valueOf(words);
 
         return answer;
+    }
+
+    public static void validateCase(String word){
+        if( word.length() < 1 || word.length() > 1000) throw new RuntimeException("문자열의 길이가 적합하지 않습니다.");
     }
 }
