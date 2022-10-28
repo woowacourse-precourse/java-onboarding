fe5caf15b26ceb44d0a2402947c2058e740b78a8
EUNJEONGMUN <munej26@gmail.com>
2022-10-28 10:44:40 +0900

    fix: 필요 없는 코드 삭제 - 이메일과 이름 key, value로 저장하는 코드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,11 +6,9 @@ public class Problem6 {
     final static int EMAIL = 0;
     final static int NAME = 1;
     public static List<String> solution(List<List<String>> forms) {
-        HashMap<String, String> contacts = new HashMap<>();
         HashMap<String, Integer> combinationCount = new HashMap<>();
         for (int i=0; i<forms.size(); i++) {
             List<String> form = forms.get(i);
-            contacts.put(form.get(EMAIL),form.get(NAME));
             HashSet<String> combinations = getCombinations(form.get(NAME));
             for (String combination : combinations) {
                 combinationCount.put(combination, combinationCount.getOrDefault(combination, 0)+1);
