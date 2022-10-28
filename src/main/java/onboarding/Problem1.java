113263859fc514cb27ad2b039a22119587e7307d
fantasyshrimp <fantasy.shrimp@gmail.com>
2022-10-28 23:27:35 +0900

    do something

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,121 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        GameData pobiData = new GameData(pobi);
+        GameData crongData = new GameData(crong);
+
+        answer = PageNumberGame.PlayGame(pobiData,crongData);
+
         return answer;
+    }
+}
+
+class GameData {
+    private List<Integer> gameData;
+
+    GameData(List<Integer> gameData) {
+        this.gameData = gameData;
+    }
+
+    private boolean verifyGameData() {
+
+        if(gameData.size() > 2 ){
+            return false;
+        }
+
+        if((gameData.get(0) % 2) != 1) {
+            return false;
+        }
+
+        if((gameData.get(1) - gameData.get(0)) != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isValid() {
+        return verifyGameData();
+    }
+
+    public int calculateScore() {
+        int leftPageNumber = gameData.get(0);
+        int rightPageNumber = gameData.get(1);
+
+        int[] arrayForLeft = divideEachDigit(leftPageNumber);
+        int[] arrayForRight = divideEachDigit(rightPageNumber);
+
+        int leftScore = getBiggerScore(
+                addEachDigit(arrayForLeft),multiplyEachDigit(arrayForLeft));
+        int rightScore = getBiggerScore(
+                addEachDigit(arrayForRight),multiplyEachDigit(arrayForRight));
+
+        return getBiggerScore(leftScore,rightScore);
+    }
+
+    public int getBiggerScore(int ScoreA, int ScoreB) {
+        if (ScoreA > ScoreB) {
+            return ScoreA;
+        }
+        return ScoreB;
+    }
+
+    private int[] divideEachDigit(int number) {
+        int n = number;
+        int digit = (int)Math.log10(n) + 1;
+        int[] arrayEachDigit = new int[digit];
+
+        for (int i = 0; i < digit; i++) {
+            arrayEachDigit[i] = (n % 10);
+            n /= 10;
+        }
+        return arrayEachDigit;
+    }
+
+    private int addEachDigit(int[] arrayEachDigit) {
+        int dst = 0;
+        for (int a : arrayEachDigit) {
+            dst += a;
+        }
+        return dst;
+    }
+
+    private int multiplyEachDigit(int[] arrayEachDigit) {
+        int dst = 1;
+        for (int a : arrayEachDigit) {
+            dst *= a;
+        }
+        return dst;
+    }
+}
+final class GameResult {
+    public static final int DATA_ERR = -1;
+    public static final int DRAW = 0;
+    public static final int WINNER_POBI = 1;
+    public static final int WINNER_CRONG = 2;
+}
+
+class PageNumberGame {
+
+    public static int PlayGame(GameData playerOne, GameData playerTwo) {
+
+        if(!(playerOne.isValid() && playerTwo.isValid())) {
+            return GameResult.DATA_ERR;
+        }
+
+        int playerOneScore = playerOne.calculateScore();
+        int playerTwoScore = playerTwo.calculateScore();
+
+        if(playerOneScore > playerTwoScore) {
+            return GameResult.WINNER_POBI;
+        }
+
+        if(playerOneScore < playerTwoScore) {
+            return GameResult.WINNER_CRONG;
+        }
+
+        return GameResult.DRAW;
+
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,99 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        BrownCiphertext Ciphertext = new BrownCiphertext(cryptogram);
+        answer = BrownCiphertextDecoder.decode(Ciphertext);
         return answer;
+    }
+}
+
+class BrownCiphertext {
+    private final String ciphertext;
+
+    BrownCiphertext(String ciphertext) {
+        this.ciphertext = ciphertext;
+    }
+
+    public String[] divideCiphertext() {
+        return ciphertext.split("");
+    }
+
+}
+
+class IndexLinker {
+    public int before = -1;
+    public int next = 1;
+
+}
+
+class BrownCiphertextDecoder {
+    private static IndexLinker[] linkers;
+    private static int startIndex;
+
+    public static String decode(BrownCiphertext ciphertext) {
+        String[] arrayChar = ciphertext.divideCiphertext();
+        linkers = new IndexLinker[arrayChar.length];
+        for (int i = 0; i < arrayChar.length; i++) {
+            linkers[i] = new IndexLinker();
+        }
+
+        startIndex = 0;
+
+        excludeDuplicate(arrayChar);
+        return makeCode(arrayChar);
+    }
+
+    private static void excludeDuplicate(String[] alphabets){
+        int progressIndex = 0;
+
+        while (progressIndex + linkers[progressIndex].next < alphabets.length) {
+            progressIndex = checkDuplicate(alphabets,progressIndex);
+        }
+    }
+
+    private static int checkDuplicate(String[] alphabets, int progressIndex){
+
+        int indexDelta = linkers[progressIndex].next;
+
+        while (progressIndex + indexDelta < alphabets.length &&
+                alphabets[progressIndex]
+                        .equals(alphabets[progressIndex + indexDelta])) {
+            indexDelta++;
+        }
+
+        if (indexDelta == linkers[progressIndex].next) {
+            return progressIndex + indexDelta;
+        }
+
+        if (progressIndex > 0 &&
+                progressIndex + indexDelta < linkers.length) {
+            linkers[progressIndex + linkers[progressIndex].before].next += indexDelta;
+            linkers[progressIndex + indexDelta].before -= indexDelta;
+
+            return progressIndex + linkers[progressIndex].before;
+        }
+
+        if (progressIndex + linkers[progressIndex].before < 0) {
+            progressIndex = indexDelta;
+            startIndex = progressIndex;
+        }
+
+        if (progressIndex + indexDelta >= linkers.length) {
+            linkers[progressIndex + linkers[progressIndex].before].next += indexDelta;
+            progressIndex++;
+        }
+
+        return progressIndex;
+    }
+
+    private static String makeCode(String[] alphabets) {
+        String decodedCode = "";
+
+        while (startIndex < alphabets.length) {
+            decodedCode += alphabets[startIndex];
+            startIndex += linkers[startIndex].next;
+        }
+
+        return decodedCode;
     }
 }
