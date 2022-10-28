26eec8bb057ef360f4afb03a68492cc41747321b
shpark30 <seohyeonpark30.gmail.com>
2022-10-29 00:22:53 +0900

    fix(Problem3.java): debug "count369" method
    input number가한자리 수 일대도 유효하도록
    while문 조건을 number >= 10에서 number >= 3으로 수정
    
    다음 자리 수를 구하는 로직 오류 수정.
    number /= number;에서  number /= 10;

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,7 +8,7 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            answer += count369(answer);
+            answer += count369(i);
         }
         return answer;
     }
@@ -18,10 +18,10 @@ public class Problem3 {
         List<Integer> condition = new ArrayList<>(Arrays.asList(3, 6, 9));
         int count = 0;
         int remainder = 0;
-        while (number >= 10) {
+        while (number >= 3) {
             // number를 10으로 나눈 나머지 remainder가 3, 6, 9에 해당하면 count한다.
             remainder = number % 10;
-            number /= number;
+            number /= 10;
             if (condition.indexOf(remainder) != -1) {
                 count += 1;
             }
