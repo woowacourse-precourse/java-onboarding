8ed7e3d91a8686cec0219005d0de01c8746b6827
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-28 21:29:33 +0900

    [Problem 5] 최소 지폐로 입력받은 돈을 구성하여 리스트로 반환하는 함수

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -29,4 +29,18 @@ public class Problem5 {
         return List.of(50000,10000,5000,1000,500,100,50,10,1);
     }
 
+    private static List<Integer> getLeastBills(int money, List<Integer> moneyList){
+        ArrayList<Integer> result = new ArrayList<>();
+        Map<Integer,Integer> billStorage = new HashMap<>();
+        moneyList.iterator().forEachRemaining(a -> billStorage.put(a,0));
+        while(money!=0){
+            for(int i=0; i<moneyList.size(); i++){
+                int bill = moneyList.get(i);
+                billStorage.put(bill, money/bill);
+                money%=bill;
+            }
+        }
+        billStorage.keySet().stream().sorted(Comparator.reverseOrder()).forEach(a-> result.add(billStorage.get(a)));
+        return result;
+    }
 }
