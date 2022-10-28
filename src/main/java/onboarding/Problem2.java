e9c95c5614a5f72ac2ab09576ef1cf38eec8dd42
syeonbot <asdf748596123@gmail.com>
2022-10-29 03:20:26 +0900

    feat: problem2 연속된 문자를 없애는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,35 @@
 package onboarding;
 
 public class Problem2 {
+    public static final char NOT_LOWER_CASE = '0';
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+        Stack<Character> cryptogramStack = stringToStack(cryptogram);
+        Stack<Character> filteredStack = filterDuplicatedChar(cryptogramStack);
+
+        return stackToString(filteredStack);
+    }
+    public static Stack<Character> filterDuplicatedChar(Stack<Character> originalStack) {
+        Stack<Character> resultStack = new Stack<>();
+        char duplicateChar = NOT_LOWER_CASE;
+
+        while (!originalStack.isEmpty()) {
+            Character pop = originalStack.pop();
+
+            if (!resultStack.isEmpty() && pop == resultStack.peek()) {
+                duplicateChar = pop;
+                resultStack.pop();
+                continue;
+            }
+
+            if (pop != duplicateChar) {
+                resultStack.add(pop);
+                duplicateChar = NOT_LOWER_CASE;
+            }
+        }
+
+        return resultStack;
     }
     public static Stack<Character> stringToStack(String string) {
         Stack<Character> stack = new Stack<>();
