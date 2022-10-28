d3d6eb493d9e79c2d81d3c700c0734f6760b5dc6
lzlkolo <lzlko1209@gmail.com>
2022-10-28 14:41:11 +0900

    PROBLEM4.변환 함수 구현 및 변환 문자열 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     static boolean isCheck(String word) {
         if(word.length()<1 || word.length()>1000) {
@@ -8,15 +10,34 @@ public class Problem4 {
         return false;
     }
 
-    public static String solution(String word) {
-        String answer="";
+    static String changeWord(String word) {
+        String changeWord ="" ;
+        HashMap<Character, Integer> wordValue = new HashMap<>();
 
-        if(isCheck(word)) {
-            answer = "문자열의 길이가 잘못되었습니다.";
+        wordValue.put('A', 25);
+        for(int i=66; i<=90; i++) {
+            wordValue.put((char)i, wordValue.get((char)(i-1))-2);
         }
 
+        for(char c : word.toCharArray()) {
+            if(97 <= c && c <= 122) {
+                c = (char) (c-32);
+                changeWord += (char) (c + wordValue.get(c) + 32);
+            }
+            else if(65 <=c && c <= 90) {
+                changeWord += (char) (c + wordValue.get(c));
+            }
+            else changeWord += c;
+        }
+        return changeWord;
+    }
+
+    public static String solution(String word) {
+        if(isCheck(word)) {
+            return "문자열의 길이가 잘못되었습니다.";
+        }
         word = word.replaceAll("[^a-zA-Z]", " ");
-        return answer;
+        return changeWord(word);
     }
 }
 
