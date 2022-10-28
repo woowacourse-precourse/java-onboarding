5709c55430fd1c24cdf9a5c840f67277eac382c4
situodys <situodys@gmail.com>
2022-10-28 13:44:37 +0900

    feat(문제4): 알파벳 변환기 생성

---
+++ src/main/java/onboarding/problem4/AlphabetConverter.java
@@ -0,0 +1,13 @@
+package onboarding.problem4;
+
+public class AlphabetConverter {
+
+    private String targetWord;
+    private StringBuilder convertedWord;
+
+    public AlphabetConverter(String targetWord) {
+        this.targetWord = targetWord;
+        convertedWord = new StringBuilder();
+    }
+
+}
