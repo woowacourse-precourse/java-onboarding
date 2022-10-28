5b93e1af7988c1348928596b13bbcf43ff9ce387
Kangbin <hz0819@naver.com>
2022-10-28 15:43:10 +0900

    6 Slove

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,41 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String,List<String>> Duplicatemap=new HashMap<>();
+        for (int i=0;i<forms.size();i++) {
+            String Email = forms.get(i).get(0);
+            String Name = forms.get(i).get(1);
+            for (int j=0; j<Name.length()-1;j++) {
+                String TmpString = Name.substring(j,j+2);
+                if (Duplicatemap.containsKey(TmpString)) {
+                    List<String> Tmp = Duplicatemap.get(TmpString);
+                    Tmp.add(Email);
+                    Duplicatemap.put(TmpString,Tmp);
+                }
+                else {
+                    List<String> Tmp = new ArrayList<>();
+                    Tmp.add(Email);
+                    Duplicatemap.put(TmpString,Tmp);
+                }
+            }
+        }
+        for (String str : Duplicatemap.keySet()) {
+            List<String> Tmp =  Duplicatemap.get(str);
+            if (Tmp.size()>=2) {
+                for (String j : Tmp) {
+                    if (!answer.contains(j)) answer.add(j);
+                }
+            }
+        }
+        Collections.sort(answer);
         return answer;
     }
 }
