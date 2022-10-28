b7963a513645b4f5ede20259c5aab5f20a488da5
ywj9811 <ywj9811@naver.com>
2022-10-28 18:03:19 +0900

    만약 반복 도중에 현재의 문자와 다음 문자가 동일하다면 if문 가동

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,7 +14,9 @@ public class Problem2 {
 
     static String duplicated(String cryptogram) {
         for (int i = 0; i < cryptogram.length()-1; i++){
+            if (cryptogram.substring(i, i+1).equals(cryptogram.substring(i+1, i+2))){
 
+            }
         }
     }
     /**
