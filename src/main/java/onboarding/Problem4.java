3be5862b9709077dc5bcfec3838652b5c78ab28e
YuHayeon <youha0930@gmail.com>
2022-10-29 01:13:03 +0900

    feat(Problem4): 올바른 범위가 아닐 때 예외처리
    문자열의 길이가 1미만 1000초과일 때 예외처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		String answer = "";
+		return answer;
+	}
+
+	static class InputValidator {
+		private static boolean isRightWordRange(String word) {
+			int wordLength = word.length();
+			if (wordLength < 1 && wordLength > 1000) {
+				return false;
+			}
+			return true;
+		}
+	}
 }
