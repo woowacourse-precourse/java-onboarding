d366ebe8252c47ecae34d5b58bf53e5ef151b18a
youngkyunIm <yee0230@naver.com>
2022-10-28 23:26:37 +0900

    feat(Problem5) : loop wonUnit

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,9 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         int[] wonUnit = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
         for (int unit : wonUnit) {
+            int count = money / unit;
+            answer.add(count);
+            money = money - (unit * count);
         }
         return answer;
     }
