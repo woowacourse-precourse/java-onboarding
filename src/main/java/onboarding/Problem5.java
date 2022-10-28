3bdc5a889163bc8836810493bd92bc08d98d6fe6
minhee020 <minhee020@naver.com>
2022-10-28 22:52:10 +0900

    Problem5 | 거스름돈 알고리즘 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,9 +13,12 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>(List.of(50000,10000,5000,1000,500,100,50,10,1));
+        int divisor = 0;
 
         for (int i = 0; i < 9; i++){
-
+            divisor = answer.get(i);
+            answer.set(i, money / divisor);
+            money %= divisor;
         }
 
         return answer;
