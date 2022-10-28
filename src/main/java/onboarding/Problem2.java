3561ce8004bf9d8a504981d7d70edcc737c9811c
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 16:18:21 +0900

    feat: [2번] 중복 문자열 제거 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,29 @@
 package onboarding;
 
 public class Problem2 {
+
+    /**
+     * 연속되는 중복 문자의 idx 반환
+     */
+    private static int getDuplicateCharIdx(String cryptogram) {
+        return Integer.MAX_VALUE;
+    }
+
+    /**
+     * 연속되는 중복 문자의 갯수 반환
+     */
+    private static int getDuplicateCharCount(String cryptogram, int idx) {
+        return Integer.MAX_VALUE;
+    }
+
+    /**
+     * 주어진 범위의 문제열을 제거하는 함수
+     */
+    private static String removeStringRange(String cryptogram, int idx, int count) {
+        return "";
+    }
+
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
