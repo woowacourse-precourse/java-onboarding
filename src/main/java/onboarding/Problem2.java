8fd9f51eb39091e38689526f7d87015a3c7d7992
cksdud8028 <cksdud8028@naver.com>
2022-10-28 16:44:35 +0900

    FEAT checkingCode

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,16 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static boolean checkingCode(String cryptogram) {
+        String[] code = cryptogram.split("");
+        for (int i = 0; i < code.length - 1; i++) {
+            if (code[i] == code[i + 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
