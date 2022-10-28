3ec08b5948dfbf1238d236fb4fb0f6bd135edd20
WhalesBob <ndw8200@naver.com>
2022-10-28 11:57:33 +0900

    Complete to make Problem7(test complete, after refactoring)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,114 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String,Integer> scoreBoardOfSuggestedFriends;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+        scoreBoardOfSuggestedFriends = scoreBoardMapping(peopleNameSet(friends));
+
+        Map<String,LinkedList<String>> eachFriendsList = friendsList();
+        setFriendsInfo(eachFriendsList,friends);
+
+        LinkedList<String> userFriendList = eachFriendsList.get(user);
+        ArrayList<String> alreadyCountedFriend = new ArrayList<>();
+
+        considerFriendsScore(user,userFriendList,alreadyCountedFriend,eachFriendsList);
+        considerVisitorScore(visitors);
+
+        return finalSortedFriendList(alreadyCountedFriend);
+    }
+    static Map<String,Integer> scoreBoardMapping(Set<String> peopleSet){
+        Map<String,Integer> map = new HashMap<>();
+
+        for (String person : peopleSet) map.put(person, 0);
+        return map;
+    }
+
+    static Set<String> peopleNameSet(List<List<String>> friends) {
+        Set<String> friendSet = new HashSet<>();
+        for(int i = 0; i < friends.size(); i++){
+            friendSet.add(friends.get(i).get(0));
+            friendSet.add(friends.get(i).get(1));
+        }
+
+        return friendSet;
+    }
+    static Map<String,LinkedList<String>> friendsList(){
+        Map<String,LinkedList<String>> friendsList = new HashMap<>();
+
+        scoreBoardOfSuggestedFriends.forEach((nameOfPerson,value) -> {
+            LinkedList<String> newAdjacencyList = new LinkedList<>();
+            friendsList.put(nameOfPerson,newAdjacencyList);
+        });
+        return friendsList;
+    }
+
+    static void setFriendsInfo(Map<String,LinkedList<String>> list,List<List<String>> friends){
+        // add friends information
+        for (List<String> twoOfFriend : friends) {
+            String personA = twoOfFriend.get(0);
+            String personB = twoOfFriend.get(1);
+
+            // add friends on LinkedList
+            list.get(personA).addLast(personB);
+            list.get(personB).addLast(personA);
+        }
+    }
+    static void considerFriendsScore(String user,LinkedList<String> userFriendList,ArrayList<String> countedFriend,Map<String,LinkedList<String>> friendsList){
+        while(!userFriendList.isEmpty()){
+            String selectedFriend = userFriendList.removeFirst();
+            countedFriend.add(selectedFriend);
+
+            LinkedList<String> friendsListOfSelectedFriends = friendsList.get(selectedFriend);
+
+            while(!friendsListOfSelectedFriends.isEmpty()){
+                raiseFriendsOfFriendsScore(user,friendsListOfSelectedFriends);
+            }
+        }
+    }
+    static void raiseFriendsOfFriendsScore(String targetUser, LinkedList<String> friendsList){
+        String friendKnownTogether = friendsList.removeFirst();
+
+        if(!friendKnownTogether.equals(targetUser)){
+            int score = scoreBoardOfSuggestedFriends.get(friendKnownTogether);
+            scoreBoardOfSuggestedFriends.replace(friendKnownTogether,score + 10);
+        }
+    }
+    static void considerVisitorScore(List<String> visitors){
+        for(int i = 0; i < visitors.size(); i++){
+            String visitedPerson = visitors.get(i);
+            changeScoreOfVisitors(scoreBoardOfSuggestedFriends,visitedPerson);
+        }
+    }
+    static void changeScoreOfVisitors(Map<String,Integer> map, String visitor){
+        try{
+            int score = map.get(visitor);
+            map.replace(visitor,score+1);
+        }catch(NullPointerException ex){
+            map.put(visitor,1);
+        }
+    }
+    static List<String> finalSortedFriendList(ArrayList<String> alreadyCountedFriend){
+        List<String> keySetList = new ArrayList<>(scoreBoardOfSuggestedFriends.keySet());
+        keySetList.sort((o1, o2) -> scoreBoardOfSuggestedFriends.get(o2).compareTo(scoreBoardOfSuggestedFriends.get(o1)));
+
+        List<String> answer = new ArrayList<>(Collections.emptyList());
+
+        for(String key : keySetList){
+            if(isValidNewFriend(alreadyCountedFriend,key,scoreBoardOfSuggestedFriends.get(key))){
+                answer.add(key);
+            }
+        }
+
         return answer;
+    }
+    static boolean isValidNewFriend(ArrayList<String> selectedFriends, String friends, int value){
+        if(value == 0){
+            return false;
+        }else{
+            return !selectedFriends.contains(friends);
+        }
     }
 }
