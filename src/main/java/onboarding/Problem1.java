c064ee121883d3e8045316396dc785c2b0bc8855
sysliar <sysliar@gmail.com>
2022-10-28 17:24:00 +0900

    Q1. add isNormal method for ExceptionHandling
    Q1. change var name [checkList -> pages]

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,12 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private boolean isNormal(List<Integer> pages) {
+        if(pages.get(0) + pages.get(1) < 2 || pages.get(0) + pages.get(1) > 800) return false;
+        if(pages.get(0) - pages.get(1) != -1) return false;
+        if(pages.get(0) % 2 != 0) return false;
+        return true;
+    }
+    
 }
