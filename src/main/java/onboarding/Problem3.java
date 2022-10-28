c81bb0088565eeeb79d026664f2f737cf077486a
yeonhee <ydol2@naver.com>
2022-10-29 01:08:19 +0900

    [3번] feat : 3,6,9 개수를 반환하는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,19 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * 3.6.9 개수를 return하는 함수 구현
+     */
+    private static int get369(int num){
+        int count=0;
+
+        while (num>0){
+             if(num%10%3==0){
+                 count++;
+             }
+             num=num/10;
+        }
+        return count;
+    }
 }
