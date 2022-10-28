f31ab3dfd3186584c7a95a4a6e012bfb5cc7b955
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-28 21:17:41 +0900

    연속하는 중복문자들을 삭제하는 for문 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,13 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        String result = "";
+        for(int i =0; i<cryptogram.length(); i++){
+            if(cryptogram.charAt(i-1)!=cryptogram.charAt(i) && cryptogram.charAt(i)!=cryptogram.charAt(i+1) ){
+                result += cryptogram.charAt(i);
+            }
+        }   
+        
         String answer = "answer";
         return answer;
     }
