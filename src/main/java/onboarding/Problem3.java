e60f67b9558c60f321700d46b2dbd4f16377f6bc
Heosejin98 <sejin3319@naver.com>
2022-10-29 03:25:19 +0900

    Refactor : PROBLEM3 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,33 +1,50 @@
 package onboarding;
 
 public class Problem3 {
-    static boolean Exception(int number) {
-        return (0 < number && number <= 10000) ? false : true;
+
+    private final static int ERROR = -1;
+    private final static int MIN_NUMBER = 1;
+    private final static int MAX_NUMBER = 10000;
+
+    public static int solution(int number) {
+        int answer;
+
+        if (exception(number)) {
+            return ERROR;
+        }
+
+        answer = sumCount369(number);
+
+        return answer;
     }
 
-    static int Count369(String str) {
-        int     result = 0;
-        final char[]  compare_number_array = {'3', '6', '9'};
+    private static boolean exception(int number) {
+        return MIN_NUMBER > number || number > MAX_NUMBER;
+    }
 
-        for (int i=0; i<3; i++) {
-            char compare_number = compare_number_array[i];
-            result += str.chars().filter(c -> c == compare_number).count();
+    private static int sumCount369(int number) {
+        String stringNumber;
+        int result = 0;
+
+        for (int i = 1; i <= number; i++) {
+            stringNumber = Integer.toString(i);
+            result += count369(stringNumber);
+        }
+        return result;
+    }
+
+    private static int count369(String str) {
+        int result = 0;
+        final char[] COMPARE_NUMBER = {'3', '6', '9'};
+
+        for (int i = 0; i < 3; i++) {
+            int finalI = i;
+            result += str.chars()
+                .filter(c -> c == COMPARE_NUMBER[finalI]).count();
         }
 
         return result;
     }
-    public static int solution(int number) {
-        int     answer = 0;
-        String  string_number;
 
-        if (Exception(number))
-            return answer;
 
-        for (int i = 1; i <= number; i++){
-            string_number = Integer.toString(i);
-            answer += Count369(string_number);
-        }
-
-        return  answer;
-    }
 }
