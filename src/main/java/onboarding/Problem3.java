1071c83d1c9e7dd6de38ed28d8e6bc256293a055
woogi <dnrlwo11@gmail.com>
2022-10-28 22:16:36 +0900

    [problem3] 1의 자리수가 369인지 체크하는 메서드 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static boolean isRemain369(int number) {
+        int remainder = number % 10;
+        if (remainder == 3 || remainder == 6 || remainder == 9) {
+            return true;
+        }
+        return false;
+    }
 }
