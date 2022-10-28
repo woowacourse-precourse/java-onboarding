5e3f751c5e697347f4efcd13a865d659fe100888
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-28 15:27:18 +0900

    feat:7번 map(친구: 친구1, 친구2, 친구3) 구조 생성 기능추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,39 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, List<String>> map = new HashMap<>();
+
+        makeMap(user, friends, map);
         return answer;
+    }
+
+    private static void makeMap(String user, List<List<String>> friends, Map<String, List<String>> map) {
+        List<String> user_list = new ArrayList<>();
+        user_list.add(user);
+        map.put(user, new ArrayList<>());
+
+        for(int i = 0; i< friends.size(); i++){
+            user_list.add(friends.get(i).get(0));
+            user_list.add(friends.get(i).get(1));
+        } //map(친구: 친구1, 친구2, 친구3) 중 친구 부분을 만들기 위한 과정
+
+        Set<String> set = new HashSet<>(user_list);
+        List<String> result= new ArrayList<>(set); // 중복되어있는 친구 이름 중복된 이름 제거
+
+        for(int i=0; i<result.size(); i++){
+            map.put(result.get(i), new ArrayList<>());
+        }
+
+        for(int i = 0; i< friends.size(); i++){
+            String friend1= friends.get(i).get(0);
+            String friend2= friends.get(i).get(1);
+
+            map.get(friend1).add(friend2);
+            map.get(friend2).add(friend1); // 친구1,2,3 추가 (양방향적으로)
+        }
     }
 }
