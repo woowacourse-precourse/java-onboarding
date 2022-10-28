e41098debc48a0e221c0f532a2cd934df12f8d9c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-28 16:37:46 +0900

    feat: 3번 수의 한 숫자를 입력받아 3, 6, 9면 1을 리턴하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem3 {
+
+    /*기능 목록
+    1. 한 수를 한 글자씩 나누어 입력받을 때, 그 값이 3, 6, 9면 1를 리턴한다.
+    2. 이를 한 수를 가지고 반복시킨다. (예를 들어 “33”이면 십의 자리 3, 일의 자리 3을 반복시킨다. 값을 합쳐 2를 리턴한다.)
+    3. 이를 1부터 주어진 수 number 까지 반복해 총합을 리턴한다.
+    */
+
+    static String THREE = "3";
+    static String SIX = "6";
+    static String NINE = "9";
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int countClaps(String each_number){
+        if(each_number.equals(THREE)) return 1;
+        if(each_number.equals(SIX)) return 1;
+        if(each_number.equals(NINE)) return 1;
+        return 0;
     }
 }
