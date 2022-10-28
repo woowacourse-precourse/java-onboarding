4a45934cfe628a74c9398f545f233e3a600f4e1a
Heesom <heesom.hs@gmail.com>
2022-10-28 14:29:33 +0900

    Problem2 : findAnswer 메소드 추가
    문자열 안에 중복이 있다면 저장하지 않고 중복이 발생하지 않아야 저장, 저장된 문자열을 리턴하는 메소드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,6 +7,36 @@ public class Problem2 {
         return answer;
     }
 
+    public static String findAnswer(String cryptogram) {
+        StringBuilder tempAnswer = new StringBuilder();
+        boolean noticeDoubleToEnd = false;
+        boolean checkPairDouble = false;
+        int cryptogramArrlen = cryptogram.length()-1;
+
+        for (int i = 0; i < cryptogramArrlen; i++) {
+            checkPairDouble = (cryptogram.charAt(i) == cryptogram.charAt(i+1)); // 중복인지 체크
+
+            //만약 중복이 아니면 정답 저장
+            if (!checkPairDouble) {
+
+                if(!noticeDoubleToEnd){ //저번쌍이 중복이 아니었으면 요번도 저장
+                    tempAnswer.append(cryptogram.charAt(i));
+                } else{  //저번쌍이 중복이었으면 이번턴은 지나가고 대신 다음에는 중복아니라고 신호주고 끝
+                    noticeDoubleToEnd = false;
+                }
+
+            }else{ //만약 중복이면
+                noticeDoubleToEnd = true;
+            }
+        }
+
+        if(!checkPairDouble){ //범위 마지막 1개
+            System.out.println("store>"+ (cryptogramArrlen)+" : "+cryptogram.charAt(cryptogramArrlen));
+        }
+
+        return tempAnswer.toString();
+    }
+
     public static boolean checkDouble(String cryptogram) {
         for (int i = 0; i < cryptogram.length()-1; i++) {
             if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
