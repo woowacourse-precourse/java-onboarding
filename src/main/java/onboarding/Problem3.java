574d62c6f92736233dc80605d5ad4310d2e2f154
nakalic <nakalic28@gmail.com>
2022-10-28 16:34:35 +0900

    feat(Problem3): 카운트하는 메소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = count369(number);
+		return answer;
+	}
+
+	private static int count369(int num) {
+		int cnt = 0;
+		for (int i = 1; i < num + 1; i++) {
+			int temp = i;
+
+			while (temp != 0) {
+				if (temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9)
+					cnt++;
+				temp /= 10;
+			}
+		}
+		return cnt;
+	}
 }
