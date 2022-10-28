5e364876454209fff4d2e63f22fa06bf5b3f02b0
myeongju00 <j51677767@gmail.com>
2022-10-28 15:08:35 +0900

    feature: 페이지 번호의 각 숫자를 더하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,23 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
         return answer;
     }
+    //TODO(페이지 번호의 각 숫자를 모두 더하는 함수)
+    private static int sumOfDigits(int pageNumber) {
+        int result = 0;
+        while (pageNumber > 0) {
+            result += pageNumber % 10;
+            pageNumber /= 10;
+        }
+        return result;
+    }
+
+    //TODO(페이지 번호를 모두 곱하는 함수)
+
+
+    //TODO(숫자 비교 함수)
+
 }
