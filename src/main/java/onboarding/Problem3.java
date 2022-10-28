9927a6a8efce32fdd04df45ed9de53afa7d86358
Ilchul Kim <huj0159@gmail.com>
2022-10-29 07:00:13 +0900

    feat: add method count clap times

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
     }
+
+    public static int clapCount(String number){
+        int sum=0;
+        for(int i=0;i<number.length();i++){
+            sum+=isClap(number.charAt(i));
+        }
+        return sum;
+    }
+
 }
