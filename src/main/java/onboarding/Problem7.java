5a47d07d9b97a7ab41873463b0f8f2446352cfc3
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-28 16:45:02 +0900

    문제 7 - 친구 리스트 생성 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        // 전체 유저 리스트 생성
+        // 유저 친구 관계 리스트 생성
+        // 유저 친구의 친구 관계 리스트 생성
+        // 전체 유저 리스트에서 유저와 친구를 뺀 리스트 생성
+        // 유저 및 친구 제외 리스트의 구성원 중 방문객 점수 부여
+        // 유저및 친구 제외 리스트의 구성원 중 친구의 친구 점수 부여
+        // answer 리스트에 add
+        // 추천 점수가 같은 경우는 이름순
+
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    static List<String> makeFriendList(List<List<String>> friends, String user) {
+        List<String> friendList = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if(friend.get(0).equals(user))
+                friendList.add(friend.get(1));
+            else if (friend.get(1).equals(user))
+                friendList.add(friend.get(1));
+        }
+        return friendList;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
