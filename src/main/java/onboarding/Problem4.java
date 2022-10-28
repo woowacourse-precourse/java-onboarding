07a223d1ddb1adf29c1a516671a61d81c3fbb838
KSH <tkdgns543@gmail.com>
2022-10-29 00:24:28 +0900

    feat(Problem4): 주어진 대문자를 기능 구현 조건대로 치환하는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,4 +10,9 @@ public class Problem4 {
     private static char lowerCaseReplace(char c) {
         return (char)('z'+'a'-c);
     }
+
+    /* 주어진 대문자를 기능 구현 조건대로 치환하는 메서드 */
+    private static char upperCaseReplace(char c) {
+        return (char)('Z'+'A'-c);
+    }
 }
