04dc18788a7ba3346ad46d3568183a6e850cfb47
Your Name <dbsdbxkrzz@naver.com>
2022-10-28 14:00:23 +0900

    feat: Problem3의 기본 틀을 작성합니다.
    Problem3의 기능 목록에 맞추어 기본 틀을 작성합니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+
+	public static final int MOD = 10;
+
+	public static int solution(int number) {
+		int answer = 0;
+		return answer;
+	}
+
+	public static int getClapCount(int num) {
+		int cnt = 0;
+		
+		return cnt;
+	}
+
 }
