c9f914f1c78423dc4f04d5db5849249d0752bd93
nswon <swnam0729@gmail.com>
2022-10-28 21:26:58 +0900

    feat(Problem3) <solution>: 1부터 숫자까지 돌면서 각 자릿수가 규칙에 걸리는지 체크하는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,14 +2,15 @@ package onboarding;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Problem3 {
     private static final List<Integer> checkNumbers = Arrays.asList(3, 6, 9);
     private static int count = 0;
 
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        IntStream.range(1, number+1).forEach(Problem3::checkRole);
+        return count;
     }
 
     private static void checkRole(int number) {
