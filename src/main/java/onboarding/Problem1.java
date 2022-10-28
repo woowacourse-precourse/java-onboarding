8a37443cf45ebd454793a71a40a876181d3f8fba
JungYoonshin <sju08227@naver.com>
2022-10-28 23:15:54 +0900

    feat: #problem1 -> add the function of summing each digit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static final int START_PAGE = 1;
+    public static final int FINAl_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+
+    private static int sumScore(List<Integer> pageList, int index) {
+        int pageNumber = pageList.get(index);
+        int sum = 0;
+
+        while (pageNumber > 1) {
+            sum += pageNumber % 10;
+            pageNumber /= 10;
+        }
+
+        return sum;
     }
 }
