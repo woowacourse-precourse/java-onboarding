7fead10309a789ff6033754504e31f5b4094b83a
hectick <chyeon1223@cau.ac.kr>
2022-10-28 22:53:28 +0900

    feat : [Problem4] 기능 목록 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        //알파벳 대문자일 경우 변환
+
+        //알파벳 소문자일 경우 변환
+
+        //공백일 경우 유지
+
         return answer;
     }
 }
