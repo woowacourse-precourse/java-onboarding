4a66ef5b85aaafd31c014c96cb9ddb83bdba4546
win9 <krrr8@naver.com>
2022-10-28 22:43:46 +0900

    [기능4번] 주어진 글자가 아스키코드로 알파벳인지 확인하는 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            int strToCharDigit = word.charAt(i);                //글자 하나를 불러온다.
+            if (isAlphabet(answer, strToCharDigit)) continue;
+
+        }
+        return answer.toString();
+    }
+
+
+    private static boolean isAlphabet(StringBuilder answer, int strToCharDigit) {
+        if (strToCharDigit == 32) {
+            answer.append(Character.toString(strToCharDigit));
+            return true;
+        }
+        return false;
     }
 }
