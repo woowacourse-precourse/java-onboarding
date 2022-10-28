66011d1a2511c7a7c11e5c7af37815131730a6d4
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2022-10-28 17:29:42 +0900

    feat: 예외사항 처리를 위한 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,6 +13,9 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        answer = exception(pobi, crong);
+        if (answer == -1) return answer;
+
         int pobiScore = pageCal(pobi);
         int crongScore = pageCal(crong);
         answer = scoreComp(pobiScore, crongScore);
@@ -48,4 +51,17 @@ class Problem1 {
         return ans;
     }
 
+    public static int exception(List<Integer> pobi, List<Integer> crong) {
+        for (int i = 0; i < 2; i++) {
+            if (pobi.get(i) < 1 || pobi.get(i) > 400) return -1;
+        }
+        for (int i = 0; i < 2; i++) {
+            if (crong.get(i) < 1 || crong.get(i) > 400) return -1;
+        }
+
+        if (pobi.get(0) + 1 != pobi.get(1)) return -1;
+        else if (crong.get(0) + 1 != crong.get(1)) return -1;
+
+        return 0;
+    }
 }
