b3ce3c7c628128d16d0d2cdc8716b3edb2d055c6
YuHayeon <youha0930@gmail.com>
2022-10-28 23:28:24 +0900

    feat(Problem3): 하나의 숫자에서 3,6,9가 있는지 확인 후 있으면 count

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,22 @@ public class Problem3 {
 		return answer;
 	}
 
+	private static int countOneNumber(int number) {
+		int count = 0;
+		while (number > 0) {
+			count += check369InNumber(number % 10);
+			number /= 10;
+		}
+		return count;
+	}
+
+	private static int check369InNumber(int number) {
+		if (number != 0 && number % 3 == 0) {
+			return 1;
+		}
+		return 0;
+	}
+
 	static class InputValidator {
 		private static boolean isRightNumberRange(int number) {
 			if (number < 1 || number > 10000) {
