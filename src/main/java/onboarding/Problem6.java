2aee885588c91f50d2a75de3279464e7a4509ee2
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 14:58:00 +0900

    feat : design method that compare elements by one dimension

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,24 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
+
+        for (int i = 0; i < forms.size(); i++) {
+
+            List ElementByTwoDimension = forms.get(i);
+
+            for (int j = 0; j < forms.size(); j++) {
+                if (j == i) {
+                } else {
+                    List ElementByOneDimension = forms.get(j);
+                    System.out.println(ElementByTwoDimension);
+                    System.out.println(ElementByOneDimension);
+                }
+            }
+
+
+        }
+
         List<String> answer = List.of("answer");
         return answer;
     }
