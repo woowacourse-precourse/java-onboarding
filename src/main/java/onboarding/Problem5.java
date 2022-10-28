1c10e729dd2840aaec0ee7a7220950a023c6b452
junho lee <junho1273@gmail.com>
2022-10-28 15:39:03 +0900

    Problem 5 기능구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,7 +8,12 @@ import java.util.List;
 // 2.지폐의 단위별로 나눈 몫을 배열에 추가 후 나머지를 money로 설정
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int [] won = {50000,10000,5000,1000,500,100,50,10,1};
+        for(int w : won){
+            answer.add(money/w);
+            money %= w;
+        }
         return answer;
     }
 }
