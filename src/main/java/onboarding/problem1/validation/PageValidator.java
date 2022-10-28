c5b7b8c7a94f4ec53e097e667672aa70337c08d6
backFox <jorealbusiness@gmail.com>
2022-10-28 11:54:11 +0900

    docs(problem1): 각 클래스의 역할을 설명하는 Javadoc 추가

--- src/main/java/onboarding/problem1/validation/PageValidator.java
+++ src/main/java/onboarding/problem1/validation/PageValidator.java
@@ -2,10 +2,20 @@ package onboarding.problem1.validation;
 
 import onboarding.problem1.vo.Pages;
 
+/**
+ * Pages 객체가 유효한 객체인 지 검증하는 작업을 모아둔 Validator 클래스입니다.
+ * @author BackFoxx
+ */
 public class PageValidator {
 	public static final int PAGE_MAX_LIMIT = 400;
 	public static final int PAGE_MIN_LIMIT = 1;
 
+	/**
+	* Pages 객체를 검증하기 위해 외부에서 사용할 수 있는 유일한 메소드입니다.
+	* 필요한 검증 작업을 ||(or) 를 사용해 연이어 실행하도록 설계할 수 있습니다.
+	* @param pages 검증 대상인 Pages 객체를 인자로 받을 수 있습니다.
+	* @return 해당 메소드가 true 를 반환하는 경우, 인자로 넘어온 Pages 객체는 유효하지 않는 객체로 간주됩니다.
+	*/
 	public static boolean isInValidPages(Pages pages) {
 		return isPageSeparated(pages) ||
 				isLeftPageNotOdd(pages) ||
@@ -13,20 +23,35 @@ public class PageValidator {
 				isPageFirstOrLower(pages);
 	}
 
+	/**
+	 * 오른쪽 페이지와 왼쪽 페이지가 1 이상 차이가 나는 지 검증합니다.
+	*/
 	private static boolean isPageSeparated(Pages pages) {
 		Integer higherPageNumber = Math.max(pages.getLeftPage().getNumber(), pages.getRightPage().getNumber());
 		Integer lowerPageNumber = Math.min(pages.getLeftPage().getNumber(), pages.getRightPage().getNumber());
 		return (higherPageNumber - lowerPageNumber) != 1;
 	}
 
+	/**
+	 * 왼쪽 페이지 숫자가 홀수가 아닌 지 검증합니다.
+	 * isPageSeparated 메소드 실행 후에 실행하는 경우
+	 * 오른쪽 페이지가 짝수인 지도 동시에 검증이 이루어집니다.
+	 * @return 왼쪽 페이지가 홀수가 아닌 경우 true 를 반환합니다.
+	 * */
 	private static boolean isLeftPageNotOdd(Pages pages) {
 		return (pages.getLeftPage().getNumber() % 2) == 0;
 	}
 
+	/**
+	 * 페이지가 책의 마지막 페이지 또는 초과한 페이지를 펼치고 있는 지 검증합니다.
+	 * */
 	private static boolean isPageLastOrHigher(Pages pages) {
 		return PAGE_MAX_LIMIT <= pages.getLeftPage().getNumber() || PAGE_MAX_LIMIT <= pages.getRightPage().getNumber();
 	}
 
+	/**
+	 * 페이지가 책의 첫번 째 페이지 또는 미만의 페이지를 펼치고 있는 지 검증합니다.
+	 */
 	private static boolean isPageFirstOrLower(Pages pages) {
 		return pages.getLeftPage().getNumber() <= PAGE_MIN_LIMIT || pages.getRightPage().getNumber() <= PAGE_MIN_LIMIT;
 	}
--- src/main/java/onboarding/problem1/vo/Score.java
+++ src/main/java/onboarding/problem1/vo/Score.java
@@ -1,5 +1,9 @@
 package onboarding.problem1.vo;
 
+/**
+ * 점수 값을 보관하고 있는 VO 클래스입니다.
+ * @author BackFoxx
+ */
 public class Score implements Comparable<Score> {
 	private final Integer score;
 
@@ -7,6 +11,9 @@ public class Score implements Comparable<Score> {
 		this.score = score;
 	}
 
+	/**
+	 * 인자로 들어온 값을 score 로 하는 Score 객체를 만드는 정적 팩토리 메소드입니다.
+	 */
 	public static Score of(Integer score) {
 		return new Score(score);
 	}
@@ -16,10 +23,16 @@ public class Score implements Comparable<Score> {
 		return Integer.compare(score, anotherScore.score);
 	}
 
+	/**
+	 * 해당 메소드를 호출한 Score 값이 인자로 들어온 Score 값보다 높은 지 검증하는 메소드입니다.
+	 */
 	public boolean isHigherThan(Score anotherScore) {
 		return this.compareTo(anotherScore) > 0;
 	}
 
+	/**
+	 * 해당 메소드를 호출한 Score 값이 인자로 들어온 Score 값보다 낮은 지 검증하는 메소드입니다.
+	 */
 	public boolean isLowerThan(Score anotherScore) {
 		return this.compareTo(anotherScore) < 0;
 	}
