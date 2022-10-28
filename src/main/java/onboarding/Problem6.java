a406601e105f8a425e57afb0ac8cab2557795ab1
hong-sile <gurwns9325@naver.com>
2022-10-28 15:25:02 +0900

    feat : 닉네임에서 연속된 두 문자를 전부 추출한 list를 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,14 +8,25 @@ import java.util.Map;
 public class Problem6 {
     private static Map<String,String> nickEmailMapper = new HashMap<>();
     private static List<String> nickNameList = new ArrayList<>();
+    private static List<String> continuousSubStrList = new ArrayList<>();
     private static final int EMAIL=0;
     private static final int NICKNAME=1;
 
     public static List<String> solution(List<List<String>> forms) {
         initNickEmailMapper(forms);
         initNickNameList(forms);
+        generateContinuousSubStrList();
 
         return new ArrayList<>();
+    }
+
+    private static void generateContinuousSubStrList() {
+        for (String nickName : nickNameList) {
+            for(int i = 0; i < nickName.length()-1; i++){
+                String continuousStr = nickName.substring(i, i + 1);
+                continuousSubStrList.add(continuousStr);
+            }
+        }
     }
 
     private static void initNickNameList(List<List<String>> forms) {
