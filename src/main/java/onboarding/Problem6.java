f420b7d6f2007f640f593647d3dc3230be853efe
csj <koreaxman@naver.com>
2022-10-29 00:53:20 +0900

    feat: Extract nicknames consecutive duplicate characters

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,6 +7,7 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> nicknameList = extractNicknames(forms);
         List<List<String>> nicknameSplitList = extractNicknameByTwoChar(nicknameList);
+        List<String> checkDuplicate = checkDuplicateNicknames(nicknameSplitList, nicknameList);
 
         return nicknameList;
     }
@@ -35,5 +36,29 @@ public class Problem6 {
         }
 
         return splitNicknameList;
+    }
+
+    public static List<String> checkDuplicateNicknames(List<List<String>> splitNicknameList,
+                                                       List<String> nicknameList) {
+        List<String> duplicateNicknameList = new ArrayList<>();
+
+        for (int i = 0; i < splitNicknameList.size(); i++) {
+            for (int j = i + 1; j < splitNicknameList.size(); j++) {
+                for (int k = 0; k < splitNicknameList.get(i).size(); k++) {
+                    if (nicknameList.get(j).contains(splitNicknameList.get(i).get(k))) {
+                        duplicateNicknameList.add(nicknameList.get(j));
+                        duplicateNicknameList.add(nicknameList.get(i));
+                    }
+                }
+            }
+        }
+        return removeMultipleTimesName(duplicateNicknameList);
+    }
+
+    public static List<String> removeMultipleTimesName(List<String> duplicateNicknameList) {
+        List<String> removeMultipleName =
+                duplicateNicknameList.stream().distinct().collect(Collectors.toList());
+
+        return removeMultipleName;
     }
 }
