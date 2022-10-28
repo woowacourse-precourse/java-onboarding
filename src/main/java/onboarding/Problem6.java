f173fc6fead15bc81cad94d4bdc3b002f970b96a
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-28 13:56:07 +0900

    feat:6번 부분 문자열 생성을 위한 세팅

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        return answer;
+
+        List<String> realAnswer = new ArrayList<>();
+
+        Set<String> set = new HashSet<>(realAnswer);
+        List<String> result= new ArrayList<>(set); // result
+
+        subStringMatching(forms, realAnswer);
+
+
+        Collections.sort(result);
+        return result;
+    }
+
+    private static void subStringMatching(List<List<String>> forms, List<String> answer){ // 부분 문자열 생성을 위한 메서드 생성
+
+
     }
 }
