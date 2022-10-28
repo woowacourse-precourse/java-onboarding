2725d6bd30fb2cee8ee46757334854827b373c54
Hyeonjae-K <href.hjk@gmail.com>
2022-10-28 09:39:54 +0900

    add: 기능 목록 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/*기능 목록
+ * 알파벳을 반대로 변환해 주는 기능
+ * 문자열을 반복하는 기능
+ * */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
