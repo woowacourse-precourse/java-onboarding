5ff6381ad3fd005caf079184dfb12f9427e0be94
bradbrad97 <taehyun0121@naver.com>
2022-10-28 20:00:27 +0900

    problem4: 알파벳 청개구리 사전대로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -11,8 +11,35 @@ public class Problem4 {
         }
     }
 
+    public static char convertChar (char ch){
+        char minChar;
+        char maxChar;
+        if (ch >= 'a' && ch <= 'z'){
+            minChar = 'a';
+            maxChar = 'z';
+        }
+        else {
+            minChar = 'A';
+            maxChar = 'Z';
+        }
+        int toAdd = maxChar - ch;
+        char convertedChar = (char) (minChar + toAdd);
+        return convertedChar;
+    }
+
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i<word.length(); i++){
+            char currentChar = word.charAt(i);
+            if (isAlphabet(currentChar)){
+                answer += convertChar(currentChar);
+            }
+            else {
+                answer += currentChar;
+            }
+        }
+
         return answer;
     }
 }
