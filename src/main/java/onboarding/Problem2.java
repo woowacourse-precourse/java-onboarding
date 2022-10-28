6b8aa8d889444ddeed952b451680feaa73bf740f
KSH <tkdgns543@gmail.com>
2022-10-28 22:21:17 +0900

    style(Problem2): 문자열 2개로 압축하는 메서드에서 = 앞뒤로 공백 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,13 +8,13 @@ public class Problem2 {
 
     /* 3 이상 연속된 target 문자열을 2개까지 줄이는 메서드 */
     private static String compressToSizeTwo(String cryptogram, char target) {
-        String result=cryptogram;
-        String tmp=Character.toString(target);
-        String start=tmp.repeat(3);
-        String end=tmp.repeat(2);
+        String result = cryptogram;
+        String tmp = Character.toString(target);
+        String start = tmp.repeat(3);
+        String end = tmp.repeat(2);
 
         while (result.contains(tmp)) {
-            result=result.replace(start,end);
+            result = result.replace(start,end);
         }
 
         return result;
