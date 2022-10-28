023883e6885d44ab1a6e2e673440db45893a671d
swandevson <shai4956@gmail.com>
2022-10-29 00:07:22 +0900

    feat: P6 중복 닉네임 유저 이메일 반환 부분 기능 분리
    isDuplicate
    - 해당 인원과 다른 인원의 닉네임 중복 여부 확인
    - 메일이 같을 경우 자기 자신이므로 중복 여부 체크 X
    
    getDupUserMail
    - 모든 인원을 순환하며 해당 인원과 닉네임 중복일 경우 이메일 추가
    - 해당 인원과 닉네임이 중복인 유저들의 이메일 반환 (중복 X)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import org.mockito.internal.matchers.Null;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -10,38 +12,48 @@ public class Problem6 {
         return nickname.substring(idx, idx+2);
     }
 
-    public static HashSet<String> chckDuplicates(List<String> userForm, List<List<String>> forms) {
-        HashSet<String> dupMailSet = new HashSet<>();
-
+    public static boolean isDuplicate(List<String> userForm, List<String> otherForm) {
         String userMail = userForm.get(0);
         String userNickname = userForm.get(1);
+        String otherMail = otherForm.get(0);
+        String otherNickname = otherForm.get(1);
+
+        if(otherMail.equals(userMail)) {
+            return false;
+        }
 
         for(int i = 0; i < userNickname.length() - 1; i++) {
             String subNickname = sliceNickname(userNickname, i);
-
-            for(int j = 0; j < forms.size(); j++) {
-                List<String> otherForm = forms.get(j);
-                String otherMail = otherForm.get(0);
-                String otherNickname = otherForm.get(1);
-
-                if (otherNickname.contains(subNickname) && !otherMail.equals(userMail)) {
-                    dupMailSet.add(otherMail);
-                }
+            if(otherNickname.contains(subNickname)) {
+                return true;
             }
         }
-        return dupMailSet;
+        return false;
+    }
+    public static HashSet<String> getDupUserMail(List<String> userForm, List<List<String>> forms) {
+        HashSet<String> dupUserMailSet = new HashSet<>();
+
+        for(int i = 0; i < forms.size(); i++) {
+            List<String> otherForm = forms.get(i);
+            if(isDuplicate(userForm, otherForm)) {
+                String dupUserMail = otherForm.get(0);
+
+                dupUserMailSet.add(dupUserMail);
+            }
+        }
+        return dupUserMailSet;
     }
 
-    public static List<String> getDupUsers(List<List<String>> forms) {
-        HashSet<String> dupUserSet = new HashSet<>();
+    public static List<String> getDupResult(List<List<String>> forms) {
+        HashSet<String> dupResult = new HashSet<>();
 
         for(List<String> form: forms) {
-            HashSet<String> dupMailSet = chckDuplicates(form, forms);
-            System.out.println(dupMailSet.toString());
-            dupUserSet.addAll(dupMailSet);
+            HashSet<String> dupUserMailSet = getDupUserMail(form, forms);
+
+            dupResult.addAll(dupUserMailSet);
         }
 
-        List<String> dupUserList = new ArrayList<>(dupUserSet);
+        List<String> dupUserList = new ArrayList<>(dupResult);
         Collections.sort(dupUserList);
 
         return dupUserList;
@@ -50,7 +62,7 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
-        answer = getDupUsers(forms);
+        answer = getDupResult(forms);
 
         return answer;
     }
