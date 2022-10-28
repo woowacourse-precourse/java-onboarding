ab214c44f736c0b029d86311c7b12e3a0a78d748
kimminsu <xjfcnfw3@naver.com>
2022-10-28 20:12:50 +0900

    feat(문제2): 반복 문자 존재를 확인하는 기능 구현

--- src/main/java/onboarding/problem2/Decryption.java
+++ src/main/java/onboarding/problem2/Decryption.java
@@ -1,11 +1,17 @@
 package onboarding.problem2;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Decryption {
     public static final String REPEAT_PATTERN = "([a-z])\\1+";
 
-    private static String removeRepeatedCharacters(String cryptogram){
+    private static String removeRepeatedCharacters(String cryptogram) {
         return cryptogram.replaceAll(REPEAT_PATTERN, "");
+    }
+
+    private static boolean hasNotRepeatedCharacters(String cryptogram, Pattern pattern) {
+        Matcher cryptogramMatcher = pattern.matcher(cryptogram);
+        return !cryptogramMatcher.find();
     }
 }
