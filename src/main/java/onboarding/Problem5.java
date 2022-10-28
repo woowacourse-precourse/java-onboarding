24651bbbee0c17cf6708fbc2339f0c9261dfde14
KSH <tkdgns543@gmail.com>
2022-10-29 02:11:05 +0900

    feat(Problem5): 화폐 종류 배열 순회하면서 정답 리스트에 총액 나누기 화폐크기 몫 추가, 남은 액수는 나눗셈으이 나머지

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,15 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        int totalMoney = money;
         int[] moneyTypes = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> answer = new ArrayList<>(9);
+
+        for (int type : moneyTypes) {
+            answer.add(totalMoney/type);
+            totalMoney %= type;
+        }
+
+        return answer;
     }
 }
