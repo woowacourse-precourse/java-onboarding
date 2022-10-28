abcb2af9169c7588e95b978981ff7aba29a1394f
matt <matt1235@naver.com>
2022-10-28 13:06:55 +0900

    feat: Problem3 전체 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,11 +3,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        check(number);
+        answer = count369(number, answer);
         return answer;
     }
 
     /**
-     * 입력값의 범위 지정, 오류 처리*/
+     * 입력값의 범위 지정*/
     private static void check(int number) {
         if(number < 1 || number > 10000)
             throw new IllegalArgumentException("숫자의 범위를 확인하세요");
@@ -24,5 +26,4 @@ public class Problem3 {
         }
         return answer;
     }
-
 }
