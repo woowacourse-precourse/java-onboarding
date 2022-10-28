dd010a486d852c94fbb5e7022154770c965298ea
sosow0212 <sosow0212@naver.com>
2022-10-28 17:36:30 +0900

    feat: Problem7 - static 변수 선언

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,11 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static final Map<String, Integer> userAndScoreMap = new HashMap<>();
+    private static final Set<String> myFriendsSet = new HashSet<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
