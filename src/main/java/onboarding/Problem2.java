67634eb4c6eba05e326baf061d1fe0070e6aa2cd
kkm5291 <kkm23125291@gmail.com>
2022-10-28 23:48:49 +0900

    ArrayIndexOutOfBoundsException 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,6 +13,10 @@ public class Problem2 {
             if (cryptoArr[currentIdx - 1] != cryptoArr[currentIdx]) {
                 cryptoArr[prevIdx++] = cryptoArr[currentIdx - 1];
             } else {
+                //두 word만 남았을 경우 while문에서 ArrayIndexOutOfBoundsException 가 발생했는데
+                //두개만 남았을 경우 0번째와 1번째 인덱스가 같다면 굳이 for문 뒷부분이 실행될 필요가 없음
+                if(cryptoArr.length == 2 && cryptoArr[0] == cryptoArr[1])
+                    return answer = "";
                 while (currentIdx < cryptoArr.length && cryptoArr[currentIdx - 1] == cryptoArr[currentIdx])
                     currentIdx++;
             }
