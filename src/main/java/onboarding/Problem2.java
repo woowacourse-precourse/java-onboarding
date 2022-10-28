c9b4b3ea25d1fd3af60f82be60919b53ca9f8eb1
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2022-10-28 14:33:46 +0900

    Problem2 : 중복 문자 여부 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,18 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    /***
+     * 중복 문자 여부 확인
+     * @param cryptogram
+     * @return
+     */
+    private static boolean check(String cryptogram) {
+        for (int i = 0; i < cryptogram.length()-1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
