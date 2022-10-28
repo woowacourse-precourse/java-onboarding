c06864b4b838538a27fe15fb349ae2519d3d39ea
Limjongho <whdgh9873@icloud.com>
2022-10-28 19:52:14 +0900

    feat(Problem4): 변환되는 문자 매퍼 변수를 갖는 일급 컬렉션을 위한 내부 클래스 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    static class ConvertMap{
+        private final Map<Character, Character> convertMap;
+
+        public ConvertMap(Map<Character, Character> convertMap) {
+            this.convertMap = convertMap;
+        }
+
+        public char getConvertedLetter(char letter) {
+            return convertMap.get(letter);
+        }
+
     }
 }
