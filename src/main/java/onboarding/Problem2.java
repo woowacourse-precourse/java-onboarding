01e2759889973d8d5cf2f1c4d3e450057616b7ef
Hojinida <opqrs1103@gmail.com>
2022-10-29 02:41:03 +0900

    feat: 만약 중복되지 않았다면 문자열에 더하는 add 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,12 @@ public class Problem2 {
         String answer="";
         return answer;
     }
+    public static void add(StringBuilder answer,String cryptogram,int index){
+        answer.append(cryptogram.charAt(index));
+        if(index==cryptogram.length()-2){
+            answer.append(cryptogram.charAt(index+1));
+        }
+    }
     public static boolean duplicate(String cryptogram,int index){
         return cryptogram.charAt(index) == cryptogram.charAt(index + 1);
     }
