1e41361f89bae929c9411b9020207ab4530191e8
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 17:44:17 +0900

    feat: Add upperChange in Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,6 +7,12 @@ public class Problem4 {
         return (char)(219 - ch);
     }
 
+    private static Character upperChange(char ch){
+        // A to Z, B to X... 모두 두 문자의 아스키코드 값을 더하면 155가 된다.
+        // 따라서 155에서 현재 문자의 아스키코드 값을 뺴면 변환 후 문자의 아스키코드 값이 된다.
+        return (char)(155 - ch);
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
