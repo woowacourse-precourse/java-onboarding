14a79dda133cb904eef791718fff36793bf0ce55
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-28 16:24:55 +0900

    feat: 1번 예외사항의 경우 메인 메소드가 -1를 리턴한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,34 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    /*
+    기능 목록
+ 1. 한 사람의 두 페이지가 바로 다음 페이지가 아니면 -1을 리턴하고 게임을 끝낸다.
+ 2. 페이지 수를 받아와 숫자들의 합을 구한다.
+ 3. 페이지 수를 받아와 숫자들의 곱을 구한다.
+ 4. 페이지 수의 합과 곱을 구해와 그 중 큰 값을 점수로 사용한다.
+ 5. 한 사람이 가질 수 있는 두 페이지중 더 큰 값을 사람의 점수로 사용한다.
+ 6. 두 사람의 점수를 구해와 더 큰 사람이 이긴다.
+    * */
+    static final int RIGHT_PAGE = 1;
+    static final int LEFT_PAGE = 0;
+    static final int PAGE_GAP = 1;
+    static final int POBI_WINS = 1;
+    static final int CRONG_WINS = 2;
+    static final int DRAWS = 0;
+    static final int ERROR = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(isNotValidPage(pobi) || isNotValidPage(crong)) return ERROR;
+        return getResult(getScore(pobi), getScore(crong));
     }
+    private static boolean isNotValidPage(List<Integer> person){
+        if(person.get(LEFT_PAGE) > MAX_PAGE || person.get(LEFT_PAGE) < MIN_PAGE) return true;
+        if(person.get(RIGHT_PAGE) > MAX_PAGE || person.get(RIGHT_PAGE) < MIN_PAGE) return true;
+        if(person.get(RIGHT_PAGE) - person.get(LEFT_PAGE) != PAGE_GAP) return true;
+        return false;
+    }
+
+
 }
