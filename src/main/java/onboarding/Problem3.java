24c6e0a3fdbeb6df74ca4eae43791db7a2c4d062
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 15:51:39 +0900

    [Problem3] int형 -> string형으로 자료형 변환 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,15 @@ public class Problem3 {
      * 2. 해당 숫자 3, 6, 9의 개수 찾기
      */
 
+    /**
+     * 1. int형 -> string형으로 변환
+     * @param number : 변환할 숫자
+     * @return : string으로 변환된 숫자
+     */
+    static String convert(int number){
+        return Integer.toString(number);
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
