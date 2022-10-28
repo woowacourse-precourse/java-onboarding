3d9707a4aff7fd8756410b2b224c5b0bbfa7b21c
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-28 14:53:19 +0900

    [Problem 4] 문자열 유효성 검사를 위한함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        if(!checkValidation(word)){
+            return null;
+        }
         return answer;
     }
+
+    private static boolean checkValidation(String word){
+        if(1 <= word.length() || word.length()<=1000){
+            return true;
+        }
+        return false;
+    }
+
 }
