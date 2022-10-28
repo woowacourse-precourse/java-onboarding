ee90bbcf5069e3e6b8762103ad21ad7fae2e2f85
kdkdhoho <hkim4410@naver.com>
2022-10-28 14:16:14 +0900

    refactor: Problem4 - 문자 변환 메소드 명 수정, 해당 메소드 간결화

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,25 +5,20 @@ public class Problem4 {
         String answer = "";
 
         for (char ch : word.toCharArray()) {
-            answer += changeWord(ch);
+            answer += convertWord(ch);
         }
 
         return answer;
     }
 
-    static char changeWord(char word) {
-        char newWord = 0;
-        int wordValue = word;
+    static char convertWord(char word) {
+        char newWord;
 
-        if(65 <= wordValue && wordValue <= 77) {            // [a-m]
-            newWord = (char)(wordValue + (Math.abs(77-wordValue)*2 + 1));
-        }else if(78 <= wordValue && wordValue <= 90) {      // [n-z]
-            newWord = (char)(wordValue - (Math.abs(78-wordValue)*2 + 1));
-        } else if(97 <= wordValue && wordValue <= 109) {    // [A-M]
-            newWord = (char)(wordValue + (Math.abs(109-wordValue)*2 + 1));
-        } else if(110 <= wordValue && wordValue <= 122) {   // [N-Z]
-            newWord = (char)(wordValue - (Math.abs(110-wordValue)*2 + 1));
-        } else {                                            // 알파벳 외의 문자
+        if('a' <= word && word <= 'z') {            // 소문자
+            newWord = (char) ('a' + 'z' - word);
+        } else if ('A' <= word && word <= 'Z') {    // 대문자
+            newWord = (char) ('A' + 'Z' - word);
+        } else {                                    // 알파벳 외의 문자
             newWord = word;
         }
 
