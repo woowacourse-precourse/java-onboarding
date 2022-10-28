fc3b96d7351fd236d531b6d002ac2d565fa7438c
jin8073 <jin8073@gmail.com>
2022-10-28 17:41:32 +0900

    feat(problem3): 한 숫자를 매개변수로 받아 그 숫자에 3, 6, 9가 몇 번 들어가는지 세어서 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+/** 구현 기능 목록
+ * 1. 한 숫자를 매개변수로 받아 그 숫자에 3, 6, 9가 몇 번 들어가는지 세어서 반환
+ * 2. 1부터 매개변수로 받은 숫자까지 총 3, 6, 9가 몇 번 들어가는지 세어서 반환
+ * */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int count369(int number) {
+        int count = 0;
+        while (number > 0) {
+            if ((number % 10) % 3 == 0)
+                count++;
+            number /= 10;
+        }
+        return count;
     }
 }
