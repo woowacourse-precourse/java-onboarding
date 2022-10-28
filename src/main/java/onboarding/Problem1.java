e67e65736dbfadaa9acb9f520728ae650d800058
mywnajsldkf <mywnajsldkf@gmail.com>
2022-10-29 01:24:08 +0900

    feat : 유효한 페이지 리스트인 경우, 계산하고 결과를 출력한다.
    - 각 player의 점수 비교

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,15 +1,52 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         if (!isValidPage(pobi) || !isValidPage(crong)) {
-            answer = -1;
+            return -1;
         }
+        answer = calculateGameResult(pobi, crong);
 
         return answer;
+    }
+
+    private static int calculateGameResult(List<Integer> pobi, List<Integer> crong) {
+        if (calculateScore(pobi) > calculateScore(crong)) {
+            return 1;
+        } else if (calculateScore(pobi) < calculateScore(crong)) {
+            return 2;
+        }
+        return 0;
+    }
+
+    private static int calculateScore(List<Integer> player) {
+        int[] leftPage = integerToDigit(player.get(0));
+        int[] rightPage = integerToDigit(player.get(1));
+
+        int leftResult = calculatePageNumber(leftPage);
+        int rightResult = calculatePageNumber(rightPage);
+
+        return Math.max(leftResult, rightResult);
+    }
+
+    private static int calculatePageNumber(int[] page) {
+        int add = 0;
+        int multiple = 1;
+        for (int j : page) {
+            add += j;
+            multiple *= j;
+        }
+
+        return Math.max(add, multiple);
+    }
+
+    private static int[] integerToDigit(Integer integer) {
+        return Stream.of(String.valueOf(integer).split("")).mapToInt(Integer::parseInt).toArray();
     }
 
     private static boolean isValidPage(List<Integer> page) {
