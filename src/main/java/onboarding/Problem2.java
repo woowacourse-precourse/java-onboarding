baa49ce13c3933119edfeee4d73ad79f550cd7c1
homecoder98 <homecoder98@gmail.com>
2022-10-28 23:51:29 +0900

    Feat : Remove overlay character using reculsive

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return removeOverlay(cryptogram);
+    }
+
+    static String removeOverlay(String s) {
+        Integer cnt = 0;
+        for (int i=1;i<s.length();i++) {
+            if (s.charAt(i) == s.charAt(i-1)) {
+                cnt++;
+            } else if (cnt > 0) {
+                return removeOverlay(s.substring(0, i-cnt-1) + s.substring(i));
+            }
+            if (s.length() == 2 && cnt > 0) return "";
+        }
+        return s;
     }
 }
