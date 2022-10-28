c56e7965e76755449d274382c6e0abcab1749bf8
sonsoowon <yuwoo1018@gmail.com>
2022-10-28 13:38:03 +0900

    feat(P4): solution 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        Stream<Character> tokenStream = word.chars().mapToObj(i -> (char) i);
+        Stream<Character> reversedStream = tokenStream.map(token -> reverse(token));
+        answer = reversedStream.toString();
+
         return answer;
     }
     static char reverse(char token){
