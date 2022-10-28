f303aaff410a8331e7da38ea7289455448819874
star-sil <kimse9811@naver.com>
2022-10-29 00:46:23 +0900

    SOLVE problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (Integer i = 1; i < number + 1; i++) {
+            String stringNum = i.toString();
+            answer += countClap(stringNum);
+        }
         return answer;
+    }
+
+    //숫자로부터 쳐야될 박수 구하기
+  private static int countClap(String number) {
+        int count = 0;
+        for (int i = 0; i < number.length(); i++) {
+            if (isClap(number.charAt(i))) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isClap(char c) {
+        return c == '3' | c == '6' | c == '9';
     }
 }
