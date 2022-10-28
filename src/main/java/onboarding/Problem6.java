98eacfcdd1b63690a4d5882f260afe70cd334488
ljh <dusk5588@naver.com>
2022-10-28 17:16:16 +0900

    P6 -> 닉네임 2글자씩을 비교하여 곂치는 경우가 있다면, 그 2명의 이메일을 treeset에 저장하여 정답을 반환하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -16,6 +16,18 @@ public class Problem6 {
             //신청한 닉네임을 두글자씩 잘라서 HashSet에 저장(중복 제거)
             for(int i=0; i<nickname.length()-1; i++)
                 hashSet.add(nickname.substring(i, i+2));
+            
+            for(String s : hashSet){
+                //잘라낸 닉네임의 2글자가 hashMap에 없으면 추가
+                if(!hashMap.containsKey(s))
+                    hashMap.put(s, list.get(0));
+                //이미 hashMap에 존재한다면 이어진 두글자가 곂치는 다른 닉네임이 존재한다는 것.
+                //이 닉네임을 신청한 사람의 이메일과 hashmap에 등록한 사람의 이메일을 treeset에 저장.
+                else{
+                    treeSet.add(list.get(0));
+                    treeSet.add(hashMap.get(s));
+                }
+            }
         }
 
         List<String> answer = new ArrayList<>(treeSet);
