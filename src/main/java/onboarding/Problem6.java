becc6f9569b8f8fc0d14d3a14eedb2db6d22102a
wlsh44 <k87913j@gmail.com>
2022-10-29 03:03:56 +0900

    chore: 필요 없는 코드 제거 [문제6]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,6 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
