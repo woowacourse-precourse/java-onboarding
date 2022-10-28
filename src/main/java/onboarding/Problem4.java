3231f8d8e2764b42421dff12acb0cd87f2b489bc
shpark30 <seohyeonpark30.gmail.com>
2022-10-29 00:43:00 +0900

    feat(Problem4.java): char 배열에서 대문자 알파벳 위치 배열 반환 기능추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    static List<Integer> getUpperCharIndex(char[] wordCharacter) {
+        List<Integer> upperCharIndex = new ArrayList<>();
+        for (int i = 0; i < wordCharacter.length; i++) {
+            int asciiChar = (int)wordCharacter[i];
+            if (asciiChar >= 97 & asciiChar <=122) {
+                upperCharIndex.add(i);
+            }
+        }
+        return upperCharIndex;
     }
 }
