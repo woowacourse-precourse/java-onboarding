3e5b5d0f053b11b4c1a8cfcb0c5ab912ee6384e1
hectick <chyeon1223@cau.ac.kr>
2022-10-29 01:45:53 +0900

    feat : [Problem6] 기능 목록 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        //중복 닉네임이 될 수 있는 단어들의 경우의 수를 뽑아서 map에 <단어, 이메일>로 저장하는 기능
+        //map에 저장된 단어가 사용자의 닉네임에 들어가는지 체크하는 기능
+        //중복 닉네임이 될 경우 이메일을 set에 추가하는 기능
+        //set을 list로 변경하여 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 메일 목록을 반환하는 기능
+
         return answer;
     }
 }
