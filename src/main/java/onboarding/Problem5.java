fc3548aaf2424838ab87644d70de08a4083c675a
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 09:38:29 +0900

    feat : counting wonUnit as to currency unit accordingly

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,6 +10,12 @@ public class Problem5 {
         int moneyCount = 0;
         int wonUnit[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        for (int i = 0; i < 9; i++){
+            moneyCount = money / wonUnit[i];
+            money = money % wonUnit[i];
+//            answer.add(moneyCount);
+            System.out.printf("%d %d %d \n", i, moneyCount, money);
+        }
         return answer;
     }
 }
