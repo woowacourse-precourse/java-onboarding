a43bc1440cb4b8d8ab98afae0dc6e0445900e81f
junho lee <junho1273@gmail.com>
2022-10-28 13:44:14 +0900

    Problem3 369 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        //입력받은 number만큼 반복
+        for(int i = 0 ; i <= number ; i++){
+            int count = i;
+            // 각 자리 수 마다 10으로 나눠가며 369를 확인
+            while (count > 1){
+                if(count % 10 == 3 || count % 10 == 6 || count % 10 == 9){
+                    answer++;
+                }
+                count /=10;
+            }
+        }
         return answer;
     }
 }
