c651123e5228c29fd83525ce80a5a45d19a4fa37
kevstevie <jianna442@gmail.com>
2022-10-28 17:17:37 +0900

    feat: add nicknamesToTwoLetters

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
+    }
+
+    static Map<String, Integer> makeTwoLetters(List<List<String>> forms){
+        Map<String, Integer> map = new HashMap<>();
+
+        for(List list : forms){
+            String str = (String)list.get(1);
+            putDevidedIntoMap(str, map);
+        }
     }
 }
