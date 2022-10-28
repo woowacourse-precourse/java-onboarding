66554d89ba583450e83a14ae4c6c70e2e6e65847
HubCreator <chocomilk8604@gmail.com>
2022-10-28 10:17:07 +0900

    feat(Problem6) : 연속된 문자 구분 로직 구현

--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -11,8 +11,12 @@ class Problem6Test {
     void nameMatchingTest() {
         String str1 = "제이엠";
         String str2 = "이엠제";
-        String str3 = "크롱";
+        String str3 = "제엠이";
+        String str4 = "크롱";
+        String str5 = "크크크크크크크크크크크롱";
         assertThat(Problem6Util.isDuplicated(str1, str2)).isTrue();
         assertThat(Problem6Util.isDuplicated(str2, str3)).isFalse();
+        assertThat(Problem6Util.isDuplicated(str2, str4)).isFalse();
+        assertThat(Problem6Util.isDuplicated(str4, str5)).isTrue();
     }
 }
