a964321c37c7eac176d00e97a5933ccc8e2da953
SH <gram753@naver.com>
2022-10-28 23:37:52 +0900

    feat: Problem4 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,48 @@
 package onboarding;
 
 public class Problem4 {
+
+	static char[] upperCaseList = new char[26];
+	static char[] lowerCaseList = new char[26];
+
 	public static String solution(String word) {
 		String answer = "";
-		return answer;
+
+		for (int i = 0; i < 26; i++) {
+			upperCaseList[i] = (char)(i + 65);
+			lowerCaseList[i] = (char)(i + 97);
+		}
+		return reverseWord(word);
+
 	}
+
+	private static String reverseWord(String word) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < word.length(); i++) {
+			char changedAlphabet = reverseAlphabet(word.charAt(i));
+			stringBuilder.append(changedAlphabet);
+		}
+		return stringBuilder.toString();
+	}
+
+	private static char reverseAlphabet(char alphabet) {
+		if (alphabet > 96 && alphabet < 123) {
+			return reverseLowerCase(alphabet);
+		}
+		if (alphabet > 64 && alphabet < 91) {
+			return reverseUpperCase(alphabet);
+		}
+		return alphabet;
+	}
+
+	private static char reverseUpperCase(char alphabet) {
+		int index = alphabet - 'A';
+		return upperCaseList[25 - index];
+	}
+
+	private static char reverseLowerCase(char alphabet) {
+		int index = alphabet - 'a';
+		return lowerCaseList[25 - index];
+	}
+
 }
