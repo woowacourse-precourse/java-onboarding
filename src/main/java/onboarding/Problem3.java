d3495f0525d9f9ae455392267a56c5a640885001
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-28 15:25:58 +0900

    Problem3(설계): 기능 목록 작성
    Problem3(설계): 기능 목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,24 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * 369 게임에서 1 부터 주어진 숫자까지 손뼉을 몇 번 쳐아하는지 횟수를 계산하는 메소드
+     * @param number : 369 게임의 마지막 숫자
+     * @return : 마지막 숫자까지 손뼉을 친 횟수
+     */
+    private static int getClapCount(int number){
+
+    }
+
+    /**
+     * n의 자리숫자에 의해 손뼉을 친 횟수를 반환하는 함수
+     * @param number : 손뼉을 친 횟수를 계산할 숫자
+     * @param digitBase : 손뼉을 친 횟수를 계산할 자리
+     * @return : n의 자리 숫자에서 손뼉을 친 횟수
+     */
+    private static int digitBaseCount(int number, int digitBase){
+
+    }
+
 }
