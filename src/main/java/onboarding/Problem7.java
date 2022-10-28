3796802b2173bc555614937b6dc13af53d26106d
JunDonghyuk <ehdgur4814@naver.com>
2022-10-29 00:04:42 +0900

    addFriendScore 구현
    친구 관계로 인한 점수 추가 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,16 +5,28 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        // 친구 리스트
-        ArrayList<String> friendsList = new ArrayList<String>();
+        // 친구명과 점수
+        HashMap<String, Integer> friendsScore = new HashMap<>();
 
         // 친구 관계를 표시할 HashMap
         HashMap<String, ArrayList<String>> friendsMap = new HashMap<String, ArrayList<String>>();
 
-        //
+        // 친구 관계 friendsMap 만들기
         for(List<String> friendsRelation : friends) {
             makeFriendsMap(friendsRelation, friendsMap);
         }
+
+        // 친구들의 Score 초기화
+        for(String friend : friendsMap.keySet()) {
+            makeFriendsScore(friend, user, friendsScore);
+        }
+
+        // 친구 관계 점수 추가
+        for(String friend : friendsScore.keySet()) {
+            addFriendScore(friend, friendsScore, getSameFriendsScore(user, friend, friendsMap));
+        }
+
+        // 
         return answer;
     }
 
@@ -34,4 +46,27 @@ public class Problem7 {
             friendsMap.put(friend2, new ArrayList<>(Arrays.asList(friend1)));
         }
     }
+
+    public static void makeFriendsScore(String friend, String user, HashMap<String, Integer> friendsScore) {
+        if(!friend.equals(user)) {
+            friendsScore.put(friend, 0);
+        }
+    }
+
+    public static Integer getSameFriendsScore(String user, String friend, HashMap<String, ArrayList<String>> friendsMap) {
+        int score = 0;
+        for(String userFriend : friendsMap.get(user)) {
+            if(friendsMap.get(friend).contains(userFriend)) {
+                score += 10;
+            }
+        }
+        return score;
+    }
+
+    public static void addFriendScore(String friend, HashMap<String, Integer> friendsScore, Integer value) {
+        Integer curScore = friendsScore.get(friend);
+        curScore += value;
+        friendsScore.put(friend, curScore);
+    }
+
 }
