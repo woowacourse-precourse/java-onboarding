982fb3a04e34e0acae0335b1b12ccbe2176f1b16
woosung1223 <dntjd991223@naver.com>
2022-10-28 15:40:39 +0900

    feat(Problem6): validationCheck 함수 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,9 +11,16 @@ public class Problem6 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+    public static boolean validationCheck(List<List<String>> forms) {
+        if (inputValidaiton(forms) && contentValidation(forms)) {
+            return true;
+        }
+        return false;
+    }
     public static void splitLexim(List<String> repository, String name) {
         for (int j = 0; j < name.length()-1; j++) {
             repository.add(name.substring(j, j+2));
         }
     }
+
 }
