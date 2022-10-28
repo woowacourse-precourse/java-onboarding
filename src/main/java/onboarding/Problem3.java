1e09658ba652e93aaabcc62eee5f6c2feafec36e
SEONGJOON KIM <jobcho6320@naver.com>
2022-10-28 17:48:41 +0900

    feat : Problem3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            char[] numCharArr = Integer.toString(i).toCharArray();
+            for (char c : numCharArr) {
+                if (c == '3' || c == '6' || c == '9') {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
