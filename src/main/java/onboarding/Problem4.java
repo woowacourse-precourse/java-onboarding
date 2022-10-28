f5fa9e03e64dab92f721d10ac585715e4d90e10e
chj <bluedog129@gmail.com>
2022-10-28 16:45:03 +0900

    [P4] 대소문자 변환 조건 x 따라서기능 삭제

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,14 +1,6 @@
 package onboarding;
 
 public class Problem4 {
-    private static char convertUpperLower(char c) {
-        if (Character.isUpperCase(c)) {
-            c = Character.toLowerCase(c);
-        } else if (Character.isLowerCase(c)) {
-            c = Character.toUpperCase(c);
-        }
-        return c;
-    }
     private static char convertAscii(char c) {
         if ('a' <= c && 'z' >= c) {
             c = (char)('z' - (c - 'a'));
@@ -21,7 +13,6 @@ public class Problem4 {
         char[] wordArr = word.toCharArray();
         for (int i = 0; i < word.length(); i++) {
             wordArr[i] = convertAscii(wordArr[i]);
-            wordArr[i] = convertUpperLower(wordArr[i]);
         }
         word = String.valueOf(wordArr);
         return word;
