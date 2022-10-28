ff42ca37b5a533f0e4424cbf1f07f8c409963f6c
jeongwle <dldl4568@naver.com>
2022-10-28 14:43:55 +0900

    [Feat] Problem4 완료  - 주어진 word를 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,8 +5,7 @@ public class Problem4 {
         if (!isValidate(word)) {
             return "error";
         }
-        String answer = "";
-        return answer;
+        return convertString(word);
     }
 
     /**
@@ -15,5 +14,33 @@ public class Problem4 {
     public static boolean isValidate(String word) {
         int length = word.length();
         return (length > 0 && length < 1001);
+    }
+
+    public static String convertString(String word) {
+        StringBuilder sb = new StringBuilder();
+        char[] toCharArray = word.toCharArray();
+        for (char ch : toCharArray) {
+            if (isAlpha(ch)) {
+                sb.append(convertAlpha(ch));
+                continue;
+            }
+            sb.append(ch);
+        }
+        return String.valueOf(sb);
+    }
+
+    public static char convertAlpha(char alpha) {
+        int index = getIndexOfAlpha(alpha);
+        int covert = 27 - 2 * index;
+        return (char) (alpha + covert);
+    }
+
+    public static int getIndexOfAlpha(char alpha) {
+        char upperCase = Character.toUpperCase(alpha);
+        return upperCase - 'A' + 1;
+    }
+
+    public static boolean isAlpha(char ch) {
+        return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
     }
 }
