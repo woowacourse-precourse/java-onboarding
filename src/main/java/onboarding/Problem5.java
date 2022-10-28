99f04be7795c88cba139cba5a6f7abd287ce723d
devjordy14 <devjordy14@gmail.com>
2022-10-28 15:41:15 +0900

    more 1.do functionalization

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,42 +13,33 @@ import java.util.List;
 // 2. 받은 자연수를 각 단위로 나누어준다.
 // 3. 나눈 몫을 차례대로 리스트에 담은 후 반환한다.
 
+// more
+// 1. 함수화 하는 방법 생각해보기
+// 2. 단위를 리스트화해서 반복문으로 함수를 돌리는 방법 생각해보기
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
 
         answer = new ArrayList<>();
 
-        answer.add(money / 50000);
-        money = (money % 50000);
-
-        answer.add(money / 10000);
-        money = (money % 10000);
-
-        answer.add(money / 5000);
-        money = (money % 5000);
-
-        answer.add(money / 1000);
-        money = (money % 1000);
-
-        answer.add(money / 500);
-        money = (money % 500);
-
-        answer.add(money / 100);
-        money = (money % 100);
-
-        answer.add(money / 50);
-        money = (money % 50);
-
-        answer.add(money / 10);
-        money = (money % 10);
-
-        answer.add(money / 1);
-        money = (money % 1);
-
-        System.out.println(money);
-        System.out.println(answer);
+        money = changeDivider(50000, money, answer);
+        money = changeDivider(10000, money, answer);
+        money = changeDivider(5000, money, answer);
+        money = changeDivider(1000, money, answer);
+        money = changeDivider(500, money, answer);
+        money = changeDivider(100, money, answer);
+        money = changeDivider(50, money, answer);
+        money = changeDivider(10, money, answer);
+        money = changeDivider(1, money, answer);
 
         return answer;
     }
+
+    public static int changeDivider(int unit, int money, List<Integer> answerList){
+        answerList.add(money / unit);
+        money = (money % unit);
+        return money;
+    }
+
 }
