d9ad2bef18a8282a2689da0f5a4db113a315d024
khj1 <dhepd8849@gmail.com>
2022-10-28 16:24:23 +0900

    feat(problem4): 소문자인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    public static final char A_LOWER_CASE = 'a';
+    public static final char Z_LOWER_CASE = 'z';
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return convertIntoBlueFrogWord(word);
+    }
+
+    private static String convertIntoBlueFrogWord(String word) {
+        char[] letters = word.toCharArray();
+        for (char letter : letters) {
+            if (isLowerCase(letter)) {
+            }
+        }
+        return null;
+    }
+
+    private static boolean isLowerCase(char letter) {
+        return letter >= A_LOWER_CASE && letter <= Z_LOWER_CASE;
     }
 }
