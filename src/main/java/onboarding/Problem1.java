643cedce37b4e0852168e9b092cac609e4354aa2
cksdud8028 <cksdud8028@naver.com>
2022-10-28 15:36:28 +0900

    moved location judgmentException

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,9 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
+
         return answer;
     }
 
@@ -16,13 +19,6 @@ class Problem1 {
         return firstNumber * secondNumber;
     }
 
-    private static int judgmentException(int firstNumber, int secondNumber) {
-        if (secondNumber - firstNumber > 1) {
-            return -1;
-        }
-        return 0;
-    }
-
     private static int comparingNumbers(int addedNum, int multipliedNum) {
         return addedNum > multipliedNum ? addedNum : multipliedNum;
     }
@@ -32,6 +28,13 @@ class Problem1 {
             return 1;
         } else if (pobiNumber < crongNumber) {
             return 2;
+        }
+        return 0;
+    }
+
+    private static int judgmentException(int firstNumber, int secondNumber) {
+        if (secondNumber - firstNumber > 1) {
+            return -1;
         }
         return 0;
     }
