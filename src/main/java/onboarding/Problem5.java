1f0428c2b7a551ba153e50086abd4235501c41bc
jeujeon <1132jjw@naver.com>
2022-10-28 20:53:29 +0900

    feat: add convert money feat in Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,11 +9,24 @@ public class Problem5 {
     static final int MIN_MONEY = 1;
     static final int MAX_MONEY = 1000000;
     static final List<Integer> ERROR = Collections.emptyList();
+    static final int LIST_SIZE = 9;
+    static final int[] CURRENCYS = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         if (!isValidInput(money)) {
             answer = ERROR;
             return answer;
+        }
+        answer = convertMoney(money);
+        return answer;
+    }
+
+    private static List<Integer> convertMoney(int money) {
+        List<Integer> answer = new ArrayList<Integer>(LIST_SIZE);
+
+        for (int currency : CURRENCYS) {
+            answer.add(money / currency);
+            money %= currency;
         }
         return answer;
     }
