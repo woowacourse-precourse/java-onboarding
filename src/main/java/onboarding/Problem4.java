16d1638911ffa65f9519a8888aa41752b7135d4d
ymkim97 <rladudaud97@naver.com>
2022-10-28 23:45:44 +0900

    feat: add function boundary exception

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        String[] stringArray = word.split(" ");
+
+        if (throwBoundaryException(word)) {
+            return "failed with boundary exception";
+        }
+
+        return ;
+    }
+
+    public static boolean throwBoundaryException(String word) {
+
+        if (word.length() < 1 || word.length() > 1000) {
+            return true;
+        }
+        return false;
     }
 }
