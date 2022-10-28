9444b37ef98d1b38a1771ecc74fb9db9d82a8adb
nakalic <nakalic28@gmail.com>
2022-10-28 19:05:13 +0900

    feat(Problem4): 변환 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,4 +9,17 @@ public class Problem4 {
 	private static boolean isAlphabet(char c) {
 		return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z');
 	}
+
+	private static char convertAlphabet(char c) {
+
+		if (!isAlphabet(c))
+			return c;
+
+		if (Character.isUpperCase(c))
+			return (char)(155 - (int)c);
+
+		else
+			return (char)(219 - (int)c);
+
+	}
 }
