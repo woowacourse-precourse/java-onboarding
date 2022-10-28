34cee5c9d458e297f0017700fe4040d499cace9e
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:04:36 +0900

    docs(문제2): 기능목록 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/**
+ * 📚 기능 목록
+ *
+ * 1. 중복 문자의 존재여부를 확인하는 기능
+ * 2. 문자 중복을 제거하는 기능
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
