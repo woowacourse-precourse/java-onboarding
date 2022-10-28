013b4e44824f8ca87b72b38f8438bf4a6effdb3c
Dabeen Jeong <hello70825@gmail.com>
2022-10-28 16:45:04 +0900

    feat: [Problem4] 올바른 입력값이 들어왔는지 확인하는 기능을 추가하다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        if (checkValidation(word)) {
+
+        } else {
+            answer = "-1";
+        }
+
         return answer;
+    }
+
+    public static boolean checkValidation(String inputString) {
+        int inputStringLength = inputString.length();
+
+        // 길이가 1 이상 1,000 이하인지 확인한다.
+        if (!(1 <= inputStringLength && inputStringLength <= 1000)) {
+            return false;
+        }
+
+        return true;
     }
 }
