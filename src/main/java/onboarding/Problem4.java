910a7007219d213ef73e87c750d8e806781fb9dd
swandevson <shai4956@gmail.com>
2022-10-28 20:03:13 +0900

    feat: P4 단어 변환기 작성 (solve)
    각 문자가 알파벳일 경우 청개구리 문자로 변환하여 새로운 단어로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,7 @@ public class Problem4 {
 
 
     public static char convertAlpahbet(char ch) {   // 해당 문자를 변환
-        char converted;
+        char converted = ch;
 
         if (Character.isUpperCase(ch)) {
             converted = (char)('Z' - (ch - 'A'));
@@ -16,10 +16,24 @@ public class Problem4 {
         return converted;
     }
 
+    public static String convertWord(String word) {
+        String converted = "";
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                ch = convertAlpahbet(ch);
+            }
+            converted += ch;
+        }
+
+        return converted;
+    }
 
 
     public static String solution(String word) {
         String answer = "";
+
+        answer = convertWord(word);
 
         return answer;
     }
