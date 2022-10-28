da9be50a257c1d3819a88429bd3cadf72e1d041f
shinyehwan <jghisjl007@naver.com>
2022-10-28 14:00:18 +0900

    Problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		String answer = "";
+
+		// 먼저 String을 하나하나 char로 바꿔준다.
+		char[] chars = word.toCharArray();
+
+		// char 하나하나 만큼 반복하여
+		for (int i = 0; i < chars.length; i++) {
+			// 공백이면 공백을 리턴
+			if (chars[i] == ' ') {
+				answer += ' ';
+			}
+			// 대문자는 대문자 (아스키 코드)
+			if (Character.isUpperCase(chars[i])) {
+				answer += (char)(155 - (int)chars[i]);
+			}
+			// 소문자는 소문자 (아스키 코드)
+			if (Character.isLowerCase(chars[i])) {
+				answer += (char)(219 - (int)chars[i]);
+			}
+		}
+
+		return answer;
+	}
 }
