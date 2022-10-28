de62c86747ec655123663a62ab4a8c1eb5b16af3
joohyeongKim <wnguddl96@knou.ac.kr>
2022-10-28 19:27:14 +0900

    구현 기능 정의

---
+++ src/main/java/onboarding/problem2/Converter.java
@@ -0,0 +1,2 @@
+package onboarding.problem2;public class Converter {
+}
---
+++ src/main/java/onboarding/problem2/Limiter.java
@@ -0,0 +1,8 @@
+package onboarding.problem2;
+
+public class AssertLength {
+
+    //길이가1이상1000이하
+    void limitLength(String cryptogram){
+    }
+}
