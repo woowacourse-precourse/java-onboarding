3b87cef80ccf868a30dee714edf8f1d5bf16718e
Jueun <jueun000101@gmail.com>
2022-10-28 16:10:33 +0900

    feat: 중복 문자가 있는지, 있다면 시작과 끝 위치도 찾는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,11 +7,42 @@ package onboarding;
  * 3. 연속하는 중복 문자를 삭제할 수 있을 때까지 삭제하는 함수
  */
 public class Problem2 {
+    static class Duplication {
+        Boolean isDupl;
+        Integer startPos, endPos;
+
+        public Duplication(boolean c, int s, int e) {
+            this.isDupl = c;
+            this.startPos = s;
+            this.endPos = e;
+        }
+    }
+
     static String deleteDupl(String cry, int startPos, int endPos) {
         String front = cry.substring(0, startPos);
         String back = cry.substring(endPos);
         return front + back;
     }
+
+    static Duplication hasDuplStr(String cry) {
+        if (cry.isEmpty() || cry.length() == 1) {
+            return new Duplication(false, 0, 0);
+        }
+        for (int i = 1; i < cry.length(); i++) {
+            if (cry.charAt(i-1) != cry.charAt(i)) {
+                continue;
+            }
+            int j;
+            for (j = i + 1; j < cry.length(); j++) {
+                if (cry.charAt(i-1) != cry.charAt(j)) {
+                    break;
+                }
+            }
+            return new Duplication(true, i-1, j);
+        }
+        return new Duplication(false, 0, 0);
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
