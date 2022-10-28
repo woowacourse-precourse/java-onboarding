fc7c649d13e874bb251415202994d0b4f72ae657
soyunim <togksk123@naver.com>
2022-10-29 00:30:20 +0900

    Problem1 1)open books, check pages

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int[] page = {pobi.get(0), pobi.get(1) ,crong.get(0),crong.get(1)}; // pages
         return answer;
     }
 }
