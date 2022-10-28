64db5c965a4bb28cbc59422215f43cb629aa8462
junhei Lee <jun2ee22@naver.com>
2022-10-28 19:35:30 +0900

    feat:{사용자 = 사용자의 친구들} 매핑하는 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    //{인간=[이 인간의 친구들]} 매핑
+    private static Map<String, List<String>> friendsOfPerson = new HashMap<>();
+
+
+
+    private static Map<String, List<String>> mapFriendsOfPerson(List<List<String>> friends, String person) {
+        List<String> friendsList = new ArrayList<>();
+
+        for(List<String> friend : friends){
+            if (friend.indexOf(person) == 0) {
+                friendsList.add(friend.get(1));
+            } else if (friend.indexOf(person) == 1) {
+                friendsList.add(friend.get(0));
+            }else{
+                continue;
+            }
+        }
+        friendsOfPerson.put(person, friendsList);
+        return friendsOfPerson;
+    }
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+
         return answer;
     }
 }
