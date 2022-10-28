7ede2fe6e1d87a4c4af762bff0db3cf0a85d8d2d
matt <matt1235@naver.com>
2022-10-28 12:00:25 +0900

    feat: Problem2 (기능 목록1 - 인자의 유효성 검사) 기능 구현 인자의 유효성을 검사하는 기능을 구현했습니다. 인자의 크기 범위(1~1000), 연속된 문자 확인, 소문자 알파벳만 포함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
+    /**
+     * 기능 목록 1번: 인자의 유효성 검사 메서드
+     * */
+    private static void check(String cryptogram) {
+        if(cryptogram.length() < 1 || cryptogram.length() > 1000)
+            throw new IllegalArgumentException("인자의 길이를 확인해주십시오.");
+        for (char c : cryptogram.toCharArray()) {
+            if(c==' ') throw new IllegalArgumentException("연속된 문자여야 합니다.");
+            if (c < 'a' || c > 'z')
+                throw new IllegalArgumentException("소문자 알파벳이 아닌 문자가 포함되어있습니다.");
+        }
+    }
+
 }
