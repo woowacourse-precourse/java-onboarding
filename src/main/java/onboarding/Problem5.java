9a69511073b9ded724dd9a4c897c69931306512c
Namyunsuk <yunsuk0328@naver.com>
2022-10-28 23:17:52 +0900

    feat(validateMoney): 돈의 액수를 확인하고 예외를 처리하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,14 @@ class money{
     static final int[] moneyList = {50000,10000,5000,1000,500,100,50,10,1};
     static final String invalidArgument = "InvalidArgument";
 }
-
+class validateMoney{
+    public static boolean isDivide(int money,int divideMoney){
+        return (money/divideMoney)>0;
+    }
+    public static boolean isValidAmount(int money){
+        return (money>=1)&&(money<=1_000_000);
+    }
+}
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
