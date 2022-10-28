bbfd47d11e9971df42acfb6f33a55809b319e5d8
ECHO <eunchan1001@gmail.com>
2022-10-28 17:46:27 +0900

    feat(Problem2): 중복된 문자가 없을때까지 반복하는 solution 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,8 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String answer = cryptogram;
+        while (true) {
             // 문자열의 길이가 0일 경우 바로 종료
             if (answer.length() == 0) {
                 return answer;
@@ -12,5 +12,14 @@ public class Problem2 {
             if (isReversible(answer)) {
                 return "";
             }
+            // 전 문자열을 저장
+            String prev_answer = answer;
+            // 중복 제거
+            answer = getRemovedDuplicateString(answer);
+            // 전 문자열과 중복 제거한 문자열이 같을 경우 문자열 반환 후 종료
+            if(answer == prev_answer) {
+                return answer;
+            }
+        }
     }
 }
