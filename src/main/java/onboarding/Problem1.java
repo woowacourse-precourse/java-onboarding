61806fec760cc180a0a9b2a5eebd989329c8a488
June <68291395+june-777@users.noreply.github.com>
2022-10-28 13:08:06 +0900

    feat: 왼쪽페이지 범위확인 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -21,8 +21,17 @@ class Problem1 {
     	
     	if((pobi.get(0)==null) || (pobi.get(1)==null) || (crong.get(0)==null) || (crong.get(1)==null)) {
     		return true;
-    	} else {
-    		return false;
     	}
+    	return false;
+    }
+    public static boolean isLeftPageInRange(List<Integer> pobi, List<Integer> crong) {
+    	
+    	int pobiLeftPage = pobi.get(0);
+    	int crongLeftPage = crong.get(0);
+    	
+    	if(1<=pobiLeftPage && pobiLeftPage<=399 && 1<=crongLeftPage && crongLeftPage<=399) {
+    		return true;
+    	}
+    	return false;
     }
 }
