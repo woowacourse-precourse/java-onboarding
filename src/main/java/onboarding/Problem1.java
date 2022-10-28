d158080b896a010dca1b16a506b3f4994092107c
Hojinida <opqrs1103@gmail.com>
2022-10-29 00:09:41 +0900

    feat: 예외가 있다면 true값을 반환해 주는 exceptionHandler 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,22 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
+    private static final int DRAW=0;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int EXCEPTION = -1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer=Integer.MAX_VALUE;
+        if(exceptionHandler(pobi, crong))
+            return EXCEPTION;
         return answer;
     }
+
+    public static boolean exceptionHandler(List<Integer> pobi, List<Integer> crong){
+        return false;
+    }
+
 }
