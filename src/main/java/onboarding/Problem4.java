c33b0a5b7fa1c7526c76533066bee445046d0e17
akskflwn <akskflwn@gmail.com>
2022-10-28 18:55:11 +0900

    (feat) Problem4 1차 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        char[] wordList = word.toCharArray();
+
+
+        String answer = getWords(wordList);
         return answer;
     }
+
+    private static String getWords(char[] wordList) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < wordList.length; i++) {
+            if (Character.isWhitespace(wordList[i])) {
+                sb.append(" ");
+                continue;
+            }
+            if(Character.isUpperCase(wordList[i])){
+                sb.append((char)(155-(int) wordList[i]));
+            } else {
+                sb.append((char)(219-(int) wordList[i]));
+            }
+            System.out.println(sb);
+        }
+        return sb.toString();
+    }
+
 }
