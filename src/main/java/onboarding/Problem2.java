0e173265dadd685a5cb440274a3a08c7b4acccee
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-10-28 14:32:29 +0900

    Problem2 기능 목록 3 : 중복 판별 후, LinkedList에 저장한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,10 +4,28 @@ import java.util.LinkedList;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer="";
 
         char[] characterArray = cryptogram.toCharArray();
         LinkedList<Character> characterList = new LinkedList<>();
+
+
+        characterList.add(characterArray[0]);
+        char savePreChar = characterArray[0];
+
+        for (int i = 1; i < characterArray.length; i++) {
+
+            if(characterArray[i] == characterList.getLast()){
+                characterList.removeLast();
+            }
+            else if(savePreChar == characterArray[i]){
+
+            }
+            else{
+                characterList.add(characterArray[i]);
+            }
+            savePreChar = characterArray[i];
+        }
 
         return answer;
     }
