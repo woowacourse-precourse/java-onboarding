6ad2b42c1c3a43525698997f18c66563f03175a2
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-29 00:28:48 +0900

    feat :: 입력 받은 수를 오만 원권부터 차례대로 검증함

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,19 +2,37 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static void main(String[] args) {
+        System.out.println(solution(50237));
+    }
 
     private static List<Integer> moneyUnit;
+    private static List<Integer> moneyStorage;
 
     private static void initMoneyUnit() {
-        moneyUnit = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10));
+        moneyUnit = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+        moneyStorage = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0));
     }
 
     public static List<Integer> solution(int money) {
         initMoneyUnit();
+        validateMoneyUnit(money);
+        return moneyStorage;
+    }
 
+    private static void validateMoneyUnit(int money) {
+        for (int i = 0; i < moneyUnit.size(); i++) {
+            while (true) {
+                if (money / moneyUnit.get(i) > 0) {
+                    moneyStorage.set(i, money / moneyUnit.get(i));
+                    money %= moneyUnit.get(i);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
