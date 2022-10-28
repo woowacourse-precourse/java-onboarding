b5d1ef111cb420da9f3f9d2140662371a432fe2e
Lee Min Woo <lmw7414@naver.com>
2022-10-28 20:39:09 +0900

    problem3 - 기능 요소 설계
    필요한 기능들에 대해 생각해보았다.
    숫자를 String으로 변환 후 3 또는 6 또는 9를 직접 카운팅 하려고 한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/** 구현해야 할 기능
+ * number를 String으로 변환
+ * String에서 3 or 6 or 9 카운트
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
