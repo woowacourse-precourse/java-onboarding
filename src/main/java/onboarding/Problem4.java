3a4935318303cbfeed5593194440a710670bdbf4
Your Name <dbsdbxkrzz@naver.com>
2022-10-28 15:46:55 +0900

    feat: Problem4의 기본 구조를 작성합니다.
    Problem4의 기능 목록에 맞추어 메서드 기본 구조를 작성합니다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static StringBuilder sb = new StringBuilder();
+
+	public static String solution(String word) {
+		treeFrogDictionary(word);
+		return sb.toString();
+	}
+
+	public static void treeFrogDictionary(String word) {
+
+	}
+
+	public static char isAlpha(char c) {
+
+		return c;
+	}
+
+	public static char translate(char c) {
+
+		return c;
+	}
 }
