5ec6b0993ef25bc15d84b4b09f2e218c327d0618
Heesom <heesom.hs@gmail.com>
2022-10-28 14:47:53 +0900

    Problem2 : findAnswer 메소드 업데이트, checkDouble 메소드명 변경
    findAnswer 메소드에 재귀함수 추가
    checkDouble -> isDouble 로 메소드명 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,35 +9,40 @@ public class Problem2 {
 
     public static String findAnswer(String cryptogram) {
         StringBuilder tempAnswer = new StringBuilder();
-        boolean noticeDoubleToEnd = false;
-        boolean checkPairDouble = false;
+        boolean EndOfPairIsDouble = false;
+        boolean pairIsDouble = false;
         int cryptogramArrlen = cryptogram.length()-1;
 
         for (int i = 0; i < cryptogramArrlen; i++) {
-            checkPairDouble = (cryptogram.charAt(i) == cryptogram.charAt(i+1)); // 중복인지 체크
+            pairIsDouble = (cryptogram.charAt(i) == cryptogram.charAt(i+1)); // 중복인지 체크
 
             //만약 중복이 아니면 정답 저장
-            if (!checkPairDouble) {
+            if (!pairIsDouble) {
 
-                if(!noticeDoubleToEnd){ //저번쌍이 중복이 아니었으면 요번도 저장
+                if(!EndOfPairIsDouble){ //저번쌍이 중복이 아니었으면 요번도 저장
                     tempAnswer.append(cryptogram.charAt(i));
                 } else{  //저번쌍이 중복이었으면 이번턴은 지나가고 대신 다음에는 중복아니라고 신호주고 끝
-                    noticeDoubleToEnd = false;
+                    EndOfPairIsDouble = false;
                 }
 
             }else{ //만약 중복이면
-                noticeDoubleToEnd = true;
+                EndOfPairIsDouble = true;
             }
         }
 
-        if(!checkPairDouble){ //범위 마지막 1개
-            System.out.println("store>"+ (cryptogramArrlen)+" : "+cryptogram.charAt(cryptogramArrlen));
+        if(!pairIsDouble){ //범위 마지막 1개
+            tempAnswer.append(cryptogram.charAt(cryptogramArrlen));
         }
 
-        return tempAnswer.toString();
+        String whetherRecursion = tempAnswer.toString();
+        if(isDouble(whetherRecursion)){
+            return findAnswer(whetherRecursion);
+        }else{
+            return whetherRecursion;
+        }
     }
 
-    public static boolean checkDouble(String cryptogram) {
+    public static boolean isDouble(String cryptogram) {
         for (int i = 0; i < cryptogram.length()-1; i++) {
             if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
                 return true;
