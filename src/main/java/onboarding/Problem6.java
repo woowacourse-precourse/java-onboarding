7f9089b1fba1be08d09f01829996a2259e536249
kiarakim <kiarakim413@gmail.com>
2022-10-29 00:31:23 +0900

    feat(Problem6): compare per two characters whether it is overlapped

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,14 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashMap<String, String> map = new HashMap<>();
+        HashSet<String> emails = new HashSet<>();
 
         for (List<String> person : forms) {
             int emailLength = person.get(0).length();
@@ -16,6 +20,15 @@ public class Problem6 {
             int nameLength = nickname.length();
             if (nameLength < 1 || nameLength >= 20) {
                 break;
+            }
+
+            for (int i = 0; i < nameLength - 1; i++) {
+                String key = nickname.substring(i, i + 2);
+                if (map.containsKey(key)) {
+                    emails.add(map.get(key));
+                    emails.add(person.get(0));
+                }
+                map.put(key, person.get(0));
             }
         }
         return answer;
