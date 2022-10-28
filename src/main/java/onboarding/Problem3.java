71d9e1d1b075c20de86956acc8b4b4aa06b45421
Isu Shin <isu010614@naver.com>
2022-10-28 23:01:27 +0900

    feat: Add function to put digits in list

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+    public static List<Integer> putDigitsToList(int number) {
+        List<Integer> numberList = new ArrayList<>();
+        while (number > 0) {
+            numberList.add(number % 10);
+            number /= 10;
+        }
+        return numberList;
+    }
+
 }
