ff03de2987c81ca90e9f2f82e48ca4dad1590416
whipbaek <jibaek0513@gmail.com>
2022-10-28 19:51:35 +0900

    P7 Make SolutionClass

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
-
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    public static class SolutionClass {
+
+        private String user;
+        private List<List<String>> friends;
+        private List<String> visitors;
+
+        private Map<String, ArrayList<String>> map = new HashMap<>(); // 친구관계를 나타내는 map
+        private Map<String, Integer> result = new HashMap<>(); // 결과를 담을 map
+        private List<String> answer = new ArrayList<>(); // map 에서 이름 저장할 list
+
+        public SolutionClass(String user, List<List<String>> friends, List<String> visitors) {
+            this.user = user;
+            this.friends = friends;
+            this.visitors = visitors;
+        }
+
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        return null;
     }
 }
