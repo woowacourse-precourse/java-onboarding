4676a0909b4b91c960acd7415e845e7926996e22
hectick <chyeon1223@cau.ac.kr>
2022-10-29 02:32:44 +0900

    feat : [Problem6] 중복 닉네임이 될 수 있는 단어 후보들을 추출하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,31 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashMap<String, String> map = new HashMap<>(); //닉네임 중복 체크를 위해 닉네임의 파편들을 이메일주소와 함께 저장
+        HashSet<String> set = new HashSet<>(); //중복되지 않도록 이메일을 저장
 
-        //중복 닉네임이 될 수 있는 단어들의 경우의 수를 뽑아서 map에 <단어, 이메일>로 저장하는 기능
-        //map에 저장된 단어가 사용자의 닉네임에 들어가는지 체크하는 기능
-        //중복 닉네임이 될 경우 이메일을 set에 추가하는 기능
+        for(int i = 0; i < forms.size(); i++){
+            String nickname = forms.get(i).get(1);
+            if(nickname.length() == 1){
+                continue;
+            }
+            //중복 닉네임이 될 수 있는 단어들을 추출
+            for(int j = 0; j < nickname.length() - 1; j++){
+                for(int k = j+1; k < nickname.length(); k++){
+                    String str = nickname.substring(j, k+1);
+                    //map에 저장된 단어가 사용자의 닉네임에 들어가는지 체크하는 기능
+                    //이미 map에 있던 단어는 중복 닉네임이 되므로 이메일을 set에 추가
+                    //처음 발견된 단어는 map에 <단어, 이메일>로 저장
+                    }
+                }
+            }
+        }
         //set을 list로 변경하여 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 메일 목록을 반환하는 기능
 
         return answer;
