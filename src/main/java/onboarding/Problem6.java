471cc116a19b12557158a012bcc9d3988f357ca4
kevstevie <jianna442@gmail.com>
2022-10-28 17:19:51 +0900

    feat: add twoLetterIntoMap

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -15,5 +15,14 @@ public class Problem6 {
             String str = (String)list.get(1);
             putDevidedIntoMap(str, map);
         }
+
+        return map;
+    }
+
+    static void putDevidedIntoMap(String str, Map<String, Integer> map){
+        for (int i = 0; i < str.length() - 1; i++) {
+            String substring = str.substring(i, i + 2);
+            map.put(substring, map.getOrDefault(substring, 0) + 1);
+        }
     }
 }
