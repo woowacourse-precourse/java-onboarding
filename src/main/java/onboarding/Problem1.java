98a5a6c26a06468e52404782b7eb447b4c81932a
김태용 <amico741@naver.com>
2022-10-28 16:25:05 +0900

    feat: 각 자리 숫자를 모두 더하는 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,7 +6,7 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = -1;
         if(validation(pobi, crong)){
-            
+
         }
         return answer;
     }
@@ -18,5 +18,14 @@ class Problem1 {
         if(array1.get(0) == 1 || array1.get(0) == 399 || array2.get(0) == 1 || array2.get(1) == 399){ // 시작면이나 마지막면일 때, 두 페이지의 차가 2이상일 때 return false
             return false;
         } else return pobi <= 1 && crong <= 1;
+    }
+
+    public static int add(int number){
+        int result = 0;
+        String[] array = String.valueOf(number).split("");
+        for(String s: array){
+            result += Integer.parseInt(s);
+        }
+        return result;
     }
 }
