15810382c1e65edc82a5c243ededadbad4a9cd0d
KyungKyu Park <rudrb0505@gmail.com>
2022-10-28 15:49:10 +0900

    feat(Problem4): 소문자와 대문자를 구분하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem4 {
 	public static String solution(String word) {
 		char [] wordArray = word.toCharArray();
+		int asciiNum = 0;
+
+		for (int i = 0; i <wordArray.length; i++) {
+			asciiNum = (int)wordArray[i];
+			if (asciiNum >= 65 && asciiNum <= 90) {
+				System.out.println("upper case : " + (char) asciiNum);
+			} else if (asciiNum >= 97 && asciiNum <= 122) {
+				System.out.println("lower case: " + (char) asciiNum);
+			}
+		}
 
 		String answer = "";
 		return answer;
