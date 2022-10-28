70e8bb5b47a4620ff0bc59226f8cf96c11885739
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-28 19:15:23 +0900

    [문제6] - 제공된 닉네임을 연속적인 단어 리스트로 반환하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    public static List<String> getWords(String nick) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < nick.length() - 1; i++) {
+            result.add(nick.substring(i, i + 2));
+        }
+
+        return result;
+    }
+    
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
