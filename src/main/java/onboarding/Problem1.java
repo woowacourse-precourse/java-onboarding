972a9d61b148243a1d359bf02eae0f85312d5fde
homecoder98 <homecoder98@gmail.com>
2022-10-28 21:52:30 +0900

    Feat : Check Exception

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (isException(pobi) || isException(crong)) return -1;
         int pobiScore = getScore(pobi.get(1));
         int crongScore = getScore(crong.get(1));
         if (pobiScore == crongScore) return 0;
@@ -19,5 +20,9 @@ class Problem1 {
             i /= 10;
         }
         return (j > k)?j : k;
+    }
+
+    static boolean isException(List<Integer> pages) {
+        return (pages.get(0) + 1 != pages.get(1))? true : false;
     }
 }
