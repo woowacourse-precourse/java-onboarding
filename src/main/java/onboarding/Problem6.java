1580861437501002132f3973cc6a704f08a7ee48
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:23:01 +0900

    feat(문제6): 사용자, 중복 사용자 저장소, 이름 저장소 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 📚 기능 목록
@@ -11,8 +12,49 @@ import java.util.List;
  * 4. 결과 목록을 정렬하는 기능
  */
 public class Problem6 {
+
+    /** 사용자 */
+    public static class Member {
+        private String name;
+        private String email;
+        private boolean duplicated = false;
+
+        Member() {
+        }
+
+        Member(String email, String name) {
+            this.email = email;
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return this.email;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public boolean isDuplicated() {
+            return this.duplicated;
+        }
+
+        public void setDuplicated() {
+            this.duplicated = true;
+        }
+    }
+
+    /** 중복이름을 가진 사용자 저장소 */
+    public static Set<Member> duplicateMemberList = new HashSet<>();
+    /** 2글자로 잘린 이름 저장소 */
+    public static Map<String, Member> nameStorage = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<Member> members = forms.stream()
+                .map(form -> new Member(form.get(0), form.get(1)))
+                .collect(Collectors.toList());
+
         return answer;
     }
 }
