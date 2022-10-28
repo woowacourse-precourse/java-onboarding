300935051a28f4adcae89eb7cc2f75bf67b181bf
ninaaano <tvxqinak@naver.com>
2022-10-28 16:24:26 +0900

    221028 16:24 prob5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> coin = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
+        List<Integer> answer = new ArrayList<>();
+
+        for(int price : coin){
+            answer.add(money/price);
+            money %= price;
+        }
         return answer;
     }
 }
