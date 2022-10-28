d6c673e2d51d5e3a018ff2bd78c5a58593f0384c
kimJaeHyeon <kmr2644@gmail.com>
2022-10-28 22:00:49 +0900

    fix: change collection type from coinList to coinArray

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
@@ -9,19 +9,19 @@ public class Problem5 {
     }
 
     public static List<Integer> getCoinList(int money) {
-        List<Integer> coinList = new ArrayList<>();
+        Integer[] coinArray = new Integer[]{0,0,0,0,0,0,0,0,0};
         int[] coinUnitArray = getCoinUnitToArray();
         int index = 0;
         while (money > 0) {
             int coinUnit = coinUnitArray[index];
-            coinList.add(money / coinUnit);
+            coinArray[index] = money / coinUnit;
             money = money % coinUnit;
             index++;
         }
-        return coinList;
+        return Arrays.asList(coinArray);
     }
 
     private static int[] getCoinUnitToArray() {
-        return new int[]{50000,10000,5000,1000,500,100,50,10,1};
+        return new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     }
 }
