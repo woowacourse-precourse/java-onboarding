317e2bf763f8fe63caac1b29f013e2814d135a02
kwYoohae <gocksdl7979@gmail.com>
2022-10-28 19:51:22 +0900

    feat(Problem.java): 돈을 거슬러주는 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = changeMoney(money);
         return answer;
     }
+
+    public static List<Integer> changeMoney(int money) {
+        int[] exchangeMoneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int exchangeMoney : exchangeMoneyList) {
+            if (money / exchangeMoney != 0) {
+                list.add(money / exchangeMoney);
+                money %= exchangeMoney;
+            } else {
+                list.add(0);
+            }
+        }
+        return list;
+    }
+
 }
+
