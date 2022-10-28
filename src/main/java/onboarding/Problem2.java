98968a7e993fd667ae8b2318b16d9392c1e6784b
sen <seeunchoi99@gmail.com>
2022-10-28 19:25:40 +0900

    🎨 Problem2.deleteDuplication 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -22,4 +22,24 @@ public class Problem2 {
 
         return stack.stream().reduce("", (x, y) -> x + y);
     }
+
+    /**
+     * 문자 배열의 중복 문자 삭제
+     *
+     * @param strings
+     * @return 삭제 후 스택 배열
+     */
+    private static Stack<String> deleteDuplication(String[] strings) {
+        Stack<String> stack = new Stack<>();
+
+        for (String string : strings) {
+            if (!stack.isEmpty() && stack.peek().equals(string)) {
+                stack.pop();
+            } else {
+                stack.add(string);
+            }
+        }
+
+        return stack;
+    }
 }
