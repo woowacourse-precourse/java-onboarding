adcaa923504d993c80494ce70e594df5c72edfb7
kdkdhoho <hkim4410@naver.com>
2022-10-28 14:42:11 +0900

    Refactor(Problem5): amountUnit에 private 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Problem5 {
 
-    static int[] amountUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static int[] amountUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
