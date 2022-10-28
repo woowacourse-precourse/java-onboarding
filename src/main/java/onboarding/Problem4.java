62e9bff004b25339c785bb44aaf4b22ee6e97db0
juno-junho <ssmm0205@naver.com>
2022-10-29 06:42:47 +0900

    refactor(Problem4): remove bracket
    간결한 코드를 위해 if 문과 for문의 괄호 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,9 +3,7 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder convertedWord = new StringBuilder(word.length());
-        for (char letter : word.toCharArray()) {
-            convertedWord.append(convertWithDictionary(letter));
-        }
+        for (char letter : word.toCharArray()) convertedWord.append(convertWithDictionary(letter));
         return convertedWord.toString();
     }
 
@@ -15,12 +13,8 @@ public class Problem4 {
         final char ALPHABET_LOWERCASE_START = 'a';
         final char ALPHABET_LOWERCASE_END = 'z';
 
-        if (Character.isUpperCase(letter)) {
-            return (char) (ALPHABET_UPPERCASE_END - (letter - ALPHABET_UPPERCASE_START));
-        }
-        if (Character.isLowerCase(letter)) {
-            return (char) (ALPHABET_LOWERCASE_END - (letter - ALPHABET_LOWERCASE_START));
-        }
+        if (Character.isUpperCase(letter)) return (char) (ALPHABET_UPPERCASE_END - (letter - ALPHABET_UPPERCASE_START));
+        if (Character.isLowerCase(letter)) return (char) (ALPHABET_LOWERCASE_END - (letter - ALPHABET_LOWERCASE_START));
         return letter;
     }
 }
