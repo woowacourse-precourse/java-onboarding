118f1fa58fdd5dd61e90c47bd117f31ee5a68a85
juno-junho <ssmm0205@naver.com>
2022-10-28 18:04:23 +0900

    refactor(Problem6): rename the hashmap variable
    crew인 map 변수를 crew를 담는 map이라는 것을 명확하게 하기 위해 변수 이름 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,25 +1,25 @@
 package onboarding;
 
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> result = new ArrayList<>();
 
-        Map<String, String> crew = new HashMap<>();
+        Map<String, String> crewMap = new HashMap<>();
 
         for (List<String> form : forms) {
             for (int i = 0; i < form.get(1).length() - 1; i++) {
-                crew.put(form.get(1), form.get(0));
+                crewMap.put(form.get(1), form.get(0));
             }
         }
-        Set<String> nicknameSet = crew.keySet();
+        Set<String> nicknameSet = crewMap.keySet();
         nicknameSet.forEach(i -> {
             for (int j = 0; j < i.length() - 1; j++) {
                 String consecutiveLetter = i.substring(j, j + 2);
                 nicknameSet.forEach(k -> {
-                    if (!k.equals(i) && k.contains(consecutiveLetter)) result.add(crew.get(k));});
+                    if (!k.equals(i) && k.contains(consecutiveLetter)) result.add(crewMap.get(k));});
             }
         });
         return result.stream().distinct().sorted().collect(Collectors.toList());
