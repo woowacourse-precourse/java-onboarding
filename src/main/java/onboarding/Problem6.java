5a5b34910b0bcfddce0d4d8f1770e82529ae2722
youhobin <ghdb132@naver.com>
2022-10-28 21:19:35 +0900

    feat(onboarding) : 리스트의 값을 HashMap에 key는 email, value는 닉네임 쌍으로 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,17 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        Map<String, String> form = new HashMap<>();
+        for (int i = 0; i < forms.size(); i++) {
+            List a = forms.get(i);
+            form.put((String) a.get(0), (String) a.get(1));
+        }
+
         List<String> answer = List.of("answer");
         return answer;
     }
