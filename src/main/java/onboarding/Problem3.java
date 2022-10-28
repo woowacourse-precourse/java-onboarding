01910e29e400a96b5b4f946f872a844939d6f0ff
YoosunMoon <muzikue@naver.com>
2022-10-28 23:57:09 +0900

    feat: 3번 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,30 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int numberSize = 0;
+
+        String str = Integer.toString(number);
+        numberSize = str.length();
+
+        for(int i = 0; i < numberSize; i++) {
+            int digits = (int)Math.pow(10, i);
+            int currDigitsNum;
+
+            if( i == 0) {
+                currDigitsNum = number % (digits * 10);
+                answer += currDigitsNum / 3;
+                continue;
+            }
+
+            currDigitsNum = (number / digits) % 10;
+
+            if(currDigitsNum != 0 && currDigitsNum % 3 == 0) {
+                answer += (number % digits) + 1;
+            }
+
+            answer += ((currDigitsNum - 1) / 3) * digits;
+            answer += (number / digits) * 3 * (digits / 10);
+        }
         return answer;
     }
 }
