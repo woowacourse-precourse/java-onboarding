5f7da84115e901899d8086f0fa0a5f8ba8fdbf0d
jinny-l <jinny.l.dev@gmail.com>
2022-10-28 16:42:59 +0900

    [feat] (problem.2) 연속하는 중복 문자를 제거하는 메소드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,6 +6,22 @@ public class Problem2 {
         return answer;
     }
 
+    // 연속하는 중복 문자를 제거하는 메소드
+    private static String deleteRepetition(String cryptogram) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(cryptogram);
+
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+                break;
+            }
+        }
+        return sb.toString();
+    }
+
+
     // 연속하는 중복 문자가 있는지 확인하는 메소드
     private static int isRepetitive(String cryptogram) {
         for (int i = 0; i < cryptogram.length() - 1; i++) {
