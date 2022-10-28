8d605188aa2376c2f7b1e1b30e5968284a52c763
JuHyeokParkk <hyeok5596@naver.com>
2022-10-28 12:30:38 +0900

    feat(Problem2): Stack에서의 문자열 중복 제거를 위한 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,47 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static Stack<Character> removeDuplication(String cryptogram) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+
+            char currentChar = cryptogram.charAt(i);
+
+            if(!stack.isEmpty() && stack.peek() == currentChar) {
+                i += getEqualCharCount(cryptogram, i + 1, currentChar);
+                stack.pop();
+                continue;
+            }
+
+            stack.add(currentChar);
+
+        }
+
+        return stack;
+    }
+
+    static int getEqualCharCount(String cryptogram, int index, char currentChar) {
+
+        int equalCount = 0;
+
+        while(index < cryptogram.length()) {
+
+            if(cryptogram.charAt(index) != currentChar)
+                return equalCount;
+
+            index++;
+            equalCount++;
+        }
+
+        return equalCount;
     }
 }
