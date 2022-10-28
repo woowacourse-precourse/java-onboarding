5ca817a4df6e1da5a34fb4ddd0645c5e3be849a0
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-28 21:56:26 +0900

    while문을 통해 루프를 돌리고 result와 cryptogram이 같아지는 순간 break; 그리고 return함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,27 +3,32 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String result = "";
-        for(int i =0; i<cryptogram.length(); i++){
-            try{
-                if(cryptogram.charAt(i-1)!=cryptogram.charAt(i) && cryptogram.charAt(i)!=cryptogram.charAt(i+1) ){
-                    result += cryptogram.charAt(i);
-                }
-            }
-            catch(StringIndexOutOfBoundsException e){
-                if(i==0){
-                    if(cryptogram.charAt(i)!=cryptogram.charAt(i+1)){
+        while(true){
+            for(int i =0; i<cryptogram.length(); i++){
+                try{
+                    if(cryptogram.charAt(i-1)!=cryptogram.charAt(i) && cryptogram.charAt(i)!=cryptogram.charAt(i+1) ){
                         result += cryptogram.charAt(i);
                     }
                 }
-                if(i==cryptogram.length()-1){
-                    if(cryptogram.charAt(i-1)!=cryptogram.charAt(i)){
-                        result += cryptogram.charAt(i);
+                catch(StringIndexOutOfBoundsException e){
+                    if(i==0){
+                        if(cryptogram.charAt(i)!=cryptogram.charAt(i+1)){
+                            result += cryptogram.charAt(i);
+                        }
+                    }
+                    if(i==cryptogram.length()-1){
+                        if(cryptogram.charAt(i-1)!=cryptogram.charAt(i)){
+                            result += cryptogram.charAt(i);
+                        }
                     }
                 }
+            }        
+            if(result.equals(cryptogram)) {
+                break;
             }
-        }       
-        
-        String answer = "answer";
-        return answer;
+            cryptogram = result;
+            result = "";
+        }
+        return cryptogram;
     }
 }
