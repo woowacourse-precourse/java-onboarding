35703cdb0973169ed0a8ed1afb2b1b559db39467
seongha <11pi885@gmail.com>
2022-10-29 02:46:36 +0900

    feat: dp배열 선언 및dp테이블을 채우는  fillTable 함수작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,10 +2,22 @@ package onboarding;
 
 public class Problem3 {
 
-
+    static int[] dp= new int[10005];
 
     //1~10000까지 dp 테이블을 생성하는 함수
+    static void fillTable(int number){
+        for(int i=1; i<=number; i++){
+           int now = i;
+           int cnt = 0;
+           while (now>0){
+               if(now%10 == 3||now%10 == 6||now%10 == 9)
+                   cnt++;
 
+               now /=10;
+           }
+           dp[i] = dp[i-1] +cnt;
+        }
+    }
 
     public static int solution(int number) {
         int answer = 0;
