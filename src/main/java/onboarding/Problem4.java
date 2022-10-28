cb2a9343b93889276a39ae93a2a60d92c1f2ed31
MoonJeWoong <munjin0201@naver.com>
2022-10-28 10:55:55 +0900

    [4번 문제] feat: solution 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    public HashMap<Character, Character> makeDictionary(){
+        HashMap<Character, Character> dictionary = new HashMap<>();
+        return dictionary;
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Problem4 T = new Problem4();
+        StringBuilder answer = new StringBuilder();
+
+        HashMap<Character, Character> dictionary = T.makeDictionary();
+
+        for(char c : word.toCharArray()){
+            if(Character.isAlphabetic(c)){
+                answer.append(dictionary.get(c));
+                continue;
+            }
+            answer.append(c);
+        }
+        return answer.toString();
     }
 }
