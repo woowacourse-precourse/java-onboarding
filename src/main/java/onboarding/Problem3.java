126188ec956e92ea2da98be94841343d82ff0d3b
JuHyeokParkk <hyeok5596@naver.com>
2022-10-28 13:25:35 +0900

    feat(Problem3): 수의 3, 6, 9 개수를 파악하는 메소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
+    static Integer[] countRecord;
+    static final int MAX_INDEX = 10001;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    static int getClapCount(int number) {
+
+        if(countRecord[number] != null)
+            return countRecord[number];
+
+        if(number == 0)
+            return 0;
+
+        int currentNum = number % 10;
+
+        return (currentNum == 3 || currentNum == 6 || currentNum == 9 ? 1 : 0) + getClapCount(number / 10);
     }
 }
