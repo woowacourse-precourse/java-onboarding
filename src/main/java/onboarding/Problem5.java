56484ab8cc2f8f6c9817b5629f8de169614dcc39
JJongmen <pjy90123@gmail.com>
2022-10-28 20:35:53 +0900

    feat(Problem5): 모든 금액을 다 받았다면 리스트를 반환한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,19 +1,21 @@
 package onboarding;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    static int[] moneys = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static final int[] moneys = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        int[] wallet = new int[9];
+        List<Integer> wallet = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
-            if (money < moneys[i]) continue;
-            wallet[i] += money / moneys[i];
+            if (money < moneys[i]) {
+                wallet.add(0);
+                continue;
+            }
+            wallet.add(money / moneys[i]);
             money %= moneys[i];
         }
-        return answer;
+        return wallet;
     }
 }
