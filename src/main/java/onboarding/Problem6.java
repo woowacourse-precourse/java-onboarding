e814996f82da654ebeafd80b777f515db1314c29
1eejisoo <jisu991012@gmail.com>
2022-10-28 21:15:33 +0900

    [Problem 6] Feat: 닉네임을 2문자 단위로 쪼개는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,6 +10,17 @@ public class Problem6 {
         return checkDuplicate(forms, answer);
     }
 
+    // 닉네임을 2문자 단위로 쪼개는 기능
+    public static List<String> twoUnits (String nickname) {
+        List<String> twoUnitList = new ArrayList<>();
+
+        for(int i = 0; i<nickname.length()-1; i++){
+            if (nickname.length() == 2) twoUnitList.add(nickname);
+            else twoUnitList.add(nickname.substring(i,i+2));
+        }
+        return twoUnitList;
+    }
+
     // 자기 자신 빼고의 닉네임들을 문자열로 만드는 기능
     public static String nicknamesToString (List<String> names, int i) {
         StringBuilder sb = new StringBuilder();
