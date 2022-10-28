3b07ca46372abdc316c11c861b52b06ffcca5d5d
JuHyeokParkk <hyeok5596@naver.com>
2022-10-28 15:34:59 +0900

    feat(Problem4): 조건에 맞는 문자 변환 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,21 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    static char convert(char currentChar) {
+
+        if(!isAlphabet(currentChar))
+            return currentChar;
+
+        if(Character.isLowerCase(currentChar))
+            return (char)('a' + ('z' - currentChar));
+
+        return (char)('A' + ('Z' - currentChar));
+    }
+
+    static boolean isAlphabet(char c) {
+
+        return ('a' <= c && c <= 'z')
+                || ('A' <= c && c <= 'Z');
+    }
 }
