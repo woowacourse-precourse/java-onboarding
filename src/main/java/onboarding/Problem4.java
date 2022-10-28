200219ed6bf24550c13d521305d7fea4d493a359
Jimin <thswlals0219@gmail.com>
2022-10-28 21:08:33 +0900

    feat: add convertToTreefrogDictionary function to problem3
    청개구리 사전에 따라 알파벳 변환 기능 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,8 +9,25 @@ public class Problem4 {
         return false;
     }
 
+    public static String convertToTreefrogDictionary(String word) {
+        char[] convertWord = word.toCharArray();
+
+        for(int i = 0; i < convertWord.length; i++){
+            if(!isAlpha(convertWord[i])) {
+                continue;
+            }
+
+            if(Character.isUpperCase(convertWord[i])) {
+                convertWord[i] = (char) ('Z' - convertWord[i] + 'A');
+            } else {
+                convertWord[i] = (char) ('z' - convertWord[i] + 'a');
+            }
+        }
+
+        return String.valueOf(convertWord);
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return convertToTreefrogDictionary(word);
     }
 }
