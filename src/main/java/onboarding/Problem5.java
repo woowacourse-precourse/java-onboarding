44a9b1b5fbe46b547d245c4440af73e6487396a2
jeongwle <dldl4568@naver.com>
2022-10-28 15:01:32 +0900

    [Feat] Problem5 제한사항 검증하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        if (!isValidate(money)) {
+            ArrayList<Integer> error = new ArrayList<>();
+            error.add(-1);
+            return error;
+        }
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    /**
+     * 제한사항 검증하는 기능
+     */
+    public static boolean isValidate(int money) {
+        return (money > 0 && money < 1_000_001);
     }
 }
