97a39ac1d5b5850c63d38ea0bbdf9c789e940308
kwYoohae <gocksdl7979@gmail.com>
2022-10-28 12:25:51 +0900

    feat(Problem4.java): 대문자 소문자를 확인하는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static final int UPPERCASE_START_ALPHABET = 'A';
+    public static final int UPPERCASE_END_ALPHABET = 'Z';
+    public static final int LOWERCASE_START_ALPHABET = 'a';
+    public static final int LOWERCASE_END_ALPHABET = 'z';
+
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    public static int checkUpperOrLower(char alphabet) {
+        if (alphabet >= LOWERCASE_START_ALPHABET && alphabet <= LOWERCASE_END_ALPHABET) {
+            return 1;
+        }
+        else if (alphabet >= UPPERCASE_START_ALPHABET && alphabet <= UPPERCASE_END_ALPHABET) {
+            return 0;
+        }
+        return -1;
+    }
+
 }
