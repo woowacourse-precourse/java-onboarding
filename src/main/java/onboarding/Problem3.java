23a16d5bdfd680cbdee049fb4366d7716e5f35e7
koo <rnqudgur@naver.com>
2022-10-28 18:43:25 +0900

    기능 요구사항 3
    기능 분리
    1. 해당 숫자에서 손뼉을 몇번치는지 구분하는 기능 (clapCounting)
    2. 주어진 숫자를 순회하면서 전체적으로 손뼉친 횟수를 반환하는 기능(solution)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
-}
+        int clapCountSum = 0;
+        Problem3 problem3 = new Problem3();
+        
+        for(int i=0;i<=number;i++) {
+        	clapCountSum += problem3.clapCounting(i);
+        }//for 
+        
+        return clapCountSum;
+    }// solution
+    
+    public int clapCounting(int number) {
+    	int clapCount = 0;
+    	String numberStr = String.valueOf(number);
+    	
+    	for(char digit:numberStr.toCharArray()) {
+    		if(digit=='3' || digit=='6' || digit=='9') clapCount++;
+    	}//for
+    	
+    	return clapCount;
+    }//clapCounting
+}//end class
