90bbf1991e0c6f67cb238e6570b2595b0fafc7e0
KyungKyu Park <rudrb0505@gmail.com>
2022-10-28 16:19:38 +0900

    refactor(Problem4): 문자열 변환 기능을 메소드로 정리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,22 +3,26 @@ package onboarding;
 public class Problem4 {
 	public static String solution(String word) {
 		char [] wordArray = word.toCharArray();
+		reverseWord(wordArray);
+		return new String(wordArray);
+	}
+
+	private static void reverseWord(char[] wordArray) {
 		int asciiNum;
 		int reverseAsciiNum;
 		int distanceOfWord;
-
-		for (int i = 0; i <wordArray.length; i++) {
+		for (int i = 0; i < wordArray.length; i++) {
 			asciiNum = (int)wordArray[i];
 			if (asciiNum >= 65 && asciiNum <= 90) {
 				distanceOfWord = asciiNum - 65;
 				reverseAsciiNum = 90 - distanceOfWord;
 				wordArray[i] = (char) reverseAsciiNum;
-			} else if (asciiNum >= 97 && asciiNum <= 122) {
+			} 
+			if (asciiNum >= 97 && asciiNum <= 122) {
 				distanceOfWord = asciiNum - 97;
 				reverseAsciiNum = 122 - distanceOfWord;
 				wordArray[i] = (char) reverseAsciiNum;
 			}
 		}
-		return new String(wordArray);
 	}
 }
