9ee96db6fe8cdc2b2c28fccc4fc768a84aa3da11
chj <bluedog129@gmail.com>
2022-10-28 14:21:43 +0900

    P3 3, 6, 9 조건에 맞는지 체크하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem3 {
+    private static int checkThreeSixNine(int number) {
+        if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+            return 1;
+        }
+        return 0;
+    }
     private static int divideNumber(int number) {
         int count = 0;
 
