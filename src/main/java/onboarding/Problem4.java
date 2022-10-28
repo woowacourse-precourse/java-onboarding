b34285bfa46e25e2886212628668b82db09c5d20
syeonbot <asdf748596123@gmail.com>
2022-10-29 05:13:24 +0900

    feat: problem4 문자열 validate 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+    public static final int MINIMUM_STRING_LENGTH = 1;
+    public static final int MAXIMUM_STRING_LENGTH = 1000;
     public static String solution(String word) {
         String answer = "";
         return answer;
+        validateRange(word);
+    }
+
+    public static void validateRange(String word) {
+        if (word.length() < MINIMUM_STRING_LENGTH || MAXIMUM_STRING_LENGTH < word.length()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
