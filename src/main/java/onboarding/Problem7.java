e89e433591b7063218227258ba07710ef49208a9
viyott-lover <jackk0616@gmail.com>
2022-10-28 22:53:24 +0900

    p7 2차원 배열을 활용하여 친구관계를 표시

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,6 +16,8 @@ public class Problem7 {
         Set<String> crews = new HashSet<>();
         HashMap<String, Integer> crewIndex = new HashMap<>();
         HashMap<String, Integer> crewScore = new HashMap<>();
+        boolean[][] relationship;
+        int len = 0;
 
         // 1. 크루원들 이름을 구하고 2차원 배열을 위한 인덱스 부여
         for (List<String> friend : friends) {
@@ -26,6 +28,14 @@ public class Problem7 {
         for (String crew : crews) {
             crewIndex.put(crew, idx++);
             crewScore.put(crew, 0);
+        }
+
+        // 2. 2차원 배열을 활용하여 친구관계를 표시
+        len = crews.size();
+        relationship = new boolean[len][len];
+        for (List<String> friend : friends) {
+            relationship[crewIndex.get(friend.get(0))][crewIndex.get(friend.get(1))] = true;
+            relationship[crewIndex.get(friend.get(1))][crewIndex.get(friend.get(0))] = true;
         }
         return answer;
     }
