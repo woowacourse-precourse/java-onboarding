5ec99f55f59749369dd42d249a37e83c93f8bca5
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-28 20:28:24 +0900

    feat(Problem4): 문자열을 문자 단위로 청개구리 변환 함수(getGreenFrogChar)에 넣기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            getGreenFrogChar(word.charAt(i));
+        }
+
         return answer;
+    }
+
+    private static void getGreenFrogChar(char originalChar) {
+        System.out.println(originalChar);
     }
 }
