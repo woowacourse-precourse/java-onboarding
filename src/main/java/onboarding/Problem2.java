b47cf636bb10a5c9ca0883cda2f68434e23ce3ac
Joon <orijoon98@gmail.com>
2022-10-28 18:23:58 +0900

    feat: 문제2 - 문자열 해독 메소드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,4 +12,17 @@ public class Problem2 {
         }
         return true;
     }
+
+    private static String decode(String cryptogram) {
+        if (isDecoded(cryptogram)) {
+            return cryptogram;
+        }
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                String next = cryptogram.replaceAll("(([a-z])\\2{1,})", "");
+                return decode(next);
+            }
+        }
+        return "error";
+    }
 }
