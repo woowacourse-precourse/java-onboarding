f503c13af36a9f2dea88b1145517760906333521
Reznov <reznov03048@gmail.com>
2022-10-28 22:55:48 +0900

    solution 메소드에 주석 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        // 리스트에 있는 닉네임과 이메일을 맵에 넣음
+
+        // 닉네임 중 같은 글자가 연속적으로 포함 되는 경우 찾기
+
+        // 오름차순 정렬, 중복 제거
+
         return answer;
     }
 }
