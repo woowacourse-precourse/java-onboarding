6395b99bfd362c9512ae06e9246c9aa9b590b1b2
squareyun <squareyun@gmail.com>
2022-10-28 22:25:26 +0900

    feat: 닉네임 2글자 이하일 때 처리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
+    private static Map<String, Set<String>> limitedNicknameMap = new HashMap<>();
+    private static Set<String> answer = new HashSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            if (nickname.length() < 2)
+                continue;
+        }
+
+        List<String> retAnswer = new ArrayList<>(answer);
+        return retAnswer;
     }
 }
