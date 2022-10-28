29cf484b906db6ba3d1add8a6fd7778750958c6f
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 13:08:02 +0900

    feat: PROBLEM4 알파벳 확인 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		String answer = "";
+		return answer;
+	}
+
+	static class Frog {
+
+		private boolean isLowerAlphabet(char c) {
+			return Character.isAlphabetic(c) && Character.isLowerCase(c);
+		}
+
+		private boolean isHigherAlphabet(char c) {
+			return Character.isAlphabetic(c) && (!Character.isLowerCase(c));
+		}
+	}
 }
