b4965226241b8b535e7435fd74b2f8119d2b2dc4
matt <matt1235@naver.com>
2022-10-29 00:31:49 +0900

    feat: Problem4 (기능 목록1 - 입력값 길이 검사) 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    static char[] alphabet1 = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    static char[] alphabet2 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
     public static String solution(String word) {
         String answer = "";
+        try{ check(word); }
+        catch (IllegalArgumentException e){
+            return e.toString();
+        }
         return answer;
     }
+
+    /**
+     * 인자의 길이 범위 검사*/
+    private static void check(String word){
+        if(word.length() <1 || word.length() >1000)
+            throw new IllegalArgumentException("인자의 길이가 맞지 않습니다.");
+    }
+
 }
