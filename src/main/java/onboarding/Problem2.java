33305adf3cabdf00ed3342a547ae9b90b4df19c2
flydog98 <guinnessoverflow@gmail.com>
2022-10-29 08:33:11 +0900

    feat: solution 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        char top, now;
+
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < cryptogram.length(); i++) {
+            now = cryptogram.charAt(i);
+            if(stack.empty()) {
+                stack.push(now);
+                continue;
+            }
+            top = stack.peek();
+            if(top == now) {
+                stack.pop();
+            }
+            else{
+                stack.push(now);
+            }
+        }
+
+        while(!stack.empty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+
+        answer = sb.toString();
+
         return answer;
     }
 }
