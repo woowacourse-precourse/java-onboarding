260a84fb263b0677a9361b54b128d5a6ccc5006c
KNH <nh2004@naver.com>
2022-10-29 00:36:36 +0900

    add : problem1 solution
    문제1 풀이

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,54 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static Long multiply(Integer n){
+        long sum = 0;
+        while (n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+
+    public static Long add(Integer n){
+        long sum = n % 10;
+        n /= 10;
+        while (n > 0){
+            sum *= n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+
+    public static Long pickBigger(Integer left, Integer right){
+        Long biggerLeft = Math.max(add(left), multiply(left));
+        Long biggerRight = Math.max(add(right), multiply(right));
+
+        return Math.max(biggerLeft, biggerRight);
+    }
+
+    public static Boolean checkException(List<Integer> pages){
+        Integer left = pages.get(0);
+        Integer right = pages.get(1);
+        if (left == 1 && right == 2) return false;
+        else if(left == 399 && right == 400) return false;
+        else if(right - left != 1) return false;
+        else if(!(left % 2 == 1 && right %2 == 0)) return false;
+        else return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!(checkException(pobi) && checkException(crong)))
+            return -1;
+
+        Long pobiNum = pickBigger(pobi.get(0), pobi.get(1));
+        Long crongNum = pickBigger(crong.get(0), crong.get(1));
+
+        if (pobiNum > crongNum) return 1;
+        if (pobiNum < crongNum) return 2;
+        else return 0;
     }
 }
