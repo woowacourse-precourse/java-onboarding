bf7aaeb7d7128741fb0f2dcea0e5a313223c689c
csj <koreaxman@naver.com>
2022-10-29 00:25:40 +0900

    feat: Seperate each nickname with two characters to list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> nicknameList = extractNicknames(forms);
+        List<List<String>> nicknameSplitList = extractNicknameByTwoChar(nicknameList);
 
         return nicknameList;
     }
@@ -18,5 +19,21 @@ public class Problem6 {
         }
 
         return nicknameList;
+    }
+
+    public static List<List<String>> extractNicknameByTwoChar(List<String> nicknameList) {
+        List<List<String>> splitNicknameList = new ArrayList<>();
+
+        for (int i = 0; i < nicknameList.size(); i++) {
+            List<String> splitIndividualNickname = new ArrayList<>();
+
+            for (int j = 0; j < nicknameList.get(i).length() - 1; j++) {
+                splitIndividualNickname.add(nicknameList.get(i).substring(j, j + 2));
+            }
+
+            splitNicknameList.add(splitIndividualNickname);
+        }
+
+        return splitNicknameList;
     }
 }
