04bd8bf44a765fbf5b0780d2c4822161e0c95907
yjlee0235 <yjlee0235@naver.com>
2022-10-28 14:31:34 +0900

    feat: 입력된 수에 대해 손뼉을 친 횟수를 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
+
+    private static final int[] CLAP_COND_DIGITS = {3, 6, 9};
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int clapCountAtNumber(int number) {
+        int count = 0;
+        int tmp = number;
+        while (tmp >= 1) {
+            if (clapCond(tmp % 10))
+                count++;
+            tmp /= 10;
+        }
+        return count;
+    }
+
+    private static boolean clapCond(int digit) {
+        return Arrays.stream(CLAP_COND_DIGITS).anyMatch(num -> digit == num);
     }
 }
