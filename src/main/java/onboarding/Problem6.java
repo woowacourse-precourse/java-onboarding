b001cd34547a6f6f6b2ecc27c8027472dc93b054
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 21:38:37 +0900

    [Problem6] 기능 목록 설계

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    /**
+     * 기능 목록
+     * 1. 닉네임 2글자로 자르기
+     * 2. 자른 2글자로 중복 확인
+     */
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
