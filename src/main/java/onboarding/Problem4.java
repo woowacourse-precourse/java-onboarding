107d0631555d4dbd64a33bd12d0bc4a6d75ea5a8
bradbrad97 <taehyun0121@naver.com>
2022-10-28 19:26:04 +0900

    problem4: 알파벳인지 아닌지 검출하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static boolean isAlphabet(char ch){
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
