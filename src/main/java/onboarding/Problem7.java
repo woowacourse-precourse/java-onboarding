b7965fd258549f7094da0958cc4ee52c619d4b84
KimTaeKang57 <zizoen123@gmail.com>
2022-10-28 15:59:50 +0900

    문제 7 : 리팩토링 -> static 사용 지양

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,11 +10,11 @@ import java.util.*;
  * 4. 점수를 저장하는 map 을 value 기준으로 내림차순 정렬하는 메소드 -> stream 사용 treemap 을 사용했기 때문에 key 는 자동 오름차순 정렬
  **/
 public class Problem7 {
-    private static Map<String, ArrayList> map_friends = new HashMap<>();
-    private static Map<String, Integer> map_points = new TreeMap<>();
+    private Map<String, ArrayList> map_friends = new HashMap<>();
+    private Map<String, Integer> map_points = new TreeMap<>();
 
     // 각자의 친구 목록을 자료구조 hashmap 을 사용해 저장해주는 메소드
-    private static void makeFriendsMap(List<List<String>> friends) {
+    private void makeFriendsMap(List<List<String>> friends) {
         for (List<String> list : friends) {
             ArrayList A_List = map_friends.getOrDefault(list.get(0), new ArrayList());
             ArrayList B_List = map_friends.getOrDefault(list.get(1), new ArrayList());
@@ -28,7 +28,7 @@ public class Problem7 {
     }
 
     // 함께 아는 친구 점수 추가해주는 메서드
-    private static void friendsPlusPoint(String user, ArrayList arrayList) {
+    private void friendsPlusPoint(String user, ArrayList arrayList) {
         for (String str : map_friends.keySet()) {
             if (!str.equals(user)) {
                 ArrayList friendList = map_friends.get(str); // jun {donut, shakevan}
@@ -42,7 +42,7 @@ public class Problem7 {
     }
 
     // visitor 기반으로 방문한 친구 점수 추가해주는 메서드
-    private static void visitorPlusPoint(List<String> visitors, ArrayList arrayList) {
+    private void visitorPlusPoint(List<String> visitors, ArrayList arrayList) {
         for (String str : visitors) {
             if (!arrayList.contains(str)) {
                 map_points.put(str, map_points.getOrDefault(str, 0) + 1);
@@ -51,7 +51,7 @@ public class Problem7 {
     }
 
     // map_points 를 정렬하는 메서드
-    private static void sortPointList(List<String> answer) {
+    private void sortPointList(List<String> answer) {
         List<String> keySet = new ArrayList<>(map_points.keySet());
         // Value 값으로 오름차순 정렬
         keySet.sort((o1, o2) -> map_points.get(o2).compareTo(map_points.get(o1)));
@@ -62,7 +62,7 @@ public class Problem7 {
         }
     }
 
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+    public List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
 
         makeFriendsMap(friends);
