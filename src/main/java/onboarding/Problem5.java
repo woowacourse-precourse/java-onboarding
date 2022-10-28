ea39ba55c97a7d9a03c358485a971e05207701c6
skydreamer21 <skydreamer210@gmail.com>
2022-10-28 18:04:52 +0900

    [Comment] Problem5 - 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,3 +1,15 @@
+/*
+[기능 목록]
+1. 은행 클래스 구현
+    1) 프로퍼티 : 화폐 종류가 담긴 자료구조
+    2) 메서드
+        - constructor
+        - 화폐 종류 추가
+        - 화폐 종류 삭제
+        - 유효한 화폐 여부 확인
+        - 출금
+ */
+
 package onboarding;
 
 import java.util.Collections;
