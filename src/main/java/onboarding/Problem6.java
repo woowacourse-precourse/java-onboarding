d5fe5f508cb78a240474f6fa70db571baf191001
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 21:39:08 +0900

    [Problem6] 비교할 닉네임 2글자씩 자르기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
@@ -9,6 +10,19 @@ public class Problem6 {
      * 1. 닉네임 2글자로 자르기
      * 2. 자른 2글자로 중복 확인
      */
+
+    /**
+     * 1. 닉네임 2글자로 자르기
+     * @param name: 닉네임
+     * @return : 주어진 닉네임을 2글자씩 자른 string list
+     */
+    static List<String> split(String name){
+        List<String> twoWord = new ArrayList<>();
+        for (int i = 0; i < name.length() - 1; i++){
+            twoWord.add(name.substring(i, i+2));
+        }
+        return twoWord;
+    }
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
