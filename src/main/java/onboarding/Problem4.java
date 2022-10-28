acd5112882b7d2a9fefb696d6cf3770f620a9baf
ksh-beginner <ohk9134@naver.com>
2022-10-28 20:29:48 +0900

    refactor : 하드 코딩했던 것들 상수로 리팩토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,14 +1,27 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static final String STRING_INIT_VARIABLE_VALUE = "";
+    private static final int WORD_INDEX_START_NUM = 0;
+    private static final int INT_INIT_VARIABLE_VALUE = 0;
+    private static final Character FIRST_UPPER_CASE_ALPHABET = 'A';
+    private static final Character LAST_UPPER_CASE_ALPHABET = 'Z';
+    private static final Character FIRST_LOWER_CASE_ALPHABET = 'a';
+    private static final Character LAST_LOWER_CASE_ALPHABET = 'z';
+    private static final String EXCEPTION_MESSAGE_PREFIX = "[ERROR] : ";
+    private static final String WRONG_WORD_RANGE_EXCEPTION_MESSAGE = "엄마 말씀은 1 이상 1,000이하의 문자열이어야합니다.";
+    private static final int MIN_WORD_LENGTH = 1;
+    private static final int MAX_WORD_LENGTH = 1000;
+
     public static String solution(String momWord) {
         validateWord(momWord);
         return convertWord(momWord);
     }
 
     public static String convertWord(String word) {
-        String convertWord = "";
-        for (int wordIndex = 0; wordIndex < word.length(); wordIndex++) {
+        String convertWord = STRING_INIT_VARIABLE_VALUE;
+        for (int wordIndex = WORD_INDEX_START_NUM; wordIndex < word.length(); wordIndex++) {
             char charAtWord = word.charAt(wordIndex);
             if (Character.isAlphabetic(charAtWord)) {
                 convertWord += convertAlphabet(charAtWord);
@@ -26,7 +39,7 @@ public class Problem4 {
     }
 
     public static int getCalculationNumberByAlphabetUpperOrLowerCase(Character alphabet) {
-        int calculationNumberByAlphabetUpperOrLowerCase = 0;
+        int calculationNumberByAlphabetUpperOrLowerCase = INT_INIT_VARIABLE_VALUE;
         if (Character.isUpperCase(alphabet)) {
             calculationNumberByAlphabetUpperOrLowerCase = getUpperCaseCalculationNumber();
         }
@@ -37,20 +50,20 @@ public class Problem4 {
     }
 
     public static int getUpperCaseCalculationNumber() {
-        return 'A' + 'Z';
+        return FIRST_UPPER_CASE_ALPHABET + LAST_UPPER_CASE_ALPHABET;
     }
 
     public static int getLowerCaseCalculationNumber() {
-        return 'a' + 'z';
+        return FIRST_LOWER_CASE_ALPHABET + LAST_LOWER_CASE_ALPHABET;
     }
 
     public static void validateWord(String momWord) {
         if(!validateWordRange(momWord)) {
-            throw new IllegalArgumentException("[ERROR] : 엄마 말씀은 1 이상 1,000이하의 문자열이어야합니다.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + WRONG_WORD_RANGE_EXCEPTION_MESSAGE);
         }
     }
 
     public static boolean validateWordRange(String momWord) {
-        return (momWord.length() >= 1 && momWord.length() <= 1000);
+        return (momWord.length() >= MIN_WORD_LENGTH && momWord.length() <= MAX_WORD_LENGTH);
     }
 }
