554e3a73e96493b2a3de75a29021aeff3a97e545
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-28 16:27:47 +0900

    feat: 닉네임의 두글자와 닉네임 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+  public static List<String> solution(List<List<String>> forms) {
+    List<String> nicknames = forms.stream().map(item -> item.get(1)).collect(Collectors.toList());
+    Map<String, Set<String>> keywordStore = getKeywordStore(nicknames);
+    return List.of("");
+  }
+
+  private static Map<String, Set<String>> getKeywordStore(List<String> nicknames) {
+    Map<String, Set<String>> keywordStore = new HashMap<>();
+    for (String name : nicknames) {
+      for (int i = 0; i < name.length() - 1; i++) {
+        String s = "" + name.charAt(i) + name.charAt(i + 1);
+        if (keywordStore.containsKey(s)) {
+          keywordStore.get(s).add(name);
+          continue;
+        }
+        Set<String> nicknameStore = new HashSet<>();
+        nicknameStore.add(name);
+        keywordStore.put(s, nicknameStore);
+      }
     }
+    return keywordStore;
+  }
 }
