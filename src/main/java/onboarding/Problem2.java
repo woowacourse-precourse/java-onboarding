4a5cbabc6b170d8e2c6dfd994e1d09f7a3e397b1
은지 <midzy0228@gmail.com>
2022-10-29 02:41:58 +0900

    feat(Problem2): 1회 암호 해독

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
+/*
+ * 클래스명: Problem2
+ *
+ * 시작 날짜: 2022-10-27
+ */
+
 package onboarding;
 
 public class Problem2 {
+    static String solving(String word){
+        String solvedWord = "";
+        char overlapChar = ' ';
+        int wordLength = word.length();
+
+
+        for (int i=0; i<wordLength-1; i++){
+            if (word.charAt(i) != word.charAt(i+1) && word.charAt(i) != overlapChar){
+                solvedWord = solvedWord + word.charAt(i);
+                overlapChar = ' ';
+            } else if (word.charAt(i) == word.charAt(i+1)){
+                overlapChar = word.charAt(i);
+            }
+        }
+        // 마지막 문제까지 체크
+        if (word.charAt(wordLength-1)!=word.charAt(wordLength-2) && word.charAt(wordLength-1)!=overlapChar){
+            solvedWord = solvedWord + word.charAt(wordLength-1);
+        }
+
+        return solvedWord;
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer = solving(cryptogram);
         return answer;
     }
 }
