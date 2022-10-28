fb7395f704e49be46abb07584d59058c1863d8f4
antaeho <an981022@naver.com>
2022-10-28 10:41:16 +0900

    Problem3 기능구현사항목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
-
+/*
+- 기능 구현사항 목록 -
+1. 1부터 입력값까지 for 문 실행
+2. 해당 숫자를 문자열이 담긴 배열로 변환
+3. 해당 배열에서 3/6/9 count -> answer += count
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
