f47cac70f8e48848f3b0eafbff7b782946915b50
devmizz <garamminchan@gmail.com>
2022-10-28 10:36:11 +0900

    problem4: 입력 문자 분기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,8 +2,25 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        char c;
+
+        for(int i = 0; i < word.length(); i++) {
+            c = word.charAt(i);
+            if(Character.isUpperCase(c)) {
+                sb.append(convertUpper(c));
+                continue;
+            }
+
+            if(Character.isLowerCase(c)) {
+                sb.append(convertLower(c));
+                continue;
+            }
+
+            sb.append(c);
+        }
+
+        return sb.toString();
     }
 
     private static char convertUpper(char c) {
