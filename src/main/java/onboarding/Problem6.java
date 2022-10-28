26e97cee50d0fb5a8a26371ea39376db9a4e063d
hong-sile <gurwns9325@naver.com>
2022-10-28 15:23:39 +0900

    feat : 닉네임을 key, 이메일을 value로 갖는 map 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+    private static Map<String,String> nickEmailMapper = new HashMap<>();
+    private static final int EMAIL=0;
+    private static final int NICKNAME=1;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        initNickEmailMapper(forms);
+
+        return new ArrayList<>();
+    }
+
+    private static void initNickEmailMapper(List<List<String>> forms) {
+        nickEmailMapper.clear();
+        for (List<String> user : forms) {
+            nickEmailMapper.put(user.get(NICKNAME),user.get(EMAIL));
+        }
     }
 }
