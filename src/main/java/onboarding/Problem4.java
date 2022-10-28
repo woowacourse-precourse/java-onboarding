019da99dee2bd87075bf4ccc0fe7b6c68fd75df2
devmizz <garamminchan@gmail.com>
2022-10-28 09:43:48 +0900

    problem4: char converter

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static char convertUpper(char c) {
+        int ascii = c;
+
+        return (char) (Math.abs(ascii - 90) + 65);
+    }
+
+    private static char convertLower(char c) {
+        int ascii = c;
+
+        return (char) (Math.abs(ascii - 122) + 97);
+    }
 }
