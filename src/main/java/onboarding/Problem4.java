7928f3d33e5feb7ffbb6c85a9bdc820d9fb2e199
Hojinida <opqrs1103@gmail.com>
2022-10-29 07:51:29 +0900

    refactor: 문제 4번 아스키코드 값을 'A'와 같은 문자로 대체

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -26,8 +26,8 @@ public class Problem4 {
     }
     public static void setConvert(){
         for(int i=0;i<=25;i++){
-            upperConvert.put((char)(i+65),(char)(90-i));
-            lowerConvert.put((char)(i+97),(char)(122-i));
+            upperConvert.put((char)(i+'A'),(char)('Z'-i));
+            lowerConvert.put((char)(i+'a'),(char)('z'-i));
         }
     }
     public static int checkLowerUpper(char c){
