a3c74917ba6557942b77687424a3a2396e65fe26
이경범 <tkdwk567@naver.com>
2022-10-29 02:33:27 +0900

    # 3 solution - Clap 은 숫자를 받고 3, 6, 9 숫자가 몇번인지 판단하고 박수 횟수를 반환한다.

--- src/main/java/onboarding/problem3/objects/Clap.java
+++ src/main/java/onboarding/problem3/objects/Clap.java
@@ -4,12 +4,18 @@ import java.util.Arrays;
 
 public class Clap {
 
+    private int number;
+
+    public Clap(int number) {
+        this.number = number;
+    }
+
     /*
      *  숫자를 문자열 배열로 만든다.
      *  게임 룰 값과 문자열 배열에 들어 있는 원소와 비교하여 같은 것만 카운트 한다.
      */
-    public int getClapCount(int number) {
-        String numberToString = String.valueOf(number);
+    public int getClapCount() {
+        String numberToString = String.valueOf(this.number);
 
         return (int) Arrays.stream(numberToString.split(""))
                 .flatMap(data ->
