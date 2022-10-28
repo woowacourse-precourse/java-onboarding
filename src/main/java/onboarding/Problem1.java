d1778dd63e3b4446a1b4f84d161d474fbb2308b1
kdkdhoho <hkim4410@naver.com>
2022-10-28 14:34:15 +0900

    Refactor: Problem1 - 접근 제한자 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 class Problem1 {
 
-    static final int EXCEPTION_ANSWER = -1;
-    static final int POBI_WIN = 1;
-    static final int CRONG_WIN = 2;
-    static final int DRAW = 0;
+    static private final int EXCEPTION_ANSWER = -1;
+    static private final int POBI_WIN = 1;
+    static private final int CRONG_WIN = 2;
+    static private final int DRAW = 0;
 
-    static final int FIRST_PAGE = 1;
-    static final int LAST_PAGE = 400;
+    static private final int FIRST_PAGE = 1;
+    static private final int LAST_PAGE = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = 0;
