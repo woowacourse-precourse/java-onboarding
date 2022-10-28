4bde766accbcb937b47676ee25b935240a7375a5
wonseokjee <wsji9404@gmail.com>
2022-10-28 16:02:18 +0900

    221028 prblm4 기능 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,7 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+//    1.hashmap으로 key,value 저장하는 메서드
+//    2. 알파벳이 아니면 그대로 가고 알파벳이면 1번 메서드로 대소문자 판별하는 메서드
+//    3. 소문자이면 아스키코드로 소문자로 변환하는 메서드
 }
