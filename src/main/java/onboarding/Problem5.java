66528e1f31ba7e8f402b57b3f5e11fe15d3b8047
woogi <dnrlwo11@gmail.com>
2022-10-28 23:22:40 +0900

    [problem5] 화폐단위를 남은 금액으로 나누어 answer에 add하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,11 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
         int[] kind_of_money = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         int remain_money = money;
         for (int won: kind_of_money) {
@@ -13,6 +13,9 @@ public class Problem5 {
                 answer.add(0);
                 continue;
             }
+            int won_count = remain_money / won;
+            answer.add(won_count);
+            remain_money -= won_count * won;
         }
         return answer;
     }
