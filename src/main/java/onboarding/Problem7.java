775b9993a8471c53443762e87f106baa0b4dad40
JDK <ehdrjf2062@gmail.com>
2022-10-28 13:38:10 +0900

    feat: 문제7 친구 찾기 기능 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> answer = Collections.emptyList();
+		
+		List<String> myFriendList = findFriends(user, friends);
+		
+		
+		return answer;
+	}
+	
+	private static List<String> findFriends(String user, List<List<String>> friends) {
+		
+		List<String> myFriendList = new ArrayList<>();
+		
+		for (List<String> relationship : friends) {
+			if (relationship.get(0).equals(user)) {
+				myFriendList.add(relationship.get(1));
+			}
+			if (relationship.get(1).equals(user)) {
+				myFriendList.add(relationship.get(0));
+			}
+		}
+		
+		return myFriendList;
+	}
 }
