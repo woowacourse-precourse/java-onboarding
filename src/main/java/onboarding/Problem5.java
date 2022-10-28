275e0432f641594e5959945a4b5d9d1efc764594
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-10-28 16:17:45 +0900

    문제 5번 <1번 기능 구현 완료>

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,39 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        while (money > 0) {
+            answer.add(money/50000);
+            money %= 50000;
+
+            answer.add(money/10000);
+            money %= 10000;
+
+            answer.add(money/5000);
+            money %= 5000;
+
+            answer.add(money/1000);
+            money %= 1000;
+
+            answer.add(money/500);
+            money %= 500;
+
+            answer.add(money/100);
+            money %= 100;
+
+            answer.add(money/50);
+            money %= 50;
+
+            answer.add(money/10);
+            money %= 10;
+
+            answer.add(money);
+            money = 0;
+        }
         return answer;
     }
 }
