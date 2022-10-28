56fcff6f81367ec1603d43221fabbfd6ecea2854
QLoooad <ckcc0813@gmail.com>
2022-10-28 15:36:08 +0900

    각 페이지 홀짝수 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,22 +17,64 @@ class Problem1 {
         }while(Page <= 1 || Page >= 400); //조건 충족 시 리셋
         return Page;
 	}
+	public static int PageL(int A) {
+		int L = A;
+		int PageL;
+		if(L % 2 == 1) {//홀수
+			PageL = L;
+    		} else {//짝수
+    		PageL = L+1;
+    		}
+		return PageL;
+	}
+	public static int PageR(int A) {
+		int R = A;
+		int pageR;
+		if(R % 2 == 1) {//홀수
+			pageR = R;
+    		} else {//짝수
+    		pageR = R-1;
+    		}
+		return pageR;
+	}
     public static void main(String[] args){
-    
-       int pobiPage = makePage();
-       int crongPage = makePage()+2;
+    	
+    	int pobiPage = makePage();
+    	int crongPage = makePage();
+    	
+    	if(pobiPage % 2 == 1) {
+    		
+    	}
+    	
+    	int pobiL = 0;
+        int pobiR = 0;
+        int crongL = 0;
+        int crongR = 0;
         
+    	if(pobiPage % 2 == 1) {//홀수
+    		pobiL = pobiPage;
+    		pobiR = pobiPage+1;
+    		} else {//짝수
+    		pobiL = pobiPage-1;
+    		pobiR = pobiPage;
+    		}
+    	if(crongPage % 2 == 1) {//홀수
+    		crongL = crongPage;
+    		crongR = crongPage+1;
+    		} else {//짝수
+    		crongL = crongPage-1;
+    		crongR = crongPage;
+    		}
+    	
+    	}
+    	
         
-        int pobiL;
-        int pobiR;
-        int crongL;
-        int crongR;
     }
-}
+
 
 
 /*
- * 랜덤번호 2~399 **
+ * 랜덤번호 3~398 첫,막페이지 x *완료*
  * 나온번호 올수짝수 구분 후 홀수 +1 짝수 -1
  * 나온 두 수 *페이지 번호의 각 자리 숫자를 모두 더함 || 모두 곱해 가장 큰 수를 구한다.
  * 더하거나 곱한 수 큰수로 비교
