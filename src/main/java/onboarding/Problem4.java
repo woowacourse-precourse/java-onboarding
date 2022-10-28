651a08af49435c2264448257329fe837d7c5eba9
phobyjun <phobyjun@gmail.com>
2022-10-29 03:00:31 +0900

    [PROBLEM4] COMPLETE1. 문자 대소문자 및 알파벳 구별 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static boolean isUpper(char c) {
+        return 65 <= (int) c && (int) c <= 90;
+    }
+
+    public static boolean isLower(char c) {
+        return 97 <= (int) c && (int) c <= 122;
+    }
+
+    public static boolean isAlpha(char c) {
+        return isUpper(c) || isLower(c);
+    }
 }
