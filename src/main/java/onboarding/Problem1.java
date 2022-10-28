173420e618a540425d7063ced95a28ab60742109
SEONGJOON KIM <jobcho6320@naver.com>
2022-10-28 17:15:10 +0900

    feat : Problem1 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,54 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        // 예외처리
+        if (pobi.get(0) != pobi.get(1) - 1) {
+            return -1;
+        }
+        if (crong.get(0) != crong.get(1) - 1) {
+            return -1;
+        }
+
+        int pobiBigNum = 0;
+        for (Object obj : pobi) {
+            String pobiBookNum = obj.toString();
+            char[] pobiArr = pobiBookNum.toCharArray();
+            int addNum = 0;
+            int mulNum = 1;
+            for (char c : pobiArr) {
+                addNum += (c - 48);
+                mulNum *= (c - 48);
+
+            }
+            if (addNum >= mulNum && addNum >= pobiBigNum) {
+                pobiBigNum = addNum;
+            } else if (addNum <= mulNum && mulNum >= pobiBigNum) {
+                pobiBigNum = mulNum;
+            }
+        }
+
+        int crongBigNum = 0;
+        for (Object obj : crong) {
+            String crongBookNum = obj.toString();
+            char[] crongArr = crongBookNum.toCharArray();
+            int num = 0;
+            int addNum = 0;
+            int mulNum = 1;
+            for (int i = 0; i < crongArr.length; i++) {
+                addNum += (crongArr[i] - 48);
+                mulNum *= (crongArr[i] - 48);
+            }
+            if (addNum >= mulNum && addNum >= crongBigNum) {
+                crongBigNum = addNum;
+            } else if (addNum <= mulNum && mulNum >= crongBigNum) {
+                crongBigNum = mulNum;
+            }
+        }
+
+        if (pobiBigNum > crongBigNum) {
+            return 1;
+        } else if (pobiBigNum < crongBigNum) {
+            return 2;
+        } else return 0;
     }
 }
