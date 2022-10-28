eed31b01d2e8171df556f4c55900834cf6826b6b
wwwndus <simock22@gmail.com>
2022-10-28 20:12:44 +0900

    feat:for문을 진행하며 화폐 개수를 리스트에 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,11 @@ public class Problem5 {
 
         int[] moneyUnit = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        for (int i = 0; i < moneyUnit.length; i++) {
+            answer.add(money/moneyUnit[i]);
+            money %= moneyUnit[i];
+        }
+
         return answer;
     }
 }
