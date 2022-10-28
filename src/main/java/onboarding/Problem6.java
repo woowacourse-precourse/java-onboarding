a9f199fd69ef41c5172e8f2367333f77326e8d6f
egoing <dudqk9696@naver.com>
2022-10-28 23:24:24 +0900

    feat : 중복 구분, 문자파싱후 map에 삽입해주는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
+    static HashMap<String, String> names = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+    static void inputNames(String form) {
+        Set<String> set = new HashSet<>();
+        for(int i=0; i<form.length()-1; i++) {
+            String rem="";
+            rem = rem + form.charAt(i) + form.charAt(i+1);
+            set.add(rem);
+        }
+        for (String strings : set) {
+            checkNames(strings);
+        }
+    }
+
 }
