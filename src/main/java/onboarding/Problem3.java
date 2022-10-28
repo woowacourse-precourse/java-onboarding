713c4bd23f446cea8140d0572450bf6a635fc9b7
wonseokjee <wsji9404@gmail.com>
2022-10-28 13:52:00 +0900

    221028 prblm3-3 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,20 +1,19 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Problem3 {
     public static int solution(int number) {
 
-        int answer = 0;
+        int answer = count369(intToLst(number));
         return answer;
     }
 
     //  1부터 number만큼 문자로 더한 문자열 생성후 한 단위의 숫자배열로 변환하는 메서드
     public static ArrayList<Integer> intToLst(int number) {
         String lstToStr = "";
-        for (int i = 0; i < number; i++) {
+        for (int i = 1; i <= number; i++) {
             lstToStr += Integer.toString(i);
         }
         ArrayList<Integer> strToNumlist = new ArrayList<>();
