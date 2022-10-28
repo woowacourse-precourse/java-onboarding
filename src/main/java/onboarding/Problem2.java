fc73c66321142bf1075ecc26f0f790f1593fb290
tenta <eunoia3802@gmail.com>
2022-10-28 18:21:41 +0900

    feat: 연속하는 중복 문자 삭제 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = deduplicate(cryptogram);
         return answer;
+    }
+
+    public static String deduplicate(String cryptogram) {
+        StringBuilder sb = new StringBuilder(cryptogram);
+        int i = 0;
+        while (true) {
+            if (sb.length() - 2 == i) {
+                if (sb.charAt(i) == sb.charAt(i + 1)) {
+                    sb.delete(i, i + 2);
+                }
+                break;
+            }
+            if (sb.charAt(i) == sb.charAt(i + 1)) {
+                sb.delete(i, i + 2);
+                i = -1;
+            }
+            System.out.println(sb);
+            i++;
+        }
+        return sb.toString();
     }
 }
