a1438d894b695f38214aaf78f7e4009aae1118b9
Amyhds <dasomhan524@gmail.com>
2022-10-28 22:21:17 +0900

    feat: Add connect function and except user&user's friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,24 +1,63 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.Comparator;
 
 public class Problem7 {
-    static HashMap<String,Integer> map = new HashMap<>();
-    public static void connect(List<List<String>> friends){
+    static HashMap<String,Integer> pointMap = new HashMap<>();
+    //find connected friends
+    public static void connect(String start, HashMap<String, ArrayList<String>> lst){
         Stack<String> stack = new Stack<>();
+        //DFS
+        stack.addAll(lst.get(start));
+        while(!stack.isEmpty()){
+            String v = stack.pop();
+            pointMap.put(v, pointMap.getOrDefault(v, 10) + 10);
+            stack.addAll(lst.get(v));
+        }
     }
-    public static void visit(List<String> visitors){
-        for(String v : visitors){
-            map.put(v, map.getOrDefault(v, 1) + 1);
+    //check visited people
+    public static void visit(List<String> lst){
+        for(String v : lst){
+            pointMap.put(v, pointMap.getOrDefault(v, 1) + 1);
         }
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        connect(friends);
+        //generate friend graph
+        HashMap<String, ArrayList<String>> friendMap = new HashMap<>();
+        for(int i = 0; i < friends.size(); i++){
+            String a = friends.get(i).get(0);
+            String b = friends.get(i).get(1);
+            ArrayList<String> aList = new ArrayList<>();
+            ArrayList<String> bList = new ArrayList<>();
+            if(friendMap.containsKey(a)){
+                aList = friendMap.get(a);
+                aList.add(b);
+            }
+            else{
+                aList.add(b);
+            }
+            friendMap.put(a, aList);
+            if(friendMap.containsKey(b)){
+                bList = friendMap.get(b);
+                bList.add(a);
+            }
+            else{
+                bList.add(a);
+            }
+            friendMap.put(b, bList);
+        }
+        connect(user, friendMap);
         visit(visitors);
-        return answer;
+        //make a list to except user and user's friends
+        List<String> except = new ArrayList<>();
+        except.add(user);
+        except.addAll(friendMap.get(user));
+        for(String e : except){
+            pointMap.remove(e);
+        }
+
+        return res;
     }
 }
