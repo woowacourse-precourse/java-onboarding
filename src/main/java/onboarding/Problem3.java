4c5d67cd9b6a55443ccbbe2ac913a5acfdd4cc2b
kiarakim <kiarakim413@gmail.com>
2022-10-28 13:20:07 +0900

    feat(Problem3): Integer to String by using for loop

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String stringNumber = "";
+
+        for (int i = 1; i <= number; i++) {
+            stringNumber = Integer.toString(i);
+        }
         return answer;
     }
 }
