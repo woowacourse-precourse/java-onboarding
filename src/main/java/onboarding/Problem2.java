aa2c840e666c52f385dba11b0aaf3dbffd9cc3e6
Seiyon Park <seiyonpdev@gmail.com>
2022-10-28 16:39:07 +0900

    feat: ADD method isRepeated

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        return deleteRecursive(cryptogram);
     }
+
+    public static Boolean isRepeated(String cryptogram) {
+
+        if(cryptogram.equals(deleteRepeated(cryptogram)))
+            return false;
+        else
+            return true;
+    }
+
+    public static String deleteRepeated(String cryptogram) {
+
+    }
+
+    public static String deleteRecursive(String cryptogram) {
+
+    }
+
 }
