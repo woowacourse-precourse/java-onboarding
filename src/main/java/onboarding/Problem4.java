934320e12f1d32e39d3ecdd7757fd42e059559ab
ymkim97 <rladudaud97@naver.com>
2022-10-29 01:10:52 +0900

    feat: add function change word in reverse

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,18 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
     public static String solution(String word) {
         String[] stringArray = word.split(" ");
+        ArrayList<String> answerArray = new ArrayList<>();
 
         if (throwBoundaryException(word)) {
             return "failed with boundary exception";
         }
 
         for (String eachWord : stringArray) {
-
+            answerArray.add(changeWord(eachWord));
         }
 
-        return ;
+        return String.join(" ", answerArray);
     }
 
     public static boolean throwBoundaryException(String word) {
@@ -20,12 +23,32 @@ public class Problem4 {
         return word.length() < 1 || word.length() > 1000;
     }
 
-    public static boolean isAlphabet(char word) {
+    public static boolean isNotAlphabet(char word) {
         return word < 65 || word > 122 || (word > 90 && word < 97);
     }
 
     public static boolean isLowerCase(char word) {
         return word >= 97 && word <= 122;
+    }
+
+    public static String changeWord(String word) {
+        char[] charArray = word.toCharArray();
+        int index;
+
+        for (index = 0; index < charArray.length; index++) {
+            if (isNotAlphabet(charArray[index])) {
+                continue;
+            }
+
+            if (isLowerCase(charArray[index])) {
+                charArray[index] += 219 - (2 * charArray[index]);
+                continue;
+            }
+
+            charArray[index] += 155 - (2 * charArray[index]);
+        }
+
+        return String.valueOf(charArray);
     }
 
 }
