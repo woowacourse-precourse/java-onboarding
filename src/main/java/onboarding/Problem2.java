e3c00ec46197343bdb6c3f9804e44447d2e6b5d6
baeseohyeon <formnarago@naver.com>
2022-10-28 12:37:46 +0900

    feat: 문제2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> st = new Stack<>();
+        for (char c : cryptogram.toCharArray()) {
+            if(!st.isEmpty() && st.peek() == c){
+                while(!st.isEmpty() && st.peek() == c) {
+                    st.pop();
+                }
+            } else {
+                st.push(c);
+            }
+        }
+        StringBuilder answer = new StringBuilder();
+        while (!st.isEmpty()) {
+            answer.insert(0, st.pop());
+        }
+        return answer.toString();
     }
 }
