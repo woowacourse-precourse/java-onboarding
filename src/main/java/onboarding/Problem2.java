ee32eacc59d7a74fdf0e20bf83bc0f50028af866
hectick <chyeon1223@cau.ac.kr>
2022-10-28 22:29:21 +0900

    feat : [Problem2] 문자열을 1회 검사하며 연속되는 중복문자들을 1로 대체하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,12 +4,20 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
+        int a = cryptogram.charAt(0); //초기 세팅
         //문자열의 처음부터 끝까지 검사하는 것을 반복
         while(true){
             int count = 0; //중복 문자를 처리했을 경우 증가
             //문자열의 처음부터 끝까지 검사하여 연속되는 중복문자들은 1로 대체(문제 조건이 입력은 알파벳 소문자만 주어진다 하였으므로)
+            StringBuffer str = new StringBuffer(cryptogram);
             for(int i = 1; i < cryptogram.length(); i++){
-
+                int b = cryptogram.charAt(i);
+                if(a == b) {
+                    //겹치는 문자를 1로 대체
+                    str.setCharAt(i-1, '1');
+                    str.setCharAt(i, '1');
+                }
+                a = b;
             }
 
             if(count == 0){
