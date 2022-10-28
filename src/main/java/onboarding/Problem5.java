6a7d59d2ec19d989c92e6d7b3cfc8f89a91869c7
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 22:28:28 +0900

    refactor: Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,27 +6,27 @@ import java.util.List;
 
 public class Problem5 {
     private static List<Integer> withdraw(int money){
-        List<Integer> moneyList = new ArrayList<>();
+        List<Integer> currencies = new ArrayList<>();
 
-        moneyList.add(money / 50000);
+        currencies.add(money / 50000);
         money %= 50000;
-        moneyList.add(money / 10000);
+        currencies.add(money / 10000);
         money %= 10000;
-        moneyList.add(money / 5000);
+        currencies.add(money / 5000);
         money %= 5000;
-        moneyList.add(money / 1000);
+        currencies.add(money / 1000);
         money %= 1000;
-        moneyList.add(money / 500);
+        currencies.add(money / 500);
         money %= 500;
-        moneyList.add(money / 100);
+        currencies.add(money / 100);
         money %= 100;
-        moneyList.add(money / 50);
+        currencies.add(money / 50);
         money %= 50;
-        moneyList.add(money / 10);
+        currencies.add(money / 10);
         money %= 10;
-        moneyList.add(money);
+        currencies.add(money);
 
-        return moneyList;
+        return currencies;
     }
 
     public static List<Integer> solution(int money) {
