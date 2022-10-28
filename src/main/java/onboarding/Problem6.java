fbbeb0b13668f49c52eb61c2b0a8ca9b1f416a2c
swandevson <shai4956@gmail.com>
2022-10-28 23:40:05 +0900

    feat: P6 중복 닉네임 체크
    1. 체크할 유저와 다른 유저들을 인자로 받는다.
    2. 해당 유저의 substring과 다른 유저들의 닉네임을 비교한다. 이 때, 자신을 중복체크 할 수 있으니 메일도 함께 비교한다.
    3. 다른 유저와 중복일 경우 set에 추가한다.
    4. 모두 추가한 후 set 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
@@ -7,8 +10,35 @@ public class Problem6 {
         return nickname.substring(idx, idx+2);
     }
 
+    public static HashSet<String> chckDuplicates(List<String> userForm, List<List<String>> forms) {
+        HashSet<String> dupMailSet = new HashSet<>();
+
+        String userMail = userForm.get(0);
+        String userNickname = userForm.get(1);
+
+        for(int i = 0; i < userNickname.length() - 1; i++) {
+            String subNickname = sliceNickname(userNickname, i);
+
+            for(int j = 0; j < forms.size(); j++) {
+                List<String> otherForm = forms.get(j);
+                String otherMail = otherForm.get(0);
+                String otherNickname = otherForm.get(1);
+
+                if (otherNickname.contains(subNickname) && !otherMail.equals(userMail)) {
+                    dupMailSet.add(otherMail);
+                }
+            }
+        }
+        return dupMailSet;
+    }
+
+
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+
+
         return answer;
     }
 }
