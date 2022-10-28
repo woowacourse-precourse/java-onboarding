ca6ad46e62b7f1288d1417cf4f91e07bfc27e38d
Amyhds <dasomhan524@gmail.com>
2022-10-28 19:28:34 +0900

    feat: Add HashMap and visit function

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,24 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
+import java.util.HashMap;
 
 public class Problem7 {
+    static HashMap<String,Integer> map = new HashMap<>();
+    public static void connect(List<List<String>> friends){
+
+    }
+    public static void visit(List<String> visitors){
+        for(int i = 0; i < visitors.size(); i++){
+            String key = visitors.get(i);
+            map.put(key, map.containsKey(key) ? map.get(key) + 3: 3);
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        connect(friends);
+        visit(visitors);
         return answer;
     }
 }
