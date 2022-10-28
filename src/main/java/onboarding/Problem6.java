6dd553f601554e1dd17b0daa3842beacc4f20c36
seokjin <rnrn8678@naver.com>
2022-10-28 11:31:54 +0900

    feat(Problem6.java): 매개변수로 받은 리스트를 User 리스트로 변환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,15 +1,25 @@
 package onboarding;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
-		List<String> answer = List.of("answer");
-		return answer;
+		List<User> userList = forms.stream()
+				.map(User::new)
+				.collect(Collectors.toList());
+
+		return Collections.emptyList();
 	}
 
 	static class User {
 		private String email;
 		private String nickname;
+
+		public User(List<String> form) {
+			this.email = form.get(0);
+			this.nickname = form.get(1);
+		}
 	}
 }
