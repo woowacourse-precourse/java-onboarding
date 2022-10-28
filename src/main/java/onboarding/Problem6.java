7dc1b5d1d85dbc9b1b5c783f1c3cd09b016fbf23
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-28 18:14:30 +0900

    feat: [Problem6] - Map을 통해 이메일과, 2글자로 분리한 닉네임을 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+
+    private final static int EMAIL = 0;
+    private final static int NICKNAME = 1;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String, List<String>> map = new LinkedHashMap<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(NICKNAME);
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                list.add(nickname.substring(i, i + 2));
+            }
+            map.put(form.get(EMAIL), list);
+        }
+
         return answer;
+
     }
+
 }
