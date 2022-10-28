ba7c3db781c93102f01d62ea23e2053d7fcc25b8
ParkRootSeok <g_e_un@naver.com>
2022-10-28 14:00:10 +0900

    docs : Problem4 Function List 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 public class Problem4 {
+
+    static public boolean isAlphabet(char alphabet) {
+
+    }
+
+    static public boolean reverseAlphabet(StringBuilder word) {
+
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
