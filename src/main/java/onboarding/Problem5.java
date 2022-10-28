1c6ab18ced3656113b3828b9e8c6c60622cb4d59
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-28 12:52:59 +0900

    style: [Problem5] - 메소드 파라미터 순서 변경(가독성 개선)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,12 +12,12 @@ public class Problem5 {
         List<Integer> moneyUnits = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
         for (Integer moneyUnit : moneyUnits) {
-            money = exchangeMoney(money, answer, moneyUnit);
+            money = exchangeMoney(money, moneyUnit, answer);
         }
         return answer;
     }
 
-    private static int exchangeMoney(int money, List<Integer> answer, int moneyUnit) {
+    private static int exchangeMoney(int money, int moneyUnit, List<Integer> answer) {
         if (money < moneyUnit) {
             answer.add(DID_NOT_EXCHANGE_THIS_UNIT);
             return money;
