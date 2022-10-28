c10c5efb52d9945c99728753770da890d093cb3a
JunTaeINC <juntea3523@naver.com>
2022-10-29 00:10:47 +0900

    Problem 1 - 기능구현 리스트 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+	
+	/* 기능 구현 리스트 
+	 * 1. 예외검사 - 페이지 수가 1~400 인지 검사하고 / 오른쪽페이지수 - 왼쪽페이지수 = 1 인지 검사 / 시작면 마지막 면이 아닌지 검사 
+	 * 2. 왼쪽,오른쪽 페이지 수의 Max(각자리수의 합 / 각자리수의 곱) 구현  
+	 * 3. 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1 구현  
+	***/
+	
+	
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
