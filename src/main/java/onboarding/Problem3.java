1f1b677edc4b147da18223e473831604f473fa21
dydtmd6801 <dydtmd6801@naver.com>
2022-10-28 22:34:34 +0900

    feat : 각 자리수에 3의 배수가 있다면 손뼉 증가 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++){
+            answer += clap(i);
+        }
         return answer;
+    }
+
+    private static int clap(int number){
+        int clap = 0;
+        String strNumber = Integer.toString(number);
+        for(int i = 0; i < strNumber.length(); i++){
+            if((strNumber.charAt(i) - '0') % 3 == 0 && strNumber.charAt(i) - '0' != 0){
+                clap++;
+            }
+        }
+        return clap;
     }
 }
