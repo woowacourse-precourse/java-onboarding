9adcd7811ebcae9fc485f45332fc32c404e7b028
이경범 <tkdwk567@naver.com>
2022-10-28 14:13:58 +0900

    # 3 solution 게임 규칙의 값을 정의하는 열거타입 생성

---
+++ src/main/java/onboarding/problem3/objects/RuleValue.java
@@ -0,0 +1,14 @@
+package onboarding.problem3.objects;
+
+public enum RuleValue {
+
+    TREE("3"),
+    SIX("6"),
+    NINE("9");
+
+    private final String value;
+
+    RuleValue(String value) {
+        this.value = value;
+    }
+}
