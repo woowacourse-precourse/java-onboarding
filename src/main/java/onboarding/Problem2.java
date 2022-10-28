632e6a1c8d35980217e68116fd2ff4f5073d59a7
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-28 09:18:02 +0900

    Problem2(설계): 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    /**
+     * cryptogram 으로 암호화된 문자열을 해독하는 메소드
+     * @param cryptogram : 해독할 문자열
+     * @return 해독된 문자열
+     */
+    private static String decodeCryptogram(String cryptogram){
+
+    }
+
+    /**
+     * 문자열의 중복 문자열을 찾아 제거하는 메소드
+     * @param str : 중복 문자열을 삭제할 메소드
+     * @return 중복 문자열이 제거된 문자열
+     */
+    private static String removeDuplication(String str){
+
     }
 }
