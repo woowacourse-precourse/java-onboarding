164942eb71727db14e5fe8d279c197fad704fa91
cookienc <kim76744@naver.com>
2022-10-28 13:07:30 +0900

    refactor(Problem2): Problem2 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,23 +1,40 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static final char ALPHABET_START = 'a';
+    public static final char ALPHABET_END = 'z';
+    public static final String DUPLICATE = "{2,}";
+
     public static String solution(String cryptogram) {
-        char start = 'a';
-        char end = 'z';
-
         while (true) {
-            int length = cryptogram.length();
-            for (char c = start; c <= end; c++) {
-                StringBuilder duplicateAlphabet = new StringBuilder();
-                duplicateAlphabet.append(c).append("{2,}");
-                cryptogram = cryptogram.replaceAll(duplicateAlphabet.toString(), "");
-            }
+            int originLength = cryptogram.length();
+            cryptogram = removeDuplicateAlphabet(cryptogram);
 
-            if (cryptogram.length() == length) {
+            if (isSame(cryptogram, originLength)) {
                 break;
             }
         }
 
         return cryptogram;
+    }
+
+    private static String removeDuplicateAlphabet(String cryptogram) {
+        for (char c = ALPHABET_START; c <= ALPHABET_END; c++) {
+            StringBuilder duplicateAlphabetRegex = getRegex(c);
+            cryptogram = cryptogram.replaceAll(duplicateAlphabetRegex.toString(), "");
+        }
+        return cryptogram;
+    }
+
+    private static boolean isSame(String cryptogram, int originLength) {
+        return cryptogram.length() == originLength;
+    }
+
+    private static StringBuilder getRegex(char alphabet) {
+        StringBuilder duplicateAlphabet = new StringBuilder();
+        duplicateAlphabet.append(alphabet)
+                .append(DUPLICATE);
+        return duplicateAlphabet;
     }
 }
