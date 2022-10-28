e825f0ab311b0c181b0d36b2ffed764ebeebf7b2
dmdrk1414 <seungchan141414@gmail.com>
2022-10-29 00:26:39 +0900

    [Add] Problem_5 완료
    이문제는 학교에서 많이 풀었던 지페를 나누는 문제입니다.
    옛날에 풀었던 것 처럼 풀수있지만
    메서드를 이용해서 풀고싶었습니다.
    향후 문제 생기면 바로 수정 할 것입니다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,40 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    static int[] seatMoney = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int resultArrSize = seatMoney.length;
+        ArrayList<Integer> resultArr = new ArrayList<>(resultArrSize);
+        getResultArr(resultArr, money);
+        return resultArr;
+    }
+
+    private static void getResultArr(ArrayList<Integer> arr, int money) {
+        int[] intArr = new int[2];
+        int end = seatMoney.length;
+        for (int i = 0; i < end; i++) {
+            if (invertorMoney(money, seatMoney[i], intArr)) {
+                int typeMoney = intArr[0];
+                arr.add(typeMoney);
+                money = intArr[1];
+            } else {
+                arr.add(0);
+            }
+        }
+    }
+
+    // intArr 에 [ 지페의 갯수, 남은 돈 ] 을 하여 할당한다.
+    private static boolean invertorMoney(int money, int typeMoney, int[] intArr) {
+        if (money >= typeMoney) {
+            intArr[0] = money / typeMoney;
+            intArr[1] = money - intArr[0] * typeMoney;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
