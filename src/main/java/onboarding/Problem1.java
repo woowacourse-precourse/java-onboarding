ea3208cca34a960ff70d940c5f67a518a1b8ca40
kimjaehwi1 <meatandcoffee@naver.com>
2022-10-28 18:14:24 +0900

    pobi와 crong이 편 책 페이지가 성립하는지 테스트하는 메소드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,21 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int[] po = pobi.stream().mapToInt(Integer::intValue).toArray();
+        int[] cr = crong.stream().mapToInt(Integer::intValue).toArray();
+
+        if(equals(po[0],po[1])&&equals(cr[0],cr[1])) {
+
+        }
+
+
+
         return answer;
     }
+    public static boolean equals(int arr0, int arr1) {
+
+        return (arr0+1==arr1&&arr0%2!=0&&arr1%2==0);
+
+    }
+
 }
