bf90c60bf485aec8e1b44410e9756a9f52e0f950
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-28 10:10:12 +0900

    comment: change comment of function list

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,7 @@ import onboarding.problem4.StringReverser;
 
 /**
  * Problem 4 function list
- * - change string
+ * - reverse string
  */
 public class Problem4 {
     public static String solution(String word) {
