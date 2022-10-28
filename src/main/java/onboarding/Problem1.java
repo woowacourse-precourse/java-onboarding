70bb0f63decf4d584da5188328b01d23fca3b56a
Kwon-sang <kwonsg514@gmail.com>
2022-10-28 14:13:29 +0900

    Problem1 and Problem2 리팩토링 Problem2 : 기존 Queue 대신, StringBuilder로

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,44 +5,39 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        //When incorrect input
         if (!isCorrect(pobi) || !isCorrect(crong)) return -1;
 
-        //Calculating scores respectively
         int pobiScore = score(pobi);
         int crongScore = score(crong);
 
-        //Judging winner
         if(pobiScore == crongScore) return 0;
-        else return pobiScore > crongScore ? 1 : 2;
+        return pobiScore > crongScore ? 1 : 2;
     }
 
     private static boolean isCorrect(List<Integer> person) {
-        int leftPage = person.get(0);
-        int rightPage = person.get(1);
-        if(rightPage-leftPage!=1 || rightPage%2 != 0 || rightPage<1) return false;
+        Integer leftPage = person.get(0);
+        Integer rightPage = person.get(1);
+        if(rightPage-leftPage!=1) return false;
+        if(leftPage < 1 || rightPage > 400) return false;
         return true;
     }
 
     static int score(List<Integer> person) {
         int leftPage = person.get(0);
         int rightPage = person.get(1);
-
         int maxDigitSum = Math.max(digitSum(leftPage), digitSum(rightPage));
         int maxDigitProduct = Math.max(digitProduct(leftPage), digitProduct(rightPage));
 
         return Math.max(maxDigitSum, maxDigitProduct);
     }
 
-    static int digitSum(int n){
-        String[] digits = String.valueOf(n).split("");
-        return Arrays.stream(digits).
-                mapToInt(Integer::parseInt).sum();
+    static int digitSum(int page){
+        String[] digits = String.valueOf(page).split("");
+        return Arrays.stream(digits).mapToInt(Integer::parseInt).sum();
     }
 
-    static int digitProduct(int n){
-        String[] digits = String.valueOf(n).split("");
-        return Arrays.stream(digits).
-                mapToInt(Integer::parseInt).reduce(1, (x,y)->x*y);
+    static int digitProduct(int page){
+        String[] digits = String.valueOf(page).split("");
+        return Arrays.stream(digits).mapToInt(Integer::parseInt).reduce(1, (x,y)->x*y);
     }
 }
