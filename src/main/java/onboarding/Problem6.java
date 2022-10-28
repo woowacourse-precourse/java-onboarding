03209a3d57f0f26ab509bef1e0043ed994bb316d
kiarakim <kiarakim413@gmail.com>
2022-10-29 00:26:35 +0900

    feat(Problem6): limit the length of email and nickname

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,19 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for (List<String> person : forms) {
+            int emailLength = person.get(0).length();
+            if (emailLength < 11 || emailLength >= 20) {
+                break;
+            }
+
+            String nickname = person.get(1);
+            int nameLength = nickname.length();
+            if (nameLength < 1 || nameLength >= 20) {
+                break;
+            }
+        }
         return answer;
     }
 }
