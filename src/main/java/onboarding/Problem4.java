9653d148a354179c93ac2749dfa7a7287cee1cf3
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:14:44 +0900

    docs(문제4): 기능목록 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 📚 기능 목록
+ *
+ * 1. 문자를 알파벳 역순으로 매칭하여 변환하는 기능
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
