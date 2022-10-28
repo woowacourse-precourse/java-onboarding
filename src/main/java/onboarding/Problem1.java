60ccc5ea2820de745ea688e3818aa85a92cd90ff
im-shung <seoyoung1113@gmail.com>
2022-10-28 23:09:58 +0900

    :recycle: [문제1] Person객체로 페이지 번호 입력 예외처리 로직을 리팩토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.List;
 import java.util.Map;
+import onboarding.problem1.Person;
 
 class Problem1 {
 
@@ -16,28 +17,37 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
+        List<Person> personList = List.of(
+                new Person("pobi", pobi.get(LEFT), pobi.get(RIGHT)),
+                new Person("crong", crong.get(LEFT), crong.get(RIGHT))
+        );
+
         // 1. [예외처리] 페이지 번호 입력
+        for (Person person: personList) {
+            int leftPageNumber = person.getLeftPageNumber();
+            int rightPageNumber = person.getRightPageNumber();
 
-        // 시작면(0) 또는 마지막면(401) 입력한 경우
-        if (isOutRange(pobi) || isOutRange(crong))
-            return scoreMenu.get("exception");
+            // 시작면(0) 또는 마지막면(401) 입력한 경우
+            if (isOutRange(leftPageNumber) || isOutRange(rightPageNumber))
+                return scoreMenu.get("exception");
 
-        // 왼쪽페이지가 홀수가 아닌 경우
-        if (!isOddNumber(pobi.get(LEFT)) || !isOddNumber(crong.get(LEFT)))
-            return scoreMenu.get("exception");
+            // 왼쪽페이지가 홀수가 아닌 경우
+            if (!isOddNumber(leftPageNumber))
+                return scoreMenu.get("exception");
 
-        // 오른쪽페이지가 짝수가 아닌 경우
-        if (!isEvenNumber(pobi.get(RIGHT)) || !isEvenNumber(crong.get(RIGHT)))
-            return scoreMenu.get("exception");
+            // 오른쪽페이지가 짝수가 아닌 경우
+            if (!isEvenNumber(rightPageNumber))
+                return scoreMenu.get("exception");
 
+        }
 
         int answer = Integer.MAX_VALUE;
         return answer;
     }
 
     // 책의 시작 면이나 마지막 면이 있는지 확인하는 메서드
-    public static boolean isOutRange(List<Integer> pageList) {
-        return pageList.contains(firstPageNumber) || pageList.contains(lastPageNumber);
+    public static boolean isOutRange(int number) {
+        return number==firstPageNumber || number==lastPageNumber;
     }
 
     // 홀수인지 확인하는 메서드
