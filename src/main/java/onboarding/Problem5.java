a29e9c9209df907d3bebc829c98881b4ae41a78d
Lee Sang Bin <rhkd2612@naver.com>
2022-10-28 19:15:30 +0900

    docs: problem5 methods

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ * # 기능 정리
+ * [X] 필요한 화폐 계산 -> 그리디
+ * [X] solution 작성
+ * [X] 테스트 확인
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
