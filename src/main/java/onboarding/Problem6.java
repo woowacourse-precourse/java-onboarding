cd3b0a490ba6e2fa8a704574e0a4bb51e3877688
woo-chang <clllickme@naver.com>
2022-10-28 21:23:41 +0900

    feat: 닉네임을 2글자로 나눈 결과 생성 기능 구현 (Problem6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+  public static List<String> solution(List<List<String>> forms) {
+    List<List<String>> parsingForms = new ArrayList<>();
+    for (List<String> form : forms) {
+      parsingForms.add(Converter.convert(form));
     }
+    return new ArrayList<String>();
+  }
+
+  static class Converter {
+
+    public static List<String> convert(List<String> form) {
+      List<String> result = new ArrayList<>();
+      result.add(form.get(0));
+
+      String nickname = form.get(1);
+      for (int i = 0; i < nickname.length() - 1; i++) {
+        result.add(nickname.substring(i, i + 2));
+      }
+      return result;
+    }
+  }
 }
