e2cd0529843669c59403eac8592c717093ecb304
opuopuo <pureuml.dev@gmail.com>
2022-10-28 18:05:03 +0900

    feat: 점수 구하기 및 승자 판별 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,14 +9,69 @@ class Problem1 {
     static final int RIGHT = 1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
-        if ( !verifyException(pobi) && !verifyException(crong)) {
+        if ( !verifyException(pobi) || !verifyException(crong)) {
             return -1;
         }
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiScore = getMaxNumber(pobi);
+        int crongScore = getMaxNumber(crong);
+
+        return getWinner(pobiScore, crongScore);
+    }
+
+    private static int getWinner(int pobiScore, int crongScore) {
+       if (pobiScore > crongScore) {
+           return 1;
+       }
+       if (crongScore > pobiScore) {
+           return 2;
+       }
+       return 0;
+    }
+
+    private static int getMaxNumber(List<Integer> someone) {
+        Integer left = calculateNumber(someone.get(LEFT));
+        Integer right = calculateNumber(someone.get(RIGHT));
+
+        return (Math.max(left, right));
+    }
+
+    private static int calculateNumber(Integer page) {
+        int getSum = addNumber(page);
+        int getMul = mulNumber(page);
+
+        return (Math.max(getSum, getMul));
+    }
+
+    private static int addNumber(Integer page) {
+        int result = 0;
+
+        if (page < 10) {
+            return page;
+        }
+        while(page != 0) {
+            result += page % 10;
+            page /= 10;
+        }
+        return result;
+    }
+
+    private static int mulNumber(Integer page) {
+        int result = 1;
+
+        if (page < 10) {
+            return page;
+        }
+        while(page != 0) {
+            result *= page % 10;
+            page /= 10;
+        }
+        return result;
+
     }
 
     private static boolean verifyException(List<Integer> someone) {
+        if (!sizeCheck(someone))
+            return false;
         if (!isInRange(someone))
             return false;
         if (!isContinuous(someone))
@@ -25,6 +80,13 @@ class Problem1 {
             return false;
         return true;
     }
+
+    private static boolean sizeCheck(List<Integer> someone) {
+        if (someone == null)
+            System.out.println("null");
+        return (someone != null && someone.size() == 2);
+    }
+
     private static boolean isInRange(List<Integer> someone) {
         return (someone.get(LEFT) >= 1 && someone.get(LEFT) <= 399 && someone.get(RIGHT) >= 2 && someone.get(RIGHT) <= 400);
     }
