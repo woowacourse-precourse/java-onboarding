0c475b532c5d0a29623348f0b4a1531bbdebaaee
Your Name <dbsdbxkrzz@naver.com>
2022-10-28 16:08:43 +0900

    feat: Problem4의 문자 변환 메서드를 구현합니다
    translateLower 메서드와 translateUpper메서드를 구현합니다
    상수 UPPER = 155, LOWER = 219
    를 선언합니다.
    아스키 코드 값이 매개변수로 주어지면
    대문자의 경우 155에서 뺀 값을 문자로 변환하여 return하고
    소문자의
    경우 219에서 뺀 값을 문자로 변환하여 return합니다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+
+	public static final int LOWER = 219;
+	public static final int UPPER = 155;
+
 	public static StringBuilder sb = new StringBuilder();
 
 	public static String solution(String word) {
@@ -9,16 +13,21 @@ public class Problem4 {
 	}
 
 	public static void treeFrogDictionary(String word) {
-
+		for (int i = 0; i < word.length(); i++) {
+			sb.append(isAlpha(word.charAt(i)));
+		}
 	}
 
 	public static char isAlpha(char c) {
-
+		
 		return c;
 	}
 
-	public static char translate(char c) {
+	public static char translateLower(int c) {
+		return (char)(LOWER - c);
+	}
 
-		return c;
+	public static char translateUpper(int c) {
+		return (char)(UPPER - c);
 	}
 }
