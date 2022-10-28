1ea484ba81bea902bd3d6a400b76343e7f37eb45
footdev <ekclstkfka5@gmail.com>
2022-10-28 16:08:57 +0900

    feat: 기능2 메소드 및 Solution 함수 작성
    메소드 내부 구현은 X
    전체적인 명세만 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
+    public static StringBuilder cryptogramTranslation;
+    public static String answer;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        cryptogramTranslation = new StringBuilder(cryptogram);
+
+        while (cryptogram.length() > 0 && checkOverlap(cryptogramTranslation)) {
+            removeOverlap(cryptogramTranslation);
+        }
+
+        answer = cryptogramTranslation.toString();
         return answer;
+    }
+
+    public static boolean checkOverlap(StringBuilder cryptogram) {
+        return true;
+    }
+
+    public static void removeOverlap(StringBuilder cryptogram) {
+
     }
 }
