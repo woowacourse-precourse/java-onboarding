deb7527c1ba5c2720086aa5625fd5fa10582ac5b
jooys130 <wndustn130@gmail.com>
2022-10-28 15:55:36 +0900

    feat: <Problem1> p길이는 2이다 다(예외처리)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(listException(pobi) || listException(crong)){
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean listException(List<Integer> list){
+        return list.size() != 2;
     }
 }
