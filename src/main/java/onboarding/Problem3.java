eb744cfa5e71ea1aa8743073fe0dcbaf074c562d
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-29 04:57:36 +0900

    refactor(problem3): divide by function
    * Before: countClap() 내부에 자릿수 나누는 함수 포함
    
    * After: splitByDigit(), countClap()으로 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
 
 	private static int clap;
@@ -7,18 +10,26 @@ public class Problem3 {
 	public static int solution(int number) {
 		clap = 0;
 		for (int i = 1; i < number + 1; i++) {
-			countClap(i);
+			List<Integer> digits = splitByDigit(i);
+			countClap(digits);
 		}
 		return clap;
 	}
 
-	private static void countClap(int number) {
+	private static List<Integer> splitByDigit(int number) {
+		List<Integer> digits = new ArrayList<>();
 		while (number > 0) {
-			int digit = number % 10;
+			digits.add(number % 10);
+			number /= 10;
+		}
+		return digits;
+	}
+
+	private static void countClap(List<Integer> digits) {
+		for (Integer digit : digits) {
 			if (is369(digit)) {
 				clap += 1;
 			}
-			number /= 10;
 		}
 	}
 
