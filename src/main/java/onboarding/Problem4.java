d5c6ac4c43ce096de469e71df3748d45646279f7
youhobin <ghdb132@naver.com>
2022-10-28 11:17:18 +0900

    feat(onboarding) : 제시된 word에서 한글자씩 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            changeWord(word.charAt(i));
+        }
         String answer = "";
         return answer;
+    }
+
+    private static void changeWord(char charAt) {
+        
     }
 }
