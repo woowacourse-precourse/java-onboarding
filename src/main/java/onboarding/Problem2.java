c350e6e70aec50c3eea28bb611540ff0bccd7358
taeyun-work <wmfajr1357@gmail.com>
2022-10-29 01:28:36 +0900

    [Solved] Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        boolean flag = false;
+        int startIdx = 0;
+        int endIdx = 0;
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                if (flag == false) {
+                    startIdx = i;
+                }
+                flag = true;
+            } else {
+                if (flag == true) {
+                    endIdx = i;
+                    break;
+                }
+            }
+        }
+
+        if (flag == false) {
+            return cryptogram;
+        }
+
+        return solution(cryptogram.replaceAll(cryptogram.substring(startIdx, endIdx + 1), ""));
     }
 }
