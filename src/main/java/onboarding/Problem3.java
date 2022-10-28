311fb15bcaaffb170db6808b3779c878b94d751f
cookienc <kim76744@naver.com>
2022-10-28 16:09:45 +0900

    feat(Problem3): Problem3 제한사항 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,14 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static final String PLEASE_ENTER_RIGHT_NUMBER = "범위 내의 숫자를 입력해주세요";
+    public static final int MIN = 1;
+    public static final int MAX = 10000;
+
     public static int solution(int number) {
+        validateNumber(number);
+
         int clap = 0;
         for (int i = 1; i <= number; i++) {
             String currentNum = String.valueOf(i);
@@ -16,5 +23,11 @@ public class Problem3 {
         }
 
         return clap;
+    }
+
+    private static void validateNumber(int number) {
+        if (number < MIN || number > MAX) {
+            throw new IllegalArgumentException(PLEASE_ENTER_RIGHT_NUMBER);
+        }
     }
 }
