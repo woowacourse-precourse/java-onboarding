47e84a032648d24748cc3b9d9ac9a5e06812ff83
yeonhee <ydol2@naver.com>
2022-10-29 02:27:36 +0900

    [4번] feat : 문자 변환 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /**
+     * 문자 변환 함수 구현
+     */
+    private static char conversion(char character){
+        if (Character.isUpperCase(character))
+            return (char)('Z' - (character - 'A'));
+        else if (Character.isLowerCase(character))
+            return (char)('z' - (character - 'a'));
+        else
+            return character;
+    }
 }
