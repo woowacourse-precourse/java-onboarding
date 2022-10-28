9d79965b5cce9afb4dab6ddccb523acbba4e07f1
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:06:58 +0900

    feat(문제2): 문자의 중복을 확인, 제거하는 함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 📚 기능 목록
  *
@@ -8,7 +11,35 @@ package onboarding;
  */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        answer = removeDuplicate(answer);
         return answer;
+    }
+
+    /**
+     * 문자의 중복을 제거한다.
+     * @param cryptogram 문자열
+     * @return 중복이 제거된 문자열
+     */
+    private static String removeDuplicate(String cryptogram) {
+
+        String result = cryptogram.replaceAll("(\\w)\\1+", "");
+
+        if(checkDuplicate(result)) {
+            return removeDuplicate(result);
+        }
+
+        return result;
+    }
+
+    /**
+     * 문자의 중복을 확인한다.
+     * @param cryptogram 문자열
+     * @return 문자의 중복 여부
+     */
+    private static boolean checkDuplicate(String cryptogram) {
+        Pattern pattern = Pattern.compile("(\\w)\\1+");
+        Matcher matcher = pattern.matcher(cryptogram);
+        return matcher.find();
     }
 }
