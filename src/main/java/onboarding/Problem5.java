a2d571945d088f16d8b630e746a76b8ad92f70cc
devmizz <garamminchan@gmail.com>
2022-10-28 15:07:37 +0900

    problem5: 단위 별 개수 세기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,39 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    private static int getFiveMillion(int money) {
+        return money / 50000;
+    }
+
+    private static int getOneMillion(int money) {
+        return money / 10000;
+    }
+
+    private static int getFiveThousand(int money) {
+        return money / 5000;
+    }
+
+    private static int getOneThousand(int money) {
+        return money / 1000;
+    }
+
+    private static int getFiveHundred(int money) {
+        return money / 500;
+    }
+
+    private static int getOneHundred(int money) {
+        return money / 100;
+    }
+
+    private static int getFifty(int money) {
+        return money / 50;
+    }
+
+    private static int getTen(int money) {
+        return money / 10;
     }
 }
