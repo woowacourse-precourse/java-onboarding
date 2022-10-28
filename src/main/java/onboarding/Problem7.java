e1969550ab3fac8d431e3e83c1ada6d7e1e32f9a
hectick <chyeon1223@cau.ac.kr>
2022-10-29 02:50:14 +0900

    feat : [Problem7] 기능 목록 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        //함께아는 친구 점수 계산하는 기능
+        //visitor 점수 계산하는 기능
+        //점수높은 5명 뽑는 기능
+
         return answer;
     }
 }
