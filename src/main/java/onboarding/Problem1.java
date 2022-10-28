110fe8a190ec40ffa0d0eae992cf567f12903952
heejeong <hjx66@naver.com>
2022-10-29 00:17:00 +0900

    feat: 문제 1 예외 상황 처리 - docs에 구현 기능 목록에도 예외 상황 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,7 +13,18 @@ class Problem1 {
         }
         return Math.max(plus,mul);
     }
+    public static boolean checkException(int left, int right){
+        if(left%2==0 || right%2!=0)
+            return false;
+        if(right != left+1)
+            return false;
+        return true;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(!checkException(pobi.get(0),pobi.get(1)) || !checkException(crong.get(0),crong.get(1))){
+            return -1;
+        }
+
         int p_left = getPageMaxValue(pobi.get(0));
         int p_right = getPageMaxValue(pobi.get(1));
         int p_max = Math.max(p_left,p_right);
@@ -21,6 +32,7 @@ class Problem1 {
         int c_left = getPageMaxValue(crong.get(0));
         int c_right = getPageMaxValue(crong.get(1));
         int c_max = Math.max(c_left,c_right);
+
         if(p_max > c_max)
             return 1;
         if (p_max < c_max)
