2d39d2333e00c3416be84dcb7ded167e44025728
sosow0212 <sosow0212@naver.com>
2022-10-28 16:56:24 +0900

    feat: Problem5 - solution 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,7 +8,7 @@ public class Problem5 {
     static final int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = getCount(money);
         return answer;
     }
 
