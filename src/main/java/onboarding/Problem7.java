733d4082e69fc31c1c3abeab9ac9124eeef348b0
LHS-11 <ghktjq1119@naver.com>
2022-10-28 16:14:17 +0900

    Problem7 : 1-1 예외 체크 (user 길이 체크)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import onboarding.problem7.NotMatchingUserLength;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        //1. 예외사항
+        //- user 길이 1 이상 30 이하인지 체크
+        if(isNotMatchingUserLength(user.length())){
+            throw new NotMatchingUserLength("사용자의 길이가 맞지 않습니다.");
+        }
+
+        //- friends 길이 1 이상 10000 이하인지 체크
+        //- friends 의 각 원소의 길이가 2인지 체크
+        //- 아이디 길이 1 이상 30 이하인지 체크
+        //- visitors 길이 0 이상 10000 이하인지 체크
+        //- 사용자 아이디 소문자인지 체크
+
+
         return answer;
+    }
+
+    private static boolean isNotMatchingUserLength(int length) {
+        return length < 1 || length > 30;
     }
 }
---
+++ src/main/java/onboarding/problem7/NotMatchingUserLength.java
@@ -0,0 +1,8 @@
+package onboarding.problem7;
+
+public class NotMatchingUserLength extends RuntimeException{
+
+    public NotMatchingUserLength(String message) {
+        super(message);
+    }
+}
