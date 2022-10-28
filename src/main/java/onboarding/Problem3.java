38ef7e8789f2ce9a10008b233c5746777ece6834
이경범 <tkdwk567@naver.com>
2022-10-28 15:43:47 +0900

    # 3 solution - 제약 사항에 맞지 않으면 예외를 던지기 때문에 try catch 로 예외처리 진행 - 예외 발생 시 결과값 0;

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import onboarding.problem3.objects.Game;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        try {
+            Game game = new Game(number);
+            return game.start();
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 }
