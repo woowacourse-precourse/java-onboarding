ac96c9892bc29062098f1acf6767cb04ac00f757
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-10-28 18:30:23 +0900

    문제 6번 <1번 기능 구현 완료>

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>(forms.size());
+
+        //입력받은 이메일과 닉네임 중, 닉네임만 추출
+        for (int i = 0; i < forms.size(); i++) {
+            answer.add(forms.get(i).get(1));
+        }
+
         return answer;
     }
 }
