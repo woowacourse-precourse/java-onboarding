5c0cd2d004da469bf145fed33393932690e36979
seokjin <rnrn8678@naver.com>
2022-10-28 11:29:27 +0900

    feat(Problem5.java): 가장 큰 금액부터 낮은 금액까지 거스름돈 거르는 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+	public static final List<Integer> MONEY_LIST = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10);
+
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = new ArrayList<>();
+		for (Integer exchange : MONEY_LIST) {
+			answer.add(money / exchange);
+			money %= exchange;
+		}
+		answer.add(money);
+		return answer;
+	}
 }
