50f83ff640d87423d60a643953bb2ce5a5574565
김태용 <amico741@naver.com>
2022-10-28 22:08:56 +0900

    feat: 청개구리 사전 생성 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static void setValue(HashMap<Character, Character> map){
+        int start = 0;
+        for(int i = 65; i < 91; i++){
+            map.put((char)i, (char)(90 - start));
+            start += 1;
+        }
+        start = 0;
+        for(int i = 97; i < 123; i++){
+            map.put((char)i, (char)(123 - start));
+        }
     }
 }
