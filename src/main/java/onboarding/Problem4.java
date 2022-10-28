1aeb910a1f6c9aeb16cee1d296ec7bec6b3c3e38
Plastic_hong <zakkslash18@naver.com>
2022-10-28 19:42:40 +0900

    problem4 2.청개구리 사전 만들기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,19 @@ import java.util.*;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        Map<Integer, Character> map = new HashMap();
+        List<Integer> greenFrogList = new ArrayList<>();
+
+        for (int i = 90; i >= 65; i--) {
+            greenFrogList.add(i);
+        }
+        for (int i = 0; i < greenFrogList.size(); i++) {
+            int value = greenFrogList.get(i);
+            map.put(i + 65, (char) +value);
+        }
+
+
         return answer;
     }
 }
