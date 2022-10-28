70eb8df5f9491bfb5e0028a0d86234ce6040f0aa
wonseokjee <wsji9404@gmail.com>
2022-10-28 12:44:01 +0900

    221028 prblm3 기능 목록 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
-//      1부터 number만큼 숫자 배열 생성
+//
+
         int answer = 0;
         return answer;
     }
+//  1부터 number만큼 문자로 더한 문자열 생성후 숫자배열로 변환하는 메서드
 
-//  숫자를 받아서 쪼개진 문자배열로 변환하는 메서드
-
-//  문자배열을 받아서 숫자배열로 변환한 다음 3,6,9 개수 세는 메서드
+//  3,6,9 개수 세는 메서드
 }
