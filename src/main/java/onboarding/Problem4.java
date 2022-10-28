b0ce2ad844c1d3592499493b1cf292f20cc524c8
Hojinida <opqrs1103@gmail.com>
2022-10-29 04:20:42 +0900

    feat: 문제 4번 조건에 맞게 변환해주는 converter 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,17 +9,27 @@ public class Problem4 {
     private static final int LOWER=2;
     private static final int NOTTHING=0;
     public static String solution(String word) {
+        StringBuilder answer=new StringBuilder();
         setConvert();
-        return null;
+        for(int i=0;i<word.length();i++){
+            answer.append(converter(word.charAt(i)));
+        }
+        return answer.toString();
     }
-
+    public static char converter(char c){
+        int type=checkLowerUpper(c);
+        if(type==UPPER)
+            c=upperConvert.get(c);
+        if(type==LOWER)
+            c=lowerConvert.get(c);
+        return c;
+    }
     public static void setConvert(){
         for(int i=0;i<=25;i++){
             upperConvert.put((char)(i+65),(char)(90-i));
             lowerConvert.put((char)(i+97),(char)(122-i));
         }
     }
-
     public static int checkLowerUpper(char c){
         if(Character.isUpperCase(c))
             return UPPER;
