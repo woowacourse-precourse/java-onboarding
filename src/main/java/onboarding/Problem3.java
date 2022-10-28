a159ec611d1dd90e1695806fa1014dd685056087
JJongmen <pjy90123@gmail.com>
2022-10-28 17:09:01 +0900

    feat(Problem3): 숫자에 3, 6, 9가 들어가는 횟수를 반환하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,16 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int count369ByNumber(int number) {
+        int count = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit == 3 || digit == 6 || digit == 9) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
 }
