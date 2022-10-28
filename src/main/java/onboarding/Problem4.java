093c07d480a025882df37a042bceb9f3f105e22c
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:15:26 +0900

    feat(문제4): 문자를 사전의 역순으로 변환하는 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,7 +7,30 @@ package onboarding;
  */
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder(word);
+
+        for (int i = 0; i < answer.length(); i++) {
+            answer.setCharAt(i, flipWord(answer.charAt(i)));
+        }
+        return answer.toString();
+    }
+
+    /**
+     * 문자를 사전의 역순으로 변환한다.
+     * @param letter 문자
+     * @return 문자를 사전의 역순으로 변환한 결과
+     */
+    private static char flipWord(char letter) {
+        char result = letter;
+
+        if (Character.isUpperCase(letter)) {
+            result = (char)('A' + 'Z' - letter);
+        }
+
+        if(Character.isLowerCase(letter)) {
+            result = (char)('a' + 'z' - letter);
+        }
+
+        return result;
     }
 }
