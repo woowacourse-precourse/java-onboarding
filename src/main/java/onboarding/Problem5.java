fb1a79449e1e52596a8dd83d4f99ce0ce439be56
Namyunsuk <yunsuk0328@naver.com>
2022-10-28 23:13:52 +0900

    feat(money): PROBLEM5 상수관리 클래스 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+class money{
+    static final int[] moneyList = {50000,10000,5000,1000,500,100,50,10,1};
+    static final String invalidArgument = "InvalidArgument";
+}
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
