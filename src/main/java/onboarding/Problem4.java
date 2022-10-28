013a73fb2d1b382d601b4e9eefc796a84ca46ca8
ykmxxi <ykmxxi97@gmail.com>
2022-10-28 23:37:03 +0900

    feat: (Problem4)문자가 소문자인지 확인하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,8 @@ package onboarding;
 public class Problem4 {
     private static int UPPER_CASE_A = 65;
     private static int UPPER_CASE_Z = 90;
+    private static int LOWER_CASE_A = 97;
+    private static int LOWER_CASE_Z = 122;
 
     public static String solution(String word) {
         String answer = "";
@@ -15,6 +17,13 @@ public class Problem4 {
 
     private static boolean isUpperCase(char character) {
         if (character >= UPPER_CASE_A && character <= UPPER_CASE_Z) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isLowerCase(char character) {
+        if (character >= LOWER_CASE_A && character <= LOWER_CASE_Z) {
             return true;
         }
         return false;
