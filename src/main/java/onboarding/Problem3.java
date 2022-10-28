c8a67c2af2c5515dcbdf71b0c525d893c2d386b6
김태용 <amico741@naver.com>
2022-10-28 21:06:12 +0900

    feat: 문자열에 3,6,9 포함된 개수 세는 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static int getCount(ArrayList<String> array){
+        int cnt = 0;
+        for(String s: array){
+            cnt = cnt + (s.length() - s.replace(String.valueOf('3'), "").length()) + (s.length() - s.replace(String.valueOf('6'), "").length()) + (s.length() - s.replace(String.valueOf('9'), "").length());
+        }
+        return cnt;
     }
 }
