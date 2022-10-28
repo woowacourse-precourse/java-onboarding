9be6ec18ec8c5631b0df577ba85e3969de5ea805
Seiyon Park <seiyonpdev@gmail.com>
2022-10-28 11:52:30 +0900

    feat: ADD method Problem1.isException

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
+
+    public static boolean isException(List<Integer> pobi, List<Integer> crong) {
+
+        if(pobi.get(1) == pobi.get(0) + 1 && crong.get(1) == crong.get(0) + 1)
+            return false;
+        else
+            return true;
+    }
+
 }
