90a202d83b0527667877369a7858e8d43b47254b
viyott-lover <jackk0616@gmail.com>
2022-10-28 22:44:08 +0900

    p7 기능 목록 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    /**
+     * 기능 목록
+     * 1. 크루원들 이름을 구하고 2차원 배열을 위한 인덱스 부여
+     * 2. 2차원 배열을 활용하여 친구관계를 표시
+     * 3. user의 친구의 친구들 중 user를 제외하고 10점씩 부여
+     * 4. visitors가 방문한 수만큼 1점씩 부여
+     * 5. 점수를 역순으로 정렬한 후 앞에서 최대 5명까지 잘라서 출력
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
