a1526b5ba781061c14d044fcb8cc8489575752d2
lecocococo <chemwang13@naver.com>
2022-10-29 00:54:18 +0900

    Feat: Problem6의 모든 crew의 닉네임의 토큰들을 전부 모으고 나온 횟수를 저장하는 기능 및  하위  기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,21 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
 
+	public static List<Set<String>> createAllCrewNicknameTokens(List<List<String>> forms, Map<String, Integer> allCrewNicknameTokens) {
+		String crewNickname = "";
+		Map<String, Integer> tokenizedNickname;
+		List<Set<String>> allCrewNicknameTokenList = new ArrayList<>();
+
+		for (List<String> crew : forms) {
+			crewNickname = crew.get(1);
+			tokenizedNickname = nicknameTokenizer(crewNickname);
+			allCrewNicknameTokenList.add(tokenizedNickname.keySet());
+			allCrewNicknameTokens = mapMerge(allCrewNicknameTokens, tokenizedNickname);
+		}
+
+		return allCrewNicknameTokenList;
+	}
+
 	public static Map<String, Integer> nicknameTokenizer(String crewNickname){
 		Map<String, Integer> nicknameTokens = new HashMap<>();
 		String nicknameToken = "";
@@ -17,8 +32,24 @@ public class Problem6 {
 		return nicknameTokens;
 	}
 
+	public static Map<String, Integer> deleteNotDuplicatedToken(Map<String, Integer> allCrewNicknameTokens) {
+		allCrewNicknameTokens.values().removeAll(Arrays.asList(1));
+		return allCrewNicknameTokens;
+	}
+
+	public static Map<String, Integer> mapMerge(Map<String, Integer> map1, Map<String, Integer> map2) {
+		map2.forEach((key, value) -> map1.merge(key, value, (value1, value2) -> value1 + value2));
+		return map1;
+	}
+
     public static List<String> solution(List<List<String>> forms) {
-		List<String> answer = List.of("answer");
+		// List<String> answer = List.of("answer");
+		List<String> answer = new ArrayList<>();
+		Map<String, Integer> allCrewNicknameTokens = new HashMap<>();
+
+		List<Set<String>> allCrewTokenList = createAllCrewNicknameTokens(forms, allCrewNicknameTokens);
+		allCrewNicknameTokens = deleteNotDuplicatedToken(allCrewNicknameTokens);
+
         return answer;
     }
 }
