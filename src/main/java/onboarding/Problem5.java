36ea6b6209b3050c873fedad1044259eb673df04
SH <gram753@naver.com>
2022-10-29 00:00:45 +0900

    feat: Problem5 기능구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
 	public static List<Integer> solution(int money) {
-		List<Integer> answer = Collections.emptyList();
+		List<Integer> answer = new ArrayList<Integer>();
+		int[] typeOfChanges = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+		convertToChanges(typeOfChanges, answer, money);
+
 		return answer;
 	}
+
+	private static void convertToChanges(int[] typeOfChanges, List<Integer> answer, int money) {
+		int credit = money;
+		for (int i = 0; i < typeOfChanges.length; i++) {
+
+			int count = credit / typeOfChanges[i];
+
+			if (count != 0) {
+				credit -= count * typeOfChanges[i];
+				answer.add(count);
+				continue;
+			}
+			answer.add(0);
+		}
+	}
+
 }
