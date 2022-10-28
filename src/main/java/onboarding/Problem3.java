ba21047563647eb2ae7454626498a2c32b193fa3
hectick <chyeon1223@cau.ac.kr>
2022-10-28 22:38:45 +0900

    feat : [Problem3] 기능 목록 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        //입력값의 맨 뒷자리 수부터 차례대로 3,6,9가 들어가는지 검사
+
+        //3,6,9가 포함된 횟수만큼 answer를 증가
+
         return answer;
     }
 }
