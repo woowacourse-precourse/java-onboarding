81b1d37a44fe1f14e9d7f0e72460ac30dd88a50d
hectick <chyeon1223@cau.ac.kr>
2022-10-29 01:08:40 +0900

    feat : [Problem5] 기능 목록 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,12 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        //제일 큰 금액의 화폐부터 필요한 개수를 차례대로 계산
+        //큰 금액의 화폐를 계산하고 남은 돈으로 그 다음 큰 금액의 화폐가 필요한 개수를 계산하기를 반복
+
+        //한자릿수의 돈이 남은 경우 해당 액수를 일원 동전의 개수로 저장
+
         return answer;
     }
 }
