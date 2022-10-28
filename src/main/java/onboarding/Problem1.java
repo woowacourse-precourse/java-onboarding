2cab205634feb516f9613e7dbc31bb8b4da05ef9
seonghoo1217 <seonghoo1217@naver.com>
2022-10-28 15:57:43 +0900

    feat : Left Page and Right Page must be diffrent 1Page if not throw Exception

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,10 +13,17 @@ class Problem1 {
         }
         return answer;
     }
+    public static void getPageBestScore(List<Integer> list) throws Exception {
+        int left = list.get(0);
+        int right = list.get(1);
 
+        checkPages(left,right);
+    }
 
     public static void checkLength(List<Integer> pobi,List<Integer> crong) throws Exception{
         if (pobi.size()!=2||crong.size()!=2) throw new Exception("페이지 불일치");
     }
-
+    public static void checkPages(int left,int right) throws Exception {
+        if (right-left!=1) throw new Exception ("페이지는 1차이어야 합니다.");
+    }
 }
