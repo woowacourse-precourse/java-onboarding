b950a55e4339806ea23b4259ae0f875531f3a1ea
DESKTOP-A4VVJHE\윤지혜 <dbsquddlfz@naver.com>
2022-10-28 15:45:20 +0900

    문제2완료 , stack을 몰라서 stack활용을 못했음

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,11 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = 0;
 
-        String pobi1 = ""+pobi.get(0);
-        String pobi2 = ""+pobi.get(1);
+        /**
+         * 포비의 기본 값 받기
+         */
+        String pobi1 = "" + pobi.get(0);
+        String pobi2 = "" + pobi.get(1);
 
         int pobiLeft1 = 0;
         int pobiLeft2 = 1;
@@ -24,9 +27,9 @@ class Problem1 {
         /**
          * 포비의 왼쪽 최댓값
          */
-        for (int i=0; i<pobi1.length(); i++) {
-            pobiLeft1 += pobi1.charAt(i)-48;
-            pobiLeft2 *= pobi1.charAt(i)-48;
+        for (int i = 0; i < pobi1.length(); i++) {
+            pobiLeft1 += pobi1.charAt(i) - 48;
+            pobiLeft2 *= pobi1.charAt(i) - 48;
         }
 
         if (pobiLeft1 >= pobiLeft2) {
@@ -40,9 +43,9 @@ class Problem1 {
         /**
          * 포비의 오른쪽 최댓값
          */
-        for (int i=0; i<pobi2.length(); i++) {
-            pobiRight1 += pobi2.charAt(i)-48;
-            pobiRight2 *= pobi2.charAt(i)-48;
+        for (int i = 0; i < pobi2.length(); i++) {
+            pobiRight1 += pobi2.charAt(i) - 48;
+            pobiRight2 *= pobi2.charAt(i) - 48;
         }
 
         if (pobiRight1 >= pobiRight2) {
@@ -64,8 +67,12 @@ class Problem1 {
             pobiRealMax = pobiRightMax;
         }
 
-        String crong1 = ""+crong.get(0);
-        String crong2 = ""+crong.get(1);
+        /**
+         * 크롱의 기본 값 받기
+         */
+
+        String crong1 = "" + crong.get(0);
+        String crong2 = "" + crong.get(1);
 
         int crongLeft1 = 0;
         int crongLeft2 = 1;
@@ -82,9 +89,9 @@ class Problem1 {
         /**
          * 크롱의 왼쪽 최댓값
          */
-        for (int i=0; i<crong1.length(); i++) {
-            crongLeft1 += crong1.charAt(i)-48;
-            crongLeft2 *= crong1.charAt(i)-48;
+        for (int i = 0; i < crong1.length(); i++) {
+            crongLeft1 += crong1.charAt(i) - 48;
+            crongLeft2 *= crong1.charAt(i) - 48;
         }
 
         if (crongLeft1 >= crongLeft2) {
@@ -98,9 +105,9 @@ class Problem1 {
         /**
          * 크롱의 오른쪽 최댓값
          */
-        for (int i=0; i<crong2.length(); i++) {
-            crongRight1 += crong2.charAt(i)-48;
-            crongRight2 *= crong2.charAt(i)-48;
+        for (int i = 0; i < crong2.length(); i++) {
+            crongRight1 += crong2.charAt(i) - 48;
+            crongRight2 *= crong2.charAt(i) - 48;
         }
 
         if (crongRight1 >= crongRight1) {
@@ -147,11 +154,11 @@ class Problem1 {
             answer = -1;
         }
 
-        if (pobi.get(0)+1 != pobi.get(1) || crong.get(0)+1 != crong.get(1)) {
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
             answer = -1;
         }
 
-        if (pobi.get(0) ==1 || pobi.get(0) == 399) {
+        if (pobi.get(0) == 1 || pobi.get(0) == 399) {
             answer = -1;
         }
 
@@ -160,138 +167,5 @@ class Problem1 {
         }
 
         return answer;
-
-        /**int pobiMax1 = 0;
-        int pobiMax1_1 = 1;
-
-        int pobiMax2 = 0;
-        int pobiMax2_2 = 1;
-
-        int pobiLeftMax = 0;
-        int pobiRightMax = 0;
-
-        int pobiRealMax = 0;
-        ////////////////////////////////////////////////////
-        int crongMax1 = 0;
-        int crongMax1_1 = 1;
-
-        int crongMax2 = 0;
-        int crongMax2_2 = 1;
-
-        int crongLeftMax = 0;
-        int crongRightMax = 0;
-
-        int crongRealMax = 0;
-
-
-        String num1 = (int)((Math.random()*400)+1)+"";
-        System.out.println("num1 = " + num1);
-        System.out.println("num1 = " + num1.length());
-
-        ////////////////////////////////////
-        if (pobiLeftMax >= pobiRightMax) {
-            pobiRealMax = pobiLeftMax;
-        }
-
-        if (pobiLeftMax < pobiRightMax) {
-            pobiRealMax = pobiRightMax;
-        }
-
-        for (int i=0; i<num1.length(); i++) {
-            pobiMax1 += num1.charAt(i)-48;
-            pobiMax1_1 *= num1.charAt(i)-48;
-            System.out.println("num1.charAt(i) = " + num1.charAt(i));
-        }
-        System.out.println("pobiMax1 = " + pobiMax1);
-        System.out.println("pobiMax1_1 = " + pobiMax1_1);
-        if (pobiMax1>=pobiMax1_1) {
-            pobiLeftMax = pobiMax1;
-        }
-        if (pobiMax1<pobiMax1_1) {
-            pobiLeftMax = pobiMax1_1;
-        }
-
-        System.out.println("pobiLeftMax = " + pobiLeftMax);
-
-
-        //오른쪽 페이지 구하는 거임 +1해야 된다
-        int num1_1 = Integer.parseInt(num1) +1;
-        String num2 = ""+num1_1;
-        System.out.println("num1_1 = " + num1_1);
-        System.out.println("num2 = " + num2);
-        System.out.println("num2 = " + num2.length());
-        for (int i=0; i<num2.length(); i++) {
-            pobiMax2 += num2.charAt(i)-48;
-            pobiMax2_2 *= num2.charAt(i)-48;
-            System.out.println("num2.charAt(i) = " + num2.charAt(i));
-        }
-        System.out.println("pobiMax2 = " + pobiMax2);
-        System.out.println("pobiMax2_2 = " + pobiMax2_2);
-
-        if (pobiMax2 >= pobiMax2_2) {
-            pobiRightMax = pobiMax2;
-        }
-        if (pobiMax2 < pobiMax2_2) {
-            pobiRightMax = pobiMax2_2;
-        }
-
-        System.out.println("pobiRightMax = " + pobiRightMax);
-
-        if (pobiLeftMax >= pobiRightMax) {
-            pobiRealMax = pobiLeftMax;
-        }
-
-        if (pobiLeftMax < pobiRightMax) {
-            pobiRealMax = pobiRightMax;
-        }
-
-        System.out.println("pobiRealMax = " + pobiRealMax);
-
-        String num3 = (int)((Math.random()*400)+1)+"";
-        for (int i=0; i<num3.length(); i++) {
-            crongMax1 += num3.charAt(i)-48;
-            crongMax1_1 *= num3.charAt(i)-48;
-        }
-        if (crongMax1>=crongMax1_1) {
-            crongLeftMax = crongMax1;
-        }
-        if (crongMax1<crongMax1_1) {
-            crongLeftMax = crongMax1_1;
-        }
-        /////////////////////////
-        int num3_3 = Integer.parseInt(num3) +1;
-        String num4 = ""+num3_3;
-
-        for (int i=0; i<num4.length(); i++) {
-            crongMax2 += num4.charAt(i)-48;
-            crongMax2_2 *= num4.charAt(i)-48;
-        }
-
-        if (crongMax2 >= crongMax2_2) {
-            crongRightMax = crongMax2;
-        }
-        if (crongMax2 < crongMax2_2) {
-            crongRightMax = crongMax2_2;
-        }
-
-        if (crongLeftMax >= crongRightMax) {
-            crongRealMax = crongLeftMax;
-        }
-
-        if (crongLeftMax < crongRightMax) {
-            crongRealMax = crongRightMax;
-        }
-
-        //answer
-        int answer = 0;
-        if (pobiRealMax > crongRealMax) {
-            answer = 1;
-        }
-        if (pobiRealMax == crongRealMax) {
-            answer = 0;
-        }
-        if (pobiRealMax < crongRealMax) {
-            answer = -1;
-        }*/
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        String test = cryptogram;
+        int count = 0;
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            for (int j = 0; j < cryptogram.length(); j++) {
+                if (cryptogram.charAt(j) == cryptogram.charAt(i)) {
+                    count++;
+                }
+            }
+            if (count % 2 != 0) {
+                set.add(""+cryptogram.charAt(i));
+            }
+
+
+            count = 0;
+        }
+
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            answer += it.next();
+        }
+
+
         return answer;
     }
 }
