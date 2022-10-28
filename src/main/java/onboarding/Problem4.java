e606180bef2a76805b3687a389fbd7c49599293f
Lee Min Woo <lmw7414@naver.com>
2022-10-28 22:32:34 +0900

    problem4 - 대소문자 비교 메서드 추가
    대소문자를 비교할 수 있는 메서드를 추가하였다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,7 @@
 package onboarding;
 
-/** 구현해야할 기능
+/**
+ * 구현해야할 기능
  * A에서 Z, a 에서 Z 사이애서 중간 값을 기준으로 결과를 찾는다
  * 대문자 소문자 비교
  * 띄어쓰기는 넘어가기
@@ -9,5 +10,11 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static boolean isUpperCase(char c) {
+        if (c >= 65 && c <= 90)
+            return true;
+        return false;
     }
 }
