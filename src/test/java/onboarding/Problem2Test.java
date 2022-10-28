3d7c329f06d62d27e208f973cc9d0d56426a1d2a
chanminmaxscaler <jcm04@naver.com>
2022-10-28 13:54:40 +0900

    test(Problem2Test): isStringContainingRegularExpression 메소드의 Test 코드를 작성한다
    isStringContainingRegularExpressionText 메소드는 isStringContainingRegularExpression 메소드를 테스트한다.
    문자열과 정규식을 입력하면 정규식 포함 여부를 정상적으로 반환하는지 테스트한다.

---
+++ src/test/java/onboarding/Problem2Test.java
@@ -0,0 +1,39 @@
+package onboarding;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Problem2Test {
+
+    private Problem2 problem2;
+    private String RegularExpressionOfConsecutiveDuplicateCharacters;
+
+    @BeforeEach
+    public void setUp(){
+        problem2 = new Problem2();
+        RegularExpressionOfConsecutiveDuplicateCharacters = "(\\w)\\2";
+    }
+
+    @Test
+    void isStringContainingRegularExpressionTest() {
+        String falseCase1 = "brown";
+        assertThat(problem2.isStringContainingRegularExpression(falseCase1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
+
+        String falseCase2 = "";
+        assertThat(problem2.isStringContainingRegularExpression(falseCase2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
+
+        String falseCase3 = "ab";
+        assertThat(problem2.isStringContainingRegularExpression(falseCase3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
+
+        String trueCase1 = "browoanoommnaon";
+        assertThat(problem2.isStringContainingRegularExpression(trueCase1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
+
+        String trueCase2 = "ababb";
+        assertThat(problem2.isStringContainingRegularExpression(trueCase2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
+
+        String trueCase3 = "  ";
+        assertThat(problem2.isStringContainingRegularExpression(trueCase3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
+    }
+}
