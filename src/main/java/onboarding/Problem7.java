958ec212f25a4e706e7adb79d55d8da9e1e2ced5
antaeho <an981022@naver.com>
2022-10-28 21:41:46 +0900

    Problem7 기능구현 1 완성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 /*
 - 기능 구현사항 목록 -
@@ -11,6 +13,18 @@ import java.util.List;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String,Integer> dic = new HashMap<String,Integer>();
+        HashMap<String,Integer> new_dic = new HashMap<String,Integer>();
+        List std = new ArrayList();
+        for(List relate : friends) {
+            for(int i = 0; i < 2; i++) {
+                dic.put((String) relate.get(i), 0);
+            }
+        }
+        for (String who : visitors) {
+            dic.put(who, 0);
+        }
+        System.out.println(dic);
         return answer;
     }
 }
