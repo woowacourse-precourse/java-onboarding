a769b7186267f76e69e163d30c4827827a142925
Seph-23 <seongmlyu95@gmail.com>
2022-10-28 11:47:08 +0900

    2번 문제 리팩터링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,11 +4,11 @@ public class Problem2 {
     
     static char lastRemoved;
 
-//    public static void main(String[] args) {
-//        String crypto = "browoanoommnaon";
-//
-//        System.out.println("answer = " + solution(crypto));
-//    }
+    public static void main(String[] args) {
+        String crypto = "baaaoat";
+
+        System.out.println("answer = " + solution(crypto));
+    }
 
     public static String solution(String cryptogram) {
         if (cryptogram.length() == 0 || cryptogram.length() == 1)
@@ -17,27 +17,22 @@ public class Problem2 {
         if (cryptogram.charAt(0) == cryptogram.charAt(1)) {
             lastRemoved = cryptogram.charAt(0);
             while (cryptogram.length() > 1 && cryptogram.charAt(0) == cryptogram.charAt(1)) {
-                cryptogram = cryptogram.substring(1, cryptogram.length());
+                cryptogram = cryptogram.substring(1);
             }
-            cryptogram = cryptogram.substring(1, cryptogram.length());
+            cryptogram = cryptogram.substring(1);
             return solution(cryptogram);
         }
 
-        String remStr = solution(cryptogram.substring(1, cryptogram.length()));
+        String remStr = solution(cryptogram.substring(1));
 
         if (remStr.length() != 0 && remStr.charAt(0) == cryptogram.charAt(0)) {
             lastRemoved = cryptogram.charAt(0);
-            return remStr.substring(1, remStr.length());
+            return remStr.substring(1);
         }
 
         if (remStr.length() == 0 && lastRemoved == cryptogram.charAt(0)) {
             return remStr;
         }
         return (cryptogram.charAt(0) + remStr);
-    }
-
-    static String remove(String str) {
-        lastRemoved = '\0';
-        return solution(str);
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    public static void main(String[] args) {
+        int money = 50237;
+        System.out.println("solution(money) = " + solution(money));
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
