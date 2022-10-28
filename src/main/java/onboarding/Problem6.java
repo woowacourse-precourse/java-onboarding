2f7aa30313d3819200411e3d7bb67f72a541d2d0
sosow0212 <sosow0212@naver.com>
2022-10-28 17:15:25 +0900

    feat: Problem6 - 상수와, static 변수 선언

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    static final int FORM_EMAIL = 0;
+    static final int FORM_NICKNAME = 1;
+    static List<String> answer = new ArrayList<>();
+    static List<Set<String>> partsOfNicknameList = new ArrayList<>();
+    static Map<String, Integer> nicknameAndCountMap = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
         return answer;
     }
+
 }
