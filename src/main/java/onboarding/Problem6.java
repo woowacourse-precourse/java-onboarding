f329f44166a090f39afde541aad15a27aaa9db66
Eunkeee <eunkeeeee@gmail.com>
2022-10-28 19:00:53 +0900

    ✨[P6] feat: 각 닉네임 두 글자씩 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        HashSet<Integer> index = new HashSet();
+        List<List<String>> nickSplitArr = new ArrayList<>();
+
+        for(int i=0; i < forms.size();i++){
+            ArrayList<String> nickSplit = new ArrayList();
+            String nickname = forms.get(i).get(1);
+            System.out.println(nickname);
+            for(int k=0; k<nickname.length()-1;k++){
+                nickSplit.add(nickname.substring(k,k+2));
+            }
+            nickSplitArr.add(nickSplit);
+        }
+        System.out.println(nickSplitArr);
         return answer;
     }
 }
