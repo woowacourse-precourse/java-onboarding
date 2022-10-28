f4bd4f3982abcafac52c063078fd23ab88d48177
Gibeom Nam <rlqja0523@naver.com>
2022-10-29 02:45:13 +0900

    feat: enum result code

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+    }
+
+    private enum result {
+        POBI_WIN(1),
+        CRONG_WIN(2),
+        DRAW(0),
+        EXCEPTION(-1);
+
+        private final int code;
+
+        result(int code) {
+            this.code = code;
+        }
+
+        public int getCode() {
+            return code;
+        }
     }
 }
