53cfd3a41e19179003fc0182be6f0d88a50978ba
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-10-28 19:57:40 +0900

    [문제6] - 최종 코드 작성 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
@@ -15,7 +17,39 @@ public class Problem6 {
     }
     
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> dict = new HashMap<>();
+        List<List<String>> wordOfNick = new ArrayList<>();
+
+        for(int i = 0; i < forms.size(); i++){
+            String nick = forms.get(i).get(1);
+            List<String> words = getWords(nick);
+
+            wordOfNick.add(words);
+
+            for (String word : words) {
+                if (dict.containsKey(word)) {
+                    dict.put(word, dict.get(word) + 1);
+                } else {
+                    dict.put(word, 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < forms.size(); i++) {
+            String email = forms.get(i).get(0);
+            List<String> words = wordOfNick.get(i);
+
+            for (String word : words) {
+                if (dict.get(word) > 1) {
+                    answer.add(email);
+                    break;
+                }
+            }
+        }
+        
+        Collections.sort(answer);
+
         return answer;
     }
 }
