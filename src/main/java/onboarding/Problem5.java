e8a57a3074f615567822aff312f5ef8139de8d3a
Hyeonjae-K <href.hjk@gmail.com>
2022-10-28 10:08:00 +0900

    add: 기능 목록 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/* 기능 목록
+ * 변환 가능한 화폐 개수를 반환해 주는 기능
+ * 각 단위의 화폐에 대해 반복하여 업데이트하는 기능
+ * */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
