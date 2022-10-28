16532ddc9daed9624961aba8f2eb04016626c390
seungmin-park <tmddudals369@naver.com>
2022-10-29 02:08:09 +0900

    1주차 과제 3번

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++){
+            int length = (int)Math.log10(i)+1;
+            int temp = i;
+            for (int j = 0; j < length; j++){
+                int a = temp%10;
+                if (a == 3 || a ==6 || a== 9) answer++;
+                temp /=10;
+            }
+        }
         return answer;
     }
 }
