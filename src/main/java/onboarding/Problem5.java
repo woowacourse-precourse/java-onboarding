823f8d27f032672709d431a158af3d2d2775dd33
great-park <cksgh1735@naver.com>
2022-10-29 03:19:19 +0900

    fix(Problem5): type 명시

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,13 +7,11 @@ import java.util.List;
 public class Problem5 {
     private static final Integer[] amount = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-
         return pay(money);
     }
 
     public static List<Integer> pay(int money){
-        List<Integer> answer = new ArrayList<>();
-
+        List<Integer> answer = new ArrayList<Integer>();
         for (int i = 0; i < amount.length; i++){
             int moneyCount = money / amount[i]; // greedy
             money %= amount[i];
