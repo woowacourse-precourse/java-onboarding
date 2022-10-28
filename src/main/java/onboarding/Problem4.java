f2ea4e90a695b8f3dbb3890f89111b5063a0b0a0
jipyo0106 <jg020106@naver.com>
2022-10-28 15:31:42 +0900

    word를 표를 참고하여 치환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    private static final HashMap<Character, Character> map = new HashMap<>();
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        initMap();
+
+        return "";
     }
+
+
+    private static void initMap() {
+        for (int i = 'A', j = 25; i <= 'Z'; i++, j-=2) {
+            // Lower Case
+            map.put((char)(i), (char)(i + j));
+
+            // Upper Case
+            map.put((char)(i + 32), (char)(i + 32 + j));
+        }
+    }
+
+
 }
