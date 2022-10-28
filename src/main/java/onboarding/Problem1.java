4165c291262395908e87979d2c0d70f9e760d9fe
wonseokjee <wsji9404@gmail.com>
2022-10-28 11:50:47 +0900

    221027 prblm1 - 1 예외사항 및 점수가 가장 높은사람 정하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -28,29 +28,4 @@ class Problem1 {
         return answer;
     }
 
-//    2. Solution에서 한 사람의 페이지 번호를 받는 메서드
-    public static int someone(List<Integer> bookNum) {
-//      페이지List 받아서 String으로 쪼갬
-        String[] leftpage = Integer.toString(bookNum.get(0)).split("");
-        String[] rightpage = Integer.toString(bookNum.get(1)).split("");
-
-//        int bookMax = Math.max(findMax(leftpage), findMax(rightpage));
-
-        return Math.max(findMax(leftpage), findMax(rightpage));
-    }
-
-//    3. 2번에서 페이지 한쪽을 String[]으로 받아서 더하기 곱하기 비교.
-//    4. 가장 큰 수를 본인의 점수로 한다.
-    public static int findMax(String[] page) {
-        int plus = 0;
-        int multiply = 1;
-        for (String i: page) {
-            plus += Integer.parseInt(i);
-            multiply = Integer.parseInt(i) * multiply;
-        }
-//        int Fin = Math.max(plus,multiply);
-
-        return Math.max(plus,multiply);
-    }
-
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.*;
 
 public class Problem2 {
+    public static String solution(String cryptogram) {
+//      cryptogram 받아서 단일 문자 list에 넣기
+        ArrayList<String> lst = new ArrayList<String>();
+
+        for (String s : cryptogram.split("")) {
+            lst.add(s);
+        }
+        String answer = sameLang(lst);;
+        return answer;
+    }
+
     //   쪼갠 문자 중복되는지 확인
     public static String sameLang(ArrayList<String> lst) {
         ArrayList<String> lstIns = new ArrayList<String>();
