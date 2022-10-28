b384b45df5a1ebf00be4a7bcfc1aa31996e2b677
hoonn9 <hun9935@gmail.com>
2022-10-28 15:09:59 +0900

    Add sum, multiply of eachNum method (Problem1) Update toDigitList to toDigitStream

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,11 +9,24 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        pobi.stream().forEach((page) -> {
+            System.out.println(sumOfEachNum(page));
+            System.out.println(multiplyOfEachNum(page));
+        });
+
         return answer;
     }
 
-    private static List<Integer> toDigitList(Integer num) {
+    private static Integer sumOfEachNum(Integer num) {
+        return toDigitStream(num).reduce(0, Integer::sum);
+    }
+
+    private static Integer multiplyOfEachNum(Integer num) {
+        return toDigitStream(num).reduce(1, (prev, curr) -> prev * curr);
+    }
+
+    private static Stream<Integer> toDigitStream(Integer num) {
         return Arrays.stream(num.toString()
-                .split("")).map(Integer::parseInt).collect(Collectors.toList());
+                .split("")).map(Integer::parseInt);
     }
 }
