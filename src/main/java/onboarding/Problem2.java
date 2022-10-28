d25b47e582e8b214e4edefa0a613edf8f19cde22
opuopuo <pureuml.dev@gmail.com>
2022-10-28 20:12:47 +0900

    feat: 예외사항 처리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,41 @@
 package onboarding;
 
 public class Problem2 {
+        private static final int MIN_LENGTH = 1;
+        private static final int MAX_LENGTH = 1000;
     public static String solution(String cryptogram) {
+        String errorMessage = null;
+
+        if ((errorMessage = verifyException(cryptogram)) != null) {
+            return errorMessage;
+        }
         String answer = "answer";
         return answer;
+    }
+
+    private static String verifyException(String cryptogram) {
+        String result = null;
+
+        /* [TODO] 에러 메세지 enum으로 ..? 어떻게 변경할지 고려해보자 */
+        if (!isLowerCase(cryptogram))
+            result = "Error: Must be lowercase";
+        if (result == null && !checkLength(cryptogram)) {
+            result = "Error: Length 1 to 1000";
+        } else if (!checkLength(cryptogram))
+            result = "Error: Length 1 to 1000 and Must be lowercase";
+        return result;
+    }
+
+    private static boolean isLowerCase(String cryptogram) {
+        for (char c : cryptogram.toCharArray()) {
+            if (!Character.isLowerCase(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkLength(String cryptogram) {
+        return (cryptogram.length() >= MIN_LENGTH && cryptogram.length() <= MAX_LENGTH);
     }
 }
