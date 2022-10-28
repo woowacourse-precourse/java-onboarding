2165cd41afcd58088e24a20ed565e8b8880fc3fe
KNH <nh2004@naver.com>
2022-10-29 02:09:12 +0900

    add : Problem3 Solution
    문제3 풀이

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem3 {
+    public static int count(int number){
+        int claps = 0;
+        while (number > 0){
+            if (number % 10 == 3 || number %10 == 6 || number %10 == 9)
+                claps+= 1;
+            number /= 10;
+        }
+        return claps;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int sum = 0;
+
+        for(int i = 1; i <= number; i++){
+            sum += count(i);
+        }
+
+        return sum;
     }
 }
