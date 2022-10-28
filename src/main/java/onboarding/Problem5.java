49326cb6d6d90d403da6f0819fe4d62464d6d5f7
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:16:00 +0900

    docs(문제5): 기능목록 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 📚 기능 목록
+ *
+ * 1. 주어진 수를 각 단위별로 구분하는 기능
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
