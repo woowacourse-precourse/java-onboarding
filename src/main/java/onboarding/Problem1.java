c26375864381e47f48d1bb59c8416f4fb56701f6
parkjungyoon <tbr06057@naver.com>
2022-10-29 00:31:29 +0900

    refactor(PROBLEM1): validation(오류처리)코드 불필요한 if문 삭제

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,10 +10,10 @@ class Problem1 {
     private final static int INPUT_SIZE = 2;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (!validateLength(pobi) || !validateRange(pobi) || !validateSequence(pobi)) {
+        if (validateLength(pobi) || validateRange(pobi) || validateSequence(pobi) || validateOddOrEven(pobi)) {
             return exception;
         }
-        if (!validateLength(crong) || !validateRange(crong) || !validateSequence(crong)) {
+        if (validateLength(crong) || validateRange(crong) || validateSequence(crong) || validateOddOrEven(crong)) {
             return exception;
         }
 
@@ -62,27 +62,24 @@ class Problem1 {
     }
 
     private static boolean validateLength(List<Integer> input) {
-        if (input.size() != INPUT_SIZE) {
-            return false;
-        }
-        return true;
+        return input.size() != INPUT_SIZE;
     }
 
     private static boolean validateRange(List<Integer> input) {
-        if (input.get(0) < 1 && input.get(1) > 400) {
-            return false;
-        }
-        return true;
+        return input.get(0) < 1 || input.get(1) > 400;
     }
 
     private static boolean validateSequence(List<Integer> input) {
+        return input.get(1) - input.get(0) != 1;
+    }
+
+    private static boolean validateOddOrEven(List<Integer> input) {
         if (input.get(0) % 2 != 1) {
-            return false;
-        } else if (input.get(1) % 2 != 0) {
-            return false;
-        } else if (input.get(1) - input.get(0) != 1) {
-            return false;
+            return true;
         }
-        return true;
+        if (input.get(1) % 2 != 0) {
+            return true;
+        }
+        return false;
     }
 }
