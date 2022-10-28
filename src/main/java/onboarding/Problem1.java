5e6704d83135122204d55d1408e4718f24f42ffb
HyowonSin <tlsgydnjs@naver.com>
2022-10-28 17:16:18 +0900

    feat: Add problem1
    - 문제 1번입니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,51 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiScore = max(pobi);
+        int crongScore = max(crong);
+
+        if (pobiScore == crongScore) {
+            return 0;
+        } else if (compare(pobiScore, crongScore) == pobiScore) {
+            return 1;
+        } else if (compare(pobiScore, crongScore) == crongScore) {
+            return 2;
+        } else {
+            return -1;
+        }
+    }
+
+    // 각 자리수 더하기
+    public static int sum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    // 각 자리수 곱하기
+    public static int multiply(int n) {
+        int multiply = 1;
+        while (n > 0) {
+            multiply *= n % 10;
+            n /= 10;
+        }
+        return multiply;
+    }
+
+    // 큰 수 비교
+    public static int compare(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    // 큰 수 구하기
+    public static int max(List<Integer> numbers) {
+        return compare(compare(sum(numbers.get(0)), multiply(numbers.get(0))), compare(sum(numbers.get(1)), multiply(numbers.get(1))));
     }
 }
