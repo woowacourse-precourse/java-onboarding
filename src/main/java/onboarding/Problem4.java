08c0272a63aaed624c68dddbb46eb1cbeb294646
JunDonghyuk <ehdgur4814@naver.com>
2022-10-28 21:35:32 +0900

    Solve Problem 4
    ASCII를 이용한 풀이

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            answer += String.valueOf(changeLetter(letter));
+        }
         return answer;
     }
+
+    public static char changeLetter(char letter) {
+        if(letter >= 'a' && letter <= 'z') {
+            return (char) ('z' - letter + 'a');
+        } else if (letter >= 'A' && letter <= 'Z') {
+            return (char) ('Z' - letter + 'A');
+        } else {
+            return letter;
+        }
+    }
+
 }
