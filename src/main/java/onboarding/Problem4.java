83c297803a7a71674de83a0a7823ca8a5b24913a
splitube <voicesplit@daum.net>
2022-10-28 16:37:03 +0900

    4번 문제 - 기능 목록 완성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,18 @@
 package onboarding;
 
+/**
+ * Frog
+ *
+ * 1. listen(String word) = 청개구리가 문자열을 듣는다.
+ * 2. speak() = 들은 단어를 모두 반대로 바꾸어 반환한다.
+ * 3. change_string(String word) = 현재 문자열을 반대로 바꾼다.
+ * 4. change_character(char c) = c를 알맞게 바꾸어 반환
+ * 5. is_uppercase(char c) = 현재 문자가 대문자인지 반환
+ * 6. is_lowercase(char c) = 현재 문자가 소문자인지 반환
+ * 7. change_uppercase(char c) = 대문자 문자를 반대로 바꿔서 반환
+ * 8. change_lowercase(char c) = 소문자 문자를 반대로 바꿔서 반환
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
