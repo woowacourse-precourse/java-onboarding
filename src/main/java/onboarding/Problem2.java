b4f1ba0d400dcfcaba998b24659f98397b704971
ECHO <eunchan1001@gmail.com>
2022-10-28 17:45:39 +0900

    feat(Problem2): 문자열이 0인 경우와 대칭인 경우 빈 문자열을 반환하는 로직 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,5 +4,13 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+            // 문자열의 길이가 0일 경우 바로 종료
+            if (answer.length() == 0) {
+                return answer;
+            }
+            // 문자열이 대칭일 경우 빈 문자열 반환
+            if (isReversible(answer)) {
+                return "";
+            }
     }
 }
