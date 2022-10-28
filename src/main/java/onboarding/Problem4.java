cb3efd8cfb128e1745739fa2006e6ccf0178718d
situodys <situodys@gmail.com>
2022-10-28 14:17:21 +0900

    feat(문제4): 변환된 문자열 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,10 @@
 package onboarding;
 
+import onboarding.problem4.AlphabetConverter;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        AlphabetConverter alphabetConverter = new AlphabetConverter(word);
+        return alphabetConverter.convert();
     }
 }
