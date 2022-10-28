60f20ca188c2719e224a9c8532a300692f53b2ed
kokodak <kokodakadokok@gmail.com>
2022-10-28 11:32:07 +0900

    feat: [문제6] 크루들의 모든 닉네임들을 두 글자씩 잘라 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<List<String>> makeSubstringWithTwoWords(List<List<String>> forms) {
+        List<List<String>> substringList = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> substrings = new ArrayList<>();
+            String Nickname = forms.get(i).get(1);
+            for (int j = 0; j < Nickname.length() - 1; j++) {
+                String substring = Nickname.substring(j, j + 2);
+                substrings.add(substring);
+            }
+            substringList.add(substrings);
+        }
+
+        return substringList;
     }
 }
