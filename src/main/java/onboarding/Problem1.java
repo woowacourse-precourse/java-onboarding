9f592eba5880908cd25620a6cc0618db3c586e59
parkjungyoon <tbr06057@naver.com>
2022-10-28 23:41:41 +0900

    feat(PROBLEM1): 예외처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,45 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private final static int exception = -1;
+    private final static int SAME_SCORE = 0;
+    private final static int POBI_WIN = 1;
+    private final static int CRONG_WIN = 2;
+    private final static int INPUT_SIZE = 2;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!validateLength(pobi) || !validateRange(pobi) || !validateSequence(pobi)) {
+            return exception;
+        }
+        if (!validateLength(crong) || !validateRange(crong) || !validateSequence(crong)) {
+            return exception;
+        }
         return answer;
+    }
+
+    private static boolean validateLength(List<Integer> input) {
+        if (input.size() != INPUT_SIZE) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean validateRange(List<Integer> input) {
+        if (input.get(0) < 1 && input.get(1) > 400) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean validateSequence(List<Integer> input) {
+        if (input.get(0) % 2 != 1) {
+            return false;
+        } else if (input.get(1) % 2 != 0) {
+            return false;
+        } else if (input.get(1) - input.get(0) != 1) {
+            return false;
+        }
+        return true;
     }
 }
