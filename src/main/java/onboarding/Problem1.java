b56d1f692071d1d84c85c31d0796cc40473ac867
Soomin Lim <augustth511@gmail.com>
2022-10-28 16:23:22 +0900

    기능 추가: 각 자리 숫자를 모두 더하거나 모두 곱해 가장 큰 수를 구하는 findMaxNum 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,22 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiMax = Math.max(findMaxNum(pobi.get(0)), findMaxNum(pobi.get(1)));
+        int crongMax = Math.max(findMaxNum(crong.get(0)), findMaxNum(crong.get(1)));
+
         return answer;
+    }
+
+    public static int findMaxNum(int n) {
+        int sum = 0, mul = 1;
+
+        while (n > 0) {
+            sum += n % 10;
+            mul *= n % 10;
+            n /= 10;
+        }
+
+        return Math.max(sum, mul);
     }
 }
