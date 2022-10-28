8f51a06992efa4e831263dad7981619ca59a9678
5uhwann <5uhwann27@gmail.com>
2022-10-28 14:39:06 +0900

    feat: PROBLEM3 - 기능2 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+  private static final int MAX_NUMBER = 10000;
+
+  public static int solution(int number) {
+    int answer = 0;
+    return answer;
+  }
+
+  public static boolean isAllowedNumber(int number) throws IllegalStateException {
+    if (number < 1 || number > MAX_NUMBER) {
+      throw  new IllegalStateException("1 ~ 10000 사이의 숫자가 아닙니다.");
     }
+    return true;
+  }
+
 }
