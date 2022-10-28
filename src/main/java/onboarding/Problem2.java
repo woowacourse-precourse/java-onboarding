373f7d329bd56a0c4663589f70bf88fccf1d22b9
kimminsu <xjfcnfw3@naver.com>
2022-10-28 20:48:52 +0900

    feat(문제2): [예외 사항추가] 문자열이 소문자로 구성 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,11 +1,17 @@
 package onboarding;
 
 import onboarding.problem2.Decryption;
+import onboarding.problem2.Validation;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        answer = Decryption.decrypt(cryptogram);
+        try {
+            Validation.validate(cryptogram);
+            answer = Decryption.decrypt(cryptogram);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/problem2/Validation.java
+++ src/main/java/onboarding/problem2/Validation.java
@@ -1,9 +1,24 @@
 package onboarding.problem2;
 
+import java.util.regex.Pattern;
+
 public class Validation {
     private static final int MAX_LENGTH = 1000;
     private static final int MIN_LENGTH = 1;
+    private static final String LOWERCASE_STR_REGEXP = "^[a-z]+$";
     private static final String LENGTH_EXCEPTION_MESSAGE = "문자열의 길이가 범위 밖에 있습니다.";
+    private static final String LOWERCASE_EXCEPTION_MESSAGE = "문자열이 소문자로만 구성되어 있어야 합니다.";
+
+    public static void validate(String cryptogram) {
+        validateIsNotLowerCaseStr(cryptogram);
+        validateNotInRange(cryptogram);
+    }
+
+    private static void validateIsNotLowerCaseStr(String cryptogram) {
+        if(!Pattern.matches(LOWERCASE_STR_REGEXP, cryptogram)) {
+            throw new IllegalArgumentException(LOWERCASE_EXCEPTION_MESSAGE);
+        }
+    }
 
     private static void validateNotInRange(String cryptogram) {
         if ((cryptogram.length() < MIN_LENGTH) || (cryptogram.length() > MAX_LENGTH)) {
