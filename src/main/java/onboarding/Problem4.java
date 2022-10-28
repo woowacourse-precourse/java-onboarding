f5c43b4846f10200dce883ace8b6bc0b6f97f046
ParkRootSeok <g_e_un@naver.com>
2022-10-28 14:04:36 +0900

    feat : Problem4 - 알파벳 확인 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,14 +4,21 @@ public class Problem4 {
 
     static public boolean isAlphabet(char alphabet) {
 
+        if ('a' <= alphabet && alphabet <= 'z' || 'A' <= alphabet && alphabet <= 'Z')
+            return true;
+
+        return false;
+
     }
 
-    static public boolean reverseAlphabet(StringBuilder word) {
-
+    static public char reverseAlphabet(StringBuilder word) {
+        char result = ' ';
+        return result;
     }
 
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
 }
