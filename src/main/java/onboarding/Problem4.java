275a33bc45f265bf928142b1e93eab9f1fd6ddda
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 16:26:04 +0900

    [Problem4] 문자 대문자, 소문자 판단

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,6 +8,21 @@ public class Problem4 {
      * 2. 청개구리 사전
      */
 
+    /**
+     * 1. 문자 대문자, 소문자 판단
+     * @param a : 비교 문자
+     * @return : 대문자 = 1, 소문자 = 2, 그 외의 문자 = -1
+     */
+
+    static int checkCapital(char a){
+        // 대문자
+        if (a >= 65 && a <= 90) return 1;
+        // 소문자
+        if (a >= 97 && a <= 122) return 2;
+        // 그 외의 문자
+        return -1;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
