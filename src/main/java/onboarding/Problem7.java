afae1c19423d5ae9e61a044f250ca1aea79f27a5
jaehee329 <jaehee329@yonsei.ac.kr>
2022-10-28 18:09:36 +0900

    feat(Problem7): 점수가 높은 순으로, 같은 점수라면 이름을 사전순으로 가지는 우선순위 큐 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Problem7 {
     public static class Score {
@@ -23,6 +24,12 @@ public class Problem7 {
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        PriorityQueue<Score> scorePriorityQueue = getScorePriorityQueue();
         return answer;
+    }
+
+    public static PriorityQueue<Score> getScorePriorityQueue() {
+        return new PriorityQueue<>((a, b) -> (a.score == b.score) ?
+                                            a.username.compareTo(b.username) : b.score-a.score);
     }
 }
--- src/test/java/onboarding/Problem7Test.java
+++ src/test/java/onboarding/Problem7Test.java
@@ -3,7 +3,10 @@ package onboarding;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.PriorityQueue;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +27,28 @@ class Problem7Test {
 
 		user1.addScore1();
 		assertThat(user1.score).isEqualTo(21);
+	}
+
+	@Test
+	@DisplayName("점수가 높은 순, 같은 점수라면 이름을 오름차순으로 가져야 한다.")
+	void getScorePriorityQueueTest() {
+		PriorityQueue<Problem7.Score> scorePriorityQueue = Problem7.getScorePriorityQueue();
+		Problem7.Score user1 = new Problem7.Score("abe", 100);
+		Problem7.Score user2 = new Problem7.Score("abc", 100);
+		Problem7.Score user3 = new Problem7.Score("abd", 100);
+		Problem7.Score user4 = new Problem7.Score("efg", 101);
+		Problem7.Score user5 = new Problem7.Score("aaa", 99);
+
+		scorePriorityQueue.add(user1);
+		scorePriorityQueue.add(user2);
+		scorePriorityQueue.add(user3);
+		scorePriorityQueue.add(user4);
+		scorePriorityQueue.add(user5);
+
+		assertThat(scorePriorityQueue.poll()).isSameAs(user4);
+		assertThat(scorePriorityQueue.poll()).isSameAs(user2);
+		assertThat(scorePriorityQueue.poll()).isSameAs(user3);
+		assertThat(scorePriorityQueue.poll()).isSameAs(user1);
+		assertThat(scorePriorityQueue.poll()).isSameAs(user5);
 	}
 }
