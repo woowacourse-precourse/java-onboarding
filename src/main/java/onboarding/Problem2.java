ce455a2a984605f7f2db5cb50b43ac55fa574665
Seph-23 <seongmlyu95@gmail.com>
2022-10-28 10:10:10 +0900

    Problem 3 solved - 369 박수 세기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -16,26 +16,23 @@ public class Problem2 {
 
         if (cryptogram.charAt(0) == cryptogram.charAt(1)) {
             lastRemoved = cryptogram.charAt(0);
-            while (cryptogram.length() > 1
-              && cryptogram.charAt(0) == cryptogram.charAt(1))
+            while (cryptogram.length() > 1 && cryptogram.charAt(0) == cryptogram.charAt(1)) {
                 cryptogram = cryptogram.substring(1, cryptogram.length());
+            }
             cryptogram = cryptogram.substring(1, cryptogram.length());
             return solution(cryptogram);
         }
 
         String remStr = solution(cryptogram.substring(1, cryptogram.length()));
 
-        if (remStr.length() != 0
-          && remStr.charAt(0) == cryptogram.charAt(0)) {
+        if (remStr.length() != 0 && remStr.charAt(0) == cryptogram.charAt(0)) {
             lastRemoved = cryptogram.charAt(0);
-
             return remStr.substring(1, remStr.length());
         }
 
-        if (remStr.length() == 0
-          && lastRemoved == cryptogram.charAt(0))
+        if (remStr.length() == 0 && lastRemoved == cryptogram.charAt(0)) {
             return remStr;
-
+        }
         return (cryptogram.charAt(0) + remStr);
     }
 
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static void main(String[] args) {
+        int number = 33;
+        System.out.println("answer = " + solution(number));
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int current = i;
+            while (current > 0) {
+                if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
+                    answer++;
+                }
+                current /= 10;
+            }
+        }
+
         return answer;
     }
 }
