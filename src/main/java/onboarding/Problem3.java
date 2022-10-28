b3170406e1d9cb0e657375c12484a3aab7f7f96d
Soomin Lim <augustth511@gmail.com>
2022-10-28 21:57:26 +0900

    기능 추가: 숫자에 들어가는 3, 6, 9의 개수를 카운트하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += count(i);
+        }
+
         return answer;
+    }
+
+    public static int count(int n) {
+        int cnt = 0;
+        int num = n;
+
+        while (n > 0) {
+            int tmp = n % 10;
+            if (tmp == 3 || tmp == 6 || tmp == 9)
+                cnt++;
+            n /= 10;
+        }
+
+        return cnt;
     }
 }
