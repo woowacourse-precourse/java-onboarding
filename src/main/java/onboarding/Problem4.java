1bae599873590cac5791103c9b12e4c6ae3b420d
lzlkolo <lzlko1209@gmail.com>
2022-10-28 14:29:35 +0900

    PROBLEM4.문자열 정돈

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,6 +10,12 @@ public class Problem4 {
 
     public static String solution(String word) {
         String answer="";
+
+        if(isCheck(word)) {
+            answer = "문자열의 길이가 잘못되었습니다.";
+        }
+
+        word = word.replaceAll("[^a-zA-Z]", " ");
         return answer;
     }
 }
