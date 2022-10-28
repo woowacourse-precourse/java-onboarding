35d541ce09d607e20007bf4bdabf03e0795471f2
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-10-29 00:52:30 +0900

    feature : Ploblem4 구현 및 유닛 테스트 코드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import onboarding.problem4.LetterReverser;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for (char letter : word.toCharArray()) {
+            char changedLetter = LetterReverser.reverseOnlyEnglish(letter);
+            sb.append(changedLetter);
+        }
+        return sb.toString();
     }
 }
