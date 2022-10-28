d96ad719b10f3522eaf49be5754cfa6f03020964
Eunkeee <eunkeeeee@gmail.com>
2022-10-28 20:42:40 +0900

    ✨[P7] feat: user의 friend 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // user의 friend 구하기
+        ArrayList<String> userFriends = new ArrayList<String>();
+        for(List<String> friendList : friends){
+            if(friendList.contains(user)){
+                // add friend
+                for(String friendName: friendList){
+                    if(friendName != user){
+                        userFriends.add(friendName);
+                    }
+                }
+            }
+        }
+        System.out.println(userFriends);
+
         return answer;
     }
 }
