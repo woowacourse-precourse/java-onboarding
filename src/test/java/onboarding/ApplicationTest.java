6af71c040c4a28e8ae41c66910be8cabf83591b4
yj2980 <gum0945@naver.com>
2022-10-28 23:18:45 +0900

    test(program3) : 테스트케이스 수정

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -68,13 +68,6 @@ class ApplicationTest {
             int result = 14;
             assertThat(Problem3.solution(number)).isEqualTo(result);
         }
-
-        @Test
-        void case3() {
-            int number = 333;
-            int result = 14;
-            assertThat(Problem3.solution(number)).isEqualTo(result);
-        }
     }
 
     @Nested
