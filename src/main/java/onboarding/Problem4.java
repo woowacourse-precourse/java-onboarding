1f3d5bc60343a858df60c670391e36d1f7cf7930
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-10-28 19:15:46 +0900

    [feat] Problem4: 단어 변경 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,40 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int ASCII_UPPER_A = 65;
+    private static final int ASCII_LOWER_A = 97;
+    private static final int ASCII_UPPER_Z = 90;
+    private static final int ASCII_LOWER_Z = 122;
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for(char c : word.toCharArray()){
+            sb.append(convertWord(c));
+        }
+        return sb.toString();
+    }
+    private static char convertWord(char word) {
+        if(isLowerChar(word)){
+            return convertLowerChar(word);
+        }
+        if(isUpperChar(word)){
+            return convertUpperChar(word);
+        }
+
+        return word;
+    }
+    private static boolean isUpperChar(char word) {
+        return word > ASCII_UPPER_A && word < ASCII_UPPER_Z;
+    }
+
+    private static boolean isLowerChar(char word) {
+        return word > ASCII_LOWER_A && word < ASCII_LOWER_Z;
+    }
+
+    private static char convertLowerChar(char word) {
+        return (char) (ASCII_LOWER_Z - (word - ASCII_LOWER_A));
+    }
+
+    private static char convertUpperChar(char word) {
+        return (char) (ASCII_UPPER_Z - (word - ASCII_UPPER_A));
     }
 }
