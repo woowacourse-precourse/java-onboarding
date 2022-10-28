1960b612a1595939667e98954da1ecd5a4cf8bf7
KTH-96 <taehyun960@gmail.com>
2022-10-28 23:28:49 +0900

    comment(Problem4): 기능구현 목록 정리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+
+	/**
+	 * 기능 목록 작성
+	 * 1. 주어진 조건으로 문자 변환
+	 */
+	public static String solution(String word) {
+		String answer = "";
+		return answer;
+	}
+
 }
