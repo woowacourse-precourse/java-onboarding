4daddc2a621bc0f104edee3b5639d7b647382ab8
math0917 <math0917@naver.com>
2022-10-29 01:55:12 +0900

    test : Input validation 테스트코드 작성

---
+++ src/main/java/onboarding/problem3/Problem3Validation.java
@@ -0,0 +1,7 @@
+package onboarding.problem3;
+
+public class Problem3Validation {
+    public static boolean problem3InputValidation(int userInput, int minInput, int maxInput) {
+        return true;
+    }
+}
