32b52f8bfe83cd8735459a179e507465d4e08bff
win9 <krrr8@naver.com>
2022-10-28 22:31:56 +0900

    [기능3번] 범위체크 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        if (!isValid(number)){
+            return Integer.MAX_VALUE;
+        }
+
         return answer;
     }
+
+    /**
+     * 주어진 범위를 체크하는 메소드
+     * @param number
+     * @return
+     */
+    public static boolean isValid(int number){
+        return number >= 1 && number <= 10000;
+    }
+
+
 }
