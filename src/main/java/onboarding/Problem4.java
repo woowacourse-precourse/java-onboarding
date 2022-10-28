ce9b9386ac937fa244ee907d39c743e05e64914b
wwwndus <simock22@gmail.com>
2022-10-28 18:13:48 +0900

    feat: 문자열이 알파벳으로 이루어졌는지 확인하고 문자열을 배열로 저장, 반대 알파벳으로 변환된 문자열을 담은 배열 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Problem4 {
     public static String solution(String word) {
@@ -16,6 +17,15 @@ public class Problem4 {
 
             map.put(String.valueOf(key), String.valueOf(value));
         }
+
+        String copyWord = word;
+
+        if (Pattern.matches("^[a-zA-Z]*$", copyWord.replaceAll(" ", ""))) {
+            String[] wordArr = word.split("");
+            String[] answerArr = new String[wordArr.length];
+
+        }
+
 
         return answer;
     }
