93b8fb196a5d944d74228ee99be5dfd14ce4265a
backFox <jorealbusiness@gmail.com>
2022-10-29 00:16:32 +0900

    refactor(problem1): 페이지 번호 게임을 실행하는 핵심 비즈니스 로직을 별도의 서비스 레이어로 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,30 +1,11 @@
 package onboarding;
 
-import onboarding.problem1.service.ScoreService;
-import onboarding.problem1.vo.Score;
+import onboarding.problem1.service.PageGameService;
 
 import java.util.List;
 
 class Problem1 {
-    public static final int EXCEPTION_RETURN_VALUE = -1;
-    public static final int POBI_WINNER_RETURN_VALUE = 1;
-    public static final int CRONG_WINNER_RETURN_VALUE = 2;
-    public static final int TIE = 0;
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        try {
-            Score pobiScore = ScoreService.getHighestScore(pobi);
-            Score crongScore = ScoreService.getHighestScore(crong);
-
-            if (pobiScore.isHigherThan(crongScore)) {
-                return POBI_WINNER_RETURN_VALUE;
-            } else if (pobiScore.isLowerThan(crongScore)) {
-                return CRONG_WINNER_RETURN_VALUE;
-            }
-
-            return TIE;
-        } catch (IllegalArgumentException exception) {
-            return EXCEPTION_RETURN_VALUE;
-        }
+        return PageGameService.playGame(pobi, crong);
     }
 }
