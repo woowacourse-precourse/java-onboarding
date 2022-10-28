c8f9d29c7b4bcea7aa6e15a61f97b79e4131a3d8
xxeol2 <jhj01177@naver.com>
2022-10-28 12:52:02 +0900

    feat(prob1): 문자열에 연속된 문자 존재 여부 판별 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+		return answer;
+	}
+
+	public boolean checkContinuity(String cryptogram) {
+		char previousC = ' ';
+		for (char c : cryptogram.toCharArray()) {
+			if (previousC == c) {
+				return true;
+			}
+			previousC = c;
+		}
+		return false;
+	}
 }
