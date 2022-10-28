425034b9f3b116c0ee2710ff657fef957e75e366
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-28 09:27:32 +0900

    solution, complete

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -30,7 +30,7 @@ public class Problem2 {
     }
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = deleteCryptogram(cryptogram);
         return answer;
     }
 }
