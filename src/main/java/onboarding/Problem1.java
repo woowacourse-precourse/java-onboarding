076a76e118fd6f3f8fc326d8704b352a49436923
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 00:13:45 +0900

    feat : add getMaxPageValue function
    자리수의 합과 곱 중 큰 값을 return하는 함수 제작

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,33 +11,49 @@ import java.util.List;
 /**
  * 우아한 테크코스 - 프리코스 1주차 페이지 번호 게임
  *
- * @version         1.00 2022년 10월 27일
- * @author          이준현
+ * @author 이준현
+ * @version 1.00 2022년 10월 27일
  */
 class Problem1 {
 
-    public static boolean validCheck(List<Integer> list){
-        if(list.size() != 2){
+    public static boolean validCheck(List<Integer> list) {
+        if (list.size() != 2) {
             return false;
         }
-        for(int el : list){
-            if(el <= 1 || 400 <= el){
+        for (int el : list) {
+            if (el <= 1 || 400 <= el) {
                 return false;
             }
         }
-        if(list.get(0) % 2 != 1 && list.get(1) % 2 != 0){
+        if (list.get(0) % 2 != 1 && list.get(1) % 2 != 0) {
             return false;
         }
-
-        if(list.get(1) - list.get(0) != 1){
+        if (list.get(1) - list.get(0) != 1) {
             return false;
         }
-
         return true;
-    };
+    }
+
+
+    public static int getMaxPageValue(int pageNum) {
+        int sum = 0;
+        int multi = 0;
+        while (pageNum != 0) {
+            sum += pageNum % 10;
+            if (multi == 0) {
+                multi = pageNum % 10;
+            } else {
+                multi *= pageNum % 10;
+            }
+            pageNum /= 10;
+        }
+        return Math.max(sum, multi);
+    }
+
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 }
