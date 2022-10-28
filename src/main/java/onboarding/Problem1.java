996e0d08f30583587763fcda2de8af7b9c62f401
beombu <sunil1369@bible.ac.kr>
2022-10-28 19:53:18 +0900

    Feat(P1) : 기능목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,21 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /**
+     * 각 자리수를 더하거나 곱한 수 중 큰값 추출
+     */
+    private int getMaxNum(int leftPage, int rightPage){
+        return 0;
+    }
+
+    /**
+     * 포비와 크롱의 Max값을 비교해 결과 반환
+     *
+     * @return 무승부 : 0, 포비 승 : 1, 크롱 승 : 2
+     */
+    private int compareNum(int pobiNum, int crongNum){
+        return 0;
+    }
+
 }
