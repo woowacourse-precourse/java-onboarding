2a003c76b4eb9eb371c00683f3adfd18cf2d2f2e
swandevson <shai4956@gmail.com>
2022-10-28 19:33:03 +0900

    feat: P4 문자 변환기 작성
    단어의 각 문자를 청개구리 언어로 변환시켜주는 함수 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+
+
+    public static char convertAlpahbet(char ch) {   // 해당 문자를 변환
+        char converted;
+
+        if (Character.isUpperCase(ch)) {
+            converted = (char)('Z' - (ch - 'A'));
+        }
+        else {
+            converted = (char)('z' - (ch - 'a'));
+        }
+
+        return converted;
+    }
+
+
+
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
