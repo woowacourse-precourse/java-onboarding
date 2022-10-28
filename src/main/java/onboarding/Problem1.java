6327a1fd0b090df8a1a80da00fd5db691a392daf
Donghui KO <85717356+dongdong-119@users.noreply.github.com>
2022-10-28 17:03:20 +0900

    Problem1 - 기능목록작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,8 +2,30 @@ package onboarding;
 
 import java.util.List;
 
+
+/**
+ * <기능 목록>
+ *     1. 메소드
+ *          1) max
+ *          : 파라미터로 받는 두 변수(int) 중 큰 것을 반환한다.
+ *          2) calculate
+ *          : 왼쪽, 오른쪽 페이지의 각 자리 수를 모두 더하거나 뺀 것 중 가장 큰 수를 반환한다.
+ *          3) exceptionValidate
+ *          : pobi와 crong의 승패를 결정할 수 없는 예외의 경우를 검증. 예외 상황의 경우 -1를 반환한다.
+ *            이 함수가 검증하는 예외사항은 [참고1]과 같다.
+ *
+ *
+ *      [참고1]
+ *      # exceptionValidate가 검증하는 예외사항
+ *
+ *      - 펼친 페이지의 두 수가 연속하지 않는 경우.
+ *      - 펼친 페이지의 두 수가 책의 범위를 벗어나는 경우.
+ */
+
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
