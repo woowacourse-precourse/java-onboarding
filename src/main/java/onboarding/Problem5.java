4cde03928e7647779fff899dd2953535d6b1bfda
KSH <tkdgns543@gmail.com>
2022-10-29 02:01:13 +0900

    feat(Problem5): 화폐 액수 종류 배열 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] moneyTypes = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     }
 }
