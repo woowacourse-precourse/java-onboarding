df80af3a70c40b06295ed240c8ba1ee5e572718e
im-shung <seoyoung1113@gmail.com>
2022-10-28 23:54:53 +0900

    :sparkles: [문제1] 가장 큰 수 구하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,7 +1,10 @@
 package onboarding;
 
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import onboarding.problem1.Person;
 
 class Problem1 {
@@ -17,28 +20,44 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
-        List<Person> personList = List.of(
+        Person[] personList = {
                 new Person("pobi", pobi.get(LEFT), pobi.get(RIGHT)),
                 new Person("crong", crong.get(LEFT), crong.get(RIGHT))
-        );
+        };
 
-        // 1. [예외처리] 페이지 번호 입력
-        for (Person person: personList) {
+        for (Person person : personList) {
+            /* [예외처리] 페이지 번호 입력 */
             int leftPageNumber = person.getLeftPageNumber();
             int rightPageNumber = person.getRightPageNumber();
 
             // 시작면(0) 또는 마지막면(401) 입력한 경우
-            if (isOutRange(leftPageNumber) || isOutRange(rightPageNumber))
+            if (isOutRange(leftPageNumber) || isOutRange(rightPageNumber)) {
                 return scoreMenu.get("exception");
+            }
 
             // 왼쪽페이지가 홀수가 아닌 경우
-            if (!isOddNumber(leftPageNumber))
+            if (!isOddNumber(leftPageNumber)) {
                 return scoreMenu.get("exception");
+            }
 
             // 오른쪽페이지가 짝수가 아닌 경우
-            if (!isEvenNumber(rightPageNumber))
+            if (!isEvenNumber(rightPageNumber)) {
                 return scoreMenu.get("exception");
+            }
 
+            /* [요구] 가장 큰 수 구하기  */
+            // 왼쪽, 오른쪽 페이지 번호 자릿수 구하기
+            int[] leftDegitList = getDigitList(leftPageNumber);
+            int[] rightDegitList = getDigitList(rightPageNumber);
+
+            for (int i = 0; i < 2; i++) {
+                // (1). 왼쪽 페이지 번호 자릿수 모두 더한 값, 모두 곱한 값 중 가장 큰 수 구하기
+                int leftScore = Math.max(getPlusScore(leftDegitList), getMultiplyScore(leftDegitList));
+                // (2). 오른쪽 페이지 번호 자릿수 모두 더한 값, 모두 곱한 값 중 가장 큰 수 구하기
+                int rightScore = Math.max(getPlusScore(rightDegitList), getMultiplyScore(rightDegitList));
+                // (3). (1)과 (2) 중 가장 큰 수를 본인 점수로 한다.
+                person.setMaxScore(Math.max(leftScore, rightScore));
+            }
         }
 
         int answer = Integer.MAX_VALUE;
@@ -47,17 +66,32 @@ class Problem1 {
 
     // 책의 시작 면이나 마지막 면이 있는지 확인하는 메서드
     public static boolean isOutRange(int number) {
-        return number==firstPageNumber || number==lastPageNumber;
+        return number == firstPageNumber || number == lastPageNumber;
     }
 
     // 홀수인지 확인하는 메서드
     public static boolean isOddNumber(int number) {
-        return number%2!=0;
+        return number % 2 != 0;
     }
 
     // 짝수인지 확인하는 메서드
     public static boolean isEvenNumber(int number) {
-        return number%2==0;
+        return number % 2 == 0;
+    }
+
+    // 페이지 번호 자릿수 구하는 메서드
+    public static int[] getDigitList(int number) {
+        return Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    // 페이지 번호 자릿수 모두 더한 값 구하는 메서드
+    public static int getPlusScore(int[] digitList) {
+        return Arrays.stream(digitList).reduce(0, (a, b) -> a + b);
+    }
+
+    // 페이지 번호 자릿수 모두 곱한 값 구하는 메서드
+    public static int getMultiplyScore(int[] digitList) {
+        return Arrays.stream(digitList).reduce(1, (a, b) -> a * b);
     }
 
 
