13a49118cc00c1f42a8fe0585fe0d479758e37a6
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-28 21:34:06 +0900

    feat(problem4): 문제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,24 @@ public class Problem4 {
     public static String solution(String word) throws Exception {
         validate(word);
         String answer = "";
+        answer = sayFrog(word.toCharArray());
         return answer;
+    }
+
+    private static String sayFrog(char[] charsInWord) {
+        char[] result = new char[charsInWord.length];
+        int idx = 0;
+        for (char each : charsInWord) {
+            if (Character.isLowerCase(each)) {
+                result[idx] = ((char) (97+122 - (int) each));
+            } else if (Character.isUpperCase(each)) {
+                result[idx] = ((char) (155 - (int) each));
+            } else {
+                result[idx] = (each);
+            }
+            idx += 1;
+        }
+        return new String(result);
     }
 
     private static void validate(String word) throws Exception {
