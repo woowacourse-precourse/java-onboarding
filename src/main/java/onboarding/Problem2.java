222433c206031ccd93b124c1b44cf3b8746b5245
Your Name <dbsdbxkrzz@naver.com>
2022-10-28 13:26:01 +0900

    feat: Problem2의 isDeletable 메서드를 구현합니다.
    boolean 타입의 isConsecutive 배열을 전역로 선언한 후,
    문자열에서 연속하여 중복된 문자열이 있는 위치의 인덱스를
    true로 체크합니다.
    deletable 변수를 선언하여 한번이라도 체크가 되었다면, true를, 그렇지 않다면 false를
    return하도록 합니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 public class Problem2 {
+	public static boolean[] isConsecutive;
+
 	public static String solution(String cryptogram) {
 		while (isDeletable(cryptogram)) {
 			cryptogram = deleteConsecutive(cryptogram);
@@ -9,10 +11,20 @@ public class Problem2 {
 	}
 
 	public static boolean isDeletable(String cryptogram) {
-		return false;
+		isConsecutive = new boolean[cryptogram.length()];
+		boolean deletable = false;
+		for (int i = 0; i < cryptogram.length() - 1; i++) {
+			if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+				isConsecutive[i] = true;
+				isConsecutive[i + 1] = true;
+				deletable = true;
+			}
+		}
+		return deletable;
 	}
 
 	public static String deleteConsecutive(String cryptogram) {
+		
 		return cryptogram;
 	}
 
