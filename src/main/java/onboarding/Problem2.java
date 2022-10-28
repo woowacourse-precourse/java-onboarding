7bd4c583d411789f427dbb096793255cd91c6b40
yjlee0235 <yjlee0235@naver.com>
2022-10-28 13:24:41 +0900

    feat: 문자열에서 연속하는 중복 문자를 삭제하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,27 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static String removeContinuousDupChar(String dupStr) {
+        if (dupStr.isEmpty())
+            return "";
+
+        int dupStrLength = dupStr.length();
+        boolean[] checkDup = new boolean[dupStrLength];
+        char tmpCh = dupStr.charAt(0);
+        for (int i = 1; i < dupStrLength; i++) {
+            if (tmpCh == dupStr.charAt(i)) {
+                checkDup[i - 1] = checkDup[i] = true;
+            }
+            tmpCh = dupStr.charAt(i);
+        }
+
+        String result = "";
+        for (int i = 0; i < checkDup.length; i++) {
+            if (!checkDup[i]) {
+                result += dupStr.charAt(i);
+            }
+        }
+        return result;
+    }
 }
