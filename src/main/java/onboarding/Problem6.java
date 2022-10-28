2e99d461b5a5dabd06d2be0b3d08d03e9fdbf888
Eundms <dms011221@gmail.com>
2022-10-28 21:31:17 +0900

    add(Problem6-TODO)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        //TODO: 토큰화; 이름의 두 글자씩 잘라서 {토큰:[토큰이포함된이메일주소,..]} 을 만든다.
+        //TODO: 공통값찾기;
         return answer;
     }
 }
