d4b5b87d459d168c9f26a5d6950160574b975f0b
¤knj <krkoknj@gmail.com>
2022-10-28 14:49:10 +0900

    1부터 매개변수로 들어온 값까지의 숫자를 배열에 넣는다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int[] ints = fillinArrayNumbers(number);
+
         return answer;
+    }
+
+    private static int[] fillinArrayNumbers(int number) {
+        int[] tmp = new int[number];
+
+        for (int i = 1; i <= number; i++) {
+            tmp[i - 1] = i;
+        }
+
+        return tmp;
     }
 }
