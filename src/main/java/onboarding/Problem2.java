76ccc3487a13bfdd1ebee6989e913c6299bdf027
xxeol2 <jhj01177@naver.com>
2022-10-28 13:10:17 +0900

    feat(prob2): 문자열의 연속된 문자를 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,14 +6,38 @@ public class Problem2 {
 		return answer;
 	}
 
-	public boolean checkContinuity(String cryptogram) {
+	public static boolean checkContinuity(char[] cryptogram) {
 		char previousC = ' ';
-		for (char c : cryptogram.toCharArray()) {
+		for (char c : cryptogram) {
 			if (previousC == c) {
 				return true;
 			}
 			previousC = c;
 		}
 		return false;
+	}
+
+	public static String removeContinuity(char[] cryptogram) {
+		char previousC = ' ';
+		boolean flag = false;
+		String result = "";
+		for (int i = 0; i < cryptogram.length; i++) {
+			if (previousC == cryptogram[i]) {
+				flag = true;
+				continue;
+			}
+
+			if (!flag) {
+				result += previousC;
+			}
+
+			previousC = cryptogram[i];
+			flag = false;
+		}
+		if (!flag) {
+			result += previousC;
+		}
+
+		return result;
 	}
 }
