e123be20fcb849d7c2df83e036ef3613e27d2a6d
jihong <clara5091@naver.com>
2022-10-28 15:19:49 +0900

    problem1-제한사항 검사 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,18 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+    private static boolean checkList(List<Integer> list) {
+        if (list.size() != 2)
+            return false;
+        if (list.get(0) % 2 == 0 || list.get(1) != (list.get(0) + 1))
+            return false;
+        if (list.get(0) <= 1 || list.get(1) >= 400)
+            return false;
+        return true;
     }
+
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+    }
+
 }
