33be63431ffd1c7e6bdddbbfb4534c2ef2ac90e1
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-28 16:10:59 +0900

    Feat(Problem1): 에러처리기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (checkError(pobi) || checkError(crong)) {
+            return -1;
+        }
+
         return answer;
+    }
+
+    static boolean checkError(List<Integer> user) {
+        for (Integer page : user) {
+            if (page > 400) {
+                return true;
+            }
+        }
+
+        return user.get(1) - user.get(0) != 1;
     }
 }
