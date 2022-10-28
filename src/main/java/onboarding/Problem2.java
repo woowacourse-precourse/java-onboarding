73a924ab00fe80533c321c967c265f56a5ce070e
glaehfdl1654 <glaehfdl1654@naver.com>
2022-10-29 02:54:04 +0900

    [Problem2] find and remove character function, finish

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,12 +6,23 @@ public class Problem2 {
         while (beforeLength != cryptogram.length()) {
             beforeLength = cryptogram.length();
             for (int i = 0; i < cryptogram.length() - 1; i++) {
-
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    cryptogram = removeRepeatedChar(cryptogram, i);
+                }
             }
         }
         return cryptogram;
     }
     public static String removeRepeatedChar(String cryptogram, int starting) {
-
+        StringBuilder ret = new StringBuilder();
+        ret.append(cryptogram);
+        int i = starting;
+        for (; i < ret.length() - 1; i++) {
+            if (ret.charAt(i) != ret.charAt(i + 1)) {
+                break;
+            }
+        }
+        ret.delete(starting, i + 1);
+        return ret.toString();
     }
 }
