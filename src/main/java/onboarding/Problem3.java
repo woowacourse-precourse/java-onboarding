6f28426d9d7f25f8ec5149b42e35e3ce77029f33
hectick <chyeon1223@cau.ac.kr>
2022-10-28 22:41:51 +0900

    feat : [Problem3] 주어진 숫자의 맨 뒷자리 수부터 차례대로 3,6,9가 들어가는지 검사하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,17 @@ public class Problem3 {
         int answer = 0;
 
         //입력값의 맨 뒷자리 수부터 차례대로 3,6,9가 들어가는지 검사
+        for(int i = 1; i <= number; i++){
+            int currentNumber = i; //갱신된 숫자를 저장
+            while(currentNumber > 0){
+                int tmp = currentNumber%10;
+                if(tmp != 0 && tmp%3 == 0){
+                    //3,6,9가 들어간 경우
 
+                }
+                currentNumber /= 10;
+            }
+        }
         //3,6,9가 포함된 횟수만큼 answer를 증가
 
         return answer;
