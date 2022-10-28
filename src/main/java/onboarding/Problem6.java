922bb1ef0979921ad6c848a7007f578d4424a10a
1eejisoo <jisu991012@gmail.com>
2022-10-28 21:14:38 +0900

    [Problem 6] Feat: 자기 자신 빼고의 닉네임들을 한 문자열로 만드는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>();
+        return checkDuplicate(forms, answer);
+    }
+
+    // 자기 자신 빼고의 닉네임들을 문자열로 만드는 기능
+    public static String nicknamesToString (List<String> names, int i) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < names.size(); j++) {
+            if (j != i)
+                sb.append(names.get(j)).append(".");
+        }
+        return sb.toString();
     }
 }
