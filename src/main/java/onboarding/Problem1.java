6ea5a749a9c6741986eb10f6da23c1db658143af
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-28 13:54:04 +0900

    페이지의 각 자리수들을 List자료형으로 저장

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,17 +1,38 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList; //ArrayList를 사용하기 위해 import
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int por =(int) (Math.random()*197+2)*2;    // 포비가 펼친 오른쪽 페이지
-        int pol = por -1;                          // 포비가 펼친 왼쪽 페이지
+        int pobiright =(int) (Math.random()*197+2)*2;
+        int pobileft = pobiright -1;                       
         
-        int crr =(int) (Math.random()*197+2)*2;    // 크롱이 펼친 오른쪽 페이지
-        int crl = crr -1;                          // 크롱이 펼친 왼쪽 페이지
+        int crongright =(int) (Math.random()*197+2)*2;
+        int crongleft = crongright -1;                     
         
+        List<Integer> listpobiright = new ArrayList<>();
+        while(pobiright > 0){
+            listpobiright.add(pobiright%10);
+            pobiright /= 10;                               
+        }
+        List<Integer> listpobileft = new ArrayList<>();
+        while(pobileft > 0){
+            listpobileft.add(pobileft%10);
+            pobileft /= 10;
+        }
         
+        List<Integer> listcrongright = new ArrayList<>();
+        while(crongright > 0){
+            listcrongright.add(crongright%10);
+            crongright /= 10;
+        }
         
+        List<Integer> listcrongleft = new ArrayList<>();
+        while(crongleft > 0){
+            listcrongleft.add(crongleft%10);
+            crongleft /= 10;                               // 포비와 크롱이 펼친 페이지들의 각 자리수를 List자료형으로 저장 
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
     }
