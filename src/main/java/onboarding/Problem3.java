2932ef9fcd08e6ff13dfbc602ae9ec1db0558a6c
dmdrk1414 <seungchan141414@gmail.com>
2022-10-28 19:19:37 +0900

    [Add] problem_3
    문제는 풀었지만 for문안에 배열을 생성해주는 점에서 배열을 접근하는 방법만있는지 다시 생각해보아야겠다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,32 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int sumResult = 0;
+        for (int index = 1; index <= number; index++) {
+            String indexStr = String.valueOf(index);
+            String[] arrStr = new String[indexStr.length()];
+            arrStr = indexStr.split("");
+            sumResult = getSumResult(sumResult, arrStr);
+            arrStr = null;
+        }
+        return sumResult;
+    }
+
+    private static int getSumResult(int sumResult, String[] arrStr) {
+        for (String value : arrStr) {
+            if (isThreeSixNine(value)) {
+                sumResult = sumResult + 1;
+            }
+        }
+        return sumResult;
+    }
+
+    private static boolean isThreeSixNine(String value) {
+        if (value.equals("3") || value.equals("6") || value.equals("9")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+
