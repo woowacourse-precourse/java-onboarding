92af50f4f7f5ed5fe64586f2a8364f4c9c8ed565
KSH <tkdgns543@gmail.com>
2022-10-29 00:28:16 +0900

    set(Problem4): 두개의 문자치환 메서드명 변경
    lowerCaseReplace -> changeLowerCase
    upperCaseReplace -> changeUpperCase

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,12 +7,12 @@ public class Problem4 {
     }
 
     /* 주어진 소문자를 기능 구현 조건대로 치환하는 메서드 */
-    private static char lowerCaseReplace(char c) {
+    private static char changeLowerCase(char c) {
         return (char)('z'+'a'-c);
     }
 
     /* 주어진 대문자를 기능 구현 조건대로 치환하는 메서드 */
-    private static char upperCaseReplace(char c) {
+    private static char changeUpperCase(char c) {
         return (char)('Z'+'A'-c);
     }
 }
