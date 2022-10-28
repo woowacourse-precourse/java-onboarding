998b95fc06c31e0f8320b22bd077bc2f5cf05170
Seph-23 <seongmlyu95@gmail.com>
2022-10-28 12:40:06 +0900

    5번 문제 구현 완료 - ATM 기계

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,17 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
-    public static void main(String[] args) {
-        int money = 50237;
-        System.out.println("solution(money) = " + solution(money));
-    }
+//    public static void main(String[] args) {
+//        int money = 50237;
+//        System.out.println("solution(money) = " + solution(money));
+//    }
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] temp = new int[moneyType.length];
+
+        for (int i = 0; i < moneyType.length; i++) {
+            temp[i] += money/moneyType[i];
+            money %= moneyType[i];
+            answer.add(temp[i]);
+        }
         return answer;
     }
 }
