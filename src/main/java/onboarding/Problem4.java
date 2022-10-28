b9e6ad5d9fb6d0b3fa86ba75d2e4dd72362b063f
Identityless <rlandwnd555@naver.com>
2022-10-28 17:48:45 +0900

    FEAT : 문자열을 하나하나의 토큰으로 분할해주는 StringTokenizer 클래스 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+/*
+ *  구현 기능
+ *  1. 문자열을 각 토큰으로 분할하는 기능
+ *  2. 각 토큰이 알파벳인지 검사하는 기능
+ *  3. 알파벳을 대응하는 뒤집힌 알파벳으로 변환하는 기능
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+}
+
+class StringTokenizer {
+    String[] wordToken;
+
+    public StringTokenizer(String word) {
+        this.wordToken = word.split("");
+    }
+
+    public String[] getToken(){
+        return wordToken;
+    }
+
 }
