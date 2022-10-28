c9212f8b240e8f0fd34224442f4e89827ec678b9
JongHo <imasp@naver.com>
2022-10-28 12:50:08 +0900

    pro5_1

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+    public static int[] solution(int money) {
+        //List<Integer> answer = Collections.emptyList();
+        int[] answer=new int[9];
+        int[] sort_money = new int[]{50000,10000,5000,1000,500,100,50,10,1};
+
+        for(int s = 0; s< sort_money.length; s++){ //s=sort
+            if(s==0) answer[s]=(money/sort_money[s]);
+            else answer[s] = (money%sort_money[s-1])/sort_money[s];
+        }
         return answer;
     }
 }
