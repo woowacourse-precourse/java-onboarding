0dd4fc4d11e4df4c82ce07c266689dec3df68cea
minsu <bill7666@naver.com>
2022-10-28 17:33:58 +0900

    문제4번 알파벳 배열에넣기완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String answer="";
+        char[] alphabet = new char[26];
+        HashMap<String,String> map = new HashMap<String,String>();
+        for(int i =0;i<26;i++){
+            alphabet[i] = (char)('A'+i);
+        }
+       return answer
     }
 }
--- src/main/java/onboarding/TestProtocol.java
+++ src/main/java/onboarding/TestProtocol.java
@@ -1,9 +1,14 @@
 package onboarding;
 
-public class TestProtocol {
+import java.util.HashMap;
 
+public class TestProtocol {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        char[] alphabet = new char[26];
+        HashMap<String,String> map = new HashMap<String,String>();
+        for(int i =0;i<26;i++){
+            alphabet[i] = (char)('A'+i);
+        }
 
     }
 }
