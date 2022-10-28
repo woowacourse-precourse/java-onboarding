5bef8424fb98591e0df0cf4ea58bd86fba3cd3bf
eunbii0213 <eunbii0213@naver.com>
2022-10-28 17:11:35 +0900

    feat: (Problem7) User와 친구인지 체크한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    public static List<String> friendsWithUser = new ArrayList<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        addFriendsWithUser(user,friends);
         return answer;
+    }
+
+    public static void addFriendsWithUser(String user, List<List<String>> friends) {
+        for(List<String> list : friends){
+            if(list.contains(user)) {
+                for (String friend : list) {
+                    if(!friend.equals(user)){
+                        friendsWithUser.add(friend);
+                    }
+                }
+            }
+        }
     }
 }
