1ec888670f5ec263d6f0e2e346e9721c1faa2405
dev_kong <ahdjd5@gmail.com>
2022-10-29 00:30:59 +0900

    refactor(Problem1): move method to Pages class

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,42 +1,28 @@
 package onboarding;
 
+import onboarding.problem1.Pages;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int pobiMaxValue = getMaxValue(pobi);
-        int crongMaxValue = getMaxValue(crong);
-        if (pobiMaxValue == crongMaxValue) {
+        try{
+            Pages pobiPages = new Pages(pobi);
+            Pages crongPages = new Pages(crong);
+            int pobiMaxValue = pobiPages.getMaxValue();
+            int crongMaxValue = crongPages.getMaxValue();
+
+            if (pobiMaxValue > crongMaxValue) {
+                return 1;
+            }
+
+            if (pobiMaxValue < crongMaxValue) {
+                return 2;
+            }
+
             return 0;
+        }catch (IllegalArgumentException ex){
+            return -1;
         }
-
-        if (pobiMaxValue > crongMaxValue) {
-            return 1;
-        }
-
-        return 2;
-    }
-
-    private static int getMaxValue(List<Integer> values) {
-        int maxValue = 0;
-        for (Integer value : values) {
-            maxValue = Math.max(maxValue, getMaxBetweenSumAndMultiple(value));
-        }
-
-        return maxValue;
-    }
-
-    private static int getMaxBetweenSumAndMultiple(int value) {
-        int sum = 0;
-        int multiple = 1;
-        String string = Integer.toString(value);
-
-        for (int i = 0; i < string.length(); i++) {
-            int targetNumber = Integer.parseInt(string.substring(i, i + 1));
-            sum += targetNumber;
-            multiple *= targetNumber;
-        }
-
-        return Math.max(sum, multiple);
     }
 }
--- src/main/java/onboarding/problem1/Pages.java
+++ src/main/java/onboarding/problem1/Pages.java
@@ -5,11 +5,36 @@ import onboarding.problem1.utils.Validator;
 import java.util.List;
 
 public class Pages {
-    private int left;
-    private int right;
+    private final int left;
+    private final int right;
 
     public Pages(List<Integer> pages) {
+        if(!Validator.validatePages(pages)){
+            throw new IllegalArgumentException();
+        }
+
         this.left = pages.get(0);
         this.right = pages.get(1);
+    }
+
+    public int getMaxValue() {
+        int leftMaxValue = getMaxBetweenSumAndMultiple(this.left);
+        int rightMaxValue = getMaxBetweenSumAndMultiple(this.right);
+
+        return Math.max(leftMaxValue,rightMaxValue);
+    }
+
+    private int getMaxBetweenSumAndMultiple(int value) {
+        int sum = 0;
+        int multiple = 1;
+        String string = Integer.toString(value);
+
+        for (int i = 0; i < string.length(); i++) {
+            int targetNumber = Integer.parseInt(string.substring(i, i + 1));
+            sum += targetNumber;
+            multiple *= targetNumber;
+        }
+
+        return Math.max(sum, multiple);
     }
 }
