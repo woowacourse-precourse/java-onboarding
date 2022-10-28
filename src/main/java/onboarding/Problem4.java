b3ab7429a663a6921eaa056aaf9a34836723590d
lzlkolo <lzlko1209@gmail.com>
2022-10-28 14:27:05 +0900

    PROBLEM4.기능목록 및 예외사항 처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+    static boolean isCheck(String word) {
+        if(word.length()<1 || word.length()>1000) {
+            return true;
+        }
+        return false;
+    }
+
     public static String solution(String word) {
-        String answer = "";
+        String answer="";
         return answer;
     }
 }
+
