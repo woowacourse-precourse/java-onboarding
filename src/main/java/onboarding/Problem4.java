af918eb52712297cf89be8cc69a377b744fda776
sinryuji <siler2474@gmail.com>
2022-10-28 17:31:38 +0900

    Feat: Problem4 complete

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,10 +7,31 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            answer += getReverse(word.charAt(i));
+        }
         return answer;
     }
 
     private static boolean isAlpha(char ch) {
         return ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122));
+    }
+
+    private static char getReverse(char ch) {
+        if (isAlpha(ch) == false)
+            return ch;
+        if (Character.isUpperCase(ch) == true) {
+            if (ch <= 'M')
+                ch += 1 + (('M' - ch) * 2);
+            else
+                ch -= 1 + ((ch - 'N') * 2);
+        }
+        else if (Character.isLowerCase(ch) == true) {
+            if (ch <= 'm')
+                ch += 1 + (('m' - ch) * 2);
+            else
+                ch -= 1 + ((ch - 'n') * 2);
+        }
+        return ch;
     }
 }
