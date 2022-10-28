3dfe42ea70091995898c3f8303668982abe3c995
Park Sungchul <try615@naver.com>
2022-10-29 00:52:58 +0900

    [fix] Problem1.java : 각 자리 숫자 곱셈 연산 수정 - 숫자 도중의 0과 처음 0을 만났을 때의 차이 고려

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,19 +1,52 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
-    static int LEN_NUMBER_OF_DIGITS = 3;
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        list1.add(97);
+        list1.add(98);
+        list2.add(197);
+        list2.add(198);
+
+        System.out.println(solution(list1, list2));
+    }
+    static final int LEN_NUMBER_OF_DIGITS = 3;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        return playGame(pobi, crong);
+        int pobiScore = calcScore(pobi);
+        int crongScore = calcScore(crong);
+
+        System.out.println("pobiScore = " + pobiScore);;
+        System.out.println("crongScore = " + crongScore);
+
+        if (pobiScore == Integer.MAX_VALUE || crongScore == Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        if (pobiScore > crongScore) {
+            return 1;
+        } else if (pobiScore < crongScore) {
+            return 2;
+        } else {
+            return 0;
+        }
     }
 
     private static int operMulti(int[] arr) {
         int result = 1;
-        for (int num : arr) {
-            result *= num;
+        boolean numberStart = false;
+        for (int i = arr.length-1; i >= 0; i--) {
+            if (arr[i] == 0 && !numberStart) {
+                continue;
+            }else if (arr[i] != 0 && !numberStart) {
+                numberStart = true;
+            }
+            result *= arr[i];
         }
         return result;
     }
@@ -35,32 +68,15 @@ class Problem1 {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (page % (int) Math.pow(10, i + 1)) / (int) Math.pow(10, i);
         }
+        System.out.println(page + " " + Arrays.toString(arr));
         return arr;
     }
 
-    private static int calcScore(List<Integer> list) {
+    private static int calcScore(List<Integer> pages) {
         int score = Integer.MIN_VALUE;
-        for (Integer num : list) {
-            score = Math.max(score, compareScore(intToArr(num)));
+        for (Integer page : pages) {
+            score = Math.max(score, compareScore(intToArr(page)));
         }
         return score;
     }
-
-    private static int playGame(List<Integer> pobi, List<Integer> crong) {
-        int pobiScore = calcScore(pobi);
-        int crongScore = calcScore(crong);
-
-        if (pobiScore == Integer.MAX_VALUE || crongScore == Integer.MAX_VALUE) {
-            return -1;
-        }
-
-        if (pobiScore > crongScore) {
-            return 1;
-        } else if (pobiScore < crongScore) {
-            return 2;
-        } else {
-            return 0;
-        }
-    }
-
 }
