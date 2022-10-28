c66e2cb4962b9a5a12e67f30b0af1e1e73ef3220
Soomin Lim <augustth511@gmail.com>
2022-10-28 16:52:40 +0900

    기능 추가: 예외사항인 경우를 처리하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,10 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        if (!validate(pobi) || !validate(crong)) {
+            return -1;
+        }
+
         int pobiMax = Math.max(findMaxNum(pobi.get(0)), findMaxNum(pobi.get(1)));
         int crongMax = Math.max(findMaxNum(crong.get(0)), findMaxNum(crong.get(1)));
 
@@ -20,6 +24,22 @@ class Problem1 {
         }
 
         return answer;
+    }
+
+    public static boolean validate(List<Integer> list) {
+        int left = list.get(0);
+        int right = list.get(1);
+
+        if (left % 2 != 1 || right % 2 != 0)
+            return false;
+
+        else if (right - left != 1)
+            return false;
+
+        else if (left == 1 || right == 400)
+            return false;
+
+        return true;
     }
 
     public static int findMaxNum(int n) {
