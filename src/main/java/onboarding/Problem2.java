de929f091827c482b631b5d765ad1faa05087a1d
patronum98 <zkrhklkl98@gmail.com>
2022-10-28 17:14:57 +0900

    기능요구사항 문제 2의 기능목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+// 기능 목록
+// 1. 문자열을 분석하기 위해 문자열을 배열로 변환한다.
+// 2. 배열에서 연속된 중복된 문자들을 제거한다.
+// 3. 배열을 다시 문자열로 변환한 뒤 결과를 반환한다.
 
 public class Problem2 {
     public static String solution(String cryptogram) {
