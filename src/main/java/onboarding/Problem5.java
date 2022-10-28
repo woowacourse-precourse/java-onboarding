b245385e34e7fb98c69dd436fd96ad14ff0e6461
minhee020 <minhee020@naver.com>
2022-10-28 22:56:24 +0900

    Problem5 | 예외 사항 처리 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,12 +8,17 @@ import java.util.List;
  * 기능 구현
  * 1. 배열 생성. 길이 9
  * 2. 거스름돈 알고리즘 구현
+ * 3. 예외 처리
  */
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>(List.of(50000,10000,5000,1000,500,100,50,10,1));
         int divisor = 0;
+
+        if (1 <= money && money <= 1000_000){
+            return Collections.emptyList();
+        }
 
         for (int i = 0; i < 9; i++){
             divisor = answer.get(i);
