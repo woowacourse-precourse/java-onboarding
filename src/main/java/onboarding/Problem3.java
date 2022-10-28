bc7f0a710562a6c33580f8732047d4d35eddab94
CMHyuk <alsgur9710@naver.com>
2022-10-28 20:27:10 +0900

    feat: 입력 값 문자 변환 후 3,6,9 카운트 로직

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,31 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = intToStringAndCount(number, answer);
         return answer;
+    }
+
+    private static int intToStringAndCount(int number, int answer) {
+        for (int i = 1; i <= number; i++) {
+            String num = String.valueOf(i);
+            answer = countNumber(answer, num);
+        }
+        return answer;
+    }
+
+    private static int countNumber(int count, String num) {
+        for (int j = 0; j < num.length(); j++) {
+            char checkNumber = num.charAt(j);
+            if (checkNumber == '3') {
+                count++;
+            }
+            if (checkNumber == '6') {
+                count++;
+            }
+            if (checkNumber == '9') {
+                count++;
+            }
+        }
+        return count;
     }
 }
