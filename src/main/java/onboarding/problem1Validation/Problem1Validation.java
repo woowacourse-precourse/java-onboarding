f2d34058a711031af405005c02cf026d9e19a822
math0917 <math0917@naver.com>
2022-10-28 17:28:13 +0900

    feat : 책페이지가 연속된 값인지 확인하는 validation code 작성

--- src/main/java/onboarding/problem1Validation/Problem1Validation.java
+++ src/main/java/onboarding/problem1Validation/Problem1Validation.java
@@ -2,6 +2,8 @@ package onboarding.problem1Validation;
 
 import java.util.List;
 
+import static java.lang.Math.*;
+
 public class Problem1Validation {
     public static boolean userInputSizeEquals(List<Integer> userInput, int listSize) {
         if (userInput.size() == 2) {
@@ -17,15 +19,21 @@ public class Problem1Validation {
         return Long.valueOf(userInput.size()) == count;
     }
 
+    public static boolean isBookNumberContinuous(List<Integer> list) {
+        int abs = abs(list.get(0) - list.get(1));
+        if (abs == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
     private static boolean isBetween(Integer pageNum, int bookMin, int bookMax) {
         if (bookMin <= pageNum && pageNum <= bookMax) {
             return true;
         } else {
             return false;
         }
-    }
-
-    public static boolean isBookNumberContinuous(List<Integer> list) {
-        return false;
     }
 }
