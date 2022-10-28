24f703b96170c07dfe33139f8eb3974a456ee8dd
backFox <jorealbusiness@gmail.com>
2022-10-28 12:36:59 +0900

    refactor(problem2): 내용을 쉽게 파악할 수 있도록 변수를 상수화하고 메소드를 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,15 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static final String REPEATING_MORE_THAN_ONCE_REGEX = "(.)\\1+";
+
     public static String solution(String cryptogram) {
         return removeDuplicated(cryptogram);
     }
 
     public static String removeDuplicated(String target) {
-        String result = target.replaceAll("(.)\\1+", "");
-        if (!target.equals(result)) {
+        String result = target.replaceAll(REPEATING_MORE_THAN_ONCE_REGEX, "");
+        if (isWordLengthChanged(target, result)) {
             result = removeDuplicated(result);
         }
         return result;
+    }
+
+    private static boolean isWordLengthChanged(String target, String result) {
+        return !target.equals(result);
     }
 }
