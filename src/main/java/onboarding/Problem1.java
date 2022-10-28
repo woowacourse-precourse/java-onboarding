8131564f7064b9ec13349e277bb54564bc4eede0
dev_kong <ahdjd5@gmail.com>
2022-10-29 00:36:18 +0900

    refactor(Problem1): split solution method to keep indent 1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,24 +5,31 @@ import onboarding.problem1.Pages;
 import java.util.List;
 
 class Problem1 {
+    private static Pages pobiPages;
+    private static Pages crongPages;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         try{
-            Pages pobiPages = new Pages(pobi);
-            Pages crongPages = new Pages(crong);
-            int pobiMaxValue = pobiPages.getMaxValue();
-            int crongMaxValue = crongPages.getMaxValue();
-
-            if (pobiMaxValue > crongMaxValue) {
-                return 1;
-            }
-
-            if (pobiMaxValue < crongMaxValue) {
-                return 2;
-            }
-
-            return 0;
+            pobiPages = new Pages(pobi);
+            crongPages = new Pages(crong);
+            return checkWinner();
         }catch (IllegalArgumentException ex){
             return -1;
         }
+    }
+
+    private static int checkWinner () {
+        int pobiMaxValue = pobiPages.getMaxValue();
+        int crongMaxValue = crongPages.getMaxValue();
+
+        if (pobiMaxValue > crongMaxValue) {
+            return 1;
+        }
+
+        if (pobiMaxValue < crongMaxValue) {
+            return 2;
+        }
+
+        return 0;
     }
 }
