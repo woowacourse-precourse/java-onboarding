0e48b769d819791a3a15511bc37a7b57e2d3f639
Heesom <heesom.hs@gmail.com>
2022-10-28 13:39:35 +0900

    Problem2 : checkDouble 메소드 추가
    문자열 안의 중복이 존재하는지 검사해 boolean을 리턴하는 메소드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
-
+import java.lang.String;
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
         return answer;
     }
+
+    public static boolean checkDouble(String cryptogram) {
+        for (int i = 0; i < cryptogram.length()-1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
 }
