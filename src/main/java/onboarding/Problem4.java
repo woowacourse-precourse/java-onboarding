f886b8efc8dddb7ba92df69220a8bb6433dcebc5
great-park <cksgh1735@naver.com>
2022-10-29 02:44:48 +0900

    feat(Problem4): ASCII code를 활용한 풀이

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,35 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int space = ' ';
+    private static final int upperA = 'A';
+    private static final int upperZ = 'Z';
+    private static final int lowerA = 'a';
+    private static final int lowerZ = 'z';
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for(int i = 0; i < word.length(); i++){
+            int asciiCode = word.charAt(i);
+            append(asciiCode, answer);
+        }
+        System.out.println("answer.toString() = " + answer.toString());
+        return answer.toString();
+    }
+
+    public static void append(int asciiCode, StringBuilder answer){
+        // 공백 ASCII code -> 32
+        if (asciiCode == space){
+            answer.append((char) space);
+            return;
+        }
+        if (asciiCode >= lowerA && asciiCode <= lowerZ){
+            answer.append((char) (lowerZ - (asciiCode - lowerA)));
+            return;
+        }
+        if (asciiCode >= upperA && asciiCode <= upperZ){
+            answer.append((char) (upperZ - (asciiCode - upperA)));
+        }
     }
 }
