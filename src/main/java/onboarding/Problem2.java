d051314a8c71b925afca1d38139bc6de34f490fe
dydtmd6801 <dydtmd6801@naver.com>
2022-10-28 17:22:34 +0900

    feat : 연속되는 중복 문자 확인 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int overlap = isOverlap(cryptogram);
         return answer;
+    }
+
+    private static int isOverlap(String secretMessage){
+        String[] arrayString = secretMessage.split("");
+        for(int i = 0; i < arrayString.length - 1; i++){
+            if(arrayString[i].equals(arrayString[i+1])){
+                System.out.println("중복발생");
+                return 1;
+            }
+        }
+        return 0;
     }
 }
