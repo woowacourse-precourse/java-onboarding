6c2840251db6ba741347308d6df576a8acf76917
Isu Shin <isu010614@naver.com>
2022-10-29 01:01:22 +0900

    feat: Add function to convert upper/lowercase alphabet

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,19 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+
+    public static char convertAlphabet(char letter){
+        String alphabet = Character.toString(letter);
+        if(alphabet.matches("[a-z]")){
+            int number = 219 - letter;
+            return (char)number;
+        }
+        if(alphabet.matches("[A-Z]")){
+            int number = 155 - letter;
+            return (char)number;
+        }
+        return '@';
+    }
+
 }
