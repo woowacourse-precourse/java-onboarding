be6d827b8214fb4259b6f781687ce9c96a9edd8a
recicoco068 <gkapf838@gmail.com>
2022-10-28 22:01:46 +0900

    feat : Problem4 문자가 알파벳인지 판별하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
+    public static final int BIG = 0;
+    public static final int SMALL = 1;
+    public static final int NONE = -1;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static int isAlphabet(char c) {
+        if ('A' <= c && c <= 'Z')
+            return BIG;
+        if ('a' <= c && c <= 'z')
+            return SMALL;
+        return NONE;
     }
 }
