3e2b2bd94b66077d9cc38e6b9b795ab6f5b2387d
math0917 <math0917@naver.com>
2022-10-28 19:43:30 +0900

    test : input validation을 위한 test code 작성

---
+++ src/main/java/onboarding/problem2Validation/Problem2Validation.java
@@ -0,0 +1,9 @@
+package onboarding.problem2Validation;
+
+import java.util.List;
+
+public class Problem2Validation {
+    public static boolean Problem2InputValidation(String userInput, int inputMinLength, int inputMaxLength) {
+        return true;
+    }
+}
