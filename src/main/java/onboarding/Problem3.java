ea4cbc145f874a03f681a64859fe77bee18434b2
seongha <11pi885@gmail.com>
2022-10-29 02:44:56 +0900

    feat: 기능목록작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+
+
+
+    //1~10000까지 dp 테이블을 생성하는 함수
+
+
     public static int solution(int number) {
         int answer = 0;
+
+        //주어진 숫자에 손뼉을 몇번 쳐야하는지 dp 테이블에서 구하기
         return answer;
     }
 }
