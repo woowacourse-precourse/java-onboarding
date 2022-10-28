263ecf60a9cef3139d32298fef24efd108f5b38a
SeHyeok <imasp@naver.com>
2022-10-28 13:11:49 +0900

    problem5_1

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,7 +8,7 @@ public class Problem5 {
         int[] answer=new int[9];
         int[] sort_money = new int[]{50000,10000,5000,1000,500,100,50,10,1};
 
-        for(int s = 0; s< sort_money.length; s++){ //s = sort
+        for(int s = 0; s< sort_money.length; s++){ //s=sort
             if(s==0) answer[s]=(money/sort_money[s]);
             else answer[s] = (money%sort_money[s-1])/sort_money[s];
         }
