791b80fe66e6e282e1be4f4586403d66885f62bd
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-28 13:04:19 +0900

    feat: Problem_3 369 카운트 기능 구현
    정수를 문자열로 변환해서 해당 문자를 세서
    갯수를 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem3 {
+    final static char THREE = '3';
+    final static char SIX = '6';
+    final static char NINE = '9';
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return count369(number);
     }
+
+    private static int count369(int range){
+        int count = 0;
+
+        for (int i=0; i<=range; i++){
+            String num = String.valueOf(i);
+            count += charCount(num);
+        }
+
+        return count;
+    }
+
+    private static int charCount(String num){
+        int count = 0;
+        for (Character ch : num.toCharArray()){
+            if(ch == THREE | ch == SIX | ch == NINE){
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
