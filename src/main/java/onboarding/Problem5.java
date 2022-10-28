1e98fae0aa7670d5894004e2921f11129e5d04f3
Identityless <rlandwnd555@naver.com>
2022-10-28 22:40:34 +0900

    FEAT : solution() 메서드 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -16,6 +16,8 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        MoneyCounter moneyCounter = new MoneyCounter(money);
+        answer = moneyCounter.getList();
         return answer;
     }
 }
@@ -32,56 +34,56 @@ class MoneyCounter {
     }
 
     private int fiveM(){
-        int count = 0;
+        int count;
         count = money / 50000;
         money -= 50000 * count;
         return count;
     }
 
     private int oneM(){
-        int count = 0;
+        int count;
         count = money / 10000;
         money -= 10000 * count;
         return count;
     }
 
     private int fiveT(){
-        int count = 0;
+        int count;
         count = money / 5000;
         money -= 5000 * count;
         return count;
     }
 
     private int oneT(){
-        int count = 0;
+        int count;
         count = money / 1000;
         money -= 1000 * count;
         return count;
     }
 
     private int fiveH(){
-        int count = 0;
+        int count;
         count = money / 500;
         money -= 500 * count;
         return count;
     }
 
     private int oneH(){
-        int count = 0;
+        int count;
         count = money / 100;
         money -= 100 * count;
         return count;
     }
 
     private int fifty(){
-        int count = 0;
+        int count;
         count = money / 50;
         money -= 50 * count;
         return count;
     }
 
     private int ten(){
-        int count = 0;
+        int count;
         count = money / 10;
         money -= 10 * count;
         return count;
