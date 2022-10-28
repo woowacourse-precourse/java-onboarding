e8f7964cee74c5be3db02a1b5914882db0f8326c
Dongmin Kim <kimdongmin68@naver.com>
2022-10-28 21:17:10 +0900

    feat: Add get visit score method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,7 +9,16 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Map<String, Integer> recommendFriend = new HashMap<>();
+
+        getFriendsScore(user, friends, recommendFriend);
+
         return answer;
+    }
+
+    public static void getVisitScore(List<String> visitors, Map<String, Integer> recommendFriend){
+        for(String visitor : visitors){
+            recommendFriend.put(visitor, recommendFriend.getOrDefault(visitor,0)+1);
+        }
     }
 
     public static void getFriendsScore(String user, List<List<String>> friends , Map<String,Integer> recommendFriend){
