efa54e25d281147e418f20a6886aa722ad743db4
김태용 <amico741@naver.com>
2022-10-28 22:20:37 +0900

    refactor: solution 리팩토링 및 아스키값 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,20 +3,31 @@ package onboarding;
 import java.util.HashMap;
 
 public class Problem4 {
+    public static void main(String[] args){
+        solution("I love you");
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        HashMap<Character, Character> map = new HashMap<>();
+        StringBuilder answer = new StringBuilder();
+        setValue(map);
+        for(int i = 0; i < word.length(); i++){
+            answer.append(map.get(word.charAt(i)));
+        }
+        System.out.println(answer);
+        return answer.toString();
     }
 
     public static void setValue(HashMap<Character, Character> map){
         int start = 0;
+        map.put(' ', ' ');
         for(int i = 65; i < 91; i++){
             map.put((char)i, (char)(90 - start));
             start += 1;
         }
         start = 0;
         for(int i = 97; i < 123; i++){
-            map.put((char)i, (char)(123 - start));
+            map.put((char)i, (char)(122 - start));
+            start += 1;
         }
     }
 }
