3452f35b53aeb1102639839a2e81008afae6bcd7
seonghoo1217 <seonghoo1217@naver.com>
2022-10-28 17:57:58 +0900

    feat : Cryptogram decode used by Stack

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder sb=new StringBuilder();
+        Stack<Character> stack=new Stack<>();
+        for (char x:cryptogram.toCharArray()){
+            if (stack.isEmpty()) stack.push(x);
+            else {
+                char peek = stack.peek();
+                if (x==peek) {
+                    stack.pop();
+                    continue;
+                }
+                stack.push(x);
+            }
+        }
+        while (!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
 }
