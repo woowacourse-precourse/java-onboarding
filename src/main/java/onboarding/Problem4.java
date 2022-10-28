dc35c2d1ce2b4aa790d6ca0096cbfff0f6a533d6
woogi <dnrlwo11@gmail.com>
2022-10-28 22:36:36 +0900

    [problem4] 알파벳 해쉬멥 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,38 @@
 package onboarding;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
+        Map<String, String> alphabet_map = new HashMap<>(){{
+            put("A", "Z");
+            put("B", "Y");
+            put("C", "X");
+            put("D", "W");
+            put("E", "V");
+            put("F", "U");
+            put("G", "T");
+            put("H", "S");
+            put("I", "R");
+            put("J", "Q");
+            put("K", "P");
+            put("L", "O");
+            put("M", "N");
+            put("N", "M");
+            put("O", "L");
+            put("P", "K");
+            put("Q", "J");
+            put("R", "I");
+            put("S", "H");
+            put("T", "G");
+            put("U", "F");
+            put("V", "E");
+            put("W", "D");
+            put("X", "C");
+            put("Y", "B");
+            put("Z", "A");
+        }};
         String answer = "";
         return answer;
     }
