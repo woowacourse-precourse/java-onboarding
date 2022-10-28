5c15920bc38b1f995e017dbed8d2c5ae3fe6bbb4
Eunkeee <eunkeeeee@gmail.com>
2022-10-28 15:25:12 +0900

    🔨 [P5] refactor: 코드 개선 (반복)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,56 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
 
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
 
-        int num50000 = money / 50000;
-        System.out.println(num50000); // 1
-        int leftover = money % 50000;
-        System.out.println(leftover); // 237
-
-        int num10000 = leftover / 10000;
-        System.out.println(num10000); // 0
-        leftover = leftover % 10000;
-        System.out.println(leftover); // 237
-
-        int num5000 = leftover / 5000;
-        System.out.println(num5000); // 0
-        leftover=leftover% 5000;
-        System.out.println(leftover); // 237
-
-        int num1000 =  leftover / 1000;
-        System.out.println(num1000); // 0
-        leftover=leftover% 1000;
-        System.out.println(leftover); // 237
-
-        int num500 =  leftover / 500;
-        System.out.println(num500); // 0
-        leftover=leftover% 500;
-        System.out.println(leftover); // 237
-
-        int num100 =  leftover / 100;
-        System.out.println(num100); // 2
-        leftover=leftover% 100;
-        System.out.println(leftover); // 37
-
-        int num50 =  leftover / 50;
-        System.out.println(num50); // 0
-        leftover=leftover% 50;
-        System.out.println(leftover); // 37
-
-        int num10 =  leftover / 10;
-        System.out.println(num10); // 3
-        leftover=leftover% 10;
-        System.out.println(leftover); // 7
-
-        int num1 =  leftover ;
-        System.out.println(num1); // 7
-
+        int[] dividerArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int dividend = money;
+        for(int i = 0; i<dividerArr.length; i++){
+            int divider = dividerArr[i];
+            answer.add(dividend / divider);
+            int leftover = money % divider;
+            dividend = leftover;
+        }
+        System.out.println(answer); // []
 
         return answer;
     }
