b4422fe961c559a19059d1213f75e89241370d39
YuHayeon <youha0930@gmail.com>
2022-10-28 09:59:05 +0900

    feat(Problem3): 입력이 1미만이거나 10000초과인 경우 예외처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = 0;
+		return answer;
+	}
+
+	static class InputValidator {
+		private static boolean isRightNumberRange(int number) {
+			if (number < 1 || number > 10000) {
+				return false;
+			}
+			return true;
+		}
+	}
 }
