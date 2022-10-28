656ef5e9d8b5bf68f2cfdef515461f1b4778de6b
Kloong1 <dkdlsmsm98@gmail.com>
2022-10-29 02:00:09 +0900

    problem5 - get num of changes

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private final static List<Integer> MONEY_LIST = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> changes = new ArrayList<>(MONEY_LIST.size());
+
+        for (int change : MONEY_LIST) {
+            int numOfChange = money / change;
+            changes.add(numOfChange);
+            money -= change * numOfChange;
+        }
+
+        return changes;
     }
 }
