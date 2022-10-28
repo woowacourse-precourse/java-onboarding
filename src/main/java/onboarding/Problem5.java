dbfa02770afa4a620d413be88036a87d8817fe41
swandevson <shai4956@gmail.com>
2022-10-28 21:35:53 +0900

    feat: P5 화폐 단위 init
    화폐 단위를 설정하고 각 개수를 0개로 초기화한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,23 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
 
 public class Problem5 {
+    public static List<Integer> calcMoneyUnit(int money) {
+        // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
+        List<Integer> moneyUnit = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
+        List<Integer> moneyUnitNum = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        return moneyUnitNum;
+    }
+
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        System.out.println(answer);
+
         return answer;
     }
 }
