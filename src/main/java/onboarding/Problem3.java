5cbb7fd2806ed7ef4a8c3bb1c840914c40e42772
KyungKyu Park <rudrb0505@gmail.com>
2022-10-28 13:54:07 +0900

    feat(Problem3): 자리수마다 숫자를 확인하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,15 +2,22 @@ package onboarding;
 
 public class Problem3 {
 	public static int solution(int number) {
-		int count = 0;
-		int clapCount = 0;
+		int totalClapCount = 0;
+		for (int count = 1; count <= number; count++) {
+			totalClapCount += getClapCount(count);
+		}
+		return totalClapCount;
+	}
 
-		for (count = 1; count <= number; count++) {
-			if (count % 10 == 3 || count % 10 == 6 || count % 10 == 9) {
+	private static int getClapCount(int number) {
+		int clapCount = 0;
+		while (number > 0) {
+			if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
 				clapCount++;
 			}
+			number /= 10;
 		}
-
 		return clapCount;
 	}
+
 }
