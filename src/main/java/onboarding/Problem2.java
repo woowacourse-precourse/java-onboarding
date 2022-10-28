5a162780e8d4b780741d2f15e26e30d12e100381
jihong <clara5091@naver.com>
2022-10-28 19:17:54 +0900

    problem2-연속하는 문자 확인 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Problem2 {
+    private static List<Integer> checkOverload(StringBuffer cryptogram) {
+        return IntStream.range(0, cryptogram.length() - 1)
+                .filter(x -> cryptogram.charAt(x) == cryptogram.charAt(x + 1))
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(cryptogram);
+
+        return "answer";
     }
 }
