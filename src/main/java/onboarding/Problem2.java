e671c2efca8941ade81bd8704252344363ff65ac
JJongmen <pjy90123@gmail.com>
2022-10-28 16:14:22 +0900

    feat(Problem2): 연속되는 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeConsecutiveDuplicates(cryptogram);
         return answer;
+    }
+
+    private static String removeConsecutiveDuplicates(String cryptogram) {
+        return cryptogram.replaceAll("([a-z])\\1+", "");
     }
 }
