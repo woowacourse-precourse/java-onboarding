dd452f97d71f9fdd3c5e09db3b49c1f3582c68aa
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-29 00:54:54 +0900

    Comment: Problem5 구현 기능목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,14 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/**
+ * 구현 기능 목록
+ * 1. greedy를 이용해 최적의 해를 찾도록 함
+ * 2. 돈의 액수를 나열한 배열을 생성한다. {50000, 10000 … 10, 1}
+ * 2. 돈의 액수를 담은 배열을 순회하면서, 변환된 개수를 list에 담는다.
+ * 3. ist를 반환한다.
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
