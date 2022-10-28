5b6773a1bde171ab78726f27d916ca05f03b0f09
junho lee <junho1273@gmail.com>
2022-10-28 15:36:47 +0900

    Problem 5 기능목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+// 기능목록
+// 1.배열에 모든 지폐의 단위 +
+// 2.지폐의 단위별로 나눈 몫을 배열에 추가 후 나머지를 money로 설정
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
