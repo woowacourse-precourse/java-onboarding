fc868c0c4aa6128cdff5fb910bd2b96dfd193aa4
yxnxx <jh4rgh0@gmail.com>
2022-10-28 23:15:41 +0900

    feat: Add Problem2 checking character continuation

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        int last_index = 0;
+        for(int i = 0; i < cryptogram.length(); i++){
+            last_index = isContinuous(cryptogram, i);
+        }
         return answer;
+    }
+    public static int isContinuous(String cryptogram, int index) {
+        int last_index = -1;
+        if (cryptogram.charAt(index) == cryptogram.charAt(index + 1)) last_index = index + 1;
+        return last_index;
     }
 }
