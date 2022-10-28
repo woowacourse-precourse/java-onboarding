b000237c7a5a05a20800cfc515a8bbe660c7b0a8
Dabeen Jeong <hello70825@gmail.com>
2022-10-28 15:27:19 +0900

    feat: [Problem3] 올바른 입력값이 들어오는지 확인하는 기능을 추가하다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        if (checkValidation(number)) {
+
+        } else {
+            answer = -1;
+        }
+
         return answer;
+    }
+
+    private static boolean checkValidation(int inputNumber) {
+        // 1 이상 40,000 이하인 자연수인지 확인하다.
+        if (1 <= inputNumber && inputNumber <= 40000) {
+            return false;
+        }
+
+        return true;
     }
 }
