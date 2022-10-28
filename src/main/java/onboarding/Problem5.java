0e5158173767c45f19a7519d5b2828ffe2ef3c03
sinryuji <siler2474@gmail.com>
2022-10-28 17:55:03 +0900

    Feat: Problem5 complete

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int bill[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int cnt = 0;
+        int i = 0;
+
+        while (money >= 0 && i < bill.length) {
+            if (money >= bill[i]) {
+                money -= bill[i];
+                cnt++;
+            }
+            else {
+                answer.add(cnt);
+                cnt = 0;
+                i++;
+            }
+        }
         return answer;
     }
 }
