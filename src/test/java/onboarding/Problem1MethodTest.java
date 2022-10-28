c7afc9c0a7e793fc495521d844518ff58962aef6
phobyjun <phobyjun@gmail.com>
2022-10-28 17:43:53 +0900

    [PROBLEM1] ADD: FEATURE3. 최대 점수 계산 함수 단위 테스트 코드

--- src/test/java/onboarding/Problem1MethodTest.java
+++ src/test/java/onboarding/Problem1MethodTest.java
@@ -17,10 +17,27 @@ class Problem1MethodTest {
             Method getDigitsFromNumber = problem1.getClass().getDeclaredMethod("getDigitsFromNumber", int.class);
             getDigitsFromNumber.setAccessible(true);
 
-
             int number = 15223442;
             List<Integer> digits = (List<Integer>) getDigitsFromNumber.invoke(problem1, number);
             assertThat(digits).isEqualTo(List.of(2, 4, 4, 3, 2, 2, 5, 1));
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getMaxScoreTest() {
+        Problem1 problem1 = new Problem1();
+        try {
+            Method getMaxScore = problem1.getClass().getDeclaredMethod("getMaxScore", List.class);
+            getMaxScore.setAccessible(true);
+
+            List<Integer> digits1 = List.of(1, 9, 7);
+            List<Integer> digits2 = List.of(2, 1, 1);
+            int maxScore1 = (int) getMaxScore.invoke(problem1, digits1);
+            int maxScore2 = (int) getMaxScore.invoke(problem1, digits2);
+            assertThat(maxScore1).isEqualTo(63);
+            assertThat(maxScore2).isEqualTo(4);
         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
