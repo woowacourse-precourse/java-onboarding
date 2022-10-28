e71472e9e26449f0cc16b984ff44ea111e60e7ee
yxnxx <jh4rgh0@gmail.com>
2022-10-28 23:54:18 +0900

    feat: Add Problem2 finding last index

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,13 +5,17 @@ public class Problem2 {
         String answer = "";
         int last_index = 0;
         for(int i = 0; i < cryptogram.length(); i++){
-            last_index = isContinuous(cryptogram, i);
+            last_index = findLastIndex(cryptogram, i);
         }
         return answer;
     }
-    public static int isContinuous(String cryptogram, int index) {
+    public static int findLastIndex(String cryptogram, int index) {
+        if (cryptogram.charAt(index) != cryptogram.charAt(index + 1)) return -1;
         int last_index = -1;
-        if (cryptogram.charAt(index) == cryptogram.charAt(index + 1)) last_index = index + 1;
-        return last_index;
+        int initial = index;
+        last_index = findLastIndex(cryptogram, index++);
+        // 함수의 반환값이 -1이면 현재 index가 연속된 문자의 마지막 인덱스 값
+        if (last_index != -1) return last_index;
+        return index;
     }
 }
