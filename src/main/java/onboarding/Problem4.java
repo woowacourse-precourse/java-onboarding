cf9e72b19dc58761d12885381ae7d2236b6c08fa
5nam <ohnam90@gmail.com>
2022-10-29 00:31:03 +0900

    feat: 대문자인지, 소문자인지 판단하는 기능 추가 대문자, 소문자, 공백 별로 처리 방식이 다르기 때문에 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -12,4 +12,21 @@ public class Problem4 {
 
         return wordChar;
     }
+
+    // 대문자인지, 소문자인지 판단하는 함수
+    private static char checkUpper(char input) throws IllegalArgumentException {
+        // 대문자일 경우
+        if(input >= 65 && input <= 90 ) {
+            return 'A';
+        }
+        // 소문자일 경우
+        else if(input >= 97 && input <= 122) {
+            return 'a';
+        }
+        else if (input == ' ') {
+            return ' ';
+        }
+        // 대문자도, 소문자도, 공백도 아닐 경우
+        throw new IllegalArgumentException("알파벳 외의 문자는 입력할 수 없습니다.");
+    }
 }
