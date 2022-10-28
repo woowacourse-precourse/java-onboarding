7a1d6aeb8a73c779528ad0ae3fda2779f52b42b1
Lee Min Woo <lmw7414@naver.com>
2022-10-28 14:23:36 +0900

    problem1 - 문제 설명 및 TODO 주석 추가
    문제를 해결하기 전 주석으로 문제를 정리하고 구현해야 할 메서드를 TODO로 작성하였다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,28 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 책을 임의로 펼친다.
+ * 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+ * 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+ * 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+ * 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+ * 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+ *
+ * 포비가 이기면1, 크롱이 이기면 2, 무승부 0, 예외사항 -1
+ *
+ * TESTCASE
+ *   pobi   /   crong   / result
+ * [97, 98] / [197,198] / 0
+ * [131, 132] / [211, 212] / 1
+ * [99, 102]  / [211, 212] / -1
+ */
+
+/**TODO
+ * 각 자리 합을 더하는 메서드
+ * 각 자리를 모두 곱하는 메서드
+ */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
