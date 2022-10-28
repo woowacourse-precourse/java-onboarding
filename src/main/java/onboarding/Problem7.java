0e4082bba07de459b697cbe10196d81de7b90169
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 16:50:29 +0900

    feat : 기준 친구 리스트와 다른 친구리스트를 비교할 수 있는 반복문 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,17 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        for (int i = 0; i < friends.size(); i++) {
+            List friendsList = friends.get(i);
+            for (int j = 0; j < friends.size(); j++) {
+                if (j == i) {
+                } else {
+                    List otherFriendsList = friends.get(j);         // 기준 친구 리스트와 다른 친구리스트를 비교
+                }
+            }
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
