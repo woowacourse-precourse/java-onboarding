1836591f5f4a2c00eac3edc365fe5c5941c34c83
dydtmd6801 <dydtmd6801@naver.com>
2022-10-28 22:54:45 +0900

    feat : 한 글자씩 대문자, 소문자, 공백 체크 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i = 0; i < word.length(); i++) {
+            int check = isCheckCase(word.charAt(i));
+        }
         return answer;
+    }
+
+    private static int isCheckCase(Character checkWord){
+        if(Character.isUpperCase(checkWord)) return 1;
+        if(Character.isLowerCase(checkWord)) return 2;
+        return 0;
     }
 }
