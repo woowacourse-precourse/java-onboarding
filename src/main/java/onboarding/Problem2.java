7f31f4f16a725ec274ded2b251afa8a3df757b81
KyungKyu Park <rudrb0505@gmail.com>
2022-10-28 11:15:18 +0900

    feat(Problem2): 중복된 문자를 찾는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,13 @@ package onboarding;
 
 public class Problem2 {
 	public static String solution(String cryptogram) {
-		char [] arr = cryptogram.toCharArray();
+		char [] cryptogramArray = cryptogram.toCharArray();
+
+		for (int i = 1; i < cryptogramArray.length; i++) {
+			if (cryptogramArray[i] == cryptogramArray[i + 1]) {
+				System.out.println("중복된 문자열: " + cryptogramArray[i] + " " + cryptogramArray[i + 1]);
+			}
+		}
 
 		String answer = "answer";
 		return answer;
