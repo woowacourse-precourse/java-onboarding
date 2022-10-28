846700620ce889802b08abd513c1d9a63970bf4f
KTH-96 <taehyun960@gmail.com>
2022-10-29 00:48:10 +0900

    feat(Problem4): 조건에 맞게 문자변환하는 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,8 +7,23 @@ public class Problem4 {
 	 * 1. 주어진 조건으로 문자 변환
 	 */
 	public static String solution(String word) {
-		String answer = "";
-		return answer;
+		char[] alphabets = word.toCharArray();
+
+		StringBuilder sb = new StringBuilder();
+		for (char alphabet : alphabets) {
+			if (alphabet == 32) {
+				sb.append(' ');
+				continue;
+			}
+			if (alphabet >= 65 && alphabet <= 90) {
+				sb.append((char) (65 + (90 - alphabet)));
+				continue;
+			}
+			if (alphabet >= 97 && alphabet <= 122) {
+				sb.append((char) (97 + (122 - alphabet)));
+			}
+		}
+		return sb.toString();
 	}
 
 }
