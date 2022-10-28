0162e52c54f1a1b902b777685c330ac575c1ecf4
wonseokjee <wsji9404@gmail.com>
2022-10-28 11:55:57 +0900

    221027 prblm1 - 3 페이지별 더하기 곱하기 비교 후 가장 큰 수 고르는 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,17 +5,18 @@ import java.util.List;
 class Problem1 {
 
 
-//    2. Solution에서 한 사람의 페이지 번호를 받는 메서드
-    public static int someone(List<Integer> bookNum) {
-//      페이지List 받아서 String으로 쪼갬
-        String[] leftpage = Integer.toString(bookNum.get(0)).split("");
-        String[] rightpage = Integer.toString(bookNum.get(1)).split("");
+//    3. 2번에서 페이지 한쪽을 String[]으로 받아서 더하기 곱하기 비교.
+//    4. 가장 큰 수를 본인의 점수로 한다.
+    public static int findMax(String[] page) {
+        int plus = 0;
+        int multiply = 1;
+        for (String i: page) {
+            plus += Integer.parseInt(i);
+            multiply = Integer.parseInt(i) * multiply;
+        }
+//        int Fin = Math.max(plus,multiply);
 
-//        int bookMax = Math.max(findMax(leftpage), findMax(rightpage));
-
-        return Math.max(findMax(leftpage), findMax(rightpage));
+        return Math.max(plus,multiply);
     }
-
-
 
 }
