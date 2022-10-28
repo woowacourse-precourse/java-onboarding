fae3103442243ed45a555021c4304bc33b22007c
chanminmaxscaler <jcm04@naver.com>
2022-10-28 21:46:00 +0900

    feat(Problem3): countTargetCharacterInNumber 메소드의 기능을 구현한다
    countTargetCharacterInNumber 메소드는 다음과 같이 기능한다.
    targetCharacter와 number를 입력받는다.
    number를 String 타입으로 변환하여, targetCharacter를 포함하는지 여부를 확인한다.
    targetCharacter를 포함하면 그 개수를 세서 반환한다. 포함하지 않으면 0을 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public int countTargetCharacterInNumber(char targetCharacter, int number) {
+        String stringOfNumber = Integer.toString(number);
+        if (stringOfNumber.contains(Character.toString(targetCharacter))) {
+            return (int)stringOfNumber.chars().filter(x -> x == targetCharacter).count();
+        }
+        return 0;
+    }
 }
