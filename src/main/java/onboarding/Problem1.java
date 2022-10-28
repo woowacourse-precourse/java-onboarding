3de4b3e7f5ee55989d497f3ca4db53682ef72ef3
yj2980 <gum0945@naver.com>
2022-10-28 17:11:30 +0900

    feat(problem1) : null일 경우 예외처리하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,13 +6,15 @@ import java.util.List;
 class Problem1 {
     static final int leftPage = 0;
     static final int rightPage = 1;
-    static final int userListSize = 2;
-
-    static int errorNum = 250;
+    static final int exceptionNum = -1;
+    static int errorNum = 244;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        //null일 경우
+        if (exceptionCheck(pobi) || exceptionCheck(crong))
+            return exceptionNum;
 
         int pobiScore = Math.max(findScore(pobi.get(leftPage)), findScore(pobi.get(rightPage)));
         int crongScore = Math.max(findScore(crong.get(leftPage)), findScore(crong.get(rightPage)));
@@ -26,6 +28,8 @@ class Problem1 {
     public static List<Integer> findPlaceValue(int number) {
         List<Integer> numberList = new ArrayList<>();
 
+        if (number == 0)
+            numberList.add(number);
         while(number > 0)
         {
             numberList.add(number % 10);
@@ -56,12 +60,12 @@ class Problem1 {
         int sumResult = calculation(findPlaceValue(number), '+');
         int multiplicationResult = calculation(findPlaceValue(number), '*');
 
-        return Math.max(sumResult, multiplicationResult);
+        return Math.max(sumResult,  exceptionCheck(multiplicationResult));
     }
 
     //점수 비교 후 결과 반환
     public static int compareNum(int pobiNum, int crongNum) {
-        if (pobiNum == errorNum || crongNum == errorNum)
+        if (pobiNum >= errorNum || crongNum >= errorNum)
             return -1;
         if (pobiNum == crongNum)
             return 0;
@@ -73,9 +77,16 @@ class Problem1 {
     }
 
     //연산한 값에 0이 있을 경우
-    public static int exceptionCheck(int multiplyNum) {
-        if (multiplyNum == 0)
+    public static int exceptionCheck(int num) {
+        if (num >= 1)
             return errorNum;
-        return multiplyNum;
+        return num;
+    }
+
+    //size가 없을 경우
+    public static boolean exceptionCheck(List<Integer> numList) {
+        if (numList.size() == 0)
+            return true;
+        return false;
     }
 }
--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -3,6 +3,7 @@ package onboarding;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
