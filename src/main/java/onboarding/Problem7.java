541410b3694c93affe92f1dffd63af9dcdfdb034
Dongmin Kim <kimdongmin68@naver.com>
2022-10-29 03:24:24 +0900

    feat: Add sort method & Fix get friend score method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,19 +3,42 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
+    static final int MAXRECOMMEND = 5;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new LinkedList<>();
         List<String> alreadyFriends;
         Map<String, Integer> recommendFriend = new HashMap<>();
+        List<Map.Entry<String,Integer>> entryRecomFirends;
+
 
         alreadyFriends = getFriend(user, friends);
         getFriendScore(user, alreadyFriends, friends, recommendFriend);
+        getVisitScore(alreadyFriends, visitors,recommendFriend);
+        entryRecomFirends = sortByScoreAndName(recommendFriend);
 
+        for(int idx = 0; idx<entryRecomFirends.size(); idx++){
+            if(idx == MAXRECOMMEND) break;
+            String recomFriend = entryRecomFirends.get(idx).getKey();
+            answer.add(recomFriend);
+        }
         return answer;
     }
 
-    public static void getVisitScore(List<String> visitors, Map<String, Integer> recommendFriend){
+    public static List<Map.Entry<String,Integer>> sortByScoreAndName(Map<String, Integer> recommendFriend){
+        List<Map.Entry<String, Integer>> entryRecomFriends = new LinkedList<>(recommendFriend.entrySet());
+        Collections.sort(entryRecomFriends, (o1, o2) -> {
+            if(o2.getValue() > o1.getValue()) return 1;
+            else if(o1.getValue() == o2.getValue()){
+                return o1.getKey().compareTo(o2.getKey());
+            }
+            return -1;
+        });
+        return entryRecomFriends;
+    }
+
+    public static void getVisitScore(List<String> alreadyFriends, List<String> visitors, Map<String, Integer> recommendFriend){
         for(String visitor : visitors){
+            if(!alreadyFriends.contains(visitor))
             recommendFriend.put(visitor, recommendFriend.getOrDefault(visitor,0)+1);
         }
     }
@@ -26,8 +49,10 @@ public class Problem7 {
                 String firstHuman = friend.get(0);
                 String secondHuman = friend.get(1);
                 if(!(user.equals(firstHuman)||user.equals(secondHuman))) {
-                    if (alreadyFriend.equals(firstHuman)) recommendFriend.put(secondHuman,10);
-                    if (alreadyFriend.equals(secondHuman)) recommendFriend.put(firstHuman,10);
+                    if (alreadyFriend.equals(firstHuman) && !alreadyFriends.contains(secondHuman))
+                        recommendFriend.put(secondHuman,10);
+                    if (alreadyFriend.equals(secondHuman) && !alreadyFriends.contains(firstHuman))
+                        recommendFriend.put(firstHuman,10);
                 }
             }
         }
