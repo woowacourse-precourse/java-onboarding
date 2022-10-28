e99e695a39aa3bfc1ea1094de032b0166438971d
Lee Min Woo <lmw7414@naver.com>
2022-10-28 18:16:21 +0900

    problem1 - 최종 테스트 마무리
    테스트 케이스가 돌아가는 것을 확인하였고, 불필요한 주석과 메인 메서드를 삭제하였다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,49 +1,18 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 책을 임의로 펼친다.
- * 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
- * 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
- * 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
- * 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
- * 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
- *
- * 포비가 이기면1, 크롱이 이기면 2, 무승부 0, 예외사항 -1
- *
- * TESTCASE
- * pobi   /   crong   / result
- * [97, 98] / [197,198] / 0
- * [131, 132] / [211, 212] / 1
- * [99, 102]  / [211, 212] / -1
- */
-
-/**TODO
+ * 구현할 메서드
  * 각 자리 합을 더하는 메서드
  * 각 자리를 모두 곱하는 메서드
  * 합과 곱 중 가장 큰 값을 리턴
  * 예외 처리
  * * 페이지가 1보다 작거나 400보다 큰 경우
  * * 왼쪽과 오른쪽 페이지의 번호의 차가 1인경우
- * *
  */
 
 class Problem1 {
-
-    public static void main(String[] args) {
-        ArrayList<Integer> pobi = new ArrayList<>();
-        ArrayList<Integer> crong = new ArrayList<>();
-
-        pobi.add(4);
-        pobi.add(5);
-
-        crong.add(397);
-        crong.add(398);
-
-        System.out.println(solution(pobi, crong));
-    }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
@@ -70,7 +39,7 @@ class Problem1 {
     }
 
     public static int addCalc(int num) {
-        
+
         String numToString = String.valueOf(num);
         int result = 0;
         for (int i = 0; i < numToString.length(); i++) {
@@ -93,12 +62,12 @@ class Problem1 {
 
     static boolean validation(List<Integer> list) {
 
-        int firstPage =list.get(0);
+        int firstPage = list.get(0);
         int secondPage = list.get(1);
 
-        if(firstPage < 3 || firstPage > 398 || secondPage < 3 || secondPage > 398 )
+        if (firstPage < 3 || firstPage > 398 || secondPage < 3 || secondPage > 398)
             return false;
-        if(firstPage % 2 != 1 || secondPage % 2 !=0)
+        if (firstPage % 2 != 1 || secondPage % 2 != 0)
             return false;
         if (firstPage - secondPage != -1)
             return false;
