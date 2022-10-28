6df9f23118c68a106daf9f6daec4f103c74a5cee
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-28 21:57:56 +0900

    [Problem1] feat: getScore구현. 자리수 합과 곱 구하는 메서드 선언
    - 페이지 자릿수들의 합을 계산하는 메서드 int sumOfPageNum(List<Integer> pageNum) 프로토타입 선언
    - 페이지 자릿수들의 곱을 계산하는 메서드 int multipleOfPageNum(List<Integer> pageNum) 프로토타입 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Problem1 {
 
@@ -37,20 +39,44 @@ class Problem1 {
      */
     private static int getScore(List<Integer> pages) {
         int score = null;
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
 
         // 각 페이지 숫자의 자리 합과 곱(4개)을 저장할 배열 scores 선언
+        List<Integer> scores = ArrayList<Integer>();
+
         // pageList 순회하며 배열 scores에 요소의 합과 곱을 저장. List<Integer> pageNumToList(int page)
+        List<Integer> leftPageNum = pageNumToList(leftPage);
+        List<Integer> rightPageNum = pageNumToList(rightPage);
+
+        // pageNum(페이지의 자릿수들)의 합과 곱을 계산하는 메서드를 사용해 return값을 scores에 추가
+        scores.add(sumOfPageNum(leftPageNum));
+        scores.add(multipleOfPageNum(leftPageNum));
+        scores.add(sumOfPageNum(rightPageNum));
+        scores.add(multipleOfPageNum(rightPageNum));
+
         // 배열 scores 요소 중 가장 큰 값을 score에 저장
+        score = Collections.max(scores);
 
         return score
     }
-    
+
+    /*
+    페이지 자릿수들의 합을 계산하는 메서드
+     */
+    private static int sumOfPageNum(List<Integer> pageNum);
+
+    /*
+    페이지 자릿수들의 곱을 계산하는 메서드
+     */
+    private static int multipleOfPageNum(List<Integer> pageNum);
+
     /*
     특정 페이지 번호의 각 자리 숫자를 리스트에 저장하여 반환하는 메서드
     각 숫자를 더하거나 곱하므로 리스트 배치 순서를 고려하지 않아도 됨
      */
     private static List<Integer> pageNumToList(int page) {
-        List<Integer> pageNum = List<Integer>();
+        List<Integer> pageNum = ArrayList<Integer>();
         while(page > 0) {
             pageNum.add(page % 10);
             page = (int)(page / 10);
@@ -63,8 +89,8 @@ class Problem1 {
     유효할 시 true, 유효하지 않을 시 false
      */
     private static boolean isVaild(List<Integer> pages) {
-        leftPage = pages.get(0);
-        rightPage = pages.get(1);
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
             // 왼쪽 페이지가 홀수이고, 오른페이지가 짝수이고, 페이지 수가 2이고, 첫 장(1, 2)이나 마지막 장(399, 400)이 아니면 true
         return leftPage % 2 == 1 && rightPage % 2 == 0 && pages.size() == 2 && leftPage != 1 && rightPage != 400;
     }
