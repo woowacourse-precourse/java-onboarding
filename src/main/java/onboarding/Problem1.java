67b9127a60487be694ca922cbb1be97ddc97c206
whale0928 <rlagusrl928@gmail.com>
2022-10-28 18:49:05 +0900

    1번 문제 해결
    세상세상세상.....
    재귀함수나 경우의 수를 고려해야하는 문제인줄 알고
    관련 메서드를 엄청 구현했는데
    
    문제 지문에 " 모둔 곱하거나 모두 더하거나 "
    라고 명시 되있었다...
    
    문제를 제대로 읽자....

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,46 +13,41 @@ class Problem1 {
      * @return
      */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        /*포비와 크롱의 승부
-          파라미터는 각각 펼친 페이지 수.
-          0 = 왼쪽편의 페이지 수 , 1= 오른쪽의 페이지 수
-
-           각각 자리수를 연산해 가장 큰 수를 구해야 한다.
-           재귀함수를 사용해야 할듯하다.
-
-          , 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0,
-        */
 
         int pobisNumber;
         int crongsNumber;
         try {
             pobisNumber = findMaxValue(pobi);
             crongsNumber = findMaxValue(crong);
-            System.out.println("pobisNumber = " + pobisNumber);
-            System.out.println("crongsNumber = " + crongsNumber);
         } catch (Exception e) {
             return -1;
         }
-        return pobisNumber > crongsNumber?1:pobisNumber < crongsNumber?2:0;
+        System.out.println(pobisNumber+"////"+crongsNumber);
+        return pobisNumber>crongsNumber?1:(pobisNumber==crongsNumber?0:2);
     }
 
     public static int findMaxValue(List<Integer>list)throws Exception{
+        if((list.get(1)-list.get(0))>1){
+            throw new Exception();
+        }
         List<Integer> left =Arrays.stream(String.valueOf(list.get(0)).split("")).map(Integer::parseInt).collect(Collectors.toList());
         List<Integer> right =Arrays.stream(String.valueOf(list.get(1)).split("")).map(Integer::parseInt).collect(Collectors.toList());
 
-//        원래라면 재귀를 쓰겟지만 문제에서 400 페이지 미만이라는 조건을 주었기 때문에 각각의 경우를 만든다.
-//        재귀를 이용 할 수 있을 것 같다!!
-//        int leftMax = recursive(list,"",0);
-//        int rightMax = recursive(right,"",0);
-//        포기 게속 하면 찾을거 같은데 더 이상 시간을 투자하게 되면 힘들듯.
+        int leftMult = 1;
+        int leftPlus = 0;
+        int rightMult = 1;
+        int rightPlus = 0;
 
-        int leftMax = 1;
-        int rightMax = 1;
+        for (Integer i : left){
+            leftMult = leftMult * i;
+            leftPlus = leftPlus + i;
+        }
+        for (Integer i : right){
+            rightMult = rightMult * i;
+            rightPlus = rightPlus + i;
+        }
 
-        for (Integer i : left) leftMax = Math.max((leftMax * i), (leftMax + i));
-        for (Integer i : right) rightMax = Math.max((rightMax * i), (rightMax + i));
-
-        return Math.max(leftMax,rightMax);
+        return Math.max(Math.max(leftMult,leftPlus),Math.max(rightMult,rightPlus));
     }
 
     public static int recursive(List<Integer> list,String simpol,int result){
