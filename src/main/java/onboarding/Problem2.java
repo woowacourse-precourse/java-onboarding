35461bbce0a0faf3ccc95d97287d832594d1448b
CMHyuk <alsgur9710@naver.com>
2022-10-28 20:06:39 +0900

    feat: 중복 문자 제거 로직

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        Stack<Character> stack = checkDuplicate(cryptogram);
         return answer;
+    }
+
+    private static Stack<Character> checkDuplicate(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        stack.push(cryptogram.charAt(0));
+        char before = stack.peek();
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            char after = cryptogram.charAt(i);
+            if (before == after) {
+                stack.pop();
+                if(!stack.isEmpty()) {
+                    before = stack.peek();
+                }
+            } else {
+                stack.push(cryptogram.charAt(i));
+                before = cryptogram.charAt(i);
+            }
+        }
+        return stack;
     }
 }
