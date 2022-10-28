8b6610e98a3b06fb5e2b91c79a30c8891d77e7e5
Eunkeee <eunkeeeee@gmail.com>
2022-10-28 19:57:38 +0900

    ✨[P6] feat: 전체 nickname을 담은 set 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,19 +8,15 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
-        HashSet<Integer> index = new HashSet();
-        List<List<String>> nickSplitArr = new ArrayList<>();
-
-        for(int i=0; i < forms.size();i++){
-            ArrayList<String> nickSplit = new ArrayList();
-            String nickname = forms.get(i).get(1);
-            System.out.println(nickname);
-            for(int k=0; k<nickname.length()-1;k++){
-                nickSplit.add(nickname.substring(k,k+2));
-            }
-            nickSplitArr.add(nickSplit);
+        // 1. 전체 nickname을 담은 set 생성
+        HashSet<String> totalNicks = new HashSet<>();
+        for(List<String> info: forms){
+            String nickname = info.get(1);
+            totalNicks.add(nickname);
         }
-        System.out.println(nickSplitArr);
+
+
+
         return answer;
     }
 }
