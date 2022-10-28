2a37702fc70b6777aa228ed2518e71453e4048d3
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2022-10-28 15:38:47 +0900

    기능 요구사항 및 필요해보이는 기능 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 
 class Problem1 {
+
+    static ArrayList<Integer> pars(Integer num){
+        //각 자릿수 숫자를 List에 담아 반환
+        return new ArrayList<>();
+    }
+    static int cal(ArrayList<Integer> parsed_num){
+        //나눠진 수를 2 or 3번 규칙에 맞게 계산
+        return 0;
+    }
+    static int larger(int num1, int num2){
+        //더 큰 수를 반환
+        return 0;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
