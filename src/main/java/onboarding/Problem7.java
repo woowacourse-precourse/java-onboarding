bfc2783a53abd49ca2c3a60042761b7b15532d8e
devmizz <garamminchan@gmail.com>
2022-10-29 00:16:21 +0900

    problem7: user class

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    private static class User {
+        private String username;
+        private List<User> friends;
+
+        public String getUsername() {
+            return this.username;
+        }
+
+        private List<User> getFriends() {
+            return this.friends;
+        }
+
+        public User(String username) {
+            isValidUsername(username);
+            this.username = username;
+            this.friends = new ArrayList<>();
+        }
+
+        private void isValidUsername(String username) {
+            int length = username.length();
+            if(length < 1 || 30 < length) {
+                throw new IllegalArgumentException("닉네임의 길이가 적절치 않습니다.");
+            }
+        }
     }
 }
