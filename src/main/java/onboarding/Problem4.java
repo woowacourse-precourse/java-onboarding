c88662161855be96535c680eeddd5bf4340c0f07
Jin409 <sualng123@gmail.com>
2022-10-28 14:41:15 +0900

    feat(problem4): 청개구리 사전에 맞는 알파벳 반환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            getChar(c);
+        }
+        return " "; // 임시
+    }
+
+    private static char getChar(char c) {
+        if (Character.isUpperCase(c)) {
+            return (char) ('Z' + 'A' - c);
+        }
+        if (Character.isLowerCase(c)) {
+            return (char) ('z' + 'a' - c);
+        }
+        return c;
     }
 }
