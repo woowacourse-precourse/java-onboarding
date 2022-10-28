ff2a23369aaaca972cb7bd2cd3b4f6834d1fd523
nswon <swnam0729@gmail.com>
2022-10-29 02:12:27 +0900

    feat(Problem5): 계좌 돈을 출금하는 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
+    private static final List<Integer> moneys = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getAccountMoneyInRange(money);
+    }
+
+    private static List<Integer> getAccountMoneyInRange(int money) {
+        int[] arr = new int[moneys.size()];
+
+        for(int i=0; i<moneys.size(); i++) {
+            arr[i] += money / moneys.get(i);
+            money %= moneys.get(i);
+        }
+        return Arrays.stream(arr).boxed().collect(Collectors.toList());
     }
 }
