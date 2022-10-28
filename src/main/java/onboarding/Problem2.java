509efdceeb9288ee17a98bec77921ad7e0634795
ykmxxi <ykmxxi97@gmail.com>
2022-10-28 18:27:37 +0900

    feat: (Problem2)String을 StringBuilder로 변환하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,9 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static StringBuilder changeToStringBuilder(String crpytogram) {
+        StringBuilder stringBuilder = new StringBuilder(crpytogram);
+        return stringBuilder;
+    }
 }
