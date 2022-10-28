f52965418d2b485726f63e4e3ca3eee0422d4324
cookienc <kim76744@naver.com>
2022-10-28 12:02:17 +0900

    feat(Problem2): Problem2 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        char start = 'a';
+        char end = 'z';
+
+        while (true) {
+            int length = cryptogram.length();
+            for (char c = start; c <= end; c++) {
+                StringBuilder duplicateAlphabet = new StringBuilder();
+                duplicateAlphabet.append(c).append("{2,}");
+                cryptogram = cryptogram.replaceAll(duplicateAlphabet.toString(), "");
+            }
+
+            if (cryptogram.length() == length) {
+                break;
+            }
+        }
+
+        return cryptogram;
     }
 }
