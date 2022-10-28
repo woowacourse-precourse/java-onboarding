b21cfe181861a899f217b8e8c7f338dec1489dde
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-28 14:08:54 +0900

    refactor: Problem3 함수 분리를 통해 가독성 증가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,18 +5,23 @@ package onboarding;
  */
 public class Problem3 {
     public static int solution(int number) {
+        int answer = 0;
+        answer = getClap(number);
+
+        return answer;
+    }
+
+    public static int getClap(int number) {
         int cnt = 0;
-        for(int i = 1; i<= number; i++) {
+        for (int i = 1; i <= number; i++) {
             int current = i;
-            while(current != 0) {
+            while (current != 0) {
                 if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
                     cnt++;
                 }
                 current /= 10;
             }
         }
-
-
         return cnt;
     }
 }
