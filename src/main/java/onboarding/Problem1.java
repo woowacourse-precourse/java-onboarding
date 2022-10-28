fefb5f730a772a3d1418ade4e894dd8656a68d48
sun007021 <sun007021@gmail.com>
2022-10-28 18:44:13 +0900

    feat: Player class 생성 및 set, get함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,32 @@ package onboarding;
 
 import java.util.List;
 
+class Player {
+    private int maxNum;
+    private int leftPageNum, rightPageNum;
+
+    public void setLeftPageNum(int leftPageNum) {
+        this.leftPageNum = leftPageNum;
+    }
+
+    public void setRightPageNum(int rightPageNum) {
+        this.rightPageNum = rightPageNum;
+    }
+
+    public int getMaxNum() {
+        return maxNum;
+    }
+}
+
+class PageException extends RuntimeException {
+}
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    //game 에서 player 마다 값 넣어줌
+    // game 에서 값 넣어주기 전에 예외처리 검사
+
 }
