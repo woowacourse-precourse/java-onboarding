e0631d92f4c6f2ed347b1ea5041f37bfef552c07
Polite-Jehu <*****@***.com>
2022-10-28 20:26:12 +0900

    기능 3 완료 반환 : 박수친 횟수 =============== 기능흐름 3부터 박수횟수를 숫자를 자릿수로 나누면서 1개씩 추가한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=3;i<=number;i++)
+        {
+            int subNum=i;
+            while(subNum>0)
+            {
+                int check=subNum%10;
+                if(check==3||check==6||check==9) {
+                    answer++;
+                }
+                subNum=subNum/10;
+            }
+        }
         return answer;
     }
 }
