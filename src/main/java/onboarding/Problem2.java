3448c99016294714823afb3ffa6172969266fa60
ymkim97 <rladudaud97@naver.com>
2022-10-28 15:47:08 +0900

    feat: add function capital exception

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,12 +7,28 @@ public class Problem2 {
             return "failed with boundary exception";
         }
 
+        if (throwCapitalException(cryptogram)) {
+            return "failed with capital exception";
+        }
+
         return cryptogram;
     }
 
     public static Boolean throwBoundaryException(String cryptogram) {
         if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
             return true;
+        }
+        return false;
+    }
+
+    public static Boolean throwCapitalException(String cryptogram) {
+        char[] charArray = cryptogram.toCharArray();
+        int index;
+
+        for (index = 0; index < charArray.length; index++) {
+            if (Character.isUpperCase(charArray[index])) {
+                return true;
+            }
         }
         return false;
     }
