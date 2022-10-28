a5c22d7736c7557b54f990f18e4a1272104a5c3f
Hojinida <opqrs1103@gmail.com>
2022-10-29 07:47:12 +0900

    feat: 문제 5번 money와 변환되는 value값(ex:50000)을 대입했을 때 몫은 결과값을 담은 리스트에 저장하고 나머지를 반환해주는 기능추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,18 @@ public class Problem5 {
     private static final List<Integer> values =Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
+        int index=0;
+        while (money!=0 && index < values.size()){
+            money=calculate(answer,money,values.get(index));
+            index++;
+        }
         return answer;
+    }
+    public static int calculate(List<Integer> answer,int money,int value){
+        if(money>=value)
+            answer.add(money/value);
+        if(money<value)
+            answer.add(0);
+        return money%value;
     }
 }
