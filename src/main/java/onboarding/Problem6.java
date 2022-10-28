5e833e853ad6e2ab7aca39179d32bfb2dbf240ce
woosung1223 <dntjd991223@naver.com>
2022-10-28 15:36:28 +0900

    feat(Problem6): splitLexim 함수 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+    static final HashMap<String, String> hm = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = Collections.emptyList();
         return answer;
+    }
+    public static void splitLexim(List<String> repository, String name) {
+        for (int j = 0; j < name.length()-1; j++) {
+            repository.add(name.substring(j, j+2));
+        }
     }
 }
