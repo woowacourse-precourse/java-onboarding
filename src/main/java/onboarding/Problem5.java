24f69a40b94a95d212cd435ea840ca32129ecdb8
Reznov <reznov03048@gmail.com>
2022-10-28 21:06:05 +0900

    solution 메소드 내부 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,21 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
 
         int[] unit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         // 큰 화폐 단위 순으로 얻을 수 있는지 확인하며 값 구함
         int unitIdx = 0;
         while(money > 0){
             int nowUnit = unit[unitIdx];
+            if(checkUnit(money, nowUnit)){
+                int unitAns = money / nowUnit;
+                answer.add(unitAns);
 
+                money %= nowUnit * unitAns;
+            } else {
+                answer.add(0);
+            }
 
+            unitIdx++;
         }
 
+        while (answer.size() < 9){
+            answer.add(0);
+        }
 
         return answer;
     }
