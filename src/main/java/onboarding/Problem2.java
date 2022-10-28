87522ddf92d184fc29a7883de2e47e33df2f925d
chanminmaxscaler <jcm04@naver.com>
2022-10-28 16:09:32 +0900

    feat(Problem2): solution 메소드의 기능을 구현한다
    solution 메소드는 다음과 같이 기능한다.
    문자열을 입력받는다.
    Problem2 객체를 생성한다.
    입력받은 문자열에 연속으로 중복되는 문자가 있는지 확인하고, 해당 문자를 지운다.
    이 과정을 문자열에 연속으로 중복되는 문자가 없을 때까지 진행한다.
    중복을 지운 문자열을 반환한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,16 +1,23 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
 
+    public String regularExpressionOfConsecutiveDuplicateCharactersForMatch = "(.*)(\\w)\\2{1,}(.*)";
+    public String regularExpressionOfConsecutiveDuplicateCharacters = "(\\w)\\1{1,}";
     public boolean isStringContainingRegularExpression(String string, String regularExpression) {
         return string.matches(regularExpression);
     }
 
     public String deleteStringByRegularExpression(String string, String regularExpression) {
         return string.replaceAll(regularExpression, "");
+    }
+    public static String solution(String cryptogram) {
+
+        Problem2 problem2 = new Problem2();
+
+        while (problem2.isStringContainingRegularExpression(cryptogram, problem2.regularExpressionOfConsecutiveDuplicateCharactersForMatch)) {
+            cryptogram = problem2.deleteStringByRegularExpression(cryptogram, problem2.regularExpressionOfConsecutiveDuplicateCharacters);
+        }
+        return cryptogram;
     }
 }
