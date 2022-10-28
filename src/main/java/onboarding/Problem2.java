4cc7e104dd72880c7fbf6d3d6d539ccd41941938
Dasol Jung <greteljds0251@gmail.com>
2022-10-28 19:26:13 +0900

    feat: 2-1. Delete consecutive duplicate characters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,29 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
     }
+
+    // 중복 문자 확인, 제거
+    public static int chackSameChar(String cryptogram) {
+        char c = NULL;
+        int count = 0;
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (c == cryptogram.charAt(i)) {
+                cryptogram = cryptogram.substring(0, i-1) + cryptogram.substring(i);
+                count++;
+            }
+            c = cryptogram.charAt(i);
+        }
+        return count;
+    }
 }
+
+/*
+기능
+1. 중복 문자 저장
+2. 중복 문자 제거
+3. 반복
+4. 중복 문자 없을 시 스탑
+ */
