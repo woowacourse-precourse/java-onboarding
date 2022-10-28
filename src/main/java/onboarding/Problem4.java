81b7677b71fefab90508b4b38d3d0a86e1f8f8b6
Hojinida <opqrs1103@gmail.com>
2022-10-29 04:14:27 +0900

    feat: 문제 4번 대문자, 소문자, 둘다아닌 것으로 구분하여 반환하는 checkLowerUpper 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class Problem4 {
     private static final HashMap<Character, Character> upperConvert=new HashMap<>();
     private static final HashMap<Character, Character> lowerConvert=new HashMap<>();
+    private static final int UPPER=1;
+    private static final int LOWER=2;
+    private static final int NOTTHING=0;
     public static String solution(String word) {
         setConvert();
         return null;
@@ -15,5 +18,13 @@ public class Problem4 {
             upperConvert.put((char)(i+65),(char)(90-i));
             lowerConvert.put((char)(i+97),(char)(122-i));
         }
+    }
+
+    public static int checkLowerUpper(char c){
+        if(Character.isUpperCase(c))
+            return UPPER;
+        if(Character.isLowerCase(c))
+            return LOWER;
+        return NOTTHING;
     }
 }
