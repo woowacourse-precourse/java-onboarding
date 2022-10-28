5ec1f981118235cd55247ada7567296bfc83e229
ywj9811 <ywj9811@naver.com>
2022-10-29 00:12:19 +0900

    돈을 각 단위별로 배열에 담아서 반복문을 진행할 것이다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,8 +8,7 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-
-
+        int moneyList[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         return answer;
     }
 }
