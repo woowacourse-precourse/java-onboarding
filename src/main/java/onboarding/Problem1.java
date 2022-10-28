c52e26d5a0d09c4a55655bbfe3f59d58475e918a
채우석 <wochae@wochae-MacBookAir.local>
2022-10-28 21:00:01 +0900

    [feat] 위배 조건에 따라 반환하는 유효성검사 함수 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,24 @@
 package onboarding;
 
 import java.util.List;
-
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(validation(pobi) || validation(crong)) {
+            return -1;
+        }
         return answer;
     }
+    public static boolean validation(List<Integer> pages) {
+        if (pages.get(0) + 1 != pages.get(1)) {
+            return false;
+        }
+        if (pages.get(0) % 2 == 0 && pages.get(1) % 2 == 1) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
