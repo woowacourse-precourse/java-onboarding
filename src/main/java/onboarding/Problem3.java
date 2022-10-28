a5b7f6dd82b109a6e24d00517670a6e58479dfcc
김성훈 <kimseonghun@gimseonghun-ui-MacBookAir.local>
2022-10-28 13:07:57 +0900

    feat : 손뼉을 몇 번 쳐야하는지 구하는 기능 메소드 추가
    숫자 하나를 파라미터로 받아서 해당 숫자는 손뼉을 몇 번 치는지
    계산하여 반환하는 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int getClappingCount(int number) {
+        int clappingCount = 0;
+        while(number != 0) {
+            if(number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                clappingCount += 1;
+            }
+            number /= 10;
+        }
+        return clappingCount;
+    }
 }
