0a626900c84e50620ed769b26d6bbebcc461aca7
footdev <ekclstkfka5@gmail.com>
2022-10-29 01:26:51 +0900

    feat: 기능4 메소드 명세 및 solution 메소드 작성
    기능 구현 목록을 토대로 명세 작성 및 solution 메소드 내부 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    private static Map<Character, Character> dictionary;
+
     public static String solution(String word) {
-        String answer = "";
+        dictionary = makeDictionary();
+        String answer = translate(word);
         return answer;
+    }
+
+    public static Map<Character, Character> makeDictionary() {
+        Map<Character, Character> dictionary = new HashMap<>();
+        return dictionary;
+    }
+
+    public static String translate(String word) {
+        return "";
     }
 }
