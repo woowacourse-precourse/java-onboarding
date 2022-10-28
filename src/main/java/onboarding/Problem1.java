2ccf46ef18e736721994907c29dbf025b4d1a2fe
yxnxx <jh4rgh0@gmail.com>
2022-10-28 18:31:48 +0900

    refactor: Fix page range

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,7 +16,7 @@ class Problem1 {
         return answer;
     }
     public static boolean isPageInvalid(List<Integer> array) {
-        return (array.get(0) < 1) || (array.get(0) > 400) || (array.get(1) < 1) || (array.get(1) > 400);
+        return (array.get(0) <= 1) || (array.get(0) >= 400) || (array.get(1) <= 1) || (array.get(1) >= 400);
     }
     public static boolean isPageNotContinuous(List<Integer> array) {
         return array.get(0) != array.get(1) -1;
