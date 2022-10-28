75faaa56631348dab562c9c989e6246d0a5161ba
gyuturn <rbtjrrbals@gmail.com>
2022-10-28 23:22:18 +0900

    feat: Problem1-solution 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,21 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        //예외처리
+        if(!isIncludePage(pobi)) return -1;
+        if(!isIncludePage(crong)) return -1;
+
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
+        int pobiMaxNum = Math.max(calMaxPageNum(pobiLeftPage), calMaxPageNum(pobiRightPage));
+        int crongMaxNum = Math.max(calMaxPageNum(crongLeftPage), calMaxPageNum(crongRightPage));
+
+        if(pobiMaxNum>crongMaxNum) return 1;
+        else if(pobiMaxNum<crongMaxNum) return 2;
+        else return 0;
     }
     private static int calMaxPageNum(int page) {
         String pageToString = String.valueOf(page);
