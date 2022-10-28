92d3813d0f374705e1f17b551942ec72edba70c7
sugamypapa <sk000801@naver.com>
2022-10-28 19:04:44 +0900

    feat: 문제 5 닉네임 쪼개는 함수와 배열 간의 공통 값 찾는 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,7 +9,29 @@ public class Problem6 {
     // 두 배열씩 비교하여 겹치는 값이 있는지 확인하는 함수 생성
     // 겹치는 배열이 존재하는 아이디만 정렬 후 반환
 
+    public static List<String> nameDivideTwo(String s) {
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.length() % 2 == 1 && i == s.length()-1) {
+                list.add(String.valueOf(s.charAt(i)));
+            }
+            list.add(String.valueOf(s.charAt(i)+s.charAt(i+1)));
+        }
+        return list;
+    }
 
+    public static boolean hasCommon(List<String> one, List<String> two) {
+        boolean hasCommon = false;
+        for (String s_one : one) {
+            for (String s_two : two) {
+                if (s_one.contains(s_two)) {
+                    hasCommon = true;
+                    break;
+                }
+            }
+        }
+        return hasCommon;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
