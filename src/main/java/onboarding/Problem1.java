933a526c7af73ec71ef797b1a5efefa973d1b44c
김태용 <amico741@naver.com>
2022-10-28 16:19:53 +0900

    feat: 예외 처리 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = -1;
+        if(validation(pobi, crong)){
+            
+        }
         return answer;
+    }
+    public static boolean validation(List<Integer> array1, List<Integer> array2){
+        //페이지 차이가 1인지 아닌지 확인하기 위한 변수
+        int pobi = Math.abs(array1.get(0) - array1.get(1));
+        int crong = Math.abs(array2.get(0) - array2.get(1));
+
+        if(array1.get(0) == 1 || array1.get(0) == 399 || array2.get(0) == 1 || array2.get(1) == 399){ // 시작면이나 마지막면일 때, 두 페이지의 차가 2이상일 때 return false
+            return false;
+        } else return pobi <= 1 && crong <= 1;
     }
 }
