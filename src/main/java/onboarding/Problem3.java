bea1b1c60aaee937a0bc9cc8a1b3c5022926a501
leewoojin <zmfpdl64@naver.com>
2022-10-28 14:06:47 +0900

    문제 3번 이우진 제출합니다.
    문제: 숫자 3, 6, 9 가 있다면 그 횟수에 맞춰 박수를 쳐야한다.
    
    문제해결:
    SOL1: 숫자를 문자열로 쪼개서 3, 6, 9가 나올 때마다 count를 증가시킨다.
    
    SOL2: 숫자를 10으로 나눈 나머지를 3, 6, 9가 나오는 만큼 count를 증가시킨다.
    
    2번으로 문제를 해결했음
    1. for문을 통해 박수를 치기 시작하는 3부터 n까지 반복문을 설정하고
    
    2. for문 안에 while문을 이용하여 정수를 자릿수 마다 3, 6, 9가 포함되어 있는지 확인하고 count를 늘리는 방식으로 구현했다.
    
    1번 해결방법과 2번 해결방법을 이용하여 비교해 보니
    약 4배정도 2번 해결방법이 빨랐습니다.
    input: 13000000
    결과:
    
    26400001
    PT0.8167287S
    
    26400001
    PT0.2249884S

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,55 @@
 package onboarding;
 
+
+//SOL 1. 숫자를 문자열로 쪼개서 3, 6 9가 나올때마다 count를 증가시킨다.
+//SOL 2. 숫자를 10으로 나눈 나머지를 3, 6 9 가 나오는 만큼 count를 증가시킨다.
+
+import net.bytebuddy.asm.Advice;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+// 2번 채택
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = 0 ;
+
+        for(int i = 3; i <= number; i++){
+            int num = i;
+            while(num > 0){     //수
+                int mod = num % 10;
+                if(mod == 3 || mod == 6 || mod == 9)
+                    answer +=  1;
+                num /= 10;
+            }
+        }
         return answer;
+    }
+    public static int solution2(int number) {
+        int answer = 0;
+
+        for(int i = 3; i <= number; i++){
+            String s = String.valueOf(i);
+            for(int j = 0; j < s.length(); j++){
+                Character c = s.charAt(j);
+                if(c.equals('3') || c.equals('6') || c.equals('9'))
+                    answer+=1;
+            }
+        }
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime start = LocalDateTime.now();
+        System.out.println(solution(13000000));
+        LocalDateTime end = LocalDateTime.now();
+        System.out.println(Duration.between(start, end));
+
+        LocalDateTime start2 = LocalDateTime.now();
+        System.out.println(solution2(13000000));
+        LocalDateTime end2 = LocalDateTime.now();
+        System.out.println(Duration.between(start2, end2));
     }
 }
