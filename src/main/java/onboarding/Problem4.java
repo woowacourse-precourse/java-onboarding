93786572ce61dc970804ab598b8191a0f74eb362
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-29 03:18:06 +0900

    refactor(problem4): change to inner class

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,9 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+
+	private static final int BLANK = 32;
+	private static final int UPPER = 65 + 90;
+	private static final int LOWER = 97 + 122;
+
 	public static String solution(String word) {
-		ReverseCharacter reverseCharacter = new ReverseCharacter(word);
-		String answer = reverseCharacter.run();
+		char[] chars = word.toCharArray();
+
+		String answer = "";
+
+		for (char c : chars) {
+			int ascii = (int)c;
+
+			if (ascii == BLANK) {
+				answer += " ";
+			} else if (Character.isUpperCase(ascii)) {
+				answer += Character.toString(UPPER - ascii);
+			} else if (Character.isLowerCase(ascii)) {
+				answer += Character.toString(LOWER - ascii);
+			}
+
+		}
 		return answer;
 	}
 }
