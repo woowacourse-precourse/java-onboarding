28d783dca848f221cbae1a3dc6b9f406b6e4f389
eunbii0213 <eunbii0213@naver.com>
2022-10-29 01:34:31 +0900

    refactor: (Problem1) refactoring

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,70 +7,97 @@ class Problem1 {
     public static final int POBI_WIN = 1;
     public static final int CRONG_WIN = 2;
     public static final int DRAW = 0;
+    public static final int LEFT_PAGE_INDEX = 0;
+    public static final int RIGHT_PAGE_INDEX = 1;
+    public static final int MIN_PAGE = 1;
+    public static final int MAX_PAGE = 400;
+    public static final int INIT_ZERO = 0;
+    public static final int INIT_ONE = 1;
+    public static final int PAGE_INTERVAL = 1;
+    public static final int DIVIDE_NUMBER = 10;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        int pobiMax = 0;
-        int crongMax = 0;
-
-
         if (isException(pobi) || isException(crong)) {
-            answer = EXCEPTION;
-        } else {
-            pobiMax = calculatePageMax(pobi);
-            crongMax = calculatePageMax(crong);
-            answer = makeWinner(pobiMax, crongMax);
+            return EXCEPTION;
         }
+
+        int pobiMax = maxScore(pobi);
+        int crongMax = maxScore(crong);
+        int answer = makeWinner(pobiMax, crongMax);
 
         return answer;
     }
 
-    private static boolean isException(List<Integer> pageList) {
-        if (pageList.get(1) - pageList.get(0) > 1) {
+    public static boolean isException(List<Integer> pageList) {
+        if (isPageIntervalException(pageList)) {
             return true;
-        } else if (pageList.get(1) < pageList.get(0)) {
+        }
+
+        if (isPagePositionException(pageList)) {
             return true;
         }
 
         for (Integer pageNumber : pageList) {
-            if (pageNumber > 400) {
+            if (isNotValidException(pageNumber)) {
                 return true;
             }
         }
         return false;
     }
 
-    private static int calculatePageMax(List<Integer> pageList) {
-        int max = 0;
+    private static boolean isNotValidException(Integer pageNumber) {
+        return (pageNumber > MAX_PAGE || pageNumber < MIN_PAGE);
+    }
+
+    private static boolean isPagePositionException(List<Integer> pageList) {
+        return (pageList.get(RIGHT_PAGE_INDEX) < pageList.get(LEFT_PAGE_INDEX));
+    }
+
+    private static boolean isPageIntervalException(List<Integer> pageList) {
+        return (pageList.get(RIGHT_PAGE_INDEX) - pageList.get(LEFT_PAGE_INDEX) > PAGE_INTERVAL);
+    }
+
+    public static int maxScore(List<Integer> pageList) {
+        int max = INIT_ZERO;
 
         for (Integer pageNumber : pageList) {
-            int pageNumberToInt = pageNumber.intValue();
-            if (max < seperateOneNumber(pageNumberToInt)) {
-                max = seperateOneNumber(pageNumberToInt);
+            if (max < compareSum(pageNumber)) {
+                max = compareSum(pageNumber);
             }
         }
         return max;
     }
 
-    private static int seperateOneNumber(int pageNumberToInt) {
-        int plusSum = 0;
-        int multiplySum = 1;
-
-        while (pageNumberToInt != 0) {
-            plusSum += pageNumberToInt % 10;
-            multiplySum *= pageNumberToInt % 10;
-            pageNumberToInt /= 10;
-        }
-        return (plusSum >= multiplySum) ? plusSum : multiplySum;
+    public static int compareSum(int pageNumber) {
+        return (maxPlusSum(pageNumber) >= maxMultiplySum(pageNumber)) ? maxPlusSum(pageNumber) : maxMultiplySum(pageNumber);
     }
 
-    private static int makeWinner(int pobiMax, int crongMax) {
+    private static int maxMultiplySum(int pageNumber) {
+        int multiplySum = INIT_ONE;
+
+        while (pageNumber != 0) {
+            multiplySum *= pageNumber % DIVIDE_NUMBER;
+            pageNumber /= DIVIDE_NUMBER;
+        }
+        return multiplySum;
+    }
+
+    private static int maxPlusSum(int pageNumber) {
+        int plusSum = INIT_ZERO;
+
+        while (pageNumber != 0) {
+            plusSum += pageNumber % DIVIDE_NUMBER;
+            pageNumber /= DIVIDE_NUMBER;
+        }
+        return plusSum;
+    }
+
+    public static int makeWinner(int pobiMax, int crongMax) {
         if (pobiMax < crongMax) {
             return CRONG_WIN;
         } else if (pobiMax > crongMax) {
             return POBI_WIN;
-        } else {
-            return DRAW;
         }
+        return DRAW;
     }
 }
