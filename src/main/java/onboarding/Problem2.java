a4371929bc119ed71b6e35f8dfb0b668cb4ec36a
bradbrad97 <taehyun0121@naver.com>
2022-10-28 18:30:22 +0900

    problem2: 연속된 중복 문자 제거 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i<cryptogram.length(); i++){
+            char currentChar = cryptogram.charAt(i);
+            if (stack.empty() || stack.peek() != currentChar){
+                stack.push(currentChar);
+            }
+            else{
+                stack.pop();
+            }
+        }
+
+        String answer = "";
+        while(!stack.empty()){
+            answer = stack.pop() + answer;
+        }
+
         return answer;
     }
 }
