f6918502a5be8f9a3d04175279143390fb20d4be
skck0226 <skck0226@ajou.ac.kr>
2022-10-28 10:37:45 +0900

    [refactor] Problem2 decodeCryptogram 로직 정규식으로 교체

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem2 {
     private static final int MINIMUM_INPUT = 1;
     private static final int MAXIMUM_INPUT = 1000;
@@ -7,8 +9,6 @@ public class Problem2 {
 
     public static String solution(String cryptogram) {
         if (isExceptionCase(cryptogram)) return INPUT_ERROR;
-
-        if (cryptogram.length() == 1) return cryptogram;
 
         while (true) {
             String decoded = decodeCryptogram(cryptogram);
@@ -24,22 +24,6 @@ public class Problem2 {
     }
 
     private static String decodeCryptogram(String cryptogram) {
-        String newCrypt = "";
-        char prev = ' ';
-
-        if(cryptogram.length() < 2) return cryptogram;
-
-        for (int i = 0; i < cryptogram.length() - 1; i++) {
-            char now = cryptogram.charAt(i);
-            char next = cryptogram.charAt(i + 1);
-            if (now != prev && now != next) {
-                newCrypt += now;
-            }
-            prev = now;
-        }
-        if (cryptogram.charAt(cryptogram.length() - 1) != cryptogram.charAt(cryptogram.length() - 2)) {
-            newCrypt += cryptogram.charAt(cryptogram.length() - 1);
-        }
-        return newCrypt;
+        return cryptogram.replaceAll("(\\w)\\1{1,}", "");
     }
 }
