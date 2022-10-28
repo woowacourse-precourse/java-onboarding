bab1063b4ea1766f3ac6db8c9cd2fe1a0b36368b
seonghoo1217 <seonghoo1217@naver.com>
2022-10-28 18:51:30 +0900

    feat : [Task 3] Get Contains 3 or 6 or 9 Count and minus total_number_length

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+    static int count=0;
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        if (number<3) return 0;
+
+        for (int i=3;i<=number;i++){
+            getCount(i);
+        }
+
+        return count;
+    }
+
+    public static void getCount(int num){
+        String s = String.valueOf(num);
+        int num_length = s.length();
+        count+= num_length - s.replaceAll("[369]","").length();
     }
 }
