0cf1a5e11285d6f1f33f906fb54921ad91f59ecb
seoeunbae <enyoupark@gmail.com>
2022-10-28 21:27:37 +0900

    [4] < feature > 아스키코드 활용한 변환 메소드 구현 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+
+        char[] words = word.toCharArray();
+        for(int i=0 ; i < words.length ; i++){
+            if( (byte) words[i] == 32){  //공백 유지
+                continue;
+            } else if( (byte) words[i] > 90 ){  //대문자
+                System.out.println((byte)words[i]);
+                words[i] = (char)(219 - (byte) words[i]);
+                System.out.println(words[i]);
+            } else {  //소문자
+                words[i] = (char)(155 - (byte) words[i]);
+            }
+        }
+
+        answer = String.valueOf(words);
+
         return answer;
     }
 }
