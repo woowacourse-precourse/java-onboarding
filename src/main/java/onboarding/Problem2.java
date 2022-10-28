ddf7737d4b6dc85cd815f8c2ee739c1f9ebc91ad
Jueun <jueun000101@gmail.com>
2022-10-28 16:02:55 +0900

    docs: 2번 기능 목록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 연속하는 중복 문자를 삭제하는 함수
+ * 2. 연속하는 중복 문자가 있는지, 있다면 시작 위치와 끝 위치까지 찾는 함수
+ * 3. 연속하는 중복 문자를 삭제할 수 있을 때까지 삭제하는 함수
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
