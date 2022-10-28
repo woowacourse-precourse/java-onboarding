6f0711c77eb60f7f3e3af7d6389f641b9f1f0f9e
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-28 16:46:20 +0900

    feat: Problem4  문자열을 받아 역순으로 반드는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,12 +3,31 @@ package onboarding;
 /**
  * 구현 기능 목록
  * 1. 문자열을 받아 역순으로 바꿔주는 기능
- * 2. 치환한 문자열의 첫 글자를 대문자로 바꿔주기
- * 3. result에 치환한 문자열을 담고 반환
+ * 2. result에 치환한 문자열을 담고 반환
  */
 public class Problem4 {
+    static final int lowera = 'a';
+    static final int lowerz = 'z';
+    static final int upperA = 'A';
+    static final int upperZ = 'Z';
+
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    public static StringBuilder ReverseWord(String words) {
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i<words.length(); i++) {
+            int charcasting = words.charAt(i);
+            if (charcasting>lowera && charcasting<lowerz) {
+                charcasting = lowerz-(charcasting-lowera);
+            } else if (charcasting>upperA&&charcasting<upperZ) {
+                charcasting = upperZ-(charcasting-upperA);
+            }
+            result.append((char) charcasting);
+        }
+        return result;
+    }
 }
+
