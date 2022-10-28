ca8511ee135af45f0c6a4e6212840d9429c4c4e4
soyunim <togksk123@naver.com>
2022-10-29 00:31:46 +0900

    Problem1 2)each page's additions and multiplications

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int[] answer = {0,0,0,0};
+        int[] sum = {0,0,0,0};
+        int[] multi = {1,1,1,1};
         int[] page = {pobi.get(0), pobi.get(1) ,crong.get(0),crong.get(1)}; // pages
+
+        // each page's additions and multiplications
+        for(int i=0; i<page.length; i++){
+            while(page[i]>0){
+                sum[i] += page[i]%10;
+                multi[i] *= page[i]%10;
+                page[i]/=10;
+            }
+            if(sum[i]>multi[i]){
+                answer[i] += sum[i];
+            }
+            else{
+                answer[i] += multi[i];
+            }
+        }
+
         return answer;
     }
 }
