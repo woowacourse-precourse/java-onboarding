f2b7e74d56aec43199fca2c8c73f7a47194574c1
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-28 19:03:32 +0900

    feat : 기준을 세운 현재 친구 리스트의 1번째 이름과 그 짝 이름 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,18 +5,43 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-
+        int setFriendNameCount = 0;
         for (int i = 0; i < friends.size(); i++) {
             List friendsList = friends.get(i);
-            for (int j = 0; j < friends.size(); j++) {
-                if (j == i) {
-                } else {
-                    List otherFriendsList = friends.get(j);         // 기준 친구 리스트와 다른 친구리스트를 비교
+
+//            System.out.println("--------------");
+//            System.out.println(friendsList);
+
+            for (int j = 0; j < 2; j++) {
+                String FriendName = (String) friendsList.get(j);
+                String pairName = getPairName(friendsList, j);
+//                System.out.println("--------------");
+//                System.out.println(FriendName);
+                for (int k = 0; k < friends.size(); k++) {
+                    if (k == i) {
+                    } else {
+                        List otherFriendsList = friends.get(k);         // 기준 친구 리스트와 다른 친구리스트를 비교
+                        if (otherFriendsList.contains(FriendName) == true) {
+                            System.out.println(otherFriendsList);
+                        }
+                    }
                 }
             }
         }
-
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+
+    private static String getPairName(List friendsList, int j) {
+
+        if (j == 0) {
+            j = 1;
+        } else if (j == 1) {
+            j = 0;
+        }
+        String pairName = (String) friendsList.get(j);
+
+        return pairName;
     }
 }
