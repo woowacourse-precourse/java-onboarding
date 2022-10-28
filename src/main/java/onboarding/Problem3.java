f118670641d9e13754c819adfc97a7d197ae8b44
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-28 21:05:47 +0900

    feat(problem3): 입력값을 검증하다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
+    public static int solution(int number) throws Exception {
         int answer = 0;
+        validate(number);
         return answer;
+    }
+
+    private static void validate(int number) throws Exception {
+        if(number < 1 || number > 10000) {
+            throw new Exception("number가 범위를 벗어났습니다");
+        }
     }
 }
