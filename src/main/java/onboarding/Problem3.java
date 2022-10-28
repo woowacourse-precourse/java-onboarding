e042355e878cdf803f7d845e7a025b681845291a
scyllacore <yms8795@gmail.com>
2022-10-28 09:53:38 +0900

    feat(problem3) : Implement getNumberList 구현 , add 기능 정리 3번

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,18 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     /*
     * 기능 정리
     * 1. 1부터 number까지 숫자 나열
     * 2. 나열된 각 숫자에 대해 3이 들어간 횟수를 카운트
+    * 3. 카운트 한 숫자에 대한 총합 반환
      */
-    
 
+    //1. 기능1
+    static List<Integer> getNumberList(int number){
 
+        List<Integer> numberList = new ArrayList<>();
+
+        for(int i=1; i<=number; i++){
+            numberList.add(i);
+        }
+
+        return numberList;
+    }
+
+ 
 
 
     public static int solution(int number) {
-        int answer = 0;
+        int answer = getCountSummation(number);
         return answer;
     }
 }
