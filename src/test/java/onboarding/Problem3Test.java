46a2e179b7d9bdcbd13bafc48aa2050d7f0bae3d
chanminmaxscaler <jcm04@naver.com>
2022-10-28 22:12:36 +0900

    test(Problem3Test): play369Game 메소드의 Test 코드를 작성한다
    play369GameTest 메소드는 play369Game 메소드를 테스트한다.
    숫자를 입력하면 1부터 입력 숫자까지 369게임의 룰대로 박수의 수를 세서 반환하는지 검증한다.

--- src/test/java/onboarding/Problem3Test.java
+++ src/test/java/onboarding/Problem3Test.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem3Test {
     @Test
+    @DisplayName("숫자가 포함하는 특정 문자의 개수를 반환하는지")
     void countTargetNumberInNumberTest() {
         Problem3 problem3 = new Problem3();
         char targetCharacter = '3';
@@ -21,6 +22,13 @@ class Problem3Test {
         int numberCase3 = 999987;
         int result3 = 0;
         assertThat(problem3.countTargetCharacterInNumber(targetCharacter, numberCase3)).isEqualTo(result3);
+    }
 
+    @Test
+    void play369GameTest() {
+        Problem3 problem3 = new Problem3();
+        int numberCase1 = 33;
+        int result1 = 14;
+        assertThat(problem3.play369Game(numberCase1)).isEqualTo(result1);
     }
 }
