910023fd6d38abe6cf68fe8ab42d119625e847e3
hectick <chyeon1223@cau.ac.kr>
2022-10-29 03:39:08 +0900

    refactor : [Problem1] 예외처리 조건문을 이해하기 쉽도록 조건식 단순화, 메소드명 및 반환값 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,15 +7,15 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
 
         //예외 처리
-        if(checkPageValidation(pobi) == false || checkPageValidation(crong) == false){
+        if(checkPageValidationException(pobi) || checkPageValidationException(crong)){
             //시작면이나 마지막면을 펼친 경우(=유효한 페이지가 아님. 1~400 범위 안의 숫자가 아닌 경우)
             answer = -1;
             return answer;
-        }else if(checkPageSequence(pobi) == false || checkPageSequence(crong) == false){
+        }else if(checkPageSequenceException(pobi) || checkPageSequenceException(crong)){
             //리스트의 숫자가 연속되지 않을 경우
             answer = -1;
             return answer;
-        }else if(checkPagePosition(pobi) == false || checkPagePosition(crong) == false){
+        }else if(checkPagePositionException(pobi) || checkPagePositionException(crong)){
             //리스트의 숫자가 홀짝 순서가 아니라 짝홀 순서로 왔을 경우
             answer = -1;
             return answer;
@@ -66,32 +66,32 @@ class Problem1 {
         return sum;
     }
 
-    private static boolean checkPageValidation(List<Integer> pages){
+    private static boolean checkPageValidationException(List<Integer> pages){
         for(int i = 0; i < pages.size(); i++){
             if(pages.get(i) < 1 || pages.get(i) > 400){
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
-    private static boolean checkPageSequence(List<Integer> pages){
+    private static boolean checkPageSequenceException(List<Integer> pages){
         int num = pages.get(0);
         for(int i = 1; i < pages.size(); i++){
             if(pages.get(i) != num+1){
-                return false;
+                return true;
             }else{
                 num = pages.get(i);
             }
         }
-        return true;
+        return false;
     }
 
-    private static boolean checkPagePosition(List<Integer> pages){
+    private static boolean checkPagePositionException(List<Integer> pages){
         //왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수여야 함
         if(pages.get(0)%2 == 0 || pages.get(1)%2 == 1){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
