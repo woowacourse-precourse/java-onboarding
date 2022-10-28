c8fae0e33a4756d71bed11bdb538c3c9448b278a
squareyun <squareyun@gmail.com>
2022-10-28 22:32:09 +0900

    feat: 제한할 닉네임 목록 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -12,12 +13,24 @@ public class Problem6 {
     private static Set<String> answer = new HashSet<>();
 
     public static List<String> solution(List<List<String>> forms) {
+
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickname = form.get(1);
 
             if (nickname.length() < 2)
                 continue;
+
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String splitNickname = nickname.substring(i, i+2);
+
+                if (limitedNicknameMap.containsKey(splitNickname)) {
+                    limitedNicknameMap.get(splitNickname).add(email);
+                } else {
+                    Set<String> emailListSet = new HashSet<>(Arrays.asList(email));
+                    limitedNicknameMap.put(splitNickname, emailListSet);
+                }
+            }
         }
 
         List<String> retAnswer = new ArrayList<>(answer);
