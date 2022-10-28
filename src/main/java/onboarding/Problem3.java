90a06f284d32e1798fd22c599bf2699c2e615814
cookienc <kim76744@naver.com>
2022-10-28 16:07:13 +0900

    feat(Problem3): Problem3 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clap = 0;
+        for (int i = 1; i <= number; i++) {
+            String currentNum = String.valueOf(i);
+
+            for (int j = 0; j < currentNum.length(); j++) {
+                char digit = currentNum.charAt(j);
+
+                if (digit == '3' || digit == '6' || digit == '9') {
+                    clap++;
+                }
+            }
+        }
+
+        return clap;
     }
 }
