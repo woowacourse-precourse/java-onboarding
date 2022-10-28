252b470fe1d4ae9c2dd3dc0c80065af1e9d906cc
csj <koreaxman@naver.com>
2022-10-29 00:14:54 +0900

    feat: Create a list of all nicknames

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> nicknameList = extractNicknames(forms);
+
+        return nicknameList;
+    }
+
+    public static List<String> extractNicknames(List<List<String>> forms) {
+        List<String> nicknameList = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            nicknameList.add(forms.get(i).get(1));
+        }
+
+        return nicknameList;
     }
 }
