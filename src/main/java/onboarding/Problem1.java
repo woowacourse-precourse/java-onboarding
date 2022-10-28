b0c529f979948f4f910d9ff160b39ce48ca97df9
HyowonSin <tlsgydnjs@naver.com>
2022-10-28 19:15:34 +0900

    fix: Add exceptions of problem1
    - 문제 1번에서 예외 상황들을 처리하였습니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        // 예외 처리
+        if (pobi.size() != 2 || crong.size() != 2) {
+            return -1;
+        } else if (pobi.get(0) % 2 != 1 || crong.get(0) % 2 != 1) {
+            return -1;
+        } else if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
+            return -1;
+        } else if (pobi.get(0) < 1 || pobi.get(1) > 400 || crong.get(0) < 1 || crong.get(1) > 400) {
+            return -1;
+        }
+
         int pobiScore = max(pobi);
         int crongScore = max(crong);
 
