393d9b7eee86c2a8d6559d99aff8747f4d4e36dd
HamSeungChan <head0618@naver.com>
2022-10-28 17:22:24 +0900

    feat: Problem1 큰 페이지 값 구하기 &포비와 크롱 점수 비교

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,30 +1,61 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 class Problem1 {
+
+  private static final int pobi_Win = 1;
+  private static final int crong_Win = 2;
+  private static final int draw = 0;
+  private static final int LeftPage = 0;
+  private static final int RightPage = 1;
+
   public static int solution(List<Integer> pobi, List<Integer> crong) {
     int answer = Integer.MAX_VALUE;
-    pobi = BookPageSelect();
-    crong = BookPageSelect();
 
-    return answer;
+    return answer = Winner(pobi,crong);
   }
 
-  private static List<Integer> BookPageSelect(){
-    List<Integer> bookPageSelect = new ArrayList<>();
-    Random random = new Random();
-    int randomPageSelect = Integer.MAX_VALUE;
-    do{
-      randomPageSelect = random.nextInt(200)*2+1;
-    }while(randomPageSelect==1||randomPageSelect==399);
+  private static int Winner (List<Integer> pobi, List<Integer> crong){
 
-    bookPageSelect.add(randomPageSelect);
-    bookPageSelect.add(randomPageSelect+1);
+    int pobi_Score = CompareScore(pobi);
+    int crong_Score = CompareScore(crong);
+    int result = Integer.MAX_VALUE;
 
-    return bookPageSelect;
+    if(pobi_Score>crong_Score) result = pobi_Win;
+    else if(pobi_Score==crong_Score) result = draw;
+    else result = crong_Win;
+
+    return result;
+  }
+
+  private static int CompareScore(List<Integer> page){
+    int LeftPage_Score = PageBestScore(page.get(LeftPage));
+    int RightPage_Score = PageBestScore(page.get(RightPage));
+    return Math.max(LeftPage_Score,RightPage_Score);
+  }
+
+  private static int PageBestScore(int bookpage){
+    return Math.max(Plus_Score(bookpage),Multiply_Score(bookpage));
+  }
+
+  private static int Plus_Score(int bookpage){
+    int plusScore = 0;
+    while(bookpage !=0) {
+      plusScore += bookpage % 10;
+      bookpage = bookpage / 10;
+    }
+    return plusScore;
+  }
+
+  private static int Multiply_Score(int bookpage) {
+    int multiplyScore = 1;
+    while(bookpage !=0) {
+      multiplyScore *= bookpage % 10;
+      bookpage = bookpage / 10;
+    }
+    return multiplyScore;
   }
 
 }
+
