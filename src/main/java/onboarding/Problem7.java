ab56bd358ff2df67e78392b6d37a8f62c7bc015b
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-10-28 15:37:00 +0900

    사용자 리스트 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> users = createUserList(friends);
+        Map<String, Long> recommendScore = new HashMap<>();
+
+        // 사용자들 뽑아내기
+        // 유저의 친구들 구하기
+        // 유저의 친구들 구해서 +10하기
+        // 타임 라인 visitor +1하기
+        // 추천 점수대로 정렬하기
+        // 추천 점수가 같은 친구들 이름순으로 정렬하기
+
+
+    }
+
+    private static List<String> createUserList(List<List<String>> friends) {
+        List<String> users = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            if(!users.contains(friend.get(0))) users.add(friend.get(0));
+            if(!users.contains(friend.get(1))) users.add(friend.get(1));
+        }
+
+        return users;
     }
 }
