903b761a309725a433b3ccb2ee1a0251f3ab17a7
siwookim97 <tldn12gh@gmail.com>
2022-10-29 03:30:58 +0900

    feat : problem1) 예외사항 확인 기능 추가
    예외사항을 확인하는 메서드
    길이가 2인 List<Integer>를 받아 예외 사항이 있을 시 true,
    그 외엔 false를 반환하는 메서드
    예외사항)
     - 책의 페이지의 범위는 1~400
     - 책의 페이지는 null값이 들어갈 수 없다
     - 책의 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수
     - 책의 페이지는 연속되어야 한다
     - 책의 첫 페이지, 마지막 페이지는 펴지 않는다, ex) List.of(1,2)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,33 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    /**
+     * 예외사항 확인 메서드
+     *
+     * @param name 책을 펼치는 사람, Integer형의 List로 길이는 2
+     * @return 예외 사항이 있을 시 true, 그 외엔 false를 반환
+     */
+    static boolean isException(List<Integer> name) {
+        if ((name.get(0) < 1) || name.get(1) > 400)
+            return true;
+        else if ((name.get(0) == null) || (name.get(1) == null))
+            return true;
+        else if (name.get(0) % 2 != 1)
+            return true;
+        else if (name.get(1) % 2 != 0)
+            return true;
+        else if ((name.get(1) - name.get(0)) != 1)
+            return true;
+        else if ((name.get(0) == 1 || name.get(0) == 399 || name.get(1) == 2 || name.get(1) == 400)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
