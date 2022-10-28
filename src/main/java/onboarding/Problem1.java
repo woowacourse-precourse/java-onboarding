4aa972a1f7854c06e23dadef26d011acc4f7961c
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2022-10-28 17:20:07 +0900

    feat: 점수 비교를 위한 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,18 +1,25 @@
 package onboarding;
 
 import java.util.List;
+
 /***
  * 기능 목록
  *
  * 1. 페이지 번호 계산 기능
  * 2. 점수 비교 기능
+ * 3. 예외사항 처리 기능
  */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        int pobiScore = pageCal(pobi);
+        int crongScore = pageCal(crong);
+        answer = scoreComp(pobiScore, crongScore);
+
         return answer;
     }
+
     public static int pageCal(List<Integer> player) {
         int score = 0;
         for (int i = 0; i < 2; i++) {
@@ -26,9 +33,19 @@ class Problem1 {
                 mulSum *= page % 10;
                 page /= 10;
             }
-        if(score<addSum) score = addSum;
-        if (score<mulSum) score = mulSum;
+            if (score < addSum) score = addSum;
+            if (score < mulSum) score = mulSum;
         }
         return score;
     }
+
+    public static int scoreComp(int s1, int s2) {
+        int ans = 0;
+        if (s1 > s2) ans = 1;
+        else if (s1 == s2) ans = 0;
+        else if (s1 < s2) ans = 2;
+
+        return ans;
+    }
+
 }
