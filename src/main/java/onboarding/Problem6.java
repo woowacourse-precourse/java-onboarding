1a82a1fd31a5eadd0d9218f8bcfd2e8a464b8b49
Kimhyeyun <cqqwer@gmail.com>
2022-10-29 00:10:40 +0900

    [PROBLEM 6] UPDATE 더 간단한 방식으로 변경, value를 list로 추가하지 않고 바로 중복 확인 후, answer에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,29 +5,29 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
-        Map<String, List<Integer>> twoConsecutiveLetters = findConsecutiveLetters(forms);
-
-        for (List<Integer> list : twoConsecutiveLetters.values()) {
-            if (list.size() < 2) continue;
-            for (int idx : list) {
-                answer.add(forms.get(idx).get(0));
-            }
-        }
+        List<String> answer = findConsecutiveLetters(forms);
 
         return answer.stream().distinct().sorted(String::compareTo).collect(Collectors.toList());
 
     }
 
-    private static Map<String, List<Integer>> findConsecutiveLetters(List<List<String>> forms) {
-        Map<String, List<Integer>> tmp = new HashMap<>();
+
+    private static List<String> findConsecutiveLetters(List<List<String>> forms) {
+        List<String> duplicated = new ArrayList<>();
+        Map<String, Integer> tmp = new HashMap<>();
+
         for (int i = 0; i < forms.size(); i++) {
             String nickName = forms.get(i).get(1);
             if(nickName.length() < 2) continue;
             for (int j = 0; j < nickName.length() - 1; j++) {
-                tmp.computeIfAbsent(nickName.substring(j, j + 2), s -> new ArrayList<>()).add(i);
+                String subName = nickName.substring(j, j + 2);
+                if(!tmp.containsKey(subName)) tmp.put(subName, i);
+                else {
+                    duplicated.add(forms.get(i).get(0));
+                    duplicated.add(forms.get(tmp.get(subName)).get(0));
+                }
             }
         }
-        return tmp;
+        return duplicated;
     }
 }
