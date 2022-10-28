6e3bbea7e4001b0268f3de0e54e3323d3c688ea3
leewoojin <zmfpdl64@naver.com>
2022-10-28 14:41:49 +0900

    문제5번 이우진 제출합니다.
    문제: 지폐를 최소한으로 줄이기
    
    해결방법:
    SOL1: 5만원권 부터 큰 금액으로 나눈 리스트에 몫을 저장한다.
    
    1. 지폐의 단위 배열을 큰값부터 정의한다.
    2. 반복문을 통해 돈을 5만원권 지폐부터 작은 단위로 나눈 몫을 리스트에 저장한다.
    나눈 몫을 빼주고 다시 돈을 저장해준다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+//Sol1: 큰 금액부터 나누어 몫을 리스트에 저장한다.
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        Integer[] amount = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1}; //지폐의 단위 배열을 만들어준다.
+
+        for(int i = 0; i < amount.length; i++){ // 지폐의 큰 단위부터 나누어 몫을 저장한다.
+            answer.add(money / amount[i]);
+            money %= amount[i]; //나누고 난 나머지를 다시 money에 저장한다.
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(50237));
     }
 }
