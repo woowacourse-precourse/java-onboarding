e7d162ff8494c66750c1550584b3a5ff893a2e60
Lee Min Woo <lmw7414@naver.com>
2022-10-28 19:49:16 +0900

    problem2 - 기능 요소 설계
    * 문자 중복 시 제거 메서드
    * 중복 문자 검증 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/** 만들어야 할 기능
+ * 문자 중복시 제거 메서드
+ * 중복문자 검증 메서드
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
