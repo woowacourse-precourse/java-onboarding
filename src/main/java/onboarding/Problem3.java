a3e1e65024b67c2120dc25d9d5fb7df7aee32dad
Hojinida <opqrs1103@gmail.com>
2022-10-29 03:11:08 +0900

    feat: 문제 3번 3, 6, 9 이면 true를 반환하는 메서드 check369 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,8 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static boolean check369(int num){
+        return num == 3 || num == 6 || num == 9;
+    }
 }
