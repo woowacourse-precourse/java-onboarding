af4554cfccd900f0803c3580158fc720e119e57d
JJongmen <pjy90123@gmail.com>
2022-10-28 16:44:56 +0900

    feat(Problem2): 더 이상 중복 문자를 제거할 필요가 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,12 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+    private static final String REGEX = "([a-z])\\1+";
+
     public static String solution(String cryptogram) {
-        String answer = removeConsecutiveDuplicates(cryptogram);
-        return answer;
+        String decodedCryptogram = cryptogram;
+        do {
+            cryptogram = decodedCryptogram;
+            decodedCryptogram = removeConsecutiveDuplicates(cryptogram);
+        } while (!isDecoded(cryptogram, decodedCryptogram));
+        return decodedCryptogram;
+    }
+
+    private static boolean isDecoded(String cryptogram, String decodedCryptogram) {
+        return cryptogram.equals(decodedCryptogram);
     }
 
     private static String removeConsecutiveDuplicates(String cryptogram) {
-        return cryptogram.replaceAll("([a-z])\\1+", "");
+        return cryptogram.replaceAll(REGEX, "");
     }
 }
