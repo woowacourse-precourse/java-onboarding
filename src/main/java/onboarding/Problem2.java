f9cdad0fc8cb339be363b62956b4244b0584d298
김현재 <41482946+galug@users.noreply.github.com>
2022-10-28 10:14:22 +0900

    feat(problem2): Add 제한 사항에 맞는 인자가 들어왔는지 검증하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        if (!validateCryptogram(cryptogram)) {
+            return null;
+        }
+
+        return "";
+    }
+
+    public static boolean validateCryptogram(String cryptogram) {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            return false;
+        }
+        String pattern = "^[A-Z]*$";
+        return Pattern.matches(pattern, cryptogram);
     }
 }
