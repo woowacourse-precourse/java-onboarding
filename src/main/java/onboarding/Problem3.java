8fea4a8400e6a7c5577d1054a68b3d850aa04cfb
ykmxxi <ykmxxi97@gmail.com>
2022-10-28 22:13:28 +0900

    feat: (Problem3)3, 6, 9 가 존재하면 손뼉을 한번 치는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem3 {
+    private static final String THREE = "3";
+    private static final String SIX = "6";
+    private static final String NINE = "9";
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
@@ -8,5 +12,21 @@ public class Problem3 {
 
     private static String toString(int number) {
         return String.valueOf(number);
+    }
+
+    private static int hasThreeSixNine(String number, int length) {
+        int clapCount = 0;
+        for (int i = 0; i < length; i++) {
+            if (number.substring(i, i + 1).equals(THREE)) {
+                clapCount += 1;
+            }
+            if (number.substring(i, i + 1).equals(SIX)) {
+                clapCount += 1;
+            }
+            if (number.substring(i, i + 1).equals(NINE)) {
+                clapCount += 1;
+            }
+        }
+        return clapCount;
     }
 }
