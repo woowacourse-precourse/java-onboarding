baebc08dd7a821d7da54a7dbc1fcd22c338f5dcc
eunbii0213 <eunbii0213@naver.com>
2022-10-28 20:12:23 +0900

    feat: (Problem7) 점수가 같다면 알파벳 순으로 정렬한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,48 +4,94 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> friendsWithUser = new ArrayList<>();
-    public static Map<String, Integer> sameFriendWithUser = new HashMap<String, Integer>();
+    public static Map<String, Integer> sameFriendWithUser = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        addFriendsWithUser(user,friends);
-        findSameFriendWithUser(user,friends);
+
+        addFriendsWithUser(user, friends);
+        findSameFriendWithUser(user, friends);
         findVisitor(visitors);
-        makeOrder();
+
+        List<String> answer = makeOrder();
         return answer;
     }
 
-    private static void makeOrder() {
-        List<Map.Entry<String, Integer>> list_entries = new ArrayList<Map.Entry<String, Integer>>(sameFriendWithUser.entrySet());
+    public static List<String> addResult(List<Map.Entry<String, Integer>> listEntries) {
+        List<String> result = new ArrayList<>();
+        List<String> sameValueList = new ArrayList<>();
 
-        Collections.sort(list_entries, new Comparator<Map.Entry<String, Integer>>() {
+        int count = 0;
+
+        for (int i = 0; i < listEntries.size(); i++) {
+
+            if(count>=5){
+                break;
+            }
+
+            Map.Entry<String, Integer> entry = listEntries.get(i);
+            int target = entry.getValue();
+
+            for (int j = i + 1; j < listEntries.size(); j++) {
+                if (listEntries.get(j).getValue() == target) {
+                    sameValueList.add(listEntries.get(j).getKey());
+                }
+            }
+            if (!sameValueList.isEmpty()) {
+                sameValueList.add(listEntries.get(i).getKey());
+                i+=(sameValueList.size()-1);
+                Collections.sort(sameValueList);
+
+                for (int z = 0; z < sameValueList.size(); z++) {
+                    result.add(sameValueList.get(z));
+                    count++;
+                }
+
+                sameValueList.clear();
+
+            } else {
+                result.add(entry.getKey());
+                count++;
+            }
+        }
+
+        return result;
+    }
+
+    private static List<String> makeOrder() {
+        List<Map.Entry<String, Integer>> listEntries = new ArrayList<Map.Entry<String, Integer>>(sameFriendWithUser.entrySet());
+
+        Collections.sort(listEntries, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> obj1, Map.Entry<String, Integer> obj2) {
-
                 return obj2.getValue().compareTo(obj1.getValue());
             }
         });
+
+        return addResult(listEntries);
     }
 
     private static void findVisitor(List<String> visitors) {
         for (String visitor : visitors) {
-            if(sameFriendWithUser.containsKey(visitor)){
-                sameFriendWithUser.replace(visitor, sameFriendWithUser.get(visitor)+1);
-            }else{
-                sameFriendWithUser.put(visitor,1);
+            if (!friendsWithUser.contains(visitor)) {
+                if (sameFriendWithUser.containsKey(visitor)) {
+                    sameFriendWithUser.replace(visitor, sameFriendWithUser.get(visitor) + 1);
+                } else {
+                    sameFriendWithUser.put(visitor, 1);
+                }
             }
+
         }
     }
 
     public static void findSameFriendWithUser(String user, List<List<String>> friends) {
         for (List<String> list : friends) {
             for (String friendWithUser : friendsWithUser) {
-                if(list.contains(friendWithUser)) {
+                if (list.contains(friendWithUser)) {
                     for (String friend : list) {
-                        if(!friend.equals(user) && !friend.equals(friendWithUser)){
-                            if(sameFriendWithUser.containsKey(friend)){
-                                sameFriendWithUser.replace(friend, sameFriendWithUser.get(friend)+20);
-                            }else{
-                                sameFriendWithUser.put(friend,20);
+                        if (!friend.equals(user) && !friend.equals(friendWithUser)) {
+                            if (sameFriendWithUser.containsKey(friend)) {
+                                sameFriendWithUser.replace(friend, sameFriendWithUser.get(friend) + 20);
+                            } else {
+                                sameFriendWithUser.put(friend, 20);
 
                             }
                         }
@@ -56,10 +102,10 @@ public class Problem7 {
     }
 
     public static void addFriendsWithUser(String user, List<List<String>> friends) {
-        for(List<String> list : friends){
-            if(list.contains(user)) {
+        for (List<String> list : friends) {
+            if (list.contains(user)) {
                 for (String friend : list) {
-                    if(!friend.equals(user)){
+                    if (!friend.equals(user)) {
                         friendsWithUser.add(friend);
                     }
                 }
