ab6a9db33cc384bd376a7ee223502d761d09f9be
seoeunbae <enyoupark@gmail.com>
2022-10-28 21:28:11 +0900

    [4] < style > 출력메소드 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -11,9 +11,7 @@ public class Problem4 {
             if( (byte) words[i] == 32){  //공백 유지
                 continue;
             } else if( (byte) words[i] > 90 ){  //대문자
-                System.out.println((byte)words[i]);
                 words[i] = (char)(219 - (byte) words[i]);
-                System.out.println(words[i]);
             } else {  //소문자
                 words[i] = (char)(155 - (byte) words[i]);
             }
