486212fd26782431a0463d58ecec226e96f53858
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 15:52:08 +0900

    [Problem3] 3, 6, 9의 개수 찾는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -17,6 +17,22 @@ public class Problem3 {
         return Integer.toString(number);
     }
 
+    /**
+     * 2. 해당 숫자 3, 6, 9의 개수 찾기
+     * @param number : 3, 6, 9의 개수를 찾을 숫자
+     * @return : 3, 6, 9의 개수
+     */
+    static int find369(String number){
+        int total = 0;
+
+        for (int i = 0; i < number.length(); i++){
+            if (number.charAt(i) == '3' || number.charAt(i) == '6' || number.charAt(i) == '9')
+                total++;
+        }
+
+        return total;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
