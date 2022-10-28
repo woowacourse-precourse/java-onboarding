7812e0723a59b231e5fc945944264460a9804a78
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-28 22:44:07 +0900

    승자 구하는 기능 -> getWin 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,10 +3,13 @@ package onboarding;
 import java.util.List;
 /*
 기능목록
-1. 페이지별 최대 점수 구하는 기능
+1. 페이지 규칙
+    1-1. 시작면이나 마지막 면이 나오는 경우X
+    1-2. 왼족 오른쪽 페이지의 차이가 1인지
+2. 페이지별 최대 점수 구하는 기능
     1-1. 모두 더한 경우와 모두 곱한 경우 구해서 최대 찾기
-2. 오른쪽, 왼쪽 중 최대 기능 구하는 기능
-2. 승자 구하는 기능
+3. 오른쪽, 왼쪽 중 최대 기능 구하는 기능
+4. 승자 구하는 기능
  */
 class Problem1 {
     //페이지를 전달인자로 최대 점수 구하는 기능
@@ -21,19 +24,31 @@ class Problem1 {
         }
         return plus>multi ? plus : multi;
     }
+
     //오른쪽, 왼쪽 페이지 중에 최대 구하는 기능
     public static Integer MaxPage(List<Integer> pages){
         int left=calcScore(pages.get(0));
         int right=calcScore(pages.get(1));
         return left>right? left : right;
     }
+    //승자 구하는 기능(1: 포비 승/2: 크롱 승/0:무승부/예외 -1)
+    public static Integer getWin(List<Integer> pobi, List<Integer> crong){
+        int pobiScore=MaxPage(pobi);
+        int crongScore=MaxPage(crong);
+        if(pobiScore==-1 || crongScore==-1) return -1;
+        if (pobiScore>crongScore){
+            return 1;
+        }else if(pobiScore<crongScore){
+            return 2;
+        }else if(pobiScore==crongScore){
+            return 0;
+        }
+        return -1;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        System.out.printf("pobi left : %d right: %d\n", calcScore(pobi.get(0)), calcScore(pobi.get(1)));
-        System.out.printf("crong left : %d right: %d\n", calcScore(crong.get(0)), calcScore(crong.get(1)));
-        System.out.printf("pobi max : %d", MaxPage(pobi));
-        System.out.printf("crong max : %d", MaxPage(crong));
+        answer=getWin(pobi, crong);
         return answer;
     }
 }
