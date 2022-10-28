b3655bba11cb5f735d8080a067f73439d05331f7
dlrjs2360 <lhg3670@naver.com>
2022-10-28 16:52:48 +0900

    :sparkles: feat: < 문제3 | 2.리스트에 3,6,9가 포함되면 개수만큼 세어서 반환 >

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,11 +8,26 @@ import java.util.List;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += counter(String.valueOf(i).toCharArray());
+            }
         return answer;
     }
 
-    // 1. 숫자를 문자열로 바꾸기
+    // 1. 숫자를 문자열리스트로 변형
     public static String numToStr(int number) {
         return String.valueOf(number);
+    }
+
+    // 2. 리스트에 3,6,9가 포함되면 개수만큼 "answer"에 1 더하기
+    public static int counter(char[] num) {
+        int count = 0;
+        String[] arr = {"3", "6", "9"};
+        for (char x : num) {
+            if (Arrays.asList(arr).contains(String.valueOf(x))) {
+                count += 1;
+            }
+        }
+        return count;
     }
 }
