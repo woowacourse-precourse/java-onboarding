5bec1cd5747d55c13491c548a035a41c5d40e9ff
kjb95 <topmedia44@gmail.com>
2022-10-28 21:04:08 +0900

    feat: Problem6 - 예외처리 코드 추가
    1. 크루는 1명 미만 또는 10000 초과인 경우
    
    2. 이메일
    - 이메일 형식에 부합하지 않는 경우 ('아이디@email.com')
    - 이메일의 전체 길이가 11자 미만 또는 20자 초과인 경우
    
    3. 닉네임
    - 한글이 아닌 경우
    - 전체 길이가 1자 미만 또는 20자 이상인 경우

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,19 +4,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Problem6 {
 
     private static final int TWO = 2;
+    private static final int STUDENT_NUM_MIN = 1;
+    private static final int STUDENT_NUM_MAX = 10000;
+    private static final String EMAIL_REGEX = "^[[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*]{1,9}@email.com$";
+    private static final String NICKNAME_REGEX = "^[ㄱ-ㅎ|가-힣|]{1,19}$";
 
     private static Map<String, List<String>> nicknameToTwoLetters = new HashMap();
     private static Map<String, String> nicknameToEmail = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
+        validateForms(forms);
         parseForms(forms);
         return findDuplicatedNicknameAndComputeEmail();
+    }
+
+    private static void validateForms(List<List<String>> forms) {
+        if (isValidFormSize(forms) && isValidFormEmail(forms) && isValidFormNickname(forms)) {
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static boolean isValidFormSize(List<List<String>> forms) {
+        return forms.size() >= STUDENT_NUM_MIN && forms.size() <= STUDENT_NUM_MAX;
+    }
+
+    private static boolean isValidFormEmail(List<List<String>> forms) {
+        return forms.stream()
+            .filter(form -> !Pattern.matches(EMAIL_REGEX, form.get(0)))
+            .count() == 0;
+    }
+
+    private static boolean isValidFormNickname(List<List<String>> forms) {
+        return forms.stream()
+            .filter(form -> !Pattern.matches(NICKNAME_REGEX, form.get(1)))
+            .count() == 0;
     }
 
     private static List<String> findDuplicatedNicknameAndComputeEmail() {
---
+++ src/test/java/onboarding/Problem6Test.java
@@ -0,0 +1,37 @@
+package onboarding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Problem6Test {
+
+    @Test
+    void 크루_인원이_비정상_입력인_경우() {
+        List<List<String>> forms = new ArrayList<>();
+        Assertions.assertThatThrownBy(() -> Problem6.solution(forms));
+
+        IntStream.range(0, 10001)
+            .forEach(i -> forms.add(List.of("hi@email.com", "진범")));
+        Assertions.assertThatThrownBy(() -> Problem6.solution(forms));
+    }
+
+    @Test
+    void 이메일이_비정상_입력인_경우() {
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("@email.com", "진범"))));
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("eeeemail.com", "진범"))));
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("eee@emailcom", "진범"))));
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("eee@email.co", "진범"))));
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("eeeeeeeeeee@email.com", "진범"))));
+    }
+
+    @Test
+    void 닉네임이_비정상_입력인_경우() {
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("hi@email.com", "k진범"))));
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("hi@email.com", ""))));
+        Assertions.assertThatThrownBy(() -> Problem6.solution(List.of(List.of("hi@email.com", "가나다라마가나다라마가나다라마가나다라마"))));
+    }
+
+}
