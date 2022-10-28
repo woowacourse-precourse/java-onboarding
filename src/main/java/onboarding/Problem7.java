d6c5e8bd200a8afdb1c38b7e7d15a68eafc06051
Choi-JJunho <junho5336@gmail.com>
2022-10-28 23:53:19 +0900

    docs(문제7): 기능목록 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 📚 기능 목록
+ *
+ * 1. 사용자의 친구를 구하는 기능
+ * 2. 함께 아는 친구(위에서 구한 친구)를 가진 사용자를 구하는 기능
+ * 3. 점수를 부여하는 기능
+ * 4. 결과를 정렬하는 기능
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
