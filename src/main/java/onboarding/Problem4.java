5ecd2541cde6036d53bbb5ae016ff91cdfc91c92
kjb95 <topmedia44@gmail.com>
2022-10-28 12:59:02 +0900

    feat: Problem4 - 청개구리 사전을 참고해 문자열 변경하기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,21 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Problem4 {
 
     private static final int UPPER_A_ASCII = 'A';
-    private static final int UPPER_Z_ASCII = 'A';
+    private static final int UPPER_Z_ASCII = 'Z';
     private static final int LOWER_A_ASCII = 'a';
     private static final int LOWER_Z_ASCII = 'z';
 
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return Arrays.stream(word.split(""))
+            .map(Problem4::treeFrogDictionary)
+            .collect(Collectors.joining());
     }
 
-    public static char treeFrogDictionary(char word) {
-        if (Character.isUpperCase(word)) {
-            return (char) (UPPER_A_ASCII + UPPER_Z_ASCII - (int) word);
+    private static String treeFrogDictionary(String word) {
+        if (Character.isUpperCase(word.charAt(0))) {
+            return String.valueOf((char) (UPPER_A_ASCII + UPPER_Z_ASCII - (int) word.charAt(0)));
         }
-        return (char) (LOWER_A_ASCII + LOWER_Z_ASCII - (int) word);
+        if (Character.isLowerCase(word.charAt(0))) {
+            return String.valueOf((char) (LOWER_A_ASCII + LOWER_Z_ASCII - (int) word.charAt(0)));
+        }
+        return word;
     }
 }
