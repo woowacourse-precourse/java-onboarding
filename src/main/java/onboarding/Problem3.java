030b3ae46fb6d384c56a06b2620865906e23cf9a
antaeho <an981022@naver.com>
2022-10-28 10:48:07 +0900

    Problem3 기능 구현사항1/2 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,4 +1,7 @@
 package onboarding;
+
+import java.util.Arrays;
+
 /*
 - 기능 구현사항 목록 -
 1. 1부터 입력값까지 for 문 실행
@@ -8,6 +11,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i < number+1; i++) {
+            int num = i;
+            String temp = Integer.toString(num);
+            int[] digits = new int[temp.length()];
+            for (int j = 0; j < temp.length(); j++) digits[j] = temp.charAt(j) - '0';
+            System.out.print( Arrays.toString(digits) );
+        }
         return answer;
     }
 }
