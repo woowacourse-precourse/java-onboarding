0cda4d2337268329a561f826e7beaf98c6c3c83d
kwYoohae <gocksdl7979@gmail.com>
2022-10-28 12:58:52 +0900

    feat(Problem4.java): 알파벳을 변환하는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,8 +8,20 @@ public class Problem4 {
     public static final int LOWERCASE_END_ALPHABET = 'z';
 
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] splitWord = word.toCharArray();
+
+        for(int i = 0 ; i < splitWord.length; i++) {
+            int checkAlphabet = checkUpperOrLower(splitWord[i]);
+
+            if(checkAlphabet == 0) {
+                splitWord[i] = (char) (UPPERCASE_END_ALPHABET - (splitWord[i] - UPPERCASE_START_ALPHABET));
+            }
+            else if (checkAlphabet == 1) {
+                splitWord[i] = (char) (LOWERCASE_END_ALPHABET - (splitWord[i] - LOWERCASE_START_ALPHABET));
+            }
+        }
+
+        return String.valueOf(splitWord);
     }
 
     public static int checkUpperOrLower(char alphabet) {
