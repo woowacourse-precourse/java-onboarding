0e7c1a2d8fac864be78af47c92d144e5ec57a346
hoonn9 <hun9935@gmail.com>
2022-10-28 17:48:38 +0900

    Add stack solution (Problem2)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> s = new Stack<>();
+
+
+        for (Character c: cryptogram.toCharArray()) {
+            if (!s.empty() && s.peek() == c) {
+                s.pop();
+                continue;
+            }
+            s.push(c);
+        }
+
+
+        while (!s.empty()) {
+            answer.insert(0, s.pop());
+        }
+
+        return answer.toString();
     }
 }
