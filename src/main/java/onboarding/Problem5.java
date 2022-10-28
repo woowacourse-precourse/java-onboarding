b6ce80153ea113a776d25ad556bf91fb5bfb64de
5nam <ohnam90@gmail.com>
2022-10-29 00:39:01 +0900

    feat: 리스트를 0 으로 초기화하는 기능 추가 리스트가 빈 상태이기 때문에, 거스름돈 종류 개수에 맞추어 자리를 마련해두는 것

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,14 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    // 리스트 0 으로 초기화
+    private static List<Integer> initList(List<Integer> list) {
+        list = new ArrayList<>();
+        for(int i = 0; i<9; i++) {
+            list.add(0);
+        }
+        return list;
     }
 }
