1e5824b4c1771b3c22a703d25d854ce05f404924
Koonayoung518 <koony000415@gmail.com>
2022-10-29 00:50:14 +0900

    feat(problem1) : 페이지의 큰 수 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
@@ -8,9 +9,19 @@ class Problem1 {
         //예외 확인
         if(checkException(pobi) || checkException(crong))
             return -1;
+        //포비
+        List<Integer> pobiPages = new ArrayList<>();
+        pobiPages.add(getMaxPageNumber(pobi.get(0)));
+        pobiPages.add(getMaxPageNumber(pobi.get(1)));
+
+        //크롱
+        List<Integer> crongPages = new ArrayList<>();
+        crongPages.add(getMaxPageNumber(crong.get(0)));
+        crongPages.add(getMaxPageNumber(crong.get(1)));
 
         return answer;
     }
+
     private static boolean checkException(List<Integer> list){
         //페이지가 1 미만 400 초과일 때
         for(int i=0; i< list.size(); i++){
@@ -21,6 +32,42 @@ class Problem1 {
         if(list.get(0)+1 != list.get(1))
             return true;
         return false;
+    }
+
+    private static int getMaxPageNumber(int num){
+        List list = separateNumber(num);
+        return (maxNumber(sumNumbers(list), mulNumbers(list)));
+    }
+
+    private static List<Integer> separateNumber(int num){
+        List<Integer> list = new ArrayList<>();
+        while(num > 0){
+            list.add(num % 10);
+            num = num /10;
+        }
+        return list;
+    }
+
+    private static int sumNumbers(List<Integer> list){
+        int result = 0;
+        for(int i=0; i< list.size(); i++){
+            result += list.get(i);
+        }
+        return result;
+    }
+
+    private static int mulNumbers(List<Integer> list){
+        int result = list.get(0);
+        for(int i=1; i< list.size(); i++){
+            result *= list.get(i);
+        }
+        return result;
+    }
+
+    private static int maxNumber(int num1, int num2){
+        if(num1 > num2)
+            return num1;
+        return num2;
     }
 
 }
