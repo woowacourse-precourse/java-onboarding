ef36e88a3be32f2622615c0cac9dd637083b5dd5
Dabeen Jeong <hello70825@gmail.com>
2022-10-28 15:41:19 +0900

    feat: [Problem3] 3, 6, 9의 개수를 확인하고, 답을 구해주는 기능을 추가하다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,7 @@ public class Problem3 {
         int answer = 0;
 
         if (checkValidation(number)) {
-
+            answer = getAnswer(number);
         } else {
             answer = -1;
         }
@@ -15,10 +15,37 @@ public class Problem3 {
 
     private static boolean checkValidation(int inputNumber) {
         // 1 이상 40,000 이하인 자연수인지 확인하다.
-        if (1 <= inputNumber && inputNumber <= 40000) {
+        if (!(1 <= inputNumber && inputNumber <= 40000)) {
             return false;
         }
 
         return true;
+    }
+
+    private static int getAnswer(int inputNumber) {
+        String nowValueString = null;
+        int result = 0;
+
+        for(int i = 1; i <= inputNumber; i++) {
+           nowValueString = String.valueOf(i);
+
+           result += find369(nowValueString);
+        }
+
+        return result;
+    }
+
+    private static int find369(String inputString) {
+        int result = 0;
+        char nowChar;
+
+        for (int i = 0; i < inputString.length(); i++) {
+            nowChar = inputString.charAt(i);
+            if (nowChar == '3' || nowChar == '6' || nowChar == '9') {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
