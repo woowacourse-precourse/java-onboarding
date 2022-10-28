d6a5babbfa73467d466da47b49a8f879df1e4e59
dclxxi <dclxxi@kakao.com>
2022-10-28 21:09:26 +0900

    feat(문제4): 알파벳을 반대로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,17 +6,32 @@ public class Problem4 {
     public static final char LAST_LOWER = 'z';
     public static final char FIRST_UPPER = 'A';
     public static final char LAST_UPPER = 'Z';
+    public static final char LOWER_SUM = 'a' + 'z';
+    public static final char UPPER_SUM = 'A' + 'Z';
 
 
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
-    public static boolean isLower(char alphabet) {
+
+    private static char conversionChar(char character) {
+        if (isLower(character)) {
+            return (char) (LOWER_SUM - character);
+        }
+
+        if (isUpper(character)) {
+            return (char) (UPPER_SUM - character);
+        }
+
+        return character;
+    }
+
+    private static boolean isLower(char alphabet) {
         return alphabet >= FIRST_LOWER && alphabet <= LAST_LOWER;
     }
 
-    public static boolean isUpper(char alphabet) {
+    private static boolean isUpper(char alphabet) {
         return alphabet >= FIRST_UPPER && alphabet <= LAST_UPPER;
     }
 }
