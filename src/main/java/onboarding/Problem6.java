bf9373c96b20f21d2c7a3cd07bea459714229225
eugene1204 <s1061001@naver.com>
2022-10-28 23:59:35 +0900

    p7 ~ing

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,63 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+//        - 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되어 있는 경우 중복으로 간주한다.
+//        - 크루는 1명 이상 10,000명 이하이다.
+//        - 이메일은 이메일 형식에 부합하며, 전체 길이는 11자 이상 20자 미만이다.
+//        - 신청할 수 있는 이메일은 `email.com` 도메인으로만 제한한다.
+//        - 닉네임은 한글만 가능하고 전체 길이는 1자 이상 20자 미만이다.
+//        - result는 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬하고 중복은 제거한다.
+
+        // 1. 중복확인 메소드
+        // 2. 이메일 형식 확인 메소드
+        // 3. 닉네임 형식 확인 메소드
+        // 4.
+        // map을 이용해서 키, 값으로 만들기
+        Map<String,String> mapFroms = new LinkedHashMap<>();
+
+
+        for (int i =0; i<forms.size(); i++){
+            mapFroms.put(forms.get(i).get(0), forms.get(i).get(1));
+            }
+
+        // Map 전체를 보내서 중복용 메소드 만들기
+
+        List<String> answer = duplicateCheck(mapFroms);
         return answer;
+    }
+
+    public static List<String> duplicateCheck(Map<String, String> map){ // 제이엠, 0 |제이슨, 0|.....
+        int count = 1;
+        String first="";
+        List<String> list = new ArrayList<>();
+        List<String> emailList = new ArrayList<>();
+        Set<String> checkSet = new LinkedHashSet<>();
+
+        // 중복된 값찾고 해당값을 list에 넣기
+        for (Map.Entry<String, String> pair : map.entrySet()) { // 제이엠 제이슨 워니 엠제이 이제엠
+            String s = pair.getValue(); //제이엠
+
+            if(count++==1)  first=s;
+            for(int i =0; i<s.length()-1; i++){ //제이 이엠 제이 이슨 워니 엠제 이제 제엠
+            if(!checkSet.add(s.substring(i,i+2))) {
+                if(i==1) list.add(first);
+                list.add(s);}
+            }
+        }
+        // 중복된 값에 해당하는 key 찾기
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            for(int c =0; c<list.size(); c++){
+                if (value.equals(list.get(c))) {emailList.add(value);}
+            }
+
+        }
+
+        Collections.sort(emailList);
+
+        return emailList;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,70 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        // 1. map으로 user, friends 분리시켜
+        Map<String, String> friendsMap = new LinkedHashMap<>();
+        for (int i =0; i<friends.size(); i++){
+            friendsMap.put(friends.get(i).get(0), friends.get(i).get(1));
+        }
+
+
+        // 6. 단, 리스트 0번에있는 애는 제외시켜
+        Set friendsSet  =addfrieds(friendsMap,user);// andole, jun
+
+        // 4. 방문자를  +1
+
+
+
+        // 5. 리스트에  있어 +10
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+
+    public static LinkedList<String> friendsList(Map<String,String> map , String users){
+        // 2. value값에 mrko가 있으면 그 key값에 해당하는 리스트를 하나 만들어
+
+        //LinkedList<LinkedList<String>> friends = new LinkedList<LinkedList<String>>();
+        LinkedList<String> friends = new LinkedList<String>();
+
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            String s = pair.getValue();
+            String ss = pair.getKey();
+            if(s.equals(users)) friends.add(ss); // shakevan, donut
+        }
+
+        // 3. 이 리스트 이후에는 인덱스 0번의 친구들을 저장해
+
+
+        return friends;
+    }
+
+
+    public static Set addfrieds(Map<String,String> map , String users, List<String> visitors){
+        LinkedList<String> friends =  friendsList(map,users); // shakevan, donut
+        Set friendsSet = new HashSet();
+        int count =0;
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            String userFriends = friends.get(count++);
+            String s = pair.getValue();
+            String ss = pair.getKey();
+            if(s.equals(userFriends)) friendsSet.add(ss); // andole, jun
+        }
+        for(int j =0; j<visitors.size();j++){ //bedi", "bedi", "donut", "bedi", "shakevan"
+
+        }
+
+        return friendsSet;
+    }
+
+
+
+
+
 }
