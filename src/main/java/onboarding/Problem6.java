95757188a1f227c97969d26582087ac0e00bc068
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 21:39:45 +0900

    [Problem6] 잘린 2글자 닉네임으로 중복 확인

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -24,6 +24,26 @@ public class Problem6 {
         return twoWord;
     }
 
+    /**
+     * 2. 잘린 2글자 닉네임으로 중복 확인
+     * @param twoWord : 주어진 닉네임 2글자씩 자른 string list
+     * @param subForms : 비교할 닉네임, 이메일 담긴 Forms
+     * @return : 중복된 닉네임의 이메일
+     */
+    static List<String> findDuplicate(List<String> twoWord, List<List<String>> subForms){
+        List<String> email = new ArrayList<>();
+
+        for (String s : twoWord) {
+            for (List<String> subForm : subForms) {
+                String cmpEmail = subForm.get(0);
+                String cmpName = subForm.get(1);
+                if (cmpName.contains(s)) email.add(cmpEmail);
+            }
+        }
+
+        return email;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
