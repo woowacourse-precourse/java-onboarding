eaa7e637453ed56599b371ad5698ca72a8ba9d33
JJongmen <pjy90123@gmail.com>
2022-10-28 17:34:29 +0900

    알파벳 소문자가 들어오는 경우 알파벳 소문자로 반대로 변환한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        return word;
+        StringBuilder oppositeWord = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            oppositeWord.append(translateLowerCase(word.charAt(i)));
+        }
+        return oppositeWord.toString();
+    }
+
+    private static char translateLowerCase(char ch) {
+        if ('a' <= ch && ch <= 'z') {
+            ch = (char) ('z' - (ch - 'a'));
+        }
+        return ch;
     }
 }
