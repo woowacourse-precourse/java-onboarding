aa80c967e935ee4b6d183b45c1a6d21f5511a21a
jeongwle <dldl4568@naver.com>
2022-10-28 19:43:23 +0900

    [Feat] Problem6 해결  - 중복된 닉네임인 학생의 이메일을 저장하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -15,7 +16,14 @@ public class Problem6 {
         if (!isValidate(forms)) {
             return Collections.emptyList();
         }
-        List<String> answer = List.of("answer");
+
+        String[] nicknames = new String[forms.size()];
+        String[] emails = new String[forms.size()];
+        getNicknames(forms, nicknames);
+        getEmails(forms, emails);
+
+        List<String> answer = getAnswer(nicknames, emails);
+        Collections.sort(answer);
         return answer;
     }
 
@@ -31,37 +39,84 @@ public class Problem6 {
         return true;
     }
 
-    public static boolean validateForm(List<String> form) {
+    private static boolean validateForm(List<String> form) {
         int size = form.size();
         String email = form.get(EMAIL);
         String nickname = form.get(NICKNAME);
         return (validateSize(size) && validateEmail(email) && validateNickname(nickname));
     }
-    public static boolean validateSize(int size) {
+    private static boolean validateSize(int size) {
         return (size > 0 && size < 10001);
     }
 
-    public static boolean validateEmail(String email) {
+    private static boolean validateEmail(String email) {
         return (validateEmailLength(email) && validateEmailPrototype(email));
     }
 
-    public static boolean validateEmailLength(String email) {
+    private static boolean validateEmailLength(String email) {
         return (email != null && email.length() > 10 && email.length() < 20);
     }
 
-    public static boolean validateEmailPrototype(String email) {
+    private static boolean validateEmailPrototype(String email) {
         return Pattern.matches(EMAIL_PATTERN, email);
     }
 
-    public static boolean validateNickname(String nickname) {
+    private static boolean validateNickname(String nickname) {
         return (validateNicknameLength(nickname) && validateLangOfNick(nickname));
     }
 
-    public static boolean validateNicknameLength(String nickname) {
+    private static boolean validateNicknameLength(String nickname) {
         return (nickname != null && nickname.length() > 0 && nickname.length() < 20);
     }
 
-    public static boolean validateLangOfNick(String nickname) {
+    private static boolean validateLangOfNick(String nickname) {
         return Pattern.matches(KOREAN_PATTERN, nickname);
+    }
+
+    /**
+     * 중복된 닉네임이 존재하는 이메일을 저장하는 기능
+     */
+
+    public static void getNicknames(List<List<String>> forms, String[] nicknames) {
+        int i = 0;
+        for (List<String> form : forms) {
+            nicknames[i] = form.get(NICKNAME);
+            i++;
+        }
+    }
+    public static void getEmails(List<List<String>> forms, String[] emails) {
+        int i = 0;
+        for (List<String> form : forms) {
+            emails[i] = form.get(EMAIL);
+            i++;
+        }
+    }
+
+    public static List<String> getAnswer(String[] nicknames, String[] emails) {
+        List<String> answer = new ArrayList<>();
+        int idx = 0;
+        for (String nickname : nicknames) {
+            List<String> result = findDuplicated(nicknames, emails, nickname, idx);
+            if (result.size() != 0) {
+                answer.removeAll(result);
+                answer.addAll(result);
+            }
+            idx++;
+        }
+        return answer;
+    }
+
+    public static List<String> findDuplicated(String[] nicknames, String[] emails, String nickname, int idx) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String twoLetters = nickname.substring(i, i + 2);
+            for (int j = idx + 1; j < nicknames.length; j++) {
+                if (nicknames[j].contains(twoLetters)) {
+                    answer.add(emails[i]);
+                    answer.add(emails[j]);
+                }
+            }
+        }
+        return answer;
     }
 }
