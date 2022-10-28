9df6a08422ad2ef3586dc878c0531f408e5360d6
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-10-28 15:40:14 +0900

    feat: problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
+    private static int countClap(int num) {
+        int cnt = 0;
+
+        while (num > 0) {
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                cnt++;
+            }
+            num /= 10;
+        }
+        return (cnt);
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += countClap(i);
+        }
         return answer;
     }
 }
