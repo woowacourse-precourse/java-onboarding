99929794705bce43adccfab0671628040b7ea579
Kim woo jong <skekdnwhd@gmail.com>
2022-10-28 11:45:29 +0900

    줄 제거, 변수명 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,16 +4,13 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         if (cryptogram.length() < 1 || cryptogram.length() > 1000 || !cryptogram.matches("^[a-z]*$"))
             return cryptogram;
-
         return decoder(cryptogram);
     }
 
     private static String decoder(String cryptogram) {
-        String answer =  cryptogram.replaceAll("([a-z])\\1","");
-
+        String answer = cryptogram.replaceAll("([a-z])\\1","");
         if (answer.matches("^[a-z]*([a-z])\\1[a-z]*$"))
             return decoder(answer);
-
         return answer;
     }
 }
