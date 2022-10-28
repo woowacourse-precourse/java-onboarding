8186b07c5ad032d1d4117902a5ce386da96fdfca
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-28 16:34:18 +0900

    221028 Feat: P1 - 기능1 기능2 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,23 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //1.왼쪽 & 오른쪽 페이지 취득
+        int pLeftPage = pobi.get(0); //97
+        int pRightPage = pobi.get(1); //98
+
+        int cLeftPage = crong.get(0); //197
+        int cRightPage = crong.get(1); //198
+
+        //2.입력값 검증
+        if(pRightPage - pLeftPage != 1 || cRightPage - cLeftPage != 1)
+            return -1;
+
+
+
+        //
         return answer;
     }
+
+
 }
