cced007d8526b5809c8e5959e6c606d04e7744bb
kjb95 <topmedia44@gmail.com>
2022-10-28 15:32:22 +0900

    feat: Problem6 - 교육생 닉네임이 가지는 두 글자 경우의 수를 모두 구하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Problem6 {
 
+    private static final int TWO = 2;
+
     public static List<String> solution(List<List<String>> forms) {
+
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<String> computeTwoLetter(String input) {
+        List<String> twoLetter = new ArrayList<>();
+
+        IntStream.range(0, input.length() - TWO)
+            .forEach(i -> twoLetter.add(input.substring(i, i + TWO)));
+        return twoLetter;
     }
 }
