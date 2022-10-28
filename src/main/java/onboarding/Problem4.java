398dfb96a6f2fe7c20ddaeba741766086042e0a3
wwwndus <simock22@gmail.com>
2022-10-28 18:04:08 +0900

    feat: map에 반대되는 알파벳들을 for문을 진행하여 쌍 형태로 저장

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        Map<String, String> map = new HashMap<>();
+        map.put(" ", " ");
+
+        for (int i = 0; i < 26; i++) {
+            char key = (char) (97 + i);
+            char value = (char) (122 - i);
+
+            map.put(String.valueOf(key), String.valueOf(value));
+        }
+
         return answer;
     }
 }
