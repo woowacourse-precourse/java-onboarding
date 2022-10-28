6d11d2deaa995cd82b8756bfc531857d42494546
Lee Sang Bin <rhkd2612@naver.com>
2022-10-28 19:58:05 +0900

    docs: problem6 methods

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,15 @@ package onboarding;
 
 import java.util.List;
 
+/*
+ * # 기능 정리
+ * [X] 문자열 비교 메서드
+ * [X] 결과 리스트 정렬
+ * [X] 결과 리스트 중복 제거
+ * [X] solution 작성
+ * [X] 테스트 확인
+ */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
