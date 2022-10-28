d8a29ade8fe834b6dfd7050308f43261015fa6e2
sohyundoh <lyny123@naver.com>
2022-10-28 22:05:05 +0900

    Probelm 2 제한 사항 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+/*제한 사항
+    cryptogram은 길이가 1 이상 1000 이하인 문자열이다.
+    cryptogram은 알파벳 소문자로만 이루어져 있다. */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
     }
 }
