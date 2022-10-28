c32c1794b16faf8982a7ed7f1e710c1aeb6e63f9
matt <matt1235@naver.com>
2022-10-29 00:34:58 +0900

    feat: Problem4 (기능 목록2 - 알파벳을 반대로 변환) 기능 구현 알파벳이 소문자인지 대문자인지를 판별해, 그에 맞게 변환한다. 알파벳 이외의 문자는 변환하지 않는다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,6 +10,16 @@ public class Problem4 {
         catch (IllegalArgumentException e){
             return e.toString();
         }
+
+        for (char ch : word.toCharArray()) {
+            int flag = 0;
+            if(isAlphabet(ch)){ // 알파벳인 경우
+                if(isUpperCase(ch)) flag = 1; // 대문자 알파벳
+                else flag = 0; // 소문자 알파벳
+                ch = reverseWord(ch, flag);
+            }
+            answer += ch;
+        }
         return answer;
     }
 
@@ -18,6 +28,32 @@ public class Problem4 {
     private static void check(String word){
         if(word.length() <1 || word.length() >1000)
             throw new IllegalArgumentException("인자의 길이가 맞지 않습니다.");
+    }
+
+    /**
+     * 알파벳인지 판별하는 함수 */
+    private static boolean isAlphabet(char ch){
+        if(ch >= 'a' && ch <= 'z') return true;
+        if(ch >= 'A' && ch <= 'Z') return true;
+        return false;
+    }
+
+    /**
+     * 대문자인지 판별하는 함수 */
+    private static boolean isUpperCase(char ch){
+        if(ch >= 'A' && ch <='Z'){
+            return true;
+        }return false;
+    }
+
+    /**
+     * 알파벳 변환 */
+    private static char reverseWord(char ch, int flag){
+        if(flag==1)
+            return alphabet1['Z'-ch];
+        else if(flag==0)
+            return alphabet2['z'-ch];
+        return '1';
     }
 
 }
