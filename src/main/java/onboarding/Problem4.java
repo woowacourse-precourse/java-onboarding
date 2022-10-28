735f9c29bbcefd0a82c90efd2c30d07d49082ea8
Eunkeee <eunkeeeee@gmail.com>
2022-10-28 12:22:54 +0900

    ✨[P4] feat: 각 문자를 아스키코드로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] chArr = word.toCharArray();
+        for(char c: chArr){
+            System.out.println(c);
+            int ascii = c;
+            System.out.println(ascii);
+        }
+
         return answer;
     }
 }
