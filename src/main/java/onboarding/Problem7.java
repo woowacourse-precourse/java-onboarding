ad5b9a2d43ed0f3ead59d7debd464696e8c22c36
wonho-seo <sjdlrnen0507@naver.com>
2022-10-28 21:59:26 +0900

    [onBoarding-back-Problem7-02][wonho-seo] user의 친구목록을 구하는 것을 구현, 검색 위주로 할것이기 때문에 list보다 더 빠른 속도의 set을 사용

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Set<String> getFriend(String user, List<List<String>> friends){
+        Set<String> userFriend = new HashSet<>();
+
+        for (List<String> list : friends){
+            String friend1 = list.get(0);
+            String friend2 = list.get(1);
+
+            if (user.equals(friend1))
+                userFriend.add(friend2);
+            else if (user.equals(friend2))
+                userFriend.add(friend1);
+        }
+
+        return userFriend;
     }
 }
