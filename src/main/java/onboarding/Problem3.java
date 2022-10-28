74f6a3892fcf629e3e9ab7b44f55d979beb6cec6
kbs_seon <qudtjs0753@naver.com>
2022-10-28 10:59:32 +0900

    [problem3] feat: 3,6,9를 만났을 때 count를 올려주는 기능 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+
+/**
+ * 1. 각 숫자 확인
+ * 2. 손뼉 수 증가
+ * 3.
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+    public static int clapIfDigitIsRight(int digit) {
+        if(digit==3 || digit==6 || digit==9)
+            return 1;
+
+        return 0;
+    }
+
 }
