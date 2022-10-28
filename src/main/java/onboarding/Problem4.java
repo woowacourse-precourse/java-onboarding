7fb41f0fc3c87e369611f0629c5109cf1db4c306
sen <seeunchoi99@gmail.com>
2022-10-28 16:31:12 +0900

    💡 Problem4 기능 목록 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 알파벳 대/소문자 변환
+ * 2. 알파벳을 반대로 (a<->z) 변환
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
