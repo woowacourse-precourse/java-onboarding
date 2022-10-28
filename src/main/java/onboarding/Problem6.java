60c1e30f0dd8fde25bf395bc0114bd8ebee1fa78
kimJaeHyeon <kmr2644@gmail.com>
2022-10-28 23:03:50 +0900

    feat: implement method for getParseNameList

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        return getDuplicatedCrewName(forms);
+    }
+
+    private static List<String> getDuplicatedCrewName(List<List<String>> forms) {
+
+        return null;
+    }
+
+    private static List<String> getParseNameList(List<String> crewInfo) {
+        String crewName = crewInfo.get(1);
+        List<String> parseNameList = new ArrayList<>();
+        for (int i = 0; i < crewName.length() -1; i++) {
+            parseNameList.add(crewName.substring(i, i + 1));
+        }
+        return parseNameList;
     }
 }
