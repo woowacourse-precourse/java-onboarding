c8f1f8df0ebfe53cd13ad48b56ea0283796e88db
Identityless <rlandwnd555@naver.com>
2022-10-28 22:31:42 +0900

    FEAT : 화폐 리스트를 반환하는 MoneyCounter 클래스의 getList() 메서드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,34 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+/*
+*   구현 기능
+*   1. 5만원권 세는 기능
+*   2. 1만원권 세는 기능
+*   3. 5천원권 세는 기능
+*   4. 1천원권 세는 기능
+*   5. 5백원권 세는 기능
+*   6. 1백원권 세는 기능
+*   7. 5십원권 세는 기능
+*   8. 1십원권 세는 기능
+*/
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+}
+
+class MoneyCounter {
+    private int money;
+
+    public MoneyCounter(int money) {
+        this.money = money;
+    }
+
+    public List<Integer> getList() {
+        return List.of();
+    }
+
+
 }
