6c86c1e63ce6b3397c75a10043bce109f980cb1e
Polite-Jehu <*****@***.com>
2022-10-28 20:08:06 +0900

    기능 1 완료
    ===============
    기능흐름
    페이지 차가 1이 아니면 -1 반환
    crong과 pobi의 최대값 서로 비교해(Finder)
    포비가 크면 1
    크롱이 크면 2
    같으면 0 반환
    
    추가메소드
    Finder : 자리수로 쪼개고 더해서 최대값 찾는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,48 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(pobi.get(1)-pobi.get(0)!=1||crong.get(1)-crong.get(0)!=1)
+            return -1;
+
+        int pobiValue = finder(pobi.get(0),pobi.get(1));
+        int crongValue = finder(crong.get(0),crong.get(1));
+
+        if(pobiValue>crongValue)
+            answer=1;
+        else if(pobiValue<crongValue)
+            answer=2;
+        else if(pobiValue==crongValue)
+            answer=0;
+
         return answer;
+    }
+    public static int finder(int left,int right)
+    {
+        int sumLeft=0;
+        int sumRight=0;
+        int multipleLeft=1;
+        int multipleRight=1;
+        int maxValue=0;
+        while(left>0)
+        {
+            sumLeft +=left%10;
+            multipleRight *=left%10;
+            left=left/10;
+        }
+        while(right>0)
+        {
+            sumRight +=right%10;
+            multipleRight *=right%10;
+            right=right/10;
+        }
+        maxValue=sumLeft;
+        if(multipleLeft>maxValue)
+            maxValue=multipleLeft;
+        if(sumRight>maxValue)
+            maxValue=sumRight;
+        if(multipleRight>maxValue)
+            maxValue=multipleRight;
+        return maxValue;
     }
 }
