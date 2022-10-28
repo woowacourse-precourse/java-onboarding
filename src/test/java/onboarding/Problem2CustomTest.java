f0d631afc04adb8309e6f012a1feae18f67e68bc
math0917 <math0917@naver.com>
2022-10-28 19:35:27 +0900

    test : input에 필요한 test case 이름 작성

---
+++ src/test/java/onboarding/Problem2CustomTest.java
@@ -0,0 +1,29 @@
+package onboarding;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class Problem2CustomTest {
+    @Nested
+    class Problem1Test {
+        @Test
+        void 길이1000초과문자열통과못하게() {
+        }
+
+        @Test
+        void 길이0인문자열통과못하게() {
+        }
+
+        @Test
+        void 알파벳이아닌문자열통과못하게() {
+        }
+
+        @Test
+        void 소문자아닌문자열통과못하게() {
+        }
+
+        @Test
+        void 모든걸만족하는문자열이통과하는지() {
+        }
+    }
+}
