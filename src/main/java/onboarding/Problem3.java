06ab5b780d6ab78211872f0ef806cfc154dfc184
HubCreator <chocomilk8604@gmail.com>
2022-10-28 11:15:32 +0900

    refactor(Problem3) : Util 클래스 제거

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import utils.Problem3Util;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Problem3 {
+    static List<Character> entry = Arrays.asList('3', '6', '9');
+
     public static int solution(int number) {
-        return Problem3Util.getResult(number);
+        return getResult(number);
+    }
+
+    public static int getResult(int val) {
+        int answer = 0;
+        for (int i = 1; i <= val; i++) {
+            answer += validCnt(i);
+        }
+        return answer;
+    }
+
+    public static int validCnt(int val) {
+        int result = 0;
+        char[] chars = String.valueOf(val).toCharArray();
+        for (char ch : chars) {
+            if (entry.contains(ch)) result++;
+        }
+        return result;
     }
 }
--- src/test/java/onboarding/Problem3Test.java
+++ src/test/java/onboarding/Problem3Test.java
@@ -1,24 +1,23 @@
 package onboarding;
 
 import org.junit.jupiter.api.Test;
-import utils.Problem3Util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem3Test {
     @Test
     void validNumTest() {
-        assertThat(Problem3Util.validCnt(13)).isEqualTo(1);
-        assertThat(Problem3Util.validCnt(63)).isEqualTo(2);
-        assertThat(Problem3Util.validCnt(19)).isEqualTo(1);
-        assertThat(Problem3Util.validCnt(76)).isEqualTo(1);
-        assertThat(Problem3Util.validCnt(11)).isEqualTo(0);
-        assertThat(Problem3Util.validCnt(27)).isEqualTo(0);
+        assertThat(Problem3.validCnt(13)).isEqualTo(1);
+        assertThat(Problem3.validCnt(63)).isEqualTo(2);
+        assertThat(Problem3.validCnt(19)).isEqualTo(1);
+        assertThat(Problem3.validCnt(76)).isEqualTo(1);
+        assertThat(Problem3.validCnt(11)).isEqualTo(0);
+        assertThat(Problem3.validCnt(27)).isEqualTo(0);
     }
 
     @Test
     void clapCntTest() {
-        assertThat(Problem3Util.getResult(13)).isEqualTo(4);
-        assertThat(Problem3Util.getResult(33)).isEqualTo(14);
+        assertThat(Problem3.getResult(13)).isEqualTo(4);
+        assertThat(Problem3.getResult(33)).isEqualTo(14);
     }
 }
