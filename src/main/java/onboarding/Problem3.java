88c9eb898b40f4113361bd2e6fb838e16c988618
Plastic_hong <zakkslash18@naver.com>
2022-10-28 12:00:56 +0900

    problem3 2.조건 탐색, 함수 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,8 +6,18 @@ public class Problem3 {
         int answer = 0;
         StringBuffer sb = new StringBuffer();
 
+        final char THREE = '3';
+        final char SIX = '6';
+        final char NINE = '9';
+
         for (int i = 0; i < number ; i++) {
             sb.append(i+1);
+        }
+
+        for (int i = 0; i <sb.length() ; i++) {
+            if(sb.charAt(i)==THREE||sb.charAt(i)==SIX||sb.charAt(i)==NINE){
+                answer++;
+            }
         }
 
         return answer;
