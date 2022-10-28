3a8bb864918ab7df46d768fdae9e49c1ceccae98
H4R1B0 <qkrguswns25@naver.com>
2022-10-28 16:42:00 +0900

    solution 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.HashMap;
 
@@ -7,17 +9,36 @@ public class Problem6 {
     private static HashMap<String, Integer> subNicknameDic; //각 닉네임의 두글자씩 카운트하는 딕셔너리
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         subNicknameDic = new HashMap<>(); //닉네임카운트 초기화
         checkAllNickname(forms); //모든 닉네임 확인
+        for(int i=0;i<forms.size();i++){
+            List<String> form = forms.get(i);
+
+            //입력된 form의 사이즈가 2개가 아니라면 continue
+            if(form.size() != 2){
+                continue;
+            }
+
+            String email = form.get(0);
+            String nickname = form.get(1);
+            int count = getCountInDic(nickname);
+            
+            //딕셔너리에 닉네임 부분이 2개 이상이라면 answer에 추가
+            if(count > 1){
+                answer.add(email);
+            }
+        }
+        Collections.sort(answer);
         return answer;
     }
 
     //모든 닉네임들에 대하여 subNicknameCount하는 함수
     private static void checkAllNickname(List<List<String>> forms){
         for(int i=0;i<forms.size();i++){
-            List<String> form = forms.get(0);
-            subNicknameCount(form.get(1));
+            List<String> form = forms.get(i);
+            String nickname = form.get(1);
+            subNicknameCount(nickname);
         }
     }
 
@@ -34,7 +55,7 @@ public class Problem6 {
         int ret = -1;
         for(int i=0;i<nickname.length()-1;i++){
             String subNickname = nickname.substring(i,i+2);
-            ret = Math.max(ret, subNicknameDic.get(subNickname));
+            ret = Math.max(ret, subNicknameDic.getOrDefault(subNickname,0));
         }
         return ret;
     }
