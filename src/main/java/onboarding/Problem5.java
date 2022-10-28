d3b45ea0e921eca6c568ff859001f6dddbe7cd36
hongminjeong <meanj@hongminjeongui-MacBookAir.local>
2022-10-28 14:25:06 +0900

    feat: Add problem5 functions

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.*;
 
+class ChangeMoney{
+    private static List<Integer> bills = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+    private List<Integer> changes;
+    public List<Integer> setChange(int money){
+        this.changes = new ArrayList<>();
+        for(int i=0;i<bills.size();i++){
+            int billCnt = (int)(money/bills.get(i));
+            this.changes.add(billCnt);
+            money -= billCnt*bills.get(i);
+        }
+        return this.changes;
+    }
+}
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        ChangeMoney changeMoney = new ChangeMoney();
+        return changeMoney.setChange(money);
     }
 }
