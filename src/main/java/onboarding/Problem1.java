5186fccf316000068b0afa96ed9729028fffec30
맹순영 <symaeng98@naver.com>
2022-10-28 10:56:06 +0900

    refactor: 문제 1, 2, 3 코드 리팩토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    public static int score(int num){
+    public static int score(int num) {
         List<Integer> numList = new ArrayList<>();
 
-        while(num!=0){
-            numList.add(num%10);
+        while (num != 0) {
+            numList.add(num % 10);
             num /= 10;
         }
         int sum = 0;
@@ -19,6 +19,7 @@ class Problem1 {
         }
         return Math.max(sum, mul);
     }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobiLeft = pobi.get(0);
         int pobiRight = pobi.get(1);
@@ -26,15 +27,15 @@ class Problem1 {
         int crongRight = crong.get(1);
 
         // ----- 유효성 확인 -----
-        if(pobiRight-pobiLeft != 1 || crongRight-crongLeft != 1){
+        if (pobiRight - pobiLeft != 1 || crongRight - crongLeft != 1) {
             return -1;
         }
 
         // ----- 포비의 점수 계산 -----
-        int pobiScore = Math.max(score(pobiLeft),score(pobiRight));
+        int pobiScore = Math.max(score(pobiLeft), score(pobiRight));
 
         // ----- 크롱의 점수 계산 -----
-        int crongScore = Math.max(score(crongLeft),score(crongRight));
+        int crongScore = Math.max(score(crongLeft), score(crongRight));
 
         // ----- 결과 계산 -----
         // 포비와 크롱의 점수가 같다면,
