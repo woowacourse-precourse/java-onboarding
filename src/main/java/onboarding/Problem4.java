9acda5e299ef7681349cf5f0e5454cbd9954f07e
kjb95 <topmedia44@gmail.com>
2022-10-28 12:35:08 +0900

    feat: Problem4 - 청개구리 사전 만들기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,8 +2,20 @@ package onboarding;
 
 public class Problem4 {
 
+    private static final int UPPER_A_ASCII = 'A';
+    private static final int UPPER_Z_ASCII = 'A';
+    private static final int LOWER_A_ASCII = 'a';
+    private static final int LOWER_Z_ASCII = 'z';
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static char treeFrogDictionary(char word) {
+        if (Character.isUpperCase(word)) {
+            return (char) (UPPER_A_ASCII + UPPER_Z_ASCII - (int) word);
+        }
+        return (char) (LOWER_A_ASCII + LOWER_Z_ASCII - (int) word);
     }
 }
