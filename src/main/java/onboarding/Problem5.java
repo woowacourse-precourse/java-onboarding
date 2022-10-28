98ce9efd702b1bd4a62ef4f19660dc476fbcb04f
woosung1223 <dntjd991223@naver.com>
2022-10-28 11:59:13 +0900

    feat(Problem5): splitMoney 함수 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,12 +13,33 @@ public class Problem5 {
         return answer;
     }
 
-    public static boolean ValidationCheck(int money) {
+    public static boolean validationCheck(int money) {
         if (MONEY_LOWERBOUND <= money && money <= MONEY_UPPERBOUND) {
             return true;
         }
         return false;
     }
+    public static List<Integer> splitMoney(int money) {
+        List<Integer> resultList = Arrays.asList(0,0,0,0,0,0,0,0,0);
+        for (int i = 0; i < MONEY_LIST.size(); i++) {
+            int baseMoney = MONEY_LIST.get(i);
+            if (isBigger(money, baseMoney)) {
+                int divi = getQuotient(money, baseMoney);
+                money = updateMoney(money, baseMoney);
+                resultList.set(i, divi);
+            }
+        }
+        return resultList;
+    }
+
 }
 
 
+/*
+* 1. 입력 조건에 맞지 않는 경우 예외처리 기능
+- money가 1 이상 1,000,000 이하의 자연수인지 체크
+2. money가 총 몇 개의 화폐로 구성될 수 있는지 계산하는 기능
+3. money가 해당 화폐로 나누어지는지 확인하는 기능
+4. money를 해당 화폐로 나누고 나눈 몫을 반환하는 기능
+5. 화폐로 나누어짐에 따라 money를 갱신하는 기능
+* */
