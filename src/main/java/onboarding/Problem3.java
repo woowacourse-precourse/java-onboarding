5eafa7988f3c3693f3549364177d2a8ad9c47e04
yeonhee <ydol2@naver.com>
2022-10-29 01:18:10 +0900

    [3번] feat : 박수 수를 증가시키는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,17 +12,25 @@ public class Problem3 {
     }
 
     /**
-     * 3.6.9 개수를 return하는 함수 구현
+     * 3.6.9 개수를 return하는 함수
      */
     private static int get369(int num){
         int count=0;
 
         while (num>0){
-             if(num%10!=0 && num%10%3==0){
-                 count++;
-             }
+             count+=get_count(num);
              num=num/10;
         }
         return count;
+    }
+
+    /**
+     * count 추가하는 함수
+     */
+    private static int get_count(int num){
+        if(num%10!=0 && num%10%3==0)
+            return 1;
+        else
+            return 0;
     }
 }
