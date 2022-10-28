8369b75180ad4e476f22f92538d79df0c02808d6
ddogga <hayeonlee60327@gmail.com>
2022-10-28 21:08:11 +0900

    문제1 기능 요구사항 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -26,9 +26,9 @@ class Problem1 {
                 a_tmpp += n;
                 m_tmpp *= n;
             }
-            System.out.println(p);
-            System.out.println("a_tmpp="+a_tmpp);
-            System.out.println("m_tmpp = " + m_tmpp);
+//            System.out.println(p);
+//            System.out.println("a_tmpp="+a_tmpp);
+//            System.out.println("m_tmpp = " + m_tmpp);
 
 
             if (a_tmpp >= m_tmpp) {
@@ -55,8 +55,8 @@ class Problem1 {
         int crongMax = maxNum(crong);
 
 
-        System.out.println("pobiMax=" + pobiMax);
-        System.out.println("crongMax=" + crongMax);
+//        System.out.println("pobiMax=" + pobiMax);
+//        System.out.println("crongMax=" + crongMax);
 
 
         if (pobiMax == 0 || crongMax == 0) {
@@ -75,29 +75,5 @@ class Problem1 {
         return answer;
     }
 
-    public static void main(String[] args){
 
-        List<Integer> pobi = new ArrayList<Integer>();
-        List<Integer> crong = new ArrayList<Integer>();
-
-
-        Scanner sc = new Scanner(System.in);
-
-
-        for (int i = 0; i < 2; i++){
-            pobi.add(sc.nextInt());
-        }
-
-
-        //nextInt = 공백 구분분
-        for (int i = 0; i < 2; i++){
-            crong.add(sc.nextInt());
-        }
-
-
-        System.out.println(solution(pobi,crong));
-
-
-
-    }
 }
