57a21caf2d5005cf5960a356ba33a9606148f500
math0917 <math0917@naver.com>
2022-10-29 02:08:52 +0900

    refactor : 하나의 기능을 할 수 있는 getTotalClapHandsCount메써드 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,14 +7,18 @@ public class Problem3 {
     private static final int MAX_INPUT = 10000;
     public static int solution(int number) {
         if (Problem3Validation.problem3InputValidation(number, MIN_INPUT, MAX_INPUT)) {
-            int clapHandsCount = 0;
-            for (int i = 1; i <= number; i++) {
-                clapHandsCount += thisTurnClapCounts(i);
-            }
-            return clapHandsCount;
+            return getTotalClapHandsCount(number);
         } else {
             return 0;
         }
+    }
+
+    private static int getTotalClapHandsCount(int number) {
+        int clapHandsCount = 0;
+        for (int i = 1; i <= number; i++) {
+            clapHandsCount += thisTurnClapCounts(i);
+        }
+        return clapHandsCount;
     }
 
     private static int thisTurnClapCounts(int number) {
