79dcb322144fab6bd540c80fb42cf3d38d717c19
김경훈 <kilhyunii@naver.com>
2022-10-28 15:27:43 +0900

    Create solution3: it's naive method. int N -> 1 ~ N repeat i, answer += countThreeSixNine(i) -> return answer 숫자 N이 주어지면, 1부터 N까지 반복하며 그 숫자에 3,6,9가 들어가는 횟수를 세고 모두 더해 반환해줌.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++){
+            answer += countThreeSixNine(i);
+        }
         return answer;
+    }
+    private static int countThreeSixNine(int n){
+        int sum = 0;
+        String s = Integer.toString(n);
+        String[] strArray = s.split("");
+        for(String digit : strArray){
+            if(digit.equals("3") || digit.equals("6") || digit.equals("9")){
+                sum += 1;
+            }
+        }
+        return sum;
     }
 }
