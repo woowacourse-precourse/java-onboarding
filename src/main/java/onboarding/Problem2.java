3e9f28e2ead3dbc926053b758ee0d86fede50483
woogi <dnrlwo11@gmail.com>
2022-10-28 17:12:05 +0900

    [problem2] 연속하는 중복 문자열 삭제 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return cryptogramChecker(cryptogram);
+    }
+    private static String cryptogramChecker(String cryptogram) {
+        StringBuilder str = new StringBuilder(cryptogram);
+        for (int i = 0; i < str.length(); i++) {
+            if ((i + 1) != str.length()) {
+                if (str.charAt(i) == str.charAt(i + 1)) {
+                    str.delete(i, i + 2);
+                    return cryptogramChecker(str.toString());
+                }
+            }
+        }
+        return str.toString();
     }
 }
