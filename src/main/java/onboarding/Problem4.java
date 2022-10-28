6eff917793b5659554deb5d676361b0c078fc44b
dydtmd6801 <dydtmd6801@naver.com>
2022-10-28 22:57:59 +0900

    feat : 대문자, 소문자 반대로 변환 및 공백은 공백으로 변환 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,9 @@ public class Problem4 {
         String answer = "";
         for(int i = 0; i < word.length(); i++) {
             int check = isCheckCase(word.charAt(i));
+            if(check == 0) answer += " ";
+            if(check == 1) answer += upperCase(word.charAt(i));
+            if(check == 2) answer += lowerCase(word.charAt(i));
         }
         return answer;
     }
@@ -13,5 +16,16 @@ public class Problem4 {
         if(Character.isUpperCase(checkWord)) return 1;
         if(Character.isLowerCase(checkWord)) return 2;
         return 0;
+    }
+    private static Character upperCase(Character upperWord){
+        int upper = 155 - upperWord;
+        char upperChar = (char) upper;
+        return upperChar;
+    }
+
+    private static Character lowerCase(Character lowerWord){
+        int lower = 219 - lowerWord;
+        char lowerChar = (char) lower;
+        return lowerChar;
     }
 }
