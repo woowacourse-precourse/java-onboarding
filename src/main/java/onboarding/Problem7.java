7d67b09be0892b06bc9e05bb9dfc5d05c1d2c4ea
jiwon1217 <koagjw@gmail.com>
2022-10-29 04:44:06 +0900

    [FEATURE] Solve the Problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,50 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer;
+        Map<String, Integer> pointOfUsers = new HashMap<>();
+        Set<String> friendsList = new HashSet<>();
+
+        for (List<String> friend : friends) {
+            String userA = friend.get(0);
+            String userB = friend.get(1);
+            friendsList.add(userA);
+
+            if (!Objects.equals(userB, user)) {
+                if (!pointOfUsers.containsKey(userB)) {
+                    pointOfUsers.put(userB, 10);
+                } else {
+                    pointOfUsers.put(userB, pointOfUsers.get(userB) + 10);
+                }
+            }
+        }
+
+        for (String visitor : visitors) {
+            if (!friendsList.contains(visitor)) {
+                if (!pointOfUsers.containsKey(visitor)) {
+                    pointOfUsers.put(visitor, 1);
+                } else {
+                    pointOfUsers.put(visitor, pointOfUsers.get(visitor) + 1);
+                }
+            }
+        }
+
+        answer = new ArrayList<>(pointOfUsers.keySet());
+
+        answer.sort((obj1, obj2) -> {
+            if (!Objects.equals(pointOfUsers.get(obj1), pointOfUsers.get(obj2))) {
+                return pointOfUsers.get(obj2) - pointOfUsers.get(obj1);
+            }
+            return obj1.compareTo(obj2);
+        });
+
+        if (answer.size() > 5) {
+            answer = answer.subList(0, 5);
+        }
+
         return answer;
     }
 }
