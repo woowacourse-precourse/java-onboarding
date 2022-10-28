38362bfe407b00c5b16625ebfb3bc441e4a88d38
phobyjun <phobyjun@gmail.com>
2022-10-28 21:42:46 +0900

    [PROBLEM2] Fix Typo: reapeatedNum() -> repeatedNum()

--- src/test/java/onboarding/Problem2MethodTest.java
+++ src/test/java/onboarding/Problem2MethodTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static onboarding.Problem2.reapeatedNum;
 import static onboarding.Problem2.removeRepeated;
+import static onboarding.Problem2.repeatedNum;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Problem2MethodTest {
@@ -15,9 +15,9 @@ public class Problem2MethodTest {
         String cryptogram1 = "azzzac";
         String cryptogram2 = "zzzzzzz";
         String cryptogram3 = "zxczxc";
-        assertThat(reapeatedNum(cryptogram1)).isEqualTo(List.of(1, 1, 3));
-        assertThat(reapeatedNum(cryptogram2)).isEqualTo(List.of(1, 0, 6));
-        assertThat(reapeatedNum(cryptogram3)).isEqualTo(List.of(-1, -1, -1));
+        assertThat(repeatedNum(cryptogram1)).isEqualTo(List.of(1, 1, 3));
+        assertThat(repeatedNum(cryptogram2)).isEqualTo(List.of(1, 0, 6));
+        assertThat(repeatedNum(cryptogram3)).isEqualTo(List.of(-1, -1, -1));
     }
 
     @Test
