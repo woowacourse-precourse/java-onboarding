a63293408cd77692e1ef11b4110bf4c24b22865d
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 18:52:01 +0900

    fix: PROBLEM6 닉네임 규칙 저장 기능 수정
    - docs: PROBLEM6 기능 개발 체크리스트 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,11 +3,13 @@ package onboarding;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
-		List<String> answer = List.of("answer");
-		return answer;
+		// NicknameFilter nicknameFilter = new NicknameFilter();
+		// return nicknameFilter.filter(forms);
+		return null;
 	}
 
 	static class NicknameFilter {
@@ -15,11 +17,25 @@ public class Problem6 {
 		private static final String EMAIL_FORM = "@email.com";
 
 		private final HashMap<String, String> userMap;
-		private final HashSet<String> nicknamePatternSet;
 
 		NicknameFilter() {
 			this.userMap = new HashMap<>();
-			this.nicknamePatternSet = new HashSet<>();
+		}
+
+		private boolean isDuplicatedPattern(String nickname, HashSet<String> nicknamePattern) {
+			String[] split = nickname.split("");
+			int i = 0;
+			while (true) {
+				String pattern = split[i] + split[i + 1];
+
+				if (nicknamePattern.contains(pattern)) {
+					return true;
+				}
+				if (i == split.length - 2) {
+					return false;
+				}
+				i++;
+			}
 		}
 
 		private void initUserInfo(List<String> forms) {
@@ -27,22 +43,23 @@ public class Problem6 {
 			email = emailParser(email);
 			String nickname = forms.get(1);
 
-			nicknamePatternCreator(nickname);
-
 			userMap.put(nickname, email);
 		}
 
-		private void nicknamePatternCreator(String nickname) {
+		private HashSet<String> nicknamePatternCreator(String nickname) {
 			String[] split = nickname.split("");
+
+			HashSet<String> nicknamePattern = new HashSet<>();
 			int i = 0;
 
 			while (true) {
-				nicknamePatternSet.add(split[i] + split[i + 1]);
+				nicknamePattern.add(split[i] + split[i + 1]);
 				if (i == split.length - 2) {
 					break;
 				}
 				i++;
 			}
+			return nicknamePattern;
 		}
 
 		private String emailParser(String email) {
