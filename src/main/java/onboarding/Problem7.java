8b88a9b388ede3baf4829f7726b8e890f8c501ed
EUNJEONGMUN <munej26@gmail.com>
2022-10-28 12:06:38 +0900

    feat: 한 사람당 친구 목록 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,14 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String, List<String>> contacts = new HashMap<>();
+        for (List<String> friend : friends) {
+            String peopleA = friend.get(0);
+            String peopleB = friend.get(1);
+
+            contacts.put(peopleA, makeFriendList(peopleB, contacts.get(peopleA)));
+            contacts.put(peopleB, makeFriendList(peopleA, contacts.get(peopleB)));
+        }
         return answer;
     }
 
