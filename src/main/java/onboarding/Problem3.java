d79235a75bc54a19a3801636b645a9e19f02557b
chj <bluedog129@gmail.com>
2022-10-28 14:20:23 +0900

    탐색한 숫자 마다 자릴수를 나누는 작업 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,17 +1,23 @@
 package onboarding;
 
 public class Problem3 {
-    private static int divideNumber(int number, int count) {
+    private static int divideNumber(int number) {
+        int count = 0;
+
+        while (number > 0) {
+            if (checkThreeSixNine(number) == 1) {
+                count++;
+            }
+            number /= 10;
+        }
         return count;
     }
     public static int solution(int number) {
-        int count = 0;
         int result = 0;
 
         for (int i = 1; i <= number; i++) {
-            result += divideNumber(number, count);
+            result += divideNumber(i);
         }
-
         return result;
     }
 }
