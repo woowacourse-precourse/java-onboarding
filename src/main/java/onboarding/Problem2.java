3539d256df860376bb07a01751f9fe9266b5eaa7
Yongwoo Choi <cyw7515@naver.com>
2022-10-28 14:21:17 +0900

    fix(problem2): decryption 과정에서 3개 이상 연속으로 들어오는 중복 문자도 처리할 수 있도록 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,6 +10,32 @@ public class Problem2 {
         return answer;
     }
 
+    private static Stack<Character> decryption(String input) {
+        Stack<Character> result = new Stack<>();
+        Character duplicate = null;
+
+        for (int i = 0; i < input.length(); i++) {
+            Character cur = input.charAt(i);
+
+            if (result.isEmpty() && duplicate == null) {
+                result.push(cur);
+                continue;
+            }
+
+            if (!result.isEmpty() && result.peek() == cur) {
+                duplicate = cur;
+                result.pop();
+                continue;
+            }
+
+            if (duplicate != cur) {
+                result.push(cur);
+            }
+        }
+
+        return result;
+    }
+
     private static String concatStack(Stack<Character> nonDuplicates) {
         StringBuilder concatString = new StringBuilder();
 
@@ -19,28 +45,5 @@ public class Problem2 {
 
         return concatString.toString();
     }
-
-    private static Stack<Character> decryption(String input) {
-        Stack<Character> result = new Stack<>();
-        int index = 0;
-
-        while (index < input.length()) {
-            if (result.isEmpty()) {
-                result.push(input.charAt(index));
-            }
-            else {
-                if (result.peek() == input.charAt(index)) {
-                    result.pop();
-                }
-                else {
-                    result.push(input.charAt(index));
-                }
-            }
-            index += 1;
-        }
-
-        return result;
-    }
-
 
 }
