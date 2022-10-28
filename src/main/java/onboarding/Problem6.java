4ce943455ae505d2de8e4c71a1d2c6a867dc1221
sugamypapa <sk000801@naver.com>
2022-10-28 22:43:05 +0900

    fix: 문제 6 정렬 함수 삭제 및 결과 리스트 반환 함수 수정
    정렬이 그 정렬이 아니었다.....

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,7 +10,6 @@ public class Problem6 {
     // 각 닉네임마다 두글자씩 잘라서 배열에 받는 함수 생성
     // 두 배열씩 비교하여 겹치는 값이 있는지 확인하는 함수 생성
     // 겹치는 배열이 존재하는 아이디만 정렬 후 반환
-    // 정렬 시 "@email.com"은 제외하고 정렬 뒤 다시 뒷문장은 붙여줌
 
     public static String address = "@email.com";
     public static List<String> nameDivideTwo(String s) {
@@ -42,33 +41,28 @@ public class Problem6 {
         return hasCommon;
     }
     public static List<String> finalNameList(List<List<String>> nameSplit, List<List<String>> forms) {
+        // 쪼개진 이름 리스트들과 문제에서 주어진 조건 리스트를 인자로 받는
         List<String> emailList = new ArrayList<>();
         for (int i = 0; i < nameSplit.size()-1; i++) {
             if(hasCommon(nameSplit.get(i), nameSplit.get(i+1))) {
-                if(!emailList.contains(nameSplit.get(i).get(0))) {
+                // nameSplit.get(i) -> 큐티빠찌뽀찌 = 큐티, 빠찌, 뽀찌
+                if(!emailList.contains(forms.get(i).get(0))) {
+                // 이메일 이름 리스트에 이미 주어진 이메일이 존재한다면
                     emailList.add(forms.get(i).get(0));
                 }
-                emailList.add(forms.get(i).get(1));
+                emailList.add(forms.get(i+1).get(0));
             }
         }
         return emailList;
     }
-    public static List<String> sortList(List<String> list) {
-        List<String> newList = new ArrayList<>();
-        for (String s : list) {
-            s = s.replace(address, "");
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            newList.add(Arrays.toString(arr)+address);
-        }
-        return newList;
-    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<List<String>> nameSplit = new ArrayList<>();
         for (List<String> form : forms) {
-            nameSplit.add(nameDivideTwo(form.get(1)));
+            nameSplit.add(nameDivideTwo(form.get(1))); //이름을 받아와 쪼갬
         }
         List<String> emailList = finalNameList(nameSplit, forms);
-        return sortList(emailList);
+        Collections.sort(emailList);
+        return emailList;
     }
 }
