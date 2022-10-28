cf91307d9ddac49946bab49a95cd0d3df877769d
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-28 12:48:01 +0900

    refactor: [Problem5] - 상수 추가로 의미 명확화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    
+
+    private static final int DID_NOT_EXCHANGE_THIS_UNIT = 0;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         List<Integer> moneyUnits = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
@@ -17,7 +19,7 @@ public class Problem5 {
 
     private static int exchangeMoney(int money, List<Integer> answer, int moneyUnit) {
         if (money < moneyUnit) {
-            answer.add(0);
+            answer.add(DID_NOT_EXCHANGE_THIS_UNIT);
             return money;
         }
 
