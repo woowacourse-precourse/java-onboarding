9261588c42ca5ace24c60e737fd905580691da33
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-28 18:22:24 +0900

    feat: PROBLEM6 닉네임 규칙 저장 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,34 +3,50 @@ package onboarding;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = List.of("answer");
+		return answer;
+	}
 
-    static class NicknameFilter {
+	static class NicknameFilter {
 
-        private static final String EMAIL_FORM = "@email.com";
+		private static final String EMAIL_FORM = "@email.com";
 
-        private final HashMap<String, String> userMap;
+		private final HashMap<String, String> userMap;
+		private final HashSet<String> nicknamePatternSet;
 
-        NicknameFilter() {
-            this.userMap = new HashMap<>();
-        }
+		NicknameFilter() {
+			this.userMap = new HashMap<>();
+			this.nicknamePatternSet = new HashSet<>();
+		}
 
-        private void initUserInfo(List<String> forms) {
-            String email = forms.get(0);
-            email = emailParser(email);
-            String nickname = forms.get(1);
+		private void initUserInfo(List<String> forms) {
+			String email = forms.get(0);
+			email = emailParser(email);
+			String nickname = forms.get(1);
 
-            userMap.put(nickname, email);
-        }
+			nicknamePatternCreator(nickname);
 
-        private String emailParser(String email) {
-            return email.replaceAll(EMAIL_FORM, "");
-        }
-    }
+			userMap.put(nickname, email);
+		}
+
+		private void nicknamePatternCreator(String nickname) {
+			String[] split = nickname.split("");
+			int i = 0;
+
+			while (true) {
+				nicknamePatternSet.add(split[i] + split[i + 1]);
+				if (i == split.length - 2) {
+					break;
+				}
+				i++;
+			}
+		}
+
+		private String emailParser(String email) {
+			return email.replaceAll(EMAIL_FORM, "");
+		}
+	}
 }
