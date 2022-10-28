bc10b03597055d7ecd48bbaa957b684cd97a427d
HamSeungChan <head0618@naver.com>
2022-10-28 16:06:22 +0900

    feat: Problem1 페이지 랜덤 선택 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+  public static int solution(List<Integer> pobi, List<Integer> crong) {
+    int answer = Integer.MAX_VALUE;
+    pobi = BookPageSelect();
+    crong = BookPageSelect();
+
+    return answer;
+  }
+
+  private static List<Integer> BookPageSelect(){
+    List<Integer> bookPageSelect = new ArrayList<>();
+    Random random = new Random();
+    int randomPageSelect = Integer.MAX_VALUE;
+    do{
+      randomPageSelect = random.nextInt(200)*2+1;
+    }while(randomPageSelect==1||randomPageSelect==399);
+
+    bookPageSelect.add(randomPageSelect);
+    bookPageSelect.add(randomPageSelect+1);
+
+    return bookPageSelect;
+  }
+
 }
