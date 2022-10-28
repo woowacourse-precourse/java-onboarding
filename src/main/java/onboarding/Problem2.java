d05834222b880492bfe7e1fc8dabbb5e4291169f
eora21 <eora21@naver.com>
2022-10-29 08:34:50 +0900

    feat: 🎸 추가 해독문의 마지막 문자와 암호문의 문자가 같다면 건너뛰는 메서드
    연속하는 중복 문자들은 삭제되어야 한다. 해독문의 마지막 문자를 기준으로 같은 문자들이 들어온다면 사라지게 된다. 따라서 암호문을
    차례로 살펴보며, 해독문의 마지막 문자와 같은지를 확인한다. 같은 문자가 있었다면 idx와 newIdx는 차이가 날 것이다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
+
+    private static int increaseTail(Character decryptionLastChar, String cryptogram, int idx) {
+
+        while (decryptionLastChar == cryptogram.charAt(idx)) {
+            idx++;
+        }
+
+        return idx;
+    }
+
+    /**
+     * 임의의 문자열 cryptogram이 매개변수로 주어질 때, 연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 메서드를 완성하라.
+     *
+     * @param cryptogram 임의의 암호문 문자열
+     * @return 해독한 문자열
+     */
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Deque<Character> decryptionCharStack = new ArrayDeque<>();
+        int idx = 0;
+
+        while (idx < cryptogram.length()) {
+            int newIdx = increaseTail(decryptionCharStack.peekLast(), cryptogram, idx);
+            checkSkipCharacter(decryptionCharStack, idx, newIdx);
+            updateDeque(decryptionCharStack, cryptogram.charAt(newIdx));
+            idx = newIdx + 1;
+        }
+
+        return decryptionCharStackToString(decryptionCharStack);
     }
 }
