f7919891f37b2435cc1679eb3a3a696a0adf6052
kiarakim <kiarakim413@gmail.com>
2022-10-28 15:49:02 +0900

    feat(Problem4): get each element from String

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int wordLength = word.length();
+
+        for (int i = 0; i < wordLength; i++) {
+            char nowChar = word.charAt(i);
+        }
         return answer;
     }
 }
