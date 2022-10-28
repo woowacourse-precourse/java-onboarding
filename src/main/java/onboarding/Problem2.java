53df0079ce192d728a18257f998cf239e7a45424
JunDonghyuk <ehdgur4814@naver.com>
2022-10-28 21:07:12 +0900

    Solve Problem2
    1. 풀이
    Stack을 사용하여 push할지 skip할지 pop할지 결정
    나중에 로직 한 번 더 생각해 볼 필요

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,44 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<String> res = new Stack<String>();
+        String prev = "";
+
+        for(String str : cryptogram.split("")) {
+            pushOrSkip(str, prev, res);
+            prev = str;
+        }
+
+        while(!res.isEmpty()) {
+            answer = res.pop() + answer;
+        }
         return answer;
     }
+
+    public static void pushOrSkip(String cur, String prev, Stack<String> res) {
+        // flag가 true면 stack에 push
+        boolean flag = false;
+
+        // 이전 문자와 현재문자가 같지 않다면 push할 가능성
+        if(!cur.equals(prev)) {
+            flag = true;
+        }
+
+        // 스택의 가장 위의 원소와 같다면 스택의 peek을 pop하고 새로운 원소를 push하지 않음
+        if(!res.isEmpty()) {
+            if(res.peek().equals(cur)) {
+                res.pop();
+                flag = false;
+            }
+        }
+
+        if(flag) {
+            res.push(cur);
+        }
+    }
+
 }
