9e0008de339442cc0b52cf9a39aead866885360d
chj <bluedog129@gmail.com>
2022-10-28 18:52:10 +0900

    [P4] 길이, 알파베 외 다른 문자 예외처리 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+    private static int checkWord(String word) {
+        if ((word.length() < 1) || (word.length() > 1000)) {
+            return 0;
+        }
+        for (char c : word.toCharArray()) {
+            if (c == ' ') continue;
+            if (!Character.isAlphabetic(c)) return -1;
+        }
+        return 1;
+    }
     private static char convertAscii(char c) {
         if ('a' <= c && 'z' >= c) {
             c = (char)('z' - (c - 'a'));
@@ -10,6 +20,12 @@ public class Problem4 {
         return c;
     }
     public static String solution(String word) {
+        if (checkWord(word) == 0) {
+            return "StringLengthError";
+        } else if (checkWord(word) == -1) {
+            return "isNotAlphaError";
+        }
+
         char[] wordArr = word.toCharArray();
         for (int i = 0; i < word.length(); i++) {
             wordArr[i] = convertAscii(wordArr[i]);
