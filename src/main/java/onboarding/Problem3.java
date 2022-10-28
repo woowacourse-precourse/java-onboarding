0a6f38bbc910776b56c64888107461783458608d
jwp345 <jwp135@naver.com>
2022-10-28 19:01:20 +0900

    problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        while(number > 0) {
+            for (char ch : String.valueOf(number--).toCharArray()) {
+                if(ch == '3' || ch == '6' || ch == '9') answer++;
+            }
+        }
         return answer;
     }
 }
