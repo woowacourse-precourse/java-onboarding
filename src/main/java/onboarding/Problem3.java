50282e812fa4512bf222e52ca0bc97e02f50f4b3
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-28 12:07:52 +0900

    feat: Add a function that count multiple of 3 in number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    // 입력받은 숫자에 3의 배수가 몇 개인지 반환하는 함수
+    public static int getCount(int number) {
+        int count = 0;
+        while (number > 10) {
+            if ((number % 10) % 3 == 0) count++;
+            number /= 10;
+        }
+
+        if (number % 3 == 0) count++;
+
+        return count;
+    }
 }
+
