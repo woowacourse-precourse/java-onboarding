cc42c2304d8093aeb2c260a8659b75540eff0d98
ygreenb <nhj7911@gmail.com>
2022-10-28 10:40:46 +0900

    feat: Problem6.1 닉네임끼리 중복 여부 return하는 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,14 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static boolean isDuplicateNickName(String nick1, String nick2) {
+        for(int index=0;index<nick1.length()-1;index++){
+            String str = nick1.substring(index,index+2);
+            if(nick2.contains(str)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
