0bb71559d498781e243b72fe1ff926bd27958f91
Heosejin98 <sejin3319@naver.com>
2022-10-29 03:05:49 +0900

    Refactor : PROBLEM2 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,37 +2,70 @@ package onboarding;
 
 
 import java.util.Stack;
+import java.util.stream.Stream;
 
 public class Problem2 {
-    static boolean Exception(String cryptogram) {
-        if (cryptogram.length() < 1 || 1000 <cryptogram.length())
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
+    private static final String ERROR = "ERROR";
+
+    public static String solution(String cryptogram) {
+        String answer;
+
+        if (exception(cryptogram)) {
+            return ERROR;
+        }
+        answer = decryptPassword(cryptogram);
+
+        return answer;
+    }
+
+    private static boolean exception(String cryptogram) {
+        if (cryptogram.length() < MIN_LENGTH || MAX_LENGTH < cryptogram.length()) {
             return true;
+        }
         for (char c : cryptogram.toCharArray()) {
-            if (!(Character.isLowerCase(c)))
+            if (!(Character.isLowerCase(c))) {
                 return true;
+            }
         }
         return false;
     }
-    static boolean CheckDuplicate(String str){
+
+    private static String decryptPassword(String cryptogram) {
+        Stack<String> duplicate;
+        String result = cryptogram;
+
+        while (checkDuplicate(result)) {
+            duplicate = duplicateStr(result);
+            result = deleteDuplicate(duplicate, result);
+        }
+
+        return result;
+    }
+
+    private static boolean checkDuplicate(String str) {
         char front_char;
         char back_char;
 
-        for (int i = 0; i < str.length() - 1; i++){
+        for (int i = 0; i < str.length() - 1; i++) {
             front_char = str.charAt(i);
             back_char = str.charAt(i + 1);
 
-            if (front_char == back_char)
+            if (front_char == back_char) {
                 return true;
+            }
         }
-        return  false;
+        return false;
     }
 
-    static Stack<String> DuplicateStr(String str){
+    private static Stack<String> duplicateStr(String str) {
         String front_char;
         String back_char;
         Stack<String> result = new Stack<>();
 
-        for (int i = 0; i < str.length() - 1; i++){
+        for (int i = 0; i < str.length() - 1; i++) {
             front_char = "" + str.charAt(i);
             back_char = "" + str.charAt(i + 1);
 
@@ -40,9 +73,10 @@ public class Problem2 {
                 result.push(front_char + back_char);
             }
         }
-        return  result;
+        return result;
     }
-    static String DeleteDuplicate(Stack<String> duplicate, String str) {
+
+    private static String deleteDuplicate(Stack<String> duplicate, String str) {
         String result = str;
 
         while (!duplicate.empty()) {
@@ -50,17 +84,6 @@ public class Problem2 {
         }
         return result;
     }
-    public static String solution(String cryptogram) {
-        String answer = cryptogram;
-        Stack<String> duplicate;
 
-        if (Exception(cryptogram))
-            return answer;
 
-        while (CheckDuplicate(answer)) {
-            duplicate = DuplicateStr(answer);
-            answer = DeleteDuplicate(duplicate, answer);
-        }
-        return answer;
-    }
 }
