13711a2112f6f00e59c9bf0fe97921cddbcf81b3
jkde7721 <jkde7721@naver.com>
2022-10-29 01:26:47 +0900

    [문제 7] friends를 friend 인접 리스트로 변환하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String, List<String >> friendAdjacencyList = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        convertToFriendAdjacencyList(friends);
+
+        
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void convertToFriendAdjacencyList(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String firstFriend = friend.get(0), secondFriend = friend.get(1);
+            linkWithFriend(firstFriend, secondFriend);
+            linkWithFriend(secondFriend, firstFriend);
+        }
+    }
+
+    private static void linkWithFriend(String me, String friend) {
+        friendAdjacencyList.computeIfAbsent(me, k -> new ArrayList<>());
+        friendAdjacencyList.get(me).add(friend);
     }
 }
