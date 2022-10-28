8929c9e2964cc77f42ab09cec61466e958fee9cf
ywj9811 <ywj9811@naver.com>
2022-10-28 19:51:56 +0900

    소문자 알파벳과 대문자 알파벳 각각의 리스트 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int idx;
+        //a ~ z까지 담고 있는 List를 만들자 대문자 소문자 각각
+        List<Character> alphabetS = new ArrayList<>();
+        List<Character> alphabetL = new ArrayList<>();
+        for (int i = (int)'a'; i <= (int)'z'; i++){
+            alphabetS.add((char)i);
+        }
+        for (int i = (int)'A'; i <= (int)'Z'; i++) {
+            alphabetL.add((char)i);
+        }
+
         return answer;
     }
+    /**
+     * 알파벳 순서대로 했을 때 역순으로 대칭되는 값을 출력하자
+     * 소문자 -> 소문자
+     * 대문자 -> 대문자
+     */
 }
