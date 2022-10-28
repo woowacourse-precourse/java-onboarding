429d87d15e99dff746b7e5bd41287e14350bf2df
yewonbahn <banyaewon98@gmail.com>
2022-10-28 17:15:50 +0900

    problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
-
+import java.util.HashMap;
 public class Problem4 {
     public static String solution(String word) {
+        String a="";
+        for(int i=0;i<word.length();i++){
+            int num =(int)word.charAt(i);
+
+            if(65<=num && num<=90){
+                if(num+25>90){
+                    num=90-(num+25-90);
+                }else{
+                    num=num+25;
+                }
+            }else{
+            if(num+25>122){
+                num=122-(num+25-122);
+            }else{
+                num=num+25;
+            }
+            }
+            if(num=='9'){
+                a+=" ";
+            }
+            else {
+                a+=(char)num;
+            }
+            System.out.println((char)num);
+        }
         String answer = "";
-        return answer;
+        return a;
     }
 }
