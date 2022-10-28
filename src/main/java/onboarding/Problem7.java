31f1e421794613158b5513eeff9e3b35768ea85c
woosung1223 <dntjd991223@naver.com>
2022-10-28 21:35:10 +0900

    feat(Problem7): setDistinctID 함수 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static HashMap<Integer, Integer> weight = new HashMap<>();
+    private static HashMap<String, Integer> uniqueIDs = new HashMap<>();
+    private static int forID = 0;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void setDistinctID(List<String> friends) { // getDistinctFriends 이후 호출됨
+        for (int i = 0; i < friends.size(); i++) {
+            String name = friends.get(i);
+            uniqueIDs.put(name, forID++);
+        }
+    }
+    public static List<String> getDistinctFriends(List<List<String>> friends) {
+        List<String> forJoin = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> relation = friends.get(i);
+            forJoin.addAll(relation);
+        }
+        Set<String> distinctSet = new HashSet<>(forJoin);
+        List<String> distinctList = new ArrayList<>(distinctSet);
+        return distinctList;
     }
 }
