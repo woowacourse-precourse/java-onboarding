206972684fe7cac8e6ac2a350fa68cacf21e3b7e
JuHyeokParkk <hyeok5596@naver.com>
2022-10-28 15:55:35 +0900

    feat(Problem5): solution 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,8 +9,10 @@ public class Problem5 {
     static final List<Integer> kindOfMoney = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        List<Integer> result = withdraw(money);
+
+        return result;
     }
 
     static List<Integer> withdraw(int money) {
