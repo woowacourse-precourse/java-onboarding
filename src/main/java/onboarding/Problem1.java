0bd61e2734a7b2c2e0b2998f4bf8e33d529e8f6b
Egenieee <hhgg0925@naver.com>
2022-10-28 20:53:34 +0900

    [FEAT] 예외사항 처리 로직 추가
    - 두 페이지 차이가 1이 아닐 경우와 왼쪽 페이지가 짝수로 시작 또는 오른쪽 페이지가 홀수로 시작하는 경우 -1 반환하도록 한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,22 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (isInvalidPages(pobi) || isInvalidPages(crong)) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private static boolean isInvalidPages(List<Integer> pages) {
+        if (pages.get(1) - pages.get(0) != 1) {
+            return true;
+        }
+
+        if (pages.get(0) % 2 == 0 || pages.get(1) % 2 == 1) {
+            return true;
+        }
+
+        return false;
     }
 }
