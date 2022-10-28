34b9109b2da1bcb8f802676a19542573738bea0f
woo-chang <clllickme@naver.com>
2022-10-28 19:26:39 +0900

    feat: 사전 정보에 대한 문자열 변환 기능 구현 (Probelm4)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,45 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+  public static String solution(String word) {
+    return Converter.convert(word);
+  }
+
+  static class Converter {
+
+    public static String convert(String words) {
+      StringBuilder result = new StringBuilder();
+      for (char word : words.toCharArray()) {
+        result.append(wordConvert(word));
+      }
+      return result.toString();
     }
+
+    private static char wordConvert(char word) {
+      if (isUpperCase(word)) {
+        return upperCaseConvert(word);
+      } else if (isLowerCase(word)) {
+        return lowerCaseConvert(word);
+      }
+      return word;
+    }
+
+    private static boolean isUpperCase(char word) {
+      return Character.isUpperCase(word);
+    }
+
+    private static boolean isLowerCase(char word) {
+      return Character.isLowerCase(word);
+    }
+
+    private static char upperCaseConvert(char word) {
+      return (char) ('Z' - word + 'A');
+    }
+
+    private static char lowerCaseConvert(char word) {
+      return (char) ('z' - word + 'a');
+    }
+
+  }
 }
