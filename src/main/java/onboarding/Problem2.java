675784c1d15b72d149c664dc141842eb6796b1d9
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-10-28 23:34:22 +0900

    feat(Problem2): 형변환 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
+    public static List<String> stringToList(String cryptogram) {
+        String[] cryptogramArray = cryptogram.split("");
+        List<String> result = new ArrayList<>(Arrays.asList(cryptogramArray));
+
+        return result;
+    }
+
+    public static String listToString(List<String> stringList) {
+        String result = stringList.toString()
+            .replace("[", "")
+            .replace("]", "")
+            .replace(",", "")
+            .replace(" ", "");
+
+        return result;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
