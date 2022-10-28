2c56819d0845dda68f21aebe97de17c192b92272
baeseohyeon <formnarago@naver.com>
2022-10-28 12:38:04 +0900

    feat: 문제4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                answer.append((char) ('Z' - (c - 'A')));
+            } else if (c >= 'a' && c <= 'z') {
+                answer.append((char) ('z' - (c - 'a')));
+            } else {
+                answer.append(c);
+            }
+        }
+        return answer.toString();
     }
 }
