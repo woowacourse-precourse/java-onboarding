fe5ddc07cbb308743628ed3274fcd050cef1241e
맹순영 <symaeng98@naver.com>
2022-10-28 10:50:06 +0900

    feat: 한 숫자에 대해 3,6,9의 개수를 세는 메서드 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+    // number에서 3, 6, 9의 개수를 세주는 메서드
+    public static int count(int number) {
+        int cnt = 0;
+        while (number != 0) {
+            int n = number % 10;
+            if (n == 3 || n == 6 || n == 9) {
+                cnt++;
+            }
+            number /= 10;
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return 0;
     }
 }
