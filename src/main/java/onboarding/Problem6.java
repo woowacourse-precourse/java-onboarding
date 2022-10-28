a6b57d92d1d1dcf4edf39c900806e37c6523bc64
sugamypapa <sk000801@naver.com>
2022-10-28 18:00:33 +0900

    chore: 문제 6 간단한 아이디어

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    // 처음 아이디어
+    // 각 닉네임마다 두글자씩 잘라서 배열에 받는 함수 생성
+    // 두 배열씩 비교하여 겹치는 값이 있는지 확인하는 함수 생성
+    // 겹치는 배열이 존재하는 아이디만 정렬 후 반환
+
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
