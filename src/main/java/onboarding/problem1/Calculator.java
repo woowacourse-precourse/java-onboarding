789c4a13f95df0972ddf24f01439e1d6c25fc6c2
joohyeongKim <wnguddl96@knou.ac.kr>
2022-10-28 15:53:14 +0900

    리팩터링 : else 제거
    Signed-off-by: joohyeongKim <wnguddl96@knou.ac.kr>

--- src/main/java/onboarding/problem1/Calculator.java
+++ src/main/java/onboarding/problem1/Calculator.java
@@ -3,23 +3,23 @@ package onboarding.problem1;
 public class Calculator {
 
     public static int getEachNumber(int pageNumber) {
-        int eachNumberBySum = 0; // 중복 x 지점
-        int eachNumberByMul = 1; // 중복 x 지점
-        int result = 0;
+        int eachNumberBySum = 0;
+        int eachNumberByMul = 1;
 
-        // 리팩토링 : 공통 실행 코드 빼내기
+        /**
+         * 공통 실행 코드 추출
+         * else 키워드 제거
+         */
         while (pageNumber != 0 && pageNumber != 400) {
+
             eachNumberBySum += pageNumber % 10;
             eachNumberByMul *= pageNumber % 10;
 
             pageNumber /= 10;
         }
 
-        if (eachNumberBySum > eachNumberByMul) {
-            result = eachNumberBySum;
-        } else {
-            result = eachNumberByMul;
-        }
+        int result = eachNumberBySum > eachNumberByMul ? eachNumberBySum : eachNumberByMul ;
+
         return result;
     }
 }
