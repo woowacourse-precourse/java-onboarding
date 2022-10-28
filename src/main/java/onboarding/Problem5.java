e2ce25f0f8b0d11ea54afa086e0a05f84497c0cc
nakalic <nakalic28@gmail.com>
2022-10-29 01:12:41 +0900

    feat(Problem5): 화폐 개수를 구하는 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,8 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+	private static int withdrawMoney(int money, int won) {
+		return money / won;
+	}
 }
