31bde6bb384bc46c09750018e89b54af0afea6a5
Jeangyu <gjwlsrb1700@naver.com>
2022-10-28 18:51:07 +0900

    Add: Problem3 solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i=1; i<=number; i++)
+            if (Integer.toString(i).contains("3") || Integer.toString(i).contains("6") || Integer.toString(i).contains("9")) {
+                answer += count_three(i);
+        }
+
         return answer;
+    }
+
+    public static int count_three(int num){
+        int result = 0;
+
+        while (num > 0){
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9)
+                result+=1;
+            num /= 10;
+        }
+        return result;
     }
 }
