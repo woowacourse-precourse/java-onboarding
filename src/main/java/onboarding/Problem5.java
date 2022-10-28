652414478eb1d2e0f78827279e8274139fcf7095
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-29 01:04:17 +0900

    feat: Problem5 배열을 순회하면서 변환한 개수를 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,6 +13,11 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List <Integer> result = new ArrayList<>();
         List <Integer> amount = List.of(50000, 10000, 5000, 1000, 500, 100 ,50, 10, 1);
+
+        for(int i=0; i<amount.size(); i++){
+            result.add(money / amount.get(i));
+            money = money%amount.get(i);
+        }
         return result;
     }
 }
