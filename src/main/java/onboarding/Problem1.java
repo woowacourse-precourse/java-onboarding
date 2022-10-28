f31f1673dcf03ab9113b50d06fff8a2adf2ae7c7
Longstick0 <789456jang@naver.com>
2022-10-29 05:00:38 +0900

    feat: Problem1 계산 로직 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,11 +2,18 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Math.*;
+
 class Problem1 {
 
-    static private final int EXCEPTION_ANSWER = -1;
-    static private final int FIRST_PAGE = 1;
-    static private final int LAST_PAGE = 400;
+    private static final int INIT_ERROR = -1;
+    private static final int FIRST_PAGE = 1;
+    private static final int LAST_PAGE = 400;;
+
+    private static final int DRAW = 0;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
@@ -16,14 +23,46 @@ class Problem1 {
         int crongLeftPage = crong.get(0), crongRightPage = crong.get(1);
 
         if (checkException(pobiLeftPage, pobiRightPage) || checkException(crongLeftPage, crongRightPage)) {
-            return EXCEPTION_ANSWER;
+            return INIT_ERROR;
         }
         if (pobi.size() != 2 || crong.size() != 2) {
-            return EXCEPTION_ANSWER;
+            return INIT_ERROR;
         }
+
+        int pobiMaxScore = calcMaxScore(pobiLeftPage, pobiRightPage);
+        int crongMaxScore = calcMaxScore(crongLeftPage, crongRightPage);
+
+        answer = getResult(pobiMaxScore, crongMaxScore);
 
         System.out.println("answer = " + answer);
         return answer;
+    }
+
+    private static int getResult(int pobiMaxScore, int crongMaxScore) {
+        if (pobiMaxScore > crongMaxScore) {
+            return POBI_WIN;
+        }
+        if (pobiMaxScore < crongMaxScore) {
+            return CRONG_WIN;
+        }
+        return DRAW;
+    }
+
+    private static int calcMaxScore(int leftPage, int rightPage) {
+        return max(calcPage(leftPage), calcPage(rightPage));
+    }
+
+    private static int calcPage(int num) {
+        char[] numArr = String.valueOf(num).toCharArray();
+        int sum = 0;
+        int mul = 1;
+
+        for (int i = 0; i < numArr.length; i++) {
+            sum += numArr[i] - 48;
+            mul *= numArr[i] - 48;
+        }
+
+        return max(sum, mul);
     }
 
     private static boolean checkException(int leftPage, int rightPage) {
