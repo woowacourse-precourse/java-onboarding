a070145320651cf3b029836b4e6c3aafa827eddb
skfkgla <narahim.lee@gmail.com>
2022-10-28 18:15:41 +0900

    feat: Problem1 - 정수를 자릿수별로 나누는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
@@ -10,5 +11,13 @@ class Problem1 {
             return -1;
         }
         return answer;
+    }
+    private static List<Integer> splitInteger(int number) {
+        List<Integer> numberList = new ArrayList<Integer>();
+        while(number > 0){
+            numberList.add(number % 10);
+            number /= 10;
+        }
+        return numberList;
     }
 }
