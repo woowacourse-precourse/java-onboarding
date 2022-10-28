c9882f5f642d59955623871f6abaf3341d3f80d7
shpark30 <seohyeonpark30.gmail.com>
2022-10-28 23:35:29 +0900

    feat(Problem3.java): 주어진숫자까지손뼉치는횟수반환기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,16 +7,19 @@ import java.util.List;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += count369(answer);
+        }
         return answer;
     }
 
     static int count369(int number) {
-        // 주어진 정수 number에 3, 6, 9가 들어있는 개수를 반환
+        // 3, 6, 9와 매칭되는 자리수 개수를 반환
         List<Integer> condition = new ArrayList<>(Arrays.asList(3, 6, 9));
         int count = 0;
         int remainder = 0;
         while (number >= 10) {
-            // number를 10으로 나눈 나머지 remainder가 3, 6 9에 해당하면 count한다.
+            // number를 10으로 나눈 나머지 remainder가 3, 6, 9에 해당하면 count한다.
             remainder = number % 10;
             number /= number;
             if (condition.indexOf(remainder) != -1) {
