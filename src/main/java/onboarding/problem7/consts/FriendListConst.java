1b20c0da3dc0df6a72759c36f5053ac9c68f4e8b
apptie <appti@naver.com>
2022-10-28 15:45:56 +0900

    refactor: 문제 7 상수 클래스 추상 클래스로 변경

--- src/main/java/onboarding/problem7/consts/FriendListConst.java
+++ src/main/java/onboarding/problem7/consts/FriendListConst.java
@@ -1,6 +1,6 @@
 package onboarding.problem7.consts;
 
-public class FriendListConst {
+public abstract class FriendListConst {
 
     public static final int FRIEND_A_INDEX = 0;
     public static final int FRIEND_B_INDEX = 1;
--- src/main/java/onboarding/problem7/consts/FriendScoreConst.java
+++ src/main/java/onboarding/problem7/consts/FriendScoreConst.java
@@ -1,6 +1,6 @@
 package onboarding.problem7.consts;
 
-public class FriendScoreConst {
+public abstract class FriendScoreConst {
 
     public static final int FRIEND_RELATION_SCORE = 10;
     public static final int VISITOR_SCORE = 1;
