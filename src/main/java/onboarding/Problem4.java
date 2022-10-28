d41ae4015da0f4cab240b99612e5caf69a5e1ee7
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-28 16:25:41 +0900

    [Problem4] 기능 목록 설계

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem4 {
+
+    /**
+     * 기능 목록
+     * 1. 문자 대문자, 소문자 판단
+     * 2. 청개구리 사전
+     */
+
     public static String solution(String word) {
         String answer = "";
         return answer;
