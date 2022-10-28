148dd1e84053a510993d456d13eb028a87a2b5e5
Lee Min Woo <lmw7414@naver.com>
2022-10-28 23:29:08 +0900

    problem5 - 최종 테스트 마무리
    주어진 테스트 케이스에 대해 통과하였다.
    
    Collections.emptyList()의 사용 이유를 몰라 찾아보던 중 다음 사이트에서 정보를 얻었다.
    * https://dololak.tistory.com/48

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/** 구현해야할 기능
+/**
+ * 구현해야할 기능
  * [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
  * 5만원부터 시작하여 나눈 몫을 리스트에 차례대로 담는다.
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        int[] won = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < won.length; i++) {
+            int count = money / won[i];
+            answer.add(count);
+            money %= won[i];
+        }
+
         return answer;
     }
 }
