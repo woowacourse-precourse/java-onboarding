8496706d76f8c83766a7493474206af43478ac6e
nswon <swnam0729@gmail.com>
2022-10-29 03:47:58 +0900

    feat(Problem6): 시스템을 초기화시키는 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    private static List<String> confirm;
+    private static List<String> duplicatedNicknames;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        initSystem(forms);
         return answer;
+    }
+
+    private static void initSystem(List<List<String>> forms) {
+        confirm = new ArrayList<>();
+        confirm.add(forms.get(0).get(1));
+        duplicatedNicknames = new ArrayList<>();
     }
 }
