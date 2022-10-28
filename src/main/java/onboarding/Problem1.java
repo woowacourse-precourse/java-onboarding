32729e088589c63739f8228cb54f05b17c7fa1d6
Kimhyeyun <cqqwer@gmail.com>
2022-10-29 00:35:22 +0900

    [PROBLEM 1] MODIFY 기능 요구사항 6번에 대한 이해 변경됨 (페이지 범위가 1~400 → [1,2],[399,400] 페이지 나오지 않음)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -46,7 +46,7 @@ class Problem1 {
     private static boolean check(List<Integer> name) {
         if (name.get(1) - name.get(0) != 1) return false;
         if(name.get(0) % 2 != 1 || name.get(1) % 2 != 0) return false;
-        if(name.get(0) < 1 || name.get(1) > 400) return false;
+        if(name.get(0) < 2 || name.get(1) > 399) return false;
 
         return true;
     }
