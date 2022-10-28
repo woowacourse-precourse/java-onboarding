1e286d0f7ef1ee8c1eed8a309ff47d099c18ef87
kkm5291 <kkm23125291@gmail.com>
2022-10-28 23:47:43 +0900

    case2에서 ArrayIndexOutOfBoundsException 발생

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,28 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        char[] cryptoArr = cryptogram.toCharArray();
+
+        int prevIdx = 0;
+        int currentIdx = 0;
+
+        for (currentIdx = 1; currentIdx < cryptoArr.length; currentIdx++) {
+            if (cryptoArr[currentIdx - 1] != cryptoArr[currentIdx]) {
+                cryptoArr[prevIdx++] = cryptoArr[currentIdx - 1];
+            } else {
+                while (currentIdx < cryptoArr.length && cryptoArr[currentIdx - 1] == cryptoArr[currentIdx])
+                    currentIdx++;
+            }
+        }
+
+        cryptoArr[prevIdx++] = cryptoArr[currentIdx - 1]; // 마지막부분이 빠지기 때문에 넣어줌
+
+        answer = new String(cryptoArr).substring(0, prevIdx); // prevIdx를 사용해서 중복제거한 새 단어를 만듬
+
+        if(prevIdx != cryptoArr.length)
+            return solution(answer);
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int[] numArr = new int[number];
+
+
+
+
+
         return answer;
     }
 }
