e078e5db9b0855686c59c438b74d71da614e9020
keon0711 <fnc6488@gmail.com>
2022-10-28 17:32:22 +0900

    [Problem4] 소문자를 반대로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+    }
+
+    private static char convertAlpha(char alphabet) {
+        if (Character.isLowerCase(alphabet))
+            return convertLowerCase(alphabet);
+
+        if (Character.isUpperCase(alphabet))
+            return convertUpperCase(alphabet);
+
+        return alphabet;
+    }
+
+    private static char convertLowerCase(char lowerCase) {
+        int asciiOfa = 'a';
+        int asciiOfz = 'z';
+        int asciiOfLowerCase = lowerCase;
+
+        char covertedAlphabet = (char) (asciiOfz - (asciiOfLowerCase - asciiOfa));
+        return covertedAlphabet;
     }
 }
