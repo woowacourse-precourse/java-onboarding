d5d2a400fb91ff642000c8d89ba0f127c787237d
matt <matt1235@naver.com>
2022-10-29 00:46:50 +0900

    feat: Problem5 (기능 목록1 - 입력값의 범위 검사) 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        try{check(money);}
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return answer;
+        }
         return answer;
+    }
+
+    private static void check(int money) {
+        if(money < 1 || money > 1000000)
+            throw new IllegalArgumentException("입력값이 범위 밖입니다.");
     }
 }
