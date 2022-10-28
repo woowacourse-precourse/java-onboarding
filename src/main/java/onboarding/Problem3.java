c416982f192e742b2dc1b42ac682ef9afeab4e5e
scyllacore <yms8795@gmail.com>
2022-10-28 09:36:53 +0900

    docs(problem3) : 기능 정리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    /*
+    * 기능 정리
+    * 1. 1부터 number까지 숫자 나열
+    * 2. 나열된 각 숫자에 대해 3이 들어간 횟수를 카운트
+     */
+    
+
+
+
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
