d0ff3d6c80f8257e7b799b338667c24315998ae3
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:16:45 +0900

    feat(문제5): 주어진 수를 단위별로 분류하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +10,13 @@ import java.util.List;
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> monetary = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        for (int i = 0; i < monetary.size(); i++) {
+            answer.add(money / monetary.get(i));
+            money %= monetary.get(i);
+        }
         return answer;
     }
 }
