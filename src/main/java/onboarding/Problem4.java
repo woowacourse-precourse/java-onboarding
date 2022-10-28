b7b46cbf32afd0aa2d95eb6fa0e6459c26b45949
metorg <metrog950@gmail.com>
2022-10-28 14:22:03 +0900

    feat: [Problem4] 청개구리 대문자 변환 메서드 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,8 @@ public class Problem4 {
 
     public static final int lowerCase=1;
     public static final int upperCase=2;
+    public static final int ascUpperCase=155;
+    public static final int ascLowerCase=217;
     public static final int etc=3;
     public static StringBuilder sb;
 
@@ -14,26 +16,32 @@ public class Problem4 {
         return answer;
     }
 
-    static int letterCheck(char a){
-        if('a'<=a&&a<='z')return lowerCase;
-        else if('A'<=a&&a<='Z')return upperCase;
+    static int letterCheck(char alpa){
+        if('a'<=alpa&&alpa<='z')return lowerCase;
+        else if('A'<=alpa&&alpa<='Z')return upperCase;
         else return etc;
     }
 
     static String letterRound(String word){
         sb=new StringBuilder();
-        
+
         for(int i=0;i<word.length();i++){
             if(letterCheck(word.charAt(i))==lowerCase){
 
+
             }
             else if(letterCheck(word.charAt(i))==upperCase){
-
+                sb.append(upperCaseConv(word.charAt(i)));
+                System.out.println(upperCaseConv(word.charAt(i)));
             }
             else{
                 sb.append(word.charAt(i));
             }
         }
         return sb.toString();
+    }
+    static char upperCaseConv(char alpa){
+        System.out.println((int)(alpa));
+        return alpa=(char)(ascUpperCase-(int)(alpa));
     }
 }
