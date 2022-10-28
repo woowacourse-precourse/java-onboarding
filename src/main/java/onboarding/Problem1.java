b5247eb0c3a5cbb736351e98ce9a1e9b5b1f093a
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2022-10-28 16:24:45 +0900

    기능 완성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,22 +5,37 @@ import java.util.IntSummaryStatistics;
 import java.util.List;
 
 class Problem1 {
-
     static ArrayList<Integer> pars(Integer num){
-        //각 자릿수 숫자를 List에 담아 반환
-        return new ArrayList<>();
+        ArrayList<Integer> parsed_num=new ArrayList<Integer>();
+        while(num!=0){
+            parsed_num.add(num%10);
+            num/=10;
+        }
+        return parsed_num;
     }
     static int cal(ArrayList<Integer> parsed_num){
-        //나눠진 수를 2 or 3번 규칙에 맞게 계산
-        return 0;
+        int sum=0,mul=1;
+        for(Integer i:parsed_num){
+            sum+=i;
+            mul*=i;
+        }
+        return sum>mul?sum:mul;
     }
     static int larger(int num1, int num2){
-        //더 큰 수를 반환
-        return 0;
+        return num1>num2?num1:num2;
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if ((pobi.get(1)-pobi.get(0)!=1)||(crong.get(1)-crong.get(0)!=1))
+            return -1;
+        if(larger(cal(pars(pobi.get(0))),cal(pars(pobi.get(1))))>larger(cal(pars(crong.get(0))),cal(pars(crong.get(1)))))
+            answer=1;
+        else if (larger(cal(pars(pobi.get(0))),cal(pars(pobi.get(1))))<larger(cal(pars(crong.get(0))),cal(pars(crong.get(1)))))
+            answer=2;
+        else if (larger(cal(pars(pobi.get(0))),cal(pars(pobi.get(1))))==larger(cal(pars(crong.get(0))),cal(pars(crong.get(1)))))
+            answer=0;
+
         return answer;
     }
 }
