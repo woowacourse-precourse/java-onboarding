c098f849dbe22640a72c4650e84a31e51ae1d992
devjordy14 <devjordy14@gmail.com>
2022-10-28 15:32:48 +0900

    did todo 3

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 // todo
 // 1. 리스트에 넣은 수가 크기에 상관없이 순서대로 들어가는지 확인
-// 1. 받은 자연수를 각 단위로 나누어준다.
-// 2. 나눈 몫을 차례대로 리스트에 담은 후 반환한다.
+// 2. 받은 자연수를 각 단위로 나누어준다.
+// 3. 나눈 몫을 차례대로 리스트에 담은 후 반환한다.
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
@@ -19,11 +19,34 @@ public class Problem5 {
 
         answer = new ArrayList<>();
 
-        System.out.println("###############");
-        answer.add(4);
-        answer.add(8);
-        answer.add(2);
-        answer.add(7);
+        answer.add(money / 50000);
+        money = (money % 50000);
+
+        answer.add(money / 10000);
+        money = (money % 10000);
+
+        answer.add(money / 5000);
+        money = (money % 5000);
+
+        answer.add(money / 1000);
+        money = (money % 1000);
+
+        answer.add(money / 500);
+        money = (money % 500);
+
+        answer.add(money / 100);
+        money = (money % 100);
+
+        answer.add(money / 50);
+        money = (money % 50);
+
+        answer.add(money / 10);
+        money = (money % 10);
+
+        answer.add(money / 1);
+        money = (money % 1);
+
+        System.out.println(money);
         System.out.println(answer);
 
         return answer;
