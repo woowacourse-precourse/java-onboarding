c4df51d455a64c92bc5124a89789acfe1b27d96f
homecoder98 <homecoder98@gmail.com>
2022-10-29 01:36:04 +0900

    Feat : Make string to mirror string using ascii

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = wordFrog(word);
         return answer;
+    }
+    static String wordFrog(String s) {
+        String result = "";
+        for (int i=0;i<s.length();i++) {
+            int c = s.charAt(i);
+            if (c == ' ') {
+                result += ' ';
+                continue;
+            }
+            int d = (c<='Z')?('Z'-(c-'A')):('z'-(c-'a'));
+            result += (char)d;
+        }
+        return result;
     }
 }
