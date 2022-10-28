1a03d5c2b76e7e8adde2f57a3460e4c3bf89806e
Polite-Jehu <*****@***.com>
2022-10-28 20:08:42 +0900

    기능 2 완료 반환 : 해석된 스트링 =============== 기능흐름 인덱스 0부터 length-1까지 읽음 읽어가면서 다음것과 같다면 그 글자를 빼고 다시 처음부터 읽는다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuffer sb = new StringBuffer(cryptogram);
+        int i=0;
+        for(i=0;i<sb.length()-1;i++) {
+            if (sb.charAt(i) == sb.charAt(i + 1)) {
+                sb.delete(i, i + 2);
+                i = -1;
+            }
+        }
+        return sb.toString();
     }
 }
