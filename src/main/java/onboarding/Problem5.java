8b837aa4e17d0274863b868ee9596263d921a952
hectick <chyeon1223@cau.ac.kr>
2022-10-29 01:19:09 +0900

    feat : [Problem5] 특정 단위의 지폐 개수를 계산하는 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        List<Integer> unit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
         //제일 큰 금액의 화폐부터 필요한 개수를 차례대로 계산
         //큰 금액의 화폐를 계산하고 남은 돈으로 그 다음 큰 금액의 화폐가 필요한 개수를 계산하기를 반복
@@ -14,4 +16,13 @@ public class Problem5 {
 
         return answer;
     }
+
+    private static int countNumberOfBills(int unit, int money){
+        int count = 0;
+        if(money >= 50000){
+            count = money/50000;
+        }
+        return count;
+    }
+
 }
