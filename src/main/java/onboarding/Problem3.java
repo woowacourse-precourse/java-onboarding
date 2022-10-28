a309d858926712f374a1f092d5786850427a778b
dclxxi <dclxxi@kakao.com>
2022-10-28 20:01:36 +0900

    feat(문제3): 숫자에 포함된 3, 6, 9의 개수를 반환하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,21 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int count369(Integer num) {
+        String numString = num.toString();
+
+        int cnt = 0;
+        for (char numChar : numString.toCharArray()) {
+            if (include369(numChar)) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    private static boolean include369(char num) {
+        return num == '3' || num == '6' || num == '9';
+    }
 }
