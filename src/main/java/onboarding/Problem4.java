fbb4fac0cb1062eb4efbacf18e661557b2eda9f7
chosunghyun18 <krismon@naver.com>
2022-10-28 22:45:58 +0900

    [4번] 요구 사항 목록 2번 완성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,16 +1,16 @@
 package onboarding;
 
+
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+
         int[] arr = changeWord(word); // 아스키 코드로 변환
 
-
-        return answer;
+        return calculateAcii(arr);
     }
 
-    public static int[] changeWord(String word)
-    {
+    public static int[] changeWord(String word) {
         char[] arr = word.toCharArray();
         int [] check_list = new int[arr.length]; // 0으로 초기화된배열
         for(int i  = 0 ;i<arr.length;i++){
@@ -18,6 +18,25 @@ public class Problem4 {
         }
         return check_list ;
     }
+    public static String calculateAcii(int[] arr){
+        int n = arr.length;
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < n; i++)
+        {
+            // Uppercase check
+            if(64<arr[i] && arr[i] <91)
+            {
+                arr[i] = 155 - arr[i];
+                result.append(((char) arr[i]));
+            } else if (96<arr[i] && arr[i] <123) {
+                arr[i] = 219 - arr[i];
+                result.append(((char) arr[i]));
+            }else if (arr[i]==32){
+                result.append(' ');
+            }else result.append(((char) arr[i]));
+        }
 
+        return result.toString();
+    }
 
 }
