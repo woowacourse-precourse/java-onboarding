0461e560f5c5102df93806c74be868d1bf409b97
JoSihun <kepler452k@naver.com>
2022-10-28 17:51:48 +0900

    Problem 6, 7 Solved

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,27 +1,33 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> email = new ArrayList<String>();
-        List<String> nickName = new ArrayList<String>();
+        HashSet<String> emails = new HashSet<>();
+        HashMap<String, String> hashMap = new HashMap<>();
         for (int i = 0; i < forms.size(); i++) {
-            email.add(forms.get(i).get(0));
-            nickName.add(forms.get(i).get(1));
-        }
-        System.out.println(email);
-        System.out.println(nickName);
-        for (int i = 0; i < forms.size() - 1; i++) {
-            for (int j = i + 1; j < forms.size(); j++) {
-                if (nickName.get(i));
+            String nickName = forms.get(i).get(1);
+            if (nickName.length() < 2) {
+                break;
+            }
+
+            for (int j = 0; j < nickName.length() - 1; j++) {
+                String key = nickName.substring(j, j + 2);      // 2글자씩
+                if (hashMap.containsKey(key)) {                 // key가 이미 존재한다면
+                    String email = hashMap.get(key);
+                    if (!email.equals(forms.get(i).get(0))) {    // key와 연결된 email이 현재 email과 다르다면
+                        emails.add(email);
+                        emails.add(forms.get(i).get(0));
+                    }
+                }
+                hashMap.put(key, forms.get(i).get(0));          // 갱신
             }
         }
 
-
-        List<String> answer = List.of("answer");
-
+//        List<String> answer = List.of("answer");
+        List<String> answer = emails.stream().sorted().collect(Collectors.toList());
         return answer;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,106 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        /* HashMap Key-Value 쌍으로 친구관계 정리 */
+        Map<String, List<String>> hashMap = new HashMap<>();
+        for (int i = 0; i < friends.size(); i++) {
+            String friendA = friends.get(i).get(0);
+            String friendB = friends.get(i).get(1);
+
+            if (hashMap.containsKey(friendA)) {
+                List<String> listValue = hashMap.get(friendA);
+                listValue.add(friendB);
+                hashMap.put(friendA, listValue);
+            } else {
+                List<String> listValue = new ArrayList<String>();
+                listValue.add(friendB);
+                hashMap.put(friendA, listValue);
+            }
+
+            if (hashMap.containsKey(friendB)) {
+                List<String> listValue = hashMap.get(friendB);
+                listValue.add(friendA);
+                hashMap.put(friendB, listValue);
+            } else {
+                List<String> listValue = new ArrayList<String>();
+                listValue.add(friendA);
+                hashMap.put(friendB, listValue);
+            }
+        }
+        System.out.println(hashMap);
+
+        /* 함께 아는 친구 점수추가 */
+        Set<String> setFriends = new HashSet<>(hashMap.keySet());
+        Map<String, Integer> hashMapScores = new HashMap<>();
+
+        setFriends.remove(user);
+        for (String friendA: setFriends) {
+            for (String friendB: hashMap.get(friendA)) {
+                if (hashMap.get(user).contains(friendB)) {
+                    if (hashMapScores.containsKey(friendA)) {
+                        Integer score = hashMapScores.get(friendA) + 10;
+                        hashMapScores.put(friendA, score);
+                    } else {
+                        hashMapScores.put(friendA, 10);
+                    }
+                }
+            }
+        }
+        System.out.println(setFriends);
+
+        /* 타임라인 방문기록 점수추가 */
+        for (int i = 0; i < visitors.size(); i++) {
+            String key = visitors.get(i);
+            if (hashMapScores.containsKey(key)) {
+                Integer score = hashMapScores.get(key) + 1;
+                hashMapScores.put(key, score);
+            } else {
+                hashMapScores.put(key, 1);
+            }
+        }
+        System.out.println(hashMapScores);
+
+        /* User의 기존 친구에 해당하는 친구 삭제 */
+        for (String friend: hashMap.get(user)) {
+            hashMapScores.remove(friend);
+        }
+        System.out.println(hashMapScores);
+
+        /* 점수별 친구목록으로 변환 */
+        Map<Integer, List<String>> reverseKeyValueHashMapScores = new HashMap<>();
+        for (String key: hashMapScores.keySet()) {
+            Integer value = hashMapScores.get(key);
+            if (reverseKeyValueHashMapScores.containsKey(value)) {
+                List<String> temp = reverseKeyValueHashMapScores.get(value);
+                temp.add(key);
+                temp = temp.stream().sorted().collect(Collectors.toList());         // 이름 오름차순으로 정렬
+                reverseKeyValueHashMapScores.put(value, temp);
+            } else {
+                List<String> temp = new ArrayList<>();
+                temp.add(key);
+                temp = temp.stream().sorted().collect(Collectors.toList());         // 이름 오름차순으로 정렬
+                reverseKeyValueHashMapScores.put(value, temp);
+            }
+        }
+        System.out.println(reverseKeyValueHashMapScores);
+
+        /* 점수 내림차순으로 정렬 */
+//        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<Map.Entry<Integer, List<String>>> entries = reverseKeyValueHashMapScores.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+                .collect(Collectors.toList());
+        for (Map.Entry<Integer, List<String>> entry: entries) {
+            for (String friend: entry.getValue()) {
+                answer.add(friend);
+            }
+
+        }
+
         return answer;
     }
 }
