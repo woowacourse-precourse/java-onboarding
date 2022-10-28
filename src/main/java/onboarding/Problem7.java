2bb0fe807a93383b28472ce67837480c6857d0ed
eunbii0213 <eunbii0213@naver.com>
2022-10-28 18:30:09 +0900

    feat: (Problem7) 추천 점수가 가장 높은 순으로 정렬 한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,14 +4,26 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> friendsWithUser = new ArrayList<>();
-    public static Map<String, Integer> sameFriendWithUser = new HashMap<>();
+    public static Map<String, Integer> sameFriendWithUser = new HashMap<String, Integer>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         addFriendsWithUser(user,friends);
         findSameFriendWithUser(user,friends);
         findVisitor(visitors);
+        makeOrder();
         return answer;
+    }
+
+    private static void makeOrder() {
+        List<Map.Entry<String, Integer>> list_entries = new ArrayList<Map.Entry<String, Integer>>(sameFriendWithUser.entrySet());
+
+        Collections.sort(list_entries, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> obj1, Map.Entry<String, Integer> obj2) {
+
+                return obj2.getValue().compareTo(obj1.getValue());
+            }
+        });
     }
 
     private static void findVisitor(List<String> visitors) {
