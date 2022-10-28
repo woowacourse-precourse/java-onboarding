9f893ae42a7c90ec04db574867683fb7b5839255
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-28 16:28:39 +0900

    feat: Problem7 - 방문자들의 추천 점수를 반영하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,9 +4,23 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Problem7 {
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+            List.of("donut", "andole"),
+            List.of("donut", "jun"),
+            List.of("donut", "mrko"),
+            List.of("shakevan", "andole"),
+            List.of("shakevan", "jun"),
+            List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        solution(user, friends, visitors);
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Set<String> userFriends = new HashSet<>();
@@ -18,6 +32,14 @@ public class Problem7 {
             isAcquaintance(recommendFriends, friend, userFriends);
         }
         recommendFriends.remove(user);
+        for (String v : visitors) {
+            recommendVisitor(recommendFriends, v, userFriends);
+        }
+
+        for (Map.Entry<String, Integer> stringIntegerEntry : recommendFriends.entrySet()) {
+            System.out.println(stringIntegerEntry.getKey());
+            System.out.println(stringIntegerEntry.getValue());
+        }
 
         return answer;
     }
@@ -37,6 +59,11 @@ public class Problem7 {
         }
         if (friendSet.contains(friendship.get(1))) {
             friendMap.put(friendship.get(0), friendMap.getOrDefault(friendship.get(0), 0) + 10);
+        }
+    }
+    public static void recommendVisitor(HashMap<String, Integer> friendMap, String v, Set<String> friendSet) {
+        if (!friendSet.contains(v)){
+            friendMap.put(v, friendMap.getOrDefault(v, 0) + 1);
         }
     }
 }
