effaeae0b7ce17772f339a86b844796e5df5870a
JUN <chltmdwns96@naver.com>
2022-10-29 00:33:39 +0900

    feature : 입력받은 배열(pobi,crong) 예외 값 처리 - 배열의 0번째 값에 +1을 한 값이 배열의 1번째 값인지 검증 - 배열의 0번째 값이 홀수, 1번째 값이 짝수인지 검증 - leftPage값의 범위가 3~397 , rightPage값의 범위가 4~398인지 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,33 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(checkPageException(pobi) || checkPageException(crong)){
+            return -1;
+        }
         return answer;
+    }
+
+    private static Boolean checkPageException(List<Integer> pages){
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        System.out.println("leftPage = " + leftPage);
+        System.out.println("rightPage = " + rightPage);
+        final int FIRST_PAGE = 3;
+        final int LAST_PAGE = 398;
+
+        // 페이지가 순차적인 수인지 체크
+        if(leftPage + 1 != rightPage) {
+            return true;
+        }
+        // 페이지 홀수/짝수 체크
+        if(!(leftPage % 2 == 1 && rightPage % 2 == 0)){
+            return true;
+        }
+        // 페이지 3~398 범위 체크
+        if(!(leftPage >= FIRST_PAGE && leftPage < LAST_PAGE
+                && rightPage > FIRST_PAGE && rightPage <= LAST_PAGE)){
+            return true;
+        }
+        return false;
     }
 }
