e189bc098e5fa6b3936e403baa3af3ea91263d6e
Kim woo jong <skekdnwhd@gmail.com>
2022-10-28 19:07:39 +0900

    problem1 리팩토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,12 +6,9 @@ import java.util.stream.Stream;
 import static java.lang.Math.max;
 
 class Problem1 {
-
     private static final Integer EXCEPTION = -1;
-    private static final Integer DRAW = 0;
-    private static final Integer POBI_WIN = 1;
-    private static final Integer CRONG_WIN = 2;
     private static final Integer PAGE_ERROR = -1;
+    private static final Integer DRAW = 0, POBI_WIN = 1, CRONG_WIN = 2;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (pobi.size() != 2 || crong.size() != 2)
@@ -19,7 +16,6 @@ class Problem1 {
 
         int pobiNum = maxNum(pobi);
         int crongNum = maxNum(crong);
-
         if (pobiNum == PAGE_ERROR || crongNum == PAGE_ERROR)
             return EXCEPTION;
 
@@ -35,10 +31,9 @@ class Problem1 {
         int leftPage = pageList.get(0);
         int rightPage = pageList.get(1);
 
-        if (leftPage % 2 == 0 || rightPage % 2 == 1 || rightPage - leftPage != 1 || leftPage < 3 || rightPage < 4 || leftPage > 397 || rightPage > 398)
-            return PAGE_ERROR;
-
-        return max(pageToCalMaxNum(leftPage), pageToCalMaxNum(rightPage));
+        return leftPage % 2 == 0 || rightPage % 2 == 1 || rightPage - leftPage != 1 || leftPage < 3 || rightPage < 4 || leftPage > 397 || rightPage > 398
+            ? PAGE_ERROR
+            : max(pageToCalMaxNum(leftPage), pageToCalMaxNum(rightPage));
     }
 
     private static int pageToCalMaxNum (int page) {
