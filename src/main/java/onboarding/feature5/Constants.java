566b21ae6ace0fb62190556d063b745798fe501d
areyouhun <areyouhun@gmail.com>
2022-10-29 04:00:32 +0900

    refactor(prob4): change constant name from bill to currency unit

---
+++ src/main/java/onboarding/feature5/Constants.java
@@ -0,0 +1,8 @@
+package onboarding.feature5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constants {
+    public static final List<Integer> CURRENCY_UNIT = new ArrayList<>(List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1));
+}
