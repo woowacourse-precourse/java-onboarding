a8dd5d34f1f5b06c456c9dc2b573dd515324f1c3
jinny-l <jinny.l.dev@gmail.com>
2022-10-28 16:41:51 +0900

    [feat] (problem.2) 연속하는 중복 문자가 있는지 확인하는 메소드 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,14 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    // 연속하는 중복 문자가 있는지 확인하는 메소드
+    private static int isRepetitive(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
