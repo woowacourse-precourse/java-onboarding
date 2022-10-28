9350592f215d6404585bfaf4176646166d3f7096
YooByeongIk <quddlr96@naver.com>
2022-10-28 14:34:06 +0900

    [Problem6] docs : 기능 목록 주석 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
+/*
+##기능 목록
+
+1. 각 닉네임으로 만들 수 있는 길이 2이상의 연속된 문자열을 Key로 HashMap에 저장 (Value는 List<String>)
+2. HashMap의 Value(List<String>)의 크기가 2 이상인 경우, Value에 포함한 모든 Email을 answer에 저장
+3. answer의 모든 중복 제거 및 정렬
+
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
     }
 }
