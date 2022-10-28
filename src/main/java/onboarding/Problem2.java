b030ff987b59551a3108a7b61c36abecdadb0b9b
Kimhyeyun <cqqwer@gmail.com>
2022-10-29 00:58:21 +0900

    [PROBLEM 2] MODIFY while문내의 기능들 각기 메서드로 추출

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,37 +4,37 @@ public class Problem2 {
     public static String solution(String cryptogram) {
 
         while (true) {
-            boolean isDuplicated = false;
-            for (int i = 0; i < cryptogram.length(); i++) {
-                int duplicateCount = checkDuplicate(cryptogram, i);
-                if (duplicateCount == -1 || duplicateCount == 0) {
-                    continue;
-                } else {
-                    cryptogram = deleteDuplicate(cryptogram, i, duplicateCount);
-                    isDuplicated = true;
-                    break;
-                }
-            }
-            if(!isDuplicated) break;
-        }
+            int[] duplicateIndex = checkDuplicate(cryptogram);
+            if (duplicateIndex[0] == -1) break;
+            cryptogram = deleteDuplicate(cryptogram, duplicateIndex);
 
+        }
         return cryptogram;
     }
 
-    private static String deleteDuplicate(String cryptogram, int idx, int count) {
-        String c = cryptogram.substring(0, idx);
-        c += cryptogram.substring(idx + count + 1, cryptogram.length());
+    private static String deleteDuplicate(String cryptogram, int[] idxAndCount) {
+        String c = cryptogram.substring(0, idxAndCount[0]);
+        c += cryptogram.substring(idxAndCount[0] + idxAndCount[1] + 1, cryptogram.length());
 
         return c;
     }
 
-    private static int checkDuplicate(String cryptogram, int idx) {
+    private static int[] checkDuplicate(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            int duplicateCount = countDuplicate(cryptogram, i);
+            if (duplicateCount == -1 || duplicateCount == 0) continue;
+            return new int[]{i, duplicateCount};
+        }
+        return new int[]{-1,-1};
+    }
+
+    private static int countDuplicate(String cryptogram, int idx) {
         char c = cryptogram.charAt(idx);
         int cnt = 0;
         for (int i = idx + 1; i < cryptogram.length(); i++) {
-            if(c == cryptogram.charAt(i)) cnt += 1;
+            if (c == cryptogram.charAt(i)) cnt += 1;
             else {
-                if(i == idx + 1) cnt = -1;
+                if (i == idx + 1) cnt = -1;
                 break;
             }
         }
