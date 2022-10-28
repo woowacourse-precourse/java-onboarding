3913b0afc3cf0d941b030a0652e6a1b2bfa0fce6
jihong <clara5091@naver.com>
2022-10-28 21:40:08 +0900

    problem5-제한사항 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static void checkMoney(int money){
+        if(money>1000000||money<1)
+            throw new IllegalArgumentException("올바른 범위가 아닌 입력값입니다.");
+    }
+
     public static List<Integer> solution(int money) {
+        checkMoney(money);
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
