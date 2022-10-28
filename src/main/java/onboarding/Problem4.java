4a0f539e3cf4bdd41a307bf5852869505b6ba16e
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 16:42:28 +0900

    feat: [4번] 문자열을 반대로 변환하는 기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+
+    /** 문자를 반대로 변환하는 함수 */
+    private static char replaceChar(char ch) {
+        return Character.MAX_VALUE;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
