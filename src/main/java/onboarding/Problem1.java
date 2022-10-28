c03147bb274dd683021b7c3b3abc55f07774ee76
June <68291395+june-777@users.noreply.github.com>
2022-10-28 12:44:58 +0900

    feat: 페이지 null point Exception 확인 메소드 구현
    List<Integer> 파라미터와 시작면, 마지막면 예외처리 하기 위함

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,24 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        
         return answer;
+    }
+    public static boolean isException(List<Integer> pobi, List<Integer> crong) {
+    	
+    	if(isNullPage(pobi, crong)) {
+    		return true;
+    	}
+    	
+    	return false;
+    	
+    }
+    public static boolean isNullPage(List<Integer> pobi, List<Integer> crong) {
+    	
+    	if((pobi.get(0)==null) || (pobi.get(1)==null) || (crong.get(0)==null) || (crong.get(1)==null)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 }
