180c54c6af456a174f73096afe6f567396b104ed
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 16:26:39 +0900

    feat: [3번] 369 게임 기능 목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem3 {
+    /**
+     * 숫자의 각 자릿수를 나누는 함수
+     * ex) 123 -> [3, 2, 1]
+     */
+    private static List<Integer> splitDigit(int num) {
+        return Collections.emptyList();
+    }
+
+    /**
+     * 박수를 쳐야하는 횟수를 구하는 함수
+     */
+    private static int getClapCount(List<Integer> digitList) {
+        return Integer.MAX_VALUE;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
