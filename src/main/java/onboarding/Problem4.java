95dce956aa892e194c6b89c6dc4a940b5432319b
youhobin <ghdb132@naver.com>
2022-10-28 11:31:56 +0900

    feat(onboarding) : 알파벳인 문자만 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,14 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        String result = "";
         for (int i = 0; i < word.length(); i++) {
-            changeWord(word.charAt(i));
+             result += checkWord(word.charAt(i));
         }
-        String answer = "";
-        return answer;
+
+        return result;
     }
 
-    private static void changeWord(char charAt) {
-        
+    private static char checkWord(char word) {
+        if (word >= 'A' && word <= 'Z') {
+            changeWord(word);
+        }
+
+        if (word >= 'a' && word <= 'z') {
+            changeWord(word);
+        }
+
+        return word;
     }
 }
