4e1357d09285dda518517addc66124e9f8f4ba47
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-10-29 01:24:56 +0900

    Problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeDuplication(String crtpto) {
+        String result = "";
+        int index = 0;
+        int count = 0;
+        for (int i = 0; i < crtpto.length() - 1; i++) {
+            if (crtpto.charAt(i) == crtpto.charAt(i + 1)) {
+                result += crtpto.substring(index, i);
+                if (crtpto.length() > i + 2)
+                    result += crtpto.substring(i + 2);
+                count++;
+                break;
+            }
+        }
+        if (count == 0)
+            return crtpto;
+        return removeDuplication(result);
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = removeDuplication(cryptogram);
         return answer;
     }
 }
