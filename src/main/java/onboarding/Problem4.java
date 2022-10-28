48109f21258f5710ddff6e4176dd22ba142d68e7
ParkRootSeok <g_e_un@naver.com>
2022-10-28 15:04:08 +0900

    fix : Problem4 - 알파벳 대소 변환 기능에서 알파벳 순서 뒤집기 기능으로 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,26 +15,28 @@ public class Problem4 {
 
         char out = ' ';
 
+        // a 기준 알파벳의 순번을 이용하여 z부터 거꾸로 내려가는 새로운 순번으로 변환
+        // a 는 0번째 이므로 z로 부터 0번째인 z 반환
+        // 즉, 기존 순번이 n번 이라면 반환되는 순번은 z로 부터 n번째 전 순환이 반환
         if ('a' <= in && in <= 'z') {
-           out = (char)(in - ('a' - 'A'));
+            out = (char) ('z' - (in % 'a'));
         }
 
         if ('A' <= in && in <= 'Z') {
-            out = (char)(in + ('a' - 'A'));
+            out = (char) ('Z' - (in % 'A'));
         }
 
         return out;
     }
 
-    static public char reverseWord(String word) {
+    static public String reverseWord(String word) {
 
-        char result = ' ';
-        return result;
     }
 
 
     public static String solution(String word) {
         String answer = "";
+        answer = reverseWord(word);
         return answer;
     }
 
