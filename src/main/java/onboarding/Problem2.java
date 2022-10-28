3801758dd941452180118231a5a5b23e952085dd
wwwndus <simock22@gmail.com>
2022-10-28 13:17:59 +0900

    refactor: 메소드를 추출

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -16,28 +16,25 @@ public class Problem2 {
             if (strList.size() == 0) {
                 break;
             }
-            for (int i = 0; i < strList.size()-1; i++) {
-                if (strList.get(i).equals(strList.get(i+1))) {
-                    strList.remove(i);
-                    strList.remove(i);
-                    boo = true;
-                    break;
-                } else {
-                    boo = false;
-                }
-            }
+            boo = isBoo(strList, boo);
         }
 
         answer = String.join("", strList);
 
         return answer;
     }
+
+    private static boolean isBoo(List<String> strList, boolean boo) {
+        for (int i = 0; i < strList.size()-1; i++) {
+            if (strList.get(i).equals(strList.get(i+1))) {
+                strList.remove(i);
+                strList.remove(i);
+                boo = true;
+                break;
+            } else {
+                boo = false;
+            }
+        }
+        return boo;
+    }
 }
-
-// "browoanoommnaon"	"brown"
-// "zyelleyz"	        ""
-
-// 입력 문자열을 분리하여 배열로 저장한 후 리스트로 변환
-// while문을 진행하여 연속적으로 중복되는 문자열은 삭제
-//  -> 모두 중복되어 문자열이 없거나 더이상 중복되는 문자열이 없을 경우 반복문 빠져나옴
-// 리스트의 남은 문자열을 합쳐서 출력
