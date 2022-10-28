178a21cb56e5c67ba853e5c494f5f4e3ea167587
nakalic <nakalic28@gmail.com>
2022-10-28 09:53:00 +0900

    feat(Problem2): 중복 문자 유무 확인 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+
+		while (checkDuplication(cryptogram)) {
+
+		}
+
+		return answer;
+
+	}
+
+	private static boolean checkDuplication(String str) {
+
+		for (int i = 0; i < str.length() - 1; i++)
+			//중복이 있을 경우
+			if (str.charAt(i) == str.charAt(i + 1)) {
+				return true;
+			}
+
+		// 중복이 없을 경우
+		return false;
+	}
+
 }
