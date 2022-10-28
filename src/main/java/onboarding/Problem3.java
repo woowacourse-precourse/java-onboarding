a7fc5439c76dcf0efeca4a96442f4360ded6d8ba
footdev <ekclstkfka5@gmail.com>
2022-10-28 20:09:02 +0900

    feat: 기능3 메소드 명세 작성 및 solution 메소드 작성
    기능 구현 목록을 토대로 메소드 명세 작성 및 solution 메소드 내부 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+    private static final int[] TARGET_NUMBERS = {3, 6, 9};
+
     public static int solution(int number) {
-        int answer = 0;
+        int answer = getAnswer(number);
         return answer;
+    }
+
+    public static int getAnswer(int number) {
+
+        return 0;
+    }
+
+    public static int countTargetNum(int number) {
+
+        return 0;
     }
 }
