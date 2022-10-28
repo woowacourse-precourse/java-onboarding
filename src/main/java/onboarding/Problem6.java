9113cb53c348517a183eff60de05aa656bbdf4d3
sangil <gsl0505@naver.com>
2022-10-28 17:34:53 +0900

    refactor(PROBLEM6): 기능별 메소드 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,30 +7,44 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Problem6 {
-	public static List<String> solution(List<List<String>> forms) {
-		Map<String, Integer> duplicatedNameCount = new HashMap<>();
-		for (List<String> form : forms) {
-			for (int nameIndex = 0; nameIndex < form.get(1).length() - 1; nameIndex++) {
-				String duplicatedName = form.get(1).substring(nameIndex, nameIndex + 2);
-				if (!duplicatedNameCount.containsKey(duplicatedName)) {
-					duplicatedNameCount.put(duplicatedName, 1);
-				} else {
-					duplicatedNameCount.put(duplicatedName, duplicatedNameCount.get(duplicatedName) + 1);
-				}
-			}
-		}
+	private static final int EMAIL_INDEX = 0;
+	private static final int NAME_INDEX = 1;
 
-		List<String> limitList = new ArrayList<>();
+	public static List<String> solution(List<List<String>> forms) {
+		Map<String, Integer> duplicatedNameCount = checkDuplicatedNameCount(forms);
+		List<String> limitList = makeDuplicateEmailList(forms, duplicatedNameCount);
+		return makeResultForm(limitList);
+	}
+
+	private static Map<String, Integer> checkDuplicatedNameCount(List<List<String>> forms) {
+		Map<String, Integer> duplicatedName = new HashMap<>();
 		for (List<String> form : forms) {
-			for (int nameIndex = 0; nameIndex < form.get(1).length() - 1; nameIndex++) {
-				String duplicatedName = form.get(1).substring(nameIndex, nameIndex + 2);
-				if (duplicatedNameCount.get(duplicatedName) > 1) {
-					limitList.add(form.get(0));
-					continue;
+			for (int i = 0; i < form.get(NAME_INDEX).length() - 1; i++) {
+				String twoLengthName = form.get(NAME_INDEX).substring(i, i + 2);
+				duplicatedName.put(twoLengthName,
+					duplicatedName.getOrDefault(twoLengthName, 0) + 1);
+			}
+		}
+		return duplicatedName;
+	}
+
+	private static List<String> makeDuplicateEmailList(List<List<String>> forms,
+		Map<String, Integer> duplicatedNameList) {
+		List<String> resultList = new ArrayList<>();
+		for (List<String> form : forms) {
+			for (int i = 0; i < form.get(NAME_INDEX).length() - 1; i++) {
+				String twoLengthName = form.get(NAME_INDEX).substring(i, i + 2);
+				if (duplicatedNameList.get(twoLengthName) > 1) {
+					resultList.add(form.get(EMAIL_INDEX));
+					break;
 				}
 			}
 		}
-		return limitList.stream()
+		return resultList;
+	}
+
+	private static List<String> makeResultForm(List<String> resultList) {
+		return resultList.stream()
 			.distinct()
 			.sorted()
 			.collect(Collectors.toList());
