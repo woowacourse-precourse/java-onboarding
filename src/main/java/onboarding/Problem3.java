eec657d3a1307f9653dfe25bbb796aad71c490a3
xxeol2 <jhj01177@naver.com>
2022-10-28 13:53:12 +0900

    feat(prob3): 3,6,9 개수 카운팅 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = 0;
+		return answer;
+	}
+
+	public static int countSpecial(int number) {
+		int count = 0;
+		while (number > 0) {
+			if ((number % 10) != 0 && (number % 10 % 3) == 0) {
+				count += 1;
+			}
+			number /= 10;
+		}
+		return count;
+	}
 }
