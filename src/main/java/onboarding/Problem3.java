ae519d06aca7605906cbf16b74f859202e804125
leesky <leehaneul0623@gmail.com>
2022-10-28 12:09:17 +0900

    feat: 박수 카운트 계산 기능 구현(문제 3)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
-
 public class Problem3 {
+
     public static int solution(int number) {
+        if (number < 3) return 0;
         int answer = 0;
+        for (int i = 3; i <= number; i++) {
+            answer += getClapCount(i);
+        }
         return answer;
+    }
+
+    private static int getClapCount(int target) {
+        String strNum = Integer.toString(target);
+        int originCount = strNum.length();
+        return originCount - strNum.replaceAll("3|6|9", "").length();
     }
 }
