19010b02a7f288e4043fb84efe2a513c06e2c0a1
juhwang <juhong4930@naver.com>
2022-10-28 22:09:23 +0900

    feat(Problem7):giving recommendation score to future friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,27 +1,39 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
-    private static List<String> filterFriendsOfUser(String user, List<List<String>> friends){
+    private static List<String> findUserRelation(String user, List<List<String>> friends){
         List<String> friendsOfUser = new ArrayList<>();
         friends.forEach(relationTwoUsers -> {
             if(relationTwoUsers.contains(user)){
-                friendsOfUser.add(relationTwoUsers.get(friendsIndex(user,relationTwoUsers)));
+                friendsOfUser.add(relationTwoUsers.get(!(relationTwoUsers.indexOf(user)!=0)?1:0));
             }
         });
         return friendsOfUser;
     }
-    private static int friendsIndex(String user, List<String> relationTwoUsers) {
-        if(user.equals(relationTwoUsers.get(0))){
-            return 1;
-        }
-        return 0;
+    private static void findFriendsOfUser(String user, List<List<String>> friends){
+        findUserRelation(user,friends).forEach(friendsOfUser -> {
+            HashMap<String,Integer> recommendationFriends = findFriendsOfUserLoop(friendsOfUser,friends);
+        });
     }
+    private static HashMap<String,Integer> findFriendsOfUserLoop(String friendsOfUser, List<List<String>> friends) {
+        Map<String,Integer> recommendationFriends = new HashMap<>();
+        for (String newFriendsOfUser: findUserRelation(friendsOfUser, friends)) {
+            giveRecommendationScore(newFriendsOfUser,recommendationFriends);
+        }
+        return (HashMap<String, Integer>) recommendationFriends;
+    }
+    private static void giveRecommendationScore(String newFriendsOfUser, Map<String, Integer> recommendationFriends) {
+            if(recommendationFriends.get(newFriendsOfUser) != null){
+                recommendationFriends.put(newFriendsOfUser,recommendationFriends.get(newFriendsOfUser)+10);
+                return;
+            }
+            recommendationFriends.put(newFriendsOfUser, 10);
+    }
+
 }
