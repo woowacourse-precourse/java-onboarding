853f3b75658384b0217e375594dd3ccd5e1ba941
JunHyeon <dlwnsgus07@naver.com>
2022-10-28 23:17:26 +0900

    feat : add validation check function
    Problem1의 입력조건에 맞는지 검증하는 함수를 추가하였습니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,41 @@
+/*
+ * Problem1.java
+ *
+ * 2022-10-27
+ *
+ */
 package onboarding;
 
 import java.util.List;
 
+/**
+ * 우아한 테크코스 - 프리코스 1주차 페이지 번호 게임
+ *
+ * @version         1.00 2022년 10월 27일
+ * @author          이준현
+ */
 class Problem1 {
+
+    public static boolean validCheck(List<Integer> list){
+        if(list.size() != 2){
+            return false;
+        }
+        for(int el : list){
+            if(el <= 1 || 400 <= el){
+                return false;
+            }
+        }
+        if(list.get(0) % 2 != 1 && list.get(1) % 2 != 0){
+            return false;
+        }
+
+        if(list.get(1) - list.get(0) != 1){
+            return false;
+        }
+
+        return true;
+    };
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
