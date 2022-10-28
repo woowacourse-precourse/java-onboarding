e399c9532253b8efcb8221b62e0a94c2ccc475d4
iamminho <minho100227@kakao.com>
2022-10-28 12:36:57 +0900

    Problem6 feat(isException): 동일한 인덱스, 한글자 예외처리 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,20 +1,18 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Set;
-import java.util.TreeMap;
 import java.util.stream.Stream;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        PriorityQueue<String> overlapNames = getOverlapNames(forms);
-        return toList(overlapNames);
+        PriorityQueue<String> overlapNamesQueue = getOverlapNamesQueue(forms);
+
+        return priorityQueueToList(overlapNamesQueue);
     }
 
-    static List<String> toList(PriorityQueue<String> queue) {
+    static List<String> priorityQueueToList(PriorityQueue<String> queue) {
         List<String> result = new ArrayList<>();
 
         Stream.generate(queue::poll)
@@ -24,45 +22,74 @@ public class Problem6 {
         return result;
     }
 
-    static PriorityQueue<String> getOverlapNames(List<List<String>> forms) {
+    static PriorityQueue<String> getOverlapNamesQueue(List<List<String>> forms) {
         int size = forms.size();
-        PriorityQueue<String> emailQueue = new PriorityQueue<>();
+        PriorityQueue<String> overlapNamesQueue = new PriorityQueue<>();
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (i != j && !validateOverlapName(emailQueue, forms.get(i), forms.get(j))) {
+                if (i != j && !validateOverlapName(overlapNamesQueue, forms.get(i), forms.get(j))) {
                     break;
                 }
             }
         }
 
-        return emailQueue;
+        return overlapNamesQueue;
     }
 
-    static boolean validateOverlapName(PriorityQueue<String> emailQueue, List<String> form1, List<String> form2) {
-        String name1 = form1.get(1);
-        String name2 = form2.get(1);
+    static void setOverlapNamesQueue(List<List<String>> forms, PriorityQueue<String> overlapNamesQueue, int i, int j) {
+        if (isException(forms, i, j)) {
+            return;
+        }
 
-        if(name1.length() == 1 || name2.length() == 1) {
+    }
+
+    static boolean isException(List<List<String>> forms, int i, int j) {
+        if (!isSameIdx(i, j) || isOverOneLetter(forms, i, j))
+            return false;
+        return true;
+    }
+
+
+    static boolean isSameIdx(int i, int j) {
+        if (i == j) {
             return true;
         }
 
-        if (!isOverlapName(name1, name2)) {
-            emailQueue.add(form1.get(0));
+        return false;
+    }
+
+    static boolean isOverOneLetter(List<List<String>> forms, int i, int j) {
+        String currentName = forms.get(i).get(1);
+        String targetName = forms.get(i).get(1);
+
+        if(currentName.length() <= 1 || targetName.length() <= 1) {
             return false;
         }
 
         return true;
     }
 
-    static boolean isOverlapName(String name1, String name2) {
-        for (int i = 0; i < name1.length() -1; i++) {
-            String target = name1.substring(i, i+2);
-            if (name2.contains(target)) {
+    static boolean validateOverlapName(PriorityQueue<String> overlapNamesQueue, List<String> form1, List<String> form2) {
+        String name1 = form1.get(1);
+        String name2 = form2.get(1);
+
+        if (!isOverlapName(name1, name2)) {
+            overlapNamesQueue.add(form1.get(0));
+            return false;
+        }
+
+        return true;
+    }
+
+    static boolean isOverlapName(String currentName, String targetName) {
+        for (int i = 0; i < currentName.length() -1; i++) {
+            String target = currentName.substring(i, i+2);
+
+            if (targetName.contains(target)) {
                 return false;
             }
         }
-
         return true;
     }
 
