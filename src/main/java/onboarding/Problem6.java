e053b9e0849245069990428ca9ea571dce6a2a18
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 17:08:57 +0900

    feat: [6번] 닉네임 중복 검사 기능 목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+    /**
+     * 이름을 2글자씩 분해해 wordMap을 업데이트하는 함수 (3글자 이상 중복 되는 경우 2글자도 중복되기 때문에 2글자로만 중복 판단을 한다.)
+     * 이때 분해된 단어를 Key로 가지는 List에 email을 추가
+     * <pre>
+     * 이름 분해 예시 : "제이엠" -> "제이", "이엠"
+     * </pre>
+     */
+    private static void updateWordMap(List<String> userInfo, Map<String, List<String>> wordMap) {
+
+    }
+
+    /**
+     * wordEmailList에 있는 email을 duplicateEmailList에 추가하는 함수
+     * 이때 email이 이미 있으면 추가하지 않는다.
+     */
+    private static void updateDuplicateEmailList(List<String> duplicateEmailList, List<String> wordEmailList) {
+
+    }
+
+    /**
+     * WordMap에서 중복된 단어를 가진 유저가 있는 경우 emailList에 추가해 반환하는 함수
+     */
+    private static List<String> checkDuplicateUser(Map<String, List<String>> wordMap) {
+        return Collections.emptyList();
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
