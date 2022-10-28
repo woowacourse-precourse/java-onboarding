a32cc88d5885c0633b7bf49add0bf399283cd5d8
antaeho <an981022@naver.com>
2022-10-28 11:22:57 +0900

    Problem5 기능 구현사항 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,7 +2,13 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+/*
+- 기능 구현사항 목록 -
+1. 화폐 단위가 들어있는 array 생성(내림차순)
+2. 크기와 같은 빈 array_answer 생성
+3. array 를 돌면서 n을 나눈 몫을 array_answer 에 저장
+4. n %= array[i] 갱신
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
