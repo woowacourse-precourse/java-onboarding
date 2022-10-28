0574f27f464dd9c5b55e1c1dab5a24f3afb8591a
BuildTools <unconfigured@null.spigotmc.org>
2022-10-29 02:56:01 +0900

    refactor : Problem6 변수 네이밍 재설정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,27 +4,27 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Map<String, String> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
+        Map<String, String> userMap = new HashMap<>();
+        Set<String> wrongNameSet = new HashSet<>();
         List<String> answer;
         String name;
         String email;
-        String tmp;
+        String partedWord;
 
         for(List<String> list : forms){
             email = list.get(0);
             name = list.get(1);
             for(int i=0; i<name.length()-1; i++){
-                tmp = name.substring(i, i+2);
-                if(map.containsKey(tmp)){
-                    set.add(email);
-                    set.add(map.get(tmp));
+                partedWord = name.substring(i, i+2);
+                if(userMap.containsKey(partedWord)){
+                    wrongNameSet.add(email);
+                    wrongNameSet.add(userMap.get(partedWord));
                 }else
-                    map.put(tmp, email);
+                    map.put(partedWord, email);
             }
         }
 
-        answer = new ArrayList<>(set);
+        answer = new ArrayList<>(wrongNameSet);
         Collections.sort(answer);
 
         return answer;
