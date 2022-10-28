498bd63f7790aad82303bfda19b45f2bf6ed26a8
davejang <davejang98@naver.com>
2022-10-28 22:45:58 +0900

    feat: Problem2 implement
    온보딩 미션 문제2 기능 요구 사항 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Problem2 {
+    public static String removeDuplicate(String str) {
+        int count = 0;
+        String answer = str;
+
+        while(true) {
+            String temp = "";
+            HashMap<Integer,Integer> duplicateIndexList = checkNearDuplicate(answer);
+            char[] chars = answer.toCharArray();
+
+            if(duplicateIndexList.size() == 0) break;
+
+            for(int i = 0; i < chars.length; i++) {
+                if(duplicateIndexList.containsKey(i) == false){
+                    temp += Character.toString(chars[i]);
+                }
+            }
+
+            answer = temp;
+            count ++;
+        }
+        return answer;
+    }
+    public static HashMap<Integer,Integer> checkNearDuplicate(String str) {
+        HashMap<Integer,Integer> duplicateIndexList = new HashMap<>();
+        char[] chars = str.toCharArray();
+        for(int index = 1; index < chars.length; index++) {
+            if(chars[index] == chars[index-1]){
+                duplicateIndexList.put(index-1,1);
+                duplicateIndexList.put(index,1);
+            }
+        }
+        return duplicateIndexList;
+    }
     public static String solution(String cryptogram) {
-        String answer = cryptogram;
+        String answer = removeDuplicate(cryptogram);
         return answer;
     }
 }
