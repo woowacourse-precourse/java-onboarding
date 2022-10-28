b68ccbd884f3d754a75ff309a9df769f706ec18b
0112leesy <0112leesy@naver.com>
2022-10-28 23:59:56 +0900

    [Problem5] feat: solution

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class Problem5 {
 
     static int[] cash = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        List<Integer> money_to_cash = new ArrayList<>();
+
+        for(int i=0; i<cash.length; i++){
+            money_to_cash.add(money / cash[i]);
+            money %= cash[i];
+        }
+
+        return money_to_cash;
     }
 }
