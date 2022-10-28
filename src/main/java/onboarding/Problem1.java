fcc7334138e0107bfe413146fca256f0068a698d
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2022-10-28 21:15:39 +0900

    /**  * 2. 페이지 번호 각 자리 덧셈 함수 구현  * **/ public -> private : 다른 클래스의 함수이름 중복 방지를 위해 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,7 +7,8 @@ import java.util.List;
  * 1. 입력값 체크
  * 2. 페이지 번호 각 자리 덧셈 함수 구현
  * 3. 페이지 번호 각 자리 곳셈 함수 구현
- * 4. 결과 비교 함수
+ * 4. 가장 큰 수 출력을 위한 비교 함수
+ * 5. 결과 값 출력을 위한 함수
  * **/
 /**
  * 1. 입력값 체크
@@ -16,14 +17,16 @@ import java.util.List;
  * - [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어왔는지
  * - 입력 숫자 : 시작면이나 마지막 또는 0보다 더 작은 값 400보다 더 큰값이 나올 때 결과값
  * */
-
+/**
+ * 2. 페이지 번호 각 자리 덧셈 함수 구현
+ * **/
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
 
-//    public static void main(String[] args){
+    public static void main(String[] args){
 //        List<Integer> test1 = new ArrayList<Integer>();
 //        test1.add(3);
 //        test1.add(4);
@@ -32,8 +35,10 @@ class Problem1 {
 //        test2.add(12);
 //        System.out.println(checkInput(test1));
 //        System.out.println(checkInput(test2));
-//    }
-    public static boolean checkInput(List<Integer> inputlist){
+//        int testNum = 020;
+//        System.out.println(sumOfeach(testNum));
+        }
+    private static boolean checkInput(List<Integer> inputlist){
         if(checkSize(inputlist))
         {
             if(checkOdd(inputlist) && checkNext(inputlist) && checkVal(inputlist))
@@ -41,24 +46,34 @@ class Problem1 {
         }
         return false;
     }
-    public static boolean checkSize(List<Integer> inputlist){
+    private static boolean checkSize(List<Integer> inputlist){
         if(inputlist.size() == 2)
             return true;
         return false;
     }
-    public static boolean checkNext(List<Integer> inputlist){
+    private static boolean checkNext(List<Integer> inputlist){
         if(inputlist.get(0) == inputlist.get(1) - 1)
             return true;
         return false;
     }
-    public static boolean checkOdd(List<Integer> inputlist){
+    private static boolean checkOdd(List<Integer> inputlist){
         if(inputlist.get(0) % 2 == 1)
             return true;
         return false;
     }
-    public static boolean checkVal(List<Integer> inputlist){
+    private static boolean checkVal(List<Integer> inputlist){
         if(inputlist.get(0) > 0 && inputlist.get(1) < 400)
             return true;
         return false;
     }
+    private static int sumOfeach(int num){
+        int ret = 0;
+        while(num != 0){
+            ret += num % 10;
+            num = num / 10;
+            System.out.println(ret);
+        }
+        return ret;
+    }
+
 }
