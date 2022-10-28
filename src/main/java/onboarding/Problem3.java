2f2320bf2a3c4a53da04ba137c4749a66d173fd3
Identityless <rlandwnd555@naver.com>
2022-10-28 17:10:31 +0900

    FEAT : 각 숫자 토큰이 3의 배수인지 확인하는 기능의 checkNumber() 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,14 +7,24 @@ import java.util.stream.Stream;
 *   구현 기능
 *   1. 현재 숫자를 자릿수 별로 나누는 기능
 *   2. 나눠진 숫자에서 박수를 몇번 쳐야하는지 카운트 하는 기능
+*   3. 각 토큰이 3의 배수인지 확인하는 기능
 */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
-}
 
+    public static int checkNumber(int num) {
+        int result = 0;
+
+        if(num != 0 && num % 3 == 0) {
+            result = 1;
+        }
+
+        return result;
+    }
+}
 class StringNumSpliter {
     public int[] splitNum(int num) {
         return Stream.of(makeStringArr(num)).mapToInt(Integer::parseInt).toArray();
