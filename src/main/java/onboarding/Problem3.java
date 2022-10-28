64e3d3fd94e643a373032d298ccb8b42f241d8a7
newh08 <newh08@gmail.com>
2022-10-29 03:24:01 +0900

    Problem3 checkToClap 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+    static int answer;
+
     public static int solution(int number) {
-        int answer = 0;
+        answer = 0;
         return answer;
+    }
+    public static boolean checkToClap(char singlenumber){
+        return singlenumber=='3' || singlenumber=='6' || singlenumber =='9';
     }
 }
