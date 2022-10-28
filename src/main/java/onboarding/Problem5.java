49c4947eb1debef92f62e550b8c4c53365de3acc
SEONMl <sm000117@naver.com>
2022-10-28 19:36:48 +0900

    feat: Problem5 잔돈을 계산하는 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,6 +12,18 @@ public class Problem5 {
         private int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         private int target;
 
+        public Wallet(int target) {
+            this.target = target;
+        }
+
+        private void calculate() {
+            int tmp = target;
+            for(int i=0 ; i<MONEY_MAX_COUNT ; i++){
+                money.add(tmp / currency[i]);
+                tmp%=currency[i];
+            }
+        }
+
     }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
