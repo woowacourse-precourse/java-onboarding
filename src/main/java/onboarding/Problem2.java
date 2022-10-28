4fc45c4e08d43960971c400aec775bbbb49398e9
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-29 02:30:29 +0900

    [Problem2] feat: solution 메서드 구현
    - 알고리즘 flow에 따라 그에 알맞은 함수목록과 함께 solution 메서드를 구현함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,13 +2,13 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-
+        String answer = "";
         // crytogram와 길이, 값이 일치한 StringBuffer 생성
         StringBuffer sb = getStringBuffer(cryptogram);
-
-        //
-
+        // sb의 중복을 제거하여 반환
+        sb = decodeCryptogram(sb);
+        // 중복 제거된 cryptogramStringBuffer을 String으로 변환
+        answer = sb.toString();
         return answer;
     }
 
@@ -17,5 +17,10 @@ public class Problem2 {
         StringBuffer sb = new StringBuffer(cryptogram.length());
         sb.append(cryptogram);
         return sb;
+    }
+
+    // StringBuffer의 중복을 제거하여 암호해독한 StringBuffer를 반환
+    private static StringBuffer decodeCryptogram(StringBuffer cryptogarmStringBuffer) {
+
     }
 }
