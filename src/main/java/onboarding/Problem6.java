51303b9aa5318e23f510032c596b51d23a67a981
swandevson <shai4956@gmail.com>
2022-10-28 22:15:26 +0900

    feat: P6 substring 반환
    해당 닉네임의 idx로 시작하여 연속하는 substring 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    public static String sliceNickname(String nickname, int idx) {
+        return nickname.substring(idx, idx+2);
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
