34868fb1104b920e679ac1dcf63e76b5d15106fb
YuHayeon <youha0930@gmail.com>
2022-10-29 01:59:06 +0900

    feat(Problem5): 입력의 범위가 올바르지 않는 경우 예외처리
    1미만 1000000초과인 경우 예외처리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = Collections.emptyList();
+		return answer;
+	}
+
+	static class InputValidator {
+		private static boolean isRightMoneyRange(int money) {
+			if (money < 1 || money > 1000000) {
+				return false;
+			}
+			return true;
+		}
+	}
 }
