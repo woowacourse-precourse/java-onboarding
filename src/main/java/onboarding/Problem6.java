74fe4ca2cdb163b26c5a9a53c39778a939b6938e
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:17:37 +0900

    docs(문제6): 기능목록 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,14 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 📚 기능 목록
+ *
+ * 1. 사용자의 이름을 두글자 씩 분리하는 기능
+ * 2. 사용자의 이름 중복을 확인하는 기능
+ * 3. 중복된 이름을 가진 사용자를 저장하는 기능
+ * 4. 결과 목록을 정렬하는 기능
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
