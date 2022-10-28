c8bed4f794e478c5842bd69aaf44d8d8076cb47d
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-28 23:26:26 +0900

    feat(problem6): check nickname length

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+
+		for (List<String> form : forms) {
+			String nickname = form.get(1);
+			if (nickname.length() < 2) {
+				continue;
+			}
+		}
+
+		List<String> answer = List.of("answer");
+		return answer;
+	}
 }
