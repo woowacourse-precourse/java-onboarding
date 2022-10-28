abc19262ad6f089ac4d47e46631a9af6a8344dc2
es <mascoeskim@gmail.com>
2022-10-28 20:23:38 +0900

    Problem6/5.getEmail구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,7 +7,7 @@ public class Problem6 {
         forms = new ArrayList<>(forms);
         ArrayList<String> separateName = new ArrayList<String>();
 
-        for ( int s=0;forms.size()>s;s++){
+        for (int s=0;forms.size()>s;s++){
             for (int k=0;forms.get(s).size()-1>k;k++){
               separateName.add(String.valueOf(forms.get(s).get(1).charAt(k))+(forms.get(s).get(1).charAt(k+1)));
             }
@@ -17,7 +17,7 @@ public class Problem6 {
 
         ArrayList<String> checkDuplicateName = new ArrayList<String>();
 
-        for  ( int s=0;separateName.size()>s;s++){
+        for  (int s=0;separateName.size()>s;s++){
             for (int k=0;separateName.size()>k;k++){
                 if (s != k && Objects.equals(separateName.get(s), separateName.get(k))){
                     checkDuplicateName.add(separateName.get(s));
@@ -27,9 +27,20 @@ public class Problem6 {
         System.out.println("중복체크"+checkDuplicateName);
 //seprateName에서 중복값이 존재하는 경우에만 checkDuplicateName에 담음. 블록커 : ==연산이 적용되지않고 Objects.equals로 변경하여 비교해야함
         HashSet<String> hashSet = new HashSet<>(checkDuplicateName);
-        System.out.println(hashSet);
-//HashSet을 이용한 중복제거
+        ArrayList<String> duplicateName = new ArrayList<String>(hashSet);
+//HashSet을 이용한 중복제거 후 다시 duplicateName 동적배열로 선언
 
+        ArrayList<String> getEmail = new ArrayList<String>();
+        for  (int s=0;forms.size()>s;s++){
+            for (int k=0;duplicateName.size()>k;k++){
+                if (forms.get(s).get(1).contains(duplicateName.get(k))){
+                    getEmail.add(forms.get(s).get(0));
+                }
+            }
+        }
+        System.out.println(getEmail);
+// duplicateName이 포함되어있는 유저의 이메일을 getEmail에 add
+//forms.get(s).get(1).indexOf(duplicateName.get(k) --> forms.get(s).get(1).contains(duplicateName.get(k)) 리팩토링
 
         List<String> answer = List.of("answer");
         return answer;
