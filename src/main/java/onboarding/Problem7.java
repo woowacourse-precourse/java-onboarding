aa8cd9180eb24f222812925686462c877864aab6
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-28 13:27:31 +0900

    feat : user와 친구 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        
+        // user와 친구         
+        HashSet<String> userFriends = new HashSet<>();     
+        for (int i = 0; i < friends.size(); i++) {
+            Iterator it = friends.get(i).iterator();
+            while(it.hasNext()) {
+                Object tmp = it.next();
+                if(tmp.equals(user)) {
+                    userFriends.addAll(friends.get(i));      
+                    userFriends.remove(user);
+                }
+            }
+        }
+        
         return answer;
     }
 }
