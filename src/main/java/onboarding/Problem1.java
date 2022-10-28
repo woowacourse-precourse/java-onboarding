9477f1834b898ce6e8da9fc3e0f358f5307ddf61
dmdrk1414 <seungchan141414@gmail.com>
2022-10-28 16:39:18 +0900

    [Refactor]:recycle: problem_1 문제 접근 방식 수정
    1. 문자열로 수정을 안하고 바로 Integer 로 접근함
    2. Integer을 문자열로 변환하여 구현을 하였다.
    3. 지금은 문자열 변환을 하지 않고 Integer로 계산을 함
       getAnserValueTwoList을 호출하여 answer을 할당되도록 만듬 나의 방식은 메서드를 만들어서 main에서 콜을 하는 식의
       코드를 많이 만든다. 잘못 한 느낌이 들면 추후 수정함.
    4. 처음에 접근을 잘못 하였다. 단순하게 문자열로 parse하여 푸는 것을 많이하여, 바로 Integer을 접근한는 것을 잊어 버림

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,36 +3,67 @@ package onboarding;
 import java.util.List;
 
 public class Problem1 {
+    // [121, 122] , [211, 212]
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (isException(pobi) || isException(crong)) {
+        int answer = Integer.MAX_VALUE;
+        answer = getAnwserValueTwoList(pobi, crong);
+        return answer;
+    }
+
+    private static int getAnwserValueTwoList(List<Integer> ListOne, List<Integer> ListTwo) {
+        int left = 0;
+        int right = 1;
+        if (isException(ListOne) || isException(ListTwo)) {
             return -1;
         }
+        int ListOneValueMax = getMaxTwoIntValue(ListOne.get(left), ListOne.get(right));
+        int ListTwoValueMax = getMaxTwoIntValue(ListTwo.get(left), ListTwo.get(right));
 
-        // pobi = [97, 98] crong = [197, 198]
-        String[] pobiStrArr = new String[2];
-        String[] crongStrArr = new String[2];
+        if (ListOneValueMax == ListTwoValueMax) {
+            return 0;
+        } else if (ListOneValueMax > 0) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
 
-        for (int index = 0; index < 2; index++) {
-            pobiStrArr[index] = String.valueOf(pobi.get(index));
-            crongStrArr[index] = String.valueOf(crong.get(index));
+    // 2개의 값중 큰값을 리턴한다.
+    private static int getMaxTwoIntValue(Integer valueOne, Integer valueTwo) {
+        int getLeftMaxValue = getMaxSeatValueNum(valueOne);
+        int getRightMaxValue = getMaxSeatValueNum(valueTwo);
+        return Math.max(getLeftMaxValue, getRightMaxValue);
+    }
+
+    // num의 자릿수를 가져와서 더하고, 곱한값의 최댓값을 가져온다.
+    private static int getMaxSeatIntValue(Integer num) {
+        int hundSeat = num / 100;
+        int tenSeat = num / 10 % 10;
+        int oneSeat = num % 10;
+
+        int sum = hundSeat + tenSeat + oneSeat;
+        int multiple = 0;
+
+        if (hundSeat != 0) {
+            multiple = hundSeat * tenSeat * oneSeat;
+        } else {
+            multiple = tenSeat * oneSeat;
         }
 
-        int valuePobi = getMaxValue(pobiStrArr);
-        int valueCrong = getMaxValue(crongStrArr);
-
-        int answer = getAnswer(valuePobi, valueCrong);
-        return answer;
+        if (sum > multiple) {
+            return sum;
+        } else {
+            return multiple;
+        }
     }
 
     private static boolean isException(List<Integer> list) {
         // 범위를 넘었는가 검사
-        if (list.get(0) < 1 || list.get(0) > 400
-                || list.get(1) < 1 || list.get(1) > 400) {
+        if (list.get(0) < 1 || list.get(0) > 400 || list.get(1) < 1 || list.get(1) > 400) {
             return true;
         }
         // left가 홀수인가, right가 짝수인가
-        else if (list.get(0) % 2 != 1
-                || list.get(1) % 2 != 0) {
+        else if (list.get(0) % 2 != 1 || list.get(1) % 2 != 0) {
             return true;
         }
         // left와 right가 연달아 있는가
@@ -41,52 +72,5 @@ public class Problem1 {
         } else {
             return false;
         }
-    }
-
-    private static int getAnswer(int valuePobi, int valueCrong) {
-        if (valuePobi == valueCrong) {
-            return 0;
-        } else if (valuePobi > valueCrong) {
-            return 1;
-        } else if (valuePobi < valueCrong) {
-            return 2;
-        } else {
-            return -1;
-        }
-    }
-
-    private static int getMaxValue(String[] strArr) {
-        int left = 0, right = 1;
-        int[] valueArr = new int[4];
-        valueArr[0] = getSumParseString(strArr[left]);
-        valueArr[1] = getSumParseString(strArr[right]);
-        valueArr[2] = getMultiplParseString(strArr[left]);
-        valueArr[3] = getMultiplParseString(strArr[right]);
-        int addMax = Math.max(valueArr[0], valueArr[1]);
-        int MultiplMax = Math.max(valueArr[2], valueArr[3]);
-
-        return Math.max(addMax, MultiplMax);
-    }
-
-    public static int getSumParseString(String str) {
-        String[] strArr = getSplitArr(str);
-        int sum = 0;
-        for (String value : strArr) {
-            sum = sum + Integer.parseInt(value);
-        }
-        return sum;
-    }
-
-    public static int getMultiplParseString(String str) {
-        String[] strArr = getSplitArr(str);
-        int sum = 1;
-        for (String value : strArr) {
-            sum = sum * Integer.parseInt(value);
-        }
-        return sum;
-    }
-
-    public static String[] getSplitArr(String strArr) {
-        return (strArr.split(""));
     }
 }
