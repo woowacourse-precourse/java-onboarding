1c4122377091a2886a75fd68bf051d33431a9b2d
jihong <clara5091@naver.com>
2022-10-28 21:21:43 +0900

    problem4-알파벳 변환기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,10 +1,19 @@
 package onboarding;
 
 public class Problem4 {
-    public static void checkWord(String word){
-        if(word.length()>1000||word.length()<1)
+    private static void checkWord(String word) {
+        if (word.length() > 1000 || word.length() < 1)
             throw new IllegalArgumentException("올바른 범위의 입력값이 아닙니다.");
     }
+
+    private static char convert(char c){
+        if(c>=65&&c<=90)
+            return (char)(90-c+65);
+        if(c>=97&&c<=122)
+            return (char)(122-c+97);
+        return c;
+    }
+
     public static String solution(String word) {
         checkWord(word);
         String answer = "";
