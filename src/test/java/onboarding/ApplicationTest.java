21fa06e54a5190275661943e47bb0151d1afae86
June <68291395+june-777@users.noreply.github.com>
2022-10-28 12:57:16 +0900

    fix: Text file encoding 변경 후 복구
    최초 테스트 케이스 추가 및 저장 시, 인코딩 오류 발생
    MS949(기존) 에서 utf-8로 변경
    변경 후, 기존 테스트 케이스의 한글 깨짐 오류 발생
    utf-8에서 다시 MS949 변경 후, 깨진 한글 재입력

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -140,11 +140,11 @@ class ApplicationTest {
         @Test
         void case1() {
             List<List<String>> forms = List.of(
-                    List.of("jm@email.com", "�젣�씠�뿞"),
-                    List.of("jason@email.com", "�젣�씠�뒯"),
-                    List.of("woniee@email.com", "�썙�땲"),
-                    List.of("mj@email.com", "�뿞�젣�씠"),
-                    List.of("nowm@email.com", "�씠�젣�뿞")
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@email.com", "제이슨"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("nowm@email.com", "이제엠")
             );
             List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
             assertThat(Problem6.solution(forms)).isEqualTo(result);
