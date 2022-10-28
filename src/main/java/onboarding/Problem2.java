739f38699b93fb0cd07778db40b39d0b247fc03a
Kim woo jong <skekdnwhd@gmail.com>
2022-10-28 18:50:05 +0900

    problem2 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,17 +1,17 @@
 package onboarding;
 
 public class Problem2 {
-    private static final String EXCEPTION = "EXCEPTION";
+    private static final String RESTRICTIONS = "word length error or character error";
     public static String solution(String cryptogram) {
-        if (cryptogram.length() < 1 || cryptogram.length() > 1000 || !cryptogram.matches("^[a-z]*$"))
-            return EXCEPTION;
-        return decode(cryptogram);
+        return cryptogram.length() < 1 || cryptogram.length() > 1000 || !cryptogram.matches("^[a-z]*$")
+            ? RESTRICTIONS
+            : decode(cryptogram);
     }
 
     private static String decode(String cryptogram) {
         String answer = cryptogram.replaceAll("([a-z])\\1","");
-        if (answer.matches("^[a-z]*([a-z])\\1[a-z]*$"))
-            return decode(answer);
-        return answer;
+        return answer.matches("^[a-z]*([a-z])\\1[a-z]*$")
+                ? decode(answer)
+                : answer;
     }
 }
