001c887a369bca40ec20b05b1f85bdd08da1806f
leesky <leehaneul0623@gmail.com>
2022-10-28 15:33:34 +0900

    docs: 기초 명세 작성(문제 6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+
 public class Problem6 {
+    /**
+     * <div> 신청받은 닉네임 중 <b>같은 글자가 연속적으로 포함 되는 닉네임</b>을 작성한 지원자의 이메일 목록을
+     * 리턴합니다. </div>
+     *
+     * <br>
+     * 세부 기능 목록 :
+     *
+     *  <ol>
+     *      <li>중복되는 단어 자료구조(해시)에 저장</li>
+     *      <li>저장된 자료구조에 중복 유저를 찾는 메서드</li>
+     *      <li>form의 유효성 검사</li>
+     *  </ol>
+     *
+     *
+     * @param forms 크루의 정보 [이메일, 닉네임]의 리스트
+     * @return 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 {@code List}
+     *
+     */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
