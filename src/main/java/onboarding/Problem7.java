36e70be4a9f8d4e418d412db53ca5c310128af19
Eundms <dms011221@gmail.com>
2022-10-28 20:12:20 +0900

    add(network)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,7 +5,18 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         //TODO: network 만들기
+        Map<String,List<String>> network = new HashMap<>();//{'이름':new ArrayList<>{}}
+        for(List<String> friendship:friends){
+            String oneName=friendship.get(0);
+            String twoName=friendship.get(1);
+            List<String>oneFriend=network.getOrDefault(oneName,new ArrayList<>());
+            oneFriend.add(twoName);
+            List<String>twoFriend=network.getOrDefault(twoName,new ArrayList<>());
+            twoFriend.add(oneName);
 
+            network.put(oneName,oneFriend);
+            network.put(twoName,twoFriend);
+        }
         //TODO: 'user 친구' 리스트
         //TODO: user와 친구가 아닌 'user 친구의 친구' 리스트 만들기
 
