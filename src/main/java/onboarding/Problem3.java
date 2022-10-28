36701494987afe517c10af3ed7c17cb15257ee59
win9 <krrr8@naver.com>
2022-10-28 22:34:01 +0900

    [기능3번] 박수 횟수를 계산하는 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -19,5 +19,22 @@ public class Problem3 {
         return number >= 1 && number <= 10000;
     }
 
+    /**
+     * 주어진 매개변수가 박수를 쳐야하는 횟수를 계산하는 메소드
+     * @param num
+     * @return
+     */
+    public static int check(int num){
+        int sum = 0;
+        while (num != 0){
+            int rest = num % 10;
+            num /= 10;
+            if (rest == 3 || rest == 6 || rest == 9){
+                sum++;
+            }
+        }
+
+        return sum;
+    }
 
 }
