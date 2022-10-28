71f77e3a4da874c66bc2c0187db547f037d9e56e
math0917 <math0917@naver.com>
2022-10-29 01:55:52 +0900

    feat : input validation 확인하는 코드 작성

--- src/main/java/onboarding/problem3/Problem3Validation.java
+++ src/main/java/onboarding/problem3/Problem3Validation.java
@@ -2,6 +2,10 @@ package onboarding.problem3;
 
 public class Problem3Validation {
     public static boolean problem3InputValidation(int userInput, int minInput, int maxInput) {
-        return true;
+        if (minInput <= userInput && userInput <= maxInput) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
