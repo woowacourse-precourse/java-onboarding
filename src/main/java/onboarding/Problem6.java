db3f53b3a78e001d7c89d31dadaaddc3dcec48e7
whibaek <jibaek0513@gmail.com>
2022-10-29 00:36:39 +0900

    P6 solution clear

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        HashSet<String> mailSet = new HashSet<>();
+
+        for(List<String> val : forms) {
+
+            String email = val.get(0);
+            String name = val.get(1);
+
+            for (int i = 0; i < name.length() - 1; i++) {
+                for (int j = i + 2; j <= i+2; j++) {
+                    String part = name.substring(i, j);  // 2 글자로 검사
+
+                    for(List<String> userList : forms){
+                        if(val.equals(userList)) continue;
+                        if(userList.get(1).contains(part)){ // 중복되는게 있다면
+                            mailSet.add(userList.get(0));
+                            mailSet.add(email);
+                        }
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(mailSet);
     }
+
+
 }
