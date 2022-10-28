bed44ab3c18aacf6c2ba58644eeec1ecc44a04d3
sangwon <swchoi1997@naver.com>
2022-10-29 01:49:29 +0900

    fix : solution 메서드에 있던 로직을 리팩터링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,18 +8,26 @@ public class Problem5 {
     private static final int[] MONEY_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         checkValidRange(money);
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < MONEY_LIST.length; i++) {
-            int count = 0;
-            if (money >= MONEY_LIST[i]){
-                count = money / MONEY_LIST[i];
-            }
-            money = money - MONEY_LIST[i] * count;
-            answer.add(count);
-        }
+        List<Integer> answer = getResult(money);
 
         return answer;
 
+    }
+
+    private static List<Integer> getResult(int money) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < MONEY_LIST.length; i++) {
+            money = getRemainMoney(money, answer, i);
+        }
+        return answer;
+    }
+
+    private static int getRemainMoney(int money, List<Integer> answer, int i) {
+        int count = 0;
+        if (money >= MONEY_LIST[i]) count = money / MONEY_LIST[i];
+        money = money - MONEY_LIST[i] * count;
+        answer.add(count);
+        return money;
     }
 
     private static void checkValidRange(final int money) {
