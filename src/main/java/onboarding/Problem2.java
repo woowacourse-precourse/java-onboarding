682cc19c77821cd3e9b8dbd5737b83f06bdf9071
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 14:18:44 +0900

    feat: Add deleteRepeat in Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+
+    private static String deleteRepeat(String cryptogram){
+        char previousChar = cryptogram.charAt(0);
+
+        for(int i = 1; i < cryptogram.length(); i++){
+            if(previousChar == cryptogram.charAt(i)){
+                cryptogram = cryptogram.substring(0, i - 1).concat(cryptogram.substring(i + 1));
+                i--;
+            }
+            else{
+                previousChar = cryptogram.charAt(i);
+            }
+        }
+
+        return cryptogram;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
