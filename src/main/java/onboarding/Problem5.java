afc85e2aed19a93216c336a409e4431b105cc8c6
swandevson <shai4956@gmail.com>
2022-10-28 21:52:51 +0900

    feat: P5 화폐 변환(solve)
    1. 남은 money에 대한 해당 화폐의 최대 개수를 구하고 갱신한다.
    2. 변환 후 money를 차감한다.
    3. money가 0이 될 때 까지 또는 마지막 화폐단위까지 반복한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,17 +7,26 @@ import java.util.Arrays;
 public class Problem5 {
     public static List<Integer> calcMoneyUnit(int money) {
         // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
-        List<Integer> moneyUnit = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
-        List<Integer> moneyUnitNum = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        List<Integer> moneyUnitList = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
+        List<Integer> moneyUnitNumList = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-        return moneyUnitNum;
+        for(int i = 0; i < moneyUnitList.size() && 0 < money; i++) {
+            int moneyUnit = moneyUnitList.get(i);
+            int moneyUnitNum = money / moneyUnit;
+
+            moneyUnitNumList.set(i, moneyUnitNum);
+
+            money -= moneyUnitNum * moneyUnit;
+        }
+
+        return moneyUnitNumList;
     }
 
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
 
-        System.out.println(answer);
+        answer = calcMoneyUnit(money);
 
         return answer;
     }
