b785c894f0748b9f9b61c12ee8ce8032511e9346
phobyjun <phobyjun@gmail.com>
2022-10-29 00:21:41 +0900

    [PROBLEM3] ADD: FEATURE2. solution 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int n = 1; n <= number; n++) {
+            answer += getClapCount(n);
+        }
+
         return answer;
     }
 
