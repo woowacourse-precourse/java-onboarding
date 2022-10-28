f6a84d69691a9ffee998a0fac2e32739ed8ba032
phobyjun <phobyjun@gmail.com>
2022-10-29 00:19:56 +0900

    [PROBLEM3] ADD: FEATURE1. 숫자 속 3, 6, 9의 개수를 반환하는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int getClapCount(int number) {
+        String numberToString = String.valueOf(number);
+        int result = 0;
+        for (int idx = 0; idx < numberToString.length(); idx++) {
+            char charAtString = numberToString.charAt(idx);
+            if (charAtString == '3' || charAtString == '6' || charAtString == '9')
+                result++;
+        }
+        return result;
+    }
 }
