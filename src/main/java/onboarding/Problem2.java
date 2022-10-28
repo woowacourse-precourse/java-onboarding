5db504c825ba283ba3c53dd9d6c3c5ef514e5866
Your Name <dbsdbxkrzz@naver.com>
2022-10-28 13:13:57 +0900

    feat: 코드의 기본 틀을 작성합니다.
    기능 목록에 맞추어 필요한 메서드의 기본 틀을 작성합니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		while (isDeletable(cryptogram)) {
+			cryptogram = deleteConsecutive(cryptogram);
+		}
+		return cryptogram;
+	}
+
+	public static boolean isDeletable(String cryptogram) {
+		return false;
+	}
+
+	public static String deleteConsecutive(String cryptogram) {
+		return cryptogram;
+	}
+
 }
