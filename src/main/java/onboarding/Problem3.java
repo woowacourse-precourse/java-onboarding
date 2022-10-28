483ebb329c8f8073a0263521d78abe9231792275
dlrjs2360 <lhg3670@naver.com>
2022-10-28 16:41:27 +0900

    :sparkles: feat: < 문제3 | 1.숫자를 문자열로 변형 >

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    // 1. 숫자를 문자열로 바꾸기
+    public static String numToStr(int number) {
+        return String.valueOf(number);
     }
 }
