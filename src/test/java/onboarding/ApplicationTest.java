008aa2b6cdfd4066fde109041aa9f66272267411
neppiness <0414kjh@naver.com>
2022-10-28 11:18:35 +0900

    Draft for java on-boarding mission

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -86,9 +86,9 @@ class ApplicationTest {
             int money = 50_237;
             List<Integer> result = List.of(1, 0, 0, 0, 0, 2, 0, 3, 7);
             assertThat(Problem5.solution(money)).isEqualTo(result);
-        }
+    }
 
-        @Test
+    @Test
         void case2() {
             int money = 15_000;
             List<Integer> result = List.of(0, 1, 1, 0, 0, 0, 0, 0, 0);
