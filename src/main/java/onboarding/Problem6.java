8d5a030b505fe18c84f3958d5f5cea0f4284f3c6
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-28 23:47:29 +0900

    feat(problem6): add key
    * 두글자 단위로 키 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+
+	private static Map<String, String> keys = new HashMap<>(); // word, email
+
 	public static List<String> solution(List<List<String>> forms) {
 
 		for (List<String> form : forms) {
 			String nickname = form.get(1);
 			if (nickname.length() < 2) {
 				continue;
+			}
+
+			String email = form.get(0);
+			for (int j = 0; j < nickname.length() - 1; j++) {
+				String key = nickname.substring(j, j + 2);
+				keys.put(key, email);
 			}
 		}
 
