f37ff4fa7bc91eb07d97261007deaeaf282d59b9
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-29 05:07:20 +0900

    refactor(problem4): divide by function
    * 변환 글자 선택 기능 메소드로 추출

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,22 +7,27 @@ public class Problem4 {
 	private static final int LOWER = 97 + 122;
 
 	public static String solution(String word) {
+		String answer = "";
 		char[] chars = word.toCharArray();
 
+		for (char c : chars) {
+			answer += decideWhatToAdd(c);
+		}
+
+		return answer;
+	}
+
+	private static String decideWhatToAdd(int ascii) {
 		String answer = "";
 
-		for (char c : chars) {
-			int ascii = (int)c;
-
-			if (ascii == BLANK) {
-				answer += " ";
-			} else if (Character.isUpperCase(ascii)) {
-				answer += Character.toString(UPPER - ascii);
-			} else if (Character.isLowerCase(ascii)) {
-				answer += Character.toString(LOWER - ascii);
-			}
-
+		if (ascii == BLANK) {
+			answer = " ";
+		} else if (Character.isUpperCase(ascii)) {
+			answer = Character.toString(UPPER - ascii);
+		} else if (Character.isLowerCase(ascii)) {
+			answer = Character.toString(LOWER - ascii);
 		}
+
 		return answer;
 	}
 }
