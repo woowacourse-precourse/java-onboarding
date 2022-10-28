c4cf0eb21dd68f8698da1ea7e0a32dd7082b532f
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-28 12:15:25 +0900

    포비와 크롱이 임의로 책을 펼친 페이지 수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int por =(int) (Math.random()*197+2)*2;    // 포비가 펼친 오른쪽 페이지
+        int pol = por -1;                          // 포비가 펼친 왼쪽 페이지
+        
+        int crr =(int) (Math.random()*197+2)*2;    // 크롱이 펼친 오른쪽 페이지
+        int crl = crr -1;                          // 크롱이 펼친 왼쪽 페이지
+        
+        
+        
         int answer = Integer.MAX_VALUE;
         return answer;
     }
