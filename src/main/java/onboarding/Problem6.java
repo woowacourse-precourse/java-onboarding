840f02405245a4d0dd20c7598ca4795e75b2ed44
kjb95 <topmedia44@gmail.com>
2022-10-28 18:53:55 +0900

    refactor: Problem6 - 메소드를 분리하여 코드 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,10 +2,8 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,12 +11,39 @@ public class Problem6 {
 
     private static final int TWO = 2;
 
-    public static List<String> solution(List<List<String>> forms) {
-        Map<String, List<String>> nicknameToTwoLetters = new HashMap();
-        Map<String, String> nicknameToEmail = new HashMap<>();
-        Set<String> twoLettersSet = new HashSet<>();
-        List<String> answer = new ArrayList<>();
+    private static Map<String, List<String>> nicknameToTwoLetters = new HashMap();
+    private static Map<String, String> nicknameToEmail = new HashMap<>();
 
+    public static List<String> solution(List<List<String>> forms) {
+        parseForms(forms);
+        return findDuplicatedNicknameAndComputeEmail();
+    }
+
+    private static List<String> findDuplicatedNicknameAndComputeEmail() {
+        return nicknameToTwoLetters.keySet()
+            .stream()
+            .filter(nickname -> isDuplicatedNicknames(nickname, nicknameToTwoLetters.get(nickname)))
+            .map(nickname -> nicknameToEmail.get(nickname))
+            .distinct()
+            .sorted()
+            .collect(Collectors.toList());
+    }
+
+    private static boolean isDuplicatedNicknames(String baseNickname, List<String> baseTwoLetters) {
+        return nicknameToTwoLetters.keySet()
+            .stream()
+            .filter(nickname -> nickname != baseNickname)
+            .filter(nickname -> isDuplicatedTwoLetters(baseTwoLetters, nicknameToTwoLetters.get(nickname)))
+            .count() != 0;
+    }
+
+    private static boolean isDuplicatedTwoLetters(List<String> twoLetters1, List<String> twoLetters2) {
+        return twoLetters1.stream()
+            .filter(twoLetter1 -> twoLetters2.contains(twoLetter1))
+            .count() != 0;
+    }
+
+    private static void parseForms(List<List<String>> forms) {
         forms.forEach(man -> {
             String email = man.get(0);
             String nickname = man.get(1);
@@ -26,25 +51,7 @@ public class Problem6 {
 
             nicknameToTwoLetters.put(nickname, twoLetters);
             nicknameToEmail.put(nickname, email);
-            twoLetters.forEach(twoLetter -> twoLettersSet.add(twoLetter));
         });
-
-        nicknameToTwoLetters.forEach((baseNickname, baseTwoLetters) -> {
-            nicknameToTwoLetters.forEach((nickname, twoLetters) -> {
-                if (baseNickname.equals(nickname)) {
-                    return;
-                }
-                if (baseTwoLetters.stream()
-                    .filter(baseTwoLetter -> twoLetters.contains(baseTwoLetter))
-                    .count() != 0) {
-                    answer.add(nicknameToEmail.get(baseNickname));
-                }
-            });
-        });
-        return answer.stream()
-            .distinct()
-            .sorted()
-            .collect(Collectors.toList());
     }
 
     private static List<String> computeTwoLetters(String input) {
