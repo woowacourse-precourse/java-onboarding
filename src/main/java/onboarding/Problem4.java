470727505226e64d1705821fdf85cc6d1b4b0a12
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 17:42:49 +0900

    feat: Add lowerChange in Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+    private static Character lowerChange(char ch){
+        // a to z, b to x... 모두 두 문자의 아스키코드 값을 더하면 219가 된다.
+        // 따라서 219에서 현재 문자의 아스키코드 값을 뺴면 변환 후 문자의 아스키코드 값이 된다.
+        return (char)(219 - ch);
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
