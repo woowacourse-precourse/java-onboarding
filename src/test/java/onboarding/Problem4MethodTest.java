fada355e5d6d4fbdb5a05ee9766dfa2db558ad0c
phobyjun <phobyjun@gmail.com>
2022-10-29 03:00:40 +0900

    [PROBLEM4] COMPLETE1. 문자 대소문자 및 알파벳 구별 함수 단위 테스트 코드

---
+++ src/test/java/onboarding/Problem4MethodTest.java
@@ -0,0 +1,48 @@
+package onboarding;
+
+import org.junit.jupiter.api.Test;
+
+import static onboarding.Problem4.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Problem4MethodTest {
+
+    @Test
+    void isUpperTest() {
+        char c1 = 'X';
+        char c2 = 'a';
+
+        boolean result1 = isUpper(c1);
+        boolean result2 = isUpper(c2);
+
+        assertThat(result1).isEqualTo(true);
+        assertThat(result2).isEqualTo(false);
+    }
+
+    @Test
+    void isLowerTest() {
+        char c1 = 'x';
+        char c2 = 'A';
+
+        boolean result1 = isLower(c1);
+        boolean result2 = isLower(c2);
+
+        assertThat(result1).isEqualTo(true);
+        assertThat(result2).isEqualTo(false);
+    }
+
+    @Test
+    void isAlphaTest() {
+        char c1 = 'X';
+        char c2 = 'a';
+        char c3 = ' ';
+
+        boolean result1 = isAlpha(c1);
+        boolean result2 = isAlpha(c2);
+        boolean result3 = isAlpha(c3);
+
+        assertThat(result1).isEqualTo(true);
+        assertThat(result2).isEqualTo(true);
+        assertThat(result3).isEqualTo(false);
+    }
+}
