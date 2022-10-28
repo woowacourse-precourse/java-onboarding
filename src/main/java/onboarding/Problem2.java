73624d1ba459e1281652579cf9f21891f6d03d08
hectick <chyeon1223@cau.ac.kr>
2022-10-28 22:23:36 +0900

    feat : [Problem2] 기능 목록 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        //문자열의 처음부터 끝까지 검사하는 것을 반복
+        while(true){
+            int count = 0; //중복 문자를 처리했을 경우 증가
+            //문자열의 처음부터 끝까지 검사하여 연속되는 중복문자들은 1로 대체(문제 조건이 입력은 알파벳 소문자만 주어진다 하였으므로)
+            for(int i = 1; i < cryptogram.length(); i++){
+
+            }
+
+            if(count == 0){
+                //연속되는 중복되는 문자가 더 이상 없을 경우 반복문 탈출
+
+            }else{
+                //숫자로 대체된 문자들을 한번에 삭제
+
+            }
+        }
+
         return answer;
     }
 }
