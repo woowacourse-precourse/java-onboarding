48c372d7ab8cd19f10a8a833612d59982f99b705
heesang <dlagmltkd12@naver.com>
2022-10-28 19:11:30 +0900

    feat(problem4): 알파벳 이외의 문자는 그대로 반환하는 기능

--- src/main/java/problem4/SentenceTranslator.java
+++ src/main/java/problem4/SentenceTranslator.java
@@ -1,11 +1,17 @@
 package problem4;
 
+import static java.lang.Character.*;
+
 public class SentenceTranslator {
 
     public static final char DEFAULT_ALPHABET_A = 'A';
     public static final int ALPHABET_MAX_INDEX = 25;
 
     public char translate(char alphabet) {
+        if (!isAlphabetic(alphabet)) {
+            return alphabet;
+        }
+
         int temp = alphabet - DEFAULT_ALPHABET_A;
         temp = ALPHABET_MAX_INDEX - temp;
 
--- src/test/java/problem4/SentenceTranslatorTest.java
+++ src/test/java/problem4/SentenceTranslatorTest.java
@@ -1,14 +1,21 @@
 package problem4;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SentenceTranslatorTest {
 
+    private SentenceTranslator translator;
+
+    @BeforeEach
+    void setUp() {
+        translator = new SentenceTranslator();
+    }
+
     @Test
     void 알파벳_한개를_변환() {
-        SentenceTranslator translator = new SentenceTranslator();
         char result1 = translator.translate('A');
         char result2 = translator.translate('I');
         char result3 = translator.translate('Z');
@@ -16,5 +23,14 @@ public class SentenceTranslatorTest {
         assertThat(result1).isEqualTo('Z');
         assertThat(result2).isEqualTo('R');
         assertThat(result3).isEqualTo('A');
+    }
+
+    @Test
+    void 알파벳_이외의문자는_그대로반환() {
+        char result1 = translator.translate(' ');
+        char result2 = translator.translate('!');
+
+        assertThat(result1).isEqualTo(' ');
+        assertThat(result2).isEqualTo('!');
     }
 }
