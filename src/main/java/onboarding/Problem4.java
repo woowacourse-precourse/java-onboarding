fb139e274ec2bd485b4316c4a393119ede1117d4
alstjr706 <alstjr706@naver.com>
2022-10-28 18:19:17 +0900

    feat : (청개구리 문제) Problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,9 +1,19 @@
 package onboarding;
 
+import onboarding.problem4.CompareWords;
+import onboarding.problem4.DictionaryGenerator;
+
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
 
-        return answer;
+        DictionaryGenerator generator = new DictionaryGenerator();
+        Map<Character, Character> characterMap = generator.createDictionary();
+
+        CompareWords compareWords = new CompareWords();
+        String result = compareWords.compareSplit(characterMap, word);
+
+        return result;
     }
 }
