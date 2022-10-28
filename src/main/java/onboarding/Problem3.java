f4413da3bc27be6ca1272e63dd14ecfb435f0540
KSH <tkdgns543@gmail.com>
2022-10-28 23:40:07 +0900

    feat(Problem3): 주어진 정수에서 3,6,9 개수의 합을 구하는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    /* 주어진 정수에서 3,6,9 개수의 합을 구하는 메서드 */
+    private static int countThreeNumbers(int number) {
+        int result = 0;
+        String stringNumber = Integer.toString(number);
+
+        for (int i = 0; i < stringNumber.length(); i++) {
+            char c =stringNumber.charAt(i);
+            if (c == '3' || c == '6' || c=='9') {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
