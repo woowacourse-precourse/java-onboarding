988342d87d34a7e5ba7f69fd3996f84556d895d4
situodys <situodys@gmail.com>
2022-10-28 12:11:54 +0900

    feat(문제3): 총 박수 횟수를 계산하여 반환

--- src/main/java/onboarding/problem3/ClapCounter.java
+++ src/main/java/onboarding/problem3/ClapCounter.java
@@ -9,12 +9,15 @@ public class ClapCounter {
     private int maxRangeValue;
 
     public ClapCounter(int maxRangeValue) {
-        this.clapCount=0;
+        this.clapCount = 0;
         this.maxRangeValue = maxRangeValue;
     }
 
     public int count() {
-        return 0;
+        for (int number = 1; number <= maxRangeValue; number++) {
+            this.clapCount += countSatisfiedDigit(number);
+        }
+        return this.clapCount;
     }
 
     private boolean isSatisfy369Rules(int digit) {
@@ -22,10 +25,10 @@ public class ClapCounter {
     }
 
     private int countSatisfiedDigit(int number) {
-        int count=0;
-        while(number>0){
-            if(isSatisfy369Rules(number%10)) count++;
-            number/=10;
+        int count = 0;
+        while (number > 0) {
+            if (isSatisfy369Rules(number % 10)) count++;
+            number /= 10;
         }
         return count;
     }
