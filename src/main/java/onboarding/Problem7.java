47ffb91dea7f13127f037fcd87c1e059b1961928
HyunggiPark <phg9587@naver.com>
2022-10-28 23:33:25 +0900

    feat : 친구 추천시에 쉽게 데이터들을 다루기 위해서 User 클래스 추가, 조건에 맞춰서 comparable 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,66 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    static class User implements Comparable<User> {
+        private String name;
+        private int recommendScore;
+        private List<User> friendList = new ArrayList<>();
+
+        public User(String name, int recommendScore) {
+            this.name = name;
+            this.recommendScore = recommendScore;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<User> getFriendList() {
+            return friendList;
+        }
+
+        public int getRecommendScore() {
+            return recommendScore;
+        }
+
+        public void setRecommendScore(int recommendScore) {
+            this.recommendScore = recommendScore;
+        }
+
+        public void addRecommendScoreByFriendship() {
+            if (this.recommendScore < 0) {
+                return;
+            }
+
+            this.recommendScore += 10;
+        }
+
+        public void addRecommendScoreByVisitors() {
+            if (this.recommendScore < 0) {
+                return;
+            }
+
+            this.recommendScore += 1;
+        }
+
+        public void setFriendState() {
+            this.recommendScore = -1;
+        }
+
+        @Override
+        public int compareTo(User u) {
+            if (this.getRecommendScore() > u.getRecommendScore()) {
+                return -1;
+            } else if (this.getRecommendScore() < u.getRecommendScore()) {
+                return 1;
+            } else {
+                return this.getName().compareTo(u.getName());
+            }
+        }
+    }
 }
