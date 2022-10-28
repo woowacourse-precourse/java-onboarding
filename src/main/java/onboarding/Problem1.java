11c6c97bb07fc015fa4d31691ece94fd6f75ab9d
stk346 <stk346@gamil.com>
2022-10-28 13:39:52 +0900

    feat. 승, 패, 무승부 로직 구현 완료.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 class Problem1 {
 
-    private int doSum(int num) {
+    private static int doSum(int num) {
         String stringInt = String.valueOf(num);
         int sum = 0;
         for (int i=0; i< stringInt.length(); i++) {
@@ -14,7 +14,7 @@ class Problem1 {
         return sum;
     }
 
-    private int doMultiply(int num) {
+    private static int doMultiply(int num) {
         String stringInt = String.valueOf(num);
         int multiply = 1;
         for (int i=0; i< stringInt.length(); i++) {
@@ -24,15 +24,33 @@ class Problem1 {
         return multiply;
     }
 
-    private int chooseLargeNumber(int num) {
+    private static int chooseLargeNumber(int num) {
         int summedNum = doSum(num);
         int multipliedNum = doMultiply(num);
         return Math.max(summedNum, multipliedNum);
     }
 
+    private static int chooseLageNumber(List<Integer> person) {
+        int leftNum = chooseLargeNumber(person.get(0));
+        int rightNum = chooseLargeNumber(person.get(1));
+        return Math.max(leftNum, rightNum);
+    }
+
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int pobiNum = chooseLageNumber(pobi);
+        int crongNum = chooseLageNumber(crong);
+        int answer = Math.max(pobiNum, crongNum);
+
+        if (pobiNum == crongNum) {
+            return 0;
+        }
+        if (answer == pobiNum) {
+            return 1;
+        }
+        if (answer == crongNum) {
+            return 2;
+        }
 
         return answer;
     }
