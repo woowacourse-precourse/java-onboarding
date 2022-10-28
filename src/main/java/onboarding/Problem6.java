9f9d5780edf72bba7d86c7f1050de4b14957fc81
MoonJeWoong <munjin0201@naver.com>
2022-10-29 00:42:06 +0900

    [6번 문제] feat: solution 메소드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public HashMap<String,Integer> makeCheckMap(List<List<String>> forms){
+        HashMap<String,Integer> map = new HashMap<>();
+        return map;
+    }
+
+    public ArrayList<String> duplicatedEmails(HashMap<String,Integer> map, List<List<String>> forms){
+        ArrayList<String> emails = new ArrayList<>();
+        return emails;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Problem6 T = new Problem6();
+
+        HashMap<String,Integer> map = T.makeCheckMap(forms);
+        List<String> answer = T.duplicatedEmails(map, forms);
+
         return answer;
     }
 }
