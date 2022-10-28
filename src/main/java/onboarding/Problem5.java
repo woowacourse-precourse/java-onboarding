f0e9146f75be2ffc1fb7358d796c6c7adfd42f58
CHAYEONIL <jeay123@naver.com>
2022-10-28 18:29:44 +0900

    문제5 예외처리 1미만, 100,000초과 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,10 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         int[] check_money = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        boolean account = validation(money);
+        if (!account) {
+            return answer;
+        }
         //큰 금액별로 나누고 나머지 넘기기
         for(int i = 0; i < check_money.length; i++) {
             if (i == 0) {
@@ -18,5 +22,13 @@ public class Problem5 {
            }
         }
         return answer;
+    }
+
+    private static boolean validation(int num) {
+        // 1미만, 1000000 초과
+        if(num < 1 || num > 1000000) {
+            return false;
+        }
+        return true;
     }
 }
