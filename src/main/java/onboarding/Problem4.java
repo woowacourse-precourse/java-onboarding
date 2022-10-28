4d871a83e0e38c0e765158e366cdc9f8ee12996d
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-28 21:04:38 +0900

    feat(Problem4): 대소문자 구분
    문자 변환을 위해 대문자인지 소문자인지 구분한다. 또한, 공백 문자는 넘어가도록 한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,17 +1,31 @@
 package onboarding;
 
 public class Problem4 {
+    private static final char BLANK_CHARACTER = ' ';
+
+
     public static String solution(String word) {
         String answer = "";
 
         for (int i = 0; i < word.length(); i++) {
-            getGreenFrogChar(word.charAt(i));
+            char originalChar = word.charAt(i);
+            if (originalChar != BLANK_CHARACTER) {
+                getGreenFrogChar(originalChar);
+            }
         }
 
         return answer;
     }
 
     private static void getGreenFrogChar(char originalChar) {
-        System.out.println(originalChar);
+        if (isCapitalLetter(originalChar)) {
+            System.out.println(originalChar + " : 대문자입니다.");
+        } else {
+            System.out.println(originalChar + " : 소문자입니다.");
+        }
+    }
+
+    private static boolean isCapitalLetter(char checkChar) {
+        return Character.isUpperCase(checkChar);
     }
 }
