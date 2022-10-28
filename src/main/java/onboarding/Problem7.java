1ee5f45f94b2051dd286093ff8746d75b2d70eed
seokjin <rnrn8678@naver.com>
2022-10-28 11:40:08 +0900

    feat(Problem7.java): friends 배열의 아이디 B에 대한 점수를 계산하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		HashMap<String, Integer> recommendFriends = new HashMap<>();
+		for (List<String> friend : friends) {
+			if (recommendFriends.containsKey(friend.get(1))) {
+				recommendFriends.put(friend.get(1), recommendFriends.get(friend.get(1)) + 10);
+			} else {
+				recommendFriends.put(friend.get(1), 10);
+			}
+		}
+		System.out.println(recommendFriends);
+
+		return Collections.emptyList();
+	}
 }
