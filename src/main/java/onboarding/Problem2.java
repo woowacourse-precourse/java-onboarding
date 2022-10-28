4f79cd741ac5dc46b361983b48a96d522f62c3dc
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-29 02:55:36 +0900

    [Problem2] feat: decodeCruptogram 구현, solution 수정
    - 매개변수 참조를 변경하지 않으면 반환없이 solution의 sb를 변경할 수 있으므로 decodeCruptogram()이 void값을 반환하도록 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,22 +5,30 @@ public class Problem2 {
         String answer = "";
         // crytogram와 길이, 값이 일치한 StringBuffer 생성
         StringBuffer sb = getStringBuffer(cryptogram);
-        // sb의 중복을 제거하여 반환
-        sb = decodeCryptogram(sb);
+        // sb의 중복을 제거
+        decodeCryptogram(sb);
         // 중복 제거된 cryptogramStringBuffer을 String으로 변환
         answer = sb.toString();
         return answer;
     }
 
-    // String 매개변수와 길이, 값이 일치한 StringBuffer 반환
+    /* String 매개변수와 길이, 값이 일치한 StringBuffer 반환 */
     private static StringBuffer getStringBuffer(String cryptogarm) {
         StringBuffer sb = new StringBuffer(cryptogram.length());
         sb.append(cryptogram);
         return sb;
     }
 
-    // StringBuffer의 중복을 제거하여 암호해독한 StringBuffer를 반환
-    private static StringBuffer decodeCryptogram(StringBuffer cryptogarmStringBuffer) {
-
+    /* StringBuffer의 중복을 제거하는 메서드 */
+    private static void decodeCryptogram(StringBuffer cryptogarmStringBuffer) {
+        // 중복이 없을 때까지 중복을 찾아 제거하는 함수 실행
+        do {
+            // 특정 인덱스의 char와 연속된 중복을 찾아 제거
+            boolean isRemoved = removeDuplication(cryptogarmStringBuffer);
+        } while(isRemoved == true) // 더이상 중복이 제거되지 않으면 while문 종료
+        return;
     }
+
+    private static boolean removeDuplication(StringBuffer cryptogarmStringBuffer);
+
 }
