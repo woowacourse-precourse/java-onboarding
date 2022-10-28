584f426aacfe5b121b7513105206978e8259f325
Isu Shin <isu010614@naver.com>
2022-10-29 01:03:27 +0900

    feat: Add function to concat letters

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); i++){
+            answer += isAlphabet(word.charAt(i));
+        }
         return answer;
     }
 
