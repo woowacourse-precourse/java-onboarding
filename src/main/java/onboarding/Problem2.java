c50d4d5883889083f44f8a34fbd06262d80c3a11
sosow0212 <sosow0212@naver.com>
2022-10-28 15:56:55 +0900

    feat: Problem2 - 2개 이상의 중복된 문자열을 제거해주는 deleteOverlapChar 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
@@ -19,5 +22,27 @@ public class Problem2 {
             }
         }
         return isNotMoreDelete;
+    }
+
+    public static String deleteOverlapChar(String cryptogram) {
+        List<Integer> deleteIndexOfCharAt = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(cryptogram);
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i < sb.length(); i++) {
+            char now = sb.charAt(i);
+            if (now == sb.charAt(i - 1)) {
+                deleteIndexOfCharAt.add(i - 1);
+                deleteIndexOfCharAt.add(i);
+            }
+        }
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!deleteIndexOfCharAt.contains(i)) {
+                result.append(cryptogram.charAt(i));
+            }
+        }
+
+        return result.toString();
     }
 }
