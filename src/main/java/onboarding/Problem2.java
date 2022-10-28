9acb3d97d170536bff3928254473be6a704273aa
math0917 <math0917@naver.com>
2022-10-28 22:00:58 +0900

    feat : validation을 통과못하면 빈 문자열반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import onboarding.problem2Validation.Problem2Validation;
+
 public class Problem2 {
+
+    private final static int INPUT_MAX_LENGTH = 1000;
+    private final static int INPUT_MIN_LENGTH = 1;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        if (Problem2Validation.problem2InputValidation(cryptogram, INPUT_MIN_LENGTH, INPUT_MAX_LENGTH)) {
+            String answer = "answer";
+            return answer;
+        } else {
+            return "";
+        }
+
     }
 }
