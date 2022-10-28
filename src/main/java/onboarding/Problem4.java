f35b215605c8e2207bb8a2840ee6a025ee38fdba
whdcks420 <whdcks420@gmail.com>
2022-10-28 18:27:57 +0900

    refector: Problem4 refectoring

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,13 +7,13 @@ public class Problem4 {
         return word.chars().map(Problem4::swapAlphabet).mapToObj(Character::toString).collect(Collectors.joining());
     }
 
-    private static int swapAlphabet(int i) {
-        if (i > 96 && i < 123) {
-            return 219 - i;
+    private static int swapAlphabet(int alphabetASCIICode) {
+        if (alphabetASCIICode > 96 && alphabetASCIICode < 123) {
+            return 219 - alphabetASCIICode;
         }
-        if (i > 64 && i < 91) {
-            return 155 - i;
+        if (alphabetASCIICode > 64 && alphabetASCIICode < 91) {
+            return 155 - alphabetASCIICode;
         }
-        return i;
+        return alphabetASCIICode;
     }
 }
