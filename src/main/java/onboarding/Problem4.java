fd85bf40bd80c0058b3efd1dbe982d71d936b2f8
khj1 <dhepd8849@gmail.com>
2022-10-28 16:51:37 +0900

    feat(problem4): 소문자를 반대로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,8 @@ public class Problem4 {
     public static final char Z_LOWER_CASE = 'z';
     public static final char A_UPPER_CASE = 'A';
     public static final char Z_UPPER_CASE = 'Z';
+    public static final String EMPTY_STRING = "";
+    public static final int LOWER_CASE_CONVERTER = 155;
 
     public static String solution(String word) {
         return convertIntoBlueFrogWord(word);
@@ -12,13 +14,19 @@ public class Problem4 {
 
     private static String convertIntoBlueFrogWord(String word) {
         char[] letters = word.toCharArray();
-        for (char letter : letters) {
+        for (int i = 0; i < letters.length; i++) {
+            char letter = letters[i];
             if (isLowerCase(letter)) {
+                letters[i] = convertLowerCaseIntoFrogLetter(letter);
             }
             if (isUpperCase(letter)) {
             }
         }
         return null;
+    }
+
+    private static char convertLowerCaseIntoFrogLetter(char letter) {
+        return (char)(LOWER_CASE_CONVERTER - letter);
     }
 
     private static boolean isUpperCase(char letter) {
