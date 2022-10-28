d75ef1e562de968d8dc93d6672141c85f6672001
June <68291395+june-777@users.noreply.github.com>
2022-10-28 14:20:02 +0900

    feat: 예외사항 확인 기능 추가 및 통합
    1. isRightPageOneMore(): 연속된 페이지 여부 확인
    2. isLeftPageOdd(): 왼쪽 페이지 홀수 확인
    3. isRightPageEven(): 오른쪽 페이지 짝수 확인
    4. 전체 예외사항 확인 메소드에 각 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,20 +8,46 @@ class Problem1 {
         
         return answer;
     }
-    public static boolean isException(Integer leftPage, Integer rightPage) {
+    public static boolean isCorrectPage(Integer leftPage, Integer rightPage) {
     	
     	if(isNullPage(leftPage, rightPage)) {
-    		return true;
+    		return false;
     	}
+    	if(!isRightPageOneMore(leftPage, rightPage)) {
+    		return false;
+    	}
+    	if(!isLeftPageInRange(leftPage)) {
+    		return false;
+    	}
+    	if(!isRightPageInRange(rightPage)) {
+    		return false;
+    	}
+    	if(!isLeftPageOdd(leftPage)) {
+    		return false;
+    	}
+    	if(!isRightPageEven(rightPage)) {
+    		return false;
+    	}
+    	return true;
     	
-    	return false;
-    	
+    }
+    public static boolean isRightPageEven(int rightPage) {
+    	return (rightPage % 2) == 0;
+    }
+    public static boolean isLeftPageOdd(int leftPage) {
+    	return (leftPage % 2) == 1;
+    }
+    public static boolean isRightPageOneMore(int leftPage, int rightPage) {
+    	return (rightPage - leftPage)==1;
     }
     public static boolean isNullPage(Integer leftPage, Integer rightPage) {
     	return leftPage==null || rightPage==null;
     }
     public static boolean isLeftPageInRange(int leftPage) {
     	return 1<=leftPage && leftPage<=399;
+    }
+    public static boolean isRightPageInRange(int rightPage) {
+    	return 2<=rightPage && rightPage<=400;
     }
     
 }
