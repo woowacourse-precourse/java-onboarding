94d90f909a1ee93ca6688b462e2804d6521ca98b
jeujeon <1132jjw@naver.com>
2022-10-28 16:26:49 +0900

    feat: add Input exception in Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+    static final int MIN_LENGTH = 1;
+    static final int MAX_LENGTH = 10000;
     public static int solution(int number) {
         int answer = 0;
+
+        if (!isValidInput(number)) {
+            answer = -1;
+            return answer;
+        }
+
         return answer;
+    }
+
+    private static boolean isValidInput(int number) {
+        return isValidRange(number);
+    }
+
+    private static boolean isValidRange(int number) {
+        return number >= MIN_LENGTH && number <= MAX_LENGTH;
     }
 }
