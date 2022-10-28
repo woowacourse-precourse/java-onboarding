4563c9e9408d8bb4e6c4c699700a0f4a0fcb74b9
areyouhun <areyouhun@gmail.com>
2022-10-29 05:41:17 +0900

    feat(prob5): add money sorter class to problem5 file

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,9 +3,12 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+import onboarding.feature5.MoneySorter;
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        MoneySorter moneySorter = new MoneySorter(money);
+        List<Integer> answer = moneySorter.calculateNumberOfBill();
         return answer;
     }
 }
