21fe1bfdef32958d0fdd910673dc0191aa891d43
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-10-28 20:39:22 +0900

    Problem4 : 대문자청개구리변환 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        char[] charArray = word.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            toUpperCaseBackward(charArray[i]);
+            toLowerCaseBackward(charArray[i]);
+        }
+
         String answer = "";
         return answer;
     }
+
+    public static char toUpperCaseBackward(char upperCase) {
+        if (upperCase >= 65 && upperCase <= 90) {
+            upperCase = (char)(90 - (upperCase - 65));
+        }
+        return upperCase;
+    }
+
+    public static void toLowerCaseBackward(char lowerCase) {
+
+    }
+
+
 }
