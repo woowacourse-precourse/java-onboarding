86a79eb67622256458bf4949e91232c49268ae1f
DESKTOP-A4VVJHE\윤지혜 <dbsquddlfz@naver.com>
2022-10-28 22:22:47 +0900

    아직 4번 해결 못함 3번도 더 수정하자 / 리팩토링 , 필기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String check = "";
+        char numCheck = ' ';
+
+        for (int i = 1; i <= number; i++) {
+            check = ""+i;
+            for (int j = 0; j < check.length(); j++) {
+                numCheck = check.charAt(j);
+                System.out.println("numCheck = " + numCheck);
+                if (numCheck == '3' || numCheck == '6' || numCheck == '9') {
+                    answer++;
+                }
+            }
+        }
+        System.out.println("answer = " + answer);
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,34 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int code = 0;
+        char[] c = new char[word.length()];
+        char tttt = 'A';
+
+        System.out.println("tttt = " + (tttt-48));
+
+        for (int i = 0; i < 10; i++) {
+            i++;
+            System.out.println("i = " + i);
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            c = word.toCharArray();
+            System.out.println("c[i] = " + c[i]);
+            System.out.println("(c[i]-'0') = " + (c[i]+'0'));
+            int test = c[i]-'0';
+            System.out.println("test = " + (test+'0'));
+        }
+
+
+
+//        for (int i = 0; i < word.length(); i++) {
+//            code = word.charAt(i)+19;
+//            System.out.println("code = " + code);
+//            char c = (char)code;
+//            System.out.println("c = " + c);
+//        }
+        
         return answer;
     }
 }
