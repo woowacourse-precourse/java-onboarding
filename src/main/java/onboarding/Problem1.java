1a1a504de4e87ab84cbb15083c9aef2a8d9d3270
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 17:32:29 +0900

    refactor : Problem1 예외처리 추가, 메소드 기능 분리 및 변수네이밍 재설정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,26 +6,38 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int left;
-        int right;
-        // exception check
-        // Q1. 예외가 어떤 케이스를 제공하는지 모르겠다..
-        if(pobi.get(0)+1 != pobi.get(1) || crong.get(0)+1 != crong.get(1)) return -1;
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+        int pobiMax;
+        int crongMax;
 
-        // find max value
-        // Q1. 따로 함수를 만들어서 하는게 깔끔한가?
-        // Q2. Math.max 가 아닌 리스트에 넣어서 sort 해서 max 값을 찾는게 더 빠른가?
-        left = Math.max(plusBookNum(pobi.get(0)), multiBookNum(pobi.get(0)));
-        right = Math.max(plusBookNum(pobi.get(1)), multiBookNum(pobi.get(1)));
-        int pobi_max = Math.max(left, right);
-        left = Math.max(plusBookNum(crong.get(0)), multiBookNum(crong.get(0)));
-        right = Math.max(plusBookNum(crong.get(1)), multiBookNum(crong.get(1)));
-        int crong_max = Math.max(left, right);
+        if(checkException(pobiLeftPage, pobiRightPage) || checkException(crongLeftPage, crongRightPage))
+            return -1;
 
-        if(pobi_max > crong_max) answer = 1;
-        else if(pobi_max < crong_max) answer = 2;
-        else if(pobi_max == crong_max) answer = 0;
+        pobiMax = getMaxScore(pobiLeftPage, pobiRightPage);
+        crongMax = getMaxScore(crongLeftPage, crongRightPage);
+
+        if(pobiMax > crongMax) answer = 1;
+        else if(pobiMax < crongMax) answer = 2;
+        else if(pobiMax == crongMax) answer = 0;
         return answer;
+    }
+
+    public static int getMaxScore(int leftPage, int rightPage){
+        return Math.max(Math.max(plusBookNum(leftPage), multiBookNum(leftPage)),
+                Math.max(plusBookNum(rightPage), multiBookNum(rightPage)));
+    }
+
+    public static boolean checkException(int leftPage, int rightPage){
+        if(leftPage+1 != rightPage
+                ||(leftPage < 3) ||(rightPage < 3)
+                ||(leftPage > 398) ||(rightPage > 398)
+                ||((leftPage % 2) == 0) ||((rightPage % 2) == 1))
+            return true;
+        else
+            return false;
     }
 
     public static int plusBookNum(int num){
