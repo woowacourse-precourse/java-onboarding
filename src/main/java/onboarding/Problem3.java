4e9dacc6ace8405711ff876d79f3b5d9671a9a3f
김태용 <amico741@naver.com>
2022-10-28 21:13:49 +0900

    refactor: solution 메서드 개선

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,16 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Problem3 {
+    
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        ArrayList<String> array = new ArrayList<>();
+        for(int i = 1; i < number + 1; i++){
+            array.add(String.valueOf(i));
+        }
+        return getCount(array);
     }
 
     public static int getCount(ArrayList<String> array){
