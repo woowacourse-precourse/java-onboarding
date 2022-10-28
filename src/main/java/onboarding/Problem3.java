9d33f58fb64e97c68c913e8e5e50dad0ccb7d860
jipyo0106 <jg020106@naver.com>
2022-10-28 14:36:28 +0900

    증가된 숫자가 369가 포함되어 있는지 확인 후 count 증가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,17 @@ public class Problem3 {
         int count = 0;
 
         for (int oneFromNumber = 1; oneFromNumber <= number; oneFromNumber++) {
+            int digit = 1;
 
+            while (oneFromNumber / digit != 0) {
+                switch (oneFromNumber / digit % 10) {
+                    case 3:
+                    case 6:
+                    case 9:
+                        count += 1;
+                }
+                digit *= 10;
+            }
         }
 
         return count;
