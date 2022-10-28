184ebc26a41cf24c1ac39338060027ad630cbe16
sonsoowon <yuwoo1018@gmail.com>
2022-10-28 16:51:57 +0900

    feat(P6): 이중 리스트 입력을 HashMap으로 변환하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    public static Map<String, String> listToMap(List<List<String>> forms){
+        Map<String, String> result = new HashMap<>();
+        forms.stream().forEach(form -> result.put(form.get(0), form.get(1)));
+
+        return result;
     }
 }
