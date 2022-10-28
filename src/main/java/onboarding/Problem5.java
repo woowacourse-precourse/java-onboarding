bf8dbefa8ae8998e9c8bb2688d6b89c95e6c410e
kimJaeHyeon <kmr2644@gmail.com>
2022-10-28 21:28:34 +0900

    feat: implement method for getCoinList

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,20 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    public static List<Integer> getCoinList(int money) {
+        List<Integer> coinList = Collections.emptyList();
+        int[] coinUnitArray = getCoinUnitToArray();
+        int index = 0;
+        while (money > 0) {
+            int coinUnit = coinUnitArray[index];
+            coinList.add(money / coinUnit);
+            money = money % coinUnit;
+        }
+        return coinList;
+    }
+
+    private static int[] getCoinUnitToArray() {
+        return new int[]{50000,10000,5000,1000,500,100,50,10,1};
+    }
 }
