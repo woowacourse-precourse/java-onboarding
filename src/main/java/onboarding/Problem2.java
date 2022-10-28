19ce9ba40cef78e3cb8782b0621cd05893e0ad52
KyungKyu Park <rudrb0505@gmail.com>
2022-10-28 11:45:05 +0900

    feat(Problem2): 중복된 문자열들을 반복해서 없애는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,13 +2,18 @@ package onboarding;
 
 public class Problem2 {
 	public static String solution(String cryptogram) {
-		char [] cryptogramArray = cryptogram.toCharArray();
+		char[] cryptogramArray = cryptogram.toCharArray();
 
 		int outputIndex = 1;
+		char checkingChar = cryptogramArray[0];
 		for (int i = 1; i < cryptogramArray.length; i++) {
 			if (outputIndex > 0 && cryptogramArray[i] == cryptogramArray[outputIndex - 1]) {
-				outputIndex--;
-			}  else {
+				checkingChar = cryptogramArray[outputIndex - 1];
+				while (outputIndex > 0 && cryptogramArray[outputIndex - 1] == checkingChar) {
+					outputIndex--;
+				}
+			} else if (cryptogramArray[i] == checkingChar) {
+			} else {
 				cryptogramArray[outputIndex++] = cryptogramArray[i];
 			}
 		}
