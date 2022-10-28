c3250ffa57e80ffd3b2948d36b79442afff26390
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 22:26:29 +0900

    refactor: Problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,80 +3,80 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
-    private static List<String> createFriendList(String user, List<List<String>> friends){
-        List<String> friendList = new ArrayList<>();
+    private static List<String> createUserFriends(String user, List<List<String>> friends){
+        List<String> userFriends = new ArrayList<>();
 
         for(int i = 0; i < friends.size(); i++){
             if(friends.get(i).get(0) == user)
-                friendList.add(friends.get(i).get(1));
+                userFriends.add(friends.get(i).get(1));
             else if(friends.get(i).get(1) == user)
-                friendList.add(friends.get(i).get(0));
+                userFriends.add(friends.get(i).get(0));
         }
 
-        return friendList;
+        return userFriends;
     }
 
-    private static List<String> createFriendsFriendList(String user, List<String> friendList, List<List<String>> friends){
-        List<String> friendsFriendList = new ArrayList<>();
+    private static List<String> createUserFriendFriends(String user, List<String> userFriends, List<List<String>> friends){
+        List<String> userFriendFriends = new ArrayList<>();
 
-        for(int i = 0; i < friendList.size(); i++){
+        for(int i = 0; i < userFriends.size(); i++){
             for(int j = 0; j < friends.size(); j++){
-                if(friendList.get(i) == friends.get(j).get(0))
-                    friendsFriendList.add(friends.get(j).get(1));
-                else if(friendList.get(i) == friends.get(j).get(1))
-                    friendsFriendList.add(friends.get(j).get(0));
+                if(userFriends.get(i) == friends.get(j).get(0))
+                    userFriendFriends.add(friends.get(j).get(1));
+                else if(userFriends.get(i) == friends.get(j).get(1))
+                    userFriendFriends.add(friends.get(j).get(0));
             }
         }
 
-        if(friendsFriendList.contains(user)){
-            friendsFriendList.remove(user);
+        if(userFriendFriends.contains(user)){
+            userFriendFriends.remove(user);
         }
 
-        return friendsFriendList;
+        return userFriendFriends;
     }
 
-    private static HashMap<String, Integer> getRecommendPoint(String user, List<String> friendList, List<String> ffList, List<String> visitorList){
-        HashMap<String, Integer> pointMap = new HashMap<>();
+    private static HashMap<String, Integer> createPoints(String user, List<String> userFriends, List<String> userFriendFriends, List<String> visitorList){
+        HashMap<String, Integer> points = new HashMap<>();
 
-        for(int i = 0; i < ffList.size(); i++){
-            if(pointMap.containsKey(ffList.get(i)))
-                pointMap.put(ffList.get(i), pointMap.get(ffList.get(i)) + 10);
+        for(int i = 0; i < userFriendFriends.size(); i++){
+            if(points.containsKey(userFriendFriends.get(i)))
+                points.put(userFriendFriends.get(i), points.get(userFriendFriends.get(i)) + 10);
             else
-                pointMap.put(ffList.get(i), 10);
+                points.put(userFriendFriends.get(i), 10);
         }
 
         for(int i = 0; i < visitorList.size(); i++){
-            if(pointMap.containsKey(visitorList.get(i)))
-                pointMap.put(visitorList.get(i), pointMap.get(visitorList.get(i)) + 1);
+            if(points.containsKey(visitorList.get(i)))
+                points.put(visitorList.get(i), points.get(visitorList.get(i)) + 1);
             else
-                pointMap.put(visitorList.get(i), 1);
+                points.put(visitorList.get(i), 1);
         }
 
-        for(int i = 0; i < friendList.size(); i++){
-            if(pointMap.containsKey(friendList.get(i)))
-                pointMap.remove(friendList.get(i));
+        for(int i = 0; i < userFriends.size(); i++){
+            if(points.containsKey(userFriends.get(i)))
+                points.remove(userFriends.get(i));
         }
 
-        if(pointMap.containsKey(user)){
-            pointMap.remove(user);
+        if(points.containsKey(user)){
+            points.remove(user);
         }
 
-        return pointMap;
+        return points;
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
-        List<String> friendList = createFriendList(user, friends);
-        List<String> ffList = createFriendsFriendList(user, friendList, friends);
-        HashMap<String, Integer> pointMap = getRecommendPoint(user, friendList, ffList, visitors);
+        List<String> userFriends = createUserFriends(user, friends);
+        List<String> userFriendFriends = createUserFriendFriends(user, userFriends, friends);
+        HashMap<String, Integer> points = createPoints(user, userFriends, userFriendFriends, visitors);
 
-        List<Map.Entry<String, Integer>> sortedPointList = new ArrayList<>(pointMap.entrySet());
-        sortedPointList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        sortedPointList.forEach(System.out::println);
+        List<Map.Entry<String, Integer>> sortedPoints = new ArrayList<>(points.entrySet());
+        sortedPoints.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        sortedPoints.forEach(System.out::println);
 
-        for(int i = 0; i < sortedPointList.size(); i++) {
-            if (pointMap.get(sortedPointList.get(i).getKey()) != 0 && answer.size() < 5)
-                answer.add(Objects.toString(sortedPointList.get(i).getKey()));
+        for(int i = 0; i < sortedPoints.size(); i++) {
+            if (points.get(sortedPoints.get(i).getKey()) != 0 && answer.size() < 5)
+                answer.add(Objects.toString(sortedPoints.get(i).getKey()));
             else
                 break;
         }
