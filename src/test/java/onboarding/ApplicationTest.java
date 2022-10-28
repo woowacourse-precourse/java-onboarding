9379db9ce05d15977f1d0977f5d624f17b0812e9
im-shung <seoyoung1113@gmail.com>
2022-10-29 00:12:55 +0900

    :white_check_mark: [문제1] 테스트케이스 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -33,6 +33,46 @@ class ApplicationTest {
             int result = -1;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
+
+        @Test
+        void case4() {
+            List<Integer> pobi = List.of(99, 99);
+            List<Integer> crong = List.of(211, 212);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+
+        @Test
+        void case5() {
+            List<Integer> pobi = List.of(400, 401);
+            List<Integer> crong = List.of(211, 212);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+
+        @Test
+        void case6() {
+            List<Integer> pobi = List.of(0, 1);
+            List<Integer> crong = List.of(211, 212);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+
+        @Test
+        void case7() {
+            List<Integer> pobi = List.of(101, 102);
+            List<Integer> crong = List.of(399, 400);
+            int result = 2;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
+
+        @Test
+        void case8() {
+            List<Integer> pobi = List.of(-1, 0);
+            List<Integer> crong = List.of(245, 246);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
     }
 
     @Nested
