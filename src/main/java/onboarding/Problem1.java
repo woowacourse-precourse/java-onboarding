ccd75889f7b4835ac47f234d57a0598a2d9a9be5
es <mascoeskim@gmail.com>
2022-10-28 12:58:03 +0900

    Problem1/1.초기세팅,페이지조건설정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1){
+            System.out.println(pobi.get(1) - pobi.get(0));
+            System.out.println(crong.get(1) - crong.get(0));
+            return -1;
+        }
+        int sumLeftPobi = 0;
+        int sumRightPobi = 0;
+        int sumLeftCrong = 0;
+        int sumRightCrong = 0;
+
+        System.out.println(sumLeftPobi +","+ sumRightPobi +","+ sumLeftCrong +","+ sumRightCrong);
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
-}
+}w
