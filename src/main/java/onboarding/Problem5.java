b4b2b12fc35aec2e27341c9d120cb1f88f6f1d09
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 18:21:22 +0900

    feat: Add withdraw in Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static List<Integer> withdraw(int money){
+        List<Integer> moneyList = Collections.emptyList();
+
+        moneyList.add(money / 50000);
+        money %= 50000;
+        moneyList.add(money / 10000);
+        money %= 10000;
+        moneyList.add(money / 5000);
+        money %= 5000;
+        moneyList.add(money / 1000);
+        money %= 1000;
+        moneyList.add(money / 500);
+        money %= 500;
+        moneyList.add(money / 100);
+        money %= 100;
+        moneyList.add(money / 50);
+        money %= 50;
+        moneyList.add(money / 10);
+        money %= 10;
+        moneyList.add(money);
+
+        return moneyList;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
