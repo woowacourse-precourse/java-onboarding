0fb3bd73f6ae099a75f43ae4246c7843ceb05f35
antaeho <an981022@naver.com>
2022-10-28 11:11:14 +0900

    Problem4 기능구현 사항 1번 완성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,10 +1,24 @@
 package onboarding;
+
+
 /*
 - 기능 구현사항 목록 -
 1. 각 자리의 문자를 받아 규칙에 따라 변역하는 함수 구현
 2. 번역된 문자를 배열로 모아두었다가 문자열로 Return
  */
 public class Problem4 {
+    static String upper_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static String lower_char = "abcdefghijklmnopqrstuvwxyz";
+
+    static char trans_char(char a) {
+        char temp = a;
+        if (Character.isLowerCase(a)) {
+            temp = lower_char.charAt(25 - a + 'a');
+        } else if (Character.isUpperCase(a)) {
+            temp = upper_char.charAt(25 - a + 'A');
+        }
+        return temp;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
