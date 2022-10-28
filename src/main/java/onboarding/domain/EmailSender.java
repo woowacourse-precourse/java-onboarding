4083c65fdd85de6d9e2810b080f855ec2b396795
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-10-28 13:09:29 +0900

    feat(Problem6, EmailSender): 모든 닉네임을 검사, 결과를 저장하는 기능 구현

--- src/main/java/onboarding/domain/EmailSender.java
+++ src/main/java/onboarding/domain/EmailSender.java
@@ -4,14 +4,25 @@ import java.util.HashSet;
 import java.util.List;
 
 public class EmailSender {
-    private final HashSet<String> duplicateCrews;
+    private final HashSet<String> duplicateCrews = new HashSet<>();
 
     public EmailSender(List<List<String>> forms) {
-        this.duplicateCrews = extractDuplicateCrew(forms);
+        for (int i = 0; i < forms.size() - 1; i++) {
+            compareCrew(i, forms);
+        }
     }
 
-    private HashSet<String> extractDuplicateCrew(List<List<String>> forms) {
-        return null;
+    private void compareCrew(int currentIndex, List<List<String>> forms) {
+        for (int i = currentIndex + 1; i < forms.size(); i++) {
+            checkCrewsNickname(forms.get(currentIndex), forms.get(i));
+        }
+    }
+
+    private void checkCrewsNickname(List<String> currentCrew, List<String> anotherCrew) {
+        if (isDuplicateNickname(currentCrew.get(1), anotherCrew.get(1))) {
+            duplicateCrews.add(currentCrew.get(0));
+            duplicateCrews.add(anotherCrew.get(0));
+        }
     }
 
     private boolean isDuplicateNickname(String currentNickname, String anotherNickname) {
