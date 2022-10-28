c63682dd76d4af6f7af7253dcd06bd2cc88c2b00
KTH-96 <taehyun960@gmail.com>
2022-10-28 16:55:52 +0900

    comment(Problem3): 기능구현 목록 정리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+
+	/**
+	 * 기능 목록 작성
+	 * 1. 주어진 자연수까지 박수를 몇번 쳤는지 계산하는 기능
+	 */
+	public static int solution(int number) {
+		int answer = 0;
+		return answer;
+	}
 }
