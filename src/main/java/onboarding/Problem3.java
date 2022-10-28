a6f63b9c3c1ded40bb0289b7ccddd0e6fb196f6b
kiarakim <kiarakim413@gmail.com>
2022-10-28 13:23:06 +0900

    feat(Problem3): check each number-character

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,9 +4,17 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         String stringNumber = "";
+        int stringLength = 0;
 
         for (int i = 1; i <= number; i++) {
             stringNumber = Integer.toString(i);
+            stringLength = stringNumber.length();
+
+            for (int s = 0; s < stringLength; s++) {
+                if (stringNumber.charAt(s) == '3') {
+                    //
+                }
+            }
         }
         return answer;
     }
