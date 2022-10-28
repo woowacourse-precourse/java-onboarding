9bd4058320732f92591126a4a2941c73b61dc465
KangHyeon3315 <son2786w@gmail.com>
2022-10-28 17:32:15 +0900

    feat: [7번] 친구 추천 기능 목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    /** 친구 관계 리스트를 각 유저를 Key로 가지는 친구 List Map으로 변환 */
+    private static Map<String, List<String>> analyzeFirendsInfo(List<List<String>> friends) {
+        return null;
+    }
+
+    /** 친구의 친구인 경우 친구 가산점을 추가하는 함수*/
+    private static void updateFriendsScore(String user, Map<String, List<String>> friendsMap, Map<String, Integer> scoreInfo) {
+
+    }
+
+    /** 방문자의 방문 가산점을 추가하는 함수 */
+    private static void updatVisitorScore(Map<String, Integer> scoreInfo, List<String> userFriends, List<String> visitors) {
+
+    }
+
+    /** 해당 점수의 유저 List를 반환하는 함수 */
+    private static List<String> getScoreUserList(Map<String, Integer> scoreInfo, int score) {
+        return Collections.emptyList();
+    }
+
+    /** 추천할 친구 List를 반환하는 함수 */
+    private static List<String> extractSuggestionFriends(Map<String, Integer> scoreInfo) {
+        return Collections.emptyList();
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
