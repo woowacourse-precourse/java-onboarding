f755d5ab28098119f35a84c744ede779deef9b9e
bomin1996 <bomin199628@gmail.com>
2022-10-28 19:45:58 +0900

    문제6. 이메일형식에 부합/이메일 길이제한/email.com 도메인제한 기능 구현완료. 문제6번 완료!

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,28 +7,34 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        //List<String> answer = List.of("answer");
 
         HashSet<String> emails = new HashSet<>();
         HashMap<String,String> map = new HashMap<>();
-        for(int i=0;i< forms.size();i++){
-            String name = forms.get(i).get(1);
-            if (name.length()<1 && name.length()>20){
-                break;
-            }//닉네임의 길이가 한자리이상 20자리미만
-            for (int j=0; j<name.length()-1;j++){
-                String key = name.substring(j,j+2);
-                if (map.containsKey(key)){
-                    String email = map.get(key);
-                    if (!email.equals(forms.get(i).get(0))){
-                        emails.add(email);
-                        emails.add(forms.get(i).get(0));
+        for (int k=0;k< forms.size(); k++){
+            String emailcheck = forms.get(k).get(0);
+            if (emailcheck.lastIndexOf("@eamil.com")<10 && emailcheck.lastIndexOf("@email.com")>1){
+                //이메일은 이메일 형식에 부합하며, 전체길이는 11자 이상 20자 미만이다/email.com 도메인제한
+                for(int i=0;i< forms.size();i++){
+                    String name = forms.get(i).get(1);
+                    if (name.length()<1 && name.length()>20){
+                        break;
+                    }//닉네임의 길이가 한자리이상 20자리미만
+                    for (int j=0; j<name.length()-1;j++){
+                        String key = name.substring(j,j+2);
+                        if (map.containsKey(key)){
+                            String email = map.get(key);
+                            if (!email.equals(forms.get(i).get(0))){
+                                emails.add(email);
+                                emails.add(forms.get(i).get(0));
+                            }
+                        }
+                        map.put(key,forms.get(i).get(0));
                     }
                 }
-                map.put(key,forms.get(i).get(0));
             }
         }
-        List<String> collect = emails.stream().sorted().collect(Collectors.toList());//오름차순으로 정렬 밑 중복제거
-        return collect;
+        List<String> answer = emails.stream().sorted().collect(Collectors.toList());//오름차순으로 정렬 밑 중복제거
+        return answer;
     }
 }
