8233089f7bc1db6b2b7011613f61299a904a890a
wonjunYou <youwjune@gmail.com>
2022-10-28 17:04:04 +0900

    feat : (Problem 2) 연속하는 중복 문자를 삭제하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,20 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static final int MAX_LENGTH = 1000;
 
     private static final char FIRST_LETTER = 'a';
     private static final char LAST_LETTER = 'z';
 
+    private static Stack<Character> charStack;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
 
         try {
-            validate(answer);
+            validate(cryptogram);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
-            return "";
+            return answer;
         }
+
+        deleteContinuousAndDuplicatedLetters(cryptogram);
 
         return answer;
     }
@@ -41,5 +47,31 @@ public class Problem2 {
         if (letter < FIRST_LETTER || letter > LAST_LETTER) {
             throw new IllegalArgumentException("소문자가 아닌 다른 값이 포함되어 있습니다.");
         }
+    }
+
+    private static void deleteContinuousAndDuplicatedLetters(String cryptogram) {
+        charStack = new Stack<>();
+        char[] letters = convertStringToCharArray(cryptogram);
+
+        for (char letter : letters) {
+            deleteContinuousAndDuplicatedLetter(letter);
+        }
+    }
+
+    private static char[] convertStringToCharArray(String input) {
+        return input.toCharArray();
+    }
+
+    private static void deleteContinuousAndDuplicatedLetter(char letter) {
+        if (checkStackIsEmptyAndTopEqualToLetter(letter)) {
+            charStack.pop();
+            return;
+        }
+
+        charStack.push(letter);
+    }
+
+    private static boolean checkStackIsEmptyAndTopEqualToLetter(char letter) {
+        return ((!charStack.isEmpty()) && charStack.peek() == letter);
     }
 }
