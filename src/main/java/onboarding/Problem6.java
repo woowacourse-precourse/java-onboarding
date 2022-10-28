0b57fdea29ee2dcab8133809e7223e8f19fb7c22
Lee Sang Bin <rhkd2612@naver.com>
2022-10-28 20:41:10 +0900

    feat: unique result list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,29 +1,28 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /*
  * # 기능 정리
  * [O] 문자열 비교 메서드
  * [O] 결과 리스트 정렬
- * [X] 결과 리스트 중복 제거
- * [X] solution 작성
- * [X] 테스트 확인
+ * [O] 결과 리스트 중복 제거
+ * [O] solution 작성
+ * [O] 테스트 확인
  */
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = findEmailsWithDuplicateNickname(forms);
         Collections.sort(answer);
-
         return answer;
     }
 
     // 전략 -> 각 닉네임을 두글자씩 조회해서 Map에서 이전에 있었는지 조회, 조회된 경우 처음에 추가된 이메일과, 비교된 이메일 결과에 추가
     private static List<String> findEmailsWithDuplicateNickname(final List<List<String>> forms){
-        List<String> resultList = new ArrayList<>();
+        HashSet<String> resultList = new HashSet<>();
         Map<String, String> checkSubstrMap = new HashMap<>();
-        Map<String, Boolean> checkVisitMap = new HashMap<>();
         Map<String, String> firstAddedSubstrOriginEmailMap = new HashMap<>();
 
         for(int i = 0; i < forms.size(); i++){
@@ -37,22 +36,16 @@ public class Problem6 {
 
                     if(checkSubstrMap.get(substr) == null){
                         checkSubstrMap.put(substr, email);
-                        checkVisitMap.put(substr, false);
                         firstAddedSubstrOriginEmailMap.put(substr, email);
                         continue;
                     }
 
-                    checkVisitMap.put(substr, true);
                     resultList.add(email);
+                    resultList.add(firstAddedSubstrOriginEmailMap.get(substr));
                 }
             }
         }
 
-        for (String s : checkVisitMap.keySet()) {
-            if(checkVisitMap.get(s))
-                resultList.add(firstAddedSubstrOriginEmailMap.get(s));
-        }
-
-        return resultList;
+        return new ArrayList<>(resultList);
     }
 }
