79acadd50b5a5efea7df78fb62086d509059244f
khj1 <dhepd8849@gmail.com>
2022-10-28 11:04:27 +0900

    feat(problem2): 연속하는 중복 문자들을 제거하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,41 @@
 package onboarding;
 
 public class Problem2 {
+    public static final String EMPTY_STRING = "";
+    public static final int ONE_LETTER = 1;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decrypt(cryptogram);
+    }
+
+    private static String decrypt(String cryptogram) {
+        if (cryptogram.equals(EMPTY_STRING)) {
+            return cryptogram;
+        }
+        String refinedCryptogram = removeDuplicateCharacters(cryptogram);
+        if (cryptogram.equals(refinedCryptogram)) {
+            return cryptogram;
+        }
+        return decrypt(refinedCryptogram);
+    }
+
+    private static String removeDuplicateCharacters(String cryptogram) {
+        char[] cryptogramChars = cryptogram.toCharArray();
+        String refinedCryptogram = EMPTY_STRING;
+        if (cryptogram.length() == ONE_LETTER) {
+            refinedCryptogram = cryptogram;
+        }
+        if (cryptogram.length() > ONE_LETTER && cryptogramChars[0] != cryptogramChars[1]) {
+            refinedCryptogram += cryptogramChars[0];
+        }
+        for (int i = 1; i < cryptogram.length() - 1; i++) {
+            if (cryptogramChars[i] != cryptogramChars[i + 1] && cryptogramChars[i] != cryptogramChars[i - 1]) {
+                refinedCryptogram += cryptogramChars[i];
+            }
+        }
+        if (cryptogram.length() > ONE_LETTER && cryptogramChars[cryptogram.length() - 1] != cryptogramChars[cryptogram.length() - 2]) {
+            refinedCryptogram += cryptogramChars[cryptogram.length() - 1];
+        }
+        return refinedCryptogram;
     }
 }
