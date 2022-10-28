af99fb524b089c2d50ba4e300c40238e525c39ec
junho lee <junho1273@gmail.com>
2022-10-28 14:56:35 +0900

    Problem 4 기능목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+//기능목록
+//1.단어의 길이 만큼 반복해서 아스키 코드로 대문자와 소문자 그리고 나머지로 구분
+//2.대문자는 A~Z 소문자는 a~z의 아스키코드를 더한 값 각각 대문자 155 소문자 219 각 단어의 아스키 코드를 빼준 값을 추가
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        return answer.toString();
     }
 }
