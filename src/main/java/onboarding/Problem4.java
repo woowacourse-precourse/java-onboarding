801afdb9673672f2aab3c839dcdcbc6fd4efaa4a
Jimin <thswlals0219@gmail.com>
2022-10-28 21:07:30 +0900

    feat: add isAlpha function to Problem3
    알파벳 구분 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static boolean isAlpha(char alpha) {
+        if(alpha >= 'A' && alpha <= 'Z' || alpha >= 'a' && alpha <= 'z'){
+            return true;
+        }
+        return false;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
