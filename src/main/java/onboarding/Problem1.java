3a776726e7e49beb59d46690df6d30a75ce31df7
Joonsoo Kim <kjsoo1901@gmail.com>
2022-10-28 11:13:23 +0900

    feat: Add plusNumber in Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,24 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private int plusNumber(int pageNumber){
+        int result = 0;
+
+        result += pageNumber / 100;
+        pageNumber %= 100;
+        result += pageNumber / 10;
+        pageNumber %= 10;
+        result += pageNumber;
+
+        return result;
+    }
+
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 }
