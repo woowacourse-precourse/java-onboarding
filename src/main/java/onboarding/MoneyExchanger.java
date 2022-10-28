ff00d60f0c6caa184598e37c5882c22e57a5446d
cpot5620 <=cpot5620@naver.com>
2022-10-28 18:45:54 +0900

    Make 'MoneyExchanger' class and implement main logic
    - MoneyExchanger class is responsible for exchanging money for users. It's methods is seperated to bills exchanger and coins exchanger.

--- src/main/java/onboarding/MoneyExchanger.java
+++ src/main/java/onboarding/MoneyExchanger.java
@@ -1,2 +1,42 @@
-package onboarding;public class MoneyExchanger {
+package onboarding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoneyExchanger {
+    private List<Integer> numberOfUnits = new ArrayList<>();
+    private int moneyToExchange;
+
+    public MoneyExchanger(int money) {
+        this.moneyToExchange = money;
+    }
+
+    public List<Integer> toUnits() {
+        toBills();
+        toCoins();
+
+        return numberOfUnits;
+    }
+
+    private void toBills() {
+        int countOfBills;
+        int[] bills = {50000, 10000, 5000, 1000};
+
+        for (int i = 0; i < bills.length; i++) {
+            countOfBills = moneyToExchange / bills[i];
+            numberOfUnits.add(countOfBills);
+            moneyToExchange -= countOfBills * bills[i];
+        }
+    }
+
+    private void toCoins() {
+        int countOfCoins;
+        int[] coins = {500, 100, 50, 10, 1};
+
+        for (int i = 0; i < coins.length; i++) {
+            countOfCoins = moneyToExchange / coins[i];
+            numberOfUnits.add(countOfCoins);
+            moneyToExchange -= countOfCoins * coins[i];
+        }
+    }
 }
