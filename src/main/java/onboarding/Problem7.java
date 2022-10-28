64b29c5be9219f5fe15718e7e442209c2118c6fe
EUNJEONGMUN <munej26@gmail.com>
2022-10-28 11:33:52 +0900

    feat: 친구 목록을 추가하여 반환하는 함수 구현하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> makeFriendList(String people, List<String> friendList) {
+        if (friendList==null) {
+            return List.of(people);
+        }
+        friendList.add(people);
+        return friendList;
     }
 }
