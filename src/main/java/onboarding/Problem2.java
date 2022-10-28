9f479ecaa51191ccf9e56de5397c9c32dea0102c
son <shhmun02@naver.com>
2022-10-28 17:08:31 +0900

    Problem2_req_1

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
-
+//1. 연속하는 중복 문자 찾기
+//2. 연속하는 중복 문자 제거
+//3. 1~2를 연속하는 중복문자가 없을 때 까지 반복
+//4. 연속하는 중복 문자들을 삭제한 결과를 return
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String s = cryptogram;
+        String answer;
+        int sw;
+        while(true)
+        {
+            answer = "";
+            sw = 0;
+            for(int i = 0; i < s.length(); i++)
+            {
+                //1. 연속하는 중복 문자 찾기
+                if(i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1))
+                {
+                    sw = 1;
+                    continue;
+                }
+                else if(i > 0 && s.charAt(i) == s.charAt(i - 1))
+                {
+                    sw = 1;
+                    continue;
+                }
+
+            }
+            if(sw == 0)
+                break;
+        }
         return answer;
     }
 }
