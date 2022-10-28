e18c437dff18c4ea8cde2b95ac5d3ead8a483cb7
SEONMl <sm000117@naver.com>
2022-10-28 19:35:41 +0900

    feat: Problem5 Wallet class 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    static class Wallet {
+        static final int MONEY_MAX_COUNT = 9;
+
+        private List<Integer> money = new ArrayList<>(MONEY_MAX_COUNT);
+        private int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        private int target;
+
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
