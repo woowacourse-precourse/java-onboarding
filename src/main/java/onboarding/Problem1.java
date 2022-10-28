1b0f8743eb5efcddfb4755749d1ad8e7090bb11f
w0ntae <bitcampcoding@gmail.com>
2022-10-28 17:40:56 +0900

    p1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,58 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int pobiMaxValue = 0, crongMaxValue = 0, digitdiff, answer;
+        
+        if(pobi.get(0) - pobi.get(1) != -1){
+            answer = -1;
+            return answer;
+        }
+        
+        if(crong.get(0) - crong.get(1) != -1){
+            answer = -1;
+            return answer;
+        }
+        
+        for (int i : pobi){
+            int addedSum = 0, multipliedProduct = 1;
+            for (int currentDigit; i != 0; i /=10){
+                currentDigit = i % 10;
+                addedSum += currentDigit;
+                multipliedProduct *= currentDigit;
+            }
+            if (multipliedProduct >= addedSum){
+                pobiMaxValue = multipliedProduct;
+            }
+            else {
+                pobiMaxValue = addedSum;
+            }
+        }
+    
+        for (int i : crong){
+            int addedSum = 0, multipliedProduct = 1;
+            for (int currentDigit; i != 0; i /=10){
+                currentDigit = i % 10;
+                addedSum += currentDigit;
+                multipliedProduct *= currentDigit;
+            }
+            if (multipliedProduct >= addedSum){
+                crongMaxValue = multipliedProduct;
+            }
+            else {
+                crongMaxValue = addedSum;
+            }
+        }
+        
+        if (crongMaxValue > pobiMaxValue){
+            answer = 2;
+        }
+        if (crongMaxValue == pobiMaxValue){
+            answer = 0;
+        }
+        else {
+            answer = 1;
+        }
+        
         return answer;
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,13 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        
+        for(int i = 0; i < cryptogram.length() ; i++){
+            
+        }
+        
+        
+        
         String answer = "answer";
         return answer;
     }
