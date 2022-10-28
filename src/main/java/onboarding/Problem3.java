1232bd02bd1532cef3f63a51980884a0e75d0d79
Hojinida <opqrs1103@gmail.com>
2022-10-29 03:26:41 +0900

    feat: 문제 3번 정수를 입력하면 그 정수에 3, 6, 9의 개수를 세어 반환해 주는 메서드 calculate 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,21 @@ import java.util.stream.Stream;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer=0;
+        for(int i=1;i<=number;i++){
+            answer+=calculate(i);
+        }
         return answer;
+    }
+
+    public static int calculate(int num){
+        int result=0;
+        int[] nums=converter(num);
+        for(int n:nums){
+            if(check369(n))
+                result++;
+        }
+        return result;
     }
 
     public static int[] converter(int num){
