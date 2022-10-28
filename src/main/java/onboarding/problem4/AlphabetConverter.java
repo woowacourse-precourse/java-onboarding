3cf5642e9522467fbbd7d8c36e2734bcfef0daa8
situodys <situodys@gmail.com>
2022-10-28 13:56:19 +0900

    feat(문제4): 알파벳 소문자인 경우 변환

--- src/main/java/onboarding/problem4/AlphabetConverter.java
+++ src/main/java/onboarding/problem4/AlphabetConverter.java
@@ -1,6 +1,8 @@
 package onboarding.problem4;
 
 public class AlphabetConverter {
+    private static final char MIN_LOWER_CASE = 'a';
+    private static final char MAX_LOWER_CASE = 'z';
 
     private String targetWord;
     private StringBuilder convertedWord;
@@ -10,4 +12,17 @@ public class AlphabetConverter {
         convertedWord = new StringBuilder();
     }
 
+    public String convert() {
+        for (Character charInWord : targetWord.toCharArray()) {
+            if (Character.isLowerCase(charInWord)) {
+                convertLowerCase(charInWord);
+                continue;
+            }
+        }
+        return convertedWord.toString();
+    }
+
+    public void convertLowerCase(char currentChar) {
+        convertedWord.append((char) (MAX_LOWER_CASE - currentChar + MIN_LOWER_CASE));
+    }
 }
