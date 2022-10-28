702058b96e16d228ec2feedb8828af2e4ec1c7dc
ymkim97 <rladudaud97@naver.com>
2022-10-28 15:39:07 +0900

    feat: add function boundary exception

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        if (throwBoundaryException(cryptogram)) {
+            return "failed with boundary exception";
+        }
+
+        return cryptogram;
     }
+
+    public static Boolean throwBoundaryException(String cryptogram) {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            return true;
+        }
+        return false;
+    }
+
 }
