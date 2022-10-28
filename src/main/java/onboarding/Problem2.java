00509e05201b6900b085cf9f07e4ff2e89b9cb8b
chj <bluedog129@gmail.com>
2022-10-28 12:39:06 +0900

    P2 UpperCaseError, outOfStringRangeError 예외 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
+    private static int checkCryptogram(String str) {
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return 0;
+            }
+        }
+        if (str.length() < 1 || str.length() > 1000) {
+            return -1;
+        }
+        return 1;
+    }
     private static String reverseString(String str) {
         String result = "";
         for (int i = (str.length() - 1); i >= 0; i--) {
@@ -11,6 +22,12 @@ public class Problem2 {
         return result;
     }
     public static String solution(String cryptogram) {
+        if (checkCryptogram(cryptogram) == 0) {
+            return "UpperCaseError";
+        } else if (checkCryptogram(cryptogram) == -1) {
+            return "outOfStringRangeError";
+        }
+
         String result = "";
         Stack<Character> stack = new Stack<>();
         for (char c : cryptogram.toCharArray()) {
