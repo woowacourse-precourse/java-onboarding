209813ad7d6d5d58b55ad69c43153cb4e787c71e
squareyun <squareyun@gmail.com>
2022-10-28 11:41:46 +0900

    feat: 청개구리 사전 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        Map<Character, Character> frogDictionary = new HashMap<>();
         String answer = "";
+
+        for (int i = 0; i < 26; i++) {
+            Character alpha = (char) ('A' + i);
+            frogDictionary.put(alpha, (char)('Z' - i));
+        }
+
         return answer;
     }
 }
