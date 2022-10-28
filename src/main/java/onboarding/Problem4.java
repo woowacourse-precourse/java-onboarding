c4766c287b37874503bc2d03ef9f47ebb7353750
limzzum <chfhddl1234@naver.com>
2022-10-28 17:04:32 +0900

    [문제 4] 청개구리 사전 wordMap 생성.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,9 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        char [] wordMap = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N',	'M', 'L', 'K',
+                'J','I','H','G','F','E','D','C', 'B', 'A'};
+
         String answer = "";
         return answer;
     }
