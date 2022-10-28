e5b3e093e341cc3c93e6a0958dc2a0837c487f0a
Choi-JJunho <junho5336@gmail.com>
2022-10-28 22:09:19 +0900

    feat(문제3): 주어진 숫자에 포함된 3,6,9의 개수를 구하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += getClapCount(i);
+        }
+
         return answer;
+    }
+
+    /**
+     * 숫자에 포함된 3,6,9의 개수를 구한다.
+     * @param number 숫자
+     * @return 숫자에 포함된 3,6,9의 개수
+     */
+    private static int getClapCount(int number) {
+        String str = String.valueOf(number);
+        return str.replaceAll("[^3|6|9]", "").length();
     }
 }
