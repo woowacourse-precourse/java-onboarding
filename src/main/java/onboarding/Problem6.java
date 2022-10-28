e8ae923594a33124d9d745aad6a9521508057e1c
devmizz <garamminchan@gmail.com>
2022-10-28 15:57:15 +0900

    problem6: 멤버 클래스 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,33 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    class Member {
+        private final String email;
+        private final String nickname;
+
+        public Member(String email, String nickname) {
+            isValidEmail(email);
+            isValidNickname(nickname);
+            this.email = email;
+            this.nickname = nickname;
+        }
+
+        private void isValidEmail(String email) {
+            int length = email.length();
+            if(11 <= length && length <= 20) {
+                throw new IllegalArgumentException("이메일의 길이가 적절하지 않습니다.");
+            }
+            if(!email.endsWith("@email.com")) {
+                throw new IllegalArgumentException("이메일의 형식이 적합하지 않습니다.");
+            }
+        }
+
+        private void isValidNickname(String nickname) {
+            int length = nickname.length();
+            if(length < 1 || 20 < length) {
+                throw new IllegalArgumentException("닉네임의 길이가 적절하지 않습니다.");
+            }
+        }
+    }
 }
