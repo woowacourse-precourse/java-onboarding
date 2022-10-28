e1d55acd63452054568d3fb4437ae2c59b1dd6b5
ykmxxi <ykmxxi97@gmail.com>
2022-10-28 23:35:59 +0900

    feat: (Problem4)문자가 대문자인지 확인하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+    private static int UPPER_CASE_A = 65;
+    private static int UPPER_CASE_Z = 90;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
@@ -8,5 +11,12 @@ public class Problem4 {
 
     private static StringBuilder toStringBuilder(String word) {
         return new StringBuilder(word);
+    }
+
+    private static boolean isUpperCase(char character) {
+        if (character >= UPPER_CASE_A && character <= UPPER_CASE_Z) {
+            return true;
+        }
+        return false;
     }
 }
