0d314c85eaea3ad31f79faeb23b3d4e2e8ec65f0
Seph-23 <seongmlyu95@gmail.com>
2022-10-28 12:43:04 +0900

    5번 문제 리팩터링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,20 +7,18 @@ import java.util.List;
 
 public class Problem5 {
 
-//    public static void main(String[] args) {
-//        int money = 50237;
-//        System.out.println("solution(money) = " + solution(money));
-//    }
+    public static void main(String[] args) {
+        int money = 15000;
+        System.out.println("solution(money) = " + solution(money));
+    }
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        int[] temp = new int[moneyType.length];
 
         for (int i = 0; i < moneyType.length; i++) {
-            temp[i] += money/moneyType[i];
+            answer.add(money/moneyType[i]);
             money %= moneyType[i];
-            answer.add(temp[i]);
         }
         return answer;
     }
