f089a891f2ee5f838dcecf4cec9335fd3e80af28
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-10-28 23:31:53 +0900

    Problem4 기능 목록 2 : 주어진 문자열을 순회하면서 사전에서 알맞은 문자로 변환 후, 새로운 배열에 저장한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem4 {
@@ -13,6 +14,30 @@ public class Problem4 {
         List<Character> upperDictionary2 = List.of('Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N');
         List<Character> lowerDictionary2 = List.of('z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n');
 
+        char[] characterArray = word.toCharArray();
+        List<Character> characterList = new LinkedList<>();
+
+        for (int i = 0; i < characterArray.length; i++) {
+
+            if (upperDictionary.contains(characterArray[i])) {
+                characterList.add(upperDictionary2.get(upperDictionary.indexOf(characterArray[i])));
+            }
+            else if (upperDictionary2.contains((characterArray[i]))) {
+                characterList.add(upperDictionary.get(upperDictionary2.indexOf(characterArray[i])));
+            }
+            else if (lowerDictionary.contains(characterArray[i])) {
+                characterList.add(lowerDictionary2.get(lowerDictionary.indexOf(characterArray[i])));
+            }
+            else if (lowerDictionary2.contains((characterArray[i]))) {
+                characterList.add(lowerDictionary.get(lowerDictionary2.indexOf((characterArray[i]))));
+            }
+            else if (characterArray[i] == ' ') {
+                characterList.add(' ');
+            } else {
+                characterList.add(characterArray[i]);
+            }
+
+        }
 
         return answer;
     }
