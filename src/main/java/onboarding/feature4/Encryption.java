86f5302bcce7a1866ec73719f9b83958b6bfc047
areyouhun <areyouhun@gmail.com>
2022-10-29 02:30:51 +0900

    feat(prob4): encrypt given letters

---
+++ src/main/java/onboarding/feature4/Encryption.java
@@ -0,0 +1,30 @@
+package onboarding.feature4;
+
+import static onboarding.feature4.Constants.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import onboarding.feature4.AlphabetValidator;
+
+
+public class Encryption {
+    public static List<Character> encryptLetters(List<Character> givenLetters) {
+        List<Character> lettersEncrypted = new ArrayList<>();
+        for (Character letter : givenLetters) {
+            if (AlphabetValidator.isUppercase(letter)) {
+                shiftLetter(UPPER_CASE, letter);
+            }
+            if (AlphabetValidator.isLowercase(letter)) {
+                shiftLetter(LOWER_CASE, letter);
+            }
+            lettersEncrypted.add(letter);
+        }
+        return lettersEncrypted;
+    }
+
+    public static void shiftLetter(List<Character> letterCase, Character letter) {
+        int indexOfLetter = letterCase.indexOf(letter);
+        letter  = letterCase.get(25 - indexOfLetter);
+    }
+}
