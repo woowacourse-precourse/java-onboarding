982bea09f9de1c962319ebc9d8f0c3941a448e72
hyun-seung <ardimento22@naver.com>
2022-10-28 13:19:44 +0900

    [FEAT] friends에 있는 전체 사람의 수를 구한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        HashMap<String, Integer> friendList = getTotalUser(friends);
         return answer;
+    }
+
+    public static HashMap<String, Integer> getTotalUser(List<List<String>> friends) {
+        HashMap<String, Integer> userList = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            userList.put(friend.get(0), 0);
+            userList.put(friend.get(1), 0);
+        }
+
+        return userList;
     }
 }
