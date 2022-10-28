fad69941433be2f34c232fac9acca7b5aa5144c1
win9 <krrr8@naver.com>
2022-10-28 22:15:04 +0900

    [기능1번] 페이지 valid 확인 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,25 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        if (isValid(pobi, crong)){
+            return -1;
+        }
+
+
+
+        return 0;
     }
+
+
+    /**
+     * 페이지의 범위가 옳바른지 체크
+     * @param pobi
+     * @param crong
+     * @return
+     */
+    private static boolean isValid(List<Integer> pobi, List<Integer> crong) {
+        return pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1);
+    }
+
 }
