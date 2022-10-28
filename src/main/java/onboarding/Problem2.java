dbd7f59780da6d77fc335ad3d8d3b67d6711aac0
dydtmd6801 <dydtmd6801@naver.com>
2022-10-28 17:32:08 +0900

    fix : 반복문 무한반복 버그 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,19 +3,19 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        int overlap = isOverlap(cryptogram);
+        answer = isOverlap(cryptogram);
         return answer;
     }
 
-    private static int isOverlap(String secretMessage){
-        String[] arrayString = secretMessage.split("");
-        for(int i = 0; i < arrayString.length - 1; i++){
+    private static String isOverlap(String secretMessage){
+        for(int i = 0; i < secretMessage.length() - 1; i++){
+            String[] arrayString = secretMessage.split("");
             if(arrayString[i].equals(arrayString[i+1])){
                 secretMessage = overlapDelete(secretMessage, i);
                 i = -1;
             }
         }
-        return 0;
+        return secretMessage;
     }
 
     private static String overlapDelete(String deleteMessage, int idx){
