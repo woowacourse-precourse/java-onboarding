b4ca72772b7d041c90ab1fee68eb228994934a76
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-10-28 11:46:27 +0900

    Solve4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] chars = word.toCharArray();
+
+        for(int i=0; i<chars.length; i++) {
+            chars[i] = momToChild(chars[i]);
+        }
+
+        return new String(chars);
+    }
+
+    private static char momToChild(char word) {
+        if(Character.isWhitespace(word)) return word;
+
+        char childWord;
+        if(Character.isUpperCase(word)) childWord = (char) ('Z' - (word - 'A'));
+        else childWord = (char) ('z' - (word - 'a'));
+        return childWord;
     }
 }
