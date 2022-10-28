f5905a44433bdf08e0231155992f9d942ad0f154
wonjunYou <youwjune@gmail.com>
2022-10-29 00:21:07 +0900

    feat : (Problem 3) number 입력값을 검증하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    public static final int MAX_NUMBER = 10000;
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        try {
+            validateRange(number);
+        } catch (IllegalArgumentException e) {
+            return 0;
+        }
+
+        return 0;
+    }
+
+    public static void validateRange(int number) {
+        if (number < 1 || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("유효하지 않은 범위 내의 숫자입니다.");
+        }
     }
 }
