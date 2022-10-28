cc661af015435a48f2f10a40debbd586caa91273
Namyunsuk <yunsuk0328@naver.com>
2022-10-28 23:24:51 +0900

    feat(calculateMoney): 액수에 대한 연산 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -15,6 +15,18 @@ class validateMoney{
         return (money>=1)&&(money<=1_000_000);
     }
 }
+class calculateMoney{
+    private static int divideMoney;
+    public static void setDivideMoney(int money){
+        divideMoney = money;
+    }
+    public static int divide(int money){
+        return  money/divideMoney;
+    }
+    public static int remainder(int money){
+        return money%divideMoney;
+    }
+}
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
