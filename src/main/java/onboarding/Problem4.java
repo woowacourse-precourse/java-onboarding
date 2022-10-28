2e5a3404cf1e028c19d2d19e0006d2b5b8a0f0f2
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-28 21:19:31 +0900

    feat(problem3): 입력값을 검증한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
+    public static String solution(String word) throws Exception {
+        validate(word);
         String answer = "";
         return answer;
+    }
+
+    private static void validate(String word) throws Exception {
+        if (word.length() < 1 || word.length() > 1000) {
+            throw new Exception("입력값의 길이가 잘못되었습니다");
+        }
     }
 }
