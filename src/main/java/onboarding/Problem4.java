9ddd8643a3e2ebd20a270dd14fb041ab2850ff3c
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-28 10:02:48 +0900

    comment: add function list of problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ * Problem 4 function list
+ * - change string 
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
