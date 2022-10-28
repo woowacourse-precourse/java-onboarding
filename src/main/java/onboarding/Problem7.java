bcc60c0d6a034f187bc0302bda64442efd10c412
Dongmin Kim <kimdongmin68@naver.com>
2022-10-28 21:33:22 +0900

    feat: Add get already friend method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,16 +1,15 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> alreadyFriends;
         Map<String, Integer> recommendFriend = new HashMap<>();
 
-        getFriendsScore(user, friends, recommendFriend);
+        alreadyFriends = getFriend(user, friends);
+        getFriendScore(user, alreadyFriends, friends, recommendFriend);
 
         return answer;
     }
@@ -21,12 +20,27 @@ public class Problem7 {
         }
     }
 
-    public static void getFriendsScore(String user, List<List<String>> friends , Map<String,Integer> recommendFriend){
+    public static void getFriendScore(String user, List<String> alreadyFriends, List<List<String>> friends , Map<String,Integer> recommendFriend){
+        for(String alreadyFriend : alreadyFriends) {
+            for (List<String> friend : friends) {
+                String firstHuman = friend.get(0);
+                String secondHuman = friend.get(1);
+                if(!(user.equals(firstHuman)||user.equals(secondHuman))) {
+                    if (alreadyFriend.equals(firstHuman)) recommendFriend.put(secondHuman,10);
+                    if (alreadyFriend.equals(secondHuman)) recommendFriend.put(firstHuman,10);
+                }
+            }
+        }
+    }
+
+    public static List<String> getFriend(String user, List<List<String>> friends){
+        List<String> alreadyFriend = new ArrayList<>();
         for(List<String> friend : friends){
             String firstHuman = friend.get(0);
             String secondHuman = friend.get(1);
-            if(user.equals(firstHuman)) recommendFriend.put(secondHuman,10);
-            if(user.equals(secondHuman)) recommendFriend.put(firstHuman,10);
+            if(user.equals(firstHuman)) alreadyFriend.add(secondHuman);
+            if(user.equals(secondHuman)) alreadyFriend.add(firstHuman);
         }
+        return alreadyFriend;
     }
 }
