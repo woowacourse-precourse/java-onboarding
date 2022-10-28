03ad43547522a206f0564688d66a3f377488e796
sinryuji <siler2474@gmail.com>
2022-10-28 16:10:50 +0900

    Feat: Problem2 add checkDup method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
-
+/*
+    기능 목록
+    1. 연속된 문자가 있는지 체크하는 기능 -> checkDup
+    2. 연속된 문자를 자르는 기능
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
+    }
+
+    private static int checkDup(String cryptogram, int i) {
+        int ret = 0;
+
+        while(cryptogram.charAt(i) == cryptogram.charAt(i + ret + 1))
+            ret++;
+        return ret;
     }
 }
