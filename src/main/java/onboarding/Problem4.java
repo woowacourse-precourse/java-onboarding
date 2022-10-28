00d32c65c62f0c78dd0d9dbdf8ac10944bac443a
KimJaeSung <behappy0916@naver.com>
2022-10-28 20:23:18 +0900

    feat java-onboarding 대문자 소문자 사전 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,11 +3,23 @@ package onboarding;
 import java.util.HashMap;
 
 public class Problem4 {
+    static void createLowerCaseDictionary(HashMap dictionary){
+        int n = 25;
+        for(int i=97; i<123; i++,n-=2){
+            dictionary.put((char)i,(char)(i+n));
+        }
+    }
+    static void createUpperCaseDictionary(HashMap dictionary){
+        int n = 25;
+        for(int i=65; i<91; i++,n-=2){
+            dictionary.put((char)i,(char)(i+n));
+        }
+    }
 
     public static String solution(String word) {
         String answer = "";
-
-        HashMap<String,Character> dictionary = new HashMap<String, Character>();
+        HashMap<String,Character> upperDictionary = new HashMap<String, Character>();
+        HashMap<String,Character> lowerDictionary = new HashMap<String, Character>();
 
         return answer;
     }
