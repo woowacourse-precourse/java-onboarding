4e069cb2dc32014ebde2a62e46cd08ea293d32b1
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2022-10-29 01:09:34 +0900

    [문제3] 각 자리 숫자 파악하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    public static int[] divide=new int[] {0,0,0,0,0};
+    public static int[] division=new int[] {10000,1000,100,10,10};
+
+
+    public static void dividenumber(int number){
+        for(int i=0;i<4;i++){
+            divide[i]=number/division[i];
+        }
+        divide[4]=number%division[4];
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
