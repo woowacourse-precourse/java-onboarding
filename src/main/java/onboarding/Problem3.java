84d2f3fcfaf54c396182fa8bef26efe06fdadd6d
Jueun <jueun000101@gmail.com>
2022-10-28 16:17:15 +0900

    docs: 3번 기능 목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 주어진 숫자는 몇 번의 손뼉을 쳐야하는지 계산하는 함수
+ * 2. 1~number까지 1번 함수를 호출하고 리턴값을 모두 더해 반환
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
