51dfed23dc7f8a06de0e7857f403a2397b0712a5
LHS-11 <ghktjq1119@naver.com>
2022-10-28 16:27:50 +0900

    Problem7 : 1-4 예외 체크 (ID 길이 체크)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -15,23 +15,24 @@ public class Problem7 {
         int friendsSize = friends.size();
 
         //1. 예외사항
-        //- user 길이 1 이상 30 이하인지 체크
+        //1-1 user 길이 1 이상 30 이하인지 체크
         if(isNotMatchingUserLength(userLength)){
             throw new NotMatchingUserLength("사용자의 길이가 맞지 않습니다.");
         }
 
-        //- friends 길이 1 이상 10000 이하인지 체크
+        //1-2 friends 길이 1 이상 10000 이하인지 체크
         if(isNotMatchingFriendsSize(friendsSize)){
             throw new NotMatchingFriendsSize("친구 관계 정보의 길이가 맞지 않습니다.");
         }
 
-        //- friends 의 각 원소의 길이가 2인지 체크
+        //1-3 friends 의 각 원소의 길이가 2인지 체크
         int friendSize = friends.get(0).size();
         if(isNotMatchingFriendSize(friendSize)){
             throw new NotMatchingFriendSize("친구 관계 정보의 원소의 길이가 맞지 않습니다.");
         }
 
-        //- 아이디 길이 1 이상 30 이하인지 체크g
+
+
         //- visitors 길이 0 이상 10000 이하인지 체크
         //- 사용자 아이디 소문자인지 체크
 
@@ -39,15 +40,20 @@ public class Problem7 {
         return answer;
     }
 
+    //1-4 아이디 길이 1 이상 30 이하인지 체크g
+    private static boolean isNotMatchingId(int idLength) {
+        return idLength < 1 || idLength > 30;
+    }
+
     private static boolean isNotMatchingFriendSize(int friendSize) {
         return friendSize != 2;
     }
 
     private static boolean isNotMatchingFriendsSize(int size) {
-        return size<1 || size>10000;
+        return isNotMatchingId(size, 10000);
     }
 
     private static boolean isNotMatchingUserLength(int length) {
-        return length < 1 || length > 30;
+        return isNotMatchingId(length, 30);
     }
 }
