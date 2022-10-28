dfda6172aad70ff5732df9c159323643f3b8bae2
nswon <swnam0729@gmail.com>
2022-10-28 20:09:44 +0900

    feat(Problem3) <checkRole>: 숫자가 3, 6, 9에 걸리면 더하는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+    private static final List<Integer> checkNumbers = Arrays.asList(3, 6, 9);
+    private static int count = 0;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static void checkRole(int number) {
+        if (checkNumbers.contains(number)) {
+            count += 1;
+        }
     }
 }
