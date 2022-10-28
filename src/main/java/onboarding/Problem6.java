bc3c9b31bd5af860427d529033d4313340abc8cf
JJongmen <pjy90123@gmail.com>
2022-10-28 23:02:09 +0900

    feat(Problem6): 중복된 크루의 이메일을 구하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -13,7 +10,26 @@ public class Problem6 {
             List<String> subNicknames = splitNickname(nickname);
             saveSubNicknames(subNicknameMap, order, subNicknames);
         }
+        Set<Integer> duplicateCrews = getDuplicateCrews(subNicknameMap);
+        List<String> duplicateEmails = getDuplicateEmails(forms, duplicateCrews);
         return Collections.emptyList();
+    }
+
+    private static List<String> getDuplicateEmails(List<List<String>> forms, Set<Integer> duplicateCrews) {
+        List<String> duplicateEmails = new ArrayList<>();
+        for (int order : duplicateCrews) {
+            duplicateEmails.add(forms.get(order).get(0));
+        }
+        return duplicateEmails;
+    }
+
+    private static Set<Integer> getDuplicateCrews(HashMap<String, List<Integer>> subNicknameMap) {
+        Set<Integer> duplicateCrews = new HashSet<>();
+        for (List<Integer> orders : subNicknameMap.values()) {
+            if (orders.size() < 2) continue;
+            duplicateCrews.addAll(orders);
+        }
+        return duplicateCrews;
     }
 
     private static void saveSubNicknames(HashMap<String, List<Integer>> subNicknameMap, int order, List<String> subNicknames) {
