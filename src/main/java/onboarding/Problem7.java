2936c367870423fcf9914cd65844357c1a0ee859
Eunkeee <eunkeeeee@gmail.com>
2022-10-28 21:10:13 +0900

    ✨[P7] feat: user의 mutual friend 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,28 +1,46 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-        // user의 friend 구하기
-        ArrayList<String> userFriends = new ArrayList<String>();
+        // user의 direct friend 구하기
+        ArrayList<String> directFriends = new ArrayList<String>();
         for(List<String> friendList : friends){
             if(friendList.contains(user)){
                 // add friend
                 for(String friendName: friendList){
                     if(friendName != user){
-                        userFriends.add(friendName);
+                        directFriends.add(friendName);
                     }
                 }
             }
         }
-        System.out.println(userFriends);
+
+        // user의 mutual friend 구하기
+        HashSet<String> mutualFriends = new HashSet<>();
+        for(String directFriend: directFriends){
+            for(List<String> friendList: friends){
+                for(String friendName: friendList){
+                    if(friendName != directFriend && friendName != user){
+                        mutualFriends.add(friendName);
+                    }
+                }
+            }
+        }
+        mutualFriends.removeAll(directFriends);
+        System.out.println(mutualFriends);
+
+
+
+
 
         return answer;
     }
+
 }
