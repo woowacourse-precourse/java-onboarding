f690d556266be747cb8ec7ab793f53e9a90b9990
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-28 19:36:39 +0900

    feat(problem6): 닉네임 유사도 검사

--- src/main/java/onboarding/problem6/NicknameChecker.java
+++ src/main/java/onboarding/problem6/NicknameChecker.java
@@ -1,15 +1,28 @@
 package onboarding.problem6;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class NicknameChecker {
 
     private List<Crew> crews;
+    private TreeSet<String> similarUserEmails;
 
     public NicknameChecker(){
         crews = new LinkedList<>();
+        similarUserEmails = new TreeSet<>();
     }
+
+    public NicknameChecker checkSimilarNickname(){
+        List<Crew> checkedCrews = new ArrayList<>();
+        for(Crew crew : crews){
+            if(hasSimilarNickname(crew, checkedCrews)){
+                similarUserEmails.add(crew.getEmail());
+            }
+            checkedCrews.add(crew);
+        }
+        return this;
+    }
+
 
     public void addCrew(List<String> crewInfo){
         crews.add(new Crew(crewInfo.get(0), crewInfo.get(1)));
@@ -19,6 +32,15 @@ public class NicknameChecker {
         for(List<String> crewInfo : crewsInfo){
             addCrew(crewInfo);
         }
+    }
+
+    private boolean hasSimilarNickname(Crew crew, List<Crew> targetCrews) {
+        for(Crew targetCrew : targetCrews){
+            if(isSimilar(crew.getNickname(), targetCrew.getNickname())){
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean isSimilar(String nickname1, String nickname2){
