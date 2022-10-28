a90db5e9438317f77105d4b38a9e804240055ffa
ma-an-jong <alswhd1113@gmail.com>
2022-10-28 19:09:56 +0900

    problem5 passed test

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
+    static final int[] arr = {50_000,10_000,5_000,1_000,500,100,50,10,1};
+    private static List<Integer> changeMoney(int money)
+    {
+        List<Integer> changedCount = new LinkedList<>();
+        for(int i = 0 ; i < arr.length;i++)
+        {
+            changedCount.add(money/arr[i]);
+            money %= arr[i];
+        }
+        return changedCount;
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        return changeMoney(money);
     }
 }
