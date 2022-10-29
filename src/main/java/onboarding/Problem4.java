e1cd79299e02c4ae1991ccce33142c41d1ab6586
HaODay <ycis5013@naver.com>
2022-10-29 16:42:05 +0900

    [Problem4] word를 문자 단위로 쪼개기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,8 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] arr= word.toCharArray();
+
         return answer;
     }
 }
