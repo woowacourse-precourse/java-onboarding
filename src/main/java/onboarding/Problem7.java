f835a61423aa2b9a70a9d4146c0a03789a27d019
soyunim <togksk123@naver.com>
2022-10-30 02:51:36 +0900

    Problem7 1)find user's friend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> myFriend = new ArrayList<>();
+        List<List<String>> theirFriend = new ArrayList<>();
+        List<String> listName = new ArrayList<>();
+        List<Integer> listScore = new ArrayList<>();
+
+        // Who is my friend?
+        for(int i=0; i<friends.size(); i++){
+            if(friends.get(i).get(0).equals(user)){
+                myFriend.add(friends.get(i).get(1));
+                listName.add(friends.get(i).get(1));
+                listScore.add(0);
+            }
+            else if(friends.get(i).get(1).equals(user)){
+                myFriend.add(friends.get(i).get(0));
+                listName.add(friends.get(i).get(0));
+                listScore.add(0);
+            }
+            else{
+                theirFriend.add(friends.get(i));
+            }
+        }
+
+        return listName;
     }
 }
