3f365a20c1334ee6a077ad350a6757dab64fbe0d
jw0293 <k38836919@gmail.com>
2022-10-29 15:05:49 +0900

    feature : Integer To StringArray [정수를 문자열 배열로 반환하는 기능]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+class IntegerToStringArray{
+    public String[] toStringArray(int number){
+        return Integer.toString(number).split("");
+    }
+}
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
