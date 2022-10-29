6c27cda4f63b1746f8c7b99d0834c04bb58e1de6
opuopuo <pureuml.dev@gmail.com>
2022-10-29 18:13:23 +0900

    feat: 예외사항 처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        if (!verifyException(number)) {
+            return -1;
+        }
+
         return answer;
+    }
+
+    public static boolean verifyException(int number) {
+        return (number >= 1 && number <= 10000);
     }
 }
