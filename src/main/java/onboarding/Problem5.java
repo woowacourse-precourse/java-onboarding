17fa4520bf44fe101dc74124a386278df1323b04
sohyundoh <lyny123@naver.com>
2022-10-29 20:47:40 +0900

    problem 5 제한사항 및 기능 목록 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,7 +2,15 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+/*
+제한사항
+money는 1 이상 1,000,000 이하인 자연수이다.
 
+기능 목록
+1. answer 가변 객체로 변경
+2. list에 각각의 지폐로 지불 할 몫 추가
+3. money 값 변경
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
