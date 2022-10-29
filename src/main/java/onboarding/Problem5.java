8453fe7f3600f638fa2b23fae2d8254901afbd40
Dabeen Jeong <hello70825@gmail.com>
2022-10-29 22:51:57 +0900

    refactor: [Problem5] 환전할 돈을 배열에 저장해서 사용하다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -25,52 +25,17 @@ public class Problem5 {
     }
 
     private static List<Integer> convertMoney(int money) {
+        int[] exchangeMoneyArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         int nowMoney = money;
         int getValue;
         List<Integer> result = new ArrayList<>();
 
-        // 50,000원 지폐로 환전하다.
-        getValue = getExchange(nowMoney, 50000);
-        result.add(getValue);
-        nowMoney -= 50000 * getValue;
-
-        // 10,000원 지폐로 환전하다.
-        getValue = getExchange(nowMoney, 10000);
-        result.add(getValue);
-        nowMoney -= 10000 * getValue;
-
-        // 5,000원 지폐로 환전하다.
-        getValue = getExchange(nowMoney, 5000);
-        result.add(getValue);
-        nowMoney -= 5000 * getValue;
-
-        // 1,000원 지폐로 환전하다.
-        getValue = getExchange(nowMoney, 1000);
-        result.add(getValue);
-        nowMoney -= 1000 * getValue;
-
-        // 500원 동전으로 환전하다.
-        getValue = getExchange(nowMoney, 500);
-        result.add(getValue);
-        nowMoney -= 500 * getValue;
-
-        // 100원 동전으로 환전하다.
-        getValue = getExchange(nowMoney, 100);
-        result.add(getValue);
-        nowMoney -= 100 * getValue;
-
-        // 50원 동전으로 환전하다.
-        getValue = getExchange(nowMoney, 50);
-        result.add(getValue);
-        nowMoney -= 50 * getValue;
-
-        // 10원 동전으로 환전하다.
-        getValue = getExchange(nowMoney, 10);
-        result.add(getValue);
-        nowMoney -= 10 * getValue;
-
-        // 1원 동전으로 환전하다.
-        result.add(nowMoney);
+        // 돈을 내림차순으로 환전하다.
+        for(int exchangeMoney : exchangeMoneyArray) {
+            getValue = getExchange(nowMoney, exchangeMoney);
+            result.add(getValue);
+            nowMoney -= exchangeMoney * getValue;
+        }
 
         return result;
     }
