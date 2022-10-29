602b4e4ae60fff8e9ccc3f37307ddfc9c85e23ef
beombu <sunil1369@bible.ac.kr>
2022-10-29 23:24:13 +0900

    Feat(P3) : 조정 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clapCnt = 0;
+
+        for(int i = 1; i <= number; i++){
+            if(includeNum(i)){
+                clapCnt += cntNum(i);
+            }
+        }
+
+        return clapCnt;
     }
 
     /**
@@ -29,6 +36,7 @@ public class Problem3 {
         while(number > 0){
             if(number % 10 == 3 || number % 10 == 6 || number % 10 == 9){
                 cnt++;
+                number /= 10;
             }else{
                 number /= 10;
             }
