61bb6108481e1b021cec6575d5348261c09f2330
Invidam <hansupark0208@gmail.com>
2022-10-29 23:57:13 +0900

    refactor(Problem7): change parameter approach in fill user list
    extract relationships because it needs in variety.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,19 +12,18 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
 
         Map<String, Integer> userList = fillUserList(user, friends, visitors);
-
-        List<String> userFriends = fillUserFriends(user, friends);
+        Map<String, List<String>> relationships = makeRelationShips(friends);
+        List<String> userFriends = fillUserFriends(user, relationships);
 
         return answer;
     }
 
-    public static List<String> fillUserFriends(String user, List<List<String>> friends) {
-        Map<String, List<String>> relationships = makeRelationShips(friends);
+    public static List<String> fillUserFriends(String user,Map<String, List<String>> relationships) {
         List<String> userFriend = relationships.get(user);
         return userFriend;
     }
 
-    private static Map<String, List<String>> makeRelationShips(List<List<String>> friends) {
+    public static Map<String, List<String>> makeRelationShips(List<List<String>> friends) {
         Map<String, List<String>> relationships = new HashMap<>();
         for (List<String> friendRelation : friends) {
             enrichRelation(relationships, friendRelation.get(0), friendRelation.get(1));
