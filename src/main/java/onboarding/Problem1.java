4198f4a671c349c3f4f617aa493deca43491a6c9
seungmin-park <tmddudals369@naver.com>
2022-10-30 02:45:18 +0900

    1번 문제 예외사항 처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,16 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(!validData(pobi, crong)) return -1;
+
         return answer;
+    }
+
+    private static boolean validData(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(0) < 1 || crong.get(0) < 1 || pobi.get(1) > 400 || crong.get(1) > 400) return false;
+        if (pobi.get(0) %2 != 1 || crong.get(0) %2 != 1 || pobi.get(1) %2 != 0 || crong.get(1) %2 != 0) return false;
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) return false;
+        return true;
     }
 }
