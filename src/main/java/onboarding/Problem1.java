4d28ce81a5d67bc3ce3be9e7be6ad62a2f57c2bc
QLoooad <ckcc0813@gmail.com>
2022-10-29 19:57:18 +0900

    홀짝 점수 다시 만듬

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -18,58 +18,87 @@ class Problem1 {
         }while(Page <= 1 || Page >= 400); //조건 충족 시 리셋
         return Page;
 	}
-	public static int Odd_Even_N_HighScore(int Page) { //홀수 짝수,점수
+
+	public static int Odd_L_HighScore(int Page) {
 		int PageL = 0;
-        int PageR = 0;
-		int length = (int)(Math.log10(PageL)+1);
+		
 		if(Page % 2 == 1) {//홀수
 			PageL = Page;
-			PageR = Page+1;
 	    }else {//짝수
 	    	PageL = Page-1;
-	    	PageR = Page;
 	    }
-		int hun1 = PageL / 100; // 백
-		int ten1 = (PageL / 10) % 10; // 십
-		int one1 = PageL % 10; // 일*/
-		int hun2 = PageR / 100; // 백
-		int ten2 = (PageR / 10) % 10; // 십
-		int one2 = PageR % 10; // 일*/
-		int plusL = hun1+ten1+one1;
-		int multiL = hun1*ten1*one1;
-		int plusR = hun2+ten2+one2;
-		int multiR = hun2*ten2*one2;
-		int PageLSco = (plusL < multiL) ? multiL : plusL;
-		int PageRSco = (plusR < multiR) ? multiR : plusR;
-		int HighSco = (PageLSco < PageRSco) ? PageRSco : PageLSco;
-			
+		int length = (int)(Math.log10(PageL)+1);//int자리수
+		
+		int hun = PageL / 100; // 백
+		int ten = (PageL / 10) % 10; // 십
+		int one = PageL % 10; // 일*/
+		
+		int plusL = 0;
+		int multiL = 0;
+		
+		if(length == 3) { //자릿수에 따른 곱하기
+			plusL = hun+ten+one;
+			multiL = hun*ten*one;//100의자리 아니면 곱했을때 무조
+		}else if(length == 2) {
+			plusL = ten+one;
+			multiL = ten*one;
+		}else {
+			plusL = PageL;
+			multiL = PageL;//100의자리 아니면 곱했을때 무조
+		}
+
+		int HighSco = (plusL < multiL) ? multiL : plusL;
 		return HighSco;
 	}
+	public static int Even_R_HighScore(int Page) {
+        int PageR = 0;
+        
+		if(Page % 2 == 0) {//짝수
+			PageR = Page;
+	    }else {//짝수
+	    	PageR = Page+1;
+	    }
+		int length = (int)(Math.log10(PageR)+1);//int자리수
+		
+		int hun = PageR / 100; // 백
+		int ten = (PageR / 10) % 10; // 십
+		int one = PageR % 10; // 일*/
+		
+		int plusR = 0;
+		int multiR = 0;
+		
+		if(length == 3) { //자릿수에 따른 곱하기
+			plusR = hun+ten+one;
+			multiR = hun*ten*one;//100의자리 아니면 곱했을때 무조
+		}else if(length == 2) {
+			plusR = ten+one;
+			multiR = ten*one;
+		}else {
+			plusR = PageR;
+			multiR = PageR;//100의자리 아니면 곱했을때 무조
+		}
+		
+		int HighSco = (plusR < multiR) ? multiR : plusR;
+		return HighSco;
+	}
+	public static int HighScore(int Page) {
+		int score = 0;
+		return score;
+	}
+	
 	
     public static void main(String[] args){
     	
     	int pobiPage = makePage();
+    	System.out.println(pobiPage+ " pobiPage");
     	int crongPage = makePage();
+    	System.out.println(crongPage+ " crongPage");
     	
-    	int pobiL = 0;
-        int pobiR = 0;
-        int crongL = 0;
-        int crongR = 0;
-
-        if(pobiPage % 2 == 1) {//홀수
-    		pobiL = pobiPage;
-    		pobiR = pobiPage+1;
-    		} else {//짝수
-    		pobiL = pobiPage-1;
-    		pobiR = pobiPage;
-    		}
-    	if(crongPage % 2 == 1) {//홀수
-    		crongL = crongPage;
-    		crongR = crongPage+1;
-    		} else {//짝수
-    		crongL = crongPage-1;
-    		crongR = crongPage;
-    		}
+    	int pobiLScore = Odd_L_HighScore(pobiPage);
+    	int pobiRScore = Even_R_HighScore(pobiPage);
+    	int crongLScore = Odd_L_HighScore(crongPage);
+    	int crongRScore = Even_R_HighScore(crongPage);
+       
     	/*int hun = i / 100; // 백
 		int ten = (i / 10) % 10; // 십
 		int one = i % 10; // 일*/
