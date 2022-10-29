bb4669b3ff3317eadd5ebba862492763fd09449c
abbd2 <ajkety00@gmail.com>
2022-10-30 02:08:07 +0900

    절댓값을 활용하여 반대 영문자로 변환 되도록 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,13 +5,22 @@ public class Problem4 {
 
         StringBuffer sb = new StringBuffer();
         char[] wordCharArr = word.toCharArray();
-        int ascii, absVal, reverseWord = 0;
+        int ascii = 0;
+        int absVal = 0;
+        int reverseWord = 0;
+
 
         for(char wordTemp : wordCharArr){
             if(Character.isUpperCase(wordTemp)){
+                ascii = 65;
             }else if(Character.isLowerCase(wordTemp)){
+                ascii = 97;
             }
-        }
+            // ascii에서 해당 문자의 아스키 코드 값을 밴 후 절댓값을 취함
+            absVal = Math.abs(ascii - wordTemp);
+            // ascii에서 영문자의 개수 만큼 더한 후 avsVal을 빼게되면 반대 영문자로 변환됨
+            reverseWord = (ascii + 25) - absVal;
+        } // end for
 
         String answer = sb.toString();
 
