8a2c73423c773fa6d7eb2a74abd34ac0249802a2
wonju-dev <wonjulee.dev@gmail.com>
2022-10-29 16:35:33 +0900

    [feat] 예외 처리 로직 구현
    다음의 상황을 입력과 관련있는 예외 상황이라고 판단했다
    1. 입력값이 정렬이 안 된 경우 (왼쪽 >= 오른쪽)
    2. 왼쪽 페이지, 오른쪽 페이지의 번호 차이가 1보다 큰 경우
    3. 1 이하, 400이상 입력값이 들어온 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -19,4 +19,39 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+
+    // 개선 가능 (with strategy pattern)
+    private static boolean isWrongInput(List<Integer> inputs) {
+        int left = inputs.get(LEFT);
+        int right = inputs.get(RIGHT);
+        boolean isValid = true;
+
+        // 입력값이 정렬이 안 된 경우 (왼쪽 >= 오른쪽)
+        if (isNotSorted(left, right)) {
+            isValid = false;
+        }
+        // 왼쪽 페이지, 오른쪽 페이지의 번호 차이가 1보다 큰 경우
+        if (getDifference(left, right) > 1) {
+            isValid = false;
+        }
+        // 1 이하, 400이상 입력값이 들어온 경우
+        if (isOutOfBound(left, right)) {
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    private static boolean isOutOfBound(Integer left, Integer right) {
+        return left <= BOOK_START_PAGE || right >= BOOK_END_PAGE;
+    }
+
+    private static boolean isNotSorted(Integer left, Integer right) {
+        return left >= right;
+    }
+
+    private static int getDifference(Integer left, Integer right) {
+        return Math.abs(left - right);
+    }
 }
