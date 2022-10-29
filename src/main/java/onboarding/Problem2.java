05189cab37f4360765997aaa205414afcf1f28f9
khj <bbb6254177@gmail.com>
2022-10-30 00:45:36 +0900

    initial commit

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuffer ans = new StringBuffer(cryptogram);
+
+        int left = 0;
+        int right = 1;
+
+        while (right < ans.length()){
+            while (right < ans.length() && ans.charAt(left) == ans.charAt(right)){
+                right += 1;
+            }
+
+            if (right - left > 1){
+                ans.delete(left, right);
+                left -= 1;
+                right = left + 1;
+                continue;
+            }
+
+            right += 1;
+            left += 1;
+        }
+
+        return ans.toString();
     }
 }
