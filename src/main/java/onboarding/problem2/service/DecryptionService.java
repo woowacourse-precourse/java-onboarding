146530a67dc1b249f2405bc7e3755aa97ede219d
song-wooseok <sws981202@gmail.com>
2022-10-29 22:04:56 +0900

    feat: [PROBLEM2 - 가능3] 연속된 한 단어에 대한 위치를 반환하는 기능

--- src/main/java/onboarding/problem2/service/DecryptionService.java
+++ src/main/java/onboarding/problem2/service/DecryptionService.java
@@ -2,6 +2,8 @@ package onboarding.problem2.service;
 
 import onboarding.problem2.domain.DuplicateCryptedText;
 
+import java.util.ArrayList;
+
 public class DecryptionService {
     public static void decryption(DuplicateCryptedText cryptedText){
 
@@ -28,5 +30,31 @@ public class DecryptionService {
 
     public static boolean compareWord(char c1, char c2){
         return (c1 == c2);
+    }
+
+    public static boolean notNullPositionCheck(int[] position){
+        return position != null;
+    }
+
+    public static int[] getContinuedCharPosition(int start, String word){
+        char checkChar = word.charAt(start);
+        int[] position = null;
+
+        for(int index = (start+1); index < word.length(); index++){
+            if(!compareWord(checkChar, word.charAt(index))){
+                break;
+            }
+            position = updatePosition(position, start, index);
+        }
+        return position;
+    }
+
+    public static int[] updatePosition(int [] position, int start, int index){
+        if(position == null){
+            position = new int[]{start, index};
+        }
+        position[1] = index;
+
+        return position;
     }
 }
