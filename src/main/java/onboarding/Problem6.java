23c90d5def637e173bd6b0b2bc0892d8a76da364
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-10-30 06:07:48 +0900

    Problem6: 닉네임을 2글자씩 잘라서 중복된다면 해당 닉네임과 중복되는 닉네임 이메일 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
@@ -19,8 +20,24 @@ public class Problem6 {
     }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashMap<String, String> dict = new HashMap<>(); //닉네임 두글자씩 잘라서 저장
 
-
+        for(int i=0; i<forms.size(); i++){
+            if(!checkEmail(forms.get(i).get(0)) && !checkKor(forms.get(i).get(1))){ //조건 검사
+                break;
+            }
+            String name = forms.get(i).get(1); //닉네임
+            String email = forms.get(i).get(0); //이메일
+            for(int j=0; j<name.length()-1; j++){
+                String subName = name.substring(j, j+2); //닉네임 두글자씩 자르기
+                if(dict.containsKey(subName)){ //해당 닉네임이 있다면 해당 닉네임의 이메일과 중복된 닉네임의 이메일 저장
+                    answer.add(email);
+                    answer.add(dict.get(subName));
+                }else{
+                    dict.put(subName, email);
+                }
+            }
+        }
 
         return answer;
     }
