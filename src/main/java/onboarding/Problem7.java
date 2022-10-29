d62a21886fac396530e3f366e1a56ce461760996
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-29 18:31:53 +0900

    [Problem 7] 함수들을 제어하는 제어 함수 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    private static Map<String, Integer> friendCandidate = new HashMap<>();
+    private static Set<String> oldFriend = new HashSet<>();
+    private static List<List<String>> friendData;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        answer = getRecomendedFriends(user,friends,visitors);
         return answer;
     }
+
+    private static List<String> getRecomendedFriends(String user, List<List<String>> friends, List<String> visitors){
+        oldFriend.add(user);
+        friendData=friends;
+        List<String> olderFriends = getFriendOfUser(user);
+
+    }
+
 }
