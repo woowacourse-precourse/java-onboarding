3c19facde06d9cde940af73ed4c8b99d09393648
tlsqj <rnjstlsqja@inha.edu>
2022-10-29 18:31:26 +0900

    문제 4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int wordIterator = 0; wordIterator < word.length(); wordIterator++) {
+            if (word.charAt(wordIterator) >= 'A' && word.charAt(wordIterator) <= 'Z') {
+                answer += (char)(90 - (int)word.charAt(wordIterator) - 65);
+            }
+
+            else if (word.charAt(wordIterator) >= 'a' && word.charAt(wordIterator) <= 'z') {
+                answer += (char)(122 - (int)word.charAt(wordIterator) - 97);
+            }
+
+            else answer += word.charAt(wordIterator);
+        }
+
         return answer;
     }
 }
