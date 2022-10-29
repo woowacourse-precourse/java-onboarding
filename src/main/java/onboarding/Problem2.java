b97816b772dd0672c57f95a4f79ee557e005719c
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-29 16:53:56 +0900

    feat(Problem2): 연속하는 중복 문자 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static void deleteConsecutiveDuplicate(String cryptogram){
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i = 0; i < cryptogram.length(); i++){
+            if(!stack.isEmpty() && stack.peek()==cryptogram.charAt(i)){
+                stack.pop();
+                continue;
+            }
+            stack.push(cryptogram.charAt(i));
+        }
+        return;
     }
 }
