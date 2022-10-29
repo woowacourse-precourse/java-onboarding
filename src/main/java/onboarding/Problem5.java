b3b6326e5f982a5121ca4054dbbdfae22cfb8ef1
iamjooon2 <iamjooon2@gmail.com>
2022-10-29 22:12:03 +0900

    refactor(problem5): 공용으로 사용할 잔돈 배열 메서드 밖으로 분리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,16 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
+    public static List<Integer> moneyList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-
-        List<Integer> moneyList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
         List<Integer> result = new ArrayList<>();
-
         for (int change : moneyList) {
             result.add(money / change);
             money %= change;
         }
-
         return result;
     }
 }
