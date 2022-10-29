d057cf87a983ad9b027152e8df9a34d3f9140143
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 01:36:18 +0900

    type:feat 숫자를 자릿수별로 분할하는 기능추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,19 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private List<Integer> divideNumberByDigit (int pageNumber) {
+        List<Integer> result = new ArrayList();
+        for (int i = pageNumber; i > 0; i /= 10) {
+            result.add(i % 10);
+        }
+        return result;
     }
 }
