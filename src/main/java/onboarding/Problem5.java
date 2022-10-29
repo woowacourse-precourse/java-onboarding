16be579a608ec4fcb8722851ab32aa020eeafa54
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 23:09:41 +0900

    Feat : add get money list function
    큰 액수에 따라 돈의 개수를 찾는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
+    private static final List<Integer> moneyList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static boolean validCheck(int money) {
         return money >= 1 && money <= 1000000;
+    }
+
+    public static List<Integer> getSortedMoney(int money) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer list : moneyList) {
+            result.add(money / list);
+            money %= list;
+        }
+        return result;
     }
 
     public static List<Integer> solution(int money) {
@@ -14,6 +27,7 @@ public class Problem5 {
         if(!validCheck(money)){
             throw new IllegalStateException("잘못된 입력입니다.");
         }
+        answer = getSortedMoney(money);
         return answer;
     }
 }
