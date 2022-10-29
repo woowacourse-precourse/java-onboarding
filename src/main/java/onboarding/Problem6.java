0cf334cf4cf4768291dbddd5c57121094409175f
NoseSSG <dmlcks1998@naver.com>
2022-10-29 15:53:03 +0900

    6번 문제 완성 코드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,44 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    static List<String> Cut_The_Name(String name){
+        List<String> cut_name = new ArrayList<String>();
+        for(int i = 0 ; i < name.length() -1; i++){
+            cut_name.add(name.substring(i,i+2));
+        }
+        return cut_name;
+    }
+    static Set<String> Find_Same(List<List<String>> list){
+        List<String> cut_name;
+        Set<String > same = new HashSet<String>();
+
+        for (int i = 0 ; i < list.size() ; i++){
+            cut_name = Cut_The_Name(list.get(i).get(1));
+            for(int j = 0 ; j < list.size(); j++){
+                if( j == i) continue;
+                for(Iterator<String > name = cut_name.iterator();name.hasNext();){
+                    if(list.get(j).get(1).contains(name.next())){
+                        same.add(list.get(j).get(0));
+                    }
+                }
+            }
+        }
+        return same;
+    }
+
+    static List<String> StoL(Set<String> before){
+        List<String> after = new ArrayList<>();
+        for(Iterator<String> i = before.iterator();i.hasNext();){
+            after.add(i.next());
+        }
+        return  after;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        System.out.println( Find_Same(forms));
+        List<String> answer = StoL(Find_Same(forms));
+
         return answer;
     }
 }
