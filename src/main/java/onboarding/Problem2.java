e845c9141eb66713ce945ba36b74eae5896c61db
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-29 22:47:00 +0900

    feat: 연속하는 중복 문자열 제거 (#2)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+
+import java.util.Stack;
+
+
 public class Problem2 {
+    private static final Stack<Character> st = new Stack<>();
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        // 1. 중복 문자열을 제거한다.
+        removeDuplicateString(cryptogram);
+        return "";
+    }
+
+    private static void removeDuplicateString(String str) {
+        for (Character c : str.toCharArray()) {
+            if (!st.isEmpty()) {
+                Character last = st.peek();
+                if (c.equals(last)) {
+                    st.pop();
+                    continue;
+                }
+            }
+            st.add(c);
+        }
     }
 }
