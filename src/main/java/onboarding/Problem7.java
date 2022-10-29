78df4c4e5dcd3a128a99e8450da48b2e3505c23e
jagajang <kosb7544@gmail.com>
2022-10-29 14:22:23 +0900

    p7/make friend info map

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String, Set<String>> friendMap;
+    private static Map<String, Integer> scores;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        friendMap = new HashMap<>();
+        scores = new HashMap<>();
+
+        makeFriendMap(friends);
+
         return answer;
+    }
+
+    private static void makeFriendMap(List<List<String>> friends) {
+        for(List<String> relation : friends) {
+            String n1 = relation.get(0);
+            String n2 = relation.get(1);
+
+            friendMap.putIfAbsent(n1, new HashSet<>());
+            friendMap.get(n1).add(n2);
+
+            friendMap.putIfAbsent(n2, new HashSet<>());
+            friendMap.get(n2).add(n1);
+        }
     }
 }
