1b2abbff3fccb5e8e648f268ec1d076382ffa91a
seongha <11pi885@gmail.com>
2022-10-30 03:35:29 +0900

    feat: 6번 기능목록 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,13 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        //두 단어로 끊어서 해쉬맵에 저장하고, 해쉬맵에 이미 존재한다면, 중복을 허용하지 않는 셋에 추가해서 저장
+
+
         return answer;
     }
 }
