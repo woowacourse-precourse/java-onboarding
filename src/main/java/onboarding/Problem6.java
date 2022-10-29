6080958e2b14c6cb217e7f9bd52c50c965e45400
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-29 12:38:41 +0900

    [Problem 6] 입력 받은 List의 유효성을 검사하는 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,47 @@
 package onboarding;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        if(!checkValidation(forms)){
+            return null;
+        }
         return answer;
     }
+
+    private static boolean checkValidation(List<List<String>> forms){
+        if(dataAmountValidation(forms) && nicknameValidation(forms) && emailValidation(forms)){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean dataAmountValidation(List<List<String>> forms){
+        if(1<=forms.size() && forms.size()<=10000){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean nicknameValidation(List<List<String>> forms){
+        final int MAX_LENGTH = 20;
+        final String NICKNAME_REGEX = "[가-힣]+$";
+        return forms.stream()
+                .allMatch((List a) -> (Pattern.matches(NICKNAME_REGEX,(String)a.get(1))) && ((String) a.get(1)).length()<=MAX_LENGTH);
+    }
+
+    private static boolean emailValidation(List<List<String>> forms){
+        final int MIN_LENGTH = 11;
+        final int MAX_LENGTH = 20;
+        final String EMAIL_REGEX  = "^\\w+@email\\.com";
+        return forms.stream().allMatch((List a)-> Pattern.matches(EMAIL_REGEX, (String)a.get(0))
+                && ((String) a.get(0)).length()<=MAX_LENGTH
+                && (MIN_LENGTH<=((String) a.get(0)).length()));
+    }
+
+
+
 }
