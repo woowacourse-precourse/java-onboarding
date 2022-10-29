b03efd8f1b0ab0406b297dfe9d0ead6f2a40768f
seheonCho <whtpgjs94@gmail.com>
2022-10-30 02:41:31 +0900

    feat: [문제3] 룰 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,7 +8,14 @@ public class Problem3 {
     private static Set<Character> clapRule = new HashSet<>();
 
     public static int solution(int number) {
+        addClapRule();
         int answer = 0;
         return answer;
+    }
+
+    private static void addClapRule() {
+        clapRule.add('3');
+        clapRule.add('6');
+        clapRule.add('9');
     }
 }
