a3f52d535f2712d494769ca1314df83285f4936d
himjjong <jjijji5728@tukorea.ac.kr>
2022-10-29 18:53:26 +0900

    feat : 주어진 number크기까지 값을 문자열로 쪼개는 splitNumber 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = splitNumber(number);
         return answer;
+    }
+    static int splitNumber(int number){
+        int result = 0;
+        for(int i=1; i<=number; i++){
+            String str = Integer.toString(i);
+            String[] strarr = str.split("");
+            if(clapCount(strarr) > 0) result += clapCount(strarr);
+        }
+        return result;
+    }
+    static int clapCount(String[] strarr){
+        int count = 0;
+        return count;
     }
 }
