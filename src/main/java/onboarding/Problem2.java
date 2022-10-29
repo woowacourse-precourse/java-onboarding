a104aae5b324ae6ae2d36ecfcef9bb571a296fef
keon0711 <fnc6488@gmail.com>
2022-10-30 02:48:31 +0900

    [Problem2] 인접 중복 문자들을 체크하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,18 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        return "a";
+
     }
 
-    private static boolean isSolitary(String cryptogram, int index) {
-        if (index == 0)
-            return cryptogram.charAt(index) != cryptogram.charAt(index + 1);
-        if (index == cryptogram.length() - 1)
-            return cryptogram.charAt(index) != cryptogram.charAt(index - 1);
-        if (cryptogram.charAt(index) != cryptogram.charAt(index-1)
-                && cryptogram.charAt(index) != cryptogram.charAt(index + 1))
-            return true;
-        return false;
-    }
+    private static boolean[] checkDuplicatedChar(String cryptogram) {
+        boolean[] duplicated = new boolean[cryptogram.length()];
 
+        for (int i = 1; i < cryptogram.length(); i++) {
+            char curChar = cryptogram.charAt(i);
+            char prevChar = cryptogram.charAt(i - 1);
+            if (curChar == prevChar) {
+                duplicated[i - 1] = true;
+                duplicated[i] = true;
+            }
+        }
+        return duplicated;
+    }
 }
+
+
