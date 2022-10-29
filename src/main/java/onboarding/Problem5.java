2076f4a5d530815993c5fdf383861847f7cfc211
JUN <chltmdwns96@naver.com>
2022-10-29 23:19:34 +0900

    feature : 단위 별 금액이 담긴 배열 생성 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> moneyArr = createMoneyArr();
         return answer;
+    }
+
+    private static List<Integer> createMoneyArr(){
+        return Arrays.asList(50000,10000,5000,1000,500,100,10,1);
     }
 }
