0ba6fa6a37b458e16f7f4e8b6514246077907684
5uhwann <5uhwann27@gmail.com>
2022-10-30 00:15:42 +0900

    feat: PROBLEM5 - solution 검증 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,22 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
 
   private static int[] moneyTypeList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+  private static final int MAX_MONEY_SIZE = 1_000_000;
+  private static final Integer[] EXCEPTION_LIST = {-99999};
+
 
   public static List<Integer> solution(int money) {
-    List<Integer> answer = countEachTypeOfMoney(money);
-    return answer;
+    if (isAllowedMoney(money)) {
+      return countEachTypeOfMoney(money);
+    }
+    List<Integer> returnList = new ArrayList<>(Arrays.asList(EXCEPTION_LIST));
+    return returnList;
   }
 
   public static List<Integer> countEachTypeOfMoney(int money) {
@@ -23,6 +30,13 @@ public class Problem5 {
       }
     }
     return eachMoneyCountList;
+  }
+
+  public static boolean isAllowedMoney(int money) {
+    if (money < 1 || money > MAX_MONEY_SIZE) {
+      throw new IllegalStateException("허용된 금액이 아닙니다.");
+    }
+    return true;
   }
 
 }
