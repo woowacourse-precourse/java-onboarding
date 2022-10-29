ffa268c50ec9e697caea6c695e809eba24491b4b
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-29 14:42:39 +0900

    아스키코드를 이용하여 문자열을 변환하여 return

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,17 @@ public class Problem4 {
     public static String solution(String word) {
         char[] wordArr = word.toCharArray();
         
-        String answer = "";
-        return answer;
+        for(int i=0; i<wordArr.length; i++){
+            int n = wordArr[i];
+            if(64<wordArr[i] && wordArr[i]<91){
+                wordArr[i] = (char)(155-n);
+            }
+            if(96<wordArr[i] && wordArr[i]<123){
+                wordArr[i] = (char)(219-n);
+            }
+        }
+        word = new String(wordArr);
+        
+        return word;
     }
 }
