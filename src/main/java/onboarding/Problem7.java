a62d07d8461ce487cc4bc68fd87165d4d49c12f3
davejang <davejang98@naver.com>
2022-10-29 18:42:05 +0900

    feat: Problem7 user initialize method
    전체 유저 정보 초기화 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,7 +6,24 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
+    public static HashSet<String> userList;
     public static HashMap<String, HashSet<String>> friendListInfo;
+    public static void setUserList(String user, List<List<String>> friends, List<String> visitors) {
+        HashSet<String> emptyList = new HashSet<>();
+        friendListInfo.put(user,emptyList);
+        for(int index = 0; index < friends.size(); index++) {
+            String person1 = friends.get(index).get(0);
+            String person2 = friends.get(index).get(1);
+
+            friendListInfo.put(person1,emptyList);
+            friendListInfo.put(person2,emptyList);
+        }
+        for(int index = 0; index < visitors.size(); index++) {
+            String person = visitors.get(index);
+
+            friendListInfo.put(person,emptyList);
+        }
+    }
     public static void setFriendList(List<List<String>> friends) {
         for(int index = 0; index < friends.size(); index ++) {
             String person1 = friends.get(index).get(0);
