445c822316a74f7497b3f75187e9f31c1cb7d91c
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-29 20:14:43 +0900

    FEAT : Problem 2-1. cryptogram을 입력받기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+/*
+ *
+ * Class : Problem 2
+ *
+ * Date : 2022.10.29
+ *
+ * Author : kathyleesh
+ *
+ * <기능 목록>
+ *
+ * 1. 암호문(cryptogram)을 입력받는다.
+ * 2. 암호문(cryptogram)을 각 자리 별로 쪼개어 리스트에 저장시킨다.
+ * 3. 리스트를 탐색하며 연속하는 중복 문자들을 삭제한다.
+ * 4. 연속하는 중복된 문자들이 없을 때까지 3번을 반복한다.
+ * 5. 연속하는 중복된 문자들이 없으면 결과를 출력한다.
+ *
+ * */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        String crypto = cryptogram;
+        System.out.println(crypto);
         return answer;
     }
 }
