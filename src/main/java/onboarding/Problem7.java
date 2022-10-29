c72a169cc070a96680593098ba6f3748daab4a6a
swandevson <shai4956@gmail.com>
2022-10-30 00:21:58 +0900

    feat: P7 user가 포함된 친구 목록인지 체크
    - user가 포함된 친구 목록인지 체크
    - 포함되었을 경우 true, 아니면 false

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
+    public static boolean isUser(String user, List<String> userList) {
+        if(user.equals(userList.get(0)) || user.equals(userList.get(1))) {
+            return true;
+        }
+        return false;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashSet<String> userFriends;
+
+
         return answer;
     }
 }
