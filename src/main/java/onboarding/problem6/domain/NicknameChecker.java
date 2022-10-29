102efc08ad791211e81972324b9d0711fb1be7be
Yunsik-Choi <choiys0311@gmail.com>
2022-10-30 03:19:55 +0900

    ♻️ refactor : NicknameChecker distinctSubString 분리 NicknameChecker distinctSubString 메서드를 NicknameService로 이동

--- src/main/java/onboarding/problem6/domain/NicknameChecker.java
+++ src/main/java/onboarding/problem6/domain/NicknameChecker.java
@@ -1,15 +1,11 @@
 package onboarding.problem6.domain;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class NicknameChecker {
 
     private final Map<String, Integer> nicknameChecker;
-    private static final int NICKNAME_DUPLICATE_LENGTH = 2;
     private static final int NICKNAME_DUPLICATE = 2;
 
     public NicknameChecker() {
@@ -17,15 +13,7 @@ public class NicknameChecker {
     }
 
     public void saveNickname(String nickname) {
-        distinctSubString(nickname)
-                .forEach(key -> nicknameChecker.put(key, nicknameChecker.getOrDefault(key, 0) + 1));
-    }
-
-    public List<String> distinctSubString(String text) {
-        return IntStream.range(0, text.length() - 1)
-                .mapToObj(i -> text.substring(i, i + NICKNAME_DUPLICATE_LENGTH))
-                .distinct()
-                .collect(Collectors.toList());
+        nicknameChecker.put(nickname, nicknameChecker.getOrDefault(nickname, 0) + 1);
     }
 
     public boolean isDuplicate(String nickname) {
