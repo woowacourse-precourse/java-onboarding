3085885145981cb302c683815e9278810268bb98
Jeongthingamajig <netpetet@naver.com>
2022-10-29 15:12:10 +0900

    Problem4 refactoring ? ! 같은 문자가 아닌 알파벳만 바뀌게 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,32 +4,30 @@ package onboarding;
  * 예외 생각해보기
  */
 public class Problem4 {
+
     public static String solution(String word) {
-        for (char a : word.toCharArray()){ // 예외처리 문자열이 아니면
-            if((a < (char)'A') && (a > (char)'z' && " ".equals(a))){
-                System.out.println("잘못된 입력입니다. word를 다시 주세요 잘못된 문자는 : " + a);;
-                continue;
+
+        StringBuilder answer = new StringBuilder();
+        char[] arr = word.toCharArray();
+        for (char a : arr){
+            if(a >= 'A' && a <='Z' || a >= 'a' && a <= 'z') {
+                answer.append(change(a));
+            }else{
+                answer.append(a);
             }
         }
-        String answer = "";
-        char[] arr = word.toCharArray();
-
-        for(char a : arr){
-            answer += change(a);
-        }
-        return answer;
+        return answer.toString();
     }
 
-    public static String change(char alphabet){
-        if(Character.isUpperCase(alphabet)){ //대문자면
-            alphabet = (char)(('Z' + 'A') - (int)alphabet);
-        }else if(Character.isLowerCase(alphabet)){ //소문자면
-            alphabet = (char)(('z' + 'a') - (int)alphabet);
-        }else if(Character.isWhitespace(alphabet)){ //
-            alphabet = ' ';
+
+    public static String change (char alphabet){
+        if (Character.isUpperCase(alphabet)) { //대문자면
+            alphabet = (char) (('Z' + 'A') - (int) alphabet);
+        } else { //소문자면
+            alphabet = (char) (('z' + 'a') - (int) alphabet);
         }
-        System.out.println(String.valueOf(alphabet));
         return String.valueOf(alphabet);
     }
 }
+
 
