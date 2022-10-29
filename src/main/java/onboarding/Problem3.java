1d42b1e6255d86d018bb72d6fd52e76a2cc95fa8
BaeDongHyeon <sasd730@gmail.com>
2022-10-29 21:17:07 +0900

    feat(Problem3.java): 각 자리의 수와 개수 저장

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,13 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        for (int i = 1; i <= number; i++) {
+        int[] clapCount = new int[10];
 
+        for (int i = 1; i <= number; i++) {
+            String[] numberSeat = String.valueOf(i).split("");
+            for (int j = 0; j < numberSeat.length; j++) {
+                ++clapCount[Integer.parseInt(numberSeat[j])];
+            }
         }
 
         return 0;
