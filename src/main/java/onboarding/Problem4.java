0bf26af6549ba85e1a58fea016813a5e94727d3b
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 20:06:57 +0900

    :memo: [문제 4] : 풀이 전략

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,11 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        // 1. 입력된 문자가 알파벳인지 판별
+        // 2. 알파벳이라면 대문자 소문자인지 판별
+        // 3. 변환(아스키코드) => 대문자 : 155 - 문자 , 소문자 : 219 - 문자
+        // 4. 변환한 문자 합치기
+        // 5. 합친 결과 값 반환
         String answer = "";
         return answer;
     }
