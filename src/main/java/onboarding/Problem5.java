5a74718d3d4fbc2a5516c6eea91823e03c95aa44
skfkgla <narahim.lee@gmail.com>
2022-10-30 05:30:57 +0900

    feat: Problem5 - 원화를 기준으로 금액이 큰 순서대로 리스트에 담아 반환(Problem5의 solution 메서드 구현 완료)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] unit = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+        answer = calculateExchange(money, unit);
+
         return answer;
     }
 
