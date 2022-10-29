9d7bc9ec02614377de915a00ed1642985de34c22
DaWoon Lee <qkgk61@gmail.com>
2022-10-29 16:51:20 +0900

    Style : class 순서 변경, Problem1 클래스를 public으로 변환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,64 +2,7 @@ package onboarding;
 
 import java.util.List;
 
-class ScoreCalculator {
-
-    private static class ScoreCalculatorHolder {
-        private static final ScoreCalculator INSTANCE = new ScoreCalculator();
-    }
-
-    private ScoreCalculator() {
-
-    }
-
-    public static ScoreCalculator getInstance() {
-        return ScoreCalculatorHolder.INSTANCE;
-    }
-
-    public int getScore(List<Integer> playerPages) {
-        int maxScore = Integer.MIN_VALUE;
-
-        for (int len = playerPages.size(), i = 0; i < len; i++) {
-            int page = playerPages.get(i);
-            int score = getPageScore(page);
-            maxScore = Math.max(maxScore, score);
-        }
-
-        return maxScore;
-    }
-
-    private int getPageScore(int page) {
-        int sumScore = getSumScore(page);
-        int productScore = getProductScore(page);
-
-        return Math.max(sumScore, productScore);
-    }
-
-    private int getSumScore(int page) {
-        String pageStr = String.valueOf(page);
-        int sum = 0;
-
-        for (int len = pageStr.length(), i = 0; i < len; i++) {
-            sum += pageStr.charAt(i) - '0';
-        }
-
-        return sum;
-    }
-
-    private int getProductScore(int page) {
-        String pageStr = String.valueOf(page);
-        int product = 1;
-
-        for (int len = pageStr.length(), i = 0; i < len; i++) {
-            product *= pageStr.charAt(i) - '0';
-        }
-
-        return product;
-    }
-
-}
-
-class Problem1 {
+public class Problem1 {
 
     private static final int START_PAGE_NO = 1;
     private static final int END_PAGE_NO = 400;
@@ -130,6 +73,63 @@ class Problem1 {
         }
 
         return 0;
+    }
+
+}
+
+class ScoreCalculator {
+
+    private static class ScoreCalculatorHolder {
+        private static final ScoreCalculator INSTANCE = new ScoreCalculator();
+    }
+
+    private ScoreCalculator() {
+
+    }
+
+    public static ScoreCalculator getInstance() {
+        return ScoreCalculatorHolder.INSTANCE;
+    }
+
+    public int getScore(List<Integer> playerPages) {
+        int maxScore = Integer.MIN_VALUE;
+
+        for (int len = playerPages.size(), i = 0; i < len; i++) {
+            int page = playerPages.get(i);
+            int score = getPageScore(page);
+            maxScore = Math.max(maxScore, score);
+        }
+
+        return maxScore;
+    }
+
+    private int getPageScore(int page) {
+        int sumScore = getSumScore(page);
+        int productScore = getProductScore(page);
+
+        return Math.max(sumScore, productScore);
+    }
+
+    private int getSumScore(int page) {
+        String pageStr = String.valueOf(page);
+        int sum = 0;
+
+        for (int len = pageStr.length(), i = 0; i < len; i++) {
+            sum += pageStr.charAt(i) - '0';
+        }
+
+        return sum;
+    }
+
+    private int getProductScore(int page) {
+        String pageStr = String.valueOf(page);
+        int product = 1;
+
+        for (int len = pageStr.length(), i = 0; i < len; i++) {
+            product *= pageStr.charAt(i) - '0';
+        }
+
+        return product;
     }
 
 }
