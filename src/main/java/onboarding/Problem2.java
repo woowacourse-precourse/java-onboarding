aaa781e88c4206c9e0c2dc889c80e68ca068629c
hyunho12 <hyundho12@gmail.com>
2022-10-29 17:39:45 +0900

    feat: Problem2 완성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,47 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        String UpdateCryptogram = cryptogram;
+
+        while (true){
+            if(duplicate_check(UpdateCryptogram)){
+                UpdateCryptogram = duplicate_delete(UpdateCryptogram);
+            }
+            else{
+                break;
+            }
+
+            answer = UpdateCryptogram;
+        }
+
         return answer;
+    }
+
+    public static boolean duplicate_check(String input){
+        String afterCheck = "";
+
+        for(int i=0;i<input.length();i++){
+            if(i != 0 && input.charAt(i) == input.charAt(i-1)){
+                return true;
+            }
+            if(i != input.length() - 1 && input.charAt(i) == input.charAt(i+1)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static String duplicate_delete(String input){
+        String afterDelete = "";
+
+        for(int i=0;i<input.length();i++){
+            if(i != 0 && input.charAt(i-1) == input.charAt(i)) continue;
+            if(i != input.length() - 1 && input.charAt(i) == input.charAt(i+1)) continue;
+            afterDelete += input.charAt(i);
+        }
+        return afterDelete;
     }
 }
