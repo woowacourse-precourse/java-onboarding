c67e853ce1d401c520ceab5045ce55be4bfa9b92
Limjongho <whdgh9873@icloud.com>
2022-10-29 19:15:46 +0900

    feat(Problem6): 중복 된지 여부를 확인하기 위해 Set 자료구조 -> Map 자료구조로 변경 및 중복된 닉네임을 신청한 사람의 이메일 List 를 추출하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem6 {
@@ -17,17 +15,37 @@ public class Problem6 {
                 .collect(Collectors.toList());
     }
 
-    private static Set<String> getSplitNicknames(String nickname) {
-        Set<String> splitNicknames = new HashSet<>();
-
+    private static void getSplitNicknames(Map<String, Integer> nicknamesMap, String nickname) {
         int length = nickname.length();
         int sequence = 0;
-        while (sequence < length) {
-            String splitNickname = nickname.substring(sequence, sequence + 1);
-            splitNicknames.add(splitNickname);
+        while (sequence < length - 1) {
+            String splitNickname = nickname.substring(sequence, sequence + 2);
+            insertNickname(nicknamesMap, splitNickname);
             sequence++;
         }
+    }
 
-        return splitNicknames;
+    private static void insertNickname(Map<String, Integer> nicknamesMap, String splitNickname) {
+        Set<String> splitNicknames = nicknamesMap.keySet();
+        if (splitNicknames.contains(splitNickname)) {
+            Integer count = nicknamesMap.get(splitNickname);
+            nicknamesMap.replace(splitNickname, count + 1);
+        } else {
+            nicknamesMap.put(splitNickname, 1);
+        }
+    }
+
+    private static List<String> checkDuplicateNickname(Map<String, Integer> nicknamesMap) {
+        return nicknamesMap.keySet().stream()
+                .filter(key -> nicknamesMap.get(key) > 1)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> findEmailsByDuplicateNickname(List<List<String>> forms, String getDuplicateNickname) {
+        return forms.stream()
+                .filter(form -> form.get(1).contains(getDuplicateNickname))
+                .map(form -> form.get(0))
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
