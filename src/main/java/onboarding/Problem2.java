c890e5d665158e469a678663f4eb76fea5901d39
Donghyun <flaehan@gmail.com>
2022-10-29 21:55:58 +0900

    case 1 해결. 재귀함수 개념 공부, ""로 리턴될 경우 해결 못함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,47 +2,33 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "";   // String answer ="answer"인데 임의로 변경
+        String answer = "answer";   // String answer ="answer"인데 임의로 변경
 
+        if (cryptogram == null || cryptogram.length() == 0){
+            return cryptogram;
+        }
 
+        char [] chars = cryptogram.toCharArray();
 
-        return answer;
+        int i,k = 0;
+
+        for (i = 1; i < chars.length; i++){
+            if(chars[i-1] != chars[i]){
+                chars[k++] = chars[i-1];
+            }else {
+                while ( i < chars.length && chars[i-1] == chars[i]){
+                    i++;
+                }
+            }
+        }
+
+        chars [k++] = chars [i-1];
+
+        String s = new String(chars).substring(0,k);
+
+        if (k != chars.length){
+            return solution(s);
+        }
+        return s;
     }
 }
-
-//		for (int i = 0; i < str.length(); i ++) {
-//			if(!result.contains(String.valueOf(str.charAt(i)))) {
-//				result += String.valueOf(str.charAt(i));
-//			}
-//		}
-
-/*
- * 문자열 [0]과 문자열 [1] 비교 하려고 했는데 안됨
- * for (int i = 0; i < str.length(); i++) {
- * for (int j = 1; j < str.length(); j++) {
- * if (!(str.charAt(i) == str.charAt(j))){
- * result += String.valueOf(str.charAt(i));
- * }
- * }
- * }
- */
-
-/*
- * 일차원적 생각으로 연속되는 두 문자 중복되면 제
-		int j = 1;
-		for (int i = 0; i < str.length()-1; i++) {
-
-			if ((str.charAt(i) == str.charAt(j))) {
-				result += "";
-				i++;
-				i++;
-				j++;
-				j++;
-			} else {
-				result += str.charAt(i);
-				j++;
-			}
-
-		}
- */
-
