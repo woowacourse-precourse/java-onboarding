bb5b664e69de2c043d47abf8eb1f539b611c55da
Noah <shgustmd6865@naver.com>
2022-10-29 21:48:05 +0900

    Feat : 알파벳 대문자와 소문자에 대한 아스키코드 값 지정
    알파벳 대문자와 소문자에 대한 아스키코드 값을 final int 로 지정하여 사용할 수 있게 합니다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static final int UpperCaseMinCode = 65;
+    private static final int UpperCaseMaxCode = 90;
+    private static final int LowerCaseMinCode = 97;
+    private static final int LowerCaseMaxCode = 122;
+    
     public static String solution(String word) {
         String answer = "";
 
