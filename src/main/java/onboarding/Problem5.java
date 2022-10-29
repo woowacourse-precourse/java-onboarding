85929213bc636f9be6695d1a93016c58bc4977ed
wonju-dev <wonjulee.dev@gmail.com>
2022-10-29 22:41:53 +0900

    [feat]problem5 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < units.length; i++) {
+            int share = money / units[i];
+            int remainder = money % units[i];
+
+            answer.add(share);
+
+            money = remainder;
+        }
+
         return answer;
     }
 }
