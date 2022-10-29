37e39cc575c7694153ea7a81fe8db26bdc659ac8
June <68291395+june-777@users.noreply.github.com>
2022-10-29 23:00:25 +0900

    feat: 숫자에 포함된 369 개수세기 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+    
+    	int answer = 0;
         return answer;
     }
+
+	public static int countAllThreeSixNine(int num) {	
+		
+		int singleDigitNum = 0;
+		int cnt = 0;
+		while(num!=0) {
+			singleDigitNum = num % 10;
+			if(singleDigitNum % 3 == 0) {
+				cnt += 1;
+			}
+			num = num / 10;
+		}
+		return cnt;
+	}
+    
 }
