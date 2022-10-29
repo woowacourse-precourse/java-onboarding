3f31745b355baff1d11ea90911c24f1af3a43f35
good9804 <good9804@naver.com>
2022-10-29 15:56:10 +0900

    문제 5번 지폐개수구하는기능+ 현재 남아있는 돈의 금액구하는기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        
         return answer;
+    }
+    public static int CountJang(int unit,int money){
+        int jang;
+        jang=money/unit;
+        return jang;
+    }
+    public static int currentmoney(int unit,int money){
+        int currentmoney=money%unit;
+        return currentmoney;
     }
 }
