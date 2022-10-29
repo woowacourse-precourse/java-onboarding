9cbeb69e5fd707ec19b5556a02170125bee14c51
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-29 16:03:02 +0900

    feat: create class for calculating number of each money unit

---
+++ src/main/java/onboarding/problem5/MoneyCalculator.java
@@ -0,0 +1,14 @@
+package onboarding.problem5;
+
+/**
+ * Calculate count number of each money unit
+ */
+public class MoneyCalculator {
+
+    private final int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private int money;
+
+    public MoneyCalculator(int money) {
+        this.money = money;
+    }
+}
