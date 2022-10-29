f1f21f0a31fca8acb3ad480d6ee456bf52024166
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-29 22:17:03 +0900

    feat(problem7): 요구사항 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
+
 import java.util.List;
 
+
+
+/**
+ * > 요구사항
+ * 1. 친구 관계 그래프 생성
+ * 2. 사용자와 함께 아는 친구 수에 따른 점수 계산하기
+ * 3. visitor 점수 계산하기
+ * 4. 원래 User와 친구이거나 User는 결과에서 제외하기
+ * 5. 정렬해서 출력
+ */
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
+
     }
+
 }
