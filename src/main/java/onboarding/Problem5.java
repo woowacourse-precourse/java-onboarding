10b164ae220a82aafcaad5694a299f3cb593040f
정핸모 <gusah009@naver.com>
2022-10-29 14:24:24 +0900

    feat[문제 5번]: 단위 화폐에 따라 동전 개수를 반환해주는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem5 {
 
+  private static final Integer[] monetaryUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+  static {
+    Arrays.sort(monetaryUnit, Collections.reverseOrder());
+  }
+
   public static List<Integer> solution(int money) {
-    List<Integer> answer = Collections.emptyList();
+    List<Integer> answer = new ArrayList<>();
+    for (int unit : monetaryUnit) {
+      answer.add(money / unit);
+      money %= unit;
+    }
     return answer;
   }
 }
