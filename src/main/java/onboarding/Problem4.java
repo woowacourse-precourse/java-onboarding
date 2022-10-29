3468d2e9a37ce2a08fdfac7df8777c5239006cef
yejin <kwonyj1022@naver.com>
2022-10-30 02:10:44 +0900

    solution 메서드 : String -> char 한 후 알파벳 변환하고 char -> String

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,13 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        char[] alphabet = word.toCharArray();   // String을 char 배열로 만듦
+
+        for (int i = 0; i < alphabet.length; i++) { // 배열의 각 요소에 접근해서 청개구리 사전에 따라 변환
+            alphabet[i] = change(alphabet[i]);
+        }
+
+        String answer = String.valueOf(alphabet);   // char 배열을 다시 String 배열로 만듦
         return answer;
     }
 }
