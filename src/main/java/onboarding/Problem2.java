32e23939bd503d2839d51c29df8fcff19228c117
Invidam <hansupark0208@gmail.com>
2022-10-29 16:35:13 +0900

    refactor(Problem2): extract method that add character to deque.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,21 +2,33 @@ package onboarding;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.InputMismatchException;
 
 public class Problem2 {
 
     private static Deque<Character> removeDuplicates(String cryptogram) {
         Deque<Character> deque = new ArrayDeque<>();
         for (char ch : cryptogram.toCharArray()) {
-
-            if (!deque.isEmpty() && deque.getLast() == ch) {
-                deque.removeLast();
-                continue;
-            }
-
-            deque.addLast(ch);
+            enrichDeque(deque, ch);
         }
         return deque;
+    }
+
+    private static void enrichDeque(Deque<Character> deque, char ch) {
+        if (!Character.isLowerCase(ch)) {
+            throw new InputMismatchException("영문자 소문자가 아닌 입력이 들어왔습니다.");
+        }
+
+        if (isDuplicates(deque, ch)) {
+            deque.removeLast();
+            return;
+        }
+
+        deque.addLast(ch);
+    }
+
+    private static boolean isDuplicates(Deque<Character> deque, char ch) {
+        return !deque.isEmpty() && deque.getLast() == ch;
     }
 
 
