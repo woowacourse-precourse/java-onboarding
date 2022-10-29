3ec294ff52194eda7c57ab345aa7e4abdfdf2fde
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-10-29 16:17:30 +0900

    feat: [Prob 1] 계산된 최댓값을 점수로 설정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -48,6 +48,8 @@ class Problem1 {
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiScore=getScoreByMaxValue(pobi);
+        int crongScore=getScoreByMaxValue(crong);
         return answer;
     }
 }
