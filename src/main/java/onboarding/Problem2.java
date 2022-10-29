70bd3b652d73a0af608fee5994b33467f5933081
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-10-30 05:53:13 +0900

    Problem2: 중복문자가 존재하는지 확인하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem2 {
+    /* compare: 중복 문자가 존재하는지 확인하는 함수 */
+    public static boolean compare(List<Character> chr){
+        for(int i=0; i<chr.size()-1; i++){
+            if(chr.get(i)==chr.get(i+1)) return true;
+        }
+        return false;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
 
@@ -11,6 +18,10 @@ public class Problem2 {
         List<Character> cyp = new ArrayList<Character>();
         for(int i=0; i<cryptogram.length(); i++){
             cyp.add(cryptogram.charAt(i));
+        }
+
+        while(compare(cyp)){
+
         }
 
         return answer;
