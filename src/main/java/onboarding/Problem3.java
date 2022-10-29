7d8879c0d984af9a7f719fc0fd1ff8067eec8cec
YooDongSeon <inko51366@naver.com>
2022-10-29 16:07:07 +0900

    기능목록 작성 후 커밋

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+    /**
+     * 기능목록 :
+     *
+     * 1부터 number까지 자리수 하나하나가 3, 6, 9를 가지고 있는지 검사 한 후
+     * 가지고있으면 clapCount += 1
+     */
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
