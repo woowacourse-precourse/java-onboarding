db1d79e8dddc14fe963c90abc10c4fec0ef17a40
opuopuo <pureuml.dev@gmail.com>
2022-10-29 22:31:03 +0900

    feat: Problem4 기능 구현 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,36 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int UPPER_ASCII_SUM = 155;
+    private static final int LOWER_ASCII_SUM = 219;
+
     public static String solution(String word) {
+        char[] charWord = word.toCharArray();
+
+        if (!verifyException(charWord)) {
+            return "Error";
+        }
+        return convertReverse(charWord).toString();
+    }
+
+    private static String convertReverse(char[] word) {
         String answer = "";
+
+        for (char alphabet : word) {
+            if (alphabet == ' ') {
+                answer += ' ';
+            }
+            if (Character.isUpperCase(alphabet)) {
+                answer += (char)(UPPER_ASCII_SUM - (int)alphabet);
+            }
+            if (Character.isLowerCase(alphabet)) {
+                answer += (char)(LOWER_ASCII_SUM - (int)alphabet);
+            }
+        }
         return answer;
+    }
+
+    private static boolean verifyException(char[] word) {
+        return (word != null && word.length >= 1 && word.length <= 1000);
     }
 }
