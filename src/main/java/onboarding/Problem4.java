3c99315f5d6a8ae1a2a11644f63819cb0a98927f
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-29 14:40:50 +0900

    문자열 word로 char배열로 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,8 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        char[] wordArr = word.toCharArray();
+        
         String answer = "";
         return answer;
     }
