1f7e73f8f54dfd56bebc975488aa18a804cda776
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-29 13:55:10 +0900

    feat: Problem_5 화폐단위별로 나머지와 몫을 구하는 기능 구현
    화폐단위 리스트를 생성
    큰 값 부터 단위 별로 나머지와 몫을 구해서
    사용한 화폐들을 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getMinPaperMoney(money);
+    }
+
+    private static List<Integer> getMinPaperMoney(int money){
+        final int[] MONETARY_UNIT =
+                new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> resultUnit = new ArrayList<>();
+
+        for (int i=0; i<MONETARY_UNIT.length; i++){
+            resultUnit.add(money / MONETARY_UNIT[i]);
+            money %= MONETARY_UNIT[i];
+        }
+
+        return resultUnit;
     }
 }
