95921f0fb0274a3fd1aba76cc6fc8988cb1e06a1
Ridealist <cjsn92@gmail.com>
2022-10-29 12:28:13 +0900

    feat/Problem3 : 필요 메소드 정의

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int countThreeSixNine(int n) {
+        String n_str = String.valueOf(n);
+        int cnt = 0;
+        for (int i=0; i<n_str.length(); i++) {
+            if (n_str.charAt(i) == '3' | n_str.charAt(i) == '6' | n_str.charAt(i) == '9') {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
 }
