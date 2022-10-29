325c2a8263f18a39460476e9464abb56c5be3924
im-shung <seoyoung1113@gmail.com>
2022-10-29 17:01:48 +0900

    :sparkles: 연속하는 중복 문자 삭제 기능 구현 #2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        /* [요구] 연속하는 중복 문자들을 삭제 후 리턴 */
+        return removeDuplicate(cryptogram);
+    }
+
+    // 연속하는 중복 문자들을 삭제하는 메서드
+    public static String removeDuplicate(String cryptogram) {
+        List<String> newCryptogram = new ArrayList<>();
+        String pre = "";
+        for (String s: cryptogram.split("")){
+            if (newCryptogram.isEmpty()) {
+                newCryptogram.add(s);
+                continue;
+            }
+            if (s.equals(newCryptogram.get(newCryptogram.size()-1))) {
+                newCryptogram.remove(newCryptogram.size()-1);
+                pre = s;
+                continue;
+            }
+            if (pre.equals(s)) {
+                continue;
+            }
+            pre = s;
+            newCryptogram.add(s);
+        }
+        return String.join("", newCryptogram);
     }
 }
