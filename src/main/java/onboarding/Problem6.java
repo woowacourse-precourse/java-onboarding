410daf8afc2a297ac99a853e308975179d4f828d
wath1457 <wath1457@gmail.com>
2022-10-29 19:02:57 +0900

    [Problem6] Add last_character_process

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,7 +6,7 @@ import java.util.*;
 * 중복의 최소 단위인 2글자만 탐색하면 됨
 1. 금지 단어목록인 빈 리스트 생성, appeared 리스트 생성
 2. 모든 닉네임에 대해서 순차적으로 탐색
-3. 각 닉네임마다 2글자씩 탐색하면서 appeared에 없으면 추가
+3. 각 닉네임마다 2글자씩 탐색하면서 appeared에 없으면 추가 + 마지막 문자이면 break(다음 문자가 없으므로)
 4. 만약 appeared에 이미 존재하는 2글자이면 금지목록에 추가
 5. 다시 forms[][1] 닉네임 부분을 탐색하면서 금지목록의 단어가 포함되어있으면 result리스트에 이메일 추가
 6. result 오름차순 정렬
@@ -23,7 +23,9 @@ public class Problem6 {
         for (List<String> user : forms) {
             String user_name = user.get(1);
             for(int i = 0; i < user_name.length(); i++) {
-                String part = user_name.substring(i,i+1);
+                if (i == user_name.length() - 1)  // 마지막 문자 처리
+                    break;
+                String part = user_name.substring(i,i+2);
                 if (!appeared.contains(part))
                     appeared.add(part);
                 else
@@ -40,8 +42,10 @@ public class Problem6 {
             String user_name = user.get(1);
             String user_email = user.get(0);
             for (String p : prohibition) {
-                if (user_name.contains(p))
+                if (user_name.contains(p)) {
                     answer.add(user_email);
+                    break; // 이미 중복이 확인됨
+                }
             }
         } // 이메일 오름차순 정렬
         Collections.sort(answer);
