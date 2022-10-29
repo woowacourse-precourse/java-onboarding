dd62331c8055f59b4f30eca98e2563a159e8a975
Koonayoung518 <koony000415@gmail.com>
2022-10-30 01:47:46 +0900

    feat(problem4) : 대문자와 소문자 구분하기 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,9 +8,14 @@ public class Problem4 {
         for(int i=0; i< splitWord.length; i++){
             reverse = splitWord[i];
             if(!splitWord[i].equals(" "))
-                reverse = null;
+                reverse = compareLetter(splitWord[i]);
             answer+= reverse;
         }
         return answer;
+    }
+    private static String compareLetter(String letter){
+        if(65 <= letter.charAt(0) && 90 >= letter.charAt(0))
+            return "대문자";
+        return "소문자";
     }
 }
