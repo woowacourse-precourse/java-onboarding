7561712a861301b9ad1e20701cb6aa75f0d78c60
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 23:08:18 +0900

    Feat : add validation check function to Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    public static boolean validCheck(int money) {
+        return money >= 1 && money <= 1000000;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        if(!validCheck(money)){
+            throw new IllegalStateException("잘못된 입력입니다.");
+        }
         return answer;
     }
 }
