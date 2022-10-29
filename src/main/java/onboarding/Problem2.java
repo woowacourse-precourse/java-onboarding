bedc1f8d55d72d4f32ec9dcd5a1e807a8c1e7884
sangeun-b <baek0007@algonquinlive.com>
2022-10-29 23:04:42 +0900

    feat(PROBLEM2): Add feature for checking duplicate string

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+    // 중복 문자열 있는 지 확인
+    public static int isDuplicated(ArrayList<String> word){
+        for(int i = 0; i<word.size()-1; i++){
+            if(word.get(i).equals(word.get(i+1))){
+                return i;
+            }
+        }
+        return -1;
     }
 }
