e90c11641cec7b8422f15bf0db32f0652ac3b3bd
AhnJoonSung <tjdtna01@naver.com>
2022-10-29 13:47:59 +0900

    split number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        System.out.println();
         return answer;
     }
+
+    public static List<Integer> splitNum(int num) {
+        int myNum = num;
+        List<Integer> result = new ArrayList<>();
+        while (myNum > 0) {
+            result.add(myNum % 10);
+            myNum /= 10;
+        }
+
+        return result;
+    }
+
+
 }
