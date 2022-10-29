f6a4cbef5ccd01c55c3018f9d55648c15ef8d9c9
dohyun <ldh19738@gmail.com>
2022-10-29 20:57:09 +0900

    Problem4 기능 요구 사항 분석을 하고 기능 구현하기 위한 solution 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String errorMessage = "[Error] 범위 초과";
+        char[] wordArray = word.toCharArray();
+
+        if (word.length()<1 || word.length()>1000) {
+            return errorMessage;
+        }
+        for (int i=0; i<wordArray.length; i++) {
+            if (wordArray[i]>='a' && wordArray[i]<='z') {
+                answer += 'z' -wordArray[i]+'a';
+            } else if (wordArray[i]>='A' && wordArray[i]<='Z') {
+                answer += 'Z' -wordArray[i]+'A';
+            }
+        }
+
         return answer;
     }
 }
