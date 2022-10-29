e26a1111db7a748135c1f41aa8c2b9e4386fab11
Invidam <hansupark0208@gmail.com>
2022-10-29 22:26:18 +0900

    refactor(Problem6): extract methods in fill fragment owners
    1. fill name fragment owners.
    2. add fragments owner each.
    3. add fragment owner each.
    4. make name fragment.
    5. check map's value is empty.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,22 +11,7 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        Map<String, List<String>> nameFragmentOwners = new HashMap<>();
-
-        for (int i = 0; i < forms.size(); ++i) {
-            String email = forms.get(i).get(0);
-            String nickname = forms.get(i).get(1);
-
-            for (int j = 0; j < nickname.length() - 1; ++j) {
-                String nicknameFragment = nickname.substring(j, j + 2);
-
-                if (nameFragmentOwners.get(nicknameFragment) == null) {
-                    nameFragmentOwners.put(nicknameFragment, new ArrayList<>());
-                }
-
-                nameFragmentOwners.get(nicknameFragment).add(email);
-            }
-        }
+        Map<String, List<String>> nameFragmentOwners = fillNameFragmentOwners(forms);
 
         for (List<String> owners : nameFragmentOwners.values()) {
             if (owners.size() == 1) {
@@ -39,7 +24,7 @@ public class Problem6 {
 
         List<String> ret = new ArrayList<>();
         for (int i = 0; i < answer.size(); i++) {
-            if (i != 0 && answer.get(i).equals(answer.get(i-1))) {
+            if (i != 0 && answer.get(i).equals(answer.get(i - 1))) {
                 continue;
             }
 
@@ -47,5 +32,41 @@ public class Problem6 {
         }
 
         return ret;
+    }
+
+    private static Map<String, List<String>> fillNameFragmentOwners(List<List<String>> forms) {
+        Map<String, List<String>> nameFragmentOwners = new HashMap<>();
+
+        for (int i = 0; i < forms.size(); ++i) {
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
+
+            addFragmentsOwner(nameFragmentOwners, email, nickname);
+        }
+        return nameFragmentOwners;
+    }
+
+    private static void addFragmentsOwner(Map<String, List<String>> nameFragmentOwners, String email, String nickname) {
+        for (int j = 0; j < nickname.length() - 1; ++j) {
+            addFragmentOwner(nameFragmentOwners, email, getNameFragment(nickname, j));
+        }
+    }
+
+    private static void addFragmentOwner(Map<String, List<String>> nameFragmentOwners, String email,
+            String nicknameFragment) {
+        if (isEmptyList(nameFragmentOwners, nicknameFragment)) {
+            nameFragmentOwners.put(nicknameFragment, new ArrayList<>());
+        }
+
+        nameFragmentOwners.get(nicknameFragment).add(email);
+    }
+
+    private static String getNameFragment(String nickname, int j) {
+        String nicknameFragment = nickname.substring(j, j + 2);
+        return nicknameFragment;
+    }
+
+    private static boolean isEmptyList(Map<String, List<String>> nameFragmentOwners, String nicknameFragment) {
+        return nameFragmentOwners.get(nicknameFragment) == null;
     }
 }
