f342b1375c19631a65fc7e7eb6be685d2b3e587a
DevJay1024 <devjay1024@gmail.com>
2022-10-29 17:30:41 +0900

    refactor: 기능별 주석 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,8 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    // 기능#4 (기능#1과 기능#3을 실행하여) 상황에 맞는 결과값을 반환
+    // 예외상황 -1, 무승부 0, 포비 승리 1, 크롱 승리 2 반환
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         if(checkInvalidInput(pobi) || checkInvalidInput(crong))
@@ -10,6 +12,10 @@ class Problem1 {
         return answer;
     }
 
+    // 기능#3 주어지는 List의 left, right에 대해 2번을 실행하고, 더 큰 수를 반환
+
+
+    // 기능#2 주어진 정수의 각 자리 숫자를 모두 더하거나 모두 곱해 가장 큰 수를 반환
     static int calculatePageScore(int pageNumber) {
         int multiple = 1;
         int summation = 0;
@@ -22,6 +28,8 @@ class Problem1 {
         return multiple > summation? multiple: summation;
     }
 
+    // 기능#1 입력값이 유효하지 않은 입력인지 확인
+    // 유효하지 않으면 false, 유효하면 true 반환
     static boolean checkInvalidInput(List<Integer> list) {
         int left = list.get(0), right = list.get(1);
 
