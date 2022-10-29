a352717393017efb7ae4c8ed4f20018a54315430
heesang <dlagmltkd12@naver.com>
2022-10-30 05:20:34 +0900

    test(problem6): 최종 기능확인 추가 테스트 구현

--- src/test/java/problem6/CrewsTest.java
+++ src/test/java/problem6/CrewsTest.java
@@ -25,6 +25,14 @@ public class CrewsTest {
         assertThat(emails).containsExactly("jason@email.com", "jm@email.com", "mj@email.com");
     }
 
+    @Test
+    void 최종기능_테스트() {
+        Crews crews = new Crews(createTestInputCrews2());
+        List<String> emails = crews.filterDuplicateCrew();
+
+        assertThat(emails).containsExactly("a@email.com", "b@email.com", "c@email.com", "d@email.com", "e@email.com");
+    }
+
     private List<List<String>> createTestInputCrews() {
         List<List<String>> crewList = new ArrayList<>();
 
@@ -33,6 +41,17 @@ public class CrewsTest {
         crewList.add(List.of("woniee@email.com", "워니"));
         crewList.add(List.of("mj@email.com", "엠제이"));
         crewList.add(List.of("nowm@email.com", "이제엠"));
+        return crewList;
+    }
+
+    private List<List<String>> createTestInputCrews2() {
+        List<List<String>> crewList = new ArrayList<>();
+
+        crewList.add(List.of("c@email.com", "제이엠"));
+        crewList.add(List.of("d@email.com", "제이"));
+        crewList.add(List.of("a@email.com", "워니엠"));
+        crewList.add(List.of("b@email.com", "엠워니"));
+        crewList.add(List.of("e@email.com", "이워니"));
         return crewList;
     }
 }
