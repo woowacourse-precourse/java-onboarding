a9d75bf2cbbea4a93627d813bafc5a52f25cbaff
wonju-dev <wonjulee.dev@gmail.com>
2022-10-29 19:07:01 +0900

    [feat]problem3: bruteforce로 해결 -> 성능 개선 필요

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String num = Integer.toString(i);
+            for (int j = 0; j < num.length(); j++) {
+                if (num.charAt(j) == '3') {
+                    count += 1;
+                }
+                if (num.charAt(j) == '6') {
+                    count += 1;
+                }
+                if (num.charAt(j) == '9') {
+                    count += 1;
+                }
+            }
+        }
+        return count;
     }
 }
