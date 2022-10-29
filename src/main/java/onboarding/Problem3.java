47d37fa5e93e4952eadac67d8a784376ccc7a121
Longstick0 <789456jang@naver.com>
2022-10-29 23:18:24 +0900

    feat: Problem3 369 판별 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,12 +9,13 @@ public class Problem3 {
         for(int i = 1; i <= number; i++) {
             answer += count369(i);
         }
+
         return answer;
     }
 
     public static int count369(int number){
         int clapCount = 0;
-        char[] array = String.valueOf(number).toCharArray();
+        char[] array = integerToCharArr(number);
 
         for (char c : array) {
             if (is369(characterToInteger(c))) {
@@ -26,7 +27,15 @@ public class Problem3 {
     }
 
     private static boolean is369(int checkNum) {
-        return true;
+        if (checkNum == 3) return true;
+        if (checkNum == 6) return true;
+        if (checkNum == 9) return true;
+
+        return false;
+    }
+
+    private static char[] integerToCharArr(int number) {
+        return String.valueOf(number).toCharArray();
     }
 
     private static int characterToInteger(char c) {
