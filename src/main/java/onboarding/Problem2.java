78a0898cd69e013af004751a5915289603691137
SeokHwan An <asok369258@naver.com>
2022-10-29 23:00:57 +0900

    feat(Problem2) : 주어진 암호문의 길이 검증 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem2 {
+
+    private static final int CRYPTOGRAM_MIN_LENGTH = 1;
+    private static final int CRYPTOGRAM_MAX_LENGTH = 1000;
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static boolean checkCryptogramLength(String cryptogram) {
+        return CRYPTOGRAM_MIN_LENGTH <= cryptogram.length()
+            && cryptogram.length() <= CRYPTOGRAM_MAX_LENGTH;
     }
 }
