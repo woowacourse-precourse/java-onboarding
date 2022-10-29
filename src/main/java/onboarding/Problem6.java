1652adcfb733c6005171ae08dabfb4d8972ab1df
49ehyeon42 <49ehyeon42@gmail.com>
2022-10-29 12:51:46 +0900

    문제6 닉네임 유효성 검사 기능 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -14,6 +14,7 @@ public class Problem6 {
     // 1. 이메일 형식에 부합하는가?
     // 2. 이메일 길이가 11자 이상 20자 미만인가?
     // 3. 신청한 이메일이 email 도메인인가?
+    // TODO 앞부분 문자 제한이 없다면 논리 오류 발생, 수정 필요
     private static boolean isValidEmail(String email) {
         if (11 <= email.length() && email.length() < 20) {
             Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+@email\\.com+$");
@@ -25,6 +26,15 @@ public class Problem6 {
 
     // 4. 닉네임이 한글인가?
     // 5. 닉네임 길이가 1자 이상 20자 미만인가?
+    // Q. 한글은 공백이 포함되는가?
+    private static boolean isValidNickname(String nickname) {
+        if (1 <= nickname.length() && nickname.length() < 20) {
+            Pattern pattern = Pattern.compile("^[ㄱ-ㅣ가-힣]+$");
+            Matcher matcher = pattern.matcher(nickname);
+            return matcher.matches();
+        }
+        return false;
+    }
 
     // 6. 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되어 있는 경우 중복으로 간주
 
