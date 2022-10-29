bd51b3036b7b08b6925031d3c1a19505c0371f6f
nswon <swnam0729@gmail.com>
2022-10-29 15:43:25 +0900

    fix(Problem7): User 객체 생성 방식 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,42 +8,23 @@ import java.util.List;
 
 public class Problem7 {
     private static Users users;
-    private static List<String> userFriend;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         init();
-        setUserFriend(friends, user);
-        setRecommendFriends(friends);
+        createUserInFriends(friends, user);
         return users.getRecommendFriendsRankingFive();
     }
 
     private static void init() {
         users = new Users();
-        userFriend = new ArrayList<>();
     }
 
-    private static void setUserFriend(List<List<String>> friends, String user) {
-        for(List<String> friend : friends) {
-            String friend1 = friend.get(0);
-            String friend2 = friend.get(1);
-
-            if(friend1.equals(user)) {
-                userFriend.add(friend2);
-            }
-            else if(friend2.equals(user)) {
-                userFriend.add(friend1);
-            }
-        }
-    }
-
-    private static void setRecommendFriends(List<List<String>> friends) {
+    private static void createUserInFriends(List<List<String>> friends, String user) {
         for(List<String> friendship : friends) {
-            for (String friend : friendship) {
-                if (userFriend.indexOf(friend) == 0) {
-                    User user = users.findUser(friendship.get(1));
-                    user.addVisits();
-                } else {
-                    User user = users.findUser(friendship.get(0));
-                    user.addVisits();
+            for(String friend : friendship) {
+                if(!friend.equals(user)) {
+                    User findUser = users.findUser(friend);
+                    findUser.addVisits();
                 }
             }
         }
