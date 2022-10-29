9f2f3f837819647e54ebeb41d0c2d1aa32d0fe6b
jamangstangs <jamangsong@gmail.com>
2022-10-30 01:30:33 +0900

    FEAT: Problem1 클래스 Controller 연결

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+import onboarding.problem1.Controller;
+import onboarding.problem1.Service;
+
 import java.util.List;
 
 class Problem1 {
+
+    private static Controller controller;
+
+    Problem1(Controller controller) {
+        this.controller = controller;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        Integer answer = controller.startGame(pobi, crong);
         return answer;
     }
 }
