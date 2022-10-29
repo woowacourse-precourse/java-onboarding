002c90e3bd0d085ee975bd1c94a5bf493ecab806
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-29 21:54:07 +0900

    feat: 공통 예외 처리 추가 (#1)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,8 +8,15 @@ class Problem1 {
 
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        // 1. 예외 상황을 처리한다.
+        if (checkInvalidPage(pobi) || checkInvalidPage(crong)) {
+            return -1;
+        }
+        return 0;
+    }
+
+    private static boolean checkInvalidPage(List<Integer> pages) {
+        return !isRightPageRange(pages.get(0), pages.get(1)) || !isValidPageFormat(pages.get(0), pages.get(1));
     }
 
     private static boolean isRightPageRange(Integer leftPage, Integer rightPage) {
