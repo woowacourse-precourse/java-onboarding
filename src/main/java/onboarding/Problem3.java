3c47f85ab2ec597b7f76e16b8030927695b61d48
ddingmin <ddingminyt@gmail.com>
2022-10-30 06:51:14 +0900

    feat(Ploblem3): 3, 6, 9 상황의 박수의 수를 구하는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int countClap(int n){
+        String temp = Integer.toString(n);
+        int count = 0;
+        for (int i = 0; i < temp.length(); i++){
+            if ((temp.charAt(i) == '3') || (temp.charAt(i) == '6') ||(temp.charAt(i) == '9')){
+                count += 1;
+            }
+        }
+        return count;
+    }
 }
