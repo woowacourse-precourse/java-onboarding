6ba0fe2d7f92f75b72f597a2d92277e0c2035c15
GraphTree <graphnodesed@gmail.com>
2022-10-29 09:28:11 +0900

    Feat: 각 자리의 숫자를 더하는 adder 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
         return answer;
     }
+
+
+    public static int adder(int value) {
+        if (value / 10 == 0) {
+            return value;
+        } else {
+            return adder(value / 10) + value % 10;
+        }
+    }
+
+
+
 }
