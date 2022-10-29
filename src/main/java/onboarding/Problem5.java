02bd94cb0ba578dd2a5c598ef830ec7632aed8dc
stk346 <stk346@gamil.com>
2022-10-29 14:06:26 +0900

    feat. PROBLEM5 구현 완료.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
-    public static int[] moneyList = new int[9];
+    public static List<Integer> moneyList = new ArrayList<>();
     public static List<Integer> divideList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+    private int doDivide(int money, int divideUnit) {
+        int quotient = money / divideUnit;
+        int remainder = money % divideUnit;
+        return remainder;
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int count = 0;
+        while (moneyList.size() != 9) {
+            int divideUnit = divideList.get(count);
+            int quotient = money / divideUnit;
+            moneyList.add(quotient);
+            int remainder = money % divideUnit;
+            money = remainder;
+            count++;
+        }
+        return moneyList;
     }
 }
