c10be96150c17791a56fafd675663871d9515206
KYBee <kyb3634@naver.com>
2022-10-30 00:09:46 +0900

    :zap: Implement multipleEachNumber Method
    Implement multipleEachNumber Method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -52,6 +52,10 @@ class Problem1 {
      */
     public static int multipleEachNumber(int page) {
         int score = 1;
+        while (page > 0) {
+            score *= page % 10;
+            page /= 10;
+        }
         return score;
     }
 }
