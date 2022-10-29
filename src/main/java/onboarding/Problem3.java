bea0bc8f2f1739cb6d83034e339e4c113524041e
Kid-Chang <pch3310@gmail.com>
2022-10-29 19:17:51 +0900

    refactor: code refactor in problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,8 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        if(number > 10000) return -1;
-        for(int i = 0; i<=number; i++){
+        if (number > 10000)
+            return -1;
+        for (int i = 0; i <= number; i++) {
             answer += count3(i);
         }
         return answer;
@@ -15,12 +16,13 @@ public class Problem3 {
      * @param value
      * @return
      */
-    public static int count3(int value){
+    public static int count3(int value) {
         int count = 0;
         char[] num = Integer.toString(value).toCharArray();
 
-        for(int i=0; i<num.length; i++) {
-            if (num[i] == '3' || num[i] == '6' || num[i] == '9') count++;
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == '3' || num[i] == '6' || num[i] == '9')
+                count++;
         }
         return count;
     }
