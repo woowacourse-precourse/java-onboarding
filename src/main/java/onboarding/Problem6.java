195d114ffac10c58cd61417cbf39f20dbf211723
math0917 <math0917@naver.com>
2022-10-29 18:56:53 +0900

    feat : trie에 추가하는 코드 생성 (나중에 refactor 요소로 Optional 도입 생각해보기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -40,6 +40,42 @@ public class Problem6 {
             }
         }
     }
+    private static void addTrie(Map<String, String> firstLayerTrie, String subString, String email) {
+        String leafNodeEmail = findSubString(firstLayerTrie, subString);
+        if (leafNodeEmail != null) {
+            resultEmailSet.add(leafNodeEmail);
+            resultEmailSet.add(email);
+        } else {
+            firstLayerTrie.put(subString, email);
+        }
 
+    }
+
+    private static String findSubString(Map<String, String> firstLayerTrie, String subString) {
+        return firstLayerTrie.get(subString);
+    }
+
+
+    private static Map<String, String> getFirstLayerTrie(String subString) {
+        return findSubMap(trie, String.valueOf(subString.charAt(0)));
+    }
+
+    private static Map<String, String> findSubMap(Map<String, Map<String, String>> trie, String subString) {
+
+        Map<String, String> subMap = trie.get(subString);
+        if (subMap == null) {
+            Map<String, String> newMap = new HashMap<>();
+            trie.put(subString, newMap);
+            return newMap;
+        }
+        return subMap;
+    }
+
+
+
+
+    private static boolean nicknameIsBiggerThanOne(String nickname) {
+        return nickname.length() >= 2;
+    }
 
 }
