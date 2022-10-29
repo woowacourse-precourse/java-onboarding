99addb7deb4b36ffa59bfb1edd21334d212103b9
good9804 <good9804@naver.com>
2022-10-30 00:18:51 +0900

    문제 6번 검사용 단어만드는 기능구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,36 +1,40 @@
 package onboarding;
 //닉네임을 2글자씩 끊어서 반복문을통해 contains 이용한후 찾으면 set 함수에 넣기
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
         List<String> duplicates=duplicates(forms);
 
         return duplicates;
     }
     public static List<String>  duplicates(List<List<String>> forms){//중복 닉네임 구하는 배열
-        Set<String> duplicatesSet = new HashSet<>();
+        Set<String> duplicatesSet = new HashSet<>();//중복제거하는 배열을위해 Set 생성
         for (int i = 0; i < forms.size();i++ ) {
             String Nick = forms.get(i).get(1);
-            String name="";
+
             for(int j=0;j<Nick.length()-1;j++){
-                name += (char) Nick.charAt(j);
-                name += (char) Nick.charAt(j+1);
+               String name=Makeword(Nick,j);
                 for(int k=0;k<forms.size();k++){
                     if(k==i){
-                        continue;
+                        continue;//자기자신제외
                     }
                     String Nickname=forms.get(k).get(1);
                 if(Nickname.contains(name)){
                     duplicatesSet.add(forms.get(k).get(0));
                 }}
-                name="";
+
             }
         }
-        List<String> duplicates=new ArrayList<>(duplicatesSet);//
+        List<String> duplicates=new ArrayList<>(duplicatesSet);
+        Collections.sort(duplicates);
         return duplicates;
+    }
+    public static String Makeword(String Nick,int index){//검사하는 단어
+        String name="";
+        name += (char) Nick.charAt(index);
+        name += (char) Nick.charAt(index+1);
+        return name;
     }
 }
