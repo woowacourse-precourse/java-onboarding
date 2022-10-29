0a9723e8ca80e2de1f66ec567acab9da62809072
JungYoonshin <sju08227@naver.com>
2022-10-29 18:34:31 +0900

    feat: #problem6 -> 이메일을 오름차순으로 정렬하고 중복을 제거하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,41 +1,60 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
+    public static HashMap<String, String> hashMap = new HashMap<>();
+    public static HashSet<String> emails = new HashSet<>();
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
+        emails = getEachListInForms(forms);
+        answer = Arrays.asList(sortEmailList(emails));
+
         return answer;
     }
 
-    private static void getEachListInForms(List<List<String>> forms) {
+    private static HashSet<String> getEachListInForms(List<List<String>> forms) {
         for (List<String> nameAndEmailList : forms) {
-            checkNameDuplication(nameAndEmailList);
+            emails = checkNameDuplication(nameAndEmailList);
         }
+        return emails;
     }
 
-    private static void checkNameDuplication(List<String> nameAndEmail) {
-        HashMap<String, String> hashMap = new HashMap<>();
-        HashSet<String> emails = new HashSet<>();
+    private static HashSet<String> checkNameDuplication(List<String> nameAndEmail) {
 
         String name = nameAndEmail.get(1);
         String email = nameAndEmail.get(0);
 
         for (int i = 0; i < name.length() - 1; i++) {
-            name = name.substring(i, i + 2);
+            String key = name.substring(i, i + 2);
             //해당키가 이미 있다면
-            if (hashMap.containsKey(name)) {
-                String duplicateEmail = hashMap.get(name);
+            if (hashMap.containsKey(key)) {
+                String duplicateEmail = hashMap.get(key);
                 emails.add(email);
                 emails.add(duplicateEmail);
             } else {
-                hashMap.put(name, email);
+                hashMap.put(key, email);
             }
         }
+        return emails;
     }
 
+    private static String[] sortEmailList(HashSet<String> emails) {
+        List<String> finalEmail = emails.stream()
+            .sorted()
+            .collect(Collectors.toList());
+
+        final String[] results = finalEmail.toArray(new String[0]);
+
+        return results;
+
+    }
 }
