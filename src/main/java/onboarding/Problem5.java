6016e285464d418d4af98f2734aecf331869cbac
Noah <shgustmd6865@naver.com>
2022-10-29 22:10:04 +0900

    Feat : 각 구간에 해당하는 돈에 대한 정보 추가
    입력받은 money 값을 나누기 위해 구간 별 돈에 대한 단위 저장하는 배열 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    private static Integer[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
         return answer;
     }
 }
