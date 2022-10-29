3f56bd3d4241e001c3ecc86c33f7a776854da66c
parkjungyoon <tbr06057@naver.com>
2022-10-29 20:07:38 +0900

    feat(PROBLEM3): 예외처리 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+
+    private final static int MIN_RANGE_NUM = 1;
+    private final static int MAX_RANGE_NUM = 10000;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static void validateRange(String input) {
+        int inputLength = input.length();
+        if (inputLength < MIN_RANGE_NUM || inputLength > MAX_RANGE_NUM) {
+            throw new IllegalArgumentException("1 이상 10000 이하인 자연수를 입력하세요.");
+        }
     }
 }
