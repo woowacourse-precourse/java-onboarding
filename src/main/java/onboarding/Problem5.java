a1e90dd93570c05559c35cf134e3cb58c02dd702
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-29 12:29:51 +0900

    feat:Add a function that divide money

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,61 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+
+    //큰 단위의 돈 순서로 금액을 변환하는 함수
+    public static int divideMoney(int money, List<Integer> answer) {
+        int count;
+        if (money >= 50000) {
+            count = money / 50000;
+            answer.add(count);
+            money -= 50000 * count;
+            return money;
+        }
+        if (money >= 10000) {
+            count = money / 10000;
+            answer.add(count);
+            money -= 10000 * count;
+            return money;
+        }
+        if (money >= 5000) {
+            answer.add(1);
+            money -= 5000;
+            return money;
+        }
+        if (money >= 1000) {
+            count = money / 1000;
+            answer.add(count);
+            money -= 1000 * count;
+            return money;
+        }
+        if (money >= 500) {
+            answer.add(1);
+            money -= 500;
+            return money;
+        }
+        if (money >= 100) {
+            count = money / 100;
+            answer.add(count);
+            money -= 100 * count;
+            return money;
+        }
+        if (money >= 50) {
+            answer.add(1);
+            money -= 50;
+            return money;
+        }
+        if (money >= 10) {
+            count = money / 10;
+            answer.add(count);
+            money -= 10 * count;
+            return money;
+        }
+
+        answer.add(money);
+        money -= money;
+        return money;
+
     }
 }
