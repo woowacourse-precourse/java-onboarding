0d78c8efd72e975475579fb5215b22eabc5b7579
Go-Jaecheol <gojaech@naver.com>
2022-10-29 15:23:30 +0900

    feat: Problem1 - [홀수, 짝수] 형태의 입력이 맞는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,16 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!validateInputFormat(pobi) || !validateInputFormat(crong)) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static boolean validateInputFormat(List<Integer> numList) {
+        Integer leftNum = numList.get(0);
+        Integer rightNum = numList.get(1);
+        return (leftNum % 2) == 1 && (rightNum % 2) == 0;
     }
 }
