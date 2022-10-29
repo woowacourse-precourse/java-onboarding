c0b4bf47fb30964556084c09b90d230d52c36df0
DDing77 <devseo2021@gmail.com>
2022-10-30 07:27:42 +0900

    [Problem1] feat : 예외처리 기능 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,8 +9,26 @@ import java.util.List;
   4. 각 자리 숫자를 모두 곱하기  */
 
 class Problem1 {
+
+    // 입력 예외처리
+    public static void inputException(List<Integer> pobi, List<Integer> crong) throws Exception {
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) throw new Exception();
+        if (pobi.get(0) < 0 || pobi.get(1) > 400) throw new Exception();
+        if (crong.get(0) < 0 || crong.get(1) > 400) throw new Exception();
+        if (pobi.get(0) % 2 != 1 || crong.get(0) % 2 != 1) throw new Exception();
+        if (pobi.get(1) % 2 != 0 || crong.get(1) % 2 != 0) throw new Exception();
+        if (pobi.size() != 2 || crong.size() != 2) throw new Exception();
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = -1;
+
+        try {
+            inputException(pobi, crong);
+        } catch (Exception e) {
+            return answer;
+        }
+        
         return answer;
     }
 }
