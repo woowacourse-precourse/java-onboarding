d2569b94dd162f44fecf47728079f47d7440afc5
Heesom <heesom.hs@gmail.com>
2022-10-29 20:42:22 +0900

    Problem4 : reverseFrog 메소드 추가
    청개구리 문자열을 HashMap에 저장해 리턴하는 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
+    }
+
+    public static Map<Character,Character> reverseFrog(){
+        Map<Character,Character> frogMap = new HashMap<>();
+        //A=65 a=97
+        int count = 0;
+        for(int i=65;i<=90;i++){
+            //대문자
+            frogMap.put( (char)i ,(char)(i-count) );
+            //소문자
+            int low = i+32;
+            frogMap.put( (char)low, (char)(low-2) );
+
+            count+=2;
+        }
+        return frogMap;
     }
 }
