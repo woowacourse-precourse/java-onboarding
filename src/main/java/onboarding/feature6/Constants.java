f20cdcb06089db9e00ea6182f15eed9aaad7bfb2
areyouhun <areyouhun@gmail.com>
2022-10-30 01:31:30 +0900

    feat(prob6): constants for regex and index values

---
+++ src/main/java/onboarding/feature6/Constants.java
@@ -0,0 +1,8 @@
+package onboarding.feature6;
+
+public class Constants {
+    public static final String EMAIL_REGEX = "^\\w+[-\\w]@email.com$";
+
+    public static final int EMAIL_INDEX = 0;
+    public static final int NICKNAME_INDEX = 1;
+}
