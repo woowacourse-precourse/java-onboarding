3fb8e509a34129ebbdcca8a11b692d36317533a9
K-Diger <kitten.diger@gmail.com>
2022-10-30 02:05:06 +0900

    fix : static 변수 -> Local 변수로 사용하도록 변경 - static 변수는 리소스 낭비가 있을 수 있으므로 로컬 변수(purse)로 수정 - divideMoney() 메서드 추가 -> 총 금액으로부터 분리할 수 있는 화폐 종류 카운트

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,27 +5,16 @@ import java.util.List;
 
 public class Problem5 {
 
-    // [오만원, 만원, 오천원, 천원, 오백원, 백원, 오십원, 십원, 일원]
-    static List<Integer> purse = new ArrayList<>() {{
-        add(50000);
-        add(10000);
-        add(5000);
-        add(1000);
-        add(500);
-        add(100);
-        add(50);
-        add(10);
-        add(1);
-    }};
 
-    public static List<Integer> divideMoney(int money) {
+    public static List<Integer> divideMoney(List<Integer> purse, int money) {
         int remainMount = money;
 
         List<Integer> result = new ArrayList<>(purse.size());
 
         for (int i = 0; i < purse.size(); i++) {
-            // 결과 값 배열에 각 인덱스를 넣음
+            // 결과 값 배열에 각 인덱스를 넣는다.
             result.add(remainMount / purse.get(i));
+
             // 배열에 넣은 후 나머지 현재 남은 돈에 반영
             remainMount %= purse.get(i);
         }
@@ -33,6 +22,20 @@ public class Problem5 {
     }
 
     public static List<Integer> solution(int money) {
-        return divideMoney(money);
+        // 내 지갑 정보
+        // [오만원, 만원, 오천원, 천원, 오백원, 백원, 오십원, 십원, 일원]
+        List<Integer> purse = new ArrayList<>() {{
+            add(50000);
+            add(10000);
+            add(5000);
+            add(1000);
+            add(500);
+            add(100);
+            add(50);
+            add(10);
+            add(1);
+        }};
+
+        return divideMoney(purse, money);
     }
 }
