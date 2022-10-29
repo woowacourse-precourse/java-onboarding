8d1b52ffc8f358c94499e3131e5c51575b3b9862
Isu Shin <isu010614@naver.com>
2022-10-29 15:20:44 +0900

    feat: Add function to inqueue list by bill unit
    Declare bills by unit & Add inqueue function

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,32 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Problem5 {
+    static final int FIVE_MILLION_WON = 50000;
+    static final int ONE_MILLION_WON = 10000;
+    static final int FIVE_THOUSAND_WON = 5000;
+    static final int ONE_THOUSAND_WON = 1000;
+    static final int FIVE_HUNDRED_WON = 500;
+    static final int ONE_HUNDRED_WON = 100;
+    static final int FIFTY_WON = 50;
+    static final int TEN_WON = 10;
+    static final int ONE_WON = 1;
+    static Queue<Integer> billUnitQueue = new LinkedList<>();
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void addBillUnitQueue() {
+        List<Integer> billList
+                = List.of(FIVE_MILLION_WON, ONE_MILLION_WON,
+                FIVE_THOUSAND_WON, ONE_THOUSAND_WON, FIVE_HUNDRED_WON,
+                ONE_HUNDRED_WON, FIFTY_WON, TEN_WON, ONE_WON);
+        billUnitQueue.addAll(billList);
     }
 }
