a144aba2a82fee6d5f688b9251f49e9341cdfdef
saehim <saeilee.vxv@gmail.com>
2022-10-30 00:21:05 +0900

    Feat(Problem4): 소문자 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+    // 소문자 변환
+    static char lowercase(char a){
+        char answer = (char)(219 - a);
         return answer;
     }
 }
