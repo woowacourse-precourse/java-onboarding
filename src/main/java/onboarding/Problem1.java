7619f51afd0e6424eb9ace9f587e5e2ddc9a6c29
ddingmin <ddingminyt@gmail.com>
2022-10-30 05:05:29 +0900

    feat(Ploblem1): 자신의 최고 점수를 구하는 기능 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int pobiScore, crongScore;
+
         if (!checkPages(pobi) || !checkPages(crong)){
             return -1;
         }
+
+        pobiScore = getScore(pobi);
+        crongScore = getScore(crong);
+
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
@@ -26,5 +33,25 @@ class Problem1 {
     private static Boolean isCorrectPage(int page){
         return (page > 2) && (page < 399);
     }
+
+    private static int getScore(List<Integer> pages){
+        return getMax(sumScore(pages), mulScore(pages));
+    }
+
+    private static int getMax(int a, int b){
+        if (a > b){
+            return a;
+        }
+        return b;
+    }
+
+    private static int sumScore(List<Integer> pages){
+        return pages.get(0) + pages.get(1);
+    }
+
+    private static int mulScore(List<Integer> pages){
+        return pages.get(0) * pages.get(1);
+    }
+
 
 }
