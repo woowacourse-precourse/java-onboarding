3f03279792497fb0992169e1e7982b4a3d6396da
Invidam <hansupark0208@gmail.com>
2022-10-29 13:15:12 +0900

    feat(Problem1): add method get page max point
    add method that get page max point.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import static java.lang.Integer.max;
+
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    public static int getPageMaxPoint(int page) {
+
+        List<Integer> digits = new ArrayList<>();
+
+        while(page > 0) {
+            digits.add(page % 10);
+            page /= 10;
+        }
+
+        int cand1 = 0;
+        for(int digit: digits)
+            cand1 += digit;
+
+        int cand2 = 1;
+        for(int digit: digits)
+            cand2 *= digit;
+
+        return max(cand1,cand2);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
