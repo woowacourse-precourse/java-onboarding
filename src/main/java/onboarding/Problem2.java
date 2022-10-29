efd0efeda06da2797d52b2a9d22804f01f064b5f
joohyeongKim <wnguddl96@knou.ac.kr>
2022-10-29 20:14:57 +0900

    뇌 정지..
    Signed-off-by: joohyeongKim <wnguddl96@knou.ac.kr>

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,11 +2,17 @@ package onboarding;
 
 import java.util.*;
 
+import static onboarding.problem2.Saver.saveString;
+
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
 
+        HashSet<String> hashSet = new HashSet<>();
+        for (String s : hashSet) {
+            
+        }
 
         return answer;
     }
