b67b41460fcb606141ae22215934bcec627339ec
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-30 00:23:17 +0900

    Add checkSingleDigitNumber function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,11 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    // 주어진 한 자리수의 값이 3, 6, 9인지 확인하는 함수
+    public static int checkSingleDigitNumber(int number) {
+        int value = 0;
+        if (number == 3 || number == 6 || number == 9) value = 1;
+        return value;
+    }
 }
