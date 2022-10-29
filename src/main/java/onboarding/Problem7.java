fb711eb831ef7839c360c284e133a0fe0f7d3cfa
HaODay <ycis5013@naver.com>
2022-10-29 22:06:23 +0900

    [Problem7] 친구 List와 추천 친구 후보 Map 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,36 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        answer=new ArrayList<>();
+        final int LENGTH=friends.size();
+
+        //사용자의 친구 리스트 생성
+        ArrayList list=new ArrayList();
+
+        for(int i=0;i<LENGTH;i++) {
+            if (friends.get(i).get(0).equals(user)) {
+                list.add(friends.get(i).get(1));
+            }else if (friends.get(i).get(1).equals(user)) {
+                list.add(friends.get(i).get(0));
+            }
+        }
+        //추천 친구 후보 Map 생성
+        HashMap map=new HashMap();
+        //Map 초기화
+        for(int i=0;i<LENGTH;i++){
+            map.put(friends.get(i).get(0),0);
+            map.put(friends.get(i).get(1),0);
+        }
+        for(int i=0;i<visitors.size();i++){
+            map.put(visitors.get(i),0);
+        }
+
         return answer;
     }
 }
