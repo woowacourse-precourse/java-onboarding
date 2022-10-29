8a52ce3a198194b74bb8e7913d58b3b33ac60879
Park Sungchul <try615@naver.com>
2022-10-29 21:17:48 +0900

    [feat] Problem4.java : 코딩테스트 형식으로 프로토타입 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+    public static void main(String[] args) {
+        String word = "I love you";
+        System.out.println(solution(word));
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        int strLen = word.length();
+
+        for (int i = 0; i < strLen; i++) {
+            Character c = word.charAt(i);
+
+            if ((int) c >= 65 && (int) c <= 90) {
+                sb.append((char)(90 - ((int) c - 65)));
+            } else if ((int) c >= 97 && (int) c <= 122) {
+                sb.append((char) (122 - ((int) c - 97)));
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 }
