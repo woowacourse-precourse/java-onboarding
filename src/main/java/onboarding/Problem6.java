670fca7d06d5bd9640181a9f1c12a084601fadd1
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-10-30 01:06:24 +0900

    [feat] Problem6: 중복 닉네임 유저 이메일 전송리스트 리턴 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        Set<String> sendEmailList = new HashSet<>();
+        int size = forms.size();
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                String myNicName = forms.get(i).get(1);
+                String compareNicName = forms.get(j).get(1);
 
+                if(isDuplicateNicName(myNicName, compareNicName)) {
+                    sendEmailList.add(forms.get(i).get(0));
+                    sendEmailList.add(forms.get(j).get(0));
+                    break;
+                }
+            }
+        }
+
+        return sendEmailList.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     private static boolean isDuplicateNicName(String name, String compare) {
