c329c80dd2f47ac6a460a6a1b19462d661299544
JJongmen <pjy90123@gmail.com>
2022-10-29 16:25:46 +0900

    feat(Problem7): 타임라인에 방문한 횟수에 따른 점수를 주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,13 +6,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Map<String, List<String>> friendship = getFriendshipByFriends(friends);
-        Map<String, Integer> scores = new HashMap<>();
-        for (String id : friendship.keySet()) {
-            if (user.equals(id)) {
-                continue;
-            }
-            scores.put(id, 10 * countSameFriend(user, id, friendship));
-        }
+        Map<String, Integer> scores = calculateScores(user, visitors, friendship);
         return answer;
     }
 
@@ -29,13 +23,43 @@ public class Problem7 {
         return friendship;
     }
 
-    private static int countSameFriend(String user, String id, Map<String, List<String>> friendship) {
+    private static Map<String, Integer> calculateScores(String user, List<String> visitors, Map<String, List<String>> friendship) {
+        Map<String, Integer> scores = new HashMap<>();
+        addScoresByFriendship(user, friendship, scores);
+        addScoresByVisitors(user, visitors, friendship, scores);
+        return scores;
+    }
+
+    private static Map<String, Integer> addScoresByFriendship(String user, Map<String, List<String>> friendship, Map<String, Integer> scores) {
+        for (String id : friendship.keySet()) {
+            if (user.equals(id) || isFriend(user, id, friendship)) {
+                continue;
+            }
+            scores.put(id, 10 * countSameFriend(user, id, friendship));
+        }
+        return scores;
+    }
+
+    private static int countSameFriend(String user, String otherUser, Map<String, List<String>> friendship) {
         int sameFriendCnt = 0;
-        for (String friend : friendship.get(id)) {
-            if (friendship.get(user).contains(friend)) {
+        for (String friend : friendship.get(otherUser)) {
+            if (isFriend(user, friend, friendship)) {
                 sameFriendCnt++;
             }
         }
         return sameFriendCnt;
+    }
+
+    private static void addScoresByVisitors(String user, List<String> visitors, Map<String, List<String>> friendship, Map<String, Integer> scores) {
+        for (String visitor : visitors) {
+            if (isFriend(user, visitor, friendship)) {
+                continue;
+            }
+            scores.put(visitor, scores.getOrDefault(visitor, 0) + 1);
+        }
+    }
+
+    private static boolean isFriend(String user, String otherUser, Map<String, List<String>> friendship) {
+        return friendship.get(user).contains(otherUser);
     }
 }
