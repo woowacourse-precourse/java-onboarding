0846cc1e574f56c0afcfb2d0b18caa7647e2bbc1
jagajang <kosb7544@gmail.com>
2022-10-29 13:53:14 +0900

    p4/add linear check per character

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(char c : word.toCharArray())
+            answer += decode(c);
+
         return answer;
+    }
+
+    private static char decode(char c) {
+        return '0';
     }
 }
