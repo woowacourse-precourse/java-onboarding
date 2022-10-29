82df44d5765649cb47754440e003e188c72395c7
KYR <freekyr7529@gmail.com>
2022-10-29 16:37:21 +0900

    docs (Problem2) : 2번 문제 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,8 +4,14 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
-
+        answer = decryption(cryptogram);
 
         return answer;
+    }
+
+    static String decryption(String ) {
+        String plaintext = "";
+
+        return plaintext;
     }
 }
