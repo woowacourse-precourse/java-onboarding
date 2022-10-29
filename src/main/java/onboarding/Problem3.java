02c9ae44aaebf1e7a8c61191458a51fb635893ff
leeyongjun <leeyongjun0310@naver.com>
2022-10-29 16:35:10 +0900

    feat(PROBLEM3): 3, 6, 9가 있는지 확인해주는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,10 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static boolean isIn369(String string){
+        // 3, 6, 9가 있는지 확인해주는 메소드
+        int number = Integer.parseInt(string);
+        return number == 3 || number == 6 || number == 9;
+    }
 }
