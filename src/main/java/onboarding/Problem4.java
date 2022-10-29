ffa500e8368c30da57b3f47d36ddeb041d148ee7
kkm5291 <kkm23125291@gmail.com>
2022-10-29 15:46:03 +0900

    Problem 4 Feat : 알파벳 배열 생성 메서드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,21 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static void makeArr(char[] arr) {
+        char[] alphArr = new char[26];
+        char[] reverseArr = new char[26]; // 기존 array에서 숫자를 빼면 다시 재사용이 가능할 것 같지만 일단 보류
+
+        for (int i = 0; i < alphArr.length; i++) {
+            alphArr[i] = (char)('a' + i);
+        }
+
+        for (int i = reverseArr.length - 1; i >= 0; i--) {
+            reverseArr[i] = (char) ('z' - i);
+        }
+    }
+
+
+
+
 }
