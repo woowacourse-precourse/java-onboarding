0c1afbf917abf7a180ec2773b00fb134bd14db4e
syhan7516 <syhan7516@naver.com>
2022-10-30 03:00:41 +0900

    Problem3 - 모든 요구 사항 체크

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -39,9 +39,6 @@ public class Problem3 {
             curNumber += 1;
         }
 
-        // 박수 수 결과 확인
-        System.out.println(clapCount);
-
         // 총 박수 수
         answer = clapCount;
 
@@ -50,15 +47,15 @@ public class Problem3 {
 }
 
 // 기능 요구 사항
-// 1. 숫자 한 자리씩 3,6,9인지를 확인하기
-// 2. 1번 작업 반복하며, 박수 더하기
+// ✔ 1. 숫자 한 자리씩 3,6,9인지를 확인하기
+// ✔ 2. 1번 작업 반복하며, 박수 더하기
 
 // 프로그래밍 요구 사항
 // ✔ 1. JDK 11 버전
-// 2. "build.gradle"을 변경 X, 외부 라이브러리 X
-// 3. 프로그램 종료 시 "System.exit()"를 호출 X
-// 4. 프로그램 구현이 완료되면 "ApplicationTest" 의 모든 테스트 성공하기
-// 5. 프로그래밍 요구 사항에서 달리 명시하지 않은 한 파일, 패키지 이름을 수정하거나 이동 X
+// ✔ 2. "build.gradle"을 변경 X, 외부 라이브러리 X
+// ✔ 3. 프로그램 종료 시 "System.exit()"를 호출 X
+// ✔ 4. 프로그램 구현이 완료되면 "ApplicationTest" 의 모든 테스트 성공하기
+// ✔ 5. 프로그래밍 요구 사항에서 달리 명시하지 않은 한 파일, 패키지 이름을 수정하거나 이동 X
 
 // 과제 진행 요구 사항
 // ✔ 1. 미션은 "java-onboarding" 저장소를 "Fork & Clone" 해 시작하기
