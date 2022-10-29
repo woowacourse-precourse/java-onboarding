a5b21441bcdb3e561e9f2b5257ac7b05c562b7aa
CordHouse <qkqktl5310@gmail.com>
2022-10-30 00:23:47 +0900

    refactor : Problem1 예외처리 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,6 +17,13 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // Step 1. 예외 케이스 처리
+        if(pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1
+                || pobi.get(0) == 1 || crong.get(1) == 400){
+            return -1;
+        }
+
         return answer;
     }
 }
