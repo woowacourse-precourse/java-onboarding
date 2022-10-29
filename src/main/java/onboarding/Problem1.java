b0a25005c641eb6fba5b1442134ef1186fa79bd4
Heesom <heesom.hs@gmail.com>
2022-10-29 10:20:10 +0900

    Problem1 : exception 메소드 추가
    페이지가 연속으로 나올 경우, 1 혹은 400이 나올 경우 예외로 취급하고 -1을 리턴하는 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,20 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int exception(List<Integer> pobi, List<Integer> crong) {
+        //페이지 연속
+        int pageDiffpobi = Math.abs(pobi.get(0) - pobi.get(1));
+        int pageDiffcrong = Math.abs(crong.get(0) - crong.get(1));
+        if(pageDiffcrong != 1 || pageDiffpobi != 1){
+            return -1;
+        }
+
+        //1 이나 400 나오면
+        if((pobi.get(0)==1) || (crong.get(0)==1)) {return -1;}
+        if((pobi.get(1)==400) || (crong.get(1)==400)) {return -1;}
+
+
+        return 0;
+    }
 }
