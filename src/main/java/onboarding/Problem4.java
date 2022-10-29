a305fe85e05f1fea6080988d0792115d1bbf4386
Eleisia <blackcube222@gmail.com>
2022-10-30 02:36:01 +0900

    4번 문제 1번기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,4 +9,17 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    static char[] translation(String word) {
+        char[] charList = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            if((word.charAt(i) >= 'A' && word.charAt(i) <= 'Z'))
+                charList[i] = (char)('Z' - (word.charAt(i) - 'A'));
+            else if(word.charAt(i) >= 'a' && word.charAt(i) <= 'z')
+                charList[i] = (char)('z' - (word.charAt(i) - 'a'));
+            else
+                charList[i] = word.charAt(i);
+        }
+        return charList;
+    }
 }
