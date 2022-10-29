d28f7dd345fa20c5abbec311164c85821e0cda2a
Baejw0111 <bjw0111@naver.com>
2022-10-29 17:10:14 +0900

    Problem 3 complete & code reshape

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,78 +3,75 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    //x의 각 자리수들을 모두 더한 값을 반환
-    int Sum(int x){
-        int ret=0;
+    // x의 각 자리수들을 모두 더한 값을 반환
+    int Sum(int x) {
+        int ret = 0;
 
-        while(x!=0){
-            ret+=x%10;
-            x/=10;
+        while (x != 0) {
+            ret += x % 10;
+            x /= 10;
         }
 
         return ret;
     }
 
-    //x의 각 자리수들을 모두 곱한 값을 반환
-    int Mul(int x){
-        int ret=1;
+    // x의 각 자리수들을 모두 곱한 값을 반환
+    int Mul(int x) {
+        int ret = 1;
 
-        while(x!=0){
-            ret*=x%10;
-            x/=10;
+        while (x != 0) {
+            ret *= x % 10;
+            x /= 10;
         }
 
         return ret;
     }
 
-    //예외 처리 메서드
-    boolean exception(List<Integer> page){
-        int leftPage=page.get(0),rightPage=page.get(1);
-        if(leftPage>rightPage || rightPage-leftPage!=1 || leftPage%2!=1 || rightPage%2!=0){
+    // 예외 처리 메서드
+    boolean exception(List<Integer> page) {
+        int leftPage = page.get(0), rightPage = page.get(1);
+        if (leftPage > rightPage || rightPage - leftPage != 1 || leftPage % 2 != 1 || rightPage % 2 != 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        Problem1 prob=new Problem1();
+        Problem1 prob = new Problem1();
         int answer = Integer.MAX_VALUE;
 
-        if(prob.exception(pobi) || prob.exception(crong)){
+        if (prob.exception(pobi) || prob.exception(crong)) {
             return -1;
         }
 
-        //포비 점수 계산
-        int pobiLeftSum=prob.Sum(pobi.get(0)),pobiLeftMul=prob.Mul(pobi.get(0));
-        int pobiLeftMax=Math.max(pobiLeftSum,pobiLeftMul);
+        // 포비 점수 계산
+        int pobiLeftSum = prob.Sum(pobi.get(0)), pobiLeftMul = prob.Mul(pobi.get(0));
+        int pobiLeftMax = Math.max(pobiLeftSum, pobiLeftMul);
 
-        int pobiRightSum=prob.Sum(pobi.get(1)),pobiRightMul=prob.Mul(pobi.get(1));
-        int pobiRightMax=Math.max(pobiRightSum,pobiRightMul);
+        int pobiRightSum = prob.Sum(pobi.get(1)), pobiRightMul = prob.Mul(pobi.get(1));
+        int pobiRightMax = Math.max(pobiRightSum, pobiRightMul);
 
-        int pobiMax=Math.max(pobiLeftMax,pobiRightMax);
+        int pobiMax = Math.max(pobiLeftMax, pobiRightMax);
 
-        //크롱 점수 계산
-        int crongLeftSum=prob.Sum(crong.get(0)),crongLeftMul=prob.Mul(crong.get(0));
-        int crongLeftMax=Math.max(crongLeftSum,crongLeftMul);
+        // 크롱 점수 계산
+        int crongLeftSum = prob.Sum(crong.get(0)), crongLeftMul = prob.Mul(crong.get(0));
+        int crongLeftMax = Math.max(crongLeftSum, crongLeftMul);
 
-        int crongRightSum=prob.Sum(crong.get(1)),crongRightMul=prob.Mul(crong.get(1));
-        int crongRightMax=Math.max(crongRightSum,crongRightMul);
+        int crongRightSum = prob.Sum(crong.get(1)), crongRightMul = prob.Mul(crong.get(1));
+        int crongRightMax = Math.max(crongRightSum, crongRightMul);
 
-        int crongMax=Math.max(crongLeftMax,crongRightMax);
+        int crongMax = Math.max(crongLeftMax, crongRightMax);
 
-        //승패 결정
-        if(pobiMax>crongMax){
-            answer=1;
+        // 승패 결정
+        if (pobiMax > crongMax) {
+            answer = 1;
+        } else if (pobiMax < crongMax) {
+            answer = 2;
+        } else {
+            answer = 0;
         }
-        else if(pobiMax<crongMax){
-            answer=2;
-        }
-        else{
-            answer=0;
-        }
-        
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,18 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        for(int i=0;i<cryptogram.length()-1;++i){
-            if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
-                //연속하는 중복 문자열 제거
-                char[] targetArray={cryptogram.charAt(i),cryptogram.charAt(i+1)};
-                String targetString=new String(targetArray);
-                cryptogram = cryptogram.replace(targetString, ""); 
+        for (int i = 0; i < cryptogram.length() - 1; ++i) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                // 연속하는 중복 문자열 제거
+                char[] targetArray = { cryptogram.charAt(i), cryptogram.charAt(i + 1) };
+                String targetString = new String(targetArray);
+                cryptogram = cryptogram.replace(targetString, "");
 
-                //중복한 문자 삭제 후 그 이전 문자부터 다시 검사
-                if(i!=0){
-                    i-=2;
-                }
-                else{
+                // 중복한 문자 삭제 후 그 이전 문자부터 다시 검사
+                if (i != 0) {
+                    i -= 2;
+                } else {
                     --i;
                 }
             }
