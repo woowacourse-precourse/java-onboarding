6c2c0af263356eeb9ed7cfa7c869c0810ef074f4
JUWON LEE <leeez0128@gmail.com>
2022-10-29 23:36:17 +0900

    [Solving] Problem1 -포비와 크롱의 점수를 비교해 높은 사람을 승자로 결정한다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = whoIsWinner(pobi, crong);
         return answer;
+    }
+
+    private static int whoIsWinner(List<Integer> pobi, List<Integer> crong) {
+        List<Integer> pobiPageScores = new ArrayList<>();
+        pobiPageScores.add(getPageScore(pobi.get(0)));
+        pobiPageScores.add(getPageScore(pobi.get(1))); 
+
+        List<Integer> crongPageScores = new ArrayList<>();
+        crongPageScores.add(getPageScore(crong.get(0)));
+        crongPageScores.add(getPageScore(crong.get(1))); 
+
+        if(pobiPageScores.contains(-1) || crongPageScores.contains(-1)) {
+            return -1;
+        }
+
+        int pobiScore = (pobiPageScores.get(0) > pobiPageScores.get(1)) ? pobiPageScores.get(0) : pobiPageScores.get(1);
+        int crongScore = (crongPageScores.get(0) > crongPageScores.get(1)) ? crongPageScores.get(0) : crongPageScores.get(1);
+
+        return (pobiScore > crongScore) ? 1 : (pobiScore == crongScore) ? 0 : 2;
     }
 }
