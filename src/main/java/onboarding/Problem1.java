a8dc1792c1b6234f412c9bb60809b655bc8163fa
hhiyeon <hhyyeon0214@gmail.com>
2022-10-29 15:56:18 +0900

    feat: p1-check page number range

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,7 @@
 package onboarding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Problem1 {
     private static final int POBI_WIN = 1;
@@ -9,11 +10,17 @@ class Problem1 {
     private static final int EXCEPTION = -1;
     private static final int LEFT_PAGE = 0;
     private static final int RIGHT_PAGE = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
         return answer;
     }
+
+    private static boolean isStartOrEndPageNum(int pageNum) {
+        return pageNum == 400 || pageNum == 1;
+    }
+
     private static boolean oddEvenCheck(int leftPage, int rightPage) {
         return isLeftOdd(leftPage) && isRightEven(rightPage);
     }
@@ -25,16 +32,20 @@ class Problem1 {
     private static boolean isRightEven(int rightPage) {
         return rightPage % 2 == 0;
     }
+
     private static int getMaxScore(int pageNumber) {
         List<Integer> splitNumber = splitDigitNumber(pageNumber);
         return Math.max(getPlusScore(splitNumber), getMultiplyScore(splitNumber));
     }
+
     private static int getPlusScore(List<Integer> splitNumber) {
         return splitNumber.stream().mapToInt(i -> i).sum();
     }
+
     private static int getMultiplyScore(List<Integer> splitNumber) {
         return splitNumber.stream().reduce(1, (a, b) -> a * b);
     }
+
     private static List<Integer> splitDigitNumber(int pageNumber) {
         List<Integer> splitNumber = new ArrayList<>();
         while (pageNumber > 0) {
@@ -43,6 +54,7 @@ class Problem1 {
         }
         return splitNumber;
     }
+
     private static int getWinner(int pobiMaxScore, int crongMaxScore) {
         if (pobiMaxScore < crongMaxScore) {
             return CRONG_WIN;
