730462a95e492f37df6c20446fba16ee9f1b1087
JunTaeINC <juntea3523@naver.com>
2022-10-29 09:32:50 +0900

    Problem 2 - 단어길이 확인 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem2 {
+	
+	public static String chkOrDelStr(String str) {
+		// 단어의 길이 0~1인지 확인  
+		if (str.length() == 0 || str.length() == 1) {
+			return str;
+		// 2이상은 중복이 있을수도있으므로 중복 검사   
+		} else {
+	
+		}
+	
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
