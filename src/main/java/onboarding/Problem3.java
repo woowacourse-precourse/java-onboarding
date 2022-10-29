85cfe8bd34f624f75d69e607608248cfb22c67a4
ensk26 <tjensk26@gmail.com>
2022-10-30 01:57:25 +0900

    docs: 기능 목록 구성, problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+
         int answer = 0;
         return answer;
     }
+
+    //한 숫자에 대해 손벽을 치는 횟수 반환
+    static int getClapCount(int number) {
+
+        int cnt = 0;
+        return cnt;
+    }
+
 }
