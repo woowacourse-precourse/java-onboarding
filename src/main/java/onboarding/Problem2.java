9914c21ae18ab5df23b578a3cd7bd7581bc33fe1
jiwon <ljwon77@daum.net>
2022-10-29 20:24:31 +0900

    refactor(problem2): extract method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,10 +8,21 @@ public class Problem2 {
         // 배열을 순회하며 해당 원소를 기준으로 중복 문자열을 체크한다.
         for(Character element : cryptogramArr) {
             // 해당 문자가 2번 이상 반복된다면 제거하여 원본 암호문을 변경한다.
-            cryptogram = cryptogram.replaceAll(element + "{2,}", "");
+            cryptogram = removeDuplicateElement(cryptogram, element);
         }
 
         return cryptogram;
+    }
+
+    /**
+     * 2번 이상 중복된 문자열을 제거한다.
+     *
+     * @param cryptogram 제거 진행할 문자열
+     * @param element 제거의 기준이 되는 요소
+     * @return String - 제거 작업이 완료된 문자열을 리턴한다.
+     */
+    private static String removeDuplicateElement(String cryptogram, Character element) {
+        return cryptogram.replaceAll(element + "{2,}", "");
     }
 
     /**
