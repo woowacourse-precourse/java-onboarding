f55bceccaa3c6e2d33da9d5c0399f231cf8db076
Kid-Chang <pch3310@gmail.com>
2022-10-29 22:26:58 +0900

    refactor: add JAVADOC in problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -20,8 +20,8 @@ public class Problem4 {
 
     /**
      * 청개구리의 방식대로 A->z, b->Y 가 출력되도록 하는 함수
-     * @param word
-     * @return
+     * @param word 입력된 단어
+     * @return 청개구리의 방식으로 변경된 단어
      */
     public static String reverseString(String word) {
         StringBuilder reverseWord = new StringBuilder();
