ddb33778ead065dc7268e545048019bad9d9cdbb
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-29 23:36:47 +0900

    Add deleteReduplicationString function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,16 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    // 반복문을 통해 문자를 만드는 함수
+    public static String deleteReduplicationString(String cryptogram) {
+        String resultString = cryptogram.substring(0, 1);
+        for (int i = 1; i < cryptogram.length(); i++) {
+            String lastChar = resultString.substring(resultString.length()-1, resultString.length());
+            String nextChar = cryptogram.substring(i, i+1);
+            if (checkReduplication(lastChar, nextChar) continue;
+            resultString += nextChar;
+        }
+        return resultString;
+    }
 }
