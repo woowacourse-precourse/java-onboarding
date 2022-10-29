fed521e072e1c86bdbfd64ef59439ec054b7f09c
김현재 <41482946+galug@users.noreply.github.com>
2022-10-29 12:13:56 +0900

    feat(clapgame): Add 1부터 number까지의 손뼉 개수를 계산하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import onboarding.problem3.ClapGame;
+
 public class Problem3 {
     public static int solution(int number) {
         if (!validateGameNumber(number)) {
             return -1;
         }
-        int answer = 0;
+        int answer = ClapGame.countNumberOfClap(number);
         return answer;
     }
 
--- src/main/java/onboarding/problem3/ClapGame.java
+++ src/main/java/onboarding/problem3/ClapGame.java
@@ -1,6 +1,14 @@
 package onboarding.problem3;
 
 public class ClapGame {
+    public static int countNumberOfClap(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            count += countIncluding369(i);
+        }
+        return count;
+    }
+
     private static int countIncluding369(int number) {
         int counting = 0;
         while (number > 0) {
