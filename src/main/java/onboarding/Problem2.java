f46bf0ac3b90077df06e2d60c0c7a19cefd139ea
iamjooon2 <iamjooon2@gmail.com>
2022-10-29 15:14:41 +0900

    feat(problem2): 스택이 비어있는지 확인하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,11 +1,16 @@
 package onboarding;
-
 import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         Stack<Character> stack = new Stack<>();
+
+        for (char c : cryptogram.toCharArray()) {
+            if (!stack.isEmpty()) {
+
+            }
+        }
 
         return answer;
     }
