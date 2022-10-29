4bc5d00846275cec60088acfef7843b5f82e3539
jiwon <ljwon77@daum.net>
2022-10-29 21:08:29 +0900

    feat(problem3): loop until number and change string, get length

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    static final String[] numForClaps = {"3", "6", "9"};
+
     public static int solution(int number) {
         int answer = 0;
+
+        // 1부터 입력으로 주어진 번호까지 순회를 진행한다.
+        for (int i = 1; i <= number; i++) {
+            // 각 번호를 문자열로 변경하고, 원본 길이를 구한다.
+            String strNum = String.valueOf(i);
+            int originLen = strNum.length();
+        }
         return answer;
     }
+
 }
