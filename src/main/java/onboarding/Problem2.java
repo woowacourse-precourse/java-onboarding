8bc4218b7318ea602fa8a4b89196bd778e8a5783
Sojeong <k5k5s5s5j5j5@naver.com>
2022-10-29 21:02:35 +0900

    problem2: add comments

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,6 +11,7 @@ public class Problem2 {
         String[] cryptoArr = cryptogram.split("");
         List<String> cryptoList = Arrays.asList(cryptoArr);
 
+        //새로운 List에 중복 문자 제외하고 넣는다. 바뀌지 않을 때까지 반복
         while (changed) {
             List<String> cryptoListCopy = new ArrayList<>();
             for (int i = 0; i < cryptoList.size() - 1; i++) {
@@ -21,6 +22,8 @@ public class Problem2 {
                 else
                     cryptoListCopy.add(cryptoList.get(i));
             }
+
+            //마지막 문자는 for에서 넣지 않으므로 넣어준다. 바로 이전 문자와 중복되지 않는 경우에만!
             if (!cryptoList.get(cryptoList.size() - 2).equals(cryptoList.get(cryptoList.size() - 1)))
                 cryptoListCopy.add(cryptoList.get(cryptoList.size() - 1));
 
@@ -30,6 +33,7 @@ public class Problem2 {
                 changed = true;
             cryptoList = cryptoListCopy;
 
+            //결과 list가 비어있다면 루프 종료
             if (cryptoList.isEmpty())
                 break;
         }
