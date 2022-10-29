a4a2bb51cb79d510a00579ccea30072c5fe55f40
jeonghoon <shb03323@naver.com>
2022-10-29 18:37:15 +0900

    refactor: merge makeResult() into solution() in problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,29 +2,26 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = makeResult(cryptogram);
-        return answer;
-    }
-
-    private static String makeResult(String cryptogram) {
         int resultLength = cryptogram.length() + 1;
         while (cryptogram.length() != resultLength) {
             resultLength = cryptogram.length();
             cryptogram = reduceCryptogram(cryptogram);
         }
+
         return cryptogram;
     }
 
     private static String reduceCryptogram(String cryptogram) {
-        String cloneCryptogram = "";
+        StringBuilder cloneCryptogram = new StringBuilder();
         for (int i = 0; i < cryptogram.length(); i++) {
             if (i != 0 && cryptogram.charAt(i - 1) == cryptogram.charAt(i)) {
                 continue;
             } else if (i != cryptogram.length() - 1 && cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
                 continue;
             }
-            cloneCryptogram += cryptogram.charAt(i);
+            cloneCryptogram.append(cryptogram.charAt(i));
         }
-        return cloneCryptogram;
+
+        return cloneCryptogram.toString();
     }
 }
