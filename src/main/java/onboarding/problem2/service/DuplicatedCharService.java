4d3cb153223377e5c30a016d30a18775df01079a
song-wooseok <sws981202@gmail.com>
2022-10-29 22:20:14 +0900

    feat: [PROBLEM2 - 기능4] 현재 문자열에서 연속된 문자들의 위치를 반환하는 기능

--- src/main/java/onboarding/problem2/service/DuplicatedCharService.java
+++ src/main/java/onboarding/problem2/service/DuplicatedCharService.java
@@ -1,8 +1,28 @@
 package onboarding.problem2.service;
 
+import java.util.ArrayList;
+
 public class DuplicatedCharService {
 
-    public static boolean checkDecryption(String word){
+    //문자열을 받고 중복된 문자가 나오는 위치를 담은 ArrayList 를 반환한다.
+    public static ArrayList<int[]> getDuplicatedCharPositionList(String word){
+        ArrayList<int[]> duplicatedCharPositionList = new ArrayList<int[]>();
+        //추후 함수를 단독으로 사용했을 경우를 대비하여 if 조건문 작성
+        if(!isDuplicatedChar(word)){
+            return duplicatedCharPositionList;
+        }
+        for(int index = 0; index<word.length(); index++){
+            int [] continuedCharPosition = getDuplicatedCharPosition(index, word);
+
+            if(notNullPositionCheck(continuedCharPosition)){
+                index = (continuedCharPosition[1]);
+                duplicatedCharPositionList.add(continuedCharPosition);
+            }
+        }
+        return duplicatedCharPositionList;
+    }
+
+    private static boolean checkDecryption(String word){
         if(word.length() != 0){
             return isDuplicatedChar(word);
         }
@@ -10,7 +30,7 @@ public class DuplicatedCharService {
     }
 
     // 문자열 안에 중복된 부분이 있는지 검사
-    public static boolean isDuplicatedChar(String word){
+    private static boolean isDuplicatedChar(String word){
         char checkChar = word.charAt(0);
         for(int index = 1; index < word.length(); index++){
             if(compareWord(checkChar, word.charAt(index))){
@@ -21,15 +41,15 @@ public class DuplicatedCharService {
         return false;
     }
 
-    public static boolean compareWord(char c1, char c2){
+    private static boolean compareWord(char c1, char c2){
         return (c1 == c2);
     }
 
-    public static boolean notNullPositionCheck(int[] position){
+    private static boolean notNullPositionCheck(int[] position){
         return position != null;
     }
 
-    public static int[] getDuplicatedCharPosition(int start, String word){
+    private static int[] getDuplicatedCharPosition(int start, String word){
         char checkChar = word.charAt(start);
         int[] position = null;
 
@@ -42,7 +62,7 @@ public class DuplicatedCharService {
         return position;
     }
 
-    public static int[] updatePosition(int [] position, int start, int index){
+    private static int[] updatePosition(int [] position, int start, int index){
         if(position == null){
             position = new int[]{start, index};
         }
