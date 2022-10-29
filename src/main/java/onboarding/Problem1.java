aeeab56b6a424edd11a6c86c43678a9e2eb5b14e
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 02:03:12 +0900

    refactor : 함수 이름에 맞게 로직 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,18 +8,18 @@ class Problem1 {
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int answer = Integer.MAX_VALUE;
 
-		if (isValidCondition(pobi) || isValidCondition(crong))
+		if (!isValidCondition(pobi) || !isValidCondition(crong))
 			return ERROR_CODE;
 
-		if (isValidPages(pobi) || isValidPages(crong))
+		if (!isValidPages(pobi) || !isValidPages(crong))
 			return ERROR_CODE;
 
 		int pobiScore = Math.max(getBigScore(pobi.get(0)), getBigScore(pobi.get(1)));
 		int crongScore = Math.max(getBigScore(crong.get(0)), getBigScore(crong.get(1)));
 
-		if(pobiScore > crongScore)
+		if (pobiScore > crongScore)
 			answer = 1;
-		else if(pobiScore == crongScore)
+		else if (pobiScore == crongScore)
 			answer = 0;
 		else
 			answer = 2;
@@ -32,10 +32,10 @@ class Problem1 {
 	 */
 	public static boolean isValidCondition(List<Integer> pages) {
 		if (pages.size() != 2)            // 리스트의 길이가 2 이상인 경우
-			return true;
+			return false;
 		if (pages.get(0) >= pages.get(1)) //왼쪽 오른쪽 페이지 순서로 있지 않은 경우
-			return true;
-		return false;
+			return false;
+		return true;
 	}
 
 	/**
@@ -44,26 +44,26 @@ class Problem1 {
 	public static boolean isValidPages(List<Integer> pages) {
 		if (pages.get(1) - pages.get(0) != 1 || pages.get(0) % 2 == 0 || pages.get(1) % 2 == 1)
 			// 왼쪽 오른쪽 페이지의 차가 1이 아닌 경우, 왼쪽페이지가 짝수인 경우, 오른쪽 페이지가 홀수인 경우
-			return true;
-		if (isFirstPage(pages) || isLastPage(pages))
+			return false;
+		if (!isFirstPage(pages) || !isLastPage(pages))
 			// 펏번째, 마지막 페이지인 경우
-			return true;
+			return false;
 		if (pages.stream().anyMatch(page -> (page > 400 || page < 1)))
 			// 1 ~ 400 범위 밖의 페이지 인 경우
-			return true;
-		return false;
+			return false;
+		return true;
 	}
 
 	public static boolean isFirstPage(List<Integer> pages) {
 		if (pages.get(0) == 1 && pages.get(1) == 2)
-			return true;
-		return false;
+			return false;
+		return true;
 	}
 
 	public static boolean isLastPage(List<Integer> pages) {
 		if (pages.get(0) == 399 && pages.get(1) == 400)
-			return true;
-		return false;
+			return false;
+		return true;
 	}
 
 	/**
