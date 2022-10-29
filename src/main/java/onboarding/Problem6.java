e4fae1b518de528b6e873b7ce075a42a25249d26
hectick <chyeon1223@cau.ac.kr>
2022-10-30 02:26:05 +0900

    refactor : [Problem6] 주석 제거

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,28 +4,25 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        HashMap<String, String> part = new HashMap<>(); //닉네임 중복 체크를 위해 닉네임의 파편들을 이메일주소와 함께 저장
-        HashSet<String> emailSet = new HashSet<>(); //중복되지 않도록 이메일을 저장
+        HashMap<String, String> part = new HashMap<>();
+        HashSet<String> emailSet = new HashSet<>();
 
         for(int i = 0; i < forms.size(); i++){
             String nickname = forms.get(i).get(1);
             String email = forms.get(i).get(0);
+
             if(nickname.length() == 1){
                 continue;
             }
-            
-            //닉네임 중복 확인
+
             List<String> duplicatedNicknameOwner = checkNicknameDuplication(nickname, email, part);
-            //중복된 이메일 저장
             emailSet = addEmail(emailSet, duplicatedNicknameOwner);
 
-            //중복 닉네임이 될 수 있는 단어들(part)을 추출
             List<String> newPart = extractNicknameParts(nickname);
-            //추출한 단어들을 저장
             part = addPart(part, newPart, email);
         }
-        //set을 list로 변경하여 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 메일 목록을 반환하는 기능
-        List<String> answer = new ArrayList<>(emailSet); //set -> list
+
+        List<String> answer = new ArrayList<>(emailSet);
         Collections.sort(answer);
         return answer;
     }
@@ -64,7 +61,7 @@ public class Problem6 {
             if(part.containsKey(newPart.get(j))){
                 continue;
             }
-            part.put(newPart.get(j), email); //추출한 단어 + 이메일
+            part.put(newPart.get(j), email);
         }
         return part;
     }
