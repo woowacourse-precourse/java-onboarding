f67a792716586a12499a83408c201904292e451a
WonSeok-dd <cluejws@gmail.com>
2022-10-29 23:14:55 +0900

    Problem1 | feat: 페이지로 Score얻기, Score비교

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,8 +13,36 @@ import java.util.List;
  *  4-3 왼쪽 페이지와 오른쪽 페이지가 연속된 페이지인가?
  */
 class Problem1 {
+
+    private static int getMaxScore(int page){
+        int result;
+
+        int add = 0;
+        int mul = 1;
+
+        while (page != 0){
+            add += page % 10;
+            mul *= page % 10;
+            page /= 10;
+        }
+
+        result = Math.max(add, mul);
+
+        return result;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        int pobiScore = Math.max(getMaxScore((pobi.get(0))), getMaxScore(pobi.get(1)));
+        int crongScore = Math.max(getMaxScore((crong.get(0))), getMaxScore(crong.get(1)));
+
+        if (pobiScore > crongScore){
+            return 1;
+        } else if (pobiScore < crongScore) {
+            return 2;
+        } else if (pobiScore == crongScore) {
+            return 0;
+        }
+
+        return -1;
     }
 }
