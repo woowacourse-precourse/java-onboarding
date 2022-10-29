6a2e42cb5cc2c8ac380c84bb6c7624bd92eeff7b
song-wooseok <sws981202@gmail.com>
2022-10-29 22:25:05 +0900

    feat: [PROBLEM2 - 기능5] 제거해야할 문자열을 반환하는 기능

---
+++ src/main/java/onboarding/problem2/service/RemoveCharService.java
@@ -0,0 +1,22 @@
+package onboarding.problem2.service;
+
+import java.util.ArrayList;
+
+public class RemoveCharService {
+
+
+    //문자의 위치를 받고, 해당 문자열을 ArrayList 에 담아서 반환한다.
+    private static ArrayList<String> getWordListFromRange(String string, ArrayList<int[]> rangeList){
+        ArrayList<String> consecutiveWordArray = new ArrayList<String>();
+        for(int [] removeRange : rangeList){
+            String removeWord = getWordFromRange(string, removeRange);
+            consecutiveWordArray.add(removeWord);
+        }
+
+        return consecutiveWordArray;
+    }
+
+    private static String getWordFromRange(String string, int[] range){
+        return string.substring(range[0], (range[1]+1));
+    }
+}
