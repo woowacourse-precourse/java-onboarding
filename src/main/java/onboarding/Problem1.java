6c3abd3cb04ac84fb464d555ad3dcfaa8867ca82
woo-chang <clllickme@naver.com>
2022-10-29 18:22:33 +0900

    refactor(문제 1): pobi, crong에 특정된 점수 반환하도록 변수명 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,57 +4,60 @@ import java.util.List;
 
 class Problem1 {
 
+  static Valid valid = new Valid();
+  static Calculation calculation = new Calculation();
+  static Result result = new Result();
+
   public static int solution(List<Integer> pobi, List<Integer> crong) {
-    if (Valid.validate(pobi) && Valid.validate(crong)) {
-      return Result.getResult(Calculation.getMaxPoint(pobi), Calculation.getMaxPoint(crong));
+    if (valid.validate(pobi) && valid.validate(crong)) {
+      return result.chooseWinner(calculation.getMaxPoint(pobi), calculation.getMaxPoint(crong));
     }
     return Result.EXCEPTION;
   }
 
   static class Valid {
 
-    public static final int LENGTH = 2;
-    public static final int START_PAGE = 1;
-    public static final int END_PAGE = 400;
+    private static final int START_PAGE = 1;
+    private static final int END_PAGE = 400;
 
-    public static boolean validate(List<Integer> pages) {
-      return isCorrectLength(pages) && isCorrectRange(pages) && isOddNumber(pages.get(0))
+    public boolean validate(List<Integer> pages) {
+      return isExistPage(pages) && isCorrectRange(pages) && isOddNumber(pages.get(0))
           && isEvenNumber(pages.get(1)) && isContinues(pages);
     }
 
-    private static boolean isCorrectLength(List<Integer> pages) {
-      return pages.size() == LENGTH;
+    private boolean isExistPage(List<Integer> pages) {
+      return pages.get(0) != null && pages.get(1) != null;
     }
 
-    private static boolean isCorrectRange(List<Integer> pages) {
+    private boolean isCorrectRange(List<Integer> pages) {
       int leftPage = pages.get(0);
       int rightPage = pages.get(1);
       return START_PAGE <= leftPage && leftPage <= END_PAGE
           && START_PAGE <= rightPage && rightPage <= END_PAGE;
     }
 
-    private static boolean isOddNumber(int page) {
+    private boolean isOddNumber(int page) {
       return page % 2 == 1;
     }
 
-    private static boolean isEvenNumber(int page) {
+    private boolean isEvenNumber(int page) {
       return page % 2 == 0;
     }
 
-    private static boolean isContinues(List<Integer> pages) {
+    private boolean isContinues(List<Integer> pages) {
       return pages.get(1) - 1 == pages.get(0);
     }
   }
 
   static class Calculation {
 
-    public static int getMaxPoint(List<Integer> pages) {
-      int plusMax = Math.max(plus(pages.get(0)), plus(pages.get(1)));
-      int multipleMax = Math.max(multiple(pages.get(0)), multiple(pages.get(1)));
-      return Math.max(plusMax, multipleMax);
+    public int getMaxPoint(List<Integer> pages) {
+      int plusMaxPoint = Math.max(plus(pages.get(0)), plus(pages.get(1)));
+      int multipleMaxPoint = Math.max(multiple(pages.get(0)), multiple(pages.get(1)));
+      return Math.max(plusMaxPoint, multipleMaxPoint);
     }
 
-    private static int plus(int page) {
+    private int plus(int page) {
       int result = 0;
       while (page != 0) {
         result += page % 10;
@@ -63,7 +66,7 @@ class Problem1 {
       return result;
     }
 
-    private static int multiple(int page) {
+    private int multiple(int page) {
       int result = 1;
       while (page != 0) {
         result *= page % 10;
@@ -75,14 +78,16 @@ class Problem1 {
 
   static class Result {
 
-    public static final int EXCEPTION = -1;
-    public static final int LEFT_PLAYER_WIN = 1;
-    public static final int RIGHT_PLAYER_WIN = 2;
-    public static final int DRAW = 0;
+    private static final int DRAW = 0;
+    private static final int EXCEPTION = -1;
 
-    public static int getResult(int leftPlayerPoint, int rightPlayerPoint) {
-      if (leftPlayerPoint > rightPlayerPoint) return LEFT_PLAYER_WIN;
-      if (rightPlayerPoint > leftPlayerPoint) return RIGHT_PLAYER_WIN;
+    public int chooseWinner(int pobiPoint, int crongPoint) {
+      if (pobiPoint > crongPoint) {
+        return 1;
+      }
+      if (pobiPoint < crongPoint) {
+        return 2;
+      }
       return DRAW;
     }
   }
