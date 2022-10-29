a7cc44786c460ea9323cf430fa6a7f5a44af2ead
ddingmin <ddingminyt@gmail.com>
2022-10-30 06:15:06 +0900

    feat(Ploblem2): 중복 제거 기능 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,34 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        System.out.println("pr2: " + cryptogram);
+        System.out.println("return: " + clearDupliationWord(cryptogram));
         String answer = "answer";
         return answer;
     }
+
+    private static String clearDupliationWord(String word){
+        Boolean dup = false;
+        String returnWord = "";
+        if (word.length() < 1){
+            return returnWord;
+        }
+        char duplication = word.charAt(0);
+        word += "\0";
+        for (int i = 1; i < word.length(); i ++){
+            if (duplication == word.charAt(i)){
+                dup = true;
+            }
+            if (duplication != word.charAt(i) && dup){
+                dup = false;
+                duplication = word.charAt(i);
+            }
+            if (duplication != word.charAt(i) && !dup){
+                returnWord += duplication;
+            }
+            duplication = word.charAt(i);
+        }
+        return returnWord;
+    }
+
 }
