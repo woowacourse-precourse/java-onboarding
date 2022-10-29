665e75676b2cf04b85cc5b46f10d7326f782b057
jinny-l <jinny.l.dev@gmail.com>
2022-10-29 19:47:26 +0900

    [feat] (problem.7) 추천 친구 리스트를 반환하는 메소드 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,53 @@ public class Problem7 {
         return answer;
     }
 
+    // 추천 친구 리스트를 반환하는 메소드
+    private static List<String> recommendedFriendsOf(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> uniqueFriendsOfFriends= uniqueFriendsOfFriends(user, friends);
+        List<String> uniqueVisitors = uniqueVisitorsOf(visitors);
+        List<String> userFriends = userFriends(friends);
+
+        Set<String> recommendedFriends = new HashSet<>(uniqueFriendsOfFriends);
+        recommendedFriends.addAll(uniqueVisitors);
+
+        recommendedFriends.removeIf(userFriends::contains);
+
+        return new ArrayList<>(recommendedFriends);
+    }
+
+    // user와 friend인 리스트를 반환하는 메소드
+    private static List<String> userFriends(List<List<String>> friends) {
+        Set<String> friendsOfUser = new HashSet<>();
+
+        for (List<String> friend : friends) {
+            friendsOfUser.add(friend.get(0));
+        }
+
+        return new ArrayList<>(friendsOfUser);
+    }
+
+
+    // unique friends of friends 리스트를 반환하는 메소드
+    private static List<String> uniqueFriendsOfFriends(String user, List<List<String>> friends) {
+        List<String> friendsOfFriends = friendsOfFriends(friends);
+
+        Set<String> set = new HashSet<>(friendsOfFriends);
+        set.remove(user);
+
+        return new ArrayList<>(set);
+    }
+
+    // fiends of friends 리스트를 반환하는 메소드
+    private static List<String> friendsOfFriends(List<List<String>> friends) {
+        List<String> friendsOfFriends = new ArrayList<>();
+
+        for (List<String> friend: friends) {
+            friendsOfFriends.add(friend.get(1));
+        }
+        return friendsOfFriends;
+    }
+
+
     // visitor의 방문 횟수를 반환하는 메소드
     private static Map<String, Integer> getVisitorsScore(List<String> visitors) {
         List<String> uniqueVisitors = uniqueVisitorsOf(visitors);
