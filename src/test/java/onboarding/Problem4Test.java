6d2e8386d1fdfc53c424b5051491ff47c621f019
chanminmaxscaler <jcm04@naver.com>
2022-10-29 11:52:54 +0900

    test(Problem4Test): isUpperCase 메소드의 Test 코드를 수정하고 isLowerCase 메소드의 Test 코드를 추가한다
    isUpperCase 메소드의 테스트 코드를 BDD 방식으로 리팩토링 한다.
    isLowerCase 메소드의 테스트 코드를 BDD 방식으로 작성한다.
    isLowerCaseTest 클래스는 isLowerCase 메소드에 소문자 아스키코드가 입력되었을 때 true를 반환하는지 확인한다. 그 외 아스키코드가 입력되었을 때 false를 반환하는지 확인한다.

--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,13 +14,55 @@ class Problem4Test {
     void setUp() {
         problem4 = new Problem4();
     }
+    @Nested
+    @DisplayName("isUpperCase 메소드는")
+    class isUpperCaseTest {
+        @Nested
+        @DisplayName("대문자 아스키 코드를 입력하면")
+        class Context_with_upper_case_ascii_code{
+            @Test
+            @DisplayName("true를 반환한다.")
+            void it_returns_true() {
+                int upperCaseAsciiCode = 67;
+                assertThat(problem4.isUpperCase(upperCaseAsciiCode)).isTrue();
+            }
+        }
 
-    @Test
-    void isUpperCaseTest() {
-        int capitalLetterAsciiCode = 67;
-        assertThat(problem4.isUpperCase(capitalLetterAsciiCode)).isEqualTo(true);
+        @Nested
+        @DisplayName("다른 아스키 코드를 입력하면")
+        class Context_with_other_ascii_code{
+            @Test
+            @DisplayName("false를 반환한다.")
+            void it_returns_false() {
+                int otherAsciiCode = 91;
+                assertThat(problem4.isUpperCase(otherAsciiCode)).isFalse();
+            }
+        }
+    }
 
-        int otherAsciiCode = 91;
-        assertThat(problem4.isUpperCase(otherAsciiCode)).isNotEqualTo(true);
+
+    @Nested
+    @DisplayName("isLowerCase 메소드는")
+    class isLowerCaseTest {
+        @Nested
+        @DisplayName("소문자 아스키 코드를 입력하면")
+        class Context_with_lower_case_ascii_code {
+            @Test
+            @DisplayName("true를 반환한다")
+            void it_returns_true() {
+                int lowerCaseAsciiCode = 97;
+                assertThat(problem4.isLowerCase(lowerCaseAsciiCode)).isTrue();
+            }
+        }
+        @Nested
+        @DisplayName("다른 아스키 코드를 입력하면")
+        class Context_with_other_ascii_code {
+            @Test
+            @DisplayName("false를 반환한다")
+            void it_returns_false() {
+                int otherAsciiCode = 90;
+                assertThat(problem4.isLowerCase(otherAsciiCode)).isFalse();
+            }
+        }
     }
 }
