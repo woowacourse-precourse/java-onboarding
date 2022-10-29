fa9e06bd22e3ad650636535ff65de1d29b4571d3
sangho-jeon <jeonsangho1587@gmail.com>
2022-10-29 18:37:54 +0900

    fix       : problem6 turn splittedNickNames type <HashSet> -> <HashMap>

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,20 +14,36 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        HashSet<String> splittedNickName = mappingTwoCharacterToSet(forms);
-
-        System.out.println(splittedNickName);
-        return answer;
-    }
-
-    public static HashSet<String> mappingTwoCharacterToSet(List<List<String>> forms){
-        HashSet<String> splittedNickName = new HashSet<>();
+        HashMap<String> splittedNickName = mappingTwoCharacterToSet(forms);
+        HashSet<String> duplicatedNickNameEmail = new HashSet<>();
         for (List<String> information:
              forms) {
             String nickName = information.get(1);
             if(nickName.length() >= 2){
                 for (int i = 0; i < nickName.length()-1; i++) {
-                    splittedNickName.add(nickName.substring(i, i+2));
+                    String subNickName = nickName.substring(i, i+2);
+                    if(!splittedNickName.add(subNickName)){
+                        duplicatedNickNameEmail.add(information.get(0));
+                    }
+                }
+            }
+        }
+
+        System.out.println(duplicatedNickNameEmail);
+        return answer;
+    }
+
+    public static HashMap<String, List<String>> mappingTwoCharacterToSet(List<List<String>> forms){
+        HashMap<String, List<String>> splittedNickName = new HashMap<>();
+        for (List<String> information:
+             forms) {
+            String nickName = information.get(1);
+            if(nickName.length() >= 2){
+                for (int i = 0; i < nickName.length()-1; i++) {
+                    String subNickName = nickName.substring(i, i+2);
+                    List<String> nickNames = splittedNickName.getOrDefault(subNickName, new ArrayList<>());
+                    nickNames.add(nickName);
+                    splittedNickName.put(nickName.substring(i, i+2), nickNames);
                 }
             }
         }
