41b827c017752b9614c639da4fcda4263dada044
soyunim <togksk123@naver.com>
2022-10-29 23:41:03 +0900

    Problem4 1)if the word is upper

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Problem4 {
     public static String solution(String word) {
+        List<String> strList = new ArrayList<>();
         String answer = "";
+
+        for(int i=0; i<word.length(); i++){
+            // ASCII UPPER
+            if((int)word.charAt(i)>=65 && (int)word.charAt(i)<=90) {
+                strList.add(String.valueOf((char) ((int) word.charAt(i) + 25 - (((int) word.charAt(i) - 65) * 2))));
+            }
+        }
+        answer=strList.toString().join("",strList);
         return answer;
     }
 }
