a777e16d1b968801b30c918a192c8e596c06ea0e
Wooseobee <dntjq0807@naver.com>
2022-10-30 03:21:17 +0900

    닉네임 중복 확인 메소드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
+    }
+
+    public static boolean isDuplicated(String nick1, String nick2) {
+        for (int i = 0; i < nick1.length() - 1; i++) {
+            for (int j = 0; j < nick2.length() - 1; j++) {
+                if (nick1.charAt(i) == nick2.charAt(j) && i < nick1.length() - 1) {
+                    if (nick1.charAt(i + 1) == nick2.charAt(j + 1)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
