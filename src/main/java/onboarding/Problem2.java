42e233e1e86f2d30ae2bc36e603ca8fae680775b
KJE <12181501@inha.edu>
2022-10-29 19:37:14 +0900

    solve problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        boolean flag = false;
+        char cmp;
+
+        while(flag == false&& answer.length() > 0){
+            cmp = answer.charAt(0);
+            for(int i = 1; i < answer.length();i++){
+                if(cmp == answer.charAt(i)){
+                    answer = answer.substring(0,i-1)+answer.substring(i);
+                }
+                cmp = answer.charAt(i);
+
+            }
+
+        }
         return answer;
     }
 }
