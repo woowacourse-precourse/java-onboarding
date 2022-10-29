702c239496401d4563f834cb6ad1020dbd2de21a
sangeun-b <baek0007@algonquinlive.com>
2022-10-29 22:34:54 +0900

    feat(PROBLEM1): Add feature for plus each number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,16 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    // 각 자리 숫자 더하기
+    public static int getSum(int page) {
+        int sum = 0;
+        while(page > 0) {
+            sum += page % 10;
+            page /= 10;
+        }
+        return sum;
+    }
+   
 }
+
