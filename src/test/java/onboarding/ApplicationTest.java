5ed45fd9604998cda872bad7ed38b5425f49839f
cmong0516 <cmong0516@gmail.com>
2022-10-29 16:38:15 +0900

    2022.10.29 added Test Delete

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -132,21 +132,5 @@ class ApplicationTest {
             List<String> result = List.of("andole", "jun", "bedi");
             assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
         }
-
-        @Test
-        void case2() {
-            String user = "mrko";
-            List<List<String>> friends = List.of(
-                    List.of("donut", "andole"),
-                    List.of("donut", "jun"),
-                    List.of("donut", "mrko"),
-                    List.of("shakevan", "andole"),
-                    List.of("shakevan", "jun"),
-                    List.of("shakevan", "mrko")
-            );
-            List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan", "mong", "pobi", "james");
-            List<String> result = List.of("andole", "jun", "bedi", "james","mong");
-            assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
-        }
     }
 }
