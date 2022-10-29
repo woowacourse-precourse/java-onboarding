1ab7d1a753be55b697bd21d18bbba402256a91be
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-29 21:31:04 +0900

    Feat(Problem5): 필요한 돈 계산기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,20 +1,32 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        List<Integer> moneys = makeMoneys();
 
-        System.out.println("moneys = " + moneys);
-        return answer;
+        return calMoneys(money);
     }
 
     static List<Integer> makeMoneys(){
         return List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    }
+
+    static List<Integer> calMoneys(int money) {
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> moneys = makeMoneys();
+
+        for (Integer m : moneys) {
+            int count = 0;
+            while (money >= m) {
+                money -= m;
+                count++;
+            }
+            answer.add(count);
+        }
+
+        return answer;
     }
 
 }
