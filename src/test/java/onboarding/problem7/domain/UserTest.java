3c392e42f65e09f2b58278b5485c105ca99bf72e
JunH <tjdtls690@gmail.com>
2022-10-29 15:27:56 +0900

    :white_check_mark: test(PROBLEM7) : 테스트 클래스 접근 제어자 개선

--- src/test/java/onboarding/problem7/domain/UserTest.java
+++ src/test/java/onboarding/problem7/domain/UserTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-public class UserTest {
+class UserTest {
     private User jun;
     
     @BeforeEach
