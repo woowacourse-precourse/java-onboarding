1d8f02c2fba78e64cf5a12c87ed2c40c06bcf203
shpark30 <seohyeonpark30.gmail.com>
2022-10-29 17:30:12 +0900

    comment(Problem6.java): 기능 구현 로직 주석 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+    // HashMap<String, ArrayList<Integer>> charToIndices 생성 : {두 글자 : [닉네임 인덱스]}
+    HashMap<String, ArrayList<Integer>> charToIndices = new HashMap<String, ArrayList<Integer>>();
     public static List<String> solution(List<List<String>> forms) {
+        // 탐색 Loop forms
+            // updateEvery2SequentialChar : 닉네임에서 2글자씩 분절해  charToIndices에 추가
+        // charToIndices에서 길이가 2이상인 Index 배열 반환
+        // answer 배열 생성
         List<String> answer = List.of("answer");
+        // Index 배열을 이용해 aswer에 email add
         return answer;
     }
 }
