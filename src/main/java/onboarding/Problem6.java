076eda8d3cb5d23db5f0a3703c53ab1bd538f797
HubCreator <chocomilk8604@gmail.com>
2022-10-29 11:34:55 +0900

    refactor(Problem6) : 테스트 케이스의 메서드 제거, 아이디가 한 글자일 때 테스트 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,60 +4,53 @@ import java.util.*;
 
 public class Problem6 {
     public static final int LIMIT = 2;
+    public static final String EMAIL_FORMAT = "@email.com";
 
     public static List<String> solution(List<List<String>> forms) {
-        return getAnswer(forms);
-    }
-
-    public static List<String> getAnswer(List<List<String>> forms) {
-        Map<String, Set<String>> map = new HashMap<>();
+        Map<String, Set<String>> nickAndEmail = new HashMap<>();
         for (List<String> form : forms) {
-            makeMap(map, form.get(1), form.get(0));
+            makeMap(nickAndEmail, form.get(1), form.get(0).split("@")[0]);
         }
-        return makeResult(map);
+        List<String> answer = makeResult(nickAndEmail);
+        for (int i = 0; i < answer.size(); i++) {
+            answer.set(i, answer.get(i) + EMAIL_FORMAT);
+        }
+        return answer;
     }
 
-    private static void makeMap(Map<String, Set<String>> map, String nickName, String email) {
-        if (map.isEmpty()) {
+
+    private static void makeMap(Map<String, Set<String>> nickAndEmail, String nickName, String email) {
+        if (nickName.length() < LIMIT) return; // 한 글자면 처리를 하지 않아도 됨
+        if (nickAndEmail.isEmpty()) {
             Set<String> set = new HashSet<>();
             set.add(email);
-            map.put(nickName, set);
+            nickAndEmail.put(nickName, set);
         }
-        for (String s : map.keySet()) {
-            String key = isDuplicated(s, nickName);
+        for (String s : nickAndEmail.keySet()) {
+            String key = hasDuplicatedWords(s, nickName);
             if (key != null) {
-                map.get(key).add(email);
+                nickAndEmail.get(key).add(email);
             }
         }
     }
 
-    public static String isDuplicated(String before, String after) {
-        if (before.length() < LIMIT || after.length() < LIMIT) return null;
-        if (isMatched(before, after)) {
-            return before;
-        }
+    protected static String hasDuplicatedWords(String key, String nickName) {
+        if (isValid(key, nickName)) return key;
         return null;
     }
 
-    /**
-     * 크기가 2인 윈도우로 밀고나가면서 중복되는 값이 있는지 확인
-     * @param str1
-     * @param str2
-     * @return
-     */
-    private static boolean isMatched(String str1, String str2) {
-        for (int i = 0; i <= str2.length() - 2; i++) {
-            if (str1.contains(str2.substring(i, i + 2))) {
-                return true;
-            }
+    private static boolean isValid(String s1, String s2) {
+        for (int i = 0; i <= s2.length() - LIMIT; i++) {
+            // s2의 2개의 연속된 단어를 s1이 가지고 있을 때
+            if (s1.contains(s2.substring(i, i + LIMIT))) return true;
         }
         return false;
     }
 
-    private static List<String> makeResult(Map<String, Set<String>> map) {
+    private static List<String> makeResult(Map<String, Set<String>> nickAndEmail) {
         List<String> result = new ArrayList<>();
-        for (Map.Entry<String, Set<String>> e : map.entrySet()) {
-            if (e.getValue().size() >= 2) {
+        for (Map.Entry<String, Set<String>> e : nickAndEmail.entrySet()) {
+            if (e.getValue().size() >= LIMIT) {
                 result.addAll(e.getValue());
             }
         }
--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -9,9 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem6Test {
 
-    public static final int LIMIT = 2;
-    public static final String EMAIL_FORMAT = "@email.com";
-
     @Test
     void nameMatchingTest() {
         String str1 = "제이엠";
@@ -19,10 +16,10 @@ class Problem6Test {
         String str3 = "제엠이";
         String str4 = "크롱";
         String str5 = "크크크크크크크크크크크롱";
-        assertThat(isDuplicated(str1, str2)).isEqualTo("제이엠");
-        assertThat(isDuplicated(str2, str3)).isNull();
-        assertThat(isDuplicated(str2, str4)).isNull();
-        assertThat(isDuplicated(str4, str5)).isEqualTo("크롱");
+        assertThat(Problem6.hasDuplicatedWords(str1, str2)).isEqualTo("제이엠");
+        assertThat(Problem6.hasDuplicatedWords(str2, str3)).isNull();
+        assertThat(Problem6.hasDuplicatedWords(str2, str4)).isNull();
+        assertThat(Problem6.hasDuplicatedWords(str4, str5)).isEqualTo("크롱");
     }
 
     @Test
@@ -55,7 +52,7 @@ class Problem6Test {
                     List.of("mj@email.com", "엠제이"),
                     List.of("nowm@email.com", "이제엠")
             );
-            assertThat(getAnswer(forms)).isEqualTo(List.of("jason@email.com", "jm@email.com", "mj@email.com"));
+            assertThat(Problem6.solution(forms)).isEqualTo(List.of("jason@email.com", "jm@email.com", "mj@email.com"));
         }
 
         @Test
@@ -70,63 +67,25 @@ class Problem6Test {
                     List.of("jm@email.com", "이엠제"),
                     List.of("nowm@email.com", "이제엠")
             );
-            assertThat(getAnswer(forms)).isEqualTo(List.of("hello@email.com", "hello1@email.com", "jason@email.com", "jm@email.com", "mj@email.com"));
+            assertThat(Problem6.solution(forms)).isEqualTo(List.of("hello@email.com", "hello1@email.com", "jason@email.com", "jm@email.com", "mj@email.com"));
         }
-    }
 
-    public static List<String> getAnswer(List<List<String>> forms) {
-        Map<String, Set<String>> map = new HashMap<>();
-        for (List<String> form : forms) {
-            String nickName = form.get(1);
-            String email = form.get(0).split("@")[0]; // @ 이전 문자만 관리
-            makeMap(map, nickName, email);
+        @Test
+        void case3() {
+            List<List<String>> forms = List.of(
+                    List.of("jason@email.com", "제이슨"),
+                    List.of("hello@email.com", "토마스타이슨"),
+                    List.of("hello1@email.com", "마이클마이클이클립스이슨"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jm@email.com", "이엠제이"),
+                    List.of("xy@email.com", "크"),
+                    List.of("xy@email.com", "크"),
+                    List.of("xy@email.com", "크"),
+                    List.of("xy@email.com", "크")
+            );
+            assertThat(Problem6.solution(forms)).isEqualTo(List.of("hello@email.com", "hello1@email.com", "jason@email.com", "jm@email.com", "mj@email.com"));
         }
-        List<String> answer = makeResult(map);
-        for (int i = 0; i < answer.size(); i++) {
-            answer.set(i, answer.get(i) + EMAIL_FORMAT);
-        }
-        return answer;
-    }
-
-    private static void makeMap(Map<String, Set<String>> map, String nickName, String email) {
-        if (map.isEmpty()) {
-            Set<String> set = new HashSet<>();
-            set.add(email);
-            map.put(nickName, set);
-        }
-        for (String s : map.keySet()) {
-            String key = isDuplicated(s, nickName);
-            if (key != null) {
-                map.get(key).add(email);
-            }
-        }
-    }
-
-    public static String isDuplicated(String before, String after) {
-        if (before.length() < LIMIT || after.length() < LIMIT) return null;
-        if (isMatched(before, after)) {
-            return before;
-        }
-        return null;
-    }
-
-    private static boolean isMatched(String str1, String str2) {
-        for (int i = 0; i <= str2.length() - 2; i++) {
-            if (str1.contains(str2.substring(i, i + 2))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static List<String> makeResult(Map<String, Set<String>> map) {
-        List<String> result = new ArrayList<>();
-        for (Map.Entry<String, Set<String>> e : map.entrySet()) {
-            if (e.getValue().size() >= 2) {
-                result.addAll(e.getValue());
-            }
-        }
-        Collections.sort(result);
-        return result;
     }
 }
