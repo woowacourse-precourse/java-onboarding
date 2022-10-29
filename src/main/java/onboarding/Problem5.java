3de37323968c7c701d1f10673ca6ba56e46031b7
Ridealist <cjsn92@gmail.com>
2022-10-29 21:15:18 +0900

    feat/Problem5 : Withdrawal 클래스 생성 및 setMoneyArray 메소드 정의

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,22 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+}
+
+class Withdrawal {
+    List<Integer> money_array = new ArrayList<>(9);
+
+    void setMoneyArray() {
+        int m = 50000;
+        for (int i=0; i<8; i++) {
+            int e = (int) Math.pow(10, i/2);
+            if (i % 2 == 0) {
+                money_array.add(m/e);
+            } else {
+                money_array.add(m/(e*5));
+            }
+        }
+        money_array.add(1);
     }
 }
