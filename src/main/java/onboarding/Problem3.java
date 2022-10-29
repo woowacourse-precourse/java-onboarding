d2eede30cf99346b3cf114789aba9e7637baa9c7
Donguk <95donguk.dev@gmail.com>
2022-10-30 00:41:44 +0900

    feat: 문제3의 손뼉을 쳐야 하는 총 횟수를 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.Objects;
+
 public class Problem3 {
+    private static final int START_NUMBER = 1;
+
     public static int solution(int number) {
         int answer = 0;
+        answer = findResult(number);
         return answer;
+    }
+
+    private static int findResult(int number) {
+        int result = 0;
+        for (int i = START_NUMBER; i <= number; i++) {
+            result += Number.countClapOfNumber(i);
+        }
+        return result;
+    }
+
+    static class Number {
+        public static int countClapOfNumber(int number) {
+            int clap = 0;
+            for (int i = number; i > 0; i = i / 10) {
+                if (hasThreeOrSixOrNine(i % 10)) {
+                    clap++;
+                }
+            }
+            return clap;
+        }
+
+        private static boolean hasThreeOrSixOrNine(int digit) {
+            return Objects.equals(3, digit) || Objects.equals(6, digit) || Objects.equals(9, digit);
+        }
     }
 }
