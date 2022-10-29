c8f997b2ffadd0d9e26ce0edc4fc37249da67b50
iamjooon2 <iamjooon2@gmail.com>
2022-10-29 14:53:51 +0900

    feat(problem2): 비교용 스택 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> stack = new Stack<>();
+
         return answer;
     }
 }
