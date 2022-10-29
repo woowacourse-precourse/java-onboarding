4100683e97ec5db9fcefbf9184de17aa7a464ee1
Luka <meena2003@naver.com>
2022-10-29 16:24:43 +0900

    howManyNums 전역변수 삭제, howManyNums 변수 재생성하고, countHowManyNums 메서드에서 tmpHowManyNums 변수를 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,26 +1,37 @@
 package onboarding;
 
 public class Problem3 {
-    private static int howManyNums = 0;
+    public static void main(String[] args) {
+        System.out.println(solution(36));
+    }
+
     public static int solution(int number) {
-        // 1. 3 6 9 숫자가 몇개 존재하는지 담을 전역변수 생성
+        // 1. 3 6 9 숫자가 몇개 존재하는지 담을 변수 생성
         // 2. 숫자를 하나씩 세면서 해당 숫자내에 3 6 9 가 몇개 있는지 확인하는 메서드 생성
         // 3. 정답 반환
 
-        int answer = 0;
-        return answer;
+        if (number <= 2) {
+            return 0;
+        }
+
+        int howManyNums = countHowManyNums(number);;
+
+        return howManyNums;
     }
 
-    private void countHowManyNums(int number) {
+    private static int countHowManyNums(int number) {
+        int tmpHowManyNums = 0;
         String strNumber = "";
 
         for (int i = 3; i <= number; i++) {
-            strNumber = String.valueOf(number);
+            strNumber = String.valueOf(i);
             for (int j = 0; j < strNumber.length(); j++) {
-                if (strNumber.charAt(i) == '3' || strNumber.charAt(i) == '6' || strNumber.charAt(i) == '9') {
-                    howManyNums++;
+                if (strNumber.charAt(j) == '3' || strNumber.charAt(j) == '6' || strNumber.charAt(j) == '9') {
+                    tmpHowManyNums++;
                 }
             }
         }
+
+        return tmpHowManyNums;
     }
 }
