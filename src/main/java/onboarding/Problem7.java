5c6bf485441a8d2b90f9c73f78aaae9c096dda2d
noah.lee <rhkd2612@naver.com>
2022-10-30 06:16:55 +0900

    feat: init friends map

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,10 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /*
  * # 기능 정리
- * [X] 친구 관계 저장
+ * [O] 친구 관계 저장
  * [X] 방문자 점수 계산
  * [X] 함께 아는 친구 계산
  * [X] 추천 친구 목록 계산(원래 친구 제외)
@@ -17,6 +16,26 @@ import java.util.List;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Map<String, List<String>> friendsMap = new HashMap<>();
+
+        initFriendsMap(friends, friendsMap);
+
         return answer;
+    }
+
+    private static void initFriendsMap(List<List<String>> friends, Map<String, List<String>> friendsMap) {
+        for(int i = 0; i < friends.size(); i++){
+            String userA = friends.get(i).get(0);
+            String userB = friends.get(i).get(1);
+
+            if(!friendsMap.containsKey(userA))
+                friendsMap.put(userA, new ArrayList<>());
+            if(!friendsMap.containsKey(userB))
+                friendsMap.put(userB, new ArrayList<>());
+
+            friendsMap.get(userA).add(userB);
+            friendsMap.get(userB).add(userA);
+        }
     }
 }
