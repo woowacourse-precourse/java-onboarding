89ac6978cfe45efa564665ef623e1729e66630e6
HoeSeong <ghltjd369@gmail.com>
2022-10-29 20:34:58 +0900

    feat : (problem 5) 큰 금액부터 나누고 나머지를 구하는 방식을 반복하여 answer를 구하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,6 +10,11 @@ public class Problem5 {
 
         int[] money_list = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        for(int i = 0; i < money_list.length; i++){
+            answer.add(money / money_list[i]);
+            money %= money_list[i];
+        }
+
         return answer;
     }
 }
