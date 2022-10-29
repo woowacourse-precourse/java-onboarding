ceda2c9c519f791197dbbad81a06b18015d6e05b
dOcOb <abg1418@gmail.com>
2022-10-29 13:33:48 +0900

    complete problem1

---
+++ src/main/java/onboarding/Test.java
@@ -0,0 +1,12 @@
+package onboarding;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> pobi = List.of(97, 98);
+        List<Integer> crong = List.of(197, 198);
+
+        System.out.println(Problem1.solution(pobi, crong));
+    }
+}
