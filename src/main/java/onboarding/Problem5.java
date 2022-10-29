25335e6c6be7f2f7107d0fcbd1a4d38336941905
minaver <psmjoshua@gmail.com>
2022-10-29 10:41:47 +0900

    feat(problem5):금액에서 원하는 지폐로 얻을 수 있는 수 구하는 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,28 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+/**
+ * 0. 기본 조건
+ * - money는 1 이상 1,000,000 이하인 자연수이다.
+ * - 50,000 / 10,000 / 5,000 / 1,000 / 500 / 100 / 50 / 10 / 1
+ *
+ * 1. 접근 방식
+ * - 50000원부터 1원까지 큰 금액부터 점점 빼 나가 카운트 해준다.
+ * */
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        Problem5 problem5 = new Problem5();
         List<Integer> answer = Collections.emptyList();
+
+        answer.add(problem5.countBill(money, 50000));
+
+
         return answer;
     }
+
+    // 금액에서 원하는 지폐로 얻을 수 있는 수 구하기
+    public int countBill(int money, int targetBill){
+        return (money/targetBill);
+    }
+
 }
