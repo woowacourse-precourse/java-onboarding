202ca69b78d7b95fbecc2e8b6cb88eb220ac2cca
shinyehwan <jghisjl007@naver.com>
2022-10-29 16:43:00 +0900

    Problem2 기능 구현(재귀 함수)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		return removeDuplication(cryptogram);
+
+	}
+
+	private static String removeDuplication(String str) {
+		char recent = 0;
+		boolean isRemoved = false;
+		for (int i = 0; i < str.length(); i++) {
+
+			if (i > 0 && recent == str.charAt(i)) {
+				str = str.substring(0, i - 1) + str.substring(i + 1);
+				isRemoved = true;
+				break;
+			}
+			recent = str.charAt(i);
+		}
+		if (isRemoved) {
+			str = removeDuplication(str);
+		}
+		return str;
+	}
+
 }
