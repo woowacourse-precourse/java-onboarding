82c544d81b54e1b6637e190b1f4094d1ed1b698c
YuHayeon <youha0930@gmail.com>
2022-10-30 00:02:16 +0900

    feat(Problem2): 입력 범위에 맞지 않거나 소문자가 아닐 경우 예외처리
    길이가 1 미만 1000 초과이거나
    소문자가 아닌 경우 예외처리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+		return answer;
+	}
+
+	static class InputValidator {
+
+		public static void checkRightInput(String cryptogram) {
+			checkRightRange(cryptogram);
+			checkLowerCase(cryptogram);
+		}
+
+		private static void checkRightRange(String cryptogram) {
+			int cryptogramLength = cryptogram.length();
+			if (cryptogramLength < 1 || cryptogramLength > 1000) {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		private static void checkLowerCase(String cryptogram) {
+			char[] charArray = cryptogram.toCharArray();
+
+			for (int i = 0; i < charArray.length; i++) {
+				if (!Character.isLowerCase(charArray[i])) {
+					throw new IllegalArgumentException();
+				}
+			}
+		}
+	}
+
 }
