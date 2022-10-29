d4a5000961f4ddeb2c419029d409cd3c2faf0917
skfkgla <narahim.lee@gmail.com>
2022-10-30 05:27:18 +0900

    feat: Problem5 - 자연수를 모은 배열을 기준으로 순서대로 리스트에 담아 반환하는 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +9,22 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    /**
+     * @param money : 자연수
+     * @param unit : 자연수 배열(돈의 단위 이므로 큰 순서대로 넣을 것)
+     * @return unit의 배열 순서대로 리스트에 담아 개수를 반환
+     */
+    public static List<Integer> calculateExchange(int money, int[] unit){
+        int count = 0;
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; unit.length > i; i++){
+            result.add(money/unit[i]);
+            money %= unit[i];
+        }
+
+        return result;
     }
 }
