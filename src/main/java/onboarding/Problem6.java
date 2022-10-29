883f575f3aeb0babea999d792cdd0665fc9b2060
drunkenhw <drunkenhw@gmail.com>
2022-10-29 19:29:24 +0900

    feat(Problem6) : 리스트를 크루별로 나눈다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,17 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String, String> crew = getCrewMap(forms);
         return answer;
+    }
+
+    private static Map<String, String> getCrewMap(List<List<String>> forms) {
+        Map<String, String> crew = new HashMap<>();
+        forms.forEach(i -> crew.put(i.get(1), i.get(0)));
+        return crew;
     }
 }
