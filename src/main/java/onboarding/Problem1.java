9199b794a7eb4770026e301ffeafa56a10a0467e
한은영 <oneeqzero@gmail.com>
2022-10-29 16:42:23 +0900

    feat: problem1 나머지 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,14 +5,37 @@ import java.util.List;
 class Problem1 {
     public static boolean check_exception(List<Integer> li){
         int left = li.get(0); int right = li.get(1);
-        if((left&1) == 0 || (right&1) == 1) return false;
-        if(left + 1 != right) return false;
-        if(left<1 || left>400) return false;
-        if(right<1 || right>400) return false;
-        return true;
+        if((left&1) == 0 || (right&1) == 1) return true;
+        if(left + 1 != right) return true;
+        if(left<1 || left>400) return true;
+        if(right<1 || right>400) return true;
+        return false;
+    }
+    public static int op_val(List<Integer> li){
+        int left = li.get(0); int right = li.get(1);
+        int lmul = 1, lsum = 0;
+        while(left>0) {
+            lmul *= left%10;
+            lsum += left%10;
+            left /= 10;
+        }
+        int lval = Math.max(lmul,lsum);
+
+        int rmul = 1, rsum = 0;
+        while(right>0){
+            rmul *= right%10;
+            rsum += right%10;
+            right /= 10;
+        }
+        int rval = Math.max(rmul,rsum);
+
+        return Math.max(lval,rval);
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(check_exception(pobi) || check_exception(crong)) return -1;
+        int p = op_val(pobi); int c = op_val(crong);
+        if(p>c) return 1;
+        else if(p<c) return 2;
+        else return 0;
     }
 }
