8581652b4b5775c57c57f159b9c4955dd5ad9e7a
cksdud8028 <cksdud8028@naver.com>
2022-10-29 17:03:15 +0900

    FEAT checkingNumber and function list

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+    private static List<Integer> extractingNumbers(int number) {
+        for (int i = 1; i <= number; i++) {
+            if ()
+        }
+    }
+
+    private static boolean checkingNumber(int number) {
+        while (true) {
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                return true;
+            }
+            if (number / 10 == 0) {
+                return false;
+            }
+            number /= 10;
+        }
+    }
+
+    private static int includedCountingCheck(int number) {
+
+    }
+
 }
