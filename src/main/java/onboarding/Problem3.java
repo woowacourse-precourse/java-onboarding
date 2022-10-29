77e1b94ccf110f01d99124aa32782929c1dfae6a
tlsqj <rnjstlsqja@inha.edu>
2022-10-29 15:50:58 +0900

    문제 3번 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int gameNumber = 1; gameNumber <= number; gameNumber++) {
+            int tmpGameNumber = gameNumber;
+            while(tmpGameNumber != 0){
+                if(tmpGameNumber % 10 == 3 || tmpGameNumber % 10 == 6 || tmpGameNumber % 10 == 9) answer++;
+                tmpGameNumber /= 10;
+            }
+        }
         return answer;
     }
 }
