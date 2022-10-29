a7a07d16e5bd25fe8d1dca13749895beff501384
Yunsik-Choi <choiys0311@gmail.com>
2022-10-29 18:38:13 +0900

    ♻️ refactor : Problem3 해결

--- src/main/java/onboarding/problem3/service/NumberGameService.java
+++ src/main/java/onboarding/problem3/service/NumberGameService.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 
 public class NumberGameService {
 
+    private static final int GAME_START_NUMBER = 1;
     private final GameStrategy gameStrategy;
 
     public NumberGameService(GameStrategy gameStrategy) {
@@ -13,7 +14,7 @@ public class NumberGameService {
     }
 
     public int result(int n) {
-        return IntStream.rangeClosed(1, n)
+        return IntStream.rangeClosed(GAME_START_NUMBER, n)
                 .reduce(0, (result, number) -> result + gameStrategy.count(number));
     }
 }
