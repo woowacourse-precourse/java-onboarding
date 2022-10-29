570e1e2b06d5ff07e57a68b2b6b22d4cc3be43d5
recicoco068 <gkapf838@gmail.com>
2022-10-29 23:43:06 +0900

    feat : Problem6 조건에 해당하는 이메일들을 result에 중복 없이 추가하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> result = fillerEmail(forms);
+        return result;
+    }
+
+    public static List<String> fillerEmail(List<List<String>> forms) {
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i < forms.size() - 1; i++) {
+            if (forms.get(i).get(1).length() == 1)
+                continue;
+            for (int j = i + 1; j < forms.size(); j++) {
+                if (compareNicknames(forms.get(i).get(1).toCharArray(), forms.get(j).get(1).toCharArray())) {
+                    result.add(forms.get(i).get(0));
+                    result.add(forms.get(j).get(0));
+                }
+            }
+        }
+        return new ArrayList<>(result);
     }
 
     public static boolean compareNicknames(char[] nick1, char[] nick2) {
