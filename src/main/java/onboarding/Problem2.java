a08426b6c26385cfac240eee6b14820cdde43fb5
seonho <ysh10216@gmail.com>
2022-10-29 14:29:40 +0900

    write a solution step

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        // 1. cryptogram 예외사항 검사함수 만들기
+        // cryptogram이 대문자가 들어간 경우거나 길이가 1~1000을 벗어난 경우 false return
+
+        // 2. 연속하는 중복문자를 삭제하는 함수 만들기
+
         return answer;
     }
 }
