12ca2bf2b8f86471973e00ca6818117907ecda8f
youngkyunIm <yee0230@naver.com>
2022-10-29 18:37:48 +0900

    feat(Problem6) : loof forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+
+        }
+
+        answer.addAll(set);
+        Collections.sort(answer, String.CASE_INSENSITIVE_ORDER);
         return answer;
     }
 }
