dc30d0cd63b7db9e8830739ecad83ba906220bf4
iamsseaa <homingsea@gmail.com>
2022-10-29 17:11:52 +0900

    feat(Problem2): 입력 문자열을 문자 리스트로 바꾸는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        List<Character> cryptogramList = cryptogram.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        List<Character> result = new ArrayList<>(cryptogramList);
+
         String answer = "answer";
         return answer;
     }
