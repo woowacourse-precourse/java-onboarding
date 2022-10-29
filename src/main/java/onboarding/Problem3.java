0fbc88b26620a4338e8f9aa0fcd448f99c9d2b4e
bellljun3395 <195850@jnu.ac.kr>
2022-10-29 20:32:05 +0900

    feat : Input3 클래스 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+}
+
+class Input3 {
+    static int inputNumber() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
 }
