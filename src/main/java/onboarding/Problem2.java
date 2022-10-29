7ebd52a985cbcde5d86f9d3bbb2f004cdcedb2a1
Egenieee <hhgg0925@naver.com>
2022-10-29 22:47:19 +0900

    [FEAT] Problem2 중복 문자열 제거하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    private static final Stack<Character> stack = new Stack<>();
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        for (int index = 0 ; index < cryptogram.length() ; index++) {
+            if (stack.isEmpty()) {
+                stack.add(cryptogram.charAt(index));
+            } else {
+                removeDuplicateString(index, cryptogram);
+            }
+        }
+
+        return getOriginalInformation();
+    }
+
+    private static void removeDuplicateString(int index, String cryptogram) {
+        if (stack.peek() == cryptogram.charAt(index)) {
+            stack.pop();
+        } else {
+            stack.add(cryptogram.charAt(index));
+        }
+    }
+
+    private static String getOriginalInformation() {
+        StringBuilder cryptogram = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            cryptogram.append(stack.pop());
+        }
+
+        return cryptogram.reverse().toString();
     }
 }
