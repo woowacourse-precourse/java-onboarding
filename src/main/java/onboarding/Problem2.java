580ee653d370b4ec995de67633a4b3559bda7739
woo-chang <clllickme@naver.com>
2022-10-29 18:39:13 +0900

    refactor(문제 2): 암호 해독 책임을 가지는 Code class 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,22 +5,31 @@ import java.util.stream.Collectors;
 
 public class Problem2 {
 
+  static Code code = new Code();
+
   public static String solution(String cryptogram) {
-    Stack<Character> stack = new Stack<>();
+    return code.decode(cryptogram);
+  }
 
-    int index = 0;
-    while (index < cryptogram.length()) {
-      if (stack.isEmpty() || !stack.peek().equals(cryptogram.charAt(index))) {
-        stack.push(cryptogram.charAt(index));
-        ++index;
-        continue;
+  static class Code {
+
+    public String decode(String cryptogram) {
+      Stack<Character> stack = new Stack<>();
+
+      int index = 0;
+      while (index < cryptogram.length()) {
+        if (stack.isEmpty() || !stack.peek().equals(cryptogram.charAt(index))) {
+          stack.push(cryptogram.charAt(index));
+          ++index;
+          continue;
+        }
+        while (index < cryptogram.length() && stack.peek().equals(cryptogram.charAt(index))) {
+          ++index;
+        }
+        stack.pop();
       }
-      while (index < cryptogram.length() && stack.peek().equals(cryptogram.charAt(index))) {
-        ++index;
-      }
-      stack.pop();
+
+      return stack.stream().map(Object::toString).collect(Collectors.joining());
     }
-
-    return stack.stream().map(Object::toString).collect(Collectors.joining());
   }
 }
