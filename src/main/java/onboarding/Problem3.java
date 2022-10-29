a5f0b08a961004841635d1a15ffa08cf2faf33b9
glaehfdl1654 <glaehfdl1654@naver.com>
2022-10-29 16:40:07 +0900

    [Problem3] getClapNum function complete

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,14 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    public static int getClapNum(int n) {
+        int ret = 0;
+        while (n > 0) {
+            if ((n % 10 == 3) || (n % 10 == 6) || (n % 10 == 9)) {
+                ret++;
+            }
+            n /= 10;
+        }
+        return ret;
+    }
 }
