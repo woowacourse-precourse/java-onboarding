908c8e2be062f030cb0dd22f02809bcb6e0a1051
wonju-dev <wonjulee.dev@gmail.com>
2022-10-29 19:02:01 +0900

    [feat] problem2 메인 로직 구현
    리펙토링 필요

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        int index = 0;
+
+        while (index < cryptogram.length()) {
+            char currentChar = cryptogram.charAt(index);
+
+            if (stack.isEmpty() || stack.peek() != currentChar) {
+                stack.push(currentChar);
+            } else {
+                do {
+                    index++;
+                }
+                while (index < cryptogram.length() && stack.peek() == cryptogram.charAt(index));
+                stack.pop();
+                index--;
+            }
+            index++;
+        }
+
+        return stackToString(stack);
+    }
+
+    private static String stackToString(Stack<Character> stack) {
+        String string = "";
+        for (Character c : stack) {
+            string += c;
+        }
+
+        return string;
     }
 }
