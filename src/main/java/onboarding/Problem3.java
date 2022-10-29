23ee52dfc4ab2b4bb506fa6c7cf0fe5f5478f480
JunTaeINC <juntea3523@naver.com>
2022-10-29 14:31:44 +0900

    Problem 3 - 3,6,9 박수횟수 구하는 기능구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem3 {
+	
+	public static int tsnAnswer(Integer num) {
+		int cnt = 0;
+		// 1 부터 num 까지 반복  
+		for (int i = 1 ; i <= num ; i++) {
+			
+			// charAt 사용을 위해 문자열로 변환 
+			String strNum = String.valueOf(i);
+			// 문자 자리수 만큼 반복
+			for (int j = 0 ; j < strNum.length() ; j++) {
+				// 숫자중에 3,6,9 있으면 cnt +1    
+				if(strNum.charAt(j) == '3' || strNum.charAt(j) == '6' || strNum.charAt(j) == '9') {
+					cnt += 1;
+				}
+				
+			}
+		}return cnt;	
+	}
+	
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    	return tsnAnswer(number);
     }
 }
