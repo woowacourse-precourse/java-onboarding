c46c0cbba50d14a22e5c41c5732af44220d0347b
hyejungg <kimhyejung12@naver.com>
2022-10-30 01:10:53 +0900

    feat: [문제 5] 입력값 검증 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,14 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+		if (!isValidNumber(money)) {
+			return Collections.emptyList();
+		}
+
+	private static boolean isValidNumber(int money) {
+		return money >= 1 && money <= 1000000;
+	}
+
         return answer;
     }
 }
