b1f9f51d6e1aef52698829c1408d546e5be637f4
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 19:38:28 +0900

    :sparkles: [문제 1] : 1. 예외사항 -1 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,9 @@ class Problem1 {
         // 3. crong의 점수 도출
         // 4. pobi와 crong의 점수 비교
         // 5. 결과 값 반환
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
     }
