e738ceb480a5ea02de607c23a5eea297adb7ad2e
wonju-dev <wonjulee.dev@gmail.com>
2022-10-29 16:33:10 +0900

    [feat] 문제에서 사용할 상수 정의
    다음과 같이 상수를 정의함
    LEFT = 0 // 왼쪽 페이지
    RIGHT = 1 // 오른쪽 페이지
    POBI_WIN = 1 // pobi의 승리
    CRONG_WIN = 2 // crong의 승리
    DRAW = 0; // 무승부
    EXCEPTION = -1; // 예외 상황
    BOOK_START_PAGE = 1; // 입력값의 최소 허용 범위
    BOOK_END_PAGE = 400; // 입력값의 최대 허용 범위

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,18 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static final int LEFT = 0;
+    static final int RIGHT = 1;
+
+    static final int POBI_WIN = 1;
+    static final int CRONG_WIN = 2;
+    static final int DRAW = 0;
+    static final int EXCEPTION = -1;
+
+    static final int BOOK_START_PAGE = 1;
+    static final int BOOK_END_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
