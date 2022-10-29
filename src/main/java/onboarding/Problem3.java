0241ed8fb20b898090cb1b37f0861713430ef73f
조미정 <ing06164@naver.com>
2022-10-29 21:04:54 +0900

    문제 3 first try

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int digit=0;   // 자릿수
+
+        while (number > 0) {
+            //answer += (number%10);
+            if(number%10 == 3){
+                answer *= Math.pow(10, digit);
+            }
+            answer += (number/3);
+
+            number = number / 10;
+
+            digit++;
+        }
+        System.out.println("369횟수: "+answer);
+
         return answer;
     }
 }
