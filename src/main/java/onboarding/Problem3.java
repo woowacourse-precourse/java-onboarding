5d41edfac363f4d44efbd63dd79684573ec0bb84
정핸모 <gusah009@naver.com>
2022-10-29 14:11:18 +0900

    feat: 3 6 9 가 들어가는 숫자를 세는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,21 @@ public class Problem3 {
 
   public static int solution(int number) {
     int answer = 0;
+    for (int i = 1; i <= number; i++) {
+      answer += getCount369In(i);
+    }
     return answer;
+  }
+
+  private static int getCount369In(int number) {
+    int result = 0;
+    while (number != 0) {
+      int digit = number % 10;
+      if (digit == 3 || digit == 6 || digit == 9) {
+        result++;
+      }
+      number /= 10;
+    }
+    return result;
   }
 }
