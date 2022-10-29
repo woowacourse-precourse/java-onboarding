c116f6feaceff0deb7142e5220ef2d5cf1de8618
Invidam <hansupark0208@gmail.com>
2022-10-29 20:01:18 +0900

    refactor(Problem5): extract methods check logic and calc logics
    1. extract method check is money enough logic.
    2. extract methods calc unit count and get left money logic.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,24 +8,38 @@ public class Problem5 {
 
     static final int[] UNITS = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
 
-
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        return getResultUnitList(money);
+    }
 
+    private static List<Integer> getResultUnitList(int money) {
+        List<Integer> answer = new ArrayList<>();
         int leftMoney = money;
 
-        for(int unit: UNITS) {
-            if(leftMoney < unit) {
+        for (int unit : UNITS) {
+            if (!isEnoughMoney(leftMoney, unit)) {
                 answer.add(0);
                 continue;
             }
 
-            int unitCount = leftMoney / unit;
-            answer.add(unitCount);
-
-            leftMoney %= unit;
+            answer.add(getUnitCount(leftMoney, unit));
+            leftMoney = getLeftMoney(leftMoney, unit);
         }
 
         return answer;
+    }
+
+    private static int getLeftMoney(int leftMoney, int unit) {
+        leftMoney %= unit;
+        return leftMoney;
+    }
+
+    private static int getUnitCount(int leftMoney, int unit) {
+        int unitCount = leftMoney / unit;
+        return unitCount;
+    }
+
+    private static boolean isEnoughMoney(int leftMoney, int unit) {
+        return leftMoney >= unit;
     }
 }
