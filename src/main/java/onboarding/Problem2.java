00cb581f59069b66c78e51c7c4c69388a5d35d06
Ridealist <cjsn92@gmail.com>
2022-10-30 01:04:21 +0900

    feat:Problem2 : Encoder 클래스 및 deleteDuplicate 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,3 +6,29 @@ public class Problem2 {
         return answer;
     }
 }
+
+class Encoder {
+    String deleteDuplicate(String str) {
+        String new_str = "";
+        int l = str.length();
+        int i = 0;
+        while (i < l-1) {
+            if (str.charAt(i) == str.charAt(i+1)) {
+                int dup_cnt = 1;
+                while ( i + dup_cnt < l) {
+                    if (str.charAt(i) == str.charAt(i + dup_cnt + 1)) {
+                        dup_cnt += 1;
+                    } else {break;}
+                }
+                i += dup_cnt + 1;
+            }
+            else {
+                new_str += str.charAt(i);
+                i += 1;
+            }
+        }
+        if (str.charAt(l-1) == str.charAt(l-2)) {
+            return new_str;
+        } else {return new_str + str.charAt(l-1);}
+    }
+}
