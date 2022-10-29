5cf4b8d7ad6d96ecc44cd04c352b6e5ebc154590
seheonCho <whtpgjs94@gmail.com>
2022-10-30 01:51:30 +0900

    feat: [문제5] 재할당이 불가능한 화폐 종류의 갯수 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,7 @@ public class Problem5 {
     private static final int[] CURRENCYS_DESCENDING_ORDER = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
+        final int NUMBER_OF_CURRENCYS = CURRENCYS_DESCENDING_ORDER.length;
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
