2e14869fa173a613bfaa369a86e12e3ecac32581
hoonn9 <hun9935@gmail.com>
2022-10-29 17:52:25 +0900

    Add backward alphabet method (Problem4)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        System.out.println(backwardAlphabet('h'));
         return answer;
     }
+
+    private static Character backwardAlphabet(char c) {
+        if (!validation(c)) {
+            return c;
+        }
+
+        if (c >= 'A' && c <= 'Z') {
+            return (char) ('Z' - (c - 'A'));
+        }
+
+        return (char) ('z' - (c - 'a'));
+    }
+
+    private static boolean validation(char c) {
+        return Character.isAlphabetic(c);
+    }
+
 }
+
+
+
