2a3ede92f4f4817a6cb2e278d5380253672dc176
jiwon <ljwon77@daum.net>
2022-10-29 23:00:57 +0900

    feat(problem4): check the value is not alphabetic

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,8 +4,24 @@ public class Problem4 {
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder("");
         char[] wordArr = getCharArr(word);
-        
+
+        for(Character wordVal : wordArr) {
+            if(isNotAlphabetic(wordVal)) {
+                answer.append(' ');
+                continue;
+            }
+        }
         return answer.toString();
+    }
+
+    /**
+     * 요소가 알파벳이 아닌지 판단한다.
+     *
+     * @param value 판단할 요소
+     * @return 알파벳이 아니라면 true, 맞다면 false
+     */
+    private static boolean isNotAlphabetic(Character value) {
+        return !Character.isAlphabetic(value);
     }
 
     /**
