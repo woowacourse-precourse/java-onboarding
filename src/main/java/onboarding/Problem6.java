d5ba95c2e8c5b06df198167bfd0a0ddeef6bd0a3
Heesom <heesom.hs@gmail.com>
2022-10-29 21:59:08 +0900

    Problem6 : compareString 메소드 추가
    문자열끼리 중복되는 문자가 있는지 확인하는 메소드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,23 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static boolean compareString(String s1, String s2) {
+        char[] c1Arr = s1.toCharArray();
+        char[] c2Arr = s2.toCharArray();
+
+        for(int c1=0; c1<c1Arr.length-1; c1++){
+            StringBuilder c1Double = new StringBuilder();
+            c1Double.append(c1Arr[c1]).append(c1Arr[c1+1]);
+
+            for(int c2=0; c2<c2Arr.length-1; c2++) {
+                String c1Str = c1Double.toString();
+                String c2Str = String.valueOf(c2Arr[c2]) + c1Arr[c2 + 1];
+                if(c1Str.equals(c2Str)){
+                    return true;
+                }
+            }
+        }
+        return  false;
+    }
 }
