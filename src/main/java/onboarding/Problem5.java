c034fd72182b9550a75ed1e3283104928f59d98b
alstjr706 <alstjr706@naver.com>
2022-10-29 18:05:27 +0900

    feat : (은행 출금 문제) input을 받아 출금하는 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import onboarding.problem5.Bank;
+import onboarding.problem5.BanknoteUnit;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        Bank bank = new Bank();
+        List<Integer> result = bank.withdraw(money);
+
+        return result;
     }
 }
