ce76b3e9446099fb6c45aeca743b12be1c49d2da
nswon <swnam0729@gmail.com>
2022-10-29 16:10:04 +0900

    feat(Problem7): 사용자의 친구를 저장하는 로직 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,10 +3,12 @@ package onboarding;
 import onboarding.problem7.User;
 import onboarding.problem7.Users;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem7 {
     private static Users users;
+    private static List<String> userFriends;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         init();
@@ -17,6 +19,7 @@ public class Problem7 {
 
     private static void init() {
         users = new Users();
+        userFriends = new ArrayList<>();
     }
 
     private static void createUserInFriends(List<List<String>> friends, String user) {
@@ -25,6 +28,14 @@ public class Problem7 {
                 if(!friend.equals(user)) {
                     User findUser = users.findUser(friend);
                     findUser.addFriendScore();
+                }
+                else {
+                    if(friendship.indexOf(user) == 1) {
+                        userFriends.add(friendship.get(0));
+                    }
+                    else if(friendship.indexOf(user) == 0) {
+                        userFriends.add(friendship.get(1));
+                    }
                 }
             }
         }
