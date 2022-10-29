28f6dbfcabd2f40493583bcf45fc0852c2969b3d
chj <bluedog129@gmail.com>
2022-10-29 17:33:37 +0900

    [P5] money가 0이 될때까지 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,47 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int calculateMoney(int money) {
+        return money;
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> result = Collections.emptyList();
+        int[] moneyArray;
+        moneyArray = new int[9];
+
+        while (money > 0) {
+            if (money % 50000 == 0) {
+                money /= 50000;
+                moneyArray[0] += 1;
+            } else if (money % 10000 == 0) {
+                money /= 10000;
+                moneyArray[1] += 1;
+            } else if (money % 5000 == 0) {
+                money /= 5000;
+                moneyArray[2] += 1;
+            } else if (money % 1000 == 0) {
+                money /= 1000;
+                moneyArray[3] += 1;
+            } else if (money % 500 == 0) {
+                money /= 500;
+                moneyArray[4] += 1;
+            } else if (money % 100 == 0) {
+                money /= 100;
+                moneyArray[5] += 1;
+            } else if (money % 50 == 0) {
+                money /= 50;
+                moneyArray[6] += 1;
+            } else if (money % 10 == 0) {
+                money /= 10;
+                moneyArray[7] += 1;
+            } else if (money % 1 == 0) {
+                money /= 1;
+                moneyArray[8] += 1;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            result.add(moneyArray[i]);
+        }
+        return result;
     }
 }
