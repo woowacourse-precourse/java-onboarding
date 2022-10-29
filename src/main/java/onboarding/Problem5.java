d477eec4e438d516788b5de2075fd2d1b2e836d9
starwook <star.wookk@gmail.com>
2022-10-29 21:01:40 +0900

    add moneyUnitCount method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,22 +8,32 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        List<Integer> moneyList = new ArrayList<>();
-        moneyList = moneyInitList(moneyList);
-        answer = initList(answer);
-
+        answer = reAnswerList(answer,money);
         return answer;
     }
-    public static List<Integer>  initList(List<Integer> list){
-        for(int i=0;i<9;i++){
-            list.add(0);
+    public static List<Integer> reAnswerList(List<Integer>list ,int money){
+        List<Integer> moneyUnitList = new ArrayList<>();
+        moneyUnitList = moneyInitList(moneyUnitList);
+        for(int i=0;i<moneyUnitList.size();i++){
+            int howMany = moneyUnitCount(money,moneyUnitList.get(i));
         }
         return list;
     }
+    public static int moneyUnitCount(int money,int moneyUnit){
+        int howMany =0;
+        while(money >=moneyUnit){
+            howMany++;
+        }
+        return howMany;
+    }
+
     public static List<Integer> moneyInitList(List<Integer> list){
         list = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
         return list;
     }
-   
+
+
+
 
 }
+ㅎ
