b22c1a60ca3ce2cee34dcba9a555e6c15a16b457
ddingmin <ddingminyt@gmail.com>
2022-10-30 04:55:24 +0900

    feat(Ploblem1): 입력 예외처리 기능 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,27 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (!checkPages(pobi) || !checkPages(crong)){
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static Boolean checkPages(List<Integer> pages){
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        if (!isCorrectPage(leftPage) || !isCorrectPage(rightPage)){
+            return false;
+        }
+        if (leftPage % 2 != 1 || rightPage % 2 != 0){
+            return false;
+        }
+        return (leftPage + 1) == rightPage;
+    }
+
+    private static Boolean isCorrectPage(int page){
+        return (page > 2) && (page < 399);
+    }
+
 }
