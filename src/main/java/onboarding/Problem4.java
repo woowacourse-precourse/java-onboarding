cec1304bdea8bc4ca97295df661353c8569a141c
Egenieee <hhgg0925@naver.com>
2022-10-30 00:01:33 +0900

    [FEAT] Problem4 해당 문자열 하나씩 convert 하는 기능
    - 대문자, 소문자 나눠서 처리되도록 한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,33 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int index = 0 ; index < word.length() ; index++) {
+            String convertedString = getConvertedString(word, index);
+            stringBuilder.append(convertedString);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static String getConvertedString(String word, int index) {
+        char originalLetter = word.charAt(index);
+
+        if (Character.isUpperCase(originalLetter)) {
+            return convertUpperCase(originalLetter);
+        } else if (Character.isLowerCase(word.charAt(index))) {
+            return convertLowerCase(originalLetter);
+        }
+
+        return " ";
+    }
+
+    private static String convertUpperCase(char originalLetter) {
+        return null;
+    }
+
+    private static String convertLowerCase(char originalLetter) {
+        return null;
     }
 }
