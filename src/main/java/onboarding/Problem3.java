02a19dbb25997080436dad5143729a8863092512
RO-OR <kyk3531@gmail.com>
2022-10-30 06:43:41 +0900

    fix:3번 기능 범위 지정 부분 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,9 @@ public class Problem3 {
         String str;
         int answer = 0;
 
+        if(number<1 || 10000<number) //잘못된 범위 일떄 -1
+            return -1;
+
         for(int i=1; i<=number; i++){
             str = String.valueOf(i); //i를 문자열로 변환
             answer += str.chars().filter(c->c=='3' || c=='6' || c=='9').count(); //stream과 필터를 활용
