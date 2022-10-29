671cc9e6633ddd328bf4a9edeee84ae3062a9160
DaWoon Lee <qkgk61@gmail.com>
2022-10-30 02:32:49 +0900

    Test : Add test case to test two duplicated names

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -157,6 +157,20 @@ class ApplicationTest {
             List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
             assertThat(Problem6.solution(forms)).isEqualTo(result);
         }
+
+        @Test
+        void case2() {
+            List<List<String>> forms = List.of(
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@email.com", "제이슨"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("nowm@email.com", "이제엠"),
+                    List.of("wonnnie@email.com", "이워니")
+            );
+            List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com", "woniee@email.com", "wonnnie@email.com");
+            assertThat(Problem6.solution(forms)).isEqualTo(result);
+        }
     }
 
     @Nested
