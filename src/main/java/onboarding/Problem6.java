e0cc9886c094600b1901ba7949033f8efd83342c
seonghoo1217 <seonghoo1217@naver.com>
2022-10-29 17:48:39 +0900

    feat : Add a Crew Limit Check Exception on [Task6]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>();
+        try {
+            checkCrewLimit(forms.size());
+            return answer;
+        }catch (Exception e){
+            return answer;
+        }
     }
+
+    public static void checkCrewLimit(int crew_count) throws Exception {
+        if (crew_count<1||crew_count>10000) throw new Exception("인원 제한 초과");
+    }
+
 }
