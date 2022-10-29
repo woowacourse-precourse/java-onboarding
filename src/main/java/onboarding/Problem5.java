0bf73448a47adffa9cddcc0f68dd4f3c7cfd4640
AhnJoonSung <tjdtna01@naver.com>
2022-10-30 08:46:03 +0900

    Problem5 solve

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] units = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+        List<Integer> result = new ArrayList<>(units.length);
+        int count;
+
+        for (int i = 0; i < units.length; i++) {
+            count = money / units[i];
+            result.add(count);
+            money %= units[i];
+        }
+
+        return result;
     }
 }
