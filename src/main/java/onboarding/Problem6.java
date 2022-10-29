fbf2b5aa64d8cc4cb876f62c1795600a7c9194b5
Limjongho <whdgh9873@icloud.com>
2022-10-29 18:03:49 +0900

    feat(Problem6): 주어진 forms 에서 닉네임을 추출하여 두글자 단위로 쪼개는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<String> getNicknamesInForms(List<List<String>> forms) {
+        return forms.stream()
+                .map(form -> form.get(1))
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> getSplitNicknames(String nickname) {
+        List<String> splitNicknames = new ArrayList<>();
+
+        int length = nickname.length();
+        int sequence = 0;
+        while (sequence < length) {
+            String splitNickname = nickname.substring(sequence, sequence + 1);
+            splitNicknames.add(splitNickname);
+            sequence++;
+        }
+
+        return splitNicknames;
     }
 }
