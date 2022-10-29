d1d8675c5d8349045f597e35fc4a5c1758204c2a
Youngho <cyh6099@gmail.com>
2022-10-29 23:34:17 +0900

    feat(p7) : 정렬을 위한 UserInfo클래스 작성, 이를 이용해 List정렬 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,18 +1,44 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> friendsList;
     public static Map<String, Integer> recommendScore;
     public static Map<String, List<String>> relationships;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<UserInfo> answerList = new ArrayList<>();
         friendsList = new ArrayList<>();
         recommendScore = new HashMap<>();
         relationships = new HashMap<>();
+        for (List<String> friend : friends) {
+            initFriendsList(friend, user);
+            initRelationships(friend,user);
+        }
+        List<String> userFriendList = friendsList.stream()
+                .distinct()
+                .collect(Collectors.toList());
 
-        List<String> answer = Collections.emptyList();
-        return answer;
+        for (String username : userFriendList) {
+            relationships.get(username)
+                    .forEach(x -> addRecommendScore(x,10));
+        }
+
+        visitors.forEach(x -> addRecommendScore(x,1));
+
+
+        recommendScore.entrySet().stream()
+                .filter(x -> !userFriendList.contains(x.getKey()))
+                .forEach(x -> answerList.add(new UserInfo(x.getKey(),x.getValue())));
+
+
+        return answerList.stream()
+                .sorted(Comparator.comparing(UserInfo::getScore, Comparator.reverseOrder())
+                        .thenComparing(UserInfo::getUsername))
+                .map(UserInfo::getUsername)
+                .collect(Collectors.toList());
     }
 
     public static void initFriendsList(List<String> relationship, String user) {
@@ -52,5 +78,23 @@ public class Problem7 {
             return;
         }
         recommendScore.put(username,score);
+    }
+
+    static class UserInfo{
+        String username;
+        int score;
+
+        public UserInfo(String username, int score) {
+            this.username = username;
+            this.score = score;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public int getScore() {
+            return score;
+        }
     }
 }
