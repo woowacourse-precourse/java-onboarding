da97b2242921996f41e7739bda5b41fa61fdda4e
zcae1020 <zcae1020@naver.com>
2022-10-29 21:38:08 +0900

    feat(Problem3): method frame

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,14 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    
+    //숫자내 3,6,9 세기
+    private static int countThreeSixNine(int num) {
+        
+    }
+    
+    //3,6,9인지 체크
+    private static boolean isThreeSixNine(int num) {
+        
+    }
 }
