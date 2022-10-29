e3e803f8bb73623af73cba24bb0bc7f827be4ca2
hhiyeon <hhyyeon0214@gmail.com>
2022-10-29 16:46:10 +0900

    feat: add element to deque

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,15 +1,22 @@
 package onboarding;
 
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
     private static boolean isValidStringLength(String str) {
         return str.length() == 1;
     }
 
     private static char[] getStringToChar(String str) {
         return str.toCharArray();
+    }
+
+    private static void addElementToDeque(Deque<Character> deque, char element) {
+        deque.add(element);
     }
 }
