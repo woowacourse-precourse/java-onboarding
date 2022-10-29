e765c40518b487cac7986fbb75d11884d2bd40cc
chaos0103 <lyt1228@naver.com>
2022-10-29 17:05:25 +0900

    feat(Problem7): RecommendedFriend 클래스 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,37 @@ package onboarding;
 import java.util.*;
 import java.util.stream.Collectors;
 
+class RecommendedFriend implements Comparable<RecommendedFriend> {
+
+    private String name;
+    private int score;
+
+    public RecommendedFriend(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public int compareTo(RecommendedFriend other) {
+        if (this.score < other.score) {
+            return -1;
+        } else if (this.score == other.score) {
+            if (this.name.compareTo(other.name) > 0) {
+                return -1;
+            }
+        }
+        return 1;
+    }
+}
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> userFriendList = findFriendListByUser(user, friends);
