50878d23c1d4aa648ba41a8633c5c17e5ea92838
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 20:25:34 +0900

    :memo: [문제 5] : 풀이 전략

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,16 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        // 1. 화폐단위 배열 생성
+        // 2. 입력된 money를 withdraw 함수로 해석
+        // 3. 해석된 결과 리스트 반환
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    static void withdraw(int money, List<Integer> answer, int step) {
+        // 1. withdraw 함수 탈출 조건 : 화폐 단위의 개수 만큼 반복
+        // 2. 입력된 money를 n번째(step)화폐 단위로 나누고 값은 answer에 저장,
+        // 나머지 값은 다음 화폐 단위와 함께 withdraw에 입력
     }
 }
