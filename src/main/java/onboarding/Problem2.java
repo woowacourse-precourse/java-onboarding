9843c5d82bd8187e2746b28cf9930d9618eee20d
lvalentine6 <ysr1227@naver.com>
2022-10-29 17:10:47 +0900

    feat : 문제 2_중복된 문자를 제거하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,14 +1,17 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         while (checkString(cryptogram)) {
-
+            cryptogram = removeOverlap(cryptogram);
         }
+        answer = cryptogram;
         return answer;
     }
-    // 문자열엥서 중복된 문자가 있는지 확인하는 메서드
+    // 문자열에서 중복된 문자가 있는지 확인하는 메서드
     static boolean checkString(String cryptogram) {
         boolean flag = false;
         String[] stringArr = cryptogram.split("");
@@ -21,5 +24,26 @@ public class Problem2 {
             }
         }
         return flag;
+    }
+
+    // 중복되는 문자를 제거화는 메서드
+    static String removeOverlap(String cryptogram) {
+        String[] stringArr = cryptogram.split("");
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < stringArr.length; i++) {
+            if(!stack.isEmpty() && stack.peek().equals(stringArr[i])) {
+                stack.pop();
+                continue;
+            }
+            stack.push(stringArr[i]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int stackSize = stack.size();
+        for (int i = 0; i < stackSize; i++) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
     }
 }
