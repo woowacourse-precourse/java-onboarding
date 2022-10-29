bb0a3366f4ed7a95d46f71cded7abde12b0240df
hhiyeon <hhyyeon0214@gmail.com>
2022-10-29 17:43:00 +0900

    feat: check clap condition

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem3 {
+    private static final int CLAP_CONDITION1 = 3;
+    private static final int CLAP_CONDITION2 = 6;
+    private static final int CLAP_CONDITION3 = 9;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
 
-    private static List<Integer> splitDigitNumber(int pageNumber) {
+    private static List<Integer> splitDigitNumber(int number) {
         List<Integer> splitNumberList = new ArrayList<>();
-        while (pageNumber > 0) {
-            splitNumberList.add(pageNumber % 10);
-            pageNumber /= 10;
+        while (number > 0) {
+            splitNumberList.add(number % 10);
+            number /= 10;
         }
         return splitNumberList;
+    }
+
+    private static boolean isClapNumber(int number) {
+        return number == CLAP_CONDITION1 || number == CLAP_CONDITION2 || number == CLAP_CONDITION3;
     }
 }
