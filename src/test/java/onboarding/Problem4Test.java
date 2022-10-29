9d3f66af42b3c597b38ccae2d0860787f7e3da40
saehim <saeilee.vxv@gmail.com>
2022-10-30 00:14:55 +0900

    Test(Problem4): 문자 반환 기능

--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -1,6 +1,15 @@
 import static org.junit.jupiter.api.Assertions.*;
 class Problem4Test {
   
+    // 대소문자 판별
+
+    @Test
+    void 문자반환() {
+        char a = 'a';
+        char result = 'z';
+        assertThat(lowercase(a)).isEqualTo(result);
+        assertThat(uppercase('A')).isEqualTo('Z');
+    }
     // 대문자 변환
     char uppercase(char a){
         char answer = (char)(155 - a);
