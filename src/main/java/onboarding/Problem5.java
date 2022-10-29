4fb4477f632a20742390fa4750384f71c8c07f48
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-29 22:42:18 +0900

    Problem5(수정): 정적 메소드에서의 호출을 위해 정적 변수와 메소드로 변환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,14 +11,14 @@ public class Problem5 {
     }
 
     //돈을 환급할 화폐 리스트
-    private final int[] currencyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static final int[] currencyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     /**
      * 주어진 금액을 큰 단위의 화폐부터 담아 출금
      * @param money 출금을 하고자 하는 액수
      * @return 단위가 큰 화폐 순서대로 개수가 담긴 리스트
      */
-    private List<Integer> getWithDrawList(int money){
+    private static List<Integer> getWithDrawList(int money){
         // 결과로 반환할 리스트를 초기화
         List<Integer> result = new ArrayList<>();
         // 화폐로 바꾸고 남은 돈을 저장할 변수를 초기화
