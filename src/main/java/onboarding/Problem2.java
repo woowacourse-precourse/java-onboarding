13bd9a1ecb152fe797634552de29f3ace4a36476
임형섭 <hs.yim@illb.co.kr>
2022-10-29 13:33:37 +0900

    [Problem2] - 중복 문자열 제거
    문자열을 리스트화
    
    슬라이딩 윈도우 전략을 이용하여
    앞 뒤 문자 비교
    
    같을 경우 : 리스트에서 값 제거 후
    스코프 조정
    
    다를 경우 : 스코프 1씩 증가
    
    반환 리스트를 문자열로 변환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,59 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer = "";
+
+        // 문자열을 리스트화
+        ArrayList<Character> list = new ArrayList<>();
+        cryptogram.chars().forEach((val)-> {
+            list.add((char) val);
+        });
+
+        int left = 0;
+        int right = 1;
+
+        // 슬라이딩 윈도우 전략으로 앞,뒤가 같은 문자 제거
+        while (true) {
+            // 리스트 마지막 값보다 오른쪽 스코프가 커지면 종료
+            if(right > list.size()-1 ){
+                break;
+            }
+            // 비교할 문자 가져오기
+            Character front = list.get(left);
+            Character back = list.get(right);
+
+            // 앞 뒤 문자가 같을 때 리스트에서 값을 제거하고 스코프 조정
+            if (front == back) {
+                if(right == cryptogram.length()){
+                    list.remove(left);
+                    list.remove(--right);
+                }else {
+                    list.remove(left--);
+                    list.remove(--right);
+                }
+            }else {
+                // 앞 뒤 문자가 다르다면 다음 위치로 이동
+                left++;
+                right++;
+            }
+        }
+
+        // 구한 리스트를 문자열로 변환 ( 스트림 연산을 이용한 방법 실패 )
+        for (char l : list) {
+            answer += l;
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String cryptogram = "browoanoommnaon";
+        solution(cryptogram);
     }
 }
