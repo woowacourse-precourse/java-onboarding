7e75fd8cfcc53be3cd6ff1dcfa53c033ed82b40a
KIMSEI1124 <workju1124@gmail.com>
2022-10-29 14:08:30 +0900

    feat: Problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,50 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String[] characters = cryptogram.split("");
+        Stack<String> characterStack = new Stack<>();
+        for (String character : characters) {
+            if (characterStack.isEmpty()) {
+                characterStack.add(character);
+                continue;
+            }
+            if (characterStack.peek().equals(character)) {
+                removeContinueDuplicateCharacters(characterStack, character);
+            } else {
+                characterStack.add(character);
+            }
+        }
+        return stackChangeString(characterStack);
+    }
+
+    /**
+     * @param stack character 가 담긴 stack
+     * @param str   character
+     * @return 연속된 중복 문자가 제거된 stack
+     */
+    private static Stack<String> removeContinueDuplicateCharacters(Stack<String> stack, String str) {
+        if (stack.isEmpty()) {
+            return stack;
+        } else if (stack.peek().equals(str)) {
+            stack.pop();
+            return removeContinueDuplicateCharacters(stack, str);
+        }
+        return stack;
+    }
+
+    /**
+     * stack 을 String 으로 변환
+     *
+     * @param stack
+     * @return String
+     */
+    private static String stackChangeString(Stack<String> stack) {
+        if (stack.isEmpty()) {
+            return "";
+        }
+        return String.join("", stack);
     }
 }
