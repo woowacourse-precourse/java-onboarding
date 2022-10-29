e25ab83d80f4987ff4e4e1a410da0811b8b91ea5
sangho-jeon <jeonsangho1587@gmail.com>
2022-10-29 15:43:24 +0900

    feat      : problem2 delete duplicated character using stack

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,27 +2,44 @@ package onboarding;
 
 import java.util.Stack;
 
+/*
+기능 구현 사항
+1. stack 이용한 중복 문자 제거
+2.
+*/
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        System.out.println(deleteDuplicatedChar(answer.toCharArray()));
+        System.out.println(deleteDuplicatedChar(cryptogram.toCharArray()));
         return answer;
     }
 
     public static String deleteDuplicatedChar(char[] cryptogramCharArray) {
         Stack<Character> characterStack = new Stack<>();
-        boolean flag = false;
+        int index = 0;
+        while(index < cryptogramCharArray.length){
 
-        for (char nextChar :
-                cryptogramCharArray) {
-            if (!characterStack.isEmpty()) {
-                if (characterStack.peek() != nextChar) {
-                    characterStack.push(nextChar);
-                    continue;
+            if(characterStack.isEmpty()){
+                characterStack.push(cryptogramCharArray[index]);
+                index++;
+            }
+            else{
+                char before = characterStack.peek();
+                if(cryptogramCharArray[index] != before){
+                    characterStack.push(cryptogramCharArray[index]);
+                    index++;
+                }
+                else{
+                    while(index < cryptogramCharArray.length && cryptogramCharArray[index] == before){
+                        index++;
+                    }
+                    characterStack.pop();
                 }
             }
-            characterStack.push(nextChar);
+            System.out.println(characterStack.toString());
         }
+
 
         return characterStack.toString();
 
