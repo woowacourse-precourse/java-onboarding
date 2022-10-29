d2dc4db25eb68626a53ffe9613f591d8821edfde
WonSeok-dd <cluejws@gmail.com>
2022-10-29 23:25:13 +0900

    Problem2 | feat: 기능 정의

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 기능 구현 사항
+ * 1. 현재 문자열에서 Stack통해 중복문자 제거
+ * 2. Stack -> 문자열 바꾸기
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
