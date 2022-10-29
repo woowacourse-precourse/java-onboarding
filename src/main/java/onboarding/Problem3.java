75c36b944d87bb5d4e445a10132798e6819704f5
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 05:44:13 +0900

    feat : Problem3 박수 횟수 구하기 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
 	public static int solution(int number) {
 		int answer = 0;
@@ -10,11 +12,29 @@ public class Problem3 {
 			illegalStateException.printStackTrace();
 		}
 
+		answer = countClaps(number);
+
 		return answer;
 	}
 
 	public static void isValidNumber(int number) throws IllegalStateException {
 		if (number < 1 || number > 10000)
 			throw new IllegalStateException("number가 범위 밖입니다.");
+	}
+
+	public static int countClaps(int number) {
+		return IntStream.range(1, number + 1).map(Problem3::getClapCounts).sum();
+	}
+
+	public static int getClapCounts(int number) {
+		int clap = 0;
+		int now = number;
+		while (now > 0) {
+			if (now % 10 != 0 && (now % 10) % 3 == 0) {
+				clap++;
+			}
+			now /= 10;
+		}
+		return clap;
 	}
 }
