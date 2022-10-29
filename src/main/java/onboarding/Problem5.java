ef0f1283ac0dec033c221df13ac2c2c7dbcbc57b
abbd2 <ajkety00@gmail.com>
2022-10-30 03:30:20 +0900

    answer에 화페의 수를 저장하며 money의 값을 갱신, 최종 값 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,7 +10,8 @@ public class Problem5 {
         int[] currency = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
 
         for(int currencyTemp : currency){
-
+            answer.add(money / currencyTemp);
+            money %= currencyTemp;
         } // end for
 
         return answer;
