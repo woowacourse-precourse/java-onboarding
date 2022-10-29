6f83238d6bcefb6fa9a68c0962f5e468d149db38
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-29 19:50:23 +0900

    221029 Feat: P2 - 기능3 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,26 +2,35 @@ package onboarding;
 
 public class Problem2 {
 
-    public static StringBuilder sb = new StringBuilder();
+    public static StringBuilder sb;
     public static String solution(String cryptogram) {
         String answer = "answer";
 
-        deleteRepetition(cryptogram);
+        boolean hasRepetition = true;
+        while(hasRepetition && !cryptogram.isBlank()) {
+            hasRepetition = deleteRepetition(cryptogram, cryptogram.length());
+            cryptogram = sb.toString();
+            //2번 예제 안됨
+        }
+
+        System.out.println(sb.toString());
         //
         return answer;
     }
 
-    public static String deleteRepetition(String cryptogram) {
+    public static boolean deleteRepetition(String cryptogram, int length) {
         //1.입력 문자열을 순회하며 중복 구간을 찾는다.
-        int length = cryptogram.length();
-
+        sb = new StringBuilder();
         char prev = cryptogram.charAt(0);
         int sequenceCount = 0;
         boolean isSequence = false;
+        boolean hasSequence = false;
+
         for(int i = 1; i < length; i++) {
             if(prev == cryptogram.charAt(i)) {
                 sequenceCount++;
                 isSequence = true;
+                hasSequence = true;
             }
 
             sb.append(prev);
@@ -36,7 +45,14 @@ public class Problem2 {
             prev = cryptogram.charAt(i);
         }
         sb.append(cryptogram.charAt(length - 1));
+        if(prev == cryptogram.charAt(length - 1) && isSequence) {
+            int currentLength = sb.length();
+            sb.delete((currentLength - sequenceCount - 1), currentLength);
+            isSequence = false;
+            sequenceCount = 0;
+        }
 
-        return sb.toString();
+        return hasSequence;
     }
+
 }
