2a1168202cf67ced10f83a29b44ba9eb2239164e
slbin-park <smpts00@naver.com>
2022-10-29 20:41:05 +0900

    feat : 잔돈의 개수를 구하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,7 +2,6 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
@@ -10,6 +9,10 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<Integer>();
         List<Integer> changes = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
+        for(int i=0;i<CHANGES_SIZE;i++){
+            answer.add(i,getCoin(money,changes.get(i)));
+            money = moneyRemainder(money,changes.get(i));
+        }
         return answer;
     }
     public static int getCoin(int money,int coin){
