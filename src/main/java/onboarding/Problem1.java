9f244134e7acf123cba3292a95fd36deaf3977f1
can019 <jys01012@gmail.com>
2022-10-29 19:46:13 +0900

    :sparkles: Exception checker 및 test case 1, 2, 3 통과

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,7 +10,15 @@ class Problem1 {
         if(exceptionChecker(pobi) || exceptionChecker(crong))
             answer = -1;
         else{
-            // @TODO :: 계산
+            int pobiScore = calculatePersonalScore(pobi);
+            int cronScore = calculatePersonalScore(crong);
+
+            if(pobiScore > cronScore)
+                answer = 1;
+            else if(pobiScore < cronScore)
+                answer = 2;
+            else
+                answer = 0;
         }
         return answer;
     }
@@ -19,11 +27,42 @@ class Problem1 {
         // @TODO :: 예외상황 캐치
 
         // 1. 1부터 400 사이 값이 아닌 경우
+        if(!isBetween(1,400,page.get(0)) || !isBetween(1,400,page.get(0)))
+            return true;
         // 2. 왼쪽은 홀수 오른쪽은 짝수가 아닌 경우
+        if(!isOdd(page.get(0)) || !isEven(page.get(1)))
+            return true;
         // 3. 오른쪽 - 왼쪽이 1이 아닌 경우
-        return true;
-    }
+        if(!isBookPage(page))
+            return true;
 
+        return false;
+    }
+    // 홀수
+    private static boolean isOdd(int number){
+        if(number%2 == 1)
+            return true;
+        else
+            return false;
+    }
+    private static boolean isEven(int number){
+        if(number%2 == 0)
+            return true;
+        else
+            return false;
+    }
+    private static boolean isBookPage(List<Integer> page){
+        if(page.get(1)-page.get(0) == 1)
+            return true;
+        else
+            return false;
+    }
+    private static boolean isBetween(int min, int max, int target){
+        if(min<target && target<max)
+            return true;
+        else
+            return false;
+    }
     private static int calculatePersonalScore(List<Integer> page){
         int left = calculateSingePageScore(page.get(0));
         int right = calculateSingePageScore(page.get(1));
