983d8b52488a6da74d71fc0396c575bf37a77517
Eunkeee <eunkeeeee@gmail.com>
2022-10-29 15:10:21 +0900

    🚑[P1] fix: 기능 6 검증 오류 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 
 class Problem1 {
     public static <e> int solution(List<Integer> pobi, List<Integer> crong) {
+
         int answer = 0;
 
         try{
@@ -12,7 +13,14 @@ class Problem1 {
             int[] pobiArr = pobi.stream().mapToInt(i->i).toArray();
             int[] crongArr = crong.stream().mapToInt(i->i).toArray();
 
-            isValid(pobiArr, crongArr);
+            Exception e = new Exception("[왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있지 않음.");
+            Boolean check1 = pobiArr[0] % 2 == 1 && pobiArr[1] % 2 == 0 && pobiArr[1] == pobiArr[0] + 1;
+            Boolean check2 = crongArr[0] % 2 == 1 && crongArr[1] % 2 == 0 && crongArr[1] == crongArr[0] + 1;
+            Boolean check3 = pobiArr[0] != 1 && crongArr[0] != 1 && pobiArr[1] != 400 && crongArr[1] !=400;
+
+            if(!check1 || !check2 || !check3){
+                throw e;
+            }
 
             int pobiScore = getMax(pobiArr);
             int crongScore = getMax(crongArr);
@@ -31,17 +39,6 @@ class Problem1 {
             answer = -1;
         }
         return answer;
-    }
-
-    private static void isValid(int[] pobiArr, int[] crongArr) throws Exception {
-        Exception e = new Exception("[왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있지 않음.");
-        Boolean check1 = pobiArr[0] % 2 == 1 && pobiArr[1] % 2 == 0 && pobiArr[1] == pobiArr[0] + 1;
-        Boolean check2 = crongArr[0] % 2 == 1 && crongArr[1] % 2 == 0 && crongArr[1] == crongArr[0] + 1;
-        Boolean check3 = pobiArr[0] == 1 || crongArr[0] == 1 || pobiArr[1] == 400 || crongArr[1] ==400;
-
-        if(!check1 || !check2 || !check3){
-            throw e;
-        }
     }
 
     private static int getMax(int[] arr) {
