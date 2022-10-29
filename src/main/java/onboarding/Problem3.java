3d186607f64649796470e2e8db8f957ec9ef2b0a
seonghoo1217 <seonghoo1217@naver.com>
2022-10-29 14:16:11 +0900

    refactor : Add a Number Length Check Exception on [Task3]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,13 +3,20 @@ package onboarding;
 public class Problem3 {
     static int count=0;
     public static int solution(int number) {
-        if (number<3) return 0;
 
-        for (int i=3;i<=number;i++){
-            getCount(i);
+        try {
+            checkNumberLength(number);
+            if (number < 3) return 0;
+
+            else {
+                for (int i = 3; i <= number; i++) {
+                    getCount(i);
+                }
+                return count;
+            }
+        }catch (Exception e){
+            return 0;
         }
-
-        return count;
     }
 
     public static void getCount(int num){
@@ -17,4 +24,9 @@ public class Problem3 {
         int num_length = s.length();
         count+= num_length - s.replaceAll("[369]","").length();
     }
+
+    public static void checkNumberLength(int number) throws Exception{
+        if (number<1||number>10000) throw new Exception("수의 범위가 제한을 벗어났습니다");
+    }
+
 }
