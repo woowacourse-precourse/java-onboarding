3d368f126eed14b9e29afe5c0cb482c48e0253a4
Luka <meena2003@naver.com>
2022-10-29 16:25:45 +0900

    main 메서드 삭제, 주석 삭제

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,21 +1,13 @@
 package onboarding;
 
 public class Problem3 {
-    public static void main(String[] args) {
-        System.out.println(solution(36));
-    }
 
     public static int solution(int number) {
-        // 1. 3 6 9 숫자가 몇개 존재하는지 담을 변수 생성
-        // 2. 숫자를 하나씩 세면서 해당 숫자내에 3 6 9 가 몇개 있는지 확인하는 메서드 생성
-        // 3. 정답 반환
-
         if (number <= 2) {
             return 0;
         }
 
         int howManyNums = countHowManyNums(number);;
-
         return howManyNums;
     }
 
