1d3cf3754d6ee4dfa839d48138040b6908be60c1
HoeSeong <ghltjd369@gmail.com>
2022-10-29 20:11:23 +0900

    feat : (problem 4) 알파벳 대문자, 소문자를 역순서대로 나열한 배열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,13 +5,16 @@ public class Problem4 {
         String answer = "";
 
         String alphabet = "";                   //후에 indexOf를 사용하기 위해 String으로 선언
+        char[] frog = new char[52];
 
         for(int i = 0; i < 26; i++){
             alphabet += (char) (65 + i);       //ASCII code를 char로 형변환하여 저장 (대문자)
+            frog[i] = (char) (90 - i);         //알파벳 순서 반대로 저장
         }
 
         for(int i = 0; i < 26; i++){
             alphabet += (char) (97 + i);       //소문자
+            frog[i + 26] = (char) (122 - i);
         }
 
         return answer;
