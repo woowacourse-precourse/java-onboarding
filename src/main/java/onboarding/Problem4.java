fa2df6f1486ced2efc8b7d5c9070c6d0fa81b5b4
HaODay <ycis5013@naver.com>
2022-10-29 16:43:21 +0900

    [Problem4] 대문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,12 @@ public class Problem4 {
         String answer = "";
         char[] arr= word.toCharArray();
 
+        for(int i=0;i<arr.length;i++) {
+            //대문자
+            if (65 <= arr[i] && arr[i] <= 90) {
+                answer+=(char)(155-arr[i]);
+            }
+        }
         return answer;
     }
 }
