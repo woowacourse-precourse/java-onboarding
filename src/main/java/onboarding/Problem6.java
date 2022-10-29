0ae8e7abc9d01f04feed211715d070f6f8941add
slbin-park <smpts00@naver.com>
2022-10-30 00:40:03 +0900

    feat : 닉네임을 두 글자로 잘라 배열로 변환 해주는 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    public static List<String> getWord(String word,int index){
+        List<String> words = new ArrayList<>();
+        for(int i=0;i<word.length()-1;i++){
+            words.add(word.substring(i,i+2));
+        }
+        return words;
     }
 }
