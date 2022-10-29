682ea2f17fb58972ba9d3a88d564542ebe24e75b
ghltjd369 <ghltjd369@gmail.com>
2022-10-29 16:00:46 +0900

    refactor : if문 자바 컨벤션을 따라 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,10 +12,18 @@ class Problem1 {
         int crong_left = crong.get(0);     //crong 왼쪽 페이지
         int crong_right = crong.get(1);    //crong 오른쪽 페이지
 
-        if(pobi_left % 2 == 0 || pobi_right != pobi_left+1) return -1;     //짝수로 시작하거나 연속되지 않은 페이지
-        if(pobi_left == 1 || pobi_right == 400) return -1;                   //첫장 or 마지막장
-        if(crong_left % 2 == 0 || crong_right != crong_left+1) return -1;
-        if(crong_left == 1 || crong_right == 400) return -1;
+        if(pobi_left % 2 == 0 || pobi_right != pobi_left+1) {       //짝수로 시작하거나 연속되지 않은 페이지
+            return -1;
+        }
+        if(pobi_left == 1 || pobi_right == 400) {                   //첫장 or 마지막장
+            return -1;
+        }
+        if(crong_left % 2 == 0 || crong_right != crong_left+1) {
+            return -1;
+        }
+        if(crong_left == 1 || crong_right == 400) {
+            return -1;
+        }
 
         String pobi_left_str = Integer.toString(pobi_left);
         String pobi_right_str = Integer.toString(pobi_right);
