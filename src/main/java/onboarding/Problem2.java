b753119df601b03a55b30ceab93586603d984dd4
AhnJoonSung <tjdtna01@naver.com>
2022-10-29 20:06:37 +0900

    Problem2 decryption

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
+    }
+
+    public static String decryption(String cryptogram) {
+        boolean[] isDuplicated = new boolean[cryptogram.length()];
+        String result = "";
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i-1) == cryptogram.charAt(i)) {
+                isDuplicated[i-1] = true;
+                isDuplicated[i] = true;
+            }
+        }
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!isDuplicated[i]) {
+                result += cryptogram.charAt(i);
+            }
+        }
+        return result;
     }
 }
