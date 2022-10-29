ea1149b665baf424a72d8e7b969cfa823be56f5b
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 18:33:25 +0900

    Fix : fix index exception error to Problem2
    인덱스 에러와 연속된 문자를 지우지 못하는 문제를 해결하였습니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,9 +2,9 @@ package onboarding;
 
 public class Problem2 {
 
-    public static boolean validCheck(String cryptogram) {
-        char[] charArray = cryptogram.toCharArray();
-        if (cryptogram.length() > 1000 || cryptogram.length() < 1) {
+    public static boolean validCheck(String str) {
+        char[] charArray = str.toCharArray();
+        if (str.length() > 1000 || str.length() < 1) {
             return false;
         }
         for (char c : charArray) {
@@ -15,38 +15,49 @@ public class Problem2 {
         return true;
     }
 
-    public static String deduplication(String cryptogram){
-        boolean deleteFlag = true;
-        StringBuilder word = new StringBuilder();
-        char[] charArray = cryptogram.toCharArray();
-        for(int i=1; i<charArray.length; i++){
-            word.append(charArray[i]);
-            if(charArray[i] == charArray[i-1]){
-                word.delete(i-1, i);
-                deleteFlag = false;
+    public static String deduplicate(String str) {
+        StringBuilder builder = new StringBuilder(str);
+        int start = 0;
+        int end = 0;
+        while (true) {
+            start = findDuplicateStartIndex(builder);
+            if (start == -1) {
+                return builder.toString();
             }
+            end = findDuplicateEndIndex(builder, start);
+            builder.delete(start, end + 1);
         }
-        if(deleteFlag){
-            return word.toString();
-        }
-        return "0";
     }
 
-    public static String decode(String cryptogram){
-        String result = deduplication(cryptogram);
-        String temp = result;
-
-        while (!temp.equals("0")){
-            temp = deduplication(result);
-            if(!temp.equals("0")){
-                result = deduplication(result);
+    public static int findDuplicateStartIndex(StringBuilder strBuilder) {
+        int start;
+        for (int i = 1; i < strBuilder.length(); i++) {
+            if (strBuilder.charAt(i) == strBuilder.charAt(i - 1)) {
+                start = i - 1;
+                return start;
             }
         }
-        return result;
+        return -1;
+    }
+
+    public static int findDuplicateEndIndex(StringBuilder strBuilder, int start) {
+        int end = start;
+        for (int i = start + 1; i < strBuilder.length(); i++) {
+            if (strBuilder.charAt(i) == strBuilder.charAt(start)) {
+                end++;
+            } else {
+                break;
+            }
+        }
+        return end;
     }
 
     public static String solution(String cryptogram) {
+        if (!validCheck(cryptogram)) {
+            throw new IllegalStateException("잘못된 입력입니다.");
+        }
         String answer = "answer";
+        answer = deduplicate(cryptogram);
         return answer;
     }
 }
