a1785824e97365fdb99070763bb6e777bfa8eb80
nn98 <jkllhgb@gmail.com>
2022-10-29 18:39:05 +0900

    #4 Feat : Feature implementation

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuffer f=new StringBuffer();
+        for(char c:word.toCharArray()){
+            int d;
+            if(c<33)d=c;
+            else if(c<91)d=90-(c-65);
+            else d=122-(c-97);
+            f.append((char)d);
+        }
+        return f.toString();
     }
 }
