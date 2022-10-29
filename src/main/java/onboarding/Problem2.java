cdd40cd282b2a85340b9438f733fc99fee760389
Noah <shgustmd6865@naver.com>
2022-10-29 20:53:23 +0900

    Feat : 덱을 이용하여 중복문자 탐색 추가
    덱을 이용하여 중복 문자를 탐색합니다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (char ele : cryptogram.toCharArray()) {
+
+        }
+
         return answer;
     }
 }
