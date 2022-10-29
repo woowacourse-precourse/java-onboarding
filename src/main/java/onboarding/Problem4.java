8bdd39cb8833f1ba8db86c5da9340391b1812da3
beombu <sunil1369@bible.ac.kr>
2022-10-30 00:42:16 +0900

    Refactor(P4) : delete 대소문자 변환 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,17 +15,6 @@ public class Problem4 {
     }
 
     /**
-     * 알파벳의 대문자는 소문자로 소문자는 대문자로 바꾸는 기능
-     */
-    public static Character changeAlphabetCase(char c){
-        if(Character.isLowerCase(c)){
-            return Character.toUpperCase(c);
-        }else{
-            return Character.toLowerCase(c);
-        }
-    }
-
-    /**
      * 알파벳을 거꾸로 반환
      */
     public static Character reverseAlphabet(char c){
