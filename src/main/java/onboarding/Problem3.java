2112c60b1ab4351e9b6a56568055975d187514a7
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-29 10:12:33 +0900

    1부터 number까지의 자연수를 나열하는 리스트를 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
+        ArrayList<Integer> set = new ArrayList<>();
+
+        for(int i=1; i <= number; i++){
+            set.add(i);
+        }
+        
         int answer = 0;
         return answer;
     }
