72b55695370d41a6795fd41a04ea5618ec7de56c
beombu <sunil1369@bible.ac.kr>
2022-10-29 23:49:16 +0900

    Feat(P4) : 기능목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,18 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /**
+     * 알파벳의 대문자는 소문자로 소문자는 대문자로 바꾸는 기능
+     */
+    public static Character changeAlphabetCase(char c){
+        return null;
+    }
+
+    /**
+     * 알바벳을 거꾸로 반환
+     */
+    public static Character reverseAlphabet(char c){
+        return null;
+    }
 }
