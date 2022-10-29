e13bd33bc0704789092f586d9801c8cd5e6cc14b
김현재 <41482946+galug@users.noreply.github.com>
2022-10-29 12:07:10 +0900

    feat(problem3): Add 제한 사항에 맞는 인자가 들어왔는지 검증하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        if (!validateGameNumber(number)) {
+            return -1;
+        }
         int answer = 0;
         return answer;
+    }
+
+    public static boolean validateGameNumber(int number) {
+        if (number < 1 || number > 10000) {
+            return false;
+        }
+        return true;
     }
 }
