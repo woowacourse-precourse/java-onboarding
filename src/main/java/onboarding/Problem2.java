00c4290d99564682e2966c4a60919dac91af2191
zcae1020 <zcae1020@naver.com>
2022-10-29 21:15:45 +0900

    fix(Problem2): nextString method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,11 +6,12 @@ public class Problem2 {
         return answer;
     }
 
-    //연속된 char 하나 삭제 후 return | 연속된 char가 없다면 그대로 return
+    //모든 연속된 char 삭제 후 return
     private static String nextString(String str){
         for(int i=0;i<str.length()-1;i++){
-            if(str.charAt(i)==str.charAt(i+1))
-                return removeChar(str, i);
+            if(str.charAt(i)==str.charAt(i+1)){
+                str = removeChar(str, i--);
+            }
         }
         return str;
     }
