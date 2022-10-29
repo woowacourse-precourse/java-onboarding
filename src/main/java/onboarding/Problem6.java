02b4749a245ba5fed0f5e6018ae7b212ef0bbf54
0112leesy <0112leesy@naver.com>
2022-10-29 23:45:15 +0900

    [Problem6] feat: list_to_map

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,26 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    static Map<String, List<String>> list_to_map(List<List<String>> forms){
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(int i=0; i<forms.size(); i++) {
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
+
+            if(map.containsKey(nickname)) {
+                map.get(nickname).add(email);
+            }
+            else {
+                List<String> email_list = new ArrayList<>();
+                email_list.add(email);
+                map.put(nickname, email_list);
+            }
+        }
+
+        return map;
+    }
+
 }
