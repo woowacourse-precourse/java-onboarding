637adb15376c5c92cbac289a519f7ba86a802c8e
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 13:39:22 +0900

    Feat : add duplication check and delete method to Problem2
    중복된 문자를 확인하고 삭제하는 deduplication : String method를 작성하였습니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,6 +15,23 @@ public class Problem2 {
         return true;
     }
 
+    public static String deduplication(String cryptogram){
+        boolean deleteFlag = true;
+        StringBuilder word = new StringBuilder();
+        char[] charArray = cryptogram.toCharArray();
+        for(int i=1; i<charArray.length; i++){
+            word.append(charArray[i]);
+            if(charArray[i] == charArray[i-1]){
+                word.delete(i-1, i);
+                deleteFlag = false;
+            }
+        }
+        if(deleteFlag){
+            return word.toString();
+        }
+        return "0";
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
