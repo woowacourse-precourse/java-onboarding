7ee72b39d4c795e669edfa0cc2f61aa66b931fbb
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 18:57:33 +0900

    Feat : add validation check function to Problem3
    Problem3의 input 유효성 체크를 위한 method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,15 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static boolean validCheck(int number) {
+        return number >= 1 && number <= 10000;
+    }
+
     public static int solution(int number) {
+        if (!validCheck(number)) {
+            throw new IllegalStateException("잘못된 입력입니다.");
+        }
         int answer = 0;
         return answer;
     }
