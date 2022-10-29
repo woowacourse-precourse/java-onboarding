4fe0eee95fd6a0003cf1bf6dee3a0d7c4c6a24b1
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-29 20:53:06 +0900

    Problem4(설계): 기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,22 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /**
+     * 단어를 청개구리 사전으로 변환
+     * @param word : 변환할 단어
+     * @return 변환된 단어
+     */
+    private static String getChangedWord(String word){
+
+    }
+
+    /**
+     * 글자를 청개구리 사전으로 변환
+     * @param letter : 변환할 글자
+     * @return 변환된 글자
+     */
+    private static char getChangedChar(char letter){
+
+    }
 }
