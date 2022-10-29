e6cf78cdec05988f474281fe4ff15eea1415d2f7
Luka <meena2003@naver.com>
2022-10-29 21:04:46 +0900

    테스트용입니다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    public static void main(String[] args) {
+        System.out.println("테스트입니다.");
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
