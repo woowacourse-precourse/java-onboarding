1e2931c728fc21b2da2ae8166150da90c44b493f
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 18:59:00 +0900

    Feat : add counting Clap method to Problem3
    입력받은 값까지 369게임 규칙에 맞춰 박수횟수를 카운팅 하는 method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,11 +6,28 @@ public class Problem3 {
         return number >= 1 && number <= 10000;
     }
 
+    public static int countClap(int number) {
+        int cnt = 0;
+        int lastNumber;
+        for(int i = 0; i<=number; i++){
+            int temp = i;
+            while (temp != 0) {
+                lastNumber = temp % 10;
+                if (lastNumber == 3 || lastNumber == 6 || lastNumber == 9) {
+                    cnt++;
+                }
+                temp /= 10;
+            }
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
         if (!validCheck(number)) {
             throw new IllegalStateException("잘못된 입력입니다.");
         }
         int answer = 0;
+        answer = countClap(number);
         return answer;
     }
 }
