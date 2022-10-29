7e7bc450482cb18497776750a36807e52276a9a4
bini59 <myuni0234@gmail.com>
2022-10-29 13:13:42 +0900

    feat : Check Duplicate, Remove character
    if the characters are consecutive
    remove them

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,32 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer;
+
+        // cheek for end
+        int check = Integer.MAX_VALUE;
+        // check for do remove
+        boolean check_rmv;
+
+        // check == 0 -> not have duplicate character
+        while(check > 0){
+            check = 0;
+            for(int i = 0; i < cryptogram.length() - 1; i++) {
+                // check for do remove cryptogram[i]
+                check_rmv = false;
+                for (int j = i+1; j < cryptogram.length(); j++) {
+                    // if cryptogram[i] == cryptogram[j] remove j
+                    if (cryptogram.charAt(i) == cryptogram.charAt(j)) {
+                        cryptogram = cryptogram.substring(0, j) + cryptogram.substring(j + 1);
+                        check += 1;
+                        check_rmv = true;
+                    } else break;
+
+                }
+                if(check_rmv) cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + 1);
+            }
+        }
+        answer = cryptogram;
         return answer;
     }
 }
