e063cdcb073d3b59139685f7da07b5104fc4a5c5
HaODay <ycis5013@naver.com>
2022-10-29 16:21:34 +0900

    [Problem2] 입려 받은 문자열 쪼개서 list에 저장

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.LinkedList;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        answer = "";
+        final int LENGTH=cryptogram.length();
+        LinkedList<String> list=new LinkedList<String>();
+
+        String[] array=cryptogram.split("");
+
+        for(int i=0;i<LENGTH;i++) list.add(array[i]);
+
+
         return answer;
     }
 }
