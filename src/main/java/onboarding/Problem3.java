bdeb70dbd2d687ab6eb290a433dbd9537730081f
charlesuu <tiger0192004@gmail.com>
2022-10-29 20:21:42 +0900

    <문제3> - (기능 1).  하나의 숫자에 포함된 3, 6, 9의 개수 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+
+
         return answer;
+    }
+
+    public static int count369(int number) {
+        int count = 0;
+
+        while (number != 0) {
+            int tmp = number % 10;
+
+            if (tmp == 3 || tmp == 6 || tmp == 9) {
+                count++;
+            }
+
+            number /= 10;
+        }
+
+        return  count;
     }
 }
