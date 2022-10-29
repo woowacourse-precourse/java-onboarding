86a8829ad6b292c384f3675cc06bdbd1a9b1fe16
Luka <meena2003@naver.com>
2022-10-29 18:21:18 +0900

    main() 메서드 삭제, 주석 삭제

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,21 +1,13 @@
 package onboarding;
 
 public class Problem4 {
-    public static void main(String[] args) {
-        System.out.println(solution("I love you"));
-    }
-
     public static String solution(String word) {
-        // 1. 비교해서 변화하려는 코드표를 char 배열로 생성
-        // 2-1. word를 매개변수로 받아서 char.At()을 이용해 char 배열의 원소들과 비교하는 메서드 생성
-        // 2-2. 소문자와 대문자를 구분하기
-        // 3. 반환값 출력
-
         String sonWords = "";
 
         for (int i = 0; i < word.length(); i++) {
             char preWord = word.charAt(i);
 
+            // 소문자인 경우 대문자로 변환 후 메서드 실행. 반환값을 다시 대문자로 변환
             if (Character.isLowerCase(preWord)) {
                 preWord -= 32;
                 preWord = (char) (convertWords(preWord) + 32);
@@ -32,6 +24,7 @@ public class Problem4 {
         char[] DictionaryOfWords = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M'
                 , 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
 
+        // 빈칸인 경우 빈칸을 그대로 반환
         if (preWord == ' ') {
             return ' ';
         }
