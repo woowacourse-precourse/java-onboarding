3b313bd70f26f248c28b16089fad6bf65d0fa00e
“rotomoo” <quddlf410@naver.com>
2022-10-29 20:59:35 +0900

    feat : implementation complete of Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,12 @@
 package onboarding;
 
+import onboarding.problem1.domain.Calculator;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Calculator calculator = new Calculator();
+        return calculator.compareScore(pobi, crong);
     }
 }
