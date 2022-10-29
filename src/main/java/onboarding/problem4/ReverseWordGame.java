da658702406664eb144d766128e023bb5cfc39dd
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-10-29 20:36:24 +0900

    feat: 길이가 1000자이상인 문자열을 입력시 예외를 처리.

--- src/main/java/onboarding/problem4/ReverseWordGame.java
+++ src/main/java/onboarding/problem4/ReverseWordGame.java
@@ -1,9 +1,18 @@
 package onboarding.problem4;
 
 public class ReverseWordGame {
+    private static final int MAX = 1_000;
+
     public static int start(String input) {
         validateBlank(input);
+        validateMAX(input);
         return 1;
+    }
+
+    private static void validateMAX(String input) {
+        if (input.length() > MAX){
+            throw new ReverseException("1000 이상의 길이의 문자열은 입력할 수 없습니다");
+        }
     }
 
     private static void validateBlank(String input) {
--- src/test/java/onboarding/problem4/ReverseWordGameTest.java
+++ src/test/java/onboarding/problem4/ReverseWordGameTest.java
@@ -2,11 +2,20 @@ package onboarding.problem4;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReverseWordGameTest {
     @Test
     void if_under_MIN(){
-        assertThrows(ReverseException.class, () -> ReverseWordGame.start(""));
+        assertThrows(ReverseException.class, () -> ReverseWordGame.start(" "));
+    }
+
+    @Test
+    void if_over_MAX(){
+        String test = IntStream.range(0, 1004).mapToObj(String::valueOf).collect(Collectors.joining());
+        assertThrows(ReverseException.class, () -> ReverseWordGame.start(test));
     }
 }
