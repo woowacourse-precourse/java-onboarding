0a5dc34db308797b52b482d280cc28703a409d96
kokomong2 <ko9603@naver.com>
2022-10-29 20:39:48 +0900

    feat(문제1) 추가 : highestScore 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,25 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        highestScore(pobi.get(0));
         return answer;
     }
+
+    public static int highestScore(int book){
+
+        int addscore=0;
+        int multiplyscore=1;
+        int maxscore =0;
+        String strbook = Integer.toString(book);
+        for (int i = 0; i < strbook.length(); i++) {
+            addscore+=book%10;
+            multiplyscore*=book%10;
+            book = book/10;
+        }
+        maxscore = Math.max(addscore, multiplyscore);
+        System.out.println("addscore : " + addscore + " multiplyscore : " + multiplyscore + " maxscore : " + maxscore);
+
+        return maxscore;
+    }
+
 }
