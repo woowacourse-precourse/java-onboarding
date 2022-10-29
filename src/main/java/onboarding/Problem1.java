bfda8b9266592f2b539128f806a1b4eec714b96d
heohyeonjink <hyeonjin4870@naver.com>
2022-10-29 19:57:42 +0900

    feat(PROBLEM1) : 결론 도출하기
    - max_score 함수 : 각 유저 당 가장 큰 점수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,18 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobi_score = max_score(pobi);
+        int crong_score = max_score(crong);
+
+        q
+        if(pobi_score>crong_score) return 1;
+        else if(pobi_score<crong_score) return 2;
+        else if (pobi_score==crong_score) return 0;
 
         return answer;
     }
@@ -25,6 +33,15 @@ class Problem1 {
             multi*=(c-'0');
         }
         return multi;
+    }
+
+    public static int max_score(List<Integer> user){
+        int max = Integer.MIN_VALUE;
+        for(Integer x: user){
+            max = Math.max(max,Sum(x));
+            max = Math.max(max,Multi(x));
+        }
+        return max;
     }
 
 }
