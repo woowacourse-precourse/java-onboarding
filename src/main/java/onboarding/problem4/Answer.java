b9abfbfd183bb42b46834daca1241e92c6f27b2c
kimminsu <xjfcnfw3@naver.com>
2022-10-29 16:19:25 +0900

    feat(문제4): 알파벳 문자의 역순의 문자를 출력 기능 구현

--- src/main/java/onboarding/problem4/Answer.java
+++ src/main/java/onboarding/problem4/Answer.java
@@ -6,11 +6,29 @@ public class Answer {
     private static final int UPPER_A = 65;
     private static final int UPPER_Z = 90;
 
+    private static char getOppositeChar(char message) {
+        char oppositeChar = message;
+        if (isLowerCase(message)) {
+            oppositeChar = getOppositeLowerCase(message);
+        } else if (isUpperCase(message)) {
+            oppositeChar = getOppositeUpperCase(message);
+        }
+        return oppositeChar;
+    }
+
+    private static char getOppositeLowerCase(char message) {
+        return (char) (LOWER_Z - ((int) message - LOWER_A));
+    }
+
+    private static char getOppositeUpperCase(char message) {
+        return (char) (UPPER_Z - ((int) message - UPPER_A));
+    }
+
     private static boolean isLowerCase(char message) {
         return ((int)message >= LOWER_A) && ((int)message <= LOWER_Z);
     }
 
     private static boolean isUpperCase(char message) {
-        return ((int)message >= UPPER_A) && ((int)message <= UPPER_Z);
+        return ((int) message >= UPPER_A) && ((int) message <= UPPER_Z);
     }
 }
