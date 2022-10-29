d44cb2bd870b3b9dcaadad3d692ed7670e5d25a0
eunbii0213 <eunbii0213@naver.com>
2022-10-29 21:32:09 +0900

    refactor: (Problem6) refactoring

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,6 +10,9 @@ public class Problem6 {
     public static final int EMAIL_INDEX = 0;
     public static final int NAME_INDEX = 1;
     public static final String REGEX = "";
+    public static List<String> targetList;
+    public static List<String> compareList;
+    public static String[] targetNicknameArr;
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>(findAnswer(forms));
@@ -21,26 +24,50 @@ public class Problem6 {
         Set<String> answer = new HashSet<>();
 
         for (int targetIndex = START_INDEX; targetIndex < forms.size() - 1; targetIndex++) {
-            String[] targetNicknameArr = forms.get(targetIndex).get(NAME_INDEX).split(REGEX);
-
-            for (int nicknameIndex = targetIndex + 1; nicknameIndex < forms.size(); nicknameIndex++) {
-                String nickname = forms.get(nicknameIndex).get(NAME_INDEX);
-
-                findTwoSameLetter(forms, targetIndex, nicknameIndex, targetNicknameArr, nickname, answer);
-            }
+            chooseTargetNickName(forms, targetIndex);
+            compareWithTarget(forms, answer, targetIndex);
         }
         return answer;
     }
 
-    public static void findTwoSameLetter(List<List<String>> forms, int targetIndex, int nicknameIndex, String[] targetNicknameArr, String nickname, Set<String> answer) {
-        for (int letterIndex = START_INDEX; letterIndex < targetNicknameArr.length - 1; letterIndex++) {
-            String targetTwoLetter = targetNicknameArr[letterIndex] + targetNicknameArr[letterIndex + 1];
+    public static void compareWithTarget(List<List<String>> forms, Set<String> answer, int targetIndex) {
+        for (int compareIndex = targetIndex + 1; compareIndex < forms.size(); compareIndex++) {
+            chooseCompareNickName(forms, compareIndex);
+            findTwoSameLetter(answer);
+        }
+    }
 
-            if (nickname.contains(targetTwoLetter)) {
-                answer.add(forms.get(targetIndex).get(EMAIL_INDEX));
-                answer.add(forms.get(nicknameIndex).get(EMAIL_INDEX));
+    public static void chooseCompareNickName(List<List<String>> forms, int compareIndex) {
+        compareList = forms.get(compareIndex);
+    }
+
+    public static void chooseTargetNickName(List<List<String>> forms, int targetIndex) {
+        targetList = forms.get(targetIndex);
+        targetNicknameArr = targetList.get(NAME_INDEX).split(REGEX);
+    }
+
+    public static void findTwoSameLetter(Set<String> answer) {
+        for (int letterIndex = START_INDEX; letterIndex < targetNicknameArr.length - 1; letterIndex++) {
+            String targetTwoLetter = chooseTargetTwoLetter(letterIndex);
+
+            if (isSameTwoLetter(targetTwoLetter)) {
+                addEmailAddress(answer);
                 break;
             }
         }
     }
+
+    public static void addEmailAddress(Set<String> answer) {
+        answer.add(targetList.get(EMAIL_INDEX));
+        answer.add(compareList.get(EMAIL_INDEX));
+    }
+
+    public static boolean isSameTwoLetter(String targetTwoLetter) {
+        return compareList.get(NAME_INDEX).contains(targetTwoLetter);
+    }
+
+    public static String chooseTargetTwoLetter(int letterIndex) {
+        return targetNicknameArr[letterIndex] + targetNicknameArr[letterIndex + 1];
+    }
+
 }
