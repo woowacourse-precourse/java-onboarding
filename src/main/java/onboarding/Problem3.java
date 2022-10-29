6cc3a7ac0cf076819db60962986d1633d27e9988
hectick <chyeon1223@cau.ac.kr>
2022-10-29 19:11:11 +0900

    refactor : [Problem3] 주어진 숫자의 박수치는 횟수를 계산하는 메소드 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,20 +4,25 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        //입력값의 맨 뒷자리 수부터 차례대로 3,6,9가 들어가는지 검사
         for(int i = 1; i <= number; i++){
-            int currentNumber = i; //갱신된 숫자를 저장
-            while(currentNumber > 0){
-                int tmp = currentNumber%10;
-                if(tmp != 0 && tmp%3 == 0){
-                    //3,6,9가 들어간 경우
-                    //3,6,9가 포함된 횟수만큼 answer를 증가
-                    answer++;
-                }
-                currentNumber /= 10;
-            }
+            answer += checkCountOfClaps(i);
         }
 
         return answer;
+    }
+
+    private static int checkCountOfClaps(int number){
+        //입력값의 맨 뒷자리 수부터 차례대로 3,6,9가 들어가는지 검사
+        int count = 0;
+        while(number > 0){
+            int tmp = number%10;
+            if(tmp != 0 && tmp%3 == 0){
+                //3,6,9가 들어간 경우
+                //3,6,9가 포함된 횟수만큼 answer를 증가
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
