ef649ec5c65898df429b73c515bcf1c75d9d92c7
chesthyeon <niw927@naver.com>
2022-10-29 16:22:43 +0900

    문제2번 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,20 +1,25 @@
 package onboarding;
 
-import java.util.Stack;
-
 public class Problem2 {
-
+    public static void main(String[] args) {
+        System.out.println(solution("browoanoommnaon"));
+    }
     public static String solution(String cryptogram) {
-        StringBuilder answer = new StringBuilder(); //정답 출력
-        Stack<Character> stack = new Stack<>(); //스택 선언
-        stack.add(cryptogram.charAt(0));// 스택에 첫 문자를 담아 놓는다
-        for (int i = 1; i < cryptogram.length(); i++){ // 두번쨰 문자부터 시작한다.
-            if (stack.peek() == cryptogram.charAt(i)) stack.pop(); // 스택의 마지막 값과 값다면 제거
-            else stack.push(cryptogram.charAt(i)); // 아니라면 push()
-        }
-        while (!stack.isEmpty()){ // 스택이 텅 빌때까지
-            answer.insert(0, stack.pop()); // 스트링 빌더에 스택 값을 넣는다.
-        }
-        return answer.toString(); //문자열로 변환
+       StringBuilder answer = new StringBuilder(); // 정답을 저장할 스트링빌더 선언
+        if (cryptogram.length() < 2) return cryptogram; // 길이가 1이라면 중복이 존재하지 않는다.
+       for(int i = 1; i < cryptogram.length(); i++){ // 모든 문자를 비교를 위한 반복문
+           if (cryptogram.charAt(i - 1) != cryptogram.charAt(i)){ // 다음 문자와 현 문자과 같지 않다면
+               answer.append(cryptogram.charAt(i-1)); // 스트링 빌더에 추가
+           }
+           else while (i < cryptogram.length() && cryptogram.charAt(i - 1) == cryptogram.charAt(i)){ // 같다면 건너뛰기, 길이를 넘었다면
+               i++; // 건너뛰기
+           }
+       }
+
+        if (cryptogram.charAt(cryptogram.length() - 1) != cryptogram.charAt(cryptogram.length() - 2)) // 마지막 문자가 중복이 아니라면 리스트에 추가.
+            answer.append(cryptogram.charAt(cryptogram.length() - 1));
+
+        if (answer.length() == cryptogram.length()) return answer.toString(); // 길이가 같다면 중복이 없다.
+        return solution(answer.toString()); // 중복을 체크한다.
     }
 }
