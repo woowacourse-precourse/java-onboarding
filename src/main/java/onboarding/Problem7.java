6919ee1427c52781175e5f04947190b2aa50ea35
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-29 17:28:04 +0900

    feat :: 나와 친구인 사용자 찾고 myFriendsList에 저장하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,25 +1,45 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem7 {
-    // [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"]
-    // ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        solution(user, friends, visitors);
+    }
 
-    // visitor : ["bedi", "bedi", "donut", "bedi", "shakevan"]
-
-    // mrko가 아는 친구 : donut, shakevan
-    // andole이 아는 친구 : donut, shakevan
-    // donut이 아는 친구 : andole, mrko
-    // shakevan이 아는 친구 :andole, jun, mrko
-    // jun이 아는 친구 : donut, shakevan
-
-    // bedi : 3점
-    // andole : 20점
-    // jun : 20점
+    private static final List<String> myFriends = new ArrayList<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        System.out.println(friendsListVerifier(user, friends));
+        return null;
+    }
+
+    private static List<String> friendsListVerifier(String user, List<List<String>> friends) {
+        saveMyFriends(user, friends);
+        return myFriends;
+    }
+
+    private static void saveMyFriends(String myId, List<List<String>> friends) {
+        for (List<String> users : friends) {
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).equals(myId)) {
+                    if (i == 0) {
+                        myFriends.add(users.get(1));
+                    } else {
+                        myFriends.add(users.get(0));
+                    }
+                }
+            }
+        }
     }
 }
