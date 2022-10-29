b849aaf5ecf19d68dc766d036e081ef81c333020
JUN <chltmdwns96@naver.com>
2022-10-29 15:31:31 +0900

    test : StringToCharsTest 수정
    StringToCharStackTest로 테스트 명 정정
    ArrayList -> Stack

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -3,18 +3,26 @@ package onboarding;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Stack;
 
 class Problem2Test {
     @Test
-    void StringToCharsTest() {
-        List<Character> chars = Problem2.StringToChars("hello");
-        chars.forEach(c -> System.out.print(c + " "));
+    void StringToCharStackTest() {
+        Stack<Character> chars = Problem2.StringToCharStack("hello");
+        for(Character c : chars){
+            System.out.print(c);
+        }
     }
 
     @Test
     void CharsToStringTest() {
-        List<Character> chars = Problem2.StringToChars("hello");
+        List<Character> chars = Problem2.StringToCharStack("hello");
         String string = Problem2.CharsToString(chars);
         System.out.println("string = " + string);
+    }
+
+    @Test
+    void solutionTest(){
+        System.out.println(Problem2.solution("browoanoommnaon"));
     }
 }
