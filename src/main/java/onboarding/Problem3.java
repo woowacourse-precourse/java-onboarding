bf94a2836faf04b837aa521c56f2eb54639af6de
lvalentine6 <ysr1227@naver.com>
2022-10-29 18:22:35 +0900

    feat : 문제 3_3,6,9의 개수만큼 손뼉 횟수를 증가시키는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,12 +9,14 @@ public class Problem3 {
         for (int i = 1; i <= number; i++) {
             String[] numberArr = String.valueOf(i).split("");
             if(checkNumber(numberArr)) {
-
+                answer += countClap(numberArr);
             }
         }
 
         return answer;
     }
+
+    // 각 숫자에 3,6,9가 듣어가는지 확인하는 메서드
     static boolean checkNumber(String[] numberArr) {
         for (int i = 0; i < numberArr.length; i++) {
             if(numberArr[i].equals("3") || numberArr[i].equals("6") || numberArr[i].equals("9")) {
@@ -22,5 +24,16 @@ public class Problem3 {
             }
         }
         return false;
+    }
+
+    // 3,6,9의 개수만큼 손뼉 횟수를 증가시키는 메서드
+    static int countClap(String[] numberArr) {
+        int cnt = 0;
+        for (int i = 0; i < numberArr.length; i++) {
+            if(numberArr[i].equals("3") || numberArr[i].equals("6") || numberArr[i].equals("9")) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
