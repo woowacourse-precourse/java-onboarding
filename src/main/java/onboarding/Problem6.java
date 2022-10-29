f020e15f44d400813c193e24a7cde1893c44408b
alstjr706 <alstjr706@naver.com>
2022-10-29 17:34:03 +0900

    docs : (은행 출금 문제) 기능 목록 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
+        ArrayList<Object> arrayList = new ArrayList<>(Arrays.asList());
         List<String> answer = List.of("answer");
+
+
         return answer;
     }
 }
