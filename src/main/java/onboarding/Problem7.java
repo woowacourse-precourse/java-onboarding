ad31278fb291e3e50c9be1c71da59bf9a67cff94
SEONMl <sm000117@naver.com>
2022-10-29 22:27:39 +0900

    feat: Problem7 SNS class 생성 (추후 리팩터링 필요)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    static class SNS {
+        private String user;
+        private Map<String, List<String>> relationship = new HashMap<>();
+        private Map<String,Integer> recommendationScore = new HashMap<>();
+
+        public SNS(String user) {
+            this.user = user;
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
