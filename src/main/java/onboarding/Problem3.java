b76206d093622e7ea0e0605598c27b0588f3a22c
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-29 19:32:02 +0900

    refactor: Problem3 - 점수 계산 로직 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,20 +6,25 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            if (containThreeSixNine(i)) {
-                answer += countThreeSixNine(i);
-            }
+            String num = Integer.toString(i);
+            answer += getClapNumber(num);
         }
         return answer;
     }
 
-    public static boolean containThreeSixNine(int num) {
-        String str = Integer.toString(num);
-        return str.contains("3") || str.contains("6") || str.contains("9");
+    public static int getClapNumber(String num) {
+        if (containThreeSixNine(num)) {
+            return countThreeSixNine(num);
+        }
+        return 0;
     }
 
-    public static int countThreeSixNine(int num) {
-        String[] str = Integer.toString(num).split("");
+    public static boolean containThreeSixNine(String num) {
+        return num.contains("3") || num.contains("6") || num.contains("9");
+    }
+
+    public static int countThreeSixNine(String num) {
+        String[] str = num.split("");
         return (int)Arrays.stream(str).filter(i -> i.equals("3") || i.equals("6") || i.equals("9")).count();
     }
 }
