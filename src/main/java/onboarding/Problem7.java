e793c50971b6523de917dc843552207744788be9
leeyongjun <leeyongjun0310@naver.com>
2022-10-30 00:42:46 +0900

    feat(PROBLEM7): 사용자의 친구를 찾아주는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Set<String> getUserFriends(String user, List<List<String>> friends){
+        // 사용자의 친구를 찾아주는 메소드
+        Set<String> userFriends = new HashSet<>();
+        for (List<String> friend : friends){
+            String leftFriend = friend.get(0);
+            String rightFriend = friend.get(1);
+            if (leftFriend.equals(user)){
+                userFriends.add(rightFriend);
+            }
+            if (rightFriend.equals(user)){
+                userFriends.add(leftFriend);
+            }
+
+        }
+        return userFriends;
     }
 }
