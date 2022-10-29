918fe693ff034e6ae7a207b1c3fd0fd66de42359
eora21 <eora21@naver.com>
2022-10-29 20:40:04 +0900

    feat: 🎸 추가 크루들의 이메일-닉네임 List를 순회하며 닉네임 중 두 글자-이메일 Map을 생성하는 메서드
    같은 글자가 연속적으로 포함되는 걸 방지하기 위해, 닉네임에서 두 글자씩 가져와 Map에 유지하며 해당 글자를 key로 가지는
    크루들의 이메일을 정리한다. / List를 순회하며 Map을 만드는 건 하나의 메서드다. 해당 메서드 내부에서의 key를 만드는
    것, Map에 put하는 것 또한 메서드이다. 따라서 해당 메서드 이름은 하위 메서드의 동작 의미까지 담아야 할 것이다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
+
+    private static void putTwoCharsAndEmailsByTraversingForms(Map<String, Set<String>> twoCharsAndEmails, List<List<String>> forms) {
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            Set<String> twoCharsSet = makeTwoCharsKeyUsingNickname(nickname);
+            putTwoCharsAndEmails(twoCharsAndEmails, twoCharsSet, email);
+        }
+    }
+
+    /**
+     * 신청받은 닉네임 중 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록을 return 하도록 solution 메서드를 완성하라.
+     *
+     * @param forms 이메일, 닉네임
+     * @return 닉네임 연속 글자 포함 목록, 이메일 오름차순, 중복제거
+     */
     public static List<String> solution(List<List<String>> forms) {
+        Map<String, Set<String>> twoCharsAndEmails = new HashMap<>();
+        putTwoCharsAndEmailsByTraversingForms(twoCharsAndEmails, forms);
         List<String> answer = List.of("answer");
         return answer;
     }
+
+
 }
