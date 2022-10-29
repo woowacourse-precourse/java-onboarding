f211abe4bf315c73dbaeda0fd1b1bbcd96a6bcdc
song-wooseok <sws981202@gmail.com>
2022-10-29 21:13:18 +0900

    feat: [PROBLEM2 - 가능1] 두 개의 문자열이 같은지 검증하는 기능

---
+++ src/main/java/onboarding/problem2/domain/DuplicateCryptedText.java
@@ -0,0 +1,11 @@
+package onboarding.problem2.domain;
+
+public class DuplicateCryptedText {
+    private String cipherText;
+    private String decryptedText = null;
+
+    public DuplicateCryptedText(String cipherText){
+        this.cipherText = cipherText;
+    }
+
+}
---
+++ src/main/java/onboarding/problem2/service/DecryptionService.java
@@ -0,0 +1,13 @@
+package onboarding.problem2.service;
+
+import onboarding.problem2.domain.DuplicateCryptedText;
+
+public class DecryptionService {
+    public static void decryption(DuplicateCryptedText cryptedText){
+
+    }
+
+    public static boolean compareWord(char c1, char c2){
+        return (c1 == c2);
+    }
+}
