6ba093f1b90a0ead052ce385a86eac49fdd8fdcc
olrlobt <317tmdgjs@naver.com>
2022-10-29 19:53:44 +0900

    feat: Problem4 [2]변환된 글자를 이어 붙임

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,7 +8,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer=setFrogWord(word);
         return answer;
+    }
+
+    private static String setFrogWord(String word){
+        char c;
+        StringBuilder answer = new StringBuilder();
+
+        for(int i=0; i<word.length(); i++){
+            c = changeFrog(word.charAt(i));
+            answer.append(c);
+        }
+        return answer.toString();
     }
 
     static char changeFrog(int c){
