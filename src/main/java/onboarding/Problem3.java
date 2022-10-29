71d419bc8a6d613136a01e9f963b684d5dc0641d
knh4437 <knh4437@naver.com>
2022-10-29 17:10:21 +0900

    Sovled: 1week Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1;i<=number;i++) {
+            int a = i;
+            while (a > 0) {
+                if (check(a))
+                    answer++;
+                a/=10;
+            }
+        }
+
         return answer;
+    }
+    public static Boolean check(int num) {
+        switch (num%10) {
+            case 3:
+            case 6:
+            case 9:
+                return true;
+            default:
+                return false;
+        }
     }
 }
