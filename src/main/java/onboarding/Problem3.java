47ff757440bb655b7eea4192afd36121b15e8446
unknown <sonhl0723@naver.com>
2022-10-30 00:27:52 +0900

    Problem-3 Complete

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem3 {
+
+    // 각 자리의 수가 3 or 6 or 9 인지 확인하여 총 개수를 return
+    public static int checkNum(String str, int strLen) {
+        int res = 0;
+        char tmp;
+
+        for (int i = 0; i < strLen; i++) {
+            tmp = str.charAt(i);
+            if (tmp == '3' || tmp == '6' || tmp == '9') {
+                res += 1;
+            }
+        }
+
+        return res;
+    }
     public static int solution(int number) {
         int answer = 0;
+        String tmp;
+        int strLen;
+
+        // 1부터 number까지 3,6,9가 포함된 개수를 확인
+        for (int i = 1; i <= number; i++) {
+            tmp = String.valueOf(i);
+            strLen = tmp.length();
+            answer += checkNum(tmp, strLen);
+        }
+
         return answer;
     }
 }
