b3db8ab2c0355f5e7be9c579fe0a215ee8fd00d4
eyeofsol <kingjihong@naver.com>
2022-10-29 17:38:47 +0900

    feat(문제2): 연속된 문자를 지운 문자열을 반환하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,4 +15,18 @@ public class Problem2 {
         return false;
     }
 
+    public static StringBuilder removeConsecutiveWord(StringBuilder cryptogram) {
+        StringBuilder sb = new StringBuilder();
+        int temp;
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            temp = findConsecutiveWordEndIndex(cryptogram, i);
+            if (temp == i) {
+                sb.append(cryptogram.charAt(i));
+                continue;
+            }
+            i = temp;
+        }
+        return sb;
+    }
 }
