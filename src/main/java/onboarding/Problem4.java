b14ceb18d68d52f5cd7b3c66277e97f914d052f8
seonghoo1217 <seonghoo1217@naver.com>
2022-10-29 14:42:51 +0900

    feat : Add a word Parameter Length Check Exception on [Task4]

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,13 +1,24 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        // 97 109 a-m
-        // 110 122 n-z
+    // 97 109 a-m
+    // 110 122 n-z
 
-        // 65 77 A-M
-        // 78 90 N-Z
-        return answer;
+    // 65 77 A-M
+    // 78 90 N-Z
+    public static String solution(String word) {
+        StringBuilder sb=new StringBuilder();
+        try {
+            checkWordLength(word);
+            return sb.toString();
+        }catch (Exception e){
+            return "";
+        }
     }
+
+    public static void checkWordLength(String word) throws Exception {
+        int length = word.length();
+        if (length<1||length>1000) throw new Exception("문자열 길이 제한 초과입니다");
+    }
+
 }
