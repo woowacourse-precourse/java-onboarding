f7efd8d2a2b5c64dc772196f135d3539355f35af
ChanghyunKim <variety82p@gamil.com>
2022-10-29 20:41:32 +0900

    feat : Implement countClap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    static int countClap(int num) {
+        int cnt = 0;
+        int q = 0;
+        while(num != 0) {
+            q = num % 10;
+            if(q != 0 && q % 3 == 0) {
+                cnt++;
+            }
+            num /= 10;
+        }
+        return cnt;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
