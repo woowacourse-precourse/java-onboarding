a86b87db6bac9b0e4eba2871f79cd7fbe17d7320
HaODay <ycis5013@naver.com>
2022-10-29 16:52:50 +0900

    [Problem5] 오만 원권 개수 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,15 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer= new ArrayList<Integer>();
+
+        answer.add(money/50000);
+
         return answer;
     }
 }
