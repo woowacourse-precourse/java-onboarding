d49034cd26b13887509b9b9b62d961f642e655ab
HoeSeong <ghltjd369@gmail.com>
2022-10-29 20:10:23 +0900

    feat : (problem 4) 알파벳 대문자, 소문자를 순서대로 나열한 문자열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        String alphabet = "";                   //후에 indexOf를 사용하기 위해 String으로 선언
+
+        for(int i = 0; i < 26; i++){
+            alphabet += (char) (65 + i);       //ASCII code를 char로 형변환하여 저장 (대문자)
+        }
+
+        for(int i = 0; i < 26; i++){
+            alphabet += (char) (97 + i);       //소문자
+        }
+
         return answer;
     }
 }
