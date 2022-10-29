01e28d99e8b1e683eceac027dda27491bcb52a1c
youngkyunIm <yee0230@naver.com>
2022-10-29 18:38:35 +0900

    feat(Problem6) : loof 2 length string in nickname

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,7 +8,11 @@ public class Problem6 {
         Set<String> set = new HashSet<>();
 
         for (int i = 0; i < forms.size(); i++) {
-
+            List<String> info = forms.get(i);
+            String nickname = info.get(1);
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                String tmp = String.valueOf(nickname.charAt(j)) + (nickname.charAt(j + 1));
+            }
         }
 
         answer.addAll(set);
