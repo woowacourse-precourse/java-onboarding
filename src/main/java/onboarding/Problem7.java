6cc2b2212afcfa7d511269a60139121953e76e86
es <mascoeskim@gmail.com>
2022-10-29 11:31:59 +0900

    Problem7/1.초기세팅 및 exFriends구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        System.out.println(friends);
+        ArrayList<String> exFriends = new ArrayList<String>();
+
+        for (int k=0;friends.size()>k;k++){
+            if(Objects.equals(friends.get(k).get(0), user)) {
+                exFriends.add(friends.get(k).get(1));
+            }
+            else if(Objects.equals(friends.get(k).get(1), user)) {
+                exFriends.add(friends.get(k).get(0));
+            }
+        }
+        System.out.println("기존친구"+exFriends);
+///친구목록에서 이미 user와 친구를 exFriends로 분리///
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
