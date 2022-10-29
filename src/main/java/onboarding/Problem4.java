2937e2d192aa9538a958ef66e7d21f425e34fa36
sdcodebase <sdcodebase@gmail.com>
2022-10-29 17:43:33 +0900

    feat: Problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        HashMap<Integer, Integer> alphabetMap = new HashMap<>();
+        for (int i = 0; i < 'Z' - 'A'; i++) {
+            alphabetMap.put('a' + i, 'z' - i);
+            alphabetMap.put('A' + i, 'Z' - i);
+        }
+
+        return word.chars()
+                .map(c -> alphabetMap.getOrDefault(c, c))
+                .mapToObj(Character::toString)
+                .collect(Collectors.joining());
     }
 }
