0b552e65aa7804ea4dc9e7d16b42e2099ed98976
HoeSeong <ghltjd369@gmail.com>
2022-10-29 20:17:02 +0900

    fix : (problem 2) answer의 초기화 문자열 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
 
         LinkedList<String> password = new LinkedList<String>();
 
