59ac865083f02d44b98b3dc5ff90952418e2d1f5
Park shinyeong <syngh503@gmail.com>
2022-10-30 02:13:45 +0900

    Problem7/Refactor/if문 수정, 코드 리팩토링 if문을 수정하고 전체적으로 코드를 리팩토링 하였습니다. Relate to: #7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,8 +7,8 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         List<String> userFriends = getUserFriends(user, friends);
         HashMap<String, Integer> listOfScore = new HashMap<>();
-        listOfScore = getAcquaintance(user, listOfScore, userFriends, friends);
-        listOfScore = getVisitorScore(listOfScore, visitors, userFriends);
+        getAcquaintance(user, listOfScore, userFriends, friends);
+        getVisitorScore(listOfScore, visitors, userFriends);
         List<Map.Entry<String, Integer>> sortedListOfScore = getSortedListOfScore(listOfScore);
         answer = getTopFive(sortedListOfScore);
         return answer;
@@ -26,13 +26,12 @@ public class Problem7 {
             }
             if (user2.equals(user)) {
                 userFriends.add(user1);
-                continue;
             }
         }
         return userFriends;
     }
 
-    public static HashMap<String, Integer> getAcquaintance (String user, HashMap<String, Integer> listOfScore, List<String> userFriends, List<List<String>> friends) {
+    public static void getAcquaintance (String user, HashMap<String, Integer> listOfScore, List<String> userFriends, List<List<String>> friends) {
         for (String friendId: userFriends) {
             for (List<String> friend: friends) {
                 String user1 = friend.get(0);
@@ -40,7 +39,7 @@ public class Problem7 {
                 if(user1.equals(user) || user2.equals(user)) {
                     continue;
                 }
-                if (friendId.equals(friendId)) {
+                if (user1.equals(friendId)) {
                     listOfScore.put(user2, listOfScore.getOrDefault(user2, 0) + 10);
                 }
                 if (user2.equals(friendId)) {
@@ -48,17 +47,14 @@ public class Problem7 {
                 }
             }
         }
-        return listOfScore;
     }
 
-    public static HashMap<String, Integer> getVisitorScore (HashMap<String, Integer> listOfScore, List<String> visitors, List<String> userFriends) {
+    public static void getVisitorScore (HashMap<String, Integer> listOfScore, List<String> visitors, List<String> userFriends) {
         for(String visitor: visitors) {
-            if(userFriends.contains(visitor) == false) {
+            if(!userFriends.contains(visitor)) {
                 listOfScore.put(visitor, listOfScore.getOrDefault(visitor, 0) + 1);
             }
         }
-
-        return listOfScore;
     }
 
     public static List<Map.Entry<String, Integer>>  getSortedListOfScore(HashMap<String, Integer> listOfScore) {
@@ -66,7 +62,7 @@ public class Problem7 {
         entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> user1, Map.Entry<String, Integer> user2) {
-                if (user2.getValue() == user1.getValue()) {
+                if (Objects.equals(user2.getValue(), user1.getValue())) {
                     String user1Key = user1.getKey();
                     String user2Key = user2.getKey();
                     int n = 0;
@@ -84,10 +80,7 @@ public class Problem7 {
 
     public static List<String> getTopFive(List<Map.Entry<String, Integer>> entryList) {
         List<String> recommendList = new ArrayList<>();
-        int numOfPeople = 5;
-        if(entryList.size() < 5) {
-            numOfPeople = entryList.size();
-        }
+        int numOfPeople = Math.min(entryList.size(), 5);
         for(int i = 0; i < numOfPeople; i++) {
             Map.Entry<String, Integer> entry = entryList.get(i);
             recommendList.add(entry.getKey());
