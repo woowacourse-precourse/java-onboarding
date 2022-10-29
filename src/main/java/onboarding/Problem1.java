d81011b2bc61d9b3de0ea2ae8ee34b1690557d50
syahn <496300@naver.com>
2022-10-29 14:21:42 +0900

    feat: Problem1 입력 조건 검사 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,6 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-
     //입력 조건을 검사하는 method
     public static boolean verifyConditions(List<Integer> test){
         boolean metConditions = true;
@@ -15,7 +14,6 @@ class Problem1 {
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-
         //입력 조건 만족하지 않을 경우 예외사항 return
         if(!verifyConditions(pobi))
             return -1
