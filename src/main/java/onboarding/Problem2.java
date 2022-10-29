721dd27fc0e4ded973406abe329e7f64acf4836c
kkkwp <chxrryda@gmail.com>
2022-10-30 02:35:37 +0900

    feat(problem2): 문제 2 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,10 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        Stack<Character> decodedCryptogram = deleteChar(cryptogram);
+        answer = convertToString(decodedCryptogram);
+
         return answer;
     }
 
