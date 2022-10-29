c9f89628b46a8acecae49b1c772c97de0e40248a
JUN <chltmdwns96@naver.com>
2022-10-29 22:47:38 +0900

    feature : 알파벳 HashMap 생성 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,25 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-//## PROBLEM4 기능 구현 정리
-//        1. 예외처리 :
-//          초반 예외 :word 길이 1이상 1000 이하 검증
-//          3번 기능 예외 : 알파벳이 아니면 문자 반환 X
-//        2. 알파벳 HashMap 생성
-//          - 'a'~'z'='z'~'a'
-//          - 'A'~'Z'='Z'~'A'
-//          - ' ' -> ' '반환
-//        3. 문자열 분리
-//        4. 분리된 문자들 HashMap에 Key로 넣어서 Value반환받기
-//          - 하나씩 반한받아서 String 변수에 +로 붙이기
-//        5. 결과 출력
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         if(checkLengthException(word)){
             return null;
         }
+
+        HashMap<Character, Character> reverseAlphabet = createAlphabetMap();
         return answer;
     }
 
@@ -30,5 +22,34 @@ public class Problem4 {
             return false;
         }
         return true;
+    }
+
+    protected static HashMap<Character,Character> createAlphabetMap(){
+        HashMap<Character, Character> result = new HashMap<>();
+        List<Character> alphaList = new ArrayList<>();
+        List<Character> reverseAlphaList = new ArrayList<>();
+        result.put(' ', ' ');
+        final int ASCII_A = 65;
+        final int ASCII_Z = 90;
+        final int ASCII_a = 97;
+        final int ASCII_z = 122;
+        final int ALPHABET_NUM = 26;
+
+        for(char c=ASCII_A; c <= ASCII_Z; c++){
+            alphaList.add(c);
+        }
+        for(char c=ASCII_Z; c >= ASCII_A; c--){
+            reverseAlphaList.add(c);
+        }
+        for(char c=ASCII_a; c <= ASCII_z; c++){
+            alphaList.add(c);
+        }
+        for(char c=ASCII_z; c >= ASCII_a; c--){
+            reverseAlphaList.add(c);
+        }
+        for(int i=0; i<(ALPHABET_NUM*2);i++){
+            result.put(alphaList.get(i), reverseAlphaList.get(i));
+        }
+        return result;
     }
 }
