0b8e57f9a8b2731e4781ba56f416aa7e4104f08d
Jin409 <sualng123@gmail.com>
2022-10-29 18:18:38 +0900

    chore(problem7): User 클래스 생성 및 초기 세팅

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,12 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import onboarding.problem7.User;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        User savedUser = new User(user, friends, visitors);
+        return new ArrayList<String>(); // 임시
     }
 }
--- src/main/java/onboarding/problem7/User.java
+++ src/main/java/onboarding/problem7/User.java
@@ -1,2 +1,23 @@
-package onboarding.problem7;public class User {
+package onboarding.problem7;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+
+    private final String name;
+    private final List<List<String>> friends;
+    private final List<String> visitors;
+
+    private final List<String> relationships = new ArrayList<String>();
+
+    private final Map<String, Integer> SCORES_BY_USERS = new HashMap<String, Integer>();
+
+    public User(String name, List<List<String>> friends, List<String> visitors) {
+        this.name = name;
+        this.friends = friends;
+        this.visitors = visitors;
+    }
 }
