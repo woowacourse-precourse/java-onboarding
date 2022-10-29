01c54c3fa476e5854cc5e63960f915e9bba2432f
pushedrumex <pushedrumex@naver.com>
2022-10-29 19:58:09 +0900

    problem4: 문자열을 사전 반대로 변환(solution)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,25 +1,28 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem4 {
     public static String solution(String word) {
+        String[] strArr = word.split("");
         String answer = "";
+        for (String c: strArr) {
+            answer += Pattern.matches("[a-zA-Z]", c) ? reverseAlpha(c) : c;
+        }
         return answer;
     }
-    static char reverseAlpha(char c) {
+    static char reverseAlpha(String c) {
         String upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String revUpperAlpha = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
         String lowerAlpha = "abcdefghijklnmopqrstuvwxyz";
         String revLowerAlpha = "zyxwvutsrqponmlkjihgfedcba";
 
-        if (Character.isUpperCase(c)) {
+        if (Character.isUpperCase(c.toCharArray()[0])) {
             int index = upperAlpha.indexOf(c);
             return revUpperAlpha.charAt(index);
         } else {
             int index = lowerAlpha.indexOf(c);
             return revLowerAlpha.charAt(index);
         }
-    }
-    public static void main(String args[]) {
-        System.out.println(reverseAlpha('A'));
     }
 }
