d40a24cf91df8b3b1b17342d24960d1e4bb06ec8
JungYoonshin <sju08227@naver.com>
2022-10-29 17:20:59 +0900

    feat: #problem6 -> 닉네임의 이어진 2글자씩 추출해서 중복을 확인하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,9 +12,29 @@ public class Problem6 {
         return answer;
     }
 
-    private static void blavlala(List<List<String>> forms){
-        for(List<String> nameAndEmailList : forms){
+    private static void getEachListInForms(List<List<String>> forms) {
+        for (List<String> nameAndEmailList : forms) {
             checkNameDuplication(nameAndEmailList);
+        }
+    }
+
+    private static void checkNameDuplication(List<String> nameAndEmail) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        HashSet<String> emails = new HashSet<>();
+
+        String name = nameAndEmail.get(1);
+        String email = nameAndEmail.get(0);
+
+        for (int i = 0; i < name.length() - 1; i++) {
+            name = name.substring(i, i + 2);
+            //해당키가 이미 있다면
+            if (hashMap.containsKey(name)) {
+                String duplicateEmail = hashMap.get(name);
+                emails.add(email);
+                emails.add(duplicateEmail);
+            } else {
+                hashMap.put(name, email);
+            }
         }
     }
 
