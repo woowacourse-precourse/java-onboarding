8fece6fa99a74b462ea23638964e9ad064aeb738
채우석 <wochae@wochae-MacBookAir.local>
2022-10-29 23:27:23 +0900

    [fix] 2.1 자료형을 LinkedList 타입으로 선언 및 size = 1 나오는 문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,26 +1,31 @@
 package onboarding;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Problem2 {
     public static List<String> crypto;
     public static String solution(String cryptogram) {
         String answer = "answer";
-        crypto = Arrays.asList(cryptogram);
+        crypto = new LinkedList<String>();
+        turnIntoList(cryptogram);
         iterateList(crypto);
         answer = concatList(crypto);
         return answer;
     }
 
     private static void iterateList(List<String> list) {
-        for (int i = 0; i < list.size() - 1; i++) {
+        for (int i = 0; i < list.stream().count() - 1; i++) {
             if (list.get(i).equals(list.get(i + 1))) {
                 list.remove(i + 1);
                 list.remove(i);
                 i--;
             }
+        }
+    }
+    private static void turnIntoList(String cryptogram) {
+        StringTokenizer st = new StringTokenizer(cryptogram, "abcdefghijklmnopqrstuvwxyz", true);
+        while (st.hasMoreTokens()) {
+            crypto.add(st.nextToken());
         }
     }
     private static String concatList(List<String> list) {
