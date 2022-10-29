26b1ffc8d2e0111805c3c0caa94c15c392f69856
eyeofsol <kingjihong@naver.com>
2022-10-29 19:15:19 +0900

    feat(문제5): 화폐 갯수 기록하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem5 {
+    static HashMap<Integer, Integer> counter;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        counter = new HashMap<>();
+
+
+
         return answer;
     }
+
+    public static void updateCount(int currency, int count) {
+        counter.put(currency, count);
+    }
+
 }
