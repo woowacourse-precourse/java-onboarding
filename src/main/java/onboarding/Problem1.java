327f54984e3e73e4f7ef06f994e4f2c818f8042b
SeokHwan An <asok369258@naver.com>
2022-10-29 17:31:38 +0900

    feat(Problem1) : 주어진 페이지의 수가 1과 400사이인지 검증하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,8 @@ import java.util.List;
 class Problem1 {
 
     private static final int CONTINUITY = 1;
+    private static final int PAGE_MIN = 1;
+    private static final int PAGE_MAX = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
@@ -13,6 +15,14 @@ class Problem1 {
 
     public static boolean checkPageContinuity(List<Integer> page) {
         if (page.get(1) - page.get(0) == CONTINUITY) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkPageValidation(List<Integer> page) {
+        if (PAGE_MIN <= page.get(0) && page.get(0) <= PAGE_MAX && PAGE_MIN <= page.get(1)
+            && page.get(1) <= PAGE_MAX) {
             return true;
         }
         return false;
