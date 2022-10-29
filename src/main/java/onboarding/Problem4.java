cbba2d911df3ee1dbc36e1b43a498e22646ea99d
5uhwann <5uhwann27@gmail.com>
2022-10-29 15:08:11 +0900

    feat: PROBLEM4 - 기능1 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+  private static Map<Character, Character> greenFrogDictionary;
+
+  public static String solution(String word) {
+    String answer = "";
+    return answer;
+  }
+
+  public static void makeGreenFrogDictionary() {
+    String upperCase = "ABCDEFGJIJKLMNOPQRSTUVWXYZ";
+    String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+    greenFrogDictionary = new HashMap<>();
+
+    for (int i = 0; i < upperCase.length(); i++) {
+      greenFrogDictionary.put(upperCase.charAt(i), upperCase.charAt(upperCase.length()-1 - i));
     }
+
+    for (int i = 0; i < lowerCase.length(); i++) {
+      greenFrogDictionary.put(lowerCase.charAt(i), lowerCase.charAt(lowerCase.length()-1 - i));
+    }
+  }
+  
 }
