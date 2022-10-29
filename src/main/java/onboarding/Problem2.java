843c6b8d2c560c03d19e20de42e6135e06f912f7
dOcOb <abg1418@gmail.com>
2022-10-29 17:26:12 +0900

    complete problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,48 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        ArrayList<String> texts = new ArrayList<>(Arrays.asList(cryptogram.split("")));
+        int s;
+        int e;
+        loop:
+        while(true) {
+            if(texts.size() < 2){break;}
+            for (int i = 0; i < texts.size() - 1; i++) {
+                if (texts.get(i).equals(texts.get(i + 1))) {
+                    s = i;
+                    e = s;
+                    String t = texts.get(i);
+                    //중복 글자 끝 추척
+                    for (int count = s; count < texts.size(); count++) {
+                        if (texts.get(count).equals(t)) {
+                            e = count;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    //중복 글자 제거
+                    for (int j = 0; j <= e - s; j++) {
+                        texts.remove(i);
+                    }
+                    break;
+                }
+                if(i == texts.size()-2){
+                    break loop;
+                }
+            }
+        }
+        String answer = "";
+        for (String str : texts) {
+            answer = answer + str;
+        }
+
         return answer;
     }
 }
