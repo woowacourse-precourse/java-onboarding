8d33bd85d985ae19723692e5451c2ab74406890a
맹순영 <symaeng98@naver.com>
2022-10-29 15:28:05 +0900

    fix: 문자열 변환 기능 구현 및 wordMap 길이 수정(13->26)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,16 +5,28 @@ import java.util.Map;
 
 public class Problem4 {
     public static String solution(String word) {
-        Map<Character,Character> wordMap = new HashMap<Character, Character>();
+        Map<Character, Character> wordMap = new HashMap<Character, Character>();
 
         char upperA = 'A';
         char lowerA = 'a';
 
-        for (int i=0; i<13; i++){
-            wordMap.put((char) (upperA+i), (char) (upperA+25-i));
-            wordMap.put((char) (lowerA+i), (char) (lowerA+25-i));
+        // 알파벳의 길이
+        int dictionaryLength = 26;
+        for (int i = 0; i < dictionaryLength; i++) {
+            wordMap.put((char) (upperA + i), (char) (upperA + 25 - i));
+            wordMap.put((char) (lowerA + i), (char) (lowerA + 25 - i));
         }
 
-        return "";
+        // StringBuilder를 사용한 문자열 합치기
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char w = word.charAt(i);
+            if (w == ' ') {
+                result.append(w);
+                continue;
+            }
+            result.append(wordMap.get(w));
+        }
+        return result.toString();
     }
 }
