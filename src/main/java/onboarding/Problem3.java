3b7fb4ee4d4a45a04bccd331cc0bb8fc7eb67ae8
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-29 10:23:37 +0900

    접근방법을 수정하여 1부터 number까지의 숫자가 나열된 문자열을 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,15 +1,12 @@
 package onboarding;
 
-import java.util.ArrayList;
-
 public class Problem3 {
     public static int solution(int number) {
-        ArrayList<Integer> set = new ArrayList<>();
+        String numOrder = "";
 
         for(int i=1; i <= number; i++){
-            set.add(i);
+            numOrder += i;
         }
-        
         int answer = 0;
         return answer;
     }
