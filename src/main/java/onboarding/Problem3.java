aec3e6b8d99cce97370d4f8ac9462dffb03b1991
wonjunYou <youwjune@gmail.com>
2022-10-29 15:42:01 +0900

    feat : (Problem 3) 손뼉을 쳐야 하는 횟수를 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,12 +10,41 @@ public class Problem3 {
             return 0;
         }
 
-        return 0;
+        return countClaps(number);
     }
 
     public static void validateRange(int number) {
         if (number < 1 || number > MAX_NUMBER) {
             throw new IllegalArgumentException("유효하지 않은 범위 내의 숫자입니다.");
         }
+    }
+
+    private static int countClaps(int end) {
+        int claps = 0;
+
+        for (int i = 1; i < end + 1; i++) {
+            claps += countClapsForEachNumber(i);
+        }
+
+        return claps;
+    }
+
+    private static int countClapsForEachNumber(int number) {
+        int numberCount = 0;
+
+        while (number > 0) {
+            numberCount += checkNumber(number % 10);
+            number /= 10;
+        }
+
+        return numberCount;
+    }
+
+    private static int checkNumber(int digit) {
+        if (digit == 3 || digit == 6 || digit == 9) {
+            return 1;
+        }
+
+        return 0;
     }
 }
