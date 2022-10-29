cd0c94aeef705f79e6db287a558d7e9be5e2502d
lecocococo <chemwang13@naver.com>
2022-10-29 16:10:15 +0900

    Feat: Problem7의 모든 친구 관계 만드는 기능추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+
+	public static Map<String, List<String>> createFriendRelations(List<List<String>> friends) {
+		Map<String, List<String>> friendRelations = new HashMap<>();
+
+		for (List<String> friend : friends) {
+			createFriendRelation(friend.get(0), friend.get(1), friendRelations);
+			createFriendRelation(friend.get(1), friend.get(0), friendRelations);
+		}
+
+		return friendRelations;
+	}
+
+	public static void createFriendRelation(String friend1, String friend2,
+											Map<String, List<String>> friendRelations) {
+		List<String> friendList;
+		friendList = friendRelations.getOrDefault(friend1, new ArrayList<>());
+		friendList.add(friend2);
+		friendRelations.put(friend1, friendList);
+	}
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
