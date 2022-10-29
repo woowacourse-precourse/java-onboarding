9b30a59aab8391cfeb3b45cd18aefd7562cccb6b
2jun0 <soo28819@gmail.com>
2022-10-29 15:11:29 +0900

    Refact: 문제 1번 - Page 생성 시 검증 코드 분리

--- src/main/java/onboarding/problem1/Pages.java
+++ src/main/java/onboarding/problem1/Pages.java
@@ -5,12 +5,8 @@ public class Pages {
     private final int page2;
 
     public Pages(int page1, int page2) {
-        // Assert args
-        if (!(page1 >= 1 && page1 <= 399)) {
-            throw new IllegalArgumentException("페이지 값은 다음과 같은 조건을 만족해야 합니다 : 1 <= page1 <= 309");
-        } else if (!(page1%2 == 1 && page2 == page1+1)) {
-            throw new IllegalArgumentException("페이지 값은 다음과 같은 조건을 만족해야 합니다 : page1 = 홀수, page2 = page1 + 1");
-        }
+        validateArgumentsRange(page1, page2);
+        assertArgPage1IsOdd(page1);
 
         this.page1 = page1;
         this.page2 = page2;
@@ -22,5 +18,21 @@ public class Pages {
 
     public int getPage2() {
         return page2;
+    }
+
+    public void validateArgumentsRange(int page1, int page2) {
+        if (!(page1 >= 1 && page1 <= 399)) {
+            throw new IllegalArgumentException("페이지 값은 다음과 같은 조건을 만족해야 합니다 : 1 <= page1 <= 399");
+        }
+
+        if (page2 != page1+1) {
+            throw new IllegalArgumentException("페이지 값은 다음과 같은 조건을 만족해야 합니다 : page2 = page1 + 1");
+        }
+    }
+
+    public void assertArgPage1IsOdd(int page1) {
+        if (page1%2 == 0) {
+            throw new IllegalArgumentException("페이지 값은 다음과 같은 조건을 만족해야 합니다 : page1 = 홀수");
+        }
     }
 }
