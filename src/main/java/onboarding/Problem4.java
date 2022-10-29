1369522ee58c28a317996d6ff780482f0c4d3b73
yg0585 <yg0585@naver.com>
2022-10-29 17:30:42 +0900

    2번 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,13 +2,24 @@ package onboarding;
 
 //-----기능-----
 //1. 청개구리 사전 만들기
+//2. 대소문자 판별
 
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         String dict = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
 
-        for (int i = 0 ; i < )
+        for (int i = 0 ; i < word.length(); i++)
+        {
+            char c = word.charAt(i);
+            System.out.println((int)c);
+            if (Character.isUpperCase(c))
+                answer += dict.charAt((int)c - 'A');
+            else if(Character.isLowerCase(c))
+                answer += dict.charAt((int)c - 'a');
+            else
+                answer += c;
+        }
         return answer;
     }
 }
