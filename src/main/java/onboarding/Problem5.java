5c6e8a571ce02211cb1863e591c5690fc8c03211
es <mascoeskim@gmail.com>
2022-10-29 09:03:33 +0900

    Problem5/1.기능구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,41 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        System.out.println(money);
+
+        int fiveMillion = money / 50000;
+        int changes = money % 50000;
+        int million = changes / 10000;
+        int changes2 = changes % 10000;
+        int fiveThousand = changes2 / 5000;
+        int changes3 = changes2 % 5000;
+        int thousand = changes3 / 1000;
+        int changes4 = changes3 % 1000;
+        int fiveHundred = changes4 / 500;
+        int changes5 = changes4 % 500;
+        int hundred = changes5 / 100;
+        int changes6 = changes5 % 100;
+        int fifty = changes6 / 50;
+        int changes7 = changes6 % 50;
+        int ten = changes7 / 10;
+        int one = changes7 % 10;
+
+       ArrayList<Integer> answer = new ArrayList<Integer>();
+
+        answer.add(fiveMillion);
+        answer.add(million);
+        answer.add(fiveThousand);
+        answer.add(thousand);
+        answer.add(fiveHundred);
+        answer.add(hundred);
+        answer.add(fifty);
+        answer.add(ten);
+        answer.add(one);
+
+        System.out.println(answer);
         return answer;
     }
 }
