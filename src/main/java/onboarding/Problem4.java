d19bc17ba4dc2bf2864e6fdbd7cbe6a1b2e61925
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-29 20:23:31 +0900

    [Problem4] feat: solution, incodeIndexOf 메서드 구현
    - String.replace(String str)는 탐색이 들어가므로 인덱스를 지정할 수 있는 StringBuffer.replace()을 사용하여 이중반복을 피한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuffer sb = Problem2.getStringBuffer(word);
+        for (int i = 0; i < sb.length(); i++) {
+            incodeIndexOf(i, sb);
+        }
+        answer = sb.toString();
         return answer;
+    }
+    /** 특정 인덱스를 규칙에 맞게 인코딩하는 함수 */
+    private static void incodeIndexOf(int index, StringBuffer sb) {
+        char indexChar = sb.charAt(index);
+        char reversedChar = ' ';
+        if ('a' <= indexChar <= 'z') {
+            reversedChar = (char)('a' + 'z' - indexChar);
+        }
+        if ('A' <= indexChar <= 'Z') {
+            reversedChar = (char)('A' + 'Z' - indexChar);
+        }
+        sb.replace(index, index + 1, String.valueOf(reversedChar));
+        return;
     }
 }
