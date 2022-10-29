29f4f551bcbc463a533c8de75d2d556cfa0480af
gyuturn <rbtjrrbals@gmail.com>
2022-10-29 19:15:10 +0900

    feat: Problem5 - withdrawMoney 메서드

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,20 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<Integer> withdrawMoney(int money) {
+        List<Integer> moneyList = new ArrayList<>();
+        moneyList.add(money / 50000);
+        moneyList.add((money % 50000)/10000);
+        moneyList.add((money % 10000)/5000);
+        moneyList.add((money % 5000)/1000);
+        moneyList.add((money % 1000)/500);
+        moneyList.add((money % 500)/100);
+        moneyList.add((money % 100)/50);
+        moneyList.add((money % 50)/10);
+        moneyList.add((money % 10)/1);
+
+        return moneyList;
     }
 }
