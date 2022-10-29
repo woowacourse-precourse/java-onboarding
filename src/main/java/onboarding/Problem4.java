a6f6c7b683433273dd59cdf0a39f68c9e907a49f
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-29 18:13:31 +0900

    feat: Problem4 대문자는 대문자로, 소문자는 소문자로 변환하는 기능 구현, 모든 케이스 통과

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,14 +5,14 @@ import java.util.HashMap;
 /**
  * [o] 알파벳 외의 문자는 변환하지 않는다.
  * [o] word를 청개구리 사전을 참고해 반대로 변환한다.
- * [ ] 알파벳 대문자는 대문자로, 알파벳 소문자는 소문자로 변환한다.
+ * [o] 알파벳 대문자는 대문자로, 알파벳 소문자는 소문자로 변환한다.
  */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         HashMap<Character, Character> treeFrogDict = replaceOtherCharDict();
         answer = getReverseWord(word, treeFrogDict);
-
+        answer = checkUpperLower(word, answer);
         return answer;
     }
 
@@ -46,5 +46,20 @@ public class Problem4 {
         }
 
         return replaceWord;
+    }
+
+    public static String checkUpperLower(String originalWord, String answer) {
+        for(int i = 0; i<originalWord.length(); i++) {
+            char target = originalWord.charAt(i);
+
+            if (Character.isUpperCase(target)) {
+                continue;
+            }
+
+            char lowerWord = Character.toLowerCase(answer.charAt(i));
+            answer = answer.substring(0, i) + lowerWord + answer.substring(i+ 1);
+        }
+
+        return answer;
     }
 }
