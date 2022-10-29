7f9f4e68158ed3b3b9c9514b81e6582a60822e24
BaeDongHyeon <sasd730@gmail.com>
2022-10-29 23:05:22 +0900

    feat(Problem5.java): 각 화폐로 나누어 개수를 저장하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> moneyCount = new ArrayList<>();
+
+        for (int i = 0; i < moneyList.length; i++) {
+            moneyCount.add(money / moneyList[i]);
+            money %= moneyList[i];
+        }
+
+        return moneyCount;
     }
 }
