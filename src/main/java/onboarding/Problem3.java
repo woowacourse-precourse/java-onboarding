689d8d251f5462c6df8c9e2e232eabd2f1057f47
SeokHwan An <asok369258@naver.com>
2022-10-30 00:16:52 +0900

    feat [Problem3] : 주어진 숫자가 1과 10,000사이의 수인지 검증하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+
+    private static final int NUMBER_MIN = 1;
+    private static final int NUMBER_MAX = 10000;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static boolean checkNumberValid(int number) {
+        return NUMBER_MIN <= number && number <= NUMBER_MAX;
     }
 }
