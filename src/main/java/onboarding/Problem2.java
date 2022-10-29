7a6f22c0f9fe0fc939fb9cff21b7029b0d4166ab
kimdeokyoung <dudejrss@naver.com>
2022-10-29 16:18:26 +0900

    (write) Problem2.decipher : 2.1 주어진 텍스트를 해독한 결과를 출력

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,13 +3,30 @@ package onboarding;
 public class Problem2 {
 
     public static String decipher(String ciphertext){
-        String answer = "answer";
+        String answer = "";
+        int len = ciphertext.length(), count = 0;
 
-        return answer;
+        for (int i =0; i < len; i++ ){
+            if ( i == len-1 ){
+                answer += ciphertext.charAt(i);
+                continue;
+            }
+            if ( ciphertext.charAt(i) == ciphertext.charAt(i+1) ) {
+                i += 1;
+                count++;
+                continue;
+            }
+            answer = answer + ciphertext.charAt(i);
+        }
+
+        if(count == 0)
+            return answer;
+        return decipher(answer);
     }
 
     public static String solution(String cryptogram) {
         String answer = "answer";
+//        System.out.printf("%s %s", cryptogram, decipher(cryptogram));
         return answer;
     }
 }
