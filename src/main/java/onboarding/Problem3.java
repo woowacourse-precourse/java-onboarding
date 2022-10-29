363e5028ffc1d0b10a039de584070212c2663bbf
JJongmen <pjy90123@gmail.com>
2022-10-30 01:34:27 +0900

    refactor(Problem3): 메서드의 이름을 더 명확하도록 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,22 +2,26 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        return countClapsFrom1ToNumber(number);
+    }
+
+    private static int countClapsFrom1ToNumber(int number) {
         int clapCount = 0;
         for (int turn = 1; turn <= number; turn++) {
-            clapCount += count369ByNumber(turn);
+            clapCount += countClaps(turn);
         }
         return clapCount;
     }
 
-    private static int count369ByNumber(int number) {
-        return (int) String.valueOf(number)
+    private static int countClaps(int turn) {
+        return (int) String.valueOf(turn)
                 .chars()
                 .map(Character::getNumericValue)
-                .filter(digit -> is369(digit))
+                .filter(digit -> has369(digit))
                 .count();
     }
 
-    private static boolean is369(int digit) {
+    private static boolean has369(int digit) {
         return digit == 3 || digit == 6 || digit == 9;
     }
 }
