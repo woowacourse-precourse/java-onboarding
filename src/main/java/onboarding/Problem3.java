767a1e44451b1863c614d36590e897e159b85874
HiiWee <wpdlzhf159@gmail.com>
2022-10-30 04:45:39 +0900

    Feat problem3: 1부터 입력받은 숫자를 돌며 탐색

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,9 +4,20 @@ public class Problem3 {
     static final int THREE = 3;
     static final int SIX = 6;
     static final int NINE = 9;
+    static final int TEN = 10;
 
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += getCountOfNumber(THREE, i);
+            answer += getCountOfNumber(SIX, i);
+            answer += getCountOfNumber(NINE, i);
+        }
         return answer;
+    }
+
+    private static int getCountOfNumber(int target, int number) {
+        int count = 0;
+        return count;
     }
 }
