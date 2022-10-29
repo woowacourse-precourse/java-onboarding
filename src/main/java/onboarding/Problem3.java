c2b7f60cdc748c93bf8b772a0d0e8e8b8e3ee0b6
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-29 11:34:36 +0900

    문자열을 숫자 하나 단위로 쪼개서 배열에 넣고 배열안의 3,6,9의 수를 세서 return

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,15 @@ public class Problem3 {
         for(int i=1; i <= number; i++){
             numOrder += i;
         }
-        int answer = 0;
-        return answer;
+        
+        String[] numSet = numOrder.split("");
+        int count = 0;
+        for(int i = 0; i < numSet.length; i++){
+            if(numSet[i].equals("3")||numSet[i].equals("6")||numSet[i].equals("9")){
+                count += 1;
+            }
+        }
+
+        return count;
     }
 }
