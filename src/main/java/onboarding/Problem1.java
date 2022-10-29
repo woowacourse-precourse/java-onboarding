8b4425b70abfa3a396f52aa9cafe116bbdaacd84
이병3 <101378867+SongJSeop@users.noreply.github.com>
2022-10-30 02:18:00 +0900

    Solve Problems

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,48 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (CheckList(pobi) || CheckList(crong)) return -1;
+        else return MaxNum(pobi) > MaxNum(crong) ? 1 : MaxNum(pobi) == MaxNum(crong) ? 0 : 2;
+    }
+
+    static boolean CheckList(List<Integer> list) {
+        if (list.size() != 2 || list.get(1) - list.get(0) != 1 || list.get(0) % 2 != 1 || list.get(0) == 1) return true;
+        else return false;
+    }
+
+    static int MaxNum(List<Integer> list) {
+        int left_num = list.get(0);
+        int right_num = list.get(1);
+        return CalculateNum(left_num) > CalculateNum(right_num) ? CalculateNum(left_num) : CalculateNum(right_num);
+    }
+
+    static int CalculateNum(int page) {
+        return PageSum(page) > PageMultiply(page) ? PageSum(page) : PageMultiply(page);
+    }
+
+    static int PageSum(int page) {
+        int sum = 0;
+
+        while (page > 0) {
+            sum += page % 10;
+            page /= 10;
+        }
+
+        return sum;
+    }
+
+    static int PageMultiply(int page) {
+        int sum = 1;
+
+        while (page > 0) {
+            sum *= page % 10;
+            page /= 10;
+        }
+
+        return sum;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,40 +2,72 @@ package onboarding;
 
 import java.util.*;
 
-public class Problem7t {
+public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> user_friends = new ArrayList<>();
-        Set<String> friend_friends = new HashSet<>();
+        Map<String, Integer> friends_point = new HashMap<>();
+        Set<String> user_friends = new HashSet<>();
+        user_friends = FindFriends(user, user, friends);
 
         for (List<String> list : friends) {
-            for (String friend : friends_list(user, list)) {
-                user_friends.add(friend);
-            }
-        }
-
-        for (String friend : user_friends) {
-            for (List<String> list : friends) {
-                for (String friend_friend : friends_list(friend, list)) {
-                    if (friend_friend == user) continue;
-                    friend_friends.add(friend_friend);
+            if (list.contains(user)) continue;
+            else {
+                for (String friend : list) {
+                    friends_point.put(friend, 0);
                 }
             }
         }
 
-        for (int i = 0; i < friend_friends.size(); i++) {
-
-        }
-    }
-
-    static List<String> friends_list(String user, List<String> friends) {
-        List<String> friends_list = new ArrayList<>();
-
-        for (int i = 0; i < 2; i++) {
-            if (user == friends.get(i)) {
-                friends_list.add(friends.get(1-i));
+        for (String user_friend : user_friends) {
+            friends_point.remove(user_friend);
+            for (String friends_friend : FindFriends(user, user_friend, friends)) {
+                friends_point.put(friends_friend, friends_point.get(friends_friend) + 10);
             }
         }
 
-        return friends_list;
+        for (String visitor : visitors) {
+            if (!(user_friends.contains(visitor))) {
+                if (friends_point.keySet().contains(visitor)) {
+                    friends_point.put(visitor, friends_point.get(visitor) + 1);
+                    continue;
+                }
+                friends_point.put(visitor, 1);
+            }
+        }
+
+        friends_point.values().remove(0);
+
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(friends_point.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o1.getValue() > o2.getValue()) {
+                    return -1;
+                }
+                else if (o1.getValue() < o2.getValue()) {
+                    return 1;
+                }
+
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
+
+        List<String> answer = new ArrayList<>(friends_point.keySet());
+        return answer;
+    }
+
+    static Set<String> FindFriends(String user, String one, List<List<String>> friends) {
+        Set<String> one_friends = new HashSet<>();
+
+        for (List<String> list : friends) {
+            if (list.contains(one)) {
+                for (String friend : list) {
+                    one_friends.add(friend);
+                }
+            }
+        }
+
+        one_friends.remove(user);
+        one_friends.remove(one);
+        return one_friends;
     }
 }
