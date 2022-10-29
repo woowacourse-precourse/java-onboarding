e0a37f914de8a4a0d5e2dc77f97ea44a677c85b6
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-29 17:26:57 +0900

    etc: Problem4 기능 요구사항 정리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * [ ] 알파벳 외의 문자는 변환하지 않는다.
+ * [ ] word를 청개구리 사전을 참고해 반대로 변환한다.
+ * [ ] 알파벳 대문자는 대문자로, 알파벳 소문자는 소문자로 변환한다.
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
