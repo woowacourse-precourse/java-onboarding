f1f50b10ca2cbbaf0a26e8094091619ce6af50df
can019 <jys01012@gmail.com>
2022-10-29 22:06:56 +0900

    :sparkles: Case 2 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,23 +5,15 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
-        System.out.println("--------");
         answer = eliminate(cryptogram,0,Character.MIN_VALUE, false);
 
-        System.out.println(answer);
-        System.out.println("zz".substring(0,0));
         return answer;
     }
 
     private static String eliminate(String cryptogram, int index, char before, boolean founded){
-        System.out.println("!!!!");
-        System.out.println(index);
-        System.out.println(cryptogram);
-        if(index + 1 >= cryptogram.length()){
-            System.out.println("???");
+        if(index + 1 > cryptogram.length()){
             if(founded==true) {
                 return eliminate(cryptogram,0, Character.MIN_VALUE, false);
-
             }
             else return cryptogram;
         }
@@ -29,14 +21,8 @@ public class Problem2 {
             char current = cryptogram.charAt(index);
 
             if(before == current){
-                System.out.println("!!!");
-                System.out.println(index);
                 String part1 = cryptogram.substring(0, index-1);
                 String part2 = cryptogram.substring(index+1);
-                System.out.println("#################");
-                System.out.println(part1);
-                System.out.println(part2);
-                System.out.println("#################");
                 return eliminate(part1+part2, index-1, current, true);
             }
             else
