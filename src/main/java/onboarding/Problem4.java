1884e3d42e3d7c857de9cd6372249babb04930ee
jthugg <jthug@naver.com>
2022-10-30 03:59:18 +0900

    fix. 루프 구조 수정
    루프 구조 수정
    
    필요 없는 판별식 삭제
    
    continue 포인트 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -18,13 +18,15 @@ public class Problem4 {
 
             if(upper) { // 대문자인지 체크
                 sb.append(Z.charAt(A.indexOf(ch)));
+                continue;
             }
+
             if(lower) { // 소문자인지 체크
                 sb.append(z.charAt(a.indexOf(ch)));
+                continue;
             }
-            if(!upper && !lower) { // 대문자 소문자 둘 다 아닌경우, 알파벳이 아님
-                sb.append(ch);
-            }
+
+            sb.append(ch);
         }
 
         return sb.toString();
