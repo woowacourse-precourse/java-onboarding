f6f8994d255c85799ccf0c5794e20344f129c04d
kssumin <201566@jnu.ac.kr>
2022-10-29 23:34:59 +0900

    feat :  페이지 min,max 제한

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,7 +11,15 @@ class Problem1 {
 }
 
 class Page {
+
+    private static final Integer MIN_PAGE = 1;
+    private static final Integer MAX_PAGE = 400;
+    private static final String NOT_ALLOWED_UNDER_MIN_PAGE = "책의 최소 페이지 수는 1입니다.";
+    private static final String NOT_ALLOWED_OVER_MAX_PAGE = "책의 최대 페이지 수는 400입니다.";
+
     public Page(int pageNumber) {
+        validateMinPage();
+        validateMaxPage();
         this.pageNumber = pageNumber;
     }
 
@@ -31,5 +39,17 @@ class Page {
         return Stream.of(String.valueOf(pageNumber).split(""))
                 .mapToInt(Integer::parseInt)
                 .reduce(1, (a, b) -> a * b);
+    }
+
+    private void validateMinPage() {
+        if (pageNumber < MIN_PAGE) {
+            throw new IllegalStateException(NOT_ALLOWED_UNDER_MIN_PAGE);
+        }
+    }
+
+    private void validateMaxPage() {
+        if (pageNumber > MAX_PAGE) {
+            throw new IllegalStateException(NOT_ALLOWED_OVER_MAX_PAGE);
+        }
     }
 }
