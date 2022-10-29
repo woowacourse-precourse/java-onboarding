428b999b13cfcc60f1bacbb34fb949f57d7c49a6
Go-Jaecheol <gojaech@naver.com>
2022-10-29 17:09:29 +0900

    feat: Problem2 - 문자열 처음부터 끝까지 연속되는 문자 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return checkContinuous(cryptogram);
+    }
+
+    private static String checkContinuous(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char c = cryptogram.charAt(i);
+        }
+        return stack.stream().map(Object::toString).collect(Collectors.joining());
     }
 }
