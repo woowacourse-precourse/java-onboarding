c1b76097b852e2d96a4776a7f1d7ab4021141bba
famo1245 <kandallee38@gmail.com>
2022-10-30 03:25:51 +0900

    solving problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, ArrayList<String>> friendsMap = new HashMap<>();
+        for(List<String> list : friends) {
+            String name1 = list.get(0);
+            String name2 = list.get(1);
+            if(!friendsMap.containsKey(name1))
+                friendsMap.put(name1, new ArrayList<>());
+
+            if(!friendsMap.containsKey(name2))
+                friendsMap.put(name2, new ArrayList<>());
+
+            friendsMap.get(name1).add(name2);
+            friendsMap.get(name2).add(name1);
+        }
+
+        ArrayList<String> userFriendsList = friendsMap.get(user);
+        Map<String, Integer> recommendMap = new HashMap<>();
+        for(String name : userFriendsList) {
+
+        }
         List<String> answer = Collections.emptyList();
         return answer;
     }
