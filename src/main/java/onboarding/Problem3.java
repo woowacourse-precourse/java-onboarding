0b1e533ec4d2cd4687184bd1d08656f25a4aea94
Kuyho Chung <kchung1995@gmail.com>
2022-10-29 21:28:52 +0900

    온보딩 - 문제 3 - shouldClap 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+
+    private static boolean shouldClap(int input) {
+        while(input >= 10) {
+            if (input % 10 == 3 || input % 10 == 6 || input % 10 == 9) {
+                return true;
+            }
+            input /= 10;
+        }
+        if (input % 10 == 3 || input % 10 == 6 || input % 10 == 9) {
+            return true;
+        }
+        return false;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
