02930005392350f51dccd6d89cf4cfc171f417ad
math0917 <math0917@naver.com>
2022-10-29 14:55:32 +0900

    test : userInput validation 테스트코드 작성

---
+++ src/main/java/onboarding/problem4/Problem4Validation.java
@@ -0,0 +1,7 @@
+package onboarding.problem4;
+
+public class Problem4Validation {
+    public static boolean problem4InputValidation(String userInput, int minInput, int maxInput) {
+        return false;
+    }
+}
