0770fc64a41e4263efdc27ae0d949af4ae80889d
java-saeng <gusdn85554@naver.com>
2022-10-30 00:29:09 +0900

    feat : 펼친 페이지가 왼쪽, 오른쪽 페이지가 맞는지 isNotLeftAndRightPage 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,29 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static final int DRAW = 0;
+    public static final int POBI_WIN = 1;
+    public static final int CRONG_WIN = 2;
+    public static final int ERROR = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
+        if (isNotLeftAndRightPage(pobiLeftPage, pobiRightPage) ||
+                isNotLeftAndRightPage(crongLeftPage, crongRightPage)) {
+            return ERROR;
+        }
+
         return answer;
+    }
+
+    private static boolean isNotLeftAndRightPage(int leftPage, int rightPage) {
+        return rightPage - leftPage != 1;
     }
 }
