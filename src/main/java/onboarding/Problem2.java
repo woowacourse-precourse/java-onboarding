adff0f85e365434859e5ffbe1b27acd9e22d89a0
siwookim97 <tldn12gh@gmail.com>
2022-10-30 05:06:13 +0900

    feat : problem2) 문자열을 리스트로 변환하는 기능 추가
    문자열을 Character형 List로 변환하는 기능
    문자열을 입력받아 문자열의 앞부터 순서대로 List의 앞부터
    추가한 후 반환한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+    static List<Character> list = new ArrayList<>();
+
+    /**
+     * 문자열을 Character형 List로 변환하는 메서드
+     *
+     * @param cryptogram 입력받는 문자열
+     * @return List로 바꾼 문자열
+     */
+    static List<Character> transStringToList(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            list.add(cryptogram.charAt(i));
+        }
+
+        return list;
+    }
+
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
