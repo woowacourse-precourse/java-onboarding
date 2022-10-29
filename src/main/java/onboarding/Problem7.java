40a095037202e63ec3be77afa674acc0240cc838
Hyeonjae-K <href.hjk@gmail.com>
2022-10-29 21:07:35 +0900

    add: 기능 목록 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/* 기능 목록
+ * user의 친구 목록 반환 기능 구현
+ * user의 친구의 친구 관계 반환 기능 구현
+ * user의 친구의 친구와 방문자의 친구 추천 규칙에 따라 점수 부여
+ * 점수 순으로 정렬 기능 구현
+ * */
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
