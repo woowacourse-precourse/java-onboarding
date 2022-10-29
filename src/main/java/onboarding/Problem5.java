f1374fb6479dba7bfdd939d201cf4437193da2e2
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 08:42:55 +0900

    feat : Problem5 제한 사항 확인 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,18 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        try {
+            isValidMoney(money);
+        } catch (IllegalStateException moneyIsNotValidException) {
+            moneyIsNotValidException.printStackTrace();
+        }
+
         return answer;
+    }
+
+    public static void isValidMoney(int money) throws IllegalStateException {
+        if (money < 1 || money > 1000000)
+            throw new IllegalStateException("money의 범위가 올바르지 않습니다.");
     }
 }
