2c60689e42ba6be645ea255b349d27700c0d13d2
Dabeen Jeong <hello70825@gmail.com>
2022-10-29 13:13:48 +0900

    feat: [Problem5] 올바른 입력값이 들어왔는지 확인하는 기능을 추가하다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,19 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        if (checkValidation(money)) {
+
+        }
+
         return answer;
+    }
+
+    private static boolean checkValidation(int inputValue) {
+        // 1 이상 1,000,000 이하의 자연수인지 확인하다.
+        if (!(1 <= inputValue && inputValue <= 1_000_000)) {
+            return false;
+        }
+
+        return true;
     }
 }
