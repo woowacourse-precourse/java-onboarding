877cbc78275ff40565032b4960cd43e667f9a622
chordpli <chordplaylist@gmail.com>
2022-10-30 01:26:58 +0900

    feat(problem7) : 메서드 구상 1. 유저와 친구인 사람 Set 초기화 2. 함께 아는 친구 점수 추가 3. visitors 점수 추가 4. result 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -14,6 +14,13 @@ import java.util.Map;
 *
 * [A,B] A와 B는 친구이며, 중복해서 주어지지 않고, 추천할 친구가 없는 경우 주어지지 않는다.
 *
+* user의 친구는 return하지 않아도 됨.
+*
+* 방법 1
+* 1. 유저와 친구인 사람을 Set을 사용하여 추린다.
+* 2. 추린 사람들과 친구인 사람들을 해시맵으로 생성 후 10점씩 추가.
+* 3. 방문한 친구들 점수 추가.
+*
 */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -21,12 +28,20 @@ public class Problem7 {
         return answer;
     }
 
-    // HashMap으로 name, value로 초기화한다.
+    // 유저랑 친구인 사람들은 Set으로 초기화
+
+
+    // Set과 비교하여 유저랑 친구가 아닌 사람들은 HashMap으로 name, value로 초기화한다.
     public Map<String, Integer> initScore(List<List<String>> friends) {
         Map<String, Integer> initScore = new HashMap<>();
         return initScore;
     }
 
+    // 함께 아는 친구 점수 추가
+
+    // visitors 점수 추가
+
+    // result 반환
 
 
 }
