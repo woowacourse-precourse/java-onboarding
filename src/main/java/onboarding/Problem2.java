87373aa564b3137a7940aeb8b61d6ea5e22381fb
5onchangwoo <scw9410@gmail.com>
2022-10-30 00:34:24 +0900

    problem2/암호문 배열로 변환 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
+    // 암호문 배열로 분리
+    private static List<String> splitCryptogram(String cryptogram) {
+        return Arrays.stream(cryptogram.split("")).collect(Collectors.toList());
+    }
+
+
 }
