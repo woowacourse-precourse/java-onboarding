87e38a57671f7e2bcaee262123525b6957353fe3
Luka <meena2003@naver.com>
2022-10-29 18:38:12 +0900

    kindOfMoney 배열 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static void main(String[] args) {
+
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        // 1. 각 화폐의 개수를 담을 리스트 생성
+        // 2. 높은 금액의 화폐부터 몇 장이 필요한지 세서 int를 반환하는 메서드 생성
+        // 3. for 반복문을 만들어서 위 메서드의 반환값을 int[] 배열에 삽입
+        // 4. 결과값 반환
+
+        int[] kindOfMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        List<Integer> answer = new ArrayList<>();
         return answer;
     }
 }
