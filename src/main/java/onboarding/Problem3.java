5780bae92531cde53f3dabcd264db78919395080
hyejungg <kimhyejung12@naver.com>
2022-10-30 01:10:12 +0900

    feat: [문제 3] 박수 세는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,4 +12,16 @@ public class Problem3 {
 		return number >= 1 && number <= 1000;
 	}
 
+	private static int countClap(int number) {
+		int result = 0;
+
+		int[] numberList = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+
+		for (int n : numberList) {
+			if (n == 3 || n == 6 || n == 9) {
+				result++;
+			}
+		}
+		return result;
+	}
 }
