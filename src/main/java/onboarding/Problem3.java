b93f6a53f13f9344057dbbce3688ec4c72ea2042
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-29 18:27:52 +0900

    Feat(Problem3): 숫자 문자열 변환기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+
+
         return answer;
     }
+
+    static String makeString(int number) {
+        return Integer.toString(number);
+    }
+
 }
