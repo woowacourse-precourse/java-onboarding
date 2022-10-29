788e3eed3b9860aed242ecfca8f84a0900d02cef
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-30 01:51:38 +0900

    문자열을 변환하는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,8 +3,20 @@ package onboarding;
 public class Problem4 {
     // TODO 문자열을 변환하는 메서드
     public static String convert(String str) {
-        return null;
+        String upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerStr = upperStr.toLowerCase();
+        String result = "";
+
+        for (char chr : str.toCharArray()) {
+            if (Character.isUpperCase(chr)) {
+                result += upperStr.charAt(25 - upperStr.indexOf(chr));
+            } else if (Character.isLowerCase(chr)) {
+                result += lowerStr.charAt(25 - lowerStr.indexOf(chr));
+            }
+        }
+        return result;
     }
+
 
     public static String solution(String word) {
         String answer = "";
