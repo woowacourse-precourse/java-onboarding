b2bf37ca64f4af17314c1c906d9f6ef7e1ce84d8
KSH <tkdgns543@gmail.com>
2022-10-29 19:29:21 +0900

    refactor(Problem6): 1보다 긴 닉네임 제거 메서드의 반환타입과 상호 문자열 공유 체크 메서드의 매개변수 타입을 2차원 문자열 배열로 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,24 +11,25 @@ public class Problem6 {
     }
 
     /* 닉네임의 길이가 1보다 큰 크루들만 담긴 리스트 */
-    private static List<List<String>> seperateLengthOneNickname(List<List<String>> forms) {
-        List<List<String>> result = new ArrayList<>();
+    private static String[][] seperateLengthOneNickname(List<List<String>> forms) {
+        List<String[]> result = new ArrayList<>();
         for (List<String> crew : forms) {
             String nickname = crew.get(1);
             if (nickname.length() > 1) {
-                result.add(crew);
+                result.add(crew.toArray(new String[2]));
             }
         }
-
-        return result;
+        int resultSize = result.size();
+        return result.toArray(new String[resultSize][2]);
     }
 
     /* 상호간 공유하는 2 이상의 문자열이 있는지 체크하는 메서드 */
-    private static Set<String> checkMutualContaning(List<List<String>> forms) {
-        for (List<String> crew : forms) {
-            String nickname = crew.get(1);
-            for (int i = 0; i < nickname.length() - 1; i++) {
-                String cutString = nickname.substring(i, i + 2);
+    private static Set<String> checkMutualContaning(String[][] forms) {
+        for (int i = 0; i < forms.length; i++) {
+            String nickname = forms[i][1];
+
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                String cutString = nickname.substring(j, j + 2);
             }
         }
     }
