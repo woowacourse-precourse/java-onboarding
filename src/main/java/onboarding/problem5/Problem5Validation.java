88f221e043cc2ed7aeae0462a5f464fc073b5d3a
math0917 <math0917@naver.com>
2022-10-29 15:45:01 +0900

    feat : userInput validation 코드 작성

--- src/main/java/onboarding/problem5/Problem5Validation.java
+++ src/main/java/onboarding/problem5/Problem5Validation.java
@@ -2,6 +2,10 @@ package onboarding.problem5;
 
 public class Problem5Validation {
     public static boolean problem5InputValidation(int userInput, int minInput, int maxInput) {
-        return true;
+        if (minInput <= userInput && userInput <= maxInput) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
