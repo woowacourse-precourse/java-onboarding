a7a13dff19477dbb5c32e8a892cf80cb729c3160
jamangstangs <jamangsong@gmail.com>
2022-10-30 00:28:48 +0900

    FEAT: setScore(), 각 페이지의 덧셈, 곱셈 결과를 구해 최대 값을 User객체의 score에 저장하는 메소드

--- src/main/java/onboarding/problem1/Service.java
+++ src/main/java/onboarding/problem1/Service.java
@@ -2,6 +2,15 @@ package onboarding.problem1;
 
 public class Service {
 
+    public void setScore(User user) {
+        Integer leftPage = user.getLeftPage();
+        Integer rightPage = user.getRightPage();
+
+        Integer maxSumPage = sumPage(leftPage) > sumPage(rightPage) ? sumPage(leftPage) : sumPage(rightPage);
+        Integer maxMulPage = mulPage(leftPage) > mulPage(rightPage) ? mulPage(leftPage) : mulPage(rightPage);
+
+        user.setScore(maxSumPage > maxMulPage ? maxSumPage : maxMulPage);
+    }
 
     public Integer sumPage(Integer page) {
         Integer result = 0;
