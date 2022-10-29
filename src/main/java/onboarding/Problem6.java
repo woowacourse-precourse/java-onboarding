146b20116c1ced3df8a340dc77767a56d2192918
JuHyeokParkk <hyeok5596@naver.com>
2022-10-29 19:00:02 +0900

    feat(Problem6): 닉네임의 길이 2 이상의 모든 부분 문자열을 저장하는 메소드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
+
+    static HashMap<String, Set<String>> emailAndNicknameMap;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static void addNicknameSubstring(int index, String email, String nickname, String substring) {
+
+        if (substring.length() >= 2) {
+            Set<String> set = emailAndNicknameMap.getOrDefault(substring, new HashSet<>());
+            set.add(email);
+            emailAndNicknameMap.put(substring, set);
+        }
+
+        if(index >= nickname.length())
+            return;
+
+        addNicknameSubstring(index + 1, email, nickname, substring + nickname.charAt(index));
+
+
     }
 }
