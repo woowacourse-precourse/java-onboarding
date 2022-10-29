e7c1415c054126bb0c59268f76e59b2395452619
yg0585 <yg0585@naver.com>
2022-10-29 17:42:39 +0900

    1번 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,8 +3,12 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+//-----기능-----
+//1. 지폐 배열 만들기
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        int[] bank = {10000, 50000, 5000, 1000, 500, 100, 50, 10};
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
