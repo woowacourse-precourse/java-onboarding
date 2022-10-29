5136f3d5052d85149719f20fe74d8d35c90fa4ee
hyowonhan <wain1719@gmail.com>
2022-10-29 18:42:13 +0900

    문제4 solution 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i<word.length(); i++) {
+            if ((int)word.charAt(i)>96) {
+                answer += (char)(219-(int)word.charAt(i));
+            } else if ((int)word.charAt(i)>64) {
+                answer += (char)(155-(int)word.charAt(i));
+            } else {
+                answer += " ";
+            }
+        }
+
         return answer;
     }
     public static void main(String[] args) {
