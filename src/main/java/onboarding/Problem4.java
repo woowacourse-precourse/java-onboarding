4313ee0df527bd79737c117cacc780bd78762d4e
김현재 <41482946+galug@users.noreply.github.com>
2022-10-29 13:03:52 +0900

    feat(problem4): Add 제한 사항에 맞는 인자가 들어왔는지 검증하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem4 {
     public static String solution(String word) {
+        if (!validateWord(word)) {
+            return null;
+        }
         String answer = "";
         return answer;
+    }
+
+    public static boolean validateWord(String word) {
+        if (word.length() < 1 || word.length() > 1000) {
+            return false;
+        }
+        return true;
     }
 }
