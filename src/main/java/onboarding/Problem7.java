d6c6585dee7e7d7caf16e8e5e624de511d27c5a3
skydreamer21 <skydreamer210@gmail.com>
2022-10-29 18:58:04 +0900

    [Feature] Problem7 - Relations 클래스 프로퍼티 설정 및 constructor 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -38,12 +38,19 @@
 
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+}
+
+class Relations {
+    private Map<String, Set<String>> relations;
+
+    public Relations() {
+        this.relations = new HashMap<>();
     }
 }
