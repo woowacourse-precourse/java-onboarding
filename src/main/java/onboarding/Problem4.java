f16d9a3c63fa3f5d95278cbd63ae7835698b8e5c
Cho SangHyun <98shcho@naver.com>
2022-10-29 19:47:54 +0900

    feat: add feature to tour input string

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i=0; i<word.length(); i++) {
+            char currentCh = word.charAt(i);
+        }
+
         return answer;
     }
 }
@@ -16,7 +21,7 @@ public class Problem4 {
 
 /*
 <<기능 목록>>
-- [] 문자열의 각 문자를 순회한다
+- [V] 문자열의 각 문자를 순회한다
 - [] 각 문자가 알파벳인지 판별한다
 - [] 알파벳이 아니면 그대로 answer에 더한다
 - [] 알파벳이면 소문자인지 대문자인지 판별한다
