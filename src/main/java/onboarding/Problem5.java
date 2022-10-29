2bcf177f76e8492c6681ef8a3ea785477eb9973e
Luka <meena2003@naver.com>
2022-10-29 18:57:13 +0900

    main() 메서드 삭제. 주석 삭제

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,16 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static void main(String[] args) {
-        System.out.println(solution(9050));
-    }
-
     public static List<Integer> solution(int money) {
-        // 1. 각 화폐의 개수를 담을 리스트 생성
-        // 2. 높은 금액의 화폐부터 몇 장이 필요한지 세서 int를 반환하는 메서드 생성
-        // 3. for 반복문을 만들어서 위 메서드의 반환값을 int[] 배열에 삽입
-        // 4. 결과값 반환
-
         List<Integer> answer = countHowManyChange(money);
 
         return answer;
