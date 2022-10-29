b79671629b2184987bf2465530a2611efe0920d2
recicoco068 <gkapf838@gmail.com>
2022-10-29 18:34:50 +0900

    feat : 두 닉네임이 중복인지 판단하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,13 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static boolean compareNicknames(char[] nick1, char[] nick2) {
+        for (int i = 1; i < nick1.length; i++) {
+            for (int j = 1; j < nick2.length; j++)
+                if (nick1[i - 1] == nick2[j - 1] && nick1[i] == nick2[j])
+                    return true;
+        }
+        return false;
+    }
 }
