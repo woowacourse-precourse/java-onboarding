a62c7bee437f16f074669e7b35e72e336339f397
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-30 07:43:20 +0900

    [Problem1] fix: line87, multiple 1로 초기화. line 115, 두 페이지가 연속하는 경우 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class Problem1 {
-    /*
+    /**
     두 게임 참여자의 pages를 List<Integer>로 받아 승자를 return하는 메서드
     - 예외 사항
         1. pages List의 요소 수는 2개
@@ -17,7 +17,7 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         // 예외 사항 체크. 예외 발생 시 return -1
-        if (isVaild(pobi) == false || isVaild(crong) == false) {
+        if (isValid(pobi) == false || isValid(crong) == false) {
             answer = -1;
             return answer;
         }
@@ -29,7 +29,7 @@ class Problem1 {
         return answer;
     }
 
-    /*
+    /**
     두 게임 참여자의 점수를 비교하는 메서드
      */
     private static int compareScore(int score1, int score2) {
@@ -54,23 +54,18 @@ class Problem1 {
         int score = 0;
         int leftPage = pages.get(0);
         int rightPage = pages.get(1);
-
         // 각 페이지 숫자의 자리 합과 곱(4개)을 저장할 배열 scores 선언
         List<Integer> scores = new ArrayList<Integer>();
-
         // pageList 순회하며 배열 scores에 요소의 합과 곱을 저장. List<Integer> pageNumToList(int page)
         List<Integer> leftPageNum = pageNumToList(leftPage);
         List<Integer> rightPageNum = pageNumToList(rightPage);
-
         // pageNum(페이지의 자릿수들)의 합과 곱을 계산하는 메서드를 사용해 return값을 scores에 추가
         scores.add(sumOfPageNum(leftPageNum));
         scores.add(multipleOfPageNum(leftPageNum));
         scores.add(sumOfPageNum(rightPageNum));
         scores.add(multipleOfPageNum(rightPageNum));
-
         // 배열 scores 요소 중 가장 큰 값을 score에 저장
         score = Collections.max(scores);
-
         return score;
     }
 
@@ -89,9 +84,9 @@ class Problem1 {
     페이지 자릿수들의 곱을 계산하는 메서드
      */
     private static int multipleOfPageNum(List<Integer> pageNum) {
-        int multiple = 0;
+        int multiple = 1;
         for (int num : pageNum) {
-            multiple += num;
+            multiple *= num;
         }
         return multiple;
     }
@@ -102,7 +97,7 @@ class Problem1 {
      */
     private static List<Integer> pageNumToList(int page) {
         List<Integer> pageNum = new ArrayList<Integer>();
-        while(page > 0) {
+        while(page > 0) {   // page의 범위가 3~398이므로 처음부터 0이지 않음
             pageNum.add(page % 10);
             page = (int)(page / 10);
         }
@@ -113,10 +108,10 @@ class Problem1 {
     매개변수로 받은 List<Integer>가 유효한지 체크하는 메서드
     유효할 시 true, 유효하지 않을 시 false
      */
-    private static boolean isVaild(List<Integer> pages) {
+    private static boolean isValid(List<Integer> pages) {
         int leftPage = pages.get(0);
         int rightPage = pages.get(1);
             // 왼쪽 페이지가 홀수이고, 오른페이지가 짝수이고, 페이지 수가 2이고, 첫 장(1, 2)이나 마지막 장(399, 400)이 아니면 true
-        return leftPage % 2 == 1 && rightPage % 2 == 0 && pages.size() == 2 && leftPage != 1 && rightPage != 400;
+        return leftPage % 2 == 1 && rightPage % 2 == 0 && pages.size() == 2 && leftPage != 1 && rightPage != 400 && leftPage - rightPage != 1;
     }
 }
