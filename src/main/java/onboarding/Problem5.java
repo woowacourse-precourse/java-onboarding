2412d60537e1c6493dc04235def47a2491dd734c
hectick <chyeon1223@cau.ac.kr>
2022-10-30 00:31:43 +0900

    refactor : [Problem5] 주석 삭제

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,12 +9,10 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<Integer>();
         List<Integer> unit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
-        //제일 큰 금액의 화폐부터 필요한 개수를 차례대로 계산
-        //큰 금액의 화폐를 계산하고 남은 돈으로 그 다음 큰 금액의 화폐가 필요한 개수를 계산하기를 반복
         for(int i = 0; i < unit.size(); i++){
             int count = countNumberOfBills(unit.get(i), money);
             answer.add(count);
-            money -= unit.get(i)*count; //잔돈 계산
+            money -= unit.get(i)*count;
         }
 
         return answer;
