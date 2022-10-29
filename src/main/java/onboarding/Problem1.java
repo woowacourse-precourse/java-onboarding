ea08928908ad5aa015e23cd46785b125930f484f
Invidam <hansupark0208@gmail.com>
2022-10-29 13:15:28 +0900

    refactor(Problem1): extract method get digit Array logic
    extract Logic that get digit list.
    (getPageMaxPoint -> getDigits)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,12 +8,7 @@ import java.util.List;
 class Problem1 {
     public static int getPageMaxPoint(int page) {
 
-        List<Integer> digits = new ArrayList<>();
-
-        while(page > 0) {
-            digits.add(page % 10);
-            page /= 10;
-        }
+        List<Integer> digits = getDigits(page);
 
         int cand1 = 0;
         for(int digit: digits)
@@ -24,6 +19,17 @@ class Problem1 {
             cand2 *= digit;
 
         return max(cand1,cand2);
+    }
+
+    private static List<Integer> getDigits(int page) {
+        List<Integer> digits = new ArrayList<>();
+
+        while(page > 0) {
+            digits.add(page % 10);
+            page /= 10;
+        }
+
+        return digits;
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
