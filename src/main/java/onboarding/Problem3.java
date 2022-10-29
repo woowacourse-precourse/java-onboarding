2b36769577b0163ebbffb3ed251adf4c55aa0745
olrlobt <317tmdgjs@naver.com>
2022-10-29 19:16:00 +0900

    feat: Problem3 [1]369게임 진행

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+/**
+ * 기능목록
+ * 1. 369게임 진행
+ * 2. 손뼉 확인
+ * 3. 손뼉! 짝!
+ * */
 public class Problem3 {
+    private static int crap;
     public static int solution(int number) {
         int answer = 0;
+
+        playGame(number);
         return answer;
+    }
+
+    static void playGame(int number){
+        crap=0;
+        for(int i=1; i<=number; i++){
+
+        }
     }
 }
