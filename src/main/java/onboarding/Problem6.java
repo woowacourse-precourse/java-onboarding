b731d72469560b3a647b3f8ec27a33d8913b95f2
seonghoo1217 <seonghoo1217@naver.com>
2022-10-29 18:19:37 +0900

    feat : Add a Email Type and Email Length Check Validation Exception on [Task6]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,6 +9,12 @@ public class Problem6 {
         List<String> answer = new ArrayList<>();
         try {
             checkCrewLimit(forms.size());
+
+            for (int i=0;i<forms.size();i++){
+                String email = forms.get(i).get(0);
+                validationEmail(email);
+            }
+
             return answer;
         }catch (Exception e){
             return answer;
@@ -17,6 +23,12 @@ public class Problem6 {
 
     public static void checkCrewLimit(int crew_count) throws Exception {
         if (crew_count<1||crew_count>10000) throw new Exception("인원 제한 초과");
+    }
+
+    public static void validationEmail(String email) throws Exception {
+        int e_length = email.length();
+        if (e_length<11||e_length>=20) throw new Exception("Eamil Length must Be 11~19");
+        if (!email.matches("^[a-zA-Z0-9]+@email.com$")) throw new Exception("It's Wrong Format Email");
     }
 
 }
