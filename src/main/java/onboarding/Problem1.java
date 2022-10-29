68158a7c08d5f66a8304d3578dfee4590834a5c0
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 19:39:58 +0900

    :sparkles: [문제 1] : 2. pobi의 점수 도출

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
@@ -12,7 +13,35 @@ class Problem1 {
         if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
             return -1;
         }
+
+        int[] score = new int[4];
+        score[0] = convertNumberPlus(pobi.get(0));
+        score[1] = convertNumberMultiple(pobi.get(0));
+        score[2] = convertNumberPlus(pobi.get(1));
+        score[3] = convertNumberMultiple(pobi.get(1));
+
+        Arrays.sort(score);
+
+        int pobiScore = score[3];
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+    static int convertNumberPlus(int num) {
+        String page = Integer.toString(num);
+        int pagePlus = 0;
+        for (int i = 0; i < page.length(); i++) {
+            pagePlus += Integer.parseInt(String.valueOf(page.charAt(i)));
+        }
+        return pagePlus;
+    }
+
+    static int convertNumberMultiple(int num) {
+        String page = Integer.toString(num);
+        int pageMultiple = 1;
+        for (int i = 0; i < page.length(); i++) {
+            pageMultiple *= Integer.parseInt(String.valueOf(page.charAt(i)));
+        }
+        return pageMultiple;
     }
 }
