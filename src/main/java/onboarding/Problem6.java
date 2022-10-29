c9f905b7155a21e2e5ae756dd84efaa91b69ee22
slbin-park <smpts00@naver.com>
2022-10-30 00:43:20 +0900

    docs : PROBLEM6 기능 목록 추가 - 중복된 문자열이 있는 이메일 배열을 가져오는 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,22 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashMap<String,Set<String>> words = new HashMap<>();
+
+        for(int i=0;i<forms.size();i++){
+            List<String> word = getWord(forms.get(i).get(1));
+            word.stream().map((w) -> words.put(w,))
+        }
         return answer;
     }
-    public static List<String> getWord(String word,int index){
+    public static List<String> getWord(String word){
         List<String> words = new ArrayList<>();
         for(int i=0;i<word.length()-1;i++){
             words.add(word.substring(i,i+2));
