8ba1ba7b8ffe19c940aeb982388393ef60855507
김동규 <2016025196@hanyang.ac.kr>
2022-10-29 14:40:17 +0900

    1주차 과제 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer=cryptogram;
+        String check_list="([a-z])\\1+";
+
+        while(true) {
+
+            int before_len=answer.length();
+            answer=answer.replaceAll(check_list,"");
+
+            if(before_len==answer.length())
+                break;
+
+        }
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer="";
+        char convert_str[]=word.toCharArray();
+        int dist=0;
+        for(char c : convert_str){
+            if(Character.isLowerCase(c)) {
+                dist = 'z' - c;
+                answer += (char)('a' + dist);
+            }
+            else if(Character.isUpperCase(c)) {
+                dist = 'Z' - c;
+                answer += (char)('A' + dist);
+            }
+            else
+                answer += c;
+
+        }
+
+
+
+
         return answer;
     }
 }
