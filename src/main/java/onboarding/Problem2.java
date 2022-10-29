cfb741ff991e12f1ab590bad34a05aebfcc36794
kimdoha <kkhhjj3393@gmail.com>
2022-10-29 20:42:49 +0900

    feat: 연속하는 중복 문자 제거 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,28 @@ package onboarding;
 
 public class Problem2 {
 
+    private static String removeDuplicateCharacters(String str) {
+        char[] chars = str.toCharArray();
+        char previousChar = chars[0];
+
+        StringBuilder results = new StringBuilder();
+        results.append(previousChar);
+
+        for(int i = 1; i < chars.length; i++) {
+            char currentChar = chars[i];
+            if(previousChar != currentChar) {
+
+                results.append(currentChar);
+                previousChar = currentChar;
+            } else {
+
+                results.deleteCharAt(results.length() - 1);
+            }
+        }
+
+        return results.toString();
+    }
+
     private static boolean checkDuplicateCharacters(String str) {
 
         char[] chars = str.toCharArray();
@@ -16,4 +38,5 @@ public class Problem2 {
 
         return false;
     }
+
 }
