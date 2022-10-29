38d7dd74ab0257aeffd7b71ef5d4573ac38dd5e0
Luka <meena2003@naver.com>
2022-10-29 15:46:49 +0900

    기능 요구 사항 주석 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,10 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        // 1. 3 6 9 숫자가 몇개 존재하는지 담을 변수 생성
+        // 2. 숫자를 하나씩 세면서 해당 숫자내에 3 6 9 가 몇개 있는지 확인하는 메서드 생성
+        // 3. 정답 반환
+
         int answer = 0;
         return answer;
     }
