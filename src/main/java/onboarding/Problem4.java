ee4ca330e4df0a0a87fde8e8c3123bff03132191
June <68291395+june-777@users.noreply.github.com>
2022-10-29 20:41:21 +0900

    feat: 알파벳전환 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+    	
+    	String answer = "";
         return answer;
+    }
+    public static char convertCharReverse(char target) {
+    	char convertedChar;
+    	if(Character.isUpperCase(target)) {
+    		convertedChar = (char)(90-target+65);
+    	} else {
+    		convertedChar = (char)(122-target+97);
+    	}
+    	return convertedChar;
     }
 }
