bb5f66adb744e8d2488446cd9663a17659b450f9
yun <yshyeon102@gmail.com>
2022-10-29 23:01:18 +0900

    feat: Problem1 - solution 함수 정의
    문제1번을 해결하기 위한 solution 함수를 정의.
    
    #TODO : solution 메서드에 사용된 함수 구현
    - 예외 처리
    - 점수 계산
    - 승자 결정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,17 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        try {
+            checkException(pobi);
+            checkException(crong);
+
+            int pobiScore = calMaxScore(pobi);
+            int crongScore = calMaxScore(crong);
+
+            return findWinner(pobiScore, crongScore);
+
+        } catch (RuntimeException e) {
+            return -1;
+        }
     }
 }
