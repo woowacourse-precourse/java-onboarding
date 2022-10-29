1d8816d80587759fad2764b7ca6a4c51a95b35c7
Songyi Kim <ksl2950@naver.com>
2022-10-30 02:43:42 +0900

    feat/1: 예외 상황 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        if (isInvalidPage(pobi.get(0), pobi.get(1)) || isInvalidPage(crong.get(0), crong.get(1))) {
+            return -1;
+        }
+
+
+        return 0;
+
+    }
+
+
+
+    public static boolean isInvalidPage(int left, int right) {
+        return (Math.abs(left - right) != 1) || (left % 2 != 1) || (right % 2 != 0);
     }
 }
