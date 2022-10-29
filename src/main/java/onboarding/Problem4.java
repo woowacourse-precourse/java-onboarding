0e31001937f4bd8cd66c3e9decb582c9c0bf29b0
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-29 23:39:51 +0900

    Refactor: Problem4 코드 리팩토링 - 매소드 분리 - 가독성 개선

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,15 +3,18 @@ package onboarding;
 public class Problem4 {
 	public static String solution(String word) {
 		StringBuilder answer = new StringBuilder();
-		for (int i = 0; i < word.length(); i++) {
-			if ('A' <= word.charAt(i) && word.charAt(i) <= 'Z') {
-				answer.append((char)('Z' - word.charAt(i) + 'A'));
-			} else if ('a' <= word.charAt(i) && word.charAt(i) <= 'z') {
-				answer.append((char)('z' - word.charAt(i) + 'a'));
-			} else {
-				answer.append(word.charAt(i));
-			}
+		for (char nowLetter : word.toCharArray()) {
+			answer.append(reverseChar(nowLetter));
 		}
 		return answer.toString();
+	}
+
+	private static char reverseChar(char givenChar) {
+		if ('A' <= givenChar && givenChar <= 'Z') {
+			return (char)('Z' - givenChar + 'A');
+		} else if ('a' <= givenChar && givenChar <= 'z') {
+			return (char)('z' - givenChar + 'a');
+		}
+		return givenChar;
 	}
 }
