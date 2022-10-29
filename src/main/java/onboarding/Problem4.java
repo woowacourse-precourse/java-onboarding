42a25d8bc5a9d667e0dbe91407a867130154a4e7
himjjong <jjijji5728@tukorea.ac.kr>
2022-10-29 19:48:07 +0900

    feat : 주어진 word를 문자열 배열로 쪼개는 splitString 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem4 {
+    static int flag = 0;
     public static String solution(String word) {
         String answer = "";
+        answer = splitString(word);
         return answer;
+    }
+    static String splitString(String word){
+        String result = "";
+        String[] wordarr = word.split("");
+        for(int i=0; i<wordarr.length; i++){
+            if(checkAlpha(wordarr[i])) {
+                result += changeAlpha(wordarr[i]);
+                continue;
+            }
+            result += wordarr[i];
+        }
+        return result;
+    }
+    static boolean checkAlpha(String oneword){
+        return false;
+    }
+    static char changeAlpha(String oneword){
+        char reverseword = 0;
+        return reverseword;
     }
 }
