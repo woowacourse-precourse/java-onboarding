c678e04d1c3972f201097355704e7894c3cb0834
BaeDongHyeon <sasd730@gmail.com>
2022-10-29 23:09:05 +0900

    refactor(Problem5.java): moneyList를 상수로 변환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] MONEY_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> moneyCount = new ArrayList<>();
 
-        for (int i = 0; i < moneyList.length; i++) {
-            moneyCount.add(money / moneyList[i]);
-            money %= moneyList[i];
+        for (int i = 0; i < MONEY_LIST.length; i++) {
+            moneyCount.add(money / MONEY_LIST[i]);
+            money %= MONEY_LIST[i];
         }
 
         return moneyCount;
