c1e0eb44c295455e2309f3ed92ab7f9fcbdde14d
swandevson <shai4956@gmail.com>
2022-10-30 00:58:18 +0900

    feat: P7 user의 친구 이름 반환
    - 친구 이름의 중복은 허용하지 않으므로 set 자료구조 사용
    - user와 친구인 사용자의 이름 set 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,17 +6,39 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
-    public static boolean isUser(String user, List<String> userList) {
-        if(user.equals(userList.get(0)) || user.equals(userList.get(1))) {
+    public static boolean isUser(String user, List<String> friend) {
+        if(user.equals(friend.get(0)) || user.equals(friend.get(1))) {
             return true;
         }
         return false;
     }
+    public static String getFriendName(String user, List<String> friend) {
+        if(user.equals(friend.get(0))) {
+            return friend.get(1);
+        }
+        else {
+            return friend.get(0);
+        }
+    }
+    public static HashSet<String> getFriendSet(String user, List<List<String>> friends){
+        HashSet<String> friendSet = new HashSet<>();
+
+        for(List<String> friend: friends) {
+            if(isUser(user, friend)) {  // user가 있는 친구 목록일 경우
+                String friendName = getFriendName(user, friend);
+                friendSet.add(friendName);
+            }
+        }
+
+        return friendSet;
+    }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        HashSet<String> userFriends;
+        HashSet<String> friendSet;
 
+
+        friendSet = getFriendSet(user, friends);
 
         return answer;
     }
