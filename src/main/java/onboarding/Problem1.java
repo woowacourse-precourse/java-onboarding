b6f4593406082afc79d81da0323e2f6041f7b17a
CordHouse <qkqktl5310@gmail.com>
2022-10-30 00:29:50 +0900

    refactor : Add(sum, product) function / Problem1 최고 값 찾기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,6 +17,10 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiSum, pobiProduct;
+        int crongSum, crongProduct;
+        int pobiMax = 0;
+        int crongMax = 0;
 
         // Step 1. 예외 케이스 처리
         if(pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1
@@ -24,6 +28,43 @@ class Problem1 {
             return -1;
         }
 
+        // Step 2. 최고 값 찾기
+        for(int i = 0; i < pobi.size(); i++){
+            // sum과 product 함수를 통해 최대 값 찾기
+            pobiSum = sum(pobi.get(i));
+            crongSum = sum(crong.get(i));
+            pobiProduct = product(pobi.get(i));
+            crongProduct = product(crong.get(i));
+
+            // Math.max() 함수를 이용하여, 계산 결과에서 가장 큰 값을 선정
+            pobiMax = Math.max(pobiMax, Math.max(pobiSum, pobiProduct));
+            crongMax = Math.max(crongMax, Math.max(crongSum, crongProduct));
+        }
+
         return answer;
+    }
+
+    // input -> 주어진 배열의 값 하나를 받아오는 파라미터
+    // tmp라는 char형 배열을 선언해 input 값을 char형 배열로 형변환 하여 대입
+    // for-each를 통해 값 누적
+    public static int sum(int input){
+        char[] tmp = String.valueOf(input).toCharArray();
+        int sum = 0;
+        for(char value : tmp){
+            sum += Integer.parseInt(String.valueOf(value));
+        }
+        return sum;
+    }
+
+    // input -> 주어진 배열의 값 하나를 받아오는 파라미터
+    // tmp라는 char형 배열을 선언해 input 값을 char형 배열로 형변환 하여 대입
+    // for-each를 통해 값 곱해서 누적
+    public static int product(int input){
+        char[] tmp = String.valueOf(input).toCharArray();
+        int product = 1;
+        for(char value : tmp){
+            product *= Integer.parseInt(String.valueOf(value));
+        }
+        return product;
     }
 }
