d97ecba74747a3f10c4e853824d943ca7e84b5a2
Heesom <heesom.hs@gmail.com>
2022-10-29 21:06:38 +0900

    Problem4 : reverseFrog 메소드의 에러 수정
    HashMap의 키에 올바른 값이 오지 않았던 에러 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,23 +5,23 @@ import java.util.Map;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
 
-        return answer;
+        Map<Character, Character> frogMap = reverseFrog();
+        return getAnswer(word,frogMap);
     }
 
     public static Map<Character,Character> reverseFrog(){
         Map<Character,Character> frogMap = new HashMap<>();
         //A=65 a=97
-        int count = 0;
+        int reverseCount = 25;
         for(int i=65;i<=90;i++){
             //대문자
-            frogMap.put( (char)i ,(char)(i-count) );
+            frogMap.put( (char)i ,(char)(i+reverseCount) );
             //소문자
             int low = i+32;
-            frogMap.put( (char)low, (char)(low-2) );
+            frogMap.put( (char)low, (char)(low+reverseCount) );
 
-            count+=2;
+            reverseCount-=2;
         }
         frogMap.put( (char)32, (char)32);
         return frogMap;
