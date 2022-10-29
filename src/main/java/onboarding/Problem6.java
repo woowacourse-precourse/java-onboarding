fd83746da907ba456c83c9a61bc83560429b3217
honey170 <63653475+hingjae@users.noreply.github.com>
2022-10-30 01:44:29 +0900

    problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,29 +1,31 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<String>();
-        for(int i = 0; i < forms.size(); i++ )  {
-            List<String> tmp = forms.get(i);
-            for(int j = i + 1; j < forms.size(); j ++) {
-                List<String> tmp2 = forms.get(j);
-                String tmpId = tmp.get(1);
-                String tmp2Id = tmp2.get(1);
-                for(int k = 1; k < tmpId.length(); k++){
-                    String tmpSubstr = tmpId.substring(k-1, k);
-                    if(tmp2Id.contains(tmpSubstr)){
-                        answer.add(tmp2.get(0));
+        List<String> answer = new ArrayList<>();
+
+        for(int i = 0; i < forms.size(); i++){
+            List<String> member = forms.get(i);
+            String nickname = member.get(1);
+            for(int j = 1; j < nickname.length(); j++){
+                String tmp = nickname.substring(j - 1, j + 1);
+                for(int k = i + 1; k < forms.size(); k++){
+                    List<String> tmpMember = forms.get(k);
+                    String tmpNickname = tmpMember.get(1);
+                    if(tmpNickname.contains(tmp)){
+                        answer.add(member.get(0));
+                        answer.add(tmpMember.get(0));
                     }
                 }
             }
         }
+
         Set<String> set = new HashSet<String>(answer);
-        List<String> newAnswer =new ArrayList<String>(set);
-        return newAnswer;
+        answer = new ArrayList<>(set);
+
+        Collections.sort(answer);
+        return answer;
     }
 }
