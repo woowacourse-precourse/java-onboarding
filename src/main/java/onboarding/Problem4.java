aba4e1295109eca4393236d059a184593ca0f819
chaos0103 <lyt1228@naver.com>
2022-10-29 14:22:51 +0900

    feat(Problem4): 문자를 반대로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,16 @@ public class Problem4 {
         return answer;
     }
 
+    private static char charConverter(char target) {
+        if (isUppercase(target)) {
+            return (char) ('Z' - (target - 'A'));
+        } else if (isLowercase(target)) {
+            return (char) ('z' - (target - 'a'));
+        } else {
+            return target;
+        }
+    }
+
     private static boolean isUppercase(char target) {
         return 'A' <= target && target <= 'Z';
     }
