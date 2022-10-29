aa2d5c94e93be9310d3da33751ecde4dace85a34
JeonghoPark <pjhpjh0805@naver.com>
2022-10-29 09:46:37 +0900

    refactor: 돈 유형 담는 배열을 상수로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,21 +6,20 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        answer = new ArrayList<>();
-        int[] typeOfMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+        final int[] MONEY_TYPE = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        countChanges(typeOfMoney, answer, money);
+        countChanges(MONEY_TYPE, answer, money);
 
         return answer;
     }
 
-    private static void countChanges(int[] typeOfMoney, List<Integer> answer, int money) {
+    private static void countChanges(int[] MONEY_TYPE, List<Integer> answer, int money) {
         int balance = money;
-        for (int i=0; i<typeOfMoney.length; i++) {
-            int count = balance/typeOfMoney[i];
+        for (int i=0; i<MONEY_TYPE.length; i++) {
+            int count = balance/MONEY_TYPE[i];
             if (count != 0) {
-                balance -= count*typeOfMoney[i];
+                balance -= count*MONEY_TYPE[i];
                 answer.add(count);
                 continue;
             }
