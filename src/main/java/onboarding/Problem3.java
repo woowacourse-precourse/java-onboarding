dd9fd96471ddc33e0811dc924159c7a31a188f25
soyunim <togksk123@naver.com>
2022-10-29 23:37:40 +0900

    Problem3 2)check if there is the number 3 or 6 or 9

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Problem3 {
     public static int solution(int number) {
         List<Integer> num = new ArrayList<>();
-        int answer = 0;
+        int cnt = 0;
         int n;
 
         // split each numbers, put them in the list
@@ -17,6 +17,12 @@ public class Problem3 {
             }
         }
 
-        return answer;
+        // there are number 3 or 6 or 9 in the list?
+        for (int i = 0; i < num.size(); i++) {
+            if(num.get(i)%3==0 && num.get(i)!=0){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
