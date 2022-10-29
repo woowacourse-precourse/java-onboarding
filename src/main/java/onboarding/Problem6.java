e4228b6996d68312678e15ab027e5298471d1a72
hyowonhan <wain1719@gmail.com>
2022-10-30 02:29:13 +0900

    문제6 main 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int member = sc.nextInt();
+
+        String[][] arr = new String[member][2];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < member; i++) {
+            String inputValue = scanner.nextLine();
+            String[] splitValue = inputValue.split(",");
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = splitValue[j];
+            }
+        }
+
+        List<List<String>> forms = convertToList(arr);
+        System.out.println(solution(forms));
     }
 }
