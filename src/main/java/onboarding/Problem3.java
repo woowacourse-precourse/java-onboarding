11c4c3820909fd3055e2660cd659e37b6683fdfe
박원종 <wonjong1030@naver.com>
2022-10-29 22:40:03 +0900

    feat: 1부터 파라미터 숫자까지 3,6,9 갯수 더하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            answer += getClapCnt(i);
+        }
         return answer;
     }
     public static int getClapCnt(int num){
