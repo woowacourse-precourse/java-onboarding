10f34476248d19358d009f3e6a303011543aa2e3
leeyongjun <leeyongjun0310@naver.com>
2022-10-30 00:43:33 +0900

    feat(PROBLEM7): 모든 이름을 찾는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -24,4 +24,18 @@ public class Problem7 {
         }
         return userFriends;
     }
+
+    private static Set<String> getAllNames(List<List<String>> friends, List<String> visitors){
+        // 모든 이름을 찾는 메소드
+        Set<String> allNames = new HashSet<>();
+        for (List<String> friend : friends){
+            allNames.add(friend.get(0));
+            allNames.add(friend.get(1));
+        }
+
+        for (String visitor : visitors){
+            allNames.add(visitor);
+        }
+        return allNames;
+    }
 }
