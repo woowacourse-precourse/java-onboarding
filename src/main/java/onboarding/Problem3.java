152536097ba3e625df597c41d86ee677c2dabc64
최형준 <choihyoungjun@choehyeongjun-ui-MacBookPro.local>
2022-10-29 15:16:27 +0900

    solve problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            String str = String.valueOf(i);
+            if (str.indexOf('3') != -1 || str.indexOf('6') != -1 || str.indexOf('9') != -1) {
+                answer += countChar(str, '3');
+                answer += countChar(str, '6');
+                answer += countChar(str, '9');
+            }
+        }
         return answer;
+    }
+
+    public static long countChar(String str, char ch) {
+        return str.chars()
+                .filter(c -> c == ch)
+                .count();
     }
 }
