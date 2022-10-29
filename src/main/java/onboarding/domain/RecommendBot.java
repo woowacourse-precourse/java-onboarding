9b9d07b30202a09af3c0343eca13d7dec56f5fcc
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-10-29 09:57:04 +0900

    feat(Problem7, RecommendBot): 함께 아는 친구를 사용한 추천 점수 갱신 기능 구현

--- src/main/java/onboarding/domain/RecommendBot.java
+++ src/main/java/onboarding/domain/RecommendBot.java
@@ -1,20 +1,49 @@
 package onboarding.domain;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RecommendBot {
+    public static final int RECOMMEND_SCORE_BY_FRIEND = 10;
+
+    private final HashMap<String, Integer> recommendScore = new HashMap<>();
     private final List<String> userFriends;
 
     public RecommendBot(String user, List<List<String>> friends, List<String> visitors) {
         userFriends = findUserFriends(user, friends);
+        updateRecommendScoreByNumberOfFriend(user, friends);
     }
 
     private List<String> findUserFriends(String user, List<List<String>> friends) {
         return friends.stream()
-                .filter(friend -> friend.contains(user))
+                .filter(friend -> isUserFriend(user, friend))
                 .flatMap(List::stream)
                 .distinct()
                 .collect(Collectors.toList());
+    }
+
+    private void updateRecommendScoreByNumberOfFriend(String user, List<List<String>> friends) {
+        friends.stream()
+                .filter(this::includeInUserFriends)
+                .flatMap(List::stream)
+                .filter(friend -> !includeInUserFriends(friend) && !friend.equals(user))
+                .forEach(friend -> updateRecommendScore(friend, RECOMMEND_SCORE_BY_FRIEND));
+    }
+
+    private static boolean isUserFriend(String user, List<String> friend) {
+        return friend.contains(user);
+    }
+
+    private boolean includeInUserFriends(List<String> friend) {
+        return includeInUserFriends(friend.get(0)) || includeInUserFriends(friend.get(1));
+    }
+
+    private boolean includeInUserFriends(String friend) {
+        return userFriends.contains(friend);
+    }
+
+    private void updateRecommendScore(String friend, int score) {
+        recommendScore.replace(friend, recommendScore.getOrDefault(friend, 0) + score);
     }
 }
