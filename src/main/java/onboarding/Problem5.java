d85a3bd713ff4a67b925ef2f2035aa0905e70659
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-10-29 18:15:02 +0900

    feat: implement the solution about Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int[] currencyUnits = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        //최대한 큰 금액의 지페 위주로 담자.
+        int leftMoney = money;
+        int count;
+
+        for (int i = 0; i < currencyUnits.length; i++) {
+            count = leftMoney / currencyUnits[i];
+            answer.add(count);
+            leftMoney %= currencyUnits[i];
+        }
+
         return answer;
     }
 }
