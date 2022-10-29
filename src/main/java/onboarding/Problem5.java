04a3ecd5d812d4b6e3958fd7b9029a4c8448b3e5
namtndus <rnrmf0721@gmail.com>
2022-10-29 19:40:59 +0900

    feat(solution):문제5 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,43 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        List<Integer> answer = new ArrayList<>(9);
+
+        int copyMoney = money;
+        int price = 50000;
+
+        for(int i = 0; i<9; i++){
+            if(i==0){
+                answer.add(copyMoney/price);
+                copyMoney = copyMoney % price;
+            } else if (i%2==0) {
+                if(price !=10){
+                    price= price/2;
+                }else{
+                    price=price/10;
+                }
+                answer.add(copyMoney/price);
+                copyMoney = copyMoney % price;
+            }else{
+                price= price/5;
+
+                answer.add(copyMoney/price);
+                copyMoney = copyMoney % price;
+            }
+
+        }
+
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(50237));
     }
 }
