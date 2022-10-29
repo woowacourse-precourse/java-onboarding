723eb87763876d3d8bc33524c157030f9368359e
hyejungg <kimhyejung12@naver.com>
2022-10-30 01:49:09 +0900

    feat: [문제 4] 입력값 검증 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
+	private static final String EXCEPTION_MESSAGE = "ERROR";
     public static String solution(String word) {
-        String answer = "";
+		if (!isValidLength(word.length())) {
+			return getExceptionMessage();
+		}
         return answer;
     }
+
+	private static boolean isValidLength(int length) {
+		return length >= 1 && length <= 1000;
+	}
+
+	private static String getExceptionMessage() {
+		return EXCEPTION_MESSAGE;
+	}
+
 }
