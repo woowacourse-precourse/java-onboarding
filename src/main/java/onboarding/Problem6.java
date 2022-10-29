d01abab83cf681b31d19fd8a7c11c618ac8adab1
jagajang <kosb7544@gmail.com>
2022-10-29 14:03:54 +0900

    p6/add 2 length substring count function

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String, Integer> pieceNum = countPiece(forms);
+
         return answer;
+    }
+
+    private static Map<String, Integer> countPiece(List<List<String>> forms) {
+        Map<String, Integer> pieceNum = new HashMap<>();
+
+        for(List<String> form : forms) {
+            String name = form.get(1);
+
+            for(int i = 1; i < name.length(); i++) {
+                String piece = name.substring(i-1, i+1);
+
+                pieceNum.put(piece, pieceNum.getOrDefault(piece, 0) + 1);
+            }
+        }
+
+        return pieceNum;
     }
 }
