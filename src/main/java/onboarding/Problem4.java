a8ae8690e6e138fa165a7e2d052c4a01f2c3124e
BaeDongHyeon <sasd730@gmail.com>
2022-10-29 22:30:46 +0900

    feat(Problem4.java): 문자 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        return null;
+    }
+
+    private static char getAlphabat(char character) {
+        if ('A' <= character && character <= 'Z') {
+            return (char)(155 - character);
+        }
+        if ('a' <= character && character <= 'z') {
+            return (char)(219 - character);
+        }
+
+        return character;
     }
 }
