cc43c65be170e95d3432ee26c59cec181e74e08f
DaWoon Lee <qkgk61@gmail.com>
2022-10-29 22:21:40 +0900

    Feat : Add ClapNumber class
    박수를 쳐야하는 숫자를 저장한 Set을 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.Set;
+
 public class Problem3 {
+
+    private static final ClapNumber CLAP_NUMBERS;
+
+    static {
+        Set<Integer> clapNumbers = Set.of(3, 6, 9);
+        CLAP_NUMBERS = new ClapNumber(clapNumbers);
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+}
+
+class ClapNumber {
+
+    private final Set<Integer> CLAP_NUMBERS;
+
+    public ClapNumber(final Set<Integer> clapNumbers) {
+        this.CLAP_NUMBERS = clapNumbers;
+    }
+
+    public boolean contains(int number) {
+        return CLAP_NUMBERS.contains(number);
+    }
+
 }
