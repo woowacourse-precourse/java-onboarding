129ce3de13c3babbd4adbc9a25e52166fc8a3d2b
antaeho <an981022@naver.com>
2022-10-29 16:21:08 +0900

    Problem5 improve

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,7 +1,6 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /*
@@ -13,12 +12,10 @@ import java.util.List;
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        ArrayList answer = new ArrayList();
+        ArrayList<Integer> answer = new ArrayList<>();
         int[] arr_money = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        int i = 0;
         for(int unit : arr_money) {
             answer.add(money / unit);
-            i++;
             money %= unit;
         }
         return answer;
