f43a71a0f2e1812a5f243353adb1b050fc980b81
SeokHwan An <asok369258@naver.com>
2022-10-29 17:21:14 +0900

    feat(Problem1) : 왼쪽 페이지와 오른쪽 페이지가 연속된 수인지 파악하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,18 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int CONTINUITY = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static boolean checkPageContinuity(List<Integer> page) {
+        if (page.get(1) - page.get(0) == CONTINUITY) {
+            return true;
+        }
+        return false;
     }
 }
