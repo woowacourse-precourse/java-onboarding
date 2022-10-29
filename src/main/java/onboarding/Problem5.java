1ec8819a6e9b60c0f4e1e5ec1db8ee847769b8b1
chanminmaxscaler <jcm04@naver.com>
2022-10-29 23:30:33 +0900

    feat(Problem5): countEachDenominations 메소드의 기능을 구현한다
    countEachDenominations 메소드는 다음과 같이 기능한다.
    int money와 화폐 종류 리스트 denominationList 를 입력받는다.
    numberOfEachDenominations 리스트를 생성한다.
    화폐 종류 리스트의 요소들을 순서대로 반복하여 아래의 연산을 수행한다.
    divideNumberAByNumberB(money, denominationList)의 반환 값을 numberOfEachDenominations에 저장한다.
    money에 moduloNumberAByNumberB(money, denominationList)저장한다.
    반복이 끝나면 numberOfEachDenominations를 반환한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,11 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
 
     public int divideNumberAByNumberB(int numberA, int numberB) throws ArithmeticException{
         return numberA / numberB;
@@ -21,5 +18,21 @@ public class Problem5 {
     public List<Integer> sortListDescendingOrder(List<Integer> list) {
         list.sort(Comparator.reverseOrder());
         return list;
+    }
+
+    public List<Integer> countEachDenominations(List<Integer> denominationList, int money) throws ArithmeticException{
+        List<Integer> numberOfEachDenominations = new ArrayList<>();
+
+        for (Integer denomination : denominationList) {
+            numberOfEachDenominations.add(divideNumberAByNumberB(money, denomination));
+            money = moduloNumberAByNumberB(money, denomination);
+        }
+
+        return numberOfEachDenominations;
+    }
+
+    public static List<Integer> solution(int money) {
+        List<Integer> answer = Collections.emptyList();
+        return answer;
     }
 }
