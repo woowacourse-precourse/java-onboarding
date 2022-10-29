ca11496ca9c1a9ce6ffcbbed0630aab367dd1cbd
SeongukBaek <bsu6235@gmail.com>
2022-10-29 17:13:02 +0900

    feat: implement setIsUpperCharacter()

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 public class Problem4 {
+    static boolean isUpperCharacter;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
@@ -13,5 +15,17 @@ public class Problem4 {
     * */
     private static boolean isAlphabet(char character) {
         return (character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z');
+    }
+
+    /*
+    * 주어진 문자의 대문자 여부를 확인해 isUpperCharacter 값 설정
+    * isUpperCharacter 초기값은 false
+    *
+    * @return void
+    * */
+    private static void setIsUpperCharacter(char character) {
+        if (Character.isUpperCase(character)) {
+            isUpperCharacter = true;
+        }
     }
 }
