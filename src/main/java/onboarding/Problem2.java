fcc05587655b071df18f6064b66b8e02b07cca03
iamjooon2 <iamjooon2@gmail.com>
2022-10-29 16:04:22 +0900

    style(problem2): 띄어쓰기 포매팅

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,23 +5,21 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         Stack<Character> stack = new Stack<>();
-
         for (char c : cryptogram.toCharArray()) {
             stack = checkRepetition(stack, c);
         }
-
         return stackToString(stack);
     }
 
-    public static String stackToString (Stack<Character> stack){
+    public static String stackToString(Stack<Character> stack) {
         String answer = "";
-        for (char c: stack){
+        for (char c : stack) {
             answer += c;
         }
         return answer;
     }
 
-    public static Stack<Character> checkRepetition(Stack<Character> stack, char c){
+    public static Stack<Character> checkRepetition(Stack<Character> stack, char c) {
         if (!stack.isEmpty() && stack.peek() == c) {
             stack.pop();
         } else {
