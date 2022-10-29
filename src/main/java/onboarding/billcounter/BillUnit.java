9c9ce71b3ecb0e882b723bad67cc073daa4fc345
LEE-sh1673 <lsh901673@gmail.com>
2022-10-29 17:05:17 +0900

    refactor(BillCounter): 열거형 필드 이름 변경

--- src/main/java/onboarding/billcounter/BillUnit.java
+++ src/main/java/onboarding/billcounter/BillUnit.java
@@ -1,14 +1,14 @@
 package onboarding.billcounter;
 
 public enum BillUnit {
-	OH_MAN_WON(50_000),
-	MAN_WON(10_000),
-	OH_CHAN_WON(5_000),
-	CHAN_WON(1_000),
-	OH_BAK_WON(500),
-	BAK_WON(100),
-	SHEEP_WON(10),
-	ILL_ONE(1);
+	FIFTY_THOUSAND_WON(50_000),
+	TEN_THOUSAND_WON(10_000),
+	FIVE_THOUSAND_WON(5_000),
+	THOUSAND_WON(1_000),
+	FIVE_HUNDRED_WON(500),
+	HUNDRED_WON(100),
+	TEN_WON(10),
+	ONE_WON(1);
 
 	private final int money;
 
