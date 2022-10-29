eb51b29b4b122ef87ab128a555873627c009e6a4
yoon <beatus96@naver.com>
2022-10-29 12:25:08 +0900

    [Problem 1] validCheck함수 : 매개변수 유효성 체크 및 예외사항 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,24 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+
+    public static boolean validCheck(List<Integer> integerList){
+// 리스트 체크
+        if(integerList.isEmpty() || integerList ==null)  return false;
+
+//요소 체크
+        for (Integer i : integerList){      //오토언박싱
+            if (i ==null) return false;                 //요소값 중 하나라도 null값이면, false
+
+            int num = i.intValue();
+            if (num == 1 || num == 400)  return false; //요소값 중 하나라도 첫페이지나 마지막페이지면, false
+        }
+
+// 페이지 연결 체크
+        if (integerList.get(0).intValue()+1 != integerList.get(1).intValue()) return false;
+
+        return true;
+    }
+
 }
