7b4c2d821d3b1a20f948580df16738347cdaeb17
Invidam <hansupark0208@gmail.com>
2022-10-29 13:57:02 +0900

    test(Problem1): add test get result
    case1: right win
    case2: left win
    case3: draw
    case4: someone has exception pages

--- src/test/java/onboarding/Problem1Test.java
+++ src/test/java/onboarding/Problem1Test.java
@@ -14,8 +14,8 @@ class Problem1Test {
     void 점수측정테스트() {
 
         //given
-        int[] pages = {4,81,293};
-        int[] realResult = {4,9,54};
+        int[] pages = {4, 81, 293};
+        int[] realResult = {4, 9, 54};
 
         //when
         int[] results = new int[3];
@@ -28,24 +28,35 @@ class Problem1Test {
 
     @Test
     void 유저점수테스트() {
-        List<Integer> firstPages = new ArrayList<>(Arrays.asList(1,2));
+        List<Integer> firstPages = new ArrayList<>(Arrays.asList(1, 2));
 
         assertThrows(InputMismatchException.class, () -> Problem1.getUserMaxPoint(firstPages));
 
-
-        List<Integer> lastPages = new ArrayList<>(Arrays.asList(399,400));
+        List<Integer> lastPages = new ArrayList<>(Arrays.asList(399, 400));
         assertThrows(InputMismatchException.class, () -> Problem1.getUserMaxPoint(lastPages));
 
-
-        List<Integer> pages1 = new ArrayList<>(Arrays.asList(123,124));
+        List<Integer> pages1 = new ArrayList<>(Arrays.asList(123, 124));
 
         assertEquals(8, Problem1.getUserMaxPoint(pages1));
 
-
-        List<Integer> pages2 = new ArrayList<>(Arrays.asList(199,200));
+        List<Integer> pages2 = new ArrayList<>(Arrays.asList(199, 200));
 
         assertEquals(81, Problem1.getUserMaxPoint(pages2));
 
+
+    }
+
+    @Test
+    void 결과테스트() {
+        List<Integer> user1 = new ArrayList<>(Arrays.asList(123, 124));
+        List<Integer> user2 = new ArrayList<>(Arrays.asList(199, 200));
+        List<Integer> user3 = new ArrayList<>(Arrays.asList(99,100));
+        List<Integer> exceptinUser = new ArrayList<>(Arrays.asList(1, 2));
+
+        assertEquals(2, Problem1.getResult(user1,user2));
+        assertEquals(1, Problem1.getResult(user2,user1));
+        assertEquals(0, Problem1.getResult(user3,user2));
+        assertEquals(-1, Problem1.getResult(user1,exceptinUser));
 
     }
 }
