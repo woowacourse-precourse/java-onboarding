11f4b84e2e0229f8179b7efc1ba495eab08f8b5b
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-29 18:01:29 +0900

    feat: Problem4 청개구리 사전을 통해 단어를 변환하는 기능 구현. 단, 아직 대문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,16 +3,17 @@ package onboarding;
 import java.util.HashMap;
 
 /**
- * [ ] 알파벳 외의 문자는 변환하지 않는다.
- * [ ] word를 청개구리 사전을 참고해 반대로 변환한다.
+ * [o] 알파벳 외의 문자는 변환하지 않는다.
+ * [o] word를 청개구리 사전을 참고해 반대로 변환한다.
  * [ ] 알파벳 대문자는 대문자로, 알파벳 소문자는 소문자로 변환한다.
  */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         HashMap<Character, Character> treeFrogDict = replaceOtherCharDict();
+        answer = getReverseWord(word, treeFrogDict);
 
-        return "";
+        return answer;
     }
 
     public static HashMap replaceOtherCharDict() {
@@ -26,5 +27,24 @@ public class Problem4 {
         }
 
         return treeFrogDict;
+    }
+
+    public static String getReverseWord(String word, HashMap<Character,Character> dict) {
+        String upperWord = word.toUpperCase();
+        String replaceWord = "";
+
+        for(int i = 0; i < upperWord.length(); i++) {
+            char target = upperWord.charAt(i);
+
+            if(target == ' ') {
+                replaceWord += ' ';
+                continue;
+            }
+
+            replaceWord += dict.get(target);
+
+        }
+
+        return replaceWord;
     }
 }
