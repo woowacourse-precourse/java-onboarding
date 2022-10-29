8572d833249300ab9c08ae1ee161c84ca4a28c76
Noah <shgustmd6865@naver.com>
2022-10-29 22:12:32 +0900

    Refactor : 돈 단위에 대한 변수명 수정
    돈 단위에 대한 의미가 명확해 지도록 변수명을 arr 에서 standards로 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class Problem5 {
 
-    private static Integer[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static Integer[] standards = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
-        for (Integer standard : arr) {
+        for (Integer standard : standards) {
             int num = money / standard;
             answer.add(num);
             money -= (num * standard);
