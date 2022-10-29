d033f3044e02d7a41d8290b3852b30c8db718c8c
CordHouse <qkqktl5310@gmail.com>
2022-10-30 01:45:26 +0900

    docs : Problem3 기능 목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/*
+    기능 목록
+    1. 파라미터로 입력된 값이 중복된 숫자인지 판단
+        -> 3, 6, 9를 판단하여 반복문 실행
+    2. 중복된 숫자 중 33, 66 등은 짝짝이 나야하므로 카운트 두 번
+        -> 리스트에 하나씩 담아서 크기로 answer에 저장
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
