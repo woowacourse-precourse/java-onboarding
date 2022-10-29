6269e90629bef34113c816bf0daf46ea1f9a4aeb
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-10-30 01:03:57 +0900

    [feat] Problem6: 중복 닉네임 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+    }
+
+    private static boolean isDuplicateNicName(String name, String compare) {
+        int nameLen = name.length();
+        int compareLen = compare.length();
+        for(int m = 1; m < nameLen; m++){
+            for(int n = 1; n < compareLen; n++){
+                if(name.substring(m-1,m+1).equals(compare.substring(n-1,n+1))){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
