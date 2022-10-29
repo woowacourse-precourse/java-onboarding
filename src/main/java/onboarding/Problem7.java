44725de167f5da4eee792a7deec3b99c5c5051be
sangil <gsl0505@naver.com>
2022-10-29 23:56:28 +0900

    refactor(PROBLEM7): 기능 별 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,53 +4,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 	private static final int FRIEND_SCORE = 10;
 	private static final int VISIT_SCORE = 1;
 	private static final int MAX_RECOMMEND_FRIEND = 5;
+	private static final int LEFT_FRIEND_INDEX = 0;
+	private static final int RIGHT_FRIEND_INDEX = 1;
 
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 		Map<String, List<String>> userList = new HashMap<>();
+		userList.putAll(addVisitUser(visitors));
+		userList.putAll(addUser(friends));
+
 		Map<String, Integer> userScore = new HashMap<>();
 
-		for (String visitor : visitors) {
-			userList.put(visitor, new ArrayList<>());
-		}
-		for (List<String> friend : friends) {
-			if (userList.containsKey(friend.get(0))) {
-				userList.get(friend.get(0)).add(friend.get(1));
-			} else {
-				userList.put(friend.get(0), new ArrayList<>(List.of(friend.get(1))));
-			}
-
-			if (userList.containsKey(friend.get(1))) {
-				userList.get(friend.get(1)).add(friend.get(0));
-			} else {
-				userList.put(friend.get(1), new ArrayList<>(List.of(friend.get(0))));
-			}
-		}
 		for (String systemUser : userList.keySet()) {
-			userScore.put(systemUser, 0);
+			userScore.put(systemUser, FRIEND_SCORE * addFriendScore(userList.get(user), userList.get(systemUser)));
 		}
 
-		for (String systemUser : userList.keySet()) {
-			if (userList.get(user).contains(systemUser)) {
-				continue;
-			}
+		userScore.putAll(addVisitScore(userScore, visitors));
 
-			for (String userFriend : userList.get(systemUser)) {
-				if (userList.get(user).contains(userFriend)) {
-					userScore.put(systemUser, userScore.get(systemUser) + FRIEND_SCORE);
-				}
-			}
-		}
-
-		for (String visitor : visitors) {
-			userScore.put(visitor, userScore.get(visitor) + VISIT_SCORE);
-		}
-
-		List<Map.Entry<String, Integer>> recommendFriendList = new ArrayList<>(userScore.entrySet());
+		List<Entry<String, Integer>> recommendFriendList = new ArrayList<>(userScore.entrySet());
 		recommendFriendList.sort((o1, o2) -> {
 			if (o1.getValue().equals(o2.getValue())) {
 				return o1.getKey().compareTo(o2.getKey());
@@ -58,16 +35,55 @@ public class Problem7 {
 			return o2.getValue().compareTo(o1.getValue());
 		});
 
-		List<String> result = new ArrayList<>();
-		for (Map.Entry<String, Integer> entry : recommendFriendList) {
-			if (user.equals(entry.getKey()) || userList.get(user).contains(entry.getKey())) {
-				continue;
-			}
-			if (result.size() > MAX_RECOMMEND_FRIEND || entry.getValue() == 0) {
-				break;
-			}
-			result.add(entry.getKey());
+		return makeRecommendFriendList(user, userList, recommendFriendList);
+	}
+
+	private static Map<String, List<String>> addVisitUser(List<String> visitors) {
+		Map<String, List<String>> userList = new HashMap<>();
+		for (String visitor : visitors) {
+			userList.put(visitor, new ArrayList<>());
 		}
-		return result;
+		return userList;
+	}
+
+	private static Map<String, List<String>> addUser(List<List<String>> friends) {
+		Map<String, List<String>> userList = new HashMap<>();
+		for (List<String> friend : friends) {
+			String leftFriend = friend.get(LEFT_FRIEND_INDEX);
+			String rightFriend = friend.get(RIGHT_FRIEND_INDEX);
+			if (userList.containsKey(leftFriend)) {
+				userList.get(leftFriend).add(rightFriend);
+			} else {
+				userList.put(leftFriend, new ArrayList<>(List.of(rightFriend)));
+			}
+			if (userList.containsKey(rightFriend)) {
+				userList.get(rightFriend).add(leftFriend);
+			} else {
+				userList.put(rightFriend, new ArrayList<>(List.of(leftFriend)));
+			}
+		}
+		return userList;
+	}
+
+	private static int addFriendScore(List<String> userFriends, List<String> systemUserFriends) {
+		return (int)systemUserFriends.stream().filter(userFriends::contains).count();
+	}
+
+	private static Map<String, Integer> addVisitScore(Map<String, Integer> scoreList, List<String> visitors) {
+		for (String visitor : visitors) {
+			scoreList.put(visitor, VISIT_SCORE);
+		}
+		return scoreList;
+	}
+
+	private static List<String> makeRecommendFriendList(String user, Map<String, List<String>> userList,
+		List<Entry<String, Integer>> recommendFriend) {
+		return recommendFriend.stream()
+			.filter(key -> !user.equals(key.getKey()))
+			.filter(key -> !userList.get(user).contains(key.getKey()))
+			.filter(value -> value.getValue() != 0)
+			.map(Entry::getKey)
+			.limit(MAX_RECOMMEND_FRIEND)
+			.collect(Collectors.toList());
 	}
 }
