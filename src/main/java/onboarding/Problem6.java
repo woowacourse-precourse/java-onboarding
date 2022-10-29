c8c752f47b74be478948c4cb8bf06f30b8c8405d
yoondgu <doyou2ing@naver.com>
2022-10-29 14:43:44 +0900

    feat(Problem6): implement method getUserIdxMapByTwoWords

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,14 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-//      System.out.println(getAllTwoWordsByNickname("간장공장공장장")); // 간장, 공장, 장공, 장장
-        return answer;
+        return new ArrayList<>(answer);
     }
     
     /**
@@ -21,5 +22,27 @@ public class Problem6 {
     		twoWords.add(nickname.substring(i, i+2));
     	}
     	return twoWords;
+    }
+    
+    /**
+     * 주어진 복수 개의 닉네임에 대하여 구성된 모든 두 글자 문자열에 대한 정보를 반환한다.
+     * @param nicknames
+     * @return key값이 두 글자 문자열, value값이 해당 문자열을 닉네임에 사용한 사용자의 인덱스 리스트인 HashMap
+     */
+    private static HashMap<String, List<Integer>> getUserIdxMapByTwoWords(List<String> nicknames) {
+    	HashMap<String, List<Integer>> result = new HashMap<>();
+    	for (int i = 0; i < nicknames.size(); i++) {
+    		String nickname = nicknames.get(i);
+    		HashSet<String> twoWords = getAllTwoWordsByNickname(nickname);
+    		// 해당 닉네임의 모든 twoWord에 대해 result 정보 갱신
+    		for (String twoWord : twoWords) {
+    			// twoWord가 result의 key로 이미 존재하면 리스트값을 가져오고, 존재하지 않으면 새 리스트를 생성한다.
+    			List<Integer> userIdx = (result.containsKey(twoWord)) ? result.get(twoWord) : new ArrayList<Integer>();
+    			// TODO 리스트에 현재 인덱스를 담는다. => user 정보를 담는 것으로 수정할 것
+    			userIdx.add(i);
+    			result.put(twoWord, userIdx);
+    		}
+    	}
+    	return result;
     }
 }
