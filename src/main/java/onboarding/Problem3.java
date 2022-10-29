10772a373d5b46417000b76bcd41f4ec426640e5
leeyongjun <leeyongjun0310@naver.com>
2022-10-29 16:35:57 +0900

    feat(PROBLEM3): 3, 6, 9의 개수를 세주는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -11,4 +11,17 @@ public class Problem3 {
         int number = Integer.parseInt(string);
         return number == 3 || number == 6 || number == 9;
     }
+
+    private static int getCount(int number){
+        // 3, 6, 9의 개수를 세주는 메소드
+        String string = String.valueOf(number);
+        int totalCount = 0;
+        for(String s : string.split("")){
+            if (isIn369(s)){
+                totalCount += 1;
+            }
+        }
+
+        return totalCount;
+    }
 }
