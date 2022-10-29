a3f28abaa1d871aa41e0468e749e7bcc82c4cdda
Noah <shgustmd6865@naver.com>
2022-10-29 21:50:17 +0900

    Feat : 알파벳인지 찾는 기능 추가
    문자열의 요소가 알파벳인지를 판단하여
    알파벳이 아니라면 변환하지 않고 더하는 기능 입니다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,13 +6,25 @@ public class Problem4 {
     private static final int UpperCaseMaxCode = 90;
     private static final int LowerCaseMinCode = 97;
     private static final int LowerCaseMaxCode = 122;
-    
+
     public static String solution(String word) {
         String answer = "";
 
         for (char element : word.toCharArray()) {
+            if (isAlphabet(element)) {
 
+            }
+            else
+                answer += element;
         }
         return answer;
+    }
+
+    public static boolean isAlphabet(Character word) {
+        int wordCode = (int) word;
+        if ((UpperCaseMinCode <= wordCode && wordCode <= UpperCaseMaxCode) ||
+                (LowerCaseMinCode <= wordCode && wordCode <= LowerCaseMaxCode))
+            return true;
+        return false;
     }
 }
