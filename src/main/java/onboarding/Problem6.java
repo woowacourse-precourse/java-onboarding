404faf50d2e53c3812656f19959c06d6578ec3ae
wltks98 <wltks98@hanmail.net>
2022-10-29 13:18:26 +0900

    [문제6] feat : 저장한 닉네임을 중복 제거한 뒤 오름차순 정렬하여 반환하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,9 +7,9 @@ import onboarding.problemsix.domain.Woowacourse;
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
 
-		Woowacourse woowacourse = new Woowacourse(forms);
+		Woowacourse woowacourse= new Woowacourse();
 
-		List<String> answer = List.of("answer");
+		List<String> answer = woowacourse.getEmailList(forms);
 		return answer;
 	}
 }
--- src/main/java/onboarding/problemsix/domain/Woowacourse.java
+++ src/main/java/onboarding/problemsix/domain/Woowacourse.java
@@ -1,28 +1,44 @@
 package onboarding.problemsix.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public class Woowacourse {
 
-	private HashMap<String, List<String>> crewMap = new HashMap<>();
+	private HashMap<String, String> morphemeMap = new HashMap<>();
+	HashSet<String> emailSet = new HashSet<>();
 
-	public Woowacourse(List<List<String>> forms) {
+	public List<String> getEmailList(List<List<String>> forms) {
 
 		for (List<String> form : forms) {
 			String email = form.get(0);
 			String nickname = form.get(1);
 
-			if (crewMap.containsKey(nickname)) {
-				crewMap.get(nickname).add(email);
-			} else if (!crewMap.containsKey(nickname)) {
-				crewMap.put(nickname, new ArrayList<>());
-			}
-
+			validateNickname(email, nickname);
 		}
 
+		List<String> emailList = new ArrayList<>(emailSet);
+		Collections.sort(emailList);
+		return emailList;
+	}
+
+	private void validateNickname(String email, String nickname) {
+		for (int start = 0; start < nickname.length()-1; start++) {
+			for (int length = 2; length <= nickname.length() && start + length <= nickname.length(); length++) {
+
+				String morpheme = nickname.substring(start, start + length);
+
+				if (morphemeMap.containsKey(morpheme)) {
+					emailSet.add(morphemeMap.get(morpheme));
+					emailSet.add(email);
+				} else {
+					morphemeMap.put(morpheme, email);
+				}
+			}
+		}
 	}
 
 }
