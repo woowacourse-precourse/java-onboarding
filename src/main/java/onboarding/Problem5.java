eda4f4db728d2c8873abfb9357ea2a39a4825ffd
Invidam <hansupark0208@gmail.com>
2022-10-29 19:56:19 +0900

    feat(Problem5): add get result array function
    1. add const array about units from 50_000 to 1.
    2. add logic codes about calc result array.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    static final int[] UNITS = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int leftMoney = money;
+
+        for(int unit: UNITS) {
+            if(leftMoney < unit) {
+                answer.add(0);
+                continue;
+            }
+
+            int unitCount = leftMoney / unit;
+            answer.add(unitCount);
+
+            leftMoney %= unit;
+        }
+
         return answer;
     }
 }
