752d824e528fee3cb2c181836aa01bbbda113c2f
youhobin <ghdb132@naver.com>
2022-10-29 14:26:48 +0900

    feat(onboarding) : 닉네임만을 가져와서 Collection에 담는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,15 +1,22 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Map<String, String> form = new HashMap<>();
+        Map<String, String> formsMap = new HashMap<>();
         for (int i = 0; i < forms.size(); i++) {
-            List a = forms.get(i);
-            form.put((String) a.get(0), (String) a.get(1));
+            List form = forms.get(i);
+            formsMap.put((String) form.get(0), (String) form.get(1));
+        }
+
+        Collection<String> nicknames = formsMap.values();
+
+        Iterator iterator = nicknames.iterator();
+        while (iterator.hasNext()) {
+            String nickname = (String) iterator.next();
+            System.out.println(nickname);
+            checkNickName(nickname, nicknames);
         }
 
         List<String> answer = List.of("answer");
