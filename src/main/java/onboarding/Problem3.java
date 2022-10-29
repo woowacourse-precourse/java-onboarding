f0b78423d6565cb5f332ce8e8f7ebd6c6fb9bfd5
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-10-29 18:03:45 +0900

    feat: implement the solution about Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            String convertedNumber = Integer.toString(i);
+            sum += countOf369(convertedNumber);
+        }
+        return sum;
+    }
+
+    private static int countOf369(String num) {
+        int count = 0;
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            if (c == '3' || c == '6' || c == '9') {
+                count++;
+            }
+        }
+        return count;
     }
 }
