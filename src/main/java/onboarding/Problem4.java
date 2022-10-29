9b016e51680bbb83456cca532a1c01e841600e2c
hyowonhan <wain1719@gmail.com>
2022-10-29 18:44:55 +0900

    문제4 주석 달기 및 마무리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        for(int i=0; i<word.length(); i++) {
-            if ((int)word.charAt(i)>96) {
+        for(int i=0; i<word.length(); i++) { //반복문을 이용하여 문자 하나하나 변환하기
+            if ((int)word.charAt(i)>96) { //대문자의 경우
                 answer += (char)(219-(int)word.charAt(i));
-            } else if ((int)word.charAt(i)>64) {
+            } else if ((int)word.charAt(i)>64) { //소문자의 경우
                 answer += (char)(155-(int)word.charAt(i));
-            } else {
+            } else { //공백일 경우
                 answer += " ";
             }
         }
