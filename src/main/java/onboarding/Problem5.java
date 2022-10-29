f007fa05f8710c774ca5f35f179095da5441b057
Koonayoung518 <koony000415@gmail.com>
2022-10-30 03:10:42 +0900

    feat(problem5) : 값(money) 나누기 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        for(int i=10000; i>0; i/=10){
+            // money / i
+            money %= i;
+        }
         return answer;
     }
+
 }
