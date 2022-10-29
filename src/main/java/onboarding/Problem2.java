441da2ae1a52831bf7aa691fa4d56b14446d74a3
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-10-30 05:51:53 +0900

    Problem2: string데이터를 list<Character>형으로 변환하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        //String을 List<Character>으로 변환
+        List<Character> cyp = new ArrayList<Character>();
+        for(int i=0; i<cryptogram.length(); i++){
+            cyp.add(cryptogram.charAt(i));
+        }
+
         return answer;
     }
 }
