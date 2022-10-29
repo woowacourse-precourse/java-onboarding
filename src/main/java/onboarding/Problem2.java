d226576ab3cfacfaf3e7681e57be9a3bad0a2f42
jagajang <kosb7544@gmail.com>
2022-10-29 12:21:50 +0900

    add decode function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        for(char c : cryptogram.toCharArray()) {
+            if(answer.isEmpty() || !answer.endsWith(String.valueOf(c))) {
+                answer += c;
+                continue;
+            }
+
+            answer = answer.substring(0, answer.length() - 1);
+        }
+
         return answer;
     }
 }
