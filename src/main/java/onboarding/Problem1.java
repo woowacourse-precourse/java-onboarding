4f0867d448740285c93075a9127bb6c5c9cb5e8d
HaODay <ycis5013@naver.com>
2022-10-29 16:07:51 +0900

    [Problem1] pobi 점수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,6 +15,30 @@ class Problem1 {
             return answer;
         }
 
+        int[] pobi_num=new int[2];
+        int pobi_max;
+        int sum=0, mul=1;
+
+        for(int i=0;i<2;i++) {
+            int num=pobi.get(i);
+            int num2=pobi.get(i);
+
+            while (num!= 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            while (num2 != 0) {
+                mul *= num2 % 10;
+                num2 /= 10;
+            }
+            if (sum > mul) pobi_num[i] = sum;
+            else pobi_num[i] = mul;
+
+            sum=0;
+            mul=1;
+        }
+        if (pobi_num[0] > pobi_num[1]) pobi_max = pobi_num[0];
+        else pobi_max = pobi_num[1];
 
         return answer;
     }
