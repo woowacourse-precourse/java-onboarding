d99f41141eaabcc29efe56309b86e9622880b7e6
opuopuo <pureuml.dev@gmail.com>
2022-10-30 01:49:36 +0900

    refactor: 변수 선언 위치 이동

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        int[] unitChange = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-
         if (!verifyException(money)) {
             return Collections.emptyList();
         }
+
+        int[] unitChange = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         return exchangeChange(money, unitChange);
     }
 
