d8089e7a206a699cd035232b093c874c6d90871d
hyejungg <kimhyejung12@naver.com>
2022-10-30 01:09:49 +0900

    feat: [문제 3] 입력값 검증 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+		if (!isValidNumber(number)) {
+			return -1;
+		}
+
         return answer;
     }
+	private static boolean isValidNumber(int number) {
+		return number >= 1 && number <= 1000;
+	}
+
 }
