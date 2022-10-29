50a68e993c6c434c1cac86ba438d93670b425249
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-29 18:53:25 +0900

    Feat(Problem4): 알파벳 역순 해시맵 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    static HashMap<Character, Character> makeHashMap() {
+        HashMap<Character, Character> alphabetHash = new HashMap<>();
+
+        for (int i = 0; i < 26; i++) {
+            alphabetHash.put((char) (65 + i), (char) (90 - i));
+            alphabetHash.put((char) (97 + i), (char) (122 - i));
+        }
+
+        return alphabetHash;
     }
 }
