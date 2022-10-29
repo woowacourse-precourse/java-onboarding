1b8608dd8dbbf46f4be8a83a2ffebd8daf66c0dc
ghkdgus29 <ghkdgus29@naver.com>
2022-10-29 14:18:54 +0900

    #6 제한사항 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,8 +7,14 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = gather(forms);
-        return answer;
+        try {
+            crewNumberValidation(forms.size());
+
+            List<String> answer = gather(forms);
+            return answer;
+        } catch (RuntimeException e) {
+            throw e;
+        }
     }
 
     public static boolean check(List<List<String>> forms, int idx) {
@@ -16,6 +22,8 @@ public class Problem6 {
             if (idx == i) {
                 continue;
             }
+
+            formValidation(forms.get(idx));
 
             String fivotName = forms.get(idx).get(1);
             String name = forms.get(i).get(1);
@@ -42,5 +50,36 @@ public class Problem6 {
 
         emailList.sort(Comparator.naturalOrder());
         return emailList;
+    }
+
+    public static void crewNumberValidation(int size) {
+        if (size < 1 || size > 10000) {
+            throw new RuntimeException("크루는 1명 이상, 10,000 명 이하입니다.");
+        }
+    }
+
+    public static void formValidation(List<String> fivot) {
+        String email = fivot.get(0);
+        String name = fivot.get(1);
+        int emailSize = email.length();
+        int nameSize = name.length();
+
+        int idx = email.indexOf("@");
+        if (!(email.substring(idx + 1).equals("email.com"))) {
+            throw new RuntimeException("email.com 이메일만 사용할 수 있습니다.");
+        }
+
+        if (emailSize < 11 || emailSize > 19) {
+            throw new RuntimeException("이메일 길이는 11자 이상 20자 미만입니다.");
+        }
+
+        if (nameSize < 1 || nameSize > 19) {
+            throw new RuntimeException("닉네임의 길이는 1자 이상 20자 미만입니다.");
+        }
+
+        String regExp = "^[가-힣]*$";
+        if (!name.matches(regExp)) {
+            throw new RuntimeException("닉네임은 한글만 가능합니다.");
+        }
     }
 }
--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -3,9 +3,11 @@ package onboarding;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Problem6Test {
@@ -44,5 +46,62 @@ class Problem6Test {
         List<String> result = Problem6.gather(forms);
 
         assertThat(result).containsExactly("jason@email.com", "jm@email.com", "mj@email.com");
+    }
+
+    @Test
+    void exception() {
+        List<List<String>> forms1 = Collections.emptyList();
+
+        assertThatThrownBy(() -> Problem6.solution(forms1))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("크루는 1명 이상, 10,000 명 이하입니다.");
+
+        List<List<String>> forms2 = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@naver.com", "워니"),
+                List.of("mj@gmail.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+
+        assertThatThrownBy(() -> Problem6.solution(forms2))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("email.com 이메일만 사용할 수 있습니다.");
+
+        List<List<String>> forms3 = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jasdddddssdddon@email.com", "제이슨"),
+                List.of("woniee@email.com", "워니"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+
+        assertThatThrownBy(() -> Problem6.solution(forms3))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("이메일 길이는 11자 이상 20자 미만입니다.");
+
+        List<List<String>> forms4 = List.of(
+                List.of("jm@email.com", "제이엠제이엠제이엠제이엠제이엠제이엠제이제미에에지에에"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "워니"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+
+        assertThatThrownBy(() -> Problem6.solution(forms4))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("닉네임의 길이는 1자 이상 20자 미만입니다.");
+
+        List<List<String>> forms5 = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "wooney"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+
+        assertThatThrownBy(() -> Problem6.solution(forms5))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("닉네임은 한글만 가능합니다.");
     }
 }
