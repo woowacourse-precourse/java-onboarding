555d411f8a0ebd9c196821b498f2f853b2cd08d1
nuyh <hjaehyun25@gmail.com>
2022-10-29 10:38:49 +0900

    refactor(Problem5.java): 최종 결과 연산을 함수로 분리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,26 +1,23 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static final int[] DIVISORS=new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-    public static List<Integer> solution(int money) {
-        ArrayList<Integer> answer = new ArrayList<>();
+    public static List<Integer> divideByAllDivisors(int dividend){
+        ArrayList<Integer> result=new ArrayList<>();
 
         for (int divisor : DIVISORS) {
-            if (divisor > money)
-                answer.add(0);
-
-            if (divisor <= money)
-                answer.add(money / divisor);
-
-            money %= divisor;
+            result.add(dividend / divisor);
+            dividend %= divisor;
         }
 
-        return answer;
+        return result;
+    }
+
+    public static List<Integer> solution(int money) {
+        return divideByAllDivisors(money);
     }
 }
