2ce65a0dfe5d0c95b8bef7b1ba8254a8221d21fa
himjjong <jjijji5728@tukorea.ac.kr>
2022-10-29 17:24:15 +0900

    feat : 같은 문자를 체크하는 checkString 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String result = "";
+        result = checkString(cryptogram);
+        return result;
+    }
+    static String checkString(String str) {
+        String answer = "";
+        for(int i=0; i<str.length()-1; i++) {
+            if(str.charAt(i)==str.charAt(i+1)) {
+                str = splitString(str, i);
+                i = -1;
+            }
+        }
+        return str;
+    }
+    static String splitString(String str, int index) {
+        String newstring = "";
+        return newstring;
     }
 }
