b994a58d7bbbf7a3a48fbc2579a348d279dd1418
5onchangwoo <scw9410@gmail.com>
2022-10-29 18:14:16 +0900

    problem1/둘의 점수 비교하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,7 +8,6 @@ import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        System.out.println(maxScore(pobi));
         int answer = Integer.MAX_VALUE;
         return answer;
     }
@@ -22,6 +21,20 @@ class Problem1 {
             scoreList.add(multipleSplitNum(splitPage));
         }
         return scoreList.stream().max(Comparator.comparing(x -> x)).get();
+    }
+
+    // 점수 비교하기
+    // pobi가 승리하면 1, crong이 승리하면 2, 무승부 0, 예외 -1
+    private static int compareScore(int pobiScore, int crongScore) {
+        int result;
+        if (pobiScore > crongScore) {
+            result = 1;
+        } else if (pobiScore < crongScore) {
+            result = 2;
+        } else {
+            result = 0;
+        }
+        return result;
     }
     private static int[] splitNum(Integer num) {
         return Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
