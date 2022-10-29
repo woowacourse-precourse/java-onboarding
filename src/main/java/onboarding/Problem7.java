f1f820457374f94f6e8c90baf59d384efc0f7b4b
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-10-30 01:43:48 +0900

    feat(7번): visitors에 해당하는 사람들 점수 부여

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -20,7 +20,17 @@ public class Problem7 {
         }
         System.out.println(Friends);
 
-
+        for (int j=0; j<visitors.size(); j++){
+            if (map.containsKey(visitors.get(j))){
+                int value = map.get(visitors.get(j));
+                map.replace(visitors.get(j), value+1);
+            }
+            else{
+                if (!Friends.contains(visitors.get(j))) {
+                    map.put(visitors.get(j), 1);
+                }
+            }
+        }
         System.out.println(map);
         List<String> keySet = new ArrayList<>(map.keySet());
 
