d8a04756b042d41c0b0ce77824e0349941215cc0
wonju-dev <wonjulee.dev@gmail.com>
2022-10-29 16:45:14 +0900

    [fix] 예외 처리 로직 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,7 +16,6 @@ class Problem1 {
     static final int BOOK_END_PAGE = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        // validate inputs
         if (isWrongInput(pobi) || isWrongInput(crong)) {
             return EXCEPTION;
         }
@@ -62,22 +61,18 @@ class Problem1 {
     private static boolean isWrongInput(List<Integer> inputs) {
         int left = inputs.get(LEFT);
         int right = inputs.get(RIGHT);
-        boolean isValid = true;
 
-        // 입력값이 정렬이 안 된 경우 (왼쪽 >= 오른쪽)
         if (isNotSorted(left, right)) {
-            isValid = false;
+            return true;
         }
-        // 왼쪽 페이지, 오른쪽 페이지의 번호 차이가 1보다 큰 경우
         if (getDifference(left, right) > 1) {
-            isValid = false;
+            return true;
         }
-        // 1 이하, 400이상 입력값이 들어온 경우
         if (isOutOfBound(left, right)) {
-            isValid = false;
+            return true;
         }
 
-        return isValid;
+        return false;
     }
 
     private static boolean isOutOfBound(Integer left, Integer right) {
