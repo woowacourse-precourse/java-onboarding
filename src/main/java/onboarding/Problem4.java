f1ed9ee8ec886083162aab50fba8eada833ee030
HyeokJoon Kong <orijoon98@gmail.com>
2022-10-29 16:58:36 +0900

    feat: 문제4 - 청개구리 변환 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,25 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static String frogConvert(String word) {
+        char[] alphabets = word.toCharArray();
+        int move; // 뒤집기 위해 더해야 하는 아스키 코드 값
+        // a  b  c  d  e  f  g  h  i  j  k  l  m   n  o  p  q  r  s   t   u   v   w   x   y   z
+        // A  B  C  D  E  F  G  H  I  J  K  L  M   N  O  P  Q  R  S   T   U   V   W   X   Y   Z
+        // 25 23 21 19 17 15 13 11 9  7  5  3  1  -1 -3 -5 -7 -9 -11 -13 -15 -17 -19 -21 -23 -25
+
+        for (int i = 0; i < alphabets.length; i++) {
+            if (alphabets[i] >= 'a' && alphabets[i] <= 'z') {
+                move = 25 - 2 * (alphabets[i] - 'a');
+                alphabets[i] += move;
+            }
+            if (alphabets[i] >= 'A' && alphabets[i] <= 'Z') {
+                move = 25 - 2 * (alphabets[i] - 'A');
+                alphabets[i] += move;
+            }
+        }
+
+        return String.valueOf(alphabets);
+    }
 }
