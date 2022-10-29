a539113f459f7a3aeb27664490b7c6d2111d083d
AhnJoonSung <tjdtna01@naver.com>
2022-10-29 20:54:18 +0900

    Problem4 done

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int upBase = 'A' + 'Z';
+        int lowBase = 'a' + 'z';
+
+        for (int i = 0; i < word.length(); i++) {
+            char myChar = word.charAt(i);
+
+            if (myChar >= 'A' && myChar <= 'Z') {
+                myChar = (char)(upBase - myChar);
+            } else if (myChar >= 'a' && myChar <= 'z') {
+                myChar = (char)(lowBase - myChar);
+            }
+            answer += myChar;
+        }
         return answer;
     }
 }
