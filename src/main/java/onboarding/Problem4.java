91daffda0f650c3079fe65cac25c16853dbbd68c
MinSungKim <timkim01@naver.com>
2022-10-29 17:25:27 +0900

    feat : Problem5 getMoneyList 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,11 +2,28 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = getConvertedWord(word);
         return answer;
     }
 
-    public static void getConvertedWord(){
+    public static String getConvertedWord(String word) {
+        StringBuilder sb = new StringBuilder();
+        int len = word.length();
 
+        for (int i = 0; i < len; i++) {
+            char ch = word.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                sb.append((char)(155 - ch));
+                continue;
+            }
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append((char)(219 - ch));
+                continue;
+            }
+            sb.append(ch);
+        }
+
+        return sb.toString();
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = getMoneyList(money);
         return answer;
+    }
+
+    public static ArrayList<Integer> getMoneyList(int money){
+        int[] moneyArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            result.add(money / moneyArr[i]);
+            money %= moneyArr[i];
+        }
+
+        return result;
     }
 }
