5bf23d62f444a8737af42950a17cd5155c062afd
dldbdud314 <dldbdud314@naver.com>
2022-10-29 15:22:02 +0900

    [feat] : 6.2. value가 2이상인 크루의 이메일 뽑기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         // 1. 길이별 딕셔너리 저장
+        // 딕셔너리 구조 {"name" : [email1, email2, ...](이메일 set)}
         HashMap<String, Set<String>> map = new HashMap<>();
         for (List<String> form : forms){
             String email = form.get(0);
@@ -28,8 +29,16 @@ public class Problem6 {
             }
         }
 
-        // 2. value의 길이가 2 이상인 크루의 이메일을 뽑아서 정렬해서 return 하기
-        List<String> answer = List.of("answer");
-        return answer;
+        // 2. value(이메일 set)의 길이가 2 이상인 크루의 이메일을 뽑아서 정렬해서 return
+        Set<String> answerSet = new HashSet<>();
+        for (String key : map.keySet()) {
+            Set<String> value = map.get(key);
+            if (value.size() > 1)
+                answerSet.addAll(value);
+        }
+        List<String> answerList = new ArrayList<>(answerSet);
+        Collections.sort(answerList);
+
+        return answerList;
     }
 }
