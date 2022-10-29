918fa7be26bd55466c227483513a233c9a435fa8
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-29 16:28:11 +0900

    [Problem2] feat: searchDuplicateIndex() 메서드 구현
    - 특정 위치의 문자가 중복됐는지 확인하여 마지막 중복된 인덱스 반환 or 중복이 아니면 특정 위치를 반환하는 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -29,7 +29,7 @@ public class Problem2 {
         return;
     }
 
-    /** 특정 인덱스의 char와 연속된 중복을 찾아 제거 */
+    /** 연속된 중복을 찾아 1번 제거 */
     private static boolean removeDuplication(StringBuffer cryptogarm) {
         int start = -1; // 삭제할 범위의 시작 인덱스
         int end = -1;   // 삭제할 범위의 마지막 인덱스
@@ -37,8 +37,8 @@ public class Problem2 {
         // 삭제할 범위의 시작 인덱스가 암호문의 마지막 인덱스이거나, 중복이 검색되어 end가 start보다 커지면 while문 종료
         while (start < cryptogarm.length() - 1 && start >= end) {
             start++; // start를 1씩 증가하여 해당 인덱스의 알파벳 중복을 검사
-            end = searchDuplicateIndex(cryptogarm.charAt(start));
-            // 중복이 검색되면 연속된 중복의 마지막 인덱스가, 검색안되면 -1이 반환
+            end = searchDuplicateIndex(start, cryptogarm.charAt(start), cryptogarm);
+            // 중복이 검색되면 연속된 중복의 마지막 인덱스가, 검색안되면 start 반환
         }
         if (start < end && start > -1 && end < cryptogarm.length()) { // start와 end가 유효하다면,
             cryptogarm.delete(start, end + 1);  // start<= x <= end 범위의 문자 삭제
@@ -47,4 +47,57 @@ public class Problem2 {
         return isRemoved;
     }
 
+    /** 특정 위치의 문자가 중복됐는지 확인하여 마지막 중복된 인덱스 반환 */
+    private static int searchDuplicateIndex(int start, char c, StringBuffer cryptogarm) {
+        int end;
+        // break가 발생하지 않으면 최종적인 end의 값은 cryptogarm.length()-1
+        for (end = start; end < cryptogarm.length()-1; end++) {
+            if (cyptogarm.charAt(end + 1) != 'c') {
+                break;
+            }
+        }
+        return end;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
