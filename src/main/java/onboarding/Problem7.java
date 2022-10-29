346b583120b1074975fd3f674059fe8786a04fc8
JJongmen <pjy90123@gmail.com>
2022-10-29 15:49:30 +0900

    feat(Problem7): 사용자와 함께 아는 친구의 수를 구하여 점수에 더하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,6 +6,13 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Map<String, List<String>> friendship = getFriendshipByFriends(friends);
+        Map<String, Integer> scores = new HashMap<>();
+        for (String id : friendship.keySet()) {
+            if (user.equals(id)) {
+                continue;
+            }
+            scores.put(id, 10 * countSameFriend(user, id, friendship));
+        }
         return answer;
     }
 
@@ -20,5 +27,15 @@ public class Problem7 {
             friendship.get(idB).add(idA);
         }
         return friendship;
+    }
+
+    private static int countSameFriend(String user, String id, Map<String, List<String>> friendship) {
+        int sameFriendCnt = 0;
+        for (String friend : friendship.get(id)) {
+            if (friendship.get(user).contains(friend)) {
+                sameFriendCnt++;
+            }
+        }
+        return sameFriendCnt;
     }
 }
