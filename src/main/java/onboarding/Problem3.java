f7dd2fdb98a75c187dcff5db9a5022d4449eaa9d
dohyun <ldh19738@gmail.com>
2022-10-29 11:52:14 +0900

    Problem3 기능을 메소드로 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i=1; i<=number; i++) {
+            answer += clap(i);
+        }
         return answer;
+    }
+
+    public static int clap(int current) {
+        int count = 0;
+
+        while(current != 0) {
+            if ((current%10)%3 == 0 && (current%10) != 0) {
+                count++;
+                current /= 10;
+            }else current /= 10;
+        }
+
+        return count;
     }
 }
