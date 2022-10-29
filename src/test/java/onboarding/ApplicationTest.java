ee2744025f2a03db572b8724c6381988c8615b5d
gnoesnooj <306yyy@naver.com>
2022-10-29 19:42:16 +0800

    test : PROBLEM5 테스트코드 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -163,15 +163,22 @@ class ApplicationTest {
     class Problem5Test {
         @Test
         void case1() {
-            int money = 50_237;
+            int money = 50237;
             List<Integer> result = List.of(1, 0, 0, 0, 0, 2, 0, 3, 7);
             assertThat(Problem5.solution(money)).isEqualTo(result);
         }
 
         @Test
         void case2() {
-            int money = 15_000;
+            int money = 15000;
             List<Integer> result = List.of(0, 1, 1, 0, 0, 0, 0, 0, 0);
+            assertThat(Problem5.solution(money)).isEqualTo(result);
+        }
+
+        @Test
+        void case3() {
+            int money = 1500000000;
+            List<Integer> result = List.of();
             assertThat(Problem5.solution(money)).isEqualTo(result);
         }
     }
