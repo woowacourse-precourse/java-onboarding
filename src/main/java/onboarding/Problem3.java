450084b916dd8b1f95587005acf8065dcefef74f
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 04:14:16 +0900

    feat : Problem3 제한사항 확인 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = 0;
+
+		try {
+			isValidNumber(number);
+		} catch (IllegalStateException illegalStateException) {
+			illegalStateException.printStackTrace();
+		}
+
+		return answer;
+	}
+
+	public static void isValidNumber(int number) throws IllegalStateException {
+		if (number < 1 || number > 10000)
+			throw new IllegalStateException("number가 범위 밖입니다.");
+	}
 }
