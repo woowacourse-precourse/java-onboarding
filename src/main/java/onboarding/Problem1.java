52ef31ba2e568040275bead9053a02efb3f0e6a7
ChaCha3088 <cha3088@gmail.com>
2022-10-29 23:27:03 +0900

    feat: problem1 new function splitEachNumbers

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,7 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -13,9 +14,14 @@ class Problem1 {
     private ArrayList<Integer> drawTwoNumbersInRow() {
         Random random = new Random(); //랜덤 객체 생성(디폴트 시드값 : 현재시간)
         random.setSeed(System.currentTimeMillis());
-        int randomNumber = 2 * ( random.nextInt(197) + 1 ) + 1;
+        int randomNumber = 2 * (random.nextInt(197) + 1) + 1;
         ArrayList<Integer> result = new ArrayList<>();
         result.add(randomNumber, randomNumber + 1);
         return result;
+    }
+
+    private int[] splitEachNumbers(int number) {
+        int[] arrNum = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+        return arrNum;
     }
 }
