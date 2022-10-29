ef373691bbb0cc0b61ec7f7b7bd4f05384f610a4
bjm7701 <bjm7701@naver.com>
2022-10-29 12:48:39 +0900

    풀이 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        boolean flag = true;
+        while(flag){
+            flag = false;
+            for(int i = 0; i < 26; i++){
+                String token = "";
+                token += (char)('a' + i);
+                token += (char)('a' + i);
+                String res = answer.replace(token, "");
+                if(res.length() != answer.length()) flag = true;
+                answer = res;
+            }
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder larr = new StringBuilder("abcdefghijklmnopqrstuvwxyz").reverse();
+        StringBuilder uarr = new StringBuilder("abcdefghijklmnopqrstuvwxyz".toUpperCase()).reverse();
+        for(int i = 0; i < word.length(); i++){
+            //System.out.println(word.charAt(i));
+            if(!(word.charAt(i) >= 'a' && word.charAt(i) <= 'z') && !(word.charAt(i) >= 'A' && word.charAt(i) <= 'Z')){
+                answer += word.charAt(i);
+                continue;
+            }
+            if(word.charAt(i) >= 'A' && word.charAt(i) <= 'Z'){//대문자인경우
+                char cur = uarr.charAt((int)(word.charAt(i)-'A'));
+                //System.out.println(word.charAt(i) + " " + cur);
+                answer += cur;
+                continue;
+            }
+            char cur = larr.charAt((int)(word.charAt(i)-'a'));
+            //System.out.println(word.charAt(i) + " " + cur);
+            answer += cur;
+        }
         return answer;
     }
 }
