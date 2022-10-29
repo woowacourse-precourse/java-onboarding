ba0c6a3c0dc989756ad27bc01f90f299b2c3e316
은지 <midzy0228@gmail.com>
2022-10-30 02:01:06 +0900

    feat[Problem4]: 대문자 반대로 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        // 고민
+        // char 를 넣을 것이냐, String 을 넣을 것이냐
         String answer = "";
+        Antonym antonym = new Antonym();
+
+        for (int i=0; i<word.length(); i++){
+            answer += antonym.antonym(word.charAt(i));
+        }
+        // System.out.println(answer);
         return answer;
+    }
+}
+
+class Antonym{
+    public char antonym(char word){
+        char result = ' ';
+        int tmp = 25 - (word-65) * 2;
+        result = (char) (word + tmp);
+        return result;
     }
 }
