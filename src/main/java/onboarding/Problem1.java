b4e6e2edd759c85a73d0b9275f3df035ce20cb3e
jasoncho1021 <jhcho1021@gmail.com>
2022-10-29 20:31:37 +0900

    feat(problem1): add calculating and comparing score

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,19 +7,25 @@ class Problem1 {
     private static final String PAGE_OUT_BOUND_EXCEPTION_MSG = "page is out of bound";
     private static final String INVALID_PAGE_GAP_EXCEPTION_MSG = "page gap is invalid";
     private static final String INVALID_PAGE_ORDER_EXCEPTION_MSG = "page order is invalid";
+    private static final int LEFT_PAGE_LOWER_BOUND = 1;
+    private static final int LEFT_PAGE_UPPER_BOUND = 399;
+    private static final int RIGHT_PAGE_LOWER_BOUND = 2;
+    private static final int RIGHT_PAGE_UPPER_BOUND = 400;
 
-    private static final int GAME_EXCEPTION = 2;
-    private static final int leftPageLowerBound = 1;
-    private static final int leftPageUpperBound = 399;
-    private static final int rightPageLowerBound = 2;
-    private static final int rightPageUpperBound = 400;
+    private static final int GAME_POBI = 1;
+    private static final int GAME_CRONG = 2;
+    private static final int GAME_DRAW = 0;
+    private static final int GAME_EXCEPTION = -1;
 
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer;
         try {
             checkPages(pobi);
             checkPages(crong);
+            int pobiScore = getScore(pobi);
+            int crongScore = getScore(crong);
+            answer = whoIsWinner(pobiScore, crongScore);
         } catch (RuntimeException e) {
             answer = GAME_EXCEPTION;
         }
@@ -66,14 +72,14 @@ class Problem1 {
     }
 
     private static void checkLeftPageBound(int leftPage) {
-        if (leftPage >= leftPageLowerBound && leftPage <= leftPageUpperBound) {
+        if (leftPage >= LEFT_PAGE_LOWER_BOUND && leftPage <= LEFT_PAGE_UPPER_BOUND) {
             return;
         }
         throw new RuntimeException(PAGE_OUT_BOUND_EXCEPTION_MSG);
     }
 
     private static void checkRightPageBound(int rightPage) {
-        if (rightPage >= rightPageLowerBound && rightPage <= rightPageUpperBound) {
+        if (rightPage >= RIGHT_PAGE_LOWER_BOUND && rightPage <= RIGHT_PAGE_UPPER_BOUND) {
             return;
         }
         throw new RuntimeException(PAGE_OUT_BOUND_EXCEPTION_MSG);
@@ -91,5 +97,39 @@ class Problem1 {
             return;
         }
         throw new RuntimeException(INVALID_PAGE_GAP_EXCEPTION_MSG);
+    }
+
+    private static int getScore(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        int leftPageSum = getBiggerSum(leftPage);
+        int rightPageSum = getBiggerSum(rightPage);
+
+        return leftPageSum > rightPageSum ? leftPageSum : rightPageSum;
+    }
+
+    private static int getBiggerSum(int page) {
+        int addSum = 0;
+        int mulSum = 1;
+        int digit;
+
+        while (page > 0) {
+            digit = page % 10;
+            addSum += digit;
+            mulSum *= digit;
+            page = page / 10;
+        }
+
+        return addSum > mulSum ? addSum : mulSum;
+    }
+
+    private static int whoIsWinner(int pobiScore, int crongScore) {
+        if (pobiScore > crongScore) {
+            return GAME_POBI;
+        }
+        if (pobiScore < crongScore) {
+            return GAME_CRONG;
+        }
+        return GAME_DRAW;
     }
 }
