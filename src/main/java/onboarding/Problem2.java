816418c70d3d86dc86080d1c15f91e9a5dbab8f9
Yejin Shin <shinyejin0212@gmail.com>
2022-10-29 21:35:04 +0900

    refactor(problem2): code refactoring

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,34 +5,40 @@ import java.util.List;
 
 public class Problem2 {
 
+    // 중복 문자열 제거 기능
     public static List<Character> dltRedund(String cryptogram){
         int i = 0;
-        List<Character> temp = new ArrayList<>();
-        char prev = '0';
-        boolean end = false;
+        List<Character> temp = new ArrayList<>(); // 중복이 제거된 결과가 들어갈 List
+        char prev = '0'; // 비교를 위한 이전값
+
+        // cryptogram 길이만큼만 수행
         while(i < cryptogram.length()) {
-            char item = cryptogram.charAt(i);
-            if(i==0){
-                temp.add(item);
-                prev = item;
+            char item = cryptogram.charAt(i); // 비교할 현재 값 item
+            if(i==0){ // 만약 첫번째 문자라면 
+                temp.add(item); // temp에 일단 넣는다.
+                prev = item; // 다음 문자와의 비교를 위해 현재값을 prev에 저장
             }
-            else {
-                if (item == prev){
-                    if (item == temp.get(temp.size()-1)) {
+            else { // 첫번째 문자가 아니라면
+                if (item == prev){// 만약 현재값이 이전값과 동일할때
+                    // 현재값이 temp의 마지막 문자와도 동일하다면 temp의 마지막 문자 삭제
+                    if (item == temp.get(temp.size()-1)) { 
                         temp.remove(temp.size()-1);
                         //temp.add(item);
                         prev = item;}
+                    //temp의 마지막 문자와 일치하지 않으면 temp의 마지막 문자 삭제하지 않는다.
                 }
-                else {
-                    temp.add(item);
-                    prev = temp.get(temp.size()-1);
+                else { // 현재값과 이전값이 동일하지 않다면
+                    temp.add(item); //temp에 해당문자 삽입한다.
+                    prev = temp.get(temp.size()-1); // 다음 비교를 위해 삽입한 문자를 prev에 저장한다.
                 }
 
             }
-            i++;
+            i++; //index +1
         }
         return temp;
     }
+
+    // 원래의 값(String)과 비교를 위해 List형태를 String으로 변경하는 기능.
     public static String listToString(List<Character> temp){
         // char list to string
         String str = "";
@@ -41,26 +47,31 @@ public class Problem2 {
         }
         return str;
     }
+    
+    //만약
     public static boolean checkEnd(String str, String cryptogram){
+        // 만약 원래의 값과 중복삭제기능을 수행한 후의 값이 일치한다면
         if(str.equals(cryptogram)) {
-            return true;
-        }
+            return true; // true 반환
+        } // 원래의 값과 중복삭제기능을 수행한 후의 값이 일치하지 않는다면
+        // false 반환
         return false;
     }
 
     public static String solution(String cryptogram) {
-
-        // 0. 재귀 사용해서 끝까지 중복 제거
-        // 1. 중복 삭제 기능
-        // 2. List를 String으로 바꾸는 기능
-        // 3. 원래 값과 비교해서 재귀 끝내는 조건 확인하는 기능
+        
         String answer = "answer";
+        // 중복 삭제 메소드
         List<Character> temp = dltRedund(cryptogram);
 
+        // checkEnd 함수가 true이면
+        // 정답 return 및 재귀에서 빠져나온다.
         if (checkEnd(listToString(temp), cryptogram)){
             answer = listToString(temp);
+            return answer; 
         }
-        else {
+        else { // ckeckEnd 함수가 false이면
+            // 다시 수행 (재귀)
             answer = solution(listToString(temp));
         }
 
