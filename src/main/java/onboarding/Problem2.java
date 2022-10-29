94e38bfa48bd613ffbe7c543552c51201a4069a4
Sojeong <k5k5s5s5j5j5@naver.com>
2022-10-29 20:46:06 +0900

    problem2: returns immediately if list is empty. zyelleyz finished

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -16,19 +16,22 @@ public class Problem2 {
             for (int i = 0; i < cryptoList.size() - 1; i++) {
                 String str1 = cryptoList.get(i);
                 String str2 = cryptoList.get(i + 1);
-                if (str1.equals(str2)) {
-                    i++;
-                }
-                else cryptoListCopy.add(cryptoList.get(i));
+                if (str1.equals(str2))
+                    i += 1;
+                else
+                    cryptoListCopy.add(cryptoList.get(i));
             }
-            cryptoListCopy.add(cryptoList.get(cryptoList.size() - 1));
+            if (!cryptoList.get(cryptoList.size() - 2).equals(cryptoList.get(cryptoList.size() - 1)))
+                cryptoListCopy.add(cryptoList.get(cryptoList.size() - 1));
 
-
-            if (cryptoList.equals(cryptoListCopy)) changed = false;
-            else changed = true;
+            if (cryptoList.equals(cryptoListCopy))
+                changed = false;
+            else
+                changed = true;
             cryptoList = cryptoListCopy;
+            if (cryptoList.isEmpty())
+                break;
         }
-
         return cryptoList.toString();
     }
 
