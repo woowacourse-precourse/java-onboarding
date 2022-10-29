b93001ef2eb5d9126184a2de2256166f1e5cc9dd
desfox <desfox99@naver.com>
2022-10-30 01:08:23 +0900

    feat(Problem1): 사용자 점수 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,22 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static final int LEFT_IDX = 0;
+    public static final int RIGHT_IDX = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static int getUserScore(List<Integer> pageList) {
+        int leftPage = pageList.get(LEFT_IDX);
+        int rightPage = pageList.get(RIGHT_IDX);
+
+        int leftScore = getLargerValue(leftPage);
+        int rightScore = getLargerValue(rightPage);
+
+        return Math.max(leftScore, rightScore);
     }
 
     public static int getLargerValue(int page) {
