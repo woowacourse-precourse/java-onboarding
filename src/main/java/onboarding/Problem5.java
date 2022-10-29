87a0ad3beacfbb860c4bf5ea8ab843cb928aef08
yejin <kwonyj1022@naver.com>
2022-10-30 03:24:51 +0900

    solution 메서드 : answer 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,34 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        if ((money < 1) || (money > 1000000)){
+            return answer;
+        }
+        answer.set(0, money / 50000);
+        money %= 50000;
+        answer.set(1, money / 10000);
+        money %= 10000;
+        answer.set(2, money / 5000);
+        money %= 5000;
+        answer.set(3, money / 1000);
+        money %= 1000;
+        answer.set(4, money / 500);
+        money %= 500;
+        answer.set(5, money / 100);
+        money %= 100;
+        answer.set(6, money / 50);
+        money %= 50;
+        answer.set(7, money / 10);
+        money %= 10;
+        answer.set(8, money / 1);
+        money %= 1;
+
         return answer;
     }
 }
