d112a11af77b706b1a788606d023ce0f6f955571
song-wooseok <sws981202@gmail.com>
2022-10-29 21:18:36 +0900

    feat: [PROBLEM2 - 가능2] 주어진 문자열에 중복된 부분이 있는지 검증하는 기능

--- src/main/java/onboarding/problem2/service/DecryptionService.java
+++ src/main/java/onboarding/problem2/service/DecryptionService.java
@@ -7,6 +7,25 @@ public class DecryptionService {
 
     }
 
+    public static boolean checkDecryption(String word){
+        if(word.length() != 0){
+            return isConsecutiveChar(word);
+        }
+        return false;
+    }
+
+    // 문자열 안에 중복된 부분이 있는지 검사
+    public static boolean isConsecutiveChar(String word){
+        char checkChar = word.charAt(0);
+        for(int index = 1; index < word.length(); index++){
+            if(compareWord(checkChar, word.charAt(index))){
+                return true;
+            }
+            checkChar = word.charAt(index);
+        }
+        return false;
+    }
+
     public static boolean compareWord(char c1, char c2){
         return (c1 == c2);
     }
