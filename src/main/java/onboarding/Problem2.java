6f81c514e37680c9253d59eefabba01708b1f273
jiwon <ljwon77@daum.net>
2022-10-29 20:11:17 +0900

    feat(problem2): split string into array

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        // 입력으로 들어온 문자열을 char형 배열로 분해한다.
+        char[] cryptogramArr = getCharArr(cryptogram);
+
+        return cryptogram;
+    }
+
+    /**
+     * 파라미터로 받은 문자열을 char형 배열로 분해하여 리턴한다.
+     *
+     * @param str 문자열
+     * @return char[] - 입력으로 받은 문자열에 대해 char형 배열로 쪼갠 결과를 리턴한다.
+     */
+    private static char[] getCharArr(String str) {
+        return str.toCharArray();
     }
 }
