ac94d65042f49ad6e26e36df53793c315b065684
shpark30 <seohyeonpark30.gmail.com>
2022-10-29 17:55:16 +0900

    feat(Problem6.java): 문자열 2글자씩 분절하는 기능 추가
    문자열을 1글자씩 Loop 돌면서 다음 글자까지 새로운 List에 add

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,11 +10,25 @@ public class Problem6 {
     HashMap<String, ArrayList<Integer>> charToIndices = new HashMap<String, ArrayList<Integer>>();
     public static List<String> solution(List<List<String>> forms) {
         // 탐색 Loop forms
+        for (int i = 0; i < forms.size(); i++) {
             // updateEvery2SequentialChar : 닉네임에서 2글자씩 분절해  charToIndices에 추가
+            updateEvery2SequentialChar(i, forms.get(i).get(1));
+        }
+
         // charToIndices에서 길이가 2이상인 Index 배열 반환
+        List<Integer> duplicateIndices = List.of(0);
         // answer 배열 생성
         List<String> answer = List.of("answer");
         // Index 배열을 이용해 aswer에 email add
         return answer;
+    }
+
+    static void updateEvery2SequentialChar(int formIdx, String nickname) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String key = nickname.substring(i, i + 2);
+            ArrayList<Integer> formIndices = this.charToIndices.get(key);
+            formIndices.add(formIdx);
+            this.charToIndices.put(key, formIndices);
+        }
     }
 }
