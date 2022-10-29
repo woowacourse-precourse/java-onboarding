145d979258079602c499c8b736af5ea580763e68
hyejungg <kimhyejung12@naver.com>
2022-10-30 00:08:10 +0900

    feat: [문제 2] 입력값 검증 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,18 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+	private static boolean isValidLowerCase(String cryptogram) {
+		for (int i = 0; i < cryptogram.length(); i++) {
+			if (!Character.isLowerCase(cryptogram.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isValidLength(String cryptogram) {
+		return cryptogram.length() <= 1000;
+	}
+
 }
