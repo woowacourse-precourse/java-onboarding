3406041e4bd873975a242a3badba52627ba354e9
kkkwp <chxrryda@gmail.com>
2022-10-29 23:05:08 +0900

    feat(findMax): 페이지 자릿수의 덧셈과 곱셈 중 더 큰 값 찾는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,14 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    static int ERROR_NUM = -1;
     static int FIRST_PAGE = 1;
     static int LAST_PAGE = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        try {
+            checkPages(pobi);
+            checkPages(crong);
+        } catch (Exception e) {
+            //System.out.print(e.getMessage());
+            answer = ERROR_NUM;
+        }
         return answer;
+    }
+
+    public static int findMax(int num) {
+        List<Integer> digits = new ArrayList<>();
+
+        while (num > 0) {
+            digits.add(num % 10);
+            num /= 10;
+        }
+
+        int sumDigits = digits.stream()
+                .reduce(0, Integer::sum);
+        int mulDigits = digits.stream()
+                .reduce(1, (a, b) -> a * b);
+
+        return Math.max(sumDigits, mulDigits);
     }
 
     public static void checkPages(List<Integer> pages) throws Exception {
