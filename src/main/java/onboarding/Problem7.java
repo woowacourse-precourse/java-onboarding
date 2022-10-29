5c2c2e67c2ec53bd244a89952003ab60b39b6331
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-29 18:33:27 +0900

    [Problem 7] 기존 친구들을 식별하기 위한 set을 설정하는 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -17,6 +17,7 @@ public class Problem7 {
         oldFriend.add(user);
         friendData=friends;
         List<String> olderFriends = getFriendOfUser(user);
+        classifyOlderFriend(olderFriends);
     }
 
     private static List<String> getFriendOfUser(String user) {
@@ -28,6 +29,10 @@ public class Problem7 {
             }
         }
         return friendList;
+    }
+
+    private static void classifyOlderFriend(List<String> oldfriends){
+        oldfriends.stream().forEach(a->oldFriend.add(a));
     }
 
 }
