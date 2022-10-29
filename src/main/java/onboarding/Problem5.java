8fbcd1958114587095aab31b7835b6cf23bfb0b3
c4fiber <qudcjf153@gmail.com>
2022-10-29 16:05:39 +0900

    fix(prob5): 화폐 개수를 얻는 기능을 개별 함수로 분리
    1. solution 내부를 최대한 간단하게 보이기 위해 기능을 분리함
    2. 화폐 단위와 금액을 받아 기능을 수행하는 것도 생각해 보았으나 solution 내부에서 화폐 단위를 변수에 저장하고 이용해야 하므로 가독성이 떨어질 가능성이 있음.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,16 +9,21 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
 
         if (1 <= money && money <= 1_000_000) {
-            int[] divider = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-
-            ArrayList<Integer> temp = new ArrayList<Integer>();
-            for (int i : divider) {
-                temp.add(Integer.valueOf(money / i));
-                money %= i;
-            }
-            answer = temp;
+            answer = getAllNumOfCurrency(money);
         }
 
         return answer;
+    }
+    
+    private static ArrayList<Integer> getAllNumOfCurrency(int money) {
+        int[] divider = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        for (int i : divider) {
+            temp.add(Integer.valueOf(money / i));
+            money %= i;
+        }
+
+        return temp;
     }
 }
