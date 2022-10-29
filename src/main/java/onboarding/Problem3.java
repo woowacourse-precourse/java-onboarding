785e13bb9fa11970ba58308bea251b2957b1ecc6
dohyun <ldh19738@gmail.com>
2022-10-29 17:34:36 +0900

    Problem3 number의 범위 사용자 정의 예외 클래스로 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import onboarding.PROBLEM3.NumberRangeException;
+
 public class Problem3 {
-    public static int solution(int number) {
+    public static int solution(int number) throws NumberRangeException {
         int answer = 0;
+
+        // number 범위 초과하는 경우 예외 처리
+        if (number<1 || number>10000) {
+            throw new NumberRangeException("[Error] 범위초과");
+        }
 
         for (int i=1; i<=number; i++) {
             answer += clap(i);
@@ -10,6 +17,7 @@ public class Problem3 {
         return answer;
     }
 
+    // current에서 박수를 치는지에 대한 여부
     public static int clap(int current) {
         int count = 0;
 
