23d9564a354be23962544c8639d56568b6ac9b20
Lee Min Woo <lmw7414@naver.com>
2022-10-29 14:33:30 +0900

    problem6 - 기능 요소 설계
     일차적으로 필요한 기능을 적어보았다.
    
     *  객체 사용 Crew(부모 키, 이메일, 이름)
     *  부모 키의 경우 중복된 이름을 찾는데 도움을 줄 수 있음
     *  ex) 제이슨, 제이엠의 경우 동일한 부모키를 갖는다.
     *  이름을 두글자씩 자른 후 이것을 해시셋으로 저장
     *  ㄴ 객체로 저장 TwoLetters(2글자 이름, 부모 키)
     *  해시셋에 존재한다면 Crew의 부모키를 TwoLetters 부모키로 변경
     *  이메일을 오름차순으로 정렬하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,15 @@ package onboarding;
 
 import java.util.List;
 
+/** 구현해야할 기능
+ *  객체 사용 Crew(부모 키, 이메일, 이름)
+ *  부모 키의 경우 중복된 이름을 찾는데 도움을 줄 수 있음
+ *  ex) 제이슨, 제이엠의 경우 동일한 부모키를 갖는다.
+ *  이름을 두글자씩 자른 후 이것을 해시셋으로 저장
+ *  ㄴ 객체로 저장 TwoLetters(2글자 이름, 부모 키)
+ *  해시셋에 존재한다면 Crew의 부모키를 TwoLetters 부모키로 변경
+ *  이메일을 오름차순으로 정렬하는 기능
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
