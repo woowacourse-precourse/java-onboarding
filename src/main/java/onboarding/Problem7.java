83966887f7daecf306ed9b5ad990544afca1c4c0
eastsage <dpwk8@naver.com>
2022-10-30 01:29:12 +0900

    feat: [problem7] 입력값의 친구를 반환하는 함수 구현
    getMyFriends 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    Map<String, Integer> scoreboard = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    static List<String> getMyFriends(String user, List<List<String>> friends) {
+        List<String> myFriends = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                myFriends.addAll(friend);
+                myFriends.remove(user);
+            }
+        }
+        return myFriends;
+    }
+
+    static List<String> getMyFriends(List<String> users, List<List<String>> friends) {
+        List<String> myFriends = new ArrayList<>();
+        for (String user : users) {
+            myFriends.addAll(getMyFriends(user, friends));
+        }
+
+        return myFriends;
+    }
+    
+
 }
