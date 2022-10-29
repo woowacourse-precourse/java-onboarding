9365dac7f2d0ed7a01ee35e7f2856565320d1cfb
soyunim <togksk123@naver.com>
2022-10-29 22:27:32 +0900

    Problem2 2)get the Index where starts duplication

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,8 +8,18 @@ public class Problem2 {
         String[] str = cryptogram.split("");
         List<String> strList = new ArrayList<>();
 
+        int startIdx=0;
+
         for(int i=0; i<str.length; i++){
             strList.add(str[i]);
+        }
+
+        // duplication start Index
+        for (int i = 0; i < strList.size()-1; i++) {
+            if(strList.get(i).equals(strList.get(i+1))){
+                startIdx=i;
+                break;
+            }
         }
 
         return answer;
