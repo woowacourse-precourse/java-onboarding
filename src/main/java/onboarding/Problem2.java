3d1e3c5c872e63208dff7c8c62e232d85237b73a
goldcollar95 <yjs7298@naver.com>
2022-10-29 18:52:27 +0900

    문제2번

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (cryptogram.indexOf(cryptogram.charAt(i)) == i)	//i의 문자열을 다 가져온다, 같은것을 꺼내온다
+                answer += cryptogram.charAt(i);
+
+        }
         return answer;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String cryptogram = scanner.next();
+        System.out.println(solution(cryptogram));
     }
 }
