a44811288547dd87d4da2ddd70bd446eda33bf1d
xx10222 <yjw4960@gmail.com>
2022-10-29 19:50:55 +0900

    :bug: [Problem2] Solve the Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,31 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while(true) {
+            String removeString = removeDuplicate(cryptogram);
+            if(cryptogram.equals(removeString)) {
+                break;
+            }
+            cryptogram = removeString;
+        }
+        return cryptogram;
+    }
+
+    private static String removeDuplicate(String str) {
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        char prev = 0;
+        int i = 0;
+
+        for(char c : chars) {
+            if(prev!=c) {
+                chars[i++] = c;
+                prev = c;
+                sb.append(c);
+            } else {
+                sb.delete(sb.length()-1, sb.length());
+            }
+        }
+        return sb.toString();
     }
 }
