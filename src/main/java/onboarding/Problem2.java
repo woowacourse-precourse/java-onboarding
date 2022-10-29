8bc7633795c1f67e21bb08562dd5915587c09be6
zcae1020 <zcae1020@naver.com>
2022-10-29 21:01:39 +0900

    feat(Problem2): nextString method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,7 +8,11 @@ public class Problem2 {
 
     //연속된 char 하나 삭제 후 return | 연속된 char가 없다면 그대로 return
     private static String nextString(String str){
-
+        for(int i=0;i<str.length()-1;i++){
+            if(str.charAt(i)==str.charAt(i+1))
+                return removeChar(str, i);
+        }
+        return str;
     }
 
     //str에서 idx번째 char 삭제
