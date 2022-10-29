de0be83a08ffaf5b778a1dc935e4e8b668096938
soyunim <togksk123@naver.com>
2022-10-29 22:26:17 +0900

    Problem2 1)put the cryptogram in the list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        String[] str = cryptogram.split("");
+        List<String> strList = new ArrayList<>();
+
+        for(int i=0; i<str.length; i++){
+            strList.add(str[i]);
+        }
+
         return answer;
     }
 }
