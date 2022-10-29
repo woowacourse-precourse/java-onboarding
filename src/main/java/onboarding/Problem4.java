1b73c1e8fa72495421da5d664e7f2f92192b667d
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 06:46:45 +0900

    feat : Problem4 제한사항 확인 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        try {
+            isValidWord(word);
+        } catch (IllegalStateException illegalStateException) {
+            illegalStateException.printStackTrace();
+        }
+
         return answer;
+    }
+
+    public static void isValidWord(String word) throws IllegalStateException {
+        if (word.length() < 1 || word.length() > 1000) {
+            throw new IllegalStateException("word의 길이가 올바르지 않습니다.");
+        }
     }
 }
