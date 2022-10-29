f9d811a0edfe127c2a5030184c3f6d8bbd6ecc10
Kid-Chang <pch3310@gmail.com>
2022-10-29 19:18:08 +0900

    refactor: code refactor in problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,20 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        int duplicate=1;
-        do{
+        int duplicate = 1;
+        do {
             char[] answer = cryptogram.toCharArray();
             duplicate = 0;
-            for(int i=0; i<cryptogram.length()-1; i++){
-                if(answer[i] == answer[i+1]){
+            for (int i = 0; i < cryptogram.length() - 1; i++) {
+                if (answer[i] == answer[i + 1]) {
                     answer[i] = ' ';
-                    answer[i+1] = ' ';
-                    duplicate=1;
+                    answer[i + 1] = ' ';
+                    duplicate = 1;
                 }
             }
             cryptogram = new String(answer).replace(" ", "");
         }
-        while(duplicate == 1);
+        while (duplicate == 1);
         return String.valueOf(cryptogram);
     }
 }
