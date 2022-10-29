fd550803fec5005d0537ab2f2a19e3ecb2d43a74
beombu <sunil1369@bible.ac.kr>
2022-10-29 22:14:14 +0900

    Feat(P2) : 중복문자열 제거 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
@@ -9,7 +11,17 @@ public class Problem2 {
     /**
      * 중복된 문자열을 제거하는 기능
      */
-    private String deleteDuplication(String str){
-        return null;
+    public static Stack<Character> deleteDuplication(String str){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            if(!stack.isEmpty() && stack.peek() == str.charAt(i)){
+                stack.pop();
+            }else{
+                stack.push(str.charAt(i));
+            }
+        }
+
+        return stack;
     }
 }
