cbc3344384e742aa6b95dc064092861764aaff62
LEE-sh1673 <lsh901673@gmail.com>
2022-10-29 20:50:12 +0900

    feat(NickNameValidator): 포함된 단어 횟수 계산 기능 추가

--- src/main/java/onboarding/nicknamevalidator/NickNameValidator.java
+++ src/main/java/onboarding/nicknamevalidator/NickNameValidator.java
@@ -1,9 +1,28 @@
 package onboarding.nicknamevalidator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NickNameValidator {
+
+	private final Map<String, Integer> occurrences;
+
+	public NickNameValidator(List<List<String>> forms) {
+		occurrences = new HashMap<>();
+		for (List<String> form : forms) {
+			initMapWith(form);
+		}
+	}
+
+	private void initMapWith(final List<String> form) {
+		for (String word : getPermutationOf(form.get(1))) {
+			occurrences.merge(word, 1,
+				(occurrences, ignored) -> occurrences+1);
+		}
+	}
+
 	public static List<String> getPermutationOf(final String word) {
 		List<String> permutation = new ArrayList<>();
 		for (int i = 0; i < word.length(); i++) {
@@ -18,5 +37,9 @@ public class NickNameValidator {
 			words.add(word.substring(startIndex, i+1));
 		}
 		return words;
+	}
+
+	public int getOccurrenceOf(final String word) {
+		return occurrences.getOrDefault(word, 0);
 	}
 }
--- src/test/java/onboarding/nicknamevalidator/NickNameValidatorTest.java
+++ src/test/java/onboarding/nicknamevalidator/NickNameValidatorTest.java
@@ -9,6 +9,14 @@ import org.junit.jupiter.api.Test;
 
 public class NickNameValidatorTest {
 
+	static List<List<String>> forms = List.of(
+		List.of("jm@email.com", "제이엠"),
+		List.of("jason@email.com", "제이슨"),
+		List.of("woniee@email.com", "워니"),
+		List.of("mj@email.com", "엠제이"),
+		List.of("nowm@email.com", "이제엠")
+	);
+
 	@DisplayName("닉네임 문자 순열 생성 테스트")
 	@Test
 	void 닉네임_문자_순열_생성() {
@@ -23,5 +31,17 @@ public class NickNameValidatorTest {
 		
 		assertThat(NickNameValidator.getPermutationOf("제이엠"))
 			.isEqualTo(List.of("제이", "제이엠", "이엠"));
+	}
+
+	@DisplayName("포함된 단어 횟수 반환 테스트")
+	@Test
+	void 단어_포함_횟수() {
+		NickNameValidator validator = new NickNameValidator(forms);
+		assertThat(validator.getOccurrenceOf("제이"))
+			.isEqualTo(3);
+		assertThat(validator.getOccurrenceOf("이엠"))
+			.isEqualTo(1);
+		assertThat(validator.getOccurrenceOf("이엠제"))
+			.isEqualTo(0);
 	}
 }
