2d9f8ad496d752643d584f67c1a31ba6f68a4f58
KyungKyu Park <rudrb0505@gmail.com>
2022-10-29 19:45:53 +0900

    feat(Problem6): 닉네임과 이메일을 입력받는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,15 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		HashMap<String, String> crewHashData = new HashMap<>();
+		for (List<String> form : forms) {
+			crewHashData.put(form.get(0), form.get(1));
+		}
+		List<String> answer = List.of("answer");
+		return answer;
+	}
 }
