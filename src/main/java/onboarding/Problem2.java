968aaeeb50f27cd7185f08b07fed963c26c97386
chkim <cndghks0213@gmail.com>
2022-10-30 06:10:30 +0900

    feat(problem2): 문자열에서 연속하는 중복 문자들을 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem2 {
+    private static final String PATTERN = "([a-z])\\1{1,}";
+    private static final String SPACE = "";
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static String removeConsecutiveStrings(String cryptogram) {
+        return cryptogram.replaceAll(PATTERN, SPACE);
     }
 }
