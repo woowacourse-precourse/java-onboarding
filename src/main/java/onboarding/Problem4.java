e08178d0af816781682695cf1cd65d627c1f3c83
Invidam <hansupark0208@gmail.com>
2022-10-29 18:49:35 +0900

    feat(Problem4): add character transform method
    if lowercase call, get result from uppercase character and return make result to lowercase.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    public static char transformChar(char ch) {
+        if(Character.isLowerCase(ch)) {
+            char diff = (char) ('A' - 'a');
+
+            return Character.toLowerCase(transformChar(Character.toUpperCase(ch)));
+        }
+
+        int MID_COUNT = ('Z' - 'A' )/ 2;
+        char startChar = 'A';
+        char endChar = 'Z';
+
+        if(ch < MID_COUNT) {
+            return (char) (ch - startChar + endChar);
+        } else {
+            return (char) (endChar - ch + startChar);
+        }
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
