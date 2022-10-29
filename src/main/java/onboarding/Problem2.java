fc7f291bdaf97ce51c492a4f89afcd8366f9d7dc
honey170 <63653475+hingjae@users.noreply.github.com>
2022-10-30 01:44:13 +0900

    problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        StringBuffer sb = new StringBuffer();
+        sb.append(cryptogram);
+        int i = 1;
+        while(i < sb.length()){
+            if(sb.charAt(i) != sb.charAt(i - 1)){
+                i++;
+                continue;
+            }
+            sb.delete(i-1, i+1);
+            i = 1;
+        }
+        answer = sb.toString();
         return answer;
     }
 }
