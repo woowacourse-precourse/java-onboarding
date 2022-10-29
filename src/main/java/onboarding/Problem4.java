6caa918131a92e2d73bb5e655ea4b9ce491df4df
Noah <shgustmd6865@naver.com>
2022-10-29 21:45:43 +0900

    Feat : 문자열의 각 요소에 접근하는 기능 추가
    반복문을 이용해 문자열을 char[]로 변형하여 각 요소에 대해서 접근할 수 있는 기능입니다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (char element : word.toCharArray()) {
+
+        }
         return answer;
     }
 }
