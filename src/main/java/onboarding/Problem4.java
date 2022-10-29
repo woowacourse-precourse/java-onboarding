685d5dd133a3b19f4bc6e240bfc36bd6aeb75633
박원종 <wonjong1030@naver.com>
2022-10-29 22:44:59 +0900

    feat: 반대문자 저장하는 배열 초기화

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] upper = new char[26];
+        char[] lower = new char[26];
+        for(int i=25, j=0; i>=0; i--,j++){
+            upper[i] = (char)('A'+j);
+            lower[i] = (char)('a'+j);
+        }
         return answer;
     }
 }
