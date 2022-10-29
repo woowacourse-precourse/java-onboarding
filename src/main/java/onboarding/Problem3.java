987f75cb18ea4ecb0113669715b078c8b5bbbccb
chopinballadeno4 <qo98333@naver.com>
2022-10-29 11:02:47 +0900

    problem 3, function 2 calculate result

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -17,6 +17,9 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1 ; i <= number ; i++) {
+            answer = answer + countNumber(i);
+        }
         return answer;
     }
 }
