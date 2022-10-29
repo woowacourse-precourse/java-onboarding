d94b98a002da1cbf9176f4fb85992ede879f622b
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-29 13:00:24 +0900

    feat(problem5): 문제 해결

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -159,14 +159,14 @@ class ApplicationTest {
     @Nested
     class Problem5Test {
         @Test
-        void case1() {
+        void case1() throws Exception {
             int money = 50_237;
             List<Integer> result = List.of(1, 0, 0, 0, 0, 2, 0, 3, 7);
             assertThat(Problem5.solution(money)).isEqualTo(result);
         }
 
         @Test
-        void case2() {
+        void case2() throws Exception {
             int money = 15_000;
             List<Integer> result = List.of(0, 1, 1, 0, 0, 0, 0, 0, 0);
             assertThat(Problem5.solution(money)).isEqualTo(result);
