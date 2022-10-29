06d69e96d0446c765fea7a9ccd6fc8aa6c349d16
apptie <appti@naver.com>
2022-10-29 23:16:57 +0900

    refactor: 문제 5 moneyTypes 클래스 변수로 변경

--- src/main/java/onboarding/problem5/MoneyCalculator.java
+++ src/main/java/onboarding/problem5/MoneyCalculator.java
@@ -11,22 +11,24 @@ import static onboarding.problem5.consts.MoneyConst.TEN_THOUSAND_WON;
 import static onboarding.problem5.consts.MoneyConst.TEN_WON;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MoneyCalculator {
+
+    private static int[] moneyTypes = {FIFTY_THOUSAND_WON, TEN_THOUSAND_WON, FIVE_THOUSAND_WON,
+        ONE_THOUSAND_WON, FIVE_HUNDRED_WON, ONE_HUNDRED_WON, FIFTY_WON, TEN_WON, ONE_WON};
 
     private MoneyCalculator() {
     }
 
     public static List<Integer> calculateMoneyCount(int money) {
-        int[] moneyTypes = {FIFTY_THOUSAND_WON, TEN_THOUSAND_WON, FIVE_THOUSAND_WON,
-            ONE_THOUSAND_WON, FIVE_HUNDRED_WON, ONE_HUNDRED_WON, FIFTY_WON, TEN_WON, ONE_WON};
-        List<Integer> result = new ArrayList<>();
+        List<Integer> MoneyTypesCount = new ArrayList<>();
 
         for (int moneyType : moneyTypes) {
-            result.add(money / moneyType);
+            MoneyTypesCount.add(money / moneyType);
             money %= moneyType;
         }
-        return result;
+        return MoneyTypesCount;
     }
 }
