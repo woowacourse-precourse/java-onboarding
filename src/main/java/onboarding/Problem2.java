601d8163e8f806876307a7d6d420ed8be694b22c
iamsseaa <homingsea@gmail.com>
2022-10-29 17:13:55 +0900

    feat(Problem2): 제거할 중복 문자의 인덱스 목록을 만드는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +11,29 @@ public class Problem2 {
 
         List<Character> cryptogramList = cryptogram.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
         List<Character> result = new ArrayList<>(cryptogramList);
+
+        int beforeLength = cryptogramList.size();
+        int afterLength = -1;
+
+        while (beforeLength != afterLength) {
+
+            beforeLength = cryptogramList.size();
+            HashSet<Integer> indexSet = new HashSet<>();
+
+            for (int i = 0; i < beforeLength - 1; i++) {
+
+                if (cryptogramList.get(i) == cryptogramList.get(i+1)) {
+                    indexSet.add(i);
+                    indexSet.add(i+1);
+                }
+            }
+
+            
+
+            afterLength = result.size();
+
+            cryptogramList = new ArrayList<>(result);
+        }
 
         String answer = "answer";
         return answer;
