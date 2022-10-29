752030f5401440503f69cf66f38dca786e0d96c2
hectick <chyeon1223@cau.ac.kr>
2022-10-30 02:13:54 +0900

    refactor : [Problem6] 중복되는 닉네임을 보유한 이메일들의 목록을 나타내는 변수 set을 emailSet으로 변수명 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         HashMap<String, String> part = new HashMap<>(); //닉네임 중복 체크를 위해 닉네임의 파편들을 이메일주소와 함께 저장
-        HashSet<String> set = new HashSet<>(); //중복되지 않도록 이메일을 저장
+        HashSet<String> emailSet = new HashSet<>(); //중복되지 않도록 이메일을 저장
 
         for(int i = 0; i < forms.size(); i++){
             String nickname = forms.get(i).get(1);
@@ -17,7 +17,7 @@ public class Problem6 {
             List<String> duplicatedNicknameOwner = checkNicknameDuplication(nickname, email, part);
             //중복된 이메일 저장
             for(int j = 0; j < duplicatedNicknameOwner.size(); j++){
-                set.add(duplicatedNicknameOwner.get(j));
+                emailSet.add(duplicatedNicknameOwner.get(j));
             }
 
             //중복 닉네임이 될 수 있는 단어들(part)을 추출
@@ -31,7 +31,7 @@ public class Problem6 {
             }
         }
         //set을 list로 변경하여 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 메일 목록을 반환하는 기능
-        List<String> answer = new ArrayList<>(set); //set -> list
+        List<String> answer = new ArrayList<>(emailSet); //set -> list
         Collections.sort(answer);
         return answer;
     }
