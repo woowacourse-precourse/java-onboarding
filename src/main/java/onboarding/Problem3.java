1a707083ebbed54bb5db8c3fa1c17a778c2824b9
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-29 18:33:24 +0900

    feat(Problem3): 숫자까지 쳐야하는 손뼉 횟수를 구하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,10 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static final String GAME_NUMBER = "369";
+    public static final int MAX_NUMBER = 10000;
 
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int[] cache = new int[MAX_NUMBER+1];
+        initCache(cache);
+        countClapNumber(cache);
+        return cache[number];
+    }
+
+    public static void initCache(int[] arr){
+        arr[1] = 0;
+        arr[2] = 0;
+    }
+
+    public static void countClapNumber(int[] arr){
+        for(int i=3 ; i <= MAX_NUMBER ; i++){
+            arr[i] = arr[i-1] + countIncludedNumberOfGameNumber(i);
+        }
     }
 
     public static int countIncludedNumberOfGameNumber(int number){
