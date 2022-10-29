521e44f8a5b001fc3e3d811233326ed664a00c45
jagajang <kosb7544@gmail.com>
2022-10-29 12:00:40 +0900

    add score calculation for each user

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,21 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        try {
-            int pScore = maxScore(pobi);
-            int cScore = maxScore(crong);
+        int pScore = maxScore(pobi);
+        int cScore = maxScore(crong);
 
-            if (pScore > cScore)
-                return 1;
-            else if (pScore < cScore)
-                return 2;
-            return 0;
-        } catch(Exception e) {
-            return -1;
-        }
+        if (pScore > cScore)
+            return 1;
+        else if (pScore < cScore)
+            return 2;
+        return 0;
     }
 
-    private static int maxScore(List<Integer> pages) throws Exception {
+    private static int maxScore(List<Integer> pages) {
+        int maxSco = 0;
+
+        for(Integer page : pages)
+            maxSco = Math.max(maxSco, pageScore(page));
+
+        return maxSco;
+    }
+
+    private static int pageScore(Integer page) {
         return 0;
     }
 }
