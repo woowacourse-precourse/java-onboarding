5ea2ef3436d1e75acd405ce99314a6ff60ed20ee
rabbit0216 <rabbit1999k@gmail.com>
2022-10-29 20:12:05 +0900

    feat(PR1): 추가 페이지 범위(1-400) 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,24 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /*
+    포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1
+     */
+    static final int EXCEPTION = -1;
+    static final int POBI = 1;
+    static final int CRONG = 2;
+    static final int DRAW = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(pobi.get(0) <= 1 || pobi.get(0) >= 400 || pobi.get(1) <= 1 || pobi.get(1) >= 400) {
+            return EXCEPTION;
+        }
+        if(crong.get(0) <= 1 || crong.get(0) >= 400 || crong.get(1) <= 1 || crong.get(1) >= 400) {
+            return EXCEPTION;
+        }
+        
         return answer;
     }
 }
