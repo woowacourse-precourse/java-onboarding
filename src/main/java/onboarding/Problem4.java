e042ec2c1db4eb14f64c482ed52ce54120a46b33
math0917 <math0917@naver.com>
2022-10-29 15:01:12 +0900

    feat : 청개구리 변환하는 메써드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import onboarding.problem4.Problem4Validation;
+
 public class Problem4 {
+    private final static int MIN_INPUT = 1;
+    private final static int MAX_INPUT = 1000;
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        if (Problem4Validation.problem4InputValidation(word, MIN_INPUT, MAX_INPUT)) {
+            String answer = wordToReverseWord(word);
+            return answer;
+        } else {
+            return "";
+        }
+
+    }
+
+    private static String wordToReverseWord(String word) {
+        return null;
     }
 }
