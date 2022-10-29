77f6f952bab694974233849fe34ebf1436029d10
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-29 15:33:08 +0900

    refactor :: 메서드 명 알아보기 쉽게 변경, result 리스트 안에 이미 존재하는 이메일 처리하는 메서드 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,17 +6,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class Problem6 {
-    public static void main(String[] args) {
-        System.out.println(solution(List.of(
-                List.of("jm@email.com", "제이엠"),
-                List.of("jason@email.com", "제이슨"),
-                List.of("woniee@email.com", "워니"),
-                List.of("mj@email.com", "엠제이"),
-                List.of("nowm@email.com", "이제엠")
-        )));
-    }
 
-    private class Constants {
+    private static class Constants {
         protected static final int EMAIL = 0;
         protected static final int NICKNAME = 1;
     }
@@ -26,28 +17,26 @@ public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
         initList(forms);
-        return test(forms);
+        return validateUserNickname(forms);
     }
 
     private static void initList(List<List<String>> forms) {
         users.add(forms.get(0));
     }
-    // 제이엠
-    // 엠제이
-    // substring 으로 앞에서 부터 2글짜씩 짤라서 비교하면 될 듯
-    private static List<String> test(List<List<String>> forms) {
+
+    private static List<String> validateUserNickname(List<List<String>> forms) {
         for (int i = 1; i < forms.size(); i++) {
             List<String> user = forms.get(i);
-            boolean b = isLetterWordsInAlreadyExistsNicknames(forms.get(i).get(Constants.NICKNAME));
-            if (b) {
-                if (!isAlreadyExistsEmailInResult(user.get(Constants.EMAIL))) {
+            // 만약 중복되는 닉네임이 있다면
+            if (isLetterWordsInAlreadyExistsNicknames(user.get(Constants.NICKNAME))) {
+                // result 리스트에 이메일이 존재하지 않는다면
+                if (isAlreadyExistsEmailInResult(user.get(Constants.EMAIL))) {
                     result.add(user.get(Constants.EMAIL));
                 }
-            } else {
-                users.add(user);
             }
+            users.add(user);
         }
-
+        // 이메일 기준 오름차순 정렬
         sortList();
         return result;
     }
@@ -58,24 +47,28 @@ public class Problem6 {
             for (List<String> user : users) {
                 String alreadyExistsNickname = user.get(Constants.NICKNAME);
                 if (alreadyExistsNickname.contains(nickname.substring(i, i + 2))) {
-                    if (!isAlreadyExistsEmailInResult(user.get(Constants.EMAIL))) {
-                        result.add(user.get(Constants.EMAIL));
-                    }
+                    // 이미 존재하는 닉네임이라면 users 안에 있는 이메일도 result 에 저장해야함
+                    ifAlreadyExistsEmailInResultAndSave(user);
                     return true;
                 }
             }
         }
-
         return false;
+    }
+
+    private static void ifAlreadyExistsEmailInResultAndSave(List<String> user) {
+        if (isAlreadyExistsEmailInResult(user.get(Constants.EMAIL))) {
+            result.add(user.get(Constants.EMAIL));
+        }
     }
 
     private static boolean isAlreadyExistsEmailInResult(String email) {
         for (String alreadyExistsEmail : result) {
             if (Objects.equals(alreadyExistsEmail, email)) {
-                return true;
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     private static void sortList() {
