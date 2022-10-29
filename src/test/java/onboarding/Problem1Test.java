b20f025a200e27a6aaaee12cb0d57c26821eb786
JUN <chltmdwns96@naver.com>
2022-10-29 23:06:52 +0900

    refactor : solutionTest 수정
    - soutv에서 Assertions 검증 방식으로 변경
    - 코드 라인 정리
    - 사용하지 않는 임포트 제거

--- src/test/java/onboarding/Problem1Test.java
+++ src/test/java/onboarding/Problem1Test.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,42 +8,38 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 
 class Problem1Test {
 
     @Test
-    void solutionTest(){
+    void solutionTest() {
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
-        int pobiPage = random.nextInt(197) * 2 + 1;
-        int crongPage = random.nextInt(197) * 2 + 1;
 
         // pobi == crong
-        List<Integer> pobi1 = new ArrayList<>(Arrays.asList(123,124));
-        List<Integer> crong1 = new ArrayList<>(Arrays.asList(123,124));
+        List<Integer> pobi1 = new ArrayList<>(Arrays.asList(123, 124));
+        List<Integer> crong1 = new ArrayList<>(Arrays.asList(123, 124));
 
         // pobi > crong
-        List<Integer> pobi2 = new ArrayList<>(Arrays.asList(197,198));
-        List<Integer> crong2 = new ArrayList<>(Arrays.asList(11,12));
+        List<Integer> pobi2 = new ArrayList<>(Arrays.asList(197, 198));
+        List<Integer> crong2 = new ArrayList<>(Arrays.asList(11, 12));
 
         // pobi < crong
-        List<Integer> pobi3 = new ArrayList<>(Arrays.asList(11,12));
-        List<Integer> crong3 = new ArrayList<>(Arrays.asList(167,168));
+        List<Integer> pobi3 = new ArrayList<>(Arrays.asList(11, 12));
+        List<Integer> crong3 = new ArrayList<>(Arrays.asList(167, 168));
 
         // exception
-        List<Integer> pobi4 = new ArrayList<>(Arrays.asList(122,123));
-        List<Integer> crong4 = new ArrayList<>(Arrays.asList(0,1));
+        List<Integer> pobi4 = new ArrayList<>(Arrays.asList(122, 123));
+        List<Integer> crong4 = new ArrayList<>(Arrays.asList(0, 1));
 
-        // random
-        List<Integer> pobi5 = new ArrayList<>(Arrays.asList(pobiPage,pobiPage+1));
-        List<Integer> crong5 = new ArrayList<>(Arrays.asList(crongPage,crongPage+1));
-
-        System.out.println("pobi == crong => " + Problem1.solution(pobi1,crong1));
-        System.out.println("pobi > crong => " + Problem1.solution(pobi2,crong2));
-        System.out.println("pobi < crong => " + Problem1.solution(pobi3,crong3));
-        System.out.println("exception Result => " + Problem1.solution(pobi4,crong4));
-        System.out.println("random Result => " + Problem1.solution(pobi5,crong5));
+        //result
+        int result1 = Problem1.solution(pobi1, crong1);
+        int result2 = Problem1.solution(pobi2, crong2);
+        int result3 = Problem1.solution(pobi3, crong3);
+        int result4 = Problem1.solution(pobi4, crong4);
+        Assertions.assertThat(result1).isEqualTo(0);
+        Assertions.assertThat(result2).isEqualTo(1);
+        Assertions.assertThat(result3).isEqualTo(2);
+        Assertions.assertThat(result4).isEqualTo(-1);
     }
 }
