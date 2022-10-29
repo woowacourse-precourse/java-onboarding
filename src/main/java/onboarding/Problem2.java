817a80569a3de2d72eb469efb8bdc01139ab0b39
박원종 <wonjong1030@naver.com>
2022-10-29 22:25:55 +0900

    feat: 중복 문자가 없을 때 까지 반복하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,25 +2,32 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        StringBuilder sb = new StringBuilder();
-        char std =cryptogram.charAt(0);
-        int cnt=1;
-        for(int i=1; i<cryptogram.length(); i++){
-            char cur = cryptogram.charAt(i);
-            if(std != cur){
-                if(cnt == 1)
-                    sb.append(std);
-                std = cur;
-                cnt=1;
-                if(i == cryptogram.length()-1)
-                    sb.append(cur);
+        while(true){
+            boolean isFinish = true;
+            StringBuilder sb = new StringBuilder();
+            char std =cryptogram.charAt(0);
+            int cnt=1;
+            for(int i=1; i<cryptogram.length(); i++){
+                char cur = cryptogram.charAt(i);
+                if(std != cur){
+                    if(cnt == 1)
+                        sb.append(std);
+                    std = cur;
+                    cnt=1;
+                    if(i == cryptogram.length()-1)
+                        sb.append(cur);
+                }
+                else{
+                    isFinish = false;
+                    cnt++;
+                }
+
             }
-            else{
-                cnt++;
+            cryptogram = sb.toString();
+            if(isFinish || sb.toString().equals("")){
+                break;
             }
         }
-        System.out.println(sb.toString());
-        return answer;
+        return cryptogram;
     }
 }
