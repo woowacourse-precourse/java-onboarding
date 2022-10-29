ee338a07ca8d43f4217334a0b7fe0ed7b42a7af1
jipyo0106 <jg020106@naver.com>
2022-10-29 13:18:34 +0900

    problem 1 코드 리팩토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,25 +5,50 @@ import java.util.List;
 class Problem1 {
     private static final int LEFT_PAGE = 0;
     private static final int RIGHT_PAGE = 1;
+
+    private static final int EXCEPTION_RETURN = -1;
+    private static final int DRAW_RETURN = 0;
+    private static final int POBI_WIN_RETURN = 1;
+    private static final int CRONG_WIN_RETURN = 2;
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (pobi.get(LEFT_PAGE) - pobi.get(RIGHT_PAGE) != -1 || crong.get(LEFT_PAGE) - crong.get(RIGHT_PAGE) != -1) {
-            return -1;
+        if (isWrongData(pobi, crong)) {
+            return EXCEPTION_RETURN;
         }
 
-        int pobiHighValue = calc(pobi.get(LEFT_PAGE).toString(), pobi.get(RIGHT_PAGE).toString()),
-                crongHighValue = calc(crong.get(LEFT_PAGE).toString(), crong.get(RIGHT_PAGE).toString());
+        int pobiHighNumber = pobiOrCrong(pobi),
+                crongHighNumber = pobiOrCrong(crong);
 
-        if (pobiHighValue == crongHighValue) {
-            return 0;
-        } else if (pobiHighValue > crongHighValue) {
-            return 1;
-        } else if (pobiHighValue < crongHighValue) {
-            return 2;
+        if (pobiHighNumber == crongHighNumber) {
+            return DRAW_RETURN;
+        } else if (pobiHighNumber > crongHighNumber) {
+            return POBI_WIN_RETURN;
+        } else if (pobiHighNumber < crongHighNumber) {
+            return CRONG_WIN_RETURN;
         }
-        return -1;
+
+        return EXCEPTION_RETURN;
     }
 
-    private static int calc(String leftPage, String rightPage) {
+
+    private static boolean isWrongData(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(LEFT_PAGE) - pobi.get(RIGHT_PAGE) != -1 || crong.get(LEFT_PAGE) - crong.get(RIGHT_PAGE) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+
+    private static int pobiOrCrong(List<Integer> pobiOrCrong) {
+        String left = pobiOrCrong.get(LEFT_PAGE).toString();
+        String right = pobiOrCrong.get(RIGHT_PAGE).toString();
+
+        return getHighValueByPagePlusMultiple(left, right);
+    }
+
+
+    private static int getHighValueByPagePlusMultiple(String leftPage, String rightPage) {
         int leftPlus = 0, rightPlus = 0, leftMultiple = 1, rightMultiple = 1;
 
         for (int i = 0; i < leftPage.length(); i++) {
