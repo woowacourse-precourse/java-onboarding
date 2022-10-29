995a370aa4d31aab4f2730d83cc2f8aea1f1e9ae
leeyongjun <leeyongjun0310@naver.com>
2022-10-29 16:42:31 +0900

    refactor(PROBLEM3): getCount 메소드의 카운트 변수 이름 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -16,14 +16,14 @@ public class Problem3 {
     private static int getCount(int number){
         // 3, 6, 9의 개수를 세주는 메소드
         String string = String.valueOf(number);
-        int totalCount = 0;
+        int count = 0;
         for(String s : string.split("")){
             if (isIn369(s)){
-                totalCount += 1;
+                count += 1;
             }
         }
 
-        return totalCount;
+        return count;
     }
 
     private static int getGameResult(int number) {
