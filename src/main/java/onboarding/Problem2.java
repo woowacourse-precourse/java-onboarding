7555cca5c1511e55ffe49e4ef28c76c65d67b743
정핸모 <gusah009@naver.com>
2022-10-29 14:06:22 +0900

    feat: 암호문을 돌면서 스택엔 스택의 peek와 같지 않은 것만 넣도록 구현한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
 
   public static String solution(String cryptogram) {
-    String answer = "answer";
-    return answer;
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < cryptogram.length(); i++) {
+      char currChar = cryptogram.charAt(i);
+      if (stack.isEmpty() || stack.peek() != currChar) {
+        stack.push(currChar);
+      } else {
+        stack.pop();
+        while (i + 1 < cryptogram.length() && currChar == cryptogram.charAt(i + 1)) {
+          i++;
+        }
+      }
+    }
+    return stack.stream()
+        .map(Object::toString)
+        .collect(Collectors.joining());
   }
 }
