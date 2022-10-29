1d6ebdc5299362daa4f8895e6fb54ddbc58be3f3
채우석 <wochae@wochae-MacBookAir.local>
2022-10-30 01:40:41 +0900

    [fix] 3.3 할당한 number 배열의 크기만큼의 인덱스가 도달할 때 최종값 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -21,6 +21,12 @@ public class Problem3 {
         for (int idx = 3; idx < number; idx++) {
             idxToStr = String.valueOf(idx);
             tqqq[idx] = tqqq[idx - 1];
+
+            for (int strOfIdx = 0; strOfIdx < idxToStr.length(); strOfIdx++) {
+                if (idxToStr.charAt(strOfIdx) == '3' || idxToStr.charAt(strOfIdx) == '6' || idxToStr.charAt(strOfIdx) == '9') {
+                    tqqq[idx]++;
+                }
+            }
         }
         return tqqq[number];
     }
