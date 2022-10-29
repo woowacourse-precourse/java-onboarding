30bf13d52ce9314ba0b009de5ae1c6b5f16ed3ce
HiiWee <wpdlzhf159@gmail.com>
2022-10-29 21:44:10 +0900

    Feat problem2: 직전 문자와 현재 문자 비교 후 동작 수행 분기 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,8 +5,8 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         StringBuilder cryptogramBuilder = new StringBuilder(cryptogram);
 
-        boolean checkConversion = false;
-        while (!checkConversion) {
+        boolean checkConversion = true;
+        while (checkConversion) {
             checkConversion = decode(cryptogramBuilder);
         }
         return "";
@@ -14,9 +14,25 @@ public class Problem2 {
 
     public static boolean decode(StringBuilder encodedCode) {
         int count = 1;
+        boolean flag = false;
         for (int i = 1; i < encodedCode.length(); i++) {
+            if (encodedCode.charAt(i) == encodedCode.charAt(i - 1)) {
+                count++;
+                continue;
+            }
+            if (encodedCode.charAt(i) != encodedCode.charAt(i - 1) && count >= 2) {
+                flag = true;
+                count = 1;
+            }
 
         }
-        return false;
+        return flag;
+    }
+
+
+    public static void main(String[] args) {
+        String[] test = {"browoanoommnaon", "zyelleyz"};
+
+        solution(test[0]);
     }
 }
