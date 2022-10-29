5a63230ebdd226ff3b005e24093a61c7785d735e
jagajang <kosb7544@gmail.com>
2022-10-29 11:58:39 +0900

    add score comparison

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,21 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        try {
+            int pScore = maxScore(pobi);
+            int cScore = maxScore(crong);
+
+            if (pScore > cScore)
+                return 1;
+            else if (pScore < cScore)
+                return 2;
+            return 0;
+        } catch(Exception e) {
+            return -1;
+        }
+    }
+
+    private static int maxScore(List<Integer> pages) throws Exception {
+        return 0;
     }
 }
