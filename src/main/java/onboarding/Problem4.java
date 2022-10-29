70ead5e58ebe4442594f5074bf5ac5d7963e67fb
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-29 21:19:53 +0900

    Problem4(구현): solution 메소드에 getChangedWord 메소드 연결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = getChangedWord(word);
         return answer;
     }
 
