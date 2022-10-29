d1a5375d259be9e9145fc2e51c353a6d7672850e
chaos0103 <lyt1228@naver.com>
2022-10-29 15:08:52 +0900

    feat(Problem7): 유저의 친구를 찾는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,42 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        List<String> userFriendList = findFriendListByUser(user, friends);
+
         return answer;
+    }
+
+    private static List<String> findFriendListByUser(String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (isUserFriend(user, friend)) {
+                String friendName = findFriendByUser(user, friend);
+                friendList.add(friendName);
+            }
+        }
+        return friendList;
+    }
+
+    private static boolean isUserFriend(String user, List<String> friend) {
+        return nameEq(user, friend.get(0)) || nameEq(user, friend.get(1));
+    }
+
+    private static String findFriendByUser(String user, List<String> friend) {
+        if (nameEq(user, friend.get(0))) {
+            return friend.get(1);
+        } else {
+            return friend.get(0);
+        }
+    }
+
+    private static boolean nameEq(String user, String target) {
+        return user.equals(target);
     }
 }
