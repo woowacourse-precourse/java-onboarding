80ddaf7cfd05250b15964aefac6d2795ada11570
asas6978 <asas6978@naver.com>
2022-10-29 21:42:08 +0900

    feat(problem6): Validation 기능 구현

---
+++ src/main/java/onboarding/problem6/Validation.java
@@ -0,0 +1,60 @@
+package onboarding.problem6;
+
+import java.util.List;
+
+import static java.util.regex.Pattern.matches;
+
+public class Validation {
+    private static final String emailRegex = "^([a-zA-Z0-9\\s]*@gmail.com)$";
+    private static final String nicknameRegex = "^([가-힣]*)$";
+
+    public static boolean isCrewOutOfRange(List<List<String>> forms) {
+        int count = 0;
+
+        for (List<String> ignored : forms) {
+            count++;
+        }
+
+        return count < 1 || count > 10000;
+    }
+
+    public static boolean isEmailOutOfRange(List<String> emailForms) {
+        boolean isOutOfRange = false;
+
+        for (String emailForm : emailForms) {
+            isOutOfRange = isOutOfRange || emailForm.length() < 11 || emailForm.length() > 20;
+        }
+
+        return isOutOfRange;
+    }
+
+    public static boolean isNicknameOutOfRange(List<String> nicknameForms) {
+        boolean isOutOfRange = false;
+
+        for (String nicknameForm : nicknameForms) {
+            isOutOfRange = isOutOfRange || nicknameForm.length() < 1 || nicknameForm.length() > 20;
+        }
+
+        return isOutOfRange;
+    }
+
+    public static boolean isInvalidEmailForm(List<String> emailForms) {
+        boolean isInvalidForm = false;
+
+        for (String emailForm : emailForms) {
+            isInvalidForm = isInvalidForm || matches(emailRegex, emailForm);
+        }
+
+        return isInvalidForm;
+    }
+
+    public static boolean isInvalidNicknameForm(List<String> nicknameForms) {
+        boolean isInvalidForm = false;
+
+        for (String nicknameForm : nicknameForms) {
+            isInvalidForm = isInvalidForm || !matches(nicknameRegex, nicknameForm);
+        }
+
+        return isInvalidForm;
+    }
+}
