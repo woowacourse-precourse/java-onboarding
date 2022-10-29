23fc348f600d95981a374a45cd89051a862219af
wath1457 <wath1457@gmail.com>
2022-10-29 15:00:34 +0900

    [Problem4 traverse string]

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+/*
+1. 문자열을 순회한다.
+2. 해당 문자가 알파벳 이외의 문자인경우, answer에 그대로 더한다.
+2-1. 문자가 알파벳인지 판별하는 법 : 아스키코드표의 a-z, A-Z 범위 밖의 문자이면 알파벳이 아니다.
+3. 대문자인 경우 'Z'-('해당문자'-'A') / 소문자인 경우 'z'-('해당문자'-'a')
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char s;
+        for (int i = 0; i < word.length(); i++) {
+            s = word.charAt(i);
+        }
         return answer;
     }
 }
