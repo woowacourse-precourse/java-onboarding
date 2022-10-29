4248bc5e8e1ca544194545d8727666ce2cb6a2f8
chosunghyun18 <krismon@naver.com>
2022-10-29 22:57:44 +0900

    [5번] 기능 요구사항 리스트 1번 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+
     }
+    public static int[] calculateMoney(int money) {
+
+    }
+
 }
