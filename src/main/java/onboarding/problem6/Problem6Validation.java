67ab6de74ca016d9531abc1fdd69ea07b2901482
math0917 <math0917@naver.com>
2022-10-29 17:12:36 +0900

    test : validation test code 작성

---
+++ src/main/java/onboarding/problem6/Problem6Validation.java
@@ -0,0 +1,8 @@
+package onboarding.problem6;
+
+public class Problem6Validation {
+
+    public static boolean problem6Validation(String email, String 제이엠, int minEmailInput, int maxEmailInput, int minNicknameInput, int maxNicknameInput) {
+        return false;
+    }
+}
