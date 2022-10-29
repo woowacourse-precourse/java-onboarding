b2f48c5cd0d8c2d9fdb7ccea9f612aedfa0864f4
tjddnr7760 <tjddnr7760@naver.com>
2022-10-29 09:44:05 +0900

    <6번 - 구현> 닉네임에서 두글자 리스트 추가하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
 import java.util.List;
+import java.util.LinkedList;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        Problem6 problem = new Problem6();
         List<String> answer = List.of("answer");
+        LinkedList<String> nicknamelist = new LinkedList<>();
+        LinkedList<String> splitnickname = new LinkedList<>();
+
+        for(List e : forms) {
+            nicknamelist.add((String) e.get(1));
+        }
+
+        splitnickname = problem.splitTwoNickname(nicknamelist);
+
+
         return answer;
     }
+
+    public LinkedList<String> splitTwoNickname(LinkedList<String> nicknamelist) {
+        LinkedList<String> resultlist = new LinkedList<>();
+
+        for(String nickname : nicknamelist) {
+            for(int i=0; i<nickname.length()-1; i++) {
+                resultlist.add(nickname.substring(i,i+2));
+            }
+        }
+
+        return resultlist;
+    }
+
 }
