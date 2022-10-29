1672b15c8308566d6a2cda9415516fa406d47433
kimdeokyoung <dudejrss@naver.com>
2022-10-29 17:44:59 +0900

    [Edit] src/main/java/onboarding/Problem7.java : make structure Problem 7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem7 {
+
+    public static List<String> makeUserListByRecommendation(){
+        List<String> result = Collections.emptyList();
+        return result;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    private class SocialGraph{
+
+    }
+
+
+
+
+
 }
