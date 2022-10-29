723e830de6a288ce991b716c66123a8b6b0c1c36
SeongukBaek <bsu6235@gmail.com>
2022-10-29 15:28:12 +0900

    feat: implement moveIndex()

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    static int index;
+    static String cryptogram;
+    static int cryptogramLength;
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    /*
+     * 현재 문자와 같지 않은 다른 문자가 나올 떄까지 인덱스 증가
+     *
+     * @return void
+     * */
+    private static void moveIndex(char character) {
+        while(index < cryptogramLength && character == cryptogram.charAt(index)) {
+            index++;
+        }
     }
 }
