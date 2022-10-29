d7e498484521c8b11f41a3f0badf16e1b1c6ddc6
solesy <tkddbs9801@gmail.com>
2022-10-29 15:22:44 +0900

    problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.Hashtable;
+
 public class Problem4 {
+    private Hashtable<Character,Character> ht;
+    public Problem4(){
+        ht = new Hashtable<Character,Character>();
+        for(int i = 65, j = 25; i <= 90; ++i, j-=2)
+            ht.put((char)i, (char)(i+j));
+        for(int i = 97, j = 25; i <= 122; ++i, j-=2)
+            ht.put((char)i, (char)(i+j));
+    }
+    private String frogChangeWord(final String word){
+        String ret = "";
+        for(int i = 0; i < word.length(); ++i) {
+            if(ht.get(word.charAt(i)) != null) {
+                ret += ht.get(word.charAt(i));
+            } else{
+                ret += word.charAt(i);
+            }
+        }
+        return ret;
+    }
     public static String solution(String word) {
         String answer = "";
+        Problem4 p4 = new Problem4();
+        answer = p4.frogChangeWord(word);
         return answer;
     }
 }
