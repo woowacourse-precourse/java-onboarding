81f15516c9cd6f93ac1915bace64b76545e71e90
HaODay <ycis5013@naver.com>
2022-10-29 16:05:32 +0900

    [Problem1] 페이지 입력 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(((pobi.get(0)+1)!=(pobi.get(1)))||((crong.get(0)+1)!=(crong.get(1)))){
+            answer=-1;
+            return answer;
+        }
+        if((pobi.get(0)<1 || pobi.get(1)>400)||(crong.get(0)<1 || crong.get(1)>400)){
+            answer=-1;
+            return answer;
+        }
+
+
         return answer;
     }
 }
