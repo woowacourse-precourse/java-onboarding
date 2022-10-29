c32ac0153ccb7c5709f7731d8a084643b198b27c
yg0585 <yg0585@naver.com>
2022-10-29 23:07:02 +0900

    refactor : 불필요한 method 삭제

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,28 +9,16 @@ import java.util.List;
 
 public class Problem5 {
 
-    private static int calPaper(int bankmoney, int money) {
-        int cnt = 0;
-        while (true){
-            if (money - bankmoney < 0)
-                break;
-            cnt++;
-            money -= bankmoney;
-        }
-
-        return cnt;
-    }
-
     public static List<Integer> solution(int money) {
         int bank[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> answer = new ArrayList<>();
 
         for (int bankmoney: bank)
         {
-            int num = calPaper(bankmoney, money);
-            money -= num * bankmoney;
-            answer.add(num);
+            answer.add(money / bankmoney);
+            money -= money / bankmoney * bankmoney;
         }
+
         return answer;
     }
 }
