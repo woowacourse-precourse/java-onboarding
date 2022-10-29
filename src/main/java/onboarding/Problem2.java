2e916e0ca777fb0aae03c4f06ef7ac11abf34807
ghltjd369 <ghltjd369@gmail.com>
2022-10-29 17:06:57 +0900

    feat : 암호문의 문자를 하나씩 분리하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        LinkedList<String> password = new LinkedList<String>();
+
+        for(int i = 0; i < cryptogram.length(); i++){
+            String cryptogram_i = cryptogram.substring(i, i+1);
+            password.add(cryptogram_i);
+        }
+
         return answer;
     }
 }
