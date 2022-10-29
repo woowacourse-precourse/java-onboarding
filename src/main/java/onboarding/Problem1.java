eaddfbd453836299088359c8b8e0af7c7e2afea3
hhiyeon <hhyyeon0214@gmail.com>
2022-10-29 15:46:57 +0900

    feat: p1-divide digit number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,19 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+    private static List<Integer> splitDigitNumber(int pageNumber) {
+        List<Integer> splitNumber = new ArrayList<>();
+        while (pageNumber > 0) {
+            splitNumber.add(pageNumber % 10);
+            pageNumber /= 10;
+        }
+        return splitNumber;
     }
 }
