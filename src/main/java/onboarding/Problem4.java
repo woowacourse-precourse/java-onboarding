b3d9dd8e9016f5245490b6d808e3a5ad0de12f6a
Koonayoung518 <koony000415@gmail.com>
2022-10-30 01:46:34 +0900

    feat(problem4) : 공백과 문자 구분하기 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String reverse = "";
+        String[] splitWord = word.split("");
+        for(int i=0; i< splitWord.length; i++){
+            reverse = splitWord[i];
+            if(!splitWord[i].equals(" "))
+                reverse = null;
+            answer+= reverse;
+        }
         return answer;
     }
 }
