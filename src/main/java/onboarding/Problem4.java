f71d3604bf9145b7483f3a2513be456ecfb17772
yg0585 <yg0585@naver.com>
2022-10-29 17:06:10 +0900

    1. 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+//-----기능-----
+//1. 청개구리 사전 만들기
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String dict = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
         return answer;
     }
 }
