a727128106f5fffc94b61effc5948e53c4bdfea2
jinuk <devuks0820@gmail.com>
2022-10-29 13:48:12 +0900

    feat: Problem1 Add calculate page numbers
    페이지 점수계산을 위해 각 페이지별 자리수의 합과 곱을 비교하여 더 큰 값을 자신의 점수로 선택하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,38 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static Integer findPageNumberSum(Integer number) {
+        Integer sum = 0;
+
+        while(number > 0){
+            sum+= number%10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    private static Integer findPageNumberProduct(Integer number) {
+        Integer sum = 1;
+
+        while(number > 0){
+            sum*= number%10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    private static Integer findPageScore(List<Integer> pageNums){
+
+        Integer leftPage = pageNums.get(0);
+        Integer rightPage = pageNums.get(1);
+
+        Integer leftScore = Math.max(findPageNumberSum(leftPage),findPageNumberProduct(leftPage));
+        Integer rightScore = Math.max(findPageNumberSum(rightPage),findPageNumberProduct(rightPage));
+
+        return Math.max(leftScore,rightScore);
+    }
+
 }
