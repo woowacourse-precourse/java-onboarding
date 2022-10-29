5a63fd03e03ddbdd4f0fc83e7c463a3c1b22e462
limzzum <chfhddl1234@naver.com>
2022-10-29 18:12:11 +0900

    [문제 5] 돈의 단위가 큰 순으로 배열로 저장.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int [] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
         return answer;
     }
 }
