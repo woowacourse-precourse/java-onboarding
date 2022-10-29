2d2cf6968d9272fa2002800d0a10ad470c952b91
Egenieee <hhgg0925@naver.com>
2022-10-29 23:15:37 +0900

    [FEAT] Problem3 1부터 input 범위까지 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int totalCount = 0;
+
+        for (int index = 1 ; index <= number ; index++) {
+            String currentNumber = String.valueOf(index);
+            totalCount += getSamYukGuCount(currentNumber);
+        }
+
+        return totalCount;
+    }
+
+    private static int getSamYukGuCount(String currentNumber) {
+        int count = 0;
+
+        for (int digitIndex = 0 ; digitIndex < currentNumber.length() ; digitIndex++) {
+            if (isSamYukGu(currentNumber.charAt(digitIndex))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean isSamYukGu(char charAt) {
+        return true;
     }
 }
