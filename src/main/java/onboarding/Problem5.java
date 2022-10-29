9849a1ca6b76f9d12dc2dc4ca86b232b91603932
gnoesnooj <306yyy@naver.com>
2022-10-29 19:42:04 +0800

    feat : PROBLEM5 문제 해결 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int[] bill = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        if (!isOutOfRange(money)) {
+            return new ArrayList<>();
+        }
+        return calculate(money);
+    }
+
+    private static boolean isOutOfRange(int money) {
+        return money >= 1 && money <= 1000000;
+    }
+
+    private static List<Integer> calculate(int money) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            answer.add(money / bill[i]);
+            money %= bill[i];
+        }
+
         return answer;
     }
 }
