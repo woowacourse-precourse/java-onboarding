2615cc261441dde3ad258349f5617bd8ec90e09d
can019 <jys01012@gmail.com>
2022-10-29 19:28:11 +0900

    :bulb: Exception checker TODO 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,7 +6,22 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        // 포비 또는 크롱의 page에 문제가 있다면 -1 반환
+        if(exceptionChecker(pobi) || exceptionChecker(crong))
+            answer = -1;
+        else{
+            // @TODO :: 계산
+        }
         return answer;
+    }
+
+    private static boolean exceptionChecker(List<Integer> page){
+        // @TODO :: 예외상황 캐치
+
+        // 1. 1부터 400 사이 값이 아닌 경우
+        // 2. 왼쪽은 홀수 오른쪽은 짝수가 아닌 경우
+        // 3. 오른쪽 - 왼쪽이 1이 아닌 경우
+        return true;
     }
 
     private static int calculatePersonalScore(List<Integer> page){
