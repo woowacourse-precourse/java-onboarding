0853f4ce97ae12003bb2cc4fcf3c367be89bbc23
keon0711 <fnc6488@gmail.com>
2022-10-29 12:22:59 +0900

    [Problem6] 두 글자가 같은지 확인하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> emailSet = new HashSet<>();
+
+
+        List<String> emailList = new ArrayList<>(emailSet);
+        Collections.sort(emailList);
+        return emailList;
+    }
+
+    private static boolean isDuplicated(String name1, String name2) {
+        for (int i = 0; i < name1.length() - 1; i++) {
+            if (name2.contains(name1.substring(i, i+2)))
+                return true;
+        }
+        return false;
     }
 }
