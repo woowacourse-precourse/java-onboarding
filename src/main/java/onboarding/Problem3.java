d398f1e4f181394939a3a9d93f1c6c0adee5186b
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-29 14:16:29 +0900

    refactor: Game369 class 구현 - 기존 로직을 class로 이동

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,19 +1,34 @@
 package onboarding;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Problem3 {
-  public static int solution(int number) {
-    return IntStream.rangeClosed(1, number)
-            .map(Problem3::countIncludeGameNumber)
-            .sum();
+  static class Game369 {
+    private static final List<Integer> clapNumbers = List.of(3, 6, 9);
+    private int number;
+
+    public Game369(int number) {
+      this.number = number;
+    }
+
+    public int getTotalClapping() {
+      return IntStream.rangeClosed(1, number)
+              .map(Game369::countClapping)
+              .sum();
+    }
+
+    private static int countClapping(int number) {
+      return (int) Stream.of(Integer.toString(number).split(""))
+              .mapToInt(Integer::parseInt)
+              .filter(n -> clapNumbers.contains(n))
+              .count();
+    }
   }
 
-  public static int countIncludeGameNumber(int number) {
-    return (int) Stream.of(Integer.toString(number).split(""))
-            .mapToInt(Integer::parseInt)
-            .filter(n -> n == 3 || n == 6 || n == 9)
-            .count();
+  public static int solution(int number) {
+    Game369 game = new Game369(number);
+    return game.getTotalClapping();
   }
 }
