524bc3cbe230cdc703b2e25724d3c9be2bc8a5ac
Park shinyeong <syngh503@gmail.com>
2022-10-29 15:38:19 +0900

    Problem7/Feat/사용자의 친구 목록 추출하기 기능 구현 user의 친구 목록을 추출하는 getUserFriends 함수를 구현하였습니다. user과 friends를 매개변수로 받아, 친구 목록을 리스트로 리턴합니다. Relate to: #7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,22 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<String> getUserFriends(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+        for (List<String> friend: friends) {
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+
+            if(user1.equals(user)) {
+                userFriends.add(user2);
+                continue;
+            }
+            if (user2.equals(user)) {
+                userFriends.add(user1);
+                continue;
+            }
+        }
     }
 }
