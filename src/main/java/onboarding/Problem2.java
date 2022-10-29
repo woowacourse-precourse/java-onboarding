ad3c3017c6fe621bc1dfa6e58e4057b9d63a24c9
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 19:51:46 +0900

    :memo: [문제 2] : 풀이 전략

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        // 1. 입력된 문자를 decode 메소드로 해석
+        // 2. 해석된 결과 값 반환
+        String answer = decode(cryptogram);
         return answer;
+    }
+    static String decode(String cryptogram) {
+        // 1. decode 탈출 조건 설정
+        // 1-1. 연속된 중복 문자가 없는 경우 : 탈출, 결과 값 반환
+        // 1-2. 연속된 중복 문자가 있는 경우 : 탈출 할 때 까지 반복
+        // 2. 연속된 문자를 찾음
+        // 3. 입력된 문자열에서 연속된 문자 제거
+        // 4. 3번 결과 값을 decode에 다시 입력
     }
 }
