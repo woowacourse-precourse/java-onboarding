7ea40284d7eaf7593dd4a123f64c9d9fb69d63ec
syeonbot <asdf748596123@gmail.com>
2022-10-30 00:14:02 +0900

    feat: problem5 돈의 범위 validate 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 1000000;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        validateRange(money);
         return answer;
+    }
+
+    public static void validateRange(int number) {
+        if (number < MINIMUM_NUMBER || MAXIMUM_NUMBER < number) {
+            throw new IllegalArgumentException();
+        }
     }
 }
