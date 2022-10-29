3e077cdc8ed9ae514025fe0b9e73741860e938f6
hoonn9 <hun9935@gmail.com>
2022-10-29 22:00:04 +0900

    Add hashMap, word mapping loop logic (Problem6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,32 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+    private static final HashMap<String, String> map = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
 
-        System.out.println(sliceByLength("answer", 2));
+        for (List<String> form: forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            for (String part: sliceByLength(nickname, 2)) {
+                if (isAlreadyUse(part, email)) {
+                    answer.add(map.get(part));
+                    answer.add(email);
+                }
+                map.put(part, email);
+            }
+        }
+
         return answer;
+    }
+
+    private static boolean isAlreadyUse(String part, String email) {
+        return map.containsKey(part) && !map.get(part).equals(email);
     }
 
     private static List<String> sliceByLength(String str, Integer len) {
