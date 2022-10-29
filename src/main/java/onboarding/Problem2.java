e99e032b712516c1a9438de9429d212507e20c7f
yujindonut <hyujin0825@gmail.com>
2022-10-30 00:15:34 +0900

    [FEAT] Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,54 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return crypto(cryptogram);
     }
+    public static String crypto(String s){
+        ArrayList<String> strList = new ArrayList<>(Arrays.asList(s.split("")));
+        return String.join("", deleteSameStr(strList));
+    }
+
+    public static ArrayList<String> deleteSameStr(ArrayList<String> strList){
+
+        int cnt = 0, indexNum = 0;
+        for (int i = 0; i < strList.size() - 1; i++) {
+            if(strList.get(i).equals(strList.get(i + 1))) {
+                cnt++;
+                indexNum = i + 1;
+            }
+            if (cnt >= 1) {
+                int start = indexNum - cnt;
+                while(cnt >= 0) {
+                    strList.remove(start);
+                    cnt--;
+                }
+                cnt = 0; indexNum = 0; i = 0;
+            }
+        }
+        if (strList.size() > 1) {
+            for (int j = 0; j < strList.size() - 1; j++) {
+                if(strList.get(j).equals(strList.get(j + 1))) {
+                    cnt++;
+                    indexNum = j + 1;
+                }
+                if (cnt >= 1) {
+                    int start = indexNum - cnt;
+                    while(cnt >= 0) {
+                        strList.remove(start);
+                        cnt--;
+                    }
+                    cnt = 0; indexNum = 0; j = 0;
+                }
+            }
+        }
+        return strList;
+    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(solution("browoanoommnaon"));
+//    }
 }
