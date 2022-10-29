feda02fc17de48b7f511d574187ef9b1165a43d9
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-29 16:03:46 +0900

    feat(Problem1): 가장 큰 점수를 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,32 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int calculateMaxNumber(List<Integer> pageNumbers){
+        int leftPageNumber = comparePlusAndMultiple(pageNumbers.get(0));
+        int rightPageNumber = comparePlusAndMultiple(pageNumbers.get(1));
+        return Integer.max(leftPageNumber, rightPageNumber);
+    }
+
+    public static int comparePlusAndMultiple(int pageNumber){
+        return Integer.max(plus(pageNumber), multiple(pageNumber));
+    }
+
+    public static int plus(int pageNumber){
+        int sum = 0 ;
+        while(pageNumber != 0) {
+            sum += pageNumber%10;
+            pageNumber /= 10;
+        }
+        return sum;
+    }
+
+    public static int multiple(int pageNumber){
+        int mul = 1;
+        while(pageNumber != 0) {
+            mul *= pageNumber%10;
+            pageNumber /= 10;
+        }
+        return mul;
+    }
 }
