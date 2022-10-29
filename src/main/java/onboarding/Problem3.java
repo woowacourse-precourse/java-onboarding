eb7a1577100fb8a700444c6a44ca5a19a4ce49a2
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-30 02:51:13 +0900

    FEAT : Problem 3-1. number 입력받기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+/*
+ *
+ * Class : Problem 3
+ *
+ * Date : 2022.10.30
+ *
+ * Author : kathyleesh
+ *
+ * <기능 목록>
+ *
+ * 1. number을 입력받는다.
+ * 2. 1부터 number까지 수를 탐색한다.
+ * 3. 해당 숫자에 3, 6, 9의 개수만큼 count에 더한다.
+ * 4. 탐색을 모두 마치면 number까지 손뼉을 몇 번 쳐야 하는지를 출력한다.
+ *
+ * */
+
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int endNumber = number;
+        System.out.println(endNumber);
         return answer;
     }
 }
