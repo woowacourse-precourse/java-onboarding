b1cdf8dd7bcd0766a19a56e674f9e60c22dea5ae
nn98 <jkllhgb@gmail.com>
2022-10-29 18:17:35 +0900

    #2 Feat : Feature implementation

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+    public static String solution(String c) {
+        StringBuffer f=new StringBuffer(c);
+        int i=0,l=f.length(),j;
+        for(;i<l;){
+            for(j=i+1;j<l;) {
+                if (f.charAt(i) == f.charAt(j)) j++;
+                else break;
+            }
+            if(i+1<j){
+                f=f.replace(i,j,"");
+                l-=(j-i);
+                if(l<2)return f.toString();
+                for(;f.charAt(i)==f.charAt(i-1);){
+                    i--;
+                    if(i<1)break;
+                }
+            }else i++;
+        }
+        return f.toString();
     }
 }
