acb24010876fad49294e6a1aa93646f7b7f168dd
abbd2 <ajkety00@gmail.com>
2022-10-30 01:50:41 +0900

    문제2 i초기화 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-      StringBuilder sb = new StringBuilder(cryptogram);
+      StringBuffer sb = new StringBuffer(cryptogram);
 
       for(int i = 1; i < sb.length(); i++){
           char fstWord = sb.charAt(i - 1);
@@ -11,7 +11,7 @@ public class Problem2 {
           if(fstWord == sndWord && i <= 1){
               // 인덱스1(두번째 문자)에서 문자를 지운 경우에는 i를 1로 초기화
               sb.delete(i - 1, i + 1);
-              i = 1;
+              i = 0;
           } else if(fstWord == sndWord){
               // 문자를 지운 인덱스에서 2번 앞에 있는 인덱스부터 다시 검사
               sb.delete(i - 1, i + 1);
@@ -20,6 +20,7 @@ public class Problem2 {
 
       } // end for
       String answer = sb.toString();
-        return answer;
+
+      return answer;
     }
 }
