f9fe0f0250a654aa930bec117d7be78210cb479a
wltks98 <wltks98@hanmail.net>
2022-10-29 14:45:52 +0900

    [문제7] refactor : 사용자별 친구 목록 저장 기능 수정

--- src/main/java/onboarding/problemseven/domain/Sns.java
+++ src/main/java/onboarding/problemseven/domain/Sns.java
@@ -1,18 +1,31 @@
 package onboarding.problemseven.domain;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Sns {
 
+	private HashMap<String,List<String>> friendMap = new HashMap<>();
 
 	public Sns(List<List<String>> friends) {
 		for(List<String> friend:friends){
-			User userA=new User(friend.get(0));
-			User userB=new User(friend.get(1));
+			String userA=friend.get(0);
+			String userB=friend.get(1);
 
-			userA.addFriend(friend.get(1));
-			userB.addFriend(friend.get(0));
+			addFriend(userA,userB);
+			addFriend(userB,userA);
+		}
+	}
+
+	private void addFriend(String userA, String userB) {
+		if(friendMap.containsKey(userA)){
+			friendMap.get(userA).add(userB);
+		} else if (!friendMap.containsKey(userA)) {
+			List<String> friendList = new ArrayList<>();
+			friendList.add(userB);
+			friendMap.put(userA,friendList);
 		}
 	}
 }
