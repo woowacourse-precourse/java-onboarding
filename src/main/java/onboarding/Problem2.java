b42aedd19fcdcef558c761cb7d6f05e75c3a89f2
SeongukBaek <bsu6235@gmail.com>
2022-10-29 15:32:33 +0900

    feat: implement solution()

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,8 +8,28 @@ public class Problem2 {
     static int cryptogramLength;
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        index = 0;
+        Problem2.cryptogram = cryptogram;
+        cryptogramLength = cryptogram.length();
+
+        Stack<Character> stack = new Stack<>();
+        stack.push(cryptogram.charAt(index++));
+
+        while (index < cryptogramLength && !stack.isEmpty()) {
+            char currentCharacter = cryptogram.charAt(index);
+            char topOfStack = stack.peek();
+
+            if (currentCharacter == topOfStack) {
+                stack.pop();
+                moveIndex(currentCharacter);
+                continue;
+            }
+
+            stack.push(currentCharacter);
+            index++;
+        }
+
+        return makeAnswer(stack);
     }
 
     /*
@@ -18,7 +38,7 @@ public class Problem2 {
      * @return void
      * */
     private static void moveIndex(char character) {
-        while(index < cryptogramLength && character == cryptogram.charAt(index)) {
+        while (index < cryptogramLength && character == cryptogram.charAt(index)) {
             index++;
         }
     }
@@ -31,7 +51,7 @@ public class Problem2 {
     private static String makeAnswer(Stack<Character> stack) {
         StringBuilder answer = new StringBuilder();
 
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             answer.append(stack.pop());
         }
 
