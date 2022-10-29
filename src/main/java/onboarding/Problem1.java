ff6507db060e1c07fc54d4b1795a4e57395b478f
Devtist <yt930310@snu.ac.kr>
2022-10-29 16:30:13 +0900

    feat(Problem1): 양의 정수에 대해, 각 자리 숫자를 모두 더한 수와 모두 곱한 수 중 최대값을 리턴하는 기능을 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,16 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private int selectOperation(int num) {
+        int sum = 0;
+        int multiple = 1;
+
+        while (num != 0) {
+            sum += num % 10;
+            multiple *= num % 10;
+            num /= 10;
+        }
+        return Math.max(sum, multiple);
+    }
 }
