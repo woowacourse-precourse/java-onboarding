fc6cf2ed701af916875c1d6d2ca3cd1a2e88838a
jinny-l <jinny.l.dev@gmail.com>
2022-10-29 19:45:30 +0900

    [feat] (problem.7) visitors의 점수를 반환하는 메소드 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    // visitor의 방문 횟수를 반환하는 메소드
+    private static Map<String, Integer> getVisitorsScore(List<String> visitors) {
+        List<String> uniqueVisitors = uniqueVisitorsOf(visitors);
+        Map<String, Integer> visitCount = new HashMap<>();
+
+        for (String uniqueVisitor : uniqueVisitors) {
+            int count = 0;
+            for (String visitor : visitors) {
+                if (uniqueVisitor.equals(visitor)) {
+                    count++;
+                }
+            }
+            visitCount.put(uniqueVisitor, count);
+        }
+
+        return visitCount;
+    }
+
+
+    // unique visitor 리스트를 반환하는 메소드
+    private static List<String> uniqueVisitorsOf(List<String> visitors) {
+        Set<String> set = new HashSet<>(visitors);
+        return new ArrayList<>(set);
     }
 }
