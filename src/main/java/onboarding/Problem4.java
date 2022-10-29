9fd2e643842c575633a9f17c09f9de093b7cd474
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-29 22:07:00 +0900

    feat(problem4): 요구사항 설정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+
+
+/**
+ * > 요구사항
+ *  1. 주어진 매핑 값을 Hash 테이블에 모두 저장한다
+ *  2. StringBuilder를 활용해 출력한다.
+ */
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
     }
 }
