f3b5331cb5f7efc96c52a909660041fad2b03e92
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-29 19:06:02 +0900

    refactor: Problem2 - 코드 해독 로직 수정 및 변수명 변경
    - 코드를 해독하는 메서드를 추가로 만들고, 해독 후의 인덱스를 반환하도록 설계
    - 기존 idx -> codeIndex로, 축약어를 명확한 표현으로 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,16 +6,18 @@ import java.util.ArrayList;
 public class Problem2 {
     public static String solution(String cryptogram) {
         List<String> splitCryptogram = new ArrayList<>(List.of(cryptogram.split("")));
-        int idx = 1;
-        while (!decipherFinish(splitCryptogram, idx)) {
-            if (isDuplicate(splitCryptogram, idx)) {
-                removeDuplicate(splitCryptogram, idx);
-                idx = 1;
-                continue;
-            }
-            idx++;
+        int codeIndex = 1;
+        while (!decipherFinish(splitCryptogram, codeIndex)) {
+            codeIndex = decipherCode(splitCryptogram, codeIndex);
         }
         return String.join("", splitCryptogram);
+    }
+    public static int decipherCode(List<String> code, int idx) {
+        if (isDuplicate(code, idx)) {
+            removeDuplicate(code, idx);
+            idx = 0;
+        }
+        return idx + 1;
     }
 
     public static boolean decipherFinish(List<String> str, int idx) {
