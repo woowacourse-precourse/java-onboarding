594d62501cffa6e4e0b6537ad33e421994355c41
tjddnr7760 <tjddnr7760@naver.com>
2022-10-29 11:32:19 +0900

    <6번 - 구현> 닉네임 포함된 이메일 오름차순 출력 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,16 +2,13 @@ package onboarding;
 
 import net.bytebuddy.dynamic.scaffold.MethodGraph;
 
-import java.util.List;
-import java.util.LinkedList;
-import java.util.HashSet;
-import java.util.Collections;
+import java.util.*;
 
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Problem6 problem = new Problem6();
-        List<String> answer = List.of("answer");
+        List<String> answer = new LinkedList<>();
 
         LinkedList<String> nicknamelist = new LinkedList<>();
         LinkedList<String> splitnickname = new LinkedList<>();
@@ -23,6 +20,20 @@ public class Problem6 {
 
         splitnickname = problem.splitTwoNickname(nicknamelist);
         duplicatenickname = problem.findDuplicateNick(splitnickname);
+
+        for(String e : duplicatenickname) {
+            for(List list : forms) {
+                answer.add(problem.findEmail(e, list));
+            }
+        }
+
+        for(int i=0; i<answer.size(); i++) {
+            if(answer.get(i).equals("")) {
+                answer.remove(i);
+            }
+        }
+
+        Collections.sort(answer);
 
         return answer;
     }
@@ -40,15 +51,26 @@ public class Problem6 {
     }
 
     public LinkedList<String> findDuplicateNick(LinkedList<String> nicknamelist) {
-            HashSet<String> resultset = new HashSet<>();
+        HashSet<String> resultset = new HashSet<>();
 
-            for(String e : nicknamelist) {
-                if(Collections.frequency(nicknamelist, e) > 1) {
-                    resultset.add(e);
-                }
+        for(String e : nicknamelist) {
+            if(Collections.frequency(nicknamelist, e) > 1) {
+                resultset.add(e);
             }
+        }
 
-            LinkedList<String> resultlist = new LinkedList<>(resultset);
-            return resultlist;
+        LinkedList<String> resultlist = new LinkedList<>(resultset);
+        return resultlist;
+    }
+
+    public String findEmail(String duplicatenick, List list) {
+        String nickname = (String) list.get(1);
+        String result = "";
+
+        if(nickname.contains(duplicatenick)) {
+            result = (String) list.get(0);
+        }
+
+        return result;
     }
 }
