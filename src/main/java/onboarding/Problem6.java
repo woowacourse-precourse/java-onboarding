5097dd84db398de9692452eba6ce3740dd4350e3
yg0585 <yg0585@naver.com>
2022-10-29 22:08:58 +0900

    feat: 3번 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,19 +1,23 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 //-----기능-----
 //1. 닉네임 배열 생성
 //2. 연속적인 닉네임 조합 생성
-//2. 조합이 닉네임 중 존재하는지 확인
+//3. 조합이 닉네임 중 존재하는지 확인
+//4. 오름차순 정렬
 
 public class Problem6 {
 
     static List<String> nick = new ArrayList<>();
     private static List<String> stringCombination = new ArrayList<>();
+    private static List<String> answer = new ArrayList<>();
+    private static HashSet<Integer> idx = new HashSet<>();
 
-    private static void nCr(char[] str, int r, int i){
+    private static void comb(char[] str, int r, int i){
         String result = "";
 
         while (r-- > 0)
@@ -24,11 +28,11 @@ public class Problem6 {
     private static void callComb(char[] str, int r)
     {
         for(int i = 0; i <= str.length - r; i++){
-            nCr(str, r, i);
+            comb(str, r, i);
         }
     }
 
-    private static void nickComb(int i){
+    private static void getnickComb(int i){
         String str = nick.get(i);
         for (int j = 2; j <= str.length(); j++)
             callComb(str.toCharArray(),  j);
@@ -36,11 +40,32 @@ public class Problem6 {
     }
 
     private static void getDupNick(int i) {
-        nickComb(i);
+        getnickComb(i); //조합 얻음
+        for(int k = i + 1; k < nick.size(); k++)
+        {
+            dupCheck(i);
+        } // 중복 검사
+        stringCombination.clear();//  Comb 비우기
+        System.out.println("idx = " + idx);
+    }
+
+    private static void dupCheck(int i) {
+        for(int k = 0; k < stringCombination.size(); k++)
+            isContain(stringCombination.get(k), i);
+    }
+
+    private static void isContain(String str, int i) {
+        for (int k = i + 1; k < nick.size(); k++)
+        {
+            if (nick.get(k).contains(str))
+            {
+                idx.add(i);
+                idx.add(k);
+            }
+        }
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
 
         for (int i = 0; i <forms.size(); i++)
             nick.add(forms.get(i).get(1));
