47dd813d50d93a5e25caf55710de2f642c6087dd
pushedrumex <pushedrumex@naver.com>
2022-10-29 19:41:58 +0900

    problem4: 알파벳을 반대로 변환하는 함수(reverseAlpha)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,21 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    static char reverseAlpha(char c) {
+        String upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String revUpperAlpha = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        String lowerAlpha = "abcdefghijklnmopqrstuvwxyz";
+        String revLowerAlpha = "zyxwvutsrqponmlkjihgfedcba";
+
+        if (Character.isUpperCase(c)) {
+            int index = upperAlpha.indexOf(c);
+            return revUpperAlpha.charAt(index);
+        } else {
+            int index = lowerAlpha.indexOf(c);
+            return revLowerAlpha.charAt(index);
+        }
+    }
+    public static void main(String args[]) {
+        System.out.println(reverseAlpha('A'));
+    }
 }
