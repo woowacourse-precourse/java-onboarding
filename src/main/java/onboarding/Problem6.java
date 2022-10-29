5e4ce73c79d2d28542e0894daf57f4be56b37d13
Lee Min Woo <lmw7414@naver.com>
2022-10-29 16:57:54 +0900

    problem6 - 테스트 동작 확인
    주어진 테스트 케이스에 대해 통과한다. 다만 아직 코드가 정결하지 못하기 때문에 리팩토링 과정이 필요하다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -16,26 +16,58 @@ public class Problem6 {
 
         List<Crew> crewList = new ArrayList<>();
         Map<String, Integer> tlMap = new HashMap<>();
+        int[] crewCount;
 
         for (int i = 0; i < forms.size(); i++) {
             String email = forms.get(i).get(0);
             String name = forms.get(i).get(1);
-
-            // 이름이 한 글자인 경우
+            int parentKey = -1;
+            int key = -1;  // 해시맵 키찾을 때
+            ArrayList<String> twoLetterList = new ArrayList<>();
             if (name.length() < 2) {
-                if (!tlMap.containsKey(name.substring(0, 1)))
-                    tlMap.put(name.substring(0, 1), i);
+                twoLetterList.add(name.substring(0, 1));
             } else {
-                for (int j = 0; j < name.length() - 2; j++) {
-                    if (!tlMap.containsKey(name.substring(j, j + 2)))
-                        tlMap.put(name.substring(j, j + 2), i);
+                for (int j = 0; j <= name.length() - 2; j++)
+                    twoLetterList.add(name.substring(j, j + 2));
+            }
+
+
+            for (int j = 0; j < twoLetterList.size(); j++) {
+                if (tlMap.containsKey(twoLetterList.get(j))) {
+                    key = tlMap.get(twoLetterList.get(j));
+                    break;
                 }
             }
-            
-            Crew crew = new Crew(i, email, name);
+            for(int j = 0; j< twoLetterList.size(); j++) {
+                if(key != -1)
+                    tlMap.put(twoLetterList.get(j), key);
+                else
+                    tlMap.put(twoLetterList.get(j), i);
+            }
+
+
+            parentKey = tlMap.get(twoLetterList.get(0));
+            crewList.add(new Crew(parentKey, email, name));
         }
 
-        List<String> answer = List.of("answer");
+
+        crewCount = new int[crewList.size()];
+        for (int i = 0; i < crewList.size(); i++)
+            crewCount[crewList.get(i).parentKey]++;
+
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < crewCount.length; i++) {
+            if (crewCount[i] > 1) {
+                for (int j = 0; j < crewList.size(); j++) {
+                    if (crewList.get(j).parentKey == i)
+                        answer.add(crewList.get(j).email);
+                }
+            }
+        }
+
+        Collections.sort(answer);
+
         return answer;
     }
 
