73c5579996bcfae7b30faf710eef5e161fd5c125
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-29 22:13:53 +0900

    feat(problem6): 요구사항 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+
 import java.util.List;
 
+/**
+ * > 요구사항
+ * 1. word를 2개씩 끊어서 연속되게 hash에 저장, 이 때 hash에는 word의 소속을 알기 위해 email을 같이 매핑해준다.
+ * 2. 순회하면서 hash key에 word가 있느지 여부에 따라 중복여부를 결정한다.
+ * 3. 중복이면 해당 word에 저장된 value인 email과 현재 email을 결과 set에 넣어준다.
+ * 4. 정렬해서 출력한다.
+ */
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
     }
+
 }
+
