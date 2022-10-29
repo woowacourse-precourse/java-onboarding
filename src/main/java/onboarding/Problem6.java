3d91e252a6a1cae9597cc4edb0c95d1f5164b351
Youngho <cyh6099@gmail.com>
2022-10-29 15:29:20 +0900

    feat(p6) : 닉네임 2~n자리로 분리하는 메서드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
@@ -7,4 +8,16 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static List<String> separateNickname(String nickname) {
+        List<String> returnList = new ArrayList<>();
+        for (int i = 2; i < nickname.length()+1; i++) {
+            for (int j = 0; j < nickname.length()+1 - i; j++) {
+                returnList.add(nickname.substring(j, i+j));
+            }
+        }
+        return returnList;
+    }
+
+
 }
