a3fb823ef0905dc0fc7ff755096ae5fe2d4a0d8a
soyunim <togksk123@naver.com>
2022-10-29 22:40:58 +0900

    Problem2 5)repeat until there is no duplication

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,7 @@ public class Problem2 {
         String[] str = cryptogram.split("");
         List<String> strList = new ArrayList<>();
 
+        int cnt=1;
         int startIdx=0;
         int endIdx=0;
 
@@ -15,29 +16,39 @@ public class Problem2 {
             strList.add(str[i]);
         }
 
-        // duplication start Index
-        for (int i = 0; i < strList.size()-1; i++) {
-            if(strList.get(i).equals(strList.get(i+1))){
-                startIdx=i;
+        while(cnt>0) {
+            cnt=0;
+            
+            // duplication start Index
+            for (int i = 0; i < strList.size() - 1; i++) {
+                if (strList.get(i).equals(strList.get(i + 1))) {
+                    cnt++;
+                    startIdx = i;
+                    break;
+                }
+            }
+
+            // exit if there are no duplication
+            if(cnt==0){
                 break;
             }
-        }
 
-        // duplication end Index
-        for (int i = startIdx+1; i < strList.size(); i++) {
-            if(strList.get(startIdx).equals(strList.get(i))){
-                endIdx=i;
+            // duplication end Index
+            for (int i = startIdx + 1; i < strList.size(); i++) {
+                if (strList.get(startIdx).equals(strList.get(i))) {
+                    endIdx = i;
+                } else {
+                    break;
+                }
             }
-            else{
-                break;
+
+            // remove the duplication characters
+            for (int k = 0; k <= endIdx - startIdx; k++) {
+                strList.remove(startIdx);
             }
-        }
 
-        // remove the duplication characters
-        for (int k = 0; k <= endIdx-startIdx; k++) {
-            strList.remove(startIdx);
+            answer=strList.toString().join("",strList);
         }
-
         return answer;
     }
 }
