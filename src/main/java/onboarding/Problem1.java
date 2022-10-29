f2a52b8955ab6137d9ab447b2300a9907dd9ddf6
younghyun-na <younghyunna@naver.com>
2022-10-30 02:17:14 +0900

    :sparkles: 문제 1번: 게임 결과를 도출하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,17 +1,11 @@
 package onboarding;
 
+import onboarding.problem1.Game;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-
-        /*
-        게임 결과에 따른 값 반환하는 메서드
-        포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0
-        예외사항은 -1
-        */
-
+        return Game.getGameResult(pobi, crong);
     }
 }
--- src/main/java/onboarding/problem1/Constants.java
+++ src/main/java/onboarding/problem1/Constants.java
@@ -2,5 +2,10 @@ package onboarding.problem1;
 
 public class Constants {
 
-    // 승패 여부에 따른 반환 값 고정
+    // 게임 결과에 따른 반환 값 고정
+    public static final int POBI_WINS= 1;
+    public static final int CRONG_WINS= 2;
+    public static final int DRAW = 0;
+    public static final int EXCEPTION= -1;
+
 }
