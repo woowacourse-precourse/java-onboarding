9a20dbc2c70de0b7deabdbb393a78dc144a703bd
backFox <jorealbusiness@gmail.com>
2022-10-29 20:39:47 +0900

    refactor(problem6): 헥심 비즈니스 코드를 세부 메소드들로 분리

--- src/main/java/onboarding/problem6/service/UserService.java
+++ src/main/java/onboarding/problem6/service/UserService.java
@@ -23,23 +23,32 @@ public class UserService {
         List<User> allUsers = userRepository.findAll();
         Set<String> emailSet = new TreeSet<>();
 
-        allUsers.forEach(user -> {
-            String name = user.getName();
-            for (int spellIndex = 0; spellIndex < name.length() - 1; spellIndex++) {
-                String duplicatingWord = makeDuplicatingWord(name, spellIndex);
-                emailSet.addAll(
-                        userRepository.findAllByNameContaining(duplicatingWord)
-                                .stream().filter(find -> !user.equals(find))
-                                .map(find -> find.getEmail())
-                                .collect(Collectors.toSet())
-                );
-            }
-        });
+        allUsers
+                .stream().map(user -> findAllInvalidEmailsFromUser(user))
+                .forEach(emailSet::addAll);
 
         return new ArrayList<>(emailSet);
     }
 
-    private static String makeDuplicatingWord(String name, int index) {
+    private Set<String> findAllInvalidEmailsFromUser(User user) {
+        TreeSet<String> result = new TreeSet<>();
+        String name = user.getName();
+        for (int spellIndex = 0; spellIndex < name.length() - 1; spellIndex++) {
+            String invalidWord = makeInvalidWord(name, spellIndex);
+            Set<String> invalidEmails = findInvalidEmails(user, invalidWord);
+            result.addAll(invalidEmails);
+        }
+        return result;
+    }
+
+    private Set<String> findInvalidEmails(User user, String duplicatingWord) {
+        return userRepository.findAllByNameContaining(duplicatingWord)
+                .stream().filter(find -> !user.equals(find))
+                .map(find -> find.getEmail())
+                .collect(Collectors.toSet());
+    }
+
+    private static String makeInvalidWord(String name, int index) {
         return name.substring(index, index + 2);
     }
 }
