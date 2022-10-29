b4888aa77ae2660ff5e80f2d64232b6d1a81d5d6
JUN <chltmdwns96@naver.com>
2022-10-29 15:18:54 +0900

    fix : 분리된 문자 저장방식 변경
    ArrayList -> Stack

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,26 +1,24 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        List<Character> seperatedCryptogram = StringToChars(cryptogram);
-
+        Stack<Character> charStack = StringToCharStack(cryptogram);
         return answer;
     }
 
-    protected static List<Character> StringToChars(String text) {
-        List<Character> seperatedText = new ArrayList<>();
+    protected static Stack<Character> StringToCharStack(String text) {
+        Stack<Character> charStack = new Stack<>();
         final int TEXT_LENGTH = text.length();
         for (int i = 0; i < TEXT_LENGTH; i++) {
-            seperatedText.add(text.charAt(i));
+            charStack.push(text.charAt(i));
         }
-        return seperatedText;
+        return charStack;
     }
 
     protected static String CharsToString(List<Character> chars) {
