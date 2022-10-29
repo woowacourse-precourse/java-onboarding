1f5fa27ed897a0a009c74167e37f969b84df1d7f
iamjooon2 <iamjooon2@gmail.com>
2022-10-29 23:50:48 +0900

    refactor(problem3): 문자 존재시 count 증가 메서드 addCount 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,23 +4,32 @@ public class Problem3 {
     public static int solution(int number) {
         int count = 0;
         for (int i = 1; i <= number; i += 1) {
-            count = changeCountByExists(i, count);
-        }
-        return count;
-    }
-    public static int changeCountByExists(int number, int count) {
-        for (int j = 0; j < numberStringify(number).length(); j += 1) {
-            if (isNumber369(numberStringify(number), j)) { count += 1; }
+            count = changeCountByNumberExists(i, count);
         }
         return count;
     }
 
-    public static String numberStringify(int number){
+    public static int changeCountByNumberExists(int number, int count) {
+        for (int j = 0; j < intToString(number).length(); j += 1) {
+            count = addCount(number, j, count);
+        }
+        return count;
+    }
+
+    public static boolean isNumberHas369(int number, int j) {
+        return intToString(number).charAt(j) == '3' || intToString(number).charAt(j) == '6' || intToString(number).charAt(j) == '9';
+    }
+
+    public static String intToString(int number) {
         return number + "";
     }
 
-    public static boolean isNumber369(String string, int j){
-        return string.charAt(j) == '3' || string.charAt(j) == '6' || string.charAt(j) == '9';
+    public static int addCount(int checkingNumber, int checkedNumber, int count) {
+        if (isNumberHas369(checkingNumber, checkedNumber)) {
+            count += 1;
+        }
+        return count;
     }
+
 
 }
