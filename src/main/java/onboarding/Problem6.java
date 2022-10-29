80d77fec943905d9d3a528c17723e4acf14dc42c
박원종 <wonjong1030@naver.com>
2022-10-29 23:14:01 +0900

    feat: 닉네임 순서를 지키며 2개이상 모든 문자열 구하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,15 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        permutation(0,2,0, forms.get(0).get(1),"");
         return answer;
+    }
+    public static void permutation(int depth, int n, int idx, String name, String s){
+        if(depth == n){
+            System.out.println(s);
+        }
+        for(int i=idx; i<name.length(); i++){
+            permutation(depth+1, n, i+1, name, s+name.charAt(i));
+        }
     }
 }
