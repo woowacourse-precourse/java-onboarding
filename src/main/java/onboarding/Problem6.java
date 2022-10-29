3b2ec9696d8825564e642f1c66f8cac63f085760
youhobin <ghdb132@naver.com>
2022-10-29 15:00:42 +0900

    feat(onboarding) : 닉네임을 배열에 담고 닉네임마다 2글자씩 가져오는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,16 +10,49 @@ public class Problem6 {
             formsMap.put((String) form.get(0), (String) form.get(1));
         }
 
-        Collection<String> nicknames = formsMap.values();
+        Collection<String> nickNames = formsMap.values();
+        String[] nickNameArray = nickNames.toArray(new String[0]);
 
-        Iterator iterator = nicknames.iterator();
-        while (iterator.hasNext()) {
-            String nickname = (String) iterator.next();
-            System.out.println(nickname);
-            checkNickName(nickname, nicknames);
+        for (int i = 0; i < nickNameArray.length; i++) {
+            getDuplicationStr(nickNameArray[i], nickNameArray);
         }
+
+//        Collection<String> nicknames = formsMap.values();
+//
+//        Iterator iterator = nicknames.iterator();
+//        while (iterator.hasNext()) {
+//            String nickname = (String) iterator.next();
+//            System.out.println(nickname);
+//
+//            checkNickName(nickname, nicknames);
+//        }
 
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static void getDuplicationStr(String nickName, String[] nickNameArray) {
+        for (int i = 0; i < nickName.length() - 1; i++) {
+            String currChar = String.valueOf(nickName.charAt(i));
+            String nextChar = String.valueOf(nickName.charAt(i + 1));
+            String duplicationWord = currChar + nextChar;
+            System.out.println(duplicationWord);
+            checkDuplication(duplicationWord, nickNameArray);
+
+        }
+    }
+
+
+//    private static void checkNickName(String nickname, Collection<String> nicknames) {
+//        for (int i = 0; i < nickname.length() - 1; i++) {
+//            String currChar = String.valueOf(nickname.charAt(i));
+//            String nextChar = String.valueOf(nickname.charAt(i + 1));
+//            String duplicationWord = currChar + nextChar;
+//            System.out.println(duplicationWord);
+//            nicknames.remove(nickname);
+//            System.out.println(nicknames);
+//
+//        }
+//    }
+
 }
