6eddab9ea0f7341f83cad1cf233f387a32737c0f
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-30 00:25:08 +0900

    Add checkMultiDigitNumbers function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,4 +12,14 @@ public class Problem3 {
         if (number == 3 || number == 6 || number == 9) value = 1;
         return value;
     }
+
+    // 주어진 여러 자리수의 값에 3, 6, 9가 몇 개 들어있는지 확인하는 함수
+    public static int calcMultiDigitNumbers(int number) {
+        int numberOftarget = 0;
+        while (number > 0) {
+            numberOftarget += checkSingleDigitNumber(number % 10);
+            number /= 10;
+        }
+        return numberOftarget;
+    }
 }
