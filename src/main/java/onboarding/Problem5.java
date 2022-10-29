48a7632f47dcebb0b6c688f2e07c17ba2ff01859
HyeokJoon Kong <orijoon98@gmail.com>
2022-10-29 17:26:51 +0900

    feat: 문제5 - 화폐 단위별 개수 리스트 반환하는 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,19 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<Integer> withdraw(int money) {
+        List<Integer> result = new ArrayList<>();
+        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int remaining = money;
+
+        for (int i = 0; i < units.length; i++) {
+            int count = remaining / units[i];
+            remaining %= units[i];
+            result.add(count);
+        }
+
+        return result;
     }
 }
