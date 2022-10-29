43520f194c425bca6f6f98f33235893d2c6e6106
LHS-11 <ghktjq1119@naver.com>
2022-10-29 22:37:34 +0900

    Problem6 : 기능 2,3,4 메서드 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -19,34 +19,23 @@ public class Problem6 {
         List<String> answer = List.of("answer");
 
         ArrayList<String> sameNickNameEmail = new ArrayList<>();
-        
-        Map<String, Integer> nickNameDivideByTwoLetter = new HashMap<>();
-        
-        int crewSize=0;
 
-        for (List<String> form : forms) {
-            crewSize += form.size();
-        }
+        Map<String, Integer> nickNameDivideByTwoLetter = new HashMap<>();
+
+        int crewSize = getCrewSize(forms);
 
         checkException(forms, crewSize);
 
-        for (List<String> form : forms) {
-            String email = form.get(0);
-            String nickName = form.get(1);
-            // 2. 두 글자 이상의 문자가 연속적인 것을 확인하기 위해 교육생의 닉네임을 두 글자씩 끊어서 map 에 넣기
-            for (int i = 0; i < nickName.length()-1; i++) {
-                String nickNameDivide = nickName.substring(i,i+2);
+        makeNickNameDivideByTwoLetter(forms, nickNameDivideByTwoLetter);
 
-                if(isContainNickName(nickNameDivideByTwoLetter, nickNameDivide)){
-                    int nickNameCount = nickNameDivideByTwoLetter.get(nickNameDivide);
-                    nickNameDivideByTwoLetter.put(nickNameDivide, nickNameCount + 1);
-                }
-                if(!isContainNickName(nickNameDivideByTwoLetter,nickNameDivide)){
-                    nickNameDivideByTwoLetter.put(nickNameDivide, 1);
-                }
-            }
-        }
+        makeSameNickNameEmail(forms, sameNickNameEmail, nickNameDivideByTwoLetter);
 
+        answer=sameNickNameEmail.stream().sorted().distinct().collect(Collectors.toList());
+
+        return answer;
+    }
+
+    private static void makeSameNickNameEmail(List<List<String>> forms, ArrayList<String> sameNickNameEmail, Map<String, Integer> nickNameDivideByTwoLetter) {
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickName = form.get(1);
@@ -62,20 +51,44 @@ public class Problem6 {
                 }
             }
         }
+    }
 
-        //5. 오름차순으로 정렬하고 중복은 제거
-        answer=sameNickNameEmail.stream().sorted().distinct().collect(Collectors.toList());
-        
-        return answer;
+    private static void makeNickNameDivideByTwoLetter(List<List<String>> forms, Map<String, Integer> nickNameDivideByTwoLetter) {
+        for (List<String> form : forms) {
+            String nickName = form.get(1);
+            // 2. 두 글자 이상의 문자가 연속적인 것을 확인하기 위해 교육생의 닉네임을 두 글자씩 끊어서 map 에 넣기
+            for (int i = 0; i < nickName.length()-1; i++) {
+                String nickNameDivide = nickName.substring(i,i+2);
+
+                if(!isContainNickName(nickNameDivideByTwoLetter,nickNameDivide)){
+                    nickNameDivideByTwoLetter.put(nickNameDivide, 1);
+                }
+                if(isContainNickName(nickNameDivideByTwoLetter, nickNameDivide)){
+                    int nickNameCount = nickNameDivideByTwoLetter.get(nickNameDivide);
+                    nickNameDivideByTwoLetter.put(nickNameDivide, nickNameCount + 1);
+                }
+
+            }
+        }
+    }
+
+    private static int getCrewSize(List<List<String>> forms) {
+        int crewSize=0;
+        for (List<String> form : forms) {
+            crewSize += form.size();
+        }
+        return crewSize;
     }
 
     private static void checkException(List<List<String>> forms, int crewSize) {
         // 예외사항 1-1 크루 인원수 체크
-        if(isNotMatchingCrewSize(crewSize)){
-            throw new NotMatchingCrewSize("크루 인원수가 맞지 않습니다.");
-        }
+        checkCrewSize(crewSize);
 
         // 예외사항 1-2,3 이메일 체크 (형식 및 길이)
+        checkEmailAndNickName(forms);
+    }
+
+    private static void checkEmailAndNickName(List<List<String>> forms) {
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickName = form.get(1);
@@ -87,6 +100,12 @@ public class Problem6 {
                 throw new NotMatchingNickName("닉네임이 한글 및 길이가 맞지 않습니다.");
             }
 
+        }
+    }
+
+    private static void checkCrewSize(int crewSize) {
+        if(isNotMatchingCrewSize(crewSize)){
+            throw new NotMatchingCrewSize("크루 인원수가 맞지 않습니다.");
         }
     }
 
