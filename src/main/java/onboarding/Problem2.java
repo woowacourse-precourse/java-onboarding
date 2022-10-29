bef4190b1eace3f19d7c12726fea219e48543aa5
kkkwp <chxrryda@gmail.com>
2022-10-30 02:36:32 +0900

    docs(problem2): 문제 2 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
 
         Stack<Character> decodedCryptogram = deleteChar(cryptogram);
         answer = convertToString(decodedCryptogram);
