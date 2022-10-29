da3d07c2a96789250045e61032876952667fcd82
Invidam <hansupark0208@gmail.com>
2022-10-29 13:37:27 +0900

    refactor(Problem1): make constant first page and last page

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,9 +7,16 @@ import java.util.InputMismatchException;
 import java.util.List;
 
 class Problem1 {
+    final static int FIRST_PAGE = 1;
+    final static int LAST_PAGE = 400;
     public static int getUserMaxPoint(List<Integer> pages) {
-        return max(getPageMaxPoint(pages.get(0)),getPageMaxPoint(pages.get(1)));
+        if (pages.get(0) == FIRST_PAGE || pages.get(1) == LAST_PAGE) {
+            throw new InputMismatchException("허용되지 않는 입력입니다.");
+        }
+
+        return max(getPageMaxPoint(pages.get(0)), getPageMaxPoint(pages.get(1)));
     }
+
     public static int getPageMaxPoint(int page) {
         List<Integer> digits = getDigits(page);
 
@@ -19,23 +26,25 @@ class Problem1 {
     private static int getMaxMultiple(List<Integer> digits) {
         int ret = 1;
 
-        for(int digit: digits)
+        for (int digit : digits) {
             ret *= digit;
+        }
 
         return ret;
     }
 
     private static int getMaxSum(List<Integer> digits) {
         int ret = 0;
-        for(int digit: digits)
+        for (int digit : digits) {
             ret += digit;
+        }
         return ret;
     }
 
     private static List<Integer> getDigits(int page) {
         List<Integer> digits = new ArrayList<>();
 
-        while(page > 0) {
+        while (page > 0) {
             digits.add(page % 10);
             page /= 10;
         }
