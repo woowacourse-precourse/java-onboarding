a5911368c6df9b40392d6f5dd27dbe02559220e3
chkim <cndghks0213@gmail.com>
2022-10-30 07:16:34 +0900

    feat(problem4): 주어진 단어의 모든 알파벳을 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,19 +2,22 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (char character : word.toCharArray()) {
+            sb.append(convert(character));
+        }
+        return sb.toString();
     }
 
-    private static char convert(char input) {
-        if (!Character.isAlphabetic(input)) {
-            return input;
+    private static char convert(char character) {
+        if (!Character.isAlphabetic(character)) {
+            return character;
         }
 
-        if (Character.isLowerCase(input)) {
-            return (char)('a' + 'z' - input);
+        if (Character.isLowerCase(character)) {
+            return (char)('a' + 'z' - character);
         }
 
-        return (char)('A' + 'Z' - input);
+        return (char)('A' + 'Z' - character);
     }
 }
