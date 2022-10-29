94a9720aa2bb8cfd5316ecdc0dcd9dfdd6d9859a
junho lee <junho1273@gmail.com>
2022-10-29 20:33:21 +0900

    Problem7 기능목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,14 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
-
+import java.util.*;
+// 기능목록
+//1.사용자와 친구인 유저의 이름 배열에 담기
+//2.사용자와 함께 아는 친구의 이름 해쉬맵에 추가 후 10점 주기
+//3.방문한 유저를 해쉬맵에 넣고 1점 주기 이미 값이 있다면 1점 추가
+//4.해쉬맵을 value값으로 내림차순 정렬 후 retun!
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
         return answer;
     }
 }
