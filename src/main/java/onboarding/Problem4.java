b71533434060ee0ebbdfbec49e7c5b4a4f14e803
charlesuu <tiger0192004@gmail.com>
2022-10-29 23:22:20 +0900

    <문제4> - (기능 1).  청개구리 사전을 참고해 알파벳 1개를 반대로 변환하기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
         return answer;
+    }
+
+
+    public static char reverseAlphabet(char character) {
+        if ((65 <= character && character <= 90) || (97 <= character && character <= 122)) {
+            if (Character.isUpperCase(character)) {
+                character = (char)(65 + 90 - character);
+            } else {
+                character = (char)(97 + 122 - character);
+            }
+        }
+        return character;
     }
 }
