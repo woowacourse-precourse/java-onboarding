3a310d109187aa49488531b859b2230999af960c
dclxxi <dclxxi@kakao.com>
2022-10-29 17:33:27 +0900

    feat(문제5): 변환한 화폐의 개수를 금액이 큰 순서대로 반환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    public static final int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return withdraw(money);
+    }
+
+    public static List<Integer> withdraw(int money) {
+        List<Integer> moneyCnt = new ArrayList<>();
+
+        for (int unit : units) {
+            setMoneyCnt(moneyCnt, money, unit);
+            money = getChange(money, unit);
+        }
+
+        return moneyCnt;
     }
 
     private static void setMoneyCnt(List<Integer> moneyCnt, int money, int unit) {
