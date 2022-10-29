31fa4b5956ae2ab9af1f8784e489ed39e63c3abe
kkkwp <chxrryda@gmail.com>
2022-10-30 02:19:48 +0900

    feat(deleteChar): 중복된 문자열을 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static Stack<Character> deleteChar(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : cryptogram.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack;
     }
 }
