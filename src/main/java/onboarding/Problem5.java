192ae26923507dbc645dad31b4a00dac907c69fe
lzlkolo <lzlko1209@gmail.com>
2022-10-29 18:26:22 +0900

    PROBLEM5.기능목록 및 예외사항 처리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static void isCheck(int money) {
+        if(money < 1 || money > 1000000) {
+            System.out.println("입력이 잘못되었습니다.");
+        }
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        isCheck(money);
+
         return answer;
     }
 }
