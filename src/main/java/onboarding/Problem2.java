dfde05e59905833849bb4208b75a41075f3422c1
CordHouse <qkqktl5310@gmail.com>
2022-10-30 01:11:54 +0900

    docs : Problem2 기능 목록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/*
+    기능 목록
+    1. 중복 구간을 "*"로 바꾸고 나중엔 ""으로 바꿔준다.
+    2. 문자열의 크기가 바뀌는 것을 이용해 while 탈출 조건을 만든다.
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
