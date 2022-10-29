f21ce53118f371b145695d4276ad1e90ae0748b8
HiiWee <wpdlzhf159@gmail.com>
2022-10-30 04:32:54 +0900

    Feat problem3: 369게임 상수 정의

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem3 {
+    static final int THREE = 3;
+    static final int SIX = 6;
+    static final int NINE = 9;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
