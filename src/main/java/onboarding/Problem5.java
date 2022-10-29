9a8fbc0a185df79e17c5eeec27cc5665ccea348f
jagajang <kosb7544@gmail.com>
2022-10-29 13:57:53 +0900

    p5/add bill/coin count

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     private static final List<Integer> moneySize = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for(int m : moneySize) {
+            answer.add(money / m);
+            money %= m;
+        }
+
         return answer;
     }
 }
