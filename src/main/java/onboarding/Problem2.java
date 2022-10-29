8b7c8128f1295da85d77fc883c109ce209e6f846
dong young <dong5854@gmail.com>
2022-10-30 03:24:31 +0900

    [Problem2] feat: add decrypt function
    중복 문자들을 삭제하는 decrypt 함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,33 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = decrypt(cryptogram);
         return answer;
+    }
+
+    public static String decrypt(String cryptogram){
+        StringBuilder result = new StringBuilder(cryptogram.length());
+        boolean done = true;
+
+        for (int i = 0; i < cryptogram.length(); i++){
+            if(i - 1 >= 0){
+                if (cryptogram.charAt(i) == cryptogram.charAt(i-1)){
+                    done = false;
+                    continue;
+                }
+            }
+            if (cryptogram.length() > i + 1) {
+                if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                    done = false;
+                    continue;
+                }
+            }
+            result.append(cryptogram.charAt(i));
+        }
+
+        if (!done)
+            return decrypt(result.toString());
+        else
+            return result.toString();
     }
 }
