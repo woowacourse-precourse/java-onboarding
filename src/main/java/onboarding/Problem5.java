7e7b796aac79716da370d563b1a430327d2855c1
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-10-29 16:01:30 +0900

    Problem5 일원짜리 변환 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int a = money;
+        List<Integer> change = new ArrayList<>();
+        a = oneWon(a,change);
+
+
+        return change;
     }
+
+    public static int oneWon(int a, List<Integer> change) {
+        change.add(a % 10);
+        return a / 10;
+    }
+
+
 }
