45e7a165f4ecefdc3513f791aab56f18c1e21daa
syahn <496300@naver.com>
2022-10-29 19:13:13 +0900

    refactor: 입력 조건 검사 기능 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,19 +6,28 @@ class Problem1 {
     //입력 조건을 검사하는 method
     public static boolean verifyConditions(List<Integer> test){
         boolean metConditions = true;
-        if(!(test.get(0) >=1 & test.get(0) <=400))
+        if(!(test.get(0) >=1 & test.get(0) <=399))
+            metConditions = false;
+        else if(!(test.get(1) >=2 & test.get(1) <=400))
             metConditions = false;
         else if((test.get(0)%2 != 1) | (test.get(1)%2 !=0))
             metConditions = false;
         return metConditions;
     }
 
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        //입력 조건 만족하지 않을 경우 예외사항 return
-        if(!verifyConditions(pobi))
-            return -1
 
+
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //입력 조건 만족하지 않을 경우 예외사항 return
+        if(!(verifyConditions(pobi)))
+            answer =  -1;
+        else if(!(verifyConditions(crong)))
+            answer = -1;
+
+
+
         return answer;
     }
 }
