5e1cc618075244f2c8f0a3a8d88a6c1a33f571f8
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-10-30 00:29:11 +0900

    solve problem5

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,9 +2,11 @@ package onboarding;
 
 
 /*
-* 1. 65~90인지 확인 // 72.5, 109.5
-* 2. 그렇다면, c -  or 97~122  12.5
-* */
+* 0. word 내의 모든 char c에 대해서 반복
+* 1. c가  알파벳에 해당하는 지 확인
+* 1-1. 알파벳이 아니라면, answer에 append()
+* 1-2. 알파벳이라면, 뒤집어서 append()
+ * */
 
 
 
@@ -20,19 +22,8 @@ public class Problem4 {
         return answer.toString();
     }
 
-    public static void main(String[] args) {
-//        System.out.println(reverseCharacter('a'));
-//        System.out.println(reverseCharacter('m'));
-//        System.out.println(reverseCharacter('n'));
-//        System.out.println(reverseCharacter('M'));
-//        System.out.println(reverseCharacter('N'));
-//        System.out.println();
-    }
-
     private static char reverseCharacter(char c) {
-        System.out.println("c = " + (int)c);
         double distance = c <= 90 ? 77.5 - c : 109.5 - c;
-        System.out.println("distance = " + distance);
 
         return (char) (c + (int) (distance*2));
     }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+//        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] units = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int unit : units) {
+            answer.add(money / unit);
+            money %= unit;
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(15000));
     }
 }
