66044d73b07843c22d1e13f81bb6c35b09706381
정핸모 <gusah009@naver.com>
2022-10-29 14:42:50 +0900

    feat[문제6번]: 닉네임을 2글자 단위로 잘라서 모두 저장하는 parseNickname 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
 
   public static List<String> solution(List<List<String>> forms) {
-    List<String> answer = List.of("answer");
-    return answer;
+    Map<String, List<Integer>> nicknameParseMap = new HashMap<>();
+    parseNickname(forms, nicknameParseMap);
+  }
+
+  private static void parseNickname(List<List<String>> forms,
+      Map<String, List<Integer>> nicknameParseMap) {
+    for (int crewIndex = 0; crewIndex < forms.size(); crewIndex++) {
+      List<String> form = forms.get(crewIndex);
+      String nickname = form.get(1);
+      for (int i = 1; i < nickname.length(); i++) {
+        String substring = nickname.substring(i - 1, i + 1);
+        if (nicknameParseMap.containsKey(substring)) {
+          nicknameParseMap.get(substring).add(crewIndex);
+        } else {
+          nicknameParseMap.put(substring, new ArrayList<>(List.of(crewIndex)));
+        }
+      }
+    }
   }
 }
