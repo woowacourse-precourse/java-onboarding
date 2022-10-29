f82bac6fb42e0474949b33afc8634f1a12cf4045
song-wooseok <sws981202@gmail.com>
2022-10-30 00:36:01 +0900

    feat: [PROBLEM2 - 예외사항4] 모든 예외사항 검사

---
+++ src/main/java/onboarding/problem2/utils/validator/exception/InvalidStringException.java
@@ -0,0 +1,8 @@
+package onboarding.problem2.utils.validator.exception;
+
+public class InvalidStringException extends Exception{
+
+    public InvalidStringException(String msg){
+        super(msg);
+    }
+}
