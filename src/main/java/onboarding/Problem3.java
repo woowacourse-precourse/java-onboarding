d7f280c5ea018dc5e05131400434befc472015c7
Noah <shgustmd6865@naver.com>
2022-10-29 21:29:44 +0900

    Feat : 1부터 number까지 숫자에 대해 탐색 기능 추가
    반복문을 이용하여 1부터 number까지 숫자를 탐색합니다
    각 자리수에 대해서 3, 6, 9의 갯수에 대해 파악하기 위해 char[] 배열로 변환 합니다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i=1; i<=number; i++) {
+            char[] tmp = Integer.toString(i).toCharArray();
+        }
         return answer;
     }
 }
