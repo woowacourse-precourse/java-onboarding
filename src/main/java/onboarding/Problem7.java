d024fc5c93e745deed91adcccb19e56853fa0118
UJ15 <dbwogml15@naver.com>
2022-10-29 19:38:53 +0900

    refactor : remove friends memory class for simple code

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,13 +6,14 @@ import java.util.stream.Collectors;
 public class Problem7 {
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        FriendsMemory memory = new FriendsMemory(new HashMap<>(), new HashMap<>());
+        Map<String, List<String>> friendsList = new HashMap<>();
+        Map<String, Integer> recommendPoint = new HashMap<>();
 
-        friends.forEach(f -> addFriendList(f, memory));
-        increaseFriendPoint(user, memory);
-        increaseVisitorPoint(visitors, user, memory);
+        friends.forEach(f -> addFriendList(f, friendsList));
+        increaseFriendPoint(user, friendsList, recommendPoint);
+        increaseVisitorPoint(visitors, user, friendsList, recommendPoint);
 
-        return memory.getRecommendPoint().entrySet().stream()
+        return recommendPoint.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .map(Map.Entry::getKey)
@@ -20,9 +21,12 @@ public class Problem7 {
                 .collect(Collectors.toList());
     }
 
-    private static void increaseVisitorPoint(List<String> visitors, String user, FriendsMemory memory) {
-        Map<String, List<String>> friendsList = memory.getFriendsList();
-        Map<String, Integer> recommendPoint = memory.getRecommendPoint();
+    private static void increaseVisitorPoint(
+            List<String> visitors,
+            String user,
+            Map<String, List<String>> friendsList,
+            Map<String, Integer> recommendPoint
+    ) {
         List<String> userFriends = friendsList.get(user);
 
         visitors.stream()
@@ -30,9 +34,11 @@ public class Problem7 {
                 .forEach( v -> recommendPoint.put(v, recommendPoint.getOrDefault(v, 0) + 1));
     }
 
-    private static void increaseFriendPoint(String user, FriendsMemory memory) {
-        Map<String, List<String>> friendsList = memory.getFriendsList();
-        Map<String, Integer> recommendPoint = memory.getRecommendPoint();
+    private static void increaseFriendPoint(
+            String user, Map<String,
+            List<String>> friendsList,
+            Map<String, Integer> recommendPoint
+    ) {
         List<String> userFriends = friendsList.get(user);
 
         userFriends.stream()
@@ -43,8 +49,7 @@ public class Problem7 {
                 );
     }
 
-    private static void addFriendList(List<String> friend, FriendsMemory memory) {
-        Map<String, List<String>> friendsList = memory.getFriendsList();
+    private static void addFriendList(List<String> friend, Map<String, List<String>> friendsList) {
         int flag = -1;
 
         for (int i = 0 ; i < 2 ; i++) {
@@ -55,23 +60,5 @@ public class Problem7 {
             flag *= -1;
             friendsList.get(friend.get(i)).add(friend.get(i + flag));
         }
-    }
-}
-
-class FriendsMemory {
-    private final Map<String, List<String>> friendsList;
-    private final Map<String, Integer> recommendPoint;
-
-    public FriendsMemory(Map<String, List<String>> friendsMap, Map<String, Integer> pointMap) {
-        this.friendsList = friendsMap;
-        this.recommendPoint = pointMap;
-    }
-
-    public Map<String, List<String>> getFriendsList() {
-        return friendsList;
-    }
-
-    public Map<String, Integer> getRecommendPoint() {
-        return recommendPoint;
     }
 }
