ddc011904d28e17d108994acb407bbd85e6046da
yun <yshyeon102@gmail.com>
2022-10-29 23:12:14 +0900

    feat: Problem1 - 예외 상황 정의
    예외 상황 발생 시 
    RuntimeException에 메세지를 담아 던지도록 구현.
    
    + 예외 발생 시, 예외 메세지를 볼 수 있도록 solution함수에  예외 메세지를 출력하는 함수 추가
    
    #TODO 
    페이지 범위 부분에서  모든 페이지가 1이상 400이하 인 조건을 적용했지만, 이상-이하가 아니라 초과-미만이 될 수 있음.
    이후 이 조건이 맞는지 확인 필요.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -14,7 +14,28 @@ class Problem1 {
             return findWinner(pobiScore, crongScore);
 
         } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
             return -1;
+        }
+    }
+
+    public static void checkException(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        // 페이지 범위 예외 확인
+        if (leftPage < 1 || leftPage > 400 || rightPage < 1 || rightPage > 400) {
+            throw new RuntimeException("페이지 범위 예외");
+        }
+
+        // 왼쪽 페이지가 짝수인 경우
+        if (leftPage % 2 == 0) {
+            throw new RuntimeException("왼쪽 페이지는 홀수여야 한다.");
+        }
+
+        // 두 수가 연속이 아닌 경우
+        if (rightPage != leftPage + 1) {
+            throw new RuntimeException("두 수는 연속이어야 한다.");
         }
     }
 }
