a9a3ea08594583034739e54ea2d866ea7703a173
김경훈 <kilhyunii@naver.com>
2022-10-29 21:28:19 +0900

    Create solution4: String -> String[] -> 배열 반복 -> 아스키코드형태로 변환하여 계산을 통한, 문자 뒤집기 -> 만들어진 배열을 다시 String으로 변환후 리턴.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String[] splitWord = word.split("");
+        int size = splitWord.length;
+        String[] reverseWord = new String[size];
+        for(int i = 0; i < size ; i++){
+            reverseWord[i] = reverseAlphabet(splitWord[i]);
+        }
+        return String.join("",reverseWord);
+    }
+    private static String reverseAlphabet(String str){
+        if(!Character.isAlphabetic(str.charAt(0))){
+            return str;
+        }
+        /* 아스키코드 형태로 변환 */
+        int k = str.charAt(0);
+        /* 대문자 소문자 판별 */
+        if (k>=97){
+            k = k < 110 ? (109 + Math.abs(k-109) + 1) :
+                    (110 - (Math.abs(k-110) + 1));
+        }else{
+            k = k < 78 ? (77 + Math.abs(k-77) + 1) :
+                    (78 - (Math.abs(k-78) + 1));
+        }
+        return Character.toString((char)k);
     }
 }
