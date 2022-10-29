8f0a3e3979df13bfb1b77ae57c7f7a34cf2d5b59
alstjr706 <alstjr706@naver.com>
2022-10-29 18:03:02 +0900

    feat : (은행 출금 문제) 지폐/동전을 반환해주는 기능 구현

--- src/main/java/onboarding/problem5/Bank.java
+++ src/main/java/onboarding/problem5/Bank.java
@@ -1,17 +1,32 @@
 package onboarding.problem5;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
 
-
     public List<Integer> withdraw(int money) {
-        List<Integer> moneyList = findAmount(money);
-        return moneyList;
+
+        List<Integer> coinChanges = findAmount(money);
+
+        return coinChanges;
     }
+
 
     public List<Integer> findAmount(int money) {
 
-        return null;
+        BanknoteUnit banknoteUnit = new BanknoteUnit();
+
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> coinList = banknoteUnit.CoinList();
+
+        for (Integer banknoteList : coinList) {
+            int result = money / banknoteList;
+
+            arrayList.add(result);
+            money -= (banknoteList + result);
+        }
+
+        return arrayList;
     }
 }
