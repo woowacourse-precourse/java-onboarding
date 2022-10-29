55c7fe3c62da664115de9e1b3abf192d57952fd1
Cho Changhun <cvcvcx@naver.com>
2022-10-29 18:38:23 +0900

    2번문제 while과 StringBuffer 방식을 이용한 기능 구현 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,22 +9,25 @@ public class Problem2 {
 
     public static String checkAfterChar(String cryptogram) {
 
-        StringBuffer sb = new StringBuffer(cryptogram);
-        int counter = 0;
-        while (true){
-            int prevCounter = counter;
-            for(int i =0; i<sb.length()-1; i++){
-                if(sb.charAt(i) == sb.charAt(i+1)){
-                    counter+=1;
-                    sb.deleteCharAt(i+1);
-                    sb.deleteCharAt(i);
-                }
-            }
-            if(prevCounter== counter){
-                break;
-            }
-        }
-            return sb.toString();
+//        StringBuffer sb = new StringBuffer(cryptogram);
+//        System.out.println(sb);
+//        int counter = 0;
+//        while (true){
+//            int prevCounter = counter;
+//            for(int i =0; i<sb.length()-1; i++){
+//                if(sb.charAt(i) == sb.charAt(i+1)){
+//                    counter+=1;
+//                    sb.deleteCharAt(i+1);
+//                    sb.deleteCharAt(i);
+//                }
+//
+//            }
+//            if(prevCounter== counter){
+//                break;
+//            }
+//            System.out.println(sb);
+//        }
+//            return sb.toString();
 
     }
 
