b1ee0955ce6ba78ad1bd3b5e7765e4482e90323b
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-30 02:55:58 +0900

    FEAT : Problem 3-2. 1부터 number까지 수를 탐색하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -21,8 +21,11 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        int endNumber = number;
-        System.out.println(endNumber);
+        int endNumber = 0;
+        for(int i = 1; i < number + 1; i++){
+
+        }
+
         return answer;
     }
 }
