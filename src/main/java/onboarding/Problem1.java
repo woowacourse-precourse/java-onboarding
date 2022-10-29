c812e56cede6d489f9abf230f6f98aae2b2aceef
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-10-30 02:40:02 +0900

    feat: Problem1 풀이
    getScore 메소드를 도입하여 문제를 풀었음

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,38 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
+
+        int pobiScore = Math.max(getScore(pobiLeft), getScore(pobiRight));
+        int crongScore = Math.max(getScore(crongLeft), getScore(crongRight));
+
+        if (pobiScore > crongScore) {
+            return 1;
+        } else if (pobiScore < crongScore) {
+            return -1;
+        } else {
+            return -1;
+        }
     }
+
+    public static int getScore(int page) {
+
+        int summationScore = 0;
+        int multiplicationScore = 1;
+
+        while (page > 0) {
+            int digit = page % 10;
+            summationScore += digit;
+            multiplicationScore *= digit;
+            page /= 10;
+        }
+        return  summationScore > multiplicationScore ? summationScore : multiplicationScore;
+    }
+
+
+
 }
