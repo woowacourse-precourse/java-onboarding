2a836628bb0b6c69cac458e24ddaec715d761686
hayeongKo <polarpheno@icloud.com>
2022-10-29 21:03:36 +0900

    Fix: case3 failed

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,9 +5,7 @@ import java.util.Random;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-//        openBook(pobi);
-//        openBook(crong);
-//        int pobiScore, crongScore;
+
         int pobiScore = getScore(pobi);
         int crongScore = getScore(crong);
         int answer = getResult(pobiScore, crongScore);
@@ -36,6 +34,10 @@ class Problem1 {
         int addScore = 0, multplyScore = 1;
         int leftResult, rightResult, result;
 
+        if (rightPage != leftPage + 1){
+            return 0;
+        }
+
         while(leftPage != 0) {
             addScore += leftPage%10;
             multplyScore *= leftPage%10;
@@ -56,7 +58,10 @@ class Problem1 {
         return result;
     }
     public static int getResult(int pobi, int crong) {
-        if (pobi > crong) {
+
+        if (pobi == 0 | crong == 0) {
+            return -1;
+        } else if (pobi > crong) {
             return 1;
         } else if (pobi == crong) {
             return 0;
