aab0d5f818c5b3ccb952164bf4af322f2959e001
heejeong <hjx66@naver.com>
2022-10-29 22:39:37 +0900

    feat: 문제 4 알파벳 반대로 변환 기능 구현 - 아스키 코드 이용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char letter;
+        for(int i = 0; i < word.length();i++){
+            char c = word.charAt(i);
+            //대문자일때
+            if(c >= 'A' && c <= 'Z')
+                letter = 'A';
+                //소문자일때
+            else if(c >= 'a' && c <= 'z')
+                letter = 'a';
+                //알파벳 아닐 때
+            else{
+                answer += c;
+                continue;
+            }
+            //알파벳 반대로 변환
+            //25-(c-letter)+letter
+            answer += (char)(25-c+2*letter);
+        }
         return answer;
     }
+
 }
