03b2bc5ff2bbf679ad91ff4468e01d92e8649c7e
youngkyunIm <yee0230@naver.com>
2022-10-30 00:11:42 +0900

    feat(Problem7) : make far list

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,36 +7,58 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         List<String> near = new ArrayList<>();
-        Set<String> far = new HashSet<>();
-        Map<String, Integer> result = new HashMap<String, Integer>();
+        List<String> far = new ArrayList<>();
+        Map<String, Integer> result = new HashMap<>();
+
         for (List<String> AB : friends) {
             if (AB.contains(user)) {
                 int index = 1 - AB.indexOf(user);
                 near.add(AB.get(index));
             }
         }
-        for (String name : near) {
-            for (List<String> AB : friends) {
-                int point = 10;
-                if (!AB.contains(user) && AB.contains(name)) {
-                    int index = 1 - AB.indexOf(name);
-                    if (near.contains(AB.get(index))) continue;
-                    far.add(AB.get(index));
-                    if (result.get(AB.get(index)) != null) {
-                        point += (result.get(AB.get(index)));
-                    }
-                    result.put(AB.get(index), point);
+
+        for (List<String> AB : friends) {
+            for (String name : AB) {
+                if(near.contains(name) && !AB.contains(user)){
+                    far.addAll(AB);
                 }
             }
         }
-        for (String visit : visitors) {
-            if (near.contains(visit)) continue;
-            int point = 1;
-            if (result.get(visit) != null) {
-                point += (result.get(visit));
+
+        for (int i = far.size() - 1; i >= 0; i--) {
+            if (near.contains(far.get(i))) {
+                far.remove(i);
             }
-            result.put(visit, point);
         }
+
+//        for (String name : near) {
+//            for (List<String> AB : friends) {
+//                int point = 10;
+//                int index = 1 - AB.indexOf(name);
+//                if (!AB.contains(user) && AB.contains(name) && !near.contains(AB.get(index))) {
+//                    far.add(AB.get(index));
+//                    if (result.get(AB.get(index)) != null) {
+//                        point += (result.get(AB.get(index)));
+//                    }
+//                    result.put(AB.get(index), point);
+//                }
+//            }
+//        }
+//
+        for (String name : visitors) {
+            if (near.contains(name)) continue;
+            int point = 1;
+            if (result.get(name) != null) {
+                point += (result.get(name));
+            }
+            result.put(name, point);
+        }
+
+        answer = sortedAnswer(answer, result);
+        return answer;
+    }
+
+    private static List<String> sortedAnswer(List<String> answer, Map<String, Integer> result) {
         int count = 0;
 
         List<Map.Entry<String, Integer>> entries = result.entrySet().stream()
