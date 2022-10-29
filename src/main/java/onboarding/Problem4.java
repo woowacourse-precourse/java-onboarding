7decb8556a0bd514ecd496209ecd314d673bc433
abbd2 <ajkety00@gmail.com>
2022-10-30 02:38:03 +0900

    영문자가 아닐 경우의 로직을 구현, 변경된 문자를 StringBuilder에 추가해준 후 String형으로 변환 후 최종 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,11 +15,15 @@ public class Problem4 {
                 ascii = 65;
             }else if(Character.isLowerCase(wordTemp)){
                 ascii = 97;
+            }else {
+                sb.append(wordTemp);
+                continue;
             }
             // ascii에서 해당 문자의 아스키 코드 값을 밴 후 절댓값을 취함
             absVal = Math.abs(ascii - wordTemp);
             // ascii에서 영문자의 개수 만큼 더한 후 avsVal을 빼게되면 반대 영문자로 변환됨
             reverseWord = (ascii + 25) - absVal;
+            sb.append((char) reverseWord);
         } // end for
 
         String answer = sb.toString();
