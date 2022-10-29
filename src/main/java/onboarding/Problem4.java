c0fd91723d150a873756d617fbcdcb6f541bf3d8
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-29 19:00:17 +0900

    Fix(Problem4): 입력이 빈공간이면 null을 리턴하는 문제 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -12,6 +12,8 @@ public class Problem4 {
     static HashMap<Character, Character> makeHashMap() {
         HashMap<Character, Character> alphabetHash = new HashMap<>();
 
+        alphabetHash.put(' ', ' ');
+
         for (int i = 0; i < 26; i++) {
             alphabetHash.put((char) (65 + i), (char) (90 - i));
             alphabetHash.put((char) (97 + i), (char) (122 - i));
