491dbe0ab5b304310363a855be50f89a276f04ec
newh08 <newh08@gmail.com>
2022-10-29 13:05:52 +0900

    Problem6 checkSame 메서드 구현완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,12 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+    public static boolean checkSame(String nickname1, String nickname2){
+        for(int i=0; i<nickname1.length()-1;i++){
+            if(nickname2.contains(nickname1.substring(i,i+2))){
+                return true;
+            }
+        }
+        return false;
+    }
 }
