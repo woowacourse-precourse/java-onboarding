34dd28b4c339b429115a0bd72ca04974f45c8cf2
h0ngwon <w___on@naver.com>
2022-10-29 15:38:27 +0900

    3,6,9 가 들어가는 숫자를 판별하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++) {
+            int tmp = i;
+            while(tmp != 0) { // 자리수를 구하여 3,6,9 일때 카운트 증가
+                int remainder = tmp % 10;
+                if(remainder == 3 || remainder == 6 || remainder == 9) {
+                    answer++;
+                }
+                tmp /= 10;
+            }
+        }
         return answer;
     }
 }
