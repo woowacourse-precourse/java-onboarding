063f9aa137e7513c918f5d41302a12b1970edf05
parkmuhyeun <pjhg410@gmail.com>
2022-10-29 09:55:16 +0900

    feat: [Problem3] 해당 숫자가 3, 6, 9인지 검사

--- src/main/java/problem3/Claps.java
+++ src/main/java/problem3/Claps.java
@@ -20,4 +20,22 @@ public class Claps {
     private static String[] getSplit(int num) {
         return String.valueOf(num).split("");
     }
+
+    private static int getClapCount(int[] digits) {
+        int count = 0;
+        for (int digit : digits) {
+            count = addCount(count, digit);
+        }
+        return count;
+    }
+
+    private static int addCount(int count, int num) {
+        if (isClap(num))
+            count += 1;
+        return count;
+    }
+
+    private static boolean isClap(int num) {
+        return num == 3 || num == 6 || num == 9;
+    }
 }
