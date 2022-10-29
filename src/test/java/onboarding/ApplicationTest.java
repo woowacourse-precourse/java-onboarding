69e6f8ac9eac60cf5bca1adb6363ff8b6bad6afe
K-Diger <kitten.diger@gmail.com>
2022-10-30 02:06:44 +0900

    test : Problem 1, 테스트 케이스 추가 - List.of(399, 400) - List.of(1, 2) 에 대한 테스트 케이스 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -33,6 +33,14 @@ class ApplicationTest {
             int result = -1;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
+
+        @Test
+        void case4() {
+            List<Integer> pobi = List.of(399, 400);
+            List<Integer> crong = List.of(1, 2);
+            int result = 1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
     }
 
     @Nested
@@ -62,6 +70,13 @@ class ApplicationTest {
         void case4() {
             String cryptogram = "browoanooommnaon";
             String result = "brown";
+            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
+
+        @Test
+        void case5() {
+            String cryptogram = "bjhhjb";
+            String result = "";
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
         }
     }
