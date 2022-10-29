5fe34f375e719ab984fa1676ad9e59f788e80045
hyun-seung <ardimento22@naver.com>
2022-10-30 01:49:22 +0900

    [REFACTOR] 문제의 제한사항에 부합하는지 확인한다.
    Refactoring by Refactoring list 1. 예외사항 추가하기 (길이 체크)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,9 +4,32 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
+        if(!verificationCheck(cryptogram)) {
+            return "제한사항을 확인해주세요!";
+        }
+
         answer = solveRepetition(cryptogram);
 
         return answer;
+    }
+
+    public static String solveRepetition(String target) {
+        int startPoint = getRepetitionStartPoint(target);
+
+        if (startPoint != -1) {
+            int endPoint = getRepetitionEndPoint(target, startPoint);
+            target = cutRepetitionPart(target, startPoint, endPoint);
+            return solveRepetition(target);
+        }
+
+        return target;
+    }
+
+    public static boolean verificationCheck(String target) {
+        if(target.length() < 1 || target.length() > 1000) {
+            return false;
+        }
+        return true;
     }
 
     public static int getRepetitionStartPoint(String target) {
@@ -32,17 +55,5 @@ public class Problem2 {
 
     public static String cutRepetitionPart(String cryptogram, int startPoint, int endPoint) {
         return cryptogram.substring(0, startPoint) + cryptogram.substring(endPoint);
-    }
-
-    public static String solveRepetition(String target) {
-        int startPoint = getRepetitionStartPoint(target);
-
-        if (startPoint != -1) {
-            int endPoint = getRepetitionEndPoint(target, startPoint);
-            target = cutRepetitionPart(target, startPoint, endPoint);
-            return solveRepetition(target);
-        }
-
-        return target;
     }
 }
