5160eca6d24414d4859e6025c1bee387d7a0b47d
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-29 22:25:34 +0900

    Problem5(설계): 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,26 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    //돈을 환급할 화폐 리스트
+    private final int[] currencyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    /**
+     * 주어진 금액을 큰 단위의 화폐부터 담아 출금
+     * @param money 출금을 하고자 하는 액수
+     * @return 단위가 큰 화폐 순서대로 개수가 담긴 리스트
+     */
+    private List<Integer> getWithDrawList(int money){
+
+    }
+
+    /**
+     * 주어진 금액을 주어진 화폐로 바꿀 수 있는 최대 개수를 반환
+     * @param money : 주어진 금액
+     * @param currency : 바꿀 화폐 단위
+     * @return 화폐 단위로 바꿀 수 있는 최대 개수
+     */
+    private int getCurrencyCount(int money, int currency){
+
+    }
 }
