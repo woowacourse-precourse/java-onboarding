bc8e68c333d128c1e272986afd65e2150de3d7e3
yewon-Noh <yewo2nn16@gmail.com>
2022-10-30 00:47:21 +0900

    Problem4 : 문자를 반대로 변환(청개구리 사전)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,29 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /***
+     * 문자를 반대로 변환(청개구리 사전)
+     * @param x
+     * @param isLower
+     * @return
+     */
+    private static char getaChar(char x, boolean isLower) {
+        char[] table = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+        // 대문자로 바꿔서 변환 수행
+        x = Character.toUpperCase(x);
+
+        int i=0;
+        boolean check = false;
+        while(!check) {
+            if(x == table[i]) check=true;
+            i++;
+        }
+
+        // 청개구리 문자 = 사전의 길이 - 원래 문자의 위치
+        if(isLower)
+            return Character.toLowerCase(table[table.length-i]);
+        else return table[table.length-i];
+    }
 }
