745a4237c181ad9af6954f63cb2bd41c876af50c
DongwookKim0823 <dwkim0823@naver.com>
2022-10-29 11:17:26 +0900

    feat: (Problem6) member list to map

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        Map<String, String> memberMap = memberListToMap(forms);
+        List<String> answer = findDuplicatedNickname(fo);
         return answer;
+    }
+
+    private static Map<String, String> memberListToMap(List<List<String>> forms) {
+
+        Map<String, String> member = new HashMap<>();
+        for (List i : forms) {
+            member.put((String) i.get(1), (String) i.get(0));
+        }
+        return member;
     }
 }
