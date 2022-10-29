15c4bc31476f24c46d33911666505c350eddf336
최형준 <choihyoungjun@choehyeongjun-ui-MacBookPro.local>
2022-10-29 15:40:33 +0900

    solve problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            int ascii = (int) word.charAt(i);
+            if (ascii <= 90 && ascii >= 65) {
+                if (ascii <= 77) {
+                    answer += (char) (90 - (ascii - 65));
+                } else if (ascii >= 78) {
+                    answer += (char) (65 + (90 - ascii));
+                }
+            } else if (ascii <= 122 && ascii >= 97) {
+                if (ascii <= 109) {
+                    answer += (char) (122 - (ascii - 97));
+                } else if (ascii >= 110) {
+                    answer += (char) (97 + (122 - ascii));
+                }
+            } else {
+                answer += word.charAt(i);
+            }
+        }
         return answer;
     }
 }
