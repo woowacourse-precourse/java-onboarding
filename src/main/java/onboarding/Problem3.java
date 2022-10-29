cd79358f825b4b4b68468eb4450540ab6461bfdb
soyunim <togksk123@naver.com>
2022-10-29 23:36:30 +0900

    Problem3 1)split each numbers and put them in the list

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.List;
+import java.util.ArrayList;
 public class Problem3 {
     public static int solution(int number) {
+        List<Integer> num = new ArrayList<>();
         int answer = 0;
+        int n;
+
+        // split each numbers, put them in the list
+        for (int i = 1; i <= number; i++) {
+            n=i;
+            while(n>0){
+                num.add(n%10);
+                n/=10;
+            }
+        }
+
         return answer;
     }
 }
