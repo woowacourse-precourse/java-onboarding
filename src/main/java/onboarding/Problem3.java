7c43204a1dd7ff96143f04eb9baed139cc6df34e
chkim <cndghks0213@gmail.com>
2022-10-30 06:31:05 +0900

    feat(problem3): 숫자가 주어졌을 때 손뼉을 몇 번 쳐야 하는지 횟수를 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Problem3 {
+    private static final Set<String> clapNumbers = new HashSet<>(List.of("3", "6", "9"));
+    private static final String SPACE = "";
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int claps(int number) {
+        return Arrays.stream(String.valueOf(number).split(SPACE))
+                .filter(clapNumbers::contains)
+                .collect(Collectors.toList())
+                .size();
     }
 }
