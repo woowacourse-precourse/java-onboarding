1d3e3035808668add031f0e2d4801ef95452664f
lvalentine6 <ysr1227@naver.com>
2022-10-29 21:33:48 +0900

    feat : 문제 6_닉네임 조합 만드는기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        for (int i = 0; i < forms.size(); i++) {
+            String[] nickArr = forms.get(i).get(1).split("");
+            System.out.println(Arrays.toString(nickArr));
+            combination(nickArr, "", 0, 0);
+        }
+
         return answer;
+    }
+    static void combination(String[] nickArr, String str, int idx, int depth) {
+        if(depth > 1) {
+
+        }
+
+        for (int i = idx; i < nickArr.length; i++) {
+            combination(nickArr, str + nickArr[i], i + 1, depth + 1);
+        }
     }
 }
