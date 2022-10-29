ff0b54b0b536f93113682f35a3fc0298c62a694c
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-10-30 01:55:52 +0900

    problem2 deleteConsecuriveChar구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = deleteConsecutiveChar(cryptogram);
         return answer;
+    }
+    public static String deleteConsecutiveChar(String cryptogram)
+    {
+        int i=1;
+        while (true)
+        {
+            if(i==cryptogram.length()||cryptogram.length()==0)
+            {
+                break;
+            }
+            if(cryptogram.charAt(i)==cryptogram.charAt(i-1)) {
+                cryptogram = cryptogram.substring(0, i - 1) + cryptogram.substring(i+1);
+                i = 1;
+                continue;
+            }
+            i++;
+
+        }
+        return cryptogram;
     }
 }
