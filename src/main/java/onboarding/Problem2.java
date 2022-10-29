d1c76c306ee980d70fb789f8d2dd5db56595a387
Invidam <hansupark0208@gmail.com>
2022-10-29 16:26:41 +0900

    feat(Problem2): add method remove duplicates
    add method that remove duplicates and return deque.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,24 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
+
+    private static Deque<Character> removeDuplicates(String cryptogram) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char ch : cryptogram.toCharArray()) {
+
+            if (!deque.isEmpty() && deque.getLast() == ch) {
+                deque.removeLast();
+                continue;
+            }
+
+            deque.addLast(ch);
+        }
+        return deque;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
