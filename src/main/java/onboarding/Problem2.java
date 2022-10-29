24cff7db2ec2fba1c6dbcbfc6319644b9b38c63f
olrlobt <317tmdgjs@naver.com>
2022-10-29 18:59:13 +0900

    feat: Problem2 [1]현재 중복된 문자들만 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 현재 중복된 문자들만 제거
+ * 2. 1의 과정에서 변화가 없을때까지 반복
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    static StringBuilder deleteOverlab(StringBuilder code){
+        StringBuilder result = new StringBuilder();
+        boolean overlapChar = false;
+
+        code.append(" ");
+        for(int i =0; i<code.length()-1; i++){
+            if(code.charAt(i) == code.charAt(i+1)){
+                overlapChar= true;
+                continue;
+            }
+            if(overlapChar){
+                overlapChar= false;
+                continue;
+            }
+            result.append(code.charAt(i));
+        }
+        return result;
     }
 }
