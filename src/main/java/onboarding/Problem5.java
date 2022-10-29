0c629a8f8dd9bf7910b5b8f29854d21d6b38360b
2jun0 <soo28819@gmail.com>
2022-10-29 15:05:05 +0900

    Refact: 문제 5번 - WonBils사용

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,28 +1,20 @@
 package onboarding;
 
+import onboarding.problem5.WonBills;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    final static int FIFTY_THOUSAND = 50_000;
-    final static int TEN_THOUSAND = 10_000;
-    final static int FIVE_THOUSAND = 5_000;
-    final static int ONE_THOUSAND = 1_000;
-    final static int FIVE_HUNDRED = 500;
-    final static int ONE_HUNDRED = 100;
-    final static int FIFTY = 50;
-    final static int TEN = 10;
-    final static int ONE = 1;
 
     public static int getCapableBillCount(int bill, int money) {
         return money / bill;
     }
 
-    public static List<Integer> getCapableBillsCount(List<Integer> bills, int money) {
+    public static List<Integer> getCapableBillsCount(WonBills wonbills, int money) {
         ArrayList<Integer> counts = new ArrayList<>();
 
-        for (int bill: bills) {
+        for (int bill: wonbills.getBills()) {
             int count = getCapableBillCount(bill, money);
             money -= count * bill;
 
@@ -33,8 +25,6 @@ public class Problem5 {
     }
 
     public static List<Integer> solution(int money) {
-        return getCapableBillsCount(
-                List.of(FIFTY_THOUSAND, TEN_THOUSAND, FIVE_THOUSAND, ONE_THOUSAND, FIVE_HUNDRED, ONE_HUNDRED, FIFTY, TEN, ONE),
-                money);
+        return getCapableBillsCount(new WonBills(), money);
     }
 }
