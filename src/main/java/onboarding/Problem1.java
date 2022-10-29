fe82f6ed1389ff95d4910eb168605f1c0e34ece0
jinuk <devuks0820@gmail.com>
2022-10-29 13:53:53 +0900

    feat: Problem1 Add calculate error check method
    입력값에 대한 오류 검사를 위한 기능 추가 및 솔루션 함수 수정
    연속된 페이지, 페이지 범위, 좌측 페이지 홀수 및 우측 페이지 짝수 여부에 대한 검사
    예외사항 발생시 -1을 리턴하도록 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,24 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        try {
+
+            int answer =  Integer.MAX_VALUE;
+            Integer pobiScore = findPageScore(pobi);
+            Integer crongScore = findPageScore(crong);
+
+            if(pobiScore > crongScore)
+                return 1;
+            if(pobiScore < crongScore)
+                return 2;
+            if(pobiScore == crongScore)
+                return 0;
+
+            return answer;
+        }
+        catch (Exception e) {
+            return -1;
+        }
     }
 
     private static Integer findPageNumberSum(Integer number) {
@@ -30,10 +46,18 @@ class Problem1 {
         return sum;
     }
 
-    private static Integer findPageScore(List<Integer> pageNums){
+    private static Integer findPageScore(List<Integer> pageNums) throws Exception{
 
         Integer leftPage = pageNums.get(0);
         Integer rightPage = pageNums.get(1);
+
+        checkNumContinuity(leftPage,rightPage);
+
+        checkOddNumber(leftPage);
+        checkEvenNumber(rightPage);
+
+        checkNumRange(leftPage);
+        checkNumRange(rightPage);
 
         Integer leftScore = Math.max(findPageNumberSum(leftPage),findPageNumberProduct(leftPage));
         Integer rightScore = Math.max(findPageNumberSum(rightPage),findPageNumberProduct(rightPage));
@@ -41,4 +65,19 @@ class Problem1 {
         return Math.max(leftScore,rightScore);
     }
 
+    private static void checkNumContinuity(Integer leftNum, Integer rightNum) throws Exception {
+        if(rightNum -1 != leftNum) throw new Exception();
+    }
+
+    private static void checkNumRange(Integer num) throws Exception {
+        if(1>num||400<num) throw new Exception();
+    }
+
+    private static void checkOddNumber(Integer num) throws Exception {
+        if(num % 2 == 0) throw new Exception();
+    }
+
+    private static void checkEvenNumber(Integer num) throws Exception {
+        if(num % 2 == 1) throw new Exception();
+    }
 }
