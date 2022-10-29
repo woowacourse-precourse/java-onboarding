b9cd89b9ff3d6343f26be380d85ab8396512c77f
zcae1020 <zcae1020@naver.com>
2022-10-29 21:41:51 +0900

    feat(Problem3): countThreeSixNine method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,7 +8,14 @@ public class Problem3 {
     
     //숫자내 3,6,9 세기
     private static int countThreeSixNine(int num) {
-        
+        int ret = 0;
+        while(num>0) {
+            if (isThreeSixNine(num%10))
+                ret++;
+
+            num/=10;
+        }
+        return ret;
     }
     
     //3,6,9인지 체크
