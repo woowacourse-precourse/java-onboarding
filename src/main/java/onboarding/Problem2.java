235ceb503ba6fb0a3a17c4c9e6f0163a081a9999
jw0293 <k38836919@gmail.com>
2022-10-29 14:36:08 +0900

    feature : Delete Duplicate Word [중복 문자 삭제 기능]

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,44 @@
 package onboarding;
+import java.util.Stack;
+
+class DeleteDuplicateWord{
+    public String[] stringToArray(String str){
+        return str.split("");
+    }
+
+    public Stack<String> deleteDuplicate(String[] strArray){
+        Stack<String> stk = new Stack<>();
+        for(int i=0;i< strArray.length;i++){
+            if(stk.empty() || !stk.peek().equals(strArray[i])){
+                stk.push(strArray[i]);
+            } else{
+                stk.pop();
+            }
+        }
+        return stk;
+    }
+
+    public String stackToString(Stack<String> stk){
+        StringBuilder result = new StringBuilder();
+        while(!stk.empty()){
+            String popWord = stk.pop();
+            result.append(popWord);
+        }
+        return result.reverse().toString();
+    }
+
+    public String getResult(String gram){
+        String[] strArray = stringToArray(gram);
+        Stack<String> stk = deleteDuplicate(strArray);
+
+        return stackToString(stk);
+    }
+}
 
 public class Problem2 {
+    private final static DeleteDuplicateWord deleteDuplicateWord = new DeleteDuplicateWord();
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return deleteDuplicateWord.getResult(cryptogram);
     }
 }
