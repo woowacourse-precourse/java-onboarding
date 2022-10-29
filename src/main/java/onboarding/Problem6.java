112b92f6868c244e616485cb5c6b88540204c433
nakalic <nakalic28@gmail.com>
2022-10-30 02:44:08 +0900

    feat(Problem6): 닉네임 추출 메소드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = List.of("answer");
+		return answer;
+	}
+
+	private static List collectNickname(List<List<String>> da) {
+		List<String> collection = new ArrayList<>();
+		for (List a : da) {
+			collection.add((String)a.get(1));
+		}
+		return collection;
+	}
 }
