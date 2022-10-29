636251a414cbc0ebbdac214d910989c9cd63fc09
seonghoo1217 <seonghoo1217@naver.com>
2022-10-29 16:25:30 +0900

    feat : Add a Number Range Check Exception on [Task5]

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,14 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        return answer;
+        try {
+            checkNumberRange(money);
+            return answer;
+        }catch (Exception e){
+            return answer;
+        }
+    }
+    public static void checkNumberRange(int money_range) throws Exception {
+        if (money_range<1||money_range>1000000) throw new Exception("숫자 범위 초과");
     }
 }
