aea7c24e6f4ccc681394f63b973bed826072efbb
Bumsoo <rhqjatn2398@naver.com>
2022-10-29 16:23:46 +0900

    feat(Problem1): 입력값 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,33 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int fisrtPage = 1;
+    private static final int lastPage = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!(validateInput(pobi) && validateInput(crong))) {
+            return -1;
+        }
+
+
+
         return answer;
     }
+
+    private static boolean validateInput(List<Integer> input) {
+        if (Math.abs(input.get(0) - input.get(1)) != 1) {
+            return false;
+        }
+
+        for (Integer page : input) {
+            if (page <= fisrtPage || lastPage <= page) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
