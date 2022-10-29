a60be2a856c041a232058a2951f3fe3bc5fe5be9
heohyeonjink <hyeonjin4870@naver.com>
2022-10-29 19:36:38 +0900

    feat(PROBLEM1) : 더하기 기능 구현
    정수를 문자열로 변환한 후, 문자열의 한 문자씩 순환하며 값을 더한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
+
+    public static int Sum(int page){
+        String page_str = String.valueOf(page);
+        int sum = 0;
+        for(char c : page_str.toCharArray()){
+            sum+=(c-48);
+        }
+        return sum;
+    }
+
 }
