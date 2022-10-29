a84e186ee82a746c60648172e7cedeeb2c16bb26
jongmyeongbak <withpleasure@gmail.com>
2022-10-30 01:53:23 +0900

    calculate maxscore of addition and multiplication

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,17 +2,35 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Math.max;
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (isSatisfied(pobi) = 1 && isSatisfied(crong) = 1) {
-            int answer = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE;
+
+        if (isSatisfied(pobi) && isSatisfied(crong)) {
+
+            }
             return answer;
         }
         return -1;
     }
 
-    public static boolean isSatisfied(List<Integer> array) {
+    private static boolean isSatisfied(List<Integer> array) {
         return array.get(0) > 1 && array.get(0) < 399 && array.get(1) - array.get(0) == 1;
+    }
+
+    private static int maxScore(int n) {
+        int rest;
+        int sum = 0;
+        int product = 1;
+        while(n != 0){
+            rest = n % 10;
+            sum += rest;
+            product *= rest;
+            n /= 10;
+        }
+        return max(sum,product);
     }
 
 }
