1d8769b3185e66430cf25ff83f2fee281ade6cf0
박원종 <wonjong1030@naver.com>
2022-10-29 21:29:59 +0900

    feat: 페이지 각 자리 수 더하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,25 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static int sum(List<Integer> list){
+        int max=Integer.MIN_VALUE;
+        int num = 0;
+        for(int i=0; i<2; i++){
+            int cur = list.get(i);
+            if(cur >= 100){
+                num += cur/100;
+                num += (cur%100)/10;
+                num += cur%10;
+            }
+            else{
+                num += cur/10;
+                num += cur%10;
+            }
+            max = Math.max(max, num);
+            num=0;
+        }
+        return max;
+    }
     public static boolean checkException(List<Integer> pobi, List<Integer> crong){
         if(pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1){
             return false;
