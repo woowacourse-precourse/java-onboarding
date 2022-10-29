f6e798d369462807462f06ebfefed68d607ead29
w0ntae <bitcampcoding@gmail.com>
2022-10-29 16:01:45 +0900

    p2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,15 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+    
+        ArrayList<Character> characters = new ArrayList<Character>();
         
-        for(int i = 0; i < cryptogram.length() ; i++){
-            
+        for( char c : cryptogram.toCharArray()){
+            characters.add(c);
         }
         
+        int length = cryptogram.length();
+        int n = 1;
+        while(n < length){
+            if( characters.get(n/1) == characters.get(n-1)){
+                characters.remove(n/1);
+                characters.remove(n-1);
+                n = 1;
+                length -= 2;
+            }
+            else {
+                n += 1;
+            }
+        }
         
+        String answer = "";
         
-        String answer = "answer";
+        for(char c : characters){
+            answer += c;
+        }
+        
         return answer;
     }
 }
