18cf01d9ef43b804238a4b35e09678041c8214ff
kkkwp <chxrryda@gmail.com>
2022-10-29 21:57:52 +0900

    feature(checkPages): 페이지 검증 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static int FIRST_PAGE = 1;
+    static int LAST_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static void checkPages(List<Integer> pages) throws Exception {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        if (leftPage < FIRST_PAGE || rightPage > LAST_PAGE) {
+            throw new Exception("페이지 범위를 초과했습니다.");
+        }
+        if (leftPage + 1 != rightPage) {
+            throw new Exception("연속된 숫자를 입력해야 합니다.");
+        }
+        if (leftPage % 2 != 1) {
+            throw new Exception("왼쪽 페이지는 홀수이어야 합니다.");
+        }
     }
 }
