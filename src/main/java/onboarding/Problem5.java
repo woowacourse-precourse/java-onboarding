650cf3fbdd21b25248bcd3af1b9560b812f4a397
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 20:31:22 +0900

    :sparkles: [문제 5] : 5. 결과 값 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,7 +12,6 @@ public class Problem5 {
     static int[] monetaryUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-
         List<Integer> answer = new ArrayList<>();
         withdraw(money, answer, 0);
         return answer;
@@ -22,7 +21,6 @@ public class Problem5 {
         // 1. withdraw 함수 탈출 조건 : 화폐 단위의 개수 만큼 반복
         // 2. 입력된 money를 n번째(step)화폐 단위로 나누고 값은 answer에 저장,
         // 나머지 값은 다음 화폐 단위와 함께 withdraw에 입력
-
         if (step < 9) {
             int unit = monetaryUnit[step];
             answer.add(money / unit);
