9f52b9e94cae2f1cdd302a8d4f9e22ee0cb02c7e
GraphTree <graphnodesed@gmail.com>
2022-10-29 09:51:34 +0900

    Feat: 스트링 변환을 위해 사용할 dictionary 를 만드는createDictionary 함수 추가
    소문자 알파벳 -> 대문자 알파벳
    대문자 알파벳 -> 소문자 알파벳
    첫째줄 알파벳은 각 자리의 아랫줄 알파벳과 매칭된다
    A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z |
    ->Z | Y | X | W | V | U | T | S | R | Q | P | O | N | M | L | K | J | I | H | G | F | E | D | C | B | A |

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,40 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Problem4 {
     public static String solution(String word) {
+
         String answer = "";
         return answer;
     }
+
+
+    public static Map<String, String> createDictionary() {
+        Map<String, String> map = new HashMap<String, String>();
+
+        String[] parentString = "A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z |"
+                .replace(" |", "")
+                .split(" ");
+        String[] childString = "Z | Y | X | W | V | U | T | S | R | Q | P | O | N | M | L | K | J | I | H | G | F | E | D | C | B | A |"
+                .replace(" |", "")
+                .split(" ");
+
+        for (int i = 0; i < parentString.length; i++) {
+            String parent = parentString[i];
+            String child = childString[i];
+            String parentLower = parent.toLowerCase();
+            String childLower = child.toLowerCase();
+            map.put(parent, child);
+            map.put(parentLower, childLower);
+            map.put(child, parent);
+            map.put(childLower, parentLower);
+        }
+
+        return map;
+    }
+
 }
