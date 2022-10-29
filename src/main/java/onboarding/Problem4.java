f90da9a94fce874c587d6d8112e13b39d2d72a03
iamsseaa <homingsea@gmail.com>
2022-10-30 01:53:30 +0900

    feat(Problem4): 입력값 길이가 1이상 1000이하 범위를 벗어난 경우 예외처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,6 +7,12 @@ import java.util.regex.Pattern;
 public class Problem4 {
     public static String solution(String word) {
 
+        try {
+            ExceptionProblem4.validateStringLength(word);
+        } catch (IllegalArgumentException e){
+            return "";
+        }
+
         char[] wordCharArray = word.toCharArray();
 
         List<Character> answer = new ArrayList<>();
@@ -47,10 +53,17 @@ public class Problem4 {
 class ExceptionProblem4 {
     public static void validateWord(String word) {
         enterWord(word);
+        validateStringLength(word);
     }
 
     public static void enterWord(String word) {
         if (word.equals("")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateStringLength(String word) {
+        if (word.length() < 1 || word.length() > 1000) {
             throw new IllegalArgumentException();
         }
     }
