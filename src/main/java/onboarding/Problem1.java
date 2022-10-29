cb0c4ce1a29170efe24eed23d3e7f842c5d907c8
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-29 21:43:14 +0900

    feat: 페이지 범위 예외 처리 (#1)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int FIRST_PAGE_NUMBER = 1;
+    private static final int LAST_PAGE_NUMBER = 400;
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean isRightPageRange(Integer leftPage, Integer rightPage) {
+        return leftPage > FIRST_PAGE_NUMBER || rightPage < LAST_PAGE_NUMBER;
     }
 }
