c9acd94b73c06459fa9b24352def3d7b1129016b
JUWON LEE <leeez0128@gmail.com>
2022-10-29 23:44:34 +0900

    [Solving] Problem4 -청개구리 사전 문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = likeReverseFrog(word);
         return answer;
+    }
+
+    private static String likeReverseFrog(String word) {
+        StringBuilder result = new StringBuilder();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String dictionary = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        List<String> dictionaryList = new ArrayList<>(List.of(dictionary.split("")));
+
+        List<String> wordList = new ArrayList<>(List.of(word.split("")));
+
+        for (String text : wordList) {
+            if(" ".equals(text)) {
+                result.append(" ");
+                continue;
+            }
+            int idx = alphabet.indexOf(text.toLowerCase());
+            result.append(dictionaryList.get(idx));
+        }
+
+        return result.toString();
     }
 }
