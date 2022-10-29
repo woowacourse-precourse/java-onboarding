7b1adf3d35b9939e1a69180eae9a2ffadb062b0a
JungYoonshin <sju08227@naver.com>
2022-10-30 01:45:05 +0900

    refactor: #problem6 -> 닉네임 중복 검사와 닉네임에서 2글자 추출하는 함수 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,52 +9,47 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
 
-    public static HashMap<String, String> hashMap = new HashMap<>();
     public static HashSet<String> emails = new HashSet<>();
+    public static HashMap<String, String> nameAndEmail = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
-        emails = getEachListInForms(forms);
-        answer = Arrays.asList(sortEmailList(emails));
-
+        for (List<String> nameAndEmailList : forms) {
+            checkDuplication(nameAndEmailList);
+        }
+        answer = sortEmailList(emails);
         return answer;
     }
 
-    private static HashSet<String> getEachListInForms(List<List<String>> forms) {
-        for (List<String> nameAndEmailList : forms) {
-            emails = checkNameDuplication(nameAndEmailList);
-        }
-        return emails;
-    }
-
-    private static HashSet<String> checkNameDuplication(List<String> nameAndEmail) {
+    private static HashSet<String> checkDuplication(List<String> nameAndEmail) {
 
         String name = nameAndEmail.get(1);
         String email = nameAndEmail.get(0);
 
         for (int i = 0; i < name.length() - 1; i++) {
             String key = name.substring(i, i + 2);
-            //해당키가 이미 있다면
-            if (hashMap.containsKey(key)) {
-                String duplicateEmail = hashMap.get(key);
-                emails.add(email);
-                emails.add(duplicateEmail);
-            } else {
-                hashMap.put(key, email);
-            }
+            emails = checkNameDuplication(key, email);
         }
         return emails;
     }
 
-    private static String[] sortEmailList(HashSet<String> emails) {
+    private static HashSet<String> checkNameDuplication(String key, String email){
+        if (nameAndEmail.containsKey(key)) {
+            String duplicateEmail = nameAndEmail.get(key);
+            emails.add(duplicateEmail);
+            emails.add(email);
+        } else {
+            nameAndEmail.put(key, email);
+        }
+        return emails;
+    }
+
+    private static List<String> sortEmailList(HashSet<String> emails) {
         List<String> finalEmail = emails.stream()
             .sorted()
             .collect(Collectors.toList());
 
-        final String[] results = finalEmail.toArray(new String[0]);
-
-        return results;
-
+        return finalEmail;
     }
 }
