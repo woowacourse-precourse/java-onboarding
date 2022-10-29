1e36aa2c12d1721aa7a225e1a863b45ec48d8838
olrlobt <317tmdgjs@naver.com>
2022-10-29 19:52:49 +0900

    feat: Problem4 [1]청개구리 사전대로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+/**
+ * 기능목록
+ * 1. 청개구리 사전대로 변환
+ * 2. 한 글자씩 붙임
+ * */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    static char changeFrog(int c){
+        if(c>='a' && c<='z'){
+            c='a'+'z'-c;
+        } else if (c>='A' && c<='Z') {
+            c='A'+'Z'-c;
+        }
+        return (char)c;
     }
 }
