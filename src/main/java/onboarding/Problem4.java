0589b2d65ddbd8c69122073304598f37eea0ff67
skfkgla <narahim.lee@gmail.com>
2022-10-30 03:10:33 +0900

    feat: Problem4 - 청개구리 사전에 따른 문자 번역기 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,20 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+
+    /**
+     * @param character : 문자
+     * @return  청개구리 사전으로 번역한 문자, 영어만 번역 되고 나머지는 번역되지 않는다.
+     */
+    private static char translateCharacter(char character) {
+        int ascii = (int)character;
+        int result = 0;
+        if (ascii >= 65 && ascii <= 90) {
+            ascii = (155 - ascii);
+        }else if(ascii >= 97 && ascii <= 122){
+            ascii = (219 - ascii);
+        }
+        return (char)ascii;
+    }
 }
