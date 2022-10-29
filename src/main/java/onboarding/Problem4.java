80b089b43f499cc4011bd865fbf3d0c650ca807f
lvalentine6 <ysr1227@naver.com>
2022-10-29 19:21:26 +0900

    feat : 문제 4_문자를 반대로 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,17 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < word.length(); i++) {
-            if(word.charAt(i) == ' ') {
+            char ch = word.charAt(i);
+            if(ch == ' ') {
                 sb.append(" ");
                 continue;
             }
-            boolean flag = checkWord(word.charAt(i));
+            boolean flag = checkWord(ch);
+            sb.append(convertWord(flag, ch));
         }
+
+        answer = sb.toString();
 
         return answer;
     }
@@ -22,5 +28,26 @@ public class Problem4 {
             return false;
         }
         return true;
+    }
+
+    // 문자를 반대로 변환하는 메서드
+    static char convertWord(boolean flag, char word) {
+        if(flag) {
+            HashMap<Character, Character> upperMap = new HashMap<>();
+            char upperStart = 'A';
+            char upperEnd = 'Z';
+            for (int i = 0; i < 26; i++) {
+                upperMap.put(upperStart++, upperEnd--);
+            }
+            return upperMap.get(word);
+        }
+
+        HashMap<Character, Character> lowerMap = new HashMap<>();
+        char lowerStart = 'a';
+        char lowerEnd = 'z';
+        for (int i = 0; i < 26; i++) {
+            lowerMap.put(lowerStart++, lowerEnd--);
+        }
+        return lowerMap.get(word);
     }
 }
