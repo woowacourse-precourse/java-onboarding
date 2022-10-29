fe9bf48d9ef982bb94ebe41fabb57511c36bf4de
HiiWee <wpdlzhf159@gmail.com>
2022-10-30 05:18:58 +0900

    Feat problem4: 필요 상수 정리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+    static final int UPPERCASE_A = 65;
+    static final int LOWERCASE_A = 97;
+    static final int DOUBLE = 2;
+    static final int ALPHABET_COUNT_WITHOUT_A = 25;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
