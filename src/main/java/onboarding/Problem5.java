f27a636b4cb1d8da2e16321c5cb34008f598856f
sen <seeunchoi99@gmail.com>
2022-10-29 13:48:07 +0900

    💡 Problem5 기능 목록 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,12 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 현재 돈에서 변환할 수 있는 가장 큰 금액 구하기
+ * 2. 전체 돈을 변환한 결과 구하기
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
