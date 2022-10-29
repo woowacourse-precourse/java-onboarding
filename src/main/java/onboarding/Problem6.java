8db5d60e3ee33af03e7af4ecab419df897946bef
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-29 21:14:28 +0900

    refactor: Problem6 - 중복 닉네임을 찾아 저장하는 기능의 로직 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,23 +2,28 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Problem6 {
+    public static void main(String[] args) {
+        List<List<String>> forms = List.of(
+            List.of("jm@email.com", "제이엠"),
+            List.of("jason@email.com", "제이슨"),
+            List.of("woniee@email.com", "워니"),
+            List.of("mj@email.com", "엠제이"),
+            List.of("nowm@email.com", "이제엠")
+        );
+        solution(forms);
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
-
         List<String> userNameList = createUserNameList(forms);
         String combinedNames = combineName(userNameList);
         List<String> twoLetterNames = createTwoLetterNameList(combinedNames);
+        Set<String> DuplicateNameList = createDuplicateList(twoLetterNames);
 
-
-        for (List<String> form : forms) {
-            makeWarningList(answer, DuplicateNameList, form);
-        }
-        Collections.sort(answer);
         return answer;
     }
 
@@ -44,9 +49,22 @@ public class Problem6 {
         for (int i = 0; i < str.length - 2; i++) {
             nameList.add(str[i] + str[i+1]);
         }
-        return nameList.stream().filter(i -> !i.contains(",")).collect(Collectors.toList());
+        return nameList.stream().filter(i -> !i.contains(",")).sorted().collect(Collectors.toList());
     }
 
+    public static Set<String> createDuplicateList(List<String> nameList) {
+        Set<String> nameSet = new HashSet<>();
+        for (int i = 0; i < nameList.size()-1; i++) {
+            addDuplicateName(nameSet, nameList.get(i), nameList.get(i+1));
+        }
+        return nameSet;
+    }
+
+    public static void addDuplicateName(Set<String> nameSet, String name1, String name2) {
+        if (name1.equals(name2)) {
+            nameSet.add(name1);
+        }
+    }
 
     public static void makeWarningList(List<String> warningList, Set<String> duplicateList, List<String> form) {
         for (String s : duplicateList) {
