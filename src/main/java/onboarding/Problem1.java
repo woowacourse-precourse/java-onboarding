0775f3e9394abecad8839a66c3224d8e09e26b81
myeongju00 <j51677767@gmail.com>
2022-10-29 16:08:09 +0900

    feat: 포비,크롱 점수 계산 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,9 +6,27 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiScore = Integer.MIN_VALUE;
+        int crongScore = Integer.MIN_VALUE;
+        //TODO(예외 처리)
 
+        //pobi, crong 점수 구하기
+        for (int i : pobi) {
+            pobiScore = Math.max(makeScore(i), pobiScore);
+        }
+        for (int i : crong) {
+            crongScore = Math.max(makeScore(i), crongScore);
+        }
+
+        //포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0
+        if(pobiScore > crongScore) answer = 1;
+        if(pobiScore == crongScore) answer = 0;
+        if(pobiScore < crongScore) answer = 2;
 
         return answer;
+    }
+    private static int makeScore(int score) {
+        return Math.max(sumOfDigits(score), multiplicationOfDigits(score));
     }
     //TODO(페이지 번호의 각 숫자를 모두 더하는 함수)
     private static int sumOfDigits(int pageNumber) {
@@ -29,7 +47,5 @@ class Problem1 {
         }
         return result;
     }
-
-    //TODO(숫자 비교 함수)
 
 }
