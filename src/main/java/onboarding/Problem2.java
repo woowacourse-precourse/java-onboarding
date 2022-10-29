9ccf745dcff6378d4984f9fe0b3d805d97c29189
seonho <ysh10216@gmail.com>
2022-10-29 15:53:26 +0900

    create isDuplicate function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,7 +10,6 @@ public class Problem2 {
         } else if(isUpperCase(cryptogram)){ // 대문자가 있는 경우 false return
             return false;
         }
-        System.out.println("예외처리 통과");
         return true;
     }
 
@@ -23,22 +22,39 @@ public class Problem2 {
         }
         return false;
     }
-    // 2. 연속하는 중복문자를 삭제하는 함수 만들기
-    public static String deleteDuplicate(String cryptogram, String deleteAnswer){
-        for(int i = 0; i<cryptogram.length(); i++){
-            if(cryptogram.indexOf(i) != cryptogram.indexOf(i+1)){
-                deleteAnswer += cryptogram.indexOf(i);
-                return deleteDuplicate(deleteAnswer, deleteAnswer);
+    // 중복이 있는지 체크하는 함수 만들기.
+    public static boolean isDuplicate(String cryptomgram){
+        for(int i = 0; i < cryptomgram.length(); i++){
+            if(cryptomgram.charAt(i) == cryptomgram.charAt(i++)){
+                deleteDuplicate(cryptomgram);
             }
         }
-        System.out.println("answer : " + deleteAnswer);
-        return deleteAnswer;
+        return false;
+    }
+    // 2. 연속하는 중복문자를 삭제하는 함수 만들기
+    public static String deleteDuplicate(String cryptogram){
+        char[] chars = cryptogram.toCharArray();
+        char prev = 0;
+        int k = 0;
+
+        for(char c : chars){
+            if(prev != c){
+                chars[k++] = c;
+
+                System.out.println("next : " + chars[k+1]);
+                System.out.println("current : " + chars[k]);
+                System.out.println("prev : " + c);
+                prev = c;
+            }
+        }
+
+        return new String(chars).substring(0, k);
     }
     public static String solution(String cryptogram) {
         String answer = "answer";
         if(isException(cryptogram)){
             // 중복제거 함수를 통해서 중복제거 하기.
-            answer = deleteDuplicate(cryptogram, cryptogram);
+            answer = deleteDuplicate(cryptogram);
         }
 
         return answer;
