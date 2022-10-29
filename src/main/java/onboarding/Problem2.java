19d23e29f1cec3edb598139be20306c46e95bc02
chesthyeon <niw927@naver.com>
2022-10-29 18:23:38 +0900

    문제 6번 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,12 +1,9 @@
 package onboarding;
 
 public class Problem2 {
-    public static void main(String[] args) {
-        System.out.println(solution("browoanoommnaon"));
-    }
     public static String solution(String cryptogram) {
+       if (cryptogram.length() < 2) return cryptogram; // 길이가 2 미만이면 중복이 존재하지 않는다.
        StringBuilder answer = new StringBuilder(); // 정답을 저장할 스트링빌더 선언
-        if (cryptogram.length() < 2) return cryptogram; // 길이가 1이라면 중복이 존재하지 않는다.
        for(int i = 1; i < cryptogram.length(); i++){ // 모든 문자를 비교를 위한 반복문
            if (cryptogram.charAt(i - 1) != cryptogram.charAt(i)){ // 다음 문자와 현 문자과 같지 않다면
                answer.append(cryptogram.charAt(i-1)); // 스트링 빌더에 추가
@@ -15,10 +12,8 @@ public class Problem2 {
                i++; // 건너뛰기
            }
        }
-
         if (cryptogram.charAt(cryptogram.length() - 1) != cryptogram.charAt(cryptogram.length() - 2)) // 마지막 문자가 중복이 아니라면 리스트에 추가.
             answer.append(cryptogram.charAt(cryptogram.length() - 1));
-
         if (answer.length() == cryptogram.length()) return answer.toString(); // 길이가 같다면 중복이 없다.
         return solution(answer.toString()); // 중복을 체크한다.
     }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static void main(String[] args) {
+        List<List<String>> forms = new ArrayList<>(Arrays.asList(Arrays.asList("jm@email.com", "제이엠"),Arrays.asList("jason@email.com", "제이슨"),
+                Arrays.asList("woniee@email.com", "워니"),Arrays.asList("mj@email.com", "엠제이"),Arrays.asList("nowm@email.com", "이제엠")));
+        System.out.println(solution(forms));
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        Hashtable<String,String> hashtable = new Hashtable<>(); // 이름과 이메일을 저장할 해쉬 테이블
+        HashSet<String> hashSet; // 닉네임이 중복되지 않은 두글자를 저장하기 위해
+        TreeSet<String> treeSet = new TreeSet<>(); // 이메일을 오름차순 정렬하고 중복은 제거할 트리셋
+        for (List<String> form: forms // 모든 입력을 확인
+             ) {
+            for (int i = 1; form.get(1).length() > i; i++){
+                if (hashtable.get(form.get(1).substring(i-1,i+1)) == null){
+                    hashtable.put(form.get(1).substring(i-1,i+1), form.get(0));
+                }
+                else {
+                    treeSet.add(form.get(0));
+                    treeSet.add(hashtable.get(form.get(1).substring(i-1,i+1)));
+                }
+            }
+        }
+        return new ArrayList<>(treeSet);
     }
 }
