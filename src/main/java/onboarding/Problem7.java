2b88ec5502b53eb64dad3ca9d08e39086f790b74
antaeho <an981022@naver.com>
2022-10-29 21:36:40 +0900

    Problem7 refactored

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,10 +10,8 @@ import java.util.List;
 3. 점수 측정 -> value 값으로 정렬후 5명으로 자르기
  */
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        HashMap<String,Integer> dic = new HashMap<>();
-        HashMap<String,Integer> new_dic = new HashMap<>();
-        List<String> std = new ArrayList<>();
+
+    private static HashMap<String,Integer> listOfPeople(List<List<String>> friends,HashMap<String,Integer> dic, List<String> visitors){
         for(List<String> relate : friends) {
             for(int i = 0; i < 2; i++) {
                 dic.put(relate.get(i), 0);
@@ -22,7 +20,10 @@ public class Problem7 {
         for (String who : visitors) {
             dic.put(who, 0);
         }
+        return dic;
+    }
 
+    private static void makeStandard(String user, List<List<String>> friends, List<String> std) {
         for(List<String> relate : friends) {
             if (relate.contains(user)){
                 List<String> list = new ArrayList<>(relate);
@@ -30,26 +31,58 @@ public class Problem7 {
                 std.add(list.get(0));
             }
         }
+    }
+
+    private static void knowTogather(String user, List<List<String>> friends, List<String> std, HashMap<String, Integer> dic) {
         for (List<String> relate : friends) {
             for (String j : std) {
                 if (relate.contains(j) && !relate.contains(user)) {
                     List<String> list2 = new ArrayList<>(relate);
                     list2.remove(j);
                     dic.put(list2.get(0), dic.get(list2.get(0))+10);
-                    }
                 }
             }
-
+        }
+    }
+    
+    private static void whoVisited(List<String> visitors, HashMap<String, Integer> dic) {
         for(String who : visitors){
             dic.put(who, dic.get(who)+1);
         }
+    }
+
+    private static void colanderZero(HashMap<String, Integer> new_dic, List<String> std, HashMap<String, Integer> dic) {
         for(String key : dic.keySet()){
             if(dic.get(key) > 0) {
                 if(!std.contains(key)) new_dic.put(key, dic.get(key));
             }
         }
+    }
+
+    private static void sortByScore(HashMap<String, Integer> new_dic) {
         List<String> listKeySet = new ArrayList<>(new_dic.keySet());
         listKeySet.sort((value1, value2) -> (new_dic.get(value2).compareTo(new_dic.get(value1))));
+    }
+    
+    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        HashMap<String,Integer> member = new HashMap<>();
+        HashMap<String,Integer> new_dic = new HashMap<>();
+        List<String> std = new ArrayList<>();
+
+        HashMap<String,Integer> dic = listOfPeople(friends,member, visitors);
+
+        makeStandard(user, friends, std);
+
+        knowTogather(user, friends, std, dic);
+
+        whoVisited(visitors, dic);
+
+        colanderZero(new_dic, std, dic);
+
+        sortByScore(new_dic);
+
         return new ArrayList<>(new_dic.keySet());
     }
+
 }
