98784b2ab44738c958ffd8e0b741e7f334e40ea9
song-wooseok <sws981202@gmail.com>
2022-10-29 23:04:49 +0900

    feat: [PROBLEM2 - 예외사항1] 주어진 문자열이 영어가 아닌 문자가 섞여있는지 검사

---
+++ src/main/java/onboarding/problem2/utils/validator/exception/NotEnglishException.java
@@ -0,0 +1,7 @@
+package onboarding.problem2.utils.validator.exception;
+
+public class NotEnglishException extends Exception{
+    public NotEnglishException(String msg){
+        super(msg);
+    }
+}
