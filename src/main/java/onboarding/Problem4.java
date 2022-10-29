424077b9f4d9b99ab712decfd48629d6ee80577f
ddingmin <ddingminyt@gmail.com>
2022-10-30 08:30:35 +0900

    feat(Ploblem4): 대문자와 소문자에 따라 변환되는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,4 +8,20 @@ public class Problem4 {
     private static int getAscii(char c){
         return (int)c;
     }
+    private static char changeWord(int c){
+        if ((int)'A' <= c && c <= (int)'Z'){
+            return changeBigCh(c);
+        }
+        if ((int)'a' <= c && c <= (int)'z') {
+            return changeSmallCh(c);
+        }
+        return '\0';
+    }
+    private static char changeBigCh(int c) {
+        return (char) ((-1 * (c - 65)) + (int) 'Z');
+    }
+
+    private static char changeSmallCh(int c) {
+        return (char) ((-1 * (c - 97)) + (int) 'z');
+    }
 }
