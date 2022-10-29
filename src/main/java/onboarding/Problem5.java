b05de8eb4df0a48db79b865044e7918b04bc3a2e
ghkdgus29 <ghkdgus29@naver.com>
2022-10-29 09:02:38 +0900

    금액 배열 만들기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+
+        int[] priceList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
 }
