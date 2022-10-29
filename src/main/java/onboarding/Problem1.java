2bc3d5bad9fee447d5c54c7d8d992c7792cffc95
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-29 17:12:16 +0900

    feat(PROBLEM1): versusScore 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,16 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public int calScore(String str){
-        int mulTmp = 0, addTmp = 0;
+    public int calScore(String str) {
+        int mulTmp = 1, addTmp = 0;
         for (int i = 0; i < str.length(); i++) {
-            addTmp += (int)str.charAt(i);
-            mulTmp *= (int)str.charAt(i);
+            addTmp += (int) str.charAt(i);
+            mulTmp *= (int) str.charAt(i);
         }
         int maxScore = Math.max(addTmp, mulTmp);
         return maxScore;
     }
-    public int showScore(List<Integer> pages){
+
+    public int showScore(List<Integer> pages) {
         int odd = pages.get(0);
         int even = pages.get(1);
 
@@ -24,8 +25,22 @@ class Problem1 {
         return score;
     }
 
+    public int versusScore(List<Integer> pobi, List<Integer> crong) {
+        int pobiScore = showScore(pobi);
+        int crongScore = showScore(crong);
+
+        if (pobiScore > crongScore) {
+            return 1;
+        } else if (pobiScore < crongScore) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        Problem1 problem1 = new Problem1();
+        int answer = problem1.versusScore(pobi, crong);
         return answer;
     }
 }
