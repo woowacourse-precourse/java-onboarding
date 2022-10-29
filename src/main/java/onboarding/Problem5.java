5dcddf7a19c009005aa19d31a3092bfec5d12686
imsongj <imsongj@gmail.com>
2022-10-29 15:34:26 +0900

    feat(Problem5): 각 금액 별 갯수 계산 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] UNITS = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    public static int countMoneyPerUnit(int money, int unit){
+        return money / unit;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        for(int i = 0; i < UNITS.length; i++){
+            int count = countMoneyPerUnit(money, UNITS[i]);
+            answer.add(count);
+        }
         return answer;
     }
 }
