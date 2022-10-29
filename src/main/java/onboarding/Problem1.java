1ff29d4d77773f61a5f386053ea1e9deca74e139
Invidam <hansupark0208@gmail.com>
2022-10-29 13:34:51 +0900

    feat(Problem1): add method get user's max point

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,10 +3,13 @@ package onboarding;
 import static java.lang.Integer.max;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 class Problem1 {
-
+    public static int getUserMaxPoint(List<Integer> pages) {
+        return max(getPageMaxPoint(pages.get(0)),getPageMaxPoint(pages.get(1)));
+    }
     public static int getPageMaxPoint(int page) {
         List<Integer> digits = getDigits(page);
 
@@ -16,27 +19,23 @@ class Problem1 {
     private static int getMaxMultiple(List<Integer> digits) {
         int ret = 1;
 
-        for (int digit : digits) {
+        for(int digit: digits)
             ret *= digit;
-        }
 
         return ret;
     }
 
     private static int getMaxSum(List<Integer> digits) {
         int ret = 0;
-
-        for (int digit : digits) {
+        for(int digit: digits)
             ret += digit;
-        }
-
         return ret;
     }
 
     private static List<Integer> getDigits(int page) {
         List<Integer> digits = new ArrayList<>();
 
-        while (page > 0) {
+        while(page > 0) {
             digits.add(page % 10);
             page /= 10;
         }
@@ -46,7 +45,6 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        
         return answer;
     }
 }
