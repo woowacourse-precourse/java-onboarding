a229450ceb3e74f6f1daefd6a09010e73d1d85d9
can019 <jys01012@gmail.com>
2022-10-29 22:20:46 +0900

    :construction: Input exception 처리 method 정의

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,24 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static boolean inputExceptionChecker(int number){
+        // @TODO :: Input Exception 처리
+        return false ;
+    }
+
+    /**
+     *
+     * Target이 min 이상 max 이하에 있는 값인지 검사
+     * @param min 검사하고 싶은 범위 중 작은 값
+     * @param max 검사하고 싶은 범위 중 큰 값
+     * @param target 검사하고 싶은 값
+     * @return 사이에 존재한다면 true, 아니라면 false를 반환
+     */
+    private static boolean isBetween(int min, int max, int target){
+        if(min<=target && target<=max)
+            return true;
+        else
+            return false;
+    }
 }
