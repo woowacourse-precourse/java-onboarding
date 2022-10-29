03672dd0d41ad4b08f0aaf06d65dc0474c7080fa
Seoyoung <ryupark1006@gmail.com>
2022-10-29 12:29:35 +0900

    feat: solve problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] chars = word.toCharArray();
+
+        for (char c : chars) {
+            if (Character.isAlphabetic(c)) {
+                if (Character.isUpperCase(c))
+                    c = (char)((int)'A' + (int)'Z' - (int)c);
+                else
+                    c = (char)((int)'a' + (int)'z' - (int)c);
+            }
+            answer += c;
+        }
+
         return answer;
     }
 }
