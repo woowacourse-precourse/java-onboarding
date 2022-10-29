3a0f29ccba5b870fffba44b68755cd3db87b62c5
leesky <leehaneul0623@gmail.com>
2022-10-29 11:02:31 +0900

    refactor: 누락된 예외 처리 구현(문제 3)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,9 @@
 package onboarding;
 public class Problem3 {
-
     public static int solution(int number) {
-        if (number < 3) return 0;
+        if(!(0 < number && number < 10001)) return 0;
+        if(number < 3) return 0;
+
         int answer = 0;
         for (int i = 3; i <= number; i++) {
             answer += getClapCount(i);
