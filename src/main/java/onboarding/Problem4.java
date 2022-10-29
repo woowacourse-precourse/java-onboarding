f0fbbe17938efbe5b11aa37343d277361d1e6e8b
bini59 <myuni0234@gmail.com>
2022-10-29 17:03:40 +0900

    feat : Check alphabet
    check_alpha
    % input  : char a
    % output : boolean
    
    when char a is alphabet return true, when not return false

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem4 {
+    /*
+        check_alpha
+        % input  : char a
+        % output : boolean
+
+        when char a is alphabet return true, when not return false
+     */
+    public static boolean check_alpha(char a){
+        return a >= 65 && a <= 90 || a >= 97 && a <= 122;
+    }
     public static String solution(String word) {
+
+
+
         String answer = "";
+
         return answer;
     }
 }
