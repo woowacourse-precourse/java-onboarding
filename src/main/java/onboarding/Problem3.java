91ed33d4fdf4505d7e02974801373bf3d6a8579b
JUN <chltmdwns96@naver.com>
2022-10-30 02:49:21 +0900

    feature : 1~number까지 clap 수 체크 기능 구현 contain369 : 10 미만의 정수 계산 contain369s : 10 이상의 정수 계산 countNumClap : contain369 OR contain369s 를 사용해 입력받은 정수의 clap수를 리턴 countAllClap : 1~number까지 모든 clap 수의 합 구하여 리턴

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,34 +1,54 @@
 package onboarding;
-//## PROBLEM3 기능 구현 정리
-//        1. 예외처리 :
-//        초반 예외 :word 길이 1이상 10000 이하 검증
-//        2. 1 ~ number까지 반복문을 돌리며 3,6,9 포함한 수 갯수 체크:
-//        - 숫자를 문자열로 변환
-//        - 문자열에서 contains 메서드를 통해 3,6,9 체크
-//        있다면 cnt+1
-//        없다면 continue
-//        3. 결과 리턴
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        if(checkLengthException(number)){
+        if(checkNumberRangeException(number)){
             return 0;
         }
+        answer = countAllClap(number);
         return answer;
     }
 
-    private static boolean checkLengthException(int number){
+    private static boolean checkNumberRangeException(int number){
         if(number >= 1 && number <= 10000){
             return false;
         }
         return true;
     }
-
-    private static boolean checkNumberHas369(int num){
-        String strNum = String.valueOf(num);
-        if(strNum.contains("3") || strNum.contains("6") || strNum.contains("9")){
-            return true;
+    private static int countAllClap(int number) {
+        int clapCnt = 0;
+        for(int i=1; i<=number; i++){
+            clapCnt += countNumClap(i);
         }
-        return false;
+        return clapCnt;
     }
+
+    private static int countNumClap(int num){
+        int numClapCnt = 0;
+        if(num < 10){
+            numClapCnt += contain369(num);
+        } else {
+            numClapCnt += contain369s(num);
+        }
+        return numClapCnt;
+    }
+
+    private static int contain369(int num){
+        if(num == 3 || num == 6 || num == 9){
+            return 1;
+        }
+        return 0;
+    }
+
+    private static int contain369s(int num){
+        int numClapCnt = 0;
+        while(num > 0){
+            int temp = num % 10;
+            numClapCnt += contain369(temp);
+            num/=10;
+        }
+        return numClapCnt;
+    }
+
 }
