b6436248aa8a9d5050c6f7e83a1bb4ea23557855
Hayoon <hy97@sju.ac.kr>
2022-10-29 17:04:52 +0900

    refactor: java 코드 컨벤션에 따른 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,24 +1,27 @@
 package onboarding;
 
 public class Problem3 {
-
     static int clappingHands(int number) {
 
         int count = 0;
 
         while(number != 0) {
             int val = number % 10;
-            if (val == 3 || val == 6 || val == 9)
+            if (val == 3 || val == 6 || val == 9) {
                 count++;
-            number /= 10;
             }
+            number /= 10;
+        }
 
         return count;
     }
+
     public static int solution(int number) {
+
         int answer = 0;
-        for(int i = 1; i <= number; i++)
+        for(int i = 1; i <= number; i++) {
             answer += clappingHands(i);
+        }
 
         return answer;
     }
