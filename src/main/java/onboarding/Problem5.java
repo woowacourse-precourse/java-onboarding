b94526e04b7993d98a4c849a4e7520a1e37d0684
bellljun3395 <195850@jnu.ac.kr>
2022-10-30 01:33:00 +0900

    feat : Input5 클래스 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,19 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+}
+
+class Input5 {
+    static int inputMoney() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
 }
