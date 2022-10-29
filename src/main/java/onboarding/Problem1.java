8db3629cdfee39fcc2c625e84263a8b524a92b47
kimdeokyoung <dudejrss@naver.com>
2022-10-29 15:38:58 +0900

    (write) Problem1.solve : 1.0.3 예외 요구사항3의 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,8 @@ import java.util.List;
 class Problem1 {
 
     private static int findGameValue(List<Integer> ls){
+        if(ls.size() != 2) throw new Error("list out of Index");
+
         int a = ls.get(0);
         int b = ls.get(1);
         int x = Math.max(sumOfDigits(a), productOfDigits(a));
@@ -35,9 +37,22 @@ class Problem1 {
 
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-//        System.out.printf("########## %d", findGameValue(pobi));
-        return answer;
+        int valueOfPobi = findGameValue(pobi);
+        int valueOfCrong = findGameValue(crong);
+
+//        System.out.printf("########## %d %d", valueOfPobi, valueOfCrong);
+
+        try {
+            if (valueOfPobi > valueOfCrong) {
+                return 1;
+            } else if (valueOfPobi < valueOfCrong) {
+                return 2;
+            } else {
+                return 0;
+            }
+        } catch (Error e) {
+            return -1;
+         }
     }
 
 }
