a7587d62f39158841ba01b36189811394c90d2b2
kimdoha <kkhhjj3393@gmail.com>
2022-10-29 16:49:35 +0900

    Chore: Google Java Style Guide 적용하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,9 +3,10 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     private static int sumOfDigit(int num) {
         int sum = 0;
-        while(num > 0) {
+        while (num > 0) {
             sum += num % 10;
             num /= 10;
         }
@@ -14,7 +15,7 @@ class Problem1 {
 
     private static int multiplyOfDigit(int num) {
         int multiply = 1;
-        while(num > 0) {
+        while (num > 0) {
             multiply *= num % 10;
             num /= 10;
         }
@@ -23,8 +24,10 @@ class Problem1 {
 
     private static int getMaxScore(List<Integer> pages) {
         int maxScore = 0;
-        for(int page : pages) {
-            if(sumOfDigit(page) == 0 || multiplyOfDigit(page) == 0) return -1;
+        for (int page : pages) {
+            if (sumOfDigit(page) == 0 || multiplyOfDigit(page) == 0) {
+                return -1;
+            }
 
             int newScore = Math.max(sumOfDigit(page), multiplyOfDigit(page));
             maxScore = Math.max(newScore, maxScore);
@@ -34,19 +37,23 @@ class Problem1 {
     }
 
     private static int getPageNumberGameResult(int pobiScore, int crongScore) {
-        if( pobiScore == -1 || crongScore == -1 ) return -1;
-        if( pobiScore > crongScore ) return 1;
-        else if( pobiScore < crongScore ) return 2;
-        else return 0;
+        if (pobiScore == -1 || crongScore == -1) {
+            return -1;
+        }
+        if (pobiScore > crongScore) {
+            return 1;
+        } else if (pobiScore < crongScore) {
+            return 2;
+        } else {
+            return 0;
+        }
     }
 
     public static int solution(List<Integer> pobi,
-                               List<Integer> crong) {
-        int pobiMaxScore = Integer.MIN_VALUE,
-            crongMaxScore = Integer.MIN_VALUE;
+            List<Integer> crong) {
 
-        pobiMaxScore = getMaxScore(pobi);
-        crongMaxScore = getMaxScore(crong);
+        int pobiMaxScore = getMaxScore(pobi);
+        int crongMaxScore = getMaxScore(crong);
 
         return getPageNumberGameResult(pobiMaxScore, crongMaxScore);
     }
