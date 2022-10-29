7fe6e122aec0ccbf107dcc633f283d9a0a619644
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 20:04:25 +0900

    :sparkles: [문제 3] : 4. count의 합 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,7 @@ public class Problem3 {
         // 1. 1부터 number 까지의 숫자 확인
         // 2. 각각의 숫자가 3또는 6또는 9를 포함하는 지 확인
         // 3. 2번의 조건이 참인 숫자에 3또는 6또는 9의개수만큼 count++
-        // 3. count의 합 반환
+        // 4. count의 합 반환
         int answer = 0;
         for (int i = 1; i <= number; i++) {
             String NUMBER = Integer.toString(i);
@@ -16,6 +16,7 @@ public class Problem3 {
                         count++;
                     }
                 }
+                answer += count;
             }
         }
         return answer;
