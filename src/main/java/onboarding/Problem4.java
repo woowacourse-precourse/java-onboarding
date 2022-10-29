0107c0f210d12f4e9c182398b604e860906b78d2
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-10-29 18:14:07 +0900

    feat: implement the solution about Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            char tmp = word.charAt(i);
+            if (tmp >= 'A' && tmp <= 'Z') {
+                tmp = (char)('Z' - tmp + 'A');
+            } else if (tmp >= 'a' && tmp <= 'z') {
+                tmp = (char)('z' - tmp + 'a');
+            }
+
+            answer += Character.toString(tmp);
+        }
+
         return answer;
     }
 }
