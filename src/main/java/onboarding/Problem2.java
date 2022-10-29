7578dd8244341e37fd24fcf6ee08f6050c6e6834
JUN <chltmdwns96@naver.com>
2022-10-29 14:25:54 +0900

    feature : 문자 배열 -> 문자열 변환 기능 구현 CharsToString 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,20 +1,31 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        List<Character> seperatedCryptogram = StringToChars(cryptogram);
 
         return answer;
     }
 
-    private static List<Character> StringToChars(String text){
-        List<Character> seperatedCryptogram = new ArrayList<>();
-        for(int i = 0; i < text.length(); i++){
-            seperatedCryptogram.add(text.charAt(i));
+    protected static List<Character> StringToChars(String text) {
+        List<Character> seperatedText = new ArrayList<>();
+        final int TEXT_LENGTH = text.length();
+        for (int i = 0; i < TEXT_LENGTH; i++) {
+            seperatedText.add(text.charAt(i));
         }
-        return seperatedCryptogram;
+        return seperatedText;
+    }
+
+    protected static String CharsToString(List<Character> chars) {
+        Stream<Character> charStream = chars.stream();
+        String text = charStream.map(String::valueOf).collect(Collectors.joining());
+        return text;
     }
 }
