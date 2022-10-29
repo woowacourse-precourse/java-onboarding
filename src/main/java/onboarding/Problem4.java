63153a246d4e9d591f5dea0abe1e86c1913d678a
ensk26 <tjensk26@gmail.com>
2022-10-30 02:32:36 +0900

    docs: 기능 목록 구성, problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+
         String answer = "";
         return answer;
+    }
+
+    //알파벳을 반대로 반환
+    static String getReverseAlphabet(String alphabet) {
+
+        return alphabet;
     }
 }
