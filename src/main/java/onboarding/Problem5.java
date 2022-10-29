4466ee7b6b928e88386f29ba2bf065a54db6b9f9
SeongukBaek <bsu6235@gmail.com>
2022-10-30 00:29:56 +0900

    feat: implement getCountByUnit()

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,17 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    /*
+    * 주어진 돈이 주어진 단위 몇 개로 변환되는지 반환
+    *
+    * @return int
+    * */
+    private static int getCountByUnit(int money, int unit) {
+        if (money < unit) {
+            return 0;
+        }
+
+        return money / unit;
+    }
 }
