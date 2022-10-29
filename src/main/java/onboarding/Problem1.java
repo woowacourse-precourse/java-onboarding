513755b3baaf0ac125ce7eeec169f68aa671c0f6
choijoohee213 <choijoohee@naver.com>
2022-10-29 19:10:10 +0900

    feat: 페이지 번호 리스트 유효성 검사 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,37 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int length = 2;
+    private static final int minPage = 3;
+    private static final int maxPage = 398;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(!validatePage(pobi) || !validatePage(crong)) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static boolean validatePage(List<Integer> pages) {
+        int leftIdx = 0;
+        int rightIdx = 1;
+
+        if(pages.size() != length) {
+            return false;
+        }
+        if(!pageInRange(pages)) {
+            return false;
+        }
+        if(pages.get(leftIdx) % 2 == 0 || pages.get(rightIdx) % 2 != 0) {
+            return false;
+        }
+        return pages.get(leftIdx) + 1 == pages.get(rightIdx);
+    }
+
+    private static boolean pageInRange(List<Integer> pages) {
+        return pages.stream()
+            .allMatch(page -> page >= minPage && page <= maxPage);
     }
 }
