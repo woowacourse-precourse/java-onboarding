a14bf4fee655c52494b5adb56370c0b750db1ce3
can019 <jys01012@gmail.com>
2022-10-30 00:12:56 +0900

    :sparkles: Upper case, lower case 변환 method 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,22 +1,37 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int SUM_OF_ASCII_UPPER_A_AND_UPPER_Z = 155;
+    private static final int SUM_OF_ASCII_LOWER_A_AND_LOWER_Z = 219;
+
     public static String solution(String word) {
         String answer = "";
-//        System.out.println(charToInt('a'));
-//        System.out.println(intToChar(97));
+
+       for(int i = 0; i<word.length();i++){
+           char current = word.charAt(i);
+
+            if(Character.isUpperCase(current))
+                current = convertUpperCaseToSonFrogWord(current);
+            else if(Character.isLowerCase(current))
+                current = convertLowerCaseToSonFrogWord(current);
+
+        answer+=current;
+       }
+
         return answer;
     }
 
     private static char convertUpperCaseToSonFrogWord(char target){
-        // @TODO :: Upper case
-        char result = Character.MIN_VALUE;
-        return result;
+        int targetToAscii = charToInt(target);
+        int convertedAsciiSonFrogWord = SUM_OF_ASCII_UPPER_A_AND_UPPER_Z - targetToAscii;
+
+        return (char)convertedAsciiSonFrogWord;
     }
     private static char convertLowerCaseToSonFrogWord(char target){
-        // @TODO :: Lower case
-        char result = Character.MIN_VALUE;
-        return result;
+        int targetToAscii = charToInt(target);
+        int convertedAsciiSonFrogWord = SUM_OF_ASCII_LOWER_A_AND_LOWER_Z - targetToAscii;
+
+        return (char)convertedAsciiSonFrogWord;
     }
 
     private static int charToInt(char target){
