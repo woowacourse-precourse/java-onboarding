3fd713edbc1ae7bee6f9b97db1517dc935d31325
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-10-30 03:58:20 +0900

    problem3 Exception처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.io.CharArrayReader;
-
+import Exception.RangeException;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        answer=count369(number);
+        try {
+            exception(number);
+            answer = count369(number);
+        }catch (RangeException e)
+        {
+            //문제에 예외를 어떻게 처리하라는 말이 없음
+            //log.error OR log.warn
+            //System.out.println(e.toString());
+        }
         return answer;
     }
     public static int count369(int number)
@@ -26,5 +33,10 @@ public class Problem3 {
             }
         }
         return count;
+    }
+    public static void exception(int number) throws RangeException
+    {
+        if(number<1||number>10000) throw new RangeException("입력범위 1~10000초과");
+
     }
 }
