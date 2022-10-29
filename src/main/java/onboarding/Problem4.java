c4adb048f9564120402bbea71b9d8c0db8225932
lvalentine6 <ysr1227@naver.com>
2022-10-29 19:08:25 +0900

    feat : 문제 4_문자가 대문자인지 소문자인지 판별하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) == ' ') {
+                sb.append(" ");
+                continue;
+            }
+            boolean flag = checkWord(word.charAt(i));
+        }
+
         return answer;
+    }
+
+    // 현재 문자가 대문자인지 소문자인지 판별하는 메서드
+    static boolean checkWord(char word) {
+        if(Character.isLowerCase(word)) {
+            return false;
+        }
+        return true;
     }
 }
