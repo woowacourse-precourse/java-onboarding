82a31e7b1f3224e9d1dbe536c678cc45aa294af0
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-29 12:52:24 +0900

    feat : Add a function that generate and initialize list

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,14 +6,22 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = getMoney(money);
-        return answer;
+        List<Integer> answer = generateList();
+        return getMoney(money,answer);
     }
 
-    //금액 리스트 반환 함수
-    public static List<Integer> getMoney(int money) {
+    //리스트 생성 함수
+    public static List<Integer> generateList(){
         List<Integer> answer = new ArrayList<>();
-        while (money > 0) {
+        for(int i =0;i<8;i++){
+            answer.add(0);
+        }
+        return answer;
+    }
+    //금액 리스트 반환 함수
+    public static List<Integer> getMoney(int money,List<Integer> answer) {
+
+        while(money>0){
             money = divideMoney(money, answer);
         }
         return answer;
@@ -24,51 +32,51 @@ public class Problem5 {
         int count;
         if (money >= 50000) {
             count = money / 50000;
-            answer.add(count);
+            answer.set(0, count);
             money -= 50000 * count;
             return money;
         }
         if (money >= 10000) {
             count = money / 10000;
-            answer.add(count);
+            answer.set(1, count);
             money -= 10000 * count;
             return money;
         }
         if (money >= 5000) {
-            answer.add(1);
+            answer.set(2, 1);
             money -= 5000;
             return money;
         }
         if (money >= 1000) {
             count = money / 1000;
-            answer.add(count);
+            answer.set(3, count);
             money -= 1000 * count;
             return money;
         }
         if (money >= 500) {
-            answer.add(1);
+            answer.set(4, 1);
             money -= 500;
             return money;
         }
         if (money >= 100) {
             count = money / 100;
-            answer.add(count);
+            answer.set(5, count);
             money -= 100 * count;
             return money;
         }
         if (money >= 50) {
-            answer.add(1);
+            answer.set(6, 1);
             money -= 50;
             return money;
         }
         if (money >= 10) {
             count = money / 10;
-            answer.add(count);
+            answer.set(7, count);
             money -= 10 * count;
             return money;
         }
 
-        answer.add(money);
+        answer.set(8, money);
         money -= money;
         return money;
 
