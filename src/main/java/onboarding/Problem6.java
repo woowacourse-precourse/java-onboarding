93da073acc72c69a44e9d584e7dfb166ef2e36ec
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-30 01:51:11 +0900

    feat(Problem6): 각 문자열에서 두 글자씩 불러오기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        for (List<String> userInfo : forms) {
+            int checkIndex = userInfo.get(1).length() - 1;
+            checkDuplicate(checkIndex, userInfo);
+
+        }
+
         return answer;
+    }
+
+    private static void checkDuplicate(int checkIndex, List<String> userInfo) {
+        for (int j = 0; j < checkIndex; j++) {
+            String checkTwoLetter = userInfo.get(1).substring(j, j + 2);
+            System.out.println(checkTwoLetter);
+        }
     }
 }
