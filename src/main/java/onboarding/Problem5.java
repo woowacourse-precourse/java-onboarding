5904cd0364b00e7ce0c61fcbb3bdf41139048e2f
Bumsoo <rhqjatn2398@naver.com>
2022-10-29 23:34:36 +0900

    feat(Problem5): 금액이 큰 화폐부터 작은 화폐순으로 최대한 변환한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> coinsAndBills = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int coinOrBill : coinsAndBills) {
+            answer.add(money / coinOrBill);
+            money %= coinOrBill;
+        }
+
         return answer;
     }
 }
