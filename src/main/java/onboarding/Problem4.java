6c611bb10dc863e9ffe9677b074e59448692c67e
DaWoon Lee <qkgk61@gmail.com>
2022-10-29 22:49:02 +0900

    Feat : Add methods that translate word

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,8 @@ package onboarding;
 public class Problem4 {
 
     public static String solution(String word) {
-        String answer = "";
+        Translator translator = Translator.getInstance();
+        String answer = translator.translate(word);
         return answer;
     }
 
@@ -20,6 +21,30 @@ class Translator {
 
     public static Translator getInstance() {
         return TranslatorHolder.INSTANCE;
+    }
+
+    public String translate(String word) {
+        StringBuilder translated = new StringBuilder();
+
+        for (int len = word.length(), i = 0; i < len; i++) {
+            char current = word.charAt(i);
+            current = translate(current);
+            translated.append(current);
+        }
+
+        return translated.toString();
+    }
+
+    public char translate(char character) {
+        if (Character.isUpperCase(character)) {
+            return (char) ('Z' - character + 'A');
+        }
+
+        if (Character.isLowerCase(character)) {
+            return (char) ('z' - character + 'a');
+        }
+
+        return character;
     }
 
 }
