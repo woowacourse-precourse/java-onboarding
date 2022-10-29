0da70afbfb9e5d7098f9228e7396a85c782d5f08
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-29 16:15:28 +0900

    problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1;i<=number;i++) {
+            String s=Integer.toString(i);
+            for(int j=0;j<s.length();j++){
+                char c=s.charAt(j);
+                if(c=='3'||c=='6'||c=='9'){
+                    answer+=1;
+                }
+            }
+        }
         return answer;
     }
 }
