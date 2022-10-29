1ebed558c4fdfabb0717133c84eb842a3cab7317
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-29 16:24:22 +0900

    feat(PROBLEM1): showScore 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Iterator;
 import java.util.List;
 
 class Problem1 {
+    public int calScore(String str){
+        return 0;
+    }
+    public int showScore(List<Integer> pages){
+        int odd = pages.get(0);
+        int even = pages.get(1);
+
+        String oddStr = Integer.toString(odd);
+        String evenStr = Integer.toString(even);
+
+        int score = Math.max(calScore(oddStr), calScore(evenStr));
+
+        return score;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
