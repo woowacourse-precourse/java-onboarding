d289de30582a57caee129f9311eb1b37fecb6261
kimdoha <kkhhjj3393@gmail.com>
2022-10-29 20:41:53 +0900

    feat: 연속하는 중복 문자가 있는지 판별 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+    private static boolean checkDuplicateCharacters(String str) {
+
+        char[] chars = str.toCharArray();
+        char previousChar = chars[0];
+
+        for(int i = 1; i < chars.length; i++) {
+            char currentChar = chars[i];
+
+            if(previousChar == currentChar) return true;
+            else previousChar = currentChar;
+        }
+
+        return false;
     }
 }
