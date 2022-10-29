17a9dcc34ba152280998edc55b58f0ee7db40877
hong <hong@hongui-MacBookAir.local>
2022-10-29 17:00:40 +0900

    feat: 문제 조건에 따라 solution 함수 코드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,16 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobi_high_num = maxScore(pobi);
+        int crong_high_num = maxScore(crong);
+
+        if(pobi_high_num>crong_high_num)
+            answer = 1;
+        else if (pobi_high_num<crong_high_num)
+            answer = 2;
+        else answer = 0;
+
         return answer;
     }
     public static int addEachDigit(int num){
