5d93213fdb98bd03322cf7fb75bd14728e43a255
juhee park <jouhi3322@gmail.com>
2022-10-29 18:46:50 +0900

    feat: Problem5 기능목록, 기능구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능목록
+ * 1. 가장 큰 위주의 돈을 담았을 경우의 리스트 반환
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> availableMoney = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        Collections.sort(availableMoney, Collections.reverseOrder());//큰 값부터 계산한다.
+
+        for (int i = 0; i < availableMoney.size(); i++) {
+            answer.add(i, 0);
+            if (money >= availableMoney.get(i)) {
+                answer.set(i, money / availableMoney.get(i));
+                money %= availableMoney.get(i);
+            }
+        }
         return answer;
     }
 }
