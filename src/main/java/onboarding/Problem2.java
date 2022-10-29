d157a997861a9661ab9f954ce9ab11560b6dc23c
syahn <496300@naver.com>
2022-10-29 21:57:02 +0900

    feat : 연속된 중복 문자 있는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem2 {
+    //중복 문자 있는 지 검사
+    public static boolean isOverlap(StringBuffer sb){
+        if(sb.length()<=0)
+            return false;
+
+        for(int i = 0; i<sb.length()-1; i++){
+            if(sb.charAt(i) == sb.charAt(i+1))
+                return true;
+        }
+        return false;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        //나중에 중복 문자 제거 위해 string을 StringBuffer로 변경
+        StringBuffer sb = new StringBuffer(answer);
+
+
+        //중복 제거 후 다시 string으로 변환
+        answer = sb.toString();
         return answer;
     }
 }
