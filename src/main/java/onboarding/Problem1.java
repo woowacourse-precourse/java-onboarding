82b55353026a291fec2986cd9e7b2bd783c6f8df
ChanghyunKim <variety82p@gamil.com>
2022-10-29 20:09:20 +0900

    feat : 게임 예외 사항 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
+    static int left = 0, right = 1;
+    static boolean isAvail(List<Integer> player) {
+        int leftPage = player.get(0);
+        int rightPage = player.get(1);
+        return leftPage > 1 && rightPage < 400 && (rightPage - leftPage == 1);
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
