09fe39eb03715b94c13baa6e8c357df3efd1c7cc
ddingmin <ddingminyt@gmail.com>
2022-10-30 05:10:55 +0900

    feat(Ploblem1): 게임의 승자 구하는 기능 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int pobiScore, crongScore;
+        int pobiScore, crongScore, answer;
 
         if (!checkPages(pobi) || !checkPages(crong)){
             return -1;
@@ -13,8 +13,7 @@ class Problem1 {
         pobiScore = getScore(pobi);
         crongScore = getScore(crong);
 
-
-        int answer = Integer.MAX_VALUE;
+        answer = gameResult(pobiScore, crongScore);
         return answer;
     }
 
@@ -51,6 +50,16 @@ class Problem1 {
 
     private static int mulScore(List<Integer> pages){
         return pages.get(0) * pages.get(1);
+    }
+
+    private static int gameResult(int pobiScore, int crongScore){
+        if (pobiScore > crongScore){
+            return 1;
+        }
+        if (crongScore > pobiScore){
+            return 2;
+        }
+        return 0;
     }
 
 
