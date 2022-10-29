65fc7c5fc0ae5d52d049e1fa97c9bc72cad2a584
can019 <jys01012@gmail.com>
2022-10-29 22:11:07 +0900

    :recycle: 일부 method 및 변수명 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,19 +1,18 @@
 package onboarding;
 
-import java.util.Stack;
-
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
-        answer = eliminate(cryptogram,0,Character.MIN_VALUE, false);
+        answer = eliminateDuplicateChars(cryptogram,0,Character.MIN_VALUE, false);
 
         return answer;
     }
 
-    private static String eliminate(String cryptogram, int index, char before, boolean founded){
+    private static String eliminateDuplicateChars(String cryptogram, int index, char before, boolean duplicateFoundInSequence){
         if(index + 1 > cryptogram.length()){
-            if(founded==true) {
-                return eliminate(cryptogram,0, Character.MIN_VALUE, false);
+            if(duplicateFoundInSequence==true) {
+                // Back to initial sequence
+                return eliminateDuplicateChars(cryptogram,0, Character.MIN_VALUE, false);
             }
             else return cryptogram;
         }
@@ -23,10 +22,10 @@ public class Problem2 {
             if(before == current){
                 String part1 = cryptogram.substring(0, index-1);
                 String part2 = cryptogram.substring(index+1);
-                return eliminate(part1+part2, index-1, current, true);
+                return eliminateDuplicateChars(part1+part2, index-1, current, true);
             }
             else
-                return eliminate(cryptogram, index+1, current, founded);
+                return eliminateDuplicateChars(cryptogram, index+1, current, duplicateFoundInSequence);
         }
     }
 }
