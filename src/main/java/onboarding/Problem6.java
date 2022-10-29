f7f1d04a3c71f0875c06c481afa610c2c76cc1ab
newh08 <newh08@gmail.com>
2022-10-29 13:26:58 +0900

    Problem6 changeToMap 메서드 구현완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,18 +1,30 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
-    public static boolean checkSame(String nickname1, String nickname2){
+    static boolean checkSame(String nickname1, String nickname2){
         for(int i=0; i<nickname1.length()-1;i++){
             if(nickname2.contains(nickname1.substring(i,i+2))){
                 return true;
             }
         }
         return false;
+    }
+
+    static Map<String, String> changeToMap(List<List<String>> forms){
+        Map<String, String> nickname_email_map;
+
+        nickname_email_map = Stream.of(forms).flatMap(List::stream)
+                .collect(Collectors.toMap(i -> i.get(1), i ->i.get(0)));
+
+        return nickname_email_map;
     }
 }
