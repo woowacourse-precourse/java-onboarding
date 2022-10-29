deaceefd69ce82bbece3e16ba007898adf2c9a76
soyunim <togksk123@naver.com>
2022-10-29 22:28:28 +0900

    Problem2 3)get the Index where ends duplication

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,6 +9,7 @@ public class Problem2 {
         List<String> strList = new ArrayList<>();
 
         int startIdx=0;
+        int endIdx=0;
 
         for(int i=0; i<str.length; i++){
             strList.add(str[i]);
@@ -18,6 +19,16 @@ public class Problem2 {
         for (int i = 0; i < strList.size()-1; i++) {
             if(strList.get(i).equals(strList.get(i+1))){
                 startIdx=i;
+                break;
+            }
+        }
+
+        // duplication end Index
+        for (int i = startIdx+1; i < strList.size(); i++) {
+            if(strList.get(startIdx).equals(strList.get(i))){
+                endIdx=i;
+            }
+            else{
                 break;
             }
         }
