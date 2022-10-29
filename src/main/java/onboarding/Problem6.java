e465c407c01c9f24c0d56cd89d3cd697315869a0
5nam <ohnam90@gmail.com>
2022-10-29 19:37:13 +0900

    feat: 닉네임만 모아서 String 배열로 반환하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,15 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    // nickname 만 모아서 String 배열로 반환하는 함수
+    private static String[] returnNickname(List<List<String>> forms) {
+        String[] nickname = new String[forms.size()];
+        for(int i = 0; i<forms.size(); i++) {
+            nickname[i] = forms.get(i).get(1);
+        }
+
+        return nickname;
+    }
+
 }
