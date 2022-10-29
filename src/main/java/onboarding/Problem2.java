234aa24658ae9b07c44e261497b1c8b82f85b2ae
KYBee <kyb3634@naver.com>
2022-10-30 01:51:36 +0900

    :art: [Problem 2] Define & Distribute Requirements
    [Problem 2] Define & Distribute Requirements

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    /**
+     * 암호문을 input으로 전달하면 복호화하여 리턴하는 메서드이다.
+     *
+     * @param : String, 암호문
+     * @return : String, 복호화된 문자열
+     */
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = null;
+        return answer;
+    }
+
+    /**
+     * 암호문을 복호화하는 메서드이다.
+     *
+     * @param : String, 암호문
+     * @return : String, 복호화된 문자열
+     */
+    public static String decrypt(String encrypt) {
+        String answer = null;
         return answer;
     }
 }
