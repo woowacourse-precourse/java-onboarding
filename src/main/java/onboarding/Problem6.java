580d2f326fa5e891add62f9b3525de2dea13a78a
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-29 18:34:31 +0900

    feat: Problem_6 중복되는 이름이 있는지 검사하는 기능 구현
    해쉬 맵의 키로 중복가능한 2글자의 문자열을 저장
    값으로 중복된 문자열을 저장해서 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,49 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String, String> formsMap = forms.stream()
+                .collect(Collectors.toMap(i -> i.get(1), i -> i.get(0)));
+
+        List<String> names = forms.stream()
+                .map(i -> i.get(1))
+                .collect(Collectors.toList());
+
+        Set<String> overlappingName = getOverlappingName(names);
+
+        return overlappingName.stream()
+                .map(formsMap::get)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private static Set<String> getOverlappingName(List<String> names){
+        Map<String, List<String>> overlappingMap = new HashMap<>();
+
+        for (String name : names) {
+            for(int i=0; i<name.length() - 1; i++) {
+                String overlappingWord = name.substring(i, i+2);
+                List<String> registeredNames =
+                        overlappingMap.getOrDefault(overlappingWord, new ArrayList<>());
+                registeredNames.add(name);
+
+                overlappingMap.put(overlappingWord, registeredNames);
+            }
+        }
+        overlappingMap.entrySet().stream()
+                .forEach(i -> System.out.println(i.getKey() + " " + i.getValue()));
+
+        return overlappingMap.values().stream()
+                .filter(i -> i.size() > 1)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
 }
+
+
+
+
+
