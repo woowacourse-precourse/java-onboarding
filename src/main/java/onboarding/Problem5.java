0540944824f348ea6bafbbf573fe03f5efab2774
seongha <11pi885@gmail.com>
2022-10-29 19:51:40 +0900

    feat: 5번문제 기능목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    //화폐 금액별 배열, 저장할 배열 만들고 화폐단위별로 나누면서 결과를 저장하고, 반환
+
+
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+
         return answer;
     }
 }
