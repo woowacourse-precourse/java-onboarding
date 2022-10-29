02a6c56b9930555f35e5054919a7f58604b9c030
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-10-29 12:09:02 +0900

    Update Problem2.java
    misson 2-1
    add comments

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,17 +6,17 @@ public class Problem2 {
         StringBuffer crypto = new StringBuffer(cryptogram);
         for(int i=1;i<crypto.length();i++)
         {
-            if(compare(crypto.charAt(i-1),crypto.charAt(i)))
+            if(compare(crypto.charAt(i-1),crypto.charAt(i)))//두값이 같다면
             {
-                crypto.delete(i-1,i+1);
-                i=1;
+                crypto.delete(i-1,i+1);//두 단어 삭제
+                i=1; //for문 초기화
             }
         }
-        exception(crypto);
+        exception(crypto); //예외사황 처리
         answer = crypto.toString();
         return answer;
     }
-    public static boolean compare(char index1, char index2)
+    public static boolean compare(char index1, char index2)//두 문자열 값 확인하기
     {
         boolean sum = false;
 
@@ -25,7 +25,7 @@ public class Problem2 {
 
         return sum;
     }
-    public static StringBuffer exception(StringBuffer crypto)
+    public static StringBuffer exception(StringBuffer crypto)//2개가 남으면 지워지지않는 예외상황 제거
     {
         if(compare(crypto.charAt(0),crypto.charAt(1)))
             crypto.delete(0,3);
