8b9ca69941aaaa0bb0dc44e89869463f920e66b9
leeyongjun <leeyongjun0310@naver.com>
2022-10-29 17:18:56 +0900

    feat(PROBLEM4): 알파벳 키-값으로 매핑해서 초기화 해주는 기눙 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    private static Map initAlphaMap() {
+        // 알파벳 키-값으로 매핑해서 초기화 해주는 메소드
+        Map<Character, Character> map = new HashMap<>();
+
+        // 대문자 매핑
+        for (int i = 0; i < 26; i++) {
+            int left = 65 + i;
+            int right = 90 - i;
+            map.put((char) left, (char) right);
+        }
+
+        // 소문자 매핑
+        for (int i = 0; i < 26; i++) {
+            int left = 97 + i;
+            int right = 122 - i;
+            map.put((char) left, (char) right);
+        }
+
+        return map;
+    }
+
 }
