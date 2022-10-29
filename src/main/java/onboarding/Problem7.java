0a5d2fb47cbd41bf07aaa8d31eae7e0e01b72b20
marinesnow34 <m4rinesnow34@gmail.com>
2022-10-29 20:22:18 +0900

    Feat: 구현 Problem7 sortedFriendScore에 friendScore을 담고 정렬

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -42,7 +44,17 @@ public class Problem7 {
 		while (friendScore.size() < PERSON_NUM_SHOW) {
 			friendScore.put("emptyUser", 0);
 		}
-
+		List<Map.Entry<String, Integer>> sortedFriendScore = new ArrayList<>(friendScore.entrySet());
+		sortedFriendScore.sort(new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				if (o1.getValue() == o2.getValue()) {
+					return o1.getKey().compareTo(o2.getKey());
+				}
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+		
 		return answer;
 	}
 }
