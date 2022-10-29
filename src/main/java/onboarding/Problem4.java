99b876bfdc0df51bd5f7c1028c21a7f3dabf6249
맹순영 <symaeng98@naver.com>
2022-10-29 14:58:22 +0900

    feat: 알파벳에 대한 Map 생성 구현 (key,value)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Map<Character,Character> wordMap = new HashMap<Character, Character>();
+
+        char upperA = 'A';
+        char lowerA = 'a';
+
+        for (int i=0; i<13; i++){
+            wordMap.put((char) (upperA+i), (char) (upperA+25-i));
+            wordMap.put((char) (lowerA+i), (char) (lowerA+25-i));
+        }
+
+        return "";
     }
 }
