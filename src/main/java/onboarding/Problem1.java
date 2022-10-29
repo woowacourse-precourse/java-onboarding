074e7b2b7341e5cf671e70ea01e7c946431beb0e
ONE <sodlapdlf13@gmail.com>
2022-10-29 15:19:51 +0900

    feat: [Problem1] 참여자의 최대 점수를 얻는 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,14 +2,12 @@ package onboarding;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 /*
 * 1. 페이지 넘버 값을 필드로 가지는 Page 클래스 생성
 * 2. 왼쪽 페이지와 오른쪽 페이지를 필드로 가지는 Gamer 클래스 생성
 * 3. Page class methods
 *   3-1. Page(int num): 페이지 값을 파라미터로 받는 생성자 메소드
-*   3-2. getScore(int page): 페이지의 최대 점수를 얻는 메소드
+*   3-2. getScore(): 페이지의 최대 점수를 얻는 메소드
 * 4. Gamer class methods
 *   4-1. Gamer(int left, int right): 왼쪽 페이지와 오른쪽 페이지를 파라미터로 받는 생성자 메소드
 *   4-2. getMaxScore(): Gamer 의 최대 점수를 얻는 메소드
@@ -54,6 +52,10 @@ class Gamer {
     public Gamer(int leftNum, int rightNum) {
         this.left = new Page(leftNum);
         this.right = new Page(rightNum);
+    }
+
+    public int getMaxScore() {
+        return Math.max(left.getScore(), right.getScore());
     }
 }
 
