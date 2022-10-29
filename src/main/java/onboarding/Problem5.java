5fed8fbc155b774182f8127ccab7d01ded1be834
limzzum <chfhddl1234@naver.com>
2022-10-29 19:35:06 +0900

    [문제 5] 돈의 단위순으로 나눌 수 있는 최대한의 수를 저장하고 나머지 값은 더 작은 단위로 나눈다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,23 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         int [] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        Integer [] result = new Integer[unit.length];
+        int idx=0;
+
+        for(int n: unit){
+            result[idx]= money/n;
+            money %= n;
+            idx++;
+        }
+
 
         return answer;
     }
