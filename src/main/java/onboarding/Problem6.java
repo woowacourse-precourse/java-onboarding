cec36b6938d7e48edcd6ee3571568ea594cde7b0
Heesom <heesom.hs@gmail.com>
2022-10-29 23:07:31 +0900

    Problem6 : compaerString 메소드 수정
    잘못 기입된 변수 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -38,7 +38,7 @@ public class Problem6 {
 
             for(int c2=0; c2<c2Arr.length-1; c2++) {
                 String c1Str = c1Double.toString();
-                String c2Str = String.valueOf(c2Arr[c2]) + c1Arr[c2 + 1];
+                String c2Str = String.valueOf(c2Arr[c2]) + c2Arr[c2 + 1];
                 if(c1Str.equals(c2Str)){
                     return true;
                 }
