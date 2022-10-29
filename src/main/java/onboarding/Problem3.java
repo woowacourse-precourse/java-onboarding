8005d11e617da52e5cf7a5191912c56b6c8e186e
hhiyeon <hhyyeon0214@gmail.com>
2022-10-29 17:42:09 +0900

    feat: divide digit number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static List<Integer> splitDigitNumber(int pageNumber) {
+        List<Integer> splitNumberList = new ArrayList<>();
+        while (pageNumber > 0) {
+            splitNumberList.add(pageNumber % 10);
+            pageNumber /= 10;
+        }
+        return splitNumberList;
     }
 }
