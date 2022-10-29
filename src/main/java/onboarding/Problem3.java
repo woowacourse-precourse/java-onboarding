7cf6e0bf6f2557bd6d536e1427cae6efaa276263
Koonayoung518 <koony000415@gmail.com>
2022-10-29 15:27:20 +0900

    feat(problem3) : 값 자리수대로 나누기 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+    private static List<Integer> separateNumber(int num){
+        List<Integer> list = new ArrayList<>();
+        while(num > 0){
+            list.add(num % 10);
+            num = num /10;
+        }
+        return list;
+    }
+
 }
