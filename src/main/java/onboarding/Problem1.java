5b4af8482a2c9cf11661853ea4a21dd9f767a22d
Jenn Kim <grdd0419@gmail.com>
2022-10-29 23:59:42 +0900

    feat(Problem1): add a function to get the maximum sum value

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,22 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int addPages(int leftPage, int rightPage) {
+
+        int leftSum = 0, rightSum = 0;
+
+        while (leftPage > 0) {
+            leftSum += leftPage % 10;
+            leftPage /= 10;
+        }
+        while (rightPage > 0) {
+            rightSum += rightPage % 10;
+            rightPage /= 10;
+        }
+
+        if (leftSum >= rightSum)
+            return leftSum;
+        return rightSum;
+    }
 }
