16c2aa07ec2cc08d5808e09f19d543dfcc025541
thdwoqor <celebarte01@gmail.com>
2022-10-29 16:25:44 +0900

    add Problem2 Solution

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+    public static String decoding(String cryptogram) {
+        StringBuilder result = new StringBuilder(cryptogram);
+        for (int i = 0; i < cryptogram.length()-1; i++) {
+            char left = cryptogram.charAt(i);
+            char right = cryptogram.charAt(i+1);
+
+            if (left==right){
+                result=result.deleteCharAt(i+1);
+                result=result.deleteCharAt(i);
+                return decoding(result.toString());
+            }
+        }
+        return result.toString();
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = decoding(cryptogram);
         return answer;
     }
 }
