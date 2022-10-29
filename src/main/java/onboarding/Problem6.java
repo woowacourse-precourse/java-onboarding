90e9518a4a13c12b60b860a34aae11ce50eddefa
devjordy14 <devjordy14@gmail.com>
2022-10-29 19:18:52 +0900

    do todo 1

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // [이메일, 닉네임]으로 이루어진 리스트를 받음
@@ -11,19 +12,30 @@ import java.util.List;
 // 이메일 목록은 오름차순으로 정렬하고 중복은 제거
 
 // todo
-// 1. 일단.. 제약조건부터 구현해보자!
-// 1-1. 이메일의 뒷부분이 @email.com인지 검사
-// 1-2. 이메일의 길이가 11이상 20미만인지 검사
-// 1-3. 닉네임이 한글인지 검사
-// 1-4. 닉네임의 길이가 1이상 20미만인지 검사
-// 2. 두개 이상의 스트링 객체에서 길이가 2 이상인 같은 문자열이 있는지.. 검사하는 로직 생각해보기
-// 2-1.
+// 1. 하나의 닉네임을 두글자씩 나눠서 각 두글자가 다른 닉네임에도 있는지 검사
+// 1-1. 스트링을 돌면서 각 두글자가 있는지 전체 순회, 있다면 answer 리스트에 해당 form과 현재 form 추가
+// 1-2. 이미 지나간 form은 검사하지 않음
+// 2. answer 리스트에서 중복을 제거
+
 
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
 
+        for(int i = 0; i < forms.size(); i++){
+            for(int j = 0; j < forms.get(i).get(1).length()-1; j++){
+                for(int k = i+1; k < forms.size(); k++){
+                    if (forms.get(k).get(1).contains(forms.get(i).get(1).substring(j, j+2)))
+                    {
+                        answer.add(forms.get(i).get(0));
+                        answer.add(forms.get(k).get(0));
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
         return answer;
     }
 }
