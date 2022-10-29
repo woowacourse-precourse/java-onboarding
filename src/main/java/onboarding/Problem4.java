f9bcc656d5b813a1b0ca35923c42cf23a385fe52
cocomozila <joseyun94@naver.com>
2022-10-30 03:49:39 +0900

    [문제 4] 주어진 문자열을 0번 인덱스부터 문자열 길이만큼 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+
+    // 주어진 문자열을 0번 인덱스부터 문자열 길이만큼 변환하는 기능
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        return sb.toString();
     }
 }
