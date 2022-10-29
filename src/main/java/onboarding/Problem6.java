7e3cb7fdd52fe614b94392558bfd6c4d64e7b65d
Egenieee <hhgg0925@naver.com>
2022-10-30 03:32:49 +0900

    [FEAT] Problem6 중복 닉네임을 사용한 지원자 추출하는 기능 추가
    - Map의 value를 조회하며 Set size가 2 이상인 것 추출하도록 한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,16 +5,23 @@ import java.util.*;
 public class Problem6 {
     private static final Map<String, Set<String>> subNicknames = new HashMap<>();
     public static void main(String[] args) {
-        Problem6.solution(List.of(
+        List<String> answer = Problem6.solution(List.of(
                 List.of("jm@email.com", "제이엠"),
                 List.of("jason@email.com", "제이슨"),
-                List.of("woniee@email.com", "워니"),
+                List.of("woniee@email.com", "워닝닝"),
                 List.of("mj@email.com", "엠제이"),
-                List.of("nowm@email.com", "이제엠")
+                List.of("nowm@email.com", "엠제임마"),
+                List.of("hhgg@email.com", "워니버니"),
+                List.of("dasd@email.com", "버니룽띵"),
+                List.of("asdas@email.com", "딩룽띵룽띵룽")
         ));
-    }
-    public static List<String> solution(List<List<String>> forms) {
 
+        for (String a : answer) {
+            System.out.println(a);
+        }
+    }
+
+    public static List<String> solution(List<List<String>> forms) {
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickname = form.get(1);
@@ -22,8 +29,9 @@ public class Problem6 {
             splitNickname(email, nickname);
         }
 
-        return Arrays.asList("", "");
+        return getCrewEmails();
     }
+
 
     private static void splitNickname(String email, String nickname) {
         for (int i = 0 ; i < nickname.length() - 1 ; i++) {
@@ -43,5 +51,26 @@ public class Problem6 {
 
     private static String getSubNickname(String nickname, int startIndex) {
         return nickname.substring(startIndex, startIndex + 2);
+    }
+
+    private static List<String> getCrewEmails() {
+        Set<String> totalDuplicated = getTotalDuplicated();
+        return null;
+    }
+
+    private static Set<String> getTotalDuplicated() {
+        Set<String> totalDuplicated = new HashSet<>();
+
+        for (String subNickName : subNicknames.keySet()) {
+            if (isDuplicated(subNickName)) {
+                totalDuplicated.addAll(subNicknames.get(subNickName));
+            }
+        }
+
+        return totalDuplicated;
+    }
+
+    private static boolean isDuplicated(String subNickName) {
+        return subNicknames.get(subNickName).size() >= 2;
     }
 }
