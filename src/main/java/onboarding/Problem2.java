9ab7a62878e2b51a0d9865df5a9a6d54f6c76840
Cho Changhun <cvcvcx@naver.com>
2022-10-29 21:09:06 +0900

    2번문제 연속되는 문자열을 찾아서 리스트로 넣어주는 함수

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -17,20 +17,32 @@ public class Problem2 {
         ArrayList<String> deleteList = new ArrayList<>();
         while (true){
 
-         for (int i = 0; i < cryptogram.length() - 1; i++) {
-             if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
-                 String deleteString = cryptogram.charAt(i) + "" + cryptogram.charAt(i + 1);
-                 deleteList.add(deleteString);
-             }
-         }
+        findDeleteList(cryptogram, deleteList);
         if(cryptogram==""||deleteList.isEmpty()){
-            return cryptogram;
+        return cryptogram;
         }
-            for (int i = 0; i < deleteList.size(); i++) {
-                cryptogram =cryptogram.replace(deleteList.get(i),"");
-                System.out.println(cryptogram);
-            };
-            deleteList.clear();
+//        cryptogram = afterDeleteCryptogram(cryptogram, deleteList);
+        deleteList.clear();
         }
+    }
+//
+//    private static String afterDeleteCryptogram(String cryptogram, ArrayList<String> deleteList) {
+//        for (int i = 0; i < deleteList.size(); i++) {
+//            cryptogram = cryptogram.replace(deleteList.get(i),"");
+//        }
+//        return cryptogram;
+//    }
+
+    /**
+     * 연속되는 문자열을 찾아서 미리 만들어둔 deleteList로 반환하는 함수
+     */
+    private static void findDeleteList(String cryptogram, ArrayList<String> deleteList) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                String deleteString = cryptogram.charAt(i) + "" + cryptogram.charAt(i + 1);
+                deleteList.add(deleteString);
+            }
+        }
+
     }
 }
