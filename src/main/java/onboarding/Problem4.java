6e5f43e7d8f37cd8f14b4374ffb248ca83914242
khj <bbb6254177@gmail.com>
2022-10-30 01:47:39 +0900

    initial commit

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    private static HashMap<Character,Character> map = new HashMap<Character,Character>(){{
+        for(int i = 97; i<=122; i++){
+            put((char)i, (char)(219-i));
+        }
+    }};
+
+    private static Character mapping(Character c){
+        if (Character.isAlphabetic(c)){
+            char mom = Character.toLowerCase(c);
+            char son = (Character.isUpperCase(c)) ? Character.toUpperCase(map.get(mom)): map.get(mom);
+            return son;
+        }
+        else return c;
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuffer ans = new StringBuffer();
+
+        for(int i = 0; i<word.length(); i++){
+            ans.append(mapping(word.charAt(i)));
+        }
+
+        return ans.toString();
     }
 }
