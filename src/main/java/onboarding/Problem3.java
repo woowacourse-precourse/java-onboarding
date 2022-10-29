73441d3d2bf833ca0feae09af3df4f7d033aeab3
hyowonhan <wain1719@gmail.com>
2022-10-29 17:48:11 +0900

    문제3 main 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,14 @@
 package onboarding;
-
+import java.util.Scanner;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(solution(number));
     }
 }
