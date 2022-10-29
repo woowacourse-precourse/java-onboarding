e6439d9afa3909a96de4acdec938d808137d3a2b
hyunho12 <hyundho12@gmail.com>
2022-10-29 21:45:33 +0900

    feat: Problem3 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1;i<=number;i++){
+            answer += clap_check(i);
+        }
+
         return answer;
+    }
+
+    public static int clap_check(int num){
+        String str = Integer.toString(num);
+        int cnt = 0;
+
+        for(int i=0;i<str.length();i++){
+            if(str.split("")[i].equals("3") || str.split("")[i].equals("6")
+                    || str.split("")[i].equals("9")){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
