030e62d42dab9728b58ad55a80af9e1e57cac27f
박원종 <wonjong1030@naver.com>
2022-10-29 22:39:08 +0900

    feat: 3,6,9 개수 세는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,11 +6,14 @@ public class Problem3 {
         return answer;
     }
     public static int getClapCnt(int num){
+        int cnt=0;
         while(num > 0){
             int cur = num%10;
+            if(is369(cur))
+                cnt++;
             num/=10;
         }
-        return 0;
+        return cnt;
     }
     public static boolean is369(int num){
         if(num == 3 || num == 6 || num ==9){
