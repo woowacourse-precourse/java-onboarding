471c416c1a026cf423110b35eec79131c3b05877
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-29 21:44:01 +0900

    feat(Problem4): 알파벳을 대문자인지 소문자인지 확인하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,11 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i<word.length(); i++){
+            char letter = word.charAt(i);
+            if(Character.isUpperCase(letter)){
+                answer += convertUpperCaseLetterReverse(letter);
+            }
+            if(Character.isLowerCase(letter)){
+                answer += convertLowerCaseLetterReverse(letter);
+            }
+        }
         return answer;
     }
 
     public static String convertUpperCaseLetterReverse(char letter){
-        return Character.toString((char)(155-letter));
+        return Character.toString((char)(155 - letter));
     }
 
     public static String convertLowerCaseLetterReverse(char letter){
