60497cf859ce2f4a8cf12ae42846313d07582b61
pushedrumex <pushedrumex@naver.com>
2022-10-29 20:17:21 +0900

    problem5: 특정 금액으로 나눈 몫과 나머지를 반환하는 함수(divMoney)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    static HashMap<String, Integer> divMoney(int money, int bill) {
+        HashMap<String, Integer> map = new HashMap<>();
+        int quotient = money / bill;
+        int remainder = money % bill;
+        map.put("quotient", quotient);
+        map.put("remainder", remainder);
+        return map;
+    }
+    public static void main(String args[]) {
+        System.out.println(divMoney(12000, 10000));
     }
 }
