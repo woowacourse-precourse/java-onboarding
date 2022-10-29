f43781be199d5035739e1fe9193c43db87f94086
Youngho <cyh6099@gmail.com>
2022-10-29 20:25:03 +0900

    feat(p7) : user와 친구인 사용자 목록 초기화 기능 함수 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static List<String> friendsList;
+    public static Map<String, Integer> recommendScore;
+    public static Map<String, List<String>> relationships;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        friendsList = new ArrayList<>();
+        recommendScore = new HashMap<>();
+        relationships = new HashMap<>();
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static void initFriendsList(List<String> relationship, String user) {
+        String userA = relationship.get(0);
+        String userB = relationship.get(1);
+
+        if(userA.equals(user)){
+            friendsList.add(userB);
+            return;
+        }
+        friendsList.add(userA);
+    }
+
 }
