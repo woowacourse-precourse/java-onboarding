2da53b7fe7749d5e64d5aed4454ac8c75f281635
gyuturn <rbtjrrbals@gmail.com>
2022-10-29 22:27:06 +0900

    feat: Problem6 - findDupNameEmailList 메서드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
@@ -9,7 +10,6 @@ public class Problem6 {
     }
     private static boolean isCorrectInput(List<List<String>> forms) {
         if(forms.size()<1||forms.size()>10000) return false;
-        System.out.println("테스트 통과1");
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickName = form.get(1);
@@ -20,7 +20,25 @@ public class Problem6 {
             if(email.length()<11||email.length()>=20) return false;
             if (!nickName.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) return false;
         }
-
         return true;
+    }
+
+    private static List<String> findDupNameEmailList(List<List<String>> forms) {
+        List<String> hasDupNameEmails = new ArrayList<>();
+        for (int student = 0; student < forms.size()-1; student++) {
+            String nickName = forms.get(student).get(1);
+            for (int comparedStudent = student+1; comparedStudent < forms.size(); comparedStudent++) {
+                String comparedNickName = forms.get(comparedStudent).get(1);
+                for (int i = 0; i < nickName.length()-1; i++) {
+                    for (int j = 0; j < comparedNickName.length()-1; j++) {
+                        if(nickName.substring(i,i+2).equals(comparedNickName.substring(j,j+2))){
+                            hasDupNameEmails.add(forms.get(student).get(0));
+                            hasDupNameEmails.add(forms.get(comparedStudent).get(0));
+                        }
+                    }
+                }
+            }
+        }
+        return hasDupNameEmails;
     }
 }
