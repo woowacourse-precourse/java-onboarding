645e957833de7d2076cd9cf815860e0e011457a7
Eleisia <blackcube222@gmail.com>
2022-10-29 23:12:48 +0900

    1029

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/*
+* 기능 목록
+* 1. 연속되는 값을 판별하는 기능
+* 2. 연속되는 값들을 제거하는 기능
+* */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
