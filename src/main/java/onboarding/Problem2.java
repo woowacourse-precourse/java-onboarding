a89efe3148310a883b438e34cdbb1cdcde429e21
hhiyeon <hhyyeon0214@gmail.com>
2022-10-29 16:59:16 +0900

    feat: remove adjacent duplicate character

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,15 +1,51 @@
 package onboarding;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Problem2 {
+    private static final String OUTPUT_IS_BLANK = "";
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        if (isValidStringLength(cryptogram)) {
+            return cryptogram;
+        }
+
+        return removeAdjacentDuplicate(getStringToChar(cryptogram));
     }
 
     private static boolean isValidStringLength(String str) {
         return str.length() == 1;
+    }
+
+    private static String removeAdjacentDuplicate(char[] cryptogramArray) {
+        Deque<Character> charDeque = new ArrayDeque<>();
+        char lastRemove = initLastRemove();
+
+        for (int i = 0; i < cryptogramArray.length; i++) {
+            if (isEmptyDeque(charDeque, i)) {
+                addElementToDeque(charDeque, cryptogramArray[i]);
+                continue;
+            }
+
+            char lastElement = charDeque.getLast();
+            if (isSameLastRemove(cryptogramArray[i], lastRemove)) {
+                continue;
+            }
+
+            if (isSamePrevElement(cryptogramArray[i], lastElement, charDeque)) {
+                lastRemove = initLastRemove(lastElement);
+                continue;
+            }
+            lastRemove = initLastRemove();
+            addElementToDeque(charDeque, cryptogramArray[i]);
+        }
+
+        if (isOutputBlank(charDeque)) {
+            return OUTPUT_IS_BLANK;
+        }
+
+        return getCharDequeToString(charDeque);
     }
 
     private static char[] getStringToChar(String str) {
