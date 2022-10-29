925e29560e52f28f6ad5be5ad33aa7b2a0efdd8e
LEE-sh1673 <lsh901673@gmail.com>
2022-10-29 14:29:05 +0900

    feat(Problem3): 문제3 solution 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,9 @@
 package onboarding;
 
+import onboarding.clapgame.ClapGame;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return ClapGame.getTotalClapCountOf(number);
     }
 }
