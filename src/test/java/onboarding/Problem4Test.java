3bf602d845c75dee9e3fb0094586cf62c69d0c6b
saehim <saeilee.vxv@gmail.com>
2022-10-30 00:14:24 +0900

    Test(Problem4): 대문자 반환 기능

--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -1,6 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 class Problem4Test {
   
+    // 대문자 변환
+    char uppercase(char a){
+        char answer = (char)(155 - a);
+        return answer;
+    }
     // 소문자 변환
     char lowercase(char a){
         char answer = (char)(219 - a);
