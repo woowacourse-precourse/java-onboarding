b6c63daa425068c48107ea03b117b1edc5c2b923
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-29 22:53:41 +0900

    Add calcScore function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -30,9 +30,22 @@ class Problem1 {
 
     // 각 자리수를 더하고 곱한 값 중 가장 큰 수를 구하는 함수
     public static Integer maxNumber(Integer pageNum) {
-        Integer maxValue, addValue = addDigits(pageNum), mulValue = mulDigits(pageNum);
+        Integer maxValue;
+        Integer addValue = addDigits(pageNum);
+        Integer mulValue = mulDigits(pageNum);
+
         if (addValue.compareTo(mulValue) == 1) maxValue = addValue;
         else maxValue = mulValue;
         return maxValue;
+    }
+
+    // 양 페이지 번호를 전달받아 점수를 구하는 함수
+    public static Integer calcScore(List<Integer> pageList) {
+        Integer score;
+        Integer value1 = maxNumber(pageList.get(0));
+        Integer value2 = maxNumber(pageList.get(1));
+        if (value1.compareTo(value2) == 1) score = value1;
+        else score = value2;
+        return score;
     }
 }
