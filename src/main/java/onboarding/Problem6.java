7afc8f20a93bae711afd70e4108ca19d3fde1b87
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-29 21:13:46 +0900

    comment: Problem6 구현기능목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,12 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
+/**
+ * 1. map을 생성하고, key에 두 글짜씩 substring한 닉네임, value에 사용된 횟수를 저장한다.
+ * 2. 이 때, map에서 key값(두 글짜씩 substring한 닉네임)이 2번 이상 사용되었고, email에 포함되지 않았다면 그 이메일을 추가한다.
+ * 3. 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬한다.
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
