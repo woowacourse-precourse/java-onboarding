a8c923aeaa8469f06e10cf140171a747521f75ab
jthugg <jthug@naver.com>
2022-10-29 18:06:04 +0900

    refactor. 구조, 기능 리팩토링
    기존 방식에선 중복문자열을 찾으면 즉시 삭제하고 다시 중복문자열을 찾는 기능을 수행함, 현재 상태에서 중복된 문자열들을 모두 제거 한 후 다음 단계에서 새로운 중복 문자열을 찾아야 하는 방식으로 코드 리팩토링
    
    findRm() : 현재 매개변수 상태에서 중복된 문자열이 있는지 검사 하고 있다면 그 리스트를 반환해주는 메서드
    
    rmStr() : 중복된 문자열 리스트를 매개변수로 받아 현재 문자열에서 해당 인덱스를 공백문자로 변환 후 한꺼번에 삭제하는 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,33 +1,59 @@
 package onboarding;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        //1. 중복체크 기능
-        //2. 지워야 할 문자열
-        //3. 루프내에서 중복 문자열을 삭제할때마다 체크
-        //4. 더이상 지울 문자열이 없으면 리턴
+        // 현재상태에서의 중복 문자열 리스트를 만든다
+        // 없다면 루프 탈출 후 그대로 리턴, 있다면 중복된 문자열을 순서대로 공백문자로 변환한다
+        // 현재상태에서의 공백문자들을 한꺼번에 삭제한다
+        // 다시 루프의 처음으로 돌아가 위 기능들을 반복한다
+        // refactor
+        boolean hasRm = true;
 
-        int hasRm = 1; // 0이 아니면 지워야할 문자열이 있음, 루프 진입을 위해 1로 초기화.
-        String str = ""; // 중복 문자열을 담을 변수
+        while(hasRm) {
+            hasRm = false;
 
-        while(hasRm != 0) {
-            hasRm = 0; // 루프 진입 후 1 -> 0
+            List<String> rmList = findRm(cryptogram); // 현재 매개변수에서 중복 문자열이 있는지, 있다면 리스트를 반환해주는 메서드
 
-            for(int i = 0; i < cryptogram.length(); i++) {
-                char ch = cryptogram.charAt(i);
-                str = ch + "";
+            hasRm = rmList.size() > 0 ? true : false; // 리스트에 하나라도 들어가 있다면 삭제하는 기능 수행
 
-                for(int j = i + 1; j < cryptogram.length() && ch == cryptogram.charAt(j); j++) { // i 번째 인덱스와 다음 오는 문자들이 중복되는 경우, 중복아니면 패스
-                    str += cryptogram.charAt(j);
-                }
-
-                if(str.length() > 1) { // str 길이가 1 보다 크면 중복되는게 있음
-                    hasRm = 1; // hasRM이 0이 아니므로 기능 수행 후 루프 재진입 예정
-                    cryptogram = cryptogram.replaceFirst(str, ""); // 지워야 할 문자열 하나만 지우기. 다른것도 지워버리면 문제 발생. ex) axxbxxxc -> abxc
-                    i = 0; // n번째 중복문자열 삭제 후 다시 0번 인덱스부터 다시 체크하기 위해 i = 0
-                }
+            if(hasRm) {
+                cryptogram = rmStr(rmList, cryptogram); // 중복 문자열 삭제 메서드, 중복 문자열 리스트에 해당하는 인덱스들을 공백 문자로 변환 후 한꺼번에 삭제
             }
+
         }
+
+        return cryptogram;
+    }
+    private static List<String> findRm(String cryptogram) {
+        List<String> rmList = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < cryptogram.length(); i++) {
+            char ch = cryptogram.charAt(i);
+            sb.append(ch);
+
+            for(int j = i + 1; j < cryptogram.length() && ch == cryptogram.charAt(j); j++) {
+                sb.append(cryptogram.charAt(j));
+            }
+
+            if(sb.length() > 1) {
+                rmList.add(sb.toString());
+            }
+
+            sb.setLength(0);
+        }
+
+        return rmList;
+    }
+    private static String rmStr(List<String> rmList, String cryptogram) {
+        for(String str : rmList) {
+            cryptogram = cryptogram.replaceFirst(str, " ");
+        }
+
+        cryptogram = cryptogram.replaceAll(" ", "");
 
         return cryptogram;
     }
