ba39b85e95a99f3e9c73de37317819f7d6721484
박원종 <wonjong1030@naver.com>
2022-10-29 22:54:13 +0900

    feat: 가장 큰 돈 단위부터 가장작은 단위까지 나누고 answer에 몫 저장하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        for(int i=0; i<currency.length; i++){
+            int cur = currency[i];
+            answer.add(money/cur);
+            money %= cur;
+        }
         return answer;
     }
 }
