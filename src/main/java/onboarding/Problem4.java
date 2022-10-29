dff3d63f3515db958c7735d3659f4d25d04372c3
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-29 23:56:51 +0900

    refactor: 4번 문제 리팩토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,16 +10,32 @@ public class Problem4 {
 
     private static String reverseString(String word, String answer) {
         for (int i = 0; i < word.length(); i++) {
-            char reverse = word.charAt(i);
-            if (reverse >= 'A' && reverse <= 'Z') {
-                answer += (char) (('Z' - reverse) + 'A'); // 대문자 청개구리 사전 참고하여 ASCII 코드 활용
-            } else if (reverse >= 'a' && reverse <= 'z') {
-                answer += (char) (('z' - reverse) + 'a'); // 소문자 청개구리 사전 참고하여 ASCII 코드 활용
-            } else {
-                answer += reverse; // 알파벳이 아닌 경우에는 그대로 넣기.
-            }
-
+            char now_word = word.charAt(i);
+            answer = putReverseChar(answer, now_word);
         }
         return answer;
+    }
+
+    private static String putReverseChar(String answer, char now_word) {
+        if (isUpperReverse(now_word)) {
+            answer += (char) changeReverse('Z', now_word, 'A');
+        } else if (isLowerReverse(now_word)) {
+            answer += (char) changeReverse('z', now_word, 'a'); 
+        } else {
+            answer += now_word; 
+        }
+        return answer;
+    }
+
+    private static int changeReverse(char z, char reverse, char a) {
+        return (z - reverse) + a;
+    }
+
+    private static boolean isLowerReverse(char reverse) {
+        return reverse >= 'a' && reverse <= 'z';
+    }
+
+    private static boolean isUpperReverse(char reverse) {
+        return reverse >= 'A' && reverse <= 'Z';
     }
 }
