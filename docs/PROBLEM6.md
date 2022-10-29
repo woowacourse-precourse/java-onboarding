577cb32dc1e562c122ba077bb37a351714965caa
limjimin <dkdldsla0000@naver.com>
2022-10-29 22:31:12 +0900

    Feat(Problem6): 닉네임을 두 글자로 나누는 기능

--- docs/PROBLEM6.md
+++ docs/PROBLEM6.md
@@ -25,7 +25,7 @@
 ## ✅ 구현할 기능 목록
 
 - [ ] forms의 닉네임을 두 글자로 나누고, 나눈 닉네임에 해당하는 이메일을 HashMap에 저장 (`createNicknameMap`)
-  - [ ] 닉네임을 두 글자씩 나누는 함수 (`splitTwoLetter`)
+  - [x] 닉네임을 두 글자씩 나누는 함수 (`splitTwoLetter`)
   - [ ] 두 글자씩 나눈 닉네임에 맞게 이메일을 HashMap에 저장하는 함수 (`storeEmails`)
     - key(String): 두 글자로 나눈 닉네임, value(List<String>): 이메일
 - [ ] 글자에 해당하는 이메일이 2개 이상이라면, 그 이메일들을 결과로 추출하는 함수 (`getDuplicateEmails`)
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<String> splitTwoLetter(String nickname) {
+        List<String> twoLetterNicknames = new ArrayList<>();
+
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String twoLetterNickname = nickname.substring(i, i + 2);
+            twoLetterNicknames.add(twoLetterNickname);
+        }
+
+        return twoLetterNicknames;
     }
 }
