9122a402f24d407c9923d48cd640a514855d5dd5
GraphTree <graphnodesed@gmail.com>
2022-10-29 09:42:23 +0900

    Feat: 중복 알파벳 제거하는 dupRemover함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
         return answer;
     }
+
+
+    public static String dupRemover(String value) {
+        String result;
+        if (value.length() <= 1) {
+            return value;
+        }
+        if (value.charAt(0) == value.charAt(1)) {
+            return dupRemover(value.substring(2));
+        } else {
+            return value.charAt(0) + dupRemover(value.substring(1));
+        }
+
+    }
+
 }
