11be39c4c90bfeefb7fb45c273a3a5fd4c493176
jasoncho1021 <jhcho1021@gmail.com>
2022-10-29 23:22:14 +0900

    feat(problem2): 압축 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,42 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static String compress(String input) {
+        int inputLength = input.length();
+        if (inputLength == 0) {
+            return "";
+        }
+        int targetCharStartIdx = 0;
+        int targetCharEndIdx = 0;
+        char targetChar = input.charAt(0);
+        char currentChar = targetChar;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 1; i < inputLength; i++) {
+            currentChar = input.charAt(i);
+            if (targetChar != currentChar) {
+                if (doesTargetCharHasNotSequence(targetCharStartIdx, targetCharEndIdx)) {
+                    stringBuilder.append(targetChar);
+                }
+                targetChar = currentChar;
+                targetCharStartIdx = i;
+            }
+            targetCharEndIdx = i;
+        }
+        /*
+         * 마지막 문자가 연속된 문자열의 끝인지 아닌지 판단한다.
+         */
+        if (doesTargetCharHasNotSequence(targetCharStartIdx, targetCharEndIdx)) {
+            stringBuilder.append(targetChar);
+        }
+        return stringBuilder.toString();
+    }
+
+    private static boolean doesTargetCharHasNotSequence(int targetCharStartIdx, int targetCharEndIdx) {
+        if (targetCharStartIdx == targetCharEndIdx) {
+            return true;
+        }
+        return false;
+    }
 }
