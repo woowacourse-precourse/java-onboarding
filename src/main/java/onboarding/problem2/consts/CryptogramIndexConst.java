4e41e5187daa2616a12bc7d252f0b1898469b48f
apptie <appti@naver.com>
2022-10-29 11:07:03 +0900

    fix: 문제 2 잘못된 상수 값 수정

--- src/main/java/onboarding/problem2/consts/CryptogramIndexConst.java
+++ src/main/java/onboarding/problem2/consts/CryptogramIndexConst.java
@@ -2,6 +2,6 @@ package onboarding.problem2.consts;
 
 public abstract class CryptogramIndexConst {
 
-    public static final int CHARACTER_START_INDEX = 0;
+    public static final int CHARACTER_START_INDEX = 1;
     public static final int CHARACTER_BEFORE_INDEX = 1;
 }
