39c8a9f4f80ce65db5070578d765af8ffcc679bc
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-29 13:21:33 +0900

    feat: 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱한 값 중 가장 큰 수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+    /**
+     * 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱한 값 중 가장 큰 수 구하기
+     * @param x
+     * @return 가장 큰 값
+     */
+    private static int getMaxOfSumAndMulti(Integer x) {
+        int remain = -1;
+        int multiple = 1;
+        int sum = 0;
+
+        while (x > 0) {
+            remain = x % 10;
+            sum += remain;
+            multiple *= remain;
+            x = x / 10;
+        }
+        return Math.max(sum, multiple);
     }
+
 }
