50d03dbf90fdedb4916c17606464221beeacddef
hong-sile <gurwns9325@naver.com>
2022-10-29 23:15:12 +0900

    feat : 문제에 필요한 멤버변수 추가
    사람 이름을 key, 점수를 value로 저장하는 friendScoreMap 생성,
    user와 현재 친구인 사람을 저장할 userFriendList를 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
+    private static Map<String,Integer> friendScoreMap = new HashMap<>();
+    private static List<String> userFriendList = new ArrayList<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+
+
+
+
         return answer;
     }
 }
