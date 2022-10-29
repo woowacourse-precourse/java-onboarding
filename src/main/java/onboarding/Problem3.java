8244d912af9d47a2857b0fd18afd5256aaa8b62e
choijoohee213 <choijoohee@naver.com>
2022-10-30 01:25:44 +0900

    refactor: 유효성 검사 부분을 메서드로 빼기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+    private static final int min = 1;
+    private static final int max = 10000;
     public static int solution(int number) {
-        int answer = 0;
+        if(number < min || number > max) {
+            throw new IllegalArgumentException("숫자의 범위를 벗어났습니다.");
+        }
+
         return answer;
     }
 }
