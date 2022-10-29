e819fc48c4a1a1948877c3aea5c48e6ffc72ba39
cocomozila <joseyun94@naver.com>
2022-10-30 03:58:26 +0900

    [문제 4] 알파벳 대문자,소문자를 제외한 글자를 판별하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,21 @@ public class Problem4 {
     // 주어진 문자열을 0번 인덱스부터 문자열 길이만큼 변환하는 기능
     public static String solution(String word) {
         StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(distinguishAlphabet(word.charAt(i)));
+        }
         return sb.toString();
+    }
+
+    // 알파벳 대문자,소문자를 제외한 글자를 판별하는 기능
+    private static char distinguishAlphabet(char alphabet) {
+        if (Character.isUpperCase(alphabet)) {
+            return toReverseUppercase(alphabet);
+        }
+        if (Character.isLowerCase(alphabet)) {
+            return toReverseLowercase(alphabet);
+        }
+        return alphabet;
     }
 
     // 대문자를 청개구리 대문자로 번역하는 기능
