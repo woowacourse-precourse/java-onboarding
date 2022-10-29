2a05b4350c517da0e34633af7c69e4a80a0b9da2
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 19:57:54 +0900

    :memo: [문제 3] : 풀이 전략

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,10 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        // 1. 1부터 number 까지의 숫자 확인
+        // 2. 각각의 숫자가 3또는 6또는 9를 포함하는 지 확인
+        // 3. 2번의 조건이 참인 숫자에 3또는 6또는 9의개수만큼 count++
+        // 3. count의 합 반환
         int answer = 0;
         return answer;
     }
