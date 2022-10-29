a0b346c18dffed3d47841ffdd6df5c22fa87e1bd
noparamin <noparamin@naver.com>
2022-10-29 12:02:19 +0900

    Problem2 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,48 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        String tmp = "";
+        while (true) {
+            tmp = removeDup(cryptogram);
+
+            if(cryptogram.length() == tmp.length()) {
+                break;
+            }
+
+            cryptogram = tmp;
+
+        }
+        answer = cryptogram;
+
         return answer;
+    }
+
+    public static String removeDup(String s) {
+
+        if (s.length() == 0 || s.length() == 1) {
+            return s;
+        }
+
+        char[] chars = s.toCharArray();
+
+        int k = 0;
+        int i = 0;
+        for(i = 1; i < chars.length; i++) {
+            if(chars[i - 1] != chars[i]) {
+                chars[k++] = chars[i - 1];
+            } else {
+                while(i < chars.length - 1 && chars[i - 1] == chars[i]) {
+                    i++;
+                }
+            }
+        }
+        if(chars[i - 1] != chars[i - 2]) {
+            chars[k++] = chars[i - 1];
+        }
+
+        String result = new String(chars).substring(0, k);
+
+        return result;
     }
 }
