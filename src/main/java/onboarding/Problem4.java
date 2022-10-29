ba3526d8842b68be3b8fcde0f91c4f202d87b281
saehim <saeilee.vxv@gmail.com>
2022-10-30 00:21:15 +0900

    Feat(Problem4): 대문자 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+    // 대문자 변환
+    static char uppercase(char a){
+        char answer = (char)(155 - a);
+        return answer;
+    }
     // 소문자 변환
     static char lowercase(char a){
         char answer = (char)(219 - a);
