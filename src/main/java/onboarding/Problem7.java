69ea5e004d71b0ead132d3c3a5e6dbd665479c88
lee <higgs95@naver.com>
2022-10-29 20:38:02 +0900

    feat(problem7): organize friendship information

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,44 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String, HashSet<String>> friendMap = arrangeFriends(friends);
+
+
         return answer;
+    }
+
+    public static HashMap<String, HashSet<String>> arrangeFriends(List<List<String>> friends) {
+        HashMap<String, HashSet<String>> friendMap = new HashMap<>();
+
+        for(List<String> friend : friends) {
+            HashSet<String> friendsSet;
+
+            if (friendMap.containsKey(friend.get(0))) {
+                friendsSet = friendMap.get(friend.get(0));
+            }else{
+                friendsSet = new HashSet<>();
+            }
+
+            friendsSet.add(friend.get(1));
+            friendMap.put(friend.get(0), friendsSet);
+
+            if (friendMap.containsKey(friend.get(1))) {
+                friendsSet = friendMap.get(friend.get(1));
+            }else{
+                friendsSet = new HashSet<>();
+            }
+
+            friendsSet.add(friend.get(0));
+            friendMap.put(friend.get(1), friendsSet);
+        }
+
+        return friendMap;
     }
 }
