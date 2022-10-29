fecb3dcec9be84e409a7683338331a56baca14fe
Reznov <reznov03048@gmail.com>
2022-10-30 00:06:52 +0900

    친구 관계를 리스트에 넣어 그래프 구조를 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,7 +6,43 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
+        List<String> member = new ArrayList<>();
+
         // 친구 관계를 그래프로 저장
+        Iterator<List<String>> friendsItr = friends.iterator();
+        for(int i=0; i<friends.size(); i++){
+            List<String> ABfriend = friendsItr.next();
+            String A = ABfriend.get(0);
+            String B = ABfriend.get(1);
+
+            if(!member.contains(A)){
+                member.add(A);
+            }
+            if(!member.contains(B)){
+                member.add(B);
+            }
+        }
+
+        List<String>[] friendList = new ArrayList[member.size()];
+        for(int i=0; i< member.size(); i++){
+            friendList[i] = new ArrayList<>();
+        }
+
+        // 친구 되어있는 관계를 리스트에 넣음
+        friendsItr = friends.iterator();
+        for(int i=0; i<friends.size(); i++){
+            List<String> ABfriend = friendsItr.next();
+            String A = ABfriend.get(0);
+            String B = ABfriend.get(1);
+
+            int aIdx = member.indexOf(A);
+            int bIdx = member.indexOf(B);
+
+            friendList[aIdx].add(B);
+            friendList[bIdx].add(A);
+        }
+
+
 
 
 
