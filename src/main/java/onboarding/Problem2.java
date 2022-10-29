b93c00f5e0e50087690961d137b74a9efd384326
qpdh <74190329+qpdh@users.noreply.github.com>
2022-10-29 11:25:32 +0900

    Update Problem2.java
    3개 이상 연속된 것 지우기 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,10 @@
 package onboarding;
 
-import java.util.Stack;
-
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        // 3개 이상  연속한 것 지우기
+
         String answer = "answer";
 
         // 연속 중복 문자를 삭제하기
@@ -12,25 +12,35 @@ public class Problem2 {
         // 스택을 이용해서 가장 위의 값과 삽입하려는 값이 같으면 삭제
         StringBuffer sb = new StringBuffer();
 
-        // 0은 바로 삽입
-        sb.append(cryptogram.charAt(0));
+        // 연속해서 지우는 중인가?
+        boolean isChainDelete = false;
+        char checkAlphabet = cryptogram.charAt(0);
 
         for (int i = 1; i < cryptogram.length(); i++) {
-            // 가장 뒤의 값 추출
-            char lastAlphabet = sb.charAt(sb.length() - 1);
-
             // 만약 가장 뒤의 값과 삽입하려는 값이 같으면 -> 삭제
-            if (cryptogram.charAt(i) == lastAlphabet) {
-                sb.deleteCharAt(sb.length() - 1);
+            if (cryptogram.charAt(i) == checkAlphabet) {
+                isChainDelete = true;
             }
+
             // 아니라면 삽입
             else {
-                sb.append(cryptogram.charAt(i));
+                if (!isChainDelete) {
+                    sb.append(checkAlphabet);
+                }
+                isChainDelete = false;
+                checkAlphabet = cryptogram.charAt(i);
             }
+        }
+        if (!isChainDelete) {
+            sb.append(checkAlphabet);
         }
 
         answer = sb.toString();
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("bcdaaaa"));
     }
 }
