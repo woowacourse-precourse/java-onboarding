e15fa704177d554a217544a59a569e0fee04509c
parksey <parkseyeon99@naver.com>
2022-10-29 23:12:59 +0900

    feat(problem 2): 기능 요구사항 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,22 @@
 package onboarding;
 
+/**
+ * Problem 2
+ *
+ * 기능 요구 사항
+ * 1. 임의의 문자열 cryptogram이 매개변수 주어진다.
+ * 2. 연속하는 중복 문자들을 삭제한 결과 return
+ *
+ * 제한사항
+ * 1. cryptogram은 길이가 1부터 1000이하인 문자열
+ * 2. cryptogram은 알파벳 소문자로만 이루어져 있다.
+ */
 public class Problem2 {
+    /**
+     *
+     * @param cryptogram
+     * @return 중복 문자 삭제한 결과 값
+     */
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
