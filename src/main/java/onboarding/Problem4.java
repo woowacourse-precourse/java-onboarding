0f9b5d806af42d9733689cf83b950d8bf6e56868
LHS-11 <ghktjq1119@naver.com>
2022-10-29 19:03:41 +0900

    Problem4 : getAnswer 메서드에서 대문자, 소문자, 빈칸, 예외사항 메서드로 추출

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,30 +7,54 @@ import java.util.ArrayList;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-
         ArrayList<Character> upperDictionary = new ArrayList<>();
         ArrayList<Character> lowerDictionary = new ArrayList<>();
         saveFrogDictionary(upperDictionary, lowerDictionary);
-
-        //2.  주어진 word 청개구리 사전에 맞춰서 변환
-        for (int i = 0; i < word.length(); i++) {
-
-            char characterBeforeConversion = word.charAt(i);
-
-            if(isUpperCase(characterBeforeConversion))
-                answer += upperDictionary.get((int)(word.charAt(i)-'A'));
-            else if(isLowerCase(characterBeforeConversion))
-                answer+=lowerDictionary.get((int)(word.charAt(i)-'a'));
-            else if(isBlank(characterBeforeConversion)){
-                answer+=' ';
-            }else{
-                throw new NotWantedCharacter(characterBeforeConversion + "은 변환되지 않는 문자입니다.");
-            }
-        }
-
+        answer = getAnswer(word, answer, upperDictionary, lowerDictionary);
         return answer;
     }
 
+    //2.  주어진 word 청개구리 사전에 맞춰서 변환
+    private static String getAnswer(String word, String answer, ArrayList<Character> upperDictionary, ArrayList<Character> lowerDictionary) {
+        for (int i = 0; i < word.length(); i++) {
+            char characterBeforeConversion = word.charAt(i);
+            answer = getUpperCase(word, answer, upperDictionary, i, characterBeforeConversion);
+            answer = getLowerCase(word, answer, lowerDictionary, i, characterBeforeConversion);
+            answer = getBlank(answer, characterBeforeConversion);
+            checkException(characterBeforeConversion);
+        }
+        return answer;
+    }
+
+    // 예외사항 변환할 수 있는 문자인지 체크
+    private static void checkException(char characterBeforeConversion) {
+        if(!isLowerCase(characterBeforeConversion) && !isUpperCase(characterBeforeConversion) && !isBlank(characterBeforeConversion)){
+            throw new NotWantedCharacter(characterBeforeConversion + "은 변환되지 않는 문자입니다.");
+        }
+    }
+
+    private static String getBlank(String answer, char characterBeforeConversion) {
+        if(isBlank(characterBeforeConversion)){
+            answer +=' ';
+        }
+        return answer;
+    }
+
+    private static String getLowerCase(String word, String answer, ArrayList<Character> lowerDictionary, int i, char characterBeforeConversion) {
+        if(isLowerCase(characterBeforeConversion)) {
+            answer += lowerDictionary.get((int) (word.charAt(i) - 'a'));
+        }
+        return answer;
+    }
+
+    private static String getUpperCase(String word, String answer, ArrayList<Character> upperDictionary, int i, char characterBeforeConversion) {
+        if(isUpperCase(characterBeforeConversion)) {
+            answer += upperDictionary.get((int) (word.charAt(i) - 'A'));
+        }
+        return answer;
+    }
+
+    // 1. 배열에 청개구리 사전 저장
     private static void saveFrogDictionary(ArrayList<Character> upperDictionary, ArrayList<Character> lowerDictionary) {
         for (int i = 0; i < 26; i++) {
             upperDictionary.add((char) ('Z'-i));
