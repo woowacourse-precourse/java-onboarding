c4b25b8fb1bffa4d9c1ba1c7a388b109c15d6375
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-29 15:59:25 +0900

    comment: write function list of problem 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Problem 5 function list
+ * - Count each unit's number
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
