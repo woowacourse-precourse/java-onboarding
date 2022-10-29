4e6080e7ae9dc1f6d3a765d8aef6fe6710c1c855
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-10-30 01:31:31 +0900

    feat(6번): 두개 이상 같은 글자가 있는지 확인하는 함수 hasDuplicatedStr 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
+import org.assertj.core.util.Lists;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    public static boolean hasDuplicatedStr(String A, String B){
+
+        for (int i=0; i<A.length()-1; i++){
+            if (B.contains(A.substring(i,i+2))){
+                return true;
+            }
+        }
+        return false;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = new ArrayList<>();
+        for (int i=0; i<forms.size(); i++){
+            for (int j=0; j<i; j++){
+                if(hasDuplicatedStr(forms.get(i).get(1), forms.get(j).get(1))){
+                    answer.add(forms.get(i).get(0));
+                    answer.add(forms.get(j).get(0));
+                }
+            }
+        }
+        answer = sortByEmail(answer);
         return answer;
     }
 }
