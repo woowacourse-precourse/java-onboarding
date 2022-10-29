68e319273900c841d112b9dc5815b9813dc1f068
kimdeokyoung <dudejrss@naver.com>
2022-10-29 15:58:59 +0900

    [Edit] src/main/java/onboarding/Problem2.java : make structure Problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static String decipher(String ciphertext){
+        String answer = "answer";
+
+        return answer;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
