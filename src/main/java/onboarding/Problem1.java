fe065f916095b2ec7a9b05f9cfb3bbb87403f30f
hong <hong@hongui-MacBookAir.local>
2022-10-29 16:33:28 +0900

    feat: 각 자리수 더하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,10 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static int addEachDigit(int num){
+        int first = num / 100;
+        int second = (num%100) /10;
+        int third = ((num%100) %10);
+        return first + second + third;
+    }
 }
