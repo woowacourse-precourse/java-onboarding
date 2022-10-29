08d274c5733e00aedc7d98ef8674d1e0b4ab512b
im-shung <seoyoung1113@gmail.com>
2022-10-29 17:41:25 +0900

    :sparkles: 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수 구하기 기능 구현 #3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -11,8 +11,8 @@ public class Problem3 {
     );
 
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        /*  [요구] 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수 구하기 */
+        return getGameResult(number);
     }
 
     //숫자에 3,6,9가 들어갈 때 3,6,9의 개수를 구하는 메서드
@@ -26,5 +26,14 @@ public class Problem3 {
         return cnt;
     }
 
+    //  1부터 number까지 손뼉 치는 횟수 구하는 메서드
+    public static int getGameResult(int endNumber) {
+        int result = 0;
+        /* [요구] 숫자에 3,6,9가 들어갈 때 3,6,9의 개수만큼 손뼉을 친다. */
+        for (int number=1;number<=endNumber;number++){
+            result += getClapCount(number);
+        }
+        return result;
+    }
 
 }
