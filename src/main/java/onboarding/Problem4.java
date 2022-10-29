770498f3d8b0e610353be522517b0dd4c4b560dd
SeongukBaek <bsu6235@gmail.com>
2022-10-29 17:10:21 +0900

    feat: implement isAlphabet()

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,13 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /*
+    * 주어진 문자의 알파벳 여부를 반환
+    *
+    * @return boolean
+    * */
+    private static boolean isAlphabet(char character) {
+        return (character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z');
+    }
 }
