27f7cac785516cf4951adc82e5da5497b3ea4e83
kongji <kongji4092@gmail.com>
2022-10-29 20:46:59 +0900

    [프리코스] 4번 문제

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        ArrayList<Character> alphabet = new ArrayList<>();
+        ArrayList<Character> alphabetReverse = new ArrayList<>();
+        StringBuffer sb = new StringBuffer();
+        char[] wordList = word.toCharArray();
+
+        for (int i = 0; i < 26; i++) {
+            alphabet.add((char) ('a' + i));
+            alphabetReverse.add((char) ('a' + i));
+        }
+
+        Collections.reverse(alphabetReverse);
+
+        for (char i: wordList){
+            if (Character.isUpperCase(i)){
+                int idx = alphabet.indexOf(Character.toLowerCase(i));
+                sb.append(Character.toUpperCase(alphabetReverse.get(idx)));
+            } else if (Character.isLowerCase(i)){
+                int idx = alphabet.indexOf(i);
+                sb.append(alphabetReverse.get(idx));
+            } else {
+                sb.append(' ');
+            }
+        }
+
+        return sb.toString();
     }
 }
