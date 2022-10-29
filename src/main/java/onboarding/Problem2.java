3e76966177a2a14457683982c9f7d7951a66d782
chanwonlee <clw8679@naver.com>
2022-10-29 17:38:15 +0900

    FEAT: [Problem2] 문자 중복 확인 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,18 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    /*
+    중복 문자가 있으면 true
+    없으면 false 반환
+     */
+    public static boolean checkString(String cryptogram) {
+        int n = cryptogram.length();
+        for (int i = 0; i < n-1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
