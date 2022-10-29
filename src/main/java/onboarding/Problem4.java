bdd1bbf2b515b593a87ecf38fb8663bdf74b8b1d
sangho-jeon <jeonsangho1587@gmail.com>
2022-10-29 16:22:56 +0900

    fix       : problem4 deleting case Converter due to the problem rule

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,29 +1,22 @@
 package onboarding;
 
 /*
-1. 대문자 소문자 컨버팅
-2. 규칙에 맞게 반대 케이스로 컨버팅
-3. 두 규칙을 모두 적용한 컨버터 메서드 작성
-4. String의 각 character 마다 변환 및 최종 string 구현
+1. 규칙에 맞게 반대 케이스로 컨버팅
+2. 두 규칙을 모두 적용한 컨버터 메서드 작성
+3. String의 각 character 마다 변환 및 최종 string 구현
 */
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         for (char character :
                 word.toCharArray()) {
             if (Character.isLetter(character)) {
-                caseConverter(character);
+                character = reverseConverter(character);
             }
+            sb.append(character);
         }
-        return answer;
-    }
-
-    public static char caseConverter(char character) {
-        if (Character.isUpperCase(character)) {
-            return Character.toLowerCase(character);
-        } else
-            return Character.toUpperCase(character);
+        return sb.toString();
     }
 
     public static char reverseConverter(char character) {
