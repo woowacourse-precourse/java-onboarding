d899a9ab8ba8fb7c2e11e9407d79ec766703f171
최형준 <choihyoungjun@choehyeongjun-ui-MacBookPro.local>
2022-10-29 13:42:49 +0900

    solve problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,34 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobi1 = cal(pobi.get(0));
+        int pobi2 = cal(pobi.get(1));
+        int crong1 = cal(crong.get(0));
+        int crong2 = cal(crong.get(1));
+        int large_pobi = (pobi1 > pobi2) ? pobi1 : pobi2;
+        int large_crong = (crong1 > crong2) ? crong1 : crong2;
+        if (large_pobi > large_crong) {
+            answer = 1;
+        } else if (large_pobi < large_crong) {
+            answer = 2;
+        } else if (large_pobi == large_crong) {
+            answer = 0;
+        } else {
+            answer = -1;
+        }
         return answer;
+    }
+
+    public static int cal(Integer num) {
+        int result = 0;
+        int sum = num % 10;
+        int mul = num % 10;
+        while (num / 10 != 0) {
+            num = num / 10;
+            sum += num % 10;
+            mul *= num % 10;
+        }
+        result = (sum > mul) ? sum : mul;
+        return result;
     }
 }
