17f23191447fb8a6db16aa76195945864294b15f
Park Sungchul <try615@naver.com>
2022-10-29 21:28:54 +0900

    [feat] Problem5.java : 코딩테스트 형식으로 프로토타입 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
+    public static void main(String[] args) {
+        int money = 15000;
+        System.out.println(solution(money));
+    }
+
+    private static int[] moneys = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < moneys.length; i++) {
+            int m = money / moneys[i];
+
+            money -= moneys[i] * m;
+            result.add(m);
+        }
+
+        return result;
     }
 }
