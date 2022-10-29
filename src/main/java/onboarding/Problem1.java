913e0509ea59aa8726f29a35391cb1edefbddbf0
heohyeonjink <hyeonjin4870@naver.com>
2022-10-29 20:08:05 +0900

    feat(PROBLEM1) : 예외 사항 처리
     - 왼쪽 페이지 또는 오른쪽 페이지가 1미만 이거나 400 초과인 경우
     - 오른쪽 페이지 - 왼쪽 페이지가 1이 아닌 경우
     - 왼쪽 페이지가 오른쪽 페이지보다 크거나 같은 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,20 +1,17 @@
 package onboarding;
-
 import java.util.*;
-
 class Problem1 {
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+
+        if(Except(pobi)) return -1;
+        if(Except(crong)) return -1;
+
         int pobi_score = max_score(pobi);
         int crong_score = max_score(crong);
 
-        q
         if(pobi_score>crong_score) return 1;
         else if(pobi_score<crong_score) return 2;
-        else if (pobi_score==crong_score) return 0;
-
-        return answer;
+        else return 0;
     }
 
     public static int Sum(int page){
@@ -42,6 +39,18 @@ class Problem1 {
             max = Math.max(max,Multi(x));
         }
         return max;
+    }
+
+    public static boolean Except(List<Integer> user){
+
+        int pg_left = user.get(0);
+        int pg_right = user.get(1);
+
+        if(pg_left<1 || pg_right<1 || pg_left > 400 || pg_right > 400) return true;
+        if(pg_right-pg_left!=1) return true;
+        if(pg_left>=pg_right) return true;
+
+        return false;
     }
 
 }
