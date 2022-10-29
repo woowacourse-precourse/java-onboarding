8e53f24f911f315decc056df419c8d253680f9ae
Haaae <101785628+Haaae@users.noreply.github.com>
2022-10-30 08:02:32 +0900

    [Problem2] fix: removeDuplication() 수정
    - 중복을 삭제하는 한 시퀸스에 중복된 알파벳이 여러 개일 경우, 동시에 삭제하도록 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
@@ -35,15 +38,22 @@ public class Problem2 {
         int start = -1; // 삭제할 범위의 시작 인덱스
         int end = -1;   // 삭제할 범위의 마지막 인덱스
         boolean isRemoved = false;
+        List<Integer> starts = new ArrayList();
+        List<Integer> ends = new ArrayList();
         // 삭제할 범위의 시작 인덱스가 암호문의 마지막 인덱스이거나, 중복이 검색되어 end가 start보다 커지면 while문 종료
-        while (start < cryptogram.length() - 1 && start >= end) {
+        while (start < cryptogram.length() - 1) {
             start++; // start를 1씩 증가하여 해당 인덱스의 알파벳 중복을 검사
-            end = searchDuplicateIndex(start, cryptogram.charAt(start), cryptogram);
             // 중복이 검색되면 연속된 중복의 마지막 인덱스가, 검색안되면 start 반환
+            end = searchDuplicateIndex(start, cryptogram.charAt(start), cryptogram);
+            if (start < end && end < cryptogram.length()) { // start와 end가 유효하다면,
+                starts.add(start);
+                ends.add(end);
+                isRemoved = true;
+            }
         }
-        if (start < end && start > -1 && end < cryptogram.length()) { // start와 end가 유효하다면,
-            cryptogram.delete(start, end + 1);  // start<= x <= end 범위의 문자 삭제
-            isRemoved = true;
+        // 삭제 시 인덱스가 줄어드는 것을 고려하여 뒤의 중복부터 삭제
+        for (int i = starts.size() - 1; i >= 0; i--) {
+            cryptogram.delete(starts.get(i), ends.get(i) + 1);  // start<= x <= end 범위의 문자 삭제
         }
         return isRemoved;
     }
@@ -53,52 +63,10 @@ public class Problem2 {
         int end;
         // break가 발생하지 않으면 최종적인 end의 값은 cryptogarm.length()-1
         for (end = start; end < cryptogram.length()-1; end++) {
-            if (cryptogram.charAt(end + 1) != 'c') {
+            if (cryptogram.charAt(end + 1) != c) {
                 break;
             }
         }
         return end;
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
