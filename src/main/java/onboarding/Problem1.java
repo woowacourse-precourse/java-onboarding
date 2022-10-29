b58e3e696d822f0aedd7a4ede1ad0ccd210c9ea9
zcae1020 <zcae1020@naver.com>
2022-10-29 17:57:29 +0900

    docs: add requirement in Problem1 fix: method names in Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,11 +7,11 @@ class Problem1 {
         if(detectError(pobi) || detectError(crong))
             return -1;
 
-        int pobiNum = getMax(pobi), crongNum = getMax(crong);
+        int pobiNum = getMaxOfNumbers(pobi), crongNum = getMaxOfNumbers(crong);
 
-        if(pobiNum==crongNum)
+        if(pobiNum == crongNum)
             return 0;
-        else if(pobiNum>crongNum)
+        else if(pobiNum > crongNum)
             return 1;
         else
             return 2;
@@ -30,15 +30,15 @@ class Problem1 {
     }
 
     //리스트 안 숫자들의 getSum, getMultiple의 값들 중 최대값 구하기
-    private static int getMax(List<Integer> list){
+    private static int getMaxOfNumbers(List<Integer> list){
         int ret = 0, num;
 
         for(int element : list) {
-            num = getSum(element);
+            num = getSumEachDigit(element);
             if(ret < num)
                 ret = num;
 
-            num = getMultiple(element);
+            num = getMultipleEachDigit(element);
             if(ret < num)
                 ret = num;
         }
@@ -47,7 +47,7 @@ class Problem1 {
     }
 
     //각 자리 숫자 더하기 값 구하기
-    private static int getSum(int num) {
+    private static int getSumEachDigit(int num) {
         int ret = 0;
 
         while(num>0) {
@@ -59,7 +59,7 @@ class Problem1 {
     }
 
     //각 자리 숫자 곱하기 값 구하기
-    private  static int getMultiple(int num) {
+    private  static int getMultipleEachDigit(int num) {
         int ret = 1;
 
         while(num>0) {
