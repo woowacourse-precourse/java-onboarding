93ef00b47434b6041130845e76f1a1b328b6dff8
noah.lee <rhkd2612@naver.com>
2022-10-30 01:17:13 +0900

    docs: problem7 methods

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ * # 기능 정리
+ * [X] 친구 관계 저장
+ * [X] 방문자 점수 계산
+ * [X] 함께 아는 친구 계산
+ * [X] 추천 친구 목록 계산(원래 친구 제외)
+ * [X] 추천 친구 정렬
+ * [X] solution 작성
+ * [X] 테스트 확인
+ */
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
