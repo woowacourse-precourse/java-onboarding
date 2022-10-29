0f50c785177b6ae8f7f6e97e48b8e344c19c271d
chj <bluedog129@gmail.com>
2022-10-29 18:16:20 +0900

    [P5] money 나누기 구현 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static int calculateMoney(int money) {
-        return money;
-    }
     public static List<Integer> solution(int money) {
-        List<Integer> result = Collections.emptyList();
-
+        List<Integer> result = new ArrayList<>();
+        while (money > 0) {
+            if (money >= 50000) {
+                result.add(money / 50000);
+                money %= 50000;
+            } else if (money >= 10000) {
+                result.add(money / 10000);
+                money %= 10000;
+            } else if (money >= 5000) {
+                result.add(money / 5000);
+                money %= 5000;
+            } else if (money >= 1000) {
+                result.add(money / 1000);
+                money %= 1000;
+            } else if (money >= 100) {
+                result.add(money / 100);
+                money %= 100;
+            } else if (money >= 50) {
+                result.add(money / 50);
+                money %= 50;
+            } else if (money >= 10) {
+                result.add(money / 10);
+                money %= 10;
+            } else if (money >= 1) {
+                result.add(money / 1);
+                money %= 1;
+            }
+        }
         return result;
     }
 }
