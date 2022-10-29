26cb5ac813e938522a510dd96d5ddb960654a7ff
SEONMl <sm000117@naver.com>
2022-10-29 22:22:40 +0900

    feat: Problem6 개인 정보를 캡슐화하는 Crew class 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    static class Crew {
+        private String email;
+        private String nickname;
+
+        public Crew(List<String> list) {
+            this.email = list.get(0);
+            this.nickname = list.get(1);
+        }
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
