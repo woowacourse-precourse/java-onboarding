e7e48fc6fe55da558be909e22a57deba89c111e3
wonjunYou <youwjune@gmail.com>
2022-10-30 01:39:28 +0900

    feat : (Problem 4) 알파벳을 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -21,6 +21,8 @@ public class Problem4 {
         }
 
         initLetters();
+
+        return makeReverseWord(word);
     }
 
     public static void validateLength(String word) {
@@ -44,5 +46,36 @@ public class Problem4 {
         for (int index = 0; index < COUNTS_OF_ALPHABET; index++) {
             LOWERCASE_LETTERS.add((char) (ASCII_CODE_OF_LAST_LOWERCASE_ALPHABET - index));
         }
+    }
+
+    private static String makeReverseWord(String word) {
+        String reverseWord = "";
+        char[] letters = word.toCharArray();
+
+        for (char letter : letters) {
+            reverseWord += convertLetter(letter);
+        }
+
+        return reverseWord;
+    }
+
+    private static char convertLetter(char letter) {
+        if (isUppercaseLetter(letter)) {
+            return convertUppercaseLetter(letter);
+        }
+
+        if (isLowercaseLetter(letter)) {
+            return convertLowercaseLetter(letter);
+        }
+
+        return letter;
+    }
+
+    private static char convertLowercaseLetter(char letter) {
+        return LOWERCASE_LETTERS.get(letter - 'a');
+    }
+
+    private static char convertUppercaseLetter(char letter) {
+        return UPPERCASE_LETTERS.get(letter - 'A');
     }
 }
