9dc322e50f4107cf1770d322117ec720afbc1cf0
yg0585 <yg0585@naver.com>
2022-10-29 17:50:21 +0900

    2번 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,11 +5,32 @@ import java.util.List;
 
 //-----기능-----
 //1. 지폐 배열 만들기
+//2. 각 액수 마다 몇 장 필요한지 계산
 
 public class Problem5 {
+
+    private static int calPaper(int bankmoney, int money) {
+        int cnt = 0;
+
+        while (true){
+            if (money - bankmoney < 0)
+                break;
+            cnt++;
+            money -= bankmoney;
+        }
+
+        return cnt;
+    }
+
     public static List<Integer> solution(int money) {
-        int[] bank = {10000, 50000, 5000, 1000, 500, 100, 50, 10};
+        int[] bank = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> answer = Collections.emptyList();
+
+        for (int bankmoney: bank)
+        {
+            int num = calPaper(bankmoney, money);
+        }
+
         return answer;
     }
 }
