4aeaf0b0c30781a6494e42cdbf10720f05ca144b
Wooseobee <dntjq0807@naver.com>
2022-10-30 03:22:52 +0900

    result와 이메일의 오름차순 정렬 및 중복 제거 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,37 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            String email = forms.get(i).get(0);
+            String nick = forms.get(i).get(1);
+            boolean duplicated = false;
+
+            for (int j = i + 1; j < forms.size() - 1; j++) {
+                if (isDuplicated(nick, forms.get(j).get(1))) {
+                    if (!answer.contains(forms.get(j).get(0))) {
+                        answer.add(forms.get(j).get(0));
+                        duplicated = true;
+                    }
+                }
+            }
+            if (duplicated) {
+                if (!answer.contains(email)) {
+                    answer.add(email);
+                }
+            }
+        }
+        if (answer == null) {
+            return Collections.emptyList();
+        }
+        answer.sort(Comparator.naturalOrder());
         return answer;
     }
 
