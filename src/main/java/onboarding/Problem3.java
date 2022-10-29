8be85b781907d3524a80b56496c7819bbdd63b73
BaeDongHyeon <sasd730@gmail.com>
2022-10-29 21:23:54 +0900

    refactor(Problem3.java): 각 자리의 수와 개수를 구하는 기능 메소드로 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,17 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int[] clapCount = new int[10];
 
+        int totalClapCount = 0;
         for (int i = 1; i <= number; i++) {
-            String[] numberSeat = String.valueOf(i).split("");
-            for (int j = 0; j < numberSeat.length; j++) {
-                ++clapCount[Integer.parseInt(numberSeat[j])];
-            }
+            totalClapCount += getClapCount(i);
         }
 
-        int totalClapCount = clapCount[3] + clapCount[6] + clapCount[9];
-
         return totalClapCount;
+    }
+
+    private static int getClapCount(int number) {
+        int[] clapCount = new int[10];
+
+        String[] numberSeat = String.valueOf(number).split("");
+
+        for (int j = 0; j < numberSeat.length; j++) {
+            ++clapCount[Integer.parseInt(numberSeat[j])];
+        }
+
+        return clapCount[3] + clapCount[6] + clapCount[9];
     }
 }
