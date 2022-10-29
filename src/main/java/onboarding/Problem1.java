8d93efc7cb7e75ae332ec1d27125e86b715cd3e6
Noah <shgustmd6865@naver.com>
2022-10-29 19:07:37 +0900

    Feat : 반환되는 결과 값에 대한 상수 지정
    반환 되는 결과 값을 지정하여 사용할 수 있도록 상수로 설정합니다.
    POBY_WIN_CODE = 1 / CRONG_WIN_CODE = 2 / DRAW_CODE = 0 / EXCEPTION_CASE_CODE = -1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int POBY_WIN_CODE = 1;
+    private static final int CRONG_WIN_CODE = 2;
+    private static final int DRAW_CODE = 0;
+    private static final int EXCEPTION_CASE_CODE = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
