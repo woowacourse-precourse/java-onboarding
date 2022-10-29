ae5d2310da8a719f8f148459dd04c280dc68e679
KJE <12181501@inha.edu>
2022-10-29 20:26:45 +0900

    solve problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        while(number > 2){
+            int i = number;
+            while(i > 2){
+                if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+                    answer++;
+                }
+                i = i/10;
+            }
+            number--;
+        }
         return answer;
     }
 }
