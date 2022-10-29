66d576627be211a252f093c2bc4bd85d34b83d32
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2022-10-29 18:07:04 +0900

    4. 결과값 출력 함수 -> while문을 통해 위의 과정 반복 후 결과값 출력  - start가 음수 -> 중복 부분이 없다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,8 +4,8 @@ package onboarding;
  * - 큰 틀의 기능 구현
  * 1. 문자열 비교 : 문자열 내 문자와 다음 문자가 같은지 체크 하는 함수
  * 2. 해당 인덱스 출력 함수 : 문자가 언제까지 같은지 출력하는 함수 - > 두 함수가 필요 시작점 출력 함수 끝점 출력함수
- * 2. 문자열에서 시작점과 끝점을 통한 삭제 함수
- * 3. 결과값 출력 함수 -> while문을 통해 위의 과정 반복 후 결과값 출력
+ * 3. 문자열에서 시작점과 끝점을 통한 삭제 함수
+ * 4. 결과값 출력 함수 -> while문을 통해 위의 과정 반복 후 결과값 출력
  * **/
 
 /**
@@ -28,19 +28,18 @@ package onboarding;
  * - 해당 substr을 replace 함수로 제거한다.
  * **/
 
+/**
+ * 4. 결과값 출력 함수 -> while문을 통해 위의 과정 반복 후 결과값 출력
+ * - start가 음수 -> 중복 부분이 없다
+ * **/
+
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "answer";
-        return answer;
+        return makeResult(answer);
     }
-//    public static void main(String[] args){
-//        char test1 = 'a';
-//        char test2 = 'b';
-//        String test3 = "zyelleyz";
-//        int i = searchStart(test3);
-//        int j = searchEnd(test3, i);
-//        System.out.println(deleteChar(test3, i,j));
-//    }
 
     private static boolean compareChar(char current, char next)
     {
@@ -61,22 +60,39 @@ public class Problem2 {
         return -1;
     }
     private static int searchEnd(String inputStr,int start){
+        if(start == -1)
+            return -1;
         char ret = inputStr.charAt(start);
-        System.out.println(start);
-        for (int i = start; i< inputStr.length(); i++)
+        int i = 0;
+        for (i = start; i < inputStr.length(); i++)
         {
-            if(compareChar(ret,inputStr.charAt(i))==false){
+            if(!compareChar(ret,inputStr.charAt(i))){
                 return i;
             }
             ret = inputStr.charAt(i);
         }
+        if(i == inputStr.length())
+            return i;
         return -1;
     }
     private static String deleteChar(String inputstr, int start, int end){
-        String subStr ="";
+        String subStr = "";
         String ret = inputstr;
         subStr = inputstr.substring(start,end);
         ret = ret.replace(subStr,"");
+        return ret;
+    }
+    private static String makeResult(String inputstr){
+        String ret = inputstr;
+        int start = 0;
+        int end = 0;
+        while(true) {
+            start = searchStart(ret);
+            if(start < 0)
+                break;
+            end = searchEnd(ret, start);
+            ret = deleteChar(ret,start,end);
+        }
         return ret;
     }
 }
