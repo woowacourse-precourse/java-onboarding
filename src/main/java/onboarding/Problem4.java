dbad130a41cc5b8ac77f836406ffb106b2363606
thdwoqor <celebarte01@gmail.com>
2022-10-29 16:25:58 +0900

    add Problem4 Solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        StringBuilder result = new StringBuilder(word);
         String answer = "";
+
+        for (int i = 0; i < result.length(); i++) {
+            int left = result.charAt(i);
+            if (left>=65&&left<=90)
+                answer+= (char)(65+90-left);
+            else if (left>=97&&left<=122)
+                answer+= (char)(97+122-left);
+            else
+                answer+=result.charAt(i);
+        }
         return answer;
     }
 }
