22d64f3334bca30480f130fd394c1455f13edd14
Park Sungchul <try615@naver.com>
2022-10-29 21:08:21 +0900

    [feat] Problem3.java : 코딩테스트 형식으로 프로토타입 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+    public static void main(String[] args) {
+        int number = 13;
+        System.out.println(solution(number));
+    }
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int result = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int pow = 0;
+            while (i >= Math.pow(10, pow)) {
+                int n = i % (int)Math.pow(10, pow + 1) / (int)Math.pow(10, pow);
+                if (n == 3 || n == 6 || n == 9) {
+                    result += 1;
+                }
+                pow += 1;
+            }
+        }
+
+        return result;
     }
 }
