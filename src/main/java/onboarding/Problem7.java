bf53fd21e3be0d1aadab967316db4ed7176f7181
정핸모 <gusah009@naver.com>
2022-10-29 14:52:11 +0900

    feat[문제 7번]: 내 친구 목록 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,13 +1,33 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 
+  private static final Set<String> myFriends = new HashSet<>();
+
   public static List<String> solution(String user, List<List<String>> friends,
       List<String> visitors) {
-    List<String> answer = Collections.emptyList();
-    return answer;
+    initMyFriends(user, friends);
+  }
+
+  private static void initMyFriends(String user, List<List<String>> friends) {
+    myFriends.add(user);
+    for (List<String> friend : friends) {
+      if (user.equals(friend.get(0))) {
+        myFriends.add(friend.get(1));
+        continue;
+      }
+      if (user.equals(friend.get(1))) {
+        myFriends.add(friend.get(0));
+      }
+    }
   }
 }
