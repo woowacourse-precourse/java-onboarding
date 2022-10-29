818b85a052093f9fc91f30feb065b12879e7b753
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-29 22:09:39 +0900

    feat(problem5): 요구사항 설정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+
 import java.util.List;
 
+/**
+ * > 요구사항
+ * 1. money를 저장
+ * 2. 반복문을 돌면서 money가 큰 수부터 차례로 최대한 많이 가져온다.
+ * 3. 각 단위 별로 가져온 배열을 출력
+ */
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
     }
 }
