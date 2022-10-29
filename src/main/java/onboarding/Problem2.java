b6700ecb21497284d189c335ecc90631caec1ff7
채우석 <wochae@wochae-MacBookAir.local>
2022-10-29 22:50:28 +0900

    [feat] 2.2 순회하는 메소드 내에서 중복된 문자를 발견하면 지우고 다시 순회하는 동작 로직 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        List<String> crypto = Arrays.asList(cryptogram);
+        iterateList(crypto);
+
         return answer;
+    }
+
+    private static void iterateList(List<String> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).equals(list.get(i + 1))) {
+                list.remove(i + 1);
+                list.remove(i);
+                i--;
+            }
+        }
     }
 }
