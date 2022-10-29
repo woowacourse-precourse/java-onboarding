053ebe8e95a45c0277b78b91b4816d5413ce01cc
Seiyon Park <seiyonpdev@gmail.com>
2022-10-29 23:28:43 +0900

    feat: ADD method Problem4.solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        char[] decode = word.toCharArray();
+
+        for (int i = 0; i < decode.length; i++) {
+
+            if(Character.isUpperCase(decode[i])) {
+                decode[i] = (char) ('A' + 'Z' - decode[i]);
+            }
+            else if(Character.isLowerCase(decode[i])) {
+                decode[i] = (char) ('a' + 'z' - decode[i]);
+            }
+
+        }
+
+        return new String(decode);
     }
+
 }
