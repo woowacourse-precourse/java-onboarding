7c537d045cd368d07679af22c7efd6c996e5aec3
kwYoohae <gocksdl7979@gmail.com>
2022-10-29 13:40:08 +0900

    feat(Problem.6): 모든 배열에 있는 값에 중복이 존재하는지 확인하는 메서드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
+
+    private static Set<String> answer = new HashSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        return null;
+    }
+
+    public static void checkDuplicate(List<List<String>> forms, String checkName) {
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String name = form.get(1);
+
+            if (name.contains(checkName)) {
+                answer.add(email);
+            }
+        }
     }
 }
