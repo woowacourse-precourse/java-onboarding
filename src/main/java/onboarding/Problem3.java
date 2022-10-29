a177d6ab215744b80f8618b8c05502499cf263fb
glaehfdl1654 <glaehfdl1654@naver.com>
2022-10-29 16:52:57 +0900

    [Problem3] solution function finish

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += getClapNum(i);
+        }
         return answer;
     }
     public static int getClapNum(int n) {
