a2ac67357ee389024c7584e70a8781a55f75bbdd
kimsinhyun <ksh415622@naver.com>
2022-10-29 18:55:12 +0900

    구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
-
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int result = 0;
+        for(int i=0; i<=number; i++){
+            result+=check_num(i);
+        }
+        return result;
+    }
+    public static int check_num(int num){
+        int result = 0;
+        while (num>0){
+            int n = num%10;
+            if (n!=0 && n%3==0){
+                result+=1;
+            }
+            num/=10;
+        }
+        return result;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        int[] temp = new int[] {50_000, 10_000, 5_000, 1_000, 500, 100, 50,10, 1};
+        for(int i=0; i<temp.length; i++){
+            answer.add(money/temp[i]);
+            money %= temp[i];
+        }
         return answer;
     }
 }
