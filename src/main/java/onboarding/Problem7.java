19c82c0ab9ef6010551e10762337b13236c2e85d
qdeu22 <qdeu22@gmail.com>
2022-10-30 00:02:56 +0900

    friends 리스트에서 user의 친구 아이디를 찾아 ar 리스트에 추가하는 기능 커밋

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        /* ArrayList를 이용하여 user의 친구 아이디를 추가 */
+        List<String> ar = new ArrayList<>();
+
+        /* friends 리스트에서 user의 친구 아이디를 찾아 ar 리스트에 추가하는 기능 */
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).contains(user)) {
+                int n = friends.get(i).indexOf(user);
+                ar.add(friends.get(i).get(1 - n));
+            }
+        }
+
         return answer;
     }
 }
