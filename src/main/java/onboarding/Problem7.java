8b6ae5dba48b40f3ca005b66eecd9447ca242cb3
juhee park <jouhi3322@gmail.com>
2022-10-29 21:54:22 +0900

    refactor : Problem7 추천 친구에 대한 객체 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,9 +9,9 @@ import java.util.List;
  * 기능목록
  * 1. 유저 객체 생성(친구 리스트 생성, 추천친구 hash생성)
  * 2. 모든 유저의 객체 생성, 친구관계 저장
- * 2. 유저의 친구의 친구에 대해 점수 증가
- * 3. 유저의 타임라인의 방문자에게 점수부여 (이때 친구는 증가 시켜선 안된다)
- * 4. 점수가 가장 높은 순으로 정렬하여 최대 5명 출력, 같을시에는 이름순정렬
+ * 3. 유저의 친구의 친구에 대해 점수 증가
+ * 4. 유저의 타임라인의 방문자에게 점수부여 (이때 친구는 증가 시켜선 안된다)
+ * 5. 점수가 가장 높은 순으로 정렬하여 최대 5명 출력, 같을시에는 이름순정렬
  *     (0점은 해시맵에 들어오지 않은 경우여서 존재하지 않음)
  */
 
@@ -19,7 +19,7 @@ public class Problem7 {
     private static class User {
         String name;
         List<String> friendsList = new ArrayList<>();
-        HashMap<String, Integer> recommendFriendsList = new HashMap<>();
+        HashMap<String, RecommendFriend> recommendFriendsList = new HashMap<>();
 
 
         public User(String name) {
@@ -28,6 +28,18 @@ public class Problem7 {
 
         private void addFriends(String name) {
             friendsList.add(name);
+        }
+
+
+    }
+
+    private static class RecommendFriend {
+        String name;
+        int point;
+
+        public RecommendFriend(String name, int point) {
+            this.name = name;
+            this.point = point;
         }
     }
 
