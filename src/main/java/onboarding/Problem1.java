afb25cd8275944168d6d8f00733bb37e9dd15037
5onchangwoo <scw9410@gmail.com>
2022-10-29 17:31:09 +0900

    problem1/숫자 분리하기 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,16 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int[] splitNum(Integer num) {
+        return Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
     }
 }
