57913261e68da089a0006eb1c97833f0e49af153
jagajang <kosb7544@gmail.com>
2022-10-29 12:24:08 +0900

    p3/add linear check

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++)
+            answer += clapNum(i);
+
         return answer;
+    }
+
+    private static int clapNum(int num) {
+        return 0;
     }
 }
