ac62ff5e9f6f96ae251c5bcc18d0d0b9183f6df0
JeongHongJin <mistake9518@gmail.com>
2022-10-29 13:20:05 +0900

    feat: Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        char[] alpha = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R'
+                , 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G'
+                , 'F', 'E', 'D', 'C', 'B', 'A'};
+        char[] alpha2 = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r'
+                , 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g'
+                , 'f', 'e', 'd', 'c', 'b', 'a'};
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                sb.append(alpha2[c - 'a']);
+            } else if (c >= 'A' && c <= 'Z') {
+                sb.append(alpha[c - 'A']);
+            } else {
+                sb.append(' ');
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("I love you"));
     }
 }
