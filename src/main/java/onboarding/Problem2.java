fb752178379ae7edaa25be54cd4c65baa15d9620
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 02:55:41 +0900

    feat : Problem2 제한사항 확인 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+
+		try {
+			isValidCryptogram(cryptogram);
+		} catch (IllegalStateException illegalStateException) {
+			illegalStateException.printStackTrace();
+		}
+
+		return answer;
+	}
+
+	/**
+	 * 1. 제한사항 확인
+	 * @param cryptogram : 검사할 문자열
+	 * @return 문자열의 유효 여부
+	 */
+	public static void isValidCryptogram(String cryptogram) throws IllegalStateException {
+		if (cryptogram.length() > 1000 || cryptogram.length() < 1)
+			throw new IllegalStateException("문자열의 길이가 맞지 않습니다.");
+		if (cryptogram.chars().allMatch(Character::isUpperCase))
+			throw new IllegalStateException("문자열이 소문자로 이루어져있지 않습니다.");
+	}
 }
