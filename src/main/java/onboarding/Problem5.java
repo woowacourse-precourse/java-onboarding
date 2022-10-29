0d061531f723b9a90f9d8247109653a6eccd0614
ymkim97 <rladudaud97@naver.com>
2022-10-29 16:04:39 +0900

    feat: add function boundary exception

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        if (throwBoundaryException(money)) {
+            return Collections.emptyList();
+        }
+        
         return answer;
+    }
+
+    public static boolean throwBoundaryException(int money) {
+        return money < 1 || money > 1000000;
     }
 }
