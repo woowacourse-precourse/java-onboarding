630c8bbc210fef94585d448b8654da9892957ab0
good9804 <good9804@naver.com>
2022-10-29 23:56:20 +0900

    문제 6번 케이스통과

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,28 +8,29 @@ import java.util.Set;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> duplicates=duplicates(forms);
-       List<String> answer= CheckDuplicates(duplicates);
-       
-        return answer;
+
+        return duplicates;
     }
-    public static List<String>  duplicates(List<List<String>> forms){//중복 닉네임 모아둔 배열
-        List<String> duplicates = new ArrayList<>();
+    public static List<String>  duplicates(List<List<String>> forms){//중복 닉네임 구하는 배열
+        Set<String> duplicatesSet = new HashSet<>();
         for (int i = 0; i < forms.size();i++ ) {
             String Nick = forms.get(i).get(1);
             String name="";
             for(int j=0;j<Nick.length()-1;j++){
                 name += (char) Nick.charAt(j);
                 name += (char) Nick.charAt(j+1);
-                if(forms.contains(name)){
-                    duplicates.add(forms.get(i).get(0));
-                }
+                for(int k=0;k<forms.size();k++){
+                    if(k==i){
+                        continue;
+                    }
+                    String Nickname=forms.get(k).get(1);
+                if(Nickname.contains(name)){
+                    duplicatesSet.add(forms.get(k).get(0));
+                }}
                 name="";
             }
         }
+        List<String> duplicates=new ArrayList<>(duplicatesSet);//
         return duplicates;
-    }
-    public static List<String>  CheckDuplicates(List<String> duplicates){//중복 닉네임 모아둔 배열속 같은 주소록은 하나만 만들기
-        Set<String> set = new HashSet<>(duplicates);]
-        return duplicates=new ArrayList<>(set);
     }
 }
