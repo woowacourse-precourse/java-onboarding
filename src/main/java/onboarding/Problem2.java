7a9e484943ba87b93a33c9cfbd0803c0a0d2512b
Sieun Park <psieun0606@gmail.com>
2022-10-30 04:50:59 +0900

    feat: problem2 문자열 분리 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem2 {
+
+    static char[] separateString(String cryptogram) {
+        char[] cryptoChar = cryptogram.toCharArray();
+
+        return cryptoChar;
+    }
+
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        char[] separatedCrypto = separateString(cryptogram);
+
         return answer;
     }
 }
