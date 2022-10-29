5fe16d4460216704ef9cfb4fd29614ec8302c69a
taekwon-dev <taekwon_dev@naver.com>
2022-10-29 14:37:48 +0900

    problem 4 - feat : 청개구리 사전 HashMap 사용하여  저장

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    private static HashMap<Character, Character> map = new HashMap<>();
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static void makeDictionary() {
+        int diff = 25;
+        for (int i = 'A'; i <= 'Z'; i++) {
+            map.put((char) i, (char) (i + diff));
+            diff -= 2;
+        }
     }
 }
