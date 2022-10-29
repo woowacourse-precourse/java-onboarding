3f566d4f1fe4b1e79d1e01fba6b9ceafb3c7a7c1
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-29 14:55:59 +0900

    refactor: Problem4 reverse메소드 내에 아스키코드부분 상수처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,10 @@ public class Problem4 {
     private static final int BIG = 1;
     private static final int SMALL = 2;
     private static final int ELSE = 3;
+    private static final int BIG_A = 65;
+    private static final int BIG_Z = 90;
+    private static final int SMALL_A = 97;
+    private static final int SMALL_Z = 122;
 
     public static String solution(String word) throws Exception {
         String answer = new String(reverse(word.toCharArray()));
@@ -12,10 +16,10 @@ public class Problem4 {
     private static char[] reverse(char[] charArray){
         for(int index=0;index<charArray.length;index++){
             if(isBigOrSmall(charArray[index])==BIG){
-                charArray[index]=(char)(90-(charArray[index]-65));
+                charArray[index]=(char)(BIG_Z-(charArray[index]-BIG_A));
             };
             if(isBigOrSmall(charArray[index])==SMALL){
-                charArray[index]=(char)(122-(charArray[index]-97));
+                charArray[index]=(char)(SMALL_Z-(charArray[index]-SMALL_A));
             }
             if(isBigOrSmall(charArray[index])==ELSE)continue;
         }
