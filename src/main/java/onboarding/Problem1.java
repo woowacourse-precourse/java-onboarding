e616b3ee1aaee035da968272ecebecb6b41f6005
ONE <sodlapdlf13@gmail.com>
2022-10-29 15:31:46 +0900

    feat: [Problem1] 두 참여자의 점수를 비교해 결과를 반환하는 로직 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,7 +11,10 @@ import java.util.List;
 * 4. Gamer class methods
 *   4-1. Gamer(int left, int right): 왼쪽 페이지와 오른쪽 페이지를 파라미터로 받는 생성자 메소드
 *   4-2. getMaxScore(): Gamer 의 최대 점수를 얻는 메소드
-*   4-3. getWinner(Gamer opponent): 다른 게이머의 최대 점수와 비교해 승패 결과를 int 값으로 얻는 메소드
+* 5. main logic
+*   5-1. pobi, crong 입력값에 대해 예외 처리하여 예외의 경우 -1 리턴
+*   5-2. Gamer pobi, crong 생성
+*   5-3. 두 Gamer의 MaxScore을 비교하여 결과 리턴
 * ※ 예외처리
 *   1. 페이지의 범위가 1 ~ 400 인지 검사
 *   2. 왼쪽 페이지가 홀수, 오른쪽 페이지가 짝수인지 검사
@@ -61,7 +64,13 @@ class Gamer {
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Gamer gamerPobi = new Gamer(pobi.get(0), pobi.get(1));
+        Gamer gamerCrong = new Gamer(crong.get(0), crong.get(1));
+
+        if (gamerPobi.getMaxScore() == gamerCrong.getMaxScore())
+            return 0;
+        if (gamerPobi.getMaxScore() > gamerCrong.getMaxScore())
+            return 1;
+        return 2;
     }
 }
