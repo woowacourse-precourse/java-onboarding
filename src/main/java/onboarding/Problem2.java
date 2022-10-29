fe6fceb6adbf02d29e53dd781170a8073f52e5b7
KYR <freekyr7529@gmail.com>
2022-10-30 07:17:48 +0900

    feature (Problem2) : 2번 문제 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.List;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
@@ -9,9 +12,27 @@ public class Problem2 {
         return answer;
     }
 
-    static String decryption(String ) {
-        String plaintext = "";
+    private static String decryption(String cipher) {
+        char[] cipherText = cipher.toCharArray();
+        Stack<Character> stack = new Stack<>();
 
-        return plaintext;
+        for (Character character: cipherText) {
+            if(stack.isEmpty()) {
+                stack.push(character);
+                continue;
+            }
+            if (character == stack.lastElement()) {
+                stack.pop();
+                continue;
+            }
+            stack.push(character);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Character character: stack) {
+            stringBuilder.append(character);
+        }
+
+        return stringBuilder.toString();
     }
 }
