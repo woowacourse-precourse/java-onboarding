40620a1950c23083dc1a02a8768d5dc7c725401c
skfkgla <narahim.lee@gmail.com>
2022-10-29 23:29:01 +0900

    feat: Problem2 - ArrayList 타입으로 한글자씩 자른 문자에서 인접한 문자중 중첩된 문자를 제거하는 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -18,4 +18,28 @@ public class Problem2 {
         String[] strArray = str.split("");
         return  new ArrayList<>(Arrays.asList(strArray));
     }
+
+    /**
+     * @param cryptogram : 복호화 하고싶은 문자열(한글자씩 ArrayList<String> 타입)
+     * @return 복호화된 문자열
+     */
+    private static String decryption(ArrayList<String> cryptogram){
+        int count = 0;
+        for(int i=1; cryptogram.size() > i; i++){
+            if(cryptogram.get(i).equals(cryptogram.get(i-1))){
+                int start = i-1;
+                count++;
+                while(cryptogram.size() > i && cryptogram.get(i).equals(cryptogram.get(i-1))){
+                    i++;
+                }
+                for(int j=0;i - start > j; j++){
+                    cryptogram.remove(start);
+                }
+            }
+        }
+        if(count != 0){
+            decryption(cryptogram);
+        }
+        return String.join("",cryptogram);
+    }
 }
