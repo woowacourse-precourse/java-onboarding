34dfa065e2047bf5601a7d925aa50983b7d997d2
jongmyeongbak <withpleasure@gmail.com>
2022-10-29 16:19:17 +0900

    check the page rule

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (isSatisfied(pobi) = 1 && isSatisfied(crong) = 1) {
+            int answer = Integer.MAX_VALUE;
+            return answer;
+        }
+        return -1;
     }
+
+    public static boolean isSatisfied(List<Integer> array) {
+        return array.get(0) > 1 && array.get(0) < 399 && array.get(1) - array.get(0) == 1;
+    }
+
 }
