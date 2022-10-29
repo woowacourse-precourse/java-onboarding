7ec01aca1c5ceda6fb3535c5ab2a9ba1d9a74fe5
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-29 23:56:27 +0900

    refactor: 3번 문제 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,19 +5,23 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            answer += judge(i); // judge를 통하여 짝 몇번 할 지 계산
+            answer += countClap(i);
         }
         return answer;
     }
 
-    public static int judge(int i) {
+    public static int countClap(int i) {
         int sum = 0;
         while (i > 0) { // 자릿수별로 3,6,9 가 나온 경우 더하기 위한 반복문
-            if ((i % 10 == 3) || (i % 10 == 6) || (i % 10 == 9)) {
+            if (is369(i)) {
                 sum++;
             }
             i /= 10;
         }
         return sum;
+    }
+
+    private static boolean is369(int i) {
+        return (i % 10 == 3) || (i % 10 == 6) || (i % 10 == 9);
     }
 }
