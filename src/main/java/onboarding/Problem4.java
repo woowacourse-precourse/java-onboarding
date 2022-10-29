b693c519a0e57ba1e271f962eef8433d1d98523d
권민정 <22alswjd@naver.com>
2022-10-29 21:19:30 +0900

    feat: Problem4 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int size = word.length();
+
+        for (int i = 0; i < size; i++) {
+            char c = word.charAt(i);
+            char replacedC;
+            if(c >= 65 && c <= 90) replacedC = (char) (90 + 65 - c);
+            else if(c >= 97 && c <= 122) replacedC = (char) (122 + 97 - c);
+            else replacedC = c;
+            answer = answer.concat(String.valueOf(replacedC));
+        }
         return answer;
     }
 }
