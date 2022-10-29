1ea081309c3c3aea3ddc253a5e22637416bbe7a8
apptie <appti@naver.com>
2022-10-29 11:20:34 +0900

    refactor: 문제 2 암호문에서 연속적인 문자를 삭제하는 기능 deleteDuplicateCharacter 메소드로 분리

--- src/main/java/onboarding/problem2/Decryptor.java
+++ src/main/java/onboarding/problem2/Decryptor.java
@@ -15,23 +15,28 @@ public class Decryptor {
 
     public static String decrypt(String cryptogram) {
         List<String> cryptogramList = Arrays.stream(cryptogram.split("")).collect(Collectors.toList());
-        boolean removeDuplicateCharacter = false;
+        boolean removeDuplicateCharacter = true;
 
-        while (!removeDuplicateCharacter) {
-            removeDuplicateCharacter = true;
-            for (int i = CHARACTER_START_INDEX; i < cryptogramList.size(); i++) {
-                int startCursor = i - CHARACTER_BEFORE_INDEX;
-                if (cryptogramList.get(startCursor).equals(cryptogramList.get(i))) {
-                    removeDuplicateCharacter = false;
-                    String target = cryptogramList.get(i);
-                    while (cryptogramList.size() > 0 && cryptogramList.get(startCursor).equals(target)) {
-                        cryptogramList.remove(startCursor);
-                    }
-                    i = startCursor;
-                }
-            }
+        while (removeDuplicateCharacter) {
+            removeDuplicateCharacter = deleteDuplicateCharacter(cryptogramList);
         }
         return decryptCharacterToString(cryptogramList);
+    }
+
+    private static boolean deleteDuplicateCharacter(List<String> cryptogramList) {
+        boolean removeDuplicateCharacter = false;
+        for (int i = CHARACTER_START_INDEX; i < cryptogramList.size(); i++) {
+            int startCursor = i - CHARACTER_BEFORE_INDEX;
+            if (cryptogramList.get(startCursor).equals(cryptogramList.get(i))) {
+                removeDuplicateCharacter = true;
+                String target = cryptogramList.get(i);
+                while (cryptogramList.size() > 0 && cryptogramList.get(startCursor).equals(target)) {
+                    cryptogramList.remove(startCursor);
+                }
+                i = startCursor;
+            }
+        }
+        return removeDuplicateCharacter;
     }
 
     private static String decryptCharacterToString(List<String> cryptogramList) {
