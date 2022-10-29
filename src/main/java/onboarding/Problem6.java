b5f62337a584d2e39bf441c6871d6cae449984e9
imsongj <imsongj@gmail.com>
2022-10-29 18:30:39 +0900

    feat(Problem6): 문자열을 두글자 단위로 나눔 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
 import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Problem6 {
+    private static HashMap<String, String> TwoCharacterMap = new HashMap<>();
+
+    public static List<String> splitNameEveryTwoCharacter(String name){
+        List<String> twoCharacterList = new LinkedList<>();
+        for(int i = 0; i < name.length() - 1; i++){
+            String twoCharacters = name.substring(i, i + 2);
+            twoCharacterList.add(twoCharacters);
+        }
+        return twoCharacterList;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         return answer;
     }
 }
