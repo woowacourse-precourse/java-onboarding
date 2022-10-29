9a30202bbb2caab1783240650d8e320af1c5823e
seheonCho <whtpgjs94@gmail.com>
2022-10-30 02:40:52 +0900

    feat: [문제3] 룰을 보관할 정적 변수 추가(박수 조건에 해당하는 문자가 존재하는지 확인하기 위함)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,12 @@
 package onboarding;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem3 {
+
+    private static Set<Character> clapRule = new HashSet<>();
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
