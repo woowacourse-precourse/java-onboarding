cd3738d8278ff77a24eb8d1a9944b9a4c4b5758f
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 05:45:48 +0900

    comment : Problem3 함수 주석 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -17,11 +17,21 @@ public class Problem3 {
 		return answer;
 	}
 
+	/**
+	 * 1. 제한사항 확인
+	 * @param number : 범위를 검색할 숫자
+	 * @throws IllegalStateException : 숫자가 범위 밖일 경우
+	 */
 	public static void isValidNumber(int number) throws IllegalStateException {
 		if (number < 1 || number > 10000)
 			throw new IllegalStateException("number가 범위 밖입니다.");
 	}
 
+	/**
+	 *
+	 * @param number : 박수횟수를 검색할 숫자
+	 * @return 박수 횟수
+	 */
 	public static int countClaps(int number) {
 		return IntStream.range(1, number + 1).map(Problem3::getClapCounts).sum();
 	}
