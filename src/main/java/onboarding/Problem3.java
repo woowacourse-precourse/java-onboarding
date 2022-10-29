27e9af4c4e22254bcba3ec3f43fb18dc3562ac12
gyuturn <rbtjrrbals@gmail.com>
2022-10-29 14:12:42 +0900

    feat: Problem3 - countClap 메서드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+       int ans=0;
+       return ans=0;
+    }
+
+    private static int countClap(int num) {
+        String numToString = String.valueOf(num);
+        int count=0;
+
+        for (int i = 0; i < numToString.length(); i++) {
+            char eachDigit = numToString.charAt(i);
+
+            if(eachDigit=='3'||eachDigit=='6'||eachDigit=='9')
+                count++;
+        }
+        return count;
     }
 }
