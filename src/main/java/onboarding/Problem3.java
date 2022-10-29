82bb31825a8a100a0dbbd0fdd83fa7a2c51675e9
kimminsu <xjfcnfw3@naver.com>
2022-10-29 15:48:46 +0900

    feat(문제3): [예외 사항추가] 입력된 숫자가 1 ~ 10,000사이인지 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,17 @@
 package onboarding;
 
 import onboarding.problem3.Game;
+import onboarding.problem3.Validation;
 
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        answer = Game.playGame(number);
+        try {
+            Validation.validate(number);
+            answer = Game.playGame(number);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return answer;
     }
 }
---
+++ src/main/java/onboarding/problem3/Validation.java
@@ -0,0 +1,17 @@
+package onboarding.problem3;
+
+public class Validation {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 10_000;
+    private static final String RANGE_EXCEPTION_MESSAGE = "숫자는 1부터 10,000사이에 입력되어야 합니다.";
+
+    public static void validate(int number) {
+        validateInRange(number);
+    }
+
+    private static void validateInRange(int number) {
+        if((number < MIN_NUMBER) || (number > MAX_NUMBER)) {
+            throw new IllegalArgumentException(RANGE_EXCEPTION_MESSAGE);
+        }
+    }
+}
