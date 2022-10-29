64904d207d264ff4cdf440c106097b8309f68053
49ehyeon42 <49ehyeon42@gmail.com>
2022-10-29 16:54:01 +0900

    문제7 - 친구 그래프 메소드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,19 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
         // 1. 친구 그래프 작성
+        Map<String, Set<String>> friendGraph = getFriendGraph(friends);
+
         // 2. 사용자 친구 목록 작성
         // 3. 사용자 친구 목록을 통한 사용자와 함께 아는 친구의 수 계산
         // 4. 사용자 친구 목록을 통해 친구를 제외하고 사용자의 타임 라인에 방문한 횟수 계산
@@ -13,5 +21,26 @@ public class Problem7 {
 
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map<String, Set<String>> getFriendGraph(List<List<String>> friends) {
+        Map<String, Set<String>> graph = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String A = friend.get(0);
+            String B = friend.get(1);
+
+            if (!graph.containsKey(A)) {
+                graph.put(A, new HashSet<>());
+            }
+            if (!graph.containsKey(B)) {
+                graph.put(B, new HashSet<>());
+            }
+
+            graph.get(A).add(B);
+            graph.get(B).add(A);
+        }
+
+        return graph;
     }
 }
