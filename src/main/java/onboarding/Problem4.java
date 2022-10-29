3faf836928f98477b8eae6c8ba3703d5f8756840
kimminsu <xjfcnfw3@naver.com>
2022-10-29 16:36:32 +0900

    feat(문제4): [예외 사항추가] 입력 문자열 범위 확인

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,17 @@
 package onboarding;
 
 import onboarding.problem4.Answer;
+import onboarding.problem4.Validation;
 
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        answer = Answer.sayTheOpposite(word);
+        try {
+            Validation.validate(word);
+            answer = Answer.sayTheOpposite(word);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return answer;
     }
 }
---
+++ src/main/java/onboarding/problem4/Validation.java
@@ -0,0 +1,17 @@
+package onboarding.problem4;
+
+public class Validation {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 10_000;
+    private static final String LENGTH_EXCEPTION_MESSAGE = "문자열의 길이는 1 ~ 10,000여야 합니다.";
+
+    public static void validate(String word) {
+        validateInRange(word);
+    }
+
+    private static void validateInRange(String word) {
+        if((word.length() < MIN_LENGTH) || (word.length() > MAX_LENGTH)) {
+            throw new IllegalArgumentException(LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+}
