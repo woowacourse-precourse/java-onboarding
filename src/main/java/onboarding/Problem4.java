6d60e4c287b87ad5988a8b11828d74a1b663ba64
hyejungg <kimhyejung12@naver.com>
2022-10-30 01:49:37 +0900

    feat: [문제 4] 사전 초기화 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
 	private static final String EXCEPTION_MESSAGE = "ERROR";
+	private static Map<Character, Character> dictionary = new HashMap<>();
     public static String solution(String word) {
 		if (!isValidLength(word.length())) {
 			return getExceptionMessage();
 		}
+
+		initDictionary();
+
         return answer;
     }
 
@@ -15,6 +22,13 @@ public class Problem4 {
 
 	private static String getExceptionMessage() {
 		return EXCEPTION_MESSAGE;
+	}
+
+	private static void initDictionary() {
+		for (int i = 0; i < 26; i++) {
+			dictionary.put((char)('A' + i), (char)('A' + 25 - i));
+			dictionary.put((char)('a' + i), (char)('a' + 25 - i));
+		}
 	}
 
 }
