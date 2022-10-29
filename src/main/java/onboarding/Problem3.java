3086beffc3426ae7727c3c0bd3ed2789e81589dd
skfkgla <narahim.lee@gmail.com>
2022-10-30 00:26:25 +0900

    feat: Problem3 - 3, 6, 9 가 몇개 있는지 찾는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,20 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * @param number : 0보다 큰 정수
+     * @return 3, 6, 9의 총 개수
+     */
+    private static int checkNumber(int number) {
+        int count = 0;
+
+        while(number != 0){
+            if(number % 10 == 3 || number % 10 == 6 || number % 10 == 9){
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
 }
