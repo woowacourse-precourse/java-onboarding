b83d5a3ae063b77b93b47ba3de44449b75406d38
siwookim97 <tldn12gh@gmail.com>
2022-10-30 05:17:03 +0900

    feat : problem2) 리스트를 문자열로 변환하는 기능 추가
    List를 String으로 변환하는 기능
    list를 입력받아 String으로 변환한다
    변환한 String을 반환한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -21,6 +21,22 @@ public class Problem2 {
     }
 
     /**
+     * List를 String으로 변환하는 메서드
+     *
+     * @param inputList String으로 바꿀 List ,static 변수 list가 들어간다
+     * @return String으로 바꾼 List
+     */
+    static String transListToString(List<Character> inputList) {
+        String str = "";
+
+        for (int i = 0; i < inputList.size(); i++) {
+            str += inputList.get(i);
+        }
+
+        return str;
+    }
+
+    /**
      * 연속적으로 중복되는 문자을 삭제하는 메서드
      *
      * @return 문자가 삭제 되었으면 true, 아니라면 false
