acbf9e9cee6ca2b1ac9b481881b43c3f94e7cdd9
HyunseokLee99 <hyun104437@gmail.com>
2022-10-30 02:40:29 +0900

    feature: 2-4

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        String txt = cryptogram;
+
+        for (int i = 1; i < txt.length(); i++) {
+            if(txt.charAt(i - 1) != txt.charAt(i)) continue;
+
+            txt = txt.substring(0, i - 1) + txt.substring(i + 1);
+            i = i - 2;
+            System.out.println(txt);
+        }
+
+        answer = txt;
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int count = 0;
+        int tmp = 0;
+
+        for (int i = 1; i <= number; i++) {
+            tmp = i;
+            while (tmp > 0) {
+                if (tmp % 10 == 3 ||
+                        tmp % 10 == 6 ||
+                        tmp % 10 == 9) {
+                    count++;
+                }
+                tmp /= 10;
+            }
+        }
+        System.out.println(count);
+        answer = count;
+
         return answer;
     }
 }
