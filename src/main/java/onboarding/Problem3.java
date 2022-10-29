d0945db90d49ed719b7e8eb284333a97db11843b
hoonn9 <hun9935@gmail.com>
2022-10-29 16:56:23 +0900

    Add number to digit list method (Problem3)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            for (int num: toDigitList(i)) {
+                System.out.println(num);
+            }
+        }
+
         return answer;
+    }
+
+    private static List<Integer> toDigitList(Integer number) {
+        List<Integer> result = new ArrayList<>();
+
+        while (number != 0) {
+            result.add(number % 10);
+            number /= 10;
+        }
+
+        return result;
     }
 }
