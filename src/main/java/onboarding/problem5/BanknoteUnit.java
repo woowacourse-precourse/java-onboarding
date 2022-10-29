ac552bd85e3f9225c62d7384218f6e0d1a057dcc
alstjr706 <alstjr706@naver.com>
2022-10-29 17:43:04 +0900

    feat : (은행 출금 문제) 잔돈 리스트 구현

--- src/main/java/onboarding/problem5/BanknoteUnit.java
+++ src/main/java/onboarding/problem5/BanknoteUnit.java
@@ -6,7 +6,9 @@ public class BanknoteUnit {
 
     public List<Integer> CoinList() {
 
-        return null;
+        List<Integer> coinList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        return coinList;
     }
 
 }
