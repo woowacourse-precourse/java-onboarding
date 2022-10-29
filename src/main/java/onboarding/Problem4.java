e7fef18aa9cd196844a098ebc903cb71c37966cc
Brian <bwook9908@gmail.com>
2022-10-29 14:01:30 +0900

    [문제4] test 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,38 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        return revertArrayToString(convertArray(changeStringToAsciiArray(word)));
+
     }
+
+    static int[] changeStringToAsciiArray(String text){
+        int[] arr = new int[text.length()];
+        for(int i=0; i<text.length(); i++){
+            arr[i] = (int) text.toCharArray()[i];
+        }
+        return arr;
+    }
+
+    static int[] convertArray(int[] arr){
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==32){
+                arr[i]=32;
+            }else if(arr[i]>=65 && arr[i]<=90){
+                arr[i]=155-arr[i];
+            }else if(arr[i]>=97 && arr[i]<=122){
+                arr[i]=219-arr[i];
+            }
+        }
+        return arr;
+    }
+    static String revertArrayToString(int [] arr){
+        char[] charr = new char[arr.length];
+        for(int i=0 ; i<arr.length; i++){
+            charr[i]= (char) arr[i];
+        }
+        return new String(charr);
+    }
+
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,19 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    static int[] quotientAndRemainder(int number, int with){
+        int[] array= new int[2];
+        int quotient = number / with;
+        int remainder= number % with;
+        array[0]= quotient;
+        array[1]= remainder;
+
+        return array;
+    }
+
 }
