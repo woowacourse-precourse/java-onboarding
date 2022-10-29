349b92781087fd1436a81524b48913d8b40b56f8
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-29 23:57:17 +0900

    refactor: 5번 문제 리팩토링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,24 +2,31 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        List<Integer> money_unit = new ArrayList<>(
-            Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));// 돈 단위별로 list에 넣어놓기
+        List<Integer> money_unit = setMonetaryUnit();
 
         for (int i = 0; i < money_unit.size(); i++) { //반복문 진행시 각 돈 단위에 따라 처리
-
-            int unit = money_unit.get(i);
-            answer.add(money / unit);
-            money = money % unit;
-
+            money = classifyMoney(money, answer, money_unit, i);
         }
 
         return answer;
+    }
+
+    private static int classifyMoney(int money, List<Integer> answer, List<Integer> money_unit, int i) {
+        int unit = money_unit.get(i);
+        answer.add(money / unit);
+        money = money % unit;
+        return money;
+    }
+
+    private static List<Integer> setMonetaryUnit() {
+        List<Integer> money_unit = new ArrayList<>(
+            Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));// 돈 단위별로 list에 넣어놓기
+        return money_unit;
     }
 }
