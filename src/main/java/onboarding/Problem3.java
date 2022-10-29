0f0b80197edd40d633a8b62a1466f09992dbf160
jw0293 <k38836919@gmail.com>
2022-10-29 15:23:09 +0900

    feature : Count 369 Clap [3,6,9 개수 세는 기능]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,9 +6,40 @@ class IntegerToStringArray{
     }
 }
 
+class CountClapGame{
+    IntegerToStringArray integerToStringArray = new IntegerToStringArray();
+
+    public int countInArrayClap(int number){
+        int count = 0;
+        String[] array = integerToStringArray.toStringArray(number);
+        for (String st : array) {
+            if(st.equals("3") || st.equals("6") || st.equals("9")){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countClap(int number){
+        int cnt = 0;
+        for(int i=1;i<=number;i++){
+            if(i < 10){
+                if(i == 3 || i == 6 || i == 9){
+                    cnt++;`
+                }
+            } else{
+                cnt += countInArrayClap(i);
+            }
+        }
+        return cnt;
+    }
+}
+
 public class Problem3 {
+
+    private final static CountClapGame countClapGame = new CountClapGame();
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return countClapGame.countClap(number);
     }
 }
