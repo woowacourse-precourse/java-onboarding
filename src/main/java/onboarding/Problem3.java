d627e7eae0753e3a4da1dd16f57ac1c63c3e9ed3
khj <bbb6254177@gmail.com>
2022-10-30 01:02:08 +0900

    initial commit

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem3 {
+    private static int count_369(int num){
+        int count = 0;
+        while(num >= 1){
+            if (num%10 == 3 || num%10 == 6 || num%10 == 9){
+                count += 1;
+            }
+            num /= 10;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        for(int i = 1; i<=number; i++){
+            count += count_369(i);
+        }
+
+        return count;
     }
 }
