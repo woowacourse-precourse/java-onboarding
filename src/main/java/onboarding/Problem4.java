1d7bf7031a0a0a502a1f46484e6d21b768c32cba
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-10-29 12:13:25 +0900

    problem4 function implemented

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,29 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (!Character.isLetter(c)){
+                answer += c;
+                continue;
+            }
+            answer += treeFrog(c);
+        }
+
         return answer;
+    }
+    public static char treeFrog(char c){
+        int a = 'a';
+        int A = 'A';
+        int z = 'z';
+        int Z = 'Z';
+
+        if (Character.isLowerCase(c)){
+            return (char) (z+a-c);
+        }
+
+        return (char) (Z+A-c);
     }
 }
