7e73e694984a8089f8bd543371b642cc8588765b
hyejungg <kimhyejung12@naver.com>
2022-10-30 00:09:32 +0900

    feat: [문제 2] 문자열 해독 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 public class Problem2 {
+	private static final String EXCEPTION_MESSAGE = "NOT LOWER CASE";
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
@@ -19,4 +21,29 @@ public class Problem2 {
 		return cryptogram.length() <= 1000;
 	}
 
+	private static String getExceptionMessage() {
+		return EXCEPTION_MESSAGE;
+	}
+
+	private static boolean isDecoded(String cryptogram) {
+		for (int i = 0; i < cryptogram.length() - 1; i++) {
+			if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static String decodeString(String cryptogram) {
+		if (isDecoded(cryptogram)) {
+			return cryptogram;
+		}
+		for (int i = 0; i < cryptogram.length() - 1; i++) {
+			if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+				String next = cryptogram.replaceAll("(([a-z])\\2{1,})", "");
+				return decodeString(next);
+			}
+		}
+		return getExceptionMessage();
+	}
 }
