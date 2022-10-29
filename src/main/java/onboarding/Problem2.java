6c22b0aff3dee42fbbbce1f32d5ba7f83cc92195
JUN <chltmdwns96@naver.com>
2022-10-29 13:22:55 +0900

    feature : 문자열 -> 문자 리스트로 분리(StringToChars) 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
+    }
+
+    private static List<Character> StringToChars(String text){
+        List<Character> seperatedCryptogram = new ArrayList<>();
+        for(int i = 0; i < text.length(); i++){
+            seperatedCryptogram.add(text.charAt(i));
+        }
+        return seperatedCryptogram;
     }
 }
