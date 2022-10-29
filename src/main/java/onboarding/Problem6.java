4cf074684309d5a753657f098e8cff6be2b9e125
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-29 20:31:55 +0900

    feat: Problem6 - 유저의 이름을 하나로 합치는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,15 +10,11 @@ import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        HashMap<String, Integer> userTwoLetterList = new HashMap<>();
         Set<String> DuplicateNameList = new HashSet<>();
         List<String> answer = new ArrayList<>();
 
         List<String> userNameList = createUserNameList(forms);
-
-        for (List<String> form : forms) {
-            makeTwoLetterList(form.get(1), userTwoLetterList);
-        }
+        String combinedNames = combineName(userNameList);
         for (Map.Entry<String, Integer> entry : userTwoLetterList.entrySet()) {
             makeDuplicateNameList(DuplicateNameList, entry);
         }
@@ -37,13 +33,14 @@ public class Problem6 {
         return nameList;
     }
 
-    public static void makeTwoLetterList(String nickName, HashMap<String, Integer> twoLetterList) {
-        String[] splitNickName = nickName.split("");
-        for (int i = 0; i < splitNickName.length-1; i++) {
-            String userTwoLetter = splitNickName[i]+splitNickName[i+1];
-            twoLetterList.put(userTwoLetter, twoLetterList.getOrDefault(userTwoLetter, 0) + 1);
+    public static String combineName(List<String> nameList) {
+        StringBuilder joinName = new StringBuilder();
+        for (String s : nameList) {
+            joinName.append(s).append(",");
         }
+        return joinName.toString();
     }
+
     public static void makeDuplicateNameList(Set<String> duplicateList, Map.Entry<String, Integer> entry) {
         if (entry.getValue() > 1) {
             duplicateList.add(entry.getKey());
