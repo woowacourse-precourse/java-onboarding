bfb1959ef10c11e6edc2a032ff1f60c944652b4f
heejeong <hjx66@naver.com>
2022-10-29 16:39:47 +0900

    feat: 문제 2 중복 문자 검사 & replace - 인접 문자가 중복인지 검사 후 빈 문자열로 replace

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int i = 0;
+        while(i < cryptogram.length()-1){
+            if(cryptogram.charAt(i) != cryptogram.charAt(i+1)) {
+                i++;
+                continue;
+            }
+            cryptogram = cryptogram.replace(cryptogram.substring(i, i + 2), "");
+            i--;
+        }
+        return cryptogram;
     }
+
 }
