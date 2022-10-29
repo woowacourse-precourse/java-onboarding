d4f2a08520b177a0419df0d4aa5d731d62f4e70c
flydog98 <guinnessoverflow@gmail.com>
2022-10-29 14:23:23 +0900

    feat: sort name by ascending direction

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -13,8 +13,6 @@ public class Problem7 {
         sns.calculateFriendsScore();
         sns.calculateVisitorScore(visitors);
 
-        sns.print();
-
         return sns.getRecommendFriendList();
     }
 }
@@ -96,6 +94,9 @@ class SNS {
         candidate_entry.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if(o2.getValue().equals(o1.getValue())) {
+                    return o1.getKey().compareTo(o2.getKey());
+                }
                 return o2.getValue().compareTo(o1.getValue());
             }
         });
