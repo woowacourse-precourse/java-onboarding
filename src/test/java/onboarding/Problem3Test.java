cc27084911143f1c0517d0979c56c47506144930
Invidam <hansupark0208@gmail.com>
2022-10-29 17:17:29 +0900

    test(Problem3): add clap count test method

---
+++ src/test/java/onboarding/Problem3Test.java
@@ -0,0 +1,28 @@
+package onboarding;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.InputMismatchException;
+import org.junit.jupiter.api.Test;
+
+class Problem3Test {
+
+    @Test
+    void 손뼉여부테스트() {
+
+        //given
+        int num1 = 3;
+        int num2 = 93;
+        int num3 = 1;
+        int num4 = 611;
+
+        //when
+
+        //then
+        assertThat(Problem3.getClapCount(num1)).isEqualTo(1);
+        assertThat(Problem3.getClapCount(num2)).isEqualTo(2);
+        assertThat(Problem3.getClapCount(num3)).isEqualTo(0);
+        assertThat(Problem3.getClapCount(num4)).isEqualTo(1);
+    }
+}
