3c6a579cd06568f376ea8ad140502900e0ac38f9
kimdeokyoung <dudejrss@naver.com>
2022-10-29 14:23:32 +0900

    src/main/java/onboarding/Problem1.java : make structure Problem 1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private int findGameValue(List<Integer> ls){
+        return 0;
+    }
+
+    private int sumOfDigits(int v){
+        return 0;
+    }
+
+    private int productOfDigits(int v){
+        return  0;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
