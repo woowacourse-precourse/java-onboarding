ac2ce458d32a61df770b846040b534e49d08cc47
heohyeonjink <hyeonjin4870@naver.com>
2022-10-29 23:36:30 +0900

    feat(PROBLEM2) : 중복 문자 제거하기
    stack을 활용하여 중복 문자를 제거한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
-
+import java.util.*;
+import java.io.*;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String removeDuplication(String input){
+        Stack<Character> stack = new Stack<>();
+        stack.add('0');
+
+        boolean duple = false;
+        for(char c : input.toCharArray()){
+            if(c==stack.peek()) duple = true; // 같은 문자 checking
+            else{
+                if(duple){
+                    stack.pop();
+                    duple = false;
+                }
+                stack.push(c);
+            }
+        }
+
+        if(duple) stack.pop();
+        StringBuffer sb = new StringBuffer();
+        for(char c : stack){
+            sb.append(c);
+        }
+        return sb.toString().substring(1);
     }
 }
