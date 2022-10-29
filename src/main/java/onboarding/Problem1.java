3863ab7a53677ff73e2f5f74c5710b5d86ccaba2
can019 <jys01012@gmail.com>
2022-10-29 19:16:27 +0900

    :sparkles: 한 페이지 점수 계산 기능 작성 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,28 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    private static int caculateSingePageScore(int number){
+        int[] tokenizedNumber = tokenize(Integer.toString(number));
+
+        // init
+        int product = tokenizedNumber[0];
+        int plus = tokenizedNumber[0];
+
+        // 모든 자리수를 더한 값과 곱한 값을 계산
+        for(int i = 1; i < tokenizedNumber.length; i++){
+            product = product * tokenizedNumber[i];
+            plus = plus + tokenizedNumber[i];
+        }
+
+        // 더 큰 값을 반환
+        if(product > plus)
+            return product;
+        else
+            return plus;
     }
 
     private static int[] tokenize(String number){
