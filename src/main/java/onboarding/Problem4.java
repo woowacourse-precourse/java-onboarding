b2e24638e8114f6b701c1bb1df0f485d4a79a634
slbin-park <smpts00@naver.com>
2022-10-29 20:26:17 +0900

    feat : 알파벳을 변환하는 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,21 @@ import java.util.HashMap;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        HashMap<String,String> words = changeAlpha();
         return answer;
     }
-
+    public static HashMap<String,String> changeAlpha(){
+        HashMap<String,String> words = new HashMap<String,String>();
+        int ascii = 90;
+        for(int i=65;i<=90;i++){
+            words.put(String.valueOf((char)i),String.valueOf((char)ascii));
+            ascii-=1;
+        }
+        ascii = 122;
+        for(int i=97;i<=122;i++){
+            words.put(String.valueOf((char)i),String.valueOf((char)ascii));
+            ascii-=1;
+        }
+        return words;
+    }
 }
