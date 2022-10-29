278361067b010710b3e42fdd8913f3a8f14387b3
Sojeong <k5k5s5s5j5j5@naver.com>
2022-10-29 22:07:51 +0900

    problem4: complete with comments

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        char[] wordArr = word.toCharArray();
+        char[] changedArr = new char[wordArr.length];
+        int ascii;
+        int changedAscii;
+
+        //ascii로 계산해서 변환. 알파벳이 아니면 변환 x
+        int i = 0;
+        for (Character cha : wordArr) {
+            ascii = (int) cha;
+
+            if (Character.isUpperCase(cha))
+                changedAscii = 155 - ascii;
+            else if (Character.isLowerCase(cha))
+                changedAscii = 219 - ascii;
+            else
+                changedAscii = ascii;
+
+            char c = (char) changedAscii;
+            changedArr[i] = c;
+            i++;
+        }
+        String answer = new String(changedArr);
         return answer;
     }
 }
