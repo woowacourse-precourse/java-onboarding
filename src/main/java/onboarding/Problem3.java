2a67444511b8e186dc9efb00b0dd284cf432224e
chanwonlee <clw8679@naver.com>
2022-10-29 22:23:33 +0900

    FEAT: [Problem3] 3,6,9 개수 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,17 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int count369(int number) {
+        int count = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            //각 자리수가 3,6,9(3의 배수)인지 확인해서 카운트
+            if (digit % 3 == 0 && digit != 0) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
 }
