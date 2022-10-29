318c5a3491582e692b26ea3e3a02a539f1dde6f7
seungmin-park <tmddudals369@naver.com>
2022-10-30 02:56:47 +0900

    페이지 별 점수 구하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,4 +17,23 @@ class Problem1 {
         if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) return false;
         return true;
     }
+
+    private static int getScore(int page){
+        int sum = 0;
+        int temp = page;
+        int pageSize = (int) Math.log10(page) + 1;
+        for (int i = 0; i < pageSize; i++){
+            sum += temp%10;
+            temp /= 10;
+        }
+
+        int duplicate = 1;
+        temp = page;
+        for (int i = 0; i < pageSize; i++){
+            duplicate *= temp%10;
+            temp /= 10;
+        }
+
+        return Math.max(sum, duplicate);
+    }
 }
