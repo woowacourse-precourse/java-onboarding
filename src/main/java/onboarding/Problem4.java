4485323442ce2400124da9495e6a447d3c411182
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2022-10-29 21:10:14 +0900

    1. 소문자 대문자 문자 판별 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+/**
+ * 큰 틀의 기능
+ * - 아스키 코드 개념
+ * 1. 소문자 대문자 문자 판별 함수
+ * 2. 소문자 알파벳 변환기능
+ * 3. 대문자 알파벳 변환기능
+ * 4. 알파벳 합치는 기능
+ * 5. 전체 기능을 실핼 시켜주는 기능
+ */
+
+/**
+ * 1. 소문자 대문자 문자 판별 함수
+ */
+
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private static String distinguishInput(char inputChar){
+        if(inputChar >= 97 && inputChar <= 122)
+            return "lower";
+        if(inputChar >= 65 && inputChar <= 90)
+            return "upper";
+        return "nothing";
     }
 }
