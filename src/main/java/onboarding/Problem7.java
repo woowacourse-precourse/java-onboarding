c11cd87cb470a81b25c830a9ba8749171bb28b98
davejang <davejang98@naver.com>
2022-10-29 18:29:53 +0900

    feat: Problem7 setFriendList method
    친구 관계 정보를 세팅하는 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
+    public static HashMap<String, HashSet<String>> friendListInfo;
+    public static void setFriendList(List<List<String>> friends) {
+        for(int index = 0; index < friends.size(); index ++) {
+            String person1 = friends.get(index).get(0);
+            String person2 = friends.get(index).get(1);
+
+            HashSet<String> person1FriendList = friendListInfo.get(person1);
+            HashSet<String> person2FriendList = friendListInfo.get(person2);
+
+            person1FriendList.add(person2);
+            person2FriendList.add(person1);
+
+            friendListInfo.put(person1,person1FriendList);
+            friendListInfo.put(person2,person2FriendList);
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
