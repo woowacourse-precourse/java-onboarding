db8df480b59586988412e113a1283a29a97cf9d9
jh-jin <devjin0122@gmail.com>
2022-10-29 17:44:40 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int dupIdx;
+
+        while (true) {
+            dupIdx = dupIndex(cryptogram);
+            if (dupIdx == -1)
+                break;
+            cryptogram = deleteDup(cryptogram, dupIdx);
+        }
+        return cryptogram;
     }
+    private static int dupIndex(String str) {
+        for(int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i+1))
+                return i;
+        }
+        return -1;
+    }
+
+    private static String deleteDup(String str, int dupIdx) {
+        return str.substring(0,dupIdx) + str.substring(dupIdx+2);
+    }
+
 }
