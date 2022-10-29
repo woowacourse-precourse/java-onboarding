a75c8712efccaf55dcce7496112d2cc8e6d9d211
tlsqj <rnjstlsqja@inha.edu>
2022-10-29 15:54:48 +0900

    main함수 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,10 +8,10 @@ public class Problem2 {
             String nextCryptogram = "";
             char current = cryptogram.charAt(0);
             int sameCount = 1;
-            for(int i = 1; i < cryptogram.length() ; i++) {
-                if(cryptogram.charAt(i) != current) {
+            for(int cryptoIterator = 1; cryptoIterator < cryptogram.length() ; cryptoIterator++) {
+                if(cryptogram.charAt(cryptoIterator) != current) {
                     if(sameCount == 1) nextCryptogram += current;
-                    current = cryptogram.charAt(i);
+                    current = cryptogram.charAt(cryptoIterator);
                     sameCount = 1;
                 }
                 else {
@@ -23,9 +23,5 @@ public class Problem2 {
             cryptogram = nextCryptogram;
         }
         return cryptogram;
-    }
-
-    public static void main(String args[]) {
-        System.out.println(solution("browoanoommnaon"));
     }
 }
