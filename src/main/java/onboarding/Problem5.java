3aaf4e89ce3a67e5dc4a99f6f02847675335529a
KTH-96 <taehyun960@gmail.com>
2022-10-29 16:22:46 +0900

    comment(Problem5): 기능구현 목록 정리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+
+	/**
+	 * 기능 목록 작성
+	 * 1. 주어진 값을 화폐단위로 몇개씩 사용되는지 확인하는 기능
+	 */
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = Collections.emptyList();
+		return answer;
+	}
 }
