e72692862d6be75f11219bd25caffcae97eba4aa
iamsseaa <homingsea@gmail.com>
2022-10-30 01:47:30 +0900

    feat(Problem4): 문자가 영어 알파벳인지 확인하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,9 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        char[] wordCharArray = word.toCharArray();
+
+        List<Character> answer = new ArrayList<>();
+
+        for (char one : wordCharArray) {
+
+            if (!Pattern.matches("^[a-zA-Z]*$", Character.toString(one))) {
+                answer.add(one);
+                continue;
+            }
+        }
+
+
+        String result = answer.stream()
+                .map(e -> e.toString())
+                .reduce((acc, e) -> acc + e)
+                .get();
+
+        return result;
     }
 
     public static char convertUppercase(char one) {
