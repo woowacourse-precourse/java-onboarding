c45f0d754f2e5aed092ee17f82cf24e74fa09b85
jiwon <ljwon77@daum.net>
2022-10-29 21:09:03 +0900

    refactor(problem3): change variable name

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,9 +7,9 @@ public class Problem3 {
         int answer = 0;
 
         // 1부터 입력으로 주어진 번호까지 순회를 진행한다.
-        for (int i = 1; i <= number; i++) {
+        for (int num = 1; num <= number; num++) {
             // 각 번호를 문자열로 변경하고, 원본 길이를 구한다.
-            String strNum = String.valueOf(i);
+            String strNum = String.valueOf(num);
             int originLen = strNum.length();
         }
         return answer;
