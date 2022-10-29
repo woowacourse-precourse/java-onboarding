ab631b44b9259855872e8b4cfbdfe87a9c8fd934
chkim <cndghks0213@gmail.com>
2022-10-30 07:13:54 +0900

    feat(problem4): 문자가 주어졌을 때 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static char convert(char input) {
+        if (!Character.isAlphabetic(input)) {
+            return input;
+        }
+
+        if (Character.isLowerCase(input)) {
+            return (char)('a' + 'z' - input);
+        }
+
+        return (char)('A' + 'Z' - input);
+    }
 }
