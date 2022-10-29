809567b8a30393a9a33dc8fa7c3c45be29ffe625
kimjaehwi1 <meatandcoffee@naver.com>
2022-10-30 00:52:59 +0900

    4번문제 청개구리 문제 [최종본]

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        word = flog(word);
+
+        return word;
+    }
+
+    public static String flog(String word) { // 매개변수를 반대로 변환하는 메소드
+        char[] arr = word.toCharArray();
+
+        for(int i=0;i<arr.length;i++) {
+            if((65<=arr[i])&&(arr[i]<=90)) {
+                arr[i] = (char)(arr[i]-'A');
+                arr[i] = (char) ('Z'-arr[i]);
+                continue;
+            }
+            if(97<=arr[i]) {
+                arr[i] = (char)(arr[i]-'a');
+                arr[i] = (char)('z'-arr[i]);
+            }
+        }
+        return new String(arr);
     }
 }
