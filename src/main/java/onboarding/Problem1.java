db2cd745cff7f3a567b21cd794ad8fa4dadbc241
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-10-29 17:36:39 +0900

    페이지 번호의 각 자리 숫자 합과 곱 중 가장 큰것을 구하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
@@ -15,6 +16,35 @@ class Problem1 {
     // 두개를 비교해서 큰 숫자 구하기
     // 점수 비교 승자 나타내기
     // 예외사항
+
+    public static int getMaxPageNum(Integer integer){
+        List<Integer> digit = new ArrayList<>();
+        if(integer<10){
+            return integer;
+        }else if(integer< 100){
+            digit.add(integer%10);
+            digit.add(integer/10);
+            //덧셈 곱셈 중 큰 것 가져오기
+            return getMaxAddMul(digit);
+        }else{
+            digit.add(integer/100);
+            digit.add(integer%10);
+            digit.add((integer-(digit.get(0)*100))/10);
+            return getMaxAddMul(digit);
+        }
+    }
+
+    //덧셈 곱셈 큰 것 가져오기
+    public static int getMaxAddMul(List<Integer> list){
+        int addv = 0;
+        int mulv = 1;
+        for (Integer integer : list) {
+            addv+=integer;
+            mulv*=integer;
+        }
+        return (addv<=mulv) ? mulv : addv;
+    }
+
 
     public static Boolean exception(List<Integer> pobi, List<Integer> crong){
         if(pobi.get(1)- pobi.get(0)==1 && crong.get(1)- crong.get(0)==1){
