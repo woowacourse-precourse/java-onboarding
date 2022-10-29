b430b67790d50e70f6e70316bdb9ceb8e236ffb8
Donghyun <flaehan@gmail.com>
2022-10-29 22:09:06 +0900

    한 가지 경우 예외로 if 삽입해서 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,16 +2,13 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";   // String answer ="answer"인데 임의로 변경
+        String answer = "answer";
 
         if (cryptogram == null || cryptogram.length() == 0){
             return cryptogram;
         }
-
         char [] chars = cryptogram.toCharArray();
-
         int i,k = 0;
-
         for (i = 1; i < chars.length; i++){
             if(chars[i-1] != chars[i]){
                 chars[k++] = chars[i-1];
@@ -21,9 +18,11 @@ public class Problem2 {
                 }
             }
         }
-
-        chars [k++] = chars [i-1];
-
+        if (i<chars.length+1) {
+            chars[k++] = chars[i - 1];
+        }else{
+            chars[k] = chars[i-2];
+        }
         String s = new String(chars).substring(0,k);
 
         if (k != chars.length){
