ec7638515debd729348d0c257b06880f1dbc090f
ksh-beginner <ohk9134@naver.com>
2022-10-29 10:55:48 +0900

    feat : 크루 폼에서 이메일, 닉네임을 추출하여 크루 인포 맵에 추가하는 기능 추가
    파라미터로 주어진 크루 폼을 받아서 크루 폼을 돌면서
    해당 크루의 이메일, 닉네임을 크루 인포 맵에 추가하는 메소드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,12 +6,19 @@ import java.util.Map;
 
 public class Problem6 {
 
+    private static final Map<String, String> crewInfoMap = new HashMap<>();
+
     public static List<String> solution(List<List<String>> crewInfoForms) {
         List<String> answer = List.of("answer");
         return answer;
     }
 
-
-
-
+    public void initCrewInfoMap(List<List<String>> crewForms) {
+        crewForms.stream()
+                .forEach(crewInfo -> {
+                    String crewEmail = crewInfo.get(0);
+                    String crewNickname = crewInfo.get(1);
+                    crewInfoMap.put(crewEmail, crewNickname);
+                });
+    }
 }
