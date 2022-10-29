ed7edae77465f99f38a1ca07718d70d2fea496d0
jkde7721 <jkde7721@naver.com>
2022-10-29 10:18:12 +0900

    [문제 7] 친구 추천 점수 계산하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,14 +4,17 @@ import java.util.*;
 
 public class Problem7 {
     private static Map<String, List<String >> friendAdjacencyList = new HashMap<>();
+    private static Map<String, Integer> friendRecommendationScore = new HashMap<>();
+    private static List<String> friendRecommendationResult = new ArrayList<>();
+    private static final int SCORE_BY_FRIENDS = 10;
+    private static final int SCORE_BY_VISITORS = 1;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         convertToFriendAdjacencyList(friends);
-
-        
-
-        List<String> answer = Collections.emptyList();
-        return answer;
+        calculateFriendRecommendationScoreBy(user);
+        calculateFriendRecommendationScoreBy(visitors);
+        makeFriendRecommendationResult(user);
+        return friendRecommendationResult;
     }
 
     private static void convertToFriendAdjacencyList(List<List<String>> friends) {
@@ -23,7 +26,40 @@ public class Problem7 {
     }
 
     private static void linkWithFriend(String me, String friend) {
-        friendAdjacencyList.computeIfAbsent(me, k -> new ArrayList<>());
+        friendAdjacencyList.putIfAbsent(me, new ArrayList<>());
         friendAdjacencyList.get(me).add(friend);
+    }
+
+    private static void calculateFriendRecommendationScoreBy(String user) {
+        for (String anotherUser : friendAdjacencyList.keySet()) {
+            if (Objects.equals(anotherUser, user)) continue;
+            int numberOfOverlapFriends = getNumberOfOverlapFriends(anotherUser, user);
+            plusFriendRecommendationScore(anotherUser, SCORE_BY_FRIENDS * numberOfOverlapFriends);
+        }
+    }
+
+    private static void calculateFriendRecommendationScoreBy(List<String> visitors) {
+        for (String visitor : visitors) {
+            plusFriendRecommendationScore(visitor, SCORE_BY_VISITORS);
+        }
+    }
+
+    private static int getNumberOfOverlapFriends(String me, String user) {
+        int number = 0;
+        List<String> friendsOfUser = friendAdjacencyList.get(user);
+
+        for (String friend : friendsOfUser) {
+            if (friendAdjacencyList.get(me).contains(friend)) number++;
+        }
+        return number;
+    }
+
+    private static void plusFriendRecommendationScore(String friend, int plusScore) {
+        friendRecommendationScore.putIfAbsent(friend, 0);
+        int updatedScore = friendRecommendationScore.get(friend) + plusScore;
+        friendRecommendationScore.replace(friend, updatedScore);
+    }
+
+    private static void makeFriendRecommendationResult(String user) {
     }
 }
