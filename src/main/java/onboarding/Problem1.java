f83ff524770448f23348edc55be4bb8b1edeb8cd
can019 <jys01012@gmail.com>
2022-10-29 18:52:20 +0900

    :sparkles: String 토크나이즈 method 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,15 @@
 package onboarding;
-
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static String[] tokenize(String number){
+        // @TODO :: String 토크나이즈 후 반환
+        String[] strArray = number.split("");
+       return strArray;
     }
 }
