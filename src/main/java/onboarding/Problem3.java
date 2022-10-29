2851a0b14d8872d04f61698af6fde7548c6a6533
채우석 <wochae@wochae-MacBookAir.local>
2022-10-30 01:29:55 +0900

    [feat] 3.2 숫자가 3 미만일 때 0으로 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,17 +1,26 @@
 package onboarding;
 
 public class Problem3 {
-    public static int[] notation;
+    public static int[] tqqq;
     public static int solution(int number) {
         int answer = 0;
 
-        notation = new int[10000];
-        notation[0] = 0;
-        notation[1] = 0;
-        notation[2] = 0;
+        tqqq = new int[10000];
+        tqqq[0] = 0;
+        tqqq[1] = 0;
+        tqqq[2] = 0;
 
         answer = increasingCrapCount(number);
 
         return answer;
+    }
+
+    private static int increasingCrapCount(int number) {
+        String idxToStr = "";
+
+        for (int idx = 3; idx < number; idx++) {
+            
+        }
+        return tqqq[number];
     }
 }
