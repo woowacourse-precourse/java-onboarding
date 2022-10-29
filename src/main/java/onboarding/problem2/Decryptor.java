534c12200ae1596d46febfc817a3c8616b8cec2f
apptie <appti@naver.com>
2022-10-29 11:34:34 +0900

    refactor: 문제 2 중복된 문자열을 삭제하는 deleteDuplicateCharacter 메소드 분리 및 메소드 이름 변경

--- src/main/java/onboarding/problem2/Decryptor.java
+++ src/main/java/onboarding/problem2/Decryptor.java
@@ -5,7 +5,6 @@ import static onboarding.problem2.consts.CryptogramIndexConst.CHARACTER_START_IN
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class Decryptor {
@@ -18,25 +17,29 @@ public class Decryptor {
         boolean removeDuplicateCharacter = true;
 
         while (removeDuplicateCharacter) {
-            removeDuplicateCharacter = deleteDuplicateCharacter(cryptogramList);
+            removeDuplicateCharacter = findAndDeleteDuplicateCharacter(cryptogramList);
         }
         return decryptCharacterToString(cryptogramList);
     }
 
-    private static boolean deleteDuplicateCharacter(List<String> cryptogramList) {
+    private static boolean findAndDeleteDuplicateCharacter(List<String> cryptogramList) {
         boolean removeDuplicateCharacter = false;
         for (int i = CHARACTER_START_INDEX; i < cryptogramList.size(); i++) {
             int startCursor = i - CHARACTER_BEFORE_INDEX;
-            if (cryptogramList.get(startCursor).equals(cryptogramList.get(i))) {
+            String nowCharacter = cryptogramList.get(i);
+            if (cryptogramList.get(startCursor).equals(nowCharacter)) {
                 removeDuplicateCharacter = true;
-                String target = cryptogramList.get(i);
-                while (cryptogramList.size() > 0 && cryptogramList.get(startCursor).equals(target)) {
-                    cryptogramList.remove(startCursor);
-                }
+                deleteDuplicateCharacter(cryptogramList, nowCharacter, startCursor);
                 i = startCursor;
             }
         }
         return removeDuplicateCharacter;
+    }
+
+    private static void deleteDuplicateCharacter(List<String> cryptogramList, String nowCharacter, int startCursor) {
+        while (cryptogramList.size() > 0 && cryptogramList.get(startCursor).equals(nowCharacter)) {
+            cryptogramList.remove(startCursor);
+        }
     }
 
     private static String decryptCharacterToString(List<String> cryptogramList) {
