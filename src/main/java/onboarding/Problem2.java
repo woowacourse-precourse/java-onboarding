eb9aa6d2dfbdf33c9826c26f25c20fc8665540fb
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-29 23:09:57 +0900

    feat:2번 문제 재확인 후 로직 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,15 +8,50 @@ public class Problem2 {
             cryptogram); //delete를 사용하여 중복 문자열을 제거하기 위하여 StringBuffer 사용
 
         int index = 0;
+        int flag=0; // 0 이면 중복 걸러내기 한번이라도 해서 다시 체크해야됨, 1 이면 아예 안둘러봤으므로 종료해도됨.
+        int end_index= result.length();
         while (true) {
-            if (index + 1 >= result.length()) { //중복된 문자열이 없이 범위를 넘어가면 종료
+
+            if(index>=end_index-1 && flag==0){
+                index=0;
+                flag=1;
+            }
+
+             if((index>=end_index-1 || end_index==1) && flag==1){
                 break;
-            }
-            if (result.charAt(index) == result.charAt(index + 1)) {  // 중복이자 연속된 문자열이 진행될 경우
-                deDuplication(result, index);
-                index = -1; // index -1로 초기화
-            }
-            index++;
+             }
+
+            System.out.println("index = " + index);
+            System.out.println("end_index = " + end_index);
+             if(result.charAt(index)==result.charAt(index+1)){
+
+                 flag=0;
+                 for(int i=index+1; i<end_index; i++){
+                     if(result.charAt(index) != result.charAt(i)){
+
+                         result.delete(index, i);
+                         System.out.println("result = " + result);
+                         end_index= result.length();
+                         int size= i-index;
+                         index=i-size;
+                         break;
+
+                     }
+                     else if(i==end_index-1)
+                     {
+                         result.delete(index, i+1);
+                         System.out.println("result = " + result);
+                         index=0;
+                         end_index=result.length();
+                     }
+
+                 }
+             }
+             else{
+
+                 index++;
+             }
+
         }
 
         answer = String.valueOf(result);
@@ -24,18 +59,21 @@ public class Problem2 {
         return answer;
     }
 
-    private static void deDuplication(StringBuffer result, int index) {
+   /* private static void deDuplication(StringBuffer result, int index) {
         if (result.length() == 2) {
             result.delete(index, index + 2);
         } else if (result.length() > 2) {
-            for (int j = index; j < result.length(); j++) {
-                if (result.charAt(index) != result.charAt(j)) { // 맨 끝까지 같은 문자가 나오지 않을 경우 걸러내게 됨.
-                    result.delete(index, j);
-                    break;
-                }
+         for(int i=index; ; i++){
+             char now_index= result.charAt(i);
+             if(now_index != )
 
-            }
+
+         }
+
+
+
+
         }
         return;
-    }
+    }*/
 }
