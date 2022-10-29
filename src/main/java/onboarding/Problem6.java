33c528637ae1cba18ba112082542c40d4acc5a9a
HaODay <ycis5013@naver.com>
2022-10-29 17:49:22 +0900

    [Problem6] 연속 문자열을 집합에 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer= new ArrayList<>();
+
+        final int LENGTH=forms.size();
+        String[] strArr= new String[LENGTH];
+        //닉네임 문자열 배열
+        for(int i=0;i<LENGTH;i++){
+            strArr[i]=forms.get(i).get(1);
+        }
+        Set set1=new HashSet(); //닉네임 문자열 집합
+        Set set2=new HashSet(); //중복 닉네임 문자열 집합
+
+        for(int i=0;i<LENGTH;i++){
+            int length=strArr[i].length();
+            for(int j=0;j<length-1;j++){
+                if(set1.contains(strArr[i].substring(j,j+2))){
+                    set2.add(strArr[i].substring(j,j+2));
+                }
+                set1.add(strArr[i].substring(j,j+2));
+            }
+        }
+
         return answer;
     }
 }
