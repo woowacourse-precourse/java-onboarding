6d4e9be87d88bf841587b147fa49fbb7003c64e3
Yejin Shin <shinyejin0212@gmail.com>
2022-10-29 21:18:46 +0900

    docs : [Problem2] 구현할 기능 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,11 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        // 0. 재귀 사용해서 끝까지 중복 제거
+        // 1. 중복 삭제 기능
+        // 2. List를 String으로 바꾸는 기능
+        // 3. 원래 값과 비교해서 재귀 끝내는 조건 확인하는 기능
         String answer = "answer";
         return answer;
     }
