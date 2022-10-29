6f24c5d8d7d62075a735903bb257adfedbf068c2
leesky <leehaneul0623@gmail.com>
2022-10-29 11:05:05 +0900

    refactor: 누락된 예외 처리 구현(문제 4)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,9 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        int length = word.length();
+        if(!(0 < length && length < 1001)) return "";
+
         StringBuilder stringBuilder = new StringBuilder();
 
         for(int i = 0; i < word.length(); i++) {
