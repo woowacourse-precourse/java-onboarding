4d55a74371aa67d8ca690271314b37c447192868
RO-OR <kyk3531@gmail.com>
2022-10-30 02:53:32 +0900

    feat:2번기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        String result = cryptogram;
+        String result2= "";
+
+        while(true) {
+            for(int i=0; i<result.length(); i++) {
+                if(i==0 &&result.charAt(i) != result.charAt(i+1)) //i가 0일때
+                    result2 += result.charAt(i);
+                if(i==result.length()-1 && result.charAt(i) != result.charAt(i-1)) //i가 마지막원소 일때
+                    result2 += result.charAt(i);
+                if(i!=0 && i!=result.length()-1 && result.charAt(i) != result.charAt(i+1) && result.charAt(i-1) != result.charAt(i)) //앞과 뒤 모두 비교
+                    result2 += result.charAt(i);
+            }
+
+            if(result2.equals(result)) break; //더 이상 중복이 없으면 끝내기
+            result = result2;
+            result2 = "";
+        }
+
+        answer = result;
         return answer;
     }
 }
