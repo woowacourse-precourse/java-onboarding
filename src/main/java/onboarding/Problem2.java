18524a5c98cc7cb79f77cc9f8c65acba9eb5411e
syahn <496300@naver.com>
2022-10-29 21:58:51 +0900

    feat: 연속된 중복 문자 제거 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,6 +13,25 @@ public class Problem2 {
         return false;
     }
 
+    //중복 문자 제거하는 method
+    public static StringBuffer delOverlap(StringBuffer sb){
+        for(int i = 0; i<sb.length()-1; i++){
+            int count = 0;
+
+            if(sb.charAt(i) == sb.charAt(i+1)){
+                //연속된 중복 문자열의 마지막 index 구하기
+                for(int j = i+1; j<sb.length(); j++){
+                    if(sb.charAt(i) == sb.charAt(j))
+                        count++;
+                    else
+                        break;
+                }
+                sb.delete(i, (i+1+count));
+            }
+        }
+        return sb;
+    }
+
     public static String solution(String cryptogram) {
         String answer = cryptogram;
         //나중에 중복 문자 제거 위해 string을 StringBuffer로 변경
