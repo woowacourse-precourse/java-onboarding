1f54e1c0d3be94404a55d2b03617caad8aedb155
Invidam <hansupark0208@gmail.com>
2022-10-29 19:07:57 +0900

    refactor(Problem4): extract method transform formula and get center value formula

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,17 +4,35 @@ public class Problem4 {
 
     static final char START_CHAR = 'A';
     static final char END_CHAR = 'Z';
-    static final char DIFF_A_Z = (char) (END_CHAR- START_CHAR);
+    static final char DIFF_A_Z = (char) (END_CHAR - START_CHAR);
+
     public static char transformChar(char ch) {
-        if(Character.isLowerCase(ch)) {
+        if (Character.isLowerCase(ch)) {
             return Character.toLowerCase(transformChar(Character.toUpperCase(ch)));
         }
-        if(ch < DIFF_A_Z/2) {
-            return (char) (ch - START_CHAR + END_CHAR);
-        } else {
-            return (char) (END_CHAR - ch + START_CHAR);
+        if (isLeftCharacter(ch)) {
+            return transformLeftChar(ch);
         }
+
+        return transformRightChar(ch);
     }
+
+    private static char transformLeftChar(char ch) {
+        return (char) (ch - START_CHAR + END_CHAR);
+    }
+
+    private static char transformRightChar(char ch) {
+        return (char) (END_CHAR - ch + START_CHAR);
+    }
+
+    private static boolean isLeftCharacter(char ch) {
+        return ch < centerValue();
+    }
+
+    private static int centerValue() {
+        return DIFF_A_Z / 2;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
