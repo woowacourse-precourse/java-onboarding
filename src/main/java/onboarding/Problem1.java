0c1ea69c7e27c6a39f514f6716c2b65658e4fe79
ChaCha3088 <cha3088@gmail.com>
2022-10-29 22:15:49 +0900

    feat: Problem1 new function  drawTwoNumbersInRow

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private ArrayList<Integer> drawTwoNumbersInRow() {
+        Random random = new Random(); //랜덤 객체 생성(디폴트 시드값 : 현재시간)
+        random.setSeed(System.currentTimeMillis());
+        int randomNumber = 2 * ( random.nextInt(197) + 1 ) + 1;
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(randomNumber, randomNumber + 1);
+        return result;
     }
 }
