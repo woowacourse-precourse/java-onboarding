55091d707e465a651e21a14693d4f6d0cef2aa36
Ridealist <cjsn92@gmail.com>
2022-10-29 14:14:51 +0900

    feat/Problem4 : GreenFlog 클래스 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+}
+
+class GreenFlog {
+    Map<Character, Character> flog_map = new HashMap<>();
+    void makeMap() {
+        int len = 'z' - 'a' + 1;
+
+        for (int i=0; i<len; i++) {
+            char key = (char) ('a' + i);
+            char value = (char) ('z' - i);
+            flog_map.put(key, value);
+        }
+
+        for (int j=0; j<len; j++) {
+            char key = (char) ('A' + j);
+            char value = (char) ('Z' - j);
+            flog_map.put(key, value);
+        }
+    }
+
+    char reform(char ch) {
+        if (Character.isAlphabetic(ch)) {
+            return flog_map.get(ch);
+        }
+        return ch;
     }
 }
