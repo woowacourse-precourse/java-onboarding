3fa5c54a0db406c2cea536077a782f6d09cf648a
hyejungg <kimhyejung12@naver.com>
2022-10-29 12:43:35 +0900

    feat: [문제 1] 페이지 검증 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,23 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
-}
+
+	private static boolean isValidPage(List<Integer> pages) {
+		int leftPage = pages.get(0);
+		int rightPage = pages.get(1);
+
+		if (leftPage >= rightPage)
+			return false;
+		if (leftPage < 1 || leftPage > 400)
+			return false;
+		if (rightPage > 400)
+			return false;
+		if (rightPage - leftPage != 1)
+			return false;
+		if (leftPage % 2 != 1)
+			return false;
+		if (rightPage % 2 != 0)
+			return false;
+
+		return true;
+	}
