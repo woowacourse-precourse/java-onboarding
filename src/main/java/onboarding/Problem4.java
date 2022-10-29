1d09a23c9cadc8cb6c644c48b71157e361c22264
eora21 <eora21@naver.com>
2022-10-29 14:30:56 +0900

    feat: 🎸 추가 규칙에 맞는 map을 생성
    글자를 변환하는 규칙은 언제나 같다. 메서드에 접근하거나 글자 하나씩 가져왔을 때 일일히 변환해 줄 필요가 없다. 따라서
    static 초기화 블럭을 사용하였다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    private final static Map<Character, Character> wordSwitchRule;
+
+    static {
+        wordSwitchRule = new HashMap<>();
+
+        for (int i = 0; i < 26; i++) {
+            wordSwitchRule.put((char)('a' + i), (char)('z' - i));
+            wordSwitchRule.put((char)('A' + i), (char)('Z' - i));
+        }
+    }
+
+    /**
+     * 엄마 말씀 word가 매개변수로 주어질 때, 아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록 solution 메서드를 완성하라.
+     * @param word 원본 String
+     * @return 규칙에 맞게 변환한 String
+     */
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return "";
     }
 }
