30fec274ddfae78a65d597587b649d139eb25eb5
dclxxi <dclxxi@kakao.com>
2022-10-29 23:49:44 +0900

    feat(문제6): 닉네임을 두 글자씩 잘라 리스트로 반환하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    public static final int LETTERS = 2;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<String> getSubNicknames(String nickname) {
+        List<String> subNicknames = new ArrayList<>();
+        for (int i = 0; i <= nickname.length() - LETTERS; i++) {
+            subNicknames.add(nickname.substring(i, i + LETTERS));
+        }
+
+        return subNicknames;
     }
 }
