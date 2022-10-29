d8c49bbfdd480cc178d1e61c91d153a315f7bd31
KyungKyu Park <rudrb0505@gmail.com>
2022-10-29 14:30:55 +0900

    feat(Problem5): 받게 될 화폐의 종류와 개수를 출력하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,20 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
 	public static List<Integer> solution(int money) {
 		int withdrawalMoney = money;
 		int[] monetaryUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-		int[] withdrawalUnitCount = new int[monetaryUnit.length];
+		List<Integer> unitCount = new ArrayList<>(monetaryUnit.length);
 
 		for (int i = 0; i < monetaryUnit.length; i++) {
-			withdrawalUnitCount[i] = withdrawalMoney / monetaryUnit[i];
+			unitCount.add(withdrawalMoney / monetaryUnit[i]);
 			withdrawalMoney %= monetaryUnit[i];
 		}
 
-		List<Integer> answer = Collections.emptyList();
-		return answer;
+		return unitCount;
 	}
 }
