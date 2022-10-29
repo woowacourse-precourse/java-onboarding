e14cbfa786b00633bb623c4a1d8053a334ebd651
noparamin <noparamin@naver.com>
2022-10-29 14:49:25 +0900

    Problem4 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,45 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    static boolean isAlphabet(char w) {
+        if(w >= 65 && w <= 90) {
+            return true;
+        } else if(w >= 97 && w <= 122) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static String solution(String word) {
+
         String answer = "";
+
+        Map<Character, Character> charMap = new HashMap<>();
+
+        for(int i = 0; i < 26; i++) {
+            int asciiKey = i + 97;
+            int asciiVal = 122 - i;
+            charMap.put((char)asciiKey, (char)asciiVal);
+        }
+
+        for(char c : word.toCharArray()) {
+            if(isAlphabet(c)) {
+                if(Character.isUpperCase(c)) {
+                    char lowerAlpha = Character.toLowerCase(c);
+                    answer += Character.toUpperCase(charMap.get(lowerAlpha));
+                } else {
+                    answer += charMap.get(c);
+                }
+            } else {
+                answer += c;
+            }
+        }
+
         return answer;
     }
+
 }
