f6dc54dc88c6e982d6040041bdc34e0b2bde9187
seongha <11pi885@gmail.com>
2022-10-29 18:05:01 +0900

    feat: 4번 기능목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    //대문자, 소문자 배열생성
+
+
+    //맵으로 각각의 알파벳 마다 매칭되는 값 저장하기
+
     public static String solution(String word) {
         String answer = "";
+
+        //문자 하나씩 치환하고, 알파벳이 아니라면 추가하지 않기
+
         return answer;
     }
 }
