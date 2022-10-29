54d0d6f7890992d0134cd83cffad079bd90c497f
soyunim <togksk123@naver.com>
2022-10-29 23:41:36 +0900

    Problem4 2)if the word is lower

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,8 +9,12 @@ public class Problem4 {
 
         for(int i=0; i<word.length(); i++){
             // ASCII UPPER
-            if((int)word.charAt(i)>=65 && (int)word.charAt(i)<=90) {
-                strList.add(String.valueOf((char) ((int) word.charAt(i) + 25 - (((int) word.charAt(i) - 65) * 2))));
+            if((int)word.charAt(i)>=65 && (int)word.charAt(i)<=90){
+                strList.add(String.valueOf((char)((int)word.charAt(i)+25-(((int)word.charAt(i)-65)*2))));
+            }
+            // ASCII LOWER
+            else if((int)word.charAt(i)>=97 && (int)word.charAt(i)<=122) {
+                strList.add(String.valueOf((char) ((int) word.charAt(i) + 25 - (((int) word.charAt(i) - 97) * 2))));
             }
         }
         answer=strList.toString().join("",strList);
