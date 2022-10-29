6f173722e9f4e69ae2d1f683bc281a6df0550258
Invidam <hansupark0208@gmail.com>
2022-10-29 13:22:43 +0900

    refactor(Problem1): active auto align

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+
     public static int getPageMaxPoint(int page) {
         List<Integer> digits = getDigits(page);
 
@@ -15,23 +16,27 @@ class Problem1 {
     private static int getMaxMultiple(List<Integer> digits) {
         int ret = 1;
 
-        for(int digit: digits)
+        for (int digit : digits) {
             ret *= digit;
+        }
 
         return ret;
     }
 
     private static int getMaxSum(List<Integer> digits) {
         int ret = 0;
-        for(int digit: digits)
+
+        for (int digit : digits) {
             ret += digit;
+        }
+
         return ret;
     }
 
     private static List<Integer> getDigits(int page) {
         List<Integer> digits = new ArrayList<>();
 
-        while(page > 0) {
+        while (page > 0) {
             digits.add(page % 10);
             page /= 10;
         }
@@ -41,6 +46,7 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        
         return answer;
     }
 }
