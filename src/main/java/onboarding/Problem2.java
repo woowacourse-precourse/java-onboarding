23398c54a8b634fe30da4e7c7d2365a78d810e27
math0917 <math0917@naver.com>
2022-10-29 23:36:15 +0900

    docs : 문제풀이를 위한 새로운 생각 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,7 @@ import onboarding.problem2.Problem2Validation;
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
+import java.util.Stack;
 
 public class Problem2 {
 
@@ -13,44 +14,15 @@ public class Problem2 {
 
 
     public static String solution(String cryptogram) {
+        String result = "";
         if (Problem2Validation.problem2InputValidation(cryptogram, INPUT_MIN_LENGTH, INPUT_MAX_LENGTH)) {
-            while (true) {
-                Optional<String> duplicateIndex = returnExceptForDuplicateSubString(cryptogram);
-                if (duplicateIndex.isPresent()) {
-                    cryptogram = duplicateIndex.get();
-                } else {
-                    return cryptogram;
-                }
-            }
-        } else {
-            return "";
+            result = compressOverlappingWord(cryptogram);
         }
+        return result;
     }
 
-    private static Optional<String> returnExceptForDuplicateSubString(String cryptogram) {
-        if (cryptogram.length() <= 1) {
-            return Optional.ofNullable(null);
-        }
-        StringBuilder result = new StringBuilder();
-        int front = 0;
-        int back = 1;
-        for (; front < cryptogram.length()-1; front++) {
-            for (back = front + 1; back < cryptogram.length(); back++) {
-                if (cryptogram.charAt(front) != cryptogram.charAt(back)) {
-                    break;
-                }
-            }
-            if (front + 1 != back) {
-                break;
-            }
-        }
-        if (front != back) {
-            result.append(cryptogram, 0, front);
-            result.append(cryptogram.substring(back));
-            return Optional.ofNullable(result.toString());
-        } else {
-            return Optional.ofNullable(null);
-        }
+    private static String compressOverlappingWord(String cryptogram) {
+        StringBuilder afterCompressionExtractFromStack = new StringBuilder();
 
 
     }
