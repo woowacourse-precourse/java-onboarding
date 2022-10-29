a934ab90f7bfe918f0ad9e65daa42e97673f3293
Bumsoo <rhqjatn2398@naver.com>
2022-10-29 22:13:01 +0900

    feat(Problem3): 임의의 숫자 n에 대해 손뼉 칠 횟수를 구하는 메서드 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
+    static int[] numsToClap = new int[] {3, 6, 9} ;
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
     }
+
+    private static int calculateNumClap(int n) {
+        int result = 0;
+        for (int num : splitNum(n)) {
+            for (int numToClap : numsToClap) {
+                if (num == numToClap) {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static List<Integer> splitNum(int n) {
+        ArrayList<Integer> splitedNums = new ArrayList<>();
+        while (n != 0) {
+            splitedNums.add(n % 10);
+            n /= 10;
+        }
+
+        return splitedNums;
+    }
+
 }
