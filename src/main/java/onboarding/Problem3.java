6e1f1d82da579b754d37c7472bb3d14152cbdd4c
jeonghoon <shb03323@naver.com>
2022-10-29 18:53:34 +0900

    fix: fixed non-alphabetic characters limited to ' ' in problem4

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,14 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         for (int i = 1; i <= number; i++) {
             answer += count369(i);
         }
+
         return answer;
     }
 
     private static int count369(int number) {
         int count = 0;
+
         while (number != 0) {
             int remainder = number % 10;
             if (remainder == 3 || remainder == 6 || remainder == 9) {
@@ -18,6 +21,7 @@ public class Problem3 {
             }
             number /= 10;
         }
+
         return count;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,21 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = reverseWord(word);
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (char c: word.toCharArray()) {
+            answer.append(reverseWord(c));
+        }
+
+        return answer.toString();
     }
 
-    private static String reverseWord(String word) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char c: word.toCharArray()) {
-            if (c >= 'A' && c <= 'Z') {
-                stringBuilder.append((char)('Z' + 'A' - c));
-            } else if (c >= 'a' && c <= 'z') {
-                stringBuilder.append((char)('z' + 'a' - c));
-            } else if (c == ' ') {
-                stringBuilder.append(c);
-            }
+    private static Character reverseWord(char character) {
+        if (character >= 'A' && character <= 'Z') {
+            return (char)('Z' + 'A' - character);
+        } else if (character >= 'a' && character <= 'z') {
+            return (char)('z' + 'a' - character);
         }
-        return stringBuilder.toString();
+        return character;
     }
 }
