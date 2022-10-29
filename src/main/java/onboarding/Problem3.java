ae7601c79710b1d96e9319f69aa1f0867d8703cf
SeongukBaek <bsu6235@gmail.com>
2022-10-29 16:28:16 +0900

    feat: implement countThreeSixNine()

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,24 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /*
+     * 주어진 문자열에 존재하는 3, 6, 9의 개수를 반환
+     * 문자열을 한 자리씩 정수로 변환해서 0이 아니고, 정수 % 3 == 0인 경우 카운트 증가
+     *
+     * @return int
+     * */
+    private static int countThreeSixNine(String number) {
+        int count = 0;
+        int numberLength = number.length();
+
+        for (int index = 0; index < numberLength; index++) {
+            int currentNumber = number.charAt(index) - '0';
+            if (currentNumber != 0 && currentNumber % 3 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
