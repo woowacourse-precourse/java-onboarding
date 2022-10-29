f1e4957e0fae0749e145730d6248d889d6070c55
HyeokJoon Kong <orijoon98@gmail.com>
2022-10-29 20:57:26 +0900

    feat: 문제6 - solution 함수 완성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,25 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String, Set<String>> nicknameSetMap = mapByTwoCharacters(forms);
+        Set<String> emailSet = new HashSet<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String twoCharacters = nickname.substring(i, i + 2);
+                Set<String> nickNames = nicknameSetMap.get(twoCharacters);
+                if (nickNames.size() > 1) {
+                    emailSet.add(form.get(0));
+                }
+            }
+        }
+
+        return emailSet.stream().sorted().collect(Collectors.toList());
     }
 
     public static Map<String, Set<String>> mapByTwoCharacters(List<List<String>> forms) {
