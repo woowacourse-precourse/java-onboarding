52502b7c44dd752f9d6898b1848249474e68d2bd
한은영 <oneeqzero@gmail.com>
2022-10-29 16:08:07 +0900

    feat: problem1 예외사항 체크 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean check_exception(List<Integer> li){
+        int left = li.get(0); int right = li.get(1);
+        if((left&1) == 0 || (right&1) == 1) return false;
+        if(left + 1 != right) return false;
+        if(left<1 || left>400) return false;
+        if(right<1 || right>400) return false;
+        return true;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
