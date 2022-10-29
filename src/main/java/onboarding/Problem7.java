162b63e8fe910467c2631fcfea2b1b30e5d54d6f
minaver <psmjoshua@gmail.com>
2022-10-29 16:47:24 +0900

    feat(problem7): HashMap으로 FriendGraph 구성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,7 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * >> 기능 요구 사항 정리
@@ -15,17 +13,59 @@ import java.util.Queue;
  * 1. 접근 방식
  *  - 사용자와 친구간 그래프를 만들어 사용자를 기준으로 BFS 탐색 2depth 실행
  *      - 1 depth : 사용자의 친구들
- *      - 2 depth : 사용자와 동일한 친구를 아는 사람들
+ *      - 2 depth : 사용자와 동일한 친구를 아는 사람들 <- 해당 사람들에게 +10
  *  - visitor는 최종적으로 더함
  *
  * */
 public class Problem7 {
 
-    private LinkedList<String> friendGraph = new LinkedList<>();
+    private HashMap<String, List<String>> friendGraph = new HashMap<>();
+    private HashMap<String, Integer> friendScore = new HashMap<>();
+
+    public static void main(String[] args) {
+        solution("mrko", new ArrayList<>(Arrays.asList(Arrays.asList("donut", "andole"),
+                Arrays.asList("donut", "jun"),
+                Arrays.asList("donut", "mrko"),
+                Arrays.asList("shakevan", "andole"),
+                Arrays.asList("shakevan", "jun"),
+                Arrays.asList("shakevan", "mrko"))),new ArrayList<>(Arrays.asList("bedi", "bedi", "donut", "bedi", "shakevan")));
+    }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        Problem7 problem7 = new Problem7();
         List<String> answer = Collections.emptyList();
+        
+        problem7.makeFriendGraph(friends);
+
         return answer;
+    }
+
+    // HashMap으로 FriendGraph 구성
+    public void makeFriendGraph(List<List<String>> friends){
+        for(List<String> crew : friends){
+            boolean isFirstCrewExist = friendGraph.containsKey(crew.get(0));
+            boolean isSecondCrewExist = friendGraph.containsKey(crew.get(1));
+
+            if(isFirstCrewExist && isSecondCrewExist) {
+                // 둘다 그래프에 있을시
+                friendGraph.get(crew.get(0)).add(crew.get(1));
+                friendGraph.get(crew.get(1)).add(crew.get(0));
+                continue;
+            }
+            if(!isFirstCrewExist){
+                // 첫번째 친구가 Graph에 없을시
+                friendGraph.put(crew.get(0),new ArrayList<>());
+                friendScore.put(crew.get(0),0);
+            }
+            if(!isSecondCrewExist){
+                // 두번째 친구가 Graph에 없을시
+                friendGraph.put(crew.get(1),new ArrayList<>());
+                friendScore.put(crew.get(1),0);
+            }
+            friendGraph.get(crew.get(0)).add(crew.get(1));
+            friendGraph.get(crew.get(1)).add(crew.get(0));
+        }
+        System.out.println("friendGraph = " + friendGraph);
     }
     
 }
