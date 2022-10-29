5ac7d4b4fd42a2d6bb378ee2974c25a1480ff1f5
goldcollar95 <yjs7298@naver.com>
2022-10-29 20:09:01 +0900

    문제3번

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,42 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+
+
+        int count = 0;
+
+
+
+        for(int i = 1; i <= number; i++) {
+
+            int current = i;
+            int temp = count;
+            while (current != 0) {
+                if(current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
+
+                    count++;
+
+
+
+                }
+
+                current /= 10;
+
+            }
+
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(solution(number));
     }
 }
