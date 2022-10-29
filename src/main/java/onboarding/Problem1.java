212ca5b4e073cb862961eff9604bfdd668f171f5
chkim <cndghks0213@gmail.com>
2022-10-30 05:33:39 +0900

    feat(problem1): 포비와 크롱의 점수를 비교하여 결과를 반환하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,12 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int DRAW = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (exceptional(pobi, crong)) {
             return -1;
         }
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        int pobiScore = maxScore(pobi);
+        int crongScore = maxScore(crong);
+
+        if (pobiScore > crongScore) {
+            return POBI_WIN;
+        }
+        if (pobiScore < crongScore) {
+            return CRONG_WIN;
+        }
+        return DRAW;
     }
 
     private static boolean exceptional(List<Integer> pobi, List<Integer> crong) {
