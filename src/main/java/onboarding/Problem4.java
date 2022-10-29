84c3cc9971e155aeab110a378a2458a604d4b06c
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 22:01:11 +0900

    Feat : add convert character function
    문자를 받고 청개구리 형식에 맞게 바꿔주는 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,20 @@ package onboarding;
 
 public class Problem4 {
 
+    private static char getConvertCharacter(char c) {
+        int valueDifference;
+        if(isAlpha(c)){
+            if(Character.isUpperCase(c)){
+                valueDifference = 'Z' - c + 'A';
+            }
+            else{
+                valueDifference = 'z' - c + 'a';
+            }
+            c = (char)valueDifference;
+        }
+        return c;
+    }
+
     private static boolean isAlpha(char c) {
         return (int) c >= 65 && (int) c <= 122;
     }
