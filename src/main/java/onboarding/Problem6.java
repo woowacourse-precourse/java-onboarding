81e86ffcc08c44f82cb00480dfccff53dddadb38
Soomin Lim <augustth511@gmail.com>
2022-10-29 21:21:26 +0900

    기능 추가: 닉네임의 모든 쌍들을 비교하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        int size = forms.size();
+        Set<Integer> indexSet = new HashSet<>();
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                String nicknameOne = forms.get(i).get(1);
+                String nicknameTwo = forms.get(j).get(1);
+
+                if (!validateDuplication(nicknameOne, nicknameTwo)) {
+                    indexSet.add(i);
+                    indexSet.add(j);
+                }
+            }
+        }
+
         return answer;
+    }
+
+    public static boolean validateDuplication(String nicknameOne, String nicknameTwo) {
+
     }
 }
