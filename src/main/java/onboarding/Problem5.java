ba65577ef8eb3ce7843ffd2901e4239b9db165d6
phobyjun <phobyjun@gmail.com>
2022-10-29 21:53:58 +0900

    [PROBLEM5] ADD: FEATURE1. 기능 요구 사항에 명시된 금액 리스트

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public List<Integer> moneyList = List.of(
+            50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
+    );
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
