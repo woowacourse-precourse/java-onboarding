75fe8df572ca5928e656b551421b5a81123fd122
채우석 <wochae@wochae-MacBookAir.local>
2022-10-30 00:05:12 +0900

    [feat] 1.1 초기화 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+    public static int[] notation;
     public static int solution(int number) {
         int answer = 0;
+
+        notation = new int[10000];
+        notation[0] = 0;
+        notation[1] = 0;
+        notation[2] = 0;
+
+        answer = increasingCrapCount(number);
+
         return answer;
     }
 }
