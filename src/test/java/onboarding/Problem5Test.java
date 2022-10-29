7c095f29df91a2776a2e4e0e1b5718a4028d6459
chanminmaxscaler <jcm04@naver.com>
2022-10-29 22:36:15 +0900

    test(Problem5Test): sortListDescendingOrder 메소드의 테스트 코드를 작성한다
    sortListDescendingOrderTest 클래스는 sortListDescendingOrder 메소드를 테스트한다.
    리스트를 입력하면 내림차순으로 정렬된 리스트를 반환하는지 확인한다.

--- src/test/java/onboarding/Problem5Test.java
+++ src/test/java/onboarding/Problem5Test.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -68,6 +73,21 @@ class Problem5Test {
                 int numberA = 5;
                 int numberB = 0;
                 assertThatThrownBy(() -> problem5.moduloNumberAByNumberB(numberA, numberB)).isInstanceOf(ArithmeticException.class);
+            }
+        }
+    }
+    @Nested
+    @DisplayName("sortListDescendingOrder 메소드는")
+    class sortListDescendingOrderTest {
+        @Nested
+        @DisplayName("리스트가 입력되면")
+        class Context_with_list {
+            @Test
+            @DisplayName("내림차순으로 정렬한 리스트를 반환한다")
+            void it_returns_descending_sorted_list() {
+                List<Integer> list = new ArrayList<>(Arrays.asList(5, 4, 2, 1, 3));
+                List<Integer> sortedList = List.of(5, 4, 3, 2, 1);
+                assertThat(problem5.sortListDescendingOrder(list)).isEqualTo(sortedList);
             }
         }
     }
