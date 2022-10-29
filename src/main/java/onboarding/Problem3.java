8ffc0d6a0e242edc8eabd7bd8d094b90f3babbec
Cho Changhun <cvcvcx@naver.com>
2022-10-29 21:24:11 +0900

    3번문제 주어진 숫자를 얼마나 포함하고 있었는가 계산하는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,12 +6,27 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-//        answer = checkNumber(number);
+        answer = checkNumber(number);
 
         return answer;
     }
+    public static int checkNumber(int number){
+        int count = 0;
+        for(int i=1;i<=number;i++){
+            int current = i;
+            count = getCount(count, current);
+        }
+        return count;
+    }
+// 현재 주어진 숫자가 3,6,9가 들어간 숫자를 얼마나 포함하고 있었는가 계산하는 함수
+    private static int getCount(int count, int current) {
+        while (current != 0){
+            count = increseCounts(count, current);
+            current /=10;
+        }
+        return count;
+    }
 
-//369를 지니고 있을때 카운트를 세서 반환하는 함수
     private static int increseCounts(int count, int current) {
         if((current %10)==3||(current %10)==6||(current %10)==9){
             count++;
