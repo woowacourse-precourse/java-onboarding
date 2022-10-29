a2490ea07deaffe9dced4422509862f881aec7d6
bellljun3395 <195850@jnu.ac.kr>
2022-10-29 22:57:25 +0900

    feat : Input4 클래스 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+}
+
+class Input4 {
+    static String inputWord() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+
 }
