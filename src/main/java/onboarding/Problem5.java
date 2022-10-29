737b6bdf220c145dea01fb04bf0e7120e0925444
Baejw0111 <bjw0111@naver.com>
2022-10-29 18:08:34 +0900

    Problem 5 complete

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        //화폐 종류
+        int[] currency={50000,10000,5000,1000,500,100,50,10,1};
+
+        for(int i=0;i<currency.length;++i){
+            int number=money/currency[i];
+            money%=currency[i];
+            answer.add(number);
+        }
+
         return answer;
     }
 }
