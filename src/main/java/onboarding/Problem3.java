b931c82a1dffd3ddfde334a84c0d9eb6851b8694
im-shung <seoyoung1113@gmail.com>
2022-10-29 17:40:49 +0900

    :sparkles: 숫자에 3,6,9가 들어갈 때 3,6,9의 개수를 구하기 기능 구현 #3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+
+    private static final List<String> criteriaList = List.of(
+            new String("3"),
+            new String("6"),
+            new String("9")
+    );
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
+
+    //숫자에 3,6,9가 들어갈 때 3,6,9의 개수를 구하는 메서드
+    public static int getClapCount(int number) {
+        int cnt = 0;
+        for (String s : String.valueOf(number).split("")) {
+            if (criteriaList.contains(s)) {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+
+
 }
