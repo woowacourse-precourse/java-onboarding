6543850c9ee7b671e332d124d6c85905d7f8aed7
kssumin <201566@jnu.ac.kr>
2022-10-29 22:50:46 +0900

    feat : 페이지의 각 숫자 더하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+}
+
+class Page {
+    public Page(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    private final Integer pageNumber;
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public int sumPage() {
+        return Stream.of(String.valueOf(pageNumber).split(""))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
