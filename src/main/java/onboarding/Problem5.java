cf18bcbe7b3f1da35af492fefaf20fd0ce1fa15a
eora21 <eora21@naver.com>
2022-10-29 16:39:19 +0900

    feat: 🎸 추가 금액을 인출하며 남은 금액을 반환받는 메서드 작성
    단위에 맞게 금액을 인출하고, 남은 금액을 받게끔 구성한다. 인출하여 리스트에 추가하는 메서드와 남은 금액을 반환하는 메서드를
    따로 구성하고, 남은 금액을 반환 전 인출하여 추가하는 형태로 구성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,26 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final int[] monetaryUnits = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    private static void bankWithdrawal(List<Integer> answer, int money) {
+
+        for (int monetaryUnit : monetaryUnits) {
+            money = getLeftMoney(answer, money, monetaryUnit);
+        }
+    }
+
+    /**
+     * 돈의 액수 money가 매개변수로 주어질 때
+     * 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 각 몇 개로 변환되는지
+     * 금액이 큰 순서대로 리스트/배열에 담아 return 하도록 solution 메서드를 완성하라.
+     *
+     * @param money
+     * @return
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        bankWithdrawal(answer, money);
         return answer;
     }
 }
