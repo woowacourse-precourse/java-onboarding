a509e8b3aa77680235eacebf3905d1e538f538ab
beombu <sunil1369@bible.ac.kr>
2022-10-29 23:03:57 +0900

    Feat(P3) : 기능목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * 3,6,9가 들어가는지 여부
+     */
+    public static Boolean includeNum(int number){
+        return false;
+    }
+
+    /**
+     * 수의 3,6,9 의 갯수
+     */
+    public static int cntNum(int number){
+        return 0;
+    }
 }
