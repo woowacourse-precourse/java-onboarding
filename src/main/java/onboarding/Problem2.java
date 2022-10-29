807c5af19f712ed39be88d05b3783766f22af0e1
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-10-29 21:41:37 +0900

    string을 list로 변환하는 기능 업데이트(ver 1.1)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,13 +12,8 @@ public class Problem2 {
     // 리스트를 돌리면서 겹치는 부분을 제거한다.
 
 
-    public static List<Character> getList(String cryptogram){
-        List<Character> cryptoList = new ArrayList<>();
-        for(int i=0; i<cryptogram.length(); i++){
-            cryptoList.add(cryptogram.charAt(i));
-        }
-        return cryptoList;
+    public static String[] getListFromString(String cryptogram){
+        return cryptogram.split("");
     }
-
 }
 
