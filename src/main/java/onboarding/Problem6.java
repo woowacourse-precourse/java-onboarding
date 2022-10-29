5986967fd5231c0f04f46308ea71e7a3fc9360e3
wwwndus <simock22@gmail.com>
2022-10-29 17:11:29 +0900

    feat: 닉네임 변수 생성 및 중복문자를 포함하는지 확인하는 메소드 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = 0; j < forms.size(); j++) {
+                if (i != j) {
+                    String firstNick = forms.get(i).get(1);
+                    String secNick = forms.get(j).get(1);
+
+                    checkNick(forms, answer, i, j, firstNick, secNick);
+                }
+            }
+        }
         return answer;
+    }
+
+    private static void checkNick(List<List<String>> forms, List<String> answer, int i, int j, String firstNick, String secNick) {
+
     }
 }
