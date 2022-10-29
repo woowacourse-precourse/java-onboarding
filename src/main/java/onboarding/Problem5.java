3657a26f77b6ea711ad79cd9931728fe3ddcc01f
hyowonhan <wain1719@gmail.com>
2022-10-29 20:49:22 +0900

    문제5 main 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,17 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int money = sc.nextInt();
+        System.out.println(solution(money));
     }
 }
