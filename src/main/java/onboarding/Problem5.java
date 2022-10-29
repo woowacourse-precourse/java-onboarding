caf4be626e16e9d3b118672b148e0a9f505cdb4d
5uhwann <5uhwann27@gmail.com>
2022-10-30 00:03:47 +0900

    feat: PROBLEM5 - 기능1 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+  private static int[] moneyTypeList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+  public static List<Integer> solution(int money) {
+    List<Integer> answer = countEachTypeOfMoney(money);
+    return answer;
+  }
+
+  public static List<Integer> countEachTypeOfMoney(int money) {
+    List<Integer> eachMoneyCountList = new ArrayList<>();
+    for (int moneyType : moneyTypeList) {
+      if (money / moneyType != 0) {
+        eachMoneyCountList.add(money / moneyType);
+        money = money % moneyType;
+      } else {
+        eachMoneyCountList.add(0);
+      }
     }
+    return eachMoneyCountList;
+  }
+
 }
