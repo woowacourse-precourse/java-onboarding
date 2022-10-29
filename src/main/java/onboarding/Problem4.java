af99e18683b6ced654e9bfef8fc92e6e9f918698
iamjooon2 <iamjooon2@gmail.com>
2022-10-29 20:17:28 +0900

    feat(problem4): 뒤집은 문자를 저장하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
+        final String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        final String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+        HashMap<Character, Character> frogMap = new HashMap<>();
+        for (int i = 0; i < upperCase.length() - 1; i += 1) {
+            frogMap.put(upperCase.charAt(i), upperCase.charAt(upperCase.length() - 1 - i));
+            frogMap.put(lowerCase.charAt(i), lowerCase.charAt(lowerCase.length() - 1 - i));
+        }
+
         String answer = "";
+
         return answer;
     }
 }
