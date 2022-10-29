29b01b1ae9cfb209972aa067cca9da81bd6ae34c
hoonn9 <hun9935@gmail.com>
2022-10-29 22:04:07 +0900

    Add hashSet for email (Problem6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,31 +2,32 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    private static final HashMap<String, String> map = new HashMap<>();
+    private static final HashMap<String, String> dupCheckMap = new HashMap<>();
+    private static final HashSet<String> emailSet = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
-
         for (List<String> form: forms) {
             String email = form.get(0);
             String nickname = form.get(1);
 
             for (String part: sliceByLength(nickname, 2)) {
                 if (isAlreadyUse(part, email)) {
-                    answer.add(map.get(part));
-                    answer.add(email);
+                    emailSet.add(dupCheckMap.get(part));
+                    emailSet.add(email);
                 }
-                map.put(part, email);
+                dupCheckMap.put(part, email);
             }
         }
 
-        return answer;
+        return new ArrayList<>(emailSet);
     }
 
     private static boolean isAlreadyUse(String part, String email) {
-        return map.containsKey(part) && !map.get(part).equals(email);
+        return dupCheckMap.containsKey(part) && !dupCheckMap.get(part).equals(email);
     }
 
     private static List<String> sliceByLength(String str, Integer len) {
