b980aef14a1f9b8ad0b5bf98ccb378b5d1d0d371
tlsqj <rnjstlsqja@inha.edu>
2022-10-29 15:35:51 +0900

    문제2번 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while(true) {
+            cryptogram += " ";
+            String nextCryptogram = "";
+            char current = cryptogram.charAt(0);
+            int sameCount = 1;
+            for(int i = 1; i < cryptogram.length() ; i++) {
+                if(cryptogram.charAt(i) != current) {
+                    if(sameCount == 1) nextCryptogram += current;
+                    current = cryptogram.charAt(i);
+                    sameCount = 1;
+                }
+                else {
+                    sameCount ++;
+                }
+            }
+            cryptogram = cryptogram.substring(0, cryptogram.length() - 1);
+            if(nextCryptogram.equals(cryptogram)) break;
+            cryptogram = nextCryptogram;
+        }
+        return cryptogram;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(solution("browoanoommnaon"));
     }
 }
