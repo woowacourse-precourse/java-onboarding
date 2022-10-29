d6ba3441303eaf42b9e18a55eec57c3b443bd2f2
thdwoqor <celebarte01@gmail.com>
2022-10-29 16:25:50 +0900

    add Problem3 Solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+    public static int get369(int num){
+        int add=0;
+        while(num > 0) {
+            if ((num %10)==3 ||(num %10)==6||(num %10)==9 ) add+= 1;
+            num /= 10;
+        }
+        return add;
+    }
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer+=get369(i);
+        }
         return answer;
     }
 }
