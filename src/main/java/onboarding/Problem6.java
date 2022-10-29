d6120485f2ade232416f5f3fbd0684e2e070ad52
jootang2 <joohwan.song15b@gmail.com>
2022-10-29 21:10:47 +0900

    :memo: [문제 6] : 풀이 전략

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        // 1. 닉네임(key) - 이메일(value) map 생성
+        // 2. 두 글자 씩 자른 값들이 다른 닉네임에 포함되어 있는 지 확인
+        // 3. 포함되어 있으면 list에 추가 (비교하는 닉네임, 비교되는 닉네임)
+        // 4. list 중복 제거
+        // 5. list 오른차순 정렬
+        // 6. list 반환
         List<String> answer = List.of("answer");
         return answer;
     }
