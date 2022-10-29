41338d0de734b60867834071b39aac9bbc6ce898
glaehfdl1654 <glaehfdl1654@naver.com>
2022-10-29 18:23:44 +0900

    [Problem4] declare reverse alphabet in map

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String alphabetSmall = "abcdefghijklmnopqrstuvwxyz";
+        String alphabetBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < alphabetSmall.length(); i++) {
+            map.put(alphabetSmall.charAt(i), alphabetSmall.charAt(alphabetSmall.length() - i - 1));
+        }
+        for (int i = 0; i < alphabetBig.length(); i++) {
+            map.put(alphabetBig.charAt(i), alphabetBig.charAt(alphabetBig.length() - i - 1));
+        }
+        return reverseChar(word, map);
+    }
+    public static String reverseChar(String word, HashMap<Character, Character> map) {
+
     }
 }
