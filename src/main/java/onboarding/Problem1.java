26644ec01c5ed55c19452396762ce65d1767317c
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-29 18:01:54 +0900

    feat(PROBLEM1): 예외처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,10 @@ class Problem1 {
     public int calScore(String str) {
         int mulTmp = 1, addTmp = 0;
         for (int i = 0; i < str.length(); i++) {
-            addTmp += Character.getNumericValue(str.charAt(i));;
-            mulTmp *= Character.getNumericValue(str.charAt(i));;
+            addTmp += Character.getNumericValue(str.charAt(i));
+            ;
+            mulTmp *= Character.getNumericValue(str.charAt(i));
+            ;
         }
         int maxScore = Math.max(addTmp, mulTmp);
         return maxScore;
@@ -25,11 +27,13 @@ class Problem1 {
         return score;
     }
 
-    public int versusScore(List<Integer> pobi, List<Integer> crong) {
+    public int versusScore(List<Integer> pobi, List<Integer> crong){
         int pobiScore = showScore(pobi);
         int crongScore = showScore(crong);
-
-        if (pobiScore > crongScore) {
+        List<Integer> startEnd = List.of(1, 400);
+        if(pobi.containsAll(startEnd) || crong.containsAll(startEnd) ||pobi.get(1) != pobi.get(0) + 1 || crong.get(1) != crong.get(0) + 1){
+            return -1;
+        } else if (pobiScore > crongScore) {
             return 1;
         } else if (pobiScore < crongScore) {
             return 2;
