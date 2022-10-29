ca3ad8cb534040d8fe9ee99aa617af16ab71d9cf
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-29 22:42:41 +0900

    Add mulDigits function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,10 +8,21 @@ class Problem1 {
         return answer;
     }
 
+    // 각 자리수를 더하는 함수
     public static Integer addDigits(Integer pageNum) {
         Integer value = 0;
         while (pageNum > 0) {
             value += pageNum % 10;
+            pageNum /= 10;
+        }
+        return value;
+    }
+
+    // 각 자리수를 곱하는 함수
+    public static Integer mulDigits(Integer pageNum) {
+        Integer value = 1;
+        while (pageNum > 0) {
+            value *= pageNum % 10;
             pageNum /= 10;
         }
         return value;
