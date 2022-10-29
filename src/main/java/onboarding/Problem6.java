079d27b4513970de0498600304f345aa9f9d0b14
HyeokJoon Kong <orijoon98@gmail.com>
2022-10-29 20:56:15 +0900

    feat: 문제6 - 연속하는 두 문자로 매핑한 닉네임 set 반환 메소드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static Map<String, Set<String>> mapByTwoCharacters(List<List<String>> forms) {
+        Map<String, Set<String>> nicknameSetMap = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String twoCharacters = nickname.substring(i, i + 2);
+                Set<String> nicknameList = nicknameSetMap.getOrDefault(twoCharacters, new HashSet<>());
+                nicknameList.add(nickname);
+                nicknameSetMap.put(nickname.substring(i, i + 2), nicknameList);
+            }
+        }
+
+        return nicknameSetMap;
     }
 }
