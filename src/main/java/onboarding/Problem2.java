e7a801098e18dcce58a4e714d73c52a04c83dd68
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-29 22:08:56 +0900

    feat(PROBLEM2): removeOverlap 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeOverlap(String str) {
+        char[] chars = str.toCharArray();
+        char prev = chars[0];
+
+        for (int i = 1; i < chars.length; i++)
+        {
+           if(prev == chars [i]){
+               chars[i-1] = ' ';
+               chars[i] = ' ';
+           }
+           prev = chars[i];
+        }
+        String ans = String.valueOf(chars);
+        ans = ans.replaceAll(" ", "");
+        return ans;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
