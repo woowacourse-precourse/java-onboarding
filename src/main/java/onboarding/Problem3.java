9ca3e7d0c26cf86c0cc4ad7a85b7f23b660040ca
dOcOb <abg1418@gmail.com>
2022-10-29 17:46:48 +0900

    complete problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int num = 1; num <= number; num++) {
+            String numStr = String.valueOf(num);
+
+            String[] numArr = numStr.split("");
+            for (String test : numArr) {
+                 if(test.equals("3") || test.equals("6") || test.equals("9")){
+                     answer++;
+                 }
+            }
+        }
         return answer;
     }
 }
---
+++ src/main/java/onboarding/Test.java
@@ -0,0 +1,7 @@
+package onboarding;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Problem3.solution(33));
+    }
+}
