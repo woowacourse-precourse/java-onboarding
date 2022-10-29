e2844df84627d56a3b7cc9bd9fe5909f62b5d3ca
ONE <sodlapdlf13@gmail.com>
2022-10-29 17:16:22 +0900

    docs: [Problem2] 필요 기능에 대한 주석을 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+/*
+* 1. getResult(String cryptogram): Stack 을 이용해 문자를 처리하는 method
+*   1-1. 문자열의 처음 문자부터 끝 문자까지 탐색
+*   1-2. Stack 이 비어있지 않고 Stack 의 제일 위의 문자가 현재 문자와 같을 때
+*       1-2-1. 같지 않은 문자가 나올 때까지 문자를 가리키는 포인터를 이동
+*       1-2-2. Stack 의 제일 위의 문자를 pop()하고 새로운 문자를 add()
+*   1-3. Stack 에 남아있는 문자들을 문자열로 변환하여 반환
+*/
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
