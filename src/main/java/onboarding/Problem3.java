7314e0bf010304132ee352e381a351cfa0b8fd68
Luka <meena2003@naver.com>
2022-10-29 15:48:12 +0900

    howManyNums 변수 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,8 @@ public class Problem3 {
         // 2. 숫자를 하나씩 세면서 해당 숫자내에 3 6 9 가 몇개 있는지 확인하는 메서드 생성
         // 3. 정답 반환
 
+        int howManyNums = 0;
+
         int answer = 0;
         return answer;
     }
