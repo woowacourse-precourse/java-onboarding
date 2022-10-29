831b67094e55faaf6c1a0b5f556dfe61930afcf3
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-29 18:16:04 +0900

    feat(Problem3): 숫자에 3,6,9,가 얼마나 포함 되었는지 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    public static final String GAME_NUMBER = "369";
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static int countIncludedNumberOfGameNumber(int number){
+        String[] numberArray =  Integer.toString(number).split("");
+        int count = 0;
+        for (String num: numberArray){
+            if(GAME_NUMBER.contains(num)){
+            count++;
+            }
+        }
+        return count;
     }
 }
