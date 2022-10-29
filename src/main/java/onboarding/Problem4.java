e4e7eca0e7f29ccd50eeb6c9cf52c1d1e6b590f2
sysliar <sysliar@gmail.com>
2022-10-30 01:57:28 +0900

    Q4. Solve

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            if(isUpper(word.charAt(i))) answer += (char)('A' + 'Z' - word.charAt(i));
+            else if(isLower(word.charAt(i))) answer += (char)('a' + 'z' - word.charAt(i));
+            else answer += word.charAt(i);
+        }
         return answer;
     }
 
