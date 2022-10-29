080a0d85b34e223487b51eaffc449b2bf17147ed
good9804 <good9804@naver.com>
2022-10-29 15:23:29 +0900

    문제 4번 대문자 변환기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,15 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    public static char upper(char alphabet){
+        char answer;
+        if(alphabet>77){
+           answer= (char) (77-(alphabet-78));
+           return answer;
+
+        } else if(alphabet==77){
+            answer=77;
+            return 77;
+        } else {return answer = (char) (77+(77 - alphabet+1));}
+    }
 }
