a6ef2a6aa4dff064f821a1af94ada2a13f9dcfbe
조미정 <ing06164@naver.com>
2022-10-29 23:17:17 +0900

    문제 4 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        Map<Character, Character> map = new HashMap<>();
+
+        for(int i=0; i<26; i++){
+            map.put((char)(65+i), (char)(90-i));
+            map.put((char)('a'+i), (char)('z'-i));
+        }
+
+        for(int i=0; i<word.length(); i++){
+            char w = word.charAt(i);
+
+            if(w == ' '){                // 공백처리
+                answer += w;
+                continue;
+            }
+
+            answer += map.get(w);
+        }
+
         return answer;
     }
 }
