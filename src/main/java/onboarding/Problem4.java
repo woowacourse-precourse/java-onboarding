0696e78bcd59011d06c698296dab282dc52b97d5
seonghoo1217 <seonghoo1217@naver.com>
2022-10-29 14:29:35 +0900

    style : Add a Comment on [Task4]

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        // 97 109 a-m
+        // 110 122 n-z
+
+        // 65 77 A-M
+        // 78 90 N-Z
         return answer;
     }
 }
