4fb91711710e495923f721caa664a9decac4d25c
K-Diger <kitten.diger@gmail.com>
2022-10-30 01:38:19 +0900

    fix : 메서드 분리 - isCountableNumber(int index, String currentValue) 메서드 추가 -> 현재 순회중인 값에 대한 수가 3, 6, 9 에 해당하면 1을 반환하여 카운트를 할 수 있는 역할 - countClap(String currentValue) 메서드 추가 -> 순회 중인 값이 몇번의 박수를 쳐야하는지 카운트 - extractResult(int number) 메서드 추가 -> 1부터 입력받은 수 까지 반복하여 총 박수를 쳐야하는 횟수를 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,29 +4,46 @@ package onboarding;
 // 뭔가 하드코딩한 느낌이나서 다시 살펴보면 좋을 것 같음
 public class Problem3 {
 
-    public static int countClap(int number) {
+    // 현재 순회중인 수를 인덱스로 슬라이싱하여
+    // 박수를 쳐도 되는 수 인지 확인하는 메서드
+    // 1 이면 박수를 쳐야 하는 수
+    // 0 이면 박수를 치지 않아야 하는 수
+    public static int isCountableNumber(int index, String currentValue) {
+        if (currentValue.charAt(index) == '3') {
+            return 1;
+        } else if (currentValue.charAt(index) == '6') {
+            return 1;
+        } else if (currentValue.charAt(index) == '9') {
+            return 1;
+        }
+        return 0;
+    }
+
+    // 현재 순회중인 값이 몇 번의 박수를 쳐야하는지 세는 메서드
+    public static int countClap(String currentValue) {
+
+        int currentValueLength = currentValue.length();
+        int count = 0;
+
+        for (int j = 0; j < currentValueLength; j++) {
+            count += isCountableNumber(j, currentValue);
+        }
+        return count;
+    }
+
+    // 총 몇 번의 박수를 쳐야하는지 검사하는 메서드
+    public static int extractResult(int number) {
         int result = 0;
 
         for (int i = 1; i < number + 1; i++) {
-
             String currentValue = String.valueOf(i);
-            int currentValueLength = currentValue.length();
-
-            for (int j = 0; j < currentValueLength; j++) {
-                if (currentValue.charAt(j) == '3') {
-                    result += 1;
-                } else if (currentValue.charAt(j) == '6') {
-                    result += 1;
-                } else if (currentValue.charAt(j) == '9') {
-                    result += 1;
-                }
-            }
+            result += countClap(currentValue);
         }
         return result;
     }
 
 
     public static int solution(int number) {
-        return countClap(number);
+        return extractResult(number);
     }
 }
