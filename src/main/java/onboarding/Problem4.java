eb6f3910d7fc6459167e3989777650f81fe5de7e
hyowonhan <wain1719@gmail.com>
2022-10-29 18:40:31 +0900

    문제4 main 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,13 @@
 package onboarding;
-
+import java.util.Scanner;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(solution(word));
     }
 }
