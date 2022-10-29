1b7387be6778a2442d8fc201c079ba88356816a1
Youngho <cyh6099@gmail.com>
2022-10-29 20:23:40 +0900

    test(p7) : unitTest를 위한 별도 test class작성 및 BeforeEach작성

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -1,10 +1,12 @@
 package onboarding;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.as;
@@ -356,22 +358,32 @@ class ApplicationTest {
         }
     }
 
-//    @Nested
-//    class Problem7Test {
-//        @Test
-//        void case1() {
-//            String user = "mrko";
-//            List<List<String>> friends = List.of(
-//                    List.of("donut", "andole"),
-//                    List.of("donut", "jun"),
-//                    List.of("donut", "mrko"),
-//                    List.of("shakevan", "andole"),
-//                    List.of("shakevan", "jun"),
-//                    List.of("shakevan", "mrko")
-//            );
-//            List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
-//            List<String> result = List.of("andole", "jun", "bedi");
-//            assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
-//        }
-//    }
+    @Nested
+    class Problem7Test {
+        @Test
+        void case1() {
+            String user = "mrko";
+            List<List<String>> friends = List.of(
+                    List.of("donut", "andole"),
+                    List.of("donut", "jun"),
+                    List.of("donut", "mrko"),
+                    List.of("shakevan", "andole"),
+                    List.of("shakevan", "jun"),
+                    List.of("shakevan", "mrko")
+            );
+            List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+            List<String> result = List.of("andole", "jun", "bedi");
+            assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+        }
+
+    }
+    @Nested
+    class Problem7UnitTest{
+        @BeforeEach
+        void setUp() {
+            Problem7.friendsList = new ArrayList<>();
+            Problem7.recommendScore = new HashMap<>();
+            Problem7.relationships = new HashMap<>();
+        }
+    }
 }
