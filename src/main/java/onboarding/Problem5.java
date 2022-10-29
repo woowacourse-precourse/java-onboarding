65cd3efb50c597dd06aa40355cc304c47655f268
shpark30 <seohyeonpark30.gmail.com>
2022-10-29 16:19:20 +0900

    comment(Problem5.java): 기능 구현 로직 주석 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,13 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        /* 화폐 배열 생성 */
+        /* 정답 배열 생성 */
         List<Integer> answer = Collections.emptyList();
+        /* 화폐 배열 Loop
+        *1. 가장 큰 화폐 단위로 money를 나눈다. -> 몫: 해당 화폐 개수
+        *2. 위보다 작은 화폐 단위로 1번의 나머지를 다시 나눈다 -> 몫: 해당 화폐 개수
+        */
         return answer;
     }
 }
