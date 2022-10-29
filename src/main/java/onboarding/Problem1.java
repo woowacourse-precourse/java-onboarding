b99bc17ba95ca19089ad92dc18a8ee1811898133
tlsqj <rnjstlsqja@inha.edu>
2022-10-29 15:09:33 +0900

    점수 계산 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private int pageSumMul(int pageNum) {
+        int sum = 0, multiply = 1;
+        while(pageNum > 0) {
+            sum += pageNum % 10;
+            multiply *= pageNum % 10;
+            pageNum /= 10;
+        }
+
+        return sum > multiply ? sum : multiply;
+    }
+    private int calcScore(List<Integer> page) {
+        // 왼쪽 페이지 계산
+        int leftScore = pageSumMul(page.get(0));
+        int rightScore = pageSumMul(page.get(1));
+
+        return leftScore > rightScore ? leftScore : rightScore;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int pobiSocre = calcScore(pobi), crongScore = calcScore(crong);
+
         return answer;
     }
 }
