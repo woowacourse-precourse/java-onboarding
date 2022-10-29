c7bd36f1609852ff2a467cc2f0137f31805d7aac
jjmin9797 <jjmin9797@naver.com>
2022-10-29 23:38:13 +0900

    [PUSH][Problem7][준비]

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("mrko", "donut"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        System.out.println(solution(user,friends,visitors));
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> userFriend = new ArrayList<>();
+        for (List<String> f : friends) {
+            if (f.indexOf(user) == 1) {
+                userFriend.add(f.get(0));
+            }else if (f.indexOf(user) == 0) {
+                userFriend.add(f.get(1));
+            }
+
+        }
+        System.out.println(userFriend);
+
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
