894b12bdeef30aeb7d07beb1566b8de7a299d579
sdcodebase <sdcodebase@gmail.com>
2022-10-29 17:43:08 +0900

    feat: Problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clap = 0;
+        for (int i = 1; i <= number; i++) {
+            String str = String.valueOf(i);
+            clap += (int) Arrays.stream(str.split(""))
+                    .filter(s -> {
+                        int n = Integer.parseInt(s);
+                        return n == 3 || n == 6 || n == 9;
+                    })
+                    .count();
+        }
+        return clap;
     }
 }
