74c343f0840665bea379abc070c93abe971e1ae8
qkrdmswl <eunji303@naver.com>
2022-10-30 05:32:37 +0900

    [feat] 닉네임의 부분 문자열을 추출하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    /**
+     *  기능 명세
+     *  1. 닉네임 간 중복된 문자 있는지 검사하기
+     *  2. 중복된 닉네임의 이메일 출력하기
+     */
+
+    static Map<String, Integer> partOfNickList = new HashMap<String, Integer>();
+
+    public static List<String> ListOfDuplicate(List<List<String>> forms) {
+        ArrayList<String> emailOfDuplicateNickname = new ArrayList<>();
+        return emailOfDuplicateNickname;
+    }
+    
+    public static void extractPartOfNick(String nickName) {
+        for (int i=0; i < nickName.length()-2; i++) {
+            String extract = nickName.substring(i, i+2);
+            if (partOfNickList.containsKey(extract)) { partOfNickList.put(extract, partOfNickList.get(extract)+1); }
+            else                                     { partOfNickList.put(extract, 1); }
+        }
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = ListOfDuplicate(forms);
         return answer;
     }
 }
