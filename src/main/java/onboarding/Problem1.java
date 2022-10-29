e9a7417b56977dbe5a2a8c497ad4342e71de6c3b
good9804 <good9804@naver.com>
2022-10-29 13:11:46 +0900

    문제1번 각자리수의 합 최댓값 반환 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,7 +2,6 @@ package onboarding;
 
 import java.util.List;
 import java.util.Random;
-//임의로 책 펼치는 기능
 //페이지번호를 더하거나 곱해서 높은걸로 반환하는 기능
 //포비가 이기면 1  크롱이 이기면 2 무승부 0 예외 -1리턴
 class Problem1 {
@@ -10,9 +9,25 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
-    public static int RandomPage(int pagemax){
-        Random random = new Random();
-        int page= random.nextInt(pagemax)+1;
-        return page;
+    public static int resultMax(List<Integer> list){
+        int Plusmax;//더하기 최대값
+        int Multimax;//곱하기 최대값
+        Plusmax=PlusMax(list.get(0), list.get(1));
+        
+        }
+    }
+    public static int PlusMax(int odd,int max){ //각 자리수의 합을 구하는 기능
+            int sum=0;
+            if(max%10==0){//페이지가 39 40처럼 앞자리수가 달라질때를 구분
+            while(odd!=0){
+                sum+=odd&10;
+                odd/=10;
+            }} else {
+                while(max!=0){
+                    sum+=max&10;
+                    max/=10;
+                }
+            }
+            return sum;
     }
 }
