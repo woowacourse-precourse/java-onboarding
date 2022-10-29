a76488c572a5f0417ed26fe9bf4d9abc3ad51302
박원종 <wonjong1030@naver.com>
2022-10-30 00:24:58 +0900

    feat: 모든 닉네임과 중복되는 닉네임 체크 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,26 +1,41 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
+    static boolean[] isDuplication;
     static List<List<String>> list;
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
         list = forms;
-
-        for(int i=0; i<forms.size(); i++) {
+        isDuplication = new boolean[forms.size()];
+        for(int i=0; i<forms.size(); i++){
             String curName = forms.get(i).get(1);
             permutation(curName, i);
         }
-
         return answer;
     }
     public static void permutation(String name, int listIdx){
         for(int i=0; i<name.length(); i++){
             if(i+1 >= name.length()) break;
             String s = name.substring(i, i+2);
-            System.out.println(s);
+            checkDuplication(s, listIdx);
         }
+    }
+    public static void checkDuplication(String s, int listIdx){
+        int cnt=0;
+        for(int i=0; i<list.size(); i++){
+            if(i == listIdx || isDuplication[i]) continue;
+            if(list.get(i).get(1).contains(s)){
+                isDuplication[i] = true;
+                cnt++;
+            }
+        }
+        //1개이상 중복되면 자기 자신도 제거
+        if (cnt >0)
+            isDuplication[listIdx] = true;
+
     }
 }
