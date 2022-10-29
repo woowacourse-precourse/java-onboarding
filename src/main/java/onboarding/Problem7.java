900c434649f273eec16206cbcbffc3b8c4adfb8b
Park shinyeong <syngh503@gmail.com>
2022-10-29 16:56:55 +0900

    Problem7/Feat/최종 결과값 도출 기능 구현 최종적으로 결과값을 리턴하는 기능을 구현하였습니다. Relate to: #7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,11 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> userFriends = getUserFriends(user, friends);
+        HashMap<String, Integer> listOfScore = new HashMap<>();
+        listOfScore = getAcquaintance(user, listOfScore, userFriends, friends);
+        listOfScore = getVisitorScore(listOfScore, visitors, userFriends);
+        answer = getTheTopFive(listOfScore);
         return answer;
     }
 
@@ -23,18 +28,21 @@ public class Problem7 {
                 continue;
             }
         }
-
         return userFriends;
     }
 
-    public static HashMap<String, Integer> getAcquaintance (HashMap<String, Integer> listOfScore, List<String> userFriends, List<List<String>> friends) {
-        for (String user: userFriends) {
+    public static HashMap<String, Integer> getAcquaintance (String user, HashMap<String, Integer> listOfScore, List<String> userFriends, List<List<String>> friends) {
+        for (String friendId: userFriends) {
             for (List<String> friend: friends) {
                 String user1 = friend.get(0);
                 String user2 = friend.get(1);
-                if (user1.equals(user)) {
+                if(user1.equals(user) || user2.equals(user)) {
+                    continue;
+                }
+                if (friendId.equals(friendId)) {
                     listOfScore.put(user2, listOfScore.getOrDefault(user2, 0) + 10);
-                } else if (user2.equals(user)) {
+                }
+                if (user2.equals(friendId)) {
                     listOfScore.put(user1, listOfScore.getOrDefault(user1, 0) + 10);
                 }
             }
@@ -57,12 +65,15 @@ public class Problem7 {
         entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> user1, Map.Entry<String, Integer> user2) {
-                return user1.getValue() - user2.getValue();
+                return user2.getValue() - user1.getValue();
             }
         });
 
         List<String> recommendList = new ArrayList<>();
         int numOfPeople = 5;
+        if(entryList.size() < 5) {
+            numOfPeople = entryList.size();
+        }
         for(int i = 0; i < numOfPeople; i++) {
             Map.Entry<String, Integer> entry = entryList.get(i);
             recommendList.add(entry.getKey());
