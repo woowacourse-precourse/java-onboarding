fcfc0ef64edaaf9aa44673f5dd8e4be374f44b4a
saehim <saeilee.vxv@gmail.com>
2022-10-29 16:16:01 +0900

    Feat(Problem3): 숫자 1개에 대한 박수 횟수 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    // 박수칠 숫자 체크
+    static int check(int number) {
+        int result = 0;
+        while (number > 0) {
+            int jarisoo_1 = number%10;
+            if (jarisoo_1 == 3 || jarisoo_1 == 6 || jarisoo_1 == 9) result ++;
+            number /= 10;
+        }
+        return result;
+    }
 }
