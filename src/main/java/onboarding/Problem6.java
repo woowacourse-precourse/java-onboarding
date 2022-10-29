57a8455453501c15f06c92dd7313c63e78d95ee3
ksh-beginner <ohk9134@naver.com>
2022-10-29 09:38:39 +0900

    refactor : 실행 시 파라미터 네이밍 리팩토링
    실행 시 받는 파라미터의 네이밍 'forms' -> 'crewInfoForms'로
    의미가 잘 전달될 수 있게 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
+
+    public static List<String> solution(List<List<String>> crewInfoForms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+
+
+
 }
