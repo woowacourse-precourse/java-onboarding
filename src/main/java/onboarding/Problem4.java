0e2c2ac88ae56fcb8be6721942de107f06fd2f1d
wonjunYou <youwjune@gmail.com>
2022-10-30 01:20:18 +0900

    feat : (Problem 4) word 문자열 길이를 검증하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem4 {
+    public static final int MAX_WORD_LENGTH = 1000;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        try {
+            validateLength(word);
+        } catch (IllegalArgumentException e) {
+            return e.toString();
+        }
+    }
+
+    public static void validateLength(String word) {
+        if (word.length() < 1 || word.length() > MAX_WORD_LENGTH) {
+            throw new IllegalArgumentException("허용되지 않은 길이의 문자열입니다.");
+        }
     }
 }
