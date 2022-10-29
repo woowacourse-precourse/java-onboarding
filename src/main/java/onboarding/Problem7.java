9a44cd8881684c5b36cf5d7d5fdfce81a51008ca
JuHyeokParkk <hyeok5596@naver.com>
2022-10-29 19:10:04 +0900

    feat(Problem7): 친구 간 관계를 HashMap에 저장하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+
+    static HashMap<String, ArrayList<String>> friendsMap;
+    static HashMap<String, Integer> userScoreMap;
+    static final int VISITOR_SCORE = 1;
+    static final int WITH_FRIEND_SCORE = 10;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    static void addFriendsMap(List<List<String>> friends) {
+
+        friendsMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+
+            String firstFriend = friend.get(0);
+            String secondFriend = friend.get(1);
+
+            ArrayList<String> firstFriendArr = friendsMap.getOrDefault(firstFriend, new ArrayList<>());
+            ArrayList<String> secondFriendArr = friendsMap.getOrDefault(secondFriend, new ArrayList<>());
+
+            firstFriendArr.add(secondFriend);
+            secondFriendArr.add(firstFriend);
+
+            friendsMap.put(firstFriend, firstFriendArr);
+            friendsMap.put(secondFriend, secondFriendArr);
+        }
     }
 }
