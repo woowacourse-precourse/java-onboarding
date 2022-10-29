f6d531681dff3551033f9118975adc5e03ceb536
dmdrk1414 <seungchan141414@gmail.com>
2022-10-29 22:45:08 +0900

    :poop:[Add] problem_4 제코드에서 똥냄세가 납니다.
    나의 코드에서 똥냄세가 나는데요...
    아스키코드의 규칙이 있을꺼 같습니다. 좀더 고민해야 겠네요...
    똥냄세나는 나의 코드는 봐꿔야됩니다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,8 +3,101 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        System.out.println("answer = " + answer);
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 0; i < word.length(); i++) {
+            char charWord = word.charAt(i);
 
+            if (charWord == ' ') {
+                sb.append(charWord);
+            } else if (charWord == 'a') {
+                charWord = 'z';
+                sb.append(charWord);
+            } else if (charWord == 'b') {
+                charWord = 'y';
+                sb.append(charWord);
+            } else if (charWord == 'c') {
+                charWord = 'x';
+                sb.append(charWord);
+            } else if (charWord == 'd') {
+                charWord = 'w';
+                sb.append(charWord);
+            } else if (charWord == 'e') {
+                charWord = 'v';
+                sb.append(charWord);
+            } else if (charWord == 'f') {
+                charWord = 'u';
+                sb.append(charWord);
+            } else if (charWord == 'g') {
+                charWord = 'T';
+                sb.append(charWord);
+            } else if (charWord == 'h') {
+                charWord = 's';
+                sb.append(charWord);
+            } else if (charWord == 'i') {
+                charWord = 'r';
+                sb.append(charWord);
+            } else if (charWord == 'j') {
+                charWord = 'q';
+                sb.append(charWord);
+            } else if (charWord == 'k') {
+                charWord = 'p';
+                sb.append(charWord);
+            } else if (charWord == 'l') {
+                charWord = 'o';
+                sb.append(charWord);
+            } else if (charWord == 'm') {
+                charWord = 'n';
+                sb.append(charWord);
+            } else if (charWord == 'n') {
+                charWord = 'm';
+                sb.append(charWord);
+            } else if (charWord == 'o') {
+                charWord = 'l';
+                sb.append(charWord);
+            } else if (charWord == 'p') {
+                charWord = 'k';
+                sb.append(charWord);
+            } else if (charWord == 'q') {
+                charWord = 'j';
+                sb.append(charWord);
+            } else if (charWord == 'r') {
+                charWord = 'i';
+                sb.append(charWord);
+            } else if (charWord == 's') {
+                charWord = 'h';
+                sb.append(charWord);
+            } else if (charWord == 't') {
+                charWord = 'g';
+                sb.append(charWord);
+            } else if (charWord == 'u') {
+                charWord = 'f';
+                sb.append(charWord);
+            } else if (charWord == 'v') {
+                charWord = 'e';
+                sb.append(charWord);
+            } else if (charWord == 'w') {
+                charWord = 'd';
+                sb.append(charWord);
+            } else if (charWord == 'x') {
+                charWord = 'c';
+                sb.append(charWord);
+            } else if (charWord == 'y') {
+                charWord = 'b';
+                sb.append(charWord);
+            } else if (charWord == 'z') {
+                charWord = 'a';
+                sb.append(charWord);
+            }
+        }
+
+        System.out.println("sb.toString() = " + sb.toString());
+//        "R olev blf"
         return answer;
+    }
+
+    public static void main(String[] args) {
+        solution("i love you");
     }
 }
