74efda1b22565ffdb16d0c8b1636416dbbe80e3d
김재현 <kjhkk1020@naver.com>
2022-10-29 21:25:40 +0900

    Problem 4 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,16 +6,16 @@ import java.lang.Math;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer;
-        boolean pobiValid = (pobi.get(1) - pobi.get(0) == 1) && (pobi.get(0)%2 == 1); // pobiValid: 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자이면서, 왼쪽페이지가 홀수라면 true
-        boolean crongValid = (crong.get(1) - crong.get(0) == 1) && (crong.get(0)%2 == 1); // crongValid: 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자이면서, 왼쪽페이지가 홀수라면 true
+        boolean pobiValid = (pobi.get(1) - pobi.get(0) == 1) && (pobi.get(0) % 2 == 1); // pobiValid: 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자이면서, 왼쪽페이지가 홀수라면 true
+        boolean crongValid = (crong.get(1) - crong.get(0) == 1) && (crong.get(0) % 2 == 1); // crongValid: 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자이면서, 왼쪽페이지가 홀수라면 true
 
         /* pobi 또는 crong 중 유효하지 않은 것이 있다면 예외사항 발생 */
-        if(!(pobiValid && crongValid)) {
+        if (!(pobiValid && crongValid)) {
             answer = -1;
         } else {
-            if(getScore(pobi.get(0), pobi.get(1)) > getScore(crong.get(0), crong.get(1))) {
+            if (getScore(pobi.get(0), pobi.get(1)) > getScore(crong.get(0), crong.get(1))) {
                 answer = 1; // 포비가 이기는 경우
-            } else if(getScore(pobi.get(0), pobi.get(1)) < getScore(crong.get(0), crong.get(1))) {
+            } else if (getScore(pobi.get(0), pobi.get(1)) < getScore(crong.get(0), crong.get(1))) {
                 answer = 2; // 크롱이 이기는 경우
             } else {
                 answer = 0; // 무승부인 경우
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,33 +12,34 @@ public class Problem2 {
 
     /**
      * 주어진 문자열에서 중복하는 문자들을 지움
+     *
      * @return 연속하는 중복 문자를 삭제한 결과 String
      */
     public static String solveCryptogram(String cryptogram) {
         char[] cryptogramArray = cryptogram.toCharArray();
         ArrayList<Character> cryptogramList = new ArrayList<>();
 
-        for(char c : cryptogramArray) {
+        for (char c : cryptogramArray) {
             cryptogramList.add(c);
         }
 
-        while(true) {
+        while (true) {
             boolean isDuplication = false;
-            for(int i = 1; i < cryptogramList.size(); i++) {
-                if(cryptogramList.get(i) == cryptogramList.get(i - 1)) {
+            for (int i = 1; i < cryptogramList.size(); i++) {
+                if (cryptogramList.get(i) == cryptogramList.get(i - 1)) {
                     isDuplication = true; // 연속된 같은 문자가 있으면 true
-                    cryptogramList.remove(i-1);
-                    cryptogramList.remove(i-1);
+                    cryptogramList.remove(i - 1);
+                    cryptogramList.remove(i - 1);
                 }
             }
             /* 만약 중복된 문자가 없었다면 : 암호 해독 완료 */
-            if(!isDuplication) {
+            if (!isDuplication) {
                 break;
             }
         }
 
         String result = new String("");
-        for(char c : cryptogramList) {
+        for (char c : cryptogramList) {
             result += c;
         }
 
