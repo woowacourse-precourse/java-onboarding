4e16a202efcdca27751ab5c897c2d3073d594651
leeyongjun <leeyongjun0310@naver.com>
2022-10-29 17:30:11 +0900

    fix(PROBLEM4): initAlphaMap 메소드 Map 원소 타입 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Problem4 {
@@ -9,22 +11,22 @@ public class Problem4 {
         return answer;
     }
 
-    private static Map initAlphaMap() {
+    private static Map<String, String> initAlphaMap() {
         // 알파벳 키-값으로 매핑해서 초기화 해주는 메소드
-        Map<Character, Character> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
 
         // 대문자 매핑
         for (int i = 0; i < 26; i++) {
             int left = 65 + i;
             int right = 90 - i;
-            map.put((char) left, (char) right);
+            map.put(String.valueOf((char) left), String.valueOf((char) right));
         }
 
         // 소문자 매핑
         for (int i = 0; i < 26; i++) {
             int left = 97 + i;
             int right = 122 - i;
-            map.put((char) left, (char) right);
+            map.put(String.valueOf((char) left), String.valueOf((char) right));
         }
 
         return map;
