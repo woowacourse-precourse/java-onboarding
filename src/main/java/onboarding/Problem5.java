8b84ba8d7cad07369078d7d345540b5d63f0271c
Kid-Chang <pch3310@gmail.com>
2022-10-29 19:17:22 +0900

    refactor: code refactor in problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,23 +1,20 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        if (money > 1000000 || money < 0)
+            return answer;
 
-        if(money > 1000000 || money < 0) return answer;
-
-        for (int unit: moneyUnit){
+        for (int unit : moneyUnit) {
             System.out.println(money / unit);
-            answer.add((int)(money / unit));
-            money= money % unit;
+            answer.add((money / unit));
+            money = money % unit;
         }
 
         return answer;
