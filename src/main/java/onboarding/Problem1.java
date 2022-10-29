58beb5b84351d84ba13c23fffe66a1f415f3d299
idoburnish <doyun7433@naver.com>
2022-10-29 18:00:59 +0900

    feat(PROBLEM1): 페이지 예외 상황 확인 기능
    페이지가 시작면이나 마지막면이 나오거나, 페이지가 연속되지 않거나, 페이지 규칙을 따르지 않은 경우에 -1 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,14 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!findPageException(pobi) || !findPageException(crong)) return -1;
         return answer;
+    }
+
+    public static boolean findPageException(List<Integer> user) {
+        if (user.get(0) % 2 == 0) return false;
+        if (user.get(1) != user.get(0) + 1) return false;
+        if (user.get(0) == 1 || user.get(1) == 400) return false;
+        return true;
     }
 }
