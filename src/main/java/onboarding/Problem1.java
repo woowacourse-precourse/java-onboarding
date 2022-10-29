406cce7ce10959855a13ecde22a7148a99f51365
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-29 17:37:26 +0900

    FEAT : Problem 1-1. 책을 임의로 펼치기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,36 @@ package onboarding;
 
 import java.util.List;
 
+/*
+*
+* Class : Problem 1
+*
+* Date : 2022.10.29
+*
+* Author : kathyleesh
+*
+* <기능 목록>
+*
+* 1. 책을 임의로 펼친다.
+* 2. 각자의 패를 선택하기 위한 클래스를 만든다.
+* 3. 왼쪽 페이지 번호의 각 자리 숫자를 구한다.
+* 4. 왼쪽 페이지 번호의 각 자리 숫자를 더한 값을 저장한다.
+* 5. 왼쪽 페이지 번호의 각 자리 숫자를 곱한 값을 저장한다.
+* 6. 오른쪽 페이지도 3~5번을 수행한다.
+* 7. 4~6번으로 저장된 4개의 값 중 가장 큰 수를 본인의 점수로 한다.
+* 8. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+* 9. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+* 10. 책의 페이지 번호는 연속적인 수이며 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수만 가능하다.
+*
+* */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        //pobi's card
+        System.out.println(pobi);
+        //crong's card
+        System.out.println(crong);
         return answer;
     }
 }
