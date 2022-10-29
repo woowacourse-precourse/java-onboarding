2709ad8a02acc6d89c0c788ebd03ea75ec1c92f3
iamjooon2 <iamjooon2@gmail.com>
2022-10-29 20:35:03 +0900

    refactor(problem4): 대문자, 소문자 상수 solution 메서드 밖으로 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,11 +3,13 @@ package onboarding;
 import java.util.HashMap;
 
 public class Problem4 {
+    public static final String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
     public static String solution(String word) {
-        final String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        final String lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
         HashMap<Character, Character> frogMap = new HashMap<>();
+
         for (int i = 0; i < upperCase.length() - 1; i += 1) {
             frogMap.put(upperCase.charAt(i), upperCase.charAt(upperCase.length() - 1 - i));
             frogMap.put(lowerCase.charAt(i), lowerCase.charAt(lowerCase.length() - 1 - i));
