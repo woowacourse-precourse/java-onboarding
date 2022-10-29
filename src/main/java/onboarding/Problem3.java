4f631c8a19b5546e54649eaf0bb2caaec4a050e0
EUNJEONGMUN <munej26@gmail.com>
2022-10-29 11:07:24 +0900

    refactor: 숫자 상수 처리하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,11 +5,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Problem3 {
+    static final int LEFT_PAGE_BOUNDARY = 1;
+    static final int RIGHT_PAGE_BOUNDARY = 10000;
+    static final int RADIX = 10;
+
     private static final Set<Integer> numbers369 = new HashSet<>(Arrays.asList(3, 6, 9));
     public static int solution(int number) {
         int answer = 0;
 
-        if (number<1 || number>10000) {
+        if (number<LEFT_PAGE_BOUNDARY || number>RIGHT_PAGE_BOUNDARY) {
             return answer;
         }
 
@@ -23,11 +27,11 @@ public class Problem3 {
         int count = 0;
         int remainder;
         while (num>0) {
-            remainder = num%10;
+            remainder = num%RADIX;
             if (numbers369.contains(remainder)) {
                 count++;
             }
-            num /= 10;
+            num /= RADIX;
         }
         return count;
     }
