3f3068684731bb7d6b0a43ae5b1c23542a555222
Donguk <95donguk.dev@gmail.com>
2022-10-29 20:37:20 +0900

    feat: 문제2의 문자열을 문자로 쪼개 리스트에 담는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = decrypt(cryptogram);
         return answer;
+    }
+
+    private static String decrypt(String cryptogram) {
+        List<Character> charList = new LinkedList<>();
+
+        splitString(cryptogram, charList);
+
+        return cryptogram;
+    }
+
+    private static List<Character> splitString(String cryptogram, List<Character> charList) {
+        char[] chars = cryptogram.toCharArray();
+        for (char aChar : chars) {
+            charList.add(aChar);
+        }
+        return charList;
     }
 }
