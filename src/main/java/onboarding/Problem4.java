48d32c20bb161f5411fd003b661a42b47ac37474
eora21 <eora21@naver.com>
2022-10-29 14:41:41 +0900

    feat: 🎸 추가 주어진 word를 순회하며 한 글자씩 result에 넣는 메서드
    한 글자씩 가져와 규칙에 맞는 글자를 반환해주는 메서드를 통해 결과를 완성시키는 메서드를 구현. 해당 규칙에 맞지 않는 글자는
    그대로 내보내야 하기에, 이것 또한 판단할 메서드를 따로 작성하기로 했다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,12 +15,23 @@ public class Problem4 {
         }
     }
 
+    private static void mamaWordTransformation(char[] treeFrogSay, String word) {
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            ch = treeFrogRule(ch);
+            treeFrogSay[i] = ch;
+        }
+    }
+
     /**
      * 엄마 말씀 word가 매개변수로 주어질 때, 아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록 solution 메서드를 완성하라.
      * @param word 원본 String
      * @return 규칙에 맞게 변환한 String
      */
     public static String solution(String word) {
+        char[] treeFrogSay = new char[word.length()];
+        mamaWordTransformation(treeFrogSay, word);
         return "";
     }
 }
