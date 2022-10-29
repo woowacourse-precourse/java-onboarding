fdd8a5db4d399a52df0c17a8453200d105b9184a
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-29 20:43:13 +0900

    221029 Feat: P3 - 기능1 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,30 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int total = 0;
+        //1. 1부터 number까지 반복하며 3, 6, 9가 들어가는지 체크한다.
+        for(int i = 1; i <= number; i++){
+
+            //2. 하나의 수를 다 체크하면 3, 6, 9의 개수를 변수에 담는다.
+        }
+
+        //3. 다음 수를 체크한다. number까지 다 체크하면 반복을 종료한다.
+
+        //4. 3, 6, 9의 누계를 리턴한다.
         return answer;
+    }
+
+    public static int checkDigits(int number) {
+        int count = 0;
+
+        while(number > 0) {
+            if((number - 3) % 10 == 0 || (number - 6) % 10 == 0 || (number - 9) % 10 == 0) {
+                count += 1;
+            }
+            number = number / 10;
+        }
+
+        return count;
     }
 }
