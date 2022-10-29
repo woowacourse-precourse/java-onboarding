2d94348fea96df2b89da1853515a491273c22322
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-30 02:15:01 +0900

    문자열 변환 메서드 수정, Problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,17 +2,14 @@ package onboarding;
 
 public class Problem4 {
     // TODO 문자열을 변환하는 메서드
-    public static String convert(String str) {
+    public static char convert(char chr) {
         String upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowerStr = upperStr.toLowerCase();
-        String result = "";
-
-        for (char chr : str.toCharArray()) {
-            if (Character.isUpperCase(chr)) {
-                result += upperStr.charAt(25 - upperStr.indexOf(chr));
-            } else if (Character.isLowerCase(chr)) {
-                result += lowerStr.charAt(25 - lowerStr.indexOf(chr));
-            }
+        char result = ' ';
+        if (Character.isUpperCase(chr))
+            result = upperStr.charAt(25 - upperStr.indexOf(chr));
+        else if (Character.isLowerCase(chr)) {
+            result = lowerStr.charAt(25 - lowerStr.indexOf(chr));
         }
         return result;
     }
@@ -20,6 +17,9 @@ public class Problem4 {
 
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            answer += convert(word.charAt(i));
+        }
         return answer;
     }
 }
