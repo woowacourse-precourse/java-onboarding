f00b6824b061a6e8414da7f90b43b5a9f31985d0
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-10-29 18:07:56 +0900

    problem2: 연속되는 중복 문자들을 제거하는 함수(removeDup)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String removeDup(String str) {
+        String[] strArr = str.split("");
+        ArrayList result = new ArrayList();
+        int flag = 0;
+        for (String s: strArr) {
+            if (result.size() == 0) {
+                result.add(s);
+                continue;
+            }
+            if ((result.get(result.size()-1)).equals(s)) {
+                flag = 1;
+                continue;
+            }
+            if (flag == 1) {
+                result.remove(result.size()-1);
+                flag = 0;
+            }
+            result.add(s);
+        }
+        return String.join("", result);
+    }
+
+    public static void main(String[] args){
+        System.out.println(removeDup("browoanoommnaon"));
     }
 }
