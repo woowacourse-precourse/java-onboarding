355c4d67dfbe3e9d2916a5b37451c7688012db1b
정핸모 <gusah009@naver.com>
2022-10-29 13:46:11 +0900

    feat: pobi나 crong이 주운 책이 유효한 책인지 확인한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,11 +1,35 @@
 package onboarding;
 
+import static java.lang.Math.max;
+
 import java.util.List;
 
 class Problem1 {
 
   public static int solution(List<Integer> pobi, List<Integer> crong) {
-    int answer = Integer.MAX_VALUE;
-    return answer;
+    if (isInvalidBook(pobi) || isInvalidBook(crong)) {
+      return -1;
+    }
+  }
+
+  private static boolean isInvalidBook(List<Integer> book) {
+    if (isInvalidBookSize(book) ||
+        isNotContinuousPages(book) ||
+        isLeftPageBiggerThanRightPage(book)) {
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean isInvalidBookSize(List<Integer> book) {
+    return book.size() != 2;
+  }
+
+  private static boolean isNotContinuousPages(List<Integer> book) {
+    return Math.abs(book.get(0) - book.get(1)) != 1;
+  }
+
+  private static boolean isLeftPageBiggerThanRightPage(List<Integer> book) {
+    return book.get(0) > book.get(1);
   }
 }
