cf48e4c4fbdf554a8629ec4dbd6d1bad02604b2b
Eleisia <blackcube222@gmail.com>
2022-10-30 00:17:07 +0900

    3번 문제 기능구현사항 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/*
+*  기능구현 사항
+* 1. 1부터 number까지 반복문을 도는 기능
+* 2. 해당 숫자가 3, 6, 9를 포함한다면 값을 더해주는 기능
+* */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
