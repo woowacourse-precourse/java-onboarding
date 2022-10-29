406fbb84e87781a420d4b49db5c3fa50d21f5d56
yxnxx <jh4rgh0@gmail.com>
2022-10-30 00:16:52 +0900

    feat: Add Problem3 counting number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = numberCount(number);
         return answer;
+    }
+
+    public static int numberCount(int number) {
+        int count = 0;
+        while(number!=0) {
+            if ((number%10 == 3) || (number%10 == 6) || (number%10 == 9)) count++;
+            number /= 10;
+        }
+        return count;
     }
 }
