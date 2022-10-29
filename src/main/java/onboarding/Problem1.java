bd3813d1bbf324be7aad8d0ad4647f935e61fd3e
박원종 <wonjong1030@naver.com>
2022-10-29 22:11:55 +0900

    feat: 페이지 각자리 수 곱하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,25 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static int mul(List<Integer> list){
+        int max = Integer.MIN_VALUE;
+        int num = 1;
+        for(int i=0; i<2; i++){
+            int cur = list.get(i);
+            if(cur >= 100){
+                num *= cur/100;
+                num *= (cur%100)/10;
+                num *= cur%10;
+            }
+            else{
+                num *= cur/10;
+                num *= cur%10;
+            }
+            max = Math.max(max, num);
+            num=1;
+        }
+        return max;
+    }
     public static int sum(List<Integer> list){
         int max=Integer.MIN_VALUE;
         int num = 0;
