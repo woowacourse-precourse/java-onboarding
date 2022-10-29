768d1ab20d44273a8a406f479d4bf2f6e46f425c
hectick <chyeon1223@cau.ac.kr>
2022-10-30 00:34:47 +0900

    refactor : [Problem5] 불필요한 코드 삭제

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,11 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<Integer>();
+        List<Integer> answer = new ArrayList<>();
         List<Integer> unit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
         for(int i = 0; i < unit.size(); i++){
