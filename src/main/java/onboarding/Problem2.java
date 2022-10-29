6288d9c0083884605403665172c863fe96b6701e
DaWoon Lee <qkgk61@gmail.com>
2022-10-29 21:29:37 +0900

    Feat : Add decrypt method
    Decrypter 클래스에 복호화 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        Decrypter decrypter = Decrypter.getInstance();
+        String answer = decrypter.decrypt(cryptogram);
         return answer;
     }
 }
@@ -18,6 +21,42 @@ class Decrypter {
 
     public static Decrypter getInstance() {
         return DecrypterHolder.INSTANCE;
+    }
+
+    public String decrypt(String cryptogram) {
+        Stack<Character> chars = new Stack<>();
+
+        for (int len = cryptogram.length(), i = 0; i < len; i++) {
+            char current = cryptogram.charAt(i);
+
+            if (isDuplicated(chars, current)) {
+                chars.pop();
+                continue;
+            }
+
+            chars.push(current);
+        }
+
+        String decrypted = stringOf(chars);
+        return decrypted;
+    }
+
+    private boolean isDuplicated(Stack<Character> chars, char current) {
+        if (chars.isEmpty()) {
+            return false;
+        }
+
+        return chars.peek() == current;
+    }
+
+    private String stringOf(Stack<Character> chars) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char current : chars) {
+            stringBuilder.append(current);
+        }
+
+        return stringBuilder.toString();
     }
 
 }
