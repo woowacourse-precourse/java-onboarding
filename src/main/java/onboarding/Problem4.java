b20bdb795c402b88c2746d99f2a16ce00c4e622b
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-29 10:33:41 +0900

    feat:Add a function that make dictionary

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    //청개구리 사전을 만드는 함수
+    public static HashMap<Integer,Integer> getDictionary(){
+        HashMap<Integer,Integer> dictionary = new HashMap<>();
+        //소문자
+        for(int i=97;i<110;i++){
+            dictionary.put(i,219-i);
+        }
+        //대문자
+        for(int i=65;i<78;i++){
+            dictionary.put(i,155-i);
+        }
+
+        return dictionary;
+    }
+
+
 }
