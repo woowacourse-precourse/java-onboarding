ab4a0e48476370403fbd402e8e82a581cec88afc
Wonkyun2 <kimwon1031@hanmail.net>
2022-10-29 15:38:53 +0900

    화폐 단위로 나누어 지갑에  담고 return

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,8 +8,11 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         int[] price= {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         ArrayList<Integer> wallet = new ArrayList<>();
+        for(int i=0; i< price.length; i++){
+            wallet.add(money/price[i]);
+            money = money%price[i];
+        }
         
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return wallet;
     }
 }
