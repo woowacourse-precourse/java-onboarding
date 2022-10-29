43ce3aade4d01f469364465212aebaf8b62ed208
eora21 <eora21@naver.com>
2022-10-29 10:56:51 +0900

    feat: 🎸 추가 주어진 number만큼 반복하며 전체 손뼉 횟수를 구하는 메서드
    손뼉의 횟수를 구하려면 우선 number만큼 수를 올려야 한다. 현재의 수를 전달하여 전체 손뼉 횟수를 얻도록 하였다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Set;
+
 public class Problem3 {
+    private static final Set<Integer> clapNumbers = Set.of(3, 6, 9);
+
+    private static int clapCount(int maxNumber) {
+        int totalClap = 0;
+
+        for (int number = 0; number <= maxNumber; number++) {
+            totalClap += splitNumberOfDigits(number);
+        }
+
+        return totalClap;
+    }
+    /**
+     * 숫자 number가 매개변수로 주어질 때, 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수를 return 하도록 solution 메서드를 완성하라.
+     *
+     * @param number 주어지는 최대 숫자
+     * @return 손뼉 횟수
+     */
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return clapCount(number);
     }
 }
