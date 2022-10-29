fd1e1d9caa1ac5498b94009ecde325e8580cbfcc
lzlkolo <lzlko1209@gmail.com>
2022-10-29 18:30:36 +0900

    PROBLEM5.단위별 교환 개수 구하기 및 money의 각 단위별 개수 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,9 +12,19 @@ public class Problem5 {
     }
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(9);
         isCheck(money);
 
+        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i=0; i<unit.length; i++) {
+            if(money/unit[i] >=1) {
+                answer.add(i, money/unit[i]);
+            } else {
+                answer.add(i, 0);
+            }
+            money = money % unit[i];
+        }
         return answer;
     }
 }
