162c492a9cab21a81b3b7f098a974d5b2176d3b6
Koonayoung518 <koony000415@gmail.com>
2022-10-30 03:21:58 +0900

    feat(problem5) :  5의 단위 지폐와 1의 단위 지폐 구분하기 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,10 +8,17 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         for(int i=10000; i>0; i/=10){
-            // money / i
+            List<Integer> list = divideUnit(money / i);
             money %= i;
         }
         return answer;
     }
-
+    private static List<Integer> divideUnit(int money){
+        List<Integer> list = new ArrayList<>();
+        //5의 단위 화폐 개수
+        list.add(money / 5);
+        //1의 단위 화폐 개수
+        list.add(money % 5);
+        return list;
+    }
 }
