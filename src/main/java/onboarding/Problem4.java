1bfc58ff382112c4c4043502e5b1f7dd2fd83e35
JunHyeon <dlwnsgus07@naver.com>
2022-10-29 22:00:03 +0900

    Feat : add distinguish letters function
    해당 문자가 알파벳인지 여부를 알려주는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static boolean isAlpha(char c) {
+        return (int) c >= 65 && (int) c <= 122;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
