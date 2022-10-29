bcdbabc74ef2288800bd75de1f41143db06a7797
apptie <appti@naver.com>
2022-10-29 22:47:05 +0900

    refactor: NumberCommonConst 클래스 이름 변경

--- src/main/java/onboarding/common/consts/DigitNumberCommonConst.java
+++ src/main/java/onboarding/common/consts/DigitNumberCommonConst.java
@@ -1,6 +1,6 @@
 package onboarding.common.consts;
 
-public abstract class NumberCommonConst {
+public abstract class DigitNumberCommonConst {
 
     public static final int CALCULATE_REMAINDER_VALUE = 0;
     public static final int DECIMAL_UNIT_VALUE = 10;
--- src/main/java/onboarding/problem3/consts/ClapCacheConst.java
+++
@@ -1,6 +0,0 @@
-package onboarding.problem3.consts;
-
-public abstract class ClapCacheConst {
-
-    public static final int CHECK_CACHE_VALUE = 0;
-}
