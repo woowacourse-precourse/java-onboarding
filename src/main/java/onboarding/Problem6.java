01655052a95f12aa0ae795f85bc68c9471e846f6
nswon <swnam0729@gmail.com>
2022-10-29 11:03:35 +0900

    feat(Problem6): 포함되지 않는 크루의 이메일과 닉네임을 기억해두는 로직 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 public class Problem6 {
     private static List<String> duplicateNicknames;
     private static List<String> answer;
+    private static RememberDto rememberDto;
 
     public static List<String> solution(List<List<String>> forms) {
         initSystem(forms);
@@ -15,6 +16,7 @@ public class Problem6 {
     }
 
     private static void initSystem(List<List<String>> forms) {
+        rememberDto = new RememberDto();
         duplicateNicknames = new ArrayList<>();
         duplicateNicknames.add(forms.get(0).get(1));
         answer = new ArrayList<>();
@@ -23,14 +25,17 @@ public class Problem6 {
 
     private static void checkNicknameInForms(List<List<String>> forms) {
         for (int i = 1; i < forms.size(); i++) {
-            if (isDuplicateNicknamesContainsNickname(forms.get(i).get(1))) {
+            if (isDuplicateNicknamesContainsNickname(forms.get(i))) {
                 answer.add(forms.get(i).get(0));
             }
             duplicateNicknames.add(forms.get(i).get(1));
         }
     }
 
-    private static boolean isDuplicateNicknamesContainsNickname(String nickname) {
+    private static boolean isDuplicateNicknamesContainsNickname(List<String> crew) {
+        String email = crew.get(0);
+        String nickname = crew.get(1);
+
         for (String duplicateNickname : duplicateNicknames) {
             if (duplicateNickname.length() == 1) {
                 continue;
@@ -38,11 +43,17 @@ public class Problem6 {
 
             for (int i = 0; i < nickname.length() - 1; i++) {
                 String check = nickname.substring(i, i + 2);
+                if(rememberDto.nickname.contains(check)) {
+                    answer.add(rememberDto.email);
+                    rememberDto = new RememberDto();
+                    return true;
+                }
                 if (duplicateNickname.contains(check)) {
                     return true;
                 }
             }
         }
+        rememberDto.init(email, nickname);
         return false;
     }
 
@@ -53,5 +64,20 @@ public class Problem6 {
         }
 
         return answer.stream().sorted().collect(Collectors.toList());
+    }
+
+    public static class RememberDto {
+        private String email;
+        private String nickname;
+
+        public RememberDto() {
+            this.email = "";
+            this.nickname = "";
+        }
+
+        public void init(String email, String nickname) {
+            this.email = email;
+            this.nickname = nickname;
+        }
     }
 }
