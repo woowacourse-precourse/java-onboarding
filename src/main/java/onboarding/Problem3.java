5a3b4fa8122a5118cd00019de01e1c4c929b5d32
antaeho <an981022@naver.com>
2022-10-29 16:18:10 +0900

    Problem3 improve

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,5 @@
 package onboarding;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 /*
 - 기능 구현사항 목록 -
 1. 1부터 입력값까지 for 문 실행
@@ -14,12 +11,11 @@ public class Problem3 {
         int answer = 0;
         for(int i = 1; i < number+1; i++) {
             String temp = Integer.toString(i);
-            int[] digits = new int[temp.length()];
-            for (int j = 0; j < temp.length(); j++) digits[j] = temp.charAt(j) - '0';
+            int l = temp.length();
+            int[] digits = new int[l];
+            for (int j = 0; j < l; j++) digits[j] = temp.charAt(j) - '0';
             int[] counter = new int[10];
-            for(int k = 0; k < digits.length; k++){
-                counter[digits[k]]++;
-            }
+            for (int digit : digits) counter[digit]++;
             for(int m = 1; m<4; m++){
                 answer += counter[3*m];
             }
