8e69f32d4a9472839d70dec9fb628e61d75f8649
tlsqj <rnjstlsqja@inha.edu>
2022-10-29 15:12:30 +0900

    예외 처리 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,16 +13,21 @@ class Problem1 {
 
         return sum > multiply ? sum : multiply;
     }
-    private int calcScore(List<Integer> page) {
+    private static int calcScore(List<Integer> page) {
         // 왼쪽 페이지 계산
         int leftScore = pageSumMul(page.get(0));
         int rightScore = pageSumMul(page.get(1));
 
         return leftScore > rightScore ? leftScore : rightScore;
     }
+    private static boolean checkPage(List<Integer> page) {
+        return page.get(0) % 2 == 1 && page.get(1) % 2 == 0 && page.get(1) - page.get(0) == 1;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(!checkPage(pobi) || !checkPage(crong)) return -1;
         int pobiSocre = calcScore(pobi), crongScore = calcScore(crong);
 
+        if()
         return answer;
     }
 }
