1585f3592fa4a04cb5dfba6ece4b10ab785a72c0
ensk26 <tjensk26@gmail.com>
2022-10-30 01:12:11 +0900

    feat: 중복 문자 제거하는 기능 추가, problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,11 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
 
         String answer = "answer";
+
         return answer;
     }
 
@@ -11,6 +13,19 @@ public class Problem2 {
     static String getRemoveDuplicateWord(String word) {
 
         StringBuffer resultWord = new StringBuffer();
+
+        for (int i = 0; i < word.length(); i++) {
+
+            if (i > 0 && word.charAt(i) == word.charAt(i - 1)) {
+                continue;
+            }
+            if (i < word.length() - 1 && word.charAt(i) == word.charAt(i + 1)) {
+                continue;
+            }
+
+            resultWord.append(word.charAt(i));
+        }
+
         return resultWord.toString();
     }
 
