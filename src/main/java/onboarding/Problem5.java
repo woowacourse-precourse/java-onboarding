db84b210fa01b3314cf6ae516bf93273a49dd2c3
wath1457 <wath1457@gmail.com>
2022-10-29 16:56:42 +0900

    [Problem5] traverse money list & Add value in answer list

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,7 +2,6 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /*
@@ -10,10 +9,17 @@ import java.util.List;
 2. 리스트를 순회하면서 money = money % 화폐단위, money / 화폐단위를 answer 리스트에 추가한다.
  */
 public class Problem5 {
+    public static void main(String args[]){
+        List<Integer> result = solution(50237);
+        System.out.println(result);
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        List<Integer> money_list = new ArrayList<>(Arrays.asList(50000,10000,1000,500,100,50,10));
-
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> money_list = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
+        for (int m : money_list) {
+            answer.add(money / m);
+            money = money % m;
+        }
         return answer;
     }
 }
