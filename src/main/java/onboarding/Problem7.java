2a8a4b19ad3d3f31ee62bfa47f26f9526454f613
kjb95 <topmedia44@gmail.com>
2022-10-29 17:35:57 +0900

    feat: Problem7 - friends 입력값 파싱하기
    TDD - GREEN
    키는 사용자 아이디, 값은 사용자의 친구 리스트를 가지는 해시맵 형태의 데이터 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,8 +13,29 @@ public class Problem7 {
         return answer;
     }
 
-    public static Map<String, List<String>> parseFriends(List<List<String>> friends) {
-        return null;
+    static Map<String, List<String>> parseFriends(List<List<String>> friends) {
+        Map<String, List<String>> parsedFriends = new HashMap<>();
+
+        friends.forEach(f -> {
+            String user1 = f.get(0);
+            String user2 = f.get(1);
+
+            if (!parsedFriends.containsKey(user1)) {
+                parsedFriends.put(user1, new ArrayList<>());
+            }
+            List<String> user1Friends = parsedFriends.get(user1);
+            user1Friends.add(user2);
+            parsedFriends.put(user1, user1Friends);
+
+            if (!parsedFriends.containsKey(user2)) {
+                parsedFriends.put(user2, new ArrayList<>());
+            }
+            List<String> user2Friends = parsedFriends.get(user2);
+            user2Friends.add(user1);
+            parsedFriends.put(user2, user2Friends);
+        });
+
+        return parsedFriends;
     }
 
 }
