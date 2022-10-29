52c35798dec580c4cfb948eccfe570d2247de346
newh08 <newh08@gmail.com>
2022-10-29 15:03:34 +0900

    Problem7 findUserFriends 메서드 구현 완료

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,11 +2,22 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    static List<String> findUserFriends(String user, List<List<String>> friends){
+        List<String> user_friends = Stream.of(friends).flatMap(List::stream)
+                .filter(i->i.contains(user))
+                .flatMap(List::stream)
+                .filter(i -> !i.equals(user))
+                .collect(Collectors.toList());
+        return user_friends;
     }
 }
 class Friend{
