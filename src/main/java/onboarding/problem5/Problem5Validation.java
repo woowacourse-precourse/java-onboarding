0ee691914be02f3779f380d06193f3c8242ce86f
math0917 <math0917@naver.com>
2022-10-29 15:44:35 +0900

    test : userInput validation test code 작성

---
+++ src/main/java/onboarding/problem5/Problem5Validation.java
@@ -0,0 +1,7 @@
+package onboarding.problem5;
+
+public class Problem5Validation {
+    public static boolean problem5InputValidation(int userInput, int minInput, int maxInput) {
+        return true;
+    }
+}
