749df575069a55d33c0f469a6f85e1b775faa1cb
JungYoonshin <sju08227@naver.com>
2022-10-29 17:03:28 +0900

    feat: #problem6 -> forms 리스트 안의 각 리스트를 추출하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         return answer;
     }
+
+    private static void blavlala(List<List<String>> forms){
+        for(List<String> nameAndEmailList : forms){
+            checkNameDuplication(nameAndEmailList);
+        }
+    }
+
 }
