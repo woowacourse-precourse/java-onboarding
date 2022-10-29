4584a051144f3b7b2eea400f92cc276e5c4bf9dd
kjb95 <topmedia44@gmail.com>
2022-10-29 12:32:19 +0900

    feat: Problem5 - 예외처리 코드 추가
    - 돈이 1 이상 1,000,000 이하가 아닌 경우

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public class Problem5 {
 
+    private static final int MONEY_MIN = 1;
+    private static final int MONEY_MAX = 1000000;
+
     private static final List<Integer> changeList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
     public static List<Integer> solution(int money) {
+        validateMoney(money);
         List<Integer> answer = new ArrayList<>();
 
         for (int change : changeList) {
@@ -17,6 +21,12 @@ public class Problem5 {
             money -= (changeCount * change);
         }
         return answer;
+    }
+
+    private static void validateMoney(int money) {
+        if (money < MONEY_MIN || money > MONEY_MAX) {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
