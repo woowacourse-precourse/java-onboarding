ee9244d0c35d4355d261d72d17ea7fd28479602c
chanwonlee <clw8679@naver.com>
2022-10-29 21:45:29 +0900

    FEAT: [Problem2] 문자 중복 제거 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,6 +6,33 @@ public class Problem2 {
         return answer;
     }
 
+
+    public static String deleteSameCharacters(String cryptogram) {
+        int n = cryptogram.length();
+        char[] chars = cryptogram.toCharArray();
+
+        //중복 문자는 X로 변환
+        for (int i = 1; i < n; i++) {
+            if (chars[i] == chars[i - 1]) {
+                chars[i - 1] = 'X';
+                for (int j = i+1; j < n; j++) {
+                    if (chars[i] != chars[j]) break;
+                    if (chars[i] == chars[j]) chars[j] = 'X';
+                }
+                chars[i] = 'X';
+            }
+        }
+
+        //중복 문자 제거
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (chars[i] != 'X') {
+                sb.append(chars[i]);
+            }
+        }
+        return sb.toString();
+    }
+
     /*
     중복 문자가 있으면 true
     없으면 false 반환
