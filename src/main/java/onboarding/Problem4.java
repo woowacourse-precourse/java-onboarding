dac8ba12f6c170699a6dd01d8d68da6fb3a9965f
chanminmaxscaler <jcm04@naver.com>
2022-10-29 11:54:43 +0900

    feat(Problem4): isLowerCase 메소드의 기능을 구현한다
    isLowerCase 메소드는 다음과 같이 기능한다.
    아스키코드를 int 형으로 입력받고, 아스키코드가 소문자 범위 "97보다 크거나 같고, 122보다 작거나 같다"에 포함되는지 확인하고 결과를 반환한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,4 +9,8 @@ public class Problem4 {
     public boolean isUpperCase(int characterInt) {
         return ((characterInt >= 65) && (characterInt <= 90));
     }
+
+    public boolean isLowerCase(int characterInt) {
+        return ((characterInt >= 97) && (characterInt <= 122));
+    }
 }
