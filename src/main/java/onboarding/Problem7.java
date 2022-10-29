d627ba7d5cc09519b2cfbe6c8ba998642fd2e6c4
49ehyeon42 <49ehyeon42@gmail.com>
2022-10-29 16:36:00 +0900

    문제7 - 기능 목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        // 1. 친구 그래프 작성
+        // 2. 사용자 친구 목록 작성
+        // 3. 사용자 친구 목록을 통한 사용자와 함께 아는 친구의 수 계산
+        // 4. 사용자 친구 목록을 통해 친구를 제외하고 사용자의 타임 라인에 방문한 횟수 계산
+        // 5. 추천 점수 내림차순으로 정렬하고, 같다면 이름순으로 정렬
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
