896010103930a92ee4c20e4b8151cb09b7632d3a
김현재 <41482946+galug@users.noreply.github.com>
2022-10-29 23:58:56 +0900

    feat(problem5): Add 제한 사항에 맞는 인자가 들어왔는지 검증하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import onboarding.problem5.Bank;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final int MAX_PRICE = 1000000 ;
+    static final int MIN_PRICE = 1;
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        if (!validateMoney(money)) {
+            return null;
+        }
+
+        List<Integer> answer = null;
         return answer;
+    }
+
+    public static boolean validateMoney(int money) {
+        if (money < MIN_PRICE || money > MAX_PRICE) {
+            return false;
+        }
+        return true;
     }
 }
