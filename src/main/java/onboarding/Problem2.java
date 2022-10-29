f65aa3851f9ec158e14fba4d13da2aa5acb554a1
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-10-29 21:56:43 +0900

    Problem2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int cryptogramIndex=0; //문자열 순회 변수
+        int end = cryptogram.length(); // 순회의 끝 조건 지정을 위해
+
+        while (cryptogramIndex < end){
+            if (cryptogramIndex != end-1 && cryptogram.charAt(cryptogramIndex) == cryptogram.charAt(cryptogramIndex+1) ){
+                cryptogram = cryptogram.replace(cryptogram.substring(cryptogramIndex,cryptogramIndex+2),"");
+                cryptogramIndex=0;
+                end = cryptogram.length();
+            }
+            else {
+                cryptogramIndex++;
+            }
+        }
+
+        answer = cryptogram;
         return answer;
     }
 }
