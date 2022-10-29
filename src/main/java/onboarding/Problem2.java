a8211e4bd97154714354f9d7a44439a7f871bf26
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-10-29 18:02:59 +0900

    feat: implement the solution about Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        for (int i = 0; i < answer.length() - 1; i++) {
+            char c = answer.charAt(i);
+            if (c == answer.charAt(i + 1)) {
+                answer = removeConsecutiveChars(answer, i);
+                i = -1;
+            }
+        }
+
         return answer;
+    }
+
+    private static String removeConsecutiveChars(String str, int index) {
+        String removedStr = str.substring(index, index + 2);
+        return str.replace(removedStr, "");
     }
 }
