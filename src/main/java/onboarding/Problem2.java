6e26e6997788d7df86a9b40668e887c6b7fb7c64
kimdonghun <zhfptm12@naver.com>
2022-10-29 19:20:24 +0900

    problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,31 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        String before = cryptogram;
+
+        while (true) {
+            String after = makeNewCryptogram(before);
+            if (before.equals(after)) {
+                answer = after;
+                break;
+            }
+            before = after;
+        }
         return answer;
+    }
+
+    private static String makeNewCryptogram(String before) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < before.length() - 1; i++) {
+            if (before.charAt(i) == before.charAt(i + 1)) {
+                i++;
+            } else {
+                sb.append(before.charAt(i));
+            }
+        }
+        if(before.charAt(before.length() -1) != before.charAt(before.length()-2)){
+            sb.append(before.charAt(before.length() - 1));
+        }
+        return new String(sb);
     }
 }
