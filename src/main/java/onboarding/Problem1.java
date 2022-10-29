61edeec29ac7c6a360bd68973e576fdfb4f43660
RadSparkle <wwwartg1@naver.com>
2022-10-29 23:16:02 +0900

    FEAT : 페이지의 자릿수 더한값과 곱한값을 담은 List를 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,18 +3,24 @@ package onboarding;
 import java.util.*;
 
 class Problem1 {
-    static List <Integer> pageNumberArray = new ArrayList();
-    static int pageSum = 0;
-    static int pageMultifly = 1;
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
         int answer = Integer.MAX_VALUE;
         return answer;
     }
 
+    //페이지의 자릿수 더한값과 곱한값을 담은 List를 리턴
+    public static List<Integer> test(List<Integer> page) {
+        List<Integer> testList = new ArrayList<>();
+
+        testList.add(0, multiflyPageNumber(page));
+        testList.add(1, sumPageNumber(page));
+
+        return testList;
+    }
     //페이지 숫자 쪼개는 메소드
     public static List<Integer> seperatePageNumber (int pageNumber) {
+        List <Integer> pageNumberArray = new ArrayList();
         for (int i = 0; pageNumber > 0; i ++) {
             pageNumberArray.add(pageNumber % 10);
             pageNumber /= 10;
@@ -24,6 +30,7 @@ class Problem1 {
 
     // 페이지 자릿수를 더해주는 메소드
     public static int sumPageNumber (List<Integer> pageNumberArray) {
+        int pageSum = 0;
         for (int i = 0; pageNumberArray.size() > i; i++) {
             pageSum += pageNumberArray.get(i);
         }
@@ -32,6 +39,7 @@ class Problem1 {
 
     //페이지 자릿수를 곱해주는 메소드
     public static int multiflyPageNumber (List<Integer> pageNumberArray) {
+        int pageMultifly = 1;
         for(int i = 0; pageNumberArray.size() > i; i++) {
             pageMultifly *= pageNumberArray.get(i);
         }
