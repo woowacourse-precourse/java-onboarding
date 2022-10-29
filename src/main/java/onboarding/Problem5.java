a4a01d7e2bf80b847b4db9457589bdf39ce0ffa8
khj1 <dhepd8849@gmail.com>
2022-10-29 10:54:08 +0900

    feat(problem5): 계좌에 있는 돈을 화폐로 변환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    public static final int[] MONEYTARY_UNITS = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return convertMoneyIntoCurrencies(money);
+    }
+
+    private static List<Integer> convertMoneyIntoCurrencies(int money) {
+        List<Integer> convertedCurrencies = new ArrayList<>();
+        for (int moneytaryUnit : MONEYTARY_UNITS) {
+            int convertedCount = money / moneytaryUnit;
+            money = money - convertedCount * moneytaryUnit;
+            convertedCurrencies.add(convertedCount);
+        }
+        return convertedCurrencies;
     }
 }
