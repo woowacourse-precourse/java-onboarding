004d13a592cab95d3a4dba22d2134c050030273f
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-30 00:54:34 +0900

    feat(PROBLEM4): convertAlphabet 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,20 @@
 package onboarding;
 
 public class Problem4 {
+    public static char convertAlphabet(char alphabet) {
+        int convertAscii = 0;
+        int ascii = 0;
+        if(Character.isUpperCase(alphabet)){
+            ascii = Character.getNumericValue(alphabet);
+            convertAscii = Math.abs(155 - ascii);
+        } else if (Character.isLowerCase(alphabet)) {
+            ascii = Character.getNumericValue(alphabet);
+            convertAscii = Math.abs(219 - ascii);
+        }
+        char convert = (char)(convertAscii);
+        return convert;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
