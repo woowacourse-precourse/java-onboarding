1444fa377ee597ea54dc3a6e730ac9b73f0f3d89
dhkim <akdlzmf1123@gmail.com>
2022-10-30 00:48:38 +0900

    refactor(problem2): 에러 코드 -1을 상수로 추출

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,11 +3,14 @@ package onboarding;
 import problem3.Game369ClapCounter;
 
 public class Problem3 {
+
+    public static final int ERROR_CODE = -1;
+
     public static int solution(int number) {
         try {
             return Game369ClapCounter.countFromOneTo(number);
         } catch (RuntimeException e) {
-            return -1;
+            return ERROR_CODE;
         }
     }
 }
