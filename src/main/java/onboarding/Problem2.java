f815e98897b83d1de65a60f5a3fc2cb1eff943a0
Devtist <yt930310@snu.ac.kr>
2022-10-30 01:32:47 +0900

    feat(Problem2): 연속된 문자열을 삭제하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,36 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static String removeRepetition(String cryptogram) {
+        String tmp = "";
+        tmp = tmp.concat(checkFirstLetter(cryptogram));
+
+        for (int i = 1; i < cryptogram.length()-1; i++) {
+            tmp = tmp.concat(checkMiddleLetter(cryptogram, i));
+        }
+        tmp = tmp.concat(checkLastLetter(cryptogram));
+        return tmp;
+    }
+
+    private static String checkFirstLetter(String cryptogram) {
+        if (cryptogram.charAt(0) == cryptogram.charAt(1)) {
+            return "";
+        }
+        return cryptogram.substring(0,1);
+    }
+
+    private static String checkMiddleLetter(String cryptogram, int i) {
+        if (cryptogram.charAt(i) == cryptogram.charAt(i-1) || cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+            return "";
+        }
+        return cryptogram.substring(i,i+1);
+    }
+
+    private static String checkLastLetter(String cryptogram) {
+        if (cryptogram.charAt(cryptogram.length()-1) == cryptogram.charAt(cryptogram.length()-2)) {
+            return "";
+        }
+        return cryptogram.substring(cryptogram.length()-1);
+    }
 }
