35dfb864709f98028322674aa4c4204f967edb22
chj <bluedog129@gmail.com>
2022-10-29 19:49:53 +0900

    [P5] money를 충족하기 위해 필요한 화폐 리스트 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,37 +1,59 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> result = Collections.emptyList();
-        while (money > 0) {
-            if (money >= 50000) {
-                result.add(money / 50000);
-                money %= 50000;
-            } else if (money >= 10000) {
-                result.add(money / 10000);
-                money %= 10000;
-            } else if (money >= 5000) {
-                result.add(money / 5000);
-                money %= 5000;
-            } else if (money >= 1000) {
-                result.add(money / 1000);
-                money %= 1000;
-            } else if (money >= 100) {
-                result.add(money / 100);
-                money %= 100;
-            } else if (money >= 50) {
-                result.add(money / 50);
-                money %= 50;
-            } else if (money >= 10) {
-                result.add(money / 10);
-                money %= 10;
-            } else if (money >= 1) {
-                result.add(money / 1);
-                money %= 1;
-            }
+        List<Integer> result = new ArrayList<>();
+        if (money >= 50000) {
+            result.add(money / 50000);
+            money %= 50000;
+        } else {
+            result.add(0);
+        }
+        if (money >= 10000) {
+            result.add(money / 10000);
+            money %= 10000;
+        } else {
+            result.add(0);
+        }
+        if (money >= 5000) {
+            result.add(money / 5000);
+            money %= 5000;
+        } else {
+            result.add(0);
+        }
+        if (money >= 1000) {
+            result.add(money / 1000);
+            money %= 1000;
+        } else {
+            result.add(0);
+        }
+        if (money >= 100) {
+            result.add(money / 100);
+            money %= 100;
+        } else {
+            result.add(0);
+        }
+        if (money >= 50) {
+            result.add(money / 50);
+            money %= 50;
+        } else {
+            result.add(0);
+        }
+        if (money >= 10) {
+            result.add(money / 10);
+            money %= 10;
+        } else {
+            result.add(0);
+        }
+        if (money >= 1) {
+            result.add(money / 1);
+            money %= 1;
+        } else {
+            result.add(0);
         }
         return result;
     }
