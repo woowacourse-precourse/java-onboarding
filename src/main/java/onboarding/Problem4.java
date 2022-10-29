3d883a46c868ae79dba88b061b581b649ed5e3f0
wonju-dev <wonjulee.dev@gmail.com>
2022-10-29 22:29:18 +0900

    [feat]Problem4 로직 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String reversedString = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (Character.isAlphabetic(c)) {
+                c = getReversedChar(c);
+            }
+            reversedString += c;
+        }
+
+        return reversedString;
+    }
+
+    private static char getReversedChar(char c) {
+        if (Character.isLowerCase(c)) {
+            return (char) (219 - c);
+        }
+
+        return (char) (155 - c);
+
     }
 }
