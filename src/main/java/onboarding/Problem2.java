7674c84683395a32abcb56f3db54dcefdaec039b
yesjuhee <juhee0924@g.skku.edu>
2022-10-30 01:39:24 +0900

    [PROBLEM2] 문자열에서 중복되는 문자를 제거하는 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,34 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+
         String answer = "answer";
         return answer;
+    }
+
+    public static String removeConsecutiveChar(String strInput) {
+        String strOutput = "";
+        int index = 0;
+        int strLen = strInput.length();
+
+        while (true) {
+            if (index < strLen - 1) {
+                if (strInput.charAt(index) != strInput.charAt(index + 1)) {
+                    strOutput += strInput.charAt(index);
+                    index++;
+                    continue;
+                }
+                index += 2;
+                continue;
+            }
+            if (index == strLen - 1) {
+                strOutput += strInput.charAt(index);
+                break;
+            }
+            if (index > strLen - 1) break;
+        }
+
+        return strOutput;
     }
 }
