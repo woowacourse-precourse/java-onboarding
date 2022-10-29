925e6d8dec09c30fa09bda5428bb71063f6e2472
lvalentine6 <ysr1227@naver.com>
2022-10-29 18:18:11 +0900

    feat : 문제 3_각 숫자에 3,6,9가 듣어가는지 확인하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String[] numberArr = String.valueOf(i).split("");
+            if(checkNumber(numberArr)) {
+
+            }
+        }
+
         return answer;
+    }
+    static boolean checkNumber(String[] numberArr) {
+        for (int i = 0; i < numberArr.length; i++) {
+            if(numberArr[i].equals("3") || numberArr[i].equals("6") || numberArr[i].equals("9")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
