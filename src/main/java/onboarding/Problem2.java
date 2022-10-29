6970a6740d114d7735fef9b6c23e940cad549639
skfkgla <narahim.lee@gmail.com>
2022-10-29 23:27:15 +0900

    feat: Problem2 - 문자열을 ArrayList 타입으로 한글자씩 자르는 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
         return answer;
+    }
+
+    /**
+     * @param str : 문자열
+     * @return str을 한글자씩 자른 ArrayList<String>
+     */
+    private static ArrayList<String> splitStringToArrayList(String str){
+        String[] strArray = str.split("");
+        return  new ArrayList<>(Arrays.asList(strArray));
     }
 }
