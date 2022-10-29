9887ad26226f1543b9ea58fbaaeff4e377a4c701
gyuturn <rbtjrrbals@gmail.com>
2022-10-29 14:48:55 +0900

    feat: Problem4 - isIncludeAlpa 메서드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,17 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static boolean isIncludeAlpa(String word) {
+        if(word.length()<1 || word.length()>1000) return false;
+
+        for (int i = 0; i < word.length(); i++) {
+            char alpa = word.charAt(i);
+            if((alpa<'a'||alpa>'z')&&(alpa<'A'||alpa>'Z')) return false;
+        }
+
+        return true;
+    }
+
+
 }
