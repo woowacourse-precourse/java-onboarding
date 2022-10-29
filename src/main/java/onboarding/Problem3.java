b38ffa031f6ab59578b8fc5ded3ab76178431b3c
kongji <kongji4092@gmail.com>
2022-10-29 20:24:50 +0900

    [프리코스] 3번 문제

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            String temp = Integer.toString(i);
+            char[] chr = temp.toCharArray();
+
+            for (char j: chr) {
+                if (j == '3' || j == '6' || j =='9') {
+                    answer += 1;
+                }
+            }
+        }
+
         return answer;
     }
 }
