acd08226491ed00c9eebd569960ec9bb8cadacc7
starwook <star.wookk@gmail.com>
2022-10-29 20:38:23 +0900

    add initList method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> moneyList = new ArrayList<>();
+        moneyList = moneyInitList(moneyList);
+        answer = initList(answer);
+
         return answer;
     }
+    public static List<Integer>  initList(List<Integer> list){
+        for(int i=0;i<9;i++){
+            list.add(0);
+        }
+        return list;
+    }
+    public static List<Integer> moneyInitList(List<Integer> list){
+        list = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
+        return list;
+    }
+   
+
 }
