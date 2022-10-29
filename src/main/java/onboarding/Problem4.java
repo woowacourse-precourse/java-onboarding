a6811d3c06a7729b6a64454e3ef784b5ce2af156
NoseSSG <dmlcks1998@naver.com>
2022-10-29 15:50:46 +0900

    4번 문제 완성 코드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
+    public static Character Reverse(char word){
+        int temp;
+        if(word >= 'a' && word<= 'z'){
+            temp = (int)word - 'a';
+            word = (char)((int)'z' - temp);
+        }
+        else if(word >= 'A' && word <= 'Z'){
+            temp = (int)word - 'A';
+            word = (char)((int)'Z' - temp);
+        }
+//        System.out.println(word);
+        return word;
+    }
+    public static String Reverse_Frog(String word){
         String answer = "";
+        for(int i = 0 ; i < word.length(); i++){
+            answer += Reverse(word.charAt(i));
+//            System.out.println(answer);
+        }
+        return answer;
+    }
+    public static String solution(String word) {
+        String answer = Reverse_Frog(word);
         return answer;
     }
 }
