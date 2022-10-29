d5eebd1e292c237dc97add4e6d7ea3fba8da8d78
chopinballadeno4 <qo98333@naver.com>
2022-10-29 10:54:23 +0900

    problem 3, function 1 count number method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+    public static int countNumber(int number) {
+        int count = 0;
+        int dividedNumber = number;
+        while(dividedNumber != 0) {
+            int digit = dividedNumber % 10;
+            if(digit == 3 | digit == 6 | digit == 9) {
+                count = count + 1;
+            }
+            dividedNumber = dividedNumber / 10;
+        }
+
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
