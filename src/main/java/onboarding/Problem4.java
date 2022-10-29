8f9a7076afb8240d4b671a6eb1d018b6c39c4f53
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-29 17:43:23 +0900

    feat: Problem4 word를 변경하기 위한 사전을 HashMap으로 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.HashMap;
+
 /**
  * [ ] 알파벳 외의 문자는 변환하지 않는다.
  * [ ] word를 청개구리 사전을 참고해 반대로 변환한다.
@@ -8,6 +10,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        return answer;
+        HashMap<Character, Character> treeFrogDict = replaceOtherCharDict();
+
+        return "";
+    }
+
+    public static HashMap replaceOtherCharDict() {
+        HashMap<Character, Character> treeFrogDict = new HashMap<>(26);
+
+        for (int i = 65; i <= 90; i++) {
+            char originalWord = (char) i;
+            char replaceWord = (char) (155 - i);
+
+            treeFrogDict.put(originalWord, replaceWord);
+        }
+
+        return treeFrogDict;
     }
 }
