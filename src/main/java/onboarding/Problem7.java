eb3109b372f98d0a6bbac99441e544cb7ce2cd78
minhee020 <minhee020@naver.com>
2022-10-29 13:15:51 +0900

    Problem7 | 친구의 친구 HashMap 구현 & score 합산

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -22,12 +22,58 @@ public class Problem7 {
         // user의 친구 목록 추가
         for (int i = 0; i < friends.size(); i++){
             if (friends.get(i).get(0).equals(user)){
-                userFriends.add(friends.get(i).get(0));
-            } else if (friends.get(i).get(1).equals(user)){
                 userFriends.add(friends.get(i).get(1));
+            } else if (friends.get(i).get(1).equals(user)){
+                userFriends.add(friends.get(i).get(0));
             }
         }
 
+        // 친구의 친구 목록 추가, score 합산
+        for (int i = 0; i < friends.size(); i++){
+            for (int j = 0; j < userFriends.size(); j++) {
+                if (friends.get(i).contains(user)) {
+                    continue;
+                }
+
+                if (friends.get(i).get(0).equals(userFriends.get(j))){
+                    if (nonFriends.containsKey(friends.get(i).get(1))){
+                        nonFriends.put(
+                                friends.get(i).get(1),
+                                nonFriends.get(friends.get(i).get(1)) + 10
+                        );
+                    } else {
+                        nonFriends.put(friends.get(i).get(1), 10);
+                    }
+                } else if (friends.get(i).get(1).equals(userFriends.get(j))){
+                    if (nonFriends.containsKey(friends.get(i).get(0))){
+                        nonFriends.put(
+                                friends.get(i).get(0),
+                                nonFriends.get(friends.get(i).get(0)) + 10
+                        );
+                    } else {
+                        nonFriends.put(friends.get(i).get(0), 10);
+                    }
+                }
+            }
+        }
+
+        System.out.println(userFriends);
+        System.out.println(nonFriends);
+
         return answer;
+    }
+
+    public static void main(String[] args){
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        solution(user, friends, visitors);
     }
 }
