0366688bca4a806b9d4ea8f9923f76175d120c93
yg0585 <yg0585@naver.com>
2022-10-30 00:19:24 +0900

    feat: 기능 2번 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -14,23 +14,27 @@ import java.util.List;
 public class Problem7 {
 
     private static HashMap<String, List<String>> friendsMap = new HashMap<>();
+    private static HashMap<String, Integer> score = new HashMap<>();
 
     private static void addFriends(List<String> strings) {
 
         List<String> flist;
         if (friendsMap.containsKey(strings.get(0)))
             flist = friendsMap.get(strings.get(0));
-        else
+        else {
             flist = new ArrayList<>();
+            score.put(strings.get(0), 0);
+        }
 
         flist.add(strings.get(1));
         friendsMap.put(strings.get(0), flist);
 
         if (friendsMap.containsKey(strings.get(1)))
             flist = friendsMap.get(strings.get(1));
-        else
+        else {
             flist = new ArrayList<>();
-
+            score.put(strings.get(1), 0);
+        }
         flist.add(strings.get(0));
         friendsMap.put(strings.get(1), flist);
     }
@@ -39,12 +43,34 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
 
         for (int i = 0; i < friends.size(); i++)
-        {
             addFriends(friends.get(i));
-        }
 
         System.out.println(friendsMap);
 
+        getFriendsScore(user);
+
+        System.out.println("score = " + score);
+
         return answer;
+    }
+
+    private static void getFriendsScore(String user) {
+        List<String> friendsList = friendsMap.get(user);
+
+        for (int i = 0; i < friendsList.size(); i++)
+            addFriendsScore(friendsList.get(i), user);
+    }
+
+    private static void addFriendsScore(String s, String user) {
+        List<String> friendsList = friendsMap.get(s);
+        List<String> userFriends = friendsMap.get(user);
+
+        for (int i = 0; i < friendsList.size(); i++) {
+            String name = friendsList.get(i);
+            if (name == "mrko" || userFriends.contains(name))
+                continue;
+            int sc = score.get(name);
+            score.put(name, sc + 10);
+        }
     }
 }
