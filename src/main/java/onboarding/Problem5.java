473c62cc08356c7b79c2da957eb439c0293f8b05
hoonn9 <hun9935@gmail.com>
2022-10-29 18:36:27 +0900

    Add getCountByUnit method (Problem5)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,12 @@ public class Problem5 {
     static final List<Integer> moneyList = Arrays.asList(50000, 10000, 5000, 500, 100, 50, 10);
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        System.out.println(getCountByUnit(money, moneyList.get(0)));
         return answer;
+    }
+
+    private static Integer getCountByUnit(int money, int unit) {
+        return money >= unit ? money / unit : 0;
     }
 }
