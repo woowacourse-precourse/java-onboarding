fc179e833c6fecba9cc42590d8f4fec3acb1b872
hoonn9 <hun9935@gmail.com>
2022-10-29 18:29:08 +0900

    Add money list (Problem5)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,11 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final List<Integer> moneyList = Arrays.asList(50000, 10000, 5000, 500, 100, 50, 10);
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
