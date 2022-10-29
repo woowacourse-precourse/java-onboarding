818a97810f90fce52e8098c356c2523510765ee4
Youngho <cyh6099@gmail.com>
2022-10-29 16:21:16 +0900

    feat(p6) : solution함수 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+    public static Queue<List<String>> nicknameEmailQ;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        nicknameEmailQ = new LinkedList<>();
+        for (List<String> form : forms) {
+            nicknameEmailQ.offer(form);
+        }
+        int qLength = nicknameEmailQ.size();
+
+        for (int i = 0; i < qLength; i++) {
+            List<String> checkNicknameEmail = nicknameEmailQ.poll();
+            String email = checkNicknameEmail.get(0);
+            List<String> nicknameUnits = separateNickname(checkNicknameEmail.get(1));
+
+            long count = nicknameEmailQ.stream()
+                    .map(x -> x.get(1))
+                    .filter(nickname -> isDuplicate(nickname, nicknameUnits))
+                    .count();
+            if (count > 0) {
+                answer.add(email);
+            }
+
+            nicknameEmailQ.add(checkNicknameEmail);
+        }
+        Collections.sort(answer);
+        answer.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
         return answer;
     }
 
@@ -19,7 +46,14 @@ public class Problem6 {
         return returnList;
     }
 
-    public static boolean isDuplicate(String nickname, String nicknameUnit) {
-        return nickname.matches(".*" + nicknameUnit + ".*");
+    public static boolean isDuplicate(String nickname, List<String> nicknameUnits) {
+        for (String nicknameUnit : nicknameUnits) {
+            if (nickname.matches(".*" + nicknameUnit + ".*")) {
+                return true;
+            }
+        }
+        return false;
+
+//        return ;
     }
 }
