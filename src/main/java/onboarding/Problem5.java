5df991d3e5acfee80d84249b67266abc4f213c16
chosunghyun18 <krismon@naver.com>
2022-10-29 23:00:27 +0900

    [5번] 기능 요구사항 리스트 1번 ver.1 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,10 +9,22 @@ import java.util.stream.Collectors;
 public class Problem5 {
     public static List<Integer> solution(int money) {
 
-
+        int[] result = calculateMoney(money);
+        List<Integer> answer = new ArrayList<>(Arrays.stream(result).boxed().collect(Collectors.toList()));
+        return answer;
     }
+    //ver1  greedy algorithm
     public static int[] calculateMoney(int money) {
-
+        int[] wont = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] result = new int[9];
+        while (money != 0) {
+            for (int i = 0; i < wont.length; i++) {
+                if (money < wont[i]) continue;
+                result[i] += (money / wont[i]);
+                money %= wont[i];
+            }
+        }
+        return result;
     }
 
 }
