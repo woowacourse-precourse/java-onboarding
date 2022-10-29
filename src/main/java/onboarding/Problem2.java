7950b83f1b3ff0c1e6f7cbea33e526922c31977b
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-29 21:05:42 +0900

    FEAT : Problem 2-3. 리스트를 탐색하며 연속하는 중복 문자들의 인덱스 값을 찾아내기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,7 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /*
  *
@@ -16,9 +17,10 @@ import java.util.List;
  *
  * 1. 암호문(cryptogram)을 입력받는다.
  * 2. 암호문(cryptogram)을 각 자리 별로 쪼개어 리스트에 저장시킨다.
- * 3. 리스트를 탐색하며 연속하는 중복 문자들을 삭제한다.
- * 4. 연속하는 중복된 문자들이 없을 때까지 3번을 반복한다.
- * 5. 연속하는 중복된 문자들이 없으면 결과를 출력한다.
+ * 3. 리스트를 탐색하며 연속하는 중복 문자들의 인덱스 값을 찾아낸다.
+ * 4. 리스트를 탐색하며 연속하는 중복 문자들을 삭제한다.
+ * 5. 연속하는 중복된 문자들이 없을 때까지 3번을 반복한다.
+ * 6. 연속하는 중복된 문자들이 없으면 결과를 출력한다.
  *
  * */
 
@@ -31,7 +33,15 @@ public class Problem2 {
         String[] cryptoWord = crypto.split("");
         ArrayList<String> cryptoList = new ArrayList<>(Arrays.asList(cryptoWord));
 
-        System.out.println(cryptoList);
+        // to find duplication words
+        List<Integer> duplicationList = new ArrayList<>();
+        for (int i = 1; i < cryptoList.size(); i++) {
+            if (Objects.equals(cryptoList.get(i - 1), cryptoList.get(i))) {
+                if (!duplicationList.contains(i - 1)){duplicationList.add(i - 1);}
+                if (!duplicationList.contains(i)){duplicationList.add(i);}
+            }
+        }
+        System.out.println(duplicationList);
         return answer;
     }
 }
