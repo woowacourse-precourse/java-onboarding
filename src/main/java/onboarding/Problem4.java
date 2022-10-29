d663b30790f83f427e246b0fb9d83d3011641b31
syahn <496300@naver.com>
2022-10-29 23:58:29 +0900

    feat: 문자열의 문자가 알파벳인지 확인하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+
+    //문자열의 문자가 알파벳인지 아닌지 확인
+    public static boolean isAlphabet(char c){
+        if((c-'a'>=0) & (c-'a'<26))
+            return true;
+        else if((c-'A'>=0) & (c-'Z' <26))
+            return true;
+        else
+            return false;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
