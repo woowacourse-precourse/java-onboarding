c817ac101aeb100e2086b1fd05220551507b7cdc
ghltjd369 <ghltjd369@gmail.com>
2022-10-29 17:24:44 +0900

    feat : (problem 3) 1부터 number까지의 모든 숫자를 하나의 문자열로 이어붙이는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String number_str = "";
+
+        for(int i = 1; i <= number; i++){
+            number_str += i;
+        }
+
         return answer;
     }
 }
