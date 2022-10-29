0889203cabf3b698a349a6ee6a8446eff8324225
qpdh <74190329+qpdh@users.noreply.github.com>
2022-10-29 11:36:31 +0900

    Update Problem6.java
    email.com 이 아닌 닉네임 필터링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeSet;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Problem6 {
     // createToken 길이가 2인 부분 문자열 생성하는 메소드
     public static List<String> createToken(String nickname) {
@@ -17,7 +19,11 @@ public class Problem6 {
     }
 
     public static List<String> solution(List<List<String>> forms) {
+        // TODO email.com 인 경우만 처리
 
+        // forms 필터링
+        forms = new ArrayList<>(forms);
+        forms.removeIf(n -> (!n.get(0).split("@")[1].equals("email.com")));
 
         TreeSet<String> treeSet = new TreeSet<>();
 
@@ -54,10 +60,16 @@ public class Problem6 {
     }
 
     public static void main(String[] args) {
-        List<String> list = createToken("제이엠");
-
-        for(String n : list){
-            System.out.println(n);
-        }
+        List<List<String>> forms = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "워니"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠"),
+                List.of("nowm@naver.com", "이제엠오")
+        );
+        List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
+        System.out.println(result);
+        assertThat(Problem6.solution(forms)).isEqualTo(result);
     }
 }
