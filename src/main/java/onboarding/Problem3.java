699b6a2b83405a67d034a670e99c4a4b6cb7b222
권민정 <22alswjd@naver.com>
2022-10-29 20:49:27 +0900

    feat: Problem3 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            int tmp = i;
+            while(tmp > 0) {
+                int tmp2 = tmp % 10;
+                if(tmp2 != 0 && (tmp2 % 3) == 0) {
+                    answer++;
+                }
+                tmp /= 10;
+            }
+        }
         return answer;
     }
 }
