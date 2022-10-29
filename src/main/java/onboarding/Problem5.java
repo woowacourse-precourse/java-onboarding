455b3ed4c829d7af5a90aa421812571f62365f05
hyejungg <kimhyejung12@naver.com>
2022-10-30 01:11:24 +0900

    feat: [문제 5] 금액별 단위 세는 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,6 +13,17 @@ public class Problem5 {
 		return money >= 1 && money <= 1000000;
 	}
 
-        return answer;
-    }
+	private static List<Integer> changeMoney(int money) {
+		List<Integer> answer = new ArrayList<>();
+		int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		int realMoney = money;
+
+		for (int u : unit) {
+			int cnt = realMoney / u;
+			realMoney %= u;
+			answer.add(cnt);
+		}
+
+		return answer;
+	}
 }
