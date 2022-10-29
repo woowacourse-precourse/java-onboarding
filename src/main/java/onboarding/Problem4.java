71a48b487881ecd90aa08d4368d0f042187729bb
Luka <meena2003@naver.com>
2022-10-29 18:03:00 +0900

    convertWords() 메서드 생성. Solution() 메서드 내에서 소문자, 대문자 처리 로직 완성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,18 +1,37 @@
 package onboarding;
 
 public class Problem4 {
+    public static void main(String[] args) {
+        
+    }
     public static String solution(String word) {
         // 1. 비교해서 변화하려는 코드표를 char 배열로 생성
         // 2-1. word를 매개변수로 받아서 char.At()을 이용해 char 배열의 원소들과 비교하는 메서드 생성
         // 2-2. 소문자와 대문자를 구분하기
         // 3. 반환값 출력
 
-        String answer = "";
-        return answer;
+        String sonWords = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            char preWord = word.charAt(i);
+
+            if (Character.isLowerCase(preWord)) {
+                preWord -= 32;
+                preWord = (char) (convertWords(preWord) + 32);
+                sonWords += preWord;
+            } else {
+                sonWords += convertWords(preWord);
+            }
+        }
+
+        return sonWords;
     }
 
-    public String convertWords(String word) {
-        char[] dictionaryOfWords = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    public static char convertWords(char preWord) {
+        char[] DictionaryOfWords = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M'
+                , 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+        int idx = preWord - 65;
+
+        return DictionaryOfWords[idx];
     }
 }
