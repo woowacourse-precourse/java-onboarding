13a3f10f71397cf35fc55b3547e1dbb799016251
idoburnish <doyun7433@naver.com>
2022-10-29 18:10:08 +0900

    feat(PROBLEM1): 페이지 번호의 각 자리수 더하는 기능
    페이지 번호를 받아, 각 자리수를 모두 더하는 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,4 +15,13 @@ class Problem1 {
         if (user.get(0) == 1 || user.get(1) == 400) return false;
         return true;
     }
+
+    public static int addPageDigits(int pageNum) {
+        int pageDigitsSum = 0;
+        while (pageNum > 0) {
+            pageDigitsSum += pageNum % 10;
+            pageNum /= 10;
+        }
+        return pageDigitsSum;
+    }
 }
