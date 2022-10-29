45b1618408418b38c66b4f93b3972e27f4e9184c
taekwon-dev <taekwon_dev@naver.com>
2022-10-29 16:21:36 +0900

    problem 5 - feat : 보유하고 있는 돈 단위 리스트 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,10 +1,13 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        List<Integer> wons = new ArrayList<>(Arrays.asList(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1));
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
