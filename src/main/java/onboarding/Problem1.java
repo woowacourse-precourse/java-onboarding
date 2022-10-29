635d1696f7b1efcec76bed789ef3661e9320ab6f
kimdoha <kkhhjj3393@gmail.com>
2022-10-29 16:25:02 +0900

    feat: 각 자릿수의 합 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+    private static int sumOfDigit(int num) {
+        int sum = 0;
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
