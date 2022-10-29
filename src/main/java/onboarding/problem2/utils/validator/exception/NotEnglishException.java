2121269a8190caaa008797ff66f1fe0110a165de
song-wooseok <sws981202@gmail.com>
2022-10-29 23:30:37 +0900

    remove: [PROBLEM2] 주어진 문자열이 영어인지 검사하는 예외 제거

--- src/main/java/onboarding/problem2/utils/validator/exception/NotEnglishException.java
+++
@@ -1,7 +0,0 @@
-package onboarding.problem2.utils.validator.exception;
-
-public class NotEnglishException extends Exception{
-    public NotEnglishException(String msg){
-        super(msg);
-    }
-}
