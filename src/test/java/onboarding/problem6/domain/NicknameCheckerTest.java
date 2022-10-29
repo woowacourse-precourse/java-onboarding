a29eb27b04bb2d8344cf6a9456421f309b2cd61e
Yunsik-Choi <choiys0311@gmail.com>
2022-10-30 03:38:07 +0900

    ⛑️ test : NicknameCheckerTest 수정 NicknameCheckerTest 수정

--- src/test/java/onboarding/problem6/domain/NicknameCheckerTest.java
+++ src/test/java/onboarding/problem6/domain/NicknameCheckerTest.java
@@ -21,10 +21,8 @@ public class NicknameCheckerTest {
     void save_nickname_after_check_duplicate() {
         String nickname = "제이제이";
         nicknameChecker.saveNickname(nickname);
-        assertThat(nicknameChecker.isDuplicate("제이")).isFalse();
-        assertThat(nicknameChecker.isDuplicate("이제")).isFalse();
+        assertThat(nicknameChecker.isDuplicate(nickname)).isFalse();
         nicknameChecker.saveNickname(nickname);
-        assertThat(nicknameChecker.isDuplicate("제이")).isTrue();
-        assertThat(nicknameChecker.isDuplicate("이제")).isTrue();
+        assertThat(nicknameChecker.isDuplicate(nickname)).isTrue();
     }
 }
