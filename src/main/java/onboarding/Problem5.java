ef79e2e6712e3de805299e03c45b640952ef30ef
HyeokJoon Kong <orijoon98@gmail.com>
2022-10-29 17:27:41 +0900

    feat: 문제5 - solution 함수 완성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,11 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = withdraw(money);
         return answer;
     }
 
