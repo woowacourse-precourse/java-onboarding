58524fa738bcf33dab656f31ddb2bad68561d81c
senor14 <dkssud940@gmail.com>
2022-10-29 21:23:22 +0900

    feat(problem6): 중복 닉네임 반환 로직 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,17 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 🚀 기능 요구 사항
- * 우아한테크코스에서는 교육생(이하 크루) 간 소통 시 닉네임을 사용한다. 간혹 비슷한 닉네임을 정하는 경우가 있는데, 이러할 경우 소통할 때 혼란을 불러일으킬 수 있다.
+ * 우아한테크코스에서는 교육생(이하 크루) 간 소통 시 닉네임을 사용한다.
+ * 간혹 비슷한 닉네임을 정하는 경우가 있는데, 이러할 경우 소통할 때 혼란을 불러일으킬 수 있다.
  *
- * 혼란을 막기 위해 크루들의 닉네임 중 같은 글자가 연속적으로 포함 될 경우 해당 닉네임 사용을 제한하려 한다. 이를 위해 같은 글자가 연속적으로 포함되는 닉네임을 신청한 크루들에게 알려주는 시스템을 만들려고 한다.
+ * 혼란을 막기 위해 크루들의 닉네임 중 같은 글자가 연속적으로 포함 될 경우 해당 닉네임 사용을 제한하려 한다.
+ * 이를 위해 같은 글자가 연속적으로 포함되는 닉네임을 신청한 크루들에게 알려주는 시스템을 만들려고 한다.
  *
- * 신청받은 닉네임 중 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록을 return 하도록 solution 메서드를 완성하라.
+ * 신청받은 닉네임 중 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록을 return 하도록
+ * solution 메서드를 완성하라.
  *
  * 제한사항
  * 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되어 있는 경우 중복으로 간주한다.
@@ -21,7 +24,26 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> answer = new TreeSet<>();
+        Map<String, Set<String>> consecutiveSubString = new HashMap<>();
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String nicknameSubString = nickname.substring(i, i + 2);
+                if (consecutiveSubString.containsKey(nicknameSubString)) {
+                    consecutiveSubString.get(nicknameSubString).add(email);
+                    continue;
+                }
+                consecutiveSubString.put(nicknameSubString, new HashSet<String>(Collections.singleton(email)));
+            }
+        }
+        for (String nicknameSubString : consecutiveSubString.keySet()) {
+            Set<String> duplicatedUsedEmailSet = consecutiveSubString.get(nicknameSubString);
+            if (duplicatedUsedEmailSet.size() > 1) {
+                answer.addAll(duplicatedUsedEmailSet);
+            }
+        }
+        return new ArrayList<>(answer);
     }
 }
