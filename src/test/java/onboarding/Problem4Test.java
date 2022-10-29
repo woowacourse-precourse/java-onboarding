2fba9f0b4fa070daa984ad290b8fa6ab5705730e
saehim <saeilee.vxv@gmail.com>
2022-10-30 00:14:12 +0900

    Test(Problem4): 소문자 반환 기능

--- src/test/java/onboarding/Problem4Test.java
+++ src/test/java/onboarding/Problem4Test.java
@@ -1,4 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 class Problem4Test {
   
+    // 소문자 변환
+    char lowercase(char a){
+        char answer = (char)(219 - a);
+        return answer;
+    }
 }
