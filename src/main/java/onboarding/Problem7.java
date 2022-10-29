3aa42d11b4e4ae9ef0d6abcbed64362aa3738183
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-10-30 01:41:53 +0900

    feat(7번): user 친구를 list로 담기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        ArrayList<String> Friends = new ArrayList<>();
+        answer = new ArrayList<String>();
+        Map<String, Integer> map = new TreeMap<>();
+        for (int k=0; k<friends.size(); k++){
+            if (friends.get(k).get(0) == user) {
+                Friends.add(friends.get(k).get(1));
+            }
+            if (friends.get(k).get(1) == user){
+                Friends.add(friends.get(k).get(0));
+            }
+        }
+        System.out.println(Friends);
+
+
+        System.out.println(map);
+        List<String> keySet = new ArrayList<>(map.keySet());
+
+        answer = keySet;
         return answer;
     }
 }
