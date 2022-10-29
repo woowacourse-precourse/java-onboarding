ba82a4dff441f2533ab1ca091e762f30ed171d6a
Park Sungchul <try615@naver.com>
2022-10-29 20:57:12 +0900

    [feat] Problem2.java : 코딩테스트 형식으로 프로토타입 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.*;
+import java.io.*;
+
 public class Problem2 {
+    public static void main(String[] args) {
+        String str = "browoanoommnaon";
+        System.out.println(solution(str));
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        stack.push('/');
+        int strLen = cryptogram.length();
+
+        for (int i = 0; i < strLen; i++) {
+            Character c = cryptogram.charAt(i);
+
+            if (stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int stackLen = stack.size();
+        for (int i = 1; i < stackLen; i++) {
+            System.out.println(sb.toString());
+        }
+
+        return sb.reverse().toString();
     }
 }
