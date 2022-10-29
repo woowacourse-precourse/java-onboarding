4c426fc8104cd5afaa1d12484633454da6c4351e
yewon-Noh <yewo2nn16@gmail.com>
2022-10-29 23:56:03 +0900

    Problem3 : 숫자의 (3,6,9) 포함 개수 체크

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,21 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * 해당 숫자의 (3,6,9) 포함 개수 체크
+     * @param i
+     * @return
+     */
+    private static int checkTable(int i) {
+        int check = 0;
+        char[] table = {'3','6','9'};
+
+        for (char x: Integer.toString(i).toCharArray()) {
+            for (char tb : table) {
+                if (x == tb) check++;
+            }
+        }
+        return check;
+    }
 }
