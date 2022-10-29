09b653c34a85e984188c418306e5e41bd72e73be
hyowonhan <wain1719@gmail.com>
2022-10-29 17:26:03 +0900

    문제2 solution 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
-
+import java.util.Scanner;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        while(true) {
+            for(int i=0; i<cryptogram.length()-1; i++) {
+                if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                    cryptogram = cryptogram.replace(cryptogram.substring(i,i+2), "");
+                }
+            }
+            answer = cryptogram;
+
+            int check = 0;
+            for(int i = 0; i<cryptogram.length()-1; i++) {
+                if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                    check += 1;
+                }
+            }
+            if(check ==0) {
+                break;
+            }
+        }
         return answer;
     }
 
