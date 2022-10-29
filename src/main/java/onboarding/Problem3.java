9155757af748a979267c3ed80d5c1357653902fe
조미정 <ing06164@naver.com>
2022-10-29 21:59:53 +0900

    문제 3 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,23 +2,28 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = 0;      // 369횟수
 
-        int digit=0;   // 자릿수
-
-        while (number > 0) {
-            //answer += (number%10);
-            if(number%10 == 3){
-                answer *= Math.pow(10, digit);
-            }
-            answer += (number/3);
-
-            number = number / 10;
-
-            digit++;
+        for(int i=1; i<=number; i++) {
+            answer += get369(i);
         }
         System.out.println("369횟수: "+answer);
 
         return answer;
+    }
+    static int get369(int number){
+        int digit;           // 각 자리의 값
+        int count = 0;       // 자릿수
+        while (number > 0) {
+            digit = (number%10);
+
+            // digit이 0인 경우도 3, 6, 9로 나눴을 때 나머지가 0이므로 0이 포함되지 않도록 제외!
+            if(digit != 0 && (digit%3 == 0 || digit%6 == 0 || digit%9 == 0))
+                count++;
+
+            number = number / 10;
+
+        }
+        return count;
     }
 }
