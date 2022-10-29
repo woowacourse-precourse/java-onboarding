8e9073f01e05639fe3b2d16802fecacd22684cf8
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-30 01:29:30 +0900

    Problem4 기능요구사항 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+    // TODO 문자열을 변환하는 메서드
+    public static String convert(String str) {
+        return null;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
