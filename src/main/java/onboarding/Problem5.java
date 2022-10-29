a2046cf3aa24c55996ea4b27e9ae1682006e7ed2
seheonCho <whtpgjs94@gmail.com>
2022-10-30 01:50:53 +0900

    feat: [문제5] 화폐 종류 선언(내림차순)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int[] CURRENCYS_DESCENDING_ORDER = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
