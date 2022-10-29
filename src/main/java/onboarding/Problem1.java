987f9b2238da28de65679a40806233d3a705074c
chkim <cndghks0213@gmail.com>
2022-10-30 05:12:06 +0900

    feat(problem1): 입력이 잘못된 경우 -1을 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (exceptional(pobi, crong)) {
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean exceptional(List<Integer> pobi, List<Integer> crong) {
+        return !PageValidator.validate(pobi) || !PageValidator.validate(crong);
     }
 }
