c0cf793876e95e88a7929593e350bfe73dd7661f
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-29 21:36:48 +0900

    feat(Problem4): 대문자를 반대로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,8 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static String convertUpperCaseLetterReverse(char letter){
+        return Character.toString((char)(155-letter));
+    }
 }
