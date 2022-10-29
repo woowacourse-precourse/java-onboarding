0e708a059b96d772fd04e8041b4345bfcab96bfe
DaWoon Lee <qkgk61@gmail.com>
2022-10-29 22:35:45 +0900

    Feat : Add Translator class
    문자열 변환을 위한 싱글톤 클래스 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+}
+
+class Translator {
+
+    private static class TranslatorHolder {
+        private static final Translator INSTANCE = new Translator();
+    }
+
+    private Translator() {
+    }
+
+    public static Translator getInstance() {
+        return TranslatorHolder.INSTANCE;
+    }
+
 }
