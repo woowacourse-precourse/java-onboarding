f67114d0365d7b747d1a6a9c12a141144d8cb497
soyunim <togksk123@naver.com>
2022-10-30 02:52:38 +0900

    Problem7 2)find friend's friend and add the score 10

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,6 +10,9 @@ public class Problem7 {
         List<String> listName = new ArrayList<>();
         List<Integer> listScore = new ArrayList<>();
 
+        int idx;
+        int temp;
+
         // Who is my friend?
         for(int i=0; i<friends.size(); i++){
             if(friends.get(i).get(0).equals(user)){
@@ -24,6 +27,23 @@ public class Problem7 {
             }
             else{
                 theirFriend.add(friends.get(i));
+            }
+        }
+
+        // if my friend's friend, add score
+        for(int i=0; i<theirFriend.size(); i++){
+            for(int j=0; j<myFriend.size(); j++){
+                if(theirFriend.get(i).get(0).equals(myFriend.get(j))){
+                    if(listName.contains(theirFriend.get(i).get(1))){
+                        idx=listName.indexOf(theirFriend.get(i).get(1));
+                        temp=listScore.get(i)+10;
+                        listScore.set(idx,temp);
+                    }
+                    else{
+                        listName.add(theirFriend.get(i).get(1));
+                        listScore.add(10);
+                    }
+                }
             }
         }
 
