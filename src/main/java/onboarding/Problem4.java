391e3ba3a43ab19ffde6a4a86675bb36b7a4c72f
heejeong <hjx66@naver.com>
2022-10-29 23:02:08 +0900

    refactor: 대소문자 구분 메소드로 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,25 +1,30 @@
 package onboarding;
 
 public class Problem4 {
+    public static char getLetterCase(char letter){
+        //대문자일때
+        if(letter >= 'A' && letter <= 'Z'){
+            return 'A';
+        }
+        //소문자일때
+        if(letter >= 'a' && letter <= 'z'){
+            return 'a';
+        }
+        //알파벳 아닐 때
+        return 0;
+    }
     public static String solution(String word) {
         String answer = "";
-        char letter;
         for(int i = 0; i < word.length();i++){
-            char c = word.charAt(i);
-            //대문자일때
-            if(c >= 'A' && c <= 'Z')
-                letter = 'A';
-                //소문자일때
-            else if(c >= 'a' && c <= 'z')
-                letter = 'a';
-                //알파벳 아닐 때
-            else{
-                answer += c;
+            char letter = word.charAt(i);
+            char letterCase = getLetterCase(letter);
+            if(letterCase==0) {
+                answer += letter;
                 continue;
             }
             //알파벳 반대로 변환
-            //25-(c-letter)+letter
-            answer += (char)(25-c+2*letter);
+            //25-(letter-letterCase)+letterCase
+            answer += (char)(25-letter+2*letterCase);
         }
         return answer;
     }
