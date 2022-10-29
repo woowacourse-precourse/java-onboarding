7fa079ea037a21a762af458fd8278dd754dbfc56
Qulip <ilkwon.ryu@gmail.com>
2022-10-30 07:18:51 +0900

    feat : Problem4 청개구리 사전 적용 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,12 +10,30 @@ public class Problem4 {
             illegalStateException.printStackTrace();
         }
 
+        answer = applyTreeFrogDict(word);
+
         return answer;
     }
 
     public static void isValidWord(String word) throws IllegalStateException {
         if (word.length() < 1 || word.length() > 1000) {
             throw new IllegalStateException("word의 길이가 올바르지 않습니다.");
+        }
+    }
+
+    public static String applyTreeFrogDict(String word) {
+        StringBuilder result = new StringBuilder();
+        word.chars().map(Problem4::treeFrogDict).forEach(chars -> result.append((char)chars));
+        return result.toString();
+    }
+
+    public static int treeFrogDict(int c) {
+        if (c == ' ')
+            return c;
+        if (c >= 'a') {
+            return 'z' - (c - 'a');
+        } else {
+            return 'Z' - (c - 'A');
         }
     }
 }
