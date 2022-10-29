435190dab0dbcfbf05540c0837e5bb133cdb0c74
Seiyon Park <seiyonpdev@gmail.com>
2022-10-29 16:02:03 +0900

    feat: ADD method breakNum

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem3 {
     public static int solution(int number) {
+
         int answer = 0;
+
         return answer;
     }
+
+    public static List<Integer> breakNum(Integer page) {
+
+        List<Integer> broken = new ArrayList<>();
+
+        while(page > 0) {
+            broken.add(page % 10);
+            page /= 10;
+        }
+
+        return broken;
+    }
+
 }
