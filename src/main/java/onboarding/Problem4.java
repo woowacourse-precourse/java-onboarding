7fe0afa9c964b3e4ed3cc7ef5ac558c3fb3d93c7
namtndus <rnrmf0721@gmail.com>
2022-10-29 10:38:38 +0900

    feat:기능 목록 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        final int num = 25;
+        char[] words = word.toCharArray();
+
+        for(int i = 0; i< words.length; i++){
+            if(64<words[i] && words[i]<91){
+                if(64<words[i] && words[i] <= 78){
+                    words[i] = (char) (words[i]+(num - ((words[i] + num) - 90) * 2));
+                }else{
+                    words[i] = (char) (words[i] - (num - (65 - (words[i] - num)) * 2));
+                }
+            }else if(96<words[i] && words[i]<123){
+                if(96<words[i] && words[i] <= 109){
+                    words[i] = (char) (words[i]+(num - ((words[i] + num) - 122) * 2));
+                }else{
+                    words[i] = (char) (words[i] - (num - (97 - (words[i] - num)) * 2));
+                }
+            }
+        }
+        
+        return String.valueOf(words);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("I love you"));
     }
 }
