0ae09b0fb1ffd7f7f793831814b2e4db6ff2a42c
yejin <kwonyj1022@naver.com>
2022-10-30 02:12:25 +0900

    change 메서드 : 알파벳을 청개구리 사전에 따라 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -11,4 +11,19 @@ public class Problem4 {
         String answer = String.valueOf(alphabet);   // char 배열을 다시 String 배열로 만듦
         return answer;
     }
+
+    public static char change(char c) { // 알파벳을 청개구리 사전에 따라 변환
+        int ascii = (int)c;
+
+        // 변환할 알파벳의 아스키 코드와 변환 후 알파벳의 아스키 코드 합이 같음을 이용
+        if ((ascii >= 65) && (ascii <= 90)) { // c가 대문자라면
+            ascii = 155 - ascii;
+            return (char)ascii;
+        }
+        if ((ascii >= 97) && (ascii <= 122)) { // c가 소문자라면
+            ascii = 219 - ascii;
+            return (char) ascii;
+        }
+        return c;
+    }
 }
