6e7605d10d58f7f66a5675c005360240a0729215
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2022-10-29 18:42:51 +0900

    problem3: 1부터 특정숫자까지 이어 문자열로 만드는 함수 구현(linkNums)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,14 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    static String linkNums(int num) {
+        String result = "";
+        for (int i=1;i<=num;i++) {
+            result += String.valueOf(i);
+        }
+        return result;
+    }
+    public static void main(String args[]) {
+        System.out.println(linkNums(13));
+    }
 }
