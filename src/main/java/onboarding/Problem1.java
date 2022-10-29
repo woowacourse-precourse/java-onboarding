ce4f4fdaf871e973a6a7b39ecbb4a7475e74e629
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-10-30 00:36:43 +0900

    feat(1주차): pobi와 crong 변수 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,13 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobi_score_sum = getSum(pobi);
+        int pobi_score_mul= getMul(pobi);
+        int pobi_score = max(pobi_score_mul, pobi_score_sum);
+        int crong_score_sum = getSum(crong);
+        int crong_score_mul = getMul(crong);
+        int crong_score = max(crong_score_mul, crong_score_sum);
         return answer;
     }
 }
