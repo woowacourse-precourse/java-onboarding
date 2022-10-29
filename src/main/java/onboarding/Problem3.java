f230472922feef4ca7fbffd12d9de1c49733b077
opuopuo <pureuml.dev@gmail.com>
2022-10-29 21:16:01 +0900

    feat: Problem3 기능 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,15 +2,33 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
         if (!verifyException(number)) {
             return -1;
         }
+        return countClap(number);
+    }
 
+    private static int countClap(int number) {
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String stringNumber = Integer.toString(i);
+            String[] splitNumber = stringNumber.split("");
+
+            for (int j = 0; j < splitNumber.length; j++) {
+                if (isClap(splitNumber, j)) {
+                    answer += 1;
+                }
+            }
+        }
         return answer;
     }
 
-    public static boolean verifyException(int number) {
+    private static boolean isClap(String[] splitNumber, int j) {
+        return (splitNumber[j].contains("3") || splitNumber[j].contains("6") || splitNumber[j].contains("9"));
+    }
+
+    private static boolean verifyException(int number) {
         return (number >= 1 && number <= 10000);
     }
 }
