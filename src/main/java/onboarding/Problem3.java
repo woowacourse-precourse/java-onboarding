72779c97728918bbba9982b8d631ad1336735c50
GraphTree <graphnodesed@gmail.com>
2022-10-29 09:46:06 +0900

    Feat: 주어진 값이 3,6,9 중 하나일 때 1 반환 하는 clap 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+
+    public static int clap(int value) {
+        if (value == 3 || value == 6 || value == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
