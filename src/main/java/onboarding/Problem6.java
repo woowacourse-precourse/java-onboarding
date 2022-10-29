c72675cfb58ea673780c47b01610eae2f73f5947
Hayoon <hy97@sju.ac.kr>
2022-10-29 18:23:43 +0900

    feat: 기준이름 추출 메서드(isStandardName) 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,55 +6,83 @@ import java.util.List;
 
 public class Problem6 {
     static class Email {
+
         List<String> forms;
 
-        public Email(List<List<String>> forms, int[] checkList) {
+        Email(List<List<String>> forms, int[] checkList) {
             this.forms = new ArrayList<>(forms.size());
+
             for (int i = 0; i < checkList.length; i++) {
                 if (checkList[i] == 1) {
-                    System.out.println(forms.get(i).get(0));
                     this.forms.add(forms.get(i).get(0));
                 }
             }
         }
-        List<String> sortEmails(List<String> forms) {
-            String[] arr = forms.toArray(new String[forms.size()]);
+
+        private List<String> sortEmails(List<String> forms) {
+            String[] arr = forms.toArray(new String[0]);
             Arrays.sort(arr);
+
             return Arrays.asList(arr);
         }
     }
-    static class CheckList {
-        int[] checkName;
 
-        public CheckList(List<List<String>> forms) {
-            checkName = new int[forms.size()];
-            for (int i = 0; i < forms.size(); i++)
+    static class CheckList {
+
+        int[] checkName;
+        int formsLength;
+        CheckList(List<List<String>> forms) {
+            this.formsLength = forms.size();
+            checkName = new int[formsLength];
+
+            for (int i = 0; i < forms.size(); i++) {
                 checkName[i] = 0;
+            }
         }
 
-        int[] checkingList(List<List<String>> forms) {
+        private int[] checkingList(List<List<String>> forms) {
             String compName = "";
-            int flag = 0;
-            for (int i = 0; i < forms.size(); i++) {
-                if (checkName[i] == 0) {
-                    compName = forms.get(i).get(1);
-                } else continue;
-                for (int j = 1; j < forms.size(); j++) {
-                    if (isDupChar(forms, compName, j)
-                            && checkName[j] == 0 && i != j) {
-                        checkName[j] = 1;
-                        flag = 1;
-                    }
-                }
-                if (flag == 1) checkName[i] = 1;
-                flag = 0;
+
+            for (int i = 0; i < formsLength; i++) {
+                compName = isStandardName(i, forms, compName);
+                boolean flag = isComparisonName(i, forms, compName);
+                checkFlag(flag, i);
             }
 
             return checkName;
         }
 
+        private String isStandardName(int i, List<List<String>> forms, String compName) {
+            if (checkName[i] == 0) {
+                compName = forms.get(i).get(1);
+            }
+
+            return compName;
+        }
+
+        private boolean isComparisonName(int i, List<List<String>> forms, String compName) {
+            boolean flag = false;
+
+            for (int j = i; j < formsLength; j++) {
+                if (isDupChar(forms, compName, j)
+                        && checkName[j] == 0 && i != j) {
+                    checkName[j] = 1;
+                    flag = true;
+                }
+            }
+
+            return flag;
+        }
+
+        private void checkFlag(boolean flag, int i) {
+            if (flag) {
+                checkName[i] = 1;
+                System.out.println(Arrays.toString(checkName));
+            }
+        }
+
         private boolean isDupChar(List<List<String>> forms, String compName, int j) {
-            /***
+            /**
              * [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"],
              * ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]
              */
@@ -62,34 +90,26 @@ public class Problem6 {
                 for (int n = 0; n < compName.length() - 1; n++) {
                     String compNameSlicing = compName.substring(n, n + 2);
                     String formsNameSlicing = forms.get(j).get(1).substring(m, m + 2);
-                    if (compNameSlicing.equals(formsNameSlicing))
+
+                    if (compNameSlicing.equals(formsNameSlicing)) {
                         return true;
+                    }
                 }
             }
+
             return false;
         }
     }
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         CheckList checkList = new CheckList(forms);
-        int[] ints = checkList.checkingList(forms);
-        System.out.println(Arrays.toString(ints));
-        Email email = new Email(forms, ints);
+
+        Email email = new Email(forms, checkList.checkingList(forms));
         answer = email.sortEmails(email.forms);
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        List<List<String>> forms = List.of(
-                List.of("jm@email.com", "제이엠"),
-                List.of("jason@email.com", "제이슨"),
-                List.of("woniee@email.com", "워니"),
-                List.of("mj@email.com", "엠제이"),
-                List.of("nowm@email.com", "이제엠")
-        );
-        solution(forms);
     }
 }
 
