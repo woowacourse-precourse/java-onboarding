6f61edcca78079f8ffa57c508946b937db121a79
Invidam <hansupark0208@gmail.com>
2022-10-29 17:24:46 +0900

    feat(Problem3): add method check one digit num is clap case

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
+
+    public static boolean isClapCase(int oneDigitNum) {
+        int[] clapCases = {3, 6, 9};
+
+        for (int clapCase : clapCases) {
+            if (oneDigitNum == clapCase) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
