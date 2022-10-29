ccc6e5e82a6bf0ab76931d1edb22035246e00348
채우석 <wochae@wochae-MacBookAir.local>
2022-10-29 23:53:30 +0900

    [fix] 2.2 요소 삭제 후 순번이 앞 순번을 가리키도록 인덱스 감소값 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,11 +14,11 @@ public class Problem2 {
     }
 
     private static void iterateList(List<String> list) {
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size() - 1; i++) {
             if (list.get(i).equals(list.get(i + 1))) {
                 list.remove(i + 1);
                 list.remove(i);
-                i--;
+                i -= 2;
             }
         }
     }
