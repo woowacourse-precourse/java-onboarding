31ea85a8c3bc5596b0f81ddba9045118f8cff8e2
Hyeonjae-K <href.hjk@gmail.com>
2022-10-29 16:01:58 +0900

    add: 기능 목록 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,11 @@ package onboarding;
 
 import java.util.List;
 
+/* 기능 목록
+ * 두 이름이 중복인지 검사하는 기능
+ * 중복되지 않은 것 끼리 검사하는 기능
+ * */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
