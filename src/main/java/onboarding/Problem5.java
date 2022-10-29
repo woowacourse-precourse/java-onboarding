5cd8b3ddf0af10b868692b164796e0ff11fe7f5e
sen <seeunchoi99@gmail.com>
2022-10-29 22:21:33 +0900

    ✨ Problem5.getMoneyWithBill 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,8 +10,26 @@ import java.util.List;
  */
 
 public class Problem5 {
+    private static final List<Integer> bills = List.of(
+            50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
+    );
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    /**
+     * 현재 지폐를 이용해 변환할 수 있는 가장 큰 금액 구하기
+     *
+     * @param money
+     * @param index
+     * @return bills[index]으로 바꿀 수 있는 돈 액수
+     */
+    private static int getMoneyWithBill(int money, int index) {
+        if (money >= bills.get(index)) {
+            return money / bills.get(index);
+        }
+        return 0;
     }
 }
