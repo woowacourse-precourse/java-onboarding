63b0b27ea5de26758dac6435dee8439a802d0f88
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-29 23:45:49 +0900

    refactor:2번 문제 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,72 +8,63 @@ public class Problem2 {
             cryptogram); //delete를 사용하여 중복 문자열을 제거하기 위하여 StringBuffer 사용
 
         int index = 0;
-        int flag=0; // 0 이면 중복 걸러내기 한번이라도 해서 다시 체크해야됨, 1 이면 아예 안둘러봤으므로 종료해도됨.
-        int end_index= result.length();
-        while (true) {
+        int check_duplicate = 0;
+        int end_index = result.length();
+        int cutSize = 0;
 
-            if(index>=end_index-1 && flag==0){
-                index=0;
-                flag=1;
+        while (true) {
+            if (isCheckDuplicate(index >= end_index - 1, check_duplicate, 0)) {
+                index = 0;
+                check_duplicate = 1;
             }
 
-             if((index>=end_index-1 || end_index==1) && flag==1){
+            if (isCheckDuplicate((index >= end_index - 1 || end_index == 1), check_duplicate, 1)) {
                 break;
-             }
+            }
 
-            System.out.println("index = " + index);
-            System.out.println("end_index = " + end_index);
-             if(result.charAt(index)==result.charAt(index+1)){
+            if (isContinuosSame(result, index)) {
+                check_duplicate = 0;
+                for (int i = index + 1; i < end_index; i++) {
 
-                 flag=0;
-                 for(int i=index+1; i<end_index; i++){
-                     if(result.charAt(index) != result.charAt(i)){
+                    if (findOtherValue(result, index, i)) {
+                        result.delete(index, i);
+                        end_index = result.length();
+                        cutSize = i - index;
+                        index = i - cutSize;
+                        break;
+                    } else if (isSameFinalIndex(i, end_index - 1)) {
+                        result.delete(index, i + 1);
+                        index = 0;
+                        end_index = result.length();
+                    }
+                }
+            } else {
 
-                         result.delete(index, i);
-                         System.out.println("result = " + result);
-                         end_index= result.length();
-                         int size= i-index;
-                         index=i-size;
-                         break;
+                index++;
 
-                     }
-                     else if(i==end_index-1)
-                     {
-                         result.delete(index, i+1);
-                         System.out.println("result = " + result);
-                         index=0;
-                         end_index=result.length();
-                     }
-
-                 }
-             }
-             else{
-
-                 index++;
-             }
+            }
 
         }
 
         answer = String.valueOf(result);
-        System.out.println("answer = " + answer);
         return answer;
     }
 
-   /* private static void deDuplication(StringBuffer result, int index) {
-        if (result.length() == 2) {
-            result.delete(index, index + 2);
-        } else if (result.length() > 2) {
-         for(int i=index; ; i++){
-             char now_index= result.charAt(i);
-             if(now_index != )
+    private static boolean isSameFinalIndex(int i, int end_index) {
+        return i == end_index;
+    }
+
+    private static boolean findOtherValue(StringBuffer result, int index, int i) {
+        return result.charAt(index) != result.charAt(i);
+    }
+
+    private static boolean isContinuosSame(StringBuffer result, int index) {
+        return result.charAt(index) == result.charAt(index + 1);
+    }
+
+    private static boolean isCheckDuplicate(boolean condition, int flag, int x) {
+        return condition && flag == x;
+    }
 
 
-         }
-
-
-
-
-        }
-        return;
-    }*/
 }
