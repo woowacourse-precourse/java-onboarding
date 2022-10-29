91ff3d50b56196bbfacb35faf2c0519a0601fab7
opuopuo <pureuml.dev@gmail.com>
2022-10-29 18:00:50 +0900

    feat: 암호 해독 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,20 +3,59 @@ package onboarding;
 public class Problem2 {
         private static final int MIN_LENGTH = 1;
         private static final int MAX_LENGTH = 1000;
+
     public static String solution(String cryptogram) {
         String errorMessage = null;
 
         if ((errorMessage = verifyException(cryptogram)) != null) {
             return errorMessage;
         }
-        String answer = "answer";
-        return answer;
+        return decodeCryptogram(cryptogram);
+    }
+
+    private static String decodeCryptogram(String cryptogram) {
+        String newAnswer = "";
+
+        while(true) {
+            newAnswer = removeDuplicate(cryptogram);
+            if (newAnswer.equals(cryptogram))
+                break;
+            cryptogram = newAnswer;
+        }
+        return newAnswer;
+    }
+
+    private static String removeDuplicate(String cryptogram) {
+        int endIndex;
+        String result = "";
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char target = cryptogram.charAt(i);
+
+            if ((endIndex = findDuplicateIndex(cryptogram, i + 1, target)) != i) {
+                i += (endIndex - i);
+            } else {
+                result += Character.toString(target);
+            }
+        }
+        return result;
+    }
+
+    private static int findDuplicateIndex(String cryptogram, int endIndex, char c) {
+        while (endIndex < cryptogram.length() && c == cryptogram.charAt(endIndex)) {
+            endIndex++;
+        }
+        return endIndex - 1;
     }
 
     private static String verifyException(String cryptogram) {
         String result = null;
 
         /* [TODO] 에러 메세지 enum으로 ..? 어떻게 변경할지 고려해보자 */
+        if (cryptogram == null) {
+            result = "Error: Don't allow null string";
+            return result;
+        }
         if (!isLowerCase(cryptogram))
             result = "Error: Must be lowercase";
         if (result == null && !checkLength(cryptogram)) {
