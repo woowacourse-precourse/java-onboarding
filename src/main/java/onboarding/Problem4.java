59b807a9d2027e2502c9d9a09fad4d24e833be51
GraphTree <graphnodesed@gmail.com>
2022-10-29 09:57:21 +0900

    Feat: 주어진 스트링을 요구사항에 따라 변환 시키는 converter 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,10 +8,35 @@ import java.util.Set;
 public class Problem4 {
     public static String solution(String word) {
 
-        String answer = "";
+        String answer = converter(word);
         return answer;
     }
 
+    public static String converter(String value) {
+        String result = "";
+        Map<String, String> map = createDictionary();
+        Set<String> alphSet = new HashSet<>();
+        String alph = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+
+        for (int i = 0; i < alph.length(); i++) {
+            alphSet.add(String.valueOf(alph.charAt(i)));
+        }
+
+
+
+        for (int i = 0; i < value.length(); i++) {
+            String letter = String.valueOf(value.charAt(i));
+            if (!alphSet.contains(letter)) {
+                result += letter;
+            } else {
+                String convertedLetter = map.get(letter);
+                result += convertedLetter;
+            }
+
+
+        }
+        return result;
+    }
 
     public static Map<String, String> createDictionary() {
         Map<String, String> map = new HashMap<String, String>();
