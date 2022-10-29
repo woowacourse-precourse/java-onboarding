d51d6a565d8c2dd7d459c0554d4483aedf1e9ac0
Bumsoo <rhqjatn2398@naver.com>
2022-10-29 22:48:04 +0900

    feat(Problem4): 청개구리 사전을 대소문자 모두 포함하여 HashMap으로 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    static final HashMap<Character, Character> dict = new HashMap<>();
     public static String solution(String word) {
         String answer = "";
+        initDict();
         return answer;
+    }
+
+    private static void initDict() {
+        for (int i = 0; i < 26; i++) {
+            dict.put((char) ('A' + i), (char) ('Z' - i));
+        }
+
+        for (int i = 0; i < 26; i++) {
+            dict.put((char) ('a' + i), (char) ('z' - i));
+        }
     }
 }
