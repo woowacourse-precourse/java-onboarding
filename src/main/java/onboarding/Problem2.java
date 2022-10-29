bc89f1989d9de7b617f88c4fc014f2f313b74cf3
HiiWee <wpdlzhf159@gmail.com>
2022-10-29 21:11:38 +0900

    Feat problem2: 인덱스는 1 ~ 문자열의 길이 - 1까지 돌며 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         StringBuilder cryptogramBuilder = new StringBuilder(cryptogram);
 
+        boolean checkConversion = false;
+        while (!checkConversion) {
+            checkConversion = decode(cryptogramBuilder);
+        }
         return "";
+    }
+
+    public static boolean decode(StringBuilder encodedCode) {
+        int count = 1;
+        for (int i = 1; i < encodedCode.length(); i++) {
+
+        }
+        return false;
     }
 }
