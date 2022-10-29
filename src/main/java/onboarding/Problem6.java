920366563dfd48b8b9ebe52c3a2e2f839d04eeeb
hoonn9 <hun9935@gmail.com>
2022-10-29 21:28:45 +0900

    Add sliceByLength method (Problem6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        System.out.println(sliceByLength("answer", 2));
         return answer;
     }
+
+    private static List<String> sliceByLength(String str, Integer len) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i <= str.length() - len; i++) {
+            result.add(str.substring(i, i + len));
+        }
+
+        return result;
+    }
 }
+
