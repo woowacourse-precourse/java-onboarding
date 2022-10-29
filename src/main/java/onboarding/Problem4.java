820f6422304b0f1f008e5415daee0e9dc5b9f61e
wonjunYou <youwjune@gmail.com>
2022-10-30 01:37:50 +0900

    feat : (Problem 4) 알파벳 변환을 위한 배열 생성 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static final int MAX_WORD_LENGTH = 1000;
+
+    public static final int COUNTS_OF_ALPHABET = 26;
+    public static final int ASCII_CODE_OF_LAST_UPPERCASE_ALPHABET = 90;
+    public static final int ASCII_CODE_OF_LAST_LOWERCASE_ALPHABET = 122;
+
+    private static final List<Character> UPPERCASE_LETTERS = new ArrayList<>();
+    private static final List<Character> LOWERCASE_LETTERS = new ArrayList<>();
 
     public static String solution(String word) {
         try {
@@ -9,11 +19,30 @@ public class Problem4 {
         } catch (IllegalArgumentException e) {
             return e.toString();
         }
+
+        initLetters();
     }
 
     public static void validateLength(String word) {
         if (word.length() < 1 || word.length() > MAX_WORD_LENGTH) {
             throw new IllegalArgumentException("허용되지 않은 길이의 문자열입니다.");
+        }
+    }
+
+    private static void initLetters() {
+        initUppercaseLetters();
+        initLowercaseLetters();
+    }
+
+    private static void initUppercaseLetters() {
+        for (int index = 0; index < COUNTS_OF_ALPHABET; index++) {
+            UPPERCASE_LETTERS.add((char) (ASCII_CODE_OF_LAST_UPPERCASE_ALPHABET - index));
+        }
+    }
+
+    private static void initLowercaseLetters() {
+        for (int index = 0; index < COUNTS_OF_ALPHABET; index++) {
+            LOWERCASE_LETTERS.add((char) (ASCII_CODE_OF_LAST_LOWERCASE_ALPHABET - index));
         }
     }
 }
