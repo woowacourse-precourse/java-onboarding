dc1f78686dc618597042d7ba4b6a725e77014a03
KJE <12181501@inha.edu>
2022-10-29 20:39:25 +0900

    solve problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char tmp;
+        for(int idx = 0; idx<word.length();idx++){
+            //대문자
+            if(word.charAt(idx) >64 && word.charAt(idx)<91){
+                tmp = (char)(155- word.charAt(idx));
+                answer+=tmp;
+            }
+
+            //소문자
+            else if(word.charAt(idx) >96 && word.charAt(idx)<123){
+                tmp = (char)(219- word.charAt(idx));
+                answer+=tmp;
+            }
+
+            else
+                answer+=" ";
+        }
         return answer;
     }
 }
