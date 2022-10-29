8eb069a92080b9b0845602927847f750fc427740
ksh-beginner <ohk9134@naver.com>
2022-10-29 22:44:48 +0900

    feat : 하드 코딩했던 것 상수로 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,24 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
 
+    private static final int CREW_EMAIL_INDEX = 0;
+    private static final int CREW_NICKNAME_INDEX = 1;
+    private static final int SUBSTRING_START_INDEX = 0;
+    private static final String EXCEPTION_MESSAGE_PREFIX = "[ERROR] : ";
+    private static final String WRONG_NUMBER_OF_CREW_EXCEPTION_MESSAGE = "크루는 1명 이상 10,000명 이하여야합니다.";
+    private static final String WRONG_CREW_EMAIL_TYPE_EXCEPTION_MESSAGE = "이메일 형식에 부합해야 하며, 도메인은 email.com이어야합니다.";
+    private static final String WRONG_CREW_EMAIL_LENGTH_EXCEPTION_MESSAGE = "이메일 길이는 11자 이상 20자 미만이어야합니다.";
+    private static final String WRONG_CREW_NICKNAME_TYPE_EXCEPTION_MESSAGE = "닉네임은 한글만 가능합니다.";
+    private static final String WRONG_CREW_NICKNAME_LENGTH_EXCEPTION_MESSAGE = "닉네임의 전체 길이는 1자 이상 20자 미만이어야합니다.";
+    private static final int MIN_NUMBER_OF_CREW = 1;
+    private static final int MAX_NUMBER_OF_CREW = 10000;
+    private static final String CREW_EMAIL_REGEX = "^[a-zA-Z0-9]+@email.com$";
+    private static final int MIN_CREW_EMAIL_LENGTH = 11;
+    private static final int MAX_CREW_EMAIL_LENGTH = 20;
+    private static final String CREW_NICKNAME_REGEX = "^[가-힣]+$";
+    private static final int MIN_CREW_NICKNAME_LENGTH = 1;
+    private static final int MAX_CREW_NICKNAME_LENGTH = 20;
+
     private static final Map<String, String> crewInfoMap = new HashMap<>();
 
     public static List<String> solution(List<List<String>> crewInfoForms) {
@@ -21,8 +39,8 @@ public class Problem6 {
     private static void initCrewInfoMap(List<List<String>> crewForms) {
         crewForms.stream()
                 .forEach(crewInfo -> {
-                    String crewNickname = crewInfo.get(1);
-                    String crewEmail = crewInfo.get(0);
+                    String crewNickname = crewInfo.get(CREW_NICKNAME_INDEX);
+                    String crewEmail = crewInfo.get(CREW_EMAIL_INDEX);
                     crewInfoMap.put(crewNickname, crewEmail);
                 });
     }
@@ -38,8 +56,8 @@ public class Problem6 {
         List<String> substringCrewNicknames = new ArrayList<>();
         duplicableCrewNicknames.forEach(
                 duplicableCrewNickname -> {
-                    for (int subStringIndex = 0; subStringIndex < duplicableCrewNickname.length() - 1; subStringIndex++) {
-                        substringCrewNicknames.add(duplicableCrewNickname.substring(subStringIndex, subStringIndex + 2));
+                    for (int substringIndex = SUBSTRING_START_INDEX; substringIndex < duplicableCrewNickname.length() - 1; substringIndex++) {
+                        substringCrewNicknames.add(duplicableCrewNickname.substring(substringIndex, substringIndex + 2));
                     }
                 }
         );
@@ -80,45 +98,45 @@ public class Problem6 {
 
     private static void validateCrewInfo(List<List<String>> crewForms) {
         if(!validateNumberOfCrew(crewForms)) {
-            throw new IllegalArgumentException("[ERROR] : 크루는 1명 이상 10,000명 이하여야합니다.");
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + WRONG_NUMBER_OF_CREW_EXCEPTION_MESSAGE);
         }
         crewForms.stream()
                 .forEach(crewForm -> {
                     String crewEmail = crewForm.get(0);
                     String crewNickname = crewForm.get(1);
                     if(!validateEmailType(crewEmail)) {
-                        throw new IllegalArgumentException("[ERROR] : 이메일 형식에 부합해야 하며, 도메인은 email.com이어야합니다.");
+                        throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + WRONG_CREW_EMAIL_TYPE_EXCEPTION_MESSAGE);
                     }
                     if(!validateEmailLength(crewEmail)) {
-                        throw new IllegalArgumentException("[ERROR] : 이메일 길이는 11자 이상 20자 미만이어야합니다.");
+                        throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + WRONG_CREW_EMAIL_LENGTH_EXCEPTION_MESSAGE);
                     }
                     if(!validateNicknameType(crewNickname)) {
-                        throw new IllegalArgumentException("[ERROR] : 닉네임은 한글만 가능합니다.");
+                        throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + WRONG_CREW_NICKNAME_TYPE_EXCEPTION_MESSAGE);
                     }
                     if(!validateNicknameLength(crewNickname)) {
-                        throw new IllegalArgumentException("[ERROR] : 닉네임의 전체 길이는 1자 이상 20자 미만이어야합니다.");
+                        throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + WRONG_CREW_NICKNAME_LENGTH_EXCEPTION_MESSAGE);
                     }
                 });
     }
 
     private static boolean validateNumberOfCrew(List<List<String>> crewForms) {
-        return (crewForms.size() >= 1 && crewForms.size() <= 10000);
+        return (crewForms.size() >= MIN_NUMBER_OF_CREW && crewForms.size() <=MAX_NUMBER_OF_CREW);
     }
 
     private static boolean validateEmailType(String crewEmail) {
-        return Pattern.compile("^[a-zA-Z0-9]+@email.com$").matcher(crewEmail).matches();
+        return Pattern.compile(CREW_EMAIL_REGEX).matcher(crewEmail).matches();
     }
 
     private static boolean validateEmailLength(String crewEmail) {
-        return (crewEmail.length() >= 11 && crewEmail.length() < 20);
+        return (crewEmail.length() >= MIN_CREW_EMAIL_LENGTH && crewEmail.length() < MAX_CREW_EMAIL_LENGTH);
     }
 
 
     private static boolean validateNicknameType(String crewNickname) {
-        return Pattern.compile("^[가-힣]+$").matcher(crewNickname).matches();
+        return Pattern.compile(CREW_NICKNAME_REGEX).matcher(crewNickname).matches();
     }
 
     private static boolean validateNicknameLength(String crewNickname) {
-        return (crewNickname.length() >= 1 && crewNickname.length() < 20);
+        return (crewNickname.length() >= MIN_CREW_NICKNAME_LENGTH && crewNickname.length() < MAX_CREW_NICKNAME_LENGTH);
     }
 }
