ac06ad8e030ffd19ba74f52cba44f0fed3369d86
Jimin <thswlals0219@gmail.com>
2022-10-29 21:24:24 +0900

    docs : PROBLEM5 기능 목록 추가
    큰 화폐 위주로 돈 출금하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static List<Integer> withdrawMoney(int money) {
+        int[] currencyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> currencyCount = new ArrayList<>(Collections.nCopies(currencyType.length, 0));
+
+        for(int i = 0; i < currencyType.length; i++) {
+            if(currencyType[i] <= money) {
+                int cnt = money / currencyType[i];
+                currencyCount.set(i, cnt);
+                money %= currencyType[i];
+            }
+        }
+
+        return currencyCount;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return withdrawMoney(money);
     }
 }
