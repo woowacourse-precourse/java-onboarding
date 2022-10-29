5358bccb3306f29aa7343f59a04b520ad0a7bac9
ddogga <hayeonlee60327@gmail.com>
2022-10-29 19:57:02 +0900

    문제2 주어진 문자열 안에서 연속되는 지점을 찾기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,59 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        int startIdx = -1;
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i-1)) {
+                startIdx = i-1;
+                break;
+            }
+        }
+
+
+        answer = findIdx(cryptogram, startIdx);
+
+
         return answer;
+    }
+
+
+    public static String findIdx(String cryptogram, int startIdx) {
+
+        int lastIdx = startIdx;
+
+        for (int i = startIdx; i < cryptogram.length(); i++) {
+
+            if (cryptogram.charAt(i) == cryptogram.charAt(i-1)) {
+                System.out.println(cryptogram.charAt(i));
+                System.out.println(cryptogram.charAt(i-1));
+                lastIdx += 1;
+
+            }
+
+            if ((cryptogram.charAt(i) != cryptogram.charAt(i-1)) && lastIdx != startIdx) {
+
+                System.out.println("startIdx = " + startIdx);
+                System.out.println("lastIdx = " + lastIdx);
+
+                break;
+
+            }
+
+
+
+
+        }
+
+        return cryptogram;
+    }
+
+
+    public static void main (String[] args) {
+
+        String cryptogram = "browoanoommnaon";
+
+        solution(cryptogram);
     }
 }
