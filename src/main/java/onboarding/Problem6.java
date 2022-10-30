20414cd4557c4a3530ceae867e1196b3276a0b6d
bellljun3395 <195850@jnu.ac.kr>
2022-10-30 13:51:59 +0900

    feat : Input6 클래스 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+}
+
+class Input6 {
+    static Scanner scanner = new Scanner(System.in);
+    static String inputEmail() {
+        return scanner.next();
+    }
+
+    static String inputNickName() {
+        return scanner.next();
+    }
+
 }
