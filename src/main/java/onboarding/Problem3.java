dc044be433409a9e2b7666b379cbe575c1aa4337
yun8565 <yunyun8565@naver.com>
2022-10-30 23:55:45 +0900

    feat(P3): 총 손뼉 횟수 반환 기능 추가
    1부터 number까지 총 몇 번의 손뼉을 쳐야 하는지  반환하는 기능을 구현했다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,13 +6,20 @@ public class Problem3 {
     public static final String REGEX = "[3|6|9]";
 
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return getTotalClaps(number);
     }
 
     public static int getClapCount(int number) {
         return (int)Stream.of(String.valueOf(number).split(""))
-                .filter(num -> num.matches(REGEX))
+                .filter(a -> a.matches(REGEX))
                 .count();
+    }
+
+    public static int getTotalClaps(int endNumber) {
+        int total = 0;
+        for(int number = 1; number <= endNumber; number++) {
+            total += getClapCount(number);
+        }
+        return total;
     }
 }
