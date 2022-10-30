202720be9b2b75fe896e2816dbbfd3379e4ffca7
desfox <desfox99@naver.com>
2022-10-30 20:00:59 +0900

    feat(Problem3): 현재 숫자에서 3, 6, 9의 개수 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static void getClapCount(int number) {
+        List<Integer> clapList = Arrays.asList(3, 6, 9);
+
+        for (int i = 1; i <= number; i++) {
+            int clapCnt = 0;
+            String[] splitNums = String.valueOf(i).split("");
+
+            for (String num : splitNums) {
+                if (clapList.contains(Integer.parseInt(num))) {
+                    clapCnt++;
+                }
+            }
+        }
     }
 }
