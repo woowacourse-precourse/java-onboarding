f45bfbc5fe8b55117cd6f04b03786639919efaf0
wugawuga <dnr3655@naver.com>
2022-10-31 03:43:48 +0900

    feat: list에 문자 하나씩 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 		if (cryptogram.length() == 1) {
 			return cryptogram;
 		}
+		List<Character> list = new ArrayList<>();
+		for (char c : cryptogram.toCharArray()) {
+			add(list, c);
+		}
         return answer;
     }
+
+	private static void add(List<Character> list, char c) {
+		list.add(c);
+	}
 }
