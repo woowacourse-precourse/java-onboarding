c698393bf74d0f431fe7eae464142e130b2080c6
Chanwoo Lee <lcw3176@naver.com>
2022-10-30 11:57:53 +0900

    Refactor/problem2 (#6)
    * refactor: 중복 문자 탐색 함수 분리
    
    * refactor: 인덱스 탐색 후 요소 삭제

--- src/main/java/onboarding/problem2/DecryptionUtil.java
+++ src/main/java/onboarding/problem2/DecryptionUtil.java
@@ -1,6 +1,10 @@
 package onboarding.problem2;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class DecryptionUtil {
@@ -11,24 +15,36 @@ public class DecryptionUtil {
 			.mapToObj(c -> (char) c)
 			.collect(Collectors.toList());
 
-		boolean workedFlag = false;
+		List<Integer> indexList = findContinuousValueIndex(passwordList);
 
-		for(int i = 0; i < passwordList.size() - 1; i++){
-			if(passwordList.get(i) == passwordList.get(i + 1)){
-				passwordList.remove(i);
-				passwordList.remove(i);
-				workedFlag = true;
-			}
-		}
-
-		if(!workedFlag){
+		if(indexList.size() == 0){
 			return password;
 		}
+		Collections.reverse(indexList);
+
+		for(int i : indexList){
+			passwordList.remove(i);
+		}
+
 
 		password.changeValue(passwordList.stream()
 			.map(Object::toString)
 			.collect(Collectors.joining()));
 
 		return decrypt(password);
+	}
+
+
+	private static List<Integer> findContinuousValueIndex(List<Character> characterList){
+		Set<Integer> indexSet = new HashSet<>();
+
+		for(int i = 0; i < characterList.size() - 1; i++){
+			if(characterList.get(i) == characterList.get(i + 1)){
+				indexSet.add(i);
+				indexSet.add(i + 1);
+			}
+		}
+
+		return new LinkedList<>(indexSet);
 	}
 }
