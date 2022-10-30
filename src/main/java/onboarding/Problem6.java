a2a8a3c880be1835fbb323418ccb191651bafdf5
yeonhee <ydol2@naver.com>
2022-10-30 21:05:02 +0900

    [6번] feat : 이메일 도메인이 @email.com인지 확인하는 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,13 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    /**
+     * 이메일 도메인이 @email.com 인지 확인하는 함수
+     */
+    private static boolean isEmailDomain(String email){
+        if (!email.endsWith("@email.com"))
+            return false;
+        else return true;
+    }
 }
