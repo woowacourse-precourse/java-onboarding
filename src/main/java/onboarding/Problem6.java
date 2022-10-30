e4d28ae3f44e3cd9bf4b89511e66f5901997b02b
myeongju00 <j51677767@gmail.com>
2022-10-30 20:16:05 +0900

    feat: 중복된 닉네임의 이메일 리스트 생성하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,29 +1,37 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
     static Map<String, Integer> nicknameList = new HashMap<>();
-    static List<Integer> indexList = new ArrayList<>();
+    static Set<Integer> indexList = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        boolean[] duplicationArr = new boolean[forms.size()];
+        for (int i = 0; i < forms.size(); i++) {
+            inputNameToMap(forms.get(i).get(1), i);
+        }
+        for(int i : indexList) {
+            System.out.println(i);
+            answer.add(forms.get(i).get(0));
+        }
+        Collections.sort(answer);
         return answer;
     }
     private static void inputNameToMap(String name, int index) {
-        for (int i = 0; i < name.length() - 2; i++) {
+        for (int i = 0; i < name.length() - 1; i++) {
             String subString = name.substring(i, i + 2);
             if(nicknameList.containsKey(subString)) {
                 //정답 베열에 넣는 함수
+                putNameIndex(nicknameList.get(subString));
                 putNameIndex(index);
             } else {
-                nicknameList.put(name, index);
+                nicknameList.put(subString, index);
             }
         }
     }
     private static void putNameIndex(int index) {
         indexList.add(index);
     }
+
 }
