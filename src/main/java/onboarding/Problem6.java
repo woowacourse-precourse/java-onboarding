49bef52bf58a9e65daa2a80a1f21276fc5ffe393
kimminsu <xjfcnfw3@naver.com>
2022-10-30 14:05:37 +0900

    feat(문제6): solution함수에 검증 기능추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,12 +2,18 @@ package onboarding;
 
 import java.util.List;
 import onboarding.problem6.NickNameSystem;
+import onboarding.problem6.validation.Validation;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        NickNameSystem nickNameSystem = new NickNameSystem();
-        answer = nickNameSystem.getEmailsToBeSent(forms);
+        try {
+            Validation.validate(forms);
+            NickNameSystem nickNameSystem = new NickNameSystem();
+            answer = nickNameSystem.getEmailsToBeSent(forms);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return answer;
     }
 }
