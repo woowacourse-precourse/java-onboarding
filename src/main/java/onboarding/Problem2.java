a16bf107c64f550f22e8ab7eda9eb74b2a994053
goldcollar95 <yjs7298@naver.com>
2022-10-30 15:37:36 +0900

    2번 다시수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,22 +1,25 @@
 package onboarding;
 
-import java.util.Scanner;
+import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "";
-        for (int i = 0; i < cryptogram.length(); i++) {
-            if (cryptogram.indexOf(cryptogram.charAt(i)) == i)	//i의 문자열을 다 가져온다, 같은것을 꺼내온다
-                answer += cryptogram.charAt(i);
+        Stack<Character> stack = new Stack<>();
 
+        for (char c : cryptogram.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
         }
-        return answer;
 
-    }
+        StringBuilder answer = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String cryptogram = scanner.next();
-        System.out.println(solution(cryptogram));
+        for (char c : stack) {
+            answer.append(c);
+        }
+
+        return answer.toString();
     }
 }
