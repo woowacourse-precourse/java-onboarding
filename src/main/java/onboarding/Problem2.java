01a52378e0250ff5dc29f67162b78c3103179d2c
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-10-31 05:32:36 +0900

    Problem2_Completed

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+
 public class Problem2 {
+
+/*
+
+ idea 1) 문자열 -> 배열 변환 후, 같은 두 단어가 연속 될 때 삭제 처리
+ idea 2) charAt 함수로 다음 단어랑 같은게 있으면 삭제 처리
+ idea 3) indexof 활용해보기?
+ */
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        StringBuffer sb = new StringBuffer();
+        sb.append(cryptogram);
+
+        boolean a = true;
+
+        while (a) {
+            a = false;
+            for (int i = 0; i < sb.length() - 1; i++) {
+                if (sb.charAt(i) == sb.charAt(i + 1)) {
+                    sb.replace(i, i + 2, "");
+                    a = true;
+                }
+            }
+        }
+        answer = sb.toString();
         return answer;
     }
+
 }
