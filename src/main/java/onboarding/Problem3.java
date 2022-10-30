1f5a9bc8ad62e9cf30d010118de03602f94b1716
siwookim97 <tldn12gh@gmail.com>
2022-10-30 23:23:38 +0900

    feat : problem3) 손뼉을 치는 횟수를 구하는 기능 추가
    숫자를 입력받아 손뼉을 치는 횟수를 구하는 기능
    int형 매개변수를 입력받아 해당 매개변수에서
    3,6,9가 있는 횟수만큼 반환한다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+
+    /**
+     * 숫자를 입력받아 손뼉을 치는 횟수를 구하는 메서드
+     *
+     * @param number 입력받은 숫자
+     * @return 손뼉을 치는 횟수
+     */
+    static int countClap (int number) {
+        int count = 0;
+        while (number > 0) {
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                count++;
+            }
+            number /= 10;
+        }
+
+        return count;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
