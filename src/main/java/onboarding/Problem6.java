4310339ceb0972517c579f1f81b9fe43b0e90c3c
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-10-30 18:24:33 +0900

    feat:Add a function that get nickname from list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
     }
+
+    //List에서 닉네임을 받아오는 함수
+    public static String getNickname(List<List<String>> forms, int index) {
+        List<String> data = forms.get(index);
+        String nickname = data.get(1);
+        return nickname;
+    }
+
 }
