ee7a77a9dd5e39faab39ff5cd0947e129a804bdb
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 21:15:17 +0900

    feat: Problem5 - 거스름돈의 단위를 배열로 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,7 +8,11 @@ import java.util.List;
 /**
  * moneyCount 메서드 돈을 50000부터 나누어 몫을 저장후 값을 차감하며 1까지 카운트를 List로 변환
  */
+
+
 public class Problem5 {
+
+    final static int[] AMOUNT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
