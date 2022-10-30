510e247ae2592441baec2f250d8a2eb4f61928b6
Minky Baek <55613446+toyo30@users.noreply.github.com>
2022-10-30 23:10:10 +0900

    [add] perform - finding friends of user

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,6 +9,17 @@ public class Problem7 {
         List<String> answer = new ArrayList<>();
         List<String> friendsOfUser = new ArrayList<>();
         HashMap<String, Integer> recommendFriendsScore = new HashMap<>();
+
+//먼저 HashMap에서 user의 친구를 조사한다.
+        for(int i = 0; i < friends.size(); i++) {
+            if(friends.get(i).contains(user)){
+                for(int j = 0; j < friends.get(i).size(); j++) {
+                    if(!friends.get(i).get(j).equals(user)) {
+                        friendsOfUser.add(friends.get(i).get(j));
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
