e3935f93def888ff52e346b3677731df989ca513
java-saeng <gusdn85554@naver.com>
2022-10-30 15:30:23 +0900

    refactor : Problem5 바꿀 수 있는 거스름돈 후보 상수화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,17 +1,16 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
+    public static final List<Integer> CHANGES_CANDIDATE = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
-        List<Integer> changes = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
-
-        for (int change : changes) {
+        for (int change : CHANGES_CANDIDATE) {
             answer.add(money / change);
 
             money %= change;
