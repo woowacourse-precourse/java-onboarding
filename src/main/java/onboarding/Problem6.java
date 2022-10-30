5628a9f2d1e1d87d724fbb355d73f6faf84536ea
kimdeokyoung <dudejrss@naver.com>
2022-10-30 20:06:01 +0900

    [Edit] src/main/java/onboarding/Problem6.java : make structure Problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+
+    public static Map<String, LinkedList<String>> makeTokens(List<List<String>> forms){
+        Map<String, LinkedList<String>> result = new HashMap<>();
+        return result;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
