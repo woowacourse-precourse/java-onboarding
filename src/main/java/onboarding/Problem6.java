fc7949df45d6e1b800f1668d5b370173b4977411
myeongju00 <j51677767@gmail.com>
2022-10-30 19:31:28 +0900

    feat: 닉네임을 두글자씩 잘라서 맵에 넣는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+    static Map<String, Integer> nicknameList = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    private static void inputNameToMap(String name, int index) {
+        for (int i = 0; i < name.length() - 2; i++) {
+            String subString = name.substring(i, i + 2);
+            if(nicknameList.containsKey(subString)) {
+                //정답 베열에 넣는 함수
+            } else {
+                nicknameList.put(name, index);
+            }
+        }
     }
 }
