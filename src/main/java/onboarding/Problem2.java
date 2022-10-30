9cd63a4dbbffd2c783cee0d3b856c6269e65c6c4
doongu <must1080@naver.com>
2022-10-30 18:25:50 +0900

    docs(Problem2): 스택이 비었는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
+    static Stack<String> stack = new Stack<>();
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static boolean isEmpty(Stack checkStack){
+        boolean returnBoolean = false;
+
+        if (checkStack.isEmpty()){
+            returnBoolean = true;
+        }
+
+        return returnBoolean;
     }
 }
