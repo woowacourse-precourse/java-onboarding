0829913b7ce471b4b83cfaa59db6954f5f23ad70
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 20:45:34 +0900

    feat(PR6): 추가 닉네임에서 연속되는 문자열 저장 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+
+    static List<List<String>> findContinualChar(List<List<String>> forms){
+        List<List<String>> continualCharOfCrew = new ArrayList<>();
+        for(int crewIdx = 0; crewIdx < forms.size(); crewIdx++) {
+            String nickName = forms.get(crewIdx).get(1);
+            List<String> continualChar = new ArrayList<>();
+            String divededChar;
+
+            for(int nickNameIdx = 1; nickNameIdx < nickName.length(); nickNameIdx++){
+                divededChar = Character.toString(nickName.charAt(nickNameIdx-1)) + Character.toString(nickName.charAt(nickNameIdx));
+                continualChar.add(divededChar);
+            }
+            continualCharOfCrew.add(continualChar);
+        }
+        return continualCharOfCrew;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+
         return answer;
     }
 }
