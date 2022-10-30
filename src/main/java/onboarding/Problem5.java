f53d331dc7ffbf7fc07e276b47ccbc71031e38e2
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:23:59 +0900

    [문제5]각 돈의 단위로 나눠 결과를 내는 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,7 +11,16 @@ public class Problem5 {
         if (money<1 || money>1000000)
             return null;
 
-        
+        answer = new ArrayList<Integer>();
+        answer.add(money/50000);
+        answer.add((money%50000)/10000);
+        answer.add((money%10000)/5000);
+        answer.add((money%5000)/1000);
+        answer.add((money%1000)/500);
+        answer.add((money%500)/100);
+        answer.add((money%100)/50);
+        answer.add((money%50)/10);
+        answer.add((money%10));
 
         return answer;
     }
