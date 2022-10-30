1cb7933afd9889dab44837184f8648e5646e294b
hossang <zkzk104@naver.com>
2022-10-30 19:55:36 +0900

    feat: 문제1 가장 큰 수 구하는 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    private static int searchMaxNum(Integer num) {
+        int mutiply = 1;
+        int sum = 0;
+        while (num != 0) {
+            mutiply *= num % 10;
+            sum += num % 10;
+            num /= 10;
+        }
+        return Math.max(mutiply, sum);
+    }
 }
