80b0ea12b673ee594e60c5c5a05b3fe3bc8e26ea
dclxxi <dclxxi@kakao.com>
2022-10-30 20:13:58 +0900

    feat(문제7): 친구 목록을 생성하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,11 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static final int USER = 0;
+    private static final int FRIEND = 1;
+
     public static class User {
         private String id;
         private int score;
@@ -40,5 +41,26 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map<String, User> setFriends(List<List<String>> friends, String user) {
+        Map<String, User> users = new HashMap<>();
+        users.put(user, new User(user));
+
+        for (List<String> friend : friends) {
+            String userId = friend.get(USER);
+            String friendId = friend.get(FRIEND);
+
+            addFriendOfUser(users, userId, friendId);
+            addFriendOfUser(users, friendId, userId);
+        }
+
+        return users;
+    }
+
+    private static void addFriendOfUser(Map<String, User> users, String userId, String friendId) {
+        User user = users.getOrDefault(userId, new User(userId));
+        user.addFriend(friendId);
+        users.put(userId, user);
     }
 }
