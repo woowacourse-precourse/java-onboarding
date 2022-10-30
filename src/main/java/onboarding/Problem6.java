a0f131f6ecf3f1448c1a577183fc35aa7051be1f
khyojun <38220795+khyojun@users.noreply.github.com>
2022-10-30 11:50:14 +0900

    refactor:6번 코드 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,49 +11,78 @@ public class Problem6 {
 
         subStringMatching(forms, realAnswer);
 
-        Set<String> set = new HashSet<>(realAnswer);
-        List<String> result = new ArrayList<>(set); // result
+        List<String> result = getNoDuplicateStrings(realAnswer);
 
         Collections.sort(result);
         return result;
     }
 
+    private static List<String> getNoDuplicateStrings(List<String> realAnswer) {
+        Set<String> set = new HashSet<>(realAnswer);
+        List<String> result = new ArrayList<>(set);
+        return result;
+    }
+
     private static void subStringMatching(List<List<String>> forms,
-        List<String> answer) { // 부분 문자열 생성을 위한 메서드 생성
+        List<String> answer) { 
         int form_size = forms.size();
         for (int i = 0; i < form_size; i++) {
-            String nickName = forms.get(i).get(1); // 닉네임 꺼내기
+            String nickName = getNickName(forms, i);
             for (int start = 0; start < nickName.length(); start++) {
                 for (int end = start + 1; end <= nickName.length(); end++) {
                     String subString = nickName.substring(start, end);
-                    if (subString.length() == 1) {
+                    if (isSubChar(subString)) {
                         continue;
                     } else {
                         for (int other = 0; other < form_size; other++) {
-                            String comp_nickName = forms.get(other)
-                                .get(1); // 부분 문자열과 비교하기위한 다른 사람의 닉네임 비교
-                            if (nickName.equals(comp_nickName)) { // 비교 문자열과 아예 같으면 비교 x
+                            String comp_nickName = getNickName(forms, other);
+                            if (isSameNickName(nickName, comp_nickName)) {
                                 continue;
                             }
 
-                            if (comp_nickName.equals(subString)) { // 부분 문자열과 비교 문자열과 같을 경우 비교 X
+                            if (isSameComparationNickName(subString,
+                                comp_nickName)) {
                                 continue;
                             }
 
-                            if (comp_nickName.contains(subString)) { // 부분 문자열이 포함이 되어있는 경우 추가
-                                String nickName_email = forms.get(i).get(0);
-                                String comp_nickName_email = forms.get(other).get(0);
-                                answer.add(nickName_email);
-                                answer.add(comp_nickName_email);
+                            if (isCompNickNameContainSubString(subString, comp_nickName)) {
+                                putSameSubstringInAnswer(forms, answer, i, other);
                             }
 
                         }
                     }
-
                 }
             }
         }
 
 
+    }
+
+    private static boolean isCompNickNameContainSubString(String subString, String comp_nickName) {
+        return comp_nickName.contains(subString);
+    }
+
+    private static boolean isSubChar(String subString) {
+        return subString.length() == 1;
+    }
+
+    private static void putSameSubstringInAnswer(List<List<String>> forms, List<String> answer,
+        int i, int other) {
+        String nickName_email = forms.get(i).get(0);
+        String comp_nickName_email = forms.get(other).get(0);
+        answer.add(nickName_email);
+        answer.add(comp_nickName_email);
+    }
+
+    private static boolean isSameComparationNickName(String subString, String comp_nickName) {
+        return comp_nickName.equals(subString);
+    }
+
+    private static boolean isSameNickName(String nickName, String comp_nickName) {
+        return nickName.equals(comp_nickName);
+    }
+
+    private static String getNickName(List<List<String>> forms, int i) {
+        return forms.get(i).get(1);
     }
 }
