0837912d125eb454874b76778c99e719fd84303b
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 16:13:54 +0900

    feat(PR5): 화폐 종류 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,30 +1,29 @@
 package onboarding;
 import java.util.LinkedList;
 import java.util.Deque;
+import java.util.List;
 
 public class Problem2 {
     static boolean isFinished = false;
-
+    static final int MAXIDX = 1010;
     //연속된 문자열 찾기
     static Deque<Integer> findRedundant(String cryptogram){
         Deque<Integer> deque = new LinkedList<>();
 
         for(int i = 0; i < cryptogram.length(); i++){
-            if(deque.size() > 1) break;
             if(i == cryptogram.length() - 1) break; //뒤에 문자열 없음
-            deque.add(i);
             char prevAlp = cryptogram.charAt(i);
             for(int j = i + 1; j< cryptogram.length();){
                 char curAlp = cryptogram.charAt(j);
                 if(prevAlp == curAlp) {
+                    deque.add(i);
                     deque.add(j);
                     j++;
                 }
-                else if(deque.size() > 1) break; //뒤에 같은 문자열 더이상 없으면
-                else {
-                    deque.remove();
-                    break;
-                }
+
+                else
+                    break; //뒤에 같은 문자열 더이상 없으면
+
             }
         }
         if(deque.isEmpty()) isFinished = true;
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> moneyList = new ArrayList<Integer>();
+
+        moneyList.add(50000);
+        moneyList.add(10000);
+        moneyList.add(5000);
+        moneyList.add(1000);
+        moneyList.add(500);
+        moneyList.add(100);
+        moneyList.add(50);
+        moneyList.add(10);
+        moneyList.add(1);
+
         return answer;
     }
 }
