34a005ea2239a244dbd8e959930e003762755aa9
KangHyeon3315 <son2786w@gmail.com>
2022-10-30 15:04:26 +0900

    refactor: [6번] 메모리 사용량을 줄이도록 성능 개선

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,66 +3,98 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
-    /**
-     * 이름을 2글자씩 분해해 wordMap을 업데이트하는 함수 (3글자 이상 중복 되는 경우 2글자도 중복되기 때문에 2글자로만 중복 판단을 한다.)
-     * 이때 분해된 단어를 Key로 가지는 List에 email을 추가
-     * <pre>
-     * 이름 분해 예시 : "제이엠" -> "제이", "이엠"
-     * </pre>
-     */
-    private static void updateWordMap(List<String> userInfo, Map<String, List<String>> wordMap) {
-        String email = userInfo.get(0);
-        String nickname = userInfo.get(1);
 
+    /**
+     * 이름을 2글자씩 분해하는 함수 <br/>
+     * 3글자 이상 중복시 2글자도 중복이므로 2글자씩 추출 <br/>
+     *
+     * @param nickname 구성원의 닉네임
+     * @return 분해된 단어 List를 반환 ex): "제이엠" -> "제이", "이엠"
+     */
+    private static List<String> extractNickname(String nickname) {
+        List<String> wordList = new ArrayList<>();
         for (int i = 0; i < nickname.length() - 1; i++) {
             String word = nickname.substring(i, i + 2);
 
-            List<String> emailList = wordMap.getOrDefault(word, new ArrayList<>());
+            wordList.add(word);
+        }
+        return wordList;
+    }
 
-            emailList.add(email);
+    /**
+     * 주어진 단어를 닉네임에 가지고있는 유저의 email들을 가져오는 함수
+     *
+     * @param forms 유저 정보 List
+     * @param word  검사할 단어
+     * @param idx   탐색을 시작할 index
+     * @return word를 가지고 있는 유저 email List 반환
+     */
+    private static List<String> findUserUsingWord(List<List<String>> forms, String word, int idx) {
+        List<String> userEmailList = new ArrayList<>();
+        for (int i = idx; i < forms.size(); i++) {
+            List<String> userInfo = forms.get(i);
+            String email = userInfo.get(0);
+            String nickname = userInfo.get(1);
 
-            wordMap.put(word, emailList);
+            if (!nickname.contains(word)) continue;
+
+            userEmailList.add(email);
+        }
+        return userEmailList;
+    }
+
+
+    /**
+     * 요소의 중복이 없도록 String List를 합치는 기능
+     *
+     * @param origin 값이 수정되는 List
+     * @param source 합쳐지는 List
+     */
+    private static void concatWithoutDuplicate(List<String> origin, List<String> source) {
+        for (String email : source) {
+            if (origin.contains(email)) continue;
+
+            origin.add(email);
         }
     }
 
     /**
-     * wordEmailList에 있는 email을 duplicateEmailList에 추가하는 함수
-     * 이때 email이 이미 있으면 추가하지 않는다.
+     * idx 번째 이후의 유저 중 idx 번쨰 유저와 중복되는 email list가 있는지 체크하는 함수
+     *
+     * @param forms 유저 정보 리스트
+     * @param idx 검사할 유저 index
+     * @return 유사한 유저의 email list (idx 번째 유저 포함)
      */
-    private static void updateDuplicateEmailList(List<String> duplicateEmailList, List<String> wordEmailList) {
-        if (wordEmailList.size() <= 1) return;
+    private static List<String> checkSimilarUser(List<List<String>> forms, int idx) {
+        List<String> userInfo = forms.get(idx);
+        String nickname = userInfo.get(1);
 
-        for (String email : wordEmailList) {
-            if (duplicateEmailList.contains(email)) continue;
+        List<String> wordList = extractNickname(nickname);
 
-            duplicateEmailList.add(email);
-        }
-    }
+        List<String> userList = new ArrayList<>();
+        for (String word : wordList) {
+            List<String> emailList = findUserUsingWord(forms, word, idx);
 
-    /**
-     * WordMap에서 중복된 단어를 가진 유저가 있는 경우 emailList에 추가해 반환하는 함수
-     */
-    private static List<String> checkDuplicateUser(Map<String, List<String>> wordMap) {
-        List<String> duplicateEmailList = new ArrayList<>();
-        for (String word : wordMap.keySet()) {
-            List<String> wordEmailList = wordMap.get(word);
-
-            updateDuplicateEmailList(duplicateEmailList, wordEmailList);
+            concatWithoutDuplicate(userList, emailList);
         }
 
-        Collections.sort(duplicateEmailList);
-
-        return duplicateEmailList;
+        return userList;
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        Map<String, List<String>> wordMap = new HashMap<>();
 
-        for (List<String> userInfo : forms) {
-            updateWordMap(userInfo, wordMap);
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> similarUserEmailList = checkSimilarUser(forms, i);
+
+            if(similarUserEmailList.size() < 2) continue;
+
+            concatWithoutDuplicate(answer, similarUserEmailList);
         }
 
-        return checkDuplicateUser(wordMap);
+        Collections.sort(answer);
 
+        return answer;
     }
+
 }
