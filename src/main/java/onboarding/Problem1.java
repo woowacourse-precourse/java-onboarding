11def33bd24fd8108856c837b1959e94aa0f5ad1
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 21:13:25 +0900

    feat: [Problem1] page가 올바르게 입력되어 있는지 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,15 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /*
+    page가 올바르게 입력되어 있는지 체크 (차이는 무조건 1이어야함)
+     */
+    private static boolean checkPage(List<Integer> man){
+        if(man.get(1).intValue()-man.get(0).intValue()!=1){
+            return false;
+        }
+        return true;
+    }
+
 }
