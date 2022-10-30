5f52954b5bd5c8dc43e671aa03d1471d09ca14d5
wonjunYou <youwjune@gmail.com>
2022-10-30 23:32:36 +0900

    feat : (Problem 5) 각 화폐별로 반환할 개수를 구하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,26 +1,46 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static final int MAX_VALUE_OF_MONEY = 1_000_000;
 
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+    private static final List<Integer> LIST_OF_MONETARY_UNIT = initMonetaryUnits();
 
+
+    public static List<Integer> solution(int money) {
         try {
             validateInputSize(money);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
 
-        return answer;
+        return countNumberOfEachUnit(money);
+    }
+
+    private static List<Integer> initMonetaryUnits() {
+        return List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     }
 
     private static void validateInputSize(int money) {
         if (money < 1 || money > MAX_VALUE_OF_MONEY) {
             throw new IllegalArgumentException("허용되지 않는 크기의 입력값입니다.");
         }
+    }
+
+    private static List<Integer> countNumberOfEachUnit(int money) {
+        List<Integer> countList = new ArrayList<>();
+
+        for (int monetaryUnit : LIST_OF_MONETARY_UNIT) {
+            countList.add(divide(money, monetaryUnit));
+        }
+
+        return countList;
+    }
+
+    private static int divide(int money, int monetaryUnit) {
+        return money / monetaryUnit;
     }
 }
