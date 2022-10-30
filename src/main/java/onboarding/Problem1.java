51b89490bb0acfa0164054348abf5d4fd183b5e4
rt3310 <btac3310@gmail.com>
2022-10-30 15:03:41 +0900

    feat(Problem1): 포비, 크롱 매치 결과 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -29,4 +29,16 @@ class Problem1 {
     public static int getMaxNumber(int number1, int number2) {
         return Math.max(number1, number2);
     }
+
+    public static int getMatchResult(int pobiScore, int crongScore) {
+        if (pobiScore > crongScore) {
+            return 1;
+        }
+
+        if (crongScore > pobiScore) {
+            return 2;
+        }
+
+        return 0;
+    }
 }
