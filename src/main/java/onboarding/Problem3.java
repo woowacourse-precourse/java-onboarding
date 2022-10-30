90198dbab43274652bbc589d6386708c979eca3e
jwjin95 <jwjin95@yonsei.ac.kr>
2022-10-31 01:01:08 +0900

    Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i=1 ; i<=number ; i++) {
+            int n = i;
+            while (n > 0) {
+                if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) {
+                    answer += 1;
+                }
+                n /= 10;
+            }
+        }
+
         return answer;
     }
 }
