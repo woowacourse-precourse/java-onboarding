2e159118eeba8e73404859422e337b5489f17767
java-saeng <gusdn85554@naver.com>
2022-10-30 14:29:54 +0900

    feat : Problem3 숫자가 3의 배수인지 확인하는 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,8 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static boolean isDigitMultipleOfThree(int number) {
+        return number != 0 && number % 3 == 0;
+    }
 }
