f328c7bb1b9fc7d4224d058fba1c8d3b741ee4c5
zcae1020 <zcae1020@naver.com>
2022-10-30 21:09:15 +0900

    feat(Problem5): method frame

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static int countUnitInMoney(int num, int idx){
+
+    }
+
+    private static boolean isBiggerThanUnit(int num, int idx){
+
     }
 }
