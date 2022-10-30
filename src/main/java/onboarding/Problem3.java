0cb1bc9f493d3c9b1acdda6c2c01edd8fa201907
210_reverof <sheltonwon@gmail.com>
2022-10-30 21:48:33 +0900

    feat: add solution of the requested function - 3 (solved Problem3)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String numStr = String.valueOf(i);
+            answer += numStr.chars().filter(c-> c=='3' || c=='6' || c=='9').count();
+        }
+
         return answer;
     }
 }
