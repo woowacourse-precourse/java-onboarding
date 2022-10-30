ff3609059e37476cc657c7b2f4a000b1cccf4d53
sunwng <sunwgna@gmail.com>
2022-10-30 22:30:04 +0900

    [problem_7] apply excetion class, method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import onboarding.exceptions.LengthException;
+
 import java.lang.reflect.Array;
 import java.util.*;
 
@@ -8,14 +10,36 @@ public class Problem7 {
     static String userName;
 
     static ArrayList<String> common;
+    static void stringLengthCheck(String strIn) throws LengthException {
+        if (strIn.length() < 1 || strIn.length() > 30) {
+            throw new LengthException();
+        }
+    }
+    static void friendsLengthCheck(List<List<String>> listIn) throws LengthException {
+        if (listIn.size() > 10000) {
+            throw new LengthException();
+        }
+    }
+    static void visitorsLengthCheck(List<String> listIn) throws LengthException {
+        if (listIn.size() > 10000) {
+            throw new LengthException();
+        }
+    }
 
     static void visitorsCheck(List<String> visitors) {
-        for (String targetName : visitors) {
-            if (!common.contains(targetName)) {
-                result.putIfAbsent(targetName, 0);
-                result.put(targetName, result.get(targetName) + 1);
+        try {
+            visitorsLengthCheck(visitors);
+            for (String targetName : visitors) {
+                stringLengthCheck(targetName);
+                if (!common.contains(targetName)) {
+                    result.putIfAbsent(targetName, 0);
+                    result.put(targetName, result.get(targetName) + 1);
+                }
             }
+        } catch (LengthException e) {
+            System.err.println("check the length of visitors or elements in visitors");
         }
+
     }
 
     static List<String[]> commonCheck(List<List<String>> friends) {
@@ -35,17 +59,25 @@ public class Problem7 {
     }
 
     static void friendsCheck(List<List<String>> friends) {
-        List<String[]> notCommon = commonCheck(friends);
+        try {
+            friendsLengthCheck(friends);
+            List<String[]> notCommon = commonCheck(friends);
 
-        for (String[] notI : notCommon) {
-            if (common.contains(notI[0]) && !common.contains(notI[1])) {
-                result.putIfAbsent(notI[1], 0);
-                result.put(notI[1], result.get(notI[1]) + 10);
-            } else if (!common.contains(notI[0]) && common.contains(notI[1])) {
-                result.putIfAbsent(notI[0], 0);
-                result.put(notI[0], result.get(notI[0]) + 10);
+            for (String[] notI : notCommon) {
+                stringLengthCheck(notI[0]);
+                stringLengthCheck(notI[1]);
+                if (common.contains(notI[0]) && !common.contains(notI[1])) {
+                    result.putIfAbsent(notI[1], 0);
+                    result.put(notI[1], result.get(notI[1]) + 10);
+                } else if (!common.contains(notI[0]) && common.contains(notI[1])) {
+                    result.putIfAbsent(notI[0], 0);
+                    result.put(notI[0], result.get(notI[0]) + 10);
+                }
             }
+        } catch (LengthException e) {
+            System.err.println("check the length of friends or elements in friends");
         }
+
     }
 
     static List<String> sorter() {
@@ -72,13 +104,18 @@ public class Problem7 {
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-//        List<String> answer = Collections.emptyList();
-        result = new HashMap<>();
-        common = new ArrayList<>();
-        userName = user;
-        friendsCheck(friends);
-        visitorsCheck(visitors);
+        try {
+            result = new HashMap<>();
+            common = new ArrayList<>();
+            stringLengthCheck(user);
+            userName = user;
+            friendsCheck(friends);
+            visitorsCheck(visitors);
 
-        return sorter();
+            return sorter();
+        } catch (LengthException e) {
+            System.err.println("check the length of user: " + user);
+            return Collections.emptyList();
+        }
     }
 }
