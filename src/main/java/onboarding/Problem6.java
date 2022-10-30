1072c67739a8e1fcacf5dfa937c6bb4c063f27f5
endlessmomo <soohyuk96@naver.com>
2022-10-30 16:20:05 +0900

    feat : Problem6 닉네임 중 연속적으로 같은 닉네임을 갖는 크루들을 찾는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,43 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+    public static Map<String, Integer> distinctNickList;
+
+    public static List <String> solution(List <List <String>> forms) {
+        for (List <String> crew : forms) {
+            distinctNick(crew.get(1));
+        }
+
+        return null;
     }
+
+    public static void distinctNick(String nickName) {
+        for(int i = 0; i < nickName.length() - 2; i++){
+            String partOfNick = nickName.substring(i, i+2);
+
+            if(distinctNickList.containsKey(partOfNick)){
+                distinctNickList.put(partOfNick, 1 + distinctNickList.get(partOfNick));
+            } else
+                distinctNickList.put(partOfNick, 1);
+        }
+    }
+
+    /*class Problem6Test {
+        @Test
+        void case1() {
+            List<List<String>> forms = List.of(
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@email.com", "제이슨"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("nowm@email.com", "이제엠")
+            );
+            List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
+            assertThat(Problem6.solution(forms)).isEqualTo(result);
+        }
+    }*/
 }
