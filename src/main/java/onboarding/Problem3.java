8657ab9e64b80ef5389c6db22254407bbbd97e0f
jmjmjames <chungjm0711@gmail.com>
2022-10-30 19:39:04 +0900

    feat: solve Problem3 by dividing

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 3; i <= number; i++) {
+            int curr = i;
+
+            while (curr > 0) {
+                int mod = curr % 10;
+                if (mod == 3 || mod == 6 || mod == 9) {
+                    answer += 1;
+                }
+                curr /= 10;
+            }
+        }
         return answer;
     }
 }
+
