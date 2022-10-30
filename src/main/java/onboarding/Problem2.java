c3e0137953286dbc99e9f46c60e67d42c74ed238
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-10-30 23:39:00 +0900

    feat(Problem2): validate cryptogram

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,18 +3,33 @@ package onboarding;
 public class Problem2 {
     private static class Cryptogram {
         private final String encrypted;
+        private final boolean IS_VALID;
 
         private Cryptogram(String encrypted) {
             this.encrypted = encrypted;
+            this.IS_VALID = validate();
         }
 
-        // TODO: validate()
+        private boolean validate() {
+            if (encrypted == null || encrypted.length() < 1 || 1000 < encrypted.length()) {
+                return false;
+            }
+            boolean isLowerCase = true;
+            for (int i = 0; isLowerCase && i < encrypted.length(); i++) {
+                char c = encrypted.charAt(i);
+                isLowerCase = Character.isLowerCase(c);
+            }
+            return isLowerCase;
+        }
 
         // TODO: decrypt()
     }
 
     public static String solution(String cryptogram) {
         var myCryptogram = new Cryptogram(cryptogram);
+        if (!myCryptogram.IS_VALID) {
+            return null;
+        }
         return myCryptogram.encrypted;
     }
 }
