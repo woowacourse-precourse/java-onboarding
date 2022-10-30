34661785559472ca14ec8d102bdc3ef8bedde2fe
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-30 23:19:15 +0900

    크루의 닉네임을 반환하는 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,7 +6,12 @@ import java.util.List;
 public class Problem6 {
     // TODO 크루의 닉네임을 가져오는 메서드
     public static List<String> getNickname(List<List<String>> list) {
-        return null;
+        List<String> nickname = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            nickname.add(list.get(i).get(1));
+        }
+        return nickname;
     }
 
     // TODO 같은 글자가 연속적으로 포함되는 판단하는 메서드
