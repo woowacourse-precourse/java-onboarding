ab60fc31004826d5ca94753bd94cd50bc6c00ac9
2jun0 <soo28819@gmail.com>
2022-10-30 21:39:41 +0900

    Feat: 문제 5번 - money의 범위 확인하는 예외처리 추가

--- src/main/java/onboarding/problem5/BillCounter.java
+++ src/main/java/onboarding/problem5/BillCounter.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 public class BillCounter {
 
-    public int getCapableBillCount(int bill, int money) {
+    private int getCapableBillCount(int bill, int money) {
         return money / bill;
     }
 
     public List<Integer> getCapableBillsCount(WonBills wonbills, int money) {
+        validateMoneyRange(money);
+
         List<Integer> counts = new ArrayList<>();
 
         for (int bill: wonbills.getBills()) {
@@ -20,5 +22,11 @@ public class BillCounter {
         }
 
         return counts;
+    }
+
+    private void validateMoneyRange(int money) throws IllegalArgumentException {
+        if (!(1 <= money && money <= 1_000_000)) {
+            throw new IllegalArgumentException("money는 1이상 1,000,000이하여야 합니다.");
+        }
     }
 }
