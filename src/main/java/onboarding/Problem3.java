3171d723d17e71b4edda408b25bc853fee33cbf3
unknown <ymecca12@gmail.com>
2022-10-30 22:27:13 +0900

    [MISSION3] / feat : validationRange

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Problem3 {
     public static int solution(int number) {
+        Game.validateOutOfRange(number);
         ArrayList<Integer> dp = Game.initGame();
         for (int i = 2; i <= number; i++) {
             dp.add(i, dp.get(i-1) + Game.countContainThree(i));
@@ -13,12 +14,20 @@ public class Problem3 {
 }
 
 class Game{
+    private static final String OUT_OF_RANGE = "1이상 10000이하를 입력해 주세요";
+
     static ArrayList<Integer> initGame(){
-        ArrayList<Integer> dp = new ArrayList<Integer>();
+        ArrayList<Integer> dp = new ArrayList<>();
         dp.add(0, 0);
         dp.add(1,0);
         return dp;
     }
+
+    public static void validateOutOfRange(int number) {
+        if(number < 1 || number > 10000)
+            throw new IllegalArgumentException(OUT_OF_RANGE);
+    }
+
     static int countContainThree(int number){
         int cnt = 0;
         while(number != 0){
@@ -28,4 +37,6 @@ class Game{
         }
         return cnt;
     }
+
+
 }
