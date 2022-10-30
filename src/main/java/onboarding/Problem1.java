af13ef6998b535962bf4f19d44706370b9be3dc8
poter0501 <poter0501@gmail.com>
2022-10-30 15:13:29 +0900

    feat: checkInputs 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,9 +7,15 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
-    private static int checkInputs(List<Integer> someone){
 
+    private static int checkInputs(List<Integer> pages) {
+        int errorValue = -11;
 
-        return -1;
+        // 페이지 번호가 시작면이나 마지막면인지 확인
+        if (pages.get(0) == 1 || pages.get(1) == 399) {
+            errorValue = -1;
+        }
+
+        return errorValue;
     }
 }
