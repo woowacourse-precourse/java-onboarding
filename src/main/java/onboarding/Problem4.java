3f3d87cccc9f40ac044db2dca3056782db5081ed
yun <yshyeon102@gmail.com>
2022-10-31 02:46:11 +0900

    feat: Problem4 - Character를 encode하는 메서드 구현
    Character를 encode하는 메서드 구현
    - encodeCharacter()  : 알파벳인 경우 encode, 아닌 경우 그대로 반환하도록 구현
    
    - encodeAlphabet() : 알파벳을 encode  - 먼저, 알파벳이 소문자인지 대문자인지 판단하고, 대문자인 경우 대문자 encode, 소문자인 경우 소문자 encode를 하도록 함.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -15,4 +15,30 @@ public class Problem4 {
         }
         return convertArrayToString(encodedWords);
     }
+
+    private static char encodeCharacter(char ch) {
+        if (Character.isAlphabetic(ch)) {
+            return encodeAlphabet(ch);
+        }
+        return ch;
+    }
+
+    private static char encodeAlphabet(char ch) {
+        if (isAlphabetUpperCase(ch)) {
+            return encodeAlphabetUpperCase(ch);
+        }
+        return encodeAlphabetLowerCase(ch);
+    }
+
+    private static boolean isAlphabetUpperCase(char ch) {
+        return 'A' <= ch && ch <= 'Z';
+    }
+
+    private static char encodeAlphabetLowerCase(char ch) {
+        return (char) ('a' + 'z' - ch);
+    }
+
+    private static char encodeAlphabetUpperCase(char ch) {
+        return (char) ('A' + 'Z' - ch);
+    }
 }
