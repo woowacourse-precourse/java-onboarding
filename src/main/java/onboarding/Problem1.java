ca51dd198a26d012c352bc7bc5b3e18bb6aca3c9
JJongmen <pjy90123@gmail.com>
2022-10-30 21:45:05 +0900

    refactor(Problem1): early return 적용 및 상수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,33 +3,33 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    private static final int EXCEPTION_CASE = -1;
+    private static final int EXCEPTION = -1;
     private static final int DRAW = 0;
     private static final int POBI_WIN = 1;
     private static final int CRONG_WIN = 2;
+    private static final int LEFT = 0;
+    private static final int RIGHT = 1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (!isBoundary(pobi) || !isBoundary(crong)) {
-            return EXCEPTION_CASE;
+            return EXCEPTION;
         }
-        if (!isContinue(pobi) || !isContinue(crong)) {
-            return EXCEPTION_CASE;
+        if (!isContinuous(pobi) || !isContinuous(crong)) {
+            return EXCEPTION;
         }
         return compareScore(calculateScore(pobi), calculateScore(crong));
     }
 
-    private static int compareScore(int pobiScore, int crongScore) {
-        if (crongScore > pobiScore) {
-            return CRONG_WIN;
-        } else if (crongScore < pobiScore) {
-            return POBI_WIN;
-        } else {
-            return DRAW;
-        }
+    private static boolean isBoundary(List<Integer> pages) {
+        return 3 <= pages.get(LEFT) && pages.get(RIGHT) <= 398;
+    }
+
+    private static boolean isContinuous(List<Integer> pages) {
+        return pages.get(LEFT) + 1 == pages.get(RIGHT);
     }
 
     private static int calculateScore(List<Integer> pages) {
-        return Math.max(getPageScore(pages.get(0)), getPageScore(pages.get(1)));
+        return Math.max(getPageScore(pages.get(LEFT)), getPageScore(pages.get(RIGHT)));
     }
 
     private static int getPageScore(int page) {
@@ -50,11 +50,13 @@ class Problem1 {
                 .reduce(1, (a, b) -> a * b);
     }
 
-    private static boolean isBoundary(List<Integer> pages) {
-        return 3 <= pages.get(0) && pages.get(1) <= 398;
-    }
-
-    private static boolean isContinue(List<Integer> pages) {
-        return pages.get(0) + 1 == pages.get(1);
+    private static int compareScore(int pobiScore, int crongScore) {
+        if (crongScore > pobiScore) {
+            return CRONG_WIN;
+        }
+        if (crongScore < pobiScore) {
+            return POBI_WIN;
+        }
+        return DRAW;
     }
 }
