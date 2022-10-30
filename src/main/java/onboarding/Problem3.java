b39cfbe9ef8dbc6e6ced3f4381556d2ab1b2449e
cmw9957 <cmw9957@gmail.com>
2022-10-31 03:15:43 +0900

    comment: 기능 목록 주석 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/*
+========================================Problem2 기능 목록========================================
+- 1의 자리가 3, 6, 9인지 확인하는 기능
+- 박수의 개수를 세는 기능
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
