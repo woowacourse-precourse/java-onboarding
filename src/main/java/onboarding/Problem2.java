11bc0f66bdc6cceebe7e220c6f3919d628272860
JaeUpSu <hyeongs2323@gmail.com>
2022-10-30 18:30:32 +0900

    PROBLEM2 | 중복된 값을 모두 처리할때 까지 갱신

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,31 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (true) {
+
+            String nextCryptogram = toDeduplicate(cryptogram);
+
+            if (cryptogram.equals(nextCryptogram)) {
+                break;
+            }
+
+            cryptogram = nextCryptogram;
+
+            if (cryptogram.isEmpty()) {
+                break;
+            }
+        }
+
+        answer = cryptogram;
+
         return answer;
     }
+
+    private static String toDeduplicate(String cryptogram) {
+        String result = "";
+
+        return result;
+    }
+
 }
