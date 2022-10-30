f1ca131fe4d66de0d242f67be1cc1c5f83e83a38
kim0914 <kosa0914@naver.com>
2022-10-30 17:32:22 +0900

    feat: Problem3 상수 및 숫자 범위 예외발생 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.List;
+
+import static onboarding.Problem3.Const.*;
+
 public class Problem3 {
+
+    private static List<Integer> targetList = List.of(3, 6, 9);
+
     public static int solution(int number) {
         int answer = 0;
+        if (isWrongRange(number)){
+            throw new IllegalArgumentException("1 이상 10000이하 자연수이어야 합니다.");
+        }
+
         return answer;
+    }
+
+
+
+    private static boolean isWrongRange(int num){
+        return (num < 1 || num > 10000);
+    }
+
+
+    static class Const{
+        public static final int START_NUMBER = 1;
     }
 }
