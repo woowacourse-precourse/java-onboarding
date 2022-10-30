8f5a8647f84ac837b14462f992dab1f487521254
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 18:58:03 +0900

    type:feat 한 수를 다른 수로 나눌 때 몫과 나머지를 반환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,8 +5,15 @@ import java.util.List;
 
 public class Problem5 {
     static int[] unitOfMoney = new int[] {50000,10000,5000,1000,500,100,50,10,1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static int[] returnQuotaAndRemainder (int number, int divider) {
+        int quota = number / divider;
+        int remainder = number % divider;
+        return new int[] {quota, remainder};
     }
 }
