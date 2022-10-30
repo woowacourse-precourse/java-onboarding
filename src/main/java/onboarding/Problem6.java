5ac586fd26620c5285f0f91ed7308fd5ffbf8955
sysliar <sysliar@gmail.com>
2022-10-31 02:18:48 +0900

    Q6. refactor splitName method

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,10 +8,10 @@ public class Problem6 {
         return answer;
     }
 
-    private List<String> splitName(String name) {
+    private static List<String> splitName(String name) {
         List<String> namePiece = new ArrayList<String>();
         for (int i = 0; i < name.length() - 1; i++) {
-            namePiece.add(name.substring(i, i + 1));
+            namePiece.add(name.substring(i, i + 2));
         }
         return namePiece;
     }
