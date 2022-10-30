6ed5233bf7b06aa7b80b5868d3acb54c072f2f86
LeeJinHo <hosboy93@naver.com>
2022-10-30 17:09:52 +0900

    문제1. pobi 페이지 덧셈, 곱셈 최댓값 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,22 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        
+        int pobiMax = 0;
+        
+        for(int i=0; i<2; i++) {
+        	int pobiValue = pobi.get(i);
+            int pobiSum = 0;
+            int pobiMul = 1;
+        	
+        	while(pobiValue!=0) {
+        		pobiSum += pobiValue % 10;
+        		pobiMul *= pobiValue % 10; 
+        		pobiValue = pobiValue / 10;
+        	}
+        	pobiMax = Math.max(pobiMax, Math.max(pobiSum, pobiMul));
+        }
+        
         return answer;
     }
 }
