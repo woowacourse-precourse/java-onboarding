e5281c145102f12a30233a2bcc82a100a6ef0ff3
cmong0516 <cmong0516@gmail.com>
2022-10-31 02:33:25 +0900

    2022.10.31 Problem2 Fix

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,14 +1,14 @@
 package onboarding;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
 
         // 중복된 인덱스들을 overlapIndex 에 저장.
         ArrayList<Integer> overlapIndex = findOverlapIndex(cryptogram);
+
         // 중복된 인덱스 제거 메서드 remove()
-        remove(cryptogram, overlapIndex);
         // cryptogram 초기화
         cryptogram = remove(cryptogram, overlapIndex);
         // 중복 index 가 있다면 초기화한 cryptogram 으로 재귀호출.
@@ -20,17 +20,23 @@ public class Problem2 {
 
     // 중복 인덱스를 ArrayList<Integer> findOverlapIndex 에 저장.
     static ArrayList<Integer> findOverlapIndex(String cryptogram) {
-        ArrayList<Integer> list = new ArrayList<>();
+
         // cryptogram 을 char 배열로 만든후
         char[] chars = cryptogram.toCharArray();
+
+        // 중복 인덱스 제거를 위해 set 을 이용.
+        Set<Integer> set = new HashSet<>();
+
+
         // for 반복문을 이용하여 앞의 문자와 같을경우 앞인덱스와 현재 인덱스를 저장.
         for (int i = 1; i < chars.length; i++) {
             if (chars[i - 1] == chars[i]) {
-                list.add(i - 1);
-                list.add(i);
+                set.add(i - 1);
+                set.add(i);
             }
         }
-        return list;
+        // set 을 ArrayList 로 return
+        return new ArrayList<>(set);
     }
 
     // 중복 문자 인덱스 제거 메서드
@@ -39,9 +45,13 @@ public class Problem2 {
         StringBuilder sb = new StringBuilder();
         sb.append(cryptogram);
 
-        // for 반복문을 돌며 StringBuilder 에서 중복 인덱스를 제거.
+        // cryptogram 을 재정의 하기때문에 뒤에서부터 삭제하기 위해 reverse()
+        Collections.reverse(list);
+
+
+        // for 반복문을 돌며 StringBuilder 에서 중복 인덱스를 뒤에서부터 제거.
         for (int i = 0; i < list.size(); i++) {
-            sb.delete(list.get(0), list.get(1));
+            sb.delete(list.get(i), list.get(i) + 1);
             cryptogram = String.valueOf(sb);
         }
 
