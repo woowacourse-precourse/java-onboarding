35f5c3e9ad4499db20e1b637123a187f09c7ebb7
parkjungyoon <tbr06057@naver.com>
2022-10-30 12:03:13 +0900

    feat(PROBLEM4): 예외처리 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem4 {
+
+    private final static int MIN_RANGE_NUM = 1;
+    private final static int MAX_RANGE_NUM = 1000;
+
     public static String solution(String word) {
+        validateRange(word);
+
         String answer = "";
         return answer;
+    }
+
+    private static void validateRange(String input) {
+        int inputLength = input.length();
+        if (inputLength < MIN_RANGE_NUM || inputLength > MAX_RANGE_NUM) {
+            throw new IllegalArgumentException("엄마 말씀은 길이가 1 이상 1000 이하인 문자열이다.");
+        }
     }
 }
