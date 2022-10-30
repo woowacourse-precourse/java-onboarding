40eb09d0116b905c5fbb9cdefdf5d2d90c3e3078
jsw6701 <anes53027@gmail.com>
2022-10-31 01:54:16 +0900

    Problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) != ' '){
+                int charValue = (int)word.charAt(i);
+                if(charValue <= 90){
+                    char ch = (char)(90 - (charValue - 65));
+                    sb.append(ch);
+                }
+                else{
+                    char ch = (char)(122 - (charValue - 97));
+                    sb.append(ch);
+                }
+            }
+            else{
+                sb.append(' ');
+            }
+        }
+        return sb.toString();
     }
 }
