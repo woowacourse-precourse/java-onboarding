d15e451faab2c7925e50d86fcec249830dfbc43e
KYBee <kyb3634@naver.com>
2022-10-30 14:09:42 +0900

    :art: [Problem 3] Define & Distribute Requirements
    [Problem 3] Define & Distribute Requirements

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+    /**
+     * number을 input으로 넘기면, number까지 몇 번의 박수를 쳐야하는지 리턴하는 메서드이다.
+     *
+     * @param : int, 게임이 진행되는 마지막 숫자
+     * @return : int, 몇 번의 박수를 쳐야 하는지
+     */
     public static int solution(int number) {
+        int answer = 0;
+        return answer;
+    }
+
+    /**
+     * 게임을 number까지 진행하면 몇 번의 박수를 쳐야하는지 리턴하는 메서드이다.
+     *
+     * @param : int, 게임이 진행되는 마지막 숫자
+     * @return : int, 몇 번의 박수를 쳐야 하는지
+     */
+    public static int playGame(int number) {
         int answer = 0;
         return answer;
     }
