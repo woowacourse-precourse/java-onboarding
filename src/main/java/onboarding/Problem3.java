79e25264dbf7ab63a392d7796fa32fb2fda2b905
cmw9957 <cmw9957@gmail.com>
2022-10-31 03:20:13 +0900

    feat: 1의 자리가 3, 6, 9인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,10 @@ package onboarding;
  */
 
 public class Problem3 {
+    public static boolean checkThreeSixNine(int number){
+        number %= 10;
+        return (number == 3 || number == 6 || number == 9);
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
