4ddb8eb05963904549c3a268a6ca03613bba091a
김지건 <jigun21@naver.com>
2022-10-30 09:10:13 +0900

    Init: Problem4 초기구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,34 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        char[] characters = word.toCharArray();
+        for (char character : characters) {
+            char changedCharacter = change(character);
+            answer.append(changedCharacter);
+        }
+
+        return answer.toString();
+    }
+
+    public static char change(char originalCharacter) {
+        if (Character.isUpperCase(originalCharacter)) {
+            return changeUpperCase(originalCharacter);
+        } else if (Character.isLowerCase(originalCharacter)) {
+            return changeLowerCase(originalCharacter);
+        } else {
+            return originalCharacter;
+        }
+    }
+
+    public static char changeLowerCase(char originalCharacter) {
+        char changedCharacter = (char) ((25 - (originalCharacter - 97)) + 97);
+        return changedCharacter;
+    }
+
+    public static char changeUpperCase(char originalAlphabet) {
+        char changedAlphabet = (char) ((25 - (originalAlphabet - 65)) + 65);
+        return changedAlphabet;
     }
 }
