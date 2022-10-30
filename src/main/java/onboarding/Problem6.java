865eac219d4a53008a00a288ee7b06a8f0a28111
Noah <shgustmd6865@naver.com>
2022-10-30 16:08:40 +0900

    Refactor : 닉네임을 가져오는 코드의 중복 방지를 위해 함수로 분리
    닉네임을 가지는 코드가 nickName, nextNickName에서 중복되어 함수로 분리합니다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
+
+    private static final int EMAIL_IDX = 0;
+    private static final int NICKNAME_IDX = 1;
+
     public static List<String> solution(List<List<String>> forms) {
         Set<String> resultSet = new HashSet<>();
 
@@ -10,7 +14,7 @@ public class Problem6 {
 
         for (int index = 0; index < forms.size(); index++) {
             if (!isSimilar[index]) {
-                String nickname = forms.get(index).get(1);
+                String nickname = getNickname(forms, index);
                 Set<String> patternsFromNickName = makePatternsFromNickName(nickname);
 
                 for (int nextIndex = 0; nextIndex < forms.size(); nextIndex++) {
@@ -18,7 +22,7 @@ public class Problem6 {
                     if (index == nextIndex || isSimilar[nextIndex])
                         continue;
 
-                    String nextNickname = forms.get(nextIndex).get(1);
+                    String nextNickname = getNickname(forms, nextIndex);
                     for (String pattern : patternsFromNickName) {
                         if (nextNickname.contains(pattern)) {
                             resultSet.add(forms.get(index).get(0));
@@ -46,5 +50,9 @@ public class Problem6 {
         patterns.add(nickname);
 
         return patterns;
+    }
+
+    public static String getNickname(List<List<String>> forms, Integer index) {
+        return forms.get(index).get(NICKNAME_IDX);
     }
 }
