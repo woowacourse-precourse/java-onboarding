542e11b5aa9cc02342581227d8dfedc1ba490133
K-jun98 <alap_u@naver.com>
2022-10-30 14:43:56 +0900

    refactor - 전역 변수 제거

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,13 +6,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    static List<Integer> moneyUnit = Arrays.asList(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        return getUnitCount(money);
-    }
-
-    private static List<Integer> getUnitCount(int money) {
+        List<Integer> moneyUnit = Arrays.asList(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
         List<Integer> unitCount = new ArrayList<>();
+
         for (int i = 0; i < moneyUnit.size(); i++) {
             unitCount.add(money / moneyUnit.get(i));
             money %= moneyUnit.get(i);
@@ -20,3 +17,4 @@ public class Problem5 {
         return unitCount;
     }
 }
+
