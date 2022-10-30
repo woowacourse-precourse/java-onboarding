b44c8ec733b9c049ec8b19f358b9ecff4a92a955
ONE <sodlapdlf13@gmail.com>
2022-10-30 18:12:34 +0900

    docs: [Problem5] 필요 기능에 대한 주석을 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+* 1. 각 금액을 가진 리스트 생성
+* 2. 금액이 큰 순서부터 돈을 나누기 연산한 몫을 결과 리스트에 넣고 나머지 연산을 통해 현재 돈을 갱신
+*/
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
