59154bbcc9ab8ffb3dcac7e080110cc91de93d6c
myeongju00 <j51677767@gmail.com>
2022-10-30 16:45:37 +0900

    feat: 알파벳 배열 생성 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    private static Map<Character, Character> map = new HashMap<>();
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+    //
+    private static void insertIntoMap() {
+        for (int i = 0; i <= 'z' - 'a'; i++) {
+            map.put((char) ('a' + i), (char) ('z' - i));
+            map.put((char) ('A' + i), (char) ('Z' - i));
+        }
     }
 }
