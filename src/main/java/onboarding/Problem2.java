00c1bc986c1c385cfd10218435581ee76896c1fe
cgk95 <kjkj5868@gmail.com>
2022-10-30 21:11:40 +0900

    P2 중복제거하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -27,5 +27,25 @@ public class Problem2 {
             }
             return nxt.toString();
         }
+        /** 중복을 제거하는 메인 기능 **/
+        String remove_rpt(String cryptogram,myfunc02 mf){
+            Stack<String> stk = new Stack<>();       /*스택 선언*/
+            stk.push(String.valueOf(cryptogram.charAt(0)));/*초기화*/
+
+            boolean flag=false; /*찌꺼기 확인용*/
+            for (int i = 1; i < cryptogram.length(); i++) {
+                if (!String.valueOf(cryptogram.charAt(i)).equals(stk.peek())){ /* 이전의 원소와 같지않다면 */
+                    if (flag){ /*찌꺼기가 있었다면 제거해준다*/
+                        stk.pop();
+                        flag=false;
+                    }
+                    stk.push(String.valueOf(cryptogram.charAt(i)));
+                }else { /*같은 원소가 있다면*/
+                    flag=true; /*현재 원소를 스택에 추가하지 않고 넘어간다*/
+                }
+            }
+            if (flag) stk.clear();
+            return mf.nxtCrypto(stk); /*일순한 문자열을 다음 다시 재전달*/
+        }
     }
 }
