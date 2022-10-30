3b12aa7373cb24528bcb9b1809ca3b0903b98b0f
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 16:26:53 +0900

    type:feat 문자열에서 중복된 문자를 찾는 기능과 그 시작 index와 끝 index를 반환하는 기능을 분리하여 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static int[] findIndexOfDuplicates (String cryptogram) {
+        int startIndex = -1;
+        int endIndex = -1;
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                startIndex = i;
+                endIndex = findEndIndexOfDuplicates (cryptogram, startIndex);
+            }
+        }
+        return new int[] {startIndex, endIndex};
+    }
+
+    private static int findEndIndexOfDuplicates (String cryptogram, int startIndex) {
+        char startIndexChar = cryptogram.charAt(startIndex);
+        int endIndex = startIndex+1;
+        while (cryptogram.charAt(endIndex) == startIndexChar) {
+            endIndex++;
+        }
+        endIndex -= 1;
+        return endIndex;
     }
 }
