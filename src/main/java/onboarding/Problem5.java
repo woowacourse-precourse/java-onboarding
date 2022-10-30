91c13d7d334ce6dc24d5fc613d4892ba0b992642
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-30 23:41:25 +0900

    chore: Problem5 기능목록 오타수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,8 +6,8 @@ import java.util.*;
  * 구현 기능 목록
  * 1. greedy를 이용해 최적의 해를 찾도록 함
  * 2. 돈의 액수를 나열한 배열을 생성한다. {50000, 10000 … 10, 1}
- * 2. 돈의 액수를 담은 배열을 순회하면서, 변환된 개수를 list에 담는다.
- * 3. list를 반환한다.
+ * 3. 돈의 액수를 담은 배열을 순회하면서, 변환된 개수를 list에 담는다.
+ * 4. list를 반환한다.
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
