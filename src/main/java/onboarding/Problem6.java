03205a59db5ee4b4acfdb15e04803adb8e416dcc
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-30 19:00:43 +0900

    feat(PROBLEM6): duplicateName 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    public static List<String> duplicateName(List<List<String>> forms){
+        List<String> dupCheck = new ArrayList<>();
+        List<String> dupList = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            String name = forms.get(i).get(2);
+            for (int j = 0; j < name.length(); j++) {
+                String splitName = name.substring(j,j+1);
+                if(dupCheck.contains(splitName) && !dupList.contains(splitName)){
+                    dupList.add(splitName);
+                } else if(!dupCheck.contains(splitName)) {
+                    dupCheck.add(splitName);
+                }
+            }
+        }
+        return dupList;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
