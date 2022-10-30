cf763f89d5bf1c1a870aea15a590b55ef4816827
sohyundoh <lyny123@naver.com>
2022-10-31 08:34:53 +0900

    Problem 6 기능 목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,13 @@
 package onboarding;
 
-import java.util.List;
-
+import java.util.*;
+/*
+기능 목록
+1. 닉네임 길이별로 나눠서 map에 저장
+2. 닉네임을 포함하고 있으면 answer에 추가, 만일 answer에 중복되는 이메일이 없다면 추가
+** 제한 사항 **
+answer의 중복 제거, 알파벳 순서 나열
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
