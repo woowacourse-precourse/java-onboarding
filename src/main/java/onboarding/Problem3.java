800ec90711652b88b092a5433165ff8bc0b5eca8
parksey <parkseyeon99@naver.com>
2022-10-30 13:34:43 +0900

    feat(problem 3): 기능 요구사항 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,22 @@
 package onboarding;
 
+import java.util.*;
+
+/**
+ * Problem 3
+ *
+ * 기능 요구 사항
+ * 1. 3, 6, 9가 들어가면 갯수만큼 손뼉을 쳐야한다.
+ * 2. 1부터 number까지 쳐야하는 횟수
+ *
+ * 제한 사항
+ * 1. number는 1부터 10,000이하의 자연수
+ */
 public class Problem3 {
+    /**
+     * @param number
+     * @return 손뼉 횟수
+     */
     public static int solution(int number) {
         int answer = 0;
         return answer;
