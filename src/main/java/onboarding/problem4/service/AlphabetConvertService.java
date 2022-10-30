eca6a783ff4628114cd8b2d76ca585bbd3946e8a
song-wooseok <sws981202@gmail.com>
2022-10-31 03:22:24 +0900

    feat: [PROBLEM4 - 기능 5] 변환된 문자을 저장하는 기능

--- src/main/java/onboarding/problem4/service/AlphabetConvertService.java
+++ src/main/java/onboarding/problem4/service/AlphabetConvertService.java
@@ -1,6 +1,24 @@
 package onboarding.problem4.service;
 
+import onboarding.problem4.utils.CharType;
+
+import java.util.ArrayList;
+
 public class AlphabetConvertService {
+
+   public static ArrayList<Character> convertContray(ArrayList<Character> charList){
+      ArrayList<Character> convertedCharList = new ArrayList<>();
+      for(char character: charList){
+         convertedCharList.add(convertChar(character));
+      }
+      return convertedCharList;
+   }
+
+   private static char convertChar(char character){
+      CharType currentChar = CharTypeCheckService.checkCharType(character);
+      return currentChar.convert(character);
+   }
+
    public static char convert(char character , int startAsciiCode, int endAsciiCode){
 
       int originAsciiCode = (int)(character);
@@ -8,6 +26,7 @@ public class AlphabetConvertService {
 
       return asciiToChar(convertedAsciiCode);
    }
+
 
    private static int asciiConverter(int originAsciiCode, int startAsciiCode, int endAsciiCode){
       int convertedAsciiCode = originAsciiCode + (endAsciiCode - originAsciiCode - (originAsciiCode - startAsciiCode));
@@ -17,4 +36,6 @@ public class AlphabetConvertService {
    private static char asciiToChar(int ascii){
       return (char)(ascii);
    }
+
+
 }
