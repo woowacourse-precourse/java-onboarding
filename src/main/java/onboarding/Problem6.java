cc7cf45c2dcf34c1484f4892e8b6aec3f78dde78
lvalentine6 <ysr1227@naver.com>
2022-10-30 18:07:24 +0900

    feat : 문제6_조합을 다른 닉네임과 비교하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,32 +1,48 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Set<String> set = new TreeSet<>();
+
         for (int i = 0; i < forms.size(); i++) {
             String[] nickArr = forms.get(i).get(1).split("");
+            List<String> list = new ArrayList<>();
 
-            combination(nickArr, "", 0, 0);
+            combination(list, nickArr, "", 0, 0);
+
+            compare(forms, list, set, i);
         }
+
+        answer = new ArrayList<>(set);
 
         return answer;
     }
 
     // 닉네임 조합 만드는 메서드
-    static void combination(String[] nickArr, String str, int idx, int depth) {
+    static void combination(List<String> list, String[] nickArr, String str, int idx, int depth) {
         if (depth > 1) {
-            System.out.println(str);
+            list.add(str);
         }
 
         for (int i = idx; i < nickArr.length; i++) {
-            combination(nickArr,  str + nickArr[i], i + 1, depth + 1);
+            combination(list, nickArr,  str + nickArr[i], i + 1, depth + 1);
             if(str.length() > 0) {
                 return;
+            }
+        }
+    }
+
+    // 조합을 다른 닉네임과 비교하는 메서드
+    static void compare(List<List<String>> forms, List<String> list, Set<String> set, int i) {
+        for (int j = i + 1; j < forms.size(); j++) {
+            for (int k = 0; k < list.size(); k++) {
+                if(forms.get(j).get(1).contains(list.get(k))) {
+                    set.add(forms.get(j).get(0));
+                    set.add(forms.get(i).get(0));
+                }
             }
         }
     }
