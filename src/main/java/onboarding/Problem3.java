65d1941c30fc2e6e04162148efd17a0b17fdcc76
Woojin Lee <dhkep03@gmail.com>
2022-10-30 12:40:20 +0900

    p3-feat: 기능 구현을 위한 solution 메서드 설계

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += count369ByNumber(i);
+        }
         return answer;
     }
 }
