8a60a7f394754edd4c11bd01bd5db035ea271eea
wonjunYou <youwjune@gmail.com>
2022-10-30 20:49:56 +0900

    feat : (Problem 5) 입력값의 크기를 검증하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static final int MAX_VALUE_OF_MONEY = 1_000_000;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        try {
+            validateInputSize(money);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
         return answer;
+    }
+
+    private static void validateInputSize(int money) {
+        if (money < 1 || money > MAX_VALUE_OF_MONEY) {
+            throw new IllegalArgumentException("허용되지 않는 크기의 입력값입니다.");
+        }
     }
 }
