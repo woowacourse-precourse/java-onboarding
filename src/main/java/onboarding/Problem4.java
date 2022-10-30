b075325564064c4eaaab3ae9d43eff3d324f26a7
CMHyuk <alsgur9710@naver.com>
2022-10-30 15:23:34 +0900

    feat: 문자가 빈칸일 경우 처리 로직

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char alphabet = word.charAt(i);
+            isBlank(sb, alphabet);
+        }
+
+        answer = sb.toString();
         return answer;
+    }
+    private static void isBlank(StringBuilder sb, char alphabet) {
+        if (alphabet == ' ') {
+            sb.append(' ');
+        }
     }
 }
