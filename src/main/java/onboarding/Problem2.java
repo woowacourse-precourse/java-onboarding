5eefe6870104bbe772ab519337a61c4ad4916327
Jinhui <jda0526@naver.com>
2022-10-31 00:15:47 +0900

    Feat(Problem2): [F-01], [F-02] 연속하는 중복 문자 찾고, 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        sb.append(cryptogram.charAt(0));
+
+        char temp = cryptogram.charAt(0);
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if(temp == cryptogram.charAt(i)){
+                temp = cryptogram.charAt(i);
+                sb.setLength(sb.length() - 1);
+                System.out.println("if : " + sb.toString());
+            }
+            else{
+                temp = cryptogram.charAt(i);
+                sb.append(cryptogram.charAt(i));
+                System.out.println("else : " + sb.toString());
+            }
+        }
+        System.out.println(sb.toString());
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String cryptogram = "browoanoommnaon";
+        solution(cryptogram);
     }
 }
