9cb7b1a82dabd3308939950f5ed1e3036fceb48d
kite707 <kite707707@gmail.com>
2022-10-30 21:28:57 +0900

    solve problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+    public static char change(char input){
+        int code = input;
+        char output='a';
+        if(input == ' '){
+            return ' ';
+        }else{
+            if(code>90){
+                output = (char) (219-code);
+            }else{
+
+                output=(char)(155-code);
+            }
+            return output;
+        }
+
+    }
+
     public static String solution(String word) {
         String answer = "";
+        for(int i = 0; i<word.length();i++){
+            answer+=change(word.charAt(i));
+        }
         return answer;
     }
 }
