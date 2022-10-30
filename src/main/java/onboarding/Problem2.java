da1c4b361df237b0fdeeaddb30c1a199bd6c7b5b
YooSuYeon <usuyn000@gmail.com>
2022-10-31 03:49:08 +0900

    feat(Problem2): 연속 중복 문자 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        Pattern pattern = Pattern.compile("([a-z])\\1+");
+
+        cryptogram = pattern.matcher(cryptogram).replaceAll("");
+
+        answer = cryptogram;
+
         return answer;
     }
 }
