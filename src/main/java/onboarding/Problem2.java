1cd3532cec6ebeb8854f3cfc9efdbaee46c6bdff
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-31 01:28:45 +0900

    feat: Create getSuccessiveIndexArrayList | [problem 2]
    - 연속한 문자열의 인덱스를 저장한다
        - 저장한 값 중 중복값은 삭제하고 반환한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static ArrayList<Integer> getSuccessiveIndexArrayList(String str) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                arrayList.add(i);
+                arrayList.add(i + 1);
+            }
+        }
+
+        return new ArrayList<>(new HashSet<>(arrayList));
     }
 }
