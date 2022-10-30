db30f19a4ae373fa07d5a50234b8e6141e175549
oreo <ohsimon77@naver.com>
2022-10-30 22:29:22 +0900

    문제 3번 commit

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        String[] games = {"3","6","9"};
+
+        int count = 0;
+
+        for (int i=0;i < number+1; i++) {
+            String str = Integer.toString(i);
+            ArrayList<String> myList = new ArrayList<>(Arrays.asList(str.split("")));  //str to list
+            //System.out.println(myList);
+            for (String my : myList) {
+                for (String game : games) {
+                    if (my.contains(game)) {
+                        count += 1;
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
