0ca96f7c4090c71dd51510b94c381ff7a221cb8b
apptie <appti@naver.com>
2022-10-31 02:24:22 +0900

    comment: 문제 3 클래스 주석 추가

--- src/main/java/onboarding/problem3/ClapGamePlayer.java
+++ src/main/java/onboarding/problem3/ClapGamePlayer.java
@@ -4,11 +4,23 @@ import static onboarding.common.consts.CommonDigitNumberConst.CALCULATE_REMAINDE
 import static onboarding.common.consts.CommonDigitNumberConst.DECIMAL_UNIT_VALUE;
 import static onboarding.problem3.consts.ClapGameConst.*;
 
+/**
+ * 3, 6, 9 게임을 진행하고 결과를 반환하는 클래스
+ */
 public class ClapGamePlayer {
 
+    /**
+     * new 연산자를 통한 생성을 방지하기 위한 private 기본 생성자
+     */
     private ClapGamePlayer() {
     }
 
+    /**
+     * 3, 6, 9 게임에서 게임의 목표 값까지 총 박수 횟수를 구하는 메소드
+     * @param number 게임의 목표 값
+     * @return 총 박수 횟수
+     * @see onboarding.problem3.consts.ClapGameConst
+     */
     public static int calculateTotalClapCount(int number) {
         int totalClapCount = 0;
 
@@ -18,6 +30,13 @@ public class ClapGamePlayer {
         return totalClapCount;
     }
 
+    /**
+     * 주어진 숫자에서 박수를 몇 번 쳐야하는지 구하는 메소드
+     *
+     * @param target 박수를 몇 번 쳐야하는지 구해야 하는 숫자
+     * @return 박수 횟수
+     * @see onboarding.common.consts.CommonDigitNumberConst
+     */
     private static int calculateTargetClapCount(int target) {
         int clapCount = 0;
 
@@ -29,6 +48,13 @@ public class ClapGamePlayer {
         return clapCount;
     }
 
+    /**
+     * 주어진 자릿수에서 박수를 친 횟수를 반환하는 메소드
+     *
+     * @param targetDigit 자릿수
+     * @return 박수를 친 횟수(0, 1)
+     * @see onboarding.problem3.consts.ClapGameConst
+     */
     private static int calculateTargetDigitClapCount(int targetDigit) {
         if (isClapped(targetDigit)) {
             return CLAPPED_VALUE;
@@ -36,6 +62,13 @@ public class ClapGamePlayer {
         return NOT_CLAPPED_VALUE;
     }
 
+    /**
+     * 주어진 자릿수가 박수를 쳐야하는지 유뮤를 검증하는 메소드
+     *
+     * @param targetDigit 자릿수
+     * @return 박수를 칠 것인지에 대한 유무
+     * @see onboarding.problem3.consts.ClapGameConst
+     */
     private static boolean isClapped(int targetDigit) {
         return targetDigit == CLAP_THREE_VALUE || targetDigit == CLAP_SIX_VALUE
             || targetDigit == CLAP_NINE_VALUE;
