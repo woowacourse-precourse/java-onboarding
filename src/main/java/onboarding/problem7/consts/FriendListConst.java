7119c6c164842f4f31021c3ddd1b6d41177dfb02
apptie <appti@naver.com>
2022-10-31 00:21:28 +0900

    comment: 상수 주석 추가

--- src/main/java/onboarding/problem7/consts/FriendListConst.java
+++ src/main/java/onboarding/problem7/consts/FriendListConst.java
@@ -1,8 +1,22 @@
 package onboarding.problem7.consts;
 
+/**
+ * 주어진 친구 리스트의 인덱스를 상수로 정의한 추상 클래스
+ */
 public abstract class FriendListConst {
 
+    /**
+     * 친구 A 인덱스(0)
+     */
     public static final int FRIEND_A_INDEX = 0;
+
+    /**
+     * 친구 B 인덱스(1)
+     */
     public static final int FRIEND_B_INDEX = 1;
+
+    /**
+     * 최대 친구 추천 수
+     */
     public static final int RECOMMEND_FRIEND_LIST_LIMIT = 5;
 }
