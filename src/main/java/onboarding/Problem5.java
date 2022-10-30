b6d6e33fe3ea3d830141afc2fcbec470a2098888
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 21:18:26 +0900

    feat: Problem5 - solution 구현
    메서드를 사용하여 각 카운트 List를 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,7 +2,7 @@
 
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,8 +14,9 @@ public class Problem5 {
 
     final static int[] AMOUNT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> count = new ArrayList<>();
+
+        return moneyCount(count, money);
     }
 
     private static List<Integer> moneyCount(List<Integer> count, int money) {
