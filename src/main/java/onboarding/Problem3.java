9edb20d52cd515ef9115919731b2d4c15e775e70
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-30 19:38:31 +0900

    feat: problem3 1부터 number까지 3,6,9 게임하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return playGame(number);
     }
+
+    /**
+     * 1부터 number까지 3,6,9 게임하기
+     * @param number
+     * @return int
+     */
+    private static int playGame(int number) {
+        int clap = 0;
+        for (int i = 1; i <= number; i++) {
+            clap = clapping(i, clap);
+        }
+        return clap;
+    }
+
 }
