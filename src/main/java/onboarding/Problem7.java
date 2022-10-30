fb2a69df761f8a9cc15b8b63b50154ac7c65ad4f
CMHyuk <alsgur9710@naver.com>
2022-10-30 20:45:37 +0900

    feat: 함께 아는 친구, 점수 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,8 +10,30 @@ public class Problem7 {
         List<String> answer;
 
         ArrayList<String> withFriends = storeUserFriends(user, friends);
+        Map<String, Integer> friendAndScore = storeFriendAndScore(user, friends, withFriends);
 
         return answer;
+    }
+
+    private static Map<String, Integer> storeFriendAndScore(String user, List<List<String>> friends, ArrayList<String> withFriends) {
+        Map<String, Integer> friendScore = new HashMap<>();
+        for (int i = 0; i < withFriends.size(); i++) {
+            String withFriend = withFriends.get(i);
+            for (int j = 0; j < friends.size(); j++) {
+                List<String> knowFriendList = friends.get(j);
+                if (knowFriendList.contains(withFriend)) {
+                    for (int k = 0; k < knowFriendList.size(); k++) {
+                        String knowFriend = knowFriendList.get(k);
+                        if (!withFriend.equals(knowFriend) && !knowFriendList.contains(user)) {
+                            if (!friendScore.containsKey(knowFriend)) {
+                                friendScore.put(knowFriend, FRIEND_SCORE);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return friendScore;
     }
 
     private static ArrayList<String> storeUserFriends(String user, List<List<String>> friends) {
