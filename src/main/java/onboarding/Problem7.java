d8f72c39601f35f4401d36ba5aa78bb7a57bceed
EUNSH1 <tkdgus968@naver.com>
2022-10-31 02:50:12 +0900

    feat : [Problem7] 함께 아는 친구의 아이디와 점수를 TreeMap에 저장하는 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class Problem7 {
 
     static Map<String, Integer> map = new TreeMap<>();
-    
+
     /**
      * 1.친구목록을 통해 사용자와 함께아는친구 찾기
      */
@@ -27,6 +27,36 @@ public class Problem7 {
         }
 
         return friendList;
+    }
+
+    /**
+     * 2.함께 아는 친구의 아이디와 점수를 TreeMap에 저장하는 함수
+     * 친구목록에서 사용자와 함께 아는 친구일 경우 +10점
+     * 사용자 제외
+     */
+    public static void friendsScore(List<String> userFriend, List<List<String>> friends, String user) {
+        for(int i =0;i<userFriend.size();i++){
+            for(int j=0; j<friends.size();j++){
+                if(userFriend.get(i).equals(friends.get(j).get(0)) && !friends.get(j).get(1).equals(user)){
+                    if(map.containsKey(friends.get(j).get(1))){
+                        int value = map.get(friends.get(j).get(1));
+                        map.replace(friends.get(j).get(1), value+10);
+                    } else{
+                        map.put(friends.get(j).get(1),10);
+                    }
+                }
+
+                if (userFriend.get(i).equals(friends.get(j).get(1)) && !friends.get(j).get(0).equals(user)){
+                    if(map.containsKey(friends.get(j).get(0))){
+                        int value = map.get(friends.get(j).get(0));
+                        map.replace(friends.get(j).get(0), value+10);
+                    } else{
+                        map.put(friends.get(j).get(0),10);
+                    }
+                }
+
+            }
+        }
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
