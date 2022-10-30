4cfbead52daeebd676d8b8d091f0885839f3492a
chkim <cndghks0213@gmail.com>
2022-10-30 19:57:46 +0900

    feat(problem6): 입력을 회원 목록으로 변환하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        List<Crew> crews = allCrews(forms);
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<Crew> allCrews(List<List<String>> forms) {
+        return forms.stream()
+                .map(form -> new Crew(form.get(0), form.get(1)))
+                .collect(Collectors.toList());
     }
 }
