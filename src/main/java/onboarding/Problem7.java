bcf36b1353b91783b1f3f9c19dffcc89d6d2f680
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-31 05:18:37 +0900

    feat(PROBLEM7): checkUserFriend 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem7 {
+    public static List<String> checkUserFriend(String user, List<List<String>> friends){
+        List<String> userFriend = new ArrayList<>();
+        List<List<String>> checkF = friends.stream()
+                .filter(strings -> strings.contains(user))
+                .collect(Collectors.toList());
+        for (int i = 0; i < checkF.size(); i++) {
+            for (int j = 0; j < checkF.get(i).size(); j++) {
+                String name = checkF.get(i).get(j);
+                if(!userFriend.contains(name) && !name.contains(user))
+                    userFriend.add(name);
+            }
+        }
+        return userFriend;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
