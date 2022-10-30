9bc228abe197cbeafa7eb7a0b54db2a8f60fd03c
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 21:20:03 +0900

    docs: Problem6 - 기능 구현 목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ *  isProperForms 메서드 적절한 형태인지 확인
+ *  isProperEmail 메서드 적절한 이메일인지 확인
+ *  isProperNickName 매서드 적절한 닉네임인지 확인
+ *  getOverlapNickName 메서드 2글자씩잘라서 중복된 값이 있다면 그 닉네임을 Map에 저환
+ */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
