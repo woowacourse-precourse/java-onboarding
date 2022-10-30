f6e45e8af531034990f00c52c013112e5477030b
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-30 18:17:03 +0900

    Add convertChar function

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,23 +10,25 @@ public class Problem4 {
     }
 
     // HashMap를 청개구리 사전으로 초기화 함수
-    public static HashMap<String, String> initFrogTable() {
-        HashMap<String, String> FrogTable = new HashMap<String, String>();
+    public static HashMap<char, char> initFrogTable() {
+        HashMap<char, char> FrogTable = new HashMap<String, String>();
         for (char i = 'a', j = 'z'; i <= 'z'; i++, j--) {
-            FrogTable.put(Character.toString(i), Character.toString(j));
+            FrogTable.put(i, j);
         }
         for (char i = 'A', j = 'Z'; i <= 'Z'; i++, j--) {
-            FrogTable.put(Character.toString(i), Character.toString(j));
+            FrogTable.put(i, j);
         }
         return FrogTable;
     }
 
     // 주어진 문자를 청개구리 사전을 통해 변환하는 함수
-    public static String convertOneString(String s, HashMap<String, String> frogTable) {
-        String changeString = s;
+    public static char convertChar(char s, HashMap<char, char> frogTable) {
+        char changeString = s;
         if (frogTable.containsKey(s)) {
             changeString = frogTable.get(s);
         }
         return changeString;
     }
+
+
 }
