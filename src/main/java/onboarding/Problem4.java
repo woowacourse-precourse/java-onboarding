b7a689dc413cfa45287c7f08576d9daf1eab0674
cookienc <kim76744@naver.com>
2022-10-30 22:14:59 +0900

    refactor(Problem4): Problem4 리팩토링 - 문자를 상수화 시킴

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,8 +6,12 @@ public class Problem4 {
     public static final String WORD_LENGTH_IS_TOO_LONG_OR_SHORT = "문자의 길이가 너무 짧거나 너무 깁니다.";
     public static final int MIN_WORD_LENGTH = 1;
     public static final int MAX_WORD_LENGTH = 1000;
+	public static final char UPPER_LETTER_START = 'A';
+	public static final char LOWER_LETTER_START = 'a';
+	public static final char UPPER_LETTER_END = 'Z';
+	public static final char LOWER_LETTER_END = 'z';
 
-    private static char[] reverseUpperLetter;
+	private static char[] reverseUpperLetter;
 	private static char[] reverseLowerLetter;
 
 	static {
@@ -17,7 +21,7 @@ public class Problem4 {
 
 	private static void initReverseUpperLetter() {
 		reverseUpperLetter = new char[ALPHABET_SIZE];
-		char upperLetter = 'Z';
+		char upperLetter = UPPER_LETTER_END;
 
 		for (int i = 0; i < ALPHABET_SIZE; i++) {
 			reverseUpperLetter[i] = upperLetter;
@@ -27,7 +31,7 @@ public class Problem4 {
 
 	private static void initReverseLowerLetter() {
 		reverseLowerLetter = new char[ALPHABET_SIZE];
-		char lowerLetter = 'z';
+		char lowerLetter = LOWER_LETTER_END;
 
 		for (int i = 0; i < ALPHABET_SIZE; i++) {
 			reverseLowerLetter[i] = lowerLetter;
@@ -36,11 +40,9 @@ public class Problem4 {
 	}
 
 	public static String solution(String word) {
-
         validateWord(word);
 
         char[] wordToChars = word.toCharArray();
-
         StringBuilder result = appendAlphabet(wordToChars);
 
         return result.toString();
@@ -56,38 +58,42 @@ public class Problem4 {
         StringBuilder result = new StringBuilder();
 
         for (char wordToChar : wordToChars) {
-            if (isUpperCase(wordToChar)) {
-                appendReverseUpperAlphabet(result, wordToChar);
-                continue;
-            }
-
-            if (isLowerCase(wordToChar)) {
-                appendReverseLowerAlphabet(result, wordToChar);
-                continue;
-            }
-
-            result.append(wordToChar);
-        }
+			appendWord(result, wordToChar);
+		}
         
         return result;
     }
 
-    private static boolean isUpperCase(char wordToChar) {
-        return 'A' <= wordToChar && wordToChar <= 'Z';
+	private static void appendWord(StringBuilder result, char wordToChar) {
+		if (isUpperCase(wordToChar)) {
+			appendReverseUpperAlphabet(result, wordToChar);
+			return;
+		}
+
+		if (isLowerCase(wordToChar)) {
+			appendReverseLowerAlphabet(result, wordToChar);
+			return;
+		}
+
+		result.append(wordToChar);
+	}
+
+	private static boolean isUpperCase(char wordToChar) {
+        return UPPER_LETTER_START <= wordToChar && wordToChar <= UPPER_LETTER_END;
     }
 
     private static boolean isLowerCase(char wordToChar) {
-        return 'a' <= wordToChar && wordToChar <= 'z';
+        return LOWER_LETTER_START <= wordToChar && wordToChar <= LOWER_LETTER_END;
     }
 
     private static void appendReverseUpperAlphabet(StringBuilder result, char wordToChar) {
-        int index = wordToChar - 'A';
+        int index = wordToChar - UPPER_LETTER_START;
         char reverse = reverseUpperLetter[index];
         result.append(reverse);
     }
 
     private static void appendReverseLowerAlphabet(StringBuilder result, char wordToChar) {
-        int index = wordToChar - 'a';
+        int index = wordToChar - LOWER_LETTER_START;
         char reverse = reverseLowerLetter[index];
         result.append(reverse);
     }
