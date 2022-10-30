8e9cb34408bd5db7ed695a532c147e7c05a8cd5f
chopinballadeno4 <qo98333@naver.com>
2022-10-30 22:14:01 +0900

    problem 6, function 3 check email overlap and insert array

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -39,9 +39,27 @@ public class Problem6 {
     }
 
 
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = new ArrayList<>();
+        boolean [] isNotNeedCheckApplicationList = new boolean[forms.size()];
 
+        for(int i=0 ; i<forms.size()-1 ; i++) {
+            if(isNotNeedCheckApplicationList[i]) continue;
+            for(int j = i+1 ; j<forms.size() ; j++) {
+                if(isNotNeedCheckApplicationList[j]) continue;
+                if(checkNicknameOverlap(forms.get(i).get(1), forms.get(j).get(1))) {
+                    isNotNeedCheckApplicationList[i] = true;
+                    isNotNeedCheckApplicationList[j] = true;
+
+                    if(checkEmailFormOverlap(answer, forms.get(i).get(0))) answer.add(forms.get(i).get(0));
+                    if(checkEmailFormOverlap(answer, forms.get(j).get(0))) answer.add(forms.get(j).get(0));
+                }
+            }
+        }
+
+        Collections.sort(answer);
 
         return answer;
     }
