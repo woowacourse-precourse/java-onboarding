16f618e60be811e9da16a8eb660caadfc70230d1
Nugur1i <duckhee092@naver.com>
2022-10-31 02:19:30 +0900

    feat: Add exception

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,28 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = pobi.get(1);
+
+        // 예외사항 작성
+        if ( pobiLeftPage < 3 || crongLeftPage < 3 || pobiLeftPage > 397 || crongLeftPage > 397 ) {
+            return -1;
+        } else if ( pobiRightPage != pobiLeftPage + 1 || crongRightPage != crongLeftPage + 1) {
+            return -1;
+        } else if ( pobiRightPage%2 != 0 || crongRightPage%2 != 0 ) {
+            return -1;
+        } else if ( pobi.size() != 2 || crong.size() != 2 ){
+            return -1;
+        }
+
+
+
+
         int answer = Integer.MAX_VALUE;
-        int ans = 1;
         return answer;
     }
+
 }
