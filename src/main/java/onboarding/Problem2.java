ac7e8220a33896b055a079432b7bb2f650d62746
chobobdev <chobobdev@naver.com>
2022-10-31 01:06:15 +0900

    feat: remove the repeating letter once.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        char[] chars = cryptogram.toCharArray();
+        for(int i =0;i<cryptogram.length()-1;i++){
+            if(chars[i]==chars[i+1]){
+                cryptogram=cryptogram.substring(0,i)+cryptogram.substring(i+2);
+                System.out.println(cryptogram);
+                break;
+            }
+        }
         String answer = "answer";
         return answer;
     }
