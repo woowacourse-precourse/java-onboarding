c820abacc615b074ed3aa7aa6426bee7702848f4
Jeawoong Kim <kimje1109@gmail.com>
2022-10-30 23:40:25 +0900

    feat: Add getMultiplyOfDigits

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int getMultiplyOfDigits(int number) {
+        int multiply = 1;
+        while(number != 0){
+            multiply *= number%10;
+            number /= 10;
+        }
+
+        return multiply;
+    }
+
     public static int getSumOfDigits(int number) {
         int sum = 0;
         while(number != 0){
