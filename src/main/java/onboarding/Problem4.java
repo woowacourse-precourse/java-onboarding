f8d3adcf6d422248ae6454c716ad64db30fef59a
poter0501 <poter0501@gmail.com>
2022-10-30 22:42:51 +0900

    feat: createFrogDictionary 함수 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    private static HashMap<Character, Character> frogDictionary;
+    private static String matchStr="ZYXWVUTSRQPONMLKJIHGFEDCBA";
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+    private static void createFrogDictionary(){
+        for (int i=65; i<91;i++){
+            frogDictionary.put((char)i, matchStr.charAt(i-65));
+        }
+        for (int i=97; i<123;i++){
+            frogDictionary.put((char)i, matchStr.charAt(i-97));
+        }
     }
 }
