c271919b11ed809a7b01469f3ba4a21559d99ed9
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-31 02:16:48 +0900

    feat: Create createOppositeAlphabetMap | [problem 4]
    - 청개구리 사전을 만드는 메서드
    - a의 대소문자를 char 로 리스트로 저장해두고, a와 A의 아스키값과 알파벳 갯수를 활용하여 키 와 값을 Opposite 한 값으로 저장한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+}
+
+class OppositeAlphabet {
+    private final List<Character> UpperLowerOfA = List.of("A".charAt(0), "a".charAt(0));
+
+    private Map<String, String> createOppositeAlphabetMap() {
+        Map<String, String> map = new HashMap<>();
+
+        for (Character a :
+                UpperLowerOfA) {
+            int asciiOfStart = (int) a;
+            int countOfAlphabet = 26;
+
+            for (int i = 0; i < countOfAlphabet; i++) {
+                String key = String.valueOf((char) (asciiOfStart + i));
+                String value = String.valueOf((char) (asciiOfStart + (countOfAlphabet - i) - 1));
+                map.put(key, value);
+            }
+        }
+
+        return map;
     }
 }
