d34e0fde63d2d9bba8c9ae2e9aa4d6ede544a9f8
iamminho <minho100227@kakao.com>
2022-10-30 13:19:50 +0900

    Problem2 refactor(getCryptogram): stackList에 더하는 기능과 index를 설정하는 기능 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -22,19 +22,39 @@ public class Problem2 {
 
     static String getCryptogram(String cryptogram) {
         List<Character> stackList = new ArrayList<>();
+        int len = cryptogram.length();
 
         for (int i = 0; i < cryptogram.length(); i++) {
-            if (i == cryptogram.length() -1 || cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
-                stackList.add(cryptogram.charAt(i));
-            }
-
-            else if(cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
-                int endIdx = getEndIdx(cryptogram, i + 1);
-                i += endIdx - i;
-            }
+            addStackList(stackList, i, cryptogram, len);
+            i = getIdx(i, cryptogram, len);
         }
 
         return joinList(stackList);
+    }
+
+    static void addStackList(List<Character> stackList, int i, String cryptogram, int len) {
+        if (canAddStackList(i, cryptogram, len)) {
+            stackList.add(cryptogram.charAt(i));
+        }
+    }
+
+    static int getIdx(int i, String cryptogram, int len) {
+        if (!canAddStackList(i, cryptogram, len)) {
+            int endIdx = getEndIdx(cryptogram, i + 1);
+            int result = i + (endIdx - i);
+
+            return result;
+        }
+
+        return i;
+    }
+
+    static boolean canAddStackList(int i, String cryptogram, int len) {
+        if (i == len -1 || cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
+            return true;
+        }
+
+        return false;
     }
 
     static int getEndIdx(String cryptogram, int i) {
