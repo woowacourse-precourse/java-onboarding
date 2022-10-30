b8607989e8c9b9ba99b5dc36afe1497b3c49a827
alstjr706 <alstjr706@naver.com>
2022-10-31 04:09:49 +0900

    feat : (중복 닉네임 제거) 크루들을 객체로 저장 구현

--- src/main/java/onboarding/problem6/SaveCrewMember.java
+++ src/main/java/onboarding/problem6/SaveCrewMember.java
@@ -1,12 +1,20 @@
 package onboarding.problem6;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SaveCrewMember {
 
     public List<CrewMember> crewMeberSet(List<List<String>> forms) {
 
+        ArrayList<CrewMember> crewMeberList = new ArrayList<>();
 
-        return null;
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            crewMeberList.add(new CrewMember(email, nickname));
+        }
+        return crewMeberList;
     }
 }
