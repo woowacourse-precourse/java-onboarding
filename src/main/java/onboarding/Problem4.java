7c44157dbabb5e928d95894ae3f9af0d71bf283b
Jeeseob <jeeseob5761@gmail.com>
2022-10-30 17:57:35 +0900

    REFACTORING: problem4 - 알고리즘 코드 수정(함수 분리)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,37 +4,41 @@ package onboarding;
  * @Problem: 엄마 말씀 word가 매개변수로 주어질 때, 아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록 solution 메서드를 완성하라.
  */
 public class Problem4 {
+    public static String solution(String word) {
+        return changeAlphabet(word);
+    }
+
     /**
      * @Method: changeAlphabet
-     * 아스키 코드를 기반은로 각 문자를 수정한다.
-     * 1. 소문자의 경우 122 - (char - 97)
-     * 2. 대문자의 경우 90 - (char - 65)
-     * 3. 나머지 값에 대해서는 그대로
      */
-
-
-    public static String solution(String word) {
+    private static String changeAlphabet(String word) {
         StringBuilder answer = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
-            char temp = word.charAt(i);
-            char next;
+            char temp = word.charAt(i); // 현재 문자
 
-            // 1. 소문자의 경우 122 - (char - 97)
             if (97 <= temp && temp <= 122){
-                next = (char) (122 - (temp - 97));
+                temp = changeFrogLowerCase(temp);
+            } else if(65 <= temp && temp <= 90) {
+                temp = changeFrogUpperCase(temp);
             }
-
-            // 2. 대문자의 경우 90 - (char - 65)
-            else if(65 <= temp && temp <= 90) {
-                next = (char) (90 - (temp - 65));
-            }
-
-            // 3. 나머지 값에 대해서는 그대로
-            else {
-                next = temp;
-            }
-            answer.append(next);
+            answer.append(temp);
         }
         return answer.toString();
+    }
+
+    /**
+     * @Method: changeFrogLowerCase
+     * 소문자인 경우
+     */
+    private static char changeFrogLowerCase(char temp) {
+        return (char) (122 - (temp - 97));
+    }
+
+    /**
+     * @Method: changeFrogUpperCase
+     * 대문자인 경우
+     */
+    private static char changeFrogUpperCase(char temp) {
+        return (char) (65 - (temp - 90));
     }
 }
