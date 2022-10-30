5ce85c50e2ffdf85ca777525abad55e431464dda
Choi-JJunho <junho5336@gmail.com>
2022-10-30 18:51:59 +0900

    refactor(문제6): 기능함수를 Dictionary 객체 내부 함수로 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
  */
 public class Problem6 {
 
-    public static class Member {
+    private static class Member {
         private String name;
         private String email;
         private boolean duplicated = false;
@@ -43,48 +43,56 @@ public class Problem6 {
         }
     }
 
-    // comment : 변수 명은 List인데 자료형은 Set?
-    // comment : 같은 Collection을 상속받으니 List로 명명해도 무관한거 아닌가?
-    public static Set<Member> duplicateMemberList = new HashSet<>();
-    public static Map<String, Member> nameStorage = new HashMap<>();
+    private static class Dictionary {
+        private final Set<Member> duplicateMemberList = new HashSet<>();
+        private final Map<String, Member> nameStorage = new HashMap<>();
+
+        Dictionary() {
+        }
+
+        Dictionary(List<Member> members) {
+            members.forEach(member -> {
+                String name = member.getName();
+                for (int i = 0; i < (name.length() - 1); i++) {
+                    String slice = name.substring(i, i + 2);
+                    nameStorage.put(slice, member);
+                }
+            });
+        }
+
+        public void checkAndSaveDuplicateMember(Member member) {
+            String name = member.getName();
+
+            for (int i = 0; i < (name.length() - 1); i++) {
+                String slice = name.substring(i, i + 2);
+                if (nameStorage.containsKey(slice)) {
+                    if (!member.equals(nameStorage.get(slice))) {
+                        duplicateMemberList.add(member);
+                        duplicateMemberList.add(nameStorage.get(slice));
+                    }
+                }
+            }
+        }
+
+        public List<String> getDuplicationMemberAsResult() {
+            return duplicateMemberList.stream()
+                    .map(o -> o.email)
+                    .sorted()
+                    .collect(Collectors.toList());
+        }
+    }
 
     public static List<String> solution(List<List<String>> forms) {
         List<Member> members = forms.stream()
                 .map(form -> new Member(form.get(0), form.get(1)))
                 .collect(Collectors.toList());
 
+        Dictionary dictionary = new Dictionary(members);
+
         for (Member member : members) {
-            String name = member.getName();
-
-            for (int i = 0; i < name.length() - 1; i++) {
-                String slice = name.substring(i, i + 2);
-                checkDuplicateName(slice, member);
-            }
+            dictionary.checkAndSaveDuplicateMember(member);
         }
 
-        List<String> answer = duplicateMemberList.stream()
-                .map(o -> o.email)
-                .sorted()
-                .collect(Collectors.toList());
-
-        return answer;
-    }
-
-    /**
-     * 유저 이름이 nameStorage에 존재한다면 duplicateUserEmail에 추가한다.
-     * 이때 nameStorage에 존재하던 유저도 함께 추가한다.
-     *
-     * @param slice  : 잘라낸 이름
-     * @param member : 사용자
-     */
-    // comment : checkDuplicateName 함수명은 중복된 이름 체크만을 수행한다고 명시되어있지만 nameStorage에 저장하는 부수행위를 수행한다.
-    // comment : 하나의 함수는 하나의 기능만 가져아한다. 부수행위는 존재하면 안된다.
-    private static void checkDuplicateName(String slice, Member member) {
-        if (nameStorage.containsKey(slice) && !member.name.equals(nameStorage.get(slice).getName())) {
-            duplicateMemberList.add(member);
-            duplicateMemberList.add(nameStorage.get(slice));
-        } else {
-            nameStorage.put(slice, member);
-        }
+        return dictionary.getDuplicationMemberAsResult();
     }
 }
