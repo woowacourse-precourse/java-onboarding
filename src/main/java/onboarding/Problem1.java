48e187986e5409b7023aa17578987e44f192c550
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-10-31 03:23:25 +0900

    problem1 기능목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /** 기능 목록
+     * 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+     * 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+     * 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+     * 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+     * 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
