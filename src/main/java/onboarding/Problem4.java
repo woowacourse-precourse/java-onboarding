9a2ffdc31d2c26a7d117fcd63a98e2c01bab8f34
desfox <desfox99@naver.com>
2022-10-30 21:16:57 +0900

    feat(Problem4): 글자가 알파벳 대문자, 소문자인지 확인 후 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,6 +9,10 @@ public class Problem4 {
     private static char reverseConvertChar(char c) {
         if (Character.isAlphabetic(c) == false) {
             return c;
+        } else if (Character.isUpperCase(c)) {
+            return (char)(('A' + 'Z') - c);
+        } else if (Character.isLowerCase(c)) {
+            return (char)(('a' + 'z') - c);
         }
 
         return c;
