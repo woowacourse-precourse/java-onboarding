965cbcfa946ef5910088d95ed396bd6eeac28aed
himjjong <jjijji5728@tukorea.ac.kr>
2022-10-30 17:00:32 +0900

    feat : 주어진 money의 값을 계산해주는 calMoney 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,9 +7,15 @@ public class Problem5 {
     static int[] moneyEx = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> checklist = new ArrayList<Integer>();
-        return cal(money, checklist);
+        return calMoney(money, checklist);
     }
-    static List<Integer> cal(int money, List<Integer> list){
+    static List<Integer> calMoney(int money, List<Integer> list){
+        int calmoney = 0;
+        for(int i=0; i<moneyEx.length; i++){
+            calmoney = money/moneyEx[i];
+            list.add(calmoney);
+            money -= calmoney * moneyEx[i];
+        }
         return list;
     }
 }
