e46137558a5202116e38577c99a7ce9677c59df1
ywj9811 <ywj9811@naver.com>
2022-10-31 01:45:37 +0900

    사용자와 이미 친구인 리스트, 친구의 친구를 저장할 리스트, 친구가 아닌 방문자를 저장할 리스트를 생성한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,15 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    // user는 본인 friends리스트는 2명씩 짝으로, visitors는 같은 인물이 중복되어 나올 수 있다
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>(); //반환용 리스트
+        List<String> realFriends = new ArrayList<>(); //사용자와 이미 친구인 리스트
+        List<String> withFriends = new ArrayList<>(); //친구의 친구 리스트(사용자와 친구X) -> +10점
+        List<String> visitFriends = new ArrayList<>(); //방문자 리스트 -> +1점
+
         return answer;
     }
 }
