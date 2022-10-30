0caa43bb4d0ce1788bfd99f181387d685926788c
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-30 20:47:34 +0900

    Problem6 기능요구사항 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    // TODO 같은 글자가 연속적으로 포함되는 판단하는 메서드
+    public static List<String> checkDuplication(List<List<String>> list) {
+        return null;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
