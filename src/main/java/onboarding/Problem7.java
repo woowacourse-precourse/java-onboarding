ace2271dabb30b41970e03cd27f3b7bd8bd8d0f2
Egenieee <hhgg0925@naver.com>
2022-10-30 21:40:35 +0900

    [FEAT] Problem7 사용자와 함께 아는 친구 찾아 점수주는 기능 추가
    - Map의 getOrDefault 메서드로 함께 아는 친구가 나올 때 마다 10점씩 추가하도록 한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Set<String> alreadyFriends = getAlreadyFriends(friends);
+        Map<String, Integer> recommendPoint = getRecommendPoint(friends, user);
 
         return Arrays.asList("", "");
     }
@@ -18,4 +19,20 @@ public class Problem7 {
 
         return alreadyFriends;
     }
+
+
+    private static Map<String, Integer> getRecommendPoint(List<List<String>> friends, String user) {
+        Map<String, Integer> recommendPoint = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String newFriend = friend.get(1);
+
+            if (!user.equals(newFriend)) {
+                recommendPoint.put(newFriend, recommendPoint.getOrDefault(newFriend, +10));
+            }
+        }
+
+        return recommendPoint;
+    }
+
 }
