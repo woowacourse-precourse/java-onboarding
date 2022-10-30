2a855d6a37840f2dcee12a3f4b364692432b0f1f
chobobdev <chobobdev@naver.com>
2022-10-31 01:14:38 +0900

    feat: repeat the following feat until duplicated is gone

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,24 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
+    public static Boolean haveDupe(String cryptogram){
         char[] chars = cryptogram.toCharArray();
-        for(int i =0;i<cryptogram.length()-1;i++){
+        for(int i =0;i<cryptogram.length()-2;i++){
             if(chars[i]==chars[i+1]){
-                cryptogram=cryptogram.substring(0,i)+cryptogram.substring(i+2);
-                System.out.println(cryptogram);
-                break;
+                return true;
+            }
+        }
+        return false;
+    }
+    public static String solution(String cryptogram) {
+        while(haveDupe(cryptogram)){
+            char[] chars = cryptogram.toCharArray();
+            for(int i =0;i<cryptogram.length()-2;i++){
+                if(chars[i]==chars[i+1]){
+                    cryptogram=cryptogram.substring(0,i)+cryptogram.substring(i+2);
+                    System.out.println(cryptogram);
+                    break;
+                }
             }
         }
         String answer = "answer";
