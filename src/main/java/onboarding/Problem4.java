38cda3c9b24690db8de81f287ce6ae7dcd455ec7
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 17:52:44 +0900

    feat(PR4): 알파벳인지 판별 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,9 +6,16 @@ import java.util.List;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        List<Character> alphabet = List.of('A','B','C','D','E','F','G',
+        List<Character> alpDict = List.of('A','B','C','D','E','F','G',
                                         'H','I','J','K','L','M','N','O','P','Q',
                                         'R','S','T','U','V','W','X','Y','Z');
+
+        for(int idx = 0; idx < word.length(); idx++){
+            char alphabet = word.charAt(idx);
+            if(Character.isAlphabetic(alphabet)){ //알파벳이면 변환
+                
+            }
+        }
 
         return answer;
     }
