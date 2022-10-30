5ab74979f58eace48fcf4345c7596ab91d702c6b
ch200203 <ch200205@naver.com>
2022-10-30 19:49:06 +0900

    Problem3 1. 숫자를 Number까지 센다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        // 손뼉을 친 횟수
+        int count = 0;
+
+        // 1. 숫자를 number 까지 센다
+        for (int i = 1; i <= number ; i++) {
+
+        }
+
         return answer;
     }
 }
