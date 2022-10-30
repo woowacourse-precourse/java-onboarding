96a1c0ecf7866de61f894f3395f9a90aa65b8940
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-10-30 23:43:14 +0900

    FEAT : 페이지 번호의 덧셈 함수(sum) 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,21 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static int sum(Integer num) {
+        int ans = 0;
+        while(num > 0) {
+            ans += (num%10);
+            num /= 10;
+        }
+        return ans;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        try {
+            int answer = Integer.MAX_VALUE;
+            return answer;
+        } catch (Exception e) {
+            return -1;
+        }
     }
 }
