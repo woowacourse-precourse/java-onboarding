22f4914444fcd4febeef87668161c049702b2840
slbin-park <smpts00@naver.com>
2022-10-31 00:24:22 +0900

    feat : 친구 리스트를 구하는 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String,Integer> friendsMap= new HashMap<>();
         return answer;
+    }
+
+    public static HashMap<String, Set<String>> createFriendsList(List<List<String>> friends){
+        HashMap<String, Set<String>> map = new HashMap<>();
+        for(List<String> friend : friends){
+            Set<String> set = map.getOrDefault(friend.get(0),new TreeSet<>());
+            set.add(friend.get(1));
+            map.put(friend.get(0),set);
+            set = map.getOrDefault(friend.get(1),new TreeSet<>());
+            set.add(friend.get(0));
+            map.put(friend.get(1),set);
+        }
+        return map;
     }
 }
