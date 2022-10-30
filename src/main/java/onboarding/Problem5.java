4e538f74158f9f8a1778405fdd445f773aa29eaa
grand7070 <grand7070@naver.com>
2022-10-30 23:15:32 +0900

    feat : Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < moneyList.length; i++) {
+            answer.add(money / moneyList[i]);
+            money -= (money / moneyList[i]) * moneyList[i];
+        }
         return answer;
     }
 }
