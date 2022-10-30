d4df359b155fe3aba14d388b10236c230608ff94
dongdong-119 <85717356+dongdong-119@users.noreply.github.com>
2022-10-30 21:12:31 +0900

    Problem 4 - 기능목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,16 @@
 package onboarding;
 
+/**
+ * <기능 목록>
+ *     1. 메소드
+ *          1) convertWord
+ *          : 엄마 말씀 word(String)을 인자로 받아 반대로 변환하여 return한다. 단, 다음과 같은 조건을 만족시키며 변환한다.
+ *
+ *              - 변환 순서는 A-Z를 Z-A로 뒤집은 것과 동일한 순서로 한다.
+ *              - 알파벳 대문자는 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
+ *              - 알파벳 이외의 문자는 변환하지 않는다.
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
