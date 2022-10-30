c344a3c221180ae10d4a3d6d4670f23c6ff768d9
desfox <desfox99@naver.com>
2022-10-30 21:21:11 +0900

    feat(Problem4): 변환된 문자열 반환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int j = 0; j <= word.length() - 1; j++) {
+            char c = word.charAt(j);
+            answer += reverseConvertChar(c);
+        }
+
         return answer;
     }
 
