d1b8777b78f017672c8228b525e61ac9201fc4c9
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-31 06:54:18 +0900

    [feat] Problem7 모든 사용자의 친구 리스트 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,25 +1,31 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        List<String> friendList = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
+        Map<String,List<String>> friendsList = new HashMap<>();
         for (List<String> friend : friends) {
-            if(friend.contains(user))
-            {
-                if(friend.get(0).equals(user)) friendList.add(friend.get(1));
-                else friendList.add(friend.get(0));
-            }
+
+            List<String> tmp = friendsList.getOrDefault(friend.get(0), new ArrayList<>());
+            tmp.add(friend.get(1));
+            friendsList.put(friend.get(0),tmp);
+            tmp = friendsList.getOrDefault(friend.get(1),new ArrayList<>());
+            tmp.add(friend.get(0));
+            friendsList.put(friend.get(1),tmp);
         }
 
 
 
         return answer;
     }
+
 }
