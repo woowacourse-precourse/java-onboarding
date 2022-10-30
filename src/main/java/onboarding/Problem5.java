bae2d98967e01350a4f72233043df54582908df9
JUHA <khabh@naver.com>
2022-10-30 16:38:41 +0900

    Add(Problem5/Bank) : Money enum에 화폐 금액 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,5 +9,22 @@ public class Problem5 {
         return answer;
     }
 
-	private static class Bank { }
+	private static class Bank {
+		private enum Money {
+			FIFTY_THOUSAND(50000),
+			TEN_THOUSAND(10000),
+			FIVE_THOUSAND(5000),
+			ONE_THOUSAND(1000),
+			FIVE_HUNDRED(500),
+			ONE_HUNDRED(100),
+			FIFTY(50),
+			TEN(10),
+			ONE(1);
+
+			private final int price;
+			Money(int price) {
+				this.price = price;
+			}
+		}
+	}
 }
