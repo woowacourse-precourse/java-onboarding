6d123adb3a444517519f18f8535062b171704fe5
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:14:54 +0900

    doc : 문제4 기능 구현 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ * 1. 각 char값이 문자인지 판단
+ * 2. 문자가 소문자인지 대문자인지 판별후 규칙에 따른 문자변환
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
