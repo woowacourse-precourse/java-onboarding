af3f607a0f66dbe8a89bbbc2d421628976002447
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-31 00:04:36 +0900

    feat: Create 예외사항 체크 | [problem 1]
    - 왼쪽 페이지 번호 + 1 == 오른쪽 페이지 번호

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (isSuccessiveInt(pobi) && isSuccessiveInt(crong)) {
+
+        }
+        return -1;
+    }
+
+    private static boolean isSuccessiveInt(List<Integer> pageIntegerList) {
+        return pageIntegerList.get(1) == pageIntegerList.get(0) + 1;
     }
 }
