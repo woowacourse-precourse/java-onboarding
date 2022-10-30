0ec0e5dc272c88062705428c907686d2630ce876
doongu <must1080@naver.com>
2022-10-31 01:48:29 +0900

    feat(Problem4): 테이블에 맞게 단어를 바꾸며 공백은 그대로 반환하는  기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,28 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char wordChar = ' ';
+        char resultWord = ' ';
+        for(int index = 0; index < word.length(); index++){
+            wordChar = word.charAt(index);
+            resultWord = changeWord(wordChar);
+            answer += resultWord;
+        }
+
         return answer;
+    }
+
+    public static char changeWord(char spelling){
+        char returnSpelling = ' ';
+
+        if (spelling >= 'a' && spelling <= 'z') {
+            returnSpelling = (char)((int)'z' - (int)spelling + (int)'a');
+        }else if (spelling >= 'A' && spelling <= 'Z'){
+            returnSpelling = (char)((int)'Z' - (int)spelling + (int)'A');
+        }else if (spelling == ' '){
+            returnSpelling = ' ';
+        }
+
+        return returnSpelling;
     }
 }
