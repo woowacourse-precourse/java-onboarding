f74ea9f5569ed18c3967bc4c2b79814fb0931434
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-10-31 02:51:57 +0900

    problem5 test completed

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    static int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int count = 0;
+        for(int i=0; i<moneyList.length; i++){
+            count = money / moneyList[i];
+            money = money % moneyList[i];
+            answer.add(count);
+        }
         return answer;
     }
 }
