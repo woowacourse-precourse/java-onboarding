fdbce4e2a32335eecfc1ce4ba5a1d3a3ffe0553f
EUNSH1 <tkdgus968@naver.com>
2022-10-31 02:03:48 +0900

    feat : [Problem6] 기능 사항 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 사항
+ * 1.닉네임을 두글자씩 배열에 저장하는 함수
+ * 2.배열에서 중복된 문자만 저장해 리턴하는 함수
+ * 3.중복문자를 가지고 있는 크루를 배열에 저장하는 함수
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
