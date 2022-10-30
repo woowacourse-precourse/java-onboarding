b5727ad4a71c75e5c6728edec3c725e67aaa2642
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-10-30 19:13:27 +0900

    P7. 친구 목록 생성
    user의 친구 리스트 생성
    나중에 해당 리스트를 참고해서 추천친구를 확인할 예정이기에 본인인 user도 0번 인덱스에 추가해줬다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        List<String> friend_list = createFrined_List(user, friends);
+        
         return answer;
+    }
+
+    public static List<String> createFrined_List(String user, List<List<String>> friends) {
+        List<String> friend_list = new ArrayList<>();
+
+        friend_list.add(user);
+        for(List<String> f : friends) {
+            if(user.equals(f.get(0))) friend_list.add(f.get(1));
+            if(user.equals(f.get(1))) friend_list.add(f.get(0));
+        }
+
+        return friend_list;
     }
 }
