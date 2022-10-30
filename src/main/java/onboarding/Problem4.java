69bebb96db33e7c904a38b998d324c21fe5d9ec8
jmjmjames <chungjm0711@gmail.com>
2022-10-30 21:48:31 +0900

    feat: add reverseCharacter method and solve Problem 4 - Using StringBuilder - O(n)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);  // 현재 문자
+            answer.append(reverseCharacter(ch));  // 청개구리 변환
+        }
+
+        return answer.toString();
     }
+
+    private static char reverseCharacter(char character) {
+        // no reverse (문자가 아닐 때)
+        if (!Character.isAlphabetic(character)) {
+            return character;
+        }
+
+        if (Character.isUpperCase(character)) {
+            return (char) ('A' + 'Z' - character);
+        } else {
+            return (char) ('a' + 'z' - character);
+        }
+    }
+
 }
