005e65cc3be41069274e996fc65cbb3b56ad252f
shpark30 <seohyeonpark30.gmail.com>
2022-10-30 17:41:14 +0900

    comment(Problem7.java): 친구 목록 생성 로직 구체화

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -28,9 +28,13 @@ class Friend {
 
 public class Problem7 {
     // List<Friend<name, score>> friendsScores 생성
-    List<Friend> list = new ArrayList<>();
+    List<Friend> scores = new ArrayList<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         // updateFriends : 친구 목록 생성
+            // 1. friends를 1중 배열로 Flatten
+            // 2. 배열 중복 제거
+            // 3. 배열에서 user 제거
+            // 4. 1중 배열 friends로 scores 리스트 업데이트
         // scoreCoFriend : friends를 순회하며 userFriend와 함께 아는 친구에 대해 score 10점 추가
         // scoreVisitor : visitors를 순회하며 방문자의 score 1점씩 추가
         // scores를 점수 내림차순, 이름 오름차순으로 정렬
