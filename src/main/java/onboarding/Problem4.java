5bd84e32a67687d9e0fb39f799735dee588b3465
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-30 22:32:36 +0900

    [ADD] 문제 4 - 1차 조건문: 문자(char)가 소문자인지 대문자인지 판단

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     private static final int CAPITAL_A = 'A';
     private static final int CAPITAL_Z = 'Z';
     private static final int SMALL_A = 'a';
     private static final int SMALL_Z = 'z';
+
+    private static List<Integer> getUnicodeThreshold(Integer unicode) {
+        List<Integer> thresholds = new ArrayList<>();
+
+        if (unicode >= CAPITAL_A && unicode <= CAPITAL_Z) {
+            thresholds.add(CAPITAL_A);
+            thresholds.add(CAPITAL_Z);
+        } else if (unicode >= SMALL_A && unicode <= SMALL_Z) {
+            thresholds.add(SMALL_A);
+            thresholds.add(SMALL_Z);
+        }
+
+        return thresholds;
+    }
     public static String solution(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            int unicode = letter;
+
+            List<Integer> threshold = getUnicodeThreshold(unicode);
+        }
         String answer = "";
         return answer;
     }
