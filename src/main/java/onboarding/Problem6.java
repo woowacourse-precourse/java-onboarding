a66f82eaa3e56eef92d98bb941e1d06d3627a709
ymkim97 <rladudaud97@naver.com>
2022-10-30 18:57:28 +0900

    feat: add function email length exception

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,11 +10,20 @@ public class Problem6 {
 
         throwCrewNumberException(forms);
 
+
         return answer;
     }
 
     public static boolean throwCrewNumberException(List<List<String>> forms) {
 
-        return forms.isEmpty() || forms.size() > 10_000;
+        return (forms.isEmpty() || forms.size() > 10_000);
     }
+
+    public static boolean throwEmailLengthException(List<String> forms) {
+
+        return (forms.get(0).length() < 11 || forms.get(0).length() > 20);
+
+    }
+
+
 }
