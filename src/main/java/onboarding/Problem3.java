1b6a33c945feb36dab1e6ec292c6d6b9b158cdb4
meunrud <y3a0522@gmail.com>
2022-10-31 02:09:39 +0900

    3. 배열 값이 3,6,9에 해당하면 +1 카운트 (chekcClap 메서드로 구현) 4. 출력

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,13 +1,12 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Problem3 {
     public static int solution(int number) {
 
         int answer = 0;
         String strNum = splitNum(number);
+        answer = checkClap(strNum);
 
         return answer;
     }
@@ -18,6 +17,15 @@ public class Problem3 {
         }
 
         return num.toString();
+    }
+    public static int checkClap(String strNum){
+        int count = 0;
+        for(int i = 0; i< strNum.length(); i++) {
+            if (strNum.charAt(i) == '3' || strNum.charAt(i) == '6' || strNum.charAt(i) == '9') {
+                count++;
+            }
+        }return count;
+
     }
 
 
