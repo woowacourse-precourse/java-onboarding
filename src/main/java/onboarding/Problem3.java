710c680e9e592d1b2e6fc842336c9b1867a1d634
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-30 19:39:27 +0900

    feat: problem3 숫자가 3,6,9를 포함하고 있는지 판별

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -34,4 +34,15 @@ public class Problem3 {
         return clap;
     }
 
+    /**
+     * 숫자가 3,6,9를 포함하고 있는지 판별
+     * @param number
+     * @return 포함하면 true / 포함하지 않으면 false
+     */
+    private static boolean isContain(char number) {
+        if (number == '3' || number == '6' || number == '9') {
+            return true;
+        }
+        return false;
+    }
 }
