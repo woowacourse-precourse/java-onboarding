4b3d7c8de5b16e9f93200db55d5dfffcb6435ae9
chobobdev <chobobdev@naver.com>
2022-10-31 01:34:01 +0900

    feat:[ Problem 1] create digit list from number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,9 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    public static List<Integer> numList (int number){
+        List<Integer> numberList = new ArrayList<>();
+        while(number!=0) {
+            numberList.add(number%10);
+            number/=10;
+        }
+        return numberList;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        List<Integer> numberList=numList(234);
+        System.out.println(numberList);
         int answer = Integer.MAX_VALUE;
         return answer;
     }
