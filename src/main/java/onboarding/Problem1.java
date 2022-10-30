b1f7da75c3869ad9391be7181229f9c7e7e1a247
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:02:07 +0900

    [문제1] getMaxNum 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,9 +15,40 @@ class Problem1 {
         if(pobi.get(0)<1 || pobi.get(1)>400 || (pobi.get(0)+1 != pobi.get(1))){
             return -1;
         }
+        pobiMaxNum = getMaxNum(pobi.get(0),pobi.get(1));
+        crongMaxNum = getMaxNum(crong.get(0),crong.get(1));
 
-        
 
         return answer;
+    }
+
+    public static int getMaxNum(int leftNum,int rightNum){
+        int rightDigit1 = rightNum/100;
+        int rightDigit2 = (rightNum%100)/10;
+        int rightDigit3 = (rightNum%10);
+
+        int leftDigit1 = leftNum/100;
+        int leftDigit2 = (leftNum%100)/10;
+        int leftDigit3 = (leftNum%10);
+
+        int rightSum = rightDigit1+rightDigit2+rightDigit3;
+        int leftSum = leftDigit1+leftDigit2+leftDigit3;
+
+        if(rightDigit1 == 0)
+            rightDigit1 = 1;
+        if(rightDigit2 == 0)
+            rightDigit2 = 1;
+        if(leftDigit1 == 0)
+            leftDigit1 = 1;
+        if(leftDigit2 == 0)
+            leftDigit2 = 1;
+
+        int rightMul = rightDigit1*rightDigit2*rightDigit3;
+        int leftMul = leftDigit1*leftDigit2*leftDigit3;
+
+        int mulResult = Math.max(rightMul,leftMul);
+        int sumResult = Math.max(rightSum,leftSum);
+
+        return Math.max(mulResult,sumResult);
     }
 }
