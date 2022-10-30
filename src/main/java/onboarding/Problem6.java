7b8cedaf17afe495e18901905ffed00ef29d572b
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 21:50:09 +0900

    feat:[Problem6] 중복된 이름 찾기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,16 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+    /*
+    중복된 이름 찾기
+     */
+    private static boolean checkDuplicate(String str1,String str2){
+        for(int i=0;i<str1.length()-1;i++){
+            String temp=str1.substring(i,i+2);
+            if(str2.contains(temp)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
