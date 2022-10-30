631d87256e9b0f4d5cb0623c0eb653450bfbfaa1
byeongsoon <byeongsoon94@gmail.com>
2022-10-30 19:47:55 +0900

    FEAT: Problem6, 중복된 닉네임 사용자 이메일 목록 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> treeSet = new TreeSet<>();
+        Map<String, String> hashMap = new HashMap<>();
+
+        for (List<String> form: forms) {
+            addEmailByDuplicateName(form, treeSet, hashMap);
+        }
+
+        return new ArrayList<>(treeSet);
+    }
+
+
+    private static void addEmailByDuplicateName(List<String> form, Set<String> treeSet, Map<String, String> hashMap) {
+        String email = form.get(0);
+        String name = form.get(1);
+
+        for (int i = 1; i < name.length(); i++) {
+            String subName = name.substring(i-1, i+1);
+
+            if (!hashMap.containsKey(subName)) {
+                hashMap.put(subName, email);
+                continue;
+            }
+
+            treeSet.add(email);
+            treeSet.add(hashMap.get(subName));
+        }
     }
 }
