d2548219f8989c50a2b4fdcfd33b379328d005da
drunkenhw <drunkenhw@gmail.com>
2022-10-30 17:34:09 +0900

    feat(Problem6) : 닉네임이 중복되는 크루 리스트의 이메일을 반환한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,25 +1,25 @@
 package onboarding;
 
+
 import java.util.*;
 import java.util.stream.Collectors;
-
 
 public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-
         CrewService crewService = new CrewService();
 
         List<Crew> crewList = forms.stream()
                 .map(Crew::new)
                 .collect(Collectors.toList());
-
         crewService.saveAll(crewList);
 
         List<Crew> duplicateCrewList = crewService.findDuplicateCrewList();
 
-        return answer;
+        return duplicateCrewList.stream()
+                .map(Crew::getEmail)
+                .sorted()
+                .collect(Collectors.toList());
     }
 
 
