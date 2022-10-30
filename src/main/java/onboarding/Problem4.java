e78d2cdd67ba4edfebb8b8014373848e8d6293ee
Yejin Shin <shinyejin0212@gmail.com>
2022-10-30 21:44:38 +0900

    docs(problem4) : 주석 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,18 +1,19 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 
 public class Problem4 {
+    
+    // 대문자, 소문자에 따라 나누어 청개구리 사전으로 변환해주는 메소드
     public static Character conversion(Character c){
         int temp = 0;
+        // 대문자일 때 변환
         if (isUpperCase(c)){
             temp = (int)'Z' - ((int)c -(int)'A');
-
-        } else if (isLowerCase(c)) {
+        } 
+        // 소문자일 때 변환
+        else if (isLowerCase(c)) {
             temp = (int)'z' - ((int)c -(int)'a');
         }
         return (char)temp;
@@ -21,11 +22,13 @@ public class Problem4 {
         String answer = "";
 
         for (int i =0; i < word.length(); i++) {
-            char c = word.charAt(i);
+            char c = word.charAt(i); // 단위 문자로 쪼갬
+            // 만약 문자 c가 알파벳이 아니라면(ex space) 그대로 추가
             if(!Character.isAlphabetic(c)){
                 answer += c;
                 continue;
             }
+            // 알파벳이라면 문자별 conversion 실행 후 추가
             answer += conversion(c);
 
         }
