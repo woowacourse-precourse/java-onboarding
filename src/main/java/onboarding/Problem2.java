7a0673bd7ca6c70b62f3d32f710edecff51f23c3
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:06:48 +0900

    [문제2] 제한사항 체크 및 찾고자 하는 패턴,매처 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000)
+            return "invalid cryptogram";
+
+        Pattern p = Pattern.compile("^.*(.)\\1.*$");
+        Matcher m = p.matcher(cryptogram);
+
         return answer;
     }
 }
