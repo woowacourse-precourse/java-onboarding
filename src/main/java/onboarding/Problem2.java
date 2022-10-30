2a20ffcd6d3e5a70b0dd2cc07cca5b065ef8ecd6
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-10-31 00:03:10 +0900

    feat : 두개의 Queue를 활용하여 연속되는 수 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,19 +2,19 @@ package onboarding;
 
 import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.stream.Collectors;
 
-
 public class Problem2 {
 	public static String solution(String cryptogram) {
-
 		Deque<Character> decryptionQueue = getCharacterQueueByString(cryptogram);
+		int beforeSize;
 
-		while (true) {
-			decrypt(decryptionQueue);
-		}
-
+		do {
+			beforeSize = decryptionQueue.size();
+			decryptionQueue = removeSerialChar(decryptionQueue);
+		} while (beforeSize != decryptionQueue.size());
 
 		return covertCharacterCollectionToString(decryptionQueue);
 	}
@@ -25,8 +25,34 @@ public class Problem2 {
 			queue.offer(character);
 		}
 
-
 		return queue;
+	}
+
+	private static Deque<Character> removeSerialChar(Deque<Character> decryptionQueue) {
+		Deque<Character> tmpQueue = new ArrayDeque<>();
+
+		while (!decryptionQueue.isEmpty()) {
+			Character c = decryptionQueue.poll();
+			if (tmpQueue.isEmpty()) {
+				tmpQueue.offerLast(c);
+				continue;
+			}
+
+			if (tmpQueue.peekLast() == c) {
+				while (tmpQueue.peekLast() == decryptionQueue.peekFirst()) {
+					decryptionQueue.poll();
+				}
+				tmpQueue.pollLast();
+				if (!decryptionQueue.isEmpty()) {
+					tmpQueue.offerLast(decryptionQueue.poll());
+				}
+				continue;
+			}
+
+			tmpQueue.offerLast(c);
+		}
+
+		return tmpQueue;
 	}
 
 	private static String covertCharacterCollectionToString(Collection<Character> collection) {
