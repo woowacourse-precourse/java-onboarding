be10cd1621bf41cfad119c0f08ece438862f3d49
dsaf2007 <dsaf2007@naver.com>
2022-10-30 22:55:38 +0900

    Problem5 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String upper = "ABCDEFGHIJKLMNOPQURTUVWXYZ";
+        String lowerFrog = "zyxwvutsrqponmlkjihgfedcba";
+        String upperFrog = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
+        for(int i = 0; i < word.length();i++)
+        {
+            for(int j = 0; j < lower.length(); j++)
+            {
+                if(word.substring(i,i+1).equals(lower.substring(j,j+1)))
+                {
+                    answer += lowerFrog.substring(j,j+1);
+                }
+                else if(word.substring(i,i+1).equals(upper.substring(j,j+1)))
+                {
+                    answer += upperFrog.substring(j,j+1);
+                }
+                else if(word.substring(i,i+1).equals(" "))
+                {
+                    answer += " ";
+                    break;
+                }
+            }
+        }
         return answer;
     }
 }
