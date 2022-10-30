3a7b78acdd03cd6589985070fa32e27ae6a08d6c
SonMinGyu <littleking13@naver.com>
2022-10-30 17:43:52 +0900

    [FEAT] Problem1 plus 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,28 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
+
+
         return answer;
     }
+
+    public static int plus(int page) {
+
+        int plusResult = 0;
+
+        String pageString = Integer.toString(page);
+        String[] stringArray = pageString.split("");
+
+        for(int i = 0; i < stringArray.length; i++) {
+            plusResult += Integer.parseInt(stringArray[i]);
+        }
+
+        return plusResult;
+    }
+
 }
