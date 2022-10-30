eaabd04c58f0db64d5b5e1130b6926028576a0cf
Laeng <Laeng@users.noreply.github.com>
2022-10-30 23:59:53 +0900

    feat(problem6): 크루 저장 및 검색 메서드 재작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,9 +7,12 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Problem6 {
+    private static ArrayList<List<String>> crews = new ArrayList<>();
     private static HashMap<String, Integer> twoLetterNicknamesAndIds = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
+
+
         return forms.get(0);
     }
 
@@ -34,7 +37,7 @@ public class Problem6 {
     /**
      * 분할된 크루 닉네임을 저장
      * @param twoLetterNickname 분할된 크루 닉네임의 일부
-     * @param id 크루의 고유 번호
+     * @param id 크루 고유 번호
      */
     private static void putTwoLetterNicknameAndId(String twoLetterNickname, int id) {
         if (!twoLetterNicknamesAndIds.containsKey(twoLetterNickname)) {
@@ -45,9 +48,27 @@ public class Problem6 {
     /**
      * 분할된 크루 닉네임을 통해 크루 고유 번호 검색
      * @param twoLetterNickname 분할된 크루 닉네임의 일부
-     * @return 크루의 고유 번호
+     * @return 크루 고유 번호
      */
     private static int getIdByTwoLetterNickname(String twoLetterNickname) {
         return twoLetterNicknamesAndIds.getOrDefault(twoLetterNickname, -1);
+    }
+
+
+    /**
+     * 크루 인덱싱
+     * @param forms 크루 신청 데이터
+     */
+    private static void AddAllCrews(List<List<String>> forms) {
+        crews.addAll(forms);
+    }
+
+    /**
+     * 크루 데이터 검색
+     * @param id 크루 고유 번호
+     * @return 크루 데이터
+     */
+    private static List<String> getCrewById(int id) {
+        return crews.get(id);
     }
 }
