d9f53b9ceb576724c6915eac772a227ce49b8db9
charlesuu <tiger0192004@gmail.com>
2022-10-30 23:48:00 +0900

    <문제7> - (기능 1).  user와 친구인 사람들의 목록 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        ArrayList<String> userFreinds = new ArrayList<>();
+        for (List<String> pair : friends) {
+            if (isUserFreind(user, pair)) {
+                userFreinds.add(getUserFreind(user, pair));
+            }
+        }
+
+
+
         return answer;
+    }
+    public static boolean isUserFreind(String user, List<String> pair) {
+        for (String id : pair) {
+            if (id == user) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static String getUserFreind(String user, List<String> pair) {
+        return pair.get(0) == user ? pair.get(1) : pair.get(0);
     }
 }
