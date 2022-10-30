18b6a2e76e1b5362915ce1a4bd584515944872f5
ddubidubap <park62000@gmail.com>
2022-10-30 14:08:45 +0900

    PROBLEM7 사용자의 친구 리스트 반환 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     /*
@@ -12,6 +11,21 @@ public class Problem7 {
         4. 해시맵 정렬 함수
         5. solution (함수 순서대로 실행)
     */
+
+    // 주어진 사용자의 친구 리스트 반환 함수
+    private static List<String> userFriend(String user, List<List<String>> friendDb, HashSet<String> users) {
+        List<String> friends = new ArrayList<>(List.of());
+
+        for (List<String> relationship : friendDb) {
+            if (relationship.contains(user)) {
+                int userIndex = relationship.indexOf(user); // 사용자의 인덱스
+                int friendIndex = 1 - userIndex;            // 사용자 친구의 인덱스
+                String userFriend = relationship.get(friendIndex);
+                friends.add(userFriend);
+            }
+        }
+        return friends;
+    }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
