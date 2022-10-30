8873b98ecf8f554db1e45c4b7de44c50c0dad723
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-30 20:12:30 +0900

    221030 Feat: P4 - 기능2 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,9 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-
+        StringBuilder sb = new StringBuilder();
         int capitalConstant = 'A' + 'Z';
         int smallConstant = 'a' + 'z';
+
         //1.입력 문자열을 맨 앞부터 순회하며 대소문자 여부를 판별한다.
         for(int i = 0; i < word.length(); i++) {
             char curr = word.charAt(i);
@@ -17,6 +18,7 @@ public class Problem4 {
                 curr = (char)(smallConstant - curr);
             }
             //2.변환한 암호를 결과 String에 이어붙인다.
+            sb.append(curr);
         }
 
         //3.입력 문자열을 끝까지 순회했다면 결과 String을 반환한다.
