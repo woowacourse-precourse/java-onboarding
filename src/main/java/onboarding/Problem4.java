c91d1674a65c857c88497d394acbaf46bb5da320
ONE <sodlapdlf13@gmail.com>
2022-10-30 17:27:30 +0900

    docs: [Problem4] 필요 기능에 대한 주석을 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/*
+* 1. convert(char letter): 문자를 받아 알파벳의 대소문자를 구분하여 반대로 변환
+*   1-1. 알파벳이 아닌 문자는 그대로 반환
+* 2. isUppercaseAlphabet(char letter): 문자가 대문자 알파벳인지의 여부를 확인하는 메서드 추가
+* 3. isLowercaseAlphabet(char letter): 문자가 소문자 알파벳인지의 여부를 확인하는 메서드 추가
+*/
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
