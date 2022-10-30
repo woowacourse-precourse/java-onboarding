a1800a0cba0700e6fc8d2d8bbef648d8d8a60729
DongwookKim0823 <dwkim0823@naver.com>
2022-10-30 19:48:39 +0900

    feat: (Problem7) find friend of friend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,21 +1,47 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-        findUserFriends(user, friends);
+        //Map<String, Integer> score = findFriendOfFriend(user, friends);
+
+
         return answer;
+    }
+
+
+    private static Map<String, Integer> findFriendOfFriend(String user, List<List<String>> friends) {
+
+        Map<String, Integer> score = new HashMap<>();
+        List<String> userFriends = findUserFriends(user, friends);
+
+        for (List i : friends) {
+            if (!i.contains(user)) {
+                if (userFriends.contains(i.get(0)) && !userFriends.contains(i.get(1))) {
+                    if (!score.containsKey(i.get(1))) {
+                        score.put((String) i.get(1), 0);
+                    }
+                    score.put((String) i.get(1), score.get(i.get(1)) + 10);
+                }
+                if (userFriends.contains(i.get(1)) && !userFriends.contains(i.get(0))) {
+                    if (!score.containsKey(i.get(0))) {
+                        score.put((String) i.get(0), 0);
+                    }
+                    score.put((String) i.get(0), score.get(i.get(0)) + 10);
+                }
+            }
+
+        }
+        return score;
     }
 
     private static List<String> findUserFriends(String user, List<List<String>> friends) {
 
         List<String> userFriends = new ArrayList<>();
-        for (List i : friends) { //user의 친구 추가
+        for (List i : friends) {
             if (i.get(0).equals(user)) userFriends.add((String) i.get(1));
             if (i.get(1).equals(user)) userFriends.add((String) i.get(0));
         }
