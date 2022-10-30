fd55dc0b35ae99fc3b0ff915efbe6ad4e7342d60
JeongHunHui <deca3248@gmail.com>
2022-10-30 21:51:02 +0900

    [1주차 - Problem7] 친구가 겹치는 유저 점수 업데이트 및 점수 업데이트 메소드 추출

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -15,15 +15,32 @@ public class Problem7 {
             else if(friendRelations.get(1) == user) userFriends.add(friendRelations.get(0));
         }
 
+        // 이미 친구가 아닌 사용자 중에 친구가 겹치면 10점
+        for (List<String> friendRelations : friends) {
+            String userA = userFriends.get(0);
+            String userB = userFriends.get(1);
+            if(userA == user || userB == user) continue;
+            if(userFriends.contains(userA) && !userFriends.contains(userB)) {
+                putScoreDict(userB, 10);
+            }
+            else if(userFriends.contains(userB) && !userFriends.contains(userA)) {
+                putScoreDict(userA, 10);
+            }
+        }
+
         // 방문한 사람의 점수를 딕셔너리에 저장 및 업데이트
         for (String visitor : visitors) {
             // 이미 친구라면 스킵
             if(userFriends.contains(visitor)) continue;
-            int score = 1;
-            if(friendScoreDict.containsKey(visitor))
-                score += friendScoreDict.get(visitor);
-            friendScoreDict.put(visitor, score);
+            putScoreDict(visitor, 1);
         }
         return answer;
+    }
+
+    // 점수 딕셔너리에 이미 있는 유저면 score업데이트, 없으면 새로 추가
+    static void putScoreDict(String name, int score) {
+        if(friendScoreDict.containsKey(name))
+            score += friendScoreDict.get(name);
+        friendScoreDict.put(name, score);
     }
 }
