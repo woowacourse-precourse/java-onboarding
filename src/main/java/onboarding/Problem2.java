b017a07fc6cf8a1b568a008ac67d54f2fe9df192
sohyundoh <lyny123@naver.com>
2022-10-30 16:51:06 +0900

    Problem 2 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,11 +2,14 @@ package onboarding;
 
 /*제한 사항
     cryptogram은 길이가 1 이상 1000 이하인 문자열이다.
-    cryptogram은 알파벳 소문자로만 이루어져 있다. */
+    cryptogram은 알파벳 소문자로만 이루어져 있다.
+기능 목록
+ 1. Stack에 문자열의 문자를 저장한다. (만일 전에 삽입된 문자가 현재 문자가 같을 경우는 pop)
+ 2. answer로 리턴할 값인 Stringbuffer에 각 문자를 붙여주고, reverse 함수를 통해 정답에 저장한다.
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-
         return answer;
     }
 }
