168b4b6866733095803188d8d2d7133109d80ea7
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-30 18:07:45 +0900

    화폐 단위 배열인 moneyList 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/*
+기능목록
+1. 화폐 단위 배열 moenyList 생성
+2. 단위별 화폐 개수 count
+    2-1. 금액이 큰 화폐부터 나눈 몫을 answer에 저장
+    2-2. 금액이 큰 화폐부터 나눈 나머지를 money에 다시 저장
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] moneyList=new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
         return answer;
     }
 }
