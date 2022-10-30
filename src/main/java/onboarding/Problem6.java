7109df20ba5149f7c70830694a0062a242e136b8
2jun0 <soo28819@gmail.com>
2022-10-30 23:24:58 +0900

    Refact: 문제 6번 - 중복 크루 검출 로직을 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import onboarding.problem6.Crew;
+import onboarding.problem6.DuplicatedCrewsFinder;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -13,46 +14,15 @@ public class Problem6 {
                 .collect(Collectors.toList());
     }
 
-    public static Map<String, Set<Crew>> getCrewsByDoubleCharMap(List<Crew> allCrews) {
-        Map<String, Set<Crew>> crewsByDoubleCharMap = new HashMap<>();
-
-        allCrews.forEach(crew -> {
-            String nickname = crew.getNickname();
-
-            for (int nicknameIdx = 0; nicknameIdx < nickname.length() - 1; nicknameIdx++) {
-                String doubleChar = nickname.charAt(nicknameIdx) + "" + nickname.charAt(nicknameIdx + 1);
-
-                crewsByDoubleCharMap.getOrDefault(doubleChar, new HashSet<>()).add(crew);
-            }
-        });
-
-        return crewsByDoubleCharMap;
-    }
-
-    public static Set<Crew> getDuplicatedCrews(List<Crew> allCrews) {
-        Map<String, Set<Crew>> crewsByDoubleCharMap = getCrewsByDoubleCharMap(allCrews);
-
-        // 두글자:[이메일]를 순회하며 이메일의 개수가 2개 이상인 것을 찾음
-        Set<Crew> duplicatedCrews = new HashSet<>();
-        crewsByDoubleCharMap.values().stream()
-                .filter(crews -> crews.size() >= 2)
-                .forEach(duplicatedCrews::addAll);
-
-        return duplicatedCrews;
-    }
-
-    public static <T> List<T> getSortedListBySet(Set<T> set) {
-        return set.stream().sorted().collect(Collectors.toList());
-    }
-
     public static List<String> solution(List<List<String>> forms) {
         List<Crew> crews = createCrews(forms);
 
-        Set<Crew> duplicatedCrewsSet = getDuplicatedCrews(crews);
-        List<Crew> duplicatedCrews = getSortedListBySet(duplicatedCrewsSet);
+        DuplicatedCrewsFinder duplicatedCrewsFinder = new DuplicatedCrewsFinder();
+        Set<Crew> duplicatedCrews = duplicatedCrewsFinder.getDuplicatedCrews(crews);
 
         return duplicatedCrews.stream()
                 .map(Crew::getEmail)
+                .sorted()
                 .collect(Collectors.toList());
     }
 }
