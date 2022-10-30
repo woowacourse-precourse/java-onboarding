af8e34da7d305fec54b05393159ee357eb14dbf7
Isu Shin <isu010614@naver.com>
2022-10-30 21:52:09 +0900

    refactor: Update function for 'clean code'

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,7 +8,7 @@ public class Problem3 {
         int answer = 0;
         List<Integer> numberList = new ArrayList<>();
 
-        for(int i = 1; i <= number; i++){
+        for (int i = 1; i <= number; i++) {
             answer += countClaps(putDigitsToList(i));
         }
         return answer;
@@ -25,20 +25,17 @@ public class Problem3 {
 
     public static int countClaps(List<Integer> numberList) {
         int clap = 0;
-
-        for (int i = 0; i < numberList.size(); i++) {
-            if (isThreeSixNine(numberList.get(i))) {
-                clap++;
-            }
+        for (int number : numberList) {
+            clap += isThreeSixNine(number, clap);
         }
         return clap;
     }
 
-    public static boolean isThreeSixNine(int number) {
+    public static int isThreeSixNine(int number, int clap) {
         if (number == 3 || number == 6 || number == 9) {
-            return true;
+            return 1;
         }
-        return false;
+        return 0;
     }
 
 }
