bff43254203a7be2756e881b8c6f100ea647f7eb
yuntaeLee <leeyt1201@gmail.com>
2022-10-30 22:36:19 +0900

    feat(Problem3): solution 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String s = String.valueOf(i);
+
+            for (int j = 0; j < s.length(); j++) {
+                int n = s.charAt(j);
+                if (n == '3' || n == '6' || n == '9') answer++;
+            }
+        }
+
         return answer;
     }
 }
