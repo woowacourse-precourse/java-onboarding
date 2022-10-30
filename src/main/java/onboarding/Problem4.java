b80023931346c93ed6ca56e4c899ac1f0534830f
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-30 21:50:15 +0900

    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] chars = word.toCharArray();
+        int reverse, tmp, ascii =0;
+
+        for (int i= 0; i < chars.length; i++) {
+            if(Character.isUpperCase(chars[i])) {
+                ascii =65;
+            } else if (Character.isLowerCase(chars[i])) {
+                ascii = 97;
+            } else {
+                answer = answer + ' ';
+                continue;
+            }
+            tmp = Math.abs(ascii - chars[i]);
+            reverse = (ascii + 25) - tmp;
+            answer = answer + String.valueOf((char) reverse);
+        }
         return answer;
     }
 }
