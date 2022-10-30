a3c8152b39ddf87598f315801023b40dd581094d
Jueun <jueun000101@gmail.com>
2022-10-30 22:18:43 +0900

    feat: 한 알파벳 변환 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,17 @@ package onboarding;
  * 2. 알파벳이면 변환, 아니면 변환하지 않고 이어붙여 정답 출력
  */
 public class Problem4 {
+    static final char LOWER = 'a';
+    static final char UPPER = 'A';
+
+    static char changeCase(char ch) {
+        char seq;
+        if (Character.isUpperCase(ch))
+            seq = UPPER;
+        else
+            seq = LOWER;
+        return (char) (seq + Math.abs((ch - seq + 1) - 26));
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
