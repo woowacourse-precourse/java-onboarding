7c20e96ae0adb765e4434ae0d5470ccc4bd87ff9
woosung1223 <dntjd991223@naver.com>
2022-10-30 21:58:26 +0900

    refactor(Problem5): depth 줄이기, 함수명, 변수명 변경

--- src/main/java/onboarding/problem5/Calculator.java
+++ src/main/java/onboarding/problem5/Calculator.java
@@ -1,12 +1,6 @@
 package onboarding.problem5;
 
 public class Calculator {
-    public static boolean isBigger(int number1, int number2) {
-        if (number1 >= number2) {
-            return true;
-        }
-        return false;
-    }
     public static int getQuotient(int number1, int number2) {
         int quotient = number1 / number2;
         return quotient;
