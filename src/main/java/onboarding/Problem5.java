a2391119565c0f58ccb82b8aa6757b2e4313c0bb
jagajang <kosb7544@gmail.com>
2022-10-30 13:09:13 +0900

    p5/add function list

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.List;
 
+/* 기능 목록
+ *
+ * solution : 금액을 가장 적은 지폐/동전으로 나타낸 결과를 반환
+ */
 public class Problem5 {
     private static final List<Integer> moneySize = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
