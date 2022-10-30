19c588315b1eda8d5914e972c831cd616cb222df
doongu <must1080@naver.com>
2022-10-31 01:17:29 +0900

    feat(Problem3): 바꾼 문자열에서 '3', '6', '9'의 갯수를 세는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,8 +7,26 @@ public class Problem3 {
         String tempNumber;
         for(int num = 1; num <= number; num++){
             tempNumber = Integer.toString(num);
+            getCountGameNumber(tempNumber);
         }
 
         return answer;
+    }
+
+    public static int getCountGameNumber(String stringNumber){
+        int countGameNumber = 0;
+        String tempNumber = "";
+        String three = "3";
+        String six = "6";
+        String nine = "9";
+
+        for(int index=0; index < stringNumber.length(); index++){
+            tempNumber = String.valueOf(stringNumber.charAt(index));
+
+            if(tempNumber.equals(three) || tempNumber.equals(six) || tempNumber.equals(nine)){
+                countGameNumber += 1;
+            }
+        }
+        return countGameNumber;
     }
 }
