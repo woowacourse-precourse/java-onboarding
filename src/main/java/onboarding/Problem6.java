1253863df193df8706ec577d82f9901391c398b4
rhqudco <rhqudco1204@naver.com>
2022-10-30 23:30:08 +0900

    FEAT : Duplicate Check Method(return index Set)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -19,4 +19,29 @@ public class Problem6 {
         }
         return tempMap;
     }
+
+    public static Set<Integer> checkDuplicate(Map<Integer, String> map) {
+        Set<Integer> indexSet = new HashSet<>(); // 중복 방지를 위해 이름을 담을 Set 생성
+
+        for (int i = 0; i < map.size(); i++) {
+            if (i == map.size() - 1) { // i가 map의 최종 인덱스와 같아지면 종료
+                return indexSet;
+            }
+            String nickname = map.get(i); // 첫 번째 인덱스와
+            for (int j = i+1; j < map.size(); j++) {
+                String nickname2 = map.get(j); // 다음 인덱스 모두 비교
+                for (int k = 0; k < nickname2.length(); k++) {
+                    if (nickname2.length() != k + 1) { // length를 벗어나는 예외 방지
+                        if (nickname.contains(nickname2.substring(k, k+2))) { // 첫 글자부터 두 글자씩 포함되면
+                            indexSet.add(i);
+                            indexSet.add(j);
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+        return indexSet;
+    }
 }
