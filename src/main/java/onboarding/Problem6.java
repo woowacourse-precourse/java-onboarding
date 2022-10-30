5408f77fbc9234545e90d19b5dd94d2728d49e10
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-10-30 17:50:44 +0900

    feat: solve Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,45 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>();
+
+        int len = forms.size();
+
+        List<String> repetitiveStrings = generateTestCaseStrings(forms);
+        for (int i=0; i<repetitiveStrings.size(); i++){
+            for (int j=0; j<forms.size(); j++){
+                if (forms.get(j).get(1).contains(repetitiveStrings.get(i)))
+                    answer.add(forms.get(j).get(0));
+            }
+        }
+
+        Set<String> setAnswer = new HashSet<String>(answer);
+        List<String> finalAnswer = new ArrayList<String>(setAnswer);
+        Collections.sort(finalAnswer);
+        return finalAnswer;
     }
+
+    public static List<String> generateTestCaseStrings(List<List<String>> forms){
+        List<String> testStrings = new ArrayList<>();
+        List<String> repetitiveStrings = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++){
+            String name = forms.get(i).get(1);
+            for (int j = 0; j < name.length()-1; j++){
+                String tmp = name.substring(j,j+2);
+                if (!testStrings.contains(tmp))
+                    testStrings.add(tmp);
+                else {
+                    if (!repetitiveStrings.contains(tmp))
+                        repetitiveStrings.add(tmp);
+                }
+            }
+        }
+
+        return repetitiveStrings;
+    }
+
 }
