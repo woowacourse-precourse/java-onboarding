169d6d8ae1a5f23886a2dabde09dc4522797d44e
Minky Baek <55613446+toyo30@users.noreply.github.com>
2022-10-30 23:12:59 +0900

    [add] problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,10 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.*;
 
+
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         List<String> friendsOfUser = new ArrayList<>();
