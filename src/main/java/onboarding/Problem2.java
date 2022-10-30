e0c02026b5be24542a0092739f8f2f9fd51f8950
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-10-30 22:53:43 +0900

    P2: 중복 문자 제거 함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,32 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+	private static String removeDup(String str) {
+		String ret = "";
+		int i = 1;
+		boolean dup = false;
+		char temp;
+
+		if(str.length() == 0)
+			return str;
+
+		temp = str.charAt(0);
+		while(i < str.length()) {
+			if(temp != str.charAt(i)) {
+				if(!dup)
+					ret += temp;
+				temp = str.charAt(i);
+				dup = false;
+			}
+			else
+				dup = true;
+			i++;
+		}
+
+		if(!dup)
+			ret += temp;
+
+		return ret;
+	}
 }
