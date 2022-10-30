1f3d9923e962d09bb59f8206dcf7a6f737efb211
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:33:48 +0900

    feat(Problem2): 문자열의 중복 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
 
     private static boolean IsSmallAlpha(char c)
@@ -15,6 +17,23 @@ public class Problem2 {
             if (!IsSmallAlpha(s.charAt(i)))
                 return true;
         return false;
+    }
+
+    private static String makeAnswer(Stack<Character> s)
+    {
+        String result = "";
+        Stack<Character> temp = new Stack<>();
+        while (!s.isEmpty())
+        {
+            temp.push(s.peek());
+            s.pop();
+        }
+        while (!temp.isEmpty())
+        {
+            result += temp.peek();
+            temp.pop();
+        }
+        return result;
     }
 
     public static String solution(String cryptogram) {
