1eb43e8efb9d97f68680eabad73f615b98b74172
SeokHwan An <asok369258@naver.com>
2022-10-30 14:56:29 +0900

    feat [Problem4] : word 중 문자가 대문자 알파벳인지 판별하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,8 @@ public class Problem4 {
     private static final int WORD_MAX_LENGTH = 1000;
     private static final char LOWERCASE_A = 'a';
     private static final char LOWERCASE_Z = 'z';
+    private static final char UPPERCASE_A = 'A';
+    private static final char UPPERCASE_Z = 'Z';
 
     public static String solution(String word) {
         String answer = "";
@@ -18,5 +20,9 @@ public class Problem4 {
 
     public static boolean checkLowercaseAlphabet(char letter) {
         return LOWERCASE_A <= letter && letter <= LOWERCASE_Z;
+    }
+
+    public static boolean checkUppercaseAlphabet(char letter) {
+        return UPPERCASE_A <= letter && letter <= UPPERCASE_Z;
     }
 }
