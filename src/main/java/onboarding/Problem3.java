c0765aca420a06c725acee6b5ffb10ac7129ab4e
LeeRangKim <pwpw102@gmail.com>
2022-10-30 22:19:15 +0900

    feat : problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 0; i < number+1; i++){
+            String[] temp = Integer.toString(i).split("");
+
+            for(int j = 0; j<temp.length; j++){
+                if(temp[j].equals("3") || temp[j].equals("6") || temp[j].equals("9")){ answer++; }
+            }
+        }
+
         return answer;
     }
 }
