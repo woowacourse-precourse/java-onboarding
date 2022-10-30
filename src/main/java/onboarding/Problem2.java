09ed460e743baf248bf3f9b1e030e522b7155899
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-31 07:53:16 +0900

    [refacotr] Problem2 코드 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,40 +3,42 @@ package onboarding;
 import java.util.List;
 
 public class Problem2 {
-    public static String deleteRepeation(String crypt){
-        StringBuilder deleteCrypt = new StringBuilder();;
-        if (crypt.equals("")) return "";
-        deleteCrypt.append(crypt.charAt(0));
-        int i = 0;
-        while(i<crypt.length()-1){
+    public static StringBuilder deleteCrypt = new StringBuilder("");
 
-            if(crypt.charAt(i) == crypt.charAt(i+1))
-            {
-                if (deleteCrypt.charAt(deleteCrypt.length()-1) == crypt.charAt(i))
-                {
-                    deleteCrypt.delete(deleteCrypt.length()-1,deleteCrypt.length());
-                }
-                i += 1;
-                continue;
-            }
-
-            deleteCrypt.append(crypt.charAt(i+1));
-            i += 1;
+    public static void chkDeleteCrypt(int cryptFirst) {
+        if(deleteCrypt.charAt(deleteCrypt.length() - 1) == cryptFirst) {
+            deleteCrypt.delete(deleteCrypt.length() - 1, deleteCrypt.length());
         }
+    }
 
-        return deleteCrypt.toString();
+    public static void deleteChar(char cryptFirst, char cryptSecond){
+        if (cryptFirst == cryptSecond) {
+            chkDeleteCrypt(cryptFirst);
+            return;
+        }
+        deleteCrypt.append(cryptSecond);
+    }
+
+
+    public static void deleteRepeation(String crypt) {
+        if (crypt.equals("")) {
+            return;
+        }
+        deleteCrypt.delete(0,deleteCrypt.length());
+        deleteCrypt.append(crypt.charAt(0));
+        for (int i = 0; i < crypt.length() - 1; i++){
+            deleteChar(crypt.charAt(i),crypt.charAt(i + 1));
+        }
 
     }
 
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        String deleteCrypt  = deleteRepeation(cryptogram);
-        while(true)
-        {
-            if(deleteCrypt.equals(cryptogram)) break;
-            cryptogram = deleteCrypt;
-            deleteCrypt = deleteRepeation(cryptogram);
+        String answer;
+        deleteRepeation(cryptogram);
+        while (!deleteCrypt.toString().equals(cryptogram)) {
+            cryptogram = deleteCrypt.toString();
+            deleteRepeation(cryptogram);
         }
         answer = cryptogram;
         return answer;
