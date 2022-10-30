b6ec33310f49920b4ca5a35f80e766fe7f6d0189
KangHyeon3315 <son2786w@gmail.com>
2022-10-30 19:10:49 +0900

    docs: [4번] javadoc에 파라미터 정보와 리턴 정보 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,12 +2,17 @@ package onboarding;
 
 public class Problem4 {
 
-    /** 문자를 반대로 변환하는 함수 */
+    /**
+     * 문자를 반대로 변환하는 함수
+     *
+     * @param ch 반대로 변환할 문자
+     * @return 반대로 변환된 문자
+     */
     private static char replaceChar(char ch) {
-        if(ch >= 'a' && ch <= 'z') {
-            return (char)('z' - (ch - 'a'));
-        } else if(ch >= 'A' && ch <= 'Z') {
-            return (char)('Z' - (ch - 'A'));
+        if (ch >= 'a' && ch <= 'z') {
+            return (char) ('z' - (ch - 'a'));
+        } else if (ch >= 'A' && ch <= 'Z') {
+            return (char) ('Z' - (ch - 'A'));
         } else {
             return ch;
         }
@@ -15,7 +20,7 @@ public class Problem4 {
 
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
-        for(int i = 0 ; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
 
             answer.append(replaceChar(ch));
