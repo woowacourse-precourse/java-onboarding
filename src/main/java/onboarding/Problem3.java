aba8fcd36c3aaf640660939627c39e9ffb7779a2
yuntaeLee <leeyt1201@gmail.com>
2022-10-31 07:32:52 +0900

    369 개수 계산 기능 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,23 +4,31 @@ public class Problem3 {
     public static int solution(int number) {
         if (outOfRange(number)) return -1;
 
+        return play369Game(number);
+    }
+
+    private static int play369Game(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            int num = i;
-
-            while (num > 0) {
-                int n = num % 10;
-                if (n == 3 || n == 6 || n == 9) {
-                    answer++;
-                }
-
-                num /= 10;
-            }
+            answer += get369Count(i);
         }
 
         return answer;
     }
-    
+
+    private static int get369Count(int num) {
+        int answer = 0;
+        while (num > 0) {
+            int n = num % 10;
+            if (n == 3 || n == 6 || n == 9) {
+                answer++;
+            }
+
+            num /= 10;
+        }
+        return answer;
+    }
+
     static boolean outOfRange(int number) {
         return number < 1 || number > 10000;
     }
