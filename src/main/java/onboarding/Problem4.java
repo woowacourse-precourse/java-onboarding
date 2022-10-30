279aa2c148544f85046fd2008b7f5dec33950ffd
lseok <lseokhyun99@gmail.com>
2022-10-31 00:56:08 +0900

    문제 4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = frogWord(word);
         return answer;
+    }
+    public static String frogWord(String word){
+        StringBuffer fword = new StringBuffer(word);
+
+        for(int i = 0 ; i < fword.length() ; i++){
+            if(fword.charAt(i) > 64 && fword.charAt(i) < 91)
+                fword.replace(i,i+1, Character.toString(155-fword.charAt(i)));
+            else if(fword.charAt(i) > 96 && fword.charAt(i) < 123)
+                fword.replace(i,i+1, Character.toString(219-fword.charAt(i)));
+        }
+        return fword.toString();
     }
 }
