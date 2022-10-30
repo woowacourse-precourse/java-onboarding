c71a4f511413cc36e181e3c22b796432f3eba50b
kokomong2 <ko9603@naver.com>
2022-10-30 20:37:35 +0900

    feat(문제4) 추가 : 알파벳을 반대로 변환하는 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        char[] upper = new char[26];
+        char[] lower = new char[26];
+        for (int i = 0; i < 26; i++) {
+            upper[i] = (char)('Z' -i);
+            lower[i]= (char)('z' -i);
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i)-'A'>=0 &&word.charAt(i)-'A'<=26){
+                sb.append(upper[word.charAt(i)-'A']);
+            }else if(word.charAt(i)-'a'>=0 &&word.charAt(i)-'a'<=26){
+                sb.append(lower[word.charAt(i)-'a']);
+            }else {
+                sb.append(" ");
+            }
+        }
+        return String.valueOf(sb);
     }
 }
