117b3e3c06cb9ba4800e67380ccd9fdb8872dc74
Jeeseob <jeeseob5761@gmail.com>
2022-10-30 21:01:50 +0900

    ADD: problem5 - 알고리즘 코드 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,6 +8,9 @@ import java.util.List;
  * @Problem: 돈의 액수 money가 매개변수로 주어질 때, 각 몇 개로 변환되는지 금액이 큰 순서대로 리스트/배열에 담아 return 하도록 solution 메서드를 완성하라.
  */
 public class Problem5 {
+    // 1. 1 ~ 50,000까지 9가지 화폐를 설정한다.
+    private static final int[] currencies = new int[]{50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
     /**
      * @Method: getMoney
      * * 각 값이 이전 값의 2배 이상씩 증가하기 때문에 Greedy로 해결가능하다.
@@ -14,8 +18,17 @@ public class Problem5 {
      * 2. 가장 큰 화폐부터 순차적으로 각 화폐 값으로 나눈다.
      * 3. 몫은 return할 List에 추가, 나머지는 2번을 다음 화폐 값으로 반복한다.
      */
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+    private static List<Integer> getMoney(int money) {
+        List<Integer> answer = new ArrayList<>();
+        int temp = money;
+        for (int currency : currencies) {
+            answer.add(temp / currency);
+            temp = temp % currency;
+        }
         return answer;
+    }
+
+    public static List<Integer> solution(int money) {
+        return  getMoney(money);
     }
 }
