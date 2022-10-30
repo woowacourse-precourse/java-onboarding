9ce48cafce39dc44dac761aac2f44917fc5a842d
kki4504 <rhkddlf9876@gmail.com>
2022-10-30 20:18:24 +0900

    feat : problem1 기능 구현
    - 두 멤버 중 이긴 멤버 구분
    - 한 페이지의 곱, 합중 큰 값
    - 두 페이지 값중 가장 큰 값
    - 홀수 짝수 구분
    - 연결된 페이지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,47 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+  public static int solution(List<Integer> pobi, List<Integer> crong) {
+    int answer;
+    int pobiValue = memberHighestValue(pobi);
+    int crongValue = memberHighestValue(crong);
+
+    answer = winMember(pobiValue, crongValue);
+    
+    return answer;
+  }
+
+  public static int winMember(int pobi, int crong) {
+    if (pobi > crong)
+      return 1;
+    else if (pobi == crong)
+      return 0;
+    else
+      return 2;
+  }
+
+  public static int memberHighestValue(List<Integer> member) {
+    List<Integer> ints = new ArrayList<>();
+    for (Integer num : member) {
+      ints.add(pageHighestValue(num));
     }
+    return Collections.max(ints);
+  }
+
+  public static int pageHighestValue(Integer num) {
+    int i = num.intValue();
+    int sum = 0;
+    int multiply = 1;
+    while (i != 0) {
+      sum += i % 10;
+      multiply *= i % 10;
+
+      i /= 10;
+    }
+    return sum > multiply ? sum : multiply;
+  }
 }
