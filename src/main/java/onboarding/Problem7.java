dfe9e1da1ea4dea5eb05c0fd0e8f58716924ebe3
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 22:47:56 +0900

    type:feat user와 친구인 ID를 저장하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,28 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Problem7 {
+    static List<String> userFriends = new ArrayList<>();
+    static HashMap<String, Integer> allUsersID = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void createUserFriendsList (String user, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String user_A = friend.get(0);
+            String user_B = friend.get(1);
+            if (user.equals(user_A)) {
+                userFriends.add(user_B);
+            }
+            if (user.equals(user_B)) {
+                userFriends.add(user_A);
+            }
+        }
     }
 }
