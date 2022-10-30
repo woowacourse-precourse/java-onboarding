2478959ca9e0bab3d567c5f00379440d294c0ac9
Sieun Park <psieun0606@gmail.com>
2022-10-30 17:58:50 +0900

    feat: problem4 알파벳/대소문자 여부 확인 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,18 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    static int isAlphabet(char alpha) {
+        if (alpha >= 65 && alpha <= 90) {   // 영어 대문자
+            return 0;
+        }
+        if (alpha >= 97 && alpha <= 122) {  // 영어 소문자
+            return 1;
+        }
+        return -1;  // 알파벳 아님
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
