395fbbb02b38e6fa4431ff6fcb973f8ad5d16184
ONE <sodlapdlf13@gmail.com>
2022-10-30 17:07:31 +0900

    feat: [Problem3] 수의 3, 6, 9 개수를 세어 누적합을 구하는 로직 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,15 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /*
 * 1. 수를 파라미터로 받아 3, 6, 9의 개수를 세어 반환하는 함수 추가
 *   1-1. 수를 각 자리 숫자로 분리하여 3, 6, 9의 개수를 센다
 * 2. 메인로직에서는 수를 함수에 map 하여 누적합을 구하는 로직을 추가
 */
 
-import java.util.Arrays;
-
 public class Problem3 {
     public static int solution(int number) {
+        return IntStream.range(1, number + 1)
+                .map(Problem3::countThreeAndSixAndNine)
+                .sum();
     }
 
     private static int countThreeAndSixAndNine(int number) {
