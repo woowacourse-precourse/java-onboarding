a633c2437cb5d38c5b0a9c889e7bc4ebee823c4c
박시균\psk98 <psg980331@naver.com>
2022-10-30 17:58:57 +0900

    feat : 기능구현 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/*
+* 기능 구현 사항
+* 1. 왼쪽,오른쪽 페이지의 결과값 계산
+* 2. 왼쪽,오른쪽 페이지의 결과값 비교후 큰 값 반환
+* 3. 크롱과 포비의 결과값 비교후 반환
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
