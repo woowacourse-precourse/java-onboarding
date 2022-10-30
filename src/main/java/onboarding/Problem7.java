b167d63153fa5b6db393d39b5260fc8d711007e1
Lee Min Woo <lmw7414@naver.com>
2022-10-30 19:44:20 +0900

    problem7 - 점수 계산을 위한 구조체 설계
    점수 계산하는 방법을 좀 더 구체화하여 주석으로 설명해보았다.  그리고 유저들의 목록을 확실히 알기 위해 새로운 배열리스트를 생성하였다.
    
    점수 계산 후 유저 이름과 점수를 리스트에 같이 담기 위해 새로운 클래스를 생성하였다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,9 @@
 package onboarding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /** 구현해야할 기능
  *  구조체 설계 User(이름, 친구 list)
@@ -13,7 +16,7 @@ import java.util.*;
 public class Problem7 {
 
     static Map<String, User> userMap = new HashMap<>();
-
+    static List<String> userList = new ArrayList<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
         createUser(user);
@@ -22,17 +25,13 @@ public class Problem7 {
             String user1 = friends.get(i).get(0);
             String user2 = friends.get(i).get(1);
 
-            if(!userMap.containsKey(user1))
-                createUser(user1);
-            if(!userMap.containsKey(user2))
-                createUser(user2);
-
-
-
-
+            createUser(user1);
+            createUser(user2);
+            addFriend(user1, user2);
+            addFriend(user2, user1);
         }
 
-        List<String> answer = Collections.emptyList();
+        List<String> answer = calcScore(user, visitors);
         return answer;
     }
 
@@ -43,19 +42,36 @@ public class Problem7 {
         User user = new User();
         user.name = userName;
         userMap.put(userName, user);
+        userList.add(userName);
     }
     public static void addFriend(String userName, String friendName) {
         User user = userMap.get(userName);
-        
+
         if(!user.friends.contains(friendName))
             user.friends.add(friendName);
     }
 
-    public static void calcScore(String userName) {
+    /**
+     * 우선 자신의 친구 목록에 있는지 확인
+     * 자신의 친구 목록에 해당하는 User가 존재한다면 0점
+     * 없다면 내 친구의 친구 목록을 탐색하여(내 친구의 친구 목록까지만 확인하면 됨) 있는지 확인 있다면 10점 * n
+     * 위에서도 없다면 방문자 목록에 있는 사람들은 1점 * n
+     */
+    public static List<String> calcScore(String userName, List<String> visitors) {
         //TODO : 점수를 계산하기 위해서는 유저 이름과 스코어를 저장할 수 있는 자료구조가 필요하다.
+        return null;
     }
     public static class User {
         String name;
         ArrayList<String> friends = new ArrayList<>();
+    }
+    public static class UserForScore {
+        String name;
+        int score;
+
+        public UserForScore(String name) {
+            this.name = name;
+            score = 0;
+        }
     }
 }
