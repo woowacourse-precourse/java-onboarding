49ca4a98a55d9b3f07b19b27bde23ba431ea5a9f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 21:01:44 +0900

    docs: Problem4 - 기능 구현 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ *  reverseAlphabet 메서드 각 char문자를 a-z 반대로 반환해준다.
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
