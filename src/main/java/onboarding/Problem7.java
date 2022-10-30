89dd15edd529a3f8333bfac4a5a879f4f2259887
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-30 15:47:44 +0900

    Feat(Problem7): 해시맵 밸류 Set에 값 넣기 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    static void putValue(HashMap<String, Set<String>> initMap, String friend1, String friend2) {
+        if (!initMap.containsKey(friend1)) {
+            initMap.put(friend1, new HashSet<>(Set.of(friend2)));
+            return;
+        }
+        Set<String> tmpSet = initMap.get(friend1);
+        tmpSet.add(friend2);
+        initMap.put(friend1, tmpSet);
+    }
+
 }
