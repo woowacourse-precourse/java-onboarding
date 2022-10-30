b4ba74ab8bf99a9473bdea57ae32d9b977d78217
shinD <huipulci1@naver.com>
2022-10-31 02:26:27 +0900

    [FEAT] : Problem6 입력을 통해 Crew들의 정보를 생성하는 기능 구현
    구현 기능
    - [X] 입력을 통해 크루원들의 정보를 생성한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,13 @@
 package onboarding;
 
+import onboarding.problem6.input.Problem6Input;
+
 import java.util.List;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
+        Problem6Input input = new Problem6Input(forms);
         List<String> answer = List.of("answer");
         return answer;
     }
