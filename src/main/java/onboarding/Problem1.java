bfe3c34ab6b6f009cabae494d3f427f6d20a7b3d
Nugur1i <duckhee092@naver.com>
2022-10-31 02:51:11 +0900

    feat: Add method to find each digit of page number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
@@ -20,6 +21,43 @@ class Problem1 {
         } else if ( pobi.size() != 2 || crong.size() != 2 ){
             return -1;
         }
+
+        // pobi와 crong 페이지 값에 따른 최대 값 계산
+        ArrayList<Integer> arrNumPobiLeft = new ArrayList<>();
+        ArrayList<Integer> arrNumPobiRight = new ArrayList<>();
+        ArrayList<Integer> arrNumCrongLeft = new ArrayList<>();
+        ArrayList<Integer> arrNumCrongRight = new ArrayList<>();
+
+        // While문 및 나누셈을 통한 페이지의 각 자리수 분리하기
+        int numPL = pobiLeftPage ;
+        int numPR = pobiRightPage ;
+        int numCL = crongLeftPage ;
+        int numCR = crongRightPage ;
+
+        while ( numPL > 0 ) {
+            arrNumPobiLeft.add(numPL%10);
+            numPL /= 10;
+        }
+
+        while ( numPR > 0 ) {
+            arrNumPobiRight.add(numPR%10);
+            numPR /= 10;
+        }
+
+        while ( numCL > 0 ) {
+            arrNumCrongLeft.add(numCL%10);
+            numCL /= 10;
+        }
+
+        while ( numCR > 0 ) {
+            arrNumCrongRight.add(numCR%10);
+            numCR /= 10;
+        }
+
+
+
+
+
 
 
 
