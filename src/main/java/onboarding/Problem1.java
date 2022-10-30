55d00a858d455139db67f5a479bfa8cb7cd98133
yw824 <yongwoo25@gmail.com>
2022-10-31 00:49:34 +0900

    feat: [Problem1] 왼쪽-오른쪽 페이지 간 최대 값 구하기 + 포비/크롱 최대값 비교( main 함수 수정 )
    - getMax(int a, int b)
    	- a : 왼쪽 페이지, b : 오른쪽 페이지
    	- 왼쪽 페이지에 대해 더한 값과 곱한 값을 받아 그 중 최대 값을
    	  정한다.
    	- 오른쪽 페이지에 대해 더한 값과 곱한 값을 받아 그 중 최대 값을
    	  정한다.
    	- 왼쪽 페이지의 최대 값과 오른쪽 페이지의 최대 값을 비교하여
    	  최종 최대 값을 반환한다.
    - main() 수정 사항 :
    	- pobi, crong에 대해 각각 getMax 함수를 호출하여 그들의 페이지
    	  최대 연산 값을 받아온다.
    	- pobi, crong 값을 비교하여, pobi가 크면 1을 반환, crong이 크면
    	  0을 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,9 +7,32 @@ class Problem1 {
 
         // right page equals left page + 1
         if(!isValid(pobi.get(0), pobi.get(1)) || !isValid(crong.get(0), crong.get(1)))
-            return -1;
+            return -1; // error : return -1
 
-        return 0;
+        // get each maximum answer between left page and right page from pobi, crong
+        int pobi_max = getMax(pobi.get(0), pobi.get(1));
+        int crong_max = getMax(crong.get(0), crong.get(1));
+
+        if(pobi_max > crong_max) return 1; // pobi > crong : return 1
+        return 0; // pobi < crong : return 0
+    }
+
+    // get maximum answer between left page and right page
+    private static int getMax(int a, int b) {
+
+        // max_a : maximum answer from left page
+        int max_a = add_each_num(a);
+        int mul_a = mul_each_num(a);
+        if(mul_a > max_a) max_a = mul_a;
+
+        // max_b : maximum answer from right page
+        int max_b = add_each_num(b);
+        int mul_b = mul_each_num(b);
+        if(mul_b > max_b) max_b = mul_b;
+
+        // comparison between answer from left and answer from right
+        if(max_a > mul_b) return max_a;
+        return max_b;
     }
 
     // check if input is valid( both pobi and crong )
