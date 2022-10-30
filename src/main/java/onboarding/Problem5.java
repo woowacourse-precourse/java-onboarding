529f2591ef1c384614f37308d415a8094f55fdf2
xxeol2 <jhj01177@naver.com>
2022-10-30 23:05:04 +0900

    feat(prob5): 금액을 화폐단위로 차례로 나누어주는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = divideMoney(money);
+		return answer;
+	}
+
+	public static List<Integer> divideMoney(int money) {
+		List<Integer> units = List.of(50_000, 10_000, 5000, 1000, 500, 100, 50, 10, 1);
+		List<Integer> result = new ArrayList<Integer>();
+		for (int unit : units) {
+			result.add(money / unit);
+			money %= unit;
+		}
+		return result;
+	}
 }
