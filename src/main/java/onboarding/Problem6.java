ff2e8cada659b1979fcdd9659ae4a62c307efa4e
yeonhee <ydol2@naver.com>
2022-10-31 04:53:37 +0900

    [6번] feat : solution 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = emailResult(checkCrews(forms));
         return answer;
     }
 
