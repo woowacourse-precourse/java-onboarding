5e89201db1df36ff80b89729b46d8905c440cc2b
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-10-30 23:32:16 +0900

    P6: 두 이름이 중복되는지 비교하는 함수 추가
    두 글자만 중복이 확인되면 되므로 이름을 2글자씩 분리하여 가능한
    모든 종류를 다른 이름에 포함되는지 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Comparator;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+	private static boolean compare(String name1, String name2) {
+		if(name1.length() == 1 || name2.length() == 1)
+			return false;
+
+		for(int i = 0;i < name1.length() - 1;i++) {
+			for(int j = 0;j < name2.length() - 1;j++) {
+				if(name1.substring(i,i + 2).equals(name2.substring(j,j + 2)))
+					return true;
+			}
+		}
+
+		return false;
+	}
 }
