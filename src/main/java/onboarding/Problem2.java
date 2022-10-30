d2adc95a937c35437ef1dfcece7037f4fb151c2d
gomljo <gomljo0307@naver.com>
2022-10-31 03:21:11 +0900

    feat(Problem2.java): 기능 구현 #4
    문자열에서 현재 위치의 문자와 다음 위치의 문자가 동일한지 검사하는 isSameString 함수를 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
-
+class StringOperation {
+    boolean isSameString(char previous, char next) {
+        return previous == next;
+    }
+}
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
