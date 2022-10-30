1235a2c7878b7d9d7df9f20b675508f076d0d7a8
GyuJeGal <lkjdee0329@gmail.com>
2022-10-31 00:11:35 +0900

    Problem 3 구현 완료 주의사항 체크: 일반적인 3,6,9 게임처럼 손뼉 한번 치는게 아님. 예를 들어 33이면 손뼉을 두번 쳐야함

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,55 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        int result = 0;
+
+        for(int i = 1; i <= number; i++) {
+            List<Integer> numberOfPosition = getNumberOfPosition(i);
+            result += checkNumberList(numberOfPosition);
+        }
+
+        return result;
+    }
+
+    /**
+     * 1 ~ 10,000 사이의 number을 인자로 받아 각자리 수가 들어있는 리스트를 반환하는 메서드
+     */
+    private static List<Integer> getNumberOfPosition(int number) {
+        List<Integer> result = new ArrayList<>();
+
+        while(number != 0) {
+            result.add(number % 10);
+            number = number / 10;
+        }
+
+        return result;
+    }
+
+    /**
+     * 각자리 수가 들어있는 리스트를 확인해서 3, 6, 9에 해당하는 갯수만큼 반환하는 메서드
+     */
+    private static int checkNumberList(List<Integer> numberList) {
+        int result = 0;
+
+        for(int value : numberList) {
+            result += isClap(value);
+        }
+
+        return result;
+    }
+
+    /**
+     * 한자리 수인 number을 인자로 받고 3, 6, 9 중 해당하면 1 반환, 아니면 0 반환하는 메서드
+     */
+    private static int isClap(int number) {
+        if(number == 3 || number == 6 || number == 9) {
+            return 1;
+        }
+        return 0;
     }
 }
