a06da839e495647a85c6b10e1b12f317469bf8d0
은지 <midzy0228@gmail.com>
2022-10-30 20:10:44 +0900

    feat[Problem5]: 거스름돈 갯수와 거르고 남은 돈 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,6 +13,16 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         // 현재 거스를 돈 단위 파악후 남은 돈 리턴 해주는 함수 구현
         List<Integer> answer = Collections.emptyList();
+        int[] wonList = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        int putMoney;
+        LeftMoney leftmoney = new LeftMoney();
+        ChangeMoney changemoney = new ChangeMoney();
+
+        for (int i=0; i<8; i++) {
+            putMoney = changemoney.changeMoney(money, wonList[i]);
+            answer.add(putMoney);
+            money = leftmoney.leftMoney(money, wonList[i]);
+        }
         return answer;
     }
 }
