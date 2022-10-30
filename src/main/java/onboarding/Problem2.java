81b42166060a7217a41c7d89201f3765da5df304
coldbucks <met.hsw@gmail.com>
2022-10-30 18:31:22 +0900

    feat: 기능 구현 및 git테스트

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,57 @@
 package onboarding;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        return decode(cryptogram);
+    }
+
+    private static String decode(String str) {
+        String result = "";
+
+        boolean check = true;
+
+        String[] array = str.split("");
+
+        // 배열의 size가 변하면 안되거나 변할 필요가 없을때 Array.asList()를 사용
+        List<String> list = Arrays.asList(array);
+
+        while(check) {
+            check = false;
+            List<String> cur_string = new ArrayList<>();
+            cur_string.addAll(list);
+
+            for(int i=0; i < list.size()-1; i++ ) {
+
+                if(list.get(i).equals(list.get(i+1))){
+                    System.out.println(cur_string);
+                    System.out.println(list.get(i) + " " + list.get(i + 1));
+                    cur_string.remove(i + 1);
+                    cur_string.remove(i);
+                    list = cur_string;
+                    check = true;
+                    continue;
+                }
+            }
+
+            list = cur_string;
+
+
+            System.out.println(list);
+        }
+        // StringBuilder는 변경 가능한 문자열 만들어 준다
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s);
+        }
+        result = sb.toString();
+        System.out.println(result);
+        return result;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,44 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
+
         String answer = "";
+
+        String[] questionArray;
+        String[] reverseArray = {"Z", "Y", "X", "W", "V", "U", "T", "S",
+                "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I",
+                "H", "G", "F", "E", "D", "C", "B", "A"};
+
+        Map<String, String> dic = new HashMap<>();
+
+        char[] alpabat = new char[26];
+
+        // Character.toString을 사용하여 java에서 ASCII를 Char로 변환
+        for (int i = 0; i < alpabat.length; i++) {
+            dic.put(Character.toString((char)('A' + i)), reverseArray[i] );
+        }
+
+        // 한 글자씩 배열에 넣는다
+        questionArray = word.split("");
+
+        for(String changeWord : questionArray) {
+
+            boolean uppLow = Character.isUpperCase(changeWord.charAt(0));
+
+            String result = dic.get(changeWord.toUpperCase());
+            if (result == null) {
+                result =" ";
+            } else if (!uppLow) {
+                result = result.toLowerCase();
+            }
+
+            // 문자열 합치기 concat()
+            answer = answer.concat(result);
+        }
         return answer;
     }
 }
