e86594dca6c90e1dadd98d97d66fdec1b8106785
syahn <496300@naver.com>
2022-10-31 01:30:04 +0900

    feat : 중복된 닉네임인지 확인하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,23 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    //중복된 닉네임인지 확인
+    public static boolean IsOverlap(String s1, String s2){
+
+        //같은 문자일 경우, 연속한 문자도 같아야함
+        for(int i = 0; i<s1.length()-1; i++){
+           for(int j = 0; j<s2.length()-1; j++){
+               if(s1.charAt(i) == s2.charAt(j)){
+                   if(s1.charAt(i+1) == s2.charAt(j+1))
+                       return true;
+               }
+           }
+        }
+        return false;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
