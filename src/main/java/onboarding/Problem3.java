98a1b30e1c464e073356002752b97bf604f5bfa2
sohyundoh <lyny123@naver.com>
2022-10-30 16:56:11 +0900

    Problem 3 1번 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int i;
+        for(i = 1;i<=number;i++){
+            int t = i;
+            while(t>0){
+                if((t%10 != 0) &&((t%10)%3 == 0)) answer ++;
+                t/= 10;
+            }
+        }
         return answer;
     }
 }
