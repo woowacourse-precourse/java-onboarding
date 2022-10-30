e5d4f870352011ef86865c100060176495141ddf
Ilchul Kim <huj0159@gmail.com>
2022-10-30 21:50:51 +0900

    feat: make dictionary for converting character

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+    public static HashMap<Character,Character> dic = new HashMap<>();
+
     public static String solution(String word) {
         String answer = "";
+
+        makeDictionary();
+
         return answer;
+    }
+
+    public static void makeDictionary(){
+        for(int i=0;i<26;i++){
+            dic.put((char)(i+'a'),(char)('z'-i));
+            dic.put((char)(i+'A'),(char)('Z'-i));
+        }
     }
 }
