1d2d5a0046a8a4a8d7485db9b6200490707214df
splitube <voicesplit@daum.net>
2022-10-30 13:47:40 +0900

    5번 문제 - 기능 목록 완성
     * 1. divide_in_order(int money) = 화폐가 큰 순서대로 money를 나눈다.
     * 2. save_result_get_left(int money, int divide) = money 를 divide 크기의 화페로 나눠서 결과를 저장하고 나눈 나머지를 반환
     * 3. save_result(int amount) = 결과를 저장한다.
     * 4. show_result() = 저장된 결과를 반환한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+/**
+ * Money_divider
+ *
+ * 1. divide_in_order(int money) = 화폐가 큰 순서대로 money를 나눈다.
+ * 2. save_result_get_left(int money, int divide) = money 를 divide 크기의 화페로 나눠서 결과를 저장하고 나눈 나머지를 반환
+ * 3. save_result(int amount) = 결과를 저장한다.
+ * 4. show_result() = 저장된 결과를 반환한다.
+ */
+
 import java.util.Collections;
 import java.util.List;
 
