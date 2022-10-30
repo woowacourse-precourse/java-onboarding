33c918179281f534dcabfc411a09a124a8c4f67c
kimjaehwi1 <meatandcoffee@naver.com>
2022-10-30 13:55:38 +0900

    5번문제 은행 출금 [최종본]

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(9);
+        distribute(answer,money);
+
+        return answer;
+    }
+    public static List<Integer> distribute(List<Integer> answer , int money) { // 입력받은 금액을 조건에 맞게 환전
+        int[] cloud = {50000 ,10000 ,5000 ,1000 ,500 ,100, 50, 10, 1};
+        int repeat;
+        for(int i=0;i< cloud.length;i++) {
+
+            repeat =0;
+
+            while(cloud[i]<=money) {
+                money -= cloud[i];
+                repeat++;
+            }
+            answer.add(i,repeat); // i칸에 반복된 횟수만큼 저장
+        }
         return answer;
     }
 }
