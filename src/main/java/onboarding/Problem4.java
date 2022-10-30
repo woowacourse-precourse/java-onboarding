4684b7d5a2fe46d86d32477666e86dab5b7f6db1
dong young <dong5854@gmail.com>
2022-10-31 02:32:51 +0900

    [Problem4] feat: add createFrogHashMap function
    청개구리의 말을 번역하는 해시맵을 생성하는 createFrogMap 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    static HashMap<Character, Character> frogHash = new HashMap<>();
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    // 청개구리의 말을 번역할 해시맵
+    public static void createFrogHashMap(){
+        // 소문자
+        int j = 'z';
+        for(int i = 'a'; i <='z'; i++){
+            frogHash.put((char)i, (char)j);
+            j--;
+        }
+
+        // 대문자
+        j = 'Z';
+        for(int i = 'A'; i < 'Z'; i++){
+            frogHash.put((char)i, (char)j);
+            j--;
+        }
     }
 }
