84c8b060be6d89dc30d6efd796a32229a7dc839a
jhsseonn <hosu0125@gmail.com>
2022-10-30 20:14:30 +0900

    feat: (Problem3) 숫자 입력 받아서 배열로 저장

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,14 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int[] input_number= Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+
+
         return answer;
     }
 }
