72bf64b7eb29aa411ce4a17f7a751d470e4b3241
song-wooseok <sws981202@gmail.com>
2022-10-30 18:26:08 +0900

    feat: [PROBLEM3] solution 메서드 작성 및 3번 문제 테스트 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,10 @@
 package onboarding;
 
+import onboarding.problem3.controller.SamYukGuGame;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = new SamYukGuGame().setNumber(number).startGame().getTotalHandClapCount();
         return answer;
     }
 }
