af610d3fff422b0318504443f829bfb456961c33
cmw9957 <cmw9957@gmail.com>
2022-10-30 20:48:41 +0900

    comment: 기능 목록 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/*
+========================================Problem2 기능 목록========================================
+- 중복 문자를 찾아서 제거를 위한 임의의 문자로 치환하는 기능
+- 임의의 문자를 제거하는 기능
+- 문자열에 연속된 문자가 없는지 확인하는 기능
+ */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
