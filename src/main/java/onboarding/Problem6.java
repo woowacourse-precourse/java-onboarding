939c490c28f4fdf970a9195b898244184f31c454
yuntaeLee <leeyt1201@gmail.com>
2022-10-31 06:42:43 +0900

    refactor(Problem6): 닉네임 중복 검증 기능 개선

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -21,10 +21,9 @@ public class Problem6 {
     static boolean validateNicknameDuplication(String input, String target) {
         for (int left = 0; left < input.length() - 2; left++) {
             int right = left + 2;
-            if (right > input.length()) right = input.length();
+            if (right > input.length()) return false;
 
             String inputSub = input.substring(left, right);
-            if (inputSub.length() < 2) return false;
 
             if (target.contains(inputSub)) return true;
         }
