d20b4af211b3ffd287713ad97c3b47f9cab6b1d6
byeongsoon <byeongsoon94@gmail.com>
2022-10-30 16:25:46 +0900

    FEAT: Problem3, 369 게임 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String numberString = Integer.toString(i);
+            numberString = numberString.replaceAll("[^369]", "");
+
+            answer += numberString.length();
+        }
+
         return answer;
     }
 }
