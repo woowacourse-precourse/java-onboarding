61bee02d58c1552ff135833c689594f5065afaca
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 20:30:02 +0900

    type:feat 예외처리 : 이메일과 닉네임의 길이가 올바른지 판단하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,9 +8,23 @@ public class Problem6 {
         return answer;
     }
 
-    private static boolean isRightEmailDomain (String Domain) {
+    private static boolean isRightEmailDomain (String domain) {
         String rightDomain = "email.com";
-        if (Domain.equals(rightDomain)) {
+        if (domain.equals(rightDomain)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isRightEmailAddress (String address) {
+        if (address.length() > 0 && address.length() < 10) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isRightNicknameLength (String nickname) {
+        if (nickname.length() > 0 && nickname.length() < 20) {
             return true;
         }
         return false;
