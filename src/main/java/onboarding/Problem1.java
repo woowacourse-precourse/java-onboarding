0a3f3d8c6a0d3a6f8e86ca16d89b581f4d0a4f06
kkirby54 <sdk926@gmail.com>
2022-10-30 21:02:21 +0900

    fix: 홀짝 예외 추가와 메소드 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -24,13 +24,22 @@ class Problem1 {
         return result;
     }
 
+    // 예외 처리:
+    // 1. 연속된 페이지여야 하며
+    // 2. 홀수, 짝수를 만족해야 한다.
+    static boolean isValid(List<Integer> person){
+        if (person.get(1) - person.get(0) != 1) return false;
+        // 왼쪽은 홀수, 오른쪽은 짝수여야 한다.
+        if ((person.get(0) & 1) != 1) return false;
+        if ((person.get(1) & 1) != 0) return false;
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        // 예외처리 - 페이지 번호 자체가 오류
-        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1){
+        if (!isValid(pobi) || !isValid(crong))
             return -1;
-        }
 
         int crongSumMax = Math.max(getDigitSum(crong.get(0)), getDigitSum(crong.get(1)));
         int crongMulMax = Math.max(getDigitMul(crong.get(0)), getDigitMul(crong.get(1)));
