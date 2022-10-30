cd6d7b7d57fe629fbb8ec8c2626a03f8ec0ebc71
jonghun <jonghuncu@gmail.com>
2022-10-31 06:44:33 +0900

    Feat : mainloop - 중복 문자열 리턴 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -28,40 +28,40 @@ public class Problem6 {
         return Arrays.asList(nameLst);
     }
 
-//    public static String mainloop(List<String> nameLst) {
-//        ArrayList<String> twoNametLst = new ArrayList<>();
-//        String returnName = null;
-//
-//        for(int i=0; i<nameLst.size(); i++) {
-//
-//            String originName = nameLst.get(i);
-//
-//            for(int j=0; j<originName.length()-1; j++) {
-//                String compareName = originName.substring(j, j + 2);
-//                twoNametLst.add(compareName);
-//                /**
-//                 * compareName = originName의 연속된 문자열
-//                 * originName 이 2글자 일 경우 compareName을 originName으로 대입
-//                 */
-//            }
-//        }
-//
-//        for(int i=0; i<twoNametLst.size(); i++) {
-//            String originWord = twoNametLst.get(i);
-//            twoNametLst.remove(i);
-//
-//
-//            for(int j=0; j<twoNametLst.size(); j++) {
-//                if(originWord.equals(twoNametLst.get(j))) {
-//                    returnName = originWord;
-//                    break;
-//                }
-//            }
-//            twoNametLst.add(i, originWord);
-//        }
-//
-//        return returnName;
-//    }
+    public static String mainloop(List<String> nameLst) {
+        ArrayList<String> twoNametLst = new ArrayList<>();
+        String returnName = null;
+
+        for(int i=0; i<nameLst.size(); i++) {
+
+            String originName = nameLst.get(i);
+
+            for(int j=0; j<originName.length()-1; j++) {
+                String compareName = originName.substring(j, j + 2);
+                twoNametLst.add(compareName);
+                /**
+                 * compareName = originName의 연속된 문자열
+                 * originName 이 2글자 일 경우 compareName을 originName으로 대입
+                 */
+            }
+        }
+
+        for(int i=0; i<twoNametLst.size(); i++) {
+            String originWord = twoNametLst.get(i);
+            twoNametLst.remove(i);
+
+
+            for(int j=0; j<twoNametLst.size(); j++) {
+                if(originWord.equals(twoNametLst.get(j))) {
+                    returnName = originWord;
+                    break;
+                }
+            }
+            twoNametLst.add(i, originWord);
+        }
+
+        return returnName;
+    }
 //
 //    public static List<Integer> finalCompare(List<String> nameLst, String returnName) {
 //        ArrayList<Integer> indexLst = new ArrayList<>();
