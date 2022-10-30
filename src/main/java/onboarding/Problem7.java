f553067e778e6bf75f21fd9ddaf0df828607d48b
sonsoowon <yuwoo1018@gmail.com>
2022-10-30 20:35:33 +0900

    feat(P7): 전체 사용자 목록 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    static List<String> getUsers(List<List<String>> friends) {
+        Stream<String> flattenUserStream = friends.stream().flatMap(Collection::stream);
+        List<String> userList = flattenUserStream
+                                        .distinct()
+                                        .collect(Collectors.toList());
+        return userList;
     }
 }
