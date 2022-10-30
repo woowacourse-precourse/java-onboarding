d9f6f248f8a7e2523b489b437a13030676a6fa46
Jeawoong Kim <kimje1109@gmail.com>
2022-10-31 00:58:43 +0900

    fix: Change removeDuplicateChars to handle last character

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,7 +14,7 @@ public class Problem2 {
     public static String removeDuplicateChars(String string) {
         StringBuilder result = new StringBuilder();
 
-        for (int index = 0; index < (string.length() -1) ; index++) {
+        for (int index = 0; index < (string.length() - 1) ; index++) {
             if (string.charAt(index) != string.charAt(index + 1)) {
                 result.append(string.charAt(index));
             }
@@ -22,12 +22,21 @@ public class Problem2 {
                 index++;
             }
         }
-
+        if (string.charAt(string.length() - 2) != string.charAt(string.length() - 1)) {
+            result.append(string.charAt(string.length() - 1));
+        }
+        
         return result.toString();
     }
 
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (checkExistDuplicateChars(cryptogram)) {
+            cryptogram = removeDuplicateChars(cryptogram);
+        }
+        answer = cryptogram;
+
         return answer;
     }
 }
