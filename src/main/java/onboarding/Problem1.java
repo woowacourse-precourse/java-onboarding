b15f8303081eaa0e7e677c19070a7e6c0cbe3ab1
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-10-30 22:01:35 +0900

    feat: declare variables and add scoring code

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -28,6 +28,26 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        return answer;
+        int pobi_left=compare(add(pobi.get(0)),mul(pobi.get(0)));
+        int pobi_right=compare(add(pobi.get(1)),mul(pobi.get(1)));
+
+        int pobi_score=compare(pobi_left,pobi_right);
+
+        int crong_left=compare(add(crong.get(0)),mul(crong.get(0)));
+        int crong_right=compare(add(crong.get(1)),mul(crong.get(1)));
+
+        int crong_score=compare(crong_left,crong_right);
+
+        int winner=compare(pobi_score,crong_score);
+
+        if (winner == pobi_score && winner !=crong_score)
+            return 1;
+        else if (winner == crong_score && winner !=pobi_score)
+            return 2;
+        else if (winner == pobi_score && winner ==crong_score)
+            return 0;
+        else
+            return -1;
+
     }
 }
