32cc46cbba41def84882c43ebece4e230038547e
doongu <must1080@naver.com>
2022-10-31 02:22:22 +0900

    feat(Problem5): 주어진 금액으로 나눈 몫 반환하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    public static int currentMoney;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static int getQuotient(int divMoney){
+        int quotient;
+        quotient = currentMoney / divMoney;
+        return quotient;
     }
 }
