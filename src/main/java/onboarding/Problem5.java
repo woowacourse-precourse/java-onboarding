0cd8fe14d24a662eceff90f0876bd2f5957db03b
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-30 16:45:59 +0900

    Refactor : edit declaration array

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-	public static List<Integer> solution(int money) {
+	public static List solution(int money) {
 
 		List answer = new ArrayList();
 		int moneyCount = 0;
-		int wonUnit[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		int[] wonUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
 		for (int i = 0; i < 9; i++) {
 			moneyCount = money / wonUnit[i];
