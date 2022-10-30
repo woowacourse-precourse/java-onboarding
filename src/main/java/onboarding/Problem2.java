203c345fd96043bbc5397780c35a089a4f810785
dungbangj <yoyob1223@gmail.com>
2022-10-30 20:35:20 +0900

    style: google java style for pro2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,64 +3,66 @@ package onboarding;
 import java.util.Stack;
 
 class StackForPro2 {
-    private static final int MINIMUM_LENGTH = 1;
-    private static final int MAXIMUM_LENGTH = 1000;
 
-    public StackForPro2(String cryptogram) {
-        validateCrypto(cryptogram);
-    }
+  private static final int MINIMUM_LENGTH = 1;
+  private static final int MAXIMUM_LENGTH = 1000;
 
-    public Stack<Character> pushAndPop(Stack<Character> stack, String cryptogram) {
-        char[] array = cryptogram.toCharArray();
-        char tmp = array[0];
-        stack.push(array[0]);
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] == tmp) {
-                continue;
-            }
-            if (stack.peek() == array[i]) {
-                tmp = stack.pop();
-            } else if (stack.peek() != array[i]) {
-                stack.push(array[i]);
-            }
-        }
-        return stack;
-    }
+  public StackForPro2(String cryptogram) {
+    validateCrypto(cryptogram);
+  }
 
-    private void validateCrypto(String crypto) {
-        validateLength(crypto);
-        validateSmallAlpha(crypto);
+  public Stack<Character> pushAndPop(Stack<Character> stack, String cryptogram) {
+    char[] array = cryptogram.toCharArray();
+    char tmp = array[0];
+    stack.push(array[0]);
+    for (int i = 1; i < array.length; i++) {
+      if (array[i] == tmp) {
+        continue;
+      }
+      if (stack.peek() == array[i]) {
+        tmp = stack.pop();
+      } else if (stack.peek() != array[i]) {
+        stack.push(array[i]);
+      }
     }
+    return stack;
+  }
 
-    private void validateLength(String crypto) {
-        if (crypto.length() < 1 || crypto.length() > 1000) {
-            throw new IllegalArgumentException(
-                "입력한 cryptogram의 길이는 " + MINIMUM_LENGTH + "이상 " + MAXIMUM_LENGTH + "이하여야 합니다.");
-        }
-    }
+  private void validateCrypto(String crypto) {
+    validateLength(crypto);
+    validateSmallAlpha(crypto);
+  }
 
-    private void validateSmallAlpha(String crypto) {
-        if (!crypto.matches("^[a-z]*$")) {
-            throw new IllegalArgumentException("cryptogram은 알파벳 소문자로만 입력해야 합니다.");
-        }
+  private void validateLength(String crypto) {
+    if (crypto.length() < 1 || crypto.length() > 1000) {
+      throw new IllegalArgumentException(
+          "입력한 cryptogram의 길이는 " + MINIMUM_LENGTH + "이상 " + MAXIMUM_LENGTH + "이하여야 합니다.");
     }
+  }
+
+  private void validateSmallAlpha(String crypto) {
+    if (!crypto.matches("^[a-z]*$")) {
+      throw new IllegalArgumentException("cryptogram은 알파벳 소문자로만 입력해야 합니다.");
+    }
+  }
 }
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        StackForPro2 material = new StackForPro2(cryptogram);
-        return stackToString(material.pushAndPop(new Stack<>(), cryptogram), new StringBuilder());
-    }
 
-    public static String stackToString(Stack<Character> stack, StringBuilder sb) {
-        if (stack.isEmpty()) {
-            return "";
-        }
-        int size = stack.size();
-        for (int i = 0; i < size; i++) {
-            sb.append(stack.pop());
-        }
-        sb.reverse();
-        return sb.toString();
+  public static String solution(String cryptogram) {
+    StackForPro2 material = new StackForPro2(cryptogram);
+    return stackToString(material.pushAndPop(new Stack<>(), cryptogram), new StringBuilder());
+  }
+
+  public static String stackToString(Stack<Character> stack, StringBuilder sb) {
+    if (stack.isEmpty()) {
+      return "";
     }
+    int size = stack.size();
+    for (int i = 0; i < size; i++) {
+      sb.append(stack.pop());
+    }
+    sb.reverse();
+    return sb.toString();
+  }
 }
