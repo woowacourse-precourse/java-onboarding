257af9200144f94444180b844866d0b94a842fe7
210_reverof <sheltonwon@gmail.com>
2022-10-30 21:48:26 +0900

    feat: add solution of the requested function - 2 (solved Problem2)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.regex.*;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Pattern pattern = Pattern.compile("(\\w)\\1+");
+
+        while (pattern.matcher(cryptogram).find()) {
+            cryptogram = cryptogram.replaceAll("(\\w)\\1+","");
+        }
+
+        return cryptogram;
     }
 }
