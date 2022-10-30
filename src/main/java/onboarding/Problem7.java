1209b956ad3906bf3ec52d9a32b6bab7880522f3
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 14:18:32 +0900

    feat(Problem7): 파라미터 예외 처리 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    private static boolean checkParam(String user, List<List<String>> friends, List<String> visitors) {
+        if (user == null || friends == null || visitors == null) {
+            return true;
+        } else if (checkUser(user)) {
+            return true;
+        } else if (checkFriends(friends)) {
+            return true;
+        } else {
+            return checkUserList(visitors);
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
