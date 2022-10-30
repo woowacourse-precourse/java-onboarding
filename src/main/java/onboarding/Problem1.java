923ea5b5c490ecbe40f1d5cc14a6767f5cea41ea
hossang <zkzk104@naver.com>
2022-10-30 20:20:13 +0900

    feat: 문제1 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,9 +4,24 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if ((pobi.get(1) - pobi.get(0) != 1) || (crong.get(1) - crong.get(0) != 1)) {
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
+        int pobiMax = Math.max(searchMaxNum(pobi.get(0)), searchMaxNum(pobi.get(1)));
+        int crongMax = Math.max(searchMaxNum(crong.get(0)), searchMaxNum(crong.get(1)));
+        if (pobiMax > crongMax) {
+            answer = 1;
+        }
+        if (pobiMax < crongMax) {
+            answer = 2;
+        }
+        if (pobiMax == crongMax) {
+            answer = 0;
+        }
         return answer;
     }
+
     private static int searchMaxNum(Integer num) {
         int mutiply = 1;
         int sum = 0;
