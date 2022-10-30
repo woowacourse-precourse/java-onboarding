482ae7a78c16442f9c123ac0bff5fab39511e1fb
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-31 00:40:40 +0900

    feat(문제 3): 답 계산해서 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,14 +1,54 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem3 {
     public static int solution(int number) {
         final int ERROR = -1;
         if (!validation(number)) return ERROR;
-        int answer = 0;
-        return answer;
+        return count(number);
     }
 
     static boolean validation(int number) {
         return number >= 1 && number <= 10000;
+    }
+
+    static Stack<Digit> digits(int number) {
+        Stack<Digit> digits = new Stack<>();
+        int positionNumber = 0;
+        int weight = 1;
+        while (number > 0) {
+            digits.push(new Digit(number % 10, weight, positionNumber));
+            positionNumber++;
+            weight *= 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    static int count(int number) {
+        int answer = 0;
+        Stack<Digit> digits = digits(number);
+        while (!digits.isEmpty()) {
+            Digit digit = digits.pop();
+            if (digit.digit == 0) continue;
+            answer += digit.digit * digit.positionNumber * digit.weight * 3 / 10;
+            answer += (digit.digit - 1) / 3 * digit.weight;
+            number -= digit.digit * digit.weight;
+            if (digit.digit % 3 == 0) answer += (number + 1);
+        }
+        return answer;
+    }
+
+    static class Digit {
+        int digit;
+        int weight;
+        int positionNumber;
+
+        public Digit(int digit, int weight, int positionNumber) {
+            this.digit = digit;
+            this.weight = weight;
+            this.positionNumber = positionNumber;
+        }
     }
 }
