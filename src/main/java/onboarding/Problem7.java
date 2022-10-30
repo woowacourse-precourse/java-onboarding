46a1903675d95ce400531cfec8bda81ad764c1a0
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-30 18:00:32 +0900

    Refactor : extract functions into separate methods

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,113 +1,137 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 
 public class Problem7 {
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
-		LinkedHashMap<String, Integer> countingMap = new LinkedHashMap<String, Integer>();
+		LinkedHashMap<String, Integer> countingMap = new LinkedHashMap<>();
 		List<String> notToCountName = new ArrayList<>();
 
-
-		/**
-		 * friends 점수 카운팅 기능
-		 */
-		for (int i = 0; i < friends.size(); i++) {
-			List friendsList = friends.get(i);
-
-			for (int j = 0; j < 2; j++) {
-				String friendName = (String) friendsList.get(j);
-				String pairName = getPairName(friendsList, j);
-
-				for (int k = 0; k < friends.size(); k++) {
-					if (k == i) {
-					} else {
-						List otherFriendsList = friends.get(k);
-						if (otherFriendsList.contains(friendName) == true && otherFriendsList.contains(user) == true) {
-							notToCountName.add(friendName);
-							break;
-						} else if (pairName == user) {
-							notToCountName.add(pairName);
-							break;
-						} else if (otherFriendsList.contains(pairName) == true && otherFriendsList.contains(user) == true) {
-							countingMap.merge(friendName, 10, (integer, integer2) -> integer + integer2);
-						}
-					}
-				}
-			}
-		}
-
-
-		notToCountName = removeDuplication(notToCountName);
-
-
-		/**
-		 * visitors 점수 카운팅 기능
+		/*
+		  friends 점수 카운팅 기능
 		 */
 
-		for (int i = 0; i < visitors.size(); i++) {
-			String visitorsName = visitors.get(i);
-			if (notToCountName.contains(visitorsName)) {
-			} else {
-				countingMap.merge(visitorsName, 1, (integer, integer2) -> integer + integer2);
-			}
-		}
+		calculatePointsByFriendsLogic(user, friends, countingMap, notToCountName);
 
 
-		/**
-		 *  출력 조건 설정
-		 *  1) 높은순 정렬
-		 *  2) 5명 미만으로 제한
-		 *  3) 추천 점수가 0점인 경우 제외
-		 *  4) 추천 점수가 같으면 이름순 정렬
+		/*
+		  visitors 점수 카운팅 기능
+		 */
+
+		calculatePointsByVisitorsLogic(visitors, countingMap, notToCountName);
+
+		/*
+		   출력 조건 설정
+		   1) 높은순 정렬
+		   2) 5명 미만으로 제한
+		   3) 추천 점수가 0점인 경우 제외
+		   4) 추천 점수가 같으면 이름순 정렬
 		 */
 
 		int limit = 5;
 		countingMap = sortByDescendingOrder(countingMap, limit);
-		countingMap = removeIfCountEqualsZero(countingMap);
+		removeIfCountEqualsZero(countingMap);
 		List<Map.Entry<String, Integer>> countingList = sortByAlphabeticalOrder(countingMap);
 		List<String> answer = fixStringAndReturn(countingList);
 
-
 		return answer;
+	}
+
+
+	private static void calculatePointsByFriendsLogic(String user, List<List<String>> friends, LinkedHashMap<String, Integer> countingMap, List<String> notToCountName) {
+
+		for (int i = 0; i < friends.size(); i++) {
+			checkEachNameAsPairs(user, friends, countingMap, notToCountName, i);
+		}
+
+	}
+
+	private static void checkEachNameAsPairs(String user, List<List<String>> friends, LinkedHashMap<String, Integer> countingMap, List<String> notToCountName, int i) {
+		List<String> friendsList = friends.get(i);
+
+		for (int j = 0; j < 2; j++) {
+			String friendName = friendsList.get(j);
+			String pairName = getPairName(friendsList, j);
+
+			addUpPointsByFriendsLogic(user, friends, countingMap, notToCountName, i, friendName, pairName);
+		}
+	}
+
+	private static void addUpPointsByFriendsLogic(String user, List<List<String>> friends, LinkedHashMap<String, Integer> countingMap, List<String> notToCountName, int i, String friendName, String pairName) {
+		for (int k = 0; k < friends.size(); k++) {
+
+			if (k != i) {
+				List<String> otherFriendsList = friends.get(k);
+
+				if (otherFriendsList.contains(friendName) && otherFriendsList.contains(user)) {
+					notToCountName.add(friendName);
+					break;
+
+				}
+
+				if (Objects.equals(pairName, user)) {
+					notToCountName.add(pairName);
+					break;
+
+				}
+
+				if (otherFriendsList.contains(pairName) && otherFriendsList.contains(user)) {
+					countingMap.merge(friendName, 10, Integer::sum);
+				}
+
+			}
+		}
+	}
+
+	private static void calculatePointsByVisitorsLogic(List<String> visitors, LinkedHashMap<String, Integer> countingMap, List<String> notToCountName) {
+		notToCountName = removeDuplication(notToCountName);
+
+		for (String visitorsName : visitors) {
+
+			addUpPointsByVisitorsLogic(countingMap, notToCountName, visitorsName);
+
+		}
+	}
+
+	private static void addUpPointsByVisitorsLogic(LinkedHashMap<String, Integer> countingMap, List<String> notToCountName, String visitorsName) {
+
+		if (!notToCountName.contains(visitorsName)) {
+			countingMap.merge(visitorsName, 1, Integer::sum);
+		}
+
 	}
 
 	private static List<String> fixStringAndReturn(List<Map.Entry<String, Integer>> countingList) {
 		List<String> answer = new ArrayList<>();
 		String regex = "=[0-9]+";
-		for (int i = 0; i < countingList.size(); i++) {
-			String returnValue = String.valueOf(countingList.get(i)).replaceAll(regex, "");
+
+		for (Map.Entry<String, Integer> stringIntegerEntry : countingList) {
+			String returnValue = String.valueOf(stringIntegerEntry).replaceAll(regex, "");
 			answer.add(returnValue);
 		}
+
 		return answer;
 	}
 
 	private static List<Map.Entry<String, Integer>> sortByAlphabeticalOrder(LinkedHashMap<String, Integer> countingMap) {
 		List<Map.Entry<String, Integer>> list = new LinkedList<>(
 				countingMap.entrySet());
-		Collections.sort(list,
-				(map1, map2) -> map1.getKey().compareTo(map2.getKey())
-		);
-		Collections.sort(list,
-				(map1, map2) -> map2.getValue().compareTo(map1.getValue())
-		);
+		list.sort(Map.Entry.comparingByKey());
+		list.sort((map1, map2) -> map2.getValue().compareTo(map1.getValue()));
 		return list;
 	}
 
-	private static LinkedHashMap<String, Integer> removeIfCountEqualsZero(LinkedHashMap<String, Integer> countingMap) {
-		LinkedHashMap<String, Integer> zeroRemoveCountingMap = countingMap;
-
+	private static void removeIfCountEqualsZero(LinkedHashMap<String, Integer> countingMap) {
 		countingMap.entrySet().removeIf(entry -> "0".equalsIgnoreCase(String.valueOf(entry.getValue())));
-
-		return zeroRemoveCountingMap;
 	}
 
 
@@ -123,20 +147,21 @@ public class Problem7 {
 	}
 
 
-	private static String getPairName(List friendsList, int j) {
+	private static String getPairName(List<?> friendsList, int j) {
 
 		if (j == 0) {
-			j = 1;
-		} else if (j == 1) {
-			j = 0;
+			return (String) friendsList.get(1);
 		}
-		String pairName = (String) friendsList.get(j);
 
-		return pairName;
+		if (j == 1) {
+			return (String) friendsList.get(0);
+		}
+
+		return (String) friendsList.get(j);
 	}
 
 	private static List<String> removeDuplication(List<String> selectedList) {
-		List<String> answer = new ArrayList<String>();
+		List<String> answer;
 		answer = selectedList.stream().distinct().collect(Collectors.toList());
 		return answer;
 	}
