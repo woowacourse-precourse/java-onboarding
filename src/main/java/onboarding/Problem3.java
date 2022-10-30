b90f08fbe588af57e4b38e67fcb849d3abc871fd
Eunkeee <eunkeeeee@gmail.com>
2022-10-30 20:19:20 +0900

    [P3] refactor: extract several methods
    countClap & splitNumByDigits

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,25 +1,31 @@
 package onboarding;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        for(int currentNum =1; currentNum<=number; currentNum++){
-            // 1 ... number
-            String numToStr = Integer.toString(currentNum);
-            String[] numToStrArr = numToStr.split("");
-            int clap = 0;
-            for(String str: numToStrArr){
-                int stringToInt = Integer.parseInt(str);
-                if(stringToInt % 3 ==0 && stringToInt != 0){
-                    clap++;
-                }
-            }
-        answer += clap;
+        for(int i = 1; i <= number; i++){ // 15
+            int clap = countClap(i);
+            answer += clap;
 
         }
         return answer;
+    }
+
+    private static int countClap(int i) {
+        String[] splitNum = splitNumByDigits(i);
+        int clap = 0;
+        for(String str: splitNum){ // "1"
+            int stringToInt = Integer.parseInt(str); // "1" => 1
+            if(stringToInt % 3 == 0 && stringToInt != 0){ // false
+                clap++;
+            }
+        }
+        return clap;
+    }
+
+    private static String[] splitNumByDigits(int i) {
+        String numToStr = Integer.toString(i); // 15 => "15"
+        String[] splitNum = numToStr.split(""); // "15" => ["1", "5"]
+        return splitNum;
     }
 }
