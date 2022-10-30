a49c7cef4a16e812fe9b2c2b66cd3ee774e46cc1
java-saeng <gusdn85554@naver.com>
2022-10-30 16:40:46 +0900

    feat : Problem6 2글자 이상의 중복 문자열 후보군 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,19 +1,21 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
         Map<String, String> crewInfo = new HashMap<>();
+        Map<String, Integer> duplicatedNicknameCandidate = new HashMap<>();
 
         initializationCrewInfo(forms, crewInfo);
+
+        for (String nickname : crewInfo.keySet()) {
+            initializationDuplicatedNicknameCandidate(nickname, duplicatedNicknameCandidate);
+        }
 
         return answer;
     }
@@ -25,6 +27,18 @@ public class Problem6 {
             String email = form.get(0);
 
             crewInfo.put(nickname, email);
+        }
+    }
+
+    private static void initializationDuplicatedNicknameCandidate(String nickname,
+                                                                  Map<String, Integer> duplicatedNicknameCandidate) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            for (int j = i + 2; j <= nickname.length(); j++) {
+                String substring = nickname.substring(i, j);
+
+                duplicatedNicknameCandidate.put(substring,
+                        duplicatedNicknameCandidate.getOrDefault(substring, 0) + 1);
+            }
         }
     }
 }
