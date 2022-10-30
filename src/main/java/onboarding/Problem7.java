0fc661721e4a97c957a7d0d227824497bd40e0b7
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-10-30 23:22:32 +0900

    문제 7번 <2번 기능 구현 완료>

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,10 +4,9 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-//    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-//        List<String> answer = Collections.emptyList();
-//        return answer;
-        List<String> names = new ArrayList<>(visitors);
+
+        //user와 친구 상태인 사용자를 제외하고, 추천 점수를 계산해야할 유저의 리스트 추출
+        List<String> names = new ArrayList<>();
         for (int i = 0; i < friends.size(); i++) {
             String name = friends.get(i).get(1);
             if (!user.equals(name)) {
@@ -21,28 +20,21 @@ public class Problem7 {
         Set<String> set = new HashSet<String>(names);
         List<String> newNames =new ArrayList<String>(set);
 
-
-        return newNames;
-    }
-}
-
-/* 잠깐 보류
-List<String> checkScore = new ArrayList<>();
-        for (int j = 0; j < visitors.size(); j++) {
-            String name = visitors.get(j);
-            checkScore.add(name);
-
-            for (int i = 0; i < friends.size(); i++) {
-                String name2 = friends.get(i).get(1);
-                if (!user.equals(name2)) {
-                    checkScore.add(friends.get(i).get(0));
-                    checkScore.add(name2);
-                }
-                if (user.equals(name2)) {
-                    for (int q = 0; q < checkScore.size(); q++) {
-                        checkScore.remove(friends.get(i).get(0));
-                    }
+        //사용자와 함께 아는 친구와 그 점수
+        String[][] checkScore = new String[newNames.size()][2];
+        for (int j = 0; j < newNames.size(); j++) {
+            checkScore[j][0] = newNames.get(j);
+            checkScore[j][1] = "10";
+        }
+        for (int m = 0; m < visitors.size(); m++) {
+            for (int y = 0; y < checkScore.length; y++) {
+                String pattern = checkScore[y][0];
+                if (pattern.equals(visitors.get(m))) {
+                    checkScore[y][1] = String.valueOf(Integer.parseInt(checkScore[y][1]) + 1);
+                    System.out.println("중도 확인용 : " + Arrays.deepToString(checkScore));
                 }
             }
         }
-*/
+        return newNames;
+    }
+}
