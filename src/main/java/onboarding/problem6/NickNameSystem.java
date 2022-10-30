cfcf4c4753fdaf8498e46e5d0edc60633dec226f
kimminsu <xjfcnfw3@naver.com>
2022-10-30 13:11:17 +0900

    feat(문제6): 선택된 닉네임의 연속된 2글자 선택 기능 구현

--- src/main/java/onboarding/problem6/NickNameSystem.java
+++ src/main/java/onboarding/problem6/NickNameSystem.java
@@ -6,10 +6,17 @@ public class NickNameSystem {
     private static final int EMAIL = 0;
     private static final int NICK_NAME = 1;
 
-    private static void selectForm(List<List<String>> forms) {
+    private void selectForm(List<List<String>> forms) {
         for (List<String> form : forms) {
-            String nickName = form.get(NICK_NAME);
-            String email = form.get(EMAIL);
+            checkNickName(form);
+        }
+    }
+
+    private void checkNickName(List<String> form) {
+        String selectedName = form.get(NICK_NAME);
+        String email = form.get(EMAIL);
+        for (int idx = 0; idx < selectedName.length() - 1; idx++) {
+            String subStr = selectedName.substring(idx, idx + 2);
         }
     }
 }
