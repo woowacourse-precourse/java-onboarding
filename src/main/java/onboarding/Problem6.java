e0b2f3e382bd06d50128ad9c02a59d278e325588
dong young <dong5854@gmail.com>
2022-10-31 02:41:31 +0900

    [Problem6] feat: add isValidEmailDomain function
    유효한 이메일 도메인인지 판별하는 isValidEmailDomain 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
+    // 추출한 두 글자 저장(두글자, 원본 이메일)
+    static HashMap<String, String> twoSequence = new HashMap<>();
+    // 이메일을 중복 추가 방지용 해시맵(email, boolean), true 면 이미 추가된 이메일
+    static HashMap<String, Boolean> emailAlreadyAdded = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static boolean isValidEmailDomain(String email){
+        String[] emailArr = email.split("@");
+        return Objects.equals(emailArr[emailArr.length - 1], "email.com");
     }
 }
