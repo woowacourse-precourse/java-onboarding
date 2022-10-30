884df1cc67eff4ebacb075de01a4d7abf7691a17
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 16:47:34 +0900

    feat(Problem3): 범위 내의 숫자중 Set에 있는 숫자가 총 몇번 등장하는지 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,6 +9,10 @@ public class Problem3 {
         Set<String> set = new HashSet<String>();
         init(set);
 
+        for (int i = 1; i <= number; i++) {
+            answer += checkingNumber(Integer.toString(i), set);
+        }
+
         return answer;
     }
 
@@ -19,5 +23,19 @@ public class Problem3 {
         set.add("3");
         set.add("6");
         set.add("9");
+    }
+
+    /**
+     * Function for checking whether number contain things in Set
+     * */
+    private static int checkingNumber(String i, Set<String> set) {
+        int total = 0;
+        String num = i;
+        for (int k = 0; k < num.length(); k++) {
+            if (set.contains(String.valueOf(num.charAt(k)))) {
+                total += 1;
+            }
+        }
+        return total;
     }
 }
