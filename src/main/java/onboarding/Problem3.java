2a56225ee1e82e2c74fe3fca9c3837f658f536eb
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-10-30 22:57:44 +0900

    P3: 숫자 수 세는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+	private static int numberCount(int num) {
+		int n = 0;
+
+		while(num > 0) {
+			n++;
+			num /= 10;
+		}
+
+		return n;
+	}
 }
