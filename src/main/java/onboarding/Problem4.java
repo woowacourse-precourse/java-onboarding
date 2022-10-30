f2ff0313c61af810f18b4949b0af6f9a636fd402
RadSparkle <wwwartg1@naver.com>
2022-10-31 02:14:20 +0900

    FEAT : 알파벳만 들어있는지 확인하는 정규식 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.Locale;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static boolean checkAlpabet(String word) {
+
+        //공백제거
+        String onlyAlpabet = word.replaceAll(" ","");
+
+        //알파벳 정규표현식
+        String pattern = "^[a-zA-Z]*$";
+
+        return onlyAlpabet.matches(pattern);
     }
 }
