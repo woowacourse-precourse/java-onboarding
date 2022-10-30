097b61245c789cfc3c68ee6c6df8031569610ae3
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 15:49:02 +0900

    feat(problem6): 입력을 이름-이메일 hashmap으로 가공하는 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashMap<String, String> infoMap = parseUserInfo(forms);
         return answer;
+    }
+
+    private static HashMap<String, String> parseUserInfo(List<List<String>> forms) {
+        Map<String, String> result = forms.stream()
+                .collect(Collectors.toMap(
+                        form -> form.get(1),
+                        form -> form.get(0)
+                ));
+        return new HashMap<>(result);
     }
 }
