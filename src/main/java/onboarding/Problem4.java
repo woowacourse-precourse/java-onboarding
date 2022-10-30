c4c37be09f2ae62c9087f741041ffa96896f0695
rt3310 <btac3310@gmail.com>
2022-10-30 16:38:12 +0900

    feat(Problem4): 문자 알파벳 여부 검사 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static boolean isAlphabet(char character) {
+        if (character >= 'A' && character <= 'Z') {
+            return true;
+        }
+
+        if (character >= 'a' && character <= 'z') {
+            return true;
+        }
+
+        return false;
+    }
 }
