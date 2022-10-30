a366feceb2bf5462870df19afdd36b7a1b634350
chobobdev <chobobdev@naver.com>
2022-10-31 00:03:02 +0900

    feat: [Problem 6] find the keyword and add them into hash map

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String, List<String>> nicknames = new HashMap<String, List<String>>();
+        for(List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            for(int i=0;i<nickname.length()-1;i++) {
+                String keyword = nickname.substring(i,i+2);
+                nicknames.putIfAbsent(keyword, new ArrayList<String>());
+                nicknames.get(keyword).add(email);
+            }
+        }
+        System.out.println(nicknames);
         return answer;
     }
 }
