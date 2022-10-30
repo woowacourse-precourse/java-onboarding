ea01a3430a97dac2dd6c0372ef24a76ccde2b7ed
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:48:39 +0900

    feat : [Problem3] 기능 사항 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 기능 사항
+ * 1.숫자가 3,6,9면 1을 리턴하는 함수
+ * 2.각 자릿수를 확인하기 위한 함수
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
