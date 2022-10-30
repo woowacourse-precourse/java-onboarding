3a6707de67076998f3d5b0d9756d83c7c2273c9a
eora21 <eora21@naver.com>
2022-10-31 08:46:54 +0900

    feat: 🎸 추가 친구 목록 클래스
    `이름 : 친구들` 형태의 Map을 지니는 클래스를 생성하였다. 해당 클래스에 메서드를 추가하여 solution에서 손쉽게 쓸 수
    있는 형태로 구현해보려 한다. 처음엔 기존 문제들처럼 문제 클래스에 메서드를 계속 추가하는 식으로 구현하려 했는데, 코드를 관리
    및 가독성 방면에서 이 방법이 더 나을 것 같다고 판단됐다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    /**
+     * 사용자 아이디 user와 친구 관계 정보 friends, 사용자 타임 라인 방문 기록 visitors가 매개변수로 주어질 때,
+     * 미스터코의 친구 추천 규칙에 따라 점수가 가장 높은 순으로 정렬하여 최대 5명을 return 하도록 solution 메서드를 완성하라.
+     * 이때 추천 점수가 0점인 경우 추천하지 않으며, 추천 점수가 같은 경우는 이름순으로 정렬한다.
+     *
+     * @param user
+     * @param friends
+     * @param visitors
+     * @return
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        FriendsMap<String, Set<String>> totalFriendsMap = new FriendsMap<>(friends);
         List<String> answer = Collections.emptyList();
         return answer;
     }
+}
+
+class FriendsMap<K, V> {
+    private final Map<K, V> totalFriendsMap = new HashMap<>();
+
+    public FriendsMap(List<List<K>> friends) {
+
+        for (List<K> friend : friends) {
+            K friendA = friend.get(0);
+            K friendB = friend.get(1);
+            this.put(friendA, friendB);
+            this.put(friendB, friendA);
+        }
+    }
+
+
 }
