7a6705a120a62efcb988f23ef5c9adee5ba6c1da
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 19:01:15 +0900

    type:feat 주어진 money를 큰 단위의 화폐부터 순서대로 나누면서 몫은 정답배열에, 나머지는 money에 저장하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,12 +2,20 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
     static int[] unitOfMoney = new int[] {50000,10000,5000,1000,500,100,50,10,1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList();
+        for (int unit : unitOfMoney) {
+            int[] quotaAndRemainder = returnQuotaAndRemainder(money, unit);
+            int quota = quotaAndRemainder[0];
+            int remainder = quotaAndRemainder[1];
+            answer.add(quota);
+            money = remainder;
+        }
         return answer;
     }
 
