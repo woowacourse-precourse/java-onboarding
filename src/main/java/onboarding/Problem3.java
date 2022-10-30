b47b18619521d42d71880f0080948785106ebc8f
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-30 17:27:10 +0900

    feat(문제 3): 제한사항 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,13 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        final int ERROR = -1;
+        if (!validation(number)) return ERROR;
         int answer = 0;
         return answer;
+    }
+
+    static boolean validation(int number) {
+        return number >= 1 && number <= 10000;
     }
 }
