347b8c01fc74e5a5ecf760106ead3838897c025b
geneaky <kaktus418@gmail.com>
2022-10-30 19:26:37 +0900

    [feat] solving problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import static java.util.stream.Collectors.toCollection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import onboarding.problem6.DuplicatedNicknameUserEmailFinder;
+import onboarding.problem6.EmailFinder;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        EmailFinder emailFinder = new DuplicatedNicknameUserEmailFinder();
+
+        return emailFinder.findDuplicatedNicknameUserEmail(forms);
     }
+
 }
