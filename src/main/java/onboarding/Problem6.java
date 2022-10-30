72f48c3f1a107dbcca6b96fb9fc97b2a8f017f5f
Koonayoung518 <koony000415@gmail.com>
2022-10-30 19:39:14 +0900

    feat(problem6) : 닉네임 두글자씩 자르기 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        for(int i=0; i< forms.size(); i++){
+            String nickname = forms.get(i).get(1);
+            for(int j=0; j < nickname.length()-1; j++){
+                String word = nickname.substring(j, j+2);
+            }
+        }
         return answer;
     }
 }
