1929e0e847b1dfc3afa3125785db7a4d719b226e
YooSuYeon <usuyn000@gmail.com>
2022-10-31 03:29:34 +0900

    feat(Problem1): 페이지 번호 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,10 +6,33 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        if (!(validatePage(pobi) && validatePage(crong))) {
+            return -1;
+        }
+
         int pobiScore = calculateScore(pobi);
         int crongScore = calculateScore(crong);
 
         return answer;
+    }
+
+    private static boolean validatePage(List<Integer> pages) {
+        int left = pages.get(0);
+        int right = pages.get(1);
+
+        if ((left < 1 || left > 400) || (right < 1 || right > 400)) {
+            return false;
+        }
+
+        if ((left + 1) != right) {
+            return false;
+        }
+
+        if ((left % 2 == 0) || (right % 2 == 1)) {
+            return false;
+        }
+
+        return true;
     }
 
     private static int calculateScore(List<Integer> pages) {
