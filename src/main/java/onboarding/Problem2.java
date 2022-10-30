1a397bcaa4e315acc3c94af7be6147002728ec36
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-30 16:41:08 +0900

    feat(문제 2): 제한사항 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        if (!validation(cryptogram)) return "ERROR";
+    }
+
+    static boolean validation(String cryptogram) {
+        int length = cryptogram.length();
+        if (length == 0 || length > 1000) return false;
+        for (char c : cryptogram.toCharArray()) {
+            if (!Character.isLowerCase(c)) return false;
+        }
+        return true;
     }
 }
