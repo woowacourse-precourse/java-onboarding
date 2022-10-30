515d36ec74046dac6c770b68efb2a75b9e55d867
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-30 21:46:56 +0900

    [ADD] 문제 3 - 숫자를 문자열로 변환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        final int THRESHOLD = 2;
+
         int clap = 0;
+
+        while (number > THRESHOLD) {
+            String digits = Integer.toString(number);
+
+            for (int i=0; i<digits.length(); i++) {
+                char digitChar = digits.charAt(i);
+                int digit = Integer.parseInt(String.valueOf(digitChar));
+            }
+        }
 
         int answer = 0;
         return answer;
