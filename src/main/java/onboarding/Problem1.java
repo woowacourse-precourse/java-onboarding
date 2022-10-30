ed6bfab5fa1b9a0711b7afb267dfa5ad396999d6
jaewoo <wjdwodn0722@naver.com>
2022-10-30 15:40:36 +0900

    feat : problem1 입력값 체크 기능 추가
    - 페이지 범위 초과 확인
    - 같은 면의 페이지 인지 확인
    - 왼쪽 페이지 홀수, 오른쪽 페이지 짝수 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,19 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(checkInput(pobi, crong))     // 예외사항 체크
+            return -1;
+
         return answer;
+    }
+
+    public static int checkInput(List<Integer> pobi, List<Integer> crong){
+        if (pobi[0] <= 1 || crong[1] >= 400)    // 페이지 범위 초과
+            return 1;
+        if (pobi[0] + 1 != pobi[1] || crong[0] + 1 != crong[1])     // 같은 면의 페이지가 아님
+            return 1;
+        if (pobi[0] % 2 != 1 || crong[0] % 2 != 1)      // 왼쪽 페이지가 홀수, 오른쪽 페이지가 짝수가 아님
+            return 1;
+        return 0;
     }
 }
