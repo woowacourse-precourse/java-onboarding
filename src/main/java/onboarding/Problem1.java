eab96ff1cc647f15559ff3db4fcfa396f95c6354
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-31 07:24:49 +0900

    [refactor] Problem1 코드 리팩토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,41 +3,53 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int maxVal(List<Integer> pages)
-    {
-        int maxVal = Integer.MIN_VALUE;
 
-        for (Integer page : pages) {
-            int sum = 0;
-            int mul = 1;
-            for (String strPage : page.toString().split("")) {
-                sum += Integer.parseInt(strPage);
-                mul *= Integer.parseInt(strPage);
-            }
-            if(maxVal<sum) maxVal = sum;
-            if(maxVal<mul) maxVal = mul;
+    public static List<Integer> getSumNMul(Integer page) {
+        int sum = 0;
+        int mul = 1;
+        for (String strPage : page.toString().split("")) {
+            sum += Integer.parseInt(strPage);
+            mul *= Integer.parseInt(strPage);
         }
-        
+        return List.of(sum,mul);
+    }
+
+    public static int maxVal(List<Integer> pages) {
+        int maxVal = Integer.MIN_VALUE;
+        for (Integer page : pages) {
+            List<Integer> sumNmul = getSumNMul(page);
+            if (maxVal<sumNmul.get(0)) {
+                maxVal = sumNmul.get(0);
+            }
+            if (maxVal<sumNmul.get(1)) {
+                maxVal = sumNmul.get(1);
+            }
+        }
         return maxVal;
     }
+
+    public static boolean exception(Integer pobiFirst,Integer pobiSecond,Integer crongFirst,Integer crongSecond) {
+        return (pobiFirst - pobiSecond != 1) ||
+                (crongFirst - crongSecond != 1) ||
+                (pobiFirst == 1) || (pobiSecond == 400) ||
+                (crongFirst == 1) || (crongSecond == 400);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        if ((pobi.get(1)-pobi.get(0) != 1)
-                || (crong.get(1)-crong.get(0) != 1)
-                || (pobi.get(0) == 1) || (pobi.get(1) == 400)
-                || (crong.get(0) == 1) || (crong.get(1) == 400))
-        {
+        int answer;
+        if (exception(pobi.get(0),pobi.get(1),crong.get(0),crong.get(1))) {
             answer = -1;
             return answer;
         }
-
         int pobiScore = maxVal(pobi);
         int crongScore = maxVal(crong);
-        
-        if(pobiScore == crongScore) answer = 0;
-        else if(pobiScore > crongScore) answer = 1;
-        else answer = 2;
-
+        if (pobiScore == crongScore) {
+            answer = 0;
+        } else if (pobiScore > crongScore) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
         return answer;
     }
 }
