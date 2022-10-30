0225539875b780ba64d24a3ed8c7f975a179e629
moonn6pence <shin421179@gmail.com>
2022-10-30 16:06:03 +0900

    feat #1 : 합과 곱 구하기 기능 추가
    - Integer인 페이지 번호의 각 자리를 List로 변환하는 converIntToIntList 메소드 추가
    - 각 자리 합 구하는 getRadixSum 메소드 추가
    - 각 자리 곱 구하는 getRadixMultiply 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -23,4 +23,31 @@ class Problem1 {
 
         return 1;
     }
+
+
+    private static List<Integer> convertIntToIntList(Integer page){
+        List<Integer> radix = new ArrayList<>();
+
+        if (page > 99)
+            radix.add((int) (page / 100));
+
+        if(page > 9)
+            radix.add((int) (page % 100 / 10));
+
+        radix.add(page % 10);
+
+        return radix;
+    }
+
+    private static int getRadixSum(Integer page){
+        List<Integer> radix = convertIntToIntList(page);
+
+        return radix.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private static int getRadixMultiply(Integer page){
+        List<Integer> radix = convertIntToIntList(page);
+
+        return radix.stream().reduce(1,(a,b) -> a*b);
+    }
 }
