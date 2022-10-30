ff35482cdba1533fb5754bba65a262b312c2048f
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-30 20:17:17 +0900

    221030 Feat: P4 - 기능3 추가, 테스트 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,11 +10,11 @@ public class Problem4 {
         //1.입력 문자열을 맨 앞부터 순회하며 대소문자 여부를 판별한다.
         for(int i = 0; i < word.length(); i++) {
             char curr = word.charAt(i);
-            if(curr >= 'A' || curr <= 'Z'){
+            if(curr >= 'A' && curr <= 'Z'){
                 curr = (char)(capitalConstant - curr);
             }
 
-            if(curr >= 'a' || curr <= 'z'){
+            if(curr >= 'a' && curr <= 'z'){
                 curr = (char)(smallConstant - curr);
             }
             //2.변환한 암호를 결과 String에 이어붙인다.
@@ -22,6 +22,8 @@ public class Problem4 {
         }
 
         //3.입력 문자열을 끝까지 순회했다면 결과 String을 반환한다.
+        answer = sb.toString();
+
         return answer;
     }
 }
