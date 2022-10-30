73e7b37b10dbb5eb4b1c3bc4e85ff914a5a604bc
JongDo737 <jongmin373@gmail.com>
2022-10-31 00:15:22 +0900

    problem7 친구추천 목록 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,36 +4,10 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
-        Set<String> totalFriends = new HashSet<>();     // 친구 추천 목록
-        for(int i=0; i< friends.size();i++) {
-            for(int j =0; j<friends.get(0).size(); j++) {
-                totalFriends.add(friends.get(i).get(j));        // friends에 있는 목록
-            }
-        }
-        for(int i=0; i<visitors.size();i++) {
-            totalFriends.add(visitors.get(i));                  // visitors 목록
-        }
-
-        for(int i=0; i< friends.size();i++) {
-            for(int j=0; j<friends.get(i).size(); j++) {
-                totalFriends.add(friends.get(i).get(j));        // 친구추천 목록 추가
-            }
-
-            if (friends.get(i).get(0).equals(user)) {
-                totalFriends.remove(friends.get(i).get(1));         // 친구는 친구추천 목록에서 제거
-                friends.remove(i);
-                i--;
-            }else if (friends.get(i).get(1).equals(user)) {
-                totalFriends.remove(friends.get(i).get(0));
-                friends.remove(i);
-                i--;
-            }
-        }
-        totalFriends.remove(user);      // 자신도 친구추천 목록에서 제거
 
         Map<String,Integer> scoreMap = new HashMap<>();  // 추천친구 목록, 점수 MAP
-        List<String> totalList = new ArrayList<>(totalFriends);
-        for(int i=0; i< totalFriends.size(); i++) {
+        List<String> totalList = commendList(user,friends,visitors);
+        for(int i=0; i< totalList.size(); i++) {
             scoreMap.put(totalList.get(i),0);
         }
         // 친구목록에서 10점씩
@@ -74,5 +48,34 @@ public class Problem7 {
 
 
         return answer;
+    }
+    public static List<String> commendList(String user, List<List<String>> friends, List<String> visitors){
+        Set<String> totalFriends = new HashSet<>();     // 친구 추천 목록
+
+        for(int i=0; i< friends.size();i++) {
+            for(int j =0; j<friends.get(0).size(); j++) {
+                totalFriends.add(friends.get(i).get(j));        // friends에 있는 목록
+            }
+        }
+        for(int i=0; i<visitors.size();i++) {
+            totalFriends.add(visitors.get(i));                  // visitors 목록
+        }
+
+        for(int i=0; i< friends.size();i++) {
+            for(int j=0; j<friends.get(i).size(); j++) {
+                totalFriends.add(friends.get(i).get(j));        // 친구추천 목록 추가
+            }
+
+            if (friends.get(i).get(0).equals(user)) {
+                totalFriends.remove(friends.get(i).get(1));         // 친구는 친구추천 목록에서 제거
+
+            }else if (friends.get(i).get(1).equals(user)) {
+                totalFriends.remove(friends.get(i).get(0));
+
+            }
+        }
+        totalFriends.remove(user);      // 자신도 친구추천 목록에서 제거
+        List<String> totalList = new ArrayList<>(totalFriends);
+        return totalList;
     }
 }
