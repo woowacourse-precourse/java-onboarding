bcd944ae30617e82ee0646fbebe4ad772bc3c6df
squirMM <jsm6616@naver.com>
2022-10-30 17:58:00 +0900

    :rocket: feat(Problem6) fin test

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> set= new HashSet<>();
+        for(int i=0 ; i<forms.size(); i++){
+            List<String> crewInfo = forms.get(i);
+            for(int j=i+1; j<forms.size();j++){
+                String crewName=crewInfo.get(1);
+                String crewEmail=crewInfo.get(0);
+                for(int idx=0; idx<crewName.length()-2;idx++){
+                    if(forms.get(j).get(1).contains(crewName.substring(idx,idx+2))){
+                        set.add(crewEmail);
+                        set.add(forms.get(j).get(0));
+                    }
+                }
+            }
+        }
+        List<String> ans = new ArrayList<>(set);
+        Collections.sort(ans);
+        return ans;
     }
 }
