5714d2930b410ff4043279e642d5812f7dcdafcf
chaerim <qkrdbsk28@naver.com>
2022-10-31 05:19:49 +0900

    feat(problem7): 사용자와 함께 아는 친구가 있는 경우 추천 점수 계산 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,12 +2,15 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<List<String>> friendsList = new ArrayList<>(friends);
         List<String> userFriendsList = getUserFriendsList(user, friendsList);
+
+        HashMap<String, Integer> userFriendScore = getUserFriendScore(userFriendsList, friendsList);
 
         List<String> answer = Collections.emptyList();
         return answer;
@@ -28,4 +31,25 @@ public class Problem7 {
 
         return userFriendsList;
     }
+
+    // 사용자와 함께 아는 친구가 있는 사람의 추천 점수를 계산하는 함수
+    public static HashMap<String, Integer> getUserFriendScore(List<String> userFriendsList, List<List<String>> friendsList){
+        HashMap<String, Integer> userFriendScore = new HashMap<>();
+
+        for(String name: userFriendsList){
+            for (List<String> friend : friendsList) {
+                friend = new ArrayList<>(friend);
+                if (friend.contains(name)) {
+                    friend.remove(name);
+                    if(userFriendScore.containsKey(friend.get(0))){
+                        userFriendScore.replace(friend.get(0), userFriendScore.get(friend.get(0)) + 10);
+                    }else{
+                        userFriendScore.put(friend.get(0), 10);
+                    }
+                }
+            }
+        }
+        return userFriendScore;
+    }
+
 }
