add3bc8dec54aecc7a80e444f5e08d7bfdc5755b
squirMM <jsm6616@naver.com>
2022-10-30 20:49:01 +0900

    :rocket: feat(Problem3) fin test

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int ans = 0;
+        for(int num=1; num<=number;num++){
+            ans+=count369(Integer.toString(num));
+        }
+        return ans;
+    }
+    public static int count369(String strNum){
+        return (int) strNum.chars()
+                .filter(n-> n=='3' || n=='6' || n=='9')
+                .count();
     }
 }
