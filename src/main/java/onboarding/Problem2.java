177b83d5806969e8ece73d13ea299848cc0e177f
jsyeo97 <jsyeo97@naver.com>
2022-10-30 23:06:34 +0900

    Problem 2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
 public class Problem2 {
+	public static boolean duplicated(String cryptogram) {
+		for(int i = 0; i < cryptogram.length()-1; i++) {
+        	if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+        		return true;
+        	}
+        }
+		return false;
+	}
+	
+	public static String deleteDuplication(String cryptogram) {
+		StringBuffer result = new StringBuffer();
+		result.append(cryptogram);
+		for(int i = 0; i < result.length() - 1; i++) {
+        	if(result.charAt(i) == result.charAt(i+1)) {
+        		result.deleteCharAt(i);
+        		result.deleteCharAt(i);
+        		i--;
+        	}
+        }
+		return result.toString();
+	}
+	
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = cryptogram;
+        while(duplicated(answer)) {
+        	answer = deleteDuplication(answer);
+        }
+                
         return answer;
     }
 }
