75f01e1556276ebc35ecbc4775bf176cb4906db3
kimminsu <xjfcnfw3@naver.com>
2022-10-30 13:15:44 +0900

    feat(문제6): 중복 닉네임 이메일 추가 기능 구현

--- src/main/java/onboarding/problem6/NickNameSystem.java
+++ src/main/java/onboarding/problem6/NickNameSystem.java
@@ -3,11 +3,13 @@ package onboarding.problem6;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 public class NickNameSystem {
     private static final int EMAIL = 0;
     private static final int NICK_NAME = 1;
     private static final Map<String, String> hashMap = new HashMap<>();
+    private static final TreeSet<String> emailTree = new TreeSet<>();
 
     private void selectForm(List<List<String>> forms) {
         for (List<String> form : forms) {
@@ -27,7 +29,15 @@ public class NickNameSystem {
     private void checkSubStr(String email, String subStr) {
         if (hashMap.containsKey(subStr)) {
             String hashMapEmail = hashMap.get(subStr);
+            addDuplicateNickNameEmail(email, hashMapEmail);
         }
         hashMap.put(subStr, email);
+    }
+
+    private void addDuplicateNickNameEmail(String email, String hashMapEmail) {
+        if (!email.equals(hashMapEmail)) {
+            emailTree.add(hashMapEmail);
+            emailTree.add(email);
+        }
     }
 }
