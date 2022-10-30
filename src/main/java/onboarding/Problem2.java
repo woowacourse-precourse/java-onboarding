02c51380c29211b160de8642be021d53cd555ce8
hanjaegyeong <hjg000223@naver.com>
2022-10-30 23:04:43 +0900

    [Mission2] feat: 전체 작동 구조 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        StringBuffer str = new StringBuffer(cryptogram);
+        Set<Integer> set;
+        Integer[] arr;
+        String answer;
+        while(true){
+            set = new HashSet<>();
+            //setSet(): 중복 문자의 인덱스를 담은 set을 생성하는 메서드
+            setSet(str, set);
+            arr = set.toArray(new Integer[0]);
+            //isLenZero(): 중복 문자가 없을 경우 반복을 중단하는 메서드
+            if (isLenZero(arr)) break;
+            //delOverlapChar(): 인덱스를 통해 중복 문자를 제거하는 메서드
+            delOverlapChar(str, arr);
+        }
+        answer = new String(str);
         return answer;
     }
 }
