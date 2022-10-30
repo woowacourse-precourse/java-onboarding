e647fe0a9439d75184457be1965e9d0fdd505c24
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-31 08:44:04 +0900

    Problem6 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,50 +1,39 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static void main(String[] args) {
-        List<List<String>> forms = List.of(
-                List.of("jm@email.com", "제이엠"),
-                List.of("jason@email.com", "제이슨"),
-                List.of("woniee@email.com", "워니"),
-                List.of("mj@email.com", "엠제이"),
-                List.of("nowm@email.com", "이제엠")
-        );
-        System.out.println(solution(forms));
-    }
     // TODO 크루의 닉네임을 가져오는 메서드
     public static List<String> getNickname(List<List<String>> list) {
-        List<String> nickname = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
+        for (int i =0; i < list.size(); i ++) {
+            for (int j = i+1; j < list.size(); j++) {
+                String name = list.get(i).get(1);
+                String compare = list.get(j).get(1);
 
-        for (int i = 0; i < list.size(); i++) {
-            nickname.add(list.get(i).get(1));
-        }
-        return nickname;
-    }
-
-    // TODO 닉네임을 두글자씩 나눈뒤 리스트에 저장
-    public static ArrayList<ArrayList<String>> checkDuplication(List<String> nickname) {
-        ArrayList<ArrayList<String>> splitNickname = new ArrayList<>();
-
-        for (int i = 0; i < nickname.size(); i++) {
-            splitNickname.add(new ArrayList<>());
-            String str = "";
-
-            for (int j = 0; j < nickname.get(i).length()-1; j++) {
-                str = nickname.get(i).substring(j, j+2);
-                splitNickname.get(i).add(str);
+                if (isDupl(name, compare)) {
+                    answer.add(list.get(i).get(0));
+                    answer.add(list.get(j).get(0));
+                }
             }
         }
-        return splitNickname;
+        return answer;
+    }
+
+    // TODO 닉네임이 연속으로 중복되는지 판단하는 메서드
+    public static boolean isDupl(String name, String compare) {
+        for (int i = 0; i < name.length()-1; i++) {
+            String str = name.substring(i, i+2);
+
+            return (compare.contains(str));
+        }
+        return false;
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-
-        List<String> nickname = getNickname(forms);
-        ArrayList<ArrayList<String>> list = checkDuplication(nickname);
-
+        List<String> answer = getNickname(forms);
+        answer = answer.stream().distinct().sorted().collect(Collectors.toList());
         return answer;
     }
 }
