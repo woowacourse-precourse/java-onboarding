f2f09ae00949b559ba1efa1c2f33b346664068b4
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 16:47:42 +0900

    feat(PR3): 각 자리수 계산 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem3 {
+
+    static int checkNumberOfDigits(int digits){
+        return (int)(Math.log10(digits) + 1);
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+
         return answer;
     }
 }
