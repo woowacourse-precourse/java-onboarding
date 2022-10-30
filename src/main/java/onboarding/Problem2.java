df3c5abde82c0228198665bdfb8dd419c07854dc
ONE <sodlapdlf13@gmail.com>
2022-10-30 16:43:05 +0900

    feat: [Problem2] 중복된 문자의 발생 여부를 확인하는 메소드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,17 +1,24 @@
 package onboarding;
 
+import java.util.Stack;
+
 /*
-* 1. getResult(String cryptogram): Stack 을 이용해 문자를 처리하는 method
-*   1-1. 문자열의 처음 문자부터 끝 문자까지 탐색
-*   1-2. Stack 이 비어있지 않고 Stack 의 제일 위의 문자가 현재 문자와 같을 때
-*       1-2-1. 같지 않은 문자가 나올 때까지 문자를 가리키는 포인터를 이동
-*       1-2-2. Stack 의 제일 위의 문자를 pop()하고 새로운 문자를 add()
-*   1-3. Stack 에 남아있는 문자들을 문자열로 변환하여 반환
-*/
+ * Stack 을 이용해 문자를 처리하는 로직을 추가
+ *   1-1. 문자열의 처음 문자부터 끝 문자까지 탐색
+ *   1-2. Stack 이 비어있지 않고 Stack 의 제일 위의 문자가 현재 문자와 같을 때
+ *       1-2-1. 같지 않은 문자가 나올 때까지 문자를 가리키는 포인터를 이동
+ *       1-2-2. Stack 의 제일 위의 문자를 pop()하고 새로운 문자를 add()
+ *   1-3. Stack 에 남아있는 문자들을 문자열로 변환하여 반환
+ */
 
 public class Problem2 {
+    private static int index = 0;
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+
+    }
+
+    private static boolean isOverlap(String cryptogram, Stack<Character> stack) {
+        return !stack.isEmpty() && index < cryptogram.length() && stack.peek().equals(cryptogram.charAt(index));
     }
 }
