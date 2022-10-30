876c82e8c727878156db9ea86a83c41008536fc0
wonju-dev <wonjulee.dev@gmail.com>
2022-10-31 01:34:33 +0900

    [refactor] problem4: primitive value 상수 처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,11 @@ package onboarding;
 
 public class Problem4 {
 
+    // A(65) + Z(90) = ASCII_UPPERCASE_RANGE(155)
+    // a(97) + z(122) = ASCII_LOWERCASE_RANGE(219)
+    static final Integer ASCII_UPPERCASE_RANGE = 155;
+    static final Integer ASCII_LOWERCASE_RANGE = 219;
+
     public static String solution(String word) {
         String reversedString = "";
 
@@ -11,6 +16,7 @@ public class Problem4 {
             if (Character.isAlphabetic(c)) {
                 c = getReversedChar(c);
             }
+
             reversedString += c;
         }
 
@@ -19,10 +25,9 @@ public class Problem4 {
 
     private static char getReversedChar(char c) {
         if (Character.isLowerCase(c)) {
-            return (char) (219 - c);
+            return (char) (ASCII_LOWERCASE_RANGE - c);
         }
 
-        return (char) (155 - c);
-
+        return (char) (ASCII_UPPERCASE_RANGE - c);
     }
 }
