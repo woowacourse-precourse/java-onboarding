2825af6abc2116933fb661cf3088cbac1c5e3fb7
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 16:32:28 +0900

    feat(PR5): 화폐 개수 계산 및 출력 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
         List<Integer> moneyList = new ArrayList<Integer>();
 
         moneyList.add(50000);
@@ -20,6 +20,19 @@ public class Problem5 {
         moneyList.add(10);
         moneyList.add(1);
 
+        for(int i=0; i < 9 ; i++){
+            answer.add(0);
+        }
+
+        int moneyCount = 0;
+        for(int i=0; i < 9; i++){
+            if(moneyList.get(i) <= money) {
+                moneyCount = answer.get(i) + (money / moneyList.get(i));
+                answer.set(i, moneyCount);
+                money %= moneyList.get(i);
+            }
+            if(money == 0) break;
+        }
         return answer;
     }
 }
