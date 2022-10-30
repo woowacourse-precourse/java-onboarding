ed3abee1f9771e7424e8825ea5b875921872a4cd
Donghyun <flaehan@gmail.com>
2022-10-30 22:02:52 +0900

    word 길이는 1이상 1000 이하 알파벳 외의 문자는 변환하지 않고 그대로 삽입, 대문자는 대문자로, 소문자는 소문자로 삽입

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,38 +3,40 @@ import java.util.Arrays;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        if (word.length()>=1 && word.length()<=1000) {
 
-        char[] arrCapital = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-        char[] arrCapitalReverse = { 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K',
-                'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A' };
-        char[] arrSmall = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        char[] arrSmallReverse = { 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j',
-                'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a' };
 
-        StringBuffer sb = new StringBuffer();
-        char[] wordToArr = word.toCharArray();
+            char[] arrCapital = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+            char[] arrCapitalReverse = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K',
+                    'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+            char[] arrSmall = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+            char[] arrSmallReverse = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j',
+                    'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
 
-        for (int i = 0; i < word.length(); i++) {
-            char b = wordToArr[i];
+            StringBuffer sb = new StringBuffer();
+            char[] wordToArr = word.toCharArray();
 
-            if (Character.isUpperCase(b)) {
+            for (int i = 0; i < word.length(); i++) {
+                char b = wordToArr[i];
 
-                int index = Arrays.binarySearch(arrCapital, wordToArr[i]);
-                sb.insert(i, arrCapitalReverse[index]);
+                if (Character.isUpperCase(b)) {
 
-            } else if (Character.isLowerCase(b)) {
+                    int index = Arrays.binarySearch(arrCapital, wordToArr[i]);
+                    sb.insert(i, arrCapitalReverse[index]);
 
-                int index = Arrays.binarySearch(arrSmall, wordToArr[i]);
-                sb.insert(i, arrSmallReverse[index]);
+                } else if (Character.isLowerCase(b)) {
 
-            } else {
-                sb.insert(i, b);
+                    int index = Arrays.binarySearch(arrSmall, wordToArr[i]);
+                    sb.insert(i, arrSmallReverse[index]);
+
+                } else {
+                    sb.insert(i, b);
+                }
+                answer = String.valueOf(sb);
             }
-            answer = String.valueOf(sb);
         }
-
         return answer;
     }
 }
