bb4d6daf2d9f7fb9ca0766bb89912cdc25bb7640
jagajang <kosb7544@gmail.com>
2022-10-30 13:00:55 +0900

    p2/seperate check function and add function list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,18 +1,26 @@
 package onboarding;
 
+/* 기능 목록
+ *
+ *  canRemove : 문자열에서 다음 문자를 뺄 수 있는지 확인
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
 
         for(char c : cryptogram.toCharArray()) {
-            if(answer.isEmpty() || !answer.endsWith(String.valueOf(c))) {
-                answer += c;
+            if(canRemove(answer, c)) {
+                answer = answer.substring(0, answer.length() - 1);
                 continue;
             }
 
-            answer = answer.substring(0, answer.length() - 1);
+            answer += c;
         }
 
         return answer;
+    }
+
+    private static boolean canRemove(String str, char c) {
+        return !str.isEmpty() && !str.endsWith(String.valueOf(c));
     }
 }
