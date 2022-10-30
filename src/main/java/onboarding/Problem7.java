35d325b1a6549e45e94482e1fd93a06f26b9dc47
JeongHunHui <deca3248@gmail.com>
2022-10-30 21:48:03 +0900

    [1주차 - Problem7] user와 친구인 유저를 리스트에 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,17 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem7 {
+    // 친구 추천 점수를 저장할 딕셔너리
+    static Map<String, Integer> friendScoreDict = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        // 친구 추천 점수를 저장할 딕셔너리
-        Map<String, Integer> friendScoreDict = new HashMap<>();
+        // 사용자의 친구를 저장할 리스트
+        List<String> userFriends = new ArrayList<>();
+        // friends 에서 사용자와 친구인 유저를 리스트에 저장
+        for (List<String> friendRelations : friends) {
+            if(friendRelations.get(0) == user) userFriends.add(friendRelations.get(1));
+            else if(friendRelations.get(1) == user) userFriends.add(friendRelations.get(0));
+        }
+
         // 방문한 사람의 점수를 딕셔너리에 저장 및 업데이트
         for (String visitor : visitors) {
+            // 이미 친구라면 스킵
+            if(userFriends.contains(visitor)) continue;
             int score = 1;
             if(friendScoreDict.containsKey(visitor))
                 score += friendScoreDict.get(visitor);
