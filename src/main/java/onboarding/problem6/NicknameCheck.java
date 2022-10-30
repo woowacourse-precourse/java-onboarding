cd984656efd318925ed68a0f81ec5b7afa8f8dd0
alstjr706 <alstjr706@naver.com>
2022-10-31 05:31:39 +0900

    feat : (중복 닉네임 제거) 중복 글자가 포함되어 있는 크루는 제한 구현

--- src/main/java/onboarding/problem6/NicknameCheck.java
+++ src/main/java/onboarding/problem6/NicknameCheck.java
@@ -1,12 +1,32 @@
 package onboarding.problem6;
 
-import java.util.List;
+import java.util.*;
 
 public class NicknameCheck {
 
     public List<String> DuplicateCrewMember(List<CrewMember> crewMemberList) {
 
-        return null;
+        List<String> result = new ArrayList<>();
+
+        NicknameAllCases nicknameAllCases = new NicknameAllCases();
+        Set<String> setList = nicknameAllCases.createCase(crewMemberList);
+
+        TwoWordVerification twoWordVerification = new TwoWordVerification();
+        Map<String, Integer> countOfCase = twoWordVerification.CountOfCase(setList, crewMemberList);
+
+        List<String> duplicatedCase = twoWordVerification.DuplicatedCase(countOfCase);
+
+        for (String cases : duplicatedCase) {
+
+            for (CrewMember crew : crewMemberList) {
+                if (crew.getNickname().contains(cases)) {
+                    result.add(crew.getEmail());
+                }
+            }
+        }
+
+        Collections.sort(result);
+        return result;
     }
 }
 
