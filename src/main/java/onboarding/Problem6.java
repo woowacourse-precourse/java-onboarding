94c8b048bffd5a5c5b021c242539b8cd75578156
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-31 01:35:30 +0900

    Add initDuplicationOfNickname function

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,4 +13,9 @@ public class Problem6 {
     public static HashMap<String, String> initDatabaseOfNickname() {
         return new HashMap<String, String>();
     }
+
+    // 이메일과 닉네임의 중복 여부를 판단할 HashMap을 초기화하는 함수
+    public static HashMap<String, Integer> initDuplicationOfNickname() {
+        return new HashMap<String, Integer>();
+    }
 }
