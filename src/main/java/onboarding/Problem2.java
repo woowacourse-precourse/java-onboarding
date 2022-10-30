0965f20b367e554956a7f6e45aa4bea211f40e2c
dongdong-119 <85717356+dongdong-119@users.noreply.github.com>
2022-10-30 20:32:32 +0900

    Problem 2 - deduplicate 메소드 구현 및 테스트 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,4 +12,41 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    public static String deduplicate(String word){
+        // 빈 문자열의 경우 : 그대로 반환함.
+        if(word == null || word.equals("")){
+            return word;
+        }
+
+        char[] chars = word.toCharArray();
+        String result = "";
+
+        int i;
+
+        for (i = 1; i<=chars.length; i++){
+
+            if(i < chars.length){
+                if (chars[i-1] != chars[i]){
+                    result += chars[i-1];
+                } else {
+                    while (i < chars.length && chars[i-1] == chars[i]){
+                        i++;
+                    }
+                }
+            } else if (chars[i-1] == chars[i-1]) {
+                result += chars[i-1];
+            }
+        }
+
+        // 중복이 있으면 temp.length() != chars.length
+        // 이런 경우 중복 제거된 result로 다시 중복 여부 체크하고, 중복있다면 제거
+        if(result.length() != chars.length){
+            return deduplicate(result);
+        }
+
+
+        return result;
+    }
+
 }
