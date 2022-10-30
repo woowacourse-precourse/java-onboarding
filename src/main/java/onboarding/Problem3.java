b5a9e43a8118aac40919bde2f0268b910f715a98
eil-you <qldirl123@gmail.com>
2022-10-30 13:14:26 +0900

    clapCount :  입력받은 숫자에 3,6,9를 포함한 숫자 세기 solution: 1부터 입력받은 숫자까지 3,6,9의 개수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <=number; i++){
+            answer += clapCount(i);
+        }
+        return answer;
+    }
+    public static int clapCount(int number){
+        String [] num = Integer.toString(number).split("");
+        int answer = 0;
+        for(int i = 0; i <num.length; i++){
+            if(num[i].equals("3")||num[i].equals("6")||num[i].equals("9")){
+                answer++;
+            }
+        }
+
+
         return answer;
     }
 }
