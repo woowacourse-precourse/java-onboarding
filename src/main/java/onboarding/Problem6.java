675ea5ea2c48319ae723371ce65e4f8eb118dea5
ymkim97 <rladudaud97@naver.com>
2022-10-30 18:51:10 +0900

    feat: add function number of crew exception

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        ArrayList<String> answer = new ArrayList<>();
+
+        throwCrewNumberException(forms);
+
         return answer;
+    }
+
+    public static boolean throwCrewNumberException(List<List<String>> forms) {
+
+        return forms.isEmpty() || forms.size() > 10_000;
     }
 }
