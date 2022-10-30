6ac0a590c86e4e83d5428c947c9eefd0bd5eae10
wugawuga <dnr3655@naver.com>
2022-10-31 01:53:33 +0900

    feat: 시작 면, 마지막 면 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (validPages(pobi, crong)) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static boolean validPages(List<Integer> pobi, List<Integer> crong) {
+        return isFirstPageAndLastPage(pobi, crong);
+    }
+
+    private static boolean isFirstPageAndLastPage(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.contains(1) || pobi.contains(400)) {
+            return true;
+        }
+        return crong.contains(1) || crong.contains(400);
     }
 }
