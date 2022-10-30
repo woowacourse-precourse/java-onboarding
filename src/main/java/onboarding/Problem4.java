f3518a992c6519bd8c2018daed62aa322d44d9ae
hanjaegyeong <hjg000223@naver.com>
2022-10-31 01:32:30 +0900

    [Mission4] feat: 전체 작동 구조 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for(int i=0;i<word.length();i++){
+            int asciiWord = word.charAt(i);
+            //알파벳의 경우
+            getAnswer(word, answer, i, asciiWord);
+        }
+        return answer.toString();
     }
 }
