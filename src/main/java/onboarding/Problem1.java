3ab0cfd5939821e183e82f3389af985621b181c0
KumaHoon <52783067+KumaHoon@users.noreply.github.com>
2022-10-30 16:26:50 +0900

    문제 1번 문서화 수정.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,71 +6,73 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        // Page 범위가 올바른지 체크하기 (2~399)
+        // 1, 6. 임의로 펼친 책 Page 범위가 올바른지 체크한다. (2~399)
         if (pobi.get(0) <= 1) return -1;
         if (crong.get(0) <= 1) return -1;
         if (pobi.get(1) >= 400) return -1;
         if (crong.get(1) >= 400) return -1;
 
-        // 각자 펼쳐본 왼쪽 페이지, 오른쪽 페이지 최댓값 뽑기
-        int pobi_max = calcMaxPageNumber(pobi);
-        int crong_max = calcMaxPageNumber(crong);
+        // 2~4. 각자 펼쳐본 왼쪽 페이지, 오른쪽 페이지 번호에서 각 자리 숫자를 더하거나 곱해 가장 큰 수를 구한다.
+        int pobiMaxNumber = calcMaxPageNumber(pobi);
+        int crongMaxNumber = calcMaxPageNumber(crong);
 
+        // 5. 점수를 비교하여 가장 높은 사람이 게임의 승자가된다.
         // pobi 승리시 1
-        if (pobi_max > crong_max)
+        if (pobiMaxNumber > crongMaxNumber)
             answer = 1;
         // crong 승리시 2
-        if (pobi_max < crong_max)
+        if (pobiMaxNumber < crongMaxNumber)
             answer = 2;
         // 무승부시 0
-        if (pobi_max == crong_max)
+        if (pobiMaxNumber == crongMaxNumber)
             answer = 0;
 
         return answer;
     }
 
     public static int calcMaxPageNumber(List<Integer> pages){
-        int max_num = 0;
-        int tmp_max_num = 0;
-        int add_result = 0;
-        int mul_result = 0;
+        int maxNum = 0;
+        int tmpMaxNum = 0;
+        int addResult = 0;
+        int mulResult = 1;
         // 자릿수를 담는 리스트 pageNums 선언하기.
         ArrayList<Integer> pageNums = new ArrayList<>();
 
-        // 왼쪽 페이지 ( i = 0 ), 오른쪽 페이지( i = 1 )
+        // 2, 3. 왼쪽 페이지 ( i = 0 ), 오른쪽 페이지( i = 1 )의 자릿수를 더하거나 곱한 최대값을 뽑는다.
         for ( int i = 0; i <= 1; i++ ){
             // 초기화
             pageNums.clear();
-            add_result = 0;
+            addResult = 0;
             // 곱하기 할 때 0인 경우를 고려.
-            mul_result = 1;
+            mulResult = 1;
 
             // 페이지 선언
             int page = pages.get(i);
-            // 자릿수 별로 0이 되기 전까지 pageNum에 원소 집어넣기.
+            // 긱 자릿수 별로 저장한다.
+            // page의 자릿수가 있는 경우까지 pageNum에 자릿수를 원소에 집어 넣는다.
             while(page != 0){
                 int tmp = page % 10;
                 pageNums.add(tmp);
                 page /= 10;
             }
-            // 각 자릿수별로 더하거나 곱한 결과 저장하기
-            for (int j = 0; j < pageNums.size(); j++) {
-                add_result += pageNums.get(j);
-                mul_result *= pageNums.get(j);
+            // 각 자릿수별로 더하거나 곱한 결과 저장한다.
+            for (Integer pageNum : pageNums) {
+                addResult += pageNum;
+                mulResult *= pageNum;
             }
 
-            /* 덧셈 결과와 곱셈 결과 비교해서 tmp_max_num에 저장하기 */
-            if (mul_result >= add_result)
-                tmp_max_num = mul_result;
-            if (add_result > mul_result)
-                tmp_max_num = add_result;
+            /* 덧셈 결과와 곱셈 결과 비교해서 가장 큰 수를 tmp_max_num에 저장한다. */
+            if (mulResult >= addResult)
+                tmpMaxNum = mulResult;
+            if (addResult > mulResult)
+                tmpMaxNum = addResult;
 
 
-            // 만약 계산값이 최댓값보다 큰 경우 최댓값 변경하기.
-            if (tmp_max_num > max_num)
-                max_num = tmp_max_num;
+            // 만약 계산한 최댓값이 이전 최댓값보다 큰 경우 최댓값 변경하기.
+            if (tmpMaxNum > maxNum)
+                maxNum = tmpMaxNum;
         }
 
-        return max_num;
+        return maxNum;
     }
 }
