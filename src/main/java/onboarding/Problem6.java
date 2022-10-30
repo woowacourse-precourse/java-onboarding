c4a4c1a807aeaf74d1b690f7ff99ff6e70808fd6
BaeDongHyeon <sasd730@gmail.com>
2022-10-31 00:37:39 +0900

    feat(Problem6.java): User정보를 저장할 클래스 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,21 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static class User {
+        private String email;
+        private String nickname;
+
+        public User(List<String> userList) {
+            this.email = userList.get(0);
+            this.nickname = userList.get(1);
+        }
+
+        public String getEmail() {
+            return email;
+        }
+        public String getNickname() {
+            return nickname;
+        }
+    }
 }
