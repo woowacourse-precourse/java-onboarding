41cf680cbeba04c2405ca43ca5085bcfbbb157f1
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-10-31 02:24:22 +0900

    4->5단계 오류
    이전 루프의 결과값인 compareToAnswer와 이번 루프에서의 결과값 answer이 동일하다면 while문 종료, answer 반환
    
    뒤에서 세번째와 두번째가 동일할 경우 동일하지 않은 마지막 문자가 answer에 포함되지 않는 오류 발견

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,21 +3,40 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
+        String compareToAnswer = "";
 
 //        1. 1차적으로 중복되는 문자들을 모두 삭제
+//        2. while 문을 씌워 연속되는 중복문자들이 없을 때까지 실행
+//          2-1. 이전 루프의 결과값 compareToAnswer과 비교하였을때
+//               이번 루프에서의 결과값 answer이 동일하다면 while문 종료, answer 반환
+
         char[] chars = cryptogram.toCharArray();
-        for(int i = 0; i < chars.length-1; i++){
-            if(chars[i] != chars[i+1] ){
-                answer += chars[i];
-                if(i == chars.length-2){
-                    answer += chars[i+1];
+        while(true) {
+            answer = "";
+            for (int i = 0; i < chars.length - 1; i++) {
+                if (chars[i] != chars[i + 1]) {
+                    answer += chars[i];
+                    if (i == chars.length - 2) {
+                        answer += chars[chars.length-1];
+                    }
+                }
+                else {
+                    if (i == chars.length - 2) {
+                        continue;
+                    }else{
+                        i += 1;
+                    }
                 }
             }
-            else { i += 1;}
 
+            chars = answer.toCharArray();
+
+            if(compareToAnswer.equals(answer)){
+                break;
+            }
+
+            compareToAnswer = answer;
         }
-
-//        2. while 문을 통해 연속되는 중복문자들이 없을 때까지 실행
 
         return answer;
     }
