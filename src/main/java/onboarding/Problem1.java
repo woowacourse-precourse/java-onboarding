17259cf030eb1ab6421e035f7d21c6030cdc8c2e
hyunsung0917 <sagiri0917@gmail.com>
2022-10-30 12:18:34 +0900

    hellotest

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        System.out.println("hello");
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+    public static void main(String[] args){
+        System.out.println("hello");
     }
 }
