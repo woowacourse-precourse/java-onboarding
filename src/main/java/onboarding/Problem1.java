1814815c5c74b3060dca8ac4a9afc36f69db2ad6
CAEL0 <kjongmin26@gmail.com>
2022-10-30 18:56:04 +0900

    기능 추가: PROBLEM1 입력 검증 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,22 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (isInputInvalid(pobi) || isInputInvalid(crong))
+            return -1;
+
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+    private static boolean isInputInvalid(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        if (leftPage % 2 == 0)
+            return true;
+
+        if (leftPage < 1 || 399 < leftPage)
+            return true;
+
+        return leftPage + 1 != rightPage;
     }
 }
