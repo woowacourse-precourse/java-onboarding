acbf6a5745e99514d0c2bc3696c538d6ca273e21
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-10-30 23:57:57 +0900

    P7: 친구 리스트 맵 생성 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,29 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.HashMap;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+	private static HashMap<String, List<String>> createFriendMap(List<List<String>> friends) {
+		HashMap<String, List<String>> friendMap = new HashMap<>();
+
+		for(List<String> friend: friends) {
+			for(int i = 0;i < 2;i++) {
+				if(!friendMap.containsKey(friend.get(i))) {
+					ArrayList<String> list = new ArrayList<>();
+					list.add(friend.get((i + 1) % 2));
+					friendMap.put(friend.get(i),list);
+				}
+				else
+					friendMap.get(friend.get(i)).add(friend.get((i + 1) % 2));
+			}
+		}
+
+		return friendMap;
+	}
 }
