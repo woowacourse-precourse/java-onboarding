135401344154277553ca0823276d79d58540ba68
KangHyeon3315 <son2786w@gmail.com>
2022-10-30 18:58:26 +0900

    docs: [1번] javadoc에 파라미터 정보와 리턴 정보 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,8 +12,11 @@ class Problem1 {
     private static final int PAGE_END = 400;
 
     /**
-     * 숫자의 각 자릿수를 나누는 함수
+     * 숫자의 각 자릿수를 나누는 함수 <br/>
      * ex) 123 -> [3, 2, 1]
+     *
+     * @param num 주어지는 숫자
+     * @return 각 자릿수 int array
      */
     private static int[] splitDigit(int num) {
         int rest = num / 10;
@@ -25,21 +28,27 @@ class Problem1 {
     }
 
     /**
-     * (요구사항 2번 및 요구사항 3번)
+     * (요구사항 2번 및 요구사항 3번) <br/>
      * 페이지의 각 자릿수를 더하는 함수
+     *
+     * @param splitedPage 각 자릿수로 나누어진 페이지
+     * @return 페이지의 각 자릿수를 더한 값
      */
     private static int getPageDigitSum(int[] splitedPage) {
         return splitedPage[0] + splitedPage[1] + splitedPage[2];
     }
 
     /**
-     * (요구사항 2번 및 요구사항 3번)
+     * (요구사항 2번 및 요구사항 3번) <br/>
      * 페이지의 각 자릿수를 곱하는 함수
+     *
+     * @param splitedPage 각 자릿수로 나누어진 페이지
+     * @return 페이지의 각 자릿수를 곱한 값
      */
     private static int getPageDigitMul(int[] splitedPage) {
-        if(splitedPage[2] == 0 && splitedPage[1] == 0) {
+        if (splitedPage[2] == 0 && splitedPage[1] == 0) {
             return splitedPage[0]; // 한자릿수의 페이지
-        } else if(splitedPage[2] == 0) {
+        } else if (splitedPage[2] == 0) {
             return splitedPage[0] * splitedPage[1]; // 두자릿수의 페이지
         } else {
             return splitedPage[0] * splitedPage[1] * splitedPage[2]; // 세자릿수의 페이지
@@ -47,8 +56,11 @@ class Problem1 {
     }
 
     /**
-     * (요구사항 4번)
+     * (요구사항 4번) <br/>
      * 플레이어의 점수를 계산하는 함수
+     *
+     * @param pageList 플레이어의 페이지 리스트
+     * @return 플레이어의 점수
      */
     private static int getScore(List<Integer> pageList) {
         int[] splitedLeftPage = splitDigit(pageList.get(0));
@@ -63,42 +75,46 @@ class Problem1 {
     private static boolean isEven(int number) {
         return number % 2 == 0;
     }
+
     private static boolean isOdd(int number) {
         return number % 2 == 1;
     }
 
     /**
-     * (요구사항 6번)
+     * (요구사항 6번) <br/>
      * 입력받은 페이지가 유효한지 확인하는 함수
+     *
+     * @param pageList 플레이어의 페이지 리스트
+     * @return 페이지의 유효성
      */
     private static boolean checkPagesValidation(List<Integer> pageList) {
         Integer leftPage = pageList.get(0);
         Integer rightPage = pageList.get(1);
 
-        if(leftPage == null || rightPage == null) return false;
+        if (leftPage == null || rightPage == null) return false;
 
         boolean pageRange = leftPage >= PAGE_START && rightPage <= PAGE_END;
-        boolean pageValidation = isOdd(leftPage) &&  isEven(rightPage);
+        boolean pageValidation = isOdd(leftPage) && isEven(rightPage);
         boolean pageContinues = leftPage + 1 == rightPage;
 
-        return  pageRange && pageValidation && pageContinues;
+        return pageRange && pageValidation && pageContinues;
     }
 
     /**
-     *  (요구사항 5번)
-     *  점수를 비교해 승자를 구분하고 게임 결과를 반환하는 함수
+     * (요구사항 5번) <br/>
+     * 점수를 비교해 승자를 구분하고 게임 결과를 반환하는 함수
      */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(!(checkPagesValidation(pobi) && checkPagesValidation(crong))) {
+        if (!(checkPagesValidation(pobi) && checkPagesValidation(crong))) {
             return EXCEPTION;  // 예외 사항
         }
 
         int pobiScore = getScore(pobi);
         int crongScore = getScore(crong);
 
-        if(pobiScore > crongScore) {
+        if (pobiScore > crongScore) {
             return POBI_WIN;   // 포비 우승
-        } else if(pobiScore < crongScore) {
+        } else if (pobiScore < crongScore) {
             return CRONG_WIN;   // 크롱 우승
         } else {
             return DRAW;   // 무승부
