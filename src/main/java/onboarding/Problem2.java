35d5c0b24e0a3bd6a2dd33a1ed2870bc521e64da
squirMM <jsm6616@naver.com>
2022-10-31 04:06:34 +0900

    :rocket: feat(Problem2) refactoring code line and logic

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,28 +5,27 @@ import java.util.Objects;
 public class Problem2 {
     public static String solution(String cryptogram) {
         while(cryptogram.length()>0){
-            char[] chars = cryptogram.toCharArray();
-            // StringBuilder = 저장공간 절약
-            StringBuilder prev = new StringBuilder(String.valueOf(chars[0]));
-            StringBuilder newCrypt= new StringBuilder();
+            char prev = cryptogram.charAt(0);
+            int lenPrev=1;
+            String newCrypt=cryptogram;
 
-            for(int i=1 ; i< chars.length;i++){
-                // 현재 문자와 prev 의 문자가 같은가?
-                if(Objects.equals(chars[i],prev.charAt(prev.length()-1))){
-                    prev.append(chars[i]);
+            for(int i=1 ; i<cryptogram.length();i++){
+                char c = cryptogram.charAt(i);
+                if(Objects.equals(c,prev)) lenPrev+=1;
+                else if (lenPrev>1) {
+                        newCrypt=newCrypt.replaceAll(repeatStr(prev,lenPrev), "");
+                        break;
                 }
-                else {
-                    // 만약 prev 의 길이가 1이라면 연속되지 않았다는 뜻이므로 newCrypt 에 붙여줌
-                    if (prev.length()==1) newCrypt.append(prev);
-                    prev = new StringBuilder(String.valueOf(chars[i]));
-                }
+                prev=c;
             }
-            // 남는 prev 확인 후 붙여주기
-            if (prev.length()==1)newCrypt.append(prev);
-            // 더 이상 변화가 없다면 break
-            if (Objects.equals(newCrypt.toString(),cryptogram)) break;
-            cryptogram= newCrypt.toString();
+            if (Objects.equals(repeatStr(prev,lenPrev),newCrypt)) newCrypt="";
+            if (Objects.equals(newCrypt,cryptogram)) break;
+            cryptogram=newCrypt;
         }
         return cryptogram;
+    }
+    public static String repeatStr(char prev, int repeatNum){
+        String s = String.valueOf(prev);
+        return s.repeat(repeatNum);
     }
 }
