417f925e95eac9fb51d20fd9aac41c6a19cb6e9f
dungbangj <yoyob1223@gmail.com>
2022-10-30 17:07:23 +0900

    feat: validate pages

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -24,6 +24,46 @@ class UserForPro1 {
     }
 }
 
+class PageForPro1 {
+    private static final int START_PAGE = 1;
+    private static final int END_PAGE = 400;
+
+    private final int max;
+
+    public PageForPro1(int left, int right) {
+        int res = -1;
+        if (!validatePage(left, right)) {
+            CalculateForPro1 calClass = new CalculateForPro1(left, right);
+            res = calClass.compareTwo();
+        }
+        this.max = res;
+    }
+
+    private boolean validatePage(int left, int right) {
+        return validatePage1r400(left) || validatePage1r400(right) || validateTwoPages(left, right);
+    }
+
+    private boolean validatePage1r400(int page) {
+        return page <= START_PAGE || page >= END_PAGE;
+    }
+
+    private boolean validateTwoPages(int left, int right) {
+        return inSerialNum(left, right) || evenOdd(left, right);
+    }
+
+    private boolean inSerialNum(int left, int right) {
+        return left + 1 != right;
+    }
+
+    public boolean evenOdd(int left, int right) {
+        return left % 2 == 0 || right % 2 == 1;
+    }
+
+    public int getMax() {
+        return max;
+    }
+}
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
