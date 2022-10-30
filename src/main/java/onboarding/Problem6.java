cb1a373b930b66d6bfe4523f806c674876d08e02
chanwonlee <clw8679@naver.com>
2022-10-30 14:36:23 +0900

    FEAT: [Problem6] 문자열 중복 확인 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,23 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static boolean hasDuplication(String name1, String name2) {
+        int n1 = name1.length();
+        int n2 = name2.length();
+
+        for (int i = 0; i < n1 - 1; i++) {
+            for (int j = 0; j < n2 - 1; j++) {
+                if (findDuplication(name1, name2, i, j)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean findDuplication(String name1, String name2, int i, int j) {
+        if (name1.charAt(i) == name2.charAt(j)) {
+            return name1.charAt(i + 1) == name2.charAt(j + 1);
+        }
+        return false;
+    }
 }
