c55aaf577ab82f81e2b10545aa9db74b75acbb26
minji <heunhanireum@gmail.com>
2022-10-30 16:28:11 +0900

    Problem3: 1부터 주어진 숫자까지 순회하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int num = 1 ; num <= number ; num += 1) {
+            answer += numOfClap(num);
+        }
         return answer;
     }
+
 }
