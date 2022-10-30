d479812972e6f23720bcf4f8fefb13a6a99aa695
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:13:41 +0900

    feat : 각 숫자가 3,6,9를 포함하는지 확인 및 덧셈

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String stringNum ="";
+        char temp;
+
+        // 1~N까지 탐색
+        for(int i = 1 ; i <= number ; i++){
+            stringNum =Integer.toString(i);
+
+            //각 숫자에 3,6,9가 포함되어있는지 확인
+            for(int j = 0 ; j < stringNum.length() ; j++){
+                temp = stringNum.charAt(j);
+                if(temp == '3' || temp == '6' || temp == '9'){
+                    answer++;
+                }
+            }
+        }
+
         return answer;
     }
 }
