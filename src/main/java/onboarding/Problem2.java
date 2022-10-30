de57f498a97cadb83671416cd57133ce3e2a1aaa
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:32:31 +0900

    feat(Problem2): 파라미터 예외 처리 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,17 @@
 package onboarding;
 
 public class Problem2 {
+
+    private static boolean checkParam(String s)
+    {
+        if (s == null)
+            return true;
+        for (int i=0; i<s.length(); i++)
+            if (!IsSmallAlpha(s.charAt(i)))
+                return true;
+        return false;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
