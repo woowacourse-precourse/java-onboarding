b5258baabc636d501ea8004164d0c412d50bfe48
Jeeseob <jeeseob5761@gmail.com>
2022-10-30 17:44:52 +0900

    Requirement-list: Problem4 - 기능 목록 초안 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,17 @@
 package onboarding;
 
+/**
+ * @Problem: 엄마 말씀 word가 매개변수로 주어질 때, 아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록 solution 메서드를 완성하라.
+ */
 public class Problem4 {
+    /**
+     * @Method: changeAlphabet
+     * 아스키 코드를 기반은로 각 문자를 수정한다.
+     * 1. 소문자의 경우 122 - (char - 97)
+     * 2. 대문자의 경우 90 - (char - 65)
+     * 3. 나머지 값에 대해서는 그대로
+     */
+
     public static String solution(String word) {
         String answer = "";
         return answer;
