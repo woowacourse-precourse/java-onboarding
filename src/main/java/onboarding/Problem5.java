c5b1dc5df254b841d87b055fe25bb4b6faeb58a4
cgk95 <kjkj5868@gmail.com>
2022-10-31 01:38:19 +0900

    P5 큰 단위부터 최대한 소모하여 각기 변환한 개수를 리스트로 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.HashMap;
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] money_lst = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < money_lst.length; i++) {
+            money = getChanges(money, answer, money_lst[i]);
+        }
         return answer;
+    }
+    /**큰 액수의 화폐를 최대한 많이 사용하여 변환**/
+    private static int getChanges(int money, List<Integer> answer, int money_lst) {
+        int num = money / money_lst;
+        answer.add(num);
+        money = money % money_lst;
+        return money;
     }
 }
