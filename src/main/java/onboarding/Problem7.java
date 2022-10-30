def86c3f44a27ad48e363ae0df2a71a88b26de75
chj <bluedog129@gmail.com>
2022-10-30 20:48:59 +0900

    [P7]친구 관계 탐색기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            for (int j = 0; j < friends.get(i).size(); j++) {
+
+            }
+        }
+
+
         return answer;
     }
 }
