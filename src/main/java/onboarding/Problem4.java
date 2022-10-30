e45493dc19a7f230cbede55b76334a6ca78571b7
parksey <parkseyeon99@naver.com>
2022-10-30 14:22:47 +0900

    feat(problem 4): 기능요구사항 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,22 @@
 package onboarding;
 
+/**
+ * Problem 4
+ *
+ * 기능 요구 사항
+ * 1. word가 주어지면 사전을 참고하여 반대로 변환
+ *
+ * 제한 사항
+ * 1. word 길이는 1이상 1000이하
+ * 2. 알파벳 외의 문자는 변환하지 않는다.
+ * 3. 대문자는 대문자, 소문자는 소문자로
+ */
 public class Problem4 {
+    /**
+     *
+     * @param word
+     * @return 변환된 word값
+     */
     public static String solution(String word) {
         String answer = "";
         return answer;
