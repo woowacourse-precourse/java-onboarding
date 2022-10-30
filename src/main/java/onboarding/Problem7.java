f2ed90b50bcc8a2df87a454b376f7d5fb89ef230
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-30 17:25:21 +0900

    feat(Problem7): user와 친구인 사람 필터링하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> userFriends;
+
+        userFriends = getUserFriends(user, friends);
+        System.out.println(userFriends);
+
         return answer;
+    }
+
+    private static List<String> getUserFriends(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            int friendIndex = friend.indexOf(user);
+
+            if (friendIndex == 0) {
+                userFriends.add(friend.get(1));
+            } else if (friendIndex == 1) {
+                userFriends.add(friend.get(0));
+            }
+        }
+
+        return userFriends;
     }
 }
