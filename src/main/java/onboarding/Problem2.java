d67894cd0b9289b511db3d01a25e826fe54f5912
YooSuYeon <usuyn000@gmail.com>
2022-10-31 03:56:43 +0900

    feat(Problem2): 암호문 최초 길이 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,6 +6,10 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
+        if (cryptogram.length() == 1) {
+            return cryptogram;
+        }
+
         Pattern pattern = Pattern.compile("([a-z])\\1+");
 
         boolean removeCheck = true;
