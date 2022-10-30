e774c3c2e21ac13fd5d594a814a10f8985e06410
yuntaeLee <leeyt1201@gmail.com>
2022-10-30 21:04:15 +0900

    Feat(Problem1): 페이지 각 자리수 합 계산 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static int getMSum(int page) {
+        int sum = 0;
+        while (page > 0) {
+            sum += page % 10;
+            page /= 10;
+        }
+
+        return sum;
+    }
 }
