be6fa2365a014a0095174d6c68170db6c8939dfd
kki4504 <rhkddlf9876@gmail.com>
2022-10-30 22:38:34 +0900

    feat : problem2 구현
    - decode 구현
      - decoded 문자열의 끝 문자와 현재 비교문자를 비교하여 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = decode(cryptogram);
         return answer;
     }
+
+    public static String decode (String cryptogram) {
+        char[] chars = cryptogram.toCharArray();
+
+        String decoded = "";
+        char prev = ' ';
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != prev) {
+                decoded += chars[i];
+                prev = chars[i];
+            } else {
+                decoded = decoded.substring(0, decoded.length() - 1);
+                prev = (decoded.length() > 0) ? decoded.charAt(decoded.length() - 1) : ' ';
+            }
+        }
+        return decoded;
+    }
+
 }
