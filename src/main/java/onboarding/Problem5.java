c55906980afb044ecf20f1fd18b7ac75ba79b5ff
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-10-30 12:14:02 +0900

    feat: solve Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] bill = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i : bill){
+            answer.add(money/i);
+            money -= (money/i)*i;
+        }
+
         return answer;
     }
 }
