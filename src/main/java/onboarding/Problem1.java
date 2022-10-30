9b9ec88c7727aa12f8e419f46cf4a65c1f9517ac
sohyundoh <lyny123@naver.com>
2022-10-30 16:30:06 +0900

    Problem 1 3번 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -29,11 +29,22 @@ class Problem1 {
      }
      return score;
     }
-
+    public static boolean exceptioncheck(List<Integer> a){
+        //3번 기능 예외 사항 구현
+        if(a.get(1) - a.get(0) != 1 ){
+            return false;
+        }else if (a.contains(1) || a.contains(400)){
+            return false;
+        }
+        return true;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         int score_pobi = score(pobi.get(1));
         int score_crong = score(crong.get(1));
+        if(!(exceptioncheck(pobi)) ||!(exceptioncheck(crong))){
+            return -1;
+        }
         if(score_pobi > score_crong){
             answer = 1;
         }else if(score_pobi < score_crong){
