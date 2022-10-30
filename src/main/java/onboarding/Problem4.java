55eeb5ece2066fac35daa12c4fcb17b6abd86711
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:51:30 +0900

    feat : [Problem4] 기능 사항 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ * 기능 사항
+ * 1.알파벳이 가지는 아스키코드 번호를 반대로 변환해 리턴하는 함수
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
