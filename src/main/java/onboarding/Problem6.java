7aa4f386c933c08373589b7ce9655f8dd3b5cd57
iamsseaa <homingsea@gmail.com>
2022-10-30 22:55:36 +0900

    feat(Problem6): 이메일 형식에 부합하지 않은 입력에 대한 예외처리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,18 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
+        try {
+            ExceptionProblem6.validateForms(forms);
+        } catch (IllegalArgumentException e) {
+            return Collections.emptyList();
+        }
 
         HashSet<String> answer = new HashSet<>();
 
@@ -36,5 +43,19 @@ public class Problem6 {
         result.sort(String.CASE_INSENSITIVE_ORDER);
 
         return result;
+    }
+}
+class ExceptionProblem6 {
+    public static void validateForms(List<List<String>> forms) {
+        for (List<String> form : forms) {
+            conformEmailFormat(form.get(0));
+        }
+    }
+
+    private static void conformEmailFormat(String email) {
+        String format = "@email.com";
+        if (!email.contains(format)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
