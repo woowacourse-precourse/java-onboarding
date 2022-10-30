9547c32f4a0883e5c3e81da191c2dd734ee4e656
goldcollar95 <yjs7298@naver.com>
2022-10-31 01:40:58 +0900

    3번 출력값 다시수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,42 +1,28 @@
 package onboarding;
 
-import java.util.Scanner;
-
 public class Problem3 {
     public static int solution(int number) {
-
-
-
-        int count = 0;
-
-
-
-        for(int i = 1; i <= number; i++) {
-
-            int current = i;
-            int temp = count;
-            while (current != 0) {
-                if(current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
-
-                    count++;
-
-
-
-                }
-
-                current /= 10;
-
-            }
-
+        int answer = 0;
+        //입력받은 숫자를 통해 총 몇 번의 손벽을 쳐야하는지 확인
+        for (int i = 1; i <= number; i++) {
+            int cnt = checkNum(i);
+            answer += cnt;
         }
-        return count;
-
+        return answer;
     }
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        System.out.println(solution(number));
+    //숫자 하나에서 3,6,9가 몇 번 포함되어 있는지 확인하는 메서드
+    private static int checkNum(int num) {
+        int cnt = 0;
+        while (num > 0) {
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                cnt++;
+                num /= 10;
+            }
+            if (num % 10 != 3 && num % 10 != 6 && num % 10 != 9) {
+                num /= 10;
+            }
+        }
+        return cnt;
     }
 }
