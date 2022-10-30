b304e9b71b2c050c5dea3a77d34ed180bff9adb4
JeongHunHui <deca3248@gmail.com>
2022-10-30 20:57:40 +0900

    [1주차 - Problem6] 문자열 중복 검사 코드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,10 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -27,6 +30,25 @@ public class Problem6 {
             if(data.get(1).length() > 20) {
                 System.out.println("닉네임의 길이가 20자 이상입니다.");
                 return null;
+            }
+        }
+        List<String> emailList = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            // 기준 문자열
+            String str = forms.get(i).get(1);
+            for (int j = 0; j < forms.size(); j++) {
+                // 기준 문자열과 동일한 경우 스킵
+                if(i == j) continue;
+                // 검사 대상 문자열
+                List<String> jStringList = forms.get(j);
+                for (int k = 0; k < str.length()-1; k++) {
+                    // 기준 문자열내에서 2글자씩 빼서 검사
+                    String checkStr = str.substring(k, k+2);
+                    // checkStr가 검사하는 문자열에 들어있으면 해당 닉네임의 이메일을 emailList에 추가
+                    if(jStringList.get(1).contains(checkStr)) {
+                        emailList.add(jStringList.get(0));
+                    }
+                }
             }
         }
         return forms.get(0);
