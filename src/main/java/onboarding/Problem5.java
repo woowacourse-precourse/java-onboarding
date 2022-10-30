1ed1ec5f63f37522e9b742258584ade9d8fbbdad
Woojin Lee <dhkep03@gmail.com>
2022-10-30 13:05:26 +0900

    p5-feat: 화폐 금액 별 개수 세기를 위한 Bank 객체 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,4 +9,22 @@ public class Problem5 {
         List<Integer> answer = Bank.getMoneyCountList(money);
         return answer;
     }
+}
+
+class Bank {
+    private static final int[] moneyArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    static List<Integer> getMoneyCountList(int money) {
+        List<Integer> moneyCountList = new ArrayList<>();
+        int countTimes = moneyArray.length;
+        int count;
+        int changes = money;
+        for (int i = 0; i < countTimes; i++) {
+            count = changes / moneyArray[i];
+            changes = changes % moneyArray[i];
+            moneyCountList.add(count);
+        }
+        return moneyCountList;
+    }
+
 }
