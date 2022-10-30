7a68b411d573d705f7c4128649b4263fdd36ffac
sinsu <sinsu19012@gmail.com>
2022-10-30 21:11:10 +0900

    🎨Style:java 코드 컨벤션 적용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,23 +5,24 @@ import java.util.List;
 import java.util.Stack;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String str=cryptogram;
-        while(true){
-            String str_rm=same_remove(str);
-            if(str_rm.equals(str)){
+        String str = cryptogram;
+        while (true) {
+            String str_rm = same_remove(str);
+            if (str_rm.equals(str)) {
                 break;
             }
-            str=str_rm;
+            str = str_rm;
         }
         return str;
     }
 
-    public static String same_remove(String s){
-        List<Character> chars=new ArrayList<>();
-        for(char ch:s.toCharArray()){
-            int size =chars.size()-1;
-            if(chars.isEmpty()){
+    public static String same_remove(String s) {
+        List<Character> chars = new ArrayList<>();
+        for (char ch : s.toCharArray()) {
+            int size = chars.size() - 1;
+            if (chars.isEmpty()) {
                 chars.add(ch);
                 continue;
             }
@@ -34,9 +35,10 @@ public class Problem2 {
         }
         return convertCharListToString(chars);
     }
-    public static String convertCharListToString(List<Character> characterList){
-        StringBuilder sb=new StringBuilder();
-        for (Character ch:characterList){
+
+    public static String convertCharListToString(List<Character> characterList) {
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : characterList) {
             sb.append(ch);
         }
         return sb.toString();
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,13 +5,14 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
-        for(int i=0;i<forms.size();i++){
-            String str=forms.get(i).get(1);
-            for(int j=0;j<str.length()-1;j++){
-                String s=str.substring(j,j+2);
-                if(find_nickname(forms,s,i) && !answer.contains(forms.get(i).get(0))){
+        for (int i = 0; i < forms.size(); i++) {
+            String str = forms.get(i).get(1);
+            for (int j = 0; j < str.length() - 1; j++) {
+                String s = str.substring(j, j + 2);
+                if (find_nickname(forms, s, i) && !answer.contains(forms.get(i).get(0))) {
                     answer.add(forms.get(i).get(0));
                     break;
                 }
@@ -20,18 +21,18 @@ public class Problem6 {
         answer.sort(Comparator.naturalOrder());
         return answer;
     }
-    public static boolean find_nickname(List<List<String>> form,String s,int index){
-        for(int i=0;i<form.size();i++){
-            if(i==index){
+
+    public static boolean find_nickname(List<List<String>> form, String s, int index) {
+        for (int i = 0; i < form.size(); i++) {
+            if (i == index) {
                 continue;
             }
-            if(form.get(i).get(1).contains(s)){
+            if (form.get(i).get(1).contains(s)) {
                 return true;
             }
         }
         return false;
     }
-
 
 
 }
