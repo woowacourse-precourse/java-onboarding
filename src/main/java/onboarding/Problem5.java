c2123fead2c30ec5317cca8bbe3a7e5680bbee68
ensk26 <tjensk26@gmail.com>
2022-10-31 03:56:17 +0900

    feat: 돈을 지정된 금액의 화폐로 반환되는 개수와 반환하고 남은 액수 반환하는 기능 추가, problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+final class MoneyInfo {
+
+    int cnt;
+    int change;
+
+    public MoneyInfo(int cnt, int change) {
+        this.cnt = cnt;
+        this.change = change;
+    }
+}
+
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
@@ -12,8 +23,11 @@ public class Problem5 {
     }
 
     //돈을 지정된 금액의 화폐로 반환되는 개수와 반환하고 남은 돈의 액수 반환
-    static int[] getCountMoney(int money,int unit) {
+    static MoneyInfo getCountMoney(int money, int unit) {
 
-        return null;
+        int cnt = money / unit;
+        int change = money % unit;
+
+        return new MoneyInfo(cnt, change);
     }
 }
