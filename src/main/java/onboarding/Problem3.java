0b3bb1aad5b46ee4a024aa286f7ba5707cf0964f
jagajang <kosb7544@gmail.com>
2022-10-30 13:03:53 +0900

    p3/add function list

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,10 @@ package onboarding;
 
 import java.util.Set;
 
+/* 기능 목록
+ *
+ *  clapNum : 해당 숫자에서 손뼉을 몇 번 쳐야 하는지 확인
+ */
 public class Problem3 {
     private static Set<Integer> clapSet = Set.of(3, 6, 9);
 
