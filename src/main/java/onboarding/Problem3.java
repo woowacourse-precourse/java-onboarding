1b76295144a842c3a821314ad4776fde6150b72d
euna <christinejung10@daum.net>
2022-10-30 20:38:40 +0900

    feat(Problem3): 기능 목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // int clap = 0;
+        // 1부터 number까지의 모든 숫자에 대하여
+        // -> 각 자리의 숫자가 3, 6, 9 중 하나라면 clap + 1
+
         return answer;
     }
+
 }
