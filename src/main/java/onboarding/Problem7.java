0171840f1b4c02d319cac2759aa8696defaa32b9
dclxxi <dclxxi@kakao.com>
2022-10-30 19:58:18 +0900

    feat(문제7): 사용자 클래스 생성 (아이디, 점수, 친구 목록)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,42 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    public static class User {
+        private String id;
+        private int score;
+        private List<String> friends;
+
+        public User(String id) {
+            this.id = id;
+            this.score = 0;
+            this.friends = new ArrayList<>();
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void addFriend(String friend) {
+            this.friends.add(friend);
+        }
+
+        public List<String> getFriends() {
+            return friends;
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
