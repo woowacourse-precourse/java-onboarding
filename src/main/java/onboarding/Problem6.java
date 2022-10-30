1af4233186810329993f2239ddf8ab6e5032ad3e
sieun <sieun960513@naver.com>
2022-10-30 17:39:59 +0900

    [Problem5] : 익셉션 활용 [수정]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,15 +3,14 @@ package onboarding;
 import java.util.*;
 
 /** 기능 목록
- * [CreList Collection]
  * addCrew              : 크루 추가
+ * checkAlarmTarget     : 알림 보내줄 대상인지 체크
+ * checkOverlap         : 겹치는 부분 있는지 체크
  * getConfusedNickname  : 알림 보낼 이메일 get
  * checkValid           : 예외 처리
  * checkCrewNums        : 크루 크기 유효성 체크
  * checkEmail           : 이메일 유효성 체크
  * checkNickName        : 닉네임 유효성 체크
- * checkAlarmTarget     : 알림 보내줄 대상인지 체크
- * checkOverlap         : 겹치는 부분 있는지 체크
  * checkEmailForm       : 이메일 형식 유효성 체크
  * checkEmailLength     : 이메일 길이 유효성 체크
  * checkEmailDomain     : 이메일 도메인 체크
@@ -21,109 +20,110 @@ import java.util.*;
  */
 
 public class Problem6 {
+
+    private static Map<String, String> continuousMap = Collections.emptyMap();
+    private static Set<String> alarm = Collections.emptySet();
+
     public static List<String> solution(List<List<String>> forms) {
-        CrewList crewList = new CrewList();
+        continuousMap = new HashMap<>();
+        alarm = new HashSet<>();
+        if(!checkCrewNums(forms))
+            throw new IllegalArgumentException("제한사항 위반 [크루 인원 수]");
         for (List<String> form : forms) {
-            if(!crewList.addCrew(form)) return Collections.emptyList();
+            addCrew(form);
         }
-        return crewList.getConfusedNickname();
+        return getConfusedNickname();
     }
 
-    private static class CrewList{
-        private final Map<String, String> crewList = new HashMap<>();
-        private final Map<String,String> continuousSet = new HashMap<>();
-        private final Set<String> alarm = new HashSet<>();
+    private static void addCrew(List<String> form) {
+        String email = form.get(0);
+        String nickname = form.get(1);
+        if (!checkValid(email, nickname)) {
+            throw new IllegalArgumentException("제한사항 위반");
+        }
+        checkAlarmTarget(nickname, email);
 
-        private boolean addCrew(List<String> form) {
-            String email = form.get(0);
-            String nickname = form.get(1);
-            if (checkValid(email, nickname)) {
-                crewList.put(email, nickname);
-            } else {
-                System.out.println("올바르지 않은 형식");
-                return false;
+    }
+
+    private static void checkAlarmTarget(String nickname, String email) {
+        Set<String> userContinuousSet = new HashSet<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            StringBuilder sb = new StringBuilder();
+            String continuousPart = (sb.append(nickname.charAt(i)).append(nickname.charAt(i + 1))).toString();
+            userContinuousSet.add(continuousPart);
+        }
+        checkOverlap(userContinuousSet, email);
+    }
+
+    private static void checkOverlap(Set<String> userContinuousSet, String email) {
+        for (String target : userContinuousSet) {
+            if(continuousMap.containsKey(target)) {
+                alarm.add(continuousMap.get(target));
+                alarm.add(email);
             }
-            checkAlarmTarget(nickname, email);
-            return checkCrewNums();
-        }
-
-        private List<String> getConfusedNickname() {
-            List<String> result = new ArrayList<>(alarm);
-            Collections.sort(result);
-            return result;
-        }
-
-        private boolean checkValid(String email, String nickname) {
-            return checkEmail(email) && checkNickName(nickname);
-        }
-
-        private boolean checkCrewNums() {
-            int crewSize = crewList.size();
-            return 1 <= crewSize && crewSize <= 10000;
-        }
-
-        private boolean checkEmail(String email) {
-            int emailLength = email.length();
-            if (!checkEmailLength(emailLength)) {
-                return false;
-            }
-            return checkEmailForm(email);
-        }
-
-        private boolean checkNickName(String nickname) {
-            return checkNickNameLength(nickname) && checkNickNameType(nickname);
-        }
-
-        private void checkAlarmTarget(String nickname, String email) {
-            Set<String> userContinuousSet = new HashSet<>();
-
-            for (int i = 0; i < nickname.length() - 1; i++) {
-                StringBuilder sb = new StringBuilder();
-                String continuousPart = (sb.append(nickname.charAt(i)).append(nickname.charAt(i + 1))).toString();
-                userContinuousSet.add(continuousPart);
-            }
-            checkOverlap(userContinuousSet, email);
-        }
-
-        private void checkOverlap(Set<String> userContinuousSet, String email) {
-            for (String target : userContinuousSet) {
-                if(continuousSet.containsKey(target)) {
-                    alarm.add(continuousSet.get(target));
-                    alarm.add(email);
-                }
-                else{
-                    continuousSet.put(target, email);
-                }
+            else{
+                continuousMap.put(target, email);
             }
         }
+    }
 
-        private boolean checkEmailForm(String email) {
-            String[] emailDetails = email.split("@");
-            String emailId = emailDetails[0];
-            String emailDomain = emailDetails[1];
-            return checkEmailDomain(emailDomain) && checkEmailId(emailId);
-        }
+    private static List<String> getConfusedNickname() {
+        List<String> result = new ArrayList<>(alarm);
+        Collections.sort(result);
+        return result;
+    }
 
-        private boolean checkEmailLength(int n) {
-            return 11 <= n && n <= 20;
-        }
+    private static boolean checkValid(String email, String nickname) {
+        return checkEmail(email) && checkNickName(nickname);
+    }
 
-        private boolean checkEmailDomain(String emailDomain) {
-            return emailDomain.length() != 0 && emailDomain.equals("email.com");
-        }
+    private static boolean checkCrewNums(List<List<String>> arr) {
+        int crewSize = arr.size();
+        return 1 <= crewSize && crewSize <= 10000;
+    }
 
-        private boolean checkEmailId(String emailId) {
-            return emailId.length() != 0;
+    private static boolean checkEmail(String email) {
+        int emailLength = email.length();
+        if (!checkEmailLength(emailLength)) {
+            throw new IllegalArgumentException("제한사항 위반 [이메일 길이]");
         }
+        return checkEmailForm(email);
+    }
 
-        private boolean checkNickNameType(String nickname) {
-            return nickname.matches("^[가-힣]*$");
-        }
+    private static boolean checkNickName(String nickname) {
+        return checkNickNameLength(nickname) && checkNickNameType(nickname);
+    }
 
-        private boolean checkNickNameLength(String nickname) {
-            int len = nickname.length();
-            return 1<= len && len < 20;
+    private static boolean checkEmailForm(String email) {
+        String[] emailDetails;
+        try {
+            emailDetails = email.split("@");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("제한사항 위반 [이메일]");
         }
+        String emailId = emailDetails[0];
+        String emailDomain = emailDetails[1];
+        return checkEmailDomain(emailDomain) && checkEmailId(emailId);
+    }
+
+    private static boolean checkEmailLength(int n) {
+        return 11 <= n && n < 20;
+    }
+
+    private static boolean checkEmailDomain(String emailDomain) {
+        return emailDomain.equals("email.com");
+    }
+
+    private static boolean checkEmailId(String emailId) {
+        return emailId.length() != 0;
+    }
+
+    private static boolean checkNickNameType(String nickname) {
+        return nickname.matches("^[가-힣]*$");
+    }
+
+    private static boolean checkNickNameLength(String nickname) {
+        int len = nickname.length();
+        return 1<= len && len < 20;
     }
 }
-
