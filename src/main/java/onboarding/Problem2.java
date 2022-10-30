606e22a09a75e316aa87b069f73463c9fb9b0d6c
kokomong2 <ko9603@naver.com>
2022-10-30 18:59:15 +0900

    feat(문제2) 추가 : 중복문자 제거 함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
 
+import java.util.Objects;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+
+        return cryptogram;
+    }
+
+
+    public static String deleteDuplicates(String str){
+
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        char prev = str.charAt(0);
+        st.push(str.charAt(0));
+
+        for (int i = 1; i < str.length(); i++) {
+
+            if(prev == str.charAt(i)){
+                if(!st.empty() && st.peek()==str.charAt(i))st.pop();
+            }else{
+                st.push(str.charAt(i));
+                prev = str.charAt(i);
+            }
+        }
+        while(!st.empty()){
+            sb.append(st.peek());
+            st.pop();
+        }
+        sb.reverse();
+
+        System.out.println(sb);
+        return String.valueOf(sb);
     }
 }
