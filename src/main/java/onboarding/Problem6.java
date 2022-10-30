83ca84e7e7c19fe24b5d631fbd0b9449b0f60c69
iamsseaa <homingsea@gmail.com>
2022-10-30 22:40:15 +0900

    feat(Problem6): 닉네임 중 같은 글자가 연속적으로 포함되는 닉네임을 골라내는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,38 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        //HashSet<String> answer = new HashSet<>();
+
+        for (List<String> form : forms) {
+
+            String testEmail = form.get(0);
+            String testName = form.get(1);
+
+            for (int i = 0; i < testName.length() - 1 ; i++) {
+                String test = testName.substring(i, i+2);
+
+                for (List<String> formTest : forms) {
+
+                    if (formTest.get(1).contains(test)) {
+
+                        if (!formTest.get(0).equals(testEmail)) {
+                            answer.add(formTest.get(0));
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+        
+
         return answer;
     }
 }
