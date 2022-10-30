71f62c8b1c8689879ab832ee75071ccc0f21c3cf
Invidam <hansupark0208@gmail.com>
2022-10-30 23:55:42 +0900

    refactor(Problem3): make Number and NumberInClapGame to collection
    collect method and variable to Number and NumberInClapGame.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,58 +6,83 @@ import java.util.List;
 
 public class Problem3 {
 
-    static final int START_RANGE = 1;
-    static final int END_RANGE = 10_000;
 
-    private static boolean isValidNumber(int num) {
-        return START_RANGE <= num && num <= END_RANGE;
-    }
-    public static boolean isClapCase(int oneDigitNum) {
-        int[] clapCases = {3, 6, 9};
+    static class Number {
 
-        for (int clapCase : clapCases) {
-            if (oneDigitNum == clapCase) {
-                return true;
+        final static int START_RANGE = 1;
+        final static int END_RANGE = 10_000;
+        protected int value;
+
+        public Number(int value) {
+            if (!isValidNumber(value)) {
+                throw new InputMismatchException("입력 범위가 [1, 10_000] 안에 있지 않습니다.");
             }
+            this.value = value;
         }
 
-        return false;
-    }
-
-    private static List<Integer> getDigits(int num) {
-        List<Integer> digits = new ArrayList<>();
-
-        while (num > 0) {
-            digits.add(num % 10);
-            num /= 10;
+        private static boolean isValidNumber(int num) {
+            return START_RANGE <= num && num <= END_RANGE;
         }
 
-        return digits;
-    }
+        List<Integer> getDigits(int num) {
+            List<Integer> digits = new ArrayList<>();
 
-    public static int getClapCount(int num) {
-        int ret = 0;
-
-        List<Integer> digits = getDigits(num);
-
-        for (int digit : digits) {
-            if (isClapCase(digit)) {
-                ++ret;
+            while (num > 0) {
+                digits.add(num % 10);
+                num /= 10;
             }
+
+            return digits;
+        }
+    }
+
+    static class NumberInClapGame extends Number {
+
+        static final int[] CLAP_CASES = {3, 6, 9};
+
+        public NumberInClapGame(int value) {
+            super(value);
         }
 
-        return ret;
+        static public boolean isClapCase(int oneDigitNum) {
+
+            for (int clapCase : CLAP_CASES) {
+                if (oneDigitNum == clapCase) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int clapCount() {
+            int ret = 0;
+
+            List<Integer> digits = getDigits(value);
+
+            for (int digit : digits) {
+                if (isClapCase(digit)) {
+                    ++ret;
+                }
+            }
+
+            return ret;
+        }
     }
+
+    static public NumberInClapGame makeNumberInClapGame(int num) {
+        return new NumberInClapGame(num);
+    }
+
 
     public static int getClapCountSum(int maxNum) {
-        if (!isValidNumber(maxNum)) {
-            throw new InputMismatchException("입력 범위가 [1, 10_000] 안에 있지 않습니다.");
-        }
+        if(!Number.isValidNumber(maxNum))
+            throw new InputMismatchException("입력한 수의 범위가 유효하지 않습니다.");
 
         int ret = 0;
 
-        for (int clapNum = 1; clapNum <= maxNum; ++clapNum) {
-            ret += getClapCount(clapNum);
+        //-1인 경우 조건문이 어떻게 되지?
+        for (int clapNum = maxNum; clapNum > 0; --clapNum) {
+            ret += makeNumberInClapGame(clapNum).clapCount();
         }
 
         return ret;
