65da6df3e426483d9f5d62b1559362dc3e0605fa
hhiyeon <hhyyeon0214@gmail.com>
2022-10-30 18:55:02 +0900

    feat: p4-create reverse dictionary

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     private static final int SUM_KEY_VALUE = 155;
 
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static Map<Character, Character> createReverseDict() {
+        Map<Character, Character> reverseDict = new HashMap<>();
+        char base = 'A';
+        while (Character.isAlphabetic(base)) {
+            reverseDict.put(base, getReverseValue(base++));
+        }
+        return reverseDict;
     }
 
     private static char getReverseValue(char base) {
