02206b83f4e4a1fb54eea7875b526aeba8058a09
Jung Ji Won <abiding77@naver.com>
2022-10-30 15:00:25 +0900

    배열리스트를 활용한 문제 해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        if(vaildateCheck(money)){
+            answer = findMoneyCnt(money);
+        }
+        return answer;
+    }
+    private static boolean vaildateCheck(int money){
+        if(money < 1 || money > 1000000){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    private static List<Integer> findMoneyCnt(int money){
+        List<Integer> cntMoney = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
+        List<Integer> answer = new ArrayList<>();
+        for(int cmp : cntMoney){
+            answer.add(money/cmp);
+            money %= cmp;
+        }
         return answer;
     }
 }
