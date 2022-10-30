64f32f24c7915ebdb046350e2f237ed6a41c2fea
jaehee329 <jaehee329@yonsei.ac.kr>
2022-10-31 01:31:51 +0900

    refactor(Problem2): 메서드명 수정 및 decrypto메서드 solution에 편입

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,36 +1,30 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "";
-        answer = decrypto(cryptogram);
-        return answer;
-    }
 
-    public static String decrypto(String string) {
-        String copy = string;
-        for (int i=0; i<string.length()-1; i++) {
-            if (isCharsDuplicatedInGivenIdx(string, i)) {
-                string = getTwoCharsDeletedInGivenIdx(string, i);
+    public static String solution(String cryptogram) {
+        String copy = cryptogram;
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (isCharsDuplicatedInGivenIdx(cryptogram, i)) {
+                cryptogram = deleteTwoCharsFromIdx(cryptogram, i);
             }
         }
-        if (string.equals(copy)) {
-            return string;
+        if (cryptogram.equals(copy)) {
+            return cryptogram;
         }
-        string = decrypto(string);
-        return string;
+        return solution(cryptogram);
     }
 
     public static boolean isCharsDuplicatedInGivenIdx(String string, int idx) {
-        if (string.charAt(idx) == string.charAt(idx+1)) {
+        if (string.charAt(idx) == string.charAt(idx + 1)) {
             return true;
         }
         return false;
     }
 
-    public static String getTwoCharsDeletedInGivenIdx(String string, int idx) {
+    public static String deleteTwoCharsFromIdx(String string, int idx) {
         String prefix = string.substring(0, idx);
-        String suffix = string.substring(idx+2, string.length());
+        String suffix = string.substring(idx + 2, string.length());
         return prefix + suffix;
     }
 }
--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -4,9 +4,7 @@ import static onboarding.Problem2.*;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class Problem2Test {
 
@@ -16,33 +14,25 @@ class Problem2Test {
 		@Test
 		@DisplayName("browoanoommnaon을 brown으로 줄여야 한다")
 		void integrationTest1() {
-			String string = "browoanoommnaon";
-
-			assertThat(solution(string)).isEqualTo("brown");
+			assertThat(solution("browoanoommnaon")).isEqualTo("brown");
 		}
 
 		@Test
 		@DisplayName("zyelleyz을 빈 문자열로 줄여야 한다")
 		void integrationTest2() {
-			String string = "zyelleyz";
-
-			assertThat(solution(string)).isEqualTo("");
+			assertThat(solution("zyelleyz")).isEqualTo("");
 		}
 
 		@Test
 		@DisplayName("aabbccd를 d로 줄여야 한다")
 		void integrationTest3() {
-			String string = "aabbccd";
-
-			assertThat(solution(string)).isEqualTo("d");
+			assertThat(solution("aabbccd")).isEqualTo("d");
 		}
 
 		@Test
 		@DisplayName("bgfaafgecddce를 b로 줄여야 한다")
 		void integrationTest4() {
-			String string = "bgfaafgecddce";
-
-			assertThat(solution(string)).isEqualTo("b");
+			assertThat(solution("bgfaafgecddce")).isEqualTo("b");
 		}
 	}
 
@@ -50,7 +40,6 @@ class Problem2Test {
 	@DisplayName("주어진 인덱스와 다음 인덱스의 문자가 중복일 때만 참을 반환해야 한다")
 	void isCharsDuplicatedInGivenIdxTest() {
 		String string = "12344";
-
 		assertFalse(isCharsDuplicatedInGivenIdx(string, 0));
 		assertFalse(isCharsDuplicatedInGivenIdx(string, 1));
 		assertFalse(isCharsDuplicatedInGivenIdx(string, 2));
@@ -59,14 +48,13 @@ class Problem2Test {
 
 	@Test
 	@DisplayName("문자열에서 주어진 인덱스와 그 다음의 두 문자를 삭제해야 한다")
-	void getTwoCharsDeletedInGivenIdxTest1() {
+	void deleteTwoCharsFromIdxTest() {
 		String string = "abcdefg";
-
-		assertThat(getTwoCharsDeletedInGivenIdx(string, 0)).isEqualTo("cdefg");
-		assertThat(getTwoCharsDeletedInGivenIdx(string, 1)).isEqualTo("adefg");
-		assertThat(getTwoCharsDeletedInGivenIdx(string, 2)).isEqualTo("abefg");
-		assertThat(getTwoCharsDeletedInGivenIdx(string, 3)).isEqualTo("abcfg");
-		assertThat(getTwoCharsDeletedInGivenIdx(string, 4)).isEqualTo("abcdg");
-		assertThat(getTwoCharsDeletedInGivenIdx(string, 5)).isEqualTo("abcde");
+		assertThat(deleteTwoCharsFromIdx(string, 0)).isEqualTo("cdefg");
+		assertThat(deleteTwoCharsFromIdx(string, 1)).isEqualTo("adefg");
+		assertThat(deleteTwoCharsFromIdx(string, 2)).isEqualTo("abefg");
+		assertThat(deleteTwoCharsFromIdx(string, 3)).isEqualTo("abcfg");
+		assertThat(deleteTwoCharsFromIdx(string, 4)).isEqualTo("abcdg");
+		assertThat(deleteTwoCharsFromIdx(string, 5)).isEqualTo("abcde");
 	}
 }
