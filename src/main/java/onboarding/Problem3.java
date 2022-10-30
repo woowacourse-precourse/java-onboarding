d2579ce0964f47e620866d104f9cf4e964b5731c
dong young <dong5854@gmail.com>
2022-10-31 02:24:26 +0900

    [Problem3] feat: add clap function
    박수치는 횟수를 반환하는 clap 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int clap(int number){
+        int result = 0;
+        while(number > 0){
+            int digit = number % 10;
+
+            if (digit == 3 || digit == 6 || digit == 9){
+                result += 1;
+            }
+
+            number /= 10;
+        }
+        return result;
+    }
 }
