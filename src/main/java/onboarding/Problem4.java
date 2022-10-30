d0ec0336e566712d439961a7369a07b06bd91d0e
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 16:50:57 +0900

    feat(Problem4): 문자와 반대되는 문자 매핑하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem4 {
     public static String solution(String word) {
+
+        Map<String, String> map = new HashMap<String, String>();
+        mappingReversingChar(map);
         String answer = "";
         return answer;
+    }
+
+    /**
+     * Function for Mapping a Reversing Char
+     * */
+    private static void mappingReversingChar(Map<String, String> map) {
+        List<String> origin =
+                new ArrayList<>(Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+                        "S","T","U","V","W","X","Y","Z"));
+
+        for (int i = 0; i < origin.size(); i++) {
+            map.put(origin.get(i), origin.get(origin.size()-1-i));
+            map.put(origin.get(i).toLowerCase(), origin.get(origin.size()-1-i).toLowerCase());
+        }
     }
 }
