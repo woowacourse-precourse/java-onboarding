2c46cd551f78f455b75e8a26debd258d70b27cfc
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:03:52 +0900

    feat : 중복문자 존재하는지 체크함수

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,4 +10,16 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+
+    //중복문자 존재하는지 구별하는 함수
+    //중복문자가 존재할시 true를 반환해준다.
+    public static boolean isDuplicate(String cryptogram){
+        for(int i = 1 ; i < cryptogram.length() ; i++){
+            if(cryptogram.charAt(i) == cryptogram.charAt(i-1)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
