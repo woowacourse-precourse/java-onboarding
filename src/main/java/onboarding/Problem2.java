f2270fbcebe72ed3162d037bf908247bbee2d6ef
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-30 19:48:15 +0900

    feat: Add Problem2 Deduplicate
    - 중복 제거 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,13 +4,17 @@ import java.util.regex.Pattern;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        if(isException(cryptogram)) throw new RuntimeException("잘못된 인자입니다.");
+        if (isException(cryptogram)) throw new RuntimeException("잘못된 인자입니다.");
         String answer = "answer";
         return answer;
     }
 
     private static Boolean isException(String str) {
-        if(str.length() < 1 || str.length() > 1000) return true;
+        if (str.length() < 1 || str.length() > 1000) return true;
         return !Pattern.matches("[a-z]*", str);
+    }
+
+    private static String deduplicate(String str) {
+        return str.replaceAll("(\\w)\\1+", "");
     }
 }
