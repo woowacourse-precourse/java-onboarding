6cb7ecbb31d1aa2733b1a8429dbff59caa67e947
keon0711 <fnc6488@gmail.com>
2022-10-30 23:26:50 +0900

    [Problem7] 하나의 친구 관계를 받아 친구 목록(map)에 친구를 추가하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        return Collections.emptyList();
+    }
+
+    private static void addFriend(List<String> friend, Map<String, List<String>> friendsList) {
+        String from = friend.get(0);
+        String to = friend.get(1);
+
+        if (friendsList.containsKey(from))
+            friendsList.get(from).add(to);
+        else
+            friendsList.put(from, new ArrayList<>(List.of(to)));
+
+        if (friendsList.containsKey(to))
+            friendsList.get(to).add(from);
+        else
+            friendsList.put(to, new ArrayList<>(List.of(from)));
     }
 }
