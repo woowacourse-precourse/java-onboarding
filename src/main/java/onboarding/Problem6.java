d9b6c111f5f93d306569cd1e75beefd9caf608e6
ghltjd369 <ghltjd369@gmail.com>
2022-10-30 14:57:32 +0900

    feat : (problem 6) 입력받은 리스트에서 순차적으로 두 크루를 뽑아 이름과 메일을 분리하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,20 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for(int i = 0; i < forms.size() - 1; i++){
+            for(int j = i + 1; j < forms.size(); j++){
+                List<String> crew1 = forms.get(i);
+                List<String> crew2 = forms.get(j);
+
+                String crew1_mail = crew1.get(0);
+                String crew1_name = crew1.get(1);
+
+                String crew2_mail = crew2.get(0);
+                String crew2_name = crew2.get(1);
+            }
+        }
+
         return answer;
     }
 }
