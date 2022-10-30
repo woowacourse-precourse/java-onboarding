27eb3ba69cc2e7d1683d6d15efceeb818d67227c
alstjr706 <alstjr706@naver.com>
2022-10-31 05:05:32 +0900

    feat : (중복 닉네임 제거) 특정 문자들의 중복을 횟수까지 알수 있는 기능 구현

--- src/main/java/onboarding/problem6/TwoWordVerification.java
+++ src/main/java/onboarding/problem6/TwoWordVerification.java
@@ -1,13 +1,30 @@
 package onboarding.problem6;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class TwoWordVerification {
 
-    public Map<String, Integer> CountOfCase(Set<String> caas, List<CrewMember> crewMemberList) {
-        return null;
+    public Map<String, Integer> CountOfCase(Set<String> cases, List<CrewMember> crewMemberList) {
+
+        Map<String, Integer> resultMap = new HashMap<>();
+
+        for (String word : cases) {
+
+            int count = 0;
+            for (CrewMember crewMember : crewMemberList) {
+                if (crewMember.getNickname().contains(word)) {
+                    count++;
+                }
+            }
+
+            resultMap.put(word, count);
+        }
+
+
+        return resultMap;
     }
 
     public List<String> DuplicatedCase(Map<String, Integer> CountCase) {
