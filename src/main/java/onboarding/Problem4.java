cf21dc963b073447e3918cd2dfe3ddc38a911c7f
java-saeng <gusdn85554@naver.com>
2022-10-30 14:54:17 +0900

    feat : Problem4 공백 구분하는 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char digit = word.charAt(i);
+
+            if (isBlank(digit)) {
+                stringBuilder.append(" ");
+                continue;
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static boolean isBlank(char digit) {
+        return digit == ' ';
     }
 }
