b4085905011c8cc3404d8e93b681499588b4ac33
thousandcows <milcamboo@gmail.com>
2022-10-30 21:04:17 +0900

    feat: created validation method to handle exception cases

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,21 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 1. Validation check
+        boolean pobiCheck = validationCheck(pobi);
+        boolean crongCheck = validationCheck(pobi);
+
+        if (!pobiCheck || !crongCheck) {
+            return -1;
+        }
+
         return answer;
     }
+
+    // Validation method: check if the pages are valid
+    private static boolean validationCheck(List<Integer> pages) {
+        return pages.get(0) + 1 == pages.get(1);
+    }
+
 }
