6634e411628dc0f1194741886a52491d14382951
es <mascoeskim@gmail.com>
2022-10-31 08:42:49 +0900

    Problem3/1.초기세팅

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,19 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem3 {
     public static int solution(int number) {
+        System.out.println(number);
+
+        String numberL = "";
+        for (int k=1;k<=number;k++){
+            numberL += Integer.toString(k);
+        }
+
+
+        System.out.println(numberL);
+
         int answer = 0;
         return answer;
     }
