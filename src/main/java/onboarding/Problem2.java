e11f4189ceb62dc6d0e18c7664b934875074f281
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-30 20:07:42 +0900

    Problem2 : 중복되는 문자 제거 (1회전만)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,29 +4,28 @@ import java.util.Stack;
 
 /*
 기능목록
-1. 2개씩 중복되는 경우 제거하는 기능
-2. 중복이 3개이상일 경우 제거하는 기능
+1. 중복되는 문자 제거 (1회전만)
+2. 중복이 없어질때까지 중복 문자 제거
  */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
         Stack<Character> stack=new Stack<>();
-        char duplication='\u0000'; //중복된 문자
-        for(int i=0; i<cryptogram.length(); i++){
+
+        char duplication=cryptogram.charAt(0); //중복된 문자
+        stack.push(cryptogram.charAt(0));
+        for(int i=1; i<cryptogram.length(); i++){
             char c=cryptogram.charAt(i);
-            if(stack.isEmpty()){
-                if(duplication==c) continue;
+            if(duplication!=c){
                 stack.push(c);
+                duplication=c;
             }else{
-                if(stack.peek()==c || duplication==c){
+                if(!stack.isEmpty()){
                     stack.pop();
-                    duplication=c;
-                }else{
-                    stack.push(c);
                 }
             }
         }
-        for (Character c : stack){
+        for(char c: stack){
             answer+=c;
         }
         return answer;
