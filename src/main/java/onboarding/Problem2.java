a5698a3d25e6bca5c1383f8acbccbf2aeafdc052
KongDuHwan <kong_du@naver.com>
2022-10-30 23:39:49 +0900

    Problem2 Complete

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.io.IOException;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int startNum = 0, endNum = 0;
+
+        for (int i = 0; i < cryptogram.length()-1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                startNum = i;
+
+                try {
+                    while (cryptogram.charAt(startNum) == cryptogram.charAt(++i)) ;
+                }catch (IndexOutOfBoundsException e){
+
+                }
+                endNum = i;
+
+                StringBuffer str=new StringBuffer(cryptogram);
+                cryptogram=str.delete(startNum,endNum).toString();
+                i=-1;
+            }
+        }
+        return cryptogram;
     }
 }
