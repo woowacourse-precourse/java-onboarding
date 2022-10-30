43a0da1460f12e1eb6bc4f0faab294f49cbe65ec
권태현 <kygkth2011@gmail.com>
2022-10-30 23:21:43 +0900

    Solve Problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i = 0; i < word.length() ; i++){
+            int cur = word.charAt(i);
+            int converted;
+            if(cur == ' ') converted = cur;
+            else if(cur >= 'A' && cur <= 'Z') converted = 'A' + (26 - (cur - 'A' + 1));
+            else converted = 'a' + (26 - (cur - 'a' + 1));
+            answer += (char)converted;
+        }
         return answer;
     }
 }
+
+
+// 90 -> 65
+// 89 -> 66
