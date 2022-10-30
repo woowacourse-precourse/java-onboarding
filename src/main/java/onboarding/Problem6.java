a1180b9318ea45c3ca4d898e1b919d854d3bd602
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-10-30 17:34:24 +0900

    문제 6번 <3번 기능 구현 완료>

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,18 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-    public static void main(String[] args) {
-        List<List<String>> forms = List.of(
-                List.of("jm@email.com", "제이엠"),
-                List.of("jason@email.com", "제이슨"),
-                List.of("woniee@email.com", "워니"),
-                List.of("mj@email.com", "엠제이"),
-                List.of("nowm@email.com", "이제엠")
-        );
-        System.out.println("닉네임 목록 : " + solution(forms));
-    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>(forms.size());
+        List<String> result = new ArrayList<>();
 
         //입력받은 이메일과 닉네임 중, 닉네임만 추출
         for (int i = 0; i < forms.size(); i++) {
@@ -31,20 +22,26 @@ public class Problem6 {
             //중복되는 닉네임을 찾을 수 있도록 패턴을 만들고
             for (int j = 1; j < nickname.length; j++) {
                 pattern = nickname[j-1] + nickname[j];
-                System.out.println("패턴은 다음과 같습니다. : " + pattern);
             }
 
             //위 패턴을 통해 겹치는 닉네임을 추출한다
-            List<String> nicknames = new ArrayList<>();
+            List<String> nicknames;
             nicknames = overlapNicknameExtraction(answer, pattern);
             if (nicknames.size() >= 2) {
-                overlapNickname.add(nicknames.toString());
+                overlapNickname = nicknames;
             }
-            System.out.println("겹치는 닉네임들은 다음과 같습니다. : " + overlapNickname);
         }
-        return answer;
-    }
 
+        //추출한 겹치는 닉네임과 입력받은 매개변수의 닉네임을 비교하여, 해당 닉네임의 이메일을 추출
+        for (String str : overlapNickname) {
+            for (int q = 0; q < forms.size(); q++) {
+                if (str.equals(forms.get(q).get(1))) {
+                    result.add(forms.get(q).get(0));
+                }
+            }
+        }
+        return result;
+    }
 
     private static List<String> overlapNicknameExtraction(List<String> nicknames, String pattern) {
         List<String> overlapNickname = new ArrayList<>(nicknames.size());
