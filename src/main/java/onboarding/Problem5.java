2e45066e94e8f8f2763c29c37cb08c9cf54440b5
squirMM <jsm6616@naver.com>
2022-10-30 22:20:26 +0900

    :rocket: feat(Problem5) refactoring logic

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,24 +1,19 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        // 돈 종류 저장 배열
         int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        // 데이터 삽입에 용이한 list 사용
-        List<Integer> ans = new ArrayList<>();
-        for (int mt : moneyType){
-            // money==0 이라면 0만 넣어줌
-            if(money==0) ans.add(0);
-            else {
-                ans.add(Math.floorDiv(money, mt));
-                money = Math.floorMod(money, mt);
-            }
+        int[] pocket = new int[moneyType.length];
+        Arrays.fill(pocket,0);
+
+        for (int idx=0; idx<moneyType.length; idx++){
+            pocket[idx]=(Math.floorDiv(money, moneyType[idx]));
+            money = Math.floorMod(money, moneyType[idx]);
+            if(money==0)break;
         }
-        return ans;
+        return Arrays.asList(Arrays.stream(pocket).boxed().toArray(Integer[]::new));
     }
 }
