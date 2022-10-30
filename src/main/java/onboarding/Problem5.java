e331cd1538a5f5cd8a9e9759399553f4c0b625cf
seonho <ysh10216@gmail.com>
2022-10-30 19:22:08 +0900

    create isException function

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,9 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    // 1. 예외처리 : money가 1~1,000,000 사이인지 체크
+    public static boolean isException(Integer money){
+        if(money >= 1 && money <= 1000000){
+            return true;
+        }
+        return false;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        // 1. 예외처리 : money가 1~1,000,000 사이인지 체크
         // 2. 큰 금액부터 순서대로 값을 구하는 함수 만들기.
         return answer;
     }
