82ebf86d2f9c2e357502340059a08c58a6c026b8
kimtaehoonDev <taehoon_dev@naver.com>
2022-10-30 21:52:09 +0900

    refactor(problem7): addBulk 내부 로직을 add로 분리

--- src/main/java/onboarding/problem7/UserRelation.java
+++ src/main/java/onboarding/problem7/UserRelation.java
@@ -11,25 +11,25 @@ public class UserRelation {
             throw new Exception("1개 이상 10_000개 이하의 리스트로만 초기화할 수 있습니다");
         }
         for (List<String> relation : relations) {
-            //relations를 검증한다
-            if (relation.size() != 2) {
-                throw new Exception("관계는 두명의 사용자만 맺을 수 있습니다");
-            }
-            UserStore.add(new User(relation.get(0)));
-            UserStore.add(new User(relation.get(1)));
-
-            // 사용자0이 관계에 없었으면 새로운 Set을 만들어주고, 값을 넣는다
-            // 있었으면 값을 넣는다
-            if (!userRelation.containsKey(relation.get(0))) {
-                userRelation.put(relation.get(0), new HashSet<>());
-            }
-            userRelation.get(relation.get(0)).add(relation.get(1));
-
-            if (!userRelation.containsKey(relation.get(1))) {
-                userRelation.put(relation.get(1), new HashSet<>());
-            }
-            userRelation.get(relation.get(1)).add(relation.get(0));
+            add(relation);
         }
+//        relations.stream().forEach(relation -> add(relation));
+// TODO exception이 있으면 상대적으로 함수형으로 짜는게 지저분해진다. 예외처리가 필요해서. 다른 방법이 있을까?
+    }
+
+    public static void add(List<String> relation) throws Exception {
+        if (relation.size() != 2) {
+            throw new Exception("관계는 두명의 사용자만 맺을 수 있습니다");
+        }
+        if (!userRelation.containsKey(relation.get(0))) {
+            userRelation.put(relation.get(0), new HashSet<>());
+        }
+        userRelation.get(relation.get(0)).add(relation.get(1));
+
+        if (!userRelation.containsKey(relation.get(1))) {
+            userRelation.put(relation.get(1), new HashSet<>());
+        }
+        userRelation.get(relation.get(1)).add(relation.get(0));
 
     }
 
