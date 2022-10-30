21f16d7347cc502846207323e880299cb8444d19
yun8565 <yunyun8565@naver.com>
2022-10-31 02:24:11 +0900

    feat(P4): 문자 반대로 변환 기능 추가
    청개구리 사전에 따른 변환 전과 변환 후의 문자의 아스키코드의 합이 대문자, 소문자 각각 155, 219로 동일하기 때문에 이를 활용했다.
    155 = 65(A) + 90(Z), 219 = 97(a) + 122(z)
    
    소문자, 대문자 구분은 Character클래스에서 제공하는 isLowerCase(), isUpperCase 메소드를 사용했다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem4 {
+    public static final int UPPER_SUM = 155;
+    public static final int LOWER_SUM = 219;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static char toFrogChar(char c) {
+        if(Character.isUpperCase(c))
+            return (char)(UPPER_SUM-c);
+        if(Character.isLowerCase(c))
+            return (char)(LOWER_SUM-c);
+        return c;
     }
 }
