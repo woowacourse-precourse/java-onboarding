945932b56b5df78832cdeb8c49ec2bb7dea6f6e5
myeongju00 <j51677767@gmail.com>
2022-10-30 15:50:30 +0900

    feat: 스택을 활용한 중복문자 제거 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,58 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = stringDeduplication(cryptogram);
         return answer;
+    }
+
+    private static String stringDeduplication(String cryptogram) {
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        char[] alphabets = getAlphabets(cryptogram);
+        char pointer = alphabets[0];
+        stack.push(pointer);
+        boolean removeFlag = false;
+        for (int i = 1; i < alphabets.length; i++) {
+            char curAlpha = alphabets[i];
+            if (stack.isEmpty()) {
+                stack.push(curAlpha);
+            } else {
+                if (pointer == curAlpha) {
+                    if (!removeFlag) {
+                        removeFlag = true;
+                    }
+                }
+                else {
+                    if (removeFlag) {
+                        stack.pop();
+                        pointer = stack.peek();
+                        removeFlag = false;
+                        i--;
+                    }
+                    else {
+                        stack.push(curAlpha);
+                        pointer = stack.peek();
+                        removeFlag = false;
+                    }
+                }
+            }
+        }
+        if (!stack.isEmpty() && removeFlag) {
+            stack.pop();
+        }
+
+        while (!stack.isEmpty()) {
+            answer.append(stack.pop());
+        }
+
+        return answer.reverse().toString();
+    }
+
+    private static char[] getAlphabets(String cryptogram) {
+        return cryptogram.toCharArray();
     }
 }
