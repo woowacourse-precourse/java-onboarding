76b33e488a29b0e34b680ba0b742c3524e2d51d9
CHAYEONIL <jeay123@naver.com>
2022-10-31 01:26:02 +0900

    문제6 예외처리 닉네임 길이가 2보다 짧으면 break 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        HashSet<String> nickname = new HashSet<>();
+        HashMap<String, String> email_nick = new HashMap<>();
+
+        // 닉네임 길이가 2보다 짧으면 break
+        for (int i = 0; i < forms.size(); i++) {
+            String name = forms.get(i).get(1);
+            if (name.length() < 2) {
+                break;
+            }
+
+        }
         return answer;
     }
 }
