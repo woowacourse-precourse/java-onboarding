17bf92b51922a7cd66159d068d415ec5eb6a4cbc
Sieun Park <psieun0606@gmail.com>
2022-10-30 14:56:33 +0900

    feat: problem3 문자 배열로 변환하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+
+    static char[] convertToCharArray(int number) {
+        String numString = Integer.toString(number);
+        char[] numCharArray = numString.toCharArray();
+
+        return numCharArray;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        convertToCharArray(number);
+
         return answer;
     }
 }
