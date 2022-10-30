d2f3f84f8053585fa3484c8ca4a91ad4b4f12b98
anneu <b.imnuj@gmail.com>
2022-10-31 02:52:26 +0900

    feat(problem1): 점수를 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,7 +7,25 @@ import static java.lang.Math.max;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiScore = getScore(pobi);
+        int crongScore = getScore(crong);
+
         return answer;
+    }
+
+    private static int getScore(List<Integer> pages) {
+        int score = 0;
+
+        for (int page : pages) {
+            int max = getMax(page);
+
+            if (score < max) {
+                score = max;
+            }
+        }
+
+        return score;
     }
 
     private static int getMax(int page) {
