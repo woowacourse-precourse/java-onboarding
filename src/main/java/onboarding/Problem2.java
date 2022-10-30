85686b9a9f4e116d80b73d9ac2a7e33b99935a0b
jsw6701 <anes53027@gmail.com>
2022-10-30 22:12:40 +0900

    Problem2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if(!stack.isEmpty()){
+                if(cryptogram.charAt(i) == stack.peek()){
+                    stack.pop();
+                }
+                else{
+                    stack.add(cryptogram.charAt(i));
+                }
+            }
+            else{
+                stack.add(cryptogram.charAt(i));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Character character : new ArrayList<>(stack)) {
+            sb.append(character);
+        }
+
+        return sb.toString();
     }
 }
