25b810020f4958a820b98f2e6ac88d53be268ced
yeada <yeadalm@gmail.com>
2022-10-31 01:00:59 +0900

    upload 대문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i<word.length(); i++){
+            char tempChar = word.charAt(i);
+            //대문자
+            if(Character.isUpperCase(tempChar)){
+                tempChar = (char)('Z' - (tempChar - 'A'));
+            }
+            answer += tempChar;
+        }
         return answer;
     }
 }
