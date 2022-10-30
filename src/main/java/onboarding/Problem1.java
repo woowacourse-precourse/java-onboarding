1ae00702c4539d678ce85b4c5a68230fe1997e32
Nugur1i <duckhee092@naver.com>
2022-10-30 16:50:26 +0900

    Last test

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int ans = 2;
+        int ans = 1;
         return answer;
     }
 }
