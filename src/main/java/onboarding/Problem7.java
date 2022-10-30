a852ef5f1370d945ad40359e8bfdd9c49bee6f6b
situodys <situodys@gmail.com>
2022-10-31 02:47:33 +0900

    feat(문제7): 결과 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import onboarding.problem7.UserManager;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        UserManager um = new UserManager(friends, visitors);
+        return um.recommandTo(user);
     }
 }
