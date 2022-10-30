f9c3f72e5ae6baac3a2a16a8e1d03c85854817b3
hhiyeon <hhyyeon0214@gmail.com>
2022-10-30 18:54:11 +0900

    feat: p4-get value by key

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int SUM_KEY_VALUE = 155;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static char getReverseValue(char base) {
+        return (char) (SUM_KEY_VALUE - (int) base);
     }
 }
