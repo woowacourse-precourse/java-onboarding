55401e4ca03219e2fe9a4a2fc31cc3c878602093
JungHun Jang <ijj5258@naver.com>
2022-10-30 23:25:35 +0900

    Problem4 (feat) 알파벳을 반대로 변환해줄 Map생성을 위한 frogDictionary 메소드 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem4 {
 	
@@ -17,11 +19,33 @@ public class Problem4 {
   
         return chars;
     }
-	
+	/* Character를 ascii코드로 Map에 추가 */
+	public static Map<Character,Character> 
+	frogDictionary(){
+		Map<Character, Character> dictionary= new HashMap<Character, Character>();
+		
+		dictionary.put(' ', ' '); //공백은 반전할 게 없으므로 키와 값을 같게 설정
+		
+		Character capital=90; //대문자 Z
+		for(Character i=65;i<=90;i++) { //대문자 A부터 시작
+			dictionary.put(i, capital);
+			capital--;
+		}
+		
+		Character small=122; //소문자 a
+		for(Character i=97;i<=122;i++) { //소문자 a부터 시작
+			dictionary.put(i, small);
+			small--;
+		}
+		
+		return dictionary;
+	}
 	
     public static String solution(String word) {
         String answer = "";
         List<Character> wordList=convertStringToCharList(word);
+		Map<Character, Character> dictionary=frogDictionary();
+		
 		
         return answer;
     }
