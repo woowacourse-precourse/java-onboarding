85d66ca1b24e3b66d09a260f871521cd33128efe
sdsr <dlxodnr1212@naver.com>
2022-10-30 20:48:45 +0900

    problem2 기능 2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,11 +6,27 @@ import java.util.List;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        //String을 List로 변환
         List<String> list = Arrays.asList(answer.split(""));
 
-
+        list = checkStr(list);
 
         return answer;
     }
-
+    //List의 중복을 검사
+    public static List<String> checkStr(List<String> list){
+        int cnt = 0;
+        while(true) {
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i) == list.get(i + 1)) {
+                    list.remove(i + 1);
+                    list.remove(i);
+                }
+                else cnt = 1;
+            }
+            if(cnt == 1)
+                break;
+        }
+        return list;
+    }
 }
