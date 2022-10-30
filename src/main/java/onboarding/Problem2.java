e1097fc463fe6c19e669b5b739e8ba13296dc264
Jenn Kim <grdd0419@gmail.com>
2022-10-30 19:06:36 +0900

    feat(Problem2): add a function to get decryption results

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,36 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer = "";
+        answer += cryptogram.charAt(0);
+
+        boolean duplicated = false;
+        for(int i=1; i<cryptogram.length(); i++) {
+
+            char cryptoNowChar = cryptogram.charAt(i);
+
+            int answerLen = answer.length();
+            char answerLastChar = answer.charAt(answerLen-1);
+
+            if (cryptoNowChar == answerLastChar)
+                duplicated = true;
+
+            if(cryptoNowChar != answerLastChar && duplicated == false)
+                answer += cryptoNowChar;
+
+            if(cryptoNowChar != answerLastChar && duplicated == true) {
+                answer = answer.substring(0, answerLen-1);
+                duplicated = false;
+                i -= 1;
+            }
+        }
+        if(duplicated) {
+            int answerLen = answer.length();
+            answer = answer.substring(0, answerLen-1);
+            duplicated = false;
+        }
+
         return answer;
     }
 }
