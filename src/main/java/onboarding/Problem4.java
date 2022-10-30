b4f92e64d411e806a7dc573497275e5c91118ce4
LEE HYE JIN <108391677+Hiluxy@users.noreply.github.com>
2022-10-30 16:40:52 +0900

    Problem4 solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+//아스키코드로 반대?
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return change(word);
+    }
+    static String change(String word){
+        String[] strings=word.split(" ");
+        StringBuffer bf = null;
+        for(String str: strings){
+            for(int i=0;i<str.length();i++){
+                char chr=str.charAt(i);
+                if(65<=(int)chr && (int)chr<=90){
+                    bf.append((char)(65+90-(int)chr));
+                }
+                bf.append(" ");
+            }
+        }
+        return bf.toString();
     }
 }
