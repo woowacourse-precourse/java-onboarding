53174f55873dd32dc37367e1daa57bf0c60991eb
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:04:13 +0900

    feat : 인전한 중복문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,6 +11,27 @@ public class Problem2 {
         return answer;
     }
 
+    //인접한 중복문자 제거함수
+    public static String removeDuplicate(String cryptogram){
+        char[] tempResult = cryptogram.toCharArray();
+        int i = 0;
+        char prev = 0;
+
+        for (char c : tempResult) {
+            /*
+            인접한 문자가 같지않으면 임시문자열에 더해준다.
+             */
+            if(prev != c){
+                tempResult[i++] = c;
+                prev = c;
+            }else{
+                i -= 1;
+            }
+        }
+
+        return new String(tempResult).substring(0,i);
+    }
+
 
     //중복문자 존재하는지 구별하는 함수
     //중복문자가 존재할시 true를 반환해준다.
