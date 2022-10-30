6264603a68ee73927555b473d0a18ea345c05160
hossang <zkzk104@naver.com>
2022-10-30 20:41:04 +0900

    feat: 문제4 구현 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c - 'A' >= 0 && c - 'A' < 26) {
+                sb.append((char) ('A' + 25 - (c - 'A')));
+                continue;
+            }
+            if (c - 'a' >= 0 && c - 'a' < 26) {
+                sb.append((char) ('a' + 25 - (c - 'a')));
+                continue;
+            }
+
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
