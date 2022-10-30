338423150449065b80c821b828d830186cf8d99b
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-30 21:18:19 +0900

    feat: Add Problem3 Get Clap Count
    - 박수 횟수를 구하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,12 +2,26 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        if(isException(number)) return -1;
+        if (isException(number)) return -1;
         int answer = 0;
         return answer;
     }
 
-    private static Boolean isException(Integer number){
+    private static Boolean isException(Integer number) {
         return number < 0 || number > 10000;
+    }
+
+    private static Integer getClapCount(Integer num) {
+        Integer result = 0;
+
+        for (Integer i = 1; i <= num; i++) {
+            String s = i.toString();
+            for (Integer j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '3' || s.charAt(j) == '6' || s.charAt(j) == '9')
+                    result += 1;
+            }
+        }
+
+        return result;
     }
 }
