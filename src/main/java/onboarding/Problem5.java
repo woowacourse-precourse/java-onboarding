0d21038ff5ea13d3cee5e19b3aa164dd9ba0bf76
ensk26 <tjensk26@gmail.com>
2022-10-31 03:02:06 +0900

    docs: 기능 목록 구성, problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
+
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    //돈을 지정된 금액의 화폐로 반환되는 개수와 반환하고 남은 돈의 액수 반환
+    static int[] getCountMoney(int money,int unit) {
+
+        return null;
     }
 }
