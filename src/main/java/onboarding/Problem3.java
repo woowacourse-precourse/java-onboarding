7f85e4090ac7dfdcd6e50b49b0b9daea4a3516d6
권도현 <tobeykwon@gmail.com>
2022-10-30 23:49:53 +0900

    feat : Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+
+    private static final List<Character> checkList = Arrays.asList('3', '6', '9');
+
+    // 박수를 몇 번 치는지 계산하는 메서드
+    private static int isClap(int num) {
+        int answer = 0;
+        String s = String.valueOf(num);
+        for(int i = 0; i < s.length(); i++) {
+            if(checkList.contains(s.charAt(i)))
+                answer++;
+        }
+        return answer;
+    }
+
+    // 1번부터 돌면서 박수를 몇 번 쳐야하는 지 계산후 합산
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            answer += isClap(i);
+        }
         return answer;
     }
 }
