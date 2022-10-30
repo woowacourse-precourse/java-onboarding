c1c9149b4a5f1ee0397dc2a07c42378e5749d398
kimminsu <xjfcnfw3@naver.com>
2022-10-30 13:40:22 +0900

    feat(문제6): [예외 사항추가] 이메일 길이 확인

--- src/main/java/onboarding/problem6/Validation.java
+++ src/main/java/onboarding/problem6/Validation.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class Validation {
     private static final int MIN_NUMBER_OF_USERS = 1;
     private static final int MAX_NUMBER_OF_USERS = 10_000;
-
+    private static final int MIN_EMAIL_LENGTH = 11;
+    private static final int MAX_EMAIL_LENGTH = 19;
     private static final String EMAIL_REGEXP = "^[a-zA-Z0-9+-_.]+@email.com+$";
 
     private static final String USERS_EXCEPTION_MESSAGE = "크루의 인원 수는 1 ~ 10,000이어야 합니다.";
     private static final String EMAIL_FORMAT_EXCEPTION_MESSAGE = "이메일 형식은 [a-zA-Z0-9+-_.]@email.com입니다.";
+    private static final String EMAIL_LENGTH_EXCEPTION_MESSAGE = "이메일은 11자 이상 20자 미만이어야 합니다.";
 
 
     private static void validateNumberOfUsers(List<List<String>> forms) {
@@ -22,6 +24,12 @@ public class Validation {
     private static void validateEmailFormat(String email) {
         if (!email.matches(EMAIL_REGEXP)) {
             throw new IllegalArgumentException(EMAIL_FORMAT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static void validateEmailLength(String email) {
+        if ((email.length() < MIN_EMAIL_LENGTH) || (email.length() > MAX_EMAIL_LENGTH)) {
+            throw  new IllegalArgumentException(EMAIL_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 }
