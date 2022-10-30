84cbf78c8870d13fbe684be1c957b1e625693950
김태용 <amico741@naver.com>
2022-10-31 01:02:15 +0900

    feat: 같은 글자가 연속적으로 포함된 체크하는 메서드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,22 +1,41 @@
 package onboarding;
 
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        HashSet<String> letters = makeLetterSet(forms);
+        TreeSet<String> emails = getDuplicateSet(letters, forms);
+        return new ArrayList<>(emails);
     }
 
     public static HashSet<String> makeLetterSet(List<List<String>> forms) {
         HashSet<String> letters = new HashSet<>();
         for(List<String> crew : forms){
             String nickname = crew.get(1);
-            for(int i = 0; i < nickname.length() - 1; i++){
+            for(int i = 0; i < nickname.length() - 1; i++){ // 두개의 연속된 문자열 HashSet에 추가
                 letters.add(nickname.substring(i, i + 2));
             }
         }
         return letters;
+    }
+
+    public static TreeSet<String> getDuplicateSet(HashSet<String> letters, List<List<String>> forms){
+        Iterator<String> iterator = letters.iterator();
+        ArrayList<String> temp = new ArrayList<>();
+        TreeSet<String> answer = new TreeSet<>();
+
+        while(iterator.hasNext()){ // 연속된 문자열 순회하면서 nickname이 포함하고 있는지 체크
+            String letter = iterator.next();
+            for(List<String> crew : forms){
+                String nickname = crew.get(1);
+                if(nickname.contains(letter)){
+                    temp.add(crew.get(0));
+                }
+            }
+            if(temp.size() >= 2) answer.addAll(temp); // 사이즈 2개 이상이라면 TreeSet에 추가 -> 이메일 오름차순 정렬 및 중복 제거
+            temp.clear();
+        }
+        return answer;
     }
 }
