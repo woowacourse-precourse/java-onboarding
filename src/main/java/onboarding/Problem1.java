a427334c59e66036bc91aec10910c5a1938ae38b
daeun <gain3436@gmail.com>
2022-10-31 02:47:57 +0900

    [feat](1) 페이지 더하기 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int pagePlus(int page) {
+        int total = 0;
+        while (page > 1) {
+            total += page%10;
+            page /= 10;
+        }
+        return total;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         int[] pobiArray = pobi.stream().mapToInt(i->i).toArray();
