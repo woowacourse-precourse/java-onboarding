b20a8c2a3288d66a6255f49768e501024deee2a1
JH-TT <leejongho9803@gmail.com>
2022-10-31 04:26:33 +0900

    7번 : user와 간접적으로 친구인 사람들 점수 증가.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,6 +16,30 @@ public class Problem7 {
             }
         }
 
+        // user와 간접적으로 친구인 사람들 점수 증가.
+        for(List<String> friend : friends) {
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+
+            // user 본인이 있으면 넘긴다.
+            if(friend.contains(user)) {
+                continue;
+            }
+            
+            // 둘 다 userFriends에 있거나, 둘 다 없으면 볼 필요가 없으니 넘긴다.
+            if((userFriends.contains(user1) && userFriends.contains(user2)) || (!userFriends.contains(user1) && !userFriends.contains(user2))) {
+                continue;
+            }
+            String snsFriend = userFriends.contains(friend.get(0)) ? friend.get(1) : friend.get(0);
+
+            // 이미 점수가 있으면 10을 추가, 아니면 10부터 시작.
+            if(userScore.containsKey(snsFriend)) {
+                userScore.put(snsFriend, userScore.get(snsFriend) + 10);
+            } else {
+                userScore.put(snsFriend, 10);
+            }
+        }
+
         return answer;
     }
 }
