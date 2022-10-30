db1137e3903448083e625f39c94b1cc129a5a435
jmj005 <jmj00005@naver.com>
2022-10-30 17:21:40 +0900

    refactor: Problem5 Code Refactoring

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-1. 변환된 돈 개수 구하기
+1. 큰 금액부터 차례로 나누기
+2. 변환된 돈 개수 구하기
  */
 
 public class Problem5 {
 
-    private static List<Integer> countMoney(int money) {
-        List<Integer> tmp = new ArrayList<>();
-        int[] coin = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        for (int i = 0; i < coin.length; i ++) {
-            tmp.add(money / coin[i]);
-            money %= coin[i];
+    private static List<Integer> getCoins(int money) {
+        List<Integer> coins = new ArrayList<>();
+        int[] elements = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int element : elements) {
+            coins.add(money / element);
+            money = money % element;
         }
-        return tmp;
+        return coins;
     }
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        answer = countMoney(money);
+        answer = getCoins(money);
         return answer;
     }
 }
