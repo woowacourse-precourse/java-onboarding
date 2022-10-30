ef4045fecff5a219ba2a277f3e6e3fd7bf281c0d
KSH <tkdgns543@gmail.com>
2022-10-31 02:25:48 +0900

    feat(Problem7): 누군가가 주어졌을 때 길이 2인 List에 그 사람이 있을 경우 나머지 한명을 목적지 List에 추가 해주는 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,13 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Map<String, Integer> scores = new HashMap<>();
+    }
 
+    /* 누군가가 주어졌을 때 길이 2인 List의 나머지 한명을 목적지 List에 추가 해주는 메서드 */
+    private static void pickAnotherOne(List<String> destination, List<String> friendPair, String someone) {
+        int index;
+        if (!(friendPair.contains(someone))) {
+            return;
+        }
+
+        index = friendPair.indexOf(someone);
+        String friend = friendPair.get(1-index);
+        destination.add(friend);
     }
 }
