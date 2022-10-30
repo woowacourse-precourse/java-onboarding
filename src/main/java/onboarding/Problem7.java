378199026ffaaada57d5fa4eaaaea22c5020f5ab
Egenieee <hhgg0925@naver.com>
2022-10-30 21:52:40 +0900

    [FEAT] Problem7 타임라인에 방문하는 친구들 중 친구아닌 사람 찾아 점수주는 기능
    - Map의 getOrDefault 메서드로 새로운 친구가 타임라인에 방문하면, 추천점수 1점씩 추가하도록 한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,9 +3,25 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+
+        System.out.println(Problem7.solution(user, friends, visitors));
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Set<String> alreadyFriends = getAlreadyFriends(friends);
         Map<String, Integer> recommendPoint = getRecommendPoint(friends, user);
+        recommendPoint = addRecommendPointByVisit(alreadyFriends, recommendPoint, visitors);
 
         return Arrays.asList("", "");
     }
@@ -28,11 +44,25 @@ public class Problem7 {
             String newFriend = friend.get(1);
 
             if (!user.equals(newFriend)) {
-                recommendPoint.put(newFriend, recommendPoint.getOrDefault(newFriend, +10));
+                recommendPoint.put(newFriend, recommendPoint.getOrDefault(newFriend, 0) + 10);
             }
         }
 
         return recommendPoint;
+    }
+
+    private static Map<String, Integer> addRecommendPointByVisit(Set<String> alreadyFriends, Map<String, Integer> recommendPoint, List<String> visitors) {
+        for (String visitor : visitors) {
+            if (isNewFriendVisiting(visitor, alreadyFriends)) {
+                recommendPoint.put(visitor, recommendPoint.getOrDefault(visitor, 0) + 1);
+            }
+        }
+
+        return recommendPoint;
+    }
+
+    private static boolean isNewFriendVisiting(String visitor, Set<String> alreadyFriends) {
+        return !alreadyFriends.contains(visitor);
     }
 
 }
