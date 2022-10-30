daef1af4eda6dcdc1278e958057a026d7b2f1f3f
somsom13 <somin1113@naver.com>
2022-10-30 22:58:19 +0900

    refactoring(문제2): 메소드 분리 / 자바 컨벤션 적용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,49 +3,84 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
+    private static Stack<Character> words = new Stack<>();
+    private static Boolean wasTopDuplicate;
     public static String solution(String cryptogram) {
-        Stack<Character> words = deleteDuplicateWordsInRow(cryptogram);
-        String answer = getFinalString(words);
+        words.clear();
+        wasTopDuplicate = false;
+        deleteDuplicateWordsInRow(cryptogram);
+        String answer = toString(words);
         return answer;
     }
 
-    public static Stack<Character> deleteDuplicateWordsInRow(String cryptogram) {
-        Stack<Character> words=new Stack<>();
-        boolean isTopDuplicate=false;
+
+    public static void deleteDuplicateWordsInRow(String cryptogram) {
         words.push(cryptogram.charAt(0)); //첫 문자는 바로 push
-        for(int i = 1; i < cryptogram.length(); i++) {
-            char nowChar=cryptogram.charAt(i);
-            if(nowChar==words.peek()) {
-                //중복이라면
-                isTopDuplicate=true;
-                continue; //이번 문자는 stack 에 삽입하지 않고 다음 문자를 체크한다
-            }
-            //여기서부터는 top 이랑 같지 않은 다른 문자일 경우 처리
-            if(isTopDuplicate) {
-                //지금까지 내내 중복된 문자 나오다가, 이번에 처음으로 중복이 아닌 문자가 나온 것
-                words.pop();
-                //그런데, pop 하고 그 다음 peek 가 같은 문자라면? -> 또 제거!
-                if(!words.isEmpty()) {
-                    if(nowChar==words.peek()) {
-                        continue;
-                    }
-                }
 
-            }
-            words.push(nowChar);
-            isTopDuplicate = false; //이제 중복 아닌 수 등장이므로 false 로 변경
+        for (int i = 1; i < cryptogram.length(); i++) {
+            char nowChar = cryptogram.charAt(i);
+            char top = words.peek();
+
+            checkNewCharDuplicate(nowChar, top);
         }
 
-        //for 문이 다 끝난  후, stack 의 top 에 있는게 duplicate 라면? -> stack 에서 제거
-        if(isTopDuplicate) {
-            words.pop();
-        }
-        return words;
+        checkLastStackTop();
+        return;
     }
 
-    private static String getFinalString(Stack<Character> words) {
+    private static void checkNewCharDuplicate(char nowChar, char top) {
+        if (isNowCharTopSame(nowChar, top)) {
+            wasTopDuplicate = true;
+            return;
+        }
+        updateStack(nowChar);
+    }
+
+    private static Boolean isNowCharTopSame(char nowChar, char top) {
+        if (nowChar == top) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void updateStack(char nowChar) {
+        if (wasTopDuplicate) {
+            changeStackTop(nowChar);
+        }
+        putNewCharToStack(nowChar);
+    }
+
+    private static void checkLastStackTop() {
+        if (wasTopDuplicate) {
+            words.pop();
+        }
+    }
+
+    private static void changeStackTop(char nowChar) {
+        words.pop();
+
+        if (words.isEmpty()) {
+            wasTopDuplicate = false;
+            return;
+        }
+        char newTop = words.peek();
+        if (isNowCharTopSame(nowChar, newTop)) {
+            wasTopDuplicate = true;
+            return;
+        }
+        wasTopDuplicate = false;
+    }
+
+    private static void putNewCharToStack(char nowChar) {
+        if (wasTopDuplicate) {
+            return;
+        }
+        words.push(nowChar);
+    }
+
+    private static String toString(Stack<Character> words) {
         StringBuilder sb = new StringBuilder();
-        while(!words.isEmpty()) {
+        while (!words.isEmpty()) {
             sb.insert(0, words.pop());
         }
         return sb.toString();
