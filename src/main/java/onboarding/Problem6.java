3aa3d303dfed0cdebb048132d096867adc635e3b
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-30 14:44:24 +0900

    comment: write function list of problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * Problem 6 function list
+ * - Create duplication map for each part of nickname
+ * - Select emails which needs to be notified
+ * - Sort 'need notified' email list
+ * - Remove duplicated email from 'need notified' email list
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
