1d58ca0a137eddf4028159d155e6e8f17e07ce4e
sosow0212 <sosow0212@naver.com>
2022-10-30 15:17:54 +0900

    refactor: Problem1 - static 필드에 private 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 class Problem1 {
 
-    static final int LEFT_PAGE = 0;
-    static final int RIGHT_PAGE = 1;
-    static final int POBI_WIN = 1;
-    static final int CRONG_WIN = 2;
-    static final int DRAW = 0;
-    static final int EXCEPTION = -1;
+    private static final int LEFT_PAGE = 0;
+    private static final int RIGHT_PAGE = 1;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int DRAW = 0;
+    private static final int EXCEPTION = -1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (checkException(pobi) || checkException(crong))
