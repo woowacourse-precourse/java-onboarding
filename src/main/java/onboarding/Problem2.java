d127ead0ad9f4ec651af702b051ca2f94d501788
dmdrk1414 <seungchan141414@gmail.com>
2022-10-30 11:27:05 +0900

    [Add] Problem_2 추가
    이문제는 제가 가장싫어하는 문제입니다. 문자열의 중복 제거
    
    1. 문자열을 나누고 추가를 하는 식으로 제거를하였다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+
+        char charAtFirst = '0';
+        char charAtSecond = '0';
+        String subString = "";
+        int second = 0;
+        int endIndex = answer.length();
+        for (int i = 0; i < endIndex; i++) {
+            for (int first = 0; first < answer.length() - 1; first++) {
+                StringBuffer sb = new StringBuffer();
+                second = first + 1;
+                charAtFirst = answer.charAt(first);
+                charAtSecond = answer.charAt(second);
+
+                if (charAtFirst == charAtSecond) {
+                    subString = answer.substring(0, first);
+                    sb.append(subString);
+                    subString = answer.substring(second + 1, answer.length());
+                    sb.append(subString);
+                    answer = sb.toString();
+                }
+            }
+        }
         return answer;
     }
 }
