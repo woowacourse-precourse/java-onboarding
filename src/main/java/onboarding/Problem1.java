4899fce6dd73141e866edfb625a541bb250a8cf3
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-10-30 19:57:54 +0900

    P1: get score

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,6 +16,25 @@ class Problem1 {
             return true;
         }
     }
+
+    public static int getScore(int num){
+        int plus = 0;
+        int multiply = 1;
+
+        while (num > 0){
+            int temp = num % 10;
+            plus += temp;
+            multiply *= temp;
+            num /= 10;
+        }
+
+        if (plus >= multiply) {
+            return plus;
+        } else {
+            return multiply;
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
