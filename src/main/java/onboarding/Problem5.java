37d64cbe0def95247221226c8637d9b676899623
himjjong <jjijji5728@tukorea.ac.kr>
2022-10-30 16:58:51 +0900

    feat : 정수형 배열 moneyEx선언 및 초기화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,15 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    static int[] moneyEx = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> checklist = new ArrayList<Integer>();
+        return cal(money, checklist);
+    }
+    static List<Integer> cal(int money, List<Integer> list){
+        return list;
     }
 }
