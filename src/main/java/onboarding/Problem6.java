d344479b95f3ca5006328b5779d6b99e848d31ec
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-30 20:28:14 +0900

    feat,fix(PROBLEM6): duplicateCrewMail 구현과 그에 따른 duplicateName에서 발생하는 버그 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
@@ -8,9 +9,9 @@ public class Problem6 {
         List<String> dupCheck = new ArrayList<>();
         List<String> dupList = new ArrayList<>();
         for (int i = 0; i < forms.size(); i++) {
-            String name = forms.get(i).get(2);
-            for (int j = 0; j < name.length(); j++) {
-                String splitName = name.substring(j,j+1);
+            String name = forms.get(i).get(1);
+            for (int j = 0; j <= name.length()-2; j++) {
+                String splitName = name.substring(j,j+2);
                 if(dupCheck.contains(splitName) && !dupList.contains(splitName)){
                     dupList.add(splitName);
                 } else if(!dupCheck.contains(splitName)) {
@@ -20,8 +21,26 @@ public class Problem6 {
         }
         return dupList;
     }
+    public static List<String> duplicateCrewMail(List<List<String>> forms){
+        List<String> dupMail = new ArrayList<>();
+        List<String> dupList = duplicateName(forms);
+        for (int i = 0; i < forms.size(); i++) {
+            String name = forms.get(i).get(1);
+            String email = forms.get(i).get(0);
+            for (int j = 0; j <= name.length()-2; j++) {
+                String splitName = name.substring(j,j+2);
+                for (int k = 0; k < dupList.size(); k++) {
+                    if(dupList.contains(splitName) && !dupMail.contains(email)){
+                        dupMail.add(forms.get(i).get(0));
+                    }
+                }
+            }
+        }
+        Collections.sort(dupMail);
+        return dupMail;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = duplicateCrewMail(forms);
         return answer;
     }
 }
