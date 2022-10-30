7e84fed2050ddf27608a5dac2b6db31246efcc84
JongDo737 <jongmin373@gmail.com>
2022-10-31 00:45:27 +0900

    problem2 암호해독 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,26 +2,12 @@ package onboarding;
 
 class Problem2 {
     public static String solution(String cryptogram) {
-        Problem2 problem2 = new Problem2();
-        String answer = "answer";
-        boolean isOK = true;
-        while (isOK) {
-            int length = cryptogram.length();
-            for (int i = 0; i < cryptogram.length() - 1; i++) {
-                String sameWords = problem2.sameWord(cryptogram);
-                cryptogram = cryptogram.replace(sameWords, "");  // 같은 문자 나왔을 때 없애줌
-                i -= 1;     // 없애줬으니 1칸 뒤로
-                break;
-            }
-            if (length == cryptogram.length()) break;            // 처음 문자와 길이가 같다 => 변화가 없다 => 암호 해독 끝났다
-        }
-
-        answer = cryptogram;
+        String answer = decryptString(cryptogram);
         return answer;
     }
 
     // 같은 문자가 연속되는 문자열을 검출해서 반환해주는 역할
-    public String sameWord(String words) {
+    public static String sameWord(String words) {
         boolean check = true;
 
         for (int i = 0; i < words.length() - 1; i++) {
@@ -48,5 +34,21 @@ class Problem2 {
             }
         }
         return "";
+    }
+    // 암호 해독해주는 메서드
+    public static String decryptString(String cryptogram) {
+        boolean isOK = true;
+        while (isOK) {
+            int length = cryptogram.length();
+            for (int i = 0; i < cryptogram.length() - 1; i++) {
+                String sameWords = sameWord(cryptogram);
+                cryptogram = cryptogram.replace(sameWords, "");  // 같은 문자 나왔을 때 없애줌
+                i -= 1;     // 없애줬으니 1칸 뒤로
+                break;
+            }
+            if (length == cryptogram.length()) isOK = false;            // 처음 문자와 길이가 같다 => 변화가 없다 => 암호 해독 끝났다
+        }
+
+        return cryptogram;
     }
 }
