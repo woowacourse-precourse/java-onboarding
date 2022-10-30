351bf0497447026060de72c985274d581fbead5c
minju412 <ln8847@ajou.ac.kr>
2022-10-31 02:22:37 +0900

    refactor: 스택 사용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,73 +1,40 @@
 package onboarding;
 
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        char[] charArr = cryptogram.toCharArray();
-        boolean flag = false;
+        Stack<Character> stack = new Stack<Character>();
+        char dup = ' '; // 중복되는 문자 -> 삭제할 문자
+        char top = ' '; // 스택의 top
+        char cur = ' '; // cur 은 cryptogram 을 순회할 문자
 
-         do {
-            flag = false;
-            for (int i = 1; i < charArr.length; i++) {
-                if (charArr[i] == charArr[i - 1]) {
-                    charArr = remove(charArr, i);
-                    charArr = remove(charArr, i - 1);
-                    flag = true; // remove 가 발생했으면 flag 에 true 대입
+        for(int i = 0; i < cryptogram.length(); i++) {
+            cur = cryptogram.charAt(i);
+
+            if(stack.isEmpty()) { // 스택이 비어있을 때는 무조건 push
+                stack.push(cur);
+            } else {
+                top = stack.peek();
+                if (cur != top && cur != dup) { // push 할 문자가 스택의 top 과 같지 않으면서 중복 문자도 아닐 때
+                    stack.push(cur);
+                    dup = ' '; // dup 초기화
+                } else if (cur == top) { // push 할 문자와 스택의 top 이 같을 때
+                    dup = top; // dup 업데이트
+                    stack.pop(); // 중복 문자 제거
                 }
             }
-        } while (flag == true); // 더이상 remove 가 발생하지 않으면 반복문 종료
-
-        return new String(charArr);
-    }
-
-    /**
-     * char 배열을 리스트로 변경하고, 인자로 전달된 index를 제거한다. 그리고 리스트를 배열로 변경해 리턴한다.
-     * @param arr
-     * @param index
-     * @return
-     */
-    private static char[] remove(char[] arr, int index) {
-        if (arr == null || index < 0 || index >= arr.length) {
-            return arr;
         }
 
-        // char array -> char list 변환
-        ArrayList<Character> charList = arrToList(arr);
-        charList.remove(index);
-        return listToArr(charList);
+        return stackToString(stack);
     }
 
-    /**
-     * char array 를 char list 로 변환하는 메서드
-     * @param charArr
-     * @return
-     */
-    private static ArrayList<Character> arrToList(char[] charArr) {
-        // char array -> string 변환
-        String s = new String(charArr);
-
-        // string -> list 변환
-        ArrayList<Character> charList = new ArrayList<Character>();
-        for(int i = 0; i < s.length(); i++) {
-            charList.add(s.charAt(i));
+    private static String stackToString(Stack<Character> stack) {
+        StringBuilder answer = new StringBuilder();
+        for (Character character : stack) {
+            answer.append(character);
         }
-
-        return charList;
-    }
-
-    /**
-     * char list 를 char array 로 변환하는 메서드
-     * @param charList
-     * @return
-     */
-    private static char[] listToArr(ArrayList<Character> charList) {
-        char[] result = new char[charList.size()];
-        for (int i = 0; i < charList.size(); i++) {
-            result[i] = charList.get(i);
-        }
-
-        return result;
+        return answer.toString();
     }
 
 }
