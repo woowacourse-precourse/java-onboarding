a0dc73ea4af604b3408be066e0aa36433e2c1db8
sosow0212 <sosow0212@naver.com>
2022-10-30 15:19:15 +0900

    refactor: Problem6 - static 필드에 private 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,11 +3,11 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
-    static final int FORM_EMAIL = 0;
-    static final int FORM_NICKNAME = 1;
-    static List<String> answer = new ArrayList<>();
-    static List<Set<String>> partsOfNicknameList = new ArrayList<>();
-    static Map<String, Integer> nicknameAndCountMap = new HashMap<>();
+    private static final int FORM_EMAIL = 0;
+    private static final int FORM_NICKNAME = 1;
+    private static List<String> answer = new ArrayList<>();
+    private static List<Set<String>> partsOfNicknameList = new ArrayList<>();
+    private static Map<String, Integer> nicknameAndCountMap = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
         setupData(forms);
