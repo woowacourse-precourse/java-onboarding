2d3a14df3b61501494bd25dae959e7125f92d6f2
WonSeok-dd <cluejws@gmail.com>
2022-10-30 18:48:42 +0900

    Problem5 | feat: 기능 정의

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 구현 사항
+ * 1. 화폐 리스트, 결과 리스트 선언
+ * 2. 계산 알고리즘 구현
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
