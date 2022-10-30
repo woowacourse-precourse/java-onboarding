39b862098421131f8cb94e42f3d27720bf004d58
donghae <donghae1999@gmail.com>
2022-10-31 00:49:52 +0900

    P4 : 청개구리 사전 변환 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem4 {
+    private static int maxUpperAscii = 90;
+    private static int minUpperAscii = 65;
+    private static int maxLowerAscii = 122;
+    private static int minLowerAscii = 97;
     public static String solution(String word) {
         String answer = "";
+        answer = cvtString(word);
         return answer;
+    }
+    private static String cvtString(String word){
+        String result="";
+        for(int i=0; i<word.length(); i++){
+            char charWord = word.charAt(i);
+            if(Character.isUpperCase(charWord)) {
+                result += (char) (maxUpperAscii -((charWord)-minUpperAscii));
+            }
+            else if(Character.isLowerCase(charWord)){
+                result += (char) (maxLowerAscii -((charWord)-minLowerAscii));
+            }
+            else{
+                result +=" ";
+            }
+        }
+        return result;
     }
 }
