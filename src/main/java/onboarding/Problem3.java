10796ce135e7cb6b324a5b4acceb015f5d6ff75f
ChoiYeonho0903 <903yh@naver.com>
2022-10-30 16:32:41 +0900

    Problem3 : solution 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+    private static final List<Integer> number369 = List.of(3, 6, 9);
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            int num = i;
+            while(num > 0) {
+                if(number369.contains(num % 10)) {
+                    answer++;
+                }
+                num /= 10;
+            }
+        }
         return answer;
     }
 }
