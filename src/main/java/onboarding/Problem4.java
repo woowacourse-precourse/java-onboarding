83a1489ccd9897a77d2c04b7602d1610ab4680eb
Albert <guang7226961@naver.com>
2022-10-30 09:01:26 +0900

    feat(Problem4) 기능 구현완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import onboarding.problem4.AlphabetChanger;
+import onboarding.problem4.WordAlphabetValidator;
+import onboarding.problem4.WordsRangeValidator;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		if (word.isBlank()) {
+            throw new IllegalArgumentException("word는 null 혹은 공백으로 될수 없다.");
+		}
+
+		WordsRangeValidator wordsRangeValidator = new WordsRangeValidator();
+		if (!wordsRangeValidator.validate(word.length())) {
+			throw new IllegalArgumentException("word의 길이는 1이상 1,000 이하인 문자열이여야 한다.");
+		}
+
+		StringBuilder answer = new StringBuilder();
+		AlphabetChanger alphabetChanger = new AlphabetChanger();
+		WordAlphabetValidator wordAlphabetValidator = new WordAlphabetValidator();
+
+		char[] characters = word.toCharArray();
+		for (char character : characters) {
+			if (wordAlphabetValidator.validate(character)) {
+				character = alphabetChanger.change(character);
+			}
+			answer.append(character);
+		}
+
+		return answer.toString();
+	}
 }
--- src/main/java/onboarding/problem4/WordAlphabetValidator.java
+++ src/main/java/onboarding/problem4/WordAlphabetValidator.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 public class WordAlphabetValidator {
 	private final Pattern pattern = Pattern.compile("[a-z|A-Z]");
 
-	public boolean validate(Character word) {
-		return pattern.matcher(word.toString()).matches();
+	public boolean validate(Character character) {
+		return pattern.matcher(character.toString()).matches();
 	}
 
 }
