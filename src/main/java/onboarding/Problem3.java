54e10ab203fc1ab752ed70a60811672c48e81f7d
Frog-Slayer <Frog-Slayer@github.com>
2022-10-30 14:23:26 +0900

    Problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (Integer i = 1; i <= number; i++){
+            String iToString = i.toString();
+            for (int j = 0; j < iToString.length(); j++){
+                if (iToString.charAt(j) - '0' != 0 && iToString.charAt(j) % 3 == 0) answer += 1;
+            }
+        }
+
         return answer;
     }
 }
