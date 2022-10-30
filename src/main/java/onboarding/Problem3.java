fca51a19e500222e95bb79f8a7dacbca81c7deed
doongu <must1080@naver.com>
2022-10-31 01:05:36 +0900

    feat(Problem3): 순회하는 숫자를 String으로 바꾸는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        String tempNumber;
+        for(int num = 1; num <= number; num++){
+            tempNumber = Integer.toString(num);
+        }
+
         return answer;
     }
 }
