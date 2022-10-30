d35a41d8969579b1ebe8118a98e1c395a728d1d1
kkkwp <chxrryda@gmail.com>
2022-10-31 01:21:54 +0900

    feat(frogDict): 대소문자를 구분하여 청개구리 사전으로 변환한 결과를 반환해주는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,14 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static char frogDict(char ch) {
+        if (Character.isLowerCase(ch)) {
+            return (char) ('z' - (ch - 'a'));
+        }
+        if (Character.isUpperCase(ch)) {
+            return (char) ('Z' - (ch - 'A'));
+        }
+        return ch;
+    }
 }
