c29ed62cd25da8e2a5d9384080a369eb9f8be90f
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 21:39:14 +0900

    feat:[Problem3] 3,6,9 숫자 세는 메서드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,17 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    private static int count369(int num){
+        int cnt=0;
+        for(int i=1;i<=num;i++){
+            int temp=i;
+            while(temp>0){
+                if(temp%10==3||temp%10==6||temp%10==9){
+                    cnt++;
+                }
+                temp/=10;
+            }
+        }
+        return cnt;
+    }
 }
