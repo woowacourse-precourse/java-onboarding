cbd56965c23b40e775c640cabfd10a707d18d50f
myeongju00 <j51677767@gmail.com>
2022-10-31 02:09:01 +0900

    feat: 유저의 친구 리스트 생성 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String, List<String>> friendsMap = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
+    private static void inputNameToMap(String user, List<List<String>> friends) {
+        List<String> userFriend = new ArrayList<>();
+        for(List<String> friend : friends) {
+            if(friend.contains(user)) {
+                for (String name : friend) {
+                    if (name.equals(friend.get(0))) {
+                        userFriend.add(friend.get(1));
+                    } else {
+                        userFriend.add(friend.get(0));
+                    }
+                }
+            }
+        }
+    }
+
 }
