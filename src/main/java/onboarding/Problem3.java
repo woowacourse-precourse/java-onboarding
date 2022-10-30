899cba71c6a98ae24f52330321e4b3fc32781704
RadSparkle <wwwartg1@naver.com>
2022-10-31 02:12:25 +0900

    FEAT : number인자를 한개씩 1부터 문자열로 바꿔주는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        int result = count(number);
+
+        return result;
+    }
+
+    public static int count(int number) {
+        int count = 0;
+
+        for(int i = 1; i <= number; i++) {
+            String str = String.valueOf(i);
+
+        }
+
+        return count;
     }
 }
