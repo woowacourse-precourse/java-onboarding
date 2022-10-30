39a71050899a1696a8b81870ad9c593340112fbd
sangwon <swchoi1997@naver.com>
2022-10-30 20:41:53 +0900

    fix : 중복 문자열 체크를 위한 배열을 만들던 도중 길이 2, 3, 4... 등 긴 단어 대신 2글자 중복만 체크하도록 로직 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,16 +13,12 @@ import java.util.Set;
  *   -> 2) 이메일 형식 체크 + 닉네임 길이 체크 + 닉네임 한글만 있는지 체크
  *       =>@뒤에 email.com 이 오는지 + 이메일 길이가 11~20인지 + 닉네임 길이가 1~20 인지 확인 + 유니코드로 가 ~ 힣까지 범위 체크
  * 2. 로직 생각
- *   -> 1) (1) split로 분리 후 전체 순회하면서 indexof로 최초로 같은 인넥스를 찾아서 거기서 부터 얼마만큼 길이가 같은지 체 2개, 3개, 4개... 따로 넣기
- *          ex) 제이엠호 제이엠슨 제이엠 이제엠 이엠슨지조리
- *              제이, 제이엠, 이엠슨, 이엠 이 패턴이 나올 수 있음
- *              => 10,000 * 20 * 10,000 ???
+ *   -> 1)   (1) 모든 단어를 돌면서 길이가 2인 패턴을 찾음 (n)개 ( 2이상을 찾을 필요가 없는 이유 : 어차피 3, 4에도 2가 포함되어있음)
+ *               HashSet을 3개 선언(전체 패턴, 중복패턴, 찾고있는 단어의 패턴)
+ *               찾고있는 단어의 패턴에 모든 패턴을 넣은 후 전체패턴과 겹치는 것이 있는지 확인 겹칠 경우 중복 패턴에 추가
  *
- *         (2) 모든 단어를 돌면서 길이가 2,3,4,... 까지 모든 패턴을 만듦 (n-1)n / 2 개
- *             HashSet을 선언 후 add 하는데 그 대 Contains해서 이미 있는 패턴이면 다른 HashSet에 담아줌 (add, contains -> O(1)
- *             => 최악의 경우 10000 * 20 * 19 라서 위 1번보다는 시간이 적게 들 것이라 예상
- *   -> 2) 중복없이 저장된 패턴을 kmp알고리즘으로 패턴을 체크함
- *       => kmp : O(N) , 중복없이 저장된 패턴을 순회하며 일치찾기? O(N) O(N^2)일거같은데.... 일단 구현해보자
+ *   -> 2) 중복패턴에 저장된 패턴을 kmp알고리즘으로 패턴을 체크함
+ *       => kmp : O(N)
  */
 public class Problem6 {
 
@@ -99,12 +95,9 @@ public class Problem6 {
     }
     public static void findPatternByWord(Set<String> allPattern, Set<String> overlapPattern, String word) {
         Set<String> wordPatterns = new HashSet<>();
-        for (int i = 2; i <= word.length(); i ++ ) {
-            for (int j = 0; j <= word.length() - i; j ++ ) {
-                String subWord = word.substring(j, j + i);
-                wordPatterns.add(subWord);
-                // addPatternToHashSet(allPattern, overlapPattern, subWord);
-            }
+        for (int j = 0; j < word.length() - 1; j ++ ) {
+            String subWord = word.substring(j, j + 2);
+            wordPatterns.add(subWord);
         }
         addPatternToHashSet(allPattern, overlapPattern, wordPatterns);
     }
