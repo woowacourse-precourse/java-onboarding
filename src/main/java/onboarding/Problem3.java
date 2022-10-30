5cf712c33d0b4378ddf29ac53697a3398f2e909f
jsw6701 <anes53027@gmail.com>
2022-10-30 23:45:48 +0900

    Problem3 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int[] result = new int[10001];
+        result[0] = result[1] = result[2] = 0;
+        for (int i = 3; i <= 10000; i++) {
+            result[i] = result[i-1];
+            int tmp = i;
+            while(tmp > 0){
+                int decimal = tmp % 10;
+                if(decimal == 3 || decimal == 6 || decimal == 9){
+                    result[i]++;
+                }
+                tmp /= 10;
+            }
+        }
+        return result[number];
     }
 }
