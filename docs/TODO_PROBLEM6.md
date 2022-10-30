49853c7faeee1f18e02e0e2d378c3918b6995b32
CHAYEONIL <jeay123@naver.com>
2022-10-31 01:31:35 +0900

    문제6 주요기능 키를 가지고 있으면 nickname에 추가 구현

--- docs/TODO_PROBLEM6.md
+++ docs/TODO_PROBLEM6.md
@@ -6,8 +6,8 @@
 | 필요기능         | 변수명(함수명)                                    | 구현 여부(O, X) |
 |--------------|---------------------------------------------|-------------|
 | 2단어씩 나누기     | for (int j = 0; j < name.length() - 1; j++) | O           |
-| 키를 가지고 있으면   | if (email_nick.containsKey(key)) | X           |
-| nickname에 추가 |  nickname.add(email);| X           |
+| 키를 가지고 있으면   | if (email_nick.containsKey(key)) | O           |
+| nickname에 추가 |  nickname.add(email);| O           |
 | 정렬           |  .sorted()| X           |
 
 ### 예외처리
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -18,9 +18,17 @@ public class Problem6 {
             for (int j = 0; j < name.length() - 1; j++) {
                 String key = name.substring(j, j + 2);
                 // 키를 가지고 있으면
-
+                if (email_nick.containsKey(key)) {
+                    String email = email_nick.get(key);
+                    // nickname에 추가
+                    if (!email.equals(forms.get(i).get(0))) {
+                        nickname.add(email);
+                        nickname.add(forms.get(i).get(0));
+                    }
                 }
+                email_nick.put(key, forms.get(i).get(0));
             }
+        }
         return answer;
     }
 }
