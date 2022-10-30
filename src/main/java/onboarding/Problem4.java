08391e27b0996be129725eed160cfd12a1605ee2
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 14:42:41 +0900

    feat(problem4): 입력받은 문자에 매칭되는 문자 반환 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,13 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static char getMatchingCharacter(char input) {
+        boolean isUpperCase = true;
+        if ('a' <= input) isUpperCase = false;
+        char standard = isUpperCase ? 'A' : 'a';
+
+        int index = input - standard;
+        return (char) (standard + 25 - index);
+    }
 }
