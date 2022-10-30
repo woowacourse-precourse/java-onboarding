3851fb8ce370b47c23f997bdd5edc432d7f3d39e
권태현 <kygkth2011@gmail.com>
2022-10-31 01:03:45 +0900

    Solve Problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Map<String, String> map = new HashMap<>();
+        TreeSet<String> email_set = new TreeSet<>();
+
+        for(List<String> form : forms){
+            String email = form.get(0);
+            String nick = form.get(1);
+            for(int i = 0 ; i < nick.length() - 1; i++){
+                String sub_string = nick.substring(i, i+2);
+                if(map.containsKey(sub_string)){
+                   email_set.add(email);
+                   email_set.add(map.get(sub_string));
+                }
+                map.put(sub_string, email);
+            }
+        }
+        List<String> answer = new ArrayList<String>(email_set);
         return answer;
     }
 }
