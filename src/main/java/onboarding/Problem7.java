607e648b589edfafc3436f872e925d96f8dad5c9
ksh-beginner <ohk9134@naver.com>
2022-10-30 09:22:17 +0900

    feat : 유저 아이디 목록을 생성하는 기능 메소드 추가
    모든 유저 아이디가 담긴 친구 관계 목록을 파라미터로 받아
    유저 아이디들을 Set에 담아서 반환하는 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static final Map<String, Integer> friendRecommendScoreMap = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Set<String> getUserIds(List<List<String>> friends) {
+        Set<String> userIds = new HashSet<>();
+        friends.forEach(nestedFriends -> {
+                    String friendName1 = nestedFriends.get(0);
+                    String friendName2 = nestedFriends.get(1);
+                    userIds.add(friendName1);
+                    userIds.add(friendName2);
+                });
+        return userIds;
     }
 }
