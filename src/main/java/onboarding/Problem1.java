99c203c69d240f95bf9f9f571da8c7f2b5ce5534
hanjaegyeong <hjg000223@naver.com>
2022-10-30 23:00:09 +0900

    [Mission1] feat: 전체 작동 구조 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 1;
+        int maxNum = 0;
+        int num;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                int addSum = 0;
+                int mulSum = 1;
+                if ((pobi.get(1) - pobi.get(0) != 1) || (crong.get(1) - crong.get(0) != 1)) {
+                    answer = -1;
+                }
+                num = getNum(pobi, crong, i, j);
+                addSum = getAddSum(addSum, num);
+                mulSum = getMulSum(mulSum, num);
+                maxNum = getMaxNum(maxNum, i, addSum, mulSum);
+                answer = getAnswer(answer, maxNum, i, addSum, mulSum);
+            }
+        }
         return answer;
     }
 }
