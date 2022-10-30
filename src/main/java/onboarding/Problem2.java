6504481d9b8405bae71f129dca86d4eff41ed4ee
HamSeungChan <head0618@naver.com>
2022-10-30 20:07:03 +0900

    스택을 사용한 중복 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = deleteRepeat(cryptogram);
         return answer;
+    }
+
+    private static String deleteRepeat(String cryptogram) {
+        String Result = "";
+        Stack<Character> deleteStack = new Stack<>();
+        for (Character x : cryptogram.toCharArray()) {
+            if (!deleteStack.isEmpty() && deleteStack.peek() == x) {
+                deleteStack.pop();
+            } else
+                deleteStack.push(x);
+        }
+
+        for (int i = 0; i < deleteStack.size(); i++)
+            Result = Result + deleteStack.get(i);
+
+        return Result;
     }
 }
