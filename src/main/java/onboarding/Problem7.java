b254a6356a8beab608eb04ed3b3212c58d370f32
ddubidubap <park62000@gmail.com>
2022-10-30 14:07:34 +0900

    PROBLEM7 기능 요구 사항 목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    /*
+        기능 요구 사항 목록
+        1. 사용자의 친구 리스트 반환 함수
+        2. 친구의 친구 점수 해시맵 반환 함수
+        3. 방문자 점수 해시맵 반환 함수
+        4. 해시맵 정렬 함수
+        5. solution (함수 순서대로 실행)
+    */
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
