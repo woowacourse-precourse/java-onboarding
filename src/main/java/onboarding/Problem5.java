3121422275621d7cec962a8b2ea851c3e16e21c3
rt3310 <btac3310@gmail.com>
2022-10-30 16:51:19 +0900

    feat(Problem5): 각 돈 개수 반환 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> cashList = new ArrayList<>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+    
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<Integer> countCash(int money) {
+        List<Integer> cashCount = new ArrayList<>();
+        for (int cash : cashList) {
+            cashCount.add(money / cash);
+            money %= cash;
+        }
+        return cashCount;
     }
 }
