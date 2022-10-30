7b3b51be0a0a702a3a88420bde0581b2ad4e530b
jw0293 <k38836919@gmail.com>
2022-10-30 17:27:12 +0900

    feature : Judge Alphabet [하나의 문자가 알파벳인지 확인하는 기능]

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+/**
+ * 알파벳인지 타 문자인지 확인하는 기능을 위한 클래스
+ */
+class JudgeAlphabet{
+    public boolean isAlpha(char ch){
+        return (('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z'));
+    }
+}
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = " ";
         return answer;
     }
 }
