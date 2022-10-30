613ef90d164e2a69853e6c808772c712ada8e672
song-wooseok <sws981202@gmail.com>
2022-10-31 05:05:51 +0900

    feat: [PROBLEM5 - 기능3] 필요한 지폐 개수를 반환하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import onboarding.problem5.service.MoneyUnitCountService;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = MoneyUnitCountService.getUnitCount(money);
         return answer;
     }
 }
