7920f7788f9fee13a8bfa478f3cecb61a2b6d0ec
song-wooseok <sws981202@gmail.com>
2022-10-31 05:13:43 +0900

    feat: solution 메서드 작성 및 5번 문제 테스트 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,12 +2,10 @@ package onboarding;
 
 import onboarding.problem5.service.MoneyUnitCountService;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = MoneyUnitCountService.getUnitCount(money);
-        return answer;
+        return MoneyUnitCountService.getUnitCount(money);
     }
 }
