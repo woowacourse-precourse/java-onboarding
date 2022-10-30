1c88022be4154944529ff4c00574fa96e559655a
Kuyho Chung <kchung1995@gmail.com>
2022-10-30 18:25:04 +0900

    온보딩 - 문제 6 - inspectEmailCondition 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    /**
+     * 정규식을 통해 @email.com을 만족하는지 확인
+     * @param input
+     * @return
+     */
+    private static boolean inspectEmailCondition(String input) {
+        return input.matches("/^.*@email.com$/");
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
