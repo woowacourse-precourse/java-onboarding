8a34cffb28e1ccd7151f143fdbd0b40216030a05
donghae <donghae1999@gmail.com>
2022-10-30 22:32:04 +0900

    P2 : 문자열 중복 체크 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = translate(cryptogram);
         return answer;
+    }
+
+    public static boolean checkDup(String cryptogram){
+        for(int i=0; i<cryptogram.length()-1; i++){
+            if(cryptogram.charAt(i)==cryptogram.charAt(i+1))
+                return true;
+        }
+        return false;
+    }
+
+    public static String translate(String cryptogram){
+        String result="";
+        int count=0;
+        while(checkDup(cryptogram)){
+
+        }
+        return result;
     }
 }
