4ca48e1fceb842bde01b7d4df8e5ef6749eab839
JUN <chltmdwns96@naver.com>
2022-10-30 21:16:32 +0900

    fix : 예외 처리 변경 - 기존 예외 삭제 - 닉네임 길이 2글자 미만 체크 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,30 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-    final static String EMAIL_DOMAIN = "email.com";
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        if (checkMemberLimit(forms)) {
-            return null;
-        }
+
         return answer;
     }
 
-    private static boolean checkMemberLimit(List<List<String>> forms) {
-        // 크루원 1 이상 10000 이하 검증
-        if (forms.size() >= 1 && forms.size() <= 10000) {
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean checkMemberException(String nickName, String email) {
-        if (checkEmailException(email) || checkNickNameLengthException(nickName)) {
-            return true;
-        }
-        return false;
-    }
 
     private static List<String> seperateNickName(String nickname){
         List<String> nickNamePieces = new ArrayList<>();
@@ -39,20 +22,8 @@ public class Problem6 {
         return nickNamePieces;
     }
 
-    private static boolean checkNickNameLengthException(String nickName) {
-        if (nickName.length() >= 1 && nickName.length() < 20) {
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean checkEmailException(String email) {
-        int section = email.indexOf("@") + 1;
-        String domain = email.substring(section);
-        if (!(email.length() >= 11 && email.length() < 20)) {
-            return true;
-        }
-        if (!domain.equals(EMAIL_DOMAIN)) {
+    private static boolean checkNicknameLength(String nickname){
+        if(nickname.length() < 2){
             return true;
         }
         return false;
