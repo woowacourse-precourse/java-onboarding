5697ed916a51b061f8aec37a87e10d0e52c1ad9d
paran <paran@hoon>
2022-10-31 08:55:52 +0900

    2번 : StringBuilder 사용 중복되지 않은 내용 저장

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+    	System.out.println("problem2 start");
+    	// 중복되지 않을 때까지 반복
+    	
+    	boolean isChange = true;
+    	StringBuilder crypto = new StringBuilder(cryptogram);
+    	char cryptocheck ;
+    	
+    	while(isChange) {
+    		int cnt = 0;
+	    	try {
+	    		for(int i = 0; i<cryptogram.length();i++) {
+	    			cryptocheck = crypto.charAt(i);
+	    			if(crypto.charAt(i) == crypto.charAt(i+1)) {
+	    				crypto.deleteCharAt(i+1);
+	    				crypto.deleteCharAt(i);
+	    				cnt ++;
+	    			}
+	    		}
+			} catch (Exception e) {
+				System.out.println("오류발생 " + e.getMessage());
+			}
+	    	cryptogram = crypto.toString();
+	    	
+	    	if(cnt == 0) {
+	    		isChange = false;
+	    	}
+	    }
+    	
+    	return cryptogram;
     }
+    
 }
