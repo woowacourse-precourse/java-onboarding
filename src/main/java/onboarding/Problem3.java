a8044003cb8955ac0370e146b98d7483458912ca
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-30 14:48:48 +0900

    feat: 총 쳐야하는 손뼉의 개수 구하기 (#3)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        return 0;
+        return findTotalClapsFromNum(number);
+    }
+
+    private static int findTotalClapsFromNum(int num) {
+        int clap = 0;
+        for (int i = 1; i <= num; i++) {
+            int temp = i;
+            while ( temp > 0) {
+                if (canClap(temp % 10)) {
+                    clap += 1;
+                }
+                temp /= 10;
+            }
+        }
+        return clap;
     }
 
     private static boolean canClap(int clap) {
