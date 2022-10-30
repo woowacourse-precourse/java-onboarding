4dcaa7a9059b18eb5b3f07d07237e1af03555330
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-30 21:16:19 +0900

    feat: Add Problem2 Exception Checker
    - 문제 2번 예외 체크 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        if(isException(number)) return -1;
         int answer = 0;
         return answer;
+    }
+
+    private static Boolean isException(Integer number){
+        return number < 0 || number > 10000;
     }
 }
