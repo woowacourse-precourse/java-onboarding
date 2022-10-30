6174c3f516cf9ef04c2fe26ebd5bed6fd9891cdf
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 16:38:40 +0900

    refactor(Problem2): 연속 부분 문자열 구하는 기능 분리 및 왼쪽 인덱스와 비교하는 과정이 오른쪽 인덱스 비교하는 과정과 겹치는 로직이므로 오른쪽 인덱스 비교 과정 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -28,17 +28,18 @@ public class Problem2 {
     private static void findingContinuousString(String cryptogram, Set<Integer> set) {
         for (int idx = 0; idx < cryptogram.length(); idx++) {
             int l_idx = Math.max(0, idx-1);
-            int r_idx = Math.min(cryptogram.length()-1,idx+1);
+            checkIndex(cryptogram, set, idx, l_idx);
+        }
+    }
 
-            if ((l_idx != idx) && (cryptogram.charAt(idx) == cryptogram.charAt(l_idx))) {
-                set.add(idx);
-                set.add(l_idx);
-            }
-
-            if ((r_idx != idx) && (cryptogram.charAt(idx) == cryptogram.charAt(r_idx))) {
-                set.add(idx);
-                set.add(r_idx);
-            }
+    /**
+     * Function for check Index
+     * if index == 0 then left index = 0, therefore left index must not be index
+     * */
+    private static void checkIndex(String cryptogram, Set<Integer> set, int idx, int l_idx) {
+        if ((l_idx != idx) && (cryptogram.charAt(idx) == cryptogram.charAt(l_idx))) {
+            set.add(idx);
+            set.add(l_idx);
         }
     }
 
