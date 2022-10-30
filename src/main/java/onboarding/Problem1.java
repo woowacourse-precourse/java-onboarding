07ed6c28c43c16b153fe25d4897a1cb2ad147676
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-10-30 22:03:08 +0900

    feat: set constraints

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -28,6 +28,10 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        if (!((pobi.get(0)<1) || (pobi.get(0)%2==0) || (crong.get(0)<1) || (crong.get(0)%2==0)
+                || (pobi.get(1)>400) || (pobi.get(1)%2!=0) || (crong.get(1)>400) || (crong.get(1)%2!=0)))
+            return -1;
+
         int pobi_left=compare(add(pobi.get(0)),mul(pobi.get(0)));
         int pobi_right=compare(add(pobi.get(1)),mul(pobi.get(1)));
 
