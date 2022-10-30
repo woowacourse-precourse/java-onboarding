3a0d3ff91bcf679e6885c091e33a9a3c6a70f986
최용태 <smc2315@naver.com>
2022-10-30 21:38:21 +0900

    1234sol

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1;i<= number;i++){
+            int tmp=i;
+            while(tmp>0){
+                int one = tmp%10;
+                if(one == 3 || one==6||one==9){
+                    answer++;
+                }
+                tmp/=10;
+            }
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for(int i=0;i<word.length();i++){
+            if(word.charAt(i)>='a'&&word.charAt(i)<='z'){
+               answer.append('z' - word.charAt(i) + 'a');
+            }
+        }
+        return answer.toString();
     }
 }
