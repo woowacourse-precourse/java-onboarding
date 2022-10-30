c9706d710b4497288fcfd3a2803e53988ffaef00
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 17:13:28 +0900

    feat(PR3): 주어진 숫자까지 369 카운팅 및 정답 출력 \nrefactor: 박수 횟수 계산 함수 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,7 @@ public class Problem3 {
     static int countClap(int number){
         int curNumber = number;
         int clap = 0;
-        while(curNumber >= 0){
+        while(curNumber > 0){
             if((curNumber % 10 == 3) || (curNumber % 10 == 6) || (curNumber % 10 == 9)){
                 clap++;
             }
@@ -18,6 +18,10 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1; i<=number; i++){
+            answer += countClap(i);
+        }
 
         return answer;
     }
