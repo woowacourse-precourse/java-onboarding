a4f3070861f26d2a4eb7c831386eeea39663240a
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-30 16:33:42 +0900

    feat(문제 1): 최종 점수 구하기, 승자 판단

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,29 +1,48 @@
 package onboarding;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 class Problem1 {
+    final static int ERROR = -1;
+    final static int POBI_WIN = 1;
+    final static int CRONG_WIN = 2;
+    final static int TIE = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!(validation(pobi) && validation(crong))) {
+            return ERROR;
+        }
+        int pobiScore = score(pobi);
+        int crongScore = score(crong);
+        if (pobiScore > crongScore) return POBI_WIN;
+        if (pobiScore < crongScore) return CRONG_WIN;
+        return TIE;
     }
 
-    boolean validation(List<Integer> pages) {
+    static boolean validation(List<Integer> pages) {
         if (pages.size() != 2) return false;
         if (pages.get(0) == 1 || pages.get(1) == 400) return false;
         if (pages.get(0) % 2 == 0) return false;
-        return pages.get(1) - pages.get(0) == 0;
+        return pages.get(1) - pages.get(0) == 1;
     }
 
-    int score(int page) {
+    static int score(int page) {
         int product = 1;
         int sum = 0;
-        while(page > 0) {
+        while (page > 0) {
             int curr = page % 10;
             product *= curr;
             sum += curr;
             page /= 10;
         }
         return Math.max(product, sum);
+    }
+
+    static int score(List<Integer> pages) {
+        return pages.stream()
+                .mapToInt(Problem1::score)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
