800c818995aec2488d0e1081924e94053435f5ec
Longstick0 <789456jang@naver.com>
2022-10-30 16:58:23 +0900

    refactor: Problem4 메서드명 직관화 및 리펙토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,27 +7,40 @@ public class Problem4 {
     private static final int ALPHABET_SIZE = 26;
 
     public static String solution(String word) {
-        StringBuilder answer = new StringBuilder();
         HashMap<Character, Character> reverseDictionary = new HashMap<>();
 
         makeDictionary(reverseDictionary);
 
+        return findInDictionary(word, reverseDictionary).toString();
+    }
+
+    public static StringBuilder findInDictionary(String word, HashMap<Character, Character> reverseDictionary) {
+        StringBuilder result = new StringBuilder();
         for (char c : word.toCharArray()) {
-            answer.append(reverseDictionary.get(c));
+            result.append(reverseDictionary.get(c));
         }
-        return answer.toString();
+        return result;
     }
 
     private static void makeDictionary(HashMap<Character, Character> reverseDictionary) {
+        putWordSpacing(reverseDictionary);
         int temp = 1;
-        reverseDictionary.put(' ', ' ');
+
         for (char i = 'A'; i <= 'z'; i++) {
-            if ('Z' < i && i < 'a') {
+            if (isNonAlphabet(i)) {
                 temp = 1;
                 continue;
             }
             reverseDictionary.put(i, (char) (i + (ALPHABET_SIZE - temp)));
             temp += 2;
         }
+    }
+
+    private static boolean isNonAlphabet(char i) {
+        return 'Z' < i && i < 'a';
+    }
+
+    private static void putWordSpacing(HashMap<Character, Character> reverseDictionary) {
+        reverseDictionary.put(' ', ' ');
     }
 }
