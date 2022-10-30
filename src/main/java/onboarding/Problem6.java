737c06dbe76d37325af064620f72db3d980555c4
June <68291395+june-777@users.noreply.github.com>
2022-10-30 16:44:37 +0900

    feat: makeIdAndNikNameForm 기능 구현
    기존의 "이메일", "닉네임"` form에서 "이메일 아이디", "닉네임" form 형식의 이차원배열 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,24 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+    	
+    	String[][] idAndNikNamesForm = new String[forms.size()][2];
+    	makeIdAndNikNameForm(idAndNikNamesForm, forms);
+    	
+    	List<String> answer = null;
+    	
         return answer;
+    }
+	public static void makeIdAndNikNameForm(String[][] newForms, List<List<String>> forms) {
+    	String[] iDAndEmailForm = null;
+    	String nikName = null;
+    	int idx = 0;
+    	for(List<String> form: forms) {
+    		iDAndEmailForm = form.get(0).split("@");
+    		nikName = form.get(1);
+    		newForms[idx][0] = iDAndEmailForm[0];
+    		newForms[idx][1] = nikName;
+    		idx += 1;
+    	}
     }
 }
