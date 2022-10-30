02a70a33194fab0dde12f0c0c9af82ec1a07f649
apptie <appti@naver.com>
2022-10-31 02:42:00 +0900

    comment: 문제 4 클래스 주석 추가

--- src/main/java/onboarding/problem4/WordCalculator.java
+++ src/main/java/onboarding/problem4/WordCalculator.java
@@ -8,11 +8,25 @@ import static onboarding.problem4.consts.AlphabetConst.UPPER_CASE_CHAR_A;
 import static onboarding.problem4.consts.AlphabetConst.UPPER_CASE_CHAR_Z;
 import static onboarding.problem4.consts.WordConst.WORD_START_INDEX;
 
+/**
+ * 단어를 변경하는 기능을 제공하는 클래스
+ */
 public class WordCalculator {
 
+    /**
+     * new 연산자를 통한 생성을 방지하기 위한 private 기본 생성자
+     */
     private WordCalculator() {
     }
 
+    /**
+     * 문자열에서 알파벳만을 규칙에 따라 변경하는 메소드
+     *
+     * @param word 변경할 문자열
+     * @return 변경한 문자열
+     * @see #changeCharacter(char)
+     * @see onboarding.problem4.consts.WordConst
+     */
     public static String calculateWord(String word) {
         char[] wordCharacters = word.toCharArray();
 
@@ -22,6 +36,13 @@ public class WordCalculator {
         return String.valueOf(wordCharacters);
     }
 
+    /**
+     * 대문자, 소문자를 판별해 문자를 변경하는 메소드
+     *
+     * @param target 변경할 문자
+     * @return 변경한 문자
+     * @see #calculateCharacter(char, char, char)
+     */
     private static char changeCharacter(char target) {
         if (!isAlphabet(target)) {
             return target;
@@ -32,6 +53,14 @@ public class WordCalculator {
         return calculateCharacter(LOWER_CASE_CHAR_A, LOWER_CASE_CHAR_Z, target);
     }
 
+    /**
+     * 주어진 문자를 변경하는 메소드
+     *
+     * @param standardA 알파벳 A, a
+     * @param standardZ 알파벳 Z, z
+     * @param target 변경할 문자
+     * @return 변경한 문자
+     */
     private static char calculateCharacter(char standardA, char standardZ, char target) {
         return (char) (standardA + (standardZ - target));
     }
