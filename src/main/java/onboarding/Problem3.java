3d15efe56caec3e0ffc6a89db24d93db056b04fb
moonn6pence <shin421179@gmail.com>
2022-10-30 22:45:27 +0900

    feat #3 : 각 자리별 count 메소드 추가
    - count1Digits : 한 자리에서 손뼉 치는 횟수
    - count2Digits : 두 자리수에서 손뼉 치는 횟수
    - count3Digits : 세 자리수에서 손뼉 치는 횟수
    - count4Digits : 네 자리수에서 손뼉 치는 횟수
    - countClap : 10000일 경우 9999취급, 아닐 경우 그냥 셈

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,79 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = countClap(number);
+
         return answer;
     }
+
+    private static int count1Digit(int number){
+        int clap = number / 3;
+
+        return clap;
+    }
+
+    private static int count2Digits(int number){
+        int clap = 0;
+        int rest = number % 10;
+        int ten = number / 10;
+
+        clap += count1Digit(rest);
+        if (ten == 0)
+            return clap;
+
+        clap += ten * 3;
+        if (ten % 3 == 0) {
+            clap += rest + 1 + (ten / 3 - 1) * 10;
+        } else {
+            clap += ten / 3 * 10;
+        }
+
+        return clap;
+    }
+
+    private static int count3Digits(int number){
+        int clap = 0;
+        int rest = number % 100;
+        int hundred = number / 100;
+
+        clap += count2Digits(rest);
+
+        if (hundred == 0)
+            return clap;
+
+        clap += hundred * 3;
+        if (hundred % 3 == 0) {
+            clap += rest + 1 + (hundred / 3 - 1) * 100;
+        } else {
+            clap += hundred / 3 * 100;
+        }
+
+        return clap;
+    }
+    private static int count4Digits(int number){
+        int clap = 0;
+        int rest = number % 1000;
+        int thousand = number / 1000;
+
+        clap += count3Digits(rest);
+
+        if (thousand == 0)
+            return clap;
+
+        if (thousand % 3 == 0) {
+            clap += rest + 1 + (thousand / 3 - 1) * 100;
+        } else {
+            clap += thousand / 3 * 100;
+        }
+
+        return clap;
+    }
+
+    private static int countClap(int number) {
+        if(number == 10000)
+            return count4Digits(9999);
+
+        return count4Digits(number);
+    }
+
 }
