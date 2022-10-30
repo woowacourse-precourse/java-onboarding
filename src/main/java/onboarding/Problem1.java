1bc9b5e0716aa08f829c9411a6ba7f7828f2b457
Woojin Lee <dhkep03@gmail.com>
2022-10-30 12:04:58 +0900

    p1-feat: 숫자를 각 자리수의 리스트로 변환하는 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,11 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -25,6 +28,12 @@ class Problem1 {
     static int sumEveryDigits(int num) {
         List<Integer> digitList = splitDigits(num);
         int result = digitList.stream().mapToInt(Integer::intValue).sum();
+        return result;
+    }
+
+    static List<Integer> splitDigits(int num) {
+        int[] arrayOfNum = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+        List<Integer> result = Arrays.stream(arrayOfNum).boxed().collect(Collectors.toList());
         return result;
     }
 }
