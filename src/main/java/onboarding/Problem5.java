702df1d9a3fd246ba58f683a8743d25099f6259a
ONE <sodlapdlf13@gmail.com>
2022-10-30 18:24:22 +0900

    feat: [Problem5] 주어진 금액에 대한 화폐에 개수를 리스트로 반환하는 로직 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -9,8 +9,15 @@ import java.util.List;
 */
 
 public class Problem5 {
+    private static final List<Integer> units = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int unit : units) {
+            answer.add(money / unit);
+            money %= unit;
+        }
+
         return answer;
     }
 }
