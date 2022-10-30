bd1682950ec8fd041ee460ea61ab8e37b5c37034
kimminsu <xjfcnfw3@naver.com>
2022-10-30 20:57:06 +0900

    feat(문제7): friends의 원소에 user의 친구의 존재 확인 기능 구현

--- src/main/java/onboarding/problem7/FriendSystem.java
+++ src/main/java/onboarding/problem7/FriendSystem.java
@@ -3,6 +3,8 @@ package onboarding.problem7;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.TreeMap;
 
 public class FriendSystem {
     private static final int MAX_FAIR_INDEX = 1;
@@ -22,6 +24,26 @@ public class FriendSystem {
         if (friendPair.contains(user)) {
             int userIdx = friendPair.indexOf(user);
             friends.add(friendPair.get(MAX_FAIR_INDEX-userIdx));
+        }
+    }
+
+    public void findOtherFriend(List<List<String>> friendList){
+        for (List<String> friendPair : friendList) {
+            checkFriendPair(friendPair);
+        }
+    }
+
+    private void checkFriendPair(List<String> friendPair) {
+        for (int idx = 0; idx <= MAX_FAIR_INDEX; idx++) {
+            String friend = friendPair.get(idx);
+            checkIsFriendOfFriend(friend, idx, friendPair);
+        }
+    }
+
+    private void checkIsFriendOfFriend(String friend, int idx, List<String> friendPair) {
+        if (friends.contains(friend)) {
+            int otherFriendIdx = MAX_FAIR_INDEX-idx;
+            String otherFriend = friendPair.get(otherFriendIdx);
         }
     }
 }
