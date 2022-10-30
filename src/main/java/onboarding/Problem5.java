fafefec1efe43c518397a2167d119c8e56e76e41
heejeong <hjx66@naver.com>
2022-10-30 16:36:21 +0900

    feat: 문제 5 화폐 갯수 구하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> result = new ArrayList<>();
+        List<Integer> moneyUnit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        for (int unit: moneyUnit) {
+            result.add(money/unit);
+            money %= unit;
+        }
+        return result;
     }
+
 }
