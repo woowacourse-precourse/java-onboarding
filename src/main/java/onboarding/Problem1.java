b2bd99f74a69af7e44ae81f039196bfdd91e0383
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:25:01 +0900

    feat : [Problem1] 기능 사항 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 사항
+ * 1.자릿수 별 합과 곱 중 큰 값을 리턴하는 함수
+ * 2.펼친 페이지 중 큰 값을 리턴하는 함수
+ * 3.예외 사항 함수
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
