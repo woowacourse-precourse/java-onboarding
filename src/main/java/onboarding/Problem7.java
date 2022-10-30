5eabbb5a56f960e5e231870656a38bb193b0a401
ycopp9 <aciees95@gmail.com>
2022-10-30 19:19:41 +0900

    feat: add the class Persons() that has Map<String, Person> personMap

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,10 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Problem7 {
@@ -14,6 +16,14 @@ public class Problem7 {
             this.name = name;
             this.visit = 0;
             this.friends = new HashSet<>();
+        }
+    }
+
+    static class Persons {
+        Map<String, Person> personMap;
+
+        Persons() {
+            personMap = new HashMap<>();
         }
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
