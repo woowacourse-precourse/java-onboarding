e8933e2c5dd475363b88ea6428a005d7c263eddd
kkm5291 <kkm23125291@gmail.com>
2022-10-30 18:52:40 +0900

    Problem5 Feat : 거스름돈 enum 클래스 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    enum Money {
+        fiftyThs(50000),
+        tenThs(10000),
+        fiveThs(5000),
+        oneThs(1000),
+        fiveHnd(500),
+        oneHnd(100),
+        fifty(50),
+        ten(10),
+        one(1);
+
+        private final  int value;
+        Money(int value) {
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+
+
         return answer;
     }
 }
