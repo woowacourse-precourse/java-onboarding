f1b36bef9ece6791affcedfed4495a127346c524
Eunkeee <eunkeeeee@gmail.com>
2022-10-30 20:10:21 +0900

    [P2] refactor: change names

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,33 +4,40 @@ import java.util.Arrays;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        if(cryptogram.length() < 2){
+            return cryptogram;
+        }
+
         String[] stringArr = cryptogram.split("");
 
-        int[] delIndexArr = new int[stringArr.length];
-        for (int i = 0; i < stringArr.length -1; i++){
+        // get index of overlapping(repeated) letters
+        int[] repIndexArr = new int[stringArr.length];
+        for (int i = 0; i < stringArr.length - 1; i++){
             if(stringArr[i].equals(stringArr[i+1])){
-                delIndexArr[i] += 1;
-                delIndexArr[i+1] += 1;}}
-        System.out.println(Arrays.toString(delIndexArr));
+                repIndexArr[i] += 1;
+                repIndexArr[i+1] += 1;
+            }
+        }
 
         // define the length of new string
         int lossLen = 0;
-        for (int i =0; i<delIndexArr.length;i++){
-            if(delIndexArr[i]>0){
+        for (int delIndex: repIndexArr){
+            if(delIndex > 0){
                 lossLen += 1;
             }
         }
-        System.out.println(lossLen); // 4
 
+
+        // get new string
         int newStringLen = stringArr.length - lossLen;
         String[] newStringArr = new String[newStringLen];
         int newStringIndex = 0;
-        for (int i =0; i<delIndexArr.length;i++){
-            if(delIndexArr[i]==0){
+        for (int i = 0; i<repIndexArr.length;i++){
+            if(repIndexArr[i] == 0){
                 newStringArr[newStringIndex] = stringArr[i];
-                newStringIndex = newStringIndex + 1;
+                newStringIndex += 1;
             }}
-        System.out.println(Arrays.toString(newStringArr));
         String res = String.join("", newStringArr);
 
         if(lossLen == 0 || newStringArr.length < 2){
