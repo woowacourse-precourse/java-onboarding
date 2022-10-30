5fad7997b5c8584c4c1daddc191bba88943d23a1
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:15:34 +0900

    feat : 각 char값이 문자인지 판단 및 대,소문자에 따라 규칙에 따른 문자변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,6 +7,27 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char c;
+
+        for(int i = 0 ; i < word.length() ; i++){
+            c = word.charAt(i);
+
+            //char값이 문자인지 판단
+            if(Character.isLetter(c)){
+
+                //문자가 소문자일시
+                if(Character.isLowerCase(c)){
+                    c = (char)(122 - (int)(c) + 97);
+                }
+                // 문자가 대문자일시
+                else if(Character.isUpperCase(c)){
+                    c = (char)(90 - (int)(c) + 65);
+                }
+            }
+
+            answer += c;
+        }
+
         return answer;
     }
 }
