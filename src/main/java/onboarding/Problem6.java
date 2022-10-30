2fb3075159355cf04fe04572972e9a5bfe4414ad
chj <bluedog129@gmail.com>
2022-10-30 17:44:25 +0900

    [p6] 이차원 리스트 탐색 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = 0; j < forms.get(i).size(); j++) {
+                System.out.println(forms.get(i).get(j));
+            }
+        }
         return answer;
     }
 }
