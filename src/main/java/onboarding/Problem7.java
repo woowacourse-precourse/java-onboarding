35908329354cb0cf4985a6fe04107f4b12cae4a2
ywj9811 <ywj9811@naver.com>
2022-10-31 01:48:50 +0900

    같은 점수끼리 정렬하기 위한 리스트와 점수별로 정리해서 리스트와 리스트 사이즈를 반환하는 Map 그리고 friendScore라는 Map을 담기 위한 리스트도 생성한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,6 +9,12 @@ public class Problem7 {
         List<String> withFriends = new ArrayList<>(); //친구의 친구 리스트(사용자와 친구X) -> +10점
         List<String> visitFriends = new ArrayList<>(); //방문자 리스트 -> +1점
         Map<String, Integer> friendScore = new HashMap<>(); //친구들의 추천 점수를 저장하는 맵 생성
+        List<Map.Entry<String, Integer>> mapSort = new LinkedList<>(friendScore.entrySet());
+        //현재 정렬이 안되어있는 추천친구들(Map)이기 때문에 리스트로 담기 위해 이렇게 생성
+        List<String> sorts = new ArrayList<>();
+        //나중에 같은 점수끼리 정렬용으로 사용하기 위해 빈 리스트 생성
+        Map<List<String>, Integer> returnMap = new HashMap<>();
+        //점수별로 정리해서 반환할 Map을 받기 위해 생성
 
         for (int i = 0; i < friends.size(); i++) {
             if (user.equals(friends.get(i).get(1))) {
