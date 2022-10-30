afc835a768bc021b03ad98ae75b86073c4ea22cb
sxxgxxnz <tmddms2292@gmail.com>
2022-10-30 14:01:50 +0900

    feat : [2] 첫번째 문자는 push

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,6 +7,9 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
         Stack<String> stk = new Stack<String>();
+        String[] arr = cryptogram.split("");
+
+        stk.push(arr[0]);
         return answer;
     }
 
