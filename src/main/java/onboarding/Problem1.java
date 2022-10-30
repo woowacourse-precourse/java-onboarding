c229dd40c623f58628fa18925b106ec8d2eb2adf
sinsu <sinsu19012@gmail.com>
2022-10-30 22:51:23 +0900

    ♻Refactor: Problem1 코드 리펙토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,55 +5,57 @@ import java.util.List;
 class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (!exceptions(pobi) || !exceptions(crong)) {
+        if (exceptions(pobi) || exceptions(crong)) {
             return -1;
         }
-        int pobi_point = user_point(pobi);
-        int crong_point = user_point(crong);
-        int answer = (pobi_point > crong_point) ? 1 : ((pobi_point < crong_point) ? 2 : 0);
+        int pobiLeft = Math.max(sum_plus(pobi.get(0)), sum_multiply(pobi.get(0)));
+        int pobiRight = Math.max(sum_plus(pobi.get(1)), sum_multiply(pobi.get(1)));
+        int crongLeft = Math.max(sum_plus(crong.get(0)), sum_multiply(crong.get(0)));
+        int crongRight = Math.max(sum_plus(crong.get(1)), sum_multiply(crong.get(1)));
 
-        return answer;
+        int pobi_point = Math.max(pobiLeft, pobiRight);
+        int crong_point = Math.max(crongLeft, crongRight);
+
+        return pobi_point > crong_point ? 1 : (pobi_point < crong_point) ? 2 : 0;
     }
 
     public static boolean exceptions(List<Integer> person) {
-        if (person.get(0) > 1 && person.get(0) < 399 && person.get(1) > 1 && person.get(1) < 399) {
-            if (person.get(0) % 2 == 1 && person.get(1) % 2 == 0) {
-                if (person.get(0) + 1 == person.get(1)) {
-                    return true;
-                }
-            }
+        int left = person.get(0);
+        int right = person.get(1);
+        if (left < 1 || left > 400 || right < 1 || right > 400) {
+            return true;
         }
-        return false;
+        if (left % 2 == 0 || right % 2 == 1) {
+            return true;
+        }
+        return left + 1 != right;
     }
 
-    public static int user_point(List<Integer> pages) {
-        int left = big_int(sum_plus(pages.get(0)), sum_multiply(pages.get(0)));
-        int right = big_int(sum_plus(pages.get(1)), sum_multiply(pages.get(1)));
-
-        return big_int(left, right);
+    public static int maxPoint(List<Integer> point) {
+        int maxScore = Integer.MIN_VALUE;
+        for (Integer num : point) {
+            if (maxScore < num) {
+                maxScore = num;
+            }
+        }
+        return maxScore;
     }
 
     public static int sum_plus(int page) {
-        String s = Integer.toString(page);
-        int sum = 0;
-        for (String num : s.split("")) {
-            sum += Integer.parseInt(num);
+        int sum = 1;
+        while (page > 0) {
+            sum += page % 10;
+            page /= 10;
         }
         return sum;
     }
 
     public static int sum_multiply(int page) {
-        String s = Integer.toString(page);
         int sum = 1;
-        for (String num : s.split("")) {
-            sum *= Integer.parseInt(num);
+        while (page > 0) {
+            sum *= page % 10;
+            page /= 10;
         }
         return sum;
     }
-
-    public static int big_int(int a, int b) {
-        return a > b ? a : b;
-    }
-
-
 }
