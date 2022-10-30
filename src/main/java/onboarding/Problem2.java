e922f3e8aab6ec8dc526a233a5364965ce0899d5
desfox <desfox99@naver.com>
2022-10-30 16:45:31 +0900

    feat(Problem2): 암호문에서 연속하는 중복 문자 삭제하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,38 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    public static String deDuplicateCryptogram(String cryptogram) {
+        int i = 0;
+        int leftEnd = 0;
+        int rightStart = 0;
+
+        String deDuplicated = cryptogram;
+
+        while (true) {
+            if (deDuplicated.charAt(i) == deDuplicated.charAt(i + 1)) {
+                for (int j = i + 1; j <= deDuplicated.length() - 1; j++) {
+                    if (deDuplicated.charAt(i) == deDuplicated.charAt(j)) {
+                        leftEnd = i;
+
+                        if (j == deDuplicated.length() - 1) {
+                            rightStart = j + 1;
+                            break;
+                        }
+
+                    } else {
+                        rightStart = j;
+                        break;
+                    }
+                }
+
+                String left = deDuplicated.substring(0, leftEnd);
+                String right = deDuplicated.substring(rightStart, deDuplicated.length());
+
+                deDuplicated = left + right;
+            }
+
+            else i++;
+        }
+    }
 }
