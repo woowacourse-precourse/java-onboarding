d36d2e62fbc93f0d05d73e6d1ffde73bcfb759c7
Longstick0 <789456jang@naver.com>
2022-10-30 16:15:04 +0900

    feat: Problem4 reverse사전 만들기 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    private static final int ALPHABET_SIZE = 26;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        HashMap<Character, Character> reverseDictionary = new HashMap<>();
+
+        makeDictionary(reverseDictionary);
+
+        for (char c : word.toCharArray()) {
+            answer.append(reverseDictionary.get(c));
+        }
+        return answer.toString();
+    }
+
+    private static void makeDictionary(HashMap<Character, Character> reverseDictionary) {
+        int temp = 1;
+        reverseDictionary.put(' ', ' ');
+        for (char i = 'A'; i <= 'z'; i++) {
+            if ('Z' < i && i < 'a') {
+                temp = 1;
+                continue;
+            }
+            reverseDictionary.put(i, (char) (i + (ALPHABET_SIZE - temp)));
+            temp += 2;
+        }
     }
 }
