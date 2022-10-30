7ac47c5cd9857c6ea78685cd424004023038d5e9
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-30 19:29:41 +0900

    feat: Add Problem2 Exception Checker
    - 문제 2번 예외 체크 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        if(isException(cryptogram)) throw new RuntimeException("잘못된 인자입니다.");
         String answer = "answer";
         return answer;
+    }
+
+    private static Boolean isException(String str) {
+        if(str.length() < 1 || str.length() > 1000) return false;
+        return Pattern.matches("[a-z]*", str);
     }
 }
