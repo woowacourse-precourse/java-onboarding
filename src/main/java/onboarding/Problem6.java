c8ce862911954bb6ef94b644d0fd4fbb23fad48b
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-30 19:29:30 +0900

    [feat] Problem6 크루의 닉네임을 두 글자 씩 분할

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<String> sepNames = new ArrayList<>();
+
+
+        for (List<String> form : forms) {
+            String name = form.get(1);
+            for(int i = 0;i<name.length()-1;i++)
+            {
+                sepNames.add(name.substring(i,i+2));
+            }
+
+        }
+  
         return answer;
     }
+
+
+
+
 }
