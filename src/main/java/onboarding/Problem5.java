2affd0fbb9b9d40e8f8d69cf308926e7c85b62e0
CordHouse <qkqktl5310@gmail.com>
2022-10-30 16:02:48 +0900

    docs : Problem5 기능 목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,7 +2,10 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+/*
+    기능 목록
+    1. 주어진 금액에 대한 변수를 초기값으로 잡고, 나머지와 나누기 연산으로 반환되야 하는 금액의 수를 리턴 한다.
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
