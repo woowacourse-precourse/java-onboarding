3ed192edf7a8b14f3a6cc0cce0db58b6d7203737
Daeeun Ko <7273322@naver.com>
2022-10-30 12:46:57 +0900

    feat: validate if a page number is out of bounds

---
+++ src/main/java/onboarding/problem1/Page.java
@@ -0,0 +1,23 @@
+package onboarding.problem1;
+
+public class Page {
+
+    private final int MIN_PAGE = 2;
+    private final int MAX_PAGE = 399;
+
+    private int number;
+
+    public Page(int number) {
+        validatePageNumber(number);
+        this.number = number;
+    }
+
+    private void validatePageNumber(int number) {
+        if (isOutOfBounds(number)) throw new IllegalArgumentException("Page number is out of bound");
+    }
+
+    private boolean isOutOfBounds(int number) {
+        return number < MIN_PAGE || number > MAX_PAGE;
+    }
+
+}
