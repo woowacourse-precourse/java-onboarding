615253393a0b1f393f2e02fba99080a888c4cadb
chaewon121 <80631952+chaewon121@users.noreply.github.com>
2022-10-31 06:21:05 +0900

    feat(problem6): 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록을 구현하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,40 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<String> getDuplicateEmail(String[][] forms){
+
+        HashSet<String> emails = new HashSet<>();
+        Map<String, String > nickNameMap = new HashMap<>();
+        for (int i = 0; i < forms.length; i++) {
+            String targetName = forms[i][1];
+            String targetEmail= forms[i][0];
+            if(targetName.length() < 2){
+                break;
+            }
+            for (int j = 0; j < targetName.length() - 1; j++) {
+                String key = targetName.substring(j, j+2);
+                if(nickNameMap.containsKey(key)){
+                    String email = nickNameMap.get(key);
+                    emails.add(email);
+                    emails.add(targetEmail);
+                }
+                nickNameMap.put(key, targetEmail);
+            }
+        }
+        List<String> result = emails.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        return result;
     }
 }
