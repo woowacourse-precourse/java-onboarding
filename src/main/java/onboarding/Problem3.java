0b6f0b185041283542d9e754eef34d789c35c5b9
dsaf2007 <dsaf2007@naver.com>
2022-10-30 22:36:03 +0900

    Problem3 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String temp;
+
+        for(int i = 0; i <= number; i++)
+        {
+            temp = Integer.toString(i);
+            for(int j = 0; j < temp.length(); j++)
+            {
+                if(temp.charAt(j) == '3' || temp.charAt(j) == '6' || temp.charAt(j) == '9')
+                {
+                    answer ++;
+                }
+            }
+          
+        }
         return answer;
     }
 }
