e508a315dc64c62ff20afb4f7a9c92d58d991c2b
thdwoqor <celebarte01@gmail.com>
2022-10-31 01:14:23 +0900

    refactor: 문제2 코드 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,23 +1,38 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Problem2 {
     public static String decoding(String cryptogram) {
-        StringBuilder result = new StringBuilder(cryptogram);
-        for (int i = 0; i < cryptogram.length()-1; i++) {
-            char left = cryptogram.charAt(i);
-            char right = cryptogram.charAt(i+1);
+        if (cryptogram.length()<2) return cryptogram;
 
-            if (left==right){
-                result=result.deleteCharAt(i+1);
-                result=result.deleteCharAt(i);
-                return decoding(result.toString());
-            }
-        }
-        return result.toString();
+        String[] word = cryptogram.split("");
+        List<String> l = IntStream.range(0, word.length)
+                .filter(i -> {
+                    boolean result = true;
+                    if(i>0)
+                        result = (word[i-1].equals(word[i]))?false:result;
+                    if (i<word.length-1)
+                        result = (word[i+1].equals(word[i]))?false:result;
+                    return result;
+                })
+                .mapToObj(i -> word[i])
+                .collect(Collectors.toList());
+        return String.join("", l);
     }
 
     public static String solution(String cryptogram) {
-        String answer = decoding(cryptogram);
-        return answer;
+        String before = null;
+        String after =cryptogram;
+        do {
+            before=after;
+            after=decoding(before);
+        } while (!after.equals(before));
+
+        return after;
     }
 }
