40faee976875630b3fb74fff5c5f1d065cfe730b
im-shung <seoyoung1113@gmail.com>
2022-10-30 15:01:12 +0900

    :sparkles: 돈을 큰 금액의 화폐 위주로 변환 기능 구현 #5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    private final static List<Integer> moneyUnitList = List.of(
+            50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1
+    );
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        /* [요구] 돈을 큰 금액의 화폐 위주로 변환한다. */
+        return reverMoneyToUnit(money);
+    }
+
+    // 돈을 큰 금액의 화폐 위주로 변환하는 함수
+    public static List<Integer> reverMoneyToUnit(int money) {
+        // 금액이 큰 순서대로 리스트/배열에 담기
+        List<Integer> answer = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0));
+        int index = 0;
+        for (int unit : moneyUnitList) {
+            if (money >= unit) {
+                answer.set(index, (int) money / unit);
+                money -= ((int) money / unit) * unit;
+            }
+            index++;
+        }
         return answer;
     }
 }
