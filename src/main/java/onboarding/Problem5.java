d77d38afc9a2498954699eae215291c501b772b2
김태용 <amico741@naver.com>
2022-10-30 21:50:40 +0900

    feat: 지갑을 최대한 가볍게 만드는 메서드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,21 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
     static class Money{
-        private int amount;
+        private final int amount;
         private int count;
         Money(int amount, int count){
             this.amount = amount;
             this.count = count;
         }
+
+        public int getAmount(){
+            return this.amount;
+        }
+
+        public void setCount(int count){
+            this.count = count;
+        }
     }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new LinkedList<>();
         LinkedList<Money> wallet = getWallet();
+        setMoneyCount(wallet, money);
+        for(Money object: wallet){
+            answer.add(object.count);
+        }
         return answer;
     }
     public static LinkedList<Money> getWallet(){
@@ -30,5 +42,12 @@ public class Problem5 {
         wallet.add(new Money(10, 0));
         wallet.add(new Money(1, 0));
         return wallet;
+    }
+
+    public static void setMoneyCount(LinkedList<Money> wallet, int money){
+        for (Money object : wallet) {
+            object.setCount(Math.floorDiv(money, object.getAmount()));
+            money = Math.floorMod(money, object.getAmount());
+        }
     }
 }
