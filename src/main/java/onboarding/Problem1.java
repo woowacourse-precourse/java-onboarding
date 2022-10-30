52815c1268f9c98ea228a52b76c6b03de957336c
LeeJinHo <hosboy93@naver.com>
2022-10-30 17:31:31 +0900

    문제1. pobi, crong 승부 예외사항

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,12 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         
+        if(pobi.get(1)-pobi.get(0)!=1 || crong.get(1)-crong.get(0)!=1) {
+        	answer = -1;
+        	System.out.println(answer);
+        	return answer;
+        }
+        
         int pobiMax = 0;
         int crongMax = 0;
         
