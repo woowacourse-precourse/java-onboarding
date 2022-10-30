0dd7b7089207f926de64321266bdd6513dd65000
seonho <ysh10216@gmail.com>
2022-10-30 20:56:13 +0900

    solve step

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        // 1. 예외처리 : 크루 인원이 1~10000 사이인지 체크하기.
+        // 이메일 : 11자이상 20자 미만 체크하기.
+        // email.com 도메인 제한 체크하기.
+        // 닉네임은 한글만 가능. 길이는 1~20 사이 체크하기.
+        // result는 문자열을 오름차순으로 정렬
+        // 2. 이름중복을 체크하는 함수 만들기.
         return answer;
     }
 }
