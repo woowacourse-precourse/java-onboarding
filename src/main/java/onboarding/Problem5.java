e65448219f810c2272e4b10e856f30c39285afa9
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:55:57 +0900

    feat : [Problem5] 기능 사항 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 사항
+ * 1. 돈의 단위를 배열로 저장
+ * 2. 금액을 배열로 나눠 리스트에 저장하는 함수
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
