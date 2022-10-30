0704f1713c688a8e8326564a2ca24e060c7e0650
seonho <ysh10216@gmail.com>
2022-10-30 17:42:04 +0900

    solved step

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,8 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        // 1. 예외처리 : 1~10000 사이의 숫자인지 체크
+        // 2. 받은 숫자가 들어가는 횟수 체크하는 함수 만들기.
         return answer;
     }
 }
