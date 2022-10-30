feff7ea86c44b1d1c7695a2b4aea338315acb946
Torres-09 <dla612@naver.com>
2022-10-31 02:00:42 +0900

    feat : 문제 1 기능요구사항 완료
    myAdd, myMultiply
    페이지 예외사항 : 왼쪽 페이지와 오른쪽 페이지가 연속된 페이지가 아닌 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,39 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int myMultiply(int n) {
+        int result = 1;
+        while (n != 0) {
+            result = result * (n % 10);
+            n /= 10;
+        }
+        return result;
+    }
+
+    public static int myAdd(int n) {
+        int result = 0;
+        while (n != 0) {
+            result = result + (n % 10);
+            n /= 10;
+        }
+        return result;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (Math.abs(pobi.get(0) - pobi.get(1)) != 1 || Math.abs(crong.get(0) - crong.get(1)) != 1) {
+            return -1;
+        } else {
+            int pobiMax = Math.max(Math.max(myMultiply(pobi.get(0)), myAdd(pobi.get(1))),
+                    Math.max(myMultiply(pobi.get(1)), myAdd(pobi.get(1))));
+            int crongMax = Math.max(Math.max(myMultiply(crong.get(0)), myAdd(crong.get(1))),
+                    Math.max(myMultiply(crong.get(1)), myAdd(crong.get(1))));
+
+            if (pobiMax > crongMax)
+                return 1;
+            else if (pobiMax < crongMax)
+                return 2;
+            else
+                return 0;
+        }
     }
 }
