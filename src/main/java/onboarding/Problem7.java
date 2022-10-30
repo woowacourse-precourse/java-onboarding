9d7fb1673d8f421ad2775931da7ee2449107aedf
beombu <sunil1369@bible.ac.kr>
2022-10-31 04:35:25 +0900

    Feat(P7) : 자료구조 & 기능목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> result = new ArrayList<>();//추천 결과
+        TreeSet<String> friendSet = new TreeSet<>();//user의 친구 리스트
+        HashMap<String, Integer> recommendScore = new HashMap<>();//key : 친구의 친구, value : 점수
+
+
+        return result;
     }
+
+    /**
+     * user의 친구 목록을 구하는 기능
+     */
+
+    /**
+     * user와 함께 아는 친구 점수 계산 기능
+     */
+
+    /**
+     * user의 타임 라인에 방문한 사람 점수 계산 기능
+     */
 }
