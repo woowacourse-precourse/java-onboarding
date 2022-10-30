5a4136ef6aac313314a0088520c21f8859c107bb
sxxgxxnz <tmddms2292@gmail.com>
2022-10-30 18:49:35 +0900

    feat : Problem4[1] map에 알파벳 대소문자를 각각 반대로 변환하여 저장한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        Map<Character,Character> map = new HashMap<>();
+        int alphaLength=26;
+        int upperA=65,upperZ=90;
+        int lowerA=97,lowerZ=122;
+        //initAlpha
+        for(int i=0;i<alphaLength;i++){
+            map.put((char)(upperA + i),(char)(upperZ-i));
+            map.put((char)(lowerA+i),(char)(lowerZ-i));
+        }
         return answer;
     }
 }
