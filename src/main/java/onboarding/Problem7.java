8a3074e83d94f21ca2102240553e5108209d3161
pushedrumex <pushedrumex@naver.com>
2022-10-30 20:44:58 +0900

    problem7: 사용자와 함께 아는 친구 점수를 계산하는 함수(setScoreOfKnown, addScore)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,11 +3,13 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
-    HashMap<String, Integer> scoreMap = new HashMap<>();
+    static HashMap<String, Integer> scoreMap = new HashMap<>();
     static HashMap<String, Boolean> userFriendMap = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        userFriendMap.put(user, true);
         recordUserFriends(user, friends);
+        setScoreOfKnown(user, friends);
         return answer;
     }
     static void recordUserFriends(String user, List<List<String>> friends) {
@@ -21,8 +23,23 @@ public class Problem7 {
             }
         }
     }
-    static void setScoreOfKnown(String user, List userFriends, List<List<String>> friends) {
-
+    static void setScoreOfKnown(String user, List<List<String>> friends) {
+        for (List friend: friends) {
+            String A = (String) friend.get(0);
+            String B = (String) friend.get(1);
+            if (userFriendMap.containsKey(A) && !userFriendMap.containsKey(B)) {
+                addScore(10, B);
+            } else if (!userFriendMap.containsKey(A) && userFriendMap.containsKey(B)) {
+                addScore(10, A);
+            }
+        }
+    }
+    static void addScore(int Score, String friend) {
+        if (scoreMap.containsKey(friend)) {
+            scoreMap.put(friend, scoreMap.get(friend) + 10);
+        } else {
+            scoreMap.put(friend, 10);
+        }
     }
     static void setScoreOfVisit(String user, List<String> visitors) {
 
@@ -30,9 +47,14 @@ public class Problem7 {
     public static void main(String args[]) {
         List<List<String>> friends = new ArrayList<>(new ArrayList<>());
         friends.add(new ArrayList<>(Arrays.asList(new String[]{"donut", "andole"})));
+        friends.add(new ArrayList<>(Arrays.asList(new String[]{"donut", "jun"})));
         friends.add(new ArrayList<>(Arrays.asList(new String[]{"donut", "mrko"})));
+        friends.add(new ArrayList<>(Arrays.asList(new String[]{"shakevan", "andole"})));
+        friends.add(new ArrayList<>(Arrays.asList(new String[]{"shakevan", "jun"})));
         friends.add(new ArrayList<>(Arrays.asList(new String[]{"shakevan", "mrko"})));
+        userFriendMap.put("mrko", true);
         recordUserFriends("mrko", friends);
-        System.out.println(userFriendMap);
+        setScoreOfKnown("mrko", friends);
+        System.out.println(scoreMap);
     }
 }
