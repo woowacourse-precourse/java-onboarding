69047f028a9f6a5e982853dde9d4d1c3a832e6d3
JungHun Jang <ijj5258@naver.com>
2022-10-30 23:09:37 +0900

    Problem3 (feat) 박수를 몇번쳐야하는지 세는 countClap 메소드 정의 후 풀이완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -11,9 +11,24 @@ public class Problem3 {
 		clapper.add(9);
 		return clapper;
 	}
+	private static int countClap(int x,List<Integer> clapper) {
+		int clap=0;
+		while(x>0) {
+			int isClap=x%10;
+			if(clapper.contains(isClap)) {
+				clap++;
+			}
+			x /= 10;
+		}
+		return clap;
+	}
     public static int solution(int number) {
         int answer = 0;
         List<Integer> clapper= getClapNumber();
+        
+        for(int i=1;i<=number;i++) {
+			answer += countClap(i,clapper);
+		}
         
         return answer;
     }
