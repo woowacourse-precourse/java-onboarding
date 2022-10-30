521d68a3d847b19c7e98c6a9bfa3563bd5c7b94e
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-31 03:02:46 +0900

    feat(문제 5): 제한사항 확인 및 동전 개수 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        if (!validation(money)) return Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(9);
+        List<Integer> coins = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        for (int coin : coins) {
+            answer.add(money / coin);
+            money %= coin;
+        }
         return answer;
+    }
+
+    static boolean validation(int money) {
+        return money > 0 && money <= 1000000;
     }
 }
