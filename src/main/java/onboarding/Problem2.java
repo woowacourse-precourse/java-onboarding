4e5d9c936799e44464b2a39735e8e4b8ffdd8893
JungYoonshin <sju08227@naver.com>
2022-10-30 18:03:45 +0900

    refactor: #problem2 -> 연속하는 문자 중복을 제거하는 기능의 시간복잡도 향상시킴

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,29 +12,30 @@ public class Problem2 {
 
     public static String removeDuplicateAlphabet(String cryptogram) {
         StringBuffer str = new StringBuffer(cryptogram);
-        int index= 0;
+        int index = 0;
         int duplicateLength = 1;
         boolean flag = true;
 
-        while(flag){
-            flag = false;
-            for (int i = 0; i < str.length() - 1; i++) {
-                char startChar = str.charAt(i);
-                if (startChar == str.charAt(i + 1)) {
-                    flag = true;
-                    for (int j = i; j < str.length()-1; j++) {
-                        if (startChar == str.charAt(j+1)) {
-                            duplicateLength += 1;
-                        } else{
-                            break;
-                        }
+        for (int i = 0; i < str.length() - 1; i++) {
+            char startChar = str.charAt(i);
+            if (startChar == str.charAt(i + 1)) {
+                for (int j = i; j < str.length() - 1; j++) {
+                    if (startChar == str.charAt(j + 1)) {
+                        duplicateLength += 1;
+                    } else {
+                        break;
                     }
-                    str.delete(i, i + duplicateLength);
-                    duplicateLength = 1;
-                    System.out.println(str);
                 }
+                str.delete(i, i + duplicateLength);
+                duplicateLength = 1;
+                i = i - 2;
             }
+            if (i < 0) {
+                i = -1;
+            }
+            System.out.println(str);
         }
+
         cryptogram = str.toString();
         return cryptogram;
     }
