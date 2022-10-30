5c68b90e9908be43bdd0234503a191903744d1f7
rt3310 <btac3310@gmail.com>
2022-10-30 16:52:08 +0900

    feat(Problem5): solution 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,10 +6,9 @@ import java.util.List;
 
 public class Problem5 {
     private static final List<Integer> cashList = new ArrayList<>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
-    
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return countCash(money);
     }
 
     public static List<Integer> countCash(int money) {
