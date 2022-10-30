cd98aecf27850d60c642e806677e7a5d659f5722
kimdonghun <zhfptm12@naver.com>
2022-10-31 02:26:02 +0900

    problem 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,49 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        System.out.println(answer);
-        answer = 
-        withdraw(money);
-
+        answer = withdraw(money);
         return answer;
     }
 
     private static List<Integer> withdraw(int money) {
-        List<Integer> list = Collections.emptyList();
-        list.add(100);
-        System.out.println(list);
-        System.out.println("ASD");
-
-        while (true) {
-
-            if (money >= 50000) {
-                money /= 50000;
-            } else if (money >= 10000) {
-                money /= 10000;
-            } else if (money >= 5000) {
-                money /= 5000;
-            } else if (money >= 1000) {
-                money /= 1000;
-            } else if (money >= 500) {
-                money /= 500;
-            } else if (money >= 100) {
-                money /= 100;
-            } else if (money >= 50) {
-                money /= 50;
-            } else if (money >= 10) {
-                money /= 10;
-            } else {
-                money /= 1;
-            }
-            break;
+        List<Integer> list = new LinkedList();
+        int arr[] = {50000,10000,5000,1000,500,100,50,10,1};
+        for(int i=0; i<arr.length; i++){
+            list.add(money / arr[i]);
+            money %= arr[i];
 
         }
-
         return list;
     }
 }
