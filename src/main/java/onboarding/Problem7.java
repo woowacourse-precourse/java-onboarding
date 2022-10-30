81b40f0636ce91d868624bbc825a8fbeb7fcd036
Ridealist <cjsn92@gmail.com>
2022-10-30 14:58:00 +0900

    feat/Problem7 : user_score 필드 선언 및 setVisitorScore 메소드 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
+
+    Map<String, Integer> user_score = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    void setVisitorScore(List<String> visitors) {
+        for (String visitor : visitors) {
+            if (user_score.containsKey(visitor)) {
+                user_score.put(visitor, user_score.get(visitor) + 1);
+            } else {user_score.put(visitor, 1);}
+        }
     }
 }
