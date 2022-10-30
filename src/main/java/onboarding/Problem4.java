e16a59355111190fcef200717a73ddf32fef8317
eil-you <qldirl123@gmail.com>
2022-10-30 13:14:26 +0900

    reverse :  거꾸로 출력 solution: 알파벳인지 판단 후 word 거꾸로 출력

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,55 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        //String[] letter = word.split("");
+
+        for(int i = 0; i < word.length() ; i++){
+           if(word.charAt(i)<65){
+               answer.append(word.charAt(i));
+               continue;
+           }
+           if(word.charAt(i)>=65 && word.charAt(i)<=97 ){
+               answer.append(reverse(word.charAt(i)));
+               continue;
+           }
+           if (word.charAt(i)<97){
+               answer.append(word.charAt(i));
+               continue;
+           }
+           if(word.charAt(i)<=122){
+               answer.append(reverse(word.charAt(i)));
+               continue;
+           }
+           if(word.charAt(i)>122){
+               answer.append(word.charAt(i));
+           }
+
+
+
+        }
+
+        return answer.toString();
+    }
+    public static char reverse(char word){
+        char answer = 0;
+        if( word <= 90 && word>=65){
+            for(int i = 0; i<=25 ; i++){
+                if (word == (65+i) ){
+                    answer += (char)(65+(25-i));
+                    break;
+                }
+            }
+        }
+        else if(word >=97 &&word <=122) {
+            for (int i = 0; i <= 25; i++) {
+                if (word == (97 + i)) {
+                    answer += (char) (97 + (25- i));
+                    break;
+                }
+            }
+        }
+
         return answer;
     }
 }
