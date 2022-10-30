846ca49cd3ed16cb3098d9a488a64691204683cc
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-10-30 21:55:31 +0900

    feat: add function add, function mul

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,30 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Scanner;
 
 class Problem1 {
+    public static int add(int num) {
+        int sum=0;
+        while (num != 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+    public static int mul(int num) {
+        int mul=1;
+        while (num!=0){
+            mul = mul * (num%10);
+            num=num/10;
+        }
+        return mul;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 }
