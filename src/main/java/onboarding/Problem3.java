f47d8120a31d53c13bc220d5448140f0af9d02f1
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-31 01:53:16 +0900

    feat: Create count369ByDigit | [problem 3]
    - 정수가 주어질때, 정수의 자리수마다 3, 6, 9의 갯수를 센다
        - 10으로 나눈 나머지가 3, 6, 9 중 하나인지 체크한다.
        - 0이 될때까지 10으로 나누면서 앞의 과정을 반복한다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,16 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int count369ByDigit(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                count += 1;
+            }
+            num /= 10;
+        }
+        return count;
+    }
 }
