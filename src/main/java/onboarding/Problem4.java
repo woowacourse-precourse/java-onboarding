8825bf221cada7ce69a92ae008bd6898f952da04
Jzakka <mouse4786@gmail.com>
2022-10-30 16:14:00 +0900

    Feat: "word 분석, 변환 기능 구현"

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import problem4.LengthValidator;
+import problem4.LetterConverter;
+
+import static problem4.LengthValidator.*;
+import static problem4.LetterConverter.*;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        try {
+            getMirroredMessage(word, answer);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return answer.toString();
+    }
+
+    private static void getMirroredMessage(String word, StringBuilder answer) {
+        int wordLength = lengthValidate(word);
+        for (int i = 0; i < wordLength; i++)
+            answer.append(letterConvert(word.charAt(i)));
     }
 }
