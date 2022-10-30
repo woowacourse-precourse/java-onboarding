1d229bc60468172a46b48892b41e04713aeb8815
kkirby54 <sdk926@gmail.com>
2022-10-30 17:46:41 +0900

    각 자리 더하기,곱하기 로직 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,30 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    // 페이지 번호를 받아서, 각 자리를 더한 값 반환
+    static int getDigitSum(Integer page){
+        int result = 0;
+        while (page != 0){
+            result += (page % 10);
+            page /= 10;
+        }
+        return result;
+    }
+
+    // 페이지 번호를 받아서, 각 자리를 곱한 값 반환
+    static int getDigitMul(Integer page){
+        int result = 1;
+        while (page != 0){
+            result *= (page % 10);
+            page /= 10;
+        }
+        return result;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 }
