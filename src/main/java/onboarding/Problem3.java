90f602a74cad8c39176f76d64583a564aa0ae476
hyun-seung <ardimento22@naver.com>
2022-10-30 15:22:41 +0900

    [FEAT] 문제의 제한사항에 부합하는지 확인한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,12 +1,28 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static final int START_NUMBER = 1;
+    public static final int LAST_NUMBER = 10000;
+    public static final int CHECK_RESTRICTIONS = -1;
+
     public static int solution(int number) {
         int answer = 0;
+
+        if (!verificationCheck(number)) {
+            return CHECK_RESTRICTIONS;
+        }
 
         answer = startOneToNumber(number);
 
         return answer;
+    }
+
+    public static boolean verificationCheck(int number) {
+        if (number < START_NUMBER || number > LAST_NUMBER) {
+            return false;
+        }
+        return true;
     }
 
     public static int count(int targetNumber) {
@@ -26,7 +42,7 @@ public class Problem3 {
     public static int startOneToNumber(int number) {
         int total = 0;
 
-        for (int i = 1; i <= number; i++) {
+        for (int i = START_NUMBER; i <= number; i++) {
             total += count(i);
         }
 
