c9a36a370d2c54fea1efaf9a32a8c9c0434a9aee
wugawuga <dnr3655@naver.com>
2022-10-31 02:24:56 +0900

    refactor: 검증 메소드 파라미터 하나로 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,27 +5,24 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        if (validPages(pobi, crong)) {
+        if (validPages(pobi) || validPages(crong)) {
             return -1;
         }
         return answer;
     }
 
-    private static boolean validPages(List<Integer> pobi, List<Integer> crong) {
-        if (isNotPairPages(pobi, crong)) {
+    private static boolean validPages(List<Integer> pages) {
+        if (isNotPairPages(pages)) {
             return true;
         }
-        return isFirstPageAndLastPage(pobi, crong);
+        return isFirstPageAndLastPage(pages);
     }
 
-    private static boolean isNotPairPages(List<Integer> pobi, List<Integer> crong) {
-        return pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1;
+    private static boolean isNotPairPages(List<Integer> pages) {
+        return pages.get(1) - pages.get(0) != 1;
     }
 
-    private static boolean isFirstPageAndLastPage(List<Integer> pobi, List<Integer> crong) {
-        if (pobi.contains(1) || pobi.contains(400)) {
-            return true;
-        }
-        return crong.contains(1) || crong.contains(400);
+    private static boolean isFirstPageAndLastPage(List<Integer> pages) {
+        return pages.get(0) == 1 || pages.get(1) == 400;
     }
 }
