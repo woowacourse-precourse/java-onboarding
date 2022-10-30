2616fa44904130c566c9274b44247d2d787d8ce7
Jeangyu <gjwlsrb1700@naver.com>
2022-10-31 00:34:34 +0900

    Add: Problem3 주석 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,7 +4,8 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        for (int i=1; i<=number; i++)
+        // 첫 숫자부터 확인하며 3, 6, 9가 포함되어 있는지 확인
+        for (int i=3; i<=number; i++)
             if (Integer.toString(i).contains("3") || Integer.toString(i).contains("6") || Integer.toString(i).contains("9")) {
                 answer += count_three(i);
         }
@@ -12,6 +13,11 @@ public class Problem3 {
         return answer;
     }
 
+    /**
+     *
+     * @param num 3,6,9가 포함된 숫자
+     * @return 각 자리에 있는 총 3,6,9의 개수
+     */
     public static int count_three(int num){
         int result = 0;
 
