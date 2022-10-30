d9d2d6039bdf08f353780d5193cb3b172ec354e5
jipyo0106 <jg020106@naver.com>
2022-10-30 15:42:18 +0900

    friends 리스트를 보고 직접 친구를 탐색

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> friend = filterFriendByFriendList(friends, user);
+
         return answer;
+    }
+
+    private static List<String> filterFriendByFriendList (List<List<String>> friends, String user) {
+        List<String> friendList = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                if (friend.get(0).equals(user)) {
+                    friendList.add(friend.get(1));
+                } else {
+                    friendList.add(friend.get(0));
+                }
+            }
+        }
+
+        Collections.sort(friendList);
+
+        return friendList;
     }
 }
