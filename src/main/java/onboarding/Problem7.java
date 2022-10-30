5ff4e3b46c5312adb7c62ab3c7e049de9ae9eb12
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-30 14:48:21 +0900

    comment: Problem7 구현 기능목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,15 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/**
+ * 구현 기능 목록
+ * 1. 이미 friends인 사람을 set에 담는다.
+ * 2. friends를 탐색하면서, map의 key에 친구, value에 추천점수 +10을 한다.
+ * 3. visitors를 탐색하면서, map의 key에 친구, value에 추천점수 +1을 한다.
+ * 4. 친구들과 점수가 담긴 map에서 이미 친구인 목록을 제거한다.
+ * 5. 결과인 result에 추천친구를 담는다.
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
