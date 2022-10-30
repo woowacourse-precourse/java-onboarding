0a44f2303e12ed2fa931cbcb2e486faae3728d43
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-30 18:35:06 +0900

    [ADD] 1차 조건문: 예외 사항인지 아닌지 판단

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,6 +9,19 @@ class Problem1 {
         final int POBI_WINS = 1;
         final int CRONG_WINS = 2;
 
+        List[] players = {pobi, crong};
+        for(List<Integer> player: players) {
+            int left = player.get(0);
+            int right = player.get(1);
+
+            if (right - left != 1 || right % 2 != 0 || left % 2 != 1 || right > 399 || left < 2) {
+                return WRONG_INPUT;
+            }
+        }
+
+
+
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
