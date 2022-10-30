750bbaff19d27d9ff51b7702d8527a4c0d3796d4
KimSoo <ilppang@naver.com>
2022-10-30 20:01:29 +0900

    [Problem1] 김현수 포비,크롱의 최대값 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,18 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiMaxValue = getMaxValue(pobi);
+        int crongMaxValue = getMaxValue(crong);
+
         return answer;
+    }
+    public static  int getMaxValue(List<Integer> list){
+        List<Integer> maxValue = new ArrayList<>();
+        for (Integer el: list){
+        maxValue.add(Math.max(getSumValue(el), getMultiplyValue(el)));
+
+        }
+        return (Math.max(maxValue.get(0), maxValue.get(1)));
     }
 
     public static int getSumValue(Integer number){
         return Stream.of(number.toString().split("")).mapToInt(Integer::parseInt).sum();
     }
-    public static int getmultiplyValue(Integer number) {
+    public static int getMultiplyValue(Integer number) {
         return Stream.of(number.toString().split("")).mapToInt(Integer::parseInt).reduce(1, (a,b) -> a * b);
     }
 }
