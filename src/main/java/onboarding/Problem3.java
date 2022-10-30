38a54697eff4e77dfcc3bb78a044302a5a94baef
siwookim97 <tldn12gh@gmail.com>
2022-10-30 23:31:26 +0900

    feat : problem3) 손뼉을 치는 최종 횟수를 구하는 기능 추가
    숫자를 입력받아 손뼉을 치는 최종 횟수를 구하는 기능
    1부터 number까지 최종 손뼉 횟수를 구한 후 반환한다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,15 +1,15 @@
 package onboarding;
 
 public class Problem3 {
-
     /**
      * 숫자를 입력받아 손뼉을 치는 횟수를 구하는 메서드
      *
      * @param number 입력받은 숫자
      * @return 손뼉을 치는 횟수
      */
-    static int countClap (int number) {
+    static int countClap(int number) {
         int count = 0;
+
         while (number > 0) {
             if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
                 count++;
@@ -19,6 +19,23 @@ public class Problem3 {
 
         return count;
     }
+
+    /**
+     * 손뼉을 치는 최종 횟수를 구하는 메서드
+     *
+     * @param number 입력받은 숫자
+     * @return 손뼉을 치는 최종 횟수
+     */
+    static int countTotalClap(int number) {
+        int count = 0;
+
+        for (int i = 1; i <= number; i++) {
+            count = count + countClap(i);
+        }
+
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
