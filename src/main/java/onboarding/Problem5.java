f9400d23c5484fd542cd1252da10fa84d76563a7
seonho <ysh10216@gmail.com>
2022-10-30 19:20:46 +0900

    solve step

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        // 1. 예외처리 : money가 1~1,000,000 사이인지 체크
+        // 2. 큰 금액부터 순서대로 값을 구하는 함수 만들기.
         return answer;
     }
 }
