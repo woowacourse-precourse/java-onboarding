5dcdfccfaa1f738de55b8424ad8505a4f385af9d
Isu Shin <isu010614@naver.com>
2022-10-30 21:24:21 +0900

    feat: Add function to put cryptogram in array list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
+    static ArrayList<Character> cryptogramList = new ArrayList<>();
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static void addCryptogramList(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            cryptogramList.add(cryptogram.charAt(i));
+        }
     }
 }
