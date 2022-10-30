094ee3be67faf026891678ad9df1231e6ab527ba
yxnxx <jh4rgh0@gmail.com>
2022-10-31 00:39:08 +0900

    feat: Add Problem5 changing money

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,9 +7,16 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<Integer>();
         int[] money_arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int left_money = money;
+        int count = 0;
         for (int j : money_arr) {
-            answer.add(j);
+            count = changeMoney(left_money, j);
+            answer.add(count);
+            if(count != 0) left_money -= (j * count);
         }
         return answer;
+    }
+    public static Integer changeMoney(int money, int using_money) {
+        return money / using_money;
     }
 }
