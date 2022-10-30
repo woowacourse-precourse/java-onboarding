ffc1151f401ee55b022ff851f4dc0e594a1bb0ee
thousandcows <milcamboo@gmail.com>
2022-10-30 21:08:34 +0900

    feat: created a game logic to return the answer

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
 
         // 1. Validation check
         boolean pobiCheck = validationCheck(pobi);
@@ -12,6 +11,21 @@ class Problem1 {
 
         if (!pobiCheck || !crongCheck) {
             return -1;
+        }
+
+        // 2-1. Calculate the maximum score of pobi
+        int pobiScore = Math.max(multiplyPages(pobi), addPages(pobi));
+
+        // 2-2. Calculate the maximum score of crog
+        int crongScore = Math.max(multiplyPages(crong), addPages(crong));
+
+        int answer = 0;
+
+        // 3. Find and return answer
+        if (pobiScore > crongScore) {
+            answer = 1;
+        } else if (pobiScore < crongScore) {
+            answer = 2;
         }
 
         return answer;
