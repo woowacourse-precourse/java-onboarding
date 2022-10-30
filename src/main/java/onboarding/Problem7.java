979820c69134389be677e7b00a645c26c58ebbe7
lee <whddlsk123@naver.com>
2022-10-30 17:00:58 +0900

    Problem7 userFriendList() 기능구현 완료

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,21 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    /**
+     * friends의 목록중 user와 같은 리스트에 안에 포함된 유저들의 리스트 반환
+     * @param {List<List<String>>} friends
+     * @param {String} user
+     * @return userFriendList
+     */
+    public static List<String> userFriendList(List<List<String>> friends, String user) {
+        List<String> userFriendList = new ArrayList<>();
+
+        for (int i = 0; i < friends.size(); i++) {
+            if(friends.get(i).get(0) == user) userFriendList.add(friends.get(i).get(1));
+            else if(friends.get(i).get(1) == user) userFriendList.add(friends.get(i).get(0));
+        }
+        return userFriendList;
     }
 }
---
+++ src/main/java/onboarding/Problem7_FunctionList.md
@@ -0,0 +1,11 @@
+# 기능 목록
+### 기능을 구현하기 위한 함수 목록은 다음과 같습니다.
+
+1. userFriendList(List<List<String>> friends, String user) --> List<String> userFriendList  
+- 제공된 friends에서 user와 같은 리스트에 포함된 친구들을 userFriendList에 넣은 뒤 반환합니다.
+
+
+
+
+
+# 전체 동작 과정
