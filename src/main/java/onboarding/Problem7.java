375508bdc7152a40e4adbbfc89dedf83e56540fc
sohyundoh <lyny123@naver.com>
2022-10-31 08:49:19 +0900

    Problem 7 기능 목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+기능 목록
+1. User의 친구를 찾는다.
+2. 친구 목록에 존재하는 친구들의 추천 점수를 계산한다.
+3. 방문한 횟수 * 1 만큼 방문점수 계산한다.
+4. 높은 점수 순서대로 나열한다.
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
