33fbb13663a6b57ebb6dc668bd2e40d497a341aa
SeokHwan An <asok369258@naver.com>
2022-10-30 14:49:33 +0900

    feat [Problem4] : word의 길이가 1이상 1000이하인지 검증하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static final int WORD_MIN_LENGTH = 1;
+    private static final int WORD_MAX_LENGTH = 1000;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static boolean checkWordLengthValid(String word) {
+        return WORD_MIN_LENGTH <= word.length() && word.length() <= WORD_MAX_LENGTH;
     }
 }
