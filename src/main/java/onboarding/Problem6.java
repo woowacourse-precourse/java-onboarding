a1af544408a806678a2d41a8ae3718482e8d962e
yoon <beatus96@naver.com>
2022-10-30 22:48:23 +0900

    [Problem 6] isValidEmail함수 : 이메일 형식 체크

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        return new ArrayList<>();
+    }
+
+
+    // 이메일 형식체크
+    public static boolean isValidEmail(String email) {
+        if (email.isEmpty()) return false;  //null값인 경우(빈값)
+
+        //방법 :정규식
+        String regex = "^[a-z_0-9]{1,9}(@email.com)$";  //전체 길이 제한, 도메인 형식에 맞게
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        if (m.matches()) return true;
+        return false;
     }
 }
