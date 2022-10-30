e011316f58cec10ac389e9d49531bc5d0493c6df
sdsr <dlxodnr1212@naver.com>
2022-10-31 00:33:04 +0900

    problem5 기능 1 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = count_money(money);
+
         return answer;
+    }
+    public static List<Integer> count_money(int money){
+        int count = 0;
+        List<Integer> moneylist = new ArrayList<Integer>(9);
+        int amount[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for(int i=0; i<amount.length; i++) {
+            moneylist.add(money / amount[i]);
+            money %= amount[i];
+        }
+
+        return moneylist;
     }
 }
