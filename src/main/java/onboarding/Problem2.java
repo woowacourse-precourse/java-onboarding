2cc1180d61e05574a87f59229e436058bbd5d11c
jisoolee7101@gmail.com <phllo154@naver.com>
2022-10-30 23:45:18 +0900

    While 반복 안에서 for문으로 중복 체크 전 세팅

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static void main(String[] args) {
+        String testStr = "browoanoommnaon";
+
+        System.out.println(solution(testStr));
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
+        String cryptoCleared = "";
+        boolean flagDupli = true;
+        int idxCrypto = 0;
+
+        while(flagDupli) {
+            for(int i = idxCrypto; i > cryptogram.length(); i++){
+
+            }
+        }
+
+
         return answer;
     }
 }
