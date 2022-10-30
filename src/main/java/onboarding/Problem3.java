f476c4ac6eee5fcf598f50590260e0a8f69c6e68
JH-web115 <kimjjong321@naver.com>
2022-10-30 10:15:25 +0900

    P3.더 빠르게 풀이 수정
    기존에 string에 모든 숫자를 추가하는 것이 더 메모리 용량도 많이 차지하리라 생각돼 바로 answer에 추가하는 방식으로 변경
    실행결과 속도도 350ms정도였던 기존 코드에서 230ms대로 줄어들었다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,12 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        String str_number = "";
 
         for(int i = 1; i <= number; i++){
-            str_number += String.valueOf(i);
+            answer += (int)String.valueOf(i).chars().filter(ch -> ch == '3' || ch == '6' || ch == '9').count();
         }
-        answer = (int)str_number.chars().filter(ch -> ch == '3' || ch == '6' || ch == '9').count();
 
         return answer;
     }
