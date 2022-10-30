470a8f115a1d9f39e6349619ce06666f9ec6bac6
seungmin-park <tmddudals369@naver.com>
2022-10-31 00:36:30 +0900

    문제 2번 '연속하는 중복 문자 제거하기' 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        StringBuilder sb = new StringBuilder(cryptogram);
+        boolean flag = false;
+        while (!flag){
+            flag = true;
+            for (int i =0; i < sb.length()-1; i++){
+                if (sb.charAt(i) == sb.charAt(i+1)){
+                    sb.delete(i,i + 2);
+                    flag = false;
+                }
+            }
+        }
+        answer = sb.toString();
         return answer;
     }
 }
