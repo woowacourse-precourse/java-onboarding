cb79eb55c289f56baf044a2e063a8da9ae14efd8
ECHO <eunchan1001@gmail.com>
2022-10-30 23:16:37 +0900

    feat(Problem3): 정규식을 이용해 입력받은 문자열에서 3,6,9만 포함한 문자열 반환 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,16 +3,18 @@ package onboarding;
 import java.util.stream.IntStream;
 
 public class Problem3 {
-    public static void main(String[] args) {
-        System.out.println(solution(13));
-    }
     static String getRangedString(int n) {
         String StringNumber = IntStream.rangeClosed(1, n).mapToObj(String::valueOf).reduce("", String::concat);
         return StringNumber;
     }
 
     static int getCountOfNumbers(String s) {
-        return 0;
+        String reducedString = getReducedString(s);
+        return reducedString.length();
+    }
+
+    static String getReducedString(String s) {
+        return s.replaceAll("([^3|6|9])","");
     }
 
     public static int solution(int number) {
