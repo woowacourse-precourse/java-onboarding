62d6830fa403f530fba8f5597f6edf5b079fe3ec
dongdong-119 <85717356+dongdong-119@users.noreply.github.com>
2022-10-30 20:36:47 +0900

    Problem 3 - clapCount 메소드 구현 및 테스트 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -13,4 +13,26 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+
+    public static int clapCount(int number){
+        int count = 0;
+
+        for (int i = 1; i <= number ; i++){
+            int index = i;
+
+            // 1부터 number까지의 수의 각 자리가 3, 6, 9 중의 하나인지를 체크하고, 만약 그렇다면 count를 1씩 높인다.
+            while (index != 0){
+                int check = index % 10;
+
+                if (check == 3 || check == 6 || check == 9){
+                    count++;
+                }
+
+                index /= 10;
+            }
+        }
+
+        return count;
+    }
 }
