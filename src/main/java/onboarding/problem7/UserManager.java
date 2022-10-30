d9ab2e46cd1c6a5b8a53d07cb9ab855fc4518621
situodys <situodys@gmail.com>
2022-10-31 01:13:23 +0900

    feat(문제7): 주어진 사용자의 친구 목록과 겹치는 친구 수 계산

--- src/main/java/onboarding/problem7/UserManager.java
+++ src/main/java/onboarding/problem7/UserManager.java
@@ -1,33 +1,41 @@
 package onboarding.problem7;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class UserManager {
     private static final int FIRST_ID = 0;
     private static final int SECOND_ID = 1;
 
-    private Map<String, List<String>> friendsTable;
+    private Map<String, Set<String>> friendsTable;
 
     public UserManager(List<List<String>> friends) {
         this.friendsTable = new HashMap<>();
         initFriendsTable(friends);
     }
 
+    private int countShardFriends(String currentUser, String inputUser) {
+        int count = 0;
+        Set<String> inputUserFriendSet = friendsTable.get(inputUser);
+
+        for (String sharedFriend : friendsTable.get(currentUser)) {
+            if (inputUserFriendSet.contains(sharedFriend)) count++;
+        }
+
+        return count;
+    }
+
     private void initFriendsTable(List<List<String>> friends) {
         for (List<String> friendRelation : friends) {
             String firstUser = friendRelation.get(FIRST_ID);
             String secondUser = friendRelation.get(SECOND_ID);
-            addFriendToList(firstUser, secondUser);
-            addFriendToList(secondUser, firstUser);
+            addFriendToSet(firstUser, secondUser);
+            addFriendToSet(secondUser, firstUser);
         }
     }
 
-    private void addFriendToList(String key, String newFriend) {
+    private void addFriendToSet(String key, String newFriend) {
         if (!friendsTable.containsKey(key)) {
-            friendsTable.put(key, new ArrayList<>());
+            friendsTable.put(key, new HashSet<>());
         }
         friendsTable.get(key).add(newFriend);
     }
