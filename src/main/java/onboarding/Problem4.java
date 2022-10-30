b90080d9471a0165ae2597d351833daab09257a6
jasoncho1021 <jhcho1021@gmail.com>
2022-10-30 16:09:32 +0900

    feat(problem4): 대,소문자 구분하여 청개구리 사전 조회 및 결과 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,52 @@
 package onboarding;
 
+class FrogDictionary {
+    private static final int LOWER_CASE_ASCII_OF_A = 97;
+    private static final int LOWER_CASE_ASCII_OF_Z = 122;
+    private static final int UPPER_CASE_ASCII_OF_A = 65;
+    private static final int UPPER_CASE_ASCII_OF_Z = 90;
+
+    public static char find(char targetChar) {
+        return transformCharacter(targetChar);
+    }
+
+    private static char transformCharacter(char targetChar) {
+        if (isLowerCase(targetChar)) {
+            return reverseAlphabet(targetChar, LOWER_CASE_ASCII_OF_A + LOWER_CASE_ASCII_OF_Z);
+        }
+        if (isUpperCase(targetChar)) {
+            return reverseAlphabet(targetChar, UPPER_CASE_ASCII_OF_A + UPPER_CASE_ASCII_OF_Z);
+        }
+        return targetChar;
+    }
+
+    private static char reverseAlphabet(char targetChar, int offset) {
+        return (char) (offset - (int) targetChar);
+    }
+
+    private static boolean isUpperCase(char targetChar) {
+        if (targetChar >= UPPER_CASE_ASCII_OF_A && targetChar <= UPPER_CASE_ASCII_OF_Z) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isLowerCase(char targetChar) {
+        if (targetChar >= LOWER_CASE_ASCII_OF_A && targetChar <= LOWER_CASE_ASCII_OF_Z) {
+            return true;
+        }
+        return false;
+    }
+}
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder transformedWord = new StringBuilder();
+        char[] wordCharacters = word.toCharArray();
+        for (char targetChar : wordCharacters) {
+            transformedWord.append(FrogDictionary.find(targetChar));
+        }
+        String answer = transformedWord.toString();
         return answer;
     }
 }
