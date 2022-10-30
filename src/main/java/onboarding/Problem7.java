9d837030de097fb22a76f5eb34c1671e4b288db3
KSH <tkdgns543@gmail.com>
2022-10-31 04:35:37 +0900

    feat(Problem7): 방문자 List를 순회하면서 그 사람의 점수를 1점씩 올려주고 Map에 기록하는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -51,4 +51,10 @@ public class Problem7 {
             }
         }
     }
+    /* 방문자 List를 순회하면서 그 사람의 점수를 1점씩 올려주고 Map에 기록하는 메서드 */
+    private static void recordVisitorScore(Map<String, Integer> scores, List<String> visitors, String user) {
+        for (String visitor : visitors) {
+            incrementScore(scores, user, 1);
+        }
+    }
 }
