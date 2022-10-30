d2ef69ad20fade14d255ee5b5715c7bfc87c275e
thdwoqor <celebarte01@gmail.com>
2022-10-30 22:04:30 +0900

    refactor: 문제5 코드 리팩토링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<Integer>();
-        int[] won={50000,10000,5000,1000,500,100,50,10,1};
+        int[] won={50_000,10_000,5_000,1_000,500,100,50,10,1};
 
         for (int i : won) {
             answer.add(money/i);
