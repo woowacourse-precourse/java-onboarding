fae06605cb7763751257540b38a2d1f1317f2172
chkim <cndghks0213@gmail.com>
2022-10-30 19:58:49 +0900

    feat(problem6): 회원 목록으로 중복회원 목록을 구하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,13 +6,32 @@ import java.util.stream.Collectors;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<Crew> crews = allCrews(forms);
-        List<String> answer = List.of("answer");
-        return answer;
+        List<Crew> answer = duplicateCrews(crews);
+        return List.of("answer");
     }
 
     private static List<Crew> allCrews(List<List<String>> forms) {
         return forms.stream()
                 .map(form -> new Crew(form.get(0), form.get(1)))
                 .collect(Collectors.toList());
+    }
+
+    private static List<Crew> duplicateCrews(List<Crew> crews) {
+        return crews.stream()
+                .map(crew -> duplicateCrews(crew, crews))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Crew> duplicateCrews(Crew crew, List<Crew> crews) {
+        List<Crew> duplicateCrews = crews.stream()
+                .filter(it -> it.duplicate(crew))
+                .collect(Collectors.toList());
+
+        if (duplicateCrews.isEmpty()) {
+            return duplicateCrews;
+        }
+        duplicateCrews.add(crew);
+        return duplicateCrews;
     }
 }
