e25cbca3ceb8ae2425e4e19609c54fe26459b13b
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 17:05:56 +0900

    feat(PR3): 각 자리수 계산 및 369 카운트 함수 추가
    refactor: 자리수 체크 함수 제거

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,14 +1,23 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
 
-    static int checkNumberOfDigits(int digits){
-        return (int)(Math.log10(digits) + 1);
+    static int countClap(int number){
+        int curNumber = number;
+        int clap = 0;
+        while(curNumber >= 0){
+            if((curNumber % 10 == 3) || (curNumber % 10 == 6) || (curNumber % 10 == 9)){
+                clap++;
+            }
+            curNumber /= 10;
+        }
+        return clap;
     }
 
     public static int solution(int number) {
         int answer = 0;
-
 
         return answer;
     }
