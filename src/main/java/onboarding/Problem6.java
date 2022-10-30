5c2293ca35a3b1421f3fbcf5087ba451bed6c1b2
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-30 19:42:47 +0900

    refactor: [Problem6] - 메소드 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem6 {
@@ -12,9 +9,28 @@ public class Problem6 {
     private final static int NICKNAME = 1;
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
         Map<String, List<String>> emailToSplitNickname = extractedSplitNickname(forms);
+        return getSimilarNicknameCrewEmails(forms, emailToSplitNickname);
+    }
 
+    private static Map<String, List<String>> extractedSplitNickname(List<List<String>> forms) {
+        Map<String, List<String>> emailToSplitNickname = new LinkedHashMap<>();
+
+        for (List<String> form : forms) {
+            List<String> splitNicknames = new ArrayList<>();
+            String nickname = form.get(NICKNAME);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                splitNicknames.add(nickname.substring(i, i + 2));
+            }
+            emailToSplitNickname.put(form.get(EMAIL), splitNicknames);
+        }
+
+        return emailToSplitNickname;
+    }
+
+    private static List<String> getSimilarNicknameCrewEmails(List<List<String>> forms,
+                                                             Map<String, List<String>> emailToSplitNickname) {
+        List<String> answer = new ArrayList<>();
         int loopStartIdx = 1;
 
         for (String email : emailToSplitNickname.keySet()) {
@@ -29,27 +45,12 @@ public class Problem6 {
             }
             loopStartIdx++;
         }
-
+        
         return answer.stream()
                 .distinct()
                 .sorted()
                 .collect(Collectors.toList());
-
     }
 
-    private static Map<String, List<String>> extractedSplitNickname(List<List<String>> forms) {
-        Map<String, List<String>> emailToSplitNickname = new LinkedHashMap<>();
-
-        for (List<String> form : forms) {
-            String nickname = form.get(NICKNAME);
-            List<String> splitNicknames = new ArrayList<>();
-            for (int i = 0; i < nickname.length() - 1; i++) {
-                splitNicknames.add(nickname.substring(i, i + 2));
-            }
-            emailToSplitNickname.put(form.get(EMAIL), splitNicknames);
-        }
-
-        return emailToSplitNickname;
-    }
 
 }
