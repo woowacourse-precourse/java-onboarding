01fabf4bf9cdc1e1a983e887324e182d23767b91
kite707 <kite707707@gmail.com>
2022-10-30 21:28:38 +0900

    solve problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,31 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int getNumber(int number) {
+        int res = 0;
+        if (number < 10) {
+            if (number == 3 || number == 6 || number == 9) {
+                return 1;
+            }
+        } else {
+
+            while (number != 0) {
+                int numToCheck = number % 10;
+                if (numToCheck == 3 || numToCheck == 6 || numToCheck == 9) {
+                    res++;
+                }
+                number /= 10;
+            }
+        }
+        return res;
+    }
     public static int solution(int number) {
         int answer = 0;
+        for(int i =1; i<=number;i++){
+            answer+=getNumber(i);
+        }
+
         return answer;
     }
 }
