753bfaa6e6c6e13cad8d58232eb072db6bf5191d
CordHouse <qkqktl5310@gmail.com>
2022-10-30 16:04:50 +0900

    refactor : Add(cal) Function / Problem5 (거스름 돈)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 /*
     기능 목록
@@ -9,6 +10,22 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] cost = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        answer = cal(cost, money);
         return answer;
+    }
+
+    // Step 1. 파라미터를 입력받아 거스름 돈의 개수를 List 형태로 반환
+    // cost -> 금액권에 대한 정보
+    // money -> 주어진 돈
+    public static List<Integer> cal(int[] cost, int money){
+        List<Integer> wallet = new LinkedList<>();
+
+        for(int i = 0; i<9; i++){
+            wallet.add(money/cost[i]);
+            money %= cost[i];
+        }
+        return wallet;
     }
 }
