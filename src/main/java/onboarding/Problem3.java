ddd52a96224c467325cd40df6a32b7628d54e7c0
zszszszs <so4823@gmail.com>
2022-10-30 23:36:18 +0900

    mission3 clear

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = count(number);
         return answer;
+    }
+
+    public static int count(int num){ // 손뼉 치는 횟수를 구하는 함수
+        int ret = 0;
+        for(int i = 1; i <= num; i++){
+            int a = i;
+            while(a != 0){
+                if(a % 10 == 3 || a % 10 == 6 || a % 10 == 9){
+                    ret++;
+                }
+                a /= 10;
+            }
+        }
+        return ret;
     }
 }
