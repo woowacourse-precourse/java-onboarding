26fd5d4c8d7f2ba0e4cd8dd4324de59bddf02029
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-30 20:12:07 +0900

    [feat] Problem7 user 친구 리스트 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> friendList = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if(friend.contains(user))
+            {
+                if(friend.get(0).equals(user)) friendList.add(friend.get(1));
+                else friendList.add(friend.get(0));
+            }
+        }
+
+
+
         return answer;
     }
 }
