06f3a6195930ff34bed585d02c75c3dac2b7ff91
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-10-30 12:52:18 +0900

    feat: [Problem7] - User 객체 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,39 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    private static class User {
+
+        private String name;
+        private List<String> friends;
+        private Map<String, Integer> friendRecommendScore = new HashMap<>();
+
+        public User() {
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<String> getFriends() {
+            return friends;
+        }
+
+        public void setFriends(List<String> friends) {
+            this.friends = friends;
+        }
+
+        public Map<String, Integer> getFriendRecommendScore() {
+            return friendRecommendScore;
+        }
+
+        public void setFriendRecommendScore(Map<String, Integer> friendRecommendScore) {
+            this.friendRecommendScore = friendRecommendScore;
+        }
+    }
+
 }
