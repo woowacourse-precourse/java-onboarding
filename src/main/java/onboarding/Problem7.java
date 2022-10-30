d87e108021faccc3ac87543137ca9f925388630a
seonghoo1217 <seonghoo1217@naver.com>
2022-10-30 17:03:51 +0900

    feat : Add a Visitors List Size Limit Exception on [Task 7]

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
-        if (validation("user_length",user.length())){
-            //닉네임 길이 예외처리
+        if (validation("user_length",user.length())&&validation("friends_size",friends.size())&&validation("visitors_size",visitors.size())){
+            //닉네임 길이 예외처리&&친구 리스트 배열길이 예외처리
         }
         return answer;
     }
@@ -22,6 +22,10 @@ public class Problem7 {
         else if (err_type.equals("friends_size")&&err_param instanceof Integer){
             int friends_size = (int) err_param;
             return 0<friends_size&&friends_size<10001;
+        }
+        else if (err_type.equals("visitors_size")&&err_param instanceof Integer){
+            int visitors_size = (int) err_param;
+            return 0<visitors_size&&visitors_size<10001;
         }
 
         return false;
