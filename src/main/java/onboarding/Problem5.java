25765856a7f63dc2e67566edd350850f81be3b63
knh4437 <knh4437@naver.com>
2022-10-30 13:40:52 +0900

    Solved: Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] unitList = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
+        int change = money;
+        for (int i=0;i<unitList.length; i++) {
+            answer.add(1);
+            change = calculate(change, unitList[i], answer, i);
+        }
+
         return answer;
+    }
+
+    public static int calculate(int money, int unit, List<Integer> list, int index){
+        if (money/unit < 0)
+            list.set(index, 0);
+        else
+            list.set(index, money/unit);
+        return money%unit;
     }
 }
