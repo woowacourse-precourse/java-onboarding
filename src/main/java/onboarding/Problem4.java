955b36cb2e1dd817c45edab279b6fa5e2af5e836
poter0501 <poter0501@gmail.com>
2022-10-30 23:04:31 +0900

    feat: solution 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,18 +3,25 @@ package onboarding;
 import java.util.HashMap;
 
 public class Problem4 {
-    private static HashMap<Character, Character> frogDictionary;
-    private static String matchStr="ZYXWVUTSRQPONMLKJIHGFEDCBA";
     public static String solution(String word) {
         String answer = "";
+        for (int i=0; i<word.length();i++){
+            char current = word.charAt(i);
+            int charCode = (int)current;
+            if ((charCode>64) && (charCode<91))
+            {
+                int idx = charCode-65;
+                idx = 90 - idx;
+                current = (char) idx;
+            }
+            if ((charCode>96) && (charCode<123))
+            {
+                int idx = charCode-97;
+                idx = 122 - idx;
+                current = (char) idx;
+            }
+            answer+=current;
+        }
         return answer;
-    }
-    private static void createFrogDictionary(){
-        for (int i=65; i<91;i++){
-            frogDictionary.put((char)i, matchStr.charAt(i-65));
-        }
-        for (int i=97; i<123;i++){
-            frogDictionary.put((char)i, matchStr.charAt(i-97));
-        }
     }
 }
