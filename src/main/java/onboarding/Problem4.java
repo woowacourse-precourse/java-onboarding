a7328215def0ad350f612942439afeb877882602
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-30 22:19:32 +0900

    [ADD] 문제 4 - 필요한 문자들의 10진법 유니코드를 상수로 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int CAPITAL_A = 'A';
+    private static final int CAPITAL_Z = 'Z';
+    private static final int SMALL_A = 'a';
+    private static final int SMALL_Z = 'z';
     public static String solution(String word) {
         String answer = "";
         return answer;
