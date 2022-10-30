53c90bc7ac2b3a46ca00c57664d863bcb5b05251
cgk95 <kjkj5868@gmail.com>
2022-10-30 22:53:09 +0900

    P4 알파벳을 뒤집어서 매칭시키는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.HashMap;
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        HashMap<String,String> map=new HashMap<>();
+        myMapping(map);
+        return "";
+    }
+    /**알파벳을 각기 매핑**/
+    private static void myMapping(HashMap<String, String> map) {
+        String forward="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String backward = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        for (int i = 0; i < forward.length(); i++) {
+            map.put(String.valueOf(forward.charAt(i)), String.valueOf(backward.charAt(i))); /*매핑*/
+        }
     }
 }
