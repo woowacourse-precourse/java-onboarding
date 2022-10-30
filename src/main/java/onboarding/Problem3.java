33bb666972c15eabec7a5bbd90e54b34a960287e
hongxeob <sjun2918@naver.com>
2022-10-31 00:17:17 +0900

    3/6/9인지 확인 하는 number클래스

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
     }
 }
