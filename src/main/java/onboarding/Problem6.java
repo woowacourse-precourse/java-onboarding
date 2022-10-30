f311789f7d6f1d8b71e63d2f929a4ed763279251
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-30 23:17:40 +0900

    새로운 기능 요구사항 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,14 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    // TODO 크루의 닉네임을 가져오는 메서드
+    public static List<String> getNickname(List<List<String>> list) {
+        return null;
+    }
+
     // TODO 같은 글자가 연속적으로 포함되는 판단하는 메서드
     public static List<String> checkDuplication(List<List<String>> list) {
         return null;
