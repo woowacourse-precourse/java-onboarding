f8ad6ca5674cddfffbfd978ed8f82e1739e34a2e
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-10-30 14:18:36 +0900

    P4.문제해결
    각 문자를 아스키코드로 생각하여 반대 문자로 치환해주기
    
    ASCII 코드가 바로 떠올랐기에 손쉽게 해결할 수 있는 문제였다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); i++) {
+            int ascii_char = word.charAt(i);
+            if(ascii_char >= 97 && ascii_char <= 122) {
+                ascii_char = 122 - (ascii_char - 97);
+            }
+            else if(ascii_char >= 65 && ascii_char <= 90) {
+                ascii_char = 90 - (ascii_char - 65);
+            }
+            answer += (char)ascii_char;
+        }
         return answer;
     }
 }
