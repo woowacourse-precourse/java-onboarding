ac51106f7fd9b504f17ba5d972f75cbd585b9e85
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-10-30 20:49:09 +0900

    P2: delete same character

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem2 {
+    public static String delSame(String cryptogram, int i) {
+        if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)){
+            cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i+2);
+        }
+        return cryptogram;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
