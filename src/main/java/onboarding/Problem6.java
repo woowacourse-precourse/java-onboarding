ff500ab56a632fdc6311b5529efdef466ac95d09
kim0914 <kosa0914@naver.com>
2022-10-30 22:00:41 +0900

    feat: Problem6 상수 및 문자열 예외 발생 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,54 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+
+import static onboarding.Problem6.Const.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         return answer;
+    }
+
+    private static boolean isFormWrongSize(List<String> form){
+        return form.size() != EACH_FORM_SIZE;
+    }
+
+    private static boolean isCrewWrongSize(List<List<String>> forms){
+        int crewSize = forms.size();
+        return (crewSize < CREW_MIN_SIZE || crewSize > CREW_MAX_SIZE);
+    }
+
+    private static boolean isWrongDomain(String domain){
+        return !domain.equals(EMAIL_DOMAIN);
+    }
+
+    private static boolean isEmailWrongSize(String email){
+        int length = email.length();
+        return (length < EMAIL_MIN_LENGTH || length >= EMAIL_MAX_LENGTH);
+    }
+
+    private static boolean isNameWrongSize(String name){
+        int length = name.length();
+        return (length < NAME_MIN_LENGTH || length >= NAME_MAX_LENGTH);
+    }
+
+
+    static class Const{
+        public static final int CREW_MIN_SIZE = 1;
+        public static final int CREW_MAX_SIZE = 10000;
+
+        public static final int EACH_FORM_SIZE = 2;
+
+        public static final int EMAIL_INDEX = 0;
+        public static final int NAME_INDEX = 1;
+
+        public static final int EMAIL_MIN_LENGTH = 11;
+        public static final int EMAIL_MAX_LENGTH = 20;
+        public static final String EMAIL_DOMAIN = "email.com";
+
+        public static final int NAME_MIN_LENGTH = 1;
+        public static final int NAME_MAX_LENGTH = 20;
     }
 }
