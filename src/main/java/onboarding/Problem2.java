3e873fe33ce8bd5e9db779a404c807fbc50c7e25
cksghks89 <song960705@naver.com>
2022-10-31 04:43:05 +0900

    problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        Stack<Character> stack = new Stack<>();
+        char[] cryptogramCharArr = cryptogram.toCharArray();
+
+        for(int i = 0; i < cryptogramCharArr.length; i++){
+            if(stack.isEmpty() || stack.peek() != cryptogramCharArr[i]){
+                stack.push(cryptogramCharArr[i]);
+            }else{
+                stack.pop();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        answer = sb.toString();
+
         return answer;
     }
 }
