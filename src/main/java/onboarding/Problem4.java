599f0fe52d4467905ff626a2dc7a60d1998da0b9
1217jdk <1217jdk@naver.com>
2022-10-30 22:45:49 +0900

    feat: solve Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        // letter: 현재 보고있는 문자
+        char letter;
+        // sb: 문자열 생성을 위한 빌더
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < word.length(); i++) {
+            letter = word.charAt(i);
+            // 현재 문자가 대문자라면 반대되는 대문자로 변환
+            if(letter >= 'A' && letter <= 'Z')
+                letter = (char)('A' + 'Z' - letter);
+            // 현재 문자가 소문자라면 반대되는 소문자로 변환
+            else if(letter >= 'a' && letter <= 'z')
+                letter = (char)('a' + 'z' - letter);
+
+            sb.append(letter);
+        }
+        answer = sb.toString();
+
         return answer;
     }
 }
