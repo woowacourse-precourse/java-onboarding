b7e4cf96f7c899e41da6464388567c44a4b9b889
JungHun Jang <ijj5258@naver.com>
2022-10-30 23:08:30 +0900

    Problem3 (feat) 3,6,9를 List 객체로 생성하는 getClapNumber 메소드 정의

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
+	private static List<Integer> getClapNumber() {
+		List<Integer> clapper= new ArrayList<Integer>();
+		clapper.add(3);
+		clapper.add(6);
+		clapper.add(9);
+		return clapper;
+	}
     public static int solution(int number) {
         int answer = 0;
+        List<Integer> clapper= getClapNumber();
+        
         return answer;
     }
 }
