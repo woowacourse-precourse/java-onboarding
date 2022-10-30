17dde673dccfb2b6a203cff8a37e998ad46b9855
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-30 21:27:18 +0900

    [ADD] 문제2 - 연속적으로 중복되는 글자의 인덱스 변수로 선언

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+    private static List<Integer> getDuplicateIndexes (String cryptogram) {
+        List<Integer> indexes = new ArrayList<>();
+
+        for (int i=0; i<cryptogram.length()-1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                indexes.add(i);
+            }
+        }
+
+        return indexes;
+    }
     public static String solution(String cryptogram) {
+        List<Integer> duplicateIndexes = getDuplicateIndexes(cryptogram);
+
         String answer = "answer";
         return answer;
     }
