3678e9d602a6e1216e26463dd33713535cfa126c
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-10-30 17:06:57 +0900

    refactor: Problem1 의미전달을 위한 메서드명 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,11 +2,17 @@ package onboarding;
 
 import java.util.List;
 
+
+/**
+ * 기능 구현 목록
+ * 1) 페이지 번호를 자릿수 별로 곱해서 수를 만드는 기능 구현
+ * 2) 예외 상황처리 구현
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        if (!Exception(pobi.get(0), pobi.get(1)) || !Exception(crong.get(0), crong.get(1))) {
+        if (!FindException(pobi.get(0), pobi.get(1)) || !FindException(crong.get(0), crong.get(1))) {
             answer = -1;
         }else {
             int pobiscore = Math.max(Score(pobi.get(0)), Score(pobi.get(1)));
@@ -22,7 +28,7 @@ class Problem1 {
         return answer;
     }
 
-    public static boolean Exception(int page1, int page2){
+    public static boolean FindException(int page1, int page2){
         if(page1<=1 || page1>=399){
             return false;
         }else if(page1!=page2-1){
