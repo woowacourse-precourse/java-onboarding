5d6c6b8111812e65b41ad92cc0b364feaee2a6e2
java-saeng <gusdn85554@naver.com>
2022-10-30 20:12:33 +0900

    feat : Problem7 친구관계 맵핑하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Map<String, List<String>> relationMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            makeFriendRelation(friend, relationMap);
+        }
+
         return answer;
+    }
+
+    private static void makeFriendRelation(List<String> friend, Map<String, List<String>> relationMap) {
+        String friend1 = friend.get(0);
+        String friend2 = friend.get(1);
+
+        relationMap.computeIfAbsent(friend1, string -> new ArrayList<>());
+        relationMap.computeIfAbsent(friend2, string -> new ArrayList<>());
+
+        relationMap.get(friend1).add(friend2);
+        relationMap.get(friend2).add(friend1);
     }
 }
