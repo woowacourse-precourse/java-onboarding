723ccafe5066e8467956abd441c0c185c274255c
lseok <lseokhyun99@gmail.com>
2022-10-31 00:56:02 +0900

    문제 3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = Count369(number);
         return answer;
+    }
+    public static int Count369(int num){
+        String str369 = "";
+        int cnt = 0;
+
+        for(int i = 1 ; i <= num; i++){
+            str369 = str369 + i;
+        }
+
+        for(int i = 0 ; i < str369.length(); i++){
+            int tmp =str369.charAt(i);
+            if (tmp == '3') cnt++;
+            else if (tmp == '6') cnt++;
+            else if (tmp == '9') cnt++;
+        }
+
+        return cnt;
     }
 }
