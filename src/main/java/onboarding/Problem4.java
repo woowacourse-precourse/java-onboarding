fbd7b79dc3f162609a0cfbb9fe05d8bc5bdc6f64
knh4437 <knh4437@naver.com>
2022-10-30 11:39:48 +0900

    Solved: Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,44 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        ArrayList<String> wordList = new ArrayList<>();
+        char[] charArr = word.toCharArray();
+
+        for (int i=0;i<charArr.length;i++) {
+            char c = charArr[i];
+
+            if (c=='\0') {
+                break;
+            }
+            else {
+                int asc = (int) c;
+                char trans=0;
+                if ((asc>=65)&&(asc<=90)) {
+                    trans = (char)reverseUpper(asc);
+                }
+                else if ((asc>=97)&&(asc<=122)) {
+                    trans = (char)reverseLower(asc);
+                }
+
+                if (trans!=0)
+                    charArr[i] = trans;
+            }
+        }
+        answer = String.valueOf(charArr);
+
         return answer;
+    }
+
+    public static int reverseUpper(int asc) {
+        return 155-asc;
+    }
+
+    // 소문자일 때
+    public static int reverseLower(int asc) {
+        return 219-asc;
     }
 }
