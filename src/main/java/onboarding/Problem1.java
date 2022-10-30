3f051cddec50d348ddc6941eecbd201071f743bd
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 21:16:48 +0900

    feat: [Problem1] 각 사람이 뽑은 페이지들을 통해 큰 수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -19,6 +19,18 @@ class Problem1 {
     }
 
     /*
+    각 사람이 뽑은 페이지들을 통해 큰 수 구하는 메서드
+     */
+    private static int calculateMax(List<Integer> man){
+        int left = man.get(0).intValue();   //Integer unboxing
+        int right = man.get(1).intValue();  //Integer unboxing
+        int plus=Math.max(plusIndex(left),plusIndex(right));
+        int multi=Math.max(multiIndex(left),multiIndex(right));
+        int maxNum=Math.max(plus,multi);
+        return maxNum;
+    }
+
+    /*
     뽑은 페이지 더하기 기능
      */
     private static int plusIndex(int num){
