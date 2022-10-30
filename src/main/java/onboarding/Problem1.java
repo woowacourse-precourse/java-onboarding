3020bad16a712b48044e2c99e5faa58a6a244c1c
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-31 00:14:29 +0900

    feat: Create multipleDigit | [problem 1]
    - 각 자리수의 수끼리 곱하는 메소드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,6 +10,16 @@ class Problem1 {
         return -1;
     }
 
+    private static int multipleDigit(int num) {
+        int result = 1;
+
+        while (num > 0) {
+            result *= num % 10;
+            num /= 10;
+        }
+        return result;
+    }
+
     private static int addDigit(int num) {
         int result = 0;
 
