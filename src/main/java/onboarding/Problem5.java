6a0406977d4d4d5c8051bcad3162ce942c327cb2
KYBee <kyb3634@naver.com>
2022-10-30 14:51:59 +0900

    :art: [Problem 5] Define & Distribute Requirements
    [Problem 5] Define & Distribute Requirements

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /**
+     * 지폐 단위만큼 몇 장이 필요한지 리턴하는 메서드이다.
+     *
+     * @param : int, 금액
+     * @return : List<Integer>, 필요한 지폐의 장수를 담고 있는 리스트
+     */
     public static List<Integer> solution(int money) {
+        List<Integer> answer = Collections.emptyList();
+        return answer;
+    }
+
+    /**
+     * 지폐 몇 장이 필요한지 계산하여 리턴하는 메서드이다.
+     *
+     * @param : int, 금액
+     * @return : List<Integer>, 필요한 지폐의 장수를 담고 있는 리스트
+     */
+    public static List<Integer> getChange(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
