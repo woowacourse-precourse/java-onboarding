8e93ed59b8ce453264eae3c886e67c7700ecfa64
siwookim97 <tldn12gh@gmail.com>
2022-10-31 04:07:04 +0900

    feat : problem6) 이메일을 TreeSet에 추가하는 기능 추가
    중복 문자열을 지닌 닉네임의 이메일을
    전역변수 TreeSet에 추가하는 기능
    문자열 email을 입력받아 전역변수인 emailSet에 추가한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem6 {
+    static TreeSet<String> emailSet = new TreeSet<>();
+
+    /**
+     * 중복된 문자열을 지닌 이메일을 전역변수 TreeSet에 추가하는 메서드
+     *
+     * @param email
+     */
+    static void addToSet(String email) {
+        emailSet.add(email);
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
