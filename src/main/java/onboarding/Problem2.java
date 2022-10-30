1eb27d1e714c5dd1aa3f34b7801fc786c4d27e0a
YongGwan <32181771@dankook.ac.kr>
2022-10-31 00:54:48 +0900

    problem2 clear

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int check = 0;
+        StringBuilder answer = new StringBuilder();
+
+        while (true) {
+            for (int i = 0; i < cryptogram.length(); i++) {
+                if (i == cryptogram.length() - 1) {
+                    answer.append(cryptogram.charAt(i));
+                    break;
+                }
+                if (cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
+                    answer.append(cryptogram.charAt(i));
+                } else {
+                    check++;
+                    i++;
+                }
+            }
+            if(check != 0) {
+                check = 0;
+                System.out.println(answer);
+                cryptogram = answer.toString();
+                answer.setLength(0);
+            } else break;
+        }
+        return answer.toString();
     }
 }
