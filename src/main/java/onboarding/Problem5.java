345ed33bc9145abf286ad181e864562fa7f5ff0e
unknown <ymecca12@gmail.com>
2022-10-31 01:57:16 +0900

    [MISSION5] / feat : 동전 분배

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        return Wallet.pay(money);
+    }
+}
+class Wallet{
+    static int[] cate = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    static List<Integer> pay(int money){
+        List<Integer> answer = new ArrayList<>();
+        for (int bill : cate) {
+                int coinCnt = money / bill;
+                answer.add(coinCnt);
+                money -= bill * coinCnt;
+        }
         return answer;
     }
 }
