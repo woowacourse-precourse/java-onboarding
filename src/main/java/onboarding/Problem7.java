0c4f8f27127418673163d7a7c3b3be479a95d93a
beombu <sunil1369@bible.ac.kr>
2022-10-31 04:47:35 +0900

    Feat(P7) : user의 친구구 목록을 구하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -15,6 +15,15 @@ public class Problem7 {
     /**
      * user의 친구 목록을 구하는 기능
      */
+    private static void makeUserFriendList(String user, List<List<String>> friends, TreeSet<String> friendSet) {
+        for(List<String> relation : friends){
+            if(relation.contains(user)){
+                friendSet.add(relation.get(0));
+                friendSet.add(relation.get(1));
+            }
+        }
+        friendSet.remove(user);
+    }
 
     /**
      * user와 함께 아는 친구 점수 계산 기능
