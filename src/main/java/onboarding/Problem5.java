29cb203dc701e9af3382e9967bab5c00054ab931
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 15:04:16 +0900

    feat(problem5): 입력 금액을 주어진 단위로 나눈 몫과 나머지를 반환하는 함수 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int[] units = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    private static int getQuotientByUnit(int money, int unit) {
+        return money / unit;
+    }
+
+    private static int getRemainderByUnit(int money, int unit) {
+        return money % unit;
     }
 }
