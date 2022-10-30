eab1cb95c3bf1fb65bcf2869474b5fc4a990f788
squirMM <jsm6616@naver.com>
2022-10-31 02:31:23 +0900

    :rocket: feat(Problem5) refactoring code line

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,7 +10,7 @@ public class Problem5 {
         Arrays.fill(pocket,0);
 
         for (int idx=0; idx<moneyType.length; idx++){
-            pocket[idx]=(Math.floorDiv(money, moneyType[idx]));
+            pocket[idx]=Math.floorDiv(money, moneyType[idx]);
             money = Math.floorMod(money, moneyType[idx]);
             if(money==0)break;
         }
