268112b06698c491220c8fea1fefc76664a7f735
seongha <11pi885@gmail.com>
2022-10-30 21:20:55 +0900

    feat: 기능목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+
+    //유저의 친구셋 만들기
+
+
+    //유저의 친구가 아닌사람 리스트를 맵으로 저장
+
+
+    //친구 관계 그래프 만들기
+
+
+    //함께 아는 친구의 수를 찾아 점수로 환산하기
+
+
+    //타임라인 방문기록을 점수화 하기
+
+
+    //점수를 기준으로 내림차순하고, 이름을 기준으로 오름차순으로 정렬하기
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        //함수 호출해서 완성하기
         return answer;
     }
 }
