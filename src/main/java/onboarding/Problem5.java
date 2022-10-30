3116d2eaa42d8c8f45b2579afb92a6940638e33f
minji <heunhanireum@gmail.com>
2022-10-30 20:15:21 +0900

    Problem5: moneyList 순회하면서 필요한 화폐의 개수만큼 moneyCount 리스트에 담아주기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,6 +13,10 @@ public class Problem5 {
         List<Integer> moneyCount = new ArrayList<>();
         int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        for (int i = 0 ; i < 9 ; i++) {
+            moneyCount.add(money/moneyList[i]);
+            money = money%moneyList[i];
+        }
         return moneyCount;
     }
 }
