363b704a59d7f20e9aeb48d521213665deac199a
chopinballadeno4 <qo98333@naver.com>
2022-10-30 16:15:42 +0900

    problem 6, function 1 check isHangeul

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    public static boolean checkOverlap(String nicknameA, String nicknameB) {
+        for(int i=0 ; i<nicknameA.length()-1 ; i++) {
+            for(int j=0 ; j<nicknameB.length()-1 ; j++) {
+                if(nicknameA.charAt(i) == nicknameB.charAt(j)) {
+                    if(nicknameA.charAt(i+1) == nicknameB.charAt(j+1)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        boolean
         return answer;
     }
 }
