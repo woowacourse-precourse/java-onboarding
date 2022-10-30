ea265280aa816c9b5c8e1d7c07014992d926ce63
yoon <beatus96@naver.com>
2022-10-30 22:38:35 +0900

    [Problem 3] count함수 : 숫자에 3,6,9 있는지 카운트

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+
         int answer = 0;
         return answer;
     }
+
+    //커밋1. 숫자에 3,6,9 있는지 세기
+    public static int count(int num){
+        int count = 0;
+        while(num !=0){
+            if(num%10%3 ==0 && num%10 != 0) {
+                count++;
+            }
+            num /= 10;
+        }
+        return count;
+    }
+
 }
