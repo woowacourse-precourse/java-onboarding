93fe9205a67abcec4c794b0436968ea739f577d8
hoiae <donald819@naver.com>
2022-10-30 21:55:52 +0900

    ãproblem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,95 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+        /* user와 친구인 사람 리스트(list) 만들기 */
+            // donut / shakevan
+        /* list의 구성요소와 친구인 사람들 */
+            // andole, jun, andole, jun
+        /* visitor에 있는 사람 카운팅 */
+            // bedi bedi bedi donut shakevan
+
+
+        List<String> answer = new ArrayList<>();
+
+        /* 이미 친구인 사람들은 최종 result 에서 제외.*/
+
+        /* 1. user와 친구인 사람 friendsList 만들기 */
+        List<String> friendsList = new ArrayList<>();
+
+        /* 2. user , user와 친구인사람 friendsList에 담기*/
+        for(int i = 0; i < friends.size(); i++){
+            int index = friends.get(i).indexOf(user);
+            if(index != -1){
+                if(index == 0)
+                    friendsList.add(friends.get(i).get(1));
+                else
+                    friendsList.add(friends.get(i).get(0));
+            }
+        }
+//        System.out.println(friendsList);
+
+        /* 3. user의 친구인 사람과 친구인 사람을 조회 후,  Map<친구이름,점수> 담기 */
+        Map<String,Integer> resultMap = new HashMap<>();
+        for(int i = 0; i < friendsList.size(); i++){
+            String search = friendsList.get(i);
+            for(int j = 0; j < friends.size(); j++){
+                int index = friends.get(j).indexOf(search);
+                if(index != -1){
+                    if(index == 0) {
+                        int value = 0;
+                        if (resultMap.containsKey(search)) {
+                            value = resultMap.get(friends.get(j).get(1));
+                        }
+                        resultMap.put(friends.get(j).get(1), 10 + value);
+                    }
+                    else{
+                        int value = 0;
+                        if (resultMap.containsKey(search)) {
+                            value = resultMap.get(friends.get(j).get(1));
+                        }
+                        resultMap.put(friends.get(j).get(0), 10 + value);
+                    }
+                }
+            }
+        }
+        /* 4. visitor를 Map<친구이름,점수>에 담기*/
+        for(int i = 0; i < visitors.size(); i++){
+            String visitor = visitors.get(i);
+            int value = 0;
+            if(resultMap.containsKey(visitor)){
+                value = resultMap.get(visitor);
+            }
+            resultMap.put(visitor, 1 + value);
+        }
+
+        /* 5. friendsList에 있는 친구들 Map에서 제외하기*/
+//        System.out.println("제거해야할 친구목록 : "+friendsList);
+        for(int i = 0; i < friendsList.size(); i++ ){
+//            System.out.println(friendsList.get(i));
+            resultMap.remove(friendsList.get(i));
+        }
+        resultMap.remove(user);
+//        System.out.println("제거된 이후 : "+resultMap);
+
+        /* 6. Map 정렬하여 answerList에 담기*/
+        List<String> keySet = new ArrayList<>(resultMap.keySet());
+        keySet.sort((o1, o2) -> resultMap.get(o2).compareTo(resultMap.get(o1)));
+
+        if(keySet.size() > 5){
+            for(int i = 0; i < 5; i++){
+                answer.add(keySet.get(i));
+            }
+        }else{
+            for(String key : keySet){
+                answer.add(key);
+            }
+        }
+
+//        System.out.println(answer);
         return answer;
     }
 }
