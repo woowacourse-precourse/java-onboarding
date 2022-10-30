0eef293e6181ee5ffcaf8e4bb0ce7593464eaadb
SonMinGyu <littleking13@naver.com>
2022-10-30 18:35:34 +0900

    [FEAT] Problem2 decipher 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,48 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String decipher(String cryptogram) {
+        Stack<String> originCryptogramStack = new Stack<>();
+        String newCrypogramString = "";
+
+        String[] cryptogramArray = cryptogram.split("");
+
+        for(int i = 0; i < cryptogramArray.length; i++) {
+            originCryptogramStack.push(cryptogramArray[i]);
+        }
+
+        if(originCryptogramStack.empty()) {
+            return "";
+        }
+
+        String before = originCryptogramStack.pop();
+        int count = 0;
+        while (originCryptogramStack.empty()) {
+            String current = originCryptogramStack.pop();
+            if(before.equals(current)) {
+                count += 1;
+            }
+            else {
+                if(count == 0) {
+                    newCrypogramString += before;
+                }
+
+                if (originCryptogramStack.empty()) {
+                    newCrypogramString += current;
+                }
+
+                before = current;
+                count = 0;
+            }
+        }
+
+        return newCrypogramString;
     }
 }
