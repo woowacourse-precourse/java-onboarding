4c6d656a7f453918e20307ae151a2110d4563362
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-30 18:38:33 +0900

    Problem3 : 두자리수 이상일 때 박수 count 하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,8 +9,12 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for(int i=1; i<=number; i++) {
-            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
-                answer += 1;
+            int num=i;
+            while(num>0) {
+                if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                    answer += 1;
+                }
+                num/=10;
             }
         }
         return answer;
