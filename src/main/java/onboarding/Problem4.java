6afcb8e5a3bae3189b36c91404d5f8d5bd00477b
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-31 02:54:11 +0900

    feat(문제 4): 암호문 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            char frogC = c;
+            if (Character.isLowerCase(c)) {
+                frogC = (char) ('z' - c + 'a');
+            } else if (Character.isUpperCase(c)) {
+                frogC = (char) ('Z' - c + 'A');
+            }
+            answer.append(frogC);
+        }
+        return answer.toString();
     }
 }
