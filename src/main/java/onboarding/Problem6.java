8e099b3610a15672cfaa5c51df95163bf3e7f743
chopinballadeno4 <qo98333@naver.com>
2022-10-30 22:13:00 +0900

    problem 6, function 2 check nickname form and isOverlap

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,26 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
-    public static boolean checkOverlap(String nicknameA, String nicknameB) {
+
+
+    public static boolean checkNicknameOverlap(String nicknameA, String nicknameB) {
+        if(nicknameA.length() < 1 || nicknameA.length() > 19 || nicknameB.length() < 1 || nicknameB.length() > 19) {
+            return false;
+        }
+
         for(int i=0 ; i<nicknameA.length()-1 ; i++) {
             for(int j=0 ; j<nicknameB.length()-1 ; j++) {
                 if(nicknameA.charAt(i) == nicknameB.charAt(j)) {
+                    if(!checkHangeul(nicknameA.charAt(i))) {
+                        return false;
+                    }
+
+                    if(!checkHangeul(nicknameB.charAt(i))) {
+                        return false;
+                    }
+
                     if(nicknameA.charAt(i+1) == nicknameB.charAt(j+1)) {
                         return true;
                     }
@@ -18,9 +31,18 @@ public class Problem6 {
         return false;
     }
 
+    public static boolean checkHangeul(char word) {
+        if(String.valueOf(word).matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
+            return true;
+        }
+        return false;
+    }
+
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        boolean
+
+
         return answer;
     }
 }
