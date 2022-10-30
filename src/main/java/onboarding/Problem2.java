9316749eb8f84b1bb38a601ba850f8c03c69444d
yoon <beatus96@naver.com>
2022-10-30 22:31:05 +0900

    [Problem 2] stack이용하여 중복 제거하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        //커밋1. 스택 이용하여 중복 제거하기
+        Stack<Character> stack = new Stack<>();
+        for(int i =0 ; i<cryptogram.length();i++){
+            Character c = cryptogram.charAt(i);
+
+            if(stack.isEmpty() || stack.peek() != c) stack.add(c);
+            else stack.pop();
+        }
+
     }
+
+
 }
