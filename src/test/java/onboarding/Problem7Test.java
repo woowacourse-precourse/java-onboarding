6adff3f7e1c909b19ca8e9e67af2fd378421f45a
Invidam <hansupark0208@gmail.com>
2022-10-30 15:35:21 +0900

    fix(Problem7): remove user's point method and logic and test method

--- src/test/java/onboarding/Problem7Test.java
+++ src/test/java/onboarding/Problem7Test.java
@@ -14,31 +14,6 @@ import org.junit.jupiter.api.Test;
 class Problem7Test {
 
     @Test
-    void 사용자목록테스트() {
-        String user = "hansu";
-        List<List<String>> friends = List.of(
-                List.of("hansu", "sungjoon"),
-                List.of("hansu", "jiwon"),
-                List.of("sungjoon", "chaemin"),
-                List.of("minjae", "hansu"),
-                List.of("jaewoo", "minjae"),
-                List.of("hansu", "jaewoo")
-        );
-        List<String> visitors = List.of("sungjoon", "sungjoon", "minjae", "jiwon", "invidam", "invidam", "sonny");
-        Map<String, Integer> result = new HashMap<String, Integer>();
-        result.put("hansu", 0);
-        result.put("sungjoon", 0);
-        result.put("jiwon", 0);
-        result.put("chaemin", 0);
-        result.put("minjae", 0);
-        result.put("jaewoo", 0);
-        result.put("invidam", 0);
-        result.put("sonny", 0);
-
-        assertThat(Problem7.makeUserPoints(user, friends, visitors)).isEqualTo(result);
-    }
-
-    @Test
     void 사용자친구목록테스트() {
         String user = "hansu";
         List<List<String>> friends = List.of(
