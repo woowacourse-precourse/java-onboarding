ef57a7c251515d70727400a90bfbf1ba8e001862
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-30 17:39:37 +0900

    [ADD] 결과값 상수로 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        final int WRONG_INPUT = -1;
+        final int TIE = 0;
+        final int POBI_WINS = 1;
+        final int CRONG_WINS = 2;
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
