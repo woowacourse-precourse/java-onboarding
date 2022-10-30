4f5ec4a595590076a16fa1cd92297083f4a5c8fb
myeongju00 <j51677767@gmail.com>
2022-10-30 19:37:56 +0900

    feat: 중복된 닉네임 인덱스를 리스트에 담는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Problem6 {
     static Map<String, Integer> nicknameList = new HashMap<>();
+    static List<Integer> indexList = new ArrayList<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
@@ -15,9 +17,13 @@ public class Problem6 {
             String subString = name.substring(i, i + 2);
             if(nicknameList.containsKey(subString)) {
                 //정답 베열에 넣는 함수
+                putNameIndex(index);
             } else {
                 nicknameList.put(name, index);
             }
         }
+    }
+    private static void putNameIndex(int index) {
+        indexList.add(index);
     }
 }
