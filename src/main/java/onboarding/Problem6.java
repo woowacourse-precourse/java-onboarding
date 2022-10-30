2de6e219effb411090a60c193c03e5a36213c966
syhan7516 <syhan7516@naver.com>
2022-10-30 21:39:29 +0900

    Problem6 - 5번 기능 : 제한사항 & 결과 확인

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,17 +7,15 @@ import java.util.Comparator;
 public class Problem6 {
 
     // 2. 닉네임 중복 체크
-    static boolean getCheckNickName(List<String> basic, List<String> compare)
-    {
+    static boolean getCheckNickName(List<String> basic, List<String> compare) {
         // 기존 문자열 저장
         String word = basic.get(1);
 
         // 두 글자씩 비교
-        for(int strIdx=0; strIdx<word.length()-1; strIdx++)
-        {
+        for (int strIdx = 0; strIdx < word.length() - 1; strIdx++) {
             String resultSubStr = word.substring(strIdx, strIdx + 2);
             // 두 글자가 비교 문자열에 포함된 경우
-            if(compare.get(1).contains(resultSubStr))
+            if (compare.get(1).contains(resultSubStr))
                 return true;
         }
         return false;
@@ -25,14 +23,56 @@ public class Problem6 {
 
     // 3. 중복 닉네임의 이메일 결과 리스트에 저장하기
     // 4-1 결과 리스트 중복 확인
-    static void addResultList(List<String> resultList, String email, String compareEmail)
-    {
+    static void addResultList(List<String> resultList, String email, String compareEmail) {
         // 리스트 내 중복 확인
-        if(!resultList.contains(email))
+        if (!resultList.contains(email))
             resultList.add(email);
 
-        if(!resultList.contains(compareEmail))
+        if (!resultList.contains(compareEmail))
             resultList.add(compareEmail);
+    }
+
+    // 제한사항 중 넥네임 한글 여부 검사
+    static boolean isHangeul(String infoNickName)
+    {
+        for(int check = 0; check<infoNickName.length(); check++)
+        {
+            char han = infoNickName.charAt(check);
+            if(!(han >= '가' && han <= '힣'))
+                return false;
+        }
+        return true;
+    }
+
+    // 5. 제한사항
+    static boolean getInfoCheck(List<String> basic) {
+        String infoEmail = basic.get(0);
+        String infoNickName = basic.get(1);
+
+        // 도메인 형식
+        if (!infoEmail.contains("@email.com")) {
+            System.out.println("도메인 형식이 올바르지 않습니다.");
+            return false;
+        }
+        // 이메일 길이 제한
+        else if (infoEmail.length() < 11 || infoEmail.length() > 19) {
+            System.out.println("이메일 길이가 올바르지 않습니다.");
+            return false;
+        }
+        // 한글만 가능한 닉네임 형식
+        else if(!isHangeul(infoNickName))
+        {
+            System.out.println("닉네임이 한글이 아닙니다!");
+            return false;
+        }
+        // 넥네임 길이 제한
+        else if(infoNickName.length() < 1 || infoNickName.length() > 19)
+        {
+            System.out.println("닉네임 길이가 올바르지 않습니다.");
+            return false;
+        }
+
+        return true;
     }
 
     public static List<String> solution(List<List<String>> forms) {
@@ -49,10 +89,28 @@ public class Problem6 {
             String nickName = forms.get(idx).get(1);
             List<String> basic = List.of(email, nickName);
 
+            // 제한사항 확인
+            boolean infoCheck = getInfoCheck(basic);
+
+            if(infoCheck == false)
+            {
+                System.out.println(email + " , " + nickName + " : 에러");
+                return answer;
+            }
+
             for(int value=idx+1; value<totalSize; value++) {
                 String compareEmail = forms.get(value).get(0);
                 String compareNickName = forms.get(value).get(1);
                 List<String> compare = List.of(compareEmail, compareNickName);
+
+                // 제한사항 확인
+                infoCheck = getInfoCheck(compare);
+
+                if(infoCheck == false)
+                {
+                    System.out.println(compareEmail + " , " + compareNickName + " : 에러");
+                    return answer;
+                }
 
                 // 닉네임 중복 체크
                 boolean resultCompare = getCheckNickName(basic, compare);
@@ -67,9 +125,8 @@ public class Problem6 {
         
         // 4-2. 결과 리스트 정렬하기
         resultList.sort(Comparator.naturalOrder());
-        
-        // 결과 리스트 정렬 결과 확인
-        System.out.println(resultList);
+
+        answer = resultList;
 
         return answer;
     }
