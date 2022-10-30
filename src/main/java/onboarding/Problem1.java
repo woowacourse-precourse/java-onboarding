c8c379a9e6a440258359260cef4b3ce8be9b8a73
moonn6pence <shin421179@gmail.com>
2022-10-30 16:02:36 +0900

    feat #1 : validate 메소드 추가
     입력 페이지 유효성 검사 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int validate(List<Integer> pages){
+        Integer leftPage = pages.get(0);
+        Integer rightPage = pages.get(1);
+
+        // 시작 면 혹은 마지막 면인지 검사
+        if(leftPage == 1 || rightPage == 400)
+            return -1;
+
+        // 연속된 페이지인지 검사
+        if(leftPage + 1 != rightPage)
+            return -1;
+
+        return 1;
     }
 }
