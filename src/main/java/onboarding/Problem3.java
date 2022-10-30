247c30db165d9f2a900c2060284eab0cdc1df2f0
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-10-30 16:53:22 +0900

    Fix: 예외사항 체크 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,9 +6,23 @@ import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
+        // 예외 사항 체크
+        if (checkInvalid(number)) {
+            return -1;
+        }
+
         List<Character> list = Arrays.asList('3', '6', '9');
 
         return countTotal(number, list);
+    }
+
+    // 제한 사항
+    private static boolean checkInvalid(int num) {
+        // number 는 1이상 10000 이하 자연수
+        if (num < 1 || num > 10000) {
+            return true;
+        }
+        return false;
     }
 
     // 1부터 number 까지 총 3,6,9 개수 세는 함수
