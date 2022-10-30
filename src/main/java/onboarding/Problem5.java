5896841486c2850f2b5c8d577d9676cc3c590cf5
권태현 <kygkth2011@gmail.com>
2022-10-30 23:42:27 +0900

    Solve Problem 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> units = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+        for(Integer unit : units){
+            int cnt;
+            if(money >= unit){
+                cnt = money / unit;
+                answer.add(cnt);
+                money %= unit;
+            }
+            else {
+                answer.add(0);
+            }
+        }
         return answer;
     }
 }
