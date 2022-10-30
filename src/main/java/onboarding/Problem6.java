c73f32d2f855926f3b8b6017519f27a7a1503243
SeokHwan An <asok369258@naver.com>
2022-10-31 00:39:43 +0900

    feat [Problem6] : form에서 email 리스트를 추출하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    // forms에서 email 리스트를 추출하는 기능
+    public static List<String> extractEmailList(List<List<String>> forms) {
+        return forms.stream()
+            .map(form -> form.get(0))
+            .collect(Collectors.toList());
     }
 }
