17409d1ea37ede103cd25e4e93439175f24e9af7
jiwon <ljwon77@daum.net>
2022-10-30 22:51:43 +0900

    refactor(problem3): extract method about getting removed length

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -29,17 +29,31 @@ public class Problem3 {
     private static int getClapCount(String strNum, int originLen) {
         int clapCount = 0;
         for(String clapNum : numForClaps) {
-            // 기준 문자열에 3/6/9가 포함이 되어 있다면 제거 후 길이를 구한다.
-            if (strNum.contains(clapNum)) {
-                String removedStr = removeClapNum(strNum, clapNum);
-                int newLen = removedStr.length();
-                // 제거된 길이만큼이 박수를 친 횟수가 된다.
-                clapCount += (originLen - newLen);
-            }
+            int newLen = checkClapNumAndGetLength(strNum, clapNum);
+            // 제거된 길이만큼이 박수를 친 횟수가 된다.
+            clapCount += (originLen - newLen);
         }
+        
         return clapCount;
     }
 
+    /**
+     * 기준 문자열에 3/6/9가 포함이 되어 있다면 제거 후 길이를 구한다.
+     *
+     * @param strNum 기준 문자열
+     * @param clapNum 369 게임의 기준이 되는 번호 (3/6/9)
+     * @return int - 제거된 문자열의 길이를 리턴한다.
+     */
+    private static int checkClapNumAndGetLength(String strNum, String clapNum) {
+        int newLen = 0;
+
+        if (strNum.contains(clapNum)) {
+            String removedStr = removeClapNum(strNum, clapNum);
+            newLen = removedStr.length();
+        }
+
+        return newLen;
+    }
 
 
     /**
