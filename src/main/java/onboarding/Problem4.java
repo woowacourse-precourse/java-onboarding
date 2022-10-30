0b1939e6441125aae8ca962ba012e955c27f19fb
chopinballadeno4 <qo98333@naver.com>
2022-10-30 11:53:27 +0900

    problem 4, function 2 repeat translate word

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -24,6 +24,10 @@ public class Problem4 {
 
     public static String solution(String word) {
         String answer = "";
+        for(int i=0 ; i<word.length() ; i++) {
+            answer = answer + translateAlpa(word.charAt(i));
+        }
+
         return answer;
     }
 }
