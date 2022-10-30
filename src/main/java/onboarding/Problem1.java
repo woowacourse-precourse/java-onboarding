c29ad7ad512681dc1520dadc97dfd5f5632fa51b
sohyundoh <lyny123@naver.com>
2022-10-30 15:58:06 +0900

    Problem 1 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,11 @@ import java.util.List;
 제한 사항
     pobi와 crong의 길이는 2이다.
     pobi와 crong에는 [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있다.
+
+기능 목록
+1. 점수 계산 기능
+2. answer 값 지정
+3. 예외사항 구현
  */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
