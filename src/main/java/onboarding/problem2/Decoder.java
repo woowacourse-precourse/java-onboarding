6d56b4f2cd4251049781378c813d62ca3a13fc83
yun <yshyeon102@gmail.com>
2022-10-31 06:11:15 +0900

    feat: Problem2 - 중복 찾기 메서드 구현

--- src/main/java/onboarding/problem2/Decoder.java
+++ src/main/java/onboarding/problem2/Decoder.java
@@ -1,7 +1,32 @@
 package onboarding.problem2;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Decoder {
     public ArrayList<int[]> duplicates = new ArrayList<>();
+
+    public static Decoder findDuplicates(LinkedList<Character> list) {
+        Decoder decoder = new Decoder();
+
+        int idx = 0;
+        int listSize = list.size();
+
+        while (idx < listSize) {
+            int currentIdx = idx;
+            int numberOfDuplicate = 1;
+
+            Character ch = list.get(idx);
+            while (idx + 1 < listSize && ch.equals(list.get(idx + 1))) {
+                idx++;
+                numberOfDuplicate++;
+            }
+            if (numberOfDuplicate > 1) {
+                decoder.duplicates
+                        .add(new int[]{currentIdx, numberOfDuplicate});
+            }
+            idx++;
+        }
+        return decoder;
+    }
 }
