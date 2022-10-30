813821e344b3039d13d943fd2f34dc3fdc667c7f
SangHun <shzero211@naver.com>
2022-10-30 18:33:54 +0900

    feat: Problem4 - 대문자,소문자 배열을 만들어서 청개구리 값을 넣어주고 - 알파벳에 시작값을 빼주어 위치를 찾아서 문자열 만들어주는 solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,32 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        //대문자 문자 저장
+        char[] upArr=new char[26];
+        //소문자 문자 저장
+        char[] lowArr=new char[26];
+
+        for(int i=0;i<upArr.length;i++){
+            upArr[i]=(char)('Z'-i);
+        }
+        for(int i=0;i<lowArr.length;i++){
+            lowArr[i]=(char)('z'-i);
+        }
+
+        StringBuilder sb=new StringBuilder();
+        for(Character c:word.toCharArray()){
+            if(Character.isUpperCase(c)){
+                //해당 문자위치의 문자 추가
+                sb.append(upArr[c-'A']);
+            }
+            if(Character.isLowerCase(c)){
+                //해당 문자위치의 문자 추가
+                sb.append(lowArr[c-'a']);
+            }
+            if(c==' '){
+                sb.append(' ');
+            }
+        }
+        return sb.toString();
     }
 }
