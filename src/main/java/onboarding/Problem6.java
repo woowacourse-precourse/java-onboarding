92d700f3881e5a278fa27532ebada62dc88d7677
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-31 03:07:50 +0900

    feat: Create splitByTwoToArrayList | [problem 6]
    - 클래스로 구성하여 복잡도를 줄이고자 시도했다
    
    - 문자열을 연속한 2개씩을 split한 문자열을 저장한다
        - ex. "제이엠" -> "제이", "이엠"

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+}
+
+class EmailArrayListValuePerSplitedNickNameKeyMap {
+    private List<String> splitByTwoToArrayList(String str) {
+        ArrayList arrayList = new ArrayList<>();
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(str.charAt(i));
+            sb.append(str.charAt(i + 1));
+
+            arrayList.add(String.valueOf(sb));
+        }
+
+        return arrayList;
     }
 }
