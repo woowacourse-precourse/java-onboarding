e2fbdeeeaf5e2bccfc7e5f5b4006cfc330ed3221
seonho <ysh10216@gmail.com>
2022-10-30 19:04:30 +0900

    create isException function

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,9 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+
+    // 1. word 예외처리 : 길이가 1 ~ 1000
+    public static boolean isException(String word){
+        if(word.length() >= 1 && word.length() <= 1000){
+            return true;
+        }
+        return false;
+    }
     public static String solution(String word) {
         String answer = "";
-        // 1. word 예외처리 : 길이가 1~1000
         // 알파벳만 변하도록 처리. 대문자는 대문자로 소문자는 소문자로.
         // 2. 단어를 한개 씩 받아서 변환해주기.
 
