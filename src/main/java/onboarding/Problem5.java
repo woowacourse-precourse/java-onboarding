a89a5747e33d20b76c703802463f2401ecf7f872
w0ntae <bitcampcoding@gmail.com>
2022-10-30 15:59:29 +0900

    p5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        while(money > 0){
+            for(int n : currency){
+                answer.add(money/n);
+                money %=n;
+            }
+        }
+        
         return answer;
     }
 }
