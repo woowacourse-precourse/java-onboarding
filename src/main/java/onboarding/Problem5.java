c17ec09f0efa0b13fcb633f03e5ebb45d46da965
sxxgxxnz <tmddms2292@gmail.com>
2022-10-30 17:29:32 +0900

    feat : Problem5[2] money를 화폐단위로 계산한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,12 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         int[] unit = {50000,10000,5000,1000,500,100,50,10,1};
 
+        for(int i=0;i<unit.length;i++){
+
+            answer.add(money/unit[i]);
+            money%=unit[i];
+        }
+        
         return answer;
     }
 }
