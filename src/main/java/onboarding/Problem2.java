05422ead284a707b4f1220096f309cd6e081bcab
seoyoung <sy9723@korea.ac.kr>
2022-10-31 00:20:40 +0900

    problem2, 5

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,44 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        Stack<Character> stk = new Stack<>();
+        boolean flag = false;
+        for (int i=0; i < cryptogram.length(); i++){
+            if (stk.empty()) {
+                stk.push(cryptogram.charAt(i));
+            }
+            else {
+                if (stk.peek() == cryptogram.charAt(i)) {
+                    flag = true;
+                }
+                else if (flag){
+                    flag = false;
+                    stk.pop();
+                    if (stk.peek() == cryptogram.charAt(i)){
+                        flag = true;
+                    }
+                    else {
+                        stk.push(cryptogram.charAt(i));    
+                    }
+                }
+                else {
+                    stk.push(cryptogram.charAt(i));
+                    
+                }
+            }
+        }
+        if (flag) {
+            stk.pop();
+        }
+
+        String answer = "";
+        while (!stk.empty()) {
+            answer = stk.pop() + answer;
+        }
         return answer;
     }
 }
