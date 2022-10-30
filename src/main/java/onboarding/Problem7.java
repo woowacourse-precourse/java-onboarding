a79c2dad63bce3cd0fee41fb61c6d2fd3211948d
yujaemin <woawoa0513@gmail.com>
2022-10-31 01:37:58 +0900

    feat: [문제 7] 친구 목록 가져오기 기능 추가
    매개변수로 입력한 사용자의 친구 목록을 String 배열로 반환하는 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    static HashMap<String, String> friendsOfUser = new HashMap<>();
+
+    public static String[] getFriends(String user) {
+        String friendString = friendsOfUser.get(user);
+
+        return friendString.split("/");
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        for (List<String> friend : friends) {
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+
+            friendsOfUser.put(user1, friendsOfUser.getOrDefault(user1, "") + user2 + "/");
+            friendsOfUser.put(user2, friendsOfUser.getOrDefault(user2, "") + user1 + "/");
+        }
+
+        String[] friendArr = getFriends(user);
+
+        for (String x : friendArr) {
+            System.out.println(x);
+        }
+
+        return List.of("Test");
     }
 }
