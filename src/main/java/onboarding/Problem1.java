89410688be2e0b88884b7de587b983ac8375ea74
donghae <donghae1999@gmail.com>
2022-10-30 20:51:01 +0900

    P1 : 페이지의 각자리 덧셈,곱셈 함수 구현 & 유저의 최댓값 계산 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
+
+    private static int secondSum=0,secondMul=0;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int firstUserScore = Math.max(numMaxSumMul(pobi.get(0)),numMaxSumMul(pobi.get(1)));
+        int SecondUserScore = Math.max(numMaxSumMul(crong.get(0)),numMaxSumMul(crong.get(1)));
+
         return answer;
+    }
+    public static int numMaxSumMul(int page){
+        int sum=0, mul=1;
+        while (page>0){
+            sum += page%10;
+            mul *= page%10;
+            page /= 10;
+        }
+        return Math.max(sum,mul);
     }
 }
