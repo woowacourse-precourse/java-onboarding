cf2d8f1d16142b3259be6981472d21531f2d73ab
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-30 17:24:13 +0900

    feat(문제 2): 중복된 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         if (!validation(cryptogram)) return "ERROR";
+        Stack<Character> answerStack = new Stack<>();
+        boolean delete = false;
+        for (char c : cryptogram.toCharArray()) {
+            if (answerStack.isEmpty()) {
+                answerStack.push(c);
+                continue;
+            }
+            if (answerStack.peek() == c) {
+                delete = true;
+                continue;
+            }
+            if (delete) {
+                answerStack.pop();
+            }
+            delete = (answerStack.peek() == c);
+            if (!delete) {
+                answerStack.push(c);
+            }
+        }
+        if (delete) {
+            answerStack.pop();
+        }
+        StringBuilder answer = new StringBuilder();
+        answerStack.forEach(answer::append);
+        return answer.toString();
     }
 
     static boolean validation(String cryptogram) {
