7f3d5f683452fcccae4ec7003f6c515b5a9979f1
iamsseaa <homingsea@gmail.com>
2022-10-30 15:34:26 +0900

    feat(Problem5): 입력 값이 범위를 벗어나는 경우 예외처리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,13 +6,29 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        int[] changeUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> answer = new ArrayList<>();
+
+        try {
+            ExceptionProblem5.validateMoneyRange(money);
+        } catch (IllegalArgumentException e) {
+            return answer;
+        }
+
+        int[] changeUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
 
         for (int unit : changeUnit) {
             answer.add(money / unit);
             money %= unit;
         }
         return answer;
+    }
+}
+
+class ExceptionProblem5 {
+    public static void validateMoneyRange(int money) {
+        if (money < 1 || money > 1000000) {
+            throw new IllegalArgumentException();
+        }
     }
 }
