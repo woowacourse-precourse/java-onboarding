8bbb90baac787f5e53d1565cd827af15ae36688b
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:19:58 +0900

    [문제4] 제한사항 체크

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        if (word.length()<1 || word.length()>1000)
+            return "Invalid Input";
+
+        
+
         return answer;
     }
 }
