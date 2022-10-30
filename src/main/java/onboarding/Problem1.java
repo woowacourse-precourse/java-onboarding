b02c9e0f75fbcf03e1e8927dd0b0a2ba36b11286
jmjmjames <chungjm0711@gmail.com>
2022-10-30 16:37:15 +0900

    feat: add getMaxScore using sum, multiply function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,6 +16,23 @@ class Problem1 {
             return -1;
         }
 
+        int pobiScore = getMaxScore(
+                List.of(sum(pobiLeftPage),
+                        sum(pobiRightPage),
+                        multiply(pobiLeftPage),
+                        multiply(pobiRightPage)
+                )
+        );
+
+        int crongScore = getMaxScore(
+                List.of(sum(crongLeftPage),
+                        sum(crongRightPage),
+                        multiply(crongLeftPage),
+                        multiply(crongRightPage)
+                )
+        );
+
+
         return answer;
     }
 
@@ -33,6 +50,37 @@ class Problem1 {
 
         // 오른쪽 페이지에서 왼쪽 페이지의 차는 1이다.
         return (rightPage - leftPage == 1);
+    }
+
+    private static int getMaxScore(List<Integer> scores) {
+        // 항상 MAX 값은 존재한다. (pobi와 crong의 길이는 2이다.)
+        return scores.stream()
+                .mapToInt(score -> score)
+                .max()
+                .getAsInt();
+        //.orElse(Integer.MIN_VALUE);
+    }
+
+    private static int sum(int page) {
+        int ret = 0;
+
+        while (page > 0) {
+            ret += (page % 10);
+            page /= 10;
+        }
+
+        return ret;
+    }
+
+    private static int multiply(int page) {
+        int ret = 1;
+
+        while (page > 0) {
+            ret *= (page % 10);
+            page /= 10;
+        }
+
+        return ret;
     }
 
 }
