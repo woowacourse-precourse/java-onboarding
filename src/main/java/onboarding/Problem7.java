e74012f2617d60f03b4932228326d3c39901e891
Cho SangHyun <98shcho@naver.com>
2022-10-30 21:12:43 +0900

    feat: add feature to add 1 point

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -45,6 +45,10 @@ public class Problem7 {
                 continue;
             if (!scoreMap.containsKey(name))
                 scoreMap.put(name, 0);
+
+            int oldScore = scoreMap.get(name);
+            int newScore = oldScore += 1;
+            scoreMap.replace(name, newScore);
         }
         return answer;
     }
@@ -72,10 +76,10 @@ public class Problem7 {
   - [V] 현재 참조하는 사람이 user인 경우나 user의 친구인 경우는 건너뛴다
   - [V] scoreMap에 현재 참조하는 사람의 점수를 0점으로 하여 넣어준다.
   - [V] 친구 목록을 통해 user의 친구들을 순회하며 각 친구가 현재 참조하는 사람의 친구목록에 있으면 10점을 더해 교체해준다
-- [] 방문 기록을 통해 점수를 계산한다
+- [V] 방문 기록을 통해 점수를 계산한다
   - [V] 현재 참조하는 방문자가 uesr의 친구인 경우는 건너뛴다
   - [V] scoreMap에 현재 참조하는 방문자가 없으면 0점으로 하여 넣어준다
-  - [] 현재 참조하는 방문자의 점수를 1점 더해서 교체해준다
+  - [V] 현재 참조하는 방문자의 점수를 1점 더해서 교체해준다
 - [] 받은 점수를 토대로 정렬한다
   - [] scoreMap을 이용해 scoreList를 만든다
   - [] scoreList를 점수 내림차순으로 정렬하고, 점수가 같으면 이름순으로 정렬한다
