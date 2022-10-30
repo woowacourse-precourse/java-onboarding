8b3bf7706818ed952b06fffe528080d6647cd5ec
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:26:06 +0900

    feat : 각 단위로 나누어서 몇 개로 변환되는지 구해준다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,7 +8,15 @@ import java.util.List;
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] coin = {50000 , 10000 , 5000 , 1000 , 500 ,100 , 50, 10 ,1};
+
+        for(int i = 0 ; i < coin.length ; i++){
+            answer.add(money/coin[i]);
+            money %= coin[i];
+        }
+
         return answer;
     }
 }
