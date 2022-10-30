106140913230c9fb2a6485a10d6a2dbc7afe125e
Egenieee <hhgg0925@naver.com>
2022-10-30 21:34:21 +0900

    [FEAT] Problem7 이미 친구인 사용자들 추출하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Set<String> alreadyFriends = getAlreadyFriends(friends);
+
+        return Arrays.asList("", "");
+    }
+
+    private static Set<String> getAlreadyFriends(List<List<String>> friends) {
+        Set<String> alreadyFriends = new HashSet<>();
+
+        for (List<String> friend : friends) {
+            alreadyFriends.add(friend.get(0));
+        }
+
+        return alreadyFriends;
     }
 }
