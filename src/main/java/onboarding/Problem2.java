317227c48b6d6742866e66ccd7cb9501b6c44499
Lee-Jiseung <ljss98@naver.com>
2022-10-30 17:55:04 +0900

    feat: problem2 중복 문자 제거하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decode(cryptogram);
+    }
+
+    private static String decode(String c) {
+        int count = 0;
+
+        for (int i = 1; i < c.length(); i++) {
+            if (i >= 0 && c.charAt(i) == c.charAt(i - 1)) {
+                count++;
+            } else if (count > 0) {
+                c = c.substring(0, i - count - 1) + c.substring(i);
+                i -= count + 2;
+                count = 0;
+            }
+        }
+
+        if (count > 0) {
+            c = c.substring(0, c.length() - count - 1);
+        }
+
+        return c;
     }
 }
