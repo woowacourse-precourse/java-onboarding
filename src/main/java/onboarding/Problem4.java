f4de2b114ad770015a6cd73e5652d4ff2eb6ee22
Donguk <95donguk.dev@gmail.com>
2022-10-30 18:58:25 +0900

    feat: 문제4의 문자 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,20 +2,42 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        List<Character> charList = new ArrayList<>();
-        splitWord(word, charList);
+        List<Character> letters = new ArrayList<>();
+        splitWord(word, letters);
+        letters = replaceLetters(letters);
         return answer;
     }
 
-    private static List<Character> splitWord(String word, List<Character> charList) {
+    private static List<Character> replaceLetters(List<Character> letters) {
+        return letters.stream().map(letter -> {
+            if (Character.isUpperCase(letter)) {
+                letter = convertForUpperCaseLetter(letter);
+            }
+            if (Character.isLowerCase(letter)) {
+                letter = convertForLowerCaseLetter(letter);
+            }
+            return letter;
+        }).collect(Collectors.toList());
+    }
+
+    private static Character convertForUpperCaseLetter(Character letter) {
+        return (char) ('A' + 'Z' - letter);
+    }
+
+    private static Character convertForLowerCaseLetter(Character letter) {
+        return (char) ('a' + 'z' - letter);
+    }
+
+    private static List<Character> splitWord(String word, List<Character> letters) {
         char[] chars = word.toCharArray();
         for (char aChar : chars) {
-            charList.add(aChar);
+            letters.add(aChar);
         }
-        return charList;
+        return letters;
     }
 }
