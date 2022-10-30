7b1127e75ea3b8578d6f55ba262cd1e2b79b47cb
euna <christinejung10@daum.net>
2022-10-30 12:16:48 +0900

    feat(Problem1): 예외사항 처리 기능 추가
    isExceptionInPageNum() : 유효한 페이지 번호인지 확인 후 예외사힝이 존재한다면 answer를 -1로 지정하는 함수
    유효한 페이지 번호이더라도 점수 계산 중 예외사항이 발생한다면 answer를 -1로 지정하도록 try-catch문을 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,19 +8,54 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        // 왼쪽, 오른쪽 페이지에 대한 각각의 점수를 저장하는 List
-        // 페이지 번호의 각 자리 숫자를 모두 더하거나 곱한 후, 둘 중 가장 큰 수들을 List로 저장
-        List<Integer> scoreList_pobi = getScoreList(pobi);
-        List<Integer> scoreList_crong = getScoreList(crong);
+        // 유효한 페이지 번호인지 확인
+        // 예외사항 존재 시 answer = -1
+        if (isExceptionInPageNum(pobi) || (isExceptionInPageNum(crong))) {
+            answer = -1;
+        }
 
-        // 가장 큰 점수 계산
-        int score_pobi = getMaxScore(scoreList_pobi);
-        int score_crong = getMaxScore(scoreList_crong);
+        // 페이지 번호에서 예외사항이 발생하지 않았을 경우
+        if (answer != -1) {
+            try {
+                // 각 페이지에 대한 점수를 저장하는 List
+                // 페이지 번호의 각 자리 숫자를 모두 더하거나 곱한 후, 둘 중 가장 큰 수들을 List로 저장
+                List<Integer> scoreList_pobi = getScoreList(pobi);
+                List<Integer> scoreList_crong = getScoreList(crong);
 
-        // 가장 큰 점수끼리 비교 후 answer 값을 지정
-        answer = getAnswer(score_pobi, score_crong);
+                // 가장 큰 점수
+                int score_pobi = getMaxScore(scoreList_pobi);
+                int score_crong = getMaxScore(scoreList_crong);
 
+                // 가장 큰 점수끼리 비교 후 answer 값을 지정
+                answer = getAnswer(score_pobi, score_crong);
+
+            } catch (Exception e) {
+                answer = -1; // 예외사항 발생 시 -1 반환
+            }
+        }
         return answer;
+    }
+
+    // 유효한 페이지 번호인지 확인하는 함수
+    private static boolean isExceptionInPageNum(List<Integer> pageNumList) {
+        boolean isException = false;
+
+        // 1. 페이지 번호가 1 미만, 400 초과인 경우 예외
+        for (int pageNum : pageNumList) {
+            if (pageNum < 1 || pageNum > 400) {
+                isException = true;
+            }
+        }
+        // 2. 두 페이지 번호가 인접한 번호가 아닐 경우 예외
+        if ((pageNumList.get(1) - pageNumList.get(0)) != 1) {
+            isException = true;
+        }
+        // 3. 왼쪽 페이지 번호가 오른쪽 페이지 번호보다 높거나 같을 경우 예외
+        if (pageNumList.get(0) >= pageNumList.get(1)) {
+            isException = true;
+        }
+
+        return isException;
     }
 
     // 왼쪽, 오른쪽 페이지에 대한 각각의 가장 큰 점수들을 List로 반환하는 함수
