beec26516d9f304b2e67b3094c59dcffb7a54d4a
dener8 <8dener@gmail.com>
2022-10-30 15:27:48 +0900

    feat: 개수를 각각 확인하여 리스트를 반환하는 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = count(money);
         return answer;
+    }
+
+    static ArrayList<Integer> count(int money) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        ArrayList<Integer> type = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+        int value;
+        for (int i = 0; i < 9; i++) {
+            value = money / type.get(i);
+            resultList.add(value);
+            money = money - type.get(i) * value;
+        }
+        return resultList;
     }
 }
