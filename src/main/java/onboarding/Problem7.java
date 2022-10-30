5b6ebb833c56608343b119091035ea4ff1eab86d
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-10-31 06:20:47 +0900

    feat(PROBLEM7): totalScore 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Problem7 {
-    public static List<String> checkUserFriend(String user, List<List<String>> friends){
+    public static List<String> checkUserFriend(String user, List<List<String>> friends) {
         List<String> userFriend = new ArrayList<>();
         List<List<String>> checkF = friends.stream()
                 .filter(strings -> strings.contains(user))
@@ -13,13 +13,14 @@ public class Problem7 {
         for (int i = 0; i < checkF.size(); i++) {
             for (int j = 0; j < checkF.get(i).size(); j++) {
                 String name = checkF.get(i).get(j);
-                if(!userFriend.contains(name) && !name.contains(user))
+                if (!userFriend.contains(name) && !name.contains(user))
                     userFriend.add(name);
             }
         }
         return userFriend;
     }
-    public static Map<String,Integer> recommendScore(String user, List<List<String>> friends,List<String> userFriend) {
+
+    public static Map<String, Integer> recommendScore(String user, List<List<String>> friends, List<String> userFriend) {
         List<String> friendScoreCheck = new ArrayList<>();
         for (int i = 0; i < friends.size(); i++) {
             for (int j = 0; j < friends.get(i).size(); j++) {
@@ -29,27 +30,54 @@ public class Problem7 {
             }
         }
         Set<String> set = new HashSet<String>(friendScoreCheck);
-        Map<String,Integer> score = new HashMap<>();
-        for (String str:set) {
-           score.put(str,10 * Collections.frequency(friendScoreCheck,str));
+        Map<String, Integer> score = new HashMap<>();
+        for (String str : set) {
+            score.put(str, 10 * Collections.frequency(friendScoreCheck, str));
         }
         return score;
     }
-    public static Map<String, Integer> viewScore(List<String> userFriend, List<String> visitors){
+
+    public static Map<String, Integer> viewScore(List<String> userFriend, List<String> visitors) {
         List<String> tmpVisitors = new ArrayList<String>(visitors);
 
-        for (String uf: userFriend) {
+        for (String uf : userFriend) {
             tmpVisitors.remove(uf);
         }
         Set<String> set = new HashSet<String>(tmpVisitors);
-        Map<String,Integer> score = new HashMap<>();
-        for (String str:set) {
-            score.put(str, Collections.frequency(tmpVisitors,str));
+        Map<String, Integer> score = new HashMap<>();
+        for (String str : set) {
+            score.put(str, Collections.frequency(tmpVisitors, str));
         }
         return score;
     }
+
+    public static List<String> totalScore(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> userFriend = checkUserFriend(user, friends);
+
+        Map<String, Integer> recommendS = recommendScore(user, friends, userFriend);
+        Map<String, Integer> viewS = viewScore(userFriend, visitors);
+
+        recommendS.forEach((key, value) -> viewS.merge(key, value, (v1, v2) -> v1 + v2));
+        List<String> totalS = new ArrayList<>(viewS.keySet());
+        Collections.sort(totalS);
+        totalS.sort((o1, o2) -> viewS.get(o2).compareTo(viewS.get(o1)));
+        for (String key : totalS) {
+            System.out.println(key);
+        }
+        List<String> answerList = new ArrayList<>();
+        if (totalS.size() >= 5) {
+            for (int i = 0; i < 5; i++) {
+                answerList.add(totalS.get(i));
+            }
+        } else {
+            for (int i = 0; i < totalS.size(); i++) {
+                answerList.add(totalS.get(i));
+            }
+        }
+        return answerList;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = totalScore(user, friends, visitors);
         return answer;
     }
 }
