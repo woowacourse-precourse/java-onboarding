b79e18d1376dfadc59d711059b5bb9c5662b210b
KangHyeon3315 <son2786w@gmail.com>
2022-10-30 19:10:58 +0900

    docs: [5번] javadoc에 파라미터 정보와 리턴 정보 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,19 +8,25 @@ public class Problem5 {
 
     private static final Integer[] baseCurrencyArr = new Integer[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-    /** 주어진 금액의 화폐 구성을 반환하는 함수 */
+    /**
+     * 주어진 금액의 화폐 구성을 반환하는 함수
+     *
+     * @param money 금액
+     * @return 주어진 금액의 화폐 구성 List
+     */
     private static List<Integer> getCureencyCombination(int money) {
         List<Integer> answer = new ArrayList<>();
 
         int rest = money;
-        for(Integer baseCurrency : baseCurrencyArr) {
-            answer.add(rest /  baseCurrency);
+        for (Integer baseCurrency : baseCurrencyArr) {
+            answer.add(rest / baseCurrency);
             rest = rest % baseCurrency;
         }
 
         return answer;
 
     }
+
     public static List<Integer> solution(int money) {
         return getCureencyCombination(money);
     }
