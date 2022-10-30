4037cfcff60146556a17457da3ca8cde672aeb58
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-30 20:17:11 +0900

    fix(problem4): handle non-alphabet
    제한사항에 맞게 오류 처리
    
    * Before: 알파벳->알파벳, 공백->공백, 이외문자->""
    
    * After: 알파벳->알파벳, 나머지->그대로 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,6 @@ package onboarding;
 
 public class Problem4 {
 
-	private static final int BLANK = 32;
 	private static final int UPPER = 65 + 90;
 	private static final int LOWER = 97 + 122;
 
@@ -18,16 +17,11 @@ public class Problem4 {
 	}
 
 	private static String decideWhatToAdd(int ascii) {
-		String answer = "";
-
-		if (ascii == BLANK) {
-			answer = " ";
-		} else if (Character.isUpperCase(ascii)) {
-			answer = Character.toString(UPPER - ascii);
+		if (Character.isUpperCase(ascii)) {
+			ascii = UPPER - ascii;
 		} else if (Character.isLowerCase(ascii)) {
-			answer = Character.toString(LOWER - ascii);
+			ascii = LOWER - ascii;
 		}
-
-		return answer;
+		return Character.toString(ascii);
 	}
 }
