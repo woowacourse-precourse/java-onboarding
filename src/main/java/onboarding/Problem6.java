5d4ff8bd44f38b659c50bd60c3fd1af08318f4bb
Ridealist <cjsn92@gmail.com>
2022-10-30 12:03:10 +0900

    feat/Problem6 : makeBigramsList 메소드 정의

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static List<List<String>> makeBigramsList(List<List<String>> forms) {
+        List<List<String>> bigrams_list = new ArrayList<>();
+        for (int i=0; i<forms.size(); i++) {
+            String str = forms.get(i).get(1);
+
+            List<String> list = new ArrayList<>();
+            int j = 0;
+            while (j < str.length() - 1) {
+                String sub_str = str.substring(j, j+2);
+                list.add(sub_str);
+                j += 1;
+            }
+            bigrams_list.add(list);
+        }
+        return bigrams_list;
     }
 }
