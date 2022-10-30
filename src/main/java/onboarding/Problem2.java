64491d088d86e6a9f927d8214f3f78da7f7e9d7d
JaeUpSu <hyeongs2323@gmail.com>
2022-10-30 18:32:17 +0900

    PROBLEM2 | 현재 암호문의 연속되는 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -26,6 +26,37 @@ public class Problem2 {
 
     private static String toDeduplicate(String cryptogram) {
         String result = "";
+        int size = cryptogram.length();
+        String nextCryptogram = cryptogram;
+
+        char checkCharacter = cryptogram.charAt(0);
+        String toBeRemoved = "";
+
+        for (int i = 1; i < size; i++) {
+
+            char currentChar = cryptogram.charAt(i);
+
+            if (checkCharacter == currentChar) {
+                if (toBeRemoved.length() == 0) {
+                    toBeRemoved = cryptogram.substring(i - 1, i + 1);
+                } else if (toBeRemoved.length() > 0) {
+                    toBeRemoved += currentChar;
+                }
+            }
+
+            // 갱신
+            if ((checkCharacter != currentChar) || (i == size - 1)) {
+                checkCharacter = currentChar;
+
+                if (toBeRemoved.length() > 0) {
+                    nextCryptogram = nextCryptogram.replaceAll(toBeRemoved, "");
+                    toBeRemoved = "";
+                }
+            }
+
+        }
+
+        result = nextCryptogram;
 
         return result;
     }
