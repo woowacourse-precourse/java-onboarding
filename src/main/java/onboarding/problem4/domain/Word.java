c2e152df9c79e9fcd435eddbe24fcc9734828b18
shinD <huipulci1@naver.com>
2022-10-31 01:48:59 +0900

    [FEAT] : Problem4 Word를 반대로 변환하는 기능 구현
    구현 기능
    - [X] Word를 반대로 변환한다.

--- src/main/java/onboarding/problem4/domain/Word.java
+++ src/main/java/onboarding/problem4/domain/Word.java
@@ -2,6 +2,18 @@ package onboarding.problem4.domain;
 
 public class Word {
 
+    private static final char BLANK = ' ';
+
+    /**
+     * 대문자와, 대응되는 대문자의 아스키코드를 더한 값
+     */
+    private static final int UPPER_CASE_COMPLEMENT = 155;
+
+    /**
+     * 소문자와, 대응되는 소문자의 아스키코드를 더한 값
+     */
+    private static final int LOWER_CASE_COMPLEMENT = 219;
+
     private final String word;
 
     public Word(String word) {
@@ -10,5 +22,28 @@ public class Word {
 
     public String value() {
         return word;
+    }
+
+    public Word reverse() {
+        StringBuilder sb = new StringBuilder();
+
+        word.chars()
+                .mapToObj(this::reverseChar)
+                .forEach(sb::append);
+        return new Word(sb.toString());
+    }
+
+    private char reverseChar(int ascii) {
+        if (!Character.isAlphabetic(ascii)) {
+            return BLANK;
+        }
+
+        return (char) (isLowerCase(ascii)
+                ? LOWER_CASE_COMPLEMENT - ascii
+                : UPPER_CASE_COMPLEMENT - ascii);
+    }
+
+    private static boolean isLowerCase(int ascii) {
+        return ('a' <= ascii) && (ascii <= 'z');
     }
 }
