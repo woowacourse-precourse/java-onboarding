436398cb362f77123c650be577ac673197fcff08
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2022-10-30 23:27:06 +0900

    문제 1 페이지 각 자리 곱 합 구해서 비교하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,25 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        String temp1,temp2;
+        temp1 = Integer.toString(pobi.get(0));
+        temp2 = Integer.toString(pobi.get(1));
+
+        int tempAdd = 0;
+        int tempMul =1;
+        int tempLeft,tempRight;
+        for (int i=0; i<temp1.length(); i++){
+            tempAdd += temp1.codePointAt(i);
+            tempMul *= temp1.codePointAt(i);
+        }
+
+        if(tempAdd>=tempMul)
+            tempLeft = tempAdd;
+        else
+            tempLeft = tempMul;
+
+
+
         return answer;
     }
 }
