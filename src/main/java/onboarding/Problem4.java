42832f015099e155130f066d12b788b84981cc35
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-10-31 00:54:47 +0900

    Problem4 소문자 테이블

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem4 {
+    private char lowerCasesTable[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     public static String solution(String word) {
         String answer = "";
         return answer;
