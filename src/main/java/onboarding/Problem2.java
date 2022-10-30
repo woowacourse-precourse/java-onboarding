be7a337e0035fd1e0cdbe8a53efd027531956223
rhqudco <rhqudco1204@naver.com>
2022-10-31 00:35:40 +0900

    FEAT : solution complete

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,6 +10,7 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = decryption(cryptogram);
         return answer;
     }
 
