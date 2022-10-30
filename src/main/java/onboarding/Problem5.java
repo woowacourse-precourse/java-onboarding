270c8d9dfeff29f489ca7fca018a95672e4f4448
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-10-30 14:12:29 +0900

    Update Problem5.java
    misson5-1
    problem solving

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        List<Integer> answer = new ArrayList<>();
+        int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        Chaneger(currency,answer,money);//돈 단위에 맞는 갯수 나누는 함수
+
         return answer;
+    }
+    public static  void  Chaneger(int[] currency, List<Integer> change, int money)
+    {
+        for(int curr:currency)
+        {
+            change.add(money/curr);
+            money%=curr;
+        }
     }
 }
