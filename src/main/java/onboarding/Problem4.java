5c3496091c1df06159e0a952fe7b9f8e0f138a21
kim0914 <kosa0914@naver.com>
2022-10-30 17:38:36 +0900

    feat: Problem4 상수 및 문자열 길이 예외반환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+
+        if (isWrongLength(answer)){
+            throw new IllegalArgumentException("1 이상 1000 이하의 길이를 가진 문자열이어야 합니다.");
+        }
+
+
         return answer;
+    }
+
+
+    private static boolean isWrongLength(String checkString){
+        int length = checkString.length();
+        return (length < 1 || length > 1000);
+    }
+
+    static class Const{
+        public static final int A_VALUE = 65;
+        public static final int Z_VALUE = 90;
+
+        public static final int a_VALUE = 97;
+        public static final int z_VALUE = 122;
+
+        public static final int UPPER_ALPHA_SUM = 155;
+        public static final int LOWER_ALPHA_SUM = 219;
     }
 }
