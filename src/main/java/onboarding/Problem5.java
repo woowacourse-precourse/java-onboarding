fd470b5fdef832e28cf6fe4e68b209397728b051
SeokHwan An <asok369258@naver.com>
2022-10-30 15:52:04 +0900

    feat [Problem5] : 주어진 money의 값이 1이상 1,000,000 이하인지 검증하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int MONEY_MIN = 1;
+    private static final int MONEY_MAX = 1000000;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    public static boolean checkMoneyValueValid(int money) {
+        return MONEY_MIN <= money && money <= MONEY_MAX;
+    }
+
 }
