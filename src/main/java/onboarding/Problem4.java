7683f3a85ba61b71e7dbf0c027716c491d1db3ca
SonMinGyu <littleking13@naver.com>
2022-10-31 03:14:13 +0900

    [FEAT] Problem4 ASCII to word 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,17 +6,18 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
+        answer = changeASCIIToWord(reverseWord(changeWordToASCII(word)));
 
         return answer;
     }
 
-    public static int[] changeASCII(String word) {
+    public static int[] changeWordToASCII(String word) {
         int[] wordASCIIArray = new int[word.length()];
         String[] wordArray = word.split("");
 
-
         for(int i = 0; i < wordArray.length; i++) {
-            wordASCIIArray[i] = Integer.parseInt(wordArray[i]);
+            char c = wordArray[i].charAt(0);
+            wordASCIIArray[i] = (int) c;
         }
 
         return wordASCIIArray;
@@ -33,5 +34,17 @@ public class Problem4 {
         }
 
         return wordASCIIArray;
+    }
+
+    public static String changeASCIIToWord(int[] reversedASCIIArray) {
+        char[] reversedWordArray = new char[reversedASCIIArray.length];
+
+        for(int i = 0; i < reversedASCIIArray.length; i++) {
+            reversedWordArray[i] = (char)reversedASCIIArray[i];
+        }
+
+        String result = new String(reversedWordArray);
+
+        return result;
     }
 }
