77203a1fedf6f0358a301bbee4507cd32a725e62
HwangSeokJin <ghkd3703@gmail.com>
2022-10-30 12:24:05 +0900

    문제 2) 중복된 문자를 제거하는 기능 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,17 +7,29 @@ public class Problem2 {
         Stack<Character> stack = new Stack<>();
 
         for (char c: cryptogram.toCharArray()){
-            if (!stack.empty() && stack.peek().equals(c)){
-                stack.pop();
-                continue;
-            }
+            if (isDuplicated(stack, c)) continue;
             stack.push(c);
         }
+
 
         for (char c: stack){
             answer += c;
         }
 
         return answer;
+    }
+
+    private static boolean isDuplicated(Stack<Character> stack, char c){
+        if (stack.isEmpty()) return false;
+        if (stack.peek().equals(c) || stack.peek().equals(Character.toUpperCase(c))) {
+            stack.pop();
+            stack.push(Character.toUpperCase(c));
+            return true;
+        }
+        if (Character.isUpperCase(stack.peek())){
+            stack.pop();
+            return isDuplicated(stack, c);
+        }
+        return false;
     }
 }
