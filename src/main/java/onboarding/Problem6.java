c6601e6bdd66a9322dba16c0a24a8b135da0fc2a
Laeng <Laeng@users.noreply.github.com>
2022-10-31 00:20:36 +0900

    feat(problem6): 크루 닉네임 중복 확인 메서드 재작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,15 +7,37 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Problem6 {
-    private static ArrayList<List<String>> crews = new ArrayList<>();
-    private static HashMap<String, Integer> twoLetterNicknamesAndIds = new HashMap<>();
+    private static ArrayList<List<String>> crews;
+    private static HashMap<String, Integer> twoLetterNicknamesAndIds;
 
     public static List<String> solution(List<List<String>> forms) {
-
+        initialize();
 
         return forms.get(0);
     }
 
+    /**
+     * 크루 닉네임 중복 확인
+     * @param id 크루 고유 번호
+     * @return 중복 여부
+     */
+    private static boolean isDuplicateNickname(int id) {
+        List<String> crew = getCrewById(id);
+        String nickname = crew.get(0);
+        String[] twoLetterNicknames = createTwoLetterNicknames(nickname);
+        ArrayList<String> internalDuplicateTwoLetterNicknames = new ArrayList<>();
+
+        for (String twoLetter : twoLetterNicknames) {
+            if (!internalDuplicateTwoLetterNicknames.contains(twoLetter) && isTwoLetterNickname(twoLetter)) {
+                return true;
+            }
+
+            internalDuplicateTwoLetterNicknames.add(nickname);
+            putTwoLetterNicknameAndId(twoLetter, id);
+        }
+
+        return false;
+    }
 
     /**
      * 크루 닉네임을 한 글자씩 겹쳐 두 글자로 묶기
@@ -45,6 +67,10 @@ public class Problem6 {
         }
     }
 
+    private static boolean isTwoLetterNickname(String twoLetterNickname) {
+        return twoLetterNicknamesAndIds.containsKey(twoLetterNickname);
+    }
+
     /**
      * 분할된 크루 닉네임을 통해 크루 고유 번호 검색
      * @param twoLetterNickname 분할된 크루 닉네임의 일부
@@ -70,5 +96,10 @@ public class Problem6 {
      */
     private static List<String> getCrewById(int id) {
         return crews.get(id);
+    }
+
+    private static void initialize() {
+        crews = new ArrayList<>();
+        twoLetterNicknamesAndIds = new HashMap<>();
     }
 }
