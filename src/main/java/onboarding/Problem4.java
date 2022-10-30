79176a382d101b5e164b2c9e781ff06f2951d9d1
WonSeok-dd <cluejws@gmail.com>
2022-10-30 17:58:16 +0900

    Problem4 | feat: 문자 반대로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,6 +8,25 @@ package onboarding;
  * 2. 문자 합쳐서 문자열로 합치기
  */
 public class Problem4 {
+
+    private static boolean isUpperCase(char alp){
+        int alpCode = (int) alp;
+        return (65 <= alpCode && alpCode <= 90);
+    }
+    private static boolean isLowerCase(char alp){
+        int alpCode = (int) alp;
+        return (97 <= alpCode && alpCode <= 122);
+    }
+    private static char reverseAlphaBet(char alp){
+
+        if (isUpperCase(alp)){
+            return (char)('Z' - alp + 'A');
+        } else if (isLowerCase(alp)) {
+            return (char)('z' - alp + 'a');
+        }else{
+            return alp;
+        }
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
