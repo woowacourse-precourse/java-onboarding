fd2a20dc18ac4ea32d4f872f21ae60b37a916d18
apptie <appti@naver.com>
2022-10-30 15:02:38 +0900

    refactor: 문제 6 CrewRepository 사용하지 않는 필드 제거

--- src/main/java/onboarding/problem6/CrewRepository.java
+++ src/main/java/onboarding/problem6/CrewRepository.java
@@ -6,20 +6,14 @@ import static onboarding.problem6.consts.FormIndexConst.FORM_NICKNAME_INDEX;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CrewRepository {
 
-    private final Map<String, String> partOfNicknameMap;
-    private final Set<String> duplicateNicknameCrewSet;
     private final List<Crew> crews;
 
     public CrewRepository(List<List<String>> forms) {
-        this.partOfNicknameMap = new HashMap<>();
-        this.duplicateNicknameCrewSet = new HashSet<>();
         this.crews = initCrews(forms);
     }
 
