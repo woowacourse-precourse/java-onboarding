53d855fde6dbf417cf0dc2b467a44b840371319f
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-31 01:21:37 +0900

    5번 문제 해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,14 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import onboarding.prob5.domain.Banker;
+import onboarding.prob5.domain.Coin;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        Banker banker = new Banker();
+        answer = banker.solve(money);
         return answer;
     }
 }
