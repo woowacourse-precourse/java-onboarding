adfbb297418bbbc376622d1bcc778ecb923b3fb3
LeeJinHo <hosboy93@naver.com>
2022-10-30 17:30:25 +0900

    문제1. crong 덧셈, 곱셈, 최댓값 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,11 +7,15 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         
         int pobiMax = 0;
+        int crongMax = 0;
         
         for(int i=0; i<2; i++) {
         	int pobiValue = pobi.get(i);
             int pobiSum = 0;
             int pobiMul = 1;
+            int crongValue = crong.get(i);
+            int crongSum = 0;
+            int crongMul = 1;
         	
         	while(pobiValue!=0) {
         		pobiSum += pobiValue % 10;
@@ -19,8 +23,20 @@ class Problem1 {
         		pobiValue = pobiValue / 10;
         	}
         	pobiMax = Math.max(pobiMax, Math.max(pobiSum, pobiMul));
+        	
+        	while(crongValue!=0) {
+        		crongSum += crongValue % 10;
+        		crongMul *= crongValue % 10;
+        		crongValue = crongValue / 10;
+        	}
+        	crongMax = Math.max(crongMax,  Math.max(crongSum,  crongMul));
         }
         
+        if(pobiMax>crongMax) answer = 1;
+        else if(pobiMax<crongMax) answer = 2;
+        else answer = 0;
+        
+        System.out.println(answer);
         return answer;
     }
 }
