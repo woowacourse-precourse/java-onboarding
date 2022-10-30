a701258f00b7fc3da9fd16bf50ef00542d42d476
Longstick0 <789456jang@naver.com>
2022-10-31 04:22:49 +0900

    refactor: Problem6 코드 리펙토링(메서드 분리 및 변수명 변경)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,24 +3,54 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
+
+    private static final int EMAIL = 0;
+    private static final int NICKNAME = 1;
+
+
     public static List<String> solution(List<List<String>> forms) {
 
-        Set<String> a = new HashSet<>();
+        Set<String> resultSet = new HashSet<>();
 
-        for (int i = 0; i < forms.size(); i++) {
-            String nickname = forms.get(i).get(1);
-
+        for (List<String> initForm : forms) {
+            String nickname = getListValue(initForm, NICKNAME);
             if (nickname.length() > 1) {
-                for (int j = 0; j < nickname.length() - 1; j++) {
-                    String nickSub = nickname.substring(j, j + 2);
-                    for (List<String> form : forms) {
-                        if (form.get(1).contains(nickSub) && !form.get(1).equals(nickname)) {
-                            a.add(form.get(0));
-                        }
-                    }
+                findEmailsWithDuplicateNickname(forms, resultSet, nickname);
+            }
+        }
+        return sortList(resultSet);
+    }
+
+    public static void findEmailsWithDuplicateNickname(List<List<String>> forms, Set<String> resultSet, String nickname) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String nickSub = nickname.substring(i, i + 2);
+            for (List<String> form : forms) {
+                if (isContainsSubString(nickSub, form) && isNotSameOwnName(nickname, form)) {
+                    addEmailToResult(resultSet, form);
                 }
             }
         }
-        return new ArrayList<>(a);
+    }
+
+    private static boolean addEmailToResult(Set<String> resultSet, List<String> form) {
+        return resultSet.add(getListValue(form, EMAIL));
+    }
+
+    private static boolean isContainsSubString(String nickSub, List<String> form) {
+        return getListValue(form, NICKNAME).contains(nickSub);
+    }
+
+    private static boolean isNotSameOwnName(String nickname, List<String> form) {
+        return !getListValue(form, NICKNAME).equals(nickname);
+    }
+
+    private static String getListValue(List<String> form, int nicknameIndexNum) {
+        return form.get(nicknameIndexNum);
+    }
+
+    private static List<String> sortList(Set<String> a) {
+        List<String> answer = new ArrayList<>(a);
+        answer.sort(Comparator.naturalOrder());
+        return answer;
     }
 }
