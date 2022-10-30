dfc15426e9d6d353810d1a4fab441f243be03991
yw824 <yongwoo25@gmail.com>
2022-10-31 04:53:16 +0900

    feat: [Problem3] Solution 함수 구현
    - Solution 함수:
    	- 매 반복마다 해당 횟수만큼 해당 반복수를 인자로 하여
    	- 해당 순번의 총 박수 횟수를 구하는 함수인
    	- getClaps(int) 함수를 호출하여
    	- 최종 반환값인 answer에 더해 준다.
    	- 반복문이 끝난 후 최종 반환값인 answer를 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,12 +7,11 @@ public class Problem3 {
         // implement
         for(int i = 1; i <= number; ++i)
         {
-            // implement
             // 각 숫자마다 쳐야 하는 박수의 횟수를 구해서 더하기
+            answer += getClaps(i);
         }
         return answer;
     }
-
     // 각 자릿수에 3, 6, 9가 있는 만큼 박수를 쳐야 한다.
     // 각 숫자마다 쳐야 하는 박수의 횟수를 구해서 반환
     static int getClaps(int input)
