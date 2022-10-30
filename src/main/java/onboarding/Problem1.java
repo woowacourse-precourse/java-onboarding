3a133a3502e1f4d734fb18310e4d958a89c77b00
baksohyeon <bori12370@gmail.com>
2022-10-30 23:28:39 +0900

    feat: add findException method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public boolean findException(List<Integer> arr) {
+        int leftPage = arr.get(0);
+        int rightPage = arr.get(1);
+        if (leftPage % 2 == 1 && rightPage == leftPage + 1) {
+            return leftPage > 1 && rightPage < 400;
+        } else {
+            return false;
+        }
+    }
 }
