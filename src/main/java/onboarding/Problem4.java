098c514d855a55acb87ed67630bbad5a60513242
Donguk <95donguk.dev@gmail.com>
2022-10-30 09:40:23 +0900

    feat: 문제4의 문자열을 쪼개는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        List<Character> charList = new ArrayList<>();
+        splitWord(word, charList);
         return answer;
+    }
+
+    private static List<Character> splitWord(String word, List<Character> charList) {
+        char[] chars = word.toCharArray();
+        for (char aChar : chars) {
+            charList.add(aChar);
+        }
+        return charList;
     }
 }
