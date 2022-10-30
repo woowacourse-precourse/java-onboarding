099b8f3a1d7871ceb7cf691fce8a5f8ed0fdb932
이지혁 <olzlgur@naver.com>
2022-10-31 02:50:44 +0900

    페이지 점수 반환 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,8 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Math.max;
+
 class Problem1 {
     private final int number = 0;
 
@@ -9,19 +11,30 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
-    private boolean validatePageNumber(List<Integer> player) {
+    private static boolean validatePageNumber(List<Integer> player) {
         int left = 0;
         int right = 1;
 
         if (player.get(left) - player.get(right) != 1) {
             return false;
         }
-        if (player.get(left) < 0 || player.get(1) > 400 ){
+        if (player.get(left) < 0 || player.get(1) > 400) {
             return false;
         }
         if (player.get(left) % 2 != 1 || player.get(right) != 0) {
             return false;
         }
         return true;
+    }
+    private static int calPageScore(int page) {
+        int sum = 0;
+        int mul = 1;
+
+        while (page > 0) {
+            sum += page % 10;
+            mul *= page % 10;
+            page /= 10;
+        }
+        return max(sum, mul);
     }
 }
