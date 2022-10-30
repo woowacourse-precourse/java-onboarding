1c2573cca0174b8edaa61c1b7635ebfa29cf7c60
yun8565 <yunyun8565@naver.com>
2022-10-30 23:51:42 +0900

    feat(P3): 손뼉 횟수 계산 기능 추가
    숫자를 문자열로 변환 후 Stream을 생성해 3,6,9가 포함된 문자의 개수를 반환하도록 했다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
+    public static final String REGEX = "[3|6|9]";
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static int getClapCount(int number) {
+        return (int)Stream.of(String.valueOf(number).split(""))
+                .filter(num -> num.matches(REGEX))
+                .count();
     }
 }
