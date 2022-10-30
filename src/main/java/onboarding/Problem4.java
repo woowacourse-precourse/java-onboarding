2e1a06c33408ebae317bde576c0f1255a0c1c5fa
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-30 16:23:44 +0900

    [feat] 입력 문자 한 글자씩 탐색 후 변환값 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,14 +2,32 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         String dictCap = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
         String dictSmall = "zyxwvutsrqponmlkjihgfedcba";
+        int num = 0;
 
+        for(int i = 0;i<word.length();i++){
 
-        return answer;
+            if((int)word.charAt(i) == 32)
+            {
+                answer.append(" ");
+                continue;
+            }
+            if(Character.isUpperCase(word.charAt(i)))
+            {
+                // 대문자
+                num = (int)word.charAt(i) - 65;
+                answer.append(dictCap.charAt(num));
+                continue;
+            }
+            num = (int)word.charAt(i) - 97;
+            answer.append(dictSmall.charAt(num));
+
+        }
+
+        return answer.toString();
     }
 
-    public static void main(String[] args) {
-    }
+
 }
