488b9ccc47165bba61c6bd006cfa4b2ad71ab408
baexxbin <soo3886@gmail.com>
2022-10-30 14:42:58 +0900

    Feat(PROBLEM4): "문자변환 기능구현"
    아스키코드를 통한 문자열 변환 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public static String solution(String word) {
+		char[] wordList = word.toCharArray();
+		int idx = 0;
+
+		for (int ascii : wordList) {
+			if (65 <= ascii && ascii <= 90) {
+				wordList[idx] = (char)((char)25 - (ascii - 65) + 65);
+			} else if (97 <= ascii && ascii <= 122) {
+				wordList[idx] = (char)((char)25 - (ascii - 97) + 97);
+			} else {
+				wordList[idx] = (char)ascii;
+			}
+			idx++;
+		}
+		return String.valueOf(wordList);
+	}
 }
