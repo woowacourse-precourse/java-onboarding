3159f4e051efda5b3b20f32b43e3d894e266c2e2
Jeawoong Kim <kimje1109@gmail.com>
2022-10-31 00:38:08 +0900

    feat: Add removeDuplicateChars

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,21 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeDuplicateChars(String string) {
+        StringBuilder result = new StringBuilder();
+
+        for (int index = 0; index < (string.length() -1) ; index++) {
+            if (string.charAt(index) != string.charAt(index + 1)) {
+                result.append(string.charAt(index));
+            }
+            if (string.charAt(index) == string.charAt(index + 1)) {
+                index++;
+            }
+        }
+
+        return result.toString();
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
