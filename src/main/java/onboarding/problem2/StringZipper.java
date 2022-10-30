e5145422dbc1b2611234f9ef0064e72933a8eb07
hodomaroo <enghemdwkd@gmail.com>
2022-10-30 14:19:44 +0900

    [Problem2/FEAT] Problem2의 요구사항 명세에 맞게 zipStringOneTime 메서드를 구현하였습니다.

--- src/main/java/onboarding/problem2/StringZipper.java
+++ src/main/java/onboarding/problem2/StringZipper.java
@@ -1,6 +1,17 @@
 package onboarding.problem2;
 
 public class StringZipper {
+    
+    public static String zipStringOneTime(String cryptogram) {
+        StringBuilder zippedString = new StringBuilder();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!checkContinuity(cryptogram, i)) {
+                zippedString.append(cryptogram.charAt(i));
+            }
+        }
+        return zippedString.toString();
+    }
 
     public static boolean checkContinuity(String string, int index) {
         if (index != 0 && string.charAt(index - 1) == string.charAt(index)) {
