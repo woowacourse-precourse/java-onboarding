979333c9162822ffa1750c563648569bcd8796bb
ECHO <eunchan1001@gmail.com>
2022-10-30 23:02:36 +0900

    feat(Problem3): 솔루션 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    public static void main(String[] args) {
+        System.out.println(solution(13));
+    }
+    static String getRangedString(int n) {
+        return null;
+    }
+
+    static int getCountOfNumbers(String s) {
+        return 0;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        String numberString = getRangedString(number);
+        answer = getCountOfNumbers(numberString);
         return answer;
     }
 }
