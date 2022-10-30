f71474efe176b7b27380c5f25d7bf4630741d72e
Lee-Jiseung <ljss98@naver.com>
2022-10-30 16:43:47 +0900

    feat: problem1 예외 처리하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,13 +7,34 @@ class Problem1 {
     private static final int WIN_POBI = 1;
     private static final int WIN_CRONG = 2;
     private static final int DRAW = 0;
+    private static final int EXCEPTIONS = -1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        if (hasExceptions(pobi) || hasExceptions(crong)) {
+            return EXCEPTIONS;
+        }
 
         int scorePobi = maxScore(List.of(add(pobi.get(0)), multiply(pobi.get(0)), add(pobi.get(1)), multiply(pobi.get(1))));
         int scoreCrong = maxScore(List.of(add(crong.get(0)), multiply(crong.get(0)), add(crong.get(1)), multiply(crong.get(1))));
 
         return decideWinner(scorePobi, scoreCrong);
+    }
+
+    private static boolean hasExceptions(List<Integer> pageNumbers) {
+        int left = pageNumbers.get(0);
+        int right = pageNumbers.get(1);
+
+        if (left + 1 != right) {
+            return true;
+        }
+        if (left % 2 == 0) {
+            return true;
+        }
+        if (left < 1 || right > 400) {
+            return true;
+        }
+        return false;
     }
 
     private static int add(int pageNumber) {
