9251e7aec2e2400a58eb24a1be395117c7deb655
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-31 04:51:41 +0900

    feat(Problem6): 중복된 이메일이 있는지 검증하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        HashMap<String, String> crewDictionary = new HashMap<>();
+        validateDuplicateEmail(forms, crewDictionary);
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static void validateDuplicateEmail(List<List<String>> forms,HashMap<String, String> crewDictionary){
+        for(int i=0; i< forms.size(); i++){
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
+            if(crewDictionary.containsKey(email)){
+              throw new IllegalArgumentException("이미 해당 이메일이 존재합니다");
+            }
+            crewDictionary.put(email, nickname);
+      }
     }
 }
