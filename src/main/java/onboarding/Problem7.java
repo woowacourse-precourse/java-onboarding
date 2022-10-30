bfb386326ed62d63a4c81748b606524edafe4165
KSH <tkdgns543@gmail.com>
2022-10-31 01:51:44 +0900

    feat(Problem7): solution메서드 내에 친구 후보 들의 아이디를 key, 점수를 value로 저장할 Map 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, Integer> scores = new HashMap<>();
+
     }
 }
