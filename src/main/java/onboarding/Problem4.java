910221c49133914b7b1d109bda4a5b3271dde625
yxnxx <jh4rgh0@gmail.com>
2022-10-30 23:19:59 +0900

    feat: Add Problem4 making dictionary

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        HashMap<Character, Character> lower_dict = makeDictionary('a', 'z');
+        HashMap<Character, Character> upper_dict = makeDictionary('A', 'Z');
         return answer;
+    }
+
+    public static HashMap<Character, Character> makeDictionary(Character start, Character end) {
+        HashMap<Character, Character> dict = new HashMap<>();
+        Character dict_word = end;
+        for(Character c = start; c <= end; c++) {
+            dict.put(c, dict_word);
+            dict_word--;
+        }
+        return dict;
     }
 }
