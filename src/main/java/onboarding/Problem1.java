ec17b21e1c178220b2386e42b3cec6312390e596
swonny <wonny921@gmail.com>
2022-10-31 00:13:50 +0900

    feat: 페이지 자리수 분리 기능 구현, 페이지 각 자리수 더하기 연산 기능 구현
    페이지 자리수 분리 기능(seperateInteger() 메서드)
    - List의 초기화 이슈를 해결하지 못해 각 자리수를 나눠 저장한 배열은 int배열로 선언하였다.
    - 하나의 함수는 하나의 기능만 하도록 구현하기 위해 seperateInteger메서드에서 integerToString메서드를 호출하도록 구현하였다.
    
    페이지 각 자리수 더하기 연산 기능 구현 (add() 메서드)
    - 기존에 add() 메서드에서 포비와 크롱의 페이지 배열을 전달받아 네 경우의 수를 모두 add안에서 처리하도록 구현했던 함수를 분리하였다.
    - 더하기 기능만 하는 메서드로 구현하고자 getHighestScore에서 점수를 하나씩 전달받아 각 자리수를 더한 결과만 리턴하도록 구현하였다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,18 +3,53 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int add(List<Integer> arr) {
-        int length = arr.size();
+    /* 숫자를 문자열로 변환하는 메서드 */
+    public static String integerToString(int num) {
+        return Integer.toString(num);
+    }
+
+    /* 10의 자리수 이상의 숫자를 나눠 배열로 만드는 메서드 */
+    public static int[] seperateInteger(int num) {
+        int[] result = new int[3];
+        if (num < 10) {
+            result[0] = num;
+            return result;
+        }
+        String stringNum = integerToString(num);
+        for (int i = 0; i < stringNum.length(); i++) {
+            int tmp = stringNum.charAt(i) - '0';
+            result[i] = tmp;
+        }
+        return result;
+    }
+
+    public static int add(int score) {
+        int[] individualNums = seperateInteger(score);
         int sum = 0;
-        for (int i = 0; i < length; i++) {
-            System.out.println(i);
-            sum += arr.get(i);
+        for (int i = 0; i < 3; i++) {
+            sum += individualNums[i];
         }
         return sum;
     }
+
+    public static int getHighestScore(List<Integer> arr) {
+        int max = 0;
+        int tmp;
+        for(Integer score : arr) {
+            System.out.println(score);
+            tmp = add(score);
+            if(tmp > max) {
+                max = tmp;
+            }
+        }
+        return max;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong){
-        int result = add(pobi);
-        System.out.println(result);
+        int pobiScore = getHighestScore(pobi);
+        int crongScore = getHighestScore(crong);
+        System.out.println(pobiScore);
+        System.out.println(crongScore);
         int answer = Integer.MAX_VALUE;
         return answer;
     }
