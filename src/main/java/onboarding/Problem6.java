7a6cfe6e1fd0c6da92a8d0fd54d759a28c4269d6
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-30 21:51:30 +0900

    Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> emails = new ArrayList<>();
+
+        for (int i=0; i < forms.size(); i++) {
+            for(int j=0; j < forms.size(); j++) {
+                if ( isDuplicated(forms.get(i).get(1), forms.get(j).get(1))) {
+                    emails.add(forms.get(i).get(0));
+                    emails.add(forms.get(j).get(0));
+                }
+            }
+        }
+
+        Collections.sort(emails);
+
+        return emails;
+    }
+
+    private static boolean isDuplicated(String email1, String email2) {
+        for (int i = 0; i < email1.length(); i++) {
+            for (int j = 0; j < email2.length(); j++) {
+                if (email1.substring(i, i+1).equals(email2.substring(j, j+1))) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
