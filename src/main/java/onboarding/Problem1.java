5a756aa8badef4ccfee4d940409cd637287f579c
wugawuga <dnr3655@naver.com>
2022-10-31 02:16:57 +0900

    feat: 왼쪽면 오른쪽면이 서로 맞은 면인지 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,7 +12,14 @@ class Problem1 {
     }
 
     private static boolean validPages(List<Integer> pobi, List<Integer> crong) {
+        if (isNotPairPages(pobi, crong)) {
+            return true;
+        }
         return isFirstPageAndLastPage(pobi, crong);
+    }
+
+    private static boolean isNotPairPages(List<Integer> pobi, List<Integer> crong) {
+        return pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1;
     }
 
     private static boolean isFirstPageAndLastPage(List<Integer> pobi, List<Integer> crong) {
