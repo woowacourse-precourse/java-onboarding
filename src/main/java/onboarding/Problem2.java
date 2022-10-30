6abf93072edfe2c3aaf4e5d15140847508b2daa1
dongdong-119 <85717356+dongdong-119@users.noreply.github.com>
2022-10-30 15:09:35 +0900

    Problem 2 - 기능목록 작성 및 테스트 클래스 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/**
+ * <기능 목록>
+ *     1. 메소드
+ *          1) deduplicate
+ *          : '현재 문자열'에서 중복되는 단어들을 제거한 문자열을 반환한다.
+ *
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
