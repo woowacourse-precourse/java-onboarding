00c6bfee6d033d6a2e38d93b5bb4ea89640ea0f3
김지건 <jigun21@naver.com>
2022-10-30 09:21:46 +0900

    Init: Problem5 초기구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    public static int[] curruncyList = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for(int curruncy : curruncyList){
+            answer.add(money/curruncy);
+            money%=curruncy;
+        }
         return answer;
     }
+
 }
