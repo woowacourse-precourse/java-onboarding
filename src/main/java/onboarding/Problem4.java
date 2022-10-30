1aa33214ab1691c1f53751ee26b2d055535b1fd6
syhan7516 <syhan7516@naver.com>
2022-10-30 14:38:45 +0900

    Problem4 - 2번 기능 : 각 문자 정보 확인 & 결과 확인

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -19,14 +19,31 @@ public class Problem4 {
         return TransList;
     }
 
+    // 2. 각 문자 정보 확인
+    static void getCheckAlpha(List<Character> wordList)
+    {
+        for(int idx=0; idx<wordList.size(); idx++)
+        {
+            // 각 문자를 저장
+            char alpha = wordList.get(idx);
+            
+            // 문자가 알파벳인지 확인
+            if(alpha >='A' && alpha <= 'Z' || alpha >= 'a' && alpha <= 'z' )
+            {
+                // 결과 확인
+                System.out.println(alpha);
+            }
+        }
+    }
+
     public static String solution(String word) {
         String answer = "";
 
         // 문자열 리스트화
         List<Character> wordList = getTransList(word);
-        
-        // 리스트화 결과 확인
-        System.out.println(wordList);
+
+        // 문자 확인
+        getCheckAlpha(wordList);
 
         return answer;
     }
@@ -34,12 +51,12 @@ public class Problem4 {
 
 // 기능 요구 사항
 // 1. 엄마 말씀을 문자 단위 리스트화
-// 2. 각 문자 정보 확인하기
+// 2. 각 문자 정보 확인
 // 3. 알파벳 변환하기 ( 제한사항 생각 )
 // 4. 문자 단위 리스트를 문자열로 변환
 
 // 프로그래밍 요구 사항
-// 1. JDK 11 버전
+// ✔ 1. JDK 11 버전
 // 2. "build.gradle"을 변경 X, 외부 라이브러리 X
 // 3. 프로그램 종료 시 "System.exit()"를 호출 X
 // 4. 프로그램 구현이 완료되면 "ApplicationTest" 의 모든 테스트 성공하기
