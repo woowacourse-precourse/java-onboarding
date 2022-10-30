a86fb99e18b9a0ff68baa90b0727438f152e58c6
kong0527 <o____ri@naver.com>
2022-10-31 01:37:11 +0900

    Problem6 complete

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,41 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        HashMap<String, List<String>> hashMap = new HashMap<>();
+        HashSet<String> ans = new HashSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            String st = forms.get(i).get(1);
+            for (int j = 0; j < st.length() - 1; j++) {
+                String tmp = st.substring(j, j + 2);
+                List<String> mail;
+                if (hashMap.containsKey(tmp)) {
+                    mail = hashMap.get(tmp);
+                }
+
+                else {
+                    mail = new ArrayList<>();
+                }
+                mail.add(forms.get(i).get(0));
+                hashMap.put(tmp, mail);
+            }
+        }
+
+        for (String key : hashMap.keySet()) {
+            List<String> mail = hashMap.get(key);
+            if (mail.size() > 1) {
+                for (int i = 0; i < mail.size(); i++) {
+                    ans.add(mail.get(i));
+                }
+            }
+        }
+
+        List<String> answer = new ArrayList<>(ans);
+        Collections.sort(answer);
+
         return answer;
     }
 }
