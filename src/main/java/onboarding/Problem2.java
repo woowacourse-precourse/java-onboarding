b1e5e01931bdf86d0f27dcf2aeb879da70ea0843
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-10-30 22:10:23 +0900

    chore(Problem2): add comments outlining feature list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,18 @@
 package onboarding;
 
 public class Problem2 {
+    // TODO: class Cryptogram
+        // TODO: Cryptogram(String encrypted) { constructor }
+
+        // TODO: validate()
+
+        // TODO: decrypt()
+
     public static String solution(String cryptogram) {
+        // Instantiate Cryptogram
+
+        // Return decrypted Cryptogram
+
         String answer = "answer";
         return answer;
     }
