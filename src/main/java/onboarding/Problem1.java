4181ea74c37fcd52e79c6f6d8ae0a12869b0b01d
benjaminuj <chosj1526@gmail.com>
2022-10-30 16:06:12 +0900

    feat: 범위 초과,미만 값과 null값 예외처리기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,28 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Scanner;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int bookFirstPage = 1;
+        int bookLastPage = 400;
+
+        try {
+            for(int index = 0; index <2 ; index ++) {
+                if ( bookFirstPage <= pobi.get(index)  &&  pobi.get(index) <= bookLastPage ) {
+
+                }
+
+                if ( bookFirstPage <= crong.get(index)  &&  crong.get(index) <= bookLastPage ) {
+
+                }
+            }
+        } catch (Exception exception) {
+            System.out.println("입력값은 1이상 400이하의 숫자만 가능합니다.(null 불가)");
+        }
         return answer;
     }
+
 }
