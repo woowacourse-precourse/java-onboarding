1a5256377dedf2646d8016d84e80975433de4760
wonseokjee <wsji9404@gmail.com>
2022-10-30 14:27:05 +0900

    221030 problem5-1, 액수 단위별Hashmap 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem5 {
@@ -8,7 +9,21 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
-//  1. hashmap으로 9개 금액 별로 나누는 매서드(큰 값이 키값 1부터 시작)
+
+    //  1. hashmap으로 9개 금액 별로 나누는 매서드(큰 값이 키값 1부터 시작)\
+    public static Integer moneyValue(int moneyKey) {
+        HashMap<Integer, Integer> dict = new HashMap<>();
+        dict.put(0, 50000);
+        dict.put(1, 10000);
+        dict.put(2, 5000);
+        dict.put(3, 1000);
+        dict.put(4, 500);
+        dict.put(5, 100);
+        dict.put(6, 50);
+        dict.put(7, 10);
+        dict.put(8, 1);
+        return dict.get(moneyKey);
+    }
 //  2. for문으로 가장 큰 값부터 나누고 나눈값 같은 index자리 result에 추가 나머지 갱신
 
 }
