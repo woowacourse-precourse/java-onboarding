9afb60e395982ab6272f47e26d680337e0aa96f1
KYBee <kyb3634@naver.com>
2022-10-30 14:10:55 +0900

    :zap: [Problem 3] Implement playGame method
    [Problem 3] Implement playGame method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -19,7 +19,19 @@ public class Problem3 {
      * @return : int, 몇 번의 박수를 쳐야 하는지
      */
     public static int playGame(int number) {
-        int answer = 0;
-        return answer;
+        int[] clapCount = new int[Math.max(10, number + 1)];
+        clapCount[3] = 1;
+        clapCount[6] = 1;
+        clapCount[9] = 1;
+
+        int[] howManyClap = new int[number + 1];
+        for (int currentNumber = 1; currentNumber <= number; currentNumber++) {
+            int lastDigit = currentNumber % 10;
+            int numberWithoutLastDigit = currentNumber / 10;
+            clapCount[currentNumber] = clapCount[lastDigit] + clapCount[numberWithoutLastDigit];
+            howManyClap[currentNumber] = clapCount[currentNumber] + howManyClap[currentNumber - 1];
+        }
+
+        return howManyClap[number];
     }
 }
