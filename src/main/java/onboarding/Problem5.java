1c4daeae5e932a1f303892bd07fd47a12ca59509
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 21:46:12 +0900

    feat:[Problem5] solution 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer =minCount(money);
         return answer;
     }
 
