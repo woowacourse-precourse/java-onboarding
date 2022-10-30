b1024e0db58df4512b623396b3f14633d8133f76
ycopp9 <aciees95@gmail.com>
2022-10-30 19:16:37 +0900

    feat: add the class Person() that has name, friends, visit

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,21 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
+    static class Person {
+        String name;
+        int visit;
+        Set<Person> friends;
+        Person(String name) {
+            this.name = name;
+            this.visit = 0;
+            this.friends = new HashSet<>();
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
