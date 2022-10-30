a30383fd9ff956f06760e5fbe52803019802f274
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-10-31 01:33:19 +0900

    for문을 통한 1차적 중복제거
    1->2단계

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,9 +4,20 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
 
-//        1. for문을 사용하여 1차적으로 연속하는 중복 문자들을 삭제할 수 있는 기능
-//        2. while 문을 통해 연속되는 중복문자들이 없을 때까지 실행
+//        1. 1차적으로 중복되는 문자들을 모두 삭제
+        char[] chars = cryptogram.toCharArray();
+        for(int i = 0; i < chars.length-1; i++){
+            if(chars[i] != chars[i+1] ){
+                answer += chars[i];
+                if(i == chars.length-2){
+                    answer += chars[i+1];
+                }
+            }
+            else { i += 1;}
 
+        }
+
+//        2. while 문을 통해 연속되는 중복문자들이 없을 때까지 실행
 
         return answer;
     }
