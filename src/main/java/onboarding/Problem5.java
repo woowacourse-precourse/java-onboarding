0757bd247a94100f9af2fdb30fe459ded241552d
HiiWee <wpdlzhf159@gmail.com>
2022-10-31 01:34:29 +0900

    Feat problem5: 특정 지폐의 카운트 구하여 리스트에 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    static final int[] MEASURE = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            answer.add(getCountOfBill(MEASURE[i], money));
+        }
         return answer;
+    }
+
+    public static int getCountOfBill(int measure, int won) {
+        return won / measure;
     }
 }
