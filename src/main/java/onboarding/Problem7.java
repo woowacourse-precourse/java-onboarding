1169885e5902995c30e029cbfef520a938c6b77b
seonghoo1217 <seonghoo1217@naver.com>
2022-10-30 18:23:07 +0900

    feat : Add a User Id Validation Exception on [Task 7]

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,18 +1,23 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         if (validation("user_length",user.length())&&validation("friends_size",friends.size())&&validation("visitors_size",visitors.size())){
             //닉네임 길이 예외처리&&친구 리스트 배열길이 예외처리
+            for (List<String> friend:friends){
+                if(validation("friends_e_length",friend.size())){
+                    String own = friend.get(0);
+                    String fri = friend.get(1);
+                    if (validation("user_valid",own)&&validation("user_valid",fri)){
+                    }
+                }
+            }
         }
         return answer;
     }
-
 
     private static <T>boolean validation(String err_type,T err_param){
         if (err_type.equals("user_length")&&err_param instanceof Integer){
@@ -27,7 +32,15 @@ public class Problem7 {
             int visitors_size = (int) err_param;
             return 0<visitors_size&&visitors_size<10001;
         }
-
+        else if(err_type.equals("friends_e_length")&&err_param instanceof Integer){
+            int elements_length = (int) err_param;
+            return elements_length==2;
+        }
+        else if (err_type.equals("user_valid")&&err_param instanceof String){
+            String user_id_regex = (String) err_param;
+            if (!user_id_regex.matches("^[a-z]*$")) return false;
+            return 0<user_id_regex.length()&&user_id_regex.length()<31;
+        }
         return false;
     }
 }
