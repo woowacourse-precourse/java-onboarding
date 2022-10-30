049c55ea94492d3b80bdbc93c69f66ec66ef4428
Minky Baek <55613446+toyo30@users.noreply.github.com>
2022-10-30 22:58:19 +0900

    [add] perform - calculate the change money

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,16 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         List<Integer> answer = new ArrayList<>();
+        int[] moneyArr = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for(int i = 0; i < moneyArr.length; i++) {
+            int quotient = 0;
+            if(money >= moneyArr[i]) {
+                quotient = money / moneyArr[i];
+                money = money % moneyArr[i];
+            }
+            answer.add(quotient);
+        };
+
         return answer;
     }
 }
