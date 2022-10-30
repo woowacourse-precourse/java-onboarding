82b922293d3032f9d1eea892451093eed5c8c975
lseok <lseokhyun99@gmail.com>
2022-10-31 01:26:24 +0900

    문제 2 기능 구현 수정필요 맘에 안듦

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = PWDecoder(cryptogram);
         return answer;
+    }
+    public static String PWDecoder(String cryptogram){
+        StringBuffer decoded = new StringBuffer(cryptogram);
+
+        for(int j = 0 ; j < decoded.length() ;j++){
+            for(int i = 0 ; i < decoded.length()-1 ; i++){
+                if(decoded.charAt(i) == decoded.charAt(i+1)) {
+                    decoded.replace(i, i + 2, "");
+                    i = 0;
+                }
+            }
+        }
+        return decoded.toString();
     }
 }
