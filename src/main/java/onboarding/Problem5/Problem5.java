32bfbf7aa0b8dcc54907f284e60dfcc7f823977d
ma-an-jong <alswhd1113@gmail.com>
2022-10-30 17:02:28 +0900

    problem5 modify UNIT_MONEY,for each

--- src/main/java/onboarding/Problem5/Problem5.java
+++ src/main/java/onboarding/Problem5/Problem5.java
@@ -1,19 +1,16 @@
 package onboarding.Problem5;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
-    static final int[] arr = {50_000,10_000,5_000,1_000,500,100,50,10,1};
+    private static final int[] UNIT_MONEY = {50_000,10_000,5_000,1_000,500,100,50,10,1};
     private static List<Integer> changeMoney(int money)
     {
         List<Integer> changedCount = new LinkedList<>();
-        for(int i = 0 ; i < arr.length;i++)
-        {
-            changedCount.add(money/arr[i]);
-            money %= arr[i];
+        for (int unit : UNIT_MONEY) {
+            changedCount.add(money / unit);
+            money %= unit;
         }
         return changedCount;
     }
