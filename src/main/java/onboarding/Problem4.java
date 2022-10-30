5ac4561ff26d637c63926256e6c32890651784fe
chopinballadeno4 <qo98333@naver.com>
2022-10-30 11:45:26 +0900

    problem 4, function 1 translate alpabet method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+    public static char translateAlpa(char alpa) {
+        int ascnum = (int)alpa;
+        // A ~ Z => 65 ~ 90
+        // a ~ z => 97 ~ 122
+
+        if(ascnum >= 65 && ascnum <= 90) {
+            int gap = 90 - ascnum;
+            char translatedChar = (char)(65 + gap);
+            return translatedChar;
+        }
+
+        if(ascnum >= 97 && ascnum <= 122) {
+            int gap = 122 - ascnum;
+            char translatedChar = (char)(97 + gap);
+            return translatedChar;
+        }
+    }
+
+
     public static String solution(String word) {
         String answer = "";
         return answer;
