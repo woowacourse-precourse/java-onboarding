47913580bb940f06cace466fb2ccb0f94c40c086
khj1 <dhepd8849@gmail.com>
2022-10-30 12:18:13 +0900

    feat(problem7): 사용자의 친구 목록을 추려내는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        List<String> friendsOfUser = findFriendsByUser(user, friends);
+
         return answer;
+    }
+
+    private static List<String> findFriendsByUser(String user, List<List<String>> friends) {
+        Set<String> friendsOfUser = new HashSet<>();
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                for (String name : friend) {
+                    friendsOfUser.add(name);
+                }
+            }
+        }
+        friendsOfUser.remove(user);
+        return new ArrayList<>(friendsOfUser);
     }
 }
