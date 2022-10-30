95a22f05601a8ddf2c1c0722d61e1108f73fbddb
Lee-Jiseung <ljss98@naver.com>
2022-10-30 16:39:22 +0900

    feat: problem1 승자 정하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,13 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int WIN_POBI = 1;
+    private static final int WIN_CRONG = 2;
+    private static final int DRAW = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
 
         int scorePobi = maxScore(List.of(add(pobi.get(0)), multiply(pobi.get(0)), add(pobi.get(1)), multiply(pobi.get(1))));
         int scoreCrong = maxScore(List.of(add(crong.get(0)), multiply(crong.get(0)), add(crong.get(1)), multiply(crong.get(1))));
 
-        return answer;
+        return decideWinner(scorePobi, scoreCrong);
     }
 
     private static int add(int pageNumber) {
@@ -32,5 +36,15 @@ class Problem1 {
 
     private static int maxScore(List<Integer> numbers) {
         return numbers.stream().max(Integer::compare).orElse(-1);
+    }
+
+    private static int decideWinner(int p, int c) {
+        if (p > c) {
+            return WIN_POBI;
+        } else if (p < c) {
+            return WIN_CRONG;
+        } else {
+            return DRAW;
+        }
     }
 }
