e6debaea3eba619d6d8acf3034f12ce5c6c5edf5
limzzum <chfhddl1234@naver.com>
2022-10-30 19:01:15 +0900

    [문제 6] 현재 닉네임이 2글자 이상이면 2글자 단위로 나눔.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,27 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    static String [] matchWord;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        for(int i=0; i<forms.size()-1; i++){
+
+            String nickname = forms.get(i).get(1);
+            if(nickname.length()==1)
+                continue;
+
+            divideWord(nickname);
+        }
+
+        List<String> answer = List.of("");
         return answer;
+    }
+
+
+    private static void divideWord(String nickname) {
+        matchWord = new String[nickname.length()-1];
+        for(int k = 0; k< nickname.length()-1; k++){
+            matchWord[k]= nickname.substring(k,k+2);
+        }
     }
 }
