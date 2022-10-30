8f552821ea993976f6135ad440f0b0c2b410778e
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-30 20:57:43 +0900

    [ADD] 2차 조건문: SUM이 큰 지, PRODUCT가 큰 지 판단 후, 변수 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
+    private static List<Integer> digitParser(Integer page) {
+        List<String> stringDigits = Arrays.asList(page.toString().split(""));
+        List<Integer> integerDigits = new ArrayList<>();
+
+        for (String digit: stringDigits) {
+            integerDigits.add(Integer.valueOf(digit));
+        }
+
+        return integerDigits;
+    }
+
+    private static int getScore(List<List<Integer>> digits) {
+        List<Integer> scores = new ArrayList<>();
+
+        for (List<Integer> digit: digits) {
+            scores.add(digit.stream().reduce(0, Integer::sum));
+            scores.add(digit.stream().reduce(0, (a, b) -> a * b));
+        }
+
+        int maxValue = scores.stream().reduce(0, Integer::max);
+        return maxValue;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         final int WRONG_INPUT = -1;
         final int TIE = 0;
@@ -10,6 +35,8 @@ class Problem1 {
         final int CRONG_WINS = 2;
 
         List[] players = {pobi, crong};
+        List<Integer> scores = new ArrayList<>();
+
         for(List<Integer> player: players) {
             int left = player.get(0);
             int right = player.get(1);
@@ -17,10 +44,13 @@ class Problem1 {
             if (right - left != 1 || right % 2 != 0 || left % 2 != 1 || right > 399 || left < 2) {
                 return WRONG_INPUT;
             }
+
+            List<List<Integer>> digits = new ArrayList<>();
+            digits.add(digitParser(left));
+            digits.add(digitParser(right));
+
+            scores.add(getScore(digits));
         }
-
-
-
 
         int answer = Integer.MAX_VALUE;
         return answer;
