f8b5b1c63ada621dd01cfa67f0dd52735e2621ee
Minky Baek <55613446+toyo30@users.noreply.github.com>
2022-10-30 23:09:30 +0900

    [add] declare - valuables

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,13 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> friendsOfUser = new ArrayList<>();
+        HashMap<String, Integer> recommendFriendsScore = new HashMap<>();
         return answer;
     }
 }
