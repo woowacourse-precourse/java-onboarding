56cbc0fd194a04d89a4dde386bc85185f3800690
kimdeokyoung <dudejrss@naver.com>
2022-10-30 09:42:19 +0900

    [Edit] src/main/java/onboarding/Problem7.java : make structure2 Problem 7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,12 +16,43 @@ public class Problem7 {
         return answer;
     }
 
-    private class SocialGraph{
+    private class SocialGraph {
+        private List<Entry> orderedList;
+        private List<String> users;
+        private EntryComparator cmp;
+        public SocialGraph(List<List<String>> friends) {
+//            Collections.sort(orderedList);
+        }
 
     }
 
+    private class Entry implements Comparable<Entry>  {
+        protected String name = "";
+        protected int recommendationValue = 0;
 
+        protected Entry(String sname, int value){
+            this.name = sname;
+            this.recommendationValue = value;
+        }
 
-
-
+        @Override
+        public int compareTo(Entry o) {
+            int tmp = this.name.compareTo( o.name);
+            if( tmp > 0){
+                return -1;
+            }else if (tmp < 0){
+                return 1;
+            }
+            return 0;
+        }
+        public boolean equals(Entry o){
+            return this.name.equals(o.name);
+        }
+    }
+    private class EntryComparator implements Comparator<Entry> {
+        @Override
+        public int compare(Entry o1, Entry o2) {
+            return (o1.recommendationValue - o2.recommendationValue);
+        }
+    }
 }
