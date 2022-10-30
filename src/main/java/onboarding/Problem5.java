49d4733ed47eb0c3fac2bb35238a52af877e4f81
은지 <midzy0228@gmail.com>
2022-10-30 19:50:57 +0900

    feat[Problem5]: 거스르고 남는 돈 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,3 +1,9 @@
+/*
+ * 클래스명: Problem3
+ *
+ * 시작 날짜: 2022-10-30
+ */
+
 package onboarding;
 
 import java.util.Collections;
@@ -5,7 +11,16 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        // 현재 거스를 돈 단위 파악후 남은 돈 리턴 해주는 함수 구현
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+}
+
+class ChangeMoney {
+    public int changeMoney(int money, int won) {
+        int result = money;
+        result %= won;
+        return result;
     }
 }
