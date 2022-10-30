05839b808400994328fb12fac113f973a8967113
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 17:42:06 +0900

    type:feat 숫자를 각 자릿수로 나누는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static List<Integer> divideNumberByDigit (int number) {
+        List<Integer> result = new ArrayList();
+        for (int i = number; i > 0; i /= 10) {
+            result.add(i % 10);
+        }
+        return result;
     }
 }
