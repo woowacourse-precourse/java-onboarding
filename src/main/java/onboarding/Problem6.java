794baae2bd7a72d3a4b3e0d5cdaddfeb11652dea
BaeDongHyeon <sasd730@gmail.com>
2022-10-31 00:40:21 +0900

    feat(Problem6.java): 닉네임 일부 포함 여부 반환 메소드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -23,5 +23,18 @@ public class Problem6 {
         public String getNickname() {
             return nickname;
         }
+
+        public boolean isNicknameInclude(User user) {
+            if (user.getEmail().equals(email)) {
+                return false;
+            }
+            for (int i = 0; i < nickname.length()-1; i++) {
+                String token = nickname.substring(i, i+2);
+                if (user.getNickname().contains(token)) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
