eea1da859310038785d806b575daa4b71b266801
이지혁 <olzlgur@naver.com>
2022-10-31 03:10:55 +0900

    게임 실행 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,17 +8,19 @@ class Problem1 {
     private final int number = 0;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!validatePageNumber(pobi) || !validatePageNumber(crong)){
+            return -1;
+        }
+        return playGame(pobi, crong);
     }
     private static boolean validatePageNumber(List<Integer> player) {
-        if (player.get(0) - player.get(1) != 1) {
+        if (player.get(1) - player.get(0) != 1) {
             return false;
         }
-        if (player.get(0) < 0 || player.get(1) > 400) {
+        if (player.get(0) < 1 || player.get(1) > 400) {
             return false;
         }
-        if (player.get(0) % 2 != 1 || player.get(1) != 0) {
+        if (player.get(0) % 2 != 1 || player.get(1) % 2 != 0) {
             return false;
         }
         return true;
@@ -33,5 +35,17 @@ class Problem1 {
             page /= 10;
         }
         return max(sum, mul);
+    }
+    private static int playGame(List<Integer> pobi, List<Integer> crong) {
+        int pobiScore = max(calPageScore(pobi.get(0)), calPageScore(pobi.get(1)));
+        int crongScore = max(calPageScore(crong.get(0)), calPageScore(crong.get(1)));
+
+        if (pobiScore > crongScore) {
+            return 1;
+        }
+        if (pobiScore < crongScore) {
+            return 2;
+        }
+        return 0;
     }
 }
