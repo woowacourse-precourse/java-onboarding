8d06fc966694d3ed6f1896343b30a2102207282b
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:38:40 +0900

    feat(Problem3): 점화식 초기화 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,9 +2,19 @@ package onboarding;
 
 public class Problem3 {
 
+    private static int[] dp;
+
     private static boolean checkParam(int n)
     {
         return n == 0 || 10000 < n;
+    }
+
+    private static void initDp()
+    {
+        dp = new int[5];
+        dp[0] = 0;
+        for (int i=0; i<4; i++)
+            dp[i + 1] = 3 * (int) Math.pow(10, i) + dp[i] * 10;
     }
 
     public static int solution(int number) {
