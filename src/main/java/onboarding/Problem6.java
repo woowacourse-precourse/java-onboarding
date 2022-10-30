3257e44e1b6a3a5360a7ef53b505c95064c9f518
jagajang <kosb7544@gmail.com>
2022-10-30 13:15:41 +0900

    p6/seperate name checking from countPiece function

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,7 @@ import java.util.*;
 /* 기능 목록
  *
  * countPiece : form 전체에서 연속한 글자의 등장 횟수를 셈
+ * countPieceInName : 닉네임에서 연속한 글자의 등장 횟수를 셈
  * getInvalidEmail : 닉네임에서 연속한 글자가 같은 교육생의 이메일 리스트를 반환
  * hasSamePiece : 닉네임의 연속한 글자가 여러 번 등장하는지 확인
  */
@@ -24,14 +25,18 @@ public class Problem6 {
         for(List<String> form : forms) {
             String name = form.get(1);
 
-            for(int i = 1; i < name.length(); i++) {
-                String piece = name.substring(i-1, i+1);
-
-                pieceNum.put(piece, pieceNum.getOrDefault(piece, 0) + 1);
-            }
+            countPieceInName(name, pieceNum);
         }
 
         return pieceNum;
+    }
+
+    private static void countPieceInName(String name, Map<String, Integer> pieceNum) {
+        for(int i = 1; i < name.length(); i++) {
+            String piece = name.substring(i-1, i+1);
+
+            pieceNum.put(piece, pieceNum.getOrDefault(piece, 0) + 1);
+        }
     }
 
     private static List<String> getInvalidUserEmail(List<List<String>> forms, Map<String, Integer> pieceNum) {
