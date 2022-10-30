b829121c2d5dd2b6415491f77d8d190aab595387
syahn <496300@naver.com>
2022-10-30 18:13:09 +0900

    feat: number가 한 자리 수일 경우 clap 수 계산 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    //number가 한 자리 수일 경우
+    public static int countOneFigures(int number){
+        int count = 0;
+
+        for(int i =0; i<=number; i++){
+            if(i%3 == 0 & i!=0)
+                count++;
+        }
+        return count;
+
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
