8b600bc6c59c44516b2f183d9fb1b865f0ffd647
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 16:44:34 +0900

    feat(Problem3): 찾아야할 숫자가 존재하는지 비교할 Set 초기화 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        Set<String> set = new HashSet<String>();
+        init(set);
+
         return answer;
+    }
+
+    /**
+     * Function for creating Set for checking 3,6,9
+     * */
+    private static void init(Set<String> set) {
+        set.add("3");
+        set.add("6");
+        set.add("9");
     }
 }
