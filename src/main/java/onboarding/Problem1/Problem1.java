5dabc389f8de3b98d9c1a1de5b5c0a231c408c7b
ma-an-jong <alswhd1113@gmail.com>
2022-10-30 16:38:34 +0900

    problem1 package make

---
+++ src/main/java/onboarding/Problem1/Problem1.java
@@ -0,0 +1,22 @@
+package onboarding.Problem1;
+
+import java.util.List;
+
+public class Problem1 {
+
+    private static final int EXCEPTION_VALUE = -1;
+
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        try {
+            Pages pobiPage = new Pages(pobi);
+            Pages crongPage = new Pages(crong);
+
+            return pobiPage.compareTo(crongPage);
+        } catch (IllegalArgumentException e) {
+            return EXCEPTION_VALUE;
+        }
+
+
+    }
+
+}
