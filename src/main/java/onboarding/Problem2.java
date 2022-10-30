1acb161ec503a627ab0962ab1bcfc510e1681049
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-30 19:28:19 +0900

    Problem2 : 문자가 2개씩 중복되는 경우 제거하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.Stack;
+
+/*
+기능목록
+1. 2개씩 중복되는 경우 제거하는 기능
+2. 중복이 3개이상일 경우 제거하는 기능
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> stack=new Stack<>();
+
+        for(int i=0; i<cryptogram.length(); i++){
+            char c=cryptogram.charAt(i);
+            if(stack.isEmpty()){
+                stack.push(c);
+            }else{
+                if(stack.peek()==c){
+                    stack.pop();
+                }else{
+                    stack.push(c);
+                }
+            }
+        }
+        for (Character c : stack){
+            answer+=c;
+        }
         return answer;
     }
 }
