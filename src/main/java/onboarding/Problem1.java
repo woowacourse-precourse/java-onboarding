2da04153310c0bed1ae1c250a02de5aa4758b59b
ggazitbyul <ngusy123@gmail.com>
2022-10-31 01:48:17 +0900

    Problem1) 1. 추가)수정) 규칙 해석 2. 추가) isException 예외 처리 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,7 +7,7 @@ import java.util.List;
 //        	1. 책을 임의로 펼친다.
 //                1-1) 매개변수를 받는다.
 //                1-2) ApplicationTest case 1,2,3 에 상수 기입되어있음
-//          >>구현 불필요, 걍 무시하자.
+//          >>입력에대한 구현 불필요, 걍 무시하자.
 //      ------------------------------------------------------------
 //        	2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
 //                2-1) 12페이지면 1, 2를 더하거나 곱해서 비교하여 큰수를 반환한다?
@@ -27,42 +27,48 @@ import java.util.List;
 //                5-2) 더 큰수 가진자 승리
 //                5-3) 포비 승리 1, 크롱 승리 2, 무승부 0, 예외사항 -1
 //        	6. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
-//                6-1) 예외사항에 포함되는 내용인듯?
+//                6-1) 1또는 400의 수는 예외사항으로 처리.
 //      포비와 크롱이 펼친 페이지가 들어있는 리스트/배열 pobi와 crong이 주어질 때, 포비가 이긴다면 1, 크롱이 이긴다면 2,
 //      무승부는 0, 예외사항은 -1로 return 하도록 solution 메서드를 완성하라.
 //
-//      제한사항
+//      제한사항 (예외사항)
 //	        • pobi와 crong의 길이는 2이다.
-//              - 길이가 뭘 의미하는걸까?
+//              - List 길이가 2
 //	        • pobi와 crong에는 [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있다.
 //              - 12 이면 13, 연번의 개념인듯.
+//          • 1~400 페이지
+//          • 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수
+//           6-1) 1또는 400의 수는 예외사항으로 처리.
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int pobiAddLeftPage = getAddNumber(pobi.get(0));
-        int pobiAddRightPage = getAddNumber(pobi.get(1));
-        int pobiMulLeftPage = getMulNumber(pobi.get(0));
-        int pobiMulRightPage = getMulNumber(pobi.get(1));
-        int crongAddLeftPage = getAddNumber(crong.get(0));
-        int crongAddRightPage = getAddNumber(crong.get(1));
-        int crongMulLeftPage = getMulNumber(crong.get(0));
-        int crongMulRightPage = getMulNumber(crong.get(1));
-        int pobiLeftResult = getHighestValue(pobiAddLeftPage, pobiMulLeftPage);
-        int pobiRightResult = getHighestValue(pobiAddRightPage, pobiMulRightPage);
-        int crongLeftResult = getHighestValue(crongAddLeftPage, crongMulLeftPage);
-        int crongRightResult = getHighestValue(crongAddRightPage, crongMulRightPage);
-        int pobiScore = getHighestValue(pobiLeftResult, pobiRightResult);
-        int crongScore = getHighestValue(crongLeftResult, crongRightResult);
-        if (pobiScore > crongScore) {
-            answer = 1;
+        if (isException(pobi) || isException(crong)) {  // 예외상황시 비교 비실행 -1 반환
+            answer = -1;
         }
-        if (pobiScore < crongScore){
-            answer = 2;
+        else {          // 예외 체크후 비교연산 시작
+            int pobiAddLeftPage = getAddNumber(pobi.get(0));
+            int pobiAddRightPage = getAddNumber(pobi.get(1));
+            int pobiMulLeftPage = getMulNumber(pobi.get(0));
+            int pobiMulRightPage = getMulNumber(pobi.get(1));
+            int crongAddLeftPage = getAddNumber(crong.get(0));
+            int crongAddRightPage = getAddNumber(crong.get(1));
+            int crongMulLeftPage = getMulNumber(crong.get(0));
+            int crongMulRightPage = getMulNumber(crong.get(1));
+            int pobiLeftResult = getHighestValue(pobiAddLeftPage, pobiMulLeftPage);
+            int pobiRightResult = getHighestValue(pobiAddRightPage, pobiMulRightPage);
+            int crongLeftResult = getHighestValue(crongAddLeftPage, crongMulLeftPage);
+            int crongRightResult = getHighestValue(crongAddRightPage, crongMulRightPage);
+            int pobiScore = getHighestValue(pobiLeftResult, pobiRightResult);
+            int crongScore = getHighestValue(crongLeftResult, crongRightResult);
+            if (pobiScore == crongScore) {
+                answer = 0;
+            } else if (pobiScore > crongScore) {
+                answer = 1;
+            } else if (pobiScore < crongScore) {
+                answer = 2;
+            }
         }
-        if (pobiScore == crongScore){
-            answer = 0;
-        }
-
         return answer;
     }
 
@@ -97,11 +103,27 @@ class Problem1 {
 
         if (comparisonValue1 >= comparisonValue2) {
             calResult = comparisonValue1;
-        }
-        if (comparisonValue1 < comparisonValue2) {
+        } else if (comparisonValue1 < comparisonValue2) {
             calResult = comparisonValue2;
         }
         return calResult;
     }
 
+    private static boolean isException(List<Integer> page) {
+        boolean isException = false;
+        if (page.get(0) <= 1 || page.get(0) >= 400 || page.get(1) <= 1 || page.get(1) >= 400) { // 페이지가 2~399 페이지 사이가 아니라면
+            isException = true; //예외처리
+        }
+
+        // left가 홀수인가, right가 짝수인가
+        else if (page.get(0) % 2 != 1 || page.get(1) % 2 != 0) {
+            isException = true; //예외처리
+        }
+        // left와 right가 연번인가
+        else if (page.get(1) - page.get(0) != 1) {
+            isException = true; //예외처리
+        }
+        return isException;
+    }
 }
+
