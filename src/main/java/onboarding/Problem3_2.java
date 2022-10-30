de96802812f4dca563017bcb5119c23fbdb27ed2
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-10-30 15:44:16 +0900

    problem3 solution2 풀이 추가

--- src/main/java/onboarding/Problem3_2.java
+++ src/main/java/onboarding/Problem3_2.java
@@ -8,11 +8,9 @@ public class Problem3_2 {
             int tmpDigits = (int) Math.pow(10, i);
             answer += (number / tmpDigits) * 3 * (tmpDigits / 10); // 3921 -> 3 * 3 * 100
             answer += (number % tmpDigits) / (3 * (tmpDigits / 10)) * (tmpDigits / 10); // 3921 -> 3821 % 1000 -> 821 -> 2*100
-//            System.out.println((number / tmpDigits) * 3 * (tmpDigits / 10) + " 이거랑 " + ((number - tmpDigits / 10) % tmpDigits) / (3 * (tmpDigits / 10)) * (tmpDigits / 10) + " 이거 더함");
             char secondDigitsNumber = ((number % tmpDigits) + "").charAt(0);
             
             if (secondDigitsNumber == '3' || secondDigitsNumber == '6' || secondDigitsNumber == '9') {
-//                System.out.println(tmpDigits / 10 - (number % (tmpDigits / 10) + 1) + ": 뺌");
                 answer -= tmpDigits / 10 - (number % (tmpDigits / 10) + 1);
             }
         }
@@ -20,6 +18,11 @@ public class Problem3_2 {
         return answer;
     }
 
+    /*
+    * 1. 1~number 범위의 수 i에 대해서 반복한다.
+    * 2. i의 각 자릿수에 포함 된 3,6,9의 개수만큼 answer에 더한다.
+    * 3. answer을 반환한다.
+    * */
     public static int solution2(int number) {
         int answer =0;
         for (int i = 1; i < number + 1; i++) {
