ff7f88743d62f869ca8320b62e908b6ed5749fec
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-10-30 23:44:44 +0900

    feat: 두 개의 문자가 일치하는지 검증하는 isDuplicate 테스트 코드 작성, 두 개씩 나누어 리스트에 담는 코드 작성. 리스트 인스턴 변수 추가. 일치하는 게 하나라도 있으면 참, 없다면 거짓을 반환하는 코드 작성. 테스트 통과.

--- src/main/java/onboarding/problem6/NickName.java
+++ src/main/java/onboarding/problem6/NickName.java
@@ -1,26 +1,51 @@
 package onboarding.problem6;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class NickName {
     private static final Pattern korean = Pattern.compile("^[ㄱ-ㅎ|가-힣]{1,20}$");
     private final String nickName;
+    private final List<String> separateNicks = new ArrayList<>();
 
     public NickName(String nickName) {
         checkKorean(nickName);
         this.nickName = nickName;
+        separateTwoParts();
     }
 
+
+
     private static void checkKorean(String nickName) {
-        if (!korean.matcher(nickName).matches()){
+        if (!korean.matcher(nickName).matches()) {
             throw new NickNameException("한글만 쓸 수 있습니다.");
         }
     }
 
-    public String getNickName() {
-        return nickName;
+    public boolean isDuplicate(NickName compareNick) {
+        return countDuplicate(compareNick) > 0;
     }
+
+    private long countDuplicate(NickName compareNick) {
+        List<String> toNick = compareNick.getSeparateNicks();
+
+        return separateNicks.stream()
+                .filter(toNick::contains)
+                .count();
+    }
+
+    private void separateTwoParts() {
+        int i = 0;
+        int duplicateLimit = 2;
+        while (i < nickName.length()) {
+            separateNicks.add(nickName.substring(i, i + duplicateLimit));
+            i++;
+            if (i + duplicateLimit > nickName.length()) {
+                break;
+            }
+        }
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -33,5 +58,11 @@ public class NickName {
     @Override
     public int hashCode() {
         return Objects.hash(nickName);
+    }
+    public String getNickName() {
+        return nickName;
+    }
+    public List<String> getSeparateNicks() {
+        return Collections.unmodifiableList(separateNicks);
     }
 }
--- src/test/java/onboarding/problem6/NickNameTest.java
+++ src/test/java/onboarding/problem6/NickNameTest.java
@@ -2,6 +2,7 @@ package onboarding.problem6;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,10 +21,26 @@ public class NickNameTest {
     void if_not_korean_nick(String nickName) {
         assertThrows(NickNameException.class, () -> new NickName(nickName));
     }
+
     @ParameterizedTest
     @ValueSource(strings = {"", "한글이지만길이가너무길면굉장히곤란해요제발"})
     void if_not_MAX_and_MIN(String nickName) {
         assertThrows(NickNameException.class, () -> new NickName(nickName));
     }
 
+    @ParameterizedTest
+    @CsvSource({"엠제이,제이엠바고사리"})
+    void duplicate(String name, String compareNick) {
+        NickName nickName = new NickName(name);
+        NickName compareNick1 = new NickName(compareNick);
+        assertThat(nickName.isDuplicate(compareNick1)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"엠제이,이제엠"})
+    void not_duplicate(String name, String compareNick) {
+        NickName nickName = new NickName(name);
+        NickName compareNick1 = new NickName(compareNick);
+        assertThat(nickName.isDuplicate(compareNick1)).isFalse();
+    }
 }
