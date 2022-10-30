54e7869dd733a6834f465900f1f6e27cfd407eeb
Albert <guang7226961@naver.com>
2022-10-30 10:15:25 +0900

    feat(Problem5) 기능 종합 구현 완료

--- src/main/java/onboarding/problem5/MoneyRangeValidator.java
+++ src/main/java/onboarding/problem5/MoneyRangeValidator.java
@@ -1,7 +1,11 @@
 package onboarding.problem5;
 
 public class MoneyRangeValidator {
+
+	public static final int MIN_SIZE = 0;
+	public static final int MAX_SIZE = 1_000_000;
+
 	public boolean validate(int n) {
-		return n > 0 && n <= 1_000_000;
+		return n > MIN_SIZE && n <= MAX_SIZE;
 	}
 }
