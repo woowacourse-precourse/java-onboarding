ae77b1da879241fb55390d47eae437193b435fb3
HamSeungChan <head0618@naver.com>
2022-10-30 21:13:21 +0900

    feat(Problem3): 자릿수 별로 숫자 나누는 기능 & 3,6,9를 찾는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
+
+    private static final int NUMBER_OF_DIGIT_DIVISION = 10;
+    private static final int MAX_INPUT_NUMBER = 10000 ;
+
     public static int solution(int number) {
-        int answer = 0;
+        int answer = number;
         return answer;
     }
+
+
+    private static int divideNumber(int number){
+        int clap = 0;
+        int dividedNumber = Integer.MIN_VALUE;
+        while(number>0){
+            dividedNumber = number % NUMBER_OF_DIGIT_DIVISION;
+            if(confirmNumber(dividedNumber))
+                clap++;
+            number = number/NUMBER_OF_DIGIT_DIVISION;
+        }
+        return clap;
+    }
+
+    private static boolean confirmNumber(int dividedNumber){
+        if(dividedNumber%3==0) return true;
+        return false;
+    }
+
 }
