76209793a5eae5a55cdba77677531421ebf8816d
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-10-31 00:02:25 +0900

    P4: change character

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,24 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem4 {
+    public static String changeChar(String word, String ans, int i) {
+        char c = word.charAt(i);
+
+        if (Character.isAlphabetic(c) && Character.isUpperCase(c)) {
+            int pos = c - 'A';
+            char temp = (char)('Z' - pos);
+            ans = ans + temp;
+        } else if (Character.isAlphabetic(c) && Character.isLowerCase(c)) {
+            int pos = c - 'a';
+            char temp = (char)('z' - pos);
+            ans = ans + temp;
+        } else {
+            ans = ans + c;
+        }
+        return ans;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
