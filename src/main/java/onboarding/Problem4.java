ce8898bc627b68f9752bbba5567b1c1dba89d26e
chaerim <qkrdbsk28@naver.com>
2022-10-31 00:34:24 +0900

    feat(problem4): 청개구리 문자 변환 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    // 청개구리 사전에 맞게 문자를 반대로 변환하는 함수
+    public static String changeWord(char before) {
+        String[] changeWord = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B",	"A"};
+
+        if(97 <= before && before <= 122) { // 소문자일 경우
+            before -= 32;
+            String beforeWord = Character.toString(before);
+            int index = Arrays.asList(changeWord).indexOf(beforeWord);
+            return changeWord[changeWord.length - index -1].toLowerCase();
+        }
+
+        String beforeWord = Character.toString(before);
+        int index = Arrays.asList(changeWord).indexOf(beforeWord);
+
+        if (index == -1) { // 알파벳이 아닌 경우
+            return beforeWord;
+        }
+
+        return changeWord[changeWord.length - index -1];
     }
 }
