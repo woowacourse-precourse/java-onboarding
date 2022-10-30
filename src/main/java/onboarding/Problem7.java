5cd8480acb94534206d7ce7412fa2ca50104d88d
Cho SangHyun <98shcho@naver.com>
2022-10-30 21:16:52 +0900

    feat: add feature to sort scoreList

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -49,6 +49,15 @@ public class Problem7 {
         }
 
         List<Map.Entry<String, Integer>> scoreList = new ArrayList<>(scoreMap.entrySet());
+        /* 점수 정렬하기 */
+        scoreList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o2.getValue() > o1.getValue())
+                    return 1;
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
         return answer;
     }
 }
@@ -79,9 +88,9 @@ public class Problem7 {
   - [V] 현재 참조하는 방문자가 uesr의 친구인 경우는 건너뛴다
   - [V] scoreMap에 현재 참조하는 방문자가 없으면 0점으로 하여 넣어준다
   - [V] 현재 참조하는 방문자의 점수를 1점 더해서 교체해준다
-- [] 받은 점수를 토대로 정렬한다
+- [V] 받은 점수를 토대로 정렬한다
   - [V] scoreMap을 이용해 scoreList를 만든다
-  - [] scoreList를 점수 내림차순으로 정렬하고, 점수가 같으면 이름순으로 정렬한다
+  - [V] scoreList를 점수 내림차순으로 정렬하고, 점수가 같으면 이름순으로 정렬한다
 - [] 상위 5명만, 0점이 아닌 경우만 뽑아서 answer에 담는다
   - [] scoreList를 순회하다가 5번째 순회 이후는 break로 탈출하도록 한다
   - [] 각 순회에 대해 0점이 아니면 answer에 넣는다
