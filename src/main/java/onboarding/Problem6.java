38692c6f7179f02935e26d9fb6dfbadd354a76e8
pushedrumex <pushedrumex@naver.com>
2022-10-30 18:01:15 +0900

    problem6: 두글자 이상 중복된다면 해당 이메일 리스트를 반환하는 기능 추가 (recordNick)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,21 +13,27 @@ public class Problem6 {
     }
 
 // 닉네임을 두글자씩 나눠 HashMap에 기록하는 함수 구현("oo", [이메일, 이메일, ... ])
-    static void recordNick(String nick, String email) {
+    static List<String> recordNick(String nick, String email) {
+        List<String> result = new ArrayList();
         String[] strArr = nick.split("");
         String twoCharOfNick;
         for (int i=0;i<strArr.length-1;i++) {
             twoCharOfNick = String.join("", Arrays.copyOfRange(strArr, i, i+2));
             if (map.containsKey(twoCharOfNick)) {
+                if (map.get(twoCharOfNick).size() == 1) {
+                    result.add(map.get(twoCharOfNick).get(0));
+                }
                 map.get(twoCharOfNick).add(email);
+                result.add(email);
             } else {
                 map.put(twoCharOfNick, new ArrayList<>(Arrays.asList(email)));
             }
         }
+        return result;
     }
     public static void main(String args[]) {
-        recordNick("제이슨", "jm@email.com");
-        recordNick("엠제이", "mj@email.com");
+        System.out.println(recordNick("제이슨", "jm@email.com"));
+        System.out.println(recordNick("엠제이", "mj@email.com"));
         System.out.println(map);
     }
 }
