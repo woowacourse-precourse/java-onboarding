02d53e1ae9d831f2ed35c8c9cd474a99f09a4702
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-30 16:06:24 +0900

    [feat] 청개구리 사전 리스트 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String dictCap = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        String dictSmall = "zyxwvutsrqponmlkjihgfedcba";
+
+
         return answer;
+    }
+
+    public static void main(String[] args) {
     }
 }
