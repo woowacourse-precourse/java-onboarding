9013bea26b8126a04cb81de18c0b7d867e06e9c7
yun8565 <yunyun8565@naver.com>
2022-10-30 17:10:50 +0900

    feat(P1): 예외사항 확인 기능 추가
    - 페이지 번호가 범위를 벗어날 때
    - 홀수, 짝수가 아닐 때
    - 연속된 페이지가 아닐 때
    
    가독성을 고려해서 반환값과 페이지의 최소, 최대값을 상수로 선언했다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,30 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static final int EXCEPTION = -1;
+    public static final int DRAW = 0;
+    public static final int POBI_WIN = 1;
+    public static final int CRONG_WIN = 2;
+    public static final int MIN_PAGE = 1;
+    public static final int MAX_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(isException(pobi) || isException(crong))
+            return EXCEPTION;
     }
+
+    public static boolean isException(List<Integer> pages) {
+        int left = pages.get(0);
+        int right = pages.get(1);
+
+        if(left < MIN_PAGE || left > MAX_PAGE || right < MIN_PAGE || right > MAX_PAGE)
+            return true;
+        if(left % 2 == 0 || right % 2 != 0)
+            return true;
+        if(left >= right || right - left > 1)
+            return true;
+
+        return false;
+    }
+
 }
