cdb381ae7422e35e0e091fde0e26203be1a481ba
saehim <saeilee.vxv@gmail.com>
2022-10-30 18:00:59 +0900

    Test(Problem5): 금액에 맞는 지폐 수 반환

--- src/test/java/onboarding/Problem5Test.java
+++ src/test/java/onboarding/Problem5Test.java
@@ -2,6 +2,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class Problem5Test {
   
 
+    @Test
+    void 지폐_바꾸기() {
+        assertThat(change(50237,50000)).isEqualTo(1);
+    }
+
+    private int change(int input, int change) {
+        int answer = 0;
+        if(input >= change){
+            answer = input / change;
+        }
+        return answer;
+    }
+
     int leftMoney(int input, int change){
         int answer = input % change;
         return answer;
