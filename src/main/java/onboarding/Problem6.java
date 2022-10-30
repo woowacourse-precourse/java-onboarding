ba4845c51ed82003eef5b21d2799d467ab391406
charlesuu <tiger0192004@gmail.com>
2022-10-30 14:17:59 +0900

    <문제6> - (기능 1).  같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자들의 이메일 목록을 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        HashSet<String> emailSet = new HashSet<>();
+        HashMap<String, String> tokens = new HashMap<>();
+
+
+        for (List<String> crewInfo : forms) {
+            String secondElement = crewInfo.get(1);
+            char[] nickname = secondElement.toCharArray();
+            for (int i = 0; i < nickname.length - 1; i++) {
+                String token = "";
+                token += nickname[i];
+                token += nickname[i + 1];
+
+                if (tokens.containsKey(token)) {
+                    emailSet.add(tokens.get(token));
+                    emailSet.add(crewInfo.get(0));
+                } else {
+                    tokens.put(token, crewInfo.get(0));
+                }
+            }
+        }
+
+
+        return new ArrayList<>();
     }
 }
