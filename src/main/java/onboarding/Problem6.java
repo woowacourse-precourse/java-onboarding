bf9c3558ebb7f337b1976c82622ac7fb90463f84
KimSangHyeok <tkdgur0906@naver.com>
2022-10-31 00:24:55 +0900

    Feat(PROBLEM6): 크루의 이름을 2글자식 slice하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<Map<String,Integer>> crew = new ArrayList<>();
+        
+        int index = 0;
+        for (List<String> info : forms) {
+            sliceName(crew, index, info);
+            index++;
+        }
         return answer;
+    }
+
+    private static void sliceName(List<Map<String, Integer>> crew, int index, List<String> info) {
+        for(int i = 0; i< info.get(1).length() - 1; i++){
+            Map<String,Integer> map = new HashMap<>();
+            map.put(info.get(1).substring(i, i + 2), index);
+            crew.add(map);
+        }
     }
 }
