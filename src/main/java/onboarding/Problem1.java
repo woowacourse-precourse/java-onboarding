8a58dfa9a425735c192ecf9ffb96e079bf44cb12
euna <christinejung10@daum.net>
2022-10-30 12:03:24 +0900

    feat(Problem1): 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 페이지 번호의 각 자리 숫자를 모두 더하거나 곱한 후, 둘 중 가장 큰 수들을 List로 저장
+
+        // 가장 큰 점수 계산
+
+        // 가장 큰 점수끼리 비교 후 answer 값을 지정
+
         return answer;
     }
+
 }
