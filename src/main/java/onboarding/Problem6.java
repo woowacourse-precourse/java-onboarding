ad7a8fc7bf89c081c3fe5a3b472b4ed9af10340e
Cho SangHyun <98shcho@naver.com>
2022-10-31 00:52:48 +0900

    feat: add emailSet

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,13 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        HashSet<String> emailSet = new HashSet<>();
         return answer;
     }
 }
