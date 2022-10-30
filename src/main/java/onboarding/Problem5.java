c8a54e19888886acb871eee52ff94830ce2870fc
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 21:45:26 +0900

    feat:[Problem5] 큰 수부터 동전 갯수 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +9,20 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    /*
+    그리디 알고리즘 (큰 수 부터 욕심내여 담음) -가능한 이유: 약수들의 집합이기 떄문
+     */
+    private static List<Integer> minCount(int money){
+        List<Integer> moneyIndex= new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
+        List<Integer> moneyCount=new ArrayList<>();
+        for(int i=0;i<8;i++){
+            int cnt=money/moneyIndex.get(i).intValue();
+            moneyCount.add(cnt);
+            money%=moneyIndex.get(i).intValue();
+        }
+        moneyCount.add(money);
+        return moneyCount;
     }
 }
