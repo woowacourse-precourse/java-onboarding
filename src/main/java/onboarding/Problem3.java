a12213ac186d049ddfeb61217278a21def733184
yun <yshyeon102@gmail.com>
2022-10-31 04:30:41 +0900

    feat: Problem3 - memoization을 위한 배열 및 상태 변수 정의
    누적 박수 횟수를 저장하는 배열과 어디까지 저장되었는지 상태를 저장하는 변수를 두었다. 
    문제3 같은 게임은 지속적으로 진행될 경우, 1번 부터 주어진 수까지 반복해서 369 개수를 구해야 한다. 하지만 같은 수에 대해서 369개수를 
    . 따라서  memoization을 활용하여 이전까지 진행된 수까지 배열에 저장하여, 다시 계산할 
    
    #TODO
    주어진 수까지 memoization 배열에 누적 박수 값을 저장하고, 어디까지 채워졌는지 나타내는 currentNumberOfFilledClapsMemo값이 업데이트 되도록 구현해야함.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem3 {
+    public static int[] clapsMemoization = new int[10001];
+    public static int currentNumberOfFilledClapsMemo = 0;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
