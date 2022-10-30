45d245251c59652f70b47843a17f918e7853b225
JungHun Jang <ijj5258@naver.com>
2022-10-30 22:37:46 +0900

    Problem1 (feat) 계산 메소드 정의 후 풀이 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,6 +11,28 @@ class Problem1 {
 		return false;
 	}
 
+	static int calc(List<Integer> list) {
+
+		for(int page : list) { //비교 반복문
+			int plus = 0;
+			int sum = 1;
+
+			while(page>0) {
+				int pageOne = page % 10;
+
+				plus += pageOne;
+				sum *= pageOne;
+
+				page = page/10;
+			}
+			if(plus >= sum) {
+				return plus;
+			} 
+			return sum;
+		}
+		return 0;
+	}
+
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int answer = Integer.MAX_VALUE;
 
@@ -18,7 +40,19 @@ class Problem1 {
 			answer = -1;
 			return answer;
 		}
+		int pobiResult = calc(pobi);
+		int crongResult = calc(crong);
 
+		if(pobiResult > crongResult) { //pobi가 이김
+			answer = 1;
+			return answer;
+		}
+		if(crongResult > pobiResult) { //crong이 이김
+			answer = 2;
+			return answer;
+		} 
+
+		answer = 0;
 		return answer;
 	}
 }
