0f1f41a585d0516e8ee5eef2aa31ca3142ab0b72
wonseokjee <wsji9404@gmail.com>
2022-10-30 14:15:45 +0900

    221030 problem4-1 알파벳 키벨류로 저장

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,44 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = findalp(word);
         return answer;
     }
-//    1.hashmap으로 key,value 저장하는 메서드
-//    2. 알파벳이 아니면 그대로 가고 알파벳이면 1번 메서드로 대소문자 판별하는 메서드
-//    3. 소문자이면 아스키코드로 소문자로 변환하는 메서드
+
+    //    1.hashmap으로 key,value 저장하는 메서드(아스키코드 활용)
+    public static Character dict(Character alp) {
+        HashMap<Character, Character> save = new HashMap<>();
+        save.put('A', 'Z');
+        save.put('B', 'Y');
+        save.put('C', 'X');
+        save.put('D', 'W');
+        save.put('E', 'V');
+        save.put('F', 'U');
+        save.put('G', 'T');
+        save.put('H', 'S');
+        save.put('I', 'R');
+        save.put('J', 'Q');
+        save.put('K', 'P');
+        save.put('L', 'O');
+        save.put('M', 'N');
+        save.put('N', 'M');
+        save.put('O', 'L');
+        save.put('P', 'K');
+        save.put('Q', 'J');
+        save.put('R', 'I');
+        save.put('S', 'H');
+        save.put('T', 'G');
+        save.put('U', 'F');
+        save.put('V', 'E');
+        save.put('W', 'D');
+        save.put('X', 'C');
+        save.put('Y', 'B');
+        save.put('Z', 'A');
+
+        return save.get(alp);
+    }
+
 }
