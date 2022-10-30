81acbfdaa0518032e8dec9098bf556e6b32d8caf
syahn <496300@naver.com>
2022-10-30 18:15:17 +0900

    feat : number가 세 자리 수일 경우 clap 수 계산 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,30 @@
 package onboarding;
 
 public class Problem3 {
+    //number가 세 자리 수일 경우
+    public static int countThreeFigures(int number){
+        int count = 0;
+        int noCount = 0;
+
+        if(number/100%3 == 0){
+            noCount = number/100;
+        }
+        for(int i =0; i<=number/100; i++) {
+            if (i % 3 == 0 & i != noCount & i!=0) {
+                count += 100;
+                count += countTwoFigures(99);
+            } else if (i == noCount & i!=0) {
+                count += number % 100 + 1;
+            }else if(i== number/100){
+                count += countTwoFigures(number%100);
+            }else {
+                count += countTwoFigures(99);
+            }
+        }
+
+        return count;
+    }
+
     //number가 두 자리 수일 경우
     public static int countTwoFigures(int number){
         int count = 0;
