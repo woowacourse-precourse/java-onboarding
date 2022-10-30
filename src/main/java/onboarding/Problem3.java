6449c3f9b8a7da3a917ff687fea518cb36f1f695
ulsandonghun <donghoon141@naver.com>
2022-10-30 22:03:13 +0900

    problem 3 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+
         int answer = 0;
+        int tmp=0;
+        int n=0;
+        for(int i=0;i<=number;i++){
+            n=i;
+
+            while(n!=0){
+                tmp=n%10;
+                if(tmp==3||tmp==6||tmp==9){
+                    answer++;
+                }
+                n/=10;
+
+            }
+        }
         return answer;
     }
 }
