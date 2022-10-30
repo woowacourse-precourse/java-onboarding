d264e627c927cbdd50da5c5c3a8fcdc16239ab31
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 20:59:48 +0900

    feat: Problem3 - getClapCount 메서드 구현
    1부터n 까지 3,6,9가 들어있다면, 카운트를 증가시키고 그 값을 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,4 +10,13 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    private static int getClapCount(int number){
+        int clapCount = 0;
+        for(int i = 1; i <= number; i++) {
+            clapCount += String.valueOf(i)
+                    .chars()
+                    .filter(num -> num == '3' || num == '6' || num == '9').count();
+        }
+        return clapCount;
+    }
 }
