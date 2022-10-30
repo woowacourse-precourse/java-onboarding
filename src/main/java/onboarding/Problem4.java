5e14686a6ae857ff57c44604ca7b36667db8e29e
Jeangyu <gjwlsrb1700@naver.com>
2022-10-30 16:54:12 +0900

    Add: Problem4 solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i=0; i<word.length(); i++){
+            char cur = word.charAt(i);
+
+            if (cur>='a' && cur <= 'z'){
+                cur = (char) ('z' - (cur - 'a'));
+            }
+            else if (cur>='A' && cur <= 'Z'){
+                cur = (char) ('Z' - (cur - 'A'));
+            }
+            answer+=cur;
+        }
+
         return answer;
     }
 }
