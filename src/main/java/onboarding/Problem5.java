01afba3dd4ceb5942279fe62cedc9bda19f8b491
JongDo737 <jongmin373@gmail.com>
2022-10-30 10:53:16 +0900

    problem5 돈계산 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        List<Integer> answer;
+
+        answer = moneyCalculator(money);
+        return answer;
+    }
+    public static List<Integer> moneyCalculator(int money) {
         List<Integer> answer = new ArrayList<>();
         // [5만,1만,5천,1천,5백,1백,50,10,1]
         int[] moneyList = {50000,10000,5000,1000,500,100,50,10,1};
