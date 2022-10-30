5c91ca47f43d30c29fd469dc6e7480ff839731ca
rt3310 <btac3310@gmail.com>
2022-10-30 15:49:14 +0900

    feat(Problem2): 연속 중복된 문자 제거 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,25 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    public static String removeDuplicatedString(String cryptogram) {
+        cryptogram += " ";
+        boolean prevDuplicated = false;
+        boolean curDuplicated;
+        String result = "";
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) != cryptogram.charAt(i - 1)) {
+                curDuplicated = false;
+            } else {
+                curDuplicated = true;
+            }
+
+            if (!curDuplicated && !prevDuplicated) {
+                result += cryptogram.charAt(i - 1);
+            }
+            prevDuplicated = curDuplicated;
+        }
+        return result;
+    }
 }
