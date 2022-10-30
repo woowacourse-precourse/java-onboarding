ea3711b5885895205766e533fa850d174db42064
ddingmin <ddingminyt@gmail.com>
2022-10-30 18:09:43 +0900

    feat(Ploblem5): 위 두 기능을 모두 수행하도록 구현 및 최종 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] bill = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+        for (int j : bill) {
+            answer.add(exchangeMoney(money, j));
+            money = getChange(money, j);
+        }
         return answer;
     }
 
@@ -14,7 +20,6 @@ public class Problem5 {
     }
 
     private static int exchangeMoney(int money, int k){
-        int[] bill = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        return money / bill[k];
+        return money / k;
     }
 }
