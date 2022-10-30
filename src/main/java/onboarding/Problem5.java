a2d24e3a201262e84f6810d73664ac51b5e360b2
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-10-30 23:49:03 +0900

    Problem5 반환형식 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,34 +3,38 @@ package onboarding;
 import org.mockito.Answers;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Problem5 {
-    public  static  void catchException(int num){
-        assert (num>=1);
-        assert (num<1000000);
+    public static void catchException(int num) {
+        assert (num >= 1);
+        assert (num < 1000000);
     }
-    public static ArrayList<Integer> countMoney(int money){
-        ArrayList<Integer> answer=new ArrayList<Integer>();
-        answer.add(money/50000);
-        money%=50000;
-        answer.add(money/10000);
-        money%=10000;
-        answer.add(money/5000);
-        money%=5000;
-        answer.add(money/1000);
-        money%=1000;
-        answer.add(money/500);
-        money%=500;
-        answer.add(money/100);
-        money%=100;
-        answer.add(money/50);
-        money%=50;
-        answer.add(money/10);
-        money%=10;
+
+    public static List<Integer> countMoney(int money) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(money / 50000);
+        money %= 50000;
+        answer.add(money / 10000);
+        money %= 10000;
+        answer.add(money / 5000);
+        money %= 5000;
+        answer.add(money / 1000);
+        money %= 1000;
+        answer.add(money / 500);
+        money %= 500;
+        answer.add(money / 100);
+        money %= 100;
+        answer.add(money / 50);
+        money %= 50;
+        answer.add(money / 10);
+        money %= 10;
         answer.add(money);
         return answer;
     }
-    public static ArrayList<Integer> solution(int money) {
+
+    public static List<Integer> solution(int money) {
         catchException(money);
         return countMoney(money);
     }
