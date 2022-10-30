77750c8c8f43d70e87b772e0a09062f0b0111111
nakalic <nakalic28@gmail.com>
2022-10-30 23:53:54 +0900

    feat(Problem7): 친구 관계 정리 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
@@ -8,4 +10,40 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+	private static HashMap makeFriendsList(String user, List<List<String>> data, List<String> visitors) {
+		HashMap<String, List<String>> temp = new HashMap<>();
+		temp.put(user, new ArrayList<>());
+
+		for (List info : data) {
+			for (Object person : info) {
+				if (!temp.containsKey((String)person)) {
+					ArrayList<String> one = new ArrayList<>();
+					one.add("0");
+					temp.put((String)person, one);
+				}
+			}
+
+			//앞 사람 확인
+			if (temp.containsKey(info.get(0))) {
+				List<String> trash = temp.get(info.get(0));
+				trash.add((String)info.get(1));
+			}
+
+			//뒷 사람 확인
+			if (temp.containsKey(info.get(1))) {
+				List<String> trash = temp.get(info.get(1));
+				trash.add((String)info.get(0));
+			}
+
+			for (String visitor : visitors) {
+				if (!temp.containsKey(visitor)) {
+					ArrayList<String> one = new ArrayList<>();
+					one.add("0");
+					temp.put(visitor, one);
+				}
+			}
+		}
+		return temp;
+	}
 }
