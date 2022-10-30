1475772c62c13c4d7b3ba25c033ee2b5f7c53cd4
bumwoo <gld55602@gmail.com>
2022-10-30 17:44:26 +0900

    feat(Problem1): 점수 획득 기능 구현
    페이지 번호의 각 자리수를 합한 값, 곱한 값 중 최댓값을 리턴
    오른쪽 페이지 점수와 왼쪽 페이지 점수 중 최댓값을 점수로 획득

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,29 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+
+	private static int getScore(Page page) {
+		int left = page.getLeft();
+		int right = page.getRight();
+
+		return Math.max(pageNumberToScore(left), pageNumberToScore(right));
+	}
+
+	private static int pageNumberToScore(int pageNumber) {
+		int sumScore = 0;
+		int multipleScore = 1;
+
+		while (pageNumber > 0) {
+			sumScore += pageNumber % 10;
+			multipleScore *= pageNumber % 10;
+
+			pageNumber /= 10;
+		}
+
+		return Math.max(sumScore, multipleScore);
+	}
 }
