75a5e58e23cf474facd68c77c1a581e88f723014
sangil <gsl0505@naver.com>
2022-10-30 18:20:28 +0900

    feat(PROBLEM6): form 형식에 맞지 않는 예외사항 확인 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,11 +9,53 @@ import java.util.stream.Collectors;
 public class Problem6 {
 	private static final int EMAIL_INDEX = 0;
 	private static final int NAME_INDEX = 1;
+	private static final int MIN_CREW_COUNT = 1;
+	private static final int MAX_CREW_COUNT = 10000;
+	private static final int MIN_EMAIL_LENGTH = 11;
+	private static final int MAX_EMAIL_LENGTH = 19;
+	private static final int MIN_NAME_LENGTH = 1;
+	private static final int MAX_NAME_LENGTH = 19;
+	private static final String POSSIBLE_DOMAIN = "email.com";
+	private static final String NAME_REGEX = "^[가-힣]*$";
 
 	public static List<String> solution(List<List<String>> forms) {
+		if (!checkInputForm(forms)) {
+			throw new IllegalArgumentException();
+		}
 		Map<String, Integer> duplicatedNameCount = checkDuplicatedNameCount(forms);
 		List<String> limitList = makeDuplicateEmailList(forms, duplicatedNameCount);
 		return makeResultForm(limitList);
+	}
+
+	private static boolean checkInputForm(List<List<String>> forms) {
+		return checkLimitCrewCount(forms) && checkEmailLength(forms) && checkEmailDomain(forms)
+			&& checkNameForm(forms) && checkNameLength(forms);
+	}
+
+	private static boolean checkLimitCrewCount(List<List<String>> forms) {
+		return forms.size() >= MIN_CREW_COUNT && forms.size() <= MAX_CREW_COUNT;
+	}
+
+	private static boolean checkEmailLength(List<List<String>> forms) {
+		return forms.stream()
+			.allMatch(form -> form.get(EMAIL_INDEX).length() >= MIN_EMAIL_LENGTH
+				&& form.get(EMAIL_INDEX).length() <= MAX_EMAIL_LENGTH);
+	}
+
+	private static boolean checkEmailDomain(List<List<String>> forms) {
+		return forms.stream()
+			.allMatch(form -> form.get(EMAIL_INDEX).contains(POSSIBLE_DOMAIN));
+	}
+
+	private static boolean checkNameForm(List<List<String>> forms) {
+		return forms.stream()
+			.allMatch(form -> form.get(NAME_INDEX).matches(NAME_REGEX));
+	}
+
+	private static boolean checkNameLength(List<List<String>> forms) {
+		return forms.stream()
+			.allMatch(form -> form.get(NAME_INDEX).length() >= MIN_NAME_LENGTH
+				&& form.get(NAME_INDEX).length() <= MAX_NAME_LENGTH);
 	}
 
 	private static Map<String, Integer> checkDuplicatedNameCount(List<List<String>> forms) {
