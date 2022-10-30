0fdbc0b0afd98612cbad730071c825238497b0d2
hectick <chyeon1223@cau.ac.kr>
2022-10-30 22:27:09 +0900

    refactor : [Problem3] 주석 삭제

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,13 +12,10 @@ public class Problem3 {
     }
 
     private static int checkCountOfClaps(int number){
-        //입력값의 맨 뒷자리 수부터 차례대로 3,6,9가 들어가는지 검사
         int count = 0;
         while(number > 0){
             int tmp = number%10;
             if(tmp != 0 && tmp%3 == 0){
-                //3,6,9가 들어간 경우
-                //3,6,9가 포함된 횟수만큼 answer를 증가
                 count++;
             }
             number /= 10;
