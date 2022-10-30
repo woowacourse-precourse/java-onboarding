7f88aed1361edd71a5c5b88dfd2657ef52b4bcd5
Dennis-K1 <dennis.k1.dev@gmail.com>
2022-10-31 00:08:40 +0900

    Problem3_modified test clear

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,31 +1,26 @@
 package onboarding;
 
-import java.util.ArrayList;
 
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
+        // 매 횟수별 손뼉수 계산
         for (int i = 1; i <= number; i++){
-            ArrayList<Integer> array = intToArray(i);
-            for (int element: array){
-                if (element == 3 || element == 6 || element == 9 ){
-                    answer += 1;
-                }
-            }
+            answer += checkDigits(i);
         }
         return answer;
     }
-
-    // 정수 배열로 변환
-    public static ArrayList intToArray(int num){
-        ArrayList<Integer> array = new ArrayList<Integer>();
+    // 정수 자릿수별 3,6,9 확인하여 손뼉수 계산
+    public static int checkDigits(int num){
+        int answer = 0;
         do{
-            array.add(num % 10);
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9){
+                answer += 1;
+            };
             num /= 10;
         } while  (num > 0);
-
-        return array;
+        return answer;
     }
 
 }
