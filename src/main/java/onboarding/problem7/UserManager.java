6f47df9e8e7bb9906697054022dff1e99ee789e3
situodys <situodys@gmail.com>
2022-10-31 01:25:49 +0900

    feat(문제7): 규칙에 따른 점수 계산

--- src/main/java/onboarding/problem7/UserManager.java
+++ src/main/java/onboarding/problem7/UserManager.java
@@ -7,10 +7,28 @@ public class UserManager {
     private static final int SECOND_ID = 1;
 
     private Map<String, Set<String>> friendsTable;
+    private Map<String,Integer> friendsScore;
+    private Map<String,Integer> visitCount;
 
-    public UserManager(List<List<String>> friends) {
+    public UserManager(List<List<String>> friends,List<String> visitors) {
         this.friendsTable = new HashMap<>();
+        this.friendsScore = new HashMap<>();
+        this.visitCount = new HashMap<>();
         initFriendsTable(friends);
+        initVisitCount(visitors);
+    }
+
+    private void initFriendsScore(String targetUser) {
+        for (String friend : friendsTable.keySet()) {
+            if(targetUser.equals(friend)) continue;
+            friendsScore.put(friend, visitCount.get(friend) + countShardFriends(friend, targetUser));
+        }
+    }
+
+    private void initVisitCount(List<String> visitors) {
+        for (String visitor : visitors) {
+            visitCount.put(visitor, visitCount.getOrDefault(visitor, 0) + 1);
+        }
     }
 
     private int countShardFriends(String currentUser, String inputUser) {
