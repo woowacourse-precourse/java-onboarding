eb7584f8ad0f3e6cdbe56bbdf1e441b283447247
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-30 15:03:21 +0900

    feat: create class for notify duplication

---
+++ src/main/java/onboarding/problem6/DuplicationNotificator.java
@@ -0,0 +1,21 @@
+package onboarding.problem6;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class searching for duplicated nickname and notify email addresses
+ */
+public class DuplicationNotificator {
+
+    private Map<String, List<String>> parts = new HashMap<>();
+
+    /**
+     * Constructor with forms
+     * @param forms email and nickname list
+     */
+    public DuplicationNotificator(List<List<String>> forms) {
+
+    }
+}
