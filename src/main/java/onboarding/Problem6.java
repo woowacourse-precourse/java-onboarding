6ad1dc70068df5487c12e662bec4bf655d5d5313
dong young <dong5854@gmail.com>
2022-10-31 02:44:38 +0900

    [Problem6] feat: add duplicatedNickName function
    같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록을 반환하는 duplicatedNickName 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,8 +10,53 @@ public class Problem6 {
     // 이메일을 중복 추가 방지용 해시맵(email, boolean), true 면 이미 추가된 이메일
     static HashMap<String, Boolean> emailAlreadyAdded = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        return duplicatedNickName(forms);
+    }
+
+    public static List<String> duplicatedNickName(List<List<String>> forms){
+        List<String> result = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            // 이메일 도메인이 email.com 이 아니면 스킵
+            if (!isValidEmailDomain(email)){
+                continue;
+            }
+
+            // 이메일을 중복 추가 방지용 해시맵에 추가
+            emailAlreadyAdded.put(email, false);
+
+            for (int i = 0; i < nickname.length(); i++) {
+                if (i + 1 < nickname.length()) {
+                    String sliced = nickname.substring(i, i + 2);
+
+                    // 중복 확인
+                    if (twoSequence.containsKey(sliced)) { // 중복이 있으면,
+                        // 이메일 추가
+                        result.add(email);
+                        emailAlreadyAdded.replace(email, true);
+
+                        String originalEmail = twoSequence.get(sliced);
+                        // 원본 이메일이 result 에 추가된 적 없다면(value 가 false), result 에 추가 후 value 를 true 로 바꿈
+                        if (!emailAlreadyAdded.get(originalEmail)){
+                            result.add(originalEmail);
+                            emailAlreadyAdded.replace(originalEmail, true);
+                        }
+                    } else { // 중복이 없으면,
+                        // 잘린 단어를 중복 확인용 해시맵에 추가
+                        twoSequence.put(sliced, email);
+                    }
+                }
+            }
+        }
+
+        // 중복 제거
+        List<String> organizedResult = removeDuplicates(result);
+        // 오름차순 정렬
+        organizedResult.sort(Comparator.naturalOrder());
+        return organizedResult;
     }
 
     public static List<String> removeDuplicates(List<String> list){
