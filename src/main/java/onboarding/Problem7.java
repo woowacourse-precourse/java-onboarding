044eb99a5c5f2d95a4db709097b61f535ec730b3
hossang <zkzk104@naver.com>
2022-10-30 23:00:05 +0900

    feat: Map 초기화하는 메소드(initializeMap) 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,36 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        SortedMap<String, Integer> map = new TreeMap<>();
+        ArrayList<ArrayList<Integer>> al = new ArrayList<>();
+
+        initializeMap(user, friends, visitors, map); 
+
         return answer;
+    }
+
+    private static void initializeMap(String user, List<List<String>> friends, List<String> visitors, SortedMap<String, Integer> map) {
+        int idx = 0;
+        map.put(user, 0);
+        for (List<String> friend : friends) {
+            if (!map.containsKey(friend.get(0))) {
+                map.put(friend.get(0), 0);
+            }
+            if (!map.containsKey(friend.get(1))) {
+                map.put(friend.get(1), 0);
+            }
+        }
+        for (String visitor : visitors) {
+            if (!map.containsKey(visitor)) {
+                map.put(visitor, 0);
+            }
+        }
+        for (String s : map.keySet()) {
+            map.replace(s, 0, idx++);
+        }
     }
 }
