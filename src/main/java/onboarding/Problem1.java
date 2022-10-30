7b3c4c5e60afed02681c17c1599d147019e7d28a
JaeUpSu <hyeongs2323@gmail.com>
2022-10-30 17:49:09 +0900

    PROBLEM1 | 각각의 점수 최댓값 구하기 & 타입변환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,12 +7,66 @@ class Problem1 {
     private static final int SIZE = 2;
     private static final int POBI_INDEX = 0;
     private static final int CRONG_INDEX = 1;
+    private static final int EXCEPTION = -1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         int[] scores = new int[SIZE];
 
         User[] users = inItUsers(pobi, crong);
+
+        scores[POBI_INDEX] = getScore(users[POBI_INDEX]);
+        scores[CRONG_INDEX] = getScore(users[CRONG_INDEX]);
+
+        return answer;
+    }
+
+    private static int getScore(User user) {
+        int maxPage = 400;
+        int maxNum = 0;
+
+        int left = user.getLeft();
+        int right = user.getRight();
+
+        if (right - left > 1 || left % 2 == 0 || right % 2 == 1 || right > maxPage || left > right) {
+            return EXCEPTION;
+        }
+
+        for (int op = 0; op < SIZE; op++) {
+            int leftPage = left;
+            int rightPage = right;
+
+            if (op == 0) {
+                leftPage = plus(leftPage);
+                rightPage = plus(rightPage);
+            } else if (op == 1) {
+                leftPage = multiple(leftPage);
+                rightPage = multiple(rightPage);
+            }
+            maxNum = Math.max(maxNum, Math.max(leftPage, rightPage));
+        }
+
+        return maxNum;
+    }
+
+    private static int multiple(int value) {
+        int answer = 1;
+        String page = intToString(value);
+
+        for (int i = 0; i < page.length(); i++) {
+            answer *= charToInt(page.charAt(i));
+        }
+
+        return answer;
+    }
+
+    private static int plus(int value) {
+        int answer = 0;
+        String page = intToString(value);
+
+        for (int i = 0; i < page.length(); i++) {
+            answer += charToInt(page.charAt(i));
+        }
 
         return answer;
     }
@@ -32,6 +86,14 @@ class Problem1 {
         users[CRONG_INDEX] = new User(left, right);
 
         return users;
+    }
+
+    private static int charToInt(char c) {
+        return Character.getNumericValue(c);
+    }
+
+    private static String intToString(int num) {
+        return String.valueOf(num);
     }
 
     private static class User {
