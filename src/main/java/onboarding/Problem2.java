71b4e4227f0ff5688229258b296a36a95b2cf47f
gomljo <gomljo0307@naver.com>
2022-10-31 03:38:24 +0900

    feat(Problem2.java): 기능 구현 #1
    입력된 문자열 내 중복 문자의 인덱스를 찾아 반환하는 findRedundantAlphabet함수를 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,20 @@
 package onboarding;
+
+import java.util.List;
+
 class StringOperation {
     boolean isSameAlphabet(char previous, char next) {
         return previous == next;
+    }
+    List<Integer> findRedundantAlphabet(String cryptogram, int startIndex) {
+
+        int movingIndex = startIndex;
+        List<Integer> redundantCharacterIndex;
+        while ((movingIndex+1<cryptogram.length())&&isSameAlphabet(cryptogram.charAt(movingIndex), cryptogram.charAt(movingIndex + 1))) {
+            movingIndex++;
+        }
+        redundantCharacterIndex = List.of(startIndex, movingIndex);
+        return redundantCharacterIndex;
     }
 }
 public class Problem2 {
