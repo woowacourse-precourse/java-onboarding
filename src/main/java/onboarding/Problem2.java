99ade75fe1f2d086323e0d2a53c3ed79e85a07b8
sdsr <dlxodnr1212@naver.com>
2022-10-30 18:41:22 +0900

    problem2 기능 1 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        List<String> list = Arrays.asList(answer.split(""));
+
+
+
         return answer;
     }
+
 }
