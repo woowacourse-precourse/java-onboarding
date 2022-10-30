364fc72963cb31ee45e20f70278389f00d2d281d
DevJay1024 <devjay1024@gmail.com>
2022-10-30 19:37:26 +0900

    test: 문제 2번의 테스트 케이스 추가 및 테스트 케이스별 한글 설명 추가

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -8,16 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Nested
 public class Problem2Test {
     @Test
-    void case1() {
+    @DisplayName("browoanoommnaon의 결과는 brown이다.")
+    void given_browoanoommnaon_then_brown() {
         String cryptogram = "browoanoommnaon";
         String result = "brown";
         assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
     }
 
     @Test
-    void case2() {
+    @DisplayName("zyelleyz의 결과는 공백이다.")
+    void given_zyelleyz_then_BLANK() {
         String cryptogram = "zyelleyz";
         String result = "";
+        assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("succession의 결과는 sueion이다.")
+    void given_succession_then_sueion() {
+        String cryptogram = "succession";
+        String result = "sueion";
         assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
     }
 }
