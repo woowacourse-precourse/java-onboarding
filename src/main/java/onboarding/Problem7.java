97d75e5fe8c1532544f7bf1d8336f65e39960385
yoon <beatus96@naver.com>
2022-10-30 23:06:44 +0900

    [Problem 7] findUserFriends함수 : 유저의 친구 set 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        return new ArrayList<>();
     }
+
+
+    //유저의 친구관계 알아내기
+    public static Set<String> findUserFriends(String user, List<List<String>> friends) {
+        Set<String> userFriend = new HashSet<>();
+        for (List<String> f : friends) {         //user이름 포함되면 set에 넣기
+            if (f.contains(user)) {
+                userFriend.add(f.get(0));
+                userFriend.add(f.get(1));
+            }
+        }
+        return userFriend;
+    }
+
 }
