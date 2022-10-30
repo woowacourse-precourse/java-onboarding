c72232c6214eb666c60a164b1c904bc4e5087ba3
Jzakka <mouse4786@gmail.com>
2022-10-30 16:00:31 +0900

    Feat: "글자 변환 기능 구현"

--- src/main/java/problem4/LetterConverter.java
+++ src/main/java/problem4/LetterConverter.java
@@ -10,4 +10,19 @@ public class LetterConverter {
         else if(letter >= 'A' && letter <= 'Z') return LetterType.CAPITAL;
         else return LetterType.NON_ALPHABET;
     }
+
+    public static char letterConvert(char letter) {
+        LetterType letterType = getLetterType(letter);
+
+        switch (letterType) {
+            case SMALL:
+                return (char)('a' + 'z' - letter);
+            case CAPITAL:
+                return (char)('A' + 'Z' - letter);
+            case NON_ALPHABET:
+                return letter;
+            default:
+                throw new RuntimeException("Undefined Letter Type");
+        }
+    }
 }
