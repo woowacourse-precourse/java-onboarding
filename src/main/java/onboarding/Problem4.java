c09c52e644baa4271830dacdb842461b7b59bbb3
YooSuYeon <usuyn000@gmail.com>
2022-10-31 04:22:22 +0900

    feat(Problem4): 문자 변환 table 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    public static final int A_DECIMAL = 65;
+    public static final int Z_DECIMAL = 90;
+
     public static String solution(String word) {
         String answer = "";
+
+        Map<Character, Character> convertTable = new HashMap<>();
+
+        setConvertTable(convertTable);
+
         return answer;
+    }
+
+    private static void setConvertTable(Map<Character, Character> convertTable) {
+        for (int i = 0; i <= (Z_DECIMAL - A_DECIMAL); i++) {
+            convertTable.put((char) (A_DECIMAL + i), (char) (Z_DECIMAL - i));
+        }
     }
 }
