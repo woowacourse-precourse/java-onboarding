1d556c9c84c3e3b5c98fc73127ec4c4b998dcfcd
splitube <voicesplit@daum.net>
2022-10-30 13:20:32 +0900

    4번 문제 - Frog 객체 기능 구현, 기능 수정
     * 1. listen(String word) = 청개구리가 문자열을 듣는다.
     * 2. speak() = 들은 단어를 모두 반대로 바꾸어 반환한다.
     * 3. change_word() = 들은 문자열을 바꿔서 반환한다.
     * 4. change_character(char c) = c를 알맞게 바꾸어 반환
     * 5. is_uppercase(char c) = 현재 문자가 대문자인지 반환
     * 6. is_lowercase(char c) = 현재 문자가 소문자인지 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,20 +2,57 @@ package onboarding;
 
 /**
  * Frog
- *
+ * <p>
  * 1. listen(String word) = 청개구리가 문자열을 듣는다.
  * 2. speak() = 들은 단어를 모두 반대로 바꾸어 반환한다.
- * 3. change_string(String word) = 현재 문자열을 반대로 바꾼다.
+ * 3. change_word() = 들은 문자열을 바꿔서 반환한다.
  * 4. change_character(char c) = c를 알맞게 바꾸어 반환
  * 5. is_uppercase(char c) = 현재 문자가 대문자인지 반환
  * 6. is_lowercase(char c) = 현재 문자가 소문자인지 반환
- * 7. change_uppercase(char c) = 대문자 문자를 반대로 바꿔서 반환
- * 8. change_lowercase(char c) = 소문자 문자를 반대로 바꿔서 반환
  */
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Frog frog = new Frog();
+        frog.listen(word);
+        return frog.speak();
+    }
+}
+
+class Frog {
+    private String word;
+
+    public void listen(String word) {
+        this.word = word;
+    }
+
+    public String speak() {
+        return change_word();
+    }
+
+    private String change_word() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(change_character(word.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    private char change_character(char c) {
+        if (is_uppercase(c)) {
+            return (char) (155 - c);
+        }
+        if (is_lowercase(c)) {
+            return (char) (219 - c);
+        }
+        return c;
+    }
+
+    private boolean is_uppercase(char c) {
+        return 'A' <= c && c <= 'Z';
+    }
+
+    private boolean is_lowercase(char c) {
+        return 'a' <= c && c <= 'z';
     }
 }
