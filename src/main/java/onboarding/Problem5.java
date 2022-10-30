52aed79c3d2cf960cf7b31420c22a365fcf4d43f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 21:16:55 +0900

    feat: Problem5 - moneyCount 메서드 구현
    50000부터 나누어 몫을 저장하고 값을 차감하며 1까지 카운트를 List로 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * moneyCount 메서드 돈을 50000부터 나누어 몫을 저장후 값을 차감하며 1까지 카운트를 List로 변환
+ * moneyCount 메서드 돈을 50000부터 나누어 몫을 저장후 값을 차감하며 1까지 카운트를 List로 반환
  */
 
 
@@ -16,5 +16,13 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<Integer> moneyCount(List<Integer> count, int money) {
+        for (int i = 0; i < AMOUNT.length; i++) {
+            count.add(money / AMOUNT[i]);
+            money %= AMOUNT[i];
+        }
+        return count;
     }
 }
