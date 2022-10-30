3c563117e826ba56bdec51cc8d70ba8db3101968
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-10-30 16:16:27 +0900

    feat(문제 1): 입력된 책 번호 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,11 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    boolean validation(List<Integer> pages) {
+        if (pages.size() != 2) return false;
+        if (pages.get(0) == 1 || pages.get(1) == 400) return false;
+        if (pages.get(0) % 2 == 0) return false;
+        return pages.get(1) - pages.get(0) == 0;
+    }
 }
