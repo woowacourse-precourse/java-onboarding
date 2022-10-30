77fae965dc812b2848d77aa99700a72fad594bfa
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 22:54:41 +0900

    [문제1] 변수 선언 및 제한사항 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,19 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiMaxNum=0;
+        int crongMaxNum=0;
+
+        if(pobi.size()<2 || crong.size()<2 || pobi.size()>3 || crong.size()>3){
+            return -1;
+        }
+        if(pobi.get(0)<1 || pobi.get(1)>400 || (pobi.get(0)+1 != pobi.get(1))){
+            return -1;
+        }
+
+        
+
         return answer;
     }
 }
