c288d21c30e881f9c2ab1cee7ad9cb97191afe80
KimTaeKang57 <zizoen123@gmail.com>
2022-10-30 20:19:22 +0900

    문제 4 : 알파벳 외의 문자는 그대로 나오도록 코드 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -16,14 +16,12 @@ public class Problem4 {
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
 
-            if (word.charAt(i) == 32) {
-                sb.append(c);
-            }
-
             if (c >= 64 && c <= 90) { // 대문자에 해당하는 경우
                 sb.append((char) ('Z' - c + 65));
             } else if (c >= 97 && c <= 122) { // 소문자에 해당하는 경우
                 sb.append((char) ('z' - c + 97));
+            } else {
+                sb.append(c);
             }
         }
 
