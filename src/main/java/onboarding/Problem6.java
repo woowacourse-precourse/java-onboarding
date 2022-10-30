6116f96a9d650c450af7062944e59ab89539023e
java-saeng <gusdn85554@naver.com>
2022-10-30 16:35:27 +0900

    feat : Problem6 forms 파싱하여 크루 정보 초기화 메서드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String, String> crewInfo = new HashMap<>();
+
+        initializationCrewInfo(forms, crewInfo);
+
         return answer;
+    }
+
+    private static void initializationCrewInfo(List<List<String>> forms,
+                                               Map<String, String> crewInfo) {
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            String email = form.get(0);
+
+            crewInfo.put(nickname, email);
+        }
     }
 }
