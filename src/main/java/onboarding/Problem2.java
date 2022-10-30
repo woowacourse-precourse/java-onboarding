176df4f68b2e259093134a0f26cb389714e14e02
hossang <zkzk104@naver.com>
2022-10-30 20:27:07 +0900

    feat: 문제2 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,46 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Deque<Character> dq1 = new ArrayDeque<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            dq1.addLast(cryptogram.charAt(i));
+        }
+        //중복 문자열 삭제하기
+        int pre;
+        do {
+            pre = dq1.size();
+            if (pre == 0) {
+                break;
+            }
+            Deque<Character> dq2 = new ArrayDeque<>();
+            dq2.addLast(dq1.pollFirst());
+            while (!dq1.isEmpty()) {
+                if (dq2.peekLast() == dq1.peekFirst()) {
+                    while (dq2.peekLast() == dq1.peekFirst()) {
+                        dq1.pollFirst();
+                    }
+                    dq2.pollLast();
+                }
+                if(dq2.peekLast() != dq1.peekFirst()) {
+                    dq2.addLast(dq1.pollFirst());
+                }
+            }
+            dq1 = dq2;
+        } while (pre != dq1.size());
+        //결과 리턴
+        if (pre == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character character : dq1) {
+            sb.append(character);
+        }
+        return sb.toString();
+
     }
 }
