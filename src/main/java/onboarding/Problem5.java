a146d016dce3625c7e05c7df6fcc153aa35256dd
kkm5291 <kkm23125291@gmail.com>
2022-10-30 19:27:12 +0900

    Problem5 Feat : 거스름돈 계산기 calc 메서드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,12 +8,21 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
 
+        int[] exchangeCount = calc(money);
 
 
         return answer;
     }
 
-    public static int calc(int money) {
+    public static int[] calc(int money) {
+        int[] exchange = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] count = new int[exchange.length];
 
+        for (int i = 0; i < exchange.length; i++) {
+            count[i] = money / exchange[i];
+            money = money % exchange[i];
+        }
+
+        return count;
     }
 }
