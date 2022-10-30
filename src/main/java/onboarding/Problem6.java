a1c66a608ef243443677d31f10e4dc58017f2fa0
limecats <limecats0331@gmail.com>
2022-10-30 16:46:56 +0900

    feat(Problem6): [추가] 각 폼에 대한 탐색 메소드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,27 +1,54 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
+
+    private static HashMap<String, Set<String>> overlap = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            searchByForm(nickname, email);
+        }
+
         return answer;
+    }
+
+    private static void searchByForm(String nickname, String email) {
+        if (nickname.length() < 2) {
+            return;
+        }
+
+        for (int size = nickname.length(); size > 1; size--) {
+            searchContinuousStringBySize(nickname, size, email);
+        }
+    }
+
+    private static void searchContinuousStringBySize(String nickname, int size, String email) {
+
     }
 }
 
 /* 구현 로직
-* 1. 연속된 문자가 저장된 해쉬맵이 있다.
-* 2. 각 form에 저장되어 있는 닉네임을 가져온다.
-*   3. 각 닉네임의 길이부터 2자리까지 모든 것들을 탐색한다.
-*       4. 탐색하면서 해쉬맵에 값이 없다면 연속된 문자열을 추가하고 이메일을 넣어준다.
-*       5. 만약 해쉬맵에 값이 있다면 해당 해쉬에 이메일만 추가해준다.
-* 6. 모든 탐색이 끝난 후 결과에 해쉬맵을 탐색하면서 email이 2개 이상 담긴 경우를 모두 넣어준다.
-* 7. 중복을 제거하고 오름차순으로 정렬한다.
-* */
+ * 1. 연속된 문자가 저장된 해쉬맵이 있다.
+ * 2. 각 form에 저장되어 있는 닉네임을 가져온다.
+ *   3. 각 닉네임의 길이부터 2자리까지 모든 것들을 탐색한다.
+ *       4. 탐색하면서 해쉬맵에 값이 없다면 연속된 문자열을 추가하고 이메일을 넣어준다.
+ *       5. 만약 해쉬맵에 값이 있다면 해당 해쉬에 이메일만 추가해준다.
+ * 6. 모든 탐색이 끝난 후 결과에 해쉬맵을 탐색하면서 email이 2개 이상 담긴 경우를 모두 넣어준다.
+ * 7. 중복을 제거하고 오름차순으로 정렬한다.
+ * */
 
 /* 기능 목록
-* 1. 각 form에 대한 연산을 하는 메소드
-*   2. 닉네임에서 n개의 연속된 문자열들을 탐색하는 메소드
-*       3. 찾은 문자열을 해쉬맵에 담는 메소드
-* 4. 모든 해쉬맵을 탐색하면서 결과를 반환하는 메소드
-* */
+ * 1. 각 form에 대한 연산을 하는 메소드 searchByForm
+ *   2. 닉네임에서 n개의 연속된 문자열들을 탐색하는 메소드
+ *       3. 찾은 문자열을 해쉬맵에 담는 메소드
+ * 4. 모든 해쉬맵을 탐색하면서 결과를 반환하는 메소드
+ * */
