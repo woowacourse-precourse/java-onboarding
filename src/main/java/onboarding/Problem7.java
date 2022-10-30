09acd2ce379d0dd513d1ea5f2e0948b3e91ab808
khj1 <dhepd8849@gmail.com>
2022-10-30 13:58:33 +0900

    feat(problem7): 함께 아는 친구의 수만큼 점수를 올리는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,17 +2,38 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Problem7 {
+    public static final int ACQUAINTANCE_SCORE = 10;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         List<String> friendsOfUser = findFriendsByUser(user, friends);
+        Map<String, Integer> scoreOfAcquaintance = addAcquaintanceScore(friends, friendsOfUser);
 
         return answer;
+    }
+
+    private static Map<String, Integer> addAcquaintanceScore(List<List<String>> friends, List<String> friendsOfUser) {
+        Map<String, Integer> numberOfAcquaintance = new HashMap<>();
+        for (String friendOfUser : friendsOfUser) {
+            List<String> acquaintances = findFriendsByUser(friendOfUser, friends);
+            for (String acquaintance : acquaintances) {
+                if (numberOfAcquaintance.containsKey(acquaintance)) {
+                    numberOfAcquaintance.put(acquaintance, ACQUAINTANCE_SCORE);
+                }
+                if (!numberOfAcquaintance.containsKey(acquaintance)) {
+                    numberOfAcquaintance.put(acquaintance, numberOfAcquaintance.get(acquaintance) + ACQUAINTANCE_SCORE);
+                }
+            }
+        }
+        return numberOfAcquaintance;
     }
 
     private static List<String> findFriendsByUser(String user, List<List<String>> friends) {
