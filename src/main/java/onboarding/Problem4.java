710637059ad72479a8b4c6c48d35fa977a7dc8b0
SonMinGyu <littleking13@naver.com>
2022-10-31 02:42:53 +0900

    [FEAT] Problem4 word to ASCII 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.sql.Array;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
         return answer;
+    }
+
+    public static int[] changeASCII(String word) {
+        int[] wordASCIIArray = new int[word.length()];
+        String[] wordArray = word.split("");
+
+
+        for(int i = 0; i < wordArray.length; i++) {
+            wordASCIIArray[i] = Integer.parseInt(wordArray[i]);
+        }
+
+        return wordASCIIArray;
     }
 }
