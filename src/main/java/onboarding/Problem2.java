757dae0dd47036478b90a18c1847f9744c342c98
jmjmjames <chungjm0711@gmail.com>
2022-10-30 19:10:12 +0900

    feat: solve Problem2 using [StringBuilder, delete method]

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,25 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer;
+        StringBuilder sb = new StringBuilder(cryptogram);
+
+        boolean isDuplicated = false;
+
+        while (!isDuplicated) {
+
+            isDuplicated = true;
+
+            for (int i = 0; i < sb.length() - 1; i++) {
+                char currChar = sb.charAt(i);
+
+                if (currChar == sb.charAt(i + 1)) {
+                    sb.delete(i, i + 2);
+                    isDuplicated = false;
+                }
+            }
+        }
+        answer = sb.toString();
         return answer;
     }
 }
