1dc945a048577a5aca4be6daf9c1c98b2c484b9d
kimdonghun <zhfptm12@naver.com>
2022-10-31 02:25:38 +0900

    problem 1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,7 +6,11 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         int maxValuePobi = Math.max(sumPage(pobi), mulPage(pobi));
-        int maxValueCrong = sumPage(pobi);
+        int maxValueCrong = Math.max(sumPage(crong), mulPage(crong));
+        if (Math.min(maxValueCrong, maxValuePobi) == -1) {
+            answer = -1;
+            return answer;
+        }
         answer = maxValuePobi > maxValueCrong ? 1 : maxValuePobi < maxValueCrong ? 2 : 0;
         return answer;
     }
@@ -15,7 +19,7 @@ class Problem1 {
         int leftPage = Integer.parseInt(left);
         int rightPage = Integer.parseInt(right);
         if (leftPage == 1 || rightPage == 400) return false;
-        if (rightPage - leftPage == 1) return false;
+        if (rightPage - leftPage != 1) return false;
 
         return true;
     }
@@ -23,13 +27,13 @@ class Problem1 {
     private static int sumPage(List<Integer> who) {
         String leftPage = String.valueOf(who.get(0));
         String rightPage = String.valueOf(who.get(1));
-        if(!checkException(leftPage, rightPage)) return -1;
-        int left=0;
-        int right=0;
-        for(int i=0; i<leftPage.length(); i++){
+        if (!checkException(leftPage, rightPage)) return -1;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < leftPage.length(); i++) {
             left += leftPage.charAt(i) - '0';
         }
-        for(int i=0; i<rightPage.length(); i++){
+        for (int i = 0; i < rightPage.length(); i++) {
             right += rightPage.charAt(i) - '0';
         }
         return Math.max(left, right);
@@ -38,13 +42,13 @@ class Problem1 {
     private static int mulPage(List<Integer> who) {
         String leftPage = String.valueOf(who.get(0));
         String rightPage = String.valueOf(who.get(1));
-        if(!checkException(leftPage, rightPage)) return -1;
-        int left=1;
-        int right=1;
-        for(int i=0; i<leftPage.length(); i++){
+        if (!checkException(leftPage, rightPage)) return -1;
+        int left = 1;
+        int right = 1;
+        for (int i = 0; i < leftPage.length(); i++) {
             left *= leftPage.charAt(i) - '0';
         }
-        for(int i=0; i<rightPage.length(); i++){
+        for (int i = 0; i < rightPage.length(); i++) {
             right *= rightPage.charAt(i) - '0';
         }
         return Math.max(left, right);
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,44 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        System.out.println(answer);
+        answer = 
+        withdraw(money);
+
         return answer;
+    }
+
+    private static List<Integer> withdraw(int money) {
+        List<Integer> list = Collections.emptyList();
+        list.add(100);
+        System.out.println(list);
+        System.out.println("ASD");
+
+        while (true) {
+
+            if (money >= 50000) {
+                money /= 50000;
+            } else if (money >= 10000) {
+                money /= 10000;
+            } else if (money >= 5000) {
+                money /= 5000;
+            } else if (money >= 1000) {
+                money /= 1000;
+            } else if (money >= 500) {
+                money /= 500;
+            } else if (money >= 100) {
+                money /= 100;
+            } else if (money >= 50) {
+                money /= 50;
+            } else if (money >= 10) {
+                money /= 10;
+            } else {
+                money /= 1;
+            }
+            break;
+
+        }
+
+        return list;
     }
 }
