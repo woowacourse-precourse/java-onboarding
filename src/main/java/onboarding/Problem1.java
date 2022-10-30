d1143f585c13e12243ae5d6e8dd3ef25b6c3dfda
ggazitbyul <ngusy123@gmail.com>
2022-10-30 23:29:07 +0900

    Problem1) 1. 규칙 해석 rev 02 2. getAddNumber 메서드 추가 rev 00 3. getMulNumber 메서드 추가 rev 00

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,15 +2,20 @@ package onboarding;
 
 import java.util.List;
 
-class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
 //      규칙
 //        	1. 책을 임의로 펼친다.
 //                1-1) 매개변수를 받는다.
+//                1-2) ApplicationTest case 1,2,3 에 상수 기입되어있음
+//          >>구현 불필요, 걍 무시하자.
+//        ------------------------------------------------------
 //        	2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
 //                2-1) 12페이지면 1, 2를 더하거나 곱해서 비교하여 큰수를 반환한다?
 //        	3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
 //                3-1) 오른쪽 페이지도 2-1과 동일한 결과 반환
+//        >> 2,3 동일한 메서드 사용가능.
+//        >> getAddNumber : 각 자리 숫자 더한값 반환 메서드
+//        >> getMulNumber : 각 자리 숫자 곱한값 반환 메서드
+
 //        	4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
 //                4-1) 왼쪽, 오른쪽 페이지 2,3 연산 후 비교
 //                4-2) 비교값 둘중에 큰 수를 pobi, crong 각각 가진다.
@@ -28,8 +33,18 @@ class Problem1 {
 //              - 길이가 뭘 의미하는걸까?
 //	        • pobi와 crong에는 [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있다.
 //              - 12 이면 13, 연번의 개념인듯.
-
+class Problem1 {
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    private static int getAddNumber(){
+
+    }
+
+    private static int getMulNumber(){
+
     }
 }
