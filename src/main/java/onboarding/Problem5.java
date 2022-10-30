6f9e6888fe3f6ca222dbcf2f0ecd13c8373988ac
Donghyun <flaehan@gmail.com>
2022-10-30 22:00:23 +0900

    money는 1이상 1,000,000이하인 자연수 제한사항 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,9 +9,11 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         int [] moneyArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        for(int i = 0; i < moneyArr.length; i++){
-            answer.add(money / moneyArr[i]);
-            money = money % moneyArr[i];
+        if(money >= 1 && money <= 1000000) {
+            for (int i = 0; i < moneyArr.length; i++) {
+                answer.add(money / moneyArr[i]);
+                money = money % moneyArr[i];
+            }
         }
         return answer;
     }
