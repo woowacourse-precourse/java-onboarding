b109bc903729c4727c081a96032a4c55c19a9eca
unknown <ymecca12@gmail.com>
2022-10-30 22:05:46 +0900

    [MISSION3] / feat : 3개수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,3 +6,15 @@ public class Problem3 {
         return answer;
     }
 }
+
+class Game{
+    static int countContainThree(int number){
+        int cnt = 0;
+        while(number != 0){
+            int rest = number % 10;
+            if(rest/3 != 0 && (rest % 3) == 0) cnt++;
+            number /= 10;
+        }
+        return cnt;
+    }
+}
