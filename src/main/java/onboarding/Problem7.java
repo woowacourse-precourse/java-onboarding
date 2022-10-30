07d190d52a31168de22dfb97bf66d57a59bdc37d
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-30 17:21:14 +0900

    add : (Problem7) 연결 정보

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String, List<String>> connectMap;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        connectMap = new HashMap<>();
+
+        makeConnection(friends);
+
         return answer;
+    }
+
+    /*
+    * 연결 관계를 Map에 저장한다.
+    * */
+    private static void makeConnection(List<List<String>> friends){
+        for (List<String> friend : friends) {
+            String f1 = friend.get(0);
+            String f2 = friend.get(1);
+
+            List<String> con = connectMap.getOrDefault(f1, new ArrayList<>());
+            con.add(f2);
+            connectMap.put(f1, con);
+
+            con = connectMap.getOrDefault(f2, new ArrayList<>());
+            con.add(f1);
+            connectMap.put(f2, con);
+        }
     }
 }
