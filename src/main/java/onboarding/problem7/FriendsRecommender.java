e2e198eebb9854a75aae3ca602d1db2a2297a8a8
Sehbeom <j3beom@gmail.com>
2022-10-31 02:08:31 +0900

    feat(Problem7): 클래스 생성 및 멤버변수 설정
    FriendsRecommender 클래스 생성 및 멤버변수 설정

---
+++ src/main/java/onboarding/problem7/FriendsRecommender.java
@@ -0,0 +1,11 @@
+package onboarding.problem7;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FriendsRecommender {
+	private String user;
+	private HashMap<String, HashSet<String>> friendsInformation;
+	private HashMap<String, Integer> recommendScore;
+
+}
