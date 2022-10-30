84a62557284e3f9a9218a1244f8c8ce4257153d5
hanjaegyeong <hjg000223@naver.com>
2022-10-31 03:27:38 +0900

    [Mission5] feat: 전체 작동 구조 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        //List<Integer> answer = Collections.emptyList();
+        ArrayList<Integer> moneys = new ArrayList<>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+        ArrayList<Integer> answer = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0));
+        setAnswer(money, moneys, answer);
         return answer;
     }
 }
