81e96147e2f97acea288569cf4dad5d7bf2e4227
kkirby54 <sdk926@gmail.com>
2022-10-30 18:09:16 +0900

    비교 후 결과 반환 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -33,6 +33,12 @@ class Problem1 {
         int pobiSumMax = Math.max(getDigitSum(pobi.get(0)), getDigitSum(pobi.get(1)));
         int pobiMulMax = Math.max(getDigitMul(pobi.get(0)), getDigitMul(pobi.get(1)));
 
+        // 최종 점수를 빼서 양수면 크롱, 음수면 포비, 0이면 무승부
+        int cmpValue = Math.max(crongSumMax, crongMulMax) - (Math.max(pobiSumMax, pobiMulMax));
+        if (cmpValue > 0) answer = 2;
+        else if (cmpValue < 0) answer = 1;
+        else answer = 0;
+
         return answer;
     }
 }
