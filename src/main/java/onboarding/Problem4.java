45322a9ff215f1013900ff8ff2a7799cf2b0e72c
ChoiYeonho0903 <903yh@naver.com>
2022-10-30 16:47:23 +0900

    Problem4 : isUpperCase, isLowerCase 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+    }
+
+    private static boolean isUpperCase(char c) {
+        return c >= 'A' && c <= 'Z';
+    }
+
+    private static boolean isLowerCase(char c) {
+        return c >= 'a' && c <= 'z';
     }
 }
