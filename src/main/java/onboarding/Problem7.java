d29e0b9d919ae66bacf8487f7b4c76510d1c77f4
yuntaeLee <leeyt1201@gmail.com>
2022-10-30 22:52:18 +0900

    feat(Problem7): Friend class 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,46 @@
 package onboarding;
 
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+    static class Friend implements Comparable<Friend> {
+        private String name;
+        private int score;
+        private List<String> myFriends;
+
+        Friend (String name, int score) {
+            this.name = name;
+            this.score = score;
+            myFriends = new ArrayList<>();
+        }
+
+        @Override
+        public int compareTo(Friend o) {
+            if (this.score == o.score) {
+                return name.compareTo(o.name);
+            }
+
+            return o.score - score;
+        }
+
+        public void addScore(int score) {
+            this.score += score;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<String> getMyFriends() {
+            return myFriends;
+        }
+    }
+
+    public static List<String> solution(String user, List<List<String>> friendships, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
