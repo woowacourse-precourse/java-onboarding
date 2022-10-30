b30bbd19118cc1a3a43c02e0739312ad09f7b82c
Woojin Lee <dhkep03@gmail.com>
2022-10-30 12:50:43 +0900

    p4-feat: 문자열을 반대로 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
+    public static String solution(String sentence) {
+        String answer = getReverseSentence(sentence);
         return answer;
+    }
+
+    static String getReverseSentence(String sentence) {
+        ArrayList<Character> characterList = convertStringToArrayList(sentence);
+        ArrayList<Character> reverseCharacterList = getReverseCharacterList(characterList);
+        String reverseSentence = joinCharactersInArrayList(reverseCharacterList);
+        return reverseSentence;
     }
 }
