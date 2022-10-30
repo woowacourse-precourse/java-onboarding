84e7cd5c1f148678fea89e71cdcf0fd6f68d3a44
oreo <ohsimon77@naver.com>
2022-10-30 22:32:40 +0900

    문제 6번 commit

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,41 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        //List<String> answer = List.of("answer");
+        ArrayList<String> answer = new ArrayList<>();
+
+        ArrayList<ArrayList<String>> word = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            ArrayList<String> word2 = new ArrayList<>();
+            String nickname = form.get(1);
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                word2.add(nickname.substring(j, j + 2));
+                //word_list.add(nickname.substring(j,j+2));
+            }
+            word.add(word2);
+        }
+
+        for(int i=0; i<word.size(); i++){
+            for(int j=0; j<word.size(); j++){
+                for (String s : word.get(j)){
+                    //System.out.println(s);
+                    if (word.get(i).contains(s) && i!=j){
+                        answer.add(forms.get(i).get(0));
+                    }
+                }
+            }
+        }
+        ArrayList<String> answer2 = new ArrayList<>();
+
+        for(String ans : answer){
+            if(!answer2.contains(ans))
+                answer2.add(ans);
+        }
+        answer2.sort(Comparator.naturalOrder());
+        return answer2;
     }
 }
