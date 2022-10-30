363ee2edde91aed84efe468ccb304ddf4853a5c7
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 17:11:30 +0900

    feat(Problem6): 모든 유저에 대해 유저가 가지는 닉네임의 연속 부분 문자열을 체크해주어 email과 mapping 해주는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+
+        Map<String, Set<String>> continuousThings = new HashMap<String, Set<String>>();
+        findContinuousStringInNickname(continuousThings, forms);
         return answer;
+    }
+
+    /**
+     * Function for continuousString that contains all 2-length string in nickname of everyone
+     * And Everyone is separated bt email
+     * */
+    private static void findContinuousStringInNickname(Map<String, Set<String>> continuousThings,
+                                                   List<List<String>> forms) {
+        for (List<String> info : forms) {
+            String nickname = info.get(1);
+            String email = info.get(0);
+
+            for (int j = 0; j < nickname.length()-1; j++) {
+                String nameForCheck = nickname.substring(j,j+2);
+
+                if (!continuousThings.containsKey(nameForCheck)) {
+                    continuousThings.put(nameForCheck, new HashSet<String>());
+                }
+                continuousThings.get(nameForCheck).add(email);
+            }
+        }
     }
 }
