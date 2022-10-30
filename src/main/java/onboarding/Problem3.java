be0b9648efec24ddff50ddcf7ea5545e93a52378
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-30 16:32:51 +0900

    Refactor : line edit

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,12 +4,15 @@ public class Problem3 {
 	public static int solution(int number) {
 
 		int count = 0;
+
 		for (int i = 1; i <= number; i++) {
 			String numberStr = String.valueOf(i);
-			count = (int) (count + countChar(numberStr, '3') + countChar(numberStr, '6') + countChar(numberStr, '9'));
+			count = (int) (count + countChar(numberStr, '3')
+					+ countChar(numberStr, '6')
+					+ countChar(numberStr, '9'));
 		}
-		int answer = count;
-		return answer;
+
+		return count;
 	}
 
 	public static long countChar(String numberStr, char threeSixNine) {
