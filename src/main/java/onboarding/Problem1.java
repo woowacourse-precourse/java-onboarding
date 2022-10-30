3a17b0f60a2e141b5451a39ea458d634ebe334e4
sxxgxxnz <tmddms2292@gmail.com>
2022-10-30 13:59:34 +0900

    feat : [1] 문자열을 비교하기 위해 스택 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,69 @@
 package onboarding;
 
+import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pMax=0;
+        int cMax=0;
+        List<Integer> pList=new ArrayList<Integer>();
+        List<Integer> cList=new ArrayList<Integer>();
+
+        for(int i=0;i<pobi.size();i++){
+
+            if(pobi.get(i) == 1 || pobi.get(i)==400){
+                answer = -1;
+                break;
+            }
+
+            int tmp = pobi.get(i);
+            int sum=0,multi=1;
+            while(tmp>0){
+                sum += tmp%10;
+                multi *=tmp%10;
+                tmp/=10;
+            }
+            pList.add(sum);
+            pList.add(multi);
+
+        }
+        for(int i=0;i<crong.size();i++){
+
+            if(crong.get(i) == 1 || crong.get(i)==400){
+                answer = -1;
+                break;
+            }
+            int tmp = crong.get(i);
+            int sum = 0, multi = 1;
+            while(tmp > 0){
+                sum+=tmp%10;
+                multi*=tmp%10;
+                tmp/=10;
+            }
+            cList.add(sum);
+            cList.add(multi);
+        }
+        pList.sort(Comparator.reverseOrder());
+        cList.sort(Comparator.reverseOrder());
+        pMax = pList.get(0);
+        cMax = cList.get(0);
+
+        if(pMax == cMax){
+            answer = 0;
+        }else if(pMax > cMax){
+            answer = 1;
+        }else if(pMax < cMax){
+            answer = 2;
+        }
+        if(pobi.get(1)-pobi.get(0)!=1){
+            answer = -1;
+        }
+        if(crong.get(1)-crong.get(0)!=1){
+            answer = -1;
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<String> stk = new Stack<String>();
         return answer;
     }
+
 }
