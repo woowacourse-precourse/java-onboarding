e1dea66909c6d9b80b62f7027fa13517f2ada58f
kim0914 <kosa0914@naver.com>
2022-10-30 22:02:09 +0900

    feat: Problem6 form 예외 처리 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,6 +11,33 @@ public class Problem6 {
         return answer;
     }
 
+    private static void checkForm(List<List<String>> forms){
+        if (isCrewWrongSize(forms)){
+            throw new IllegalArgumentException("크루는 1명 이상 10,000명 이하까지 가능합니다.");
+        }
+
+        for (List<String> form : forms) {
+            if (isFormWrongSize(form)){
+                throw new IllegalArgumentException("form 하나의 크기는 2 입니다.");
+            }
+
+            String email = form.get(EMAIL_INDEX);
+            String name = form.get(NAME_INDEX);
+
+            if (isEmailWrongSize(email)){
+                throw new IllegalArgumentException("이메일 길이는 11이상 20 미만입니다.");
+            }
+
+            if (isWrongDomain(email)){
+                throw new IllegalArgumentException("올바르지 않은 이메일 도메인입니다.");
+            }
+
+            if (isNameWrongSize(name)){
+                throw new IllegalArgumentException("닉네임은 1자 이상 20자 미만입니다.");
+            }
+        }
+    }
+
     private static boolean isFormWrongSize(List<String> form){
         return form.size() != EACH_FORM_SIZE;
     }
