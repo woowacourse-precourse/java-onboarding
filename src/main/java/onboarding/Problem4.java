07c6ae96133139e6a0bad3dab1c648eb2c9953af
jwjin95 <jwjin95@yonsei.ac.kr>
2022-10-31 01:01:18 +0900

    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String upper_dic = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        String lower_dic = "zyxwvutsrqponmlkjihgfedcba";
+
+        for (int i=0 ; i<word.length() ; i++) {
+            char ch = word.charAt(i);
+            if (65 <= (int)ch && (int)ch <= 90) {
+                answer += upper_dic.charAt((int)ch-65);
+            } else if (97 <= (int)ch && (int)ch <= 122) {
+                answer += lower_dic.charAt((int)ch-97);
+            } else {
+                answer += ch;
+            }
+
+        }
+
         return answer;
     }
 }
