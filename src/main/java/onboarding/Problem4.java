1474fbd487ff7b30e7002b4331c2e479ddbc2ce1
yuntaeLee <leeyt1201@gmail.com>
2022-10-30 22:38:32 +0900

    feat(Problem4): solution 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            int c = word.charAt(i);
+            int n = 32; // ASCII 공백 ' '
+
+            if (Character.isUpperCase(c)) n = 64 + 26 - ((int)c - 65);
+            else if (Character.isLowerCase(c)) n = 96 + 26 - ((int)c - 97);
+
+            answer.append((char) n);
+        }
+
+        return answer.toString();
     }
 }
