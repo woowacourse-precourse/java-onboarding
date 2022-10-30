71b8eb84babcbe5de66b7c4c314f3f3ddace4c6f
JJongmen <pjy90123@gmail.com>
2022-10-30 21:18:08 +0900

    refactor(Problem4): 메서드 이름 변경 및 early return 적용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,18 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        return getOppositeWord(word);
+    }
+
+    private static String getOppositeWord(String word) {
         StringBuilder oppositeWord = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
-            oppositeWord.append(translateAlphabet(word.charAt(i)));
+            oppositeWord.append(translateCharacter(word.charAt(i)));
         }
         return oppositeWord.toString();
     }
 
-    private static char translateAlphabet(char ch) {
-        if ('a' <= ch && ch <= 'z') {
-            ch = (char) ('z' - (ch - 'a'));
-        } else if ('A' <= ch && ch <= 'Z') {
-            ch = (char) ('Z' - (ch - 'A'));
+    private static char translateCharacter(char ch) {
+        if (Character.isLowerCase(ch)) {
+            return (char) ('z' - (ch - 'a'));
+        }
+        if (Character.isUpperCase(ch)) {
+            return (char) ('Z' - (ch - 'A'));
         }
         return ch;
     }
