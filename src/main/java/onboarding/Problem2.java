7a6149552112b7b2269ceea8c82575a8f0b87d88
poter0501 <poter0501@gmail.com>
2022-10-30 21:32:22 +0900

    feat: findSameCharsIdx 함수 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,14 +1,25 @@
 package onboarding;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
+        
+        HashSet<Integer> temp = findSameCharsIdx(cryptogram);
         return answer;
 
+    }
+    private static HashSet<Integer> findSameCharsIdx(String cryptogram){
+        HashSet<Integer> idxList= new HashSet<Integer>();
+
+        for (int i=0;i<cryptogram.length()-1;i++){
+            if (cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+                idxList.add(i);
+                idxList.add(i+1);
+            }
+        }
+
+        return idxList;
     }
 }
