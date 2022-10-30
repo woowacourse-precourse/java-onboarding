aa9b07f008437dcf1577821b49744869aab98c7c
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:19:02 +0900

    [문제3] 숫자마다 3,6,9를 포함 확인하는 로직 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,14 @@ public class Problem3 {
         if(number<1 || number>10000)
             return -1;
 
-        
+        for(int i =1; i<number+1;i++){
+            int tmp = i;
+            while (tmp>0) {
+                if ((tmp % 10) == 3 || (tmp % 10) == 6 || (tmp % 10) == 9)
+                    answer++;
+                tmp = tmp/10;
+            }
+        }
         return answer;
     }
 }
