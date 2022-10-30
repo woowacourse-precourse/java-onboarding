145d24575ba16f99975e1f8312a11451f7d502fd
Go-Jaecheol <gojaech@naver.com>
2022-10-30 18:31:42 +0900

    refactor: Problem1 - checkException 메소드 추가 및 변수명 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,46 +4,37 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
         int pobiScore;
         int crongScore;
 
-        if (!validateBoundary(pobi) || !validateBoundary(crong)) {
-            return -1;
-        }
-        if (!validateInputFormat(pobi) || !validateInputFormat(crong)) {
-            return -1;
-        }
-        if (!validateContinuous(pobi) || !validateContinuous(crong)) {
+        if (checkException(pobi) || checkException(crong)) {
             return -1;
         }
 
         pobiScore = Math.max(compareMaxNum(pobi.get(0)), compareMaxNum(pobi.get(1)));
         crongScore = Math.max(compareMaxNum(crong.get(0)), compareMaxNum(crong.get(1)));
 
-        answer = getResult(pobiScore, crongScore);
-        return answer;
+        return getResult(pobiScore, crongScore);
     }
 
-    private static boolean validateInputFormat(List<Integer> numList) {
-        Integer leftNum = numList.get(0);
-        Integer rightNum = numList.get(1);
+    private static boolean checkException(List<Integer> numList) {
+        int leftNum = numList.get(0);
+        int rightNum = numList.get(1);
+        return !validateBoundary(leftNum, rightNum) || !validateInputFormat(leftNum, rightNum) || !validateContinuous(leftNum, rightNum);
+    }
+    private static boolean validateInputFormat(int leftNum, int rightNum) {
         return (leftNum % 2) == 1 && (rightNum % 2) == 0;
     }
 
-    private static boolean validateContinuous(List<Integer> numList) {
-        Integer leftNum = numList.get(0);
-        Integer rightNum = numList.get(1);
+    private static boolean validateContinuous(int leftNum, int rightNum) {
         return (rightNum - leftNum) == 1;
     }
 
-    private static boolean validateBoundary(List<Integer> numList) {
-        Integer leftNum = numList.get(0);
-        Integer rightNum = numList.get(1);
+    private static boolean validateBoundary(int leftNum, int rightNum) {
         return leftNum >= 1 && leftNum <= 400 && rightNum >= 1 && rightNum <= 400;
     }
 
-    private static int addDigit(int num) {
+    private static int addAllDigit(int num) {
         int sum = 0;
         while (num != 0) {
             sum += (num % 10);
@@ -52,17 +43,17 @@ class Problem1 {
         return sum;
     }
 
-    private static int multiplyDigit(int num) {
-        int temp = 1;
+    private static int multiplyAllDigit(int num) {
+        int sum = 1;
         while (num != 0) {
-            temp *= (num % 10);
+            sum *= (num % 10);
             num /= 10;
         }
-        return temp;
+        return sum;
     }
 
     private static int compareMaxNum(int num) {
-        return Math.max(addDigit(num), multiplyDigit(num));
+        return Math.max(addAllDigit(num), multiplyAllDigit(num));
     }
 
     private static int getResult(int pobiScore, int crongScore) {
