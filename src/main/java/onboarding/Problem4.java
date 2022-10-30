1331ebad27bd8d518c167f0677051b8c0e12829a
sawol <sawol634@gmail.com>
2022-10-30 21:04:14 +0900

    Solved Problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char target = word.charAt(i);
+            if (target >= 65 && target < 91) {
+                answer.append((char) (90 - (target - 65)));
+            } else if (target >= 97 && target < 123) {
+                answer.append((char) (122 - (target - 97)));
+            } else {
+                answer.append(target);
+            }
+        }
+        return answer.toString();
     }
 }
