674c4c2c96536708c51ee37dc1ef31c144cc1b23
jasoncho1021 <jhcho1021@gmail.com>
2022-10-30 18:51:54 +0900

    feat(problem6): 순서에 맞추어 연속된 두 글자가 포함되어 있으면 중복으로 간주

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,56 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    private static final int WORD_LENGTH = 2;
+    private static Map<String, Integer> duplicatedCountPerWord = new HashMap<>();
+    private static Set<String> emailsOfCrewWhoHasDuplicatedNickName = new HashSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
+        for (List<String> crew : forms) {
+            makeLengthTwoWordsBag(crew.get(1));
+        }
+
+        for (List<String> crew : forms) {
+            findCrewWhoHasDuplicatedNickName(crew);
+        }
+
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static void makeLengthTwoWordsBag(String nickName) {
+        int nickNameLength = nickName.length();
+        nickNameLength--;
+        for (int i = 0; i < nickNameLength; i++) {
+            savePartialWordsToMap(nickName.substring(i, i + WORD_LENGTH));
+        }
+    }
+
+    private static void savePartialWordsToMap(String partialWord) {
+        if (!duplicatedCountPerWord.containsKey(partialWord)) {
+            duplicatedCountPerWord.put(partialWord, 0);
+        }
+        Integer duplicatedCount = duplicatedCountPerWord.get(partialWord);
+        duplicatedCountPerWord.put(partialWord, duplicatedCount + 1);
+    }
+
+    private static void findCrewWhoHasDuplicatedNickName(List<String> crew) {
+        String email = crew.get(0);
+        String nickName = crew.get(1);
+        int nickNameLength = nickName.length();
+        nickNameLength--;
+        for (int i = 0; i < nickNameLength; i++) {
+            saveEmailOfDuplicatedNickName(nickName.substring(i, i + WORD_LENGTH), email);
+        }
+    }
+
+    private static void saveEmailOfDuplicatedNickName(String partialWord, String email) {
+        Integer duplicatedCount = duplicatedCountPerWord.get(partialWord);
+        if (duplicatedCount > 1) {
+            emailsOfCrewWhoHasDuplicatedNickName.add(email);
+        }
     }
 }
