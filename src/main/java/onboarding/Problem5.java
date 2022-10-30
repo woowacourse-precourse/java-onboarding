e09cc57a15ccaa760224a78fe5ac442e21de1e37
parksey <parkseyeon99@naver.com>
2022-10-30 15:18:18 +0900

    feat(problem 5): 기능 요구사항 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,7 +3,24 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Problem 5
+ *
+ * 기능 요구 사항
+ * 1. 계좌에 들어있는 돈을 출금
+ * 2. 지갑이 최대한 가볍게하기 위해 큰 금액의 화폐위주로 받는다.
+ * 3. money가 주어지면, 각 화페 몇 개로 변환되는지 완성
+ * 4. 단. 금액이 큰 순서대로 리스트에 담는다.
+ *
+ * 제한 사항
+ * 1. money는 1~1,000,000 이하인 자연수
+ */
 public class Problem5 {
+    /**
+     *
+     * @param money
+     * @return 화폐의 개수 리스트
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
