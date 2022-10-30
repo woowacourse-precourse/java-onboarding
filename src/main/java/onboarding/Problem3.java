2cb678301f5fddca8e3fbf1775ee5d78103f91e3
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:49:24 +0900

    feat : [Problem3] 숫자가 3,6,9면 1을 리턴하는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,17 @@ package onboarding;
  * 2.각 자릿수를 확인하기 위한 함수
  */
 public class Problem3 {
+    /**
+     * 1.숫자가 3,6,9면 1을 리턴하는 함수
+     */
+    public static int clap (int c){
+        int count = 0;
+        if(c == 3 || c == 6 || c == 9){
+            count = 1;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
