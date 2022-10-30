4de33c6960e441dc7c555e7af896f42c75d0319d
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-10-30 14:49:25 +0900

    feat: 별명에 한글이외에 문자가 오면 예외를 던지는 테스트 코드 작성. 커스텀  NickName 예외를 만듦. 테스트 통과되게 구현.

--- src/main/java/onboarding/problem6/NickName.java
+++ src/main/java/onboarding/problem6/NickName.java
@@ -1,12 +1,21 @@
 package onboarding.problem6;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class NickName {
+    private static final Pattern korean = Pattern.compile("^[ㄱ-ㅎ|가-힣]$");
     private final String nickName;
 
     public NickName(String nickName) {
+        checkKorean(nickName);
         this.nickName = nickName;
+    }
+
+    private static void checkKorean(String nickName) {
+        if (!korean.matcher(nickName).matches()){
+            throw new NickNameException("한글만 쓸 수 있습니다.");
+        }
     }
 
     public String getNickName() {
--- src/test/java/onboarding/problem6/NickNameTest.java
+++ src/test/java/onboarding/problem6/NickNameTest.java
@@ -1,17 +1,23 @@
 package onboarding.problem6;
 
-import onboarding.Problem6;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NickNameTest {
     @Test
-    void case1() {
+    void getNickName() {
         String testNickName = "이제엠";
         NickName result = new NickName("이제엠");
         assertThat(testNickName).isEqualTo(result.getNickName());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"apple", "한글이야?", "1234", "#!#!#"})
+    void if_not_korean_nick(String nickName) {
+        assertThrows(NickNameException.class, () -> new NickName(nickName));
     }
 }
