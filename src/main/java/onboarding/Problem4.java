b5e826b408b4dd8c8ee62115972caded3b9cabde
seonpilKim <ksp970306@gmail.com>
2022-10-31 05:22:46 +0900

    Feat: 알파벳 대/소문자 여부 확인 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+
+	public static String solution(String word) {
+		return Converter.convert(word);
+	}
+
+	public static class Converter {
+
+		public static String convert(String word) {
+			return null;
+		}
+
+		public static boolean isLowerCaseAlphabet(char cur) {
+			return 'a' <= cur && cur <= 'z';
+		}
+
+		public static boolean isUpperCaseAlphabet(char cur) {
+			return 'A' <= cur && cur <= 'Z';
+		}
+
+	}
+
 }
