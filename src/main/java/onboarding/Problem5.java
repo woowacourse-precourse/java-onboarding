7eb1780f508e3b7c43a79ff1b2575102ff43ec82
Eleisia <blackcube222@gmail.com>
2022-10-30 14:27:56 +0900

    5번문제 기능구현사항 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+* 기능구현사항
+* 1. 화폐단위 갯수를 반환하는 기능
+* */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
