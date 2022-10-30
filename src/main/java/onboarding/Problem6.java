498bc63163ec5aa481752c828bc4c7340e922e52
gnoesnooj <306yyy@naver.com>
2022-10-30 10:10:58 +0800

    refactor : PROBLEM6 예외 상황에 따른 코드 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,29 +5,53 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) { List<String> tmp = new ArrayList<>();
+    public static List<String> solution(List<List<String>> forms) {
+        List<String> tmp = new ArrayList<>();
+
+        if (!isOutOfRange(forms)) {
+            return new ArrayList<>();
+        }
+
         for (int i = 0; i < forms.size() - 1; i++) {
             for (int j = i + 1; j < forms.size(); j++) {
                 for (int k = 0; k < forms.get(i).get(1).length() - 1; k++) {
-                    if (forms.get(j).get(1).contains(forms.get(i).get(1).substring(k, k + 2))) { //(check(forms.get(j).get(1), forms.get(i).get(1).substring(k, k + 2)))
-                        tmp.add(forms.get(j).get(0));
-                        tmp.add(forms.get(i).get(0));
+                    if (isContain(forms.get(j).get(1), forms.get(i).get(1).substring(k, k + 2)) && isNickNameKorean(forms.get(i).get(1))) {
+                        addEmail(tmp, forms.get(j).get(0));// tmp.add(forms.get(j).get(0));
+                        addEmail(tmp, forms.get(i).get(0)); // tmp.add(forms.get(i).get(0));
                     }
                 }
             }
         }
 
-        Set<String> set = new HashSet<>(tmp);
-
-        List<String> answer = new ArrayList<>(set);
+        List<String> answer = deleteDuplicatedValue(tmp);
 
         Collections.sort(answer);
 
         return answer;
     }
 
+    private static List<String> deleteDuplicatedValue(List<String> tmp) {
+        Set<String> set = new HashSet<>(tmp);
+
+        return new ArrayList<>(set);
+    }
+
     private static boolean isOutOfRange(List<List<String>> list) {
         return list.size() >= 1 && list.size() <= 10000;
+    }
+
+    private static boolean isNickNameKorean(String nickName) {
+        return nickName.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*");
+    }
+
+    private static boolean isContain(String a, String b) {
+        return a.contains(b);
+    }
+
+    private static void addEmail(List<String> list, String email) {
+        if (isCorrectEmailDomain(email) && isCorrectEmailForm(email) && isCorrectEmailLength(email)) {
+            list.add(email);
+        }
     }
 
     private static boolean isCorrectEmailLength(String email) {
@@ -45,7 +69,4 @@ public class Problem6 {
         return email.substring(email.length() - 9).contains("email.com");
     }
 
-    private static boolean isNickNameKorean(String nickName){
-        return nickName.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*");
-    }
 }
