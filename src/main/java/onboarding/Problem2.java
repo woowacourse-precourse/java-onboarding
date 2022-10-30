2fb32e32e26aa75a22fa75cefca63717d549e1a2
lsw725 <725lsw@hanmail.net>
2022-10-31 02:58:42 +0900

    solve Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = decode(cryptogram);
         return answer;
+    }
+
+    public static String decode(String cryptogram) {
+        int i = 0;
+        int len = cryptogram.length();
+        StringBuffer sb = new StringBuffer(len);
+        int sb_idx = -1;
+
+        while (i < len) {
+            char ch = cryptogram.charAt(i);
+            if (sb_idx != -1 && sb.charAt(sb_idx) == ch) {
+                sb.deleteCharAt(sb_idx);
+                --sb_idx;
+            }
+            else {
+                sb.append(ch);
+                ++sb_idx;
+            }
+            ++i;
+        }
+        return sb.toString();
     }
 }
