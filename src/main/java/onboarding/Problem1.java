24b819ff169ca4f5cf566e668c4d1718667d9079
hum02 <ehdgma02@gmail.com>
2022-10-30 16:18:14 +0900

    feat:1번 상수정의,예외검사 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,21 +5,46 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        final int exception = -1;
+        final int pobiWin = 1;
+        final int crongWin = 2;
+        final int draw = 0;
         final int pageMax = 400;
         int pobiNum = 0;
         int crongNum = 0;
-        //임의로 페이지 선택
-        //예외 검사 비연속,마지막이나 첫페이지, 왼쪽이 더큼, 홀짝 안맞음 ,400넘어감
 
-        for(Integer page:pobi){
+        //예외 페이지 연속성, 오른쪽이 더 큰수 검사
+        if(pobi.get(1)-pobi.get(0) != 1 || crong.get(1)-crong.get(0) != 1) {
+            return exception;
+        }
+
+        for(int i=0; i<pobi.size(); i++) {
+            Integer page = pobi.get(i);
+            //예외 시작면이나 마지막면,400페이지 미만 검사
+            if(page <= 1 || page >= pageMax) {
+                return exception;
+            }
+            //예외 왼쪽면 홀수,오른쪽 면 짝수 검사
+            if(page%2 != (i+1)%2) {
+                return exception;
+            }
             //pobi 왼쪽 오른쪽 중 큰 것 결정
             int result = getPageBiggest(page);
-            if(result > pobiNum){
+            if(result > pobiNum) {
                 pobiNum = result;
             }
         }
 
-        for(Integer page:crong){
+        for(int i=0; i<crong.size(); i++) {
+            Integer page = crong.get(i);
+            //예외 시작면이나 마지막면,400페이지 미만 검사
+            if(page <= 1 || page >= 400) {
+                return exception;
+            }
+            //예외 왼쪽면 홀수,오른쪽 면 짝수 검사
+            if(page%2 != (i+1)%2) {
+                return exception;
+            }
             //crong 왼쪽 오른쪽 중 큰 것 결정
             int result = getPageBiggest(page);
             if(result > crongNum){
@@ -27,13 +52,13 @@ class Problem1 {
             }
         }
 
-        //점수 비교해 승자 결정 1,2,0,-1
+        //점수 비교해 승자 결정 1,2,0
         if(pobiNum>crongNum){
-            answer = 1;
+            answer = pobiWin;
         } else if (pobiNum < crongNum) {
-            answer = 2;
+            answer = crongWin;
         } else {
-            answer = 0;
+            answer = draw;
         }
 
         return answer;
