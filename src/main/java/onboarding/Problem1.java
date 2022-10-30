8ca41d9d8e581ff6beb1c26e92d55a631d5090e5
이지혁 <olzlgur@naver.com>
2022-10-31 02:30:22 +0900

    페이지 검증 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private final int number = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+    public boolean validatePageNumber(List<Integer> player) {
+        int left = 0;
+        int right = 1;
+
+        if (player.get(left) - player.get(right) != 1) {
+            return false;
+        }
+        if (player.get(left) < 0 || player.get(1) > 400 ){
+            return false;
+        }
+        if (player.get(left) % 2 != 1 || player.get(right) != 0) {
+            return false;
+        }
+        return true;
     }
 }
