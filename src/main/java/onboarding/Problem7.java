633e2abd097f509e80d46d238b3dfe4fa3833b97
syhan7516 <syhan7516@naver.com>
2022-10-31 02:25:06 +0900

    Problem7 - 2번 기능 : 함께 아는 친구 찾기 & 결과 확인

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -21,17 +21,47 @@ public class Problem7 {
         }
     }
 
+    // 2-1. 함께 아는 친구 찾기
+    static void getCheckFriToFri(List<String> friFriendList, String user, List<String> myFriendsList, List<List<String>> friends)
+    {
+        for(int idx=0; idx<myFriendsList.size(); idx++)
+        {
+            String friendName = myFriendsList.get(idx);
+
+            for(int fIdx=0; fIdx<friends.size(); fIdx++)
+            {
+                List<String> shipList = friends.get(fIdx);
+
+                // 관계 정보에서 모두가 내 친구 X, 관계 정보에서 내가 포함 X
+                if((!shipList.contains(user)) && shipList.contains(friendName))
+                {
+                    // 추천 친구 확인 & 친구의 친구 리스트에 저장
+                    if((shipList.indexOf(friendName) == 0) && (!myFriendsList.contains((shipList.get(1)))))
+                        friFriendList.add(shipList.get(1));
+                    else if((shipList.indexOf(friendName) == 1) && (!myFriendsList.contains((shipList.get(0)))))
+                        friFriendList.add(shipList.get(0));
+                }
+            }
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         // 나의 친구 리스트
         List<String> myFriendsList = new ArrayList<>();
 
+        // 친구의 친구 리스트
+        List<String> friFriendList = new ArrayList<>();
+
         // 나의 친구 확인
         getCheckFriend(myFriendsList,user,friends);
 
-        // 나의 친구 관계 결과 확인
-        System.out.println(myFriendsList);
+        // 친구의 친구 확인 & 저장
+        getCheckFriToFri(friFriendList,user,myFriendsList,friends);
+
+        // 친구의 친구 리스트 결과 확인
+        System.out.println(friFriendList);
 
         return answer;
     }
@@ -41,6 +71,7 @@ public class Problem7 {
 // 1. 나와의 친구 관계 파악하기
 //  : 나의 친구 리스트 생성 후 친구 저장
 // 2. 모든 친구 관계에서 함께 아는 친구 점수 매기기
+//  : 친구의 친구 리스트를 생성 후 저장
 //  : [명단,점수] 리스트 형식 생성 후 관계 점수 저장
 // 3. 방문자 점수 매기기
 //  : [명단,점수] 리스트에 방문 점수 매기기
