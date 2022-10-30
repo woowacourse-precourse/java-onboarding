5d4f17aacafc6570f4b6771b7d7097e0d42a4315
jhsseonn <hosu0125@gmail.com>
2022-10-30 22:18:15 +0900

    feat: (Problem4) 문자열을 띄어쓰기 포함 알파벳 리스트로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        //문자열을 띄어쓰기 포함 알파벳 리스트로 변환
+        List<Character> wordCharList = new ArrayList<>();
+        wordCharList=convert(word);
+
         return answer;
     }
+
+    //문자열을 알파벳 리스트로 변환 함수
+    static List<Character> convert(String string) {
+        List<Character> chars = IntStream.range(0, string.length())
+                .mapToObj(string::charAt)
+                .collect(Collectors.toList());
+
+        return chars;
+    }
+
 }
