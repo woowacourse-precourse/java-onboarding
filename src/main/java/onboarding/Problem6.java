71d8c09cae1d89c2e7bc25c4cc66fe7210b3636f
JH-TT <leejongho9803@gmail.com>
2022-10-31 01:56:24 +0900

    6번 : README 접근방식, 기능목록 작성, 한 단어에 2글자이상 서브단어 찾는 기능 구현.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import com.sun.source.tree.SynchronizedTree;
+
+import java.util.*;
 
 public class Problem6 {
+
+    // 각 서브 단어들을 전부 모은다.
+    static Set<String> findWords(String nick) {
+        Set<String> wordset = new HashSet<>();
+        for(int i=2; i<=nick.length(); i++) {
+            for(int j=0; j<nick.length()-i+1; j++) {
+                String subword = nick.substring(j, j+i);
+                wordset.add(subword);
+            }
+        }
+        return wordset;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>(); // 이메일 담을 리스트
+        Set<String> allWords = new HashSet<>(); // 모든 서브 단어들
+        // 각 단어의 서브 단어들을 모은다.
+        for(List<String> form : forms) {
+            allWords.addAll(findWords(form.get(1)));
+        }
+
+        return new ArrayList<>(answer);
     }
 }
