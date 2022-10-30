a9fe4e09100bf581e26ed758440f750cb1d03d65
김지건 <jigun21@naver.com>
2022-10-30 14:00:03 +0900

    Init: Problem6 초기구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,62 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    public static Map<String,Integer> subNicknameCount = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            List<String> subNicknameList = getSubNicknameList(nickname);
+            addSubNicknameCount(subNicknameList);
+        }
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            if(hasDuplicateSubNickname(nickname)){
+                answer.add(email);
+            }
+        }
+
+        answer.sort((Comparator.naturalOrder()));
         return answer;
+    }
+
+    public static List<String> getSubNicknameList(String nickname){
+        List<String> subNicknameList = new ArrayList<>();
+        int length = nickname.length();
+        for(int left=0;left<length-1;left++){
+            for(int right=left+1;right<length;right++){
+                String subNickname = nickname.substring(left,right+1);
+                subNicknameList.add(subNickname);
+            }
+        }
+        return subNicknameList;
+    }
+
+    public static void addSubNicknameCount(List<String> subNicknameList){
+        for (String subNickname : subNicknameList) {
+            if(subNicknameCount.containsKey(subNickname)){
+                int tempCount = subNicknameCount.get(subNickname);
+                subNicknameCount.put(subNickname, tempCount+1);
+            }else{
+                subNicknameCount.put(subNickname,0);
+            }
+        }
+    }
+
+    public static boolean hasDuplicateSubNickname(String nickname){
+        List<String> subNicknameList = getSubNicknameList(nickname);
+        for (String subNickname : subNicknameList) {
+            if(subNicknameCount.get(subNickname)>1){
+                return true;
+            }
+        }
+        return false;
     }
 }
