18284bcddd6557c9dd83c435aadb63cfee7320c2
Lee Min Woo <lmw7414@naver.com>
2022-10-30 15:53:50 +0900

    problem6 - 이해를 돕기위한 주석 생성
    변수와 기능들 동작의 이해를 돕기 위해 주석으로 설명하였다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -16,28 +16,28 @@ public class Problem6 {
 
         List<Crew> crewList = new ArrayList<>();
         Map<String, Integer> tlMap = new HashMap<>();
-        int[] crewCount;
 
         for (int i = 0; i < forms.size(); i++) {
             String email = forms.get(i).get(0);
             String name = forms.get(i).get(1);
-            int parentKey = -1;
+            int parentKey = -1;  // Crew 객체 사이에서 parentKey가 같을 경우 중복된 글자가 있음
             int key = -1;  // 해시맵 키찾을 때
-            ArrayList<String> twoLetterList = new ArrayList<>();
+
+            ArrayList<String> twoLetterList = new ArrayList<>();  // 두글자씩 저장하기 위한 배열리스트
             if (name.length() < 2) {
                 twoLetterList.add(name.substring(0, 1));
             } else {
                 for (int j = 0; j <= name.length() - 2; j++)
                     twoLetterList.add(name.substring(j, j + 2));
             }
-
-
+            // key값을 찾는다면 twoLetterList에 있는 모든 데이터를 해시맵에 같은 value로 저장
             for (int j = 0; j < twoLetterList.size(); j++) {
                 if (tlMap.containsKey(twoLetterList.get(j))) {
                     key = tlMap.get(twoLetterList.get(j));
                     break;
                 }
             }
+
             for(int j = 0; j< twoLetterList.size(); j++) {
                 if(key != -1)
                     tlMap.put(twoLetterList.get(j), key);
@@ -45,29 +45,26 @@ public class Problem6 {
                     tlMap.put(twoLetterList.get(j), i);
             }
 
-
             parentKey = tlMap.get(twoLetterList.get(0));
             crewList.add(new Crew(parentKey, email, name));
         }
 
-
-        crewCount = new int[crewList.size()];
+        int[] crewCount = new int[crewList.size()];
+        // 중복되는 parentKey 체크
         for (int i = 0; i < crewList.size(); i++)
             crewCount[crewList.get(i).parentKey]++;
 
         List<String> answer = new ArrayList<>();
 
         for (int i = 0; i < crewCount.length; i++) {
-            if (crewCount[i] > 1) {
+            if (crewCount[i] > 1) {  // 하나 이상일 시 중복되는 글자가 있다는 의미
                 for (int j = 0; j < crewList.size(); j++) {
                     if (crewList.get(j).parentKey == i)
                         answer.add(crewList.get(j).email);
                 }
             }
         }
-
         Collections.sort(answer);
-
         return answer;
     }
 
