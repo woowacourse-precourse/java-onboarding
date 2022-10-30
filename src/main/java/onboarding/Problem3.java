0921e84b71a1589565cc418078fe6ac70deed00e
Yejin Shin <shinyejin0212@gmail.com>
2022-10-30 14:25:01 +0900

    docs(problem3) : 구현기능 정리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 public class Problem3 {
-
+    // 1. 재귀로 풀이한다. 숫자를 1씩 증가하며 clap 함수로 박수 횟수를 계산한다.
+    // 2. 특정 숫자를 10으로 나눠 자리수별로 확인한다. 3,6,9에 해당하면 cnt +1.
+        // 만약 해당 숫자가 0이 될 경우(자리수를 다 확인함) 재귀에서 빠져나오면서 횟수를 return한다.
     public static int clap(int num, int cnt){
         if (num == 0)
             return cnt;
