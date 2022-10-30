3d124051c529cb07a8e6fdf74af12cf047723962
yuntaeLee <leeyt1201@gmail.com>
2022-10-30 22:54:14 +0900

    Friend List와 Map 초기화 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,7 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
 
@@ -43,5 +44,35 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friendships, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void init(List<List<String>> friendships, List<String> visitors, Map<String, Integer> numberOfFriends, List<Friend> friends) {
+        int idx = 0;
+        for (List<String> friend : friendships) {
+            String friendA = friend.get(0);
+            String friendB = friend.get(1);
+
+            if (!numberOfFriends.containsKey(friendA)) {
+                numberOfFriends.put(friendA, idx);
+                friends.add(idx++, new Friend(friendA, 0));
+            }
+
+            if (!numberOfFriends.containsKey(friendB)) {
+                numberOfFriends.put(friendB, idx);
+                friends.add(idx++, new Friend(friendB, 0));
+            }
+
+            friends.get(numberOfFriends.get(friendA)).getMyFriends().add(friendB);
+            friends.get(numberOfFriends.get(friendB)).getMyFriends().add(friendA);
+        }
+
+        for (String visitor : visitors) {
+            if (!numberOfFriends.containsKey(visitor)) {
+                numberOfFriends.put(visitor, idx);
+                friends.add(idx++, new Friend(visitor, 0));
+            }
+
+            friends.get(numberOfFriends.get(visitor)).addScore(1);
+        }
     }
 }
