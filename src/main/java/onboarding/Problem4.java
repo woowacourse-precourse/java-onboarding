fe63cd6478d496c38cff60557f0d3964e1eca041
jaewoo <wjdwodn0722@naver.com>
2022-10-30 23:17:21 +0900

    feat : problem4 add number to reverse number method
    - 문자를 청개구리 사전을 참고해 반대로 변환하여 return 하는 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] strarr = word.toCharArray();
+
         return answer;
+    }
+    public static char frogreverse(char c){
+        if(65<=c && c<=90){
+            c = (char)(155 - c);
+        }
+        else if(97<=c && c<=122){
+            c = (char)(219 - c);
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        solution2("abc");
+        System.out.println(123);
     }
 }
