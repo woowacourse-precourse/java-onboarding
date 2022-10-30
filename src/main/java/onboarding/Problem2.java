9866bd48e39ecac23512f1d005bdd0e127eabd8d
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-10-31 00:17:54 +0900

    [pb2] feat: update LinkedList w/o duplicated characters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        LinkedList<Character> characters = new LinkedList<>();
+        characters.add(cryptogram.charAt(0));
+        for (int i = 1; i < cryptogram.length(); i++) {
+            Character prev = cryptogram.charAt(i-1);
+            Character curr = cryptogram.charAt(i);
+            Character last = characters.peekLast();
+            updateLinkedList(prev, curr, last, characters);
+        }
+
+    }
+    private static void updateLinkedList(Character prev,
+                                         Character curr,
+                                         Character last,
+                                         LinkedList<Character> chars) {
+        if (!curr.equals(prev) && !curr.equals(last)) {
+            chars.add(curr);
+        } else if (curr.equals(prev)) {
+            chars.removeLast();
+        }
     }
 }
