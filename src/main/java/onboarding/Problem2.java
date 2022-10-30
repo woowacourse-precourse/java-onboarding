db910e165e6f44425cecc8bd43b57c1cf3a03cf9
H4R1B0 <qkrguswns25@naver.com>
2022-10-30 23:25:38 +0900

    deduplication 메소드의 정규식 간단히 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -19,7 +19,7 @@ public class Problem2 {
 
     //중복 제거 함수
     private static String deduplication(String cryptogram){
-        String changeString = cryptogram.replaceAll("(([a-zA-Z0-9])\\2{1,})","");
+        String changeString = cryptogram.replaceAll("(([a-zA-Z0-9])\\2+)","");
 
         //원래 문자와 같다면 FALSE 문자 반환
         if(cryptogram.equals(changeString)){
