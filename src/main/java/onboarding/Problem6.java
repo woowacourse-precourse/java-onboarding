1402b56c8350c46f6e9a02e67bc9fecc66580709
Qulip <ilkwon.ryu@gmail.com>
2022-10-31 02:28:15 +0900

    feat : Problem6 같은 글자가 연속적으로 포함되는 모든 크루원 확인 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,57 +1,91 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
 
-    private static final String VALIDDOMAIN = "@email.com";
+	private static final String VALID_DOMAIN = "@email.com";
 
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = List.of("answer");
 
-        try{
-            isValidForms(forms);
-        } catch (IllegalStateException formErrorExceptions) {
-            formErrorExceptions.printStackTrace();
-        }
+		try {
+			isValidForms(forms);
+		} catch (IllegalStateException formErrorExceptions) {
+			formErrorExceptions.printStackTrace();
+		}
 
-        return answer;
-    }
+		return answer;
+	}
 
-    public static void isValidForms(List<List<String>> forms) throws IllegalStateException{
-        if(forms.size() < 1 || forms.size() > 10000)
-            throw new IllegalStateException("크루원의 숫자가 올바르지 않습니다.");
-        if(!forms.stream().allMatch(form -> isValidEmailLength(form.get(0))))
-            throw new IllegalStateException("크루원의 이메일의 길이가 올바르지 않습니다");
-        if(!forms.stream().allMatch(form -> isValidEmailDomain(form.get(0))))
-            throw new IllegalStateException("크루원의 이메일 도메인이 올바르지 않습니다.");
-        if(forms.stream().anyMatch(form -> isNicknameContainsEnglish(form.get(1))))
-            throw new IllegalStateException("크루원의 닉네임에 영어가 포함되어있습니다.");
-        if(forms.stream().allMatch(form -> isValidNicknameLength(form.get(1))))
-            throw new IllegalStateException("크루원의 닉네임의 길이가 올바르지 않습니다.");
-    }
+	public static void isValidForms(List<List<String>> forms) throws IllegalStateException {
+		if (forms.size() < 1 || forms.size() > 10000)
+			throw new IllegalStateException("크루원의 숫자가 올바르지 않습니다.");
+		if (!forms.stream().allMatch(form -> isValidEmailLength(form.get(0))))
+			throw new IllegalStateException("크루원의 이메일의 길이가 올바르지 않습니다");
+		if (!forms.stream().allMatch(form -> isValidEmailDomain(form.get(0))))
+			throw new IllegalStateException("크루원의 이메일 도메인이 올바르지 않습니다.");
+		if (forms.stream().anyMatch(form -> isNicknameContainsEnglish(form.get(1))))
+			throw new IllegalStateException("크루원의 닉네임에 영어가 포함되어있습니다.");
+		if (forms.stream().allMatch(form -> isValidNicknameLength(form.get(1))))
+			throw new IllegalStateException("크루원의 닉네임의 길이가 올바르지 않습니다.");
+	}
 
-    public static boolean isValidEmailLength(String email) {
-        if(email.length() >= 11 && email.length() <= 19)
-            return true;
-        return false;
-    }
+	public static boolean isValidEmailLength(String email) {
+		if (email.length() >= 11 && email.length() <= 19)
+			return true;
+		return false;
+	}
 
-    public static boolean isValidEmailDomain(String email) {
-        if(email.contains(VALIDDOMAIN))
-            return true;
-        return false;
-    }
+	public static boolean isValidEmailDomain(String email) {
+		if (email.contains(VALID_DOMAIN))
+			return true;
+		return false;
+	}
 
-    public static boolean isNicknameContainsEnglish(String nickname) {
-        if(nickname.chars().anyMatch(Character::isAlphabetic))
-            return true;
-        return false;
-    }
+	public static boolean isNicknameContainsEnglish(String nickname) {
+		if (nickname.chars().anyMatch(Character::isAlphabetic))
+			return true;
+		return false;
+	}
 
-    public static boolean isValidNicknameLength(String nickname) {
-        if(nickname.length() >0 && nickname.length() <20)
-            return true;
-        return false;
-    }
+	public static boolean isValidNicknameLength(String nickname) {
+		if (nickname.length() > 0 && nickname.length() < 20)
+			return true;
+		return false;
+	}
+
+	public static List<String> getSameNicknameList(List<List<String>> forms) {
+		Map<String, String> nicknameMap = new HashMap<>();
+		List<String> sameNicknameList = new ArrayList<>();
+
+		forms.forEach(form -> {
+			getNicknameCombinations(form).forEach(combination -> {
+				if (nicknameMap.containsKey(combination)) {
+					sameNicknameList.add(form.get(0));
+					sameNicknameList.add(nicknameMap.get(combination));
+				} else {
+					nicknameMap.put(combination, form.get(0));
+				}
+			});
+		});
+
+		return sameNicknameList;
+	}
+
+	public static List<String> getNicknameCombinations(List<String> form) {
+		List<String> combinations = new ArrayList<>();
+		StringBuilder combination;
+
+		for (int i = 0; i < form.get(1).length() - 1; i++) {
+			combination = new StringBuilder();
+			combination.append(form.get(1).charAt(i)).append(form.get(1).charAt(i + 1));
+			combinations.add(combination.toString());
+		}
+
+		return combinations;
+	}
 }
