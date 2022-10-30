5566357cc22005fa3a03c5394c94a6047e8c5ff9
rvlwldev <rvlwlasd77@gmail.com>
2022-10-30 14:07:31 +0900

    problem3 applied conventions

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,6 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-
         for (int i = 1; i <= number; i++) {
             answer += getClaps(i);
         }
@@ -11,13 +10,18 @@ public class Problem3 {
         return answer;
     }
 
-    private static int getClaps(int x) {
-        String str = String.valueOf(x);
-        String fix = str.replace("3", "")
-                .replace("6", "")
-                .replace("9", "");
+    private static int getClaps(int nowNumber) {
+        String strNumber = String.valueOf(nowNumber);
+        String removed = removeClaps(strNumber);
 
-        return str.length() - fix.length();
+        return strNumber.length() - removed.length();
     }
 
+    private static String removeClaps(String strNum) {
+        strNum = strNum.replace("3", "");
+        strNum = strNum.replace("6", "");
+        strNum = strNum.replace("9", "");
+
+        return strNum;
+    }
 }
