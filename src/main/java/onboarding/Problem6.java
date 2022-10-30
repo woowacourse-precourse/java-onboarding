1b6ad67fadee5c4a2818d111c11c74e0f6244b34
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 16:23:30 +0900

    feat(problem6): 사용자 정보 맵에서 중복을 찾아 저장하는 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,15 +1,47 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         HashMap<String, String> infoMap = parseUserInfo(forms);
+
+        Set<String> duplicatedEmailSet = findDuplicatedNames(infoMap);
+
         return answer;
+    }
+
+    private static Set<String> findDuplicatedNames(HashMap<String, String> infoMap) {
+        Set<String> duplicated = new HashSet<>();
+        List<String> names = new ArrayList<>(infoMap.keySet());
+
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            boolean isDuplicate = false;
+
+            if (name.length() < 2) continue;
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                String key = name.substring(j, j+2);
+
+                for (int k = i+1; k < names.size(); k++) {
+                    String compare = names.get(k);
+
+                    if (compare.contains(key)) {
+                        isDuplicate = true;
+                        duplicated.add(infoMap.get(compare));
+                    }
+                }
+
+                if (isDuplicate) duplicated.add(infoMap.get(name));
+            }
+        }
+
+        return duplicated;
     }
 
     private static HashMap<String, String> parseUserInfo(List<List<String>> forms) {
