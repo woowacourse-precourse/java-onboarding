ea68c411ccee7cd3a3ce90c06f9d63aa6d74d6d0
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:39:15 +0900

    feat(Problem3): 결과 반환 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -19,6 +19,29 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+
+        if (checkParam(number))
+            return -1;
+        if (dp == null)
+            initDp();
+        if (number == 10000)
+            return dp[4];
+        for (int i=4; i>0; i--)
+        {
+            int flag = (int) Math.pow(10, i - 1);
+            int cnt = number / flag;
+
+            if (cnt == 0)
+                continue;
+            answer += dp[i - 1] * cnt;
+            if (cnt == 3 || cnt == 6 || cnt == 9) {
+                answer += flag * (cnt / 3 - 1);
+                answer += (number % flag + 1);
+            }
+            else
+                answer += flag * (cnt / 3);
+            number %= flag;
+        }
         return answer;
     }
 }
