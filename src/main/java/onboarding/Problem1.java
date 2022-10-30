fb0240509f4f56b3efd1e4a2cbe833c64ab838f4
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-30 16:29:54 +0900

    Refactor : modify to get one level of indentation per method and remove else statement

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,7 +9,7 @@ class Problem1 {
 
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 
-		int answer = 0;
+		int answer = 2;
 		int pobiMaxValue;
 		int crongMaxValue;
 
@@ -19,40 +19,37 @@ class Problem1 {
 		int crongFirstNumber = crong.get(0);
 		int crongSecondNumber = crong.get(1);
 
+		List<Integer> pobiMaxNumbersList = new ArrayList<>();
+		List<Integer> crongMaxNumbersList = new ArrayList<>();
+
+		pobiMaxNumbersList.add(getMaxNumber(pobiFirstNumber));
+		pobiMaxNumbersList.add(getMaxNumber(pobiSecondNumber));
+		crongMaxNumbersList.add(getMaxNumber(crongFirstNumber));
+		crongMaxNumbersList.add(getMaxNumber(crongSecondNumber));
+
+		pobiMaxValue = Collections.max(pobiMaxNumbersList);
+		crongMaxValue = Collections.max(crongMaxNumbersList);
+
 		if (pobiFirstNumber + 1 != pobiSecondNumber || crongFirstNumber + 1 != crongSecondNumber) {
-			answer = -1;
-		} else {
+			return -1;
+		}
 
-			List pobiMaxNumbersList = new ArrayList();
-			List crongMaxNumbersList = new ArrayList();
+		if (pobiMaxValue == crongMaxValue) {
+			return 0;
+		}
 
-			pobiMaxNumbersList.add(getMaxNumber(pobiFirstNumber));
-			pobiMaxNumbersList.add(getMaxNumber(pobiSecondNumber));
-			crongMaxNumbersList.add(getMaxNumber(crongFirstNumber));
-			crongMaxNumbersList.add(getMaxNumber(crongSecondNumber));
-
-			pobiMaxValue = (int) Collections.max(pobiMaxNumbersList);
-			crongMaxValue = (int) Collections.max(crongMaxNumbersList);
-
-			if (pobiMaxValue == crongMaxValue) {
-				answer = 0;
-			} else if (pobiMaxValue > crongMaxValue) {
-				answer = 1;
-			} else if (pobiMaxValue < crongMaxValue) {
-				answer = 2;
-			}
+		if (pobiMaxValue > crongMaxValue) {
+			return 1;
 		}
 
 		return answer;
 	}
 
 	private static int getNumberLength(int number) {
-		int length = (int) (Math.log10(number) + 1);
-		return length;
+		return (int) (Math.log10(number) + 1);
 	}
 
 	private static int getMaxNumber(int number) {
-		int maxValue = 0;
 
 		if (getNumberLength(number) == 3) {
 			int firstNumberFirstDigit = number / 100;
@@ -61,44 +58,40 @@ class Problem1 {
 
 			int addition = getAddition(firstNumberFirstDigit, firstNumberSecondDigit, firstNumberThirdDigit);
 			int multiplication = getMultiplication(firstNumberFirstDigit, firstNumberSecondDigit, firstNumberThirdDigit);
-			if (addition > multiplication) {
-				maxValue = addition;
-			} else {
-				maxValue = multiplication;
-			}
 
+			return Math.max(addition, multiplication);
+		}
 
-		} else if (getNumberLength(number) == 2) {
+		if (getNumberLength(number) == 2) {
 			int firstNumberFirstDigit = (number % 100) / 10;
 			int firstNumberSecondDigit = (number % 100) % 10;
 
 			int addition = getAddition(firstNumberFirstDigit, firstNumberSecondDigit);
 			int multiplication = getMultiplication(firstNumberFirstDigit, firstNumberSecondDigit);
-			if (addition > multiplication) {
-				maxValue = addition;
-			} else {
-				maxValue = multiplication;
-			}
-		} else {
-			maxValue = number;
+
+			return Math.max(addition, multiplication);
 		}
 
-		return maxValue;
+		return number;
 	}
 
 	public static int getAddition(int... numbers) {
 		int sum = 0;
-		for (int i = 0; i < numbers.length; i++) {
-			sum += numbers[i];
+
+		for (int number : numbers) {
+			sum += number;
 		}
+
 		return sum;
 	}
 
 	public static int getMultiplication(int... numbers) {
 		int multiply = 1;
-		for (int i = 0; i < numbers.length; i++) {
-			multiply = multiply * numbers[i];
+
+		for (int number : numbers) {
+			multiply = multiply * number;
 		}
+
 		return multiply;
 	}
 }
