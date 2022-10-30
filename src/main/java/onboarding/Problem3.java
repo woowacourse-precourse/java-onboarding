7c3adf393f410c0278fa3864dabaf3efef195599
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-31 00:57:46 +0900

    3번 문제 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import onboarding.prob3.domain.ClapCounter;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        ClapCounter clapCounter = new ClapCounter();
+        answer = clapCounter.solve(number);
         return answer;
     }
 }
