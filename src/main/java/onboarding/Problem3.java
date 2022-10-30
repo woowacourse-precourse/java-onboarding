014461001576b21122a21ea889b8a093f563205f
yw824 <yongwoo25@gmail.com>
2022-10-31 04:43:46 +0900

    feat: [Problem3] 기능 요구 사항, 목록 정의
    - solution 함수:
    	- 1부터 입력받은 숫자까지 쳐야 하는 박수의 횟수를 구해서
    	- answer에 더한다 : getClaps(int) 함수를 매 loop마다 호출
    	- 쳐야 하는 총 박수의 횟수를 구해 반환한다.
    - getClaps(int) 함수:
    	- 각 자릿수에 3, 6, 9가 있는 만큼 박수를 쳐야 한다.
    	- 각 숫자마다 쳐야 하는 박수의 횟수를 구해서 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // implement
+        for(int i = 1; i <= number; ++i)
+        {
+            // implement
+            // 각 숫자마다 쳐야 하는 박수의 횟수를 구해서 더하기
+        }
         return answer;
     }
+
+    // 각 자릿수에 3, 6, 9가 있는 만큼 박수를 쳐야 한다.
+    // 각 숫자마다 쳐야 하는 박수의 횟수를 구해서 반환
+    static int getClaps(int input)
+    {
+        // implement
+        return input;
+    }
+
 }
