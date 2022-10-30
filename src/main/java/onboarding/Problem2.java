90c2387f8842d5b05f580cbdc18bcd828537849b
yw824 <yongwoo25@gmail.com>
2022-10-31 04:05:26 +0900

    feat: 순회하여 최종 문자열 반환하는 기능 구현
    - 함수명: String Loop(String input)
    	- 문자열 길이만큼 순회를 돈다.
    	- 마지막 인덱스일 시 비교할 대상이 없으므로 continue
    	- 만약 해당 인덱스에서 다음 인덱스의 것과 같다면
    		- removeRepeated() 함수를 통해 새로운 문자열 받기
    		- 그 앞 인덱스부터 다시 비교해야 하므로
    		- 순회하는 인덱스 앞으로 당기기
    		- for문을 한 번 돌면서 i가 1 추가되므로
    		- 미리 2만큼 빼기 -> 사실상 1 제거
    		- 만약 문자열 길이가 2 이하라면
    		- -1로 설정하여 다음 순회에서 0번째 되도록 설정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -16,8 +16,28 @@ public class Problem2 {
     // 끝까지 도달할 때까지 최대한 한 번만 순회하도록 한다.
     public static String Loop(String input)
     {
-        // implement
-        return input;
+        String str = input;
+        for(int i = 0; i < str.length(); ++i)
+        {
+            // 마지막 원소일 때는 비교할 것이 없으므로 넘기기
+            if( i == str.length() - 1)
+                continue;
+
+            // 만약 해당 인덱스의 문자와 다음 인덱스의 문자가 같으면 -> 제거
+            if(str.charAt(i) == str.charAt(i + 1))
+            {
+                // 제거한 새로운 문자열 반환받기
+                str = removeRepeated(str, i);
+
+                // 그 앞 인덱스부터 다시 비교해야 하므로 순회하는 인덱스 앞으로 당기기
+                // for문을 돌면서 i가 1 추가되므로 미리 2만큼 빼기 -> 사실상 1 제거
+                i -= 2;
+                // 만약 문자열 길이가 2 이하라면 : -1로 설정하여 다음 순회에서 0번째 탐색하도록 설정
+                if(str.length() < 2)
+                    i = -1;
+            }
+        }
+        return str;
     }
 
     // 겹치는 해당 인덱스와 다음 인덱스까지의 내용을 삭제한
