00932cccc7dfcabf92d141f0753df8a11a1423cb
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:25:43 +0900

    doc : 문제5 기능 구현 목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 1. 각 단위로 나누어서 몇 개로 변환되는지 구해준다.
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
