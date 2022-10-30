79fa5773ba329922b31ee71b9af089b2d13fcb0e
asas6978 <asas6978@naver.com>
2022-10-30 23:32:15 +0900

    refactor(problem4): 접근 제어자 private으로 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,7 @@ import static onboarding.problem4.Transformation.getWord;
 import static onboarding.problem4.Validation.isOutOfRange;
 
 public class Problem4 {
-    public static final String INPUT_FORM_ERROR = "문자열 형식이 잘못되었습니다.";
+    private static final String INPUT_FORM_ERROR = "문자열 형식이 잘못되었습니다.";
 
     public static String solution(String word) {
         if(isOutOfRange(word)) {
