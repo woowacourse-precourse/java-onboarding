2f00b39de810f643c4fd34fbcd37f5fdc1edac45
Noah <shgustmd6865@naver.com>
2022-10-30 15:20:16 +0900

    Feat : 닉네임으로부터 패턴을 추출하는 기능 추가
    닉네임으로부터 2글자씩 끊어서 패턴을 추출하는 기능 입니다
    전체 닉네임에 대한 검사도 진행해야 하므로 닉네임 자체를 패턴에 추가 합니다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        Set<String> patternsFromNickName = makePatternsFromNickName(forms);
+
         return answer;
+    }
+
+    public static Set<String> makePatternsFromNickName(List<List<String>> forms) {
+        Set<String> patterns = new HashSet<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String pattern = nickname.substring(i, i + 2);
+                patterns.add(pattern);
+            }
+            patterns.add(nickname);
+        }
+
+        return patterns;
     }
 }
