e8f7dd8fd6b138edc21adace12d7b0c414292420
KYR <freekyr7529@gmail.com>
2022-10-30 12:37:07 +0900

    feature (Problem3) : 3번 문제 기능 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+
+        return countNumberContainThreeOrSixOrNine(number);
+    }
+
+    private static int countNumberContainThreeOrSixOrNine(int number) {
+        int result = 0;
+        String checkStr = "3, 6, 9";
+
+        for (int i = 1; i <= number; i++) {
+            int temp = (int) Arrays.stream(String.valueOf(i).split(""))
+                                .filter(checkStr::contains)
+                                .count();
+            result += temp;
+        }
+
+        return result;
     }
 }
