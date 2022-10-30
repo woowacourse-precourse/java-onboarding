7f53361c5b43a053f15f8e0bf5f178f81ec21b7c
ChanghyunKim <variety82p@gamil.com>
2022-10-30 16:15:49 +0900

    feat : Implement changeMoney

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    static List<Integer> changeMoney(int money) {
+        int currencyLenght = currency.length;
+        List<Integer> account = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        for(int i = 0; i < currencyLenght; i++) {
+            if(money / currency[i] > 0) {
+                account.set(i, money / currency[i]);
+                money = money - (currency[i] *  (money / currency[i]));
+            }
+        }
+
+        return account;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = changeMoney(money);
         return answer;
     }
 }
