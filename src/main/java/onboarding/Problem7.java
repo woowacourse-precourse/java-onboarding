51bf0282328579d5a1b7321c82b3419cb6515352
khj <bbb6254177@gmail.com>
2022-10-30 18:03:21 +0900

    refactor

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,12 +3,11 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> ans = new ArrayList<String>();
-        HashMap<String, List<String>> graph = new HashMap<String, List<String>>();
-        HashMap<String, Integer> score = new LinkedHashMap<String, Integer>();
-        
-        //친구 관게를 hashmap을 이용해 그래프로 나타내기
+    private static HashMap<String, List<String>> graph = new HashMap<String, List<String>>();
+    private static HashMap<String, Integer> score = new LinkedHashMap<String, Integer>();
+
+    ////친구 관게를 hashmap을 이용해 그래프로 나타내는 함수
+    private static void to_graph(List<List<String>> friends){
         for(List<String> friend: friends){
             if (graph.containsKey(friend.get(0))){
                 graph.get(friend.get(0)).add(friend.get(1));
@@ -20,9 +19,10 @@ public class Problem7 {
             }
             else graph.put(friend.get(1), new ArrayList<String>(){{add(friend.get(0));}});
         }
-        System.out.println(graph);
+    }
 
-        //함께 아는 친구에게 점수 부여하기
+    //함께 아는 친구에게 점수를 부여하는 함수
+    private static void friends_score(String user){
         for(String friend: graph.get(user)){
             for(String to_friend: graph.get(friend)){
                 if(to_friend == user) continue;
@@ -31,18 +31,20 @@ public class Problem7 {
                 else score.put(to_friend, 10);
             }
         }
-        System.out.println(score);
+    }
 
-        //방문자에게 점수 부여하기
+    //방문자에게 점수를 부여하는 함수
+    private static void visitor_score(String user, List<String> visitors){
         for(String visitor: visitors){
             if (graph.get(user).indexOf(visitor) != -1) continue;
 
             if(score.containsKey(visitor)) score.put(visitor, score.get(visitor) + 1);
             else score.put(visitor, 1);
         }
-        System.out.println(score);
-        
-        //점수를 기준으로 내림차순으로 정렬, 만약 같은 점수가 존재한다면 키를 기준으로 오름차순
+    }
+
+    //점수를 기준으로 내림차순으로 정렬하고, 만약 점수가 같다면 아이디를 기준으로 오름차순으로 정렬하는 함수
+    private static List<Map.Entry<String, Integer>> sort_score_id(){
         List<Map.Entry<String, Integer>> sortedList = new LinkedList<>(score.entrySet());
         sortedList.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
@@ -52,13 +54,37 @@ public class Problem7 {
                 else return o1.getKey().compareTo(o2.getKey());
             }
         });
-        System.out.println(sortedList);
 
-        //점수가 높은 순서대로 친구 추천 5명 선택하기 
+        return sortedList;
+    }
+
+    //점수가 높은 순서대로 친구 추천 5명 선택하는 함수
+    private static List<String> choose5(List<Map.Entry<String, Integer>> sortedList){
+        List<String> result = new ArrayList<String>();
+
         for(int i = 0; i<sortedList.size(); i++){
             if (i > 4) break;
-            ans.add(sortedList.get(i).getKey());
+            result.add(sortedList.get(i).getKey());
         }
+
+        return result;
+    }
+
+    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        //friends를 graph로 나타낸다.
+        to_graph(friends);
+
+        //함께 아는 친구에게 10점 부여한다.
+        friends_score(user);
+
+        //방문자에게 1점 부여한다.
+        visitor_score(user, visitors);
+
+        //점수와 아이디를 기준으로 정렬한다.
+        List<Map.Entry<String, Integer>> sortedList = sort_score_id();
+
+        //정렬된 것을 기준으로 순위가 높은 5명을 추천한다.
+        List<String> ans = choose5(sortedList);
 
         return ans;
     }
