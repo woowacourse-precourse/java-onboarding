fe894e2d294b0d3d99f6a058f6c7a7df9d538812
zszszszs <so4823@gmail.com>
2022-10-31 00:19:10 +0900

    mission4 clear

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,28 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i = 0; i < word.length(); i++){
+            Character s = changeWord(word.charAt(i));
+            answer += s;
+        }
+
         return answer;
+    }
+    public static Character changeWord(Character c){ // 글자 변환 하는 함수
+        int ret = 0;
+        Character ret_c = null;
+        if(c >= 'A' && c <= 'Z'){
+
+            ret = 'Z' - (c-'A');
+            ret_c = (char) ret;
+        }
+        else if(c >= 'a' && c <= 'z'){
+            ret = 'z' - (c-'a');
+            ret_c = (char) ret;
+        }
+        if(ret_c == null){
+            ret_c = ' ';
+        }
+        return ret_c;
     }
 }
