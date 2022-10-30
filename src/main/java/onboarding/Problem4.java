8ecb02ffb49aef5f98627427fd39a208b57e3d59
siwookim97 <tldn12gh@gmail.com>
2022-10-30 23:53:55 +0900

    feat : problem4) 문자열을 리스트로 변환하는 기능 추가
    문자열을 Character형 List로 변환하는 기능
    List로 변경할 문자열을 입력받아 변환 후 List를 반환한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
+    /**
+     * 문자열을 Character형 List로 변환하는 메서드
+     *
+     * @param word List로 변경할 문자열
+     * @return List로 변환한 문자열
+     */
+    static List<Character> transStringToList(String word) {
+        List<Character> list = new ArrayList<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            list.add(word.charAt(i));
+        }
+
+        return list;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
