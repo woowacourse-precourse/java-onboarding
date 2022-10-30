8a2f2fea07c4f87e04ee2aa69ec9345b5fc5c904
chobobdev <chobobdev@naver.com>
2022-10-31 01:18:30 +0900

    feat: return cryptogram as answer

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,7 +3,7 @@ package onboarding;
 public class Problem2 {
     public static Boolean haveDupe(String cryptogram){
         char[] chars = cryptogram.toCharArray();
-        for(int i =0;i<cryptogram.length()-2;i++){
+        for(int i =0;i<cryptogram.length()-1;i++){
             if(chars[i]==chars[i+1]){
                 return true;
             }
@@ -13,15 +13,14 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         while(haveDupe(cryptogram)){
             char[] chars = cryptogram.toCharArray();
-            for(int i =0;i<cryptogram.length()-2;i++){
+            for(int i =0;i<cryptogram.length()-1;i++){
                 if(chars[i]==chars[i+1]){
                     cryptogram=cryptogram.substring(0,i)+cryptogram.substring(i+2);
-                    System.out.println(cryptogram);
                     break;
                 }
             }
         }
-        String answer = "answer";
+        String answer = cryptogram;
         return answer;
     }
 }
