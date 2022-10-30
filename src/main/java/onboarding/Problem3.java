99a7510c3493df7d43abc56ddf2a408fa8be4845
dongdong-119 <85717356+dongdong-119@users.noreply.github.com>
2022-10-30 20:34:30 +0900

    Problem 3 - 기능목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/**
+ * <기능 목록>
+ *     1. 메소드
+ *          1) clapCount
+ *          : number(int)를 인수로 받았을 때, 1부터 number까지 손뼉쳐야하는 횟수를 계산해 반환
+ *            1부터 number까지의 수를 돌며 수에 포함된 3, 6, 9의 수만큼 손뼉치는 횟수를 더한다.
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
