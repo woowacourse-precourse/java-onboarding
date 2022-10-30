3425d383f08ce8a35fdd8a6230edfe1bbb0b8179
Namyunsuk <yunsuk0328@naver.com>
2022-10-30 23:55:47 +0900

    feat(scoreConstant): score 저장 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+class scoreConstant{
+    static int friendsFriendScore = 10;
+    static int visitorScore = 1;
+}
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
