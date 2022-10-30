f7b0a87c497ac604c38be0d0058c2adce9512d57
¤knj <krkoknj@gmail.com>
2022-10-30 22:37:48 +0900

    for문으로 z ~ a, Z ~ A 까지를 배열에 채운다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem4 {
+
+    private static char[] lowerChars = new char[26];
+    private static char[] upperChars = new char[26];
     public static String solution(String word) {
         String answer = "";
+        filllowerAndUpper(word);
         return answer;
     }
+
+    private static void filllowerAndUpper(String word) {
+
+        for (int i = 90; i >= 65; i--) {
+            upperChars[90 - i] = (char) i;
+        }
+
+        for (int i = 122; i >= 97; i--) {
+            lowerChars[122 - i] = (char) i;
+        }
+    }
+
+
 }
