fd6d9e02d0722186c472f43eace2bdecc18e4f3d
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-30 14:36:37 +0900

    Problem5 기능요구사항 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    // TODO 금액(지폐, 동전)의 개수를 리스트에 담는 메서드
+    public static List<Integer> countMoney(int money) {
+        return null;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
