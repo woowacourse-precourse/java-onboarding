c3c1c3c0d74647996d5a0d1f2ed3ac5f6ca06525
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:25:48 +0900

    feat : [Problem1] 자릿수 별 합과 곱 중 큰 값을 리턴하는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,8 +9,25 @@ import java.util.List;
  * 3.예외 사항 함수
  */
 class Problem1 {
+    /**
+     * 1.자릿수 별 합과 곱 중 큰 값을 리턴하는 함수
+     */
+    public static int cal(int n) {
+        int sum = 0;
+        int mul = 1;
+
+        while(n > 0){
+            sum += n%10;
+            mul *= n%10;
+            n/=10;
+        }
+        return Math.max(sum,mul);
+    }
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+
 }
