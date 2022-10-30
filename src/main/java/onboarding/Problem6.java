212bc9706b6e5131006bacf93bd177c9df2893d3
taeyun-work <wmfajr1357@gmail.com>
2022-10-30 19:00:18 +0900

    [Solved] Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        Set<String> set = new HashSet<String>();
+        for (List<String> item : forms) {
+            for (int j = 0; j < item.get(1).length(); j++) {
+                for (int k = 0; k < item.get(1).length() - 1 - j; k++) {
+                    if (map.containsKey(item.get(1).substring(k, k + j + 2))) {
+                        int cnt = map.get(item.get(1).substring(k, k + j + 2));
+                        map.put(item.get(1).substring(k, k + j + 2), cnt + 1);
+                    } else {
+                        map.put(item.get(1).substring(k, k + j + 2), 1);
+                    }
+                    set.add(item.get(1).substring(k, k + j + 2));
+                }
+            }
+        }
+        List<String> resultlist = new ArrayList<String>();
+        for (String str : map.keySet()) {
+            if (map.get(str) != 1) {
+                for (List<String> item : forms) {
+                    if (item.get(1).contains(str) == true) {
+                        resultlist.add(item.get(0));
+                    }
+                }
+            }
+        }
+        Collections.sort(resultlist);
+        return (new ArrayList<String>(new HashSet(resultlist)));
     }
 }
