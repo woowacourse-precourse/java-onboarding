71cf9ae4281a54d368d84f70d6da3dbcb066d69a
jsyeo97 <jsyeo97@naver.com>
2022-10-30 23:27:16 +0900

    Problem 3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+	public static int checkNum(int n) {
+		int applause = 0;
+		 while(n != 0) {
+			int t = n % 10;
+		 	if(t == 3 || t == 6 || t == 9) applause++;
+		 	n /= 10;
+		 }
+		 return applause;
+	}
+	 
+	
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+        	answer += checkNum(i);
+        }
         return answer;
     }
 }
