445a7b42ef5b29ea260a12f2e7de6e5265e937bc
Jeawoong Kim <kimje1109@gmail.com>
2022-10-30 23:36:47 +0900

    feat: Add getSumOfDigits

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int getSumOfDigits(int number) {
+        int sum = 0;
+        while(number != 0){
+            sum += number%10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
