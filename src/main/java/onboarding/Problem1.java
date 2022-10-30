910e5012b795109b820e0d43e68a951c64e43658
wugawuga <dnr3655@naver.com>
2022-10-31 02:47:30 +0900

    feat: 각 자릿수 더하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,7 +8,26 @@ class Problem1 {
         if (validPages(pobi) || validPages(crong)) {
             return -1;
         }
+        int sumOfPobi = calculatePages(pobi);
+        int sumOfCrong = calculatePages(crong);
         return answer;
+    }
+
+    private static int calculatePages(List<Integer> pages) {
+        int maxOfSum = Integer.MIN_VALUE;
+        for (Integer page : pages) {
+            maxOfSum = Math.max(maxOfSum, sumDigit(page));
+        }
+        return maxOfSum;
+    }
+
+    private static int sumDigit(Integer page) {
+        int sum = 0;
+        while (page != 0) {
+            sum += page % 10;
+            page /= 10;
+        }
+        return sum;
     }
 
     private static boolean validPages(List<Integer> pages) {
