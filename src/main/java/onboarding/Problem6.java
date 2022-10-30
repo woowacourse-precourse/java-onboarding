fdf0596e0f3fee56a461c40f656e595d55cb2acc
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-30 17:12:17 +0900

    Refactor : extract functions into separate methods

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,29 +11,37 @@ public class Problem6 {
 		List<String> selectedList = new ArrayList<>();
 
 		for (int i = 0; i < forms.size(); i++) {
-
-			List<String> elementByTwoDimension = forms.get(i);
-
-			for (int j = 0; j < forms.size(); j++) {
-				if (j != i) {
-					List<String> ElementByOneDimension = forms.get(j);
-
-					String setName = elementByTwoDimension.get(1);
-					String setEmail = elementByTwoDimension.get(0);
-					String compareName = ElementByOneDimension.get(1);
-
-
-					if (checkConsecutiveCharacter(setName, compareName)) {
-						selectedList.add(setEmail);
-					}
-				}
-			}
+			checkElementAtOneDimensionalLevel(forms, selectedList, i);
 		}
 
 		List<String> answer = removeDuplication(selectedList);
 		answer.sort(Comparator.naturalOrder());
 
 		return answer;
+	}
+
+	private static void checkElementAtOneDimensionalLevel(List<List<String>> forms, List<String> selectedList, int i) {
+		List<String> elementByTwoDimension = forms.get(i);
+
+		for (int j = 0; j < forms.size(); j++) {
+
+			if (j != i) {
+				List<String> ElementByOneDimension = forms.get(j);
+				addSelectedToList(selectedList, elementByTwoDimension, ElementByOneDimension);
+			}
+
+		}
+	}
+
+	private static void addSelectedToList(List<String> selectedList, List<String> elementByTwoDimension, List<String> ElementByOneDimension) {
+		String setName = elementByTwoDimension.get(1);
+		String setEmail = elementByTwoDimension.get(0);
+		String compareName = ElementByOneDimension.get(1);
+
+		if (checkConsecutiveCharacter(setName, compareName)) {
+			selectedList.add(setEmail);
+		}
+
 	}
 
 	private static List<String> removeDuplication(List<String> selectedList) {
