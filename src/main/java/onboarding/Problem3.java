8f92e19846ab88178e3ac4ecbd84f29c5c663a9a
HamSeungChan <head0618@naver.com>
2022-10-30 21:47:41 +0900

    feat(Problem3): 1부터 number까지 숫자가 증가하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,24 +1,29 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Problem3 {
 
     private static final int NUMBER_OF_DIGIT_DIVISION = 10;
-    private static final int MAX_INPUT_NUMBER = 10000 ;
 
     public static int solution(int number) {
-        int answer = number;
+        int answer = countClap(number);
         return answer;
     }
 
+
+    private static int countClap(int number){
+        int countClap = 0;
+        for(int i=1;i<=number;i++){
+            countClap += divideNumber(i);
+        }
+        return countClap;
+    }
 
     private static int divideNumber(int number){
         int clap = 0;
         int dividedNumber = Integer.MIN_VALUE;
         while(number>0){
             dividedNumber = number % NUMBER_OF_DIGIT_DIVISION;
+            System.out.println(dividedNumber);
             if(confirmNumber(dividedNumber))
                 clap++;
             number = number/NUMBER_OF_DIGIT_DIVISION;
