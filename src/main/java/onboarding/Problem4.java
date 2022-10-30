d370d5045079516117c40628a2c5c0c89129561a
sangil <gsl0505@naver.com>
2022-10-30 19:28:46 +0900

    feat(PROBLEM4): 입력받은 word 길이의 범위를 확인해주는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,12 +1,22 @@
 package onboarding;
 
 public class Problem4 {
+	private static final int MIN_INPUT_LENGTH = 1;
+	private static final int MAX_INPUT_LENGTH = 1000;
+
 	public static String solution(String word) {
+		if (!checkInputLength(word)) {
+			throw new IllegalArgumentException();
+		}
 		StringBuilder answer = new StringBuilder();
 		for (char ch : word.toCharArray()) {
 			answer.append(converter(ch));
 		}
 		return answer.toString();
+	}
+
+	private static boolean checkInputLength(String input) {
+		return input.length() >= MIN_INPUT_LENGTH && input.length() <= MAX_INPUT_LENGTH;
 	}
 
 	private static char converter(char alphabet) {
