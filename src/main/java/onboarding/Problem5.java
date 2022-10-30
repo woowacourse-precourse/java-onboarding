be93ff5e814f3065e95d2550624a5aeb987218f4
kkkwp <chxrryda@gmail.com>
2022-10-31 01:57:35 +0900

    feat(change): coin 별 개수를 반환하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return change(money);
+    }
+
+    public static List<Integer> change(int money) {
+        List<Integer> coins = new ArrayList<>(List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1));
+        List<Integer> wallet = new ArrayList<>();
+        for (int coin : coins) {
+            wallet.add(money / coin);
+            money %= coin;
+        }
+        return wallet;
     }
 }
