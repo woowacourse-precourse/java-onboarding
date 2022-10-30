308ab48ba5404c7acc1a69ec1823d38f7195c2d5
Devtist <yt930310@snu.ac.kr>
2022-10-30 18:44:12 +0900

    feat(Problem6): 두 문자열 간에 같은 글자가 연속적으로 포함된 경우가 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,6 +8,14 @@ public class Problem6 {
         return answer;
     }
 
+    private static boolean checkSimilarity(String nickname, String target) {
+        for (int i = 0; i < nickname.length()-1; i++) {
+            if (target.contains(returnBiliteralSubstring(nickname, i))) {
+                return true;
+            }
+        }
+        return false;
+    }
     private static String returnBiliteralSubstring(String nickname, int index) {
         return nickname.substring(index, index+2);
     }
