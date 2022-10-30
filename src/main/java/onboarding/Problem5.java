07225b502c3f6bb202d74838ce2c1866f4bec181
KimSangHyeok <tkdgur0906@naver.com>
2022-10-30 20:43:05 +0900

    Feat(PROBLEM5): 돈을 화폐 단위로 변환 하여 개수를 구하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static Integer[] Money = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    public static List<Integer> withdrawMoney(int money){
+        int moneyCount = 0;
+        List<Integer> moneyCountList = new ArrayList<>();
+        for (Integer moneyValue : Money) {
+            moneyCountList.add(money / moneyValue);
+            money = money % moneyValue;
+        }
+        return moneyCountList;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = withdrawMoney(money);
         return answer;
     }
 }
