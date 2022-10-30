31b5574717980ab52a5faf4d93ecea43e4f3cb8f
jhsseonn <hosu0125@gmail.com>
2022-10-30 20:45:29 +0900

    feat: (Problem3) 숫자 입력받아 배열로 저장 - 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,12 +6,9 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        int[] input_number= Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+        for (int num=1; num<=number; num++) {
+            int[] inputNum = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
 
-        for (int num : input_number) {
-            if (num==3 || num==6 || num==9) {
-                answer+=1;
-            }
         }
 
         return answer;
