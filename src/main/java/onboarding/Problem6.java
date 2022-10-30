237c390c062fda570394d667fec2199788d8dec5
CordHouse <qkqktl5310@gmail.com>
2022-10-30 17:03:13 +0900

    docs : Problem6 기능 목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,12 @@
 package onboarding;
 
 import java.util.List;
-
+/*
+    기능 목록
+    1. 모든 닉네임에 대해 연속적인 두 글자 조합을 찾는다.
+    2. HashMap을 통해 key : 연속된 두 글자, value : 중복 개수
+    3. value가 1초과인 key를 기준으로 메일주소를 찾는다.
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
