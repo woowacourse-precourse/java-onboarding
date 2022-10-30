5e42bb0637e6cf1c08173f12fc4af426639b151d
byeongsoon <byeongsoon94@gmail.com>
2022-10-30 16:57:36 +0900

    FEAT: Problem4, 청개구리 사전 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,29 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int operation = 0;
+        char[] wordChars = word.toCharArray();
+
+        for (char ch: wordChars) {
+            if (ch == 32) {
+                answer += " ";
+            }
+
+            if (ch >= 65 && ch <= 97) {
+                operation = (65 - (int) ch) + (90 - (int) ch);
+                ch += operation;
+
+                answer += ch;
+            }
+
+            if (ch >= 97 && ch <= 122) {
+                operation = (97 - (int) ch) + (122 - (int) ch);
+                ch += operation;
+
+                answer += ch;
+            }
+        }
+
         return answer;
     }
 }
