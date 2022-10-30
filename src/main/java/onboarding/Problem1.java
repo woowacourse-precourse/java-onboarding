ccf0f6e45673b109472f4f6be9f38d351ec3cc1a
cgk95 <kjkj5868@gmail.com>
2022-10-30 17:35:07 +0900

    P1자릿수더하는기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,13 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        myfunc01 mf= new myfunc01(); // 메소드 임포트
         return answer;
+    }
+    public static class myfunc01 {
+        // 분리가 된 자릿수를 더하는 기능
+        int sum(List<Integer> nums) {
+            return (int) nums.stream().reduce((x, y) -> x + y).stream().toArray()[0];
+        }
     }
 }
