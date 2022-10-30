7e2a99ae1da8ed4a1d864c24709cbcae6e39a580
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-31 00:57:59 +0900

    Add initMoneyList function

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,10 +12,25 @@ public class Problem5 {
 
     // 결과값을 저장할 배열을 초기화하는 함수
     public static ArrayList<Integer> initResultList() {
-        ArrayList<Integer> temp = new ArrayList<Integer>(9);
+        ArrayList<Integer> resultList = new ArrayList<Integer>(9);
         for (int i = 1; i <= 9; i++) {
-            temp.add(0);
+            resultList.add(0);
         }
-        return temp;
+        return resultList;
+    }
+
+    // 금액 변환을 위한 배열을 초기화하는 함수
+    public static ArrayList<Integer> initMoneyList() {
+        ArrayList<Integer> MoneyList = new ArrayList<Integer>(9);
+        MoneyList.add(50000);
+        MoneyList.add(10000);
+        MoneyList.add(5000);
+        MoneyList.add(1000);
+        MoneyList.add(500);
+        MoneyList.add(100);
+        MoneyList.add(50);
+        MoneyList.add(10);
+        MoneyList.add(1);
+        return MoneyList;
     }
 }
