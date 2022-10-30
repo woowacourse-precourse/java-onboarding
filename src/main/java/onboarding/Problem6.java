d923b9ddef9aa8c9dc435104f09980fb5449da0f
kimdeokyoung <dudejrss@naver.com>
2022-10-30 20:16:46 +0900

    (wirte) Problem6.makeTokens : 6.1 닉네임 리스트들을 받아 토큰과 닉네임 리스트의 Map을 출력

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,10 +9,22 @@ public class Problem6 {
 
     public static Map<String, LinkedList<String>> makeTokens(List<List<String>> forms){
         Map<String, LinkedList<String>> result = new HashMap<>();
+
+        for (List<String> form : forms){
+            String nickname = form.get(1);
+            for(int i =0; i<nickname.length()-1;i++){
+                String token = nickname.substring(i,i+2);
+                if(!result.keySet().contains(token)){
+                    result.put(token, new LinkedList<>());
+                }
+                result.get(token).add(nickname);
+            }
+        }
         return result;
     }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         return answer;
     }
 }
