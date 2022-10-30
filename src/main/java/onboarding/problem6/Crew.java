858d8fc68565011dcbd00da0877f2e65ab17aeb2
situodys <situodys@gmail.com>
2022-10-30 19:59:15 +0900

    feat(문제6): 닉네임을 길이 2인 연속된 문자열 조각 리스트로 반환

--- src/main/java/onboarding/problem6/Crew.java
+++ src/main/java/onboarding/problem6/Crew.java
@@ -1,6 +1,11 @@
 package onboarding.problem6;
 
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 public class Crew {
+    private static final int SNIPPET_LENGTH=2;
+
     private String nickname;
     private String email;
 
@@ -9,8 +14,19 @@ public class Crew {
         this.email = email;
     }
 
-    public boolean isPossibleToMakeSnippets(){
-        return nickname.length()>1;
+    public boolean isPossibleToMakeSnippets() {
+        return nickname.length() > 1;
     }
 
+    public ArrayList<String> nicknameToSnippets() {
+        int nicknameLen = this.nickname.length();
+        ArrayList<String> snippets = new ArrayList<>();
+
+        IntStream.rangeClosed(0, nicknameLen - SNIPPET_LENGTH)
+                .forEach((startIndex) -> {
+                    snippets.add(nickname.substring(startIndex, startIndex + SNIPPET_LENGTH));
+                });
+
+        return snippets;
+    }
 }
