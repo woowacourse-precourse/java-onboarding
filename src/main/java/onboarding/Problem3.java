2c49250c67ce0d1e05290d93ed20f2d0a32f1da8
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 14:29:42 +0900

    feat(problem3): 입력받은 수에 포함된 369 개수 반환 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    private static int clapCheck(int number) {
+        String strNum = String.valueOf(number);
+        int count = 0;
+
+        for (int i = 0; i < strNum.length(); i++) {
+            if (strNum.charAt(i) == '3' || strNum.charAt(i) == '6' || strNum.charAt(i) == '9'){
+                count += 1;
+            }
+        }
+
+        return count;
     }
 }
