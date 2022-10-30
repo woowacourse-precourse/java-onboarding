46958fef048b15b39f0a19c6fcbec54daf765e23
ddubidubap <park62000@gmail.com>
2022-10-31 03:04:04 +0900

    refactor: Problem 5 clean code

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,40 +1,42 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Problem5 {
     /*
         기능 요구 사항 목록
-        1. 변환할 돈의 개수와 나머지를 반환하는 함수
-        2. solution (변환할 돈을 차례대로 입력)
+        1. 변환할 돈의 개수 반환 함수
+        2. 변환하고 남은 돈 반환 함수
+        3. 변환되는 돈의 개수를 리스트로 반환하는 함수
+        4. solution 함수
     */
 
-    // 변환할 돈의 개수와 나머지 반환 함수
-    private static HashMap<String, Integer> countMoney(int money, int convert) {
-        HashMap<String, Integer> result = new HashMap<>();
-        int count = money / convert;
-        int left = money % convert;
+    static int[] BILLS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
 
-        result.put("count", count);
-        result.put("left", left);
-        return result;
+    // 변환할 돈의 개수 반환 함수
+    private static int countMoney(int money, int bill) {
+        return money / bill;
+    }
+
+    // 변환하고 남은 돈 반환 함수
+    private static int leftMoney(int money, int bill) {
+        return money % bill;
+    }
+
+    // 변환되는 돈의 개수를 리스트로 반환하는 함수
+    private static List<Integer> changeMoney(int money) {
+        List<Integer> changeCount = new ArrayList<>();
+        for (int bill : BILLS) {
+            int count = countMoney(money, bill);
+            money = leftMoney(money, bill);
+
+            changeCount.add(count);
+        }
+        return changeCount;
     }
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-        int left = money;
-        int[] convert = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
-
-        for (int i : convert) {
-            HashMap<String, Integer> result = countMoney(left, i);
-            money = result.get("count");
-            left = result.get("left");
-
-            answer.add(money);
-        }
-
-        return answer;
+        return changeMoney(money);
     }
 }
