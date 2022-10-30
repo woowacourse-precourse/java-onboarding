32baf4a09bdb1f55e93bdc8b0852dd918d9e163a
HiiWee <wpdlzhf159@gmail.com>
2022-10-31 03:18:47 +0900

    Feat problem6: 닉네임을 연속 2글자로 자른 것을담기 위한 Map 정의

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     static final int EMAIL = 0;
     static final int NICKNAME = 1;
+    static HashMap<String, Integer> twoLetterCountMap = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        return new ArrayList<>();
     }
 }
