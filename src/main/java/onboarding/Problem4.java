ad31713d50aaebb045008e1c4005e9a8a0e72bc5
minji <heunhanireum@gmail.com>
2022-10-30 17:40:40 +0900

    Problem4: 문자열 순서대로 확인하고, 청개구리 사전을 참고해 문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
+//1. 문자열 순서대로 문자 확인
+//2. 청개구리 사전을 참고해 변환
+//3. 변환한 값을 반환
 package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        return answer;
+        StringBuffer sb = new StringBuffer();
+        sb.append(word);
+
+        for (int index = 0 ; index < sb.length() ; index += 1) {
+            int charToInt = sb.charAt(index);
+            if (65 <= charToInt && charToInt <= 90) {
+                charToInt = 90-charToInt+65;
+                sb.setCharAt(index, (char)charToInt);
+            } else if (97 <= charToInt && charToInt <= 122) {
+                charToInt = 122-charToInt+97;
+                sb.setCharAt(index, (char)charToInt);
+            }
+        }
+
     }
 }
