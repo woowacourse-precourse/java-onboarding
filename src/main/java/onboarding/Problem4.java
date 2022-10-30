cf40e0631fc4d5d76e9e672622a53b542e573bfd
KYBee <kyb3634@naver.com>
2022-10-30 14:22:55 +0900

    :art: [Problem 4] Define & Distribute Requirements
    [Problem 4] Define & Distribute Requirements

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,24 @@
 package onboarding;
 
 public class Problem4 {
+    /**
+     * 청개구리가 무엇을 말할지 리턴하는 메서드이다.
+     *
+     * @param : String, 엄마의 말
+     * @return : String, 청개구리의 말
+     */
     public static String solution(String word) {
+        String answer = "";
+        return answer;
+    }
+
+    /**
+     * 엄마의 말을 청개구리의 말로 바꾸는 메서드이다.
+     *
+     * @param : String, 엄마의 말
+     * @return : String, 청개구리의 말
+     */
+    public static String changeWord(String word) {
         String answer = "";
         return answer;
     }
