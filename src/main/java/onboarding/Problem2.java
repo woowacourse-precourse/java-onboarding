9089a450be422a33f06118c3c98701ceaaca15fd
dsaf2007 <dsaf2007@naver.com>
2022-10-30 18:34:47 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem2 {
+
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int check = 0;
+
+        do
+        { 
+            check = 0;
+            for(int i = 0; i < cryptogram.length() -1; i++)
+            {
+                if(cryptogram.charAt(i) == cryptogram.charAt(i+1))
+                {
+                    cryptogram =  cryptogram.substring(0,i) + cryptogram.substring(i+2);
+                    check = 1;
+                }
+                System.out.println(cryptogram);
+            }
+
+        }while(check == 1);
+
+        answer = cryptogram;
         return answer;
     }
 }
