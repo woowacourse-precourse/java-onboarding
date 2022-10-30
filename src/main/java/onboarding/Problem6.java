70d6af01d83641d0bce23fd42c30c6ac01c482f1
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:58:59 +0900

    [문제6] 닉네임 서로 중복되는 2글자가 있는지 체크 로직 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -16,12 +16,31 @@ public class Problem6 {
         for(int i = 0; i<forms.size()-1;i++){
             String email = forms.get(i).get(0);
             //@이후 email.com인지 확인하는 Regex
-
             String nickname = forms.get(i).get(1);
             //nickname 길이 확인 필요
             if (nickname.length()<1 || nickname.length()>=20)
                 return null;
 
+            //리스트에서 현재 닉네임에서의 연속된 2글자와 비교할 닉네임을 가져온다.
+            for (int j=0;j<nickname.length();j++){
+                if (j+2>nickname.length())
+                    break;
+                for (int k=i+1; k<forms.size()-1;k++){
+                    //만약 이미 중복된 닉네임이라면 다음 닉네임으로 이동
+                    if (answer.contains(forms.get(k).get(0)))
+                        break;
+                    //아니라면 현재 닉네임의 연속된 2글자와 중복되는 확인
+                    if (forms.get(k).get(1).contains(nickname.substring(j,j+2))) {
+                        //중복될 경우 현재 닉네임과 비교대상 같이 중복자에 추가
+                        //만약 현재 닉네임 이미 들어가 있다면 비교대상만 추가
+                        if (!answer.contains(forms.get(i).get(0)))
+                            answer.add(forms.get(i).get(0));
+                        answer.add(forms.get(k).get(0));
+                        break;
+                    }
+                }
+            }
+        }
 
         return answer;
     }
