e589eba776a562bf701b87641fdb7c07c4bf0c1e
CMHyuk <alsgur9710@naver.com>
2022-10-30 16:27:53 +0900

    feat: 2글자씩 이름 자른 후 저장하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> answer = new HashSet<>();
+        HashMap<String, String> duplicatedNameMap = new HashMap<>();
+
+        ArrayList<String> slicedNameList;
+        for (int i = 0; i < forms.size(); i++) {
+            String email = forms.get(i).get(0);
+            String name = forms.get(i).get(1);
+            slicedNameList = new ArrayList<>();
+            slicedName(name, slicedNameList);
+        }
+
+        return new ArrayList<>(answer);
+    }
+
+    private static void slicedName(String name, ArrayList<String> slicedNameList) {
+        for (int j = 0; j < name.length() - 1; j++) {
+            String slicedName = name.substring(j, j + 2);
+            slicedNameList.add(slicedName);
+        }
     }
 }
