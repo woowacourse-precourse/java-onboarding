c0dee243bdf720dd36fb1b150066753ceb722557
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 20:46:46 +0900

    feat(problem7): friends 를 읽으며 friendshipMap 을 생성하는 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static HashMap<String, List<String>> makeFriendshipMap(List<List<String>> friends) {
+        HashMap<String, List<String>> friendship = new HashMap<>();
+
+        for (List<String> relation : friends) {
+            String one = relation.get(0);
+            String other = relation.get(1);
+
+            List<String> oneFriends = friendship.getOrDefault(one, new ArrayList<>());
+            List<String> otherFriends = friendship.getOrDefault(other, new ArrayList<>());
+
+            oneFriends.add(other);
+            otherFriends.add(one);
+
+            friendship.put(one, oneFriends);
+            friendship.put(other, otherFriends);
+        }
+
+        return friendship;
     }
 }
