5b799704a7ee155acc40802381fb23d5ffcf6797
yw824 <yongwoo25@gmail.com>
2022-10-30 23:56:43 +0900

    feat: 입력 유효성 확인 기능 추가
    - 함수명: isValid(int left=왼쪽 페이지, int right 오른쪽 페이지)
    - 왼쪽 페이지와 오른쪽 페이지를 입력으로 전송
    - 왼쪽 페이지가 홀수여야 함 : 짝수이면 false 반환
    - 오른쪽 페이지가 짝수여야 함 : 홀수이면 false 반환
    - 오른쪽 페이지가 왼쪽 페이지의 +1 값이 아니면 false 반환
    - 모든 조건에 부합하면 true 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,24 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        // right page equals left page + 1
+        if(!isValid(pobi.get(0), pobi.get(1)) || !isValid(crong.get(0), crong.get(1)))
+            return -1;
+
+        return 0;
+    }
+
+    // check if input is valid( both pobi and crong )
+    private static boolean isValid(int left, int right) {
+        // left page number should be odd
+        if(left % 2 == 0) return false;
+        // right page number should be even
+        if(right % 2 == 1) return false;
+
+        // right page should be equal to left page + 1
+        if(left + 1 != right) return false;
+
+        return true;
     }
 }
