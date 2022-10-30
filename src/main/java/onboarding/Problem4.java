74c3f92250cce001a0bde9b31c682d53509d2637
CordHouse <qkqktl5310@gmail.com>
2022-10-30 15:23:11 +0900

    refactor : Add(cycle, reverseCycle) Function / Problem4 (알파벳 정렬, 역정렬)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /*
     기능 목록
     1. List를 만들어 알파벳 소문자를 집어넣는다.
@@ -12,6 +15,32 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        // small -> a ~ z, big -> A ~ Z
+        // reverseSmall -> z ~ a, reverseBig -> Z ~ A
+        List<Character> small = cycle('a');
+        List<Character> big = cycle('A');
+        List<Character> reverseSmall = reverseCycle('z');
+        List<Character> reverseBig = reverseCycle('Z');
+
         return answer;
+    }
+    // Step 1. 알파벳 a ~ z 순으로 List에 담기
+    public static List<Character> cycle(char word){
+        List<Character> tmp = new LinkedList<>();
+
+        for(int i = 0; i < 26; i++){
+            tmp.add((char) (word + i));
+        }
+        return tmp;
+    }
+
+    // Step 2. 알파벳 z ~ a 순으로 List에 담기
+    public static List<Character> reverseCycle(char word){
+        List<Character> tmp = new LinkedList<>();
+
+        for(int i = 0; i < 26; i++){
+            tmp.add((char) (word - i));
+        }
+        return tmp;
     }
 }
