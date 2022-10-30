12393d492fcd997860a62308116a74ddcb9848ec
Jeongthingamajig <111727268+Jeongthingamajig@users.noreply.github.com>
2022-10-30 22:45:10 +0900

    test

---
+++ java-onboarding/src/main/java/onboarding/Problem1.java
@@ -0,0 +1,61 @@
+package onboarding;
+
+import java.util.List;
+
+/*
+ * 흠..
+ */
+class Problem1 {
+
+    public static boolean check_list(List<Integer> list){
+        return (list.get(0) % 2 != 1) ||
+                (list.get(1) % 2 != 0) ||
+                (list.get(1) - list.get(0) != 1) ||
+                (list.get(1) > 400 || list.get(0) < 1);
+    }
+
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        int pobi_win = 1;
+        int crong_win = 2;
+        int draw = 0;
+        int except = -1;
+
+        int answer = 0;
+
+        if(check_list(pobi) || check_list(crong)){
+            return answer = except;
+        }
+
+        int pobi_cal = Math.max(sum_or_mul(pobi.get(0)), sum_or_mul(pobi.get(1)));
+        int crong_cal = Math.max(sum_or_mul(crong.get(0)), sum_or_mul(crong.get(1)));
+
+        System.out.println("포비승" + pobi_cal);
+        System.out.println("크롱승" + crong_cal);
+
+        if (pobi_cal == crong_cal){
+            return answer;
+        }else{
+            answer = (pobi_cal > crong_cal) ? pobi_win : crong_win;
+        }
+        return answer;
+    }
+
+    /*
+    계산 해주는 함수
+     */
+    static int sum_or_mul(int n){
+        int cal;
+        int sum = 0;
+        int mul = 1;
+        while(n != 0){
+            sum += n % 10;
+            mul  *= n % 10;
+            n /= 10;
+        }
+        System.out.print(sum);
+        System.out.print(mul);
+        cal = Math.max(sum, mul);
+        return cal;
+    }
+}
---
+++ java-onboarding/src/main/java/onboarding/Problem6.java
@@ -0,0 +1,47 @@
+package onboarding;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+ * 생각 놓고 풀었더니 풀렸다
+ * 다시 풀기
+ * 4중 for문 줄이기
+ * 예외 처리 생각하기
+*/
+
+public class Problem6 {
+    public static List<String> solution(List<List<String>> forms) {
+
+        List<String> arr = new ArrayList<>(); // 중복된 아이디들 모아 놓은 곳
+
+        for(int i = 0; i < forms.size(); i++){
+            for(int j = i + 1; j < forms.size() - i; j++){
+                if(isCheck(forms.get(i).get(1), forms.get(j).get(1))){
+                    arr.add(forms.get(i).get(0));
+                    arr.add(forms.get(j).get(0));
+                    System.out.println("arr : " + arr);
+                }
+            }
+        }
+        return arr.stream()
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public static boolean isCheck(String name_1, String name_2){
+        List<String> check = new ArrayList<>();
+        for(int i = 0; i < name_1.length()-1; i++){
+            check.add(name_1.substring(i, i+2));
+            for(int j = 0; j < name_2.length()-1; j++){
+                System.out.println("check.get(i) : " + check.get(i) + " name_2.substring(j, j + 1)" + name_2.charAt(j) + name_2.charAt(j+1));
+                if(Objects.equals(check.get(i), name_2.substring(j, j + 2))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+}
