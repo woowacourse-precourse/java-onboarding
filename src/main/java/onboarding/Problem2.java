a99a53d248dd8074b3a1e393a574ec2f2a95a527
joohyeongKim <wnguddl96@knou.ac.kr>
2022-10-30 13:07:57 +0900

    2번 통합 테스트
    Signed-off-by: joohyeongKim <wnguddl96@knou.ac.kr>

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,44 +1,31 @@
 package onboarding;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 
+
+import static onboarding.problem2.Repository.stored;
 import static onboarding.problem2.Returner.returnLinkedList;
-import static onboarding.problem2.Tester.testingDuplicate;
+
 
 public class Problem2 {
     public static String solution(String cryptogram) {
 
-        String result = "";
+        String answer = "";
+        LinkedList<String> stringLinkedList = new LinkedList<>();
 
-        LinkedList<String> linkedList = new LinkedList<>();
+        LinkedList<String> stored = stored(cryptogram);
 
-        // 문자열 저장을 순회
-        for (int i = 0; i < cryptogram.length(); i++) {
-            Character c = cryptogram.charAt(i);
-            Character d = cryptogram.charAt(i + 1);
-
-            // 중복 여부 == true시 삭제
-            if (c == d) {
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.deleteCharAt(c);
-                stringBuilder.deleteCharAt(d);
-
+        for(int i=0; i<stringLinkedList.size(); i++) {
+            if (stringLinkedList.get(i).equals(stringLinkedList.get(i+1))) {
+                stringLinkedList.remove(i);
+                stringLinkedList.remove(i+1);
+            }else{
+                String result = returnLinkedList(stringLinkedList);
+                answer = result;
             }
-
-            // 삭제후 남은 문자 저장
-            for (String character : linkedList) {
-                linkedList.add(character);
-            }
-
-
-            // linkedList에 저장된 값 문자열로 전달
-            Iterator<String> stringIterator = returnLinkedList(linkedList);
-
         }
 
-        return result;
-
+        return answer;
     }
 }
 
