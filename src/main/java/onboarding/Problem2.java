d29e0d97c9b885343a0ce27005707ae31d493736
Leehyoungwoo <dlguddn3024@gmail.com>
2022-10-31 06:35:01 +0900

    feature: 2번 문제기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Stack<Character> stack = new Stack<>();
+
+        deleteDuplicated(stack,cryptogram);
+        answer = makeToString(stack);
         return answer;
+    }
+    public static void deleteDuplicated( Stack<Character> stack,String cryptogram){
+
+         for (int len = cryptogram.length(),i=0;i<len;i++){
+             if (!stack.isEmpty()&&stack.peek() == cryptogram.charAt(i)){
+                 stack.pop();
+             }else{
+                 stack.push(cryptogram.charAt(i));
+             }
+         }
+
+    }
+    public static String makeToString(Stack<Character> stack){
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : stack){
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
