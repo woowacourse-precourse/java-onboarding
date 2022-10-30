0a3e7744d99c517262be5588037a627f4eef1ace
chaewon121 <80631952+chaewon121@users.noreply.github.com>
2022-10-31 01:39:47 +0900

    feat(problem2): 중복문자의 마지막 문자인지 판별하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem2 {
+
+    private static int flag=0;
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
@@ -8,5 +11,13 @@ public class Problem2 {
 
     private static boolean isDuplicate(char target, char nextTarget){
         return target==nextTarget;
+    }
+
+    private static boolean isDuplicateLastChar(){
+        if(flag==1){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
