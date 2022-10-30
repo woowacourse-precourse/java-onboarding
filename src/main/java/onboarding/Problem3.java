d9ed0dc5f4db4a4f61e131a26b8b83b629e7e3da
java-saeng <gusdn85554@naver.com>
2022-10-30 14:32:06 +0900

    feat : Problem3 각 자리의 숫자를 구하여 손뼉치는 횟수를 구하는 메서드 추가
    - Problem3 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,29 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += countClap(i);
+        }
+
         return answer;
+    }
+
+    //TODO: 손뼉 치는 횟수 최적화
+    private static int countClap(int number) {
+        int count = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+
+            if (isDigitMultipleOfThree(digit)) {
+                count++;
+            }
+
+            number /= 10;
+        }
+
+        return count;
     }
 
     private static boolean isDigitMultipleOfThree(int number) {
