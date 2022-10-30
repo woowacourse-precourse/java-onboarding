6b0534d4549b99dde0c1b14a321b4f3a0a61b617
seonpilKim <ksp970306@gmail.com>
2022-10-30 10:26:55 +0900

    Feat: 도메인 모델 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,83 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+
+	private enum Status {
+		POBI_WIN(1), CRONG_WIN(2), DRAW(0), ERROR(-1);
+
+		private final int result;
+
+		Status(int result) {
+			this.result = result;
+		}
+
+		public int getResult() {
+			return result;
+		}
+	}
+
+	private static class Member {
+
+		private String nickname;
+		private Page page;
+
+		private Member(String nickname, Page page) {
+			this.nickname = nickname;
+			this.page = page;
+		}
+
+		public static Member of(String nickname, List<Integer> pages) {
+			return new Member(nickname, Page.of(pages));
+		}
+
+		public String getNickname() {
+			return nickname;
+		}
+
+		public Page getPage() {
+			return page;
+		}
+
+	}
+
+	private static class Page {
+
+		private int left;
+		private int right;
+
+		private Page(int left, int right) {
+			this.left = left;
+			this.right = right;
+		}
+
+		public static Page of(List<Integer> pages) {
+			return new Page(pages.get(0), pages.get(1));
+		}
+
+		public int getLeft() {
+			return left;
+		}
+
+		public int getRight() {
+			return right;
+		}
+
+	}
+
+	private static class Book {
+
+		public static final int MIN_PAGE = 1;
+		public static final int MAX_PAGE = 400;
+
+	}
+
+	private static class Game {
+
+	}
+
 }
