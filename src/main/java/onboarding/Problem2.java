3625e4d07244a86fc9df5a7c3703d118b73163fb
yun <yshyeon102@gmail.com>
2022-10-31 05:52:22 +0900

    feat: Problem2 - Solution 메서드 정의
    - 문제2 를 해결하기 위한 Solution 함수 정의.
    
    - decodeCryptogram() 메서드 정의
    1.  cryptogram 문자열을 Character 리스트로 변경
    2.  중복을 찾고 제거
    3.  중복을 제거한 리스트를 문자열로 전환
    
    
    #TODO 
    decodeCryptogram() 에 필요한 메서드들을 구현해야함.
    1. convertStringToCharacterLinkedList() 메서드
    2. convertCharacterLinkedListToString() 메서드
    3. findAndRemoveDuplicate() 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import onboarding.problem2.Decoder;
+
+import java.util.LinkedList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decodeCryptogram(cryptogram);
+    }
+
+    private static String decodeCryptogram(String cryptogram) {
+        LinkedList<Character> characterLinkedList = convertStringToCharacterLinkedList(cryptogram);
+        findAndRemoveDuplicate(characterLinkedList);
+        return convertCharacterLinkedListToString(characterLinkedList);
     }
 }
