613e379d09fc72d94574596f3cfc606b0ee17900
dungbangj <yoyob1223@gmail.com>
2022-10-30 17:22:32 +0900

    feat: class for number, validate number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,28 @@
 package onboarding;
 
+class NumberFroPro3 {
+
+    private final static int FIRST_NUMBER = 1;
+    private final static int LAST_NUMBER = 10000;
+
+    private int number;
+
+    public NumberFroPro3(int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    public void validateNumber(int number) {
+        if (FIRST_NUMBER > number || number > LAST_NUMBER) {
+            throw new IllegalArgumentException(
+                "number는 " + FIRST_NUMBER + "이상 " + LAST_NUMBER + "이하의 정수여야 합니다.");
+        }
+    }
+}
+
 public class Problem3 {
     public static int solution(int number) {
+        NumberFroPro3 numberClass = new NumberFroPro3(number);
         int answer = 0;
         return answer;
     }
