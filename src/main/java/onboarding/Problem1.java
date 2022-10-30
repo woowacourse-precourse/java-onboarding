66bbefc59114727441b7e4d428254ffb2cd54ff9
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:21:18 +0900

    feat(Problem1): 파라미터 예외 처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,27 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static boolean checkParam(List<Integer> list) {
+        Integer leftPage;
+        Integer rightPage;
+
+        if (list.size() != 2) {
+            return true;
+        }
+
+        leftPage = list.get(0);
+        rightPage = list.get(1);
+
+        if (leftPage == null || rightPage == null) {
+            return true;
+        } else if (leftPage % 2 == 0 || leftPage < 1 || 399 < leftPage || rightPage - leftPage != 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
