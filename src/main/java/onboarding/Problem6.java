af640387e759cb490bed28d28152cfe7bc0fb0d3
oreo <ohsimon77@naver.com>
2022-10-30 22:52:32 +0900

    문제 6번 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,17 +4,14 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        //List<String> answer = List.of("answer");
         ArrayList<String> answer = new ArrayList<>();
-
         ArrayList<ArrayList<String>> word = new ArrayList<>();
 
-        for (List<String> form : forms) {
+        for (List<String> form : forms) {          //2차원 리스트생성
             ArrayList<String> word2 = new ArrayList<>();
             String nickname = form.get(1);
             for (int j = 0; j < nickname.length() - 1; j++) {
                 word2.add(nickname.substring(j, j + 2));
-                //word_list.add(nickname.substring(j,j+2));
             }
             word.add(word2);
         }
@@ -22,16 +19,18 @@ public class Problem6 {
         for(int i=0; i<word.size(); i++){
             for(int j=0; j<word.size(); j++){
                 for (String s : word.get(j)){
-                    //System.out.println(s);
                     if (word.get(i).contains(s) && i!=j){
                         answer.add(forms.get(i).get(0));
                     }
                 }
             }
         }
+        return result(answer);
+    }
+    static List<String> result(List<String> answer){    //answer 중복제거하고 정렬하기
         ArrayList<String> answer2 = new ArrayList<>();
 
-        for(String ans : answer){
+        for(String ans : answer){   //중복제거
             if(!answer2.contains(ans))
                 answer2.add(ans);
         }
