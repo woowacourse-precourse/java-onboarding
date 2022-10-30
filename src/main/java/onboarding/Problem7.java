e9a61389c429acc57e703e9c61c466b75ad7cabc
CMHyuk <alsgur9710@naver.com>
2022-10-30 20:35:56 +0900

    사용자의 친구 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    public static final int FRIEND_SCORE = 10;
+    public static final int VISITOR_SCORE = 1;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer;
+
+        ArrayList<String> withFriends = storeUserFriends(user, friends);
+
         return answer;
+    }
+
+    private static ArrayList<String> storeUserFriends(String user, List<List<String>> friends) {
+        ArrayList<String> withFriends = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> friendList = friends.get(i);
+            if (friendList.contains(user)) {
+                for (int j = 0; j < friendList.size(); j++) {
+                    String friend = friendList.get(j);
+                    if (!user.equals(friend)) {
+                        withFriends.add(friend);
+                    }
+                }
+            }
+        }
+        return withFriends;
     }
 }
