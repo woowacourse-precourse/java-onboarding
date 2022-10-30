22faee90bf226f18da3665d4c4d8a3c4468953b2
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-10-31 03:55:10 +0900

    Problem5 ListInMoneyCount 구현 및 완성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer=ListInMoneyCount(money);
         return answer;
     }
 
@@ -27,4 +28,22 @@ public class Problem5 {
         return count;
     }
 
+    /**
+     * if횟수를 줄여본 리스트에 담는 머니카운트
+     *
+     * else if 작업을 줄여보았습니다.
+     * @param money 입력 돈
+     * @return answer
+     */
+    public static List<Integer> ListInMoneyCount (int money)
+    {
+        int moneyUnits[] = {50000,10000,5000,1000,500,100,50,10,1};
+        List<Integer> answer = new ArrayList<>();
+        for (int moneyUnit : moneyUnits) {
+            int count = moneyCount(money, moneyUnit);
+            answer.add(count);
+            money=money-(moneyUnit*count);
+        }
+        return answer;
+    }
 }
