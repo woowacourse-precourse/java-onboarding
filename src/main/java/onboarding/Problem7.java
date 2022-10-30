87a6e2470fd13f4f1c5c498877789c2587326c4d
tenta <eunoia3802@gmail.com>
2022-10-30 21:48:12 +0900

    feat: 친구 목록 생성 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static HashMap createFriendsList(String user, List<List<String>> friends, List<String> visitors) {
+        HashMap<String, Integer> members = new HashMap<String, Integer>();
+        for (int i = 0; i < friends.size(); i++) {
+            for (int j = 0; j < 2; j++) {
+                String name = friends.get(i).get(j);
+                members.put(name, 0);
+            }
+        }
+        for (int i = 0; i < visitors.size(); i++) {
+            String name = visitors.get(i);
+            members.put(visitors.get(i), 0);
+        }
+        members.remove(user);
+        return members;
     }
 }
