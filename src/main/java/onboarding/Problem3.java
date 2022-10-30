43413b601b0d229e19e595c7a82ccd1b8c91d233
Jenn Kim <grdd0419@gmail.com>
2022-10-30 21:58:47 +0900

    refactor(Problem3): add a function to count claps of each number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,15 +4,23 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        for(int i=1; i<=number; i++) {
-            String numberToStr = String.valueOf(i);
-            for(int j=0; j<numberToStr.length(); j++) {
-                if (numberToStr.charAt(j) == '3') answer += 1;
-                if (numberToStr.charAt(j) == '6') answer += 1;
-                if (numberToStr.charAt(j) == '9') answer += 1;
-            }
-        }
+        for(int i=1; i<=number; i++)
+            answer += clappedNumber(i);
 
         return answer;
+    }
+
+    public static int clappedNumber(int now) {
+
+        int clappingCnt = 0;
+
+        String numberToStr = String.valueOf(now);
+        for(int j=0; j<numberToStr.length(); j++) {
+            if (numberToStr.charAt(j) == '3') clappingCnt += 1;
+            if (numberToStr.charAt(j) == '6') clappingCnt += 1;
+            if (numberToStr.charAt(j) == '9') clappingCnt += 1;
+        }
+
+        return clappingCnt;
     }
 }
