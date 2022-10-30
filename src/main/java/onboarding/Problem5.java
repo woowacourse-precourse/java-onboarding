256e83247e4c093e8a325c99d76f957b47f48e28
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 17:35:35 +0900

    refactor(PR5): 화폐 리스트 수정 불가능한 리스트로 변경, 코드 간소화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,17 +8,7 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>(Collections.emptyList());
-        List<Integer> moneyList = new ArrayList<Integer>();
-
-        moneyList.add(50000);
-        moneyList.add(10000);
-        moneyList.add(5000);
-        moneyList.add(1000);
-        moneyList.add(500);
-        moneyList.add(100);
-        moneyList.add(50);
-        moneyList.add(10);
-        moneyList.add(1);
+        List<Integer> moneyList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
         for(int i=0; i < 9 ; i++){
             answer.add(0);
