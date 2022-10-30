449b7d85b91321020f158b156a0b8fb0db2bb5d5
ddubidubap <park62000@gmail.com>
2022-10-31 01:05:01 +0900

    refactor: Problem 3 clean code

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,30 +4,40 @@ public class Problem3 {
     /*
         기능 요구 사항 목록
         1. 숫자 당 박수 카운터 함수
-        2. solution (반복문으로 총 박수 횟수 구하기)
+        2. 숫자에 3,6,9가 포함되는지 확인하는 조건문
+        3. 총 박수 횟수 구하는 함수
+        4. solution 함수
     */
 
-    // 숫자 당 박수 카운터 함수
-    private static int clapPerNumber(int number) {
-        int clap = 0;
-        String numString = Integer.toString(number);
+    // 3,6,9가 포함되면 인자에 1을 더하여 반환하는 함수
+    private static int addClap(char numChar, int clap) {
+        if (numChar == '3' || numChar == '6' || numChar == '9')
+            clap += 1;
+        return clap;
+    }
 
-        for (int i = 0; i < numString.length(); i++) {
-            char numChar = numString.charAt(i);
-            if (numChar == '3' || numChar == '6' || numChar == '9')
-                clap += 1;
+    // 숫자 당 박수 카운터 함수
+    private static int clapPerNumber(int clapNumber) {
+        int clap = 0;
+        String clapString = Integer.toString(clapNumber);
+
+        for (int i = 0; i < clapString.length(); i++) {
+            char numChar = clapString.charAt(i);
+            clap = addClap(numChar, clap);
         }
         return clap;
     }
 
-    public static int solution(int number) {
-        int answer = 0;
-
-        for (int i = 0; i <= number; i++) {
+    private static int countTotalClaps(int lastNumber) {
+        int totalClaps = 0;
+        for (int i = 0; i <= lastNumber; i++) {
             int clap = clapPerNumber(i);
-            answer += clap;
+            totalClaps += clap;
         }
+        return totalClaps;
+    }
 
-        return answer;
+    public static int solution(int number) {
+        return countTotalClaps(number);
     }
 }
