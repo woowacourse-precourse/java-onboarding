171d55184e569b0a3a8e69a5846a6e2fb0c14fa7
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-30 15:16:07 +0900

    feat: check duplication of nickname parts

--- src/main/java/onboarding/problem6/DuplicationNotificator.java
+++ src/main/java/onboarding/problem6/DuplicationNotificator.java
@@ -1,8 +1,6 @@
 package onboarding.problem6;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Class searching for duplicated nickname and notify email addresses
@@ -16,6 +14,22 @@ public class DuplicationNotificator {
      * @param forms email and nickname list
      */
     public DuplicationNotificator(List<List<String>> forms) {
+        checkDuplication(forms);
+    }
 
+    /**
+     * Check duplication of nickname parts and create parts map
+     * @param forms email and nickname list
+     */
+    private void checkDuplication(List<List<String>> forms) {
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nick = form.get(1);
+            for (int i = 0; i < nick.length() - 1; i++) {
+                String part = nick.substring(i, i + 2);
+                if (parts.containsKey(part)) parts.get(part).add(email);
+                else parts.put(part, new ArrayList<>(Collections.singletonList(email)));
+            }
+        }
     }
 }
