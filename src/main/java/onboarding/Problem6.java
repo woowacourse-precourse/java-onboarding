06b10c83c586f6457887b6893da91b5da25136b7
AhnJoonSung <tjdtna01@naver.com>
2022-10-30 10:42:25 +0900

    Problem6 make name list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        List<String> name = new ArrayList<>(forms.size());
+
+        for (int i = 0; i < name.size(); i++) {
+            name.add(i, forms.get(i).get(1));
+        }
         return answer;
     }
 }
