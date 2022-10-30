01677491704a67ef0e0ea46715215542977c0fe9
yun <yshyeon102@gmail.com>
2022-10-31 02:40:40 +0900

    feat: Problem4 - solution() 메서드 및 encode() 메서드 구현
    - solution() 메서드 구현
    - encode()  청개구리 규칙에 따른 encoding을 하기 위해, 적절하다고 판단되는 flow에 따라 들어가야 하는 기능을 정의함.
    
    #TODO : encode() 메서드에 필요한 추가적인 메서드, 기능 구현
    - encodeCharacter() : character를 인코딩
    - convertArrayToString() : 인코딩한 Character 배열을 다시 문자열로 바꿈.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return encode(word);
+    }
+
+    private static String encode(String word) {
+        char[] words = word.toCharArray();
+        char[] encodedWords = new char[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            char encodedCharacter = encodeCharacter(words[i]);
+            encodedWords[i] = encodedCharacter;
+        }
+        return convertArrayToString(encodedWords);
     }
 }
