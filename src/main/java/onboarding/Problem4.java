baee884ec3d4e724c98d775868fd5bc095d08f03
chordpli <chordplaylist@gmail.com>
2022-10-30 23:16:35 +0900

    fix(problem4) : 알파벳 외 문자 변환하지 않는 제한사항 적용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,7 +8,7 @@ public class Problem4 {
         // 25차이가 나니까. 문자 - 25를
         // 만약에 'C'라면 67이고 'A'로부터 2가 차이난다. 그러면 90으로부터 -2를 한다.
         char[] array = word.toCharArray();
-        // 글자를 하나씩
+
         for (int i = 0; i < word.length(); i++) {
             if (array[i] != ' ' && array[i] <= 90 && 65 <= array[i]) {
                 char temp = (char) (array[i] - 'A');
