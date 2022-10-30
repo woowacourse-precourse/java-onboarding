0774c17a111134edcaf70b80c3c3f4a9c0255554
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-10-30 22:20:26 +0900

    P1: 예외사항 검증 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,10 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		int pobi_left = pobi.get(0);
+		int pobi_right = pobi.get(1);
+		int crong_left = crong.get(0);
+		int crong_right = crong.get(1);
+
+		// 예외사항 검증
+		if(!check_sanity(pobi_left, pobi_right) ||
+			!check_sanity(crong_left, crong_right))
+			return -1;
+
+		return answer;
+	}
+
+	private static boolean check_sanity(int left, int right) {
+		// 페이지가 연속적이지 않다면 예외사항
+		return left + 1 == right ? true : false;
+	}
 
 	private static int multiply(int num) {
 		int ret = 1;
