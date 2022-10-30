1fa355af7c6b5d9346183135b5e4903350f67d5e
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 20:58:44 +0900

    docs: Problem3 - 기능 구현 목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+
+/**
+ *  getClapCount 메서드 1부터 n까지 3,6,9가 들어가면 카운트를 증가시키고 그 값을 반환
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
