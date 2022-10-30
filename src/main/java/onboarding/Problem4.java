ed586211866dba084bbbd4c90fed8bb491829eef
Jueun <jueun000101@gmail.com>
2022-10-30 22:17:16 +0900

    docs: 4번 기능 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 알파벳을 변환하는 함수
+ * 2. 알파벳이면 변환, 아니면 변환하지 않고 이어붙여 정답 출력
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
