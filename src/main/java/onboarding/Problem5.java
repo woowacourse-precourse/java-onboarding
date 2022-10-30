9af47a07422e27ec1337559eb22a71aa6c3e7319
SangHun <shzero211@naver.com>
2022-10-30 18:34:55 +0900

    feat: Problem5 - 모든화폐를 순환해서 나눠진 몫의값 저장하는 solution

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+        //화패를 나타내는 숫자배열
+        int[] arr=new int[]{50000,10000,5000,1000,500,100,50,10,1};
+        int idx=0;
+        List<Integer> list=new ArrayList<>();
+        //모든 화폐를 돌면서
+        while(idx<=8){
+            if(money>=arr[idx]){
+                list.add(money/arr[idx]);
+                money%=arr[idx];
+            }else{
+                list.add(0);
+            }
+            idx++;
+        }
+
+        return list;
+     }
 }
