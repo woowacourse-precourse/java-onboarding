450909ea8d66dc410cf6a78d95826f13c7630013
Jeangyu <gjwlsrb1700@naver.com>
2022-10-31 00:36:11 +0900

    Add: Problem4 주석 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,10 +7,10 @@ public class Problem4 {
         for (int i=0; i<word.length(); i++){
             char cur = word.charAt(i);
 
-            if (cur>='a' && cur <= 'z'){
+            if (cur>='a' && cur <= 'z'){    // a - z 경우
                 cur = (char) ('z' - (cur - 'a'));
             }
-            else if (cur>='A' && cur <= 'Z'){
+            else if (cur>='A' && cur <= 'Z'){   // A - Z 경우
                 cur = (char) ('Z' - (cur - 'A'));
             }
             answer+=cur;
