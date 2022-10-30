ce7c649472b0d3fe994a4161ba67ab5033e686d1
CordHouse <qkqktl5310@gmail.com>
2022-10-30 15:18:39 +0900

    docs : Problem4 기능 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+/*
+    기능 목록
+    1. List를 만들어 알파벳 소문자를 집어넣는다.
+    2. List를 만들어 알파벳 대문자를 집어넣는다.
+    3. List를 만들어 1, 2번과 반대되게 z~a까지, Z~A까지 넣는다.
+    4. 정렬된 알파벳을 key 그것과 역전되는 알파벳을 value로 가지는 HashMap을 만든다.
+    5. word 파라미터를 char[]로 만들고 띄어쓰기만 고려하여 해당 key를 찾아 value로 바꿔준다.
+
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
