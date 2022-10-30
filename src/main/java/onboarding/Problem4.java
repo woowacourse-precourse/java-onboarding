5a56fbfaec535e6af2d1e6419d88e5e370fbaf65
kkm5291 <kkm23125291@gmail.com>
2022-10-30 16:29:15 +0900

    Problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,48 +7,56 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
-        String[] wordList = stringToList(word);
-
-        return answer;
-    }
-
-    public static void makeArr(char[] arr) {
         char[] alphArr = new char[26];
-        char[] reverseArr = new char[26]; // 기존 array에서 숫자를 빼면 다시 재사용이 가능할 것 같지만 일단 보류
 
         for (int i = 0; i < alphArr.length; i++) {
             alphArr[i] = (char)('a' + i);
         }
 
-        for (int i = reverseArr.length - 1; i >= 0; i--) {
-            reverseArr[i] = (char) ('z' - i);
-        }
-
-        Map<Character, Character> alphabet = new HashMap<>(); // 키와 밸류로 넣음
+        Map<Character, Character> alphabetMap = new HashMap<>(); // 키와 밸류로 넣음
 
         for (int i = 0; i < alphArr.length; i++) {
-            alphabet.put(alphArr[i], reverseArr[i]);
+            alphabetMap.put(alphArr[i], alphArr[alphArr.length - i - 1]);
         }
+
+        char[] wordArr = stringToCharArr(word);
+
+        for (int i = 0; i < wordArr.length; i++) {
+            char temp;
+            char valueTemp;
+            if(checkCase(wordArr[i]) == 1) {
+                temp = Character.toLowerCase(wordArr[i]);
+                valueTemp = alphabetMap.get(temp);
+                wordArr[i] = Character.toUpperCase(valueTemp);
+            } else if (checkCase(wordArr[i]) == -1) {
+                valueTemp = alphabetMap.get(wordArr[i]);
+                wordArr[i] = valueTemp;
+            } else
+                continue;
+        }
+
+        answer = new String(wordArr);
+
+        return answer;
     }
 
-    public static boolean checkCase(char word) { // true : 대문자 false : 소문자
-        if(Character.isUpperCase(word))
-            return true;
+    public static int checkCase(char word) { // 1 : 대문자 0 : 영어 이외 글자 -1 : 소문자
+        if(Character.isUpperCase(word)) {
+            return 1;
+        } else if (Character.isLowerCase(word))
+            return -1;
         else
-            return false;
+            return 0;
 
     }
 
     // 문장 리스트로 바꾸기
-    public static String[] stringToList(String word) {
-        String[] list = word.split("");
+    public static char[] stringToCharArr(String word) {
+        char[] list = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            list[i] = word.charAt(i);
+        }
 
         return list;
     }
-
-
-
-
-
-
 }
