3a8caec195bbc6211fa5edccca78304e10da04d7
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-10-30 17:09:50 +0900

    refactor: modify the currency units array and change for loop to enhanced for loop
    -declare the currency units array 'final'
    -change the array's name 'currencyUnits' to 'CURRENCY_UNITS'
    -delcare the variable 'count' in for loop
    -change for loop to enhanced for loop

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,21 +1,18 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static int[] currencyUnits = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
+    public static final int[] CURRENCY_UNITS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        //최대한 큰 금액의 지페 위주로 담자.
         int leftMoney = money;
-        int count;
 
-        for (int i = 0; i < currencyUnits.length; i++) {
-            count = leftMoney / currencyUnits[i];
+        for (int currencyUnit : CURRENCY_UNITS) {
+            int count = leftMoney / currencyUnit;
             answer.add(count);
-            leftMoney %= currencyUnits[i];
+            leftMoney %= currencyUnit;
         }
 
         return answer;
