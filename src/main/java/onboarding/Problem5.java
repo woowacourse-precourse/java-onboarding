fd7f67504e645fc5fd4338ef29044d0d0185a574
Jzakka <mouse4786@gmail.com>
2022-10-30 17:16:33 +0900

    Feat: "액수 차감 & 돈 개수 계산 기능 구현"

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private List<Integer> values = Arrays.asList(
+            50_000,
+            10_000,
+            5_000,
+            1_000,
+            500,
+            100,
+            50,
+            10,
+            1);
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        getMoneysCounts(money, answer);
+
         return answer;
+    }
+
+    private static void getMoneysCounts(int money, List<Integer> answer) {
+
+    }
+
+    private static int getMoneyCounts(int money, List<Integer> answer, int value) {
+        answer.add(money / value);
+        return money & value;
     }
 }
