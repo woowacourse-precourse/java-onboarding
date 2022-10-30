af85a51be6489d23bf410568a00a43b2e5750123
bellljun3395 <195850@jnu.ac.kr>
2022-10-30 14:00:23 +0900

    feat : Crew 클래스 validatEmailLength 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,8 @@ package onboarding;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -30,6 +32,12 @@ class Crew {
     Crew(String email, String nickName) {
         this.email = email;
         this.nickName = nickName;
+    }
+
+    static void validateEmailLength(String email) {
+        if (email.length() < 11 || 20 <= email.length()) {
+            throw new IllegalStateException("11자 이상 20자 미만의 이메일을 입력해주세요");
+        }
     }
 
 }
