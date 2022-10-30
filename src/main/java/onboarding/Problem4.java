b541fff9e524161732ffd72e54b8fc9c2d121376
yoon <beatus96@naver.com>
2022-10-30 22:43:55 +0900

    [Problem 4] 대문자 반대로 변환하기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+    public static String solution(String input) {
+        char[] word = input.toCharArray();
+
+        for(int i =0; i<word.length ; i++){
+            char ch = word[i];
+            if('A'<= ch && ch<='Z'){
+                word[i] = (char) ('Z'-(ch-'A'));
+            }
+        }
+
+        return String.valueOf(word);
     }
 }
