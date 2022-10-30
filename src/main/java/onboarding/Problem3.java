d90c9f8f8479bf8e00ad0d5f25f7e285ae731b5d
yoon <beatus96@naver.com>
2022-10-30 22:39:51 +0900

    [Problem 3] solution함수 : 완성 (number까지 count 더하기)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,12 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-
         int answer = 0;
+
+        for(int i =1 ; i <number+1; i++){
+            answer+=count(i);
+        }
+
         return answer;
     }
 
