fe51adc434744e92d80db0d597fa7e957e1d6c46
Kuyho Chung <kchung1995@gmail.com>
2022-10-30 17:26:07 +0900

    온보딩 - 문제 5 사용되는 화폐 단위를 담은 array 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    // 사용되는 화폐 단위를 담은 int형 Array
+    final int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
