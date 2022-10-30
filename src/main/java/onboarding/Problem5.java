9e98c204078dfed8e27e48b1b996b0a30bfec81c
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 15:05:09 +0900

    feat(problem5): 단위를 내리며 나온 몫이 저장된 리스트를 반환하는 함수 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,9 +8,22 @@ public class Problem5 {
     static int[] units = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = getWalletResult(money);
 
         return answer;
+    }
+
+    private static List<Integer> getWalletResult(int money) {
+        List<Integer> wallet = new ArrayList<>();
+        int remain = money;
+
+        for (int unit: units){
+            int amount = getQuotientByUnit(remain, unit);
+            remain = getRemainderByUnit(remain, unit);
+            wallet.add(amount);
+        }
+
+        return wallet;
     }
 
     private static int getQuotientByUnit(int money, int unit) {
