c240d6ab0a7f47a96cd4d49b6b59df8e0f7367c9
Woojin Lee <dhkep03@gmail.com>
2022-10-30 13:24:26 +0900

    p6-feat: 기능 구현을 위한 solution 메서드 설계

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Database.validateData(forms);
+        Database.setData(forms);
+        List<String> answer = getEmailListByCheckNickname();
         return answer;
     }
 }
