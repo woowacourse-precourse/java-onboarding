75b8e3a8f29008019cc1a0961e655cc38692b5e3
Jenn Kim <grdd0419@gmail.com>
2022-10-30 21:54:00 +0900

    feat(Problem3): add a function to get clapped number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1; i<=number; i++) {
+            String numberToStr = String.valueOf(i);
+            for(int j=0; j<numberToStr.length(); j++) {
+                if (numberToStr.charAt(j) == '3') answer += 1;
+                if (numberToStr.charAt(j) == '6') answer += 1;
+                if (numberToStr.charAt(j) == '9') answer += 1;
+            }
+        }
+
         return answer;
     }
 }
