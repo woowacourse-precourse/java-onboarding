a275df8971e10af48d045f81f89174b1ee65ae20
math0917 <math0917@naver.com>
2022-10-30 19:24:33 +0900

    test : Problem7 validation test code 작성

---
+++ src/main/java/onboarding/problem7/Problem7Validation.java
@@ -0,0 +1,9 @@
+package onboarding.problem7;
+
+import java.util.List;
+
+public class Problem7Validation {
+    public static boolean problem7Validation(String user, List<String> friends, List<String> visitors) {
+        return true;
+    }
+}
---
+++ src/test/java/onboarding/Problem7CustomTest.java
@@ -0,0 +1,54 @@
+package onboarding;
+
+import onboarding.problem7.Problem7Validation;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Problem7CustomTest {
+    /*
+     *   문자열 길이 관련 테스트는 이미 overriding하여 작성하여 다른 테스트 코드들로
+     *   확인 했으니, 배열의 길이가 맞는 테스트 코드, 소문자를 확인하는 테스트 코드만 작성한다.
+     */
+
+    private static final int MIN_INPUT = 1;
+    private static final int MAX_INPUT = 30;
+
+
+    @Nested
+    class Problem7Test {
+
+        @Test
+        void 배열의길이를만족하지않는경우() {
+            List<String> zeroLengthList = new ArrayList<>();
+            boolean impossible = Problem7Validation.problem7Validation("m", Collections.emptyList(), List.of("m"));
+            assertThat(impossible).isFalse();
+        }
+
+        @Test
+        void 배열의길이를만족하는경우() {
+            List<String> zeroLengthList = new ArrayList<>(List.of("a","b"));
+            boolean possible = Problem7Validation.problem7Validation("m", Collections.emptyList(), List.of("m"));
+            assertThat(possible).isTrue();
+        }
+
+        @Test
+        void 소문자가아닌경우() {
+            List<String> zeroLengthList = new ArrayList<>(List.of("a","b"));
+            boolean impossible = Problem7Validation.problem7Validation("M", Collections.emptyList(), List.of("m"));
+            assertThat(impossible).isFalse();
+        }
+
+        @Test
+        void 소문자만있는경우() {
+            List<String> zeroLengthList = new ArrayList<>(List.of("a","b"));
+            boolean possible = Problem7Validation.problem7Validation("m", Collections.emptyList(), List.of("m"));
+            assertThat(possible).isTrue();
+        }
+    }
+}
