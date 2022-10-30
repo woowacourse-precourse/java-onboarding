3ad7b73eacd4e8d13b611159ab2adecd76f2f5e2
chaerim <qkrdbsk28@naver.com>
2022-10-30 22:33:20 +0900

    feat(problem1): problem1 예외사항 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        // 예외
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
+            // 페이지 번호가 연속되지 않는 경우
+            return -1;
+        }
+
+        return Integer.MAX_VALUE;
     }
 }
