6f3d2a4828cd9daee51bed7764d4001cf382012d
Hyosik Philip Joo <lux1937@gmail.com>
2022-10-30 17:46:41 +0900

    [FEAT] Solve Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Vector;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            Vector<Integer> splitNumber = new Vector<>();
+            int currentNumber = i;
+            while (currentNumber > 0) {
+                splitNumber.add(0, currentNumber % 10);
+                currentNumber /= 10;
+            }
+            for (int digit : splitNumber) {
+                if (digit == 3 || digit == 6 || digit == 9) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
