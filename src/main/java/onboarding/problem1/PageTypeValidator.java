66be102f11606e728298810f21207d5ab50a13ef
Albert <guang7226961@naver.com>
2022-10-31 07:59:05 +0900

    refactor(Problem1) 홀짝수 확인 기능 추가(PageTypeValidator)

--- src/main/java/onboarding/problem1/PageTypeValidator.java
+++ src/main/java/onboarding/problem1/PageTypeValidator.java
@@ -3,11 +3,18 @@ package onboarding.problem1;
 import java.util.List;
 
 public class PageTypeValidator {
-	public static final int DIFFERENCE = 1;
-	public static final int FIRST = 1;
-	public static final int SECOND = 0;
+	private final int DIFFERENCE = 1;
+	private final int LEFT_NUMBER = 0;
+	private final int RIGHT_NUMBER = 1;
+	private final int SECOND = 2;
+	private final int LEFT_ODD_AND_EVEN = 1;
+	private final int RIGHT_ODD_AND_EVEN = 0;
+
 
 	public boolean validate(List<Integer> list) {
-		return list.get(FIRST) - list.get(SECOND) == DIFFERENCE;
+
+		return list.get(RIGHT_NUMBER) - list.get(LEFT_NUMBER)==DIFFERENCE
+			&& list.get(LEFT_NUMBER) % SECOND == LEFT_ODD_AND_EVEN
+			&& list.get(RIGHT_NUMBER) % SECOND == RIGHT_ODD_AND_EVEN;
 	}
 }
--- src/test/java/onboarding/problem1/PageTypeValidatorTest.java
+++ src/test/java/onboarding/problem1/PageTypeValidatorTest.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.Test;
 public class PageTypeValidatorTest {
 
 	@Test
-	@DisplayName("연속된 숫자 맞는지 확인")
+	@DisplayName("연속된 숫자 확인기능")
 	void page_type() {
 		PageTypeValidator pageTypeValidator = new PageTypeValidator();
 		assertThat(pageTypeValidator.validate(List.of(97, 96))).isFalse();
 		assertThat(pageTypeValidator.validate(List.of(97, 100))).isFalse();
 
-		assertThat(pageTypeValidator.validate(List.of(96, 97))).isTrue();
+		assertThat(pageTypeValidator.validate(List.of(96, 97))).isFalse();
+
+		assertThat(pageTypeValidator.validate(List.of(97, 98))).isTrue();
 	}
 }
