84bf0f31d66c6e926a75cd3947f35e1835864e25
kim0914 <kosa0914@naver.com>
2022-10-30 15:31:13 +0900

    feat: Problem1 상수 값 설정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,30 @@ package onboarding;
 
 import java.util.List;
 
+import static onboarding.Problem1.Const.*;
+
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+
+
+    static class Const{
+        public static final int ARRAY_SIZE = 2;
+
+        public static final int LEFT_PAGE_INDEX = 0;
+        public static final int RIGHT_PAGE_INDEX = 1;
+
+        public static final int POBI_WIN = 1;
+        public static final int CRONG_WIN = 2;
+        public static final int DRAW = 0;
+        public static final int EXCEPTION = -1;
+
+        public static final int FIRST_PAGE = 1;
+        public static final int LAST_PAGE = 400;
     }
 }
