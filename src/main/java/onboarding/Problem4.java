ca758db43633a737ca025db36053c0b9689497db
Yongwoo Choi <cyw7515@naver.com>
2022-10-30 14:48:49 +0900

    feat(problem4): 입력한 문자열 복호화하는 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,8 +2,19 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = decrypt(word);
         return answer;
+    }
+
+    private static String decrypt(String word) {
+        StringBuilder outputBuilder = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++){
+            char cur = word.charAt(i);
+            outputBuilder.append(getMatchingCharacter(cur));
+        }
+
+        return outputBuilder.toString();
     }
 
     private static char getMatchingCharacter(char input) {
