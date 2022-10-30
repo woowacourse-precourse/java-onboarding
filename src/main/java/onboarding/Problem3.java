8c2796e522dd683e77c2d98ba7cfe0d8c697a688
wnswo8628 <gbjwein1234@naver.com>
2022-10-30 22:01:57 +0900

    test

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,35 @@
 package onboarding;
 
 public class Problem3 {
+    public static int isthree(String number)
+    {
+        int flag = 0;
+        int count = 0;
+
+        for (int i = 0; i < number.length(); i++)
+        {
+            if (number.charAt(i) == '3' || number.charAt(i) == '6' ||
+                    number.charAt(i) == '9')
+                count++;
+        }
+        return count;
+    }
     public static int solution(int number) {
         int answer = 0;
+        String numbers;
+
+        for (int i = 1; i <= number; i++) {
+            numbers = String.valueOf(i);
+            if (isthree(numbers) != 0)
+                answer += isthree(numbers);
+        }
         return answer;
+    }
+    public static void main(String[] args) {
+        int num = 33;
+        int result;
+
+        result = solution(num);
+        System.out.println(result);
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
         return answer;
     }
 }
