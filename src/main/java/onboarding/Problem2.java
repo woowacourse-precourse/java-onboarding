22ffa54a68c37ccab88d05b07d468c48af89b098
OhJintaek <sunshot@hanyang.ac.kr>
2022-10-30 17:36:43 +0900

    feat:refactor 한 문자열의 모든 중복된 문자를 제거한 뒤 나온 문자열을 똑같은 과정을 거치는 방식으로 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,29 +1,38 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
-        int[] duplicateIndexList = findIndexOfDuplicates(answer);
-        while (duplicateIndexList[0] != -1) {
-            answer = removeDuplicates(answer, duplicateIndexList);
+        List<int[]> duplicateIndexList = findIndexOfDuplicates(answer);
+        while (duplicateIndexList.size() != 0) {
+            String tempCryptogram = answer;
+            for (int[] indexList : duplicateIndexList) {
+                String duplicatesWord = returnDuplicatesWord(answer, indexList);
+                tempCryptogram = removeDuplicates(tempCryptogram, duplicatesWord);
+            }
+            answer = tempCryptogram;
             duplicateIndexList = findIndexOfDuplicates(answer);
         }
 
         return answer;
     }
 
-    private static int[] findIndexOfDuplicates (String cryptogram) {
-        int startIndex = -1;
-        int endIndex = -1;
-        for (int i = 0; i < cryptogram.length()-1; i++) {
+    private static List<int[]> findIndexOfDuplicates (String cryptogram) {
+        int startIndex;
+        int endIndex;
+        List<int[]> indexList = new ArrayList<>();
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
             if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
                 startIndex = i;
                 endIndex = findEndIndexOfDuplicates (cryptogram, startIndex);
+                indexList.add(new int[] {startIndex, endIndex});
+                i = endIndex;
             }
         }
-        return new int[] {startIndex, endIndex};
+        return indexList;
     }
 
     private static int findEndIndexOfDuplicates (String cryptogram, int startIndex) {
@@ -34,11 +43,16 @@ public class Problem2 {
                 break;
             }
         }
+        endIndex -= 1;
         return endIndex;
     }
 
-    private static String removeDuplicates (String cryptogram, int[] indexList) {
-        String duplicates = cryptogram.substring(indexList[0], indexList[1]);
+    private static String returnDuplicatesWord (String cryptogram, int[] indexList) {
+        String duplicates = cryptogram.substring(indexList[0], indexList[1]+1);
+        return duplicates;
+    }
+
+    private static String removeDuplicates (String cryptogram, String duplicates) {
         String resultString = cryptogram.replace(duplicates, "");
         return resultString;
     }
