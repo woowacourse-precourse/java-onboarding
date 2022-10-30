a3d407d2e6e119987a6f443890728443b2d33481
Eleisia <blackcube222@gmail.com>
2022-10-30 14:47:22 +0900

    6번 문제 기능구현사항 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,14 @@
 package onboarding;
 
 import java.util.List;
-
+/*
+* 기능 구현사항
+* 1. 한글 문자열을 2칸단위로 해쉬값에 저장하고 값을 늘리는 기능
+* 2. 문자열의 해쉬값에 대응되는 값이 1보다 큰지 판별
+* 3. 1보다 크다면 TreeSet에 담아 중복제거거와 정렬 수행
+* 4. 첫먼째 문자열을 다시 확인해서 해쉬값이 1보다 크다면 첫번째 문자열도 넣는 기능
+*
+* */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
