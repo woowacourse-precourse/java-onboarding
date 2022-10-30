d0d278efc8dc842774e19838cddb179ddade1d25
Seiyon Park <seiyonpdev@gmail.com>
2022-10-30 22:40:06 +0900

    feat: ADD method notMe

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        List<String> warningEmail = new ArrayList<>();
+
+        return warningEmail;
     }
+
+    public static List<List<String>> notMe(List<List<String>> forms, List<String> me) {
+
+        List<List<String>> withoutMe = new ArrayList<>(forms);
+        withoutMe.remove(me);
+
+        return withoutMe;
+    }
+
 }
