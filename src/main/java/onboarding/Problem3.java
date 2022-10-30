261f2a3fcd0ca20fce7ace7ea31b37152813b464
yw824 <yongwoo25@gmail.com>
2022-10-31 04:56:15 +0900

    feat: [Problem3] 각 숫자마다 쳐야 하는 박수의 횟수를 구해서 반환
    - 함수명: int getClaps(int)
    	- count : 총 횟수를 저장하는 배열
    	- 입력받은 수가 0이 될 때까지 각 자릿수를 추출하여
    		- 3, 6, 9와 비교
    	- 10으로 나눈 나머지인 1의 자릿수를 구하여
    	- 해당 자릿수가 3, 6, 9와 같으면 count에 1을 추가
    	- 입력받은 수를 10으로 나눠, 다음 반복에서 다시 1의 자릿수를 구한다.
    	- 최종 count 수가 쳐야 하는 총 박수 횟수이므로 이를 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -16,8 +16,20 @@ public class Problem3 {
     // 각 숫자마다 쳐야 하는 박수의 횟수를 구해서 반환
     static int getClaps(int input)
     {
-        // implement
-        return input;
-    }
+        // 총 횟수를 저장하는 배열
+        int count = 0;
+        int in = input;
 
+        // 입력받은 수가 0이 될 때까지 각 자릿수를 구해 3, 6, 9와 비교
+        while(in != 0)
+        {
+            // 10으로 나눈 나머지가 3으로 나눠진다면: 3의 배수
+            if((in % 10) % 3 == 0)
+                ++count;
+
+            // 자릿수 줄이기
+            in /= 10;
+        }
+        return count;
+    }
 }
