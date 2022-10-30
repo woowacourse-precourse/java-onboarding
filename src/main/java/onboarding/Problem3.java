03e2d171331fb0eee957c9b429383a63600475aa
Lee-Jiseung <ljss98@naver.com>
2022-10-30 18:10:30 +0900

    feat: problem3 손뼉 치는 횟수 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += countClap(i);
+        }
         return answer;
+    }
+
+    private static int countClap(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                count++;
+            }
+            num /= 10;
+        }
+        return count;
     }
 }
