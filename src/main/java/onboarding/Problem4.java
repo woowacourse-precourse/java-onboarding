e852d10f28c3068c97776c5a07f0c7ba0ab86369
JungHun Jang <ijj5258@naver.com>
2022-10-30 23:24:34 +0900

    Problem4 (feat) String을 List로 변환하는 convertStringToCharList 메소드 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
+	
+	/* String 타입을 List로 형변환 */
+	public static List<Character> 
+	convertStringToCharList(String str){
+  
+        List<Character> chars = new ArrayList<>();
+  
+        for (char ch : str.toCharArray()) {
+            chars.add(ch);
+        }
+  
+        return chars;
+    }
+	
+	
     public static String solution(String word) {
         String answer = "";
+        List<Character> wordList=convertStringToCharList(word);
+		
         return answer;
     }
 }
