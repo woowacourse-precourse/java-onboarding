d35b817a714ef7b2bbbb4b9e270a19bffe967514
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-10-30 19:43:46 +0900

    P1: check the input is available

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean isAvailable(List<Integer> list){
+        if (list.size() != 2) {
+            return false;
+        } else if (list.get(0) <= 1 || list.get(1) >= 400) {
+            return false;
+        } else if (list.get(0) % 2 != 1) {
+            return false;
+        } else if (list.get(1) - list.get(0) != 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
