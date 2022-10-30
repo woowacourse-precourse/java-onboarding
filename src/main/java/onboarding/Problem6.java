24b8a0c5cd1e2bc8d9ed18fc0ddbb4cf6654dd1d
Minky Baek <55613446+toyo30@users.noreply.github.com>
2022-10-30 22:59:29 +0900

    [add] declare - valuables

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,15 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.Collections;
 public class Problem6 {
+        List<String> answer = new ArrayList<>();
+        List<String> nameList = new ArrayList<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
     }
 }
