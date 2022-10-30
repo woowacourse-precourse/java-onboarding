acb8af8e65d32d4f14004b96d6b02d934d7fa64e
woo-chang <clllickme@naver.com>
2022-10-30 15:36:09 +0900

    refactor(문제 5): Converter 필드로 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,24 +5,26 @@ import java.util.List;
 
 public class Problem5 {
 
+  static Converter converter = new Converter();
+
   public static List<Integer> solution(int money) {
-    List<Integer> answer = new ArrayList<>();
-    Converter.convert(answer, money);
-    return answer;
+    return converter.convert(money);
   }
 
   static class Converter {
 
-    private static final int[] currencies = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private final int[] currencies = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-    public static void convert(List<Integer> result, int money) {
+    public List<Integer> convert(int money) {
+      ArrayList<Integer> result = new ArrayList<>();
       for (int currency : currencies) {
-        result.add(getConvertCount(currency, money));
+        result.add(count(money, currency));
         money %= currency;
       }
+      return result;
     }
 
-    private static int getConvertCount(int currency, int money) {
+    private int count(int money, int currency) {
       return money / currency;
     }
   }
