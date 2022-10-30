f5b17846f6ae52419d7927c006bdcd74b060ac95
seonpilKim <ksp970306@gmail.com>
2022-10-30 12:08:06 +0900

    Test: 점수 계산 메소드 테스트 코드 작성

--- src/test/java/onboarding/problem1/book/Problem1Test.java
+++ src/test/java/onboarding/problem1/book/Problem1Test.java
@@ -7,7 +7,9 @@ import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import onboarding.Problem1.*;
+import onboarding.Problem1;
+import onboarding.Problem1.Book;
+import onboarding.Problem1.Page;
 
 class Problem1Test {
 
@@ -72,6 +74,59 @@ class Problem1Test {
 
 			// then
 			assertThat(result).isFalse();
+		}
+
+	}
+
+	@Nested
+	class calculateScore {
+
+		@Test
+		void Page97And98_72() throws Exception {
+			// given
+			final Page page = Page.of(List.of(97, 98));
+
+			// when
+			final int result = Problem1.Game.calculateScore(page);
+
+			// then
+			assertThat(result).isEqualTo(72);
+		}
+
+		@Test
+		void Page197And198_72() throws Exception {
+			// given
+			final Page page = Page.of(List.of(197, 198));
+
+			// when
+			final int result = Problem1.Game.calculateScore(page);
+
+			// then
+			assertThat(result).isEqualTo(72);
+		}
+
+		@Test
+		void Page131And132_6() throws Exception {
+			// given
+			final Page page = Page.of(List.of(131, 132));
+
+			// when
+			final int result = Problem1.Game.calculateScore(page);
+
+			// then
+			assertThat(result).isEqualTo(6);
+		}
+
+		@Test
+		void Page211And212_5() throws Exception {
+			// given
+			final Page page = Page.of(List.of(211, 212));
+
+			// when
+			final int result = Problem1.Game.calculateScore(page);
+
+			// then
+			assertThat(result).isEqualTo(5);
 		}
 
 	}
