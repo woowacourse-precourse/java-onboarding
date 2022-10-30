a93cea3400c1661aff2019791bdb92c4667b0fe5
sdsr <dlxodnr1212@naver.com>
2022-10-30 22:15:31 +0900

    problem3 기능 1 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
     }
+
+    public static int checknumSYG(int number){
+        int temp = number;
+        while(temp > 0){
+            if(temp%10 == 3||temp%10 == 6||temp%10 == 9)
+                return 1;
+            temp /= 10;
+        }
+        return 0;
+    }
+
+
 }
