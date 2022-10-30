c272678cca029adfa50305d15ebd33d4ad7585cf
WonSeok-dd <cluejws@gmail.com>
2022-10-31 00:09:58 +0900

    Problem6 | feat: 기능 정의

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 구현 사항
+ * 1. 겹치지 않았던 닉네임끼리만 비교
+ * 2. 현재 닉네임에서 연속된 두 문자 추출해서 다른 닉네임 비교
+ * 3. 겹쳤던 닉네임을 가진 크루의 이메일 오름차순 정렬
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
