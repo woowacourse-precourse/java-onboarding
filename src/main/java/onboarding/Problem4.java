2678e47f76d08c4ed130c77f34c66d1f438c1933
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-31 01:02:40 +0900

    4번 문제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import onboarding.prob4.domain.Frog;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        Frog frog = new Frog();
+        answer = frog.solve(word);
         return answer;
     }
 }
