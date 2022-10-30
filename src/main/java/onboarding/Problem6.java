4ab5653532e745e489c0784001d159c990a9cfed
Laeng <Laeng@users.noreply.github.com>
2022-10-31 00:36:33 +0900

    feat(problem6): 검증 메서드 재작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,7 +13,49 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         initialize();
 
+        validateForm(forms);
+
+
         return forms.get(0);
+    }
+
+    /**
+     * 신청자 명부 검증
+     * @param forms 폼
+     */
+    private static void validateForm(List<List<String>> forms) {
+        int sizeForms = forms.size();
+        if (sizeForms > 0 && sizeForms <= 10_000) {
+            throw new RuntimeException("등록할 크루가 1명 이상 10,000명 이하여야 합니다.");
+        }
+    }
+
+    /**
+     * 크루 검증
+     * @param crew 등록된 크루
+     * @return 검증 결과 값
+     */
+    private static boolean validateCrew(List<String> crew) {
+        int id = getCrewIdByCrew(crew);
+        String email = crew.get(0);
+        String nickname = crew.get(1);
+
+        if(!Pattern.matches("^[.A-Za-z0-9_-]{1,9}+@email.com$", email)) {
+            System.out.printf("%d번째 입력된 %s님의 이메일 주소는 허용되지 않는 형식입니다.", id, nickname);
+            return false;
+        }
+
+        if (!Pattern.matches("^[가-힣]{1,19}$", nickname)) {
+            System.out.printf("%d번째 입력된 %s님의 닉네임은 허용되지 않는 형식입니다.", id, nickname);
+            return false;
+        }
+
+        if (isDuplicateNickname(id)) {
+            System.out.printf("%d번째 입력된 %s님의 닉네임과 비슷한 닉네임이 있습니다.", id, nickname);
+            return false;
+        }
+
+        return true;
     }
 
     /**
@@ -96,6 +138,10 @@ public class Problem6 {
      */
     private static List<String> getCrewById(int id) {
         return crews.get(id);
+    }
+
+    private static int getCrewIdByCrew(List<String> crew) {
+        return crews.indexOf(crew);
     }
 
     private static void initialize() {
