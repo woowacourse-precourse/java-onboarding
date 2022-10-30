e1a3641a49ba896334de4fc1a6b95685b52cb458
K-Diger <kitten.diger@gmail.com>
2022-10-30 19:22:59 +0900

    fix : 예외 사항에 대한 로직 수정 - convertRecommendScoreMapToSortedList() 에   .sorted(Map.Entry.comparingByKey()) 구문을 추가하여   이름순으로 먼저 정렬한 후, Value 값으로 정렬하여   Value 가 같으면 이름순으로 리스트에 담을 수 있도록 한다. - 반환하는 List 의 크기는 5가 넘지 않아야 하는 조건 추가 -> result.size() < 5 - 이미 내 친구인 유저인지 판별하는 조건 추가 -> !myFriends.contains(entry.getKey())

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,11 +9,10 @@ public class Problem7 {
     [기능 목록]
     1. 친구 추천 점수를 기록하기 위해 Map<> 타입의 변수를 초기화 하기
     2. 현재 유저와 친구인 사람의 이름을 리스트로 반환하기
-    3. 모든 유저 이름 가져오기 (방문자 및, 친구 관계 리스트를 바탕으로 모든 유저의 이름을 가져온다.)
-    4. 내 타임라인 방문자들의 친구 추천 점수를 올리기
+    3. 내 타임라인 방문자들의 친구 추천 점수를 올리기
         (단, 2번 메서드를 활용하여 이미 친구인 사람이 방문했을 경우는 점수를 올리지 않는다.)
-    5. 나와 함께 아는 친구를 바탕으로 추천 스코어 반영하기
-    6. Map 형의 친구 추천 스코어를 Value 를 기준으로 하여 정렬한 후, 리스트로 변환하기 (단, value 가 0인 Key는 변환하지 않는다.)
+    4. 나와 함께 아는 친구를 바탕으로 추천 스코어 반영하기
+    5. Map 형의 친구 추천 스코어를 Value 를 기준으로 하여 정렬한 후, 리스트로 변환하기 (단, value 가 0인 Key는 변환하지 않는다.)
      */
 
     // 추천 친구 스코어를 기록하기 위한 recommendScore 셋팅
@@ -95,7 +94,7 @@ public class Problem7 {
         // 내 친구의 수만큼 반복한다. (친구가 없다면 반복하지 않고 종료)
         for (String myFriend : myFriends) {
             // 전체 유저의 친구 관계를 순회한다.
-            // 친구 관계를 나타내느 리스트를 하나씩 순회하며
+            // 친구 관계를 나타내는 리스트를 하나씩 순회하며
             for (List<String> friendShip : friendsShip) {
                 // 내 친구목록에 있는 이름과 일치한 항목이 있고, 내 이름이 들어있지 않은 항목을 가진
                 // 리스트 원소라면 그 원소는 나와 함께하는 친구를 가리키는 것이다.
@@ -110,25 +109,33 @@ public class Problem7 {
         }
     }
 
-    // Map 형의 친구 추천 스코어를 Value 를 기준으로 하여 정렬한 후, 리스트로 변환하기 (단, value 가 0인 Key는 변환하지 않는다.)
-    public static List<String> convertRecommendScoreMapToSortedList(Map<String, Integer> recommendScore) {
+    // Map 형의 친구 추천 스코어를 Value 를 기준으로 하여 정렬한 후, 리스트로 변환하기 (단, value 가 0인 Key 는 변환하지 않는다.)
+    public static List<String> convertRecommendScoreMapToSortedList(
+            Map<String, Integer> recommendScore, List<String> myFriends) {
 
         List<String> result = new ArrayList<>();
 
+        // 221030 : 19:00 추가사항
+        // 이름순으로 먼저 정렬한 후
+        // Value 값으로 정렬한다.
         List<Map.Entry<String, Integer>> entries = recommendScore.entrySet().stream()
                 .filter(value -> value.getValue() != 0)
+                .sorted(Map.Entry.comparingByKey())
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toList());
 
+        // 221030 : 19:00 추가사항
+        // 반환하는 List 의 크기는 5가 넘지 않아야 하는 조건 추가 -> result.size() < 5
+        // 이미 내 친구인 유저인지 판별하는 조건 추가 -> !myFriends.contains(entry.getKey())
         for (Map.Entry<String, Integer> entry : entries) {
-            result.add(entry.getKey());
+            if (!myFriends.contains(entry.getKey()) && result.size() < 5) {
+                result.add(entry.getKey());
+            }
         }
-
         return result;
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
 
         // 친구 추천 스코어
         Map<String, Integer> recommendScore = initRecommendScore(user, friends, visitors);
@@ -136,15 +143,14 @@ public class Problem7 {
         // 유저의 현재 친구 목록
         List<String> myFriends = extractWhoIsMyFriends(user, friends);
 
-        // 모든 유저 목록
-        List<String> allUsers = loadAllUsers(friends, visitors);
-
         // 친구 추천 스코어에 타임라인 방문자 점수 반영하기
         setVisitorsScore(recommendScore, visitors, myFriends);
 
         // 친구 추천 스코어에 함께 아는 친구 점수 반영하기
         setTogetherAcquaintanceScore(user, recommendScore, friends, myFriends);
 
-        return convertRecommendScoreMapToSortedList(recommendScore);
+        // 221030 : 19:00 추가사항에 따른 매개변수 추가
+        // myFriends -> 함께 아는 친구 점수 반영 과정에서 이미 내 친구인 사람은 제외해야하므로 추가하였다.
+        return convertRecommendScoreMapToSortedList(recommendScore, myFriends);
     }
 }
