bde52b60c759bc1a5feab0d316c1c4e54a0c9073
wonseokjee <wsji9404@gmail.com>
2022-10-30 14:21:42 +0900

    221030 problem5 기능목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,7 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+//  1. hashmap으로 9개 금액 별로 나누는 매서드(큰 값이 키값 1부터 시작)
+//  2. for문으로 가장 큰 값부터 나누고 나눈값 같은 index자리 result에 추가 나머지 갱신
+
 }
