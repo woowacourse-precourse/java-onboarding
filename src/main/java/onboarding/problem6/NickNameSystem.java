4edaba65e9bfb5d8f2821dbcdc0137e1d52bf138
kimminsu <xjfcnfw3@naver.com>
2022-10-30 13:13:34 +0900

    feat(문제6): 2개의 문자 중복 확인 기능 구현

--- src/main/java/onboarding/problem6/NickNameSystem.java
+++ src/main/java/onboarding/problem6/NickNameSystem.java
@@ -1,10 +1,13 @@
 package onboarding.problem6;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NickNameSystem {
     private static final int EMAIL = 0;
     private static final int NICK_NAME = 1;
+    private static final Map<String, String> hashMap = new HashMap<>();
 
     private void selectForm(List<List<String>> forms) {
         for (List<String> form : forms) {
@@ -17,6 +20,14 @@ public class NickNameSystem {
         String email = form.get(EMAIL);
         for (int idx = 0; idx < selectedName.length() - 1; idx++) {
             String subStr = selectedName.substring(idx, idx + 2);
+            checkSubStr(email, subStr);
         }
+    }
+
+    private void checkSubStr(String email, String subStr) {
+        if (hashMap.containsKey(subStr)) {
+            String hashMapEmail = hashMap.get(subStr);
+        }
+        hashMap.put(subStr, email);
     }
 }
