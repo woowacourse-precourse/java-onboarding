87e682aac536162197d68a6fbf747dc90432af94
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-10-30 21:40:27 +0900

    êfeat : 기능 목록 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -22,6 +22,54 @@ public class Problem1 {
             return -1;
         }
 
-        return 0;
+        //점수 획득
+        int pobiLeft = getScore(sum(p1), multiply(p1));
+        int pobiRight = getScore(sum(p2), multiply(p2));
+
+        int pNum = getScore(pobiLeft, pobiRight);
+
+        int crongLeft = getScore(sum(c1), multiply(c1));
+        int crongRight = getScore(sum(c2), multiply(c2));
+
+        int cNum = getScore(crongLeft, crongRight);
+
+        if (pNum == cNum) {
+            return 0;
+        }
+
+        return pNum > cNum ? 1 : 2;
+
     }
+
+    //페이지 최댓값 비교
+    private static int getScore(int sum, int multiply) {
+        return sum > multiply ? sum : multiply;
+    }
+
+    //페이지 자릿수 덧셈
+    public static int sum(int n) {
+        int result = 0;
+
+        while (n > 0) {
+            result += n % 10;
+            n /= 10;
+        }
+
+        return result;
+    }
+
+    //페이지 자릿수 곱셈
+    public static int multiply(int n) {
+        int result = 1;
+
+        while (n > 0) {
+            result *= n % 10;
+            n /= 10;
+        }
+
+
+        return result;
+    }
+
+
 }
