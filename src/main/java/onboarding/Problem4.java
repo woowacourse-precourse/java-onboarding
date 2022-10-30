6bb3e2066e4b38258455df6a06986adcc653c864
Kimhyeyun <cqqwer@gmail.com>
2022-10-30 21:55:28 +0900

    [PROBLEM 4] 대/소문자 변환 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,20 +5,20 @@ public class Problem4 {
         String answer = "";
 
         for (char w : word.toCharArray()) {
-            if(w == ' ') answer += " ";
-            else answer += translate(w);
+            if(97 <= w && w <= 122) answer += translateSmall(w);
+            else if(65 <= w && w <= 90) answer += translateBig(w);
+            else answer += w;
         }
 
         return answer;
     }
 
-    private static char translate(char w) {
-        char c;
-        if (97 <= w && w <= 122) {
-            c = (char) (122 - (w - 97));
-        } else {
-            c = (char) (90 - (w - 65));
-        }
-        return c;
+    private static char translateBig(char w) {
+        return (char) (90 - (w - 65));
     }
+
+    private static char translateSmall(char w) {
+        return (char) (122 - (w - 97));
+    }
+
 }
