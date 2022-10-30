85430c0543de50d17c593583758b9c4e9171d836
YE-EUN <musiclove529@naver.com>
2022-10-30 21:56:27 +0900

    feature : establish problem7 structure - 기본 구조 구축   - getUserFriendListMap : 사용자 별 친구 리스트 구하기   - initFriendCandidateIdInfoMap : 친구 후보 구하여 추천 점수 0점으로 초기화   - calcRecommendScore : 친구 후보 추천 점수 구하기   - getRecommendationList : 추천 리스트 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,42 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+import DTO.FriendCandidate;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Map<String, List<String>> userFriendListMap = getUserFriendListMap(friends);
+        Map<String, FriendCandidate> friendCandidateIdInfoMap = initFriendCandidateIdInfoMap(user, userFriendListMap);
+        List<FriendCandidate> friendCandidateList = calcRecommendScore(friendCandidateIdInfoMap, userFriendListMap, visitors);
+        answer = getRecommendationList(friendCandidateList);
+
         return answer;
+    }
+
+    public static Map<String, List<String>> getUserFriendListMap(List<List<String>> friends){
+        Map<String, List<String>> userFriendListMap = new HashMap<>();
+
+        return userFriendListMap;
+    }
+
+    public static Map<String, FriendCandidate> initFriendCandidateIdInfoMap(String user, Map<String, List<String>> userFriendListMap){
+        Map<String, FriendCandidate> friendCandidateIdInfoMap = new HashMap<>();
+
+        return friendCandidateIdInfoMap;
+    }
+
+    public static List<FriendCandidate> calcRecommendScore(Map<String, FriendCandidate> friendCadidateIdInfoMap, Map<String, List<String>> userFriendListMap, List<String> visitors){
+        List<FriendCandidate> friendCandidateList = new ArrayList<>();
+
+        return friendCandidateList;
+    }
+
+    public static List<String> getRecommendationList(List<FriendCandidate> friendCandidateList){
+        List<String> recommendationList = new ArrayList<>();
+
+        return recommendationList;
     }
 }
