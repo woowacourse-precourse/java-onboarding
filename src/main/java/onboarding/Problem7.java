3dc01d9aac78e89c2570511e9f811920e36cc734
kwYoohae <gocksdl7979@gmail.com>
2022-10-30 23:30:29 +0900

    feat(Problem.java): uesr의 친구들의 친구들은 누가 있는지 확인하고 점수를 부여하는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Problem7 {
 
     private static Set<String> friendList = new HashSet<>();
+    private static HashMap<String, Integer> score = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
@@ -23,4 +24,24 @@ public class Problem7 {
         }
     }
 
+    public static void getScoreInFriend(String user, List<List<String>> friends) {
+        for (List<String> friendData : friends) {
+            if(user.equals(friendData.get(1)) == false && friendList.contains(friendData.get(0))) {
+                if(score.containsKey(friendData.get(1)) == false) {
+                    score.put(friendData.get(1), 10);
+                }
+                else {
+                    score.put(friendData.get(1), score.get(friendData.get(1)) + 10);
+                }
+            }
+            if(user.equals(friendData.get(0)) == false && friendList.contains(friendData.get(1))) {
+                if(score.containsKey(friendData.get(0)) == false) {
+                    score.put(friendData.get(0), 10);
+                }
+                else {
+                    score.put(friendData.get(0), score.get(friendData.get(0)) + 10);
+                }
+            }
+        }
+    }
 }
