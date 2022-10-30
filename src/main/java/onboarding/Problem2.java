18636bc53ef0ed156975dbc15af3a10b64210783
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:30:05 +0900

    feat : [Problem2] 기능 사항 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/**
+ * 기능 사항
+ * 1.중복문자가 있는 인덱스번호를 리스트에 추가하는 함수
+ * 2.연속되지 않은 중복문자라면, 배열을 잘라 리턴하는 함수
+ * 3.리스트 0번지와 마지막이 가지는 인덱스번호를 리턴하는 함수
+ * 4.중복문자 부분만 제거해서 리턴하는 함수
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
