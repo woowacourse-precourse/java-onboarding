d7fd988cf8790a509f49da26f3a4bebe9363feb3
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 17:46:24 +0900

    feat(PR4): 알파벳 저장 공간 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        List<Character> alphabet = List.of('A','B','C','D','E','F','G',
+                                        'H','I','J','K','L','M','N','O','P','Q',
+                                        'R','S','T','U','V','W','X','Y','Z');
+
         return answer;
     }
 }
