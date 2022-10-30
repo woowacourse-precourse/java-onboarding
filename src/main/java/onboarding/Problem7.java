9d277d9ca6e1dc103f56058898b37053051bd623
EUNSH1 <tkdgus968@naver.com>
2022-10-31 02:49:41 +0900

    feat : [Problem7] 친구목록을 통해 사용자와 함께아는친구 찾기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 1.친구목록을 통해 사용자와 함께아는친구 찾기
@@ -9,6 +8,26 @@ import java.util.List;
  * 3.타임라인의 방문자들을 TreeMap에 저장하는 함수
  */
 public class Problem7 {
+
+    static Map<String, Integer> map = new TreeMap<>();
+    
+    /**
+     * 1.친구목록을 통해 사용자와 함께아는친구 찾기
+     */
+    public static List<String> friendList(String user, List<List<String>> n) {
+        List<String> friendList = new ArrayList<>();
+        for(int i = 0; i<n.size();i++){
+            if(n.get(i).get(1).equals(user)){
+                friendList.add(n.get(i).get(0));
+            }
+
+            if(n.get(i).get(0).equals(user)){
+                friendList.add(n.get(i).get(1));
+            }
+        }
+
+        return friendList;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
