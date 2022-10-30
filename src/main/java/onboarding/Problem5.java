90e8bc6b23a3cd7adddc12416ceb13ac759b7b4d
sosow0212 <sosow0212@naver.com>
2022-10-30 15:18:35 +0900

    refactor: Problem3 - static 필드에 private 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    static final int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static final int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = getCount(money);
