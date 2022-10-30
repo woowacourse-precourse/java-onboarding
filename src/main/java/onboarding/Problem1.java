299c28542a99700f4fd1496bcbcccf43fe56a0fe
lynne921 <dkfls921@gmail.com>
2022-10-31 00:31:29 +0900

    [feat][problem1] 예외 처리 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static boolean isException(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(0) != pobi.get(1) - 1 || crong.get(0) != crong.get(1) - 1) {
+            return true;
+        }
+        if (pobi.get(0) == 1 || crong.get(0) == 1 || pobi.get(1) == 400 || crong.get(1) == 400) {
+            return true;
+        }
+        return false;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (isException(pobi, crong)) {
+            answer = -1;
+            return answer;
+        }
         return answer;
     }
 }
