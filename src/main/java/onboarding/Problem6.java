4c11123619ba8de66758773c25063cf4a8f183e5
matt <matt1235@naver.com>
2022-10-30 15:10:19 +0900

    feat: Problem6 (기능 목록1 - 닉네임 토큰화) 기능 구현 기능 목록 1 : 닉네임 토큰화 기능 구현 - 닉네임을 받아 2글자씩 묶어 토큰으로 만든다. - tokenList에 토큰의 Set을 넣는다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<Set<String>> tokenList = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String nickName = form.get(1);
+            Set<String> tokens = nickToToken(nickName);
+            tokenList.add(tokens);
+        }
+
         return answer;
     }
+
+    /**
+     * nickname을 글자 2개씩 끊어서 토큰화
+     * */
+    private static Set<String> nickToToken(String nickname){
+        Set<String> subSet = new HashSet<>();
+        for(int i=0;i<nickname.length()-1;i++){
+            String temp = nickname.substring(i,i+2);
+            subSet.add(temp);
+        }
+        return subSet;
+    }
+
 }
