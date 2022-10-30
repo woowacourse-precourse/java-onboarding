fd97add030055524bd1ae099d93ae76c398774e0
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-10-31 00:55:07 +0900

    Problem4 대문자 테이블

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,7 @@ package onboarding;
 
 public class Problem4 {
     private char lowerCasesTable[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private char upperCasesTable[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     public static String solution(String word) {
         String answer = "";
         return answer;
