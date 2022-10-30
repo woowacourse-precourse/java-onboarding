69ecee85baf2bd1c65e52537b3649731b26b0231
wonseokjee <wsji9404@gmail.com>
2022-10-30 14:17:03 +0900

    221030 problem4 sol 문자 변환 후 result 리턴

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -41,4 +41,25 @@ public class Problem4 {
         return save.get(alp);
     }
 
+    //    2. 알파벳이 아니면 그대로 가고 알파벳이면 1번 메서드로 변환후 대소문자 맞추기
+    public static String findalp(String word) {
+        /*문자 하나하나를 쪼개서(띄어쓰기도 포함) 대문자이면 dict에 넣어서 바꾸고 소문자로 변환후 result에 추가*/
+        String[] wordIns = word.split("");
+        String result = "";
+        for (String a : wordIns) {
+            if (Character.isUpperCase(a.charAt(0))) {
+                String ins = dict(a.charAt(0)).toString();
+                result += ins;
+                /* 소문자이면 대문자로 바꿔서 dict변환 거친후 result에 추가*/
+            } else if (Character.isLowerCase(a.charAt(0))) {
+                String dictUpper = dict(a.toUpperCase().charAt(0)).toString();
+                String strLower = dictUpper.toLowerCase();
+                result += strLower;
+                /* 알파벳이 아니면 바로 result에 추가*/
+            } else {
+                result += a;
+            }
+        }
+        return result;
+    }
 }
