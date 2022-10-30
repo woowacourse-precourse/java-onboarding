63d891788bc4b360bf7f81e5d40a6264db994999
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-10-30 11:16:25 +0900

    [ADD] Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import onboarding.problem5.CurrencyExchanger;
+
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        CurrencyExchanger currencyExchanger = new CurrencyExchanger();
+        currencyExchanger.addCurrencyUnit(1);
+        currencyExchanger.addCurrencyUnit(10);
+        currencyExchanger.addCurrencyUnit(50);
+        currencyExchanger.addCurrencyUnit(100);
+        currencyExchanger.addCurrencyUnit(500);
+        currencyExchanger.addCurrencyUnit(1_000);
+        currencyExchanger.addCurrencyUnit(5_000);
+        currencyExchanger.addCurrencyUnit(10_000);
+        currencyExchanger.addCurrencyUnit(50_000);
+        return currencyExchanger.minNumCurrencyUnits(money);
     }
 }
