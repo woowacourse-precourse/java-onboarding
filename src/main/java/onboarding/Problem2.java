a32ac4171fb0c6c22c44a5ec729b755d172b624d
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-10-30 22:54:07 +0900

    P2: 문자열 길이가 변하지 않을 때까지 반복 후 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,16 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		int len;
+
+		do {
+			len = cryptogram.length();
+			cryptogram = removeDup(cryptogram);
+		} while(len != cryptogram.length());
+
+		return cryptogram;
+	}
 
 	private static String removeDup(String str) {
 		String ret = "";
