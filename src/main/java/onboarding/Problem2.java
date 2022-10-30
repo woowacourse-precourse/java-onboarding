229c593bf01d4484cd2fca6a206408c4c48e7c99
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 16:26:35 +0900

    feat(Problem2): 문자열에서 연속적인 부분 문자열을 찾아주는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        Set<Integer> set = new HashSet<>();
+
+        findingContinuousString(cryptogram, set);
+
         String answer = "answer";
         return answer;
+    }
+
+    /**
+     * Function for finding ContinuousString in String
+     * */
+    private static void findingContinuousString(String cryptogram, Set<Integer> set) {
+        for (int idx = 0; idx < cryptogram.length(); idx++) {
+            int l_idx = Math.max(0, idx-1);
+            int r_idx = Math.min(cryptogram.length()-1,idx+1);
+
+            if ((l_idx != idx) && (cryptogram.charAt(idx) == cryptogram.charAt(l_idx))) {
+                set.add(idx);
+                set.add(l_idx);
+            }
+
+            if ((r_idx != idx) && (cryptogram.charAt(idx) == cryptogram.charAt(r_idx))) {
+                set.add(idx);
+                set.add(r_idx);
+            }
+        }
     }
 }
