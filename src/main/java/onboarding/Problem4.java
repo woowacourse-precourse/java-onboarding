4d592ee003b06696e3ad9465c30db8b2be915369
seonho <ysh10216@gmail.com>
2022-10-30 19:03:00 +0900

    solved step

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        // 1. word 예외처리 : 길이가 1~1000
+        // 알파벳만 변하도록 처리. 대문자는 대문자로 소문자는 소문자로.
+        // 2. 단어를 한개 씩 받아서 변환해주기.
+
         return answer;
     }
 }
