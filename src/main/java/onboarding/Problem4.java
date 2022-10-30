9d2a1713097f1e93ac36c969f795a844387094b3
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-10-30 14:34:17 +0900

    알파벳을 바꾸는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = changeWord(word);
         return answer;
+    }
+
+    //알파벳을 바꾸는 기능
+    //대문자 소문자 구분해서 바꾸는 기능
+
+    public static String changeWord(String word){
+        String[] split = word.split("");
+        List<String> sol = new ArrayList<>();
+        char c;
+
+        for (String s : split) {
+            int i = (int) s.toCharArray()[0];
+            if(i>=97 && i<=122){
+                c=(char)(122-(i-97));
+            }else if(i>=65 && i<=90){
+                c=(char)(90-(i-65));
+            }else{
+                c=(char)i;
+            }
+            sol.add(String.valueOf(c));
+        }
+        return String.join("",sol);
     }
 }
