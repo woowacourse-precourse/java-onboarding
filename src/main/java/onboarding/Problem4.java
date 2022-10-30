4ba88bede4861b815fe952a8d04292d7b532fde5
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:43:44 +0900

    feat(Problem4): 파라미터 예외 처리 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static boolean checkParam(String s)
+    {
+        int length = s.length();
+
+        if (length == 0 || 1000 < length)
+            return true;
+        return false;
+    }
+    
     public static String solution(String word) {
         String answer = "";
         return answer;
