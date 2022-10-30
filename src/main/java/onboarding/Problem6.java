5332abc1e68cf6bc19862d052c1b65f0ec99882f
JeongHunHui <deca3248@gmail.com>
2022-10-30 20:07:34 +0900

    [1주차 - Problem6] 정규식 수정 및 글자수 체크 코드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Pattern emailPattern = Pattern.compile("^[A-Za-z0-9]+(.[_A-Za-z0-9-]+)*@email[.]com");
-        Pattern nicknamePattern = Pattern.compile("^[ㄱ-ㅎ가-힣]*");
+        Pattern nicknamePattern = Pattern.compile("^[ㄱ-ㅎ가-힣]+(.[ㄱ-ㅎ가-힣]+)*");
         for (List<String> data : forms) {
             if(!emailPattern.matcher(data.get(0)).matches()) {
                 System.out.println("올바르지 않은 이메일 형식입니다.");
@@ -15,6 +15,14 @@ public class Problem6 {
             }
             if(!nicknamePattern.matcher(data.get(1)).matches()) {
                 System.out.println("닉네임은 한글만 가능합니다.");
+                return null;
+            }
+            if(data.get(0).length() > 20) {
+                System.out.println("이메일의 길이가 20자 이상입니다.");
+                return null;
+            }
+            if(data.get(1).length() > 20) {
+                System.out.println("닉네임의 길이가 20자 이상입니다.");
                 return null;
             }
         }
