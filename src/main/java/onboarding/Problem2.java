f2552ac8f5981f05a6a3f0d1151dcbba163b52bd
mgkim-developer <mgkim.developer@gmail.com>
2022-10-31 00:59:22 +0900

    2022.10.31_(problem2 - 기능 요구 사항 1)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,28 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    /*
+    * 기능 요구 사항
+    * 1. 주어진 문자에서 연속으로 중복된 문자의 인덱스를 반환
+    * */
+
+    private static int duplicate_index(String string) {
+        for (int index = 0; index < string.length() - 1; index++) {
+            if(string.charAt(index) == string.charAt(index + 1)) return index;
+        }
+        return -1;  // 중복된 문자가 없으면 -1 반환
+    }
+
+    /*
+     * 기능 요구 사항
+     * 2. 주어진 문자에서 연속으로 중복된 문자를 삭제후 반환
+     * */
+
+
+
+    /*
+     * 기능 요구 사항
+     * 3. 주어진 문자에서 연속으로 반복된 문자열이 없을 때 까지 반복
+     * */
 }
