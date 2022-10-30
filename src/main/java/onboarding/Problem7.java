abcbedb01d7210ebc0b6319f770b5f7d7d63f37d
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 17:44:51 +0900

    feat(Problem7): 친구관계를 나타내는 그래프 초기화 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        Map<String, Set<String>> friendshipGraph = new HashMap<>();
+        initFriendshipGraph(friends, friendshipGraph);
         return answer;
+    }
+
+    /**
+     * Function for friendshipGraph that represent all friendship as a graph
+     * */
+    private static void initFriendshipGraph(List<List<String>> friends, Map<String, Set<String>> friendshipGraph) {
+        for (List<String> each : friends) {
+            String person1 = each.get(0);
+            String person2 = each.get(1);
+
+            if (!friendshipGraph.containsKey(person1)) {
+                friendshipGraph.put(person1, new HashSet<String>());
+            }
+
+            if (!friendshipGraph.containsKey(person2)) {
+                friendshipGraph.put(person2, new HashSet<String>());
+            }
+
+            friendshipGraph.get(person1).add(person2);
+            friendshipGraph.get(person2).add(person1);
+        }
     }
 }
