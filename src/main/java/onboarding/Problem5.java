ff3b5d97eb9561bb63ec6f24857a601b77f2e264
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:53:07 +0900

    feat(Problem5): 파라미터 예외 처리 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static boolean checkParam(int n)
+    {
+        return n < 1 || 1000000 < n;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
