8e79c291eb82f4f76b658a79fe23790150c96f11
Sangwoo Shin <sangwoonoel@naver.com>
2022-10-30 21:45:44 +0900

    feat(Problem2): string을 ArrayList로 변환하는 메소드 작성(기능목록1)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
+    private static ArrayList<Character> stringToArrayList(String s) {
+        ArrayList<Character> list = new ArrayList<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char currentCharacter = s.charAt(i);
+            list.add(currentCharacter);
+        }
+        return list;
+    }
 }
+
