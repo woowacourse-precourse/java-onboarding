50a46bc9a726bc0d5ee9f0e5b8752b8f9bb5a56e
Torres-09 <dla612@naver.com>
2022-10-31 02:10:05 +0900

    feat : 문제 3 기능 요구사항 완료, 테스트 완료
    제한사항에 해당하는 범위의 자연수에 대한 369를 카운트한 후 배열에 저장

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem3 {
+    public static int[] dp = new int[100001];
+    public static int[] sam = {3, 6, 9};
+
+    public static void init() {
+        for (int i = 1; i <= 10000; i++) {
+            int temp = i;
+            int plus = 0;
+            while (temp != 0) {
+                for (int j = 0; j < 3; j++) {
+                    if (temp % 10 == sam[j])
+                        plus++;
+                }
+                temp /= 10;
+            }
+
+            dp[i] = dp[i - 1] + plus;
+        }
+    }
+
     public static int solution(int number) {
-        int answer = 0;
+        init();
+        int answer = dp[number];
         return answer;
     }
 }
