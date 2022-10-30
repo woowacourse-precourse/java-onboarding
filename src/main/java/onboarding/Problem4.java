d877665b41a3578e335488e9116c1ee0067383d2
dong young <dong5854@gmail.com>
2022-10-31 02:34:48 +0900

    [Problem4] feat: add frog function
    청개구리가 하는 말을 반환하는 frog 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,8 +7,24 @@ public class Problem4 {
     static HashMap<Character, Character> frogHash = new HashMap<>();
 
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        createFrogHashMap();
+        return frog(word);
+    }
+
+    // 청개구리가 하는 말을 반환하는 함수
+    public static String frog(String word){
+        StringBuilder frogWord = new StringBuilder(word.length());
+
+        char[] frogChars = word.toCharArray();
+        for (char c: frogChars){
+            // 알파멧 외의 문자는 변환하지 않는다.
+            if(frogHash.get(c) != null)
+                frogWord.append(frogHash.get(c));
+            else
+                frogWord.append(c);
+        }
+
+        return frogWord.toString();
     }
 
     // 청개구리의 말을 번역할 해시맵
