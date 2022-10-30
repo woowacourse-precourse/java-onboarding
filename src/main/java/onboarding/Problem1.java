272357ed820634c6c66e2abece1d5fefac256b0e
jisoolee7101@gmail.com <phllo154@naver.com>
2022-10-30 16:33:56 +0900

    getPlace 페이지수 배열 저장

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,14 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        getPlace(335);
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    public static void getPlace(int pageNum)
+    {
+        int length = (int) Math.log10(pageNum);
+        int arr[] = new int[length+1];
+        for(int i = length; i > 0; i--){
+            arr[length-i] = (int) (pageNum / Math.pow(10, i));
+            arr[length-i+1] = pageNum = (int) (pageNum % Math.pow(10, i));
+        }
+
+        System.out.println(Arrays.toString(arr));
+        //return arr;
     }
 }
