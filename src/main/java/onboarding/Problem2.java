a5494ef25e33c875a17d3101df4bf4ec16b94c85
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-30 21:48:08 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String Str = cryptogram;
+
+        int idx1 = 0;
+        int idx2 = 0;
+
+        for (idx2 = 1; idx2 < cryptogram.length(); idx2++) {
+            if(Str.charAt(idx1) == Str.charAt(idx2)) {
+                String changedStr = Str.substring(0,idx1) + Str.substring(idx2+1);
+                Str = changedStr;
+                idx1 = 0;
+                idx2 = 1;
+                continue;
+            }
+            idx1++;
+            idx2++;
+        }
+        return Str;
     }
 }
