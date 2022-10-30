5e55bf34308524617bf209730c16ac4329d1fdff
yw824 <yongwoo25@gmail.com>
2022-10-31 03:39:50 +0900

    feat: [Problem2] 기능 요구 사항, 목록 정의
    - 기능 1: 순회 동작 함수
    	- 함수명: String Loop(String input)
    	- 문자열을 한 번 순회하면서 인덱스마다 다음 것과 비교
    	- 같으면 두 문자를 제거한 새로운 배열을 생성한 후
    	- 바로 앞의 인덱스로 이동하여 다시 비교
    	- 끝까지 도달할 때까지 최대한 한 번만 순회하도록 한다.
    - 기능 2: 같은 두 문자를 제거한 새로운 문자열(String) 반환
    	- 함수명: String removeRepeated(String str, int index)
    	- 입력받은 인덱스 앞까지 자른 문자열을 구하고
    	- 입력받은 인덱스와 그 다음 인덱스까지 제외하고
    	- 2번째 다음 인덱스부터 끝까지의 문자열을 구한다.
    	- 구한 두 문자열을 합쳐서 새로운 문자열을 생성 후 반환한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        // loop을 돌며 반복을 제외한 최종 문자열을 얻는다.
+        String answer = Loop(cryptogram);
+
+        // 얻은 최종 문자열을 리턴한다.
         return answer;
     }
+
+    // 순회 돌면서, 매 인덱스마다 다음 인덱스와 비교하여, 같으면 제거하고,
+    // 제거한 새로운 input으로 루프를 다시 순회한다.
+    // 같은 부분을 제거하면, 비교하는 인덱스 바로 앞의 인덱스로 이동하여 다시 비교한다.
+    // 끝까지 도달할 때까지 최대한 한 번만 순회하도록 한다.
+    public static String Loop(String input)
+    {
+        // implement
+        return input;
+    }
+
+    // 겹치는 해당 인덱스와 다음 인덱스까지의 내용을 삭제한
+    // 새로운 문자열을 반환한다.
+    public static String removeRepeated(String str, int index)
+    {
+        // implement
+        return str;
+    }
+
 }
