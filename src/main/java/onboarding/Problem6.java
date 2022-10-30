de34b7c3b821896df7675a8feb5222c6c4bdd0dc
YuHayeon <youha0930@gmail.com>
2022-10-30 20:29:01 +0900

    feat(Problem6): 입력 범위 예외처리
    입력의 길이가 1미만이거나 10000초과인 경우
    각각 form의 길이가 2가 아닌 경우

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = List.of("answer");
+		return answer;
+	}
+
+	static class InputValidator {
+
+		private static void isRightFormsRange(List<List<String>> forms) {
+			int formsSize = forms.size();
+			if (formsSize < 1 || formsSize > 10000) {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		private static boolean isRightFormRange(List<String> form) {
+			if (form.size() != 2) {
+				throw new IllegalArgumentException();
+			}
+			return true;
+		}
+	}
 }
+
