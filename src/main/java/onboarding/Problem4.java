3930170c9788b9077375c647fb057673b9ad2e4e
CMHyuk <alsgur9710@naver.com>
2022-10-30 15:24:56 +0900

    feat: 문자가 소문자일 경우 처리 로직

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,18 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             char alphabet = word.charAt(i);
+            isLowerCase(sb, alphabet);
             isBlank(sb, alphabet);
         }
 
         answer = sb.toString();
         return answer;
     }
+
+    private static void isLowerCase(StringBuilder sb, char alphabet) {
+        if (alphabet <= 'z' && alphabet >= 'a') {
+            alphabet = (char) ('z' - (alphabet - 'a'));
+            sb.append(alphabet);
+        }
+    }
+
     private static void isBlank(StringBuilder sb, char alphabet) {
         if (alphabet == ' ') {
             sb.append(' ');
