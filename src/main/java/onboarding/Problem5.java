646e9efa5be92cc22840f0ee01c0f63509a40853
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 21:08:47 +0900

    docs: Problem5 - 기능 구현 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * moneyCount 메서드 돈을 50000~1까지 나누어 몫을 저장한 List를 반환
+ * moneyCount 메서드 돈을 50000부터 나누어 몫을 저장후 값을 차감하며 1까지 카운트를 List로 변환
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
