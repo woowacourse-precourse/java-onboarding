186a9c9c71c6ac968855d32a6daf6d8da657b2b2
siwookim97 <tldn12gh@gmail.com>
2022-10-31 04:18:33 +0900

    feat : problem6) 이미 중복된 닉네임인지 확인하는 기능 추가
    TreeSet 전역변수에 email이 존재하는지 확인하는 기능
    확인을 요청하는 이메일을 매개변수로 받은 후
    이메일이 존재하면 true, 아니라면 false를 반환한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,10 +9,26 @@ public class Problem6 {
     /**
      * 중복된 문자열을 지닌 이메일을 전역변수 TreeSet에 추가하는 메서드
      *
-     * @param email
+     * @param email TreeSet에 추가하는 이메일
      */
     static void addToSet(String email) {
         emailSet.add(email);
+    }
+
+    /**
+     * TreeSet 전역변수에 email이 존재하는지 확인하는 메서드
+     *
+     * @param email 확인을 요청하는 이메일
+     * @return 이메일이 존재하면 true, 아니라면 false
+     */
+    static boolean hasEmail(String email) {
+        boolean isTrue = false;
+
+        if (emailSet.contains(email)) {
+            isTrue = true;
+        }
+
+        return isTrue;
     }
 
     public static List<String> solution(List<List<String>> forms) {
