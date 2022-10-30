5e314d4946b29e8ecca7cd1dd37de3d6880ae277
JunTaeINC <juntea3523@naver.com>
2022-10-30 22:58:01 +0900

    Problem 6 - 중복단어 체크 기능구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,52 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+
+	/*
+	 * 기능 구현 리스트 
+	 * 1. 이메일 형식은 'email.com'로 제한 / 11자 이상 20자 미만 
+	 * 2. 닉네임은 1자이상 20자 미만 / 두 글자 이상 문자가 반복 X 
+	 * 3. 이메일형식으로 출력 / 오름차순   
+	 */
+	
+	
+
+	public static List<String> solution(List<List<String>> forms) {
+		// 닉네임 단어 2str
+		List<String> nickTwoword = new ArrayList<>();
+		List<String> answer = new ArrayList<>();
+		// 중복 체크 위해 단어 2개씩 추가 
+		for (List<String> form : forms) {
+			String nickName = form.get(1);
+			for (int i = 0; i <= nickName.length() - 2; i++) {
+				nickTwoword.add(nickName.substring(i, i + 2));
+			}
+		}
+		
+		for (int j = 0; j < forms.size(); j++) {
+			String nickName = forms.get(j).get(1);
+			// 자신의 중복 단어 삭제  
+			for (int i = 0; i <= nickName.length() - 2; i++) {
+				nickTwoword.remove(nickName.substring(i, i + 2));
+			}
+			// 중복단어 있으면 정답에 넣기 
+			// 그리고 다시 자신의 단어 넣기   
+			for (int l = 0; l <= nickName.length() - 2; l++) {
+				if (nickTwoword.contains(nickName.substring(l, l + 2))) {
+					answer.add(forms.get(j).get(0));
+					nickTwoword.add(nickName.substring(l, l + 2));
+				} else {
+					nickTwoword.add(nickName.substring(l, l + 2));
+				}
+
+			}
+
+		}
+		Collections.sort(answer);
+		return answer;
+
+	}
 }
