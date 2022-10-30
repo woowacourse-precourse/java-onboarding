acced4b9e5ef5ab9950a269e012a9a0c5a63935b
Longstick0 <789456jang@naver.com>
2022-10-30 21:55:27 +0900

    feat: Problem6 문자열 분리, 찾기 로직 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        Set<String> a = new HashSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            String nickname = forms.get(i).get(1);
+
+            if (nickname.length() > 1) {
+                for (int j = 0; j < nickname.length() - 1; j++) {
+                    String nickSub = nickname.substring(j, j + 2);
+                    for (List<String> form : forms) {
+                        if (form.get(1).contains(nickSub) && !form.get(1).equals(nickname)) {
+                            a.add(form.get(0));
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(a);
     }
 }
