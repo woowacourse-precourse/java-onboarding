5d8c6f24e1adef758891d55dfb1279dd93cdafe0
beombu <sunil1369@bible.ac.kr>
2022-10-31 00:47:49 +0900

    Feat(P6) : 기능 목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,26 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    /**
+     * 닉네임의 중복 체크 기능
+     */
+    public static Boolean isDuplicationNickName(List<String> forms){
+        return false;
+    }
+
+    /**
+     * 이메일 예외처리 기능
+     */
+    public static Boolean isEmail(String email){
+        return false;
+    }
+
+    /**
+     * 닉네임 예외처리 기능
+     */
+    public static Boolean isNickName(String nickName){
+        return false;
+    }
+
 }
