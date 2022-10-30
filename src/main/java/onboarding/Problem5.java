c808f636c4f470d152dfaafbfe7c7d51b862614a
SeokHwan An <asok369258@naver.com>
2022-10-30 16:06:39 +0900

    feat [Problem5] : 주어진 money를 죄소한의 지폐 수와 동전 수의 리스트로 변환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,6 +8,7 @@ public class Problem5 {
 
     private static final int MONEY_MIN = 1;
     private static final int MONEY_MAX = 1000000;
+    private static final int[] MONEY_TYPE_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
@@ -15,6 +17,15 @@ public class Problem5 {
 
     public static boolean checkMoneyValueValid(int money) {
         return MONEY_MIN <= money && money <= MONEY_MAX;
+    }
+
+    public static List<Integer> chageMoneyToMoneyTypeCountList(int money) {
+        List<Integer> moneyTypeCountList = new ArrayList<>();
+        for (int moneyType : MONEY_TYPE_LIST) {
+            moneyTypeCountList.add(money / moneyType);
+            money %= moneyType;
+        }
+        return moneyTypeCountList;
     }
 
 }
