c9890134cdfb441b484e683a936a25091c0effed
Woojin Lee <dhkep03@gmail.com>
2022-10-30 12:03:07 +0900

    p1-feat: 구현할 메서드 설계

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int pobiMax = calculateMaxValue(pobi);
+        int crongMax = calculateMaxValue(crong);
+        int answer = compareMaxValue(pobiMax, crongMax);
         return answer;
     }
 }
