c4a1e22b2d1d2f307ce846daa654c6ef5a245e5e
채희찬 <eqholic1125@gmail.com>
2022-10-31 02:15:09 +0900

    feat: 페이지 유효성 체크 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,30 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private final int MAX_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
+
+    public boolean isRightPageNumbers(List<Integer> playersPages) {
+        final int leftPageNumber = playersPages.get(0);
+        final int rightPageNumber = playersPages.get(1);
+
+        if (leftPageNumber % 2 != 0) {  // 왼쪽 페이지가 짝수일 경우
+            return false;
+        }
+        else if (rightPageNumber > MAX_PAGE) {  // 최대 페이지가 넘어갈 경우
+            return false;
+        }
+        else if (rightPageNumber - leftPageNumber != 1) {    // 페이지가 연속되지 않는 경우
+            return false;
+        }
+
+        return true;
+    }
+
+
 }
