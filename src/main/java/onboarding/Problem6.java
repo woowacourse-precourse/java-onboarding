a38dcb2d71a68afcdf90b9a0813f5316c72e41d7
rt3310 <btac3310@gmail.com>
2022-10-30 17:20:19 +0900

    feat(Problem6): 두 글자 이상 문자열의 등장 횟수 체크 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static Map<String, Integer> getDuplicatedStringCounts(List<List<String>> forms) {
+        Map<String, Integer> stringCount = new HashMap<>();
+        for (List<String> crew : forms) {
+            countSubStringInName(stringCount, crew.get(1));
+        }
+        return stringCount;
+    }
+
+    private static void countSubStringInName(Map<String, Integer> stringCount, String name) {
+        for (int start = 0; start < name.length(); start++) {
+            for (int end = start + 1; end < name.length(); end++) {
+                putSubString(stringCount, name, start, end);
+            }
+        }
+    }
+
+    private static void putSubString(Map<String, Integer> stringCount, String name, int start, int end) {
+        stringCount.put(name.substring(start, end + 1), stringCount.getOrDefault(name.substring(start, end + 1), 0));
     }
 }
