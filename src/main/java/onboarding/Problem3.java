824168a7c6927a33ab92f0ea9788bf913e66e3f3
Woojin Lee <dhkep03@gmail.com>
2022-10-30 12:41:23 +0900

    p3-feat: 리스트에 담긴 자리수에서 3,6,9 개수를 세는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Problem3 {
     public static int solution(int number) {
@@ -14,6 +15,14 @@ public class Problem3 {
     static int count369ByNumber(int number) {
         ArrayList<String> arrayListOfNumber = convertNumberToArrayList(number);
         int count = countByArrayListOfNumber(arrayListOfNumber);
+        return count;
+    }
+
+    static int countByArrayListOfNumber(ArrayList<String> arrayListOfNumber) {
+        int count = 0;
+        count += Collections.frequency(arrayListOfNumber, "3");
+        count += Collections.frequency(arrayListOfNumber, "6");
+        count += Collections.frequency(arrayListOfNumber, "9");
         return count;
     }
 }
