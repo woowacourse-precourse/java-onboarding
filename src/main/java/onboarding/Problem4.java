d04b44112012128c4e2415a83ca1fef8a196f091
Korean: 양호준 / English: Hojun, Yang <hojune0303@naver.com>
2022-10-31 03:20:15 +0900

    Add p4 code

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,38 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] alphabet= new char[26];
+        alphabet[0] = 'A';
+        alphabet[1] = 'B';
+
+        for(int i = 0; i < alphabet.length; i++){
+            alphabet[i] = (char)('A' + i);
+        }
+
+        for(int i=0; i<word.length(); i++){
+            int j =0;       // j 변수를 for문 밖에서 사용하기 위함
+            for(j=0; j<alphabet.length; j++){
+                if(Character.isUpperCase(word.charAt(i))){
+                    if(word.charAt(i) == Character.toUpperCase(alphabet[j])){
+                        answer += Character.toUpperCase(alphabet[25-j]);
+                        break;
+                    }
+                }
+
+                else{
+                    if(word.charAt(i) == Character.toLowerCase(alphabet[j])){
+                        answer += Character.toLowerCase(alphabet[25-j]);
+                        break;
+                    }
+                }
+            }
+            // break를 못 만나서, j == alphabet.length 가 된 경우는 공백일 때
+            if (j == alphabet.length){
+                answer += " ";
+            }
+        }
+
         return answer;
     }
 }
