bb3c07ff3c071f58acdc65b40cce20b939bcff7b
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-31 00:09:34 +0900

    Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        int getClap = 0;
+
+        for (int i = 0; i <=number; i++) {
+            getClap = i;
+            while (getClap > 0) {
+                if (getClap % 10 == 3) {
+                    count++;
+                } else if (getClap % 10 == 6) {
+                    count++;
+                } else if (getClap % 10 == 9) {
+                    count++;
+                }
+                getClap /= 10;
+            }
+        }
+
+        return count;
     }
 }
