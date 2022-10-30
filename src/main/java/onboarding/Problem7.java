321772d1b1ee18744db3f94f7576400ac494f64a
matt <matt1235@naver.com>
2022-10-30 16:12:18 +0900

    feat: Problem7 (기능 목록2 - 점수 부여 기능) 구현 scoreMap에 각 사람의 획득한 점수를 저장하였다. 사용자와 함께 하는 친구 : 10점 사용자의 타임 라인 방문 : 1점 으로 계산.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,9 +7,13 @@ public class Problem7 {
         List<String> answer = new ArrayList<>();
         Set<String> friendSet = new HashSet<>(); // 전체 친구들이 담긴 Set
         Map<String, Set<String>> friendMap = new HashMap<>();  // 친구목록
+        Map<String, Integer> scoreMap = new HashMap<>(); // 점수 목록
 
         makeFriendSet(friends, friendSet);
         makeFriendMap(friends, friendSet, friendMap);
+        makeScoreMap(visitors, friendSet, friendMap, scoreMap);
+        // 사용자와 함께 하는 친구의 수마다 10점씩 추가
+        addScores(user, friendSet, friendMap, scoreMap);
         return answer;
     }
 
@@ -29,7 +33,7 @@ public class Problem7 {
         for (List<String> friend : friends) {
             Set<String> temp1 = friendMap.getOrDefault(friend.get(0), new HashSet<>());
             Set<String> temp2 = friendMap.getOrDefault(friend.get(1), new HashSet<>());
-            
+
             temp1.add(friend.get(1));
             temp2.add(friend.get(0));
 
@@ -37,5 +41,38 @@ public class Problem7 {
             friendMap.put(friend.get(1), temp2);
         }
     }
+
+    /**
+     * scoreMap 초기화, 방문자는 1점씩 추가
+     * */
+    private static void makeScoreMap(List<String> visitors, Set<String> friendSet, Map<String, Set<String>> friendMap, Map<String, Integer> scoreMap) {
+        // 점수 0으로 초기화
+        for (String s : friendSet) {
+            scoreMap.put(s,0);
+        }
+        for (String visitor : visitors){
+            if(!friendSet.contains(visitor)){
+                friendSet.add(visitor);  // 새로운 친구 추가
+                friendMap.put(visitor, new HashSet<>());
+            }
+            scoreMap.put(visitor, scoreMap.getOrDefault(visitor, 0)+1);
+        }
+    }
+
+    /**
+     * 사용자와 함께 아는 친구마다 10점씩 추가 */
+    private static void addScores(String user, Set<String> friendSet, Map<String, Set<String>> friendMap, Map<String, Integer> scoreMap) {
+        for (String userFriend : friendMap.get(user)) {  // userFriend : 사용자의 친구
+            for (String friend : friendSet) {
+                if(friend.equals(user)) continue;
+                for (String s1 : friendMap.get(friend)) {
+                    if(s1.equals(userFriend)){
+                        scoreMap.put(friend, scoreMap.getOrDefault(friend, 0)+10);
+                    }
+                }
+            }
+        }
+    }
+
 
 }
