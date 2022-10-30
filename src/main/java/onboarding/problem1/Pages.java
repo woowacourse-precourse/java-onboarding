b4e4beb1647068043f4e7701117cef8a602f1fce
Daeeun Ko <7273322@naver.com>
2022-10-30 22:42:35 +0900

    feat: get score

--- src/main/java/onboarding/problem1/Pages.java
+++ src/main/java/onboarding/problem1/Pages.java
@@ -22,5 +22,17 @@ public class Pages {
 
         if (leftPage.isEven() || rightPage.isOdd()) throw exception;
     }
+
+    private int getMaxSum() {
+        return Math.max(leftPage.getSum(), rightPage.getSum());
+    }
+
+    private int getMaxProduct() {
+        return Math.max(leftPage.getProduct(), rightPage.getProduct());
+    }
+
+    public int getScore() {
+        return Math.max(getMaxSum(), getMaxProduct());
+    }
 }
 
