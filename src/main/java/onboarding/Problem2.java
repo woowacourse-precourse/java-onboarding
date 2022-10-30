1a34e57296b097fcb2b54340a258de07d9364c54
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 20:56:05 +0900

    feat: Problem2 - decodingCryptogram 메서드 구현
    암호문을 받은뒤, 중복된 값을 제거하여 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 /**
  * decodingCryptogram 메서드 암호문을 받은뒤 중복된 값을 제거하여 반환
  */
@@ -9,5 +12,23 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static String decodingCryptogram(String cryptogram) {
+        Stack stack = new Stack<>();
+        char[] arrayOfCryptogram = cryptogram.toCharArray();
+        stack.push(arrayOfCryptogram[0]);
+
+        for (int i = 1; i < arrayOfCryptogram.length; i++) {
+            if (!stack.empty() && stack.peek().equals(arrayOfCryptogram[i])) stack.pop();
+            else if (arrayOfCryptogram[i - 1] == arrayOfCryptogram[i]) ;
+            else stack.push(arrayOfCryptogram[i]);
+        }
+
+        return stack.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining())
+                .toString();
+
     }
 }
