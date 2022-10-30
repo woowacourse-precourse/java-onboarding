21fc2390d45d1cfaee609ba91e41fd4ff8c8da30
Eunkeee <eunkeeeee@gmail.com>
2022-10-30 20:47:21 +0900

    [P5] refactor: change names

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,17 +6,17 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
 
-        List<Integer> answer = new ArrayList<Integer>();
+        List<Integer> answer = new ArrayList<>();
 
         int[] dividerArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
         int dividend = money;
-        for(int i = 0; i<dividerArr.length; i++){
-            int divider = dividerArr[i];
-            answer.add(dividend / divider);
-            int leftover = money % divider;
-            dividend = leftover;
+        for(int divider: dividerArr){
+            int quotient = dividend / divider;
+            answer.add(quotient);
+            int remainder = money % divider;
+            dividend = remainder;
         }
-        System.out.println(answer); // []
 
         return answer;
     }
