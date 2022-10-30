504d43c69105c2bce2cf0a6127e3827f23cc199f
JeongHunHui <deca3248@gmail.com>
2022-10-30 19:55:03 +0900

    [1주차 - Problem6] 정규식을 이용하여 닉네임 형식 검사 코드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,9 +6,15 @@ import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%+-]+@email[.]com");
+        Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%+-]*+@email[.]com");
+        Pattern nicknamePattern = Pattern.compile("^[ㄱ-ㅎ가-힣]*");
         for (List<String> data : forms) {
             if(!emailPattern.matcher(data.get(0)).matches()) {
+                System.out.println("올바르지 않은 이메일 형식입니다.");
+                return null;
+            }
+            if(!nicknamePattern.matcher(data.get(1)).matches()) {
+                System.out.println("닉네임은 한글만 가능합니다.");
                 return null;
             }
         }
