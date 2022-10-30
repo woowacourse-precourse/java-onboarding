13ee21dc5d016cf6987c5d15bb4d61ce2bb33144
yuntaeLee <leeyt1201@gmail.com>
2022-10-30 22:48:55 +0900

    feat(Problem6): 닉네임 중복 검증 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    static boolean validateNicknameDuplication(String input, String target) {
+        for (int left = 0; left < input.length() - 2; left++) {
+            int right = left + 2;
+            if (right > input.length()) right = input.length();
+
+            String inputSub = input.substring(left, right);
+            if (inputSub.length() < 2) return false;
+
+            if (target.contains(inputSub)) return true;
+        }
+
+        return false;
+    }
+
 }
