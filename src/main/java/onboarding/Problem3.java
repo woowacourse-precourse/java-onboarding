4cfcc736cbcf4e87737f7789b5628dc95041fed2
meunrud <y3a0522@gmail.com>
2022-10-31 02:07:19 +0900

    1. 1부터 number까지 자리수 나누기  2. 나눈 자리수를 배열에 담기
     / splitNum 메서드로 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,13 +6,6 @@ import java.util.Arrays;
 public class Problem3 {
     public static int solution(int number) {
 
-     /*[기능 구현]
-        1. 1부터 number까지 자리수 나누기
-        2. 나눈 자리수를 배열에 담기
-        3. 배열 값이 3,6,9에 해당하면 +1 카운트
-        4. 출력
-
-      */
         int answer = 0;
         String strNum = splitNum(number);
 
