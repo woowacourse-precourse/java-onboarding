58c53febbe0b8fca018ac5bccf7c5cdc8266d304
yoojin Jang <yoojinjangjang@gmail.com>
2022-10-30 18:38:39 +0900

    [feat] 출금할 돈의 화폐별 개수 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,12 +7,14 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        List<Integer> moneyList = List.of(50000,10000,5000,1000,500,100,50,10,1);
+        List<Integer> moneyList = List.of(50000,10000,5000,1000,500,100,50,10);
 
         for (Integer m : moneyList) {
             answer.add(money/m);
             money %= m;
         }
+
+        answer.add(money);
         return answer;
     }
 }
