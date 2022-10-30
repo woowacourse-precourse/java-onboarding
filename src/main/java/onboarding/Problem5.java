dc89e4ae1dbdd06776de6c60998023b484dd1999
EUNSH1 <tkdgus968@naver.com>
2022-10-31 01:56:38 +0900

    feat : [Problem5] 돈의 단위를 배열로 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,11 @@ import java.util.List;
  * 2. 금액을 배열로 나눠 리스트에 저장하는 함수
  */
 public class Problem5 {
+    /**
+     * 돈의 단위를 배열로 저장
+     */
+    static int[] amount = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+    
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
