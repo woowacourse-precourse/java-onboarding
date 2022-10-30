810721d99de751990dfac876098d6bf41bb3e922
JungHun Jang <ijj5258@naver.com>
2022-10-30 22:34:08 +0900

    Problem1 (feat) 유효성 체크 메소드 정의

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,22 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	static boolean invalid(List<Integer> list) {
+		/*List 길이가 2가 아니거나 페이지 다음장이 아닌 경우*/
+		if(list.size() != 2 || list.get(0)+1 != list.get(1)) { 
+			return true;
+		}
+		return false;
+	}
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+
+		if(invalid(pobi) || invalid(crong)) { 
+			answer = -1;
+			return answer;
+		}
+
+		return answer;
+	}
 }
