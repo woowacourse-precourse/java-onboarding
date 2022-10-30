329a9664cd92a5c6b61d9191a9884ead1220bce9
JeongHunHui <deca3248@gmail.com>
2022-10-30 21:18:40 +0900

    [1주차 - Problem7] 추천 점수를 저장할 딕셔너리 생성 및 방문자 점수 업데이트 코드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        // 친구 추천 점수를 저장할 딕셔너리
+        Map<String, Integer> friendScoreDict = new HashMap<>();
+        // 방문한 사람의 점수를 딕셔너리에 저장 및 업데이트
+        for (String visitor : visitors) {
+            int score = 1;
+            if(friendScoreDict.containsKey(visitor))
+                score += friendScoreDict.get(visitor);
+            friendScoreDict.put(visitor, score);
+        }
         return answer;
     }
 }
