ae9742aa3f07556686e5de541338f498ad082833
kki4504 <rhkddlf9876@gmail.com>
2022-10-30 20:31:51 +0900

    feat : problem1 기능구현
    - 승리 멤버 확인
    - 두 페이지 값중 가장 큰 값
    - 한 페이지 합 곱중 가장 큰 값
    - 홀 짝 순서 확인
    - 연결된 페이지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,43 +5,67 @@ import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
-  public static int solution(List<Integer> pobi, List<Integer> crong) {
-    int answer;
-    int pobiValue = memberHighestValue(pobi);
-    int crongValue = memberHighestValue(crong);
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer;
+        if (numException(pobi) && numException(crong)) {
+            int pobiValue = memberHighestValue(pobi);
+            int crongValue = memberHighestValue(crong);
 
-    answer = winMember(pobiValue, crongValue);
-    
-    return answer;
-  }
-
-  public static int winMember(int pobi, int crong) {
-    if (pobi > crong)
-      return 1;
-    else if (pobi == crong)
-      return 0;
-    else
-      return 2;
-  }
-
-  public static int memberHighestValue(List<Integer> member) {
-    List<Integer> ints = new ArrayList<>();
-    for (Integer num : member) {
-      ints.add(pageHighestValue(num));
+            answer = winMember(pobiValue, crongValue);
+        } else {
+            answer = -1;
+        }
+        return answer;
     }
-    return Collections.max(ints);
-  }
 
-  public static int pageHighestValue(Integer num) {
-    int i = num.intValue();
-    int sum = 0;
-    int multiply = 1;
-    while (i != 0) {
-      sum += i % 10;
-      multiply *= i % 10;
-
-      i /= 10;
+    public static int winMember(int pobi, int crong) {
+        if (pobi > crong)
+            return 1;
+        else if (pobi == crong)
+            return 0;
+        else
+            return 2;
     }
-    return sum > multiply ? sum : multiply;
-  }
+
+    public static int memberHighestValue(List<Integer> member) {
+        List<Integer> ints = new ArrayList<>();
+        for (Integer num : member) {
+            ints.add(pageHighestValue(num));
+        }
+        return Collections.max(ints);
+    }
+
+    public static int pageHighestValue(Integer num) {
+        int i = num.intValue();
+        int sum = 0;
+        int multiply = 1;
+        while (i != 0) {
+            sum += i % 10;
+            multiply *= i % 10;
+
+            i /= 10;
+        }
+        return sum > multiply ? sum : multiply;
+    }
+
+    public static boolean numException(List<Integer> member) {
+        int first = member.get(0);
+        int second = member.get(1);
+
+        if (checkEvenOdd(first, second)) {
+            if (second - first == 1)
+                return true;
+            else
+                return false;
+        } else
+            return false;
+    }
+
+    public static boolean checkEvenOdd(int firstNum, int secondNum) {
+        if (firstNum % 2 == 1 && secondNum % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
