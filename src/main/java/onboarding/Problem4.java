3375a86a7b89e530211eb9847c1cbb85872163a8
Eunkeee <eunkeeeee@gmail.com>
2022-10-30 20:29:32 +0900

    [P4] refactor: change names

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,32 +1,28 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Problem4 {
     public static String solution(String word) {
+
         String answer = "";
         ArrayList<String> answerArr = new ArrayList<>();
 
-        char[] chArr = word.toCharArray();
-        for(char oldChar: chArr){
-            System.out.println(oldChar);
-            int ascii = oldChar;
-            System.out.println(ascii);
-            if(ascii >= 65 && ascii <= 90){
+        char[] chArr = word.toCharArray(); // String -> char
+        for(char asciiValue: chArr){
+            int decimalValue = asciiValue; // char -> int
+            // convert Alphabets
+            if(decimalValue >= 65 && decimalValue <= 90){
                 // Alphabet Upper Case
-                ascii = 155 - ascii;
-            }else if(ascii >= 97 && ascii <= 122){
+                decimalValue = 155 - decimalValue;
+            }else if(decimalValue >= 97 && decimalValue <= 122){
                 // Alphabet Lower Case
-                ascii = 219 - ascii;
+                decimalValue = 219 - decimalValue;
             }
-            System.out.println(ascii);
-            char newChar = (char) ascii;
+            char newChar = (char) decimalValue;
             answerArr.add(Character.toString(newChar));
         }
 
-        System.out.println(answerArr);
         answer = String.join("", answerArr);
 
 
