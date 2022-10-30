7cb9b7f06ffaf97b43c54ef4844eb3277dfef95d
Sangwoo Shin <sangwoonoel@naver.com>
2022-10-30 20:30:45 +0900

    feat(Problem1): 두 페이지가 연속하지 않을 경우 예외처리(기능목록4)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(1)-pobi.get(0) != 1) {
+            return -1;
+        }
+        if (crong.get(1)-crong.get(0) != 1) {
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
     }
