eeadc716de80702e0bbd4228ecab0a26619d2bac
chaerim <qkrdbsk28@naver.com>
2022-10-30 22:36:29 +0900

    feat(problem1): add getMaxValue function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,4 +12,20 @@ class Problem1 {
 
         return Integer.MAX_VALUE;
     }
+
+    // 페이지의 각 자리 숫자의 합과 곱 중 더 큰 값을 반환하는 함수
+    public static int getMaxValue(int pageNumber) {
+        int sum = 0;
+        int mul = 1;
+
+        String stringPage = Integer.toString(pageNumber);
+
+        for(int i = 0; i < stringPage.length(); i++) {
+            int number = Integer.parseInt(String.valueOf(stringPage.charAt(i)));
+            sum += number;
+            mul *= number;
+        }
+
+        return Math.max(sum, mul);
+    }
 }
