05beab6d805c71f3a38ed02e77b03d6866dd3fc9
SeokHwan An <asok369258@naver.com>
2022-10-30 14:53:30 +0900

    feat [Problem4] : word 중 문자가 소문자 알파벳인지 판별하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,8 @@ public class Problem4 {
 
     private static final int WORD_MIN_LENGTH = 1;
     private static final int WORD_MAX_LENGTH = 1000;
+    private static final char LOWERCASE_A = 'a';
+    private static final char LOWERCASE_Z = 'z';
 
     public static String solution(String word) {
         String answer = "";
@@ -12,5 +14,9 @@ public class Problem4 {
 
     public static boolean checkWordLengthValid(String word) {
         return WORD_MIN_LENGTH <= word.length() && word.length() <= WORD_MAX_LENGTH;
+    }
+
+    public static boolean checkLowercaseAlphabet(char letter) {
+        return LOWERCASE_A <= letter && letter <= LOWERCASE_Z;
     }
 }
