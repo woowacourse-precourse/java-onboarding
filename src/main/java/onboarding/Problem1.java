face0cf0672f63bf6dc60467f7e55edf02b72e5d
Easy-Water71 <phllo154@naver.com>
2022-10-30 15:55:42 +0900

    test

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
