ae3dd890eda1742395e74551ad86dfeb301082c2
yujaemin <woawoa0513@gmail.com>
2022-10-31 01:49:47 +0900

    refactor: [문제 7] 함께 아는 친구 점수 계산 메소드 리팩토링
    기존 solution 함수에 작성했던 코드 addScoreByRelation 메소드로 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    static String mainUser;
     static HashMap<String, String> friendsOfUser = new HashMap<>();
     static HashMap<String, Integer> scoreOfUser = new HashMap<>();
 
@@ -13,7 +14,18 @@ public class Problem7 {
         return friendString.split("/");
     }
 
+    public static void addScoreByRelation() {
+        String[] friendArr = getFriends(mainUser);
+        for (String friend : friendArr) {
+            String[] friendOfFriendArr = getFriends(friend);
+            for (String friendOfFriend : friendOfFriendArr) {
+                scoreOfUser.put(friendOfFriend, scoreOfUser.getOrDefault(friendOfFriend, 0) + 10);
+            }
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        mainUser = user;
 
         for (List<String> friend : friends) {
             String user1 = friend.get(0);
@@ -23,13 +35,7 @@ public class Problem7 {
             friendsOfUser.put(user2, friendsOfUser.getOrDefault(user2, "") + user1 + "/");
         }
 
-        String[] friendArr = getFriends(user);
-        for (String friend : friendArr) {
-            String[] friendOfFriendArr = getFriends(friend);
-            for (String friendOfFriend : friendOfFriendArr) {
-                scoreOfUser.put(friendOfFriend, scoreOfUser.getOrDefault(friendOfFriend, 0) + 10);
-            }
-        }
+        addScoreByRelation();
 
         System.out.println(scoreOfUser);
 
