abed65a7b3dcbb79b97ed3f5918c74dd165c29cb
matt <matt1235@naver.com>
2022-10-30 15:56:26 +0900

    feat: Problem7 (기능 목록1 - 각 사람의 친구 목록을 구하는 기능) 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,41 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        Set<String> friendSet = new HashSet<>(); // 전체 친구들이 담긴 Set
+        Map<String, Set<String>> friendMap = new HashMap<>();  // 친구목록
+
+        makeFriendSet(friends, friendSet);
+        makeFriendMap(friends, friendSet, friendMap);
         return answer;
     }
+
+    /**
+     * friendSet 초기화 : 기능 목록 1 */
+    private static void makeFriendSet(List<List<String>> friends, Set<String> friendSet) {
+        for (List<String> friend : friends) {
+            friendSet.add(friend.get(0));
+            friendSet.add(friend.get(1));
+        }
+    }
+
+    /**
+     * friendMap 초기화 : 기능 목록 1
+     * * */
+    private static void makeFriendMap(List<List<String>> friends, Set<String> friendSet, Map<String, Set<String>> friendMap) {
+        for (List<String> friend : friends) {
+            Set<String> temp1 = friendMap.getOrDefault(friend.get(0), new HashSet<>());
+            Set<String> temp2 = friendMap.getOrDefault(friend.get(1), new HashSet<>());
+            
+            temp1.add(friend.get(1));
+            temp2.add(friend.get(0));
+
+            friendMap.put(friend.get(0), temp1);
+            friendMap.put(friend.get(1), temp2);
+        }
+    }
+
 }
