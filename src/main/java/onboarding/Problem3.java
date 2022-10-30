b8ed5926bb815de38c4fe2360cc93b2b31cfafa8
5onchangwoo <scw9410@gmail.com>
2022-10-30 23:26:33 +0900

    problem3/메인 로직 추가 (해당 숫자까지의 모든 박수 횟수)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,8 +6,12 @@ import java.util.Arrays;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i < number + 1; i++) {
+            answer += countClapOfNum(i);
+        }
         return answer;
     }
+
     // 해당 수에서의 박수 횟수 구하기
     public static int countClapOfNum(int number) {
         int count = 0;
