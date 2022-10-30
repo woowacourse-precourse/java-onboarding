369af3790941a60833d8ffe16970b758bea13326
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-10-30 16:46:08 +0900

    Add: Problem3 Code

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        List<Character> list = Arrays.asList('3', '6', '9');
+
+        return countTotal(number, list);
+    }
+
+    // 1부터 number 까지 총 3,6,9 개수 세는 함수
+    private static int countTotal(Integer num, List<Character> list) {
+        int total = 0;
+
+        // 1부터 number 까지 3,6,9 횟수 세기
+        for (int k = 1; k <= num; k++) {
+            total += count369(k, list);
+        }
+        return total;
+    }
+
+    // 각 자릿수에 들어있는 3,6,9 개수 세는 함수
+    private static int count369(int k, List<Character> list) {
+        Integer num = k;
+        String strNum = num.toString();
+        int count = 0;
+
+        for (int i=0;i<strNum.length();i++) {
+            if (list.contains(strNum.charAt(i))) {
+                count += 1;
+            }
+        }
+        return count;
     }
 }
