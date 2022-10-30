8966da25bc182926ece372fe7d7415dfe4bd3a3e
sangil <gsl0505@naver.com>
2022-10-30 19:15:54 +0900

    feat(PROBLEM2): 암호문에 대한 예외사항 확인 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,9 +2,15 @@ package onboarding;
 
 public class Problem2 {
 	private static final String REPEAT_REGEX = "{2,}";
+	private static final String LOWERCASE_REGEX = "^[a-z]*$";
 	private static final String REMOVE_BLANK = "";
+	private static final int MIN_CRYPTOGRAM_LENGTH = 1;
+	private static final int MAX_CRYPTOGRAM_LENGTH = 1000;
 
 	public static String solution(String cryptogram) {
+		if (!checkCryptogramForm(cryptogram)) {
+			throw new IllegalArgumentException();
+		}
 		String plainText = cryptogram;
 		int cryptogramLength;
 		do {
@@ -12,6 +18,18 @@ public class Problem2 {
 			plainText = removeString(plainText);
 		} while (checkMatching(cryptogramLength, plainText.length()));
 		return plainText;
+	}
+
+	private static boolean checkCryptogramForm(String cryptogram) {
+		return isLowerCase(cryptogram) && checkCryptogramLength(cryptogram);
+	}
+
+	private static boolean isLowerCase(String cryptogram) {
+		return cryptogram.matches(LOWERCASE_REGEX);
+	}
+
+	private static boolean checkCryptogramLength(String cryptogram) {
+		return cryptogram.length() >= MIN_CRYPTOGRAM_LENGTH && cryptogram.length() <= MAX_CRYPTOGRAM_LENGTH;
 	}
 
 	private static String removeString(String plainText) {
