7c0ba61c2825999cd98a332aa66dc95e4153704d
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 23:26:39 +0900

    feat:[Problem3] 조건체크 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,9 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer =count369(number);
+        int answer=-1;
+        if(checkLimit(number)) answer =count369(number);
         return answer;
     }
+
+    /*
+    문제 조건 사항 체크
+     */
+    private static boolean checkLimit(int num){
+        //숫자 1부터 10000까지
+        if(num>=1&&num<=10000) return true;
+        return false;
+    }
+    /*
+    숫자에서 3,6,9, 숫자 세기
+     */
     private static int count369(int num){
         int cnt=0;
         for(int i=1;i<=num;i++){
