e12473c08885a17456b07a58bc54c730165d17bc
ddubidubap <park62000@gmail.com>
2022-10-31 00:35:15 +0900

    refactor: Problem 2 clean code

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,14 +5,15 @@ public class Problem2 {
         기능 요구 사항 목록
         1. 반복되는 문자의 인덱스를 찾는 함수
         2. 반복되는 문자를 제거하는 함수
-        3. solution (반복문으로 문자 제거)
+        3. solution 함수
     */
 
     // 반복되는 문자의 인덱스를 찾는 함수
-    private static Integer repeatedIndex(String cryptogram) {
+    private static Integer findRepetition(String cryptogram) {
         for (int i = 1; i < cryptogram.length(); i++) {
-            if (cryptogram.charAt(i) == cryptogram.charAt(i-1))
-                return i-1;
+            char firstLetter = cryptogram.charAt(i-1);
+            char secondLetter = cryptogram.charAt(i);
+            if (firstLetter == secondLetter) return i-1;
         }
         return null;
     }
@@ -26,7 +27,7 @@ public class Problem2 {
 
     public static String solution(String cryptogram) {
         Integer index;
-        while ((index = repeatedIndex(cryptogram)) != null)
+        while ((index = findRepetition(cryptogram)) != null)
             cryptogram = removeRepetition(cryptogram, index);
 
         return cryptogram;
