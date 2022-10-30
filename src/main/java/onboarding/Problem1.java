8b7ea52a5ccddc1eb2d6b69920d96f27f9def39b
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-30 20:36:47 +0900

    docs: Problem1 기능 구현 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,14 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * isCorrectPages 메서드 입력받은 페이지값이 올바른지 확인
+ * getSumOfPages 메서드 페이지의 각자리수의 합을 반환
+ * getMultiplyOfPages 메서드 페이지의 각자리수의 곱을 반환
+ * getMaxOfPages 메서드 페이지의 각자리수 합과 곱 중 더 큰값을 반환
+ * compareOfResult 메서드 값을 비교하여 승자를 반환
+ */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
