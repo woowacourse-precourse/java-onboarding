9918ffc54da84d01956ddb718aefe82fbfbadaaa
yun8565 <yunyun8565@naver.com>
2022-10-30 22:38:01 +0900

    feat(P2): 연속하는 중복 문자 확인 기능 추가
    정규식을 사용해 Pattern.matches로 연속되는 문자가 있는지 확인하려고 했지만 정규식에 대한 이해가 부족했던 탓인지 원하는 기능 구현을 못했다..
    
    결국 간단한 for문으로 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while(hasConsecutiveChars(cryptogram)){
+
+        }
+        return cryptogram;
+    }
+
+    public static boolean hasConsecutiveChars(String cryptogram) {
+        for(int i = 0; i < cryptogram.length() - 1; i++) {
+            if(cryptogram.charAt(i) == cryptogram.charAt(i+1))
+                return true;
+        }
+        return false;
     }
 }
