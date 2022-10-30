99e225852f68ddb0851a496447310f395b170cdf
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-10-30 15:37:49 +0900

    refactor: Problem7 메소드 내 변수 i를 변수 index로 변환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,9 +9,9 @@ public class Problem7 {
     }
     private static Set<String> findUserFriend(String user, List<List<String>> friends) {
         Set<String> friend = new HashSet<>();
-        for(int i=0;i<friends.size();i++){
-            String leftPerson = friends.get(i).get(0);
-            String rightPerson = friends.get(i).get(1);
+        for(int index=0;index<friends.size();index++){
+            String leftPerson = friends.get(index).get(0);
+            String rightPerson = friends.get(index).get(1);
             if(leftPerson.equals(user))friend.add(rightPerson);
             if(rightPerson.equals(user))friend.add(leftPerson);
         }
@@ -20,9 +20,9 @@ public class Problem7 {
     private static Map<String, Integer> withUserFriend(String user, List<List<String>> friends, List<String> visitors) {
         Map<String,Integer> friendRecommend = new HashMap<>();
         Set<String> userFriend = findUserFriend(user, friends);
-        for(int i=0;i<friends.size();i++){
-            String leftPerson = friends.get(i).get(0);
-            String rightPerson = friends.get(i).get(1);
+        for(int index=0;index<friends.size();index++){
+            String leftPerson = friends.get(index).get(0);
+            String rightPerson = friends.get(index).get(1);
             if(isAlreadyFriend(leftPerson,userFriend))friendRecommend.put(rightPerson,friendRecommend.getOrDefault(rightPerson,0)+10);
             if(isAlreadyFriend(rightPerson,userFriend))friendRecommend.put(leftPerson,friendRecommend.getOrDefault(leftPerson,0)+10);
         }
@@ -34,8 +34,8 @@ public class Problem7 {
         else return false;
     }
     private static void visitorFindRecommend(List<String> visitors, Map<String, Integer> friendRecommend, Set<String> userFriend) {
-        for(int i=0;i<visitors.size();i++){
-            String visitor = visitors.get(i);
+        for(int index=0;index<visitors.size();index++){
+            String visitor = visitors.get(index);
             if(!isAlreadyFriend(visitor, userFriend))friendRecommend.put(visitor,friendRecommend.getOrDefault(visitor,0)+1);
         }
     }
@@ -65,8 +65,8 @@ public class Problem7 {
 
     private static List<String> recommendList(String user, List<List<String>> friends, List<String> visitors) {
         List<Person> sortList = MapToListAndSort(user, friends, visitors);
-        List<String> str = new ArrayList<>();
-        for(int index=0;index<sortList.size();index++)str.add(sortList.get(index).getName());
-        return str;
+        List<String> recommend = new ArrayList<>();
+        for(int index=0;index<sortList.size();index++)recommend.add(sortList.get(index).getName());
+        return recommend;
     }
 }
