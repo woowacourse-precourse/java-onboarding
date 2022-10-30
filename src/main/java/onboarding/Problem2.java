8860e69175d7fb8befbefaed72480e280a5055c8
java-saeng <gusdn85554@naver.com>
2022-10-30 21:11:46 +0900

    feat : Problem2 중복 확인 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+
+        while (hasDuplicated(cryptogram)) {
+            //중복 제거 로직
+        }
+
+
         return answer;
+    }
+
+    private static boolean hasDuplicated(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
