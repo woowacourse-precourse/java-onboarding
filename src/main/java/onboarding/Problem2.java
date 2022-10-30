e8c7abe0f287d8b478c5729d37d5b8c4e9611efd
chaerim <qkrdbsk28@naver.com>
2022-10-30 23:09:48 +0900

    feat(problem2): 검사 후 중복 문자 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String value = cryptogram;
+
+        for(int i = 1; i < value.length(); i++){
+            if(value.charAt(i-1) == value.charAt(i)) {
+                value = value.substring(0, i-1) + value.substring(i+1);
+                i = 0;
+            }
+        }
+
+        return value;
     }
 }
