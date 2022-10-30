7d989bd81096f48f510a8e5adb1df2ce51e88e3d
kwYoohae <gocksdl7979@gmail.com>
2022-10-30 23:29:37 +0900

    feat(Problem7.java): user의 친구들이 누가 있는지 가져오는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static Set<String> friendList = new HashSet<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
+
+    public static void findFriend(String user, List<List<String>> friends) {
+        for (List<String> friendData : friends) {
+            if(user.equals(friendData.get(0)) == true) {
+                friendList.add(friendData.get(1));
+            }
+            if(user.equals(friendData.get(1)) == true) {
+                friendList.add(friendData.get(0));
+            }
+        }
+    }
+
 }
