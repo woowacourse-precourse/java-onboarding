bc682279b8ac56f57b9c24d59e9bbfaf1e278adf
EUNSH1 <tkdgus968@naver.com>
2022-10-31 02:48:48 +0900

    feat : [Problem7] 기능 사항 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 1.친구목록을 통해 사용자와 함께아는친구 찾기
+ * 2.함께 아는 친구의 아이디와 점수를 TreeMap에 저장하는 함수
+ * 3.타임라인의 방문자들을 TreeMap에 저장하는 함수
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
