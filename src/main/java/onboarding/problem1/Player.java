bfec503a8a1242885bea3cee1523264caa160da6
apptie <appti@naver.com>
2022-10-31 01:05:31 +0900

    comment: 문제 1 클래스 주석 추가

--- src/main/java/onboarding/problem1/Player.java
+++ src/main/java/onboarding/problem1/Player.java
@@ -5,26 +5,52 @@ import static onboarding.common.consts.CommonDigitNumberConst.DECIMAL_UNIT_VALUE
 import static onboarding.problem1.consts.CalculateGameScoreConst.ADD_SCORE_DEFAULT_VALUE;
 import static onboarding.problem1.consts.CalculateGameScoreConst.MULTIPLY_SCORE_DEFAULT_VALUE;
 
+/**
+ * 페이지 게임에 참여하는 플레이어의 정보를 저장하는 클래스
+ */
 public class Player {
 
     private final int leftPageNumber;
     private final int rightPageNumber;
     private final int playerScore;
 
+    /**
+     * 플레이어의 페이지 정보를 초기화하고 플레이어 점수를 계산하는 생성자
+     *
+     * @param leftPageNumber 플레이어의 검증된 왼쪽 페이지
+     * @param rightPageNumber 플레이어의 검증된 오른쪽 페이지
+     */
     public Player(int leftPageNumber, int rightPageNumber) {
         this.leftPageNumber = leftPageNumber;
         this.rightPageNumber = rightPageNumber;
         playerScore = initPlayerScore();
     }
 
+    /**
+     * 플레이어 점수를 계산해 반환
+     *
+     * @return 모든 페이지 자릿수를 모두 곱한 값과 더한 값 중 가장 큰 값
+     */
     private int initPlayerScore() {
         return Math.max(calculatePageScore(leftPageNumber), calculatePageScore(rightPageNumber));
     }
 
+    /**
+     * 주어진 페이지의 점수를 계산하는 메소드
+     *
+     * @param pageNumber 점수를 계산할 페이지
+     * @return 주어진 페이지의 자릿수를 모두 곱한 값과 더한 값 중 가장 큰 값
+     */
     private int calculatePageScore(int pageNumber) {
         return Math.max(calculateAddPageScore(pageNumber), calculateMultiplyPageScore(pageNumber));
     }
 
+    /**
+     * 주어진 페이지의 자릿수를 모두 더하는 메소드
+     *
+     * @param pageNumber 계산할 페이지
+     * @return 주어진 페이지의 자릿수를 모두 더한 값
+     */
     private int calculateAddPageScore(int pageNumber) {
         int addScore = ADD_SCORE_DEFAULT_VALUE;
 
@@ -35,6 +61,12 @@ public class Player {
         return addScore;
     }
 
+    /**
+     * 주어진 페이지의 자릿수를 모두 곱하는 메소드
+     *
+     * @param pageNumber 계산할 페이지
+     * @return 주어진 페이지의 자릿수를 모두 곱한 값
+     */
     private int calculateMultiplyPageScore(int pageNumber) {
         int multiplyScore = MULTIPLY_SCORE_DEFAULT_VALUE;
 
@@ -45,6 +77,11 @@ public class Player {
         return multiplyScore;
     }
 
+    /**
+     * 플레이어의 점수를 반환하는 메소드
+     *
+     * @return 플레이어의 점수
+     */
     public int getPlayerScore() {
         return playerScore;
     }
