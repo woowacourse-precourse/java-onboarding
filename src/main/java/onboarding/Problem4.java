ea47aac4aed90c14ab126594da724b1a050df74c
tgoddessana <twicegoddessana1229@gmail.com>
2022-10-31 02:50:59 +0900

    solve : Problem4 풀이

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+
 public class Problem4 {
+
+    public static char getTreeFrogChar(char character) {
+        int number = 0;
+        if (Character.isUpperCase(character)) {
+            number = 155 - (int) character;
+        } else if (Character.isLowerCase(character)) {
+            number = 219 - (int) character;
+        }
+        char result = (char) number;
+        return result;
+    }
+
     public static String solution(String word) {
         String answer = "";
-        return answer;
+        char[] array = word.toCharArray();
+        for (int j = 0; j < array.length; j++) {
+            array[j] = getTreeFrogChar(array[j]);
+            int a = (int) array[j];
+            System.out.println(a);
+            answer += Character.toString(array[j]);
+        }
+
+        return answer.replaceAll("\\u0000", " ");
     }
 }
