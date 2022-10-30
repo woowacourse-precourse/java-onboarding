6c6074a67a7419336e4b39f669ab67b5f70dda8f
JongDo737 <jongmin373@gmail.com>
2022-10-31 00:19:14 +0900

    problem7 친구추천 목록과 점수계산 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,48 +5,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
 
-        Map<String,Integer> scoreMap = new HashMap<>();  // 추천친구 목록, 점수 MAP
-        List<String> totalList = commendList(user,friends,visitors);
-        for(int i=0; i< totalList.size(); i++) {
-            scoreMap.put(totalList.get(i),0);
-        }
-        // 친구목록에서 10점씩
-        for(int i=0; i< friends.size(); i++) {
-            for(int j=0; j<friends.get(j).size(); j++) {
-                if (scoreMap.containsKey(friends.get(i).get(j))){
-                    scoreMap.replace(friends.get(i).get(j), scoreMap.get(friends.get(i).get(j))+10); // 친구당 10점 추가
-                }
-            }
-        }
-        // 방문자에서 1점씩
-        for(int i=0; i<visitors.size();i++) {
-            if(scoreMap.containsKey(visitors.get(i))) {
-                scoreMap.replace(visitors.get(i), scoreMap.get(visitors.get(i))+1); // 친구당 10점 추가
-            }
-        }
-        // Map.Entry 리스트 작성
-        List<Map.Entry<String, Integer>> list_entries = new ArrayList<Map.Entry<String, Integer>>(scoreMap.entrySet());
-
-        // 비교함수 Comparator를 사용하여 오름차순으로 정렬
-        Collections.sort(list_entries, new Comparator<Map.Entry<String, Integer>>() {
-            // compare로 값을 비교
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                // 오름 차순 정렬
-                return o2.getValue().compareTo(o1.getValue());
-            }
-        });
-
-//        System.out.println("오름 차순 정렬");
-        // 결과 출력 최대 5개, 0이면 출력 x
-        for(int i=0; i< list_entries.size() && i< 5; i++){
-            if(list_entries.get(i).getValue()!=0){
-                answer.add(list_entries.get(i).getKey());
-            }
-        }
-
-
-
+        
         return answer;
     }
     public static List<String> commendList(String user, List<List<String>> friends, List<String> visitors){
@@ -77,5 +36,30 @@ public class Problem7 {
         totalFriends.remove(user);      // 자신도 친구추천 목록에서 제거
         List<String> totalList = new ArrayList<>(totalFriends);
         return totalList;
+    }
+    // 추천친구 목록, 점수 Map 생성 메서드
+    public Map<String,Integer> friendsAndScore(String user, List<List<String>> friends, List<String> visitors){
+        List<String> totalList = commendList(user,friends,visitors);        // 친구추천 리스트
+        Map<String,Integer> scoreMap = new HashMap<>();  // 추천친구 목록, 점수 MAP
+
+        for(int i=0; i< totalList.size(); i++) {
+            scoreMap.put(totalList.get(i),0);
+        }
+        // 친구목록에서 10점씩
+        for(int i=0; i< friends.size(); i++) {
+            for(int j=0; j<friends.get(j).size(); j++) {
+                if (scoreMap.containsKey(friends.get(i).get(j))){
+                    scoreMap.replace(friends.get(i).get(j), scoreMap.get(friends.get(i).get(j))+10); // 친구당 10점 추가
+                }
+            }
+        }
+        // 방문자에서 1점씩
+        for(int i=0; i<visitors.size();i++) {
+            if(scoreMap.containsKey(visitors.get(i))) {
+                scoreMap.replace(visitors.get(i), scoreMap.get(visitors.get(i))+1); // 친구당 10점 추가
+            }
+        }
+        return scoreMap;
+
     }
 }
