258249a76e09b691c6919efb335d4b999ce5d898
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-30 17:27:57 +0900

    feat: Add Problem1 Exception Checker
    - 문제 1번 예외 검사 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(isException(pobi) || isException(crong)) return -1;
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean isException(List<Integer> pages){
+        if(pages.size() != 2) return true;
+        if(pages.get(0) < 1 || pages.get(1) < 1) return true;
+        if(pages.get(0) > 400 || pages.get(1) > 400) return true;
+        //연속된 페이지 검사
+        if(pages.get(0) % 2 == 1 && pages.get(1) % 2 == 0) return true;
+        if((pages.get(1) - pages.get(0)) != 1) return true;
+        return false;
     }
 }
