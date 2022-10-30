e6217a9a98cba0f793357413ac0c69daff3fcbdf
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-30 21:42:16 +0900

    feat:[Problem4] 단어 알파벳 거꾸로 만들기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,23 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    /*
+    단어 알파벳 거꾸로 만들기
+     */
+    private static String reverseWord(String str){
+        char ch[]=str.toCharArray();
+        for(int i=0;i<str.length();i++){
+            if(Character.isAlphabetic(ch[i])){
+                if(ch[i]>='A'&&ch[i]<='Z'){
+                    ch[i]=(char)('Z'-ch[i]+'A');
+                }else if(ch[i]>='a'&&ch[i]<='z'){
+                    ch[i]=(char)('z'-ch[i]+'a');
+                }else{
+                    ch[i]=ch[i];
+                }
+            }
+            ch[i]=ch[i];
+        }
+        return String.copyValueOf(ch);
+    }
 }
