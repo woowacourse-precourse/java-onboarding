8c21118a766a750e68e33fc1d6a316b3c40e9b21
Heesom <heesom.hs@gmail.com>
2022-10-30 14:25:36 +0900

    Problem7 : getVisitScore 메소드 추가
    방문자 점수 계산하는 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public  static Map<String,Integer> getVisitScore(List<String> visitors, String user) {
+        Map<String, Integer> visitScore = new TreeMap<>();
+        for (String visitor : visitors) {
+            visitScore.put(visitor, 1);
+        }
+        return  visitScore;
+    }
+
+
+
 }
