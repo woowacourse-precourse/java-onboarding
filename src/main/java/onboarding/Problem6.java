ab204094eda3d811b3a472a9a669a94bd554ae91
HiiWee <wpdlzhf159@gmail.com>
2022-10-31 03:30:29 +0900

    Feat problem6: 닉네임을 연속 2글자로 자를 수 있는 모든 경우 구하기 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,7 +10,20 @@ public class Problem6 {
     static HashMap<String, Integer> twoLetterCountMap = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
+        List<String> separatedNickname = new ArrayList<>();
+        for (List<String> crew : forms) {
+            separatedNickname.addAll(getSeparateTwoConsecutiveLetters(crew.get(NICKNAME)));
+        }
 
         return new ArrayList<>();
+    }
+
+    private static List<String> getSeparateTwoConsecutiveLetters(String nickname) {
+        List<String> separatedNickname = new ArrayList<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String twoLetters = nickname.substring(i, i + 2);
+            separatedNickname.add(twoLetters);
+        }
+        return separatedNickname;
     }
 }
