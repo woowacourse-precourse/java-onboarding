4048163f332f99d8fe8bc4049b855b05f703ae10
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-10-31 02:09:11 +0900

    problem3 test completed

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,35 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int countThree(int number){
+
+        int[] num = new int[5];
+        int decimal = 1000;
+        int sum = 0;
+        num[0] = number / 10000;
+        num[4] = number % 10;
+
+        for(int i=0; i<3; i++){
+            num[i] = (number % (decimal*10)) / decimal;
+            decimal /= 10;
+        }
+
+        for(int i=0; i<5; i++){
+            if(num[i] == 3 || num[i] == 6 || num[i] == 9){
+                sum++;
+            }
+        }
+        return sum;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        int count = 0; int three = 0;
+        for(int i=1; i<=number; i++){
+            three = countThree(i);
+            count += three;
+        }
+        return count;
     }
 }
