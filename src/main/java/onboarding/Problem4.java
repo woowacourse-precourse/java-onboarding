e7aaa097d7ec43fd9f39f31760774979a1c7d839
kimdeokyoung <dudejrss@naver.com>
2022-10-30 17:44:04 +0900

    [Edit] README.md : add Funtion List about Problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+    public static char chungGaeGuRi(char a){
+        return a;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
