47045d4beb1ad596629fd5ca55325f0b69cc0379
Sieun Park <psieun0606@gmail.com>
2022-10-30 15:30:01 +0900

    feat: problem3 3,6,9 포함 개수 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,10 +9,27 @@ public class Problem3 {
         return numCharArray;
     }
 
+    static int checkThreeSixNine(char[] numCharArray) {
+        int count = 0;
+
+        for (char x: numCharArray) {
+            if (x == '3' || x == '6' || x == '9') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        int i = 1;
 
-        convertToCharArray(number);
+        while (i <= number) {
+            char[] numCharArray = convertToCharArray(i);
+            answer += checkThreeSixNine(numCharArray);
+            i++;
+        }
 
         return answer;
     }
