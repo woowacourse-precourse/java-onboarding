5aeee4550250929fd7c541bf2d8e9b43d3985be0
Noah <shgustmd6865@naver.com>
2022-10-30 22:30:57 +0900

    Feat : 친구의 친구 찾기 기능 추가
    백트랙킹을 이용하여 친구의 친구(함께 아는 친구)를 찾는 기능입니다
    친구의 친구를 찾기 위해 depth 변수를 이용하여 depth가 2인 경우 친구의 친구로 판단합니다
    친구 관계 정보 리스트에서 자신을 제외한 친구에 대한 인덱스를 가져오기 위해
    1에서 자신의 인덱스를 빼서 친구의 인덱스를 가져옵니다
    indexOf()의 특성에 따라 자신이 들어 있지 않으면 -1을 반환함으로
    찾은 친구의 인덱스가 2인 경우는 무시합니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,38 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Map<String, Integer> scoreBoard = new HashMap<>();
+
+        findAcquaintance(0, user, user, friends, scoreBoard);
+
+        System.out.println(scoreBoard);
+
         return answer;
+    }
+
+    public static void findAcquaintance(int depth, String targetUser, String findUser, List<List<String>> friends, Map<String, Integer> scoreBoard) {
+
+        if (depth == 2) {
+            scoreBoard.put(findUser, scoreBoard.getOrDefault(findUser, 0) + 10);
+            return;
+        }
+
+        for (List<String> friend : friends) {
+            int foundFriendIdx = 1 - friend.indexOf(findUser);
+            if (foundFriendIdx == 2)
+                continue;
+            else {
+                if (!targetUser.equals(friend.get(foundFriendIdx)))
+                    findAcquaintance(depth + 1, targetUser, friend.get(foundFriendIdx), friends, scoreBoard);
+            }
+        }
     }
 }
