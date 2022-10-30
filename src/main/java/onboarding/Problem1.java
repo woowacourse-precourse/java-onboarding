38dabf1e2c60108b8d74ddc3d6a702f901b252bd
benjaminuj <chosj1526@gmail.com>
2022-10-30 23:42:50 +0900

    feat: 포비와 크롱의 왼쪽, 오른쪽 페이지별 결과 구하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,15 +11,19 @@ class Problem1 {
         int bookLastPage = 400;
 
         try {
+            int[] pobisPageByPageResult = new int[2];
+            int[] crongsPageByPageResult = new int[2];
+
             for(int index = 0; index <2 ; index ++) {
                 if ( bookFirstPage <= pobi.get(index)  &&  pobi.get(index) <= bookLastPage ) {
                     ArrayList<Integer> pobisEachDigit = getEachDigit(pobi.get(index));
+                    pobisPageByPageResult[index] = calculationMyScore(pobisEachDigit);
 
                 }
 
                 if ( bookFirstPage <= crong.get(index)  &&  crong.get(index) <= bookLastPage ) {
                     ArrayList<Integer> crongsEachDigit = getEachDigit(crong.get(index));
-
+                    crongsPageByPageResult[index] = calculationMyScore(crongsEachDigit);
                 }
             }
         } catch (Exception exception) {
@@ -37,4 +41,17 @@ class Problem1 {
         }
         return(eachDigitArray);
     }
+
+    public static int calculationMyScore(ArrayList<Integer> whoesEachDigit) {
+        int plusTotal = 0;
+        int multiplicationTotal = 0;
+        for(int loopCount = 0; loopCount < whoesEachDigit.size(); loopCount++) {
+            plusTotal += whoesEachDigit.get(loopCount);
+            multiplicationTotal += whoesEachDigit.get(loopCount);
+        }
+        int pageResult = Math.max(plusTotal, multiplicationTotal);
+
+        return pageResult;
+    }
+
 }
