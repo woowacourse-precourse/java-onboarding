bd28b0c2c9925510e60f79acb0c3ac38e7a3ea0f
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-10-31 01:36:43 +0900

    problem1 test completed

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        String ans = "";
+        int i = 0;
+
+        int count = 0; int indexEnd = 0;
+        for(i=0; i<answer.length()-1; i++){
+            if(answer.charAt(i) != answer.charAt(i+1)){
+                count = 0;
+                continue;
+            }else{
+                ans = answer.substring(i) + answer.substring(i+1, answer.length()-1);
+            }
+        }
+
         return answer;
     }
 }
