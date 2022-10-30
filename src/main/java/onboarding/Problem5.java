7a666b638f6856c94bfc8dad8ea79d9b8e400cec
minsu <bill7666@naver.com>
2022-10-30 15:39:03 +0900

    5번문제 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+
 import java.util.List;
 
 public class Problem5 {
+    public static List<Integer> result(int money){
+        List<Integer> result = new ArrayList<>();
+        result.add(0, money/50000);
+        money = money%50000;
+        result.add(1,money/10000);
+        money = money%10000;
+        result.add(2,money/5000);
+        money = money%5000;
+        result.add(3,money/1000);
+        money = money%1000;
+        result.add(4,money/500);
+        money = money%500;
+        result.add(5,money/100);
+        money = money%100;
+        result.add(6,money/50);
+        money = money%50;
+        result.add(7,money/10);
+        money = money%10;
+        result.add(8,money);//마지막은 1원이기때문에 다른연산없이 바로넣어준다.
+        return result;
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> result = result(money);
+
+        return result;
     }
 }
--- src/main/java/onboarding/TestProtocol.java
+++ src/main/java/onboarding/TestProtocol.java
@@ -5,85 +5,28 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class TestProtocol {
-    public static char[] SetAlphabet(){
-        char[] alphabet = new char[26];
-        for(int i =0;i < alphabet.length;i++){
-            alphabet[i] = (char)('A'+i);
-        }
-        return alphabet;
-    }//알파벳 배열에 집어넣기
-    public static HashMap<Character,Character> SetMap(char[] alphabet){
-        HashMap<Character,Character> map = new HashMap<Character,Character>();
 
-        for(int i = 0;i<alphabet.length;i++){
-                map.put(alphabet[i],alphabet[25-i]);
-        }
-        return map;
-    }
-
-    public static String ReturnSentence(List<Character> list){
-        String result = "";
-      for(int i = 0;i<list.size();i++) {
-           result += Character.toString(list.get(i));//char 배열을 문자열로 만들어준다.
-      }
-        return result;
-    }
-
-    public static List<Character>  ReturnBigLetter(char[] result){
-        List<Character> list = new ArrayList<>();
-        for(int i = 0;i<result.length;i++) {
-            if(Character.isUpperCase(result[i])){
-                list.add(i,result[i]);
-            }else if(Character.isLowerCase(result[i])) {
-                list.add(i,Character.toUpperCase(result[i]));
-            }
-            else{
-                list.add(i,' ');
-            }
-        }
-        return list;
-    }//hashmap에저장되어있는 알파벳이 대문자이기때문에 문자로받은값을 대문자로 다바꿔준다.
-
-    public static List<Character> Encoding(List<Character> list,HashMap<Character,Character> map){
-        List<Character> Big = new ArrayList<>();
-        for(int i = 0;i<list.size();i++){
-            if(list.get(i) == null){
-                Big.add(i,' ');
-            }
-            else {
-                Big.add(i, map.get(list.get(i)));
-            }
-        }
-        return Big;
-    }
-
-    public static List<Character> SmallToSmall(char[] result,List<Character> Big){
-        List<Character> Final = new ArrayList<>();
-        for(int i = 0;i<result.length;i++){
-            if(Character.isLowerCase(result[i])){
-                Final.add(i,Character.toLowerCase(Big.get(i)));
-            }else if(Big.get(i) == null){
-                Final.add(i,' ');
-            }
-            else{
-                Final.add(i,Big.get(i));
-            }
-        }
-        return Final;
-    }
     public static void main(String[] args){
-        String param = "I love you";
-        char[] result1 = param.toCharArray();//문자열을 char배열로 바꾼다
-        char[] alphabet = SetAlphabet();//알파벳 배열에담기
+        List<Integer> answer = Collections.emptyList();
+        int money = 50237;
+        List<Integer> list = new ArrayList<>();
+            list.add(0, money/50000);
+            money = money%50000;
+            list.add(1,money/10000);
+            money = money%10000;
+            list.add(2,money/5000);
+            money = money%5000;
+            list.add(3,money/1000);
+            money = money%1000;
+            list.add(4,money/500);
+            money = money%500;
+            list.add(5,money/100);
+            money = money%100;
+            list.add(6,money/50);
+            money = money%50;
+            list.add(7,money/10);
+            money = money%10;
+            list.add(8,money);
 
-        HashMap<Character,Character> map = SetMap(alphabet);
-       List<Character>  list = ReturnBigLetter(result1);
-
-        List<Character> Big = Encoding(list,map);
-        List<Character> Final = SmallToSmall(result1,Big);
-
-       String Finish = ReturnSentence(Final);
-
-        System.out.println(Finish);
     }
 }
