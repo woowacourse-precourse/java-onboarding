443b457f38ac834aa57a3985021626c3bd52aed9
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:22:05 +0900

    [문제4] 알파벳을 제외 변환하지 않는 로직 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,7 +7,15 @@ public class Problem4 {
         if (word.length()<1 || word.length()>1000)
             return "Invalid Input";
 
-        
+        for (int i = 0; i<word.length(); i++){
+            int alphabet = word.charAt(i);
+            if(alphabet >=65 && alphabet <= 90){
+                alphabet = 155 - alphabet;
+            }else if(alphabet >=97 && alphabet <= 122){
+                alphabet = 219 - alphabet;
+            }
+            answer += (char)alphabet;
+        }
 
         return answer;
     }
