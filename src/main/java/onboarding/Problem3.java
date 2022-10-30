ff3aa7a89ab414c2d2d1bc94ac90b20caaabf3c5
Yejin Shin <shinyejin0212@gmail.com>
2022-10-30 14:14:26 +0900

    feat : solve Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int clap(int num, int cnt){
+        if (num == 0)
+            return cnt;
+        if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9){
+            cnt ++;
+        }
+        num /= 10;
+        return clap(num, cnt);
+
+    }
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1;  i <= number; i++){
+            int current = i;
+            answer = clap(current, answer);
+        }
+
         return answer;
     }
 }
