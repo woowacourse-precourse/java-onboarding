ac82b9e7a479db19864434458d53c76f0766c57e
woosung1223 <dntjd991223@naver.com>
2022-10-30 21:22:52 +0900

    refactor(Problem4): changeAlphabet  메소드 분리

--- src/main/java/onboarding/problem4/Word.java
+++ src/main/java/onboarding/problem4/Word.java
@@ -6,16 +6,31 @@ public class Word {
     static final char Z_LOWERCASE = 'z';
     static final char Z_UPPERCASE = 'Z';
 
-    public static char changeAlphabet(char character) {
-        if ((character < A_LOWERCASE || character > Z_LOWERCASE) &&
-                (character < A_UPPERCASE || character > Z_UPPERCASE)) {
-            return character;
+    public static char changeAlphabet(char alphabet) {
+        if (!Character.isAlphabetic(alphabet)) {
+            return alphabet;
         }
-        if (character < Z_UPPERCASE) {
-            return (char)(Z_UPPERCASE - character + A_UPPERCASE);
+        if (isCapitalAlphabet(alphabet)) {
+            return capAlphabetReverse(alphabet);
         }
-        return (char)(Z_LOWERCASE - character + A_LOWERCASE);
+        return smlAlphabetReverse(alphabet);
     }
+
+    public static boolean isCapitalAlphabet(char character) {
+        if (A_UPPERCASE <= character && character <= Z_UPPERCASE) {
+            return true;
+        }
+        return false;
+    }
+
+    public static char capAlphabetReverse(char alphabet) {
+        return (char)(Z_UPPERCASE - alphabet + A_UPPERCASE);
+    }
+
+    public static char smlAlphabetReverse(char alphabet) {
+        return (char)(Z_LOWERCASE - alphabet + A_LOWERCASE);
+    }
+
     public static String changeWord(String word) {
         String newWord = "";
         for (int i = 0; i < word.length(); i++) {
