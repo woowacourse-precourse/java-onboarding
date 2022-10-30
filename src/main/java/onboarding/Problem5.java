f23d94791efc0a3f1356657f012f409df327372d
최용태 <smc2315@naver.com>
2022-10-30 22:40:58 +0900

    5sol

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,26 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer.set(0,money/50000);
+        money%=50000;
+        answer.set(1,money/10000);
+        money%=10000;
+        answer.set(2,money/5000);
+        money%=5000;
+        answer.set(3,money/1000);
+        money%=1000;
+        answer.set(4,money/500);
+        money%=500;
+        answer.set(5,money/100);
+        money%=100;
+        answer.set(6,money/50);
+        money%=50;
+        answer.set(7,money/10);
+        money%=10;
+        answer.set(8,money);
         return answer;
     }
 }
