373809e652022c7afff6abe139cd3523a2e86480
jiwon <ljwon77@daum.net>
2022-10-30 17:46:10 +0900

    feat(problem6): check duplicated and add result set

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -23,7 +23,6 @@ public class Problem6 {
 
             // 2글자씩 끊어서 부분 닉네임을 생성한 뒤 중복을 체크한다.
             createPartNicknameAndCheck(email, nameArr);
-
         }
 
         return new ArrayList<>(answer);
@@ -38,9 +37,24 @@ public class Problem6 {
     private static void createPartNicknameAndCheck(String email, String[] nameArr) {
         for (int i = 0; i < nameArr.length-1; i++) {
             String partNickname = nameArr[i] + nameArr[i+1];
+            // 부분 닉네임 문자열에 대한 중복 체크를 진행한다.
+            checkDuplicatedAndAddResult(email, partNickname);
             partNameWithEmail.put(partNickname, email);
         }
     }
 
-
+    /**
+     * 부분 닉네임 문자열에 대한 중복 체크를 진행한 뒤 결과 Set에 담는다.
+     *
+     * @param email 사용자의 이메일
+     * @param partNickname 사용자의 부분 닉네임 문자열
+     */
+    private static void checkDuplicatedAndAddResult(String email, String partNickname) {
+        if(partNameWithEmail.containsKey(partNickname)) {
+            // 해당 사용자의 이메일을 결과 Set에 추가한다.
+            answer.add(email);
+            // 중복된 부분 문자열을 가진 사용자의 이메일을 결과 Set에 추가한다.
+            answer.add(partNameWithEmail.get(partNickname));
+        }
+    }
 }
