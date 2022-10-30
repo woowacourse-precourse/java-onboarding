14938d58def2084b7d64631e7cbcc5fe7ffe8c1f
myeongju00 <j51677767@gmail.com>
2022-10-30 16:54:58 +0900

    feat: 청개구리 사전을 활용한 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,16 +7,31 @@ public class Problem4 {
     private static Map<Character, Character> map = new HashMap<>();
     public static String solution(String word) {
         String answer = "";
+        answer = getReverseString(word);
         return answer;
     }
-
+    private static String getReverseString(String word) {
+        char[] words = toCharArray(word);
+        insertIntoMap();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            char alphabet = words[i];
+            if(alphabet == ' ' ) {
+                sb.append(" ");
+            }
+            else {
+                sb.append(map.get(alphabet));
+            }
+        }
+        return sb.toString();
+    }
     private static void insertIntoMap() {
         for (int i = 0; i <= 'z' - 'a'; i++) {
             map.put((char) ('a' + i), (char) ('z' - i));
             map.put((char) ('A' + i), (char) ('Z' - i));
         }
     }
-    private static char[] ToCharArray(String word) {
+    private static char[] toCharArray(String word) {
         return word.toCharArray();
     }
 }
