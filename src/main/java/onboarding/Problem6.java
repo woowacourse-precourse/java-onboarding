1dded9e991e2e50cb9b16e786c3ead335a982aaa
pushedrumex <pushedrumex@naver.com>
2022-10-30 19:41:47 +0900

    problem6: result 오름차순 정렬과 중복 제거(solution)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,18 +1,20 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     static HashMap<String, List<String>> map = new HashMap();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        for (List<String> one: forms) {
+            answer.addAll(recordNick(one.get(1), one.get(0)));
+        }
+        Set<String> set = new HashSet<>(answer);
+        answer = new ArrayList<>(set);
+        Collections.sort(answer);
         return answer;
     }
 
-// 닉네임을 두글자씩 나눠 HashMap에 기록하는 함수 구현("oo", [이메일, 이메일, ... ])
     static List<String> recordNick(String nick, String email) {
         List<String> result = new ArrayList();
         String[] strArr = nick.split("");
@@ -30,10 +32,5 @@ public class Problem6 {
             }
         }
         return result;
-    }
-    public static void main(String args[]) {
-        System.out.println(recordNick("제이슨", "jm@email.com"));
-        System.out.println(recordNick("엠제이", "mj@email.com"));
-        System.out.println(map);
     }
 }
