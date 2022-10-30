93cf2d20551029fa6b3670557399fa271ef0fb04
SeongbinPark <binni0515@gmail.com>
2022-10-30 15:05:33 +0900

    [Problem1] 1. 각 자리 수 추출하는 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
         return answer;
     }
+
+    private static char[] getEachPageNumbers(int page) {
+        String s = Integer.toString(page);
+        return s.toCharArray();
+    }
+
 }
