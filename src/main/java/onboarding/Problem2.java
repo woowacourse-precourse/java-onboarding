76f53c365dcbb827d7d53605693bda6119ca4802
yeada <yeadalm@gmail.com>
2022-10-30 23:51:49 +0900

    upload 문자열 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String  removeChar(String s){
+        char[] array = s.toCharArray();
+        int len = array.length;
+        char tempChar = 0;
+
+        ArrayList<Character> tempList = new ArrayList<Character>();
+        for (int i = 0; i < len - 1; i++) {
+            if (array[i] != array[i + 1]) {
+                tempChar = array[i];
+                tempList.add(tempChar);
+            } else i++;
+        }
+        if (array[len - 2] != array[len - 1]) {
+            tempChar = array[len - 1];
+            tempList.add(tempChar);
+        }
+        StringBuilder text = new StringBuilder();
+        for(Character ch : tempList){
+            text.append(ch);
+        }
+
+        String result =  text.toString();
+        return result;
     }
 }
