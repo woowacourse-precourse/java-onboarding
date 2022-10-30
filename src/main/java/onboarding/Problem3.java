148a038a4789c06c3d321725b6cae0df6cbb5cfd
ssssudan <kankgm0324@gmail.com>
2022-10-30 11:56:26 +0900

    problem3 요구사항 분석과 설계

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+/**
+ * 배달이가 좋아하는 369게임을 하고자 한다. 놀이법은 1부터 숫자를 하나씩 대면서, 3, 6, 9가 들어가는 숫자는 숫자를 말하는 대신 3, 6, 9의 개수만큼 손뼉을 쳐야 한다.
+ * 숫자 number가 매개변수로 주어질 때, 1부터 number 까지 손뼉을 몇 번 쳐야 하는지 횟수를 return 하도록 solution 메서드를 완성하라.
+ * number 는 1 이상 10,000 이하인 자연수이다.
+ */
+
+/**
+ * 설계
+ * 1부터 n 까지 반복문 돌리면서 3, 6, 9의 개수만큼 카운트
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
     }
 }
