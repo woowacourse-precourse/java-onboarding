fa52466d161625d9f1fbe942213018a0054914da
yoonsj970407 <yoonsj970407@gmail.com>
2022-10-30 23:19:28 +0900

    ✨Feat: Create Calculate class

---
+++ src/main/java/onboarding/Calculate.java
@@ -0,0 +1,11 @@
+package onboarding;
+
+import java.util.List;
+
+class Calculate {
+    int num1, num2;
+    public Calculate(List<Integer> name) {
+        this.num1 = name.get(0);
+        this.num2 = name.get(1);
+    }
+}
