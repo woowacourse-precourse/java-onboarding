6119b2604e173bcd28f3f09f340b2f63aad6291e
dungbangj <yoyob1223@gmail.com>
2022-10-30 17:30:50 +0900

    feat: main logic - convert alphabet

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,8 @@ class WordForPro4 {
 
     private static final int MINIMUM_LENGTH = 1;
     private static final int MAXIMUM_LENGTH = 1000;
+    private static final int TOTAL_NUM_1 = 219;
+    private static final int TOTAL_NUM_2 = 155;
 
     private final char[] charArr;
 
@@ -18,13 +20,26 @@ class WordForPro4 {
                 "word의 길이는 " + MINIMUM_LENGTH + "이상 " + MAXIMUM_LENGTH + "이하의 정수여야 합니다.");
         }
     }
+
+    public String convertString() {
+        for (int i = 0; i < charArr.length; i++) {
+            if ('a' <= charArr[i] && charArr[i] <= 'z') {
+                charArr[i] = convertAlpha(TOTAL_NUM_1, charArr[i]);
+            } else if ('A' <= charArr[i] && charArr[i] <= 'Z') {
+                charArr[i] = convertAlpha(TOTAL_NUM_2, charArr[i]);
+            }
+        }
+        return String.valueOf(charArr);
+    }
+
+    private char convertAlpha(int totalNum, char alpha) {
+        return (char) (totalNum - (int) alpha);
+    }
 }
 
 public class Problem4 {
     public static String solution(String word) {
         WordForPro4 wordClass = new WordForPro4(word);
-
-        String answer = "";
-        return answer;
+        return wordClass.convertString();
     }
 }
