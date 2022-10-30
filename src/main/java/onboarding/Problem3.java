cf70fa545e52ad7d1aae78080618c610f5217230
ONE <sodlapdlf13@gmail.com>
2022-10-30 16:55:22 +0900

    docs: [Problem3] 필요 기능에 대한 주석을 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/*
+* 1. 수를 파라미터로 받아 3, 6, 9의 개수를 세어 반환하는 함수 추가
+*   1-1. 수를 각 자리 숫자로 분리하여 3, 6, 9의 개수를 센다
+* 2. 메인로직에서는 수를 함수에 map 하여 누적합을 구하는 로직을 추가
+*/
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
