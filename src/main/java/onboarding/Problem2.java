9af0e0fb6bf56d5026e7766c9f3f2d7934ba17ae
shinhn <rkdrpgml@naver.com>
2022-10-30 20:52:22 +0900

    Feat: Problem2 기능 요구 사항 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer = "";
+
+        for (int i = 1; i < cryptogram.length(); i++)
+        {
+            if (cryptogram.charAt(i-1) != cryptogram.charAt(i)) {
+                answer += cryptogram.charAt(i-1);
+            }
+            else {
+                while (i < cryptogram.length() && cryptogram.charAt(i-1) == cryptogram.charAt(i)) {
+                    i++;
+                }
+            }
+        }
+
+        if(answer.length() == 0) return ""; // 더이상 삭제할 중복 문자가 없을 경우, 빈 문자열 리턴
+
+        answer += cryptogram.charAt(cryptogram.length()-1);
+
+        if (answer.length() != cryptogram.length()) { // 중복 문자가 삭제된 경우, 재귀함수를 통해 다시 판별
+            return solution(answer);
+        }
+
         return answer;
     }
 }
