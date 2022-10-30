84c3bfb29f1a59825fd0d8d7ee4b24cfce6540aa
limzzum <chfhddl1234@naver.com>
2022-10-30 19:13:27 +0900

    [문제 6] email 중복 체크위해 boolean used 배열 생성. -> 중복 아닐경우 result에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,19 +6,34 @@ public class Problem6 {
     static String [] matchWord;
     public static List<String> solution(List<List<String>> forms) {
         String result = "";
+        String nickname = "";
+        String name = "";
+        String email = "";
+        boolean[] used = new boolean[forms.size()];
+
         for(int i=0; i<forms.size()-1; i++){
 
-            String nickname = forms.get(i).get(1);
+            nickname = forms.get(i).get(1);
             if(nickname.length()==1)
                 continue;
 
             divideWord(nickname);
 
             for( int j=i+1; j<forms.size(); j++){
-                String name= forms.get(j).get(1);
+                name= forms.get(j).get(1);
 
                 for(String word:matchWord){
                     if(name.contains(word) ){
+                        if(!used[i]){
+                            email = forms.get(i).get(0);
+                            result += email + " ";
+                            used[i]=true;
+                        }
+                        if(!used[j]){
+                            email = forms.get(j).get(0);
+                            result += email + " ";
+                            used[j] = true;
+                        }
 
                     }
                 }
