6db34648bf19ee49a25a9bbfc8f6cad89eeb3a1d
Donghyun <flaehan@gmail.com>
2022-10-30 18:01:52 +0900

    main 메소드에서는 실행이 가능하지만 answer에 어떻게 삽입해야 할까...

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,38 @@
 package onboarding;
-
+import java.util.Arrays;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] arrCapital = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        char[] arrCapitalReverse = { 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K',
+                'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A' };
+        char[] arrSmall = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        char[] arrSmallReverse = { 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j',
+                'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a' };
+
+        char[] wordToArr = word.toCharArray();
+
+        for (int i = 0; i < word.length(); i++) {
+            String a = "";
+            char b = wordToArr[i];
+
+            if (Character.isUpperCase(b)) {
+
+                int index = Arrays.binarySearch(arrCapital, wordToArr[i]);
+                System.out.print(arrCapitalReverse[index]);
+            } else if (Character.isLowerCase(b)) {
+
+                int index = Arrays.binarySearch(arrSmall, wordToArr[i]);
+
+                System.out.print(arrSmallReverse[index]);
+            } else {
+                System.out.print(" ");
+            }
+        }
+
         return answer;
     }
 }
