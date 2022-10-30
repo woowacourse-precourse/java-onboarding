4ba4da6433fc3e070ae662e1c13e096becfccbe4
JeongHunHui <deca3248@gmail.com>
2022-10-30 19:47:25 +0900

    [1주차 - Problem6] 정규식을 이용하여 이메일 형식 검사 코드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,17 @@
 package onboarding;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%+-]+@email[.]com");
+        for (List<String> data : forms) {
+            if(!emailPattern.matcher(data.get(0)).matches()) {
+                return null;
+            }
+        }
+        return forms.get(0);
     }
 }
