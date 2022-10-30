77fe072c1a1f25d6eca832509c4e74be989ca516
박시균\psk98 <psg980331@naver.com>
2022-10-30 18:03:28 +0900

    doc : 문제2 기능 구현 목록 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 1. 중복문자 존재하는지 확인
+ * 2. 중복문자 존재시 중복문자 제거
+ * 3. 중복문자가 존재하지 않는 결과값 반환
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
