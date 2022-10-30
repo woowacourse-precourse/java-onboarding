68592317af9e87c85c12271d06e3d40b64c33a05
wugawuga <dnr3655@naver.com>
2022-10-31 03:36:38 +0900

    feat: 문자열 길이가 1일때 바로 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+		if (cryptogram.length() == 1) {
+			return cryptogram;
+		}
         return answer;
     }
 }
