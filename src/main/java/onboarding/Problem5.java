0c5e86d4d0f2474f7efcbb098da97893dde50dd3
Seoyoung <ryupark1006@gmail.com>
2022-10-30 20:26:33 +0900

    feat: solve problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        while (money >= 10) {
+            int i = 50000;
+            while (i >= 10) {
+                answer.add(money / i);
+                money %= i;
+                i /= 5;
+                answer.add(money / i);
+                money %= i;
+                i /= 2;
+            }
+        }
+        answer.add(money);
+
         return answer;
     }
 }
