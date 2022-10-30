75b3b6ffd484da6e950e2dcfca63a6ef936e08b7
overtime01 <kkkmj25@naver.com>
2022-10-30 15:27:29 +0900

    5번 풀이

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer ={50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        char ls = new char[9];
+        for (int i = 0;answer.length;i ++){
+             ls.add(money/answer[i]);
+             money = money % answer[i];
+        }
+        for(int i = 0; i<9;i++){
+            System.out.println(ls);
+        }
         return answer;
     }
 }
