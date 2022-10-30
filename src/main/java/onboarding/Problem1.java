1c0e9a2e9d158f02b49931fe266dbb5fb6052e28
KimSoo <ilppang@naver.com>
2022-10-30 20:31:18 +0900

    [Problem1] 김현수 예외처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,18 +6,21 @@ import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int checkPobiExeptionResult = checkExceptions(pobi);
+        int checkCrongExeptionResult = checkExceptions(pobi);
+        if(!((checkCrongExeptionResult == 1) || (checkPobiExeptionResult == 1))) return -1;
+
         int pobiMaxValue = getMaxValue(pobi);
         int crongMaxValue = getMaxValue(crong);
 
-        return answer;
+        return 0;
     }
     public static  int getMaxValue(List<Integer> list){
         List<Integer> maxValue = new ArrayList<>();
         for (Integer el: list){
         maxValue.add(Math.max(getSumValue(el), getMultiplyValue(el)));
-
         }
+
         return (Math.max(maxValue.get(0), maxValue.get(1)));
     }
 
@@ -27,4 +30,17 @@ class Problem1 {
     public static int getMultiplyValue(Integer number) {
         return Stream.of(number.toString().split("")).mapToInt(Integer::parseInt).reduce(1, (a,b) -> a * b);
     }
+
+    public static int checkExceptions(List<Integer> list){
+        int leftPage = list.get(0);
+        int rightPage = list.get(1);
+        int insub = rightPage - leftPage;
+
+        if ((insub == 1)&&(leftPage%2 == 1) && (rightPage % 2 == 0)){
+            return 1;
+        }
+
+        return -1;
+    }
+
 }
