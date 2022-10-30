6cc056f6baccee100e4d2eb81233f1ad9d602c8e
김태용 <amico741@naver.com>
2022-10-31 00:15:23 +0900

    feat: 연속된 두 글자 집합 만드는 메서드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static HashSet<String> makeLetterSet(List<List<String>> forms) {
+        HashSet<String> letters = new HashSet<>();
+        for(List<String> crew : forms){
+            String nickname = crew.get(1);
+            for(int i = 0; i < nickname.length() - 1; i++){
+                letters.add(nickname.substring(i, i + 2));
+            }
+        }
+        return letters;
     }
 }
