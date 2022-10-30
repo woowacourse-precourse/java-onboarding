320b5246a94271a69e16d9b5925602fbbf15d763
sdsr <dlxodnr1212@naver.com>
2022-10-30 16:32:25 +0900

    기능 1 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,27 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //입력받은 값이 올바른지 확인
+        if(checknum(pobi, crong) == -1)
+            return -1;
+        
         return answer;
+    }
+    //올바른 값인지 확인
+    public static int checknum(List<Integer> pobi, List<Integer> crong) {
+        int checknum = 0;
+        //pobi와 crong의 왼쪽 페이지가 홀수가 아니면 예외처리
+        if(pobi.get(0)%2 != 1 || crong.get(0)%2 != 1)
+            checknum = -1;
+        //pobi와 crong의 오른쪽 페이지가 이상하면 예외처리
+        if(pobi.get(1)-pobi.get(0)!=1 || crong.get(1)-crong.get(0)!=1)
+            checknum = -1;
+        //pobi와 crong이 첫페이지나 마지막 페이지이면 예외처리
+        if(pobi.get(0) == 1 || pobi.get(0) == 400 || pobi.get(1) == 1 || pobi.get(1) == 400)
+            checknum = -1;
+        if(crong.get(0) == 1 || crong.get(0) == 400 || crong.get(1) == 1 || crong.get(1) == 400)
+            checknum = -1;
+        return checknum;
     }
 }
