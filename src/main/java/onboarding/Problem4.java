23b1bbf77685733c5c7c5e6c3d88ea293209ed42
swhyeon98 <smreo75@gmail.com>
2022-10-30 20:19:37 +0900

    Problem4 - 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                answer += (char) ('Z' - (word.charAt(i) - 'A'));
+            } else if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') {
+                answer += (char) ('z' - (word.charAt(i) - 'a'));
+            } else {
+                answer += word.charAt(i);
+            }
+        }
+
         return answer;
     }
 }
