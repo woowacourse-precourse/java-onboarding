0b27c20f0f6bf69416c2e761fd9151124dbecdeb
HiiWee <wpdlzhf159@gmail.com>
2022-10-31 03:15:22 +0900

    Feat problem6: 이메일, 닉네임에 대한 상수 정의

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    static final int EMAIL = 0;
+    static final int NICKNAME = 1;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
