4fef57cd984c78767d946adb5c4c216d2ac9941a
최용태 <smc2315@naver.com>
2022-10-30 21:44:28 +0900

    4sol

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,13 @@ public class Problem4 {
         StringBuilder answer = new StringBuilder();
         for(int i=0;i<word.length();i++){
             if(word.charAt(i)>='a'&&word.charAt(i)<='z'){
-               answer.append('z' - word.charAt(i) + 'a');
+               answer.append((char)('z' - word.charAt(i) + 'a'));
+            }
+            else if( word.charAt(i)>='A'&&word.charAt(i)<='Z'){
+                answer.append((char)('Z' - word.charAt(i) + 'A'));
+            }
+            else{
+                answer.append(word.charAt(i));
             }
         }
         return answer.toString();
