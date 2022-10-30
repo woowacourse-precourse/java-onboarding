a34de296cf750eade8ef39719ac483e0171b1923
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-30 23:23:12 +0900

    [문제5] 제한사항 체크

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        if (money<1 || money>1000000)
+            return null;
+
+        
+
         return answer;
     }
 }
