72e4a09d67354204a24f95e1f32453580f4e9a66
Lee Min Woo <lmw7414@naver.com>
2022-10-30 18:54:39 +0900

    problem7 - 메서드 이름 설정
    기능 요소 설계에서 말한 내용을 바탕으로 메서드 이름을 설정해보았다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /** 구현해야할 기능
  *  구조체 설계 User(이름, 친구 list)
@@ -12,8 +11,26 @@ import java.util.List;
  *  점수를 저장하기 위한 arrayList 필요
  */
 public class Problem7 {
+
+    static Map<String, User> userMap = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public void createUser(String userName){
+
+    }
+    public void addFriend(String friendName) {
+
+    }
+
+    public void calcScore(String userName) {
+        //TODO : 점수를 계산하기 위해서는 유저 이름과 스코어를 저장할 수 있는 자료구조가 필요하다.
+    }
+    public static class User {
+        String name;
+        ArrayList<String> friends = new ArrayList<>();
     }
 }
