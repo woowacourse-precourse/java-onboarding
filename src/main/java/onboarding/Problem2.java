17120c525b052a66cbfd3eb2ddf95595359426f2
Invidam <hansupark0208@gmail.com>
2022-10-30 22:43:31 +0900

    refactor(Problem2): make Cryptogram to collection
    collect method and variable to Cryptogram.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,54 +6,71 @@ import java.util.InputMismatchException;
 
 public class Problem2 {
 
-    static final int START_RANGE = 1;
-    static final int END_RANGE = 1_000;
+    static class Cryptogram {
 
-    private static Deque<Character> removeDuplicates(String cryptogram) {
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char ch : cryptogram.toCharArray()) {
-            enrichDeque(deque, ch);
-        }
-        return deque;
-    }
+        private String data;
+        private final int START_RANGE = 1;
+        private final int END_RANGE = 1_000;
 
-    private static void enrichDeque(Deque<Character> deque, char ch) {
-        if (!Character.isLowerCase(ch)) {
-            throw new InputMismatchException("영문자 소문자가 아닌 입력이 들어왔습니다.");
+
+        public Cryptogram(String str) {
+            if (!isValidLength(str)) {
+                throw new InputMismatchException("입력된 문자열의 크기가 [1,1000] 이내가 아닙니다.");
+            }
+            this.data = removeDuplicates(str);
         }
 
-        if (isDuplicates(deque, ch)) {
-            deque.removeLast();
-            return;
+        private boolean isValidLength(String str) {
+            return START_RANGE <= str.length() && str.length() <= END_RANGE;
         }
 
-        deque.addLast(ch);
-    }
 
-    private static boolean isDuplicates(Deque<Character> deque, char ch) {
-        return !deque.isEmpty() && deque.getLast() == ch;
-    }
-
-
-    private static StringBuffer dequeToStr(Deque<Character> deque) {
-        StringBuffer stringBuffer = new StringBuffer();
-
-        while (!deque.isEmpty()) {
-            stringBuffer.append(deque.getFirst());
-            deque.removeFirst();
+        private String removeDuplicates(String cryptogram) {
+            Deque<Character> deque = new ArrayDeque<>();
+            for (char ch : cryptogram.toCharArray()) {
+                enrichDeque(deque, ch);
+            }
+            return dequeToStr(deque);
         }
-        return stringBuffer;
+
+        private void enrichDeque(Deque<Character> deque, char ch) {
+            if (!Character.isLowerCase(ch)) {
+                throw new InputMismatchException("영문자 소문자가 아닌 입력이 들어왔습니다.");
+            }
+
+            if (isDuplicates(deque, ch)) {
+                deque.removeLast();
+                return;
+            }
+
+            deque.addLast(ch);
+        }
+
+        private boolean isDuplicates(Deque<Character> deque, char ch) {
+            return !deque.isEmpty() && deque.getLast() == ch;
+        }
+
+        private String dequeToStr(Deque<Character> deque) {
+            StringBuffer stringBuffer = new StringBuffer();
+
+            while (!deque.isEmpty()) {
+                stringBuffer.append(deque.getFirst());
+                deque.removeFirst();
+            }
+            return stringBuffer.toString();
+        }
+
+        public String result() {
+            return this.data;
+        }
     }
+
+    public static Cryptogram makeCryptogram(String str) {
+        return new Cryptogram(str);
+    }
+
 
     public static String solution(String cryptogram) {
-        if (!isValidLength(cryptogram)) {
-            throw new InputMismatchException("입력된 문자열의 크기가 [1,1000] 이내가 아닙니다.");
-        }
-
-        return dequeToStr(removeDuplicates(cryptogram)).toString();
-    }
-
-    private static boolean isValidLength(String cryptogram) {
-        return START_RANGE <= cryptogram.length() && cryptogram.length() <= END_RANGE;
+        return makeCryptogram(cryptogram).result();
     }
 }
