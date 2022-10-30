27abb4809eee877e2b91716069ba4a673918c617
seonho <ysh10216@gmail.com>
2022-10-30 17:43:43 +0900

    create isException function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    // 1. 예외처리 : 1~10000 사이의 숫자인지 체크
+    public static boolean isException(int number){
+        if(number >= 1 && number<= 10000){
+            return true;
+        }
+        return false;
+    }
     public static int solution(int number) {
         int answer = 0;
-        // 1. 예외처리 : 1~10000 사이의 숫자인지 체크
+
         // 2. 받은 숫자가 들어가는 횟수 체크하는 함수 만들기.
         return answer;
     }
