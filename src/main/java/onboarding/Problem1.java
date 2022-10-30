0d06b6952b8afc41d3cce1e336abf3000313bee7
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-10-30 23:45:25 +0900

    FEAT : 펼친 페이지의 유효성 검사 함수(checkPage) 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -21,8 +21,20 @@ class Problem1 {
         return ans;
     }
 
+    private static boolean checkPage(List<Integer> pageList) {
+        if(pageList.get(0) + 1 == pageList.get(1))
+            return true;
+        else
+            return false;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         try {
+            if(!checkPage(pobi))
+                return -1;
+            if(!checkPage(crong))
+                return -1;
+
             int answer = Integer.MAX_VALUE;
             return answer;
         } catch (Exception e) {
