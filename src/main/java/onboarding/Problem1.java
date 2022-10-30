2fbbd419e1b79548bdf6f752899f3adaddb35323
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-10-31 02:15:27 +0900

    Chore(Problem1): 기능 목록 주석 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,16 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 잘못된 페이지 입력 예외
+        // 첫 페이지와 마지막 페이지를 펼친 경우 예외 처리
+
+        // pobi 가장 큰 수 구하기
+
+        // crong 가장 큰 수 구하기
+
+        // 두 수 비교 한 값 할당
+
         return answer;
     }
 }
