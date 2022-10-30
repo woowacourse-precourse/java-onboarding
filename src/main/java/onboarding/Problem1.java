8f4907ecf68df2bd3a466cf8a455318d8702894f
anneu <b.imnuj@gmail.com>
2022-10-31 02:45:43 +0900

    feat(problem1): 각 자리 숫자의 덧셈과 곱셈 중 큰 수를 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,10 +2,16 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Math.max;
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int getMax(int page) {
+        return max(add(page), multiply(page));
     }
 
     private static int multiply(int num) {
