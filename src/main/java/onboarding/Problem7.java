fc5c5013bcfb19a1c77af93969b58fa46c01189b
dmdrk1414 <seungchan141414@gmail.com>
2022-10-30 12:38:12 +0900

    [Add] Problem_7 user의 친구리스트, 전체 사람 리스트 추가
    1. user의 친구리스트와 전체 사람 리스트를 추가 하였다.
    2. user의 친구, 전체적인 사람, user의 친구를 리스트로 추가 하고싶다.
    3. 지금은 user의 친구, 전체적인 사람 리스트를 구현함

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,71 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    final static int LEFT_FRIEND = 0;
+    final static int RIGHT_FRIEND = 1;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // user와 친구가 아닌것을 걸러야된다.
+        ArrayList<String> notFriendUserList = new ArrayList<>();
+        ArrayList<String> friendUserList = new ArrayList<>();
+        ArrayList<String> personList = new ArrayList<>();
+        List<String> listAtNow = null;
+        String leftFriendsInList = "";
+        String rightFriendsInList = "";
+
+        int endFistSize = friends.size();
+        int endSecondSize = friends.get(0).size();
+        for (int firstListIndex = 0; firstListIndex < endFistSize; firstListIndex++) {
+            // friends 리스트 안의 리스트 작업
+            listAtNow = friends.get(firstListIndex);
+
+            leftFriendsInList = listAtNow.get(LEFT_FRIEND);
+            rightFriendsInList = listAtNow.get(RIGHT_FRIEND);
+
+            // user의 친구면? FriendUserListdp 넣는다.
+            if (Objects.equals(user, leftFriendsInList)) {
+                friendUserList.add(rightFriendsInList);
+            }
+            if (Objects.equals(user, rightFriendsInList)) {
+                friendUserList.add(leftFriendsInList);
+            }
+            personList.add(leftFriendsInList);
+            personList.add(rightFriendsInList);
+        }
+
+        personList = (ArrayList<String>) personList.stream().distinct().collect(Collectors.toList());
+        System.out.println("personList.toString() = " + personList.toString());
+
+        friendUserList = (ArrayList<String>) friendUserList.stream().distinct().collect(Collectors.toList());
+        System.out.println("friendUserList.toString() = " + friendUserList.toString());
+
+    
+        notFriendUserList = (ArrayList<String>) notFriendUserList.stream().distinct().collect(Collectors.toList());
+        System.out.println("notFriendUserList.toString() = " + notFriendUserList.toString());
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        List<String> result = List.of("andole", "jun", "bedi");
+
+        solution(user, friends, visitors);
     }
 }
