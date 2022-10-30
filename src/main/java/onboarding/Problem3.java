c17c90c55452875275ac68dcbb31c86de9e55b1b
rt3310 <btac3310@gmail.com>
2022-10-30 16:10:46 +0900

    feat(Problem3): 369 개수를 새는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,11 +2,31 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return get369CountFromOneToNumber(number);
     }
 
     private static String intToString(int number) {
         return String.valueOf(number);
+    }
+
+    public static int get369CountFromOneToNumber(int number) {
+        int result = 0;
+        for (int i = 1; i <= number; i++) {
+            result += get369Count(i);
+        }
+        return result;
+    }
+
+    private static long get369Count(int number) {
+        return intToString(number).chars()
+                .filter(Problem3::is369DigitChar)
+                .count();
+    }
+
+    private static boolean is369DigitChar(int digit) {
+        if (digit == '3' || digit == '6' || digit == '9') {
+            return true;
+        }
+        return false;
     }
 }
