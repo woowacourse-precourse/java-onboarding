64adb88b9d087a829849daaee475cc0c93fcc1da
kimjeonghan <[rlawjdgks318@naver.com]>
2022-10-30 13:37:49 +0900

    feat(Problem3): 파라미터 예외 처리 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem3 {
+
+    private static boolean checkParam(int n)
+    {
+        return n == 0 || 10000 < n;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
