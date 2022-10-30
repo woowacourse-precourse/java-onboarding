cab279c7fbebdab18057eef260b571f72aa3606c
hong-sile <gurwns9325@naver.com>
2022-10-30 13:52:11 +0900

    feat : 친구의 친구에 10점을 추가해주는 메소드 추가
    friend 리스트 중 userFriendList에 속해있는 사람을 탐색하여,
    friendScoreMap에서 + 10

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,18 +5,56 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem7 {
-    private static Map<String,Integer> friendScoreMap = new HashMap<>();
-    private static List<String> userFriendList = new ArrayList<>();
-
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> userFriendList = new ArrayList<>();
+        Map<String,Integer> friendScoreMap = new HashMap<>();
 
+        initUserFriendList(user, friends, userFriendList);
+        initFriendScoreMap(friends, friendScoreMap);
 
-
-
+        addScoreInScoreMap(friends, userFriendList, friendScoreMap);
 
         return answer;
     }
+
+    private static void addScoreInScoreMap(List<List<String>> friends, List<String> userFriendList,
+        Map<String, Integer> friendScoreMap) {
+        for (List<String> friendRelation : friends) {
+            if(Collections.disjoint(friendRelation, userFriendList))
+                continue;
+            addScoreInScoreMap(userFriendList, friendScoreMap, friendRelation);
+        }
+    }
+
+    private static void addScoreInScoreMap(List<String> userFriendList, Map<String, Integer> friendScoreMap,
+        List<String> friendRelation) {
+        String friendFirst = friendRelation.get(0);
+        String friendSecond = friendRelation.get(1);
+        if(userFriendList.contains(friendFirst))
+            friendScoreMap.replace(friendSecond,friendScoreMap.get(friendSecond)+10);
+        else
+            friendScoreMap.replace(friendFirst,friendScoreMap.get(friendFirst)+10);
+    }
+
+    private static void initFriendScoreMap(List<List<String>> friends, Map<String, Integer> friendScoreMap) {
+        Set<String> userSet = friends.stream()
+            .flatMap(List::stream).collect(Collectors.toSet());
+        userSet.forEach(user -> friendScoreMap.put(user,0));
+    }
+
+    private static void initUserFriendList(String user, List<List<String>> friends, List<String> userFriendList) {
+        for (List<String> friendRelation : friends) {
+            if(friendRelation.contains(user)){
+                userFriendList.add(friendRelation.get(0));
+                userFriendList.add(friendRelation.get(1));
+                userFriendList.remove(user);
+            }
+        }
+    }
+
 }
