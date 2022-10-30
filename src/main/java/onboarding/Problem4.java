a18bc3ea68bb9e85a96e4c4671e8055a8d7fa2fd
Cho Changhun <cvcvcx@naver.com>
2022-10-30 15:33:11 +0900

    4번문제 소문자 철자를 변환하는 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,7 @@
 package onboarding;
-
+//주어진 문자열이 대문자일때, 알파벳을 반대로 변환하는 함수
+//주어진 문자열이 소문자일때, 알파벳을 반대로 변환하는 함수
+//문자열을 배열로 받아서, 알파벳을 반대로 변환해서 String 타입으로 반환한느 함수
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
@@ -11,25 +13,18 @@ public class Problem4 {
         String result = "";
         String alphabets = "abcdefghijklmnopqrstuvwxyz";
         char nextAlphabet = 0;
-        for(char c: chars){
-            if(Character.isLowerCase(c)){
-                for (int i = 0; i < alphabets.length(); i++) {
-                    if(alphabets.charAt(i)==c){
-                         nextAlphabet= alphabets.charAt(alphabets.length()-1-i);
-                    }
-                }
-            }else if(Character.isUpperCase(c)){
-                for (int i = 0; i < alphabets.length(); i++) {
-                    if(alphabets.charAt(i)==Character.toLowerCase(c)){
-                        nextAlphabet=Character.toUpperCase(alphabets.charAt(alphabets.length()-1-i));
-                    }
-                }
-            }else {
-                nextAlphabet = ' ';
-            }
-            result += nextAlphabet;
-        }
+//        result = getReverseString(chars, result, alphabets, nextAlphabet);
         return result;
+    }
+
+//소문자 철자를 발견했을때, 그 철자를 반대로 변환
+    private static char getNextAlphabet(String alphabets, char nextAlphabet, char c) {
+        for (int i = 0; i < alphabets.length(); i++) {
+            if(alphabets.charAt(i)== c){
+                 nextAlphabet = alphabets.charAt(alphabets.length()-1-i);
+            }
+        }
+        return nextAlphabet;
     }
 
 }
