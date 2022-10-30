5f617de6b271117f2bb38a0c814f1d15d7c75c3c
Ridealist <cjsn92@gmail.com>
2022-10-30 15:16:48 +0900

    feat/Problem7 : user_friends 필드 선언 및 setUserFriends 메소드 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,13 +1,11 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem7 {
 
     Map<String, Integer> user_score = new HashMap<>();
+    Map<String, List<String>> user_friends = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
@@ -19,6 +17,32 @@ public class Problem7 {
             if (user_score.containsKey(visitor)) {
                 user_score.put(visitor, user_score.get(visitor) + 1);
             } else {user_score.put(visitor, 1);}
+        }
+    }
+
+
+    void setUserFriends(List<List<String>> friends) {
+        for (List<String> friend_list : friends) {
+            String friend1 = friend_list.get(0);
+            String friend2 = friend_list.get(1);
+            if (user_friends.containsKey(friend1)) {
+                List<String> array = user_friends.get(friend1);
+                array.add(friend2);
+                user_friends.put(friend1, array);
+            } else {
+                ArrayList<String> array = new ArrayList<>();
+                array.add(friend2);
+                user_friends.put(friend1, array);
+            }
+            if (user_friends.containsKey(friend2)) {
+                List<String> array = user_friends.get(friend2);
+                array.add(friend1);
+                user_friends.put(friend2, array);
+            } else {
+                ArrayList<String> array = new ArrayList<>();
+                array.add(friend1);
+                user_friends.put(friend2, array);
+            }
         }
     }
 }
