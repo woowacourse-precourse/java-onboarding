9f85ad7de912e89194e6c2bbb86f4fef37af9bab
rabbit0216 <rabbit1999k@gmail.com>
2022-10-30 18:03:24 +0900

    feat(PR4): 엄마 말씀 변환 및 결과 출력

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -12,9 +12,19 @@ public class Problem4 {
 
         for(int idx = 0; idx < word.length(); idx++){
             char alphabet = word.charAt(idx);
+            int alphDictIdx = 0;
             if(Character.isAlphabetic(alphabet)){ //알파벳이면 변환
-                
+                if(Character.isLowerCase(alphabet)){ //소문자일 경우
+                    char tmpConvertToUpp = Character.toUpperCase(alphabet);
+                    alphDictIdx = alpDict.indexOf(tmpConvertToUpp);
+                    answer += Character.toLowerCase(alpDict.get(25 - alphDictIdx));
+                }
+                else{
+                    alphDictIdx = alpDict.indexOf(alphabet);
+                    answer += alpDict.get(25-alphDictIdx);
+                }
             }
+            else answer += alphabet;
         }
 
         return answer;
