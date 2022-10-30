86eab1a5bc5b51f991b47f987c90ebb63f636c6a
kokomong2 <ko9603@naver.com>
2022-10-30 20:10:31 +0900

    feat(문제3) 추가 : 자리수 확인 후 박수 증가 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count =0;
+        System.out.println("함수시작");
+        for (int i = 1; i <= number; i++) {
+            int tmp=i;
+            while(tmp>0){
+                if(tmp%10==3||tmp%10==6||tmp%10==9){
+                    count++;
+                }
+                tmp = tmp/10;
+            }
+        }
+
+        return count;
     }
 }
