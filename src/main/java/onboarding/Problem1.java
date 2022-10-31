e671eeec6930dbca117cfdc9768e7f1f8b17db50
gksruf2848 <4455qqq@naver.com>
2022-10-31 14:59:07 +0900

    Problem1 결과값을 반환해주는 getResult 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,12 +9,15 @@ class Problem1 {
         int crongLeft = crong.get(0);
         int crongRight = crong.get(1);
 
+        // 입력 예외처리
         if (checkInput(pobiLeft, pobiRight) || checkInput(crongLeft, crongRight)) // 하나라도 조건에 맞지 않으면 -1을 리턴함
             return -1;
 
+        return getResult(pobiLeft, pobiRight, crongLeft, crongRight);
+
     }
 
-    private static boolean checkInput(int left, int right){
+    static boolean checkInput(int left, int right){
         if (left < 1 || left > 400 || right < 1 || right > 400) //1 ~ 400 외의 범위일 경우
             return true;
         if (left + 1 != right) // 왼쪽 페이지 + 1 = 오른쪽 페이지 가 아닐 경우
@@ -45,6 +48,20 @@ class Problem1 {
         for (Integer score : scores)
             if (max < score) max = score;
         return max;
+    }
+
+    static int getResult(int l1, int r1, int l2, int r2) {
+        // 각 자릿수의 곱, 합 중 가장 큰 값을 구함
+        int Max1 = getMax(List.of(add(l1), add(r1), multiply(l1), multiply(r1)));
+        int Max2 = getMax(List.of(add(l2), add(r2), multiply(l2), multiply(r2)));
+
+        // 비교
+        if (Max1 > Max2)
+            return 1;
+        else if (Max1 < Max2)
+            return 2;
+        else
+            return 0;
     }
 
 }
