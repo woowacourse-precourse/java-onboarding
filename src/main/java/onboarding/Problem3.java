e08762e1225f58aea4d6c87fcab1f5895f53efa7
wonseok <tkddls23@konkuk.ac.kr>
2022-10-31 21:07:01 +0900

    feat: 3-2 해당 숫자까지 쳐야하는 박수의 횟수 return 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,7 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = countNumberClap(number);
-        return answer;
+        return countTotalClap(number);
     }
 
     private static int countNumberClap(int number) {
@@ -13,6 +12,14 @@ public class Problem3 {
         for(int i = 0; i < numString.length(); i++) {
             if(numString.charAt(i) == '3' || numString.charAt(i) == '6' || numString.charAt(i) == '9')
                 count += 1;
+        }
+        return count;
+    }
+
+    private static int countTotalClap(int number) {
+        int count = 0;
+        for (int  i= 1; i <= number; i++) {
+            count += countNumberClap(i);
         }
         return count;
     }
