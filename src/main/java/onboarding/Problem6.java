1a70ccd7d02ec77bcd935100da238dee6dd7b906
c4fiber <qudcjf153@gmail.com>
2022-10-31 11:33:22 +0900

    feat(prob6) : 이름을 연속되는 두글자가 되도록 쪼개는 기능 구현
    각 이름을 연속되는 두글자로 쪼개서 list에 모음 -> 이를 활용해서 연속되는 두글자가 중복인지 확인한다.
    
    answer = List.of("answer") 에서는 answer에 imutable list가 들어가게 되므로 수정이 불가능해진다. 그래서 ArrayList로 변경함

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    private static List<List<String>> candidates = new ArrayList<List<String>>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+
+        for(List<String> person : forms) {
+            List<String> words = collectTwoCharacters(person.get(0));
+
+
+            candidates.add(words);
+        }
+
         return answer;
     }
+
+    /**
+     * 입력받은 이름을 2글자가 되도록 쪼개서 모은다 (ex: 제이슨 -> 제이,이슨)
+     */
+
+    private static List<String> collectTwoCharacters(String name) {
+        ArrayList<String> words = new ArrayList<>();
+        for(int i=0; i<name.length()-1; i++) {
+            String twoCharacter = String.valueOf(name.charAt(i) + name.charAt(i+1));
+            words.add(twoCharacter);
+        }
+
+        return words;
+    }
+
 }
