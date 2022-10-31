376ed8bb4efb6e9cf1871be8a0eec9489813aebd
mikekang47 <gyeong0308@gmail.com>
2022-10-31 23:11:38 +0900

    Feat<problem5>: add getMoneyArray method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /**
+     * 크기별 돈을 담은 정적 배열을 리턴한다.
+     * @return 돈을 담은 배열
+     */
+    public static int[] getMoneyArray() {
+        return new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
