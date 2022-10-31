342fa165575626a75688fddc25b16e16007ea97f
sunghyuki <sunghyuk1609@gmail.com>
2022-11-01 00:10:43 +0900

    feat : 알파벳 외의 문자 검사 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (isNonAlphabet(c)) {
+                sb.append(c);
+                continue;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static boolean isNonAlphabet(char c) {
+        if (!Character.isLetter(c) || Character.isWhitespace(c)) {
+            return true;
+        }
+        return false;
     }
 }
