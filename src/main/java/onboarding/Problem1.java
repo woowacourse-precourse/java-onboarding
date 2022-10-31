a22f7679ba9d4821a3ba753ad8d8a76269336b55
wjh2012 <wjh2012@naver.com>
2022-10-31 17:41:27 +0900

    (PROBLEM1)페이지 번호(int)를 받아 배열(int[])로 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,20 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    // 페이지 번호(int)를 받아 배열(int[])로 반환
+    private static int[] intToArray(int num){
+
+        return Stream
+                .of(String.valueOf(num).split(""))
+                .mapToInt(Integer::parseInt).
+                toArray();
     }
 }
