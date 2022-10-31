ef66cf469e989f3a0d5f6a675e85d2d4230d96e0
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-01 01:26:56 +0900

    Problem5_Init

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/*
+ - 지폐가 가장 적도록
+ - 오만원, 만원, 오천원, 천원, 오백원, 백원, 오십원, 십원, 일원
+ - 금액이 큰 순서대로 리스트 / 배열에 담아 리턴
+
+ */
+
+
 import java.util.Collections;
 import java.util.List;
 
