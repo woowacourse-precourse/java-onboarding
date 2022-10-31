2b7c290380989360a28d205f6b86013c83fe39e0
TaemHam <tk4003950@gmail.com>
2022-10-31 09:38:18 +0900

    Feat: Implement problem 5 main solution

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,40 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private final List<Integer> moneys = List.of (
+            50000,
+            10000,
+            5000,
+            1000,
+            500,
+            100,
+            50,
+            10,
+            1
+    );
+
+    private static boolean isValidInput(int money) {
+
+        return false;
+    }
+
+    private static List<Integer> exchangeMoney(int money) {
+
+        return Collections.emptyList();
+    }
+
+    /*
+     * Problem5의 솔루션 코드입니다.
+     *
+     * @param 지폐로 바꿀 액수입니다.
+     * @return 교환할 지폐의 배열입니다.
+     * */
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        if (isValidInput(money) == false) {
+            return Collections.emptyList();
+        }
+        List<Integer> answer = exchangeMoney(money);
         return answer;
     }
 }
