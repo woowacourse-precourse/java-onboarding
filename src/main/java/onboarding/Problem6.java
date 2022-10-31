0866b7b0b1e31f9c2082aa1aeb173c2fdd0e02df
angelbaek <bsh1004ho@naver.com>
2022-10-31 20:36:52 +0900

    finish

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,40 +1,46 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");       
-        answer = new ArrayList<String>();
-		String targetOne="";
+		List<String> answer = List.of("answer");
+		answer = new ArrayList<String>();	
+		String target="";
 		String targetTwo="";
 		String temp="";
-		for(int i=0; i<forms.size(); i++) {		
-			targetOne = forms.get(i).get(1); 
-			if(targetOne.length()>1) {
-				for(int j=0; j<targetOne.length()-1; j++) {
+		for(int i=0; i<forms.size(); i++) {			
+			target = forms.get(i).get(1); // 제이엠
+			if(target.length()>1) {
+				for(int j=0; j<target.length()-1; j++) {
 					temp="";
-					temp += targetOne.charAt(j);
-					temp += targetOne.charAt(j+1);
-					System.out.println(temp);
-					for(int k=1; k<forms.size(); k++) { // 1~4
-						targetTwo=forms.get(k).get(1); // 제이슨						
-						System.out.println("비교대상: "+targetTwo);						
-						if(targetTwo.indexOf(temp)>=0) { // 중복일때 
+					temp += target.charAt(j);
+					temp += target.charAt(j+1);
+					System.out.println(temp);					
+					for(int k=1; k<forms.size()-1; k++) { // 1~3 마지막은 비교할 필요가 없음
+						targetTwo=forms.get(k).get(1);
+						System.out.println("비교대상: "+targetTwo);	
+						if(k==forms.size()-1) System.out.println("끝!");
+						if(targetTwo.indexOf(temp)>=0) { // 중복일때							
 							String email = forms.get(k).get(0);
-							System.out.println(email);
-							if(answer.size()==0) {
-								System.out.println("첫번째 이메일 추가했음");
+							System.out.println("중복된 닉네임 이메일: "+email);
+							if(answer.size()==0) { //
 								answer.add(email);
+								answer.add(forms.get(j).get(0));
 							}
 							else {
-								for(int s=0; s<answer.size(); s++) {
-									if(answer.get(s)==email) {
-										break;
-									}else if(s==answer.size()-1 && answer.get(s)!=email) {
+								for(int s=0; s<answer.size(); s++) {									
+									if(forms.get(i).get(0).equals(forms.get(s).get(0))) continue;
+									System.out.println("s는: "+s);
+									System.out.println("비교 이메일: "+answer.get(s)+" / answer Size: "+answer.size());
+									System.out.println("현재 이메일: "+email);									
+									if(answer.get(s).equals(email)) break;										
+									else if(answer.get(s)!=(email) && s==answer.size()-1) {
 										answer.add(email);
-										System.out.println("추가했음!!");
+										System.out.println("추가한 이메일");
+										System.out.println("email:"+email+" / answer.get(s):"+answer.get(s));
 									}
 								}
 							}
@@ -42,7 +48,8 @@ public class Problem6 {
 					}
 				}				
 			}			
-		}        
-        return answer;
+		}	
+		Collections.sort(answer);		
+		return answer;
     }
 }
