ce78d9e3e12245ff25411a0567faf5381a1e2aa0
YUSEONJUN <seonjun0906@gmail.com>
2022-10-31 21:37:26 +0900

    [Problem 1] calculate 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,22 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int answer = 0;
+
+
     }
+
+    public static int calculate(int n) {
+        int sum = 0;
+        int mul = 1;
+        while (n != 0) {
+            sum += n % 10;
+            mul *= n % 10;
+            n /= 10;
+        }
+        return Math.max(sum, mul);
+    }
+
 }
