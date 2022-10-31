3bdcf4b5e075474241871fb83de2436ea6f41328
ensk26 <tjensk26@gmail.com>
2022-10-31 21:17:43 +0900

    docs: 기능 목록 구성, problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,8 +4,25 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    //각 아이디의 친구인 관계를 저장
+    static void saveFriendById(String id) {
+
+    }
+
+    //사용자와 함께 아는 친구의 수의 점수 저장
+    static void saveFriendScoreByUser(String user) {
+
+    }
+
+    //방문한 횟수의 점수 저장
+    static void saveVisitScore(String id) {
+
     }
 }
