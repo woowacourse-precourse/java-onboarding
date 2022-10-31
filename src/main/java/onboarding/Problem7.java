01918c5be9a63dc5c853d5c8cc9b106d371e5b82
iamjooon2 <iamjooon2@gmail.com>
2022-11-01 00:56:37 +0900

    feat(problem7): 사용자의 친구 이름을 저장하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,15 +1,18 @@
 package onboarding;
 
-
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> friendList = new ArrayList<>();
 
         for (int i = 0; i < friends.size(); i += 1) {
-            if (friends.get(i).get(1) == user || friends.get(i).get(0) == user){
-
+            if (friends.get(i).get(1) == user){
+                friendList.add(friends.get(i).get(0));
+            } else if (friends.get(i).get(0) == user) {
+                friendList.add(friends.get(i).get(1));
             }
         }
 
