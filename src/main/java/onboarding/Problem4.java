60fb8657cc3108d8f0829debe9ffe8f682dac126
James <rlagurwo6209@naver.com>
2022-10-31 23:18:41 +0900

    문자열 바꾸기 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,28 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = solutionAnswer(word);
         return answer;
     }
+
+    static String solutionAnswer(String inputString){
+        String answer = "";
+        String s = inputString;
+
+        for(int i=0;i<s.length();i++) {
+            char k = s.charAt(i);
+            if(Character.isUpperCase(k)) {
+                answer += (char)(155 - k);
+
+            }
+            else if(Character.isLowerCase(k)) {
+                answer += (char)(219 - k);
+            }
+            else {
+                answer += k;
+            }
+        }
+        return answer;
+    }
+
 }
