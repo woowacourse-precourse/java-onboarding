b0f60b2dbb3c6179b487206d119f177aa6aa87bb
cocomozila <joseyun94@naver.com>
2022-10-31 18:47:20 +0900

    [문제 3] 문자열로 받은 숫자의 자릿수마다 박수치는 숫자인지 탐색하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,16 @@ public class Problem3 {
     public static int solution(int number) {
         return 1;
     }
+    // 문자열로 받은 숫자의 자릿수마다 박수치는 숫자인지 탐색하는 기능
+    private static int howManyClap(String number) {
+        int clapTotal = 0;
+        for (int i = 0; i < number.length(); i++) {
+            if (isDigitClapNumber(number.charAt(i))) {
+                clapTotal += 1;
+            }
+        }
+        return clapTotal;
+    }
     // 해당 문자열 타입의 숫자가 박수치는 숫자인지 판별하는 기능
     private static boolean isDigitClapNumber(char digit) {
         for (char c : CLAP_NUMBER) {
