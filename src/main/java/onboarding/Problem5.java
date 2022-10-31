79744ca7dc68811c987a99c7b9c70f002348ff45
swjeong98 <tree5678@konkuk.ac.kr>
2022-10-31 18:16:55 +0900

    문제 5 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int ConvertMoney(int money, int target){
+        return money/target;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        int[] pivot = {50000, 10000, 1000, 500, 100, 50, 10, 1};
+        for(int i=0;i<pivot.length;i++){
+            answer.add(ConvertMoney(money, pivot[i]));
+            money /= pivot[i];
+        }
+
         return answer;
     }
 }
