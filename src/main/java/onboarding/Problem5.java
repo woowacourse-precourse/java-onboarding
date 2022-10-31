c317e032072db02ce2aad6bebc491a921ba32635
tgoddessana <twicegoddessana1229@gmail.com>
2022-10-31 12:40:42 +0900

    solve : Problem5 풀이

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        List<Integer> coinTypes = asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        for (int type : coinTypes) {
+            int a = money / type;
+            answer.add(a);
+            money = money % type;
+        }
+        System.out.println(answer);
         return answer;
     }
 }
