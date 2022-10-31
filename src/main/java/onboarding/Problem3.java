d07afab4519c6ab7ce737560ae6385aba8b45982
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:32:18 +0900

    [Problem3] 기능목록 작성하기 (김민서)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+    /**
+     [기능목록]
+     1. 숫자를 문자 배열로 만들기
+     2. 문자 배열에서 3, 6, 9인 값 찾아서 1 더하기
+     */
     public static int solution(int number) {
         int answer = 0;
         return answer;
