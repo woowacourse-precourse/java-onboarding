cf7a4d732dd674dcfab512c60b74c313d68763a7
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-01 06:24:03 +0900

    problem2 feat : calculate solution by recursion

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
+import java.util.*;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        StringBuffer str = new StringBuffer(cryptogram);
+        String answer = checkOverLapByRecur(str, str.length());
+        System.out.println(answer);
         return answer;
+    }
+    public static String checkOverLapByRecur(StringBuffer str, int len){
+        StringBuffer tempStr = str;
+        for(int i=0; i<len; i++){
+            System.out.println("i : " + i);
+            if(i+1<tempStr.length()){
+                if(tempStr.charAt(i) == tempStr.charAt(i+1)){
+                    tempStr.deleteCharAt(i);
+                    tempStr.deleteCharAt(i);
+                    len -= 2;
+                    checkOverLapByRecur(tempStr,len);
+                }
+            }
+        }
+        return tempStr.toString();
     }
 }
