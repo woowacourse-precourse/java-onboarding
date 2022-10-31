ad602426d66f2d883d96ddab528e02e7d673c9a3
Soomin Lim <augustth511@gmail.com>
2022-10-31 20:41:32 +0900

    리팩토링: count -> count369로 메서드 명 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,7 @@ public class Problem3 {
         int answer = 0;
 
         for (int i = 1; i <= number; i++) {
-            answer += count(i);
+            answer += count369(i);
         }
 
         return answer;
@@ -14,13 +14,15 @@ public class Problem3 {
     /**
      * 매개변수로 주어진 n에 들어가는 3, 6, 9의 개수를 구해 리턴
      */
-    public static int count(int n) {
+    public static int count369(int n) {
         int cnt = 0;
 
         while (n > 0) {
             int tmp = n % 10;
+
             if (tmp == 3 || tmp == 6 || tmp == 9)
                 cnt++;
+
             n /= 10;
         }
 
