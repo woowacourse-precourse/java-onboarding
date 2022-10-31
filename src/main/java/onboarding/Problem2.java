52ab1dfcd8023f1832beb6b15e805946e08694ba
OhHongGi <5ghdrl@naver.com>
2022-11-01 00:56:30 +0900

    chore: Implement removeDuplicateCharacter & Solve Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,14 +5,31 @@ import java.util.Stack;
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return removeDuplicateCharacter(cryptogram);
     }
 
     /*
      주어진 문자열의 중복 문자를 제거하여 반환
      */
-    public static String removeDuplicateCharactor(String cypherText) {
-        return "";
+    public static String removeDuplicateCharacter(String cypherText) {
+        Stack<Character> detectMultiple = new Stack<>();
+        char[] cypherTextToCharList = cypherText.toCharArray();
+        String result = "";
+
+        for (char c : cypherTextToCharList) {
+            if (detectMultiple.isEmpty()) {
+                detectMultiple.add(c);
+            } else if (detectMultiple.peek().equals(c)) {
+                detectMultiple.pop();
+            } else if (!detectMultiple.peek().equals(c)) {
+                detectMultiple.add(c);
+            }
+        }
+        while (!detectMultiple.isEmpty()) {
+            result += detectMultiple.pop();
+        }
+        StringBuilder buffer = new StringBuilder(result);
+        result = buffer.reverse().toString();
+        return result;
     }
 }
