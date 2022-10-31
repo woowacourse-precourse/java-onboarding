3158f474a7e5f9c896f3f362ec8608d103ddfd44
jojaeng2 <ds4ouj@naver.com>
2022-10-31 14:18:39 +0900

    feat : 사용자의 친구 목록을 만드는 기능구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,44 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    static class Sns {
+        private final String owner;
+        private final Map<String, Integer> scores;
+        private final List<List<String>> allFriends;
+        private final List<String> visitors;
+        private final Set<String> earlyFriends;
+
+        public Sns(String owner, List<List<String>> friends, List<String> visitors) {
+            this.owner = owner;
+            this.allFriends = friends;
+            this.visitors = visitors;
+            this.scores = new HashMap<>();
+            this.earlyFriends = new HashSet<>();
+        }
+
+        private void createFriendsList() {
+            allFriends.stream()
+                    .filter(friend -> friend.contains(owner))
+                    .forEach(this::insertFriends);
+            removeOwner();
+        }
+
+        private void insertFriends(List<String> pair) {
+            String friendA = pair.get(0);
+            String friendB = pair.get(1);
+            earlyFriends.add(friendA);
+            earlyFriends.add(friendB);
+        }
+
+        private void removeOwner() {
+            earlyFriends.remove(owner);
+        }
     }
 }
