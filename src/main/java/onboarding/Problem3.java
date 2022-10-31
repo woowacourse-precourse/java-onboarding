03efc6971bfb42b2128f3d3df07df9c11755e0c4
es <mascoeskim@gmail.com>
2022-11-01 07:50:55 +0900

    Problem3/4.리팩터링2

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,18 +2,16 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        String numberList ="";
-        for (int k=1;k<=number;k++){
-            numberList += Integer.toString(k);
+        StringBuilder numberList = new StringBuilder();
+        for (int k = 1; k <= number; k++){
+            numberList.append(k);
         }
-//1부터 input 값까지의 숫자를 모두 스트링 형태로 numberList 변수에 추가
         int answer = 0;
-        for (int k=0;numberList.length()>k;k++){
-            if (numberList.charAt(k)=='3'||numberList.charAt(k)=='6'||numberList.charAt(k)=='9'){
-                answer +=1;
+        for (int k = 0; numberList.length() > k; k++){
+            if (numberList.charAt(k) == '3' || numberList.charAt(k) == '6' || numberList.charAt(k) == '9'){
+                answer += 1;
             }
         }
-//numberList의 charAt(k)값이 3,6,9이면 0부터 시작하는 answer에 +1을 주도록함 블록커 : charAt으로 조건문을할때 비교대상을 char을 나타내는 작은 따옴표로 감싸야 한다.
         return answer;
     }
 }
