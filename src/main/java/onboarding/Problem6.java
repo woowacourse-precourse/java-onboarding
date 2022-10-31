643b4d8e16b6bff2eb3e8c2435e6f1cb4b3b14cd
jin-chang-ho <ckdgh8204@naver.com>
2022-10-31 14:06:57 +0900

    Annotate_Optimize_Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,87 +5,90 @@ import java.util.List;
 import java.util.Collections;
 
 public class Problem6 {
-    public static ArrayList<ArrayList<String>> check_email(List<List<String>> check) {
-        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+    // 적합한 이메일 형식인지 판단하는 함수
+    public static List<List<String>> email_check(List<List<String>> check) {
+        List<List<String>> correct_result = new ArrayList<List<String>>();
 
         for (int i = 0; i < check.size(); i++) {
             if ((11 <= check.get(i).get(0).length()) && (check.get(i).get(0).length() < 20)) {
-
                 int check_index = check.get(i).get(0).indexOf("@");
 
                 if (check_index != -1 && check.get(i).get(0).substring(check_index + 1, check.get(i).get(0).length()).equals("email.com")) {
-                    ArrayList<String> temp_list = new ArrayList<String>();
+                    List<String> temp_list = new ArrayList<String>();
                     temp_list.add(check.get(i).get(0));
                     temp_list.add(check.get(i).get(1));
-                    result.add(temp_list);
+                    correct_result.add(temp_list);
                 }
             }
         }
 
-        return result;
+        return correct_result;
     }
 
-    public static ArrayList<ArrayList<String>> check_duplicate(ArrayList<ArrayList<String>> check) {
-        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-        ArrayList<Integer> compare_index = new ArrayList<Integer>();
+    // 글자 중복을 확인하는 함수
+    public static List<List<String>> check_duplicate(List<List<String>> check_forms) {
+        List<List<String>> remove_duplicate_forms = new ArrayList<List<String>>();
+        List<Integer> duplicate_index = new ArrayList<Integer>();
 
-        for (int i = 0; i < check.size() - 1; i++) {
-            String base_str = check.get(i).get(1);
-            ArrayList<String> base_array = new ArrayList<String>();
+        for (int i = 0; i < check_forms.size() - 1; i++) {
+            String base_str = check_forms.get(i).get(1);
+            List<String> base_array = new ArrayList<String>();
 
             for (int temp_index = 0; temp_index < base_str.length() - 1; temp_index++) {
                 base_array.add(base_str.substring(temp_index, temp_index + 2));
             }
 
-            for (int j = i + 1; j < check.size(); j++) {
-                String compare_str = check.get(j).get(1);
+            for (int j = i + 1; j < check_forms.size(); j++) {
+                String compare_str = check_forms.get(j).get(1);
                 for (int k = 0; k < base_array.size(); k++) {
                     if (compare_str.contains(base_array.get(k))) {
-                        compare_index.add(j);
+                        duplicate_index.add(j);
                     }
                 }
             }
 
-            if (!compare_index.isEmpty()) {
-                result.add(check.get(i));
+            if (!duplicate_index.isEmpty()) {
+                remove_duplicate_forms.add(check_forms.get(i));
 
-                for (int index : compare_index) {
-                    result.add(check.get(index));
+                for (int index : duplicate_index) {
+                    remove_duplicate_forms.add(check_forms.get(index));
                 }
 
-                for (int index : compare_index) {
-                    check.remove(index);
+                for (int index : duplicate_index) {
+                    check_forms.remove(index);
                 }
+
+                duplicate_index.clear();
             }
-
-            compare_index.clear();
         }
 
-        return result;
+        return remove_duplicate_forms;
     }
 
-    public static List<String> get_nickname(ArrayList<ArrayList<String>> check) {
-        List<String> result = new ArrayList<String>();
+    // 닉네임을 가져와 정렬하는 함수
+    public static List<String> get_nickname(List<List<String>> remove_duplicate_forms) {
+        List<String> nicknames = new ArrayList<String>();
 
-        for (int i = 0; i < check.size(); i++) {
-            result.add(check.get(i).get(0));
+        for (int i = 0; i < remove_duplicate_forms.size(); i++) {
+            nicknames.add(remove_duplicate_forms.get(i).get(0));
         }
 
-        Collections.sort(result);
+        Collections.sort(nicknames);
 
-        return result;
+        return nicknames;
     }
 
+    // 작동 함수
     public static List<String> solution (List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
-        ArrayList<ArrayList<String>> checkform = check_email(forms);
+        List<List<String>> check_forms = email_check(forms);
 
-        ArrayList<ArrayList<String>> duplicate = check_duplicate(checkform);
+        List<List<String>> remove_duplicate_forms = check_duplicate(check_forms);
 
-        List<String> temp_answer = get_nickname(duplicate);
+        List<String> nicknames = get_nickname(remove_duplicate_forms);
 
-        answer = temp_answer;
+        answer = nicknames;
 
         return answer;
     }
