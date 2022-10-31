0f383d1c4d15a58fdcd2a1b06dac32c8f1bfb77e
KimYongJun <rladydwns129@naver.com>
2022-11-01 01:39:45 +0900

    feat(Problem7): Add ID class & priority queue to sort

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,31 @@ import java.util.*;
 public class Problem7 {
     private static HashSet<String> userFriend;
     private static HashMap<String, Integer> friendList;
+    private static PriorityQueue<ID> priorityRank;
+
+    private static class ID implements Comparable<ID> {
+        String name;
+        int score;
+
+        public ID (String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        @Override
+        public int compareTo(ID o) {
+            if (this.score == o.score) {
+                return this.name.compareTo(o.name);
+            }
+            return (o.score - this.score);
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         userFriend = new HashSet<>();
         friendList = new HashMap<>();
+        priorityRank = new PriorityQueue<>();
         userFriend.add(user);
 
         for (List l : friends) {
@@ -17,6 +38,22 @@ public class Problem7 {
 
             addFriendNetwork(user, s1, s2);
 
+        }
+
+        for (List l : friends) {
+            String s1 = (String) l.get(0);
+            String s2 = (String) l.get(1);
+
+            plusScoreInFriends(s1, s2);
+            plusScoreInFriends(s2, s1);
+        }
+
+        for (String s : visitors) {
+            plusScoreInVisitors(s);
+        }
+
+        for (String s : friendList.keySet()) {
+            priorityRank.offer(new ID(s, friendList.get(s)));
         }
 
         return answer;
@@ -36,6 +73,18 @@ public class Problem7 {
         }
         if (!friendList.containsKey(secondPerson)) {
             friendList.put(secondPerson, 0);
+        }
+    }
+
+    private static void plusScoreInFriends(String firstPerson, String secondPerson) {
+        if (userFriend.contains(firstPerson) && !userFriend.contains(secondPerson)) {
+            friendList.put(secondPerson, friendList.getOrDefault(secondPerson, 0) + 10);
+        }
+    }
+
+    private static void plusScoreInVisitors(String visitor) {
+        if (!userFriend.contains(visitor)) {
+            friendList.put(visitor, friendList.getOrDefault(visitor, 0) + 1);
         }
     }
 }
