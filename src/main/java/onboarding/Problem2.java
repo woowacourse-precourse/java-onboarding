d67ad1861f04f486cc2786e6b65990b120180051
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 02:03:49 +0900

    feat(PROBLEM2): 중복 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,7 +4,20 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        return null;
+        return removeDupli(cryptogram);
+    }
+
+    public static String removeDupli(String crypt){
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : crypt.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == ch)
+                stack.pop();
+            else
+                stack.add(ch);
+        }
+
+        return stackToString(stack);
     }
 
     public static String stackToString(Stack<Character> stack){
