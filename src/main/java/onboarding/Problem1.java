12299528040587368771147e104c46891cf2da15
YUSEONJUN <seonjun0906@gmail.com>
2022-10-31 21:54:02 +0900

    [Problem 1] 예외 사항 조건 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,13 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = 0;
+        if (pobi.size() !=2 || crong.size() != 2 ||pobi.get(1) - pobi.get(0) != 1
+            || crong.get(1) - crong.get(0) != 1 || pobi.get(0) == 1 || crong.get(0)== 1
+            || pobi.get(1) == 2 || crong.get(1) == 2 || pobi.get(0) == 399
+            || crong.get(0)== 399 || pobi.get(1) == 400 || crong.get(1) == 400
+            || pobi.get(0) % 2 != 1 || crong.get(0) % 2 != 1 || pobi.get(1) % 2 != 0 || crong.get(1) % 2 != 0) {
+            return -1;
+        }
 
 
     }
