44a68cbf01be0ab6a0ec5bfa29ab2f8048d30cf3
swjeong98 <tree5678@konkuk.ac.kr>
2022-10-31 17:32:37 +0900

    문제 3 기능1, 기능 2 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    public static int isClip(int x){
+        if(x == 3 || x == 6 || x == 9) return 1;
+        else return 0;
+    }
+    public static int getTotalClapCount(int number){
+        int count = 0;
+        while(number != 0){
+            count += isClip(number % 10);
+            number /= 10;
+        }
+        return count;
+    }
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int result = 0;
+        for(int i=1; i<=number; i++) result += getTotalClapCount(i);
+        return result;
     }
 }
