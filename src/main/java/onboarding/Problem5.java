5732b18403913e59c4d784cff324b24942ec2a9b
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:43:34 +0900

    [Problem5] 기능목록 작성하기(김민서)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /**
+     [기능목록]
+     1. 화폐 단위로 이루어진 숫자 배열 만들기
+     2. 큰 금액의 화폐 순서로 돈의 액수 나누어서 몫을 배열에 추가하기
+     3. 나눈 나머지 값을 돈의 액수 값으로 변경하기
+     */
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
         return answer;
     }
 }
