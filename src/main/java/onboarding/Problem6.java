f12fa0db4e427295142e9f042835ea76defd14bc
Lee-Jiseung <ljss98@naver.com>
2022-10-31 18:08:16 +0900

    feat: problem6 닉네임 중복 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,18 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static boolean isDuplicated(String n1, String n2) {
+        for (int i = 0; i < n1.length() - 1; i++) {
+            for (int j = 0; j < n2.length() - 1; j++) {
+                if (n1.charAt(i) != n2.charAt(j)) {
+                    continue;
+                }
+                if (n1.charAt(i + 1) == n2.charAt(j + 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
