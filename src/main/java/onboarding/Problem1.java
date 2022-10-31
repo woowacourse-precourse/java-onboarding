cb65cff979d8461176b03945ad5cfe53d80c86f4
jhsseonn <hosu0125@gmail.com>
2022-11-01 02:40:23 +0900

    feat: (Problem1) 합, 곱 수행

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,35 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiMax = 0;
+        int crongMax = 0;
+
         return answer;
     }
+
+
+    static int sumCal(int[] array){
+        int sum=0;
+        for (int num:array) {
+            sum+=num;
+        }
+
+        return sum;
+    }
+
+    static int mulCal(int[] array){
+        int multiple=1;
+        for (int num:array) {
+            multiple*=num;
+        }
+
+        return multiple;
+    }
+
 }
