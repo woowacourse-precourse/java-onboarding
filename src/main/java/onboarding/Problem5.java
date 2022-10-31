19abccee6f607d9509bdd0a2960ef35760a1b164
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-31 21:23:11 +0900

    문제 5 : 기능 1 - greedy하게 큰 금액의 화폐부터 담는 기능.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int Money;
+
+    public static int getMoney(int unit) {
+        int result = Money / unit;
+        Money %= unit;
+        return result;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        Money = money;
+
+        List<Integer> units = new ArrayList<Integer>() {
+            {
+                add(50000); add(10000); add(5000); add(1000);
+                add(500); add(100); add(50); add(10); add(1);
+            }
+        };
+
+        List<Integer> answer = new ArrayList<>();
+        for (Integer i : units) {
+            answer.add(getMoney(i.intValue()));
+        }
+
         return answer;
     }
 }
