fdfc6eb82d4e022868871507afb1e8a84be46ea9
sami355-24 <ddung355@gmail.com>
2022-11-01 02:59:21 +0900

    [add] SumofAZ and Sumofaz

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static Integer SumofAZ = 'A'+'Z';
+    public static Integer Sumofaz = 'a'+'z';
     public static String solution(String word) {
         String answer = "";
         return answer;
