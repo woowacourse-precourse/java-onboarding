926253dae93f17a9bd0fdd5ecefe5196774d1f0a
김선동 <mok03127@hanyang.ac.kr>
2022-11-01 01:12:48 +0900

    feat: 문제 4 대문자인지 확인하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        /*
+         * 대문자 혹은 소문자라면 청개구리 사전대로 변환해서 결과를 구한다.
+         */
         String answer = "";
+
         return answer;
+    }
+
+    public static boolean isUpperAlpha(Character ch) {
+        return ch >= 'A' && ch <= 'Z';
     }
 }
