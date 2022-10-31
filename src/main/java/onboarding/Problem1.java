5f8cb3871d726d15e9f5eefd90d716749b2dc2b5
이정우 <acg6138@naver.com>
2022-10-31 09:54:48 +0900

    feat(Problem1): 페이지 값을 검증하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,18 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static boolean validateInput(List<Integer> input){
+        if (input == null) {
+            // 페이지 번호가 null 인지 확인
+            return false;
+        }else if (input.get(0) == 0 && input.get(0)>= 400 && input.get(1) == 0 && input.get(1)>= 400){
+            // 페이지의 범위가 1-400 인지 확인
+            return false;
+        }else if (input.get(0) % 2 != 1 || input.get(1) != input.get(0)+1) {
+            // 왼쪽 페이지 번호가 홀수이고, 오른쪽 페이지 번호가 왼쪽 페이지 번호 + 1 인지 확인
+            return false;
+        }
+        return true;
+    }
 }
