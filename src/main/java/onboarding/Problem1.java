9a669f369e549171d2e9c03bafeb2ff4d66abdf3
hojunking96 <junsong96@naver.com>
2022-11-01 00:54:50 +0900

    P1 feat 덧셈, 곱셈, 최대값 반환 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,7 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
     public static boolean hasError(List<Integer> pages) {
         if (pages.get(0) <= 0 || pages.get(1) <= 0
                 || pages.get(0) >= 401 || pages.get(1) >= 401
@@ -14,5 +15,30 @@ class Problem1 {
             return true;
         else
             return false;
+    }
+
+    public static int findMax(List<Integer> pages) {
+        int LeftMax = Math.max(addPage(pages.get(0)), multiplyPage(pages.get(0)));
+        int RightMax = Math.max(addPage(pages.get(1)), multiplyPage(pages.get(1)));
+        int Max = Math.max(LeftMax, RightMax);
+        return Max;
+    }
+
+    public static int addPage(int pageNum) {
+        int result = 0;
+        while (pageNum > 0) {
+            result += pageNum % 10;
+            pageNum /= 10;
+        }
+        return result;
+    }
+
+    public static int multiplyPage(int pageNum) {
+        int result = 1;
+        while (pageNum > 0) {
+            result *= pageNum % 10;
+            pageNum /= 10;
+        }
+        return result;
     }
 }
