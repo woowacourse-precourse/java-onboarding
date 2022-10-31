c3933da96b26e6e2069a7d93e20be5d63f1e456b
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 15:39:02 +0900

    cryptoAvail에서 연속되는 문자가 있는 범위의 처음과 끝 index를 start와 end에 저장

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,10 +3,16 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        String cryptoAvail = cryptogram;
         boolean flag = true;
 
         while (flag) {
-
+            for (int start = 0; start < cryptoAvail.length()-1; start++) {
+                int end = 0;
+                for (int i = start; i < cryptoAvail.length()-1; i++)  {
+                    if (cryptoAvail.charAt(i) != cryptoAvail.charAt(i+1)) break;
+                    if (cryptoAvail.charAt(i) == cryptoAvail.charAt(i+1)) end = i+1;
+                }
         }
 
         return answer;
