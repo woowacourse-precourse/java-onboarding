88ca90d9ec6c1995112695b8bb78a2adee745aa3
unknown <2021110806@dgu.ac.kr>
2022-10-31 23:23:52 +0900

    fix:problem1 multiple method, add method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,14 +3,31 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static int multiple(Integer num) {
+        int sum=1;
+        String stringNum = num.toString();
+        for (int i=0; i<stringNum.length();i++) {
+            sum*=Character.getNumericValue(stringNum.charAt(i));
+        }
+        return sum;
+    }
+
+    public static int add(Integer num) {
+        int sum=0;
+        String stringNum = num.toString();
+        for (int i=0; i<stringNum.length();i++) {
+            sum+=Character.getNumericValue(stringNum.charAt(i));
+        }
+        return sum;
+    }
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE''
 
-        int pobiMax=-0;
-        int crongMax=0;
+        int pobiMax=-1;
+        int crongMax=-1;
         boolean error = false;
-
-
 
         if (error) {
             answer=-1;
