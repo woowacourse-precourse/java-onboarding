35f7e5530f077b95c145d9c12454e0d79d2c91bf
polarisjonny <joeun705@gmail.com>
2022-10-31 19:18:20 +0900

    221031 problem1 freecourse1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-
+//
         int pLeft = pobi.get(0);
         int pRight = pobi.get(1);
 
