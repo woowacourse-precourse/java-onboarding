e42535d869c124b280431ddbc86429516a244019
한은영 <oneeqzero@gmail.com>
2022-10-31 17:11:10 +0900

    feat: problem6 사용자 이름 중복리스트 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,27 +3,21 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
-    static Map<String, Set<String>> dup_list = new HashMap<String, Set<String>>(){
-        @Override
-        public Set<String> get(Object key){
-            Set<String> set = super.get(key);
-            if(set == null){
-                set = new HashSet<String>();
-                put((String) key,set);
-            }
-            return set;
-        }
-    };
-    static Map<String, String> user_list = new HashMap<String, String>();
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+    static Set<String> dup_list = new HashSet<String>();
+
+    public static void make_duplist(List<List<String>> forms){
         for(List<String> f : forms){
-            String email = f.get(0); String nickname = f.get(1);
+            String nickname = f.get(1);
             for(int i = 0; i<nickname.length()-1; i++){
                 String sub_nick = nickname.substring(i,i+2);
-                dup_list.get(nickname).add(sub_nick);
+                dup_list.add(sub_nick);
             }
         }
+    }
+    public static List<String> solution(List<List<String>> forms) {
+        List<String> answer = List.of("answer");
+        make_duplist(forms);
+
         return answer;
     }
 }
