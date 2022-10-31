2dc31a1e439fe6b1084c89db658d46d4e941d90f
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-01 01:52:53 +0900

    [PROBLEM 1] 수정
    예외사항의 경우 바로 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,7 +6,7 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        if(exception(pobi) || exception(crong)) answer = -1;
+        if(exception(pobi) || exception(crong)) return -1;
 
         int pobiMax = Math.max(checkMaxNum(pobi.get(0)), checkMaxNum(pobi.get(1)));
         int crongMax = Math.max(checkMaxNum(crong.get(0)), checkMaxNum(crong.get(1)));
