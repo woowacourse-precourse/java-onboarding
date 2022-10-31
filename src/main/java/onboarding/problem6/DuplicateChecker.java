6b7b8452ebbb1616e8e2ebc738d6e54030abc071
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:41:39 +0900

    [problem6] DuplicateChecker 플로우 작성

--- src/main/java/onboarding/problem6/DuplicateChecker.java
+++ src/main/java/onboarding/problem6/DuplicateChecker.java
@@ -1,7 +1,19 @@
 package onboarding.problem6;
 
+import java.util.List;
+
 public class DuplicateChecker {
+    UserRepository userRepository;
     //입력받은 정보 저장 리스트 클래스
+
+
     //데이터 배열입력받아 각요소별로 중복된지 확인하고 허가되면 리스트 클래스에 추가하는 함수
+    public void inputDatas(List<List<String>> datas){
+        for(var data: datas)this.userRepository.create(data);
+    }
+
     //리스트 클래스를 반환하는 함수
+    public List<String> getUsers(){
+        return this.userRepository.getUsers();
+    }
 }
--- src/main/java/onboarding/problem6/UserRepository.java
+++ src/main/java/onboarding/problem6/UserRepository.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class UserRepository {
     List<User> userRepository = new ArrayList<>();
 
+    public void create(List<String> data) {
+    }
+
     //userRepository에 User 추가하는 함수 이 때 모든 배열 요소에 체크 함수를 검사한다.
 
     //userRepository에서 email만 String 배열로 반환하는 함수
