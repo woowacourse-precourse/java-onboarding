6bbf3baad9959a7c4a253b33466c302a647a9f5c
eugene1204 <s1061001@naver.com>
2022-10-31 16:17:26 +0900

    p4, 1. 한글자씩 떼어서 대문자 vs 소문자 아스키코드로 비교 -> 해당하는 글자로 출력

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,18 @@ package onboarding;
 public class Problem4 {
 //    1. 한글자씩 떼어서 대문자 vs 소문자 아스키코드로 비교 -> 해당하는 글자로 출력
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String newWord ="";
+        char a = ' ';
+        for (int i =0; i<word.length(); i++){
+            int num = Integer.valueOf(word.charAt(i));
+            if(word.charAt(i)==' ') newWord +=" ";
+            if(num >64 && num <91) { // 대문자 아스키 코드 A ~Z : 65~91
+                a = (char) Math.abs(155-num);
+                newWord += String.valueOf(a);}
+            if(num >97 && num <122) { // 소문자 아스키 코드 a ~z : 98 ~122
+                a = (char) Math.abs(219-num);
+                newWord += String.valueOf(a);}
+        }
+        return newWord;
     }
 }
