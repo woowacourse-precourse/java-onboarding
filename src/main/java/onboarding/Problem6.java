b47d2bc51a92e01908ed6fd0fe1f9ea2a334ba97
jeonghoon <shb03323@naver.com>
2022-10-31 22:04:46 +0900

    feat: problem6 두 닉네임이 겹치는지 확인해주는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,16 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static boolean isOverlap(String nickname, String comparisonNickname) {
+        for (int i = 1; i < nickname.length(); i++) {
+            String nicknameToken = nickname.substring(i - 1, i + 1);
+
+            if (comparisonNickname.contains(nicknameToken)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
