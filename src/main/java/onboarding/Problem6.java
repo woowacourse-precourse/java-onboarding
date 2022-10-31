da9d726024abb04cafd23cd8448982690776c2b0
dongdong-119 <85717356+dongdong-119@users.noreply.github.com>
2022-10-31 20:45:31 +0900

    Problem 6 - 기능목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,19 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * <기능 목록>
+ *     1. 메소드
+ *          1) partNames
+ *          : 교육생들의 이름에서 길이 2이상인 이름의 부분집합을 뽑아낸다.
+ *          2) checkNames
+ *          : partNames에서 반환한 교육생들의 이름 부분집합에서 중복되는 멤버의 수가 2이상일 경우, 중복된 이름을 가진 것으로 간주하고 중복 멤버 리스트에 추가한다.
+ *            Set을 이용하여 중복 추가를 방지하고, return 전에 List에 옮겨 담는다.
+ *            List에 옮겨 담은 후 오름차순으로 정렬하여 반환한다.
+ *
+ *
+ */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
