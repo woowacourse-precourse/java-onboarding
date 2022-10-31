865102614668dc602122c468a1ff52162b2f4746
OhHongGi <5ghdrl@naver.com>
2022-11-01 02:58:58 +0900

    chore: Divide Function List in Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    /*
+     * 금액이 주어졌을 때 최소한의 화폐 개수를 구하여 반환
+     */
+    public static void withdraw(int money, List<Integer> moneyUnitCountList) {
+    }
+
+    /*
+     * 금액과 금액 단위가 주어졌을 때, 화폐 단위에 따른 개수 계산과 남은 금액을 계산하는 함수
+     */
+    public static int countMonetaryUnit(int money, int moneyUnit,
+        List<Integer> moneyUnitList) {
+        return 0;
     }
 }
