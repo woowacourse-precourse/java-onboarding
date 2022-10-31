b28ea89755c2688d7c25dea7329bfc54f30330ab
kim1387 <kevink0@naver.com>
2022-11-01 01:56:30 +0900

    feat: 예외 케이스 검증 로직 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,47 @@ import java.util.List;
 class Problem1 {
   public static int solution(List<Integer> pobi, List<Integer> crong) {
     int answer = Integer.MAX_VALUE;
+    if (!(validatePage(pobi) && validatePage(crong))) {
+      return -1;
+    }
     answer = getWinnerResult(pobi, crong);
     return answer;
+  }
+
+  public static boolean validatePage(List<Integer> page) {
+    if (!validateOddAndEven(page)) {
+      return false;
+    }
+    if (!validateRange(page.get(0))) {
+      return false;
+    }
+    if (!validateRange(page.get(1))) {
+      return false;
+    }
+    return true;
+  }
+
+  private static boolean validateOddAndEven(List<Integer> page) {
+    boolean isFirstNumOdd = isOdd(page.get(0));
+    boolean isSecondNumEven = isEven(page.get(1));
+
+    return isFirstNumOdd && isSecondNumEven && validateOrder(page);
+  }
+
+  public static boolean isOdd(int num) {
+    return num % 2 == 1;
+  }
+
+  public static boolean isEven(int num) {
+    return num % 2 == 0;
+  }
+
+  private static boolean validateRange(int num) {
+    return num > 1 && num < 400;
+  }
+
+  private static boolean validateOrder(List<Integer> page) {
+    return (page.get(0) + 1) == page.get(1);
   }
 
   public static int getWinnerResult(List<Integer> pobi, List<Integer> crong) {
