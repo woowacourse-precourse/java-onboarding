e7aff0be75e674ef3c2a131d82b005e0d61cb4ff
bjk1649 <qhwns1649@naver.com>
2022-11-01 03:38:48 +0900

    refactor: Problem7의 복잡한 for 문 리팩토링
    enhanced for 문과 stream, collection 이용하여 변경

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -29,10 +30,10 @@ class FriendInfoController {
         List<String> friendsRelation;
         int usersFriendIndex;
 
-        for (int i =0; i < friends.size(); i++) {
-            friendsRelation= friends.get(i);
+        for (List<String> friend : friends) {
+            friendsRelation = friend;
             usersFriendIndex = getUsersFriendIndex(user, friendsRelation);
-            if(usersFriendIndex != -1) {
+            if (usersFriendIndex != -1) {
                 usersFriends.add(friendsRelation.get(usersFriendIndex));
                 continue;
             }
@@ -50,8 +51,8 @@ class FriendInfoController {
     private static void checkFriends(Map<String, Integer> friendAndScore, List<List<String>> friends, Set<String> usersFriends) {
         List<String> friendsRelation;
 
-        for (int i = 0; i < friends.size(); i++) {
-            friendsRelation = friends.get(i);
+        for (List<String> friend : friends) {
+            friendsRelation = friend;
             if (usersFriends.contains(friendsRelation.get(0))
                     && !usersFriends.contains(friendsRelation.get(1))) {
                 addPoint(friendAndScore, friendsRelation.get(1), SAME_FRIEND_POINT);
@@ -65,23 +66,18 @@ class FriendInfoController {
     }
 
     private static void checkVisitors(Map<String, Integer> friendAndScore, List<String> visitors, Set<String> usersFriends) {
-        for (int i = 0; i < visitors.size(); i++) {
-            if(!usersFriends.contains(visitors.get(i))) {
-                addPoint(friendAndScore, visitors.get(i), VISITOR_POINT);
+        for (String visitor : visitors) {
+            if (!usersFriends.contains(visitor)) {
+                addPoint(friendAndScore, visitor, VISITOR_POINT);
             }
         }
     }
 
     private static List<String> returnTopFiveFriends(Map<String, Integer> friendAndScore){
         List<Map.Entry<String, Integer>> friendAndScoreList = new LinkedList<>(friendAndScore.entrySet());
-        List<String> recommendList = new ArrayList<>();
+        List<String> recommendList;
         friendAndScoreList.sort((o1, o2) -> o2.getValue() - o1.getValue());
-        for (Map.Entry<String, Integer> friend : friendAndScoreList) {
-            recommendList.add(friend.getKey());
-            if (recommendList.size() == MAX_RECOMMEND_SIZE){
-                break;
-            }
-        }
+        recommendList = friendAndScoreList.stream().limit(MAX_RECOMMEND_SIZE).map(Map.Entry::getKey).collect(Collectors.toList());
         return recommendList;
     }
 
