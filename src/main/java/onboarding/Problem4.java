23df3cc6161a98e7ecef629da3f23f7629961f42
Hongsugyeong <olivia258@naver.com>
2022-10-31 11:56:24 +0900

    [Problem 4] 소문자, 대문자 전환 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,4 +9,22 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    /*
+     *소문자, 대문자 전환 함수
+     */
+    static char ch_convert(char ch){
+        String up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String low = "abcdefghijklmnopqrstuvwxyz";
+
+        char result;
+        if(Character.isUpperCase(ch)){
+            result =  up.charAt(25 - ch + 'A');
+        }else if(Character.isLowerCase(ch)){
+            result = low.charAt(25 - ch + 'a');
+        }else{
+            result=' ';
+        }
+        return result;
+    }
 }
