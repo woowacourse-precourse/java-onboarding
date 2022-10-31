c60415c1123189c8737fc5253a00a019aa327b18
Sangin <ksiisk99@gmail.com>
2022-10-31 17:18:11 +0900

    feat: problem3 - 1~number까지 각 숫자가 손뼉 횟수를 계산하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem3 {
+    private static int[] numberOfClaps;
 
     public static int solution(int number) {
         return 0;
@@ -11,6 +12,17 @@ public class Problem3 {
             return 1;
         }
         return 0;
+    }
+
+    private static void initNumberOfClaps(int number) {
+        numberOfClaps=new int[number+9]; //ArrayIndexoutofboundsexception 때문에 3,6,9를 1로 초기화 하기 위해선 +9해줘야 한다.
+        numberOfClaps[3]=1;
+        numberOfClaps[6]=1;
+        numberOfClaps[9]=1;
+
+        for(int i=10;i<=number;i++){
+            numberOfClaps[i]=numberOfClaps[i/10]+isNumber369(i%10);
+        }
     }
 
 }
