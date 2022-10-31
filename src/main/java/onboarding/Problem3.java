06e375b900ff722ca2d502d21267f85e5f238778
madlimeus <108660314+madlimeus@users.noreply.github.com>
2022-10-31 20:56:12 +0900

    Problem3, 4

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem3 {
+    public static int COUNT_3(int num){
+        int result = 0;
+        String N_to_S = String.valueOf(num);
+        int[] Number = new int[N_to_S.length()];
+
+        for(int i = 0; i < N_to_S.length(); i++){
+            char One_num = N_to_S.charAt(N_to_S.length() - i - 1);
+            Number[i] = Character.getNumericValue(One_num);
+        }
+
+        for (int i = 0; i < Number.length; i++){
+            if((Number[i] == 3) || (Number[i] == 6) || (Number[i] == 9)){
+                result += 1;
+            }
+
+        }
+        return result;
+
+    }
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++){
+            answer += COUNT_3(i);
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] charArr = word.toCharArray();
+        for (int i = 0; i < charArr.length; i++){
+            if((65 <= (int)charArr[i]) && ((int)charArr[i] <= 90)){
+                int tmp = 155 - (int)charArr[i];
+                charArr[i] = (char)tmp;
+            } else if((97 <= (int)charArr[i]) && ((int)charArr[i] <= 121)){
+                int tmp = 219 - (int)charArr[i];
+                charArr[i] = (char)tmp;
+            }
+        }
+        answer = String.valueOf(charArr);
         return answer;
     }
 }
