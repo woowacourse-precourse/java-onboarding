75a93c0ea6e23ab766679226270e89023a8cc6c2
Dabeen Jeong <hello70825@gmail.com>
2022-10-31 12:47:34 +0900

    feat: [Problem7] 친구 관계를 인접리스트로 변환하는 기능을 추가하다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        List<String> friendOfFriend = findRecommendFriend(user, friends);
+
         return answer;
+    }
+
+    private static List<String> findRecommendFriend(String user, List<List<String>> friends) {
+        Map<String, List<String>> AdjacencyList = getAdjacencyList(friends);
+
+
+    }
+
+    private static Map<String, List<String>> getAdjacencyList(List<List<String>> friends) {
+        Map<String, List<String>> resultAdjacencyList = new TreeMap<>();
+
+        for(List<String> friend : friends) {
+            String friendFirst = friend.get(0);
+            String friendSecond = friend.get(1);
+
+            if (!resultAdjacencyList.containsKey(friendFirst)) {
+                resultAdjacencyList.put(friendFirst, new ArrayList<>());
+            }
+            resultAdjacencyList.get(friendFirst).add(friendSecond);
+
+            if (!resultAdjacencyList.containsKey(friendFirst)) {
+                resultAdjacencyList.put(friendFirst, new ArrayList<>());
+            }
+            resultAdjacencyList.get(friendSecond).add(friendFirst);
+        }
+
+        return resultAdjacencyList;
     }
 }
