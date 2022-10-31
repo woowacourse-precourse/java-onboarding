be805e7f8864bfb73e4aba8782f55ebcdfa5c838
wugawuga <dnr3655@naver.com>
2022-10-31 14:30:33 +0900

    refactor: 3,6,9 아닌 숫자 제거 후 문자열 길이 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,23 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= number; i++) {
-            answer = clap(answer, i);
+            sb.append(i);
         }
+        String replaceStr = sb.toString().replaceAll("[^369]", "");
+        answer = replaceStr.length();
         return answer;
-    }
-
-    private static int clap(int answer, int i) {
-        String value = String.valueOf(i);
-        for (String s : value.split("")) {
-            if (checkThreeSixNine(s)) {
-                answer++;
-            }
-        }
-        return answer;
-    }
-
-    private static boolean checkThreeSixNine(String str) {
-        return str.equals("3") || str.equals("6") || str.equals("9");
     }
 }
