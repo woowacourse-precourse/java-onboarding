c1591bdff65d4b876474d7f8139a1e1cd108e510
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-31 23:08:09 +0900

    feat: Add Problem6 Exception Checker
    - 문제 6번 예외 점검 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        if(isException(forms)) throw new RuntimeException("잘못된 인자입니다.");
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static Boolean isException(List<List<String>> forms) {
+        if (forms.size() < 1 || forms.size() > 1000000) return true;
+        return forms.stream()
+                .anyMatch(form ->
+                        (form.get(1).length() < 1 || form.get(1).length() > 20) ||
+                                (form.get(0).length() < 11 || form.get(0).length() > 20));
     }
 }
