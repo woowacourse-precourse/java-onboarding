08656049c48c61216cab958d1adc7af7701878a0
Wooseobee <dntjq0807@naver.com>
2022-11-01 00:04:56 +0900

    user와 친구인 사람의 목록 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Set<String> friendsWithUser = new HashSet<>();
+
+        friendsWithUser = checkUserFriends(user, friends, friendsWithUser);
         return answer;
+    }
+
+    public static Set<String> checkUserFriends(String user, List<List<String>> friends, Set<String> friendsWithUser) {
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).get(0).equals(user)) {
+                friendsWithUser.add(friends.get(i).get(1));
+            } else if (friends.get(i).get(1).equals(user)) {
+                friendsWithUser.add(friends.get(i).get(0));
+            } else {
+                continue;
+            }
+        }
+        return friendsWithUser;
     }
 }
