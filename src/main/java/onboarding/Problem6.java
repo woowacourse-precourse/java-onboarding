b80216d348652d094a9ff26a2a5a22c1f3c840b6
hyungkya <hyungkya@uci.edu>
2022-11-01 01:04:40 +0900

    feat(problem 6): diassemble string to 2-char combinations
    toFragment() method returns possible 2-character length combination from a given string.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    private static Set<String> toFragments(String word) {
+        Set<String> fragments = new HashSet<>();
+        for (int i = 1; i < word.length(); i++) {
+            fragments.add(word.substring(i - 1, i + 1));
+        }
+        return fragments;
     }
 }
