1c6681cdde89c435f6411c1b82a2bd0ac55a248c
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 02:06:03 +0900

    refactor(feat) :신청받은 list 확인하는 메서드 checkList 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,10 +7,28 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-
         List<String> emailList = valueExtraction(forms,0);
         List<String> nameList = valueExtraction(forms,1);
-        return getEmail(nameList,emailList);
+        return checkList(nameList,emailList);
+    }
+    public static List<String> checkList(List<String> nameList,List<String> emailList){
+        List<String> answer = new ArrayList<>();
+        for(int i=0;i<nameList.size();i++){
+            boolean check =false;
+            for(int j=i+1;j<nameList.size();j++){
+                if(lcs(nameList.get(i), nameList.get(j))){
+                    if(!answer.contains(emailList.get(j))){
+                        answer.add(emailList.get(j));
+                        check=true; //이미 포함되어있다면 true가안됨
+                    }
+                }
+            }
+            if(check && !answer.contains(emailList.get(i))){
+                answer.add(emailList.get(i));
+            }
+        }
+        Collections.sort(answer);
+        return answer;
     }
 
     public static boolean lcs(String str1, String str2){
@@ -24,38 +42,14 @@ public class Problem6 {
 
                 if (str1.charAt(i-1) == str2.charAt(j-1)) {
                     arr[i][j]=arr[i-1][j-1]+1;
-                    max = max>arr[i][j] ? max : arr[i][j];
+                    max = Math.max(max, arr[i][j]);
                 }
             }
         }
         return false;
     }
 
-    public static List<String> getEmail(List<String> nameList,List<String> emailList){
-        List<String> answer = new ArrayList<>();
-        for(int i=0;i<nameList.size();i++){
-            String name = nameList.get(i);
-            boolean check =false;
-            for(int k=0;k<name.length()-1;k++){  // 최소 2개이상이므로 -1
-                for(int z=k+2;z<=name.length();z++){
-                    String nameSub=name.substring(k,z);
-                    for(int j=i+1;j<nameList.size();j++){
-                        if(nameList.get(j).contains(nameSub)){
-                            if(!answer.contains(emailList.get(j))){
-                                check= true;
-                                answer.add(emailList.get(j));
-                            }
-                        }
-                    }
-                }
-            }
-            if(check==true){
-                answer.add(emailList.get(i));
-            }
-        }
-        Collections.sort(answer);
-        return answer;
-    }
+
     public static List<String> valueExtraction(List<List<String>> forms,int index){
         return  forms.stream().map(o-> o.get(index)).collect(Collectors.toList());
     }
