228bd89c7e328f7a64a71ad59f30b9e474943ce0
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 23:01:00 +0900

    P7 | 친구 관계 맵 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -18,9 +18,27 @@ public class Problem7 {
         setVisitorScore(visitors, recommendScoreMap);
         deleteUserFriends(user ,friendMap, recommendScoreMap);
 
-        // 추천 점수 정보맵으로 추천 친구 리스트를 받아옴
         List<String> result = recommendListByScoreMap(recommendScoreMap);
 
+        return result;
+    }
+
+    private static Map<String , Set<String>> getFriendsMap(List<List<String>> friends) {
+        Map<String, Set<String>> result = new HashMap<>();
+        for(List<String> friend : friends) {
+
+            if (result.containsKey(friend.get(0))) {
+                result.get(friend.get(0)).add(friend.get(1));
+            } else {
+                result.put(friend.get(0), new HashSet<>(List.of(friend.get(1))));
+            }
+
+            if (result.containsKey(friend.get(1))) {
+                result.get(friend.get(1)).add(friend.get(0));
+            } else {
+                result.put(friend.get(1), new HashSet<>(List.of(friend.get(0))));
+            }
+        }
         return result;
     }
 }
