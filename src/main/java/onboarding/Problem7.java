3e0f8ddeb92e013249465b799ee05b8d3edfd1b0
kkm5291 <kkm23125291@gmail.com>
2022-10-31 18:13:48 +0900

    Problem7 Feat : 유저와 직접적인 친구관계 추가 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -11,24 +11,22 @@ public class Problem7 {
 
 
     // 유저 친구 목록
-    public static List<String> friendsList(List<List<String>> friends) { // friends 목록 친구들 list만들기
+    public static List<String> friendsList(String user, List<List<String>> friends) { // 유저와 직접적인 친구관계
         List<String> list = new ArrayList<>();
 
         for (List<String> friend : friends) {
-            String friendA = friend.get(0);
-            String friendB = friend.get(1);
-
-            if (list.contains(friendA)) {
-                continue;
-            } else
-                list.add(friendA);
-
-            if (list.contains(friendB)) {
-                continue;
-            } else
-                list.add(friendB);
+            if (friend.contains(user)) {
+                for (String name : friend) {
+                    if (name == user) {
+                        continue;
+                    }
+                    list.add(name);
+                }
+            }
         }
 
         return list;
     }
+
+
 }
