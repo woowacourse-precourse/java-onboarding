3575bd1e3dc4bc2fff962643a2425fc22494c5a3
SeoSeunghun <proto_seo@naver.com>
2022-10-31 13:36:50 +0900

    [feat] 문제 5번 해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+	private static final int[] moneySpecifications = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = new ArrayList<>();
+		for (int spec : moneySpecifications) {
+			answer.add(money / spec);
+			money %= spec;
+		}
+		return answer;
+	}
 }
