bf098e3a0d056c5829ee86b83958eb1cac6a9df8
Moon-Tree <ellaya1012@gmail.com>
2022-11-01 00:40:38 +0900

    Problem4, a-z 순으로 문자가 거꾸로 출력되어야 하는데 입력된 문자열만 거꾸로되어 나온다...

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,10 @@ public class Problem4 {
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
 
+        for(int i=word.length()-1; i>=0; i--) {
+            answer.append(word.charAt(i));
+        }
+
         return answer.toString();
     }
 }
