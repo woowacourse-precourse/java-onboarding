82408d9a39e8c6acc91f5794de02192a6a9c22a2
jmj005 <jmj00005@naver.com>
2022-10-31 15:06:51 +0900

    refactor: Problem1 Code Refactoring

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,31 +10,26 @@ import java.util.List;
 
 class Problem1 {
 
-    private static boolean isLeftPageOdd(List<Integer> pobi, List<Integer> crong) {
-        if (pobi.get(0) % 2 != 0 && crong.get(1) % 2 != 0)
-            return true;
-        return false;
+    private static boolean isValidPage(List<Integer> pobi, List<Integer> crong) {
+        return pobi.get(0) % 2 != 0 && crong.get(0) % 2 != 0;
     }
 
     private static boolean isValidScope(List<Integer> pobi, List<Integer> crong) {
-        if ((1 < pobi.get(0) && pobi.get(1) < 400) && (1 < crong.get(0) && crong.get(1) < 400))
-            return true;
-        return false;
+        return (1 < pobi.get(0) && pobi.get(1) < 400) && (1 < crong.get(0) && crong.get(1) < 400);
     }
 
     private static boolean isSeriesPage(List<Integer> pobi, List<Integer> crong) {
-        if (pobi.get(1).equals(pobi.get(0)+1) && crong.get(1).equals(crong.get(0)+1))
-            return true;
-        return false;
+        return pobi.get(1).equals(pobi.get(0) + 1) && crong.get(1).equals(crong.get(0) + 1);
     }
 
-    private static int getPageFindMaxNum(int num) {
+    private static int getPageMax(int num) {
         int sumDigit = 0;
         int mulDigit = 1;
         String number = Integer.toString(num);
         for (int i = 0; i < number.length(); i ++) {
-            sumDigit += Integer.parseInt(number.substring(i, i+1));
-            mulDigit *= Integer.parseInt(number.substring(i, i+1));
+            int digit = Integer.parseInt(number.substring(i, i+1));
+            sumDigit += digit;
+            mulDigit *= digit;
         }
         return Math.max(sumDigit, mulDigit);
     }
@@ -42,13 +37,13 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        if (!(isLeftPageOdd(pobi, crong) && !(isValidScope(pobi, crong))) && !(isSeriesPage(pobi, crong))) {
+        if (!(isValidPage(pobi, crong) || !(isValidScope(pobi, crong))) || !(isSeriesPage(pobi, crong))) {
             answer = -1;
             return answer;
         }
 
-        int pobiMaxNum = Math.max(getPageFindMaxNum(pobi.get(0)), getPageFindMaxNum(pobi.get(1)));
-        int crongMaxNum = Math.max(getPageFindMaxNum(crong.get(0)), getPageFindMaxNum(crong.get(1)));
+        int pobiMaxNum = Math.max(getPageMax(pobi.get(0)), getPageMax(pobi.get(1)));
+        int crongMaxNum = Math.max(getPageMax(crong.get(0)), getPageMax(crong.get(1)));
 
         if (pobiMaxNum > crongMaxNum) {
             answer = 1;
