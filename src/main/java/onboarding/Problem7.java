a74aed55d26577d6f4ec00cc00252eb6973f9825
2jun0 <soo28819@gmail.com>
2022-10-31 19:07:18 +0900

    Feat: 문제 7번 - 입력값 검증 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -21,6 +21,10 @@ public class Problem7 {
     }
 
     public static List<String> solution(String userStr, List<List<String>> friendsStr, List<String> visitorsStr) {
+        validateFriendsRange(friendsStr);
+        assertFriendsIsDistinct(friendsStr);
+        validateVisitorsRange(visitorsStr);
+
         CrewId userId = new CrewId(userStr);
 
         List<List<CrewId>> crewRelations = cast2DListStrToCrewId(friendsStr);
@@ -33,5 +37,30 @@ public class Problem7 {
         return recommendCrews.stream()
                 .map(CrewId::getId)
                 .collect(Collectors.toList());
+    }
+
+    private static void validateFriendsRange(List<List<String>> friends) throws IllegalArgumentException {
+        if (!(1<=friends.size() && friends.size()<=10000)) {
+            throw new IllegalArgumentException("friends의 길이는 1이상 10,000이하여야 합니다.");
+        }
+
+        if (!friends.stream().allMatch(friendRel -> friendRel.size() == 2)) {
+            throw new IllegalArgumentException("friends안 배열의 길이는 항상 2여야 합니다.");
+        }
+    }
+
+    private static void assertFriendsIsDistinct(List<List<String>> friends) throws IllegalArgumentException {
+        int originSize = friends.size();
+        int distinctSize = (int)friends.stream().map(HashSet::new).distinct().count();
+
+        if (originSize != distinctSize) {
+            throw new IllegalArgumentException("friends의 친구관계 중 중복된 값이 있으면 안됩니다.");
+        }
+    }
+
+    private static void validateVisitorsRange(List<String> visitors) throws IllegalArgumentException {
+        if (!(visitors.size()<=10000)) {
+            throw new IllegalArgumentException("visitors의 길이는 10,000이하여야 합니다.");
+        }
     }
 }
