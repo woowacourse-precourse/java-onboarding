02f9f945a7af672ed476fc37a35cb1197ac5ac8e
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-10-31 22:57:05 +0900

    feat : 문제3 각 자리 숫자 조건 해당시 카운트

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,12 @@ public class Problem3 {
         for (int i=1; i<=number; i++) {
         	//입력받은 숫자의 각 자리를 추출하기 위해 String으로 형변환
         	String str = String.valueOf(i);
-        	
+        	//n자리 수의 n만큼 반복
+        	for(int j=0; j<str.length(); j++) {
+        		//각 자리 숫자 추출 & 3,6,9일 때 박수 카운트
+        		int num = Integer.parseInt(String.valueOf(str.charAt(j)));
+        		if ( num!=0 && num%3== 0 ) answer++ ;	
+        	}
         }
         return answer;
     }   
