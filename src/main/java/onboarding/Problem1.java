bb3184197b198c436bf06d8769d4f0e1ba775ad4
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 00:10:30 +0900

    feat : problem1 점수 계산하는 메소드 calculator(번호의 각자리를 배열로 나누는 메서드 getNumArray)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -8,9 +9,22 @@ class Problem1 {
         if(checkException(pobi) || checkException(crong)){
             return -1;
         }
+        int pobiNumber  =  calculator(pobi);
+        int crongNumber =  calculator(crong);
 
 
         return answer;
+    }
+    public static int calculator(List<Integer> numList){
+        int[] leftArray = getNumArray(numList,0);
+        int[] rightArray = getNumArray(numList,1);
+
+        return 1;
+    }
+    public static int[] getNumArray(List<Integer> numList,int index){
+        return Stream.of(String.valueOf(numList.get(index)).split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
     public static boolean checkException(List<Integer> checkList){
         Integer leftNum = checkList.get(0)+1;
