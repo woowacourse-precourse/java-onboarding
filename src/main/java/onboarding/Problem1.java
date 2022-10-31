ac1fc7804ecd1fe6ce7a036f5bb26cea11e8087a
yoonjuho <dbswn990@naver.com>
2022-10-31 18:50:19 +0900

    feat: PROBLEM1-(예외처리) 리스트 길이 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        if (hasException(pobi, crong) == true)
+            return -1;
         return answer;
     }
+
+
+    private static boolean hasException(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.size() != 2 || crong.size() != 2)
+            return true;
+        return false;
+    }
+
 }
