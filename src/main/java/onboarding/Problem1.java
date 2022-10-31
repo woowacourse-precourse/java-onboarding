b2cd523c8270335e080ea7f6d16dfc2c531c4ba1
Sunghyun-0 <zkdldk95@naver.com>
2022-10-31 22:20:43 +0900

    (Problem1)점수기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int getMaxPoint(int pageNum){
+        int point1 = 0, point2 = 1;
+
+        while (pageNum>0){
+            point1 += pageNum%10;
+            point2 *= pageNum%10;
+            pageNum /= 10;
+        }
+        return Math.max(point1,point2);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
