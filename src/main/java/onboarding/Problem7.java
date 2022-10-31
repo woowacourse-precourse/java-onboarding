197251245a0d50a1856171176c8eef7e5546c829
Your Name <dbsdbxkrzz@naver.com>
2022-10-31 15:37:48 +0900

    feat: Problem7 코드의 기본 구성을 작성합니다
    기능 목록과 세부 기능 사항에 맞추어 기본 구성을 작성합니다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> answer = new ArrayList<>();
+		List<String> userFriends = new ArrayList<>();
+		Map<String, Integer> friendScore = new HashMap<>();
+		findUserFriend(friends, userFriends, user);
+		calcFriendsScore(friendScore, friends, userFriends);
+		return answer;
+	}
+
+	public static void findUserFriend(List<List<String>> friends, List<String> userFriends, String user) {
+
+	}
+
+	public static void calcFriendsScore(Map<String, Integer> friendScore, List<List<String>> friends,
+		List<String> userFriends) {
+
+	}
+
+	public static void sortScore() {
+
+	}
+
+	public static void recommendFriend() {
+		
+	}
 }
