f1ea73eb57fd87d0a21558682599e851f0a3b219
mardi2020 <mardi@khu.ac.kr>
2022-10-31 15:10:19 +0900

    feat: Problem1 기능1 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,15 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Math.abs;
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+        if (abs(pobi.get(0) - pobi.get(1)) != 1 || abs(crong.get(0) - crong.get(1)) != 1) {
+            return -1;
+        }
+
         return answer;
     }
 }
