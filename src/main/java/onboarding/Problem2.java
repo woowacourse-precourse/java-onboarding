97d75a9acacd328326b6921711ecaf69e7d7027c
kite707 <kite707707@gmail.com>
2022-11-01 02:39:48 +0900

    [problem 2] add comments when answer.length == 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,10 @@
 package onboarding;
 
+/**
+ * 연속된 문자를 지우고, 나머지 문자열을 합친 뒤 다시 맨 앞에서부터 연속된 문자를 찾아나가는 연산을 반복.
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-
-
         for(int i=0;i<cryptogram.length()-1;i++){
             if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
                 cryptogram=cryptogram.substring(0,i)+cryptogram.substring(i+2);
