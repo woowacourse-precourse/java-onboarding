b04a2a0e1ef6e35b3f3147ca8a2985a56d06e4d1
dlgosla <dlgoslaek1@gmail.com>
2022-11-01 08:53:22 +0900

    feat: [Ploblem 1] 왼쪽 페이지와 오른쪽 페이지 스코어 중 최댓값 구하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,6 +17,17 @@ class Problem1 {
     }
 
 
+    // 왼쪽 페이지와 오른쪽 페이지 연산 결과 중에 최댓값
+    public static int find_max_between_pages(List<Integer> pages){
+        int left_max = find_max_between_sum_and_multi(pages.get(0));
+        int right_max = find_max_between_sum_and_multi(pages.get(1));
+
+        int max_num = (left_max < right_max) ? right_max : left_max;
+
+        return max_num;
+
+    }
+
     // 해당 페이지의 숫자들을 더하거나 곱한 것 중에 최댓값
     public static int find_max_between_sum_and_multi(Integer num) {
 
