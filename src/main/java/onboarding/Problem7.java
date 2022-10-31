ef65371a2190372ce9472242720a34d5cf56b472
DDing77 <devseo2021@gmail.com>
2022-11-01 02:07:49 +0900

    [Problem7] feat : 이미 친구 관계인지 판별하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -26,6 +26,19 @@ public class Problem7 {
 
         return list;
     }
+
+    // 이미 친구 관계인지 판별
+    public static boolean isFriend(String user, String target) {
+        // 친구 관계 => true, 아니면 false 리턴
+
+        // 본인일 시
+        if (user.equals(target)) return true;
+        // 이미 친구관계일 시
+        if (friendMap.get(user).contains(target)) return true;
+
+        return false;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
