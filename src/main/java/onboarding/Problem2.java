ff18b84b0e7ed3d289a5ccae86e5d5c6ddc86861
glass <glass7312@gmail.com>
2022-10-31 20:24:24 +0900

    Problem2 refactor(함수명 변경,변수명 변경)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,20 +1,20 @@
 package onboarding;
 
 public class Problem2 {
-    public static String erase(String s) {
-        String t_s="";
+    public static String deduplication(String s) {
+        String transform_string="";
         if(s.length()==0) return "";
         for(int i=0; i<s.length()-1; i++) {
             if(s.charAt(i)==s.charAt(i+1)) {
-                t_s = s.substring(0, i) + s.substring(i + 2);
-                return erase(t_s);
+                transform_string = s.substring(0, i) + s.substring(i + 2);
+                return deduplication(transform_string);
             }
         }
         return s;
     }
 
     public static String solution(String cryptogram) {
-        String answer = erase(cryptogram);
+        String answer = deduplication(cryptogram);
         return answer;
     }
 }
