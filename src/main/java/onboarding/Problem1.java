1755d9393c242944db6645a0db20341bb8a48773
hongjinwon <herenever@naver.com>
2022-10-31 19:37:14 +0900

    feat: Problem1 getDigitList 추가
    - Problem1 getDigitList() 추가
    - stram을 이용해서 String<stream> -> intStream
    -> IntegerStream -> List<Integer> 로 변환해서 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,9 @@
 package onboarding;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -12,7 +15,7 @@ class Problem1 {
         int leftPage = pages.get(0);
         int rightPage = pages.get(1);
 
-        // case1) 페이지가 1~400 사이의 범위에 있지 않다.
+        // case1) 페이지가 2~399 사이의 범위에 있지 않다.
         if (leftPage <= 1 || leftPage >= 400 || rightPage <= 1 || rightPage >= 400){
             return false;
         }
@@ -20,11 +23,18 @@ class Problem1 {
         if (leftPage % 2 != 1){
             return false;
         }
-        //case4) 페이지가 연속된 숫자가 아니다.
+        //case3) 페이지가 연속된 숫자가 아니다.
         if (leftPage+1 != rightPage){
             return false;
         }
 
         return true;
+    }
+
+    public static List<Integer> getDigitList(Integer num){
+        return Stream.of(String.valueOf(num).split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
