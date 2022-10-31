8021ee3c2ce5d5c8081e5da4dd07b79d1aaaf031
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-10-31 21:23:45 +0900

    feat(Problem4): 대문자, 소문자 여부에 따라 합을 다르게 정하는 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+    public static int setSum(char alphabet) {
+        int sum;
+        if (Character.isUpperCase(alphabet)) sum = 155;
+        else if (Character.isLowerCase(alphabet)) sum = 219;
+        else sum = 2 * alphabet;
+
+        return sum;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
