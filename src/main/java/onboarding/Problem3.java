a39233395f9935b957ae0578577b4a52ec1e32d1
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:24:34 +0900

    feat: 369 counting

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,17 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int count369(int num) {
+        int cnt = 0;
+
+        while(num>0) {
+            int a = num % 10;
+            if(a != 0 && a % 3 == 0) {
+                cnt++;
+            }
+            num /= 10;
+        }
+        return cnt;
+    }
 }
