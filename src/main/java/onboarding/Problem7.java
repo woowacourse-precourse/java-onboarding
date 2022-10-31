6b40cb6c031ef71c4e71724a6bd0487eaff4ebdc
hyejungg <kimhyejung12@naver.com>
2022-10-31 20:55:03 +0900

    feat: [문제 7] 모든 사용자의 친구 관계 생성 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,20 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+	private static Map<String, Set<String>> mapByFriendMapSet(List<List<String>> friends) {
+		Map<String, Set<String>> friendMapSet = new HashMap<>();
+
+		for (List<String> friend : friends) {
+			String friendA = friend.get(0);
+			String friendB = friend.get(1);
+
+			friendMapSet.putIfAbsent(friendA, new HashSet<>());
+			friendMapSet.get(friendA).add(friendB);
+
+			friendMapSet.putIfAbsent(friendB, new HashSet<>());
+			friendMapSet.get(friendB).add(friendA);
+		}
+		return friendMapSet;
+	}
+
 }
