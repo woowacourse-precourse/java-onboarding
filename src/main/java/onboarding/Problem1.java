3dcf594ca7e2d4f5fa91cd86c77564679bc5ec63
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-10-31 18:03:58 +0900

    Add : 입력으로 들어온 두 페이지에서 각 페이지의 곱중 큰 값을 찾는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -18,6 +18,20 @@ class Problem1 {
         return result.stream().mapToInt(Integer::intValue).max().orElse(0);
     }
 
+    // 유저가 펼친 페이지의 왼쪽, 오른쪽의 각 자리수의 곱 중 큰 값을 찾는 메서드
+    public static int multiplyNumber(List<Integer> list){
+        List<Integer> result = new ArrayList<>();
+        for(Integer num : list){
+            int temp = 1;
+            while (num > 0){
+                temp *= num % 10;
+                num /= 10;
+            }
+            result.add(temp);
+        }
+        return result.stream().mapToInt(Integer::intValue).max().orElse(0);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
