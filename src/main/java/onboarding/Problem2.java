0e8c62b8075bcce565158c3cedf145e6bde046fb
SeolYoungKim <nasur4da@gmail.com>
2022-10-31 15:22:23 +0900

    refactor(P2): input value에 대한 validation은 soultion에서 해야한다고 판단하여 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,9 +3,32 @@ package onboarding;
 import onboarding.problem2.Decrypt;
 import onboarding.problem2.DuplicateProcessor;
 
+import static onboarding.common.CommonMessage.PERMIT_ONLY_LOWERCASE;
+import static onboarding.common.CommonValidator.isOnlyContainsLowerCase;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        validateCryptogram(cryptogram);
+
         Decrypt decrypt = new Decrypt(new DuplicateProcessor());
         return decrypt.doDecrypt(cryptogram);
+    }
+
+    private static void validateCryptogram(String cryptogram) {
+        if (!isValidLength(cryptogram)) {
+            throw new IllegalArgumentException("문자의 길이는 1 이상 1,000 이하여야 합니다.");
+        }
+
+        if (!isOnlyContainsLowerCase(cryptogram)) {
+            throw new IllegalArgumentException(PERMIT_ONLY_LOWERCASE.message());
+        }
+    }
+
+    private static boolean isValidLength(String cryptogram) {
+        final int MAX_VALUE = 1_000;
+        final int MIN_VALUE = 1;
+        int length = cryptogram.length();
+
+        return MIN_VALUE <= length && length <= MAX_VALUE;
     }
 }
--- src/main/java/onboarding/problem2/Decrypt.java
+++ src/main/java/onboarding/problem2/Decrypt.java
@@ -1,8 +1,5 @@
 package onboarding.problem2;
 
-import static onboarding.common.CommonMessage.PERMIT_ONLY_LOWERCASE;
-import static onboarding.common.CommonValidator.isOnlyContainsLowerCase;
-
 public class Decrypt {
 
     private final DuplicateProcessor duplicateProcessor;
@@ -12,25 +9,6 @@ public class Decrypt {
     }
 
     public String doDecrypt(String cryptogram) {
-        validateCryptogram(cryptogram);
         return duplicateProcessor.checkDuplication(cryptogram.toCharArray());
-    }
-
-    private void validateCryptogram(String cryptogram) {
-        if (!isValidLength(cryptogram)) {
-            throw new IllegalArgumentException("문자의 길이는 1 이상 1,000 이하여야 합니다.");
-        }
-
-        if (!isOnlyContainsLowerCase(cryptogram)) {
-            throw new IllegalArgumentException(PERMIT_ONLY_LOWERCASE.message());
-        }
-    }
-
-    private boolean isValidLength(String cryptogram) {
-        final int MAX_VALUE = 1_000;
-        final int MIN_VALUE = 1;
-        int length = cryptogram.length();
-
-        return MIN_VALUE <= length && length <= MAX_VALUE;
     }
 }
