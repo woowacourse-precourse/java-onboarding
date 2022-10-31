507a0e34060c6c46f4281ac4c15d762676decbd2
Donguk <95donguk.dev@gmail.com>
2022-10-31 18:40:25 +0900

    feat: 문제5의 돈을 화폐 단위로 출금하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,43 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = withdrawMoney(money);
         return answer;
+    }
+
+    private static List<Integer> withdrawMoney(int money) {
+        List<Integer> wallet = new ArrayList<>();
+        List<Currency> currencies = List.of(Currency.values());
+        for (Currency currency : currencies) {
+            wallet.add(currency.countCurrencyAmount(money));
+            money = currency.calculateBalance(money);
+        }
+        return wallet;
+    }
+
+    enum Currency {
+        FIVE_MILLION_WON_BILL(50000), ONE_MILLION_WON_BILL(10000), FIVE_THOUSAND_WON_BILL(5000),
+        ONE_THOUSAND_WON(1000), FIVE_HUNDRED_WON_COIN(500), ONE_HUNDRED_WON_COIN(100),
+        FIFTY_WON_COIN(50), TEN_WON_COIN(10), ONE_WON_COIN(1);
+
+        private final int unit;
+
+        Currency(int unit) {
+            this.unit = unit;
+        }
+
+        public int countCurrencyAmount(int money) {
+            return money / this.unit;
+        }
+
+        public int calculateBalance(int money) {
+            return money - unit * countCurrencyAmount(money);
+        }
     }
 }
