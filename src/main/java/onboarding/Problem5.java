d0e5ac72ac5885a21338f443b78086fae3caf2b4
OhHongGi <5ghdrl@naver.com>
2022-11-01 03:02:15 +0900

    chore: Solve Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,13 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new LinkedList<>();
+        withdraw(money, answer);
         return answer;
     }
 
