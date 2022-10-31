539b9f147a2f1d861916949ce794807709f13d99
dydtmd6801 <dydtmd6801@naver.com>
2022-11-01 00:57:56 +0900

    feat : 원래 친구 구분 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> friendsMap = new HashMap<>();
+        checkOldFriends(friends, user, friendsMap);
         return answer;
+    }
+
+    private static void checkOldFriends(List<List<String>> friendsList, String userName, HashMap friendsMap){
+        for(int i = 0; i < friendsList.size(); i++){
+            int oldFriends = friendsList.get(i).indexOf(userName);
+            if(oldFriends > -1){
+                String oldFriendName = friendsList.get(i).get(1-oldFriends);
+                friendsMap.put(oldFriendName, 0);
+            }
+        }
     }
 }
