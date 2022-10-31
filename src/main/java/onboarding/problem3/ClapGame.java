f60d7a74213315a1ba7fca93d63bbad252b71af2
Be-student <presentsong@naver.com>
2022-10-31 18:44:53 +0900

    feat(Problem3): 총 박수 횟수 계산 기능 추가

---
+++ src/main/java/onboarding/problem3/ClapGame.java
@@ -0,0 +1,17 @@
+package onboarding.problem3;
+
+public class ClapGame {
+    private final GameNumber gameNumber;
+
+    public ClapGame(GameNumber gameNumber) {
+        this.gameNumber = gameNumber;
+    }
+
+    public int getClapCount() {
+        return gameNumber.getRange().reduce(0, (total, number) -> total += getClapCountWithNumber(number));
+    }
+
+    private int getClapCountWithNumber(int currentNumber) {
+        return new ClapCounter(currentNumber).getClapCount();
+    }
+}
