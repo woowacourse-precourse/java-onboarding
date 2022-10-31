4d085c0947b2ca45398df0b8d8ab127735431ffb
janu01ary <janu01ary@gmail.com>
2022-10-31 17:36:26 +0900

    [Problem2] 문자열 내 중복 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,21 @@
 package onboarding;
 
 public class Problem2 {
+    private static boolean isDuplication(String str) {
+        if (str == "")
+            return false;
+
+        char tmp = str.charAt(0);
+        for (int i = 1; i < str.length(); i++) {
+            char cur = str.charAt(i);
+            if (tmp == cur) {
+                return true;
+            }
+            tmp = cur;
+        }
+        return false;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
