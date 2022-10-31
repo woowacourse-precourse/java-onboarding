e2d773f720112e8decd077ec8f6b581684482d81
gomljo <gomljo0307@naver.com>
2022-11-01 03:13:19 +0900

    feat(Problem5.java): MoneyTransformation 클래스 정의
    MoneyTransformation 클래스의 클래스 변수와 상수를 정의하고 생성자 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+class MoneyTransformation{
+    int changes;
+    final List<Integer> moneyVouchers=List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    MoneyTransformation(int money){
+        changes=money;
+    }
+}
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
