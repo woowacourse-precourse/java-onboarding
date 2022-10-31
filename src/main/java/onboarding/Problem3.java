99e228771f1f153fc33fd32a0929b29e89d09337
kite707 <kite707707@gmail.com>
2022-11-01 02:41:57 +0900

    [problem 3] add comments about core idea

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * number이 10 이하일 경우, 이상일 경우를 구분해서 구현
+ * 10 이하일 경우 3 6 9면 1 리턴.
+ * 10 이상일 경우 한자리씩 잘라서 3 6 9가 들어가는지 검사 후 최종 횟수 리턴
+ */
 public class Problem3 {
 
     public static int getNumber(int number) {
