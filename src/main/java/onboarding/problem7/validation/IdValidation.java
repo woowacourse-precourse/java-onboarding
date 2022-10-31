c9e769021bf5efd718077f9a969fb1384467f660
kimminsu <xjfcnfw3@naver.com>
2022-10-31 21:40:04 +0900

    feat(문제7): [예외 사항추가] ID가 소문자인지 확인

--- src/main/java/onboarding/problem7/validation/IdValidation.java
+++ src/main/java/onboarding/problem7/validation/IdValidation.java
@@ -1,10 +1,17 @@
 package onboarding.problem7.validation;
 
+import java.util.regex.Pattern;
+
 public class IdValidation {
     private static final int MAX_ID_LENGTH = 30;
     private static final int MIN_ID_LENGTH = 1;
+    private static final String LOWERCASE_STR_REGEXP = "^[a-z]+$";
 
-    public static boolean validateIDLengthInRange(String id) {
+    public static boolean validateIdLengthInRange(String id) {
         return (id.length() >= MIN_ID_LENGTH) && (id.length() <= MAX_ID_LENGTH);
+    }
+
+    public static boolean validateIsLowerCaseId(String id) {
+        return Pattern.matches(LOWERCASE_STR_REGEXP, id);
     }
 }
