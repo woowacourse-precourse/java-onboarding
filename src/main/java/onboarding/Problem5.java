9ab4b656515ebc3b6dc717ed311c4841b227299e
K-Diger <kitten.diger@gmail.com>
2022-10-31 21:08:36 +0900

    docs : Problem5 기능 목록 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+[기능 목록]
+1. 내가 가진 돈에서 나눠야하는 돈을 나누는 메서드
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
