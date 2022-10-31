7c358eae9b235559b202322b97475ece8ff25263
Ilchul Kim <huj0159@gmail.com>
2022-11-01 03:45:39 +0900

    feat: add method give score to user's visitor

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,10 +8,22 @@ public class Problem7 {
         HashMap<String, Integer> hm = new HashMap<>();
         List<String> friendList = findUserFriend(user, friends);
         HashMap<String, Integer> algorithmPointMap = findFriendOfFriend(user,friends,friendList);
-
+        addVisitorPoint(visitors,friendList,hm);
 
 
         return answer;
+    }
+
+    public static void addVisitorPoint(List<String> visitors,List<String> friendList,HashMap<String, Integer> hm){
+        for (String name :visitors){
+            if(!friendList.contains(name)) {
+                if (hm.get(name) == null) {
+                    hm.put(name, 1);
+                    continue;
+                }
+                hm.put(name, hm.get(name) + 1);
+            }
+        }
     }
 
     public static HashMap<String ,Integer> findFriendOfFriend(String user,List<List<String>> friends,List<String> friendList){
