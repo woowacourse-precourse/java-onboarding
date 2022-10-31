6e2ef202b4dce0cc97b7101e5b1921e7fd6131eb
devMuscle <hongsbien@naver.com>
2022-10-31 09:14:54 +0900

    feat: problem3 solving

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem3 {
+
+    public boolean is369(int number) {
+
+        return false;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        
+        for(int i=1; i<=number; i++) {
+            String numStr = Integer.toString(i);
+
+            for(int j=0; j<numStr.length(); j++) {
+                char digit = numStr.charAt(j);
+
+                if(digit=='3' || digit=='6' || digit=='9') {
+                    answer++;
+                }
+            }
+        }
+
         return answer;
     }
 }
