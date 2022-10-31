e45eebe0231d39a43c73143b1d69a1680315f964
nahyeon cho <whskgus12@nate.com>
2022-11-01 01:28:07 +0900

    Problem 4 - 예외사항 처리
    입력한 문자열의 길이가 주어진 범위 밖이면, 아무것도 반환하지 않도록 설정하였습니다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        if (isValid(word)) {
+            String answer = "";
+            return answer;
+        } else
+            return ""; // word의 길이가 주어진 범위를 벗어났을때
+    }
+    static boolean isValid(String word) {
+        int len = word.length();
+        if (len < 1 || len > 1000)
+            return false;
+        else
+            return true;
     }
 }
