1a22b8ad269bf0ad7a72e3cefec5527ec297aa4c
baekeunsun <eunsun5460@gmail.com>
2022-10-31 14:37:43 +0900

    [문제3] feats: input 예외확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        if(Check(number)){
+
+        }
+        throw new IllegalArgumentException("잘못된 입력값");
+    }
+
+    private static boolean Check(int number){
+        if(number > 0 && number <10000) return true;
+        return false;
+
     }
 }
