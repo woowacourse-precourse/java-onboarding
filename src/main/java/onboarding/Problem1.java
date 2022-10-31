609449426e2a56cc6024b944812f148c82250efa
brotherstone97 <brotherstone97@gmail.com>
2022-10-31 14:44:01 +0900

    Feat: 입력 검증 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,24 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static boolean isVaild(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        if (rightPage - leftPage != -1)
+            return false;
+
+        if (leftPage >= 3 && leftPage <= 397) {
+            return leftPage % 2 == 1;
+        } else {
+            return false;
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (!isVaild(pobi) || !isVaild(crong)){
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
     }
