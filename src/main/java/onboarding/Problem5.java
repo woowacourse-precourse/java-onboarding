08282892c0b4aad72a7f37b5d7c477d8dd3d49e3
Lee-Jiseung <ljss98@naver.com>
2022-10-31 17:21:21 +0900

    feat: problem5 돈 배열 초기화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static List<Integer> moneys;
+
     public static List<Integer> solution(int money) {
+        initMoneys();
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void initMoneys() {
+        moneys = new ArrayList<>(Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
     }
 }
