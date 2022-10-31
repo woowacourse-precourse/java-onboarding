5c4909b82e326ba96dff211977d818ef1db5a27d
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-31 15:41:46 +0900

    [ADD] 문제 7 - friends 중에서 user 이름이 포함된 항목 제거

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,14 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
     private static final int MUTUAL_SCORE = 10;
     private static final int VISIT_SCORE = 1;
+
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        System.out.println(MUTUAL_SCORE);
-        System.out.println(VISIT_SCORE);
+        List<List<String>> friendRelations = new ArrayList<>();
+        HashSet<String> userFriends = new HashSet<>();
+
+        for (List<String> friendRelation : friends) { // friends 중에 user와 친구인 항목 제외
+            if (friendRelation.contains(user)) {
+                userFriends.add(friendRelation.get(0));
+            } else {
+                friendRelations.add(friendRelation);
+            }
+        }
 
         List<String> answer = Collections.emptyList();
         return answer;
