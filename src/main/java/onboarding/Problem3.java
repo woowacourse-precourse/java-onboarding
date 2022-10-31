8b763cbeae2b70fa66644f942116b27eeaaf804e
TaeHwan <kimta2hwan@gmail.com>
2022-10-31 23:48:07 +0900

    feat(PROBLEM3): 분리한 숫자를 확인하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    static int checkTargetNumber(char charOfNumber) {
+        if (charOfNumber == '3') {
+            return 1;
+        }
+
+        if (charOfNumber == '6') {
+            return 1;
+        }
+
+        if (charOfNumber == '9') {
+            return 1;
+        }
+
+        return 0;
     }
 }
