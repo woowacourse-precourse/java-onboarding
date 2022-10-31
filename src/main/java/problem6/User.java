fea3232ce7fac27c8b6bf98f1341b756a777426a
twoosky <lhjae3@naver.com>
2022-11-01 06:23:06 +0900

    refactor(problem6): HashMap을 인터페이스인 Map으로 변환

--- src/main/java/problem6/User.java
+++ src/main/java/problem6/User.java
@@ -3,16 +3,16 @@ package problem6;
 import java.util.*;
 
 public class User {
-    private final HashMap<String, String> forms;
-    private final HashSet<String> emails;
+    private final Map<String, String> forms;
+    private final Set<String> emails;
 
     public User(List<List<String>> inputForms) {
         this.forms = initForm(inputForms);
         this.emails = new HashSet<>();
     }
     
-    private HashMap<String, String> initForm(List<List<String>> inputForms) {
-        HashMap<String, String> forms = new HashMap<>();
+    private Map<String, String> initForm(List<List<String>> inputForms) {
+        Map<String, String> forms = new HashMap<>();
         for (List<String> form : inputForms) {
             String email = form.get(1);
             String name = form.get(0);
