d4306509cb5384cbe779c3a297a345f9010d6287
es <mascoeskim@gmail.com>
2022-11-01 08:20:19 +0900

    Problem7/5.리팩터링2

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -22,7 +22,7 @@ public class Problem7 {
                 exFriends.add(strings.get(0));
             }
         }
-///친구목록에서 이미 user와 친구를 exFriends로 분리///
+
         ArrayList<String> scoreTen = new ArrayList<String>();
 
         for (List<String> friend : friends) {
@@ -34,57 +34,46 @@ public class Problem7 {
                 }
             }
         }
-//// exFriends의 친구, 10점을 받아야하는 리스트를 scoreTen으로 출력
+
         HashMap<String,Integer> scoreTable = new HashMap<String,Integer>();
 
         for (String value : scoreTen) {
-            if (scoreTable.get(value) == null) {
-                scoreTable.put(value, 10);
-            } else {
-                scoreTable.put(value, scoreTable.get(value) + 10);
-            }
+            scoreTable.merge(value, 10, Integer::sum);
             ;
         }
-//// HashscoreTable을활용, scoreTen내의 이름을 key로, 존재하는 개수 *10을 value로 부여하여 새로운 scoreTable을 생성
+
         for (String visitor : visitors) {
-            if (scoreTable.get(visitor) == null) {
-                scoreTable.put(visitor, 1);
-            } else {
-                scoreTable.put(visitor, scoreTable.get(visitor) + 1);
-            }
+            scoreTable.merge(visitor, 1, Integer::sum);
             ;
         }
-//// HashscoreTable을 활용, visitors내의 이름을 key로, 방문 횟수를 value로 부여하여 기존 scoreTable에 합산
+
         for (String exFriend : exFriends) {
             if (scoreTable.get(exFriend) != null)
             {
                 scoreTable.remove(exFriend);
             }
         }
+
         scoreTable.remove(user);
-//// 점수테이블에서 기존 친구들과 유저본인을 삭제
+
         TreeMap<String, Integer> sortKey = new TreeMap<>(scoreTable);
-///////TreeMap을 활용한 키 오름차순 정렬/////////////
         Map<String, Integer> sortDescending = sortKey.entrySet()
             .stream()
             .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-            .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(),
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
             (entry1, entry2) -> entry2, LinkedHashMap::new));
-//////entrySet과 stream,sort를 이용한 밸류 내림차순 정렬/////////////
+
         List<String> sortedKeyList = new ArrayList<>(sortDescending.keySet());
-//정렬된 맵을 배열로 만들되 이제 key만있으면됨//
         ArrayList<String> answer = new ArrayList<String>();
-        if(sortedKeyList.size()>5){
-            for (int k=0;k<5;k++){
+        if(sortedKeyList.size() > 5){
+            for (int k = 0; k < 5; k++){
                 answer.add(sortedKeyList.get(k));
             }
         }
-        if(sortedKeyList.size()<=5){
-            for (String s : sortedKeyList) {
-                answer.add(s);
-            }
+        if(sortedKeyList.size() <= 5){
+            answer.addAll(sortedKeyList);
         }
-//최대 5개를 뽑아야 하므로, 배열의 길이가 5보다 작으면 배열의 사이즈를, 5보다 크면 다섯번만 for문 통해 add
+
         return answer;
     }
 }
