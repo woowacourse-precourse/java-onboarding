c1b5ec7a4ee8dde3e53b56eb64775838a49dcd72
minju412 <ln8847@ajou.ac.kr>
2022-10-31 21:49:04 +0900

    style: [p2] 변수 명 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class Problem2 {
 	public static String solution(String cryptogram) {
 		Stack<Character> stack = new Stack<Character>();
-		char dup = ' '; // 중복되는 문자 -> 삭제할 문자
+		char duplicated = ' '; // 중복되는 문자 -> 삭제할 문자
 		char top = ' '; // 스택의 top
 		char cur = ' '; // cur 은 cryptogram 을 순회할 문자
 
@@ -16,11 +16,11 @@ public class Problem2 {
 				stack.push(cur);
 			} else {
 				top = stack.peek();
-				if (cur != top && cur != dup) { // push 할 문자가 스택의 top 과 같지 않으면서 중복 문자도 아닐 때
+				if (cur != top && cur != duplicated) { // push 할 문자가 스택의 top 과 같지 않으면서 중복 문자도 아닐 때
 					stack.push(cur);
-					dup = ' '; // dup 초기화
+					duplicated = ' '; // dup 초기화
 				} else if (cur == top) { // push 할 문자와 스택의 top 이 같을 때
-					dup = top; // dup 업데이트
+					duplicated = top; // dup 업데이트
 					stack.pop(); // 중복 문자 제거
 				}
 			}
@@ -30,11 +30,11 @@ public class Problem2 {
 	}
 
 	private static String stackToString(Stack<Character> stack) {
-		StringBuilder answer = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		for (Character character : stack) {
-			answer.append(character);
+			sb.append(character);
 		}
-		return answer.toString();
+		return sb.toString();
 	}
 
 }
