6eda1bac082c66d1203a8309ebcef1b6d621c6c6
UngHeung <t01047377477@gmail.com>
2022-10-31 22:11:50 +0900

    feat : (problem 2) 문자의 연속 체크를 위한 메소드, 스택 추가
    문자의 연속성을 체크한 후
    연속되지 않는 문자를 스택에 담아줌

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
+	
+	public static Stack<Character> stack = new Stack<>();
+
+	public static String solution(String cryptogram) {
         String answer = "answer";
+        
+        stack = continuityCheck(cryptogram);
+        
         return answer;
     }
+
+	public static Stack<Character> continuityCheck(String cryptogram) {
+
+		for(int i = cryptogram.length() - 1; i >= 0; i--) {
+			char character = cryptogram.charAt(i);
+
+			if(stack.isEmpty()) {
+				stack.push(character);
+				continue;
+			}
+
+			if(stack.peek() == character) {
+				stack.pop();
+				continue;
+			}
+
+			stack.push(character);
+		}
+
+		return stack;
+	}
 }
