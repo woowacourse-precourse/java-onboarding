fa4157a24e4a0ac4cc3bfa884fb4e71c5266972b
bini59 <myuni0234@gmail.com>
2022-10-31 23:40:16 +0900

    feat : Count Money
    Money div with monetary unit and Add quotient to answer
    Set money to remainder

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> money_list = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> answer = new java.util.ArrayList<>(Collections.emptyList());
+        for(int i = 0; i < (int)money_list.size(); i++){
+            System.out.println(money_list.get(i));
+            answer.add(money / money_list.get(i));
+            money %= money_list.get(i);
+        }
         return answer;
     }
 }
