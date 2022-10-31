9f3ebd7a4f782d262ff12bd2275171327bc486b9
Go-Jaecheol <gojaech@naver.com>
2022-10-31 15:36:04 +0900

    feat: Problem4 - 문자열 앞에서부터 하나씩 비교 후 새 문자열 만드는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return getConvertedString(word);
+    }
+
+    private static String getConvertedString(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            int c = word.charAt(i);
+            sb.append(getConvertedChar(c));
+        }
+        return sb.toString();
+    }
+
+    private static char getConvertedChar(int c) {
+        // 대문자인 경우
+        if (c >= 65 && c <= 90) {
+
+        }
+        // 소문자인 경우
+        if (c >= 97 && c <= 122) {
+
+        }
     }
 }
