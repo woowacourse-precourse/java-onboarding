2f43cd4e06c27279c00272d3648c6c5b837d0ab3
chosunghyun18 <krismon@naver.com>
2022-11-01 02:15:19 +0900

    [6번] 요구 사항 리스트 3번 구현 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,66 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+    public static List<String> solution(List<List<String>> forms)
+    {
+        List<String> answer = new ArrayList<>();
+        checkEmail(forms,answer) ;
+        System.out.println(answer);
         return answer;
     }
+
+    //email.com 인지 확인
+    public static List<String> checkEmail(List<List<String>> forms,List<String> answer)
+    {
+        List<List<String>>validate_forms = forms;
+        List<String> nickname_origin = new ArrayList<>();
+        List<Integer> nickname_index= new ArrayList<>();
+
+        for(int i =0 ; i < validate_forms.size();i++) {
+            nickname_origin.add(forms.get(i).get(1));
+        }
+
+        for(int i =0 ; i < validate_forms.size();i++) {
+            checkDuplicate(nickname_origin,nickname_index,forms.get(i).get(1),i);
+        }
+        Collections.sort(nickname_index); // 번호 정렬
+        List<Integer> listWithoutDuplicates = nickname_index.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        // 중복 제거된 인덱스 정렬 후 nickname 담기
+        for (Integer listWithoutDuplicate : listWithoutDuplicates) {
+            answer.add(validate_forms.get(listWithoutDuplicate).get(0));
+        }
+
+        return answer;
+    }
+
+    //한국어 닉네임인지 확인
+    // 중복이름 검색
+    public static void checkDuplicate(List<String> nickname_origin,List<Integer> index_list,String nick_name,int number){
+
+        for(int i = 0 ;i<nick_name.length()-1;i++)
+        {
+            // 확인할 문자열 추출 -> regex 사용 해보고 싶음
+            String sub= nick_name.substring(i,i+2); // 부분 문자열 추출
+            for(int j = 0 ; j < nickname_origin.size();j++)
+            {
+                if(j != number) {
+                    if(nickname_origin.get(j).contains(sub)){
+                        index_list.add(j);
+                    }
+                }
+            }
+
+        }
+
+    }
+
 }
+
--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -155,6 +155,21 @@ class ApplicationTest {
 
     @Nested
     class Problem6Test {
+//        @Test
+//        void case0() {
+//            List<List<String>> forms = List.of(
+//                    List.of("jm@email.com", "제이엠"),
+//                    List.of("jason@email.com", "제이슨"),
+//                    List.of("woniee@email.com", "워니"),
+//                    List.of("mj@email.com", "엠제이"),
+//                    List.of("nowm@email.com", "이제엠")
+//            );
+//            System.out.println(forms.size());
+//            System.out.println(forms.get(1));
+//            System.out.println(forms.get(1).get(1));
+//            List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
+////            assertThat(Problem6.solution(forms)).isEqualTo(result);
+//        }
         @Test
         void case1() {
             List<List<String>> forms = List.of(
