ee45e9cb8b5acf91a12ecf1ba72456171eb37a26
plusjob70 <plusjob70@gmail.com>
2022-10-31 13:18:42 +0900

    feat: 특정 숫자의 박수 횟수 계산

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return 0;
+    }
+
+    static int numClap(int number) {
+        int cnt = 0;
+        int remain;
+
+        while (number > 0) {
+            remain = number % 10;
+            if (remain == 3 || remain == 6 || remain == 9) {
+                cnt++;
+            }
+            number /= 10;
+        }
+
+        return cnt;
     }
 }
