eae982b242b7636f5fc5d6307fcf10a45e06b0ff
yubin <dbqls2821@gmail.com>
2022-10-31 22:35:10 +0900

    FEAT: [Problem7] 친구들을 Map에 추가하는 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,12 +10,28 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
         init(user);
+        putFriendsToMap(user, friends);
     }
 
     private static void init(String user) {
         alreadyFriend = new ArrayList<>();
         scoreMap = new HashMap<>();
         scoreMap.put(user, 0);
+    }
+
+    private static void putFriendsToMap(String user, List<List<String>> friends) {
+        for (List<String> list : friends) {
+            if (list.get(0).equals(user)) {
+                alreadyFriend.add(list.get(1));
+                scoreMap.put(list.get(1), 0);
+            } else if (list.get(1).equals(user)) {
+                alreadyFriend.add(list.get(0));
+                scoreMap.put(list.get(0), 0);
+            } else {
+                if(!scoreMap.containsKey(list.get(0))) scoreMap.put(list.get(0), 0);
+                if(!scoreMap.containsKey(list.get(1))) scoreMap.put(list.get(1), 0);
+            }
+        }
     }
 
 }
