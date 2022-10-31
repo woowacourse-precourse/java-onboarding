a7e4f5b6cbe0657ed884304ddfdb4d8cf3023ac8
leekyukin <leekujin14@gmail.com>
2022-11-01 00:44:51 +0900

    fix: 테스트 케이스 실패 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -38,15 +38,15 @@ public class Problem4 {
         dictionary.put('A', 'Z'); dictionary.put('B', 'Y'); dictionary.put('C', 'X'); dictionary.put('D', 'W');
         dictionary.put('E', 'V'); dictionary.put('F', 'U'); dictionary.put('G', 'T'); dictionary.put('H', 'S');
         dictionary.put('I', 'R'); dictionary.put('J', 'Q'); dictionary.put('K', 'P'); dictionary.put('L', 'O');
-        dictionary.put('M', 'N'); dictionary.put('N', 'M'); dictionary.put('O', 'L'); dictionary.put('Q', 'J');
-        dictionary.put('R', 'I'); dictionary.put('S', 'H'); dictionary.put('T', 'G'); dictionary.put('U', 'F');
-        dictionary.put('V', 'E'); dictionary.put('W', 'D'); dictionary.put('X', 'C'); dictionary.put('Y', 'B');
-        dictionary.put('Z', 'A');
+        dictionary.put('M', 'N'); dictionary.put('N', 'M'); dictionary.put('O', 'L'); dictionary.put('P', 'K');
+        dictionary.put('Q', 'J'); dictionary.put('R', 'I'); dictionary.put('S', 'H'); dictionary.put('T', 'G');
+        dictionary.put('U', 'F'); dictionary.put('V', 'E'); dictionary.put('W', 'D'); dictionary.put('X', 'C');
+        dictionary.put('Y', 'B'); dictionary.put('Z', 'A');
 
         dictionary.put('a', 'z'); dictionary.put('b', 'y'); dictionary.put('c', 'x'); dictionary.put('d', 'w');
         dictionary.put('e', 'v'); dictionary.put('f', 'u'); dictionary.put('g', 't'); dictionary.put('h', 's');
         dictionary.put('i', 'r'); dictionary.put('j', 'q'); dictionary.put('k', 'p'); dictionary.put('l', 'o');
-        dictionary.put('m', 'n'); dictionary.put('n', 'm'); dictionary.put('o', 'l'); dictionary.put('q', 'j');
+        dictionary.put('m', 'n'); dictionary.put('n', 'm'); dictionary.put('o', 'l'); dictionary.put('p', 'k'); dictionary.put('q', 'j');
         dictionary.put('r', 'i'); dictionary.put('s', 'h'); dictionary.put('t', 'g'); dictionary.put('u', 'f');
         dictionary.put('v', 'e'); dictionary.put('w', 'd'); dictionary.put('x', 'c'); dictionary.put('y', 'b');
         dictionary.put('z', 'a');
