d849cbfdc2a6095615f11f421cba4c8f7efa0dc9
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:53:43 +0900

    [problem3]3,6,9 검증 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,26 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
 
-        int count = 0;
 
-        String[] roomsize = new String[number];
-        char[][] piece = new char[number][];
-
-
-        for (int i = 0; i < number; i++) {
-            roomsize[i]=Integer.toString(i+1);
-            piece[i] = new char[roomsize[i].length()];
-
-            for (int j = 0; j < roomsize[i].length(); j++) {
-                piece[i][j] = roomsize[i].charAt(j);
+        for (int i = 0; i < piece.length; i++) {
+            for (int j = 0; j < piece[i].length; j++) {
+                if (piece[i][j] == '3' || piece[i][j] == '6' || piece[i][j] == '9') {
+                    count++;
+                }
             }
         }
-
-
+        return count;
 
     }
-
-
 
 
 }
