32199b18603c5d859e32b7b5dcd9b08b1a948dd2
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 01:10:03 +0900

    docs: problem5 입력값이 유효한지에 대해 예외체크하는 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,27 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        if(checkException(money)){
+            throw new IllegalArgumentException();
+        }
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+    public static boolean checkException(int money){
+        if(chenckRange(money) || !checkNumber(money)){
+            return true;
+        }
+        return false;
+    }
+    public static boolean chenckRange(int money){
+        return money < 1 || money >1000000;
+    }
+    public static  boolean checkNumber(int money){
+        return String.valueOf(money).chars().allMatch(Character::isDigit);
+        
     }
 }
