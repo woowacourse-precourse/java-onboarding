54ea9d61ed59d67ad56c989a2b5965e301eaca71
footdev <ekclstkfka5@gmail.com>
2022-11-01 01:27:06 +0900

    refactor: 기능5 exchange 메소드 구현
    인자로 들어온 인덱스의 값만큼 반환해주는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,63 +1,35 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static int totalMoney;
+    public static final int[] won = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         totalMoney = money;
-        List<Integer> answer = exchangeMoney(money);
-        return answer;
+        List<Integer> problemFiveAnswer = exchangeMoney();
+        return problemFiveAnswer;
     }
 
-    public static List<Integer> exchangeMoney(int money) {
-        return Collections.emptyList();
-    }
-
-    public static List<Integer> setExchangedMoney() {
-        List<Integer> exchangedMoney = new ArrayList<>();
+    public static List<Integer> exchangeMoney() {
+        Integer[] exchangedMoney = new Integer[9];
         for (int i = 0; i < 9; i++) {
-            exchangedMoney.add(0);
+            exchangedMoney[i] = 0;
+            exchangedMoney[i] += exchange(i);
         }
-        return exchangedMoney;
+        return Arrays.asList(exchangedMoney);
     }
 
-    public static int exchangeFiftyThousandWon() {
-        return 0;
-    }
+    public static int exchange(int index) {
+        if(totalMoney < won[index]) {
+            return 0;
+        }
 
-    public static int exchangeTenThousandWon() {
-        return 0;
-    }
-
-    public static int exchangeFiveThousandWon() {
-        return 0;
-    }
-
-    public static int exchangeOneThousandWon() {
-        return 0;
-    }
-
-    public static int exchangeFiveHundredWon() {
-        return 0;
-    }
-
-    public static int exchangeOneHundredWon() {
-        return 0;
-    }
-
-    public static int exchangeFiftyWon() {
-        return 0;
-    }
-
-    public static int exchangeTenWon() {
-        return 0;
-    }
-
-    public static int exchangeOneWon() {
-        return 0;
+        int numMoney = totalMoney / won[index];
+        totalMoney = totalMoney - won[index] * numMoney;
+        return numMoney;
     }
 }
