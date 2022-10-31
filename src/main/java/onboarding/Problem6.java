4cf61a7924d6748a17a07aaf2088b6b73f41abf3
yumyeonghan <mgtmh991013@gmail.com>
2022-10-31 15:24:18 +0900

    feat: Problem6, solution()
    Sort in ascending order and remove duplicates on the returned result after complete search

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> tmpList = new ArrayList<String>();
+
+        tmpList = bruteForce(forms);
+
+        List<String> collect = tmpList.stream().distinct().collect(Collectors.toList()); // 중복 제거
+        collect.sort(Comparator.naturalOrder()); // 오름차순 정렬
+
+        answer = collect;
+
         return answer;
+    }
+
+    // 모든 경우의 수 탐색
+    private static List<String> bruteForce(List<List<String>> forms) {
+
+        List<String> tmpList = new ArrayList<String>(); // 임시 리스트
+
+        return tmpList;
     }
 }
