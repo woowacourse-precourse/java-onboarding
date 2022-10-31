9a437cddbbadb8b7abcdef5a0edbf2e3ed594616
jmjmjames <chungjm0711@gmail.com>
2022-11-01 08:38:44 +0900

    refactor: final keyword 추가 및 기능 구현 docs 정리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,18 +3,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        StringBuilder answer = new StringBuilder();
+        final StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);  // 현재 문자
-            answer.append(reverseCharacter(ch));  // 청개구리 변환
+            final char ch = word.charAt(i);
+            answer.append(reverseCharacter(ch));
         }
 
         return answer.toString();
     }
 
     private static char reverseCharacter(char character) {
-        // no reverse (문자가 아닐 때)
         if (!Character.isAlphabetic(character)) {
             return character;
         }
