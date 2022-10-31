97a5b8e84b8a485a64f792fdfdd5254c7adbe2eb
eugene1204 <s1061001@naver.com>
2022-10-31 16:12:37 +0900

    기능 목록 : problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem3 {
+    // 1. number -> String으로 변경후 3,6,9가 있으면 count로 세기
     public static int solution(int number) {
         int answer = 0;
         return answer;
