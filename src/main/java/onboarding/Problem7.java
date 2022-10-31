5c45e96255adfdd4417706119a79b73416440ba8
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-31 12:42:23 +0900

    feat: Create registerRelationshipMap | [problem 7]
    - 입력받은 friends 중 각 리스트의 각각의 사람을key로, 그 한 명의 친구관계를 value로 저장한다
        - 동일한 친구 관계가 중복해서 주어지지 않기에 중복체크는 하지않았다
        - ex. 위의 예시의 friends -> {andole=[donut, shakevan], jun=[donut, shakevan], shakevan=[andole, jun, mrko], donut=[andole, jun, mrko], mrko=[donut, shakevan]}

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static HashMap<String, ArrayList<String>> registerRelationshipMap(List<List<String>> friends) {
+        HashMap<String, ArrayList<String>> relationshipMap = new HashMap<>();
+
+        for (List<String> relation :
+                friends) {
+            for (int i = 0; i < 2; i++) {
+                ArrayList<String> value;
+                String person1 = relation.get(i);
+                String person2 = relation.get(1 - i);
+
+                if (relationshipMap.containsKey(person1)) {
+                    value = relationshipMap.get(person1);
+                } else {
+                    value = new ArrayList<>();
+                }
+
+                value.add(person2);
+                relationshipMap.put(person1, value);
+            }
+        }
+        return relationshipMap;
     }
 }
