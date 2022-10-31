1cb414c9b766a03b81763d47df66ce770fe81e57
cksdud8028 <cksdud8028@naver.com>
2022-10-31 21:16:07 +0900

    FEAT checkDuplicate

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,12 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
     static Map<String, String> crews = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
         initialCrews(forms);
+
         List<String> answer = List.of("answer");
         return answer;
     }
@@ -18,4 +17,25 @@ public class Problem6 {
             crews.put(crew.get(1), crew.get(0));
         }
     }
+
+    private static List<String> checkDuplicate() {
+        List<String> result = new ArrayList<>();
+        String[] names = crews.keySet().toArray(new String[0]);
+        String name;
+        for (int i = 0; i < names.length; i++) {
+            name = names[i];
+            for (int j = 1; j < name.length(); j++) {
+                String temp = String.valueOf(name.charAt(j - 1) + name.charAt(j));
+                for (String otherName : names) {
+                    if (otherName.contains(temp)) {
+                        result.add(crews.get(name));
+                        result.add(crews.get(otherName));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
 }
