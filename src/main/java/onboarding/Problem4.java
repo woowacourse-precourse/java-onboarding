dab947bd5479b7608caa499fca0e3120e60134bc
dener8 <8dener@gmail.com>
2022-10-31 21:04:18 +0900

    [Problem4] feat: 반대로 변환해주는 changeToReverse 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = changeToReverse(word);
         return answer;
+    }
+
+    static String changeToReverse(String word) {
+        String reverseWord = "";
+        for (char alphabet : word.toCharArray()) {
+            if ('a' <= alphabet && 'z' >= alphabet) {
+                reverseWord += (char)('z' - (alphabet - 'a'));
+            } else if ('A' <= alphabet && 'Z' >= alphabet) {
+                reverseWord += (char)('Z' - (alphabet - 'A'));
+            } else {
+                reverseWord += ' ';
+            }
+        }
+        return reverseWord;
     }
 }
