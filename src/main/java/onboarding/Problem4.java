c77a3632d8119f618db7875c2c1286912b4365b6
Songyi Kim <ksl2950@naver.com>
2022-10-31 09:45:30 +0900

    feat/4: 문자열 변환 로직 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -13,11 +13,22 @@ public class Problem4 {
             interval += 1;
         }
 
-
-        return "";
+        StringBuilder answer = new StringBuilder();
+        for (int j = 0; j < word.length(); j++) {
+            char temp = word.charAt(j);
+            answer.append(getConvertedChar(temp, map));
+        }
+        return answer.toString();
     }
 
-
+    public static char getConvertedChar(char c, Map<Character, Character> map) {
+        char upperCase = Character.toUpperCase(c);
+        if (map.containsKey(upperCase)) {
+            Character convertedValue = map.get(upperCase);
+            return getLowerOrUpper(c,convertedValue);
+        }
+        return c;
+    }
 
     private static Character getLowerOrUpper(char c, Character convertedValue) {
         if (Character.isUpperCase(c)) {
