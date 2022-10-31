32bb4dc40dc5fedc3e54aa5a80375e78872c5154
yoonsj970407 <yoonsj970407@gmail.com>
2022-10-31 18:50:22 +0900

    ✨Feat: Add exception()

--- src/main/java/onboarding/Duplicate.java
+++ src/main/java/onboarding/Duplicate.java
@@ -6,4 +6,18 @@ class Duplicate {
     public Duplicate(String cryptogram) {
         this.cryptogram = cryptogram;
     }
+
+    int exception() {
+        if((cryptogram.length() < 1) || (cryptogram.length() > 1000)) {
+            return -1;
+        }
+
+        for(int i = 0; i < cryptogram.length(); i++) {
+            if(!((cryptogram.charAt(i) >= 'a') && (cryptogram.charAt(i) >= 'z'))) {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
 }
