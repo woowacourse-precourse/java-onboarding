dfc6f80b8d792e77a9af8f0aeb6ced6875c35339
khsrla9806 <kimhunsope@gmail.com>
2022-10-31 15:10:03 +0900

    refactor(Problem5): withdraw -> withdrawMoney 변경 후 내부 코드 리팩토링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,29 +1,21 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> withdraw(int money) {
-        List<Integer> withdrawList = new ArrayList<>();
-        withdrawList.add(0, money / 50000);            // 50,000원
-        withdrawList.add(1, money % 50000 / 10000);    // 10,000원
-        withdrawList.add(2, money % 10000 / 5000);     // 5,000원
-        withdrawList.add(3, money % 5000 / 1000);      // 1,000원
-        withdrawList.add(4, money % 1000 / 500);       // 500원
-        withdrawList.add(5, money % 500 / 100);        // 100월
-        withdrawList.add(6, money % 100 / 50);         // 50원
-        withdrawList.add(7, money % 50 / 10);          // 10원
-        withdrawList.add(8, money % 10);               // 1원
+    public static List<Integer> withdrawMoney(int money) {
+        List<Integer> moneyList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
-        return withdrawList;
+        for(int i = 0; i < moneyList.size(); i++) {
+            int divisor = moneyList.get(i);                // 나눠지는 수
+            moneyList.set(i, money / moneyList.get(i));    // 리스트 값을 화폐 개수로 변경
+            money = money % divisor;                       // 다음 계산을 위해 money 값 초기화
+        }
+
+        return moneyList;
     }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-
-        answer = withdraw(money);
-
-        return answer;
+        return withdrawMoney(money);
     }
 }
