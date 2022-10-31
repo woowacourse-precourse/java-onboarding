180f22d4a6acc4b4524d79a4c281e307e3e65900
ahinkim <ahin98@naver.com>
2022-11-01 08:58:14 +0900

    refactor(problem1.md): Modify feature list feat(problem1.java): Add function to find the maximum value of Poby

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,30 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+        // 1. 포비의 왼쪽 페이지와 오른쪽 페이지에서 나올 수 있는 가장 큰 수 중 더 큰 수를 구한다.
+        int pmax_total = 0;
+        for (int i = 0; i < 2; i++) {
+            String page = Integer.toString(pobi.get(i));
+            int total = 0;
+            int N = page.length();
+            for (int j = 0; j < N; j++) {
+                char num = page.charAt(j);
+                // 더해야 할 때
+                if (total <= 1 || num == '0' || num == '1') {
+                    total += num - '0';
+                }
+                // 곱해야 할 때
+                else {
+                    if (total == 0) {
+                        total = 1;
+                    }
+                    total *= num - '0';
+                }
+            }
+            System.out.println(total);
+            pmax_total = Math.max(pmax_total, total);
+        }
         return answer;
     }
 }
