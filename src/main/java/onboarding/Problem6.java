7aea98e3749756b0d56ce51388c4c4ee4d354717
subin Park <psb0320@hs.ac.kr>
2022-11-01 01:05:34 +0900

    feat: 닉네임에서 나올 수 있는 모든 두 글자 단어들을 반환하는 기능 추가 - 두 글자가 중복의 최소단위이기 때문에 두 글자만 검사해도 모든 중복을 찾아낼 수 있음

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> twoWords = getTwoWords(forms);
+
         return answer;
+    }
+
+    public static List<String> getTwoWords(List<List<String>> forms) {
+        List<String> list = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+
+            if (nickname.length() == 1) continue;
+
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                list.add(nickname.substring(j, j + 2));
+            }
+        }
+
+        return list;
     }
 }
