9c428c9f977453c466c3cf9e809638b24bc221bf
janu01ary <janu01ary@gmail.com>
2022-10-31 17:39:57 +0900

    [Problem4] 대문자인 경우만 문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,13 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                result += (char)('Z' - c + 'A');
+            }
+        }
+        return result;
     }
 }
