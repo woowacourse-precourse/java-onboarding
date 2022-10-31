94a720a8275ebb4ff29cb9d8d3be2a764d6c76e2
ys200209 <lys200209@naver.com>
2022-10-31 17:18:48 +0900

    feat(문제6): 크루별로 연속한 이름 구하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    private static Map<String, String> sequenceNamesMap = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
+
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> crew = forms.get(i);
+            addSequenceName(crew);
+        }
+
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static void addSequenceName(List<String> crew) {
+
+        String email = crew.get(0);
+        String name = crew.get(1);
+        for (int i = 1; i < name.length(); i++) {
+            String sequenceName = name.substring(i - 1, i + 1);
+            sequenceNamesMap.put(sequenceName, email);
+        }
+
+    }
+
 }
