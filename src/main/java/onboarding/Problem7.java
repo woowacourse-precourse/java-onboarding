5762ce154ecc97f65a17e9c0f1e146eedab85211
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-10-31 22:58:49 +0900

    feat(Problem7): 친구 관계 분류 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map<String, List<String>> analyzeRelationship(List<List<String>> friends) {
+        Map<String, List<String>> relationships = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            makeRelationshipsMap(relationships, friend.get(0), friend.get(1));
+            makeRelationshipsMap(relationships, friend.get(1), friend.get(0));
+        }
+
+        return relationships;
+    }
+
+    private static void makeRelationshipsMap(Map<String, List<String>> relationships, String person, String friendsOfPerson) {
+        if (relationships.containsKey(person)) {
+            relationships.get(person).add(friendsOfPerson);
+        } else {
+            List<String> friends = new ArrayList<>();
+
+            friends.add(friendsOfPerson);
+            relationships.put(person, friends);
+        }
     }
 }
