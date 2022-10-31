d9cfcc535c6ebee7c45d0883e52dd0d50cc109e2
Damgom <wisewish1@gmail.com>
2022-10-31 13:10:43 +0900

    Feat: add lowercase to alphabet map

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,12 +8,19 @@ public class Problem4 {
         String alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Map<String, String> alpMap = new HashMap<>();
         addUppercaseToAlpMap(alp, alpMap);
+        addLowercaseToAlpMap(alp, alpMap);
         return answer.toString();
     }
     private static void addUppercaseToAlpMap(String alp, Map<String, String> alpMap) {
         for(int i = 0; i < alp.length(); i++) {
             alpMap.put(String.valueOf(alp.charAt(i)),
                     String.valueOf(alp.charAt(alp.length()-1-i)));
+        }
+    }
+    private static void addLowercaseToAlpMap(String alp, Map<String, String> alpMap) {
+        for(int i = 0; i < alp.length(); i++) {
+            alpMap.put(String.valueOf(alp.charAt(i)).toLowerCase(),
+                    String.valueOf(alp.charAt(alp.length()-1-i)).toLowerCase());
         }
     }
 }
