120e568b08fd52c96d6a1781874597a654ff87c0
farmJun <junpreme1205@gmail.com>
2022-10-31 15:54:12 +0900

    Problem3. 어떤 숫자에 대해 몇 번 박수를 쳐야하는지 계산하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,10 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    //전달받은 숫자에 대해 몇 번 박수를 쳐야하는지 계산하는 함수
+    public static int calculateHowManyClap(int num) {
+        int clap = (int) String.valueOf(num).chars().filter(a -> a == '3' || a == '6' || a == '9').count();
+        return clap;
+    }
 }
