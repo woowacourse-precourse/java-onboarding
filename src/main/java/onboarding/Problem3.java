840576c6bc33a0e04b163008c9c6cbfe7c4a01ba
DongWooE <ehddn977@naver.com>
2022-11-01 04:16:18 +0900

    [p3][add] 1~number까지 반복하는 iterate 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+
+    int count = 0;
     public static int solution(int number) {
         int answer = 0;
+        Problem3 p3 = new Problem3();
+        p3.iterate(number);
         return answer;
+    }
+
+    private void iterate(int number) {
+        for(int i =1; i <= number; i++){
+            count++;
+        }
     }
 }
