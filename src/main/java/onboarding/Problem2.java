3b144f22d96dbc3e3498a5caea3b772d60ecaaf7
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-10-31 22:54:52 +0900

    feat : 문제2 스택값 비교하며 중복값 제거하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,8 +13,14 @@ public class Problem2 {
             //각 자리의 글씨 가져오기
             str = cryptogram.charAt(i) + "";
             //맨 처음에는 첫번째 글자 Stack에 넣어주기
+            if (charStack.isEmpty()) charStack.add(str);
+            else {
+                //추출한 str값이 stack 마지막 push값(=peek)과 같으면 제거(=pop)
+                if (str.equals(charStack.peek())) charStack.pop();
+                else charStack.add(str);
+            }
         }
-        
+        for (String s : charStack) answer += s;
         return answer;
     }
 }
