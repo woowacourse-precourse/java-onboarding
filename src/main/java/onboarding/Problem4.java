c0b91a0e99ab6f8b8056d1d1092163d69e2f1811
brotherstone97 <brotherstone97@gmail.com>
2022-11-01 00:11:41 +0900

    Feat: 알파벳을 반대로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+    static char changeCharacter(char c) {
+        int asciiCode = c;
+        if (65 <= asciiCode && asciiCode <= 90)
+            return (char) (155 - asciiCode);
+
+        return (char) (219 - asciiCode);
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
