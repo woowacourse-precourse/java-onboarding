0ad42c183949bd107f9e8a1fc662f0e8cbe28905
wonseok <tkddls23@konkuk.ac.kr>
2022-11-01 01:57:00 +0900

    feat: 4 단어의 대소문자, 공백을 구분해 반대로 변환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return getOppositeWord(word);
+    }
+
+    private static String getOppositeWord(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+
+            if (Character.isUpperCase(word.charAt(i)))
+                sb.append((char) ('Z' - word.charAt(i) + 'A'));
+
+            if (Character.isLowerCase(word.charAt(i)))
+                sb.append((char)('z' - word.charAt(i) + 'a'));
+
+            if (word.charAt(i) == ' ')
+                sb.append(word.charAt(i));
+        }
+        return sb.toString();
     }
 }
