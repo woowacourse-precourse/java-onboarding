5f3f96dce27a827fe50452cba54c65698ae25e78
siwookim97 <tldn12gh@gmail.com>
2022-10-31 22:18:48 +0900

    feat : problem6) 비슷한 닉네임을 확인하는 기능 추가
    비슷한 닉네임을 확인하는 기능
    닉네임에 같은 글자가 연속적으로 포함되는 크루를 확인
    크루 2명의 List를 받아 같은글자가 연속적으로 포함되면 Treeset에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -31,6 +31,25 @@ public class Problem6 {
         return isTrue;
     }
 
+    /**
+     * 닉네임에 같은 글자가 연속적으로 포함되는 크루 확인하는 메서드
+     *
+     * @param crew_1 크루 1
+     * @param crew_2 크루 2
+     */
+    static void checkDuplication (List<String> crew_1, List<String> crew_2) {
+        for (int i = 0; i < crew_1.get(1).length() - 1; i++) {
+            for (int j = 0; j < crew_2.get(1).length() - 1; j++) {
+                if ((crew_1.get(1).charAt(i) == crew_2.get(1).charAt(j))
+                        && crew_1.get(1).charAt(i + 1) == crew_2.get(1).charAt(j + 1)) {
+                    addToSet(crew_1.get(0));
+                    addToSet(crew_2.get(0));
+                    return;
+                }
+            }
+        }
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
