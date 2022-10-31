4a2de6e1a1fc844a32c84518769d1a071d8c0721
ensk26 <tjensk26@gmail.com>
2022-11-01 01:22:41 +0900

    feat: 추천 점수가 가장 높은 5명을 반환하는 기능 추가, problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,13 +7,67 @@ import java.util.List;
 
 public class Problem7 {
 
+    static class RecommendScore implements Comparable<RecommendScore> {
+        String id;
+        int score;
+
+        public RecommendScore(String id, int score) {
+            this.id = id;
+            this.score = score;
+        }
+
+        @Override
+        public int compareTo(RecommendScore o) {
+            int compare1 = Integer.compare(o.score, score);
+            if (compare1 == 0) {
+                return id.compareTo(o.id);
+            }
+            return compare1;
+        }
+    }
+
     static HashMap<String, List<String>> friendList = new HashMap<>();
 
     static HashMap<String, Integer> recommendScore = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
-        List<String> answer = Collections.emptyList();
+        for (List<String> friend : friends) {
+            saveFriendById(friend.get(0), friend.get(1));
+        }
+
+        saveFriendScoreByUser(user);
+
+        for (String visitor : visitors) {
+            saveVisitScore(visitor);
+        }
+
+        System.out.println("recommendScore = " + recommendScore.toString());
+        List<RecommendScore> scoreList = new ArrayList<>();
+
+        int score;
+        for (String id : recommendScore.keySet()) {
+
+            if (friendList.get(user).contains(id)) {
+                continue;
+            }
+
+            score = recommendScore.get(id);
+
+            if (score > 0) {
+                scoreList.add(new RecommendScore(id, score));
+            }
+        }
+
+        Collections.sort(scoreList);
+
+        List<String> answer = new ArrayList<>();
+        int size = Math.min(scoreList.size(), 5);
+
+        for (int i = 0; i < size; i++) {
+            answer.add(scoreList.get(i).id);
+        }
+
         return answer;
     }
 
