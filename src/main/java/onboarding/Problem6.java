7be854401c574084f711964cd2c243453c94d31e
ddingmin <ddingminyt@gmail.com>
2022-10-31 10:03:45 +0900

    feat(Ploblem6): 중복된 닉네임이 존재하는지 확인하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -22,6 +22,21 @@ public class Problem6 {
                 return answer;
             }
         }
+
+        for (int i = 0; i < userNickname.size(); i++) {
+            Boolean flag = false;
+            for (int j = 0; j < i; j++) {
+                if (isDuplicationName(userNickname.get(i), userNickname.get(j))){
+                    flag = true;
+                }
+            }
+            for (int j = i + 1; j < userNickname.size(); j++) {
+                if (isDuplicationName(userNickname.get(i), userNickname.get(j))){
+                    flag = true;
+                }
+            }
+            System.out.println(flag);
+        }
 //        if (isValidEmail(forms.get(0).get(0))){
 //            List<String> answer = List.of("correct");
 //            return answer;
@@ -39,7 +54,6 @@ public class Problem6 {
 
         for (int i = 0; i < email.length(); i++) {
             if (email.charAt(i) == at){
-                System.out.println(getDomain(email, i + 1));
                 return isDomainEmail(getDomain(email, i + 1));
             }
         }
@@ -61,6 +75,38 @@ public class Problem6 {
 
     private static String getNickname(List<String> form){
         return form.get(1);
+    }
+
+    private static boolean isDuplicationName(String name1, String name2){
+        char currentName1Char, previousName1Char;
+        if (name1.length() < 2 || name2.length() < 2){
+            return false;
+        }
+
+        for (int i = 1; i < name1.length(); i++) {
+            currentName1Char = name1.charAt(i);
+            previousName1Char = name1.charAt(i - 1);
+            if (checkDuplicationWord(currentName1Char, previousName1Char, name2)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkChar(char a, char b){
+        return (a == b);
+    }
+
+    private static boolean checkDuplicationWord(char currentName1Char, char previousName1Char, String name2){
+        char currentName2Char, previousName2Char;
+        for (int j = 1; j < name2.length(); j++) {
+            currentName2Char = name2.charAt(j);
+            previousName2Char = name2.charAt(j - 1);
+            if (checkChar(currentName1Char, currentName2Char) && (checkChar(previousName1Char, previousName2Char))){
+                return true;
+            }
+        }
+        return false;
     }
 
 
