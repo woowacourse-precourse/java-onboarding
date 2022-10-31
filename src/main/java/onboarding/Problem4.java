c4365bcf00e62667d41b630765dbf0ddfd699436
yeongjin <hanyj0104@naver.com>
2022-11-01 05:54:22 +0900

    영어일 경우 반전시키는기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0;i<word.length();i++){
+            char now=word.charAt(i);
+            if(now>='A'&& now<='z'){
+                char a=' ';
+                if(Character.isUpperCase(now)){
+                    a=(char)(now+('M'-now)*2+1);
+                }else {
+                    a = (char) (now + ('m' - now) * 2 + 1);
+                }
+
+
+            }
+        }
         return answer;
     }
 }
