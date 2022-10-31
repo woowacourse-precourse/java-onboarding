b5daf5a8d422baa608eb1fd401e90a070092d9db
hyejuc <chjjudy@naver.com>
2022-11-01 07:08:49 +0900

    deleteDuplicate - Stack 이용해 연속 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
         return answer;
+    }
+    public static Stack<Character> deleteDuplicate(String cryptogram){
+        Stack<Character> cryptogramStack = new Stack<>();
+        char[] cryptogramCharArray = cryptogram.toCharArray();
+        for (int i = 0; i < cryptogramCharArray.length; i++) {
+            if(!cryptogramStack.empty() && cryptogramStack.peek().equals(cryptogramCharArray[i])){
+                cryptogramStack.pop();
+            }
+            else {
+                cryptogramStack.push(cryptogramCharArray[i]);
+            }
+        }
+        return cryptogramStack;
     }
 }
