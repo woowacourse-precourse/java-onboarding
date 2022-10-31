2d934681dd74fe1589583342415c66f2b8296f4f
w0ntae <bitcampcoding@gmail.com>
2022-11-01 02:03:44 +0900

    p6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,49 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        
+        ArrayList<String> repeats = new ArrayList<>();
+    
+        ArrayList<String> names = new ArrayList<>();
+    
+        ArrayList<String> emails = new ArrayList<>();
+        
+        for(List<String> pairs : forms){
+            for(String entries : pairs){
+                if (entries.contains("@email.com")){
+                    emails.add(entries);
+                }
+                else {
+                    names.add(entries);
+                }
+            }
+        }
+        System.out.println(names);
+        System.out.println(emails);
+        
+        for(int n = 1; n < names.size(); n++){
+            String cname = names.get(n-1);
+            for (int p = 1; p < cname.length(); p++){
+                String compare = "" + cname.charAt(p-1) + cname.charAt(p);
+                for (int l = n; l < names.size(); l++){
+                    if (names.get(l).contains(compare)){
+                        System.out.println(compare + l);
+                        if(repeats.contains(emails.get(n-1)) == false){
+                            repeats.add(emails.get(l));
+                            repeats.add(emails.get(l-1));
+                        }
+                        if(repeats.contains(emails.get(l)) == false){
+                            repeats.add(emails.get(l));
+                        }
+                    }
+                }
+            }
+        }
+        
+        return repeats;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,51 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+    
+        HashMap<String, Integer> scoreList = new HashMap<>();
+        ArrayList<String> friendList = new ArrayList<>();
+        
+        for (List<String> userFriends: friends){
+            if (userFriends.contains(user)){
+                for(String names: userFriends){
+                    if(names.equals(user) == false){
+                        for(List<String> friendFriends: friends){
+                            if (friendFriends.contains(names)){
+                                for(String name : friendFriends){
+                                    if(name.equals(names) == false && name.equals(user) == false){
+                                        if (friendList.contains(name) == false){
+                                            friendList.add(names);
+                                        }
+                                        if(scoreList.containsKey(name) == false){
+                                            scoreList.put(name, 10);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        for(String name: visitors){
+            if (scoreList.containsKey(name)){
+                scoreList.replace(name,scoreList.get(name) + 1);
+            }
+            else if(friendList.contains(name) == false) {
+                scoreList.put(name, 1);
+            }
+        }
+        
+        
+        
+        System.out.println(friendList);
+        
+        System.out.println(scoreList);
+        
         List<String> answer = Collections.emptyList();
         return answer;
     }
