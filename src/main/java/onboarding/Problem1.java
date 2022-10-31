0df32a5bd431a8d2bf927c8fed100f5c7150a0a8
myungjunlee <dlaudwns789@gmail.com>
2022-10-31 18:13:32 +0900

    feat(Problem1): 페이지 번호의 각 자리 숫자를 더하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,25 +5,41 @@ import java.util.List;
 class Problem1 {
 
     boolean checkException(List<Integer> pobi, List<Integer> crong) {
-        if (pobi.get(1) - pobi.get(0) != 1)
-            return false;
-
-        if (crong.get(1) - crong.get(0) != 1)
-            return false;
-
-        if (pobi.get(0) == 1 || crong.get(0) == 1) {
+        if (pobi.get(1) - pobi.get(0) != 1) {
             return false;
         }
-
-        if (pobi.get(1) == 400 || crong.get(1) == 400) {
+        if (crong.get(1) - crong.get(0) != 1) {
             return false;
         }
+        if ((pobi.get(0) == 1) || (crong.get(0) == 1)) {
+            return false;
+        }
+        if ((pobi.get(1) == 400) || (crong.get(1) == 400) {
+            return false;
+        }
+        return true;
     }
+
+    int plusPage(Integer page) {
+        int num = 0;
+
+        String page_str = Integer.toString(page);
+
+        for(int i=0; i < page_str.length(); i++){
+            num += Integer.parseInt(page_str.substring(i, i+1));
+        }
+
+        return num;
+
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int leftPageNum;
 
-        if (!checkException(pobi, crong))
+        if (!checkException(pobi, crong)) {
             return -1;
+        }
 
 
 
