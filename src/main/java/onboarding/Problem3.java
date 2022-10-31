4626e977d38b338dc720aae17bb6b7194352e917
watterricecake <fruturum@nate.com>
2022-10-31 13:56:48 +0900

    feat: Problem3 1000이상 입력값 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -21,12 +21,14 @@ public class Problem3 {
     private static List<Integer> makeList(Integer num){
         List<Integer> li = new ArrayList<Integer>();
         List<Integer> digit = new ArrayList<Integer>();
-        digit.add(0,100);
-        digit.add(1,10);
+        digit.add(0,10000);
+        digit.add(1,1000);
+        digit.add(2,100);
+        digit.add(3,10);
         Integer number = 0;
         int i = 0;
         boolean check = false;
-        for(int n = 0; n<2; n ++) {
+        for(int n = 0; n<4; n ++) {
             number = num / digit.get(n);
             if(number != 0 ){
                 check = true;
