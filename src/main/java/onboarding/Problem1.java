7b000a8f18333ee9875b9ca00627415fc1e40e67
jihun-maru <gnswlrla0218@naver.com>
2022-10-31 12:21:22 +0900

    Add isNotValid function[Problem1]

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,22 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    
+    
+    private static isNotValid(List<Integer> page) {
+    	int leftPage = page.get(0);
+    	int rightPage = page.get(1);
+    	
+    	if (rightPage - leftPage != 1 || rightPage > 400) {
+    		return true;
+    	}
+    	
+    	if (leftPage % 2 == 0 || rightPage % 2 == 1) {
+    		return true;
+    	}
+    	
+    	return false;
+    }
+    
+    
 }
