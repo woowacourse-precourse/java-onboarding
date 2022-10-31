e7c85226111301d59439745a0a2f4a863840e975
siwookim97 <tldn12gh@gmail.com>
2022-11-01 03:11:22 +0900

    feat : problem7) 친구 리스트에서 사람을 찾는 기능 추가
    사용자의 친구 리스트에서 찾고자 하는 사람이 있는지 확인하는 기능
    확인하는 사람과 친구 리스트를 받은 후
    친구 리스트에 있으면 true, 아니라면 false를 반환한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,7 +12,7 @@ public class Problem7 {
      * @param friends 친구 관계 정보
      * @return 사용자와 친구인 리스트
      */
-    public List<String> createUserFriendsList(String user, List<List<String>> friends) {
+    static List<String> createUserFriendsList(String user, List<List<String>> friends) {
         List<String> list = new ArrayList<>();
 
         for (int i = 0; i < friends.size(); i++) {
@@ -26,6 +26,23 @@ public class Problem7 {
         }
 
         return list;
+    }
+
+    /**
+     * 사용자의 친구 리스트에서 찾고자 하는 사람이 있는지 확인하는 메서드
+     * 
+     * @param name 확인하는 사람
+     * @param friendsList 친구 리스트
+     * @return 친구 리스트에 있으면 true, 아니라면 false
+     */
+    static boolean isFriend(String name, List<String> friendsList) {
+        for (int i = 0; i < friendsList.size(); i++) {
+            if (friendsList.get(i) == name) {
+                return true;
+            }
+        }
+
+        return false;
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
