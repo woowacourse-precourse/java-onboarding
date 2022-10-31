73a0264e161eaf8ec34a5768bfa46d363fb30121
Han Jihye <haneh0714@gmail.com>
2022-10-31 16:01:18 +0900

    CHORE : Problem2 solution 수정 - 불필요한 조건 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,9 +9,11 @@ public class Problem2 {
             StringBuilder sb = new StringBuilder(""+cryptogram.charAt(0));
             for(int i=1; i<cryptogram.length(); i++) {
                 char now = sb.charAt(sb.length()-1);
-                if(i < cryptogram.length() && now == cryptogram.charAt(i)) {
+                if(now == cryptogram.charAt(i)) {
                     check = true;
-                    while(i < cryptogram.length() && now == cryptogram.charAt(i)) i++;
+                    while(i < cryptogram.length() && now == cryptogram.charAt(i)) {
+                        i++;
+                    }
                     i--;
                     sb.deleteCharAt(sb.length()-1);
                 } else {
