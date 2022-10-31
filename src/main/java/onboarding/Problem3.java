2f284bf78f86e966e01f104220646336aa00633e
yoonjuho <dbswn990@naver.com>
2022-10-31 19:25:47 +0900

    feat: PROBLEM2-(예외처리) 자연수 예외처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        if(hasException(number)){
+            return -1;
+        }
+        
+    }
+
+    public static boolean hasException(int number){
+        if(number<1 || number>10000)
+            return true;
+        return false;
     }
 }
