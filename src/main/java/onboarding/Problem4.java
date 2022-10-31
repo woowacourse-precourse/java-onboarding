b3e1476901a87ca8cdddfde250d869e73c11cc6c
hongjinwon <herenever@naver.com>
2022-11-01 02:36:30 +0900

    feat: Problem4 getAscii 추가
    - Problem4 getAscii 추가
    -  case1) 대문자  => return 155 - c
    -  case2) 소문자
    => return 219 - c
    -  case3) 공백문자 => return 32
    -  case4) 이외 => return -1

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,20 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static int getAscii(int c){
+        final int upperVal = 155;
+        final int lowerVal = 219;
+        final int spaceVal = 32;
+
+        if (65 <= c && c <= 90){
+            return upperVal - c;
+        }
+
+        if (97 <= c && c <=122){
+            return lowerVal - c;
+        }
+        return spaceVal;
+    }
+
 }
