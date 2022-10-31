41e8056fe8f2c734bbd995808ca940b46e13b784
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-10-31 21:41:24 +0900

    Docs(Problem4): 추가 기능 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/*
+기능 목록
+1. 청개구리 사전 기능
+    대문자는 (155 - 현재 글자 아스키코드)
+    소문자는 (219 - 현재 글자 아스키코드)
+    알파벳 외에는 그냥 반환하기
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
