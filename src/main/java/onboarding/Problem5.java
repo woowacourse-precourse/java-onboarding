49ad44265e98f9853918e8cd303e8316f8a5fae2
Leehyoungwoo <dlguddn3024@gmail.com>
2022-10-31 10:05:33 +0900

    feature: 5번 문제 시작

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,12 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+    public static List<Integer> countMoney(int money){
+        List<Integer> count = new ArrayList<>();
+
+        for (int len = count.length(), i=0;i<len;i++){
+
+        }
     }
 }
