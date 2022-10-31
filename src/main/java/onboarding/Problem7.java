800ba372f2770a8ca0d34388a8455c0b4316ede4
wugawuga <dnr3655@naver.com>
2022-11-01 00:08:08 +0900

    feat: 유저와 친구사이면 무시, 친구의 친구 저장하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        Map<String, List<String>> nearFriend = new HashMap<>();
+        List<String> realFriends = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (friend.get(0).equals(user)) {
+                realFriends.add(friend.get(1));
+                continue;
+            }
+            if (friend.get(1).equals(user)) {
+                realFriends.add(friend.get(0));
+                continue;
+            }
+            nearFriend.compute(friend.get(0), (key, value) -> value == null ? new ArrayList<>() : value).add(friend.get(1));
+        }
+
         return answer;
     }
 }
