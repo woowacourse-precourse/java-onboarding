af0bfd81081d946ac1eaf559ae8dcd87e325ddf3
Sangin <ksiisk99@gmail.com>
2022-10-31 23:03:11 +0900

    feat: problem5 - 입력받은 money에 해당하는 돈의 개수를 반환하는 solution 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,22 @@
 package onboarding;
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-
+    private static final int[] MONEY_VALUES={50000,10000,5000,1000,500,100,50,10,1};
+    private static final int MONEY_TYPE_SIZE=9;
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int withdrawalAmount=money;
+        for(int i=0;i<MONEY_TYPE_SIZE;i++){
+            answer.add(calculateNumberOfMoney(withdrawalAmount,MONEY_VALUES[i]));
+            withdrawalAmount=calculateRestOfMoney(withdrawalAmount,MONEY_VALUES[i]);
+        }
 
         return answer;
     }
