ee491b23992e36199e90d73bc0d62abd5d91e456
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:08:13 +0900

    feat: 왼쪽, 오른쪽 페이지 중 가장 큰 값

--- src/main/java/onboarding/problem1/Page.java
+++ src/main/java/onboarding/problem1/Page.java
@@ -1,9 +1,17 @@
 package onboarding.problem1;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Page {
+
+    public static int getMyMaxPage(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        return Math.max(findMaxPage(leftPage), findMaxPage(rightPage));
+    }
 
     private static int findMaxPage(int pageNumber) {
         int[] singleDigitArray =
