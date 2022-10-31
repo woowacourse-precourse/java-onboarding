5a2c38b2bd6b30a7ec07e7bdd7330d92230679e8
Be-student <presentsong@naver.com>
2022-10-31 18:16:42 +0900

    feat(Problem2): 중복된 값을 return하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import onboarding.problem2.Cipher;
+import onboarding.problem2.Decoder;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Cipher cipherText = new Cipher(cryptogram);
+        return new Decoder(cipherText).decode();
     }
 }
