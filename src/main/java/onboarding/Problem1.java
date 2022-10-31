4481ebcfca490350afd32d49ed68159de15d4319
lacram <chemin9898@gmail.com>
2022-10-31 23:04:18 +0900

    Problem 1 - 기능 목록 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+
+/*
+* 기능 목록
+* 1. 유효성 검증
+* 2. 펼친 페이지의 최종 점수 계산
+* 3. 페이지 번호 게임의 결과 도출
+* */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
