226f75d872d8b69eecc550cea7d11d0799bdb3e9
patronum98 <zkrhklkl98@gmail.com>
2022-11-01 02:00:24 +0900

    문제5 기능목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+//기능1. 지폐와 동전의 단위가 담긴 배열을 만든다
+//기능2. 가장 큰 지폐보다 금액이 크면 나누고 아니면 그 다음으로 큰 지폐나 동전으로 나눈다.
+//기능3. 몫은 answer 에 add 한다.
+//기능4. 기능1~3을 반복문으로 만든다.
+
 import java.util.Collections;
 import java.util.List;
 
