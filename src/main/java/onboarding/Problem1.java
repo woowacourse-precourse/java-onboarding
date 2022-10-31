3c4ca4051b137a9bca55a1a52c0b582da5abadd9
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-01 02:49:48 +0900

    chore(Problem1): add comments outlining list of features

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,28 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    // TODO: define appropriate names for results
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        // Instantiate Spread (per player)
+
+        // Get and return result
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    // TODO: int result(int pobi, int crong)
 }
+
+//TODO: class Spread
+    // TODO: Spread(List<Integer> player)
+
+    // TODO: int score()
+
+    // TODO: static class Page (static nested class)
+        // TODO: getScore()
+
+        // TODO: addDigits()
+
+        // TODO: multiplyDigits()
