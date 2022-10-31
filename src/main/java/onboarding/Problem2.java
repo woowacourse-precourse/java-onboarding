ff270c23bb41ed02455c03cb557b341d630be7b4
Fizioo0102 <a27313797@gmail.com>
2022-10-31 20:58:11 +0900

    feat: repeat_word_check()함수 작성
    boolean 배열을 활용하여 단어 내부에서 연속으로 중복되는 알파벳이 있을경우 해당되는 boolean배열을 true로 변환해 반환하는 함수 작성.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
@@ -8,6 +10,23 @@ public class Problem2 {
 
     //입력으로 주어지는 단어에 중복되는 구간값을 체크해주는 함수
 
+    static boolean[] repeat_word_check(String word) {
+
+        boolean[] check = new boolean[word.length()];
+        Arrays.fill(check, false);
+
+        for (int i = 0; i < word.length() - 1; i++) {
+            if (word.charAt(i) != word.charAt(i + 1)) continue;
+
+            if (word.charAt(i) == word.charAt(i + 1)) {
+                check[i] = true;
+                check[i + 1] = true;
+                i++;
+            }
+        }
+
+        return check;
+    }
 
     //입력으로 주어진 단어에 중복이 있는지 없는지 확인하는 함수
 
