b1a5c49d02d99a35f268f08c527c563751fbf8ed
aud19 <aud19980518@gmail.com>
2022-10-31 20:38:41 +0900

    1주차 3번 solution 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            answer += numMod(i);
+        }
+        return answer;
+    }
+
+    public static int numMod(int num) {
+        int answer = 0;
+        while(num > 0) {
+            int mod = num % 10;
+            if(mod != 0 && mod % 3 == 0) answer++;
+            num /= 10;
+        }
         return answer;
     }
 }
