578c49f4f3094b0a935e4878df870165f5c6e290
Aru428 <86353626+Aru428@users.noreply.github.com>
2022-10-31 22:08:41 +0900

    문제5 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] won = {50000,10000,5000,1000,500,100,50,10,1};
+
+        for(int i=0;i<9;i++){
+            answer.add(money /= won[i]);
+            money %= won[i];
+        }
+
         return answer;
     }
 }
