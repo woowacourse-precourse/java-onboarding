48d126727aecaa7021f9163f0e2126752f14c48d
wnsgml7267 <dlwns7267@naver.com>
2022-11-01 02:24:09 +0900

    [Problem5] 돈 변환 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    // 돈 변환
+    private static List<Integer> convert(int money) {
+        List<Integer> moneyCount =
+                new ArrayList<>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+        int current = 0;
+        int amountOfMoney;
+
+        while(current < moneyCount.size()) {
+            amountOfMoney = moneyCount.get(current);
+            moneyCount.set(current, money / amountOfMoney);
+            money %= amountOfMoney;
+            current++;
+        }
+        return moneyCount;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return convert(money);
     }
 }
