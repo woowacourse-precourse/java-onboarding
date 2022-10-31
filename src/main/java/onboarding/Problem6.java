cf77f86170963e86709a986668d5d8821041ec2b
Ridealist <cjsn92@gmail.com>
2022-10-31 20:44:32 +0900

    style/Problem6 : import 와일드카드 제거_Java 코딩 컨벤선

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,8 @@
 package onboarding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
