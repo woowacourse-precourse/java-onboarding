d327542270b364658f9ffcd8f83fd369b39b52aa
23Yong <1239824@naver.com>
2022-10-31 13:37:14 +0900

    [refactor] 돈을 세는 방법 수정
    - 기존에 있던 방식은 눈에 한 번에 들어오지 않는다.
    - 또한 메서드의 호출 순서에 따라 결과가 바뀌는 문제가 있고, 메서드 명이 비슷해 실수할 여지가 있어 이를 수정한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,67 +5,21 @@ import java.util.List;
 
 public class Problem5 {
 
-    private static int money;
-    private static List<Integer> answer;
+    private static final int[] moneyArr = new int[] { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
 
     public static List<Integer> solution(int money) {
-        Problem5.money = money;
-        answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
 
-        countFiftyThousand();
-        countTenThousand();
-        countFiveThousand();
-        countThousand();
-        countFiveHundred();
-        countHundred();
-        countFifty();
-        countTen();
-        countOne();
+        countMoneyGreedy(money, answer);
 
         return answer;
     }
 
-    private static void countFiftyThousand() {
-        answer.add(money / 50000);
-        money %= 50_000;
-    }
-
-    private static void countTenThousand() {
-        answer.add(money / 10000);
-        money %= 10_000;
-    }
-
-    private static void countFiveThousand() {
-        answer.add(money / 5000);
-        money %= 5_000;
-    }
-
-    private static void countThousand() {
-        answer.add(money / 1000);
-        money %= 1_000;
-    }
-
-    private static void countFiveHundred() {
-        answer.add(money / 500);
-        money %= 500;
-    }
-
-    private static void countHundred() {
-        answer.add(money / 100);
-        money %= 100;
-    }
-
-    private static void countFifty() {
-        answer.add(money / 50);
-        money %= 50;
-    }
-
-    private static void countTen() {
-        answer.add(money / 10);
-        money %= 10;
-    }
-
-    private static void countOne() {
-        answer.add(money);
+    private static void countMoneyGreedy(int money, List<Integer> answer) {
+        int idx = 0;
+        while (idx < moneyArr.length) {
+            answer.add(money / moneyArr[idx]);
+            money %= moneyArr[idx++];
+        }
     }
 }
