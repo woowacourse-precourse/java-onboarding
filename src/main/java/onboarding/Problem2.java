a55caa3fb788730917ac13e3a624042170b30d32
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:42:08 +0900

    [problem2]예외 처리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,15 +7,21 @@ import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidati
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        checkList(cryptogram);
 
-        for (int i = 0; i < cut.size(); i++) {
-            answer += cut.get(i);
-        }
-
-        return answer;
     }
 
-
+    public static void checkList(String cryptogram) throws IllegalArgumentException {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            throw new IllegalArgumentException("cryptogram은 길이가 1 미만 1000 초과인 문자열 입니다.");
+        }
+        if (cryptogram.toLowerCase() != cryptogram) {
+            throw new IllegalArgumentException("cryptogram은 알파벳 소문자로만 이루어져 있지 않습니다.");
+        }
+        if (cryptogram.replaceAll(" ", "") != cryptogram) {
+            throw new IllegalArgumentException("cryptogram에 공백이 있습니다.");
+        }
+    }
 }
 
 
