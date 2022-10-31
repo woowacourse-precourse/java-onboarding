bd827535a71983362ea6566b4f43bd6e303ead0f
ouohoon <begreat@kakao.com>
2022-10-31 20:47:19 +0900

    6번 문제 해결

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,66 +1,70 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem6 {
     /*
-    기능 목록
-    1. 닉네임을 2글자 단위로 해시맵에 저장해서 중복 체크용 해시맵을 만드는 기능
-    2. 닉네임 목록을 순회하며 중복된 닉네임을 가진 이메일 리스트를 만드는 기능
-    3. 이메일 리스트에서 중복을 제거하는 기능
-    4. 이메일 리스트를 정렬하는 기능
+    NicknameChecker 기능 목록
+    1. 닉네임을 2글자 단위로 검사해서 부분 닉네임 목록에 없다면 저장하고 있다면 중복 닉네임 목록에 추가하는 기능
+    2. 이메일 리스트를 오름차순 정렬하는 기능
      */
 
     private static final int EMAIL_INDEX = 0;
     private static final int NICKNAME_INDEX = 1;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
         NicknameChecker nicknameChecker = new NicknameChecker();
-        Map<String, Boolean> duplicatedNicknameList = new HashMap<>();
-        for (List<String> form : forms) {
-            String email = form.get(EMAIL_INDEX);
-            String nickname = form.get(NICKNAME_INDEX);
 
-            Boolean validNickname = nicknameChecker.addNicknameToCheckMap(nickname);
-            if (!validNickname) {
-                Boolean isExistEmail = duplicatedNicknameList.getOrDefault(email, false);
-                if (!isExistEmail) {
-                    duplicatedNicknameList.put(email, true);
-                }
-            }
-        }
+        nicknameChecker.check(forms);
 
-        return answer;
+        return nicknameChecker.getDuplicatedEmailList();
     }
 
     private static class NicknameChecker {
-        private Map<String, Boolean> checkMap;
+        private Map<String, String> subNicknames;
+        private Map<String, Boolean> duplicatedEmails;
 
         public NicknameChecker() {
-            checkMap = new HashMap<>();
+            subNicknames = new HashMap<>();
+            duplicatedEmails = new HashMap<>();
         }
 
-        public Boolean addNicknameToCheckMap(String nickname) {
-            if (nickname.length() < 2) {
-                return true;
+        public void check(List<List<String>> forms) {
+            for (List<String> form : forms) {
+                addSubNickname(form);
             }
-            Boolean result = true;
+        }
+
+        private void addSubNickname(List<String> form) {
+            String email = form.get(EMAIL_INDEX);
+            String nickname = form.get(NICKNAME_INDEX);
+            if (nickname.length() < 2) {
+                return;
+            }
+
             for (int i = 1; i < nickname.length(); i++) {
-                String partial = String.valueOf(nickname.charAt(i-1)) +
-                                String.valueOf(nickname.charAt(i));
-                Boolean isExist = checkMap.getOrDefault(partial, false);
-                if (isExist) {
-                    result = false;
+                String subNickname = nickname.substring(i - 1, i + 1);
+                String isExist = subNicknames.getOrDefault(subNickname, "");
+                if (isExist.equals("")) {
+                    subNicknames.put(subNickname, email);
                     continue;
                 }
-                checkMap.put(partial, true);
+                if (duplicatedEmails.getOrDefault(isExist, true)) {
+                    duplicatedEmails.put(isExist, false);
+                }
+                if (duplicatedEmails.getOrDefault(email, true)) {
+                    duplicatedEmails.put(email, false);
+                }
             }
-            return result;
         }
-        
-        
+
+        public List<String> getDuplicatedEmailList() {
+            Stream<String> stream = duplicatedEmails.keySet().stream().sorted();
+            return stream.collect(Collectors.toList());
+        }
     }
 }
