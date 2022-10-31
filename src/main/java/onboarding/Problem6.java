9771000c07295926d72da754e055502003c02c99
hyun-seung <ardimento22@naver.com>
2022-11-01 00:49:30 +0900

    [FEAT] 문제의 제한사항에 부합하는지 확인한다. (크루의 범위, 이메일 범위, 이메일 형식, 닉네임 한글 여부, 닉네임 범위)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,9 +13,16 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
+        if(!verificationCheckAboutCrews(forms)) {
+            return List.of("Check Restrictions");
+        }
+
         HashMap<String, Integer> twoLetterHashMap = new HashMap<>();
 
         for (List<String> form : forms) {
+            if(!verificationCheckAboutEmail(form) || !verificationCheckAboutNickname(form)) {
+                return List.of("Check Restrictions");
+            }
             twoLetterHashMap = putTwoLetters(form, twoLetterHashMap);
         }
 
@@ -23,6 +30,45 @@ public class Problem6 {
         answer = sortASC(answer);
 
         return answer;
+    }
+
+    public static boolean verificationCheckAboutCrews(List<List<String>> forms) {
+        int crews = forms.size();
+        if (crews < 1 || crews > 10000) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean verificationCheckAboutEmail(List<String> form) {
+        String email = form.get(EMAIL);
+        String emailRegex = "^.*@email.com$";
+        int emailLength = email.length();
+
+        if(emailLength < 11 || emailLength >= 20) {
+            return false;
+        }
+
+        if(!email.matches(emailRegex)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean verificationCheckAboutNickname(List<String> form) {
+        String nickname = form.get(NICKNAME);
+        String nicknameRegex = "^[가-힣]*$";
+        int nicknameLength = nickname.length();
+
+        if(nicknameLength < 1 || nicknameLength >= 20) {
+            return false;
+        }
+
+        if(!nickname.matches(nicknameRegex)) {
+            return false;
+        }
+
+        return true;
     }
 
     public static HashMap<String, Integer> putTwoLetters(List<String> form, HashMap<String, Integer> twoLetterHashMap) {
