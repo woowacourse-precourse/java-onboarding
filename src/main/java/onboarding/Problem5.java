00d58e9dde0acbf54b36a7caa80f9da5fe12e890
소대훈 <theogns0681@naver.com>
2022-10-31 18:21:53 +0900

    [feat] (문제5) 큰금액부터 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,11 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int[] coins = {50000,10000,5000,1000,500,100,50,10,1};
+        for (int coin : coins) {
+            int cnt = money / coin;
+            answer.add(cnt);
+            money %= coin;
+        }
         return answer;
     }
 }
