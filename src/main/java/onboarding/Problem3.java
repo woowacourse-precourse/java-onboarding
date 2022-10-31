591fbe6075aa88b5f1c7979f779792b90abd73bd
DongWooE <ehddn977@naver.com>
2022-11-01 04:26:26 +0900

    [p3][add] 3,6,9를 포함하는 개수를 세서 리턴하는 메소드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,15 +4,24 @@ public class Problem3 {
 
     int count = 0;
     public static int solution(int number) {
-        int answer = 0;
         Problem3 p3 = new Problem3();
         p3.iterate(number);
-        return answer;
+        return p3.count;
     }
 
     private void iterate(int number) {
         for(int i =1; i <= number; i++){
-            count++;
+            count += countContain(i);
         }
+    }
+
+    private int countContain(int n) {
+        int cnt = 0;
+        while (n >= 1) {
+            int m = n % 10;
+            if(m !=0 && m%3 == 0) cnt++;
+            n /= 10;
+        }
+        return cnt;
     }
 }
