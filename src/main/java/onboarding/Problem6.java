83e605b36636e174a5150bbe97933aeaabab875e
JaeUpSu <hyeongs2323@gmail.com>
2022-10-31 18:34:59 +0900

    PROBLEM6 | 중복 key 를 가진 nick 확인 후 해당 이메일 리스트 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem6 {
@@ -9,9 +11,66 @@ public class Problem6 {
 
         User[] crew = inItUsers(forms);
         HashMap<String, Integer> crewNickMap = getCandidateNicks(crew);
+        answer = checkUsers(crew, crewNickMap);
 
 
         return answer;
+    }
+    private static HashMap<String, Integer> getDuplicateNicks(User[] crews, HashMap<String, Integer> crewNickMap) {
+
+        int crewSize = crews.length;
+        int nickSize = crewNickMap.size();
+        List<String> keys = new ArrayList<>(crewNickMap.keySet());
+
+        for (int i = 0; i < crewSize; i++) {
+            String nick = crews[i].getNick();
+
+            for (int j = 0; j < nickSize; j++) {
+                String key = keys.get(j);
+                if (nick.contains(key)) {
+                    int count = crewNickMap.get(key) + 1;
+                    crewNickMap.replace(key, count);
+                }
+            }
+        }
+        return crewNickMap;
+    }
+    private static List<String> getEmails(User[] crews, HashMap<String, Integer> crewNickMap) {
+
+        int crewSize = crews.length;
+        List<String> emails = new ArrayList<>();
+        List<String> keys = new LinkedList<>(crewNickMap.keySet());
+
+        int keySize = keys.size();
+
+        for (int i = 0; i < keySize; i++) {
+            String key = keys.get(i);
+
+            for (int j = 0; j < crewSize; j++) {
+                String nick = crews[j].getNick();
+
+                if (nick.contains(key) && crewNickMap.get(key) > 1) {
+                    String email = crews[j].getEmail();
+
+                    if (emails.contains(email)) continue;
+
+                    emails.add(email);
+                }
+            }
+        }
+
+        return emails;
+    }
+
+    private static List<String> checkUsers(User[] crews, HashMap<String, Integer> crewNickMap) {
+
+        List<String> checkedEmails;
+
+        crewNickMap = getDuplicateNicks(crews, crewNickMap);
+
+        checkedEmails = getEmails(crews, crewNickMap);
+
+        return checkedEmails;
     }
     private static HashMap<String, Integer> getCandidateNicks(User[] crews) {
 
