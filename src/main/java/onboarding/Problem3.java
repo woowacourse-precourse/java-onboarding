8d3428dcc668f493e3a73908c104a6a4785b4f0b
Cho-El <syzzang21c@gmail.com>
2022-11-01 02:30:05 +0900

    Integer Int 오류 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class Problem3 {
     // 해당 숫자 3,6,9갯수 세기
     static int Count369(int num){
-        int[] arr = {3,6,9};
+        Integer[] arr = {3,6,9};
         int count = 0;
         while(num > 0){
-            if (List.of(arr).contains(num % 10)){
+            int mod = num % 10;
+            if (Arrays.asList(arr).contains(mod)){
                 count++;
-                num /= 10;
             }
+            num /= 10;
         }
         return count;
     }
