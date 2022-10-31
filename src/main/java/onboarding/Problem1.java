b157321e7b235c940ec75072323160fcfa9682cf
yeonji <view7186@naver.com>
2022-10-31 19:01:03 +0900

    Solved

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,21 +3,24 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int bignum(int page) {
+    public static int getBigNumberOf(int page) {
         int sum = 0, mux = 1, tmp;
         while(page > 0) {
             tmp = page % 10;
             sum += tmp;
             mux *= tmp;
+            page /= 10;
         }
-        return sum > mux? sum : mux;
+        return Math.max(sum, mux);
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int ponum = Math.max(bignum(pobi.get(0)), bignum(pobi.get(1))), crnum = Math.max(bignum(pobi.get(0)), bignum(pobi.get(1)));
-        if(ponum > crnum) return 1;
-        if(crnum > ponum) return 2;
-        if(crnum == ponum) return 0;
-        return -1;
+        if((pobi.get(0)&1) != 1 || (crong.get(0)&1) != 1 || (pobi.get(1)&1) != 0 || (crong.get(1)&1) != 0 ||
+                pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) return -1;
+        int pobiNumber = Math.max(getBigNumberOf(pobi.get(0)), getBigNumberOf(pobi.get(1)));
+        int crongNumber = Math.max(getBigNumberOf(crong.get(0)), getBigNumberOf(crong.get(1)));
+        if(pobiNumber > crongNumber) return 1;
+        if(crongNumber > pobiNumber) return 2;
+        return 0;
     }
 }
