784d6f0a4baae51ad2057379915cd21b76c1649f
K-Diger <kitten.diger@gmail.com>
2022-10-31 21:34:09 +0900

    docs : Problem7 기능 목록 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,16 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+/*
+
+[기능 목록]
+1. 친구 추천 점수를 기록하기 위해 Map<> 타입의 변수를 초기화 하기
+2. 현재 유저와 친구인 사람의 이름을 리스트로 반환하기
+3. 내 타임라인 방문자들의 친구 추천 점수를 올리기 (단, 2번 메서드를 활용하여 이미 친구인 사람이 방문했을 경우는 점수를 올리지 않는다.)
+4. 나와 함께 아는 친구를 바탕으로 추천 스코어 반영하기
+5. Map 형의 친구 추천 스코어를 Value 를 기준으로 하여 정렬한 후, 리스트로 변환하기 (단, value 가 0인 Key는 변환하지 않는다.)
+
+*/
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
