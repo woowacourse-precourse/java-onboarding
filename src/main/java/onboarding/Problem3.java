ae82cc8d90f88bcc2b8b61edb9176b928f140cb4
tkdtn4657 <tkdtn101226@gmail.com>
2022-10-31 21:25:42 +0900

    problem3 제한사항 입력 및 tsn_game 클래스 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+class tsn_game{
+	
+	tsn_game(){
+	}
+	
+	tsn_game(int n){
+		
+	}
+	
+}
+
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int answer = 0;        
+        
+        if(number >= 1 && number <= 10000) {
+        	return answer;
+        }
+        return 0;
     }
 }
