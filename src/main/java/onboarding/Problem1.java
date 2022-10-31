71276a623159cb5b8e63e0f85a1d7a67a8c9177b
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:32:02 +0900

    [problem1]전체 코드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -21,7 +21,31 @@ class Problem1 {
         }
 
 
+        int pobimax=0;
+        int crongmax=0;
+
+
+
+        pobimax = Math.max(Maxnum(pobi.get(0)), Maxnum(pobi.get(1)));
+        crongmax = Math.max(Maxnum(crong.get(0)), Maxnum(crong.get(1)));
+
+        if(pobimax > crongmax)
+            return 1;
+        else if (pobimax == crongmax)
+            return 0;
+        else
+            return 2;
+
     }
 
 
+    public static int Maxnum(int num) {
+        int sum = 0, mul = 1;
+        while(num != 0) {
+            sum += num % 10;
+            mul *= num % 10;
+            num /= 10;
+        }
+        return Math.max(sum, mul);
+    }
 }
