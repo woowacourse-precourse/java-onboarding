29bc6f94932524089dfff28c44ffb89ac7f82538
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 01:40:59 +0900

    feat: problem6 이메일,이름 리스트로 추출하는 메서드 valueExtraction 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<String> emailList = valueExtraction(forms,0);
+        List<String> nameList = valueExtraction(forms,1);
+
         return answer;
+    }
+    public static List<String> valueExtraction(List<List<String>> forms,int index){
+        List<String> responseList = new ArrayList<>();
+        for(int i=0;i<forms.size();i++){
+            responseList.add(forms.get(i).get(index));
+        }
+        return responseList;
     }
 }
