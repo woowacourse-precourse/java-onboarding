a035dae1a67ec41d2ed5cdc190e4ee81546e605d
Your Name <dbsdbxkrzz@naver.com>
2022-10-31 13:16:14 +0900

    feat: Problem6 기본 구조를 작성합니다.
    기능 목록과 세부 구현 목록에 맞추어 Problem6의 기본 구조를 작성합니다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+
+	public static boolean[] duplicateUser;
+
+	public static List<String> solution(List<List<String>> forms) {
+		duplicateUser = new boolean[forms.size()];
+		List<String> answer = new ArrayList<>();
+
+		for (int i = 0; i < forms.size() - 1; i++) {
+			List<String> form = forms.get(i);
+			checkDuplicate(forms, form, i);
+		}
+
+		return answer;
+	}
+
+	public static void checkDuplicate(List<List<String>> forms, List<String> form, int index) {
+        
+	}
+
+	public static void isDuplicateNickName(String formsNickname, String formNickname) {
+
+	}
+
+	public static void addDuplicateUser(List<String> answer) {
+
+	}
 }
