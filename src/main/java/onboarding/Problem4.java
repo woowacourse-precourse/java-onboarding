01b65ec72fd45079ab637866d818b25265a00e3a
patronum98 <zkrhklkl98@gmail.com>
2022-10-31 22:18:35 +0900

    문제4 기능목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+//기능1. 대문자의 위치를 파악한다
+//기능1-1. 리스트배열로 변환하여 각 자리에 문자를 넣은뒤 대문자가 있는 곳을 확인한다.
+//기능1-2. 모두 소문자로 바꾼다.
+//기능2. 알파벳을 거꾸로 출력한다
+//기능3. 거구로 출력한 문장에 원래 문장의 대문자 위치에 대문자로 바꾸어준다.
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
