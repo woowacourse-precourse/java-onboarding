e01697f4430da1ecdc6e7904740226700301f534
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-10-31 23:08:05 +0900

    solve Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> list = new ArrayList<>();
+        int[] coin = { 50000, 10000, 5000, 1000, 500, 100 , 50, 10, 1};
+        for(int i=0; i<coin.length; i++){
+            list.add(i, money/coin[i]);
+            money %= coin[i];
+        }
+        return list;
     }
 }
+
+/*
+[ 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 ]
+ */
