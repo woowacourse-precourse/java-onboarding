ba4592996ab307952e3696689d3c50f6043f6071
woosung1223 <dntjd991223@naver.com>
2022-10-31 15:19:57 +0900

    refactor(Problem7): weight, uniqueIDs -> weight로 축소

--- src/main/java/onboarding/problem7/Controller.java
+++ src/main/java/onboarding/problem7/Controller.java
@@ -5,28 +5,24 @@ import java.util.List;
 public class Controller {
 
     public static List<String> getSolution(String user, List<List<String>> friends, List<String> visitors) {
-
         Relations relation = new Relations(friends);
         Visitors visitor = new Visitors(visitors);
         init(relation, visitor);
-
         List<String> myFriends = relation.getFriendsList(user);
-
         for (int i = 0; i < myFriends.size(); i++) {
             List<String> acquaintance = relation.getFriendsList(myFriends.get(i));
             Friend.addWeight(acquaintance, Constant.FRIEND_WEIGHT);
         }
         Friend.addWeight(visitors, Constant.VISITOR_WEIGHT);
-
         List<String> recommendedList = Friend.recommendedList(user);
         recommendedList.removeAll(myFriends);
         return recommendedList;
     }
+
     private static void init(Relations relation, Visitors visitor) {
         List<String> distinctFriends = relation.getDistinctFriends();
         List<String> distinctVisitors = visitor.getDistinctVisitors();
         Friend.initID(distinctFriends);
         Friend.initID(distinctVisitors);
-        Friend.initWeight();
     }
 }
--- src/main/java/onboarding/problem7/Friend.java
+++ src/main/java/onboarding/problem7/Friend.java
@@ -3,41 +3,33 @@ package onboarding.problem7;
 import java.util.*;
 
 public class Friend {
-    private static HashMap<Integer, Integer> weight = new HashMap<>();
-    private static HashMap<String, Integer> uniqueIDs = new HashMap<>();
-    private static int forID = 0;
-    
+    private static HashMap<String, Integer> weight = new HashMap<>();
+
     public static void initID(List<String> people) {
         for (int i = 0; i < people.size(); i++) {
             String name = people.get(i);
-            if (!uniqueIDs.containsKey(name)) {
-                uniqueIDs.put(name, forID++);
+            if (!weight.containsKey(name)) {
+                weight.put(name, 0);
             }
-        }
-    }
-    public static void initWeight() {
-        for (int i = 0; i < uniqueIDs.size(); i++) {
-            weight.put(i, 0);
         }
     }
 
     public static void addWeight(List<String> list, int WEIGHT) {
         for (int i = 0; i < list.size(); i++) {
             String user = list.get(i);
-            int userID = uniqueIDs.get(user);
-            int updatedValue = weight.get(userID) + WEIGHT;
-            weight.put(userID, updatedValue);
+            int beforeWeight = weight.get(user);
+            int updatedValue = beforeWeight + WEIGHT;
+            weight.put(user, updatedValue);
         }
     }
+
     public static List<String> recommendedList(String user) {
-        int myID = uniqueIDs.get(user); // 결과에서 본인은 제외
-        List<Integer> IDList = new ArrayList<>(weight.values());
-        List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(weight.entrySet());
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(weight.entrySet());
         entryList.sort(Map.Entry.comparingByValue());
         List<String> resultList = new ArrayList<>();
         int count = 0;
         for (int i = entryList.size() - 1; i >= 0; i--) {
-            if (entryList.get(i).getKey() == myID) {
+            if (entryList.get(i).getKey() == user) {
                 count++;
                 continue;
             }
@@ -45,7 +37,7 @@ public class Friend {
                 break;
             }
             if (entryList.get(i).getValue() != 0) {
-                String name = Util.getKeyFromValue(uniqueIDs, entryList.get(i).getKey());
+                String name = entryList.get(i).getKey();
                 resultList.add(name);
             }
             count++;
