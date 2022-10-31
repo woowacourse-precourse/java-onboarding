a67c9bf04ea8a6d9cee19039549063943dad0b5a
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-01 01:48:18 +0900

    [PROBLEM 1] 예외사항 추가
    1. 1~400 범위 아닌 경우
    2. 두 페이지 차가 1이 아닌 경우
    3. 왼쪽 페이지가 홀수가 아닌 경우
    4. 페이지 수가 2가 아닌 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if(exception(pobi) || exception(crong)) return -1;
+
+        return answer;
+    }
+
+    static boolean exception(List<Integer> list) {
+        boolean answer = false;
+
+        if(list.get(0) < 1 || list.get(1) > 400) answer = true;
+        if(list.get(1) - list.get(0) != 1) answer = true;
+        if(list.get(0) % 2 == 0) answer = true;
+        if(list.size() != 2) answer = true;
+
         return answer;
     }
 }
