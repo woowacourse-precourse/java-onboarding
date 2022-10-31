d463663545f1737e8ea4a0e05f2ce5b09df69ad5
Sehoon Kim <shggm2000@gmail.com>
2022-10-31 23:27:47 +0900

    현재 가지고 있는 돈을 지폐의 종류에 따라 나눠 변환되는 지폐의 개수를 구하는 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
+
         List<Integer> answer = Collections.emptyList();
+        answer = new ArrayList<Integer>();
+
         return answer;
+    }
+
+    public static List<Integer> moneyCount(int money, int number, List<Integer> answer){
+
+        int count = 0;
+        count = money / number;
+        answer.add(count);
+
+        return answer;
+
     }
 }
