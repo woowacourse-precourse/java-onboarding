47365b7c590a92f5d38514283e231225ad42b61f
KimSoo <ilppang@naver.com>
2022-10-31 17:18:10 +0900

    Add(김현수/Problem5): 5만원부터 1원까지 배열 unit 생성

--- src/main/java/onboarding/Problem-Solution.md
+++ src/main/java/onboarding/Problem-Solution.md
@@ -3,18 +3,20 @@ Problem 1
 2. 페이지 번호 곱하는 기능
 3. 입력값의 최대값 구하는 기능
 4. 상황에 따른 예외처리
-5. 결과값 
+
 
 Problem2
 
 Problem3
 1. 전달 받은 number의 값을 10으로 나눈 나머지를 통해 count++ 
 2. number전달 기능 
-3. 결과값
+
 
 Problem4
 
 Problem5
+1. 5만원 부터 1원까지 배열 unit 생성
+2. 입력받은 money를 unit과 비교 기능
 
 Problem6
 
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static final List<Integer> unit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1);
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         return answer;
     }
 }
