9ad26752090c9901ebe349e124a6dd1aaa3d0650
wodnjs7800 <wodnjs7800@naver.com>
2022-10-31 16:59:54 +0900

    problem5

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0;i<word.length();i++){
+            int chr=(int)word.charAt(i);
+            if(chr>=65 && chr<=90){
+                chr-=65;
+                chr=90-chr;
+            }
+            else if(chr>=97 && chr<=122){
+                chr-=97;
+                chr=122-chr;
+            }
+            answer+=(char)chr;
+        }
         return answer;
     }
 }
