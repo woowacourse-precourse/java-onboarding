3428881d768c7820d44e3f449ddda48040905e0f
DongWooE <ehddn977@naver.com>
2022-11-01 04:54:04 +0900

    [p5][add] 금액을 5단위로 나누는 divideFiveUnit  메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,20 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
     int money;
-    List<Integer> answer;
+    List<Integer> answer =new ArrayList<>();
     public static List<Integer> solution(int money) {
-        money = money;
         Problem5 p5 = new Problem5();
-        return null;
+        p5.money = money;
+        p5.iterate();
+        return p5.answer;
     }
 
     private void iterate() {
+        int fiveUnit = 50000;
+        int oneUnit = 10000;
+        System.out.println(money);
         while(money > 0){
+            divideFiveUnit(fiveUnit);
+            if(fiveUnit > 5) fiveUnit /= 10;
         }
+    }
+
+    private void divideFiveUnit(int unit){
+        if(money < unit) return;
+        int count = 0;
+        count += money/unit;
+        money -= unit;
+        answer.add(count);
     }
 }
