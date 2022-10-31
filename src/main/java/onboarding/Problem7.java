c741a7756fb59e0ded30e47ee151ae55d52b44d9
Young37 <20171470@g.dongseo.ac.kr>
2022-10-31 21:11:15 +0900

    친구관계 맵 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+    	List<String> answerList = new ArrayList<String>();
+    	Map<String,Set<String>> friendMap = new HashMap<String,Set<String>>();
+    	Map<String,Integer> candidatesMap = new HashMap<String,Integer>();
+    	Map<Integer,List<String>> reverseCandidatesMap = new HashMap<Integer,List<String>>();
+    	
+        for(List<String> friendShip: friends) {
+        	for(int i = 0; i<2; i++) {
+        		if(friendMap.containsKey(friendShip.get(i))) {
+    				Set<String> friendSet = friendMap.get(friendShip.get(i));
+    				friendSet.add(friendShip.get(i^1));
+	        	}else {
+    				Set<String> set = new HashSet<String>();
+    				set.add(friendShip.get(i^1));
+    				friendMap.put(friendShip.get(i), set);
+	        	}
+        		candidatesMap.put(friendShip.get(i),0);
+        	}
+        }
+        return answerList;
     }
 }
