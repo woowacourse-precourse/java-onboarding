fd550ef40a365374dd78cdb049472bee0fae7649
anneu <b.imnuj@gmail.com>
2022-10-31 14:59:45 +0900

    feat(problem3): 1부터 주어진 숫자를 하나의 문자열에 더하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= number; i++) {
+            sb.append(i);
+        }
+
         return answer;
     }
 }
