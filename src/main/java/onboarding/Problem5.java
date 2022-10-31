1ad558cd7480ea8fc525e02e192c26a2e37be767
swonny <wonny921@gmail.com>
2022-10-31 17:59:26 +0900

    feat: 화폐 단위 찾기 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,12 +8,28 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        List<Integer> count = List.of(0, 0, 0, 0, 0, 0, 0, 0, 0);
 
+        /* 화폐 단위별 개수 리스트 초기화 */
+        List<Integer> cnt = new ArrayList<>(9);
+        for(int i = 0; i < 9; i++) {
+            cnt.add(0);
+        }
+
+        for(int i = 0; i < unit.length; i++) {
+            if(money <= 0) {
+                break;
+            }
+            int quotient = money / unit[i];
+            if(quotient > 0) {
+                money %= unit[i];
+                cnt.set(i, quotient);
+            }
+        }
+        answer = cnt;
         return answer;
     }
 
     public static void main(String[] args) {
-        solution(50000);
+        solution(15000);
     }
 }
