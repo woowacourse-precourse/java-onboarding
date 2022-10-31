21b5e75aa6a4850723c8ff88ce4b737878c086a2
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-31 13:24:14 +0900

    Problem6(리팩토링): 같은 정보를 분해하는 중복로직 제거, String키를 hashCode키로 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -14,17 +14,12 @@ public class Problem6 {
      * @return 같은 글자가 연속적으로 포함된 지원자들의 정렬된 이메일 목록
      */
     private static List<String> getDuplicatedCrewEmails(List<List<String>> forms){
-        // 제출 폼 중 닉네임만 추출한 리스트를 만듦
-        List<String> nicknameList = new ArrayList<>();
-        for(List<String> form : forms){
-            nicknameList.add(form.get(1));
-        }
 
         // 닉네임 리스트에서 같은 글자가 연속적으로 포함된 인덱스로 이메일 리스트를 만듦
-        Set<Integer> duplicatedIndexSet = getDuplicatedIndexSet(nicknameList);
+        Set<String> duplicatedEmailSet = getDuplicatedEmailSet(forms);
         List<String> emailList = new ArrayList<>();
-        duplicatedIndexSet.forEach((index)->{
-            emailList.add(forms.get(index).get(0));
+        duplicatedEmailSet.forEach((email)->{
+            emailList.add(email);
         });
 
         // 이메일 리스트를 정렬
@@ -35,22 +30,21 @@ public class Problem6 {
 
     /**
      * 이름 목록 중 같은 글자가 연속적으로 포함된 이름의 인덱스 집합을 반환
-     * @param nicknameList : 같은 글자가 연속적으로 포함되어있는지 확인할 닉네임 목록
+     * @param forms : 같은 글자가 연속적으로 포함되어있는지 확인할 폼 목록
      * @return 같은 글자가 연속적으로 포함된 이름의 인덱스 집합
      */
-    private static Set<Integer> getDuplicatedIndexSet(List<String> nicknameList){
+    private static Set<String> getDuplicatedEmailSet(List<List<String>> forms){
         // 연속글자가 포함된 닉네임들의 인덱스 집합
-        Set<Integer> result = new HashSet<>();
-        // 2개의 문자(charSet)를 키로 갖고, 해당 문자열을 갖는 닉네임의 인덱스 집합을 value로 갖는 hashMap
-        Map<String, Set<Integer>> charSetCountMap = new HashMap<>();
-        // 닉네임 별로 charSet을 분석하여 hashMap에 할당
-        int index = 0;
-        for(String nickname : nicknameList){
-            putCharSet(index, nickname, charSetCountMap);
-            index ++;
+        Set<String> result = new HashSet<>();
+        // 2개의 문자(charSet)를 키로 갖고, 해당 문자열을 갖는 닉네임의 이메일 집합을 value로 갖는 hashMap
+        // "key: 닉네임이 가진 2글자문자(charSet)의 해시코드"="value: 해당 charSet을 가진 email"
+        Map<Integer, Set<String>> charSetEmailMap = new HashMap<>();
+        // CharSetEmail map에 데이터를 담아줌
+        for(List<String> form : forms){
+            putCharSet(form, charSetEmailMap);
         }
-        // hashMap에서 2개의 문자가 여러 인덱스에서 발견될 경우 결과 집합에 추가
-        charSetCountMap.forEach((strKey, setValue) -> {
+        // hashMap에서 2개의 문자가 여러 이메일에서 발견될 경우 결과 집합에 추가
+        charSetEmailMap.forEach((strKey, setValue) -> {
             if(setValue.size() >= 2){
                 result.addAll(setValue);
             }
@@ -61,22 +55,28 @@ public class Problem6 {
 
     /**
      * 주어진 닉네임을 2글자씩 분해하여 해당 문자열을 갖는 닉네임의 인덱스 집합을 charSetCountMap에 할당
-     * @param index : 주어진 닉네임의 인덱스
-     * @param nickname : 주어진 닉네임
-     * @param charSetCountMap : "닉네임이 가진 2글자문자(charSet)":"인덱스"를 할당할 Map
+     * @param form : 이메일과 닉네임 정보가 담긴 리스트
+     * @param charSetEmailMap : "닉네임이 가진 2글자문자(charSet)의 해시코드":"해당 charSet을 가진 email"를 할당할 Map
      */
-    private static void putCharSet(int index, String nickname, Map<String, Set<Integer>> charSetCountMap){
-        // 2개의 문자별로 charSet을 만들어 charSetCountMap에 할당
+    private static void putCharSet(List<String> form, Map<Integer, Set<String>> charSetEmailMap){
+        // form을 분해 할당
+        String nickname = form.get(1);
+        String email = form.get(0);
+        // 2개의 문자별로 charSet을 만들어 charSetEmailMap에 할당
         for(int node = 0; node < nickname.length() -1; node++){
+            // 문자를 2개씩 쪼개어 charSet생성
             String charSet = nickname.substring(node, node+2);
-            if(charSetCountMap.containsKey(charSet)){
-                // 이미 검사해본 charSet일 경우 기존 charSet에 인덱스를 추가함
-                charSetCountMap.get(charSet).add(index);
+            //검색 속도 향상을 위해 hashCode를 키로 갖도록 함
+            int charSetHashCode = charSet.hashCode();
+
+            if(charSetEmailMap.containsKey(charSetHashCode)){
+                // 이미 검사해본 charSet일 경우 기존 charSet에 이메일 추가함
+                charSetEmailMap.get(charSetHashCode).add(email);
             } else {
                 // 새로운 charSet일 경우 새로운 set을 만들고 인덱스를 추가함
-                Set<Integer> indexSet = new HashSet<>();
-                indexSet.add(index);
-                charSetCountMap.put(charSet,indexSet);
+                Set<String> emailSet = new HashSet<>();
+                emailSet.add(email);
+                charSetEmailMap.put(charSetHashCode,emailSet);
             }
         }
     }
