2a924ab64a44f4bdf7c5ab3fae1b59eaa3fb77ee
renechoi90@gmail.com <renechoi90@gmail.com>
2022-10-31 19:40:35 +0900

    Refactor : rearrange methods function and rename those

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,39 +10,44 @@ public class Problem2 {
 	public static String solution(String cryptogram) {
 
 		do {
-			cryptogram = getFixedCryptogram(cryptogram);
+			cryptogram = rearrangeString(setPatternMatcher(cryptogram));
 		} while (doesDuplicationExist(cryptogram));
 
 		return cryptogram;
 	}
 
-	private static String getFixedCryptogram(String cryptogram) {
-		Matcher patternMatcher = setMatcher(cryptogram);
-		cryptogram = removeDuplication(patternMatcher);
-		return cryptogram;
-	}
 
 	private static boolean doesDuplicationExist(String cryptogram) {
-		Matcher patternMatcher = setMatcher(cryptogram);
+		Matcher patternMatcher = setPatternMatcher(cryptogram);
 		return patternMatcher.find();
 	}
 
-	private static Matcher setMatcher(String cryptogram) {
+	private static Matcher setPatternMatcher(String cryptogram) {
+		return setPattern(cryptogram);
+	}
+
+	private static Matcher setPattern(String cryptogram) {
 		Pattern patternRegex = Pattern.compile(regex);
 		return patternRegex.matcher(cryptogram);
 	}
 
-	private static String removeDuplication(Matcher patternMatcher) {
+	private static String rearrangeString(Matcher patternMatcher) {
 		String resultString;
 		StringBuilder stringBuilder = new StringBuilder();
 
-		while (patternMatcher.find()) {
-			patternMatcher.appendReplacement(stringBuilder, "");
-		}
+		removeString(patternMatcher, stringBuilder);
 
 		patternMatcher.appendTail(stringBuilder);
 		resultString = String.valueOf(stringBuilder);
 
 		return resultString;
+	}
+
+	private static void removeString(Matcher patternMatcher, StringBuilder stringBuilder) {
+
+		while (patternMatcher.find()) {
+			patternMatcher.appendReplacement(stringBuilder, "");
+		}
+
 	}
 }
