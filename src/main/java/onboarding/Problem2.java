1cab712198275db5e9a0d8581c7fb05c12079d1d
doehy <dbehxks02@naver.com>
2022-10-31 21:34:32 +0900

    feat : 스택구조에 있는 결과값을 문자열로 변환시키는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,8 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         Stack<Character> duplication_stack = changeToStack(cryptogram);
         Stack<Character> removed_stack = removeDuplicate(duplication_stack);
-        String answer = "answer";
+        String answer = makeToString(removed_stack);
+
         return answer;
     }
     static Stack<Character> removeDuplicate(Stack<Character> duplication_stack) {
@@ -24,5 +25,13 @@ public class Problem2 {
             temp_stack.push(cryptogram.charAt(i));
         }
         return temp_stack;
+    }
+    static String makeToString(Stack<Character> removed_stack) {
+        String result_string = "";
+        while(!removed_stack.isEmpty()){
+            char ch = removed_stack.pop();
+            result_string += ch;
+        }
+        return result_string;
     }
 }
