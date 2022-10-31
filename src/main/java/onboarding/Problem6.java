1c634210cb412b96a89561861470c1df5a92775b
yesjuhee <juhee0924@g.skku.edu>
2022-11-01 00:28:28 +0900

    [PROBLEM6] 두 닉네임이 주어졌을 때 중복되는 글자가 들어있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,13 +7,24 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> emails = new ArrayList<>();
 
-        for(int i=0; i<forms.size()-1;i++){
-            for(int j=i+1; i<forms.size();j++){
-                // forms[i][1]과 forms[j][1]을 비교해서 같다면
+        for(int i=0; i<forms.size()-1; i++){
+            for(int j=i+1; j<forms.size();j++){
+                //if (isOverlap(forms.get(i).get(1), forms.get(j).get(1));
                 // emails에 form[i][0]과 form[j][0]을 입력한다
             }
         }
 
         return emails;
+    }
+
+    public static boolean isOverlap(String name1, String name2){
+        for(int i=0; i<name1.length()-1;i++){
+            for(int j=0; j<name2.length()-1;j++){
+                if(name1.charAt(i)==name2.charAt(j)){
+                    if(name1.charAt(i+1)==name2.charAt(j+1)) return true;
+                }
+            }
+        }
+        return false;
     }
 }
