052e3c7c1188f762e66847b7739f8684e7f06caf
yubin <dbqls2821@gmail.com>
2022-10-31 22:02:48 +0900

    FEAT: [Problem4] 개구리어로 변환하는 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            answer.append(convertToFrogLanguage(c));
+        }
+        return answer.toString();
+    }
+
+    private static char convertToFrogLanguage(char c) {
+        if (Character.isLowerCase(c)) {
+            return (char) ('z' - c + 'a');
+        } else if (Character.isUpperCase(c)) {
+            return (char) ('Z' - c + 'A');
+        } else return c;
     }
 }
+
