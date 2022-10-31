9b2dd747091bf3d05511229e1c3e0d89460cbdbd
“rotomoo” <quddlf410@naver.com>
2022-10-31 18:21:47 +0900

    feat : implementation complete of Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        int[] moneyArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < moneyArr.length; i++) {
+            if (money==0) {
+                answer.add(0);
+                continue;
+            }
+            answer.add(money / moneyArr[i]);
+            money %= moneyArr[i];
+        }
+
         return answer;
     }
 }
