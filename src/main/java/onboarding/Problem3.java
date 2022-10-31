bb810b4117cb3c5b26959177e48415fc7b934ccc
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-10-31 21:15:50 +0900

    feat(Problem3): 주어진 숫자에 3, 6, 9가 포함되어 있는지 확인하는 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem3 {
+    public static boolean isClap(int number) {
+        if (number == 3 || number == 6 || number == 9) return true;
+        else return false;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
