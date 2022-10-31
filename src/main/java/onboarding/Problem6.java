9b57172509e32a14f4707e3305066964023b81fe
saehim <saeilee.vxv@gmail.com>
2022-11-01 00:11:23 +0900

    Feat(Problem6): 닉네임 중복 체크 후 처리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,13 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+    private static void checkNameMap(String nameSlice, String email) {
+        if (nameMap.containsKey(nameSlice)) {       // 이름 2글자가 중복된다면,
+            emails.add(nameMap.get(nameSlice));         // answer 배열에 중복되는 이름 있는 메일 추가
+            emails.add(email);                           // 체크하는 현재 메일도 추가
+        }
+        if (!nameMap.containsKey(nameSlice)) {      // 중복 이름이 없다면
+            nameMap.put(nameSlice, email);              // hashmap에 새로운 key 저장
+        }
+    }
 }
