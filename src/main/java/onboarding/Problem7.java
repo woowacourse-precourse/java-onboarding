1d89dea5d801ac8aa59630f74a1cca94d811bb41
wonseokjee <wsji9404@gmail.com>
2022-10-31 12:41:49 +0900

    221031 problem7-1 HasdMap으로 친구관계 구성. 좀 더 간략화 가능 할듯

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,21 +1,48 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        friendMap(friends);
         return answer;
     }
 
-    public void friendMap(List<List<String>> friends) {
-
+    //  1. HashMap으로 친구 당 가지는 모든 친구관계를 구성
+    public static void friendMap(List<List<String>> friends) {
+        HashMap<String, List<String>> friendRelation = new HashMap<>();
+        for (List<String> friend : friends) {
+            if (friendRelation.containsKey(friend.get(0))) {
+                List<String> friednVal = friendRelation.get(friend.get(0));
+                friednVal.add(friend.get(1));
+                friendRelation.remove(friend.get(0));
+                friendRelation.put(friend.get(0), friednVal);
+            }if (friendRelation.containsKey(friend.get(1))) {
+                List<String> friednVal = friendRelation.get(friend.get(1));
+                friednVal.add(friend.get(0));
+                friendRelation.remove(friend.get(1));
+                friendRelation.put(friend.get(1), friednVal);
+            }if (!(friendRelation.containsKey(friend.get(1)))) {
+                List<String> friednVal = new ArrayList<>();
+                friednVal.add(friend.get(0));
+                friendRelation.put(friend.get(1), friednVal);
+            }if (!(friendRelation.containsKey(friend.get(0)))) {
+                List<String> friednVal = new ArrayList<>();
+                friednVal.add(friend.get(1));
+                friendRelation.put(friend.get(0), friednVal);
+            }
+        }
+        System.out.println(friendRelation);
     }
 
 
+
 }
-//  1. hashmap으로 친구 당 가지는 모든 친구관계를 구성
+
 //  2. 사용자가 가지는 친구 관계와 같은 친구를 뽑음.
 //  3.친구 별로 사용자와 같은 친구 하나당 10을 추가하는 hashmap 생성
 //  visitors는 1점 추가
