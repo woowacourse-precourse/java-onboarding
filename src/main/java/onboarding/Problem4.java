11b5fa2771f1c0f6e86586b216b8801075caddf3
Soomin Lim <augustth511@gmail.com>
2022-10-31 20:52:46 +0900

    형식: 주석 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -12,9 +12,6 @@ public class Problem4 {
         return sb.toString();
     }
 
-    /**
-     * 요구사항에 맞게 문자 ch를 변환하여 변환된 문자를 리턴
-     */
     public static char reverseChar(char ch) {
         if (Character.isUpperCase(ch))
             return (char)('A' + 25 - (ch - 'A'));
