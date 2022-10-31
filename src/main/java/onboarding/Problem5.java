a1a67f5fa821447d04a60f0bb31267d5b257d4e3
ohhyungsuh <ohhyungsuh@gmail.com>
2022-11-01 00:26:41 +0900

    Problem5 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] cash = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i=0; i<9; i++){
+            int tmp = 0;
+            while(money / cash[i] >= 1){
+                tmp++;
+                money -= cash[i];
+            }
+            answer.add(tmp);
+        }
+
         return answer;
     }
 }
