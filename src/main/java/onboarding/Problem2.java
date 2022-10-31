31cd10173eb442c34c7bcaa605773ee9f97a674c
sonsoowon <yuwoo1018@gmail.com>
2022-10-31 14:51:36 +0900

    feat(P2): 연속 중복 문자를 찾는 정규식 구하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    static String getPattern(String cryptogram){
+        String pattern = "";
+        Set<String> tokenSet = Set.of(cryptogram.split(""));
+        String tokenString = String.join("", tokenSet);
+
+        pattern = "[" + tokenString + "]{2}";
+
+        return pattern;
     }
 }
