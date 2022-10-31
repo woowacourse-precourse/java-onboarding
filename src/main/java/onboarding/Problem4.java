b7e8afc16b2dc237e26a54836c6851a64d917e06
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-10-31 18:38:40 +0900

    Add : 각 문자의 반대값을 가지는 사전(Map)을 만들어주는 함수 (ex. a -> z)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    // A -> Z, Z -> A 로 변환하는 Map을 세팅하는 함수
+    public static void mapSet(Map<Character, Character> map) {
+        char small = 'a';
+        char capital = 'A';
+        int maxlen = 'Z' - 'A';
+
+        for (int i=0; i<maxlen; i++) {
+            map.put(small++, (char)('z' - i));
+            map.put(capital++, (char) ('Z' - i));
+        }
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
