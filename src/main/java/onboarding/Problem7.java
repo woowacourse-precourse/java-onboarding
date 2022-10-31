ed9ff2aaa381d43fd27b887fe472e03ec880056f
KIMSEI1124 <workju1124@gmail.com>
2022-10-31 16:12:42 +0900

    feat: Problem7 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,104 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> friendship = getFriendship(user, friends);
+        Map<String, Long> recommendScore = new HashMap<>();
+        addFriendshipScore(user, friends, friendship, recommendScore);
+        addVisitorScore(visitors, friendship, recommendScore);
+        return getSortAnswer(recommendScore);
+    }
+
+    /**
+     * 친구 관계 점수를 더한다.
+     *
+     * @param user           유저
+     * @param friends        SNS 친구 관계
+     * @param friendship     유저의 친구 관계
+     * @param recommendScore 친구 추천 점수
+     */
+    private static void addFriendshipScore(String user, List<List<String>> friends, List<String> friendship, Map<String, Long> recommendScore) {
+        for (List<String> friend : friends) {
+            if (!friend.contains(user)) {
+                if (hasId(friend.get(0), friendship)) {
+                    addRecommendScore(recommendScore, friend.get(1), 10L);
+                } else if (hasId(friend.get(1), friendship)) {
+                    addRecommendScore(recommendScore, friend.get(0), 10L);
+                }
+            }
+        }
+    }
+
+    /**
+     * 친구 관계에 점수를 더한다.
+     *
+     * @param recommendScore 친구 추천 점수
+     * @param friend         SNS 친구 관계
+     * @param score          점수
+     */
+    private static void addRecommendScore(Map<String, Long> recommendScore, String friend, long score) {
+        recommendScore.put(friend, recommendScore.getOrDefault(friend, 0L) + score);
+    }
+
+    /**
+     * 방문 친구 점수를 더한다.
+     *
+     * @param visitors       방문 목록
+     * @param friendship     유저의 친구들
+     * @param recommendScore 친구 추천 점수
+     */
+    private static void addVisitorScore(List<String> visitors, List<String> friendship, Map<String, Long> recommendScore) {
+        for (String visitor : visitors) {
+            if (!hasId(visitor, friendship)) {
+                addRecommendScore(recommendScore, visitor, 1L);
+            }
+        }
+    }
+
+    /**
+     * 해당 Id를 가지고 있는지 검색한다.
+     *
+     * @param id  아이디
+     * @param ids 아이디 리스트
+     * @return 해당 id 를 가지고 있으면 true
+     */
+    private static boolean hasId(String id, List<String> ids) {
+        return ids.contains(id);
+    }
+
+    /**
+     * 유저의 친구 관계를 구한다.
+     *
+     * @param user    유저
+     * @param friends SNS 친구관계
+     * @return 유저의 친구 관계를 반환한다.
+     */
+    private static List<String> getFriendship(String user, List<List<String>> friends) {
+        List<String> friendship = new ArrayList<>(Collections.emptyList());
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                if (friend.get(0).equals(user)) {
+                    friendship.add(friend.get(1));
+                } else if (friend.get(1).equals(user)) {
+                    friendship.add(friend.get(0));
+                }
+            }
+        }
+        return friendship;
+    }
+
+    /**
+     * 정렬된 추천 목록을 구한다.
+     * @param recommendScore    친구 추천 점수
+     * @return  친추 추천 목록 반환
+     */
+    private static List<String> getSortAnswer(Map<String, Long> recommendScore) {
+        List<String> answer = new ArrayList<>(recommendScore.keySet());
+        Collections.sort(answer);
+        answer.sort((o1, o2) -> (recommendScore.get(o2).compareTo(recommendScore.get(o1))));
+        answer.subList(0, Math.min(5, answer.size()));
         return answer;
     }
 }
