8c99af05ed665cd9a8c5629b613a428fa318fcc4
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 00:26:21 +0900

    docs : problem2 예외 체크 메서드(길이 체크 checkLength) 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        if(checkException(cryptogram)){
+            throw new IllegalArgumentException();
+        }
+
         return answer;
+    }
+    public static boolean checkException(String cryptogram){
+        if(checkLength(cryptogram.length())){
+            return true;
+        }
+
+        return false;
+    }
+    public static boolean checkLength(int length){
+        return length < 1 || length > 1000;
     }
 }
