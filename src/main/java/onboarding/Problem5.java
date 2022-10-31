e9915a601ee30a1aa052a73edead443f65dc6440
JIWEON-JEONG <dlektl6@naver.com>
2022-10-31 22:05:34 +0900

    [FEAT] exchangeMoney

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,9 +3,43 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+//오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
+
+/**
+ * 기능 목록
+ * 주어진 금액 에서
+ * 1. 오만원 권, 금액 최신화
+ * 2. 만원 권, 금액 최신화
+ * 3. 오천 원권, 금액 최신화
+ * 4. 천 원권, 금액 최신화
+ * 5. 오백원 동전, 금액 최신화
+ * 6. 백원 동전, 금액 최신화
+ * 7. 오십원 동전, 금액 최신화
+ * 8. 십원 동전, 금액 최신화
+ * 9. 일원 동전, 금액 최신화
+ */
 public class Problem5 {
+
+    public static int[] standardArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        Problem5 p5 = new Problem5();
+        List<Integer> emptyList = Collections.emptyList();
+        List<Integer> answer = p5.exchangeMoney(emptyList, money);
         return answer;
+    }
+
+    public List<Integer> exchangeMoney(List<Integer> list, int money) {
+        for (int standard : standardArr) {
+            int quotient = getQuotientMoneyDividedStandard(standard, money);
+            list.add(quotient);
+            money -= standard * quotient;
+        }
+        return list;
+    }
+
+
+    private int getQuotientMoneyDividedStandard(int standard, int money) {
+        return standard / money;
     }
 }
