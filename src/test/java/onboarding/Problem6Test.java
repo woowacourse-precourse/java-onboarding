2f83ae3e1f296a9313b0c8c0629d4e4a45130e75
saehim <saeilee.vxv@gmail.com>
2022-11-01 00:09:26 +0900

    Test(Problem6): 변수 선언

--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package onboarding;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class Problem6Test {
-  
+
+    static List<List<String>> forms = List.of(
+            List.of("jm@email.com", "제이엠"),
+            List.of("jason@email.com", "제이슨"),
+            List.of("woniee@email.com", "워니"),
+            List.of("mj@email.com", "엠제이"),
+            List.of("nowm@email.com", "이제엠")
+    );
+    static Set<String> emails = new HashSet<>();
+    static Map<String, String> nameMap = new HashMap<>();
 }
