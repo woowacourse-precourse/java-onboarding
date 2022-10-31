dd1f65e086e97f79432f4c0343de3367ad7f1dff
pjw03659 <pjw03659@naver.com>
2022-10-31 21:02:20 +0900

    some init msg

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,48 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        String pobiLeft= Integer.toString(pobi.get(0));
+        String pobiRight= Integer.toString(pobi.get(1));
+        String crongLeft= Integer.toString(crong.get(0));
+        String crongRight= Integer.toString(crong.get(1));
+        int pobiLeftSum= 0, pobiRightSum=0, crongLeftSum=0, crongRightSum=0;
+        int pobiLeftProduct=1, pobiRightProduct=1, crongLeftProduct=1, crongRightProduct=1;
+        for(int i=0;i<pobiLeft.length();i++){
+            pobiLeftSum+=(pobiLeft.charAt(i)-'0');
+            pobiLeftProduct*=(pobiLeft.charAt(i)-'0');
+        }
+        for(int i=0;i<pobiRight.length();i++){
+            pobiRightSum+=(pobiRight.charAt(i)-'0');
+            pobiRightProduct*=(pobiRight.charAt(i)-'0');
+        }
+        for(int i=0;i<crongLeft.length();i++){
+            crongLeftSum+=(crongLeft.charAt(i)-'0');
+            crongLeftProduct*=(crongLeft.charAt(i)-'0');
+        }
+        for(int i=0;i<crongRight.length();i++){
+            crongRightSum+=(crongRight.charAt(i)-'0');
+            crongRightProduct*=(crongRight.charAt(i)-'0');
+        }
+
+        int pobiLeftMax = (pobiLeftSum > pobiLeftProduct) ? pobiLeftSum : pobiLeftProduct;
+        int pobiRightMax = (pobiRightSum > pobiRightProduct) ? pobiRightSum : pobiRightProduct;
+        int crongLeftMax = (crongLeftSum > crongLeftProduct) ? crongLeftSum : crongLeftProduct;
+        int crongRightMax= (crongRightSum > crongRightProduct) ? crongRightSum : crongRightProduct;
+        int pobiMax = (pobiLeftMax > pobiRightMax) ? pobiLeftMax : pobiRightMax;
+        int crongMax= (crongLeftMax > crongRightMax) ? crongLeftMax : crongRightMax;
+        if(Math.abs(pobi.get(0)-pobi.get(1))!=1 || Math.abs(crong.get(0)-crong.get(1))!=1){
+            answer=-1;
+        }else {
+            if(pobiMax>crongMax){
+                answer=1;
+            } else if (pobiMax<crongMax) {
+                answer=2;
+            } else if (pobiMax==crongMax) {
+                answer=0;
+            }
+        }
+
         return answer;
     }
 }
+
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String[] array_word;
+        String[] change_word ={"Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"};
+
+        Map<String, String> alpha = new HashMap<>();
+
+        char[] alphabat = new char[26];
+
+        for(int i=0;i<alphabat.length;i++){
+            alpha.put(Character.toString((char) ('A'+i)),change_word[i]);
+
+        }
+        array_word=word.split("");
+
+        for (String c_word: array_word){
+            boolean up= Character.isUpperCase(c_word.charAt(0));
+
+            String temp= alpha.get(c_word.toUpperCase());
+            if(temp==null){
+                temp=" ";
+            } else if (!up) {
+                temp=temp.toLowerCase();
+            }
+            answer=answer.concat(temp);
+        }
         return answer;
     }
 }
