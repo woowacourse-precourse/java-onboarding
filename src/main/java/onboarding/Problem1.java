3ad9be01d438b8ec07e869ed5e85e3a963726bee
박상천 <pswlove38@naver.com>
2022-10-31 11:49:01 +0900

    리스트 만들기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -10,11 +11,20 @@ class Problem1 {
     }
     public static int random(){
         Random rd = new Random();
-        return rd.nextInt(400);
+        return rd.nextInt(400)+1;
     }
     // 난수 발생
+
+    public static List makeList(){
+        List<Integer> list = new ArrayList<>();
+        list.add(random());
+        list.add(random());
+        return list;
+    }
+    // 리스트 만들기
+
     public static void main(String[] args){
-        System.out.println(random());
+        System.out.println(makeList());
     }
     // 리스트 초기화
     // 리스트 비교
