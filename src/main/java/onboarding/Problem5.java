96da3fc34e17a48009fbe7f23270894c1ce1d766
WonjunS <seowj0710@gmail.com>
2022-10-31 17:11:29 +1100

    Problem 5: 주석 처리한 부분 일부 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    static int[] amounts = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 }; // 5만원 권부터 1원 동전까지
+    static int[] amounts = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 }; // 5만원짜리 지폐부터 1원 동전
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>(); // 정답을 리턴하는 리스트
 
         for(int amount : amounts) {
-            int count = money / amount; // (현재 가진돈 / 금액)
+            int count = money / amount; // 출금 가능한 최대 개수
             money -= (amount * count);  // 현재 가진 돈에서 출금한 금액만큼 차감
 
             answer.add(count);
