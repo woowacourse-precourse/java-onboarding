99face955d1faefa4e16804707a96fb8c3b7445f
ryanha <ryanha@handong.ac.kr>
2022-10-31 14:31:24 +0900

    Feat[Problem1] : 페이지 유효성을 검증하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+
+        // 0. 전달받은 페이지 정보에 대한 유효성을 검증함 (연속적으로 주어진 페이지 인가?)
+        if(!validation(pobi) || !validation(crong)) {
+            answer = -1;
+            return answer;
+        }
+
         return answer;
+    }
+
+    // 페이지 정보에 대한 유효성을 검증 (연속적으로 주어진 페이지인가?)
+    private static boolean validation(List<Integer> pages) {
+        return (pages.get(0)+1 == pages.get(1));
     }
 }
