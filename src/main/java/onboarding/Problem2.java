f869c7bbe4625bbc69da8d93962e61ee6bfd9f00
Young37 <20171470@g.dongseo.ac.kr>
2022-10-31 20:45:22 +0900

    입력값 StringBuilder로 변환(삭제 작업이 많이 일어날 것이라고 판단)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+    	StringBuilder cryptogramSB = new StringBuilder(cryptogram);
+    	return cryptogramSB.toString();
     }
 }
