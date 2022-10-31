9d5c8aa3aeb791c5befa936ca3b959ca7bebc09a
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-31 09:07:28 +0900

    feat: reconstruct Decoder class

--- src/main/java/onboarding/problem2/Decoder.java
+++ src/main/java/onboarding/problem2/Decoder.java
@@ -4,5 +4,15 @@ package onboarding.problem2;
  * Decoder for decoding cryptogram
  */
 public class Decoder {
-    
+
+    private final String CIPHERTEXT;
+
+    /**
+     * Constructor with cryptogram string
+     *
+     * @param cryptogram encrypt text
+     */
+    public Decoder(String cryptogram) {
+        CIPHERTEXT = cryptogram;
+    }
 }
