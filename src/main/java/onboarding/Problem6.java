fa69f0420fbd2553774e2a94d91296caa9185edb
Aru428 <86353626+Aru428@users.noreply.github.com>
2022-11-01 00:25:25 +0900

    문제6 구현 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,24 +4,26 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
+        List<String> answer = new ArrayList<>();;
         HashSet<String> emails = new HashSet<>();
         Map<String, String> hashmap = new HashMap<>();
 
         for(int i=0;i<forms.size();i++){
             String name = forms.get(i).get(1);
             for(int j=0;j<name.length()-1;i++){
-                String checkName = name.substring(j,j+1);
-                if(hashmap.containsKey(checkName)){
-                    String email = hashmap.get(checkName);
+                String key = name.substring(j,j+1);
+                if(hashmap.containsKey(key)){
+                    String email = hashmap.get(key);
                     if(!email.equals(forms.get(i).get(0))){
                         emails.add(email);
                         emails.add(forms.get(i).get(0));
                     }
                 }
-                hashmap.put(checkName,forms.get(i).get(0));
+                hashmap.put(key,forms.get(i).get(0));
             }
         }
+        answer = (List<String>) emails.stream();
+        Collections.sort(answer);
 
         return answer;
     }
