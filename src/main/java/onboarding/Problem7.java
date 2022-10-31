edd1e446a1a877c8435e4a05db443e5cef581f66
Eleisia <blackcube222@gmail.com>
2022-10-31 22:18:06 +0900

    7번문제 1번기능  친구목록작성 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 /*
 * 기능목록
 *
@@ -11,8 +10,25 @@ import java.util.List;
 * */
 
 public class Problem7 {
+
+    static HashMap<String, Set<String>> friendsHashMap = new HashMap<>();
+    static HashMap<String, Integer> points = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    static void friendList(String user, List<List<String>> friends){
+        for (List<String> f:friends) {
+            if (!f.get(0).equals(user)) points.put(f.get(0), 0);
+            if (!f.get(1).equals(user)) points.put(f.get(1), 0);
+            if (!friendsHashMap.containsKey(f.get(0))) {
+                friendsHashMap.put(f.get(0), new HashSet<>());
+            }
+            if (!friendsHashMap.containsKey(f.get(1))) {
+                friendsHashMap.put(f.get(1), new HashSet<>());
+            }
+            friendsHashMap.get(f.get(0)).add(f.get(1));
+            friendsHashMap.get(f.get(1)).add(f.get(0));
+        }
 }
