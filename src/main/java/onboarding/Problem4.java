eb9aa3a12f11313f2f07fe8f6997e5a587243adf
gomljo <gomljo0307@naver.com>
2022-10-31 22:11:11 +0900

    feat(Problem4.java): 기능 #7 구현
    z~a까지 알파벳 배열을 생성하는 makeFrogLetterArray 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,18 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 class FrogDictionary{
-    final char FIRST_ALPHABET='a';
-    final int NUMBER_OF_ALPHABET=26;
-    HashMap<Character, Character> frogDictionary;
+    static final char FIRST_ALPHABET='a';
+    static final char LAST_ALPHABET='z';
+    static final int NUMBER_OF_ALPHABET=26;
 
     char[] makeAlphabetArray(){
         char[] alphabetArray=new char[NUMBER_OF_ALPHABET];
-        for(int i=0;i<NUMBER_OF_ALPHABET;i++){
-            alphabetArray[i] = (char)((int)FIRST_ALPHABET + i);
+        for(int order=0;order<NUMBER_OF_ALPHABET;order++){
+            alphabetArray[order] = (char)((int)FIRST_ALPHABET + order);
         }
         return alphabetArray;
+    }
+    char[] makeFrogLetterArray(){
+        char[] frogLetter=new char[NUMBER_OF_ALPHABET];
+
+        for(int order=0;order<NUMBER_OF_ALPHABET;order++){
+            frogLetter[order] = (char)((int)LAST_ALPHABET - order);
+        }
+        return frogLetter;
     }
 }
 public class Problem4 {
