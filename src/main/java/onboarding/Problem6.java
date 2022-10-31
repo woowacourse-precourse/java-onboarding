249743ba03fa32c9f3b36a364b8579de3230a9a5
LeeJinHo <hosboy93@naver.com>
2022-11-01 00:17:37 +0900

    문제6. HashMap을 이용해 닉네임 2글자 이상 중복 찾기 & 이메일 찾기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashMap<String, String> profileMap = new HashMap<String, String>();
+        HashSet<String> emails = new HashSet<String>();
+        
+        String compareStr = "";
+        
+        for(int i=0; i<forms.size(); i++) {
+        	String name = forms.get(i).get(1);
+        	
+        	for(int j=0; j<name.length()-1; j++) {
+        		String nameKey = name.substring(j, j+2);
+        		if(profileMap.containsKey(nameKey)) {
+        			String email = profileMap.get(nameKey);
+        			emails.add(email);
+        			emails.add(forms.get(i).get(0));        			
+        		}
+        		profileMap.put(nameKey, forms.get(i).get(0));
+        	}
+        }
+        System.out.println(emails);
         return answer;
     }
 }
