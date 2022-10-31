2d3a26b26c7d3a2fadf2f8bafa771ec4938d9277
moonn6pence <shin421179@gmail.com>
2022-10-31 19:20:22 +0900

    docs #4 : 기능 목록 수정
    기능 목록 1번 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,18 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static char changeCase(char c){
+        //lower to upper
+        if (c >= 65 && c <= 90) {
+            return (char) (c + 38);
+        }
+        // upper to lower
+        else if (c >= 97 && c <= 112) {
+            return (char) (c - 38);
+        }
+
+        //not an alphabet
+        return c;
+    }
 }
