51e23d5e3ea42c623867d453031b62f954ab53c1
vcho1958 <gusckd57@gmail.com>
2022-11-01 01:04:38 +0900

    [problem7] Database 요구사항 정의

--- src/main/java/onboarding/problem7/Database.java
+++ src/main/java/onboarding/problem7/Database.java
@@ -1,4 +1,20 @@
 package onboarding.problem7;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Database {
+    //사용자 이름 인덱스 해쉬맵
+    //입력받은 방문기록
+
+    //방문기록으로 생성자
+    //사용자 추가
+    //이름으로 검색
+    //로그 기준으로 회원 등록
+    //친구관계 입력받아서 DB최신화
+    //누군가의 친구로 추천하기위해 겹치는 친구 수 방문기록을 기준으로 점수를 갱신한다.
+    //친구 추천 목록 반환함수
+
 }
