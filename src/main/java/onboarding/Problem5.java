c575d8fa5d631c9c54b53a9f5e1e77f7a7b7771f
eugene1204 <s1061001@naver.com>
2022-10-31 16:36:39 +0900

    기능 목록 : problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,9 +3,14 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+// 1. int money를 List로 바꿔주기
+// 2. 각자리에 얼마가 필요한지 나누는 메소드
+// 3. 거꾸로 만들어주는 메소드
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;}
+
+    
+
 }
