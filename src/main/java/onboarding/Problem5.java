99eae9187e50f6e70a0e65c6e9593052fcac3bd3
Sehoon Kim <shggm2000@gmail.com>
2022-10-31 23:29:18 +0900

    큰 수부터 내림차순으로 각 화폐의 최대 개수를 구하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,6 +12,24 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         answer = new ArrayList<Integer>();
 
+        moneyCount(money,50000, answer);
+        money %= 50000;
+        moneyCount(money,10000, answer);
+        money %= 10000;
+        moneyCount(money,5000, answer);
+        money %= 5000;
+        moneyCount(money,1000, answer);
+        money %= 1000;
+        moneyCount(money,500, answer);
+        money %= 500;
+        moneyCount(money,100, answer);
+        money %= 100;
+        moneyCount(money,50, answer);
+        money %= 50;
+        moneyCount(money,10, answer);
+        money %= 10;
+        moneyCount(money,1, answer);
+
         return answer;
     }
 
