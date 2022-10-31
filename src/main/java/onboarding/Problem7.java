41003962f981a4aeeeb6d56e3a453cef4964b38b
wath1457 <wath1457@gmail.com>
2022-10-31 16:53:45 +0900

    [Problem7] Fix errors : Add Integer unboxing & Add visitor in friends_score_list & except user in setScore func

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,12 @@
 package onboarding;
 
 import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 /*
 0. friends 를 순회하면서 친구점수 리스트를 만듦(중복x) [name, score] + user_info 클래스 정의
 1. friends 리스트를 순회하면서 user, user와 이미 친구인 사람은 추천목록에서 제외(친구제외 리스트에 추가, 친구 점수 map에는 점수를 -1점으로 설정)
+1-2. friends 친구관계에 없는 사람이 visitors 에 존재할 수 있으므로, visitor 도 친구 점수 map에 추가
 2. user와 함께 아는사이(친구제외 리스트에 있는 사람) 과 친구이면 + 10
 3. visitors에 들어있는 횟수만큼 + 1
 4. 람다식을 이용하여 friends_score map을 점수 내림차순, 이름 오름차순 정렬 후 answer 리스트에 추가(점수가 -1은 추가하지 앟는다)
@@ -11,15 +14,15 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        Map<String, Integer> friends_score = makeFriendsScore(friends);
+        Map<String, Integer> friends_score = makeFriendsScore(friends, visitors);
         List<String> exception_list = makeFriendsException(user, friends);
         initFriendsScore(friends_score, exception_list, user);
-        setScore(friends, friends_score, exception_list, visitors);
+        setScore(friends, friends_score, exception_list, visitors, user);
         List<Map.Entry<String, Integer>> sortedList = sortScore(friends_score);
         List<String> answer = getAnswer(sortedList);
         return answer;
     }
-    public static Map<String, Integer> makeFriendsScore(List<List<String>> friends) {
+    public static Map<String, Integer> makeFriendsScore(List<List<String>> friends, List<String> visitors) {
         List<String> name_list = new ArrayList<String>();
         HashMap<String, Integer> friends_score = new HashMap<String, Integer>();
         for (List<String> friend : friends) {
@@ -31,6 +34,12 @@ public class Problem7 {
         for (String name : name_list) {
             friends_score.put(name, 0);
         }
+        // 친구관계에 없는 visitor 도 추가
+        for (String name : visitors) {
+            if (!name_list.contains(name)) {
+                friends_score.put(name, 0);
+            }
+        }
         return friends_score;
     }
     public static List<String> makeFriendsException(String user, List<List<String>> friends) {
@@ -39,7 +48,7 @@ public class Problem7 {
             if (friend_relation.contains(user)) {
                 // user와 친구관계이면 제외리스트에 추가
                 for (String friend : friend_relation) {
-                    if (friend != user)
+                    if (!friend.equals(user))
                         exception_list.add(friend);
                 }
             }
@@ -52,19 +61,22 @@ public class Problem7 {
         }
         friends_score.put(user, -1);
     }
-    public static void setScore(List<List<String>> friends, Map<String, Integer> friends_score, List<String> exception_list, List<String> visitors) {
+    public static void setScore(List<List<String>> friends, Map<String, Integer> friends_score, List<String> exception_list, List<String> visitors, String user) {
         for (String excepted_user : exception_list) {
             for (List<String> friend_relation : friends) {
                 if (friend_relation.contains(excepted_user)) {
                     for (String name : friend_relation) {
-                        if (name != excepted_user)
-                            friends_score.put(name, friends_score.get(name) + 10);
+                        if (!name.equals(excepted_user) && !name.equals(user)) {
+                            int i = friends_score.get(name);
+                            friends_score.put(name, i + 10);
+                        }
                     }
                 }
             }
         }
         for (String visitor : visitors) {
-            friends_score.put(visitor, friends_score.get(visitor) + 1);
+            int i = friends_score.get(visitor);
+            friends_score.put(visitor, i + 1);
         }
     }
     public static List<Map.Entry<String, Integer>> sortScore(Map<String, Integer> friends_score) {
@@ -76,7 +88,7 @@ public class Problem7 {
         List<String> answer = new ArrayList<String>();
         for(Map.Entry<String, Integer> user_info : sortedList) {
             Integer score = user_info.getValue();
-            if (answer.size() < 5 && score> 0)
+            if (answer.size() < 5 && score > 0)
                 answer.add(user_info.getKey());
         }
         return answer;
