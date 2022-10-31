26d0b0c5a60265990877d54c2206af5943dbcb41
desfox <desfox99@naver.com>
2022-10-31 22:44:45 +0900

    feat(Problem6): 각 글자 조합이 포함된 닉네임을 갖는 지원자 목록을 Map에 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 public class Problem6 {
     public static final int EMAIL_IDX = 0;
@@ -13,13 +14,25 @@ public class Problem6 {
     }
 
     private static void getSimilarNicknameUser(List<List<String>> forms) {
+        HashMap<String, List<Integer>> nicknameIncludingContinuousMap = new HashMap<>();
+
         for (int i = 0; i <= forms.size() - 1; i++) {
             String nickname = forms.get(i).get(NICKNAME_IDX);
 
             for (int j = 0; j <= nickname.length() - 2; j++) {
                 char[] charArray = {nickname.charAt(j), nickname.charAt(j + 1)};
                 String continuous = String.valueOf(charArray);
+
+                getNicknameIncludingContinuousMap(nicknameIncludingContinuousMap, i, continuous);
             }
+        }
+    }
+
+    private static void getNicknameIncludingContinuousMap(HashMap<String, List<Integer>> nicknameIncludingContinuousMap, int i, String continuous) {
+        if (nicknameIncludingContinuousMap.containsKey(continuous)) {
+            nicknameIncludingContinuousMap.get(continuous).add(i);
+        } else {
+            nicknameIncludingContinuousMap.put(continuous, new ArrayList<>(List.of(i)));
         }
     }
 }
