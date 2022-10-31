7165fb5bf2688e92ac1309cd6b042764cb8a92e9
gilyeon00 <12venus08@naver.com>
2022-11-01 01:37:18 +0900

    Feat : 문제1 - checkPage 기능추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,26 @@
+/*
+* 우테코 1주차 프리코스 미션 - 문제 1
+* [https://github.com/gilyeon00/java-onboarding/blob/main/docs/PROBLEM1.md]
+*
+* */
 package onboarding;
 
 import java.util.List;
 
+/**
+ * checkPage : 전달받은 페이지 번호의 예외사항 확인하는 메서드
+ **/
+
 class Problem1 {
+    public static int checkPage (List<Integer> pages){
+        if(pages.get(1) - pages.get(0) != 1) {
+            return -1;          // 연속된 숫자가 아닐 경우 예외
+        }else if (pages.get(0) < 0 || pages.get(0) > 400) {
+            return -1;          // 왼쪽 페이지 기준, 책의 범위를 벗어날 경우 예외
+        }
+        return 0;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
