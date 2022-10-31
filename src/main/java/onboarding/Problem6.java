16136626baa31995f6f83afb294882042ecad277
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:58:19 +0900

    6번: 한 사람과 다른 사람들간의 중복 체크 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -8,24 +10,38 @@ public class Problem6 {
     private static final String DOMAIN = "@email.com";
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         
+
         return answer;
     }
 
+    private static boolean checkValidate(List<String> member, List<List<String>> forms) {
+        String memberEmail = member.get(0);
+        String memberNickName = member.get(1);
+
+        for (int i = 2; i < memberNickName.length()+1; i++) {
+           String targetSubNickName = memberNickName.substring(i-2, i);
+           if (isMatchWithOther(memberEmail, targetSubNickName, forms)){
+               return true;
+           }
+        }
+        return false;
+    }
+
     private static boolean isMatchWithOther(String myEmail, String targetSubNickName, List<List<String>> forms) {
-        for (List<String> member : forms){
-            if (member.get(0).equals(myEmail)){
+        for (List<String> member : forms) {
+            if (member.get(0).equals(myEmail)) {
                 continue;
             }
-            if (checkDuplicate(targetSubNickName, member.get(1))){
+            if (checkDuplicate(targetSubNickName, member.get(1))) {
                 return true;
             }
         }
         return false;
     }
 
-    private static boolean checkDuplicate(String targetSubNickName, String otherNickName){
+    private static boolean checkDuplicate(String targetSubNickName, String otherNickName) {
         String regex = ".*" + targetSubNickName + ".*";
         return Pattern.matches(regex, otherNickName);
     }
