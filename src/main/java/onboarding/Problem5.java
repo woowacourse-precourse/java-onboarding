fb61f14a16df5718fcda25a79513d93b345d1b56
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-31 18:22:57 +0900

    refactor : (Problem5) 관심사 분리
    관심사에 따라 클래스 및 이넘으로 역할을 분리한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,37 +5,66 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = List.of();
 
-        /*
-        * 나누는 금액이 1/2와 1/5 순서대로 줄어들고 있다.
-        * 몫을 리스트에 저장하고, 나머지를 다음 계산을 위한 돈으로 할당한다.
-        * */
-        int divide = 100_000;
-        for(int i=0; i<8; i++){
-            divide = (i%2==0) ? divide/2 : divide/5;
-
-            int[] qar = quotientAndRemainder(money, divide);
-            answer.add(qar[0]);
-            money = qar[1];
-        }
-
-        /*
-        * 1원은 나눌 필요 없이 바로 추가한다.
-        * */
-        answer.add(money);
+        MoneyCalculator moneyCalculator = new MoneyCalculator(money);
+        answer = moneyCalculator.calculation();
 
         return answer;
     }
 
-    /*
-    * qar[0] : 몫을 저장한다.
-    * qar[1] : 나머지를 저장한다.
-    * */
-    private static int[] quotientAndRemainder(int num, int divide){
-        int[] qar = new int[2];
-        qar[0] = num/divide;
-        qar[1] = num%divide;
-        return qar;
+    public enum Money{
+        AMOUNT(100_000),
+        ONE_TWO(2),
+        ONE_FIFTH(5),
+        TYPE(9);
+
+        private int value;
+
+        Money(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public static class MoneyCalculator{
+        private int money;
+
+        public MoneyCalculator(int money) {
+            this.money = money;
+        }
+
+        /*
+         * 나누는 금액이 1/2와 1/5 순서대로 줄어들고 있다.
+         * 몫을 리스트에 저장하고, 나머지를 다음 계산을 위한 돈으로 할당한다.
+         * */
+        public List<Integer> calculation(){
+            List<Integer> result = new ArrayList<>();
+
+            int divide = Money.AMOUNT.getValue();
+            for(int i = 0; i<Money.TYPE.getValue()-1; i++){
+                divide = (i % Money.ONE_TWO.getValue() == 0) ?
+                        divide / Money.ONE_TWO.getValue() :
+                        divide / Money.ONE_FIFTH.getValue();
+
+                result.add(quotient(divide));
+                money = remainder(divide);
+            }
+
+            result.add(money);
+
+            return result;
+        }
+
+        private int quotient(int divide){
+            return this.money/divide;
+        }
+
+        private int remainder(int divide){
+            return this.money%divide;
+        }
     }
 }
