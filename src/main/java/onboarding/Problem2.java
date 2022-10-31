fb7002cde45dcf3cb6a848118640a12c0e151ee7
LeeJinHo <hosboy93@naver.com>
2022-10-31 09:44:31 +0900

    문제2. 연속하는 중복 문자들 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,6 +10,25 @@ public class Problem2 {
         for(int i=0; i<cryptogram.length(); i++) {
         	password.add(cryptogram.charAt(i));
         }
+        
+        for(int i=0; i<password.size(); i++) {
+        	System.out.print(password.get(i));
+        }
+        System.out.println();
+        
+        for(int i=1; i<password.size(); i++) {
+        	if(password.get(i)==password.get(i-1)) {        		
+        		password.remove(i);
+        		password.remove(i-1);        		
+        		i=0;
+        	}
+        }
+        
+        for(int i=0; i<password.size(); i++) {
+        	System.out.print(password.get(i));
+        }
+        System.out.println();
+        
         return answer;
     }
 }
