4ea2dd5fbbbdbc1a3c58645551bae220541516be
jindaram-stu <jinsb1999@naver.com>
2022-10-31 22:50:18 +0900

    feat : [문제2] 문자열을 Stack<Character>에 저장하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        while (true) {
+            cryptogram = remove(cryptogram,stack);
+        }
+        return cryptogram;
     }
+
+    public static String getStringFromStack(Stack<Character> stack) {
+        String text = "";
+        while(!stack.isEmpty()) {
+            text += stack.peek();
+            stack.pop();
+        }
+        StringBuffer answerBuffer = new StringBuffer(text).reverse();
+        return answerBuffer.toString();
+    }
+
+    public static String remove(String cryptogram, Stack<Character> stack) {
+        int dupliCount = 1;
+        boolean isDuplicate = false;
+        for (int i = 0; i < cryptogram.length(); i++) {
+           // 하
+            stack.push(cryptogram.charAt(i));
+        }
+        return getStringFromStack(stack);
+    }
+
 }
+
