22ee12c9df6f062c58a68e6f762bc0254922d41d
James <rlagurwo6209@naver.com>
2022-10-31 20:33:46 +0900

    솔루션 답 반환 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,8 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        answer = solutionAnswer(pobi, crong);
+
         return answer;
     }
 
@@ -37,6 +39,44 @@ class Problem1 {
         MaxValue = Math.max(sum, multiple);
 
         return MaxValue;
+    }
+
+    static int solutionAnswer (List<Integer> pobiPage, List<Integer> crongPage){
+
+        int leftIndex = 0;
+        int rightIndex = 1;
+
+        if(pobiPage.get(leftIndex)+1 != pobiPage.get(rightIndex) || crongPage.get(leftIndex)+1 != crongPage.get(rightIndex)){
+            return -1;
+        }
+
+
+        int pobiScore = 0;
+        int crongScore = 0;
+
+        if(FindMaxValue(pobiPage.get(leftIndex)) > FindMaxValue(pobiPage.get(rightIndex))) {
+            pobiScore = FindMaxValue(pobiPage.get(leftIndex));
+        }
+        else {
+            pobiScore = FindMaxValue(pobiPage.get(rightIndex));
+        }
+
+        if(FindMaxValue(crongPage.get(leftIndex)) > FindMaxValue(crongPage.get(rightIndex))) {
+            crongScore = FindMaxValue(crongPage.get(leftIndex));
+        }
+        else {
+            crongScore = FindMaxValue(crongPage.get(rightIndex));
+        }
+
+
+        if(pobiScore > crongScore){
+            return 1;
+        } else if (pobiScore < crongScore) {
+            return 2;
+        } else {
+            return 0;
+        }
+
     }
 
 }
