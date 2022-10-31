fb58d0ef450035820696dc5d3f829e0e14fa1606
eugene1204 <s1061001@naver.com>
2022-10-31 16:02:59 +0900

    p1, 2. 각페이지의 곱셈, 덧셈해주는 메소드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,6 +17,24 @@ class Problem1 {
         if(list.get(1)-list.get(0)==1) return true;
         else return false;
     }
+    public static int calPlus(int num){ // 각페이지의 덧셈을 해주는 메소드
+        // 98
+        int sum =0;
+        while (num!=0){ // 98/10 = 9…8
+            sum +=num%10;
+            num = num/10;
+        }
+        return sum;
+    }
+
+    public static int calMulti(int num){ // 각페이지의 곱셈을 해주는 메소드
+        int multi =1;
+        while (num!=0){ // 98/10 = 9…8
+            multi *=num%10;
+            num = num/10;
+        }
+        return multi;
+    }
 
 
 }
