093cb62dfcb90ca52c86ee7330a9af3e82097e24
hyun-seung <ardimento22@naver.com>
2022-10-31 12:50:36 +0900

    [FEAT] 문제의 제한사항에 부한하는지 확인한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,12 +11,23 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
 
+        if(!verificationCheck(money)) {
+            return answer;
+        }
+
         answer = new ArrayList<>();
-        for(Integer moneyUnit : moneyUnitList) {
+        for (Integer moneyUnit : moneyUnitList) {
             answer.add(money / moneyUnit);
             money %= moneyUnit;
         }
 
         return answer;
+    }
+
+    public static boolean verificationCheck(int money) {
+        if(money < 1 || money > 1000000) {
+            return false;
+        }
+        return true;
     }
 }
