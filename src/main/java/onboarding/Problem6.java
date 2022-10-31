2a97c88208dd84cb568b518c239cc5b13ee77e41
juy4556 <kws05050@naver.com>
2022-10-31 18:34:32 +0900

    feat: add problem6 solution

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,38 @@
 package onboarding;
 
-import java.util.List;
+
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        String email = "", name = "";
+        Set<String> removeSet;
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> list, subStrList;
+
+        for (List<String> form : forms) {
+            email = form.get(0);
+            name = form.get(1);
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                String subStr = name.substring(j, j + 2);
+                list = new ArrayList<>(List.of(email));
+                if (map.containsKey(subStr)) {
+                    subStrList = map.get(subStr);
+                    list.addAll(subStrList);
+                }
+                map.put(subStr, list);
+            }
+        }
+
+        for (List<String> value : map.values()) {
+            if (value.size() > 1) {
+                answer.addAll(value);
+            }
+        }
+        removeSet = new TreeSet<>(answer);
+        answer = new ArrayList<>(removeSet);
         return answer;
     }
 }
