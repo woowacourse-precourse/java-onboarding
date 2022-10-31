8ecf3b8e0f57ba9d2608871620ac13e8d72dc159
xxeol2 <jhj01177@naver.com>
2022-10-31 22:21:15 +0900

    feat(prob7): Problem7, RecommendController 연결

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
 
+import onboarding.problem7.Assembler;
+import onboarding.problem7.RecommendController;
+
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		Assembler assembler = new Assembler();
+		assembler.createAndWire();
+		final RecommendController recommendController = assembler.getRecommendController();
+		List<String> answer = recommendController.recommend(user, friends, visitors);
+		return answer;
+	}
 }
