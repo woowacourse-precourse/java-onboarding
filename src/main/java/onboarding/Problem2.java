76b63078774858219b64b88bb5f7795ea6468319
mardi2020 <mardi@khu.ac.kr>
2022-10-31 15:15:18 +0900

    feat: Problem2 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,28 @@ public class Problem2 {
         Stack<Character> stack = new Stack<>();
         char prevCharacter = 0;
 
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char c = cryptogram.charAt(i);
+            if (stack.empty()) {
+                stack.add(c);
+            }
+            else {
+                if (stack.peek().equals(c)) {
+                    stack.pop();
+                    prevCharacter = c;
+                }
+                else {
+                    if (prevCharacter != c) {
+                        stack.add(c);
+                    }
+                }
+            }
+        }
+
+        for (Character c : stack) {
+            answer.append(c);
+        }
+
         return answer.toString();
     }
 }
