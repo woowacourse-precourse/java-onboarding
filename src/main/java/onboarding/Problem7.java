b2e7fcc6b642a3c22ed1f13d82b149a80185ab5c
ParangBird <seekerman@naver.com>
2022-11-01 01:36:06 +0900

    feat: 각 user에 대한 점수를 갖고 있는 Map<String, Integer> getScoreMap() 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class Problem7 {
 
+    public static Set<String> allUserSet;
+    public static Map<String, List<String>> friendGraph;
+    public static List<String> myFriends;
+
     /**
      * @param user     : 주어진 사용자 이름 string
      * @param friends  : friends 관계가 나타내져 있는 String[][]
@@ -48,6 +52,45 @@ public class Problem7 {
             }
         }
         return friendGraph;
+    }
+
+    /**
+     * @param user     : 유저 이름 String
+     * @param visitors : 방문자 목록 String
+     * @return Map<String, Integer> : 해당하는 유저 String에 대한 user의 점수
+     */
+    public static Map<String, Integer> getScoreMap(String user, List<String> visitors) {
+        Map<String, Integer> scoreMap = new TreeMap<>();
+        // 모든 유저의 친구관계를 살펴본다
+        for (String userName : allUserSet) {
+            // 자기 자신에 대한 점수는 구하지 하지 않음
+            if (userName.equals(user)) {
+                continue;
+            }
+            // 해당 유저의 친구들을 살펴보는데
+            List<String> userFriends = friendGraph.get(userName);
+            for (String myFriend : myFriends) {
+                // user의 친구들과 겹치는 것이 있고, 해당하는 유저 점수가 아직 0점일 때
+                if (userFriends != null && userFriends.contains(myFriend)) {
+                    if (scoreMap.get(userName) == null) {
+                        scoreMap.put(userName, 10);
+                    } else { // 이미 점수가 존재할 때
+                        int prevScore = scoreMap.get(userName);
+                        scoreMap.put(userName, prevScore + 10);
+                    }
+                }
+            }
+        }
+        // 방문자들에 대한 연산
+        for (String visitName : visitors) {
+            if (scoreMap.get(visitName) == null) {
+                scoreMap.put(visitName, 1);
+            } else {
+                int prevScore = scoreMap.get(visitName);
+                scoreMap.put(visitName, prevScore + 1);
+            }
+        }
+        return scoreMap;
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
