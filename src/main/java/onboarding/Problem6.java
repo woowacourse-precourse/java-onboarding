9edbc2eb08e38609cdaf4f9043eee2b33db0666f
WonjunS <seowj0710@gmail.com>
2022-11-01 01:27:12 +1100

    Problem 6: 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Problem6 {
 
-    static Map<String, Set<Integer>> mymap = new HashMap<>();
+    static Map<String, Integer> mymap = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
         return pro(forms);
@@ -25,20 +25,23 @@ public class Problem6 {
     }
 
     static List<String> getEmailList(List<List<String>> forms) {
-        Set<Integer> indexSet = new HashSet<>();
-        for(Set<Integer> set : mymap.values()) {
-            if(set.size() > 1) {
-                indexSet.addAll(set);
-            }
-        }
+        List<String> emailList = new ArrayList<>();
 
-        ArrayList<String> emailList = new ArrayList<>();
-        for(int idx : indexSet) {
-            emailList.add(forms.get(idx).get(0));
+        for(List<String> form : forms) {
+            String nickname = form.get(1);
+            String email = form.get(0);
+            for(int i = 0; i < nickname.length() - 1; i++) {
+                String str = nickname.substring(i, i + 2);
+                if(mymap.get(str) > 1) {
+                    emailList.add(email);
+                    break;
+                }
+            }
         }
         return emailList;
     }
 
+    // 문자열을 두 글자씩 조합
     static void addTwoLetters(String nickname, int idx) {
         for(int i = 0; i < nickname.length() - 1; i++) {
             String s = nickname.substring(i, i + 2);
@@ -46,12 +49,9 @@ public class Problem6 {
         }
     }
 
-
+    // 닉네임에 특정 문자열이 포함되이 있는 경우에 크루의 인덱스를 추가
     static void add(String letters, int idx) {
-        if(!mymap.containsKey(letters)) {
-            mymap.put(letters, new HashSet<>());
-        }
-        mymap.get(letters).add(idx);
+        mymap.put(letters, (mymap.getOrDefault(letters, 0) + 1));
     }
 
 }
