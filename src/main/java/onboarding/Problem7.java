b879227acd5af9a66ca80ab6495912937e406b7f
Kijung Park <kjpark4321@naver.com>
2022-11-01 01:29:12 +0900

    7번: 공통친구 점수 계산 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,8 +6,29 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-        makeFriendsGraph(friends);
+        Map<String, List<String>> friendsMap = makeFriendsGraph(friends);
+        Map<String, Integer> scoreMap = new HashMap<>();
+
+
+        getScoreSameFriends(friendsMap, user, scoreMap);
+        System.out.println(friendsMap);
+        System.out.println(scoreMap);
         return answer;
+    }
+
+    private static void getScoreSameFriends(Map<String, List<String>> friendsMap, String target, Map<String, Integer> scoreMap){
+        List<String> targetFriends = friendsMap.get(target);
+
+        for(Map.Entry<String, List<String>> entry : friendsMap.entrySet()){
+            if (entry.getKey().equals(target)){
+                continue;
+            }
+
+            List<String> friendFriendShip = entry.getValue();
+            ArrayList<String> intersect = new ArrayList<>(targetFriends);
+            intersect.retainAll(friendFriendShip);
+            scoreMap.put(entry.getKey(), 10 * intersect.size());
+        }
     }
 
     private static Map<String, List<String>> makeFriendsGraph(List<List<String>> friends){
@@ -21,7 +42,7 @@ public class Problem7 {
             relationship.add(relation.get(0));
             friendsMap.put(relation.get(1),relationship);
         }
-        
+
         return friendsMap;
     }
 }
