3ecab4ebccd28fb3f28d886ae924bfaff9163835
JungWoo <rlawjddn103@naver.com>
2022-10-31 16:24:35 +0900

    feat: Add Problem6 getDuplicatedEmail function and return answer

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,41 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        return getDuplicatedEmail(forms);
+    }
+
+    public static List<String> getDuplicatedEmail(List<List<String>> forms){
+        Map<String,String> hashMap = new HashMap<>();
+        HashSet<String> emails = new HashSet<>();
+
+        for (List<String> form : forms) {
+            String name = form.get(1);
+
+            if (name.length() < 2) {
+                break;
+            }
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                String key = name.substring(j, j + 2);
+                if (hashMap.containsKey(key)) {
+                    String email = hashMap.get(key);
+                    if (!email.equals(form.get(0))) {
+                        emails.add(email);
+                        emails.add(form.get(0));
+                    }
+                }
+                hashMap.put(key, form.get(0));
+            }
+        }
+
+        return emails.stream()
+                .sorted().collect(Collectors.toList());
     }
 }
