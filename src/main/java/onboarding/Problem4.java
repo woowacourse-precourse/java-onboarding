cecf34d4ab59327bcfd7b56e99d2e72b4e08a422
bumwoo <gld55602@gmail.com>
2022-10-31 15:21:28 +0900

    feat(Problem4): 문자열 변환 기능 구현
    빈칸이거나 알파벳이 아닐 경우 그대로 return
    아스키코드를 활용하여 대문자, 소문자를 분리해서 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	private static final int upperNum = 155;
+	private static final int lowerNum = 219;
+
+	public static String solution(String word) {
+		String answer = "";
+		return answer;
+	}
+
+	private static char getReverse(char c) {
+		if (!Character.isAlphabetic(c)) {
+			return c;
+		}
+		if (Character.isUpperCase(c)) {
+			return (char)(upperNum - c);
+		}
+		if (Character.isLowerCase(c)) {
+			return (char)(lowerNum - c);
+		}
+
+		throw new IllegalArgumentException("잘못된 입력입니다");
+	}
 }
