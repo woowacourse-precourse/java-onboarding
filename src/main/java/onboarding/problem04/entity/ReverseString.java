5a6992413e030f86ab7bd41ac3b87623d7325b18
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-10-31 22:39:44 +0900

    feat : problem04

--- src/main/java/onboarding/problem04/entity/ReverseString.java
+++ src/main/java/onboarding/problem04/entity/ReverseString.java
@@ -4,35 +4,71 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import onboarding.problem04.infra.exception.StringLengthOutOfRange;
+import onboarding.problem04.infra.util.ConsoleOut;
 
 public class ReverseString {
 
-  private List<Character> letterSet = new ArrayList<>();
-  private String result = "";
+  private char[] smallLetter = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+  private char[] capitalLetter = "abcdefghijklmnopqrstuvwxyz".toUpperCase().toCharArray();
+  private List<Character> letterSet;
+  private StringBuilder result = new StringBuilder();
 
   private ReverseString(String givenString) {
     if (givenString.length() < 1 && givenString.length() > 1000) {
       throw new StringLengthOutOfRange();
     }
-    this.result = result;
+    letterSet = givenString.chars().mapToObj(index -> (char) index)
+        .collect(Collectors.toList());
   }
 
   public static ReverseString from(String str) {
     return new ReverseString(str);
   }
 
-  private String processing(String str) {
-    letterSet = str.chars().mapToObj(index -> (char) index)
-        .collect(Collectors.toList());
+  public String processing() {
+    
     for (Character character : letterSet) {
-      if (character == ' ') {
-        continue;
-      } else if ((int) character >= 65 && (int) character <= 90) {
-        // 소문자 처리
-      } else if ((int) character >= 97 && (int) character <= 121){
-        // 대문자 처리
 
+      int temp = 0;
+      if (isSpace(character)) {
+        result.append(character);
       }
+
+      if (isSmallLetter((int) character)) {
+        for (int i = 0; i < smallLetter.length; i++) {
+          if (character == smallLetter[i]) {
+            temp = i;
+            break;
+          }
+        }
+      result.append(smallLetter[smallLetter.length - temp - 1]);
+      }
+
+      if (isCapitalLetter((int) character)) {
+        System.out.println(character);
+        for (int i = 0; i < capitalLetter.length; i++) {
+          if (character == capitalLetter[i]) {
+            temp = i;
+            break;
+          }
+        }
+        result.append(capitalLetter[capitalLetter.length - temp - 1]);
+      }
+
     }
+
+    return result.toString();
+  }
+
+  private boolean isCapitalLetter(int character) {
+    return character >= ConsoleOut.CAPITAL_LETTER_MIN && character <= ConsoleOut.CAPITAL_LETTER_MAX;
+  }
+
+  private boolean isSmallLetter(int character) {
+    return character >= ConsoleOut.SMALL_LETTER_MIN && character <= ConsoleOut.SMALL_LETTER_MAX;
+  }
+
+  private boolean isSpace(Character character) {
+    return character == ' ';
   }
 }
