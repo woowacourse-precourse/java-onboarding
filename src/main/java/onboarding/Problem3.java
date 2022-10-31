26d40d3e0c185654372bac5f5b0d766c387ce9be
swonny <wonny921@gmail.com>
2022-10-31 18:58:14 +0900

    feat: 숫자 3/6/9가 몇 개 포함되어 있는지 세는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,14 +1,23 @@
 package onboarding;
 
 public class Problem3 {
+    /* 숫자 3/6/9이 몇 개 포함되어 있는지 세는 메서드 */
+    public static int countNum(String number) {
+        int count = 0;
+        for(int i = 0; i < number.length(); i++) {
+            if(number.charAt(i) == '3' || number.charAt(i) == '6' || number.charAt(i) == '9') {
+                count++;
+            }
+        }
+        return count;
+    }
 
     public static int solution(int number) {
         int answer = 0;
         for(int i = 1; i < number + 1; i++) {
             String strNumber = Integer.toString(i);
             if(strNumber.contains("3") || strNumber.contains("6") || strNumber.contains("9")) {
-                System.out.println(strNumber);
-                answer += 1;
+                answer += countNum(strNumber);
             }
         }
         System.out.println(answer);
@@ -16,6 +25,6 @@ public class Problem3 {
     }
 
     public static void main(String[] args) {
-        solution(33);
+        solution(2);
     }
 }
