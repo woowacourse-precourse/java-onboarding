7080d5f8a00f0f159ee6ce3766c23226d858f499
code-tree <dltjdqls1200@naver.com>
2022-10-31 21:47:15 +0900

    feat: 친구 관계 파싱

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,18 +1,36 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
+        HashMap<String, ArrayList<String>> friendship = new HashMap<>();
 
         if (!checkValidInput(user, friends, visitors)) {
             return Collections.emptyList();
         }
 
+        connectFriends(friends, friendship);
+
         return answer;
+    }
+
+    private static void connectFriends(List<List<String>> friends, HashMap<String, ArrayList<String>> friendship) {
+        for (List<String> friend : friends) {
+            String userA = friend.get(0);
+            String userB = friend.get(1);
+
+            if (!friendship.containsKey(userA)) {
+                friendship.put(userA, new ArrayList<>());
+            }
+            if (!friendship.containsKey(userB)) {
+                friendship.put(userB, new ArrayList<>());
+            }
+
+            friendship.get(userA).add(userB);
+            friendship.get(userB).add(userA);
+        }
     }
 
     private static boolean checkValidInput(String user, List<List<String>> friends, List<String> visitors) {
