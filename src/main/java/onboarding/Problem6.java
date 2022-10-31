d1b2b618a0f422041650412a88f4672f83c63a24
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:44:35 +0900

    6번: 패턴 정규식 이용하여 서브스트링과 대상 스트링 매치하는지 판별하는 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,20 @@
 package onboarding;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Problem6 {
 
     private static final String DOMAIN = "@email.com";
-    
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         return answer;
+    }
+
+    private static boolean checkDuplicate(String targetSubNickName, String otherNickName){
+        String regex = ".*" + targetSubNickName + ".*";
+        return Pattern.matches(regex, otherNickName);
     }
 }
