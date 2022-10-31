4a5c9c22e5c3220530df420a588d9c8652ae3db9
meunrud <y3a0522@gmail.com>
2022-10-31 12:51:28 +0900

    리펙토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,52 +1,55 @@
 package onboarding;
-
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
-        answer = chage(word);
+        char [] lowAlpha = arrlowAlpha();
+        char [] upperAlpha = arrUpperAlpha();
+
+        HashMap<Character,Character> arrReverseAlhpa = reverseAlpha(lowAlpha,upperAlpha);
+
+        answer = reverseChange(arrReverseAlhpa,word);
 
         return answer;
     }
 
-    // ( '0' = 48, 'A' ~ 'Z' = 65 ~ 90, 'a' ~ 'z' = 97 ~ 122)
-   public static String chage(String word){
-       HashMap<Character, Character> map1 = new HashMap<>();
-       StringBuilder b = new StringBuilder();
+   public static char[] arrlowAlpha(){
        char [] lowChar = new char[26];
-       char [] upperChar = new char[26];
 
        for(int i = 0; i<lowChar.length; i++){
            lowChar[i] = (char)(97+i);
-
        }
+       return lowChar;
+   }
+
+   public static char[] arrUpperAlpha(){
+       char [] upperChar = new char[26];
        for(int i = 0; i<upperChar.length; i++){
            upperChar[i] = (char)(65+i);
-       }
+       }return upperChar;
+   }
 
-       for(int i = 0; i<lowChar.length; i++){
-           map1.put(lowChar[i],lowChar[25-i]);
-       }
-       for(int i = 0; i<upperChar.length; i++){
-           map1.put(upperChar[i],upperChar[25-i]);
-       }
+   public static HashMap<Character,Character> reverseAlpha(char[] low, char[] upper){
+       HashMap<Character, Character> reverseAlpha = new HashMap<>();
+       for(int i = 0; i<low.length; i++) {
+           reverseAlpha.put(low[i], low[25 - i]);
+           reverseAlpha.put(upper[i], upper[25 - i]);
+       }return reverseAlpha;
+   }
+
+   public static String reverseChange(HashMap<Character, Character> reverseAlpha, String word){
+       StringBuilder newWord = new StringBuilder();
        for(int i = 0; i<word.length(); i++){
-           if(word.charAt(i) != ' '){
-               char a = map1.get(word.charAt(i));
-               b.append(a);
-           }else {
-               b.append(word.charAt(i));
+           if(word.charAt(i) == ' '){
+               newWord.append(word.charAt(i));
            }
-       }return b.toString();
+           if(word.charAt(i) != ' '){
+               char a = reverseAlpha.get(word.charAt(i));
+               newWord.append(a);
+           }
 
-
-
-
-
-
-
+       }return newWord.toString();
    }
 }
