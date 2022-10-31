d585030da01767c22a83fc19b55dc015e740e1df
neppiness <0414kjh@naver.com>
2022-11-01 00:55:24 +0900

    Problem 2 Modified

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,13 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        int temp;
+        int temp, rest;
         for(int i = 1; i <= number; i++) {
             temp = i;
             while(temp != 0) {
-                if(temp % 10 == 3) answer++;
-                else if(temp % 10 == 6) answer++;
-                else if(temp % 10 == 9) answer++;
+                rest = temp % 10;
+                if(rest == 3 || rest == 6 || rest == 9) answer++;
                 temp /= 10;
             }
         }
