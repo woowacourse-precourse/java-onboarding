43f9c5934a641b30a9fc5fa4c2062032b73dc61b
seheonCho <whtpgjs94@gmail.com>
2022-10-31 23:32:05 +0900

    feat: [문제6] 이미 신청된 이름인지 파악할수 있는 변수 선언
    추후 이름으로 이메일을 찾아야해서 Map 타입으로 선언

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,13 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        Map<String, String> namesAlreadyApplied = new HashMap<>();
+
         List<String> answer = List.of("answer");
         return answer;
     }
