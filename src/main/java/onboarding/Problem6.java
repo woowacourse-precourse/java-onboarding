87a2a517400f68090efbce0e53ed359ec707d214
Soseung Soo <sss9073@naver.com>
2022-11-01 05:22:41 +0900

    'addToEmails', 'getDuplicatedEmails' function

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
@@ -8,8 +10,36 @@ public class Problem6 {
         return answer;
     }
 
+    public static HashSet<String> getDuplicatedEmails(List<List<String>>forms){
+        HashMap<String,String> partOfNameAndEmail = new HashMap<>();
+        HashSet<String> mailList = new HashSet<>();
+
+        for(int i = 0; i < forms.size(); i++){
+            String name = forms.get(i).get(1);
+            String email = forms.get(i).get(0);
+            for(int j = 0; j < name.length()-1; j++){
+                String partOfName = getPartOfName(name, j);
+                if(partOfNameAndEmail.containsKey(partOfName)){
+                    String targetEmail = partOfNameAndEmail.get(partOfName);
+                    if(!targetEmail.equals(email)){
+                        addToEmails(targetEmail,email,mailList);
+                    }
+                }
+                partOfNameAndEmail.put(partOfName,email);
+            }
+        }
+        return mailList;
+    }
+
     public static String getPartOfName(String name, int index){
         String partOfName = name.substring(index, index + 2);
         return partOfName;
+    }
+
+    public static HashSet<String> addToEmails(String targetEmail, String email, HashSet<String> mailList){
+        mailList.add(targetEmail);
+        mailList.add(email);
+
+        return mailList;
     }
 }
