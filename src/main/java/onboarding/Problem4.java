86d1dd7562270b7e4c231c06800d1a4b5accfb65
brotherstone97 <brotherstone97@gmail.com>
2022-11-01 00:15:51 +0900

    Feat: 문자가 알파벳일 경우에만 변환되도록 함

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -11,6 +11,15 @@ public class Problem4 {
 
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (Character.isLetter(c)) {
+                char res = changeCharacter(c);
+                answer += res;
+                continue;
+            }
+            answer += c;
+        }
         return answer;
     }
 }
