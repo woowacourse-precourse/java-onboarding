253afcc7bdc40be81c5f589b87e3b3661e5face3
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-10-31 17:29:36 +0900

    [pb7] feat: user의 친구들 set으로 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,11 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+
+    // 2개씩 parsing해서 set에다 다 넣어보자.
+    // 넣을때마다 교집합이 하나라도 있으면 걸린거다.
+    // 제이엠, 제이슨 (제이)
+    // 워니 , 워니맘 (워니)
+    // {제: [이], 이: [엠, 슨], 워: {니], 엠: {제]}
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Set<String> userFriends = new HashSet<>();
+        friends.forEach(elem -> getFriendsInSet(userFriends, user, elem.get(0), elem.get(1)));
+    }
+
+    private static void getFriendsInSet(Set<String> setFriends, String user, String friendA, String friendB) {
+        if (friendA.equals(user)) {
+            setFriends.add(friendB);
+        }
+        if (friendA.equals(user)) {
+            setFriends.add(friendB);
+        }
     }
 }
+
+//A
+//
+//B C D
+//
+//각 B C D의 친구들 다 list에 넣어서 elem마다 점수 10점씩 주는데
+//이때 elem이 A와 친구 set에 있으면 안주고 아니면 줌.
