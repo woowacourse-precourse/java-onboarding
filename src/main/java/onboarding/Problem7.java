670cd1442423d1b668add313f05377cb8cfa5f9c
subin Park <psb0320@hs.ac.kr>
2022-11-01 01:37:35 +0900

    feat: 모든 사용자의 아이디 & 점수 초기화 기능 추가 - 점수 0점으로 초기화

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // HashMap에 아이디 & 점수 초기화
+        HashMap<String, Integer> recommendFds = initRecommendFriends(friends, visitors);
+
         return answer;
+    }
+
+    public static HashMap<String, Integer> initRecommendFriends(List<List<String>> friends, List<String> visitors) {
+        HashMap<String, Integer> recommendFds = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            recommendFds.put(friend.get(0), 0);
+            recommendFds.put(friend.get(1), 0);
+        }
+        for (String visitor : visitors) recommendFds.put(visitor, 0);
+
+        return recommendFds;
     }
 }
