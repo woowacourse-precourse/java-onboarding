959405422d662c63c288cb342b5a48eeea8cb170
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:05:18 +0900

    [problem2] Cryptogram 생성자 추가

--- src/main/java/onboarding/problem2/Cryptogram.java
+++ src/main/java/onboarding/problem2/Cryptogram.java
@@ -1,9 +1,12 @@
 package onboarding.problem2;
 
 public class Cryptogram {
-    //StringBuilder
+    private StringBuilder cryptogram;
 
     //1.생성자, 문자열 입력받아서 초기화.
+    public Cryptogram(String input){
+        this.cryptogram = new StringBuilder(input);
+    }
 
     //2.자기자신 1회 돌아서 중복 문자 제거, 만약 한번이라도 제거되면 true반환
     //3.2번에서 중복인지 체크할 수 있는 조건함수
