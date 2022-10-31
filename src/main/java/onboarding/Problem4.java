185f78d62742492f9f9306f9bae6d94d95c81c64
채희찬 <eqholic1125@gmail.com>
2022-10-31 19:54:22 +0900

    feat: 실행 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -12,9 +12,12 @@ public class Problem4 {
     }
 
     private static String treeFrog(String word) {
-        String answer = "";
-
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int index = 0; index < word.length(); index++) {
+            int asciiLetter = word.charAt(index);
+            answer.append(convertLetter(asciiLetter));
+        }
+        return answer.toString();
     }
 
     private static char convertLetter(int asciiLetter) {
@@ -32,25 +35,17 @@ public class Problem4 {
         return convertedLetter;
     }
 
-    private static boolean isSpace(int asciiLetter) {
-        if (asciiLetter == 32) {
-            return true;
-        }
-        return false;
+    private static boolean isSpace(int asciiLetter) {   // 공백일 경우
+        return asciiLetter == 32;
     }
 
-    private static boolean isUpper(int asciiLetter) {
-        if (asciiLetter >= MIN_ASCII_UPPER_CASE && asciiLetter <= MAX_ASCII_UPPER_CASE) {   // 대문자일 경우
-            return true;
-        }
-        return false;
+    private static boolean isUpper(int asciiLetter) {   // 대문자일 경우
+
+        return asciiLetter >= MIN_ASCII_UPPER_CASE && asciiLetter <= MAX_ASCII_UPPER_CASE;
     }
 
-    private static boolean isLower(int asciiLetter) {
-        if (asciiLetter >= MIN_ASCII_LOWER_CASE && asciiLetter <= MAX_ASCII_LOWER_CASE) {   // 대문자일 경우
-            return true;
-        }
-        return false;
+    private static boolean isLower(int asciiLetter) {   // 소문자일 경우
+        return asciiLetter >= MIN_ASCII_LOWER_CASE && asciiLetter <= MAX_ASCII_LOWER_CASE;
     }
 
     private static char convertUpperCase(int asciiLetter) {
