d4ebb02ffbb32b6d1bb66a137bdb5c4ac63a028f
송민진 <m_6595@naver.com>
2022-11-01 02:58:29 +0900

    #3 feat: determine if it is a number to clap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,22 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int n = 1; n <= number; n++) {}
         return answer;
+    }
+
+    private static Boolean isClapNum(int number) {
+        while (number > 0) {
+            int eachNum = number % 10;
+            if (List.of(3, 6, 9).contains(eachNum)) {
+                return true;
+            }
+            number /= 10;
+        }
+        return false;
     }
 }
