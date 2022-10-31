74da3787837cfaa43502582697eac2217ec49e2b
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 16:15:11 +0900

    temp에 숫자 지정 숫자의 1의 자리에 3 6 9가 있으면 answer에 1씩 추가함, temp의 1의 자리를 버려 모든 자리수에서 3 6 9가 있는지 확인후 answer에 1씩 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,12 @@ public class Problem3 {
         int answer = 0;
 
         for (int i = 1; i <= number; i++) {
+            int temp = i;
+            while (temp != 0) {
+                if (temp%10 == 3 || temp%10 == 6 || temp%10 ==9) answer ++;
+                temp = temp/10;
 
+            }
         }
 
         return answer;
