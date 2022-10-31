7e5904df1c5192fef82fa43ca26097ab1c013c69
doehy <dbehxks02@naver.com>
2022-10-31 20:20:12 +0900

    feat : 점수를 얻는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,7 +6,19 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         if(!exceptionCheck(pobi) || !exceptionCheck(crong)){return -1;}
+
+        int pobi_score = getScore(pobi);
+        int crong_score = getScore(crong);
+
         return answer;
+    }
+    static int getScore(List<Integer> list) {
+        int left_page = list.get(0);
+        int right_page = list.get(1);
+        int score = Math.max(calculateSum(left_page),calculateSum(right_page));
+        score = Math.max(score,calculateMulti(left_page));
+        score = Math.max(score,calculateMulti(right_page));
+        return score;
     }
     static boolean exceptionCheck(List<Integer> list) {
         int left = list.get(0);
