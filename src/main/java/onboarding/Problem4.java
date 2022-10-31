ae80bc62ab4a6b4042ca7cb561ef8a4d89cbcf43
Limjongho <whdgh9873@icloud.com>
2022-10-31 17:34:52 +0900

    refactor(Problem4): 변수/메소드명 변경 , 메소드 구조/순서 수정, 띄어쓰기/개행 수정 등 컨벤션을 맞추기 위한 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,49 +5,41 @@ import java.util.Map;
 
 public class Problem4 {
     public static String solution(String word) {
-        StringBuilder makingConvertedString = new StringBuilder();
+        StringBuilder convertedString = new StringBuilder();
         ConvertMap convertMap = new ConvertMap(getConvertMap());
 
         char[] letters = word.toCharArray();
-        for (char token : letters) {
-            addTokenToMakingConvertedString(makingConvertedString, convertMap, token);
-        }
-        String convertedString = makingConvertedString.toString();
-
-        return convertedString;
-    }
-
-    private static void addTokenToMakingConvertedString(StringBuilder makingConvertedString, ConvertMap convertMap, char token) {
-        if (Character.isAlphabetic(token)) {
-            char convertedLetter = convertMap.getConvertedLetter(token);
-            makingConvertedString.append(convertedLetter);
-            return ;
+        for (char letter : letters) {
+            addConvertedAlphabet(convertedString, convertMap, letter);
         }
 
-        makingConvertedString.append(token);
+        return convertedString.toString();
     }
 
+    private static Map<Character, Character> getConvertMap() {
+        Map<Character, Character> convertMap = new HashMap<>();
+        convertMap.putAll(getUpperCaseConvertMap());
+        convertMap.putAll(getLowerCaseConvertMap());
 
-    static class ConvertMap{
-        private final Map<Character, Character> convertMap;
-
-        public ConvertMap(Map<Character, Character> convertMap) {
-            this.convertMap = convertMap;
-        }
-
-        public char getConvertedLetter(char letter) {
-            return convertMap.get(letter);
-        }
+        return convertMap;
     }
 
-    private static char increaseKeyCharacter(char key) {
-        char increasedKeyCharacter = (char) (key + 1);
-        return increasedKeyCharacter;
+    private static Map<Character, Character> getUpperCaseConvertMap() {
+        Map<Character, Character> upperCaseConvertMap = new HashMap<>();
+        char upperCaseKey = 'A';
+        char upperCaseValue = 'Z';
+        makeConvertMap(upperCaseConvertMap, upperCaseKey, upperCaseValue);
+
+        return upperCaseConvertMap;
     }
 
-    private static char decreaseValueCharacter(char value) {
-        char decreasedValueCharacter = (char) (value - 1);
-        return decreasedValueCharacter;
+    private static Map<Character, Character> getLowerCaseConvertMap() {
+        Map<Character, Character> lowerCaseConvertMap = new HashMap<>();
+        char lowerCaseKey = 'a';
+        char lowerCaseValue = 'z';
+        makeConvertMap(lowerCaseConvertMap, lowerCaseKey, lowerCaseValue);
+
+        return lowerCaseConvertMap;
     }
 
     private static void makeConvertMap(Map<Character, Character> convertMap, char key, char value) {
@@ -59,33 +51,33 @@ public class Problem4 {
         }
     }
 
-    private static Map<Character, Character> getUpperCaseConvertMap() {
-        Map<Character, Character> upperCaseConvertMap = new HashMap<>();
-        char upperCaseKey = 'A';
-        char upperCaseValue = 'Z';
-
-        //making upperCaseConvertMap
-        makeConvertMap(upperCaseConvertMap,upperCaseKey,upperCaseValue);
-
-        return upperCaseConvertMap;
+    private static char increaseKeyCharacter(char key) {
+        return (char) (key + 1);
     }
 
-    private static Map<Character, Character> getLowerCaseConvertMap() {
-        Map<Character, Character> lowerCaseConvertMap = new HashMap<>();
-        char lowerCaseKey = 'a';
-        char lowerCaseValue = 'z';
-
-        //making lowerCaseConvertMap
-        makeConvertMap(lowerCaseConvertMap,lowerCaseKey,lowerCaseValue);
-
-        return lowerCaseConvertMap;
+    private static char decreaseValueCharacter(char value) {
+        return (char) (value - 1);
     }
 
-    private static Map<Character, Character> getConvertMap() {
-        Map<Character, Character> convertMap = new HashMap<>();
-        convertMap.putAll(getUpperCaseConvertMap());
-        convertMap.putAll(getLowerCaseConvertMap());
+    private static void addConvertedAlphabet(StringBuilder convertedString, ConvertMap convertMap, char letter) {
+        if (Character.isAlphabetic(letter)) {
+            char convertedAlphabet = convertMap.getConvertedAlphabet(letter);
+            convertedString.append(convertedAlphabet);
+            return;
+        }
 
-        return convertMap;
+        convertedString.append(letter);
+    }
+
+    static class ConvertMap {
+        private final Map<Character, Character> convertMap;
+
+        public ConvertMap(Map<Character, Character> convertMap) {
+            this.convertMap = convertMap;
+        }
+
+        public char getConvertedAlphabet(char alphabet) {
+            return convertMap.get(alphabet);
+        }
     }
 }
