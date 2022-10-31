80c77190f6f0fd746ee70d2a82bd7dd4027f5906
TaeHwan <kimta2hwan@gmail.com>
2022-11-01 02:23:22 +0900

    refactor(PROBLEM5): calculateChange()의 매개변수명 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,18 +5,16 @@ import java.util.List;
 
 public class Problem5 {
 
-    static final int[] monetary = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
 
-    static int calculateChange(int money, int count, int index) {
+    static int calculateChange(int totalMoney, int money, int count) {
         if (count == 0) {
-            return money;
+            return totalMoney;
         }
 
-        return money % (monetary[index] * count);
+        return totalMoney % (money * count);
     }
 }
