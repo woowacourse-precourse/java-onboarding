085ead5a81ebd4000308d81c1fde4020d7a21bd6
nayooniverse <kangna99@naver.com>
2022-11-01 05:57:50 +0900

    [feat] 연속하는 중복 문자 삭제 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String match = "[^a-z]"; // 알파벳 소문자가 아닌 정규식
+        //연속하는 두 문자 발생 시 스택에서 제거
+        Stack<Character> stack = new Stack<>();
+        for(char c : cryptogram.toCharArray()) {
+            if(!stack.empty() && stack.peek().equals(c)) stack.pop();
+            else stack.push(c);
+        }
+
+        String answer = stack.toString().replaceAll(match,"");
         return answer;
     }
 }
