2546aa1c506f9329d5032dbea9a159f4d4f079f6
yejin <kwonyj1022@naver.com>
2022-10-31 13:48:25 +0900

    Problem2 뼈대 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,23 +2,25 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        char[] cryptogramArr = cryptogram.toCharArray();
+
         try{
-            validation(cryptogram);
+            validation(cryptogramArr);
         }
         catch (IllegalArgumentException e) {
             e.getMessage();
         }
 
-        char[] cryptogramArr = cryptogram.toCharArray();
         String answer = deleteDuplicate(cryptogramArr);
         return answer;
     }
 
-    public static void validation(String str) {
+    public static void validation(char[] charArr) {
 
     }
 
     public static String deleteDuplicate(char[] charArr) {
 
+        return charArr.toString();g
     }
 }
