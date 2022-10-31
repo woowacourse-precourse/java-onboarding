67cc93a96d9be6101d50a2b8dfb334a542c07549
xi_jjun <rlawowns97@naver.com>
2022-11-01 00:19:37 +0900

    feat(Problem5): money에서 화폐단위로 바꿀 수 있는 최대 화폐 개수 카운팅하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,23 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    public static int countMaxNumberOfCurrencies(final CURRENCY UNIT, int money) {
+        return money / UNIT.getValue();
+    }
+
+    enum CURRENCY {
+        W_50000(50000), W_10000(10000), W_5000(5000),
+        W_1000(1000), W_500(500), W_100(100), W_50(50), W_10(10), W_1(1);
+
+        private final int value;
+
+        CURRENCY(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
 }
