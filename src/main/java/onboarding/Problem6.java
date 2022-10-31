c4bf1dc55942e272c4333dae7004a560ecb871ec
Jeonsoyeon <itso22@naver.com>
2022-11-01 01:32:13 +0900

    feat(Problem6): set생성 및 중복이름 찾는기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        TreeSet<String> answerSet = new TreeSet<>();
         return answer;
     }
+
+    public static boolean findSimilarNickname(String nickname, String comparedNickname) {
+        for (int i = 0; i < comparedNickname.length() - 1; i++) {
+            if (nickname.contains(comparedNickname.substring(i, i + 2))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
