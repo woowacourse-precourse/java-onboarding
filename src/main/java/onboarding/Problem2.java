68c4246f09f3e35b2bbfaabe482ac3aa637405af
jmjmjames <chungjm0711@gmail.com>
2022-11-01 08:23:22 +0900

    refactor: solve problem2 using regex

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,25 +2,16 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer;
-        StringBuilder sb = new StringBuilder(cryptogram);
+        String answer = "answer";
+        answer = decrypt(cryptogram);
+        return decrypt(cryptogram);
+    }
 
-        boolean isDuplicated = false;
-
-        while (!isDuplicated) {
-
-            isDuplicated = true;
-
-            for (int i = 0; i < sb.length() - 1; i++) {
-                char currChar = sb.charAt(i);
-
-                if (currChar == sb.charAt(i + 1)) {
-                    sb.delete(i, i + 2);
-                    isDuplicated = false;
-                }
-            }
+    private static String decrypt(String cryptogram) {
+        String decryptedStr = cryptogram.replaceAll("([a-z])\\1+", "");
+        if (cryptogram.length() == decryptedStr.length()) {
+            return cryptogram;
         }
-        answer = sb.toString();
-        return answer;
+        return decrypt(decryptedStr);
     }
 }
