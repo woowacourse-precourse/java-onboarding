ef4eed0ca64e240ec0fa36c448cc6a27ff8ae992
Judy <judyb0626@gmail.com>
2022-11-01 02:55:59 +0900

    feat(problem 4): 청개구리 사전 기반 단어 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        
+
         return answer;
+    }
+
+    private static char convert(char c) {
+        if (c >= 65 && c <= 90) {
+            return (char) (155 - c);
+        }
+        if (c >= 97 && c <= 122) {
+            return (char) (219 - c);
+        }
+        return c;
     }
 }
