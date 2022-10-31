dad14ec9eeeae4629bce19b43be89c6b9e90a9fe
Sangin <ksiisk99@gmail.com>
2022-10-31 16:42:42 +0900

    feat: problem3 - 일의 자리수가 3,6,9인지 확인하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return 0;
     }
+
+    private static int isNumber369(int number){
+        if(number==3 || number==6 || number==9){
+            return 1;
+        }
+        return 0;
+    }
+
 }
