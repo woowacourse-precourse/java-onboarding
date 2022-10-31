95c6260364b84f8da2df4b6924861239c814e44b
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 23:01:47 +0900

    feat: P2 solution 함수에서 작성한 함수들 호출하여 요구사항 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -34,7 +34,7 @@ public class Problem2 {
 
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> decryptionStack = removeDuplicate(cryptogram);
+        return convertString(decryptionStack);
     }
 }
