8a0c8c4a4da32248f1944c9e83f2a677fc228718
seonghoo1217 <seonghoo1217@naver.com>
2022-10-31 15:19:40 +0900

    refactor : Add a Exception method Eliminate try/catch and Add a answerList sorted on [Task 6]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,25 +10,26 @@ public class Problem6 {
     static List<String> answer = new ArrayList<>();
 
     public static List<String> solution(List<List<String>> forms) {
-        try {
-            checkCrewLimit(forms.size());
-
-            for (List<String> form:forms){
+        if (validation("crew_limit",forms.size())) {
+            for (List<String> form : forms) {
                 String email = form.get(0);
                 String nickname = form.get(1);
 
-                validationEmail(email);
-                validationNickname(nickname);
-                for(List<String> eqForm: forms){
-                    if (form.equals(eqForm)) continue;
+                if (validation("email_valid", email)
+                        &&
+                        validation("nickname_valid", nickname)) {
+                    for (List<String> eqForm : forms) {
+                        if (form.equals(eqForm)) continue;
 
-                    String eqNickname = eqForm.get(1);
-                    getEqNickNameBySubstring(nickname,eqNickname,email);
+                        String eqNickname = eqForm.get(1);
+                        getEqNickNameBySubstring(nickname, eqNickname, email);
+                    }
                 }
             }
-
-            return answer.stream().distinct().collect(Collectors.toList());
-        }catch (Exception e){
+            List<String> collect = answer.stream().distinct().collect(Collectors.toList());
+            Collections.sort(collect);
+            return collect;
+        }else {
             return new ArrayList<>();
         }
     }
@@ -42,20 +44,23 @@ public class Problem6 {
                 });
     }
 
-    public static void checkCrewLimit(int crew_count) throws Exception {
-        if (crew_count<1||crew_count>10000) throw new Exception("인원 제한 초과");
-    }
 
-    public static void validationEmail(String email) throws Exception {
-        int e_length = email.length();
-        if (e_length<11||e_length>=20) throw new Exception("Eamil Length must Be 11~19");
-        if (!email.matches("^[a-zA-Z0-9]+@email.com$")) throw new Exception("It's Wrong Format Email");
-    }
+    private static <T> boolean validation(String err_type,T err_param){
+        if (err_type.equals("crew_limit")&&err_param instanceof Integer) {
+            int crew_limit = (int) err_param;
+            return 0<crew_limit&&crew_limit<10001;
+        }
+        else if (err_type.equals("email_valid")&&err_param instanceof String){
+            String email = (String) err_param;
+            if (!email.matches("^[a-zA-Z0-9]+@email.com$")) return false;
+            return 10<email.length()&&email.length()<20;
+        }
+        else if (err_type.equals("nickname_valid")&&err_param instanceof String){
+            String nickname = (String) err_param;
+            if (!nickname.matches("^[가-힣]*$")) return false;
+            return 0<nickname.length()&&nickname.length()<20;
+        }
 
-    public static void validationNickname(String nickname) throws Exception{
-        int n_length = nickname.length();
-        if (!nickname.matches("^[가-힣]*$")) throw new Exception("Nickname Must be written Hangeul");
-        if (n_length<1||n_length>=20) throw new Exception("Nickname Length must Be 1~19");
+        return false;
     }
-
 }
