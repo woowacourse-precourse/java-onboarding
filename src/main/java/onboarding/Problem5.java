8a3e9ed82359403146c4eaa7546a26e7949f73e0
이정우 <acg6138@naver.com>
2022-11-01 00:26:03 +0900

    feat(Problem5): solution 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,15 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> moneys = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> answer = exchangeMoney(moneys, money);
         return answer;
     }
-    
+
     public static List<Integer> exchangeMoney(List<Integer> moneys, int money) {
         int idx = 0;
         int cnt = 0;
