805dff15119d6a56dd74b20a2e315d32f63d310d
gnoesnooj <306yyy@naver.com>
2022-10-31 13:51:48 +0800

    feat : 모든 사용자를 찾는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Set<String> uniqueUser = new HashSet<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        findUsers(friends, visitors);
         return answer;
+    }
+
+    private static void findUsers(List<List<String>> friends, List<String> visitors) {
+        for (String s : visitors) {
+            uniqueUser.add(s);
+        }
+
+        for (int i = 0; i < friends.size(); i++) {
+            uniqueUser.add(friends.get(i).get(0));
+            uniqueUser.add(friends.get(i).get(1));
+        }
     }
 }
