56759eb45460c657d49fc9cbaf39bdf6d2c6a5c1
saint6839 <saint6839@gmail.com>
2022-10-31 21:02:50 +0900

    feat(Problem7): user와 함께 친구인 사람들의 이름을 담은 리스트를 반환하는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> userFriends = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                addUserFriends(user, friend, userFriends);
+            }
+        }
+
+        return null;
+    }
+
+    private static void addUserFriends(String user, List<String> friend, List<String> userFriends) {
+        for (String s : friend) {
+            if (!s.equals(user)) {
+                userFriends.add(s);
+            }
+        }
     }
 }
