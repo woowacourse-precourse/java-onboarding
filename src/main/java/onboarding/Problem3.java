274874e8801f5fc511aae1e41dd90c6e909f2a65
June <68291395+june-777@users.noreply.github.com>
2022-10-31 17:45:03 +0900

    refactor: 내부용 메소드 private으로 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,18 +6,18 @@ public class Problem3 {
     	int answer = startGame(number);
         return answer;
     }
-    public static int startGame(int number) {
+    private static int startGame(int number) {
     	int totalClapNum = 0;
+    	
     	for(int i=1; i<=number; i++) {
     		totalClapNum += countAllThreeSixNine(i);
     	}
     	return totalClapNum;
     }
-
-	public static int countAllThreeSixNine(int num) {	
-		
+	private static int countAllThreeSixNine(int num) {	
 		int singleDigitNum = 0;
 		int cnt = 0;
+		
 		while(num!=0) {
 			singleDigitNum = num % 10;
 			if(singleDigitNum!=0 && singleDigitNum % 3 == 0) {
