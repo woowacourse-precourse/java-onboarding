f7e52807877b0370b9b8c7dbbd1da03080f5541e
SeolYoungKim <nasur4da@gmail.com>
2022-10-31 22:52:59 +0900

    refactor(P3): 369 게임 속도 개선 (String 이용 -> int 연산 이용)

--- src/main/java/onboarding/problem3/ThreeSixNineGame.java
+++ src/main/java/onboarding/problem3/ThreeSixNineGame.java
@@ -6,21 +6,22 @@ public class ThreeSixNineGame {
 
     public int do369(int number) {
         return IntStream.range(1, number + 1)
-                .mapToObj(String::valueOf)
-                .filter(this::contains369)
-                .mapToInt(this::count369)
+                .map(this::count369)
                 .sum();
     }
 
-    private boolean contains369(String number) {
-        return number.contains("3") || number.contains("6") || number.contains("9");
-    }
+    private int count369(int number) {
+        int count = 0;
 
-    private int count369(String number) {
-        return (int) IntStream.range(0, number.length())
-                .mapToObj(i -> number.charAt(i) - '0')
-                .filter(num -> (num == 3 || num == 6 || num == 9))
-                .count();
-    }
+        while (number > 0) {
+            int num = number % 10;
+            if (num == 3 || num == 6 || num == 9) {
+                count++;
+            }
 
+            number /= 10;
+        }
+
+        return count;
+    }
 }
