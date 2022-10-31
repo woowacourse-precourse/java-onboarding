2b393dd6e65da66251d02e504898fa93ad4e73bd
devEdok13 <dev.edok13@gmail.com>
2022-10-31 13:35:40 +0900

    feat: P02-deleteOverlap

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem2 {
@@ -16,5 +17,14 @@ public class Problem2 {
             if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) index.add(i);
         }
         return index;
+    }
+
+    //  연속되는 중복문자를 삭제하는 메소드
+    private static String deleteOverlap(StringBuilder stringBuilder, List<Integer> index) {
+        Collections.reverse(index);
+        for (int i : index) {
+            stringBuilder.delete(i, i + 2);
+        }
+        return stringBuilder.toString();
     }
 }
