8a0788f0803842306f45cb17777f5aa264d945cc
Moon-Tree <ellaya1012@gmail.com>
2022-11-01 00:31:58 +0900

    Problem3, if문으로 코드를 작성할 떄 보다 While문을 통해서 범위를 구현하는게 더 코드가 깔끔하다 .

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,13 +1,10 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        ArrayList<Integer> pobibook = new ArrayList<>(400);
-        ArrayList<Integer> crongbook = new ArrayList<>(400);
 
 
 
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,15 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
+        for(int i=1; i<=number; i++) {
+            int current = i;
+            while(1<= current && current <=10000) {
+                if(current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
+                    answer ++;
+                }
+                current /= 10;
+            }
+        }
         return answer;
     }
 }
