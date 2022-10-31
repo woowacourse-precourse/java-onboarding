4c11b2082b7e03ce9228f5ed34d58667dce1fec5
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 00:22:47 +0900

    [Add] Prob4 Funtion list
    + Function list

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+    // list : list for alphabet
+    // func : get char and switch char
+    // func : get string and convert string
     public static String solution(String word) {
         String answer = "";
         return answer;
