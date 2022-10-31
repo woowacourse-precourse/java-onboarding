3f409799ae90102204fe9144ad6ac665b846c068
SonMinGyu <littleking13@naver.com>
2022-10-31 21:20:56 +0900

    [REFACTOR] Problem6 subName hashMap 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,34 +7,41 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        // hash 구조로 이름:{파싱한 이름} ex: 제이엠: {제이, 이엠}
-        // 파싱한 이름은 전부 한 배열에 넣고 배열 for문 돌려서 중첩 원소있는거 찾고 그 원소로 hash 구조에서 검색
-        // 그리고 중첩 원소는 지우고 for문 계속
 
         HashMap<String, List<String>> nameHashMap = new HashMap<>();
-        List<String> totalSubName = new ArrayList<>();
 
         for(int i = 0; i < forms.size(); i++) {
             String name = forms.get(i).get(1);
-            List<String> sl = subNameList(name);
-            nameHashMap.put(name, sl);
-            totalSubName.addAll(sl);
+            nameHashMap = subNameList(nameHashMap, name);
         }
 
         return answer;
     }
 
-    public static List<String> subNameList(String name) {
+    public static HashMap<String, List<String>> subNameList(HashMap<String, List<String>> nameHashMap, String name) {
         String[] nameArray = name.split("");
-
-        List<String> subNameList = new ArrayList<>();
 
         int index = 1;
         while (index < nameArray.length) {
-            subNameList.add(nameArray[index - 1] + nameArray[index]);
+            String subName = nameArray[index - 1] + nameArray[index];
+            if(nameHashMap.containsKey(subName)) {
+                // subName의 hashMap value에 name이 이미 존재하지 않으면
+                if (!(nameHashMap.get(subName).contains(name))) {
+                    List<String> containSubName = nameHashMap.get(subName);
+                    containSubName.add(name);
+                    nameHashMap.put(subName, containSubName);
+                }
+            }
+            else {
+                // subName이 없으면 새로운 key, value 생성해서 hashMap에 추가
+                List<String> containSubName = new ArrayList<>();
+                containSubName.add(name);
+                nameHashMap.put(subName, containSubName);
+            }
+
             index += 1;
         }
 
-        return subNameList;
+        return nameHashMap;
     }
 }
