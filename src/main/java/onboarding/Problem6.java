73d1aaf07e0d231cdbd2e643bbbc3b72658ab6a0
KimYongJun <rladydwns129@naver.com>
2022-11-01 01:20:07 +0900

    feat(Problem6): Make Array & Store values

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
+
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem6 {
+    private static String[][] emailAndNickname;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        emailAndNickname = new String[forms.size()][2];
+
+        addToArray(forms);
+
+
         return answer;
+    }
+
+    private static void addToArray(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> l = forms.get(i);
+            String email = l.get(0);
+            String nickname = l.get(1);
+
+            emailAndNickname[i][0] = email;
+            emailAndNickname[i][1] = nickname;
+        }
     }
 }
