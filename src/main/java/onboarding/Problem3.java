03805db92586c8efb3425194df4265ed156ec15b
ddogga <hayeonlee60327@gmail.com>
2022-10-31 15:04:17 +0900

    문제3 3의 개수를 담을 배열과 number의 자리수를 구하는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int[] countList = new int[5];
+
         return answer;
+    }
+
+
+    //몇 자리수 인지 구하기
+    public static int getPlaceValue(int number) {
+
+        String num = Integer.toString(number);
+
+        int cnt = num.length();
+
+        // 1:1의 자리, 2:10의 자리 3:100의 자리 4:1000의 자리 5:10000의 자리
+        return cnt;
     }
 }
