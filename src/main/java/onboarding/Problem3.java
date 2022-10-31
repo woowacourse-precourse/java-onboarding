3d5f7e43ad03f4a6769a6018fe1195ac749cc0bb
Songyi Kim <ksl2950@naver.com>
2022-10-31 09:38:21 +0900

    feat/3: 숫자 순회 로직 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int cnt = 0;
+        for (int i = 1; i <= number; i++) {
+            String num = String.valueOf(i);
+            for (int j = 0; j < num.length(); j++) {
+                int n = num.charAt(j);
+                if (isClapNumber(n))
+                    cnt+=1;
+            }
+        }
 
         return cnt;
     }
