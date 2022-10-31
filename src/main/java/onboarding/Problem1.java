ec30d6b03a2f10a184fc7c8d65709f5f756663d8
h0ngwon <w___on@naver.com>
2022-10-31 20:15:59 +0900

    예외처리 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,12 +13,19 @@ class Problem1 {
 
     }
 
-    public static int getMaxScore(int left, int right) { //
+    public static int getMaxScore(int left, int right) { // 곱한값과 더한값 중 더 큰 값 가져오는 기능
         if(!isValidate(left, right)) {
             return -1;
         }
 
         return Math.max(score(left, right));
     }
+    public static boolean isValidate(int left, int right) {
+        if(left % 2 == 1 && right % 2 == 0 && left + 1 == right) return true;
+        else return false;
+    }
 
+    public static int score(int left, int right) {
+
+    }
 }
