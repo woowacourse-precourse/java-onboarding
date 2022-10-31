fcc2e5f4a63814778efd86e7f861f1689828a9f3
이지혁 <olzlgur@naver.com>
2022-10-31 23:41:25 +0900

    화폐 목록 반환 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] Money = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return countBill(money);
+    }
+    private static List<Integer> countBill(int money) {
+        List<Integer> bill = new ArrayList<>();
+        for (int m : Money) {
+            bill.add(money / m);
+            money %= m;
+        }
+        return bill;
     }
 }
