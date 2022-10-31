fab2cd6d4dd82468e1d619e11a93d755b82df394
phobyjun <phobyjun@gmail.com>
2022-11-01 00:50:56 +0900

    [PROBLEM7] ADD: FEATURE1. 사용자 친구 목록 생성 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,20 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<String> getFriendsOfUser(String user, List<List<String>> friends) {
+        List<String> friendsOfUser = new ArrayList<>(List.of());
+        for (List<String> relation : friends) {
+            String userA = relation.get(0);
+            String userB = relation.get(1);
+
+            if (userA.equals(user)) {
+                friendsOfUser.add(userB);
+            } else if (userB.equals(user)) {
+                friendsOfUser.add(userA);
+            }
+        }
+        return friendsOfUser;
     }
 }
