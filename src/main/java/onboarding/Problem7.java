b28129535dd9c3f71937575767701b6cf8e56059
yxnxx <jh4rgh0@gmail.com>
2022-11-01 04:11:24 +0900

    feat: Add Problem7 calculating relationship score

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,9 +5,16 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         HashMap<String, HashSet<String>> relation = new HashMap<>();
-        String friend_name;
+        HashMap<String, Integer> score = new HashMap<>();
         for (List<String> friend : friends) {
             findRelationship(relation, friend);
+        }
+
+        int count = 0;
+        for (String name : relation.keySet()) {
+            if(name.equals(user)) continue;
+            count = relationshipScore(relation.get(name), relation.get(user));
+            score.put(name, count*10);
         }
         //return answer;
     }
@@ -19,5 +26,12 @@ public class Problem7 {
         friend_list.add(friend2);
         friend_list = relation.getOrDefault(friend2, new HashSet<>());
         friend_list.add(friend1);
+    }
+    public static Integer relationshipScore(HashSet<String> friends, HashSet<String> userfriend) {
+        int count = 0;
+        for(String friend : friends) {
+            if(userfriend.contains(friend)) count++;
+        }
+        return count;
     }
 }
