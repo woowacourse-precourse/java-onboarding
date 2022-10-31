e85a7e14a7d08d60892a0a64887e647b45b381be
alswn <alswn9938@gmail.com>
2022-10-31 15:58:02 +0900

    [Q3] feat : add "1 ~ Number까지 반복하며 3 6 9 개수 더하는" 메소드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,19 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+        for (int num = 1; num <= number; num++) {
+            answer += threeSixNineCount(num);
+        }
         return answer;
     }
 
-    public static int threeSixNineCount(int number){
-        int count  = 0;
+    public static int threeSixNineCount(int number) {
+        int count = 0;
         int nowDigit = 0;
-        while(number > 0){
+        while (number > 0) {
             nowDigit = number % 10;
-            if(nowDigit == 3 || nowDigit == 6 || nowDigit == 9){
+            if (nowDigit == 3 || nowDigit == 6 || nowDigit == 9) {
                 count++;
             }
+            number /= 10;
         }
         return count;
     }
