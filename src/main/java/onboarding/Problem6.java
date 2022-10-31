faff1e65992d5f97f1919b9a40c6463f8d53caaf
ddogga <hayeonlee60327@gmail.com>
2022-10-31 23:18:59 +0900

    문제6 구현 기능 목록 장성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+
+
+
         return answer;
     }
+
+
+
 }
