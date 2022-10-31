6afb3b4a72b60f0f915cf483243faaa0ed188304
devEdok13 <dev.edok13@gmail.com>
2022-10-31 13:29:21 +0900

    feat: P01-findMaxValueInList

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,11 +1,17 @@
 package onboarding;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    //  List로 주어진 입력값에 대한 최대값 계산
+    private static int findMaxValueInList(List<Integer> input) {
+        return input.stream().map(Problem1::findMaxValueInNum).max(Integer::compareTo).orElseThrow(NoSuchElementException::new);
     }
 
     //  자릿수별 덧셈연산, 곱셈연산된 숫자 중 최대값 계산
