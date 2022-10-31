beabab0705987b2e38bb1b0db5b36afd72eae517
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-31 22:15:22 +0900

    221031 Feat: P6 - 기능3 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,24 +3,30 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
+    public static Map<String, String> formMap = new HashMap<>();
+    public static Set<String> resultSet = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>(); //List.of("answer");
+        List<String> answer; //List.of("answer");
 
         //1. 입력받은 모든 이름들을 리스트로 만든다.
         List<String> names = new ArrayList<>();
         for(int i = 0; i < forms.size(); i++){
             List<String> data = forms.get(i);
             names.add(data.get(1));
+            formMap.put(data.get(1), data.get(0));
         }
 
         for(int i = 0; i < names.size(); i++) {
             //2. 어떤 이름 문자열을 선택하여, 그 문자열의 모든 2자 이상 부분문자열을 만든다.
             List<String> subStrings = createSubString(names.get(i));
 
-            //3. 해당 부분문자열이 다른 이름들에 포함되어 있는지 확인한다.
-            //포함되어 있다면 그 계정의 이메일을 정답 리스트에 담고 다음 반복으로 넘어간다.
+            //3-1. 해당 부분문자열이 다른 이름들에 포함되어 있는지 확인한다.
             checkContent(names, subStrings, i);
         }
+
+        answer = new ArrayList<>(resultSet);
+
+        Collections.sort(answer);
 
         return answer;
     }
@@ -40,8 +46,16 @@ public class Problem6 {
         for(int i = 0; i < names.size(); i++){
             if(i == k)
                 continue;
-            for(int j = 0; j < subStrings.size(); j++){
-                //if(names.get(i).contains())
+            compareName(names, subStrings, i);
+        }
+    }
+
+    public static void compareName(List<String> names,List<String> subStrings, int k){
+        for(int j = 0; j < subStrings.size(); j++){
+            String name = names.get(k);
+            //3-2.포함되어 있다면 그 계정의 이메일을 정답 리스트에 담고 다음 반복으로 넘어간다.
+            if(name.contains(subStrings.get(j))){
+                resultSet.add(formMap.get(name));
             }
         }
     }
