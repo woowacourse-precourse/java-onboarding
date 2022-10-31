5dc480176f480a67cf1e002103161a5f6e8eb4ca
YuHayeon <youha0930@gmail.com>
2022-10-31 14:00:02 +0900

    test(Problem7): friends 입력 테스트
    길이가 범위에 맞지 않거나, friends의 원소의 길이가 2가 아닐 때
    user 아이디의 길이, 소문자 확인
    올바른 케이스 테스트

--- src/test/java/onboarding/Problem7Test.java
+++ src/test/java/onboarding/Problem7Test.java
@@ -3,6 +3,8 @@ package onboarding;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class Problem7Test {
@@ -32,5 +34,57 @@ class Problem7Test {
 
 		assertThrows(IllegalArgumentException.class, () ->
 			Problem7.InputValidator.isRightUserInput("qwertyuiopasdfghjklzxcvbnmqwertyuiop"));
+	}
+
+	@Test
+	void 올바른_friends_입력() {
+		boolean result = true;
+		List<List<String>> friends = List.of(
+			List.of("donut", "andole"),
+			List.of("donut", "jun"),
+			List.of("donut", "mrko"),
+			List.of("shakevan", "andole"),
+			List.of("shakevan", "jun"),
+			List.of("shakevan", "mrko")
+		);
+		assertThat(Problem7.InputValidator.isRightFriendsInput(friends)).isEqualTo(result);
+
+		friends = List.of(
+			List.of("donut", "andole"),
+			List.of("skdkasd", "asd"),
+			List.of("asd", "vdsaw")
+		);
+		assertThat(Problem7.InputValidator.isRightFriendsInput(friends)).isEqualTo(result);
+	}
+
+	@Test
+	void 잘못된_friends_입력() {
+		boolean result = true;
+		List<List<String>> friends = List.of(
+			List.of("232", "andole")
+		);
+		List<List<String>> finalFriends = friends;
+		assertThrows(IllegalArgumentException.class, () ->
+			Problem7.InputValidator.isRightFriendsInput(finalFriends));
+
+		friends = List.of(
+			List.of("한글", "andole"),
+			List.of("sdsd", "andole")
+		);
+		List<List<String>> finalFriends1 = friends;
+		assertThrows(IllegalArgumentException.class, () ->
+			Problem7.InputValidator.isRightFriendsInput(finalFriends1));
+
+		friends = List.of();
+		List<List<String>> finalFriends2 = friends;
+		assertThrows(IllegalArgumentException.class, () ->
+			Problem7.InputValidator.isRightFriendsInput(finalFriends2));
+
+		friends = List.of(
+			List.of("det", "andole", "abc")
+		);
+		List<List<String>> finalFriends3 = friends;
+		assertThrows(IllegalArgumentException.class, () ->
+			Problem7.InputValidator.isRightFriendsInput(finalFriends3));
 	}
 }
