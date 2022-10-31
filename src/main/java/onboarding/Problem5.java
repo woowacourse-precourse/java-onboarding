c778da495550a292ab211d73bc639bf91976de0a
kki4504 <rhkddlf9876@gmail.com>
2022-10-31 11:45:06 +0900

    feat : 주어진 돈 단위 리스트 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,18 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Problem5 {
+    static class Exchange {
+        static List<Integer> exchangeMoneyList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
