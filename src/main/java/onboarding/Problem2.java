44db88e23d0f882e1d9b18ce5d44ec74dee267fd
mikekang47 <gyeong0308@gmail.com>
2022-10-31 18:11:04 +0900

    Feat<problem2>: create chars variable

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,6 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        char[] chars = cryptogram.toCharArray();
     }
 }
