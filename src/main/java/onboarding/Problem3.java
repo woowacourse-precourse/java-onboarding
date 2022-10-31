545e347e41094faf4da22e8e25c85061c211a07b
AnTaeWook <antk7894@naver.com>
2022-10-31 21:46:39 +0900

    221031 : all problems are solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,10 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= number; i++) {
+            builder.append(i);
+        }
+        return builder.length() - builder.toString().replaceAll("[369]", "").length();
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        HashMap<Character, Character> dictionary = new HashMap<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            dictionary.put(c, (char) ('a' + 'z' - c));
+            dictionary.put(Character.toUpperCase(c), Character.toUpperCase((char) ('a' + 'z' - c)));
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (dictionary.containsKey(word.charAt(i))) {
+                builder.append(dictionary.get(word.charAt(i)));
+            } else {
+                builder.append(word.charAt(i));
+            }
+        }
+        return builder.toString();
     }
 }
