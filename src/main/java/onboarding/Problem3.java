f3f0de75292db0097ccf8e9624348369b7bb481c
gomljo <gomljo0307@naver.com>
2022-10-31 16:25:21 +0900

    feat(Problem3.java): 기능 #1 구현
    1부터 주어진 숫자까지의 정수 배열을 생성하여 반환하는 makeArray 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,14 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 class Utility{
+    final int startNumber=1;
+    int[] makeArray(int endNumber){
+        return IntStream.range(startNumber,endNumber).toArray();
+    }
     int[] divideEachDigit(int number) {
         int[] digitNum;
         digitNum = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
