33aa30b517fd05273a58cc0f36300e2d32ab37b1
Damgom <wisewish1@gmail.com>
2022-10-31 13:10:02 +0900

    Feat: add uppercase to alphabet map

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        String alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Map<String, String> alpMap = new HashMap<>();
+        addUppercaseToAlpMap(alp, alpMap);
+        return answer.toString();
+    }
+    private static void addUppercaseToAlpMap(String alp, Map<String, String> alpMap) {
+        for(int i = 0; i < alp.length(); i++) {
+            alpMap.put(String.valueOf(alp.charAt(i)),
+                    String.valueOf(alp.charAt(alp.length()-1-i)));
+        }
     }
 }
