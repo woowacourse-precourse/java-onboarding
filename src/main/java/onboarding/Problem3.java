6c5db04e00d3c101288fda56c13a543a21e953df
wndyd <wndyd@JUYONGui-MacBookAir.local>
2022-11-01 03:14:50 +0900

    prob4

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,7 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        int temp;
+        int digit;
         for(int i = 1; i <= number; i++) {
             if (i < 10) {
                 if (i == 9 || i == 6 || i == 3)
@@ -12,31 +12,31 @@ public class Problem3 {
             else if (i >= 10 && i < 100) {
                 if (i / 10 == 9 || i / 10 == 6 || i / 10 == 3)
                     answer++;
-                temp = i % 10;
-                if (temp == 9 || temp == 6 || temp == 3)
+                digit = i % 10;
+                if (digit == 9 || digit == 6 || digit == 3)
                     answer++;
             }
             else if (i >= 100 && i < 1000) {
                 if (i / 100 == 9 || i / 100 == 6 || i / 100 == 3)
                     answer++;
-                temp = i % 100;
-                if (temp / 10 == 9 || temp / 10 == 6 || temp / 10 == 3)
+                digit = i % 100;
+                if (digit / 10 == 9 || digit / 10 == 6 || digit / 10 == 3)
                     answer++;
-                temp = temp % 10;
-                if (temp % 10 == 9 || temp % 10 == 6 || temp % 10 == 3)
+                digit = digit % 10;
+                if (digit % 10 == 9 || digit % 10 == 6 || digit % 10 == 3)
                     answer++;
             }
             else {
                 if (i / 1000 == 9 || i / 1000 == 6 || i / 1000 == 3)
                     answer++;
-                temp = i % 1000;
-                if (temp / 100 == 9 || temp / 100 == 6 || temp / 100 == 3)
+                digit = i % 1000;
+                if (digit / 100 == 9 || digit / 100 == 6 || digit / 100 == 3)
                     answer++;
-                temp = temp % 100;
-                if (temp / 10 == 9 || temp / 10 == 6 || temp / 10 == 3)
+                digit = digit % 100;
+                if (digit / 10 == 9 || digit / 10 == 6 || digit / 10 == 3)
                     answer++;
-                temp = i % 10;
-                if (temp == 9 || temp == 6 || temp == 3)
+                digit = i % 10;
+                if (digit == 9 || digit == 6 || digit == 3)
                     answer++;
             }
         }
