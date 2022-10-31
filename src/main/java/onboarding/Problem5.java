85466da340b0bfd3f001571485968a9576d81de0
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-31 21:20:41 +0900

    feat: problem5 화폐 배열 만들기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int[] moneyArr;
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        createMoneyArr();
+        List<Integer> list = resultList(money);
+        return list;
+    }
+
+    /**
+     * 화폐 배열 만들기
+     */
+    private static void createMoneyArr() {
+        moneyArr = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     }
 }
