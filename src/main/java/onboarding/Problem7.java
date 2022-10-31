a81b249529e84dd904ce95443a9e288cc5d0e5fa
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-11-01 02:13:08 +0900

    feat : 친구 관계 설정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, List<String>> relations = new HashMap<>();
+        setRelations(friends, relations);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void setRelations(List<List<String>> friends, Map<String, List<String>> relations) {
+        for (List<String> friend : friends) {
+            List<String> friend0List = relations.getOrDefault(friend.get(0), new ArrayList<>());
+            List<String> friend1List = relations.getOrDefault(friend.get(1), new ArrayList<>());
+            friend0List.add(friend.get(1));
+            friend1List.add(friend.get(0));
+
+            relations.put(friend.get(0), friend0List);
+            relations.put(friend.get(1), friend1List);
+        }
     }
 }
