8aef16f98edf33e798352b9a24b8841ff7afe022
lhyeonj120 <isk04387@gmail.com>
2022-11-01 04:19:28 +0900

    [problem4] feat: 변환 문자열 구하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,19 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         char uppercaseAlphabetArr[] = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+        
+        for(int i = 0; i < word.length(); i++) {
+        	char c = word.charAt(i);
+        	
+        	if(c == ' ') {
+        		answer += c;
+        	} else if(Character.isUpperCase(c)) {
+        		answer += uppercaseAlphabetArr[getAlphabetIdx(c)];
+        	} else {
+        		answer += (char)(uppercaseAlphabetArr[getAlphabetIdx(c)] + 'a' - 'A');
+        	}
+        }
+        
         return answer;
     }
     
