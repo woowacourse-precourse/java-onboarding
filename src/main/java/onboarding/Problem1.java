2285ae105b28ce5b9c898ceae921fb0cf68c0e77
dooboocookie <bkh3030@gmail.com>
2022-10-31 22:22:30 +0900

    2번_리팩토링_원시값 감싸기를 적용하여 메소드 기능별로 나누는 리팩토링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import onboarding.p1.Pages;
-import onboarding.p1.Score;
-
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
@@ -14,6 +12,7 @@ class Problem1 {
             pobiPages = new Pages(pobi);
             crongPages = new Pages(crong);
         } catch (Exception e) {
+            //e.printStackTrace();
             return -1;
         }
 
@@ -42,3 +41,103 @@ class Problem1 {
 
 }
 
+
+class Pages {
+
+    private final List<Integer> pageList;
+
+    public Pages(List<Integer> list) {
+        validateLength(list);
+        validateStartEnd(list);
+        validateContinuity(list);
+        this.pageList = list;
+    }
+
+    private void validateLength(List<Integer> list) {
+        int length = list.size();
+        if (length != 2) {
+            throw new IllegalArgumentException("페이지 수는 2개여야 합니다.");
+        }
+    }
+
+    private void validateStartEnd(List<Integer> list) {
+        int left = list.get(0);
+        int right = list.get(1);
+        if (left <= 1) {
+            throw new IllegalArgumentException("첫 페이지는 펼 수 없습니다.");
+        }
+        if (right >= 400) {
+            throw new IllegalArgumentException("마지막 페이지는 펼 수 없습니다.");
+        }
+    }
+
+    private void validateContinuity(List<Integer> list) {
+        int left = list.get(0);
+        int right = list.get(1);
+        if (left != right - 1) {
+            throw new IllegalArgumentException("책에는 연속된 페이지만 있을 수 있습니다.");
+        }
+    }
+
+    public int findPage(int left_right) {
+        return this.pageList
+                .get(left_right);
+    }
+
+}
+
+
+class Score {
+
+    private final int score;
+
+    public Score(Pages pages) {
+        int maxScore = 0;
+        for (int i = 0; i < 2; i++) {
+            List<Integer> nums = getNum(pages.findPage(i));
+            int plusScore = plus(nums);
+            if (plusScore > maxScore) {
+                maxScore = plusScore;
+            }
+            int multipleScore = multiple(nums);
+            if (multipleScore > maxScore) {
+                maxScore = multipleScore;
+            }
+        }
+        this.score = maxScore;
+    }
+
+    //더하기
+    private int plus(List<Integer> nums) {
+        int score = 0;
+        for (Integer num : nums) {
+            score += num;
+        }
+        return score;
+    }
+
+    //곱하기
+    public int multiple(List<Integer> nums) {
+        int score = 1;
+        for (Integer num : nums) {
+            score *= num;
+        }
+        return score;
+    }
+
+    // 자리수 나누는 메소드
+    private List<Integer> getNum (int page) {
+        int n;
+        List<Integer> nums = new ArrayList<>();
+        while (page > 0) {
+            n = page % 10;
+            page /= 10;
+            nums.add(n);
+        }
+        return nums;
+    }
+
+    public int getMaxScore() {
+        return this.score;
+    }
+}
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,56 +1,93 @@
 package onboarding;
 
-import onboarding.p2.Cryptogram;
 
 public class Problem2 {
-
-
     public static String solution(String cryptogram) {
         Cryptogram input = new Cryptogram(cryptogram);
+        Cryptogram result = deduplication(input);
 
-        String answer = deduplication(cryptogram);
-        return answer;
+        return result.getCryptogram();
     }
 
     // 중복을 제거하는 재귀함수
-    public static String deduplication(String cryptogram) {
+    public static Cryptogram deduplication(Cryptogram cryptogram) {
+
+        String currString = cryptogram.getCryptogram();
+        int currLength = currString.length();
+
+        cryptogram.removeDuplication();
+        int newLength = cryptogram.getSize();
+
+        // 재귀 호출
+        if ((newLength < currLength) && (newLength != 0)) {
+            return deduplication(cryptogram);
+        }
+        return cryptogram;
+    }
+}
+
+
+class Cryptogram {
+
+    private String cryptogram;
+
+    public Cryptogram(String cryptogram) {
+        validateLength(cryptogram);
+        validateLowerCase(cryptogram);
+        this.cryptogram = cryptogram;
+    }
+
+    private void validateLength(String cryptogram){
         int length = cryptogram.length();
+        if (length == 0) {
+            throw new IllegalArgumentException("빈 문자열은 입력할 수 없습니다.");
+        }
+        if (length > 1000) {
+            throw new IllegalArgumentException("1000자 이하로 입력해주세요.");
+        }
+    }
+
+    private void validateLowerCase(String cryptogram) {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!Character.isLowerCase(cryptogram.charAt(i))) {
+                throw new IllegalArgumentException("소문자만 입력할 수 있습니다.");
+            }
+        }
+    }
+
+    public void removeDuplication() {
+        String temp = this.cryptogram;
+        int length = temp.length();
         StringBuilder sb = new StringBuilder();
-        // 문자열을 전체적으로 탐색하여 중복이 있는지 보는 FOR문
         for (int i = 0; i < length; i++) {
-            char c = cryptogram.charAt(i);
+            char c = temp.charAt(i);
             int next = i+1;
             int count = 0;
-            // 마지막 문자인지 확인
             if (i == length-1) {
                 sb.append(c);
                 break;
             }
-            // 해당 문자가 중복이 안될 때까지 체크
-            while (cryptogram.charAt(next) == c){
+            while (temp.charAt(next) == c){
                 count ++;
                 next++;
-                // 범위 벗어나는 지 체크
-                if (next == length) break;
+                if (next == length) {
+                    break;
+                }
             }
-            // 중복되는 문자가 아니라면 append
             if (count == 0) {
                 sb.append(c);
             }
-            // 중복되는 문자를 제거했을 경우 i 증가
             i += count;
         }
-        String nextCryptogram = sb.toString();
-        // 만약 문자가 줄어들었다면 더 줄일 필요가 있는지 확인해야함
-        // 재귀 호출
-        if (nextCryptogram.length() < length) {
-            // 재귀 호출한 메소드에서 return하는 값을 return
-            return deduplication(nextCryptogram);
-        }
-        // 더이상 줄일 필요가 없다면 해당 값을 return
-        return nextCryptogram;
+        this.cryptogram = sb.toString();
     }
 
+    public int getSize() {
+        return this.cryptogram.length();
+    }
 
+    public String getCryptogram() {
+        return this.cryptogram;
+    }
 }
 
