632a6718dac4e4d587051e585469f1557c4945ec
bumwoo <gld55602@gmail.com>
2022-10-31 20:50:54 +0900

    feat(Problem6): 닉네임 분할 기능 구현
    길이가 2인 문자열로 나눈 배열을 HashMap에 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String, List<String>> nicknamePartition = new HashMap<>();
+        for (List<String> form : forms) {
+            nicknamePartition.put(form.get(0), devideByTwo(form.get(1)));
+        }
+
+        
         return answer;
     }
+
+    private static List<String> devideByTwo(String s) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            result.add(s.substring(i, i + 2));
+        }
+        return result;
+    }
+
 }
