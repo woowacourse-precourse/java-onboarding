a6d1730e48055b514a88b166bbd8c94ae8821605
송민진 <m_6595@naver.com>
2022-11-01 03:27:03 +0900

    #3 feat: calculate clap counts when number contains 3, 6, 9

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,18 +5,24 @@ import java.util.List;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        for (int n = 1; n <= number; n++) {}
+
+        for (int n = 1; n <= number; n++) {
+            answer += getClapCnt(n);
+        }
         return answer;
     }
 
-    private static Boolean isClapNum(int number) {
+    private static int getClapCnt(int number) {
+        int clapCnt = 0;
+
+        // 각 자리수에 3, 6, 9가 들어가는 횟수 구하기
         while (number > 0) {
             int eachNum = number % 10;
             if (List.of(3, 6, 9).contains(eachNum)) {
-                return true;
+                clapCnt ++;
             }
             number /= 10;
         }
-        return false;
+        return clapCnt;
     }
 }
