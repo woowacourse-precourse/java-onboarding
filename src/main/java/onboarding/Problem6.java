5e434271001f28e71da613f2695fee1bdc8388f4
shinhn <rkdrpgml@naver.com>
2022-10-31 14:56:59 +0900

    Test: Problem6 테스트 코드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,39 +4,42 @@ import java.util.*;
 
 public class Problem6 {
 
-    private static void combine(Map<String, Integer> totalCombination, String nickname){
+    private static void combine(Set<String> totalCombination, String nickname){
 
         for (int i = 0; i < nickname.length()-1; i++) {
             String name = "";
             name += nickname.charAt(i);
             name += nickname.charAt(i+1);
 
-            totalCombination.put(name, 0);
+            totalCombination.add(name);
         }
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
+        Set<String> answer1 = new HashSet<>(); // 중복을 제거하기 위해 set 사용
 
         for (int i = 0; i < forms.size(); i++) {
             // 본인 닉네임을 제외한 닉네임들로 조합 생성
-            Map<String, Integer> totalCombination = new HashMap<>();
+            Set<String> totalCombination = new HashSet<>();
             for (int j = 0; j < forms.size(); j++) {
                 if(j != i) {
                     combine(totalCombination, forms.get(j).get(1));
                 }
             }
 
-            Map<String, Integer> userCombination = new HashMap<>();
+            Set<String> userCombination = new HashSet<>();
             combine(userCombination, forms.get(i).get(1));
 
-            for (String key : userCombination.keySet()) {
-                if (totalCombination.containsKey(key)) answer.add(forms.get(i).get(0));
+            for (String key : userCombination) {
+                if (totalCombination.contains(key)) answer1.add(forms.get(i).get(0));
             }
         }
 
-        Collections.sort(answer);
+        // Set을 List로 변경
+        List<String> answer2 =new ArrayList<String>(answer1);
+        // 정렬
+        Collections.sort(answer2);
 
-        return answer;
+        return answer2;
     }
 }
