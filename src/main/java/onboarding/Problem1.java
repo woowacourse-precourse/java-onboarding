5a5d9247343fca1382d08c80035b0955379929eb
nakalic <nakalic28@gmail.com>
2022-10-31 16:41:10 +0900

    refactor(Problem1): 원시값 포장

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,12 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+	static final int EXCEPTION_CASE = -1;
+	static final int DRAW_CASE = 0;
+	static final int POBI_CASE = 1;
+	static final int CRONG_CASE = 2;
+	static final int FIRST_PAGE = 1;
+	static final int LAST_PAGE = 400;
+
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
 		int answer = Integer.MAX_VALUE;
 
 		// 값 검증
 		if (validateNum(pobi) == -1 || validateNum(crong) == -1)
-			return -1;
+			return EXCEPTION_CASE;
 
 		// 포비와 크롱의 점수 구하기
 		int pobiScore = getScore(pobi);
@@ -22,12 +30,12 @@ class Problem1 {
 
 	// 올바르면 1, 이상하면 -1을 return
 	private static int validateNum(List<Integer> lst) {
-		if (lst.get(0) < 1 || lst.get(1) > 400)
-			return -1;
+		if (lst.get(0) < FIRST_PAGE || lst.get(1) > LAST_PAGE)
+			return EXCEPTION_CASE;
 		if (lst.get(0) % 2 == 0 || lst.get(1) % 2 == 1)
-			return -1;
+			return EXCEPTION_CASE;
 		if (lst.get(0) + 1 != lst.get(1))
-			return -1;
+			return EXCEPTION_CASE;
 
 		return 1;
 	}
@@ -79,10 +87,10 @@ class Problem1 {
 
 	private static int getWinner(int num1, int num2) {
 		if (num1 > num2)
-			return 1;
+			return POBI_CASE;
 		if (num1 < num2)
-			return 2;
-		// 같을 경우
-		return 0;
+			return CRONG_CASE;
+
+		return DRAW_CASE;
 	}
 }
