90e41db4f8406819f32a808139c0a0a54c1995b3
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-01 00:08:21 +0900

    feat(문제 6): 중복 확인된 이메일 리스트를 정렬해서 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,7 +6,23 @@ import java.util.regex.Pattern;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         if (!validation(forms)) return Collections.emptyList();
+        boolean[] duplicate = new boolean[forms.size()];
+        Set<String> totalSubNickNameSet = new HashSet<>();
+        Set<String> subNickNameSetFrom2 = new HashSet<>();
+        for (int i = 0; i < forms.size(); i++) {
+            int currTotalSize = totalSubNickNameSet.size();
+            int currSize = forms.get(i).get(1).length() - 1;
+            totalSubNickNameSet.addAll(subNicknameSet(forms.get(i).get(1)));
+            duplicate[i] = (totalSubNickNameSet.size() - currTotalSize) < currSize;
+            subNickNameSetFrom2.addAll(subNicknameSet(forms.get(i).get(1)));
+        }
+        duplicate[0] = (totalSubNickNameSet.size() - subNickNameSetFrom2.size()) < (forms.get(0).get(1).length() - 1);
         List<String> answer = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            if (duplicate[i]) {
+                answer.add(forms.get(i).get(0));
+            }
+        }
         answer.sort(Comparator.naturalOrder());
         return answer;
     }
@@ -38,5 +54,13 @@ public class Problem6 {
         if (nickname.length() == 0 || nickname.length() >= 20) return false;
         String regex = "^[ㄱ-ㅎ가-힣]*$";
         return Pattern.matches(regex, nickname);
+    }
+
+    static Set<String> subNicknameSet(String nickname) {
+        Set<String> subNicknameSet = new HashSet<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            subNicknameSet.add(nickname.substring(i, i + 2));
+        }
+        return subNicknameSet;
     }
 }
