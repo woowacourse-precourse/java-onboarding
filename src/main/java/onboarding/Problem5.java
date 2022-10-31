e0bc2e84ff96a50de37f917a91eedd9bd696d2a8
Fizioo0102 <a27313797@gmail.com>
2022-10-31 22:03:14 +0900

    feat: count_money()함수 작성
    필요한 지폐의 갯수를 세어주는 함수 작성.
    시작값 50000원을 start 변수로 지정하여 점차 줄여나가는 식으로 구현해주었다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,7 +12,27 @@ public class Problem5 {
     }
 
     //돈의 변환갯수를 계산해주는 함수
+    static int[] count_money(int money) {
+        int[] change = new int[9];
+        int coin = 0;
+        int start = 50000;
 
+        for (int i=0;i<9;i++){
+            if(zero_check(money/start)){
+                change[i] = money/start;
+                money = money-(change[i]*start);
+                start = coin_change(i,start);
+
+            }
+            else{
+                change[i] = 0;
+                start = coin_change(i,start);
+            }
+        }
+
+        return change;
+
+    }
     //돈의 액수 반환값이 0인지 아닌지 판별해주는 함수
     static boolean zero_check(int change){
         if(change == 0){
