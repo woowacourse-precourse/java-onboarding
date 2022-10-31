5d58fdfc0075285a14694ffe467d50163b1d7ee3
Han Jihye <haneh0714@gmail.com>
2022-10-31 15:57:22 +0900

    FEAT : Problem4 solution 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+
+            if(c >= 'a' && c <= 'z') { // 소문자인 경우
+                answer.append((char)('a' + 'z' - c));
+            } else if(c >= 'A' && c <= 'Z') { // 대문자인 경우
+                answer.append((char)('A' + 'Z' - c));
+            } else { // 알파벳 외의 문자인 경우
+                answer.append(c);
+            }
+        }
+        return answer.toString();
     }
 }
