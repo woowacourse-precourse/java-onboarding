10f0b69d3b122119c9f6cf6fb74fcbcf80431864
DDing77 <devseo2021@gmail.com>
2022-10-31 21:48:41 +0900

    [Problem2] feat : 스택에 문자열의 추가하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,28 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
+    // 스택에 문자열을 중복없이 추가하여 결과 리턴
+    public static String getCryptogram(String cryptogram) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<cryptogram.length(); i++) {
+            char cur = cryptogram.charAt(i);
+
+            if(stack.isEmpty()) {
+                stack.push(cur);
+                continue;
+            }
+
+
+            stack.push(cur);
+        }
+
+        return sb.toString();
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
