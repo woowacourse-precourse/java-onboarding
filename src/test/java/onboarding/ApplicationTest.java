783087d1d6bec3829ce5ce344dd8182f7ffb6dea
BuildTools <unconfigured@null.spigotmc.org>
2022-10-31 17:19:14 +0900

    원래 테스트케이스로 되돌림

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -67,13 +67,6 @@ class ApplicationTest {
             int result = 14;
             assertThat(Problem3.solution(number)).isEqualTo(result);
         }
-
-        @Test
-        void case3() {
-            int number = 1;
-            int result = 0;
-            assertThat(Problem3.solution(number)).isEqualTo(result);
-        }
     }
 
     @Nested
@@ -99,13 +92,6 @@ class ApplicationTest {
         void case2() {
             int money = 15_000;
             List<Integer> result = List.of(0, 1, 1, 0, 0, 0, 0, 0, 0);
-            assertThat(Problem5.solution(money)).isEqualTo(result);
-        }
-
-        @Test
-        void case3() {
-            int money = 9;
-            List<Integer> result = List.of(0, 0, 0, 0, 0, 0, 0, 0, 9);
             assertThat(Problem5.solution(money)).isEqualTo(result);
         }
     }
