10e860239b161022dd6b6177a27fda4407e38bf5
Han Jihye <haneh0714@gmail.com>
2022-10-31 15:57:05 +0900

    FEAT : Problem2 solution 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (true) {
+            if(cryptogram.length() < 2) break;
+
+            boolean check = false;
+            StringBuilder sb = new StringBuilder(""+cryptogram.charAt(0));
+            for(int i=1; i<cryptogram.length(); i++) {
+                char now = sb.charAt(sb.length()-1);
+                if(i < cryptogram.length() && now == cryptogram.charAt(i)) {
+                    check = true;
+                    while(i < cryptogram.length() && now == cryptogram.charAt(i)) i++;
+                    i--;
+                    sb.deleteCharAt(sb.length()-1);
+                } else {
+                    sb.append(cryptogram.charAt(i));
+                }
+            }
+            cryptogram = sb.toString();
+            if(!check) break;
+        }
+
+        return cryptogram;
     }
 }
