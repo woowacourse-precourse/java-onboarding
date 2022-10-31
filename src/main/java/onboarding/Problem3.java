fa4936507d2a157cb0c5b158f05f8dc6cd8c8300
WhoIsWoony <WhoIsWoony@gmail.com>
2022-10-31 17:36:46 +0900

    problem3: TODO-'기능1' complete

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+    private static boolean isDigit369(int digit){
+        return digit == 3 || digit == 6 || digit == 9;
+    }
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int allClap = 0;
+        return allClap;
     }
 }
