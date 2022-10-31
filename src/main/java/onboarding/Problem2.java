a91de9a4c4aeefa4bac9afc21e1874d73d922b37
JunginPark4364 <wjddlsee@gmail.com>
2022-10-31 18:18:21 +0900

    feat: delete consecutive duplicate characters from a given string
    - 주어진 문자열에서 중복되며 연속하는 글자 제거하는 기능추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,6 +13,33 @@ public class Problem2 {
         return password;
     }
 
+    public static String deleteDuplicate(String cryptogram, int size) {
+        Stack<Character> stack = new Stack<>();
+        String password = "";
+
+        for (int i = 0; i < size; i++) {
+            if (stack.isEmpty()) {
+                stack.push(cryptogram.charAt(i));
+                continue;
+            }
+
+            if (stack.peek() == cryptogram.charAt(i)) {
+                // 마지막 중복값의 위치 구하기
+                i = findlastDuplicateIndex(cryptogram, i);
+                stack.pop();
+                continue;
+            }
+
+            stack.push(cryptogram.charAt(i));
+        }
+
+        while (!stack.isEmpty()) {
+            password = stack.pop() + password;
+        }
+
+        return password;
+    }
+
     public static int findlastDuplicateIndex(String cryptogram, int index) {
         char target = cryptogram.charAt(index);
         while (index < cryptogram.length()) {
