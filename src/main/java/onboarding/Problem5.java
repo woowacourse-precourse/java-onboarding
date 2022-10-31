e1822a684757a509292dc34cb8888d01991d2aa2
WonSubSub <boxer1995@naver.com>
2022-10-31 13:26:34 +0900

    Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> money_list = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        for (int i = 0; i < money_list.toArray().length; i++) {
+            int count = money / money_list.get(i);
+            money -= count * money_list.get(i);
+            money_list.set(i, count);
+        }//for
+        answer = money_list;
         return answer;
     }
 }
