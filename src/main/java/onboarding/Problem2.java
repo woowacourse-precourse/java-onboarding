1bc763ae1730c0effa57644c4b63efbd7b92d2d7
Konahyeon <koo@inha.edu>
2022-11-01 04:15:29 +0900

    중복 확인 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem2 {
+    public static boolean isOverlap(String target) {
+        for (int i = 0; i < target.length() - 1; i++) {
+            if (target.charAt(i) == target.charAt(i+1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+/*    public static String removeOverlap(String target) {
+        String result = target;
+        for (int i = 0; i < result.length(); i++) {
+
+        }
+    }*/
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = cryptogram;
+        while(isOverlap(answer)) {
+            //중복 제거
+        }
+
         return answer;
     }
 }
