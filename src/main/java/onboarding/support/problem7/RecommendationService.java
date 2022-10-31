96435b2100eb5e4f67fc38fcd75ee6a127f81eaa
kariskan <zkfzpf56@naver.com>
2022-10-31 16:31:26 +0900

    refactor: 변수 및 함수명 변경

--- src/main/java/onboarding/support/problem7/RecommendationService.java
+++ src/main/java/onboarding/support/problem7/RecommendationService.java
@@ -8,19 +8,27 @@ public class RecommendationService {
     //함께 아는 친구
     private static final int ACQUAINTANCE_SCORE = 10;
     private static final int TIME_LINE_VISIT_SCORE = 1;
-    private final List<String> sortedFriends;
+    private final List<String> recommendationFriends;
     private final String user;
 
     public RecommendationService(List<List<String>> friends, String user, List<String> visitors) {
         this.user = user;
-        this.sortedFriends = new ArrayList<>();
 
-        Map<String, Set<String>> friendRelations = new HashMap<>();
+        this.recommendationFriends = new ArrayList<>();
+        Map<String, Set<String>> friendRelations = new FriendRelation(friends).getFriendRelations();
+
         Map<String, Integer> scores = new HashMap<>();
+        calculateScore(user, visitors, friendRelations, scores);
 
-        for (List<String> friend : friends) {
-            makeFriendRelation(friendRelations, friend);
+        List<Map.Entry<String, Integer>> sortedScores = sortList(scores);
+        for (Map.Entry<String, Integer> scoresEntry : sortedScores) {
+            this.recommendationFriends.add(scoresEntry.getKey());
         }
+    }
+
+    private void calculateScore(String user, List<String> visitors,
+                                Map<String, Set<String>> friendRelations, Map<String, Integer> scores) {
+
         for (Map.Entry<String, Set<String>> friendEntry : friendRelations.entrySet()) {
             calculateAcquaintanceScore(friendRelations, scores, friendEntry);
         }
@@ -29,25 +37,19 @@ public class RecommendationService {
         for (String visitor : visitors) {
             calculateTimelineVisitScore(scores, userFriendRelation, visitor);
         }
-
-        List<Map.Entry<String, Integer>> sortedScores = sortList(scores);
-
-        for (Map.Entry<String, Integer> scoresEntry : sortedScores) {
-            this.sortedFriends.add(scoresEntry.getKey());
-        }
     }
 
     private static List<Map.Entry<String, Integer>> sortList(Map<String, Integer> scores) {
-        return scores
-                .entrySet()
+        return scores.entrySet()
                 .stream()
                 .sorted(Comparator
-                        .comparing(Map.Entry<String, Integer>::getValue)
-                        .reversed().thenComparing(Map.Entry::getKey))
+                        .comparing(Map.Entry<String, Integer>::getValue).reversed()
+                        .thenComparing(Map.Entry::getKey))
                 .collect(Collectors.toList());
     }
 
-    private static void calculateTimelineVisitScore(Map<String, Integer> scores, Set<String> userFriendRelations, String visitor) {
+    private static void calculateTimelineVisitScore(Map<String, Integer> scores,
+                                                    Set<String> userFriendRelations, String visitor) {
         if (!userFriendRelations.contains(visitor)) {
             scores.merge(visitor, TIME_LINE_VISIT_SCORE, Integer::sum);
         }
@@ -64,14 +66,6 @@ public class RecommendationService {
         }
     }
 
-    private static void makeFriendRelation(Map<String, Set<String>> friendRelations, List<String> friends) {
-        String firstFriend = friends.get(0);
-        String secondFriend = friends.get(1);
-
-        friendRelations.computeIfAbsent(firstFriend, value -> new HashSet<>()).add(secondFriend);
-        friendRelations.computeIfAbsent(secondFriend, value -> new HashSet<>()).add(firstFriend);
-    }
-
     private boolean isNotUser(String name) {
         return !user.equals(name);
     }
@@ -80,7 +74,7 @@ public class RecommendationService {
         return usersFriends.contains(relatedFriend);
     }
 
-    public List<String> getSortedFriends() {
-        return sortedFriends;
+    public List<String> getRecommendationFriends() {
+        return recommendationFriends;
     }
 }
