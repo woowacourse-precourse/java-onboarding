56d6e639fb095dbc38ed3c6116ac8c4735b53f86
linda2927 <linda2927@naver.com>
2022-11-01 01:59:13 +0900

    Problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,44 @@
 package onboarding;
 
 public class Problem2 {
+    // main method for testing purposes
+    public static void main(String[] args) {
+        System.out.println(solution("bbbllllackk"));
+        System.out.println(solution("browoanoommnaon"));
+        System.out.println(solution("zyelleyz"));
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder result = new StringBuilder();
+        boolean duplicatesExist = false;
+
+        if (cryptogram.length() == 2) {
+            return cryptogram.charAt(0) != cryptogram.charAt(1) ? cryptogram : "";
+        }
+
+        int index = 1;
+        while (cryptogram.length() >=3 && index < cryptogram.length() - 1) {
+            boolean isNotDuplicateNext = cryptogram.charAt(index) != cryptogram.charAt(index+1);
+            boolean isNotDuplicatePrev = cryptogram.charAt(index - 1) != cryptogram.charAt(index);
+
+            if (isNotDuplicateNext && isNotDuplicatePrev) {
+                if (index == 1)
+                    result.append(cryptogram.charAt(0));
+
+                result.append(cryptogram.charAt(index));
+
+                if (index == cryptogram.length() -2)
+                    result.append(cryptogram.charAt(index + 1));
+
+            } else if (index == cryptogram.length() -2 && isNotDuplicateNext) {
+                result.append(cryptogram.charAt(index + 1));
+            } else {
+                duplicatesExist = true;
+            }
+            index++;
+        }
+        if (duplicatesExist) {
+            return solution(result.toString());
+        }
+        return result.toString();
     }
 }
