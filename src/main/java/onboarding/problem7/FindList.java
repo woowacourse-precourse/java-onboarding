7d917d166c3565768ddea4c4f4c179a2b81accb9
asas6978 <asas6978@naver.com>
2022-10-31 19:07:00 +0900

    fix(problem7): 친구의 친구 목록 생성 시 친구가 포함되는 오류 수정

--- src/main/java/onboarding/problem7/FindList.java
+++ src/main/java/onboarding/problem7/FindList.java
@@ -8,15 +8,31 @@ public class FindList {
         List<String> friendList = new ArrayList<>();
 
         for (List<String> ignored : friends) {
-            friendList.addAll(extractFriendList(user, friends));
+            friendList.addAll(extractFriendListByUser(user, friends));
         }
+        friendList = friendList.stream()
+                .distinct()
+                .collect(Collectors.toList());
 
-        friendList = friendList.stream().distinct().collect(Collectors.toList());
+        List<String> updateFriendList = new ArrayList<>();
+
+        for (String friendId : friendList) {
+            String updateFriend = deleteUserByFriend(friendId, friendList, friends);
+            updateFriendList.add(updateFriend);
+        }
+        friendList.addAll(updateFriendList);
+
+        friendList = friendList.stream()
+                .filter(Objects::nonNull)
+                .filter(id -> !id.equals(user))
+                .distinct()
+                .collect(Collectors.toList());
+
 
         return friendList;
     }
 
-    private static List<String> extractFriendList(String user, List<List<String>> friends) {
+    private static List<String> extractFriendListByUser(String user, List<List<String>> friends) {
         List<String> friendList = new ArrayList<>();
 
         for (List<String> friend : friends) {
@@ -30,30 +46,48 @@ public class FindList {
         return new ArrayList<>(friendList);
     }
 
+    private static String deleteUserByFriend(String friendId, List<String> friendList, List<List<String>> friends) {
+        String alreadyFriend = null;
+
+        for (List<String> friend : friends) {
+            alreadyFriend = friend.stream()
+                    .filter(id -> (friendList.contains(friendId) && !id.equals(friendId)))
+                    .collect(Collectors.joining());
+        }
+        
+        return alreadyFriend;
+    }
+
     public static List<String> findFriendOfFriendList(String user, List<String> userFriend, List<List<String>> friends) {
         List<String> friendOfFriendList = new ArrayList<>();
 
-        for (String userFriendId : userFriend) {
-            friendOfFriendList.addAll(extractFriendList(user, userFriendId, friends));
+        for (List<String> friend : friends) {
+            String friendOfFriend = extractFriendOfFriend(user, userFriend, friend);
+            friendOfFriendList.add(friendOfFriend);
         }
 
-        friendOfFriendList = friendOfFriendList.stream().distinct().collect(Collectors.toList());
+        friendOfFriendList = friendOfFriendList.stream()
+                .filter(Objects::nonNull)
+                .distinct()
+                .collect(Collectors.toList());
 
         return friendOfFriendList;
     }
 
-    private static List<String> extractFriendList(String user, String userFriendId, List<List<String>> friends) {
-        List<String> friendList = new ArrayList<>();
+    private static String extractFriendOfFriend(String user, List<String> userFriend, List<String> elementOfFriends) {
+        String friendOfFriend = null;
 
-        for (List<String> friend : friends) {
-            if (friend.contains(userFriendId) && !friend.contains(user)) {
-                friendList.add(friend.stream()
-                        .filter(id -> !id.equals(userFriendId))
-                        .collect(Collectors.joining()));
-            }
+        for (String friendId : userFriend) {
+            friendOfFriend = elementOfFriends.stream()
+                    .filter(f -> ((!f.equals(user)) && (!userFriend.contains(f) || f.equals(friendId))))
+                    .collect(Collectors.joining());
         }
 
-        return new ArrayList<>(friendList);
+        if (!elementOfFriends.contains(friendOfFriend)) {
+            friendOfFriend = null;
+        }
+
+        return friendOfFriend;
     }
 
     public static List<String> findOthersList(List<String> visitor, List<String> friend, List<String> friendOfFriend) {
