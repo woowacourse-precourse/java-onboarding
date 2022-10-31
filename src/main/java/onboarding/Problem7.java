72d0080f67153b2cad4c2ffe835edf0ed1546a23
bumwoo <gld55602@gmail.com>
2022-10-31 21:49:58 +0900

    feat(Problem7): 점수 획득 기능 구현
    사용자와 함께 아는 친구일 경우 +10
    방문한 적이 있는 경우 +1

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,7 +7,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+	public static final int defaultValue = 0;
+	public static final int friendOfFriendScore = 10;
+	public static final int visitedScore = 1;
+
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 		List<String> answer = Collections.emptyList();
 
 		Map<String, List<String>> friendList = new HashMap<>();
@@ -16,6 +20,25 @@ public class Problem7 {
 			friendList.computeIfAbsent(f.get(1), s -> new ArrayList<>()).add(f.get(0));
 		}
 
-        return answer;
-    }
+		Map<String, Integer> score = new HashMap<>();
+		List<String> friendOfUSeer = friendList.get(user);
+
+		for (String name : friendOfUSeer) {
+			for (String name2 : friendList.get(name)) {
+				if (name2.equals(user)) {
+					continue;
+				}
+				score.put(name2, score.getOrDefault(name2, defaultValue) + friendOfFriendScore);
+			}
+		}
+
+		for (String v : visitors) {
+			if (friendList.get(user).contains(v)) {
+				continue;
+			}
+			score.put(v, score.getOrDefault(v, defaultValue) + visitedScore);
+		}
+
+		return answer;
+	}
 }
