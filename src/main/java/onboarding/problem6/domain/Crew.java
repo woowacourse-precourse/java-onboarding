d1484c9fee8d3f675a745860ef4e43f553d66233
chanminmaxscaler <jcm04@naver.com>
2022-10-31 20:49:50 +0900

    feat(Crew): equals, hashCode 메소드를 오버라이드한다
    Crew 인스턴스들의 논리적 동일성을 판단하기 위해 equals, hashCode 를 오버라이드한다.
    email, nickname 이 같은 문자열을 갖는다면 논리적으로 동일하다.
    hashCode는 email, nickname을 활용한다.

--- src/main/java/onboarding/problem6/domain/Crew.java
+++ src/main/java/onboarding/problem6/domain/Crew.java
@@ -2,6 +2,7 @@ package onboarding.problem6.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Crew {
     private String email;
@@ -67,5 +68,18 @@ public class Crew {
             index++;
         }
         return duplicateStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Crew crew = (Crew) o;
+        return nickname.equals(crew.nickname) && email.equals(crew.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nickname, email);
     }
 }
