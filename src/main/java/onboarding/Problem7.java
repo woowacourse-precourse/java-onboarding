bfa6a9c74c089e5f9e8d5636b0385757f1b93d92
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-10-31 19:24:16 +0900

    Add : 전체 유저 정보 목록 <유저, 친구목록> map에 각 friends의 친구관계를 양방향으로 매핑해주는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    // 주어진 아이디A, B 의 친구관계를 친구 관계 목록 map 에 추가하는 함수
+    public static void setUserFriendsMap(List<String> friend, Map<String, Info> map){
+        String left = friend.get(0);
+        String right = friend.get(1);
+
+        if(!map.keySet().contains(left)){
+            map.put(left, new Info(0,new ArrayList<>()));
+        }
+        if(!map.keySet().contains(right)){
+            map.put(right, new Info(0,new ArrayList<>()));
+        }
+
+        map.get(left).knowing.add(right);
+        map.get(right).knowing.add(left);
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
