b1a0df29fcb138b0a8d4feaf05a46a455742318e
hongjinwon <herenever@naver.com>
2022-10-31 19:15:46 +0900

    feat: Problem1 validation 기능 추가
    - Problem1 validation() 기능 추가
    - 예외 케이스를 더 단순화 시킬 필요 있음

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,30 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static boolean validation(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        // case1) 페이지가 1~400 사이의 범위에 있지 않다.
+        if (leftPage < 1 || leftPage > 400 || rightPage < 1 || rightPage > 400){
+            return false;
+        }
+
+        // case2) 페이지가 1 또는 400이다.
+        if (leftPage == 1 || leftPage == 400 || rightPage == 1 || rightPage == 400){
+            return false;
+        }
+        // case3) 왼쪽 페이지가 홀수가 아니다.
+        if (leftPage % 2 != 1){
+            return false;
+        }
+
+        //case4) 페이지가 연속된 숫자가 아니다.
+        if (leftPage+1 != rightPage){
+            return false;
+        }
+
+        return true;
+    }
 }
