1cfefdc86ee1eb6a3371794ff24b6fcb5e993195
hyejin0662 <hyejin0776@gmail.com>
2022-11-01 01:11:01 +0900

    onboarding coding 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
 public class Problem4 {
+
+    private final static int UPPERCASE_SUM = 155;
+    private final static int LOWERCASE_SUM = 219;
+
     public static String solution(String word) {
-        String answer = "";
+        String answer = changeWord(word);
         return answer;
+    }
+
+    private static String changeWord(String word){
+        String reverse_Word = "";
+        for(char Alphabet : word.toCharArray()) {
+            if(Character.isUpperCase(Alphabet))
+                reverse_Word += UppercaseChange(Alphabet);
+            else if (Character.isLowerCase(Alphabet))
+                reverse_Word += LowercaseChange(Alphabet);
+            else
+                reverse_Word += " ";
+        }
+        return reverse_Word;
+    }
+    private static char UppercaseChange(int original){
+        char reverseAlphabet = (char)(UPPERCASE_SUM - original);
+        return reverseAlphabet;
+    }
+
+    private static char LowercaseChange(int original){
+        char reverseAlphabet = (char)(LOWERCASE_SUM - original);
+        return reverseAlphabet;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    private static int[] numbers = {50000,10000,5000,1000,500,100,50,10,1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for(int i = 0; i< numbers.length; i++){
+            int count = getOrder(money, i);
+            answer.add(count);
+            money -= count* numbers[i];
+        }
         return answer;
+    }
+
+
+    private static int getOrder(int money, int idx){
+        return money / numbers[idx];
     }
 }
