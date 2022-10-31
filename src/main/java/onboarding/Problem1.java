377bc188f0892d8e9f2533c72ae119e46c53d82e
Jeonsoyeon <itso22@naver.com>
2022-10-31 11:44:41 +0900

    feat(Problem1): 자신이 가질수 있는 모든 수 구하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+    public static int[] eachPageNum(int num) {
+        int[] arrNum = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+        return arrNum;
+    }
+    public static int plus(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+    public static int multiple(int[] arr) {
+        int multiple = 1;
+        for (int i = 0; i < arr.length; i++) {
+            multiple *= arr[i];
+        }
+        return multiple;
     }
 }
