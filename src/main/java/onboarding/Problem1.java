bc4aa3d90498481468fe74628f0e55683a5ad5a7
leewoojin <zmfpdl64@naver.com>
2022-10-31 14:52:19 +0900

    문제1번 제한사항 구현
    조건
    1. 왼쪽 페이지가 오른쪽 페이지보다 클 때
    2. 1페이지 400페이지를 벗어 났을 때
    3. 페이지의 차이가 1이 아닐 때
    
    결과 -1을 반환한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(Math.abs(pobi.get(0) - pobi.get(1)) != 1 || Math.abs(crong.get(0) - crong.get(1)) != 1) return -1;
+        if(valid(pobi, crong)) return -1;
         int pobi_max = Math.max(sum_max(pobi), mul_max(pobi));
         int crong_max = Math.max(sum_max(crong), mul_max(crong));
         if(pobi_max > crong_max) return 1;
@@ -44,6 +44,19 @@ class Problem1 {
             sum[i] = re;
         }
         return Math.max(sum[0], sum[1]);
+    }
+    public static Boolean valid(List<Integer> pobi, List<Integer> crong){ //조건에 맞지않으면 true
+        //1. 왼쪽 페이지가 오른쪽 페이지 보다 클 때
+        if(pobi.get(0) >= pobi.get(1) || crong.get(0) >= crong.get(1)){
+            return true;
+        }
+        //2. 1페이지 400페이지를 벗어났을 때
+        if(pobi.get(0) < 0 || crong.get(0) < 0 || pobi.get(1) > 400 || crong.get(1) > 400){
+            return true;
+        }
+        //3. 왼쪽 오른쪽 페이지의 차이가 1이 아닐때
+        if(Math.abs(pobi.get(0)-pobi.get(1)) != 1 || Math.abs(crong.get(0) -pobi.get(1)) != 10)
+            return true;
     }
 
     public static void main(String[] args) {
