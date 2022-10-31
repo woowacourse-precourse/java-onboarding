6a7acc9d2633e849914f17ea9f7b70014fc20370
hantae <hantae96@naver.com>
2022-10-31 09:55:24 +0900

    Problem5 solved

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
+        List<Integer> moneyList = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
+
+
+
+        for (int i = 0;i<moneyList.toArray().length;i++){
+            int div = moneyList.get(i);
+            answer.add(money/div);
+            money = money - (money/div)*div;
+
+        }
+
         return answer;
     }
 }
