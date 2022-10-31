8a62169ec31076177c1cf1607a1a04f21d916bf7
mardi2020 <mardi@khu.ac.kr>
2022-10-31 15:34:14 +0900

    feat: Problem6 필요한 자료구조 초기화

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> answer = new HashSet<>();
+        Set<String> duplicatedPattern = new HashSet<>();
+        HashMap<String, List<String>> patternEmail = new HashMap<>();
+
+
+        return null;
     }
 }
