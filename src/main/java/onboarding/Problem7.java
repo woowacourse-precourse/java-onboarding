07709cea220aa74cdbef077b937cc46137f615a5
leesangjin <tkdwls0628@gmail.com>
2022-10-31 20:36:18 +0900

    P7, 구현 기능목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,40 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    private static void initializeTotalUserSet(List<List<String>> friends, Set<String> totalUserSet) {
+
+    }
+
+    private static void initializeEachUserFriendMap(List<List<String>> friends, Set<String> totalUserSet,
+                                                    Map<String, List<String>> totalUsersFriends) {
+
+    }
+
+    private static void calculateRelationshipScore(String user,
+                                                   Map<String, List<String>> eachUserFriendMap, Map<String, Integer> userScore) {
+
+    }
+
+    private static void calculateVisitorScore(String user, List<String> visitors,
+                                              Map<String, List<String>> eachUserFriendMap, Map<String, Integer> userScore) {
+
+    }
+
+    private static List<String> sortScoreThenName(Map<String, Integer> userScore) {
+        return null;
+    }
+
+    private static void addMaximumFiveHighScore(List<String> answer, List<String> keyset) {
+
+    }
 }
+
