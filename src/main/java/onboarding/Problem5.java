48feb4e7196212ad7f373702d05c3fd434714dab
eugene1204 <s1061001@naver.com>
2022-10-31 14:16:37 +0900

    p5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,27 +11,27 @@ public class Problem5 {
         List<Integer> moneyList = new ArrayList<>();
 
 
-//        while(money>0){ // 50237
-//            moneyList.add(money%10); // 7 3 2 0 5
-//            money = money/10;
-//
-//        }
-//
-//        moneyTemp.add(moneyList.get(0)); // 일의 자리는 그냥 넣기
-//        for(int i =1; i<moneyList.size();i++) {
-//            int m = moneyList.get(i);
-//            if(m<5) { // 5가 안넘으면
-//                moneyTemp.add(m);
-//                moneyTemp.add(0);}
-//            else {//8 -> 1...3
-//                moneyTemp.add(m%5);
-//                moneyTemp.add(m/5);
-//            }
-//        }
-//        moneyList.clear();
-//        for(int i =moneyTemp.size()-1; i>-1;i--) {
-//            moneyList.add(moneyTemp.get(i));
-//        }
+        while(money>0){ // 50237
+            moneyList.add(money%10); // 7 3 2 0 5
+            money = money/10;
+
+        }
+
+        moneyTemp.add(moneyList.get(0)); // 일의 자리는 그냥 넣기
+        for(int i =1; i<moneyList.size();i++) {
+            int m = moneyList.get(i);
+            if(m<5) { // 5가 안넘으면
+                moneyTemp.add(m);
+                moneyTemp.add(0);}
+            else {//8 -> 1...3
+                moneyTemp.add(m%5);
+                moneyTemp.add(m/5);
+            }
+        }
+        moneyList.clear();
+        for(int i =moneyTemp.size()-1; i>-1;i--) {
+            moneyList.add(moneyTemp.get(i));
+        }
         return moneyList;
     }
 }
