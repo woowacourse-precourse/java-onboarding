dc8e084845752922d58b1a5deb2d838c2af4369f
210_reverof <sheltonwon@gmail.com>
2022-10-31 23:04:39 +0900

    feat: add function4 (solve Problem4)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        char[] wordArr = word.toCharArray();
+
+        for (char c: wordArr) {
+            char frogChar = c == ' ' ? ' ' : c <= 'Z' ? (char) (155 - c) : (char) (219 - c);    // 'A' + 'C' = 155, 'a' + 'c' = 219
+            answer.append(frogChar);
+        }
+
+        return answer.toString();
     }
 }
