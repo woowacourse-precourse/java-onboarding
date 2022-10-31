b070a5e7fca40eca02c38193c48975235d93fa32
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-31 21:29:56 +0900

    문제 6 : 기능 1 - 모든 두 사람 쌍을 비교하는 기능.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = i + 1; j < form.size(); j++) {
+
+            }
+        }
+
         List<String> answer = List.of("answer");
         return answer;
     }
