7cfaf49f85f45ed40abc0e53e5189f9c19b2980d
seheonCho <whtpgjs94@gmail.com>
2022-10-31 23:34:47 +0900

    feat: [문제6] 매개변수 forms 를 반복하여 신청서를 확인한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,9 +4,15 @@ import java.util.*;
 
 public class Problem6 {
 
+    private static final int EMAIL_INDEX_OF_FORM = 0;
+    private static final int NICKNAME_INDEX_OF_FORM = 1;
+
     public static List<String> solution(List<List<String>> forms) {
         Map<String, String> namesAlreadyApplied = new HashMap<>();
         Set<String> emails = new TreeSet<>();
+
+        for (List<String> form : forms) {
+        }
 
         List<String> answer = List.of("answer");
         return answer;
