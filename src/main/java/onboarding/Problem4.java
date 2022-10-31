de965502c1b10d71a149b0ab0d529944736d9b2e
Leehyoungwoo <dlguddn3024@gmail.com>
2022-10-31 09:45:11 +0900

    feature:4번문제

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import java.time.temporal.ChronoField;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = translate(word);
         return answer;
+    }
+    public static String translate(String word){
+        StringBuilder sb = new StringBuilder();
+
+        for (int len = word.length(),i=0;i<len;i++){
+            char c = word.charAt(i);
+            c = translator(c);
+            c = UpperLowercase(c);
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static char translator(char cha){
+
+        if (Character.isUpperCase(cha)){
+            return (char)('Z' - cha + 'A');
+        }
+
+        else if(Character.isLowerCase(cha)){
+            return (char)('z' - cha + 'a');
+        }
+
+        return cha;
+    }
+    public static char UpperLowercase(char c){
+        if (Character.isUpperCase(c)){
+            c+=32;
+        }
+        else if(Character.isLowerCase(c)){
+            c-=32;
+        }
+        return c;
     }
 }
