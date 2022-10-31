dea5c41555f0e9d5c02a0956cb0041d6f33b1300
wogus4048 <wogus4048@naver.com>
2022-10-31 19:24:05 +0900

    feat(Problem4): 한개의 문자를 반대로 변환해주는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,6 +8,30 @@ public class Problem4 {
         return answer;
     }
 
+    public static char convertLowerCase(char input) {
+        if(input >= 97 && input <=109)
+        {
+            int gap = (109-input)*2 + 1 ;
+            return (char) (input + gap);
+        }
+        else{
+            int gap = (input-110)*-2 - 1 ;
+            return (char) (input + gap);
+        }
+    }
+
+    public static char convertUpperCase(char input) {
+        if(input >= 65 && input <=77)
+        {
+            int gap = (77-input)*2 + 1 ;
+            return (char) (input + gap);
+        }
+        else{
+            int gap = (input-78)*-2 - 1 ;
+            return (char) (input + gap);
+        }
+    }
+
     public static boolean checkUpperCase(char input) {
         return input >= 65 && input <= 90;
     }
