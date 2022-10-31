a87fd08aa9e99ebfe085a6ef93a872b9f9d0582d
daeun <gain3436@gmail.com>
2022-11-01 00:32:20 +0900

    [feat](2) stack pop 조건체크

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
 
     public static boolean checkNotDuplicate(String string) {
@@ -7,6 +9,13 @@ public class Problem2 {
             if (string.charAt(i-1) == string.charAt(i)) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    public static boolean checkStackPop(Stack<Character> stack, String code, char before, int idx) {
+        if (!stack.isEmpty() && before == code.charAt(idx)&& code.charAt(idx) == stack.peek()) {
+            return true;
         }
         return false;
     }
