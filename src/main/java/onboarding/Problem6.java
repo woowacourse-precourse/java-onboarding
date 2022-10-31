9760ba176d0ccd993cc30bc7fe7d2e36bf3f0467
wugawuga <dnr3655@naver.com>
2022-10-31 21:56:41 +0900

    feat: 한 명일때 체크, 닉네임 전체 순회

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        if (forms.size() == 1) {
+            return Collections.emptyList();
+        }
+
+        Set<String> set = new TreeSet<>();
+        for (int i = 0; i < forms.size() - 1; i++) {
+            String nickName = forms.get(i).get(1);
+        }
+        return new ArrayList<>(set);
     }
 }
