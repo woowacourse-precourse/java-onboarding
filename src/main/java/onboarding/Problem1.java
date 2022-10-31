ca31517e8e386e69837c182c4358793970787f19
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-10-31 22:58:45 +0900

    problem 2

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,35 +4,41 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if ((pobi.get(1) - pobi.get(0) != 1) || (crong.get(1) - crong.get(0) != 1)) return -1;
-        int pl_max = cal_mul(pobi.get(0)) > cal_sum(pobi.get(0)) ? cal_mul(pobi.get(0)) : cal_sum(pobi.get(0));
-        int pr_max = cal_mul(pobi.get(1)) > cal_sum(pobi.get(1)) ? cal_mul(pobi.get(1)) : cal_sum(pobi.get(1));
-        int p_max = pl_max > pr_max ? pl_max : pr_max;
+        if ((pobi.get(1) - pobi.get(0) != 1) || (crong.get(1) - crong.get(0) != 1)) {
+            return -1;
+        }
 
-        int cl_max = cal_mul(crong.get(0)) > cal_sum(crong.get(0)) ? cal_mul(crong.get(0)) : cal_sum(crong.get(0));
-        int cr_max = cal_mul(crong.get(1)) > cal_sum(crong.get(1)) ? cal_mul(crong.get(1)) : cal_sum(crong.get(1));;
-        int c_max = cl_max > cr_max ? cl_max : cr_max;
+        int plMax = calMul(pobi.get(0)) > calSum(pobi.get(0)) ? calMul(pobi.get(0)) : calSum(pobi.get(0));
+        int prMax = calMul(pobi.get(1)) > calSum(pobi.get(1)) ? calMul(pobi.get(1)) : calSum(pobi.get(1));
+        int pMax = plMax > prMax ? plMax : prMax;
 
-        if (p_max == c_max) return 0;
-        else if (p_max>c_max) return 1;
-        else return 2;
+        int clMax = calMul(crong.get(0)) > calSum(crong.get(0)) ? calMul(crong.get(0)) : calSum(crong.get(0));
+        int crMax = calMul(crong.get(1)) > calSum(crong.get(1)) ? calMul(crong.get(1)) : calSum(crong.get(1));
+        int cMax = clMax > crMax ? clMax : crMax;
 
+        if (pMax == cMax) {
+            return 0;
+        } else if (pMax > cMax) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 
-    public static int cal_mul(int i){
+    public static int calMul(int i) {
         int result = 1;
-        while (i>0) {
-            result *= i%10;
-            i/=10;
+        while (i > 0) {
+            result *= i % 10;
+            i /= 10;
         }
         return result;
     }
 
-    public static int cal_sum(int i){
+    public static int calSum(int i) {
         int result = 0;
-        while (i>0) {
-            result += i%10;
-            i/=10;
+        while (i > 0) {
+            result += i % 10;
+            i /= 10;
         }
         return result;
     }
