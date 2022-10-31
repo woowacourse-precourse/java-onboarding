49d66900a69caed7e9ae8ae438d095fd438d9531
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 23:32:36 +0900

    feat: P4 기능 목록 주석 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+/**
+ * 기능 1: 알파벳인지 아닌지 판별하는 함수 작성
+ * 기능 2: 소문자인지 대문자인지 확인하여 변환해주는 함수 작성
+ * 기능 3: 문자열을 순환하면서 변환해주는 기능 작성
+ */
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
