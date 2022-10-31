35426fa31d3b96e63969eb90083ae7a02e95ffca
canbeyour <wuzucy@gmail.com>
2022-10-31 21:18:22 +0900

    feat: solve Problem4

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+    public static int checkClap(int number) {
+        int clap = 0;
+        while (number != 0) {
+            int value = number % 10;
+            if (value == 3 || value == 6 || value == 9)
+                clap += 1;
+            number /= 10;
+        }
+        return clap;
+    }
     public static int solution(int number) {
         int answer = 0;
+        while (number > 0) {
+            answer += checkClap(number);
+            number -= 1;
+        }
         return answer;
     }
 }
