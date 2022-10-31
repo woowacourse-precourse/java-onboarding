a780fd61dcf75e0a4abf077a0d6829479994e92d
JH-TT <leejongho9803@gmail.com>
2022-10-31 19:00:33 +0900

    2번 : 테스트케이스 추가, 2번 수정 + README 작성.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,38 +1,19 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem2 {
-    // 기능 목록
-    // code 배열 상태에 따른 알파벳 삽입 or 삭제 기능
-    // code 배열을 toString으로 변경하는 기능.
-
-
-    // code 배열 상태에 따른 알파벳 넣을지 말지.
-    static List<Character> updateCode(List<Character> code, char alpha) {
-        if(code.size() != 0 && code.get(code.size()-1) == alpha) {
-            code.remove(code.size()-1);
-        } else {
-            code.add(alpha);
-        }
-        return code;
-    }
-
-    // code 배열 -> toString
-    static String charListToString(List<Character> code) {
-        StringBuilder sb = new StringBuilder();
-        for(Character ch : code) {
-            sb.append(ch);
-        }
-        return sb.toString();
-    }
-
     public static String solution(String cryptogram) {
-        List<Character> code = new ArrayList<>();
-        for(char alpha : cryptogram.toCharArray()) {
-            code = updateCode(code, alpha);
+        String regex = "(.)\\1+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(cryptogram);
+
+        while(match.find()) {
+            cryptogram = cryptogram.replaceAll(regex, "");
+            match = pattern.matcher(cryptogram);
         }
-        return charListToString(code);
+
+        return cryptogram;
     }
 }
