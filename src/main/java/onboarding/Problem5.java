aa83d195b86df36c5c7c93a9ffe1320a8a92c796
송민진 <m_6595@naver.com>
2022-11-01 05:07:09 +0900

    #5 refactor: rename variables and method to make it clear

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,18 +8,21 @@ import java.util.Map;
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        Map<Integer, Integer> aiConverter = getMoneyList();
-        answer = List.of(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        List<Integer> answer = List.of(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        Map<Integer, Integer> moneyToIndex = makeConverter();
+        
         return answer;
     }
 
-    private static Map<Integer, Integer> getMoneyList() {
+    private static Map<Integer, Integer> makeConverter() {
+        Map<Integer, Integer> converter = new HashMap<>();
+
+        // 금액을 key값으로 입력하면 알맞는 인덱스를 return해주는 converter 구현
         List<Integer> tmp = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
-        Map<Integer, Integer> moneyToIndex = new HashMap<>();
         for(int i = 0; i < tmp.size(); i++) {
-            moneyToIndex.put(tmp.get(i), i);
+            converter.put(tmp.get(i), i);
         }
-        return moneyToIndex;
+        return converter;
     }
 }
