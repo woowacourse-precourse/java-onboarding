cc0d6694217c2315af3186bef3605e1af3cf9f22
devEdok13 <dev.edok13@gmail.com>
2022-10-31 13:28:08 +0900

    feat: P01-valueByMultiple

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,4 +17,14 @@ class Problem1 {
         }
         return result;
     }
+
+    //  입력된 수의 자릿수별 곱셈연산
+    private static int valueByMultiple(int num) {
+        int result = num % 10;
+        while (num >= 10) {
+            num /= 10;
+            result *= num % 10;
+        }
+        return result;
+    }
 }
