c425d5f4a2ce481cec603e96101f3df0026edb4a
cksdud8028 <cksdud8028@naver.com>
2022-10-31 20:43:33 +0900

    FEAT initialCrews

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+    static Map<String, String> crews = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
+        initialCrews(forms);
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static void initialCrews(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> crew = forms.get(i);
+            crews.put(crew.get(1), crew.get(0));
+        }
     }
 }
