2fcfdcdf8d49a7a3f3658e2d3eb11cd06298f27f
Sehoon Kim <shggm2000@gmail.com>
2022-10-31 23:19:30 +0900

    숫자를 받아 각 자리 수를 문자 형태로 arraylist에 하나 씩 넣는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+
+        int count = 0;
+        for (int k = 1; k <= number; k++) {
+
+            String str = Integer.toString(k);
+
+            char[] arr = str.toCharArray();
+
+            ArrayList<Character> list = new ArrayList<Character>();
+
+            for (int i = 0; i < arr.length; i++) {
+                list.add(arr[i]);
+            }
+        }
+
+            int answer = 0;
+
         return answer;
     }
 }
