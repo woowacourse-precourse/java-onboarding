ae2cbf948bf2956800a7f7f51b7d10a69daa977d
Brian <bwook9908@gmail.com>
2022-11-01 02:08:46 +0900

    [문제7] 리팩토링 1차 완료

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,62 +1,70 @@
-package onboarding;
+/*
+ * Problem7
+ *
+ * v1.1
+ *
+ * 2022.11.01
+ *
+ * 저작권 주의
+ */
 
+package onboarding;
 import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> friendOfUsers= searchFriendsOfUser(user, friends);
-        HashMap<String, ArrayList<String>> anotherFriends = getAnotherFriendUsingHashMap(user,friendOfUsers, friends);
-        ArrayList<String> overlapedFriends = new ArrayList<String>();
+        HashMap<String, ArrayList<String>> anotherFriends = getAnotherFriend(user,friendOfUsers, friends);
+        ArrayList<String> overlapedFriend = getOverlapedFriend(anotherFriends);
 
-        for(String anotherfriend1: anotherFriends.keySet()){
-            for(String anotherfriend2: anotherFriends.keySet()){
-                if(!anotherfriend1.equals(anotherfriend2)){
-                    overlapedFriends.addAll(findOverlapFriend(anotherFriends.get(anotherfriend1), anotherFriends.get(anotherfriend2)));
-                }
-            }
-        }
-
-        HashMap<String, Integer> friendScore = plusTen(overlapedFriends);
+        HashMap<String, Integer> friendScore = plusTen(overlapedFriend);
         friendScore = plusOne(friendScore, visitors, friendOfUsers);
 
         return getRecommendedFiveFriends(friendScore);
-
     }
 
     static List<String> searchFriendsOfUser(String user, List<List<String>> friends){
         List<String> friendOfUser = new ArrayList<>();
-        for (List<String> friend : friends) {
+
+        for (List<String> friendArray : friends) {
             for (int friendIndex = 0; friendIndex < 2; friendIndex++) {
-                if (friend.get(friendIndex).equals(user)) {
-                    friendOfUser.add(friend.get(1 - friendIndex));
+                int friendOfUserIndex=(1 - friendIndex);
+
+                if (friendArray.get(friendIndex).equals(user)) {
+                    friendOfUser.add(friendArray.get(friendOfUserIndex));
                 }
             }
         }
         return friendOfUser;
     }
 
-    static HashMap<String, ArrayList<String>> getAnotherFriendUsingHashMap(String User,
+    static HashMap<String, ArrayList<String>> getAnotherFriend(String User,
                                                                            List<String> friendsOfUser,
                                                                            List<List<String>> friends){
-
         HashMap<String, ArrayList<String>> AnotherFriendList=new HashMap<String, ArrayList<String>>();
-        for (String s : friendsOfUser) {
+
+        for (String friendOfUser : friendsOfUser) {
             ArrayList<String> AnotherFriend = new ArrayList<String>();// 얘가 주인공
             AnotherFriend.clear();
-            for (List<String> friend : friends) {
-                for (int j = 0; j < 2; j++) {
-                    if (friend.get(j).equals(s) && !friend.get(1-j).equals(User)) {
-                        AnotherFriend.add(friend.get(1 - j));
+            for (List<String> friendArray : friends) {
+                for (int index = 0; index < 2; index++) {
+                    int anotherFriendIndex= (1 - index);
+
+                    if (friendArray.get(index).equals(friendOfUser)
+                            &&
+                            !friendArray.get(anotherFriendIndex).equals(User)) {
+                        AnotherFriend.add(friendArray.get(anotherFriendIndex));
                     }
                 }
             }
-            AnotherFriendList.put(s, AnotherFriend);
+            AnotherFriendList.put(friendOfUser, AnotherFriend);
         }
         return AnotherFriendList;
     }
 
     static ArrayList<String> findOverlapFriend(ArrayList<String> ArrayListfriend1, ArrayList<String> ArrayListfriend2){
         ArrayList<String> overlapedFriend = new ArrayList<String>();
+
         for (String friend1 : ArrayListfriend1) {
             for (String friend2 : ArrayListfriend2) {
                 if (friend1.equals(friend2)) {
@@ -66,8 +74,18 @@ public class Problem7 {
         }
         return overlapedFriend;
     }
+    static ArrayList<String> getOverlapedFriend(HashMap<String, ArrayList<String>> anotherFriends){
+        ArrayList<String> overlapedFriends = new ArrayList<String>();
 
-
+        for(String anotherfriend1: anotherFriends.keySet()){
+            for(String anotherfriend2: anotherFriends.keySet()){
+                if(!anotherfriend1.equals(anotherfriend2)){
+                    overlapedFriends.addAll(findOverlapFriend(anotherFriends.get(anotherfriend1), anotherFriends.get(anotherfriend2)));
+                }
+            }
+        }
+        return overlapedFriends;
+    }
 
     static HashMap<String, Integer> plusTen(ArrayList<String> overlapedFriends){
         HashMap<String, Integer> friendScore = new HashMap<String, Integer>();
@@ -83,7 +101,6 @@ public class Problem7 {
                                             List<String> visitors,
                                             List<String> friendOfUsers){
 
-
         for(String visitor: visitors){
             if(friendScore.containsKey(visitor) && !friendOfUsers.contains(visitor)){
                 friendScore.put(visitor, friendScore.get(visitor)+1);
@@ -96,22 +113,23 @@ public class Problem7 {
     }
     static ArrayList<String> getRecommendedFiveFriends(HashMap<String, Integer> friendScore){
         ArrayList<String> recommendedFriends = new ArrayList<>(friendScore.keySet());
-        ArrayList<String> recommendedFiveFriends = new ArrayList<>();
+        ArrayList<String> recommendFiveFriends = new ArrayList<>();
 
         recommendedFriends.sort(new Comparator<String>() {
             public int compare(String o1, String o2) {
                 return friendScore.get(o2).compareTo(friendScore.get(o1));
             }
         });
+
         if(recommendedFriends.size() >5){
             for(int i=0; i<5; i++){
-                recommendedFiveFriends.add(recommendedFriends.get(i));
+                recommendFiveFriends.add(recommendedFriends.get(i));
             }
         }else{
-            recommendedFiveFriends= recommendedFriends;
+            recommendFiveFriends= recommendedFriends;
         }
 
-        return recommendedFiveFriends;
+        return recommendFiveFriends;
 
     }
 }
