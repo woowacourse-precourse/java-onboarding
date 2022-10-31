6d165435081955a87790cb762c6be340eb387b05
ddogga <hayeonlee60327@gmail.com>
2022-10-31 15:19:26 +0900

    문제3 1의 자리 수 일 경우 3의 개수를 구하는 메서드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,7 +4,18 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        int[] countList = new int[5];
+
+        //0번 : 10의 자리, 1번 : 100의 자리, 2번 : 1000의 자리, 3번 : 10000의 자리
+        int[] countList = new int[4];
+
+        if (number == 10000) {return countList[3];}
+
+        int placeValue = getPlaceValue(number);
+
+        if (placeValue == 1) {
+            answer = units(number);
+        }
+
 
         return answer;
     }
@@ -20,4 +31,18 @@ public class Problem3 {
         // 1:1의 자리, 2:10의 자리 3:100의 자리 4:1000의 자리 5:10000의 자리
         return cnt;
     }
+
+
+    public static int units(int number) {
+
+        return (int) number / 3;
+
+    }
+
+    public static void main(String[] args) {
+        int number = 9;
+
+        System.out.println(solution(number));
+    }
+
 }
