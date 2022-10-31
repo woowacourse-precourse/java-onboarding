ae54ac75d4d52fff5affbacfb6862e9837f81f11
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 16:13:05 +0900

    comment: P1 기능 목록 주석 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,11 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 1: 올바른 페이지 수가 입력되었는가 확인
+ * 기능 2: 각각 개별로 최대로 얻은 점수를 최고 점수로 구하기
+ * 기능 3: 두 명의 점수를 비교하여 승패 가르기
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
