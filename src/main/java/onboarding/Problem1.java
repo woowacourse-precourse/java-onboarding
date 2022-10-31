016a1266f55d08134bee3702a1406b7fa7e473b5
WhoIsWoony <WhoIsWoony@gmail.com>
2022-10-31 17:17:01 +0900

    problem1: TODO-'예외사항3' complete

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,7 @@ class Problem1 {
     private static boolean checkCorrectOf(List<Integer> player){
         if(player.get(1) - player.get(0) != 1) return false;
         else if(player.get(0)==null || player.get(1) ==null) return false;
+        else if(player.get(0) > 400 || player.get(1) == 400) return false;
         return true;
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
