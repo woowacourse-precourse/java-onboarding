b28324fc744a2ffb5e3113ad64b4b91ea9ce32a5
Go-Jaecheol <gojaech@naver.com>
2022-10-31 16:43:05 +0900

    feat: Problem5 - 금액 단위별 변환하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,15 +7,23 @@ import java.util.List;
 public class Problem5 {
     private static final List<Integer> moneyUnit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getResultList(money);
     }
 
     private static List<Integer> getResultList(int money) {
         List<Integer> result = new ArrayList<>();
         for (int unit : moneyUnit) {
-
+            result.add(changeMoneyByUnit(money, unit));
+            money = getRemainMoney(money, unit);
         }
         return result;
+    }
+
+    private static int changeMoneyByUnit(int money, int unit) {
+        return money / unit;
+    }
+
+    private static int getRemainMoney(int money, int unit) {
+        return money % unit;
     }
 }
