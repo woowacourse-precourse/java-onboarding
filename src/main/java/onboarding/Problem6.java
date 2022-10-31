bf87c8b3624470119da0b87002a07fcf0d5bbbbd
juhwang <juhong4930@naver.com>
2022-10-31 23:19:09 +0900

    style(Problem6~7)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,49 +8,56 @@ import java.util.List;
 public class Problem6 {
     private static int START_INDEX = 0;
     private static boolean[] ALARM_SYSTEM = null;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
         fillAlarmSystem(forms.size());
         nickNameLoop(extractNickName(forms));
-        addEmailInResultList(answer,forms);
+        addEmailInResultList(answer, forms);
         return answer;
     }
-    private static void fillAlarmSystem(int formsSize){
+
+    private static void fillAlarmSystem(int formsSize) {
         ALARM_SYSTEM = new boolean[formsSize];
-        Arrays.fill(ALARM_SYSTEM,false);
+        Arrays.fill(ALARM_SYSTEM, false);
     }
-    private static List<List<String>> extractNickName(List<List<String>> forms){
+
+    private static List<List<String>> extractNickName(List<List<String>> forms) {
         List<List<String>> nickNameList = new ArrayList<>();
         forms.forEach(form -> nickNameList.add(cutNickName(form.get(1))));
         return nickNameList;
     }
-    private static List<String> cutNickName(String nickName){
+
+    private static List<String> cutNickName(String nickName) {
         List<String> TwoNickNameString = new ArrayList<>();
-        for (int i = 0; i < nickName.length()-1; i++) {
-            TwoNickNameString.add(nickName.substring(i,i+2));
+        for (int i = 0; i < nickName.length() - 1; i++) {
+            TwoNickNameString.add(nickName.substring(i, i + 2));
         }
         return TwoNickNameString;
     }
-    private static void nickNameLoop(List<List<String>> nickNameList){
-        for (int i = 0; i < nickNameList.size()-1; i++) {
+
+    private static void nickNameLoop(List<List<String>> nickNameList) {
+        for (int i = 0; i < nickNameList.size() - 1; i++) {
             START_INDEX = i;
-            nickNameToStringLoop(nickNameList.get(i),nickNameList);
+            nickNameToStringLoop(nickNameList.get(i), nickNameList);
         }
     }
-    private static void nickNameToStringLoop(List<String> nickNameToString,List<List<String>> nickNameList){
-        nickNameToString.forEach(nickNameString -> compareOtherNickName(nickNameString,nickNameList));
+
+    private static void nickNameToStringLoop(List<String> nickNameToString, List<List<String>> nickNameList) {
+        nickNameToString.forEach(nickNameString -> compareOtherNickName(nickNameString, nickNameList));
     }
 
-    private static void compareOtherNickName(String nickNameString,List<List<String>> nickNameList) {
-        for (int i = START_INDEX+1; i < nickNameList.size(); i++) {
-            if(ALARM_SYSTEM[i])continue;
-            if(nickNameList.get(i).contains(nickNameString)){
+    private static void compareOtherNickName(String nickNameString, List<List<String>> nickNameList) {
+        for (int i = START_INDEX + 1; i < nickNameList.size(); i++) {
+            if (ALARM_SYSTEM[i]) continue;
+            if (nickNameList.get(i).contains(nickNameString)) {
                 ALARM_SYSTEM[START_INDEX] = true;
                 ALARM_SYSTEM[i] = true;
             }
         }
     }
-    private static void addEmailInResultList(List<String> result,List<List<String>> forms) {
+
+    private static void addEmailInResultList(List<String> result, List<List<String>> forms) {
         for (int i = 0; i < forms.size(); i++) {
             if (ALARM_SYSTEM[i]) {
                 result.add(forms.get(i).get(0));
@@ -58,7 +65,8 @@ public class Problem6 {
         }
         sortResultList(result);
     }
-    private static void sortResultList(List<String> result){
+
+    private static void sortResultList(List<String> result) {
         Collections.sort(result);
     }
 }
