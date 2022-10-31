3c93fed75204dd90f1e7e0203c9f197681327394
p <gustjr2332@naver.com>
2022-11-01 01:29:39 +0900

    feat(Problem3): countClap() 각 자릿수에 3,6,9 가 포함된 수를 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    public static int countClap(int num){
+        int clap=0;
+
+        while(num>0){
+            if( num%10==3 || num%10==6 || num%10==9 ) clap++;
+            num/=10;
+        }
+        return clap;
     }
 }
