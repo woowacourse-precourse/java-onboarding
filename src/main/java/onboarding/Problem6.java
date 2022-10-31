bd281354b264c2263e9d7dfb23d77da194ebf087
sxxgxxnz <tmddms2292@gmail.com>
2022-10-31 19:04:59 +0900

    feat Problem6[2] 닉네임을 비교하기 위해 비교할 닉네임을 한글자씩 배열에 저장한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,10 @@ import java.util.*;
 public class Problem6 {
     public static Set<String> checkDupl(List<List<String>> forms){
         Set<String> result = new HashSet<>();
-        
+        for(int i=0;i<forms.size()-1;i++){
+            String nickname = forms.get(i).get(1);
+            String[] nameArr = nickname.split("");
+        }
         return result;
     }
     public static List<String> solution(List<List<String>> forms) {
