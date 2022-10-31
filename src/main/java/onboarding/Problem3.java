1dc959dc7ffb20fd25bda47fad433f6fb6ee258f
sa46lll <9812smh@naver.com>
2022-11-01 03:12:25 +0900

    feat: 특정 숫자에 손뼉 치는 횟수 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
+
         int answer = 0;
+
+        List<Integer> array = getArray(number);
+
         return answer;
+    }
+
+    private static List<Integer> getArray(int number) {
+        List<Integer> result = new ArrayList<>();
+
+        while (number > 0) {
+            result.add(number % 10);
+            number /= 10;
+        }
+        return result;
+    }
+
+    private static int countClap(List<Integer> array) {
+        int count = 0;
+        for (int i : array) {
+            if (isClap(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isClap(int num) {
+        if (num == 0) return false;
+        return num % 3 == 0;
     }
 }
