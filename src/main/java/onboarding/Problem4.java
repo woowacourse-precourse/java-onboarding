ceb8ab5cbb4dfd73a4aa1d90b50c24e98b09f224
yw824 <yongwoo25@gmail.com>
2022-10-31 16:02:54 +0900

    feat: solution 함수 스트림 구현 및 청개구리 문자열 반환 함수 구현
    - solution() 함수:
    	- 문자열의 길이가 1 이상, 1000 이하의 조건을 어겼을 때
    	- 처리법을 모르겠습니다. throw new Exception으로 처리하였더니
    	- test에서 에러가 발생
    	- 따라서 그냥 입력 조건을 상기시키는 문구를 반환
    - 청개구리 문자열 반환 함수: getReversedString(String word)
    	- 맨 처음에 반환을 위한 빈 문자열 생성(String str)
    	- 입력받은 word 문자열의 길이만큼 순회
    	- 문자열 연산 : 아스키코드 참조
    	- 대문자라면? : Z에서 뺸 만큼 A에 더하면 청개구리 문자 획득
    	- 소문자라면? : z에서 뺸 만큼 a에 더하면 청개구리 문자 획득
    	- 다른 종류의 문자라면? 그대로
    	- 각각 구한 문자를 str 문자열 뒤에 추가
    	- for문 순회가 끝나면 최종 청개구리 문자열인 str 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,26 +3,44 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        // 예외 처리 : 1 이상 1000 이하인 문자열 들어오면
+        if(word.length() < 1 || word.length() > 1000)
+            return "입력하는 문자열의 길이는 반드시 1 이상 1000 이하여야 합니다.";
 
         // 거꾸로 변환된 청개구리 문자열을 얻어 반환한다.
-        // implement
+        answer = getReversedString(word);
         return answer;
     }
 
     static String getReversedString(String word) {
-        // implement
-
         /* start of loop */
+        String str = "";
 
-        // 만약 대문자라면 ? ( 65 <= 아스키코드 <= 90 )
+        int len = word.length();
+        char c;
+        for(int i = 0; i < len; ++i)
+        {
+            c = word.charAt(i);
+            // 대문자라면 ?? - 아스키 코드
+            if(c >= 65 && c <= 90) // 65 = 'A' , 90 = 'Z'
+            {
+                c = (char) (90 - c + 65);
+                str += c;
+                continue;
+            }
 
-        // 만약 소문자라면 ? ( 97 <= 아스키코드 <= 122 )
+            // 소문자라면 ?? - 아스키 코드
+            if(c >= 97 && c <= 122) // 97 = 'a' , 122 = 'z'
+            {
+                c = (char) (122 - c + 97);
+                str += c;
+                continue;
+            }
 
-        // 다른 종류의 문자라면 ? -> 그대로 추가
+            // 다른 종류의 문자라면 ? 그대로 추가
+            str += c;
+        } /* end of loop */
 
-
-        /* end of loop */
-
-        return "";
+        return str;
     }
 }
