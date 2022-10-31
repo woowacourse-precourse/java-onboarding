a498a84ec4b726128b258169c879a7b8bf4ac490
aud19 <aud19980518@gmail.com>
2022-11-01 01:33:55 +0900

    1주차 7번 기능목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,3 +1,11 @@
+/*
+* 기능 목록
+* 1. user의 친구 리스트 구하는 함수
+* 2. 친구리스트의 친구들 점수 계산하기
+* 3. visit한 사람의 점수 계산하기
+* 4. 이미 친구가 되어있는 사람의 점수 삭제
+* 5. sort
+ */
 package onboarding;
 
 import java.util.Collections;
