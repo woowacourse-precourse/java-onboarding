a80d10fa0ba1851915e0b991c514b872d55c9571
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:54:24 +0900

    [Problem7] 친구 관계 정보에서 이미 친구인 경우 제거하기 (김민서)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -15,6 +15,21 @@ public class Problem7 {
      */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
+        friends = new CopyOnWriteArrayList<>(friends);
+        visitors = new CopyOnWriteArrayList<>(visitors);
+        List<String> userFriends = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 1. 친구 관계 정보에서 이미 친구인 경우 제거하기
+        for(List<String> list : friends) {
+            if(list.get(0).equals(user)) { // 친구리스트 중 0번째 이름이 사용자 이름일 경우
+                userFriends.add(list.get(1)); // 1번째 이름을 사용자 친구 리스트에 저장 (0번째는 사용자 이름이므로)
+                friends.remove(list); // 함께 아는 친구의 수를 고려할 때 필요 없는 리스트이므로 삭제
+            } else if(list.get(1).equals(user)) { // 친구리스트 중 1번째 이름이 사용자 이름인 경우
+                userFriends.add(list.get(0)); // 0번째 이름을 사용자 친구 리스트에 저장 (1번째는 사용자 이름이므로)
+                friends.remove(list); // 함께 아는 친구의 수를 고려할 때 필요 없는 리스트이므로 삭제
+            }
+        }
 
         return answer;
     }
