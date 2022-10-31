0cf462583c1b0506b41b78802b665e3284f0dde7
Xylitol311 <cktnghd0311@daum.net>
2022-11-01 07:16:01 +0900

    문제3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int cnt = 1; cnt <= number; cnt++) { // 1부터 입력 받은 숫자까지 반복
+            String str = String.valueOf(cnt); // 문자열 변환
+            for (int i = 0; i < str.length(); i++) {
+                char chk = str.charAt(i);
+                if (chk == '3' || chk == '6' || chk == '9') {//해당 일때만 answer에 1 추가
+                    answer = answer + 1;
+                }
+            }
+        }
         return answer;
     }
 }
