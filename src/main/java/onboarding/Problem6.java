e7e8248a5f7ab7ee6dad88b603a68a2bba53c0f1
codingvegeta <hong090992@gmail.com>
2022-10-31 19:50:40 +0900

    feat : soulutions 을 해결 후 오름차순으로 정렬하기 위한 콤퍼레이터를 생성하였습니다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        final Comparator<String> COMPARATOR = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] str1 = o1.split("@");
+                String[] str2 = o2.split("@");
+                return str1[0].compareTo(str2[0]);
+            }
+        };
+
         return answer;
     }
+
 }
