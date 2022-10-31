04ccfae72c87ea4ce9c284d4d37f92969b6ac932
jindaram-stu <jinsb1999@naver.com>
2022-10-31 23:28:02 +0900

    feat : [문제6] 중복을 체크하는 로직 추가
    중복을 체크한 후, 중복이라면 email ArrayList에 중복된 crew의 이메일과, 중복인 두 문자를 닉네임으로 포함하고 있는 crew의 이메일을 추가
    그렇지 않다면 segments Map에 두 문자를 추가하여 추후 중복 추가 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,42 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String,String> segments = new HashMap<>();
+        List<String> emails = new ArrayList<>();
+
+        for (List<String> crew : forms) {
+            if ( crew.get(1).length() == 1) { continue; }
+            checkDuplication(crew,emails,segments);
+        }
+
+        return emails;
     }
+
+    public static void checkDuplication (List<String> crew, List<String> emails, Map<String, String> segments) {
+        String crewEmail = crew.get(0);
+        String crewName = crew.get(1);
+        for (int i=0; i<crewName.length()-1; i++) {
+            String word = crewName.charAt(i) + "" + crewName.charAt(i+1);
+            addDuplicateEmail(word,crewEmail,segments,emails);
+        }
+    }
+
+    public static void addDuplicateEmail(String word, String crewEmail, Map<String,String> segments, List<String> emails) {
+        if (segments.containsKey(word)) {
+            emails.add(segments.get(word));
+            emails.add(crewEmail);
+        }
+        if(!segments.containsKey(word)) {
+            segments.put(word,crewEmail);
+        }
+    }
+
+
+
+
+
 }
