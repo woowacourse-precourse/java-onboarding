12c27ad343e2444c81120918292bcf4d42cac374
장한울 <swingjhw@naver.com>
2022-11-01 06:11:52 +0900

    feat(문제2) : 비어 있지 않으면 중복되는 글자를 찾는 함수 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,29 +1,39 @@
 package onboarding;
 
-import java.util.Stack;
+import java.util.*;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
 
         Stack<Character> stack = new Stack<>();
-        for (int i = 1; i < cryptogram.length(); i++) {
+        // 문자열 탐색
+        for (int i = 0; i < cryptogram.length(); i++) {
             char word =  cryptogram.charAt(i);
-            if (stack.empty()) {
+            // 스택이 비어있으면 문자열 추가
+            if (stack.isEmpty()) {
                 stack.add(word);
+                // 스택이 비어 있지않으면 체크
             } else {
-//                stack = checkSame(stack, word);
+                stack = checkSame(stack, word);
             }
         }
-        String answer = "answer";
+
+        ArrayList<String> list = new ArrayList<>();
+        stack.forEach(ch -> list.add(String.valueOf(ch)));
+        String answer = String.join("", list);
+
+
         return answer;
     }
 
-//    public static Stack<Character> checkSame(Stack<Character> stack, char word) {
-//        if (stack.peek() == word) {
-//            stack.pop();
-//        } else {
-//            stack.add(word);
-//        }
-//        return stack;
-//    }
+    public static Stack<Character> checkSame(Stack<Character> stack, char word) {
+        // 스택 최상단 값이 word와 같을 때 최상단 값 제거
+        if (stack.peek() == word) {
+            stack.pop();
+            // 최상단 값과 같지 않을 때 word 추가
+        } else {
+            stack.add(word);
+        }
+        return stack;
+    }
 }
