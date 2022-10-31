a863ba5b714c300fb902054394d664d1273b6f21
ParangBird <seekerman@naver.com>
2022-10-31 21:05:11 +0900

    feat: 슈도코드에 맞게 로직 구현 + 테스트 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -21,6 +21,9 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1;i<=number;i++){
+            answer += getClapsByNumber(i);
+        }
         return answer;
     }
 }
