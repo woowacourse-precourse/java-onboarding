fb2e4e85e570c1535d13307c79cdfa890b0132cd
glaehfdl1654 <glaehfdl1654@naver.com>
2022-11-01 01:29:12 +0900

    [Problem6] check domain form "email.com"

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,16 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for (int i = 0; i < forms.size(); i++) {
+            if (isNotEmail(forms.get(i).get(0))) {
+                forms.remove(i);
+                i--;
+            }
+        }
         return answer;
+    }
+    public static boolean isNotEmail(String emailAddress) {
+        return !emailAddress.endsWith("@email.com");
     }
 }
