a5540c4e0cdb2253804f475d0ba8f171ee2f8302
eugene1204 <s1061001@naver.com>
2022-10-31 16:54:11 +0900

    기능 목록 : problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,9 +4,14 @@ import java.util.*;
 import java.util.Collection;
 
 public class Problem7 {
+    // 1. 친구 목록 만들기
+    // 2. 친구의 친구 목록만들기 -> 추천친구에 무조건추가 +10
+    // 3. 방문자 확인하기 -> 친구만아니면 추천친구에 추가하기 +1
+
+
+    // 추천 친구목록에는 친구의친구만 추가되어있음
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
-
 
         }}
