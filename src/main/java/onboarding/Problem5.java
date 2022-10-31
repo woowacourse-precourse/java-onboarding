e8dac7436e78252b7e8e2aa015a61e9aaf112c97
glass <glass7312@gmail.com>
2022-10-31 20:35:58 +0900

    Problem5 refactor(변수명 변경)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,7 +10,7 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         Integer fif_th=0;int ten_th=0; int fiv_th=0; int th=0;
         int f_hund=0; int hund_d=0; int fif_ten=0;
-        int ten_d=0; int one=0;
+        int ten_d=0; int one_d=0;
         fif_th=money/50000;
         ten_th=(money%50000)/10000;
         fiv_th=(money%10000)/5000;
@@ -19,9 +19,9 @@ public class Problem5 {
         hund_d=(money%500)/100;
         fif_ten=(money%100)/50;
         ten_d=(money%50)/10;
-        one=(money%10);
+        one_d=(money%10);
 
-        answer= Arrays.asList(fif_th,ten_th,fiv_th,th,f_hund,hund_d,fif_ten,ten_d,one);
+        answer= Arrays.asList(fif_th,ten_th,fiv_th,th,f_hund,hund_d,fif_ten,ten_d,one_d);
 
 
 
