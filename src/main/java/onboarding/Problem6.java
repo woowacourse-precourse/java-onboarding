9f8a5dc41cce1e54fe6379dce2fd392eee0ff5a5
dldmsdn99 <dldmsdn99@naver.com>
2022-11-01 00:29:22 +0900

    Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,53 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        HashSet<String> emails = new HashSet<>();
+
+        final Map<String, String> hashMap = new HashMap<>();
+
+
+
+        for (int i = 0; i < forms.size(); i++) {
+
+            String name = forms.get(i).get(1);
+
+            if (name.length() < 2) {
+                break;
+            }
+
+            for(int j=0; j <name.length()-1; j++){
+                String key = name.substring(j,j+2); // 제이,이엠,제이,이슨,워니,엠제,제이,이제,제엠
+
+                if(hashMap.containsKey(key)){
+                    String email = hashMap.get(key);
+
+                    if(!email.equals(forms.get(i).get(0))){
+                        emails.add(email);
+                        emails.add(forms.get(i).get(0));
+
+                    }
+
+                }
+                hashMap.put(key,forms.get(i).get(0));
+
+
+
+            }
+
+
+
+
+        }
+        List<String> answer = emails.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(answer);
+
+
         return answer;
     }
 }
