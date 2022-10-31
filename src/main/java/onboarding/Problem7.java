3e344649cc983cd06b8f25391e6c88da68c3765a
wwwndus <simock22@gmail.com>
2022-10-31 19:59:11 +0900

    feat: 함께 아는 친구를 추가할 Map을 선언하고 실행하기 위한 메소드 선언

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,21 +6,36 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         List<String> friendsOfUser = new ArrayList<>();
+        Map<String, Integer> scoreMap = new HashMap<>();
 
         findFriendsOfUser(user, friends, friendsOfUser);
+
+        addAcquaintanceInMap(user, friends, friendsOfUser, scoreMap);
 
         return answer;
     }
 
-    private static void findFriendsOfUser(String user, List<List<String>> friends, List<String> friendsOfUser) {
-        for (int i = 0; i < friends.size(); i++) {
-            if (friends.get(i).contains(user)) {
-                for (String friend : friends.get(i)) {
-                    if (!friend.equals(user)) {
-                        friendsOfUser.add(friend);
+    private static void addAcquaintanceInMap(String user, List<List<String>> friends, List<String> friendsOfUser, Map<String, Integer> scoreMap) {
+        for (List<String> friend : friends) {
+            for (String friendOfUser : friendsOfUser) {
+                if (friend.contains(friendOfUser) && !friend.contains(user)) {
+                    for (int i = 0; i < friend.size(); i++) {
+                        if (!friend.get(i).equals(friendOfUser)) {
+                            if (!scoreMap.containsKey(friend.get(i))) {
+                                scoreMap.put(friend.get(i), 10);
+                            } else {
+                                int score = scoreMap.get(friend.get(i)) + 10;
+                                scoreMap.put(friend.get(i), score);
+                            }
+                        }
                     }
+
                 }
             }
         }
+    }
+
+    private static void findFriendsOfUser(String user, List<List<String>> friends, List<String> friendsOfUser) {
+        
     }
 }
