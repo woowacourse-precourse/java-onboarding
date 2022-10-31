bd9ba3cda3a3b48f176922dcf3a5e8130867b0f2
YooDongSeon <inko51366@naver.com>
2022-11-01 00:35:44 +0900

    기능목록 1

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /*
+        기능목록 :
+        1. 화폐 단위를 ArrayList로 선언. (List.of 사용 이유는 단위는 불변이기때문)
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> moneyKind = new ArrayList<>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10));
+
+
         return answer;
     }
 }
