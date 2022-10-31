08575f9e47ace4b2c16df2da7f88583999a6840c
hossang <zkzk104@naver.com>
2022-10-31 21:35:37 +0900

    refactor: 조건문을 대소문자를 판별하는 메소드(isLowerCase, isUpperCaese)로 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,11 +5,11 @@ public class Problem4 {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if (c - 'A' >= 0 && c - 'A' < 26) {
+            if (isUpperCase(c)) {
                 sb.append((char) ('A' + 25 - (c - 'A')));
                 continue;
             }
-            if (c - 'a' >= 0 && c - 'a' < 26) {
+            if (isLowerCase(c)) {
                 sb.append((char) ('a' + 25 - (c - 'a')));
                 continue;
             }
@@ -17,5 +17,13 @@ public class Problem4 {
             sb.append(c);
         }
         return sb.toString();
+    }
+
+    private static boolean isLowerCase(char c) {
+        return c - 'a' >= 0 && c - 'a' < 26;
+    }
+
+    private static boolean isUpperCase(char c) {
+        return c - 'A' >= 0 && c - 'A' < 26;
     }
 }
