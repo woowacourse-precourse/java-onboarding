2010599e6218497a21cb9d9f9895c4265ad6bc0a
Yeonji-Lim <view7186@naver.com>
2022-10-31 23:42:28 +0900

    Solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,10 +9,16 @@ public class Problem2 {
                 ei = i;
                 isDuplicateExist = true;
             } else {
-                if(isDuplicateExist) cryptogram = cryptogram.substring(0, si) + cryptogram.substring(ei+1);
+                if(si < ei) {
+                    cryptogram = cryptogram.substring(0, si) + cryptogram.substring(ei+1);
+                    i = si; // 중복을 제거하고 나서는 중복 체크가 진행되었던 곳에서 시작
+                }
                 si = i;
                 ei = i;
             }
+        }
+        if(si < ei) {
+            cryptogram = cryptogram.substring(0, si) + cryptogram.substring(ei+1);
         }
         if(isDuplicateExist) return solution(cryptogram);
         return cryptogram;
