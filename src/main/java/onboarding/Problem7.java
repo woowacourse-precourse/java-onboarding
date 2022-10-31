6b7d0344464bad3dd4b28efe57998a1ff0e94009
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-31 15:42:49 +0900

    [ADD] 문제 7 - 아는 사람의 아는 사람인 이름에게 10점 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     private static final int MUTUAL_SCORE = 10;
@@ -14,12 +11,23 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<List<String>> friendRelations = new ArrayList<>();
         HashSet<String> userFriends = new HashSet<>();
+        HashMap<String, Integer> recommendations = new HashMap<>();
 
         for (List<String> friendRelation : friends) { // friends 중에 user와 친구인 항목 제외
             if (friendRelation.contains(user)) {
                 userFriends.add(friendRelation.get(0));
             } else {
                 friendRelations.add(friendRelation);
+            }
+        }
+
+        for (List<String> friendRelation : friendRelations) { // 함께 아는 사람에게 10점 추가
+            String mutual = friendRelation.get(1);
+
+            if (!recommendations.containsKey(mutual)) { // 처음 인식된 이름이면 등록 + 10점 추가
+                recommendations.put(mutual, MUTUAL_SCORE);
+            } else {                                    // 이미 있는 이름일 시, 기존 점수에 10점 추가
+                recommendations.put(mutual, recommendations.get(mutual) + MUTUAL_SCORE);
             }
         }
 
