5d360b5767b477669945496a054892890eaff371
Jeonsoyeon <itso22@naver.com>
2022-10-31 12:53:14 +0900

    feat(Problem3): 각 자리의 숫자 판별

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+    public static int[] eachDigit(int num){
+        return Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
     }
 }
