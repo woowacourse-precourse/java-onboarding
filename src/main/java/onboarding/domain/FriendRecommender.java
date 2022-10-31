0e01647631a77997783cb1fb2962f9527fd6c8a3
yoondgu <doyou2ing@naver.com>
2022-10-31 11:42:18 +0900

    refactor(Problem7): merge getRecommendList and getTop5Entries

--- src/main/java/onboarding/domain/FriendRecommender.java
+++ src/main/java/onboarding/domain/FriendRecommender.java
@@ -13,17 +13,24 @@ import java.util.Map.Entry;
 public class FriendRecommender {
     private final MutualDictionary mutualDict;
     private final VisitsDictionary visitsDict;
+    private final Map<String, Integer> entryDict;
     
     public FriendRecommender(MutualDictionary mutualDict, VisitsDictionary visitsDict) {
         this.mutualDict = mutualDict;
         this.visitsDict = visitsDict;
+        this.entryDict = getEntryDictionary(mergeAllEntries());
     }
     
     public List<String> getRecommendList() {
-        Set<String> totalEntries = mergeAllEntries();
-        Map<String, Integer> entryDict = getEntryDictionary(totalEntries);
-        List<Map.Entry<String, Integer>> list = getSortedEntries(entryDict);
-        return getTop5Entries(list);
+        List<String> list = new ArrayList<>();
+        List<Map.Entry<String, Integer>> entryList = getSortedEntries(entryDict);
+        int idx = 0;
+        while (!(idx == 5 || idx + 1 > entryList.size())) {
+            Map.Entry<String, Integer> entry = entryList.get(idx);
+            list.add(entry.getKey());
+            idx++;
+        }
+        return list;
     }
     
     private Set<String> mergeAllEntries() {
@@ -36,7 +43,6 @@ public class FriendRecommender {
         Map<String, Integer> entryDict = new HashMap<>();
         for (String entry : entries) {
             int mutualCnt = mutualDict.getTotalMutualCounts(entry);
-            // TODO 방문자 중 친구인 경우는 visitsDictionary에서 미리 제외하고 있는데, 여기서 제외하는 게 나을지?
             int visitCnt = visitsDict.getTotalVisitCounts(entry);
             int point = mutualCnt*10 + visitCnt;
             entryDict.put(entry, point);
@@ -53,17 +59,6 @@ public class FriendRecommender {
                 return comparison == 0 ? o1.getKey().compareTo(o2.getKey()) : comparison;
             }
         });
-        return list;
-    }
-    
-    private List<String> getTop5Entries(List<Map.Entry<String, Integer>> entryList) {
-        List<String> list = new ArrayList<>();
-        int idx = 0;
-        while (!(idx == 5 || idx + 1 > entryList.size())) {
-            Map.Entry<String, Integer> entry = entryList.get(idx);
-            list.add(entry.getKey());
-            idx++;
-        }
         return list;
     }
 }
