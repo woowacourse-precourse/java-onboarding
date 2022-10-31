27972466282ef1e0d688d9a00bc370b83d0f0c5b
yoonjuho <dbswn990@naver.com>
2022-10-31 19:44:03 +0900

    feat: PROBLEM5 화폐 배열 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,19 @@ public class Problem5 {
         if(hasException(money)){
             return null;
         }
-        return answer;
+        return getChangedByAmount(money);
+    }
+
+    public static List<Integer> getChangedByAmount(int money){
+        int number = 0;
+        List<Integer> result = new ArrayList<>();
+        int[] amount = {50000,10000,5000,1000,500,100,50,10,1};
+        for(int bill : amount){
+            number = (money/bill>0)?money/bill:0;
+            result.add(number);
+            money -= bill*number;
+        }
+        return result;
     }
 
     public static boolean hasException(int money){
