2ca4013c54ff77092120dbe06bad1469942c8c7e
Jeonsoyeon <itso22@naver.com>
2022-10-31 12:23:48 +0900

    feat(Problem2): 반복되는 문자 뽑아오는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,13 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    public static String repeatWords(String s){
+        String repeat="";
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i-1)==s.charAt(i)){
+                repeat = String.valueOf(s.charAt(i-1))+String.valueOf(s.charAt(i));
+            }
+        }
+        return repeat;
+    }
 }
