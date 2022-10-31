dffb8af7446c8340553857ecc18b402e11ecbb55
ensk26 <tjensk26@gmail.com>
2022-10-31 16:49:59 +0900

    docs: 기능 목록 구성, problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,17 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
+
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    //닉네임을 두글자씩 나눈 것 기준으로 email과 함께 저장
+    static void saveEmailByDivideNickName(String nickName, String email) {
+
+    }
+
+
 }
