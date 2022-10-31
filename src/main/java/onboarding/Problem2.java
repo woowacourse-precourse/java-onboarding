509fac12d2bcb743f6b6644b3d2feef3a1d2e465
jongmyeongbak <withpleasure@gmail.com>
2022-11-01 01:19:24 +0900

    solve problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,32 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer;
+        char[] chars;
+        char prev;
+        int k;
+        boolean deleteonce = false;
+
+        do {
+            answer = cryptogram;
+            chars = answer.toCharArray();
+            prev = 0;
+            k = 0;
+
+            for (char c: chars) {
+                if (prev != c) {
+                    chars[k++] = c;
+                    prev = c;
+                    deleteonce = false;
+                } else if (!deleteonce) {
+                    --k;
+                    deleteonce = true;
+                }
+            }
+            cryptogram = new String(chars).substring(0, k);
+
+        } while (!cryptogram.equals(answer));
+
         return answer;
     }
 }
