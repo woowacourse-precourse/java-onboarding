1cce7ad3149721ce897f4b351c7ebdcef5a73add
Kijung Park <kjpark4321@naver.com>
2022-10-31 23:57:53 +0900

    3번: 1부터 number까지 박수 총 몇번 쳐야하는지 solution 구현 및 3번 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        
+
+        for (int i = 1; i <= number; i++) {
+            answer += getClapNumber(i);
+        }
 
         return answer;
     }
