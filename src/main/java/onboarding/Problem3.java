3478f0fc2e19592a7c2a5e8369d43ab57fab4ff1
mikekang47 <gyeong0308@gmail.com>
2022-10-31 18:25:42 +0900

    Feat<problem3>: add count369 method

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
 
     /**
@@ -11,7 +13,26 @@ public class Problem3 {
             throw new RuntimeException("인풋 바운더리를 초과했습니다.(number: " + number);
         }
     }
+
+    /**
+     * 주어진 숫자에 있는 3,6,9의 갯수를 세는 메서드 입니다.
+     * @param number
+     * @return
+     */
+    public static int count369(int number) {
+        String[] num = String.valueOf(number).split("");
+
+        return (int) Stream.of(num)
+                .filter(x -> x.equals("3") || x.equals("6") || x.equals("9")).count();
+    }
+
     public static int solution(int number) {
         overBoundaryException(number);
+
+        int result = 0;
+
+        for (int i = 1; i <= number; i++) {
+            result += count369(i);
+        }
     }
 }
