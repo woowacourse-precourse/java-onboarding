b0ca1af6f87daf5db29c2939b1d7ac6af70cf272
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-31 12:53:42 +0900

    feat: PROBLEM7 유저 관계 저장 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,11 @@
 package onboarding;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 public class Problem7 {
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -11,6 +14,7 @@ public class Problem7 {
 		for(List<String> relation : friends) {
 			sns.initUser(relation.get(0));
 			sns.initUser(relation.get(1));
+			sns.addRelation(relation);
 		}
 
 		return null;
@@ -18,14 +22,20 @@ public class Problem7 {
 
 	static class User {
 		private final String id;
+		private final Set<User> friendSet;
 
 		public User(String id) {
 			this.id = id;
+			this.friendSet = new HashSet<>();
+		}
+
+		public void addFriend(User id) {
+			friendSet.add(id);
 		}
 	}
 
 	static class Sns {
-		public static final HashMap<String, User> userMap = new HashMap<>();
+		public static final Map<String, User> userMap = new HashMap<>();
 
 		public Sns() {
 		}
@@ -44,6 +54,16 @@ public class Problem7 {
 			} else {
 				return Optional.empty();
 			}
+		}
+
+		public void addRelation(List<String> relation) {
+			User user1 = findUserById(relation.get(0))
+					.orElseThrow(() -> new RuntimeException("NotFoundUser"));
+			User user2 = findUserById(relation.get(1))
+					.orElseThrow(() -> new RuntimeException("NotFoundUser"));
+
+			user1.addFriend(user2);
+			user2.addFriend(user1);
 		}
 	}
 }
