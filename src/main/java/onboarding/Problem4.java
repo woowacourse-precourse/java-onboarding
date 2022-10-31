7d99aad5a3b86269df47d6f0f582337b23d5fd91
OhHongGi <5ghdrl@naver.com>
2022-11-01 01:15:23 +0900

    chore: Divide Function List in Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem4 {
+
     public static String solution(String word) {
+        String dictionary = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
         String answer = "";
+
         return answer;
     }
+
+    /*
+     * 주어진 문자열에 대해 사전을 통해 변환된 문장 전체 반환
+     */
+    public static String convertText(String text, String dictionary) {
+        String result = "";
+        return result;
+    }
+
+    /*
+     * 주어진 단어에 대해 사전을 통해 변환된 단어 반환
+     */
+    public static String convertWord(String word, String dictionary) {
+        return "";
+    }
+
 }
