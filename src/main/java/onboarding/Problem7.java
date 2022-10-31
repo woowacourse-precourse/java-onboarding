544eb8b8a5f804abcef90c4b9d2a11d32e289546
MIN-04 <kimji_0424@naver.com>
2022-10-31 16:24:09 +0900

    add Problem7 기능 구현 주석 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    /**
+     * 기능 구현
+     * 1. user와 이미 친구인 사람을 찾기
+     * 2. user의 친구와 친구인 사람을 찾기
+     * 3. 사용자와 함께 아는 친구 점수
+     * 4. 사용자의 타임 라인에 방문한 횟수
+     * 5. 결과값 정렬, 5개 제한
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
