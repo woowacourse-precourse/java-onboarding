d5b084d7a865783913a5bd8f3f0003adc4fa0c7e
JunTaeINC <juntea3523@naver.com>
2022-10-31 16:07:08 +0900

    Problem 7 - 제한사항 유의하여 정답출력

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -11,7 +11,7 @@ public class Problem7 {
 	 * 추천 점수가 0점인 경우 추천하지 않으며, 추천 점수가 같은 경우는 이름순으로 정렬
 	 * 점수가 가장 높은 순으로 정렬하여 최대 5명을 return
 	 */
-	
+
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 		List<String> answer = new ArrayList<>();
 		List<String> userFriend = new ArrayList<>();
@@ -47,7 +47,7 @@ public class Problem7 {
 				userScore.put(friend.get(0), userScore.get(friend.get(0)) + 10);
 			}
 		}
-		// 유저 삭제  
+		// 유저 삭제
 		userScore.remove(user);
 		// 이미 친구인 사람들은 제외
 		for (int i = 0; i < userFriend.size(); i++) {
@@ -55,12 +55,25 @@ public class Problem7 {
 				userScore.remove(userFriend.get(i));
 			}
 		}
-		return answer;
+		return setScore(userScore);
 
 	}
 
+	// 유저이름 정렬 / 점수 순 정렬
+	public static List<String> setScore(Map<String, Integer> userScore) {
+		// 점수가 0 이하는 삭제
+		List<String> answer = userScore.entrySet().stream().filter(entry -> entry.getValue() > 0)
+				.map(entry -> entry.getKey()).collect(Collectors.toList());
+		// 이름 순 정렬
+		Collections.sort(answer);
+		// 점수 순 정렬
+		answer.sort((value1, value2) -> (userScore.get(value2) - userScore.get(value1)));
+		// 길이가 5이상이면 5까지만 출력
+		if (answer.size() > 5) {
+			answer = answer.subList(0, 5);
+		}
+		return answer;
 
-
-	
+	}
 
 }
