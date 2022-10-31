1c9018487a7381b40e2fa8c3874d2ea05d16496a
DongWooE <ehddn977@naver.com>
2022-11-01 04:35:36 +0900

    [p4][change] iterate 메소드 내부에 결과값도 생성하도록 추

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,14 +4,16 @@ public class Problem4 {
 
     StringBuilder result = new StringBuilder();
     public static String solution(String word) {
-        String answer = "";
         Problem4 p4 = new Problem4();
         p4.iterate(word);
-        return answer;
+        return p4.result.toString();
     }
 
     private void iterate(String word) {
         for(char c : word.toCharArray()){
+            if(c > 'a' && c < 'z') result.append((char)('a' + 'z' - c));
+            else if(c > 'A' && c < 'Z') result.append((char)('A' + 'Z' - c));
+            else result.append(" ");
         }
     }
 }
