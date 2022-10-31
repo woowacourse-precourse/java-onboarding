8ef75509ad6bbf81e6aa98ca545efbafe50402fb
korjun1993 <corojoon93@naver.com>
2022-10-31 17:23:35 +0900

    기능추가: Problem5 solution 추가
    가장 큰 화폐부터 출금이 가능한지 확인한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        Integer[] count = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] currencies = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        // 가장 큰 화폐부터 해당 화폐로 출금이 가능한지 확인한다.
+        for (int i = 0; i < currencies.length; i++) {
+            if (currencies[i] <= money) {
+                int number = money / currencies[i];
+                count[i] += number;
+                money -= number * currencies[i];
+            }
+        }
+        return Arrays.asList(count);
     }
 }
