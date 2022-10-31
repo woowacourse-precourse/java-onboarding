ca65dc966379ab0d4c825c365ea08e67e8a80c16
iamjooon2 <iamjooon2@gmail.com>
2022-11-01 00:55:39 +0900

    feat(problem7): friends의 요소 중 user에서 입력받은 문자열을 찾는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        for (int i = 0; i < friends.size(); i += 1) {
+            if (friends.get(i).get(1) == user || friends.get(i).get(0) == user){
+
+            }
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
