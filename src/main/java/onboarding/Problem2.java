a51b248ee9f7affbf24b0cb0201f86bde1a8c35c
egoing <dudqk9696@naver.com>
2022-10-31 16:48:48 +0900

    refactor : deleteOverlap 기능 한가지로 줄이고 나머지 기능 메소드화

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,9 +14,7 @@ public class Problem2 {
         Stack<Character> left = new Stack<>();
         Stack<Character> right = new Stack<>();
 
-        for(int i=0; i<cryptogram.length(); i++){
-            left.push(cryptogram.charAt(i));
-        }
+        left = makeLeftStack(cryptogram);
 
         while(!left.isEmpty()) {
             if(right.isEmpty() || left.peek() != right.peek()) right.push(left.peek());
@@ -26,6 +24,20 @@ public class Problem2 {
             left.pop();
         }
 
+        answer = appendAnswer(right);
+
+        return answer;
+    }
+
+    public static Stack<Character> makeLeftStack(String cryptogram) {
+        Stack<Character> left = new Stack<>();
+        for(int i=0; i<cryptogram.length(); i++){
+            left.push(cryptogram.charAt(i));
+        }
+        return left;
+    }
+    public static StringBuilder appendAnswer(Stack<Character> right) {
+        StringBuilder answer = new StringBuilder();
         int rightsize = right.size();
         for(int i=0; i<rightsize; i++) {
             answer.append(right.pop());
