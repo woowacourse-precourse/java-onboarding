b374ecea55a439bf7873ed9832e0a17269e00a21
osm <osm0822@naver.com>
2022-11-01 01:42:32 +0900

    Add PROBLEM2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        //알파벳 소문자가 연속되는 경우
+        String pattern = "(([a-z])\\2{1,})";
+
+        //변화가 없을 때까지 반복
+        String prev = "";
+        while(prev != cryptogram){
+            prev = cryptogram;
+            cryptogram = cryptogram.replaceAll(pattern,"");
+        }
+
+        return cryptogram;
     }
 }
