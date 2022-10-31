1d240e159c7c93a99f3205fdd9a4e6b1688af209
jooys130 <wndustn130@gmail.com>
2022-10-31 23:24:26 +0900

    feat: <Problem2> 연속하는 중복 문자들을 삭제할 수 있다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,17 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public Problem2(){
     }
 
     public static String solution(String cryptogram) {
-        if(strLenException(cryptogram)){
-            return "";
+        if(strLenException(cryptogram) || !isStringLowerCase(cryptogram)){
+            return cryptogram;
         }
-        if(!isStringLowerCase(cryptogram)){
-            return "";
+        Stack<Character> stack = new Stack();
+        char[] ch = cryptogram.toCharArray();
+        stack.push(ch[0]);
+        for(int i = 1; i < cryptogram.length(); i++){
+            if(stack.isEmpty()){
+                stack.push(ch[i]);
+            }
+            else if(stack.lastElement() == ch[i]){
+                stack.pop();
+            }
+            else if(stack.lastElement() != ch[i]){
+                stack.push(ch[i]);
+            }
         }
-        String answer = "answer";
+        String answer = printStack(stack);
         return answer;
     }
 
@@ -26,5 +39,15 @@ public class Problem2 {
                 return false;
         }
         return true;
+    }
+    private static String printStack(Stack stack){
+        String str = "";
+        if(stack.isEmpty()){
+            return str;
+        }
+        while(!stack.isEmpty()){
+            str = stack.pop() + str;
+        }
+        return str;
     }
 }
