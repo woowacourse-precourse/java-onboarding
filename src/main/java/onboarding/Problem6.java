bc1452937189fcb3aa78f18b264666f3b2dfcaed
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 19:25:50 +0900

    ArraryList import, answer변수 ArrayList로 재정의, forms의 Email을 받을 변수화 Name을 받을 변수를 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+        List<String> formsEmail = new ArrayList<String>();
+        List<String> formsName = new ArrayList<String>();
+
+
+
         return answer;
     }
 }
