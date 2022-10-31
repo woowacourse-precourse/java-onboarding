d4853f33790bf26e649db7bbfd69eab5e41b3f9d
Leehyoungwoo <dlguddn3024@gmail.com>
2022-11-01 02:58:10 +0900

    feature: 3번 문제 기능구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,8 +6,19 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        int[] moneyPaper = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = moneyCount(money, moneyPaper);
         return answer;
     }
+    public static List<Integer> moneyCount(int money, int[] moneyPaper){
+        List<Integer> moneyCount = new ArrayList<>();
 
+        for (int len = moneyPaper.length, i=0; i<len;i++){
+            int count = money/moneyPaper[i];
+            moneyCount.add(count);
+            money-=moneyPaper[i]*count;
+        }
+        return moneyCount;
+    }
 }
