b7112ff37851e5d6fcc2cefc76fbc87873f53d38
baksohyeon <bori12370@gmail.com>
2022-10-31 16:53:53 +0900

    feat: add logic which gets score about related person with user

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,12 +1,13 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<List<String>> notFriendList = new ArrayList<>();
         HashSet<String> friendList = new HashSet<>();
         HashMap<String, Integer> suggestScoreList = new HashMap<>();
-        List<List<String>> notFriendList = new ArrayList<>();
 
         friends.forEach((relationship) -> {
             String person1 = relationship.get(0);
@@ -21,11 +22,42 @@ public class Problem7 {
             }
         });
 
-        System.out.println(friendList);
-        System.out.println(suggestScoreList);
-        System.out.println(notFriendList);
+        // 관련된 친구 수 만큼 점수 계산
+        notFriendList.forEach((connection) -> {
+            String person1 = connection.get(0);
+            String person2 = connection.get(1);
 
+            if (friendList.contains(person1)) {
+                if (!suggestScoreList.containsKey(person2)) {
+                suggestScoreList.put(person2, 10);
+                } else {
+                suggestScoreList.put(person2, suggestScoreList.get(person2) + 10);
+                }
+            }
+
+            if (friendList.contains(person2)) {
+                if (!suggestScoreList.containsKey(person1)) {
+                    suggestScoreList.put(person1, 10);
+                } else {
+                    suggestScoreList.put(person1, suggestScoreList.get(person1) + 10);
+                }
+            }
+        });
+
+        // 방문수 만큼 점수 계산
+        List<String> visitConnections = visitors.stream().filter(x -> !friendList.contains(x)).collect(Collectors.toList());
+        visitConnections.forEach((visitor) -> {
+            if (!suggestScoreList.containsKey(visitor)) {
+                suggestScoreList.put(visitor, 1);
+            } else {
+                suggestScoreList.put(visitor, suggestScoreList.get(visitor) + 1);
+            }
+        });
+
+        System.out.println(suggestScoreList);
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+
 }
