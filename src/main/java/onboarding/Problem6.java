54ea4d2e315763f66fc3c55e45a059263d1269f5
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-10-31 21:34:33 +0900

    [pb6] feat: 각 이름별로 연속되는 글자 parsing

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,15 +1,44 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String, List<Character>> charChecker = new HashMap<>();
+        initConsecutiveCharSet(charChecker, forms);
     }
 
-    private static void construct
+    private static void initConsecutiveCharSet(Map<String, List<Character>> charChecker,
+                                               List<List<String>> forms) {
+        forms.forEach(item -> {
 
+        });
+    }
+
+    private static void addStringToSet(Map<Character, List<Character>> charChecker,
+                                       String name) {
+        int i = 0;
+        Character keyChar;
+        Character nextChar;
+        List<Character> valueList = new ArrayList<>();
+
+        while (i < name.length() - 1) {
+            keyChar = name.charAt(i);
+            nextChar = name.charAt(i+1);
+
+            if (charChecker.containsKey(keyChar)) {
+                valueList = charChecker.get(keyChar);
+                valueList.add(nextChar);
+            } else {
+                valueList.add(nextChar);
+            }
+            charChecker.put(keyChar, valueList);
+            i++;
+        }
+    }
 
     // 2개씩 parsing해서 set에다 다 넣어보자.
     // 넣을때마다 교집합이 하나라도 있으면 걸린거다.
