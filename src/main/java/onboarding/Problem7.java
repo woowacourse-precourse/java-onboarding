28b542926314b6409610f16ef25003661fc4c9ff
은지 <midzy0228@gmail.com>
2022-11-01 00:08:02 +0900

    refactor[Problem7]: 10점 추가 메소드로 분리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -15,20 +15,20 @@ import java.util.HashMap;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        visitorsCount(visitors);
+        HashMap<String, Integer> map = visitorsCount(visitors);
         return answer;
     }
 
-    public static HashMap visitorsCount(List<String> visitors) {
+    public static HashMap<String, Integer> visitorsCount(List<String> visitors) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        for(int i=0; i<visitors.size(); i++) {
+        for (int i = 0; i < visitors.size(); i++) {
             String visitor = visitors.get(i);
-            if(map.get(visitor) == null) {
+            if (map.get(visitor) == null) {
                 //System.out.println("널 나옴");
                 map.put(visitor, 1);
                 continue;
             }
-            map.put(visitor, map.get(visitor)+1);
+            map.put(visitor, map.get(visitor) + 1);
         }
         return map;
     }
@@ -36,15 +36,15 @@ public class Problem7 {
     public static List<String> followedFriends(String user, List<List<String>> friends) {
         List<String> friendList = new ArrayList<>();
 
-        for(int i=0; i<friends.size(); i++) {
+        for (int i = 0; i < friends.size(); i++) {
             String idA = new String(friends.get(i).get(0));
             String idB = new String(friends.get(i).get(1));
 
             //자신과 자신은 친구일까
-            if(user.equals(idA)) {
+            if (user.equals(idA)) {
                 friendList.add(idB);
             }
-            if(user.equals(idB)) {
+            if (user.equals(idB)) {
                 friendList.add(idA);
             }
         }
@@ -52,17 +52,26 @@ public class Problem7 {
     }
 
     public static HashMap<String, Integer> newFriend(List<List<String>> friends, List<String> friendList, HashMap<String, Integer> map) {
-        for(int i=0; i<friends.size(); i++) {
+        for (int i = 0; i < friends.size(); i++) {
             String idA = new String(friends.get(i).get(0));
             String idB = new String(friends.get(i).get(1));
+            //A는 유저와 친구, B는 친구가 아니라면
+            if (friendList.contains(idA) && !friendList.contains(idB)) {
+                map = plusTenPoints(idB, map);
+            }
+            //B는 유저와 친구, A는 친구가 아니라면
+            if (friendList.contains(idB) && !friendList.contains(idA)) {
+                map = plusTenPoints(idA, map);
+            }
+        }
+        return map;
+    }
 
-            //null pointer error?
-            if(friendList.contains(idA) && ! friendList.contains(idB)) {
-                map.put(idB, map.get(idB)+10);
-            }
-            if(friendList.contains(idB) && ! friendList.contains(idA)) {
-                map.put(idA, map.get(idA)+10);
-            }
+    public static HashMap<String, Integer> plusTenPoints(String id, HashMap<String, Integer> map) {
+        if (map.containsKey(id)) {
+            map.put(id, map.get(id) + 10);
+        } else {
+            map.put(id, 10);
         }
         return map;
     }
