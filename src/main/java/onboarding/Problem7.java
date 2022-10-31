99775e6f09783a3f0d74cad7523386a4a12fc432
kkm5291 <kkm23125291@gmail.com>
2022-10-31 17:34:53 +0900

    Problem7 Feat : friends 목록 친구들 list 만들기 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
+    }
+
+
+    // 유저 친구 목록
+    public static List<String> friendsList(List<List<String>> friends) { // friends 목록 친구들 list만들기
+        List<String> list = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            String friendA = friend.get(0);
+            String friendB = friend.get(1);
+
+            if (list.contains(friendA)) {
+                continue;
+            } else
+                list.add(friendA);
+
+            if (list.contains(friendB)) {
+                continue;
+            } else
+                list.add(friendB);
+        }
+
+        return list;
     }
 }
