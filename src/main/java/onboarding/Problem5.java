d0907b606aed77debf9ef58f9a283da214e5cdb2
gyumin159 <gyumin159@DESKTOP-6CKHGBO>
2022-10-31 19:40:25 +0900

    오만원부터 1원까지 차례로 나누어 배열에 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,7 +9,10 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<Integer>(9);
         int temp = money;
         int[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        
+        for (int i : arr) {
+        	answer.add((int) money / i);
+        	temp = temp % i;
+        }
         return answer;
     }
 }
