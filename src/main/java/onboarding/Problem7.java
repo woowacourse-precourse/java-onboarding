a7c317950c4978a9661e195df80e842972fd430b
codingvegeta <hong090992@gmail.com>
2022-10-31 19:52:42 +0900

    feat : user 객체끼리 친구를 맺는 기능을 구현하였습니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,13 +7,38 @@ public class Problem7 {
         Map<String, User> userMap = new HashMap<>();
         userMap.put(user, new User(user));
 
+        for (int i = 0; i < friends.size(); i++) {
+            if (userMap.containsKey(friends.get(i).get(0))) { //0번 객체가 map에 있음
+                if (userMap.containsKey((friends.get(i).get(1)))) {//1번 객체가 map에 있음
+                    userMap.get(friends.get(i).get(0)).addFriend(userMap.get(friends.get(i).get(1)));
+                    userMap.get(friends.get(i).get(1)).addFriend(userMap.get(friends.get(i).get(0)));
+                } else { //1번 객체가 없음
+                    userMap.put(friends.get(i).get(1), new User(friends.get(i).get(1)));
+                    userMap.get(friends.get(i).get(0)).addFriend(userMap.get(friends.get(i).get(1)));
+                    userMap.get(friends.get(i).get(1)).addFriend(userMap.get(friends.get(i).get(0)));
+                }
+            } else { //0번 객체가 없음
+                if (userMap.containsKey(friends.get(i).get(1))) {//0번 객체는 없고 1번 객체는 있음;
+                    userMap.put(friends.get(i).get(0), new User(friends.get(i).get(0)));
+                    userMap.get(friends.get(i).get(0)).addFriend(userMap.get(friends.get(i).get(1)));
+                    userMap.get(friends.get(i).get(1)).addFriend(userMap.get(friends.get(i).get(0)));
+                } else {//0번객체도 없고 1번 객체도 없는경우
+                    userMap.put(friends.get(i).get(0), new User(friends.get(i).get(0)));
+                    userMap.put(friends.get(i).get(1), new User(friends.get(i).get(1)));
+                    userMap.get(friends.get(i).get(0)).addFriend(userMap.get(friends.get(i).get(1)));
+                    userMap.get(friends.get(i).get(1)).addFriend(userMap.get(friends.get(i).get(0)));
+                }
+
+                userMap.get(0);
+            }
 
 
-
+        }
         List<String> answer = Collections.emptyList();
         return answer;
     }
 }
+
 
 class User implements Comparator<User> {
     @Override
@@ -27,7 +52,7 @@ class User implements Comparator<User> {
 
     private String userName;
     private int point = 0;
-    private List<User> friendList;
+    private List<User> friendList = new ArrayList<>();
 
     public String getUserName() {
         return userName;
@@ -45,5 +70,8 @@ class User implements Comparator<User> {
         this.friendList = friendList;
     }
 
+    public void addFriend(User user) {
+        friendList.add(user);
+    }
 
 }
