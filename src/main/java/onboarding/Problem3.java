ccab46a1f6287263bf158b957b376dedcb257917
nayooniverse <kangna99@naver.com>
2022-11-01 06:44:11 +0900

    [feat] 369 박수 카운트 리턴 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        if(number < 1 || number > 10000) return -1; // 숫자 범위 예외처리
+        for(int i=1; i<=number; i++) {
+            answer += clap(i);
+        }
         return answer;
+    }
+
+    public static int clap(int num) {
+        int cnt = 0;
+        String target;
+        while(num != 0) {
+            target = Integer.toString(num % 10);
+            if(target.contains("3")) cnt++;
+            if(target.contains("6")) cnt++;
+            if(target.contains("9")) cnt++;
+            num /= 10;
+        }
+        return cnt;
     }
 }
