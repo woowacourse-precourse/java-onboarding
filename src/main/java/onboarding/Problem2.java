17c34d23936cf217868c33b89d18aedc6ad3342a
Albert <guang7226961@naver.com>
2022-10-31 10:29:57 +0900

    feat(Problem2) 기능종합 기능구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,16 @@
 package onboarding;
 
-import onboarding.problem2.CheckCrytogram;
-import onboarding.problem2.Cryptogram;
+import onboarding.problem2.CryptogramTranslation;
+import onboarding.problem2.CryptogramValidator;
 
 public class Problem2 {
 	public static String solution(String cryptogram) {
-		if (!CheckCrytogram.of(cryptogram)) {
-			throw new IllegalArgumentException("cyptogram은 모두 소문자여야 합니다.(a~z)");
+		CryptogramValidator cryptogramValidator = new CryptogramValidator();
+		if (!cryptogramValidator.validate(cryptogram)) {
+			throw new IllegalArgumentException("cryptogram은 길이가 1 이상 1000 이하인 문자열이다.\n"
+				+ "cryptogram은 알파벳 소문자로만 이루어져 있다.");
 		}
-		return Cryptogram.getRightAnswer(cryptogram);
+		CryptogramTranslation cryptogramTranslation = new CryptogramTranslation();
+		return cryptogramTranslation.translate(cryptogram);
 	}
 }
