76d9de16d1465267fb8d3a1b47e1b6cb39b5a314
chanwonlee <clw8679@naver.com>
2022-10-31 15:50:53 +0900

    FEAT: [Problem7] 친구 추천 점수 구하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,40 @@ public class Problem7 {
         return answer;
     }
 
+    public static Map<String,Integer> getScoreList(Map<String, List<String>> friendList, List<String> visitors, String user) {
+        //사용자와 함께 아는 친구의 수 +10점
+        Map<String, Integer> scoreList = new HashMap<>();
+        List<String> userFriendList = friendList.get(user);
+        for (String friend : friendList.keySet()) {
+            List<String> list = friendList.get(friend);
+            int score = getScore(userFriendList, list);
+            scoreList.put(friend, score);
+        }
+
+        //사용자의 타임 라인에 방문한 횟수 +1점
+        for (String visitor : visitors) {
+            if (!scoreList.containsKey(visitor)) {
+                scoreList.put(visitor, 1);
+            }else {
+                Integer score = scoreList.get(visitor);
+                score++;
+                scoreList.replace(visitor, score);
+            }
+        }
+
+        return scoreList;
+    }
+
+    private static int getScore(List<String> userFriendList, List<String> friendList) {
+        int score = 0;
+        for (String userFriend : userFriendList) {
+            if (friendList.contains(userFriend)) {
+                score += 10;
+            }
+        }
+        return score;
+    }
+
     public static Map<String, List<String>> friendList(List<List<String>> friends) {
         Map<String, List<String>> friendList = new HashMap<>();
         for (List<String> friend : friends) {
