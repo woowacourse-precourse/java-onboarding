b0322fe3f44cbf10d5a791dfa13bf40dc9c7fd1b
song-wooseok <sws981202@gmail.com>
2022-10-31 21:04:17 +0900

    feat: [PROBLEM6 - 기능3] 배열에 크루의 인덱스가 없을 경우 해당 크루의 인덱스 추가하는 기능

--- src/main/java/onboarding/problem6/service/NicknameListService.java
+++ src/main/java/onboarding/problem6/service/NicknameListService.java
@@ -3,7 +3,12 @@ package onboarding.problem6.service;
 import java.util.ArrayList;
 
 public class NicknameListService {
-    public static boolean isNicknameInList(ArrayList<Integer> crewIndexList, int crewIndex){
+    public static void addNewNicknameToList(ArrayList<Integer> crewIndexList, int crewIndex){
+        if(!isNicknameInList(crewIndexList, crewIndex)){
+            crewIndexList.add(crewIndex);
+        }
+    }
+    private static boolean isNicknameInList(ArrayList<Integer> crewIndexList, int crewIndex){
         return crewIndexList.contains(crewIndex);
     }
 
