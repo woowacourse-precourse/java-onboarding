b1c537d1063c3caa447096869a41698a9dc47a13
jeonghoon <shb03323@naver.com>
2022-10-31 22:38:55 +0900

    feat: problem7 유저의 친구 목록을 생성해주는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
+    private static final Set<String> userFriends = new HashSet<>();
+    private static final Map<String, Integer> recommendScoreMap = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        setFriendsOfUser(user, friends);
         return answer;
+    }
+
+    private static void setFriendsOfUser(String user, List<List<String>> friends) {
+        friends.stream()
+                .filter(friend -> friend.contains(user))
+                .forEach(userFriends::addAll);
+
+        userFriends.remove(user);
     }
 }
