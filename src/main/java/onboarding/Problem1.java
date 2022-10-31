25ef51a14f7dcb1b467558b137c74f676c86614e
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-10-31 17:39:17 +0900

    Feat(Problem1): Returns a larger value of the multiplier values

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -18,8 +18,23 @@ class Problem1 {
         return startPage + 1 == endPage;
     }
 
-    //페이지의 왼쪽면이 홀수이고 오른쪽 면이 짝수인치 체크
+    //페이지의 왼쪽면이 홀수이고 오른쪽 면이 짝수인지 체크
     private static boolean isCorrectOddAndEven(int startPage, int endPage) {
         return startPage % 2 == 1 && endPage % 2 == 0;
     }
+
+    //전부 곱한 값과 전부 더한 값 중 큰 값 판별
+    private static int calculation(int page) {
+        int score_multiply = 1; //전부 곱할 경우 값
+        int score_plus = 0;     //전부 더할 경우 값
+        while (page > 0) {
+            int temp = page % 10;
+            score_multiply *= temp;
+            score_plus += temp;
+            page /= 10;
+        }
+
+        return Math.max(score_multiply, score_plus);
+    }
+
 }
