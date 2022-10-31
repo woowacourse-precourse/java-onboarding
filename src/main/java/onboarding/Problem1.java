d0594fbbf69778bea03c8c78ada02231cb8b0861
JungHyun Jo <wjstkwhwjdgus@gmail.com>
2022-10-31 20:11:08 +0900

    done pobi result

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,6 +12,45 @@ class Problem1 {
 			return -1;
 		}
 
+		// 포비 더하기
+		int pobiLeftResult = 0;
+		int pobiRightResult = 0;
+		int pobiLeft = pobi.get(0);
+		int pobiRight = pobi.get(1);
+
+		while (pobiLeft > 0) {
+			pobiLeftResult += pobiLeft % 10;
+			pobiLeft /= 10;
+		}
+
+		while (pobiRight > 0) {
+			pobiRightResult += pobiRight % 10;
+			pobiRight /= 10;
+		}
+
+		int pobiPlus = Math.max(pobiLeftResult, pobiRightResult);
+
+		// 포비 곱하기
+		pobiLeftResult = 1;
+		pobiRightResult = 1;
+		pobiLeft = pobi.get(0);
+		pobiRight = pobi.get(1);
+
+		while (pobiLeft > 0) {
+			pobiLeftResult *= pobiLeft % 10;
+			pobiLeft /= 10;
+		}
+
+		while (pobiRight > 0) {
+			pobiRightResult *= pobiRight % 10;
+			pobiRight /= 10;
+		}
+
+		int pobiMul = Math.max(pobiLeftResult, pobiRightResult);
+
+		// 최종 포비 결과값
+		int pobiResult = Math.max(pobiPlus, pobiMul);
+
 		return answer;
 	}
 }
