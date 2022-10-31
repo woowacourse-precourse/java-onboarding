18e2d75e0b677b662522182dfead5b89bd7925d3
heohyeonjink <hyeonjin4870@naver.com>
2022-10-31 23:41:38 +0900

    feat(PROBLEM3) : 박수 횟수 세기 및 결론 도출
    각 숫자의 일의 자리수, 십의 자리 수 순으로 검토하며 3,6,9가 존재하는지 확인한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,24 @@
 package onboarding;
 
+import java.util.Set;
+
 public class Problem3 {
+
+    private static final Set<Integer> CLAP = Set.of(3,6,9);
     public static int solution(int number) {
         int answer = 0;
-
+        for(int i=1; i<=number;i++) {
+            answer+= getClapCount(i);
+        }
         return answer;
+    }
+
+    public static int getClapCount(int num){
+        int count = 0;
+        while(num!=0){
+            if(CLAP.contains(num%10)) count++;
+            num/=10;
+        }
+        return count;
     }
 }
