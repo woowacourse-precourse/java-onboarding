07b8aa50a339bc624a9d437d5e653dae5caee2c9
sujin0607 <goweg0607@gmail.com>
2022-11-01 00:32:28 +0900

    Feat: 추가 Problem2 문제 해결
    받은 문자를 하나씩 stack에 넣어 최상위 문자와 비교하여 같으면 빼고 다르면 넣는 방법으로 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        //Stack으로 구현
+        Stack<Character> stack = new Stack<>();
+
+        //시작문자는 먼저 넣기
+        stack.add(cryptogram.charAt(0));
+        //입력된 문자열을 하나씩 돌리기
+        for(int i=1; i<cryptogram.length(); i++){
+            //스텍의 최상위 문자와 입력된 문자가 같을 시 pop
+            if(stack.peek() == cryptogram.charAt(i)){
+                stack.pop();
+            }
+            //그렇지 않을 경우 add
+            else{
+                stack.add(cryptogram.charAt(i));
+            }
+        }
+
+        //stack을 문자열로 바꿈
+        String answer = "";
+        for (Character ch : stack) {
+            answer += ch;
+        }
+
         return answer;
     }
 }
