16076b5a92b58af1a2f85512620a7d8ec97a664d
somsom13 <somin1113@naver.com>
2022-10-31 14:39:40 +0900

    refactoring(문제3): 메소드 분리 / 자바 컨벤션 적용

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,96 +1,106 @@
 package onboarding;
 
 public class Problem3 {
+    private static final int MIN_NUMBER_PLACE = 1;
+    private static final int MAX_NUMBER_PLACE = 4;
+
     public static int solution(int number) {
         int answer = 0;
 
-        answer += countClapOnThousandsPlace(number);
-        answer += countClapOnHundredsPlace(number);
-        answer += countClapOnTensPlace(number);
-        answer +=countClapOnOnesPlace(number);
+        for (int i = MIN_NUMBER_PLACE - 1; i <= MAX_NUMBER_PLACE - 1; i++) {
+            answer += countClapOnPlace(number , i);
+        }
 
         return answer;
     }
 
-    private static int countClapOnThousandsPlace(int number) { //천의 자리 숫자가 파라미터
-        //천의 자리 수에 대한 계산
-        int thousandsCount = 0;
-        for (int i = 3 ; i <= 9 ; i+=3) {
-            //3, 6, 9 에 대해 테스트
-            int thisNumber = i*1000+999; //3999, 6999, 9999
-            if(thisNumber <= number) {
-                thousandsCount+=1000;
-                continue;
-            }
-            //작지 않고, 천의 자리 숫자가 3, 6, 9 중 하나라면 (ex. number == 6645, thisNumber == 6999 -> 625번 천의 자리가 '6'이므로 박수치게 됨)
-            if(number/1000 == i) {
-                int possibleCount = number%1000;
-                thousandsCount+=(possibleCount+1);
-            }
-            break;
+    private static int countClapOnPlace(int number, int place) {
+        int count = 0;
+        int checkNum;
+        int maxFrontPlace = calculateMaxFrontPlace(place);
+        for (checkNum = 3; checkNum <= 9; checkNum += 3) {
+            //천의 자리면 9*0 -> 1 * 0 : 0번 자리
+            //백의 자리면 9*1 -> 1 * 1 : 1번 자리
+            //십의 자리면 9*11 -> 1 * 11 : 2번 자리
+            //일의 자리면 9*111 -> 1 * 111 : 3번 자리 -> 자리 수 만큼 1이 붙는다
+
+            //천의 자리면 frontPlace*10000 + i * 1000 + 999 -> 0 번 자리 10^4
+            //백의 자리면 frontPlace*1000 + i * 100 + 99 -> 1번 자리 10^3
+            //십의 자리면 frontPlace*100 + i * 10 + 9 -> 2번 자리 10^2
+            //일의 자리면 frontPlace*10 + i * 1 -> 3번 자리
+            count += countNowCheckNum(checkNum, place, number, maxFrontPlace);
         }
-        return thousandsCount;
+        return count;
     }
 
-    private static int countClapOnHundredsPlace(int number) {
-        int hundredsCount = 0;
+    private static int countNowCheckNum(int checkNum, int place, int number, int maxFrontPlace) {
+        int frontPlace;
         int thisNumber;
-        for (int i = 3; i <= 9 ; i+=3) {
-            int thousandsPlace;
-            for(thousandsPlace = 0; thousandsPlace <= 9; thousandsPlace++) {
-                thisNumber = thousandsPlace*1000 + i*100 + 99;
-                if(thisNumber <= number) {
-                    hundredsCount+=100;
-                    continue;
-                }
-                //thisNumber > number (ex. number == 6645, thisNumber == 6699)
-                if (thisNumber/100==number/100) {
-                    hundredsCount+=(number%100+1);
-                }
-               // (ex. number == 6645, thisNumber == 7399) -> 0699, 1699, ... 로 넘어가야함
+        int count = 0;
+
+        for (frontPlace = 0; frontPlace <= maxFrontPlace; frontPlace++) {
+            thisNumber = calculateThisNumber(place, frontPlace, checkNum);
+            int thisNumberClapCount = checkThisNumberCount(thisNumber, number, place);
+            if (thisNumberClapCount == -1) {
                 break;
             }
+            count += thisNumberClapCount;
         }
-        return hundredsCount;
+        return count;
     }
 
-    private static int countClapOnTensPlace(int number) {
-        int tensCount = 0;
-        int thisNumber;
-        for (int i = 3; i <= 9 ; i+=3) {
-            int thousandHundredsPlace;
-            for(thousandHundredsPlace = 0; thousandHundredsPlace <= 99; thousandHundredsPlace++) {
-                thisNumber = thousandHundredsPlace*100 + i*10 + 9;
-                if(thisNumber <= number) {
-                    tensCount+=10;
-                    continue;
-                }
-                //ex. number == 6639, thisNumber == 6645
-                if (thisNumber/10 == number/10) {
-                    tensCount+=(number%10+1);
-                }
-                break;
-            }
+    private static int calculateMaxFrontPlace(int place) {
+        StringBuilder sb = new StringBuilder("0");
+        for (int i = 1; i <= place; i++) {
+            sb.append("1");
         }
-        return tensCount;
+        int maxNumber = Integer.parseInt(sb.toString());
+        maxNumber *= 9;
+        return maxNumber;
     }
 
-    private static int countClapOnOnesPlace(int number) {
-        int onesCount = 0;
-        int thisNumber;
-        int maxTensPlace = number/10;
-        for (int i = 3; i <= 9 ; i+=3) {
-            int thousandHundredTensPlace;
-            for(thousandHundredTensPlace = 0; thousandHundredTensPlace <= 999; thousandHundredTensPlace++) {
-                thisNumber = thousandHundredTensPlace*10 + i;
-                if(thisNumber <= number) {
-                    onesCount+=1;
-                    continue;
-                }
-                break;
-            }
+    private static int calculateThisNumber(int place, int frontPlace, int nowCheckNum) {
+        int frontNumber = calculateFrontNumber(place, frontPlace);
+        int midNumber = nowCheckNum * calculateDigit(place);
+        int lastNumber = calculateMaxFrontPlace(3 - place);
+
+        int thisNumber = frontNumber + midNumber + lastNumber;
+        return thisNumber;
+    }
+
+    private static int calculateDigit(int place) {
+        int digit = (int)Math.pow(10, 3 - place);
+        return digit;
+    }
+
+    private static int checkThisNumberCount(int thisNumber, int number, int place) {
+        int digit = calculateDigit(place);
+        int count = -1;
+        if (thisNumber <= number) {
+            count = digit;
+            return count;
         }
-        return onesCount;
+        if (isFrontNumberSame(thisNumber, number, digit)) {
+            int possibleCount = number % digit;
+            count = possibleCount + 1;
+            return count;
+        }
+        return count;
+    }
+
+    private static Boolean isFrontNumberSame(int thisNumber, int number, int digit) {
+        int thisFrontNumber = thisNumber / digit;
+        int frontNumber = number / digit;
+        if (thisFrontNumber == frontNumber) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int calculateFrontNumber(int place, int frontPlace) {
+        int leadingDigit = (int)Math.pow(10, (4 - place));
+        int frontNumber = frontPlace * leadingDigit;
+        return frontNumber;
     }
 
 }
