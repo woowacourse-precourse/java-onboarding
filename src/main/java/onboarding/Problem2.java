5764c52113546d84caaedfe243a254b7df2fd801
mrbonk97 <mrbonk97@gmail.com>
2022-10-31 16:03:18 +0900

    2번 클ë린코드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,20 +1,21 @@
 package onboarding;
 
 import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         StringBuilder sb = new StringBuilder();
-        Deque<Character> word = new ArrayDeque<>();
+        ArrayDeque<Character> word = new ArrayDeque<>();
 
-        for(Character e: cryptogram.toCharArray())
-        {
-            if(!word.isEmpty() && word.peekLast() == e) { word.pollLast(); continue; }
+        for (Character e : cryptogram.toCharArray()) {
+            if (!word.isEmpty() && word.peekLast() == e) {
+                word.pollLast();
+                continue;
+            }
             word.addLast(e);
         }
 
-        for(Character e: word) sb.append(e);
+        for (Character e : word) sb.append(e);
 
 
         return sb.toString();
