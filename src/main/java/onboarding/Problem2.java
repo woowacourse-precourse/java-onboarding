fd62f950df3295c4071799d03b754bf507d8bf32
eugene1204 <s1061001@naver.com>
2022-10-31 16:06:54 +0900

    기능 목록 : problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,9 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
-
+// 1. 문자열 분리하는 메소드
+// 2. 중복된 글자를 없애주는 메소드
+// 3. 중복 없어진 메소드를 출력함 -> solution
 
 public class Problem2 {
     public static String solution(String cryptogram) {
