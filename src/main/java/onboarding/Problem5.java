a5dde478488b3d1e5fc877e2ac1a8e70ab58f11e
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-31 09:19:22 +0900

    docs(Problem5): 구현할 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+// <기능 목록>
+// 1. 화폐로 변환하기
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
