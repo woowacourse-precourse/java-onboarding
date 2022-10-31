9cf3997fbb3242dd0cc818d6a395e876bf7ab5e2
3juhwan <13selfesteem91@naver.com>
2022-10-31 09:14:48 +0900

    update solution function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    static int[] numOfMltplOfThree = new int[10001];
+
+    public static int getNumOfMltplOfThree(int number) {
+        return 0;
+    }
+
+    public static void makeNumOfMltplOfThree() {
+
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        makeNumOfMltplOfThree();
+        return numOfMltplOfThree[number];
     }
 }
