7a7f8ce3d0cb6ae5a79b786e2a1cc4d804f4efd4
OhHongGi <5ghdrl@naver.com>
2022-11-01 00:47:27 +0900

    chore: Implement calBigOfMulAndAdd method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,7 +15,22 @@ class Problem1 {
      주어진 숫자의 각 자리수의 더한 값과 곱한 값 중 큰 값을 반환
      */
     public static int calBigOfMulAndAdd(Integer number) {
-        return number;
+        int a_hundred_digit_num = number / 100;
+        int a_decimal_digit_num = (number % 100) / 10;
+        int a_first_digit_num = (number % 100) % 10;
+        int sum_of_digit = a_hundred_digit_num + a_decimal_digit_num +
+            a_first_digit_num;
+        int mul_of_digit = 0;
+        if (a_hundred_digit_num == 0) {
+            mul_of_digit = a_decimal_digit_num *
+                a_first_digit_num;
+        } else if (a_hundred_digit_num != 0) {
+            mul_of_digit = a_hundred_digit_num * a_decimal_digit_num *
+                a_first_digit_num;
+        }
+
+        int result = Math.max(sum_of_digit, mul_of_digit);
+        return result;
     }
 
     /*
