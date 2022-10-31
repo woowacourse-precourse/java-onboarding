48d3e195a4db4177755354df43ffaa40337f2948
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 15:44:48 +0900

    연속된 문자를 빈칸으로 바꿨기에 start에 빈칸이 들어있으면 연속된 문자가 있는지 확인하는 작업을 하지 않고 루프 탈출

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,6 +10,7 @@ public class Problem2 {
             for (int start = 0; start < cryptoAvail.length() - 1; start++) {
                 int end = 0;
                 for (int i = start; i < cryptoAvail.length() - 1; i++) {
+                    if (cryptoAvail.charAt(i) == ' ') break;
                     if (cryptoAvail.charAt(i) != cryptoAvail.charAt(i + 1)) break;
                     if (cryptoAvail.charAt(i) == cryptoAvail.charAt(i + 1)) end = i + 1;
                 }
