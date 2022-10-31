520fc47a53c88570942490d5688e0b8cc02cbe2a
Egenieee <hhgg0925@naver.com>
2022-10-31 21:27:54 +0900

    [FIX] Problem2 홀수 번 문자가 연속될 때 삭제되지 않는 버그 수정
    - 마지막으로 지운 문자와 stack의 peek와 같으면 다시 pop하도록 한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,23 +6,34 @@ public class Problem2 {
     private static final Stack<Character> stack = new Stack<>();
 
     public static String solution(String cryptogram) {
+        char removedLetter = '.';
+
         for (int index = 0 ; index < cryptogram.length() ; index++) {
             if (stack.isEmpty()) {
                 stack.add(cryptogram.charAt(index));
             } else {
-                removeDuplicateString(index, cryptogram);
+                removedLetter = removeDuplicateString(index, cryptogram, removedLetter);
             }
         }
 
         return getOriginalInformation();
     }
 
-    private static void removeDuplicateString(int index, String cryptogram) {
+    private static char removeDuplicateString(int index, String cryptogram, char removedLetter) {
         if (stack.peek() == cryptogram.charAt(index)) {
-            stack.pop();
-        } else {
+            return stack.pop();
+        } else  {
             stack.add(cryptogram.charAt(index));
+            return removeOddDuplicateString(removedLetter);
         }
+    }
+
+    private static char removeOddDuplicateString(char removedLetter) {
+        if (stack.peek() == removedLetter && removedLetter != '.') {
+            return stack.pop();
+        }
+
+        return '.';
     }
 
     private static String getOriginalInformation() {
