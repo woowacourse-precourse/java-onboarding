13c266f9b6e96f627030a5b3afeff7ebebe381d6
Isu Shin <isu010614@naver.com>
2022-10-31 18:19:08 +0900

    feat: Add function to check if ID is acquaintance
    - Must be a friend of someone who is already friends with user
    - Except the userID

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,10 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     static HashSet<String> alreadyFriends = new HashSet<>();
+    static List<String> acquaintance = new ArrayList<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
@@ -27,5 +26,37 @@ public class Problem7 {
         if (rightID.equals(user)) {
             alreadyFriends.add(leftID);
         }
+    }
+
+    public static void addAcquaintanceID(List<List<String>> friends, String user) {
+        for (List<String> readOneList : friends) {
+            String leftID = readOneList.get(0);
+            String rightID = readOneList.get(1);
+            checkAcquaintance(leftID, rightID, user);
+        }
+    }
+
+    public static void checkAcquaintance(String leftID, String rightID, String user) {
+        for (Iterator<String> alreadyFriend = alreadyFriends.iterator(); alreadyFriend.hasNext(); ) {
+            isAcquaintance(leftID, rightID, user, alreadyFriend.next());
+        }
+    }
+
+    public static void isAcquaintance(String leftID, String rightID, String user, String alreadyFriend) {
+        if (excludeUser(leftID, rightID, user)
+                && leftID.equals(alreadyFriend)) {
+            acquaintance.add(rightID);
+        }
+        if (excludeUser(leftID, rightID, user)
+                && rightID.equals(alreadyFriend)) {
+            acquaintance.add(leftID);
+        }
+    }
+
+    public static boolean excludeUser(String leftID, String rightID, String user) {
+        if (!leftID.equals(user) && !rightID.equals(user)) {
+            return true;
+        }
+        return false;
     }
 }
