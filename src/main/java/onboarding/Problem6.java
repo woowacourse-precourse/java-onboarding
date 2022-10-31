281b8ef92df3ef11b560c67771e98f458080e90a
taekwon-dev <taekwon_dev@naver.com>
2022-10-31 20:52:14 +0900

    problem 6 - feat : 같은 글자가 연속적으로 포함되는 닉네임 조합 갖고 있는 이메일 목록 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,16 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+
 
 public class Problem6 {
+    private static Map<String, String> map = new HashMap<>();
+    private static Set<String> set = new TreeSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickName = form.get(1);
+            nickNameCombination(email, nickName);
+        }
+        List<String> answer = new ArrayList<>(set);
         return answer;
     }
 
-    private static void nickNameCombination(String nickName) {
+    private static void nickNameCombination(String email, String nickName) {
         for (int i = 0; i < nickName.length() - 1; i++) {
             String combination = nickName.substring(i, i + 2);
+            final String prevEmail = map.get(combination);
+            if (map.containsKey(combination)) {
+                set.add(prevEmail);
+                set.add(email);
+            }
+            map.put(combination, email);
         }
     }
 }
