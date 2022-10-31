fd9a9d7e64ea304a227db91ef27c7fd9cb4749fa
yxnxx <jh4rgh0@gmail.com>
2022-11-01 00:55:58 +0900

    feat: Add Problem6 making email list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,31 +4,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         HashMap<String, String> word_list = new HashMap<>();
         HashSet<String> email_list = new HashSet<>();
-//        for (List<String> crew : forms) {
-//            crew_list.put(crew.get(0), crew.get(1));
-//        }
-        List<String> email;
+//
         for(List<String> crew : forms) {
-            email = checkNickname(crew, word_list);
-            if(email != null) {
-                //중복임. 이메일 목록에 추가
-                email_list.add()
-            }
-            // 중복 확인 끝난 crew
-            word_list.put(crew.get(0), crew.get(1));
+            email_list.addAll(checkNickname(crew, word_list));
         }
+        answer = email_list.stream().sorted().collect(Collectors.toList());
         return answer;
     }
-    public static List<String> checkNickname(List<String> crew, HashMap<String, String> word_list) {
+    public static HashSet<String> checkNickname(List<String> crew, HashMap<String, String> word_list) {
         String email = crew.get(0);
         String nickname = crew.get(1);
-        List<String> nicklist = new ArrayList<>();
+        HashSet<String> nicklist = new HashSet<>();
+
         if(word_list.isEmpty()) nicklist = null;
         for(int i = 0; i < nickname.length() -1; i++) {
             String find_nick =  nickname.substring(i, i+2);
