deb33da002172b858d222db77438bf6414d5d17a
dev-green-flamingo <devgflamingo@gmail.com>
2022-10-31 23:25:02 +0900

    feat: problem7 구현
    - 사용자와 함께 아는 친구의 수 => 친구가 아닌 사용자들 중에 사용자와 친구인 사람과 친구인 관계(친구의 친구 관계)
    - 해시맵 구조를 이용하여 사용자의 근처 친구목록을 관리합니다.
       - key: 사용자명
       - value: 친구관계인 사용자
    - 사용자의 친구목록을 구하여 친구목록의 각 친구의 친구의 점수를 계산합니다.
       - 이때 사용자나 사용자의 친구여부는 구별하지 않고 모두 계산합니다.
       - 어차피 답을 고르는 과정에서 필터링을 수행하므로 계산해도 문없다고 판단합니다.
    - 힙 자료구조를 이용하여 삽입과 정렬을 동시에 수행하고 최대 5개를 추출합니다.
       - 친추 추천의 대상이 될 수 없는 사용자는 추가하지 않습니다.
    - 정렬기준
       1. score기준으로 내림차순 정렬
       2. score가 같다면 이름순으로 오름차순 정렬

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,67 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        Map<String, Set<String>> person2Friends = createPerson2Friends(friends);
+        Map<String, Integer> person2Score = createPerson2Score(user, visitors, person2Friends);
+        pickUpTop5ByScore(user, answer, person2Friends, person2Score);
         return answer;
     }
+
+    private static Map<String, Set<String>> createPerson2Friends(List<List<String>> friends) {
+        Map<String, Set<String>> person2Friends = new HashMap<>();
+        for (List<String> friend : friends) {
+            for (int i = 0; i < friend.size(); i++) {
+                String person1 = friend.get(i);
+                String person2 = friend.get((i + 1) % 2);
+                if (!person2Friends.containsKey(person1)) {
+                    person2Friends.put(person1, new HashSet<>());
+                }
+                person2Friends.get(person1).add(person2);
+            }
+        }
+        return person2Friends;
+    }
+
+    private static Map<String, Integer> createPerson2Score(String user, List<String> visitors, Map<String, Set<String>> person2Friends) {
+        Map<String, Integer> person2Score = new Hashtable<>();
+        for (String friend : person2Friends.get(user)) {
+            for (String friendOfFriend : person2Friends.get(friend)) {
+                person2Score.put(friendOfFriend, person2Score.getOrDefault(friendOfFriend, 0) + 10);
+            }
+        }
+        for (String visitor : visitors) {
+            person2Score.put(visitor, person2Score.getOrDefault(visitor, 0) + 1);
+        }
+        return person2Score;
+    }
+
+    private static void pickUpTop5ByScore(String user, List<String> answer, Map<String, Set<String>> person2Friends, Map<String, Integer> person2Score) {
+        Queue<String> queue = new PriorityQueue<>(
+                (p1, p2) -> {
+                    int cmp = person2Score.get(p2) - person2Score.get(p1);
+                    if (cmp != 0) {
+                        return cmp;
+                    }
+                    return p1.compareTo(p2);
+                }
+        );
+        for (String person : person2Score.keySet()) {
+            if (person.equals(user) || person2Friends.get(user).contains(person) || !person2Score.containsKey(person))
+                continue;
+            if (person2Score.get(person) == 0) {
+                continue;
+            }
+            queue.add(person);
+        }
+        while (answer.size() < 5 && !queue.isEmpty()) {
+            answer.add(queue.remove());
+        }
+    }
+
 }
