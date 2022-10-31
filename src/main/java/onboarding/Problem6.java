00686458bb4267f6c4a74336a496f7adf309716f
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-01 02:01:48 +0900

    Feat(Problem6): 이메일이 형식에 부합한지 11자 이상 20자 미만 도메인으로 email.com을 사용하는지

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,64 @@ import java.util.List;
 
 public class Problem6 {
 
+    class Form{
+        String nickname;
+        String email;
+
+        public Form(String nickname, String email) throws Exception {
+            if (!isValidEmail(email)) {
+                throw new Exception("Wrong email");
+            }
+
+            if (!isValidNickname(nickname)) {
+                throw new Exception("Wrong name");
+            }
+
+            this.nickname = nickname;
+            this.email = email;
+        }
+
+        private boolean isValidNickname(String nickname) {
+            if (!isKoreanNickname(nickname)) return false;
+            if (!isValidNicknameSize(nickname.length())) return false;
+
+            return true;
+        }
+
+        private boolean isValidNicknameSize(int length) {
+            return length >= 1 && length < 20;
+        }
+
+        private boolean isKoreanNickname(String nickname) {
+            boolean isKorean = true;
+            for (int i = 0; i < nickname.length() ; i++) {
+                isKorean = isKoreanCharacter(nickname.charAt(i));
+            }
+
+            return isKorean;
+        }
+
+        private boolean isKoreanCharacter(char c) {
+            return (c >= '가' && c <= '힣') || (c >= 'ㄱ' && c <= 'ㅎ');
+        }
+
+        private boolean isValidEmail(String email) {
+            if (!isValidEmailSize(email.length())) return false;
+            if (!isContainsEmail(email)) return false;
+
+            return false;
+        }
+
+        private boolean isContainsEmail(String email) {
+            String[] s = email.split("@");
+            return s[1].equals("email.com");
+        }
+
+        private boolean isValidEmailSize(int length) {
+            return length >= 11 && length< 20;
+        }
+    }
+
     private static boolean isValidPersonal(int n) {
         return n >= 1 && n <= 10_000;
     }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    public static void main(String[] args) {
+
+
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
