e2527960f69d87968bbfecd9bbc1dc642319fe69
sen <seeunchoi99@gmail.com>
2022-10-31 22:05:30 +0900

    💡 Problem6 기능 목록 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 현재 닉네임 중복인지 판별
+ * 2. result에서 이메일 오름차순으로 정렬
+ * 3. result에서 이메일 중복 제거
+ */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
