ad3f72a888f8e2f03b7ba43e81ce5ab7923dca34
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:42:56 +0900

    [problem6] UserRepository getUsers함수 추사

--- src/main/java/onboarding/problem6/UserRepository.java
+++ src/main/java/onboarding/problem6/UserRepository.java
@@ -2,11 +2,16 @@ package onboarding.problem6;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserRepository {
     List<User> userRepository = new ArrayList<>();
 
     public void create(List<String> data) {
+    }
+
+    public List<String> getUsers() {
+        return this.userRepository.stream().map(user->user.getEmail()).collect(Collectors.toList());
     }
 
     //userRepository에 User 추가하는 함수 이 때 모든 배열 요소에 체크 함수를 검사한다.
