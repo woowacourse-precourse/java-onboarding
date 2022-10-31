7b4ed810b7d31f4c2c0e3a6a06e940a477b846b7
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-31 15:36:45 +0900

    feat: 사용자의 친구 리스트 찾기 (#7)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 
 public class Problem7 {
+    private static final List<String> USER_FRIENDS = new ArrayList<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        getUserFriends(user, friends);
+        return new ArrayList<>();
+    }
+
+    private static void getUserFriends(String user, List<List<String>> friends) {
+        for (List<String> info : friends) {
+            String userA = info.get(0);
+            String userB = info.get(1);
+
+            if (userA.equals(user)) {
+                USER_FRIENDS.add(userB);
+            }
+            if (userB.equals(user)) {
+                USER_FRIENDS.add(userA);
+            }
+        }
     }
 }
