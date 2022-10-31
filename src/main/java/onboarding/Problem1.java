9f247cb829a33e693a22d54596988285491f90c0
Kijung Park <kjpark4321@naver.com>
2022-10-31 22:47:43 +0900

    1번: 페이지숫자 자리 더하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,13 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        
+        getMultiplyOfNumber(pobi.get(0));
+        getSumOfNumber(pobi.get(0));
         return answer;
+    }
+
+    private int selectMultiplyOrSumLarger (Integer targetNum){
+
     }
 
     private static int getMultiplyOfNumber(Integer targetNum) {
@@ -17,6 +22,21 @@ class Problem1 {
         int curPos = 10;
         while (leftNum != 0) {
             ret *= leftNum%curPos;
+            leftNum /= curPos;
+            curPos *= 10;
+        }
+
+        System.out.println(ret);
+        return ret;
+    }
+
+    private static int getSumOfNumber(Integer targetNum) {
+        int ret = 1;
+
+        int leftNum = targetNum;
+        int curPos = 10;
+        while (leftNum != 0) {
+            ret += leftNum%curPos;
             leftNum /= curPos;
             curPos *= 10;
         }
