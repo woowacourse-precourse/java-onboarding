a13acb1fb5de6af119345224104c54025e4aeab8
swjeong98 <tree5678@konkuk.ac.kr>
2022-10-31 15:12:15 +0900

    입력의  유효성을 확인하는 함수 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 class Problem1 {
+    public static boolean isValidInput(List<Integer> arr){
+        int left = arr.get(0);
+        int right = arr.get(1);
+        if(left < 1 || left > 400) return false;
+        if(right < 1 || right > 400) return false;
+
+        if(left % 2 == 0) return false;
+        if(right % 2 != 0) return false;
+
+        if(right - left != 1) return false;
+
+        return true;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        boolean pobi_valid = isValidInput(pobi);
+        if(!pobi_valid) return -1;
+        boolean crong_valid = isValidInput(crong);
+        if(!crong_valid) return -1;
         return answer;
     }
 }
