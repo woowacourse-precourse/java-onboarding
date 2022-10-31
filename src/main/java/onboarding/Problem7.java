626bb142589b303154d35b66d21a9faf7fa3470f
hhiyeon <hhyyeon0214@gmail.com>
2022-10-31 20:27:41 +0900

    feat: p7-add friend name to map

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,12 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void addFriendToMap(Map<String, List<String>> friendsMap, String userA, String userB) {
+        friendsMap.compute(userB, (k, v) -> v == null ? new ArrayList<>() : v).add(userA);
     }
 
     private static boolean isSameName(String friendName, String userName) {
