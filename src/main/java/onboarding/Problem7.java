cb66c17209bc0463d494d1cfcbc2fd038fd8c952
JungHun Jang <ijj5258@naver.com>
2022-10-31 16:55:23 +0900

    Problem7 (feat) User와 친구관계를 확인하는 getAlreadyFriend와 getusersFriend 메소드 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+	private static List<String> getAlreadyFriends
+	(String user,List<List<String>> friends){
+		List<String> alreadyFriendList = new ArrayList<String>();
+		String myFriend = "";
+		for(List<String> friendList : friends) {
+			if(friendList.contains(user)) {
+				myFriend = getUsersFriend(user, friendList);
+				alreadyFriendList.add(myFriend);
+			}
+		}
+		return alreadyFriendList;
+	}
+	private static String getUsersFriend
+	(String user,List<String> friendList) {
+		String returnId= "";
+		for(String id : friendList) {
+			if(id != user) {
+				returnId = id;
+				return returnId;
+			}
+		}
+		return returnId;
+	}
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        //"이미 친구"
+        List<String> alreadyFriends = getAlreadyFriends(user,friends);
+
+		
+		
         return answer;
     }
 }
