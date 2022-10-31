0ff1540d741efc3600fb852a0cc372e29ac83982
kkkwp <chxrryda@gmail.com>
2022-11-01 03:44:05 +0900

    feat(countFriend): 함께 아는 친구의 수를 계산하여 점수 추가하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,6 +16,33 @@ public class Problem7 {
         return answer;
     }
 
+    public static void countFriend(String user, List<List<String>> friends, HashSet<String> relation, Map<String, Integer> score) {
+        for (List<String> friend : friends) {
+            String known = friend.get(0);
+            String unknown = friend.get(1);
+            if (known.equals(user) || unknown.equals(user)) {
+                continue;
+            }
+            if (relation.contains(known) && relation.contains(unknown)) {
+                continue;
+            }
+            if (relation.contains(known)) {
+                if (score.containsKey(unknown)) {
+                    score.put(unknown, score.get(unknown) + 10);
+                    continue;
+                }
+                score.put(unknown, 10);
+            }
+            if (relation.contains(unknown)) {
+                if (score.containsKey(known)) {
+                    score.put(known, score.get(known) + 10);
+                    continue;
+                }
+                score.put(known, 10);
+            }
+        }
+    }
+
     public static void makeRelation(String user, List<List<String>> friends, HashSet<String> relation) {
         for (List<String> friend : friends) {
             String known = friend.get(0);
