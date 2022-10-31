13fe2e15df7ed2f29dedc058c3a3e1746ddc0fa8
yoonjuho <dbswn990@naver.com>
2022-10-31 18:52:17 +0900

    feat: PROBLEM1-(예외처리) 페이지 순서 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,6 +15,10 @@ class Problem1 {
     private static boolean hasException(List<Integer> pobi, List<Integer> crong) {
         if (pobi.size() != 2 || crong.size() != 2)
             return true;
+        else if (pobi.get(0) % 2 != 1 || (pobi.get(0) + 1) != pobi.get(1))
+            return true;
+        else if (crong.get(0) % 2 != 1 || (crong.get(0) + 1) != crong.get(1))
+            return true;
         return false;
     }
 
