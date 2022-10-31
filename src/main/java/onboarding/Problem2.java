9de92d53859aa88ab8f104841e57eb99d64524b4
bradbrad97 <taehyun0121@naver.com>
2022-11-01 02:39:45 +0900

    fix: problem2 세개 이상 중복 검출 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,25 +1,31 @@
 package onboarding;
 
 import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         Stack<Character> stack = new Stack<Character>();
-
-        for (int i = 0; i<cryptogram.length(); i++){
+        char prevChar = 'X';
+        for (int i = 0; i < cryptogram.length(); i++) {
             char currentChar = cryptogram.charAt(i);
-            if (stack.empty() || stack.peek() != currentChar){
-                stack.push(currentChar);
+            if (stack.empty()){
+                if (currentChar != prevChar)
+                    stack.push(currentChar);
+            } else{
+                if (stack.peek() == currentChar){
+                    stack.pop();
+                } else if (currentChar != prevChar){
+                    stack.push(currentChar);
+                }
             }
-            else{
-                stack.pop();
-            }
+            prevChar = currentChar;
         }
 
-        String answer = "";
-        while(!stack.empty()){
-            answer = stack.pop() + answer;
+        StringBuilder answer = new StringBuilder();
+        while (!stack.empty()) {
+            answer.insert(0, stack.pop());
         }
 
-        return answer;
+        return answer.toString();
     }
 }
