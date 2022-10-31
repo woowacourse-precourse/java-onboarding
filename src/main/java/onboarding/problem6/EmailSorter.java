be567e7d00b1dda85d9c071ecf966d13a49d5522
xxeol2 <jhj01177@naver.com>
2022-10-31 14:52:56 +0900

    feat(prob6): 이메일 중복체크 기능 구현

--- src/main/java/onboarding/problem6/EmailSorter.java
+++ src/main/java/onboarding/problem6/EmailSorter.java
@@ -2,16 +2,18 @@ package onboarding.problem6;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EmailSorter implements ResultStyleFormatter {
 
 	private List<String> getEmails(List<Crew> crews) {
-		List<String> emails = new ArrayList<>();
+		Set<String> emails = new HashSet<>();
 		for (Crew crew : crews) {
 			emails.add(crew.getEmail());
 		}
-		return emails;
+		return new ArrayList<>(emails);
 	}
 
 	private List<String> sortEmails(List<String> emails) {
