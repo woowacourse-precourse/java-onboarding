55011d938e7c9b6aa7bc736a3043a411352fdbcb
taekwon-dev <taekwon_dev@naver.com>
2022-10-31 20:40:50 +0900

    problem 6 - feat : 길이 2 인 닉네임 조합 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,10 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static void nickNameCombination(String nickName) {
+        for (int i = 0; i < nickName.length() - 1; i++) {
+            String combination = nickName.substring(i, i + 2);
+        }
+    }
 }
