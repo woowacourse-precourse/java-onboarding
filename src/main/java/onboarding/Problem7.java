8294c989b6d0e6830a3dd4929ce0db108fd6df57
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-11-01 00:46:55 +0900

    feat: implement save user friend list

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
+
+    private static final Set<String> userFriendList = new HashSet<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        saveUserFriendList(user, friends);
+        return Collections.emptyList();
+    }
+
+    private static void saveUserFriendList(String user, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            addFriendToSet(user, friend);
+        }
+    }
+
+    private static void addFriendToSet(String user, List<String> friend) {
+        if (user.equals(friend.get(0))) {
+            userFriendList.add(friend.get(1));
+        } else if (user.equals(friend.get(1))) {
+            userFriendList.add(friend.get(0));
+        }
     }
 }
