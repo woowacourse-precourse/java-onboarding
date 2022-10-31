358521497d9bef02571773146fe8989bcc808cde
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 20:53:18 +0900

    Feat(Problem3) : count369 메서드 추가 1부터 전달받은 숫자까지 3, 6, 9가 등장한 횟수를 계산하는 메서드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,23 @@ import java.util.regex.Pattern;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = count369(number);
         return answer;
+    }
+
+    private static int count369(int number) {
+        String numStr;
+        Pattern pattern;
+        Matcher match;
+        int count = 0;
+        for(int i = 1; i < number+1; i++) {
+            numStr = Integer.toString(i);
+            pattern = Pattern.compile("([369])");
+            match = pattern.matcher(numStr);
+            while(match.find()) {
+                count = count + 1;
+            }
+        }
+        return count;
     }
 }
