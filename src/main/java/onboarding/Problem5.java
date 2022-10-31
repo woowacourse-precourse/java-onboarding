b73714ec28cca7750ff0575070f5418f9ec338a9
drunkenhw <drunkenhw@gmail.com>
2022-10-31 19:04:56 +0900

    refactor(problem5) : 추상적인 변수명을 변경한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    private static final List<Integer> WON = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    private static final List<Integer> UNIT_OF_WON = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
     public static List<Integer> solution(int money) {
         List<Integer> wallet = new ArrayList<>();
-        for (int unitOfMoney : WON) {
+        for (int unitOfMoney : UNIT_OF_WON) {
             wallet.add(money / unitOfMoney);
             money = money % unitOfMoney;
         }
