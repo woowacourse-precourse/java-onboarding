b9e70cab09466090f9d2449d7a1c0aaaa694e7f7
khsrla9806 <kimhunsope@gmail.com>
2022-10-31 14:08:17 +0900

    refactor(Problem1): 기능 단위의 메소드로 모든 코드 리펙토링 진행

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,60 +1,67 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
-    public static int maxValue(List<Integer> pageArray) {
-        List<Integer> values = new ArrayList<>();
-
-        // 왼쪽, 오른쪽 페이지 각자리의 곱
-        for(int i = 0; i < 2; i++) {
-            if(pageArray.get(i) / 100 != 0) {    // 페이지가 100 ~ 400인 경우
-                values.add(i, (pageArray.get(i) / 100) * (pageArray.get(i) % 100 / 10) * (pageArray.get(i) % 10));
-            } else if(pageArray.get(i) / 10 != 0) {    // 페이지가 10 ~ 99인 경우
-                values.add(i, (pageArray.get(i) / 10) * (pageArray.get(i) % 10));
-            } else {    // 페이지가 1 ~ 9인 경우
-                values.add(i, pageArray.get(i));
-            }
+    public static int getPageMultiplication(int page) {        // page 각 자리수의 곱을 반환하는 메소드
+        if (page / 100 != 0) {        // 100 <= page <= 400
+            return (page / 100) * (page % 100 / 10) * (page % 10);
         }
-
-        // 왼쪽 페이지 각 자리수 합
-        values.add(2, (pageArray.get(0) / 100) + (pageArray.get(0) % 100 / 10) + (pageArray.get(0) % 10));
-        // 오른쪽 페이지 각 자리수 합
-        values.add(3, (pageArray.get(1) / 100) + (pageArray.get(1) % 100 / 10) + (pageArray.get(1) % 10));
-
-        // 4가지 값이 들어있는 배열 values 오름차순으로 정렬
-        Collections.sort(values);
-
-        return values.get(3);
+        if (page / 10 != 0) {        // 10 <= page <= 99
+            return (page / 10) * (page % 10);
+        }
+        return page;                 // 1 <= page <= 9
     }
 
-    public static boolean pageException(List<Integer> pageArray) {
-        // 왼쪽 페이지에서 1을 더한 값이 오른쪽 페이지인지 검사합니다.
-        if(pageArray.get(1) != pageArray.get(0) + 1) {
-            return false;
+    public static int getPageSum(int page) {        // page 각 자리수의 합을 반환하는 메소드
+        return (page / 100) + (page % 100 / 10) + (page % 10);
+    }
+
+    public static int getMaxvalue(List<Integer> player) {        // 플레이어의 페이지 합, 곱 중에서 가장 큰 값을 반환하는 메소드
+        int maxValue = 0;
+
+        for(int i = 0; i < 2; i++) {
+            maxValue = Math.max(maxValue, getPageSum(player.get(i)));
+            maxValue = Math.max(maxValue, getPageMultiplication(player.get(i)));
         }
 
-        // 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수인지 확인합니다.
-        if(pageArray.get(0) % 2 == 0 || pageArray.get(1) % 2 != 0) {
-            return false;
+        return maxValue;
+    }
+
+    public static int playGame(List<Integer> pobi, List<Integer> crong) {        // 게임을 시작하는 메소드
+        if (getMaxvalue(pobi) > getMaxvalue(crong)) {
+            return 1;        // 포비의 승리
+        }
+        if (getMaxvalue(pobi) < getMaxvalue(crong)) {
+            return 2;        // 크롱의 승리
+        }
+        return 0;            // 무승부
+    }
+
+    public static boolean hasPageException(List<Integer> pageList) {        // page 예외 사항이 존재하는 확인하는 메소드
+        int leftPage = pageList.get(0);
+        int rightPage = pageList.get(1);
+
+        if (leftPage < 1 && leftPage > 400 && rightPage < 1 && rightPage > 400) {        // page 범위 확인
+            return true;
         }
 
-        return true;
+        if (rightPage != leftPage + 1) {        // 왼쪽 페이지와 오른쪽 페이지가 인접한 페이지인지 확인
+            return true;
+        }
+
+        if (leftPage % 2 == 0 || rightPage % 2 != 0) {        // 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수인지 확인
+            return true;
+        }
+
+        return false;
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(pageException(pobi) && pageException(crong)) {
-            if(maxValue(pobi) > maxValue(crong)) {
-                return 1;    // 포비 승
-            } else if(maxValue(pobi) < maxValue(crong)) {
-                return 2;    // 크롱 승
-            } else {
-                return 0;    // 무승부
-            }
-        } else {
+        if (hasPageException(pobi) || hasPageException(crong)) {
             return -1;    // 페이지 예외 발생
         }
+
+        return playGame(pobi, crong);
     }
 }
