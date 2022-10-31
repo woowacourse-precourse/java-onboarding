73cb9f520a467061613becbfe0157ead82a2f18c
leesangjin <tkdwls0628@gmail.com>
2022-10-31 18:40:16 +0900

    P2, 구현 기능목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static void initList(String cryptogram, List<Character> characterList) {
+
+    }
+
+    private static boolean isAnyRemovable(List<Character> characterList) {
+        return false;
+    }
+
+    private static void deduplicate(List<Character> characterList, boolean flag) {
+
+    }
+
+    private String charListToString(List<Character> characterList) {
+        return null;
     }
 }
