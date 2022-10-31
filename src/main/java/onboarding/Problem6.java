f77e42d5620103651e1f44c4f6f0fa9705022210
chaeyeong222 <iky1505@naver.com>
2022-10-31 14:43:29 +0900

    문제 7번 기능구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        HashMap<Integer, List> checkword = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+
+            String check = forms.get(i).get(1);
+            map.put(forms.get(i).get(0), forms.get(i).get(1)); //맵에 넣기
+
+            String a =" ";
+            list.clear();
+            for (int j = 0; j < check.length()-1; j++) {
+                a =" ";
+                char [] word = check.toCharArray();
+                a += word[j];
+                a += word[j+1]; //두 글자씩 자름
+                list.add(a);
+                System.out.println("리스트" + list);
+
+            } checkword.put(i, list);
+            System.out.println(checkword);
+        }
         return answer;
     }
 }
