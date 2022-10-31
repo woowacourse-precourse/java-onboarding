d9e91c2f133f37e222cefff9c284700aa13fb627
Yebin <lyb2325@gmail.com>
2022-11-01 04:44:15 +0900

    feat: Problem5 각 화폐 단위에 대해 반환될 개수 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,14 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
         List<Integer> changeList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
         int leftMoney = money;
 
+        for (Integer change : changeList) {
+            answer.add(divideByChange(leftMoney, change));
+            leftMoney = getRemainder(leftMoney, change);
+        }
         return answer;
+    }
+
+    private static int divideByChange(int money, int change) {
+        return (money / change);
+    }
+
+    private static int getRemainder(int money, int change){
+        return (money % change);
     }
 }
