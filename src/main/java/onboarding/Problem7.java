20c621a4b09eef01cf077fe76ae9fc9baf0b096b
wwwndus <simock22@gmail.com>
2022-10-31 20:01:41 +0900

    fix: 함께 아는 친구를 추가할 Map을 선언하고 실행하기 위한 메소드 선언 commit 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,26 +16,18 @@ public class Problem7 {
     }
 
     private static void addAcquaintanceInMap(String user, List<List<String>> friends, List<String> friendsOfUser, Map<String, Integer> scoreMap) {
-        for (List<String> friend : friends) {
-            for (String friendOfUser : friendsOfUser) {
-                if (friend.contains(friendOfUser) && !friend.contains(user)) {
-                    for (int i = 0; i < friend.size(); i++) {
-                        if (!friend.get(i).equals(friendOfUser)) {
-                            if (!scoreMap.containsKey(friend.get(i))) {
-                                scoreMap.put(friend.get(i), 10);
-                            } else {
-                                int score = scoreMap.get(friend.get(i)) + 10;
-                                scoreMap.put(friend.get(i), score);
-                            }
-                        }
-                    }
 
-                }
-            }
-        }
     }
 
     private static void findFriendsOfUser(String user, List<List<String>> friends, List<String> friendsOfUser) {
-        
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).contains(user)) {
+                for (String friend : friends.get(i)) {
+                    if (!friend.equals(user)) {
+                        friendsOfUser.add(friend);
+                    }
+                }
+            }
+        }
     }
 }
