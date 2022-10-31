4987d81d1f18afc0a3bd9954b396d038aec34a5b
CAEL0 <kjongmin26@gmail.com>
2022-10-31 17:34:21 +0900

    기능 추가: PROBLEM6 닉네임을 연속한 두 글자씩 잘라 카운터에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    private static void addPartialNickname(String nickname, HashMap<String, Integer> counter) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String partialNickname = nickname.substring(i, i + 2);
+            int count = counter.getOrDefault(partialNickname, 0);
+            counter.put(partialNickname, count + 1);
+        }
     }
 }
