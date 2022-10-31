c54212dbb61708e6f2cc0d894dd661c720db58e9
seokjin <rnrn8678@naver.com>
2022-10-31 17:21:29 +0900

    feat(Problem2.java): 중복되는 문자열 체크 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+		return answer;
+	}
+
+	private static boolean isDuplicate(String cryptogram) {
+		for (int i = 1; i < cryptogram.length(); i++) {
+			if (cryptogram.charAt(i - 1) == cryptogram.charAt(i)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
