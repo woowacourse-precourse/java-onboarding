ac7aa90b93b39e069834b4d2c6211afced2fca9f
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-10-31 21:17:19 +0900

    feat(Problem3): 1부터 number까지 반복문을 돌며 손뼉을 친 총 횟수를 구하는 로직 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -16,6 +16,9 @@ public class Problem3 {
     }
     public static int solution(int number) {
         int answer = 0;
+        for (int i=1; i<=number; i++) {
+            answer += clapPerNumber(i);
+        }
         return answer;
     }
 }
