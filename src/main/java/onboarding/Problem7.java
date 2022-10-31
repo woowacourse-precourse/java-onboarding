c2fddb57bf609552e1dda92957c423bca3573200
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-31 17:15:29 +0900

    feat: Problem_7 각 유저의 친구 맵을 반환하는 기능 구현
    친구 연결 관계를 맵으로 표현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map<String, List<String>>toMap(List<List<String>> linkedNodes){
+        Map<String, List<String>> linkedMap = new HashMap<>();
+
+        for (List<String> linkedNode: linkedNodes){
+            String nodeA =  linkedNode.get(0);
+            String nodeB =  linkedNode.get(1);
+
+            List<String> nodesLinkedToA= linkedMap.getOrDefault(
+                    nodeA, new ArrayList<>());
+            List<String> nodesLinkedToB = linkedMap.getOrDefault(
+                    nodeB, new ArrayList<>());
+
+            nodesLinkedToA.add(nodeB);
+            nodesLinkedToB.add(nodeA);
+
+            linkedMap.put(nodeA, nodesLinkedToA);
+            linkedMap.put(nodeB, nodesLinkedToB);
+        }
+
+        return linkedMap;
     }
 }
