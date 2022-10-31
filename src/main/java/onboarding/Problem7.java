530c82d427a81dc0123fc358711bd7deb24187ba
Dabeen Jeong <hello70825@gmail.com>
2022-10-31 15:16:10 +0900

    feat: [Problem7] 사용자의 친구의 친구를 구하는 기능을 추가하다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,20 +1,46 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-        List<String> friendOfFriend = findRecommendFriend(user, friends);
+        List<String> friendOfFriend = findFriendOfFriend(user, friends);
+        answer = friendOfFriend;
 
         return answer;
     }
 
-    private static List<String> findRecommendFriend(String user, List<List<String>> friends) {
-        Map<String, List<String>> AdjacencyList = getAdjacencyList(friends);
+    private static List<String> findFriendOfFriend(String user, List<List<String>> friends) {
+        Map<String, List<String>> adjacencyList = getAdjacencyList(friends);
+        Map<String, Boolean> friendVisited = new TreeMap<>();
+        Map<String, Integer> friendLevel = new TreeMap<>();
+        Queue<String> friendQueue = new LinkedList<>();
 
+        friendQueue.add(user);
+        friendVisited.put(user, true);
+        friendLevel.put(user, 0);
 
+        while(!friendQueue.isEmpty()) {
+            String person = friendQueue.poll();
+
+            for(String friend : adjacencyList.get(person)) {
+                if (!friendVisited.getOrDefault(friend, false)) {
+                    friendVisited.put(friend, true);
+                    friendLevel.put(friend, friendLevel.get(person) + 1);
+                    friendQueue.add(friend);
+                }
+            }
+        }
+
+        List<String> resultTwoLevelFriend;
+        resultTwoLevelFriend = friendLevel.keySet().stream()
+                                .filter(friend -> friendLevel.get(friend) == 2)
+                                .collect(Collectors.toList());
+
+        return resultTwoLevelFriend;
     }
 
     private static Map<String, List<String>> getAdjacencyList(List<List<String>> friends) {
@@ -29,8 +55,8 @@ public class Problem7 {
             }
             resultAdjacencyList.get(friendFirst).add(friendSecond);
 
-            if (!resultAdjacencyList.containsKey(friendFirst)) {
-                resultAdjacencyList.put(friendFirst, new ArrayList<>());
+            if (!resultAdjacencyList.containsKey(friendSecond)) {
+                resultAdjacencyList.put(friendSecond, new ArrayList<>());
             }
             resultAdjacencyList.get(friendSecond).add(friendFirst);
         }
