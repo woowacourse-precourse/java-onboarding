1090a790166a7138d68fecba538d298fce0dace8
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-01 00:15:18 +0900

    Feat(Problem4): 대문자 확인 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    static class Gram {
+        String word;
+        public Gram(String word) throws Exception {
+            if (!isValidGram(word.length())) throw new Exception("Wrong String");
+            this.word = word;
+        }
+        private boolean isValidGram(int n) {
+            return n > 0 && n <= 1000;
+        }
+    }
+
+
+
+    private static boolean isLargeLetter(char c) {
+        return c >= 65 && c <= 90;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
