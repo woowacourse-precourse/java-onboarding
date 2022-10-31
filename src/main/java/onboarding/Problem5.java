45aef21ee874d59ab01a59bec9b42f30e835dc8a
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-01 02:30:40 +0900

    feat(Problem5): add Currency enum

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -24,3 +24,24 @@ class Wallet {
         this.COUNT = new Integer[9];
     }
 }
+
+enum Currency {
+    FIFTY_THOUSAND(50_000),
+    TEN_THOUSAND(10_000),
+    FIVE_THOUSAND(5_000),
+    THOUSAND(1_000),
+    FIVE_HUNDRED(500),
+    HUNDRED(100),
+    FIFTY(50),
+    TEN(10),
+    ONE(1);
+
+    private final int value;
+    Currency(int value) {
+        this.value = value;
+    }
+
+    int value() {
+        return value;
+    }
+}
