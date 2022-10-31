dd8c567228d8b790e2fd2875fd5afd1ee247b404
TaeMin <xoals3094@naver.com>
2022-10-31 21:18:25 +0900

    Problem1: add toScore method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,21 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static int toScore(int pageNum){
+        int sum = 0;
+        int mul = 1;
+
+        int num = pageNum;
+        for(; num > 0; num /= 10){
+            int digit = num % 10;
+            sum += digit;
+            mul *= digit;
+        }
+        if (sum > mul){
+            return sum;
+        }
+        else{
+            return mul;
+        }
+    }
 }
