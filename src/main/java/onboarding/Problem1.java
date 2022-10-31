befcffde44ddec025cc5dd13e5f5ebdd6d67f444
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-10-31 18:00:57 +0900

    RestricException 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,17 @@ package onboarding;
 
 import java.util.List;
 
+class RestrictException extends Exception {
+    String exMsg;
+
+    public RestrictException() {
+    }
+
+    public RestrictException(String message) {
+        exMsg = "RestrictException: " + message;
+    }
+}
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
