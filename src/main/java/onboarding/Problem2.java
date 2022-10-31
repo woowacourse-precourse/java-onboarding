3c0510884305b4c640266b09569426bcba809b4f
seokjin <rnrn8678@naver.com>
2022-10-31 17:35:41 +0900

    refactor(Problem2.java): 중복 삭제 기능 메서드 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,23 +3,27 @@ package onboarding;
 public class Problem2 {
 	public static String solution(String cryptogram) {
 		while (isDuplicate(cryptogram)) {
-			int duplicateStart = 0;
-			int duplicateEnd = cryptogram.length();
-			for (int i = 0; i < cryptogram.length(); i++) {
-				if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
-					duplicateStart = i;
-					break;
-				}
-			}
-			for (int i = duplicateStart + 1; i < cryptogram.length(); i++) {
-				if (cryptogram.charAt(duplicateStart) != cryptogram.charAt(i)) {
-					duplicateEnd = i;
-					break;
-				}
-			}
-			cryptogram = cryptogram.substring(0, duplicateStart) + cryptogram.substring(duplicateEnd);
+			cryptogram = deleteDuplicate(cryptogram);
 		}
 		return cryptogram;
+	}
+
+	private static String deleteDuplicate(String cryptogram) {
+		int duplicateStart = 0;
+		int duplicateEnd = cryptogram.length();
+		for (int i = 0; i < cryptogram.length(); i++) {
+			if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+				duplicateStart = i;
+				break;
+			}
+		}
+		for (int i = duplicateStart + 1; i < cryptogram.length(); i++) {
+			if (cryptogram.charAt(duplicateStart) != cryptogram.charAt(i)) {
+				duplicateEnd = i;
+				break;
+			}
+		}
+		return cryptogram.substring(0, duplicateStart) + cryptogram.substring(duplicateEnd);
 	}
 
 	private static boolean isDuplicate(String cryptogram) {
