a98d923ddb7291074a3680f8f26ba15efc9e8fa6
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2022-10-31 16:47:39 +0900

    Problem6 : Member 객체 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,26 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+    
+    public static class Member {
+        private String email;
+        private String name;
+
+        private Member(String email, String name) {
+            this.email = email;
+            this.name = name;
+        }
+
+        public static Member of(List<String> form) {
+            return new Member(form.get(0), form.get(1));
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
