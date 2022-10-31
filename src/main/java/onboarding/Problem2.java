c4238c6b17ae0358c44e0370f07298cd06a560e0
K-Diger <kitten.diger@gmail.com>
2022-10-31 20:51:46 +0900

    docs : Problem2 기능 목록 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/*
+
+[기능 목록]
+1. 중복 검사를 수행하는 메서드 (중복을 제거해야하는 인덱스를 뽑아 한 번에 제거하기)
+
+ */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
