8f09186bc93d000323e744d08a2eb56b1847b17c
jojaeng2 <ds4ouj@naver.com>
2022-10-31 14:07:30 +0900

    feat : 이름이 등장한 횟수를 구하는 기능구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -17,11 +15,24 @@ public class Problem6 {
         public NicknameChecker(List<List<String>> forms) {
             this.forms = forms;
             this.counts = new HashMap<>();
+            countName();
+        }
+
+        private void countName() {
+            forms.forEach(this::parseNameAndCount);
+        }
+
+        private void parseNameAndCount(List<String> form) {
+            String name = form.get(1);
+            Set<String> names = new HashSet<>();
+            for(int i=1; i<name.length(); i++) {
+                names.add(parseName(name, i));
+            }
+            names.forEach((key) -> counts.put(key, counts.computeIfAbsent(key, v -> 0) + 1));
         }
 
         private String parseName(String name, int index) {
             return name.charAt(index - 1) + String.valueOf(name.charAt(index));
         }
-
     }
 }
