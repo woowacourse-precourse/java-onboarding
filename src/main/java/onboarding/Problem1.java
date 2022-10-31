6728f68202489b96a1eb9ef5fcf40ca7a250fd5a
ckn1070 <ckn1070@naver.com>
2022-10-31 14:38:45 +0900

    Solve the problem 1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,44 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
+    static int calculateMax(int page){
+        int sum = 0;
+        int mul = 1;
+        while(page > 1){
+            sum += page % 10;
+            mul *= page % 10;
+            page /= 10;
+        }
+        if(sum >= mul){
+            return sum;
+        } else {
+            return mul;
+        }
+    }
+
+    static int myMax(List<Integer> pageList){
+        int left = calculateMax(pageList.get(0));
+        int right = calculateMax(pageList.get(1));
+        if (left >= right){
+            return left;
+        } else {
+            return right;
+        }
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int pobiValue = myMax(pobi);
+        int crongValue = myMax(crong);
+        int answer;
+        if (pobiValue > crongValue) {
+            answer = 1;
+        } else if (pobiValue < crongValue) {
+            answer = -1;
+        } else {
+            answer = 0;
+        }
         return answer;
     }
 }
