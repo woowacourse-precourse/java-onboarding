351764ec628bf449f432df153672b9771b12897c
0chil <0@chll.it>
2022-10-31 13:20:13 +0900

    test(problem5): 화폐단위를 정렬한다

--- src/test/java/onboarding/problem5/BankTest.java
+++ src/test/java/onboarding/problem5/BankTest.java
@@ -1,7 +1,9 @@
 package onboarding.problem5;
 
+import static java.util.Collections.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,12 +11,12 @@ import org.junit.jupiter.api.Test;
 
 public class BankTest {
 
-	private static final List<Integer> AMOUNTS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+	private static final List<Integer> SORTED_AMOUNTS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
 	@Test
 	@DisplayName("최대한 큰 화폐 위주로 출금한다")
 	void withdraw_big_amount_first() {
-		Bank bank = new Bank(AMOUNTS);
+		Bank bank = new Bank(SORTED_AMOUNTS);
 		List<Integer> expected = List.of(0, 1, 1, 0, 1, 1, 1, 0, 0);
 
 		assertThat(bank.withdraw(15650)).containsExactlyElementsOf(expected);
@@ -24,15 +26,30 @@ public class BankTest {
 	@DisplayName("들어간 액수가 반환된 액수와 같다")
 	void withdrawn_amount_is_consistent() {
 		int balance = 15650;
-		Bank bank = new Bank(AMOUNTS);
+		Bank bank = new Bank(SORTED_AMOUNTS);
 		List<Integer> counts = bank.withdraw(balance);
 
+		assertThat(calculateTotal(counts)).isEqualTo(balance);
+	}
+
+	@Test
+	@DisplayName("화폐단위를 정렬한다")
+	void amounts_are_sorted() {
+		List<Integer> shuffledAmounts = new ArrayList<>(SORTED_AMOUNTS);
+		shuffle(shuffledAmounts);
+
+		Bank bank = new Bank(shuffledAmounts);
+		List<Integer> expected = List.of(1, 1, 1, 0, 1, 1, 1, 0, 0);
+
+		assertThat(bank.withdraw(65650)).containsExactlyElementsOf(expected);
+	}
+
+	private int calculateTotal(List<Integer> counts) {
 		int total = 0;
-		for (int i = 0; i < AMOUNTS.size(); i++) {
-			Integer amount = AMOUNTS.get(i);
+		for (int i = 0; i < SORTED_AMOUNTS.size(); i++) {
+			Integer amount = SORTED_AMOUNTS.get(i);
 			total += amount * counts.get(i);
 		}
-
-		assertThat(total).isEqualTo(balance);
+		return total;
 	}
 }
