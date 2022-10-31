43d9fd9dacb07a10c432608000c758f2e3596a87
박세원 <tpdnjs1825@naver.com>
2022-11-01 02:04:05 +0900

    Feat : 암호문에서 연속하는 중복 문자를 삭제하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem2 {
+    private static String removeDuplicate(String cryptogram){
+        String decryption = "";
+        for(int i = 0; i < cryptogram.length() - 1; i++){
+            char firstCharacter = cryptogram.charAt(i);
+            char secondCharacter = cryptogram.charAt(i + 1);
+            if(firstCharacter == secondCharacter) {
+                i++;
+                continue;
+            }
+            decryption += String.valueOf(firstCharacter);
+            if(i == cryptogram.length() - 2) {
+                decryption += String.valueOf(secondCharacter);
+            }
+        }
+        return decryption;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        removeDuplicate(cryptogram);
+
         return answer;
     }
 }
