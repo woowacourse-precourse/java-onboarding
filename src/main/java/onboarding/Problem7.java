a3afd33e87dd43497a7566d18561fa39f9a4b82e
heejeong <hjx66@naver.com>
2022-11-01 01:20:37 +0900

    feat: 사용자와 함께 아는 친구 수 점수 계산 구현 - getFriendRelation() : 각 아이디별 친구관계 저장한 map 생성 - user와 교집합인 친구관계 Set size만큼 점수 계산

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static Map<String, Set<String>> getFriendRelation(List<List<String>> friends) {
+        Map<String, Set<String>> relation = new HashMap<>();
+        for (List<String> elem : friends) {
+            String id1 = elem.get(0);
+            String id2 = elem.get(1);
+            if (!relation.containsKey(id1)) {
+                relation.put(id1, new HashSet<>());
+            }
+            if (!relation.containsKey(id2)) {
+                relation.put(id2, new HashSet<>());
+            }
+            relation.get(id1).add(id2);
+            relation.get(id2).add(id1);
+        }
+        return relation;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        Map<String, Integer> result = new TreeMap<>(); //점수 계산용
+
+        //1. 사용자와 함께 아는 친구
+        Map<String, Set<String>> relation = getFriendRelation(friends);
+        //user와 교집합
+        for (String key : relation.keySet()) {
+            Set<String> userfriends = relation.get(user);
+            Set<String> target = relation.get(key);
+            userfriends.retainAll(target);
+            result.put(key, userfriends.size() * 10);
+        }
+
         return answer;
     }
+
 }
