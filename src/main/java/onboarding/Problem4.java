7c6fa082ffecd82fd5e639322713a0afb7021905
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-10-31 22:01:51 +0900

    refactor: Problem4 관련 Class 구조 변경 (StringReverser 추가)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,15 +1,10 @@
 package onboarding;
 
 import onboarding.problem4.LetterReverser;
+import onboarding.problem4.StringReverser;
 
 public class Problem4 {
     public static String solution(String word) {
-        StringBuilder sb = new StringBuilder();
-
-        for (char letter : word.toCharArray()) {
-            char changedLetter = LetterReverser.reverseOnlyEnglish(letter);
-            sb.append(changedLetter);
-        }
-        return sb.toString();
+        return StringReverser.reverse(word);
     }
 }
