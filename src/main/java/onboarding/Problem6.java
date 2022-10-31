540fef9769ecdd089a0397bf9790512527401567
JH-web115 <kimjjong321@naver.com>
2022-10-31 19:42:57 +0900

    P6. 한 닉네임으로 만들어지는 2글자 조합 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for(List<String> user : forms) {
+            List<String> word_combi = createWordCombi(user.get(1));
+        }
+
         return answer;
+    }
+
+    public static List<String> createWordCombi(String nickname) {
+        List<String> word_combi = new ArrayList<>();
+
+        for(int i = 0; i < nickname.length() - 1; i++) {
+            word_combi.add(nickname.substring(i,i+2));
+        }
+
+        return word_combi;
     }
 }
