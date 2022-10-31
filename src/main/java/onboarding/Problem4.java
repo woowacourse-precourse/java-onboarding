84d8219ab4960be7a24ba0a69866869cf570fc3f
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 00:53:44 +0900

    [Add] Prob4 convert func
    + convert(char) : convert char to another char follow the rule

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
-    // list : list for alphabet
     // func : get char and switch char
+    private static char convert(char c){
+
+        // convert char to int (ascii)
+        int cNum = (int) c;
+        int res;
+        // if c is upper case
+        if (Character.isUpperCase(c))
+            res = (int)'Z' - (cNum - (int)'A');
+        else
+            res = (int)'z' - (cNum - (int)'a');
+
+        return (char)res;
+    }
     // func : get string and convert string
     public static String solution(String word) {
         String answer = "";
