a93f8dc7fd058b1816d813d99caf45f6e74a29ee
Hyes-y <gptjs7970@naver.com>
2022-10-31 20:37:21 +0900

    feat: Problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,37 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            String temp = "";
+            char[] chars = answer.toCharArray();
+            if (chars[0] != chars[1]) {
+                temp = temp + chars[0];
+            }
+            for (int i=1; i < answer.length()-1; i++){
+                if (chars[i-1] == chars[i] || chars[i] == chars[i+1]) {
+                    flag = true;
+                    continue;
+                } else {
+                    temp = temp + chars[i];
+                }
+            }
+            if (chars[answer.length()-1] != chars[answer.length()-2]) {
+                temp = temp + chars[answer.length() - 1];
+            }
+
+            answer = temp;
+        }
+
         return answer;
     }
+//    public static void main(String[] args) {
+//        Problem2 prob = new Problem2();
+//        String cryptogram = "browoanoommnaon";
+////        System.out.println(cryptogram);
+//        String result = prob.solution(cryptogram);
+//        System.out.println(result);
+//    }
 }
