8109d8241a8b6cc89f903ccb6a8a1fb93b7b4209
bumwoo <gld55602@gmail.com>
2022-10-31 21:48:37 +0900

    feat(Problem7): 친구 목록 리스트 구현
    HashMap 생성 후, friends를 순회하며 리스트에 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+		List<String> answer = Collections.emptyList();
+
+		Map<String, List<String>> friendList = new HashMap<>();
+		for (List<String> f : friends) {
+			friendList.computeIfAbsent(f.get(0), s -> new ArrayList<>()).add(f.get(1));
+			friendList.computeIfAbsent(f.get(1), s -> new ArrayList<>()).add(f.get(0));
+		}
+
         return answer;
     }
 }
