eba970b66ab6a2e4b1f4a158e82d77ad97b897dd
채우석 <wochae@wochae-MacBookAir.local>
2022-10-31 18:17:51 +0900

    [feat] 1.1 9 가지 경우를 담은 배열 선언 및 초기화 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int[] won = new int[9];
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        initWon();
         return answer;
+    }
+
+    public static void initWon() {
+        won[0] = 50000;
+        won[1] = 10000;
+        won[2] = 5000;
+        won[3] = 1000;
+        won[4] = 500;
+        won[5] = 100;
+        won[6] = 50;
+        won[7] = 10;
+        won[8] = 1;
     }
 }
