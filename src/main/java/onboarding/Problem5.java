12dee76551f6f05d14c5fa1ca67fb8614ed5dd5c
이지영 <jioung93@naver.com>
2022-10-31 17:48:37 +0900

    comment: 기능사항 정리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /*
+    1. 큰 단위부터 검사
+    2. 해당 금액의 화폐로 변환 가능한지 확인
+    3. 변환 가능하면 화폐로 변환 후 남은 금액 갱신
+    4. 변환 가능하지 않으면 더 작은 단위로 넘어감
+     */
+
+    // 화폐 단위를 저장하고 있는 배열 billUnit[]
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    // 남은 금액을 해당 단위의 화폐 몇 장으로 변환 가능한지를 판단하는 함수 moneyToBill()
+
 }
