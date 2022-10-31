c7eb57af5c7b957d9a0a1cf4df9a94677b020736
ParangBird <seekerman@naver.com>
2022-10-31 23:49:58 +0900

    refactor: coinList 상수화, getChangeCoinList 주석 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,21 @@ import java.util.List;
 
 public class Problem5 {
 
+    private static final int MONEY_50000 = 50000;
+    private static final int MONEY_10000 = 10000;
+    private static final int MONEY_5000 = 5000;
+    private static final int MONEY_1000 = 1000;
+    private static final int MONEY_500 = 500;
+    private static final int MONEY_100 = 100;
+    private static final int MONEY_50 = 50;
+    private static final int MONEY_10 = 10;
+    private static final int MONEY_1 = 1;
+
+    /**
+     * @param money    : 변환할 금액
+     * @param coinList : 사용 가능한 동전 리스트(내림차순)
+     * @return List<Integer> : 사용한 동전 리스트
+     */
     public static List<Integer> getChangeCoinList(int money, int[] coinList) {
         List<Integer> answer = new ArrayList<>();
         for (int i = 0; i < coinList.length; i++) {
@@ -16,7 +31,8 @@ public class Problem5 {
     }
 
     public static List<Integer> solution(int money) {
-        int coinList[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int coinList[] = {MONEY_50000, MONEY_10000, MONEY_5000, MONEY_1000,
+                MONEY_500, MONEY_100, MONEY_50, MONEY_10, MONEY_1};
         List<Integer> answer = getChangeCoinList(money, coinList);
         return answer;
     }
