e271c9781cc7f59285b589d3d75e7f8e135f9abe
tlsqj <rnjstlsqja@inha.edu>
2022-10-31 21:37:04 +0900

    문제 5번 버그 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,16 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        int coins[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
+        List<Integer> answer = new ArrayList<Integer>();
+        int coins[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        for(int i = 0; i < 10; i++) {
-            answer.add(money / coins[i]);
-            money %= coins[i];
+        for(int coinsIterator = 0; coinsIterator < 9; coinsIterator++) {
+            answer.add(Integer.valueOf(money / coins[coinsIterator]));
+            money %= coins[coinsIterator];
         }
 
         return answer;
