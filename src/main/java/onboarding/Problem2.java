8c80b7b74ea7a9433bdd9b3a9722ddaae8386438
hanuel <ezzeezz@naver.com>
2022-11-01 05:04:24 +0900

    feat: [problem2] Add decrypt

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,36 @@
 package onboarding;
 
 public class Problem2 {
+    public static String decrypt(String cryptogram) {
+        String result = "";
+
+        int i = 0;
+        int j = 1;
+
+        if (cryptogram.length() == 1)
+            return cryptogram;
+
+        while(i < cryptogram.length() - 1) {
+            while(true) {
+                char x = cryptogram.charAt(i);
+                char y = cryptogram.charAt(j);
+                if (x != y)
+                    break;
+                j += 1;
+                if (j == cryptogram.length())
+                    break;
+            }
+            if(i + 1 == j)
+                result += cryptogram.charAt(i);
+            if(j == cryptogram.length() - 1)
+                result += cryptogram.charAt(j);
+            i = j;
+            j = i + 1;
+        }
+        
+        return result;
+    }
+
     public static String solution(String cryptogram) {
         String beforeDec = cryptogram;
         String afterDec;
