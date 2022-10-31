4cdcd35443bf9a16f6995216e49dd840edf76a19
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 16:09:39 +0900

    Feat(Problem1) : solution 메서드 solution 메서드에서 getMyPoint와 whoIsWinner를 사용해 정답을 구하는 코드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,9 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiPoint = getMyPoint(pobi);
+        int crongPoint = getMyPoint(crong);
+        answer = whoIsWinner(pobiPoint, crongPoint);
         return answer;
     }
 
