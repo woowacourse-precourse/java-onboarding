1339184fc79884adce4aecad96e5c8bd38e9d392
alswn <alswn9938@gmail.com>
2022-10-31 15:54:41 +0900

    [Q3] feat : add "숫자에 포함된 3 6 9의 개수 return" 메소드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,16 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int threeSixNineCount(int number){
+        int count  = 0;
+        int nowDigit = 0;
+        while(number > 0){
+            nowDigit = number % 10;
+            if(nowDigit == 3 || nowDigit == 6 || nowDigit == 9){
+                count++;
+            }
+        }
+        return count;
+    }
 }
