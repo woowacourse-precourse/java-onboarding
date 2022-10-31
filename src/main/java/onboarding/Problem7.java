82c44a58b8864b5595a2bd2e03c32dd40104a124
kokomong2 <ko9603@naver.com>
2022-11-01 03:05:51 +0900

    feat(문제7) 추가 : 친구와 점수를 mapping함

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,24 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+
+        for (List<String> list :friends) {
+
+            for (int i = 0; i < 2; i++) {
+                String name= list.get(i);
+                if(!scoreMap.containsKey(name)){
+                    scoreMap.put(name,0);
+                }
+            }
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
