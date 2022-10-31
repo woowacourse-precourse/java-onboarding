8f055dc58921246a9dd99531330807a927a88c14
5onchangwoo <scw9410@gmail.com>
2022-10-31 09:55:47 +0900

    problem5/돈 변환 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,11 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+    // 50,000, 10,000, 5000, 1000, 500, 100, 50, 10, 1
+    // 돈 변환 로직
+    public static int[] transMoney(int money, int criteria){
+        int count = money / criteria;
+        int charge = money % criteria;
+        return new int[]{count, charge};
+    }
 }
