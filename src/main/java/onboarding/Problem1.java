5dc30b2e832ac15b8c9eb79d3cc529332124d1b2
JungWoo <rlawjddn103@naver.com>
2022-10-31 15:35:12 +0900

    feat: Add exceptionCheck Function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,30 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        //예외 체크
+        if (exceptionCheck(pobi,crong)){
+            return -1;
+        }
+
+        return -1;
     }
+
+    public static boolean exceptionCheck(List<Integer> pobi, List<Integer> crong){
+        if (pobi.get(0) % 2 != 1 || pobi.get(1) != pobi.get(0) + 1) {
+            return true;
+        }
+        if (crong.get(0) % 2 != 1 || crong.get(1) != crong.get(0) + 1) {
+            return true;
+        }
+        if (pobi.get(0) <= 0 || pobi.get(0) > 400 || pobi.get(1) <= 0 || pobi.get(1) > 400) {
+            return true;
+        }
+        if (crong.get(0) <= 0 || crong.get(0) > 400 || crong.get(1) <= 0 || crong.get(1) > 400) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
