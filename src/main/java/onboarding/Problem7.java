9f426da8112fb740fc52ecaeaaf7b56fd6b180b2
KimJaeSung <behappy0916@naver.com>
2022-10-31 14:31:31 +0900

    feat java-onboarding-7-친구 관계를 나타내는 hashMap구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    static void createfriendsGroups(HashMap<String,List<String>> friendsGroups, List<List<String>>friends, String userName){
+        for(int i=0; i<friends.size(); i++){
+            String friends1 = friends.get(i).get(0);
+            String friends2 = friends.get(i).get(1);
+
+            if(!friendsGroups.containsKey(friends1)) {
+                friendsGroups.put(friends1, new ArrayList<>());
+            }
+            if(!friendsGroups.containsKey(friends2)) {
+                friendsGroups.put(friends2, new ArrayList<>());
+            }
+            friendsGroups.get(friends1).add(friends2);
+            friendsGroups.get(friends2).add(friends1);
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String,List<String>> friendsGroups = new HashMap<>();
+        createfriendsGroups(friendsGroups,friends,user);
         return answer;
     }
 }
