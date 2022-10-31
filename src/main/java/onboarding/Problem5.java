d9ed37c8b88f8d07dae3cb4b828f391fa2482f99
swjeong98 <tree5678@konkuk.ac.kr>
2022-10-31 19:21:38 +0900

    문제 5 코드 수정 - 로직 오류해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,20 +2,20 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
     public static int ConvertMoney(int money, int target){
         return money/target;
     }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
 
-        int[] pivot = {50000, 10000, 1000, 500, 100, 50, 10, 1};
+        int[] pivot = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         for(int i=0;i<pivot.length;i++){
             answer.add(ConvertMoney(money, pivot[i]));
-            money /= pivot[i];
+            money -= (money/pivot[i]) * pivot[i];
         }
-
         return answer;
     }
 }
