edcb6d9d3b37a051eb6f13b981782ca9a0715d66
es <mascoeskim@gmail.com>
2022-10-31 10:16:17 +0900

    Problem2/1.초기세팅

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        ArrayList<String> duplicateList = new ArrayList<>(Arrays.asList("aa","bb","cc","dd","ee","ff","gg","hh","ii","jj","kk","ll","mm","nn","oo","pp","qq","rr","ss","tt","uu","vv","ww","xx","yy","zz"));
+        System.out.println(duplicateList);
+//cryptogram에서 중복문자를 골라낼 중복문자를 담은 매열 duplicateList를 생성
+
+
         String answer = "answer";
+        System.out.println(answer);
         return answer;
     }
 }
