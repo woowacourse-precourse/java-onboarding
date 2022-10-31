667415d89d3bf4d0602dde017d4299345c48f736
recicoco068 <gkapf838@gmail.com>
2022-10-31 20:22:44 +0900

    feat : Problem7 친구 관계 인접 리스트 만드는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static Map<String, List<String>> createFriendsList(List<List<String>> friends) {
+        Map<String, List<String>> friendsList = new HashMap<>();
+        for (List<String> friend : friends) {
+            if (!friendsList.containsKey(friend.get(0)))
+                friendsList.put(friend.get(0), new ArrayList<String>());
+            if (!friendsList.containsKey(friend.get(1)))
+                friendsList.put(friend.get(1), new ArrayList<String>());
+            friendsList.get(friend.get(0)).add(friend.get(1));
+            friendsList.get(friend.get(1)).add(friend.get(0));
+        }
+        return friendsList;
     }
 }
