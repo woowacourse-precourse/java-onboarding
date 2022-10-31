19ec783d2fa98cddc953170bc81cfe488c541670
eugene1204 <s1061001@naver.com>
2022-10-31 16:43:49 +0900

    기능 목록 : problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,14 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
+    // 1. solution에서 List<List<String>>을 map으로 만들기
+    // 2. 두글자씩 잘라서 중복확인용 메소드
+    // 3. 중복된 값 찾고 정렬해서 return 하는 메소드
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        return answer;}
+        return answer;
+    
+    }
 
 }
