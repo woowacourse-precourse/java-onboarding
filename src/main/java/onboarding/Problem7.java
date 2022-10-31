2c199ed1aea388c61889199e6777754297ea0619
sen <seeunchoi99@gmail.com>
2022-11-01 00:20:56 +0900

    💡 Problem7 기능 목록 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 사용자와 함께 아는 친구의 수로 점수 계산
+ * 2. 사용자의 타임 라인에 방문한 횟수로 점수 계산
+ * 3. 총 점수 계산
+ * 4. 3번을 이용해 최대 5명 목록 구하기
+ */
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
