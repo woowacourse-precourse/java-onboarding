515514a17de23edde34f43cc31cd39ecb69585dd
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 00:32:57 +0900

    문제1 : 입력값을 검증하는 클래스 추가

---
+++ src/main/java/onboarding/problem1/Validation.java
@@ -0,0 +1,29 @@
+package onboarding.problem1;
+
+import java.util.List;
+
+public class Validation {
+    private static final int START_PAGE = 1;
+    private static final int END_PAGE = 400;
+
+    public boolean validate(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        if (!isInRange(leftPage) || !isInRange(rightPage)) {
+            return false;
+        }
+
+        if ((leftPage % 2 != 0 ) || ( rightPage % 2 != 1)) {
+            return false;
+        }
+
+        if (leftPage - rightPage != 1) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isInRange(int page) {
+        return (page < START_PAGE) || (page > END_PAGE);
+    }
+}
