00f9e675e55f5131f4d429a7364af042a08db6ed
metorg <metrog950@gmail.com>
2022-10-31 14:45:03 +0900

    feat: [Problem5] 화폐단위 카운팅 메서드 생성 돈을 가장큰 화폐단위부터 나눈 값을 카운팅하고 나머지 값을 다음 화폐단위로 나누면서 카운팅 하는 메서드

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,9 +9,21 @@ public class Problem5 {
     public static int[] monetary_unit= {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = monetaryUnitCount(money);
 
         return answer;
+    }
+
+    public static List<Integer> monetaryUnitCount(int money){
+
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0;i<monetary_unit.length;i++){
+            result.add(money/monetary_unit[i]);
+            money%=monetary_unit[i];
+        }
+
+        return result;
     }
     
 }
