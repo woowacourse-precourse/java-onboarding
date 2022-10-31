dc42693c525e163ded1794fdc754d935a01554ed
JH-web115 <kimjjong321@naver.com>
2022-10-31 15:26:03 +0900

    P2. 반복되는 문자 확인을 위한 문자열 전체 스캔
    기존에 주어진 문자열을 1번 순회하며 반복되는 문자열을 찾아 1차 해독을 한 상태까지 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
+    static int global_iter = 0;
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        boolean isRepeat = true;
+
+        while(true) {
+//            if(answer.equals("") || !isRepeat) break;
+            if(global_iter + 1 >= answer.length()) break;
+            answer = check_continuity(global_iter, answer);
+
+            global_iter += 1;
+        }
+
         return answer;
+    }
+
+    public static String check_continuity(int iter, String word) {
+        String s = word;
+        char c = word.charAt(iter);
+        int endIndex = iter;
+
+        for(int i = iter + 1; i < word.length(); i++) {
+            if (c != word.charAt(i)) break;
+
+            endIndex += 1;
+        }
+
+        if(endIndex != iter) {
+            s = s.replaceFirst(s.substring(iter, endIndex + 1), "");
+            global_iter -= 1;
+        }
+
+        return s;
     }
 }
