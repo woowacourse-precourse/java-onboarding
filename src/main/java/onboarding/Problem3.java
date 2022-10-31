27627d88ae90dbe280b5984d22d9871dd1b6ea57
eugene1204 <s1061001@naver.com>
2022-10-31 14:14:54 +0900

    p3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,13 +4,13 @@ public class Problem3 {
     public static int solution(int number) {
         String num = "";
         int count =0;
-//        for(int i = 1; i<number+1; i++) num +=i+"";//1234567891011121314
-//
-//        for(int j =0; j<num.length();j++){
-//            if(num.charAt(j) == '3') count++;
-//            if(num.charAt(j) == '6') count++;
-//            if(num.charAt(j) == '9') count++;
-//        }
+        for(int i = 1; i<number+1; i++) num +=i+"";//1234567891011121314
+
+        for(int j =0; j<num.length();j++){
+            if(num.charAt(j) == '3') count++;
+            if(num.charAt(j) == '6') count++;
+            if(num.charAt(j) == '9') count++;
+        }
         return count;
 
     }
