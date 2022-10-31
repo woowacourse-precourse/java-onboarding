033c602477029bc7eae2b29ca4e49bde8d012b1d
doehy <dbehxks02@naver.com>
2022-10-31 21:32:54 +0900

    feat : 중복 문자 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,8 +5,18 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         Stack<Character> duplication_stack = changeToStack(cryptogram);
+        Stack<Character> removed_stack = removeDuplicate(duplication_stack);
         String answer = "answer";
         return answer;
+    }
+    static Stack<Character> removeDuplicate(Stack<Character> duplication_stack) {
+        Stack<Character> result_stack = new Stack<Character>();
+        while(!duplication_stack.isEmpty()){
+            char ch = duplication_stack.pop();
+            if(result_stack.isEmpty() || ch != result_stack.peek()){result_stack.push(ch);}
+            else if(ch == result_stack.peek()){result_stack.pop();}
+        }
+        return result_stack;
     }
     static Stack<Character> changeToStack(String cryptogram){
         Stack<Character> temp_stack = new Stack<Character>();
