d58d2493a72dfb3202397c502a9d3e0fd4e8a990
JunH <tjdtls690@gmail.com>
2022-10-31 11:54:26 +0900

    :white_check_mark: test(PROBLEM6) : 테스트 케이스 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -868,6 +868,52 @@ class ApplicationTest {
             List<String> result = List.of("abcdefghi@email.com", "ㄱb@email.com", "ㄴe@email.com", "가나다라마바사아자@email.com", "나f@email.com", "마c@email.com", "사a@email.com");
             assertThat(Problem6.solution(forms)).isEqualTo(result);
         }
+    
+        @Test
+        void case15() {
+            List<List<String>> forms = List.of(
+                    List.of("kim@email.com", "김ㅡ이"),
+                    List.of("nam@email.com", "ㅡ이야"),
+                    List.of("choi@email.com", "최강"),
+                    List.of("lee@email.com", "강합니다"),
+                    List.of("jun@email.com", "왜ㅇㅡ이야")
+            );
+            List<String> result = List.of("jun@email.com", "kim@email.com", "nam@email.com");
+            assertThat(Problem6.solution(forms)).isEqualTo(result);
+        }
+    
+        @Test
+        void case16() {
+            List<List<String>> forms = List.of(
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@email.com", "제이슨"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("nowm@email.com", "이제엠"),
+                    List.of("jasp@email.com", "박이김"),
+                    List.of("jp@email.com", "김이박"),
+                    List.of("qpw@email.com", "김이순"),
+                    List.of("teemo@email.com", "티모"),
+                    List.of("banana@email.com", "대위티모"),
+                    List.of("secondJm@email.com", "제이엠"),
+                    List.of("apple@email.com", "땷횱봵"),
+                    List.of("stran@email.com", "횱봵으"),
+                    List.of("zebra@email.com", "두글자이상의문자가연속적으로이제맞티모")
+            );
+            List<String> result = List.of("apple@email.com",
+                    "banana@email.com",
+                    "jason@email.com",
+                    "jm@email.com",
+                    "jp@email.com",
+                    "mj@email.com",
+                    "nowm@email.com",
+                    "qpw@email.com",
+                    "secondJm@email.com",
+                    "stran@email.com",
+                    "teemo@email.com",
+                    "zebra@email.com");
+            assertThat(Problem6.solution(forms)).isEqualTo(result);
+        }
     }
     
     @Nested
