5109af37eb7455f6608612871ac03139c20e1b18
p <gustjr2332@naver.com>
2022-10-31 19:44:44 +0900

    feat(Problem1): multiEachDigit()  각 자릿수 곱하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,6 +12,15 @@ class Problem1 {
         return answer;
     }
 
+    public static int multiEachDigit(int num){
+        int result = 1;
+        while(num>0){
+            result *= num%10;
+            num /= 10;
+        }
+        return result;
+    }
+
     public static int plusEachDigit(int num){
         int result = 0;
         while(num>0){
