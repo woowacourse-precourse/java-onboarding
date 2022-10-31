e788190f5661bd34042dfb73b0c79022e953f3ce
WhoIsWoony <WhoIsWoony@gmail.com>
2022-10-31 17:27:26 +0900

    problem2: TODO-'기능1' complete

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+    private static Stack<Character> reduceStringByStack(String cryptogram){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < cryptogram.length(); i++){
+            if(!stack.empty() && stack.peek() == cryptogram.charAt(i))
+                stack.pop();
+            else
+                stack.push(cryptogram.charAt(i));
+        }
+        return stack;
+    }
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = reduceStringByStack(cryptogram);
+        return "";
     }
 }
