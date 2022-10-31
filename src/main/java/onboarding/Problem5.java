2a279dae3c637fbd38adeeb9a0d0c1fbcdb388b2
minje <alswp0910@naver.com>
2022-10-31 15:00:48 +0900

    문제5_구현완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] bill = {50000,10000,5000,1000,500,100,50,10,1};
+
+        for (Integer num : bill){
+            int n = money/num;
+            money %= num;
+            answer.add(n);
+
+        }
+
         return answer;
     }
 }
