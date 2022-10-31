631d037eb67c739c54345e14aabdcc1c02c64ea6
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2022-11-01 04:56:45 +0900

    feat: add function converWord

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
+    public static String converWord(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            int each = word.charAt(i);
+
+            if (each >= 97 && 122 >= each) {
+                answer += (char) (219 - each);
+            } else if (each >= 65 && 90 >= each) {
+                answer += (char) (155 - each);
+            } else {
+                answer += (char) each;
+            }
+        }
+
+        return answer;
+    }
+
+    public static String solution(String word) {
+        String answer = converWord(word);
         return answer;
     }
 }
