bdb2a862315fdd399738fe84f3cba86db3f46500
anso33 <hyeon131812@gmail.com>
2022-10-31 10:24:41 +0900

    [FEAT] make func that DELETE same char in String

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,7 +14,19 @@ public class Problem2 {
         return answer;
     }
 
+    private static String deleteSameChar(String cryptogram) {
 
+        int size = cryptogram.length();
+        for (int i = 0; i < size - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + 2);
+                //System.out.println(cryptogram);
+                return cryptogram;
+            }
+        }
+
+        return cryptogram;
+    }
 
     // 같은 문자열 있으면 true 없으면 false -> 자바는 객체에 대해 call-by-reference니까 객체값을 다른 메소드에서 바꿔도 ok
     public static int findSameChar(String cryptogram) {
