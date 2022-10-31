ccc5173a4e276fb1ea5a21b0e2292749ced5412b
wnsgml7267 <dlwns7267@naver.com>
2022-11-01 02:18:20 +0900

    [Problem4] 문자열 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+
+    // 문자열 변환
+    private static String convertString(String word) {
+        String convertString = "";
+
+        for(int i = 0; i < word.length(); i++) {
+            if (isAlphabet(word.charAt(i))) {
+                convertString += convertChar(word.charAt(i));
+            } else {
+                convertString += word.charAt(i);
+            }
+        }
+
+        return convertString;
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return convertString(word);
     }
 }
