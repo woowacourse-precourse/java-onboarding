b2927dc3aee3cf3ec78c61b1a8ad20f3e953774a
inhyeok <ss112d86@gmail.com>
2022-10-31 17:14:27 +0900

    feat: Problem1 입력값 검증 로직 추가 길이 검증(1,400), 최소 길이(1), 홀수 짝수 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,46 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+  public static int solution(List<Integer> pobi, List<Integer> crong) {
+    int answer = Integer.MAX_VALUE;
+
+    if (!isValidateBook(pobi) || !isValidateBook(crong)) {
+      return -1;
     }
+    return answer;
+  }
+
+  private static boolean isValidateBook(List<Integer> bookPages) {
+    int leftPage = bookPages.get(0);
+    int rightPage = bookPages.get(1);
+    int minPage = Math.min(leftPage, rightPage);
+    int maxPage = Math.max(leftPage, rightPage);
+
+    return parityCheck(leftPage, rightPage) && lengthCheck(minPage, maxPage);
+  }
+
+  private static boolean lengthCheck(int minPage, int maxPage) {
+    if (minPage < 1) {
+      return false;
+    }
+    if (maxPage > 400) {
+      return false;
+    }
+    if (maxPage - minPage > 1) {
+      return false;
+    }
+    return true;
+  }
+
+  private static boolean parityCheck(int leftPage, int rightPage) {
+    if (leftPage % 2 == 0) {
+      return false;
+    }
+    if (rightPage % 2 != 0) {
+      return false;
+    }
+    return true;
+  }
+
 }
