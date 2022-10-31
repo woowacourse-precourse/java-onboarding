0131cedfda4e5a6917150e2bfdba33068e9d4a93
glaehfdl1654 <glaehfdl1654@naver.com>
2022-10-31 16:58:55 +0900

    [Problem5] solution finish

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,7 +8,10 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         int[] won = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> answer = new ArrayList<>();
-
+        for (int i : won) {
+            answer.add(money / i);
+            money = money % i;
+        }
         return answer;
     }
 }
