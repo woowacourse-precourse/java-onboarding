8ced555895cb788ef36c2daf1efcc743cb057a6c
jhsseonn <hosu0125@gmail.com>
2022-11-01 02:41:18 +0900

    feat: (Problem1) 각각의 리스트에서 각각의 요소의 합들과 곱들 중 가장 큰 값 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,9 +10,24 @@ class Problem1 {
         int pobiMax = 0;
         int crongMax = 0;
 
+        pobiMax = maxScore(pobi);
+        crongMax = maxScore(crong);
+
         return answer;
     }
 
+    static int maxScore(List<Integer> list) {
+        int sum=0;
+        int multiple=0;
+
+        for(int number:list) {
+            int[] numArray = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+            sum = Math.max(sum, sumCal(numArray));
+            multiple = Math.max(multiple, mulCal(numArray));
+        }
+
+        return Math.max(sum, multiple);
+    }
 
     static int sumCal(int[] array){
         int sum=0;
