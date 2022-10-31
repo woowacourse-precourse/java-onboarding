7911e0fec6fb3218f9f9bfa9367cd2f0316db92d
baekeunsun <eunsun5460@gmail.com>
2022-10-31 14:58:38 +0900

    [문제3] feats: number까지 반복 CheckInput, Count369수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,14 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         if(CheckInput(number)){
-            return Count369(number);
+            return CountNumber(number);
         }
         throw new IllegalArgumentException("잘못된 입력값");
     }
 
     /* 기능1 : input 예외 확인 */
     private static boolean CheckInput(int number){
-        if(number > 0 && number <10000) return true;
+        if(number > 0 && number <10001) return true;
         return false;
     }
 
@@ -21,8 +21,17 @@ public class Problem3 {
             int tmp = number % 10;
             if(tmp ==3 || tmp == 6 || tmp ==9){
                 count += 1;
-                number /= 10;
             }
+            number /= 10;
         }return count;
     }
+
+    /* 기능3 : number 개수동안 반복 */
+    private static int CountNumber(int number){
+        int ans = 0;
+        for(int i = 1; i < number+1; i++){
+            ans += Count369(i);
+        }return ans;
+    }
+
 }
