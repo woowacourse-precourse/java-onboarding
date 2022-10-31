1db2730dd817f37d353cb690ea6557b0172ff2c0
Ran <lan4250@naver.com>
2022-10-31 18:58:26 +0900

    Q2. solution

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        String ans = "";
+        char pre = cryptogram.charAt(0);
+
+        Stack<Character> stack = new Stack<>();
+        stack.push(pre);
+
+        for (int i = 1; i < cryptogram.length(); ++i) {
+            if (cryptogram.charAt(i) == stack.peek()) {
+                stack.pop();
+            } else {
+                stack.push(cryptogram.charAt(i));
+            }
+        }
+
+        for (int i = 0; i < stack.size(); ++i) {
+            ans += stack.get(i);
+        }
+
+        return ans;
+
     }
 }
