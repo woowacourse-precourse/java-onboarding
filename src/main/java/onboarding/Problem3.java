b547b760944270f0e78e104ad7c0d90b96872227
Hyes-y <gptjs7970@naver.com>
2022-10-31 21:31:52 +0900

    feat: Problem3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i=1; i <= number; i++) {
+            int[] arrNum = Stream.of(String.valueOf(i).split("")).mapToInt(Integer::parseInt).toArray();
+
+            int[] game = new int[]{3, 6, 9};
+            for (int val: arrNum) {
+                if (Arrays.stream(game).anyMatch(v -> v == val)) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
+//    public static void main(String[] args) {
+//        Problem3 prob = new Problem3();
+//        int number = 14;
+////        System.out.println(cryptogram);
+//        int result = prob.solution(number);
+//        System.out.println(result);
+//    }
 }
