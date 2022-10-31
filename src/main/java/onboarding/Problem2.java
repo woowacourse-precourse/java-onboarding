f45ef7cea678851f0a0540d02dd33063c2f0fdeb
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 21:29:04 +0900

    [Problem 2] Overview

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        /* 
+        결국 연속되어 pop되는 문자들을 모두 지우면 됨
+        1. 문자열을 순회하며 중복을 체크하여 삭제하는 함수 작성 ( return string )
+        2. while 루프를 돌며 삭제가 이루어지지 않을 때 까지 반복
+        */
         return answer;
     }
 }
