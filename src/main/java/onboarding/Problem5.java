7b194f23cc8413f71880bf4f38ecab869007726f
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:26:54 +0900

    5번: 돈 액수 크기에 따라 count 구하는 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,18 +5,20 @@ import java.util.List;
 
 public class Problem5 {
 
-    private static final int OMAN = 0;
-    private static final int MAN = 1;
-    private static final int CHEON= 2;
-    private static final int OBAEK = 3;
-    private static final int BEAK = 4;
-    private static final int OSHIP = 5;
-    private static final int SHIP = 6;
-    private static final int O = 7;
-    private static final int IL = 8;
+    private static final List<Integer> moneySizeList = List.of(
+            50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1
+    );
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        for (int i = 0; i < moneySizeList.size(); i++) {
+            
+        }
         return answer;
+    }
+
+    private static int getMoneyCount(int money, int size) {
+        return money / size;
     }
 }
