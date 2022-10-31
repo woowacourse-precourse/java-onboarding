65ca5e07dd40129043d2015143d43e9ae124cc49
이연재 <leeyj0304@gmail.com>
2022-11-01 05:03:53 +0900

    Exception handled

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,18 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static Boolean isValid(List<Integer> Pages) {
+        if (Pages.size() != 2)
+            return false;
+        if (Pages.get(0) != Pages.get(1) - 1)
+            return false;
+        if (Pages.get(0) % 2 == 0)
+            return false;
+        if (Pages.get(1) % 2 == 1)
+            return false;
+        if (Pages.get(0) == 1 || Pages.get(1) == 400)
+            return false;
+        return true;
+    }
 }
