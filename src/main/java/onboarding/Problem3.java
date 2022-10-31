d1e24f0ad7491135c31c8fcfd5767c1945b4991f
AhnJoonSung <tjdtna01@naver.com>
2022-10-31 23:40:26 +0900

    Problem3 solve

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int[] count = new int[10];
+
+        for (int i = 1; i <= number; i++) {
+            int num = i;
+            while ( num > 0) {
+                count[num % 10]++;
+                num /= 10;
+            }
+        }
+
+        answer = count[3] + count[6] + count[9];
+
         return answer;
     }
 }
