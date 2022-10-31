710560e4e67a795803437a245b7b008f2ca33836
Soomin Lim <augustth511@gmail.com>
2022-10-31 21:07:05 +0900

    리팩토링: 돈의 액수를 큰 금액의 화폐 위주로 변환하여, 각 화폐의 개수를 구하는 기능을 메서드로 분리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,15 +5,21 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = convertMoneyToBill(money);
+
+        return answer;
+    }
+
+    private static List<Integer> convertMoneyToBill(int money) {
+        List<Integer> billList = new ArrayList<>();
 
         int[] bills = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
         for (int i = 0; i < bills.length; i++) {
-            answer.add(money / bills[i]);
+            billList.add(money / bills[i]);
             money %= bills[i];
         }
 
-        return answer;
+        return billList;
     }
 }
