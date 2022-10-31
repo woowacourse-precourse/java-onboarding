bc0d6385371d33d0585143e5ecf6ea31701348af
sysliar <sysliar@gmail.com>
2022-10-31 16:30:31 +0900

    Q7. add friendsMap, points HashMap

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    static HashMap<String, Set<String>> friendsMap = new HashMap<>();
+    static HashMap<String, Integer> points = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
