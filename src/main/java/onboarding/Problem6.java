7a269db960cb43761cea2f3f82e7dcff1a909e60
KangSungBum <peter9706@naver.com>
2022-10-31 15:57:54 +0900

    feat: Problem6 기능 구현 완료
    initFormsMap 메소드 추가 - 데이터 관리 FormsMap 초기화
    trimName 메소드 추가 - 문자열 trim & 기능 조건 확인 후 set 추가
    sortEmailSet 메소드 추가 - 최종 반환 리스트 값 추가 및 sort

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,52 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    static HashMap<String, String> formsMap = new HashMap<>();
+    static HashSet<String> answerSet = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        initFormsMap(forms);
+        trimName();
+        sortEmailSet(answer);
+
         return answer;
+    }
+
+    private static void initFormsMap(List<List<String>> forms) {
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String name = form.get(1);
+
+            formsMap.put(name, email);
+        }
+    }
+
+    private static void trimName() {
+        for (String name : formsMap.keySet()) {
+            int nameLength = name.length();
+
+            for (int trim = 2; trim < nameLength; trim++) {
+                for (int i = 0; i < nameLength - trim+1; i++) {
+                    String trimmedName = name.substring(i, i + trim);//error
+                    for (String nameInSet : formsMap.keySet()) {
+                        if (nameInSet.contains(trimmedName) && !nameInSet.equals(name)) {
+                            answerSet.add(formsMap.get(nameInSet));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static List<String> sortEmailSet(List<String> rtnList) {
+        for (String name : answerSet) {
+            rtnList.add(name);
+        }
+        Collections.sort(rtnList);
+
+        return rtnList;
     }
 }
