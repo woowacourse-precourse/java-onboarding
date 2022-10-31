3c9a690cc3ea672007a83b899187b4812fbb8bb4
himjjong <jjijji5728@tukorea.ac.kr>
2022-10-31 14:18:06 +0900

    feat : 필요한 함수정리와 checkForms 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,37 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    static List<String> email = new ArrayList<>();
+    static List<String> nickname = new ArrayList<>();
+    static HashSet<Integer> data = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        checkForms(forms);
+        findNickname(nickname);
+        answer = findEmail();
+        sortEmail(answer);
         return answer;
     }
+    static void checkForms(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            addData(forms.get(i));
+        }
+    }
+    static void addData(List<String> data) {
+    }
+    static void findNickname(List<String> nickname) {
+    }
+    static void compareNickName(String nickname, int mypos){
+    }
+    static void checkEmailIndex(String substr, int mypos ) {
+    }
+    static List<String> findEmail() {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+    static void sortEmail(List<String> email){
+    }
 }
+
