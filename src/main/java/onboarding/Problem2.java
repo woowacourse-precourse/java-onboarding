584b6225bf8b46cc0a4ee9a8c68ab6649d84e4bd
송민진 <m_6595@naver.com>
2022-11-01 02:29:12 +0900

    #2 feat: convert charList to String

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer;
         List<String> charList = new ArrayList<>(List.of(cryptogram.split("")));
 
         while (true) {
@@ -15,12 +15,16 @@ public class Problem2 {
             if (indexesToRemove.isEmpty()) {
                 break;
             }
-            removeIndexes(charList, indexesToRemove);
+            removeChars(charList, indexesToRemove);
         }
+
+        answer = convertToString(charList);
         return answer;
     }
 
     private static List<Integer> findIndexesToRemove(List<String> charList) {
+
+        // 반복되는 문자들의 인덱스를 indexesToRemove라는 리스트에 넣기
         List<Integer> indexesToRemove = new ArrayList<>();
         for (int i = 0; i < charList.size(); i++) {
             if (i != 0 && charList.get(i).equals(charList.get(i-1))) {
@@ -28,14 +32,23 @@ public class Problem2 {
                 indexesToRemove.add(i-1);
             }
         }
+        // 인덱스를 앞에서부터 삭제하면 index out of range 오류 발생 -> 방지 위해 reverse 정렬하기
         Collections.sort(indexesToRemove, Comparator.reverseOrder());
         return indexesToRemove;
     }
 
-    private static List<String> removeIndexes(List<String> charList, List<Integer> indexesToRemove) {
+    private static List<String> removeChars(List<String> charList, List<Integer> indexesToRemove) {
         for (int index : indexesToRemove) {
             charList.remove(index);
         }
         return charList;
+    }
+
+    private static String convertToString(List<String> charList) {
+        StringBuilder result = new StringBuilder();
+        for (String letter : charList) {
+            result.append(letter);
+        }
+        return result.toString();
     }
 }
