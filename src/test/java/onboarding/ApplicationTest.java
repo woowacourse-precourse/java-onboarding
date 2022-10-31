da800ba8d548e0866c0fb9e11168969c3976d2c8
taeyun-work <wmfajr1357@gmail.com>
2022-10-31 16:56:35 +0900

    [Refac] Problem4 - split by function

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -91,6 +91,13 @@ class ApplicationTest {
             String result = "R olev blf";
             assertThat(Problem4.solution(word)).isEqualTo(result);
         }
+
+        @Test
+        void case2() {
+            String word = "abcdefghijk ZYX $$%%!!@#$$";
+            String result = "zyxwvutsrqp ABC $$%%!!@#$$";
+            assertThat(Problem4.solution(word)).isEqualTo(result);
+        }
     }
 
     @Nested
