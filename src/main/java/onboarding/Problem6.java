e9d1c00199bee09ca21cdc0b27bf8dab41bd85e1
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 01:53:31 +0900

    refactor(feat) : 2가지이상연속된단어가 포함되어있는지 체크하는 메서드 lcs 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,6 +12,25 @@ public class Problem6 {
         List<String> nameList = valueExtraction(forms,1);
         return getEmail(nameList,emailList);
     }
+
+    public static boolean lcs(String str1, String str2){
+        int[][] arr = new int[str1.length()+1][str2.length()+1];
+        int max=0;
+        for(int i=1;i<=str1.length();i++){
+            for(int j=1;j<=str2.length();j++){
+                if(max>1 && i>=2){
+                    return true;
+                }
+
+                if (str1.charAt(i-1) == str2.charAt(j-1)) {
+                    arr[i][j]=arr[i-1][j-1]+1;
+                    max = max>arr[i][j] ? max : arr[i][j];
+                }
+            }
+        }
+        return false;
+    }
+
     public static List<String> getEmail(List<String> nameList,List<String> emailList){
         List<String> answer = new ArrayList<>();
         for(int i=0;i<nameList.size();i++){
