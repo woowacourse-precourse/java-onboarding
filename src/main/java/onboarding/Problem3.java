219e78eee52bb0b1bf05aee41b4768e3eafd2c77
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 02:34:34 +0900

    feat(PROBLEM3): 최종결과-n까지의 3, 6, 9 개수 합

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int num= 1; num<=number; num++){
+            answer += count369(num);
+        }
         return answer;
     }
 
