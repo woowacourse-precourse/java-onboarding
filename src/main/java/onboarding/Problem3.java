1e9a14762a523f519955ba332b8524dd5fb55e0c
aud19 <aud19980518@gmail.com>
2022-10-31 20:40:07 +0900

    1주차 3번 solution 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            answer += numMod(i);
+        }
         return answer;
     }
 
