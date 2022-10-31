7645a9b64a430f2e8f8ed7804d101f3890fa0321
Kijung Park <kjpark4321@naver.com>
2022-10-31 23:38:55 +0900

    2번: 특정 char에 해당되는 연속되는 문자 제거 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        replaceTargetChar(cryptogram, 'o');
+
         return answer;
+    }
+
+    public static String replaceTargetChar(String cryptogram, char targetChar){
+        String targetAlpha = Character.toString(targetChar);
+
+        String resultCryptogram = cryptogram.replaceAll(targetAlpha+"{2,}", "");
+        System.out.println(resultCryptogram);
+        return resultCryptogram;
     }
 }
