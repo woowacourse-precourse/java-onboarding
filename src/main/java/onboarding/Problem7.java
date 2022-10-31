93efce90ed0f8e9aec783a356bb28d282df6744c
Isu Shin <isu010614@naver.com>
2022-10-31 18:14:14 +0900

    feat: Add function to check if ID is already friends with user

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
+    static HashSet<String> alreadyFriends = new HashSet<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void checkAllRelationInfo(List<List<String>> friends, String user) {
+        for (List<String> readOneList : friends) {
+            String leftID = readOneList.get(0);
+            String rightID = readOneList.get(1);
+            isAlreadyFriend(leftID, rightID, user);
+        }
+    }
+
+    public static void isAlreadyFriend(String leftID, String rightID, String user) {
+        if (leftID.equals(user)) {
+            alreadyFriends.add(rightID);
+        }
+        if (rightID.equals(user)) {
+            alreadyFriends.add(leftID);
+        }
     }
 }
