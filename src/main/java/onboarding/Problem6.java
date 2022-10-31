d2457affe2ac1f21bdbcfe44030eba25cb20263c
paran <paran@hoon>
2022-10-31 13:55:58 +0900

    6번 : 코드 가독성 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,31 +11,27 @@ public class Problem6 {
         List<String> answer = new ArrayList<String>();
         String email, nickname, nickname2 = null;
         int result = 0;
-        
-        try {
-        	for(int i = 0 ; i < forms.size()-1 ; i ++) {
-        		nickname = forms.get(i).get(1);
-        		for(int j = 0 ; j < forms.size() ; j++) {
-        			nickname2 = forms.get(j).get(1);
-        			if(nickname != nickname2) {
-        				result = equals(nickname,nickname2);
-        				if(result == 1) {
-        					email = forms.get(j).get(0);
-        					answer.add(email);
-        					System.out.println(answer);
-        				}
-        			}
-        			
-        		}
-        	}
-        	
+
+    	for(int i = 0 ; i < forms.size()-1 ; i ++) {
+    		nickname = forms.get(i).get(1);
+    		
+    		for(int j = 0 ; j < forms.size() ; j++) {
+    			nickname2 = forms.get(j).get(1);
+    			
+    			if(nickname != nickname2) {
+    				result = equals(nickname,nickname2);
+    				
+    				if(result == 1) {
+    					email = forms.get(j).get(0);
+    					answer.add(email);
+    				}
+    			}
+    		}
         	List<String> newList = answer.stream().distinct().collect(Collectors.toList());
         	Collections.sort(newList);
         	
-        	answer = newList;
-		} catch (Exception e) {
-			System.out.println("오류 발생 -> " + e.getMessage());
-		}
+        	answer = newList;	
+		} 
 		return answer;
     }
     
@@ -43,26 +39,28 @@ public class Problem6 {
     public static int equals(String nickname1, String nickname2) {
     	int result = 0;
     	String a = null;
-    	try {
-    		for(int i = 0; i < nickname1.length(); i++) {
-    			if(i != nickname1.length()-1) {
-    				a = nickname1.substring(i,i+2);
-    			}
-    			for(int j = 0; j < nickname2.length(); j++) {
-    				if(j != nickname2.length()-1) {
-    					String b = nickname2.substring(j,j+2);
-    					if(a.contains(b)) {
-    						result = 1;
-    						break;
-    					} 
-    				}
-    			}
-    			if(result == 1) {
-    				break;
-    			}
-    		}
-		} catch (Exception e) {
-			System.out.println("오류 메소드 -> " + e.getMessage());
+		for(int i = 0; i < nickname1.length(); i++) {
+			
+			if(i != nickname1.length()-1) {
+				a = nickname1.substring(i,i+2);
+			}
+			
+			for(int j = 0; j < nickname2.length(); j++) {
+				
+				if(j != nickname2.length()-1) {
+					String b = nickname2.substring(j,j+2);
+					
+					if(a.contains(b)) {
+						result = 1;
+						break;
+					}
+					
+				}
+			}
+			
+			if(result == 1) {
+				break;
+			}
 		}
 		return result;
     }
