844023871cc01fd91fb4e8699fa6620716e8a4dd
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-01 00:35:33 +0900

    last_commit

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -18,7 +18,47 @@ public class Problem7 {
             return false;
         return true;
     }
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> map){
+    public static HashMap<String, Integer> add_visitor(HashMap<String, Integer> map, List<String> visitors)
+    {
+        for (int j = 0; j < visitors.size(); j++)
+            if (map.get(visitors.get(j)) != -1)
+                map.put(visitors.get(j),map.get(visitors.get(j)) + 1);
+        return map;
+    }
+    public static HashMap<String,Integer> check_friend(String user, List<List<String>> friends,
+                                                       HashMap<String, Integer> map, List<String> user_friends)
+    {
+        for (int i = 0; i < user_friends.size(); i++)
+        {
+            for (int j = 0; j < friends.size(); j++)
+            {
+                if (friends.get(j).get(0).equals(user_friends.get(i))
+                        && !friends.get(j).get(1).equals(user) && map.get(friends.get(j).get(1)) != -1)
+                    map.put(friends.get(j).get(1), map.get(friends.get(j).get(1)) + 10);
+                else if (friends.get(j).get(1).equals(user_friends.get(i))
+                        && !friends.get(j).get(0).equals(user) && map.get(friends.get(j).get(0)) != -1)
+                    map.put(friends.get(j).get(0), map.get(friends.get(j).get(0)) + 10);
+            }
+        }
+        return map;
+    }
+    public static HashMap<String, Integer> sort_by_key(HashMap<String, Integer> map){
+        List<Map.Entry<String, Integer> > list =
+                new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2)
+            {
+                return (o1.getKey()).compareTo(o2.getKey());
+            }
+        });
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+    public static HashMap<String, Integer> sort_by_value(HashMap<String, Integer> map){
         List<Map.Entry<String, Integer> > list =
                 new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
@@ -33,30 +73,6 @@ public class Problem7 {
             temp.put(aa.getKey(), aa.getValue());
         }
         return temp;
-    }
-    public static HashMap<String,Integer> check_friend(String user, List<List<String>> friends,
-                                                       HashMap<String, Integer> map, List<String> user_friends)
-    {
-        for (int i = 0; i < user_friends.size(); i++)
-        {
-            for (int j = 0; j < friends.size(); j++)
-            {
-                if (friends.get(j).get(0).equals(user_friends.get(i))
-                && !friends.get(j).get(1).equals(user) && map.get(friends.get(j).get(1)) != -1)
-                    map.put(friends.get(j).get(1), map.get(friends.get(j).get(1)) + 10);
-                else if (friends.get(j).get(1).equals(user_friends.get(i))
-                        && !friends.get(j).get(0).equals(user) && map.get(friends.get(j).get(0)) != -1)
-                    map.put(friends.get(j).get(0), map.get(friends.get(j).get(0)) + 10);
-            }
-        }
-        return map;
-    }
-    public static HashMap<String, Integer> add_visitor(HashMap<String, Integer> map, List<String> visitors)
-    {
-        for (int j = 0; j < visitors.size(); j++)
-            if (map.get(visitors.get(j)) != -1)
-                map.put(visitors.get(j),map.get(visitors.get(j)) + 1);
-        return map;
     }
     public static List<String> make_answer(HashMap<String, Integer> map)
     {
@@ -96,7 +112,8 @@ public class Problem7 {
         }
         map = add_visitor(map, visitors);
         map = check_friend(user, friends, map, user_friends);
-        map = sortByValue(map);
+        map = sort_by_key(map);
+        map = sort_by_value(map);
         answer = make_answer(map);
         return answer;
     }
