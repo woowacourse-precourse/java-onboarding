a43c63c2758b627d5918210f90ab483e78bc0060
Jeonsoyeon <itso22@naver.com>
2022-10-31 13:32:39 +0900

    feat(Problem4): 청개구리사전으로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static char convert(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            char alphabet = (char) (ch - 'A');
+            return (char) ('Z' - alphabet);
+        } else if (ch >= 'a' && ch <= 'z') {
+            char alphabet = (char) (ch - 'a');
+            return (char) ('z' - alphabet);
+        }
+        return ch;
+    }
+
 }
