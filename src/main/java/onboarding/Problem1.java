6beb5beb8b8437c3645b7999259ee21ef9e5b373
myungjunlee <dlaudwns789@gmail.com>
2022-10-31 18:19:50 +0900

    fix(Problem1): 페이지 번호의 각 자리 숫자를 곱하는 기능 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -22,28 +22,22 @@ class Problem1 {
 
     int plusPage(Integer page) {
         int num = 0;
-
         String page_str = Integer.toString(page);
 
         for(int i=0; i < page_str.length(); i++){
             num += Integer.parseInt(page_str.substring(i, i+1));
         }
-
         return num;
-
     }
 
     int multiplyPage(Integer page) {
-        int num;
-
+        int num = 1;
         String page_str = Integer.toString(page);
 
         for(int i=0; i < page_str.length(); i++){
             num *= Integer.parseInt(page_str.substring(i, i+1));
         }
-
         return num;
-
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -53,8 +47,6 @@ class Problem1 {
         if (!checkException(pobi, crong)) {
             return -1;
         }
-
-
 
         return answer;
     }
