733b48f4f5d15d444e6860ead4c8c69418c5428f
CHAYEONIL <jeay123@naver.com>
2022-10-31 23:45:32 +0900

    문제7 주요기능 해쉬맵을 value값으로 내림차순 정렬 후 return 수정, 예외처리 추천 점수가 같은 경우는 이름순으로 정렬 구현

--- docs/TODO_PROBLEM7.md
+++ docs/TODO_PROBLEM7.md
@@ -3,15 +3,17 @@
 ### 필요기능
 
 
-| 필요기능               | 변수명(함수명)                                    | 구현 여부(O, X) |
-|--------------------|---------------------------------------------|-------------|
-| 사용자와 친구인 유저의 이름 배열에 담기 | for (List<String> friend : friends) | O           |
-| 사용자와 함께 아는 친구의 이름 10점 주기 |  for (List<String> friend : friends)   | O           |
-| 방문한 유저를 1점 주기 이미 값이 있다면 1점 추가 |  for (String visit : visitors) | O           |
-| 해쉬맵을 value값으로 내림차순 정렬 후 retun | return friendWithUserDict.entrySet().stream() | O           |
+| 필요기능                           | 변수명(함수명)                                    | 구현 여부(O, X) |
+|--------------------------------|---------------------------------------------|-------------|
+| 사용자와 친구인 유저의 이름 배열에 담기         | for (List<String> friend : friends) | O           |
+| 사용자와 함께 아는 친구의 이름 10점 주기       |  for (List<String> friend : friends)   | O           |
+| 방문한 유저를 1점 주기 이미 값이 있다면 1점 추가  |  for (String visit : visitors) | O           |
+| 해쉬맵을 value값으로 내림차순 정렬 후 return | public int compare(Map.Entry<String, Integer> arr1, Map.Entry<String, Integer> arr2)| O           |
 
 ### 예외처리
 
-| 예외처리                | validation | 구현 여부(O, X) |
-|---------------------|------------|-------------|
-|    |  | X           |
+| 예외처리                | validation or 함수 | 구현 여부(O, X) |
+|---------------------|-----------------|-------------|
+|  최대 5명을 return  |    if(answer.size() > 5)  | X           |
+|  추천 점수가 0점일 경우 삭제  |    boolean removed = friendWithUserDict.entrySet().removeIf  | X           |
+|  추천 점수가 같은 경우는 이름순으로 정렬  |    if (arr1.getValue() == arr2.getValue())  | O           |
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> friendWithUserList = new ArrayList<>();
         Map<String, Integer> friendWithUserDict = new HashMap<>();
+        List<String> answer = new ArrayList<>();
         // 사용자와 친구인 유저의 이름 배열에 담기
         for (List<String> friend : friends) {
             if (friend.contains(user)) {
@@ -38,11 +39,22 @@ public class Problem7 {
                 friendWithUserDict.computeIfPresent(visit, (k, v) -> v + 1);
             }
         }
-        // 해쉬맵을 value값으로 내림차순 정렬 후 retun
-        return friendWithUserDict.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())
-                )
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+
+        // 해쉬맵을 value값으로 내림차순 정렬 후 return
+        List<Map.Entry<String,Integer>> checkArr = new ArrayList<>(friendWithUserDict.entrySet());
+        checkArr.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> arr1, Map.Entry<String, Integer> arr2) {
+                // 추천 점수가 같은 경우는 이름순으로 정렬
+                if (arr1.getValue() == arr2.getValue()) {
+                    return arr1.getKey().compareTo(arr2.getKey());
+                }
+                return arr2.getValue().compareTo(arr1.getValue());
+            }
+        });
+        for (Map.Entry<String,Integer> entry : checkArr){
+            answer.add(entry.getKey());
+        }
+        return answer;
     }
 }
