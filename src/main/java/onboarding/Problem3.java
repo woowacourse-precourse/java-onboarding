f4d522eb9a8905b72a725e9615958eea95cef9c2
Yongwoo Choi <cyw7515@naver.com>
2022-11-01 03:27:45 +0900

    fix(problem3): 숫자 입력 타당성 검증 로직 추가 (예외시 -1 반환)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,14 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        if (!validate(number)) return -1;
+
         int answer = getClapCount(number);
 
         return answer;
     }
 
+    private static boolean validate(int number) {
+        if (number < 1 || number > 1000) return false;
+        return true;
+    }
+
     private static int getClapCount(int number) {
         int[] clapCount = new int[number + 1];
         clapCount[1] = 0;
+
         for (int i = 2; i <= number; i++) {
             clapCount[i] = clapCount[i-1] + clapCheck(i);
         }
