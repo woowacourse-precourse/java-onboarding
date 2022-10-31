6a1ecf0219355110d6f5563cc51b3663c52b0154
TaeMin <xoals3094@naver.com>
2022-10-31 21:47:51 +0900

    Problem7: add createUserKnowFriends method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,20 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<String> createUserKnowFriends(List<List<String>> friends, String user){
+        List<String> userKnowFriends = new ArrayList<String>();
+
+        for(List<String> friendship: friends){
+            if(friendship.contains(user)){
+                int userIndex = friendship.indexOf(user);
+                int friendIndex = notOperator(userIndex);
+                String friend = friendship.get(friendIndex);
+                userKnowFriends.add(friend);
+            }
+        }
+        return userKnowFriends;
     }
 
     public static int notOperator(int friendAorB){
