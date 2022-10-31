49e35349a7b9362ed7003de1465ab73297d76dca
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-01 01:51:59 +0900

    [PROBLEM 1] 최댓값 체크
    1. 왼쪽 페이지의 각자리 숫자의 합과 곱중 최댓값 구하여 본인의 점수로 설정
    2. 점수 비교하여 결과값 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,9 +6,29 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        if(exception(pobi) || exception(crong)) return -1;
+        if(exception(pobi) || exception(crong)) answer = -1;
+
+        int pobiMax = Math.max(checkMaxNum(pobi.get(0)), checkMaxNum(pobi.get(1)));
+        int crongMax = Math.max(checkMaxNum(crong.get(0)), checkMaxNum(crong.get(1)));
+
+        if(pobiMax > crongMax) answer = 1;
+        else if(pobiMax < crongMax) answer = 2;
+        else answer = 0;
 
         return answer;
+    }
+
+    static int checkMaxNum(int num) {
+        int sum = 0;
+        int mul = 1;
+
+        while(num > 0) {
+            sum += num % 10;
+            mul *= num% 10;
+            num /= 10;
+        }
+
+        return Math.max(sum, mul);
     }
 
     static boolean exception(List<Integer> list) {
