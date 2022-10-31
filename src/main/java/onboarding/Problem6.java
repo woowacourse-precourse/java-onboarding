6b018e06ebb4e5f01a5cf72f4471ebc6a1230ca7
jeongjihun <wlgns2234@naver.com>
2022-10-31 15:38:17 +0900

    Feat: 두개 이름에 대해 중복부분을 판단하고 set에 추가해주는 기능 완성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,26 +1,72 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
-    public static Map<String, String> makeHashMap(List<List<String>> forms) {
-        Map<String, String> hash = new HashMap<>();
+    public static Map<String, String> hash;
+    public static Set<String> result = new HashSet<>();
+
+
+    public static void makeHashMap(List<List<String>> forms) {
+        hash = new HashMap<>();
         for(int i = 0; i < forms.size(); i++) {
             List<String> data = forms.get(i);
             hash.put(data.get(1), data.get(0));
         }
-        return hash;
+    }
+
+    public static void check(String name1, String name2) {
+        if (duplication(name1, name2)) {
+            result.add(name1);
+            result.add(name2);
+        }
+    }
+
+    public static boolean duplication(String name1, String name2) {
+        System.out.println(name1 + " : " + name2);
+        // 워니 엠제이
+        // i = 2
+        // j = 0, 1
+        for (int i = 2; i < name1.length() + 1; i++) {
+            for (int j = 0; j + i < name1.length() + 1; j++) {
+                String str = name1.substring(j, j+i);
+                System.out.println(str);
+
+                if (name2.contains(str)) {
+                    String emailName1 = hash.get(name1);
+                    String emailName2 = hash.get(name2);
+                    result.add(emailName1);
+                    result.add(emailName2);
+                }
+            }
+        }
+        return false;
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
 
-        //해시 테이블 생성
-        Map<String, String> hash = makeHashMap(forms);
+        makeHashMap(forms);
 
+        List<String> firstCrew;
+        List<String> secondCrew;
+        for (int i = 0; i < forms.size() - 1; i++) {
+            for (int j = i + 1; j < forms.size(); j++) {
+                firstCrew = forms.get(i);
+                secondCrew = forms.get(j);
+                check(firstCrew.get(1), secondCrew.get(1));
+            }
+        }
 
+        Iterator<String> iter = result.iterator();
+
+        while (iter.hasNext()) {   // 꺼낼 것이 있다면 true 리턴.
+            String str = iter.next(); // next()메소드는 하나를 꺼낸다. 하나를 꺼내면 자동으로 다음것을 참조한다.
+            answer.add(str);
+        }
+
+        //오름차순 정렬
+        Collections.sort(answer);
 
         return answer;
     }
