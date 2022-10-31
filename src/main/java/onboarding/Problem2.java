ef4a7986d67ace286d4fef204d6b104347b69c99
farmJun <junpreme1205@gmail.com>
2022-10-31 15:48:39 +0900

    Problem2. 크립토그램을 해석하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,7 +5,20 @@ import java.util.Vector;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        return answer;
+        return answer = decoder(cryptogram);
+    }
+
+    //크립토그램을 해석하는 함수
+    public static String decoder(String word) {
+        Vector<Character> answerVector = new Vector<Character>();
+        String decorded = "";
+        for (char c : word.toCharArray()) {
+            answerVector = isAddable(answerVector, c);
+        }
+        for (char c : answerVector) {
+            decorded += c;
+        }
+        return decorded;
     }
 
     //정답 문자열에 추가될 수 있는지 확인하는 함수
