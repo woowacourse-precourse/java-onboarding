0e1a7d2352003452a464e439f8f611f9332b37b5
wugawuga <dnr3655@naver.com>
2022-10-31 14:20:15 +0900

    feat: 3, 6, 9 확인 후 박수 카운트

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,22 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            String value = String.valueOf(i);
+            answer = clap(answer, i);
         }
         return answer;
+    }
+
+    private static int clap(int answer, int i) {
+        String value = String.valueOf(i);
+        for (String s : value.split("")) {
+            if (checkThreeSixNine(s)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    private static boolean checkThreeSixNine(String str) {
+        return str.equals("3") || str.equals("6") || str.equals("9");
     }
 }
