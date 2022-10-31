272bbdb55d73e22cd04d5730d63fb60980226487
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-10-31 17:43:33 +0900

    feat: 3, 6, 9 포함 유무 판별 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,8 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static boolean isContainThreeSixNine(int number) {
+        return number % 10 == 3 || number % 10 == 6 || number % 10 == 9;
+    }
 }
