6bbf69d4a1c4268823c6c93cdb7f44161c157907
pdot715 <pdot715@gmail.com>
2022-10-31 19:23:47 +0900

    feat(문제1):포비와 크롱의 점수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -34,12 +34,40 @@ class Problem1 {
 
         // 각 자리 숫자를 모두 더하거나 모두 곱해서 배열에 저장
         for (int i = 0; i < page_nums.length; i++) {
-            while(page_nums[i] > 0) {
+            while (page_nums[i] > 0) {
                 page_nums_sum[i] += page_nums[i] % 10;
                 page_nums_multi[i] *= page_nums[i] % 10;
                 page_nums[i] /= 10;
             }
         }
+
+        // 포비와 크롱의 점수 구하기
+        int pobi_num = Integer.MIN_VALUE;
+        int crong_num = Integer.MIN_VALUE;
+
+        // 포비와 크롱의 점수를 각각 구하기 위해 현재 점수를 Integer.MIN_VALUE로 초기화 후
+        // 그 값을 각 자리 숫자 더한 값부터 비교 후, 각 자리 숫자 곱한 값이랑 비교하여 점수를 구한다
+
+        // 포비 페이지 수와 관련된 값은 인덱스 0, 1에 저장되어 있다.
+        for (int i = 0; i < 2; i++) {
+            if(pobi_num <= page_nums_sum[i]) {
+                pobi_num = page_nums_sum[i];
+            }
+            if(pobi_num <= page_nums_multi[i]) {
+                pobi_num = page_nums_multi[i];
+            }
+        }
+
+        // 크롱 페이지 수와 관련된 값은 인덱스 2, 3에 저장되어 있다.
+        for (int i = 2; i < 4; i++) {
+            if(crong_num <= page_nums_sum[i]) {
+                crong_num = page_nums_sum[i];
+            }
+            if(crong_num <= page_nums_multi[i]) {
+                crong_num = page_nums_multi[i];
+            }
+        }
+
 
         return answer;
     }
