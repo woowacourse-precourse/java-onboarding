099de73cd7b9dec471522ebb8fab8a8221c96de9
KimJaeSung <behappy0916@naver.com>
2022-10-31 14:33:08 +0900

    feat java-onboarding-7-친구 점수 관계를 나타내는 hashMap구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,6 +6,26 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    static void createfriendsGroupsScore(HashMap<String, Integer> friendsGroupsScore, List<List<String>>friends,String userName, List visitor){
+        for(int i=0; i<friends.size(); i++){
+            String friends1 = friends.get(i).get(0);
+            String friends2 = friends.get(i).get(1);
+            if(friends2.equals(userName) || friends1.equals(userName)){
+                continue;
+            }
+            friendsGroupsScore.put(friends1,0);
+            friendsGroupsScore.put(friends2,0);
+        }
+        for(int i=0; i<visitor.size(); i++){
+            if(friendsGroupsScore.containsKey(visitor.get(i))){
+                friendsGroupsScore.put((String) visitor.get(i),friendsGroupsScore.get(visitor.get(i))+1);
+            }
+            else{
+                friendsGroupsScore.put((String) visitor.get(i),1);
+            }
+
+        }
+    }
     static void createfriendsGroups(HashMap<String,List<String>> friendsGroups, List<List<String>>friends, String userName){
         for(int i=0; i<friends.size(); i++){
             String friends1 = friends.get(i).get(0);
@@ -24,7 +44,11 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         HashMap<String,List<String>> friendsGroups = new HashMap<>();
+        HashMap<String, Integer> friendsGroupsScore = new HashMap<>();
+
         createfriendsGroups(friendsGroups,friends,user);
+        createfriendsGroupsScore(friendsGroupsScore,friends,user,visitors);
+
         return answer;
     }
 }
