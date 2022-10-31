2b383bbcfe15f7035b156a8263744fca8061e9e0
Woong99 <dndry1072@naver.com>
2022-10-31 21:13:44 +0900

    feat: 문제 3 - 입력받은 숫자를 문자열로 바꾸는 메소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,8 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static String intToString(int number) {
+        return Integer.toString(number);
+    }
 }
