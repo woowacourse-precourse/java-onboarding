80ddb39cfdaa4e469ee8b233eb360319de17c91b
cgk95 <kjkj5868@gmail.com>
2022-11-01 01:07:21 +0900

    P5 for-each문으로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,16 +1,14 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.HashMap;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int[] money_lst = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        for (int i = 0; i < money_lst.length; i++) {
-            money = getChanges(money, answer, money_lst[i]);
+        for (int j : money_lst) {
+            money = getChanges(money, answer, j);
         }
         return answer;
     }
