2304bca6f010fd2e922599c22d5c6bbea5b12a9c
소대훈 <theogns0681@naver.com>
2022-10-31 18:28:30 +0900

    [feat] (문제7) 직간접적 친구 점수 부여

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class Problem7 {
+    static HashMap<String,Integer> friendScore;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        friendScore = new HashMap<>();
+        findPotentialFriendAndScore(user, friends);
         return answer;
+    }
+    /*
+        사용자를 직접 알고있다면 -1(목록에서 제외)를
+        사용자를 간접적으로 알고 있다면 10점씩 부여
+     */
+    private static void findPotentialFriendAndScore(String user, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            if (Objects.equals(friend.get(0), user)) {
+                friendScore.put(friend.get(1), -1);
+            } else if (Objects.equals(friend.get(1), user)) {
+                friendScore.put(friend.get(0), -1);
+            } else {
+                friendScore.put(friend.get(0), friendScore.getOrDefault(friend.get(0), 0) + 10);
+                friendScore.put(friend.get(1), friendScore.getOrDefault(friend.get(1), 0) + 10);
+            }
+        }
     }
 }
