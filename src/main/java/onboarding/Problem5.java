aaeeaa49d344077b78dba85980001b704b30c9d9
swonny <wonny921@gmail.com>
2022-10-31 17:35:15 +0900

    feat: 화폐 배열, 화폐 개수 배열 생성 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> count = List.of(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(50000);
     }
 }
