ffcbd02b63ed01d9059a4a19cf126b529830b3f3
bradbrad97 <taehyun0121@naver.com>
2022-10-31 22:10:06 +0900

    problem7: 방문한 사람 점수 가산 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -26,7 +26,7 @@ public class Problem7 {
             if (friendConnection.containsKey(friend1)){
                 ArrayList<String> myFriends = friendConnection.get(friend1);
                 updateMyFriend(myFriends, friend2);
-                friendConnection.put(friend1, myFriends);
+                friendConnection.replace(friend1, myFriends);
             } else{
                 ArrayList<String> myFriend = setMyFriend(friend2);
                 friendConnection.put(friend1, myFriend);
@@ -35,7 +35,7 @@ public class Problem7 {
             if (friendConnection.containsKey(friend2)){
                 ArrayList<String> myFriends = friendConnection.get(friend2);
                 updateMyFriend(myFriends, friend1);
-                friendConnection.put(friend2, myFriends);
+                friendConnection.replace(friend2, myFriends);
             } else{
                 ArrayList<String> myFriend = setMyFriend(friend2);
                 friendConnection.put(friend1, myFriend);
@@ -45,37 +45,65 @@ public class Problem7 {
         return friendConnection;
     }
 
-    public static Map<String, Integer> createScoreTable(String user, Map<String, ArrayList<String>> friendConnection){
+    public static void setMutualFriendScore(ArrayList<String> friends, HashMap<String, Integer> scoreTable,
+                                            HashSet<String> notRecommended){
+        for (String friend: friends){
+            if (notRecommended.contains(friend))
+                continue;
+
+            if (scoreTable.containsKey(friend)){
+                int score = scoreTable.get(friend);
+                score += 10;
+                scoreTable.replace(friend, score);
+            } else {
+                int score = 10;
+                scoreTable.put(friend, score);
+            }
+
+        }
+    }
+
+    public static void setVisitorScore(Map<String, Integer> scoreTable, List<String> visitors,
+                                       HashSet<String> notRecommended){
+        for (String visitor: visitors){
+            if (notRecommended.contains(visitor))
+                continue;
+
+            if (scoreTable.containsKey(visitor)){
+                int score = scoreTable.get(visitor);
+                score += 1;
+                scoreTable.replace(visitor, score);
+            } else {
+                int score =1;
+                scoreTable.put(visitor,score);
+            }
+        }
+    }
+
+    public static Map<String, Integer> createScoreTable(String user, Map<String, ArrayList<String>> friendConnection,
+                                                        List<String> visitors){
         ArrayList<String> userFriends = friendConnection.get(user);
-        HashMap<String, Integer> scoreTable = new HashMap<>();
         HashSet<String> notRecommended = new HashSet<>(userFriends);
+        HashMap<String, Integer> scoreTable = new HashMap<>();
         notRecommended.add(user);
+
 
         for (String userFriend: userFriends){
             ArrayList<String> friendsOfFriend = friendConnection.get(userFriend);
-            for (String friendOfFriend : friendsOfFriend){
-                if (notRecommended.contains(friendOfFriend))
-                    continue;
-
-                if (scoreTable.containsKey(friendOfFriend)){
-                    int score = scoreTable.get(friendOfFriend);
-                    score += 10;
-                    scoreTable.replace(friendOfFriend, score);
-                } else {
-                    int score = 10;
-                    scoreTable.put(friendOfFriend, score);
-                }
-
-            }
+            setMutualFriendScore(friendsOfFriend, scoreTable, notRecommended)
         }
+
+        setVisitorScore(scoreTable, visitors, notRecommended);
 
         return scoreTable;
     }
 
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Map<String, ArrayList<String>> friendConnection = createFriendConnection(friends);
-        Map<String, Integer> scoreTable = createScoreTable(user, friendConnection);
+
 
         return answer;
     }
