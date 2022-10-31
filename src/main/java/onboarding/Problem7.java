08b97379c4aef171f0d01d7c3bc0212f4203376f
WonjunS <seowj0710@gmail.com>
2022-11-01 01:27:30 +1100

    Problem 7: 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,57 +1,81 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 
     static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<String>();
 
-        List<String> friendList = new ArrayList<>();
+        HashMap<String, Set<String>> friendList = new HashMap<>();
         HashMap<String, Integer> scores = new HashMap<>();
 
         addFriendList(user, friends, friendList);
-        addMutableFriendScore(user, friendList, friends, scores);
-        addVisitorScore(friendList, visitors, scores);
+        addMutableFriendScore(user, friendList, scores);
+        addVisitorScore(user, friendList, visitors, scores);
+
         answer = sortScore(scores);
 
         return answer;
     }
 
-    static void addFriendList(String user, List<List<String>> friends, List<String> friendList) {
+    // user 의 친구 목록
+    static void addFriendList(String user, List<List<String>> friends, HashMap<String, Set<String>> friendList) {
         for(List<String> users : friends) {
             String user1 = users.get(0);
             String user2 = users.get(1);
 
-            if(user1.equals(user)) friendList.add(user2);
-            if(user2.equals(user)) friendList.add(user1);
+            if(friendList.containsKey(user1)) {
+                friendList.get(user1).add(user2);
+            }
+            if(friendList.containsKey(user2)) {
+                friendList.get(user2).add(user1);
+            }
+
+            Set<String> set = new HashSet<>();
+            if(!friendList.containsKey(user1)) {
+                friendList.put(user1, set);
+                friendList.get(user1).add(user2);
+            }
+            if(!friendList.containsKey(user2)) {
+                friendList.put(user2, set);
+                friendList.get(user2).add(user1);
+            }
         }
     }
 
-    static void addMutableFriendScore(String user, List<String> friendList,
-                                    List<List<String>> friends, HashMap<String, Integer> scores) {
-        for(List<String> list : friends) {
-            String user1 = list.get(0);
-            String user2 = list.get(1);
-
-            if(!user1.equals(user) && !friendList.contains(user1)) {
-                scores.put(user1, (scores.getOrDefault(user1, 0) + 10));
-            }
-            if(!user2.equals(user) && !friendList.contains(user2)) {
-                scores.put(user2, (scores.getOrDefault(user2, 0) + 10));
+    // 함께 아는 친구 점수
+    static void addMutableFriendScore(String user, HashMap<String, Set<String>> friendList,
+                                    HashMap<String, Integer> scores) {
+        for(String candidate : friendList.keySet()) {
+            if(!candidate.equals(user) && !friendList.get(user).contains(candidate)) {
+                int count = countFriendNum(user, friendList, candidate);
+                scores.put(candidate, (count * 10));
             }
         }
     }
 
-    static void addVisitorScore(List<String> friendList, List<String> visitors, HashMap<String, Integer> scores) {
+    static int countFriendNum(String user, HashMap<String, Set<String>> friendList, String candidate) {
+        int count = 0;
+        for(String friend : friendList.get(user)) {
+            if(friendList.get(friend).contains(candidate)) count++;
+        }
+        return count;
+    }
+
+    // 타임라인 방문 점수
+    static void addVisitorScore(String user, HashMap<String, Set<String>> friendList,
+                                List<String> visitors, HashMap<String, Integer> scores) {
         List<String> temp = new ArrayList<>(visitors);
-        temp.removeAll(friendList);
+        temp.removeAll(friendList.get(user));
 
         for(String visitor : temp) {
             scores.put(visitor, (scores.getOrDefault(visitor, 0) + 1));
         }
     }
 
+    // 점수를 담고 있는 HashMap을 점수에 따라 내림차순 정렬
     static List<String> sortScore(HashMap<String, Integer> scores) {
         List<String> answer = new ArrayList<>();
 
