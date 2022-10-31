05c0e7da07679f935669f8e23ce929019416e6f0
YooDongSeon <inko51366@naver.com>
2022-10-31 19:46:24 +0900

    기능목록 작성 후 커밋

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+    /*
+        기능목록 :
+        1. 대문자를 저장할 upperList 초기화
+        2. 소문자를 저장할 lowerList 초기화
+        3. 결과를 담을 StringBuilder 초기화
+        4. 매개변수 word를 순회하며 대문자 - 65, 소문자 - 65 인덱스를 구한다.
+        5. 각각의 인덱스에 접근하여 StringBulder에 append한다.
+     */
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
