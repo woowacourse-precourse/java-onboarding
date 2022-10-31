24b626616ce534d2f5980ec75773a4360f4561e9
WooJin Kim <dnwls813@ajou.ac.kr>
2022-10-31 13:49:10 +0900

    [Add] Q4 : 엄마 알파벳 -> 청개구리 알파벳 변환 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,35 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
+    }
+
+
+    /**
+     * DESCRIPTION
+     *   전달받은 알파벳 1개를 청개구리 알파벳으로 변환
+     *
+     * Params
+     *   alphabet - 변환 전 알파벳(엄마 알파벳)
+     *
+     * RETURN
+     *   청개구리 알파벳
+     */
+    public static char convertAlphabet(char alphabet){
+        if(65 <= alphabet && alphabet <= 90){ // 대문자 변환
+            if(alphabet < 'A' + 13) // 26개 알파벳 중 1~13번째
+                return (char)('A' + (('Z' - 'A') - (alphabet - 'A')));
+            else
+                return (char)('Z' - (('Z' - 'A') - ('Z' - alphabet)));
+        }
+        else if(97 <= alphabet && alphabet <= 122){ // 소문자 변환
+            if(alphabet < 'a' + 13) // 26개 알파벳 중 1~13번째
+                return (char)('a' + (('z' - 'a') - (alphabet - 'a')));
+            else
+                return (char)('z' - (('z' - 'a') - ('z' - alphabet)));
+        }
+        else
+            return ' ';
     }
 }
