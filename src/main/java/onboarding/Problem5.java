91f2ae29248ebf4e585cdfd7c188125e88282f49
TaeMin <xoals3094@naver.com>
2022-10-31 21:37:49 +0900

    Problem5: add countMoneyByUnit method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int restMoney;
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static int countMoneyByUnit(int unit){
+        int count = restMoney / unit;
+        restMoney = restMoney % unit;
+        return count;
     }
 }
