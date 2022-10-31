f674ca4f9ae39a90a3b2950184d78604f276e422
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-01 03:46:24 +0900

    feat : add error validation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,31 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        boolean page = validation(pobi,crong);
         int answer = Integer.MAX_VALUE;
-        return answer;
+        if(!page){
+            return -1;
+        }
+        return 0;
+    }
+    private static boolean validation(List<Integer> pobi, List<Integer> crong){
+        boolean result = true;
+        int pobi0 = pobi.get(0);
+        int pobi1 = pobi.get(1);
+        int crong0 = crong.get(0);
+        int crong1 = crong.get(1);
+        if(1 != pobi1 - pobi0){
+            return false;
+        } else if(1 != crong1 - crong0){
+            return false;
+        } else if(pobi0 % 2 != 1 || crong0 % 2 != 1){
+            return false;
+        } else if(pobi1 % 2 != 0 || crong1 % 2 != 0){
+            return false;
+        } else if(pobi1 > 400 || crong1 > 400){
+            return false;
+        } else{
+            return result;
+        }
     }
 }
