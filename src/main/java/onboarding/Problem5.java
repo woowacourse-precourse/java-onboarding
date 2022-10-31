cbfbebddd30b826271beac998c6759343db747f7
alswn <alswn9938@gmail.com>
2022-10-31 17:19:22 +0900

    [Q5] feat : add "화폐 단위 List 초기화" 메소드

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static List<Integer> moneyList;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void initMoneyList(){
+        moneyList.add(50000);
+        moneyList.add(10000);
+        moneyList.add(5000);
+        moneyList.add(1000);
+        moneyList.add(500);
+        moneyList.add(100);
+        moneyList.add(50);
+        moneyList.add(10);
+        moneyList.add(1);
     }
 }
