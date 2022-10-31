54b08915aef79953fa4efacf6250d907489a20cf
LeeJejune <leejejune0625@gmail.com>
2022-10-31 14:45:22 +0900

    feat: [7번] 친구 목록 중  함께 아는 친구 점수를 계산하는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,5 +7,14 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    private static void calFriendScore(String user, Map<String, List<String>> friendsMap, Map<String, Integer> scoreMap) {
+        for (String name : friendsMap.get(user)) {
+            for (String recommend : friendsMap.get(name)) {
+                if (!friendsMap.get(user).contains(recommend) && !recommend.equals(user)) {
+                    scoreMap.put(recommend, scoreMap.getOrDefault(recommend, 0) + 10);
+                }
+            }
+        }
+    }
     }
 }
