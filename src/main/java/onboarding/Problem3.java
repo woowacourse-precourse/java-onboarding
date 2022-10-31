963f6e66f7cb308e9b9252e3c80148c5eb1adf92
SeungEon <js112099@gmail.com>
2022-11-01 00:49:21 +0900

    문제3 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++) {
+            if(i < 10) {
+                if(i == 3 || i == 6 || i == 9) {
+                    answer++;
+                }
+            }
+            else
+            {
+                String num_str = String.valueOf(i);
+
+                for(int j = 0; j < num_str.length(); j++) {
+                    if(num_str.charAt(j) == '3' || num_str.charAt(j) == '6' || num_str.charAt(j) == '9') {
+                        answer++;
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 }
