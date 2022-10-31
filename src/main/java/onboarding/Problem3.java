119f96e290e9548b3658481a232952b9bc238180
yesjuhee <juhee0924@g.skku.edu>
2022-10-31 18:25:40 +0900

    [PROBLEM3] 주어진 숫자에서 3,6,9가 몇번 들어가는지 카운트하는 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        int clapNumber = 0;
+
         int answer = 0;
         return answer;
+    }
+
+    public static int countMultipleOf3(int targetNumber){
+        int numberOfDigit = 0;
+        int numberOfMultipleOf3 = 0;
+        while(targetNumber > 0){
+            numberOfDigit = targetNumber % 10;
+            targetNumber /= 10;
+            if(numberOfDigit%3 == 0) numberOfMultipleOf3++;
+        }
+        return numberOfMultipleOf3;
     }
 }
