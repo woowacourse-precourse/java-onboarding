f31435ea9c15c7230402413f731a863f966476b1
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:06:50 +0900

    feat: 덧셈, 곱셈 중 큰 값

--- src/main/java/onboarding/problem1/Page.java
+++ src/main/java/onboarding/problem1/Page.java
@@ -1,8 +1,18 @@
 package onboarding.problem1;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Page {
+
+    private static int findMaxPage(int pageNumber) {
+        int[] singleDigitArray =
+                Stream.of(String.valueOf(pageNumber).split(""))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+
+        return Math.max(sumSingleDigit(singleDigitArray), multiplySingleDigit(singleDigitArray));
+    }
 
     private static int sumSingleDigit(int[] singleDigitArray) {
         return Arrays
