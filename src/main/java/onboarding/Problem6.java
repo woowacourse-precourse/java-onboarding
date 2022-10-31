17e5260a8e77a561e17aa54a9d243291a646fdd0
Your Name <you@example.com>
2022-11-01 02:09:34 +0900

    6번 기능목록 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+// 신청받은 닉네임 중                               이메일과 닉네임의 형식 [[aaa@email.com, "ㅁㅁㅁ"], ...]
+// 같은 글자가 연속적으로 포함 되는 닉네임을          같은 글자 포함을 확인하고
+//                                                 2글자를 나눠, 같은 글자 연속 포함을 확인한다.
+// 작성한 지원자의 이메일 목록을 return 하도록       닉네임 겹침이 확인된 이메일들의 값을 저장하여 
+//                                                겹침 확인시 이메일을 저장하는 리스트를 만든 후, 리스트에 포함이 되어 있지 않으면 저장한다.
+// solution 메서드를 완성하라.                      리스트 반환.
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
