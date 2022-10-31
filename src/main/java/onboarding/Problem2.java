f267562a61e4f8b6d2a96ad373c51a9e3daee201
hossang <zkzk104@naver.com>
2022-10-31 21:30:03 +0900

    refactor: 기존 코드를 중복 문자열 삭제하는 메소드(deleteOverlappedChar)와 문자열을 리턴하는 메소드(getString)로 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,42 +5,54 @@ import java.util.Deque;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        Deque<Character> dq1 = new ArrayDeque<>();
+        Deque<Character> deque = new ArrayDeque<>();
 
         for (int i = 0; i < cryptogram.length(); i++) {
-            dq1.addLast(cryptogram.charAt(i));
+            deque.addLast(cryptogram.charAt(i));
         }
+
         //중복 문자열 삭제하기
+        deque = deleteOverlappedChar(deque);
+
+        //결과 리턴
+        return getString(deque);
+
+    }
+
+    private static String getString(Deque<Character> deque) {
+        if (deque.size() == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character character : deque) {
+            sb.append(character);
+        }
+        return sb.toString();
+    }
+
+    private static Deque<Character> deleteOverlappedChar(Deque<Character> deque) {
         int pre;
         do {
-            pre = dq1.size();
+            pre = deque.size();
             if (pre == 0) {
                 break;
             }
             Deque<Character> dq2 = new ArrayDeque<>();
-            dq2.addLast(dq1.pollFirst());
-            while (!dq1.isEmpty()) {
-                if (dq2.peekLast() == dq1.peekFirst()) {
-                    while (dq2.peekLast() == dq1.peekFirst()) {
-                        dq1.pollFirst();
+            dq2.addLast(deque.pollFirst());
+            while (!deque.isEmpty()) {
+                if (dq2.peekLast() == deque.peekFirst()) {
+                    while (dq2.peekLast() == deque.peekFirst()) {
+                        deque.pollFirst();
                     }
                     dq2.pollLast();
                 }
-                if(dq2.peekLast() != dq1.peekFirst()) {
-                    dq2.addLast(dq1.pollFirst());
+                if(dq2.peekLast() != deque.peekFirst()) {
+                    dq2.addLast(deque.pollFirst());
                 }
             }
-            dq1 = dq2;
-        } while (pre != dq1.size());
-        //결과 리턴
-        if (pre == 0) {
-            return "";
-        }
-        StringBuilder sb = new StringBuilder();
-        for (Character character : dq1) {
-            sb.append(character);
-        }
-        return sb.toString();
+            deque = dq2;
+        } while (pre != deque.size());
 
+        return deque;
     }
 }
