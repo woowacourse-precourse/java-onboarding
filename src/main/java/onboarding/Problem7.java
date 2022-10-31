8cb7aa14194c3b7368c51e904dd398ea8d32996c
박시균\psk98 <psg980331@naver.com>
2022-10-31 21:41:01 +0900

    feat : 추천 점수 계산

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -25,6 +25,26 @@ public class Problem7 {
         //자기 자신 제거
         userFriend.remove(user);
 
+        //추천 점수 계산 , user의 친구의 친구일시 +10점
+        for (List<String> friendList : friends) {
+            String humanA = friendList.get(0);
+            String humanB = friendList.get(1);
+            String friendOfFriend = "";
+
+            //어떤 사람이 user의 친구일 경우
+            if(userFriend.contains(humanA) && !userFriend.contains(humanB)){
+                friendOfFriend = humanB;
+            }else if(userFriend.contains(humanB) && !userFriend.contains(humanA)){
+                friendOfFriend = humanA;
+            }
+
+            if(user.equals(friendOfFriend) || friendOfFriend.equals(""))
+                continue;
+
+            //친구의 친구Map에 추천 점수 추가
+            friendOfFriendMap.put(friendOfFriend ,  friendOfFriendMap.getOrDefault(friendOfFriend,0) + 10);
+        }
+
         return answer;
     }
 }
