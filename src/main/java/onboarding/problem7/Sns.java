e7958a6ab5dbb9434d28fd54da6b17c74bf870e0
yun <yshyeon102@gmail.com>
2022-11-01 03:49:59 +0900

    feat: Problem7 - 친구 관계를 설정하는 메서드 구현
    주어진 리스트로부터  친구 관계를 설정하는 기능 구현.
    
    - setFriendShip() : 친구 목록들을 받아와 반복문을 통해 하나의 관계씩 친구 관계를 담아 간다.
    
    - makeFriendShip() : 친구A와 친구B가 서로 친구라는 사실을 알 수 있도록 friendShip map에 각각 저장한다.
    
    - enrollMembers() : 친구 관계에 있는 2명 모두 SNS의 회원이므로,  SNS에 memberSet에 추가한다.

--- src/main/java/onboarding/problem7/Sns.java
+++ src/main/java/onboarding/problem7/Sns.java
@@ -8,4 +8,31 @@ public class Sns {
     public HashSet<String> memberSet = new HashSet<>();
     public HashMap<String, HashSet<String>> friendShip = new HashMap<>();
     public HashMap<String, HashMap<String, Integer>> visitHistory = new HashMap<>();
+
+    public void setFriendShip(List<List<String>> friendships) {
+        String memberA;
+        String memberB;
+
+        for (List<String> friend : friendships) {
+            memberA = friend.get(0);
+            memberB = friend.get(1);
+            enrollMembers(List.of(memberA, memberB));
+            makeFriendShip(memberA, memberB);
+        }
+    }
+
+    private void enrollMembers(List<String> members) {
+        memberSet.addAll(members);
+    }
+
+    private void makeFriendShip(String memberA, String memberB) {
+        HashSet<String> friendShipOfA = friendShip.getOrDefault(memberA, new HashSet<>());
+        HashSet<String> friendShipOfB = friendShip.getOrDefault(memberB, new HashSet<>());
+
+        friendShipOfA.add(memberB);
+        friendShipOfB.add(memberA);
+
+        friendShip.put(memberA, friendShipOfA);
+        friendShip.put(memberB, friendShipOfB);
+    }
 }
