e599c467f420a17ace7ea1283075145a74422e32
swonny <wonny921@gmail.com>
2022-10-31 14:20:27 +0900

    feat: 중복 문자 제거 기능 구현
    - deleteDuplication() 메서드를 통해 중복되는 문자는 공백으로 대체하였다.
    - removeSpace() 메서드를 통해 공백을 제거하였다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,11 +1,37 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeSpace(char[] chars) {
+        String result = "";
+        for(int i = 0; i < chars.length; i++) {
+            if(chars[i] == ' ') {
+                continue;
+            }
+            result += Character.toString(chars[i]);
+        }
+        return result;
+    }
+
+    public static String deleteDuplication(String cryptogram, String indexes) {
+        char[] chars = cryptogram.toCharArray();
+        String tmp = "";
+        for(int i = 0; i < indexes.length(); i++) {
+            if(indexes.charAt(i) != ' ') {
+                tmp += Character.toString(indexes.charAt(i));
+                continue;
+            }
+            chars[Integer.parseInt(tmp)] = ' ';
+            tmp = "";
+        }
+        cryptogram = removeSpace(chars);
+        return cryptogram;
+    }
+
     public static String findDuplication(String cryptogram) {
         char prevChar = cryptogram.charAt(0);
-        String indexes = "";
-
+        String indexes;
         while(true) {
+            indexes = "";
             for(int i = 1; i < cryptogram.length(); i++) {
                 if(prevChar == cryptogram.charAt(i)) {
                     indexes += Integer.toString(i - 1) + " ";
@@ -16,17 +42,14 @@ public class Problem2 {
             if(indexes.length() == 0) {
                 break;
             }
+            cryptogram = deleteDuplication(cryptogram, indexes);
         }
         return cryptogram;
     }
 
     public static String solution(String cryptogram) {
-        findDuplication(cryptogram);
         String answer = "answer";
+        answer = findDuplication(cryptogram);
         return answer;
-    }
-
-    public static void main(String[] args) {
-        solution("browoanoommnaon");
     }
 }
