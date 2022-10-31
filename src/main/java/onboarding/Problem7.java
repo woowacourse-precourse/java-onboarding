f10fece2b02bd5fd42d4dbf62ea38f97ce143d5f
baksohyeon <bori12370@gmail.com>
2022-10-31 18:44:35 +0900

    refactor: rename value

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,7 +7,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<List<String>> notFriendList = new ArrayList<>();
         HashSet<String> friendList = new HashSet<>();
-        HashMap<String, Integer> suggestScoreList = new HashMap<>();
+        HashMap<String, Integer> RelatedFriendsList = new HashMap<>();
         Problem7 problem = new Problem7();
 
         // 주어진 friends 관계도에서 user와 친구인 사람, 친구가 아닌 사람들의 관계끼리 자료 분리
@@ -30,18 +30,18 @@ public class Problem7 {
             String person2 = connection.get(1);
 
             if (friendList.contains(person1)) {
-                if (!suggestScoreList.containsKey(person2)) {
-                    suggestScoreList.put(person2, 10);
+                if (!RelatedFriendsList.containsKey(person2)) {
+                    RelatedFriendsList.put(person2, 10);
                 } else {
-                    suggestScoreList.put(person2, suggestScoreList.get(person2) + 10);
+                    RelatedFriendsList.put(person2, RelatedFriendsList.get(person2) + 10);
                 }
             }
 
             if (friendList.contains(person2)) {
-                if (!suggestScoreList.containsKey(person1)) {
-                    suggestScoreList.put(person1, 10);
+                if (!RelatedFriendsList.containsKey(person1)) {
+                    RelatedFriendsList.put(person1, 10);
                 } else {
-                    suggestScoreList.put(person1, suggestScoreList.get(person1) + 10);
+                    RelatedFriendsList.put(person1, RelatedFriendsList.get(person1) + 10);
                 }
             }
         });
@@ -49,18 +49,18 @@ public class Problem7 {
         // 방문 수 만큼 점수 계산
         List<String> visitConnections = visitors.stream().filter(x -> !friendList.contains(x)).collect(Collectors.toList());
         visitConnections.forEach((visitor) -> {
-            if (!suggestScoreList.containsKey(visitor)) {
-                suggestScoreList.put(visitor, 1);
+            if (!RelatedFriendsList.containsKey(visitor)) {
+                RelatedFriendsList.put(visitor, 1);
             } else {
-                suggestScoreList.put(visitor, suggestScoreList.get(visitor) + 1);
+                RelatedFriendsList.put(visitor, RelatedFriendsList.get(visitor) + 1);
             }
         });
 
         // 점수 별로 정렬한 후 유저 이름대로 해시맵 정렬
-        HashMap<String, Integer> suggestUserSortedByScore = problem.sortbyScoreThenName(suggestScoreList);
+        HashMap<String, Integer> sortedFriendsList = problem.sortbyScoreThenName(RelatedFriendsList);
 
         // 맵에서 key값만 가져오기
-        Set<String> keySet = suggestUserSortedByScore.keySet();
+        Set<String> keySet = sortedFriendsList.keySet();
         ArrayList<String> recommendUserList = new ArrayList<>(keySet);
 
         return problem.HeadCountLimit(recommendUserList, 5);
@@ -73,13 +73,13 @@ public class Problem7 {
                 .sorted(Collections
                         .reverseOrder(Map.Entry.<String, Integer>comparingByValue())
                         .thenComparing(Map.Entry.comparingByKey()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (newValue, oldValue) -> oldValue, LinkedHashMap::new));
+                .collect(Collectors
+                        .toMap(Map.Entry::getKey, Map.Entry::getValue, (newValue, oldValue) -> oldValue, LinkedHashMap::new));
 
         return sortedList;
     }
 
-    public List<String> HeadCountLimit (List<String> list, int limit) {
+    public List<String> HeadCountLimit(List<String> list, int limit) {
         if (list.size() <= limit) {
             return list;
         } else {
