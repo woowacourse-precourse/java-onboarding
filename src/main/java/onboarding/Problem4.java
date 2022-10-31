a0ee5c93c29f708ce8ab7e13611f6dc099897c47
hi-june <wnsgur735@gmail.com>
2022-10-31 17:22:07 +0900

    feat: TreeFrog class 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
+class TreeFrog {
+    /**
+     * 개구리마다 각자의 번역 사전이 있을 수 있다.
+     * 따라서, 인스턴스를 생성해서 매소드를 호출하는 것이 좋다.
+     */
+
+    private Map<Character, Character> lowerCaseDictionary = new HashMap<>();
+    private Map<Character, Character> upperCaseDictionary = new HashMap<>();
+
+    public TreeFrog() { // frog 자체의 사전을 정의
+        for (int i = 0; i < 26; i++) {
+            lowerCaseDictionary.put((char) ('a' + i), (char) ('z' - i));
+            upperCaseDictionary.put((char) ('A' + i), (char) ('Z' - i));
+        }
+    }
+}
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
