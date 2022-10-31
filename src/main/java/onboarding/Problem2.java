e583a393301f2c8ed8a8e30b01a389935fd8a075
OhHongGi <5ghdrl@naver.com>
2022-11-01 00:54:46 +0900

    chore: Divide Function List in Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    /*
+     주어진 문자열의 중복 문자를 제거하여 반환
+     */
+    public static String removeDuplicateCharactor(String cypherText) {
+        return "";
     }
 }
