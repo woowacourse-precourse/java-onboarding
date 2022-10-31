a337ab1defba899f92cd5bd6be07fbf43e34d64a
yellowchili <yunpaper@yu.ac.kr>
2022-11-01 01:15:21 +0900

    [Problem5]Complete solution function implementation

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] moneys = {50000,10000,5000,1000,500,100,50,10,1};
+
+        for (int m : moneys) {
+            answer.add(money / m); // 필요한 금액권 개수 구하기
+            money %= m; // 남은 금액 저장
+        }
+
         return answer;
     }
 }
