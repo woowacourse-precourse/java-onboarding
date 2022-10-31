22c9b467157b2dae0e2c959455c1181d45b9bd4e
euna <christinejung10@daum.net>
2022-10-31 20:43:31 +0900

    fix(Problem7): getScore 함수 수정
    사용자와 함께 아는 친구의 수에 대한 점수 계산은 매개변수 friends에 들어있는 유저만 확인하는 것으로 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
 
@@ -40,19 +37,15 @@ public class Problem7 {
         // user와 이미 친구가 되어 있는 사람들의 목록
         HashSet<String> userFriends = friendLists.get(user);
 
-        for (String account : totalUserList) {
+        for (String oneUser : totalUserList) {
             // 본인은 제외 & user와 이미 친구가 되어 있는 사람은 추천하지 않기 때문에 제외
-            if (!account.equals(user) && !userFriends.contains(account)) {
+            if (!oneUser.equals(user) && !userFriends.contains(oneUser)) {
                 // 점수 계산 후 scoreMap에 저장
-                scoreMap.replace(account, getScore(friendLists, visitors, account));
+                scoreMap.replace(oneUser, getScore(friendLists, visitors, oneUser));
             }
         }
 
-        // 점수가 가장 높은 순으로 정렬하여 최대 5명을 뽑음
-        // 유의사항
-        // 1. 이미 user의 친구인 경우에는 추천하지 않음
-        // 2. 추천 점수가 0점인 경우 추천하지 않음
-        // 3. 추천 점수가 같은 경우는 이름순으로 오름차순 정렬
+//        answer = getAnswer(scoreMap);
 
         return answer;
     }
@@ -92,13 +85,16 @@ public class Problem7 {
         // user와 이미 친구가 되어 있는 사람들의 목록
         HashSet<String> userFriends = friendLists.get(user);
 
-        int bothFriendsNum = 0; // 사용자와 함께 아는 친구의 수
-        for (String friend : friendLists.get(user)) {
-            if (userFriends.contains(friend)) { // 사용자와 함께 아는 친구라면 bothFriendsNum + 1
-                bothFriendsNum++;
+        // friends에 들어있는 유저만 확인
+        if (friendLists.containsKey(user)) {
+            int bothFriendsNum = 0; // 사용자와 함께 아는 친구의 수
+            for (String friend : friendLists.get(user)) {
+                if (userFriends.contains(friend)) { // 사용자와 함께 아는 친구라면 bothFriendsNum + 1
+                    bothFriendsNum++;
+                }
             }
+            score += bothFriendsNum * score_bothFriends; // score + (사용자와 함께 아는 친구의 수) * 10
         }
-        score += bothFriendsNum * score_bothFriends; // score + (사용자와 함께 아는 친구의 수) * 10
 
         for (String visitor : visitors) {
             if (user.equals(visitor)) { // 사용자의 타임 라인에 방문한 횟수만큼 score + 1
