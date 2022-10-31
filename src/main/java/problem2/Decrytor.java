1a267f951e069d3e06c75a502f5dae9bcd625b8d
twoosky <lhjae3@naver.com>
2022-11-01 01:01:31 +0900

    feat(problem2): 연속하는 중복 문자 검사 로직 구현

--- src/main/java/problem2/Decrytor.java
+++ src/main/java/problem2/Decrytor.java
@@ -6,4 +6,28 @@ public class Decrytor {
     public Decrytor(String cryptogram) {
         this.cryptogram = cryptogram;
     }
+
+    private boolean isUnDuplicate(int index) {
+        return isUnDuplicatePreviousChar(index) && isUnDuplicateNextChar(index);
+    }
+
+    private boolean isUnDuplicatePreviousChar(int index) {
+        if(index == 0) {
+            return true;
+        }
+        char previousChar = cryptogram.charAt(index-1);
+        char currentChar = cryptogram.charAt(index);
+
+        return previousChar != currentChar;
+    }
+
+    private boolean isUnDuplicateNextChar(int index) {
+        if(index == cryptogram.length()-1) {
+            return true;
+        }
+        char currentChar = cryptogram.charAt(index);
+        char nextChar = cryptogram.charAt(index+1);
+
+        return currentChar != nextChar;
+    }
 }
