5bdbd0ca7f684da27995f78d7338b2cbd10ba023
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 23:41:03 +0900

    feat: P4 기능 2 소문자일때와 대문자일때 청개구리 문자로 변환해주는 함수 작성 (기능 주석 수정)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,16 +2,27 @@ package onboarding;
 
 /**
  * 기능 1: 알파벳인지 아닌지 판별하는 함수 작성
- * 기능 2: 소문자인지 대문자인지 확인하여 변환해주는 함수 작성
+ * 기능 2: 소문자일때와 대문자일때 청개구리 문자로 변환해주는 함수 작성
  * 기능 3: 문자열을 순환하면서 변환해주는 기능 작성
  */
 public class Problem4 {
+
+    public static final int UPPER_PIVOT = 'A' + 'Z';
+    public static final int LOWER_PIVOT = 'a' + 'z';
 
     public static boolean isAlphabet(char each) {
         if ((each >= 'a' && each <= 'z') || (each >= 'A' && each <= 'Z')) {
             return true;
         }
         return false;
+    }
+
+    public static char convertUpperCase(char letter) {
+       return (char) (UPPER_PIVOT - letter);
+    }
+
+    public static char convertLowerCase(char letter) {
+        return (char) (LOWER_PIVOT - letter);
     }
 
 
