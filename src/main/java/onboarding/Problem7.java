6f3832183c5663627c926c572b7a21387871051c
박시균\psk98 <psg980331@naver.com>
2022-10-31 21:25:16 +0900

    docs : 문제6 기능 구현 목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,12 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 1. user의 친구들 구하기
+ * 2. 추천 점수 계산
+ * 3. 방문자 점수 계산
+ * 4. 점수 높은순으로 정렬 및 5명 반환
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
