626edd508fd4667c13d7ef144e0c7b4071d5be6d
dener8 <8dener@gmail.com>
2022-10-31 11:44:28 +0900

    feat: deque 만드는 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
+    static Deque<String> createDeque() {
+        Deque<String> deque = new ArrayDeque<>();
+        return deque;
+    }
+
+
+
 }
+
