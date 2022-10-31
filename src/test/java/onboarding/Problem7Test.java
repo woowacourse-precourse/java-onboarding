39cb81015ccff177b06c6bee9c91d7e27a3fc057
YuHayeon <youha0930@gmail.com>
2022-10-31 15:03:05 +0900

    test(Problem7): 점수 구하기, 올바른 케이스 테스트

--- src/test/java/onboarding/Problem7Test.java
+++ src/test/java/onboarding/Problem7Test.java
@@ -3,6 +3,7 @@ package onboarding;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -111,4 +112,87 @@ class Problem7Test {
 		assertThrows(IllegalArgumentException.class, () ->
 			Problem7.InputValidator.isVisitorsInput(finalVisitors2));
 	}
+
+	@Test
+	void 함께_아는_친구_점수_구하기() {
+		String user = "donut";
+		HashMap<String, List<String>> relations = new HashMap<>();
+		relations.put("donut", List.of("shakevan", "anna"));
+		relations.put("shakevan", List.of("lily", "anna"));
+		relations.put("anna", List.of("lily", "tom"));
+
+		HashMap<String, Integer> result = new HashMap<>();
+		result.put("lily", 20);
+		result.put("anna", 10);
+		result.put("tom", 10);
+		assertThat(Problem7.getFriendOfFriendScore(relations, user)).isEqualTo(result);
+	}
+
+	@Test
+	void 타임_라인_방문_점수_구하기() {
+		String user = "donut";
+		List<String> relations = List.of("lala", "meme", "anna", "lala");
+
+		HashMap<String, Integer> result = new HashMap<>();
+		result.put("lala", 2);
+		result.put("meme", 1);
+		result.put("anna", 1);
+		assertThat(Problem7.getVisitorsScore(relations)).isEqualTo(result);
+	}
+
+	@Test
+	void 올바른_입출력() {
+		String user = "tom";
+		List<List<String>> friends = List.of(
+			List.of("tom", "amy"),
+			List.of("jenny", "amy"),
+			List.of("donut", "amy"),
+			List.of("donut", "shakevan"),
+			List.of("shakevan", "tom"),
+			List.of("shakevan", "bedi")
+		);
+		List<String> visitors = List.of("donut", "mrko", "peter", "lily");
+		List<String> result = List.of("donut", "bedi", "jenny", "lily", "mrko");
+		assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+
+		user = "hello";
+		friends = List.of(
+			List.of("amy", "jun"),
+			List.of("amy", "lola"),
+			List.of("bedi", "shakevan"),
+			List.of("bedi", "anne"),
+			List.of("bedi", "sam"),
+			List.of("anne", "mrko")
+		);
+		visitors = List.of("donut", "anne", "mrko", "sam", "sam");
+		result = List.of("sam", "anne", "donut", "mrko");
+		assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+
+		user = "hello";
+		friends = List.of(
+			List.of("andole", "jun"),
+			List.of("andole", "bedi"),
+			List.of("tom", "shakevan"),
+			List.of("tom", "anne"),
+			List.of("anne", "mrko")
+		);
+		visitors = List.of();
+		result = List.of();
+		assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+
+		user = "hello";
+		friends = List.of(
+			List.of("amy", "jun"),
+			List.of("amy", "bedi"),
+			List.of("lily", "shakevan"),
+			List.of("lily", "anne"),
+			List.of("lily", "sam"),
+			List.of("anne", "mrko")
+		);
+		visitors = List.of("donut", "anne", "mrko", "amy");
+		result = List.of("amy", "anne", "donut", "mrko");
+		assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+
+	}
+
 }
