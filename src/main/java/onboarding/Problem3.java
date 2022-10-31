e99992db2f5f2bad017840c84ddace615052158c
jooys130 <wndustn130@gmail.com>
2022-10-31 13:36:30 +0900

    feat: <Problem3> 3, 6, 9의 개수만큼 손뼉을 칠 수 있다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,25 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = clap(number);
         return answer;
+    }
+    private static int clap(int number){
+        int clap = 0;
+        for(int i = 1; i < number + 1; i++){
+            clap += count(i);
+        }
+        return clap;
+    }
+    private static int count(int num){
+        int cnt = 0;
+        String str = String.valueOf(num);
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if (c == '3' || c == '6' || c == '9'){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
