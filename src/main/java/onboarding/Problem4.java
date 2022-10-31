4158643377b165c71dee07484ac17045f572d765
PPakSang <sanghyun-dev@naver.com>
2022-10-31 18:05:20 +0900

    Feat: Problem4 내부 연산 로직 추가 및 로직 구현 완료
    reverseEnAlphabetic(char): 알파벳에 대해서만 변환 로직 수행 후 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            sb.append(reverseEnAlphabet(c));
+        }
+        return sb.toString();
     }
 
     private static boolean isEnAlphabetic(char c) {
@@ -10,5 +16,16 @@ public class Problem4 {
             return false;
         }
         return true;
+    }
+
+    private static char reverseEnAlphabet(char c) {
+       if (!isEnAlphabetic(c)) {
+           return c;
+       }
+
+       if (Character.isLowerCase(c)) {
+           return (char)('a'+'z' - c);
+       }
+       return (char)('A'+'Z' - c);
     }
 }
