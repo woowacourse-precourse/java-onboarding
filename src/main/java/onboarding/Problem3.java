08312a0b52f16d5e81570dbf5bd236191ceb90ed
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 20:46:28 +0900

    Refactor(Problem3) : Game369 클래스 삭제

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,38 +6,6 @@ import java.util.regex.Pattern;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        Game369 game = new Game369(number);
-        answer = game.getCount();
         return answer;
-    }
-}
-
-class Game369 {
-    private int number;
-    private int count;
-
-    Game369(int number) {
-        this.number =  number;
-        count = 0;
-        calc();
-    }
-
-    void calc() {
-        String numStr;
-        Pattern pattern;
-        Matcher match;
-        for(int i = 1; i < number+1; i++) {
-            numStr = Integer.toString(i);
-            pattern = Pattern.compile("[369]");
-            match = pattern.matcher(numStr);
-            while(match.find()) {
-                count = count + 1;
-            }
-
-        }
-    }
-
-    int getCount() {
-        return count;
     }
 }
