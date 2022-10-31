b8b47e1647047902b265af2ca6df63e96b9dd3b7
shoeone96 <dlwnddnjs96@naver.com>
2022-10-31 15:58:16 +0900

    2번 문제 풀이

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        // 문자열 추가 삭제가 자유로운 StringBuilder로 변환하여 작성
+        StringBuilder bWords = new StringBuilder(cryptogram);
+        for (int i = 0; i < bWords.length() - 1; i++) {
+            while (bWords.charAt(i) == bWords.charAt(i + 1)) {
+                if (bWords.charAt(i) == bWords.charAt(i + 1)) {
+                    bWords.delete(i, i + 2);
+                    i--;
+                }
+                if (bWords.length() == 0) {
+                    break;
+                }
+            }
+        }
+        String answer = bWords.toString();
         return answer;
     }
 }
