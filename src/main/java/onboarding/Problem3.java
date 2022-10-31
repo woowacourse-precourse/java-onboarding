39305d03b292474ad1d01f7ea4efce89e5c79f8a
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-01 03:35:42 +0900

    Feat(Problem3): 풀이 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // dp로 풀 수도 있지 않을까?
+        for (int i = 3; i <= number; i++) {
+            String num = String.valueOf(i);
+            for (int j = 0; j < num.length(); j++) {
+                if (num.charAt(j) == '3' || num.charAt(j) == '6' || num.charAt(j) == '9') {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
