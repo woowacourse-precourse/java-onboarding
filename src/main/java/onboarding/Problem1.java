60ab8f1dac1769dc2ecdec8a848d04b670557671
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:27:02 +0900

    [problem1]포비 와 크롱 값 비교후 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,14 +6,12 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
 
-        int pobimax=0;
-        int crongmax=0;
-
-
-
-        pobimax = Math.max(Maxnum(pobi.get(0)), Maxnum(pobi.get(1)));
-        crongmax = Math.max(Maxnum(crong.get(0)), Maxnum(crong.get(1)));
-
+        if(pobimax > crongmax)
+            return 1;
+        else if (pobimax == crongmax)
+            return 0;
+        else
+            return 2;
 
     }
 
