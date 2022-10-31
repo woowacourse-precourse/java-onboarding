e943fc83d145c36be4bbc1719b2289e4c1b8bb03
Yejin Shin <shinyejin0212@gmail.com>
2022-10-31 17:40:08 +0900

    refactor(problem6) : code refactoring

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,39 +4,57 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        answer = new ArrayList<>();
-        List<String> split = new ArrayList<>();
-        Set<String> freq = new HashSet<>();
 
+    //2문자로 쪼개기
+    public static List<String> splitTwoWord (List<List<String>> forms){
+        List<String> arr = new ArrayList<>();
 
-        //2문자로 쪼개기
         for (int i =0; i < forms.size(); i++){
             String nickname = forms.get(i).get(1);
             for(int j = 0; j < nickname.length()-1; j++){
-                split.add(nickname.substring(j,j+2));
+                arr.add(nickname.substring(j,j+2));
             }
         }
-        // 중복 값 추출
+        return arr;
+    }
+    // 중복 값 추출
+    public static List<String> filterFreq(List<String> split){
+        Set<String> freq = new HashSet<>();
+        // 1번을 초과하여 (2번 이상) 중복해서 나타나는 크기가 2인 단어 추출
         freq = split.stream().filter(i -> Collections.frequency(split, i) > 1).collect(Collectors.toSet());
-        System.out.println(freq);
-        List<String> target = new ArrayList<>(freq);
+        
+        // set을 List화
+        List<String> result = new ArrayList<>(freq);
+        return result;
+    }
 
-        // 중복값을 포함한 이메일 추출
-        Iterator iter = freq.iterator();
+    // 중복값을 포함하고 있는 이메일을 추출하는 메소드
+    public static  List<String> filterEmail(List<List<String>> forms, List<String> target){
+        List<String> result = new ArrayList<>();
+
         for (int i =0; i < forms.size(); i++){
-            for (String t : target){
+            for (String t : target) {
                 String nickname = forms.get(i).get(1);
                 if(nickname.contains(t)){
-                    answer.add(forms.get(i).get(0));
+                    result.add(forms.get(i).get(0));
                 }
             }
         }
+        return result;
+    }
+
+    public static List<String> solution(List<List<String>> forms) {
+        List<String> answer = List.of("answer");
+        answer = new ArrayList<>();
+
+        // nickname을 2문자로 split 한 후 중복인 값 추출
+        List<String> target = new ArrayList<>(filterFreq(splitTwoWord(forms)));
+        // 중복값을 포함한 이메일 추출
+        answer = filterEmail(forms, target);
 
         // answer(이메일) 정렬
         Collections.sort(answer);
-            return answer;
+        return answer;
     }
 }
 
