45734fdf06f2763afd4a68c8b2b3d6538537d6b4
jinny-l <jinny.l.dev@gmail.com>
2022-10-31 21:39:32 +0900

    [feat] (problem.7) 최종 추천 친구 리스트에서 TOP5만 반환하는 메소드 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,166 +1,197 @@
 package onboarding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        Map<String, Integer> recommendedFriendsList = descendingSort(user, friends, visitors);
-        Map<String, Integer> top5 = recommendedFriendsList;
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		return getTop5(user, friends, visitors);
+	}
 
-        // TODO: TOP5 정렬
+	// 최종 추천 친구 리스트에서 TOP5만 반환하는 메소드
+	private static List<String> getTop5(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> sortedList = descendingSort(user, friends, visitors);
+		List<String> top5 = new ArrayList<>();
 
-        return new ArrayList<>(top5.keySet());
-    }
+		for (int i = 0; i < 5; i++) {
+			if (sortedList.size() <= i) {
+				break;
+			}
 
-    // TODO: 최종 추천 친구 리스트에서 TOP5만 반환하는 메소드
-    // 예외처리: 점수가 0일 경우 반환하지 않음
-    private static List<String> getTop5(Map<String, Integer> recommendedFriendsList) {
-        List<String> top5 = new ArrayList<>();
-        return top5;
-    }
+			top5.add(sortedList.get(i));
 
+		}
 
-    // 최종 추천 친구 리스트에서 점수 기준으로 내림차순 정렬하고, 점수가 같을 경우 이름순으로 정렬하여 TOP5만 반환하는 메소드
-    private static Map<String, Integer> descendingSort(String user, List<List<String>> friends, List<String> visitors) {
-        Map<String,Integer> recommendedFriendsList = getRecommendedFriendsList(user, friends, visitors);
+		return top5;
+	}
 
-        List<String> result = new ArrayList<>(recommendedFriendsList.keySet());
-        result.sort((o1, o2) ->  recommendedFriendsList.get(o2).compareTo(recommendedFriendsList.get(o1)));
+	// 최종 추천 친구 리스트에서 점수 기준으로 내림차순 정렬하고, 점수가 같을 경우 이름순으로 정렬하는 메소드
+	// 예외 처리: 점수가 0일 경우 반환하지 않음
+	private static List<String> descendingSort(String user, List<List<String>> friends, List<String> visitors) {
+		Map<String, Integer> recommendedFriendsList = getRecommendedFriendsList(user, friends, visitors);
 
-        //TODO: 점수가 같은 경우 이름순으로 정렬
+		Set<Integer> uniqueScores = new TreeSet<>(recommendedFriendsList.values()).descendingSet();
 
-        return recommendedFriendsList;
-    }
+		List<String> sortedList = new ArrayList<>();
 
+		for (Integer score : uniqueScores) {
+			List<String> friendsOfSameScore = new ArrayList<>();
 
-    // friends 점수와 visitors 점수를 합하여 반환하는 메소드
-    private static Map<String, Integer> getRecommendedFriendsList(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> recommendedFriends = recommendedFriendsOf(user, friends, visitors);
+			if (score == 0) {
+				continue;
+			}
 
-        Map<String, Integer> friendsScore = getFriendsScore(user, friends);
-        Map<String, Integer> visitorsScore = getVisitorsScore(visitors);
-        Map<String, Integer> recommendedFriendsScore = new HashMap<>(recommendedFriends.size());
+			for (String friend : recommendedFriendsList.keySet()) {
+				Integer friendScore = recommendedFriendsList.get(friend);
+				if (Objects.equals(friendScore, score)) {
+					friendsOfSameScore.add(friend);
+				}
+			}
 
-        int scoreSum;
-        int friendScore;
-        int visitorScore;
+			Collections.sort(friendsOfSameScore);
+			sortedList.addAll(friendsOfSameScore);
+		}
 
-        for (String recommendedFriend : recommendedFriends) {
+		return sortedList;
+	}
 
-            if (friendsScore.get(recommendedFriend) == null) {
-                friendScore = 0;
-            } else {
-                friendScore = friendsScore.get(recommendedFriend);
-            }
+	// friends 점수와 visitors 점수를 합하여 반환하는 메소드
+	private static Map<String, Integer> getRecommendedFriendsList(String user, List<List<String>> friends,
+		List<String> visitors) {
+		List<String> recommendedFriends = recommendedFriendsOf(user, friends, visitors);
 
-            if (visitorsScore.get(recommendedFriend) == null) {
-                visitorScore = 0;
-            } else {
-                visitorScore = visitorsScore.get(recommendedFriend);
-            }
+		Map<String, Integer> friendsScore = getFriendsScore(user, friends);
+		Map<String, Integer> visitorsScore = getVisitorsScore(visitors);
+		Map<String, Integer> recommendedFriendsScore = new HashMap<>(recommendedFriends.size());
 
-            scoreSum = friendScore + visitorScore;
-            recommendedFriendsScore.put(recommendedFriend, scoreSum);
-        }
+		int scoreSum;
+		int friendScore;
+		int visitorScore;
 
-        return recommendedFriendsScore;
-    }
+		for (String recommendedFriend : recommendedFriends) {
 
+			if (friendsScore.get(recommendedFriend) == null) {
+				friendScore = 0;
+			} else {
+				friendScore = friendsScore.get(recommendedFriend);
+			}
 
-    // friends of firends의 점수를 반환하는 메소드
-    private static Map<String, Integer> getFriendsScore(String user, List<List<String>> friends) {
-        List<String> friendsOfFriends = friendsOfFriends(friends);
-        List<String> uniqueFriendsOfFriends = uniqueFriendsOfFriends(user, friends);
+			if (visitorsScore.get(recommendedFriend) == null) {
+				visitorScore = 0;
+			} else {
+				visitorScore = visitorsScore.get(recommendedFriend);
+			}
 
-        Map<String,Integer> friendsScore = new HashMap<>();
+			scoreSum = friendScore + visitorScore;
+			recommendedFriendsScore.put(recommendedFriend, scoreSum);
+		}
 
-        int count;
-        int score;
+		return recommendedFriendsScore;
+	}
 
-        for (String uniqueFriendOfFriends : uniqueFriendsOfFriends) {
-            count = 0;
-            for (String friendOfFriends : friendsOfFriends) {
-                if (uniqueFriendOfFriends.equals(friendOfFriends)) {
-                    count++;
-                }
-            }
-            score = count * 10;
-            friendsScore.put(uniqueFriendOfFriends, score);
-        }
+	// 추천 친구 리스트를 반환하는 메소드
+	private static List<String> recommendedFriendsOf(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> uniqueFriendsOfFriends = uniqueFriendsOfFriends(user, friends);
+		List<String> uniqueVisitors = uniqueVisitorsOf(visitors);
+		List<String> friendsOfUser = friendsOfUser(friends);
 
-        return friendsScore;
-    }
+		Set<String> recommendedFriends = new HashSet<>();
+		recommendedFriends.addAll(uniqueFriendsOfFriends);
+		recommendedFriends.addAll(uniqueVisitors);
 
+		recommendedFriends.removeIf(friendsOfUser::contains);
 
-    // 추천 친구 리스트를 반환하는 메소드
-    private static List<String> recommendedFriendsOf(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> uniqueFriendsOfFriends= uniqueFriendsOfFriends(user, friends);
-        List<String> uniqueVisitors = uniqueVisitorsOf(visitors);
-        List<String> userFriends = userFriends(friends);
+		return new ArrayList<>(recommendedFriends);
+	}
 
-        Set<String> recommendedFriends = new HashSet<>(uniqueFriendsOfFriends);
-        recommendedFriends.addAll(uniqueVisitors);
+	// friends of firends의 점수를 반환하는 메소드
+	private static Map<String, Integer> getFriendsScore(String user, List<List<String>> friends) {
+		List<String> friendsOfFriends = friendsOfFriends(friends);
+		List<String> uniqueFriendsOfFriends = uniqueFriendsOfFriends(user, friends);
 
-        recommendedFriends.removeIf(userFriends::contains);
+		Map<String, Integer> friendsScore = new HashMap<>();
 
-        return new ArrayList<>(recommendedFriends);
-    }
+		int count;
+		int score;
 
-    // user와 friend인 리스트를 반환하는 메소드
-    private static List<String> userFriends(List<List<String>> friends) {
-        Set<String> friendsOfUser = new HashSet<>();
+		for (String uniqueFriendOfFriends : uniqueFriendsOfFriends) {
+			count = 0;
 
-        for (List<String> friend : friends) {
-            friendsOfUser.add(friend.get(0));
-        }
+			for (String friendOfFriends : friendsOfFriends) {
+				if (uniqueFriendOfFriends.equals(friendOfFriends)) {
+					count++;
+				}
+			}
 
-        return new ArrayList<>(friendsOfUser);
-    }
+			score = count * 10;
+			friendsScore.put(uniqueFriendOfFriends, score);
+		}
 
+		return friendsScore;
+	}
 
-    // unique friends of friends 리스트를 반환하는 메소드
-    private static List<String> uniqueFriendsOfFriends(String user, List<List<String>> friends) {
-        List<String> friendsOfFriends = friendsOfFriends(friends);
+	// friends of user 리스트를 반환하는 메소드
+	private static List<String> friendsOfUser(List<List<String>> friends) {
+		Set<String> friendsOfUser = new HashSet<>();
 
-        Set<String> set = new HashSet<>(friendsOfFriends);
-        set.remove(user);
+		for (List<String> friend : friends) {
+			friendsOfUser.add(friend.get(0));
+		}
 
-        return new ArrayList<>(set);
-    }
+		return new ArrayList<>(friendsOfUser);
+	}
 
-    // fiends of friends 리스트를 반환하는 메소드
-    private static List<String> friendsOfFriends(List<List<String>> friends) {
-        List<String> friendsOfFriends = new ArrayList<>();
+	// unique friends of friends 리스트를 반환하는 메소드
+	private static List<String> uniqueFriendsOfFriends(String user, List<List<String>> friends) {
+		List<String> friendsOfFriends = friendsOfFriends(friends);
 
-        for (List<String> friend: friends) {
-            friendsOfFriends.add(friend.get(1));
-        }
-        return friendsOfFriends;
-    }
+		Set<String> set = new HashSet<>(friendsOfFriends);
+		set.remove(user);
 
+		return new ArrayList<>(set);
+	}
 
-    // visitor의 방문 횟수를 반환하는 메소드
-    private static Map<String, Integer> getVisitorsScore(List<String> visitors) {
-        List<String> uniqueVisitors = uniqueVisitorsOf(visitors);
-        Map<String, Integer> visitCount = new HashMap<>();
+	// fiends of friends 리스트를 반환하는 메소드
+	private static List<String> friendsOfFriends(List<List<String>> friends) {
+		List<String> friendsOfFriends = new ArrayList<>();
 
-        for (String uniqueVisitor : uniqueVisitors) {
-            int count = 0;
-            for (String visitor : visitors) {
-                if (uniqueVisitor.equals(visitor)) {
-                    count++;
-                }
-            }
-            visitCount.put(uniqueVisitor, count);
-        }
+		for (List<String> friend : friends) {
+			friendsOfFriends.add(friend.get(1));
+		}
 
-        return visitCount;
-    }
+		return friendsOfFriends;
+	}
 
+	// visitor의 방문 횟수를 반환하는 메소드
+	private static Map<String, Integer> getVisitorsScore(List<String> visitors) {
+		List<String> uniqueVisitors = uniqueVisitorsOf(visitors);
+		Map<String, Integer> visitCount = new HashMap<>();
 
-    // unique visitor 리스트를 반환하는 메소드
-    private static List<String> uniqueVisitorsOf(List<String> visitors) {
-        Set<String> set = new HashSet<>(visitors);
-        return new ArrayList<>(set);
-    }
+		for (String uniqueVisitor : uniqueVisitors) {
+			int count = 0;
+
+			for (String visitor : visitors) {
+				if (uniqueVisitor.equals(visitor)) {
+					count++;
+				}
+			}
+
+			visitCount.put(uniqueVisitor, count);
+		}
+
+		return visitCount;
+	}
+
+	// unique visitor 리스트를 반환하는 메소드
+	private static List<String> uniqueVisitorsOf(List<String> visitors) {
+		Set<String> set = new HashSet<>(visitors);
+		return new ArrayList<>(set);
+	}
 }
