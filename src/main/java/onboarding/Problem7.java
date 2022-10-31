6fb0bf9389ac6801aa306aac2704b8904d00aa88
farmJun <junpreme1205@gmail.com>
2022-10-31 21:04:55 +0900

    Problem7. 두 사람 중 user와 친구인 다른 사람의 이름을 반환하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,12 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    //두 사람 중 user와 친구인 다름 사람의 이름을 반환하는 함수
+    public static String findAnother(List<String> twoPeople, String user) {
+        if (twoPeople.get(0).equals(user))
+            return twoPeople.get(1);
+        else
+            return twoPeople.get(0);
+    }
 }
