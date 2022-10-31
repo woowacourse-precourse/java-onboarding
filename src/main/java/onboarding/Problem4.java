604401b3e60551924cc2a0ea1fdd21306b792282
gyumin159 <gyumin159@DESKTOP-6CKHGBO>
2022-10-31 18:25:01 +0900

    한 문자씩 반복하며 인덱스를 구해 청개구리식 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,12 @@ public class Problem4 {
     	String answer = "";
     	String temp = word.toLowerCase(); // 우선 모두 소문자로 변경
     	String alpha = "abcdefghijklmnopqrstuvwxyz"; // 우영우는 거꾸로 해도 우영우
-    	
+    	for (int i=0;i<temp.length();i++) {
+    		char c = temp.charAt(i);
+    		int idx = temp.indexOf(c);
+    		answer = answer + alpha.charAt((alpha.length() - 1) - idx); // 해당 알파벳의 인덱스를 이용해 변환
+    		
+    	}
     	
         return answer;
     }
