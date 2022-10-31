c9c87d4cd48b9ea3eb979351b0c3202226a07fa1
miffyKing <whqudghk28@gmail.com>
2022-10-31 21:17:15 +0900

    feat: Change code by making my onw Pair Class

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,87 +1,88 @@
 package onboarding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class Problem7 {
-    static Set<String> friendSet = new HashSet<>();
-    static Map<String, Integer> scoreBoard = new HashMap<>();
-    static Map<String, List<String>> friendMap = new HashMap<>();
-    static List<String> userSelectedList;
 
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        makeScoreBoard(friends);
-        makeFriendSet(friends);
-        fillScoreBoeard(user, visitors);
-        removeNameFromScoreBoard(user);
-        List<String> answer = sortKeys();
-        return answer;
-    }
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+            List.of("donut", "andole"),
+            List.of("donut", "jun"),
+            List.of("donut", "mrko"),
+            List.of("shakevan", "andole"),
+            List.of("shakevan", "jun"),
+            List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        List<Pair> pairList = new ArrayList<>();
+        HashSet<String> userFriendList = new HashSet<>();
+        HashSet<Pair<String, Integer>> allNameList = new HashSet<>();
 
-    public static void makeScoreBoard(List<List<String>> friends) {
-        for (List<String> friend : friends) {
-            friendSet.add(friend.get(0));
-            friendSet.add(friend.get(1));
+        for (String iter : visitors) {        //5. 찐따들 전체이름set에 추가.
+            if (!allNameList.contains(iter)) {
+                allNameList.add(new Pair(iter,1));    //폭발 -> SET이라 바로 FInd가 안됨.
+            }
+
+
+
         }
-
-        for (String s : friendSet) {
-            scoreBoard.put(s, 0);
-        }
-    }
-
-    public static void makeFriendSet(List<List<String>> friends) {
-        for (String s : friendSet) {
-            List<String> nameList = new ArrayList<>();
-            for (List<String> friend : friends) {
-                if (s.equals(friend.get(0))) {
-                    nameList.add(friend.get(1));
-                } else if (s.equals(friend.get(1))) {
-                    nameList.add(friend.get(0));
+        for (List<String> iter : friends) {
+            pairList.add(new Pair(iter.get(0), iter.get(1)));
+            pairList.add(new Pair(iter.get(1), iter.get(0)));       //1. 리스트에 순서 상관없이 다 때려박음.
+            if (user.equals(iter.get(0)) ||user.equals(iter.get(1))) {  //3. 마르코가 들어간 pair가 나오면 마르코 친구 set에 넣는다.
+                if (user.equals(iter.get(0))) {
+                    userFriendList.add(iter.get(1));
+                }
+                if (user.equals(iter.get(1))) {
+                    userFriendList.add(iter.get(0));
                 }
             }
-            friendMap.put(s, nameList);
-        }
-    }
 
-    public static void fillScoreBoeard(String user, List<String> visitors) {
-        userSelectedList = friendMap.get(user);
-        for (String s : userSelectedList) {
-            List<String> selectedList = friendMap.get(s);
-            for (String str : selectedList) {
-                if (str.equals(user)) {
-                    continue;
-                }
-                int num10 = scoreBoard.get(str) + 10;
-                scoreBoard.put(str, num10);
+            if (!allNameList.contains(iter.get(0))) {           //2. 이름들로 구성된 이름 전부 든 set 만들기 (나중에 찐따들 추가), allNameList
+                allNameList.add(new Pair(iter.get(0), 0));
+            }
+            if (!allNameList.contains(iter.get(1))) {
+                allNameList.add(new Pair(iter.get(1), 0));
             }
         }
-
-        for (String s : visitors) {
-            if (!scoreBoard.containsKey(s)) {
-                scoreBoard.put(s, 1);
-            } else {
-                int num1 = scoreBoard.get(s) + 1;
-                scoreBoard.put(s, num1);
+//    System.out.println(pairList.size());
+//    System.out.println(pairList.get(0).getX());
+        pairList.sort(new Comparator<>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                return 0;
             }
+        });
+
+
+        for (Pair iter : pairList) {
+            System.out.println(iter.getX() + " " + iter.getY());
         }
+
+    }
+}
+
+class Pair<String, T> {
+
+    private String x;
+    private T y;
+
+    public Pair(String x, T y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static void removeNameFromScoreBoard(String user) {
-        scoreBoard.remove(user);
-        for (String s : userSelectedList) {
-            scoreBoard.remove(s);
-        }
+
+    public String getX() {
+        return x;
     }
 
-    public static List<String> sortKeys() {
-        List<String> keyList = new ArrayList<>(scoreBoard.keySet());
-        keyList.sort(((o1, o2) -> {
-            int res = scoreBoard.get(o2) - scoreBoard.get(o1);
-            if (res == 0) {
-                return o1.compareTo(o2);
-            }
-            return res;
-        }));
-        return new ArrayList<>(scoreBoard.keySet());
+    public T getY() {
+        return y;
     }
-
 }
