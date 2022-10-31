3cae41751822d01e19e02c82f49e2825d4d0883d
wonseok <tkddls23@konkuk.ac.kr>
2022-10-31 21:03:55 +0900

    feat: 3-1 숫자에 해당하는 박수횟수 return 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = countNumberClap(number);
         return answer;
+    }
+
+    private static int countNumberClap(int number) {
+        int count = 0;
+        String numString = Integer.toString(number);
+
+        for(int i = 0; i < numString.length(); i++) {
+            if(numString.charAt(i) == '3' || numString.charAt(i) == '6' || numString.charAt(i) == '9')
+                count += 1;
+        }
+        return count;
     }
 }
