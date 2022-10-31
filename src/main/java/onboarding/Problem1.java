824eaaeeaa38712cce11acd75501e50973e5d7ce
es <mascoeskim@gmail.com>
2022-11-01 08:00:37 +0900

    Problem1/4.리팩터링2

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,8 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         pobi = new ArrayList<>(pobi);
         crong = new ArrayList<>(crong);
-//        /수정가능을 위한 재선언/
-        if (pobi.get(1)-pobi.get(0)!=1 || crong.get(1)-crong.get(0)!=1){
+
+        if (pobi.get(1) - pobi.get(0) !=1 || crong.get(1) -crong.get(0) != 1){
             return -1;
         }
         int answer;
@@ -20,43 +20,43 @@ class Problem1 {
         int mulLeftCrong = 1;
         int mulRightCrong = 1;
         do{
-            mulLeftPobi *= pobi.get(0)%10;
-            sumLeftPobi += pobi.get(0)%10;
-            pobi.set(0,pobi.get(0)/10);
-        }while (pobi.get(0)>0);
-// Pobi의 왼쪽페이지의 합 및 곱을 계산
+            mulLeftPobi *= pobi.get(0) % 10;
+            sumLeftPobi += pobi.get(0) % 10;
+            pobi.set(0,pobi.get(0) / 10);
+        }while (pobi.get(0) > 0);
+
         do{
-            mulRightPobi *= pobi.get(1)%10;
-            sumRightPobi += pobi.get(1)%10;
-            pobi.set(1,pobi.get(1)/10);
-        }while (pobi.get(1)>0);
-// Pobi의 오른쪽페이지의 합 및 곱을 계산
+            mulRightPobi *= pobi.get(1) % 10;
+            sumRightPobi += pobi.get(1) % 10;
+            pobi.set(1, pobi.get(1) / 10);
+        }while (pobi.get(1) > 0);
+
         do{
-            mulLeftCrong *= crong.get(0)%10;
-            sumLeftCrong += crong.get(0)%10;
-            crong.set(0,crong.get(0)/10);
-        }while (crong.get(0)>0);
-// Crong의 왼쪽페이지의 합 및 곱을 계산
+            mulLeftCrong *= crong.get(0) % 10;
+            sumLeftCrong += crong.get(0) % 10;
+            crong.set(0, crong.get(0) / 10);
+        }while (crong.get(0) > 0);
+
         do{
-            mulRightCrong *= crong.get(1)%10;
-            sumRightCrong += crong.get(1)%10;
-            crong.set(1,crong.get(1)/10);
-        }while (crong.get(1)>0);
-// Crong의 오른쪽페이지의 합 및 곱을 계산
+            mulRightCrong *= crong.get(1) % 10;
+            sumRightCrong += crong.get(1) % 10;
+            crong.set(1,crong.get(1) / 10);
+        }while (crong.get(1) > 0);
+
         int pobiScore = sumLeftPobi;
         if(sumRightPobi > pobiScore) pobiScore = sumRightPobi;
         if(mulLeftPobi > pobiScore) pobiScore = mulLeftPobi;
         if(mulRightPobi > pobiScore) pobiScore = mulRightPobi;
-// Pobi의 모든 값 중 최대값을 pobiScore에 점수로 할당
+
         int crongScore = sumLeftCrong;
         if(sumRightCrong > crongScore) crongScore = sumRightCrong;
         if(mulLeftCrong > crongScore) crongScore = mulLeftCrong;
         if(mulRightCrong > crongScore) crongScore = mulRightCrong;
-// Crong의 모든 값 중 최대값을 crongScore에 점수로 할당
+
         if(pobiScore>crongScore){answer = 1;}
         else if(pobiScore<crongScore){answer = 2;}
         else {answer = 0;}
-// Pobi의 점수가 더 높으면 1,Crong의 점수가 더 높으면2, 같은값일땐 0을 answer에 할당
+
         return answer;
     }
 }
