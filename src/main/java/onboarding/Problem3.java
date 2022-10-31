2cc9bb20e0dafaff0e6ab03e9217abf94e235811
hwi215 <buc06137@naver.com>
2022-10-31 19:56:24 +0900

    feat: Problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    public static int solution(int number) { // 3 6 9 Game
+        // 박수 치는 개수
+        int count = 0;
+
+        for(int i = 1; i < number+1; i++){
+            // 1부터 number까지
+            int cur_num = i;
+
+            while(cur_num != 0){
+                int n = cur_num % 10;
+
+                // 일의 자리 처리
+                if(n == 3 || n == 6 || n == 9){
+                    count++;
+                }
+
+                // 십의 자리 이상 처리
+                cur_num /= 10;
+            }
+        }
+ 
+        return count;
     }
+
 }
