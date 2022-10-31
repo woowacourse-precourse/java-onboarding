3088fd68d06fa422e87c7d6b6e7076132300607e
권민정 <22alswjd@naver.com>
2022-10-31 22:16:40 +0900

    refector: Problem6 리펙토링
    makeAnswerSet 함수 만듦

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,25 +4,14 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        HashSet<String> answerSet = new HashSet<>();
+        HashSet<String> answerSet; // 알림이 필요한
         HashMap<String, HashSet<String>> words;
 
         // 크루들 닉네임을 2글자씩 분리
         words = spliteNamePerCrewLength2(forms);
 
-        // 연속되는 글자가 있는지 확인
-        for(int i = 0; i < forms.size() - 1; i++) {
-            for(int j = i + 1; j < forms.size(); j++) {
-                String firstEmail = forms.get(i).get(0);
-                String secondEmail = forms.get(j).get(0);
-                if(answerSet.contains(firstEmail) && answerSet.contains(secondEmail))
-                    continue;
-                if(isCommon(words.get(firstEmail), words.get(secondEmail))) {
-                    answerSet.add(firstEmail);
-                    answerSet.add(secondEmail);
-                }
-            }
-        }
+        // 연속된 같은 글자가 있는 크루들을 찾아 answerSet에 넣음
+        answerSet = makeAnswerSet(forms, words);
 
         // answer에 담아 오름차순 정렬
         List<String> answer = new ArrayList<>(answerSet);
@@ -43,6 +32,31 @@ public class Problem6 {
         }
 
         return words;
+    }
+
+    private static HashSet<String> makeAnswerSet(List<List<String>> forms,
+                                      HashMap<String, HashSet<String>> words) {
+        HashSet<String> answerSet = new HashSet<>();
+
+        // 두 사람씩 비교하여 연속되는 닉네임인지 확인
+        for(int i = 0; i < forms.size() - 1; i++) {
+            for(int j = i + 1; j < forms.size(); j++) {
+                String firstEmail = forms.get(i).get(0);
+                String secondEmail = forms.get(j).get(0);
+
+                // 두 사람 모두 이미 answerSet에 있다면 재비교 하지 않음
+                if(answerSet.contains(firstEmail) && answerSet.contains(secondEmail))
+                    continue;
+
+                // 드 크루의 닉네임에 연속된 같은 글자가 있는지 확인
+                if(isCommon(words.get(firstEmail), words.get(secondEmail))) {
+                    answerSet.add(firstEmail);
+                    answerSet.add(secondEmail);
+                }
+            }
+        }
+
+        return answerSet;
     }
 
     private static boolean isCommon(HashSet<String> set1, HashSet<String> set2) {
