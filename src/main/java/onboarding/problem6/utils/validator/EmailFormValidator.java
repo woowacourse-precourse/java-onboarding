baecb5f5e962eb216904d6e484e565d83d5f8a28
song-wooseok <sws981202@gmail.com>
2022-11-01 00:23:51 +0900

    feat: [PROBLEM6 - 예외사항2] 각 신청자의 이메일 형식 검사

---
+++ src/main/java/onboarding/problem6/utils/validator/EmailFormValidator.java
@@ -0,0 +1,28 @@
+package onboarding.problem6.utils.validator;
+
+import onboarding.problem6.utils.Constants;
+import onboarding.problem6.utils.validator.exception.EmailFormException;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class EmailFormValidator {
+    private List<List<String>> form;
+
+    public EmailFormValidator(List<List<String>> form) throws EmailFormException{
+        this.form = form;
+        validateAllEmailForm();
+    }
+    private void validateAllEmailForm() throws EmailFormException{
+        for(List<String> crew : form){
+            validateEmailForm(crew);
+        }
+    }
+
+    private void validateEmailForm(List<String> crew) throws EmailFormException{
+       if(Pattern.matches(Constants.EMAIL_REGEX, crew.get(Constants.CERW_EMAIL))){
+           throw new EmailFormException("신청자 중에 이메일 형식이 다른 크루가 있음");
+       }
+    }
+
+}
---
+++ src/main/java/onboarding/problem6/utils/validator/exception/EmailFormException.java
@@ -0,0 +1,7 @@
+package onboarding.problem6.utils.validator.exception;
+
+public class EmailFormException extends Exception{
+    public EmailFormException(String msg){
+        super(msg);
+    }
+}
