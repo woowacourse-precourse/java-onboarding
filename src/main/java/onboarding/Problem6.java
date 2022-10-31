7861425d9cadf4e3663e6cd34edf2aed3a1d6998
shpark30 <seohyeonpark30.gmail.com>
2022-11-01 01:05:38 +0900

    comment: 기능별 상단에 주석 처리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,5 @@
 package onboarding;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -16,23 +15,26 @@ public class Problem6 {
             problem.updateEvery2SequentialChar(i, forms.get(i).get(1)); // 닉네임에서 2글자씩 분절해  charToIndices에 추가
         }
 
+        /* 길이가 2이상인 Index 배열 반환 */
         List<Integer> answerIndices = problem.charToIndices.values()
                                                             .stream()
-                                                            .filter(intList -> intList.size() > 1)  // 길이가 2이상인 Index 배열 반환
+                                                            .filter(intList -> intList.size() > 1)
                                                             .findFirst()
                                                             .orElse(new ArrayList<>());
         if (answerIndices.size()==0) { // 중복 닉네임이 없는 경우 빈 리스트 return
             return Collections.emptyList();
         }
 
+        /* 중복 닉네임 크루의 email 리스트 */
         List<List<String>> finalForms = forms;
-        List<String> answer = answerIndices.stream()
-                                            .map(i -> finalForms.get(i).get(0)) // answer에 중복 닉네임 크루의 email add
+        List<String> emails = answerIndices.stream()
+                                            .map(i -> finalForms.get(i).get(0)) //
                                             .collect(Collectors.toList());
 
-        TreeSet<String> distinceAnswer = new TreeSet<>(answer); // 중복 제거 & 오름차순 정렬
-        answer = new ArrayList(distinceAnswer);
-        return answer;
+        /* email 중복 제거 & 오름차순 정렬 */
+        TreeSet<String> distinceAnswer = new TreeSet<>(emails);
+        emails = new ArrayList(distinceAnswer);
+        return emails;
     }
 
     static List<List<String>> except(List<List<String>> forms) {
