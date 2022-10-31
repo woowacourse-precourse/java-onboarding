0996c0fc5295d7d1591c7239785aa9c2894e9934
jeongyeonKimm <wjddus9132@gmail.com>
2022-10-31 23:28:24 +0900

    [Feature]Problem4- solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,38 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        char[] wordToChar = word.toCharArray();
+
+        for (char c: wordToChar) {
+            if (isAlphabet(c)) {
+                c = converter(c);
+            }
+            sb.append(c);
+        }
+
+        return String.valueOf(sb);
+    }
+
+    private static char converter(char alphabet) {
+        if (isUpper(alphabet)) {
+            return (char)('Z' - (alphabet - 'A'));
+        } else if (isLower(alphabet)) {
+            return (char)('z' - (alphabet - 'a'));
+        } else {
+            return alphabet;
+        }
+    }
+
+    private static boolean isAlphabet(char alphabet) {
+        return isUpper(alphabet) || isLower(alphabet);
+    }
+
+    private static boolean isUpper(char alphabet) {
+        return alphabet >= 'A' && alphabet <= 'Z';
+    }
+
+    private static boolean isLower(char alphabet) {
+        return alphabet >= 'a' && alphabet <= 'z';
     }
 }
