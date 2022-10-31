39b8501b747c068d2ed78a6c6442ea4f246cc688
JungHyun Jo <wjstkwhwjdgus@gmail.com>
2022-10-31 20:09:07 +0900

    exception handling

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+
+		// 예외처리
+		if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
+			return -1;
+		}
+
+		return answer;
+	}
 }
