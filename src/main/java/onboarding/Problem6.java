ac534790ad29602751c9f7f28bc4b42d7ebd6b14
hhiyeon <hhyyeon0214@gmail.com>
2022-10-31 12:03:47 +0900

    feat: p6-get two digit name to nickname

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,8 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static String getTwoDigitName(String nickname, int startIdx) {
+        return nickname.substring(startIdx, startIdx + 2);
+    }
 }
