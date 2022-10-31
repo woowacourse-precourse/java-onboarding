426ef28298259f474619b5bed740c23262442aa7
hyeonseog-won <smreo75@gmail.com>
2022-10-31 15:19:31 +0900

    Problem6 - 단어 쪼개기 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        ArrayList<String> word = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            for (int i = 1; i < form.get(1).length(); i++) {
+                word.add(form.get(1).substring(i - 1, i + 1));
+            }
+        }
+
         return answer;
     }
 }
