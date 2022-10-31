f81539383ff412f57543580383dbb9a6e53d631b
ensk26 <tjensk26@gmail.com>
2022-10-31 19:13:28 +0900

    feat: 닉네임을 두 글자씩 나눈 것 기준으로 email과 함께 hashMap에 저장하는 기능 추가, problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+
+    static HashMap<String, List<String>> hashMap = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
 
@@ -13,6 +17,20 @@ public class Problem6 {
     //닉네임을 두글자씩 나눈 것 기준으로 email과 함께 저장
     static void saveEmailByDivideNickName(String nickName, String email) {
 
+        String key;
+
+        for (int i = 0; i < nickName.length() - 1; i++) {
+            key = nickName.substring(i, i + 2);
+
+            if (hashMap.get(key) == null) {
+                List<String> list = new ArrayList<>();
+                list.add(email);
+                hashMap.put(key, list);
+
+            } else {
+                hashMap.get(key).add(email);
+            }
+        }
     }
 
 
