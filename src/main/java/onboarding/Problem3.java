0b2b002175c2ad07423081dc5ca51d01d96bf6f8
shinjaewon99 <tlswodnjs99@naver.com>
2022-10-31 18:19:19 +0900

    [Problem3] 미션제출합니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+
+        int cnt = 0;
+        // 1부터 number까지 3 , 6 , 9를 포함하고 있으면 cnt을 1씩증가시킨다.
+        for (int i = 1; i <= number; i ++){
+            int a = i;
+            while(a !=0){
+
+                if(a % 10 == 3 || a % 10 == 6 || a % 10 == 9) {
+                    cnt += 1;
+                }
+                a = a/10;
+
+            }
+        }
+
+
+        return cnt;
+
+
     }
+
+
 }
