0fa95889caf1c8695e1ef324d7c7cb23b2a6a31c
tlsqj <rnjstlsqja@inha.edu>
2022-10-31 20:10:46 +0900

    문제 5 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,24 +7,10 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         int coins[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
-        int coinsCount[] = {0};
-
-        int a = 0;
-        int count =0;
-        while(true){
-            if(money == 0) break;
-
-            if(money < coins[a] * count) {
-                coinsCount[a] = count - 1;
-                money -= coins[a] * (count - 1);
-
-                a++;
-                count = 0;
-            }
-        }
 
         for(int i = 0; i < 10; i++) {
-            answer.get(coinsCount[i]);
+            answer.add(money / coins[i]);
+            money %= coins[i];
         }
 
         return answer;
