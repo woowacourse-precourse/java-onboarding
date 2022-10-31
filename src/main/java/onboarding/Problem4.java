4b98e8afc43dde13cd8bc951ea49a2db0f33ef1c
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 00:54:36 +0900

    feat: problem4 입력값이 유효한지에 대해 예외체크하는 메서드 checkException

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+        if(checkException(word)){
+            throw new IllegalArgumentException();
+        }
         return answer;
+    }
+    public static boolean checkException(String word){
+        if(word == null || checkLength(word.length()) || !checkAlpa(word)){
+            return true;
+        }
+        return false;
+    }
+    public static boolean checkLength(int length){
+        return length < 1 || length >1000;
+    }
+    public static  boolean checkAlpa(String word){
+        return word.matches("^[a-zA-Z\\s]*$");
     }
 }
