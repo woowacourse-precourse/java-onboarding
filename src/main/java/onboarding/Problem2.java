850edcdc94142a040b9fdcd6bad8df19bf169f9f
plusjob70 <plusjob70@gmail.com>
2022-10-31 13:13:21 +0900

    feat: 문자열에서 연속 중복 문자 체크

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,31 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return "";
+    }
+
+    private static boolean[] checkDuplicate(String str) {
+        int strLen = str.length();
+        boolean[] duplicate = new boolean[strLen];
+
+        int left = 0;
+        int right = 1;
+
+        while (right < strLen) {
+            if ((str.charAt(left) == str.charAt(right))) {
+                duplicate[left] = true;
+                duplicate[right] = true;
+                right++;
+
+                while (right < strLen && str.charAt(left) == str.charAt(right)) {
+                    duplicate[right] = true;
+                    right++;
+                }
+            }
+            left = right;
+            right++;
+        }
+
+        return duplicate;
     }
 }
