a4ef97bb3d5b1370071abf95ba6a1ce365ab9d2e
twoosky <lhjae3@naver.com>
2022-11-01 03:51:21 +0900

    feat(problem5): 변환한 각 화폐 별 개수를 리스트에 담아 반환

--- src/main/java/problem5/AccountWithDraw.java
+++ src/main/java/problem5/AccountWithDraw.java
@@ -11,6 +11,18 @@ public class AccountWithDraw {
         this.money = money;
     }
 
+    public List<Integer> withDraw() {
+        List<Integer> result = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        int index = 0;
+
+        while(money != 0) {
+            int count = getCount(index);
+            result.set(index, count);
+            index += 1;
+        }
+        return result;
+    }
+
     private int getCount(int index) {
         int count = money / unit.get(index);
         money = money % unit.get(index);
