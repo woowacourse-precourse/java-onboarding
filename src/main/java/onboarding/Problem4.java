5c9f7ae0541b23eb545946f67ccabc92c322a8a0
lhyeonj120 <isk04387@gmail.com>
2022-11-01 04:05:09 +0900

    [problem4] feat: 대문자 변환 배열 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,7 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char uppercaseAlphabetArr[] = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
         return answer;
     }
 }
