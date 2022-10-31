8bbc6e75ca39fb60c9d56ae143bdbc880563e7f6
subin Park <psb0320@hs.ac.kr>
2022-11-01 01:39:49 +0900

    feat: friends에서 user와 이미 친구인 사람들 반환 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -10,6 +8,9 @@ public class Problem7 {
 
         // HashMap에 아이디 & 점수 초기화
         HashMap<String, Integer> recommendFds = initRecommendFriends(friends, visitors);
+
+        // user와 이미 친구인 사람들 목록
+        Set<String> alreadyFds = getAlreadyFriends(user, friends);
 
         return answer;
     }
@@ -24,5 +25,19 @@ public class Problem7 {
         for (String visitor : visitors) recommendFds.put(visitor, 0);
 
         return recommendFds;
+    }
+
+    public static Set<String> getAlreadyFriends(String user, List<List<String>> friends) {
+        Set<String> alreadyFds = new HashSet<>();
+
+        for (List<String> fd : friends) {
+            String id1 = fd.get(0);
+            String id2 = fd.get(1);
+
+            if (id1.equals(user)) alreadyFds.add(id2);
+            else if (id2.equals(user)) alreadyFds.add(id1);
+        }
+
+        return alreadyFds;
     }
 }
