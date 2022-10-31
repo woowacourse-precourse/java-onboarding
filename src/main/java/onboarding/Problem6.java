5d50d34c09dff0a6985d2467159fbc12f60dbefa
박시균\psk98 <psg980331@naver.com>
2022-10-31 11:31:18 +0900

    docs : 문제6 기능 구현 목록 정리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 1.신청받은 닉네임을 두글자 단위로 자른다.
+ * 2.두글자 단위로 자른 닉네임을 키로하고 자른 닉네임을 가지는 이메일을 값으로 가지는 맵을 만들어준다.
+ * 3.모든 닉네임에 대해서 1~2번을 반복후 특정 단어를 포함하는 이메일이 2개이상인 단어를 찾아준다.
+ */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
