0b49d7c3b83c3093012d975478cf6107521e861f
jojaeng2 <ds4ouj@naver.com>
2022-10-31 11:34:51 +0900

    feat : 연속된 문자열을 삭제하는 기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return StringParser.parsing(cryptogram);
+    }
+
+    static class StringParser {
+
+        private StringParser() {
+        }
+
+        private static String parsing(String str) {
+            Stack<String> strStack = new Stack<>();
+            for (int i = 0; i < str.length(); i++) {
+                String temp = String.valueOf(str.charAt(i));
+                boolean isFirst = true;
+                if (isLast(str, i) || !String.valueOf(str.charAt(i + 1)).equals(temp)) {
+                    while (!strStack.empty() && strStack.peek().equals(temp)) {
+                        strStack.pop();
+                        isFirst = false;
+                    }
+                }
+                if (isFirst) {
+                    strStack.push(temp);
+                }
+            }
+            return strStack.stream()
+                    .reduce("", (a, b) -> a + b);
+        }
+
+        private static boolean isLast(String str, int idx) {
+            return str.length() - 1 == idx;
+        }
     }
 }
