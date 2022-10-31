1bf824f0fb3ba78f9b0215a65fcbbc80c3b51045
QLoooad <ckcc0813@gmail.com>
2022-10-31 21:16:31 +0900

    완료2

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -75,83 +75,7 @@ class Problem1 {
 		int HighSco = (A < B) ? B : A;
 		return HighSco;
 	}
-	public static int Odd_L_HighScore(int Page) {
-		int PageL = 0;
-		
-		if(Page % 2 == 1) {//홀수
-			PageL = Page;
-	    }else {//짝수
-	    	PageL = Page-1;
-	    }
-		int length = (int)(Math.log10(PageL)+1);//int자리수
-		
-		int hun = PageL / 100; // 백
-		int ten = (PageL / 10) % 10; // 십
-		int one = PageL % 10; // 일*/
-		
-		int plusL = 0;
-		int multiL = 0;
-		
-		if(length == 3) { //자릿수에 따른 곱하기
-			plusL = hun+ten+one;
-			multiL = hun*ten*one;//100의자리 아니면 곱했을때 무조
-		}else if(length == 2) {
-			plusL = ten+one;
-			multiL = ten*one;
-		}else {
-			plusL = PageL;
-			multiL = PageL;//100의자리 아니면 곱했을때 무조
-		}
-
-		int HighSco = (plusL < multiL) ? multiL : plusL;
-		return HighSco;
-	}
-	public static int Even_R_HighScore(int Page) {
-        int PageR = 0;
-        
-		if(Page % 2 == 0) {//짝수
-			PageR = Page;
-	    }else {//짝수
-	    	PageR = Page+1;
-	    }
-		int length = (int)(Math.log10(PageR)+1);//int자리수
-		
-		int hun = PageR / 100; // 백
-		int ten = (PageR / 10) % 10; // 십
-		int one = PageR % 10; // 일*/
-		
-		int plusR = 0;
-		int multiR = 0;
-		
-		if(length == 3) { //자릿수에 따른 곱하기
-			plusR = hun+ten+one;
-			multiR = hun*ten*one;//100의자리 아니면 곱했을때 무조
-		}else if(length == 2) {
-			plusR = ten+one;
-			multiR = ten*one;
-		}else {
-			plusR = PageR;
-			multiR = PageR;//100의자리 아니면 곱했을때 무조
-		}
-		
-		int HighSco = (plusR < multiR) ? multiR : plusR;
-		return HighSco;
-	}
-	public static int HighScore(int LScore,int RScore) {
-		int HighSco = (LScore < RScore) ? RScore : LScore;
-		return HighSco;
-	}
-	public static int whoWin(int pobiScore,int crongScore) {
-		int HighSco = 0;
-		if(pobiScore > crongScore) {
-			System.out.println(1);
-		}else if(pobiScore < crongScore) {
-			System.out.println(2);
-		}else if(pobiScore == crongScore) {
-			System.out.println(0);
-		}
-		return HighSco;
-	}
+	
 	public static int makePage() { //makePage와 동시 3~398까지 추출
 		
         int Page = (int) (Math.random() * (398+1)-3)+3; //(최대+1)-최소)+최소 3~398
@@ -160,24 +84,7 @@ class Problem1 {
         }while(Page <= 1 || Page >= 400); //조건 충족 시 리셋
         return Page;
 	}
-    public static void main(String[] args){
-    	
-    	int pobiPage = makePage();
-    	System.out.println(pobiPage+ " pobiPage");
-    	int crongPage = makePage();
-    	System.out.println(crongPage+ " crongPage");
-    	
-    	int pobiLScore = Odd_L_HighScore(pobiPage);
-    	int pobiRScore = Even_R_HighScore(pobiPage);
-    	int crongLScore = Odd_L_HighScore(crongPage);
-    	int crongRScore = Even_R_HighScore(crongPage);
-       
-    	int pobiScore = HighScore(pobiLScore,pobiRScore);
-    	System.out.println(pobiScore);
-    	int crongScore = HighScore(crongLScore,crongRScore);
-    	System.out.println(crongScore);
-    	whoWin(pobiScore,crongScore);
-    	}
+   
     }
 /*
  * 랜덤번호 3~398 첫,막페이지 x **
