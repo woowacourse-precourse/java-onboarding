51b0c143b09e18d77e3f3b731dc9f205578a40e1
xxeol2 <jhj01177@naver.com>
2022-10-31 12:56:18 +0900

    feat(prob6): 두 닉네임이 같은 글자가 연속적으로 포함되는지 판별하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,17 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = List.of("answer");
+		return answer;
+	}
+
+	public static boolean checkDuplicate(String nickname1, String nickname2) {
+		for (int i = 0; i < nickname1.length(); i++) {
+			if (nickname2.contains(nickname1.substring(i, i + 2))) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
