dfb76d1533c644eadb3acb93469a79e74dc05811
tkdtn4657 <tkdtn101226@gmail.com>
2022-10-31 21:13:59 +0900

    problem2 기능구현 및 리팩토링 제한사항 미추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,48 +1,81 @@
 package onboarding;
 
-public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        
-        
-
-		String s = new String("zyelleyz");
-		String s1, s2;
-
-		char temp;
-		
-		
-		temp = s.charAt(0);
-		while(true) {
-			for(int i = 1; i < s.length(); i++) {
-				if(temp == s.charAt(i)) {				
-					s1 = s.substring(0,i-1);
-					s2 = s.substring(i+1, s.length());
-				
-					System.out.println("s1 : " + s1);
-					System.out.println("s2 : " + s2);
-				
-					s = s1.concat(s2);
-					
-					System.out.println(s);
-					
-					if(s == null) {
-						break;
-					}else {
-					i = 0;
-					temp = s.charAt(0);
-					continue;
-					}
-				}
-				else if(i == s.length()){
-					break;
-				}
-				else
-				temp = s.charAt(i);
-			}
-			
+class remove_dup{
+	String temp = new String();
+	
+	
+	String s = new String();
+	
+	remove_dup() {
+	}
+	
+	
+	remove_dup(String s) {
+		this.s = s;
+	}
+	
+	void add_temp(String s){
+		if(!s.isEmpty()) {
+			temp = s;
 		}
 		
-        
-    }
+	}
+	
+	String remove(String s) {
+		String s1, s2;
+		
+		int i;
+		
+		add_temp(s);
+		
+		for(i = 1; i < s.length(); i++) {		
+			if(temp.charAt(i - 1) == s.charAt(i)) {
+				
+				s1 = s.substring(0,i-1);
+				s2 = s.substring(i+1, s.length());
+				
+				this.s = s1.concat(s2);
+			}
+		}
+		
+		return this.s;
+	}
+	
+	boolean end_check() {
+		int i;
+		
+		for(i = 0; i < s.length()-1; i++) {		
+			if(s.charAt(i) == s.charAt(i+1)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	String result() {
+		
+		if(s.isEmpty()) {
+			return s;
+		}else 
+			while(true) {
+				if(end_check()) {
+					remove(s);	
+				}else {
+					return s;
+				}
+			}
+	}
+}
+
+public class Problem2 {
+    public static String solution(String cryptogram) {
+		
+    	
+		String s = new String(cryptogram);
+		
+		remove_dup d = new remove_dup(s);
+		
+		return d.result();
+
+	}
 }
