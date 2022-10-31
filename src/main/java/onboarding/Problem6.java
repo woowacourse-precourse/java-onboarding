107382c3516fe9da3b1d3a7c1f56abfd13de78b8
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:52:27 +0900

    [Problem6] 연속 문자가 2명 이상 겹치는 경우 연속 문자가 포함된 닉네임의 이메일 answer에 추가하기 (김민서)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -25,6 +25,16 @@ public class Problem6 {
             crews.put(list.get(1), list.get(0)); // answer에 크루 이메일 출력하기 위해 이메일, 닉네임으로 이루어진 해쉬맵에 put하기
         }
 
+        // 2. 연속 문자가 2명 이상 겹치는 경우 연속 문자가 포함된 닉네임의 이메일 answer에 추가하기
+        for(List<String> list : forms) {
+            for(int i = 0; i < list.get(1).length() - 1; i++) {
+                if(nickname.get(list.get(1).substring(i, i + 2)) >= 2) { // 2명이상 연속문자가 겹쳐야 하므로 2이상일 때 answer에 더해줌
+                    answer.add(crews.get(list.get(1)));
+                    break; // 중복을 포함하지 않기 위해 한 번 넣었으면 break로 다음 문장으로 넘어가게 함
+                }
+            }
+        }
+
         Collections.sort(answer); // 이메일을 오름차순으로 정렬하기 위함
         return answer;
     }
