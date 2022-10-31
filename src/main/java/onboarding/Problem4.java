edf928f99cddd696daadcfbd720ccb58ab334bb7
syr9707 <yrshin9771@gmail.com>
2022-10-31 11:00:02 +0900

    feat(PROBLEM4): 4번 문제 풀이

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(c >= 'A' && c <= 'Z') {
+                c = daemunja(c);
+            }
+            if(c >= 'a' && c <= 'z') {
+                c = somunja(c);
+            }
+            answer += c;
+        }
+
         return answer;
     }
 
