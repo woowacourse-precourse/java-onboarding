67ae087a37ead7b877332951015c1d60d75021be
Timothy <toydiary@kakao.com>
2022-10-31 21:39:49 +0900

    feat(문제 2): 입력 값에 대한 예외처리
    1. 입력 값에 대한 예외처리
    > - 입력된 String 값의 길이 체크 : 1 이상, 1000 이하
    > - 입력된 String 값이 알파벳 소문자로만 이루어져 있는지 체크

---
+++ src/test/java/onboarding/problem2/util/CryptogramInputVailidatorTest.java
@@ -0,0 +1,42 @@
+package onboarding.problem2.util;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CryptogramInputVailidatorTest {
+	@Nested
+	class Problem2Test {
+		@Test
+		void 입력된_String_값의_길이_체크() {
+			// give
+			String cryptogramPass = "browoanoommnaon";
+			String cryptogramFail = "1234567890";
+			StringBuilder sbCryptogramFail = new StringBuilder("1234567890");
+			for (int i = 0; i < 100; i++) sbCryptogramFail.append(cryptogramFail);
+
+			cryptogramFail = sbCryptogramFail.toString();
+			// when
+			boolean resultPassed = true;
+			boolean resultFailed = true;
+			// then
+			assertThat(CryptogramInputVailidator.lengthValidator(cryptogramPass)).isEqualTo(resultPassed);
+			assertThat(CryptogramInputVailidator.lengthValidator(cryptogramFail)).isNotEqualTo(resultFailed);
+		}
+
+		@Test
+		void 입력된_String_값이_알파벳_소문자로만_이루어져_있는지_체크() {
+			// give
+			String cryptogramPass = "browoanoommnaon";
+			String cryptogramFail = "1234567890ABc";
+
+			// when
+			boolean resultPassed = true;
+			boolean resultFailed = true;
+			// then
+			assertThat(CryptogramInputVailidator.alphabetLowcasePattern(cryptogramPass)).isEqualTo(resultPassed);
+			assertThat(CryptogramInputVailidator.alphabetLowcasePattern(cryptogramFail)).isNotEqualTo(resultFailed);
+		}
+	}
+}
