0b9cbdf161c85b2525b2777622f3fe476ea74d6e
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-10-31 18:03:35 +0900

    Add : 입력으로 들어온 두 페이지에서 각 페이지의 합중 큰 값을 찾는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    // 유저가 펼친 페이지의 왼쪽, 오른쪽의 각 자리수의 합 중 큰 값을 찾는 메서드
+    public static int sumNumber(List<Integer> list){
+        List<Integer> result = new ArrayList<>();
+        for(Integer num : list){
+            int temp = 0;
+            while (num > 0){
+                temp += num % 10;
+                num /= 10;
+            }
+            result.add(temp);
+        }
+        return result.stream().mapToInt(Integer::intValue).max().orElse(0);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
