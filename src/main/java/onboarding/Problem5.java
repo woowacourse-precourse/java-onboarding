34175fa6077f76c8f4f1cd62e1f7542c43f76ecb
plusjob70 <plusjob70@gmail.com>
2022-10-31 13:28:42 +0900

    feat: 회폐 단위 정의

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> result = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        int[] currencies = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
+        return result;
     }
 }
