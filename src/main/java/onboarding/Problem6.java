8b413944a4d01a9d2f04d1a5d91e2e46c4f23192
Jueun <jueun000101@gmail.com>
2022-10-31 20:54:00 +0900

    docs: 6번 기능 목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,14 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 두 개의 닉네임에 중복되는 문자열이 있는지 확인하기 (20^2)
+ * 2. 중복되는 닉네임을 찾아 이메일이 중복되지 않도록 리스트에 저장하기 (1e4^2)
+ * 3. 이메일을 오름차순으로 정렬하여 답 반환하기
+ *
+ * 설계한 1e4 * 1e4 * 20 * 20 = 4e10
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
