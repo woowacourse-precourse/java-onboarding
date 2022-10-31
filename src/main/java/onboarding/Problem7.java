d36bae3b029a9eab0e7d7522f6b1d60d6009f65a
Go-Jaecheol <gojaech@naver.com>
2022-11-01 00:00:17 +0900

    feat: Problem7 - 방문 횟수를 기준으로 점수 계산하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class Problem7 {
     private static Map<String, Set<String>> friendsMap;
+    private static Map<String, Integer> scoreMap;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         friendsMap = new HashMap<>();
+        scoreMap = new HashMap<>();
 
         friendsMap.put(user, new HashSet<>());
         for (List<String> friend : friends) {
@@ -15,6 +17,7 @@ public class Problem7 {
             addFriendsToMap(friend1, friend2);
             addFriendsToMap(friend2, friend1);
         }
+        addVisitorScore(visitors);
 
         return answer;
     }
@@ -24,5 +27,14 @@ public class Problem7 {
             friendsMap.put(keyFriend, new HashSet<>());
         }
         friendsMap.get(keyFriend).add(valueFriend);
+    }
+
+    private static void addVisitorScore(List<String> visitors) {
+        for (String visitor : visitors) {
+            if (!scoreMap.containsKey(visitor)) {
+                scoreMap.put(visitor, 0);
+            }
+            scoreMap.put(visitor, scoreMap.get(visitor) + 1);
+        }
     }
 }
