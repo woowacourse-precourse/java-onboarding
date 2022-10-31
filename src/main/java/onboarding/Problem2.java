15ff442e784aa2223ec6b9982a2e60344a34dba6
eil-you <qldirl123@gmail.com>
2022-10-31 23:06:36 +0900

    check: 중복 문자 확인 remove: 중복 문자 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,43 +3,70 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
         String answer = "answer";
-        if(overlapRemove(cryptogram).equals("0")){
-            answer =cryptogram;
-        }
-        if (!overlapRemove(cryptogram).equals("0")){
-            answer = overlapRemove(cryptogram);
+
+        answer = check(cryptogram);
+
+        return answer;
+    }
+
+    public static String check(String cryptogram) {
+
+        String answer = remove(cryptogram);
+
+        if (answer.equals("0")) {
+
+            answer = cryptogram;
+
         }
 
-        while (!overlapRemove(answer).equals("0")) {
-            if (!overlapRemove(answer).equals("0")) {
-                answer = overlapRemove(answer);
-            }
+        while (!remove(answer).equals("0")) {
+
+            answer = remove(answer);
 
         }
 
         return answer;
     }
-    public static String overlapRemove(String cryptogram){
+
+    public static String remove(String cryptogram){
+
         String [] letter = cryptogram.split("");
+
         StringBuilder answer = new StringBuilder();
+
         int num = 0;
+
         for(int i = 0; i<letter.length-1 ; i++){
+
             if(letter[i].equals(letter[i+1])){
+
                 letter[i]= "";
+
                 letter[i+1]= "";
+
                 num++;
             }
+
             if(!letter[i].equals(letter[i+1])){
+
                 answer.append(letter[i]);
+
             }
+
             if(i == letter.length-2){
+
                 answer.append(letter[i + 1]);
+
             }
+
         }
 
         if(num==0){
+
             answer = new StringBuilder("0");
+
         }
 
         return answer.toString();
