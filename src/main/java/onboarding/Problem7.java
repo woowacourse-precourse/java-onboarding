ba2426babb8fcf5b83a4693c6053cd22a0659537
win9 <krrr8@naver.com>
2022-10-31 11:54:44 +0900

    [기능7번] 점수등록 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,9 +6,14 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Map<String, Integer> friendsScore = new HashMap<>();
         List<String> exceptNames = new ArrayList<>();
+
     }
 
 
+
+    private static Integer countScore(Map<String, Integer> friendsScore, String friendName, int score) {
+        return friendsScore.put(friendName, friendsScore.getOrDefault(friendName, 0) + score);
+    }
 
     private static boolean isAlreadyFriends(List<String> exceptNames, List<String> friend){
         return exceptNames.contains(friend.get(0)) || exceptNames.contains(friend.get(1));
