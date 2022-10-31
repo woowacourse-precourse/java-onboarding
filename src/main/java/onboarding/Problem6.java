e5040a7437c282010a5161033704ea9470504364
한은영 <oneeqzero@gmail.com>
2022-10-31 17:03:16 +0900

    feat: problem6 사용자 이름 두 글자씩 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    static Map<String, Set<String>> dup_list = new HashMap<String, Set<String>>(){
+        @Override
+        public Set<String> get(Object key){
+            Set<String> set = super.get(key);
+            if(set == null){
+                set = new HashSet<String>();
+                put((String) key,set);
+            }
+            return set;
+        }
+    };
+    static Map<String, String> user_list = new HashMap<String, String>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        for(List<String> f : forms){
+            String email = f.get(0); String nickname = f.get(1);
+            for(int i = 0; i<nickname.length()-1; i++){
+                String sub_nick = nickname.substring(i,i+2);
+                dup_list.get(nickname).add(sub_nick);
+            }
+        }
         return answer;
     }
 }
