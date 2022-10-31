6d416651858177780f12bc1c9a3893794074675e
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-10-31 18:11:48 +0900

    feat(Problem4): 문자열을 아스키 코드로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,15 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static int[] convertStringtoASCIICode(String word) {
+        char[] words = word.toCharArray();
+        int[] asciiCodes = new int[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            asciiCodes[i] = words[i];
+        }
+
+        return asciiCodes;
+    }
 }
