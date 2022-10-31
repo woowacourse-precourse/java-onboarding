7616da06fb85c5c4098745739f2f4f7e0940e544
janu01ary <janu01ary@gmail.com>
2022-10-31 17:44:30 +0900

    [Problem1] 페이지가 유효한지 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static boolean checkPage(int[] pages) {
+        if (pages[0] - pages[1] != -1)
+            return false;
+        if (pages[0] < 1 || pages[1] > 400)
+            return false;
+        return true;
+    }
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (!checkPage(pobi) || !checkPage(crong))
+            return -1;
         int answer = Integer.MAX_VALUE;
         return answer;
     }
