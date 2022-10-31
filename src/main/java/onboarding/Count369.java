4c06eb193e95f3a2b6a93c8bb93920c0d6945e9f
yoonsj970407 <yoonsj970407@gmail.com>
2022-10-31 19:49:18 +0900

    ✨Feat: Add count()

--- src/main/java/onboarding/Count369.java
+++ src/main/java/onboarding/Count369.java
@@ -14,4 +14,22 @@ class Count369 {
             return 0;
         }
     }
+
+    int count() {
+        int cnt = 0, cur;
+
+        for(int i = 1; i < number; i++) {
+            cur = i;
+
+            while(cur > 0) {
+                if((cur % 10 == 3) || (cur % 10 == 6) || (cur % 10 == 9)) {
+                    cnt++;
+                }
+
+                cur = cur / 10;
+            }
+        }
+
+        return cnt;
+    }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer;
+        Count369 count = new Count369(number);
+
+        if(count.exception() == -1) {
+            answer = -1;
+        } else {
+            answer = count.count();
+        }
+
         return answer;
     }
 }
