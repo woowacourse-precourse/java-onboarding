4dfb121aa979eeb6f1861ef7c87b3c0db61c97c5
patronum98 <zkrhklkl98@gmail.com>
2022-11-01 00:07:04 +0900

    문제4 기능목록 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,10 +1,14 @@
 package onboarding;
 
-//기능1. 대문자의 위치를 파악한다
-//기능1-1. 리스트배열로 변환하여 각 자리에 문자를 넣은뒤 대문자가 있는 곳을 확인한다.
-//기능1-2. 모두 소문자로 바꾼다.
-//기능2. 알파벳을 거꾸로 출력한다
-//기능3. 거구로 출력한 문장에 원래 문장의 대문자 위치에 대문자로 바꾸어준다.
+//기능1. 문장을 한 글자씩 리스트에 저장한다.
+//기능2. 리스트에 있는 알파벳을 아스키코드로 변환한다.
+//기능3. 아스키코드에서 -65를 하여 몇번째 알파벳인지 구한다(N)
+//기능4. 반대문자 = 원래문자+(25-2N)을 하여 반대문자를 구한 뒤 리스트에 ADD
+//기능5. 리스트를 문자열로 변환 후 반환한다.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Problem4 {
     public static String solution(String word) {
