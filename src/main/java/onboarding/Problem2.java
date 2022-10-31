b004d89ffb7220199a67b57b29a68ea5af5811bb
채희찬 <eqholic1125@gmail.com>
2022-10-31 14:44:55 +0900

    feat: problem2 연속된 문자 찬기 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+
+    private static List<Integer> findConsecutiveChar(String cryptogram) {
+        List<Integer> charsToRemove = new ArrayList<>();
+
+        for (int index = 0; index < cryptogram.length(); index++) {
+            if (cryptogram.charAt(index) == cryptogram.charAt(index + 1)) {     // 다음 글자와 같을 경우
+                charsToRemove.add(index);
+                charsToRemove.add(index);
+            }
+        }
+        return charsToRemove;
+    }
+
+    private static String removeConsecutiveChar (String cryptogram, List < Integer > charsToRemove){
+
+    }
+
+    private static String decode (String cryptogram){
+
+    }
+
 }
