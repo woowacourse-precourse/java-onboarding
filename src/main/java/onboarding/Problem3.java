4d5750f9b8c7ccfabf36afd1144cf19036ac24af
suhyeon <suhyeon3484@naver.com>
2022-11-01 02:27:33 +0900

    problem3 solve

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int n = 0;
+
+        for (int num = 1; num <= number; num++) {
+            n = num;
+            while (true) {
+                if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9)
+                    answer += 1;
+                if (n < 10)
+                    break;
+                n = n / 10;
+            }
+        }
         return answer;
     }
 }
