c5b848872c4717440333c796e3e705f6bfec9b19
KimSoo <ilppang@naver.com>
2022-10-31 23:40:05 +0900

    Incomplete(김현수/problem6): 문제 6번 구현 중

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        System.out.println(forms.get(0).get(1));
+
         return answer;
     }
 }
