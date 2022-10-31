3d433a622cb0ed59afa1d07bf31efec7e27bd0b0
ouohoon <begreat@kakao.com>
2022-11-01 03:39:15 +0900

    리팩토링: 문제1 기능5 Page 클래스와 메소드를 사용하도록 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -20,79 +20,26 @@ class Problem1 {
     5. 점수 비교해서 승자 결정 기능
      */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (isInvalidPages(pobi) || isInvalidPages(crong)) {
+        Page pobiPage = new Page(pobi);
+        Page crongPage = new Page(crong);
+        if (pobiPage.isInvalidPages() || crongPage.isInvalidPages()) {
             return RESULT_EXCEPTION;
         }
 
-        int pobiNumber = calculatePages(pobi);
-        int crongNumber = calculatePages(crong);
+        int pobiScore = pobiPage.calculatePages();
+        int crongScore = crongPage.calculatePages();
 
-        if (crongNumber > pobiNumber) {
+        if (crongScore > pobiScore) {
             return RESULT_CRONG;
         }
-        if (pobiNumber > crongNumber) {
+        if (pobiScore > crongScore) {
             return RESULT_POBI;
         }
         return RESULT_DRAW;
     }
 
 
-    /* 두 페이지의 유효성을 검사한다.
-     1. Null 체크
-     2. 페이지 범위(1~400) 및 6번 규칙 체크
-     3. 왼쪽 페이지 홀수, 오른쪽 페이지 짝수 체크
-     4. 페이지 연속성 체크
-     */
-    private static boolean isInvalidPages(List<Integer> pages) {
-        Integer leftPage = pages.get(LEFT_PAGE_INDEX);
-        Integer rightPage = pages.get(RIGHT_PAGE_INDEX);
-        if (leftPage == null || rightPage == null) {
-            return true;
-        }
-        if (leftPage <= MIN_PAGE || rightPage >= MAX_PAGE) {
-            return true;
-        }
-        if (leftPage % 2 == 0 || rightPage % 2 == 1) {
-            return true;
-        }
-        if (leftPage + 1 != rightPage) {
-            return true;
-        }
-        return false;
-    }
 
-    // 페이지의 각 자릿수를 더한 결과를 리턴한다.
-    private static int calcAddPageNumber(int pageNumber) {
-        int result = 0;
-        while (pageNumber > 0) {
-            result += pageNumber % 10;
-            pageNumber /= 10;
-        }
-        return result;
-    }
 
-    // 페이지의 각 자릿수를 곱한 결과를 리턴한다.
-    private static int calcMulPageNumber(int pageNumber) {
-        int result = 1;
-        while (pageNumber > 0) {
-            result *= pageNumber % 10;
-            pageNumber /= 10;
-        }
-        return result;
-    }
 
-    // 양 페이지의 각 자릿수를 더한 수, 곱한 수 중에서 가장 큰 수를 리턴한다.
-    private static int calculatePages(List<Integer> pages) {
-        int leftPage = pages.get(LEFT_PAGE_INDEX);
-        int leftPageAddNumber = calcAddPageNumber(leftPage);
-        int leftPageMulNumber = calcMulPageNumber(leftPage);
-        int leftPageMaxNumber = Math.max(leftPageAddNumber, leftPageMulNumber);
-
-        int rightPage = pages.get(RIGHT_PAGE_INDEX);
-        int rightPageAddNumber = calcAddPageNumber(rightPage);
-        int rightPageMulNumber = calcMulPageNumber(rightPage);
-        int rightPageMaxNumber = Math.max(rightPageAddNumber, rightPageMulNumber);
-
-        return Math.max(leftPageMaxNumber, rightPageMaxNumber);
-    }
 }
