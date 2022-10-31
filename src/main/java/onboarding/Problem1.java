f32fc082301c02b879a477698f0429d52a2fc788
은지 <midzy0228@gmail.com>
2022-10-31 15:52:11 +0900

    refactor[Problem1]: 크기 비교 기능 class로 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,7 +9,29 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    static int maxPoint(List<Integer> player) {
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        MaxPoint maxPoint = new MaxPoint();
+        
+        int pobiMaxPoint = maxPoint.maxPoint(pobi);
+        int crongMaxPoint = maxPoint.maxPoint(crong);
+        int answer;
+
+        if (pobiMaxPoint==-1 || crongMaxPoint==-1){
+            answer = -1;
+        } else if (pobiMaxPoint>crongMaxPoint){
+            answer = 1;
+        } else if (pobiMaxPoint<crongMaxPoint){
+            answer = 2;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
+
+class MaxPoint {
+    public int maxPoint(List<Integer> player) {
         String leftPage = String.valueOf(player.get(0));
         String rightPage = String.valueOf(player.get(1));
         int myPoint;
@@ -38,22 +60,5 @@ class Problem1 {
         }
 
         return myPoint;
-    }
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        // 시작 면과 마지막 면 ([1,2] 또는 [399,400] 이 있는지 확인)
-        int pobiMaxPoint = maxPoint(pobi);
-        int crongMaxPoint = maxPoint(crong);
-        int answer;
-
-        if (pobiMaxPoint==-1 || crongMaxPoint==-1){
-            answer = -1;
-        } else if (pobiMaxPoint>crongMaxPoint){
-            answer = 1;
-        } else if (pobiMaxPoint<crongMaxPoint){
-            answer = 2;
-        } else {
-            answer = 0;
-        }
-        return answer;
     }
 }
