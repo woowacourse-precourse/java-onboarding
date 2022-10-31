5306d0e2e6d3326cc7268d62c4f036507d9387a9
shoeone96 <dlwnddnjs96@naver.com>
2022-11-01 01:17:54 +0900

    5번 문제 풀이

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,51 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/*
+돈 인출 방식에 대한 코드
+50000원, 10000원
+5000원, 1000원
+500원, 100원
+50원, 10원
+1원
+으로 인출이 가능
+
+1. 돈을 각자리수마다 배열에 담기
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        String SMoney = String.valueOf(money);
+        System.out.println(SMoney);
+        List<Integer> answer = new ArrayList<>(9);
+        int[] AMoney = {0, 0, 0, 0, 0};
+        int order = 4;
+        for (int i = SMoney.length() - 1; i >= 0; i--) {
+            if (i == SMoney.length() - 5) {
+                AMoney[0] = Integer.parseInt(SMoney.substring(0, SMoney.length() - 4));
+                break;
+            }
+            AMoney[order] = (SMoney.charAt(i) - '0');
+            order--;
+        }
+        System.out.println(Arrays.toString(AMoney));
+        int[] answerL = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        order = 0;
+        for (int i = 0; i < 8; i++) {
+            answerL[i] = AMoney[order] / 5;
+            answerL[++i] = AMoney[order] - answerL[i - 1] * 5;
+            order++;
+        }
+
+        for (int i = 0; i < answerL.length - 1; i++) {
+            answer.add(answerL[i]);
+        }
+        answer.add(AMoney[4]);
+
         return answer;
     }
 }
