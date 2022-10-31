fcd772b628c54910378f98e5ca68a4ce8c3246d9
송민진 <m_6595@naver.com>
2022-11-01 01:39:07 +0900

    #2 feat: find indexes to remove whose letter repeats

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         List<String> charList = List.of(cryptogram.split(""));
+        List<Integer> indexesToRemove = findIndexesToRemove(charList);
         return answer;
+    }
+
+    private static List<Integer> findIndexesToRemove(List<String> charList) {
+        List<Integer> indexesToRemove = new ArrayList<>();
+        for (int i = 0; i < charList.size(); i++) {
+            if (i != 0 && charList.get(i).equals(charList.get(i-1))) {
+                indexesToRemove.add(i);
+                indexesToRemove.add(i-1);
+            }
+        }
+        if (indexesToRemove.isEmpty()) {
+            return List.of(-1);
+        }
+        return indexesToRemove;
     }
 }
