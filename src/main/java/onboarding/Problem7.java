26b05a06963b2a8a36d73a70ae0cb5adaea92bc0
Ilchul Kim <huj0159@gmail.com>
2022-11-01 03:41:38 +0900

    feat: add method find friend's friend who not user's friend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,10 +7,38 @@ public class Problem7 {
         List<String> answer = new ArrayList<>();
         HashMap<String, Integer> hm = new HashMap<>();
         List<String> friendList = findUserFriend(user, friends);
+        HashMap<String, Integer> algorithmPointMap = findFriendOfFriend(user,friends,friendList);
+
 
 
         return answer;
     }
+
+    public static HashMap<String ,Integer> findFriendOfFriend(String user,List<List<String>> friends,List<String> friendList){
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for(List<String> target : friends){
+            if(!target.contains(user)){
+                if(!friendList.contains(target.get(0))){
+                    if(hm.get(target.get(0))==null){
+                        hm.put(target.get(0),10);
+                        continue;
+                    }
+                    hm.put(target.get(0),hm.get(target.get(0))+10);
+                }
+                if(!friendList.contains(target.get(1))){
+                    if(hm.get(target.get(1))==null){
+                        hm.put(target.get(1),10);
+                        continue;
+                    }
+                    hm.put(target.get(1),hm.get(target.get(1))+10);
+                }
+            }
+        }
+
+        return hm;
+    }
+
 
     public static List<String> findUserFriend(String user, List<List<String>> friends) {
         List<String> friendList = new ArrayList<>();
