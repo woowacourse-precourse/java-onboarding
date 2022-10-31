37d967000396426a292e0c6e1fa07b329fb78dcb
charlesuu <tiger0192004@gmail.com>
2022-10-31 17:06:58 +0900

    <문제7> - (기능 2).  user와 친구가 아닌 사람들의 목록 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,33 +1,73 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-        ArrayList<String> userFreinds = new ArrayList<>();
-        for (List<String> pair : friends) {
-            if (isUserFreind(user, pair)) {
-                userFreinds.add(getUserFreind(user, pair));
-            }
-        }
-
+        HashSet<String> userFriends = makeUserFriendList(user, friends);
+        HashMap<String, Integer> notUserFriends = makeNotUserFriendList(user, friends, visitors, userFriends);
 
 
         return answer;
     }
-    public static boolean isUserFreind(String user, List<String> pair) {
+
+    public static HashSet<String> makeUserFriendList(String user, List<List<String>> friends) {
+        HashSet<String> userFriends = new HashSet<>();
+
+        for (List<String> pair : friends) {
+            if (isUserFriend(user, pair)) {
+                userFriends.add(getUserFriend(user, pair));
+            }
+        }
+        return userFriends;
+    }
+    public static HashMap<String, Integer> makeNotUserFriendList(String user, List<List<String>> friends, List<String> visitors, HashSet<String> userFriends) {
+        HashMap<String, Integer> notUserFriends = new HashMap<>();
+
+        for (List<String> pair : friends) {
+            if (!isUserFriend(user, pair)) {
+                ArrayList<String> notUserFriend = getNotUserFriend(userFriends, pair);
+                for (String notFriend : notUserFriend) {
+                    notUserFriends.put(notFriend, 0);
+                }
+            }
+        }
+
+        for (String id : visitors) {
+            if (!(userFriends.contains(id) || notUserFriends.containsKey(id))) {
+                notUserFriends.put(id, 0);
+            }
+        }
+
+        if (notUserFriends.containsKey(user)) {
+            notUserFriends.remove(user);
+        }
+
+        return notUserFriends;
+    }
+
+    public static boolean isUserFriend(String user, List<String> pair) {
         for (String id : pair) {
-            if (id == user) {
+            if (id.equals(user)) {
                 return true;
             }
         }
         return false;
     }
-    public static String getUserFreind(String user, List<String> pair) {
-        return pair.get(0) == user ? pair.get(1) : pair.get(0);
+
+    public static String getUserFriend(String user, List<String> pair) {
+        return pair.get(0).equals(user) ? pair.get(1) : pair.get(0);
+    }
+
+    public static ArrayList<String> getNotUserFriend(HashSet<String> userFriends, List<String> pair) {
+        ArrayList<String> notFriendsId = new ArrayList<>(2);
+        for (String id : pair) {
+            if (!userFriends.contains(id)) {
+                notFriendsId.add(id);
+            }
+        }
+        return notFriendsId;
     }
 }
