b819656005c72cfaecf3ffc6fda15a860b413a02
ParangBird <seekerman@naver.com>
2022-10-31 23:15:23 +0900

    feat: 규칙에 맞게 주어진 문자를 변환하는 char convert(char) 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    private static Map<Character, Character> converterMap = new HashMap<>() {
+        {
+            // a -> z, z -> a
+            for (char c = 'a'; c <= 'z'; c++) {
+                put(c, (char) ('z' + 'a' - c));
+            }
+        }
+    };
+
+    /**
+     *
+     * @param plain : 평문
+     * @return char : 암호문
+     */
+    public static char convert(char plain) {
+        return converterMap.get(plain);
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
