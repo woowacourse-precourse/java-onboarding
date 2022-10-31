8a9313de59a29e908e29a50b21bc4fcb36b02ca6
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-11-01 00:46:58 +0900

    FEAT : 화폐 변환 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        final int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int count;
+
+        for(int i=0; i<moneyUnit.length; i++) {
+            count = 0;
+            if(money >= moneyUnit[i]) {
+                count = money / moneyUnit[i];
+                money -= count * moneyUnit[i];
+            }
+            answer.add(count);
+        }
         return answer;
     }
 }
