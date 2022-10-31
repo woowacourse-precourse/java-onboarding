3f492650f80466c9a68a44c442e9dd9e0c389a12
chaeyeong222 <iky1505@naver.com>
2022-10-31 15:44:21 +0900

    문제6번 기능구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,35 +1,31 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
-        HashMap<String, String> map = new HashMap<>();
-        HashMap<Integer, List> checkword = new HashMap<>();
-        ArrayList<String> list = new ArrayList<>();
+        //List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
 
+        String doubleCheck ="";
         for (int i = 0; i < forms.size(); i++) {
+            String c = forms.get(i).get(1); //닉네임
 
-            String check = forms.get(i).get(1);
-            map.put(forms.get(i).get(0), forms.get(i).get(1)); //맵에 넣기
+            for (int j = 0; j < c.length()-1; j++) {
+                doubleCheck = c.substring(j, j + 2); //두글자씩 자르기
 
-            String a =" ";
-            list.clear();
-            for (int j = 0; j < check.length()-1; j++) {
-                a =" ";
-                char [] word = check.toCharArray();
-                a += word[j];
-                a += word[j+1]; //두 글자씩 자름
-                list.add(a);
-                System.out.println("리스트" + list);
+                //자기자신이 아니고, 다른 곳에 포함되어 있다면 answer에 담기
+                for (int k = 0; k < forms.size(); k++) {
+                    if (forms.get(k).get(1).contains(doubleCheck) && forms.get(k).get(1) != c) {
+                        map.put(forms.get(k).get(0), 0);
+                    }
+                }
+            }
 
-            } checkword.put(i, list);
-            System.out.println(checkword);
         }
+        List<String> answer = new ArrayList<>(map.keySet());
+        Collections.sort(answer);
+
         return answer;
     }
 }
