71df019cede78b1fab2d5f134227fee08dcbfbbb
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-10-31 21:14:43 +0900

    Feat(Problem3): 추가 박수 횟수 세기 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+/*
+기능 목록
+1. 1 ~ 10000 까지 수 증가하면서 박수 횟수 새기
+ */
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clapCnt = 0;
+        for(int i = 1; i <= number; i++){
+            clapCnt += addClapCnt(i);
+        }
+        return clapCnt;
+    }
+
+    private static int addClapCnt(int num) {
+        int clapCnt = 0;
+        String[] split = String.valueOf(num).split("");
+        for (String s : split) {
+            int now = Integer.parseInt(s);
+            if(now == 3 || now == 6 || now == 9)
+                clapCnt++;
+        }
+        return clapCnt;
     }
 }
