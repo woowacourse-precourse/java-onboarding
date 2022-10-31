1ffc213cfb75dfaeccef2f5c883be6864dfb1d6f
daeun <gain3436@gmail.com>
2022-11-01 00:36:46 +0900

    [feat](3) 문자 개수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static long countChar(String str, char ch) {
+        return str.chars()
+                .filter(c -> c == ch)
+                .count();
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
