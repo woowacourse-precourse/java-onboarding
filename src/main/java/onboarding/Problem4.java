919466c3a7e11f45e5cacee9609ae7c148179a3c
minju412 <ln8847@ajou.ac.kr>
2022-10-31 22:58:23 +0900

    refactor: [p4] 수치의 상수화

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+	public static final int PRE_a_IN_ASCII_TABLE = 96;
+	public static final int NEXT_z_IN_ASCII_TABLE = 123;
+	public static final int PRE_A_IN_ASCII_TABLE = 64;
+	public static final int NEXT_Z_IN_ASCII_TABLE = 91;
+
 	public static String solution(String word) {
 		char[] charArr = new char[word.length()];
 		int askiiSequence = 0; // 아스키코드 표 기준 앞에서부터 몇 번째 캐릭터인지
@@ -9,11 +14,11 @@ public class Problem4 {
 		for (int i = 0; i < word.length(); i++) {
 			oneLetter = word.charAt(i);
 			if (oneLetter > 'a' && oneLetter < 'z') {
-				askiiSequence = oneLetter - 96;
-				charArr[i] = (char)(123 - askiiSequence); // 뒤에서부터 tmp 번째 캐릭터를 대입
+				askiiSequence = oneLetter - PRE_a_IN_ASCII_TABLE;
+				charArr[i] = (char)(NEXT_z_IN_ASCII_TABLE - askiiSequence); // 뒤에서부터 tmp 번째 캐릭터를 대입
 			} else if (oneLetter > 'A' && oneLetter < 'Z') {
-				askiiSequence = oneLetter - 64;
-				charArr[i] = (char)(91 - askiiSequence);
+				askiiSequence = oneLetter - PRE_A_IN_ASCII_TABLE;
+				charArr[i] = (char)(NEXT_Z_IN_ASCII_TABLE - askiiSequence);
 			} else {
 				charArr[i] = oneLetter;
 			}
