10e34de79ff4a830e33c80831a5d58de921a5498
doehy <dbehxks02@naver.com>
2022-10-31 21:30:59 +0900

    feat : 임의의 문자열을 스택구조로 바꿔주는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        Stack<Character> duplication_stack = changeToStack(cryptogram);
         String answer = "answer";
         return answer;
+    }
+    static Stack<Character> changeToStack(String cryptogram){
+        Stack<Character> temp_stack = new Stack<Character>();
+        for(int i=0; i<cryptogram.length(); i++){
+            temp_stack.push(cryptogram.charAt(i));
+        }
+        return temp_stack;
     }
 }
