5ec563f148a2511a10bb410e6f768269f3c97efc
jisoolee7101@gmail.com <phllo154@naver.com>
2022-10-31 15:48:47 +0900

    매개변수 number 자릿수 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static void main(String[] args) {
+        solution(5);
+        solution(53);
+        solution(297);
+        solution(4444);
+    }
     public static int solution(int number) {
         int answer = 0;
+        int numLength = (int) Math.log10(number)+1;
+        System.out.println(numLength);
+
         return answer;
     }
 }
