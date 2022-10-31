6b83b07b363c32fefbbb3aaf397867854479e85d
subin Park <psb0320@hs.ac.kr>
2022-10-31 18:36:30 +0900

    feat: 연속하는 중복 문자 있는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (isDuplication(cryptogram)) {
+            // 연속하는 중복 문자가 있으면 중복 문자 삭제
+        }
+
         return answer;
+    }
+
+    public static boolean isDuplication(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) return true;
+        }
+        return false;
     }
 }
