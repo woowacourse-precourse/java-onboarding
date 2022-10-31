e46f0f0d0eae1f7a33e72908a89f5d5f5c4410ca
James <rlagurwo6209@naver.com>
2022-10-31 21:27:36 +0900

    문자열 중복제거 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,35 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        answer = DeduplicationString(answer);
+        return answer;
+    }
+
+    static String DeduplicationString(String s){
+        String answer = s;
+
+        while(true) {
+
+            int startIndex = 0;
+            int endIndex = 0;
+
+            for(int i=0;i<answer.length()-1;i++) {
+                if(answer.charAt(i) == answer.charAt(i+1)) {
+                    startIndex = i;
+                    endIndex = i+1;
+                    break;
+                }
+            }
+
+            if(startIndex == 0 && endIndex == 0) {
+                break;
+            }
+            else {
+                answer = answer.substring(0,startIndex)+answer.substring(endIndex+1);
+            }
+        }
+
         return answer;
     }
 }
