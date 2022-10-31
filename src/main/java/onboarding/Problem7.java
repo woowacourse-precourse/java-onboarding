77e982a91043ec88e2a35e26664ed036d4aa9c2d
iamsseaa <homingsea@gmail.com>
2022-10-31 18:45:20 +0900

    feat(Problem7): friends(친구 관계 정보)에서 유저의 친구를 찾는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        HashMap<String, Integer> friendRecommendationMap = new HashMap<>();
+
+        List<String> usersFriends = new ArrayList<>(findUserFriend(friends, user));
+
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static HashSet<String> findUserFriend(List<List<String>> friends, String user) {
+        HashSet<String> usersFriends = new HashSet<>();
+
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                usersFriends.add(friend.get(0));
+                usersFriends.add(friend.get(1));
+            }
+        }
+
+        usersFriends.remove(user);
+        return usersFriends;
     }
 }
