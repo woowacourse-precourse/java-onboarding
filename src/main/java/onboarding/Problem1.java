a155a05dc4758d7ca10f6278ea2ba05941258967
jojaeng2 <ds4ouj@naver.com>
2022-10-31 11:22:55 +0900

    feat : 선택한 페이지가 올바른지 확인하는 기능구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,8 +15,33 @@ class Problem1 {
             this.pages = pages;
         }
 
+        private boolean isInvalidPage() {
+            if (pages.size() != 2) {
+                return true;
+            }
+
+            Integer leftPage = pages.get(0);
+            Integer rightPage = pages.get(1);
+
+            if (isPageEven(leftPage) || isPageNotAdjacent(leftPage, rightPage) || isFirstPage() || isLastPage()) {
+                return true;
+            }
+            return false;
+        }
+        private boolean isFirstPage() {
+            return pages.get(0) <= 1;
+        }
+
+        private boolean isLastPage() {
+            return pages.get(1) >= 400;
+        }
+
         private boolean isPageEven(Integer page) {
             return (page % 2) == 0;
+        }
+
+        private boolean isPageNotAdjacent(Integer leftPage, Integer rightPage) {
+            return (leftPage + 1 != rightPage);
         }
 
         public List<Integer> getPages() {
