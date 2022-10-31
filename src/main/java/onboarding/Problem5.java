a9693fd70c182e6d9424cb38c52a109930a3d244
송민진 <m_6595@naver.com>
2022-11-01 05:45:10 +0900

    #5 feat: get info to update money list

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,28 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = List.of(0, 0, 0, 0, 0, 0, 0, 0, 0);
-
-        Map<Integer, Integer> moneyToIndex = makeConverter();
-        
+        List<Integer> answer = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0));
         return answer;
     }
 
-    private static Map<Integer, Integer> makeConverter() {
-        Map<Integer, Integer> converter = new HashMap<>();
-
-        // 금액을 key값으로 입력하면 알맞는 인덱스를 return해주는 converter 구현
-        List<Integer> tmp = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
-        for(int i = 0; i < tmp.size(); i++) {
-            converter.put(tmp.get(i), i);
+    private static List<Integer> findMoneyToUpdate(int money) {
+        List<Integer> prices = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        for (int price : prices) {
+            if (money >= price) {
+                int index = prices.indexOf(price);
+                int count = money / price;
+                return List.of(index, price, count);
+            }
         }
-        return converter;
+        return List.of();
     }
 }
