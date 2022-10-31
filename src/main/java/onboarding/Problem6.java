c8032fb268f073603968ea0bd6501dc41a65547f
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-10-31 23:19:34 +0900

    [pb6] feat: 중복되는 닉네임 갖는 email 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,9 +6,11 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Map<Character, Set<Character>> charChecker = new HashMap<>();
         initConsecutiveCharSet(charChecker, forms);
-//        System.out.println(charChecker);
-        List<String> a = new ArrayList<>();
-        return a;
+
+        List<String> emails = new ArrayList<>();
+        forms.forEach(item -> addTargetEmails(emails, charChecker, item));
+
+        return emails;
     }
 
     private static void initConsecutiveCharSet(Map<Character, Set<Character>> charChecker,
@@ -34,6 +36,26 @@ public class Problem6 {
                 valueSet.add(nextChar);
             }
             charChecker.put(keyChar, valueSet);
+            i++;
+        }
+    }
+
+    private static void addTargetEmails(List<String> emails,
+                                        Map<Character, Set<Character>> charChecker,
+                                        List<String> user) {
+        String name = user.get(1);
+        int i = 0;
+        boolean stop = false;
+        Character keyChar;
+        Character nextChar;
+        while (i < name.length() - 1 && !stop) {
+            keyChar = name.charAt(i);
+            nextChar = name.charAt(i+1);
+
+            if (charChecker.get(keyChar).contains(nextChar)) {
+                emails.add(user.get(0));
+                stop = true;
+            }
             i++;
         }
     }
