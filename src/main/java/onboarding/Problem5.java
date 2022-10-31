edbbc0203bca1e55c2d7256f158b50a5677589c8
hodomaroo <enghemdwkd@gmail.com>
2022-11-01 04:38:39 +0900

    [Problem5/FEAT] Problem5 구현을 완료하였습니다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,13 +3,12 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+import onboarding.problem5.MoneyParser;
 
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<Integer>();
-
-        return answer;
+        MoneyParser parser = new MoneyParser(50000,10000,5000,1000,500,100,50,10,1);
+        return parser.parseMoney(money);
     }
 }
