cb439df74a0df6c5431b641a6e30925160aa8bc9
yumble <hnjn1101@gmail.com>
2022-10-31 23:16:22 +0900

    Problem 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static Integer[] currencies = new Integer[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        for(int currency : currencies){
+            int count = money / currency;
+            if ( count > 0 ){
+                answer.add(count);
+            }
+            else{
+                answer.add(0);
+            }
+            money %= currency;
+        }
+
         return answer;
     }
 }
