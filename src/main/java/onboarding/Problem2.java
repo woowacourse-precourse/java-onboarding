dce82207ed979567dcd0a8c6da5af67173f615b2
canbeyour <wuzucy@gmail.com>
2022-10-31 21:17:53 +0900

    feat: solve Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,41 @@
 package onboarding;
 
 public class Problem2 {
+    public static int getRepeatEndIndex(StringBuilder answer, int index) {
+        while (index > 0) {
+            if (answer.charAt(index) == answer.charAt(index - 1))
+                return index;
+            index --;
+        }
+        return -1;
+    }
+    public static int getRepeatStartIndex(StringBuilder answer, int repeatIndex) {
+        char repeatChar = answer.charAt(repeatIndex);
+        for (int index = repeatIndex - 1; index >= -1; index--) {
+            if (index == -1 || answer.charAt(index) != repeatChar)
+                return index + 1;
+        }
+        return repeatIndex;
+    }
+    public static boolean removeRepeatChar(StringBuilder answer) {
+        boolean repeat = false;
+        for (int index = answer.length() - 1; index > 0; index--) {
+            int repeatEndIndex = getRepeatEndIndex(answer, index);
+            if (repeatEndIndex == -1)
+                continue;
+            int repeatStartIndex = getRepeatStartIndex(answer, repeatEndIndex - 1);
+            answer.delete(repeatStartIndex, repeatEndIndex + 1);
+            index = repeatStartIndex;
+            repeat = true;
+        }
+        return repeat;
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder(cryptogram);
+        boolean repeat = true;
+        while (repeat) {
+            repeat = removeRepeatChar(answer);
+        }
+        return answer.toString();
     }
 }
