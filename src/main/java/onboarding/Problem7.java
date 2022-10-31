f54faf72f90b142513b9202b939669cd23151e20
tjddnr7760 <tjddnr7760@naver.com>
2022-10-31 10:57:17 +0900

    <7번 - 구현> 사용자 친구 목록 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,22 +1,26 @@
 package onboarding;
 
+import java.lang.reflect.Array;
 import java.util.Collections;
 import java.util.List;
 import java.util.HashMap;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Problem7 problem = new Problem7();
         HashMap<String, Integer> listpeople;
+        ArrayList<String> userfriendslist;
 
-        listpeople = problem.extractPeopleMap(user, friends, visitors);
+        listpeople = problem.extractPeople(user, friends, visitors);
+        userfriendslist = problem.extractUserFriend(user, friends);
 
         return answer;
     }
 
-    public HashMap<String, Integer> extractPeopleMap(String user, List<List<String>> friends, List<String> visitors) {
+    public HashMap<String, Integer> extractPeople(String user, List<List<String>> friends, List<String> visitors) {
         HashMap<String, Integer> result = new HashMap<>();
 
         for(List e : friends) {
@@ -32,5 +36,22 @@ public class Problem7 {
         result.remove(user);
 
         return result;
+    }
+
+    public ArrayList<String> extractUserFriend(String user, List<List<String>> friends) {
+        HashSet<String> resulthash = new HashSet<>();
+        ArrayList<String> resultarray;
+
+        for(List e : friends) {
+            if(e.contains(user)) {
+                resulthash.add((String) e.get(0));
+                resulthash.add((String) e.get(1));
+            }
+        }
+
+        resulthash.remove(user);
+        resultarray = new ArrayList<>(resulthash);
+
+        return resultarray;
     }
 }
