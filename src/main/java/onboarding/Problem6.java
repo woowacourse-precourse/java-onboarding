dc7d5f2ed5225677d455cfc7f04300760a8f6022
alswn <alswn9938@gmail.com>
2022-10-31 19:45:44 +0900

    [Q6] feat : add "userMap 초기화"

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+    public static Map<Integer, String> userMap;
+    public static Map<String, Integer> subStrIndexMap;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static void initUserMap(List<List<String>> forms){
+        for(int index = 0; index<forms.size(); index++){
+            userMap.put(index, forms.get(index).get(0));
+        }
     }
 }
