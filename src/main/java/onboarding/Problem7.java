2e1b5f3cafa4efa5357fcacc43cb8fa5c1d98e3d
sunghyuki <sunghyuk1609@gmail.com>
2022-11-01 01:43:20 +0900

    feat : 함께 아는 친구 목록 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> friendUsers = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                addFriendUsers(user, friend, friendUsers);
+            }
+        }
+    }
+
+    private static void addFriendUsers(String user, List<String> friend, List<String> friendUsers) {
+        for (int i = 0; i < friend.size(); i++) {
+            if (!friend.get(i).equals(user)) {
+                friendUsers.add(friend.get(i));
+            }
+        }
     }
 }
