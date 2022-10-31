42922cb86baad7eb091607f8cabd78110a064a4f
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 02:21:25 +0900

    문제1 : Operator 클래스 내부에 compare 메소드 추가

--- src/main/java/onboarding/problem1/Operator.java
+++ src/main/java/onboarding/problem1/Operator.java
@@ -1,5 +1,7 @@
 package onboarding.problem1;
 
+import java.util.List;
+
 public class Operator {
 
     public static int add(int number) {
@@ -17,5 +19,15 @@ public class Operator {
 
     public static int max(int num1, int num2) {
         return Math.max(num1, num2);
+    }
+
+    public static int compare(List<Integer> pages){
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        int leftResult = max(add(leftPage), multiply(leftPage));
+        int rightResult = max(add(rightPage), multiply(rightPage));
+
+        return max(leftResult,rightResult);
     }
 }
