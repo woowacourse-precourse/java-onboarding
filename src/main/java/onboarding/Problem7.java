1a1cc5725216f0b8267f54d379c572318342b820
WonjunS <seowj0710@gmail.com>
2022-10-31 17:31:25 +1100

    Problem 7: 변수명 변경 및 일부 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,26 +6,28 @@ public class Problem7 {
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>(); // 정답을 return 할 리스트
-        Map<String, ArrayList<String>> map = new HashMap<>(); // key : user / value : user's friends
+        Map<String, ArrayList<String>> friendList = new HashMap<>(); // 각 이용자 별 친구 목록
         Map<String, Integer> scores = new HashMap<>();  // 추천 점수 계산 (user 의 친구는 제외함)
 
         for(List<String> s : friends) {
             String f1 = s.get(0);
             String f2 = s.get(1);
 
-            if(!map.containsKey(f1)) {
-                map.put(f1, new ArrayList<>());
-                map.get(f1).add(f2);
+            // 새로운 이름일 경우
+            if(!friendList.containsKey(f1)) {
+                friendList.put(f1, new ArrayList<>());
+                friendList.get(f1).add(f2);
             }
             else {
-                map.get(f1).add(f2);
+                friendList.get(f1).add(f2);
             }
-            if(!map.containsKey(f2)) {
-                map.put(f2, new ArrayList<>());
-                map.get(f2).add(f1);
+            // 새로운 이름일 경우
+            if(!friendList.containsKey(f2)) {
+                friendList.put(f2, new ArrayList<>());
+                friendList.get(f2).add(f1);
             }
             else {
-                map.get(f2).add(f1);
+                friendList.get(f2).add(f1);
             }
             if(f1.equals(user) || f2.equals(user)) continue;
             if(!scores.containsKey(f1)) scores.put(f1, 0);
@@ -33,29 +35,30 @@ public class Problem7 {
         }
 
         // user 가 아는 친구는 scores 리스트에서 삭제
-        for(String friend : map.get(user)) {
+        for(String friend : friendList.get(user)) {
             scores.remove(friend);
         }
 
         // 함께 아는 친구의 수
-        for(String new_friend : scores.keySet()) {
+        for(String candidate : scores.keySet()) {
             int count = 0;
-            for(String friend : map.get(user)) {
-                if(map.get(friend).contains(new_friend)) count++;
+            for(String friend : friendList.get(user)) {
+                if(friendList.get(friend).contains(candidate)) count++;
             }
-            scores.put(new_friend, (scores.get(new_friend) + (10 * count))); // 기존 점수 + (10 * 함께 아는 친구 수)
+            scores.put(candidate, (scores.get(candidate) + (10 * count))); // 기존 점수 + (10 * 함께 아는 친구 수)
         }
 
         // 방문한 횟수
         for(String visitor : visitors) {
-            if(map.get(user).contains(visitor)) continue; // visitor 가 user 의 친구일 경우
+            if(friendList.get(user).contains(visitor)) continue; // visitor 가 user 의 친구일 경우
             if(!scores.containsKey(visitor)) scores.put(visitor, 1);  // 그동안 언급되지 않았던 user 일 경우
             scores.put(visitor, (scores.get(visitor) + 1)); // 기존 점수 + (1 * 방문 수)
         }
 
-        List<Map.Entry<String, Integer> > list = new LinkedList<Map.Entry<String, Integer>>(scores.entrySet());
+        List<Map.Entry<String, Integer> > scoreList = new LinkedList<>(scores.entrySet());
 
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+        // 새로 만든 리스트를 주어진 조건에 따라 정렬
+        Collections.sort(scoreList, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2)
             {
                 if(o1.getValue() == o2.getValue()) {
@@ -66,12 +69,12 @@ public class Problem7 {
             }
         });
 
-        int cnt = 0;
-        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> aa : list) {
-            if(cnt == 5) break;           // 5명 추천이 끝났으면 실행 종료
-            if(aa.getValue() == 0) break; // 추천 점수가 0점이면 실행 종료
-            answer.add(aa.getKey());      // 정답 리스트에 추천 친구의 이름 추가
+        int cnt = 0; // 추천한 횟수 카운트
+        HashMap<String, Integer> temp = new HashMap<String, Integer>();
+        for (Map.Entry<String, Integer> candidate : scoreList) {
+            if(cnt == 5) break;                  // 5명 추천이 끝났으면 실행 종료
+            if(candidate.getValue() == 0) break; // 추천 점수가 0점이면 실행 종료
+            answer.add(candidate.getKey());      // 정답 리스트에 추천 친구의 이름 추가
             cnt++;
         }
 
