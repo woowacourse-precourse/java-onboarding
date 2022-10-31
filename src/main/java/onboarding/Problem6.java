c200c9791344b770ea35c3d2bcd1fa7d92c526c6
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-01 04:26:50 +0900

    Problem6 리팩토링 및 버전정보 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,7 +3,7 @@
  * - 문제 6
  *
  * 버전정보
- * - openjdk version "11.0.15"
+ * - jdk-11.0.17
  *
  * 날짜
  * - 2022-10-31
@@ -21,17 +21,22 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        HashMap<String, Integer> nickNameCheck = new HashMap<String, Integer>();
+        // 닉네임 중복 체크 맵
+        HashMap<String, Integer> duplicationCheck = new HashMap<String, Integer>();
 
         for (List<String> form : forms) {
             String nickName = form.get(1);
-            nickNameCheckSet(nickNameCheck, nickName);
+
+            // 중복인지 확인하기 위한 세팅
+            setDuplicationCheck(duplicationCheck, nickName);
         }
 
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickName = form.get(1);
-            if (overlapCheck(nickNameCheck, nickName)) {
+
+            // 중복 확인
+            if (checkDuplication(duplicationCheck, nickName)) {
                 answer.add(email);
             }
         }
@@ -41,27 +46,32 @@ public class Problem6 {
         return answer;
     }
 
-    private static boolean overlapCheck(HashMap<String, Integer> nickNameCheck, String nickName) {
+    private static boolean checkDuplication(HashMap<String, Integer> duplicationCheck, String nickName) {
+
+        // 닉네임으로 만들 수 있는 연속적인 두 문자를 전부 만들어서 확인 -> 2이상이면 중복
         for (int i = 0; i < nickName.length() - 1; i++) {
             String substring = nickName.substring(i, i + 2);
 
-            if(nickNameCheck.get(substring)==null) continue;
+            if(duplicationCheck.get(substring)==null) continue;
 
-            if (nickNameCheck.get(substring) > 1) {
+            if (duplicationCheck.get(substring) > 1) {
                 return true;
             }
         }
         return false;
     }
 
-    private static void nickNameCheckSet(HashMap<String, Integer> nickNameCheck, String nickName) {
+
+    private static void setDuplicationCheck(HashMap<String, Integer> duplicationCheck, String nickName) {
+
+        // 닉네임으로 만들 수 있는 연속적인 두 문자를 전부 만들어서 중복체크 맵에 카운트
         for (int i = 0; i < nickName.length() - 1; i++) {
             String substring = nickName.substring(i, i + 2);
 
-            if (nickNameCheck.get(substring) == null) {
-                nickNameCheck.put(substring, 1);
+            if (duplicationCheck.get(substring) == null) {
+                duplicationCheck.put(substring, 1);
             } else{
-                nickNameCheck.put(substring, nickNameCheck.get(substring) + 1);
+                duplicationCheck.put(substring, duplicationCheck.get(substring) + 1);
             }
         }
     }
