6f62a09cc1bae1045d3034dddc4ef1845611a397
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 15:29:46 +0900

    Feat(Problem1) : getEachDigit 메서드 추가 페이지 번호의 각 자리 숫자를 구하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static List<Integer> getEachDigit(Integer pageNum) {
+        List<Integer> eachDigit = new ArrayList<>();
+        String[] stringDigitArray = pageNum.toString().split("");
+        for(String digit : stringDigitArray) {
+             eachDigit.add(Integer.parseInt(digit));
+        }
+        return eachDigit;
     }
 }
