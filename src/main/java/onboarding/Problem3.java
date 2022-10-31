d791b1bf2a4addb5589dd7336fd924e6cc0fc3d9
ddukddaki <rlfrkdms1@naver.com>
2022-10-31 23:48:55 +0900

    comment(Problem3): 구현 기능 목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 한 숫자에 대해 3, 6, 9 각각의 개수를 세는 기능  —> countNumber()
+ *     : 한 숫자 내에서 숫자의 길이만큼 반복문을 돌려 charAt을 사용해 3,6,9와 같은지 판별
+ * 2. 모든 숫자에 대한 3,6,9 개수를 합산하는 기능 —> solution()
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
