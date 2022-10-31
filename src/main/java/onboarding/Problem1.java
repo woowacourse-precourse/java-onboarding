478f0103c78d5186aafe9a9a75c79418e27847a7
NotableDeveloper <castlereal@naver.com>
2022-10-30 23:02:10 -0700

    Problem1 - create mult_digit method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,8 +7,10 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
 
         int pobi_sumMax = Integer.max(sum_digit(pobi.get(0)), sum_digit(pobi.get(1)));
+        int pobi_multMax = Integer.max(mult_digit(pobi.get(0)), mult_digit(pobi.get(1)));
+        int pobi_max = Integer.max(pobi_sumMax, pobi_multMax);
 
-        return pobi_sumMax;
+        return pobi_max;
     }
     // 주어진 숫자의 각 자리 수를 더하여 반환
     private static int sum_digit(int value){
@@ -21,6 +23,18 @@ class Problem1 {
         } while(value != 0);
 
         return sum;
+    }
+
+    private static int mult_digit(int value){
+        int temp, mult = 1;
+
+        do{
+            temp = value % 10;
+            mult *= temp;
+            value /= 10;
+        } while(value != 0);
+
+        return mult;
     }
 
 
