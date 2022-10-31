26ba1844545a05c485bbaaf0ec8c415f663ba241
wjh2012 <wjh2012@naver.com>
2022-10-31 18:55:50 +0900

    (PROBLEM4)문자열을 문자 배열로 바꾸어 한글자씩 기능순환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        char [] arrWord = word.toCharArray();
+
+        for (int i=0;i< arrWord.length;i++){
+            arrWord[i] = converse(arrWord[i]);
+        }
+
+        return new String(arrWord);
+    }
+
+    // 문자를 받아, 아스키코드로 바꾸고, 알파벳일 경우 뒤집어서 반환
+    private static char converse(char c){
+        int ascii = (int)c;
+
+        if (ascii>=65 && ascii<=122){
+            if (ascii <= 90){
+                ascii = 90+65-ascii;
+            }
+            else{
+                ascii = 122+97-ascii;
+            }
+        }
+
+        return (char)ascii;
     }
 }
