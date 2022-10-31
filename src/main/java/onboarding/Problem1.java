6305a26d90877cb39f7699443c4b4e746df33782
박세원 <tpdnjs1825@naver.com>
2022-11-01 01:14:38 +0900

    Feat : page 번호로 점수의 Max 값을 구하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int LEFT_PAGE = 0;
+    private static final int RIGHT_PAGE = 1;
+    private static int getMaxScore(int page){
+        int sum = 0;
+        int multiplication = 1;
+        while(page > 0){
+            sum += page % 10;
+            multiplication *= page % 10;
+            page /= 10;
+        }
+        return Math.max(sum, multiplication);
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiMaxScore = Integer.MIN_VALUE;
+        int crongMaxScore = Integer.MIN_VALUE;
+
+        pobiMaxScore = Math.max(getMaxScore(pobi.get(LEFT_PAGE)), getMaxScore(pobi.get(RIGHT_PAGE)));
+        crongMaxScore = Math.max(getMaxScore(crong.get(LEFT_PAGE)), getMaxScore(crong.get(RIGHT_PAGE)));
+
         return answer;
     }
 }
