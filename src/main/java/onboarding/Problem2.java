48f33e4859636a6572130019f8682e72a9125f75
TaeHwan <kimta2hwan@gmail.com>
2022-11-01 06:26:40 +0900

    feat(PROBLEM2): 중복이 끝나는 지점을 구하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+
+    static StringBuilder beforeRemoval = new StringBuilder();
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    static int findOverlapEndPoint(char nowChar, int index) {
+        char nextChar = beforeRemoval.charAt(index);
+        int length = beforeRemoval.length() - 1;
+
+        while (index < length && nowChar == nextChar) {
+            nextChar = beforeRemoval.charAt(++index);
+        }
+
+        return index;
     }
 }
