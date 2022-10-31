4502fec089b328d2d3adecb1a760045a25b03dce
ouohoon <begreat@kakao.com>
2022-11-01 04:20:01 +0900

    리팩토링: 문제4 기능 목록 주석 추가, FrogDictionary 클래스 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,41 +1,19 @@
 package onboarding;
 
+import onboarding.problem4.FrogDictionary;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Problem4 {
+    /*
+    기능목록
+    1. 청개구리 사전 초기화 기능
+    2. 청개구리 문자열 변환 기능
+     */
     public static String solution(String word) {
         FrogDictionary frogDictionary = new FrogDictionary();
 
         return frogDictionary.convertToFrogString(word);
-    }
-
-    // 청개구리 사전 클래스
-    private static class FrogDictionary {
-        private final int ALPHABET_SIZE = 26;
-        private Map<Character, Character> dictionary;
-
-        public FrogDictionary() {
-            initDictionary();
-        }
-
-        // 청개구리 사전 규칙에 맞게 초기화한다.
-        private void initDictionary() {
-            dictionary = new HashMap<>();
-            for (int i = 0; i < ALPHABET_SIZE; i++) {
-                dictionary.put((char) ('A' + i), (char) ('Z' - i));
-                dictionary.put((char) ('a' + i), (char) ('z' - i));
-            }
-        }
-
-        // 사전을 이용해서 청개구리 문자열로 변환한다.
-        public String convertToFrogString(String string) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < string.length(); i++) {
-                stringBuilder.append(dictionary.getOrDefault(string.charAt(i),
-                                                            string.charAt(i)));
-            }
-            return stringBuilder.toString();
-        }
     }
 }
