8557989073aeb38c4e310a319aa8da1262f3b4ea
bjk1649 <qhwns1649@naver.com>
2022-11-01 03:25:13 +0900

    feat: 친구 관계 배열[아이디A, 아이디B] 를 받아 특정 유저의 친구인지 확인 후 친구라면 관계 배열 안에서의 인덱스를 반환해주는 메소드  추가
    클래스 안에 추후 사용할만한 상수도 추가
    만약 친구가 아니라면 -1 반환 (추후 이용하기 위함)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+}
+
+class FriendInfoController {
+    static final int MAX_RECOMMEND_SIZE = 5;
+    static final int SAME_FRIEND_POINT = 10;
+    static final int VISITOR_POINT = 1;
+
+    private static int getUsersFriendIndex(String user, List<String> friendsRelation) {
+        if (friendsRelation.get(0).equals(user)) {
+            return 1;
+        }
+        if (friendsRelation.get(1).equals(user)) {
+            return 0;
+        }
+        return -1;
     }
 }
