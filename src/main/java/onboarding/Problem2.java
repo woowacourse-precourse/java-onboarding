79590bf399c8afb2fefb328b388922aca34277c4
yejin <kwonyj1022@naver.com>
2022-10-31 13:44:55 +0900

    Problem2 뼈대

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        try{
+            validation(cryptogram);
+        }
+        catch (IllegalArgumentException e) {
+            e.getMessage();
+        }
+
+        char[] cryptogramArr = cryptogram.toCharArray();
+        String answer = deleteDuplicate(cryptogramArr);
         return answer;
+    }
+
+    public static void validation(String str) {
+
+    }
+
+    public static String deleteDuplicate(char[] charArr) {
+
     }
 }
