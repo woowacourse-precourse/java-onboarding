ad9f338c25cf4e5ff6f8e2affde16e0d58790fa5
한은영 <oneeqzero@gmail.com>
2022-10-31 12:45:36 +0900

    feat: problem7 친구관계 살펴보기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -14,6 +14,28 @@ public class Problem7 {
             return set;
         }
     };
+    static Map<String, Integer> fscore = new HashMap<String, Integer>();
+
+    public static Integer common_friends(String user, String key){
+        int ret = 0;
+        Set<String> users = friend_list.get(user);
+        for(String ufriend : users){
+            if(friend_list.get(key).contains(ufriend)) ret++;
+        }
+        return ret;
+    }
+    public static void cal_fscore(String user){
+        Iterator iter = friend_list.keySet().iterator();
+        while(iter.hasNext()){
+            String k = iter.next().toString();
+            if(k.equals(user)) continue;
+            if(friend_list.get(user).contains(k)) continue;
+
+            Integer score = fscore.get(k);
+            if(score == null) fscore.put(k,10*common_friends(user,k));
+            else fscore.put(k,score + 10*common_friends(user,k));
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
@@ -23,6 +45,7 @@ public class Problem7 {
             friend_list.get(f1).add(f2);
             friend_list.get(f2).add(f1);
         }
+        cal_fscore(user);
 
         return answer;
     }
