0d79652cbfc9a9c5fa47e265f24b14ec151b8baa
inhyeok <ss112d86@gmail.com>
2022-11-01 07:57:57 +0900

    feat: Problem7 친구 관계정보를 매핑하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+  private static HashMap<String, Set<String>> friendsShip = new HashMap<>();
+
+  public static List<String> solution(String user, List<List<String>> friends,
+      List<String> visitors) {
+    setFriendsShip(friends);
+    System.out.println(friendsShip);
+    return new ArrayList<>();
+  }
+
+  private static void setFriendsShip(List<List<String>> friends) {
+    for (List<String> friend : friends) {
+      String userA = friend.get(0);
+      String userB = friend.get(1);
+      Set<String> userAValue = friendsShip.getOrDefault(userA, new HashSet<>());
+      userAValue.add(userB);
+      friendsShip.put(userA, userAValue);
+
+      Set<String> userBValue = friendsShip.getOrDefault(userB, new HashSet<>());
+      userBValue.add(userA);
+      friendsShip.put(userB, userBValue);
     }
+  }
 }
