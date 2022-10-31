9c5f53ce5aad6abb3599be73ef43be5e9e191f18
ChoiYeonho0903 <903yh@naver.com>
2022-11-01 02:20:11 +0900

    Problem7 : UserScore class 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -15,6 +15,54 @@ public class Problem7 {
         return userScore.getTop5();
     }
 
+    private static class UserScore {
+        private String user;
+        private List<String> userFriends;
+        private Map<String, Integer> score = new HashMap<>();
+
+        public UserScore(String user, Map<String, List<String>> relations, List<String> visitors) {
+            this.user = user;
+            this.userFriends = relations.get(user);
+            addAcquaintanceScore(user, relations, userFriends);
+            addVisitorScore(visitors, userFriends);
+        }
+
+        public List<String> getTop5() {
+            return score.keySet()
+                .stream()
+                .sorted((String s1, String s2) -> {
+                    if(score.get(s1).equals(score.get(s2))){
+                        return s1.compareTo(s2);
+                    }
+                    return score.get(s2) - score.get(s1);
+                })
+                .limit(5)
+                .collect(Collectors.toList());
+        }
+
+        private void addAcquaintanceScore(String user, Map<String, List<String>> relations, List<String> userFriends) {
+            Set<String> keys = relations.keySet();
+            for(String key : keys) {
+                int cnt = 0;
+                if(key.equals(user) || userFriends.contains(key)) continue;
+                for(String friend : relations.get(key)) {
+                    if(userFriends.contains(friend)) {
+                        cnt++;
+                    }
+                }
+                if(cnt != 0) {
+                    score.put(key, 10*cnt);
+                }
+            }
+        }
+
+        private void addVisitorScore(List<String> visitors, List<String> userFriends) {
+            for(String visitor : visitors) {
+                if(userFriends.contains(visitor)) continue;
+                score.put(visitor, score.getOrDefault(visitor, 0)+1);
+            }
+        }
+    }
 
     private static class UserRelation {
         private String user;
