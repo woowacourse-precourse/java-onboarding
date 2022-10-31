909614d56474ed019c001e99c3788257c8e82879
채희찬 <eqholic1125@gmail.com>
2022-11-01 01:19:37 +0900

    feat: 친구 찾기 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static List<String> userFriends = new ArrayList<>();
+    private static Map<String, Integer> recommendDict = new HashMap<String, Integer>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
     }
+
+    private static void findFriends(String user, List<List<String>> friends) {
+
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).get(0) == user) {
+                userFriends.add(friends.get(i).get(1));
+            } else if (friends.get(i).get(1) == user) {
+                userFriends.add(friends.get(i).get(0));
+            }
+        }
+
+    }
+
+
 }
