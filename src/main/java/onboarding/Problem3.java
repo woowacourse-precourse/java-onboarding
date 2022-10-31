fb523a5267697d97d500f20ebbfc6c3b99eebc5c
이정우 <acg6138@naver.com>
2022-10-31 22:23:18 +0900

    feat(Problem3): 문자열에서 특정 문자의 개수를 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,9 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    public static long countChar(String str, char num) {
+        return str.chars()
+                .filter(c -> c == num)
+                .count();
+    }
 }
