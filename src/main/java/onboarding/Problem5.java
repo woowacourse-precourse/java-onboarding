75c43d2c45767571bcf0a7a24df4cedddb768021
D <doxxx93@gmail.com>
2022-11-01 04:24:16 +0900

    Problem5 (#12)
    * docs: add requirement checklist
    
    * feat: solve problem5
    
    * refactor: remove unnecessary loop

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,19 +5,14 @@ import java.util.List;
 
 public class Problem5 {
 
+    private static final int ZERO = 0;
     private static final int[] CURRENCY = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-        while (money > 0) {
-            for (int unit : CURRENCY) {
-                if (money >= unit) {
-                    answer.add(money / unit);
-                    money = money % unit;
-                    continue;
-                }
-                answer.add(0);
-            }
+        List<Integer> answer = new ArrayList<>(List.of(ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO));
+        for (int index = ZERO; index < CURRENCY.length; index++) {
+            answer.set(index, money / CURRENCY[index]);
+            money %= CURRENCY[index];
         }
         return answer;
     }
