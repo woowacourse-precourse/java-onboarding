71dc376a4ffd3870e4558e0bf47c07e9f94aab6b
Nugur1i <duckhee092@naver.com>
2022-10-31 18:54:21 +0900

    feat<problem3> : Add method to find each digit (Same with problem 1 method)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Problem3 {
     public static int solution(int number) {
+        int answer = 0;
+
 
         int count1To10 = 3;
         int count1To100 = count1To10*10 + 10*3; //60
@@ -13,13 +15,23 @@ public class Problem3 {
         ArrayOfCount.add(count1To100);
         ArrayOfCount.add(count1To1000);
 
+        ArrayList<Integer> numberEachDigitArray = new ArrayList<>();
+
+        while (number > 0) {
+
+            numberEachDigitArray.add(number%10);
+            number /= 10;
+
+        }
 
 
 
 
 
 
-        int answer = 0;
+
+
+
         return answer;
     }
 }
