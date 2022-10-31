2d887ae91566adfe16e5b3cc1effb0e21fd45fd3
jindaram-stu <jinsb1999@naver.com>
2022-10-31 23:08:18 +0900

    feat : [문제3] 각각 3,6,9의 조건을 1번의 기능에 대입하여 그 더한 값을 반환하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int resultClap = 0;
+        for (int i=1; i<=number; i++) {
+            String stringNumber = String.valueOf(i);
+            int threeCount = count(stringNumber,"3");
+            int sixCount = count(stringNumber,"6");
+            int nineCount = count(stringNumber,"9");
+
+            resultClap += threeCount + sixCount + nineCount;
+        }
         return resultClap;
     }
 
