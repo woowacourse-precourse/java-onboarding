fca71dde9f3a21d2fdbcf319252f7f0c9ba398c1
phobyjun <phobyjun@gmail.com>
2022-10-31 19:49:28 +0900

    [PROBLEM6] ADD: FEATURE1. 크루 닉네임에서 2글자 부분 문자열 반환 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static List<String> getSubstrFromNickname(String nickname) {
+        List<String> substrings = new ArrayList<>(List.of());
+        for (int idx = 0; idx < nickname.length() - 1; idx++) {
+            substrings.add(nickname.substring(idx, idx + 2));
+        }
+        return substrings;
     }
 }
