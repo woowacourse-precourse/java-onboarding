56f0d70aae163dc1d37fe0eefe0c1749a7169d4d
ParangBird <seekerman@naver.com>
2022-10-31 23:50:59 +0900

    refactor: 예외처리(coinList <= 0인 경우)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -24,6 +24,9 @@ public class Problem5 {
     public static List<Integer> getChangeCoinList(int money, int[] coinList) {
         List<Integer> answer = new ArrayList<>();
         for (int i = 0; i < coinList.length; i++) {
+            if (coinList[i] <= 0) {
+                break;
+            }
             answer.add(money / coinList[i]);
             money -= money / coinList[i] * coinList[i];
         }
