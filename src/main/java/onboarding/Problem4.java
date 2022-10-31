9cb5d34044733592c774d3cfeb1086106b4ec235
Hyes-y <gptjs7970@naver.com>
2022-11-01 01:27:29 +0900

    update: Problem4 주석 추가 및 코드 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,15 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+    /**
+     * 입력 문자열을 주어진 조건대로 변환
+     * 조건 1) A - Z, B - Y, C - X , ... , Z - A 로 변경
+     * 조건 2) 대문자는 대문자, 소문자는 소문자로 변경
+     * 조건 3) 알파벳 이외의 문자는 변환 X
+     * @param word
+     * @return converted word
+     */
     public static String solution(String word) {
         String answer = "";
         char[] chars = word.toCharArray();
         for (char v: chars) {
             int ascii = (int)v;
             char ch = v;
+            // A - Z에 속하는 경우
+            // 변환 전 아스키 코드 + 변환 후 아스키 코드 = 155
             if (ascii >= 65 && ascii <= 90) {
                 ch = (char)(155 - ascii);
-            } else if (ascii >= 97 && ascii <= 122) {
+            }
+            // a - z에 속하는 경우
+            // 변환 전 아스키 코드 + 변환 후 아스키 코드 = 219
+            else if (ascii >= 97 && ascii <= 122) {
                 ch = (char)(219 - ascii);
             }
 
@@ -18,5 +31,4 @@ public class Problem4 {
         return answer;
     }
 }
-// A: 65, Z: 90
-// a: 97, z: 122
+
