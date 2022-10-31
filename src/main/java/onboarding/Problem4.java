9b6f53b34f61ff1d81bf5ca0a8a171787e96c79f
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-01 02:34:25 +0900

    feat : 문자열을 변환 - 문자열을 한글자씩 비교한다. - 글자의 대소문자 상태를 확인한다. - 청개구리 사전에서 변환되어하는 값을 확인한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,22 +1,36 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem4 {
+
+    static String[] vocabulary = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
     public static String solution(String word) {
-        String answer = "";
+        String answer = transferString(word);
         return answer;
     }
 
     private static String transferString(String word) {
         String transferString = "";
-        String[] splitWord = splitWord(word);
+        String[] transferWord = new String[word.length()];
 
-        for (int i = 0; i < splitWord.length; i++) {
-
+        for (int i = 0; i < word.length(); i++) {
+            char transferChar = word.charAt(i);
+            // 대문자인 경우
+            if (Character.isUpperCase(transferChar)) {
+                transferWord[i] = transferChar(String.valueOf(transferChar)).toUpperCase();
+            }
+            // 소문자인 경우
+            else if (Character.isLowerCase(word.charAt(i))) {
+                transferWord[i] = transferChar(String.valueOf(transferChar)).toLowerCase();
+            }
         }
         return transferString;
     }
 
-    private static String[] splitWord(String word) {
-        return word.split("");
+    private static String transferChar(String transferChar) {
+        int tIndex = Arrays.asList(vocabulary).indexOf(transferChar);
+        return vocabulary[vocabulary.length - 1 - tIndex];
     }
 }
