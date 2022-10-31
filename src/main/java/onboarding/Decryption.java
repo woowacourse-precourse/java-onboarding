35b80c19362e5b96b3dddea819e2073f229fbe03
This2sho <zx00018@naver.com>
2022-11-01 07:47:02 +0900

    feat: 암호 중복 문자 제거 구현

--- src/main/java/onboarding/Decryption.java
+++ src/main/java/onboarding/Decryption.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Decryption {
 	private static final int MAX_INPUT_LENGTH = 1000;
 	private static final int MIN_INPUT_LENGTH = 1;
@@ -26,5 +32,32 @@ public class Decryption {
 		if (!StringPatterUtils.isFormedLowerCase(cryptogram)) {
 			throw new IllegalArgumentException("암호문은 알파벳 소문자로만 이루어져야 합니다.");
 		}
+	}
+
+	public String decipher() {
+		StringBuilder stringBuilder = new StringBuilder();
+		Deque<Character> deque = getNonDuplicateCharacter();
+		while (!deque.isEmpty()) {
+			Character obj = deque.pollFirst();
+			stringBuilder.append(obj);
+		}
+		return stringBuilder.toString();
+	}
+
+	private Deque<Character> getNonDuplicateCharacter() {
+		Deque<Character> deque = new LinkedList<>();
+		char[] chars = cryptogram.toCharArray();
+		deque.add(chars[0]);
+		char pre = chars[0];
+		for (int i = 1; i < chars.length; i++) {
+			if (pre != chars[i]) {
+				deque.addLast(chars[i]);
+				pre = chars[i];
+				continue;
+			}
+			pre = chars[i];
+			deque.pollLast();
+		}
+		return deque;
 	}
 }
--- src/test/java/onboarding/DecryptionTest.java
+++ src/test/java/onboarding/DecryptionTest.java
@@ -30,4 +30,32 @@ public class DecryptionTest {
 		assertThatThrownBy(() -> new Decryption(cryptogram3))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
+
+	@Test
+	public void 암호_해독_테스트() {
+		//given
+		String cryptogram = "browoanoommnaon";
+		String cryptogram2 = "browoannaon";
+		String cryptogram3 = "browoaaon";
+		String cryptogram4 = "browoon";
+
+		//when
+		Decryption decryption = new Decryption(cryptogram);
+		Decryption decryption2 = new Decryption(cryptogram2);
+		Decryption decryption3 = new Decryption(cryptogram3);
+		Decryption decryption4 = new Decryption(cryptogram4);
+
+		//then
+		String result = decryption.decipher();
+		assertThat("browoannaon").isEqualTo(result);
+
+		String result2 = decryption2.decipher();
+		assertThat("browoaaon").isEqualTo(result2);
+
+		String result3 = decryption3.decipher();
+		assertThat("browoon").isEqualTo(result3);
+
+		String result4 = decryption4.decipher();
+		assertThat("brown").isEqualTo(result4);
+	}
 }
