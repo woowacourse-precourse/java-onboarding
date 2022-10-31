3132ad7659b530bbb1d69207f043c1e13e8398dc
lecocococo <chemwang13@naver.com>
2022-11-01 03:54:13 +0900

    Refactor: Problem4의 기능 구현 목록의 변화에 따른 기능 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,37 +5,23 @@ import java.util.Map;
 
 public class Problem4 {
 
-	public static Map<Character, Character> createFrogDictionary() {
-		Map<Character, Character> frogDictionary = new HashMap<>();
-		int tempNumber = 25;
-
-		for(int i = 65; i < 91; i++) {
-			frogDictionary.put((char)i, (char)(i + tempNumber));
-			frogDictionary.put((char)(i + 32), (char)(i + 32 + tempNumber));
-			tempNumber -= 2;
+	public static char createReversedCharacter(char alphabet) {
+		if (Character.isUpperCase(alphabet)) {
+			return (char) ('Z' - alphabet + 'A');
 		}
-		return frogDictionary;
-	}
-
-	public static String createReversedWord(String word, Map<Character, Character> frogDictionary){
-		String reversedWord = "";
-
-		for(int i =0; i<word.length(); i++) {
-			if (frogDictionary.get(word.charAt(i)) != null) {
-				reversedWord += frogDictionary.get(word.charAt(i));;
-			}
-			if (frogDictionary.get(word.charAt(i)) == null) {
-				reversedWord += word.charAt(i);
-			}
+		if (Character.isLowerCase(alphabet)) {
+			return (char) ('z' - alphabet + 'a');
 		}
-		return reversedWord;
+		return alphabet;
 	}
 
     public static String solution(String word) {
         String answer = "";
-		Map<Character, Character> frogDictionary = createFrogDictionary();
 
-		answer = createReversedWord(word, frogDictionary);
+		for (char alphabet : word.toCharArray()) {
+			answer += createReversedCharacter(alphabet);
+		}
+
 		return answer;
     }
 }
