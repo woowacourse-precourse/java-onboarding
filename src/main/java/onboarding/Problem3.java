5b51e9f332cbd9c15233d73bdeee62826f42a9cf
ckn1070 <ckn1070@naver.com>
2022-10-31 20:30:09 +0900

    Solve the problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem3 {
+    static int numCounter(int curNum){
+        int count = 0;
+        while(curNum > 1){
+            int quotient = curNum % 10;
+            if(quotient == 3 || quotient == 6 || quotient == 9){
+                count += 1;
+            }
+            curNum /= 10;
+        }
+        return count;
+    }
+    static int clapCounter(int clapNum){
+        int count = 0;
+        while(clapNum>0){
+            count += numCounter(clapNum);
+            clapNum -= 1;
+        }
+        return count;
+    }
     public static int solution(int number) {
-        int answer = 0;
+        int answer = clapCounter(number);
         return answer;
     }
 }
