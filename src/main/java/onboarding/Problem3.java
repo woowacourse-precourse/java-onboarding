e43bde6525c90e300f1f2666c75985c6887ea1bc
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-10-31 21:28:58 +0900

    feat: Problem 4

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
+
+    public static int solution(int number)
+    {
         int answer = 0;
+        // 1의 자리
+        // 3,6,9 일때
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(13));
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,41 @@
 package onboarding;
 
 public class Problem4 {
+    private static String answer = "";
+    public static void convUpper(char c)
+    {
+        answer+=(char) ('A' + 'Z' -c);
+    }
+    public static  void convLower(char c)
+    {
+        answer +=(char) ('a'+'z' - c);
+    }
+    public static boolean checkUpper (char c)
+    {
+        return c>= 'A' && c<='Z';
+    }
+    public static boolean checkLower (char c)
+    {
+        return c>= 'a' && c<='z';
+    }
+
     public static String solution(String word) {
-        String answer = "";
+        for (int i = 0 ; i< word.length(); i++)
+        {
+            if (checkUpper(word.charAt(i))) {
+                convUpper(word.charAt(i));
+            }
+            else if (checkLower(word.charAt(i)))
+            {
+                convLower(word.charAt(i));
+            }
+            else
+                answer+=word.charAt(i);
+        }
         return answer;
     }
+
+  /*  public static void main(String[] args) {
+        System.out.println(solution("I love you"));
+    }*/
 }
