d840535e6e9aa010cf0563a196ebed8052f5e24e
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-10-31 19:24:35 +0900

    Add : visits 방문자들을 확인하며 map에 유저정보가 없는 경우 넣어주고 있는 경우 추천점수를 1 늘려주는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -18,6 +18,15 @@ public class Problem7 {
         map.get(left).knowing.add(right);
         map.get(right).knowing.add(left);
     }
+
+    // 주어진 방문자 정보들을 친구 관계 목록 map에 추가하고 같은 방문자는 점수를 올리는 함수
+    public static void setVisitorsToMap(String visitUser, Map<String, Info> map){
+        if(!map.keySet().contains(visitUser)){
+            map.put(visitUser, new Info(1, new ArrayList<>()));
+        }else{
+            map.get(visitUser).point += 1;
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
