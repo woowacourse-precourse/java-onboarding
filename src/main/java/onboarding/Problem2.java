dcbb90f525e53487ab01aa8fab078d37feb57f13
kyo705 <mission0705@naver.com>
2022-11-01 02:05:27 +0900

    [UPDATE] Problem2
    기존 StringBuilder의 remove활용에서 현재 stack 방식으로 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,22 +1,27 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        StringBuilder sb = new StringBuilder(cryptogram);
+        StringBuilder sb = new StringBuilder();
         
-        for(int i=1;i<sb.length();) {
-        	char left_char = sb.charAt(i-1);
-        	char right_char = sb.charAt(i);
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<cryptogram.length();i++) {
+        	char cur_char = cryptogram.charAt(i);
         	
-        	if(left_char == right_char) {
-        		sb.delete(i-1, i+1);
-        		if(i!=1) {
-        			i--;
-        		}
+        	if(stack.size()!=0 && stack.peek()==cur_char) {
+        		stack.pop();
         	}else {
-        		i++;
+        		stack.add(cur_char);
         	}
         }
+        
+        while(!stack.isEmpty()) {
+        	sb.append(stack.pop());
+        }
+        
+        sb.reverse();
         
         return sb.toString();
     }
