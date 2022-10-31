25273cb5272d83a0657681c8a270a9ae7656c7ba
ykmxxi <ykmxxi97@gmail.com>
2022-10-31 22:01:29 +0900

    feat: (Problem7)user의 친구목록을 얻는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,17 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<String> getFriendList(String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
+        int rowLength = friends.size();
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                friendList.addAll(friend);
+            }
+        }
+        friendList.removeAll(Collections.singleton(user));
+        return friendList;
     }
 }
