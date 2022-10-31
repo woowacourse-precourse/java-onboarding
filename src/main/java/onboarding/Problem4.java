fa5b03fbd3a5e6da01456c7088ee5ae3f15fbd7c
YUSEONJUN <seonjun0906@gmail.com>
2022-11-01 00:24:12 +0900

    [Problem 4] 기능 요구 사항 1 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int[] result = new int[word.length()];
+        int[] result1 = new int[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            result[i] = (int)(word.charAt(i));
+
+        }
+
+
         return answer;
+
     }
-}
