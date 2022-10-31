162b3d6c965bcedd78428a3fd9f78f9bfb3e1c43
bumwoo <gld55602@gmail.com>
2022-10-31 15:41:52 +0900

    feat(Problem5): 화폐 변환 기능 구현
    화폐 종류 배열 생성 후 ArrayList에 추가
    money를 나머지 값으로 치환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] bills = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int bill : bills) {
+            answer.add(money / bill);
+            money %= bill;
+        }
+
         return answer;
     }
+
 }
