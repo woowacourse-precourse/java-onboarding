3ed5d2bfae964f6e3f9f531a1b2253b5fa02265b
hongjinwon <herenever@naver.com>
2022-11-01 04:08:49 +0900

    feat: problem5 solution 구현
    - problem5 solution 구현
    - 추가 리팩토링 필요

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,8 +7,7 @@ import java.util.List;
 public class Problem5 {
     private static final List<Integer> moneyUnits = List.of(50_000,10_000,5_000,1_000,500,100,50,10,1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return withdraw(money);
     }
 
     public static List<Integer> withdraw(int money){
