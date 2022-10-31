29514c0038a42f6f8ad42f2169f5b3da9153cb6d
pdot715 <pdot715@gmail.com>
2022-10-31 21:50:33 +0900

    feat(문제4):엄마 말씀을 반대로 변환(청개구리 사전 이용) 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); i++) {
+            char now_char = word.charAt(i);
+            // 알파벳 소문자면 청개구리 사전 참고하여 반대로 변환한 대문자 넣기
+            if(now_char >= 'a' && now_char <= 'z') {
+                now_char = (char) ('z' - now_char + 'a');
+                answer += now_char;
+            } // 알파벳 대문자면 청개구리 사전 참고하여 반대로 변환한 소문자 넣기
+            else if(now_char >= 'A' && now_char <= 'Z') {
+                now_char = (char) ('Z' - now_char + 'A');
+                answer += now_char;
+            } // 알파벳이 아니면 그대로 넣기
+            else {
+                answer += now_char;
+            }
+
+        }
         return answer;
     }
 }
