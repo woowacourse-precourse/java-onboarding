7b7f80e33a0c5886ff8247a1e08a1b1cba24b31c
can019 <jys01012@gmail.com>
2022-11-01 05:36:17 +0900

    :sparkles: Solution 5 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,19 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        int [] moneyUnit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        List<Integer> answer = new LinkedList<>();
+        int [] moneyUnit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        answer = method(money, moneyUnit, 0, answer);
         return answer;
     }
 
-    private static List<Integer> method(int money, int[] moneyUnit,int index, List<Integer> result){
+    private static List<Integer> method(int money, int[] moneyUnit, int index, List<Integer> result){
         // @TODO :: 재귀로 돌아 answer return
-        return result;
+        if(index == moneyUnit.length)
+            return result;
+        else{
+            int quotient = money / moneyUnit[index];
+            int remainMoney = money - quotient * moneyUnit[index];
+
+            result.add(index, quotient);
+
+            return method(remainMoney, moneyUnit, index+1, result);
+        }
     }
 }
