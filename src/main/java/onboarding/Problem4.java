2d5aecf9b9be0c7efd8f9a8842da5ac811a4a5c4
yumble <hnjn1101@gmail.com>
2022-10-31 22:57:47 +0900

    Problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.nio.charset.StandardCharsets;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        byte[] bytes = word.getBytes(StandardCharsets.US_ASCII);
+
+        for(int i = 0; i < bytes.length ; i++){
+            if(bytes[i] >= 65 && bytes[i] <= 90){
+                bytes[i] = (byte) ( 155 - bytes[i]);
+            }
+            else if (bytes[i] >= 97 && bytes[i] <= 122) {
+                bytes[i] = (byte) ( 219 - bytes[i]);
+            }
+            answer = new String(bytes);
+        }
+
         return answer;
     }
 }
