3162bcab5f92fd173e2c1be1353a4b8cd8d2a48d
소대훈 <theogns0681@naver.com>
2022-10-31 17:48:06 +0900

    [feat] (문제1) Book객체 원시값 포장

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,14 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = 0;
+        Book pobiBook;
+        Book crongBook;
+        try {
+            pobiBook = new Book(pobi.get(0), pobi.get(1));
+            crongBook = new Book(crong.get(0), crong.get(1));
+        }catch (Exception e){
+            return -1;
+        }
         return answer;
     }
     static class Book{
@@ -16,9 +24,21 @@ class Problem1 {
         public int getNextPage() {
             return nextPage;
         }
-        Book(int previousPage, int nextPage) {
+        Book(int previousPage, int nextPage) throws Exception {
+            if (findException(previousPage,nextPage)) throw new Exception();
             this.previousPage = previousPage;
             this.nextPage = nextPage;
+        }
+
+        public boolean findException(int previousPage, int nextPage){
+        /*
+        예외사항 1. 1 페이지 혹은 400 페이지가 나오는 경우
+        예외사항 2. 이전 페이지와 다음 페이지가 연속적이지 않은 경우
+        예외사항 3. 이전 페이지가 홀수이고 다음 페이지가 짝수가 아닌 경우
+        */
+            if (previousPage==1 || nextPage==400) return true;
+            if (previousPage-nextPage<-1) return true;
+            return previousPage % 2 ==0 || nextPage % 2 ==1;
         }
     }
 }
