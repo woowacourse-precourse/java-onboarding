8929dd5d8e7fab917bb3dc1e6fc27b839a95274e
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-31 11:26:57 +0900

    feat: 허용되지 않는 도메인 예외 처리 (#6)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+
 
 public class Problem6 {
+    private static final String ALLOWED_DOMAIN = "email.com";
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        for (List<String> info : forms) {
+            String email = info.get(0);
+            String nickName = info.get(1);
+            if (!isAllowedDomain(email)) {
+                throw new NotAllowedDomainException("허용되지 않는 도메인입니다.");
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    private static boolean isAllowedDomain(String email) {
+        return email.split("@")[1].equals(ALLOWED_DOMAIN);
+    }
+
+    private static class NotAllowedDomainException extends IllegalArgumentException {
+        public NotAllowedDomainException(String s) {
+            super(s);
+        }
     }
 }
