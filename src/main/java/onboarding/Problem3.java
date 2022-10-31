a5f0679e24bee4f6abbe7e67bfdaeb1cd50852fa
janu01ary <janu01ary@gmail.com>
2022-10-31 17:38:44 +0900

    [Problem3] 기능 모두 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int result = 0;
+        for (int i = 1; i <= num; i++) {
+            String str = Integer.toString(i);
+            result += str.length() - str.replaceAll("3|6|9", "").length();
+        }
+        return result;
     }
 }
