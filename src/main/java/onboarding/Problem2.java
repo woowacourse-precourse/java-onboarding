502717a320f67b0a456a722dc603a1569e6bfd80
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-31 22:28:35 +0900

    feat: implement remove duplicate char

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,10 +3,10 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         while(hasDuplicateChar(cryptogram)){
-            break;
+            cryptogram = removeDuplicateChar(cryptogram);
         }
 
-        return "";
+        return cryptogram;
     }
 
     private static boolean hasDuplicateChar(String cryptogram) {
@@ -18,4 +18,33 @@ public class Problem2 {
         return false;
     }
 
+    private static String removeDuplicateChar(String cryptogram) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            while (isValidRange(cryptogram, i + 1) && cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                i = newCharIndex(cryptogram, i);
+            }
+
+            if (i < cryptogram.length()) {
+                result.append(cryptogram.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+
+    private static boolean isValidRange(String cryptogram, int index) {
+        return index < cryptogram.length();
+    }
+
+    private static int newCharIndex(String cryptogram, int index) {
+        char curChar = cryptogram.charAt(index);
+
+        while (isValidRange(cryptogram, index) && cryptogram.charAt(index) == curChar) {
+            index++;
+        }
+
+        return index;
+    }
 }
