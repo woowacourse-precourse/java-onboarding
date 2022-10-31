6323d4913e20f8867a13aaa71906c17991de718c
kylew1004 <kylew1004@gmail.com>
2022-10-31 12:32:33 +0900

    Problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
+    private static char chWord(char c){
+        if((int)c >= 65 && (int)c < 91)
+            return (char)(155 - (int)c);
+
+        else if((int)c >= 97 && (int)c < 123)
+            return (char) (219 - (int) c);
+        return c;
+    }
+    public static String solution(String word){
         String answer = "";
+
+        for(char c : word.toCharArray()){
+            answer += chWord(c);
+        }
         return answer;
     }
 }
