0a1ef1f738a1b44c9ae651c5ba6ac19fffe3841d
ddogga <hayeonlee60327@gmail.com>
2022-10-31 21:18:16 +0900

    문제4 주어진 word 반복문 돌리기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] wordArray = word.toCharArray();
+
+        for (char c : wordArray) {
+            System.out.println("c = " + c);
+        }
         return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        String word = "I love you";
+
+        solution(word);
     }
 }
