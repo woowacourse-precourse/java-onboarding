077347746258a0867f0c036335e95eed147f21df
5nam <ohnam90@gmail.com>
2022-10-31 15:02:32 +0900

    test: 예외처리에 대한 테스트 코드 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -49,6 +50,22 @@ class ApplicationTest {
             String cryptogram = "zyelleyz";
             String result = "";
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
+
+        @Test
+        void 입력값_숫자() {
+            String cryptogram = "sagsd12";
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Problem2.solution(cryptogram);
+            });
+        }
+
+        @Test
+        void 입력값_길이() {
+            String cryptogram = "";
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Problem2.solution(cryptogram);
+            });
         }
     }
 
