c83df2eefc7f9b97ba3e089341fe29adf54552a8
seungmin-park <tmddudals369@naver.com>
2022-10-31 17:16:55 +0900

    문제 7번 '친구 관계 정리하기' 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, ArrayList<String>> friendList = new HashMap<>();
+        convertFriendList(friends, friendList);
+
         return answer;
+    }
+
+    private static void convertFriendList(List<List<String>> friends, Map<String, ArrayList<String>> friendList) {
+        for (List<String> friend : friends) {
+            friendList.put(friend.get(0), new ArrayList<>());
+            friendList.put(friend.get(1), new ArrayList<>());
+        }
+        for (List<String> friend : friends) {
+            friendList.get(friend.get(0)).add(friend.get(1));
+            friendList.get(friend.get(1)).add(friend.get(0));
+        }
     }
 }
