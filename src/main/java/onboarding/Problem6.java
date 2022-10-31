fd5f8235e731a72b0ab104b00905cb907ef6a4dd
Jimin <thswlals0219@gmail.com>
2022-10-31 21:18:41 +0900

    feat: add sendEmailNotification function to problem6
    자른 두 문자를 포함하는 닉네임이 존재할 경우, 해당 이메일을 반환하는 기능 완성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+    public static Map<String, String> splitNicknameEmail = new HashMap<>();
+    public static Set<String> emailWithTheSameName = new HashSet<>();
+
+    public static List<String> sendEmailNotification(List<List<String>> forms) {
+        List<String> answer;
+
+        for(List<String> student : forms) {
+            String nickname = student.get(1);
+            String email = student.get(0);
+            for(int i = 0; i < nickname.length() - 1; i++) {
+                String splitName = nickname.substring(i, i + 2);
+                if(splitNicknameEmail.containsKey(splitName)) {
+                    emailWithTheSameName.add(email);
+                    emailWithTheSameName.add(splitNicknameEmail.get(splitName));
+                } else {
+                    splitNicknameEmail.put(splitName, email);
+                }
+            }
+        }
+
+        answer = new ArrayList<>(emailWithTheSameName);
+
         return answer;
+    }
+
+    public static List<String> solution(List<List<String>> forms) {
+        return sendEmailNotification(forms);
     }
 }
