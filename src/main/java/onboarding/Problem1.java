3eaa966930ee691b707c0706847b77a4244db5e8
Sangin <ksiisk99@gmail.com>
2022-10-31 16:06:46 +0900

    feat: problem1 - 예외처리를 위한 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(isException(pobi,crong)){
+            return -1;
+        }
+        return -1;
+    }
+
+    private static boolean isException(List<Integer> pobi, List<Integer> crong){
+        return (!isCorrectPage(pobi)) || (!isCorrectPage(crong));
+    }
+
+    private static boolean isCorrectPage(List<Integer>pages){ //1
+        int pageGap=pages.get(1)-pages.get(0);
+
+        if(pageGap==1) {
+            return true;
+        }
+        return false;
     }
 }
