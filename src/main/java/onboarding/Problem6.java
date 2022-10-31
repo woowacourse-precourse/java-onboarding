1a651fce37752cb2838bc782392f60f46e2ed1a9
Jeonsoyeon <itso22@naver.com>
2022-11-01 01:36:50 +0900

    feat(Problem6): 비슷한 닉네임을 가진사람의 메일주소목록 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,6 +8,20 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
         TreeSet<String> answerSet = new TreeSet<>();
+        for(int i=0; i<forms.size(); i++) {
+            String nickname = forms.get(i).get(1);
+            String email = forms.get(i).get(0);
+            for(int j=0; j<forms.size(); j++) {
+                if(i==j) continue;
+                String comparedNickname = forms.get(j).get(1);
+                String comparedEmail = forms.get(j).get(0);
+
+                if(findSimilarNickname(nickname,comparedNickname)){
+                    answerSet.add(email);
+                    answerSet.add(comparedEmail);
+                }
+            }
+        }
         return answer;
     }
 
