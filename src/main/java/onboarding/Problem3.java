0c95e3865b09dd03116df7159a4644693edfa1d2
민병욱 <woook27@naver.com>
2022-11-01 02:46:09 +0900

    feat: solution 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,13 +9,16 @@ public class Problem3 {
         String[] digits = num.split("");
 
         for (int i = 0; i < digits.length; i++) {
-            if(digits[i] == "3" || digits[i] == "6" || digits[i] == "9") count++;
+            if(digits[i].equals("3") || digits[i].equals("6") || digits[i].equals("9")) count++;
         }
         return count;
     }
     public static int solution(int number) {
         int answer = 0;
-        
+
+        for (int i = 1; i <= number; i++) {
+            answer += getCount(i);
+        }
         return answer;
     }
 }
