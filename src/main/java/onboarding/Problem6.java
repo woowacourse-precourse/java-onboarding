90ed90f56b1e0501c4007b91aa723a4832d01af7
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 01:10:27 +0900

    feat: count nickname substring by length
    - 하나의 닉네임에서 주어진 길이의 연속된 모든 부분문자열 사용횟수 구하는
      기능추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+
+
         return answer;
+    }
+
+    // 해당 길이 부분문자열 카운팅
+    private static void countSubStringOfLength(Map<String, Integer> map, String nickname, int length) {
+        // 시작점
+        for (int i = 0; i <= nickname.length() - length; i++) {
+            String subString = nickname.substring(i, i + length);
+            int cnt = map.getOrDefault(subString, 0) + 1;
+            map.put(subString, cnt);
+        }
     }
 }
