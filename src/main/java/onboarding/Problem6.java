0bd9d5357c43a693b61fd511948a156d76e70515
wonseok <tkddls23@konkuk.ac.kr>
2022-11-01 04:48:49 +0900

    feat: 6-1 두 글자 이상 연속 중복인 닉네임들 리스트에 넣기 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<String> duplicatedList = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String word = nickname.substring(i,i+2);
+                duplicatedList.addAll(findDuplicate(word,nickname,forms));
+            }
+        }
         return answer;
+    }
+
+    private static List<String> findDuplicate(String word, String myNickname, List<List<String>> forms) {
+        List<String> duplicatedList = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+
+            if (nickname.equals(myNickname))
+                continue;
+            if (nickname.contains(word))
+                duplicatedList.add(nickname);
+        }
+        return duplicatedList;
     }
 }
