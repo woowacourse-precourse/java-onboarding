667a67ff1f8c6ad12058ce1e30b51b628d562b56
Aru428 <86353626+Aru428@users.noreply.github.com>
2022-10-31 21:11:50 +0900

    문제2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        for(int i=0;i<cryptogram.length()-1;i++){
+            char first = cryptogram.charAt(i);
+            char second = cryptogram.charAt(i+1);
+            String duplication = "";
+            if(first == second){
+                duplication += first;
+                duplication += second;
+                cryptogram.replace(duplication,"");
+                i=0;
+            }
+        }
+
+        answer = cryptogram;
+
         return answer;
     }
+
 }
