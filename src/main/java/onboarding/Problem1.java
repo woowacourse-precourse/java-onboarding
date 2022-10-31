5ebb5615b1d0df13f8818b91afae6ee84a6c6151
cocomozila <joseyun94@naver.com>
2022-10-31 16:59:58 +0900

    [문제 1] [ 예외 ] 오른쪽 페이지가 홀수인지 판별하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,6 +17,7 @@ class Problem1 {
         for (int i = 0; i < list.length; i++) {
             if (isNotConsecutiveNumber(list[i])) return false;
             if (isNotEvenNumber(list[i].get(1))) return false;
+            if (isNotOddNumber(list[i].get(0))) return false;
         }
         return true;
     }
@@ -27,5 +28,9 @@ class Problem1 {
     // 왼쪽 페이지가 짝수인지 판별하는 기능
     private static boolean isNotEvenNumber(int number) {
         return number % 2 != 0;
+    }
+    // 오른쪽 페이지가 홀수인지 판별하는 기능
+    private static boolean isNotOddNumber(int number) {
+        return number % 2 == 0;
     }
 }
