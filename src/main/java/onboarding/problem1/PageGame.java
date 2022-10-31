a0adf72d0eada79b18250477507ea37d3ca4cf8a
vcho1958 <gusckd57@gmail.com>
2022-10-31 19:45:53 +0900

    [fix] unsafe operation fix

--- src/main/java/onboarding/problem1/PageGame.java
+++ src/main/java/onboarding/problem1/PageGame.java
@@ -3,8 +3,9 @@ package onboarding.problem1;
 import java.util.List;
 
 class PageGame {
-    PageOperator pageOperator;
-    List<Integer>[] pageDatas = new List[2];
+    private PageOperator pageOperator;
+    private List<Integer> pobis;
+    private List<Integer> crongs;
 
     private boolean isFaultPageData(List<Integer> pageData) {
         if (pageData.get(0) + 1 != pageData.get(1)) return true;
@@ -14,16 +15,14 @@ class PageGame {
 
     public PageGame(List<Integer> pobi, List<Integer> crong) {
         pageOperator = new PageOperator();
-        pageDatas[0] = pobi;
-        pageDatas[1] = crong;
+        if(isFaultPageData(pobi) || isFaultPageData(crong))throw new RuntimeException();
+        pobis = pobi;
+        crongs = crong;
     }
 
     public Integer start() {
-        for (var pageData : pageDatas) {
-            if (isFaultPageData(pageData)) throw new RuntimeException();
-        }
-        Integer pobi = pageOperator.getMaxPageNumber(pageDatas[0]);
-        Integer crong = pageOperator.getMaxPageNumber(pageDatas[1]);
+        Integer pobi = pageOperator.getMaxPageNumber(pobis);
+        Integer crong = pageOperator.getMaxPageNumber(crongs);
         if (pobi > crong) return 1;
         if (pobi < crong) return 2;
         if (pobi == crong) return 0;
--- src/main/java/onboarding/problem1/Problem1.java
+++ src/main/java/onboarding/problem1/Problem1.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 public class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        PageGame game = new PageGame(pobi, crong);
         try {
+        PageGame game = new PageGame(pobi, crong);
+
             return game.start();
         }catch (RuntimeException e){
             return -1;
