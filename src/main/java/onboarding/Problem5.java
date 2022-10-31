41d31c0df440de9b0f34a947add763a517a39964
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 17:10:40 +0900

    money를 remania으로 복사

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<Integer>();
+        int remain = money;
 
 
 
