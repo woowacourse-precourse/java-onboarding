d332cbafd418d5063a8cee4886e9ab598371bf04
yeongjin <hanyj0104@naver.com>
2022-11-01 02:59:08 +0900

    규칙 확인해주는 메소드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,4 +17,16 @@ class Problem1 {
         }
         return Math.max(sum, mult);
     }
+
+    static boolean rulecheck(List<Integer> pobi, List<Integer> crong) {
+        if ((pobi.get(1) - pobi.get(0)) != 1 || (crong.get(1) - crong.get(0)) != 1) {//
+            return false;
+        }
+        for (int i = 0; i < 2; i++) {
+            if (pobi.get(i) % 2 != (i + 1) % 2 || crong.get(i) % 2 != (i + 1) % 2) {//홀짝
+                return false;
+            }
+        }
+        return true;
+    }
 }
