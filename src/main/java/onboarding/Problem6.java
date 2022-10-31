373894fb7ed5455a3cd945eb3665487f92b618fe
yun <yshyeon102@gmail.com>
2022-11-01 01:05:03 +0900

    feat: Problem6 - 닉네임 중복 체크 및 등록, 경고Set에 추가 메서드 구현
    - checkAndEnrollPartialNickNameList() : 나눈 닉네임 리스트를 전달 받아, 하나씩 확인하도록 구현
    
    - checkAndEnrollPartialNickName() : 
    전달 받은 닉네임이 이미 존재하면 alarmSet에 등록하도록 하고, 존재하지 않으면 닉네임과 이메일을 중복 확인을 위한Map에 등록.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -23,6 +23,34 @@ public class Problem6 {
         return convertMapToListAndSort(alarmSet);
     }
 
+    private static void checkAndEnrollPartialNickNameList(List<String> partialNickNameList, String email, HashSet<String> alarmSet, HashMap<String, String> partialNickNameEmailMap) {
+        for (String partialNickName : partialNickNameList) {
+            checkAndEnrollPartialNickName(partialNickName, email, alarmSet, partialNickNameEmailMap);
+        }
+    }
+
+    private static void checkAndEnrollPartialNickName(String partialNickName, String email, HashSet<String> alarmSet, HashMap<String, String> partialNickNameEmailMap) {
+        if (isExistingName(partialNickName, partialNickNameEmailMap)) {
+            String existingRegisteredEmail = partialNickNameEmailMap.get(partialNickName);
+            addTwoEmailsToAlarmSet(alarmSet, email, existingRegisteredEmail);
+            return;
+        }
+        enrollNickNameEmail(partialNickName, email, partialNickNameEmailMap);
+    }
+
+    private static void enrollNickNameEmail(String partialNickName, String email, HashMap<String, String> partialNickNameEmailMap) {
+        partialNickNameEmailMap.put(partialNickName, email);
+    }
+
+    private static void addTwoEmailsToAlarmSet(HashSet<String> alarmSet, String newEmail, String existingRegisteredEmail) {
+        alarmSet.add(newEmail);
+        alarmSet.add(existingRegisteredEmail);
+    }
+
+    private static boolean isExistingName(String partialNickName, HashMap<String, String> partialNickNameEmailMap) {
+        return partialNickNameEmailMap.containsKey(partialNickName);
+    }
+
     private static List<String> makePartialNickNameList(String nickName) {
         List<String> partialNickNameList = new ArrayList<>();
         for (int splitLength = 2; splitLength <= nickName.length(); splitLength++) {
