f8ba4fbe86d80aa8ccceb774b8280a5b644eae15
Hongsugyeong <olivia258@naver.com>
2022-10-31 17:30:18 +0900

    [Problem5] 솔루션

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,8 +9,7 @@ public class Problem5 {
      *화폐 갯수 세는 함수
      */
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return cal(money);
     }
 
     /*
