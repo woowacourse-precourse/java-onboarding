cafd1991f5046ab26394907bcf590de93f9cbe65
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-31 19:22:26 +0900

    finish Main

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int unitMoney = 50000;
+        boolean divideFlag = true;
+        while (unitMoney >= 10) {
+            System.out.println(money + " " + unitMoney);
+            answer.add(money / unitMoney);
+            money = (money % unitMoney);
+            unitMoney = divideFlag ? unitMoney / 5 : unitMoney / 2;
+            divideFlag = !divideFlag;
+        }
+        answer.add(money);
         return answer;
     }
 }
