917cd8984d47d7f4dc6aca081f06193c408af623
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 02:22:34 +0900

    문제1 : 주어진 클래스를 이용하여 문제 해결

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,19 +1,18 @@
 package onboarding;
 
-import onboarding.problem1.Comparator;
 import onboarding.problem1.Validator;
-
 import java.util.List;
+import static onboarding.problem1.Operator.*;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
         if (!(Validator.validate(pobi) && Validator.validate(crong))) { return -1;}
-        if (Comparator.compare(pobi) == Comparator.compare(crong)) {
+        if (compare(pobi) == compare(crong)) {
             answer = 0;
         }
-        else if (Comparator.compare(pobi) > Comparator.compare(crong)) {
+        else if (compare(pobi) > compare(crong)) {
             answer = 1;
         }
         else {
