33ba7f429d5e8696f68f86b587e3cb79803c1c6f
Sangin <ksiisk99@gmail.com>
2022-10-31 16:33:06 +0900

    feat: problem2 - 중복 문자를 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,32 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer="";
+
         return answer;
+    }
+
+    private static String removeDuplicates(String cryptogram){
+        String result="";
+        char prevCharacter=cryptogram.charAt(0);
+        boolean checkDuplication=false;
+
+        for(int i=1;i<cryptogram.length();i++){
+            if(prevCharacter != cryptogram.charAt(i)){
+                if(!checkDuplication){
+                    result+=String.valueOf(prevCharacter);
+                }
+
+                prevCharacter=cryptogram.charAt(i);
+                checkDuplication=false;
+            }else{
+                checkDuplication=true;
+            }
+        }
+
+        if(!checkDuplication){
+            result+=cryptogram.charAt(cryptogram.length()-1);
+        }
+        return result;
     }
 }
