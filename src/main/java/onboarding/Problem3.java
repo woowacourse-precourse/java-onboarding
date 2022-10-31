7e40bf9b8be7f6865aabad4234ed74ced9a059cd
SeoSeunghun <proto_seo@naver.com>
2022-10-31 13:36:26 +0900

    [feat] 문제 3번 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = 0;
+		for (int i = 1; i <= number; i++) {
+			answer += countClap(i);
+		}
+		return answer;
+	}
+
+	private static int countClap(int num) {
+		int count = 0;
+		while (num > 0) {
+			int digit = (num % 10);
+			if (digit != 0 && digit % 3 == 0) {
+				count++;
+			}
+			num /= 10;
+		}
+		return count;
+	}
 }
