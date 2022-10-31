15425ca81fb8a0bfaa23943c0fe28a19a01fb5c0
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:03:47 +0900

    [problem2] 요구사항 정의 및 클래스 역할 분배

---
+++ src/main/java/onboarding/problem2/Cryptogram.java
@@ -0,0 +1,10 @@
+package onboarding.problem2;
+
+public class Cryptogram {
+    //StringBuilder
+
+    //1.생성자, 문자열 입력받아서 초기화.
+
+    //2.자기자신 1회 돌아서 중복 문자 제거, 만약 한번이라도 제거되면 true반환
+    //3.2번에서 중복인지 체크할 수 있는 조건함수
+}
---
+++ src/main/java/onboarding/problem2/CryptogramGenerator.java
@@ -0,0 +1,8 @@
+package onboarding.problem2;
+
+public class CryptogramGenerator {
+    //Cryptogram
+
+    //1. 생성자, 문자열 받고 Cryptogram 인스턴스 생성
+    //2. 크립토그램 변환함수, Cryptogram의 중복 제거 반복호출 후 반환
+}
