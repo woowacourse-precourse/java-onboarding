dabf3be5c181177590414df8f2b1aea44d6f8457
janu01ary <janu01ary@gmail.com>
2022-10-31 17:46:50 +0900

    [Problem1] 숫자의 각 자리 숫자를 모두 곱한 수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -30,6 +30,16 @@ class Problem1 {
         return addNum;
     }
 
+    private static int getMulNum(int num) {
+        int mulNum = 1;
+        for (int i = getDigit(num) - 1; i >= 0; i--) {
+            int tens = (int)Math.pow(10, i);
+            mulNum *= num / tens;
+            num %= tens;
+        }
+        return mulNum;
+    }
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (!checkPage(pobi) || !checkPage(crong))
             return -1;
