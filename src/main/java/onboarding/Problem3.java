1baf203203b3757ee485187feb61e025cc74b0a5
Albert <guang7226961@naver.com>
2022-10-31 11:38:11 +0900

    refactor(Problem3) 재구현 feat(Problem3) 범위 제한 검증 기능 구현 (NumberRangeValidator)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,13 +1,7 @@
 package onboarding;
 
-import onboarding.problem3.CheckNumber;
-import onboarding.problem3.Game369;
-
 public class Problem3 {
-    public static int solution(int number) {
-        if (!CheckNumber.of(number)){
-            throw new IllegalArgumentException("number는 1이상 10,000 이하인 자연수이여야 한다.");
-        }
-        return Game369.getGame369sum(number);
-    }
+	public static int solution(int number) {
+        return 0;
+	}
 }
