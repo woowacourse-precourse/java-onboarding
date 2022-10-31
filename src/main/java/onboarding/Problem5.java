1de98cc197b8fb355fa10a8ab9c15306268c108c
jhsseonn <hosu0125@gmail.com>
2022-10-31 23:40:54 +0900

    feat: (Problem5) 화폐 수를 리스트에 저장, 리스트 최종 return

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,6 +10,19 @@ public class Problem5 {
 
         int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        int restMoney = money;
+
+        if (restMoney >= 1) {
+            for (int mon:moneyUnit) {
+                int moneyNum = 0;
+                moneyNum = restMoney / mon;
+                answer.add(moneyNum);
+                restMoney = restMoney % mon;
+            }
+        } else {
+            throw new RuntimeException("1 이상 입력해야합니다.");
+        }
+
         return answer;
     }
 }
