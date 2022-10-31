14b6776b759e6e0295b8129171dd71f35d68516d
ryanha <ryanha@handong.ac.kr>
2022-10-31 15:35:36 +0900

    Docs[Problem5] : 문제해결에 필요한 기능 리스트 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        // 1. 단위가 되는 각
+
         return answer;
     }
 }
