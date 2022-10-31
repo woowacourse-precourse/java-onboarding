4f9dc7ba45b512e60d28549e43c5001db0df6463
subin Park <psb0320@hs.ac.kr>
2022-10-31 18:39:28 +0900

    feat: 연속하는 중복 문자 삭제하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,13 +2,11 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-
         while (isDuplication(cryptogram)) {
-            // 연속하는 중복 문자가 있으면 중복 문자 삭제
+            cryptogram = removeDuplication(cryptogram);
         }
 
-        return answer;
+        return cryptogram;
     }
 
     public static boolean isDuplication(String str) {
@@ -16,5 +14,27 @@ public class Problem2 {
             if (str.charAt(i) == str.charAt(i + 1)) return true;
         }
         return false;
+    }
+
+    public static String removeDuplication(String str) {
+        StringBuilder result = new StringBuilder();
+
+        str += " ";
+        char preChar = ' ';
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                preChar = str.charAt(i);
+                i++;
+                continue;
+            } else if (str.charAt(i) == preChar) {
+                preChar = str.charAt(i);
+                continue;
+            }
+            preChar = str.charAt(i);
+            result.append(str.charAt(i));
+        }
+
+        return result.toString();
     }
 }
