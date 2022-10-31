8e363ad82299d2cfb4db0260af0bb2fc7cc8c4ab
zeunala <zeunala@naver.com>
2022-11-01 03:08:36 +0900

    refactor: 문제 2 불필요한 함수 정의 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,21 +1,21 @@
 package onboarding;
 
 public class Problem2 {
-    public static String getNonduplicateString(String target) {
+    public static String solution(String cryptogram) {
         StringBuilder result = new StringBuilder(); // 연속 중복 문자열이 없도록 target에서 문자를 하나씩 옮긴다.
 
         // result의 마지막과 target 현재 부분이 같을 경우 체크해놓고 넘기고, 나중에 result에서 마지막 한 칸을 제거한다.
         boolean isDuplicated = false;
-        for (int i = 0; i < target.length(); i++) {
+        for (int i = 0; i < cryptogram.length(); i++) {
             if (result.length() == 0) { // result가 비어있으면 target 현재 부분을 그대로 추가하면 된다.
-                result.append(target.charAt(i));
+                result.append(cryptogram.charAt(i));
                 continue;
             }
 
             // 아래 코드에서는 result에 원소가 있음을 보장한다.
 
             // result의 마지막과 target의 현재 문자가 같은 경우 result에 옮기지 않은 채 넘긴다.
-            if (result.charAt(result.length() - 1) == target.charAt(i)) {
+            if (result.charAt(result.length() - 1) == cryptogram.charAt(i)) {
                 isDuplicated = true;
                 continue;
             }
@@ -26,16 +26,12 @@ public class Problem2 {
                 i--;
                 continue;
             }
-            result.append(target.charAt(i));
+            result.append(cryptogram.charAt(i));
         }
         if (isDuplicated && result.length() > 0) { // aaaa와 같이 마지막 중복처리가 끝나지 않은 채 for문이 끝나는 경우 위함
             result.deleteCharAt(result.length() - 1);
         }
 
         return result.toString();
-    }
-
-    public static String solution(String cryptogram) {
-        return getNonduplicateString(cryptogram);
     }
 }
