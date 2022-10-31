a14301597d1f28f944798ac2362b03325655d72a
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-01 01:21:19 +0900

    [pb6] fix: 전체 알고리즘 수정 / feat: name마다 중복되는 email들 갯수 count

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,67 +1,34 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Map<Character, Set<Character>> charChecker = new HashMap<>();
-        initConsecutiveCharSet(charChecker, forms);
+        Map<String, List<String>> nameToEmails = new HashMap<>();
 
-        List<String> emails = new ArrayList<>();
-        forms.forEach(item -> addTargetEmails(emails, charChecker, item));
-
-        return emails;
-    }
-
-    private static void initConsecutiveCharSet(Map<Character, Set<Character>> charChecker,
-                                               List<List<String>> forms) {
-        forms.forEach(item -> addStringToSet(charChecker, item.get(1)));
-    }
-
-    private static void addStringToSet(Map<Character, Set<Character>> charChecker,
-                                       String name) {
-        int i = 0;
-        Character keyChar;
-        Character nextChar;
-
-        while (i < name.length() - 1) {
-            keyChar = name.charAt(i);
-            nextChar = name.charAt(i+1);
-            Set<Character> valueSet = new HashSet<>();
-
-            if (charChecker.containsKey(keyChar)) {
-                valueSet = charChecker.get(keyChar);
-                valueSet.add(nextChar);
-            } else {
-                valueSet.add(nextChar);
-            }
-            charChecker.put(keyChar, valueSet);
-            i++;
+        for (List<String> item : forms) {
+            String email = item.get(0);
+            String name = item.get(1);
+            updateMap(nameToEmails, email, name);
         }
+        return nameToEmails.entrySet().stream()
+                .filter(item -> item.getValue().size() > 1)
+                .flatMap(item -> item.getValue().stream())
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
-    private static void addTargetEmails(List<String> emails,
-                                        Map<Character, Set<Character>> charChecker,
-                                        List<String> user) {
-        String name = user.get(1);
-        int i = 0;
-        boolean stop = false;
-        Character keyChar;
-        Character nextChar;
-        while (i < name.length() - 1 && !stop) {
-            keyChar = name.charAt(i);
-            nextChar = name.charAt(i+1);
-
-            if (charChecker.get(keyChar).contains(nextChar)) {
-                emails.add(user.get(0));
-                stop = true;
+    private static void updateMap(Map<String, List<String>> nameToEmails,
+                                  String email,
+                                  String name) {
+        for (int i = 0; i < name.length() - 1; i++) {
+            String subStr = name.substring(i, i + 2);
+            if (!nameToEmails.containsKey(subStr)) {
+                nameToEmails.put(subStr, new ArrayList<>());
             }
-            i++;
+            nameToEmails.get(subStr).add(email);
         }
     }
 }
-// 2개씩 parsing해서 set에다 다 넣어보자.
-// 넣을때마다 교집합이 하나라도 있으면 걸린거다.
-// 제이엠, 제이슨 (제이)
-// 워니 , 워니맘 (워니)
-// {제: [이], 이: [엠, 슨], 워: {니], 엠: {제]}
