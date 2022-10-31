e00432ad5bde3b70b70d15e5ced85a94d398e5e1
hossang <zkzk104@naver.com>
2022-10-31 09:12:05 +0900

    refactor: 조건문을 메소드(is369)로 분리하고 추가 테스트 통과

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,12 +6,16 @@ public class Problem3 {
         for (int i = 1; i <= number; i++) {
             int num = i;
             while (num != 0) {
-                if (num % 10 != 0 && num % 10 % 3 == 0) {
+                if (is369(num)) {
                     answer++;
                 }
                 num /= 10;
             }
         }
         return answer;
+    }
+
+    private static boolean is369(int num) {
+        return num % 10 != 0 && num % 10 % 3 == 0;
     }
 }
