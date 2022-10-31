3c875c77bee383464240ebdf300b0e86b6fa7148
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-01 03:13:18 +0900

    P6: remove duplicated emails in the email list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static List<String> delDuplicate(List<String> emails){
+        ArrayList<String> newList = new ArrayList<String>();
+
+        if(emails.size() >= 2) {
+            for (String email : emails) {
+                if (!newList.contains(email)){
+                    newList.add(email);
+                }
+            }
+        }
+
+        return newList;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> forms = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "나이엠"),
+                List.of("woniee@email.com", "워니"),
+                List.of("mj@email.com", "임제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+        System.out.println(solution(forms));
     }
 }
