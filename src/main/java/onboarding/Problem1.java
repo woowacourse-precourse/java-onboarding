b51bbc8643b9739e4510f556788756e585e9744b
brotherstone97 <brotherstone97@gmail.com>
2022-10-31 15:20:37 +0900

    Feat: 페이지 자릿수 덧셈 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
 
 class Problem1 {
 
@@ -17,8 +18,23 @@ class Problem1 {
         }
     }
 
+    static int addCalc(List<Integer> pages) {
+        int maxResult = 0;
+        for (int page : pages) {
+            int divisor = 100;
+            int result = 0;
+            while (divisor > 0) {
+                result += (int) (page / divisor);
+                page %= divisor;
+                divisor /= 10;
+            }
+            maxResult= Math.max(maxResult, result);
+        }
+        return maxResult;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (!isVaild(pobi) || !isVaild(crong)){
+        if (!isVaild(pobi) || !isVaild(crong)) {
             return -1;
         }
         int answer = Integer.MAX_VALUE;
