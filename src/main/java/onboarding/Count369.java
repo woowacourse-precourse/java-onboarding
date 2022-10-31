aba2c86c8aeb5755cb914e94be95810e7a9a8119
yoonsj970407 <yoonsj970407@gmail.com>
2022-10-31 19:27:02 +0900

    ✨Feat: Add exception()

--- src/main/java/onboarding/Count369.java
+++ src/main/java/onboarding/Count369.java
@@ -6,4 +6,12 @@ class Count369 {
     public Count369(int number) {
         this.number = number;
     }
+
+    int exception() {
+        if((number < 1) || (number > 10000)) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
