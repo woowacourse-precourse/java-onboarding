821d6812901f688e51c71b9ed71357c1df0f9bc3
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 22:48:31 +0900

    comment: P2 기능 목록 주석 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ * 기능 1: 연속되는 문자열 제거 함수 작성 (Stack 이용)
+ * 기능 2: 중복 제거된 Stack을 통해 문자열로 변환하는 함수 작성
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
