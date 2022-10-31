90a17328ac20db7bd08373f7d830cd019fbf93fc
PPakSang <sanghyun-dev@naver.com>
2022-10-31 15:13:27 +0900

    Feat: Problem2 로직 구현 완료 연속된 문자에 대한 처리: Stack 자료구조 사용 스택이 비어있고, 문자가 연속하지 않는 경우 문자를 push 같은 문자가 연속으로 들어오는 경우에 기존 문자를 pop

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decode(cryptogram);
+    }
+
+    private static String decode(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char c = cryptogram.charAt(i);
+
+            if (stack.isEmpty() || stack.peek() != c) {
+                stack.push(c);
+            } else if (stack.peek().equals(c)) {
+                stack.pop();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (stack.size() > 0) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
     }
 }
+
+
