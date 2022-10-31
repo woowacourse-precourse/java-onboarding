691ec36321ebadb4f8345f60541a730ac517a31c
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 21:15:13 +0900

    P1 | 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -43,4 +43,23 @@ class Problem1 {
         }
 
         return Math.max(plus, multiply);
+    }
+
+    public static boolean exceptionCheck(List<Integer> pobi, List<Integer> crong){
+        if (pobi.get(0) % 2 != 1 || pobi.get(1) != pobi.get(0) + 1) {
+            return true;
+        }
+        if (crong.get(0) % 2 != 1 || crong.get(1) != crong.get(0) + 1) {
+            return true;
+        }
+        if (pobi.get(0) <= 0 || pobi.get(0) > 400 || pobi.get(1) <= 0 || pobi.get(1) > 400) {
+            return true;
+        }
+        if (crong.get(0) <= 0 || crong.get(0) > 400 || crong.get(1) <= 0 || crong.get(1) > 400) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
