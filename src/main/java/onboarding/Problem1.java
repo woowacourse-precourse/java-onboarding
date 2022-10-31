56282ef01b6ee5044e843d0541b173b117d990dc
eugene1204 <s1061001@naver.com>
2022-10-31 16:00:18 +0900

    기능목록: problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 
 class Problem1 {
-
+    // 1. 페이지가 연속적인가 -> 아닐경우 return -1
+    // 2. 오른쪽 왼쪽 페이지의 연산 메소드 => 덧셈 구하는 메소드, 곱셈 구하는 메소드
+    // 3. 나의 큰수 결정하는 메소드
+    // 4. 포비랑 크롱을 비교하는 메소드 -> return 값을 정하는 메소드 : solution
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
