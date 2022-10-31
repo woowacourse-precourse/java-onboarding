3dd0620198673cc6f8c6f99b9141edd3dbd329c7
whale0928 <rlagusrl928@gmail.com>
2022-10-31 19:34:28 +0900

    리펙토링 후 제출

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,31 +3,25 @@ package onboarding;
 import java.util.*;
 import java.util.function.Predicate;
 
+
 public class Problem7 {
 
-    //유저 정보를 다루는 클래스 자료형
+
+    /**
+     * 사용자들의 정보를 다루기 위한 클래스 자료형
+     */
     static class userInfo implements Comparable<userInfo> {
-        //유저의 이름을 저장
-        private String name;
-        //유저의 친구관계들을 저장
-        private Set<String> friends;
-        //유저의 점수를 저장
+
+        private final String name;
+        private final Set<String> friends;
         private int point;
 
         public String getName() {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-
         public Set<String> getFriends() {
             return friends;
-        }
-
-        public void setFriends(Set<String> friends) {
-            this.friends = friends;
         }
 
         public int getPoint() {
@@ -42,7 +36,6 @@ public class Problem7 {
             this.point++;
         }
 
-        //회원 정보를 저장.
         public void addFriends(String friendsName) {
             friends.add(friendsName);
         }
@@ -62,72 +55,34 @@ public class Problem7 {
 
         @Override
         public int compareTo(userInfo o) {
-            if (Integer.compare(o.point, this.point) == 0) {
+            if (o.point == this.point) {
                 return this.name.compareTo(o.name);
             }
             return Integer.compare(o.point, this.point);
         }
     }
 
-
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, userInfo> users = cleanUpData(friends); // 객체 Map을 만들어 반환
+        friendInfriend(user, users);      //친구의 친구를 탐색
+        selectVisitors(users, visitors);  //방문한 사람들을 탐색
+        userSorted(users);                //요구사항에 맞게 정렬
+
+        List<String> keySet = userSorted(users);
+
+        return returnAnswer(keySet, users);
+    }
+
+    /**
+     * 정렬된 리스트와 유저정보를 가지고 정답을 만들어 반환하는 메서드
+     * Return answer list.
+     *
+     * @param keySet the key set
+     * @param users  the users
+     * @return the list
+     */
+    public static List<String> returnAnswer(List<String> keySet, Map<String, userInfo> users) {
         List<String> answer = new ArrayList<>();
-        Map<String, userInfo> users = new HashMap<>();
-
-        //유저별 친구를 클래스 자료형으로 생성..
-        for (List<String> list : friends) {
-            String nameA = list.get(0);
-            String nameB = list.get(1);
-            // Map<String,userInfo> infos = getNames(users);
-
-            //없을 경우 객체 생성.
-            if (!users.containsKey(nameA)) users.put(nameA, new userInfo(nameA, nameB));
-            else users.get(nameA).addFriends(nameB);
-            if (!users.containsKey(nameB)) users.put(nameB, new userInfo(nameB, nameA));
-            else users.get(nameB).addFriends(nameA);
-        }
-
-        //다 만들어진 자료형에서 사용자의 이름과 다른 사용자들의 친구를 바교해 점수를 증가 한다.
-
-        // myFreinds 비교해야되는 친구 목록을 추출
-        Set<String> myFreinds = users.get(user).getFriends();
-
-        //같은 친구가 있는지 확인.
-        for (String key : users.keySet()) {
-            //나 일 경우에는 제외
-            if (key.equals(user)) continue;
-
-            //나 이외의 사용자들의 친구 목록들을 꺼낸다. 두 리스트중 동일한 값이 있는지 확인.
-            Set<String> friendsList = users.get(key).getFriends();
-            int point = matchsName(myFreinds, friendsList);
-            users.get(key).setPoint(point);
-        }
-
-        //이제 조회한 사람들을 반복해 확인
-        for (String s : visitors) {
-            if (!users.containsKey(s)) {
-                users.put(s, new userInfo(s, ""));
-                users.get(s).plusPoint();
-            }
-        }
-
-        //마지막으로 포인트가 1 이상인 사람들만 answer에 저장한다.
-        //이 때 정렬이 필요하다
-
-
-        //배열을 정렬한다.
-        // 1.포인트 순으로
-        // 1.2 이름순으로
-
-        List<String> keySet = new ArrayList<>(users.keySet());
-        keySet.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return users.get(o1).compareTo(users.get(o2));
-            }
-        });
-
-        //0포인트 이상인 유저를 저장
         for (String key : keySet) {
             int point = users.get(key).getPoint();
             if (point > 0) {
@@ -137,17 +92,92 @@ public class Problem7 {
         return answer;
     }
 
-    //두 친구 목록들을 비교해 점수를 저장하는 기능
-    public static int matchsName(Set<String> myFreinds, Set<String> friendsList) {
+    /**
+     * Map을 1:점수순 2:이름순으로 정렬한 키셋을 반환하는 메서드
+     * User sorted list.
+     *
+     * @param users the users
+     * @return the list
+     */
+    public static List<String> userSorted(Map<String, userInfo> users) {
+        List<String> keySet = new ArrayList<>(users.keySet());
+        keySet.sort(Comparator.comparing(users::get));
+        return keySet;
+    }
 
-        long point = 0;
+    /**
+     * 방문자들을 비교해 점수를 증가시켜주는 메서드 친구가 아닌 경우 점수를 증가시켜주는 메서드
+     * Select visitors map.
+     *
+     * @param users    the users
+     * @param visitors the visitors
+     */
+    public static void selectVisitors(Map<String, userInfo> users, List<String> visitors) {
+        for (String s : visitors) {
+            if (!users.containsKey(s)) {
+                users.put(s, new userInfo(s, ""));
+                users.get(s).plusPoint();
+            }
+        }
+    }
 
+    /**
+     * 본인의 친구 목록을 가지고 다른 친구들의 친구들을 비교해 점수를 계산하는 메서드
+     * Friend infriend map.
+     *
+     * @param user  the user
+     * @param users the users
+     */
+    public static void friendInfriend(String user, Map<String, userInfo> users) {
+
+        Set<String> myFreinds = users.get(user).getFriends();
+
+        for (String key : users.keySet()) {
+            if (key.equals(user)) continue;
+
+            Set<String> friendsList = users.get(key).getFriends();
+            int point = matchesName(myFreinds, friendsList);
+            users.get(key).setPoint(point);
+        }
+    }
+
+    /**
+     * 사용자의 정보를 다루기 편하게 가공하는 메서드  [ 본인이름 : 친구목록 ]
+     * Clean up data map.
+     *
+     * @param friends the friends
+     * @return the map
+     */
+    public static Map<String, userInfo> cleanUpData(List<List<String>> friends) {
+        Map<String, userInfo> users = new HashMap<>();
+        for (List<String> list : friends) {
+            String nameA = list.get(0);
+            String nameB = list.get(1);
+
+            if (!users.containsKey(nameA)) users.put(nameA, new userInfo(nameA, nameB));
+            else users.get(nameA).addFriends(nameB);
+            if (!users.containsKey(nameB)) users.put(nameB, new userInfo(nameB, nameA));
+            else users.get(nameB).addFriends(nameA);
+        }
+        return users;
+    }
+
+    /**
+     * 두 사람의 친구들이 일치하는지 확인하는 메서드
+     * Matchs name int.
+     *
+     * @param myFreinds   the my freinds
+     * @param friendsList the friends list
+     * @return the int
+     */
+    public static int matchesName(Set<String> myFreinds, Set<String> friendsList) {
         long result = myFreinds.stream()
                 .filter(old -> friendsList.stream()
                         .anyMatch(Predicate.isEqual(old))).count();
 
-        point = result * 10;
+        long point = result * 10;
 
         return (int) point;
     }
+
 }
