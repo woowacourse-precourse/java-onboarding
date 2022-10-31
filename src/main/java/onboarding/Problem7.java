cc7f64c83ac374294e0951219b4535b880c5e5e5
ouohoon <begreat@kakao.com>
2022-11-01 02:59:02 +0900

    7번 문제 기능 3, 4 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,7 @@
 package onboarding;
 
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 
@@ -13,16 +13,36 @@ public class Problem7 {
     4. 추천 점수와 이름순으로 정렬해서 최대 5명 추천하는 기능
      */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        FriendRecommender friendRecommender = new FriendRecommender();
+
+        return friendRecommender.recommend(user, friends, visitors);
     }
 
-    private class FriendRecommender {
+    private static class FriendRecommender {
         private Map<String, Map<String, Boolean>> friendsRelation;
         private Map<String, Integer> friendsScore;
 
         public List<String> recommend(String user, List<List<String>> friends, List<String> visitors) {
+            setFriendsRelation(user, friends);
+            setFriendScore(user);
+            setVisitScore(user, visitors);
 
+            List<Map.Entry<String, Integer>> friendsScoreList = new ArrayList<>(friendsScore.entrySet());
+
+            friendsScoreList.sort(new StringIntPair());
+
+            return friendsScoreList.stream().map(Map.Entry::getKey).collect(Collectors.toList());
+        }
+
+        private void setVisitScore(String user, List<String> visitors) {
+            Set<String> userFriends = friendsRelation.get(user).keySet();
+            for (String visitor : visitors) {
+                if (friendsScore.containsKey(visitor)) {
+                    friendsScore.put(visitor, friendsScore.get(visitor) + 1);
+                } else if (!userFriends.contains(visitor) && !visitor.equals(user)){
+                    friendsScore.put(visitor, 1);
+                }
+            }
         }
 
         private void setFriendScore(String user) {
@@ -68,6 +88,14 @@ public class Problem7 {
                 Map<String, Boolean> bFriends = friendsRelation.get(friendB);
                 bFriends.put(friendA, true);
             }
+        }
+    }
+
+    private static class StringIntPair implements Comparator<Map.Entry<String, Integer>> {
+        @Override
+        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            return o1.getValue() - o2.getValue() == 0 ?
+                    o1.getKey().compareTo(o2.getKey()) : -(o1.getValue() - o2.getValue());
         }
     }
 }
