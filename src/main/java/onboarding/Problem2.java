ff6598394207fcfddc63ebd130d929e05569f408
yg0585 <yg0585@naver.com>
2022-10-31 16:50:35 +0900

    fix: 중복된 문자 2개까지만 찾는 문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,23 +7,75 @@ package onboarding;
 
 public class Problem2 {
 
-    private static int dupStr(String s) {
-        for (int i = 0; i < s.length() - 1; i++)
-        {
-            if (s.charAt(i) == s.charAt(i + 1))
-                return i;
+    static class DupStr{
+        private int startIdx;
+        private int FinalIdx;
+
+        private void startDupStr(String s) {
+            for (int i = 0; i < s.length() - 1; i++)
+            {
+                if (s.charAt(i) == s.charAt(i + 1)) {
+                    this.startIdx = i;
+                    this.FinalIdx = i + 1;
+                    return ;
+                }
+            }
+            this.startIdx = -1;
         }
-        return -1;
+
+        private void FinalDupStr(String s) {
+            if (startIdx == -1)
+                return;
+
+            for (int i = this.FinalIdx + 1; i < s.length(); i++)
+            {
+                if (s.charAt(i) != s.charAt(i - 1))
+                    return;
+                FinalIdx++;
+            }
+        }
+
+        private boolean isNextDup(String s){
+            for (int i = FinalIdx + 1; i < s.length() - 1; i++)
+            {
+                if (s.charAt(i) == s.charAt(i + 1)) {
+                    FinalIdx++;
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public int getStartIdx() {
+            return startIdx;
+        }
+
+        public int getFinalIdx() {
+            return FinalIdx;
+        }
+
+        public DupStr(String s) {
+            startDupStr(s);
+            while(true) {
+                FinalDupStr(s);
+                if (!isNextDup(s))
+                    return;
+            }
+        }
     }
 
     public static String solution(String cryptogram) {
-        int idx;
+        int startIdx;
+        int FinalIdx;
         while (true)
         {
-            idx = dupStr(cryptogram);
-            if (idx == -1)
+            DupStr dupstr = new DupStr(cryptogram);
+            startIdx = dupstr.getStartIdx();
+            FinalIdx = dupstr.getFinalIdx();
+            if (startIdx == -1)
                 break;
-            cryptogram = cryptogram.substring(0, idx) + cryptogram.substring(idx + 2);
+            cryptogram = cryptogram.substring(0, startIdx) + cryptogram.substring(FinalIdx + 1);
         }
         return cryptogram;
     }
