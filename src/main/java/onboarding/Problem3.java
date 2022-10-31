a02cff8652bdb7d0ebbaa5a5e137d8d1826279a4
jindaram-stu <jinsb1999@naver.com>
2022-10-31 23:07:05 +0900

    feat : [문제3] 주어진 박수 조건을 기반으로 박수 횟수를 반환하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int resultClap = 0;
+        return resultClap;
+    }
+
+    public static Integer count(String number, String clapCondition) {
+        return number.length() - number.replace(clapCondition,"").length();
     }
 }
