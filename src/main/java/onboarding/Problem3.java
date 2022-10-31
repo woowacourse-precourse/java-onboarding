5c53002606d2c4f776a1c274db07ad5d19eb7690
yeongjin <hanyj0104@naver.com>
2022-11-01 05:52:17 +0900

    369게임 구현완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int digit=0;
+        int num=0;
+        for(int i=1;i<=number;i++){
+            digit=0;
+            num=i;
+            while(num>0){
+                digit=num%10;
+
+                if(digit%3==0&&digit!=0)
+                    answer++;
+                num/=10;
+
+            }
+
+        }
         return answer;
     }
 }
