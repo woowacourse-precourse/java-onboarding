bad148375532fe57bd938174e8bf310d6f60c67e
KimSangHyeok <tkdgur0906@naver.com>
2022-10-31 17:53:07 +0900

    사용자의 친구 목록을 구하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        List<String> userFriendList = new ArrayList<>();
+        Map<String, Integer> recommendScore = new HashMap<>();
+        for (List<String> relation : friends) {
+            if (relation.contains(user)) {
+                addUserFriend(user, userFriendList, relation);
+            }
+        }
         return answer;
     }
+
+    private static void addUserFriend(String user, List<String> userFriendList, List<String> relation) {
+        for (String friend : relation) {
+            if(friend != user){
+                userFriendList.add(friend);
+                break;
+            }
+        }
+    }
+
 }
