b1915171ed4f12a19780d22d8a00d39975568772
inhyeok <ss112d86@gmail.com>
2022-11-01 08:09:44 +0900

    feat: Problem7 친구 관계로 추천친구를 구하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -10,11 +9,12 @@ import java.util.Set;
 public class Problem7 {
 
   private static HashMap<String, Set<String>> friendsShip = new HashMap<>();
-
+  private static HashMap<String, Integer> recommendList = new HashMap<>();
   public static List<String> solution(String user, List<List<String>> friends,
       List<String> visitors) {
     setFriendsShip(friends);
-    System.out.println(friendsShip);
+    recommendByFriends(user);
+
     return new ArrayList<>();
   }
 
@@ -29,6 +29,22 @@ public class Problem7 {
       Set<String> userBValue = friendsShip.getOrDefault(userB, new HashSet<>());
       userBValue.add(userA);
       friendsShip.put(userB, userBValue);
+    }
+  }
+
+  private static void recommendByFriends(String user){
+    Set<String> myFriends = friendsShip.get(user);
+    for(String myFriend : myFriends){
+
+      Set<String> friends = friendsShip.get(myFriend);
+
+      for(String friend : friends){
+        if(!friend.equals(user)) {
+          Integer priority = recommendList.getOrDefault(friend, 0);
+          priority += 10;
+          recommendList.put(friend, priority);
+        }
+      }
     }
   }
 }
