81565620626bba65fd7353f4dcce272cfb49b691
sugamypapa <sk000801@naver.com>
2022-10-31 19:38:47 +0900

    refactor: 문제 2 코드 컨벤션 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,21 +8,10 @@ public class Problem2 {
     // 이게 잘 안돼서ㅠㅠ 두번째 아이디어
     // for문을 돌려 연속인 문자를 제거하는 함수를 만든다
     // -> 첫번째 index에서 뒤의 index 값과 일치하지 않으면 새로 선언한 string에 추가
-    // -> 마지막 index에서 뒤의 index 값과 일치하지 않으면 새로 선언한 string에 추가
+    // -> 마지막 index에서 앞의 index 값과 일치하지 않으면 새로 선언한 string에 추가
     // -> 나머지 index에서 해당 인덱스 중심으로 앞, 뒤의 값과 일치하지 않으면 새로 선언한 string에 추가
     // 문자열에서 연속이 존재하면 while문을 돌려 연속된 문자가 없을 때까지 위의 함수를 실행시킨다
-
-//    public static String isContinue() {
-//        String findContinue = "\"[";
-//        for(int i = 97; i <= 122; i++) {
-//            findContinue  += (char) i + "{2,}";
-//            if(i<122) findContinue += "|";
-//            else findContinue += "]\"";
-//        }
-//        System.out.println(findContinue);
-//        return findContinue;
-//    }
-
+    
     public static String removeContinue(String s) {
         String remove = "";
         for(int i = 0; i < s.length(); i++) {
@@ -41,7 +30,7 @@ public class Problem2 {
 
     public static boolean hasRepeat(String s) {
         boolean hasRepeat = false;
-        for(int i = 0; i < s.length()-1; i++) {
+        for(int i = 0; i < s.length() - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 hasRepeat = true;
                 break;
