f740d3f662a3547339c60c852564c9099b70d4a5
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-01 00:43:59 +0900

    Problem4_Init

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+
+/*
+
+- 주어진 문장의 글자를 반대로 변환하여 return 하는 메소드
+- 알파벳 Only, 대문자는 대문자, 소문자는 소문자, word의 길이는 1이상 1000이하인 문자열
+
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
