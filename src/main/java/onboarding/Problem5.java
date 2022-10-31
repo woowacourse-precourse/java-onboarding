bbb55faec5bc13368f00af60da45c98979f7c4de
nahyeon cho <whskgus12@nate.com>
2022-11-01 03:21:56 +0900

    Problem 5 - 큰 화폐 단위부터 할당하기 (수정)
    금액에 따라 변경되는 List를 Static으로 지정하는 것은 옳지 않은 것 같아, countMoney의 List를 반환하도록 변경합니다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    private static List<Integer> resultMoney;
     public static List<Integer> solution(int money) {
-        resultMoney = new ArrayList<>(); // static 배열 초기화
+
         if(isValid(money)){
-           countMoney(money);
-           return resultMoney;
+           return countMoney(money);
         }else
             return null;
     }
 
-    public static void countMoney(int money) {
+    public static List<Integer> countMoney(int money) {
+        List<Integer> resultMoney = new ArrayList<>();
         // 큰 화폐부터 금액 할당하기
         Integer[] moneyList = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
         for (int m : moneyList) {
             resultMoney.add(money / m);
             money %= m;
         }
+        return resultMoney;
 
     }
 
