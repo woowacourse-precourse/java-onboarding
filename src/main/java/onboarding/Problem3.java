d82de09597b1ff2a126b713eaff5a63cb647980e
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-10-31 15:24:11 +0900

    feat: [Prob 3] 주어진 수에서 손뼉을 몇 번 쳐야 하는지 계산하는 기능 추가 number의 각 자리의 숫자를 확인하여 주어진 number에서 쳐야하는 손뼉의 횟수를 계산하는 countHandclapByNumber() 함수를 구현합니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,20 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+    public static int countHandclapByNumber(int number){
+        int handclapCount=0;
+        while(number>0){
+            int eachDigitOfNumber=number%10;
+            if(eachDigitOfNumber!=0 && eachDigitOfNumber%3==0){
+                handclapCount++;
+            }
+            number/=10;
+        }
+        return handclapCount;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
