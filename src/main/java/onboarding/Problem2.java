609fe84a1e69a29db7bc81ef4aba9d76410bf548
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-10-31 22:54:14 +0900

    feat : 문제2 Stack이용하여 글자 넣기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+    	String answer = "";
+
+        //Stack 이용하기 (삽입, 삭제가 용이)
+        Stack<String> charStack = new Stack<>();
+        String str = "";
+        for(int i=0; i<cryptogram.length(); i++) {
+            //각 자리의 글씨 가져오기
+            str = cryptogram.charAt(i) + "";
+            //맨 처음에는 첫번째 글자 Stack에 넣어주기
+        }
+        
         return answer;
     }
 }
