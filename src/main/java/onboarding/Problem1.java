7808d1cf37ee7ae4613213e922e2a7d73a0225f5
yejin130 <jinis2023@gmail.com>
2022-11-01 05:33:35 +0900

    Initial commit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,44 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
+        if(pobi.get(0) %2==1 && pobi.get(1) - pobi.get(0) ==1 && crong.get(0) %2==1 && crong.get(1) - crong.get(0) ==1 && pobi.get(0) !=1 && pobi.get(0) !=399 && crong.get(0) !=1 && crong.get(0) !=399) {
+            int p_max = 0;
+            for (int i = 0; i < 2; i++) {
+                String[] a = Integer.toString(pobi.get(i)).split("");
+                int sum = 0;
+                int prod = 1;
+                for (int j = 0; j < a.length; j++) {
+                    sum += Integer.parseInt(a[j]);
+                    prod *= Integer.parseInt(a[j]);
+                }
+                p_max = Math.max(sum, prod);
+            }
+
+            int c_max = 0;
+            for (int i = 0; i < 2; i++) {
+                String[] a = Integer.toString(crong.get(i)).split("");
+                int sum = 0;
+                int prod = 1;
+                for (int j = 0; j < a.length; j++) {
+                    sum += Integer.parseInt(a[j]);
+                    prod *= Integer.parseInt(a[j]);
+                }
+                c_max = Math.max(sum, prod);
+            }
+
+            if(p_max>c_max) {
+                answer = 1;
+            }
+            if(p_max<c_max) {
+                answer = 2;
+            }
+            if(p_max==c_max) {
+                answer = 0;
+            }
+        }
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,43 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        HashMap<String, String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<String>();
+
+        for (int i = 0; i < forms.size(); i++) { // forms[i][1].substring(j,j+2)를 전체랑 비교.
+            for (int j = 0; j < forms.size(); j++) {
+                for (int k = 0; k < forms.get(i).get(1).length() - 1; k++) {
+                    for (int l = 0; l < forms.get(j).get(1).length() - 1; l++) {
+                        if (i != j) {
+
+                            boolean a = forms.get(i).get(1).substring(k, k + 2).charAt(0) == forms.get(j).get(1).substring(l, l + 2)
+                                    .charAt(0);
+                            boolean b = forms.get(i).get(1).substring(k, k + 2).charAt(1) == forms.get(j).get(1).substring(l, l + 2)
+                                    .charAt(1);
+                            if (a && b) {
+                                map.put(forms.get(i).get(1), forms.get(i).get(0));
+                                map.put(forms.get(j).get(1), forms.get(j).get(0));
+                                set.add(forms.get(i).get(1));
+                                set.add(forms.get(j).get(1));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Iterator itr = set.iterator();
+        while(itr.hasNext()) {
+            answer.add(map.get((String) itr.next()));
+        }
+
+        Collections.sort(answer);
+
         return answer;
     }
 }
