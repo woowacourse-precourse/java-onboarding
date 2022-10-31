af4fb3f7e569b5430089b4bc9096425642426aed
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-31 18:37:57 +0900

    fix(2번) 제한사항 수정 및 Regex 오류 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,13 +10,15 @@ public class Problem2 {
 
         if (cryptogram.length() < 1 || cryptogram.length() > 1000)
             return "error";
+        if (!answer.toLowerCase().equals(cryptogram))
+            return "error";
 
-        //중복되는 연속된 문자를 찾기 위한 Regex
-        Pattern p = Pattern.compile("^.*(.)\\1.*$");
+        //중복되는 연속된 2개 문자를 찾기 위한 Regex
+        Pattern p = Pattern.compile("(\\w)(\\1+)");
         Matcher duplicateChar = p.matcher(cryptogram);
 
         while (duplicateChar.find()) {
-            answer = answer.substring(0,duplicateChar.start(1)) + answer.substring(duplicateChar.end(1)+1);
+            answer = answer.substring(0,duplicateChar.start(1)) + answer.substring(duplicateChar.end(2));
             duplicateChar = p.matcher(answer);
         }
 
