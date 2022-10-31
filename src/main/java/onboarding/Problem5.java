777c011e586fb48b751451bb6e84037b21f926fa
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-10-31 18:01:01 +0900

    1주차_과제_완료 / 5번 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,9 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new java.util.ArrayList<>(Collections.emptyList());
 
+        if(!checkMoney(money))
+            return answer;
+
         int temp = money;
         int[] def_money = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
@@ -15,5 +18,9 @@ public class Problem5 {
             temp %= i;
         }
         return answer;
+    }
+
+    public static boolean checkMoney(int money){
+        return money>0 && money < 1000000;
     }
 }
