14478a420af0031a2d9b13aec4b9edd543662b46
jisoolee7101@gmail.com <phllo154@naver.com>
2022-10-31 18:40:22 +0900

    1부터 number까지의 수 자리수마다 3의 배수인지 체크

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,43 +2,18 @@ package onboarding;
 
 public class Problem3 {
 
-    public static void main(String[] args) {
-        //solution(5);
-        solution(33);
-        //solution(297);
-        //solution(4444);
-    }
     public static int solution(int number) {
-        int answer = 0;
-        int numLength;
+        int answer;
+        int numDvTen;
         int countClap = 0;
 
         for(int i = 1; i <= number; i++) {
-            numLength = (int) Math.log10(i)+1;
-
-            switch (numLength) {
-                case 1:
-                    if(i % 3 == 0)
-                        countClap++;
-                    break;
-                case 2:
-                    int[] arrDouble = new int[2];
-                    arrDouble[0] = i / 10;
-                    arrDouble[1] = i % 10;
-
-                    if(arrDouble[0] % 3 == 0)
-                        countClap++;
-                    if(arrDouble[1] % 3 == 0 && arrDouble[1] != 0)
-                        countClap++;
-
-                    break;
-                case 3:
-                    //System.out.println("three");
-                    break;
-                case 4:
-                    //System.out.println("four");
-                    break;
-
+            numDvTen = i;
+            while(numDvTen != 0) {
+                if(numDvTen % 10 == 3 || numDvTen % 10 == 6 || numDvTen % 10 == 9) {
+                    countClap++;
+                }
+                numDvTen /= 10;
             }
         }
         answer = countClap;
