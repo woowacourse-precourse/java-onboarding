a0488021cc79765f925aa4adf330b3f231595d05
kmularise <kmula222@gmail.com>
2022-10-31 16:40:35 +0900

    scores Hashmap에 요소 추가 및 변경하는 부분 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,74 +6,72 @@ import java.util.HashMap;
 import java.util.Comparator;
 
 public class Problem7 {
-    private static String checkAlreadyFriends(String user, List<String> relation){
-        String output;
-        if (relation.get(0).equals(user)){
-            output = relation.get(1);
-            return output;
-        }
-        if (relation.get(1).equals(user)){
-            output = relation.get(0);
-            return output;
-        }
-        output = "";
-        return output;
-    }
-    private static List<String> findAlreadyFriends(String user, List<List<String>> friends){
-        List<String> output = new ArrayList<>();
-        int length = friends.size();
-        String temp;
-        for(int i = 0; i < length; i++){
-            temp = checkAlreadyFriends(user, friends.get(i));
-            if(!temp.equals("") && !output.contains(temp)){
-                output.add(temp);
-            }
-        }
-        return output;
-    }
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        //List<String> answer = Collections.emptyList();
-        HashMap<String, Integer> scores = new HashMap<>();
-        List<String> alreadyFriends = findAlreadyFriends(user, friends);
-        int friendCount = alreadyFriends.size();
-        int tempScore;
-        String temp;
-        //friends와 관련하여 각 user의 점수 구하기
-        int length = friends.size();
-        for (int i = 0; i < length ;i++){
-            for (int j = 0; j < friendCount; j++){
-                temp = checkAlreadyFriends(alreadyFriends.get(j), friends.get(i));
-                if(!temp.equals("") && !temp.equals(user)) {
-                    if (!temp.equals(user) && !scores.containsKey(temp)) {
-                        scores.put(temp, 10);
-                    }else{
-                        tempScore = scores.get(temp);
-                        tempScore += 10;
-                        scores.put(temp, tempScore);
-                    }
-                }
-            }
-        }//함수로 빼는 방법 생각하기
-        //visitors와 관련하여 각 user의 점수 구하기
-        int visitorsSize = visitors.size();
-        for (int i = 0; i < visitorsSize; i++){
-            if(!alreadyFriends.contains(visitors.get(i))) {//조건 수정하기
-                if (!scores.containsKey(visitors.get(i))) {
-                    scores.put(visitors.get(i), 1);
-                } else {
-                    tempScore = scores.get(visitors.get(i));
-                    tempScore += 1;
-                    scores.put(visitors.get(i), tempScore);
-                }
-            }
-        }
-        List<String> answer = new ArrayList<>(scores.keySet());
-        answer.sort(Comparator.naturalOrder());
-        answer.sort((s1, s2) -> scores.get(s2) - scores.get(s1));
+	private static String checkAlreadyFriends(String user, List<String> relation) {
+		String output;
+		if (relation.get(0).equals(user)) {
+			output = relation.get(1);
+			return output;
+		}
+		if (relation.get(1).equals(user)) {
+			output = relation.get(0);
+			return output;
+		}
+		output = "";
+		return output;
+	}
 
-        if(answer.size() > 5){
-            answer = answer.subList(0, 5);
-        }
-        return answer;
-    }
+	private static List<String> findAlreadyFriends(String user, List<List<String>> friends) {
+		List<String> output = new ArrayList<>();
+		int length = friends.size();
+		String temp;
+		for (int i = 0; i < length; i++) {
+			temp = checkAlreadyFriends(user, friends.get(i));
+			if (!temp.equals("") && !output.contains(temp)) {
+				output.add(temp);
+			}
+		}
+		return output;
+	}
+
+	private static void addScore(String member, int scoreUnit, HashMap<String, Integer> scores) {
+		int tempScore;
+		if (!scores.containsKey(member)) {
+			scores.put(member, scoreUnit);
+		} else {
+			tempScore = scores.get(member);
+			tempScore += scoreUnit;
+			scores.put(member, tempScore);
+		}
+	}
+
+	private static List<String> getAnswer(HashMap<String, Integer> scores){
+		List<String> answer = new ArrayList<>(scores.keySet());
+		answer.sort(Comparator.naturalOrder());
+		answer.sort((s1, s2) -> scores.get(s2) - scores.get(s1));
+		if (answer.size() > 5) {
+			answer = answer.subList(0, 5);
+		}
+		return answer;
+	}
+
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		HashMap<String, Integer> scores = new HashMap<>();
+		List<String> alreadyFriends = findAlreadyFriends(user, friends);
+		String temp;
+		for (int i = 0; i < friends.size(); i++) {
+			for (int j = 0; j < alreadyFriends.size(); j++) {
+				temp = checkAlreadyFriends(alreadyFriends.get(j), friends.get(i));
+				if (!temp.equals("") && !temp.equals(user)) {
+					addScore(temp, 10, scores);
+				}
+			}
+		}
+		for (int i = 0; i < visitors.size(); i++) {
+			if (!alreadyFriends.contains(visitors.get(i))) {
+				addScore(visitors.get(i), 1, scores);
+			}
+		}
+		List<String> answer = getAnswer(scores);
+		return answer;
+	}
 }
