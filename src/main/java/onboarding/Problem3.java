3c4b1fb2900e618c20715161e09cf9850110dc85
Yebin <lyb2325@gmail.com>
2022-11-01 04:06:57 +0900

    feat: Problem3 정수를 정수 배열로 변환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i=1;i<=number;i++) {
+            List<Integer> arrayOfNumbers = convertToArray(i); // 1 ~ number 의 정수를 정수 배열로 변환
+        }
         return answer;
+    }
+    private static List<Integer> convertToArray(int number) {
+        List<Integer> arr = new ArrayList<>();
+        while (number > 0) {
+            arr.add(number % 10);
+            number /= 10;
+        }
+        return arr;
     }
 }
