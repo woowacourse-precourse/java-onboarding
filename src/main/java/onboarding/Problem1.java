68785ffdde15c7defec5dbc41a657dbf29dcc202
eora21 <eora21@naver.com>
2022-10-31 15:48:49 +0900

    refactor: 💡 변경 package-private 클래스 생성 및 static 메서드 옮김
    기존의 Problem1 내에 작성되었던 static 메서드들을 PageGame 클래스로 옮겼다. 게임을 플레이하는 데 필요한
    작업이라고 생각할 수 있게끔 되었다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,75 +3,6 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    private static final int WRONG_INPUT = -1;
-    private static final int POBI_WIN = 1;
-    private static final int CRONG_WIN = 2;
-    private static final int DRAW = 0;
-
-    private static boolean isWrongInput(List<Integer> pages) {
-        if (pages.size() != 2)
-            return true;
-
-        int left = pages.get(0);
-        int right = pages.get(1);
-
-        if (left % 2 != 1 || right - left != 1 || left <= 1 || right >= 400)
-            return true;
-
-        return false;
-    }
-
-    private static int getMyScore(List<Integer> pages) {
-
-        int leftPage = pages.get(0);
-        int rightPage = pages.get(1);
-
-        return Math.max(getMaxScoreOnEachPage(leftPage), getMaxScoreOnEachPage(rightPage));
-    }
-
-    private static int getMaxScoreOnEachPage(int page) {
-
-        return Math.max(getScoreBySumAllDigits(page), getScoreByMulAllDigits(page));
-    }
-
-    private static int getScoreBySumAllDigits(int page) {
-
-        int score = 0;
-
-        while (page > 0) {
-            score += page % 10;
-
-            page /= 10;
-        }
-
-        return score;
-    }
-
-    private static int getScoreByMulAllDigits(int page) {
-
-        int score = 1;
-
-        while (page > 0) {
-            score *= page % 10;
-
-            page /= 10;
-        }
-
-        return score;
-    }
-
-    private static int getGameResult(int pobiScore, int crongScore) {
-
-        if (pobiScore > crongScore) {
-            return POBI_WIN;
-        }
-
-        if (pobiScore < crongScore) {
-            return CRONG_WIN;
-        }
-
-        return DRAW;
-    }
 
     /**
      * 1. 책을 임의로 펼친다.
@@ -86,10 +17,90 @@ class Problem1 {
      * @return 게임 결과, 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1
      */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (isWrongInput(pobi) || isWrongInput(crong)) {
+        PageGame pageGame = new PageGame();
+        return pageGame.play(pobi, crong);
+    }
+
+}
+
+class PageGame {
+    private final int WRONG_INPUT = -1;
+    private final int POBI_WIN = 1;
+    private final int CRONG_WIN = 2;
+    private final int DRAW = 0;
+
+    public int play(List<Integer> player1, List<Integer> player2) {
+        if (isWrongInput(player1) || isWrongInput(player2)) {
             return WRONG_INPUT;
         }
 
-        return getGameResult(getMyScore(pobi), getMyScore(crong));
+        return getGameResult(getScore(player1), getScore(player2));
     }
+
+    private boolean isWrongInput(List<Integer> pages) {
+        if (pages.size() != 2)
+            return true;
+
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        if (leftPage % 2 != 1 || rightPage + 1  != leftPage || leftPage <= 1 || rightPage >= 400) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int getScore(List<Integer> pages) {
+
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        return Math.max(getMaxScoreOnEachPage(leftPage), getMaxScoreOnEachPage(rightPage));
+    }
+
+    private int getMaxScoreOnEachPage(int page) {
+
+        return Math.max(getScoreBySumAllDigits(page), getScoreByMulAllDigits(page));
+    }
+
+    private int getScoreBySumAllDigits(int page) {
+
+        int score = 0;
+
+        while (page > 0) {
+            score += page % 10;
+
+            page /= 10;
+        }
+
+        return score;
+    }
+
+    private int getScoreByMulAllDigits(int page) {
+
+        int score = 1;
+
+        while (page > 0) {
+            score *= page % 10;
+
+            page /= 10;
+        }
+
+        return score;
+    }
+
+    private int getGameResult(int pobiScore, int crongScore) {
+
+        if (pobiScore > crongScore) {
+            return POBI_WIN;
+        }
+
+        if (pobiScore < crongScore) {
+            return CRONG_WIN;
+        }
+
+        return DRAW;
+    }
+
 }
