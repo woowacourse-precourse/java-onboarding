016a233e8c5e19180bb8fb3d9212d6fc05f10a5f
dydtmd6801 <dydtmd6801@naver.com>
2022-11-01 01:46:26 +0900

    refactor : 변수 생성을 통한 코드 정리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,11 +6,12 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         HashMap<String, Integer> friendsMap = new HashMap<>();
-        checkOldFriends(friends, user, friendsMap);
+        checkOldFriends(friends, user, friendsMap, visitors);
+        System.out.println(friendsMap);
         return answer;
     }
 
-    private static void checkOldFriends(List<List<String>> friendsList, String userName, HashMap friendsMap){
+    private static void checkOldFriends(List<List<String>> friendsList, String userName, HashMap friendsMap, List<String> visitor){
         for(int i = 0; i < friendsList.size(); i++){
             int oldFriends = friendsList.get(i).indexOf(userName);
             if(oldFriends > -1){
@@ -24,8 +25,9 @@ public class Problem7 {
         for(int i = 0; i < friendsList.size(); i++){
             int newFriends = friendsList.get(i).indexOf(oldFriendName);
             if(newFriends > -1 && !friendsList.get(i).get(1 - newFriends).equals(userName)){
-                if(friendsMap.containsKey(friendsList.get(i).get(1 - newFriends))) friendsMap.put(friendsList.get(i).get(1 - newFriends), (int)friendsMap.get(friendsList.get(i).get(1 - newFriends))+10);
-                if(!friendsMap.containsKey(friendsList.get(i).get(1 - newFriends))) friendsMap.put(friendsList.get(i).get(1 - newFriends), 10);
+                String newFriendName = friendsList.get(i).get(1 - newFriends);
+                if(friendsMap.containsKey(newFriendName)) friendsMap.put(newFriendName, (int)friendsMap.get(newFriendName)+10);
+                if(!friendsMap.containsKey(newFriendName)) friendsMap.put(newFriendName, 10);
             }
         }
     }
