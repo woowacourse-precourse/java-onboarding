f9be0b032b24ee45c27fc25532dc15ea60e7aa16
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:34:45 +0900

    [Problem4] 기능목록 작성하기(김민서)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,13 @@
 package onboarding;
 
 public class Problem4 {
+    /**
+     [기능목록]
+     1. 문자열을 문자 배열로 만들기
+     2. 소문자인 경우 반대로 변환하여 대문자로 만들기
+     3. 대문자인 경우 반대로 변환하여 소문자로 만들기
+     4. 알파벳 외의 문자는 변환하지 않게 하기
+     */
     public static String solution(String word) {
         String answer = "";
         return answer;
