5416d41e4c308c75c6277b3adcc45c67d034bffe
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-01 00:09:52 +0900

    feat : 문제4 대문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        // 참고 : 알파벳의 char 값 A:65 ~ Z:90 & a:97 ~ z:122
+        for (int i=0; i<word.length(); i++) {
+            //char로 추출해서 int로 변환
+            int num = Integer.valueOf(word.charAt(i));
+            //1) 대문자의 경우
+            if(num >= 65 && num <= 90) {
+                if(num <= 77)  num = 77 + (78-num);
+                else num = 78 - (num-77);
+            }
+            answer += String.valueOf((char)num);
+        }
         return answer;
     }
 }
