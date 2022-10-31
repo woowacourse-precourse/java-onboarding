eaf252adba2050f3db8e24b0cdbf511970917de1
chener <49267211+chanosong@users.noreply.github.com>
2022-10-31 23:51:24 +0900

    [Add] Prob3 Function list
    + Funtion list

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+// func : check this number contains how many 369s, return is number of 369s
+// func : count 1 to number, return total clap count
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
