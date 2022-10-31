3b36b27a7834b30374ded3f4e848e85a3e8b47d8
Cho SangHyun <98shcho@naver.com>
2022-11-01 02:18:12 +0900

    comment: remove unnecessary comment

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-//        List<Integer> answer = Collections.emptyList();
         List<Integer> answer = new ArrayList<>();
         List<Integer> currencyList = List.of(50000, 10000, 5000, 1000,
                 500, 100, 50, 10, 1);
