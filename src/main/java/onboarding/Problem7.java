5aeef67196dc83392850dcdd04828c423645e874
임형섭 <hs.yim@illb.co.kr>
2022-11-01 08:44:30 +0900

    [Problem7] 리팩토링 - 메서드화 , 검증 로직 추가
    solution 함수 내의 로직들을
    단일 책임 원칙을 준수하기 위해 메서드 캡슐화
    
    검증 로직 추가
    함께 아는 친구 추출에서 대상 A유저 가
    유저가 아니며 유저의 친구가 아니고 B유저가 유저의 친구일 때
    결과 Map에 추가하도록 변경

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,20 +1,98 @@
 package onboarding;
 
-import org.assertj.core.data.MapEntry;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = new ArrayList<>();
 
-        HashMap<String, String> map = new LinkedHashMap<>();
-
-        List<String> usersFriends = new ArrayList<>();
-        Map<String, Integer> result = new HashMap<>();
 
         // 유저의 친구 목록
+        List<String> usersFriends = usersFriends(user, friends);
+
+        // 친구 목록을 순회하면서 함께 아는 친구 값 증가
+        Map<String, Integer> result = recommendFriends(user, friends, usersFriends);
+
+        // 방문자 값 증가
+        result = recommendFriendsVisitor(user , result, usersFriends, visitors);
+
+        // 결과 Map 을 값(내림차순) , 키(오름차순) 순으로 정렬
+        LinkedHashMap<String, Integer> sortedResult = sortValueAndKey(result);
+
+        // Map을 순회하며 결과 추출
+        List<String> answer = generateAnswer(sortedResult);
+
+        return answer;
+    }
+
+    private static List<String> generateAnswer(LinkedHashMap<String, Integer> sortedResult) {
+
+        List<String> answer = new ArrayList<>();
+
+        // 정렬된 결과값을 순회하며 리스트에 값을 넣는다.
+        sortedResult.entrySet().stream().forEach((val)->{
+            answer.add(val.getKey());
+        });
+
+        // 리스트 사이즈가 5 초과시 subList 로 잘라준 리스트를 반환한다.
+        if (answer.size() > 5) {
+            return  answer.subList(0, 5);
+        }
+
+        return answer;
+    }
+
+    private static Map<String , Integer> recommendFriendsVisitor(String user , Map<String, Integer> result, List<String> usersFriends, List<String> visitors) {
+        // 방문자 리스트를 순회하며 유저가 아니며 , 유저의 친구가 아닌 경우 값 증가
+        visitors.stream().forEach((val)->{
+            if(!usersFriends.contains(val) && !val.equals(user)){
+                if (result.containsKey(val)) {
+                    result.put(val, result.get(val) + 1);
+                }else{
+                    result.put(val, 1);
+                }
+            }
+        });
+
+        return result;
+    }
+
+    private static Map<String ,Integer> recommendFriends(String user, List<List<String>> friends , List<String> usersFriends) {
+        Map<String, Integer> result = new HashMap<>();
+
+        // 친구 관계를 순회하며 조건에 따라 result에 값을 넣는다.
+        friends.stream().forEach((val)->{
+
+            String userA = val.get(0);
+            String userB = val.get(1);
+
+            // 유저A 가 유저가 아니고 유저의 친구가 아니며 userB 가 유저의 친구일 때 ( 함께 아는 친구 )
+            if(!userA.equals(user) && !usersFriends.contains(userA) &&  usersFriends.contains(userB) ){
+                // 함께 아는 친구가 이미 있을 때 값 추가 , 없을 때 생성
+                if(result.containsKey(userA)){
+                    result.put(userA, result.get(userA) + 10);
+                }else {
+                    result.put(userA, 10);
+                }
+            }
+
+            // 유저B 가 유저가 아니고 유저의 친구가 아니며 userB 가 유저의 친구일 때 ( 함께 아는 친구 )
+            if(!userB.equals(user) && !usersFriends.contains(userB) && usersFriends.contains(userA)){
+                if(result.containsKey(userB)){
+                    result.put(userB, result.get(userB) + 10);
+                }else {
+                    result.put(userB, 10);
+                }
+            }
+
+        });
+
+        return result;
+    }
+
+    private static List<String> usersFriends(String user , List<List<String>> friends) {
+        List<String> usersFriends = new ArrayList<>();
+
         friends.stream().forEach((val) -> {
             String userA = val.get(0);
             String userB = val.get(1);
@@ -28,62 +106,7 @@ public class Problem7 {
             }
         });
 
-        // 친구 목록을 순회하면서 함께 아는 친구 값 증가
-        friends.stream().forEach((val)->{
-
-            String userA = val.get(0);
-            String userB = val.get(1);
-
-            // 유저A 가 유저가 아니고 userB 가 유저의 친구일 때
-            if(!userA.equals(user) && usersFriends.contains(userB) ){
-                // 함께 아는 친구가 이미 있을 때 값 추가 , 없을 때 생성
-                if(result.containsKey(userA)){
-                    result.put(userA, result.get(userA) + 10);
-                }else {
-                    result.put(userA, 10);
-                }
-            }
-
-            // 유저B 가 유저가 아니고 userB 가 유저의 친구일 때
-            if(!userB.equals(user) && usersFriends.contains(userA)){
-                if(result.containsKey(userB)){
-                    result.put(userB, result.get(userB) + 10);
-                }else {
-                    result.put(userB, 10);
-                }
-            }
-
-        });
-
-        // 방문자 값 증가
-        visitors.stream().forEach((val)->{
-            if(!usersFriends.contains(val)){
-                if (result.containsKey(val)) {
-                    result.put(val, result.get(val) + 1);
-                }else{
-                    result.put(val, 1);
-                }
-            }
-        });
-
-        // 결과 Map 을 값(내림차순) , 키(오름차순) 순으로 정렬
-        LinkedHashMap<String, Integer> lastResult = sortValueAndKey(result);
-
-        // Map을 순회하며 리스트에 넣기
-        lastResult.entrySet().stream().forEach((val)->{
-            answer.add(val.getKey());
-        });
-
-        List<String> lastAnswer = new ArrayList<>();
-
-        // 사이즈가 5 초과 시 길이 조정
-        if (answer.size() > 5) {
-            lastAnswer = answer.subList(0, 5);
-        }else {
-            return answer;
-        }
-
-        return lastAnswer;
+        return usersFriends;
     }
 
     private static LinkedHashMap<String, Integer> sortValueAndKey(Map<String, Integer> result) {
