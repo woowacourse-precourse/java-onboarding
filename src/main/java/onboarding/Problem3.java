46ccaeaddb304f33b0f811ebc801dfe5e55149fe
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-10-31 11:16:46 +0900

    chore(Problem3): add comments outlining list of features

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,13 @@
 package onboarding;
 
 public class Problem3 {
+    // TODO: int countClaps(int number)
+
     public static int solution(int number) {
+        // Check if number is in valid range
+
+        // Count claps
+
         int answer = 0;
         return answer;
     }
