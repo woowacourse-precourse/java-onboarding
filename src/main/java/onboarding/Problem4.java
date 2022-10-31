ab552e7aa4eede542477b219ea19d6dddb0c173a
swonny <wonny921@gmail.com>
2022-10-31 16:45:15 +0900

    feat: 알파벳에 해당하는 역순 알파벳 매칭하기 구현
    - 대문자는 아스키코드를 사용하여 판별하였다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,11 +5,22 @@ public class Problem4 {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         StringBuffer sb = new StringBuffer(alphabet);
         String reversedAlphabet = sb.reverse().toString();
-        String answer = "";
-        return answer;
-    }
 
-    public static void main(String[] args) {
-        solution("I love you");
+        String answer = "";
+        int idx = -1;
+        for(int i = 0; i < word.length(); i++) {
+            char tmp = word.charAt(i);
+            System.out.println((int)tmp);
+            if(tmp == ' ') { // 공백
+                answer += " ";
+            } else if((int)tmp >= 65 && (int)tmp <= 90) { // 대문자
+                idx = alphabet.indexOf(Character.toLowerCase(word.charAt(i)));
+                answer += Character.toString(Character.toUpperCase(reversedAlphabet.charAt(idx)));
+            } else {
+                idx = alphabet.indexOf(word.charAt(i));
+                answer += Character.toString(reversedAlphabet.charAt(idx));
+            }
+        }
+        return answer;
     }
 }
