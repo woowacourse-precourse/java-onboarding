52b9b5e94294c84578bd831694313ddb955b25e4
ryanha <ryanha@handong.ac.kr>
2022-10-31 14:57:58 +0900

    Feat[Problem2] : 임의의 문자열 cryptogram을 문자의 배열로 변환하는 기능추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        // 1. 임의의 문자열 cryptogram을 문자의 배열로 변환
+        char[] letters = cryptogram.toCharArray();
+
         return answer;
     }
 }
