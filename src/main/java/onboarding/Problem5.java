b67e8b398b1be87e416c1be33b5a236849391328
myungjunlee <dlaudwns789@gmail.com>
2022-10-31 22:22:48 +0900

    feat(Problem5): 금액에 맞는 지폐 개수를 산출하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
+    public final static int MAX_PAPER = 50000;
+    private static int withdrawPaper(int bankMoney, int paperMoney) {
+        int paperNumber = bankMoney / paperMoney;
+        return paperNumber;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int bankMoney = money;
+        int paperMoney = MAX_PAPER;
+
         return answer;
     }
 }
