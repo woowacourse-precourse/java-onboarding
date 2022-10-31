72ad8ebd66b7c1aa1795a79271cb7d3d4aff4100
민병욱 <woook27@naver.com>
2022-11-01 02:39:35 +0900

    feat: getCount 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int getCount(int number) {
+        int count = 0;
+
+        String num = String.valueOf(number);
+        String[] digits = num.split("");
+
+        for (int i = 0; i < digits.length; i++) {
+            if(digits[i] == "3" || digits[i] == "6" || digits[i] == "9") count++;
+        }
+        return count;
+    }
     public static int solution(int number) {
         int answer = 0;
+        
         return answer;
     }
 }
