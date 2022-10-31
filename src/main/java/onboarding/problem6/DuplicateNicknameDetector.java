85646d08889e193406ec221cdd9b9e5fff87cf58
hodomaroo <enghemdwkd@gmail.com>
2022-11-01 05:18:02 +0900

    [Problem6/FEAT] DuplicateNicknameDetector 클래스의 모든 메서드 구현을 완료하였습니다.

--- src/main/java/onboarding/problem6/DuplicateNicknameDetector.java
+++ src/main/java/onboarding/problem6/DuplicateNicknameDetector.java
@@ -1,2 +1,35 @@
-package onboarding.problem6;public class DuplicateNickanemDetector {
+package onboarding.problem6;
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+public class DuplicateNicknameDetector {
+    private Set<String> duplicateNicknameOwnerEmailGroup = new HashSet<>();
+    private Map<String,String> slicedNicknameStorage = new HashMap<>();
+
+    public void registerUserAndUpdateDuplicate(List<String> userInfo){
+        String userEmail = userInfo.get(0);
+        String userNickname = userInfo.get(1);
+        
+        for(int i = 0 ; i < userNickname.length() - 1; i++){
+            String slicedNickname = userNickname.substring(i, i + 2);
+
+            if(slicedNicknameStorage.containsKey(slicedNickname)){
+                duplicateNicknameOwnerEmailGroup.add(slicedNicknameStorage.get(slicedNickname));
+                duplicateNicknameOwnerEmailGroup.add(userEmail);
+            }
+            slicedNicknameStorage.put(slicedNickname,userEmail);
+        }
+    }
+
+    public List<String> getDuplicateNicknameOwnerEmails(){
+        return new ArrayList<>(duplicateNicknameOwnerEmailGroup);
+    }
 }
