5c19499be888c821b2aabb49cfc94f55d5f746ff
hoyeoon <hoyeon.backend@mgail.com>
2022-10-31 23:16:33 +0900

    feat: Problem5 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> moneyList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> answer = new ArrayList<>();
+
+        for(int m : moneyList) {
+            answer.add(money / m);
+            money %= m;
+        }
+
         return answer;
     }
 }
