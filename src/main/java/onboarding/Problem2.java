e161ae288e1b4ebac321fe7e5fc246279f2296ee
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 15:31:26 +0900

    바꿀문자가 없을 때까지 하기에 While문 작성, While 실행시키기 위해 Flag 설정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        boolean flag = true;
+
+        while (flag) {
+
+        }
+
         return answer;
     }
 }
