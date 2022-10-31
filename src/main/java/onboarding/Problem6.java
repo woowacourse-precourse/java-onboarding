b95c049115635459d93e395c8a8075b9bbe28d65
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-01 03:32:30 +0900

    P6: remove unavailable info in forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> delDuplicate(List<String> emails){
@@ -17,19 +18,24 @@ public class Problem6 {
         return newList;
     }
 
+    public static List<List<String>> isAvailable(List<List<String>> forms){
+        List<List<String>> available = new ArrayList<List<String>>();
+
+        for (int i = 0; i < forms.size(); i ++){
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
+            String emailregex = "^[a-zA-Z0-9]{1,9}@email[.]com$";
+            String nameregex = "^[ㄱ-ㅎㅏ-ㅣ가-힣]{1,19}$";
+
+            if (email.length() > 10 && email.length() < 20 && Pattern.matches(emailregex, email) && Pattern.matches(nameregex, nickname)) {
+                available.add(forms.get(i));
+            }
+        }
+        return available;
+    }
+    
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
-    }
-
-    public static void main(String[] args) {
-        List<List<String>> forms = List.of(
-                List.of("jm@email.com", "제이엠"),
-                List.of("jason@email.com", "나이엠"),
-                List.of("woniee@email.com", "워니"),
-                List.of("mj@email.com", "임제이"),
-                List.of("nowm@email.com", "이제엠")
-        );
-        System.out.println(solution(forms));
     }
 }
