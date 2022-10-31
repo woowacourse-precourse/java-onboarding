2abbbaa64817c048a925a54e526cd86eb3b3cd3a
Isu Shin <isu010614@naver.com>
2022-10-31 18:08:47 +0900

    edit: Change letter space

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,32 +4,32 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
-        for(int i = 0; i < word.length(); i++){
+        for (int i = 0; i < word.length(); i++) {
             answer += isAlphabet(word.charAt(i));
         }
         return answer;
     }
 
-    public static char isAlphabet(char letter){
+    public static char isAlphabet(char letter) {
         String something = Character.toString(letter);
-        if(something.matches("[a-zA-Z]")){
+        if (something.matches("[a-zA-Z]")) {
             return convertAlphabet(letter);
         }
-        if(something.matches("[^a-zA-Z]")){
+        if (something.matches("[^a-zA-Z]")) {
             return letter;
         }
         return '@';
     }
 
-    public static char convertAlphabet(char letter){
+    public static char convertAlphabet(char letter) {
         String alphabet = Character.toString(letter);
-        if(alphabet.matches("[a-z]")){
+        if (alphabet.matches("[a-z]")) {
             int number = 219 - letter;
-            return (char)number;
+            return (char) number;
         }
-        if(alphabet.matches("[A-Z]")){
+        if (alphabet.matches("[A-Z]")) {
             int number = 155 - letter;
-            return (char)number;
+            return (char) number;
         }
         return '@';
     }
