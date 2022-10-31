844db68d4f8613db99057fe99df02acfc8a32c1f
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-10-31 23:07:46 +0900

    solve Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            int num = i;
+            while(num>0){
+                int mod = num%10;
+                if(mod==3||mod==6||mod==9)answer++;
+                num /= 10;
+            }
+        }
         return answer;
     }
 }
