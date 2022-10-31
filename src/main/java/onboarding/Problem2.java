850cb99d93bdc0286d6d23813a6fa3c8de1c6361
namtndus <rnrmf0721@gmail.com>
2022-10-31 23:31:48 +0900

    fix:문제 2 요구사항 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,9 +13,16 @@ public class Problem2 {
             str = changeLetter(str);
             count -= 1;
 
-            for(int i = 0; i<str.length()-1; i++){
-                if(str.charAt(i) == str.charAt(i+1)) {
-                    count +=1;
+            // todo : 연속해서 중복되는 부분이 있는 지 확인하는 반복문
+            for(int i = 0; i<str.length(); i++){
+                for(int j = i+1; j<str.length(); j++){
+
+                    if(str.charAt(i) == str.charAt(j) && j+j-i < str.length()){
+                        if(str.substring(i,j).equals(str.substring(j,j+j-i)) ) {
+                            count +=1;
+                        }
+                    }
+
                 }
             }
         }
@@ -24,6 +31,8 @@ public class Problem2 {
     }
 
     public static String changeLetter(String name){
+
+
         List<String> list = new ArrayList<>();
 
         for(int i = 0; i<name.length(); i++){
@@ -32,18 +41,36 @@ public class Problem2 {
 
 
         for(int i = 0; i<list.size()-1; i++){
-            if (list.get(i).equals(list.get(i+1))){
-                list.remove(i);
-                list.remove(i);
-                i=i-1;
+            for(int j = i+1; j<list.size(); j++){
+                int num = j-i;
+                if(list.get(i).equals(list.get(j)) && j+num-1 <list.size()){
+                    boolean isChecked = true;
+                    for(int z = 0; z<num; z++){
+                        if(!list.get(i+z).equals(list.get(j+z))){
+                            isChecked = false;
+                        }
+                        if(!isChecked){
+                            break;
+                        }
+                    }
+                    if(isChecked){
+                        for(int y = 0; y<num*2; y++){
+                            list.remove(i);
+
+                        }
+                        i-=num*2;
+                        break;
+                    }
+
+                }
             }
         }
         return String.join("",list);
     }
 
     public static void main(String[] args) {
-        String str = new String("browoanoommnaon");
-
+        String str = new String("oooommmcccmcbbbbbab");
+        System.out.println(solution(str));
 
     }
 }
