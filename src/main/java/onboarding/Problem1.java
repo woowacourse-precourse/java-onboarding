ea933251916a01b58e803803c016ea1f96f76c32
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:18:25 +0900

    [Problem1] 기능목록 작성 (김민서)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    /**
+     [기능목록]
+     1. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나 모두 곱하기
+     2. 왼쪽 페이지를 모두 더한 값과 모두 곱한 값 중에서 가장 큰 수 구하여 자신의 점수로 사용하기
+     3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나 모두 곱하기
+     4. 오른쪽 페이지를 모두 더한 값과 모두 곱한 값 중에서 가장 큰 수 구하여 자신의 점수로 사용하기
+     5. 점수를 비교해 게임의 승자 정하기
+     6. 예외사항 처라하기
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
