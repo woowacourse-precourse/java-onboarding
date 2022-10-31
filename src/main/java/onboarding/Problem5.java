8a605f5fcb02356c079bc6589769600e6ee0f19b
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-01 04:26:13 +0900

    Problem5 변수명 변경, 버전정보 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,7 +3,7 @@
  * - 문제 5
  *
  * 버전정보
- * - openjdk version "11.0.15"
+ * - jdk-11.0.17
  *
  * 날짜
  * - 2022-10-31
@@ -20,12 +20,12 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
-        int[] cashType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] coinType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        for (int cash : cashType) {
-            int count = money / cash;
-            money %= cash;
-            answer.add(count);
+        for (int coin : coinType) {
+            int coinCount = money / coin;
+            money %= coin;
+            answer.add(coinCount);
         }
         return answer;
     }
