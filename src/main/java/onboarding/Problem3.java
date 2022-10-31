21eb3f951abbb7ed7b77056f2653955e38c1f8e5
lacram <chemin9898@gmail.com>
2022-11-01 00:13:48 +0900

    Problem 3 - 특정 숫자의 369 개수 구하기 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,20 @@ package onboarding;
 * 2. 1부터 해당 숫자까지 369개수 구하기
 * */
 public class Problem3 {
+
+    // 특정 숫자의 369 개수 구하기
+    public static int get369Count(int number) {
+        int cnt = 0;
+        while (number > 0) {
+            int num = number % 10;
+            if (num == 3 || num == 6 || num == 9) {
+                cnt++;
+            }
+            number /= 10;
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
