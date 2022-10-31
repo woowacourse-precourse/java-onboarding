3328ddc36aae717d7077a4d526387e011668ad73
woosung1223 <dntjd991223@naver.com>
2022-10-31 16:21:44 +0900

    refactor(Problem7): uniqueIDs 삭제에 따른 메소드 제거

--- src/main/java/onboarding/problem7/Util.java
+++ src/main/java/onboarding/problem7/Util.java
@@ -3,15 +3,6 @@ package onboarding.problem7;
 import java.util.*;
 
 public class Util {
-    public static String getKeyFromValue(HashMap<String, Integer> hashMap, int value) {
-        String result = "";
-        for (String key :  hashMap.keySet()) {
-            if (value == hashMap.get(key)) {
-                result = key;
-            }
-        }
-        return result;
-    }
     public static List<String> removeOverlap(List<String> target) {
         HashSet<String> removedSet = new HashSet<>(target);
         ArrayList<String> removedList = new ArrayList<>(removedSet);
