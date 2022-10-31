294dba89d6a8663fa830fe542dbb7e764c8c8d69
gnoesnooj <306yyy@naver.com>
2022-10-31 13:53:15 +0800

    feat : 모든 사용자들이 점수를 갖는 Map 및 초기화 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,14 @@ import java.util.*;
 public class Problem7 {
     private static Set<String> uniqueUser = new HashSet<>();
 
+    private static Map<String, Integer> scoreMap = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         findUsers(friends, visitors);
+        makeScoreMap();
+
         return answer;
     }
 
@@ -20,6 +24,12 @@ public class Problem7 {
         for (int i = 0; i < friends.size(); i++) {
             uniqueUser.add(friends.get(i).get(0));
             uniqueUser.add(friends.get(i).get(1));
+        }
+    }
+
+    private static void makeScoreMap() {
+        for (String s : uniqueUser) {
+            scoreMap.put(s, 0);
         }
     }
 }
