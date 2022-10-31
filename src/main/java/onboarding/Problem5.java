49e6b5451729b037e4ec65c4e778d2596b896d09
TaeHwan <kimta2hwan@gmail.com>
2022-11-01 02:01:51 +0900

    feat(PROBLEM5): 남은 돈을 계산하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    static final int[] monetary = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    static int calculateChange(int money, int count, int index) {
+        if (count == 0) {
+            return money;
+        }
+
+        return money % (monetary[index] * count);
     }
 }
