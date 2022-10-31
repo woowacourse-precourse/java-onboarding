0be4c9fcbf281fd5600181e7939a38a63de2bfc2
NotableDeveloper <castlereal@naver.com>
2022-10-30 23:39:13 -0700

    Problem2, 3 (refactoring) - 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,6 +7,7 @@ public class Problem2 {
         while(true){
             answer = cryptogram;
             cryptogram = remove_repeatChar(cryptogram);
+            // 반복 문자를 제거하는 메서드를 수행하고 나서 변화가 없다면 탈출
             if (answer.equals(cryptogram)) break;
         }
 
@@ -16,13 +17,17 @@ public class Problem2 {
     public static String remove_repeatChar(String str){
         StringBuilder sb = new StringBuilder(str);
         sb.append(' ');
-
+        /*
+            문자열을 탐색하면서 현재 문자와 다음 문자가 같으면 현재 문자와
+            다음 문자를 문자열 상에서 삭제하고, 함수를 탈출한다.
+         */
         for(int i = 0; i < sb.length() - 1; i++){
             if(sb.charAt(i) == sb.charAt(i + 1)){
                 sb.delete(i, i + 2);
                 break;
             }
         }
+        // 앞서 넣었던 공백 문자를 지운다.
         sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
     }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,7 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-
+        // 입력으로 받은 숫자를 문자열로 바꾸어 각 자릿 수에 3, 6, 9이 들어있을 때마다 카운팅한다.
         for(int i = 1; i <= number; i++){
             String str = String.valueOf(i);
 
