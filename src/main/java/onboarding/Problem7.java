0566bb76292536f6d34530102d2b7ba4886fc645
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:53:31 +0900

    [Problem7] 기능목록 작성하기 (김민서)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Problem7 {
+    /**
+     [기능목록]
+     1. 친구 관계 정보에서 이미 친구인 경우 제거하기
+     2. 타임 라인 방문 기록에서 이미 친구인 경우 제거하기
+     3. 사용자와 함께 아는 친구인 경우 해쉬맵에 10점 추가하기
+     4. 사용자의 타임 라인에 방문한 경우 해쉬맵에 1점 추가하기
+     5. 점수가 가장 높은 순으로 정렬하고 추천 점수가 같은 경우 이름순으로 정렬하기
+     6. 최대 5명만 추천하고, 추천 점수가 0점인 경우 추천하지 않게 처리하기
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
 }
