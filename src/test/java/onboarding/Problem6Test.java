51258f3ac1956849a08e92f076e152b78a512ecf
saehim <saeilee.vxv@gmail.com>
2022-11-01 00:10:10 +0900

    Test(Problem6): 닉네임 분할 및 중복 체크

--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -26,4 +26,28 @@ class Problem6Test {
         answer.sort(Comparator.naturalOrder());
         System.out.println("end");
     }
+
+    // 닉네임 2글자씩 나누어 중복 체크 함수 호출
+    private void checkNicknames(List<List<String>> forms) {
+        for (int i=0; i<forms.size(); i++) {
+            String name = forms.get(i).get(1);              // 닉네임
+
+            for (int j=0; j<name.length()-1; j++){
+                String nameSlice = name.substring(j,j+2);   // 닉네임 2글자씩 자르기
+                checkNameMap(nameSlice, i);                 // 자른 이름 중복 체크
+            }
+        }
+    }
+
+    // 중복 체크
+    private void checkNameMap(String nameSlice, int i) {
+        if (nameMap.containsKey(nameSlice)) {       // 이름 2글자가 중복된다면
+            emails.add(nameMap.get(nameSlice));     // answer 배열에 중복되는 이름 있는 메일 추가
+            emails.add(forms.get(i).get(0));        // 체크하는 현재 메일도 추가
+        }
+        if (!nameMap.containsKey(nameSlice)) {      // 중복 이름이 없다면
+            nameMap.put(nameSlice, forms.get(i).get(0)); // hashmap에 새로운 key 저장
+        }
+    }
+
 }
