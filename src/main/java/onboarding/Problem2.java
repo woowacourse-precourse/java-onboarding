e90fcee5cab5e4b796b220143e46d57b5430c5d7
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 21:27:48 +0900

    P2 | 중복 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,4 +14,32 @@ public class Problem2 {
         return answer.toString();
     }
 
+    public static List<String> removeDouble(String str) {
+
+        List<String> strList = new ArrayList<>(Arrays.asList(str.split("")));
+
+        while (true) {
+            HashSet<Integer> duplicateList = new HashSet<>();
+            for (int i = 0; i < strList.size() - 1; i++) {
+                if (Objects.equals(strList.get(i), strList.get(i + 1))) {
+                    duplicateList.add(i);
+                    duplicateList.add(i + 1);
+                }
+            }
+            if (duplicateList.size() == 0) {
+                break;
+            }
+
+            Iterator<String> strIter = strList.iterator();
+            int idx = 0;
+            while (strIter.hasNext()) {
+                strIter.next();
+                if (duplicateList.contains(idx)) strIter.remove();
+                idx += 1;
+            }
+        }
+
+        return strList;
+
+    }
 }
