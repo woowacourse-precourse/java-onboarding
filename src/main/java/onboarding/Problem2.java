bcd560da5bc7014ac40a76ae7720d68d37320c5e
ChaCha3088 <cha3088@gmail.com>
2022-10-31 19:37:20 +0900

    feat: problem2 new function replaceMethod

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = replaceMethod(cryptogram);
+        System.out.println(cryptogram);
         return answer;
+    }
+
+    private static String replaceMethod(String cryptogram) {
+        String result = cryptogram.replaceAll("(.)\1+", "");
+        return result;
     }
 }
