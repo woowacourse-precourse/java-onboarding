a2e8f83dfe41d3a2af031c79e69d01af94b771b3
kariskan <zkfzpf56@naver.com>
2022-10-31 15:49:34 +0900

    refactor: Set 자료구조에서 List 자료구조로 변경
    - List 자료구조로 stream을 이용한 중복 제거 및 정렬

--- src/main/java/onboarding/support/problem6/DuplicateNicknameChecker.java
+++ src/main/java/onboarding/support/problem6/DuplicateNicknameChecker.java
@@ -1,49 +1,68 @@
 package onboarding.support.problem6;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DuplicateNicknameChecker {
 
-    private final List<String> DuplicateUserEmails;
+    private List<String> duplicateUserEmails;
+    private static final int BASE_INDEX = 0;
 
     public DuplicateNicknameChecker(List<List<String>> forms) {
-        Set<String> duplicatedUserEmail = new HashSet<>();
-        checkForms(forms, duplicatedUserEmail);
-        this.DuplicateUserEmails = setToList(duplicatedUserEmail);
+        this.duplicateUserEmails = new ArrayList<>();
+        checkForms(forms, BASE_INDEX, BASE_INDEX + 1);
+        this.duplicateUserEmails = toSortedList();
     }
 
-    private static void checkForms(List<List<String>> forms, Set<String> duplicatedUserEmails) {
-        for (int i = 0; i < forms.size(); i++) {
-            for (int j = i + 1; j < forms.size(); j++) {
-
-                List<String> first = forms.get(i);
-                List<String> second = forms.get(j);
-
-                String firstEmail = first.get(0);
-                String firstName = first.get(1);
-
-                String secondEmail = second.get(0);
-                String secondName = second.get(1);
-
-                checkDuplicate(duplicatedUserEmails, firstName, firstEmail, secondName, secondEmail);
-            }
+    private void checkForms(List<List<String>> forms, int firstIndex, int secondIndex) {
+        if (isNotFormIndexValid(forms, firstIndex, secondIndex)) {
+            return;
         }
+
+        List<String> first = forms.get(firstIndex);
+        List<String> second = forms.get(secondIndex);
+
+        checkDuplicate(first.get(BASE_INDEX), first.get(BASE_INDEX + 1),
+                second.get(BASE_INDEX), second.get(BASE_INDEX + 1), BASE_INDEX, BASE_INDEX + 1);
+
+        checkForms(forms, firstIndex, secondIndex + 1);
     }
 
-    private static void checkDuplicate(Set<String> duplicatedUserEmails,
-                                       String firstName, String firstEmail, String secondName, String secondEmail) {
-        for (int i = 0; i < firstName.length() - 1; i++) {
-            for (int j = 0; j < secondName.length() - 1; j++) {
-                if (isDuplicated(firstName, secondName, i, j)) {
-                    duplicatedUserEmails.add(firstEmail);
-                    duplicatedUserEmails.add(secondEmail);
-                    return;
-                }
-            }
+    private boolean isNotFormIndexValid(List<List<String>> forms, int firstIndex, int secondIndex) {
+        if (firstIndex >= forms.size()) {
+            return true;
         }
+        if (secondIndex >= forms.size()) {
+            checkForms(forms, firstIndex + 1, firstIndex + 2);
+            return true;
+        }
+        return false;
+    }
+
+    private void checkDuplicate(String firstEmail, String firstName,
+                                String secondEmail, String secondName, int firstIndex, int secondIndex) {
+        if (isNotNameIndexValid(firstEmail, firstName, secondEmail, secondName, firstIndex, secondIndex)) {
+            return;
+        }
+
+        if (isDuplicated(firstName, secondName, firstIndex, secondIndex)) {
+            this.duplicateUserEmails.add(firstEmail);
+            this.duplicateUserEmails.add(secondEmail);
+            return;
+        }
+        checkDuplicate(firstEmail, firstName, secondEmail, secondName, firstIndex, secondIndex + 1);
+    }
+
+    private boolean isNotNameIndexValid(String firstEmail, String firstName,
+                                        String secondEmail, String secondName, int firstIndex, int secondIndex) {
+        if (firstIndex >= firstName.length() - 1) {
+            return true;
+        }
+        if (secondIndex >= secondName.length() - 1) {
+            checkDuplicate(firstEmail, firstName, secondEmail, secondName, firstIndex + 1, firstIndex + 2);
+            return true;
+        }
+        return false;
     }
 
     private static boolean isDuplicated(String firstName, String secondName, int firstNameIndex, int secondNameIndex) {
@@ -51,14 +70,14 @@ public class DuplicateNicknameChecker {
                 && firstName.charAt(firstNameIndex + 1) == secondName.charAt(secondNameIndex + 1);
     }
 
-    private static List<String> setToList(Set<String> duplicatedUserEmails) {
-        return duplicatedUserEmails
-                .stream()
-                .sorted()
+    private List<String> toSortedList() {
+        return duplicateUserEmails.stream()
+                .distinct()
+                .sorted(Comparator.naturalOrder())
                 .collect(Collectors.toList());
     }
 
     public List<String> getDuplicateEmails() {
-        return DuplicateUserEmails;
+        return this.duplicateUserEmails;
     }
 }
