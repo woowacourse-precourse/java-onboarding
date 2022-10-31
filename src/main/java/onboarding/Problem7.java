374349c0f94acee20cdf564d3acd3c91c40e278f
Songyi Kim <ksl2950@naver.com>
2022-10-31 10:06:18 +0900

    feat/7: 함께 아는 친구 점수 주기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,43 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, Integer> userScore = new HashMap<>();
+        Map<String, List<String>> friendMap = createFriendMap(friends);
+
+        // 친구 점수 주기
+        for (String userFriend : friendMap.get(user)) {
+            List<String> scoreTargets = friendMap.get(userFriend);
+            plusFriendScore(userScore, scoreTargets);
+        }
+        return List.of("");
     }
+
+
+    private static void plusFriendScore(Map<String, Integer> userScore, List<String> scoreTargets) {
+        for (String target : scoreTargets) {
+            userScore.put(target, userScore.getOrDefault(target, 0) + 10);
+        }
+    }
+
+    private static Map<String, List<String>> createFriendMap(List<List<String>> friends) {
+        Map<String, List<String>> friendMap = new HashMap<>();
+        for (List<String> friendPair : friends) {
+            makeFriends(friendMap, friendPair.get(0), friendPair.get(1));
+            makeFriends(friendMap, friendPair.get(1), friendPair.get(0));
+        }
+        return friendMap;
+    }
+
+
+    public static void makeFriends(Map<String, List<String>> userFriends, String me, String friend) {
+        List<String> friendList = userFriends.getOrDefault(me, Collections.emptyList());
+        List<String> newFriendList = new ArrayList<>(friendList);
+        newFriendList.add(friend);
+        userFriends.put(me, newFriendList);
+    }
+
 }
