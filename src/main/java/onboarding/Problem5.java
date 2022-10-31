9794c71cd23c06762fa8b1b1513b04fe0f75c11f
eugene1204 <s1061001@naver.com>
2022-10-31 16:37:03 +0900

    p5, 1. int money를 List로 바꿔주기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,7 +10,14 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;}
+    public static List<Integer> intToList(int i){  // int money -> List로 떼어냄
+        List<Integer> moneyList = new ArrayList<>();
+        while(i>0){ // 50237
+            moneyList.add(i%10); // 7 3 2 0 5
+            i = i/10;
+        }
+        return moneyList;
+    }
 
-    
 
 }
