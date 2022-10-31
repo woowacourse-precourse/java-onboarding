5404968c39eb11089ffae28dbce9e3c3c0a9bdb8
wonseok <tkddls23@konkuk.ac.kr>
2022-10-31 20:08:12 +0900

    feat: 2 중복 암호문 해독기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        while (!cryptogram.equals(decrypt(cryptogram))) {
+            cryptogram = decrypt(cryptogram);
+        }
+        return cryptogram;
+    }
+
+    private static String decrypt(String cryptogram) {
+
+        StringBuilder sb = new StringBuilder();
+
+        if (cryptogram.isEmpty())
+            return "";
+
+        for(int i = 0; i < cryptogram.length()-1; i++){
+            if(cryptogram.charAt(i) != cryptogram.charAt(i+1))
+                sb.append(cryptogram.charAt(i));
+            if(cryptogram.charAt(i) == cryptogram.charAt(i+1))
+                i++;
+        }
+        if(cryptogram.charAt(cryptogram.length()-2) != cryptogram.charAt(cryptogram.length()-1)){
+            sb.append(cryptogram.charAt(cryptogram.length()-1));
+        }
+
+        return sb.toString();
     }
 }
