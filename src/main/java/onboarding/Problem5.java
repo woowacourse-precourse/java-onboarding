d37c4ddd8501e55a0f7192c3d3f2f4dbdf316a13
cocomozila <joseyun94@naver.com>
2022-10-31 19:41:00 +0900

    [문제 5] 각 반환 하고 싶은 금액 배열 CHANGE_MONEY_LIST

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    // 각 반환 하고 싶은 금액 배열 CHANGE_MONEY_LIST
+    private static final int[] CHANGE_MONEY_LIST = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
         return answer;
     }
 }
