46a0265af4cef57a223245144188a4dbd9e42ad4
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-10-31 14:26:45 +0900

    chore(Problem5): add comments outlining list of features

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    // TODO: class Wallet
+        // TODO: Wallet(int money)
+
+        // TODO: countCurrency()
+
     public static List<Integer> solution(int money) {
+        // Check valid input
+
+        // Make wallet
+
+        // County currency in wallet
+
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
