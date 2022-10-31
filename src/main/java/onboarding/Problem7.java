ec5f31e1a8e5adfb6bd7c2c0816cc4c87563cf28
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-31 17:14:13 +0900

    Add initRecommendScoreList function

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,7 +10,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         HashMap<String, ArrayList<String>> friendGraph = initFriendGraph();
-
+        ArrayList<HashMap<String, Integer>> recommendScoreList = initRecommendScoreList();
         return answer;
     }
 
@@ -20,11 +20,16 @@ public class Problem7 {
     }
 
     // 친구관계를 추가하는 함수
-    public static void addFriend(HashMap<String, ArrayList<String>> friendGraph, ArrayList<String> friends) {
+    public static void addFriends(HashMap<String, ArrayList<String>> friendGraph, ArrayList<String> friends) {
         for (int i = 0; i < 2; i++) {
             if (friendGraph.keySet().contains(friends.get(i%2))) {
                 friendGraph.get(friends.get(i%2)).add(friends.get((i+1) % 2));
             }
         }
+    }
+
+    // user의 추천친구에 대한 정보를 저장할 ArrayList를 초기화하는 함수
+    public static ArrayList<HashMap<String, Integer>> initRecommendScoreList() {
+        return new ArrayList<HashMap<String, Integer>>();
     }
 }
