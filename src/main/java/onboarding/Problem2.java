40598e5f379de8956884fd2837e1ad3e6217363a
소대훈 <theogns0681@naver.com>
2022-10-31 18:12:00 +0900

    [feat] (문제2) 중복 문자 찾기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,7 +8,6 @@ public class Problem2 {
         deleteDuplicate(answer);
         return answer;
     }
-
     /*
         duplicateWord가 비어있는 문자열("")이라면
         연속적인 알파벳이 없다.
@@ -20,4 +19,23 @@ public class Problem2 {
             deleteDuplicate(answer);
         }
     }
+    /*
+         단어 하나를 시작으로
+         연속적인 단어의 개수만큼
+         문자열로 만드는 과정
+         ex) 단어 "a"가 2개가 중복된다면 문자열 "aa"로 반환
+        그러나 단어의 개수가 1개라면 중복된 단어가 아니어서 빈 문자열 ""을 반환
+    */
+    public static String findDuplicate(String answer){
+        char word; //연속이 될 단어의 첫 번째
+        StringBuilder sb;
+        for (int startPoint = 0 ; startPoint< answer.length()-1 ;startPoint++){
+            word = answer.charAt(startPoint);
+            sb = new StringBuilder();
+            sb.append(String.valueOf(word).repeat(getDuplicateLength(word, startPoint)));
+            if (sb.length()>=2) return sb.toString();
+        }
+        return "";
+    }
+
 }
