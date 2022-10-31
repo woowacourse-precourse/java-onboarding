f02fe328f64fb93f0958a89d4e7cb86624776b44
jisoolee7101@gmail.com <phllo154@naver.com>
2022-10-31 15:02:23 +0900

    중복 제거 후 answer에 대입 case1 통과했으나 case2 통과 X

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,39 +2,49 @@ package onboarding;
 
 public class Problem2 {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         String testStr = "browoanoommnaon";
 
         solution(testStr);
     }
+
+     */
     public static String solution(String cryptogram) {
         String answer = "answer";
         String cryptoCleared = "";
         boolean flagDupli = true;
         int idxCrypto = 0;
 
-        for(int i = idxCrypto; i < cryptogram.length(); i++) {
-            if(i+1 == cryptogram.length()) {
-                cryptoCleared += cryptogram.charAt(i);
-                break;
-            }
+        while (flagDupli) {
 
-            if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
-                for(int j = i+1; j < cryptogram.length(); j++) {
-                    if(cryptogram.charAt(i) != cryptogram.charAt(j)) {
-                        //System.out.println(j);
-                        i = j-1;
-                        break;
+            flagDupli = false;
+
+            for (int i = 0; i < cryptogram.length(); i++) {
+                if (i + 1 == cryptogram.length()) {
+                    cryptoCleared += cryptogram.charAt(i);
+                    break;
+                }
+
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    flagDupli = true;
+
+                    for (int j = i + 1; j < cryptogram.length(); j++) {
+                        if (cryptogram.charAt(i) != cryptogram.charAt(j)) {
+                            //System.out.println(j);
+                            i = j - 1;
+                            break;
+                        }
                     }
+                } else {
+                    cryptoCleared += cryptogram.charAt(i);
                 }
             }
-            else {
-                cryptoCleared += cryptogram.charAt(i);
-            }
+            cryptogram = cryptoCleared;
+            cryptoCleared = "";
+            System.out.println(cryptogram);
         }
 
-        System.out.println(cryptoCleared);
-
+        answer = cryptogram;
         return answer;
     }
 }
