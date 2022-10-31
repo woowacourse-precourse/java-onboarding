07c7bc86e3f038b07fa13ab73fa3732f09925fa2
siwookim97 <tldn12gh@gmail.com>
2022-11-01 03:02:31 +0900

    feat : problem7) 사용자와 친구인 사람을 구하는 기능 추가
    사용자와 친구인 사람을 구하는 기능
    사용자 아이디와 친구 관게정보 리스트를 받은 후
    사용자와 친구인 리스트를 반환한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    /**
+     * 사용자와 친구인 사람을 구하는 메서드
+     *
+     * @param user 사용자 아이디
+     * @param friends 친구 관계 정보
+     * @return 사용자와 친구인 리스트
+     */
+    public List<String> createUserFriendsList(String user, List<List<String>> friends) {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> friendList = friends.get(i);
+
+            if (friendList.get(0) == user) {
+                list.add(friendList.get(1));
+            } else if (friendList.get(1) == user) {
+                list.add(friendList.get(0));
+            }
+        }
+
+        return list;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
