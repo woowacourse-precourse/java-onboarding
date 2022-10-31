5bfc02f72752682b0980d53fca2c0ddde7fc0bdb
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-31 09:37:15 +0900

    Revert "[문제6] 답안 오름차순 정렬"
    This reverts commit bc1cbbe506e5a70c281bdc530998c695fdcd0a67.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -41,12 +41,6 @@ public class Problem6 {
                 }
             }
         }
-        answer.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
-            }
-        });
 
         return answer;
     }
