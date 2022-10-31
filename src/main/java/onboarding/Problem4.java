9fd9ddb355c88fe7021e9ffa6bc2b2071e7f20e5
채우석 <wochae@wochae-MacBookAir.local>
2022-10-31 21:22:20 +0900

    [feat] 4.1 대소문자 알파벳 문자열 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+    public static final String UPPER_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWER_ALPHA = "abcdefghijknlmopqrstuvwxyz";
     public static String solution(String word) {
+
         String answer = "";
+
         return answer;
     }
 }
