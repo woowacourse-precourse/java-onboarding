aae4d65543412bdb94d5c2625fe28ba318fa002a
cpot5620 <=cpot5620@naver.com>
2022-10-31 23:11:17 +0900

    Refactor 'User' and 'Score' class
    Just apply convention.

--- src/main/java/onboarding/Score.java
+++ src/main/java/onboarding/Score.java
@@ -3,19 +3,19 @@ package onboarding;
 public class Score {
     private int score;
 
-    public Score(){
+    public Score() {
         this.score = 0;
     }
 
-    public int getScore(){
+    public int getScore() {
         return this.score;
     }
 
-    public void addFriendScore(){
-        this.score +=10;
+    public void addFriendScore() {
+        this.score += 10;
     }
 
-    public void addVisitorScore(){
-        this.score +=1;
+    public void addVisitorScore() {
+        this.score += 1;
     }
 }
