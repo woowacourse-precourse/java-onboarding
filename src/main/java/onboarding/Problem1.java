d994b91d29ce75ef688911a53be33100c8cb4722
YUSEONJUN <seonjun0906@gmail.com>
2022-10-31 22:00:55 +0900

    [Problem 1] Problem1 result

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,7 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = 0;
+
         if (pobi.size() !=2 || crong.size() != 2 ||pobi.get(1) - pobi.get(0) != 1
             || crong.get(1) - crong.get(0) != 1 || pobi.get(0) == 1 || crong.get(0)== 1
             || pobi.get(1) == 2 || crong.get(1) == 2 || pobi.get(0) == 399
@@ -13,8 +14,10 @@ class Problem1 {
             || pobi.get(0) % 2 != 1 || crong.get(0) % 2 != 1 || pobi.get(1) % 2 != 0 || crong.get(1) % 2 != 0) {
             return -1;
         }
+
         int pobimax = Math.max(calculate(pobi.get(0)), calculate(pobi.get(1)));
         int crongmax = Math.max(calculate(crong.get(0)), calculate(crong.get(1)));
+
         if (pobimax > crongmax) {
             answer = 1;
         } else if (pobimax < crongmax) {
@@ -24,9 +27,6 @@ class Problem1 {
 
         return answer;
     }
-
-
-}
 
     public static int calculate(int n) {
         int sum = 0;
