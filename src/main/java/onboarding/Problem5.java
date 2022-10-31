b0d9815bc631822c9bcdff6cc7dfa11c304a0bd8
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:23:44 +0900

    5번: 돈 액수 크기 있는 불변 리스트 static으로 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int OMAN = 0;
+    private static final int MAN = 1;
+    private static final int CHEON= 2;
+    private static final int OBAEK = 3;
+    private static final int BEAK = 4;
+    private static final int OSHIP = 5;
+    private static final int SHIP = 6;
+    private static final int O = 7;
+    private static final int IL = 8;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
