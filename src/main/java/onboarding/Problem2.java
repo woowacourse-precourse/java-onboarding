876fba546ece602993f2a0da7fd537756a84e6b7
yoonjuho <dbswn990@naver.com>
2022-10-31 19:21:14 +0900

    feat: PROBLEM2 해독과정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         if(hasException(cryptogram)){
             return null;
         }
+        List<String> crytogram = new ArrayList<>(Arrays.asList(cryptogram.split("")));
+        return getDecryption(crytogram)
+                .stream()
+                .collect(Collectors.joining(""));
     }
 
     public static boolean hasException(String crytogram) {
@@ -13,5 +22,20 @@ public class Problem2 {
         else if (crytogram.equals(crytogram.toLowerCase()) == false)
             return true;
         return false;
+    }
+
+    public static List<String> getDecryption(List<String> cryptogram){
+        int currentIndex=0;
+        while(cryptogram.size() > currentIndex && cryptogram.size() > currentIndex+1){
+            int nextIndex = currentIndex+1;
+            if (cryptogram.get(currentIndex).equals(cryptogram.get(nextIndex)) == true) {
+                cryptogram.remove(currentIndex);
+                cryptogram.remove(currentIndex);
+                currentIndex = 0;
+                continue;
+            }
+            currentIndex = nextIndex;
+        }
+        return cryptogram;
     }
 }
