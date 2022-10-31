026aa55f21487f1a573df3be0cb7a8cd6302f51e
minju412 <ln8847@ajou.ac.kr>
2022-10-31 17:33:09 +0900

    style: [p5] apply checkstyle

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,15 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>(Collections.emptyList());
-        int[] unit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+	public static List<Integer> solution(int money) {
+		List<Integer> answer = new ArrayList<>(Collections.emptyList());
+		int[] unit = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        for (int i : unit) {
-            answer.add(money / i);
-            money %= i;
-        }
+		for (int i : unit) {
+			answer.add(money / i);
+			money %= i;
+		}
 
-        return answer;
-    }
+		return answer;
+	}
 }
