1687f086b7e6fc478ba60f0b0cb839f8014e379c
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-31 19:01:53 +0900

    refactor : (Problem6) 관심사 분리
    관심사에 따라 클래스 및 이넘으로 역할을 분리한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,70 +11,121 @@ public class Problem6 {
     private static List<List<String>> snList = new ArrayList<>();
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
-        Map<String, Integer> snAndCountMap = new HashMap<>();
-        snList = new ArrayList<>();
+        List<String> answer = List.of();
 
-        makeSnList(forms);
-        makeCount(snAndCountMap);
-        makeAnswer(forms, snAndCountMap, answer);
-
-        Collections.sort(answer);
+        CrewCalculator crewCalculator = new CrewCalculator(forms);
+        answer = crewCalculator.calculation();
 
         return answer;
     }
 
-    /*
-     * 이름에서 두 자리 이상의 부분 문자열을 구해 리스트로 반환한다.
-     * */
-    private static List<String> subName(String name) {
-        List<String> sub = new ArrayList<>();
-        int n = name.length();
-        for (int i = 2; i <= n; i++) { // 크기
-            for (int j = 0; i + j <= n; j++) { // 위치
-                if (n - j >= 2) {
-                    sub.add(name.substring(j, i + j));
+    public enum Crew{
+        EMAIL(0), NAME(1);
+
+        private int info;
+
+        Crew(int lo) {
+            this.info = lo;
+        }
+
+        public int getInfo() {
+            return info;
+        }
+    }
+
+    public static class CrewCalculator{
+        private List<List<String>> forms;
+        private static final int REPEAT = 2;
+
+        public CrewCalculator(List<List<String>> forms) {
+            this.forms = forms;
+        }
+
+        public List<String> calculation(){
+            List<String> answer = new ArrayList<>();
+            Map<String, Integer> snAndCountMap = new HashMap<>();
+            snList = new ArrayList<>();
+
+            makeSnList(forms);
+            makeCount(snAndCountMap);
+            makeAnswer(forms, snAndCountMap, answer);
+
+            Collections.sort(answer);
+
+            return answer;
+        }
+
+        private static String getInfo(List<String> form, Crew crew){
+            return form.get(crew.getInfo());
+        }
+
+        /*
+         * 이름에서 두 자리 이상의 부분 문자열을 구해 리스트로 반환한다.
+         * */
+        private static List<String> subName(String name) {
+            List<String> sub = new ArrayList<>();
+            int n = name.length();
+            for (int i = REPEAT; i <= n; i++) { // 크기
+                for (int j = 0; i + j <= n; j++) { // 위치
+                    isSizeTwoOrMore(name, i, j, sub);
                 }
             }
+            return sub;
         }
-        return sub;
-    }
 
-    /*
-     * 각 이름에서 부분 문자열을 구한다.
-     * */
-    private static void makeSnList(List<List<String>> forms){
-        for (List<String> form : forms) {
-            String name = form.get(1);
-            snList.add(subName(name));
+        /*
+        * 크기가 REPEAT보다 큰 부분 문자열을 추가한다.
+        * */
+        private static void isSizeTwoOrMore(String name, int i, int j, List<String> sub){
+            if(name.length()-j >= REPEAT){
+                sub.add(name.substring(j, i + j));
+            }
         }
-    }
 
-    /*
-     * 구해진 부분 문자열로 개수를 센다.
-     * */
-    private static void makeCount(Map<String, Integer> snAndCountMap){
-        for (List<String> strings : snList) {
+        /*
+         * 각 이름에서 부분 문자열을 구한다.
+         * */
+        private static void makeSnList(List<List<String>> forms){
+            for (List<String> form : forms) {
+                String name = getInfo(form, Crew.NAME);
+                snList.add(subName(name));
+            }
+        }
+
+        /*
+         * 구해진 부분 문자열로 개수를 센다.
+         * */
+        private static void makeCount(Map<String, Integer> snAndCountMap){
+            for (List<String> strings : snList) {
+                partition(strings, snAndCountMap);
+            }
+        }
+
+        /*
+        * 부분 문자열의 개수를 Map에 저장한다.
+        * */
+        private static void partition(List<String> strings,
+                                      Map<String, Integer> snAndCountMap){
             for (String par : strings) {
                 snAndCountMap.put(par, snAndCountMap.getOrDefault(par, 0) + 1);
             }
         }
-    }
 
-    /*
-     * 부분 문자열의 길이가 2 이상일 경우 정답에 추가한다.
-     * */
-    private static void makeAnswer(List<List<String>> forms,
-                                   Map<String, Integer> snAndCountMap,
-                                   List<String> answer){
+        /*
+         * 부분 문자열의 길이가 2 이상일 경우 정답에 추가한다.
+         * */
+        private static void makeAnswer(List<List<String>> forms,
+                                       Map<String, Integer> snAndCountMap,
+                                       List<String> answer){
 
-        Person : for (int i = 0; i < snList.size(); i++) {
-            for (int j = 0; j < snList.get(i).size(); j++) {
-                int val = snAndCountMap.get(snList.get(i).get(j));
-                if (val >= 2) {
-                    String email = forms.get(i).get(0);
-                    answer.add(email);
-                    continue Person;
+            Person : for (int i = 0; i < snList.size(); i++) {
+                for (int j = 0; j < snList.get(i).size(); j++) {
+                    int val = snAndCountMap.get(snList.get(i).get(j));
+                    if (val >= REPEAT) {
+                        String email = getInfo(forms.get(i), Crew.EMAIL);
+                        answer.add(email);
+                        continue Person;
+                    }
                 }
             }
         }
