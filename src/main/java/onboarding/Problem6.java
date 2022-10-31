c3beb8a24f8140b7cf29c0fa0e65ca834b4a6445
leesangjin <tkdwls0628@gmail.com>
2022-10-31 19:26:33 +0900

    P6, substringMap을 순회하며 value가 2 이상인(중복된) 닉네임의 이메일을 추출하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -25,6 +25,15 @@ public class Problem6 {
 
     private static void extractDuplicateSubstring(List<String> answer, List<List<String>> forms,
                                                   Map<String, Integer> substringMap) {
-
+        for (List<String> crew : forms) {
+            String email = crew.get(0);
+            String nickname = crew.get(1);
+            for (int i = 0; i < nickname.length()-1; i++) {
+                String substring = nickname.substring(i, i + 2);
+                if (substringMap.get(substring) > 1 && !answer.contains(email)) {
+                    answer.add(email);
+                }
+            }
+        }
     }
 }
