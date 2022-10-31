5970e8b72fc30ac02b0e5d40b56f5731cfe12577
eora21 <eora21@naver.com>
2022-10-31 20:50:00 +0900

    refactor: 💡 변경 쓰이지 않는 기존 static 메서드 제거
    옳지 않은 알고리즘으로 작성되어 더 이상 쓰이지 않는 메서드들을 삭제.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,54 +4,6 @@ import java.util.Deque;
 
 public class Problem2 {
 
-    private static void checkCryptogramCharsUsingIdx(Deque<Character> decryptionCharStack, String cryptogram) {
-        int idx = 0;
-
-        while (idx < cryptogram.length()) {
-            int newIdx = increaseIdxWhenCharEqual(decryptionCharStack.peekLast(), cryptogram, idx);
-            idx = deleteOrAppendDecryption(decryptionCharStack, cryptogram, idx, newIdx);
-        }
-    }
-    private static int increaseIdxWhenCharEqual(Character decryptionLastChar, String cryptogram, int idx) {
-
-        while (decryptionLastChar != null && idx < cryptogram.length() && decryptionLastChar == cryptogram.charAt(idx)) {
-            idx++;
-        }
-
-        return idx;
-    }
-
-    private static int deleteOrAppendDecryption(Deque<Character> decryptionCharStack, String cryptogram, int idx, int newIdx) {
-
-        if (idx != newIdx) {
-            deleteLastDecryptionChar(decryptionCharStack);
-            return newIdx;
-        }
-
-        appendDecryptionChar(decryptionCharStack, cryptogram, newIdx);
-        return idx + 1;
-    }
-
-    private static void deleteLastDecryptionChar(Deque<Character> decryptionCharStack) {
-        decryptionCharStack.removeLast();
-    }
-
-    private static void appendDecryptionChar(Deque<Character> decryptionCharStack, String cryptogram, int newIdx) {
-        if (newIdx < cryptogram.length()) {
-            decryptionCharStack.addLast(cryptogram.charAt(newIdx));
-        }
-    }
-
-    private static String decryptionCharStackToString(Deque<Character> decryptionCharStack) {
-        StringBuilder sb = new StringBuilder();
-
-        for (char ch : decryptionCharStack) {
-            sb.append(ch);
-        }
-
-        return sb.toString();
-    }
-
     /**
      * 임의의 문자열 cryptogram이 매개변수로 주어질 때, 연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 메서드를 완성하라.
      *
