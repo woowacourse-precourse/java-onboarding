8222ab74a74ac8c66cf29e6122a0b33b0e3cb0d8
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-10-31 18:46:42 +0900

    feat: add function frog

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
+    public static ArrayList<Character> frog(String word) {
+
+        char[] charArr = word.toCharArray();
+        ArrayList<Character> result = new ArrayList<Character>();
+        for (int i = 0; i < charArr.length; i++) {
+            if (Character.isUpperCase(charArr[i])) {
+                char after = (char) Math.abs(155 - (int) (charArr[i]));
+                result.add(after);
+            } else if (Character.isLowerCase(charArr[i])) {
+                char after = (char) Math.abs(219 - (int) (charArr[i]));
+                result.add(after);
+            } else if ((charArr[i]) == ((char) (32))) {
+                result.add((charArr[i]));
+            }
+            else continue;
+        }
+        return result;
+    }
     public static String solution(String word) {
+
         String answer = "";
         return answer;
     }
