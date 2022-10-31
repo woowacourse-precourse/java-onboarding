d5c912a1f6902b0357b506cf6005fa957215ab0c
박시균\psk98 <psg980331@naver.com>
2022-10-31 11:33:17 +0900

    feat : 각 닉네임을 분할해서 단어를 만들어준다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,7 +10,28 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        //특정 단어를 가지고있는 이메일리스트
+        HashMap<String, List<String>> checkDuplicateWord = new HashMap<>();
+
+        /*
+        1.각 닉네임을 분할해서 단어를 만든다.
+        2.각 단어를 키로하고 단어를 포함하는 이메일을 값으로 만들어준다.
+        3.모든 닉네임을 반복해서 돌린후 특정단어를 포함하는 이메일이 2개이상인 단어를 찾아준다.
+         */
+        for(int i = 0 ; i < forms.size() ; i++){
+            String email = forms.get(i).get(0);
+            String name = forms.get(i).get(1);
+
+            for(int j = 0 ; j < name.length() - 1 ; j++){
+                String tempStr = name.substring(j,j+2);
+
+
+            }
+        }
+
+
         return answer;
     }
 }
