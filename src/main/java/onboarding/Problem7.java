2bf008c364dd8c31ad7b4c94d22a7d1bf6cfaaa1
daeun <gain3436@gmail.com>
2022-11-01 05:31:53 +0900

    [feat](7) 친구관계 해시맵 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    private static HashMap<String, List<String>> friendsRelation(List<List<String>> friends) {
+        HashMap<String, List<String>> relation = new HashMap<String, List<String>>();
+        for (List<String> twoFriends : friends) {
+            List<String> basic = new ArrayList<String>();
+            String person1 = twoFriends.get(0);
+            String person2 = twoFriends.get(1);
+            List<String> friends1 = relation.getOrDefault(person1, basic);
+            List<String> friends2 = relation.getOrDefault(person2, basic);
+            friends1.add(person2);
+            friends2.add(person1);
+            relation.put(person1, friends1);
+            relation.put(person2, friends2);
+        }
+        return relation;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
