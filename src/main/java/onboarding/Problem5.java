045a768cb9e62948fdd3094f112564ba9f3d0f9d
이정우 <acg6138@naver.com>
2022-11-01 00:23:35 +0900

    feat(Problem5): 금액을 큰 화폐순으로 변환하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,23 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+    
+    public static List<Integer> exchangeMoney(List<Integer> moneys, int money) {
+        int idx = 0;
+        int cnt = 0;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < moneys.size(); i++) {
+            cnt = money / moneys.get(idx);
+            if (cnt == 0) {
+                result.add(idx, cnt);
+                idx += 1;
+                continue;
+            }
+            money -= cnt * moneys.get(idx);
+            result.add(idx, cnt);
+            idx += 1;
+        }
+        return result;
     }
 }
