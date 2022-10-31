9403ee34adbbfbede181777f10d3f796fc6b825a
KimRiun <kimriun27@gmail.com>
2022-11-01 05:22:53 +0900

    FEAT: Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,49 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+
+        while (checkForDuplication(answer)) {
+            answer = removeDuplication(answer);
+        }
+
         return answer;
     }
+
+    // 중복이 있는지만 검사
+    static boolean checkForDuplication (String cryptogram) {
+        for(int i = 1; i < cryptogram.length(); i++) {
+            if(cryptogram.charAt(i) == cryptogram.charAt(i-1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static String removeDuplication (String cryptogram) {
+        String answer = "";
+        boolean dup = false;
+
+        // 마지막 문자는 i+1이 배열 밖이므로 length-1 미만 까지 반복
+        for(int i = 0; i < cryptogram.length()-1; i++){
+            if(cryptogram.charAt(i) != cryptogram.charAt(i+1)) {
+                if (dup) {
+                    dup = false;
+                    continue;
+                }
+                answer += cryptogram.charAt(i);
+            }
+            else {
+                dup = true;
+            }
+        }
+
+
+        if(!dup) {
+            answer += cryptogram.charAt(cryptogram.length()-1);
+        }
+
+        return answer;
+    }
+
 }
--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -8,50 +8,50 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest {
-    @Nested
-    class Problem1Test {
-        @Test
-        void case1() {
-            List<Integer> pobi = List.of(97, 98);
-            List<Integer> crong = List.of(197, 198);
-            int result = 0;
-            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
-        }
-
-        @Test
-        void case2() {
-            List<Integer> pobi = List.of(131, 132);
-            List<Integer> crong = List.of(211, 212);
-            int result = 1;
-            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
-        }
-
-        @Test
-        void case3() {
-            List<Integer> pobi = List.of(99, 102);
-            List<Integer> crong = List.of(211, 212);
-            int result = -1;
-            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
-        }
-    }
-
 //    @Nested
-//    class Problem2Test {
+//    class Problem1Test {
 //        @Test
 //        void case1() {
-//            String cryptogram = "browoanoommnaon";
-//            String result = "brown";
-//            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+//            List<Integer> pobi = List.of(97, 98);
+//            List<Integer> crong = List.of(197, 198);
+//            int result = 0;
+//            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
 //        }
 //
 //        @Test
 //        void case2() {
-//            String cryptogram = "zyelleyz";
-//            String result = "";
-//            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+//            List<Integer> pobi = List.of(131, 132);
+//            List<Integer> crong = List.of(211, 212);
+//            int result = 1;
+//            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+//        }
+//
+//        @Test
+//        void case3() {
+//            List<Integer> pobi = List.of(99, 102);
+//            List<Integer> crong = List.of(211, 212);
+//            int result = -1;
+//            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
 //        }
 //    }
-//
+
+    @Nested
+    class Problem2Test {
+        @Test
+        void case1() {
+            String cryptogram = "browoanoommnaon";
+            String result = "brown";
+            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
+
+        @Test
+        void case2() {
+            String cryptogram = "zyelleyz";
+            String result = "";
+            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
+    }
+
 //    @Nested
 //    class Problem3Test {
 //        @Test
