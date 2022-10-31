b8026c11fb3b1bef1d62c714685b9ab35a6288ca
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-01 03:16:38 +0900

    feat: 최대값 구하기, fix 변수 줄이기
    Signed-off-by: joohyeongKim <wnguddl96@knou.ac.kr>

--- src/main/java/onboarding/problem1/EachSide.java
+++ src/main/java/onboarding/problem1/EachSide.java
@@ -2,7 +2,7 @@ package onboarding.problem1;
 
 import java.util.List;
 
-// 각 페이지 번호 단순하게 구하기
+// refactor : 각 페이지 번호 구하기 기능 추출
 public class EachSide {
     public static int pageLeft(List<Integer> player){return player.get(0);}
     public static int pageRight(List<Integer> player){return player.get(1);}
