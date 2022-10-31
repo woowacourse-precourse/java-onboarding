bd9766823e832f3c9cd003b0990d52d17fcc6253
taeyun-work <wmfajr1357@gmail.com>
2022-10-31 16:46:38 +0900

    [Refac] Problem2 - split by function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,17 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        boolean flag = false;
+        return removeDupl(cryptogram);
+    }
+
+    public static List<Integer> checkDupl(String cryptogram){
         int startIdx = 0;
         int endIdx = 0;
+        boolean flag = false;
         for (int i = 0; i < cryptogram.length() - 1; i++) {
             if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
                 if (flag == false) {
@@ -19,10 +26,37 @@ public class Problem2 {
             }
         }
 
-        if (flag == false) {
+        if (flag == true && startIdx >= endIdx) {
+            endIdx = cryptogram.length() - 1;
+        }
+        return Arrays.asList(startIdx, endIdx);
+    }
+
+    public static String removeDupl(String cryptogram){
+        List<Integer> duplIdx = checkDupl(cryptogram);
+        int startIdx = duplIdx.get(0);
+        int endIdx = duplIdx.get(1);
+        String removeDuplStr;
+//        for (int i = 0; i < cryptogram.length() - 1; i++) {
+//            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+//                if (flag == false) {
+//                    startIdx = i;
+//                }
+//                flag = true;
+//            } else {
+//                if (flag == true) {
+//                    endIdx = i;
+//                    break;
+//                }
+//            }
+//        }
+        if (startIdx == 0 && endIdx == 0) {
             return cryptogram;
         }
 
-        return solution(cryptogram.replaceAll(cryptogram.substring(startIdx, endIdx + 1), ""));
+        removeDuplStr = cryptogram.replaceAll(cryptogram.substring(startIdx, endIdx + 1), "");
+
+        return removeDupl(removeDuplStr);
     }
+
 }
