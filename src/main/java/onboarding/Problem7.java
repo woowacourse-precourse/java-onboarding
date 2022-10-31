3adfcd5360735275d89bb9063516f5d63c2d86a0
leesangjin <tkdwls0628@gmail.com>
2022-10-31 20:41:42 +0900

    P7, user와 함께 아는 친구 점수 계산 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -36,7 +36,19 @@ public class Problem7 {
 
     private static void calculateRelationshipScore(String user,
                                                    Map<String, List<String>> eachUserFriendMap, Map<String, Integer> userScore) {
-
+        for (String finalUserFriend : eachUserFriendMap.get(user)) {
+            for (String friend : eachUserFriendMap.get(finalUserFriend)) {
+                // user 제외, user와 이미 친구인 사용자 제외
+                if (!friend.equals(user) && !eachUserFriendMap.get(user).contains(friend)) {
+                    // 여러 친구와 함께 아는 사용자의 경우, 기존 점수 + 10
+                    if (userScore.containsKey(friend)) {
+                        userScore.put(friend, userScore.get(friend) + 10);
+                    } else {
+                        userScore.put(friend, 10);
+                    }
+                }
+            }
+        }
     }
 
     private static void calculateVisitorScore(String user, List<String> visitors,
