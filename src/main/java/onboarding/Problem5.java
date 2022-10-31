7c4f273c8a2578e3a432475c6e1fcf7d10e6de95
myeongju00 <j51677767@gmail.com>
2022-11-01 02:16:28 +0900

    refactor: 필요없는 import 구문 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,12 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        int[] moneyArr = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] moneyArr = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         for(int i : moneyArr) {
             int charge = diviedMoney(money, i);
             answer.add(charge);
