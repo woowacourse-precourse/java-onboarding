e43664f8cecf08d4f32253dff8ee9cd38c659caf
juhwang <juhong4930@naver.com>
2022-10-31 22:47:12 +0900

    refactor(Problem3):removing some methods

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,31 +4,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Problem3 {
-    private static final int[] GAME_369_BOARD = new int[10001];
+    private static Integer[] GAME_369_BOARD;
+
     public static int solution(int number) {
-        game369Board();
-        int answer = numberOnGame369Board(number);
-        return answer;
+        return game369Board()[number];
     }
-    public static int numberOnGame369Board(int number){
-        return GAME_369_BOARD[number];
-    }
-    public static String[] numToString(int num){
-        return String.valueOf(num).split("(?<=.)");
-    }
+
     public static int validCheck369(int num){
-        List<String> numToString = List.of(numToString(num));
-        return numToString.stream()
-                          .filter(value->List.of("3","6","9").contains(value))
-                          .collect(Collectors.toList())
-                          .size();
+        List<String> numToString = List.of(String.valueOf(num).split("(?<=.)"));
+        return (int) numToString.stream()
+                .filter(value -> List.of("3", "6", "9").contains(value)).count();
     }
-    public static void markClapNumber(int num){
-        GAME_369_BOARD[num] = GAME_369_BOARD[num-1]+validCheck369(num);
-    }
-    public static void game369Board(){
+
+    public static Integer[] game369Board(){
+        GAME_369_BOARD = new Integer[10001];
         for (int i = 3; i < 10001; i++) {
             markClapNumber(i);
         }
+        return GAME_369_BOARD;
+    }
+
+    public static void markClapNumber(int num){
+        GAME_369_BOARD[num] = GAME_369_BOARD[num-1]+validCheck369(num);
     }
 }
