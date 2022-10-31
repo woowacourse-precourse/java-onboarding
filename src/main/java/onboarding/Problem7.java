b857a44d02a14c74376ff709802607295b119a93
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-31 10:35:11 +0900

    comment: add function list of problem 7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Problem 7 function list
+ * - Create relations from friends list
+ * - Create score list of each friend candidates
+ * --> if candidate is a friend of user's friend add 10 point
+ * --> if candidate visited user's SNS add 1 point
+ * - Sort candidate list (Descending order of score -> Ascending order of name)
+ * - Check maximum 5 new friends
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
