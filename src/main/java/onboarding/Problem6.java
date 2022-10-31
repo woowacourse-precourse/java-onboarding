2aa5f854b4512029701e29f1737328bfd2c9ca0e
juhee park <jouhi3322@gmail.com>
2022-10-31 21:12:32 +0900

    Refactor: Problem6 주석추가,메서드 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,37 +12,49 @@ import java.util.*;
 public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer;
-        HashMap<String,String> allSubNickname = new HashMap<>();
-        HashSet<String> duplicateCrewEmail= new HashSet<>();
+        HashMap<String, String> allSubNickname = new HashMap<>();
+        HashSet<String> duplicateCrewEmail = new HashSet<>();
 
-        for (List<String> form : forms) {
-            String crewEmail = form.get(0);
-            String crewNickname = form.get(1);
-
-            for (int j = 0; j < crewNickname.length() - 1; j++) {
-                String subNickname = crewNickname.substring(j, j + 2);
-
-                if (allSubNickname.containsKey(subNickname)) {
-                    //현재 닉네임과 그전에 저장된 닉네임을 duplicateCrewEmail 에 저장함
-                    saveDuplicateCrewEmail(crewEmail, duplicateCrewEmail);
-                    saveDuplicateCrewEmail(allSubNickname.get(subNickname), duplicateCrewEmail);
-                }
-                //모든 부분 닉네임 저장
-                saveSubNickname(subNickname, crewEmail, allSubNickname);
-            }
-
+        for (List<String> crew : forms) {
+            String crewEmail = crew.get(0), crewNickname = crew.get(1);
+            //중복되는 문자가 있는 크루인 경우 이메일 저장
+            checkingDuplicateNickname(allSubNickname, duplicateCrewEmail, crewEmail, crewNickname);
         }
-        answer = new ArrayList<>(duplicateCrewEmail);
-        Collections.sort(answer);
 
+        return changeEmailhashToSortarray(duplicateCrewEmail);
+    }
+
+    //중복 닉네임이 있는 이메일을 추가한다.
+    private static void checkingDuplicateNickname(HashMap<String, String> allSubNickname, HashSet<String> duplicateCrewEmail, String crewEmail, String crewNickname) {
+        for (int i = 0; i < crewNickname.length() - 1; i++) {
+            String subNickname = crewNickname.substring(i, i + 2);
+            //같은 부분닉네임이 이미 존재한다면 크루의 이메일을 저장한다.
+            if (allSubNickname.containsKey(subNickname)) {
+                saveDuplicateCrewsEmail(allSubNickname, duplicateCrewEmail, crewEmail, subNickname);
+            }
+            //모든 부분닉네임저장
+            saveSubNickname(subNickname, crewEmail, allSubNickname);
+        }
+    }
+
+    private static void saveDuplicateCrewsEmail(HashMap<String, String> allSubNickname, HashSet<String> duplicateCrewEmail, String crewEmail, String subNickname) {
+        saveDuplicateCrewEmail(crewEmail, duplicateCrewEmail);
+        saveDuplicateCrewEmail(allSubNickname.get(subNickname), duplicateCrewEmail);
+    }
+
+    //이메일 정렬 및 리스트 변환
+    private static List<String> changeEmailhashToSortarray(HashSet<String> duplicateCrewEmail) {
+        List<String> answer = new ArrayList<>(duplicateCrewEmail);
+        Collections.sort(answer);
         return answer;
     }
 
+    //모든 부분닉네임 저장
     private static void saveSubNickname(String subName, String email, HashMap<String, String> allSubNickname) {
-        allSubNickname.put(subName,email);
+        allSubNickname.put(subName, email);
     }
 
+    //이메일 저장
     private static void saveDuplicateCrewEmail(String email, HashSet<String> duplicateCrewEmail) {
         duplicateCrewEmail.add(email);
     }
