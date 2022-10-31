0b8b99e950c2131c754d306cd23ea94cc784a56d
HyebinShin <hyeviin.dev@gmail.com>
2022-10-31 19:36:06 +0900

    대소문자 배열

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,11 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        //대소문자 배열
+        System.out.println("====solution====");
+        char[] lower = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] upper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
         String answer = "";
         return answer;
     }
