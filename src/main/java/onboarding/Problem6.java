b7f7fd78e0dd3d15cd2216e92dc9e3ac92dc2c40
plusjob70 <plusjob70@gmail.com>
2022-10-31 13:57:18 +0900

    feat: form에서 이름과 이메일 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        return new ArrayList<>();
+    }
+
+    private static String getEmail(List<String> form) {
+        return form.get(0);
+    }
+
+    private static String getName(List<String> form) {
+        return form.get(1);
     }
 }
