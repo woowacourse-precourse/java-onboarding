f66f939f71b6c8eb27694d3775f219373b977d08
cookienc <kim76744@naver.com>
2022-10-31 19:12:48 +0900

    feat(Problem5): Problem5 기능 요구 사항 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,54 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return Calculator.calculateMoney(money);
+    }
+
+    public enum Calculator {
+        FIFTY_THOUSAND(50_000),
+        TEN_THOUSAND(10_000),
+        FIVE_THOUSAND(5_000),
+        THOUSAND(1_000),
+        FIVE_HUNDRED(500),
+        HUNDRED(100),
+        FIFTY(50),
+        TEN(10),
+        ONE(1);
+
+        private final int money;
+
+        Calculator(int money) {
+            this.money = money;
+        }
+
+        public int getMoney() {
+            return money;
+        }
+
+        public static List<Integer> calculateMoney(int account) {
+            Calculator[] types = Calculator.values();
+
+            List<Integer> money = new ArrayList<>();
+            for (Calculator type : types) {
+                int count = getCount(type, account);
+                money.add(count);
+                account -= type.getMoney() * count;
+            }
+
+            return money;
+        }
+
+        public static int getCount(Calculator money, int account) {
+            int count = 0;
+            while (account - money.getMoney() >= 0) {
+                account -= money.getMoney();
+                count++;
+            }
+            return count;
+        }
     }
 }
