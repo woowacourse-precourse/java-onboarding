842bb86de27e2703bfbbb0741ca6b34662a272f9
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 21:30:51 +0900

    [Problem 2] Complete main stream

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,13 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
         /* 
         결국 연속되어 pop되는 문자들을 모두 지우면 됨
         1. 문자열을 순회하며 중복을 체크하여 삭제하는 함수 작성 ( return string )
         2. while 루프를 돌며 삭제가 이루어지지 않을 때 까지 반복
         */
-        return answer;
+        while(true){
+            String temp = deleteContinuity(cryptogram);
+            if ( temp.length() == cryptogram.length()) break;
+            cryptogram = temp;
+        }
+        return cryptogram;
     }
     /*
     암호 해독 순서를 중간 skip할 수 있지만, 해독 결과는 같다.
