24b0b4365df56a44210016fb112a4d9c5bdc6a79
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:27:33 +0900

    [Problem2] 기능목록 작성하기 (김민서)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem2 {
+    /**
+     [기능목록]
+     1. 연속하는 중복 문자 찾기 (없을 때 까지 찾기)
+     2. 중복 문자를 공백으로 바꾸기
+     3. 공백 문자 제거하여 결과 리턴하기
+     */
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
