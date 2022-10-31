c9a40dffd64a0489a748365dcf8db415cffc79a1
MyunghyunNero <mh1kim0910@gmail.com>
2022-10-31 12:52:14 +0900

    Refactoring: [Problem6] Map 으로 substring 받아 성능 개선

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -15,24 +13,23 @@ public class Problem6 {
     문제 조건 사항 체크
      */
     private static boolean checkLimit(List<List<String>> forms){
-        boolean check=true;
         //크루 1명부터 10000까지
-        if(!(forms.size()>=1&&forms.size()<=10000)) check=false;
+        if(!(forms.size()>=1&&forms.size()<=10000)) return false;
         for(List<String> s: forms){
             String email=s.get(0);
             //이메일 길이 11부터 20까지
-            if(!(email.length()>=11&&email.length()<=20)) check= false;
+            if(!(email.length()>=11&&email.length()<=20)) return false;
 
             //도메인은 email.com
             int idx=email.indexOf("@");
             String domain=email.substring(idx+1);
-            if(!(domain.contains("email.com"))) check= false;
+            if(!(domain.contains("email.com"))) return false;
 
             //닉네임은 한글 그리고 길이 1부터 20미만
             String nickName=s.get(1);
-            if(!(nickName.matches("^[가-힣]*$")&&nickName.length()>=1&&nickName.length()<20)) check= false;
+            if(!(nickName.matches("^[가-힣]*$")&&nickName.length()>=1&&nickName.length()<20)) return false;
         }
-        return check;
+        return true;
     }
 
     /*
@@ -40,16 +37,15 @@ public class Problem6 {
      */
     private static List<String> duplicateNickName(List<List<String>> forms){
         List<String> result=new ArrayList<>();
-        int size= forms.size();
-        for(int i=0;i<size;i++){
-            String str1=forms.get(i).get(1);   //닉네임 가져오기
-            String email=forms.get(i).get(0); //이메일 가져오기
-            for(int k=0;k<size;k++){
-                if(i!=k){
-                    String str2=forms.get(k).get(1);
-                    if(checkDuplicate(str1,str2)&&email.contains("email.com")){
+        Map<String,List<String>> subMap=checkDuplicate(forms);
+        for(int i=0;i< forms.size();i++){
+            String email = forms.get(i).get(0);
+            String nickName = forms.get(i).get(1);
+            if(nickName.length()>1) {
+                for (int k = 0; k < nickName.length() - 1; k++) {
+                    String subNick = nickName.substring(k, k + 2);
+                    if(subMap.get(subNick).size()>1){
                         result.add(email);
-                        break;
                     }
                 }
             }
@@ -61,13 +57,21 @@ public class Problem6 {
     /*
     중복된 이름 찾기
      */
-    private static boolean checkDuplicate(String str1,String str2){
-        for(int i=0;i<str1.length()-1;i++){
-            String temp=str1.substring(i,i+2);
-            if(str2.contains(temp)){
-                return true;
+    private static Map<String,List<String>> checkDuplicate(List<List<String>> forms){
+        Map<String ,List<String> > subMap=new HashMap<>();
+        for(int i=0;i<forms.size();i++) {
+            String email = forms.get(i).get(0);
+            String nickName = forms.get(i).get(1);
+
+            if (nickName.length() > 1) {
+                for (int k = 0; k < nickName.length() - 1; k++) {
+                    String subNick = nickName.substring(k, k + 2);
+                    List<String> list = subMap.getOrDefault(subNick, new ArrayList<>());
+                    list.add(email);
+                    subMap.put(subNick, list);
+                }
             }
         }
-        return false;
+        return subMap;
     }
 }
