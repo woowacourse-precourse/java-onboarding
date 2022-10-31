b39c40ec2697cc2747f59d3e9d1909623dead816
2tsumo <sherlockid8@naver.com>
2022-10-31 22:16:07 +0900

    [problrm2] 인접한 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,40 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        if (cryptogram == null || cryptogram.length() == 0) {
+            return cryptogram;
+        }
+
+        char[] chars = cryptogram.toCharArray();
+
+        int i, k = 0;
+
+        for (i = 1; i < chars.length; i++)
+        {
+            if (chars[i - 1] != chars[i]) {
+                chars[k++] = chars[i - 1];
+            }
+            else {
+                while (i < chars.length && chars[i - 1] == chars[i]) {
+                    i++;
+                }
+            }
+        }
+
+        if(k!=0){
+            chars[k++] = chars[i - 1];
+
+            String s = new String(chars).substring(0, k);
+
+            if (k != chars.length) {
+                return solution(s);
+            }
+            return s;
+        }else return "";
+
     }
 }
+
+
