59194c9aa2c7d8772d249b698d9d3d2d2e240072
이석환 <2018008886@hanyang.ac.kr>
2022-11-01 00:35:29 +0900

    refactor: Problem5: 변수 및 함수명 변경
    * numOfPaperAndCoin -> numsOfPaperAndCoin
    * countLeastNumOfPaperAndCoin -> countLeastNumsOfPaperAndCoin

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,23 +1,22 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        return countLeastNumOfPaperAndCoin(money);
+        return countLeastNumsOfPaperAndCoin(money);
     }
 
     private static final int[] paperAndCoins = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-    private static List<Integer> countLeastNumOfPaperAndCoin(int money) {
-        List<Integer> numOfPaperAndCoin = new ArrayList<>();
+    private static List<Integer> countLeastNumsOfPaperAndCoin(int money) {
+        List<Integer> numsOfPaperAndCoin = new ArrayList<>();
         for (int paperAndCoin : paperAndCoins) {
-            numOfPaperAndCoin.add(money / paperAndCoin);
+            numsOfPaperAndCoin.add(money / paperAndCoin);
             money = money % paperAndCoin;
         }
 
-        return numOfPaperAndCoin;
+        return numsOfPaperAndCoin;
     }
 }
