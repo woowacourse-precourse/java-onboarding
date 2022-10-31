d6aea63e023a969afbc25c356d0091950c62b42f
subin Park <psb0320@hs.ac.kr>
2022-10-31 18:23:13 +0900

    feat: 승부 결과 반환 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
 
         // 양쪽 페이지 중 더 큰 수를 본인의 점수로 저장
         int pobiMax = Math.max(getCalMax(pobi.get(0)), getCalMax(pobi.get(1)));
         int crongMax = Math.max(getCalMax(crong.get(0)), getCalMax(crong.get(1)));
 
-        return answer;
+        return getResult(pobiMax, crongMax);
     }
 
     public static int getCalMax(int page) {
@@ -24,5 +23,11 @@ class Problem1 {
         }
 
         return Math.max(sum, muti);
+    }
+
+    public static int getResult(int pobiMax, int crongMax) {
+        if (pobiMax > crongMax) return 1;
+        else if (pobiMax < crongMax) return 2;
+        return 0;
     }
 }
