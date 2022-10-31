b27ef53a4d09565b978332fc918a8c484d8e6daf
KimYongJun <rladydwns129@naver.com>
2022-11-01 00:54:26 +0900

    feat(Problem4): Split String & Rebuild String

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char currentLetter = word.charAt(i);
+            sb.append(makeFrogWord(currentLetter));
+        }
+
+        answer = sb.toString();
         return answer;
     }
 }
