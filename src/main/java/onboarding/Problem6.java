88f0a95cc90911a0335485e2f6d892703715da52
leesangjin <tkdwls0628@gmail.com>
2022-10-31 19:27:58 +0900

    P6, 리스트 정렬 및 구현된 메소드들을 통한 정답 도출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String, Integer> substringMap = new HashMap<>();
+
+        initializeSubstringMap(forms, substringMap);
+        extractDuplicateSubstring(answer, forms, substringMap);
+        Collections.sort(answer);
+
         return answer;
     }
 
