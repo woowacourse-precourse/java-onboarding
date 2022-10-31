99580485171869654bf3fb8fde62e926649c0722
bellljun3395 <195850@jnu.ac.kr>
2022-10-31 16:53:05 +0900

    feat : User 클래스 멤버변수 선언 및 getter 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+}
+
+class User {
+
+    private final String id;
+
+    User(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
 }
