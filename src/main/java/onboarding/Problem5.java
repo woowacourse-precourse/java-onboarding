1ad6ca068644dfd4b3ab9a04d10a705e69065844
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-10-31 20:25:00 +0900

    feat: add function withdraw

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,29 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int[] withdraw(int money){
+        int oman = money / 50000;
+        money = money % 50000;
+        int man = money / 10000;
+        money = money % 10000;
+        int ochun = money / 5000;
+        money = money % 5000;
+        int chun = money / 1000;
+        money = money % 1000;
+        int obaek = money / 500;
+        money = money % 500;
+        int baek = money / 100;
+        money = money % 100;
+        int osib = money / 50;
+        money = money % 50;
+        int sib = money / 10;
+        int ill = money % 10;
+
+        return new int[]{oman, man,ochun,chun,obaek,baek,osib,sib,ill};
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         return answer;
     }
 }
