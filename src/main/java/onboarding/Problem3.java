fe330760942265bbb3b7ade5d023553e880918da
ryanha <ryanha@handong.ac.kr>
2022-10-31 15:13:31 +0900

    Feat[Problem3] : 입력받은 수를 문자열로 변환하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // 1. 주어진 숫자(number)를 문자열로 변경함
+        String numStr = numberToString(number);
+
         return answer;
+    }
+
+    // 입력받은 수를 문자열로 변환하여 반환함
+    private static String numberToString(int number) {
+        return Integer.toString(number);
     }
 }
