f79e471cb81d3eb72006a540ca87db73f0cc15b1
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-10-31 16:32:33 +0900

    feat: solve problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final List<Integer> papers
+            = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for (int paper : papers) {
+            int paperCount = 0;
+            while (money >= paper) {
+                money -= paper;
+                paperCount += 1;
+            }
+            answer.add(paperCount);
+        }
         return answer;
     }
 }
