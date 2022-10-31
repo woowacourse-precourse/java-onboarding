2cb6d1275cd6cdabacd1e19784b69aa063b216da
Be-student <presentsong@naver.com>
2022-10-31 17:46:29 +0900

    feat(Problem1): 곱셈 점수 계산 기능 추가

---
+++ src/main/java/onboarding/problem1/MultipleCalculator.java
@@ -0,0 +1,15 @@
+package onboarding.problem1;
+
+import java.util.List;
+
+class MultipleCalculator {
+    private final List<Integer> division;
+
+    public MultipleCalculator(List<Integer> division) {
+        this.division = division;
+    }
+
+    public int getScore() {
+        return division.stream().reduce(1, (total, number) -> total * number);
+    }
+}
