f607102cbc543d3dc940888b17fd8b01b7707b7c
LeeJinHo <hosboy93@naver.com>
2022-10-31 22:38:49 +0900

    문제5. 나누는 값의 앞자리를 이용해 지폐&동전 구분

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -14,6 +14,16 @@ public class Problem5 {
         for(int i=0; i<wallet.length; i++) {
         	wallet[i] = money/nowUnit;
         	money = money%nowUnit;
+        	
+        	if(firstNum==5) {
+        		nowUnit = nowUnit/5;
+        		firstNum = 1;
+        	}else if(firstNum==1 && nowUnit==10) {
+        		nowUnit = nowUnit/10;
+        	}else {
+            	nowUnit = nowUnit/2;
+            	firstNum = 5;
+        	}
         }
         
         return answer;
