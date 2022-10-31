43c16676605f3c457f2a59780b45ec5c49b75d67
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 00:36:25 +0900

    feat(PROBLEM1): validPages() 유효성 검사 + 최종결과

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,29 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        return 0;
+        if (!validPages(pobi) || !validPages(crong))
+            return -1;
+
+        int pobi_score = pageCom(pobi);
+        int crong_score = pageCom(crong);
+
+        //최종 결과 출력
+        if (pobi_score > crong_score)    return 1;
+        else if (pobi_score < crong_score)  return 2;
+        else    return 0;
+    }
+
+    // 예외처리
+    public static boolean validPages(List<Integer> pageList){
+        //유효한 페이지
+        if (pageList.get(0) +1 != pageList.get(1))
+            return false;
+        //2~399 (처음과 마지막을 제외한 유효한 책 범위)
+        for(int i=0; i<2; i++){
+            if(pageList.get(i) <= 1 || pageList.get(i) >= 400)
+                return false;
+        }
+        return true;
     }
 
     // 페이지의 각 자리 수의 합과 곱 중 큰 수를 구하는 기능
