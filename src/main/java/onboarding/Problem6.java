a1a28d6315099f8c93c24c756daaf3d9b26d18ad
Damgom <wisewish1@gmail.com>
2022-10-31 13:37:07 +0900

    Feat: nickname to token list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    public static List<String> token(String nickName) {
+        List<String> tokens = new ArrayList<>();
+        for(int i = 0; i < nickName.length()-1; i++) {
+            tokens.add(nickName.substring(i, i+2));
+        }
+        return tokens;
     }
 }
