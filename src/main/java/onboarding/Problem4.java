0c91e3f9fdde579b2e427401e3f8f5c40829cdb3
채우석 <wochae@wochae-MacBookAir.local>
2022-10-31 21:59:11 +0900

    [feat] 4.2 반대로 정렬한 문자열로 대칭적인 반환값 만들도록 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,12 +1,35 @@
 package onboarding;
 
-public class Problem4 {
-    public static final String UPPER_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final String LOWER_ALPHA = "abcdefghijknlmopqrstuvwxyz";
-    public static String solution(String word) {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Problem4 {
+    public static String UPPER_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String LOWER_ALPHA = "abcdefghijknlmopqrstuvwxyz";
+    public static final String TOKEN = UPPER_ALPHA + LOWER_ALPHA + " ";
+    public static String solution(String word) {
+        Map<Character, Character> alphaMap = new HashMap<>();
+        List<Character> originSrc = new ArrayList<>();
+
+
+        backwardMap(alphaMap);
         String answer = "";
 
         return answer;
+    }
+
+    public static String reverseAlpha() {
+        String result = "";
+
+
+        return result;
+    }
+    public static void backwardMap(Map<Character, Character> alphaMap) {
+        for (int i = 0; i < 26; i++) {
+            alphaMap.put((char) ('A' + i), (char) ('Z' - 1));
+            alphaMap.put((char) ('a' + i), (char) ('z' - 1));
+        }
     }
 }
