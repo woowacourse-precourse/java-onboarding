e7d30f7dd5d42a37217e09cb1cc9370986647256
pdot715 <pdot715@gmail.com>
2022-10-31 20:43:23 +0900

    feat(문제3):1부터 입력값까지 올라가면서 3, 6, 9의 개수만큼 손뼉 치기 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int now_num = 0;
+
+        // 1부터 number까지 손뼉을 몇 번 치는지
+        for(int i = 1; i <= number; i++) {
+            now_num = i;
+
+            // 현재 숫자가 i일때 손뼉을 몇 번 치는지 answer에 더해주기
+            while(now_num > 0) {
+                // 0이 아니면서 3의 배수(3, 6, 9)일때 손뼉 치기
+                if(((now_num % 10) != 0) && (((now_num % 10) % 3) == 0)) {
+                    answer++;
+                }
+                now_num /= 10;
+            }
+        }
+
         return answer;
     }
 }
