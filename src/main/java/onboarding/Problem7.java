9c0748ec42f0eec3be9423d3e549b833ad1641c8
h0ngwon <w___on@naver.com>
2022-10-31 18:50:06 +0900

    유저의 친구관계 생성하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Set<String> friendSet = new HashSet<>();
+    static Map<String, Integer> map = new HashMap<>();
+
+    public static Set<String> userFriends (String user, List<List<String>> friends) {
+        Set<String> userFriendSet = new HashSet<>();
+
+        for(List<String> f : friends) {
+            String f1 = f.get(0);
+            String f2 = f.get(1);
+
+            if(f1.equals(user)) {
+                userFriendSet.add(f2);
+            }else if(f2.equals(user)) {
+                userFriendSet.add(f1);
+            }
+        }
+        return userFriendSet;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        friendSet = userFriends(user, friends);
+
+        for(List<String> friend : friends) {
+            String f1 = friend.get(0);
+            String f2 = friend.get(1);
+
+            if(f1.equals(user) || f2.equals(user)) {
+                continue;
+            }
+        }
         return answer;
     }
 }
