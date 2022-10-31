61ab3d1f79756e82a660bc74bece0292ddcdf1ec
aossuper7 <aossuper7@naver.com>
2022-10-31 22:33:32 +0900

    feat: 7번 각각의 사용자를 key값, 함께 아는 친구를 value값으로 저장하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
+        HashMap<String, List<String>> mates = new HashMap<>();
+        HashMap<String, Integer> score = new HashMap<>();
+        List<String> result = new ArrayList<>();
+
+        for (List<String> name : friends) {
+            if (!mates.containsKey(name.get(0))) {
+                List<String> friend = new ArrayList<>();
+                mates.put(name.get(0), friend);
+            }
+            if (!mates.containsKey(name.get(1))) {
+                List<String> friend = new ArrayList<>();
+                mates.put(name.get(1), friend);
+            }
+            List<String> oneFriend = mates.get(name.get(0));
+            oneFriend.add(name.get(1));
+
+            List<String> twoFriend = mates.get(name.get(1));
+            twoFriend.add(name.get(0));
+        }
+
+
     }
 }
