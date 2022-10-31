9a4d42cda208e081deabd43e7063465bc22ea8e0
dydtmd6801 <dydtmd6801@naver.com>
2022-10-31 11:29:24 +0900

    feat : 두 글자 이상의 문자가 연속적으로 중복일 경우, 중복 문자 확인 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        getKey(forms);
         return answer;
+    }
+
+    // 중복되는 문자 찾아내기 => key 찾는 과정
+    private static String getKey(List<List<String>> forms){
+        for(int i = 0; i < forms.size(); i++){
+            String keyName = forms.get(i).get(1);
+            for(int j = 2; j < keyName.length() + 1; j++){
+                String key = keyName.substring(0, j);
+                for(int k = i + 1; k < forms.size(); k++){
+                    if(forms.get(k).get(1).contains(key)){
+                        return key;
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
