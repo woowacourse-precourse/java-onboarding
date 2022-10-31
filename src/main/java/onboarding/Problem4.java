f00047436a5dc38a2476caab271f04d5d5c3e11b
cmw9957 <cmw9957@gmail.com>
2022-10-31 12:06:18 +0900

    comment: 기능 목록 주석 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/*
+========================================Problem4 기능 목록========================================
+- 문자열을 문자열 배열로 변환하는 기능
+- 해당 문자가 알파벳인지 확인하는 기능
+- 알파벳을 반대로 변환하는 기능
+- 문자열 배열을 문자열로 변환하는 기능
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
