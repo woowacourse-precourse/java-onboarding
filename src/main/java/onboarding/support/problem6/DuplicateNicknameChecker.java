a8c67018cebffba533199b0b478ce42f4cfdc87d
kariskan <zkfzpf56@naver.com>
2022-10-31 11:13:38 +0900

    feat: 닉네임 중복 검사 클래스 생성 및 멤버 변수 작성

---
+++ src/main/java/onboarding/support/problem6/DuplicateNicknameChecker.java
@@ -0,0 +1,16 @@
+package onboarding.support.problem6;
+
+import java.util.List;
+
+public class DuplicateNicknameChecker {
+
+    private List<String> DuplicateUserEmails;
+
+    public DuplicateNicknameChecker() {
+
+    }
+
+    public List<String> getDuplicateEmails() {
+        return DuplicateUserEmails;
+    }
+}
