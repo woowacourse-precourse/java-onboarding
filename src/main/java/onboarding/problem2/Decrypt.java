d5460739b13f20f46664532a7df71fb22e4b9392
SeolYoungKim <nasur4da@gmail.com>
2022-10-31 15:16:49 +0900

    refactor(P2): validation을 Decrypt 내에서 수행

--- src/main/java/onboarding/problem2/Decrypt.java
+++ src/main/java/onboarding/problem2/Decrypt.java
@@ -1,6 +1,7 @@
 package onboarding.problem2;
 
-import static onboarding.problem2.CryptogramValidator.*;
+import static onboarding.common.CommonMessage.PERMIT_ONLY_LOWERCASE;
+import static onboarding.common.CommonValidator.isOnlyContainsLowerCase;
 
 public class Decrypt {
 
@@ -13,5 +14,23 @@ public class Decrypt {
     public String doDecrypt(String cryptogram) {
         validateCryptogram(cryptogram);
         return duplicateProcessor.checkDuplication(cryptogram.toCharArray());
+    }
+
+    private void validateCryptogram(String cryptogram) {
+        if (!isValidLength(cryptogram)) {
+            throw new IllegalArgumentException("문자의 길이는 1 이상 1,000 이하여야 합니다.");
+        }
+
+        if (!isOnlyContainsLowerCase(cryptogram)) {
+            throw new IllegalArgumentException(PERMIT_ONLY_LOWERCASE.message());
+        }
+    }
+
+    private boolean isValidLength(String cryptogram) {
+        final int MAX_VALUE = 1_000;
+        final int MIN_VALUE = 1;
+        int length = cryptogram.length();
+
+        return MIN_VALUE <= length && length <= MAX_VALUE;
     }
 }
