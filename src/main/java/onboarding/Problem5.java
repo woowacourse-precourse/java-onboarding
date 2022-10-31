0581cb1771da0807e22db1d34574269e2a471782
baekeunsun <eunsun5460@gmail.com>
2022-10-31 16:57:53 +0900

    [문제5] feats: input 예외확인

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        if(CheckInput(money)){
+        }
+        throw new IllegalArgumentException("잘못된 입력값");
+    }
+
+    /* 기능1 : input 확인 */
+    private static boolean CheckInput(int money){
+        if(money > 0 && money < 1000001){
+            return true;
+        }return false;
     }
 }
