e73f2f377330975d0654114bb13af580d66432ee
Brian <bwook9908@gmail.com>
2022-11-01 01:11:21 +0900

    [문제5] 리팩토링 1차 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,26 +1,35 @@
-package onboarding;
+/*
+ * Problem5
+ *
+ * v1.1
+ *
+ * 2022.11.01
+ *
+ * 저작권 주의
+ */
 
+package onboarding;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-//        List<Integer> answer = Collections.emptyList();
         ArrayList<Integer> answer = new ArrayList<Integer>();
-        int[] arr={50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] standardMoneyArray={50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         int remainMoney=money;
-        for (int j : arr) {
-            answer.add(quotientAndRemainder(remainMoney, j)[0]);
-            remainMoney = quotientAndRemainder(remainMoney, j)[1];
+
+        for (int standardMoney : standardMoneyArray) {
+            answer.add(quotientAndRemainder(remainMoney, standardMoney)[0]);
+            remainMoney = quotientAndRemainder(remainMoney, standardMoney)[1];
         }
+
         return answer;
     }
 
-    static int[] quotientAndRemainder(int number, int with){
+    static int[] quotientAndRemainder(int number, int numberToDivide){
         int[] array= new int[2];
-        int quotient = number / with;
-        int remainder= number % with;
+        int quotient = number / numberToDivide;
+        int remainder= number % numberToDivide;
         array[0]= quotient;
         array[1]= remainder;
         return array;
