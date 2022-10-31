6a529cbb816136eb62f4041f444292e75ab9fd80
farmJun <junpreme1205@gmail.com>
2022-10-31 15:30:57 +0900

    Problem1. 필요한 상수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,12 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    static final int POBI_WIN = 1;
+    static final int CRONG_WIN = 2;
+    static final int DRAW = 0;
+    static final int EXCEPTION = -1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
         //예외처리
         if (!isPossible(pobi) || !isPossible(crong)) {
-            return answer = -1;
+            return EXCEPTION;
         }
 
         int pobi_score = findHighScore(pobi);
