2493228b3b48629a8ce3f54924089c576e114251
1eejisoo <jisu991012@gmail.com>
2022-10-31 17:13:00 +0900

    [Problem 7] Feat: 사용자의 친구 목록 생성 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> friendList = new ArrayList<>();   // 사용자의 친구 목록
+        Map<String, Integer> recommendedFriends = new HashMap<>();   // 친구 추천 목록
+
+        createFriendList(user, friends, friendList);
+        checkFriendOfFriend(user, friends, friendList, recommendedFriends);
+        checkVisitors(visitors, recommendedFriends);
+        makeRecommendFriendsList(recommendedFriends, friendList, answer);
+
         return answer;
     }
+
+    // 1. 사용자의 친구 목록 생성
+    public static void createFriendList(String user, List<List<String>> friends, List<String> friendList) {
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                for (String name : friend) {
+                    if (!name.equals(user)) friendList.add(name);
+                }
+
+            }
+        }
+    }
+
 }
