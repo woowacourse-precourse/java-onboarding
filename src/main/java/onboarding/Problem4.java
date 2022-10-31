acca02145587522c6b0794f5862c43dbc6278496
권민정 <22alswjd@naver.com>
2022-11-01 02:01:54 +0900

    feat: Problem4 기능 목록 주석 추가
    기능 목록을 작성한 주석 추가함.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,13 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        // 1. 변환된 i번째 문자를 저장할 character 변수 선언
+        // 2. i번째 문자가 대문자인 경우
+        // 3. i번째 문자가 소문자인 경우
+        // 4. i번째 문자가 알파벳이 아닌 경우
+        // 5. answer에 변환된 문자 추가
+
         return answer;
     }
 }
