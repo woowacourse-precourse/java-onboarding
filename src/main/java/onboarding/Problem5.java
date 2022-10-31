343ee773da87349687fa183826ec9f33dfed0808
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-01 01:26:49 +0900

    Feat(Problem5): 입력받은 숫자가 정상인지 확인

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,34 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static class Money{
+        int cost;
+
+        Money(int cost) throws Exception {
+            if (!isValidCost(cost)) {
+                throw new Exception("Wrong cost");
+            }
+            this.cost = cost;
+        }
+
+        private boolean isValidCost(int cost) {
+            return cost >= 1 && cost <= 1000_000;
+        }
+
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        Money m;
+        try {
+            m = new Money(money);
+        } catch (Exception e) {
+            return answer;
+        }
+
+        money = m.cost;
+
+
         return answer;
     }
 }
