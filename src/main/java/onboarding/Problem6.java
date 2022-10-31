91334228ca9c315cd551b0138ab92367f82934eb
shpark30 <seohyeonpark30.gmail.com>
2022-11-01 00:16:07 +0900

    feat: 도메인 예외 사항 처리 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,11 +6,12 @@ public class Problem6 {
     // HashMap<String, ArrayList<Integer>> charToIndices 생성 : {두 글자 : [닉네임 인덱스]}
     HashMap<String, ArrayList<Integer>> charToIndices = new HashMap<String, ArrayList<Integer>>();
     public static List<String> solution(List<List<String>> forms) {
+        /* 제한 사항 처리 */
+        forms = except(forms);
+
         Problem6 problem = new Problem6();
-        // 탐색 Loop forms
         for (int i = 0; i < forms.size(); i++) {
-            // updateEvery2SequentialChar : 닉네임에서 2글자씩 분절해  charToIndices에 추가
-            problem.updateEvery2SequentialChar(i, forms.get(i).get(1));
+            problem.updateEvery2SequentialChar(i, forms.get(i).get(1)); // 닉네임에서 2글자씩 분절해  charToIndices에 추가
         }
         // charToIndices 에서 길이가 2이상인 Index 배열 반환
         List<Integer> answerIndices = new ArrayList<>();
@@ -30,6 +31,20 @@ public class Problem6 {
         TreeSet<String> distinceAnswer = new TreeSet<>(answer);
         answer = new ArrayList<String>(distinceAnswer);
         return answer;
+    }
+
+    static List<List<String>> except(List<List<String>> forms) {
+        List<List<String>> newForms = new ArrayList<>();
+        for (List<String> form : forms) {
+            if (getEmailDomain(form.get(0)).equals("email.com")) {
+                newForms.add(form);
+            }
+        }
+        return newForms;
+    }
+
+    static String getEmailDomain(String email) {
+        return email.split("@")[1];
     }
 
     void updateEvery2SequentialChar(int formIdx, String nickname) {
