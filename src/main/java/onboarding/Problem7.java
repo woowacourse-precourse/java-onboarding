ffa9b6db9a6f19cb6114ffd5f9b5842de5c5b210
은지 <midzy0228@gmail.com>
2022-10-31 22:13:39 +0900

    feat[Problem7]: 방문자 수 HashMap에 저장 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,36 @@
+/*
+ * 클래스명: Problem7
+ *
+ * 시작 날짜: 2022-10-31
+ */
+
+
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.HashMap;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        visitorsCount(visitors);
+
         return answer;
+    }
+
+    public static HashMap visitorsCount(List<String> visitors) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < visitors.size(); i++) {
+            String visitor = visitors.get(i);
+            if (map.get(visitor) == null) {
+                System.out.println("널 나옴");
+                map.put(visitor, 1);
+                continue;
+            }
+            map.put(visitor, map.get(visitor) + 1);
+        }
+        return map;
     }
 }
