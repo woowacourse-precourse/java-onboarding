64915b4d82562e031cf06afcdc3a87feb3e5af78
cksdud8028 <cksdud8028@naver.com>
2022-10-31 11:39:19 +0900

    FEAT checkingExchangeTermination

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,19 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int fiveMillion = money / 50000;
+        money -= 50000 * fiveMillion;
+        int million = money / 10000;
+        money -= 10000 * million;
         return answer;
     }
+
+    private static boolean checkingExchangeTermination(int balance) {
+        if (balance == 0) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
