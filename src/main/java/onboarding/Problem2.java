6c64282a2af5af5d684502a5e074a1009d68553a
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-31 19:59:50 +0900

    finish Main

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuffer answer = new StringBuffer(cryptogram);
+        int index = 0;
+        while (index < answer.length() - 1) {
+            if (answer.charAt(index) == answer.charAt(index + 1)) {
+                answer.deleteCharAt(index + 1);
+                answer.deleteCharAt(index);
+                index--;
+            } else {
+                index++;
+            }
+        }
+        return answer.toString();
     }
 }
