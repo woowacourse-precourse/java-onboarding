fd90432025a9ec6e2750bf477bf2feb44ef7f490
jindaram-stu <jinsb1999@naver.com>
2022-10-31 23:11:16 +0900

    feat : [문제4] 주어진 문자를 청개구리식으로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static final String spelling = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static Character convert(Character originalChar) {
+        Integer position = spelling.indexOf(Character.toUpperCase(originalChar));
+        Character convertCharacter = spelling.charAt((spelling.length()-1) - position);
+        return convertCharacter;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
 }
