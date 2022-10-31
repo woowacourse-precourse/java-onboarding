5f4fb425794e1476435df75b69a4023389884cb6
gomljo <gomljo0307@naver.com>
2022-10-31 20:48:48 +0900

    feat(Problem4.java): 기능 #6 구현
    A~Z까지 알파벳 배열을 생성하는 makeAlphabetArray 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+
+class FrogDictionary{
+    final char FIRST_ALPHABET='a';
+    final int NUMBER_OF_ALPHABET=26;
+    HashMap<Character, Character> frogDictionary;
+
+    char[] makeAlphabetArray(){
+        char[] alphabetArray=new char[NUMBER_OF_ALPHABET];
+        for(int i=0;i<NUMBER_OF_ALPHABET;i++){
+            alphabetArray[i] = (char)((int)FIRST_ALPHABET + i);
+        }
+        return alphabetArray;
+    }
+}
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
