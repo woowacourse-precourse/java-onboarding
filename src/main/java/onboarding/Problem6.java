3d52e500651e9bcc28f68504cdf676ad0277c58e
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-31 21:41:54 +0900

    Feat(Problem6): 닉네임조각 해시맵 생성기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String, List<String>> FragmentsMap = new HashMap<>();
+
+        makeFragmentsMap(forms, FragmentsMap);
         return answer;
     }
+    private static void makeFragmentsMap(List<List<String>> forms,
+        Map<String, List<String>> nickNameFragmentsMap) {
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickName = form.get(1);
+            for (int i = 0; i < nickName.length() - 1; i++) {
+                String nickNameFragment = "" + nickName.charAt(i) + nickName.charAt(i + 1);
+                putValue(nickNameFragmentsMap, email, nickNameFragment);
+            }
+        }
+    }
+
+    private static void putValue(Map<String, List<String>> nickNameFragmentsMap, String email,
+        String nickNameFragment) {
+    }
+
 }
