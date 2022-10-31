bfa428d8c8da85362997d42331cb495eec166ed3
hodomaroo <enghemdwkd@gmail.com>
2022-10-31 20:41:47 +0900

    [Problem3/FIX] test 과정에서 발견한 getClapTimeOfNumber 메서드의 구현을 수정하고, Game369 클래스 내부의 테스트 코드를 제거하였습니다.

--- src/main/java/onboarding/problem3/Game369.java
+++ src/main/java/onboarding/problem3/Game369.java
@@ -1,10 +1,6 @@
 package onboarding.problem3;
 
-import java.lang.reflect.Type;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-public class Clap{
+public class Game369 {
 
     public static boolean checkNumberIs369(int number){
         return (number == 3 || number == 6 || number == 9);
@@ -17,18 +13,11 @@ public class Clap{
 
 
         for(char digit : digitsOfNumber.toCharArray()){
-            if(checkNumberIs369(Character.digit(digit,Character.DECIMAL_DIGIT_NUMBER))){
+            if(checkNumberIs369(Character.digit(digit,10))){
                 clapTimeOfNumber += 1;
             }
         }
         return clapTimeOfNumber;
     }
 
-    @Test
-    @DisplayName("Config")
-    void test(){
-        System.out.println(getClapTimeOfNumber(933256155));
-        System.out.println(getClapTimeOfNumber(3613622));
-        System.out.println(getClapTimeOfNumber(6131355));
-    }
 }
