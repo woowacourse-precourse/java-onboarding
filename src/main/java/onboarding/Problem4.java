38beb8435de20f562c71bd64f8bdb1467628a38c
kssumin <201566@jnu.ac.kr>
2022-10-31 22:00:24 +0900

    feat : word 길이 제한

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,3 +6,28 @@ public class Problem4 {
         return answer;
     }
 }
+
+class Word {
+    private String letters;
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
+    private static final String NOT_ALLOWED_UNDER_MIN_LENGTH = "주어진 word의 길이는 1이상입니다.";
+    private static final String NOT_ALLOWED_OVER_MAX_LENGTH = "주어진 word의 길이는 1000이하입니다.";
+
+    public Word(String letters) {
+        this.letters = letters;
+        validateLength();
+    }
+
+    private void validateLength() {
+        if (letters.length() < MIN_LENGTH) {
+            throw new IllegalStateException(NOT_ALLOWED_UNDER_MIN_LENGTH);
+        }
+
+        if (letters.length() > MAX_LENGTH) {
+            throw new IllegalStateException(NOT_ALLOWED_OVER_MAX_LENGTH);
+        }
+    }
+}
+
+
