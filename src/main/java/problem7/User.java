262adc6ac6f5bb98a5792ac35b73f865b180a2a0
parkmuhyeun <pjhg410@gmail.com>
2022-10-31 14:37:18 +0900

    feat: [Problem7] 사용자와 함께 아는 친구를 가진 유저 확인

--- src/main/java/problem7/User.java
+++ src/main/java/problem7/User.java
@@ -5,10 +5,39 @@ import java.util.HashMap;
 import java.util.List;
 
 public class User {
-    String name;
+    private String name;
 
     public User(String name) {
         this.name = name;
+    }
+
+    private void checkHaveSameFriends(FriendDto friendDto, boolean[] isFriend, HashMap<String, Integer> score) {
+        if (hasConnection(name, getConnection(friendDto))) {
+            check(isFriend, score, friendDto);
+        }
+    }
+
+    private void check(boolean[] isFriend, HashMap<String, Integer> score, FriendDto friendDto) {
+        HashMap<String, Integer> connection = getConnection(friendDto);
+        List<List<String>> friends = getFriends(friendDto);
+        int userId = getUserId(name, connection);
+        isFriend[userId] = true;
+        List<String> userFriends = friends.get(userId);
+        
+        for (int i = 0; i < userFriends.size(); i++) {
+            int friendId = getUserId(getFriend(userFriends, i), connection);
+            isFriend[friendId] = true;
+            List<String> friendFriends = friends.get(friendId);
+
+            addScore(score, friendFriends);
+        }
+    }
+
+    private void addScore(HashMap<String, Integer> score, List<String> friendFriends) {
+        for (int j = 0; j < friendFriends.size(); j++) {
+            String friend = getFriend(friendFriends, j);
+            score.put(friend, score.getOrDefault(friend, 0) + 10);
+        }
     }
 
     private int addFriends(List<List<String>> friends, FriendDto friendDto, int cnt) {
@@ -40,15 +69,15 @@ public class User {
     private int setConnection(FriendDto friendDto, int cnt, String User) {
         HashMap<String, Integer> connection = getConnection(friendDto);
 
-        if (needConnection(User, connection)) {
+        if (!hasConnection(User, connection)) {
             connection.put(User, cnt++);
             getFriends(friendDto).add(new ArrayList<>());
         }
         return cnt;
     }
 
-    private boolean needConnection(String User, HashMap<String, Integer> connection) {
-        return !connection.containsKey(User);
+    private boolean hasConnection(String User, HashMap<String, Integer> connection) {
+        return connection.containsKey(User);
     }
 
     private HashMap<String, Integer> getConnection(FriendDto friendDto) {
