00e1c93e34e25c17dca22676d0da534b6eceb6ee
jisoolee7101@gmail.com <phllo154@naver.com>
2022-10-31 16:00:29 +0900

    1부터 매개변수까지의 자릿수 구함

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,15 +3,36 @@ package onboarding;
 public class Problem3 {
 
     public static void main(String[] args) {
-        solution(5);
-        solution(53);
-        solution(297);
-        solution(4444);
+        //solution(5);
+        solution(15);
+        //solution(297);
+        //solution(4444);
     }
     public static int solution(int number) {
         int answer = 0;
-        int numLength = (int) Math.log10(number)+1;
-        System.out.println(numLength);
+        int numLength = 0;
+        int countClap = 0;
+
+        for(int i = 1; i <= number; i++) {
+            numLength = (int) Math.log10(i)+1;
+            System.out.println(numLength);
+
+            switch (numLength) {
+                case 1:
+                    System.out.println("one");
+                    break;
+                case 2:
+                    System.out.println("two");
+                    break;
+                case 3:
+                    System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+
+            }
+        }
 
         return answer;
     }
