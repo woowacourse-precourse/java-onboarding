e892c489d13052f2821c978aa55e0a60775d46b7
cmw9957 <cmw9957@gmail.com>
2022-11-01 00:26:00 +0900

    comment: 기능 목록 주석 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,15 @@ package onboarding;
 
 import java.util.List;
 
+/*
+========================================Problem6 기능 목록========================================
+- 2차원 List를 2차원 array로 변환하는 기능
+- 한 닉네임으로부터 모든 경우의 연속되는 2글자를 배열로 만드는 기능
+- 해당 닉네임이 중복 키워드를 포함하는지 확인하는 기능
+- 중복 닉네임에 해당하는 이메일을 set으로 포함하는 기능
+- set을 List로 변환하는 기능
+ */
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
