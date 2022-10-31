ba6e2995a42eeb53e8095058703557b68a92b2c6
donghae <donghae1999@gmail.com>
2022-11-01 00:06:20 +0900

    P6 : 최종 수정, 주석

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,7 +3,7 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
-    public static Set<String> set = new HashSet<String>();
+    private static Set<String> set = new HashSet<String>();
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
@@ -15,13 +15,13 @@ public class Problem6 {
 
         return answer;
     }
-
+    //닉네임 중복 확인 함수 실행 함수
     private static void execCheckDup(List<List<String>> forms){
         for(int i=0; i<forms.size()-1; i++)
             for(int j=i+1; j<forms.size();j++)
                 checkDup(forms.get(i), forms.get(j));
     }
-
+    //닉네임 중복 확인 함수
     private static boolean checkDup(List<String> first,List<String> second){
         String firstName = first.get(1);
         String secondName = second.get(1);
