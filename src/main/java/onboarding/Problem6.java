2a9fad286f242c1e2453a4d1d5605565cfbfea1a
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-31 21:21:16 +0900

    221031 Feat: P6 - 기능2 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -14,11 +16,25 @@ public class Problem6 {
             names.add(data.get(1));
         }
 
-        //2. 어떤 이름 문자열을 선택하여, 그 문자열의 모든 2자 이상 부분문자열을 만든다.
+        for(int i = 0; i < names.size(); i++) {
+            //2. 어떤 이름 문자열을 선택하여, 그 문자열의 모든 2자 이상 부분문자열을 만든다.
+            Set<String> subStrings = createSubString(names.get(i));
 
-        //3. 해당 부분문자열이 다른 이름들에 포함되어 있는지 확인한다.
-        //포함되어 있다면 그 계정의 이메일을 정답 리스트에 담고 다음 반복으로 넘어간다.
+            //3. 해당 부분문자열이 다른 이름들에 포함되어 있는지 확인한다.
+            //포함되어 있다면 그 계정의 이메일을 정답 리스트에 담고 다음 반복으로 넘어간다.
+        }
 
         return answer;
+    }
+
+    public static Set<String> createSubString(String name){
+        Set<String> subStrings = new HashSet<>();
+
+        for(int i = 0; i < name.length() - 1; i++){
+            String subStr = name.substring(i, i + 2);
+            subStrings.add(subStr);
+        }
+
+        return subStrings;
     }
 }
