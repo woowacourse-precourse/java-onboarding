c2283072a1b1d9bd877b1ef148b267075dcf90f4
hwanee__ <fkfkthth@naver.com>
2022-11-01 02:23:29 +0900

    더하기/곱하기/비교_함수구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,8 +5,43 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pnum_left,pnum_right,cnum_left,cnum_right;
+        int max_pleft,max_pright,max_cleft,max_cright;
+
+        pnum_left = pobi.get(0);
+        pnum_right = pobi.get(1);
+        cnum_left = crong.get(0);
+        cnum_right = crong.get(1);
+
+
 
 
         return answer;
     }
+
+    static int add_num(int num){
+        int result=0;
+
+        while(num>0){
+            result += num%10;
+            num /= 10;
+        }
+        return result;
+    }
+
+    static int multi_num(int num){
+        int result=1;
+
+        while(num>0){
+            result *= (num%10);
+            num /= 10;
+        }
+        return result;
+    }
+
+    static int compare_num(int num1,int num2){
+        return Math.max(num1,num2);
+    }
+
+
 }
