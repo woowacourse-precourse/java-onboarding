0b9ac82feb0d31b7085fa847f4344a9632ea00b4
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-10-31 17:52:39 +0900

    Add Function: convert alpha

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem4 {
+    public static char convertAlpha(char alpha, boolean isLower) {
+        int MAX_LOWER = 'a' + 'z';
+        int MAX_UPPER = 'A' + 'Z';
+        int alphaInt;
+
+        alphaInt = isLower ? MAX_LOWER - (int)alpha : MAX_UPPER - (int)alpha;
+        return isLower ? Character.toUpperCase((char)alphaInt) : Character.toLowerCase((char)alphaInt);
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+
+        return answer.toString();
     }
 }
