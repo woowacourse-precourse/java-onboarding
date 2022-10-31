04e6252e6deb425dfb971621c5eeb982420ae868
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-10-31 22:40:03 +0900

    [pb6] fix: 연속된 char저장하는 HashMap 자료형을 List<Character>에서 Set<Character>로 변경. 순서상관없기때문

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,48 +1,45 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Map<String, List<Character>> charChecker = new HashMap<>();
+        Map<Character, Set<Character>> charChecker = new HashMap<>();
         initConsecutiveCharSet(charChecker, forms);
+//        System.out.println(charChecker);
+        List<String> a = new ArrayList<>();
+        return a;
     }
 
-    private static void initConsecutiveCharSet(Map<String, List<Character>> charChecker,
+    private static void initConsecutiveCharSet(Map<Character, Set<Character>> charChecker,
                                                List<List<String>> forms) {
-        forms.forEach(item -> {
-
-        });
+        forms.forEach(item -> addStringToSet(charChecker, item.get(1)));
     }
 
-    private static void addStringToSet(Map<Character, List<Character>> charChecker,
+    private static void addStringToSet(Map<Character, Set<Character>> charChecker,
                                        String name) {
         int i = 0;
         Character keyChar;
         Character nextChar;
-        List<Character> valueList = new ArrayList<>();
 
         while (i < name.length() - 1) {
             keyChar = name.charAt(i);
             nextChar = name.charAt(i+1);
+            Set<Character> valueSet = new HashSet<>();
 
             if (charChecker.containsKey(keyChar)) {
-                valueList = charChecker.get(keyChar);
-                valueList.add(nextChar);
+                valueSet = charChecker.get(keyChar);
+                valueSet.add(nextChar);
             } else {
-                valueList.add(nextChar);
+                valueSet.add(nextChar);
             }
-            charChecker.put(keyChar, valueList);
+            charChecker.put(keyChar, valueSet);
             i++;
         }
     }
-
-    // 2개씩 parsing해서 set에다 다 넣어보자.
-    // 넣을때마다 교집합이 하나라도 있으면 걸린거다.
-    // 제이엠, 제이슨 (제이)
-    // 워니 , 워니맘 (워니)
-    // {제: [이], 이: [엠, 슨], 워: {니], 엠: {제]}
 }
+// 2개씩 parsing해서 set에다 다 넣어보자.
+// 넣을때마다 교집합이 하나라도 있으면 걸린거다.
+// 제이엠, 제이슨 (제이)
+// 워니 , 워니맘 (워니)
+// {제: [이], 이: [엠, 슨], 워: {니], 엠: {제]}
