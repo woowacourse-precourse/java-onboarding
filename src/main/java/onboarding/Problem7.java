eae3273d2e849d2eab764639d4cd300629112b81
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 22:57:57 +0900

    P7 | 결과 반환 (친구 추천)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        answer = recommendList(user, friends, visitors);
         return answer;
+    }
+
+    private static List<String> recommendList(String user,List<List<String>> friends ,List<String> visitors) {
+
+        Map<String , Set<String>> friendMap = getFriendsMap(friends);
+        Map<String , Integer> recommendScoreMap = new HashMap<>();
+
+        setFriendsScore(user, friendMap, recommendScoreMap);
+        setVisitorScore(visitors, recommendScoreMap);
+        deleteUserFriends(user ,friendMap, recommendScoreMap);
+
+        // 추천 점수 정보맵으로 추천 친구 리스트를 받아옴
+        List<String> result = recommendListByScoreMap(recommendScoreMap);
+
+        return result;
     }
 }
