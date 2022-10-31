0f375703876f91c3af8549737a9beec15bef5990
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-01 01:53:09 +0900

    두번째제출

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        int count = 1;
+        String answer = cryptogram;
+        while(count != answer.length()) {
+            count = 1;
+            for (int i = 0; i <answer.length() -1; i++) {
+                if (answer.charAt(i) == answer.charAt(i+1)) {
+                    if (i ==0){
+                        answer = answer.substring(2);
+                    }else {
+                        answer = answer.substring(0,i) + answer.substring(i+2);
+                    }
+                    count += 1;
+                    break;
+                } else {
+                    count += 1;
+                }
+            }
+        }
+
         return answer;
     }
 }
