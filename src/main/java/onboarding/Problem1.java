84801d647ab67c54c1410bbc3690f55b287cebeb
Dongmin Kim <kimdongmin68@naver.com>
2022-10-31 21:22:44 +0900

    feat: 예외 사항 조건 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -23,7 +23,7 @@ class Problem1 {
         int leftPage = bookPages.get(0);
         int rightPage = bookPages.get(1);
 
-        if(leftPage % 2 != 1 || rightPage % 2 != 1) return false;
+        if(leftPage % 2 != 1 || rightPage % 2 == 1) return false;
         if(leftPage<1 || leftPage>400 || rightPage<1 || rightPage>400) return false;
         if(rightPage - leftPage != 1) return false;
 
