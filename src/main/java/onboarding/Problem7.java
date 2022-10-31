c3fca5d860512dd89862e0c5ebfd35e5293233b3
PPakSang <sanghyun-dev@naver.com>
2022-10-31 21:46:03 +0900

    Feat: Problem7 내부 연산 로직(친구 관계 추가) 추가
    follower: 나와 직접적인 친구
    
    followerStorage: 사용자 이름 - 친구 집합 쌍을 저장하는 메모리 저장소
    
    computeRecommendScore(String, List<List<String>>, List<String>): 주어진 정보(친구 관계, 방문자) 를 바탕으로 사용자의 추천 점수를 계산
    
    setFollowers(List<List<String>>): 친구 관계 정보를 바탕으로 사용자 별 친구(follower) 를 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,81 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+class ScoredUser {
+    private int score;
+    private String name;
+
+    ScoredUser(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    void plusScore(int plus) {
+        this.score += plus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScoredUser user = (ScoredUser) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 
 public class Problem7 {
+
+    private static final Map<String, Set<String>> followerStorage = new HashMap<>();
+
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        computeRecommendScore(user, friends, visitors);
+
+
     }
+
+    private static void computeRecommendScore(String user, List<List<String>> friends, List<String> visitors) {
+        setFollowers(friends);
+
+    }
+
+    private static void setFollowers(List<List<String>> friends) {
+        friends.forEach(friendPair -> {
+            String username1 = friendPair.get(0);
+            String username2 = friendPair.get(1);
+
+            Set<String> followers1 = followerStorage.get(username1);
+            Set<String> followers2 = followerStorage.get(username2);
+
+            if (followers1 == null) {
+                followers1 = new HashSet<>();
+                followerStorage.put(username1, followers1);
+            }
+
+            if (followers2 == null) {
+                followers2 = new HashSet<>();
+                followerStorage.put(username2, followers2);
+            }
+
+            followers1.add(username2);
+            followers2.add(username1);
+        });
+    }
+
 }
