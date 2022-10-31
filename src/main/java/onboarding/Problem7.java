b95e71f949e12733397863e730bc746e58c536d7
c4fiber <qudcjf153@gmail.com>
2022-11-01 01:55:58 +0900

    feat(prob7): 조건에 따른 점수 부여 기능 추가
    사용자와 같이 아는 친구는 10점, 사용자의 타임라인을 방문한 사람은 1점을 부여한 scoreBoard를 얻는 기능을 추가함
    
    이 둘을 합쳐서 총 점수를 계산하고 정렬하여 친구추천 리스트를 얻을 목적

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,14 +1,25 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         List<String> myFriends = getUsersFriends(user, friends);
+
+        // 내 친구의 친구를 찾아서 tenPoint에 추가. 나 자신(user)는 제거
+        List<String> tenPoint = new ArrayList<>();
+        for(String name : myFriends) {
+            tenPoint.addAll(getUsersFriends(name, friends));
+        }
+        while(tenPoint.contains(user)) {
+            tenPoint.remove(user);
+        }
+
+        // tenPoint에 있는 이름에 점수 부여 (친구의 친구: 10점, 방문자: 1점)
+        Map<String,Integer> RelationScore = getAddedScoreBoard(tenPoint, myFriends,10);
+        Map<String,Integer> visitorScore = getAddedScoreBoard(visitors, myFriends, 1);
 
 
         return answer;
@@ -28,5 +39,21 @@ public class Problem7 {
         }
 
         return my;
+    }
+
+    private static Map<String,Integer> getAddedScoreBoard (List<String> members, List<String> exceptMembers, int point) {
+        Map<String,Integer> score = new HashMap<>();
+
+        for(String name : members) {
+            if(exceptMembers.contains(name)) continue;
+
+            if (score.containsKey(name)) {
+                score.put(name, score.get(name) + 10);
+            }else {
+                score.put(name,10);
+            }
+        }
+
+        return score;
     }
 }
