2184baa17d4cc155fbf376b4f5864fcf882ad971
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-31 09:56:11 +0900

    feat(7번) FriendScore객체 리스트 및 업데이트 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -19,9 +19,29 @@ public class Problem7 {
         //만약 대문자가 존재한다면 전부 소문자로 변환한 것과 달라 거짓이 나오게 된다.
         if (!user.toLowerCase().equals(user))
             return null;
-
+        //기존 친구들만 따로 리스트에 저장
         ArrayList<String> oldFriends= (ArrayList<String>) getOldFriends(user, friends);
+        //기존 친구들의 친구들의 점수 계산
+        ArrayList<FriendScore> friendScores = new ArrayList<FriendScore>();
 
+        for (int i=0; i<oldFriends.size(); i++){
+            for (int j=0; j<friends.size();j++){
+                if (friends.get(j).contains(user))
+                    break;
+                if (friends.get(j).contains(oldFriends.get(i))){
+                    if (oldFriends.get(i).equals(friends.get(j).get(0))){
+                        friendScores = (ArrayList<FriendScore>) updateScore(friendScores, friends.get(j).get(1),10);
+                    } else{
+                        friendScores = (ArrayList<FriendScore>) updateScore(friendScores, friends.get(j).get(0),10);
+                    }
+                }
+            }
+        }
+
+        for (int i=0; i<visitors.size();i++){
+            if (!visitors.get(i).equals(user) && !oldFriends.contains(visitors.get(i)))
+                friendScores = (ArrayList<FriendScore>) updateScore(friendScores,visitors.get(i),1);
+        }
 
         return answer;
     }
@@ -38,6 +58,49 @@ public class Problem7 {
             }
         }
         return oldFriends;
+    }
+
+    public static List<FriendScore> updateScore(List<FriendScore> friendScores, String nickname, int scoreSize){
+        if (friendScores.size()==0){
+            FriendScore friendScore = new FriendScore(nickname,scoreSize);
+            friendScores.add(friendScore);
+            return friendScores;
+        }
+
+        for (int i =0; i<friendScores.size();i++){
+            if (friendScores.get(i).getNickname()==nickname){
+                int score = friendScores.get(i).getScore();
+                score += scoreSize;
+                friendScores.get(i).setScore(score);
+                return friendScores;
+            }
+        }
+
+        FriendScore friendScore = new FriendScore(nickname,scoreSize);
+        friendScores.add(friendScore);
+        return friendScores;
+    }
+}
+
+class FriendScore {
+    public int score;
+    public String nickname;
+
+    public FriendScore(String nickname, int score) {
+        this.score = score;
+        this.nickname = nickname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getNickname() {
+        return nickname;
     }
 }
 
