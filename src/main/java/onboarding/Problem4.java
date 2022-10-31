3d6544478894c78d01296c3b597d6e8e71b3a70b
eora21 <eora21@naver.com>
2022-10-31 21:59:41 +0900

    refactor: 💡 변경 청개구리 객체 생성 및 메서드명 수정
    청개구리 객체를 생성하고, 엄마의 말을 변환하는 규칙을 수정할 수 있게 하였다. 또한, 엄마의 말을 전달받아 규칙에 맞게끔 변환할
    수 있도록 say 메서드에 인자를 받을 수 있게 하였다. 기존의 엄마의 말을 변환시킨다는 뜻의 메서드는 손쉽게 청개구리가 말한다는
    의미로 say로 변경했다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,29 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem4 {
-    private final static Map<Character, Character> wordSwitchRule;
-
-    static {
-        wordSwitchRule = new HashMap<>();
-
-        for (int i = 0; i < 26; i++) {
-            wordSwitchRule.put((char)('a' + i), (char)('z' - i));
-            wordSwitchRule.put((char)('A' + i), (char)('Z' - i));
-        }
-    }
-
-    private static void mamaWordTransformation(char[] treeFrogSay, String word) {
-
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            ch = treeFrogRule(ch);
-            treeFrogSay[i] = ch;
-        }
-    }
-
-    private static char treeFrogRule(char ch) {
-        return wordSwitchRule.getOrDefault(ch, ch);
-    }
 
     /**
      * 엄마 말씀 word가 매개변수로 주어질 때, 아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록 solution 메서드를 완성하라.
@@ -34,8 +11,43 @@ public class Problem4 {
      * @return 규칙에 맞게 변환한 String
      */
     public static String solution(String word) {
-        char[] treeFrogSay = new char[word.length()];
-        mamaWordTransformation(treeFrogSay, word);
-        return new String(treeFrogSay);
+        TreeFrog treeFrog = new TreeFrog();
+        return treeFrog.say(word);
+    }
+}
+
+class TreeFrog {
+    private Map<Character, Character> wordSwitchRule = new HashMap<>();
+
+    public TreeFrog() {
+
+        for (int i = 0; i < 26; i++) {
+            wordSwitchRule.put((char)('a' + i), (char)('z' - i));
+            wordSwitchRule.put((char)('A' + i), (char)('Z' - i));
+        }
+    }
+
+    public TreeFrog(Map<Character, Character> wordSwitchRule) {
+        setWordSwitchRule(wordSwitchRule);
+    }
+
+    public void setWordSwitchRule(Map<Character, Character> wordSwitchRule) {
+        this.wordSwitchRule = wordSwitchRule;
+    }
+
+    public String say(String word) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            ch = treeFrogRule(ch);
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+
+    private char treeFrogRule(char ch) {
+        return wordSwitchRule.getOrDefault(ch, ch);
     }
 }
