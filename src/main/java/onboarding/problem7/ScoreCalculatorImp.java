2d3729ec118b4bf867738db779a81531f6713dc9
xxeol2 <jhj01177@naver.com>
2022-10-31 17:18:35 +0900

    feat(prob7): 두 점수 계산 합산 기능 구현

--- src/main/java/onboarding/problem7/ScoreCalculatorImp.java
+++ src/main/java/onboarding/problem7/ScoreCalculatorImp.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
-public class ScoreCalculatorImp {
+public class ScoreCalculatorImp implements ScoreCalculator {
 	private Map<String, List<String>> getFriendMap(List<List<String>> friends) {
 		Map<String, List<String>> friendMap = new HashMap<>();
 		friends.forEach(friend -> {
@@ -29,11 +29,24 @@ public class ScoreCalculatorImp {
 		return friendScoreMap;
 	}
 
-	private Map<String, Integer> getVisitScoreMap(String user, List<String> visitors) {
+	private Map<String, Integer> getVisitorScoreMap(String user, List<String> visitors) {
 		Map<String, Integer> visitorScoreMap = new HashMap<>();
 		visitors.forEach(visitor -> {
 			visitorScoreMap.merge(visitor, 1, Integer::sum);
 		});
 		return visitorScoreMap;
+	}
+
+	private Map<String, Integer> mergeScoreMaps(Map<String, Integer> scoreMap1, Map<String, Integer> scoreMap2) {
+		scoreMap2.forEach((key, value) -> scoreMap1.merge(key, value, Integer::sum));
+		return scoreMap1;
+	}
+
+	@Override
+	public Map<String, Integer> getScoreMap(String user, List<List<String>> friends, List<String> visitors) {
+		Map<String, List<String>> friendMap = getFriendMap(friends);
+		Map<String, Integer> friendScoreMap = getFriendScoreMap(user, friendMap);
+		Map<String, Integer> visitorScoreMap = getVisitorScoreMap(user, visitors);
+		return mergeScoreMaps(friendScoreMap, visitorScoreMap);
 	}
 }
