452c18d4197079e5decc66867a296581aa0f1406
noparamin <noparamin@naver.com>
2022-10-31 17:30:45 +0900

    Problem5 리팩토링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,27 +1,31 @@
 package onboarding;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
-        List<Integer> unit = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    public static List<Integer> classifyMoney(int money, List<Integer> unit) {
+        List<Integer> result = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
 
         while(money > 0) {
             for(int i = 0; i < unit.size(); i++) {
                 if(money >= unit.get(i)) {
                     if(unit.get(i) == 1) {
-                        answer.set(i, money);
+                        result.set(i, money);
                         money = 0;
                     } else {
-                        answer.set(i, money / unit.get(i));
+                        result.set(i, money / unit.get(i));
                         money %= unit.get(i);
                     }
                 }
             }
         }
+
+        return result;
+    }
+    public static List<Integer> solution(int money) {
+        List<Integer> unit = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> answer = classifyMoney(money, unit);
 
         return answer;
     }
