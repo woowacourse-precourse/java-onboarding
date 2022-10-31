bdff2f1793ca88811a582618eb890c742d076edd
EstelleYU <iamestelleyu@gmail.com>
2022-11-01 01:41:02 +0900

    (#3) Problem2 solved by EstelleYU

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        char[] chars = cryptogram.toCharArray();
+        char prev = 0;
+        int k = 0;
+        for (char c: chars)
+        {
+            if (prev != c)
+            {
+                chars[k++] = c;
+                prev = c;
+            }
+        }
+        return new String(chars).substring(0, k);
     }
 }
