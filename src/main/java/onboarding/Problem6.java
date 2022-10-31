96d2f922fcaa5a8e65f672b95054aebb287ad275
metorg <metrog950@gmail.com>
2022-10-31 17:10:47 +0900

    feat: [Problem6] 중복체크 메서드 생성 닉네임에 두글자씩 잘라가며 맵에 저장한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,21 +1,43 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+
+    public static HashMap<String,Integer> map;
+    public static List<String> answer;
+    public static int[] visit;
+
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        map = new HashMap<String,Integer>();
+        answer = new ArrayList<>();
+        visit = new int[10001];
+
         return answer;
     }
 
-    public static List<String> listCheck(List<List<String>> forms){
-
-        List<String> result = new ArrayList<>();
+    public static void listCheck(List<List<String>> forms){
 
         for(int i=0;i<forms.size();i++){
-
+            overlapCheck(forms.get(i).get(1),i);
         }
-        return result;
+
     }
+
+    public static void overlapCheck(String nickname,int index){
+        for(int i=0;i<nickname.length()-2;i++){
+
+            if(map.containsKey(nickname.substring(i,i+2))){
+                map.put(nickname,index);
+            }
+            else{
+
+            }
+        }
+    }
+
 }
