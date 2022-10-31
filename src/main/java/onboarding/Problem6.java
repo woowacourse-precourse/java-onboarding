3f0b0374052aea380c99f4b3e775e73c66bad3cb
wugawuga <dnr3655@naver.com>
2022-10-31 22:05:18 +0900

    refactor: 한 명만 입력 시 닉네임 중복체크

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,7 +9,7 @@ import java.util.TreeSet;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         if (forms.size() == 1) {
-            return Collections.emptyList();
+            return onlyOnePeopleCheckRepeated(forms);
         }
 
         Set<String> set = new TreeSet<>();
@@ -17,5 +17,28 @@ public class Problem6 {
             String nickName = forms.get(i).get(1);
         }
         return new ArrayList<>(set);
+    }
+
+    private static List<String> onlyOnePeopleCheckRepeated(List<List<String>> forms) {
+        String email = forms.get(0).get(0);
+        String nickName = forms.get(0).get(1);
+        if (checkRepeated(nickName)) {
+            return List.of(email);
+        }
+        return Collections.emptyList();
+    }
+
+    private static boolean checkRepeated(String nickName) {
+        if (nickName.length() <= 3) {
+            return false;
+        }
+        for (int i = 0; i < nickName.length() - 2; i += 2) {
+            if (nickName.substring(i, i + 2).equals(
+                nickName.substring(i + 2, i + 4)
+            )) {
+                return true;
+            }
+        }
+        return false;
     }
 }
