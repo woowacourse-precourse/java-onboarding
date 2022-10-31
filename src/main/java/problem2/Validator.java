3726b6f956ddf75eb9e5c3299b605be0d6fa7956
twoosky <lhjae3@naver.com>
2022-11-01 00:54:46 +0900

    feat(problem2): 알파벳 소문자로만 이루어져 있는지 예외 처리 메소드 구현

--- src/main/java/problem2/Validator.java
+++ src/main/java/problem2/Validator.java
@@ -9,12 +9,19 @@ public class Validator {
 
     public void validate() {
         validateRange();
+        validateLowerCase();
     }
 
     private void validateRange() {
         int length = cryptogram.length();
         if (length < 1 || length > 1000) {
             throw new IllegalArgumentException("1~1000 크기의 문자열만 입력 가능합니다.");
+        }
+    }
+
+    private void validateLowerCase() {
+        if(!cryptogram.matches("^[a-z]*$")){
+            throw new IllegalArgumentException("알파벳 소문자만 입력 가능합니다.");
         }
     }
 }
