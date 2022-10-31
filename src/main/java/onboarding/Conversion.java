5d6592e9e14d4a93d0a3922b2f52815973d62e2d
yoonsj970407 <yoonsj970407@gmail.com>
2022-10-31 23:33:35 +0900

    ✨Feat: Add exception()

--- src/main/java/onboarding/Conversion.java
+++ src/main/java/onboarding/Conversion.java
@@ -9,4 +9,14 @@ class Conversion {
         this.capital = 'A' + 'Z';
         this.small = 'a' + 'z';
     }
+
+    int exception() {
+        if((word.length() < 1) || (word.length() > 1000)) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+
 }
