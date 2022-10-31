ebb90870b0db68bfc50240f90210528721dcb301
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 23:23:33 +0900

    Feat(Problem6) : 연속된 닉네임의 이메일을 구하는 로직 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,40 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String,String> m = new HashMap<>();
+        Set<String> s = new HashSet<>();
+        String nickname;
+        String email;
+        String twoLetters;
+        for(List<String> l: forms) {
+            email = l.get(0);
+            nickname = l.get(1);
+            for(int j = 0;j < nickname.length()-1; j++) {
+                twoLetters = nickname.substring(j,j+2);
+                if(m.containsKey(twoLetters)) {
+                    s.add(m.get(twoLetters));
+                    s.add(email);
+                } else {
+                    m.put(twoLetters, email);
+                }
+            }
+        }
+
+        List<String> emailList = new ArrayList<>();
+        emailList.addAll(s);
+        String[] emailArray = new String[emailList.size()];
+        emailList.toArray(emailArray);
+        Arrays.sort(emailArray);
+        answer = List.of(emailArray);
+
+
         return answer;
     }
+
+
+
 }
