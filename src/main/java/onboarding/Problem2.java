29052b26c9edbc2cf5cdf4469ef53a39b6799b50
This2sho <zx00018@naver.com>
2022-11-01 08:03:34 +0900

    feat: 문제 2 요구사항에 맞게 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Decryption decryption = new Decryption(cryptogram);
+        return decryption.getResult();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("browoanoommnaon"));
+        System.out.println(solution("zyelleyz"));
     }
 }
