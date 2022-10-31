c61f0c792be29a4cb50d0e5ef7e7a8eaf1ce271d
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-10-31 21:38:12 +0900

    FEAT : number까지의 박수 횟수 총합 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,6 +14,9 @@ public class Problem3 {
     }
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++) {
+            answer += countClap(i);
+        }
         return answer;
     }
 }
