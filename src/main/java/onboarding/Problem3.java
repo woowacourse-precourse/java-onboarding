eb37b9a765c62365385b414f655ccefc260584c4
nahyeon cho <whskgus12@nate.com>
2022-10-31 22:59:29 +0900

    Problem 3 - 박수 횟수 계산 기능
    1부터 입력된 숫자 전까지, 박수 횟수를 합쳐 반환하는 기능과 각 숫자에 포함된 3,6,9 개수를 세어 반환하는 기능을 추가하였습니다. Test까지 정상적으로 마쳤으므로, Problem3 기능 구현 최종 커밋 합니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,30 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        if(isValid(number)){
+            return countClap(number);
+        }else{
+            return -1; // 잘못된 범위의 수를 입력했을때
+        }
+    }
+
+    static int countClap(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += countThreeSixNine(i);
+        }
+        return sum;
+    }
+
+    static int countThreeSixNine(int number){
+        int count = 0;
+        while (number != 0) {
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
     }
 
     static boolean isValid(int number) {
