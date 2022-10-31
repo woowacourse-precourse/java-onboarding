7de90fa63f78f77dec1022c573d2b167856874ff
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-31 19:11:04 +0900

    feat: Problem3 - isProperNumber 메서드 구현
    예외처리 추가 - 숫자가 1~10000인지 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,12 +3,19 @@ package onboarding;
 
 /**
  *  getClapCount 메서드 1부터 n까지 3,6,9가 들어가면 카운트를 증가시키고 그 값을 환반환
+ *  isProperNumber 메서드 number범위가 1~10000인지 확인
  */
 
 public class Problem3 {
     public static int solution(int number) {
+        isProperNumber(number);
         return getClapCount(number);
     }
+
+    private static void isProperNumber(int number){
+        if (number<1||number>10000)throw new IllegalArgumentException();
+    }
+
     private static int getClapCount(int number){
         int clapCount = 0;
         for(int i = 1; i <= number; i++) {
