6d3067b359363497b73f5369f34546b1498e8362
¤knj <krkoknj@gmail.com>
2022-10-31 20:16:12 +0900

    List 안에 닉네임을 배열에 각각 채운다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        test(forms);
         return answer;
+    }
+
+    private static void test(List<List<String>> forms) {
+        String[] nicknames = new String[forms.size()];
+        int idx = 0;
+        for (List<String> i : forms) {
+            nicknames[idx] = i.get(1);
+            idx++;
+        }
     }
 }
