4af6099bea3e01c61c867bb4fa9915c31cc7797b
squirMM <jsm6616@naver.com>
2022-10-31 17:42:05 +0900

    :rocket: feat(Problem2) refactoring logic

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,25 +7,20 @@ public class Problem2 {
         while(cryptogram.length()>0){
             char prev = cryptogram.charAt(0);
             int lenSamePrev=1;
-            String newCrypt=cryptogram;
+            StringBuilder newCrypt= new StringBuilder();
 
             for(int i=1 ; i<cryptogram.length();i++){
                 char c = cryptogram.charAt(i);
                 if(Objects.equals(c,prev)) lenSamePrev+=1;
-                else if (lenSamePrev>1) {
-                        newCrypt=newCrypt.replaceAll(repeatStr(prev,lenSamePrev), "");
-                        break;
-                }
+                else if (lenSamePrev>1) lenSamePrev=1;
+                else newCrypt.append(prev);
                 prev=c;
             }
-            if (Objects.equals(repeatStr(prev,lenSamePrev),newCrypt)) newCrypt="";
-            if (Objects.equals(newCrypt,cryptogram)) break;
-            cryptogram=newCrypt;
+
+            if (lenSamePrev==1)newCrypt.append(prev);
+            if (Objects.equals(newCrypt.toString(),cryptogram)) break;
+            cryptogram= newCrypt.toString();
         }
         return cryptogram;
-    }
-    public static String repeatStr(char prev, int repeatNum){
-        String s = String.valueOf(prev);
-        return s.repeat(repeatNum);
     }
 }
