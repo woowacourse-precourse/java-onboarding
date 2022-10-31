0ed1aaa13a77ee7f597a22d4a6f7c9c40c21adbe
Torres-09 <dla612@naver.com>
2022-10-31 18:49:05 +0900

    feat : 문제 4 기능 목록 및 기능 구현
    아스키 코드 이용하여 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static int[] change = new int[26];
+
+    public static void init() {
+        int i = 0;
+        for (int j = 25; j >= 0; j--) {
+            change[i] = j;
+            i++;
+        }
+    }
+
     public static String solution(String word) {
-        String answer = "";
+        init();
+            char[] chars = word.toCharArray();
+            String answer = "";
+            for (char c : chars) {
+                if (c == ' ') answer += ' ';
+                else if (c >= 'a' && c <= 'z') answer += (char)(change[c - 'a'] + 'a');
+                else if (c >= 'A' && c <= 'Z') answer += (char)(change[c - 'A'] + 'A');
+        }
         return answer;
     }
 }
