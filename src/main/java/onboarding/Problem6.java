13e9b7ba4acf5a72b3469effd7ead269bac3287c
kjb95 <topmedia44@gmail.com>
2022-10-31 12:53:32 +0900

    refactor(Problem6): 자잘한 수정
    1. isValidFormEmail(), isValidFormNickname(), isDuplicatedNickname(), isDuplicatedTwoLetters
    - stream의 filter() + counter() 대신 allMatch() 또는 anyMatch() 사용
    
    2. computeAnswer(), findDuplicatedNickname()
    - 메소드 참조 사용하여 코드 간결화
    
    3. isDuplicatedNickname()
    - 문자열을 == 대신 equals를 이용하여 비교

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -13,8 +14,8 @@ public class Problem6 {
     private static final int TWO = 2;
     private static final int STUDENT_NUM_MIN = 1;
     private static final int STUDENT_NUM_MAX = 10000;
-    private static final String EMAIL_REGEX = "^[[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*]{1,9}@email.com$";
-    private static final String NICKNAME_REGEX = "^[ㄱ-ㅎ|가-힣|]{1,19}$";
+    private static final String EMAIL_REGEX = "^[0-9a-zA-Z(-_.?0-9a-zA-Z)*]{1,9}@email.com$";
+    private static final String NICKNAME_REGEX = "^[ㄱ-ㅎ|가-힣]{1,19}$";
 
     public static List<String> solution(List<List<String>> forms) {
         validateForms(forms);
@@ -38,19 +39,17 @@ public class Problem6 {
 
     private static boolean isValidFormEmail(List<List<String>> forms) {
         return forms.stream()
-            .filter(form -> !Pattern.matches(EMAIL_REGEX, form.get(0)))
-            .count() == 0;
+            .allMatch(form -> Pattern.matches(EMAIL_REGEX, form.get(0)));
     }
 
     private static boolean isValidFormNickname(List<List<String>> forms) {
         return forms.stream()
-            .filter(form -> !Pattern.matches(NICKNAME_REGEX, form.get(1)))
-            .count() == 0;
+            .allMatch(form -> Pattern.matches(NICKNAME_REGEX, form.get(1)));
     }
 
     private static List<String> computeAnswer(List<String> duplicatedNicknameList, Map<String, String> nicknameAndEmail) {
         return duplicatedNicknameList.stream()
-            .map(nickname -> nicknameAndEmail.get(nickname))
+            .map(nicknameAndEmail::get)
             .distinct()
             .sorted()
             .collect(Collectors.toList());
@@ -60,24 +59,21 @@ public class Problem6 {
         return nicknameAndTwoLetters.entrySet()
             .stream()
             .filter(nicknameAndTwoLettersMap -> isDuplicatedNickname(nicknameAndTwoLetters, nicknameAndTwoLettersMap.getKey(), nicknameAndTwoLettersMap.getValue()))
-            .map(map -> map.getKey())
+            .map(Entry::getKey)
             .collect(Collectors.toList());
     }
 
     private static boolean isDuplicatedNickname(Map<String, List<String>> nicknameAndTwoLetters, String baseNickname, List<String> baseTwoLetters) {
         return nicknameAndTwoLetters.keySet()
             .stream()
-            .filter(nickname -> nickname != baseNickname)
-            .filter(nickname -> isDuplicatedTwoLetters(baseTwoLetters, nicknameAndTwoLetters.get(nickname)))
-            .count() != 0;
+            .filter(nickname -> !nickname.equals(baseNickname))
+            .anyMatch(nickname -> isDuplicatedTwoLetters(baseTwoLetters, nicknameAndTwoLetters.get(nickname)));
     }
 
     private static boolean isDuplicatedTwoLetters(List<String> twoLetters1, List<String> twoLetters2) {
         return twoLetters1.stream()
-            .filter(twoLetter1 -> twoLetters2.contains(twoLetter1))
-            .count() != 0;
+            .anyMatch(twoLetters2::contains);
     }
-
 
     private static Map<String, String> computeEmailMap(List<List<String>> forms) {
         return forms.stream()
