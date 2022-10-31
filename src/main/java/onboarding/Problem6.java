e5884f6595085fa032ea180ce46455f6beaa410c
DDing77 <devseo2021@gmail.com>
2022-10-31 23:58:56 +0900

    [Problem6] feat : 이메일 집합 set

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,11 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    // 중복인 닉네임을 가진 유저의 이메일 집합 set
+    public static Set<String> set = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
