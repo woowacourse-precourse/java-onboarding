8b3dba177154049835fa297df6123cef9bd6c9d1
inhyeok <ss112d86@gmail.com>
2022-11-01 00:34:13 +0900

    feat:Problem4 알파벳 변환 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+  public static String solution(String word) {
+    String answer = "";
+    return answer;
+  }
+
+  public static char convertAlpha(char alpha) {
+
+    if (alpha >= 65 && alpha <= 90) {
+      int offset = 25 - (alpha - 'A') * 2;
+      return (char) (alpha + offset);
     }
+    if (alpha >= 97 && alpha <= 122) {
+      int offset = 25 - (alpha - 'a') * 2;
+      return (char) (alpha + offset);
+    }
+    return alpha;
+  }
 }
