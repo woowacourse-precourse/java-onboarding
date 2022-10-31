5a5a4c2a58193f40683ac73e24427faaf1663ba7
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-31 15:36:34 +0900

    refactor: rename and delete comments of problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,15 +5,14 @@ import onboarding.problem6.DuplicationNotificator;
 import java.util.List;
 
 /**
- * Problem 6 function list
- * - Create duplication map for each part of nickname
- * - Select emails which needs to be notified
- * - Sort 'need notified' email list
- * - Remove duplicated email from 'need notified' email list
+ * Problem 6 기능 목록
+ * 1. Nickname 의 부분들과 email 을 mapping 하는 map 을 생성한다.
+ * 2. Nickname 의 부분과 mapping 된 email 이 1개 이상이라면 email 들을 notify 목록에 저장한다.
+ * 3. 알림이 필요한 email list 를 정렬하고 중복 email 을 제거한다.
  */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         DuplicationNotificator notificator = new DuplicationNotificator(forms);
-        return notificator.duplicationNotify();
+        return notificator.notifyEmails();
     }
 }
--- src/main/java/onboarding/problem6/DuplicationNotificator.java
+++ src/main/java/onboarding/problem6/DuplicationNotificator.java
@@ -3,45 +3,29 @@ package onboarding.problem6;
 import java.util.*;
 import java.util.stream.Collectors;
 
-/**
- * Class searching for duplicated nickname and notify email addresses
- */
 public class DuplicationNotificator {
 
-    private Map<String, List<String>> parts = new HashMap<>();
+    private final Map<String, List<String>> parts = new HashMap<>();
 
-    /**
-     * Constructor with forms
-     * @param forms email and nickname list
-     */
     public DuplicationNotificator(List<List<String>> forms) {
         checkDuplication(forms);
     }
 
-    /**
-     * Check duplication of nickname parts and create parts map
-     * @param forms email and nickname list
-     */
     private void checkDuplication(List<List<String>> forms) {
         for (List<String> form : forms) {
             String email = form.get(0);
             String nick = form.get(1);
             for (int i = 0; i < nick.length() - 1; i++) {
                 String part = nick.substring(i, i + 2);
-                if (parts.containsKey(part)) parts.get(part).add(email);
-                else parts.put(part, new ArrayList<>(Collections.singletonList(email)));
+                if (parts.containsKey(part))
+                    parts.get(part).add(email);
+                else
+                    parts.put(part, new ArrayList<>(Collections.singletonList(email)));
             }
         }
     }
 
-    /**
-     * Get email list needs to be notified
-     * - Check if part of nickname is duplicated
-     * - Sort email list
-     * - Remove duplicated email address
-     * @return email list needs notification
-     */
-    public List<String> duplicationNotify() {
+    public List<String> notifyEmails() {
         List<String> emails = new ArrayList<>();
         for (String key : parts.keySet()) {
             List<String> emailsOfParts = parts.get(key);
