e5d45d2f3c948024e4b14a18de9c24c8593b96de
jsw6701 <anes53027@gmail.com>
2022-11-01 05:39:59 +0900

    (MODIFY)Problem2 Add method remark

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,14 +8,16 @@ public class Problem2 {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < cryptogram.length(); i++) {
+            //스택에서 꺼낸 문자와 현재 문자를 비교
             if(!stack.isEmpty()){
                 if(cryptogram.charAt(i) == stack.peek()){
-                    stack.pop();
+                    stack.pop(); // 같음으로 스택에서 문자 제거 & 넣으려던 문자 제거
                 }
                 else{
-                    stack.add(cryptogram.charAt(i));
+                    stack.add(cryptogram.charAt(i)); // 다름으로 스택에 문자 집어넣음
                 }
             }
+            //스택이 비어있을 경우에는 문자 바로 집어넣음
             else{
                 stack.add(cryptogram.charAt(i));
             }
