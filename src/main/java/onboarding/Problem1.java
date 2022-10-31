19d6d3eb625d3ba0c29e32127395a9675db12e91
linda2927 <linda2927@naver.com>
2022-11-01 00:12:59 +0900

    Handle more exceptions

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,7 +8,8 @@ class Problem1 {
         System.out.println(solution(List.of(97, 98), List.of(197, 198)));
         System.out.println(solution(List.of(131, 132), List.of(211, 212)));
         System.out.println(solution(List.of(99, 102), List.of(211, 212)));
-        System.out.println(solution(List.of(1, 2), List.of(99, 100)));
+        System.out.println(solution(List.of(11, 12), List.of(99, 100)));
+        System.out.println(solution(List.of(21, 22), List.of(100, 101)));
     }
 
     public static int returnMax(int num) {
@@ -25,10 +26,17 @@ class Problem1 {
     }
 
     public static boolean isValidList(List<Integer> numList) {
+        int left = numList.get(0);
+        int right = numList.get(1);
+
+        if (left == 1 || right == 400)
+            return false;
+
         if (numList.get(1) - numList.get(0) != 1) {
             return false;
         }
-        return numList.get(0) % 2 == 1 || numList.get(1) % 2 == 0;
+
+        return left % 2 == 1 || right % 2 == 0;
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
