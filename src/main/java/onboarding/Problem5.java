586e1ea4fbfefa310d501e949820ead73b9e2aae
Hongsugyeong <olivia258@naver.com>
2022-10-31 12:06:59 +0900

    [Problem 5] 화폐 갯수 세는 함수

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,6 +10,22 @@ public class Problem5 {
      */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        return answer;
+    }
+
+    /*
+     *화폐 갯수 세는 함수
+     */
+    static List<Integer> cal(int money){
+        List<Integer> answer = new ArrayList<>();
+        int[] list = {50000,10000,5000,1000,500,100,50,10,1};
+
+        answer.add((money/list[0]));
+
+        for(int i=0;i<list.length-1;i++){
+            int count = (money%list[i])/list[i+1];
+            answer.add(count);
+        }
         return answer;
     }
 }
