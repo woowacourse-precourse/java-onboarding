38bb79c9ebc437b25783bd28702cbba3beae53fa
eil-you <qldirl123@gmail.com>
2022-10-31 23:56:36 +0900

    for문 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+
         List<Integer> answer = new java.util.ArrayList<>(Collections.emptyList());
+
         int[] moneyValue = {50000,10000,5000,1000, 500,100, 50, 10,1};
 
-       for(int i = 0; i<moneyValue.length; i++){
-           answer.add(money / moneyValue[i]);
-           money %= moneyValue[i];
+       for(int i : moneyValue){
+
+           answer.add(money / i);
+
+           money %= i;
+
        }
 
         return answer;
