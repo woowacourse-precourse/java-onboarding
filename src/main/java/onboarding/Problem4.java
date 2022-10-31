c1e21d6ada668f3abb98176dc1e5dad170220b3d
shpark30 <seohyeonpark30.gmail.com>
2022-10-31 23:56:37 +0900

    refact: 전체 로직 수정
    기존 모든 대문자를 소문자로 변환해서 소문자를 반대로 변환하는 로직에서
    대문자와 소문자를 따로 반대로 변환하는 로직으로 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,45 +1,27 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem4 {
     public static String solution(String word) {
         /* ASCII 코드를 활용해 소문자, 대문자 알파벳을 반대로 변환*/
-        List<Integer> upperCharIndex = getUpperCharIndex(word.toCharArray());   // 대문자 알파벳의 위치 리스트
-        char[] wordCharacter = word.toLowerCase().toCharArray();    // 모든 알파벳을 소문자로 변환
-        wordCharacter = reverseWord(wordCharacter);     // 소문자 알파벳을 반대로 변환
-        wordCharacter = restoreUpperCharacter(wordCharacter, upperCharIndex);   // 대문자 알파벳 복원
-        String answer = new String(wordCharacter);  // 문자열String으로 변환
+        List<Character> characters = word.chars().mapToObj(e->(char)e).collect(Collectors.toList());
+        characters = reverseUpperCase(characters);  // 대문자 알파벳 반대로 변환
+        characters = reverseLowerCase(characters);  // 소문자 알파벳 반대로 변환
+        String answer = characters.stream()
+                                    .map(String::valueOf)
+                                    .collect(Collectors.joining());;  // 문자열 String으로 변환
         return answer;
     }
 
-    static List<Integer>getUpperCharIndex(char[] wordCharacter) {
-        List<Integer> upperCharIndex = new ArrayList<>();
-        for (int i = 0; i < wordCharacter.length; i++) {
-            int asciiChar = wordCharacter[i];
-            if (asciiChar >= 65 & asciiChar <= 90) {
-                upperCharIndex.add(i);
-            }
-        }
-        return upperCharIndex;
+    static List<Character> reverseUpperCase(List<Character> characters) {
+        characters.replaceAll(c -> (int) c >= 65 && (int) c <= 90 ? (char) (90+65-(int)c) : c);
+        return characters;
     }
 
-    static char[] reverseWord(char[] wordCharacter) {
-        for (int i = 0; i < wordCharacter.length; i++) {
-            int asciiChar = wordCharacter[i];
-            if (asciiChar >= 97 & asciiChar <=122) {
-                char reverseChar = (char)(122+97-asciiChar);
-                wordCharacter[i] = reverseChar;
-            }
-        }
-        return wordCharacter;
-    }
-
-    static char[] restoreUpperCharacter(char[] wordCharacter, List<Integer> upperCharIndex) {
-        for (int i : upperCharIndex) {
-            wordCharacter[i] = Character.toUpperCase(wordCharacter[i]);
-        }
-        return wordCharacter;
+    static List<Character> reverseLowerCase(List<Character> characters) {
+        characters.replaceAll(c -> (int) c >= 97 && (int) c <= 122 ? (char) (122+97-(int)c) : c);
+        return characters;
     }
 }
