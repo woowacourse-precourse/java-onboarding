0f3954cd45ce40b5835f12dd5b1225a78335aa41
sa46lll <9812smh@naver.com>
2022-11-01 02:28:40 +0900

    feat: 반복되는 문자열 확인, 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,29 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
         String answer = "answer";
+
+        String result = deduplicate(cryptogram);
+
         return answer;
+    }
+
+    private static String deduplicate(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            char c1 = cryptogram.charAt(i);
+            char c2 = cryptogram.charAt(i + 1);
+            if (c1 != c2) continue;
+
+            for (int j = i + 1; j < cryptogram.length(); j++) {
+                char c3 = cryptogram.charAt(j);
+                if (c1 != c3) {
+                    String subString = String.valueOf(c1).repeat(j - i);
+                    cryptogram = cryptogram.replace(subString, "");
+                    break;
+                }
+            }
+        }
+        return cryptogram;
     }
 }
