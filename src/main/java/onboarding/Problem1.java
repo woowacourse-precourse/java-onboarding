d3b355a77363674d306255cd88eb0ed457083956
WhoIsWoony <WhoIsWoony@gmail.com>
2022-10-31 17:18:33 +0900

    problem1: TODO-'기능1' complete

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,8 +10,30 @@ class Problem1 {
         else if(player.get(0) < 1 || player.get(1) < 1) return false;
         return true;
     }
+    private static int getScoreOf(List<Integer> player){
+        int score = 0;
+        for (int num : player) {
+            int sumOfNum = 0;
+            int mulOfNum = 1;
+            while (num != 0) {
+                sumOfNum += num % 10;
+                mulOfNum *= num % 10;
+                num /= 10;
+            }
+            if (score < mulOfNum)
+                score = mulOfNum;
+            if (score < sumOfNum)
+                score = sumOfNum;
+        }
+        return score;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if(!checkCorrectOf(pobi) || !checkCorrectOf(crong)) return -1;
+
+        int scoreOfPobi = getScoreOf(pobi);
+        int scoreOfCrong = getScoreOf(crong);
+
         return 0;
     }
 }
