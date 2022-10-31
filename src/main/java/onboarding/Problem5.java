34c2b80023971eb8ef6e464af2995a1d1c841a6b
DDing77 <devseo2021@gmail.com>
2022-10-31 23:10:20 +0900

    [Problem5] feat : 문제에서 주어진 정적 단위 배열 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    // 문제에서 주어진 금액 단위
+    public static int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
