c4aac6c7b217bf3e6c0de8e04ad5b5279120fc34
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-31 14:11:59 +0900

    [ADD] 문제 7 - 점수들을 상수로 선언

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,7 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    private static final int MUTUAL_SCORE = 10;
+    private static final int VISIT_SCORE = 1;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        System.out.println(MUTUAL_SCORE);
+        System.out.println(VISIT_SCORE);
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
