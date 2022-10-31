07c18dcf7047b7dadab241223636388830b149c8
yesjuhee <juhee0924@g.skku.edu>
2022-10-31 19:14:17 +0900

    [PROBLEM4] 문자가 대소문자인지 구분하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int index = 0; index < word.length() ; index++){
+            int ascii = (int)word.charAt(index);
+
+            if (ascii>=65 && ascii<=90) ascii = convertCapitalLetter(ascii);
+            else if (ascii>=97 && ascii<=122) ascii = convertSmallLetter(ascii);
+
+            answer += (char)ascii;
+        }
+
         return answer;
+    }
+
+    public static int convertCapitalLetter(int inputAscii){
+        return outputAscii;
+    }
+
+    public static int convertSmallLetter(int inputAscii){
+        return outputAscii;
     }
 }
