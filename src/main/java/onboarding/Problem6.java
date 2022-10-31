b5aa886e834b593d9231fbcaa8489f60affddc04
Jeeseob <jeeseob5761@gmail.com>
2022-10-31 15:54:54 +0900

    REFACTORING: problem6 - 알고리즘 함수 기능별 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,38 +8,74 @@ import java.util.stream.Collectors;
  */
 public class Problem6 {
     /**
-     * @Method: checkNickName
-     * 1. 이름을 각각 2글자씩 겹치게 쪼개서 해당 값을 key, 이메일을 value로 HashMap에 저장한다.
-     * 2. HashMap에서 Key가 겹치는 경우, 겹치는 닉네임이 존재하는 것이므로 판단한다.
-     * 2 - 1. 이때, 한명의 닉네임에 겹치는 단어 (ex: 제이제이)가 존재할 수 있기 때문에, value의 이메일과 현재 이메이을 비교하여 예외처리를 한다.
-     * 3. Set을 사용하여 중복되는 이메일을 제거한다.
-     * 4. 결과를 정렬한다.
-     * 5. 결과를 반환한다.
+     * @Method: solution
      */
-    private static List<String> checkNickName(List<List<String>> crews) {
-        Set<String> answerSet = new HashSet<>();
+    public static List<String> solution(List<List<String>> forms) {
+        Set<String> answerSet = checkNickName(forms);
+        List<String> answer = toSortedList(answerSet);
+        return answer;
+    }
+
+    /**
+     * @Method: checkNickName
+     */
+    private static Set<String> checkNickName(List<List<String>> crews) {
+        Map<String, List<String>> subNameMap = makeSubNameMap(crews);
+        Set<String> answerSet = toAnswerSet(subNameMap);
+        return answerSet;
+    }
+
+    /**
+     * @Method: makeSubNameMap
+     */
+    private static Map<String, List<String>> makeSubNameMap(List<List<String>> crews) {
         Map<String, List<String>> subNameMap = new HashMap<>();
 
         for (List<String> crew : crews) {
             String email = crew.get(0);
             String nickName = crew.get(1);
+            // 1. 이름을 각각 2글자씩 겹치게 쪼개서 해당 값을 key, 이메일을 value로 HashMap에 저장한다.
             for (int i = 0; i < nickName.length() - 1; i++) {
-                String subName = nickName.substring(i,i+2);
-                if(subNameMap.containsKey(subName)) {
-                    List<String> emailList = subNameMap.get(subName);
-                    if(!emailList.contains(email)) {
-                        emailList.add(email);
-                        subNameMap.replace(subName, emailList);
-                    }
-                }
-                else {
-                    List<String> emailList = new ArrayList<>();
-                    emailList.add(email);
-                    subNameMap.put(subName, emailList);
-                }
+                String subName = getSubName(nickName, i);
+                addSubNameToMap(subNameMap, email, subName);
             }
         }
+        return subNameMap;
+    }
 
+    /**
+     * @Method: addSubNameToMap
+     */
+    private static void addSubNameToMap(Map<String, List<String>> subNameMap, String email, String subName) {
+        // 2. HashMap에서 Key가 겹치는 경우, 겹치는 닉네임이 존재하는 것이므로 판단한다.
+        if(subNameMap.containsKey(subName)) {
+            List<String> emailList = subNameMap.get(subName);
+            //  2 - 1. 이때, 한명의 닉네임에 겹치는 단어 (ex: 제이제이)가 존재할 수 있기 때문에, value의 이메일과 현재 이메일을 비교하여 예외처리를 한다.
+            if(!emailList.contains(email)) {
+                emailList.add(email);
+                subNameMap.replace(subName, emailList);
+            }
+        }
+        else {
+            List<String> emailList = new ArrayList<>();
+            emailList.add(email);
+            subNameMap.put(subName, emailList);
+        }
+    }
+
+    /**
+     * @Method: getSubName
+     */
+    private static String getSubName(String nickName, int index) {
+        String subName = nickName.substring(index, index + 2);
+        return subName;
+    }
+
+    /**
+     * @Method: toAnswerSet
+     */
+    private static Set<String> toAnswerSet(Map<String, List<String>> subNameMap) {
+        Set<String> answerSet = new HashSet<>();
         for (String key : subNameMap.keySet()) {
             List<String> emailList = subNameMap.get(key);
             if (emailList.size() > 1) {
@@ -48,11 +84,15 @@ public class Problem6 {
                 }
             }
         }
-
-        return answerSet.stream().sorted().collect(Collectors.toList());
+        return answerSet;
     }
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = checkNickName(forms);
-        return answer;
+
+    /**
+     * @Method: toSortedList
+     */
+    private static List<String> toSortedList(Set<String> answerSet) {
+        // 3. Set을 사용하여 중복되는 이메일을 제거한다.
+        // 4. 결과를 정렬한다.
+        return answerSet.stream().sorted().collect(Collectors.toList());
     }
 }
