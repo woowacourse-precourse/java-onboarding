3a7e97ce96cdeab6c504446cb724715daa1fe49d
noparamin <noparamin@naver.com>
2022-10-31 17:04:14 +0900

    Problem3 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,16 +5,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-
-        for(int i = 1; i <= number; i++) {
-            answer += count(i);
-        }
-
-        return answer;
-    }
-
     public static int count(int num) {
         int cnt = 0;
         while(num > 0) {
@@ -26,4 +16,15 @@ public class Problem3 {
 
         return cnt;
     }
+
+    public static int solution(int number) {
+        int answer = 0;
+
+        for(int i = 1; i <= number; i++) {
+            answer += count(i);
+        }
+
+        return answer;
+    }
+
 }
