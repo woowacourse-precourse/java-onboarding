aa47b1808c0006bd29ee7ed5d1e7a5e784c394d1
wogus4048 <wogus4048@naver.com>
2022-10-31 18:10:22 +0900

    refactor(Problem2): 무한반복문-> dowhile로 수정, String -> Stringbuilder로 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -21,39 +21,35 @@ public class Problem2 {
     //지울 중복문자가 없을때까지 반복해서 중복문자를 지우는 함수
     public static String removeDuplicateWords(String cryptogram) {
 
-        String result = "";
-
-        while (true) {
-            for (int i = 0; i < cryptogram.length(); i++) {
+        StringBuilder result = new StringBuilder();
+        int currentLength;
+        do {
+            currentLength = cryptogram.length();
+            for (int i = 0; i < currentLength; i++) {
                 char currentCharacter = cryptogram.charAt(i);
                 //현재 문자가 앞부분 문자와 같다면 넘어간다.
-                if( i+1 <cryptogram.length()
-                    && cryptogram.charAt(i+1)== currentCharacter)
-                {
+                if (i + 1 < cryptogram.length()
+                    && cryptogram.charAt(i + 1) == currentCharacter) {
                     continue;
                 }
                 //현재 문자가 뒷부분 문자와 같다면 넘어간다.
-                if( i-1 >= 0
-                    && cryptogram.charAt(i-1)== currentCharacter)
-                {
+                if (i - 1 >= 0
+                    && cryptogram.charAt(i - 1) == currentCharacter) {
                     continue;
                 }
                 //그게아니라면 결과값 변수에 저장한다.
-                result += currentCharacter;
+                result.append(currentCharacter);
 
             }
-            //중복문자가 존재해 처음들어온 문자열과 결과문자열이 다르다면
-            if (!result.equals(cryptogram)) {
-                cryptogram = result; // 결과문자열로 암호문을 바꿔주고
-                result = ""; // 결과를 저장할 변수도 초기화해준다.
-            }
-            //지워질게없어서 들어온 문장과 결과문장이 같다면
-            else{
-                break; //반복문을 탈출한다.
+            //중복문자가 존재해서 지웠기에 처음 문자열의 길이와 결과 문자열 길이가 다르다면
+            if (currentLength != result.length()) {
+                cryptogram = result.toString(); // 결과문자열로 암호문을 바꿔주고
+                result.delete(0, result.length()); // 결과를 저장할 변수도 초기화해준다.
             }
         }
+        while (currentLength > result.length());
 
-        return result;
+        return result.toString();
 
     }
 
