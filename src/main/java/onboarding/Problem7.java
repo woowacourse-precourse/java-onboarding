6f87e0fe46b7ff9ee00b0fbd5300892538d86e4e
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 03:27:42 +0900

    feat: store a friend list for each person
    - 사람마다 친구목록 저장하는 기능추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        Map<String, List<String>> map = new HashMap<>();
+        Map<String, Integer> score = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+
+            if (!map.keySet().contains(friend1)) map.put(friend1, new ArrayList<>());
+            if (!map.keySet().contains(friend2)) map.put(friend2, new ArrayList<>());
+
+            map.get(friend1).add(friend2);
+            map.get(friend2).add(friend1);
+        }
+
         return answer;
     }
 }
