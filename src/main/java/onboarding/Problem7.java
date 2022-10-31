6fce0f60a62b2f8b5fa8d368b995afeed1951a5a
eil-you <qldirl123@gmail.com>
2022-11-01 00:26:51 +0900

    줄 간격 수정 // 같은 점수일때 이름순으로 정렬 구현 x

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,33 +4,47 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
        List<String> answer = new ArrayList<>(Collections.emptyList());
 
         Map<String, Integer> hashMap = new HashMap<>();
 
-
         List<String> buddyList = userList(user, friends);
 
         List<String> buddyBuddy;
+
         for(String  i : buddyList){
+
             buddyBuddy = userList(i, friends);
+
             for(String j : buddyBuddy){
+
                 if(j.equals(user)){
+
                     continue;
+
                 }
+
                 if (hashMap.containsKey(j)){
+
                     hashMap.put(j, hashMap.get(j) + 10);
+
                 }
+
                 hashMap.put(j,10);
             }
         }
 
         for(String i : visitors){
+
             if(buddyList.contains(i)){
+
                 continue;
             }
             if (hashMap.containsKey(i)){
+
                 hashMap.put(i, hashMap.get(i) + 1);
+
                 continue;
             }
             hashMap.put(i,1);
@@ -46,15 +60,20 @@ public class Problem7 {
     }
 
     public static List<String> userList(String user, List<List<String>> freind){
+
         List<String> answer = new ArrayList<>();
+
         for (List<String> strings : freind) {
+
             for (int j = 0; j < 2; j++) {
+
                 if (strings.get(j).equals(user)) {
+
                     answer.add(strings.get(1 - j));
+
                 }
             }
         }
-        // answer.remove(user);
 
         return answer;
     }
