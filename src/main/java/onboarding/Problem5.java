3328c0ea1a96e1ddf4412ed4ad3df9cf4f84b8ef
jindaram-stu <jinsb1999@naver.com>
2022-10-31 23:19:39 +0900

    feat : [문제5] 반복문을 통해 현금 각각의 갯수 계산하여 반환
    calculate 메소드로 분리하여 계산 진행

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,8 +10,19 @@ public class Problem5 {
     private static final List<Integer> CASH_LIST = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0));
+        List<Integer> answer = calculate(money);
 
         return answer;
     }
+
+    public static List<Integer> calculate (int money) {
+        List<Integer> cashCounts = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0));
+
+        for (int i=0; i<CASH_LIST.size(); i++) {
+            cashCounts.set(i,money/CASH_LIST.get(i));
+            money = money%CASH_LIST.get(i);
+        }
+        return cashCounts;
+    }
+
 }
