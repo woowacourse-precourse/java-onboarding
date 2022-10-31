b8d17eb9ccc2b480303d82696c538cd010a7ac87
Cho SangHyun <98shcho@naver.com>
2022-10-31 17:49:55 +0900

    refactor: refactor feature to delete duplicated range
    before: delete it whenever overlapping parts come out and initialize the for loop
    after: when overlapping parts come out, store it and delete them at once

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,28 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
+        List<String> duplicatedParts = new ArrayList<>();
 
         while (true) {
-            duplicate: {
-                /* 문자열을 왼쪽부터 읽는다 */
-                for (int i=0; i<cryptogram.length() - 1; i++) {
-                    char currentCh = cryptogram.charAt(i);
-                    char nextCh = cryptogram.charAt(i + 1);
-                    /* 연속되는 문자가 있는지 체크한다 */
-                    if (currentCh == nextCh) {
-                        int j = getLastConsecutiveIndex(cryptogram, currentCh, i + 1);
-                        /* 연속되는 구간을 빈 문자열로 교체한다 */
-                        cryptogram = deleteConsecutiveRange(cryptogram, i, j);
-                        /* label break로 처음으로 돌아간다 */
-                        break duplicate;
-                    }
+            duplicatedParts.clear();
+            /* 문자열을 왼쪽부터 읽는다 */
+            for (int i=0; i<cryptogram.length() - 1; i++) {
+                char currentCh = cryptogram.charAt(i);
+                char nextCh = cryptogram.charAt(i + 1);
+                /* 연속되는 문자가 있는지 체크한다 */
+                if (currentCh == nextCh) {
+                    int j = getLastConsecutiveIndex(cryptogram, currentCh, i + 1);
+                    duplicatedParts.add(cryptogram.substring(i, j));
                 }
-                /* 암호문을 answer에 담고 반복문을 벗어난다 */
-                answer = cryptogram;
-                break;
             }
+            /* 연속되는 구간들을 빈 문자열로 교체한다 */
+            if (duplicatedParts.size() != 0) {
+                cryptogram = deleteConsecutiveRange(cryptogram, duplicatedParts);
+                continue;
+            }
+            /* 암호문을 answer에 담고 반복문을 벗어난다 */
+            answer = cryptogram;
+            break;
         }
         return answer;
     }
@@ -35,8 +41,11 @@ public class Problem2 {
         return j;
     }
 
-    public static String deleteConsecutiveRange(String cryptogram, int start, int end) {
-        return cryptogram.replace(cryptogram.substring(start, end), "");
+    public static String deleteConsecutiveRange(String cryptogram, List<String> duplicatedParts) {
+        for (String part: duplicatedParts) {
+            cryptogram = cryptogram.replaceFirst(part, "");
+        }
+        return cryptogram;
     }
 }
 
@@ -51,7 +60,7 @@ public class Problem2 {
 - [V] 문자열을 왼쪽부터 읽는다
 - [V] 연속되는 문자가 있는지 체크한다.
   - [V] 연속되는 문자가 있다면 어디까지 연속되는지 체크한다
-  - [V] 연속되는 구간을 빈 문자열로 대체한다
-  - [V] label break를 통해 다시 처음단계로 돌아간다
+  - [V] 연속되는 구간을 duplicatedParts에 모은다
+  - [V] duplicatedParts의 문자들을 빈 문자열로 바꾼다
 - [V] 연속되는 문자가 없다면 그 문자열을 answer에 담고 반복문을 벗어난다
  */
