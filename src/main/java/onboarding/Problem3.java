b6490f4bc5a3fcf4e247e54ba8125ffb5f665322
KimSoo <ilppang@naver.com>
2022-10-31 16:37:10 +0900

    Feature(김현수/Problem3): number전달 기눙구현, 문제3번 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,18 +5,23 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
+        for (int i = 0; i <= number; i++) {
+            answer += count369byNumber(i);
+        }
+
         return answer;
     }
 
     public static int count369byNumber(int number) {
         int count = 0;
+
         while (number > 0) {
             if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
                 count += 1;
             }
             number /= 10;
         }
-        return count;
 
+        return count;
     }
 }
