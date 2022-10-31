e276bdc1575444c264484ed5eac193467de916ac
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-10-31 20:09:06 +0900

    feat: Problem2 풀이
    List를 이용해서 문제를 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String decoded = "";
+        List<String> stringList = new ArrayList<>(Arrays.asList(cryptogram.split("")));
+        for (int i = 0; i < stringList.size() - 1; i++) {
+            if (stringList.get(i).equals(stringList.get(i + 1))) {
+                stringList.remove(i);
+                stringList.remove(i);
+                i = -1;
+            }
+        }
+        for (String string : stringList) {
+            decoded += string;
+        }
+        return decoded;
     }
+
+
 }
