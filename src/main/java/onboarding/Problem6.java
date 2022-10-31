fca8ed1bfe7f46ad494e5899b0e54247be402a22
MIN-04 <kimji_0424@naver.com>
2022-10-31 15:18:52 +0900

    add Problem3 기능 구현 주석 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    /**
+     * 기능 구현
+     * 1. 제한사항의 이메일, 닉네임 검사
+     * 2. 닉네임 중복을 검사하는 메서드 생성
+     * 3. 닉네임 중복된 사용자의 이메일을 찾는 코드
+     * 4. 중복과 순서 정렬
+     */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
