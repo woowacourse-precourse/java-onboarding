c8d2e87b6d200a072b591f85c953e91e8b70145c
minju412 <ln8847@ajou.ac.kr>
2022-10-31 22:04:42 +0900

    style: [p4] 변수 명 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,19 +3,19 @@ package onboarding;
 public class Problem4 {
 	public static String solution(String word) {
 		char[] charArr = new char[word.length()];
-		int tmp = 0; // 아스키코드 표 기준 앞에서부터 몇 번째 캐릭터인지
-		char ch = ' ';
+		int askiiSequence = 0; // 아스키코드 표 기준 앞에서부터 몇 번째 캐릭터인지
+		char oneLetter = ' ';
 
 		for (int i = 0; i < word.length(); i++) {
-			ch = word.charAt(i);
-			if (ch > 'a' && ch < 'z') {
-				tmp = ch - 96;
-				charArr[i] = (char)(123 - tmp); // 뒤에서부터 tmp 번째 캐릭터를 대입
-			} else if (ch > 'A' && ch < 'Z') {
-				tmp = ch - 64;
-				charArr[i] = (char)(91 - tmp);
+			oneLetter = word.charAt(i);
+			if (oneLetter > 'a' && oneLetter < 'z') {
+				askiiSequence = oneLetter - 96;
+				charArr[i] = (char)(123 - askiiSequence); // 뒤에서부터 tmp 번째 캐릭터를 대입
+			} else if (oneLetter > 'A' && oneLetter < 'Z') {
+				askiiSequence = oneLetter - 64;
+				charArr[i] = (char)(91 - askiiSequence);
 			} else {
-				charArr[i] = ch;
+				charArr[i] = oneLetter;
 			}
 		}
 
