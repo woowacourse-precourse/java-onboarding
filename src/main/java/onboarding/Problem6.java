c52f2eb8c9efba5f5f2859fb52b9241f7f721f5e
leesangjin <tkdwls0628@gmail.com>
2022-10-31 19:21:07 +0900

    P6, 구현 기능목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static void initializeSubstringMap(List<List<String>> forms, Map<String, Integer> substringMap) {
+
+    }
+
+    private static void extractDuplicateSubstring(List<String> answer, List<List<String>> forms,
+                                                  Map<String, Integer> substringMap) {
+
     }
 }
