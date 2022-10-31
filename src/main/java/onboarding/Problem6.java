7bc2a224ecca6b993ba0d8e5f5c40119d252a2c7
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-10-31 21:13:26 +0900

    Feat : 문제 6 기능 6.3 구현 및 문제 6 구현 완료 문제 6 기능 6.3 구현 완료로 문제 6 구현 완료, 테스트 성공

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,9 +4,30 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-
+        // 중복 닉네임을 제출한 이메일을 set객체에 저장 (중복 데이터 제외하기 우해)
+        Set<String> emailSet = new HashSet<>();
         List<String> answer = null;
 
+        int i = 0;
+        while (i < (forms.size() - 1)) {
+            // i번째 리스트와 i에서 j만큼 떨어진 리스트를 비교
+            int j = i + 1;
+
+            while (j < (forms.size() - i)) {
+                String nickname1 = forms.get(i).get(1);
+                String nickname2 = forms.get(i + j).get(1);
+
+                // 중복 단어가 존재한다면, emailSet에 두 이메일 추가
+                if (isDupWordExist(nickname1, nickname2)) {
+                    emailSet.add(forms.get(i).get(0));
+                    emailSet.add(forms.get(i + j).get(0));
+                }
+                j++;
+            }
+            i++;
+        }
+
+        answer = SetToAscList(emailSet);
         return answer;
     }
 
@@ -41,5 +62,16 @@ public class Problem6 {
         return false;
     }
 
+    /**
+     * 기능 6.3
+     * Set객체를 오름차순 리스트객체로 변환
+     */
+    private static List<String> SetToAscList(Set<String> emailSet) {
+        List<String> answer;
+        answer = new ArrayList<>(emailSet);
 
+        // 오름차순으로 정렬
+        Collections.sort(answer);
+        return answer;
+    }
 }
