373674017b7a233ce536fa939ac56e052092be31
이소영 <th42500@naver.com>
2022-11-01 01:10:50 +0900

    [Feat] 두 개의 페이지 덧셈과 곱셈에 대하여 각각의 최대값 구하는 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,45 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static int findMax(List<Integer> person) {
+        int max = -1;
+
+        for (int i = 0; i < person.size(); i++) {
+            int sum = 0;
+            int multiple = 1;
+            int page = person.get(i);
+            while(page > 0) {
+                sum += page%10;
+                page = page/10;
+            }
+            page = person.get(i);
+            while(page > 0) {
+                multiple *= page%10;
+                page = page/10;
+            }
+            max = Math.max(max, Math.max(sum, multiple));
+        }
+        return max;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiMax = findMax(pobi);
+        int crongMax = findMax(crong);
+
+        System.out.println(pobiMax);
+        System.out.println(crongMax);
+        if(pobiMax > crongMax) {
+            answer = 1;
+        }else if(pobiMax < crongMax) {
+            answer = 2;
+        }else {
+            answer = 0;
+        }
+
         return answer;
     }
+
 }
