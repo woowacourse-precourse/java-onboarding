ba3156bcef34f400fbc1c6269ed548d0956f3232
Brian <bwook9908@gmail.com>
2022-11-01 00:43:08 +0900

    [문제3] 리팩토링 1차 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,3 +1,14 @@
+/*
+ * Problem3
+ *
+ * v1.1
+ *
+ * 2022.11.01
+ *
+ * 저작권 주의
+ */
+
+
 package onboarding;
 
 public class Problem3 {
@@ -19,14 +30,14 @@ public class Problem3 {
     }
 
     static int check369Number(int number){
-        int [] arr = makeNumToArray(number);
+        int [] ArrayNum = makeNumToArray(number);
         int count = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i]==3 ||arr[i]==6 || arr[i]==9){count++;}
+        for (int num : ArrayNum) {
+            if (num == 3 || num == 6 || num == 9) {
+                count++;
+            }
         }
         return count;
     }
-
-    //for each문 써서 풀면 훨씬 쉽고 보기 좋을듯?
 
 }
