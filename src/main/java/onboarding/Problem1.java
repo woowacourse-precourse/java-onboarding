c9ef87da4727c52ebc1d9245a2b58c75d4012736
이지영 <wldudwldhd@naver.com>
2022-10-31 21:07:00 +0900

    feat: ã합/곱을 구하는 함수, 페이지의 최대값을 반환하는 함수, main함수에서 답 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,6 +11,25 @@ class Problem1 {
     * */
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        try {
+            int pobiPoint = getMaxPoint(pobi);
+            int crongPoint = getMaxPoint(crong);
+
+            // 누가 이겼는지 판단
+            if (pobiPoint > crongPoint){
+                return 1;
+            }
+            if (pobiPoint < crongPoint){
+                return 2;
+            }
+            else{
+                return 0;
+            }
+            // 예외상황
+        } catch (IllegalArgumentException e) {
+            return -1;
+        }
+    }
 
     // 예외 사항 검사 함수
     private static void Validation(int page1, int page2) {
@@ -18,6 +37,38 @@ class Problem1 {
             throw new IllegalArgumentException("page error");
         }
     }
+    public static int getMaxPoint(List<Integer> pages) {
+        int page1 = pages.get(0);
+        int page2 = pages.get(1);
+
+        // 먼저 유효성 검사를 한 후,
+        Validation(page1, page2);
+
+        // pobi, crong 각자의 합, 곱의 최댓값 반환
+        int leftMax = Math.max(getPlus(page1), getMultiple(page1));
+        int rightMax = Math.max(getPlus(page2), getMultiple(page2));
+        return Math.max(leftMax, rightMax);
+    }
+
+
+    // 숫자는 index별 계산이 안되기 때문에 string으로 변환 후, 계산 시 c - '0'로 int형 변환을 해주었습니다.
+    public static int getPlus(int p) {
+        int plus = 0;
+        String s = Integer.toString(p);
+        for (char c : s.toCharArray()) {
+            plus += c - '0';
+        }
+        return plus;
+    }
+
+    public static int getMultiple(int p) {
+        int mul = 1;
+        String s = Integer.toString(p);
+        for (char c : s.toCharArray()) {
+            mul *= c - '0';
+        }
+        return mul;
+
 
     }
 }
