20fa08b735e566b3d5de48a37fe732f2a490caa4
JunginPark4364 <wjddlsee@gmail.com>
2022-10-31 18:14:20 +0900

    feat: find the last index of consecutive duplicate characters
    - 주어진 위치의 글자부터 해당글자가 연속으로 중복되는 마지막 위치 구하는
      기능추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String password = "";
+
+        while (true) {
+            if (cryptogram.equals(password)) break;
+        }
+
+        return password;
+    }
+
+    public static int findlastDuplicateIndex(String cryptogram, int index) {
+        char target = cryptogram.charAt(index);
+        while (index < cryptogram.length()) {
+            if (target != cryptogram.charAt(index)) {
+                break;
+            }
+
+            index++;
+        }
+
+        return index - 1;
     }
 }
