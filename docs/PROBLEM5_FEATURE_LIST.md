eef476816763596feae36015ed71c854e180effd
ParangBird <seekerman@naver.com>
2022-10-31 23:33:15 +0900

    feat: 주어진 money에 맞게 사용한 동전 개수 리스트를 반환하는 int[] getChangeCoinList(char) 함수

--- docs/PROBLEM5_FEATURE_LIST.md
+++ docs/PROBLEM5_FEATURE_LIST.md
@@ -9,8 +9,9 @@ problem5(int money){
 ```
 
 ## 기능 요구 사항 
-- 주어진 money에 맞게 사용한 동전 개수 리스트를 반환하는 int[] getChangeCoinList(char) 함수
+- ~~주어진 money에 맞게 사용한 동전 개수 리스트를 반환하는 int[] getChangeCoinList(char) 함수~~
 
 
 TODO
 - coinList 상수화
+- 예외처리(coinList <= 0인 경우)
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    public static List<Integer> getChangeCoinList(int money, int[] coinList) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < coinList.length; i++) {
+            answer.add(money / coinList[i]);
+            money -= money / coinList[i] * coinList[i];
+        }
+        return answer;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
