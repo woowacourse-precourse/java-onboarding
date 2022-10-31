9f3016d112c4222d55b18a829a7f4f705ef9e98d
anneu <b.imnuj@gmail.com>
2022-10-31 15:08:41 +0900

    feat(problem3): 문자열의 길이를 구해 박수 횟수를 반환하는 기능 구현
    - 3, 6, 9만 있는 문자열의 길이는 즉 박수 횟수를 의미

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,6 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-
         StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= number; i++) {
@@ -12,6 +10,6 @@ public class Problem3 {
 
         String clap = sb.toString().replaceAll("[^369]", "");
 
-        return answer;
+        return clap.length();
     }
 }
