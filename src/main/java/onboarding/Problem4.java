05401f053dcefdfb3f8541a96b4a5ac06516f256
WhoIsWoony <WhoIsWoony@gmail.com>
2022-10-31 17:57:43 +0900

    problem4: TODO-'기능2' complete

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,7 +9,17 @@ public class Problem4 {
         return ascii;
     }
 
+    private static String wordTransfer(String word){
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < word.length(); i++)
+        {
+            char ascii = word.charAt(i);
+            ascii = reverseTransferChar(ascii);
+            answer.append(ascii);
+        }
+        return answer.toString();
+    }
     public static String solution(String word) {
-        return "";
+        return wordTransfer(word);
     }
 }
