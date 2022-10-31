f6cbf2c71255780505b90fa1cca7ffdc8f937c7c
egoing <dudqk9696@naver.com>
2022-10-31 14:09:03 +0900

    feat : user와 친구 관계인 사람 저장 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String,Integer> foaf = new HashMap<>();
+    static Set<String> userfriends = new HashSet<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static Set<String> friendsWithUser(List<List<String>> friends,String user) {
+        Set<String> userfriends = new HashSet<>();
+        for(List<String> strings : friends) {
+            if(strings.get(0).equals(user)) {
+                userfriends.add(user + strings.get(1));
+            }
+            else if(strings.get(1).equals(user)) {
+                userfriends.add(strings.get(0) + user);
+            }
+        }
+        return userfriends;
+    }
+
 }
