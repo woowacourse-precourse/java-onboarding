5e995d798c983fc8361c1f011fc320d3eee4c28e
yxnxx <jh4rgh0@gmail.com>
2022-11-01 01:40:35 +0900

    fix: Fix Problem6 null error

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,9 +11,11 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         HashMap<String, String> word_list = new HashMap<>();
         HashSet<String> email_list = new HashSet<>();
-//
-        for(List<String> crew : forms) {
-            email_list.addAll(checkNickname(crew, word_list));
+        HashSet<String> email = new HashSet<>();
+
+        for(int i = 0; i <forms.size(); i++) {
+            email = checkNickname(forms.get(i), word_list);
+            if(!email.isEmpty()) email_list.addAll(email);
         }
         answer = email_list.stream().sorted().collect(Collectors.toList());
         return answer;
@@ -23,10 +25,12 @@ public class Problem6 {
         String nickname = crew.get(1);
         HashSet<String> nicklist = new HashSet<>();
 
-        if(word_list.isEmpty()) nicklist = null;
         for(int i = 0; i < nickname.length() -1; i++) {
             String find_nick =  nickname.substring(i, i+2);
-            if(word_list.containsValue(find_nick)) nicklist.add(word_list.get(find_nick));
+            if(word_list.containsKey(find_nick)) {
+                nicklist.add(email);
+                nicklist.add(word_list.get(find_nick));
+            }
             word_list.put(find_nick, email);
         }
         return nicklist;
