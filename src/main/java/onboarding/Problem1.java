587ffc7c517b769f027ddfec413b85ad71d97d4d
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-10-31 16:05:41 +0900

    [FEAT] : problem1 ê¸  승자도출, 예외처리 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,39 +1,62 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    /** 기능 목록
-     * 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
-     * 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
-     * 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
-     * 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
-     * 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
-     */
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int pobiScore = getMaxScore(pobi);
-        int crongScore = getMaxScore(crong);
-
-        return Math.max(pobiScore, crongScore);
+    static int pobiWin = 1;
+    static int crontWin = 2;
+    static int draw = 0;
+    static int exception = -1;
+    public static int solution(List<Integer> pobi, List<Integer> crong){
+        if(exceptCheck(pobi) || exceptCheck(crong)){
+            return exception;
+        };
+        int pobiScore = getScore(pobi);
+        int crongScore = getScore(crong);
+        int result = getWinner(pobiScore,crongScore);
+        return result;
     }
-    // 원시값 포장을 위한 점수 계산 메소드
-    public static int getMaxScore(List<Integer> pages){
-        int leftPage = pages.get(0);
-        int rightPage = pages.get(1);
-        int leftScore = calScore(leftPage);
-        int rightScore = calScore(rightPage);
 
-        return Math.max(leftScore, rightScore);
-    }
-    public static int calScore(Integer page){
-        int sum = 0;
-        int mul = 1;
-        while (page > 0) {
-            int calVar = page % 10;
-            sum += calVar;
-            mul *= calVar;
-            page /= 10;
+    private static int getScore(List<Integer> pages){
+
+        ArrayList score = new ArrayList();
+        for(Integer page : pages){
+            int sum = 0;
+            int mul = 1;
+            while (page > 0) {
+                int calVar = page % 10;
+                sum += calVar;
+                mul *= calVar;
+                page /= 10;
+            }
+            score.add(Math.max(sum,mul));
         }
-        return Math.max(sum, mul);
+        int leftPage = (int) score.get(0);
+        int rightPage = (int) score.get(1);
+        return (leftPage > rightPage) ? leftPage : rightPage;
+    }
+
+    private static int getWinner(Integer user1, Integer user2){
+        int compareScore = user1 - user2;
+        if (compareScore == 0){
+            return draw;
+        }
+        return (compareScore > 0) ? pobiWin : crontWin;
+    }
+
+    private static boolean exceptCheck(List<Integer> pages){
+        int leftPage = pages.get(0);
+        int rightPage= pages.get(1);
+        if (leftPage %2 != 1 || rightPage %2 != 0){
+            return true;
+        }
+        if(leftPage > rightPage || leftPage <1 || rightPage >400){
+            return true;
+        }
+        if(leftPage != (rightPage -1)){
+            return true;
+        }
+        return (leftPage == 1 || rightPage == 400) ? true : false;
     }
 }
