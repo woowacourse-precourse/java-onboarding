3e4c3a2a07c0589960741adbd5e12709e1da43db
Ryokuman <ryokuman@naver.com>
2022-10-31 15:00:21 +0900

    problem 6 comp

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int unit : units) {
+            answer.add(money / unit);
+            money %= unit;
+        }
+
         return answer;
     }
 }
+
+
+
