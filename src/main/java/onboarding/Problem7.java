415f6939343d7010c3565eba20c30926f99eac0e
D <doxxx93@gmail.com>
2022-10-31 13:29:01 +0900

    Problem7 (#9)
    * docs: add requirements checklist
    
    * feat: add get user's friends
    
    * feat: solve problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,68 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Set<String> userFriends = getUserFriends(user, friends);
+        Map<String, Integer> scoreMap = new TreeMap<>();
+        getAcquaintanceScore(user, friends, scoreMap, userFriends);
+        getVisitorScore(visitors, scoreMap, userFriends);
+
+        return scoreMap
+            .entrySet()
+            .stream()
+            .filter(entry -> !entry.getKey().equals(user))
+            .sorted(Problem7::compare)
+            .map(Entry::getKey)
+            .limit(5).collect(Collectors.toList());
+    }
+
+    private static Set<String> getUserFriends(String user, List<List<String>> friends) {
+        Set<String> userFriends = new TreeSet<>();
+        for (List<String> friend : friends) {
+            IntStream.range(0, 2)
+                .filter(index -> friend.get(index).equals(user))
+                .mapToObj(i -> friend.get(i ^ 1))
+                .forEach(userFriends::add);
+        }
+        return userFriends;
+    }
+
+    private static void getAcquaintanceScore(String user, List<List<String>> friends, Map<String, Integer> scoreMap,
+        Set<String> userFriends) {
+        for (List<String> friend : friends) {
+            IntStream.range(0, 2)
+                .filter(index -> isAcquaintance(user, userFriends, friend, index))
+                .forEach(index ->
+                    scoreMap.put(
+                        friend.get(index ^ 1),
+                        scoreMap.getOrDefault(friend.get(index ^ 1), 0) + 10));
+        }
+    }
+
+    private static boolean isAcquaintance(String user, Set<String> userFriends, List<String> friend, int i) {
+        return userFriends.contains(friend.get(i)) && !friend.get(i).equals(user) && !userFriends.contains(
+            friend.get(i ^ 1));
+    }
+
+    private static void getVisitorScore(List<String> visitors, Map<String, Integer> scoreMap, Set<String> userFriends) {
+        for (String visitor : visitors) {
+            if (!userFriends.contains(visitor)) {
+                scoreMap.put(visitor, scoreMap.getOrDefault(visitor, 0) + 1);
+            }
+        }
+    }
+
+    private static int compare(Entry<String, Integer> e1, Entry<String, Integer> e2) {
+        return e2.getValue().compareTo(e1.getValue());
     }
 }
