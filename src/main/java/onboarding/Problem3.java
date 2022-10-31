fabf05826e2d8f35616d7fbe2eb4dd249f3b098c
ChaCha3088 <cha3088@gmail.com>
2022-11-01 01:53:33 +0900

    final: problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,9 +4,13 @@ import java.util.stream.Stream;
 
 public class Problem3 {
     public static int solution(int number) {
-        int[] arrNum = splitEachNumbers(number);
-        int clapCount = countTargetNumber(arrNum);
-        return clapCount;
+        int finalClapCount = 0;
+        for (int i = 1; i<=number; i++) {
+            int[] arrNum = splitEachNumbers(i);
+            int clapCount = countTargetNumber(arrNum);
+            finalClapCount = finalClapCount + clapCount;
+        }
+        return finalClapCount;
     }
 
     private static int[] splitEachNumbers(int number) {
@@ -17,7 +21,7 @@ public class Problem3 {
     private static int countTargetNumber(int[] arrNum) {
         int clapCount = 0;
         for (int i = 0; i<arrNum.length; i++) {
-            if (i == 3 || i == 6 || i == 9) {
+            if (arrNum[i] == 3 || arrNum[i] == 6 || arrNum[i] == 9) {
                 clapCount = clapCount + 1;
             }
         }
