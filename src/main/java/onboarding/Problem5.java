5710bc278ae267db74c5b36010d2f450adc7678e
jindaram-stu <jinsb1999@naver.com>
2022-10-31 23:17:01 +0900

    feat : [문제5] 기본적인 현금 리스트 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final List<Integer> CASH_LIST = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0));
+
         return answer;
     }
 }
