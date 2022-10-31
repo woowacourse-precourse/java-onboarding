d5c2b1fa6269512f6e23e878adc95e4d1e7b2180
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-31 19:23:11 +0900

    feat: Problem5 - isProperMoney 메서드 구현
    돈의 범위가 1~1000000인지 확인

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * moneyCount 메서드 돈을 50000부터 나누어 몫을 저장후 값을 차감하며 1까지 카운트를 List로 반환
+ * isProperMoney 메서드 돈의 범위가 적절한지 확인
  */
 
 
@@ -17,8 +18,12 @@ public class Problem5 {
     final static int[] AMOUNT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> count = new ArrayList<>();
-
+        isProperMoney(money);
         return moneyCount(count, money);
+    }
+
+    private static void isProperMoney(int money){
+        if (money<MIN_MONEY||money>MAX_MONEY)throw new IllegalArgumentException();
     }
 
     private static List<Integer> moneyCount(List<Integer> count, int money) {
