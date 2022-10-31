9626d51bc7bf2199a71465aea3725c8153087dca
wltks98 <wltks98@hanmail.net>
2022-10-31 10:22:10 +0900

    [문제7] style : ; 오타 삭제

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,8 +8,8 @@ import onboarding.problemseven.domain.Sns;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
-        Sns sns = new Sns(friends);
-        ;
+        Sns sns = new Sns();
+        sns.saveFriends(friends);
 
         List<String> answer = sns.recommendFriend(user,visitors);
         return answer;
