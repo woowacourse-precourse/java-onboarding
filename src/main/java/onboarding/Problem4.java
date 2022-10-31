78a9a8919ee1995cb835dcb39febc90940d3ea1e
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:25:04 +0900

    [problem4] Problem4 로직 완성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,10 @@
 package onboarding;
 
+import onboarding.problem4.ReverseGenerator;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        ReverseGenerator generator = new ReverseGenerator();
+        return generator.reverse(word);
     }
 }
