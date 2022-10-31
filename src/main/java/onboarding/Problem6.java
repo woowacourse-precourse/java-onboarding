28079801c52718996e7f292c1e114d99528af2eb
yesjuhee <juhee0924@g.skku.edu>
2022-11-01 00:10:30 +0900

    [PROBLEM6] 모든 form을 하나씩 구분하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> emails = new ArrayList<>();
+
+        for(int i=0; i<forms.size()-1;i++){
+            for(int j=i+1; i<forms.size();j++){
+                // forms[i][1]과 forms[j][1]을 비교해서 같다면
+                // emails에 form[i][0]과 form[j][0]을 입력한다
+            }
+        }
+
+        return emails;
     }
 }
