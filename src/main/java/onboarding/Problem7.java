80022e58770d5ec2453a1a8964bdf0e6944bef1a
MIN-04 <kimji_0424@naver.com>
2022-10-31 16:25:48 +0900

    update Problem7 기능 1 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     /**
@@ -13,7 +12,16 @@ public class Problem7 {
      * 5. 결과값 정렬, 5개 제한
      */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        // user와 이미 친구인 사람을 찾는 로직
+        List<String> alreadyFriends = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (friend.get(1).equals(user)) {
+                alreadyFriends.add(friend.get(0));
+            }
+        }
+
         return answer;
     }
 }
