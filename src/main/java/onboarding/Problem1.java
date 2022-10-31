cd81317a729a84fdbd3135c610c13dd929b7a356
dlgosla <dlgoslaek1@gmail.com>
2022-11-01 08:51:45 +0900

    feat: [Ploblem 1] 숫자의 합과 곱 중에 최댓값 구하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,35 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        try {
+
+
+        } catch (Exception e) {
+            return -1;
+        }
+
         return answer;
+    }
+
+
+    // 해당 페이지의 숫자들을 더하거나 곱한 것 중에 최댓값
+    public static int find_max_between_sum_and_multi(Integer num) {
+
+        int sum = 0;
+        int multi = 1;
+
+        while (num != 0) {
+            int units = num % 10;
+
+            sum += units;
+            multi *= units;
+
+            num /= 10;
+        }
+
+        int max_num = (sum < multi) ? multi : sum;
+
+        return max_num;
     }
 }
