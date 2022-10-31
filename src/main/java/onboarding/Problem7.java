bacf4d11285d3d3b4186a46af7898b9076e9710e
Chanwoo Lee <lcw3176@naver.com>
2022-10-31 17:51:17 +0900

    Feat/problem7 (#12)
    * docs: 기능 구현 사항 정리
    
    * feat: 각각 유저에게 친구 목록 추가
    
    * feat: 함께 아는 친구 탐색
    
    * feat: 해당되는 점수 부여
    
    * feat: 조건에 맞게 추천 결과 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,54 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import onboarding.problem7.Crew;
+import onboarding.problem7.CrewRecommender;
+import onboarding.problem7.RecommendPoint;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, Crew> crewMap = new HashMap<>();
+        CrewRecommender crewRecommender = new CrewRecommender();
+
+        crewRecommender.addCrewToMap(List.of(user), crewMap);
+        crewRecommender.addCrewToMap(visitors, crewMap);
+
+        for(List<String> i : friends){
+            crewRecommender.addCrewToMap(i, crewMap);
+            crewRecommender.relateFriendship(i, crewMap);
+        }
+
+
+        Crew userCrew = crewMap.get(user);
+
+        for(Crew unknownCrew : crewMap.values()){
+            if(crewRecommender.isRelatedCrewExist(userCrew, unknownCrew)){
+                unknownCrew.addPoint(RecommendPoint.FRIEND.getPoint());
+            }
+        }
+
+
+        for(String i : visitors){
+            Crew visitorCrew = crewMap.get(i);
+            visitorCrew.addPoint(RecommendPoint.VISITOR.getPoint());
+        }
+
+
+        return new ArrayList<>(crewMap.values())
+            .stream()
+            .filter(crews -> crews.getPoint() != 0)
+            .sorted(Comparator.comparing(Crew::getPoint).reversed()
+                .thenComparing(Crew::getName))
+            .map(Crew::getName)
+            .filter(name -> !name.equals(user))
+            .filter(name -> !userCrew.getKnownFriendsSet().contains(crewMap.get(name)))
+            .limit(5)
+            .collect(Collectors.toList());
     }
 }
