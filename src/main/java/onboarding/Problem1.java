2562fc4f679782786e5f526cd9f7abf4ad1bd2a7
saint6839 <saint6839@gmail.com>
2022-10-31 15:24:18 +0900

    feat(Problem1): pobi와 crong이 가진 페이지의 예외 검증 메서드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,28 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!validatePages(pobi) || !validatePages(crong)) {
+            return -1;
+        }
+        return 0;
     }
+
+    private static boolean validatePages(List<Integer> player) {
+        if (player.get(1) - player.get(0) != 1) {
+            return false;
+        }
+        for (int page : player) {
+            if (page < 1 || page > 400) {
+                return false;
+            }
+        }
+        if (player.get(0) % 2 == 0 || player.get(1) % 2 != 0) {
+            return false;
+        }
+        if (player.size() != 2) {
+            return false;
+        }
+        return true;
+    }
+
 }
