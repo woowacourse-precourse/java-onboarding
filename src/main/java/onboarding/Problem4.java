d047cfb90ea9e973d4e3da880d8b37965173f2fa
3juhwan <13selfesteem91@naver.com>
2022-10-31 13:24:27 +0900

    update getCvtWord function

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,7 +8,12 @@ public class Problem4 {
     }
 
     public static String getCvtWord(String word) {
-
+        char[] wordArr = word.toCharArray();
+        for(int i=0; i<wordArr.length; i++) {
+            wordArr[i] = getCvtChar(wordArr[i]);
+        }
+        word = new String(wordArr);
+        return word;
     }
 
     public static String solution(String word) {
