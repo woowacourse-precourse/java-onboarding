b0fbbe7aa319b7d7d305b5debed3af8e6e8b73ac
inhyeok <ss112d86@gmail.com>
2022-10-31 22:00:32 +0900

    feat: Problem2 삭제해야하는 index List method 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+  public static String solution(String cryptogram) {
+    String answer = "answer";
+    List<int[]> needRemoveIndexes = findNeedRemoveIndexes(cryptogram);
+
+    return answer;
+  }
+
+  public static List<int[]> findNeedRemoveIndexes(String str) {
+    List<int[]> needRemoveIndex = new ArrayList<>();
+
+    char[] chars = str.toCharArray();
+    int startIndex = 0;
+    char compareChar = chars[0];
+    for (int i = 1; i < chars.length; i++) {
+      if (compareChar == chars[i]) {
+        if (i == chars.length - 1) {
+          needRemoveIndex.add(new int[]{startIndex, i});
+        }
+        continue;
+      }
+
+      if (startIndex != i - 1) {
+        needRemoveIndex.add(new int[]{startIndex, i - 1});
+      }
+      startIndex = i;
+      compareChar = chars[i];
     }
+    return needRemoveIndex;
+  }
 }
