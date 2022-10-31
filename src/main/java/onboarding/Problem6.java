803b12fbae594437b50c15c24744c66bd257b776
NotableDeveloper <castlereal@naver.com>
2022-10-31 00:12:19 -0700

    Problem6 (feat) - 닉네임 두 글자 추출 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for(List<String> crew : forms){
+            for(String word : split_word(crew.get(1))){
+                System.out.println(word);
+            }
+        }
+
         return answer;
+    }
+
+    private static ArrayList<String> split_word(String str){
+        ArrayList<String> result = new ArrayList<>();
+        int index = 0, end_index = 2;
+
+        while(true){
+            if(str.length() <= end_index){
+                result.add(str.substring(index));
+                break;
+            }
+            result.add(str.substring(index++, end_index++));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> forms = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "워니"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+
+        solution(forms);
     }
 }
