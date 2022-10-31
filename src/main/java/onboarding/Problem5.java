df2041771c2da2e22dda468bdb345ba0f7b3350b
egoing <dudqk9696@naver.com>
2022-10-31 16:28:21 +0900

    refactor : static -> public static 으로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,7 +11,7 @@ public class Problem5 {
 
         return answer;
     }
-    static List<Integer> greedMoney(int money) {
+    public static List<Integer> greedMoney(int money) {
         List<Integer> result = new ArrayList<>();
         int[] tmp = {50000,10000,5000,1000,500,100,50,10,1};
         for(int i=0; i<9; i++) {
