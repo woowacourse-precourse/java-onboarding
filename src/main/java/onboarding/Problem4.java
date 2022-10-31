bfb5a15fd3559c39b10263dcf859bba95436d5d9
gyumin159 <gyumin159@DESKTOP-6CKHGBO>
2022-10-31 18:13:41 +0900

    모두 소문자로 변경후 표로 사용할 문자열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+    	String answer = "";
+    	String temp = word.toLowerCase(); // 우선 모두 소문자로 변경
+    	String alpha = "abcdefghijklmnopqrstuvwxyz"; // 우영우는 거꾸로 해도 우영우
+    	
+    	
         return answer;
     }
 }
