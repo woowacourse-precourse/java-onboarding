4977efc8a0185ced3ffae736c666aa30a587121a
Koonayoung518 <koony000415@gmail.com>
2022-10-31 09:42:38 +0900

    feat(problem7) : 유저의 현재 친구 목록 찾기 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> userFriends = findUserFriends(user, friends);
         return answer;
+    }
+
+    private static List<String> findUserFriends(String user, List<List<String>> friends){
+        List<String> list = new ArrayList<>();
+        for(List<String> item : friends){
+            if(item.contains(user))
+                list.add(item.get(1 - item.indexOf(user)));
+        }
+        return list.stream().distinct().collect(Collectors.toList());
     }
 }
