b47ad1c5173de90cbe4b06df6ef3498662eff33e
Nugur1i <duckhee092@naver.com>
2022-10-31 23:21:55 +0900

    feat<problem4> : Add code to change alphabet

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -12,6 +12,40 @@ public class Problem4 {
         }
 
 
+        while (wordLength > 0 ) {
+            char character = word.charAt(wordLength-1); // character를 하나씩 뽑아오기
+            int numASCII = character; // 뽑아온 character를 아스키 코드로 바꾸기
+
+
+            if (numASCII >= 65|| numASCII <= 90 ){ //아스키 코드로 (대문자)알파벳인지 확인
+
+                int changeNumASCII = 155 - numASCII;
+
+                char changeCharacter = (char) changeNumASCII;
+
+                answer = changeCharacter + answer;
+
+
+            } else if (numASCII >= 97 || numASCII <= 122) { //아스키 코드로 (소문자)알파벳인지 확인
+
+                int changeNumASCII = 219 - numASCII;
+
+                char changeCharacter = (char) changeNumASCII;
+
+                answer = changeCharacter + answer;
+
+            } else if (numASCII < 65 || numASCII > 90 || numASCII < 97 || numASCII > 122){ //아스키 코드로 나머지 문자인지 확인
+
+                char changeCharacter = (char) numASCII;
+
+                answer = changeCharacter + answer;
+
+            }
+
+            wordLength -= 1;
+
+        }
+
 
 
 
