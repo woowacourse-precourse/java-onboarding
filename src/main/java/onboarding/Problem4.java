411693decd522f9c786688968af54999cdc1b2ad
sujin0607 <goweg0607@gmail.com>
2022-11-01 01:35:47 +0900

    Feat: 추가 문제4
    온보딩 문제4 해결, 아스키코드와 캐스팅을 사용하여 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i=0; i<word.length(); i++){
+            int ascii = (int)word.charAt(i);
+            char ch;
+            //대문자
+            if(ascii > 65 && ascii < 90){
+                ch = (char)(155 - ascii);
+            //소문자
+            }else if(ascii > 97 && ascii < 122){
+                ch = (char)(219 - ascii);
+            //공백
+            }else{
+                ch = (char)32;
+            }
+            answer += (char)ch;
+        }
         return answer;
     }
 }
