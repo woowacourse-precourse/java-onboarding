1df8426caadea9e4d64e46fb9ac8adbc8edffd32
sora5421 <21800804@handong.edu>
2022-11-01 08:43:03 +0900

    Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,6 +12,7 @@ public class Problem5 {
         return answer;
     }
     static void counting(List<Integer> count, int money){
+        //save the money list
         int[] lists = {50000,10000,5000,1000,500,100,50,10,1};
         //compute count money
         for(int i = 0; i < 9; i++){
