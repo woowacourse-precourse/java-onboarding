c1c8f6ab5c47452e25b46b10e8b612fdfdad676f
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-10-31 22:50:17 +0900

    fat : 문제1 크롱 큰 수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -19,6 +19,19 @@ class Problem1 {
         }
         pobi_max = Math.max(pobi_sum, pobi_calc);
         
+        //2) 크롱 큰 수 구하기
+        int crong_max = 0;
+        int crong_sum = 0;
+        int crong_calc = 1;
+        for(int i=0; i<crong.size(); i++){
+            String num = String.valueOf(crong.get(i));
+            for(int j=0; j<num.length(); j++){
+                crong_sum += Character.getNumericValue(num.charAt(j));
+                crong_calc = crong_calc * Character.getNumericValue(num.charAt(j));
+            }
+        }
+        crong_max = Math.max(crong_sum, crong_calc);
+        
         return answer;
     }
 }
