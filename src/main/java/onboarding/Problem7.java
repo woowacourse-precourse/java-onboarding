ac024b0b0ab47ee8b57f7f38fb2e13644a4902fc
KimSunU <kgw6147@naver.com>
2022-11-01 00:23:41 +0900

    Problem7 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,6 +9,7 @@ public class Problem7 {
         List<String> answer = new ArrayList<>();
         HashMap<String, Integer> score = new HashMap<>();
 
+        //user의 진짜 친구 찾기
         for(List<String> friend : friends){
             for(int i=0; i<2; i++){
                 if(friend.get(i) == user)
@@ -16,17 +17,19 @@ public class Problem7 {
             }
         }
 
+        //user의 진짜 친구의 친구, 한 다리 건너 아는 친구 찾기
         for(String realFriend : realFriends){
             for(List<String> friend : friends){
                 for(int i=0; i<2; i++){
                     if(friend.get(i) == realFriend) {
-                        if(friend.get(1-i) != user)
+                        if(friend.get(1-i) != user && !realFriends.contains(friend.get(1-i)))
                             frOfFriends.add(friend.get(1 - i));
                     }
                 }
             }
         }
 
+        //한 다리 건너 아는 친구의 점수를 10점 올림
         for(String frOfFriend : frOfFriends){
             if(score.containsKey(frOfFriend))
                 score.put(frOfFriend, score.get(frOfFriend) +10);
@@ -34,6 +37,7 @@ public class Problem7 {
                 score.put(frOfFriend,10);
         }
 
+        //user에 방문한 사람의 점수를 1점 올림
         for(String visitor : visitors){
             if(realFriends.contains(visitor)) continue;
 
@@ -43,9 +47,11 @@ public class Problem7 {
                 score.put(visitor,1);
         }
 
+        //숫자가 높은 순으로 정렬
         List<Map.Entry<String, Integer>> scoreList = new LinkedList<>(score.entrySet());
         scoreList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
 
+        //5명까지만 리턴
         int i=0;
         for(Map.Entry<String, Integer> scoreHigh : scoreList){
             if(i<5) {
