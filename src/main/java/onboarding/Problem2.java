a0bfcaf5e170f0901f1c062ca492d7ad9f4c03a3
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-01 03:17:12 +0900

    Chore(Problem2): 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,24 +5,26 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
 
-        // 종료 조건?
+        // answer의 길이가 0이 되면 종료
         while (answer.length() != 0) {
 
             char pre = answer.charAt(0);
             String temp = pre + "";
+            // 반복문을 돌면서 연속되는 문자 확인
             for(int i = 1; i < answer.length(); i++) {
                 if(pre == answer.charAt(i)) {
                     temp += pre;
                     continue;
                 }
 
+                //
                 if(pre != answer.charAt(i)) {
                     if(temp.length() < 2) {
                         pre = answer.charAt(i);
                         temp = String.valueOf(pre);
                         continue;
                     } else {
-                        answer = answer.replace(temp, "");
+                        answer = answer.replaceFirst(temp, "");
                         break;
                     }
                 }
@@ -32,6 +34,7 @@ public class Problem2 {
                 answer = answer.replaceFirst(temp, "");
             }
 
+            // 더 이상 연속으로 중복되는 문자가 없으면 종료
             if(temp.length() < 2) {
                 break;
             }
