cb7fec682502e706ca3f5340d07e4c2b19987420
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-31 13:54:56 +0900

    [ADD] 문제 5 - 큰 단위부터 나눠서 몫은 결과값에 저장, 나머지는 변수에 할당

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,7 +8,19 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> sorted = new ArrayList<>();
+
+        while (money > 0) {
+            for (int unit: units) {
+                if (money / unit >= 1) {
+                    sorted.add((int) Math.floor(money/unit));
+                    money = money % unit;
+                } else {
+                    sorted.add(0);
+                }
+            }
+        }
+
+        return sorted;
     }
 }
