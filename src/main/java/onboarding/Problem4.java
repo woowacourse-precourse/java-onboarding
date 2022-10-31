fd9ccdf67126c575b40e4cc286abdca9aadc1898
NotableDeveloper <castlereal@naver.com>
2022-10-30 23:55:03 -0700

    Problem4 (feat) - 단일문자 반전 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,29 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int pos = 0;
+
+        for(char c : word.toCharArray())
+            System.out.println(reverse_char(c));
+
         return answer;
+    }
+
+    private static char reverse_char(char c){
+        int pos = 0;
+
+        if(Character.isLowerCase(c)){
+            pos = c - 97;
+            return (char)(122 - pos);
+        }
+
+        else{
+            pos = c - 65;
+            return (char)(90 - pos);
+        }
+    }
+
+    public static void main(String[] args) {
+        solution("abcd");
     }
 }
