9b2f9ae544eb386a43a6038a00685e6ec4107034
doongu <must1080@naver.com>
2022-11-01 01:28:08 +0900

    feat(Problem6): List를 순회하며 해시맵에 값을 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,18 +2,26 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
+
+    static Map<String, String> splitNickMap;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         List<String> form;
+        List<String> subStringList;
         String currentEmail;
         String currentNickName;
         for (int index = 0; index< forms.size(); index++){
             form = forms.get(index);
             currentEmail = form.get(0);
             currentNickName = form.get(1);
-            splitTwiceList(currentNickName);
+
+            subStringList = splitTwiceList(currentNickName);
+            addSplitNickName(subStringList, currentEmail);
         }
 
         return answer;
@@ -29,5 +37,12 @@ public class Problem6 {
         }
 
         return twiceList;
+    }
+
+    public static void addSplitNickName(List<String> subList, String email){
+        for ( int index = 0; index < subList.size(); index++){
+            splitNickMap.put(subList.get(index), email);
+        }
+
     }
 }
