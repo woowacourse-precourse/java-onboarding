49d4d18233f47798f43cb989d3372b4ddebfda3c
yubin <dbqls2821@gmail.com>
2022-10-31 21:56:26 +0900

    FEAT: [Problem2] string ~ arraylist간에 변환하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+    }
+
+    private static ArrayList<Character> convertStringToArrayList(String cryptogram) {
+        ArrayList<Character> list = new ArrayList<>();
+        char[] characterArray = cryptogram.toCharArray();
+        for(char c : characterArray) list.add(c);
+        return list;
+    }
+
+    private static String convertArrayListToString(ArrayList<Character> list) {
+        StringBuilder sb = new StringBuilder();
+        for (Character item : list) sb.append(item);
+        return sb.toString();
     }
 }
