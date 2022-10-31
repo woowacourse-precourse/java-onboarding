a8f775a86b343a8d76e3606c2581f1e6934ad5b7
kwYoohae <gocksdl7979@gmail.com>
2022-10-31 14:37:58 +0900

    refactor(Problem5.java): else문을 피하기 위해서 돈을 교환하는 기능을 함수로 추출함
    return 을 통해서 else가 없이 돈을 교환하고 list에 교환한 값을 넣고 나머지 거스름돈을 반환함

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,23 +6,28 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = changeMoney(money);
+        List<Integer> answer = doExchangeMoney(money);
         return answer;
     }
 
-    public static List<Integer> changeMoney(int money) {
+    public static List<Integer> doExchangeMoney(int money) {
         int[] exchangeMoneyList = {50_000, 10_000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> list = new ArrayList<Integer>();
 
         for (int exchangeMoney : exchangeMoneyList) {
-            if (money / exchangeMoney != 0) {
-                list.add(money / exchangeMoney);
-                money %= exchangeMoney;
-            } else {
-                list.add(0);
-            }
+            money = exchangeAndSubtractMoney(money, exchangeMoney, list);
         }
         return list;
+    }
+
+    public static int exchangeAndSubtractMoney(int money, int exchangeMoney, List<Integer> list) {
+        if (money / exchangeMoney != 0) {
+            list.add(money / exchangeMoney);
+            return money % exchangeMoney;
+        }
+
+        list.add(0);
+        return money;
     }
 
 }
