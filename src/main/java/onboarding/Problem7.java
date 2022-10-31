7f71aea956a7f31bef0803a8f2013f36dd8da30a
saint6839 <saint6839@gmail.com>
2022-10-31 21:04:46 +0900

    feat(Problem7): 서로 친구인 사람 리스트 중, user와 친구인 사람이 존재할 때마다 map의 해당 사람(key)의 점수(value)를 10점씩 증가시킨다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -13,6 +13,21 @@ public class Problem7 {
             }
         }
 
+        Map<String, Integer> friendsScoreMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                continue;
+            }
+
+            for (String userFriend : userFriends) {
+                if (friend.contains(userFriend)) {
+                    addKnowTogetherFriendsScore(friend, userFriend, friendsScoreMap);
+                }
+            }
+        }
+
+
         return null;
     }
 
@@ -20,6 +35,14 @@ public class Problem7 {
         for (String s : friend) {
             if (!s.equals(user)) {
                 userFriends.add(s);
+            }
+        }
+    }
+
+    private static void addKnowTogetherFriendsScore(List<String> friend, String userFriend, Map<String, Integer> friendsScoreMap) {
+        for (String s : friend) {
+            if (!s.equals(userFriend)) {
+                friendsScoreMap.merge(s, 10, (oldValue, newValue) -> oldValue + 10);
             }
         }
     }
