223401bff404c0684fdc13ae8585f72378baaa6b
daeun <gain3436@gmail.com>
2022-11-01 00:53:39 +0900

    [feat](4) 알파벳인지 아닌지 체크

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+    public static boolean checkNotAlphabet(char alphabet) {
+        if (alphabet >= 65 && alphabet <= 122) {
+            return false;
+        }
+        return true;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
