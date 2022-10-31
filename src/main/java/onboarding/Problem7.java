0011e655096b62374ee554e77be343172c58b6a8
yxnxx <jh4rgh0@gmail.com>
2022-11-01 03:46:05 +0900

    refactor: Fix Problem7 finding relationship func

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,23 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        HashMap<String, Integer> friend_score = new HashMap<>();
+        HashMap<String, HashSet<String>> relation = new HashMap<>();
+        String friend_name;
         for (List<String> friend : friends) {
-            //
+            findRelationship(relation, friend);
         }
         //return answer;
     }
-    public static String findRelationship(String user, List<String> friends) {
-        if (friends.contains(user)) {
-            if (user.equals(friends.get(0))) return friends.get(1);
-            return friends.get(0);
-        }
-        return null;
+    public static void findRelationship(HashMap<String, HashSet<String>> relation, List<String> friends) {
+        String friend1 = friends.get(0);
+        String friend2 = friends.get(1);
+
+        HashSet<String> friend_list = relation.getOrDefault(friend1, new HashSet<>());
+        friend_list.add(friend2);
+        friend_list = relation.getOrDefault(friend2, new HashSet<>());
+        friend_list.add(friend1);
     }
 }
