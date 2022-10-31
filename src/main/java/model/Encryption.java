c598e3e8c0c92d6ce2ee556445d8e3e85ce08846
jybin96 <jybin96@changwon.ac.kr>
2022-10-31 17:12:44 +0900

    문제 2 코드 주석 추가 및 필요없는 멤버변수 삭제

--- src/main/java/model/Encryption.java
+++ src/main/java/model/Encryption.java
@@ -3,27 +3,30 @@ package model;
 import java.util.regex.Pattern;
 
 public class Encryption {
-    private String cryptogram;
+    //복호화한 문자열
     private String decryptedText;
+    // 동일한 문자가 연속하는지 정규표현식
     private static final String CONTINUERS = "(\\w)\\1";
 
     public Encryption(String cryptogram) {
-        this.cryptogram = cryptogram;
         this.decryptedText = cryptogram;
         decryption();
     }
 
+    //복호화
     void decryption() {
         while (isContinueChar()) {
             deleteContinueChar();
         }
     }
 
+    //연속된 문자 삭제
     void deleteContinueChar() {
         String stringTest = this.decryptedText;
         setDecryptedText(stringTest.replaceAll(CONTINUERS, ""));
     }
 
+    // 현재의 문자열에 연속된 문자가 있는지 판별하는 함수
     boolean isContinueChar() {
         Pattern pattern = Pattern.compile(CONTINUERS);
         return pattern.matcher(this.decryptedText).find();
