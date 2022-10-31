b8d80921d1cc931036473e1973d1786aa47cf75c
wjh2012 <wjh2012@naver.com>
2022-10-31 19:31:25 +0900

    (PROBLEM7)관계(HashMap), 점수표(HashMap) 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,36 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    // 관계 그래프를 저장할 해시맵
+    private static HashMap<String, ArrayList<String>> relation = new HashMap<>();
+    // 점수를 저장할 해시맵
+    private static HashMap<String, Integer> friendPoint = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        // 관계 생성
+        for (List<String>friend : friends) {
+            String userA = friend.get(0);
+            String userB = friend.get(1);
+
+            createRelation(userA, userB);
+            createRelation(userB, userA);
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    // userA, userB를 담아 관계 HashMap에 저장
+    private static void createRelation(String userA, String userB) {
+        if (relation.containsKey(userA)){
+            relation.get(userA).add(userB);
+        }else{
+            ArrayList<String> list = new ArrayList<>(Arrays.asList(userB));
+            relation.put(userA, list);
+        }
     }
 }
