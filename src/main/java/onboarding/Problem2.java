78c6684946e81e2542e772fe0868b5dce475b9d6
DongWooE <ehddn977@naver.com>
2022-11-01 04:00:28 +0900

    [p2][add] 중복 문자열을 stack에 넣는 putIntoStack 메소드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : cryptogram.toCharArray()){
+            if(stack.empty()) stack.push(c);
+            else if(stack.peek() != c) stack.push(c);
+            else{
+                while(stack.peek() != c) stack.pop();
+            }
+        }
+        return null;
     }
 }
