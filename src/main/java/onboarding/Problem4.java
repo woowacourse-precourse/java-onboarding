eb0a02f08663d4620058fa9a5a125e3011281c60
wugawuga <dnr3655@naver.com>
2022-10-31 14:43:07 +0900

    feat: 알파벳 체크 후 그대로 유지

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        char[] split = word.toCharArray();
+        for (char c : split) {
+            if (isAlphabetic(c)) {
+                continue;
+            }
+            answer.append(c);
+        }
+        return answer.toString();
+    }
+
+    private static boolean isAlphabetic(char c) {
+        return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
     }
 }
