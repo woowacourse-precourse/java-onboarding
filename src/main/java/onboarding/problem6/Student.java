e9185dff75228d6759a6fbc25711f310b5525fe1
Jin409 <sualng123@gmail.com>
2022-10-31 16:49:26 +0900

    add(problem6): Student 클래스 생성

---
+++ src/main/java/onboarding/problem6/Student.java
@@ -0,0 +1,12 @@
+package onboarding.problem6;
+
+public class Student {
+    private final String email;
+    private final String name;
+
+
+    public Student(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+}
