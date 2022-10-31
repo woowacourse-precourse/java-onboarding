505f81e5cfa175bd6b8dc2e66c7954cafb75aca5
p <gustjr2332@naver.com>
2022-10-31 19:32:42 +0900

    feat(Problem1): checkException() 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(checkException(pobi, crong)){
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
+
+
+    public static boolean checkException(List<Integer> pobi, List<Integer> crong){
+        if(pobi.get(0)%2 != 1 || pobi.get(1)%2 == 0 || crong.get(0)%2 !=1 || crong.get(1)%2 ==0){
+            return true;
+        }
+        if(pobi.get(1)-pobi.get(0) != 1 || crong.get(1)-crong.get(0) !=1){
+            return true;
+        }
+        if (pobi.get(0) < 1 || pobi.get(1) > 400 || crong.get(0) < 1 || crong.get(1) > 400) {
+            return true;
+        }
+        return false;
+    }
+
 }
