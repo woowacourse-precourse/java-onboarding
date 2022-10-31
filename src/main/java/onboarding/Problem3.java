780a4c2908cd82af02ce76db1bc9148130471472
3juhwan <13selfesteem91@naver.com>
2022-10-31 09:18:21 +0900

    update getNumOfMltplOfThree function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,7 +4,14 @@ public class Problem3 {
     static int[] numOfMltplOfThree = new int[10001];
 
     public static int getNumOfMltplOfThree(int number) {
-        return 0;
+        int ret = 0;
+        while(number > 0) {
+            int digit = number % 10;
+            number /= 10;
+            if(digit%3==0 || digit%6==0 || digit%9==0)
+                ret ++;
+        }
+        return ret;
     }
 
     public static void makeNumOfMltplOfThree() {
