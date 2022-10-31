26d2e368d9db877049266d16e78a4c105db17bd4
Dasol Jung <greteljds0251@gmail.com>
2022-10-31 12:28:34 +0900

    feat: 3-1. The count increases every time 3, 6, and 9 appear in the 1 position

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        /*
+        기능
+        1. 1의 자리에서 3, 6, 9 나올 때마다 count
+        2. 10의 자리 이상에서 3, 6, 9 나올 때마다 count
+         */
+
+        int count = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (((float)i % 10 == 3) || ((float)i % 10 == 6) || ((float)i % 10 == 9)) {
+                count++;
+            }
+            System.out.println((float)i % 10);
+            System.out.println(count);
+        }
+
+        return count;
     }
 }
