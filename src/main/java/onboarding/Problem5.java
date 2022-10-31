13217278fa47470c9e7712738778424365652634
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-31 10:56:18 +0900

    feat: 각 화폐 단위마다 변환 가능한 개수 구하기 (#5)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class Problem5 {
+    private static final List<Integer> units = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+       return findAllCntByMoneyUnit(money);
+    }
+
+    private static List<Integer> findAllCntByMoneyUnit(int money) {
+        List<Integer> answer = new ArrayList<>();
+        for (int unit : units) {
+            int cnt = money / unit;
+            answer.add(cnt);
+            money %= unit;
+        }
         return answer;
     }
 }
