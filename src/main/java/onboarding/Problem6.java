e06cf27b43c92363b7c58254a2abd72e783ef2d9
sxxgxxnz <tmddms2292@gmail.com>
2022-10-31 19:04:09 +0900

    feat Problem[1] 중복제거를 위해 Set컬렉션을 생성한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static Set<String> checkDupl(List<List<String>> forms){
+        Set<String> result = new HashSet<>();
+        
+        return result;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>(checkDupl(forms));
+        Collections.sort(answer);
         return answer;
     }
 }
