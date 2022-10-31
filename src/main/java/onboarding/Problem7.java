e2a1f3023d21a6c9e8d21b12bb019e664b74c522
chanwonlee <clw8679@naver.com>
2022-10-31 21:08:37 +0900

    FEAT: [Problem7] solution 메서드 완성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,18 +4,60 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        //기능별 메서드를 만들어 원하는 자료형으로 변환
+        Map<String, List<String>> friendList = friendList(friends);
+        Map<String, Integer> scoreList = getScoreList(friendList, visitors, user);
+        List<String> userFriends = friendList.get(user);
+
+        //user, user와 이미 친구가 되있는경우 제거
+        if (friendList.get(user) != null) {
+            scoreList.remove(user);
+            for (String userFriend : userFriends) {
+                scoreList.remove(userFriend);
+            }
+        }
+        //점수가 0이면 제거
+        while(scoreList.values().remove(0));
+
+        //점수에 따라 오름차순으로 정렬, 정수가 같으면 이름순으로 정렬
+        List<Map.Entry<String, Integer>> result = sortScore(scoreList);
+
+        //크기가 5보다 큰경우 자르기
+        return get5List(result);
+    }
+
+    private static List<Map.Entry<String, Integer>> sortScore(Map<String, Integer> scoreList) {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(scoreList.entrySet());
+        list.sort((o1, o2) -> {
+            int comparision = (o1.getValue() - o2.getValue()) * -1;
+            return comparision == 0 ? o1.getKey().compareTo(o2.getKey()) : comparision;
+        });
+        return list;
+    }
+
+    private static List<String> get5List(List<Map.Entry<String, Integer>> list) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : list) {
+            result.add(entry.getKey());
+        }
+
+        if (list.size() > 5) {
+            return result.subList(0, 5);
+        }
+        return result;
     }
 
     public static Map<String,Integer> getScoreList(Map<String, List<String>> friendList, List<String> visitors, String user) {
         //사용자와 함께 아는 친구의 수 +10점
         Map<String, Integer> scoreList = new HashMap<>();
+        //user가 친구가 한명도 없을 경우 진행하지 않음
+        if (friendList.get(user) != null) {
         List<String> userFriendList = friendList.get(user);
         for (String friend : friendList.keySet()) {
             List<String> list = friendList.get(friend);
             int score = getScore(userFriendList, list);
             scoreList.put(friend, score);
+            }
         }
 
         //사용자의 타임 라인에 방문한 횟수 +1점
@@ -28,7 +70,6 @@ public class Problem7 {
                 scoreList.replace(visitor, score);
             }
         }
-
         return scoreList;
     }
 
