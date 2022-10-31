715eaeefbb75b910498d735dc9e945d230b19975
donghae <donghae1999@gmail.com>
2022-10-31 17:41:28 +0900

    P7 : 친구 목록 구하는 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        List<String> friendMember = new ArrayList<>();
+        HashMap<String,Integer> friendScore = new HashMap<>();
+
+
+        friendMember = friendList(user, friendMember, friends);
+
         return answer;
     }
+
+    private static List friendList(String user, List<String> friendMember, List<List<String>> friends){
+        for(int i=0; i<friends.size(); i++) {
+            if (user.equals(friends.get(i).get(0))) {
+                friendMember.add(friends.get(i).get(1));
+            } else if (user.equals(friends.get(i).get(1))) {
+                friendMember.add(friends.get(i).get(0));
+            }
+        }
+        return friendMember;
+    }
+
 }
