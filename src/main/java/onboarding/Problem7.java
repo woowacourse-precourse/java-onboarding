b3a6ff0466b06a59cb6bbbdc3ce3ad2dc947809f
swhyeon98 <smreo75@gmail.com>
2022-10-31 23:35:23 +0900

    Problem7 - 함께 아는 친구의 수의 점수 구하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        HashMap<String, Integer> recommendFriends = new HashMap<>();
+        Set<String> userFriends = new HashSet<>();
+
+        for (List<String> friend : friends) {
+            String idA = friend.get(0);
+            String idB = friend.get(1);
+            userFriends.add(idA);
+            if (!idB.equals(user)) {
+                addKnowFriendScore(recommendFriends, idB);
+            }
+        }
+
         return answer;
+    }
+
+    private static void addKnowFriendScore(HashMap<String, Integer> recommendFriends, String id) {
+        if (recommendFriends.containsKey(id)) {
+            recommendFriends.put(id, recommendFriends.get(id) + 10);
+        } else {
+            recommendFriends.put(id, 10);
+        }
     }
 }
