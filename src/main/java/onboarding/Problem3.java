27a8676fd1010dc5f7eb0de5546b1c7d9425dbb8
KangYoungSeo <22019112024@dgu.ac.kr>
2022-11-01 01:29:18 +0900

    add problems code

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,36 @@
 package onboarding;
 
 public class Problem3 {
+	
+	static int[] clap = new int[]{12000, 900, 60, 3, 0};
+    static int[] unit = new int[]{10000, 1000, 100, 10, 1};
+	
+    private static int curPositionCalc(int value, int position) {
+        int curClap = 0;
+
+        for (int i = 0; i < value; i++) {
+            curClap += clap[position];
+            if (i == 3 || i == 6 || i == 9) curClap += unit[position];
+        }
+
+        return curClap;
+    }
+    
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    	int[] digit = new int[5];
+
+        for (int i = 0, temp = number; i < 5; i++, temp /= 10) digit[4 - i] = temp % 10;
+
+        int totalClap = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int value = digit[i];
+            totalClap += curPositionCalc(value, i);
+
+            number = number - value * unit[i];
+            if (value == 3 || value == 6 || value == 9) totalClap += number + 1;
+        }
+
+        return totalClap;
     }
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+    	HashSet<String> overlapEmail = new HashSet<>();
+        HashMap<String, Integer> everyName = new HashMap<>();
+        List<String> answer = null;
+
+        for (int i = 0; i < forms.size(); i++) {
+            String name = forms.get(i).get(1);
+            if (name.length() == 1) continue;
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                String twoLetter = name.substring(j, j + 2);
+
+                if (everyName.containsKey(twoLetter)) {
+                    String email1 = forms.get(i).get(0);
+                    String email2 = forms.get(everyName.get(twoLetter)).get(0);
+                    overlapEmail.add(email1);
+                    overlapEmail.add(email2);
+                    continue;
+                }
+
+                everyName.put(twoLetter, i);
+            }
+
+            answer = new ArrayList<>(overlapEmail);
+            Collections.sort(answer);
+
+
+        }
         return answer;
     }
 }
