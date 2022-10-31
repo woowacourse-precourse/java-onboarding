0a1ac80122a35e67e4662e279594bcd352a55a31
saint6839 <saint6839@gmail.com>
2022-10-31 15:42:51 +0900

    feat(Problem2): 재귀 함수로 사용할 메서드 구현, 문자열 길이가 0이면 바로 리턴하도록 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return deleteContinuousDuplicated(cryptogram);
+    }
+
+    public static String deleteContinuousDuplicated(String cryptogram) {
+        if (cryptogram.length() == 0) {
+            return cryptogram;
+        }
+
+        return null;
     }
 }
