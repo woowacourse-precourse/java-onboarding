25fa309f98fc97990c881d232b0f4794cb744beb
LeeJinHo <hosboy93@naver.com>
2022-10-31 10:21:37 +0900

    문제4. 입력값 ascii 코드로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        
+        for(char spell : word.toCharArray()) {
+        	frog(spell);
+        }
+        
         return answer;
+    }
+    
+    public static char frog(char spell) {
+    	int ascii = (int)spell;
+    	System.out.println(ascii);
+    	
+    	return spell;
     }
 }
