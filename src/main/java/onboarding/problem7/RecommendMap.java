cf9bb13e3482728f9c3ddd9ddc2ce845f93942fe
Be-student <presentsong@naver.com>
2022-10-31 20:01:27 +0900

    feat(Problem7): 점수 순으로 5명 반환하는 기능 추가

---
+++ src/main/java/onboarding/problem7/RecommendMap.java
@@ -0,0 +1,34 @@
+package onboarding.problem7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecommendMap {
+    private final Map<String, Integer> stringIntegerMap;
+
+
+    public RecommendMap() {
+        this.stringIntegerMap = new HashMap<>();
+    }
+
+    public RecommendMap(RecommendMap recommendMap) {
+        this.stringIntegerMap = recommendMap.getStringListMap();
+    }
+
+    public Map<String, Integer> getStringListMap() {
+        return stringIntegerMap;
+    }
+
+    public void put(String key, int value) {
+        stringIntegerMap.put(key, value);
+    }
+
+    public void add(UserId key, int value) {
+        int before = this.get(key.getId());
+        stringIntegerMap.put(key.getId(), before + value);
+    }
+
+    public int get(String key) {
+        return stringIntegerMap.getOrDefault(key, 0);
+    }
+}
---
+++ src/main/java/onboarding/problem7/Recommender.java
@@ -0,0 +1,43 @@
+package onboarding.problem7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Recommender {
+    private final RecommendMap score;
+    private final SnsNetwork snsNetwork;
+
+    public Recommender(RecommendMap dto, SnsNetwork snsNetwork) {
+        this.score = dto;
+        this.snsNetwork = snsNetwork;
+    }
+
+    public List<String> getFive(UserId user) {
+        score.put(user.getId(), 0);
+        removeFriendsScore(user);
+
+        List<String> keySet = new ArrayList<>(score.getStringListMap().keySet());
+        keySet.sort(this::sort);
+
+        return keySet.stream().filter(this::isNotZero).limit(5).collect(Collectors.toList());
+    }
+
+    public int sort(String o1, String o2) {
+        Integer value1 = score.getStringListMap().get(o1);
+        Integer value2 = score.getStringListMap().get(o2);
+        if (value1 != value2) {
+            return value2.compareTo(value1);
+        }
+        return o1.compareTo(o2);
+    }
+
+    private void removeFriendsScore(UserId user) {
+        System.out.println(snsNetwork.getConnection(user).stream().map((id) -> id.getId()).collect(Collectors.toList()));
+        snsNetwork.getConnection(user).forEach((friendId) -> score.put(friendId.getId(), 0));
+    }
+
+    private boolean isNotZero(String key) {
+        return score.getStringListMap().get(key) != 0;
+    }
+}
