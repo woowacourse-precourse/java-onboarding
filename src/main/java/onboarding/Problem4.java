41f4209acf36106c511c7a5906d9c5a021d6dc37
Juuuic <sn990908a@gmail.com>
2022-10-31 10:41:07 +0900

    4번문제구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        //char[] word_arr = word.toCharArray();
+        for (int i=0; i<word.length(); i++) {
+            char word_ch = word.charAt(i);
+            int word_int = (int)word_ch;
+
+            if (word_int >= 65 && word_int <= 90) {
+                answer += String.valueOf((char)(155 - word_int));
+            }
+            else if (word_int >= 97 && word_int <= 122) {
+                answer += String.valueOf((char)(219 - word_int));
+            }
+            else {
+                answer += word_ch;
+            }
+        }
+
         return answer;
     }
 }
