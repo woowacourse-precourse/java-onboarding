24fc0de80f3bb2313f187a85f4d66f1a8eea7407
Ilchul Kim <huj0159@gmail.com>
2022-11-01 03:36:52 +0900

    feat: add user friend find method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> hm = new HashMap<>();
+        List<String> friendList = findUserFriend(user, friends);
+
+
         return answer;
+    }
+
+    public static List<String> findUserFriend(String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
+
+        for (List<String> target : friends) {
+            if (target.get(0) == user) {
+                friendList.add(target.get(1));
+            }
+            if (target.get(1) == user) {
+                friendList.add(target.get(0));
+            }
+        }
+
+        return friendList;
     }
 }
