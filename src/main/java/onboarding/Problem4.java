beeb63a483d6fedc7628a0a289c97b118f4ec6ac
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 16:24:47 +0900

    wordCopyNum의 값을 문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,6 +10,14 @@ public class Problem4 {
             wordCopyNum[i] = (int)wordCopy[i];
         }
 
+        for (int i = 0; i < wordCopyNum.length; i++) {
+            if (wordCopyNum[i] >= 65 && wordCopyNum[i] <= 90) wordCopyNum[i] = 155-wordCopyNum[i];
+            if (wordCopyNum[i] >= 97 && wordCopyNum[i] <= 122) wordCopyNum[i] = 219-wordCopyNum[i];
+        }
+
+        for (int i = 0; i < wordCopyNum.length; i++) {
+            wordCopy[i] = (char)wordCopyNum[i];
+        }
 
         return answer;
     }
