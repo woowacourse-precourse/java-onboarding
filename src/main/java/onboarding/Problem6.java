6c8b0f39ccf1208c1163fe9d931434edf1f5fd27
doongu <must1080@naver.com>
2022-10-31 22:04:16 +0900

    feat(Problem6): 주어진 닉네임을 2개씩 잘라 List로 만드는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> form;
+        String currentEmail;
+        String currentNickName;
+        for (int index = 0; index< forms.size(); index++){
+            form = forms.get(index);
+            currentEmail = form.get(0);
+            currentNickName = form.get(1);
+            splitTwiceList(currentNickName);
+        }
+
         return answer;
+    }
+
+    public static List<String> splitTwiceList(String nickName){
+        List<String> twiceList = new ArrayList<>();
+        String subNickName;
+
+        for (int index = 0; index < nickName.length()-1; index++){
+            subNickName = nickName.substring(index, index + 2);
+            twiceList.add(subNickName);
+        }
+
+        return twiceList;
     }
 }
