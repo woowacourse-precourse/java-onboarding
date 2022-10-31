207cb67129801b8859e7f3164724e740ceb18731
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-31 17:39:38 +0900

    refactor(problem3): 상수 set 대문자로 rename

--- src/main/java/onboarding/problem3/Game369.java
+++ src/main/java/onboarding/problem3/Game369.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class Game369 {
 
-    private static final Set<Integer> clapNumbers = Set.of(3, 6, 9);
+    private static final Set<Integer> CLAP_NUMBERS = Set.of(3, 6, 9);
 
     public static int getTotalClapCount(int lastNumber){
         int count = 0;
@@ -17,7 +17,7 @@ public class Game369 {
     private static int getClapCount(int number){
         int count = 0;
         while(number != 0){
-            if(clapNumbers.contains(number%10)){
+            if(CLAP_NUMBERS.contains(number%10)){
                 count += 1;
             }
             number /= 10;
