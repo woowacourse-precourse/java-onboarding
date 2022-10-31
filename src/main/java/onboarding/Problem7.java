02a5959bc87f2ec860f8529c2a2f6b84b988e056
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-11-01 00:59:41 +0900

    feat: implement scoring mutual friend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,16 +2,21 @@ package onboarding;
 
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Problem7 {
+    public static final int MUTUAL_SCORE = 10;
 
     private static final Set<String> userFriendList = new HashSet<>();
+    private static final Map<String, Integer> recommendScore = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         saveUserFriendList(user, friends);
+        addMutualFriend(user, friends);
         return Collections.emptyList();
     }
 
@@ -28,4 +33,36 @@ public class Problem7 {
             userFriendList.add(friend.get(0));
         }
     }
+
+    private static void addMutualFriend(String user, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            scoreMutualFriend(user, friend);
+        }
+    }
+
+    private static void scoreMutualFriend(String user, List<String> friend) {
+        if (!isNewFriend(user, friend.get(0))) {
+            if (isNewFriend(user, friend.get(1))) {
+                addRecommendScore(friend.get(1), MUTUAL_SCORE);
+            }
+        } else if (!isNewFriend(user, friend.get(1))) {
+            if (isNewFriend(user, friend.get(0))) {
+                addRecommendScore(friend.get(0), MUTUAL_SCORE);
+            }
+        }
+    }
+
+    private static boolean isNewFriend(String user, String friend) {
+        return !(userFriendList.contains(friend) || user.equals(friend));
+    }
+
+    private static void addRecommendScore(String friend, int x) {
+        if (recommendScore.containsKey(friend)) {
+            recommendScore.replace(friend, recommendScore.get(friend) + x);
+        } else {
+            recommendScore.put(friend, x);
+        }
+    }
+
+
 }
