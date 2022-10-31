abea0a9333538ea769f1b32339b17a28ae9efd8e
LeeJinHo <hosboy93@naver.com>
2022-10-31 09:59:53 +0900

    문제3. number의 각 자리가 3,6,9 일때 박수치기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        
+        for(int i=1; i<number+1; i++) {
+        	int checkNum = i;
+        	System.out.println("checkNum 값 받았을 때 : " + checkNum);
+        	while(checkNum!=0) {
+        		if(checkNum%10==3 || checkNum%10==6 || checkNum%10==9) {
+        			answer++;
+        		}
+        		checkNum = checkNum/10;
+        	}
+        	System.out.println("박수 횟수 : " + answer);
+        }
+        
+        System.out.println(answer);
         return answer;
     }
 }
