03b71778e3885a74d4ae01137158f0fc7778cfbd
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:28:26 +0900

    [problem1]예외 사항 -1리턴 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,17 +4,24 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.size() != 2 && crong.size() != 2) {
+            return -1;
+        }
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
+            return -1;
+        }
+        if (pobi.get(0) <= 1 || pobi.get(1) >= 400 || crong.get(0) <= 1 || crong.get(1) >= 400) {
+            return -1;
+        }
+        if (pobi.get(0) > pobi.get(1) || crong.get(0) > crong.get(1)) {
+            return -1;
+        }
+        if (pobi.get(0) % 2 == 0 || pobi.get(1) % 2 != 0 || crong.get(0) % 2 == 0 || crong.get(1) % 2 != 0) {
+            return -1;
+        }
 
-
-        if(pobimax > crongmax)
-            return 1;
-        else if (pobimax == crongmax)
-            return 0;
-        else
-            return 2;
 
     }
-
 
 
 }
