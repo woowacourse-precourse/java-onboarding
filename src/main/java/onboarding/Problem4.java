5f2f62574df0cd8a0e7b75588adcbc1e472a27a5
donghae <donghae1999@gmail.com>
2022-10-31 23:57:50 +0900

    P4 : 최종 수정, 주석

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,15 +1,16 @@
 package onboarding;
 
 public class Problem4 {
-    private static int maxUpperAscii = 90;
-    private static int minUpperAscii = 65;
-    private static int maxLowerAscii = 122;
-    private static int minLowerAscii = 97;
+    //대소문자 최대 최솟값 아스키코드
+    private static int maxUpperAscii = 90, minUpperAscii = 65;
+    private static int maxLowerAscii = 122, minLowerAscii = 97;
+
     public static String solution(String word) {
         String answer = "";
         answer = cvtString(word);
         return answer;
     }
+    //청개구리 사전 변환 함수
     private static String cvtString(String word){
         String result="";
         for(int i=0; i<word.length(); i++){
