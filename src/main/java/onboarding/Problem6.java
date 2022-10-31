c9c4643a281a817630176ef03a8d6e490aad150c
Sehoon Kim <shggm2000@gmail.com>
2022-10-31 23:33:30 +0900

    닉네임이 1이상 20미만인 조건을 만족하기 위한 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
+        HashSet<String> emails = new HashSet<>();
+        Map<String, String > hashMap = new HashMap<>();
+
+        for(int i = 0; i<forms.size(); i++) {
+            String name = forms.get(i).get(1);
+
+            if (name.length() < 2 || name.length() > 19) {
+                break;
+            }
+        }
+
         List<String> answer = List.of("answer");
+
         return answer;
     }
 }
+
+
