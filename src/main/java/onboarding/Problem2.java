14d58bba096442ea41c766dbf97add60eb328d30
jindaram-stu <jinsb1999@naver.com>
2022-10-31 22:51:34 +0900

    feat : [문제2] 중복이 검출되었을 때, 중복 횟수 증가 로직 적용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -26,7 +26,19 @@ public class Problem2 {
         int dupliCount = 1;
         boolean isDuplicate = false;
         for (int i = 0; i < cryptogram.length(); i++) {
-           // 하
+            if (!stack.isEmpty() && cryptogram.charAt(i) == stack.peek()) {
+                isDuplicate = true;
+                dupliCount += 1;
+                if (i == cryptogram.length() - 1 && (isDuplicate || cryptogram.length() == 2)) {
+                    stack.push(cryptogram.charAt(i));
+                    // 중복값 제거 로직
+                    break;
+                }
+            } else if (!stack.isEmpty() && cryptogram.charAt(i) != stack.peek() && isDuplicate) {
+                // 중복값 제거 로직
+                dupliCount = 1;
+                isDuplicate = false;
+            }
             stack.push(cryptogram.charAt(i));
         }
         return getStringFromStack(stack);
