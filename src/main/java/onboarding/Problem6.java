baebb1df44321af52784274420b097e01419f42a
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-10-31 21:47:19 +0900

    221031 Fix: P6 - 기능2 함수 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -18,23 +15,34 @@ public class Problem6 {
 
         for(int i = 0; i < names.size(); i++) {
             //2. 어떤 이름 문자열을 선택하여, 그 문자열의 모든 2자 이상 부분문자열을 만든다.
-            Set<String> subStrings = createSubString(names.get(i));
+            List<String> subStrings = createSubString(names.get(i));
 
             //3. 해당 부분문자열이 다른 이름들에 포함되어 있는지 확인한다.
             //포함되어 있다면 그 계정의 이메일을 정답 리스트에 담고 다음 반복으로 넘어간다.
+            checkContent(names, subStrings, i);
         }
 
         return answer;
     }
 
-    public static Set<String> createSubString(String name){
-        Set<String> subStrings = new HashSet<>();
+    public static List<String> createSubString(String name){
+        Map<String, Integer> subStrMap = new HashMap<>();
 
         for(int i = 0; i < name.length() - 1; i++){
             String subStr = name.substring(i, i + 2);
-            subStrings.add(subStr);
+            subStrMap.put(subStr, 0);
         }
 
-        return subStrings;
+        return new ArrayList<>(subStrMap.keySet());
+    }
+
+    public static void checkContent(List<String> names, List<String> subStrings, int k){
+        for(int i = 0; i < names.size(); i++){
+            if(i == k)
+                continue;
+            for(int j = 0; j < subStrings.size(); j++){
+                //if(names.get(i).contains())
+            }
+        }
     }
 }
