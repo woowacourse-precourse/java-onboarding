762d12d3aea90cb74e1119c96145611eb2811402
doehy <dbehxks02@naver.com>
2022-10-31 20:03:44 +0900

    feat:예외처리해주는기능구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,18 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(!exceptionCheck(pobi) || !exceptionCheck(crong)){return -1;}
+
         return answer;
+    }
+    static boolean exceptionCheck(List<Integer> list) {
+        int left = list.get(0);
+        int right = list.get(1);
+
+        if(left+1 != right) {return false;}
+        if(left%2 == 0) {return false;}
+        if(left < 1 || left > 400) {return false;}
+        if(right <1 || right > 400) {return false;}
+        return true;
     }
 }
