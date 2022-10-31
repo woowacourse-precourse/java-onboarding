52bde344f841da1fbbc57ce40a99c8cf005202ab
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:22:49 +0900

    [problem4] ReverseGenerator 생성자 추가

--- src/main/java/onboarding/problem4/ReverseGenerator.java
+++ src/main/java/onboarding/problem4/ReverseGenerator.java
@@ -1,2 +1,11 @@
-package onboarding.problem4;public class ReverseGenerator {
+package onboarding.problem4;
+
+public class ReverseGenerator {
+    private StringBuilder result;
+
+    public ReverseGenerator(){
+        result = new StringBuilder();
+    }
+    //reverse 함수, 문자열 입력받아서 한글자씩 변환 하여 result에 추가한 후 반환
+    //한글자씩 변환하는 함수
 }
