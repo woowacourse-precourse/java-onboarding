b052989342127cf8955c9f29c250c72342bcf80e
“rotomoo” <quddlf410@naver.com>
2022-10-31 16:48:29 +0900

    feat : implementation complete of Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem3 {
+
+    private int checkThreeSixNine(int number) {
+
+        String stringNumber = String.valueOf(number);
+        int cnt = 0;
+
+        for (char curChar : stringNumber.toCharArray()) {
+            curChar = (char) (curChar - '0');
+            if (curChar == 3 || curChar == 6 || curChar == 9) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        Problem3 problem3 = new Problem3();
+        int[] tSNArray = new int[10001];
+
+        for (int i = 1; i <= 10000; i++) {
+            tSNArray[i] = tSNArray[i - 1] + problem3.checkThreeSixNine(i);
+        }
+
+        return tSNArray[number];
     }
 }
