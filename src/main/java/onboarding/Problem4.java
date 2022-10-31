fab18a96ed8248e04340d56c4b1adb92fe4c1dae
saint6839 <saint6839@gmail.com>
2022-10-31 16:01:52 +0900

    feat(Problem4): 대,소문자에 맞게 문자를 대치하여 결과를 반환하도록 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,12 +6,13 @@ public class Problem4 {
         StringBuilder reverseWord = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             if (validateAlphabet(word.charAt(i))) {
-
+                reverseWord.append(replaceCharacter(word.charAt(i)));
+                continue;
             }
-
+            reverseWord.append(word.charAt(i));
         }
 
-        return null;
+        return reverseWord.toString();
     }
 
     private static boolean validateAlphabet(char character) {
@@ -25,5 +26,12 @@ public class Problem4 {
             return true;
         }
         return false;
+    }
+
+    private static char replaceCharacter(char character) {
+        if (validateUpperCase(character)) {
+            return (char) ('Z' - (character - 'A'));
+        }
+        return (char) ('z' - (character - 'a'));
     }
 }
