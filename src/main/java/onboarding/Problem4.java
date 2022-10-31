891a32d74289a3e72e66ab184ee7cbbbbec57de0
saint6839 <saint6839@gmail.com>
2022-10-31 16:00:53 +0900

    feat(Problem4): 대문자인지 소문자인지 검증(구분)하는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,11 +8,20 @@ public class Problem4 {
             if (validateAlphabet(word.charAt(i))) {
 
             }
+
         }
+
         return null;
     }
+
     private static boolean validateAlphabet(char character) {
         if (character >= 'A' && character <= 'Z' || character >= 'a' && character <= 'z') {
+            return true;
+        }
+        return false;
+    }
+    private static boolean validateUpperCase(char character) {
+        if (character >= 'A' && character <= 'Z') {
             return true;
         }
         return false;
