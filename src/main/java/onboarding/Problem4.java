5e1c0695a6b7fea9e403df0c3ce9edc90463d50e
glass <glass7312@gmail.com>
2022-10-31 20:34:13 +0900

    Problem4 refactor(줄바꿈)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,18 +10,15 @@ public class Problem4 {
         for(int i=0; i<word.length(); i++) {
             Ascii_Num=word.charAt(i);
             if(32==Ascii_Num) answer+=" ";
-
             if(Ascii_Num>64 && Ascii_Num<91) {
                 sub_num = 90 - word.charAt(i);
                 answer += (char) (65 + sub_num);
             }
-
             if(Ascii_Num>96 && Ascii_Num<123) {
                 sub_num=122 - word.charAt(i);
                 answer += (char)(97+sub_num);
             }
         }
-
         return answer;
     }
 }
