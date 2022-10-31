02642c94ec2def369e70a4c1d1a2db5b0bf68bdb
pbkdpwls <pbkdpwls@naver.com>
2022-11-01 03:08:14 +0900

    feat(Problem5):기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        int[] sort_money = new int[]{50000,10000,5000,1000,500,100,50,10,1};
+        int money_ = money;
+
+        for(int i=0; i<sort_money.length;i++) {
+            answer.add(money_/sort_money[i]);
+            money_ = money_% sort_money[i];
+        }
+
         return answer;
     }
 }
