aad1b1b8eca6d6be6e8b8362c85dbba27e540276
Amenable-C <duswo0624@naver.com>
2022-11-01 00:30:08 +0900

    feat : [문제1] 입력 값 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,25 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean exception_handling(int pobi_left, int pobi_right, int crong_left, int crong_right) {
+        if((pobi_left % 2!= 1) || (pobi_right % 2 != 0) || (pobi_left + 1 != pobi_right)) {
+            return true;
+        }
+        if((crong_left % 2!= 1) || (crong_right % 2 != 0) || (crong_left + 1 != crong_right)) {
+            return true;
+        }
+        return false;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobi_left = pobi.get(0);
+        int pobi_right = pobi.get(1);
+        int crong_left = crong.get(0);
+        int crong_right = crong.get(1);
+
+        if(exception_handling(pobi_left, pobi_right, crong_left, crong_right))
+            return -1;
+
         return answer;
     }
 }
