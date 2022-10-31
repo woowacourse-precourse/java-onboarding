116a0224ac69d20d8b5a5ccd123f4a2359eed78e
kmularise <kmula222@gmail.com>
2022-10-31 21:37:28 +0900

    문자가 같은 부분인지 체크하는 부분 코드 길이 줄임

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,16 +1,9 @@
 package onboarding;
 
 public class Problem2 {
-	private static boolean checkSame(char ch1, char ch2) {
-		if (ch1 == ch2) {
-			return true;
-		}
-		return false;
-	}
-
 	private static void solveCryptogram(char ch, StringBuffer answer) {
 		if (answer.length() > 0 &&
-			checkSame(ch, answer.charAt(answer.length() - 1))) {
+			ch == answer.charAt(answer.length() - 1)) {
 			answer.deleteCharAt(answer.length() - 1);
 		} else {
 			answer.append(ch);
