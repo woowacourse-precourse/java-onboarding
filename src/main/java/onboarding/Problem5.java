0aa4d7876949ffdea54d02e7bea31c811846c403
moonn6pence <shin421179@gmail.com>
2022-10-31 19:52:18 +0900

    feat #5 : currency 리스트, getChange 메소드 추가
    - 인덱스로 화폐 금액 치환하는 currency 리스트 추가
    - getChange메소드에서 그리디 알고리즘으로 화폐 변환 후 결과 리스트 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = getChange(money);
+
+        return answer;
+    }
+
+    // 화폐 크기 lookup 테이블
+    private static List<Integer> currency = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+    // 그리디 알고리즘은 화폐 변환
+    private static List<Integer> getChange(int money){
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < currency.size(); i++) {
+            answer.add(money/currency.get(i));
+            money %= currency.get(i);
+        }
+
         return answer;
     }
 }
