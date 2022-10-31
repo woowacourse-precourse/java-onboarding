1cb9fdd2ec815ac62018b5dea1cb908decdd597b
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-01 04:47:41 +0900

    [PROBLEM 5] 단위별 개수 구하는 메소드
    1. 금액 단위 배열 추가
    2. 단위별 몫과 남은 금액 계산 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] unitArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
         return answer;
+    }
+
+    static List<Integer> unitCalc(int money, int unit) {
+        List<Integer> list = new ArrayList<>();
+
+        int quo = money / unit;
+        int rem = money - (quo * unit);
+        list.add(quo);
+        list.add(rem);
+
+        return list;
     }
 }
