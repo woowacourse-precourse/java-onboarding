b8321bba2130e1986d5d4e6174b6466001c3d711
MinjeongKong <kong1301@ajou.ac.kr>
2022-10-31 21:10:34 +0900

    Problem7 - add method: findFriend, calcFriend, calcVisitor

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Problem7 {
 
     public static HashMap<String, List<String>> hpFriend = new HashMap<>();
+    public static HashMap<String, Integer> hpScore = new HashMap<>();
 
     /**
      * 친구 관계 저장 메서드
@@ -31,6 +32,44 @@ public class Problem7 {
             hpFriend.put(o.get(0), friendList1);
             hpFriend.put(o.get(1), friendList2);
 
+        }
+    }
+
+    /**
+     *함께 아는 친구의 수를 세는 메서드
+     * @param user1 사용자1
+     * @param user2 사용자2
+     * @return user1과 user2가 함께 아는 친구의 수
+     */
+    public static int findFriend(List<String> user1, List<String> user2) {
+        int friend = 0;
+        for (String name : user1) {
+            if(user2.contains(name)) friend++;
+        }
+        return friend;
+    }
+
+    /**
+     * 친구 점수 계산 메서드
+     * @param user 친구 추천을 받을 사용자
+     */
+    public static void calcFriend(String user) {
+        List<String> userFriend = hpFriend.get(user);
+        hpFriend.forEach((o, friends) ->{
+            if (user.equals(o)) return;
+            int score = findFriend(userFriend, friends) * 10;
+            if(score==0) return;
+            hpScore.put(o, hpScore.getOrDefault(o, 0)+score);
+        });
+    }
+
+    /**
+     * 방문 점수 계산 메서드
+     * @param visitors 방문 기록
+     */
+    public static void calcVisitor(List<String> visitors) {
+        for (String o : visitors) {
+            hpScore.put(o, hpScore.getOrDefault(o, 0) + 1);
         }
     }
 
