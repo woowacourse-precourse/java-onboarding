1f97c32b1f14a1553006d487811e491309b5c997
yun <yshyeon102@gmail.com>
2022-11-01 00:59:22 +0900

    feat: Problem6 - solution() 메서드 정의
    - solution() 메서드 구현
    - getAlarmList() 메서드 정의
    
    #TODO
    getAlarmList() 메서드에 필요한,
    -makePartialNickNameList() 메서드 : 닉네임을 2글자, 3글자, ....로 나눈후 리스트로 만들기
    - checkAndEnrollPartialNickNameList() : 나눈 닉네임들을 검사하여 중복이 있으면 알람Set에 추가, 없으면 Map에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        return getAlarmList(forms);
+    }
+
+    private static List<String> getAlarmList(List<List<String>> forms) {
+        HashSet<String> alarmSet = new HashSet<>();
+        HashMap<String, String> partialNickNameEmailMap = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickName = form.get(1);
+
+            List<String> partialNickNameList = makePartialNickNameList(nickName);
+            checkAndEnrollPartialNickNameList(partialNickNameList, email, alarmSet, partialNickNameEmailMap);
+        }
+
+        return convertMapToListAndSort(alarmSet);
     }
 }
