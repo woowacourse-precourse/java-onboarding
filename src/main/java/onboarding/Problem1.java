898f8862cb175ebe31f856bfc4ed64517919054c
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2022-10-31 23:31:44 +0900

    feat: add fuctnion 'findMax'

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,44 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int findMax(List<Integer> list) {
+        int maxValue = 0;
+
+        if (list.get(0) + 1 != list.get(1)) {
+            return -1;
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            int sum = 0;
+            int mul = 1;
+            int page = list.get(i);
+
+            while (page != 0) {
+                int n = page % 10;
+
+                sum += n;
+                mul *= n;
+
+                page /= 10;
+            }
+
+            if (sum > maxValue) {
+                maxValue = sum;
+            }
+
+            if (mul > maxValue) {
+                maxValue = mul;
+            }
+
+        }
+
+        return maxValue;
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
     }
 }
