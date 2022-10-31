e12f2903bb35a53b5fb6a8b7e61189eaa34c5af6
채우석 <wochae@wochae-MacBookAir.local>
2022-10-31 18:27:30 +0900

    [fix] 5.1 answer을 담는 리스트 타입 구체화 필요로 인한 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,12 +1,13 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static int[] won = new int[9];
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
 
         initWon();
         bankDraw(money, answer);
