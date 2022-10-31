a3f8df1098a155913da0686bfd5ec53d4d701942
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-10-31 19:17:24 +0900

    feat: [Prob 3] 1~number까지의 손뼉 횟수를 모두 더하는 기능 추가 number에 도달하기까지 전체 박수 횟수를 계산하는 getSumOfEntireHandclap() 함수를 추가합니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,14 +4,8 @@ import java.util.List;
 
 public class Problem3 {
     public static int maxNumber=10000;
-    public static Integer[] handclapCountList=new Integer[maxNumber];
+    public static Integer[] handclapCountList=new Integer[maxNumber+1];
 
-    public static void recordHandclapByNumber(){
-        for(int num=1;num<=maxNumber;num++){
-            int handclapResultByNumber=countHandclapByNumber(num);
-            handclapCountList[num]=handclapResultByNumber;
-        }
-    }
     public static int countHandclapByNumber(int number){
         int handclapCount=0;
         while(number>0){
@@ -23,9 +17,24 @@ public class Problem3 {
         }
         return handclapCount;
     }
+    public static void recordHandclapByNumber() {
+        for (int num = 1; num <= maxNumber; num++) {
+            int handclapResultByNumber = countHandclapByNumber(num);
+            handclapCountList[num] = handclapResultByNumber;
+        }
+    }
+
+    public static int getSumOfEntireHandclap(int number){
+        int sumOfEntireHandclap=0;
+        for(int numIndex=0;numIndex<number;numIndex++){
+            sumOfEntireHandclap+=handclapCountList[numIndex];
+        }
+        return sumOfEntireHandclap;
+    }
+
     public static int solution(int number) {
         int answer = 0;
-
+        answer=getSumOfEntireHandclap(number);
         return answer;
     }
 }
