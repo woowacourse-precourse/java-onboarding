e39c8c0fe5e1b8478545a1352da287317d42373a
zcae1020 <zcae1020@naver.com>
2022-10-31 11:01:02 +0900

    feat(Problem6): detectError method

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -15,7 +15,18 @@ public class Problem6 {
     }
 
     private static boolean detectError(List<List<String>> forms){
+        for(List<String> l: forms){
+            if(l.get(0).length()<11||l.get(0).length()>=20)
+                return false;
+            if(l.get(1).length()>=20)
+                return false;
 
+            String email = l.get(0);
+            if(!email.substring(email.length()-10,email.length()).equals("@email.com"))
+                return false;
+        }
+
+        return true;
     }
 
     private static void convertList2TwoCharCount(String str, int idx){
