c79aa709f04c11e5d618dfc0f02d0729d44370ab
이지혁 <olzlgur@naver.com>
2022-10-31 22:11:02 +0900

    손뼉 횟수 카운팅 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
-        return 1;
+        return countClap(number);
+    }
+    private static int countClap(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            count += checkNumber(i);
+        }
+        return count;
     }
     private static int checkNumber(int number){
         int count = 0;
