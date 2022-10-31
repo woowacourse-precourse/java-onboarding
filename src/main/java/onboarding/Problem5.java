63c31175a4917a3bb57fc33d9f5124397837e1e5
wugawuga <dnr3655@naver.com>
2022-10-31 15:00:42 +0900

    refactor: 지폐, 동전 순으로 배열 생성 후 나머지 List 에 입력

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,24 +8,11 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>(Collections.emptyList());
 
-        answer.add(money / 50000);
-        money %= 50000;
-        answer.add(money / 10000);
-        money %= 10000;
-        answer.add(money / 5000);
-        money %= 5000;
-        answer.add(money / 1000);
-        money %= 1000;
-        answer.add(money / 500);
-        money %= 500;
-        answer.add(money / 100);
-        money %= 100;
-        answer.add(money / 50);
-        money %= 50;
-        answer.add(money / 10);
-        money %= 10;
-        answer.add(money);
-
+        int[] don = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i : don) {
+            answer.add(money / i);
+            money %= i;
+        }
         return answer;
     }
 }
