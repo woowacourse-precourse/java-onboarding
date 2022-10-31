a1e473fef2923996bcfab8479206e98a6383d9e9
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:19:38 +0900

    feat: 스택으로 암호해독

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static Stack<Character> decodeCryptogram(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!stack.empty() && cryptogram.charAt(i) == stack.peek()) {
+                stack.pop();
+            }else {
+                stack.push(cryptogram.charAt(i));
+            }
+        }
+        return stack;
     }
 }
