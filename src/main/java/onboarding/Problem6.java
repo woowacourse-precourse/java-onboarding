fd02d92c13b3370ec3f8af447ab6cd2e4cf26055
ddingmin <ddingminyt@gmail.com>
2022-10-31 09:07:43 +0900

    feat(Ploblem6): 이메일 도메인 체크 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,24 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static Boolean isValidEmail(String email){
+        char at = '@';
+
+        for (int i = 0; i < email.length(); i++) {
+            if (email.charAt(i) == at){
+                return isDomainEmail(getDomain(email, i + 1));
+            }
+        }
+        return false;
+    }
+
+    private static String getDomain(String email, int i){
+        return email.substring(i);
+    }
+
+    private static Boolean isDomainEmail(String emailDomain){
+        String domain = "email.com";
+        return emailDomain.equals(domain);
+    }
 }
