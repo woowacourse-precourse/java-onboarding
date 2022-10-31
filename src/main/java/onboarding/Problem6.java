110a504fc00fb7afc92f8ee4f266fbe33b2ff2c5
Hongsugyeong <olivia258@naver.com>
2022-10-31 12:47:40 +0900

    [Problem 6] 닉네임 중복 체크 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,4 +12,18 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    /**
+     * 닉네임 중복 체크 함수
+     */
+    static boolean check(String name, String name2){
+        for(int i=1;i<name.length();i++){
+            for(int j=1;j<name2.length();j++){
+                if(name.substring(i-1,i+1).equals(name2.substring(j-1,j+1))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
