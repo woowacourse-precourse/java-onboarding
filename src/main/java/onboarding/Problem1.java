56d64b6ade986b7ff7d3ae48334355dc726e7074
KimSunU <kgw6147@naver.com>
2022-11-01 00:22:05 +0900

    Problem1 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,9 +10,14 @@ class Problem1 {
         int c0 = crong.get(0);
         int c1 = crong.get(1);
 
+        //페이지가 연속된 번호가 아닌경우
         if(p0 + 1 != p1 || c0 +1 != c1)
             return -1;
-        int ppl0 =0, pmul0 =1, ppl1=0, pmul1=1 ,cpl0 =0, cmul0 =1, cpl1=0, cmul1=1;
+        //페이지가 왼쪽 오른쪽이 정확하지 않은경우
+        if(p0 %2 ==0 || p1 %2 ==1 || c0 %2 ==0 || c1 %2 ==1)
+            return -1;
+        int ppl0 =0, pmul0 =1, ppl1=0, pmul1=1,
+                cpl0 =0, cmul0 =1, cpl1=0, cmul1=1;
         int pmax =0, cmax =0;
 
         // Pobi의 최대값 구하기
@@ -44,6 +49,8 @@ class Problem1 {
         }
         cmax = Math.max(cmax, cpl1);
         cmax = Math.max(cmax, cmul1);
+
+        // 게임의 결과 확인
         if(pmax > cmax) answer = 1;
         else if(cmax > pmax) answer = 2;
         else answer = 0;
