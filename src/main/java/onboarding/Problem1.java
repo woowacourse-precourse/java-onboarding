039008914bddde8cda94ae2cab3ff5ed473b5567
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2022-11-01 01:11:54 +0900

    첫업데이트

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,51 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight =crong.get(1);
+        int answer = 0;
+
+        if(getMax(pobiLeft) > getMax(crongLeft)){
+            if(getMax(pobiRight) > getMax(crongRight)){answer = 1;}
+            else{answer = -1;}
+        }else if (getMax(pobiLeft) == getMax(crongLeft)){
+            if(getMax(pobiRight)==getMax(crongRight)){answer =0;}
+            else {answer = -1;}
+        }else{
+            if (getMax(pobiRight)<getMax(crongRight)){answer = 2;}
+            else {answer = -1;}
+        }
+
         return answer;
     }
+
+    static int getMax(Integer a){
+        int mul = 1;
+        int sum = 0;
+        List<Integer> arrNum = new ArrayList<>();
+        while(a > 0){
+            arrNum.add(a % 10);
+            a /= 10;
+        }
+        for(int num : arrNum){
+            mul *= num;
+        }
+
+        for(int num : arrNum){
+            sum += num;
+        }
+
+        if(mul >= sum){return mul;}
+        else {return sum;}
+
+    }
+
+
+
 }
