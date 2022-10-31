1c50af5e45f50eaad793525f4f4d9c0a14e78f02
eugene1204 <s1061001@naver.com>
2022-10-31 16:45:01 +0900

    p6, 1. solution에서 List<List<String>>을 map으로 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,9 +8,12 @@ public class Problem6 {
     // 3. 중복된 값 찾고 정렬해서 return 하는 메소드
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    
+        Map<String,String> mapFroms = new LinkedHashMap<>();
+
+        for (int i =0; i<forms.size(); i++){
+            mapFroms.put(forms.get(i).get(0), forms.get(i).get(1));
+        }
+
     }
 
 }
