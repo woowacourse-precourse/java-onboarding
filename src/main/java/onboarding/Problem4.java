be0bac8e2b1e852bfd455ff0be3b4ee998a8fd07
jsw6701 <anes53027@gmail.com>
2022-11-01 05:51:59 +0900

    (MODIFY)Problem4 Add method remark

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,15 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < word.length(); i++) {
             if(word.charAt(i) != ' '){
-                int charValue = (int)word.charAt(i);
-                if(charValue <= 90){
-                    char ch = (char)(90 - (charValue - 65));
+                int charValue = word.charAt(i);
+
+                if(charValue <= 'Z' && charValue >= 'A'){ //대문자 A~Z일 경우
+                    char ch = (char)('Z' - (charValue - 'A'));
                     sb.append(ch);
                 }
-                else{
-                    char ch = (char)(122 - (charValue - 97));
+
+                else if(charValue <= 'z' && charValue >= 'a'){ //소문자 a~z일 경우
+                    char ch = (char)('z' - (charValue - 'a'));
                     sb.append(ch);
                 }
             }
