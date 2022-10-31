f0316c6efbedbb3d092e49ab80e3502c4299c321
p <gustjr2332@naver.com>
2022-11-01 02:07:53 +0900

    feat(Problem4): solution() word에서 한 단어씩 사전에 따라 뒤집어서 answer에 저장 후 return

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,10 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
+        for(int i=0; i<word.length(); i++){
+            answer += reverseLetter(word.charAt(i));
+        }
+
         return answer;
     }
 
