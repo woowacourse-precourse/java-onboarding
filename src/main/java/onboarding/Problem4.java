e1e57c65f9e1827029ce2278337a87b9434cb9ae
xi_jjun <rlawowns97@naver.com>
2022-10-31 15:32:43 +0900

    feat(Problem4): 문자 하나를 반대로 바꿔주는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int TOTAL_ALPHABET_CNT = 25;
+    private static final char LOWERCASE_BASE_CHAR = 'a';
+    private static final char UPPERCASE_BASE_CHAR = 'A';
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static char convertCharacterToOpposite(char letter) {
+        if (Character.isLowerCase(letter)) {
+            int letterSeqOfAlphabet = letter - LOWERCASE_BASE_CHAR;
+            int opposedLetterSeq = TOTAL_ALPHABET_CNT - letterSeqOfAlphabet;
+
+            return (char) (opposedLetterSeq + LOWERCASE_BASE_CHAR);
+        } else if (Character.isUpperCase(letter)) {
+            int letterSeqOfAlphabet = letter - UPPERCASE_BASE_CHAR;
+            int opposedLetterSeq = TOTAL_ALPHABET_CNT - letterSeqOfAlphabet;
+
+            return (char) (opposedLetterSeq + UPPERCASE_BASE_CHAR);
+        }
+
+        return letter;
     }
 }
