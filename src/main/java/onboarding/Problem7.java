d346bc7c9c40f15b792fe563115b7d4f97b78d23
youhobin <ghdb132@naver.com>
2022-10-31 20:44:04 +0900

    feat(onboarding) : 위의 사용자와 친구 관계이며 user가 아닌 사용자를 구하고, 이 사용자의 점수를 0으로 하여 map에 데이터를 넣는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,13 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     static List<String> userFriends = new ArrayList<>();
+    static HashMap<String, Integer> recommendedFriends = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         for (int i = 0; i < friends.size(); i++) {
             List<String> friendsList = friends.get(i);
@@ -13,9 +16,32 @@ public class Problem7 {
         }
         System.out.println(userFriends);
 
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> friendsList = friends.get(i);
+            findRecommendedFriend(friendsList, user);
+        }
+        System.out.println(recommendedFriends);
 
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void findRecommendedFriend(List<String> friendsList, String user) {
+        for (int i = 0; i < userFriends.size(); i++) {
+            if (friendsList.contains(userFriends.get(i)) && !friendsList.contains(user)) {
+                addRecommendedFriend(friendsList, userFriends.get(i));
+            }
+        }
+    }
+
+    private static void addRecommendedFriend(List<String> friendsList, String userFriend) {
+        if (friendsList.indexOf(userFriend) == 0) {
+            recommendedFriends.put(friendsList.get(1), 0);
+        }
+
+        if (friendsList.indexOf(userFriend) == 1) {
+            recommendedFriends.put(friendsList.get(0), 0);
+        }
     }
 
     private static void findUserFriend(List<String> friendsList, String user) {
