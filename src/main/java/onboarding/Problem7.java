62f71331172538dfea3bbecf84eef039b1f6932d
kariskan <zkfzpf56@naver.com>
2022-10-31 14:09:37 +0900

    style: 필요 없는 구문 삭제

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,7 +3,6 @@ package onboarding;
 import onboarding.support.problem7.RecommendationService;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
