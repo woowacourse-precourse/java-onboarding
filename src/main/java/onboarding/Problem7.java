bba1f7774301703c456cd12a3d94cf6fea88abea
Kijung Park <kjpark4321@naver.com>
2022-11-01 01:12:56 +0900

    7번: 침구 관계 그래프 생성해주는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        makeFriendsGraph(friends);
         return answer;
+    }
+
+    private static Map<String, List<String>> makeFriendsGraph(List<List<String>> friends){
+        Map<String, List<String>> friendsMap = new HashMap<>();
+
+        for (List<String> relation : friends){
+            List<String> relationship = friendsMap.getOrDefault(relation.get(0), new ArrayList<>());
+            relationship.add(relation.get(1));
+            friendsMap.put(relation.get(0),relationship);
+            relationship = friendsMap.getOrDefault(relation.get(1), new ArrayList<>());
+            relationship.add(relation.get(0));
+            friendsMap.put(relation.get(1),relationship);
+        }
+        
+        return friendsMap;
     }
 }
