29cba38f67c27560b59e733fb27dc4756bab5666
p <gustjr2332@naver.com>
2022-11-01 02:06:14 +0900

    feat(Problem4): reverseLetter() 영문자가 아닌 경우 그대로 반환, 영문자인 경우 대소문자 구분 후 사전에 따라 뒤집은 값 반환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
+    }
+
+    public static char reverseLetter(char letter){
+        char result=' ';
+        String lowcaseword = "abcdefghijklmnopqrstuvwxyz";
+        String uppercaseword = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        if(!Character.isAlphabetic(letter)){
+            return letter;
+        }
+        else if(Character.isLowerCase(letter)){
+            int index = lowcaseword.indexOf(letter);
+            result = lowcaseword.charAt( (lowcaseword.length()-1) - index );
+        }
+        else if (Character.isUpperCase(letter)) {
+            int index = uppercaseword.indexOf(letter);
+            result = uppercaseword.charAt( (uppercaseword.length()-1) - index );
+        }
+
+        return result;
     }
 }
