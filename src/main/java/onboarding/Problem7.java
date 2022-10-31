05ed8e41d223d9a0db0a1f46f84e0cc45fb1fcc3
syr9707 <yrshin9771@gmail.com>
2022-10-31 19:55:01 +0900

    feat(PROBLEM7): 7번 문제 풀이

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        //List<String> answer = Collections.emptyList();
+
+        // user의 친구 리스트
+        ArrayList<String> friendsList = friend(user, friends);
+
+        // user의 친구의 친구 리스트
+        ArrayList<String> people = people(user, friends, friendsList);
+
+        // user와 함께 아는 친구의 수 카운트
+        HashMap<String, Integer> count = count(people, friends);
+
+        // 추천 친구
+        for(int i = 0; i < visitors.size(); i++) {
+            if(friendsList.contains(visitors.get(i))) continue;
+
+            if(count.get(visitors.get(i)) == null) count.put(visitors.get(i), 1);
+            else count.put(visitors.get(i), count.get(visitors.get(i)) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(count.entrySet());
+        entryList.sort(((o1, o2) -> count.get(o2.getKey()) - count.get(o1.getKey())));
+
+        List<String> answer = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry : entryList){
+            answer.add(entry.getKey());
+        }
+
         return answer;
     }
 
