72fe0808db7d6f492658be20fee1885ab25f25df
jmjmjames <chungjm0711@gmail.com>
2022-11-01 08:44:26 +0900

    refactor: 변경되지 않는 변수 final 키워드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-        int[] currencies = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+        final List<Integer> answer = new ArrayList<>();
+        final int[] currencies = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
 
         for (int currency : currencies) {
             answer.add(money / currency);
