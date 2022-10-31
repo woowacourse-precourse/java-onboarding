db6d17a56ce4f2684fc52be43d4869a7e9dccd67
baekeunsun <eunsun5460@gmail.com>
2022-10-31 13:10:57 +0900

    [문제2] feats: 중복 문자 탐색 기능 추가, input예외 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,14 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         if(CheckCryptogram(cryptogram)){
-
+            return CheckDuplicates(cryptogram).toString();
         }
-        throw new IllegalArgumentException();
+        return "0";
+        //throw new IllegalArgumentException();
     }
 
+    /* 기능1 : input 예외 확인 */
     private static boolean CheckCryptogram(String str){
         if(isLower(str) && isInRange(str) )return true;
         return false;
@@ -18,8 +23,9 @@ public class Problem2 {
         char[] charArray = str.toCharArray();
 
         for(int i = 0; i < charArray.length; i++){
-            if( !Character.isUpperCase(charArray[i]))
+            if( !(Character.isLowerCase(charArray[i]))){
                 return false;
+            }
         }
         return true;
     }
@@ -27,5 +33,19 @@ public class Problem2 {
     private static boolean isInRange(String str){
         if(str.length() > 0 && str.length() < 1001) return true;
         return false;
+    }
+
+    /* 기능2 : 중복 문자 탐색 */
+    private static List<Integer> CheckDuplicates(String str){
+        char tmp = '0';
+        List<Integer> duplicatelist = new ArrayList<>();
+        for(int i=0; i<str.length(); i++){
+            if(tmp == str.charAt(i)){
+                duplicatelist.add(i-1);
+                duplicatelist.add(i);
+            }
+            tmp = str.charAt(i);
+        }
+        return duplicatelist;
     }
 }
