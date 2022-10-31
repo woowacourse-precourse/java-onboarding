d06b3a08541b92ddf24e4a0b6344e1ec1f5ac773
nahyeon cho <whskgus12@nate.com>
2022-11-01 03:14:43 +0900

    Problem 5 - 큰 화폐 단위부터 할당하기
    입력된 금액에 대해, 큰 화폐 단위부터 필요한 개수를 할당하였습니다. Test까지 정상적으로 마쳤으므로, Problem5 기능 구현 최종 커밋 합니다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static List<Integer> resultMoney;
     public static List<Integer> solution(int money) {
+        resultMoney = new ArrayList<>(); // static 배열 초기화
         if(isValid(money)){
-            List<Integer> answer = Collections.emptyList();
-            return answer;
+           countMoney(money);
+           return resultMoney;
         }else
             return null;
+    }
+
+    public static void countMoney(int money) {
+        // 큰 화폐부터 금액 할당하기
+        Integer[] moneyList = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
+        for (int m : moneyList) {
+            resultMoney.add(money / m);
+            money %= m;
+        }
+
     }
 
     static boolean isValid(int money) {
