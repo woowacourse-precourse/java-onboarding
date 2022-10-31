0aca6d2ec031c37ab0141f974d51d72b357dc1dc
YuHayeon <youha0930@gmail.com>
2022-10-31 14:21:03 +0900

    feat(Problem7): 점수순 -> 이름순 으로 정렬

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,8 +2,11 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 
@@ -24,6 +27,26 @@ public class Problem7 {
 
 		List<String> friendsOfUser = friendsRelations.get(user);
 		HashMap<String, Integer> totalScoresExcludingUserFriends = removeUsers(user, friendsOfUser, totalScores);
+
+		List<Map.Entry<String, Integer>> sortedTotalScores = sortHashMap(totalScoresExcludingUserFriends);
+	}
+
+	public static List<Map.Entry<String, Integer>> sortHashMap(HashMap<String, Integer> totalScores) {
+		List<Map.Entry<String, Integer>> totalScoreList = totalScores
+			.entrySet()
+			.stream()
+			.collect(Collectors.toList());
+
+		Collections.sort(totalScoreList, new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				if (o1.getValue() == o2.getValue()) {
+					return (o1.getKey().compareTo(o2.getKey()));
+				}
+				return o2.getValue() - o1.getValue();
+			}
+		});
+		return totalScoreList;
 	}
 
 	public static HashMap<String, Integer> removeUsers(String user, List<String> friendsOfUser,
