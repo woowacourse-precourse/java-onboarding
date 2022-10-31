a5bf5fbb2f7370575b41c019ceb7bc99f5ccc852
wltks98 <wltks98@hanmail.net>
2022-10-31 09:35:01 +0900

    [문제4] refactor : 생성자의 로직을 새로운 메소드로 분리

--- src/main/java/onboarding/problemfour/domain/Frog.java
+++ src/main/java/onboarding/problemfour/domain/Frog.java
@@ -7,12 +7,14 @@ public class Frog {
 	private HashMap<Character, Character> dictionary = new HashMap<>();
 
 	public Frog() {
+		makeDictionary();
+	}
 
+	private void makeDictionary() {
 		for (int i = 0; i < 26; i++) {
 			dictionary.put((char)('A' + i), (char)('Z' - i));
 			dictionary.put((char)('a' + i), (char)('z' - i));
 		}
-
 	}
 
 	public String convert(String word) {
