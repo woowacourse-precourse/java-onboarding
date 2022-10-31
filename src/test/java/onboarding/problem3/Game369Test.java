95ec4756432086aab2a91b1dd5f3ff25dfe90baf
hodomaroo <enghemdwkd@gmail.com>
2022-10-31 20:33:56 +0900

    [Problem3/TEST] 현재까지 구현한checkNumberIs369, getClapTimeOfNumber 메서드의 테스트 코드를 구현하고, 검증을 완료하였습니다.

---
+++ src/test/java/onboarding/problem3/Game369Test.java
@@ -0,0 +1,34 @@
+package onboarding.problem3;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Game369Test {
+    @Test
+    @DisplayName("CheckNumberIs 3 6 9")
+    void validateCheckNumberIs369(){
+        Assertions.assertThat(Game369.checkNumberIs369(0)).isEqualTo(false);
+        Assertions.assertThat(Game369.checkNumberIs369(1)).isEqualTo(false);
+        Assertions.assertThat(Game369.checkNumberIs369(2)).isEqualTo(false);
+        Assertions.assertThat(Game369.checkNumberIs369(3)).isEqualTo(true);
+        Assertions.assertThat(Game369.checkNumberIs369(4)).isEqualTo(false);
+        Assertions.assertThat(Game369.checkNumberIs369(5)).isEqualTo(false);
+        Assertions.assertThat(Game369.checkNumberIs369(6)).isEqualTo(true);
+        Assertions.assertThat(Game369.checkNumberIs369(7)).isEqualTo(false);
+        Assertions.assertThat(Game369.checkNumberIs369(8)).isEqualTo(false);
+        Assertions.assertThat(Game369.checkNumberIs369(9)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("get clap counts of a number")
+    void validateGetClapTimeOfNumber(){
+        Assertions.assertThat(Game369.getClapTimeOfNumber(1255)).isEqualTo(0);
+        Assertions.assertThat(Game369.getClapTimeOfNumber(665)).isEqualTo(2);
+        Assertions.assertThat(Game369.getClapTimeOfNumber(155)).isEqualTo(0);
+        Assertions.assertThat(Game369.getClapTimeOfNumber(305)).isEqualTo(1);
+        Assertions.assertThat(Game369.getClapTimeOfNumber(240)).isEqualTo(0);
+        Assertions.assertThat(Game369.getClapTimeOfNumber(145255)).isEqualTo(0);
+        Assertions.assertThat(Game369.getClapTimeOfNumber(9999)).isEqualTo(4);
+    }
+}
