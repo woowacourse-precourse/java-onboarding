fa3b76d7ebcba301879d68d0384db4c70dd36b23
Soseung Soo <sss9073@naver.com>
2022-11-01 04:11:07 +0900

    feat: 'findFriend' function

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,21 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static ArrayList<String> findFriend(String user, List<List<String>> friends){
+        ArrayList<String> userFriend = new ArrayList<>();
+        for(int i = 0; i < friends.size(); i++){
+            String nameA = friends.get(i).get(0);
+            String nameB = friends.get(i).get(1);
+
+            if(nameA.equals(user)){
+                userFriend.add(nameB);
+            }
+            if(nameB.equals(user)){
+                userFriend.add(nameA);
+            }
+        }
+        return userFriend;
     }
 }
