4f2b817d84cf16988f8716b7cb6c9e57c7af1832
wormjoo <wormjoo@naver.com>
2022-10-31 23:36:13 +0900

    feat: problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int num = 0;
+        char[] chars = word.toCharArray();
+
+        for (char c : chars) {
+            num = (int) c;
+            if (Character.isUpperCase(c)) {
+                num = 155 - num;
+            } else if (Character.isLowerCase(c)) {
+                num = 219 - num;
+            }
+            answer += (char) num;
+        }
+
         return answer;
     }
 }
