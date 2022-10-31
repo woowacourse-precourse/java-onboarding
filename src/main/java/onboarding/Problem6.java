1f1315a468cbb7caa9f9f05ca0bd72f69054098b
sen <seeunchoi99@gmail.com>
2022-10-31 23:36:52 +0900

    ✨ Problem6 각 닉네임이 중복인지 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,8 +8,7 @@ import java.util.List;
  * 기능 목록
  * 1. 중복될 수 있는 2글자 단어들 모으기
  * 2. 1번으로 해당 닉네임이 중복인지 검사
- * 2. result에서 이메일 오름차순으로 정렬
- * 3. result에서 이메일 중복 제거
+ * 3. result에서 이메일 오름차순으로 정렬
  */
 
 public class Problem6 {
@@ -20,6 +19,14 @@ public class Problem6 {
         for (List<String> form : forms) {
             for (int i = 0; i < form.get(1).length() - 1; i++) {
                 duplications.put(form.get(1).substring(i, i + 2), Boolean.TRUE);
+            }
+        }
+
+        for (List<String> form : forms) {
+            for (int i = 0; i < form.get(1).length() - 1; i++) {
+                if (duplications.containsKey(form.get(1).substring(i, i + 2))) {
+                    answer.add(form.get(0));
+                }
             }
         }
 
