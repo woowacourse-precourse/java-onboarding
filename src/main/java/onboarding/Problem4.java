cb2dc4f8da9ab965e1fb6731911539cef17eeeb5
이지영 <wldudwldhd@naver.com>
2022-11-01 00:43:41 +0900

    docs:기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+    /*기능구현
+    조건 : 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
+          A - Z, B - Y 과 같은 조합은 아스키코드(정수형 숫자)의 계산으로 알 수 있다.
+    1. word의 특정 char값의 reverse값이 무엇인지 반환하는 함수를 구현
+    * */
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
