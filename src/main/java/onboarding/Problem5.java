afd620deb5f1b99ed2e06147f0e38944de3dd35f
JungWoo <rlawjddn103@naver.com>
2022-10-31 16:07:51 +0900

    feat: Add Problem5 calculateAnswer function and return answer

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        return calculateAnswer(money);
+    }
+
+    public static List<Integer> calculateAnswer(int money){
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyList = new int[]{50000,10000,5000,1000,500,100,50,10,1};
+
+        for (int m : moneyList) {
+            if (money >= m) {
+                answer.add(money / m);
+                money -= m * (money / m);
+            } else {
+                answer.add(0);
+            }
+        }
+
         return answer;
     }
 }
