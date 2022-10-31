bc503cf6fbeb6ddf9ce5cf83758c585dadaa8055
민병욱 <woook27@naver.com>
2022-11-01 01:54:39 +0900

    feat: getScore 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,30 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Math.max;
+
 class Problem1 {
+    public static int getScore(List<Integer> player) {
+        int score = 0;
+        for (int i = 0; i < player.size(); i++) {
+            String page = String.valueOf(player.get(i));
+            String[] digits = page.split("");
+
+            int mul = 1, sum = 0;
+            for (int j = 0; j < digits.length; j++) {
+                sum += Integer.valueOf(digits[i]);
+                mul *= Integer.valueOf(digits[i]);
+            }
+
+            score = max(score, sum);
+            score = max(score, mul);
+        }
+        return score;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+    
         return answer;
     }
 }
