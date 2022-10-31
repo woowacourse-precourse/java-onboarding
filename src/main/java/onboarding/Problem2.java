48dcc8728cae49ba6852b95c29c0a62f9526587b
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-10-31 16:53:37 +0900

    Docs(Problem2): 추가 기능 목록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+
+
+/*
+기능 목록
+1. 중복 유무 확인
+2. 중복 없을때까지 중복 제거
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
