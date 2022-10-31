2f48c541c2d2d9efb5302efe9795cce4aad9ac62
Soseung Soo <sss9073@naver.com>
2022-11-01 03:33:39 +0900

    feat: 'solution' function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = getAnswer(number);
         return answer;
     }
 
