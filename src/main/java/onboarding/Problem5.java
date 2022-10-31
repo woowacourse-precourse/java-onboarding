7e1c7ff434f193cdf00005893f2af1f8ea159565
Amenable-C <duswo0624@naver.com>
2022-11-01 00:41:51 +0900

    feat : [문제5] money를 화폐 단위별로 변환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        Integer[] money_unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < 9; i++){
+            Integer cnt = money / money_unit[i];
+            answer.add(cnt);
+            money %= money_unit[i];
+        }
+
         return answer;
     }
 }
