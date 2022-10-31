b5acc94487717f2a4bd1bb52eb0a5e383c2455df
zeunala <zeunala@naver.com>
2022-11-01 02:28:34 +0900

    feat: 문제 3 숫자 하나에 대한 손뼉 횟수 계산 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+    public static int countClap(int number) {
+        int result = 0;
+
+        while (number > 0) {
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                result++;
+            }
+            number /= 10;
+        }
+
+        return result;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
