f13e3ed0cca3d7bfb66c2d5a374eb299aa1a26ea
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:36:43 +0900

    [problem2]cryptogram을 하나씩 받아와 저장하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidationException;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer = "";
+        int count = 1;
+
+        List<Character> cut = new ArrayList<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            cut.add(cryptogram.charAt(i));
+        }
+
+
         return answer;
     }
+
 }
+
+
+
