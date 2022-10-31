9065d351647fd2b2bd86029209670f274d8422e2
Eleisia <blackcube222@gmail.com>
2022-10-31 21:20:04 +0900

    7번문제 기능목록작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+/*
+* 기능목록
+*
+* 1. 사용자와 친구인 사람 목록 작성
+* 2. 친구와 아는사람 점수부여 기능
+* 3. 정렬기능
+* */
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
