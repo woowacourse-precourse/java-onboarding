24f71aadbc0bd4558a939ba920029396a78a37b6
이정우 <acg6138@naver.com>
2022-11-01 01:13:47 +0900

    feat(Problem6): 잘라낸 두 글자를 모든 유저 닉네임과 비교하여 포함되어 있는지 확인하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
+    static HashSet<String> result = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static void checkContainsSerialChar(String serialChar, int currentUserIdx, List<List<String>> forms) {
+        for (int i = currentUserIdx+1; i < forms.size(); i++) {
+            if (forms.get(i).get(1).contains(serialChar)) {
+                result.add(forms.get(currentUserIdx).get(0));
+                result.add(forms.get(i).get(0));
+            }
+        }
     }
 }
