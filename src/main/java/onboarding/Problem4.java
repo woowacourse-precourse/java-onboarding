974d863c71cdfe92c2cee56d8de8f3c850d57146
OhHongGi <5ghdrl@naver.com>
2022-11-01 02:20:05 +0900

    chore: Implement convertWord method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,6 @@ import java.util.Stack;
 public class Problem4 {
 
     public static String solution(String word) {
-        String dictionary = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
 
         String answer = "";
 
@@ -15,7 +14,7 @@ public class Problem4 {
     /*
      * 주어진 문자열에 대해 사전을 통해 변환된 문장 전체 반환
      */
-    public static String convertText(String text, String dictionary) {
+    public static String convertText(String text) {
         String result = "";
         return result;
     }
@@ -23,8 +22,24 @@ public class Problem4 {
     /*
      * 주어진 단어에 대해 사전을 통해 변환된 단어 반환
      */
-    public static String convertWord(String word, String dictionary) {
-        return "";
+    public static String convertWord(String word) {
+        String dictionaryUpperCase = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+        String dictionaryLowerCase = "zyxwvutsrqponmlkjihgfedcba";
+        StringBuilder builder = new StringBuilder();
+
+        for (char c : word.toCharArray()) {
+            int charToAscii = c;
+            if ((97 <= charToAscii) && (charToAscii <= 122)) {
+                char convertedCharacter = dictionaryLowerCase.charAt(charToAscii - ((int) 'a'));
+                builder.append(convertedCharacter);
+            } else if ((65 <= charToAscii) && (charToAscii <= 90)) {
+                char convertedCharacter = dictionaryUpperCase.charAt(charToAscii - ((int) 'A'));
+                builder.append(convertedCharacter);
+            } else if ((charToAscii < 97) || (charToAscii > 122)) {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
     }
 
 }
