2bfaf1d6ae4cc0f8ff36ed5c440106137cb485cc
michaelp3 <chan-park@hotmail.com>
2022-11-01 00:35:56 +0900

    Problem7 Solved

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,72 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
+
+        HashMap<String, List<String>> friendMap = new HashMap<>();
+        TreeMap<String, Integer> friendScore = new TreeMap<>();
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+
+            List<String> list1 = friendMap.getOrDefault(friend1, new ArrayList<>());
+            List<String> list2 = friendMap.getOrDefault(friend2, new ArrayList<>());
+
+            list1.add(friend2);
+            list2.add(friend1);
+
+            friendMap.put(friend1, list1);
+            friendMap.put(friend2, list2);
+        }
+
+        for (String visitor :
+            visitors) {
+            if (!friendMap.get(user).contains(visitor) && !visitor.equals(user)) {
+                friendScore.put(visitor, friendScore.getOrDefault(visitor, 0) + 1);
+            }
+        }
+
+        for (String friend : friendMap.get(user)) {
+            List<String> mutual = friendMap.get(friend);
+            for (String m :
+                mutual) {
+                if (!friendMap.get(user).contains(m) && !m.equals(user)) {
+                    friendScore.put(m, friendScore.getOrDefault(m, 0) + 10);
+                }
+
+            }
+        }
+        int cnt = 0;
+        List<String> answer = new LinkedList<>();
+
+
+
+        while (!friendScore.isEmpty() && cnt < 5) {
+            int max = Integer.MIN_VALUE;
+            String maxFriend = "";
+            for (Entry<String, Integer> friend :
+                friendScore.entrySet()) {
+                String name = friend.getKey();
+                int score = friend.getValue();
+                if (score > max) {
+                    max = score;
+                    maxFriend = name;
+                }
+            }
+            answer.add(maxFriend);
+            friendScore.remove(maxFriend);
+            cnt++;
+        }
         return answer;
     }
 }
