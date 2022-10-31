f4f9b01ef009bcad5fd95f67ecf09e06c835756a
권민정 <22alswjd@naver.com>
2022-11-01 02:05:59 +0900

    feat: Problem4 기능 '2. i번째 문자가 대문자인 경우' 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,8 +8,13 @@ public class Problem4 {
         // 1. 변환된 i번째 문자를 저장할 character 변수 선언
         for (int i = 0; i < size; i++) {
             char character = word.charAt(i);
+            char replacedCharacter;
+
+            // 2. i번째 문자가 대문자인 경우
+            if(character >= 65 && character <= 90)
+                replacedCharacter = (char) (90 + 65 - character);
+
         }
-        // 2. i번째 문자가 대문자인 경우
         // 3. i번째 문자가 소문자인 경우
         // 4. i번째 문자가 알파벳이 아닌 경우
         // 5. answer에 변환된 문자 추가
