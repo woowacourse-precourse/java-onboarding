7966e644f4efc211a9e9d7ef43646e6abaa773a9
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-31 20:45:42 +0900

    Problem7 기능요구사항 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    // 점수가 높은 순으로 5명을 리턴한다.
+    // 점수가 같으면 이름순으로 정렬한다.
+    // 0점이면 리턴하지 않는다
+    // 함께아는 친구: 10점
+    // 타임라인에 방문한 횟수: 1점
+
+    // TODO friends를 돌며 유저들의 닉네임을 2차원 배열에 담는다. -> 중복을 제거한다
+    // TODO freinds의 요소중 user가 들어있는 리스트를 뽑는다.
+    // TODO 뽑은 요소들에서 user와 친구인 유저를 찾는다.
+    // TODO 찾은 친구의 리스트에 담겨있는 유저에게 +10
+    // TODO visitors를 돌며 2차원배열에 +1
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
