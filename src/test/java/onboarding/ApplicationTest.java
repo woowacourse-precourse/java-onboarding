dbfd9416b1fe2d11193618df2b9b964e36ecc52b
jin-chang-ho <ckdgh8204@naver.com>
2022-10-31 19:33:14 +0900

    Add_Problem4

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -128,6 +128,13 @@ class ApplicationTest {
             String result = "R olev blf";
             assertThat(Problem4.solution(word)).isEqualTo(result);
         }
+
+        @Test
+        void case2() {
+            String word = "Hello  Everyone!";
+            String result = "Svool  Veviblmv!";
+            assertThat(Problem4.solution(word)).isEqualTo(result);
+        }
     }
 
     @Nested
