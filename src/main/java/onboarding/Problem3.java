a57158fae8b4498c25352e5e0085a4bfc9554ea5
Soseung Soo <sss9073@naver.com>
2022-11-01 03:32:17 +0900

    feat: 'calculate' function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int calculate(int number){
+        int sum = 0;
+        while(number != 0){
+            if(number % 10 == 3 || number % 10 == 6 || number % 10 == 9){
+                sum++;
+            }
+            number /= 10;
+        }
+        return sum;
+    }
 }
