8d55bd6ce7784e52bb0209042fe3ed967cf9a85a
sugamypapa <sk000801@naver.com>
2022-10-31 20:33:50 +0900

    refactor: 문제 6 코드 컨벤션 및 아이디어 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,9 +4,13 @@ import java.util.*;
 
 public class Problem6 {
     // 처음 아이디어
-    // 각 닉네임마다 두글자씩 잘라서 배열에 저장하는 함수 생성
-    // 두 배열씩 비교하여 겹치는 값이 있는지 확인하는 함수 생성
-    // 겹치는 배열이 존재하는 아이디만 정렬 후 반환
+    // 닉네임을 두글자씩 겹치게 자른 문자열이 모두 저장된 리스트를 반환하는 함수 생성 - (1)
+    // 모든 크루들의 (1)배열을 저장한 List<List<String>> 리스트 반환하는 함수 생성
+    // 두 배열끼리 비교하여 겹치는 값의 존재 여부를 반환하는 함수 생성
+    // 이중 for문을 활용해 존재하는 모든 크루들의 닉네임이 겹치는지 여부를 확인하고, 그 크루들의 이메일만 담은 배열을 반환하는 함수 생성
+    // -> 위의 존재여부 함수를 활용해 반환값이 true면 두 크루의 이메일을 결과 배열에 넣어준다
+    // -> ex.form.get(i).get(0) 이 들어가는 이유는 i번째 인덱스(한 크루의 [이메일,닉네임]을 담은 배열)의 0번째 인덱스 값인 이메일을 담기 위해
+    // 겹치는 배열이 존재하는 아이디만 정렬(리스트이므로 Collections.sort()활용) 후 반환
 
     public static List<String> nameDivideTwo(String s) {
         List<String> list = new ArrayList<>();
@@ -15,6 +19,15 @@ public class Problem6 {
         }
         return list;
     }
+
+    public static List<List<String>> splitCrewName(List<List<String>> forms) {
+        List<List<String>> splitCrewName = new ArrayList<>();
+        for (List<String> form : forms) {
+            splitCrewName.add(nameDivideTwo(form.get(1))); //이름을 받아와 쪼갬
+        }
+        return splitCrewName;
+    }
+
     public static boolean hasCommon(List<String> one, List<String> two) {
         boolean hasCommon = false;
         for (String s_one : one) {
@@ -27,7 +40,8 @@ public class Problem6 {
         }
         return hasCommon;
     }
-    public static List<String> finalNameList(List<List<String>> nameSplit, List<List<String>> forms) {
+
+    public static List<String> commonNameList(List<List<String>> nameSplit, List<List<String>> forms) {
         // 쪼개진 이름 리스트들과 문제에서 주어진 조건 리스트를 인자로 받는
         List<String> emailList = new ArrayList<>();
         for (int i = 0; i < nameSplit.size()-2; i++) {
@@ -43,12 +57,9 @@ public class Problem6 {
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        List<List<String>> nameSplit = new ArrayList<>();
-        for (List<String> form : forms) {
-            nameSplit.add(nameDivideTwo(form.get(1))); //이름을 받아와 쪼갬
-        }
-        List<String> emailList = finalNameList(nameSplit, forms);
-        Collections.sort(emailList);
-        return emailList;
+        List<List<String>> nameSplit = splitCrewName(forms);
+        List<String> commonEmailList = commonNameList(nameSplit, forms);
+        Collections.sort(commonEmailList);
+        return commonEmailList;
     }
 }
