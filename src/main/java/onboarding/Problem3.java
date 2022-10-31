29d7455cb2003a59dd016961ecfd36834543864e
hojunking96 <junsong96@naver.com>
2022-11-01 01:05:54 +0900

    P3 feat 본문 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,8 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++)
+            answer += countClaps(i);
         return answer;
     }
     public static int countClaps(int number) {
