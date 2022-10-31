fce478ad8cf67f5359dc63ed8d972ba6ebd08e16
lacram <chemin9898@gmail.com>
2022-11-01 00:44:59 +0900

    Problem 7 - 기능 목록 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+* 기능 목록
+* 1. 친구 관계 저장
+* 2. 함께 아는 친구 점수 계산
+* 3. 타임라인에 방문한 횟수 점수 계산
+* 4. 점수가 높은 5명 구하기
+* */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
