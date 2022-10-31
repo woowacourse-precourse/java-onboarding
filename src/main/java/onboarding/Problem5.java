2de2ca790d0e55516067b5e301b1d4d16863eacf
ddogga <hayeonlee60327@gmail.com>
2022-10-31 22:22:12 +0900

    문제5 화폐 단위마다 개수를 구하고, 해당 금액만큼 빼주기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,16 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
 
         int[] cash = {50000,10000,5000,1000,500,100,50,10,1};
 
-
+        for (int i = 0; i < cash.length; i++) {
+            int count = money / cash[i];
+            answer.add(count);
+            money -= cash[i] * count;
+        }
 
         return answer;
+    }
+
+
+    public static void main(String[] args) {
+//        int money = 50237;
+        int money = 15000;
+        System.out.println(solution(money));
     }
 }
