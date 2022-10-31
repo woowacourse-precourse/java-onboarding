728c8a69a8f0bf34f74965da2032963fa0dece70
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-11-01 00:21:51 +0900

    refactor: Problem1 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,29 +1,19 @@
 package onboarding;
 
-import onboarding.problem1.BookPageDiscriminator;
-import onboarding.problem1.BookScoreCalculator;
+import onboarding.exception.InputException;
+import onboarding.problem1.BookScoreComparator;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Problem1 {
 
-    private static final int POBI_WIN = 1;
-    private static final int CRONG_WIN = 2;
-    private static final int DRAW = 0;
     private static final int EXCEPTIONAL_CIRCUMSTANCES = -1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-
-        if (!BookPageDiscriminator.isProperBookPage(pobi, crong)){
+        try {
+            return new BookScoreComparator().compare(pobi, crong);
+        } catch (InputException e){
             return EXCEPTIONAL_CIRCUMSTANCES;
         }
-
-        int pobiScore = BookScoreCalculator.score(pobi);
-        int crongScore = BookScoreCalculator.score(crong);
-
-        if (pobiScore > crongScore) return POBI_WIN;
-        if (pobiScore < crongScore) return CRONG_WIN;
-        return DRAW;
     }
 }
