454be699e268a014a3ee9ed1ef1ad9cc60c718fd
K-Diger <kitten.diger@gmail.com>
2022-10-31 21:02:59 +0900

    docs : Problem4 기능 목록 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/*
+1. 대문자 아스키코드 변환 Map 을 생성하는 메서드
+2. 소문자 아스키코드 변환 Map 을 생성하는 메서드
+3. 실제 입력 값과 1, 2 메서드로 변환된 Map 을 매핑하는 메서드
+4.
+
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
