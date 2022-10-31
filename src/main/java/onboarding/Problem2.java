8487c2d18e102d86faed39b24d4bd700d702d905
suhyeon <suhyeon3484@naver.com>
2022-11-01 02:27:44 +0900

    problem2 solve

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,41 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder(cryptogram);
+        int before_len = 0;
+        int count = 0;
+        char now = 'X';
+        char previous = 'S';
+
+        while (true) {
+            cryptogram = answer.toString();
+            before_len = cryptogram.length();
+            previous = 'S';
+            answer = new StringBuilder();
+            count = 0;
+            for (int i = 0; i < before_len; i++) {
+                now = cryptogram.charAt(i);
+                if (previous == 'S') {
+                    previous = now;
+                    continue;
+                }
+                if (previous == now) {
+                    count += 1;
+                }
+                else {
+                    if (count == 0)  {
+                        answer.append(previous);
+                    }
+                    if (i == before_len -1) {
+                        answer.append(now);
+                    }
+                    previous = now;
+                    count = 0;
+                }
+            }
+            if ((answer.length() == before_len) || answer.length() == 0)
+                break;
+        }
+        return answer.toString();
     }
 }
