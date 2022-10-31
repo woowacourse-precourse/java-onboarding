99b4634c3d6bc55a7f0a828084ecfd97eb049c37
kkkwp <chxrryda@gmail.com>
2022-11-01 03:35:12 +0900

    feat(makeRelation): 친구 목록을 relation에 분류하여 넣는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashSet<String> relation = new HashSet<>();
+        Map<String, Integer> score = new HashMap<>();
+
+        makeRelation(user, friends, relation);
+
+
+
+
         return answer;
+    }
+
+    public static void makeRelation(String user, List<List<String>> friends, HashSet<String> relation) {
+        for (List<String> friend : friends) {
+            String known = friend.get(0);
+            String unknown = friend.get(1);
+            if (known.equals(user)) {
+                relation.add(known);
+            }
+            if (unknown.equals(user)) {
+                relation.add(known);
+            }
+        }
     }
 }
