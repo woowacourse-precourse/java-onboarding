9321f651f8110ebb6af7010211d1e03b85c2bbb6
chener <49267211+chanosong@users.noreply.github.com>
2022-10-31 18:54:45 +0900

    [Set] Prob1 Function list

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    // func : sum each digit
+
+    // func : multiple each digit
+
+    // func : Get max number on page number
+
+    // func : Get max number result between left and right page
+
+    // func : decide answer by compare two lists
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
