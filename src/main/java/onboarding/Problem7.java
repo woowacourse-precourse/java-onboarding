c18132f4113bb7df073a1af78424346c0d843b92
DDing77 <devseo2021@gmail.com>
2022-11-01 02:06:27 +0900

    [Problem7] feat : 친구관계 HashMap 생성 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    // 친구관계 Map
+    public static HashMap<String, List<String>> friendMap;
+    // 친구 관계 Map 생성
+    public static void getFriendMap(List<List<String>> friends) {
+        friendMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            // 왼쪽 친구 <- 오른쪽 친구 관계 생성
+            friendMap.put(friend.get(0), addFriend(friend.get(0), friend.get(1)));
+            // 오른쪽 친구 <- 왼쪽 친구 관계 생성
+            friendMap.put(friend.get(1), addFriend(friend.get(1), friend.get(0)));
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
