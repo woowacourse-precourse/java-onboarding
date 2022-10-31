b4c3285981b05c86de3d3ff6952e970ef1a04004
Seph-23 <seongmlyu95@gmail.com>
2022-10-31 15:46:58 +0900

    7번 문제 구현 완료

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,79 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Problem7 {
+
+    static Map<String, Integer> friendScore = new HashMap<>();
+
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(List.of("donut", "andole"),
+                                             List.of("donut", "jun"),
+                                             List.of("donut", "mrko"),
+                                             List.of("shakevan", "andole"),
+                                             List.of("shakevan", "jun"),
+                                             List.of("shakevan", "mrko"));
+
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        System.out.println(solution(user, friends, visitors));
+
+    }
+
+    static void addScore(String name, int score) {
+        if (friendScore.containsKey(name)) {
+            score += friendScore.get(name);
+        }
+        friendScore.put(name, score);
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        List<String> userFriends = new ArrayList<>();       //유저의 친구 리스트
+        for (List<String> friend : friends) {
+            if (friend.get(0).equals(user)) {
+                userFriends.add(friend.get(1));
+            } else if (friend.get(1).equals(user)) {
+                userFriends.add(friend.get(0));
+            }
+        }
+
+        for (List<String> friend : friends) {           //친구 점수 업데이트
+            if (friend.get(0).equals(user) || friend.get(1).equals(user)) {
+                continue;
+            } else if (userFriends.contains(friend.get(0)) && !userFriends.contains(friend.get(1))) {
+                addScore(friend.get(1), 10);
+            } else if (userFriends.contains(friend.get(1)) && !userFriends.contains(friend.get(0))) {
+                addScore(friend.get(0), 10);
+            }
+        }
+
+        for (String visitor : visitors) {       //방문 점수 업데이트
+            if (userFriends.contains(visitor)) {
+                continue;
+            }
+            addScore(visitor, 1);
+        }
+
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(friendScore.entrySet());
+        entryList.sort(Map.Entry.comparingByKey());
+        entryList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        int count = 5;
+        for (Entry<String, Integer> entry : entryList) {
+            answer.add(entry.getKey());
+            count--;
+            if (count <= 0) {
+                break;
+            }
+        }
         return answer;
     }
 }
