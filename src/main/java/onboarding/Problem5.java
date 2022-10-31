0e4bf4535c585bde58425ea8c87301b80e57231d
minju412 <ln8847@ajou.ac.kr>
2022-10-31 22:08:28 +0900

    style: [p5] 변수 명 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class Problem5 {
 	public static List<Integer> solution(int money) {
 		List<Integer> answer = new ArrayList<>(Collections.emptyList());
-		int[] unit = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		int[] amountOfMoney = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-		for (int i : unit) {
+		for (int i : amountOfMoney) {
 			answer.add(money / i);
 			money %= i;
 		}
