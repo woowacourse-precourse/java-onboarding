7267b2f4aa152be767d0793a5acda75af1feabaf
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-10-31 21:52:37 +0900

    [4번] feat : "청개구리 사전 기능 적용"

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import onboarding.p4.Dictionary;
+import onboarding.p4.GreenFrogDictionary;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Dictionary dictionary = new GreenFrogDictionary();
+        return dictionary.convertString(word);
     }
 }
