b6b07bf9505bf1c07399a007e79dc87103cdc9e4
vcho1958 <gusckd57@gmail.com>
2022-11-01 00:57:14 +0900

    [problem7] reset

--- src/main/java/onboarding/problem7/SNS.java
+++ src/main/java/onboarding/problem7/SNS.java
@@ -1,19 +1,14 @@
 package onboarding.problem7;
 
+import onboarding.problem6.UserRepository;
+
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SNS {
-    UserRelationRepository userRelationRepository;
-    UserRepository userRepository;
 
-    public void setDatas(List<List<String>> friends, List<String> visitors){
-        friends.stream().flatMap(arr->arr.stream()).distinct().forEach(this.userRepository::save);
-        this.userRelationRepository.addRelations(friends);
-    }
-
-    public List<String> recommendFriend(String name){
-
-    }
 }
--- src/main/java/onboarding/problem7/User.java
+++ src/main/java/onboarding/problem7/User.java
@@ -1,15 +1,7 @@
 package onboarding.problem7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    public User(String name) {
-        this.name = name;
-    }
-
-    private String name;
-
-    private int score = 0;
-
-    public String getName() {
-        return name;
-    }
 }
