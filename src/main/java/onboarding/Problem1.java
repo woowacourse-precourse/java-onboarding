da6cc406cba7935b2dc2a14e53b35ecb891ecc44
Young37 <20171470@g.dongseo.ac.kr>
2022-10-31 20:15:51 +0900

    입력 값 예외 처리(기능 요구 사항을 만족하기 위해)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,14 +5,24 @@ import java.util.List;
 
 class Problem1 {
 	public static int solution(List<Integer> pobi, List<Integer> crong) {
-    	int answer = Integer.MAX_VALUE;
-    	List<List<Integer>> userPagesList = new ArrayList<List<Integer>>();
-    	userPagesList.add(pobi);
-    	userPagesList.add(crong);
-    	List<Integer> userMaxList = new ArrayList<Integer>();
-    	for(int i = 0; i<userPagesList.size();i++) {
-    		userMaxList.add(-1);
-    	}
-    	return answer;			
+    	try {
+	    	int answer = Integer.MAX_VALUE;
+	    	List<List<Integer>> userPagesList = new ArrayList<List<Integer>>();
+	    	userPagesList.add(pobi);
+	    	userPagesList.add(crong);
+	    	List<Integer> userMaxList = new ArrayList<Integer>();
+	    	for(int i = 0; i<userPagesList.size();i++) {
+	    		userMaxList.add(-1);
+	    	}
+		
+    		for(List<Integer> userPages : userPagesList) {
+    			if (userPages.size() != 2 || userPages.get(0) <= 2 || userPages.get(0) >= 399 || userPages.get(0) % 2 == 0 || userPages.get(0) + 1 != userPages.get(1)) {
+					throw new IllegalArgumentException();
+				}					                                                                                                                                                    
+    		}
+			return answer;
+		} catch (IllegalArgumentException e) {
+			return -1;
+		}
 	}
 }
