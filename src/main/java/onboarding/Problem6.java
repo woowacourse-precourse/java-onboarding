c4e59ef03f4dc0a3e9fde749c59bd9c04e2c4ad4
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2022-10-31 16:48:14 +0900

    Problem6 : 닉네임을 두글자씩 잘라 해당 닉네임을 포함하는 멤버의 이메일을 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
-    
+
+    /**
+     * 닉네임을 두글자씩 잘라 해당 닉네임을 포함하는 멤버의 이메일을 저장
+     * @param members
+     * @return
+     */
+    private static Map<String, List<Member>> getMemberMapByNickname(List<Member> members) {
+        Map<String, List<Member>> memberMap = new HashMap<>();
+        members.forEach(member -> {
+            String nickname = member.getName();
+            for (int i=0; i<nickname.length()-1; i++) {
+                String key = nickname.substring(i, i+2);
+                memberMap.putIfAbsent(key, new ArrayList<>());  // key 값이 이미 존재하는 경우 Map의 value 반환, 존재하지 않는 경우 Map에 저장하고 null 반환
+                memberMap.get(key).add(member); // key에 맴버를 추가
+            }
+        });
+        return memberMap;
+    }
+
     public static class Member {
         private String email;
         private String name;
