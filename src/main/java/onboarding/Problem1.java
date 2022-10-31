4a6164f7a2c6df80c8975cbf293914fdfc95c1a1
kim1387 <kevink0@naver.com>
2022-11-01 00:58:27 +0900

    feat: 숫자를 자리별로 나누어 리스트에 저장하고 반환하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+  public static int solution(List<Integer> pobi, List<Integer> crong) {
+    int answer = Integer.MAX_VALUE;
+    return answer;
+  }
+
+  public static List<Integer> getDigitListByPageList(Integer page) {
+    List<Integer> digitList = new ArrayList<>();
+    while (page > 0) {
+      digitList.add(page % 10);
+      page /= 10;
     }
+    return digitList;
+  }
+
 }
