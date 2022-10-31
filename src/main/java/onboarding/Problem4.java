a577e5828386fd5425a78c17e17de377cbef2ce5
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-10-31 23:07:57 +0900

    solve Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] arr = word.toCharArray();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==32){
+                answer += " ";
+            }
+            else if(Character.isUpperCase(arr[i])){
+                int temp = arr[i] - 65;
+                int reverse = (65 + 25) - temp;
+                answer += String.valueOf((char)reverse);
+            }
+            else{
+                int temp = arr[i] - 97;
+                int reverse = (97 + 25) - temp;
+                answer += String.valueOf((char)reverse);
+            }
+        }
+        System.out.println(answer);
         return answer;
     }
 }
