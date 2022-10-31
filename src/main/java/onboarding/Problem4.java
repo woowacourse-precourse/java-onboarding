6096dfb9af0b231712dab025702602fe709a2f5f
farmJun <junpreme1205@gmail.com>
2022-10-31 16:06:22 +0900

    Problem4. 기능 요구 사항대로 문자를 뒤집는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,17 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    //문자를 뒤집어서 반환하는 함수
+    public static Character reverse(char c) {
+        if (!Character.isAlphabetic(c)) {
+            return c;
+        }
+        if (Character.isLowerCase(c)) {
+            return (char) ('a' + 'z' - c);
+        }
+        if (Character.isUpperCase(c)) {
+            return (char) ('A' + 'Z' - c);
+        }
+    }
 }
