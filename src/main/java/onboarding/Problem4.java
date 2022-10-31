b9ff07bcbfd5db8e99ea770dd4d7b5928cb1c234
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2022-10-31 22:08:51 +0900

    feat(Problem4): 대문자, 소문자 변환 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem4 {
+
+    private final static int  UPPER_CASE_SUM = 155;
+    private final static int  LOWER_CASE_SUM = 219;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static char chageUppercase(int c){
+        char changeWord = (char)(UPPER_CASE_SUM - c);
+        return changeWord;
+    }
+
+    private static char changeLowercase(int c){
+        char changeWord = (char)(LOWER_CASE_SUM - c);
+        return changeWord;
     }
 }
