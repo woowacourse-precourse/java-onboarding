c3e2e04c39679780b7919a4b35eddcabf2cd8eee
LeeJejune <leejejune0625@gmail.com>
2022-10-31 14:36:32 +0900

    feat: 에러 체킹 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,5 +6,35 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    /**
+     * 1-3 에러체크
+     * @param list
+     * @return 페이지의 범위 및 페이지의 길이 검사 후 옳은 리스트라면 fasle 리턴 아니면 true 리턴
+     */
+    private static boolean validation(List<Integer> list) {
+        return !(checkValidatePage(list.get(0), list.get(1)) &&
+            checkValidateRange(list.get(0), list.get(1)));
+    }
+
+    /**
+     * 1-2 왼쪽 페이지와 오른쪽 페이지의 범위 검사
+     * @param leftPage
+     * @param rightPage
+     * @return 옳은 페이지 범위면 true 틀리면 false 리턴
+     */
+    private static boolean checkValidateRange(int leftPage, int rightPage) {
+        return (leftPage <= PAGE_MAX_RANGE && rightPage <= PAGE_MAX_RANGE) &&
+            (leftPage >= PAGE_MIN_RANGE && rightPage >= PAGE_MIN_RANGE);
+    }
+
+    /**
+     * 1-1 왼쪽 페이지와 오른쪽 페이지의 길이 검사
+     * @param leftPage
+     * @param rightPage
+     * @return 옳은 페이지 길이면 true 틀리면 false 리턴
+     */
+    private static boolean checkValidatePage(int leftPage, int rightPage) {
+        return leftPage % 2 == 1 && rightPage % 2 == 0 && leftPage + 1 == rightPage;
+    }
     }
 }
