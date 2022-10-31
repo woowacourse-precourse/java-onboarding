8b26f2884fb184273bdfcc1c98502b04d1848e8c
song-wooseok <sws981202@gmail.com>
2022-10-31 20:56:08 +0900

    feat: [PROBLEM6 - 기능2] 닉네임에 문자가 중복되어 있는지 확인하는 기능

---
+++ src/main/java/onboarding/problem6/service/DuplicatedWordService.java
@@ -0,0 +1,7 @@
+package onboarding.problem6.service;
+
+public class DuplicatedWordService {
+    public static boolean isDuplicatedWord(String duplicateWord, String word){
+        return word.contains(duplicateWord);
+    }
+}
