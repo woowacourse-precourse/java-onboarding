df304ac6f42fb04599c52391c2d413d9808c3e53
seokjin <rnrn8678@naver.com>
2022-10-31 17:33:00 +0900

    feat(Problem2.java): 중복되는 문자열 삭제 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,24 @@ package onboarding;
 
 public class Problem2 {
 	public static String solution(String cryptogram) {
-		String answer = "answer";
-		return answer;
+		while (isDuplicate(cryptogram)) {
+			int duplicateStart = 0;
+			int duplicateEnd = cryptogram.length();
+			for (int i = 0; i < cryptogram.length(); i++) {
+				if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+					duplicateStart = i;
+					break;
+				}
+			}
+			for (int i = duplicateStart + 1; i < cryptogram.length(); i++) {
+				if (cryptogram.charAt(duplicateStart) != cryptogram.charAt(i)) {
+					duplicateEnd = i;
+					break;
+				}
+			}
+			cryptogram = cryptogram.substring(0, duplicateStart) + cryptogram.substring(duplicateEnd);
+		}
+		return cryptogram;
 	}
 
 	private static boolean isDuplicate(String cryptogram) {
