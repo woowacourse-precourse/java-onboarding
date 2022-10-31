a24e4ce29ba544f968d1a654c1f18d5e4278653f
jaewoo <wjdwodn0722@naver.com>
2022-10-31 18:39:47 +0900

    feat : problem6 add compare two string method
    - add findRedundancy method

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,19 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+    public static boolean findRedundancy(String string1, String string2){
+        char c1, c2;
+        for(int i=0; i<string1.length() - 1; i++){
+            c1 = string1.charAt(i);
+            c2 = string1.charAt(i+1);
+            for(int j=0; j<string2.length()-1; j++){
+                if(c1 == string2.charAt(j)){
+                    if(c2 == string2.charAt(j+1)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
