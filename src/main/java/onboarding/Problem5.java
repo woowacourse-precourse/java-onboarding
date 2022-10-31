9153d51f7e67ec39bf72ace7c4a880b0ad76a8c6
donghae <donghae1999@gmail.com>
2022-10-31 23:59:57 +0900

    P5 : 최종 수정, 주석

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,13 +10,14 @@ public class Problem5 {
         answer=calcMoney(money);
         return answer;
     }
+    //큰 금액부터 돈 나누는 함수 구현
     private static List<Integer> calcMoney(int money){
         int []arr= {50000,10000,5000,1000,500,100,50,10,1};
         List<Integer> result = new ArrayList<>(arr.length);
         for(int i=0; i<arr.length; i++){
             int moneyNum = money/arr[i];
             result.add(moneyNum);
-            money-=arr[i]*moneyNum;
+            money-=(arr[i]*moneyNum);
         }
         return result;
     }
