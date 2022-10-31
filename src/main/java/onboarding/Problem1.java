6d28591b4f73ecf50661b7265613cc7933059140
박상천 <pswlove38@naver.com>
2022-10-31 12:38:12 +0900

    한 페이지 값 계산 수정 리스트 값 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -29,16 +29,16 @@ class Problem1 {
 
     public static int calc(List<Integer> list) {
         int ls = 0;
-        int lp = 1;
+        int lp = 0;
         int rs = 0;
-        int rp = 1;
+        int rp = 0;
         int num1, num2, sum, product;
         List<Integer> arr = null;
         for (int i = 0; i < list.size(); i++) {
             num1 = list.get(i);
             num2 = list.get(i);
             sum = 0;
-            product = 0;
+            product = 1;
             while (num1 != 0) {
                 sum += num1 % 10;
                 product *= num2 % 10;
@@ -57,10 +57,19 @@ class Problem1 {
         return Collections.max(arr);
     }
     // 한 페이지에서 큰 값 계산
+
+    public static boolean listCheck(List<Integer> list){
+        if(list.get(list.size()-1) - list.get(list.size()-2) == 1)
+            return true;
+        else
+            return false;
+    }
+    // 리스트 값 체크
     public static void main(String[] args){
         List<Integer> list = makeList();
         System.out.println(list);
         System.out.println(calc(list));
+        System.out.println(listCheck(list));
     }
     // 리스트 초기화
     // 리스트 비교
