f394c13f0e468caed8be088d213876c8ad50a548
gksruf2848 <4455qqq@naver.com>
2022-10-31 14:29:49 +0900

    Problem1 입력값 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,22 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
+
+        if (checkInput(pobiLeft, pobiRight) || checkInput(crongLeft, crongRight)) // 하나라도 조건에 맞지 않으면 -1을 리턴함
+            return -1;
+
+    }
+
+    private static boolean checkInput(int left, int right){
+        if (left < 1 || left > 400 || right < 1 || right > 400) //1 ~ 400 외의 범위일 경우
+            return true;
+        if (left + 1 != right) // 왼쪽 페이지 + 1 = 오른쪽 페이지 가 아닐 경우
+            return true;
+        return left % 2 != 1; // 왼쪽 페이지가 홀수가 아닐 경우
     }
 
     static int add(int num) {
