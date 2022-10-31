33ea1534014dec8bcb2b7a17af54ea0a5b98abd5
sdsr <dlxodnr1212@naver.com>
2022-10-31 13:34:23 +0900

    problem7 기능 1 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> myfriend = new ArrayList<String>();
+
+        myfriend = find_friend(friends);
+
         return answer;
+    }
+    // 친구 list 반환
+    public static List<String> find_friend(List<List<String>> friends) {
+        List<String> friend = new ArrayList<String>();
+        for (int i = 0; i < friends.size(); i++) {
+            friend.add(friends.get(i).get(0));
+        }
+        return friend;
     }
 }
