7bb1a84e6f954b46c38e80bc42b34d7c8fd081ca
송민진 <m_6595@naver.com>
2022-11-01 02:47:26 +0900

    #3 feat: raise number to the given parameter

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,7 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int n = 1; n <= number; n++) {}
         return answer;
     }
 }
