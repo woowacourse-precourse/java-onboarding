3fe0fb46d6769bc5538960e5cd259a4c8a81bd1f
jisoolee7101@gmail.com <phllo154@naver.com>
2022-10-31 16:03:41 +0900

    한자리수 countClap 횟수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,30 +10,30 @@ public class Problem3 {
     }
     public static int solution(int number) {
         int answer = 0;
-        int numLength = 0;
+        int numLength;
         int countClap = 0;
 
         for(int i = 1; i <= number; i++) {
             numLength = (int) Math.log10(i)+1;
-            System.out.println(numLength);
 
             switch (numLength) {
                 case 1:
-                    System.out.println("one");
+                    if(i == 3 || i == 6 || i == 9)
+                        countClap++;
                     break;
                 case 2:
-                    System.out.println("two");
+                    //System.out.println("two");
                     break;
                 case 3:
-                    System.out.println("three");
+                    //System.out.println("three");
                     break;
                 case 4:
-                    System.out.println("four");
+                    //System.out.println("four");
                     break;
 
             }
         }
-
+        System.out.println(countClap);
         return answer;
     }
 }
