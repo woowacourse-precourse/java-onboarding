ace272b0d11973620b4f5689ebddc6f95a893928
AhnJoonSung <tjdtna01@naver.com>
2022-10-31 22:00:58 +0900

    Problem7 score

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,13 +6,8 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-        HashMap<String, Set<String>> friendsMap = friendInit(friends);
-
-        return answer;
-    }
-
-    static HashMap<String, Set<String>> friendInit(List<List<String>> friends) {
         HashMap<String, Set<String>> friendsMap = new HashMap<>();
+        HashMap<String, Integer> recommendScore = new HashMap<>();
 
         for (int i = 0; i < friends.size(); i++) {
             String a = friends.get(i).get(0);
@@ -28,6 +23,24 @@ public class Problem7 {
             friendsMap.put(b, bFriends);
         }
 
-        return friendsMap;
+        // 함께 아는 친구 점수
+        Set<String> userFriends = friendsMap.get(user);
+        for (String friend: userFriends) {
+            Set<String> recommendFriends = friendsMap.get(friend);
+            recommendFriends.remove(user);
+
+            for (String recommendFriend : recommendFriends) {
+                recommendScore.put(recommendFriend, recommendScore.getOrDefault(recommendFriend, 0) + 10);
+            }
+        }
+
+        // 방문 횟수 점수
+        for (String visitor : visitors) {
+            recommendScore.put(visitor, recommendScore.getOrDefault(visitor, 0) + 1);
+        }
+
+        return answer;
     }
+
+
 }
