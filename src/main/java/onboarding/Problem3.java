fb2d61d75342d76a6659f1659f54f4addf6fe0db
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-10-31 23:17:23 +0900

    feat : Problem3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        return countClap(number);
+    }
+
+    // 3, 6, 9의 개수만큼 손뼉친 횟수 리턴
+    public static int countClap(int number) {
         int answer = 0;
+        int nowNumber;
+        for (int i = 1; i <= number; i++) {
+            nowNumber = i;
+            while (nowNumber != 0) {
+                if (nowNumber % 10 == 3 || nowNumber % 10 == 6 || nowNumber % 10 == 9) {
+                    answer++;
+                }
+                nowNumber /= 10;
+            }
+        }
         return answer;
     }
 }
