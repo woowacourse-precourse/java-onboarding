09a42fb1199f65c692e8c84c6fa1467445fe071e
MinjeongKong <kong1301@ajou.ac.kr>
2022-10-31 22:29:57 +0900

    Problem2 -  메서드 자바독 수정 및 리팩토링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,22 +1,28 @@
 package onboarding;
 
-/**
- * [기능 요구사항 목록]
- * 1. 중복 문자를 삭제한 결과를 저장하는 메서드 getMessage
- */
+
 public class Problem2 {
 
-    public static String getMessage(String str, boolean flag) {
-        if (flag) {
+    public static final boolean NO_DUPLICATION = true;
+    public static final boolean YES_DUPLICATION = false;
+
+    /**
+     * 중복 문자를 삭제한 결과를 저장하는 메서드
+     * @param str 해독 해야할 암호문
+     * @param isComplete 더이상 해독할 게 없으면 true
+     * @return 해독된 암호문
+     */
+    public static String getMessage(String str, boolean isComplete) {
+        if (isComplete) {
             return str;
         }
         String msg = "";
         str += " ";
-        flag = true;
+        isComplete = NO_DUPLICATION;
 
         if (str.length() > 1) {
             if (str.charAt(0) == str.charAt(1)) {
-                flag = false;
+                isComplete = YES_DUPLICATION;
             } else {
                 msg += str.charAt(0);
             }
@@ -24,14 +30,14 @@ public class Problem2 {
 
         for (int i = 1; i < str.length() - 1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {
-                flag = false;
+                isComplete = YES_DUPLICATION;
                 continue;
             }
             if (str.charAt(i) != str.charAt(i - 1)) {
                 msg += str.charAt(i);
             }
         }
-        return getMessage(msg, flag);
+        return getMessage(msg, isComplete);
     }
     public static String solution(String cryptogram) {
         return getMessage(cryptogram, false);
