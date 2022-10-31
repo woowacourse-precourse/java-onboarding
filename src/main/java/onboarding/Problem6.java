0e2b0e7010039fabc691b72f5b673b43a33588fc
D <doxxx93@gmail.com>
2022-11-01 04:24:27 +0900

    Problem6 (#13)
    * docs: add requirement checklist
    
    * feat: solve problem6
    
    * refactor: sort answer list comparing without domain
    
    * refactor: extract method, enhance code complexity

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,30 +3,37 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Problem6 {
 
     private static final int EMAIL_LENGTH = 10;
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
+        Set<String> answer = new TreeSet<>(Comparator.comparing(s -> s.substring(0, s.length() - EMAIL_LENGTH)));
         for (List<String> form : forms) {
-            for (List<String> anotherForm : forms) {
-                if (form.equals(anotherForm)) {
-                    continue;
-                }
-                String nickname = form.get(1);
-                String anotherNickname = anotherForm.get(1);
-                IntStream.range(0, nickname.length() - 1)
-                    .mapToObj(index -> nickname.substring(index, index + 2))
-                    .filter(anotherNickname::contains)
-                    .map(piece -> form.get(0))
-                    .forEach(answer::add);
+            findDuplicatedNickname(forms, answer, form);
+        }
+        return toList(answer);
+    }
+
+    private static void findDuplicatedNickname(List<List<String>> forms, Set<String> answer, List<String> form) {
+        String nickname = form.get(1);
+        for (int index = 0; index < nickname.length() - 1; index++) {
+            if (isDuplicated(forms, form, nickname, index)) {
+                answer.add(form.get(0));
             }
         }
-        return answer.stream().distinct().sorted(
-            Comparator.comparing(o -> o.substring(0, o.length() - EMAIL_LENGTH))).collect(Collectors.toList());
+    }
+
+    private static boolean isDuplicated(List<List<String>> forms, List<String> form, String nickname, int index) {
+        return forms.stream()
+            .anyMatch(anotherForm -> anotherForm.get(1).contains(nickname.substring(index, index + 2)) && !form.equals(
+                anotherForm));
+    }
+
+    private static ArrayList<String> toList(Set<String> answer) {
+        return new ArrayList<>(answer);
     }
 }
