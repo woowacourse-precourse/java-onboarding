08c545e67404ec4a5dc8bff1c762cf78fc9d49ec
hossang <zkzk104@naver.com>
2022-10-31 21:33:14 +0900

    refactor: 기존 코드를 i증가할 때마다 369계산하는 메소드(count369)로 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,15 +4,20 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            int num = i;
-            while (num != 0) {
-                if (is369(num)) {
-                    answer++;
-                }
-                num /= 10;
-            }
+            answer += count369(i);
         }
         return answer;
+    }
+    
+    private static int count369(int i) {
+        int count = 0;
+        while (i != 0) {
+            if (is369(i)) {
+                count++;
+            }
+            i /= 10;
+        }
+        return count;
     }
 
     private static boolean is369(int num) {
