3cf4bf15c01abeb883b1251fd2752579bd6cdac4
NotableDeveloper <castlereal@naver.com>
2022-10-30 23:21:15 -0700

    Problem2 (feat) - 중복문자 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        StringBuilder sb = new StringBuilder(cryptogram);
+        sb.append(' ');
+
+        for(int i = 0; i < sb.length() - 1; i++){
+            if(sb.charAt(i) == sb.charAt(i + 1)){
+                sb.delete(i, i + 2);
+                break;
+            }
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        System.out.println(sb.toString());
         return answer;
+    }
+
+    public static void main(String[] args) {
+        solution("browoanoommnaon");
+        solution("browoanmmnaon");
     }
 }
