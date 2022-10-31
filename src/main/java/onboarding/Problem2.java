4bce9c439f935cfc96e68a37a8f8f4c731fd7cb1
jeongyeonKimm <wjddus9132@gmail.com>
2022-10-31 22:35:56 +0900

    [Feature]Problem2 - solution

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,40 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decipher(cryptogram);
+    }
+
+    private static String decipher(String cryptogram) {
+        if (cryptogram == null) {
+            return null;
+        }
+
+        Deque<Character> queue = new ArrayDeque<>();
+        char[] cryptogramToChar = cryptogram.toCharArray();
+
+        for (char c: cryptogramToChar) {
+            if (!queue.isEmpty() && queue.peekLast() == c) {
+                queue.pollLast();
+                continue;
+            }
+            queue.add(c);
+        }
+
+        return queueToString(queue);
+    }
+
+    private static String queueToString(Deque<Character> queue) {
+        char[] result = new char[queue.size()];
+
+        int k = 0;
+        while (!queue.isEmpty()) {
+            result[k++] = queue.poll();
+        }
+
+        return String.valueOf(result);
     }
 }
