62cc71264886ae9a19cae7bbd60d8a07c75978b6
Damgom <wisewish1@gmail.com>
2022-10-31 13:01:53 +0900

    Fix: fix compare return value

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if((pobi.get(1) - pobi.get(0)) != 1 || (crong.get(1) - crong.get(0)) != 1) {
+        if(!exception(pobi, crong)) {
             return -1;
-        }else return Integer.compare(maxValue(pobi.get(0), pobi.get(1)), maxValue(crong.get(0), crong.get(1)));
+        } return compare(maxValue(pobi.get(0), pobi.get(1)), maxValue(crong.get(0), crong.get(1)));
+    }
+    public static boolean exception(List<Integer> pageList, List<Integer> pageList2) {
+        return (pageList.get(1) - pageList.get(0)) == 1 && (pageList2.get(1) - pageList2.get(0)) == 1;
+    }
+    public static int compare(int pobiNum, int crongNum) {
+        if(pobiNum > crongNum) {
+            return 1;
+        }else if(pobiNum < crongNum) {
+            return 2;
+        }else return 0;
     }
     public static int higherValue(int num) {
         String number = String.valueOf(num);
