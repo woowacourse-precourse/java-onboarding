69f699f77a6578f98d8be8f75e4d65435c8c1b97
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-31 19:03:15 +0900

    7번 문제 해결

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,12 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import onboarding.prob7.domain.RecommendService;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> answer = Collections.EMPTY_LIST;
+        RecommendService service = new RecommendService(user, friends, visitors);
+        return service.solve();
     }
 }
