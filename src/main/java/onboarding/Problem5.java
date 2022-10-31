2e801e2af6c25130f44a030077eadb1fbc9ec712
daeun <gain3436@gmail.com>
2022-11-01 00:30:50 +0900

    [feat](5) 모든 화폐에 대해 큰 화폐부터 몫과 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +14,13 @@ public class Problem5 {
     }
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        int[] paperMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i=0; i<paperMoney.length; i++) {
+            int paperCnt = divide(money, paperMoney[i]);
+            money = remainder(money, paperMoney[i]);
+            answer.add(paperCnt);
+        }
         return answer;
     }
 }
