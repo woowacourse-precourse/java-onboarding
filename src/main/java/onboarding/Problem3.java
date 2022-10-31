a84a080c7d18e97879d54e9e00e0064195dc3ff7
Ran <lan4250@naver.com>
2022-10-31 18:58:41 +0900

    Q3. solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; ++i) {
+            String tmp = "" + i;
+            for (int j = 0; j < tmp.length(); ++j) {
+                if (tmp.charAt(j) == '3' || tmp.charAt(j) == '6' || tmp.charAt(j) == '9') {
+                    ++answer;
+                }
+            }
+        }
         return answer;
     }
 }
