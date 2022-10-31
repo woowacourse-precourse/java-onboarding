040861322db7234ce0e592e80ffc434f8277cf92
박상천 <pswlove38@naver.com>
2022-10-31 12:29:01 +0900

    리스트 만들기 수정 한 페이지 값 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -11,20 +9,58 @@ class Problem1 {
     }
     public static int random(){
         Random rd = new Random();
-        return rd.nextInt(400)+1;
+        return rd.nextInt(400); // 1 ~ 399
     }
     // 난수 발생
 
     public static List makeList(){
         List<Integer> list = new ArrayList<>();
-        list.add(random());
-        list.add(random());
+        int n1 = random();
+        int n2 = 0;
+        if(n1==399)
+            n2 = 400;
+        else
+            n2 = n1+1;
+        list.add(n1);
+        list.add(n2);
         return list;
     }
     // 리스트 만들기
 
+    public static int calc(List<Integer> list) {
+        int ls = 0;
+        int lp = 1;
+        int rs = 0;
+        int rp = 1;
+        int num1, num2, sum, product;
+        List<Integer> arr = null;
+        for (int i = 0; i < list.size(); i++) {
+            num1 = list.get(i);
+            num2 = list.get(i);
+            sum = 0;
+            product = 0;
+            while (num1 != 0) {
+                sum += num1 % 10;
+                product *= num2 % 10;
+                num1 /= 10;
+                num2 /= 10;
+            }
+            if (i == 0) {
+                ls = sum;
+                lp = product;
+            } else {
+                rs = sum;
+                rp = product;
+            }
+            arr = Arrays.asList(ls, lp, rs, rp);
+        }
+        return Collections.max(arr);
+    }
+    // 한 페이지에서 큰 값 계산
     public static void main(String[] args){
-        System.out.println(makeList());
+        List<Integer> list = makeList();
+        System.out.println(list);
+        System.out.println(calc(list));
     }
     // 리스트 초기화
     // 리스트 비교
