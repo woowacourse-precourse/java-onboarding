2d496eb1d0a2772b632740149ae8b38183aabf8c
gilyeon00 <12venus08@naver.com>
2022-11-01 03:04:59 +0900

    Feat : 문제2 - 중복 문자 삭제 기능추가 및 문제2 풀이완료

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 미션 - 온보딩
-## :로켓: 기능 요구 사항
+## :rocket: 기능 요구 사항
 아래의 7가지 기능 요구 사항을 모두 해결해야 한다.
 - [x] [문제 1](./docs/PROBLEM1.md)
 - [문제 1 풀이](./src/main/java/onboarding/Problem1.java) 
@@ -10,23 +10,23 @@
 - [ ] [문제 6](./docs/PROBLEM6.md)
 - [ ] [문제 7](./docs/PROBLEM7.md)
 ---
-## :다트: 프로그래밍 요구 사항
+## 🎯 프로그래밍 요구 사항
 - [x] JDK 11 버전에서 실행 가능해야 한다. **JDK 11에서 정상적으로 동작하지 않을 경우 0점 처리한다.**
 - [ ] `build.gradle`을 변경할 수 없고, 외부 라이브러리를 사용하지 않는다.
 - [ ] 프로그램 종료 시 `System.exit()`를 호출하지 않는다.
 - [ ]  프로그램 구현이 완료되면 `ApplicationTest`의 모든 테스트가 성공해야 한다. **테스트가 실패할 경우 0점 처리한다.**
 - [ ] 프로그래밍 요구 사항에서 달리 명시하지 않는 한 파일, 패키지 이름을 수정하거나 이동하지 않는다.
 ---
-## :연필2: 과제 진행 요구 사항
+## ✏️ 과제 진행 요구 사항
 - [x]  미션은 [java-onboarding](https://github.com/woowacourse-precourse/java-onboarding) 저장소를 Fork & Clone해 시작한다.
 - [ ]  과제 진행 및 제출 방법은 [프리코스 과제 제출](https://github.com/woowacourse/woowacourse-docs/tree/master/precourse) 문서를 참고한다.
 ---
-## :rocket: 기능 요구 사항 
+## 📑 기능 요구 사항 명세
 ### 문제 1
 - [x] 페이지 유효 체크
 - [x] 가장 큰 수 구하기
 - [x] solution
 ### 문제 2
-- [ ] 문자열 유효 체크 - 길이
-- [ ] 문자열 유효 체크 - 소문자
-- [ ] 중복 문자 삭제
+- [x] 문자열 유효 체크 - 길이
+- [x] 문자열 유효 체크 - 소문자
+- [x] 중복 문자 삭제
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,20 +11,52 @@ package onboarding;
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
-
+        /* 제한 사항 처리 */
         if (checkStr(cryptogram) == null) {
             return "문자열 cryptogram이 제한 사항을 위반했습니다.";
+        }
+
+        String answer = "answer";
+        String tmp = answer;
+        String subString = "";
+        boolean isComplete = false;
+
+        while (isComplete != true) {
+            tmp = answer;
+
+            if (cryptogram.length() == 2 && cryptogram.charAt(0) == cryptogram.charAt(1)) {
+                answer = "";        // 문자열이 2글자, 중복 문자로 구성되어있을때 (ex. bb)
+                break;
+            } else {    // 앞뒤 문자가 같은 지 확인 -> 같다면 substring에 넣어주기
+                for (int i = 1; i < cryptogram.length() - 1; i++) {
+                    if (cryptogram.charAt(i) == cryptogram.charAt(i + 1) || cryptogram.charAt(i) == cryptogram.charAt(i - 1))
+                        subString += cryptogram.charAt(i);
+                }
+            }
+
+            /* 중복문자에 해당하는 substring은 없애주기 (null로 replace)*/
+            answer = cryptogram.replaceAll(subString, "");
+            cryptogram = answer;
+
+            /* 이전과 바뀐 값이 없다면(중복 문자가 없다면) */
+            if (tmp.equals(answer)) {
+                isComplete = true;
+            }
+
+            /* 다음 번 중복문자를 담기위해, subString값 초기화 */
+            subString = "";
         }
 
         return answer;
     }
 
     private static String checkStr(String cryptogram) {
+        /* 문자열 길이가 1이상 1000이하인지 확인 */
         if (cryptogram.length() < 1 && cryptogram.length() > 1000) {
             return null;
         }
 
+        /* 문자열이 소문자로 이뤄졌는지 확인 */
         for (int i = 0; i < cryptogram.length(); i++) {
             if (Character.isLowerCase(cryptogram.charAt(i)) != true) {
                 return null;
