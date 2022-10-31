9a2b1f239f2d50f8f14d3752acdbe8ca1cc7a1bb
wnsgml7267 <dlwns7267@naver.com>
2022-11-01 02:38:25 +0900

    [Problem7] 사용자 친구 찾기 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+    //사용자 친구 찾기
+    private static List<String> findUserFriends(String user, List<List<String>> friends) {
+        List<String> list = new ArrayList<>();
+        for(List<String> item : friends){
+            if(item.contains(user))
+                list.add(item.get(1 - item.indexOf(user)));
+        }
+        return list.stream().distinct().collect(Collectors.toList());
     }
+
 }
