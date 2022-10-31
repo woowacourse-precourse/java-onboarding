eda3869d20ce5193c135a1d08a6bfc1e5b201684
c4fiber <qudcjf153@gmail.com>
2022-10-31 22:06:51 +0900

    feat(prob7): 선택한 user의 친구목록을 얻는 기능 추가
    user 뿐만 아니라 user의 friends의 친구목록도 얻을 수 있도록 인자를 하나 더 추가함

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        List<String> myFriends = getUsersFriends(user, friends);
+
+
         return answer;
+    }
+
+    private static List<String> getUsersFriends(String me, List<List<String>> friends) {
+        List<String> my = new ArrayList<>();
+
+        for(List<String> connection : friends) {
+            if (connection.contains(me)) {
+                my.addAll(connection);
+            }
+        }
+
+        while(my.contains(me)) {
+            my.remove(me);
+        }
+
+        return my;
     }
 }
