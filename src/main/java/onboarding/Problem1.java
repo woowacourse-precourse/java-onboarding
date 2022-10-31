73be3ed7a1ea8f6fe9e6d52c08a0f2d7a2bbe52e
Kijung Park <kjpark4321@naver.com>
2022-10-31 22:44:47 +0900

    1번: 페이지숫자 자리 곱하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,23 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        
         return answer;
+    }
+
+    private static int getMultiplyOfNumber(Integer targetNum) {
+        int ret = 1;
+
+        int leftNum = targetNum;
+        int curPos = 10;
+        while (leftNum != 0) {
+            ret *= leftNum%curPos;
+            leftNum /= curPos;
+            curPos *= 10;
+        }
+
+        System.out.println(ret);
+        return ret;
     }
 }
