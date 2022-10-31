c760730163835df182db46ccccaa0e5d87f169df
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-10-31 10:47:24 +0900

    feat(Problem6): 구현할 기능 목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,11 @@ package onboarding;
 
 import java.util.List;
 
+// <기능 목록>
+// 1. 중복으로 간주되는 닉네임 찾기
+// 2. 오름차순으로 이메일 정렬
+// 3. 이메일 중복 제거
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
