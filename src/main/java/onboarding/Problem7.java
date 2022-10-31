d38ba2c51bef5e2841f1705faa96ea7f08760e96
TaemHam <tk4003950@gmail.com>
2022-10-31 18:09:06 +0900

    Feat: Implement recommendFriends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,47 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
 
-    public static List<String> recommendFriends(String user, List<List<String>> friends, List<String> visitors) {
+    /**
+     * 추천 점수대로 친구를 추천합니다.
+     * @param user 추천 친구를 조회할 유저 ID 입니다.
+     * @param friends 친구 관계가 포함된 배열입니다.
+     * @param visitors 타임라인을 방문한 유저의 배열입니다.
+     * @return 추천할 친구의 배열입니다.
+     */
+    private static List<String> recommendFriends(String user, List<List<String>> friends, List<String> visitors) {
+        HashMap<String, List<String>> friendGraph = getFriendGraph(friends);
+        HashMap<String, Integer> friendPoints = new HashMap<>();
+        HashSet<String> notToAdd = new HashSet<>(friendGraph.get(user));
+        notToAdd.add(user);
+
+        add10points(user, friendGraph, friendPoints, notToAdd);
+        add1point(visitors, friendPoints, notToAdd);
+        List<String> sortedList = sortByPointAndId(friendPoints);
+
+        return Collections.emptyList();
+    }
+
+    private static HashMap<String, List<String>> getFriendGraph(List<List<String>> friends) {
+        return new HashMap<>();
+    }
+
+    private static void add10points(String user,
+                               HashMap<String, List<String>> friendGraph,
+                               HashMap<String, Integer> friendPoints,
+                               HashSet<String> notToAdd) {
+        return;
+    }
+
+    private static void add1point(List<String> visitors,
+                                  HashMap<String, Integer> friendPoints,
+                                  HashSet<String> notToAdd) {
+        return;
+    }
+
+    private static List<String> sortByPointAndId(HashMap<String, Integer> friendPoints) {
         return Collections.emptyList();
     }
 
