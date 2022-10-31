d7cd440af23614c92e3f84f48d917cf747fd9250
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-10-31 22:46:10 +0900

    [FEAT] : 특정 숫자의 3,6,9개수 확인 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int checkNum(int number){
+        String numStr = Integer.toString(number);
+        int result = 0;
+        for(char digit : numStr.toCharArray()){
+            if(digit == '3' || digit == '6' || digit == '9'){
+                result += 1;
+            }
+        }
+        return result;
     }
 }
