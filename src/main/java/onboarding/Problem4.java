b22b33a89142395d07a32556b8ec6fde1e0efdb7
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-10-31 19:05:58 +0900

    refact(4번) translateWord 함수 추출

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,18 +5,24 @@ public class Problem4 {
         String answer = "";
 
         if (word.length()<1 || word.length()>1000)
-            return "Invalid Input";
+            return "error";
 
         for (int i = 0; i<word.length(); i++){
-            int alphabet = word.charAt(i);
-            if(alphabet >=65 && alphabet <= 90){
-                alphabet = 155 - alphabet;
-            }else if(alphabet >=97 && alphabet <= 122){
-                alphabet = 219 - alphabet;
-            }
-            answer += (char)alphabet;
+            answer += translateWord(word.charAt(i));
+            System.out.println(answer);
         }
 
         return answer;
+    }
+
+    public static char translateWord(int alphabet){
+
+        if(alphabet >=65 && alphabet <= 90){
+            alphabet = 155 - alphabet;
+        }else if(alphabet >=97 && alphabet <= 122){
+            alphabet = 219 - alphabet;
+        }
+        System.out.println(alphabet);
+        return (char) alphabet;
     }
 }
