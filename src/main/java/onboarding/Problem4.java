9327c8e8bcf9531976c9dc33961f27dfe3d5fe82
baksohyeon <bori12370@gmail.com>
2022-10-31 12:21:51 +0900

    feat: add logic which get array of converted ascii code

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        List<Character> characterList = word.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        List<Integer> convertAscii = characterList.stream().map(x -> {
+            int rawCharAscii = (int) x;
+            if (rawCharAscii >= 65 && rawCharAscii <= 90) {
+                return 155 - rawCharAscii;
+            } else if (rawCharAscii < 97 || rawCharAscii > 122) {
+                return rawCharAscii;
+            } else {
+                return 219 - rawCharAscii;
+            }
+        }).collect(Collectors.toList());
+
+        return "abc";
     }
 }
