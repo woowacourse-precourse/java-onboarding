4f3cf95319fda9b1bf79733a4ecbb4bc3bb152ee
donghae <donghae1999@gmail.com>
2022-10-31 17:42:07 +0900

    P7 : 함께 아는 친구 계산 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -13,6 +13,8 @@ public class Problem7 {
 
 
         friendMember = friendList(user, friendMember, friends);
+        friendScore = calcBothFriend(user,friendScore,friendMember,friends);
+
 
         return answer;
     }
@@ -26,6 +28,32 @@ public class Problem7 {
             }
         }
         return friendMember;
+    }
+    private static HashMap calcBothFriend(String user,HashMap<String,Integer> friendScore, List<String> friendMember,  List<List<String>> friends){
+        for(int i=0; i<friendMember.size(); i++){
+            String userFriend = friendMember.get(i);
+            for(int j=0; j<friends.size(); j++){
+                String firstName = friends.get(j).get(0);
+                String secondName = friends.get(j).get(1);
+                if(firstName == user || secondName == user) continue;
+
+                if(firstName.equals(userFriend) && friendMember.contains(firstName)){
+                    int score = 0;
+                    if(!friendScore.containsKey(secondName))
+                        friendScore.put(secondName,0);
+                    score += friendScore.get(secondName) + 10;
+                    friendScore.put(secondName,score);
+                }
+                if(secondName.equals(userFriend) && friendMember.contains(secondName)){
+                    int score = 0;
+                    if(!friendScore.containsKey(firstName))
+                        friendScore.put(firstName,0);
+                    score += friendScore.get(firstName) + 10;
+                    friendScore.put(firstName,score);
+                }
+            }
+        }
+        return friendScore;
     }
 
 }
