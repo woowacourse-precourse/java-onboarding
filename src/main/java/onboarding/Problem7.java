bb528c177ac89bb7d1542c1578bec2ac08f593bf
Jueun <jueun000101@gmail.com>
2022-10-31 21:00:50 +0900

    docs: 7번 기능 목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/**
+ * 기능 목록
+ * 1. 친구 관계를 인접 리스트로 나타내기
+ * 2. 사용자와 함께 아는 친구에게 10점씩 주기
+ * 3. 방문한 친구에게 1점씩 주기
+ * 4. 사용자, 사용자가 아는 사람들은 결과에 영향을 안끼치도록 점수 없애기
+ * 5. 점수와 이름으로 정렬하기
+ * 6. 최대 5명의 이름 반환하기
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
