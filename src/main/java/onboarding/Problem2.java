2dcc8324a13391c7522c221eb5155a65fd821485
Sehoon Kim <shggm2000@gmail.com>
2022-10-31 23:13:39 +0900

    암호문을 받아 Arraylist에 넣는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        char[] arr = cryptogram.toCharArray();
+
+        ArrayList<Character> list = new ArrayList<Character>();
+
+        for(int l=0; l<arr.length; l++){
+            list.add(arr[l]);
+        }
+
+        String answer = "";
         return answer;
     }
 }
