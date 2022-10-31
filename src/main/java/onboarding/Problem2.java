61c8f2a0c1c8e335f6e1b560388c96d87447d3ed
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-31 19:45:37 +0900

    Feat(Problem2): 결과스택 만드는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,15 +5,35 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         StringBuilder answer = new StringBuilder();
-        Stack<Character> initialStack = new Stack<>();
+        Stack<Character> stackOrigin = new Stack<>();
+        Stack<Character> stackResult = new Stack<>();
+        stackResult.push('A');
 
-        makeInitialStack(cryptogram, initialStack);
+        makeStackOrigin(cryptogram, stackOrigin);
+        makeStackResult(stackOrigin, stackResult);
 
         return answer.toString();
     }
 
+    private static void makeStackResult(Stack<Character> stackOrigin, Stack<Character> stackResult) {
+        int flag = 0;
 
-    private static void makeInitialStack(String cryptogram, Stack<Character> stackOrigin) {
+        while (!stackOrigin.empty()) {
+            char checkChar = stackOrigin.pop();
+            flag = popResultStack(stackResult, flag, checkChar);
+            flag = pushResultStack(stackResult, flag, checkChar);
+        }
+    }
+
+    private static int pushResultStack(Stack<Character> stackResult, int flag, char checkChar) {
+        return flag;
+    }
+
+    private static int popResultStack(Stack<Character> stackResult, int flag, char checkChar) {
+        return flag;
+    }
+
+    private static void makeStackOrigin(String cryptogram, Stack<Character> stackOrigin) {
         stackOrigin.push('A');
         for (char crypto : cryptogram.toCharArray()) {
             stackOrigin.push(crypto);
