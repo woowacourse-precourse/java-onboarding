e46580901fdc709521eaeb9550edfde0ab406ae1
채희찬 <eqholic1125@gmail.com>
2022-10-31 15:45:43 +0900

    feat: problem2 연속된 문자 제거 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,31 +1,44 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decode(cryptogram);
     }
 
-    private static List<Integer> findConsecutiveChar(String cryptogram) {
+    private static List<Integer> findConsecutiveChar(StringBuffer cryptogram) {
         List<Integer> charsToRemove = new ArrayList<>();
 
-        for (int index = 0; index < cryptogram.length(); index++) {
+        for (int index = 0; index < cryptogram.length() - 1; index++) {
             if (cryptogram.charAt(index) == cryptogram.charAt(index + 1)) {     // 다음 글자와 같을 경우
                 charsToRemove.add(index);
-                charsToRemove.add(index);
+                charsToRemove.add(index + 1);
+                index++;
             }
         }
         return charsToRemove;
     }
 
-    private static String removeConsecutiveChar (String cryptogram, List < Integer > charsToRemove){
+    private static StringBuffer removeConsecutiveChar(StringBuffer cryptogram) {
+        List<Integer> charsToRemove = findConsecutiveChar(cryptogram);
 
+        if (charsToRemove.size() == 0) {    // 더이상 연속되는 문자가 없을 경우
+            return cryptogram;
+        }
+        Collections.sort(charsToRemove);
+        Collections.reverse(charsToRemove);
+
+        for (int index : charsToRemove) {
+            cryptogram.deleteCharAt(index);
+        }
+
+        return cryptogram;
     }
 
-    private static String decode (String cryptogram){
+    private static String decode(String cryptogram) {
 
     }
 
