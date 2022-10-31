d70f7e5ed304450ea1d95870159010ce49688975
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-10-31 13:53:14 +0900

    chore(Problem4): add comments outlining list of features

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,27 @@
 package onboarding;
 
 public class Problem4 {
+    // TODO: reverseCharacter(char c)
+        // If c is non-alphabetic
+            // Return c
+
+        // If c is uppercase
+            // Convert c to lowercase
+
+        // Get alphabet index of c
+
+        // Assign character at opposite index to c
+
+        // If uppercase
+            // Return c to uppercase
+        // Return c
+
     public static String solution(String word) {
+        // Check valid input
+
+        // For each character in word
+            // Reverse character
+
         String answer = "";
         return answer;
     }
