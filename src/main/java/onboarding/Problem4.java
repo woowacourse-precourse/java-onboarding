d8a72864e858d26b5c847d1b6c5aa38cf5ec2dc1
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 21:53:13 +0900

    Feat(Problem4) : treeFrog 메서드 추가 전달받은 문자열의 각 문자를 청개구리 사전에 따라 반대로 변환하는 메서드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = treeFrog(word);
         return answer;
+    }
+
+    private static String treeFrog(String word) {
+        char appendChar;
+        StringBuilder sb = new StringBuilder();
+        for(char c : word.toCharArray()) {
+            if(c >= 65 && c <= 90) {
+                appendChar = (char) (90 - c + 65);
+            } else if (c >= 97 && c < 122) {
+                appendChar = (char) (122 - c + 97);
+            } else {
+                appendChar = c;
+            }
+            sb.append(appendChar);
+        }
+        return sb.toString();
     }
 }
