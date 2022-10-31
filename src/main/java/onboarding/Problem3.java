2d60a00ca14fd8f20f2d6fe440a747390cd99978
EstelleYU <iamestelleyu@gmail.com>
2022-11-01 01:41:19 +0900

    (#4) Problem3 solved by EstelleYU

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        if (number > 10000 || number < 1) {
+            return -1;
+        }
+        for (int i = 1; i <= number; i++) {
+            answer += func(i);
+        }
         return answer;
     }
+
+    public static int func(int num) {
+        String[] str = String.valueOf(num).split("");
+        int count = 0;
+        for (String j : str) {
+            int k = Integer.parseInt(j);
+            if (k % 3 == 0 && k != 0) {
+                count += 1;
+            }
+        }
+        return count;
+    }
 }
+
+
