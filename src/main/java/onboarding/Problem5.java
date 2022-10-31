2ab1ff9b4bd9dce774bf2a5a5722b0709a3ca92e
imjy2 <imjy2@192.168.35.106>
2022-11-01 00:40:10 +0900

    기능 5. 수정. list get을 add로 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,7 +10,7 @@ public class Problem5 {
         answer = new LinkedList<Integer>();
         int[] money_list = {50000,10000,5000,1000,500,100,50,10,1};
         for(int i=0; i<money_list.length; i++) {
-        	answer.get(money/money_list[i]);
+        	answer.add(money/money_list[i]); // get을 add 로 수정
         	money = money - (money/money_list[i])*money_list[i];
         }
         return answer;
