bcf1b29293ec295192f33510990d409ab3b2f141
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 00:02:56 +0900

    feat(PROBLEM1): pageMax 페이지의 자리 수 합과 곱 중 큰 수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,29 +4,10 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int pobi_score =0; int crong_score = 0;
-        int temp;
-
-        //예외처리
-        if (pobi.get(0) +1 != pobi.get(1))  return -1;
-        if (crong.get(0) +1 != crong.get(1))  return -1;
-
-        for(int i = 0; i<2 ; i++){
-            // 포비의 점수
-            temp = pageMax(pobi.get(i));
-            pobi_score = Math.max(pobi_score,temp);
-
-            // 크롱의 점수
-            temp = pageMax(crong.get(i));
-            crong_score = Math.max(crong_score,temp);
-        }
-
-        if (pobi_score > crong_score)    return 1;
-        else if (pobi_score < crong_score)  return 2;
-        else    return 0;
+        return 0;
     }
 
-    // sum과 mul 중 큰 수를 구하는 함수
+    // 페이지의 각 자리 수의 합과 곱 중 큰 수를 구하는 기능
     public static int pageMax(int pageInt){
         String page = Integer.toString(pageInt);
         int sum = 0; int mul = 1, n;
@@ -38,5 +19,7 @@ class Problem1 {
         }
         return Math.max(sum,mul);
     }
+
+
 }
 
