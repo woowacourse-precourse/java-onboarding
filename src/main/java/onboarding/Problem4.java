5ba7d8f81314749a8daa60e28753914c94e61628
Songyi Kim <ksl2950@naver.com>
2022-10-31 09:40:05 +0900

    feat/4: 알파벳 변환 map 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Map<Character, Character> map = new HashMap<>();
+        int interval = 0;
+
+        for (int i = 65; i <= 90; i++) {
+            map.put((char) i, (char) (90 - interval));
+            interval += 1;
+        }
+
+        return "";
     }
+
+
 }
