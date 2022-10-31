0dff1846f0262c3e2a646483fbc95867cb78599a
seheonCho <whtpgjs94@gmail.com>
2022-10-31 21:07:23 +0900

    feat: [문제4] 매개변수를 한글자씩 다른문자를 바꾸기 위해 매개변수로 문자 배열생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,8 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        char[] wordToChars = word.toCharArray();
+
         String answer = "";
         return answer;
     }
