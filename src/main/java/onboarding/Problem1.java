b90bbdcdc54d820c2e98e7dadd483a20f234102b
dvlpr0 <rladjswkd@naver.com>
2022-10-31 17:03:04 +0900

    added: isValidList. 1) 리스트의 사이즈가 2인지 확인. 2) 페이지가 [2, 399] 범위에 있는지 확인. 3) 페이지 값의 차가 1인지 확인. 4) 인덱스 0은 홀수, 인덱스 1은 짝수인지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,21 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	private static boolean isValidList(List<Integer> list) {
+		int left;
+		int right;
+
+		left = list.get(0);
+		right = list.get(1);
+		return (list.size() == 2
+				&& right - left == 1
+				&& 1 < left && left < 400
+				&& left % 2 == 1
+				&& 1 < right && right < 400
+				&& right % 2 == 0);
+	}
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
 }
