38a7ab95c75191e34dbe9ea61ef4c02f8c629b48
ashlovesliitea <skyrider1811@gmail.com>
2022-10-31 23:19:20 +0900

    feature: 6번 - 두 닉네임이 중복되는 구간이 있다면 메일 주소를 set에 추가한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,42 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        HashSet<String> mails=new HashSet<String>();
+
+        int formSize=forms.size();
+        int[] visited=new int[formSize];
+        Arrays.fill(visited,0);
+
+        for(int i=0;i<forms.size()-1;i++){
+            for(int j=i+1;j<forms.size();j++){
+                if(visited[i]==0 && visited[j]==0){
+                    String name1=forms.get(i).get(1);
+                    String name2=forms.get(j).get(1);
+                    if(isDuplicate(name1,name2)){
+                        visited[i]=0;
+                        visited[j]=0;
+                        mails.add(forms.get(i).get(0));
+                        mails.add(forms.get(j).get(0));
+                    }
+                }
+            }
+        }
+
+        ArrayList<String> answer= new ArrayList<>(mails);
+
         return answer;
     }
+
+    public static boolean isDuplicate(String name1,String name2){
+
+        return false;
+    }
+    //두 닉네임이 중복되는 경우가 있으면 True를 리턴한다.
 }
