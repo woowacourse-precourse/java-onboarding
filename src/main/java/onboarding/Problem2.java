6446462adac7e5eb064ecf463603b5e49b70f10c
qkdk <86948395+qkdk@users.noreply.github.com>
2022-10-31 19:43:19 +0900

    Feat(Problem2): 초기스택 만드는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> initialStack = new Stack<>();
+
+        makeInitialStack(cryptogram, initialStack);
+
+        return answer.toString();
+    }
+
+
+    private static void makeInitialStack(String cryptogram, Stack<Character> stackOrigin) {
+        stackOrigin.push('A');
+        for (char crypto : cryptogram.toCharArray()) {
+            stackOrigin.push(crypto);
+        }
     }
 }
