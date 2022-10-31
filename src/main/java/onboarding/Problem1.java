249d7fc1bc687393aa43057dc210e85152d770f3
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 22:24:14 +0900

    feat: P1 기능 1 올바른 페이지가 입력 되었는 지 확인 하는 함수 작성 및 결과 상수 필드 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,38 @@ import java.util.List;
  * 기능 3: 두 명의 점수를 비교하여 승패 가르기
  */
 class Problem1 {
+
+    private static final int DRAW = 0;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int EXCEPTION = -1;
+
+    public static boolean isEvenPage(int pageNumber) {
+        return pageNumber % 2 == 0;
+    }
+
+    private static boolean validatePage(List<Integer> pages) {
+
+        int left = pages.get(0), right = pages.get(1);
+
+        if (left < 1 || right > 400) {
+            return false;
+        }
+
+        if (isEvenPage(left) || !isEvenPage(right)) {
+            return false;
+        }
+
+        if (left > right) {
+            return false;
+        }
+
+        if (Math.abs(left - right) != 1) {
+            return false;
+        }
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
