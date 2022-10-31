82e7e15fa5ed88a5efdfbda58c23651b1762e7d8
sujin0607 <goweg0607@gmail.com>
2022-11-01 01:08:40 +0900

    Feat: 추가 문제3
    온보딩 문제3 해결, 문자열의 원래 길이에서 replace를 사용하여 3, 6, 9의 문자가 빠진 문자열의 길이를 빼서 그 차이값을 카운팅

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1; i <= number; i++){
+            String numToStr = Integer.toString(i);
+            answer += numToStr.length() - numToStr.replace("3","").replace("6","").replace("9","").length();
+        }
+        System.out.println(answer);
         return answer;
     }
 }
