d320f5af2476eb202e28d1004a241af1bb5f80d3
박상천 <pswlove38@naver.com>
2022-11-01 07:34:44 +0900

    중복 닉네임 찾는 solution 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,37 @@
 package onboarding;
 
-import java.util.List;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        HashSet<String> emails = new HashSet<>();
+
+        final Map<String, String> hashMap = new HashMap<>();
+
+        for (int i = 0; i < forms.get(1).size(); i++) {
+            final String name = forms.get(1).get(i).toString();
+
+            if (name.length() < 2) {
+                break;
+            }
+
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                final String key = name.substring(j, j + 2);
+                if (hashMap.containsKey(key)) {
+                    final String email = hashMap.get(key);
+                    if (!email.equals(forms.get(0).get(i))) {
+                        emails.add(email);
+                        emails.add(forms.get(0).get(i));
+                    }
+                }
+                hashMap.put(key, forms.get(0).get(i));
+            }
+        }
+        List<String> answer = emails.stream().sorted().collect(Collectors.toList());
         return answer;
     }
 }
