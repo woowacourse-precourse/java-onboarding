1fcc42bc6fef1e1a3c295da2bc316192f9daf51d
한은영 <oneeqzero@gmail.com>
2022-10-31 12:07:36 +0900

    feat: problem7 친구 리스트 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String, Set<String>> friend_list = new HashMap<String, Set<String>> (){
+        @Override
+        public Set<String> get(Object key){
+            Set<String> set = super.get(key);
+            if(set == null){
+                set = new HashSet<String>();
+                put((String) key,set);
+            }
+            return set;
+        }
+    };
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        //make friend list
+        for(List<String>f : friends) {
+            String f1 = f.get(0); String f2 = f.get(1);
+            friend_list.get(f1).add(f2);
+            friend_list.get(f2).add(f1);
+        }
+
         return answer;
     }
 }
