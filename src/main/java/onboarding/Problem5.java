04e63615a395a4c981ed0e8ba3c0d53d0e4d0786
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 22:11:55 +0900

    P5 | 단위가 큰 순서로 처리하여 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = new ArrayList<>();
+
+        int[] change_types = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        while(money > 0) {
+            for(int c : change_types) {
+                if(money >= c) {
+                    int change_count = money / c;
+                    answer.add(change_count);
+                    money -= (c * change_count);
+                }
+                else answer.add(0);
+            }
+        }
+
         return answer;
     }
 }
