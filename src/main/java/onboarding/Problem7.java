af492489a248fff3d76a37e70fb91390ba14532c
sdsr <dlxodnr1212@naver.com>
2022-10-31 13:35:48 +0900

    problem7 기능 2 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,8 +8,12 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         List<String> myfriend = new ArrayList<String>();
+        List<String> friendsfriend = new ArrayList<String>();
 
         myfriend = find_friend(friends);
+        friendsfriend = finds_friend(friends);
+        //친구의 친구에서 user 제거
+        friendsfriend.remove(user);
 
         return answer;
     }
@@ -18,6 +22,15 @@ public class Problem7 {
         List<String> friend = new ArrayList<String>();
         for (int i = 0; i < friends.size(); i++) {
             friend.add(friends.get(i).get(0));
+        }
+        return friend;
+    }
+
+    // 친구의 친구 list 반환
+    public static List<String> finds_friend(List<List<String>> friends) {
+        List<String> friend = new ArrayList<String>();
+        for (int i = 0; i < friends.size(); i++) {
+            friend.add(friends.get(i).get(1));
         }
         return friend;
     }
