11e65ba7771a7f31a9a3e59258db3c13857938f2
cocomozila <joseyun94@naver.com>
2022-10-31 16:53:35 +0900

    [문제 1] 전체 기능 호출

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,9 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    // 전체 기능 호출
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        return -1;
     }
+
 }
