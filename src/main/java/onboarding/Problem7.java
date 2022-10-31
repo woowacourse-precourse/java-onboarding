5c9edfc47ca04e4e4fa24559c94e6f57c6fa15ef
June <68291395+june-777@users.noreply.github.com>
2022-10-31 14:35:00 +0900

    feat: makeAllFriendship 기능 구현
    모든 친구관계를 만드는 기능
    user의 친구는 HashSet으로 관리
    나머지 친구들은 HashMap으로 관리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,54 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
+	static Map<String, List<String>> remainUserAndFriends;
+	static Map<String, Integer> remainUserAndScore;
+	static Set<String> mainUserFriends;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        
+        remainUserAndFriends = new HashMap<>();
+        remainUserAndScore = new HashMap<>();
+        mainUserFriends = new HashSet<>();
+        
+        makeAllFriendship(user, friends);
+        //for debug
+        System.out.println(mainUserFriends);
+        System.out.println(remainUserAndFriends);
+        
         return answer;
+    }
+    public static void makeAllFriendship(String user, List<List<String>> friends) {
+    	
+    	String userA = null;
+    	String userB = null;
+    	for(List<String> friendship: friends) {
+    		userA = friendship.get(0);
+    		userB = friendship.get(1);
+    		
+    		if(userA.equals(user)) {
+    			mainUserFriends.add(userB);
+    		} else if(userB.equals(user)) {
+    			mainUserFriends.add(userA);
+    		} else {
+    			if(!remainUserAndFriends.containsKey(userA)) {
+    				remainUserAndFriends.put(userA, new LinkedList<>());
+    			}
+    			remainUserAndFriends.get(userA).add(userB);
+    			
+    			if(!remainUserAndFriends.containsKey(userB)) {
+    				remainUserAndFriends.put(userB, new LinkedList<>());
+    			}
+    			remainUserAndFriends.get(userB).add(userA);
+    		}
+    	}
     }
 }
