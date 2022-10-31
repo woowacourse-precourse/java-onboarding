e513cf49de16dc8f84ab9dc62c66c79be8b72b64
hyejuc <chjjudy@naver.com>
2022-11-01 05:37:15 +0900

    기능 목록

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        if (checkException(pobi) == false || checkException(crong) == false){
+            return -1;
+        }
+        int answer;
         return answer;
     }
+    public static boolean checkException(List<Integer> player){
+        if(player.get(0) < 1 || player.get(1) < 1 || player.get(0) > 400 || player.get(1) > 400){
+            return false;
+        }
+        if (player.get(0) + 1 != player.get(1)){
+            return false;
+        }
+        return true;
+    }
+    
 }
