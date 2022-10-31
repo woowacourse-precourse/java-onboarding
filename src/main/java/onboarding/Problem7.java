6667952565517709bd8188108e2489c9a3bb4e9d
yoondgu <doyou2ing@naver.com>
2022-10-31 11:29:36 +0900

    feat(Problem7): implement solution method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+
+import onboarding.domain.FriendRecommender;
+import onboarding.domain.FriendRelations;
+import onboarding.domain.MutualDictionary;
+import onboarding.domain.MutualDictionaryGenerator;
+import onboarding.domain.VisitsDictionary;
+import onboarding.domain.VisitsDictionaryGenerator;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        FriendRelations relations = new FriendRelations(friends);
+        MutualDictionaryGenerator mgen = new MutualDictionaryGenerator(relations, user);
+        VisitsDictionaryGenerator vgen = new VisitsDictionaryGenerator(relations, user, visitors);
+        FriendRecommender rc = new FriendRecommender(new MutualDictionary(mgen), new VisitsDictionary(vgen));
+        return rc.getRecommendList();
     }
 }
