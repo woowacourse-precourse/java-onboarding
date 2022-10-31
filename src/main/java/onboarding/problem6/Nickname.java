78d8977a57c17f1c3d809a76d76f98d5f454ef8e
0chil <0@chll.it>
2022-11-01 02:02:22 +0900

    feat(problem6): 닉네임 길이 검증

--- src/main/java/onboarding/problem6/Nickname.java
+++ src/main/java/onboarding/problem6/Nickname.java
@@ -5,6 +5,7 @@ public class Nickname {
 	private final String nickname;
 
 	public Nickname(String nickname) {
+		validateLength(nickname);
 		this.nickname = nickname;
 	}
 
@@ -16,5 +17,14 @@ public class Nickname {
 			}
 		}
 		return false;
+	}
+
+	private void validateLength(String nickname) {
+		if (nickname.length() < 1) {
+			throw new IllegalArgumentException("이름은 1자 이상입니다");
+		}
+		if (nickname.length() >= 20) {
+			throw new IllegalArgumentException("이름은 20자 미만입니다");
+		}
 	}
 }
--- src/test/java/onboarding/problem6/NicknameTest.java
+++ src/test/java/onboarding/problem6/NicknameTest.java
@@ -2,10 +2,15 @@ package onboarding.problem6;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class NicknameTest {
+
+	private static final String NICKNAME_LENGTH_20 = "a".repeat(20);
+	public static final String NICKNAME_LENGTH_0 = "";
 
 	@ParameterizedTest(name = "{0}, {1}에는 중복이 있다 == {2}")
 	@CsvSource(value = {"제이엠,제이슨,true", "가나다라,다,false", "가다가다,가가,false", "가가이슨,나나이슨,true"})
@@ -14,5 +19,12 @@ public class NicknameTest {
 		Nickname otherNickname = new Nickname(otherName);
 
 		assertThat(nickname.hasOverlapOf(otherNickname)).isEqualTo(expected);
+	}
+
+	@Test
+	@DisplayName("닉네임은 1자이상 20자 미만이다")
+	void nickname_has_length_but_less_than_20() {
+		assertThatIllegalArgumentException().isThrownBy(() -> new Nickname(NICKNAME_LENGTH_0));
+		assertThatIllegalArgumentException().isThrownBy(() -> new Nickname(NICKNAME_LENGTH_20));
 	}
 }
