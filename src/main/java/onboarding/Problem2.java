135e050b2239fb52eed1e0bce79529743150f8d7
ttaehee <ttaehee1105@gmail.com>
2022-11-01 00:18:52 +0900

    문제2) feat : 연속 동일문자 체크 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuffer answer = new StringBuffer(cryptogram);
+
+        while(true) {
+            int sameIdx = getSameIdx(answer);
+
+            if(sameIdx == -1) {
+                break;
+            }
+        }
+
+        return answer.toString();
     }
+
+    private static int getSameIdx(StringBuffer string) {
+        char[] charArray = string.toString().toCharArray();
+
+        for(int i=0; i<charArray.length-1; i++) {
+            if(charArray[i] == charArray[i+1]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
