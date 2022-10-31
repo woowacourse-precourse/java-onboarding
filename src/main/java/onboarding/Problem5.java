0aaeed5287a5aaab4529839e9757270dbb9c9719
KimYongJun <rladydwns129@naver.com>
2022-11-01 01:06:15 +0900

    feat(Problem5): Implement all features

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] moneyValue = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < moneyValue.length; i++) {
+            int numberOfMoney = money / moneyValue[i];
+            answer.add(numberOfMoney);
+            money %= moneyValue[i];
+        }
+
         return answer;
     }
+
 }
