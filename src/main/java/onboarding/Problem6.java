b3ffd7e6ae88265829847ec0d016b95339981e24
hayeongKo <polarpheno@icloud.com>
2022-10-31 23:50:28 +0900

    initial settings

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,13 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
     }
+    //public static List<String> Remove nickname deduplication() {}
+    //public static List<String> sortByEmail() {};
 }
