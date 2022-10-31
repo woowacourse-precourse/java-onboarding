5b390c25efe786008705e7f9e30169b361925b19
leesangjin <tkdwls0628@gmail.com>
2022-10-31 19:24:08 +0900

    P6, 닉네임을 2글자씩 슬라이싱한 개수를 map에 저장하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,7 +10,17 @@ public class Problem6 {
     }
 
     private static void initializeSubstringMap(List<List<String>> forms, Map<String, Integer> substringMap) {
-
+        for (List<String> crew : forms) {
+            String nickname = crew.get(1);
+            for (int i = 0; i < nickname.length()-1; i++) {
+                String substring = nickname.substring(i, i + 2);
+                if (substringMap.containsKey(substring)) {
+                    substringMap.put(substring, substringMap.get(substring) + 1);
+                } else{
+                    substringMap.put(substring, 1);
+                }
+            }
+        }
     }
 
     private static void extractDuplicateSubstring(List<String> answer, List<List<String>> forms,
