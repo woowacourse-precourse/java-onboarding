80aa75f753b7ec714bc576680e67ae5d43a7d3d6
HyebinShin <hyeviin.dev@gmail.com>
2022-10-31 19:29:40 +0900

    369게임 1부터 주어진 숫자 number 까지 반복

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // 369게임 1부터 주어진 숫자 number 까지 반복
+        for(int i=1; i<=number; i++) {
+
+        }
+
         return answer;
     }
 }
