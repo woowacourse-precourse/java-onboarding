25654a467c20aee9c9d26caca33d4e9341ab2511
patronum98 <zkrhklkl98@gmail.com>
2022-10-31 21:26:02 +0900

    문제3 기능목록 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+// 기능1. 숫자를 숫자별로 자릿수 분리한다. ex) number=13이면 1,2,3,4,5,6,7,8,9,1,0,1,1,1,2,1,3 으로 분리
+// 기능2. 분리된 숫자들을 리스트 안에 넣는다.
+// 기능2. 리스트 중 3,6,9 가 있으면 answer++ 한뒤 반환한다.
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
