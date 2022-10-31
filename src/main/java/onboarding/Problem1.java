a070dc640801c2d2a35cfe5098978ace1f85b055
mardi2020 <mardi@khu.ac.kr>
2022-10-31 15:12:00 +0900

    feat: Problem1 최댓값 계산 및 비교 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,12 +7,27 @@ import static java.lang.Math.max;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = 0;
         if (abs(pobi.get(0) - pobi.get(1)) != 1 || abs(crong.get(0) - crong.get(1)) != 1) {
             return -1;
         }
+        int pobiLeftMax = calPage(pobi.get(0).toString());
+        int crongLeftMax = calPage(crong.get(0).toString());
 
-        return answer;
+        int pobiRightMax = calPage(pobi.get(1).toString());
+        int crongRightMax = calPage(crong.get(1).toString());
+
+        int pobiMax = max(pobiLeftMax, pobiRightMax);
+        int crongMax = max(crongLeftMax, crongRightMax);
+
+        if (pobiMax < crongMax) {
+            return 2;
+        }
+        else if (pobiMax > crongMax) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 
     private static int calPage(String page) {
