5806527ba0d98ad84a15f172637a752a7c8e1320
KimYongJun <rladydwns129@naver.com>
2022-11-01 01:35:54 +0900

    feat(Problem7): Make map & method to make connection of friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,15 +1,14 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     private static HashSet<String> userFriend;
+    private static HashMap<String, Integer> friendList;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         userFriend = new HashSet<>();
+        friendList = new HashMap<>();
         userFriend.add(user);
 
         for (List l : friends) {
@@ -21,5 +20,22 @@ public class Problem7 {
         }
 
         return answer;
+    }
+
+    private static void addFriendNetwork(String user, String firstPerson, String secondPerson) {
+        if (firstPerson.equals(user)) {
+            userFriend.add(secondPerson);
+            return;
+        }
+        if (secondPerson.equals(user)) {
+            userFriend.add(firstPerson);
+            return;
+        }
+        if (!friendList.containsKey(firstPerson)) {
+            friendList.put(firstPerson, 0);
+        }
+        if (!friendList.containsKey(secondPerson)) {
+            friendList.put(secondPerson, 0);
+        }
     }
 }
