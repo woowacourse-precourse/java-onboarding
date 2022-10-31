388f8f0d71f58099f9062a31544005b364395fd8
juhwang <juhong4930@naver.com>
2022-10-31 17:11:49 +0900

    refactor(Problem2):removing class and refactoring production code

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,64 +5,57 @@ import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        Duplication duplication = new Duplication();
-        while (true){
-            List<Integer[]> duplicateIndexList = duplication.DuplicateStringFind(cryptogram);
-            if(duplicateIndexList.size() == 0){
-                break;
-            }
-            cryptogram = duplication.removeDuplication(cryptogram,duplicateIndexList);
-
+        while (duplicateStringFind(cryptogram).size() != 0) {
+            cryptogram = processCryptogram(cryptogram);
         }
         return cryptogram;
     }
-}
 
+    private static String processCryptogram(String cryptogram) {
+        return removeDuplication(cryptogram, duplicateStringFind(cryptogram));
+    }
 
-class Duplication{
-    public List<Integer[]> DuplicateStringFind(String cryptogram){
+    private static List<Integer[]> duplicateStringFind(String cryptogram) {
         List duplicateIndexList = new ArrayList();
         String[] charList = cryptogram.split("(?<=.)");
-
         Integer[] duplicateIndex = new Integer[2];
         boolean duplicateCheck = false;
 
         for (int i = 0; i < charList.length; i++) {
-            if( i == charList.length-1){
-                if(duplicateCheck){
-
+            if (i == charList.length - 1) {
+                if (duplicateCheck) {
                     duplicateIndex[1] = i;
                     duplicateIndexList.add(duplicateIndex);
                 }
                 break;
             }
-            if(charList[i].equals(charList[i+1]) && !duplicateCheck){
+            if (charList[i].equals(charList[i + 1]) && !duplicateCheck) {
                 duplicateIndex[0] = i;
                 duplicateCheck = true;
-            }
-            else if(!charList[i].equals(charList[i+1]) && duplicateCheck){
+            } else if (!charList[i].equals(charList[i + 1]) && duplicateCheck) {
                 duplicateIndex[1] = i;
                 duplicateIndexList.add(duplicateIndex);
                 duplicateIndex = new Integer[2];
                 duplicateCheck = false;
             }
         }
-
         return duplicateIndexList;
     }
 
-    public String removeDuplication(String cryptogram, List<Integer[]> duplicateIndexList){
-        final String[][] charList = {cryptogram.split("(?<=.)")};
-
+    private static String removeDuplication(String cryptogram, List<Integer[]> duplicateIndexList) {
+        String[] charList = cryptogram.split("(?<=.)");
         duplicateIndexList.forEach(index -> {
-            int start = index[0], stop = index[1];
-            for (int i = start; i < stop+1; i++) {
-                charList[0][i] = "";
-            }
+            removeDuplicationLoop(index, charList);
         });
-
-        return String.join("",charList[0]);
+        return String.join("", charList);
     }
 
-
+    private static void removeDuplicationLoop(Integer[] index, String[] charList) {
+        int start = index[0];
+        int stop = index[1];
+        while (start <= stop) {
+            charList[start] = "";
+            start++;
+        }
+    }
 }
