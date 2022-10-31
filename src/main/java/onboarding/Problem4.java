f260318478ead0068804fee6ad8955ebf379ec3b
송민진 <m_6595@naver.com>
2022-11-01 04:01:26 +0900

    #4 feat: separate given word to chars

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,9 +10,12 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         Map<String, String> frogDict = makeFrogDictionary();
+
+        List<String> charList = new ArrayList<>(List.of(word.split("")));
+
         return answer;
     }
-    
+
     public static Map<String, String> makeFrogDictionary() {
         Map<String, String> frogDict = new HashMap<String, String>();
         List<String> keys = List.of("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
