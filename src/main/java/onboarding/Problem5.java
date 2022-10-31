437be710fa713be8673dbb6a07be906d807c416c
Soseung Soo <sss9073@naver.com>
2022-11-01 03:58:03 +0900

    feat: 'getMoneyList' function

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +9,9 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+    public static ArrayList<Integer> getMoneyList(){
+        ArrayList<Integer> moneyList = new ArrayList<>(List.of(50000,10000,5000,1000,500,100,50,10,1));
+        return moneyList;
     }
 }
