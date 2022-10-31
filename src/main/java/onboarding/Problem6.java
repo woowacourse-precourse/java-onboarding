8cdecedf7e3c1b97acfea31e818e8851fbd7448f
Hongsugyeong <olivia258@naver.com>
2022-10-31 12:45:00 +0900

    [Problem 6] 기능 목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    /***
+     * 닉네임 중복 체크 함수
+     * 중복 제거, 닉네임 오름차순 정렬 함수
+     * 중복되는 이메일 List에 넣는 함수
+     */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
