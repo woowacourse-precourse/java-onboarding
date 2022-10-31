a58614497611698b478ec964f957478479dceaea
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 16:38:10 +0900

    ArrayList import, answer 변수를 ArrayList를 사용하여 재지정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,14 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+
+
+
         return answer;
     }
 }
