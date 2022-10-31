07a2104d0c8a0ebe1f1286e364898d84972cf607
bumwoo <gld55602@gmail.com>
2022-10-31 14:34:49 +0900

    feat(Problem3): 결과 출력 구현
    1부터 합한 박수 개수를 return

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,6 +9,11 @@ public class Problem3 {
 
 	public static int solution(int number) {
 		int answer = 0;
+
+		for (int i = 1; i <= number; i++) {
+			answer += checkNumber(i);
+		}
+
 		return answer;
 	}
 
