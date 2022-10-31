263fe9ccdff20d64d795ee5c268b4d4a486cf682
Jueun <jueun000101@gmail.com>
2022-10-31 20:50:42 +0900

    docs: 5번 기능 목록

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 큰 금액부터 나누어서 몫이 있다면 정답 배열에 담는 함수
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
