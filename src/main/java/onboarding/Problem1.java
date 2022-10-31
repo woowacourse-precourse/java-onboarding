a9be7046edbc3113985c4f8e3bf90f196b9422e4
yeongjin <hanyj0104@naver.com>
2022-11-01 02:57:52 +0900

    더하거나 곱한것중 큰값 구해주는 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,14 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    static int calc(int num) {
+        String numstr = Integer.toString(num);
+        int sum = 0;
+        int mult = 1;
+        for (int i = 0; i < numstr.length(); i++) {
+            sum += (numstr.charAt(i) - '0');
+            mult *= (numstr.charAt(i) - '0');
+        }
+        return Math.max(sum, mult);
+    }
 }
