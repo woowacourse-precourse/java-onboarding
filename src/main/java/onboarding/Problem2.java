646a66c0541a6a35ba62efe08a3083b005cdb17e
JDK <ehdrjf2062@gmail.com>
2022-10-31 13:23:42 +0900

    refactor(문제2): 3개 이상 중복문자 대응 코드 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,35 +1,67 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
 	public static String solution(String cryptogram) {
-		
-		String crypto = cryptogram;
-		
 		while (true) {
-			String decoded = decodeLogic(crypto);
+			ArrayList<Integer[]> indexPair = new ArrayList<>();
+			boolean changeFlag = false;
 			
-			// 이전값과 비교
-			if (crypto.equals(decoded)) {
-				crypto = decoded;
-				break;
-			} else {
-				crypto = decoded;
-			}
-		}
-		
-		return crypto;
-	}
-	
-	private static String decodeLogic(String cryptogram) {
-		for (int i = 0; i < cryptogram.length() - 1; i++) {
-			if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
-				String beforeStr = cryptogram.substring(0, i);
-				String afterStr = cryptogram.substring(i + 2);
+			// 역순
+			for (int end = cryptogram.length() - 1; end > 0; end--) {
 				
-				cryptogram = beforeStr + afterStr;
-				i--;
+				// 1. 중복되는 글자가 2글자 이상인가? boolean (중복되는 글자가 없으면 continue)
+				if (cryptogram.charAt(end) != cryptogram.charAt(end - 1)) {
+					continue;
+				}
+				changeFlag = true;
+				
+				// 2. 중복되는 글자의 인덱스는 어디 어디인가?
+				int start = findStartIndex(cryptogram, end);
+				
+				// 3. 한사이클에서 중복되는 글자 저장
+				Integer[] pair = {start, end};
+				indexPair.add(pair);
+				
+				// 4. 중복된 값 아래부터 시작
+				end = start;
 			}
+			
+			if (!changeFlag) {
+				break;
+			}
+			
+			for (Integer[] pair : indexPair) {
+				cryptogram = substringCrytogram(cryptogram, pair[0], pair[1] + 1);
+			}
+			
 		}
 		return cryptogram;
 	}
+	
+	private static String substringCrytogram(String cryptogram, int start, int end) {
+		
+		String beforeTarget = cryptogram.substring(0, start);
+		String afterTarget = cryptogram.substring(end);
+		String result = beforeTarget.concat(afterTarget);
+		
+		return result;
+	}
+	
+	private static int findStartIndex(String cryptogram, int lastIndex) {
+		int firstIndex = lastIndex - 1;
+		for (int i = lastIndex - 1; i >= 0; i--) {
+			if (cryptogram.charAt(i) != cryptogram.charAt(lastIndex)) {
+				firstIndex = i + 1;
+				break;
+			}
+			if (i == 0) {
+				firstIndex = 0;
+				break;
+			}
+		}
+		return firstIndex;
+	}
+	
 }
