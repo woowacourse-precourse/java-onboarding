06d7887b286ac3683cc483a9e26346784a8128a7
DongWooE <ehddn977@naver.com>
2022-11-01 04:30:52 +0900

    [p4][add] 문자열을 순회하는  iterate 메소드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+
+    StringBuilder result = new StringBuilder();
     public static String solution(String word) {
         String answer = "";
+        Problem4 p4 = new Problem4();
+        p4.iterate(word);
         return answer;
+    }
+
+    private void iterate(String word) {
+        for(char c : word.toCharArray()){
+        }
     }
 }
