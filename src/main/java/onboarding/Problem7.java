66eeafbd86297b9ab3773ee801faea9f2b36406b
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-31 16:07:15 +0900

    feat: PROBLEM7 추천 리스트 도출 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem7 {
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -17,12 +19,17 @@ public class Problem7 {
 			sns.addRelation(relation);
 		}
 
+		for (String visitor : visitors) {
+			sns.initUser(visitor);
+		}
+
 		User targetUser = sns.findUserById(user)
 				.orElseThrow(() -> new RuntimeException("NotFoundUser"));
 
 		targetUser.addVisitors(visitors);
 
-		return null;
+		Recommender recommender = new Recommender();
+		return recommender.getRecommendList(user);
 	}
 
 	static class User {
@@ -45,6 +52,18 @@ public class Problem7 {
 				Integer visitScore = visitorMap.getOrDefault(visitor, 0);
 				visitorMap.put(visitor, visitScore + 1);
 			}
+		}
+
+		public Set<User> getFriendSet() {
+			return friendSet;
+		}
+
+		public Map<String, Integer> getVisitorMap() {
+			return visitorMap;
+		}
+
+		public String getId() {
+			return id;
 		}
 	}
 
@@ -78,6 +97,67 @@ public class Problem7 {
 
 			user1.addFriend(user2);
 			user2.addFriend(user1);
+		}
+
+		public Map<String, User> getUserMap() {
+			return userMap;
+		}
+	}
+
+	static class Recommender {
+
+		private Sns sns;
+
+		public Recommender() {
+			this.sns = new Sns();
+		}
+
+		public List<String> getRecommendList(String targetId) {
+			User targetUser = sns.findUserById(targetId)
+					.orElseThrow(() -> new RuntimeException("NotFoundUser"));
+
+			Map<String, User> userMap = sns.getUserMap();
+			Set<User> friendSet = targetUser.getFriendSet();
+			Map<String, Integer> visitorMap = targetUser.getVisitorMap();
+
+			Map<String, Integer> recommendScoreMap = new HashMap<>();
+			for (String userId : userMap.keySet()) {
+				User user = userMap.get(userId);
+				if (isFriends(targetUser, user) || targetUser.equals(user)) {
+					continue;
+				} else {
+					int score = 0;
+
+					for (User friend : friendSet) {
+						if (isFriends(user, friend)) {
+							score += 10;
+						}
+					}
+
+					score += visitorMap.getOrDefault(userId, 0);
+					recommendScoreMap.put(userId, score);
+				}
+			}
+
+			List<String> recommendList = recommendScoreMap
+					.entrySet()
+					.stream()
+					.filter(entry -> entry.getValue() > 0)
+					.sorted(Map.Entry.comparingByKey())
+					.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+					.map(Map.Entry::getKey)
+					.collect(Collectors.toList());
+
+
+			if (recommendList.size() > 5) {
+				return recommendList.subList(0, 5);
+			} else {
+				return recommendList;
+			}
+		}
+
+		private boolean isFriends(User user, User friend) {
+			return user.getFriendSet().contains(friend);
 		}
 	}
 }
