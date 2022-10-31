546ad3d2151ce68b62ecc9f388852b27c5407ffb
parkjungyoon <tbr06057@naver.com>
2022-11-01 03:20:17 +0900

    feat(PROBLEM7): 친구 찾는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> userFriends = findFriend(user, friends);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> findFriend (String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
+
+        for(List<String> friend : friends) {
+            int friendIdx = friend.indexOf(user);
+            if (friendIdx == 0) {
+                friendList.add(friend.get(1));
+            } else if (friendIdx == 1) {
+                friendList.add(friend.get(0));
+            }
+        }
+        return friendList;
     }
 }
