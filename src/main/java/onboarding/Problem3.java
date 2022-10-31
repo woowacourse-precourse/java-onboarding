61f16f481c0ec10ce6d4774e219854d6d5026f79
wugawuga <dnr3655@naver.com>
2022-10-31 14:17:07 +0900

    feat: 입력 받은 수만큼 반복

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            String value = String.valueOf(i);
+        }
         return answer;
     }
 }
