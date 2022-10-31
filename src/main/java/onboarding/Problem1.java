f99efbb94cd0369a6043d64264e6ea8867f42b62
codingvegeta <hong090992@gmail.com>
2022-10-31 19:45:24 +0900

    feat: score 메서드를 만들었습니다. score는 포비나 크롱의 계산 중 최댓값을 구하는 메서드 입니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,20 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    public static int score(List<Integer> list){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            char[] chars = list.get(i).toString().toCharArray();
+
+            int a = 0;
+            int b = 1;
+            for (char cha:chars) {
+                a += cha - '0';
+                b *= cha - '0';
+            }
+            max = Integer.max(max, a);
+            max = Integer.max(max, b);
+        }
+        return max;
+    }
 }
