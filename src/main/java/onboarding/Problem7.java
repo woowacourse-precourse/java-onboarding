0bb97d3ff1424ea1165e04c66096142e2781155a
chopinballadeno4 <qo98333@naver.com>
2022-10-31 23:50:27 +0900

    problem 7, function 1 parameter Exception

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static boolean checkParameter(String user, List<List<String>> friends, List<String> visitors) {
+        //user는 길이가 1 이상 30 이하인 문자열이다.
+        if(user.length() < 1 || user.length() > 30) {
+            return false;
+        }
+
+        //friends는 길이가 1 이상 10,000 이하인 리스트/배열이다.
+        if(friends.size() < 1 || friends.size() > 10000) {
+            return false;
+        }
+
+        //visitors는 길이가 0 이상 10,000 이하인 리스트/배열이다.
+        if(visitors.size() > 10000) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        return answer;
+        if(!checkParameter(user, friends, visitors)) {
+            return answer;
+        }
+
     }
 }
