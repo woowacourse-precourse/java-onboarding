940c22345a32ffad92779e3f715b757f30570aa9
sysliar <sysliar@gmail.com>
2022-10-31 16:38:10 +0900

    Q7. add addFriends method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,4 +12,18 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    private static void addFriends(List<String> friend, String user) {
+        if (!friend.get(0).equals(user)) points.put(friend.get(0), 0);
+        if (!friend.get(1).equals(user)) points.put(friend.get(1), 0);
+
+        if (!friendsMap.containsKey(friend.get(0))) friendsMap.put(friend.get(0), new HashSet<>());
+        if (!friendsMap.containsKey(friend.get(1))) friendsMap.put(friend.get(1), new HashSet<>());
+
+        friendsMap.get(friend.get(1)).add(friend.get(0));
+        friendsMap.get(friend.get(0)).add(friend.get(1));
+
+        return;
+    }
+
 }
