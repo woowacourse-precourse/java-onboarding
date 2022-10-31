168660018b4cb945a775cd5e8ada6e8ac4aa7996
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 22:37:00 +0900

    P6 | 결과 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = duplicatedCrewEmail(forms);
         return answer;
     }
+    private static List<String> duplicatedCrewEmail(List<List<String>> forms) {
+
+        Set<String> e = (Set<String>) duplicatedCrewEmail(forms);
+        List<String> emailList = new ArrayList<>(e);
+
+        sortEmailList(emailList);
+
+        return emailList;
+    }
+
+
 }
