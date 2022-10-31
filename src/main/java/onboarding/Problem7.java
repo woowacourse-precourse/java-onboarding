6fd2cf168bbcb7b3922fd299089d5c76e90ca932
kimjaehwi1 <meatandcoffee@naver.com>
2022-10-31 18:26:51 +0900

    7번문제 모든 유저 리스트 메소드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        List<String> result = userlist(friends,visitors);
+
+
+
+        return result;
+    }
+
+    public static List<String> userlist (List<List<String>> friends, List<String> visitors) {
+        List<String> userdata = new ArrayList<>();
+        for(int i=0;i< friends.size();i++) {
+            userdata.add(friends.get(i).get(0));
+            userdata.add(friends.get(i).get(1));
+
+        }
+        for(int j=0;j< visitors.size();j++) {
+            userdata.add(visitors.get(j));
+        }
+        return overlap(userdata);
+    }
+
+    public static List<String> overlap (List<String> list) {
+        Set<String> set = new HashSet<>(list);
+        list = new ArrayList<>(set);
+
+        return list;
     }
 }
