25a057ccf18df1120f6ff703bf3ee48fd0a05a4e
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:33:33 +0900

    [Problem3] 문자 배열에서 3, 6, 9인 값 찾아서 1 더해주기(김민서)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,6 +12,10 @@ public class Problem3 {
         for(int i = 1; i <= number; i++) {
             // 숫자를 문자 배열로 만들기
             for(char c : String.valueOf(i).toCharArray()) {
+                // 문자 배열에서 3, 6, 9인 값 찾아서 1 더해주기
+                if(c == '3' || c == '6' || c == '9') {
+                    answer += 1;
+                }
             }
         }
 
