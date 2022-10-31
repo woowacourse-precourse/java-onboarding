906e582157a028b36f39ae85de3d0f9565934114
making-a-scene <4936joo@naver.com>
2022-10-31 14:52:02 +0900

    problem 6 listToMap()

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static Map<String, String> listToMap(List<List<String>> forms) {
+        Map<String, String> formsMap = new HashMap<>();
+        for(List<String> form: forms) {
+            formsMap.put(form.get(1), form.get(0));
+        }
+        return formsMap;
+    }
+
 }
