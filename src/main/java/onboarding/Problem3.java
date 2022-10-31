d7602399b47bd1b3014f6bbc439d72498fb5f5cc
hongjinwon <herenever@naver.com>
2022-11-01 00:26:41 +0900

    feat: Problem3 getDigitList, count, checkCondition 추가
    - Problem3 getDigitList, count, checkCondition 추가
    - stream의 성능에 대한 검토 필요

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public static List<Integer> getDigitList(int num){
+        return Stream.of(String.valueOf(num).split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public static void count(int count,List<Integer> digitList){
+        for (Integer digit : digitList) {
+            checkCondition(count,digit);
+        }
+    }
+
+    public static void checkCondition(int count,int digit){
+        if (digit % 3 ==0){
+            count++;
+        }
     }
 }
