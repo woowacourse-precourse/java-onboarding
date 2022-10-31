478d79e7e0e7b6f139efb08f0989452cb30043c5
yxnxx <jh4rgh0@gmail.com>
2022-11-01 04:21:47 +0900

    feat: Add Problem7 calculating visit score

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,6 +16,13 @@ public class Problem7 {
             count = relationshipScore(relation.get(name), relation.get(user));
             score.put(name, count*10);
         }
+
+        int visitscore = 0;
+        for (String visitor : visitors) {
+            visitscore = score.getOrDefault(score.get(visitor), 0);
+            visitscore++;
+            score.put(visitor, visitscore);
+        }
         //return answer;
     }
     public static void findRelationship(HashMap<String, HashSet<String>> relation, List<String> friends) {
@@ -24,8 +31,11 @@ public class Problem7 {
 
         HashSet<String> friend_list = relation.getOrDefault(friend1, new HashSet<>());
         friend_list.add(friend2);
+        relation.put(friend1, friend_list);
+
         friend_list = relation.getOrDefault(friend2, new HashSet<>());
         friend_list.add(friend1);
+        relation.put(friend2, friend_list);
     }
     public static Integer relationshipScore(HashSet<String> friends, HashSet<String> userfriend) {
         int count = 0;
