906cce8dd1209d757ede48c001cab07227209440
mardi2020 <mardi@khu.ac.kr>
2022-10-31 15:14:04 +0900

    feat: Problem2 stack 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,12 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        return answer.toString();
     }
 }
