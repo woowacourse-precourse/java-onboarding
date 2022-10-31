8be7632e645e6278e165361fbac65d0220675adc
wogus4048 <wogus4048@naver.com>
2022-10-31 19:22:35 +0900

    feat(Problem4): 대문자인지 소문자인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
+
+    public static boolean checkUpperCase(char input) {
+        return input >= 65 && input <= 90;
+    }
+
+    public static boolean checkLowerCase(char input) {
+        return input >= 97 && input <= 122;
+    }
+
+
 }
