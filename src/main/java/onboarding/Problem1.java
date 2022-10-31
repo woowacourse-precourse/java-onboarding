f7c935c268358183edf061ca1c829d8aff668638
Amenable-C <duswo0624@naver.com>
2022-11-01 00:32:38 +0900

    feat : [문제1] 가장 높은 값들을 비교

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
@@ -37,7 +38,6 @@ class Problem1 {
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
         int pobi_left = pobi.get(0);
         int pobi_right = pobi.get(1);
         int crong_left = crong.get(0);
@@ -45,6 +45,32 @@ class Problem1 {
 
         if(exception_handling(pobi_left, pobi_right, crong_left, crong_right))
             return -1;
+
+        int pobi_left_each_sum = add_each_digit(pobi_left);
+        int pobi_right_each_sum = add_each_digit(pobi_right);
+        int pobi_left_each_mul = mul_each_digit(pobi_left);
+        int pobi_right_each_mul = mul_each_digit(pobi_right);
+
+        List<Integer> pobi_list = List.of(pobi_left_each_sum, pobi_right_each_sum, pobi_left_each_mul, pobi_right_each_mul);
+        int pobi_max = Collections.max(pobi_list);
+
+
+        int crong_left_each_sum = add_each_digit(crong_left);
+        int crong_right_each_sum = add_each_digit(crong_right);
+        int crong_left_each_mul = mul_each_digit(crong_left);
+        int crong_right_each_mul = mul_each_digit(crong_right);
+
+        List<Integer> crong_list = List.of(crong_left_each_sum, crong_right_each_sum, crong_left_each_mul, crong_right_each_mul);
+        int crong_max = Collections.max(crong_list);
+
+        int answer = 0;
+        if(pobi_max > crong_max)
+            answer = 1;
+        else if (pobi_max < crong_max) {
+            answer = 2;
+        } else if (pobi_max == crong_max) {
+            answer = 0;
+        }
 
         return answer;
     }
