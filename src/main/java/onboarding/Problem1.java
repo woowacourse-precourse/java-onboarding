d6d2acf71eabd2c097c42b5cb1f950e1cd1d3723
cocomozila <joseyun94@naver.com>
2022-10-31 16:54:48 +0900

    [문제 1] 예외처리 기능 틀 구상

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,18 @@ import java.util.List;
 class Problem1 {
     // 전체 기능 호출
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (checkValidPage(pobi, crong)) {
+            return 0;
+        }
         return -1;
     }
 
+    // 예외처리 기능
+    @SafeVarargs
+    private static boolean checkValidPage(List<Integer>...list) {
+        for (int i = 0; i < list.length; i++) {
+            //예외처리 들어갈 곳
+        }
+        return true;
+    }
 }
