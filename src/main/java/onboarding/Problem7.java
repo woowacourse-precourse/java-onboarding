7e01d94dd838ab4e9c362bcf5d7412c183ddec41
phobyjun <phobyjun@gmail.com>
2022-11-01 01:03:25 +0900

    [PROBLEM7] ADD: FEATURE2. 사용자의 추천 친구 목록 생성 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
@@ -23,5 +24,28 @@ public class Problem7 {
             }
         }
         return friendsOfUser;
+    }
+
+    public static List<String> getRecommendFriends(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> recommendFriends = new ArrayList<>(List.of());
+
+        List<String> friendsOfUser = getFriendsOfUser(user, friends);
+
+        for (List<String> relation : friends) {
+            String userA = relation.get(0);
+            String userB = relation.get(1);
+
+            if (!(friendsOfUser.contains(userA) || userA.equals(user)))
+                recommendFriends.add(userA);
+            if (!(friendsOfUser.contains(userB) || userB.equals(user)))
+                recommendFriends.add(userB);
+        }
+
+        for (String visitor : visitors) {
+            if (!(friendsOfUser.contains(visitor)))
+                recommendFriends.add(visitor);
+        }
+
+        return new ArrayList<>(new HashSet<>(recommendFriends));
     }
 }
