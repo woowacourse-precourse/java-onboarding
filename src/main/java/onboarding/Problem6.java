68a6e8eca1f972597f16ebec2d66e2f31d4b69d6
JaeUpSu <hyeongs2323@gmail.com>
2022-10-31 18:32:12 +0900

    PROBLEM6 | 중복될 수 있는 키, 두글자 단위로 Map 에 입력

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
@@ -7,9 +8,34 @@ public class Problem6 {
         List<String> answer = List.of("answer");
 
         User[] crew = inItUsers(forms);
+        HashMap<String, Integer> crewNickMap = getCandidateNicks(crew);
+
 
         return answer;
     }
+    private static HashMap<String, Integer> getCandidateNicks(User[] crews) {
+
+        int size = crews.length;
+        HashMap<String, Integer> duplicateMap = new HashMap<>();
+
+        for (int i = 0; i < size; i++) {
+
+            String nick = crews[i].getNick();
+            int nickLength = nick.length();
+
+            for (int j = 0; j < nickLength - 1; j++) {
+                String twoLetters = nick.substring(j, j + 2);
+
+                if (duplicateMap.containsKey(twoLetters)) {
+                    continue;
+                }
+                duplicateMap.put(twoLetters, 0);
+            }
+        }
+
+        return duplicateMap;
+    }
+
     private static User[] inItUsers(List<List<String>> forms) {
 
         int size = forms.size();
