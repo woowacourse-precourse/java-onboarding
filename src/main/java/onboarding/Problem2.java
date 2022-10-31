8e6ff0b2746752d06cd338206a9b612b5d5d4c14
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2022-10-31 23:00:12 +0900

    feat: Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,33 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int i;
+        int cnt = 0;
+        boolean hasDuplicatedChar;
+
+        do {
+            hasDuplicatedChar = false;
+            answer = "";
+
+            for (i = 0; i < cryptogram.length()-1; i++) {
+                if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                    cnt++;
+                    hasDuplicatedChar = true;
+                } else if (cnt > 0) {
+                    cnt = 0;
+                } else {
+                    answer = answer + cryptogram.charAt(i);
+                }
+            }
+
+            if (cnt == 0) {
+                answer = answer + cryptogram.charAt(i);
+            }
+
+            cryptogram = answer;
+
+        } while (hasDuplicatedChar);
+
         return answer;
     }
 }
