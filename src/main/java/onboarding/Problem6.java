8c8a7df8982a97531ce8b4bb9b3f1a3f866e9dd9
Amenable-C <duswo0624@naver.com>
2022-11-01 00:44:44 +0900

    feat : [문제6] 각 닉네임을 두 글자 단위로 중복 횟수 카운팅

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        HashMap<String, Long> map = new HashMap<>();
+        for(List<String> form : forms){
+            String id = form.get(1);
+            for(int j = 0; j < id.length() - 1; j++){
+                String s = id.substring(j, j + 2);
+                map.put(s, map.getOrDefault(s, 0L) + 1L);
+            }
+        }
+
         return answer;
     }
 }
