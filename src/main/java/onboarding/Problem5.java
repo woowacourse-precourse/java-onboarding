ed4deb64438e254ce129603e8bba38c0bbc2b3b1
kch4731 <kch4731@naver.com>
2022-10-31 23:59:25 +0900

    [Refactor : make clean code by use List]

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,55 +1,17 @@
 package onboarding;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-//        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-        List<Integer> answer = new ArrayList<Integer>();
-        int money50000, money10000, money5000, money1000, money500, money100, money50, money10, money1;
-
-        money50000 = money / 50000;
-        money -= money50000 * 50000;
-
-        money10000 = money / 10000;
-        money -= money10000 * 10000;
-
-        money5000 = money / 5000;
-        money -= money5000 * 5000;
-
-        money1000 = money / 1000;
-        money -= money1000 * 1000;
-
-        money500 = money / 500;
-        money -= money500 * 500;
-
-        money100 = money / 100;
-        money -= money100 * 100;
-
-        money50 = money / 50;
-        money -= money50 * 50;
-
-        money10 = money / 10;
-        money -= money10 * 10;
-
-        money1 = money % 10;
-
-        answer.add(money50000);
-        answer.add(money10000);
-        answer.add(money5000);
-        answer.add(money1000);
-        answer.add(money500);
-        answer.add(money100);
-        answer.add(money50);
-        answer.add(money10);
-        answer.add(money1);
-
+        for (int i = 0; i < 9; i++) {
+            answer.set(i, money / moneyList[i]);
+            money = money - moneyList[i] * answer.get(i);
+        }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(solution(50237));
     }
 }
