7234a102f48c619ba508f4ccb456a31ffb3760a9
cocomozila <joseyun94@naver.com>
2022-11-01 01:47:28 +0900

    [문제 6] 본인 닉네임안의 중복은 중복되는걸 제외

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,9 +32,13 @@ public class Problem6 {
     }
     // 닉네임을 2글자씩 분리하는 기능
     private static void splitNickmame(String nickName, String email) {
+        // 본인 닉네임은 중복되는걸 제외
+        HashSet<String> nickmameSplitList = new HashSet<>();
         for (int j = 0; j < nickName.length()-1; j++) {
             String partOfNickname = nickName.substring(j, j+2);
-            checkPartOfNickname(partOfNickname, email);
+            if (nickmameSplitList.add(partOfNickname)) {
+                checkPartOfNickname(partOfNickname, email);
+            }
         }
     }
     // 분리한 닉네임이 중복인지 확인하는 기능
