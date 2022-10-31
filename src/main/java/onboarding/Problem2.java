73b4bbcec056e919a6c6bff9a0e01aba08d5f746
이지영 <wldudwldhd@naver.com>
2022-10-31 21:37:29 +0900

    docs:기능구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
-
+/*기능구현
+1. 중복되는 연속적인 인덱스 2개 찾고 삭제하는 함수
+2. 위의 함수가 더이상 작동하지 않을 때(즉, length() == 0 이거나 중복되는 것이 없을 경우) 정답을 반환
+* */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+
+
+
         return answer;
     }
 }
