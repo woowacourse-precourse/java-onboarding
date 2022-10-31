67312eb980b7b8d8e9ab9288cfb21cc11cd69e63
LeeJejune <leejejune0625@gmail.com>
2022-10-31 14:45:44 +0900

    feat: [7번] 방문자 목록 중 방문자 점수를 계산하는 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,6 +7,12 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    private static void calVisitorScore(String user, List<String> visitors, Map<String, List<String>> friendsMap,
+        Map<String, Integer> scoreMap) {
+        visitors.stream().filter(visitor -> !friendsMap.get(user).contains(visitor))
+            .forEach(visitor -> scoreMap.put(visitor, scoreMap.getOrDefault(visitor, 0) + 1));
+    }
+
     private static void calFriendScore(String user, Map<String, List<String>> friendsMap, Map<String, Integer> scoreMap) {
         for (String name : friendsMap.get(user)) {
             for (String recommend : friendsMap.get(name)) {
