fcaf7a95d0c3e7b8d84516d221c9b42341570244
can019 <jys01012@gmail.com>
2022-11-01 05:10:47 +0900

    :construction: 화폐단위를 담는 배열 정의/할당 및 solution5 해결을 위한 핵심 method 정의

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int [] moneyUnit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10};
         return answer;
+    }
+
+    private static List<Integer> method(int money, int[] moneyUnit,int index, List<Integer> result){
+        // @TODO :: 재귀로 돌아 answer return
+        return result;
     }
 }
