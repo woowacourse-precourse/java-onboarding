a89efc2cf154ee76a2bdc412f427909098f02eaa
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:05:49 +0900

    4번: 반대 문자 구하는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
         return answer;
+    }
+
+    private static char getOppositeAlpha(char targetChar){
+        int asciiOrder = targetChar - 'a';
+        int convertOrder = 'z' - asciiOrder;
+
+        char convertChar = (char)convertOrder;
+        System.out.println(convertChar);
+        return convertChar;
     }
 }
