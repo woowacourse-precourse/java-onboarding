fe46fcc233ca03801c1a977d7432093365652994
euna <christinejung10@daum.net>
2022-10-31 21:06:30 +0900

    feat(Problem7): getAnswer, sortResult 함수 추가
    getAnswer() : 점수 기준 내림차순 정렬 후, 추천 친구 목록을 반환하는 함수
    sortResult 함수를 호출하는 것을 통해 추천 점수가 같은 경우, 이름 기준 오름차순으로 정렬하여 반환
    
    sortResult() : 추천 친구 목록을 반환하는 함수 (List)
    점수가 같은 경우는 이름 기준 오름차순 정렬을 하여 최종 추천 친구 목록을 List 형태로 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -45,8 +45,8 @@ public class Problem7 {
             }
         }
 
-//        answer = getAnswer(scoreMap);
-
+        // 추천 친구 목록
+        answer = getAnswer(scoreMap);
         return answer;
     }
 
@@ -103,6 +103,74 @@ public class Problem7 {
         }
 
         return score;
+    }
+
+    // 점수 기준 내림차순 정렬 후, 추천 친구 목록을 반환하는 함수
+    // sortResult 함수를 호출하는 것을 통해 추천 점수가 같은 경우, 이름 기준 오름차순으로 정렬하여 반환
+    private static List<String> getAnswer(HashMap<String, Integer> scoreMap) {
+        // 점수(value) 기준 내림차순 정렬
+        List<Map.Entry<String, Integer>> sortScore = new LinkedList<>(scoreMap.entrySet());
+        sortScore.sort((o1, o2) -> o2.getValue() - o1.getValue());
+
+        // 추천 친구로 반환할 목록 (Map)
+        // 추천 점수가 같은 경우에는 이름 기준 오름차순으로 정렬해야 하기 때문에 우선 Map으로 저장 (key : username / value : score)
+        List<Map.Entry<String, Integer>> resultMap = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : sortScore) {
+            if (entry.getValue() == 0) { // 추천 점수가 0점인 경우 추천하지 않음
+                break;
+            }
+            resultMap.add(entry);
+            if (resultMap.size() == 5) { // 최대 5명을 뽑음
+                break;
+            }
+        }
+
+        // 추천 친구 목록 반환 (점수가 같은 경우는 이름 기준 오름차순 정렬)
+        return sortResult(resultMap);
+    }
+
+    // 추천 친구 목록을 반환하는 함수 (List)
+    // 점수가 같은 경우는 이름 기준 오름차순 정렬
+    private static List<String> sortResult(List<Map.Entry<String, Integer>> resultMap) {
+        List<String> result = new ArrayList<>(); // 추천 친구 목록
+
+        Set<String> sameScoreUsers = new HashSet<>(); // 점수가 같은 유저들의 이름을 저장하는 List
+        boolean hasSame = false; // 이전에 점수가 같은 유저들이 있었는지 확인하기 위한 변수 (true : 이전에 점수가 같은 유저들이 존재 / false : 존재하지 않았음)
+
+        for (int i = 1; i < resultMap.size(); i++) {
+            // 유저 이름
+            String user1_username = resultMap.get(i - 1).getKey();
+            String user2_username = resultMap.get(i).getKey();
+            // 점수
+            int user1_score = resultMap.get(i - 1).getValue();
+            int user2_score = resultMap.get(i).getValue();
+
+            // 두 유저의 점수가 같았을 경우 -> sameScoreUsers에 둘 다 저장
+            if (user1_score == user2_score) {
+                sameScoreUsers.add(user1_username);
+                sameScoreUsers.add(user2_username);
+                hasSame = true; // 점수가 같은 유저가 있음을 표시
+            }
+
+            // 두 유저의 점수가 달랐을 경우
+            // 1. 이전에 점수가 같은 유저들이 있었다면 -> sameScoreUsers에 있던 값들을 오름차순 정렬한 후 result에 전부 추가
+            if (user1_score != user2_score && hasSame) {
+                List<String> userList = new ArrayList<>(sameScoreUsers); // sameScoreUsers set에 저장되어 있던 값들을 가져오기
+                Collections.sort(userList); // 이름순 오름차순 정렬
+                result.addAll(userList);
+                sameScoreUsers = new HashSet<>(); // sameScoreUsers 초기화
+            }
+            // 2. 이전에 점수가 같은 유저들이 없었다면 -> result에 user1의 이름을 저장
+            if (user1_score != user2_score && !hasSame) {
+                result.add(user1_username);
+            }
+            // 3. 마지막 for문이었다면 -> result에 user2의 이름을 저장
+            if (user1_score != user2_score &&i == resultMap.size() - 1) {
+                result.add(user2_username);
+            }
+        }
+
+        return result;
     }
 
 }
