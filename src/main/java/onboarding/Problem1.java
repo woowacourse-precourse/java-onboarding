f4e75c7193c3f4f0e27960ec1bc5c20795b0dfbe
ChaCha3088 <cha3088@gmail.com>
2022-10-31 09:41:59 +0900

    feat: problem1 delete function drawTwoNumbersInRow

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -28,15 +28,6 @@ class Problem1 {
         }
     }
 
-    private static ArrayList<Integer> drawTwoNumbersInRow() {
-        Random random = new Random(); //랜덤 객체 생성(디폴트 시드값 : 현재시간)
-        random.setSeed(System.currentTimeMillis());
-        int randomNumber = 2 * (random.nextInt(197) + 1) + 1;
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(randomNumber, randomNumber + 1);
-        return result;
-    }
-
     private static int[] splitEachNumbers(int number) {
         int[] arrNum = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
         return arrNum;
