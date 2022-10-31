3d304a24a305231d0a02db35e2eca0c44aedc657
youngkyunIm <yee0230@naver.com>
2022-10-31 14:57:06 +0900

    refactor(Problem6) : getContainsSet -> method

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,20 +8,32 @@ public class Problem6 {
         Set<String> set = new HashSet<>();
 
         for (int i = 0; i < forms.size(); i++) {
-            List<String> info = forms.get(i);
-            String nickname = info.get(1);
-            for (int j = 0; j < nickname.length() - 1; j++) {
-                String tmp = String.valueOf(nickname.charAt(j)) + (nickname.charAt(j + 1));
-                for (int q = 0; q < forms.size(); q++) {
-                    if (i == q) continue;
-                    String check = forms.get(q).get(1);
-                    if (check.contains(tmp)) set.add(forms.get(q).get(0));
-                }
-            }
+            set.addAll(getContainsSet(forms, i));
         }
 
         answer.addAll(set);
         Collections.sort(answer, String.CASE_INSENSITIVE_ORDER);
         return answer;
+    }
+
+    private static Set<String> getContainsSet(List<List<String>> forms, int i) {
+        Set<String> containsSet = new HashSet<>();
+        List<String> info = forms.get(i);
+        String nickname = info.get(1);
+
+        for (int j = 0; j < nickname.length() - 1; j++) {
+            String tmp = String.valueOf(nickname.charAt(j)) + (nickname.charAt(j + 1));
+            for (int q = 0; q < forms.size(); q++) {
+                if (i == q) continue;
+
+                List<String> targetList = forms.get(q);
+                String targetNickname = targetList.get(1);
+                String targetEmail = targetList.get(0);
+
+                if (targetNickname.contains(tmp)) containsSet.add(targetEmail);
+            }
+        }
+
+        return containsSet;
     }
 }
