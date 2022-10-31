370c5566a039fe3315ba51f69f9baaca5e9365b6
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-31 23:47:06 +0900

    feat: 5번 예외처리 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,10 +7,28 @@ import java.util.List;
 public class Problem5 {
 
     static final int[] MONEY_UNIT = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    static final int INPUT_MINIMUM_AMOUNT = 1;
+    static final int INPUT_MAXIMUM_AMOUNT = 1000000;
+    static final String INPUT_TOO_SMALL_EXCEPTION = "입력한 금액이 1보다 작습니다. ";
+    static final String INPUT_TOO_BIG_EXCEPTION = "입력한 금액이 1,000,000보다 큽니다. ";
 
 
     public static List<Integer> solution(int money) {
+        checkMoney(money);
         return countCurrencies(money);
+    }
+
+    private static void checkMoney(int money){
+        if(money < INPUT_MINIMUM_AMOUNT) inputTooSmallException();
+        if(money > INPUT_MAXIMUM_AMOUNT) inputTooBigException();
+    }
+
+    private static void inputTooSmallException(){
+        throw new IllegalArgumentException(INPUT_TOO_SMALL_EXCEPTION);
+    }
+
+    private static void inputTooBigException(){
+        throw new IllegalArgumentException(INPUT_TOO_BIG_EXCEPTION);
     }
 
     private static List<Integer> countCurrencies(int money){
