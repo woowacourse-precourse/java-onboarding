458c7f4fad8a1c7c3c03925adf64d64f488790c2
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:20:57 +0900

    [problem1]숫자의 각 자리수를 합,곱한 것 중 큰수 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+
+    }
+
+
+    public static int Maxnum(int num) {
+        int sum = 0, mul = 1;
+        while(num != 0) {
+            sum += num % 10;
+            mul *= num % 10;
+            num /= 10;
+        }
+        return Math.max(sum, mul);
     }
 }
