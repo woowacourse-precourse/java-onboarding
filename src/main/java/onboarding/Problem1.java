b35ec979561abe8d64afacead9de1c258cc10f08
jindaram-stu <jinsb1999@naver.com>
2022-10-31 20:17:46 +0900

    feat : [문제1] 포비와 크롱이 각각 펼친 페이지 수를 수식에 기반한 최대값 계산 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,29 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static int calculate(int leftPage, int rightPage) {
+        Integer leftMax = getMax(leftPage);
+        Integer rightMax = getMax(rightPage);
+        return Math.max(leftMax, rightMax);
+    }
+
+    public static Integer getMax(Integer page) {
+        String number = String.valueOf(page);
+        Integer sum = 0, multiple = 1;
+        for (int i=-0; i<number.length(); i++) {
+            sum += Character.getNumericValue(number.charAt(i));
+            multiple *= Character.getNumericValue(number.charAt(i));
+        }
+        return Math.max(sum,multiple);
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiMax = calculate(pobi.get(0),pobi.get(1));
+        int crongMax = calculate(crong.get(0), crong.get(1));
+
         return answer;
     }
 }
