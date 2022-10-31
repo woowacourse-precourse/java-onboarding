5c6c603204889a51c38ebe4bba20833259604392
kariskan <zkfzpf56@naver.com>
2022-10-31 10:51:15 +0900

    refactor: Money 클래스를 이용한 solution 함수 리팩토링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,12 @@
 package onboarding;
 
-import java.util.Collections;
+import onboarding.support.problem5.Money;
+
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        Money answer = new Money(money);
+        return answer.getMoneyList();
     }
 }
