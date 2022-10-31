b4b365d9dee18f7ca54b5ea0d635609e074a6df7
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:38:05 +0900

    feat: user의 친구 찾기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,22 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> getUserFriends(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+        userFriends.add(user);
+
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+
+            if (friend1.equals(user)) {
+                userFriends.add(friend2);
+            } else if (friend2.equals(user)) {
+                userFriends.add(friend1);
+            }
+        }
+        return userFriends;
     }
 }
