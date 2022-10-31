8fa80f10579d3f644cd54c2b2897277839ad2c53
Timothy <toydiary@kakao.com>
2022-10-31 23:25:53 +0900

    feat(문제 2): 입력받은 단어에서 중복되는 글자를 제거
    1. 입력받은 단어에서 중복되는 글자를 제거.
    > - 입력받은 단어에서 중복되는 글자를 제거하고 중복되는 글자가 없으면 리턴하는 함수를 제작
    
    Closes #3

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import onboarding.problem2.SolveBrownCryptogram;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        SolveBrownCryptogram solveBrownCryptogram = new SolveBrownCryptogram();
+        if (solveBrownCryptogram.isFailedInputValidation(cryptogram)) throw new IllegalArgumentException();
+
+        answer = solveBrownCryptogram.removeDuplicationSpelling(cryptogram);
+
         return answer;
     }
 }
