6804bd25fa61c0c351ba7685a1a9b55957c4973d
cmw9957 <cmw9957@gmail.com>
2022-10-31 21:25:55 +0900

    add: solution method 내용 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -42,6 +42,9 @@ public class Problem5 {
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        answer = countUnit(money);
+
         return answer;
     }
 }
