8500845bf21699f066627251381f81653f6ef8c9
Young37 <20171470@g.dongseo.ac.kr>
2022-10-31 20:12:06 +0900

    입력값을 리스트로 관리(코드 중복을 줄이기 위해, 쉬운 수정을 위해)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+    	int answer = Integer.MAX_VALUE;
+    	List<List<Integer>> userPagesList = new ArrayList<List<Integer>>();
+    	userPagesList.add(pobi);
+    	userPagesList.add(crong);
+    	List<Integer> userMaxList = new ArrayList<Integer>();
+    	for(int i = 0; i<userPagesList.size();i++) {
+    		userMaxList.add(-1);
+    	}
+    	return answer;			
+	}
 }
