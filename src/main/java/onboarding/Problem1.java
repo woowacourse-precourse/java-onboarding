f04e47f106c1d59a6853c010cb766cd1f49a9f6c
소대훈 <theogns0681@naver.com>
2022-10-31 17:46:43 +0900

    [feat] (문제1) Book 객체 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,21 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
         return answer;
+    }
+    static class Book{
+        private final int previousPage;
+        private final int nextPage;
+        public int getPreviousPage() {
+            return previousPage;
+        }
+        public int getNextPage() {
+            return nextPage;
+        }
+        Book(int previousPage, int nextPage) {
+            this.previousPage = previousPage;
+            this.nextPage = nextPage;
+        }
     }
 }
