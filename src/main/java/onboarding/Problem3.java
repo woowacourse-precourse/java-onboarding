ed9f16ab7badb119659d2bb5252b998fb7bed155
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-10-31 21:28:11 +0900

    네자리 수 측정완료 및 오류수정
    네자릿수의 경우 369 손뼉 횟수를 측정하는 기능 추가 + 누적 형식으로 더해지다보니 중복으로 가산되는 것을 발견하여 오류수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,14 +6,15 @@ public class Problem3 {
         int answer10 = 0;
         int answer100 = 0;
         int answer = 0;
-        int restTen = number % 10;
+        int restTen = number % 10; // number를 10으로 나눈 나머지
 
         int restHun = number % 100;
 
         int restTho = number % 1000;
+        int shareTho = number /1000;
 
         //1~10까지 각각 박수치는 횟수를 repeatNum에 담음
-
+        //10의 자리가 1일때 3씩 더함
         final int[] repeatNum = {0, 0, 1, 0 , 0, 1, 0, 0, 1, 0};
 
         answer10 = (restHun / 10) * 3;
@@ -38,22 +39,39 @@ public class Problem3 {
 
         answer100 += answer10 + ((restTho-restHun) / 100) * 60;
 
-        if((restTho-restHun) / 100 == 3){
-            answer100 += answer10;
+        if((restTho-restHun) / 100 >= 3 && (restTho-restHun) / 100 < 6){
+            answer100 += restHun+1;
         }
-        else if((restTho-restHun) / 100 == 6){
-            answer100 += 60 + answer10;
+        else if((restTho-restHun) / 100 >= 6 && (restTho-restHun) / 100 < 9){
+            answer100 += restHun + 101;
         }
         else if((restTho-restHun) / 100 == 9){
-            answer100 += 120 + answer10;
+            answer100 += restHun + 201;
+        }
+
+//      1000의 자리
+        if(number >=1000) {
+            if(shareTho >= 3 && shareTho< 6){
+                answer += restTho+1;
+            }
+            else if(shareTho >= 6 && shareTho< 9){
+                answer += restTho + 1001;
+            }
+            else if(shareTho == 9){
+                answer += restTho + 2001;
+            }
+        }
+        else if(number <1000){
+            return answer100;
         }
 
         return answer;
+//        0010010010 -> 1~10까지
 
     }
 
     public static void main(String[] args) {
-        int number = 33;
+        int number = 999;
         System.out.println(solution(number));
     }
 }
