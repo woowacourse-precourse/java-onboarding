6a44d7632c9420d9bac9e19afd6547b40415d0ea
yeonji <view7186@naver.com>
2022-10-31 19:01:09 +0900

    Solving

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        boolean isDuplicateExist = false;
+        int si = 0, ei = 0;
+        for(int i = 1; i < cryptogram.length(); i++) {
+            if(cryptogram.charAt(si) == cryptogram.charAt(i)) {
+                ei = i;
+                isDuplicateExist = true;
+            } else {
+                if(isDuplicateExist) cryptogram = cryptogram.substring(0, si) + cryptogram.substring(ei+1);
+                si = i;
+                ei = i;
+            }
+        }
+        if(isDuplicateExist) return solution(cryptogram);
+        return cryptogram;
     }
 }
