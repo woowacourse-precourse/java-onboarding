69352efedc0b3a874b6f1fa0c886faacd89980d1
이승훈 <qwqw8019@naver.com>
2022-10-31 16:33:34 +0900

    problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        int index = 0;
+        while(true){
+            int length = cryptogram.length();
+            if(index >= length-1 || length < 2){
+                break;
+            }
+            if(cryptogram.charAt(index) == cryptogram.charAt(index+1)){
+                cryptogram = cryptogram.replace(cryptogram.substring(index, index+2),"");
+                index = 0;
+                continue;
+            }
+
+            index ++;
+        }
+
+        answer = cryptogram;
         return answer;
     }
 }
