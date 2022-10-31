fe21a856dd0dceb7f4ea185b5f182ac09c148a58
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 22:57:33 +0900

    feat: P2 기능 1 연속되는 문자를 제거하는 함수 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.Stack;
+
 /**
- * 기능 1: 연속되는 문자열 제거 함수 작성 (Stack 이용)
+ * 기능 1: 연속되는 문자 제거 함수 작성 (Stack 이용)
  * 기능 2: 중복 제거된 Stack을 통해 문자열로 변환하는 함수 작성
  */
 public class Problem2 {
+
+    public static Stack<Character> removeDuplicate(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        stack.push(cryptogram.charAt(0));
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (stack.peek() == cryptogram.charAt(i)) {
+                stack.pop();
+                continue;
+            }
+            stack.push(cryptogram.charAt(i));
+        }
+        return stack;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
