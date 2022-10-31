22c5432da56fcbec5a217f41ff9df5abd03de5cf
민병욱 <woook27@naver.com>
2022-11-01 03:14:35 +0900

    feat: transform 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,29 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static char transform(char c) {
+
+        if (c >= 'A' && c <= 'Z') {
+            if (c <= 'M') {
+                c = (char) ('Z' - (c - 'A'));
+            }
+            else {
+                c = (char) ('A' + ('Z' - c));
+            }
+        }
+
+        if (c >= 'a' && c <= 'z') {
+            if (c <= 'm') {
+                c = (char) ('z' - (c - 'a'));
+            }
+            else {
+                c = (char) ('a' + ('z' - c));
+            }
+        }
+        return c;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
