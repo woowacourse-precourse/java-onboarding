8af702f740eea31dd98cce291ab659a4fd7e2d24
ykmxxi <ykmxxi97@gmail.com>
2022-10-31 15:36:29 +0900

    feat: (Problem6)닉네임을 2글자씩 분리하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static List<String> separateNickname(String nickname) {
+        List<String> parts = new ArrayList<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            parts.add(nickname.substring(i, i + 2));
+        }
+        return parts;
     }
 }
