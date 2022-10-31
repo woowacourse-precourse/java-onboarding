05e075304dff74bcda006f8cae92e4fdc8861699
chener <49267211+chanosong@users.noreply.github.com>
2022-10-31 21:25:11 +0900

    [Add] Prob2 Functional list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem2 {
+    // func : get rid of successive char and return result
+    // func : check need to more decode
+    // func : recursive decoding algorithm return decoded string
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
