08b38fc96b58150fd5923b0a930337542ccb2a33
K-Diger <kitten.diger@gmail.com>
2022-10-31 20:57:58 +0900

    docs : Problem3 기능 목록 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,13 @@
 package onboarding;
 
+/*
+
+[기능 목록]
+1. 입력받은 문자열에 3, 6, 9 가 포함되면 그 갯수를 카운트를 하는 메서드
+2. 1번 메서드를 활용하여 현재 순회중인 문자열이 몇 개의 박수를 쳐야하는지 카운트 하는 메슫
+3. 총 박수를 몇번 처야하는지 카운트하는 메서드 2번 메서드를 이용해야한다.
+ */
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
