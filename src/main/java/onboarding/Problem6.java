b0bf7953c7c99c7d4bd3e9a46fcb3ead6a260b3b
akskflwn <akskflwn@gmail.com>
2022-10-31 14:14:36 +0900

    (refactor) Problem 6 리펙토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,81 +1,32 @@
 package onboarding;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        Students studentList = new Students(forms);
-
-        return studentList.getEmail();
+        Map<String, List<String>> duplicatedEmails = getDuplicatedEmails(new HashMap<>(), forms);
+        List<String> answer = new ArrayList<>();
+        duplicatedEmails.values().stream().filter(s -> s.size() > 1).forEach(answer::addAll);
+        return answer.stream().sorted().collect(Collectors.toList());
     }
 
+    private static Map<String, List<String>> getDuplicatedEmails(HashMap<String, List<String>> duplicatedEmails, List<List<String>> forms) {
+        for (List<String> form : forms) {
+            String arr[] = form.get(1).split("");
+            for (int i = 1; i < arr.length; i++) {
+                String nickName = arr[i - 1] + arr[i];
 
-}
-
-class Students {
-    private List<Student> studentList = new ArrayList<>();
-
-    public Students(List<List<String>> forms) {
-        for (List list : forms) {
-            this.studentList.add(new Student(String.valueOf(list.get(0)), String.valueOf(list.get(1))));
-        }
-    }
-    public List<String> getEmail() {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < this.studentList.size(); i++) {
-            String nickname = "";
-            String[] arr = studentList.get(i).getNickName().split("");
-            System.out.println(Arrays.toString(arr));
-            for (int j = 1; j < arr.length; j++) {
-                nickname = arr[j - 1] + arr[j];
-                System.out.println("현제 검사할 닉네임 : " + nickname);
-                for (Student s : studentList) {
-                    if (s.getNickName().contains(nickname)) {
-                        System.out.println("검열된 닉네임 : " + s.getNickName());
-                        System.out.println("검열된 이메일 : " + s.getEmail());
-                        if (studentList.get(i).getNickName().equals(s.nickName)) {
-                            continue;
-                        }
-                        list.add(s.getEmail());
-                    }
+                if (!duplicatedEmails.containsKey(nickName)) {
+                    duplicatedEmails.put(nickName, new ArrayList<>());
                 }
+                duplicatedEmails.get(nickName).add(form.get(0));
 
             }
+
         }
-
-
-        return list.stream().distinct().sorted().collect(Collectors.toList());
-    }
-}
-
-class Student {
-    String email;
-    String nickName;
-
-
-    public Student(String email, String nickName) {
-        this.email = email;
-        this.nickName = nickName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "email='" + email + '\'' +
-                ", nickName='" + nickName + '\'' +
-                '}';
+        return duplicatedEmails;
     }
 }
 
