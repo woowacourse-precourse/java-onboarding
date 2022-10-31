5757e3cbb668ee43a5829a3665fe7133ba08fbbb
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:28:16 +0900

    [problem5] MoneyChanger 필드 정의

--- src/main/java/onboarding/problem5/MoneyChanger.java
+++ src/main/java/onboarding/problem5/MoneyChanger.java
@@ -2,7 +2,9 @@ package onboarding.problem5;
 
 public class MoneyChanger {
     //화폐 종류 리스트
+    private int[] currencies = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     //result 배열
+    private int[] result = new int[9];
     //화폐 종류와 잔금을 입력받아 result배열을 업데이트하고 잔금을 반환
     //환금 함수
 }
