fe7f8cf8c26f58c64fe61e76fcd1335f49a4db84
DDing77 <devseo2021@gmail.com>
2022-10-31 22:04:53 +0900

    [Problem3] feat : 각 숫자의 3, 6, 9 갯수를 카운팅 하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem3 {
+
+    // 1 ~ number 까지 각 숫자의 3, 6, 9의 갯수를 카운트하여 리턴
+    public static int getResult(int number) {
+        int cnt = 0;
+
+        for(int i=1; i<=number; i++) {
+            int num = i;
+            // 각 숫자의 3, 6, 9 갯수를 확인
+            while(num > 0) {
+                int temp = num % 10;
+                if(temp == 3 || temp == 6 || temp == 9 ) cnt++;
+                num /= 10;
+            }
+        }
+
+        return cnt;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
+        int answer = getResult(number);
         return answer;
     }
 }
