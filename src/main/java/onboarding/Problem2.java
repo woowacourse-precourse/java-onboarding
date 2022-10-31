1b9b1427bb1d172d3574720df2956d56b85f5b66
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-01 03:36:12 +0900

    refactor(Problem2): make Cryptogram a top-level class
    Cryptogram benefits little from being a nested class. Make top-level class to increase legibility.
    Remove IS_VALID field and test validity via validate method instead.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,50 +1,48 @@
 package onboarding;
 
 public class Problem2 {
-    private static class Cryptogram {
-        private final String encrypted;
-        private final boolean IS_VALID;
-
-        private Cryptogram(String encrypted) {
-            this.encrypted = encrypted;
-            this.IS_VALID = validate();
-        }
-
-        private boolean validate() {
-            if (encrypted == null || encrypted.length() < 1 || 1000 < encrypted.length()) {
-                return false;
-            }
-            boolean isLowerCase = true;
-            for (int i = 0; isLowerCase && i < encrypted.length(); i++) {
-                char c = encrypted.charAt(i);
-                isLowerCase = Character.isLowerCase(c);
-            }
-            return isLowerCase;
-        }
-
-        private String decrypt() {
-            String decrypted = encrypted;
-            while (true) {
-                String[] stringArray = decrypted.split("([a-z])\\1{1,}");
-                if (stringArray.length == 1) {
-                    decrypted = stringArray[0];
-                    break;
-                }
-                StringBuilder builder = new StringBuilder();
-                for (String s : stringArray) {
-                    builder.append(s);
-                }
-                decrypted = builder.toString();
-            }
-            return decrypted;
-        }
-    }
-
     public static String solution(String cryptogram) {
         var myCryptogram = new Cryptogram(cryptogram);
-        if (!myCryptogram.IS_VALID) {
+        if (!myCryptogram.validate()) {
             return null;
         }
         return myCryptogram.decrypt();
+    }
+}
+
+class Cryptogram {
+    private final String encrypted;
+
+    Cryptogram(String encrypted) {
+        this.encrypted = encrypted;
+    }
+
+    boolean validate() {
+        if (encrypted == null || encrypted.length() < 1 || 1000 < encrypted.length()) {
+            return false;
+        }
+        boolean isLowerCase = true;
+        for (int i = 0; isLowerCase && i < encrypted.length(); i++) {
+            char c = encrypted.charAt(i);
+            isLowerCase = Character.isLowerCase(c);
+        }
+        return isLowerCase;
+    }
+
+    String decrypt() {
+        String decrypted = encrypted;
+        while (true) {
+            String[] stringArray = decrypted.split("([a-z])\\1{1,}");
+            if (stringArray.length == 1) {
+                decrypted = stringArray[0];
+                break;
+            }
+            StringBuilder builder = new StringBuilder();
+            for (String s : stringArray) {
+                builder.append(s);
+            }
+            decrypted = builder.toString();
+        }
+        return decrypted;
     }
 }
