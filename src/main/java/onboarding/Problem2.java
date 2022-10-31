36c30583072d7235350aa0aa1c4172397073f00e
DongWooE <ehddn977@naver.com>
2022-11-01 04:03:48 +0900

    [p2][add] 중복 문자열을 stack의 문자를 String으로 바꾸어주는 메소드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
+    private Stack<Character> stack = new Stack<Character>();
+
     public static String solution(String cryptogram) {
-        Stack<Character> stack = new Stack<Character>();
+        Problem2 p2 = new Problem2();
+        p2.putIntoStack(cryptogram);
+        String result = p2.transToString();
+        return result;
+    }
+
+    private void putIntoStack(String cryptogram){
         for(char c : cryptogram.toCharArray()){
             if(stack.empty()) stack.push(c);
             else if(stack.peek() != c) stack.push(c);
@@ -12,6 +20,14 @@ public class Problem2 {
                 while(stack.peek() != c) stack.pop();
             }
         }
-        return null;
+    }
+
+    private String transToString(){
+        StringBuilder sb = new StringBuilder();
+        while(!stack.empty()){
+            sb.append(stack.peek());
+            stack.pop();
+        }
+        return sb.reverse().toString();
     }
 }
