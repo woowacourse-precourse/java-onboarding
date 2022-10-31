038a60c2672a6f98540c68193feb7ebeb94b04f0
WonSubSub <boxer1995@naver.com>
2022-10-31 18:05:47 +0900

    Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> answer1 = new ArrayList<>();
+
+        for (int i = 0; i < forms.size()-1; i++) {
+            int name_length = forms.get(i).get(1).length();
+
+            for (int a = 0; a < name_length-1; a++){
+                String word = forms.get(i).get(1).substring(a,a+2);
+
+                for (int b = i+1; b < forms.size(); b++) {
+
+                    if (forms.get(b).get(1).contains(word)) {
+                        answer1.add(forms.get(b).get(0));
+                        answer1.add(forms.get(i).get(0));
+                    }//if
+                }//for3
+            }//for2
+        }//for1
+        Set<String> answer_set = new HashSet<>(answer1);
+        answer = new ArrayList<>(answer_set);
         return answer;
     }
 }
