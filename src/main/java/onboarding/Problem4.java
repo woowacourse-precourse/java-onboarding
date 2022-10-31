9eb8bc5ba3eb09f6d3e6e2817dd01f2e54d1303a
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-31 21:02:06 +0900

    문제 4 : 기능 2 - 어떤 문자가 매개변수로 주어질 때 청개구리 사전에 따라 문자를 변환하여 return하는 메소드.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,23 @@
 package onboarding;
 
 public class Problem4 {
+    public static String converse(char c) {
+        if ('A' <= c && c <= 'Z') {
+            String treeFrogDictionary = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            return treeFrogDictionary.substring(c - 'A', c - 'A' + 1);
+        }
+        if ('a' <= c && c <= 'z') {
+            String treeFrogDictionary = "zyxwvutsrqponmlkjihgfedcba";
+            return treeFrogDictionary.substring(c - 'a', c - 'a' + 1);
+        }
+        return " ";
+    }
+
     public static String solution(String word) {
         String answer = "";
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-
+            answer = answer + converse(c);
         }
         return answer;
     }
