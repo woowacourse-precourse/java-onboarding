c123b3fd122a55dc175237eb3c8391f0837e4fa4
소대훈 <theogns0681@naver.com>
2022-10-31 22:37:50 +0900

    [feat] (문제4) 개구리 문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,24 +1,16 @@
 package onboarding;
 
-import java.util.HashMap;
-
 public class Problem4 {
     public static String solution(String word) {
-        String[][] convertMessage = new String[2][26];
-        StringBuilder sb = new StringBuilder();
-        HashMap<String,String> frogAlphabet = new HashMap<>();
-        convertMessage[0] = new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M",
-                "a","b","c","d","e","f","g","h","i","j","k","l","m"};
-        convertMessage[1] = new String[]{"Z","Y","X","W","V","U","T","S","R","Q","P","O","N",
-                "z","y","x","w","v","u","t","s","r","q","p","o","n"};
-        for (int j = 0 ; j < convertMessage[0].length; j++){
-            frogAlphabet.put(convertMessage[0][j],convertMessage[1][j]);
-            frogAlphabet.put(convertMessage[1][j],convertMessage[0][j]);
+        char[] ch = word.toCharArray();
+        for (int i = 0 ; i < ch.length ; i++){
+            ch[i] = applyFrog(ch[i]);
         }
-        for (int i = 0 ; i < word.length(); i++){
-            String ch = String.valueOf(word.charAt(i));
-            sb.append(ch.replace(ch,frogAlphabet.getOrDefault(ch,ch)));
-        }
-        return sb.toString();
+        return "";
+    }
+    public static char applyFrog(char ch){
+        if (ch>='a' && ch<='z') return (char) ('a' + Math.abs(ch-'z'));
+        if (ch>='A' && ch<='Z') return (char) ('A' + Math.abs(ch-'Z'));
+        return ch;
     }
 }
