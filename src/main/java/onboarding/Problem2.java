1a0c3d07107ec51e6bc8df944ff8453079a362f2
wjh2012 <wjh2012@naver.com>
2022-10-31 18:33:19 +0900

    (PROBLEM2)문자열을 배열로 바꾸고, 연속된 문자를 찾아 제거하여 문자열로 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,24 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static String decode(String cryptogram) {
+
+        int len = cryptogram.length();
+
+        if (len<2){
+            return cryptogram;
+        }
+
+        char [] charCrypt = cryptogram.toCharArray();
+
+        for (int i = 1; i < len; i++) {
+            if (charCrypt[i - 1] == charCrypt[i]) {
+                charCrypt[i-1]=' ';
+                charCrypt[i]=' ';
+            }
+        }
+
+        return new String(charCrypt).replaceAll(" ","");
+    }
 }
