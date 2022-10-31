8a6f7d9ca3e0cf91f9f817bcbe61ed85902c751c
Denny-Ko <donghar@naver.com>
2022-10-31 20:18:26 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        int length = cryptogram.length();
+
+        boolean check = true;
+        while(check) {
+            check = false;
+
+            for (int i = 0; i < length - 1; i++) {
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    cryptogram = cryptogram.substring(0, i).concat(cryptogram.substring(i + 2, length));
+                    check = true;
+                    i--; length = length-2;
+                }
+            }
+        }
+
+        return cryptogram;
     }
 }
