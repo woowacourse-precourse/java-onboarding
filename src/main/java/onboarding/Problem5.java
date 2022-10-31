c6ff7dd44b37d861bf4a198031d0bf173abf4349
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 22:17:22 +0900

    [Problem 5] Overview

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,16 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
+    static int[] monetaryUnits = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        /*
+        1. 화폐 단위를 내림차순으로 저장하는 배열 선언
+        2. 배열을 순회하며 파라미터 money를 가능한 한 화폐로 교환하여 카운트
+        */
         return answer;
     }
 }
