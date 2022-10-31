9391587b89da778a0088ab160b87d358404b7555
baksohyeon <bori12370@gmail.com>
2022-10-31 11:07:22 +0900

    feat: add method to convert number to array of digits

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public List<Integer> getDigits(int num) {
+        List<Integer> digits = new ArrayList<Integer>();
+        while (num > 0) {
+            digits.add(num % 10);
+            num = num / 10;
+        }
+        return digits;
     }
 }
