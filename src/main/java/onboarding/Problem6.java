78eaf10644189debf6fa3ab4e514ea56078edb13
JunHyeon <dlwnsgus07@naver.com>
2022-10-31 19:38:06 +0900

    Feat : add validation check method to Problem6
    Problem6의 문제 조건에 맞춰 유효성검증을 하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,44 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    public static boolean validCheck(List<List<String>> list){
+        if(list.size()< 1 || list.size() > 10000){
+            return false;
+        }
+        for(List<String> user : list){
+            if(user.get(0).length() < 11 || user.get(0).length() >= 20){
+                return false;
+            }
+            if(!user.get(0).substring(user.get(0).length() - 9, user.get(0).length()).equals("email.com")){
+                return false;
+            }
+            if(!isItKorean(user.get(1))){
+                return false;
+            }
+            if(user.get(1).length() < 1 || user.get(1).length() >= 20){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isItKorean(String s) {
+        for(int i=0;i<s.length();i++){
+            if(Character.getType(s.charAt(i)) != 5){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        if (!validCheck(forms)) {
+            throw new IllegalStateException("잘못된 입력입니다.");
+        }
         return answer;
     }
 }
