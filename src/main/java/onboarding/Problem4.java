343464b3973dea21023735b6edb2a1ae63c09cbc
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-10-31 23:11:23 +0900

    [FEAT] : 알파벳 판별 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+    }
+
+    /** 알파벳인지 확인하는 정규표현식 */
+    private static boolean checkStr(String str){
+        return Pattern.matches("^[a-zA-Z]*$", str);
     }
 }
