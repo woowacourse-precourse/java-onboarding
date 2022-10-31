400c098ef081fb43a3b248aa3fe497892ee9ae16
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-10-31 17:10:51 +0900

    2번기능 문서작성, 제작 (2022.10.31)

--- docs/PROBLEM2.md
+++ docs/PROBLEM2.md
@@ -21,3 +21,14 @@
 | --- | --- |
 | "browoanoommnaon" | "brown" |
 | "zyelleyz" | "" |
+
+### 요구사항 분석
+- 주어진 문자열에서 중복을 제거하고, 이에 만들어진 결과를 기반으로 다시 중복을 제거하는 것을 반복한다.
+- 문자열의 길이가 1인 경우, 입력값을 반환한다.
+- 해독의 결과가 빈 문자열인 경우, 빈 문자열을 반환한다.
+- 문자의 중복은 좌우 한 칸의 문자와 비교해서 같은 문자가 있는 경우 중복 문자로 판단한다.
+
+### 메서드 구현 목록
+- boolean isDuplicated(String str, int i) : 문자열의 i번째 인덱스의 문자가 중복문자인지를 판단하는 메서드
+- String deleteDuplicated(String str) : 현재 주어진 문자열에서 중복을 제거하는 메서드
+- String getAnswer(String str) : deleteDuplicated()를 더 이상 중복을 제거 할 수 없을때까지 반복해 결과를 반환하는 메서드
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,74 @@
 package onboarding;
 
+/**
+ * @작성자 rjsah09
+ * @작성일 2022.10.31
+ *
+ * @수정내역
+ * */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+
+        if(cryptogram.length() == 1) {
+            return answer;
+        }
+
+        answer = getAnswer(cryptogram);
+        System.out.println(answer);
         return answer;
     }
+
+    private static boolean isDuplicated(String str, int i) {
+        char c = str.charAt(i);
+
+        if(i == 0) {
+            if(c != str.charAt(i + 1)) {
+                return false;
+            }
+        } else if (i == str.length() - 1) {
+            if(c != str.charAt(i - 1)) {
+                return false;
+            }
+        } else {
+            if(str.charAt(i - 1) != c && c != str.charAt(i + 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static String deleteDuplicated(String str) {
+        if(str.length() == 0) {
+            return str;
+        }
+
+        String decrypted = "";
+        for(int i = 0; i < str.length(); i++) {
+            if(!isDuplicated(str, i)) {
+                decrypted += str.substring(i, i + 1);
+            }
+        }
+
+        return decrypted;
+    }
+
+    private static String getAnswer(String str) {
+        String prev = str;
+        String decrypted = "";
+
+        while(true) {
+            decrypted = deleteDuplicated(prev);
+            if(!decrypted.equals(prev)) {
+                prev = decrypted;
+            } else {
+                break;
+            }
+        }
+
+        return decrypted;
+    }
+
 }
