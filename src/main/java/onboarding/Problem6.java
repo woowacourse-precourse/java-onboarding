9bd64c9e68c0bea81a77381497421665d439a247
moonn6pence <shin421179@gmail.com>
2022-11-01 01:30:04 +0900

    feat #6 : 부분문자열 정보 구성 기능
    - addHashMap : 연속하는 두 글자 정보를 HashMap으로 구성하는 메소드
    - crewSubstring : 각 크루원들 각각의 HashMap을 담는 ArrayList
    - InitializeCrewSubstring : 모든 크루원에 대해 crewSubstring을 초기화하는 메소드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,42 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+
+    private static ArrayList<HashMap<Character,ArrayList<Character>>> crewSubstring;
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static HashMap<Character, ArrayList<Character>> addHashMap(String nickname){
+        HashMap<Character, ArrayList<Character>> substringMap = new HashMap<Character, ArrayList<Character>>();
+
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            Character key = nickname.charAt(i);
+            Character value = nickname.charAt(i + 1);
+
+            ArrayList<Character> list = new ArrayList<Character>();
+
+            if (substringMap.containsKey(key)) {
+                list = substringMap.get(key);
+                list.add(value);
+            }else{
+                list.add(value);
+            }
+
+            substringMap.put(key, list);
+        }
+
+        return substringMap;
+    }
+
+    private static void initializeCrewSubstring(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            crewSubstring.add(addHashMap(forms.get(i).get(1)));
+        }
     }
 }
