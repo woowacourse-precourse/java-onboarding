230fa2d3f934b1f743b8e6ebd34b33984e0129da
Park shinyeong <syngh503@gmail.com>
2022-10-31 19:23:31 +0900

    Problem5/Refactor/getMoneyList, getNumberOfMoney 함수 추가 및 기능 분리 getMoneyList 함수를 추가해 지폐의 종류가 담긴 리스트를 리턴하는 기능을 구분했습니다. getNumberOfMoney 함수를 추가해 지폐 개수를 구하는 기능을 구분하였습니다. Relate to: #5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,8 +5,17 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        Problem5 pb5 = new Problem5();
+        List<Integer> moneyList = pb5.getMoneyList();
+        return pb5.getNumberOfMoney(money, moneyList);
+    }
+
+    public List<Integer> getMoneyList () {
+        return List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+    }
+
+    public List<Integer> getNumberOfMoney (int money, List<Integer> moneyList) {
         List<Integer> answer = new ArrayList<>();
-        int[] moneyList = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         for(int coin : moneyList) {
             int numberOfMoney= money / coin;
             answer.add(numberOfMoney);
