23ee326edf7efa9517e9d1c66e51d42c0ab7d7fb
yj2980 <gum0945@naver.com>
2022-10-31 15:26:20 +0900

    rename(program5) : 변수명 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,10 +6,26 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    static final int max = 9;
-    static final int division = 5;
+    enum Cash {
+        length(9),
+        division(5),
+        def(0),
+        fiveUnit(1);
+        private final int value;
+        Cash(int type) {
+            this.value = type;
+        }
+    }
+    enum Index {
+        front(1);
+
+        private  final int info;
+        Index(int info) {
+            this.info = info;
+        }
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer;
 
         checkException(money);
 
@@ -19,8 +35,8 @@ public class Problem5 {
         return answer;
     }
 
-    //자리수 구분해 리스트에 넣음음
-   public static List<Integer> placeNumber(int money) {
+    //자리수 구분해 리스트에 넣음
+    public static List<Integer> placeNumber(int money) {
         List<Integer> digitNumber_list = new ArrayList<>();
 
         while (money > 0) {
@@ -29,36 +45,38 @@ public class Problem5 {
         }
         return digitNumber_list;
     }
-
     public static Integer [] divisionMoney(List<Integer> digitNumber_list) {
-        Integer[] money_list = new Integer[max];
+        Integer[] money_list = new Integer[Cash.length.value];
         int j = 0;
+        int i;
+        money_list[j] = digitNumber_list.get(j);
 
-        money_list[0] = digitNumber_list.get(0);
-        for (int i = 1; i < max; i++) {
+        for (i = 1; i < Cash.length.value; i++) {
             //홀수 일 때
             if (i % 2 == 1)
                 money_list[i] = digitNumber_list.get(i - j);
             //짝수일 때
             if (i % 2 == 0) {
-                if (money_list[i - 1] >= division) {
-                    money_list[i] = 1;
-                    money_list[i - 1] = money_list[i - 1] - division;
-                } else if (money_list[i - 1] < division)
-                    money_list[i] = 0;
+                if (money_list[i - Index.front.info] < Cash.division.value)
+                    money_list[i] = Cash.def.value;
+                if (money_list[i - 1] >= Cash.division.value) {
+                    money_list[i] = Cash.fiveUnit.value;
+                    money_list[i - 1] -= Cash.division.value;
+                }
                 j++;
             }
-            //9의 자리가 있을 경우
-            if (i == max - 1)
-            {
-                if (digitNumber_list.size() == 6)
-                    money_list[i] += (digitNumber_list.get(5) * 10) / division;
-                if (digitNumber_list.size() == 7)
-                    money_list[i] += (digitNumber_list.get(6) * 100) / division;
-            }
+        }
+        //9의 자리가 있을 경우
+        if (i == Cash.length.value)
+        {
+            if (digitNumber_list.size() == 6)
+                money_list[i - Index.front.info] += (digitNumber_list.get(5) * 10) / Cash.division.value;
+            if (digitNumber_list.size() == 7)
+                money_list[i - Index.front.info] += (digitNumber_list.get(6) * 100) / Cash.division.value;
         }
         return money_list;
     }
+
     /*
     예외 처리
      */
--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -114,6 +114,20 @@ class ApplicationTest {
             List<Integer> result = List.of(0, 1, 1, 0, 0, 0, 0, 0, 0);
             assertThat(Problem5.solution(money)).isEqualTo(result);
         }
+
+        @Test
+        void case3() {
+            int money = 999_999;
+            List<Integer> result = List.of(19, 4, 1, 4, 1, 4, 1, 4, 9);
+            assertThat(Problem5.solution(money)).isEqualTo(result);
+        }
+
+        @Test
+        void case4() {
+            int money = 1_000_000;
+            List<Integer> result = List.of(20, 0, 0, 0, 0, 0, 0, 0, 0);
+            assertThat(Problem5.solution(money)).isEqualTo(result);
+        }
     }
 
     @Nested
