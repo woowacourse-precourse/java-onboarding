cfe78c91c8f4343f6f56985d7a3234b42fa6eba5
song-wooseok <sws981202@gmail.com>
2022-10-31 20:32:11 +0900

    docs: [PROBLEM6.md] 기능 목록 추가

---
+++ src/main/java/onboarding/problem6/utils/Constants.java
@@ -0,0 +1,21 @@
+package onboarding.problem6.utils;
+
+public class Constants {
+    public static final int CERW_EMAIL = 0;
+
+    public static final int CREW_NICNAME = 1;
+
+    public static final int MAX_CREW_NUMBER = 1000;
+
+    public static final int MIN_CREW_NUMBER = 1;
+
+    public static final int DOMAIN_POSITION = 10;
+
+    public static final int MIN_NICKNAME_LENGTH = 1;
+
+    public static final int MAX_NICKNAME_LENGTH = 19;
+
+    public static final String DOMAIN = "@email.com";
+
+    public static final String HANGUL_REGEX = "^[ㄱ-ㅎ가-힣]*$";
+}
