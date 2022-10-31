600c16b9f362ff587efac7ec76710d64a94f81cb
jojaeng2 <ds4ouj@naver.com>
2022-10-31 13:28:44 +0900

    feat : 사용자의 이름을 길이 2로 파싱하는 기능구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static class NicknameChecker {
+        private final List<List<String>> forms;
+        private final Map<String, Integer> counts;
+
+        public NicknameChecker(List<List<String>> forms) {
+            this.forms = forms;
+            this.counts = new HashMap<>();
+        }
+
+        private String parseName(String name, int index) {
+            return name.charAt(index - 1) + String.valueOf(name.charAt(index));
+        }
+
     }
 }
