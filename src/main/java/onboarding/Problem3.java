2c8dcfa1287174fd272abe6d48ed179821df0a4a
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-01 00:01:10 +0900

    feat : 입력받은 숫자를 자리수로 나눈다.(findDigits())

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int play369(int n) {
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> digits = findDigits(i);
+
+        }
+        return count;
+    }
+
+    public static List<Integer> findDigits(int n) {
+        List<Integer> digits = new ArrayList<>();
+        while (n != 0) {
+            int remainder = n % 10;
+            n = n / 10;
+            digits.add(remainder);
+            findDigits(n);
+        }
+        return digits;
     }
 }
