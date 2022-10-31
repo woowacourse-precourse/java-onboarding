4f31df0577d6e6338cfffc1f9f2d1992b4e17eee
이정우 <acg6138@naver.com>
2022-11-01 01:16:30 +0900

    feat(Problem6): 현재 유저의 닉네임을 전부 두 글자로 잘라내고 이를 통해 검증 후 결과에 포함시키는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,6 +10,15 @@ public class Problem6 {
         return answer;
     }
 
+    public static void findAndAddSerialNickname(List<List<String>> forms, int currentUserIdx) {
+        String serialChar = "";
+        List<String> currentUserInfo = forms.get(currentUserIdx);
+        for (int i = 0; i < forms.get(currentUserIdx).get(1).length()-1; i++) {
+            serialChar = currentUserInfo.get(1).charAt(i) + String.valueOf(currentUserInfo.get(1).charAt(i+1));
+            checkContainsSerialChar(serialChar, currentUserIdx, forms);
+        }
+    }
+    
     public static void checkContainsSerialChar(String serialChar, int currentUserIdx, List<List<String>> forms) {
         for (int i = currentUserIdx+1; i < forms.size(); i++) {
             if (forms.get(i).get(1).contains(serialChar)) {
