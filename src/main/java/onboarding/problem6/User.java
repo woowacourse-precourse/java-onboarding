195c80d857c277abadec5a19bcdf70706d3babfa
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:46:56 +0900

    [problem6] UserRepository save, getUsers 함수 추가

--- src/main/java/onboarding/problem6/User.java
+++ src/main/java/onboarding/problem6/User.java
@@ -1,10 +1,23 @@
 package onboarding.problem6;
 
+import java.util.List;
+
 public class User {
     private String email;
     private String name;
 
-    //User입력받고 name 중복 부분 비교 후 boolean 반환하는 함수
+    public User(List<String> data) {
+        this.email = data.get(0);
+        this.name = data.get(1);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean checkDuplicated(User user1) {
+    }
+//User입력받고 name 중복 부분 비교 후 boolean 반환하는 함수
 
     //중복 부분 비교 시 사용할 조건 함수
 }
--- src/main/java/onboarding/problem6/UserRepository.java
+++ src/main/java/onboarding/problem6/UserRepository.java
@@ -8,10 +8,14 @@ public class UserRepository {
     List<User> userRepository = new ArrayList<>();
 
     public void create(List<String> data) {
+        boolean isDuplicated = false;
+        User user1 = new User(data);
+        for(var user: userRepository)if(isDuplicated = user.checkDuplicated(user1))return;
+        userRepository.add(user1);
     }
 
     public List<String> getUsers() {
-        return this.userRepository.stream().map(user->user.getEmail()).collect(Collectors.toList());
+        return this.userRepository.stream().map(User::getEmail).collect(Collectors.toList());
     }
 
     //userRepository에 User 추가하는 함수 이 때 모든 배열 요소에 체크 함수를 검사한다.
