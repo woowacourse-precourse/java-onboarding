f68ff79b4a45454962ef94696e6fe51d6e53d59c
LJW25 <dbjwl@g.skku.edu>
2022-10-31 23:26:29 +0900

    p1. define functions

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,20 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private int sumNum(int num){
+
+    }
+
+    private int mulNum(int num){
+
+    }
+
+    private int calMaxNum(List<Integer> numbers){
+
+    }
+
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
