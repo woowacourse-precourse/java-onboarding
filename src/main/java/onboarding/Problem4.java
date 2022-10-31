073764e9891212aa179a3fb08604913eedc60e1a
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 22:02:24 +0900

    [Problem 4] Reverse converter

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,12 +1,20 @@
 package onboarding;
-
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         /*
         1. 알파벳 내의 문자에 대해서만 반대로 변환하는 함수 작성
+          - A : 65 , Z : 90 / a : 97 , z : 122
         2. 문자열을 순회하며 변환 후 반환할 문자열에 누적
         */
         return answer;
+    }
+    static char reverse(char ch){
+        // 알파벳 체크
+        if ( ch < 65 || ch > 122 ) return ch;
+        
+        if ( ch >= 'a' ) return (char)('a' + 'z' - ch);
+
+        return (char)('A' + 'Z' - ch);
     }
 }
