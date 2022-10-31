2f37528e88940a14569f17aa27575a068c53c349
syr9707 <yrshin9771@gmail.com>
2022-10-31 10:33:19 +0900

    feat(PROBLEM3): 3번 문제 풀이

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++) {
+            String str = String.valueOf(i);
+            for(int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (c == '3' || c == '6' || c == '9') {
+                    answer++;
+                }
+            }
+        }
+
         return answer;
     }
 }
