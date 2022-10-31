8bd0ee50f1ec0908debe23ab818a871be94c571a
권민정 <22alswjd@naver.com>
2022-11-01 01:49:36 +0900

    feat: Problem3 기능 '1. 1~number까지 반복문을 돌기' 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,10 @@ public class Problem3 {
         int answer = 0;
 
         // 1. 1~number까지 반복문을 돌기
+        for (int i = 1; i <= number; i++) {
+
+        }
+
         // 2. 임시 숫자 temp에 3이 몇 개 포함되는지 세기
 
         return answer;
