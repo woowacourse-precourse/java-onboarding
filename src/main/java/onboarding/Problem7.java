5910f9e44d99e537985adcae115521cb2ace8ecc
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-01 05:29:47 +0900

    feat(Problem7): 함께 아는 친구의 수를 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+        HashMap<String, List<String>> friendsMap = new HashMap<>();
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void findFriendShip(List<List<String>> friends, HashMap<String, List<String>> friendsMap){
+        for(int i=0; i< friends.size(); i++){
+          String user1 = friends.get(i).get(0);
+          String user2 = friends.get(i).get(1);
+          List<String> FriendsOfUser1 = friendsMap.getOrDefault(user1, new ArrayList<>());
+          FriendsOfUser1.add(user2);
+          List<String> FriendsOfUser2 = friendsMap.getOrDefault(user2, new ArrayList<>());
+          FriendsOfUser2.add(user1);
+          friendsMap.put(user1, FriendsOfUser1);
+          friendsMap.put(user2, FriendsOfUser2);
+      }
+    }
+
+    public static int countRelatedFriendsWithUser(String user, String other, HashMap<String, List<String>> friendsMap){
+        List<String> friendsOfUser = friendsMap.get(user);
+        List<String> friendsOfOther = friendsMap.get(other);
+        int relatedFriends = 0;
+        for(String friend : friendsOfOther){
+            if(friendsOfUser.contains(friend)){
+            relatedFriends++;
+        }
+        }
+        return relatedFriends;
     }
 }
