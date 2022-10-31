e5051c68fb2d64b2a366a6a2cab7ec2cdbe2d368
WonjunS <seowj0710@gmail.com>
2022-10-31 16:55:55 +1100

    Problem 3: update

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,7 @@
 package onboarding;
 
 public class Problem3 {
-    static int answer = 0;
+    static int answer;
 
     public static int solution(int number) {
         answer = pro(number);
@@ -10,18 +10,18 @@ public class Problem3 {
     }
 
     static int pro(int number) {
-        int count = 0;
+        int count = 0; // 3, 6, 9가 나온 총 횟수
 
         for(int i = 1; i <= number; i++) {
             String s = String.valueOf(i);
-            count += count(s);
+            count += count369(s);
         }
 
         return count;
     }
 
     // 해당 숫자에 3, 6, 9가 총 몇 개 들어 있는지 카운트하는 함수
-    static int count(String number) {
+    static int count369(String number) {
         int count = 0;
 
         for(int i = 0; i < number.length(); i++) {
