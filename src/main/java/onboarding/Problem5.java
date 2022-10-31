92f150b62b44330031306d3c37e6670c6cd0817b
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 01:15:02 +0900

    [Add] Prob5 currency list
    + currency list for calculate

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Problem5 {
     // list for cost of currency
+    private List<Integer> currency = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     // func : calculate amount of each currency, return List
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
