725429010abbb003487eeacdebca5cc43ca6b0cf
hoiae <donald819@naver.com>
2022-10-31 20:22:00 +0900

    5ë²docs: 5번문제 주석정리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        //50000,10000,5000,1000,500,100,50,10,1
-        //List<Integer> answer = Collections.emptyList();
         List<Integer> answer = new ArrayList<>();
         int[] bill = {50000,10000,5000,1000,500,100,50,10,1};
         int cnt = 0;
