720b9b0dd161ba46a86d00d6e8f630cc7afbf135
권민정 <22alswjd@naver.com>
2022-11-01 01:48:32 +0900

    feat: Problem3 기능 목록 주석 추가
    기능 목록을 작성한 주석 추가함.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // 1. 1~number까지 반복문을 돌기
+        // 2. 임시 숫자 temp에 3이 몇 개 포함되는지 세기
+
         return answer;
     }
 }
