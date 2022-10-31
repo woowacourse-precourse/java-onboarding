17ab1a8116e5aa013d381a3f74220a2c7a34b32d
sunghyuki <sunghyuk1609@gmail.com>
2022-10-31 22:32:46 +0900

    feat : 왼쪽 오른쪽 페이지가 순서대로 들어와있지 않는 상황 검증 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,8 +8,16 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiLeftPageNumber = pobi.get(0);
+        int pobiRightPageNumber = pobi.get(1);
+        int crongLeftPageNumber = crong.get(0);
+        int crongRightPageNumber = crong.get(1);
 
         if (isNotLengthTwo(pobi.size()) || isNotLengthTwo(crong.size())) {
+            return EXCEPTION;
+        }
+
+        if (isNotLeftAndRight(pobiLeftPageNumber, pobiRightPageNumber) || isNotLeftAndRight(crongLeftPageNumber, crongRightPageNumber)) {
             return EXCEPTION;
         }
 
@@ -21,5 +29,9 @@ class Problem1 {
             return true;
         }
         return false;
+    }
+
+    private static boolean isNotLeftAndRight(int leftPageNumber, int rightPageNumber) {
+        return rightPageNumber - leftPageNumber != 1;
     }
 }
