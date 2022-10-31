ec213f79c562a6ccd6775ae205d941742b212ba1
Cho SangHyun <98shcho@naver.com>
2022-11-01 02:40:01 +0900

    refactor: refactor feature to choose shortest nickname

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -17,28 +17,33 @@ public class Problem6 {
 
         return answer;
     }
+    public static List<List<String>> getEachRequests(List<List<String>> forms,
+                                               int i,
+                                               int j) {
+        List<List<String>> requests = new ArrayList<>();
 
+        if (forms.get(i).get(1).length() > forms.get(j).get(1).length()) {
+            requests.add(forms.get(j));
+            requests.add(forms.get(i));
+            return requests;
+        }
+
+        requests.add(forms.get(i));
+        requests.add(forms.get(j));
+        return requests;
+    }
     public static void findAndAddEmail(HashSet<String> emailSet,
                                        List<List<String>> forms,
                                        int start) {
-        String myNickName = forms.get(start).get(1);
-        String myEmail = forms.get(start).get(0);
         for (int j=start+1; j<forms.size(); j++) {
-            String otherNickName = forms.get(j).get(1);
-            String otherEmail = forms.get(j).get(0);
-            String shortNickName = myNickName;
-            String longNickName = otherNickName;
-
-            if (myNickName.length() > otherNickName.length()) {
-                shortNickName = otherNickName;
-                longNickName = myNickName;
-            }
+            List<List<String>> eachRequests = getEachRequests(forms, start, j);
+            String shortNickName = eachRequests.get(0).get(1);
 
             for (int k=0; k<shortNickName.length() - 1; k++) {
-                String somePart = shortNickName.substring(k, k + 2);
-                if (longNickName.contains(somePart)) {
-                    emailSet.add(myEmail);
-                    emailSet.add(otherEmail);
+                String somePart = eachRequests.get(0).get(1).substring(k, k + 2);
+                if (eachRequests.get(1).get(1).contains(somePart)) {
+                    emailSet.add(eachRequests.get(0).get(0));
+                    emailSet.add(eachRequests.get(1).get(0));
                 }
             }
         }
@@ -55,10 +60,11 @@ public class Problem6 {
 /*
 <<기능 목록>>
 - [V] 중복되는 사람들의 이메일을 모을 emailSet를 만든다
-- [V] forms를 순회하며 이메일과 닉네임을 가져온다
-- [V] 그 다음 번 사람부터 마지막 사람까지 순회하며 이메일과 닉네임을 가져온다
-- [V] 두 사람의 닉네임 중 더 짧은 걸 고른다
-- [V] 더 짧은 닉네임에서 2글자씩 뽑는다
+- [V] forms를 순회하며 기준점을 잡는다
+- [V] 기준점 다음 번 사람부터 마지막 사람까지 순회한다
+- [V] 두 사람의 닉네임 신청 정보를 가져온다
+- [V] 짧은 닉네임을 가져온다
+- [V] 짧은 닉네임에서 2글자씩 뽑는다
 - [V] 뽑은 문자열이 긴 닉네임에 포함되면 두 사람의 이메일을 emailSet에 넣는다
 - [V] emailSet을 이용해 answer를 만들고 오름차순으로 정렬한다
  */
