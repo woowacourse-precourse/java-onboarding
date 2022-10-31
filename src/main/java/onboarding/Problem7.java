72f0bc395a8d6aaef5747ddcdaf226295acf2b70
sjkim <seoungjoo01@gmail.com>
2022-10-31 20:59:45 +0900

    [refactor] 7번 문제 : ==를 equals로 변경

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -86,7 +86,7 @@ public class Problem7 {
         scoreList.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                if(o1.getValue() == o2.getValue()){
+                if(o1.getValue().equals(o2.getValue())){
                     return o1.getKey().compareTo(o2.getKey());
                 }
                 return o2.getValue().compareTo(o1.getValue());
