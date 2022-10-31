bcdf2c63f75af191b72a997b2a5fd3a1f93ca377
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-10-31 14:27:47 +0900

    feat: Implement solution in problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,47 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        int maxValueOfPobi = pobi.stream()
+            .map(Problem1::getMaxValue)
+            .mapToInt(x-> x)
+            .max().orElse(-1);
+
+        int maxValueOfCrong = crong.stream()
+            .map(Problem1::getMaxValue)
+            .mapToInt(x-> x)
+            .max().orElse(-1);
+
+        if (maxValueOfPobi < 0 || maxValueOfCrong < 0 ) {
+            return -1;
+        }
+
+        return maxValueOfPobi > maxValueOfCrong ? 1 :
+            (maxValueOfPobi == maxValueOfCrong ? 0 : 2);
+    }
+
+    private static int getMaxValue(Integer number) {
+        List<Integer> digitList = getDigitListFrom(number);
+        int sum = digitList.stream()
+            .mapToInt(x-> x)
+            .sum();
+        int product = digitList.stream()
+            .mapToInt(x -> x)
+            .reduce(1, Math::multiplyExact);
+
+        return Math.max(sum, product);
+    }
+
+    private static List<Integer> getDigitListFrom(Integer number) {
+        List<Integer> digit = new ArrayList<>();
+        while(number > 0) {
+            digit.add(number % 10);
+            number /= 10;
+        }
+        return digit;
     }
 }
