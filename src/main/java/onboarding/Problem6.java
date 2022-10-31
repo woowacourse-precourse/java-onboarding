be270dd78773d02a971add420e8aba348e718b8f
hhiyeon <hhyyeon0214@gmail.com>
2022-10-31 12:07:51 +0900

    feat: p6-add duplicate emails to set

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String, List<String>> duplicateMap = new HashMap<>();
+        Set<String> deduplicatedEmail = new LinkedHashSet<>();
+
         return answer;
+    }
+
+    private static void addValueToSet(Map<String, List<String>> duplicateMap, Set<String> duplicatedEmail) {
+        duplicatedEmail.addAll(duplicateMap.values().stream()
+                .filter(emailList -> emailList.size() >= 2)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList()));
     }
 
     private static String getTwoDigitName(String nickname, int startIdx) {
