fa9a13001415fc12124c9ef0fdc0b9d0f94ae818
Bumsoo <rhqjatn2398@naver.com>
2022-10-31 22:47:18 +0900

    feat(Problem6): 닉네임에서 길이가 2인 모든 연속된 부분 문자열과 이메일로 Map을 구성
    길이가 2이상인 닉네임에 한하며, key(2자리 연속 부분
    문자열)-value(이메일)로 Map을 구성한다

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+    private static final Map<String, List<String>> map = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        initMap(forms);
+
         return answer;
+    }
+
+    private static void initMap(List<List<String>> forms) {
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                putSubstringAndEmail(nickname.substring(i, i + 2), email);
+            }
+        }
+    }
+
+    private static void putSubstringAndEmail(String substring, String email) {
+        if (!map.containsKey(substring)) {
+            map.put(substring, new ArrayList<>(List.of(email)));
+            return;
+        }
+
+        map.get(substring).add(email);
+        return;
     }
 }
