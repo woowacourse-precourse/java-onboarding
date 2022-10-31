9cc0a4948a54d7f7364f570c63a9ce887f87a460
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-11-01 04:35:51 +0900

    Problem7 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static void catchException(String user, List<List<String>> friends, List<String> visitors) {
@@ -39,20 +37,107 @@ public class Problem7 {
         List<String> togetherKnowFriends = new ArrayList<>();
         for (int i = 0; i < directFriends.size(); i++) {
             for (int j = 0; j < friends.size(); j++) {
-                if (directFriends.get(i).equals(friends.get(j).get(0)) && !friends.get(i).get(0).equals(user)) {
-                    togetherKnowFriends.add(friends.get(i).get(0));
+                if (directFriends.get(i).equals(friends.get(j).get(0)) && !friends.get(j).get(1).equals(user)) {
+                    togetherKnowFriends.add(friends.get(j).get(1));
                 }
-                if (directFriends.get(i).equals(friends.get(j).get(1)) && !friends.get(i).get(1).equals(user)) {
-                    togetherKnowFriends.add(friends.get(i).get(1));
+                if (directFriends.get(i).equals(friends.get(j).get(1)) && !friends.get(j).get(0).equals(user)) {
+                    togetherKnowFriends.add(friends.get(j).get(0));
                 }
             }
         }
         return togetherKnowFriends;
     }
 
+    public static void countVisitorsRecommendScore(List<String> visitors, List<List<String>> countScore) {
+        for (int i = 0; i < visitors.size(); i++) {
+            {
+                countScore.get(i).add(visitors.get(i));
+                countScore.get(i).add("1");
+            }
+        }
+    }
+
+    public static void countTogetherRecommendScore(List<String> visitors, List<String> togetherKnowFriends, List<List<String>> countScore) {
+        for (int i = 0; i < togetherKnowFriends.size(); i++) {
+            countScore.get(i + visitors.size()).add(togetherKnowFriends.get(i));
+            countScore.get(i + visitors.size()).add("10");
+        }
+    }
+
+    public static void mergeRecommendScore(List<List<String>> countScore) {
+        for (int i = 0; i < countScore.size() - 1; i++) {
+            for (int j = i + 1; j < countScore.size(); j++) {
+                if (countScore.get(i).get(0).equals(countScore.get(j).get(0))) {
+                    int num = Integer.parseInt(countScore.get(i).get(1)) + Integer.parseInt(countScore.get(j).get(1));
+                    countScore.get(i).remove(1);
+                    countScore.get(i).add(String.valueOf(num));
+                    countScore.remove(j);
+                }
+            }
+        }
+    }
+
+    public static void removeDirectFriendInRecommend(List<String> directFriends, List<List<String>> countScore) {
+        for (int i = 0; i < directFriends.size(); i++)
+            for (int j = 0; j < countScore.size(); j++) {
+                if (directFriends.get(i).equals(countScore.get(j).get(0)))
+                    countScore.remove(j);
+            }
+    }
+
+    public static List<List<String>> sortRecommendList(List<List<String>> countScore) {
+        List<List<String>> result = new ArrayList<>();
+        for (int i = 0; i < countScore.size(); i++)
+            result.add(new ArrayList<>());
+        int maxIndex;
+        int countSize = countScore.size();
+        for (int i = 0; i < countSize; i++) {
+            maxIndex = 0;
+            for (int j = 0; j < countScore.size() - 1; j++) {
+                if (Integer.parseInt(countScore.get(j).get(1)) < Integer.parseInt(countScore.get(j + 1).get(1))) {
+                    maxIndex = j + 1;
+                }
+                if ((Integer.parseInt(countScore.get(j).get(1)) == Integer.parseInt(countScore.get(j + 1).get(1))) &&
+                        (countScore.get(j).get(0).compareTo(countScore.get(j + 1).get(0)) < 0)) {
+                    maxIndex = j;
+                }
+            }
+            result.get(i).add(countScore.get(maxIndex).get(0));
+            result.get(i).add(countScore.get(maxIndex).get(1));
+            countScore.remove(maxIndex);
+        }
+        return result;
+    }
+
+    public static List<String> selectRecommendFriends(List<List<String>> sortScore) {
+        List<String> finalRecommendFriends = new ArrayList<>();
+        int count = 5;
+        if (sortScore.size() < 5)
+            count = sortScore.size();
+        for (int i = 0; i < count; i++) {
+            finalRecommendFriends.add(sortScore.get(i).get(0));
+        }
+        return finalRecommendFriends;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         catchException(user, friends, visitors);
-        List<String> answer = Collections.emptyList();
+        List<List<String>> countScore = new ArrayList<>();
+        List<String> answer;
+        List<String> directFriends = FindDirectFriends(user, friends);
+        List<String> togetherKnowFriends = FindTogetherKnowFriends(user, friends, directFriends);
+
+        for (int i = 0; i < visitors.size() + togetherKnowFriends.size(); i++) {
+            countScore.add((new ArrayList<>()));
+        }
+
+        countVisitorsRecommendScore(visitors, countScore);
+        countTogetherRecommendScore(visitors, togetherKnowFriends, countScore);
+        mergeRecommendScore(countScore);
+        removeDirectFriendInRecommend(directFriends, countScore);
+        List<List<String>> sortScore = sortRecommendList(countScore);
+
+        answer = selectRecommendFriends(sortScore);
         return answer;
     }
 }
