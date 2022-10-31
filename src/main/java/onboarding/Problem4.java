95041c0b0c17b82ef6cb36773ab3c557cc359b65
eugene1204 <s1061001@naver.com>
2022-10-31 16:15:53 +0900

    기능 목록 : problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem4 {
+//    1. 한글자씩 떼어서 대문자 vs 소문자 아스키코드로 비교 -> 해당하는 글자로 출력
     public static String solution(String word) {
         String answer = "";
         return answer;
