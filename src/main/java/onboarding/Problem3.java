69b9f72a2dfb29cf6fe119a0a7d601ced6db441e
cocomozila <joseyun94@naver.com>
2022-10-31 18:46:00 +0900

    [문제 3] 박수를 쳐야하는 숫자 배열

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,8 @@
 package onboarding;
 
 public class Problem3 {
-
+    // 박수를 쳐야하는 숫자 배열
+    static final char[] CLAP_NUMBER = {'3', '6', '9'};
     // 전체 기능 호출
     public static int solution(int number) {
         return 1;
