352b2966f3bd1e44df3cea2e8255e18b4bcd380d
aud19 <aud19980518@gmail.com>
2022-10-31 20:39:39 +0900

    1주차 3번 3의 개수를 세는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,9 +7,6 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        for(int i = 1; i <= number; i++) {
-            answer += numMod(i);
-        }
         return answer;
     }
 
