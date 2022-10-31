09b3860e5712f0f93f1626529acad86df6b38595
Judy <judyb0626@gmail.com>
2022-11-01 02:58:11 +0900

    feat(problem 4): 변환 단어 출력

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        
+
+        for (int i = 0; i < word.length(); i++) {
+            answer = answer + convert(word.charAt(i));
+        }
 
         return answer;
     }
