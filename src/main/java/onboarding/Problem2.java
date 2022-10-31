5e4385e896077bc1c49bf4389640f1fa8ee6aea1
kimsinhyun <ksh415622@naver.com>
2022-10-31 22:08:19 +0900

    problem 2 completed

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        for (int i=0; i<answer.length()-1; i++){
+            if (answer.charAt(i) == answer.charAt(i+1)){
+                answer = answer.substring(0, i) + answer.substring(i+2, answer.length());
+//                System.out.println(answer);
+                answer = solution(answer);
+            }
+        }
         return answer;
     }
 }
