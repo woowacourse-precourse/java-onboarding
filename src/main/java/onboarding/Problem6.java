24b04e4fa12567719983e9311baeb4c2faf7a394
kokomong2 <ko9603@naver.com>
2022-11-01 02:35:30 +0900

    feat(문제6) 추가 : Pair클래스, List<Pair>에 조건별로 Pair를 추가하는 함수,findCount 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,61 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        HashMap<String, String> strmap = new HashMap<>();
+        HashMap<Character, List<Pair>> nickmap= new HashMap<>();
+        List<String> answer = new ArrayList<>();
+
+
         return answer;
+    }
+    public static List<Pair> addmapList(List<Pair> list, char cr){
+
+        int flag=0;
+        if(list!=null){
+            for(int i=0; i<list.size(); i++){
+                Pair pair = list.get(i);
+                if(pair.value==cr){
+                    pair.addCount();
+                    list.set(i,pair);
+                    flag =1;
+                }
+            }
+            if(flag ==0){
+                list.add(new Pair(cr));
+            }
+        }else {
+            list = new ArrayList<>();
+            list.add(new Pair(cr));
+        }
+
+        return list;
+    }
+
+    public static int findCount(List<Pair> list, char cr){
+        for (Pair pair : list) {
+            if(pair.value == cr){
+                return pair.count;
+            }
+        }
+
+        return 0;
+    }
+}
+
+class Pair{
+    char value;
+    int count;
+
+    public Pair(char value){
+        this.value = value;
+        this.count = 1;
+    }
+    public void addCount(){
+        this.count++;
     }
 }
