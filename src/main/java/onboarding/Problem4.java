8666a6ffca2a64a96efaacc86f3b21b55481667a
hyungkya <hyungkya@uci.edu>
2022-10-31 23:19:49 +0900

    feat(problem 4): add character reversing (corresponding to frog dictionary) method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return "";
+    }
+    private static char toFrogChar(char c) {
+        if (Character.isAlphabetic(c)) {
+            // "155, 219" are each sum of uppercase, lowercase alphabet ascii code's start/end.
+            // Subtracting original ascii code from it will result in a reversed alphabet.
+            c = (char) (Character.isUpperCase(c) ? 155 - c : 219 - c);
+        }
+        return c;
     }
 }
