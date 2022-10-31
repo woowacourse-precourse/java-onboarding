5dad2ac7b590c1671a24def3502f093e8a8342c0
yoonsj970407 <yoonsj970407@gmail.com>
2022-11-01 00:48:28 +0900

    ✨Feat: Create NicknameDuplicate class

---
+++ src/main/java/onboarding/NicknameDuplicate.java
@@ -0,0 +1,13 @@
+package onboarding;
+
+import java.util.List;
+
+class NicknameDuplicate {
+    List<List<String>> forms;
+
+    public NicknameDuplicate(List<List<String>> forms) {
+        this.forms = forms;
+    }
+
+
+}
