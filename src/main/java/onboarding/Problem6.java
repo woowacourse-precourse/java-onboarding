c7e60fb29417fe866946cc22599e48d35099e1f6
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-10-31 21:37:01 +0900

    문제 6 : 기능 2 - 어떤 두 사람의 닉네임이 같은 글자를 연속적으로 포함하는지 검사하는 기능.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,10 +3,28 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        for (int i = 0; i < forms.size(); i++) {
-            for (int j = i + 1; j < form.size(); j++) {
+    public static boolean containsSameLetterConsecutively(String name1, String name2) {
+        for (int i = 0; i + 1 < name1.length(); i++) {
+            for (int j = 0; j  + 1 < name2.length(); j++) {
+                if (name1.charAt(i) != name2.charAt(j)) continue;
+                if (name1.charAt(i + 1) != name2.charAt(j + 1)) continue;
+                return true;
+            }
+        }
 
+        return false;
+    }
+    public static List<String> solution(List<List<String>> forms) {
+        boolean addToAnswer[] = new boolean[forms.size()];
+
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = i + 1; j < forms.size(); j++) {
+                String name1 = forms.get(i).get(1);
+                String name2 = forms.get(j).get(1);
+                if (containsSameLetterConsecutively(name1, name2)) {
+                    addToAnswer[i] = true;
+                    addToAnswer[j] = true;
+                }
             }
         }
 
