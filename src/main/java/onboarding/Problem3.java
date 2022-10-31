959f8758ef743ea57a2926bba5050884cd429f10
minju412 <ln8847@ajou.ac.kr>
2022-10-31 17:31:40 +0900

    style: [p3] apply checkstyle

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,21 +1,21 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int cnt = 0;
+	public static int solution(int number) {
+		int cnt = 0;
 
-        for (int i=1; i<=number; i++) {
-            String str = String.valueOf(i); // int -> string 변환
+		for (int i = 1; i <= number; i++) {
+			String str = String.valueOf(i); // int -> string 변환
 
-            // 예를 들어, 36 이라는 숫자에는 박수 두 번
-            for (int j=0; j<str.length(); j++) {
-                char c = str.charAt(j);
-                if (c == '3' || c == '6' || c == '9') {
-                    cnt++;
-                }
-            }
-        }
+			// 예를 들어, 36 이라는 숫자에는 박수 두 번
+			for (int j = 0; j < str.length(); j++) {
+				char c = str.charAt(j);
+				if (c == '3' || c == '6' || c == '9') {
+					cnt++;
+				}
+			}
+		}
 
-        return cnt;
-    }
+		return cnt;
+	}
 }
