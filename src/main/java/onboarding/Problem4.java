65ff461f37e1f2e0260f7fc07f225dd21dfa58d5
Young37 <20171470@g.dongseo.ac.kr>
2022-10-31 20:57:57 +0900

    입력값 StringBuilder로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+    	StringBuilder wordSB = new StringBuilder(word);
+    	return "";
     }
 }
