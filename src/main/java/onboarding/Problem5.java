ef2091e3a909c0150cad2d18e94e12d188af18af
cmw9957 <cmw9957@gmail.com>
2022-10-31 18:11:40 +0900

    comment: 기능 목록 주석 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+========================================Problem5 기능 목록========================================
+- 주어진 금액으로 나눌 수 있는지 확인하는 기능
+- 주어진 금액으로 나누는 기능
+- 주어진 금액으로 나눈 값을 리스트에 저장하는 기능
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
