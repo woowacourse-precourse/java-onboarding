def6902cb265cac5b07d821a50ed2b9c15fff4af
jeonghoon <shb03323@naver.com>
2022-10-31 18:24:12 +0900

    feat: problem1 입력값이 올바른 페이지 형식인지 확인해주는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,28 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = -1;
+
+        if (isException(pobi) || isException(crong)) {
+            return answer;
+        }
+
         return answer;
+    }
+
+    private static boolean isException(List<Integer> person) {
+        int leftPage = person.get(0);
+        int rightPage = person.get(1);
+        int pageDifference = rightPage - leftPage;
+
+        if (pageDifference != 1) {
+            return true;
+        } else if (rightPage > 400 || leftPage < 1) {
+            return true;
+        } else if (rightPage % 2 == 1) {
+            return true;
+        }
+
+        return false;
     }
 }
