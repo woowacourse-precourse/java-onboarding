9a55b7b2c0cde324365a7058aa29d4ac671f11bf
LeeMyeongJin <dlaudwls1203@naver.com>
2022-11-01 03:17:33 +0900

    Problem1 sol

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            int current = i;
+            int temp = count;
+            while (current != 0) {
+                if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9){
+                    count++;
+                }
+                current /= 10;
+            }
+        }
+        return count;
     }
 }
