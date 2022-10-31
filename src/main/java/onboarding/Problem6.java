800fcf98822dd799201f8cbb6e92591974fdb2d6
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-10-31 21:02:08 +0900

    Revert "Feat : 문제 6 기능 6.1 구현 및 테스트"
    This reverts commit c943a6008b6767b776c50e3a32bfd09c7bbddcb8.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,45 +1,10 @@
 package onboarding;
 
-import java.util.*;
+import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-
-        List<String> answer = null;
-
+        List<String> answer = List.of("answer");
         return answer;
     }
-
-    /**
-     * 6.1
-     * 두 닉네임에 중복이 존재하는지 판별
-     */
-    public static boolean isDupWordExist(String word1, String word2) {
-        int wordLength = 2;
-
-        while (wordLength <= Math.min(word1.length(), word2.length())) {
-            for (int i = 0; i <= (word1.length() - wordLength); i++) {
-                String subWord = word1.substring(i, i + wordLength);
-
-                if (hasSubWord(word2, subWord))
-                    return true;
-            }
-            wordLength++;
-        }
-        return false;
-    }
-
-    /**
-     * 6.1.1
-     * 단어 word에 subword가 존재하는지 판별
-     */
-    public static boolean hasSubWord(String word, String subWord) {
-        for (int i = 0; i <= (word.length() - subWord.length()); i++)
-            if (word.substring(i, i + subWord.length()).equals(subWord))
-                return true;
-
-        return false;
-    }
-
-
 }
