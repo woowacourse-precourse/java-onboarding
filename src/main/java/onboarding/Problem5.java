0d9861dd718846052a2b42930a3fd719496369e5
YongGwan <32181771@dankook.ac.kr>
2022-10-31 13:40:30 +0900

    problem5 clear

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(9);
+        for (int index = 0; index < 9; index++) {
+            int won;
+            if ((index % 2 == 0) && index != 8) {
+                won = (int)(5 * Math.pow(10,4-index/2));
+                answer.add(money / won);
+            } else if (index == 8) {
+                won = 1;
+                answer.add(money/won);
+            }
+            else {
+                won = (int)(1 * Math.pow(10,4- index/2));
+                answer.add(money / won);
+            }
+            money = money % won;
+        }
         return answer;
     }
 }
