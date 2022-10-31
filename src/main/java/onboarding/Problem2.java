dd08414ce9009e71dc073f780d7b393e19df8bdc
dvlpr0 <rladjswkd@naver.com>
2022-10-31 19:25:05 +0900

    added/Problem2: 주어진 문자열의 길이가 [1,1000]인지 확인하고 모두 소문자로 이루어져있는지 확인하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem2 {
+    private static boolean isAllLowerCase(String cryptogram)
+    {
+        int i = 0;
+        int len = cryptogram.length();
+
+        while (i < len && Character.isLowerCase(cryptogram.charAt(i))) {
+            i++;
+        }
+        return (i == len);
+    }
+    private static boolean isValidLength(String cryptogram)
+    {
+        int len = cryptogram.length();
+
+        return (0 < len && len < 1001);
+    }
+    private static boolean isValidCryptogram(String cryptogram)
+    {
+        return (cryptogram != null && isAllLowerCase(cryptogram) && isValidLength(cryptogram));
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        if (!isValidCryptogram(cryptogram))
+            throw (new RuntimeException("유효하지 않은 문자열"));
+        return ("");
     }
 }
