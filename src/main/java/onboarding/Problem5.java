baf333dbec051fad78df0b15b3b86a9f27762b68
footdev <ekclstkfka5@gmail.com>
2022-11-01 00:47:15 +0900

    feat: 기능5 메소드 명세 추가 및 setExchangedMoney 메소드 구현
    메소드 명세를 추가하고 교환된 돈이 담긴 리스트를 만드는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,16 +1,63 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int totalMoney;
 
     public static List<Integer> solution(int money) {
+        totalMoney = money;
         List<Integer> answer = exchangeMoney(money);
         return answer;
     }
 
     public static List<Integer> exchangeMoney(int money) {
         return Collections.emptyList();
+    }
+
+    public static List<Integer> setExchangedMoney() {
+        List<Integer> exchangedMoney = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            exchangedMoney.add(0);
+        }
+        return exchangedMoney;
+    }
+
+    public static int exchangeFiftyThousandWon() {
+        return 0;
+    }
+
+    public static int exchangeTenThousandWon() {
+        return 0;
+    }
+
+    public static int exchangeFiveThousandWon() {
+        return 0;
+    }
+
+    public static int exchangeOneThousandWon() {
+        return 0;
+    }
+
+    public static int exchangeFiveHundredWon() {
+        return 0;
+    }
+
+    public static int exchangeOneHundredWon() {
+        return 0;
+    }
+
+    public static int exchangefiftyWon() {
+        return 0;
+    }
+
+    public static int exchangeTenWon() {
+        return 0;
+    }
+
+    public static int exchangeOneWon() {
+        return 0;
     }
 }
