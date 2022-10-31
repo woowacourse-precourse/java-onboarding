1075a1359551c31ae8c1151252de6ff80b3eee23
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-11-01 00:17:00 +0900

    feat(Problem7): 크루 목록 반환 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,24 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
+    public static Set<String> getCrewList(List<List<String>> friends, List<String> visitors) {
+        Set<String> crewSet = new HashSet<>();
+        for (List<String> friend : friends) {
+            for (String name : friend) {
+                crewSet.add(name);
+            }
+        }
+        for (String visitor : visitors) {
+            if (!crewSet.contains(visitor)) crewSet.add(visitor);
+        }
+
+        return crewSet;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
