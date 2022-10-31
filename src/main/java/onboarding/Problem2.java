96114f05f3e8cd45f3f6d9788662df558a362716
WonSubSub <boxer1995@naver.com>
2022-10-31 11:28:51 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        int count = 0;
+        do {
+            count = 0;
+            for(int i = 0; i < answer.length()-1; i++){
+                if (answer.charAt(i) == answer.charAt(i+1)){
+                    answer = answer.substring(0,i) + answer.substring(i+2);
+                    count += 1;
+                    break;
+                } // if - 앞뒤문자비교
+            }
+        } while(count != 0);
+
         return answer;
     }
 }
