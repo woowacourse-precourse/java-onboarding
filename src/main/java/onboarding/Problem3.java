db4a312b2abf03e68d4f44e8a042d64e23bb8fb8
gomljo <gomljo0307@naver.com>
2022-10-31 16:21:47 +0900

    feat(Problem3.java): 기능 #2 구현
    주어진 숫자를 자릿수별로 나누어 배열의 형태로 전달하는 divideEachDigit 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,15 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+class Utility{
+    int[] divideEachDigit(int number) {
+        int[] digitNum;
+        digitNum = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+        return digitNum;
+    }
+}
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
