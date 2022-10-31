75ab05f1953fea329cc464b2781837ef1d6422c9
heesang <dlagmltkd12@naver.com>
2022-10-31 23:36:22 +0900

    feat(problem7): Problem7 클라이언트 코드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
+import problem7.FriendCommendResponseDto;
+import problem7.UserRepository;
+import problem7.UserService;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        UserService userService = new UserService(new UserRepository());
+        userService.addFriends(friends);
+        List<FriendCommendResponseDto> responseDtos = userService.operateFriendCommendation(user, visitors);
+
+        return responseDtos.stream().map(FriendCommendResponseDto::getUserId).collect(Collectors.toList());
     }
 }
