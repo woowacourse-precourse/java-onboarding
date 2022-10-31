d8c8d2a9908408c51339104e4f4a310639674d61
한은영 <oneeqzero@gmail.com>
2022-10-31 17:29:40 +0900

    feat: problem6 중복이름 확인 exist_duplist 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,21 +3,41 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
-    static Set<String> dup_list = new HashSet<String>();
-
+    static Map<String, Set<String>> dup_list = new HashMap<String, Set<String>> (){
+        @Override
+        public Set<String> get(Object key){
+            Set<String> set = super.get(key);
+            if(set == null){
+                set = new HashSet<String>();
+                put((String) key,set);
+            }
+            return set;
+        }
+    };
     public static void make_duplist(List<List<String>> forms){
         for(List<String> f : forms){
             String nickname = f.get(1);
             for(int i = 0; i<nickname.length()-1; i++){
                 String sub_nick = nickname.substring(i,i+2);
-                dup_list.add(sub_nick);
+                dup_list.get(nickname).add(sub_nick);
             }
         }
     }
+    public static boolean exist_duplist(String name){
+        for(int i = 0; i<name.length()-1; i++){
+            String sub_name = name.substring(i,i+2);
+            Iterator iter = dup_list.keySet().iterator();
+            while(iter.hasNext()){
+                String key = iter.next().toString();
+                if(key.equals(name)) continue;
+                if(dup_list.get(key).contains(sub_name)) return true;
+            }
+        }
+        return false;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
         make_duplist(forms);
-
         return answer;
     }
 }
