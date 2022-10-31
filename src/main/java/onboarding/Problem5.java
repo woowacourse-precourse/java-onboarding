4887135fe72c2a08b3e0e220d16b8c59f310ae30
cmw9957 <cmw9957@gmail.com>
2022-10-31 20:50:58 +0900

    feat: 주어진 금액으로 나눌 수 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,6 +13,10 @@ import java.util.List;
 public class Problem5 {
     int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+    public static boolean checkDivisibleByUnit(int money, int unit){
+        return (money / unit) > 0 ? true : false;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
