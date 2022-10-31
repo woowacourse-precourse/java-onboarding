6ceb7d5f620cfb4c5890c70b121109eb27198954
yhlee9098 <yhlee9098@gmail.com>
2022-11-01 00:03:31 +0900

    Problem 3 요약사항 * 구현 기능 - 특정 문자열 포함 시 Count 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        return answer;
+
+        // Do loop until input value
+        for (int i = 1; i <= number; ++i) {
+            // Convert number to string
+            String input = Integer.toString(i);
+
+            for (int j = 0; j < input.length(); ++j) {
+                // Get each number as a string
+                char checkNumber = input.charAt(j);
+                // Count if it equals to number that occurs clap
+                if (checkNumber == '3' || checkNumber == '6' || checkNumber == '9') {
+                    answer += 1;
+                }
+            }
+        }
+        return answer ;
     }
 }
