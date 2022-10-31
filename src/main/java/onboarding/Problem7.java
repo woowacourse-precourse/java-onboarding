437a1dc37c544fd0389efc6396a60393a9b59d28
Jueun <jueun000101@gmail.com>
2022-10-31 21:01:27 +0900

    feat: 친구 관계 인접 리스트로 표현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,6 +12,29 @@ import java.util.*;
  * 6. 최대 5명의 이름 반환하기
  */
 public class Problem7 {
+    static Map<String, List<String>> makeRelation(List<List<String>> friends) {
+        Map<String, List<String>> relation = new HashMap<>();
+
+        for (List<String> friend: friends) {
+            String p0 = friend.get(0), p1 = friend.get(1);
+
+            List<String> l0 = new ArrayList<>();
+            if (relation.containsKey(p0)) {
+                l0 = relation.get(p0);
+            }
+            l0.add(p1);
+            relation.put(friend.get(0), l0);
+
+            List<String> l1 = new ArrayList<>();
+            if (relation.containsKey(p1)) {
+                l1 = relation.get(p1);
+            }
+            l1.add(p0);
+            relation.put(friend.get(1), l1);
+        }
+        return relation;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
