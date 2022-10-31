fedb0d592fa749d128f6ea00eb739d9a2f10703f
박세원 <tpdnjs1825@naver.com>
2022-11-01 01:22:38 +0900

    Style : Problem1 소스코드 정렬

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,10 +5,11 @@ import java.util.List;
 class Problem1 {
     private static final int LEFT_PAGE = 0;
     private static final int RIGHT_PAGE = 1;
-    private static int getMaxScore(int page){
+
+    private static int getMaxScore(int page) {
         int sum = 0;
         int multiplication = 1;
-        while(page > 0){
+        while (page > 0) {
             sum += page % 10;
             multiplication *= page % 10;
             page /= 10;
@@ -16,24 +17,24 @@ class Problem1 {
         return Math.max(sum, multiplication);
     }
 
-    private static int determineWinner(int pobiMaxScore, int crongMaxScore){
-        if(pobiMaxScore > crongMaxScore) {
+    private static int determineWinner(int pobiMaxScore, int crongMaxScore) {
+        if (pobiMaxScore > crongMaxScore) {
             return 1;
         }
-        if(pobiMaxScore < crongMaxScore) {
+        if (pobiMaxScore < crongMaxScore) {
             return 2;
         }
         return 0;
     }
 
-    private static boolean exceptionHandler(int leftPage, int rightPage){
-        if(Math.abs(leftPage - rightPage) != 1) {
+    private static boolean exceptionHandler(int leftPage, int rightPage) {
+        if (Math.abs(leftPage - rightPage) != 1) {
             return false;
         }
-        if(leftPage % 2 != 1) {
+        if (leftPage % 2 != 1) {
             return false;
         }
-        if(rightPage % 2 != 0) {
+        if (rightPage % 2 != 0) {
             return false;
         }
         return true;
@@ -44,7 +45,7 @@ class Problem1 {
         int pobiMaxScore = Integer.MIN_VALUE;
         int crongMaxScore = Integer.MIN_VALUE;
 
-        if(!exceptionHandler(pobi.get(LEFT_PAGE), pobi.get(RIGHT_PAGE)) || !exceptionHandler(crong.get(LEFT_PAGE), crong.get(RIGHT_PAGE))) {
+        if (!exceptionHandler(pobi.get(LEFT_PAGE), pobi.get(RIGHT_PAGE)) || !exceptionHandler(crong.get(LEFT_PAGE), crong.get(RIGHT_PAGE))) {
             return -1;
         }
 
