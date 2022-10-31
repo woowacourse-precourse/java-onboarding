487a4b5b6b27ebf7c8cb6401607af1a03e85c68d
이정우 <acg6138@naver.com>
2022-10-31 15:04:18 +0900

    feat(Problem2): solution 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeDuplication(cryptogram);
         return answer;
     }
 
