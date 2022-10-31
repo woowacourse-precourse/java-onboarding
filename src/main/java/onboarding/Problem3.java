6017ea147fe7e3659ea7d026f430f1dfe90cbdfe
ChaCha3088 <cha3088@gmail.com>
2022-10-31 23:01:37 +0900

    feat: problem3 new function countTargetNumber

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,12 +4,21 @@ import java.util.stream.Stream;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clapCount = 0;
+
     }
 
     private static int[] splitEachNumbers(int number) {
         int[] arrNum = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
         return arrNum;
+    }
+
+    private static void countTargetNumber(int[] arrNum) {
+        for (int i = 0; i<arrNum.length; i++) {
+            if (i == 3 || i == 6 || i == 9) {
+                clapCount = clapCount + 1;
+            }
+        }
+
     }
 }
