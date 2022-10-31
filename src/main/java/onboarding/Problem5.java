905575c30479464d4b8b35396c967a5cfd7d0c5e
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-01 01:30:29 +0900

    Feat(Problem5): 5만원부터 1원까지 차례로 사용하여 money 만들기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,10 +1,11 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    static class Money{
+    static class Money {
         int cost;
 
         Money(int cost) throws Exception {
@@ -17,21 +18,28 @@ public class Problem5 {
         private boolean isValidCost(int cost) {
             return cost >= 1 && cost <= 1000_000;
         }
-
     }
+    static int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
         Money m;
         try {
             m = new Money(money);
         } catch (Exception e) {
-            return answer;
+            return Collections.emptyList();
         }
 
         money = m.cost;
+        return getResultList(money);
+    }
 
-
-        return answer;
+    private static List<Integer> getResultList(int money) {
+        List<Integer> result =  new ArrayList<>();
+        for (int i = 0; i <currency.length ; i++) {
+            int temp = money / currency[i];
+            result.add(temp);
+            money %= currency[i];
+        }
+        return result;
     }
 }
