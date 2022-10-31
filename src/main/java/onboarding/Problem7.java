c54295ef8e4a45809dc0e38655a6089b6e846827
kmw2378 <rlarla677@gmail.com>
2022-10-31 20:33:45 +0900

    [온보딩] 김민우 미션 제출합니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -28,47 +28,16 @@ public class Problem7 {
 
         public List<String> result() {
 
-            List<String> acquaintance = new ArrayList<>();
+            List<String> acquaintance = getAcquaintance();
             Map<String, Integer> result = new HashMap<>();
 
-            for (List<String> friend : friends) {
+            calculateAcquaintanceScore(acquaintance, result);
+            calculateVisitorScore(acquaintance, result);
 
-                if (friend.get(0).equals(user)) {
-                    acquaintance.add(friend.get(1));
-                } else if (friend.get(1).equals(user)) {
-                    acquaintance.add(friend.get(0));
-                }
-            }
+            return getAnswer(result);
+        }
 
-            for (List<String> friend : friends) {
-
-                for (String aqt : acquaintance) {
-                    if (friend.get(0).equals(aqt) && !friend.get(1).equals(user)) {
-                        result.put(friend.get(1), getValue(result, friend.get(1)) + 10);
-                    } else if (friend.get(1).equals(aqt) && !friend.get(0).equals(user)) {
-                        result.put(friend.get(0), getValue(result, friend.get(0)) + 10);
-                    }
-                }
-            }
-
-            for (String visitor : visitors) {
-
-                boolean isFriend = false;
-
-                for (String aqt : acquaintance) {
-
-                    if (aqt.equals(visitor)) {
-                        isFriend = true;
-                        break;
-                    }
-                }
-
-                if (!isFriend) {
-                    result.put(visitor, getValue(result, visitor) + 1);
-                }
-
-            }
-
+        private static List<String> getAnswer(Map<String, Integer> result) {
             List<Map.Entry<String, Integer>> collect = result.entrySet().stream()
                     .sorted(Map.Entry.comparingByKey())
                     .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
@@ -86,8 +55,56 @@ public class Problem7 {
                 answer.add(nameScoreEntity.getKey());
                 count++;
             }
-
             return answer;
+        }
+
+        private void calculateAcquaintanceScore(List<String> acquaintance, Map<String, Integer> result) {
+            for (List<String> friend : friends) {
+
+                for (String aqt : acquaintance) {
+                    if (friend.get(0).equals(aqt) && !friend.get(1).equals(user)) {
+                        result.put(friend.get(1), getValue(result, friend.get(1)) + 10);
+                    } else if (friend.get(1).equals(aqt) && !friend.get(0).equals(user)) {
+                        result.put(friend.get(0), getValue(result, friend.get(0)) + 10);
+                    }
+                }
+            }
+        }
+
+        private List<String> getAcquaintance() {
+
+            List<String> acquaintance = new ArrayList<>();
+
+            for (List<String> friend : friends) {
+
+                if (friend.get(0).equals(user)) {
+                    acquaintance.add(friend.get(1));
+                } else if (friend.get(1).equals(user)) {
+                    acquaintance.add(friend.get(0));
+                }
+            }
+
+            return acquaintance;
+        }
+
+        private void calculateVisitorScore(List<String> acquaintance, Map<String, Integer> result) {
+            for (String visitor : visitors) {
+
+                boolean isFriend = false;
+
+                for (String aqt : acquaintance) {
+
+                    if (aqt.equals(visitor)) {
+                        isFriend = true;
+                        break;
+                    }
+                }
+
+                if (!isFriend) {
+                    result.put(visitor, getValue(result, visitor) + 1);
+                }
+
+            }
         }
 
         private int getValue(Map<String, Integer> result, String friend) {
