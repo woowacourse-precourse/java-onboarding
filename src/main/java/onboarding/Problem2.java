d652a84215bb6937d13619d9ea707fcf9d3e7461
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-10-31 23:24:44 +0900

    2번 문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,50 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        List<Character> list = new ArrayList<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            list.add(cryptogram.charAt(i));
+        }
+
+        List<Character> tmp = new ArrayList<>();
+
+        tmp.add(list.get(0));
+        int listIndex = 1;
+        int tmpIndex = 0;
+        while (listIndex < list.size()) {
+            if(tmp.size()==0){
+                tmp.add(list.get(listIndex));
+                listIndex++;
+            }
+            else{
+                if (list.get(listIndex) == tmp.get(tmpIndex)) {
+                    if (listIndex == list.size() - 1) {
+                        tmp.remove(tmpIndex);
+                        break;
+                    } else {
+                        if (list.get(listIndex) != list.get(listIndex + 1)) {
+                            tmp.remove(tmpIndex);
+                            if(tmpIndex!=0)
+                                tmpIndex--;
+                        }
+                        listIndex++;
+                    }
+                } else {
+                    tmp.add(list.get(listIndex));
+                    tmpIndex++;
+                    listIndex++;
+                }
+            }
+        }
+
+        for (int i = 0; i < tmp.size(); i++) answer.append(tmp.get(i));
+
+        return answer.toString();
     }
 }
