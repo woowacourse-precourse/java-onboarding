ad13be6006cd06ee122ecafafd1a5f9eda733aee
jojaeng2 <ds4ouj@naver.com>
2022-10-31 12:50:50 +0900

    feat : 알파벳을 뒤집는 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,10 +5,30 @@ import java.util.Map;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Frog frog = new Frog(reverseAlpha());
+        return frog.speak(word);
     }
 
+    static class Frog {
+        private final Map<Character, Character> mapper;
+
+        public Frog(Map<Character, Character> mapper) {
+            this.mapper = mapper;
+        }
+
+        private String speak(String word) {
+            String result = "";
+            for (int i = 0; i < word.length(); i++) {
+                Character key = word.charAt(i);
+                if (key.equals(' ')) {
+                    result += ' ';
+                } else {
+                    result += mapper.get(key);
+                }
+            }
+            return result;
+        }
+    }
     private static Map<Character, Character> reverseAlpha() {
         Map<Character, Character> alphabets = new HashMap<>();
         for (int i = 0; i < 26; i++) {
