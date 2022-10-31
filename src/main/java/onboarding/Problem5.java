cef0cef1e6a2a00bd281cc67805e344ef5034f7e
TaeMin <xoals3094@naver.com>
2022-10-31 21:38:13 +0900

    Problem5: add withdraw method

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,6 +9,17 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<Integer> withdraw(){
+        int[] moneyUnitList = {50000,10000,5000,1000,500,100,50,10,1};
+        List<Integer> unitCount = new ArrayList<Integer>();
+        for(int i = 0; i < moneyUnitList.length; i++){
+            int unit = moneyUnitList[i];
+            int count = countMoneyByUnit(unit);
+            unitCount.add(count);
+        }
+        return unitCount;
     }
 
     public static int countMoneyByUnit(int unit){
