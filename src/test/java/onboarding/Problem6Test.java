544b774d0f4d8eb2f1c2eaed012bbd2370b5c2b4
정현수 <wjdgustn15@naver.com>
2022-10-31 17:52:39 +0900

    test : add Problem6Test
    * 크루닉네임이 한글자일떄, 테스트 추가

--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -12,39 +12,66 @@ public class Problem6Test {
 
     @Test
     void worstCaseTimeTest() {
-        List<List<String>> forms =new ArrayList<>();
+        List<List<String>> forms = new ArrayList<>();
         forms.addAll(
                 List.of(
-                    List.of("jm@email.com", "제이엠"),
-                    List.of("jason@email.com", "제이슨"),
-                    List.of("woniee@email.com", "워니"),
-                    List.of("mj@email.com", "엠제이"),
-                    List.of("nowm@email.com", "이제엠")
+                        List.of("jm@email.com", "제이엠"),
+                        List.of("jason@email.com", "제이슨"),
+                        List.of("woniee@email.com", "워니"),
+                        List.of("mj@email.com", "엠제이"),
+                        List.of("nowm@email.com", "이제엠")
                 )
         );
-        List <String>idParts = List.of("a","b","c","d","q");
-        List<String>nickParts=List.of("가","나","다","라","마","바","ㅋ","ㅂ");
+        List<String> idParts = List.of("a", "b", "c", "d", "q");
+        List<String> nickParts = List.of("가", "나", "다", "라", "마", "바", "ㅋ", "ㅂ");
 
-        for (int i = 0; i <10_000-5 ; i++) {
-            String id=makeTestStr(idParts,10)+"@email.com";
-            String nickName = makeTestStr(nickParts,20);
+        for (int i = 0; i < 10_000 - 5; i++) {
+            String id = makeTestStr(idParts, 10) + "@email.com";
+            String nickName = makeTestStr(nickParts, 20);
 
-            forms.addAll(List.of(List.of(id,nickName)));
+            forms.addAll(List.of(List.of(id, nickName)));
         }
         long startTime = System.currentTimeMillis();
         Problem6.solution(forms);
-        long endTime =System.currentTimeMillis();
-        long time = (endTime-startTime)/1000;
+        long endTime = System.currentTimeMillis();
+        long time = (endTime - startTime) / 1000;
         assertThat(1L).isGreaterThan(time);
-
-
     }
 
-    private static String makeTestStr(List<String> list,int size) {
-        StringBuffer id= new StringBuffer();
+    private static String makeTestStr(List<String> list, int size) {
+        StringBuffer id = new StringBuffer();
         for (int j = 0; j < size; j++) {
             id.append(new Random().nextInt(list.size()));
         }
         return id.toString();
     }
+
+    @Test
+    void crewNameSizeOneTest() {
+        List<List<String>> forms = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "워"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+        List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
+        assertThat(Problem6.solution(forms)).isEqualTo(result);
+    }
+
+    @Test
+    void crewNameSizeHasNotAnswerTest() {
+        List<List<String>> forms = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "워"),
+                List.of("woniee1@email.com", "워"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+        List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
+        assertThat(Problem6.solution(forms)).isEqualTo(result);
+    }
+
+
 }
