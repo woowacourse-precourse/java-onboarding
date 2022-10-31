2dc45fc1d8b097017f1a19668570b09b25105f88
can019 <jys01012@gmail.com>
2022-11-01 07:42:07 +0900

    :sparkles: Solutionn 6 구현 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,54 @@
 package onboarding;
 
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Problem6 {
+    private static final int UTF_8_KOR_BYTE = 3;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(List<String> list:forms){
+            String currentEmail = list.get(0);
+            String currentNickName = list.get(1);
+
+            int currentOffset = 0;
+            // tokenize and add email
+            // map key로 nickname을 차례로 잘라 만들 수 있는 두 글자, value에는 email list 추가
+            // key ex) 안녕하세요 -> 안녕, 녕하, 하세, 세요
+            while(currentOffset < currentNickName.getBytes(StandardCharsets.UTF_8).length - UTF_8_KOR_BYTE){
+
+                // 두 글자 생성
+                String possibleKorean2Word = new String(currentNickName.getBytes(), currentOffset, UTF_8_KOR_BYTE * 2);
+
+                if(map.containsKey(possibleKorean2Word)){
+                    List<String> newList = new ArrayList<>();
+
+                    newList.addAll(map.get(possibleKorean2Word));
+                    newList.add(currentEmail);
+
+                    map.replace(possibleKorean2Word,newList);
+                } else{
+                   map.put(possibleKorean2Word,Arrays.asList(currentEmail));
+                }
+                currentOffset += UTF_8_KOR_BYTE;
+            }
+
+        }
+        Set<String> duplicatedEmailOfNickName = new HashSet<>();
+
+        for( String strKey : map.keySet() ){
+            List<String> emailList = map.get(strKey);
+            if(emailList.size()>1) {
+                duplicatedEmailOfNickName.addAll(emailList);
+            }
+        }
+
+        List<String> setToList = new ArrayList<>(duplicatedEmailOfNickName);
+
+        Collections.sort(setToList);
+
+        return setToList;
     }
 }
