494ae3ec8f5d6583c5ba5cdd62aef4f499ace845
jhsseonn <hosu0125@gmail.com>
2022-10-31 23:27:09 +0900

    feat: (Problem2) 문자열을 알파벳 리스트로 변환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        List<Character> code = new ArrayList<>();
+        code = convert(cryptogram);
+
         return answer;
+    }
+
+    //문자열을 알파벳 리스트로 변환 함수
+    static List<Character> convert(String string) {
+        List<Character> chars = IntStream.range(0, string.length())
+                .mapToObj(string::charAt)
+                .collect(Collectors.toList());
+
+        return chars;
     }
 }
