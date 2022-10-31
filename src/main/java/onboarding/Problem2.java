32b08c47243a42162caabb65806de1dc37929196
조병우 <whquddn55@gmail.com>
2022-10-31 21:27:21 +0900

    Add: Problem2 Sovled

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder stringBuilder = new StringBuilder(cryptogram);
+        while(true) {
+            List<Integer> nearByDuplication = getNearByDuplication(stringBuilder);
+            if (nearByDuplication.isEmpty()) {
+                break;
+            }
+            removeNearByDuplication(stringBuilder, nearByDuplication);
+        }
+        return stringBuilder.toString();
+    }
+
+    private static void removeNearByDuplication(StringBuilder stringBuilder, List<Integer> nearByDuplication) {
+        nearByDuplication.sort(Comparator.reverseOrder());
+        nearByDuplication.forEach(stringBuilder::deleteCharAt);
+    }
+
+    private static List<Integer> getNearByDuplication(StringBuilder stringBuilder) {
+        List<Integer> ret = new ArrayList<>();
+
+        for (int i = 0; i < stringBuilder.length() - 1; ++i) {
+            if (stringBuilder.charAt(i) == stringBuilder.charAt(i + 1)) {
+                ret.add(i);
+                ret.add(i + 1);
+            }
+        }
+        return ret;
     }
 }
