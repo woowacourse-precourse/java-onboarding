4a3dd1e530d206ebef5a73b999a101339d6ff8bf
Hongsugyeong <olivia258@naver.com>
2022-10-31 15:05:13 +0900

    [Problem7] 기능 목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    /***
+     * user의 친구 추출
+     * user와 함께 아는 친구 추출
+     * 방문자 추출
+     * 점수 내림차순 정렬
+     * map의 key 값만 추출
+     * 이미 친구인 사람 추출
+     * 리스트에서 이미 친구인 사람 삭제
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
