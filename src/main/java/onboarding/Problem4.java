4a6c35caabf146ec83265546c1d12afb454c51ca
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:09:35 +0900

    4번: 소문자 대문자 반대 문자 구하는 메서드 구분하여 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,12 +2,34 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
 
-        return answer;
+        for(int i=0; i<word.length(); i++){
+            answer.append(getOppositeAlpha(word.charAt(i)));
+        }
+
+        return answer.toString();
     }
 
     private static char getOppositeAlpha(char targetChar){
+        int asciiOrder = targetChar - 'a';
+        int convertOrder = 'z' - asciiOrder;
+
+        char convertChar = (char)convertOrder;
+        System.out.println(convertChar);
+        return convertChar;
+    }
+
+    private static char getUpperCharOppositeAlpha(char targetChar){
+        int asciiOrder = targetChar - 'A';
+        int convertOrder = 'Z' - asciiOrder;
+
+        char convertChar = (char)convertOrder;
+        System.out.println(convertChar);
+        return convertChar;
+    }
+
+    private static char getLowerAlpha(char targetChar){
         int asciiOrder = targetChar - 'a';
         int convertOrder = 'z' - asciiOrder;
 
