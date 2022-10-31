81f6e452f06682c557d73c4dae908ef2e9dca4b0
KTH-96 <taehyun960@gmail.com>
2022-10-31 16:47:30 +0900

    comment(Problem6): 기능구현 목록 정리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,12 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    /**
+     * 기능 목록 작성
+     * 1. 중복 체크를 할 수 있는 패턴들을 찾는 기능
+     * 2. 패턴에 중복되는 닉네임이 2개 이상일때 set에 추가하는 기능
+     */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
