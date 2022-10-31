e6f77acdbc1e24bf930d1bafc01bc058f3d6152f
yubin <dbqls2821@gmail.com>
2022-10-31 22:34:34 +0900

    FEAT: [Problem7] init 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static List<String> alreadyFriend;
+    private static HashMap<String, Integer> scoreMap;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        init(user);
     }
+
+    private static void init(String user) {
+        alreadyFriend = new ArrayList<>();
+        scoreMap = new HashMap<>();
+        scoreMap.put(user, 0);
+    }
+
 }
