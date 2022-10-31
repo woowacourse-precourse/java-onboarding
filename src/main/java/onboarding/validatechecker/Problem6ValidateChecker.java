eb383efeca33eaabe478e06cea9e62bf3fcb31d2
hong-sile <gurwns9325@naver.com>
2022-11-01 05:02:28 +0900

    feat : 크루 수가 0이거나, 10,000 초과인 경우 IllegalArgumentException 발생

--- src/main/java/onboarding/validatechecker/Problem6ValidateChecker.java
+++ src/main/java/onboarding/validatechecker/Problem6ValidateChecker.java
@@ -5,7 +5,12 @@ import java.util.List;
 public class Problem6ValidateChecker {
 	public static void isFormValidate(List<List<String>> form){
 		isFormNotNull(form);
+		isCrewSizeInRange(form);
+	}
 
+	private static void isCrewSizeInRange(List<List<String>> form) {
+		if(form.size() == 0 || form.size() > 10000)
+			throw new IllegalArgumentException("크루의 수는 1이상이거나, 10,000이하여야합니다.");
 	}
 
 	private static void isFormNotNull(List<List<String>> form) {
