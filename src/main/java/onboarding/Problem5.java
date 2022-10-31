7fe1e35d828134c6a4fb89986e33e71c13f47237
권민정 <22alswjd@naver.com>
2022-11-01 02:17:01 +0900

    feat: Problem5 기능 '1. 화폐 금액 타입 리스트 내림차순 선언' 완료
    50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 순으로 리스트에 들어감

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,9 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
 
-        // 1. 화폐 금액 리스트 내림차순 선언
+        // 1. 화폐 금액 타입 리스트 내림차순 선언
+        List<Integer> types = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
         // 2. 각 금액이 반환될 수 있는 최대 갯수 구하기
 
         return answer;
