331320ffe66ec58582e2ce3086971aea6b038cbb
hi-june <wnsgur735@gmail.com>
2022-10-31 17:59:53 +0900

    feat: LightWallet class의 인스턴스를 통한 solution 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -36,7 +36,12 @@ class LightWallet {
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        LightWallet wallet = new LightWallet();
+
+        wallet.countMoney(money, answer);
+
         return answer;
     }
 }
