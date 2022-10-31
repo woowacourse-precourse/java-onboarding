21fc42f254d9b96005eaec73408ba5f45cbc34f3
yxnxx <jh4rgh0@gmail.com>
2022-10-31 23:50:48 +0900

    feat: Add Problem6 checking nickname

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -9,9 +10,27 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         HashMap<String, String> crew_list = new HashMap<>();
         HashSet<String> email_list = new HashSet<>();
-        for (List<String> crew : forms) {
+//        for (List<String> crew : forms) {
+//            crew_list.put(crew.get(0), crew.get(1));
+//        }
+        List<String> email;
+        for(List<String> crew : forms) {
+            email = checkNickname(crew.get(1), crew_list);
+            if(email != null) {
+                //중복임. 이메일 목록에 추가
+            }
+            // 중복 확인 끝난 crew
             crew_list.put(crew.get(0), crew.get(1));
         }
         return answer;
+    }
+    public static List<String> checkNickname(String Nickname, HashMap<String, String> crew_list) {
+        if(crew_list.isEmpty()) return null;
+        List<String> nicklist = new ArrayList<>();
+        for(int i = 0; i < Nickname.length(); i++) {
+            String find_nick = Nickname.substring(i, i+2);
+            if(crew_list.containsValue(find_nick)) nicklist.add(crew_list.get(find_nick));
+        }
+        return nicklist;
     }
 }
