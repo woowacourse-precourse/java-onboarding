9dddc85be73dd43ae3b3240c59226ec2b560b1bf
hossang <zkzk104@naver.com>
2022-10-31 09:30:49 +0900

    feat: 문제6 구현 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,43 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        boolean[] overlapString = new boolean[forms.size()];
+        for (int i = 0; i < forms.size() - 1; i++) {
+            Set<String> set = new HashSet<>();
+            StringBuilder sb1 = new StringBuilder(forms.get(i).get(1));
+            if (sb1.length() == 1) {
+                continue;
+            }
+            for (int j = 0; j < sb1.length() - 1; j++) {
+                set.add(sb1.substring(j, j + 2));
+            }
+            for (int j = i + 1; j < forms.size(); j++) {
+                StringBuilder sb2 = new StringBuilder(forms.get(j).get(1));
+                if (sb2.length() == 1) {
+                    continue;
+                }
+                for (int k = 0; k < sb2.length() - 1; k++) {
+                    if (set.contains(sb2.substring(k, k + 2))) {
+                        overlapString[i] = true;
+                        overlapString[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < overlapString.length; i++) {
+            if (overlapString[i]) {
+                answer.add(forms.get(i).get(0));
+            }
+        }
+        Collections.sort(answer);
+
+
         return answer;
+
     }
 }
