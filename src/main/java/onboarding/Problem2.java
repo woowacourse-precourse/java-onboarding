cf837c9521fc545863edb8270e9b2b234015c7fb
hongsikcho <jjdd9758@naver.com>
2022-10-31 10:42:41 +0900

    feat #2 Problem2 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,53 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        char[] cry = cryptogram.toCharArray();
+        Stack stack = new Stack();
+        int count = 0;
+
+        for (int i = 0 ; i < cryptogram.length() ; i ++){
+            System.out.println(stack);
+
+            if(stack.size() == 0){
+                stack.push(cry[i]);
+                continue;
+            }
+
+            if(String.valueOf(stack.peek()).charAt(0) == cry[i]) {
+                count = 1;
+                continue;
+            }
+
+            if(count == 0){
+                stack.push(cry[i]);
+            }else{
+                stack.pop();
+                if(stack.size() == 0){
+                    stack.push(cry[i]);
+                    count = 0;
+                    continue;
+                } else if(String.valueOf(stack.peek()).charAt(0) == cry[i]) {
+                    continue;
+                }
+                stack.push(cry[i]);
+                count = 0;
+            }
+        }
+        if(count == 1){
+            stack.pop();
+        }
+
+        System.out.println(stack);
+        StringBuffer answer = new StringBuffer();
+
+        while (stack.size() != 0){
+            answer.append(stack.pop());
+        }
+        System.out.println(answer);
+        String a = String.valueOf(answer.reverse());
+        return a;
     }
 }
