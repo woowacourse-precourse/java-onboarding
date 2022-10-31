43f4d53c8a195140f2141c752cb716a7c97e5301
yoonjuho <dbswn990@naver.com>
2022-10-31 19:27:30 +0900

    feat: PROBLEM3 박수 개수 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,16 +1,30 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
         if(hasException(number)){
             return -1;
         }
-        
+        return getClapNum(number);
     }
 
     public static boolean hasException(int number){
         if(number<1 || number>10000)
             return true;
         return false;
+    }
+
+    public static int getClapNum(int number){
+        int result = 0;
+        for(int i = 1; i <= number; i++){
+            long count = Arrays.stream(Integer.toString(i).split(""))
+                    .map(Integer::parseInt)
+                    .filter(num -> num == 3 || num == 6 || num == 9)
+                    .count();
+            result += count;
+        }
+        return result;
     }
 }
