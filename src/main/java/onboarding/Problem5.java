31178aefe22bd6722027bdc55f9be7fbf0a42cec
Hyes-y <gptjs7970@naver.com>
2022-10-31 22:18:22 +0900

    feat: Problem5 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+//        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] temp = {};
+        int[] kinds = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int cur = money;
+        for (int val: kinds) {
+            int cnt = cur / val;
+            cur = cur % val;
+            answer.add(cnt);
+        }
         return answer;
     }
 }
