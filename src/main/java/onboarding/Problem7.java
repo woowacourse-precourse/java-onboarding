f927858422eafc7fd69e4cce759af0d2093abb9d
Sunghyun-0 <zkdldk95@naver.com>
2022-10-31 22:40:55 +0900

    (Problem7)점수획득기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,18 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    static HashMap<String,Integer> recommendPoint = new HashMap<>();
+    public static void calculatePoint(String target, Integer point){
+        if(!recommendPoint.containsKey(target)){ //add
+            recommendPoint.put(target,0);
+        }
+        recommendPoint.replace(target, recommendPoint.get(target)+ point);
+    }
+    
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
