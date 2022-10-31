f4eab2bca33036016dad8820dff72fe408bc816f
subin Park <psb0320@hs.ac.kr>
2022-10-31 19:41:11 +0900

    feat: 369 포함 여부 확인 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String str = String.valueOf(i);
+            if (is369Contained(str)) {
+                // 369 포함되어 있으면 개수 세서 answer에 추가
+            }
+        }
+
         return answer;
+    }
+
+    public static boolean is369Contained(String str) {
+        return str.contains("3") || str.contains("6") || str.contains("9");
     }
 }
