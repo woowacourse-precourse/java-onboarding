92e18cf94a837b8276df004d7fb08d1e552838f7
wjh2012 <wjh2012@naver.com>
2022-10-31 18:44:07 +0900

    (PROBLEM3)단일기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,49 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int clap [] = new int[number+1];
+
+        int quotient;
+        int remainder;
+
+        for (int i=1; i<number+1; i++){
+            if (i<10){
+                if (i%3 == 0){
+                    clap[i]++;
+                }
+
+            } else if (i<100) {
+                quotient = i/10;
+                remainder = i%10;
+
+                if (quotient%3==0) {
+                    clap[i]++;
+                }
+                clap[i]+=clap[remainder];
+
+            } else if (i<1000) {
+                quotient = i/100;
+                remainder = i%100;
+
+                if (quotient%3==0) {
+                    clap[i]++;
+                }
+                clap[i]+=clap[remainder];
+
+            } else if (i<10000) {
+                quotient = i/1000;
+                remainder = i%1000;
+
+                if (quotient%3==0) {
+                    clap[i]++;
+                }
+                clap[i]+=clap[remainder];
+            }
+
+            answer+=clap[i];
+        }
+
         return answer;
     }
 }
