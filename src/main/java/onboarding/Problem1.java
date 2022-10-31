5d638959844775d228ac6c150a75c57be1049e83
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 21:09:32 +0900

    [Problem 1] Overview

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,11 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        /*
+        1. 포비와 크롱의 페이지가 유효한 지 체크 - 예외 시 -1 리턴
+        2. 포비와 크롱, 각 자리수를 더하는 것과, 곱하는 것 중 최댓값을 찾음
+        3. 크기 비교하여 answer 리턴
+        */        
         return answer;
     }
 }
