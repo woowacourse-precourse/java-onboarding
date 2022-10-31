8e038dab7ca3649eb27e490b11965f9c5d6803f1
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-31 14:57:54 +0900

    solve problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static int[] moneyArr = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return exchangeMoney(money);
+    }
+
+    private static List<Integer> exchangeMoney(int money) {
+        List<Integer> moneyList = new ArrayList<>();
+        for (int i = 0; i < moneyArr.length; i++) {
+            moneyList.add(money / moneyArr[i]);
+            money %= moneyArr[i];
+        }
+
+        return moneyList;
     }
 }
