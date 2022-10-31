0734e8fcf76e4a60cde1434ca39cadc45a2d9f35
Isu Shin <isu010614@naver.com>
2022-10-31 16:50:15 +0900

    feat : Add function to count nickname subset's duplicated number
    1. reset nickname subset's hash map
    2. count the number of times each element of a subset is duplicated
    3. reset nickname subset list
    4. restore in nickname subset list if counted number is 2 or more

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,12 +1,11 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     static List<String> nicknameList = new ArrayList<>();
     static List<String> nicknameSubset = new ArrayList<>();
+    static HashMap<String, Integer> nicknameSubsetNumber = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
@@ -36,6 +35,29 @@ public class Problem6 {
     public static void cutNickname(String nickname) {
         for (int i = 0; i <= nickname.length() - 2; i++) {
             nicknameSubset.add(nickname.substring(i, i + 2));
+        }
+    }
+
+    public static void resetSubsetNumber() {
+        for (String subset : nicknameSubset) {
+            nicknameSubsetNumber.put(subset, 0);
+        }
+    }
+
+    public static void countDuplicateSubset() {
+        for (String subset : nicknameSubset) {
+            nicknameSubsetNumber.put(subset, nicknameSubsetNumber.get(subset) + 1);
+        }
+    }
+
+    public static void isDuplicatedSubset() {
+        for (Map.Entry<String, Integer> entry : nicknameSubsetNumber.entrySet()) {
+            String subset = entry.getKey();
+            int duplicatedNum = entry.getValue();
+
+            if (duplicatedNum >= 2) {
+                nicknameSubset.add(subset);
+            }
         }
     }
 }
