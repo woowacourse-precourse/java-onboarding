f7797fa0df5facdbdb01f15df2a8ef188de3a7ef
Hongsugyeong <olivia258@naver.com>
2022-10-31 12:04:21 +0900

    [Problem 5] 기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /**
+     *화폐 갯수 세는 함수
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
