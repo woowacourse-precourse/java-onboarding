8061cc0a13f6815e229858bf5a7fcf2d50e97734
donghae <donghae1999@gmail.com>
2022-10-31 10:48:11 +0900

    P6 : 닉네임 중복 확인 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static Set<String> set = new HashSet<String>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for(int i=0; i<forms.size()-1; i++)
+            for(int j=i+1; j<forms.size();j++)
+                checkDuplicated(forms.get(i), forms.get(j));
+
+        answer = new ArrayList<String>(set);
+        Collections.sort(answer);
+
         return answer;
+    }
+    private static boolean checkDuplicated(List<String> first,List<String> second){
+        String firstName = first.get(1);
+        String secondName = second.get(1);
+        for(int i=0; i<firstName.length()-1; i++){
+            String subFirstName = firstName.substring(i,i+2);
+            for(int j=0; j<secondName.length()-1; j++){
+                String subSecondName = secondName.substring(j,j+2);
+                if(subFirstName.equals(subSecondName)){
+                    set.add(first.get(0));
+                    set.add(second.get(0));
+                }
+            }
+        }
+        return true;
     }
 }
