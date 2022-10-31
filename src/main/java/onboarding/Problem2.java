96f99ebc860bc7c8d01cc2160b39b9c92bdd7a81
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-10-31 22:17:24 +0900

    [FEAT] : problem2 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,14 +7,51 @@ public class Problem2 {
     }
 
     private static String decoding(String str){
+        boolean check = true;
+        String result = str;
+        do{
+            result = deleteDuplication(result);
+            boolean isCheck = isDuplicated(result);
+            if(isCheck == false){
+                check = false;
+            }
+        }while (check == true);
+        return result;
+    }
+
+    private static String deleteDuplication(String str){
         String result = "";
         char preCh = ' ';
-        for(int index = 1; index < str.length(); index++){
+        for(int index = 0; index < str.length(); index++){
             char ch = str.charAt(index);
-            if(ch != preCh){
+            if(preCh == ch){
+                preCh = ' ';
+                continue;
+            }
+            if(index == str.length() - 1){
                 result += ch;
             }
+            if(preCh == ' ' && index != 0){
+                preCh = ch;
+                continue;
+            }
+            result += preCh;
+            preCh = ch;
         }
         return result;
+    }
+
+    private static boolean isDuplicated(String str){
+        char prech = ' ';
+        boolean isCheck = false;
+        for(int index = 0; index < str.length(); index++){
+            char ch = str.charAt(index);
+            if(ch == prech){
+                isCheck = true;
+                break;
+            }
+            prech = ch;
+        }
+        return (isCheck == true) ? true : false;
     }
 }
