be50d78172240ba74fa355c28ce23babc5a788d6
code-tree <dltjdqls1200@naver.com>
2022-10-31 19:03:10 +0900

    feat: 크루들의 중복 닉네임을 검사
    중복된 닉네임(연속된 두 글자)을 별도의 limitedWords 에 넣음
    닉네임을 두글자씩 나누는 for문 코드가 중복돼서 메서드로 만듬

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,18 @@
 package onboarding;
 
 import onboarding.problem6.Crew;
-import onboarding.problem6.Email;
-import onboarding.problem6.Nickname;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.Collections;
+import java.util.stream.Stream;
 
 public class Problem6 {
+    static final Set<String> usingNickname = new HashSet<>();
+    static final Set<String> limitedWords = new HashSet<>();
+    static final List<String> words = new ArrayList<>();
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
@@ -17,10 +21,37 @@ public class Problem6 {
             return Collections.emptyList();
         }
 
-        List<Crew> crews = forms.stream()
+        forms.forEach(Problem6::registerNickname);
+
+        Stream<Crew> crewStream = forms.stream()
                 .map(Crew::new)
-                .collect(Collectors.toList());
+                .filter(Problem6::isDuplicatedName);
 
         return answer;
+    }
+
+    private static void registerNickname(List<String> form) {
+        String nickname = form.get(1);
+
+        splitNickname(nickname).stream()
+                .filter(word -> !usingNickname.add(word))
+                .forEach(limitedWords::add);
+    }
+
+    private static boolean isDuplicatedName(Crew crew) {
+        String nickname = crew.getNickname().getValue();
+
+        return splitNickname(nickname).stream()
+                .anyMatch(limitedWords::contains);
+    }
+
+    private static List<String> splitNickname(String nickname) {
+        words.clear();
+
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            words.add(nickname.substring(i, i + 2));
+        }
+
+        return words;
     }
 }
