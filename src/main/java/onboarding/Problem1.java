0efd65f7b326fa3e11b9b7696c54da07521d4494
bnbnac <bnbnac@naver.com>
2022-11-01 07:53:34 +0900

    feat: (P1)오른쪽 페이지가 왼쪽 페이지의 다음 페이지인지 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!validatePageList(pobi) || !validatePageList(crong)) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static boolean validatePageList(List<Integer> pageList) {
+        if (!isConsecutive(pageList)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isConsecutive(List<Integer> pageList) {
+        return pageList.get(0) + 1 == pageList.get(1);
     }
 }
