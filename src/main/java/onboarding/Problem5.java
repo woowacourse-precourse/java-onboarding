6eed50a60da5de5c4dd2de8bac59d191012bcd24
Denny-Ko <donghar@naver.com>
2022-10-31 20:29:04 +0900

    Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        int[] check = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] ans = new int[9];
+
+        for (int i = 0; i < check.length; i++) {
+            ans[i] = money / check[i];
+            money %= check[i];
+        }
+
+        return Arrays.stream(ans).boxed().collect(Collectors.toList());
     }
 }
