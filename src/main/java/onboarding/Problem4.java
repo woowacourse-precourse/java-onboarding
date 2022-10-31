e121e3199584c9770a5218dacd4a5b9467d996e1
고범석 <kobums23@gmail.com>
2022-10-31 23:53:45 +0900

    problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,30 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+
+            char ch = word.charAt(i);
+            int num = (int) ch;
+
+            if (num == 32) {
+               answer += " "; 
+            }
+
+            if (64 < num && num < 91) {
+                int chaNum = 155 - num;
+                char cha = (char) chaNum;
+                String str = String.valueOf(cha);
+                answer += str;
+            }
+
+            if (96 < num && num < 123) {
+                int chaNum = 219 - num;
+                char cha = (char) chaNum;
+                String str = String.valueOf(cha);
+                answer += str;
+            }
+
+        }
         return answer;
     }
 }
