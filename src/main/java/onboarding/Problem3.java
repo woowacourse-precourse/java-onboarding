a6bb93229cd153d9fd4892a2eec2a8fdc958df1b
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-31 23:13:40 +0900

    feat: P3 반복문을 통해 1부터 n까지 숫자를 증가시키면서 각 자리수를 추출하여 박수 횟수 카운트 하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int num = i;
+
+            while(num != 0) {
+                if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                    answer++;
+                }
+                num /= 10;
+            }
+        }
         return answer;
     }
 }
