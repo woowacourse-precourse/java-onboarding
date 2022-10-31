a7290a3b10663ebd458000578673a5b1fbc8a479
ECHO <eunchan1001@gmail.com>
2022-10-31 17:04:21 +0900

    feat(Problem5): 화폐단위를 반복하면서 몫은 배열에 저장하고, 나머지는 다음 연산에 넣는 로직 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class Problem5 {
     static int[] moneyCurrency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < moneyCurrency.length; i++) {
+            int count = money / moneyCurrency[i];
+            answer.add(count);
+            money = money % moneyCurrency[i];
+        }
         return answer;
     }
 }
