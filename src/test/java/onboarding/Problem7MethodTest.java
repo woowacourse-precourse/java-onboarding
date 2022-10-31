eb18ce926ca8703d64ee131364543b7a32e2a8e1
phobyjun <phobyjun@gmail.com>
2022-11-01 01:25:45 +0900

    [PROBLEM7] ADD: FEATURE3. 사용자와 함께 아는 친구 점수 계산 함수 단위 테스트 코드

--- src/test/java/onboarding/Problem7MethodTest.java
+++ src/test/java/onboarding/Problem7MethodTest.java
@@ -3,10 +3,11 @@ package onboarding;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static onboarding.Problem7.getFriendsOfUser;
-import static onboarding.Problem7.getRecommendFriends;
+import static onboarding.Problem7.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem7MethodTest {
@@ -42,5 +43,24 @@ class Problem7MethodTest {
         List<String> result1 = getRecommendFriends(user1, friends1, visitors1);
 
         assertThat(result1).isEqualTo(List.of("andole", "jun", "bedi"));
+    }
+
+    @Test
+    void getScoreKnownFriendTest() {
+        String user1 = "mrko";
+        List<List<String>> friends1 = new ArrayList<>(List.of());
+        friends1.add(List.of("donut", "andole"));
+        friends1.add(List.of("donut", "jun"));
+        friends1.add(List.of("donut", "mrko"));
+        friends1.add(List.of("shakevan", "andole"));
+        friends1.add(List.of("shakevan", "jun"));
+        friends1.add(List.of("shakevan", "mrko"));
+
+        HashMap<String, Integer> result1 = getScoreKnownFriend(user1, friends1);
+
+        assertThat(result1).isEqualTo(Map.of(
+                "andole", 10,
+                "jun", 10
+        ));
     }
 }
