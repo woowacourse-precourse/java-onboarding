3c9dd70298607b53bd374ce0e1602238f8ffe559
Jeonsoyeon <itso22@naver.com>
2022-10-31 12:56:17 +0900

    feat(Problem3): 3,6,9 숫자가 있으면 박수치기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
-    public static int[] eachDigit(int num){
+
+    public static int[] eachDigit(int num) {
         return Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static int countThreeSixNine(int[] arr) {
+        int clap = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 3 || arr[i] == 6 || arr[i] == 9) {
+                clap++;
+            }
+        }
+        return clap;
     }
 }
