6ef756519f9972c9de472dfd305d43dcca82e6a8
jongmyeongbak <withpleasure@gmail.com>
2022-11-01 01:46:16 +0900

    clap number of a number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,17 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int clapnumber(int n) {
+        int m;
+        int countmultiple = 0;
+        do {
+            m = n % 10;
+            if (m % 3 == 0) {
+                countmultiple++;
+            }
+            n /= 10;
+        } while (n != 0);
+        return countmultiple;
+    }
 }
