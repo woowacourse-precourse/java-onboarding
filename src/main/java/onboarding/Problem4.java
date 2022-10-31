4ece62055f25a654a16cf9c59dd7748427fb9116
mikekang47 <gyeong0308@gmail.com>
2022-10-31 22:37:54 +0900

    Feat<problem4>: add getUpperCaseHashMap method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,24 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    /**
+     * UpperCaseHashMap을 반환해주는 메서드입니다.
+     *
+     * @return UpperCaseHashMap
+     */
+    public static Map<Character, Character> getUpperCaseHashMap() {
+        Map<Character, Character> map = new HashMap<>();
+        char last = 'Z';
+
+        for (int i = 65; i < 91; i++) {
+            map.put((char) i, last);
+            last--;
+        }
+        return map;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
