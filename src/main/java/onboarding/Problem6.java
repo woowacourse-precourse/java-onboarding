21dbfcf1784215cd2c7bbf5fc92bda9cfc6c55fb
Go-Jaecheol <gojaech@naver.com>
2022-10-31 22:41:05 +0900

    feat: Problem6 - 닉네임 중 연속되는 2글자를 key, 해당되는 이메일 Set을 value로 가지는 HashMap 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Map<String, Set<String>> overlapMap = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+        }
+
         return answer;
     }
 }
