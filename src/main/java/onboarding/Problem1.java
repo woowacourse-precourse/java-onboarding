138d2cdb9704e841c28437d5535afd3e058a95ee
2jun0 <soo28819@gmail.com>
2022-10-31 19:15:28 +0900

    Feat: 문제 1번 - 입력 검증 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,9 +8,21 @@ import java.util.List;
 class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        validatePobiCrongRange(pobi, crong);
+
         Game game = new Game();
         Result result = game.play(pobi, crong);
 
         return result.getValue();
+    }
+
+    private static void validatePobiCrongRange(List<Integer> pobi, List<Integer> crong) throws IllegalArgumentException {
+        if (pobi.size() != 2) {
+            throw new IllegalArgumentException("pobi의 길이는 2여야 합니다.");
+        }
+
+        if (crong.size() != 2) {
+            throw new IllegalArgumentException("crong의 길이는 2여야 합니다.");
+        }
     }
 }
