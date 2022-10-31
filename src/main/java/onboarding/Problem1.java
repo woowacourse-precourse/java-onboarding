40515bf718d5ffc0473fc771cf5434fa9119337a
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-01 04:12:48 +0900

    feat : problem1 add page sum calculation

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,6 +12,19 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return 0;
     }
+    private static int sum(List<Integer> l){
+        int val1 = 0;
+        int val2 = 0;
+        String[] arr1 = String.valueOf(l.get(0)).split("");
+        String[] arr2 = String.valueOf(l.get(1)).split("");
+        for(String val : arr1){
+            val1 = val1 + Integer.valueOf(val);
+        }
+        for(String val : arr2){
+            val2 = val2 + Integer.valueOf(val);
+        }
+        return max(val1,val2);
+    }
     private static int max(int a, int b){
         if(a>b){
             return a;
