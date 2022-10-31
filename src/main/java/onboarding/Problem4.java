151641ada07967bb19d96d96c864e26cb48efeb9
swjeong98 <tree5678@konkuk.ac.kr>
2022-10-31 17:54:30 +0900

    문제 4 기능  - 알파벳 1개의 대/소문자를 구분

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,7 +10,19 @@ public class Problem4 {
         if(ch >= 'A' && ch <= 'Z') return true;
         else return false;
     }
-    
+
+    public static char ConvertCharacter(char ch){
+        int diff = 0;
+        if(isUpperCharacter(ch)) {
+            diff = ch - 'A';
+            ch = (char)('A' + (25 - diff));
+        }
+        else{
+            diff = ch - 'a';
+            ch = (char)('a' + (25 - diff));
+        }
+        return ch;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
