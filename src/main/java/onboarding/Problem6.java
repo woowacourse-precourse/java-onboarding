60016cedf3d256b237675f38747f231320eac546
sdsr <dlxodnr1212@naver.com>
2022-10-31 11:18:37 +0900

    problem6 기능 2 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -31,4 +31,26 @@ public class Problem6 {
         return strlist;
     }
 
+    public static List<String> reverse_checkString(List<List<String>> forms){
+        List<String> strlist = new ArrayList<>();
+        String strtemp = "";
+        String check2word = "";
+
+        for(int i=0; i<forms.size(); i++){
+            strtemp = forms.get(i).get(1);
+            for(int j = 0; j<strtemp.length()-1; j++){
+                check2word = "";
+                check2word += strtemp.charAt(j+1);
+                check2word += strtemp.charAt(j);
+                for(int k = 0; k<forms.size(); k++){
+                    if(forms.get(k).get(1).contains(check2word)){
+                        strlist.add(forms.get(k).get(0));
+                    }
+                }
+            }
+        }
+
+        return strlist;
+    }
+
 }
