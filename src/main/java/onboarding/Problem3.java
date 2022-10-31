84e1ba9b6b1afd87051a50e3ca2287cc32308758
nahyeon cho <whskgus12@nate.com>
2022-10-31 22:55:59 +0900

    Problem 3 - 예외사항 처리 기능
    입력한 숫자의 범위가 주어진 범위를 벗어나면, False를 반환합니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,11 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    static boolean isValid(int number) {
+        if (number < 1 || number > 10000)
+            return false;
+        else
+            return true;
+    }
 }
