fc9769085f545812bb75d1a75a42c8641237870e
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-10-31 18:00:19 +0900

    Feat(Problem2): Determine if String Length is Normal

---
+++ docs/PROBLEM2_FEATURELIST.md
@@ -0,0 +1,14 @@
+
+# 기능 목록
+
+## 1. 입력 받은 문자열 체크
+###     1) 문자열의 길이가 1 이상 1000 이하인가?
+            입력(int cryptogram.length), 출력(boolean)
+###     2) 문자열이 알파벳 소문자로만 이루어졌는가?
+            입력(char c), 출력(boolean)
+
+## 2. 암호 해독
+###     1) 이후 순서 문자와 같은 문자인가?
+            입력(char a, char b), 출력 (boolean)
+###     2) 중복 제거 후 새 문자열 생성
+            입력(String cryptogram, PriorityQueue<Integer> PQ), 출력 (String)
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,18 @@
 package onboarding;
 
+import java.util.PriorityQueue;
+
 public class Problem2 {
+    static class Password {
+        String cryptogram;
+
+
+
+        private boolean isValidLength(int cryptogramLength) {
+            return cryptogramLength>0 &&cryptogramLength<=1000;
+        }
+
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
