6230a412bf41608187b5131147805d1b0d1118c0
Aru428 <86353626+Aru428@users.noreply.github.com>
2022-11-01 00:15:25 +0900

    문제7 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,19 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        int size = forms.size();
-        int[] check =  new int[size];
+        List<String> answer = new ArrayList<>();
+        HashSet<String> emails = new HashSet<>();
+        Map<String, String> hashmap = new HashMap<>();
 
         for(int i=0;i<forms.size();i++){
             String name = forms.get(i).get(1);
             for(int j=0;j<name.length()-1;i++){
-                String duplication = name.substring(j,j+2);
-
-
+                String checkName = name.substring(j,j+1);
+                if(hashmap.containsKey(checkName)){
+                    String email = hashmap.get(checkName);
+                    if(!email.equals(forms.get(i).get(0))){
+                        emails.add(email);
+                        emails.add(forms.get(i).get(0));
+                    }
+                }
+                hashmap.put(checkName,forms.get(i).get(0));
             }
         }
 
