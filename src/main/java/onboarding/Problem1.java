a486deb4f2b59f0163efb9dba441f12dfaed88ef
unknown <2021110806@dgu.ac.kr>
2022-10-31 23:38:19 +0900

    fix:problem1 findMax method, solution method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -21,13 +21,34 @@ class Problem1 {
         }
         return sum;
     }
-    
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE''
 
-        int pobiMax=-1;
-        int crongMax=-1;
+    public static int findMax (int num1, int num2, int num3, int num4) {
+        int maxCandidate1 = 0;
+        int maxCandidate2 = 0;
+        maxCandidate1 = Math.max(num1, num2);
+        maxCandidate2 = Math.max(num3, num4);
+        return Math.max(maxCandidate1, maxCandidate2);
+    }
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer = Integer.MAX_VALUE;
+
+        int pobiMax = 0;
+        int crongMax = 0;
         boolean error = false;
+
+        if (pobi.get(0)%2==0||pobi.get(1)%2==1||crong.get(0)%2==0||crong.get(1)%2==1) {
+            error=true;
+        }
+        if (pobi.get(0)+1!=pobi.get(1)||crong.get(0)+1!=crong.get(1)) {
+            error=true;
+        }
+        if (pobi.get(0)<1||pobi.get(0)>400||pobi.get(1)<0||pobi.get(1)>400||crong.get(0)<1||crong.get(0)>400||crong.get(1)<1||crong.get(1)>400) {
+            error = true;
+        }
+
+        pobiMax = findMax(add(pobi.get(0)),add(pobi.get(1)),multiple(pobi.get(0)),multiple(pobi.get(1)));
+        crongMax = findMax(add(crong.get(0)),add(crong.get(1)),multiple(crong.get(0)),multiple(crong.get(1)));
+
 
         if (error) {
             answer=-1;
@@ -38,6 +59,7 @@ class Problem1 {
         } else if (pobiMax==crongMax) {
             answer=0;
         }
+
         return answer;
     }
 }
