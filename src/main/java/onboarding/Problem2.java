b25687cd47c38cdeae7c6313d9aa2eb19936512c
devEdok13 <dev.edok13@gmail.com>
2022-10-31 13:33:16 +0900

    feat: P02-checkCryptogram

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    //  연속되는 중복 문자의 Index를 확인하는 메소드
+    private static List<Integer> checkCryptogram(String cryptogram) {
+        List<Integer> index = new ArrayList<>();
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) index.add(i);
+        }
+        return index;
     }
 }
