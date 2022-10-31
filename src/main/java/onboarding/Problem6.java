53a98ea01e767636e233faea60dab289424b4805
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-31 12:52:01 +0900

    6번 문제 해결

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import onboarding.prob6.domain.Judge;
+import onboarding.prob6.domain.Transformer;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<List<String>> processed = Transformer.encodeDecodLists(forms);
+        Judge judge = new Judge(processed);
+        return getSortedStrings(judge);
+    }
+
+    private static List<String> getSortedStrings(Judge judge) {
+        List<String> answer;
+        answer = judge.getListOfSameNickName().stream().sorted().collect(Collectors.toList());
         return answer;
     }
 }
