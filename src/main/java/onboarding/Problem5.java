b46aafac67312f7de021fcc8cabd13b7b2d46a60
ihn <ch200205@naver.com>
2022-10-31 17:04:13 +0900

    Problem5 가장큰 화폐 단위부터 사용하여 금액을 맞춰준다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,17 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        // 예외처리
+        try {
+            if(money < 1 || money > 1000000 ) {
+                throw  new Exception("금액의 범위를 벗어났습니다.");
+            }
+        } catch (Exception e) {
+            System.out.println("Exception :: " + e.getMessage());
+            return new ArrayList<>();
+        }
 
         // 1. 돈의 단위 배열 생성
         int[] moneyArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-
-
+        // 2. 가장 큰 다위 화폐부터 사용
+        for (int i : moneyArr) {
+            money = money % i;
+            answer.add(money / i);
+        }
 
         return answer;
     }
