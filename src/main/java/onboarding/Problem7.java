a54654641292fbe16dfb10bdb167e17672ae5d87
wath1457 <wath1457@gmail.com>
2022-10-31 15:57:39 +0900

    [Problem7] Sort friend_recommend score

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,10 +3,10 @@ package onboarding;
 import java.util.*;
 /*
 0. friends 를 순회하면서 친구점수 리스트를 만듦(중복x) [name, score] + user_info 클래스 정의
-1. friends 리스트를 순회하면서 user, user와 이미 친구인 사람은 추천목록에서 제외(친구제외 리스트에 추가, 친구 점수 리스트에는 점수를 -1점으로 설정)
+1. friends 리스트를 순회하면서 user, user와 이미 친구인 사람은 추천목록에서 제외(친구제외 리스트에 추가, 친구 점수 map에는 점수를 -1점으로 설정)
 2. user와 함께 아는사이(친구제외 리스트에 있는 사람) 과 친구이면 + 10
 3. visitors에 들어있는 횟수만큼 + 1
-4. 람다식을 이용하여 friends 리스트를 점수 내림차순, 이름 오름차순 정렬 후 answer 리스트에 추가(점수가 -1은 추가하지 앟는다)
+4. 람다식을 이용하여 friends_score map을 점수 내림차순, 이름 오름차순 정렬 후 answer 리스트에 추가(점수가 -1은 추가하지 앟는다)
  */
 
 public class Problem7 {
@@ -15,7 +15,7 @@ public class Problem7 {
         List<String> exception_list = makeFriendsException(user, friends);
         initFriendsScore(friends_score, exception_list, user);
         setScore(friends, friends_score, exception_list, visitors);
-
+        List<Map.Entry<String, Integer>> sortedList = sortScore(friends_score);
         return answer;
     }
     public static Map<String, Integer> makeFriendsScore(List<List<String>> friends) {
@@ -65,6 +65,11 @@ public class Problem7 {
         for (String visitor : visitors) {
             friends_score.put(visitor, friends_score.get(visitor) + 1);
         }
+    }
+    public static List<Map.Entry<String, Integer>> sortScore(Map<String, Integer> friends_score) {
+        List<Map.Entry<String, Integer>> sortedList = new LinkedList<>(friends_score.entrySet());
+        sortedList.sort(((o1, o2) -> friends_score.get(o2.getKey()) - friends_score.get(o1.getKey())));
+        return sortedList;
     }
 
 }
