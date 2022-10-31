fdb64092e5acf2065596cbb53a865583435767ea
jihwan <sgh002400@naver.com>
2022-11-01 02:20:29 +0900

    style: Problem5 code formatting

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,31 +6,31 @@ import java.util.List;
 
 public class Problem5 {
 
-    public static List<Integer> solution(int money) {
-        List<Integer> moneyTypes = createMoneyTypes();
-        List<Integer> answer = new ArrayList<>(moneyTypes.size());
+	public static List<Integer> solution(int money) {
+		List<Integer> moneyTypes = createMoneyTypes();
+		List<Integer> answer = new ArrayList<>(moneyTypes.size());
 
-        calculateMoneyCount(money, answer, moneyTypes);
+		calculateMoneyCount(money, answer, moneyTypes);
 
-        return answer;
-    }
+		return answer;
+	}
 
-    private static List<Integer> createMoneyTypes() {
-        return Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
-    }
+	private static List<Integer> createMoneyTypes() {
+		return Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+	}
 
-    private static void calculateMoneyCount(int money, List<Integer> answer, List<Integer> moneyTypes) {
-        for (int moneyType : moneyTypes) {
-            calculateEachTypeOfMoneyCount(money, answer, moneyType);
-            money = getChanges(money, moneyType);
-        }
-    }
+	private static void calculateMoneyCount(int money, List<Integer> answer, List<Integer> moneyTypes) {
+		for (int moneyType : moneyTypes) {
+			calculateEachTypeOfMoneyCount(money, answer, moneyType);
+			money = getChanges(money, moneyType);
+		}
+	}
 
-    private static void calculateEachTypeOfMoneyCount(int money, List<Integer> answer, int moneyType) {
-        answer.add(money / moneyType);
-    }
+	private static void calculateEachTypeOfMoneyCount(int money, List<Integer> answer, int moneyType) {
+		answer.add(money / moneyType);
+	}
 
-    private static int getChanges(int money, int moneyType) {
-        return money % moneyType;
-    }
+	private static int getChanges(int money, int moneyType) {
+		return money % moneyType;
+	}
 }
