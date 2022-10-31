39d02088ebefe703c75fba15de6be9c35910dc23
jini_11 <jieun21124@naver.com>
2022-10-31 14:07:05 +0900

    feat: 문제3 입력 예외 처리 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,11 +8,19 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
+        if (isError(number)) {
+            return -1;
+        }
+
         for (int i = 1; i <= number; i++) {
             answer += find369(i);
         }
 
         return answer;
+    }
+
+    public static boolean isError(int number) {
+        return number < 1 || number > 10000;
     }
 
     public static  int find369(int number) {
