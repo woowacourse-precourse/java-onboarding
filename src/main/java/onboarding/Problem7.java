1b4ea78642f48fee3a66c08b22aeb67401bc1975
Qulip <ilkwon.ryu@gmail.com>
2022-10-31 22:34:37 +0900

    feat : Problem7 사용자들의 친구 추천 점수 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,12 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -47,14 +52,68 @@ public class Problem7 {
 	}
 
 	public static boolean isValidIdLength(String id) {
-		if (id.length() >= 1 && id.length() <= 30)
-			return true;
-		return false;
+		return id.length() >= 1 && id.length() <= 30;
 	}
 
 	public static boolean isContainUpperClass(String id) {
-		if (id.chars().anyMatch(Character::isUpperCase))
-			return true;
-		return false;
+		return id.chars().anyMatch(Character::isUpperCase);
+	}
+
+	public static Map<String, Integer> getRecomScore(String user, List<List<String>> friends, List<String> visitors) {
+		Map<String, Integer> recomScoreMap = new HashMap<>();
+		List<String> userList = getUserList(user, friends, visitors);
+		Map<String, Set<String>> friendSet = getFriendList(friends);
+
+		getFriendRecomScore(user, recomScoreMap, userList, friendSet);
+		getVisitorRecomScore(recomScoreMap, visitors);
+
+		return recomScoreMap;
+	}
+
+	public static List<String> getUserList(String user, List<List<String>> friends, List<String> visitors) {
+		Set<String> userSet = new HashSet<>();
+		userSet.add(user);
+		friends.forEach(friend -> {
+			userSet.add(friend.get(0));
+			userSet.add(friend.get(1));
+		});
+		userSet.addAll(visitors);
+		return new ArrayList<>(userSet);
+	}
+
+	public static Map<String, Set<String>> getFriendList(List<List<String>> friends) {
+		Map<String, Set<String>> friendMap = new HashMap<>();
+		friends.forEach(friend -> {
+			if (!friendMap.containsKey(friend.get(0))) {
+				friendMap.put(friend.get(0), new HashSet<>());
+			}
+			friendMap.get(friend.get(0)).add(friend.get(1));
+		});
+		return friendMap;
+	}
+
+	public static int getFriendScore(Set<String> myFriends, Set<String> friendList) {
+		return (int)myFriends.stream().filter(friendList::contains).count();
+	}
+
+	public static void getFriendRecomScore(String user, Map<String, Integer> recomScoreMap, List<String> userList,
+		Map<String, Set<String>> friendSet) {
+		userList.stream()
+			.filter(userName -> !user.equals(userName))
+			.filter(friendSet::containsKey)
+			.forEach(userName -> {
+				if (!recomScoreMap.containsKey(userName))
+					recomScoreMap.put(userName, 0);
+				recomScoreMap.put(userName,
+					recomScoreMap.get(userName) + getFriendScore(friendSet.get(user), friendSet.get(userName)));
+			});
+	}
+
+	public static void getVisitorRecomScore(Map<String, Integer> recomScoreMap, List<String> visitors) {
+		visitors.forEach(vistor -> {
+			if (!recomScoreMap.containsKey(vistor))
+				recomScoreMap.put(vistor, 0);
+			recomScoreMap.put(vistor, recomScoreMap.get(vistor) + 1);
+		});
 	}
 }
