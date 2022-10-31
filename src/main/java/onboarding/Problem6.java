82800faadeefac999c162776fed94645920087d0
Fizioo0102 <a27313797@gmail.com>
2022-11-01 02:46:25 +0900

    feat: part_of_nickname()함수 작성
    - 닉네임을 두글자씩 분할하여 2차원리스트로 반환하는 함수 구현.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
@@ -9,6 +10,22 @@ public class Problem6 {
     }
 
     //닉네임을 두글자씩 슬라이싱
+    static List<List<String>> part_of_nickname(List<List<String>> forms){
+        List<List<String>> part = new ArrayList<>();
+
+        for(int i=0;i<forms.size();i++){
+            String tmp = forms.get(i).get(1);
+            List<String> tmp_part = new ArrayList<>();
+            for(int j=0;j<tmp.length()-1;j++) {
+                if(tmp.length()>=2) {
+                    tmp_part.add(tmp.substring(j, j + 2));
+                }
+            }
+            part.add(tmp_part);
+        }
+
+        return part;
+    }
 
     //닉네임 조각을 비교해서 중복체크를 하는 함수
 
