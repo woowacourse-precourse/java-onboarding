fadce19919167729fbfd1f8ae0fd066b9f8c4196
kkyu0718 <net605@naver.com>
2022-11-01 00:58:57 +0900

    [3] problem3 박수 계산

---
+++ src/main/java/onboarding/problem3/Problem3.java
@@ -0,0 +1,26 @@
+package onboarding.problem3;
+
+public class Problem3 {
+    public static int solution(int number) {
+        return getClapNumber(number);
+    }
+
+    public static int getClapNumber(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++ ){
+            String str = Integer.toString(i);
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '3') {
+                    count += 1;
+                }
+                if (str.charAt(j) == '6') {
+                    count += 1;
+                }
+                if (str.charAt(j) == '9') {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+}
