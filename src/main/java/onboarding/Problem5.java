21a8d59ff9333361451f8dddf0d4e5e4fd07592f
wogus4048 <wogus4048@naver.com>
2022-10-31 19:49:50 +0900

    feat(Problem5): 금액 순서대로 몇개씩 반환되는지 리스트에 저장하는 기능추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,73 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        if (money / 50000 > 0) {
+            answer.add(money / 50000);
+            money = money % 50000;
+        }
+        else{
+            answer.add(0);
+        }
+        if (money / 10000 > 0) {
+            answer.add(money / 10000);
+            money = money % 10000;
+        }
+        else{
+            answer.add(0);
+        }
+        if (money / 5000 > 0) {
+            answer.add(money / 5000);
+            money = money % 5000;
+        }
+        else{
+            answer.add(0);
+        }
+        if (money / 1000 > 0) {
+            answer.add(money / 1000);
+            money = money % 1000;
+        }
+        else{
+            answer.add(0);
+        }
+        if (money / 500 > 0) {
+            answer.add(money / 500);
+            money = money % 500;
+        }
+        else{
+            answer.add(0);
+        }
+        if (money / 100 > 0) {
+            answer.add(money / 100);
+            money = money % 100;
+        }
+        else{
+            answer.add(0);
+        }
+        if (money / 50 > 0) {
+            answer.add(money / 50);
+            money = money % 50;
+        }
+        else{
+            answer.add(0);
+        }
+        if (money / 10 > 0) {
+            answer.add(money / 10);
+            money = money % 10;
+        }
+        else{
+            answer.add(0);
+        }
+        answer.add(money);
+
+
+
         return answer;
     }
+
 }
