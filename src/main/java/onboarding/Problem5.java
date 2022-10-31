cf7738c0e91b31168e7a2cb91950f518d26f9311
Nugur1i <duckhee092@naver.com>
2022-11-01 01:13:54 +0900

    feat<problem5> : Add 'while loop changing money to money unit' and 'if statement change money unit'

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,32 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int moneyUnit = 50000;
+        int numberOfKindOfMoney = 9;
+
+
+        while (numberOfKindOfMoney > 0) {
+            if(numberOfKindOfMoney>2 && numberOfKindOfMoney%2 == 1){
+                answer.add(money/moneyUnit);
+                money %= moneyUnit;
+                moneyUnit /= 5;
+
+            } else if (numberOfKindOfMoney>2 && (numberOfKindOfMoney&2) == 0) {
+                answer.add(money/moneyUnit);
+                money %= moneyUnit;
+                moneyUnit /= 2;
+            } else if (numberOfKindOfMoney == 2){
+                answer.add(money/moneyUnit);
+                money %= moneyUnit;
+                moneyUnit /= 10;
+            } else if (numberOfKindOfMoney == 1){
+                answer.add(money/moneyUnit);
+            }
+            numberOfKindOfMoney -= 1;
+
+        }
+
+
         return answer;
     }
 }
