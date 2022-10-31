ea56254585397feecd61ce7f18f7e30d4c65950d
nuyh <hjaehyun25@gmail.com>
2022-11-01 01:28:31 +0900

    fix(Problem2.java): 연속된 문자열을 모두 제거하도록 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,26 +1,45 @@
 package onboarding;
 
-import java.util.Stack;
-import java.util.stream.Collectors;
-
 public class Problem2 {
-    public static String removeDuplication(String cipher) {
-        Stack<Character> stack = new Stack<>();
+    public static String[] removeDuplication(String cryptogram) {
+        StringBuilder cipher = new StringBuilder(cryptogram);
+        int count = 0;
 
-        cipher.chars()
-                .forEach(o -> {
-                    if (!stack.isEmpty() && stack.peek() == o)
-                        stack.pop();
-                    else
-                        stack.add((char) o);
-                });
+        for (int i = 0; i < cipher.length() - 1; i++) {
+            if (cipher.charAt(i) != cipher.charAt(i + 1))
+                continue;
 
-        return stack.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining(""));
+            int startIndex = i;
+            int endIndex = i + 1;
+
+            while (endIndex != cipher.length() &&
+                    cipher.charAt(startIndex) == cipher.charAt(endIndex)) {
+                endIndex++;
+            }
+
+            count = endIndex - startIndex;
+            cipher.replace(startIndex, endIndex, "");
+            i--;
+        }
+
+        return new String[]{cipher.toString(), String.valueOf(count)};
+    }
+
+    public static boolean removed(int countRemovedChars) {
+        return countRemovedChars > 0;
     }
 
     public static String solution(String cryptogram) {
-        return removeDuplication(cryptogram);
+        while (true) {
+            String[] result = removeDuplication(cryptogram);
+
+            cryptogram = result[0];
+            int countRemovedChars = Integer.parseInt(result[1]);
+
+            if (!removed(countRemovedChars))
+                break;
+        }
+
+        return cryptogram;
     }
 }
