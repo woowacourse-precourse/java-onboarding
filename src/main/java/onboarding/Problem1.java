9528735f76bf278b34431f6190019c6cf71ea723
Choi-JY <Choi-JY1107@github.com>
2022-11-01 03:30:45 +0900

    refactor: 기능 별 메소드  분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,41 +4,58 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        int pobiScore = Integer.MIN_VALUE;
-        int crongScore = Integer.MIN_VALUE;
+        int pobiScore;
+        int crongScore;
+
+        if (!isValid(pobi, crong)) {
+            return -1;
+        }
+
+        pobiScore = calculateMaxScore(pobi);
+        crongScore = calculateMaxScore(crong);
+        return checkWinner(pobiScore, crongScore);
+    }
+
+    private static boolean isValid(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(0) < 3 || pobi.get(0) > 397 || pobi.get(1) < 4 || pobi.get(1) > 398) {
+            return false;
+        }
+        if (crong.get(0) < 3 || crong.get(0) > 397 || crong.get(1) < 4 || crong.get(1) > 398) {
+            return false;
+        }
+        if (pobi.get(0) % 2 == 0 || pobi.get(1) % 2 == 1 || crong.get(0) % 2 == 0 || crong.get(1) % 2 == 1) {
+            return false;
+        }
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static int calculateMaxScore(List<Integer> human) {
+        int maxScore = Integer.MIN_VALUE;
         int tempScore;
-        int value;
 
-        if(pobi.get(0)<3 || pobi.get(0)>398 || pobi.get(1)<3 || pobi.get(1)>398) answer=-1;
-        else if(crong.get(0)<3 || crong.get(0)>398 || crong.get(1)<3 || crong.get(1)>398) answer=-1;
-        else if(pobi.get(0)%2==0 || pobi.get(1)%2==1 || crong.get(0)%2==0 || crong.get(1)%2==1) answer=-1;
-        else if(pobi.get(0)+1 != pobi.get(1) || crong.get(0)+1 != crong.get(1) ) answer=-1;
+        for (int i = 0; i < 2; ++i) {
+            tempScore = 0;
+            for (int value = human.get(i); value != 0; value /= 10){
+                tempScore += value % 10;
+            }
+            maxScore = (maxScore < tempScore) ? tempScore : maxScore;
+        }
 
-        if(answer == -1) return answer;
+        for (int i = 0; i < 2; ++i) {
+            tempScore = 1;
+            for (int value = human.get(i); value != 0; value /= 10){
+                tempScore *= value % 10;
+            }
+            maxScore = (maxScore < tempScore) ? tempScore : maxScore;
+        }
 
-        for(tempScore=0, value=pobi.get(0); value!=0; value/=10) tempScore += value%10;
-        pobiScore = pobiScore > tempScore ? pobiScore : tempScore;
-        for(tempScore=0, value=pobi.get(1); value!=0; value/=10) tempScore += value%10;
-        pobiScore = pobiScore > tempScore ? pobiScore : tempScore;
-        for(tempScore=1, value=pobi.get(0); value!=0; value/=10) tempScore *= value%10;
-        pobiScore = pobiScore > tempScore ? pobiScore : tempScore;
-        for(tempScore=1, value=pobi.get(1); value!=0; value/=10) tempScore *= value%10;
-        pobiScore = pobiScore > tempScore ? pobiScore : tempScore;
+        return maxScore;
+    }
 
-        for(tempScore=0, value=crong.get(0); value!=0; value/=10) tempScore += value%10;
-        crongScore = crongScore > tempScore ? crongScore : tempScore;
-        for(tempScore=0, value=crong.get(1); value!=0; value/=10) tempScore += value%10;
-        crongScore = crongScore > tempScore ? crongScore : tempScore;
-        for(tempScore=1, value=crong.get(0); value!=0; value/=10) tempScore *= value%10;
-        crongScore = crongScore > tempScore ? crongScore : tempScore;
-        for(tempScore=1, value=crong.get(1); value!=0; value/=10) tempScore *= value%10;
-        crongScore = crongScore > tempScore ? crongScore : tempScore;
-
-        if(pobiScore > crongScore) answer = 1;
-        else if(pobiScore < crongScore) answer = 2;
-        else if(pobiScore == crongScore) answer = 0;
-
-        return answer;
+    private static int checkWinner(int scoreA, int scoreB) {
+        return (scoreA == scoreB) ? 0 : ((scoreA > scoreB) ? 1 : 2);
     }
 }
