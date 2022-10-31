dfd8e99ffeae0bde990fcafdfcc0a7047c418fce
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 01:45:28 +0900

    refactor: extract method(is369 method)
    - 해당 숫자가 3, 6, 9 인지 확인하는 메소드 추출

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,9 +14,13 @@ public class Problem3 {
     public static int get369Count(int n) {
         int cnt = 0;
         while (n > 0) {
-            if ((n % 10 != 0) && (n % 10) % 3 == 0) cnt++;
+            if (is369(n)) cnt++;
             n /= 10;
         }
         return cnt;
+    }
+
+    private static boolean is369(int n) {
+        return (n % 10 != 0) && (n % 10) % 3 == 0;
     }
 }
