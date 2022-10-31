6a9f5d6fe1f67e48b2aa681edc65a424636586cc
Go-Jaecheol <gojaech@naver.com>
2022-10-31 22:45:18 +0900

    feat: Problem6 - 닉네임을 연속되는 2글자씩 나눠서 List로 만드는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -13,8 +10,17 @@ public class Problem6 {
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickname = form.get(1);
+
         }
 
         return answer;
+    }
+
+    private static List<String> getSubStringList(String nickname) {
+        List<String> subStringList = new ArrayList<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            subStringList.add(nickname.substring(i, i + 2));
+        }
+        return subStringList;
     }
 }
