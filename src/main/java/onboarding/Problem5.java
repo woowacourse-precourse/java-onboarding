8bff1693a49459984e2a563ed950f4e8cf22abff
LJW25 <dbjwl@g.skku.edu>
2022-11-01 02:46:06 +0900

    p5. complete

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i=0;i<units.length;i++){
+            int u = units[i];
+            answer.add(money/u);
+            money = money%u;
+        }
+
         return answer;
     }
 }
