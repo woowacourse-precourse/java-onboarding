3710495bca140764ee3785b2590d2cb722bf308e
hyeongu <wancern@naver.com>
2022-10-31 23:48:48 +0900

    Problem7. 친구관계 메소드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 /*
  * 1. 친구관계를 저장하는 해시맵과 점수를 저장하는 트리맵 선언
  * 2. 양방향 친구관계를 해시맵에 저장
@@ -11,7 +10,26 @@ import java.util.List;
  * */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        Map<String, List<String>> hm = makeRelation(friends);
+        Map<String, Integer> tm = new TreeMap<>();
+
         return answer;
+    }
+    // 해시맵에 {이름 : {친구인 사람들}} 형식으로 저장
+    private static Map<String, List<String>> makeRelation(List<List<String>> friends) {
+        Map<String, List<String>> hm = new HashMap<>();
+
+        for(List<String> li : friends){
+            List<String> from = hm.getOrDefault(li.get(0), new ArrayList<>());
+            List<String> to = hm.getOrDefault(li.get(1), new ArrayList<>());
+
+            from.add(li.get(1));
+            to.add(li.get(0));
+
+            hm.put(li.get(0), from);
+            hm.put(li.get(1), to);
+        }
+        return hm;
     }
 }
