aefb6ebaf239a8e0d975d514dfe08d27631866f5
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-10-31 11:27:19 +0900

    feat(Problem3): count claps up to number
    BREAKING CHANGE: solution method no longer returns placeholder value answer

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,16 +1,23 @@
 package onboarding;
 
 public class Problem3 {
-    // TODO: int countClaps(int number)
+    private static int countClaps(int number) {
+        int count = 0;
+        while (number >= 3) {
+            count += (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) ? 1 : 0;
+            number /= 10;
+        }
+        return count;
+    }
 
     public static int solution(int number) {
         if (number < 1 || 10000 < number) {
             return 0;
         }
-
-        // Count claps
-
-        int answer = 0;
-        return answer;
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            count += countClaps(i);
+        }
+        return count;
     }
 }
