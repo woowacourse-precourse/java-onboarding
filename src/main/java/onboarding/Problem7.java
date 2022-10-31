adc609b1d47fe08659a0ee343d1fb9416b8bbac7
yujindonut <hyujin0825@gmail.com>
2022-10-31 23:18:49 +0900

    [FEAT] HashMap value로 sort 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -45,4 +45,21 @@ public class Problem7 {
         return notMyFriendVisitors;
     }
 
+    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm){
+        List<Map.Entry<String, Integer> > list =
+                new LinkedList<Map.Entry<String, Integer>>(hm.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2){
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
 }
