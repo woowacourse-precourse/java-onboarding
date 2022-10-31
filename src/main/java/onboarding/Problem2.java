f4c8451ce85a311e3359d2c2ecadff99b38013e4
farmJun <junpreme1205@gmail.com>
2022-10-31 15:39:11 +0900

    Problem2. 정답 문자열에 문자가 추가 될 수 있는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,14 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    //정답 문자열에 추가될 수 있는지 확인하는 함수
+    public static Vector<Character> isAddable(Vector<Character> vector, char c) {
+        if (vector.size() != 0 && vector.get(vector.size() - 1).equals(c)) {
+            vector.remove(vector.size() - 1);
+        } else {
+            vector.add(c);
+        }
+        return vector;
+    }
 }
