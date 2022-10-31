a9620fd1b59ba4dc4527bdbd3d05067672ed906e
LeeJinHo <hosboy93@naver.com>
2022-10-31 10:30:02 +0900

    문제4. print 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -13,12 +13,10 @@ public class Problem4 {
     
     public static char frog(char spell) {
     	int ascii = (int)spell;
-    	System.out.println("변환 전 : " + ascii);
     	
     	if(ascii>=65&&ascii<=90) ascii = 155-ascii;
     	else if(ascii>=97&&ascii<=122) ascii = 219-ascii;
     	
-    	System.out.println("변환 후 : " + ascii);
     	spell = (char)ascii;
     	
     	return spell;
