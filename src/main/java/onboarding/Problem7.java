04e1810e1a31ea847a43be31e3a03aba493d5726
hayeongKo <polarpheno@icloud.com>
2022-11-01 00:14:42 +0900

    initial settings

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
+
+//    public static List<String> getAlreadyFriend() {}
+//    public static List<String> getFriendCountKnownTogether() {}
+//    public static List<String> getCountofVisitor() {}
+//    public static List<String> getRecommendedFriend() {}
+//    public static void sortByScore() {}
+
 }
