539348a1b076bb741d47e850c1c9da1a4df53fa5
cgk95 <kjkj5868@gmail.com>
2022-11-01 01:08:00 +0900

    P6 닉네임을 키 값으로 하여 이메일을 값으로 매핑하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
 import java.util.List;
-
+import java.util.HashMap;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = List.of();
+        HashMap<String, String> map = new HashMap<>();
+        doMapping(forms,map);
+
+        return List.of("answer");
+
+    }
+    /**닉네임을 키로 하여 이메일을 값으로 매핑**/
+    private static void doMapping(List<List<String>> forms,HashMap<String,String> map) {
+        for (List<String> val : forms) { /*닉네임을 k, 이메일을 v로 저장*/
+            map.put(val.get(1), val.get(0));
+        }
     }
 }
