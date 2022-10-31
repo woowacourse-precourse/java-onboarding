1c0376de849dad11276d20478e437ae39a3ccae3
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-01 03:27:34 +0900

    feat: 승자 구하기,  refactor : 클래스 크기 줄이기
    Signed-off-by: joohyeongKim <wnguddl96@knou.ac.kr>

--- src/main/java/onboarding/problem1/Comparator.java
+++ src/main/java/onboarding/problem1/Comparator.java
@@ -3,23 +3,12 @@ package onboarding.problem1;
 // 리팩터링 : 클래스 크기 줄이기
 public class Comparator {
 
-    // 승자 구하기
+    // 승자 구하기 기능
     public static int getWinner(int pobiMax, int crongMax) {
 
         int result = (pobiMax > crongMax) ? 1 : 2;
         if (crongMax == pobiMax) {result = 0;}
 
         return result;
-    }
-
-    // 큰수 구하기
-    public static int getBigger(int firstIn, int lastOut) {
-
-        // 과도한 축약 지양하기
-        int result = (firstIn > lastOut) ? firstIn : lastOut;
-        if (firstIn==lastOut){result = firstIn;}
-
-        return result;
-
     }
 }
