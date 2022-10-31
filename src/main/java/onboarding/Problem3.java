f05a033a019ca953dcc86ce869ac0eb4b183da75
MoonJeWoong <munjin0201@naver.com>
2022-10-31 16:36:11 +0900

    [3번 문제] refactor: 메소드 기능 구분

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,18 @@
 package onboarding;
 
 public class Problem3 {
-    public int count369(int checkNum){
+
+    public int count369(int digit, int cnt){
+        if(digit==3 || digit==6 || digit==9){
+            return cnt+1;
+        }
+        return cnt;
+    }
+    public int countClap(int checkNum){
         int cnt = 0;
         while(checkNum!=0){
             int digit = checkNum%10;
-            if(digit==3 || digit==6 || digit==9) cnt++;
+            cnt = count369(digit, cnt);
             checkNum/=10;
         }
         return cnt;
@@ -14,7 +21,7 @@ public class Problem3 {
         Problem3 T = new Problem3();
         int answer = 0;
         for(int checkNum=1; checkNum<=number; checkNum++){
-            answer+=T.count369(checkNum);
+            answer+=T.countClap(checkNum);
         }
         return answer;
     }
