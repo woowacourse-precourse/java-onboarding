290b3e449a11615bf5003635d0a1aadcb6b6a9ab
KyungKyu Park <rudrb0505@gmail.com>
2022-10-31 14:17:24 +0900

    feat(Problem7): 친구 관계 정보를 입력받는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		HashMap<String, ArrayList<String>> friendshipChart = new HashMap<>();
+
+		for (List<String> friend : friends) {
+			String member = friend.get(0);
+			String friendOfMember = friend.get(1);
+
+			if (!friendshipChart.containsKey(member)) {
+				friendshipChart.put(member, new ArrayList<>());
+			}
+			if (!friendshipChart.containsKey(friendOfMember)) {
+				friendshipChart.put(friendOfMember, new ArrayList<>());
+			}
+			friendshipChart.get(member).add(friendOfMember);
+			friendshipChart.get(friendOfMember).add(member);
+		}
+
+		List<String> answer = Collections.emptyList();
+		return answer;
+	}
 }
