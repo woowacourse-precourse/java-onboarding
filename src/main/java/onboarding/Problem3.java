10a71c51461ea4f74f7b32f0d51165519a29c0ef
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-31 14:57:33 +0900

    solve problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,36 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        return getClapCount(number);
     }
+
+    private static int getClapCount(int number) {
+        int clapCount = 0;
+
+        for (int i = 1; i <= number; i++) {
+            clapCount += get369Count(i);
+        }
+
+        return clapCount;
+    }
+
+    private static int get369Count(int number) {
+        int count = 0;
+
+        while (number > 0) {
+            if (is369(number % 10)) {
+                count++;
+            }
+
+            number /= 10;
+        }
+
+        return count;
+    }
+
+    private static boolean is369(int number) {
+        return number == 3 || number == 6 || number == 9;
+    }
+
 }
