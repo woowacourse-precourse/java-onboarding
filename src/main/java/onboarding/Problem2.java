9b455dba8e1d4d5b20eddc0bd1fbec544ff0c010
myeongju00 <j51677767@gmail.com>
2022-10-31 16:09:52 +0900

    refactor: insertIntoMap 함수 형변환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,39 +12,56 @@ public class Problem2 {
     private static String stringDeduplication(String cryptogram) {
         Stack<Character> stack = new Stack<>();
         char[] alphabets = getAlphabets(cryptogram);
-        char pointer = alphabets[0];
-        stack.push(pointer);
-        boolean removeFlag = false;
-        for (int i = 1; i < alphabets.length; i++) {
-            char curAlpha = alphabets[i];
-            if (stack.isEmpty()) {
-                stack.push(curAlpha);
-            } else {
-                if (pointer == curAlpha) {
-                    if (!removeFlag) {
-                        removeFlag = true;
-                    }
-                }
-                else {
-                    if (removeFlag) {
+        char tempAlphabet = '0';
+        boolean flag = true;
+
+        for (int i = 0; i < alphabets.length; i++) {
+            char alphabet = alphabets[i];
+            if (!stack.isEmpty()) {
+                if (stack.peek() == alphabet) {
+                    if (tempAlphabet != alphabet) {
+                        tempAlphabet = alphabet;
                         stack.pop();
-                        pointer = stack.peek();
-                        removeFlag = false;
-                        i--;
                     }
-                    else {
-                        stack.push(curAlpha);
-                        pointer = stack.peek();
-                        removeFlag = false;
-                    }
+                    continue;
+                } else {
+                    System.out.println(" 알파벳과 peek이 다르면? stack.peek() = " + stack.peek() + " / push = " + alphabet);
+                    stack.push(alphabet);
                 }
+                System.out.println(stack);
+            } else {
+                stack.push(alphabet);
             }
         }
-        if (!stack.isEmpty() && removeFlag) {
-            stack.pop();
-        }
-
-
+//        char pointer = alphabets[0];
+//        stack.push(pointer);
+//        boolean removeFlag = false;
+//        for (int i = 1; i < alphabets.length; i++) {
+//            char curAlpha = alphabets[i];
+//            if (stack.isEmpty()) {
+//                stack.push(curAlpha);
+//            } else {
+//                if (pointer == curAlpha) {
+//                    if (!removeFlag) {
+//                        removeFlag = true;
+//                    }
+//                }
+//                else {
+//                    if (removeFlag) {
+//                        stack.pop();
+//                        pointer = stack.peek();
+//                        removeFlag = false;
+//                        i--;
+//                    } else {
+//                        stack.push(curAlpha);
+//                        pointer = stack.peek();
+//                    }
+//                }
+//            }
+//        }
+//        if (!stack.isEmpty() && removeFlag) {
+//            stack.pop();
+//        }
         return makeStackToString(stack);
     }
     private static String makeStackToString(Stack<Character> stack) {
