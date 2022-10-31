ec435faa7942d5d7d4ca3b549fa65643550e91c4
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-01 01:45:11 +0900

    Feat(Problem6): 인원이 1명 이상 10000명 이상인지

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    private static boolean isValidPersonal(int n) {
+        return n >= 1 && n <= 10_000;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = List.of("");
+        if(!isValidPersonal(forms.size())) return answer;
+
+
+
         return answer;
     }
 }
