a5933d7bb887d60d8cf1f2911235375a74d606c0
HASEUNGHEEE <ariul123@gmail.com>
2022-11-01 00:59:45 +0900

    feat: Problem6 닉네임을 토큰화하는 기능 구현
    닉네임을 2글자 토큰으로 자르고, 닉네임 토큰들을 맵에 저장한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static Map<String,Integer> nicknameTokensToCount = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static void tokenizeNickname(String nickname) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String token = nickname.substring(i ,i + 2);
+            if (nicknameTokensToCount.containsKey(token)) {
+                nicknameTokensToCount.put(token, nicknameTokensToCount.get(token) + 1);
+            } else {
+                nicknameTokensToCount.put(token, 1);
+            }
+        }
     }
 }
