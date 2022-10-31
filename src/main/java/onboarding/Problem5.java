643c57c4c3846d562d6aba7cc4e1d50562988e24
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-01 08:54:18 +0900

    comment: P5 기능 목록 주석 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 1: 가장 큰 금액 화폐부터 뽑도록 하는 기능 구현
+ */
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
