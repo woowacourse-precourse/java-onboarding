96dbad5e0cbdd9f8f0503a2aaf50b3288b6d896d
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-10-31 13:31:39 +0900

    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
+import java.util.Scanner;
 
 public class Problem4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String W = sc.nextLine();
+       System.out.println(solution(W));
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+      String answer = "";
+      for(int i = 0; i < word.length(); i++){
+        char c = word.charAt(i);
+        if(Character.isUpperCase(c)){
+          c = (char) (90 - c + 65);
+        }
+        else if (Character.isLowerCase(c)) {
+          c = (char) (122 - c + 97);
+        }
+        answer += String.valueOf(c);
+      }
+      return answer;
     }
 }
