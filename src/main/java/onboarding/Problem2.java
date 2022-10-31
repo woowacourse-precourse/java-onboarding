6071000e768849bb5e7af6eef272adcbaad23421
YUSEONJUN <seonjun0906@gmail.com>
2022-10-31 23:44:47 +0900

    [Problem 2] 기능 요구 사항 정리의 1,2번 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Stack;
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+	public static String solution(String cryptogram) {
+		String answer = "answer";
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < cryptogram.length(); i++) {
+			if (!stack.isEmpty()) {
+				if (stack.peek() == cryptogram.charAt(i)) {
+					stack.pop();
+					continue;
+				}
+			}
+			stack.push(cryptogram.charAt(i));
+		}
+	}
 }
