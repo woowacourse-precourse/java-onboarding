67de8f62e29820365da0b547b01c5ee1f6919609
ddogga <hayeonlee60327@gmail.com>
2022-10-31 12:29:13 +0900

    문제2 연속되는 문자를 삭제하는 메서드 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,18 +5,33 @@ public class Problem2 {
 
         String answer = "answer";
 
+
         for (int i = 1; i < cryptogram.length(); i++) {
             if (cryptogram.charAt(i) == cryptogram.charAt(i-1)) {
-                
+
+
                 int lastIdx = findLastIdx(cryptogram, i-1);
-                System.out.println("startIdx = " + (i-1));
-                System.out.println("lastIdx = " + lastIdx);
+                String result = deleteStr(i-1,lastIdx,cryptogram);
+                System.out.println("result = " + result);
             }
         }
 
         
 
         return answer;
+    }
+
+
+    //찾은 인덱스로 삭제하는 함수
+    public static String deleteStr(int startIdx, int lastIdx, String cryptogram){
+
+        StringBuffer str = new StringBuffer(cryptogram);
+
+
+        cryptogram = str.delete(startIdx,lastIdx + 1).toString();
+
+
+        return cryptogram;
     }
 
 
