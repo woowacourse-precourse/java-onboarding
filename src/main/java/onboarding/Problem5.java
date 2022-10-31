86bdb826148fc0a21d1e39de37786f69291f144b
pdot715 <pdot715@gmail.com>
2022-10-31 22:26:49 +0900

    feat(문제5):출금 금액에 따른 화폐 종류별 개수 구하기 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,9 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
+        // 변환할 화폐 종류를 금액이 큰 순서대로 넣은 배열 선언
         int unit[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        // 금액이 큰 순서대로 변환 받고 answer 리스트에 삽입
         for (int i = 0; i < 9; i++) {
             answer.add((money/unit[i]));
             money %= unit[i];
