c610cbf1fdd49d6deebbd8ab425efbec66c96f39
tjddnr7760 <tjddnr7760@naver.com>
2022-10-31 20:07:38 +0900

    <7번 - 구현> 정렬 및 출력, 예외처리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,11 +5,16 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Comparator;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
         Problem7 problem = new Problem7();
+
         HashMap<String, Integer> listpeople;
         ArrayList<String> userfriendslist;
         ArrayList<String> friendfrinedlist;
@@ -34,6 +39,27 @@ public class Problem7 {
             if(listpeople.containsKey(e)) {
                 listpeople.put(e, listpeople.get(e) + 1);
             }
+        }
+
+        ArrayList<PointPair> resultlist = new ArrayList<>();
+
+        listpeople.forEach((key, value) -> {
+            resultlist.add(new PointPair(key, value));
+        });
+
+        Collections.sort(resultlist, new NameComparator());
+        Collections.sort(resultlist, new pointComparator().reversed());
+
+        for(int i=0; i<resultlist.size(); i++) {
+            if(resultlist.get(i).point == 0) {
+                break;
+            }
+
+            if(i == 5) {
+                break;
+            }
+
+            answer.add(resultlist.get(i).name);
         }
 
         return answer;
@@ -101,5 +127,28 @@ public class Problem7 {
         }
 
         return result;
+    }
+
+}
+
+class PointPair {
+    String name;
+    Integer point;
+
+    PointPair(String name, int point){
+        this.name = name;
+        this.point = point;
+    }
+}
+
+class NameComparator implements Comparator<PointPair> {
+    public int compare(PointPair o1, PointPair o2) {
+        return o1.name.compareTo(o2.name);
+    }
+}
+
+class pointComparator implements Comparator<PointPair> {
+    public int compare(PointPair o1, PointPair o2) {
+        return o1.point.compareTo(o2.point);
     }
 }
