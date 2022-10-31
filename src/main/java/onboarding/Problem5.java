80c38fbf5d739b20fa1ec7a25409d276ddcb9f62
lacram <chemin9898@gmail.com>
2022-11-01 00:29:38 +0900

    Problem 5 - 금액 단위가 주어졌을 때 반환되는 동전 개수 구하기 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,14 @@ import java.util.List;
 * 2. 모든 단위별 동전 개수 구하기
 * */
 public class Problem5 {
+
+    // 금액 단위가 주어졌을 때 반환되는 동전 개수 구하기
+    public static int getMoneyCount(int money, int moneyUnit, List<Integer> answer) {
+        answer.add(money / moneyUnit);
+        // 남은 돈 리턴
+        return money % moneyUnit;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
