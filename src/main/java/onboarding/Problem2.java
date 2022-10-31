f9df4bb700418524887db3b44e9f343c2b4193f1
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-01 03:10:31 +0900

    P2: fix the function that delete duplication characters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,37 +3,55 @@ package onboarding;
 public class Problem2 {
     public static String delSame(String cryptogram, int i) {
         char tar = cryptogram.charAt(i);
-        if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)){
-            int start = i;
-            int end = i;
+        int count = cryptogram.length() - cryptogram.replaceAll(String.valueOf(tar),"").length();
+        int temp = i;
+        int start = i;
+        int end = i;
 
-            if (cryptogram.length() == 2){
-                return "";
+        if (count == cryptogram.length()){
+            return "";
+        }
+
+        if (i > 0 && tar == cryptogram.charAt(i - 1)){
+            while (start > 0 && tar == cryptogram.charAt(i)){
+                start -= 1;
+                i -= 1;
             }
+        }
 
-            while (tar == cryptogram.charAt(i)){
+        i = end;
+
+        if (i < cryptogram.length() - 1 && tar == cryptogram.charAt(i + 1)){
+            while (end < cryptogram.length() - 1 && tar == cryptogram.charAt(i)){
                 end += 1;
                 i += 1;
             }
-
-            cryptogram = cryptogram.substring(0, start) + cryptogram.substring(end);
         }
+
+        if (start != end){
+            if (end == cryptogram.length() -1 && tar == cryptogram.charAt(end)) {
+                cryptogram = cryptogram.substring(0, start);
+            } else if (temp > start) {
+                cryptogram = cryptogram.substring(0, start + 1) + cryptogram.substring(end + 1);
+            } else {
+                cryptogram = cryptogram.substring(0, start) + cryptogram.substring(end);
+            }
+        }
+
         return cryptogram;
     }
 
     public static String solution(String cryptogram) {
         String answer = "answer";
         int i = 0;
-        while (i < cryptogram.length() - 1) {
-            int temp = cryptogram.length();
+        while (i < cryptogram.length()) {
+            String temp = cryptogram;
             cryptogram = delSame(cryptogram, i);
 
-            if(temp != cryptogram.length()){
-                if (i > 0){
-                    i -= 1;
-                }
-            } else {
+            if(temp.length() == cryptogram.length()) {
                 i += 1;
+            } else if (i != 0 && temp.charAt(i) != cryptogram.charAt(i)) {
+                i -= 1;
             }
         }
         answer = cryptogram;
