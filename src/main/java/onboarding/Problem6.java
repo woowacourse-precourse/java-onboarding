fd506b02f742d677f76395a30d1b8141fe29ed8a
hodomaroo <enghemdwkd@gmail.com>
2022-11-01 05:19:08 +0900

    [Problem6/FEAT] Problem6 클래스를 DuplicateNicknameDectector 클래스의 메서드 구현에 맞춰 수정, 테스트 검증을 완료하였습니다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,12 +1,20 @@
 package onboarding;
 
-import javax.xml.stream.FactoryConfigurationError;
+
 import java.util.*;
+import onboarding.problem6.DuplicateNicknameDetector;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = Collections.emptyList();
+        DuplicateNicknameDetector nicknameDetector = new DuplicateNicknameDetector();
 
-        return answer;
+        for(List<String> form : forms){
+            nicknameDetector.registerUserAndUpdateDuplicate(form);
+        }
+
+        List<String> duplicateNicknameOwnerEmails = nicknameDetector.getDuplicateNicknameOwnerEmails();
+        Collections.sort(duplicateNicknameOwnerEmails);;
+
+        return duplicateNicknameOwnerEmails;
     }
 }
