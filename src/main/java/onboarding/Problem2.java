5696e7f8dc7dd3528c1a9060d46d32ca70e4685a
eugene1204 <s1061001@naver.com>
2022-10-31 16:08:55 +0900

    p2, 2. 중복된 글자를 없애주는 메소드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -16,6 +16,28 @@ public class Problem2 {
         List<String> list = new ArrayList<>();
         for(String s1 : arr) list.add(s1);
         return list;
+    }
+
+    public static List<String> cryptoDedupli(List<String> list){ // 중복된 글자를 없애는 매소드
+        List<String> lastList = list;
+
+        List<String> newList = new ArrayList<>();
+        if(lastList.size()== newList.size()) return lastList;
+        String last =lastList.get(0);
+
+        for (int i =1; i<lastList.size(); i++){ // zyelleyz -> zyeeyz -> zyyz -> zz
+            if(!last.equals(lastList.get(i))) {
+                newList.add(lastList.get(i-1));
+                last= lastList.get(i);
+            }else {
+                last= lastList.get(i);
+                i++;
+            }
+        }
+        if(!(lastList.size()==2 && last.equals(lastList.get(lastList.size()-1)))) {
+            newList.add(lastList.get(lastList.size()-1));
+        }
+        return  newList;
 
     }
 
