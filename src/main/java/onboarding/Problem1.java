625743ab57407d37d486332fe1dd40ea8429f12f
lhyeonj120 <isk04387@gmail.com>
2022-11-01 02:37:26 +0900

    [problem1] feat: 페이지 번호의 각 숫자 곱하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,12 +13,26 @@ class Problem1 {
 		return res;
 	}
 	
+	private static int allMul(int n) {
+		int res = 1;
+		
+		while(n != 0) {
+			res *= n % 10;
+			n /= 10;
+		}
+		return res;
+	}
+	
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         int pobiAddLeftPage = allAdd(pobi.get(0));
         int pobiAddRightPage = allAdd(pobi.get(1));
         int crongAddLeftPage = allAdd(crong.get(0));
         int crongAddRightPage = allAdd(crong.get(1));
+        int pobiMulLeftPage = allMul(pobi.get(0));
+        int pobiMulRightPage = allMul(pobi.get(1));
+        int crongMulLeftPage = allMul(crong.get(0));
+        int crongMulRightPage = allMul(crong.get(1));
         
         return answer;
     }
