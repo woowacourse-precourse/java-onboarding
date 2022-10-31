e432040dadfd9eaf5b0b681480c62cf397a4c33c
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-10-31 22:40:42 +0900

    feat: Implement solution in problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
+
+    final static List<Integer> billList = new ArrayList<>(
+        Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int bill: billList) {
+            int count = money / bill;
+            answer.add(count);
+            money = money % bill;
+        }
+
         return answer;
     }
 }
