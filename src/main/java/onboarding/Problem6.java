4e052c1124b53dd1c463260ec9b6aa7cda37cec1
PPakSang <sanghyun-dev@naver.com>
2022-10-31 18:25:55 +0900

    Feat: Problem6 내부 연산 로직 추가
    이름 패턴: 이름을 연속된 두 글자 단위로 나눈 것
    patternMatchedEmailStorage: 같은 이름 패턴의 이메일 집합 쌍을 저장하는 메모리 저장소
    savePattern(String, String): username 의 이름 패턴에 맞게 패턴 저장소에 이메일을 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    private static final Map<String, Set<String>> patternMatchedEmailStorage = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> emails = new HashSet<>();
+
+        forms.forEach(userInfo -> {
+            String email = userInfo.get(0);
+            String username = userInfo.get(1);
+
+            savePattern(email, username);
+        });
+
+    }
+
+    private static void savePattern(String email, String username) {
+        for (int i = 0; i < username.length()-1; i++) {
+            String pattern = username.substring(i, i + 2);
+
+            Set<String> emailSet = patternMatchedEmailStorage.get(pattern);
+            if (emailSet == null) {
+                emailSet = new HashSet<>();
+                patternMatchedEmailStorage.put(pattern, emailSet);
+            }
+            emailSet.add(email);
+        }
     }
 }
