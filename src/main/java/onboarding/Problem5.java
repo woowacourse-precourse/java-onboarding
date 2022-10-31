6669f7246d6a07e8874e86c33b3f48ed01849ac4
juy4556 <kws05050@naver.com>
2022-10-31 15:53:30 +0900

    feat: add problem5 solution

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> valueList = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        for (Integer value : valueList) {
+            answer.add(money / value);
+            money -= (money / value) * value;
+        }
         return answer;
     }
 }
