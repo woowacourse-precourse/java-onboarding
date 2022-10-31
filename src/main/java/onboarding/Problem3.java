55502dfcf5e53513d2c31d2966fb89dbe1e32d88
tkdtn4657 <tkdtn101226@gmail.com>
2022-10-31 22:47:13 +0900

    prblem3 tsn_calc 10의자리까지 369계산

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,22 +2,39 @@ package onboarding;
 
 class tsn_game{
 	
+	int tsn = 0;
+	int clap = 0;
+	
 	tsn_game(){
 	}
 	
 	tsn_game(int n){
-		
+		this.tsn = n;
 	}
 	
+	int tsn_calc() {
+		int i, temp;
+		
+		for(i = 1; i <= tsn; i++) {
+			if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+				clap++;
+			}
+			if(i/10 == 3 || i/10 == 6 || i/10 == 9) {
+				clap++;
+			}	
+		}
+		return clap;
+	}
 }
 
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;        
+    public static int solution(int number) {      
+        tsn_game g = new tsn_game(number);
+        
         
         if(number >= 1 && number <= 10000) {
-        	return answer;
+        	return g.tsn_calc();
         }
         return 0;
     }
