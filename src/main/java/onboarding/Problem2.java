256b9c65cf6c6324b3610de658fddf87a848d913
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-10-31 23:01:23 +0900

    feat: Problem 2, make deleteDubplicate Function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,31 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        String answer  = deleteDuplicate(cryptogram);
         return answer;
+    }
+
+    private static String deleteDuplicate(String crytogram) {
+        char[] charArray = crytogram.toCharArray();
+        char preChar = charArray[0];
+        int continueLength = 1;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < charArray.length; i++){
+            if(preChar == charArray[i]){
+                continueLength++;
+                continue;
+            }
+            else if(continueLength == 1){
+                sb.append(preChar);
+                preChar = charArray[i];
+                continueLength = 1;
+            }
+            else{
+                preChar = charArray[i];
+                continueLength = 1;
+            }
+        }
+        return sb.toString();
     }
 }
