bcdbd716545a4159b1f321ebf758630e5d145662
metorg <metrog950@gmail.com>
2022-10-31 14:52:48 +0900

    feat: [Problem6] 배열체크 메서드 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static List<String> listCheck(List<List<String>> forms){
+
+        List<String> result = new ArrayList<>();
+
+        for(int i=0;i<forms.size();i++){
+
+        }
+        return result;
     }
 }
