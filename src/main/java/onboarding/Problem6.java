f5eba55e0b164f394ea91c47ca13759edd9d9bcf
doehy <dbehxks02@naver.com>
2022-10-31 22:56:45 +0900

    feat : HashMap에 닉네임당 두글자씩 갯수를채우는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    static Map<String,String> maps = new HashMap<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        fillToMap(forms);
         return answer;
     }
+    static void fillToMap(List<List<String>> forms){
+        for(List<String> data : forms) {fillToSet(data.get(1));}
+    }
+
 }
