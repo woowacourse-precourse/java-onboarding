ba17b4000ebecf6cf8f9dcd8d734c5715180a4c6
panorama713 <yaiba713@gmail.com>
2022-10-31 15:02:43 +0900

    problem solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,51 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        // 엄마 사전 대문자
+        char[] mom_arr_up = new char[26];
+        for(int i=0; i<26; i++){
+            mom_arr_up[i] = (char)(65 + i);
+        }
+        // 엄마 사전 소문자
+        char[] mom_arr_lo = new char[26];
+        for(int i=0; i<26; i++){
+            mom_arr_lo[i] = (char)(97 + i);
+        }
+        // 청개구리 사전 대문자
+        char[] ch_arr_up = new char[26];
+        for(int i=0; i<26; i++){
+            ch_arr_up[i] = (char)(90 - i);
+        }
+        // 청개구리 사전 소문자
+        char[] ch_arr_lo = new char[26];
+        for(int i=0; i<26; i++){
+            ch_arr_lo[i] = (char)(122 - i);
+        }
+        // 입력받은 문자를 배열로 변환
+        char[] arr = word.toCharArray();
+        // 배열의 문자를 청개구리 사전으로 변환
+        for(int i=0; i<arr.length; i++){
+            // 대문자의 경우
+            for(int j=0; j<26; j++){
+                if(arr[i] == mom_arr_up[j]){
+                    arr[i] = ch_arr_up[j];
+                    // 같은 알파벳을 찾았을 때 변환 후 if문 종료
+                    break;
+                }
+            }
+            // 소문자의 경우
+            for(int j=0; j<26; j++){
+                if(arr[i] == mom_arr_lo[j]){
+                    arr[i] = ch_arr_lo[j];
+                    // 같은 알파벳을 찾았을 때 변환 후 if문 종료
+                    break;
+                }
+            }
+        }
+        // answer에 정답 입력
+        for(int i=0; i<arr.length; i++){
+            answer += arr[i];
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,66 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+    public static List<String> solution(
+            String user, List<List<String>> friends, List<String> visitors) {
+        List<String> answer = new ArrayList<>();
+        // user의 친구
+        List<String> user_friend = new ArrayList<>();
+        // user의 함께 아는 친구
+        List<String> too_friend = new ArrayList<>();
+        // 추천 알고리즘 점수
+        Map<String, Integer> score = new HashMap<>();
+        // user의 친구 추가
+        for(int i=0; i<friends.size(); i++){
+            if(friends.get(i).get(1) == user){
+                user_friend.add(friends.get(i).get(0));
+            }
+        }
+        // user의 함께 아는 친구 추가
+        for(int i=0; i<friends.size(); i++){
+            for(int j=0; j<user_friend.size(); j++){
+                if(friends.get(i).get(0) == user_friend.get(j)
+                        && user != friends.get(i).get(1)){
+                    too_friend.add(friends.get(i).get(1));
+                }
+            }
+        }
+        // 추천 알고리즘 점수 계산
+        // 함께 아는 친구 점수 계산
+        for(String too : too_friend){
+            if(score.containsKey(too)){
+                score.put(too,score.get(too) + 10);
+            }else{
+                score.put(too,10);
+            }
+        }
+        // 방문한적 있는 함께 아는 친구 점수 계산
+        for(String v : visitors){
+            if(score.containsKey(v) && user_friend.contains(v) == false){
+                score.put(v,score.get(v) + 1);
+            }else if(user_friend.contains(v) == false){
+                score.put(v,1);
+            }
+        }
+        // 점수 크기 순으로 정렬(내림차순)
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(score.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+//            @Override
+            public int compare(Map.Entry<String, Integer> t1, Map.Entry<String, Integer> t2) {
+                return t2.getValue() - t1.getValue();
+            }
+        });
+
+        // 5개까지 answer 추가
+        for(Map.Entry<String, Integer> t: list){
+            if(answer.size() < 5){
+                answer.add(t.getKey());
+            }else{
+                break;
+            }
+        }
         return answer;
     }
 }
