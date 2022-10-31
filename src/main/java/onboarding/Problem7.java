68d99ba3030fab738644d5a15d09bca7dc627c5b
GraphTree <graphnodesed@gmail.com>
2022-10-31 16:55:04 +0900

    Feat: 기준이 되는 유저와 몇명이나 같은 친구를 가지는가에 따라 점수를 매기고, 이를 메트릭으로 나타내는 friendScorePare 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -20,4 +20,66 @@ public class Problem7 {
         Collections.sort(result);
         return result;
     }
+    public static Map<String, Integer> friendScorePare(String user, List<List<String>> friends){
+        List<String> namesList = nameSetCreator(user,friends);
+        Collections.sort(namesList);
+        List<List<Integer>> matrix = new ArrayList<>(namesList.size());
+
+        for (int i = 0; i < namesList.size(); i++) {
+            matrix.add(new ArrayList<>(namesList.size()));
+            for (int j = 0; j < namesList.size(); j++) {
+                matrix.get(i).add(0);
+            }
+        }
+
+        for (List<String> friend : friends) {
+            int x = namesList.indexOf(friend.get(0));
+            int y = namesList.indexOf(friend.get(1));
+
+            matrix.get(x).set(y, 1);
+            matrix.get(y).set(x, 1);
+
+        }
+
+        // test
+
+//        System.out.println(String.join(", ", namesList));
+//        for (int i = 0; i < matrix.size(); i++) {
+//            System.out.println(matrix.get(i).toString());
+//        }
+
+        //test end
+
+
+        Map<String, Integer> scorePare = new HashMap<String, Integer>();
+        for (String name : namesList) {
+            scorePare.put(name, 0);
+        }
+
+
+
+        int userIndex = namesList.indexOf(user);
+        List<Integer> userScore = matrix.get(userIndex);
+        for (int i = 0; i <namesList.size(); i++) {
+            if (i != userIndex) {
+                List<Integer> compUserScore = matrix.get(i);
+                for (int j = 0; j < compUserScore.size(); j++) {
+                    int adder = (userScore.get(j)==1 && compUserScore.get(j)==1) ? 10 : 0;
+
+                    //test®
+//                    if(userScore.get(j)==1 && compUserScore.get(j)==1) {
+//                        System.out.println(userScore.get(j) + ", " + compUserScore.get(j));
+//                    }
+                    //test end
+
+                    scorePare.put(namesList.get(i), scorePare.get(namesList.get(i)) + adder);
+                }
+            }
+        }
+
+        return scorePare;
+
+
+    }
+
 }
