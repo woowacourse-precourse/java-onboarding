e8c5b12f8df09f570e38cc856c56f1a03c3e3eac
gyumin159 <gyumin159@DESKTOP-6CKHGBO>
2022-10-31 19:39:48 +0900

    가장 큰 단위부터 나누어서 몫과 나머지를 계산하자

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+    public static List<Integer> solution(int money) { // 50000원 부터 나누어서 가장 작은 단위까지 몫을 구하자
+        List<Integer> answer = new ArrayList<Integer>(9);
+        int temp = money;
+        int[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        
         return answer;
     }
 }
