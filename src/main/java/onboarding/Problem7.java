a02eb169fed9794a88b611e75ee8de6373432a53
minaver <psmjoshua@gmail.com>
2022-10-31 16:28:32 +0900

    refactor(problem7): removeUserFriend, calcScoreFriends 메소드 loop 돌리는 list 변수화(duration 5ms 단축)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -47,8 +47,12 @@ public class Problem7 {
 
     // user와 user의 직접적인 친구는 추천에서 제외
     public void removeUserFriend(String user){
+        // user 본인 제외
         friendScoreMap.remove(user);
-        for(String userFriend : friendGraphMap.get(user)){
+
+        // user 직접적인 친구들 제외
+        List<String> userFriendList = friendGraphMap.get(user);
+        for(String userFriend : userFriendList){
             friendScoreMap.remove(userFriend);
         }
     }
@@ -57,8 +61,10 @@ public class Problem7 {
     public void calcScoreFriends(String user){
         List<String> userFriendList = friendGraphMap.get(user);
 
+        // 사용자의 직접적인 친구들의 친구들이 득점의 대상이 된다.
         for(String userFriend : userFriendList){
-            for(String scoredFriend : friendGraphMap.get(userFriend)){
+            List<String> userTwoDepthFriendList = friendGraphMap.get(userFriend);
+            for(String scoredFriend : userTwoDepthFriendList){
                 friendScoreMap.put(scoredFriend,(friendScoreMap.get(scoredFriend) + 10));
             }
         }
@@ -67,13 +73,13 @@ public class Problem7 {
     // 사용자의 타임 라인에 방문한 횟수로 score 계산(1점)
     public void calcScoreVisitor(List<String> visitors) {
         for (String visitor : visitors) {
-            // 이미 friend에서 추가된 crew일 경우
+            // 이미 friendGraph에서 추가된 crew일 경우
             if (friendScoreMap.containsKey(visitor)) {
                 friendScoreMap.put(visitor, (friendScoreMap.get(visitor) + 1));
                 continue;
             }
 
-            // friend에서 추가되지 않은 신규 crew일 경우
+            // friendGraph에서 추가되지 않은 신규 crew일 경우
             friendScoreMap.put(visitor, 1);
         }
     }
@@ -84,22 +90,20 @@ public class Problem7 {
             boolean isFirstCrewExist = friendGraphMap.containsKey(crew.get(0));
             boolean isSecondCrewExist = friendGraphMap.containsKey(crew.get(1));
 
-            if(isFirstCrewExist && isSecondCrewExist) {
-                // 둘다 그래프에 있을시
+            if(isFirstCrewExist && isSecondCrewExist) {     // 둘다 그래프에 있을시
                 friendGraphMap.get(crew.get(0)).add(crew.get(1));
                 friendGraphMap.get(crew.get(1)).add(crew.get(0));
                 continue;
             }
-            if(!isFirstCrewExist){
-                // 첫번째 친구가 Graph에 없을시
+            if(!isFirstCrewExist){      // 첫번째 친구가 Graph에 없을시 Graph와 Score에 추가
                 friendGraphMap.put(crew.get(0),new ArrayList<>());
                 friendScoreMap.put(crew.get(0),0);
             }
-            if(!isSecondCrewExist){
-                // 두번째 친구가 Graph에 없을시
+            if(!isSecondCrewExist){     // 두번째 친구가 Graph에 없을시 Graph와 Score에 추가
                 friendGraphMap.put(crew.get(1),new ArrayList<>());
                 friendScoreMap.put(crew.get(1),0);
             }
+            // 없는 친구를 Graph에 넣은 후 친구 관계 추가(VALUE)
             friendGraphMap.get(crew.get(0)).add(crew.get(1));
             friendGraphMap.get(crew.get(1)).add(crew.get(0));
         }
