79c9fc86ca48b1a265abee0542be5568213ad0b9
yongseungLeeDec <yonson.dev@gmail.com>
2022-10-31 23:01:36 +0900

    feat: Problem1 solution 메서드 구현
    - Problem1 의 기능요구사항을 반영한 solution 함수를 구현함.
    - solution 함수의 도우미 함수getMaximalNumberOption 함수를 구현함.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,38 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
+        if (pobiRightPage - pobiLeftPage != 1 || crongRightPage - crongLeftPage != 1) {
+            return -1;
+        }
+
+        int pobiNum = Math.max(getMaximalNumberOption(pobiLeftPage), getMaximalNumberOption(pobiRightPage));
+        int crongNum = Math.max(getMaximalNumberOption(crongLeftPage), getMaximalNumberOption(crongRightPage));
+
+        if (pobiNum > crongNum) {
+            return 1;
+        } else if (pobiNum < crongNum) {
+            return 2;
+        }
+
+        return 0;
+    }
+
+    private static int getMaximalNumberOption(int num) {
+        int summedDigits = 0;
+        int multipliedByDigits = 1;
+
+        while (num > 0) {
+            int digitValue = num % 10;
+            num /= 10;
+            summedDigits += digitValue;
+            multipliedByDigits *= digitValue;
+        }
+
+        return Math.max(summedDigits, multipliedByDigits);
     }
 }
