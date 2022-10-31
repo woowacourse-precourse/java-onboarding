bafcb85bce65f86766f4d1ce2dd3c5fe34280288
HyebinShin <hyeviin.dev@gmail.com>
2022-10-31 19:25:28 +0900

    암호문 문자 리스트

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        System.out.println("====solution====");
+        // 암호문 문자 리스트
+        ArrayList<Character> result = new ArrayList<>();
+        for(int i=0; i<cryptogram.length(); i++) {
+            result.add(cryptogram.charAt(i));
+        }
+        System.out.println("result: "+ Arrays.toString(result.toArray()));
+
         String answer = "answer";
         return answer;
     }
