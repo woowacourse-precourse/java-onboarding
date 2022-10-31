24061e1714826ed0f0907a5d7682bd56ed8ebde3
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-10-31 23:55:59 +0900

    feat: Problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,17 +1,29 @@
 package onboarding;
 
 public class Problem3 {
-
+    public static int calNum(int num){
+        int count = 0;
+        while (num >0)
+        {
+            int oneDigit = num%10;
+            if (oneDigit ==3 || oneDigit ==6 || oneDigit ==9)
+            {
+                count++;
+            }
+            num= num/10;
+        }
+        return count;
+    }
     public static int solution(int number)
     {
         int answer = 0;
-        // 1의 자리
-        // 3,6,9 일때
-
+        for (int i = 1 ; i<= number ; i++) {
+            answer += calNum(i);
+        }
         return answer;
     }
 
-    public static void main(String[] args) {
-        System.out.println(solution(13));
-    }
+   /* public static void main(String[] args) {
+        System.out.println(solution(10000));
+    }*/
 }
