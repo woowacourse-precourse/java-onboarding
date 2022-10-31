2994523b20cc02037cc9052644fb0e5051cc59bc
swjeong98 <tree5678@konkuk.ac.kr>
2022-10-31 17:45:36 +0900

    문제 4 기능 1 - 입력이 알파벳인지 확인

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+    public static boolean isAlpha(char ch){
+        if(ch >= 'a' && ch <= 'z') return true;
+        if(ch >= 'A' && ch <= 'Z') return true;
+        return false;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
