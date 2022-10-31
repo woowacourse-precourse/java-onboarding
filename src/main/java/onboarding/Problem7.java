ced0d14f94c29ca9374ac5944dbcadc54eb770a0
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-11-01 02:38:46 +0900

    feat : 추천 점수 계산

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,14 +1,23 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Map<String, List<String>> relations = new HashMap<>();
-        setRelations(friends, relations);
+        Map<String, Integer> scoreMap = new HashMap<>();
 
-        List<String> answer = Collections.emptyList();
-        return answer;
+        setRelations(friends, relations);
+        calCloseFriendScore(user, relations, scoreMap);
+        calVisitorsScore(visitors, scoreMap);
+        removeOriginFriend(user, relations, scoreMap);
+
+        return scoreMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .map(Map.Entry::getKey)
+                .limit(5)
+                .collect(Collectors.toList());
     }
 
     private static void setRelations(List<List<String>> friends, Map<String, List<String>> relations) {
@@ -22,4 +31,29 @@ public class Problem7 {
             relations.put(friend.get(1), friend1List);
         }
     }
+
+    private static void calCloseFriendScore(String user, Map<String, List<String>> relations, Map<String, Integer> scoreMap) {
+        List<String> userFriends = relations.get(user);
+        for (String userFr : userFriends) {
+            List<String> closeFriends = relations.get(userFr); // 친구의 친구 목록
+            for (String closeFr : closeFriends) {
+                scoreMap.put(closeFr, scoreMap.getOrDefault(closeFr, 0) + 10); // 10점 더하기
+            }
+        }
+    }
+
+    private static void calVisitorsScore(List<String> visitors, Map<String, Integer> scoreMap) {
+        for (String visitor : visitors) {
+            scoreMap.put(visitor, scoreMap.getOrDefault(visitor, 0) + 1); // 1점 더하기
+        }
+    }
+
+    private static void removeOriginFriend(String user, Map<String, List<String>> relations, Map<String, Integer> scoreMap) {
+        scoreMap.remove(user);
+        List<String> userFriends = relations.get(user);
+        for (String friend : userFriends) {
+            scoreMap.remove(friend);
+        }
+    }
+
 }
