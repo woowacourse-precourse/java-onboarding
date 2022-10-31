bb013579f9111f512d523def4191f136f0518ca0
minju412 <ln8847@ajou.ac.kr>
2022-10-31 21:59:00 +0900

    style: [p3] 변수 명 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,14 +3,16 @@ package onboarding;
 public class Problem3 {
 	public static int solution(int number) {
 		int cnt = 0;
+		String strNumber = "";
+		char oneLetter = ' ';
 
 		for (int i = 1; i <= number; i++) {
-			String str = String.valueOf(i); // int -> string 변환
+			strNumber = String.valueOf(i); // int -> string 변환
 
 			// 예를 들어, 36 이라는 숫자에는 박수 두 번
-			for (int j = 0; j < str.length(); j++) {
-				char ch = str.charAt(j);
-				if (ch == '3' || ch == '6' || ch == '9') {
+			for (int j = 0; j < strNumber.length(); j++) {
+				oneLetter = strNumber.charAt(j);
+				if (oneLetter == '3' || oneLetter == '6' || oneLetter == '9') {
 					cnt++;
 				}
 			}
