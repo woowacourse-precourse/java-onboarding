98d2e885c95b6084b1852d40252402799deaa60d
D <doxxx93@gmail.com>
2022-10-31 13:28:32 +0900

    Problem5 (#7)
    * docs: add requirement checklist
    
    * feat: solve problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int[] CURRENCY = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        while (money > 0) {
+            for (int unit : CURRENCY) {
+                if (money >= unit) {
+                    answer.add(money / unit);
+                    money = money % unit;
+                    continue;
+                }
+                answer.add(0);
+            }
+        }
         return answer;
     }
 }
