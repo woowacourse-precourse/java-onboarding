360c236fc6ab75b96b30a87f6e54e39c9ff8cacc
jongmyeongbak <withpleasure@gmail.com>
2022-11-01 02:17:05 +0900

    clap number of all

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++){
+            answer += clapnumber(i);
+        }
         return answer;
     }
 
