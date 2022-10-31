602cd26d1ab754b2ff0cc4baf38ed0a98ef3cf5c
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-10-31 17:03:55 +0900

    Add initFriendGraph function

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    // 친구관계를 저장할 HashMap을 초기화하는 함수
+    public static HashMap<String, ArrayList<String>> initFriendGraph() {
+        return new HashMap<String, ArrayList<String>>();
     }
 }
