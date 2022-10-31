58c398f1c408ccae631629e48295fa2dc1683378
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-31 21:21:13 +0900

    feat: problem5 필요한 화폐 개수만큼 리스트 넣기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -19,4 +19,19 @@ public class Problem5 {
     private static void createMoneyArr() {
         moneyArr = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     }
+
+    /**
+     * 필요한 화폐 개수만큼 리스트 넣기
+     * @param money
+     * @return List<Integer>
+     */
+    private static List<Integer> resultList(int money) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < moneyArr.length; i++) {
+            int share = money / moneyArr[i];
+            money %= moneyArr[i];
+            list.add(share);
+        }
+        return list;
+    }
 }
