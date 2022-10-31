d2c6da9a1c07272e1bccd0bc48c97c3bea451698
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 02:32:46 +0900

    feat(PROBLEM3): count369() 해당 수의 각자리에 들어있는 369개수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,17 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int n, answer = 0;
-
-        for(int num= 1; num<=number; num++){
-            n = num;
-
-            while(n!=0){
-                if(n%10==3|| n%10==6|| n%10==9)
-                    answer++;
-                n /= 10;
-            }
-        }
+        int answer = 0;
         return answer;
+    }
+
+    public static int count369(int n){
+        int count = 0;
+
+        while(n!=0){
+            if(n%10==3|| n%10==6|| n%10==9)
+                count++;
+            n /= 10;
+        }
+        return count;
     }
 }
