6bf622530124b1e2c79787cc21f02dd66bb970ec
Taehwan2 <taehwansw@gmail.com>
2022-10-31 17:26:23 +0900

    가장 큰액수의 화폐단위부터 나누어 주면서 값이 0 이 될때까지 위에서부터 아래로 연산해준다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,7 +3,7 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-/*
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
         int arr[] = {50000,10000,5000,1000,500,100,50,10,1};
@@ -27,4 +27,3 @@ public class Problem5 {
     }
 
 }
-*/
