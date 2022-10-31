3d8a2297c0e52d3e62436b6ad52474240b52b45d
parkseryu <pjhwork97@gmail.com>
2022-10-31 20:19:05 +0900

    testtest

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+
+
         return answer;
     }
 }
