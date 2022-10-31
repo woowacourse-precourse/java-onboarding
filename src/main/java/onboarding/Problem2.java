8bf8e3515515d33a47b8c8bab1b03f0ae2167226
김선동 <mok03127@hanyang.ac.kr>
2022-11-01 00:22:23 +0900

    feat: 문제 2 더 이상 연속하는 중복 문자들이 없어질 때까지 반복하여 결과 구하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,8 +9,11 @@ public class Problem2 {
          * 더 이상 연속하는 중복 문자들이 없어질 때까지 반복하여 결과를 구한다.
          */
 
-        String answer = "answer";
         String pattern = "([a-z])\\1+"; // 알파벳 소문자들만인 동시에 연속하는 중복 문자들을 찾아주는 정규식
+
+        while(hasConsecutiveDuplicates(cryptogram, pattern)){
+            cryptogram = removeConsecutiveDuplicates(cryptogram, pattern);
+        }
 
         return cryptogram;
     }
@@ -19,4 +22,13 @@ public class Problem2 {
         return cryptogram.replaceAll(pattern, "");
     }
 
+    public static boolean hasConsecutiveDuplicates(String cryptogram, String p){
+        Pattern pattern = Pattern.compile(p);
+        Matcher matcher = pattern.matcher(cryptogram);
+        if (matcher.find()){
+            return true; // 패턴이 있다면 참
+        }else{
+            return false; // 패턴이 없다면 거짓
+        }
+    }
 }
