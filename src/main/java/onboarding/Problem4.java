5621d4e4c30f4f8ba2ca99826fd4515dadbb41f2
박상천 <pswlove38@naver.com>
2022-10-31 18:07:06 +0900

    char받아 반대로 출력하는 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,18 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    public static char Reverse(char c){
+        int up = 155;
+        int low = 219;
+        int temp = c;
+        if(temp >= 65 && temp <= 90){
+            return (char)(up-temp);
+        }
+        return (char)(low-temp);
+    }
+    // char받아 반대로 출력하는 함수
+    public static void main(String[] args){
+        System.out.println(Reverse('A'));
+        System.out.println(Reverse('a'));
+    }
 }
