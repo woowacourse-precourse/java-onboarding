7e50159631af34cb9c951efb4fa9e9713cd42d07
김선동 <mok03127@hanyang.ac.kr>
2022-11-01 00:49:00 +0900

    feat: 문제 3 숫자가 3, 6, 9인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        /*
+         * 숫자를 자릿수 별로 확인하여 3,6,9인지 확인하여 횟수 구한다.
+         */
         int answer = 0;
+        
         return answer;
+    }
+
+    public static boolean is369(int num) {
+        if (num == 3 || num == 6 || num == 9) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
