ed55febc88035b8d30e85f49a4ea19c98fab8008
shpark30 <seohyeonpark30.gmail.com>
2022-11-01 00:15:51 +0900

    test: add 1 case for Problem6

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -214,6 +214,19 @@ class ApplicationTest {
             List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
             assertThat(Problem6.solution(forms)).isEqualTo(result);
         }
+
+        @Test
+        void case3() {
+            List<List<String>> forms = List.of(
+                    List.of("mj@gmail.com", "엠제이"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@email.com", "제이슨"),
+                    List.of("nowm@email.com", "이제엠")
+            );
+            List<String> result = List.of("jason@email.com", "jm@email.com");
+            assertThat(Problem6.solution(forms)).isEqualTo(result);
+        }
     }
 
     @Nested
