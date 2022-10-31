7dd544e70d089be720fe40f105b1fb42ca775a59
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 16:19:33 +0900

    wordCopyNum에 word의 아스키코드 값 입력

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,12 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         char[] wordCopy = word.toCharArray();
+        int[] wordCopyNum = new int[word.length()];
+
+        for (int i = 0; i < wordCopy.length;  i++) {
+            wordCopyNum[i] = (int)wordCopy[i];
+        }
+
 
         return answer;
     }
