0a13531e8652d5d39e199a116e04f6ec9394a1f9
aud19 <aud19980518@gmail.com>
2022-10-31 20:31:24 +0900

    1주차 2번 solution 메소드 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,7 +8,53 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return cryptogramInterpretation(cryptogram);
+    }
+
+    public static String cryptogramInterpretation(String cryptogram) {
+        String newCryptogram = cryptogram;
+        do {
+            cryptogram = newCryptogram;
+            newCryptogram = overlapCheckfromStringOneTime(cryptogram);
+        }while(!cryptogram.equals(newCryptogram) && newCryptogram.length() != 0);
+        cryptogram = newCryptogram;
+        return cryptogram;
+    }
+
+    public static String overlapCheckfromStringOneTime(String cryptogram) {
+        int stringSize = cryptogram.length();
+        boolean overlapCheck = false;
+        int overlapCount = 1;
+        char pastChar = getCharfromStringIndex(cryptogram, stringSize - 1);
+        for (int i = stringSize - 2; i >= 0; i--) {
+            if (pastChar == cryptogram.charAt(i)) {
+                overlapCheck = true;
+                overlapCount++;
+                continue;
+            }
+            if (overlapCheck) {
+                cryptogram = overlapCharsDelete(cryptogram, i + 1, overlapCount);
+                overlapCheck = false;
+                overlapCount = 1;
+            }
+            if (i == cryptogram.length()) {
+                i = cryptogram.length() - 1;
+            }
+            pastChar = cryptogram.charAt(i);
+        }
+        if (overlapCheck) {
+            cryptogram = overlapCharsDelete(cryptogram, cryptogram.length() - overlapCount, overlapCount);
+        }
+        return cryptogram;
+    }
+
+    public static char getCharfromStringIndex(String cryptogram, int index) {
+        return cryptogram.charAt(index);
+    }
+
+    public static String overlapCharsDelete(String cryptogram, int position, int size) {
+        String startSubString = cryptogram.substring(0, position);
+        String endSubString = cryptogram.substring(position + size, cryptogram.length());
+        return startSubString.concat(endSubString);
     }
 }
