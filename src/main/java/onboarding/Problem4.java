f4303c70dc3d8f5c7d7aec4c5b735ec6f90d0798
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-31 18:57:38 +0900

    feat: problem4 청개구리 사전 만들기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    static Map<Character, Character> dictionary = new HashMap<>();
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        dictionary();
+        return "";
+    }
+
+    /**
+     * 청개구리 사전 만들기
+     */
+    public static void dictionary() {
+        for (int i = 97; i <= 122; i++) {
+            dictionary.put((char)i, (char)(219-i));
+        }
     }
 }
