c189cdf9edf2c365f0a83ec2f6dbc19b76b113c8
kssumin <201566@jnu.ac.kr>
2022-10-31 18:09:39 +0900

    fix : 필드에 값 넣어주도록 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,25 +4,29 @@ import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int answer = number;
+        Clap clap = new Clap(answer);
+        return clap.sumClap();
     }
 }
 
 class Clap{
     private int number;
     private int sumClap;
+    private Number numberObject;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 10000;
     private static final int START_NUMBER=1;
     private static final String NOT_ALLOWED_UNDER_MIN_NUMBER = "number은 최소 1 입니다.";
     private static final String NOT_ALLOWED_OVER_MAX_NUMBER = "number은 최대 1000 입니다.";
 
-    public Clap(){
-        validateNumberRange();
+    public Clap(int number){
+        validateNumberRange(number);
+        this.number=number;
+        numberObject=new Number();
     }
 
-    private void validateNumberRange() {
+    private void validateNumberRange(int number) {
         if (number < MIN_NUMBER) {
             throw new IllegalStateException(NOT_ALLOWED_UNDER_MIN_NUMBER);
         }
@@ -30,11 +34,19 @@ class Clap{
             throw new IllegalStateException(NOT_ALLOWED_OVER_MAX_NUMBER);
         }
     }
+
+    public int sumClap(){
+        for(int i=START_NUMBER;i<number;i++){
+            sumClap+=numberObject.countClap(i);
+        }
+        return sumClap;
+    }
 }
 
 class Number{
     private static final List<Integer> CLAP_NUMBER=List.of(3,6,9);
     private int clapCount;
+
     public int countClap(int specificNunber){
         while(specificNunber!=0){
             int remainder=specificNunber%10;
