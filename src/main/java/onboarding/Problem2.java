8aaea9713115cdb592c73512f93b83983c50ba79
LeeJejune <leejejune0625@gmail.com>
2022-10-31 14:38:35 +0900

    feat: 중복된 문자 없애는 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,5 +4,21 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    private static String deleteDuplicateWord(String cryptogram) {
+        while (true) {
+            int duplicateCharacterIndex = checkDuplicateIndex(cryptogram);
+            if (duplicateCharacterIndex == IS_NOT_DUPLICATE)
+                return cryptogram;
+
+            int index = duplicateCharacterIndex;
+            for (; index < cryptogram.length(); index++) {
+                if (cryptogram.charAt(duplicateCharacterIndex) == cryptogram.charAt(index)) {
+                    continue;
+                }
+                break;
+            }
+            cryptogram = cryptogram.substring(0, duplicateCharacterIndex) + cryptogram.substring(index);
+        }
+    }
     }
 }
