dd7cccaff5e87775dfcf28e13ce470a7a995c9d4
OhHongGi <5ghdrl@naver.com>
2022-11-01 00:44:38 +0900

    chore: Divide Function List in Problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        // pobi win = 1, crong win = 2, draw = 0, exception = -1
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    /*
+     주어진 숫자의 각 자리수의 더한 값과 곱한 값 중 큰 값을 반환
+     */
+    public static int calBigOfMulAndAdd(Integer number) {
+        return number;
+    }
+
+    /*
+     주어진 사람이 펼친 두 페이지 번호의 숫자에 대한 결과를 비교하여 반환
+     */
+    public static int calSelfScore(List<Integer> person) {
+        return 0;
+    }
+
 }
