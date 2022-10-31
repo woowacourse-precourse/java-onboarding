04fa4e2d84d9059daa38270965bbc5f2ae8b940d
LeeJinHo <hosboy93@naver.com>
2022-10-31 10:24:21 +0900

    문제4. 대문자 변환(65~90)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -13,7 +13,13 @@ public class Problem4 {
     
     public static char frog(char spell) {
     	int ascii = (int)spell;
-    	System.out.println(ascii);
+    	System.out.println("변환 전 : " + ascii);
+    	
+    	if(ascii>=65&&ascii<=90) {
+    		ascii = 155-ascii;
+    	}
+    	System.out.println("변환 후 : " + ascii);
+    	spell = (char)ascii;
     	
     	return spell;
     }
