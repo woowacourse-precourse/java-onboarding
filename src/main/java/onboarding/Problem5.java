aad6d57d3e7b036a7ce8cbdd07e2ff9480476f7f
Leehyoungwoo <dlguddn3024@gmail.com>
2022-11-01 02:59:16 +0900

    feature: 5번문제 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-
         int[] moneyPaper = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> answer = moneyCount(money, moneyPaper);
         return answer;
