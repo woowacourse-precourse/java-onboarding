5d8f9e1c700e189b013eb9abb27e9bc4e86311b6
Torres-09 <dla612@naver.com>
2022-10-31 18:31:14 +0900

    feat : 문제 2 기능 목록 및 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,40 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeReduplicate(String str) {
+        String result = "";
+
+        if (str.charAt(0) != str.charAt(1))
+            result += str.charAt(0);
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) != str.charAt(i - 1) && str.charAt(i) != str.charAt(i + 1))
+                result += str.charAt(i);
+        }
+
+        if (str.length() > 2 && str.charAt(str.length() - 1) != str.charAt(str.length() - 2))
+            result += str.charAt(str.length() - 1);
+
+        return result;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        int length = cryptogram.length();
+        String answer = "";
+
+        while (true) {
+            if(cryptogram.length()==0)
+                break;
+
+            answer = removeReduplicate(cryptogram);
+
+            if (answer.length() == cryptogram.length()) {
+                break;
+            } else {
+                cryptogram = answer;
+            }
+        }
+
         return answer;
     }
 }
