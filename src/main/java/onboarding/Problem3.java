9074e3d8dd647f812f69768d91da0401a61283a8
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:51:45 +0900

    [problem3]자리수로 나눠 문자의 형태로 저장 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        int count = 0;
+
+        String[] roomsize = new String[number];
+        char[][] piece = new char[number][];
+
+
+        for (int i = 0; i < number; i++) {
+            roomsize[i]=Integer.toString(i+1);
+            piece[i] = new char[roomsize[i].length()];
+
+            for (int j = 0; j < roomsize[i].length(); j++) {
+                piece[i][j] = roomsize[i].charAt(j);
+            }
+        }
+
+
+
     }
+
+
+
+
 }
