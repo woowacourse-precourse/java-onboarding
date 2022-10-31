6c578a48463003b702ee875094c7d5750b712d1f
semi-cloud <sksmsrkdtpal@naver.com>
2022-10-31 10:58:13 +0900

    style: 불필요한 임포트 제거 (#5)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 public class Problem5 {
