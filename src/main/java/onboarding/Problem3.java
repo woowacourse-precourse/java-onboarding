eb1cda85796f52a1e5943f01ae5e7a3bf8f0be0f
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 21:48:55 +0900

    [Problem 3] Complete main stream

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,16 @@ public class Problem3 {
         2. 각 차례의 박수 횟수를 구함
         3. 누적
         */
+        for ( int i = 1 ; i <= number; i ++ ){
+            int clap = 0;
+            int current = i;
+            while ( current != 0 ){
+                int num = current % 10;
+                if ( num == 3 || num == 6 || num == 9 ) clap ++;
+                current /= 10;
+            }
+            total += clap;
+        }
         return total;
     }
 }
