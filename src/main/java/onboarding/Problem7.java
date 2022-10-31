d907e53ce903ed62b66b3f239b2d54d91201fde8
wwwndus <simock22@gmail.com>
2022-10-31 19:56:52 +0900

    feat:for문을 진행하며 유저가 포함되어 있는 리스트에서 친구를 리스트에 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> friendsOfUser = new ArrayList<>();
+
+        findFriendsOfUser(user, friends, friendsOfUser);
+
         return answer;
+    }
+
+    private static void findFriendsOfUser(String user, List<List<String>> friends, List<String> friendsOfUser) {
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).contains(user)) {
+                for (String friend : friends.get(i)) {
+                    if (!friend.equals(user)) {
+                        friendsOfUser.add(friend);
+                    }
+                }
+            }
+        }
     }
 }
