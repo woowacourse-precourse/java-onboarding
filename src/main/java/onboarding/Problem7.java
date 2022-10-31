2d147711b7ee1b5937a4710596d7273c50f3d619
hyeongu <wancern@naver.com>
2022-10-31 23:43:04 +0900

    Problem7. 요구사항 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,7 +2,13 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+/*
+ * 1. 친구관계를 저장하는 해시맵과 점수를 저장하는 트리맵 선언
+ * 2. 양방향 친구관계를 해시맵에 저장
+ * 3. user의 친구와 친구관계인 경우 점수 추가
+ * 4. visiters 배열 점수 추가
+ * 5. 트리맵을 정렬하여 정답 출력
+ * */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
