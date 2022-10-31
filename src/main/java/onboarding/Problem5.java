9db3671f912f578293b7522472aa2fea27fe6fed
KimSunU <kgw6147@naver.com>
2022-11-01 00:23:21 +0900

    Problem5 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,11 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         int moneyTmp = money;
         int []moneyCan = {50000, 10000, 5000, 1000, 500, 100, 50, 10,1};
+        /**
+         * 그리디 방법을 이용하여
+         * 5만원 부터 1원까지 순차적으로
+         * 큰돈이 필요로 하면 큰돈을 사용하는 방식으로 구현함
+         */
         for(int i=0; i<9; i++){
             int cnt =0;
             cnt = moneyTmp / moneyCan[i];
