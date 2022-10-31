ffbca5586a30fc2d4e8523b373341b0f921712bd
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-10-31 21:39:53 +0900

    Refactor(Problem3): 변경 클래스와 메소드로 나누기 변경 박수치는 조건 판별 메소드 extract

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,13 +1,15 @@
 package onboarding;
 
-/*
-기능 목록
-1. 1 ~ 10000 까지 수 증가하면서 박수 횟수 새기
- */
-public class Problem3 {
-    public static int solution(int number) {
+class Game369 {
+    private int number;
+
+    public Game369(int number) {
+        this.number = number;
+    }
+
+    public static int clapCountOf(int number) {
         int clapCnt = 0;
-        for(int i = 1; i <= number; i++){
+        for (int i = 1; i <= number; i++) {
             clapCnt += addClapCnt(i);
         }
         return clapCnt;
@@ -18,9 +20,26 @@ public class Problem3 {
         String[] split = String.valueOf(num).split("");
         for (String s : split) {
             int now = Integer.parseInt(s);
-            if(now == 3 || now == 6 || now == 9)
+            if (isClapCondition(now))
                 clapCnt++;
         }
         return clapCnt;
+    }
+
+    private static boolean isClapCondition(int now) {
+        if(now == 3 || now == 6 || now == 9)
+            return true;
+        return false;
+    }
+}
+
+/*
+기능 목록
+1. 1 ~ 10000 까지 수 증가하면서 박수 횟수 새기
+ */
+public class Problem3 {
+    public static int solution(int number) {
+        return Game369
+                .clapCountOf(number);
     }
 }
