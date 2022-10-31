6ee6b15242ad3b54ab95e789ee64fbd5afd9eadb
hyeonseog-won <smreo75@gmail.com>
2022-10-31 15:25:08 +0900

    Problem6 -닉네임 리스트에 저장 -중복되는 단어가 들어가는 닉네임 찾기 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,11 +7,13 @@ import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        HashSet<String> nickname = new HashSet<>();
         ArrayList<String> word = new ArrayList<>();
         Set<String> duplicateWord = new HashSet<>();
+        HashSet<String> duplicateNickname = new HashSet<>();
 
         for (List<String> form : forms) {
+            nickname.add(form.get(1));
             for (int i = 1; i < form.get(1).length(); i++) {
                 word.add(form.get(1).substring(i - 1, i + 1));
             }
@@ -20,6 +22,14 @@ public class Problem6 {
         for (String str : word) {
             if (word.indexOf(str) != word.lastIndexOf(str)) {
                 duplicateWord.add(str);
+            }
+        }
+
+        for (String str : duplicateWord) {
+            for (String name : nickname) {
+                if (name.contains(str)) {
+                    duplicateNickname.add(name);
+                }
             }
         }
 
