6746c12b7b83586289749bd87bc6dca9bce95f43
ddingmin <ddingminyt@gmail.com>
2022-11-01 03:19:18 +0900

    fix(Problem3): if문 캡슐화 및 박수횟수 세는 반복 부분 메소드화

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,11 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        int answer = countAllClaps(number);
+        return answer;
+    }
+
+    private static int countAllClaps(int number){
         int answer = 0;
         for (int i = 1; i <= number; i++) {
             answer += countClap(i);
@@ -13,10 +18,14 @@ public class Problem3 {
         String temp = Integer.toString(n);
         int count = 0;
         for (int i = 0; i < temp.length(); i++){
-            if ((temp.charAt(i) == '3') || (temp.charAt(i) == '6') ||(temp.charAt(i) == '9')){
+            if (isClap(temp.charAt(i))){
                 count += 1;
             }
         }
         return count;
+    }
+
+    private static Boolean isClap(char c){
+        return (c == '3') || (c == '6') ||(c == '9');
     }
 }
