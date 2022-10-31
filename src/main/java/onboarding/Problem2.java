8005c438d7d2798592416c73153ef8389ab28e52
squirMM <jsm6616@naver.com>
2022-10-31 17:04:44 +0900

    :rocket: feat(Problem2) refactoring name

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,19 +6,19 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         while(cryptogram.length()>0){
             char prev = cryptogram.charAt(0);
-            int lenPrev=1;
+            int lenSamePrev=1;
             String newCrypt=cryptogram;
 
             for(int i=1 ; i<cryptogram.length();i++){
                 char c = cryptogram.charAt(i);
-                if(Objects.equals(c,prev)) lenPrev+=1;
-                else if (lenPrev>1) {
-                        newCrypt=newCrypt.replaceAll(repeatStr(prev,lenPrev), "");
+                if(Objects.equals(c,prev)) lenSamePrev+=1;
+                else if (lenSamePrev>1) {
+                        newCrypt=newCrypt.replaceAll(repeatStr(prev,lenSamePrev), "");
                         break;
                 }
                 prev=c;
             }
-            if (Objects.equals(repeatStr(prev,lenPrev),newCrypt)) newCrypt="";
+            if (Objects.equals(repeatStr(prev,lenSamePrev),newCrypt)) newCrypt="";
             if (Objects.equals(newCrypt,cryptogram)) break;
             cryptogram=newCrypt;
         }
