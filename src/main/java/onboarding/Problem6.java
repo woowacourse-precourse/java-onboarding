8b43c1ab284a6f02d8bdd13e389aebb86e19e23a
dsaf2007 <dsaf2007@naver.com>
2022-11-01 01:39:54 +0900

    Problem 6 solved

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,41 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Collections;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();//List.of("answer");
+        List<String> name = new ArrayList<String>();
+        answer.clear();
+        for(List<String> form : forms)
+        {
+            name.clear();
+            for(int i = 0; i < form.get(1).length() - 1; i++)
+            {
+                name.add(form.get(1).substring(i,i+2));
+            }
+            for(List<String> form2 : forms)
+            {
+                for(String name_ : name)
+                {
+                    if(form != form2)
+                        if(form2.get(1).contains(name_)==true)
+                        {
+                            answer.add(form2.get(0));
+                            break;
+                        }
+                }
+            }
+        }
+        Set<String> set = new HashSet<String>(answer);
+        List<String> newList = new ArrayList<String>(set);
+        answer = newList;
+        Collections.sort(answer);
         return answer;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,12 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> resultList = new ArrayList<String>();
         return answer;
     }
 }
