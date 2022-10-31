356ebdd23c9629e8af7b84d491c3c37a759a231f
yubin <dbqls2821@gmail.com>
2022-11-01 00:42:23 +0900

    FEAT: [Problem6] init 함수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    private static HashMap<String, String> map;
+    private static HashMap<String, Integer> duplicationCountMap;
+    private static List<String> output;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        init();
+        return output;
     }
+
+    private static void init() {
+        map = new HashMap<>();
+        duplicationCountMap = new HashMap<>();
+        output = new ArrayList<>();
+    }
+
 }
