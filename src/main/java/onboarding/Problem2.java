af56e23050c1bc3062af839028c9fd3bd30d828d
3juhwan <13selfesteem91@naver.com>
2022-10-31 17:32:39 +0900

    problem2: update delSameChar function

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Problem2 {
+    public static String delSameChar(String str) {
+        String result = new String();
+        for(int i=0, j=i+1; i<str.length(); i++) {
+            while(j < str.length() && str.charAt(i)==str.charAt(j)) j++;
+            if(i+1==j) result += str.substring(i, j);
+            else i = j-1;
+        }
+        return result;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return cryptogram;
     }
 }
