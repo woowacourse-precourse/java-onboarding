f0e9aa2fbe36d13b77d3b229b5c447573b4b29fa
Judy <judyb0626@gmail.com>
2022-11-01 01:55:49 +0900

    feat(problem 1): 페이지 시작 면, 마지막 면 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,4 +11,11 @@ class Problem1 {
     private static boolean checkPage(List<Integer> page) {
         return page.get(1) - page.get(0) == 1;
     }
+
+    private static boolean validation(List<Integer> page) {
+        if (page.get(0) == 1 || page.get(1) == 400) {
+            return false;
+        }
+        return true;
+    }
 }
