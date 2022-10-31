b07ded77eb40c8b0b3857636dff1ae6c9abb3ac0
akskflwn <akskflwn@gmail.com>
2022-10-31 19:50:44 +0900

    (refactor) Problem6 스트림 추가,리펙토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,6 +8,7 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Map<String, List<String>> duplicatedEmails = getDuplicatedEmails(new HashMap<>(), forms);
         List<String> answer = new ArrayList<>();
+
         duplicatedEmails.values().stream().filter(s -> s.size() > 1).forEach(answer::addAll);
         return answer.stream().sorted().collect(Collectors.toList());
     }
@@ -17,14 +18,11 @@ public class Problem6 {
             String arr[] = form.get(1).split("");
             for (int i = 1; i < arr.length; i++) {
                 String nickName = arr[i - 1] + arr[i];
-
                 if (!duplicatedEmails.containsKey(nickName)) {
                     duplicatedEmails.put(nickName, new ArrayList<>());
                 }
                 duplicatedEmails.get(nickName).add(form.get(0));
-
             }
-
         }
         return duplicatedEmails;
     }
