43ac123d6707ac831e56b2d743f46aa33a7fde1b
Aru428 <86353626+Aru428@users.noreply.github.com>
2022-10-31 22:59:12 +0900

    ë문제6 2글자씩 닉네임 체크

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,18 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        int size = forms.size();
+        int[] check =  new int[size];
+
+        for(int i=0;i<forms.size();i++){
+            String name = forms.get(i).get(1);
+            for(int j=0;j<name.length()-1;i++){
+                String duplication = name.substring(j,j+2);
+
+
+            }
+        }
+
         return answer;
     }
 }
