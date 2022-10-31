115ee26181b61a88590e0e65a9aad154fce85aff
plusjob70 <plusjob70@gmail.com>
2022-10-31 13:24:30 +0900

    feat: 청개구리 사전 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    static Map<Character, Character> frogDict;
+
+    static {
+        frogDict = new HashMap<>();
+
+        char from = 'a';
+        char to = 'z';
+        for (int i = 0; i < 26; i++, from++, to--) {
+            frogDict.put(from, to);
+            frogDict.put(Character.toUpperCase(from), Character.toUpperCase(to));
+        }
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return "";
     }
 }
