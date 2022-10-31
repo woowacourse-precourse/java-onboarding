f88a9d1b24e477783a90ac66a80e014f5b75daab
jihoon-gh <jihoon806@gmail.com>
2022-11-01 03:17:19 +0900

    FEAT Problem5.java  - solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,15 @@
 package onboarding;
 
+/*
+기능 구현 목록
+1. 숫자의 분리
+2. 문자열로 변환 후 각 글자마다 분리 (split() 이용)
+3. 각 글자마다 3, 6, 9 인지 확인 후 맞으면 answer++
+ */
+
+
+
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
-
+/*
+알파벳 변환
+1. 조건 확인 - > 알파벳만 변환. 즉 문자 c의 범위는 65 <= c <= 90 || 97 <= c <= 112
+2. A+Z - Z  = A임을 기억하며 문자열 순회. 문자 변경
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] ch = word.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            int a =divideCase(ch[i]);
+            if(a==1) ch[i] = (char)(209 - (int)ch[i]);
+            if(a==2) ch[i] = (char)(155 - (int)ch[i]);
+        }
+        answer=ch.toString();
         return answer;
     }
+
+    public static Integer divideCase(char c){
+        if(c>=97 && c<=112) return 1; //소문자
+        if(c>=65 && c<=90) return 2;
+        return 0;
+    }
+
 }
