548af3a357eeba55f95285c69d8a7ab94e1c89bd
yubin <dbqls2821@gmail.com>
2022-10-31 21:50:08 +0900

    FEAT: [Problem1] 각 자리수를 더한 총합을 계산하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,4 +12,13 @@ class Problem1 {
         return pobi.get(0) + 1 == pobi.get(1) && crong.get(0) + 1 == crong.get(1);
     }
 
+    private static int addEachDigit(int pageNum) {
+        int answer = 0;
+        String s = Integer.toString(pageNum);
+
+        for (int i = 0; i < s.length(); i++) {
+            answer += Integer.parseInt(s.substring(i, i + 1));
+        }
+        return answer;
+    }
 }
