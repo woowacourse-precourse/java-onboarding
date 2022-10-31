6740f85666bb7741208f368da25412e8d795f936
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-01 03:45:57 +0900

    [PROBLEM 3] 369 카운트 메소드
    문자열로 변환 후, 3 6 9 숫자 체크하여 횟수 계산

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    static int countThreeSixNine(int num) {
+        String strNum = String.valueOf(num);
+
+        int cnt = 0;
+        for(int i=0; i<strNum.length(); i++) {
+            Character charNum = strNum.charAt(i);
+            if(charNum == '3' || charNum == '6' || charNum == '9' ) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
 }
