f6d46d9a084c5f1f0fa319232130ee1667ebe71d
HyebinShin <hyeviin.dev@gmail.com>
2022-10-31 20:02:58 +0900

    점수 계산 메소드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        HashMap<String, Integer> scores = new HashMap<>();
+        HashSet<String> alreadyFriend = new HashSet<>();
+
+        //친구 점수
+        for(List<String> friend : friends) {
+            alreadyFriend.add(friend.get(0));
+            if(!friend.get(1).contains(user)) {
+                System.out.println("friend: "+ Arrays.toString(friend.toArray()));
+
+
+            }
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void getScoreValue(HashMap<String, Integer> scores, String id, int score) {
+        //점수 계산하기
+        if(scores.containsKey(id)) {
+            scores.replace(id, scores.get(id)+score);
+        } else {
+            scores.put(id, score);
+        }
     }
 }
