a0b54b0c30f157ea54cacd8c98a86679246e803e
YooSuYeon <usuyn000@gmail.com>
2022-11-01 03:38:02 +0900

    feat(Problem6): 동일 글자 포함 닉네임 작성 지원자 이메일 정보 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+    private static final int EMAIL = 0;
+    private static final int NICKNAME = 1;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        Map<String, List<String>> nicknamePieces = new HashMap<>();
+
+        for (List<String> crewInfo : forms) {
+            String nickname = crewInfo.get(NICKNAME);
+            String email = crewInfo.get(EMAIL);
+
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String piece = nickname.substring(i, i + 2);
+                List<String> emailList = nicknamePieces.getOrDefault(piece, new ArrayList<>());
+
+                if (emailList.contains(email)) {
+                    continue;
+                }
+                emailList.add(email);
+                nicknamePieces.put(piece, emailList);
+            }
+
+        }
+
         return answer;
     }
 }
