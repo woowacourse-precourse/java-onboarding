40353c028f462755e8b3e391d54ee0560a08464c
cpot5620 <=cpot5620@naver.com>
2022-10-31 23:01:51 +0900

    Make UserFriends class
    It is responsible for to store user's friends.

--- src/main/java/onboarding/UserFriends.java
+++ src/main/java/onboarding/UserFriends.java
@@ -1,2 +1,27 @@
-package onboarding;public class UserFriends {
+package onboarding;
+
+import java.util.*;
+
+public class UserFriends {
+    private Set<User> userFriends = new HashSet<>();
+
+    public UserFriends(String name){
+        userFriends.add(new User(name));
+    }
+
+    public void addUserFriend(String user, List<String> friends) {
+        int index = friends.indexOf(user);
+
+        if (index >= 0) {
+            index = (index + 1) % 2;
+            String friendName = friends.get(index);
+
+            userFriends.add(new User(friendName));
+        }
+    }
+
+    public boolean isUserFriend(User friend){
+        return userFriends.contains(friend);
+    }
+    
 }
