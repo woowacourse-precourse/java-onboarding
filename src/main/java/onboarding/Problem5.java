0f7e36ed1d3ba16f4772afe8a078a9c6e2b80614
Songyi Kim <ksl2950@naver.com>
2022-10-31 09:48:00 +0900

    feat/5: 동전 변환 결과 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> unitList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        int index = 0;
+        for (int unit : unitList) {
+            int quotient = money / unit;
+            answer.add(index, quotient);
+            money -= unit * (quotient);
+            index += 1;
+        }
+
         return answer;
     }
+
 }
