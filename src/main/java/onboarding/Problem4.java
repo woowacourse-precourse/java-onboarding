f49e491807b778453640492af54011d4af9c5895
SeolYoungKim <nasur4da@gmail.com>
2022-10-31 15:25:47 +0900

    refactor(P4): input value에 대한 validation은 soultion에서 해야한다고 판단하여 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,9 +3,44 @@ package onboarding;
 import onboarding.problem4.CharacterConverter;
 import onboarding.problem4.WordConverter;
 
+import static java.lang.Character.isAlphabetic;
+import static java.lang.Character.isSpaceChar;
+
 public class Problem4 {
     public static String solution(String word) {
+        validateWord(word);
+
         WordConverter wordConverter = new WordConverter(new CharacterConverter());
         return wordConverter.reverseWord(word);
+    }
+
+    private static void validateWord(String word) {
+        if (!isValidLength(word)) {
+            throw new IllegalArgumentException("1글자 이상 1000글자 이하로 입력 해주세요.");
+        }
+
+        if (!containsOnlyAlphabet(word)) {
+            throw new IllegalArgumentException("알파벳만 입력 해주세요.");
+        }
+    }
+
+    private static boolean isValidLength(String word) {  // 길이 체크
+        int length = word.length();
+        final int MIN_VALUE = 1;
+        final int MAX_VALUE = 1_000;
+
+        return MIN_VALUE <= length && length <= MAX_VALUE;
+    }
+
+    private static boolean containsOnlyAlphabet(String word) {  // 알파벳 체크
+        char[] chars = word.toCharArray();
+
+        for (char aChar : chars) {
+            if (!isAlphabetic(aChar) && !isSpaceChar(aChar)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/main/java/onboarding/problem4/WordConverter.java
+++ src/main/java/onboarding/problem4/WordConverter.java
@@ -1,7 +1,5 @@
 package onboarding.problem4;
 
-import static onboarding.problem4.WordValidator.*;
-
 public class WordConverter {
 
     private final CharacterConverter characterConverter;
@@ -11,8 +9,6 @@ public class WordConverter {
     }
 
     public String reverseWord(String word) {
-        validateWord(word);  // 검증
-
         // 반대 단어로 변환 후 반환
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
