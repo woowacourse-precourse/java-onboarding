903b1e53083f1d060462f1aeb36f2a0290d19ccb
juhwang <juhong4930@naver.com>
2022-10-31 23:17:24 +0900

    refactor(Problem5):changing for loop to stream

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,24 +5,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    private static final List<Integer> MONEYLIST = List.of(50000,10000,5000,1000,500,100,50,10,1);
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        answer = MoneyLoop(money);
+        List<Integer> answer = MoneyLoop(money);
+        if (answer.size() == 0) {
+            return Collections.emptyList();
+        }
         return answer;
     }
-    private static int divideMoney(int money, int divMoney){
-        return Math.floorDiv(money,divMoney);
-    }
-    private static int moduloMoney(int money, int divMoney){
-        return Math.floorMod(money,divMoney);
-    }
-    private static List<Integer> MoneyLoop(int money){
+
+    private static List<Integer> MoneyLoop(int money) {
         List<Integer> list = new ArrayList<>();
-        for (Integer divmoney : MONEYLIST) {
-            list.add(divideMoney(money, divmoney));
-            money = moduloMoney(money,divmoney);
-        }
+        List<Integer> moneyList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        moneyList.stream().reduce(money, (total, divMoney) -> {
+            list.add(Math.floorDiv(total, divMoney));
+            return Math.floorMod(total, divMoney);
+        });
         return list;
     }
 }
