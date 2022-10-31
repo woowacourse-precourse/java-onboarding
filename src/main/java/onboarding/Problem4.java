1671e2ecee8f535e3c72421474caf52f91e0ba5d
sunhokim1 <srrsr14@gmail.com>
2022-10-31 12:04:58 +0900

    commit

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = translation(word);
         return answer;
+    }
+
+    public static String translation(String str) {
+        int lenstr = str.length();
+        String temp = "";
+        for (int i=0;i<lenstr;++i)
+            temp = temp + (char)wordBox(str.charAt(i));
+        return temp;
+    }
+
+    public static int wordBox(char c) {
+        String lowerstr = "abcdefghijklmnopqrstuvwxyz";
+        String upperstr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i=0;i<26;++i) {
+            if (c == lowerstr.charAt(i))
+                return 'z' - i;
+            else if (c == upperstr.charAt(i))
+                return 'Z' - i;
+        }
+        return c;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> moneyList = new ArrayList<>();
+        moneyList.add(money / 50000);
+        money = money - (50000 * (money / 50000));
+        moneyList.add(money / 10000);
+        money = money - (10000 * (money / 10000));
+        moneyList.add(money / 5000);
+        money = money - (5000 * (money / 5000));
+        moneyList.add(money / 1000);
+        money = money - (1000 * (money / 1000));
+        moneyList.add(money / 500);
+        money = money - (500 * (money / 500));
+        moneyList.add(money / 100);
+        money = money - (100 * (money / 100));
+        moneyList.add(money / 50);
+        money = money - (50 * (money / 50));
+        moneyList.add(money / 10);
+        money = money - (10 * (money / 10));
+        moneyList.add(money);
+        money = 0;
+        answer = moneyList;
         return answer;
     }
 }
