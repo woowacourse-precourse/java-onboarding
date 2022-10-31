4e96e8511055690155d4098e66c371431aaa14e2
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 00:07:12 +0900

    feat : problem1 예외 체크 구현(홀짝)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,19 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(checkException(pobi) || checkException(crong)){
+            return -1;
+        }
+
+
         return answer;
+    }
+    public static boolean checkException(List<Integer> checkList){
+        Integer leftNum = checkList.get(0)+1;
+        Integer rightNum = checkList.get(1);
+        return checkSniffling(leftNum, rightNum);
+    }
+    public static boolean checkSniffling(Integer num1, Integer num2){
+        return num1 % 2 != 0 || num2 % 2 != 0;
     }
 }
