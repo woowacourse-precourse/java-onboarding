46872bfd0699a028637bef45ff04573d9fc1f788
syahn <496300@naver.com>
2022-10-31 21:30:48 +0900

    feat:user의 friend의 friend 찾아 점수 추가하고 friend list 반환 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -21,6 +21,10 @@ class RecommendScore{
         return score;
     }
 
+    public void plusTen(){
+        score += 10;
+    }
+
 }
 public class Problem7{
     public static List<RecommendScore> AllFriendsList(String user, List<List<String>> friends, List<String> visitors){
@@ -50,11 +54,53 @@ public class Problem7{
         return allfriends;
     }
 
+    //freind의 friend 찾아 list로 만들고 친구 리스트를 반환
+    public static List<String>  friendsKnowTogether(String user, List<List<String>> friends, List<RecommendScore> allfriends){
+        Set<String> userFriendSet = new HashSet<>();
+        List<String> userFFList = new ArrayList<>();
+
+        //user와 친구인 아이디 집합에 저장
+        for(int i = 0; i<friends.size(); i++){
+            if((friends.get(i).get(0)).equals(user)){
+                userFriendSet.add(friends.get(i).get(1));
+            }else if((friends.get(i).get(1)).equals(user)){
+                userFriendSet.add(friends.get(i).get(0));
+            }
+        }
+
+        //집합을 다시 list로 반환(중복 제거 위해)
+        List<String> userFriendList = new ArrayList<>(userFriendSet);
+
+        //user 친구와 친구인 아이디 리스트에 저장
+        for(int i= 0; i<friends.size(); i++){
+            for(int j = 0; j <userFriendList.size(); j++) {
+                if (!(friends.get(i).get(0)).equals(user) | !(friends.get(i).get(1)).equals(user)) {
+                    if (friends.get(i).get(0).equals(userFriendList.get(j))) {
+                        userFFList.add(friends.get(i).get(1));
+                    } else if (friends.get(i).get(1).equals(userFriendList.get(j))) {
+                        userFFList.add(friends.get(i).get(0));
+                    }
+                }
+            }
+        }
+
+
+        for(int i = 0; i<userFFList.size(); i++){
+            for(int j = 0; j<allfriends.size(); j++){
+                if((userFFList.get(i)).equals((allfriends.get(j)).getName())){
+                    (allfriends.get(j)).plusTen();
+                }
+            }
+        }
+        return userFriendList;
+
+    }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         List<RecommendScore> allList = AllFriendsList(user, friends, visitors);
+        List<String> friendList = friendsKnowTogether(user, friends, allList);
 
         return answer;
     }
