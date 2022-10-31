2d30b5538796b1535cdabd390bc5863a0a0e421a
Sangkyun Seo <tkdrbs0802@naver.com>
2022-10-31 19:49:46 +0900

    from p1 to p6 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        
+        Stack<Character> s = new Stack<>();
+        for(int i=cryptogram.length()-1;i>=0;i--) {
+        	if(s.size()==0) {
+        		s.push(cryptogram.charAt(i));
+        	}else {
+        		if(s.peek()!=cryptogram.charAt(i)) {
+            		s.push(cryptogram.charAt(i));
+            	}else {
+            		s.pop();
+            	}
+        	}
+        }
+        
+        if(s.isEmpty())answer="";
+        else {
+        	while(!s.isEmpty()) {
+            	sb.append(s.pop());
+            }
+            answer = sb.toString();
+        }
+         return answer;
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        
+        for(int i=1;i<=number;i++) {
+        	int a = i;
+        	while(a!=0) {
+        		if(a%10==3||a%10==6||a%10==9) {
+        			answer+=1;
+        		}
+        		a=a/10;
+        	}
+        }
         return answer;
     }
 }
