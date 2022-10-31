03b3dab9ac66802831a39919f744ff37ad1225a4
ttaehee <ttaehee1105@gmail.com>
2022-10-31 20:41:08 +0900

    문제1) feat : 곱하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,12 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
         int pobiSum = add(pobi);
+        int pobiMulti = multiply(crong);
+        int pobiMax = max(pobiSum, pobiMulti);
 
         int crongSum = add(crong);
+        int crongMulti = multiply(crong);
+        int crongMax = max(crongSum, crongMulti);
 
         int answer = Integer.MAX_VALUE;
         return answer;
@@ -36,6 +40,24 @@ class Problem1 {
             return first;
         }
         return second;
+    }
+
+    private static int multiply(List<Integer> values) {
+        int leftPageMulti = getOnePageMulti(values.get(0));
+        int rightPageMulti = getOnePageMulti(values.get(1));
+
+        return max(leftPageMulti, rightPageMulti);
+    }
+
+    private static int getOnePageMulti(int pageNumber) {
+        int onePageMulti = 1;
+        String[] stringArray = String.valueOf(pageNumber).split("");
+
+        for(String number : stringArray) {
+            onePageMulti *= Integer.parseInt(number);
+        }
+
+        return onePageMulti;
     }
 
 }
