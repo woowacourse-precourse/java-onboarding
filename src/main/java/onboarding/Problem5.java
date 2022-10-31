294c7718adb82949a3286a38d5f92e95b8919e07
footdev <ekclstkfka5@gmail.com>
2022-10-31 21:07:40 +0900

    feat: 기능5 메소드 명세 및 solution 메소드 작성
    기능 구현 목록을 토대로 명세 작성 및 solution 메소드 내부 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = exchangeMoney(money);
         return answer;
+    }
+
+    public static List<Integer> exchangeMoney(int money) {
+        return Collections.emptyList();
     }
 }
