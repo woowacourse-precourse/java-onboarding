1eb1aef7f6f18f737d5135bbc937fb734a781e08
Torres-09 <dla612@naver.com>
2022-11-01 03:06:02 +0900

    feat : 문제 6 기능 목록 및 기능 추가
    문자열에서 연속될 수 있는 최소 문자열 길이인 2에 해당하는 모든 문자열을 hashmap으로 저장
    기존에 있었던 문자열이라면 중복체크

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,43 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static boolean[] already;
+    public static HashMap<String, Integer> hashMap;
+
+    public static void init(int size) {
+        already = new boolean[size];
+        hashMap = new HashMap<>();
+    }
+
+    public static void check(String str, int order) {
+        for (int i = 0; i < str.length()-1; i++) {
+            if (!hashMap.containsKey(str.substring(i, i + 2))) {
+                hashMap.put(str.substring(i, i + 2), order);
+            } else {
+                already[order] = true;
+                already[hashMap.get(str.substring(i, i + 2))] = true;
+            }
+        }
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        init(forms.size());
+        int i = 0;
+        for (List<String> form : forms) {
+            check(form.get(1), i);
+            i++;
+        }
+
+        for (int j = 0; j < forms.size(); j++) {
+            if(already[j] == true)
+                answer.add(forms.get(j).get(0));
+        }
+
+        Collections.sort(answer);
         return answer;
     }
 }
