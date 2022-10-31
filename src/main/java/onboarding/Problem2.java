f844ce0d8e52596a6863d79cf544494bc0158c1e
lhyeonj120 <isk04387@gmail.com>
2022-11-01 03:25:41 +0900

    [problem2] feat: 중복 문자 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,28 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    
+    private static String removeOverlap(String s) {
+    	char c = s.charAt(0);
+    	int n = 1;
+    	
+    	for(int i = 1; i < s.length(); i++) {
+    		if(c != s.charAt(i)) {
+    			if(n > 1) {
+    				s = s.replace(s.substring(i - n, i), "");
+    				i -= n;
+    				n = 1;
+    			}
+    			c = s.charAt(i);
+    		} else {
+    			n++;
+    		}
+    	}
+    	
+    	if(n > 1) {
+    		s = s.replace(s.substring(s.length() - n, s.length()), "");
+    	}
+    	
+    	return s;
+    }
 }
