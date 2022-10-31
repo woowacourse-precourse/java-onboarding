26598a9a7107f3c9c690c76f1d0f4a4b97e81971
baexxbin <soo3886@gmail.com>
2022-10-31 22:14:21 +0900

    Feat(PROBLEM7): "친구관계 그래프 생성함수 작성" 친구목록의 용이한 접근을 위한 친구관계 그래프 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	static HashMap<String, List<String>> friendList = new HashMap<>();
+
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> answer = Collections.emptyList();
+
+		String mate1;
+		String mate2;
+		for (List<String> mate : friends) {
+			mate1 = mate.get(0);
+			mate2 = mate.get(1);
+			makeGraph(mate1, mate2);
+		}
+		return answer;
+	}
+
+	public static void makeGraph(String mate1, String mate2) {
+		String firstMate;
+		String secondMate;
+		for (int i = 0; i < 2; i++) {
+			firstMate = i == 0 ? mate1 : mate2;
+			secondMate = i == 0 ? mate2 : mate1;
+			if (!friendList.containsKey(firstMate)) {
+				friendList.put(firstMate, new ArrayList<>());
+				continue;
+			}
+			friendList.get(firstMate).add(secondMate);
+		}
+	}
 }
