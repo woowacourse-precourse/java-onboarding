2d201541c6e9321ec419a199c12fd5af3036529e
judyi19997 <judyi1999@kookmin.ac.kr>
2022-10-31 20:22:33 +0900

    add/ 문제 1) 자릿수별로 나누는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
         return answer;
+    }
+
+    // 1-자릿수 별로 나누는 기능
+    public static List<Integer> slice(int page){
+        List<Integer> sliceList = new ArrayList<>();
+        while(page>0){
+            sliceList.add(page%10);
+            page = page/10;
+        }
+        return sliceList;
     }
 }
