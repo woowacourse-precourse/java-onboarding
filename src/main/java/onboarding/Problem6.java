78a2e2a1f9b18de4fd3e8a2b10d797c0be9bb265
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-10-31 13:58:01 +0900

    [ADD] 문제 6 - 이메일과 닉네임을 각자 다른 리스트에 할당

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        List<String> emails = new ArrayList<>();
+        List<String> nicknames = new ArrayList<>();
+
+        for (List<String> form: forms) {
+            emails.add(form.get(0));
+            nicknames.add(form.get(1));
+        }
+
         List<String> answer = List.of("answer");
         return answer;
     }
