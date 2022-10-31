eba69f3646d02a9ee124d239876235452b061ae4
inhyeok <ss112d86@gmail.com>
2022-11-01 08:19:23 +0900

    feat: Problem7 추천 친구의 아이디를 구하는 메소드 구현 최대 5명, 우선순위로 정렬, 이후 같으면 이름 순서로 정렬

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,22 +1,47 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class Problem7 {
 
   private static HashMap<String, Set<String>> friendsShip = new HashMap<>();
   private static HashMap<String, Integer> recommendList = new HashMap<>();
+
   public static List<String> solution(String user, List<List<String>> friends,
       List<String> visitors) {
     setFriendsShip(friends);
     recommendByFriends(user);
     recommendByVisitor(user, visitors);
-    System.out.println(recommendList);
-    return new ArrayList<>();
+    List<String> answer = getRecommendUsers();
+    return answer;
+  }
+
+  private static List<String> getRecommendUsers() {
+    List<String> answer = new ArrayList<>();
+    List<Entry<String, Integer>> recommendEntries = new LinkedList<>(recommendList.entrySet());
+    recommendEntries.sort(new Comparator<Entry<String, Integer>>() {
+      @Override
+      public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+        if (o2.getValue().equals(o1.getValue())) {
+          return o1.getKey().compareTo(o2.getKey());
+        }
+        return o2.getValue() - o1.getValue();
+      }
+    });
+    for (Entry<String, Integer> friend : recommendEntries) {
+      if (answer.size() == 5) {
+        break;
+      }
+      answer.add(friend.getKey());
+    }
+    return answer;
   }
 
   private static void setFriendsShip(List<List<String>> friends) {
@@ -33,14 +58,14 @@ public class Problem7 {
     }
   }
 
-  private static void recommendByFriends(String user){
+  private static void recommendByFriends(String user) {
     Set<String> myFriends = friendsShip.get(user);
-    for(String myFriend : myFriends){
+    for (String myFriend : myFriends) {
 
       Set<String> friends = friendsShip.get(myFriend);
 
-      for(String friend : friends){
-        if(!friend.equals(user)) {
+      for (String friend : friends) {
+        if (!friend.equals(user)) {
           Integer priority = recommendList.getOrDefault(friend, 0);
           priority += 10;
           recommendList.put(friend, priority);
@@ -49,13 +74,13 @@ public class Problem7 {
     }
   }
 
-  private static void recommendByVisitor(String user, List<String> visitors){
+  private static void recommendByVisitor(String user, List<String> visitors) {
     Set<String> myFriends = friendsShip.get(user);
-    for(String visitor : visitors){
-      if(myFriends.contains(visitor)){
+    for (String visitor : visitors) {
+      if (myFriends.contains(visitor)) {
         continue;
       }
-      Integer priority =  recommendList.getOrDefault(visitor, 0);
+      Integer priority = recommendList.getOrDefault(visitor, 0);
       priority += 1;
       recommendList.put(visitor, priority);
     }
