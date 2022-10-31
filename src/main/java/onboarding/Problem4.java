7f0fb1c2d957f9341bc303278b215c078b7e13c1
minju412 <ln8847@ajou.ac.kr>
2022-10-31 13:41:03 +0900

    refactor: 반복문 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,19 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         char[] charArr = new char[word.length()];
-        int tmp=0;
+        int tmp = 0; // 아스키코드 표 기준 앞에서부터 몇 번째 캐릭터인지
+        char ch = ' ';
 
-        for (int i=0; i<word.length(); i++) {
-            charArr[i] = word.charAt(i);
-        }
-
-        for (int i=0; i<word.length(); i++) {
-            if (charArr[i] > 'a' && charArr[i] < 'z') {
-                tmp = charArr[i] - 96; // 앞에서부터 몇 번째 캐릭터인지 -> tmp
+        for (int i = 0; i < word.length(); i++) {
+            ch = word.charAt(i);
+            if (ch > 'a' && ch < 'z') {
+                tmp = ch - 96;
                 charArr[i] = (char)(123 - tmp); // 뒤에서부터 tmp 번째 캐릭터를 대입
-            } else if (charArr[i] > 'A' && charArr[i] < 'Z') {
-                tmp = charArr[i] - 64;
+            } else if (ch > 'A' && ch < 'Z') {
+                tmp = ch - 64;
                 charArr[i] = (char)(91 - tmp);
+            } else {
+                charArr[i] = ch;
             }
         }
 
