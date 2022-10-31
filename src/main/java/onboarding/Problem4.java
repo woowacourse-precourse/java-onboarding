92875376095d8ec28e13cc0c45436ce589bcf0de
Toa <ld980105@snu.ac.kr>
2022-11-01 01:08:02 +0900

    Feat : add convertCharcter method
    To convert character as decribed in problem 4, add convertCharacter method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return null;
+    }
+    public static char convertCharcter(Character ch){
+        int sumUpper = 'A' + 'Z';
+        int sumLower = 'a' + 'z';
+        if (Character.isUpperCase(ch)){
+            return (char)(sumUpper - Integer.valueOf(ch));
+        }
+        else if (Character.isLowerCase(ch)){
+            return (char)(sumLower - Integer.valueOf(ch));
+        }
+        else
+            return ch;
     }
 }
