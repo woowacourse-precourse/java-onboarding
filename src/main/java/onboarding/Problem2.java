07e6dc1ef56f603234bb9f332cc8393904c3226a
TonyWoo <do0134@naver.com>
2022-10-31 15:07:45 +0900

    1,3 problem sol

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String[] word_array;
+        word_array = cryptogram.split("");
+        boolean flag = true;
+        String answer = "";
+        while (flag) {
+
+        }
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i < number+1; i++) {
+            String temp = Integer.toString(i);
+            for (int j = 0; j < temp.length(); j++){
+                if (temp.charAt(j) == '3' || temp.charAt(j) == '6' || temp.charAt(j) == '9') {
+                    answer++;
+                }
+
+            }
+        }
         return answer;
     }
 }
