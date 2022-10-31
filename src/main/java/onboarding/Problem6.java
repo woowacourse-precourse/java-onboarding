b59447af730661e714ae04574c99b117daf2156a
jmjmjames <chungjm0711@gmail.com>
2022-10-31 12:48:41 +0900

    feat: add Crew class Problem6 - make List<Crew> from List<List<String>> forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,40 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        final List<Crew> crews = forms.stream()
+                .map(Crew::of)
+                .collect(Collectors.toList());
+
+        final Map<String, List<Crew>> crewMap = new HashMap<>();
+
+        return new ArrayList<String>();
     }
+
+    public static class Crew {
+
+        private final String email;
+        private final String nickname;
+
+        private Crew(String email, String nickname) {
+            this.email = email;
+            this.nickname = nickname;
+        }
+
+        public static Crew of(List<String> form) {
+            return new Crew(form.get(0), form.get(1));
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+    }
+
 }
