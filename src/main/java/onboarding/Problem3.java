8ef6cc0a582dc5f19cd6b7d6066fdc37f785b37d
doehy <dbehxks02@naver.com>
2022-10-31 21:40:35 +0900

    feat : 1부터 number까지 3,6,9 숫자  합 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = checkNumber(number);
         return answer;
+    }
+    static int checkNumber(int number) {
+        int clabtion_num = 0;
+        for(int i=1; i <= number; i++) {
+            int temp = i;
+            while (temp > 0) {
+                if (temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9) {clabtion_num += 1;}
+                temp /= 10;
+            }
+        }
+        return clabtion_num;
     }
 }
