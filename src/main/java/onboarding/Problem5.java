4e528be5a74cc601e8da1db3eb00826a0a0a078e
yeongjin <hanyj0104@naver.com>
2022-11-01 05:56:51 +0900

    동전 단위 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] unit={50000,10000,5000,1000,500,100,50,10,1};
+
+        List<Integer> answer = new ArrayList<>();
         return answer;
     }
 }
