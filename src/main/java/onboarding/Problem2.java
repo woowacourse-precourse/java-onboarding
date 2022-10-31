59846ee5b5722405b6edc5a102ac420d345d8d0b
yubin <dbqls2821@gmail.com>
2022-10-31 21:57:08 +0900

    FEAT: [Problem2] 문자열 내의 중복을 확인하고, 이를 제거하는 함수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,23 @@ import java.util.Objects;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        return removeContinuousCharacters(cryptogram);
+    }
+
+    private static String removeContinuousCharacters(String cryptogram) {
+        ArrayList<Character> list = convertStringToArrayList(cryptogram);
+        while (true) {
+            boolean duplication = false;
+            for (int i = list.size() - 1; i > 0; i--) {
+                if (list.get(i) == list.get(i - 1)) {
+                    list.remove(i);
+                    list.remove(i - 1);
+                    duplication = true;
+                }
+            }
+            if(!duplication) break;
+        }
+        return convertArrayListToString(list);
     }
 
     private static ArrayList<Character> convertStringToArrayList(String cryptogram) {
