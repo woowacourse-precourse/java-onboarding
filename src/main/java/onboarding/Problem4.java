4c9030bcc869271f03d3ef7e89a1ba75daa2bbb8
aud19 <aud19980518@gmail.com>
2022-10-31 20:46:38 +0900

    1주차 4번 문자 변환하는 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,4 +9,13 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static char charReturnConvert(char c) {
+        if (c >= 65 && c <= 90) {
+            return (char) (155 - c);
+        } else if (c >= 97 && c <= 122) {
+            return (char) (219 - c);
+        }
+        return c;
+    }
 }
