b43b4f30d86dc7b07c63bd6fbdea47b806e41c8a
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 21:08:05 +0900

    P1 | 결과 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,5 +6,24 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+
+        if (exceptionCheck(pobi,crong)){
+            return -1;
+        }
+
+        int pobiScore = resultScore(pobi.get(0), pobi.get(1));
+        int crongScore = resultScore(crong.get(0), crong.get(1));
+
+        if (pobiScore > crongScore) {
+            return 1;
+        }
+        if (pobiScore == crongScore) {
+            return 0;
+        }
+        if (pobiScore < crongScore){
+            return 2;
+        }
+
+        return -1;
     }
 }
