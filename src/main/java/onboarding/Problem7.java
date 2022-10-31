db0d0f9bc0bc2c9c0d549c65fa5665978cfb0381
KTH-96 <taehyun960@gmail.com>
2022-10-31 20:54:27 +0900

    comment(Problem7): 기능구현 목록 정리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    /**
+     * 기능 목록 작성
+     * 1. 유저의 친구를 찾는 기능
+     * 2. 유저의 친구의 친구를 찾는 기능
+     * 3. 친구의 친구 점수와 방문자의 점수를 합해서 가지고있는 기능
+     * 4. 점수를 비교해 상위 5명만 반환하는 기능
+     */
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
