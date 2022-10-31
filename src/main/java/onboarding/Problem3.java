c1b5560c81db4c73d476a6bdda4c08d26f3400f7
ChaCha3088 <cha3088@gmail.com>
2022-10-31 22:41:33 +0900

    feat: problem3 new function splitEachNumbers

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int[] splitEachNumbers(int number) {
+        int[] arrNum = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
+        return arrNum;
     }
 }
