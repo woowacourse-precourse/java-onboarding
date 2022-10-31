bf164fae38f3b23888c13bc0facc63c63c2cf3cf
baksohyeon <bori12370@gmail.com>
2022-10-31 13:50:26 +0900

    feat: add logic which get email, username separately from forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> emails = forms.stream().map(x -> x.get(0)).collect(Collectors.toList());
+        List<String> usernames = forms.stream().map(x -> x.get(1)).collect(Collectors.toList());
+
+
+
+        List<String> answer = List.of("jason@email.com", "jm@email.com", "mj@email.com");
         return answer;
     }
 }
