366ea0ca7b416f92b933ec0ef72dc1306f6e5f30
aossuper7 <aossuper7@naver.com>
2022-10-31 22:05:08 +0900

    refactor(Problem3): 함수제거

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,24 +3,15 @@ package onboarding;
 public class Problem3 {
 
     public static int solution(int number) {
-        if (number < 3) {
+        int hap = 0;
+        if (number < 3)
             return 0;
-        }
-
-        int hap = 0;
         for (int i = 3; i <= number; i++) {
-            hap += count(Integer.toString(i));
-        }
-
-        return hap;
-    }
-
-    public static int count(String number) {
-        int hap = 0;
-        for (int i = 0; i < number.length(); i++) {
-            char ch = number.charAt(i);
-            if (ch == '3' || ch == '6' || ch == '9') {
-                hap += 1;
+            String temp = Integer.toString(i);
+            for (int j = 0; j < temp.length(); j++) {
+                char ch = temp.charAt(j);
+                if (ch == '3' || ch == '6' || ch == '9')
+                    hap += 1;
             }
         }
         return hap;
