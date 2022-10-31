4382b2a333a611f178467ee6a0fa6f69e7fb60f9
eora21 <eora21@naver.com>
2022-10-31 21:36:17 +0900

    refactor: 💡 변경 369게임 객체 생성 및 게임 룰, 박수칠 최대 숫자 변경 가능하도록 메서드 수정
    369게임을 돌릴 수 있는 게임 객체 생성. 만약 룰을 바꾸거나, 룰은 그대로 두고 숫자만 변경하고 싶다면 Game 객체의 룰
    수정 및 play 시 숫자 변경으로 손쉽게 변경되는 데이터에 대처할 수 있도록 했다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,21 +3,46 @@ package onboarding;
 import java.util.Set;
 
 public class Problem3 {
-    private static final Set<Integer> CLAP_NUMBERS = Set.of(3, 6, 9);
-    private static final int CLAP = 1;
-    private static final int DO_NOT_CLAP = 0;
 
-    private static int clapCount(int maxNumber) {
+    /**
+     * 숫자 number가 매개변수로 주어질 때, 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수를 return 하도록 solution 메서드를 완성하라.
+     *
+     * @param number 주어지는 최대 숫자
+     * @return 손뼉 횟수
+     */
+    public static int solution(int number) {
+        ClapGame clapGame = new ClapGame();
+        return clapGame.play(number);
+    }
+}
+
+class ClapGame {
+    private Set<Integer> clapNumbers = Set.of(3, 6, 9);
+    private final int clap = 1;
+    private final int doNotClap = 0;
+
+    public ClapGame() {
+    }
+
+    public ClapGame(Set<Integer> clapNumbers) {
+        setClapNumbers(clapNumbers);
+    }
+
+    public void setClapNumbers(Set<Integer> clapNumbers) {
+        this.clapNumbers = clapNumbers;
+    }
+
+    public int play(int maxNumber) {
         int totalClap = 0;
 
         for (int number = 0; number <= maxNumber; number++) {
-            totalClap += splitNumberOfDigits(number);
+            totalClap += countClap(number);
         }
 
         return totalClap;
     }
 
-    private static int splitNumberOfDigits(int number) {
+    private int countClap(int number) {
         int clap = 0;
 
         while (number > 0) {
@@ -30,21 +55,11 @@ public class Problem3 {
         return clap;
     }
 
-    private static int isClapNumber(int placeNumber) {
-        if (CLAP_NUMBERS.contains(placeNumber)) {
-            return CLAP;
+    private int isClapNumber(int placeNumber) {
+        if (clapNumbers.contains(placeNumber)) {
+            return clap;
         }
 
-        return DO_NOT_CLAP;
-    }
-
-    /**
-     * 숫자 number가 매개변수로 주어질 때, 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수를 return 하도록 solution 메서드를 완성하라.
-     *
-     * @param number 주어지는 최대 숫자
-     * @return 손뼉 횟수
-     */
-    public static int solution(int number) {
-        return clapCount(number);
+        return doNotClap;
     }
 }
