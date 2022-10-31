c6b84fe0ec5796991da3bdfc84537b8d28900086
“rotomoo” <quddlf410@naver.com>
2022-10-31 20:20:51 +0900

    feat : implementation complete of Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,49 @@
 package onboarding;
 
+import onboarding.problem6.domain.Member;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+
+    private List<String> sortRemoveEmail(List<String> duplicateEmailList) {
+        List<String> sortedRemovedEmailList = new ArrayList<>();
+        Collections.sort(duplicateEmailList);
+        for (String email : duplicateEmailList) {
+            if (!sortedRemovedEmailList.contains(email)) sortedRemovedEmailList.add(email);
+        }
+        return sortedRemovedEmailList;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        Problem6 problem6 = new Problem6();
+
+        List<Member> memberList = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            memberList.add(new Member(form.get(0), form.get(1), 0));
+        }
+
+        HashMap<String, Integer> duplicateNicknameCheckedMap = new HashMap<>();
+
+        for (Member member : memberList) {
+            member.checkNickname(duplicateNicknameCheckedMap);
+        }
+
+        for (Member member : memberList) {
+            member.setDuplicateStatus(duplicateNicknameCheckedMap);
+        }
+
+        List<String> duplicatedNicknameEmailList = new ArrayList<>();
+
+        for (Member member : memberList) {
+            if (member.getDuplicateStatus() == 1) duplicatedNicknameEmailList.add(member.getEmail());
+        }
+
+        return problem6.sortRemoveEmail(duplicatedNicknameEmailList);
     }
 }
