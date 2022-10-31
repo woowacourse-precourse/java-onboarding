b472aa25cdf11a764e8271a73cadf3a4a366dac5
Fizioo0102 <a27313797@gmail.com>
2022-11-01 02:01:17 +0900

    feat: solution()함수 완성
    - answer에 최종 실행을 위한 process()함수를 불러와 답을 반환하도록 solution함수를 완성.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -14,7 +14,7 @@ public class Problem7 {
        }
    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer =  process(user,friends,visitors);
         return answer;
     }
 
