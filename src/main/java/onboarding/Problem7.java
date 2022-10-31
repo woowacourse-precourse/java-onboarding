450a98706d156e6f2c4998803443ec80f0217e58
Kayoung <rkdkdudjd@gmail.com>
2022-10-31 20:23:40 +0900

    7번

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,76 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+    public static List<String> solution(String targetUser, List<List<String>> friends, List<String> visitors) {
+        // 친구 set 만들기
+        Map<String, Set<String>> userToFriends = new HashMap<>(); // 타겟 유저 제외하고 전부 들어감
+        Set<String> targetUserFriends = new HashSet<>();
+        LinkedHashMap<String, Integer> userToRecommendScore = new LinkedHashMap<>();
+
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+
+            if (friend1.equals(targetUser)) { // user 의 친구관계일 경우
+                targetUserFriends.add(friend2);
+            } else if (friend2.equals(targetUser)) {
+                targetUserFriends.add(friend1);
+            }
+        }
+
+        friends.stream().filter(f -> {
+            String friend1 = f.get(0);
+            String friend2 = f.get(1);
+            return !friend1.equals(targetUser) && !friend2.equals(targetUser);
+        }).collect(Collectors.toList()).forEach(f -> {
+            String friend1 = f.get(0);
+            String friend2 = f.get(1);
+
+
+            if (!userToFriends.containsKey(friend1)) {
+                // 유저의 친구목록을 초기화
+                userToFriends.put(friend1, new HashSet<>());
+
+                // 점수 해시맵 초기화
+                if (!targetUserFriends.contains(friend1)) userToRecommendScore.put(friend1, 0);
+            }
+            userToFriends.get(friend1).add(friend2);
+
+
+            if (!userToFriends.containsKey(friend2)) {
+                userToFriends.put(friend2, new HashSet<>());
+                if (!targetUserFriends.contains(friend2)) userToRecommendScore.put(friend2, 0);
+            }
+            userToFriends.get(friend2).add(friend1);
+        });
+
+        System.out.println(userToFriends.keySet());
+        for (String friend : userToFriends.keySet()) {
+            if (targetUserFriends.contains(friend)) {
+                for (String user : userToFriends.get(friend)) {
+                    Integer score = userToRecommendScore.get(user);
+                    userToRecommendScore.put(user, score + 10);
+                }
+            }
+        }
+
+        for (String visitor : visitors) {
+            if (!targetUserFriends.contains(visitor)) { // 친구가 아니어야함
+                Integer score = userToRecommendScore.get(visitor);
+                if (score == null) {
+                    score = 0;
+                }
+                userToRecommendScore.put(visitor, score + 1);
+            }
+        }
+
+        return userToRecommendScore.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
