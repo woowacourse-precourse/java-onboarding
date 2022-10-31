043e999c9e29f357daa51d929584812fb9ddd181
wjh2012 <wjh2012@naver.com>
2022-10-31 19:21:01 +0900

    (PROBLEM6)닉네임을 두글자씩 잘라, 다른 사람의 닉네임에 포함되있는지 확인

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,46 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        HashSet<String> email = new HashSet<>();
+
+        int len = forms.size()-1;
+
+        // 마지막 사람은 확인하지 않음
+        for (int i=0; i<len-1;i++){
+
+            // 선택한 사람
+            String name = forms.get(i).get(1);
+
+            // 닉네임이 한글자라면 건너뜀
+            int nameSize = name.length();
+            if (nameSize < 2) {
+                break;
+            }
+
+            // 선택한 사람의 다음 사람부터 확인
+            for (int t = i + 1; t < len; t++) {
+                // 두 글자씩 잘라서
+                for (int j=0; j<nameSize-2;j++){
+                    String subName = name.substring(j,j+2);
+                    // 연속된 두 글자가 포함되면 이메일 대상에 포함/루프 탈출
+                    if (forms.get(t).get(1).contains(subName)) {
+                        email.add(forms.get(t).get(0));
+                        email.add(forms.get(i).get(0));
+                        break;
+                    }
+                }
+            }
+        }
+
+        List answer = new ArrayList(email);
+        Collections.sort(answer);
         return answer;
     }
 }
