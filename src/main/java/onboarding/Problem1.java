f7e2f02c7371f7189665685d38e56fd8a4b40813
mikekang47 <gyeong0308@gmail.com>
2022-10-31 17:46:27 +0900

    Feat<problem1>: return decideWinner value for solution method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -146,12 +146,13 @@ class Problem1 {
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-
         if (!handleErrors(pobi) || !handleErrors(crong)) {
             return -1;
         }
 
-        return answer;
+        int pobiResult = getHigherNumber(pobi);
+        int crongResult = getHigherNumber(crong);
+
+        return decideWinner(pobiResult, crongResult);
     }
 }
