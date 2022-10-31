c35a0aa1c1204dae9ec7fb6cb2bf9a755fafb41e
alstjr706 <alstjr706@naver.com>
2022-11-01 05:21:08 +0900

    fix : (SNS 친구 추천) 점수 계산을 위한 기능 구현

--- src/main/java/onboarding/problem7/FriendListScoreCalculation.java
+++ src/main/java/onboarding/problem7/FriendListScoreCalculation.java
@@ -9,10 +9,43 @@ public class FriendListScoreCalculation {
                                                       List<List<String>> friends,
                                                       List<String> visitors) {
 
-        HashMap<Object, Object> scoreMap = new HashMap<>();
+        HashMap<String, Integer> scoreMap = new HashMap<>();
 
+        FriendMap friendMap = new FriendMap();
+        HashMap<String, List<String>> friendList = friendMap.createFriendList(friends);
 
+        List<String> userFriends = friendList.remove(user);
+
+        calculations(visitors, scoreMap, friendList, userFriends);
 
         return null;
+    }
+
+    public void calculations(List<String> visitors,
+                             HashMap<String, Integer> scoreMap,
+                             HashMap<String, List<String>> relationships,
+                             List<String> userFriends) {
+
+        for (String nickname : relationships.keySet()) {
+
+            List<String> otherFriends = relationships.get(nickname);
+            int score = scoreMap.getOrDefault(nickname, 0);
+
+            for (String friendId : otherFriends) {
+                if (userFriends.contains(friendId)) {
+                  score += 10;
+                }
+            }
+
+            scoreMap.put(nickname, score);
+        }
+
+        for (String nickname : visitors) {
+            if (!userFriends.contains(nickname)) {
+                int score = scoreMap.getOrDefault(nickname, 0);
+                scoreMap.put(nickname, score + 1);
+            }
+        }
+
     }
 }
