7462b863d9554da6e519a0cd7f3dc626d5fa5eed
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 21:17:12 +0900

    Comment(Problem4) : 기능 목록 comment 삭제 기능 목록 comment를 삭제하고 PROBLEM4.md에 기능 목록을 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,12 +1,6 @@
 package onboarding;
 
 public class Problem4 {
-    /*
-    * 청개구리 사전 클래스인 TreeFrog는 청개구리 사전에 따라 알파벳을 반대로 변환해주는 convert 메서드를 포함한다.
-    * convert는 전달받은 word를 한글자씩 아스키코드로 소문자, 대문자, 알파벳 외의 경우로 나누고 계산을 통해 청개구리 사전과 같이 변환해줌.
-    * StringBuilder 클래스를 활용하여 변환한 문자들을 이어 붙여 String으로 만들어줌.
-    * */
-
     public static String solution(String word) {
         String answer = "";
         TreeFrog tf = new TreeFrog(word);
