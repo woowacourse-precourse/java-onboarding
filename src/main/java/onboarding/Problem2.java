da8a7e5738b13d2c63357568cf9af8ea7f1ff9a5
juhwang <juhong4930@naver.com>
2022-10-31 22:04:44 +0900

    refactor(Problem2):refactoring duplicate data processing

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,41 +6,47 @@ import java.util.List;
 public class Problem2 {
     public static String solution(String cryptogram) {
         while (duplicateStringFind(cryptogram).size() != 0) {
-            cryptogram = processCryptogram(cryptogram);
+            cryptogram = removeDuplication(cryptogram, duplicateStringFind(cryptogram));
         }
         return cryptogram;
     }
 
-    private static String processCryptogram(String cryptogram) {
-        return removeDuplication(cryptogram, duplicateStringFind(cryptogram));
-    }
-
     private static List<Integer[]> duplicateStringFind(String cryptogram) {
-        List duplicateIndexList = new ArrayList();
         String[] charList = cryptogram.split("(?<=.)");
+        List<Integer[]> duplicateIndexList = new ArrayList<>();
         Integer[] duplicateIndex = new Integer[2];
-        boolean duplicateCheck = false;
 
-        for (int i = 0; i < charList.length; i++) {
-            if (i == charList.length - 1) {
-                if (duplicateCheck) {
-                    duplicateIndex[1] = i;
-                    duplicateIndexList.add(duplicateIndex);
-                }
-                break;
-            }
-            if (charList[i].equals(charList[i + 1]) && !duplicateCheck) {
-                duplicateIndex[0] = i;
-                duplicateCheck = true;
-            } else if (!charList[i].equals(charList[i + 1]) && duplicateCheck) {
-                duplicateIndex[1] = i;
-                duplicateIndexList.add(duplicateIndex);
-                duplicateIndex = new Integer[2];
-                duplicateCheck = false;
-            }
+        if (putDuplicateIndexList(duplicateIndexList, charList, duplicateIndex)) {
+            duplicateIndex[1] = charList.length - 1;
+            duplicateIndexList.add(duplicateIndex);
         }
         return duplicateIndexList;
     }
+
+    private static boolean putDuplicateIndexList(List<Integer[]> duplicateIndexList, String[] charList, Integer[] duplicateIndex) {
+        boolean duplicateCheck = false;
+
+        for (int i = 0; i < charList.length - 1; i++) {
+            if (charList[i].equals(charList[i + 1]) && !duplicateCheck) {
+                duplicateCheck = updateDuplication(duplicateIndex, false, i);
+            }
+            if (!charList[i].equals(charList[i + 1]) && duplicateCheck) {
+                duplicateCheck = updateDuplication(duplicateIndex, true, i);
+                duplicateIndexList.add(duplicateIndex);
+            }
+        }
+        return duplicateCheck;
+    }
+
+    private static boolean updateDuplication(Integer[] duplicateIndex, boolean duplicateCheck, int i) {
+        if (duplicateCheck) {
+            duplicateIndex[1] = i;
+            return false;
+        }
+        duplicateIndex[0] = i;
+        return true;
+    }
+
 
     private static String removeDuplication(String cryptogram, List<Integer[]> duplicateIndexList) {
         String[] charList = cryptogram.split("(?<=.)");
