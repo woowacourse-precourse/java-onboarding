3fd2e8835e5c39b6883ea2822de8c2f306bcb43e
leewoojin <zmfpdl64@naver.com>
2022-10-31 14:28:50 +0900

    문제5번 제한사항 구현
    조건
    1. moeny의 크기가 1 이상 1,000,000이하의 자연수이다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,8 @@ import java.util.List;
 //Sol1: 큰 금액부터 나누어 몫을 리스트에 저장한다.
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
+    public static List<Integer> solution(int money) throws Exception {
+        valid(money);
         List<Integer> answer = new ArrayList<>();
         Integer[] amount = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1}; //지폐의 단위 배열을 만들어준다.
 
@@ -18,7 +19,13 @@ public class Problem5 {
         return answer;
     }
 
-    public static void main(String[] args) {
+    public static void valid(int money) throws Exception {
+        if(money < 1 || money > 1000000){
+            throw new Exception("입력받은 돈이 1이상 1,000,000이하의 자연수가 아닙니다."+money);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         System.out.println(solution(50237));
     }
 }
