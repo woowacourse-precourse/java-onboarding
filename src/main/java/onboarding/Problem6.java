498332b9811e8e36edbde2e1ff0fc1e6ed407bc4
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-10-31 21:10:16 +0900

    Problem6 기능 목록 3 : 해당 리스트에서 원소들끼리 같은 글자가 연속적인 경우를 체크하고, 해당될 경우 리스트를 새로 선언하여 해당 닉네임의 이메일을 추가한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,11 +9,23 @@ public class Problem6 {
         List<String> answer = List.of("answer");
 
         List<List<String>> saveInList = new ArrayList<>();
+        List<String> Deduplication = new ArrayList<>();
 
         for(int i = 0; i < forms.size(); i++){
 
             String nowString = forms.get(i).get(1);
             saveInList.add(getAllWordList(nowString));
+        }
+
+        for(int j = 0; j < saveInList.size(); j++){
+            for(int k = 0; k < saveInList.get(j).size(); k++){
+                for(int l = 1; l+j <saveInList.size(); l++){
+                    if(saveInList.get(j+l).contains(saveInList.get(j).get(k))){
+                        Deduplication.add(forms.get(j).get(0));
+                        Deduplication.add(forms.get(j+l).get(0));
+                    }
+                }
+            }
         }
 
         return answer;
