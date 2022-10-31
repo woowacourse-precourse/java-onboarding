4acdaa48add120d5428de984b35f9cfd6e4c30d2
buddy <81221429+taehwan0@users.noreply.github.com>
2022-10-31 18:14:15 +0900

    chore: PROBLEM4 클래스 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,53 +1,10 @@
 package onboarding;
 
+import onboarding.problem4.Frog;
+
 public class Problem4 {
 	public static String solution(String word) {
 		Frog frog = new Frog();
 		return frog.translate(word);
-	}
-
-	static class Frog {
-
-		private final char[] reversedLowerAlphabetArray;
-		private final char[] reversedHigherAlphabetArray;
-
-		Frog() {
-			char[] reversedLowerAlphabetArray = new char[26];
-			char[] reversedHigherAlphabetArray = new char[26];
-
-			for (int i = 0; i < 26; i++) {
-				reversedLowerAlphabetArray[i] = (char)(122 - i);
-				reversedHigherAlphabetArray[i] = (char)(90 - i);
-			}
-			this.reversedLowerAlphabetArray = reversedLowerAlphabetArray;
-			this.reversedHigherAlphabetArray = reversedHigherAlphabetArray;
-		}
-
-		public String translate(String word) {
-			StringBuilder stringBuilder = new StringBuilder();
-			for(char c : word.toCharArray()) {
-				stringBuilder.append(reverse(c));
-			}
-			return stringBuilder.toString();
-		}
-
-		private char reverse(char c) {
-			if (isLowerAlphabet(c)) {
-				int index = c - 97;
-				return reversedLowerAlphabetArray[index];
-			} else if (isHigherAlphabet(c)) {
-				int index = c - 65;
-				return reversedHigherAlphabetArray[index];
-			}
-			return c;
-		}
-
-		private boolean isLowerAlphabet(char c) {
-			return Character.isAlphabetic(c) && Character.isLowerCase(c);
-		}
-
-		private boolean isHigherAlphabet(char c) {
-			return Character.isAlphabetic(c) && (!Character.isLowerCase(c));
-		}
 	}
 }
---
+++ src/main/java/onboarding/problem4/Frog.java
@@ -0,0 +1,54 @@
+package onboarding.problem4;
+
+public class Frog {
+
+	private static final Integer MAX_LENGTH = 1000;
+	private static final Integer MIN_LENGTH = 1;
+
+	private final char[] reversedLowerAlphabetArray;
+	private final char[] reversedHigherAlphabetArray;
+
+	public Frog() {
+		char[] reversedLowerAlphabetArray = new char[26];
+		char[] reversedHigherAlphabetArray = new char[26];
+
+		for (int i = 0; i < 26; i++) {
+			reversedLowerAlphabetArray[i] = (char)(122 - i);
+			reversedHigherAlphabetArray[i] = (char)(90 - i);
+		}
+		this.reversedLowerAlphabetArray = reversedLowerAlphabetArray;
+		this.reversedHigherAlphabetArray = reversedHigherAlphabetArray;
+	}
+
+	public String translate(String word) {
+
+		if (word.length() < MIN_LENGTH || word.length() > MAX_LENGTH) {
+			throw new RuntimeException("1 이상 1000 이하의 값만 입력 가능합니다.");
+		}
+
+		StringBuilder stringBuilder = new StringBuilder();
+		for(char c : word.toCharArray()) {
+			stringBuilder.append(reverse(c));
+		}
+		return stringBuilder.toString();
+	}
+
+	private char reverse(char c) {
+		if (isLowerAlphabet(c)) {
+			int index = c - 97;
+			return reversedLowerAlphabetArray[index];
+		} else if (isHigherAlphabet(c)) {
+			int index = c - 65;
+			return reversedHigherAlphabetArray[index];
+		}
+		return c;
+	}
+
+	private boolean isLowerAlphabet(char c) {
+		return Character.isAlphabetic(c) && Character.isLowerCase(c);
+	}
+
+	private boolean isHigherAlphabet(char c) {
+		return Character.isAlphabetic(c) && (!Character.isLowerCase(c));
+	}
+}
