55b63ef6830f41c8945db0a78cb1fdd051c506f3
phobyjun <phobyjun@gmail.com>
2022-11-01 01:03:50 +0900

    [PROBLEM7] ADD: FEATURE2. 사용자의 추천 친구 목록 생성 함수 단위 테스트 코드

--- src/test/java/onboarding/Problem7MethodTest.java
+++ src/test/java/onboarding/Problem7MethodTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static onboarding.Problem7.getFriendsOfUser;
+import static onboarding.Problem7.getRecommendFriends;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem7MethodTest {
@@ -18,11 +19,28 @@ class Problem7MethodTest {
         friends1.add(List.of("donut", "jun"));
         friends1.add(List.of("donut", "mrko"));
         friends1.add(List.of("shakevan", "andole"));
-        friends1.add(List.of("shakeven", "jun"));
+        friends1.add(List.of("shakevan", "jun"));
         friends1.add(List.of("shakevan", "mrko"));
 
         List<String> result1 = getFriendsOfUser(user1, friends1);
 
         assertThat(result1).isEqualTo(List.of("donut", "shakevan"));
+    }
+
+    @Test
+    void getRecommendFriendsTest() {
+        String user1 = "mrko";
+        List<List<String>> friends1 = new ArrayList<>(List.of());
+        friends1.add(List.of("donut", "andole"));
+        friends1.add(List.of("donut", "jun"));
+        friends1.add(List.of("donut", "mrko"));
+        friends1.add(List.of("shakevan", "andole"));
+        friends1.add(List.of("shakevan", "jun"));
+        friends1.add(List.of("shakevan", "mrko"));
+        List<String> visitors1 = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+
+        List<String> result1 = getRecommendFriends(user1, friends1, visitors1);
+
+        assertThat(result1).isEqualTo(List.of("andole", "jun", "bedi"));
     }
 }
