cc2c3127dc2644b888e1f4e545e237f8e3a96f37
sunghyuki <sunghyuk1609@gmail.com>
2022-10-31 22:24:24 +0900

    feat : 길이가 2가 아닌 상황 검증 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (isNotLengthTwo(pobi.size()) || isNotLengthTwo(crong.size())) {
+            return EXCEPTION;
+        }
+
         return answer;
+    }
+
+    private static boolean isNotLengthTwo(int length) {
+        if (length != 2) {
+            return true;
+        }
+        return false;
     }
 }
