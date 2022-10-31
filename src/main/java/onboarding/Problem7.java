3096c9206ff0358afa2c7ae8a282c9b37dcf1fe4
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-01 03:20:12 +0900

    Docs(Problem7): 프렌드 리스트 문자열 길이 확인

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -12,6 +12,11 @@ public class Problem7 {
     static boolean isValidIdLength(int n) {
         return n >= 1 && n <= 30;
     }
+
+    static boolean isValidFriendsListLength(int n) {
+        return n >= 1 && n <= 10_000;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
