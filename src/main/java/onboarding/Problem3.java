dd74c9dce5eeb5a5e4f3f4e273fe0f23f8dfa743
yubin <dbqls2821@gmail.com>
2022-10-31 22:00:21 +0900

    FEAT: [Problem3] 전체 손뼉 횟수를 계산하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        return sumNumOfClaps(number);
+    }
+
+    private static int sumNumOfClaps(int number) {
+        int sum = 0;
+        for (int i = 1; i < number + 1; i++) {
+            for (char c : Integer.toString(i).toCharArray()) {
+                if(areThere369(c)) sum++;
+            }
+        }
+        return sum;
     }
 
     private static boolean areThere369(char c) {
