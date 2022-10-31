99655338cee7b9fa1016f6d6b8d90090976d2384
iamjooon2 <iamjooon2@gmail.com>
2022-10-31 21:41:57 +0900

    refactor(problem6): depth 2 넘어가는 부분 nameCount, getOberlapEmail 메서드로 분리 리팩터링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,31 +5,40 @@ import java.util.*;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         ArrayList<String> answer = new ArrayList<>();
-        HashMap<String, Integer> nameCount = new HashMap<>();
+        HashMap<String, Integer> nameCountMap = new HashMap<>();
 
         for (int i = 0; i < forms.size(); i += 1) {
-            for (int j = 0; j < forms.get(i).get(1).length() - 1; j += 1) {
-                String slicedName = forms.get(i).get(1).substring(j, j + 2);
-                if (nameCount.containsKey(slicedName)) {
-                    nameCount.put(slicedName, nameCount.get(slicedName) + 1);
-                } else {
-                    nameCount.put(slicedName, 1);
-                }
-            }
+            nameCount(forms, nameCountMap, i);
         }
 
-        for (String key : nameCount.keySet()) {
-            if (nameCount.get(key) > 1) {
-                for (int i = 0; i < forms.size(); i += 1) {
-                    if (forms.get(i).get(1).contains(key)) {
-                        answer.add(forms.get(i).get(0));
-                    }
-                }
+        for (String key : nameCountMap.keySet()) {
+            if (nameCountMap.get(key) > 1) {
+                getOverlapEmail(forms, key, answer);
             }
         }
 
         Collections.sort(answer);
 
         return answer;
+    }
+
+    public static HashMap<String, Integer> nameCount(List<List<String>> forms, HashMap<String, Integer> nameCountMap, int index){
+        for (int j = 0; j < forms.get(index).get(1).length() - 1; j += 1) {
+            String slicedName = forms.get(index).get(1).substring(j, j + 2);
+            if (nameCountMap.containsKey(slicedName)) {
+                nameCountMap.put(slicedName, nameCountMap.get(slicedName) + 1);
+            } else {
+                nameCountMap.put(slicedName, 1);
+            }
+        }
+        return nameCountMap;
+    }
+
+    public static void getOverlapEmail(List<List<String>> forms, String key, ArrayList<String> answer){
+        for (int i = 0; i < forms.size(); i += 1) {
+            if (forms.get(i).get(1).contains(key)) {
+                answer.add(forms.get(i).get(0));
+            }
+        }
     }
 }
