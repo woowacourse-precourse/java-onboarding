430cec3c13be7c6376f53983e77e9db576b3520e
situodys <situodys@gmail.com>
2022-10-31 09:52:08 +0900

    style,refactor(문제7): 메서드 순서 변경 및 filter 조건 메서드로 분리

--- src/main/java/onboarding/problem7/UserManager.java
+++ src/main/java/onboarding/problem7/UserManager.java
@@ -8,10 +8,10 @@ public class UserManager {
     private static final int SECOND_ID = 1;
 
     private Map<String, Set<String>> friendsTable;
-    private Map<String,Integer> friendsScore;
-    private Map<String,Integer> visitCount;
+    private Map<String, Integer> friendsScore;
+    private Map<String, Integer> visitCount;
 
-    public UserManager(List<List<String>> friends,List<String> visitors) {
+    public UserManager(List<List<String>> friends, List<String> visitors) {
         this.friendsTable = new HashMap<>();
         this.friendsScore = new HashMap<>();
         this.visitCount = new HashMap<>();
@@ -24,10 +24,10 @@ public class UserManager {
 
         return friendsScore.keySet()
                 .stream()
-                .filter(name -> friendsTable.get(targetUser) == null ||!friendsTable.get(targetUser).contains(name))
-                .filter(name-> friendsScore.get(name)>0)
+                .filter(name -> !isAlreadyFriend(targetUser, name))
+                .filter(name -> isScoreOverZeroAt(name))
                 .sorted((userName1, userName2) -> {
-                    if(friendsScore.get(userName2) == friendsScore.get(userName1))
+                    if (friendsScore.get(userName2) == friendsScore.get(userName1))
                         return userName1.compareTo(userName2);
                     return friendsScore.get(userName2) - friendsScore.get(userName1);
                 })
@@ -35,44 +35,12 @@ public class UserManager {
                 .collect(Collectors.toList());
     }
 
-    private void calculateFriendsScore(String targetUser) {
-        addFriendsScoreBySharedFriendCount(targetUser);
-        addFriendsScoreByVisitCount();
+    private boolean isAlreadyFriend(String targetUser, String name) {
+        return friendsTable.get(targetUser) != null && friendsTable.get(targetUser).contains(name);
     }
 
-    private void addFriendsScoreByVisitCount() {
-        for (String visitor : visitCount.keySet()) {
-            if (friendsScore.containsKey(visitor)) {
-                friendsScore.put(visitor,friendsScore.get(visitor)+visitCount.get(visitor));
-                continue;
-            }
-            friendsScore.put(visitor,visitCount.get(visitor));
-        }
-    }
-
-    private void addFriendsScoreBySharedFriendCount(String targetUser) {
-        for (String friend : friendsTable.keySet()) {
-            if(targetUser.equals(friend)) continue;
-            friendsScore.put(friend,10*countShardFriends(friend, targetUser));
-        }
-    }
-
-    private void initVisitCount(List<String> visitors) {
-        for (String visitor : visitors) {
-            visitCount.put(visitor, visitCount.getOrDefault(visitor, 0) + 1);
-        }
-    }
-
-    private int countShardFriends(String currentUser, String inputUser) {
-        int count = 0;
-        Set<String> inputUserFriendSet = friendsTable.get(inputUser);
-
-        if(inputUserFriendSet==null) return count;
-        for (String sharedFriend : friendsTable.get(currentUser)) {
-            if (inputUserFriendSet.contains(sharedFriend)) count++;
-        }
-
-        return count;
+    private boolean isScoreOverZeroAt(String name) {
+        return friendsScore.get(name) > 0;
     }
 
     private void initFriendsTable(List<List<String>> friends) {
@@ -84,10 +52,50 @@ public class UserManager {
         }
     }
 
+    private void initVisitCount(List<String> visitors) {
+        for (String visitor : visitors) {
+            visitCount.put(visitor, visitCount.getOrDefault(visitor, 0) + 1);
+        }
+    }
+
     private void addFriendToSet(String key, String newFriend) {
         if (!friendsTable.containsKey(key)) {
             friendsTable.put(key, new HashSet<>());
         }
         friendsTable.get(key).add(newFriend);
+    }
+
+    private void calculateFriendsScore(String targetUser) {
+        addFriendsScoreBySharedFriendCount(targetUser);
+        addFriendsScoreByVisitCount();
+    }
+
+    private void addFriendsScoreByVisitCount() {
+        for (String visitor : visitCount.keySet()) {
+            if (friendsScore.containsKey(visitor)) {
+                friendsScore.put(visitor, friendsScore.get(visitor) + visitCount.get(visitor));
+                continue;
+            }
+            friendsScore.put(visitor, visitCount.get(visitor));
+        }
+    }
+
+    private void addFriendsScoreBySharedFriendCount(String targetUser) {
+        for (String friend : friendsTable.keySet()) {
+            if (targetUser.equals(friend)) continue;
+            friendsScore.put(friend, 10 * countShardFriends(friend, targetUser));
+        }
+    }
+
+    private int countShardFriends(String currentUser, String inputUser) {
+        int count = 0;
+        Set<String> inputUserFriendSet = friendsTable.get(inputUser);
+
+        if (inputUserFriendSet == null) return count;
+        for (String sharedFriend : friendsTable.get(currentUser)) {
+            if (inputUserFriendSet.contains(sharedFriend)) count++;
+        }
+
+        return count;
     }
 }
