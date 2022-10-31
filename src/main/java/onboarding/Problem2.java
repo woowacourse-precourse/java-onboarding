3fbfe7286d248d140b1bebe2a1ff0fa0a257b99e
kksks3192 <kksks3192@gmail.com>
2022-11-01 05:25:09 +0900

    feat(problem2): 연속된 문자를 제거하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,20 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    public static String removeDuplicates(String cryptogram){
+        char[] cryptoArray = cryptogram.toCharArray();
+
+        char prev_char = 0;
+        int cnt = 0;
+        for(char c:cryptoArray){
+            if(prev_char!=c){
+                cryptoArray[cnt++] = c;
+                prev_char = c;
+            }else{
+                cnt--;
+            }
+        }
+        return new String(cryptoArray).substring(0,cnt);
+    }
 }
