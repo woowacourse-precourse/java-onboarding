487b16a22333615111c057a85e6244dec9cdd0a6
codingvegeta <hong090992@gmail.com>
2022-10-31 19:52:04 +0900

    feat : 친구 추천 점수를 가지고 있는 User 객체를 생성했습니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,44 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        User user1 = new User();
+        user1.setUserName(user);
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
+}
+
+class User implements Comparator<User> {
+    @Override
+    public int compare(User o1, User o2) {
+        return o1.getPoint() > o2.getPoint() ? 1 : ( o1.getPoint() < o2.getPoint() ? -1 : 0 );
+    }
+
+    private String userName;
+    private int point;
+    private List<User> friendList;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setFriendList(List<User> friendList) {
+        this.friendList = friendList;
+    }
+
+
 }
