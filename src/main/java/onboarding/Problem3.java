a023f1dad83f1330007f804698af21238d068eaf
mikekang47 <gyeong0308@gmail.com>
2022-10-31 18:24:20 +0900

    Feat<problem3>: add overBoundaryException method for checking boundary

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+
+    /**
+     * 바운더리(1미만, 10000 초과)를 체크하는 메서드 입니다.
+     * @param number
+     */
+    public static void overBoundaryException(int number) {
+        if(number > 10000 || number < 1) {
+            throw new RuntimeException("인풋 바운더리를 초과했습니다.(number: " + number);
+        }
+    }
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        overBoundaryException(number);
     }
 }
