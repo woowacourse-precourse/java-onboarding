c99683feba756d2b973259ef6b7065ba6dd9cb5c
star-sil <kimse9811@naver.com>
2022-10-31 19:27:48 +0900

    SOLVE problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        char[] frogWord = new char[1000];
+        for (int i = 0; i < word.length(); i++) {
+            frogWord[i] = changeCase(word.charAt(i));
+        }
+
+        return String.valueOf(frogWord).substring(0,word.length());
+    }
+
+    // 대소문자 변경하기
+    private static char changeCase(char ch) {
+        if (ch >= 97 && ch <= 122) { // 소문자
+            return (char) ('z' - (ch - 'a'));
+        } else if (ch >= 65 && ch <= 90) { // 대문자
+            return (char) ('Z' - (ch - 'A'));
+        } else return ch;
     }
 }
