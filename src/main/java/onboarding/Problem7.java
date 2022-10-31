4546da796a422da3536a594d3eafe2e26af18917
sdsr <dlxodnr1212@naver.com>
2022-10-31 13:39:36 +0900

    problem7 기능 4 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,14 +10,33 @@ public class Problem7 {
         List<String> myfriend = new ArrayList<String>();
         List<String> friendsfriend = new ArrayList<String>();
         List<String> visitors_name = new ArrayList<String>();
-
+        Score score[] = new Score[friendsfriend.size() + visitors_name.size()];
+        for (int i = 0; i < friendsfriend.size() + visitors_name.size(); i++) {
+            score[i] = new Score();
+        }
 
         myfriend = find_friend(friends);
         friendsfriend = finds_friend(friends);
         //친구의 친구에서 user 제거
         friendsfriend.remove(user);
 
+        // 친구가 될수있는 score
+        int count = 0;
+        for (int i = 0; i < friendsfriend.size(); i++, count++) {
+            score[count].name = friendsfriend.get(i);
+            score[count].name_score += 10;
+        }
+        for (int i = 0; i < visitors_name.size(); i++, count++) {
+            score[count].name = visitors_name.get(i);
+            score[count].name_score += 1;
+        }
+
         return answer;
+    }
+
+    static class Score {
+        String name = "";
+        int name_score = 0;
     }
     // 친구 list 반환
     public static List<String> find_friend(List<List<String>> friends) {
