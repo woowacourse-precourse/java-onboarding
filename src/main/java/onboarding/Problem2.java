3556585015fb7693217b680aab6e5a9ca56fb823
dener8 <8dener@gmail.com>
2022-10-31 12:58:44 +0900

    feat: 결과값 반환하는 decrypt()

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,20 +1,27 @@
 package onboarding;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = decrypt(cryptogram);
         return answer;
     }
 
-    static Deque<String> createDeque() {
-        Deque<String> deque = new ArrayDeque<>();
-        return deque;
+    static String decrypt(String cryptogram) {
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!stack.empty() && String.valueOf(cryptogram.charAt(i)).equals(stack.peek())) {
+                stack.pop();
+            } else {
+                stack.push(String.valueOf(cryptogram.charAt(i)));
+            }
+        }
+        String answer = stack.stream().map(String::valueOf).collect(Collectors.joining(""));
+        return answer;
     }
-
-
 
 }
 
