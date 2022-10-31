1f210d1c7e05764779cde003f1d7962aa2584e69
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:26:06 +0900

    [Problem1] 예외사항 처리하기 (김민서)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,14 +10,26 @@ class Problem1 {
      3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나 모두 곱하기
      4. 오른쪽 페이지를 모두 더한 값과 모두 곱한 값 중에서 가장 큰 수 구하여 자신의 점수로 사용하기
      5. 점수를 비교해 게임의 승자 정하기
-     6. 예외사항 처라하기
+     6. 예외사항 처리하기
      */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         int pobiTop = 0;
         int crongTop = 0;
 
+        // 예외사항 처리하기
+        if((pobi.get(0) + 1 != pobi.get(1)) || (crong.get(0) + 1 != crong.get(1))) {
+            answer = -1;
+            return answer;
+        }
+
         for(int i = 0; i< pobi.size(); i++) {
+            // 예외사항 처리하기
+            if(pobi.get(i) <= 1 || pobi.get(i) >= 400 || crong.get(i) <= 1 || crong.get(i) >= 400) {
+                answer = -1;
+                return answer;
+            }
+
             // 포비 왼쪽, 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나 모두 곱하기
             char[] pobiChar = pobi.get(i).toString().toCharArray();
             int pobisum = 0;
@@ -41,6 +53,7 @@ class Problem1 {
             crongTop = Math.max(Math.max(crongsum, crongmul), crongTop);
         }
 
+        // 점수를 비교해 게임의 승자 정하기
         if(pobiTop > crongTop) {
             answer = 1;
         } else if(pobiTop < crongTop) {
