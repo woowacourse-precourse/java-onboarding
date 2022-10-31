b3332e518cd274b3f1cb7a4efdfcf7050c6be072
송민진 <m_6595@naver.com>
2022-11-01 03:54:36 +0900

    #4 feat: make frogDictionary with HashMap

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+        Map<String, String> frogDict = makeFrogDictionary();
         return answer;
+    }
+    
+    public static Map<String, String> makeFrogDictionary() {
+        Map<String, String> frogDict = new HashMap<String, String>();
+        List<String> keys = List.of("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+        List<String> values = List.of("Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A");
+        for(int i = 0; i < keys.size(); i++) {
+            frogDict.put(keys.get(i), values.get(i));
+            frogDict.put(keys.get(i).toLowerCase(), values.get(i).toLowerCase());
+        }
+        return frogDict;
     }
 }
