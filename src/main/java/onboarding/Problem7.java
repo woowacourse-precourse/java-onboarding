b8d051845cbbd87185440de1d2419279633c0cc0
alswn <alswn9938@gmail.com>
2022-10-31 21:00:35 +0900

    [Q7] feat : add "FriendsMap 초기화" 메소드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static Map<String, List<String>> friendsMap;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void initFriendsMap(List<List<String>> friends){
+        friendsMap = new HashMap<>();
+        for (List<String> friend : friends) {
+            String firstFriend = friend.get(0);
+            String secondFriend = friend.get(1);
+            //  first에 second 추가하기
+            if(friendsMap.containsKey(firstFriend)){
+                friendsMap.get(firstFriend).add(secondFriend);
+            }
+            else{
+                List<String> friendsList = new ArrayList<>();
+                friendsList.add(secondFriend);
+                friendsMap.put(firstFriend, friendsList);
+            }
+            // second에 first 추가하기
+            if(friendsMap.containsKey(secondFriend)){
+                friendsMap.get(secondFriend).add(firstFriend);
+            }
+            else{
+                List<String> friendsList = new ArrayList<>();
+                friendsList.add(firstFriend);
+                friendsMap.put(secondFriend, friendsList);
+            }
+        }
     }
 }
