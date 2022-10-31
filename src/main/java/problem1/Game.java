fa6aae9a41d24454d6c283013a8d02f58c37e390
twoosky <lhjae3@naver.com>
2022-10-31 20:13:24 +0900

    feat(problem1): 점수 계산 및 비교, 결과값 반환 메소드를 호출하는 main 클래스 구현

--- src/main/java/problem1/Game.java
+++ src/main/java/problem1/Game.java
@@ -2,13 +2,37 @@ package problem1;
 
 import java.util.List;
 
-public class Winner {
+public class Game {
+    private final List<Integer> pobi;
+    private final List<Integer> crong;
+    private final int ERROR = -1;
 
-    public Winner(List<Integer> pobi, List<Integer> crong) {
-
+    public Game(List<Integer> pobi, List<Integer> crong) {
+        this.pobi = pobi;
+        this.crong = crong;
     }
 
-    public int getWinner(List<Integer> pobi, List<Integer> crong) {
-        PageScore  = new PageScore(pobi);
+    public int run() {
+        if (!inputValidate()) {
+            throw new IllegalArgumentException("올바르지 않은 입력값입니다.");
+        }
+        int pobiScore = getScore(pobi);
+        int crongSocre = getScore(crong);
+        Winner winner = Winner.of(pobiScore, crongSocre);
+        return winner.getResult();
+    }
+
+    private boolean inputValidate() {
+        return validate(pobi) && validate(crong);
+    }
+
+    private boolean validate(List<Integer> pages) {
+        Validation inputValidation = new Validation(pages);
+        return inputValidation.validate();
+    }
+
+    private int getScore(List<Integer> pages) {
+        Score score = new Score(pages);
+        return score.getMaxScore();
     }
 }
