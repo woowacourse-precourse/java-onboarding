0abe0c774073b6cfe231ef6fe54d40133595e265
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:30:13 +0900

    feat: Problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,8 +2,16 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+
+            if (isAlphabet(letter)) {
+                letter = convertLetterToReverse(letter);
+            }
+            builder.append(letter);
+        }
+        return builder.toString();
     }
 
     private static boolean isAlphabet(char letter) {
