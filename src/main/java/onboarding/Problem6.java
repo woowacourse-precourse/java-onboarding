8c849a2d49fa728ed878f6be4c118cad77c98adc
yun8565 <yunyun8565@naver.com>
2022-11-01 03:01:21 +0900

    feat(P6): 중복 문자 찾기 기능 추가
    모든 닉네임에서 길이가 2인 문자를 추출한 다음 횟수를 해시맵을 통해 기록한다.
    문자가 1번 이상 발견되면 키워드 목록에 추가한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,9 +7,15 @@ public class Problem6 {
     public static final int NICKNAME = 1;
 
     public static HashMap<String, String> crews;
+    public static List<String> answer;
+    public static HashSet<String> keywords;
 
     public static List<String> solution(List<List<String>> forms) {
+        answer = new ArrayList<>();
         crews = toHashMap(forms);
+        keywords = new HashSet<>();
+
+        findRepeatedWords(crews.keySet());
 
         return answer;
     }
@@ -20,5 +26,21 @@ public class Problem6 {
             crewInfo.putIfAbsent(crew.get(NICKNAME), crew.get(EMAIL));
         }
         return crewInfo;
+    }
+
+    public static void findRepeatedWords(Set<String> nicknames) {
+        HashMap<String, Integer> wordCount = new HashMap<>();
+
+        for(String nickname : nicknames) {
+            for(int i = 0; i < nickname.length() - 1; i++) {
+                String word = nickname.substring(i, i+2);
+                wordCount.put(word, wordCount.getOrDefault(word, 0)+1);
+            }
+        }
+
+        wordCount.forEach((word, count) -> {
+            if(count > 1)
+                keywords.add(word);
+        });
     }
 }
