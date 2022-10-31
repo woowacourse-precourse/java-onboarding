17b9194b1690d477ea8b21c7bf36deaa105c25ab
sysliar <sysliar@gmail.com>
2022-10-31 17:06:00 +0900

    Q2. restore testcase

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -50,12 +50,6 @@ class ApplicationTest {
             String result = "";
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
         }
-        @Test
-        void case3() {
-            String cryptogram = "browooon";
-            String result = "brown";
-            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
-        }
     }
 
     @Nested
