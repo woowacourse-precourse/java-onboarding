8de1c8343d85e63f46672a308d3c8bbf5078a22e
Young37 <20171470@g.dongseo.ac.kr>
2022-10-31 20:52:45 +0900

    배열 형태로 1씩 증가하는 숫자를 표현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int[] numArr = new int[5];
+        for(int i = 0; i<number;i++) {
+        	numArr[0]++;
+        	for(int j = 0; j<5 ; j++) {
+        		if(numArr[j]==10) {
+        			numArr[j+1]++;
+        			numArr[j] = 0;
+        		}
+        	}
+        }
         return answer;
     }
 }
