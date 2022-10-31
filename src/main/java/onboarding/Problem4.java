463943fcaae6241f78a4a199c0338dbb8a57f719
ttaehee <ttaehee1105@gmail.com>
2022-11-01 04:21:54 +0900

    문제4) feat : 해시맵에 변환문자 저장 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    private static Map<Character, Character> convertMap = new HashMap<>();
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        char[] frogWords = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S',
+                'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H',
+                'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+
+        convertMap = makeMap(frogWords);
+
+        return answer.toString();
     }
+
+    private static Map<Character, Character> makeMap(char[] words) {
+        Map<Character, Character> convertMap = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+            convertMap.put(words[words.length - 1 - i], words[i]);
+        }
+        return convertMap;
+    }
+
 }
