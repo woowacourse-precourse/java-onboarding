8a3b2afe5e488e5779c08ad24eb0805e3954b1e9
jihwan <sgh002400@naver.com>
2022-11-01 01:42:45 +0900

    feat: Problem4 청개구리 사전 구성 로직 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    private static final int ALPHABET_COUNT = 26;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Map<Character, Character> wordDictionary = createWordDictionary();
+
+        return "tmp"; //TODO - 임시값 수정
+    }
+
+    private static Map<Character, Character> createWordDictionary() {
+        Map<Character, Character> wordDictionary = new HashMap<>(ALPHABET_COUNT);
+
+        for (int i = 0; i < ALPHABET_COUNT; i++) {
+            putAlphabetPairInWordDictionary(wordDictionary, i);
+        }
+        return wordDictionary;
+    }
+
+    private static void putAlphabetPairInWordDictionary(Map<Character, Character> wordDictionary, int i) {
+        wordDictionary.put((char)('a' + i), (char)('z' - i));
+        wordDictionary.put((char)('A' + i), (char)('Z' - i));
     }
 }
