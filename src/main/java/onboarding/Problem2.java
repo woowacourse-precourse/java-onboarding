985f9f2b6efd682e56baff89b14e2f07e2d7b852
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 18:47:54 +0900

    Feat(Problem2) : replaceChar 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem2 {
+    private static final int MAX_LENGTH = 1000;
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = replaceChar(cryptogram);
+        return answer;
+    }
+
+    private static String replaceChar(String cryptogram) {
+        String tmp;
+        String answer;
+        answer = cryptogram.replaceAll("([a-z])\\1", "");
+        for(int i=0; i < MAX_LENGTH; i++) {
+            tmp = answer;
+            answer = answer.replaceAll("([a-z])\\1", "");
+
+            // 이전과 결과가 같을경우 반복문 탈출
+            if(answer.equals(tmp)) {
+                break;
+            }
+        }
         return answer;
     }
 }
