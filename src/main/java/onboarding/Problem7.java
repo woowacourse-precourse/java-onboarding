688472e92719a4cebc2fe5681b9b157b3f11b5f9
vcho1958 <gusckd57@gmail.com>
2022-11-01 01:09:48 +0900

    [problem7] Problem7 로직 완성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        SNS sns = new SNS();
-        sns.setDatas(friends, visitors);
-        return sns.recommendFriend(user);
+        SNS sns = new SNS(friends, visitors);
+        return sns.recommendFriends(user);
     }
 }
--- src/main/java/onboarding/problem7/SNS.java
+++ src/main/java/onboarding/problem7/SNS.java
@@ -10,5 +10,28 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SNS {
+    //데이터베이스
+    private Database database;
+
+    //정보 바로 DB에 등록
+    public SNS(List<List<String>> friends, List<String> visitors) {
+        database = new Database(visitors);
+        for (var relation : friends) {
+            database.addRelation(relation);
+        }
+        database.registerUsersFromLog(visitors);
+    }
+
+    //친구추천 후 스트림을 통한 필터링
+    public List<String> recommendFriends(String name) {
+        return database.getRecommendedUsersByName(name)
+                .stream()
+                .sorted(Comparator.<User,Integer>
+                                comparing(user -> -user.getScore())
+                        .thenComparing(user->user.getName()))
+                .map(user -> user.getName())
+                .limit(5)
+                .collect(Collectors.toList());
+    }
 
 }
