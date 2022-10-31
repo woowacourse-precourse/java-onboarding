6668e3e2970c5f7effe64060d53cf575d83b4466
ckn1070 <ckn1070@naver.com>
2022-10-31 21:04:49 +0900

    Solve the problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem4 {
+    static char convertReverse(char character){
+        int ascii = (int)character;
+        int startPoint = 0;
+        int endPoint = 0;
+        int displacement;
+        int reverseAscii;
+
+        if(65 <= ascii && ascii <= 90){
+            startPoint = 65;
+            endPoint = 90;
+        }
+        if(97<= ascii && ascii <= 122){
+            startPoint = 97;
+            endPoint = 122;
+        }
+        if(ascii == 32){
+            return (char)ascii;
+        }
+        displacement = ascii - startPoint;
+        reverseAscii = endPoint - displacement;
+        return (char)reverseAscii;
+    }
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i<word.length(); i++){
+            answer = answer.concat(String.valueOf(convertReverse(word.charAt(i))));
+        }
         return answer;
     }
 }
