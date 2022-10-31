239f96a2cd6ac8abb659dfd7b15bf92a1f574e4a
Konahyeon <koo@inha.edu>
2022-11-01 05:10:36 +0900

    중복 제거 메서드

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,10 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
-    public static boolean isOverlap(String target) {
+    public static boolean anyOverlap(String target) {
         for (int i = 0; i < target.length() - 1; i++) {
             if (target.charAt(i) == target.charAt(i+1)) {
                 return true;
@@ -10,18 +13,33 @@ public class Problem2 {
         return false;
     }
 
-/*    public static String removeOverlap(String target) {
-        String result = target;
-        for (int i = 0; i < result.length(); i++) {
+    public static String removeOverlap(String target) {
+        String result = "";
 
+        boolean overlapPosition[] = new boolean[target.length()];
+
+        for (int i = 0; i < target.length() - 1; i++) {
+            if (target.charAt(i) == target.charAt(i+1)) {
+                overlapPosition[i] = true;
+                overlapPosition[i+1] = true;
+                break;
+            }
         }
-    }*/
+
+        for (int i = 0; i < target.length(); i++) {
+            if (!overlapPosition[i]) {
+                result += target.charAt(i);
+            }
+        }
+
+        return result;
+    }
 
     public static String solution(String cryptogram) {
         String answer = "answer";
         answer = cryptogram;
-        while(isOverlap(answer)) {
-            //중복 제거
+        while(anyOverlap(answer)) {
+            answer = removeOverlap(answer);
         }
 
         return answer;
