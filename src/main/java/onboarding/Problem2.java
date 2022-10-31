8c54e4da8ee9d22a5550d4e7b28bdaa12764c071
KimYongJun <rladydwns129@naver.com>
2022-11-01 00:29:27 +0900

    feat(Problem2): Check duplication of input

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
+
 package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+
+        while (hasDuplication(answer)) {
+            answer = makeNewWord(answer);
+        }
+
         return answer;
+    }
+
+    private static boolean hasDuplication(String word) {
+        boolean duplicationValue = false;
+        int i = 0;
+
+        while (!duplicationValue && (i < word.length() - 1)) {
+            char nowLetter = word.charAt(i);
+            char backLetter = word.charAt(i + 1);
+
+            duplicationValue = (nowLetter == backLetter);
+            i++;
+        }
+
+        return duplicationValue;
     }
 }
