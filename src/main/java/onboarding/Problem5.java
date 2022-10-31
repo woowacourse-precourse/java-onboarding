143e52b5ddfae54b01b5c62c03ef8767fcc69fd6
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-01 02:19:03 +0900

    Problem5_Completed

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,12 +8,32 @@ package onboarding;
  */
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        //List<Integer> answer = Collections.emptyList();
+
+        ArrayList<Integer> billList = new ArrayList<Integer>(List.of(50000,10000,5000,1000,500,100,50,10,1));
+
+        for(int i=0; i<9; i++){
+            int cash = billList.get(i);
+            if(money/cash>0);
+            billList.set(i,money/cash);
+            money = money % cash;
+
+        }
+
+
+
+        return billList;
     }
+
+
+
+
+
+
 }
