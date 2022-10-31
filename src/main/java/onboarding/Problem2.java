d63c88204d866ec7954d6ef841153cec593da923
JungWoo <rlawjddn103@naver.com>
2022-10-31 15:55:24 +0900

    feat: Add Problem2 duplicateReplace function and return answer

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,44 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        List<String> answerList = duplicationReplace(cryptogram);
+
+        for (String st : answerList) {
+            answer.append(st);
+        }
+
+        return answer.toString();
+    }
+
+    public static List<String> duplicationReplace(String str){
+
+        List<String> strList = new ArrayList<>(Arrays.asList(str.split("")));
+
+        while (true){
+            HashSet<Integer> duplicateList = new HashSet<>();
+            for (int i = 0; i < strList.size() - 1; i++) {
+                if (Objects.equals(strList.get(i), strList.get(i + 1))){
+                    duplicateList.add(i);
+                    duplicateList.add(i+1);
+                }
+            }
+            if (duplicateList.size() == 0){
+                break;
+            }
+
+            Iterator<String> strIter = strList.iterator();
+            int idx = 0;
+            while (strIter.hasNext()){
+                strIter.next();
+                if (duplicateList.contains(idx)) strIter.remove();
+                idx += 1;
+            }
+        }
+
+        return strList;
     }
 }
