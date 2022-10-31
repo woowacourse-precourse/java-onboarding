37a9e159d55b0f7cce89e73b023a3cda47cc0227
Songyi Kim <ksl2950@naver.com>
2022-10-31 10:01:01 +0900

    feat/6: 닉네임, 이메일 Map 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Map<String, String> crewInfo = new HashMap<>();
+        List<String> answer = new ArrayList<>();
+
+        forms.forEach(form ->
+                crewInfo.put(form.get(1), form.get(0))
+        );
+
         return answer;
     }
+    
+
 }
