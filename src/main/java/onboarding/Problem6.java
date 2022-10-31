927d221528c17e12aa3e724ac9f17266493aa30e
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-10-31 19:00:44 +0900

    Add : 이메일중 email.com이 아닌 도메인이나 닉네임이 1글자인 경우를 찾는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+    static Map<String, String> map;
+    static Set<String> overLapNickNameEmail;
+
+    // 이메일 중 email.com이 아닌것을 찾는 함수
+    public static boolean checkEmail(String email){
+        String[] emailArr = email.split("@");
+        if(!emailArr[1].equals("email.com")) return false;
+        return true;
+    }
+
+    // 유저의 이메일의 도메인이 유효하지 않거나 닉네임이 1자라 중복이 발생할 수 없는 경우 체크
+    public static boolean exceptionCheck(List<String> user){
+        if(user.get(1).length() == 1 || !checkEmail(user.get(0))){
+            return true;
+        }
+        return false;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
