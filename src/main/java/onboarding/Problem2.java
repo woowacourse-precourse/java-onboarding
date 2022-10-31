1d4fe4000f32f20eb7c88b69aadc098e8e11ecf0
kmularise <kmula222@gmail.com>
2022-10-31 14:46:29 +0900

    solveCryptogram 함수를 통해, 조건에 따라 answer의 마지막 요소를 삭제하거나, cryptogram의 해당 인덱스의 요소 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,23 +1,27 @@
 package onboarding;
 
 public class Problem2 {
-    private static boolean checkSame(char ch1, char ch2){
-        if (ch1 == ch2){
-            return true;
-        }
-        return false;
-    }
-    public static String solution(String cryptogram) {
-        StringBuffer answer = new StringBuffer("");
-        for(int i = 0; i < cryptogram.length(); i++){
-            if (answer.length() > 0 &&
-                    checkSame(cryptogram.charAt(i), answer.charAt(answer.length() -1))){
-                answer.deleteCharAt(answer.length() - 1);
-            }
-            else{
-                answer.append(cryptogram.charAt(i));
-            }
-        }
-        return answer.toString();
-    }
+	private static boolean checkSame(char ch1, char ch2) {
+		if (ch1 == ch2) {
+			return true;
+		}
+		return false;
+	}
+
+	private static void solveCryptogram(char ch, StringBuffer answer) {
+		if (answer.length() > 0 &&
+			checkSame(ch, answer.charAt(answer.length() - 1))) {
+			answer.deleteCharAt(answer.length() - 1);
+		} else {
+			answer.append(ch);
+		}
+	}
+
+	public static String solution(String cryptogram) {
+		StringBuffer answer = new StringBuffer("");
+		for (int i = 0; i < cryptogram.length(); i++) {
+			solveCryptogram(cryptogram.charAt(i), answer);
+		}
+		return answer.toString();
+	}
 }
