b274c4e3f106b2bdf9da64d230bfe0d42fba41bc
aossuper7 <aossuper7@naver.com>
2022-10-31 22:22:50 +0900

    feat: 6번 모든 닉네임을 2글자씩 잘라서 list 넣는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> result = new ArrayList<>();
+        HashMap<String, Integer> name = new HashMap<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = 0; j < forms.get(i).get(1).length() - 1; j++) {
+                String subName = forms.get(i).get(1).substring(j, j + 2);
+                if (!name.containsKey(subName)) {
+                    name.put(subName, i);
+                }
+            }
+        }
     }
 }
