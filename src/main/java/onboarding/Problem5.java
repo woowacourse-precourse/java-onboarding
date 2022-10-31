21a8d27c7ea6648a213492dea7a83ca24844cf2a
kimdoha <kkhhjj3393@gmail.com>
2022-10-31 19:16:00 +0900

    feat: 은행에 데이터 삽입(bankInit) 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import com.sun.source.tree.Tree;
+import java.util.*;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+    private static LinkedHashMap<Integer, Integer> bank = new LinkedHashMap<>();
+
+    private static void bankInit() {
+        Integer[] moneys = new Integer[]{ 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(Integer money : moneys) {
+            bank.put(money, 0);
+        }
     }
+
 }
