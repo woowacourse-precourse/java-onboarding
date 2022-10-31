8b90024f48b678735c1e5e256ccc3b7f37377a54
Cho-El <syzzang21c@gmail.com>
2022-11-01 01:40:20 +0900

    3,6,9 갯수 세는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,21 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+    // 해당 숫자 3,6,9갯수 세기
+    static int Count369(int num){
+        int[] arr = {3,6,9};
+        int count = 0;
+        while(num > 0){
+            if (List.of(arr).contains(num % 10)){
+                count++;
+                num /= 10;
+            }
+        }
+        return count;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
