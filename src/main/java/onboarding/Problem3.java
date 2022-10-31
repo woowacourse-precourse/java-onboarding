4d637a1a1981adf5ae9c34cf228abeea7bce3d92
gomdol <oioifor@naver.com>
2022-11-01 03:10:36 +0900

    (Problem3) 369 확인 후 결과 값 출력

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+//1. 3 6 9 가 있는지 파악 후 개수 만큼 결과 값 더해준다
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        while (number > 0) {
+            // 자리 값 마다 3 6 9 체크 후 count 더해줌
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                answer++;
+            }
+            number /= 10;
+
+        }
         return answer;
     }
 }
