c6a95731590805e3f1e47135fb324bdec890261b
KongDuHwan <kong_du@naver.com>
2022-10-31 16:30:31 +0900

    Problem3 Complete

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+
+        int sum=0;
+        for(int i=1;i<=number;i++)
+        {
+            for(int tmp=i;tmp>0;tmp/=10){
+                if((tmp%10)%3==0 && (tmp%10)!=0){
+                    sum++;
+                }
+            }
+        }
+//        3, 3*10 + 3*9, (3*10+3*9)*10 + (3*10+3*9)*9
+        return sum;
     }
 }
