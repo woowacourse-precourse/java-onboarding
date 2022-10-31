137b7c948560f0bf3f3293437b7bef5762c2f250
kariskan <zkfzpf56@naver.com>
2022-10-31 13:43:13 +0900

    refactor: 주어진 클래스를 사용한 solution 함수 리팩토링

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import onboarding.support.problem7.RecommendationService;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        RecommendationService recommendationService = new RecommendationService(friends, user, visitors);
+        return recommendationService.getSortedFriends();
     }
 }
