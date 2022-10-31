4b3f170bcccee539d4ee7e58932acc3412f218b7
Umin <adorecamus@gmail.com>
2022-10-31 19:21:43 +0900

    Add: Problem2 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,65 +1,51 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-//    public static List<Integer> pobi = new ArrayList<>();
-//
-//    public static List<Integer> crong = new ArrayList<>();
-
-    public static int listMax(List<Integer> list){
-        int listMax=0;
-        if(list.get(0) != list.get(1)-1 | list.get(0)%2 != 1 | list.get(1)%2 != 0 | list.get(0) <= 1 | list.get(1) >= 400){
-            return listMax;}
-        for(int i=0; i<list.size(); i++) {
-            int num = list.get(i);
-//            if (num <= 1 | num >= 400 ) {
-//                listMax = 0;
-//                System.out.println("listMax: " + listMax);
-//                return listMax;
-//            } else {
-                int plus;
-                int by;
-                if (num >= 100) {
-                    plus = num / 100 + num % 100 / 10 + num % 10;
-//                    System.out.println("plus: " + plus);
-                    by = (num / 100) * (num % 100 / 10) * (num % 10);
-//                    System.out.println("by: " + by);
-                } else {
-                    plus = num % 100 / 10 + num % 10;
-//                    System.out.println("plus: " + plus);
-                    by = (num % 100 / 10) * (num % 10);
-//                    System.out.println("by: " + by);
-                }
-                int max = Math.max(plus, by);
-//                System.out.println("max: " + max);
-                listMax = (listMax > max) ? listMax : max;
-//                System.out.println("listMax: " + listMax);
-//            }
-        }
-//        System.out.println("finalListMax: " + listMax);
-        return listMax;
-    }
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = -1;
-        int maxPobi = listMax(pobi);
-        if (maxPobi==0) {return answer;}
-        int maxCrong = listMax(crong);
-        if (maxCrong==0) {return answer;}
 
-        if (maxPobi > maxCrong) {answer = 1;}
-        else if (maxPobi < maxCrong) {answer = 2;}
-        else if (maxPobi == maxCrong) {answer = 0;}
+        int maxPobi = listMax(pobi);
+        //예외사항
+        if(maxPobi==0) return answer;
+
+        int maxCrong = listMax(crong);
+        //예외사항
+        if(maxCrong==0) return answer;
+
+        if(maxPobi > maxCrong) answer = 1;
+        else if(maxPobi < maxCrong) answer = 2;
+        else answer = 0;
+
         return answer;
     }
 
-//    public static void main(String[] args){
-//        pobi.add(99);
-//        pobi.add(102);
-//        crong.add(211);
-//        crong.add(212);
-//        System.out.println(solution(pobi, crong));
-//    }
+    //최댓값을 구한다.
+    public static int listMax(List<Integer> list){
+        int listMax = 0;
+
+        //예외사항(양쪽 페이지가 아닌 경우, 왼쪽-홀수&오른쪽-짝수 아닌 경우, 1 이하 400 이상인 경우)
+        if(list.get(0) != list.get(1)-1 |
+                list.get(0)%2 != 1 | list.get(1)%2 != 0 |
+                list.get(0) <= 1 | list.get(1) >= 400)
+            return listMax;
+
+        for(int num : list) {
+            int sum;
+            int multiple;
+
+            if(num >= 100) {
+                sum = num / 100 + num / 10 % 10 + num % 10;
+                multiple = (num / 100) * (num / 10 % 10) * (num % 10);
+            } else {
+                sum = num / 10 % 10 + num % 10;
+                multiple = (num / 10 % 10) * (num % 10);
+            }
+            int max = Math.max(sum, multiple);
+            listMax = (listMax > max)? listMax : max;
+        }
+        return listMax;
+    }
+
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        //연속하는 중복 문자가 없을 때까지
+        while(duplicate(cryptogram)) {
+            //연속하는 중복 문자 앞의 문자들과 뒤의 문자들을 저장
+            for(int i=0; i<cryptogram.length()-1; i++) {
+                if(cryptogram.charAt(i)==cryptogram.charAt(i+1)) {
+                    cryptogram = "" + cryptogram.subSequence(0, i)
+                            + cryptogram.subSequence(i+2, cryptogram.length());
+                }
+            }
+        }
+        answer = cryptogram;
+
         return answer;
     }
+
+    //연속하는 중복 문자가 있는지 검사
+    public static boolean duplicate(String cryptogram) {
+        for(int i=1; i<cryptogram.length(); i++) {
+            if(cryptogram.charAt(i-1) == cryptogram.charAt(i)) return true;
+        }
+        return false;
+    }
+
 }
