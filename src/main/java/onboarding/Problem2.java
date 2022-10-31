b62bebb85671207493ce680c9da6642b3b0ad79e
kevstevie <jianna442@gmail.com>
2022-10-31 17:55:25 +0900

    feat: add do while line

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,11 +6,29 @@ public class Problem2 {
     public static String solution(String cryptogram) {
 
         Deque<Character> chDeque = new LinkedList<>();
-        List cryptoList = StringToList(cryptogram);
+        List<Character> cryptoList = StringToList(cryptogram);
+        boolean cnt;
+        boolean continuous = false;
 
         do {
+            cnt = false;
+            int len = cryptoList.size();
+            for (int i = 0; i<len; i++) {
+                if (!chDeque.isEmpty() && chDeque.getLast() == cryptoList.get(i)) {
+                    continuous = true;
+                    continue;
+                }
+                if (continuous) {
+                    chDeque.removeLast();
+                    continuous = false;
+                    cnt = true;
+                } else {
+                    chDeque.addLast(cryptoList.get(i));
+                }
+            }
+            cryptoList = initialList(chDeque);
+        } while (cnt);
 
-        }
         return printDeque(chDeque);
     }
 
@@ -18,6 +36,14 @@ public class Problem2 {
         List result = new ArrayList();
         for (int i = 0; i < str.length(); i++) {
             result.add(str.charAt(i));
+        }
+        return result;
+    }
+
+    static List initialList(Deque deq) {
+        List result = new ArrayList();
+        while (!deq.isEmpty()) {
+            result.add(deq.removeFirst());
         }
         return result;
     }
