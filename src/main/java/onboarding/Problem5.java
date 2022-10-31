2a75d8e8c8b6f9b55333c5dd4ba133003fd7e0bc
CMHyuk <alsgur9710@naver.com>
2022-10-31 22:25:58 +0900

    refactor: 1 ~ 50000원 배열로 초기화, 배열을 통해 계산으로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,49 +1,22 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
-    public static final int FIRST_NUMBER_FIVE = 5;
-    public static final int FIRST_NUMBER_ONE = 1;
-    public static final int TENS = 10;
+    public static final int[] moneyArray = {50000, 10000, 5000, 1000, 500, 100 ,50 ,10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-
-        int calculateMoney = 50000;
-        int check = 5;
-
-        calculate(money, answer, calculateMoney, check);
+        calculate(money, answer);
         return answer;
     }
 
-    private static void calculate(int money, List<Integer> answer, int calculateMoney, int check) {
-        while (true) {
-            int countMoney = money / calculateMoney;
-
+    private static void calculate(int calculatedMoney, List<Integer> answer) {
+        for (int money : moneyArray) {
+            int countMoney = calculatedMoney / money;
             answer.add(countMoney);
-            money -= countMoney * calculateMoney;
-
-            if (isOnes(calculateMoney)) {
-                break;
-            }
-            if (check == FIRST_NUMBER_FIVE) {
-                calculateMoney /= 5;
-                check = 1;
-            } else if (check == FIRST_NUMBER_ONE) {
-                if (calculateMoney == TENS) {
-                    calculateMoney /= 10;
-                } else if (calculateMoney != TENS) {
-                    calculateMoney /= 2;
-                    check = 5;
-                }
-            }
+            calculatedMoney %= money;
         }
-    }
-
-    private static boolean isOnes(int calculateMoney) {
-        return calculateMoney == 1;
     }
 }
