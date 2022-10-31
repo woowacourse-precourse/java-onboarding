c5fdbf93bb6eed8e115ca02ce759b7d1cdce0979
LeeJejune <leejejune0625@gmail.com>
2022-10-31 14:40:14 +0900

    feat: 상수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static final int WORD_MAX_LANGE = 1000;
+    private static final int WORD_MIN_LANGE = 1;
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+
     public static String solution(String word) {
         String answer = "";
         return answer;
