6ed9e705e15f9f2f077b66329013c5fee6ab469b
chanminmaxscaler <jcm04@naver.com>
2022-10-31 21:11:11 +0900

    feat(Crews): Crews 클래스를 구현한다
    Crews 클래스는 List<Crew>를 유일한 멤버변수로 갖는 일급 컬렉션이다.

---
+++ src/main/java/onboarding/problem6/collection/Crews.java
@@ -0,0 +1,13 @@
+package onboarding.problem6.collection;
+
+import onboarding.problem6.domain.Crew;
+
+import java.util.List;
+
+public class Crews {
+    private final List<Crew> crews;
+
+    public Crews(List<Crew> crews) {
+        this.crews = crews;
+    }
+}
