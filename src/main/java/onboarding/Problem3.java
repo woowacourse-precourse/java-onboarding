fda0424fd1688c4b5799bf3ef4c0bc760cca9e5c
LeeMyeongJin <dlaudwls1203@naver.com>
2022-11-01 03:18:15 +0900

    Problem3 sol

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,6 @@ public class Problem3 {
         int count = 0;
         for (int i = 1; i <= number; i++) {
             int current = i;
-            int temp = count;
             while (current != 0) {
                 if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9){
                     count++;
