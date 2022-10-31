e4ed618c84fe2c1a1007ff21f386a9238bdac7f0
mckim123 <minchan06@gmail.com>
2022-10-31 16:17:15 +0900

    문제 6 기능 목록 구성 및 기능 1 구현
    문제 6의 기능 목록을 구성하여 PROBLEM6.md에 작성하고, 기능 1을 구현하였습니다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,35 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        HashMap<String, Set<Integer>> map = new HashMap<>();
+        for (int i=0;i<forms.size();i++) {
+            List<String> user_info = forms.get(i);
+            String nickname = user_info.get(1);
+            if (nickname.length() >= 2){
+                Set<String> key_set = new HashSet<>();
+                char curr = nickname.charAt(0);
+                int j = 1;
+                while (j < nickname.length()){
+                    char next = nickname.charAt(j);
+                    String key = "" + curr + next;
+                    if (!key_set.contains(key)){
+                        key_set.add(key);
+                        if(map.containsKey(key)){
+                            Set<Integer> id_set = map.get(key);
+                            id_set.add(i);
+                            map.put(key, id_set);
+                        } else {
+                            map.put(key, new HashSet<>(List.of(i)));
+                        }
+                    }
+                    curr = next;
+                    j += 1;
+                }
+            }
+        }
         List<String> answer = List.of("answer");
         return answer;
     }
