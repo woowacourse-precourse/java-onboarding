1ad2facee491a7f616a72ea45053027fd7cf28f3
thousandcows <milcamboo@gmail.com>
2022-10-31 10:57:47 +0900

    feat: created logic to change word cases

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        // 1. Initialized answer as StringBuilder
+        StringBuilder answer = new StringBuilder();
+
+        // 2. Reverse all digits with ascii
+        for (int i = 0; i < word.length(); i++) {
+            char currLetter = word.charAt(i);
+            if (currLetter == ' ') {    // a. spaces
+                answer.append(currLetter);
+            } else if (currLetter >= 97 && currLetter <= 122){  // b. lowercase letters
+                answer.append((char) (219 - currLetter));
+            } else {    // c. uppercase letters
+                answer.append((char) (155 - currLetter));
+            }
+        }
+        // 3. Return answer in String
+        return answer.toString();
     }
 }
