4b004bcd1e6287d83c0e4a506ca78e7770420578
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-10-31 21:41:32 +0900

    Comment : CharSpliter 주석 추가

--- src/main/java/onboarding/CharSpliter.java
+++ src/main/java/onboarding/CharSpliter.java
@@ -3,13 +3,16 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.List;
 
+// 단어를 한글자씩 Character 객체로 변환한 리스트를 반환하는 객체
 public class CharSpliter<T> {
     private List<Character> chars = new ArrayList<>();
 
     public List<Character> getChars(T data) {
 
+        // 제네릭 타입의 data를 String으로 변환
         String strData = String.valueOf(data);
 
+        // String data에서 한글자씩 Character 형태로 자름
         for (int i = 0; i < strData.length(); i++)
             chars.add(strData.charAt(i));
 
