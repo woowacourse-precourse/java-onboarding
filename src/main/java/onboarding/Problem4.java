ef621b3e8f57b799a66e3b75a960a256b3ddca1b
권민정 <22alswjd@naver.com>
2022-11-01 02:03:08 +0900

    feat: Problem4 기능 '1. 변환된 i번째 문자를 저장할 character 변수 선언' 완료
    기능 목록을 작성한 주석 추가함.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,8 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int size = word.length();
 
         // 1. 변환된 i번째 문자를 저장할 character 변수 선언
+        for (int i = 0; i < size; i++) {
+            char character = word.charAt(i);
+        }
         // 2. i번째 문자가 대문자인 경우
         // 3. i번째 문자가 소문자인 경우
         // 4. i번째 문자가 알파벳이 아닌 경우
