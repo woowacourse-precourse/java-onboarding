820259e906df0427653146d865496a1a1de46f05
unknown <2021110806@dgu.ac.kr>
2022-10-31 14:00:44 +0900

    feat:problem6 Stringcheckermethod/function List

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+    
+    public static int stringChecker(String string1, String string2) {
+
+        int idx = 0;
+        int cnt = 0;
+        int maxIdx = Math.min(string1.length(), string2.length());
+
+        for (int i = 0; i < maxIdx; i++) {
+            for (int j = 0; j < maxIdx; j++) {
+                if (string1.charAt(i) == string2.charAt(j)) {
+                    while (j + idx < maxIdx && string1.charAt(i + idx) == string2.charAt(j + idx)) {
+                        idx++;
+                        cnt++;
+                    }
+                    return cnt;
+                }
+            }
+        }
+        return cnt;
     }
+
 }
