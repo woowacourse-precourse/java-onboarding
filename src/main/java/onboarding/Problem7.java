60fae9fe51423191110a4eff82898ecd4127aa85
GraphTree <graphnodesed@gmail.com>
2022-10-31 16:53:15 +0900

    Feat: 유저 이름만 뽑아서 유저 이름 세트를 만드는 nameCreator 함수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<String> nameSetCreator(String user ,List<List<String>> friends) {
+        Set<String> names = new HashSet<String>();
+        for (List<String> friend : friends) {
+            names.add(friend.get(0));
+            names.add(friend.get(1));
+        }
+        names.add(user);
+
+        List<String> result = new ArrayList<>(names);
+        Collections.sort(result);
+        return result;
     }
 }
