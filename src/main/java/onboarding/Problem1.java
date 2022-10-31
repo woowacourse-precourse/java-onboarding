4c016ec7a9a0f3c82a77b68987ab51f2b9c90e53
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-10-31 22:49:46 +0900

    feat : 문제1 포비 큰 수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        
+        //1) 포비 큰 수 구하기
+        int pobi_max = 0;
+        int pobi_sum = 0;
+        int pobi_calc = 1;
+        for(int i=0; i<pobi.size(); i++){
+            String num = String.valueOf(pobi.get(i));
+            for(int j=0; j<num.length(); j++){
+                pobi_sum += Character.getNumericValue(num.charAt(j));
+                pobi_calc = pobi_calc * Character.getNumericValue(num.charAt(j));
+            }
+        }
+        pobi_max = Math.max(pobi_sum, pobi_calc);
+        
         return answer;
     }
 }
