df3c1f9d2776f412d10be5e487296b8d8ee23b04
Jinhui <jda0526@naver.com>
2022-11-01 00:02:35 +0900

    Feat(Problem3): 1부터 number까지 3,6,9가 들어가는 횟수 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
 
 public class Problem3 {
+    static int result;
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        result = 0;
+        return game(number);
+    }
+
+    private static int game(int number) {
+        for (int i = 1; i <= number; i++) {
+            rule(i);
+        }
+//        System.out.println("result : " + result);
+
+        return result;
+    }
+
+    private static void rule(int number){
+        int quotient = number / 10; // 몫
+        int remainder = number % 10; // 나머지
+
+        if(remainder == 3 || remainder == 6 || remainder == 9){
+            result += 1;
+        }
+
+        // 한 자리 숫자가 아닐 때 다른 자리에서도 찾기 위해서 재귀 함수 실행
+        if(quotient != 0)
+            rule(quotient);
     }
 }
