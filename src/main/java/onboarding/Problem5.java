d25737dae216a6ff9c880838e610802281c3168a
saint6839 <saint6839@gmail.com>
2022-10-31 16:59:46 +0900

    feat(Problem5): 결과를 계산하여 반환하는 메서드 구현
    - 입력 받은 돈을 화폐 가치로 나눈 몫 = 해당 화폐가 사용된 갯수
    - 입력 받은 돈을 화폐 가치로 나눈 나머지 = 다음 인덱스의 화폐와 비교를 위해 값을 업데이트 시켜줌

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,18 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         int[] moneyValues = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        return null;
+        return calculateResult(money, moneyValues);
+    }
+
+    private static List<Integer> calculateResult(int money, int[] moneyValues) {
+        List<Integer> results = new ArrayList<>();
+
+        for (int i = 0; i < moneyValues.length; i++) {
+            int count = money / moneyValues[i];
+            money %= moneyValues[i];
+            results.add(count);
+        }
+
+        return results;
     }
 }
