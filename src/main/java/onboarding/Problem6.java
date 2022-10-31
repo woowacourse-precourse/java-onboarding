30ee04da626d46c882006d394bc1f507d8bebef8
swjeong98 <tree5678@konkuk.ac.kr>
2022-10-31 23:17:51 +0900

    문제 6 완전 탐색 버전으로 작성 중

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    // 기능 1
+    public static List<String> getSubString(String str, int len) {
+        List<String> ret = new ArrayList<>();
+
+        int limit = str.length() - len;
+        for(int i=0;i<=limit;i++) {
+            ret.add(str.substring(i, i+len));
+        }
+        return ret;
+    }
+    //기능 2
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        Set<String> result = new TreeSet<>();
+
+        for(int i=0;i<forms.size();i++){
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
+            if(nickname.length() == 1) continue;
+            for(int j=2;j<=nickname.length();j++){
+                List<String> target = getSubString(nickname, j);
+            }
+        }
+
         return answer;
     }
 }
