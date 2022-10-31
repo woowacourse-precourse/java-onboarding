fa1a39c193337ac21d24cf2538c9d7e3558ba1f9
nahyeon cho <whskgus12@nate.com>
2022-11-01 01:38:08 +0900

    Problem 4 - 대소문자 별로 변환하기
    알파벳 대문자는 정반대 대문자 알파벳으로, 알파벳 소문자는 정반대 소문자 알파벳으로 변환하는 기능입니다. 대,소문자가 구별되어 대문자면 upper 함수에 소문자면 lower 함수에 입력됩니다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,13 +1,36 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    private static Map<Character, Character> frogdict = new HashMap<>();
     public static String solution(String word) {
         if (isValid(word)) {
+            makeFrogDict();
             String answer = "";
             return answer;
         } else
             return ""; // word의 길이가 주어진 범위를 벗어났을때
     }
+    static void makeFrogDict(){
+        for (int i = 0; i < 25; i++) {
+            frogdict.put((char) ('A' + i), (char) ('Z' - i));
+        }
+    }
+
+    static char upperCaseAlpha(char alpha) {
+        // 대문자 알파벳 정반대로 바꾸기
+        return frogdict.get(alpha);
+    }
+
+    static char lowerCaseAlpha(char alpha) {
+        // 소문자 알파벳 정반대로 바꾸기
+        char alpha_value = frogdict.get(Character.toUpperCase(alpha));
+        alpha_value = Character.toLowerCase(alpha_value);
+        return alpha_value;
+    }
+
     static boolean isValid(String word) {
         int len = word.length();
         if (len < 1 || len > 1000)
