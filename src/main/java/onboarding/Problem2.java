f0e0ae156c5bd7da3fc392fd1fdf8703781169f7
brotherstone97 <brotherstone97@gmail.com>
2022-10-31 20:18:11 +0900

    Feat: 암호문을 리스트로 변환하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+    static List<Character> ConvertStringToList(String str){
+        List<Character> list_cryptogram = new ArrayList<Character>();
+        for (char c: str.toCharArray()){
+            list_cryptogram.add(c);
+        }
+        return list_cryptogram;
+    }
+
     public static String solution(String cryptogram) {
+        List<Character> list_cryptogram = ConvertStringToList(cryptogram);
         String answer = "answer";
         return answer;
     }
