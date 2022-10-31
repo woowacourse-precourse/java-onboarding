24ed709cd4e1a97fe77ef3a495404a3a6ec39d3d
5onchangwoo <scw9410@gmail.com>
2022-10-31 12:13:55 +0900

    problem6/두 글자씩 잘라서 set으로 생성하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    // 2글자씩 잘라서 set으로 만드는 기능
+    public static HashSet<Integer> makeHashList(String nickname) {
+        HashSet<Integer> hashList = new HashSet<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String substring = nickname.substring(i, i + 2);
+            int hashCode = substring.hashCode();
+            hashList.add(hashCode);
+        }
+        return hashList;
     }
 }
