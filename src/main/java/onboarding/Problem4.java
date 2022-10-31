97dc927ccad197df471b3802a74b1a1bf1994833
Hyes-y <gptjs7970@naver.com>
2022-10-31 21:42:31 +0900

    feat: Problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] chars = word.toCharArray();
+        for (char v: chars) {
+            int ascii = (int)v;
+            char ch = v;
+            if (ascii >= 65 && ascii <= 90) {
+                ch = (char)(155 - ascii);
+            } else if (ascii >= 97 && ascii <= 122) {
+                ch = (char)(219 - ascii);
+            }
+
+            answer = answer + ch;
+        }
         return answer;
     }
 }
+// A: 65, Z: 90
+// a: 97, z: 122
