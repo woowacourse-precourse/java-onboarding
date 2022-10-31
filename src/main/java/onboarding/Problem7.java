639ad893e79a508944eed835fe9941fc37e1d2da
YooByeongIk <quddlr96@naver.com>
2022-10-31 16:06:31 +0900

    [Problem7] docs : 기능 목록 주석 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import javax.swing.*;
+import java.util.*;
 
+/*
+##기능 목록
+1. 모든 사용자의 아이디(Key : String)와 친구 목록(Value : List<String>)을 HashMap에 저장
+2. 1번 규칙. 주어진 사용자의 친구가 아닐 때, 함께 아는 친구의 수를 계산해 점수 계산
+3. 2번 규칙. 방문자 중, 사용자의 친구가 아닐 때, 점수 계산
+4. 결과 List 정렬 및 Resize
+
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
