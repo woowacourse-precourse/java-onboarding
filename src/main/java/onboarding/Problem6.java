25dcbfcce647e1203e8035457d2fdf8c02fbff9f
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 01:15:33 +0900

    feat: count the number of consecutive substrings of length 2 or more
    - 하나의 닉네임에서 연속된 2글자 이상의 부분문자열 사용횟수 구하는
      기능추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,6 +13,14 @@ public class Problem6 {
         return answer;
     }
 
+    // 현재 닉네임에서 2글자 이상 연속된 부분문자열의 사용횟수
+    public static void countSubstring(Map<String, Integer> map, String nickname) {
+        // 문자열 개수
+        for (int i = 2; i < nickname.length(); i++) {
+            countSubStringOfLength(map, nickname, i);
+        }
+    }
+
     // 해당 길이 부분문자열 카운팅
     private static void countSubStringOfLength(Map<String, Integer> map, String nickname, int length) {
         // 시작점
