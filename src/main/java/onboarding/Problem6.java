95fdf90b4fc0fcdf7a605ae96d8a213ce0a7ba8e
mardi2020 <mardi@khu.ac.kr>
2022-10-31 15:34:54 +0900

    feat: Problem6 중복 패턴 검출 로직 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -11,6 +8,24 @@ public class Problem6 {
         Set<String> duplicatedPattern = new HashSet<>();
         HashMap<String, List<String>> patternEmail = new HashMap<>();
 
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            for(int i = 0; i < nickname.length()-1; i++) {
+                String pattern = nickname.charAt(i) + Character.toString(nickname.charAt(i+1));
+
+                if (duplicatedPattern.contains(pattern)) {
+                    answer.add(email);
+                    answer.addAll(patternEmail.get(pattern));
+                }
+                else {
+                    duplicatedPattern.add(pattern);
+                    patternEmail.put(pattern, new ArrayList<>());
+                    patternEmail.get(pattern).add(email);
+                }
+            }
+        }
 
         return null;
     }
