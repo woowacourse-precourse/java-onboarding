42859a00cf7b8d22b9207d8e49c5f8f08581f3b2
seheonCho <whtpgjs94@gmail.com>
2022-10-31 21:07:53 +0900

    feat: [문제4] 반환할 청개구리 문자 배열 생성
    문자를 변환하기 때문에 문자열이 아닌 문자 배열로 생성함

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,7 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         char[] wordToChars = word.toCharArray();
+        char[] reverseChars = new char[wordToChars.length];
 
         String answer = "";
         return answer;
