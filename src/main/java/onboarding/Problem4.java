18408c7f688323ec571aea6a93ddf51e4f5c58dd
3juhwan <13selfesteem91@naver.com>
2022-10-31 10:37:35 +0900

    update getCvtChar function

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,9 @@ package onboarding;
 
 public class Problem4 {
     public static char getCvtChar(char ch) {
-
+        if(ch>='a' && ch<='z') return (char)('a'+('z'-ch));
+        if(ch>='A' && ch<='Z') return (char)('A'+('Z'-ch));
+        return ch;
     }
 
     public static String getCvtWord(String word) {
