abe03cf4ee22f62b78a4665af5fa80811348f888
patronum98 <zkrhklkl98@gmail.com>
2022-10-31 19:31:00 +0900

    문제2 기능1 수정, 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,22 +1,19 @@
 package onboarding;
 
 // 기능 목록
-// 1. 문자열을 분석하기 위해 문자열을 배열로 변환하는 기능.
+// 1. 문자열을 분석하기 위해 문자열을 배열 -> 리스트 로 변환하는 기능.
 // 2. 배열에서 연속된 중복된 문자들을 제거하는 기능.
-// 2-1. 반복문을 사용해 String cryptogram 을 char[] gram_char 로 변경
-// 2-2. gram_char 의 요소들을 비교하면서 연속되면서 중복되는 문자들을 찾는다
-// 2-3. 제거한다.
 // 3. 배열을 다시 문자열로 변환한 뒤 결과를 반환한다.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        String[] str = cryptogram.split("");
+        List<String> strList = new ArrayList<>(Arrays.asList(str));
 
-        char[] gram_char = new char[cryptogram.length()];
-
-        // char배열로 만듬
-        for (int i =0; i < gram_char.length; i++) {
-            gram_char[i] = cryptogram.charAt(i);
-        }
 
         String answer = "answer";
         return answer;
