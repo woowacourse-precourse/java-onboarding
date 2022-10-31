7d75b82ce8db28af76532c30146688165280299d
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-31 18:24:18 +0900

    fix: check if character is alphabet

--- src/main/java/onboarding/problem4/AlphabetReverser.java
+++ src/main/java/onboarding/problem4/AlphabetReverser.java
@@ -6,10 +6,12 @@ public class AlphabetReverser {
         StringBuilder builder = new StringBuilder(word);
         for (int i = 0; i < builder.length(); i++) {
             char alphabet = builder.charAt(i);
-            if (Character.isUpperCase(alphabet))
-                builder.setCharAt(i, (char) (155 - alphabet));
-            else if (Character.isLowerCase(alphabet))
-                builder.setCharAt(i, (char) (219 - alphabet));
+            if (Character.isAlphabetic(alphabet)) {
+                if (Character.isUpperCase(alphabet))
+                    builder.setCharAt(i, (char) (155 - alphabet));
+                else if (Character.isLowerCase(alphabet))
+                    builder.setCharAt(i, (char) (219 - alphabet));
+            }
         }
         return builder.toString();
     }
