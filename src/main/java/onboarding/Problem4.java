04d854e1d9cb0a98ef032dbe9028413ef5b63f98
yw824 <yongwoo25@gmail.com>
2022-10-31 15:46:23 +0900

    feat: [Problem4] 기능 설명
    - solution():
    	- 청개구리 문자열을 작성하는 함수에게 입력받은 문자열을 전달하고
    	- 해당 함수에서 새로 생성한 청개구리 문자열을 받아서 그대로 반환한다.
    - 청개구리 문자열을 반환하는 함수:
    	- 함수명: getReversedString(String word)
    	- 초기에 빈 문자열을 생성한다.
    	- 만약 대문자라면? : 대문자 반대로 변환하여 문자열에 추가한다.
    	- 만약 소문자라면? : 소문자 반대로 변환하여 문자열에 추가한다.
    	- 다른 종류의 문자라면? 그대로 문자열에 추가한다.
    	- ( 아스키코드 참조 )
    	- 완성된 새로운 문자열을 청개구리 문자열로 반환한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        // 거꾸로 변환된 청개구리 문자열을 얻어 반환한다.
+        // implement
         return answer;
+    }
+
+    static String getReversedString(String word) {
+        // implement
+
+        /* start of loop */
+
+        // 만약 대문자라면 ? ( 65 <= 아스키코드 <= 90 )
+
+        // 만약 소문자라면 ? ( 97 <= 아스키코드 <= 122 )
+
+        // 다른 종류의 문자라면 ? -> 그대로 추가
+
+
+        /* end of loop */
+
+        return "";
     }
 }
