05ee8f8af3491f4bc1d3cb89833c29b8b3b9625e
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-11-01 00:45:57 +0900

    REFACTOR : indent 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,29 +1,19 @@
 package onboarding;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Problem2 {
-    private static Queue<Integer> queue = new LinkedList<>();
-
-    private static void indexOfDuplicatedChar(String cryptogram) {
+    private static int indexOfDuplicatedChar(String cryptogram) {
         for(int i=0; i<cryptogram.length()-1; i++) {
             if(cryptogram.charAt(i) == cryptogram.charAt(i+1))
-                queue.offer(i);
+                return i;
         }
+        return -1;
     }
-
     public static String solution(String cryptogram) {
         while(true) {
-            indexOfDuplicatedChar(cryptogram);
-            if(queue.isEmpty())
+            int idx = indexOfDuplicatedChar(cryptogram);
+            if(idx == -1) // 중복 문자가 없는 경우
                 break;
-            int temp = 0;
-            while(!queue.isEmpty()) {
-                int idx = queue.poll();
-                cryptogram = cryptogram.substring(0,idx - temp*2) + cryptogram.substring(idx+2 - temp*2);
-                temp++;
-            }
+            cryptogram = cryptogram.substring(0,idx) + cryptogram.substring(idx+2);
         }
         return cryptogram;
     }
