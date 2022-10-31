11e697667015a86049003eef49bd13823369b6cf
iamjooon2 <iamjooon2@gmail.com>
2022-10-31 21:14:15 +0900

    feat(problem6): 나올 수 있는 모든 문자열을 저장하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        ArrayList<String> answer = new ArrayList<>();
+        HashMap<String, Integer> nameCount = new HashMap<>();
+
+        for (int i = 0; i < forms.size(); i += 1) {
+            for (int j = 0; j < forms.get(i).get(1).length() - 1; j += 1) {
+                String slicedName = forms.get(i).get(1).substring(j, j + 2);
+
+            }
+        }
+
         return answer;
     }
 }
