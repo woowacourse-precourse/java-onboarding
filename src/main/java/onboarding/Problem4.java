929b161439c93574717b582f02f4ef70f4a4a103
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 21:51:26 +0900

    [Problem 4] Overview

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        /*
+        1. 알파벳 내의 문자에 대해서만 반대로 변환하는 함수 작성
+        2. 문자열을 순회하며 변환 후 반환할 문자열에 누적
+        */
         return answer;
     }
 }
