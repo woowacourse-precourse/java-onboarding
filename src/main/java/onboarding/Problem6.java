8f58046b73594b154b7a6877e476f17b0f26bb34
채희찬 <eqholic1125@gmail.com>
2022-10-31 23:40:36 +0900

    feat: 닉네임 비교 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+
+
         return answer;
     }
+
+    private static boolean checkSimilarNickname(String name1, String name2) {
+        for (int i = 0; i < name1.length() - 1; i++) {
+            for (int j = 0; j < name2.length() - 1; j++) {
+                if (name1.charAt(i) == name2.charAt(j) && name1.charAt(i + 1) == name2.charAt(j + 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
 }
