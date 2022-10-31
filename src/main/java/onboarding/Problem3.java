d0d8b71528d86a0e5cdda094254e63a4e72b457a
shpark30 <seohyeonpark30.gmail.com>
2022-10-31 23:11:16 +0900

    refact(Problem3.java): delete a useless varialbe and edit comment

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
+        /* 숫자마다 박수 카운트 누적 */
         int answer = 0;
         for (int i = 1; i <= number; i++) {
             answer += count369(i);
@@ -14,17 +15,14 @@ public class Problem3 {
     }
 
     static int count369(int number) {
-        // 3, 6, 9와 매칭되는 자리수 개수를 반환
+        /* 3, 6, 9와 매칭되는 자리수 개수를 반환 */
         List<Integer> condition = new ArrayList<>(Arrays.asList(3, 6, 9));
         int count = 0;
-        int remainder = 0;
-        while (number >= 3) {
-            // number를 10으로 나눈 나머지 remainder가 3, 6, 9에 해당하면 count한다.
-            remainder = number % 10;
-            number /= 10;
-            if (condition.indexOf(remainder) != -1) {
+        while (number > 0) {
+            if (condition.contains(number % 10)) {  // 끝 자리수 숫자가 3, 6, 9에 해당하면
                 count += 1;
             }
+            number /= 10;   // 남은 자리수
         }
         return count;
     }
