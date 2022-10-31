0dcb2d695d346223a44a9d9d1ba9677a4063f31c
patronum98 <zkrhklkl98@gmail.com>
2022-11-01 00:22:09 +0900

    문제4 기능목록 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,10 +1,9 @@
 package onboarding;
 
-//기능1. 문장을 한 글자씩 리스트에 저장한다.
-//기능2. 리스트에 있는 알파벳을 아스키코드로 변환한다.
-//기능3. 아스키코드에서 -65를 하여 몇번째 알파벳인지 구한다(N)
-//기능4. 반대문자 = 원래문자+(25-2N)을 하여 반대문자를 구한 뒤 리스트에 ADD
-//기능5. 리스트를 문자열로 변환 후 반환한다.
+//기능1. 알파벳을 아스키코드로 변환한다.
+//기능2. 아스키코드에서 -65를 하여 몇번째 알파벳인지 구한다(N)
+//기능3. 반대문자 = 원래문자+(25-2N)을 하여 반대문자를 구한 뒤 리스트에 ADD
+//기능4. 리스트를 문자열로 변환 후 반환한다.
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,10 +12,9 @@ import java.util.List;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        String[] str = word.split("");
-        List<String> strList = new ArrayList<>(Arrays.asList(str));
 
-        System.out.println(strList);
+        System.out.println((int)word.charAt(0)+25-2*((int)word.charAt(0)-65));
+        System.out.println((char)82);
         return answer;
     }
 }
