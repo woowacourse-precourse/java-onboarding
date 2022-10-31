c9aa130f8541cecb8a500666a4e998c3f393628c
plusjob70 <plusjob70@gmail.com>
2022-10-31 13:14:30 +0900

    feat: 문자열에서 연속 중복 문자가 없을 때까지 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        return "";
+        boolean changed;            // 처리 전 문자열과 처리 후 문자열의 변화를 감지
+        boolean[] duplicateChar;    // 연속으로 나오는 문자
+        StringBuilder result;       // 처리 후 문자열
+
+        do {
+            changed = false;
+            duplicateChar = checkDuplicate(cryptogram);
+
+            result = new StringBuilder();
+            for (int i = 0; i < cryptogram.length(); i++) {
+                if (!duplicateChar[i]) {
+                    result.append(cryptogram.charAt(i));
+                } else {
+                    changed = true;
+                }
+            }
+
+            cryptogram = result.toString();
+
+        } while (changed);
+
+        return cryptogram;
     }
 
     private static boolean[] checkDuplicate(String str) {
