51005b48800a844d918831b614ad784671fd9f99
yellowchili <yunpaper@yu.ac.kr>
2022-10-31 23:10:42 +0900

    [Problem1]Add function that finds maximum number in each book

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +14,37 @@ class Problem1 {
         return answer;
     }
 
-    /* 주어진 리스트로 만들 수 있는 가장 큰 수를 구하는 함수 */
+    /* 분리된 자릿수로 만들 수 있는 가장 큰 수를 구하는 함수 */
     private static int getMax(List<Integer> book) {
-        int max = 0;
+        int max;
         int left, right;
+        int lsum=0, lmul=1, rsum=0, rmul=1;
+
+        left = book.get(0);
+        right = book.get(1);
+
+        ArrayList<Integer> leftList = splitNum(left);
+        ArrayList<Integer> rightList = splitNum(right);
+
+        for(int i=0; i<leftList.size(); i++) {
+            int tmp = leftList.get(i);
+            lsum += tmp;
+            lmul *= tmp;
+        }
+        for(int i=0; i<rightList.size(); i++) {
+            int tmp = rightList.get(i);
+            rsum += tmp;
+            rmul *= tmp;
+        }
+
+        max = Math.max(lsum, lmul);
+        max = Math.max(max, rsum);
+        max = Math.max(max, rmul);
 
         return max;
     }
 
-    /* 각 자릿수를 구하는 함수 */
+    /* 각 자릿수를 분리하는 함수 */
     private static ArrayList<Integer> splitNum(int num) {
         ArrayList<Integer> result = new ArrayList<>();
 
