0fed68b1262ac3e7498311cc40cb30a17a5d40cc
JungWoo <rlawjddn103@naver.com>
2022-10-31 16:25:43 +0900

    feat: Add Problem7 findUserFriends function

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<String> findUserFriends(String user, List<List<String>> friends){
+
+        List<String> userFriends = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            if (Objects.equals(friend.get(0), user)) {
+                userFriends.add(friend.get(1));
+            }
+            if (Objects.equals(friend.get(1), user)) {
+                userFriends.add(friend.get(0));
+            }
+        }
+
+        return userFriends;
     }
 }
