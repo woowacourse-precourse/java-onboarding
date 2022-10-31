14ec29ca7d76e64b856b1b2a26114efc1ebf62f7
Sunghyun-0 <zkdldk95@naver.com>
2022-10-31 22:42:34 +0900

    (Problem7)친구확인기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,17 @@ public class Problem7 {
             recommendPoint.put(target,0);
         }
         recommendPoint.replace(target, recommendPoint.get(target)+ point);
+    }
+
+    public static List<String> getUserFriends(String user, List<List<String>> friends){
+        List<String> userFriends = new ArrayList<>();
+        for(int i=0; i<friends.size(); i++){
+            if(friends.get(i).contains(user)){
+                String userFriend = getAnotherOne(friends.get(i),user);
+                userFriends.add(userFriend);
+            }
+        }
+        return userFriends;
     }
     
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
