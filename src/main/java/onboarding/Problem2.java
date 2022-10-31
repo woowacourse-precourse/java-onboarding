5b23f9560cf2ae432b3f06cddf7360a76c2768ba
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:20:38 +0900

    feat: stack to string

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -20,4 +20,16 @@ public class Problem2 {
         }
         return stack;
     }
+
+    private static String makeStackToString(Stack<Character> stack) {
+        int size = stack.size();
+        String[] letterArray = new String[size];
+        size--;
+
+        while (!stack.empty()) {
+            char letter = stack.pop();
+            letterArray[size--] = String.valueOf(letter);
+        }
+        return String.join("", letterArray);
+    }
 }
