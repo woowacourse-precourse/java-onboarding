d9b875ea77747cb012a9ed5bd11d055b06616a9f
eil-you <qldirl123@gmail.com>
2022-10-31 23:54:00 +0900

    specialSeparate : 알파벳과 특수문자 구분 letterSeparate : 대소문자 구분 remove : 알파벳 반대로 출력

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,55 +2,92 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        StringBuilder answer = new StringBuilder();
-        //String[] letter = word.split("");
+
+        String answer = "";
 
         for(int i = 0; i < word.length() ; i++){
-           if(word.charAt(i)<65){
-               answer.append(word.charAt(i));
-               continue;
-           }
-           if(word.charAt(i)>=65 && word.charAt(i)<=97 ){
-               answer.append(reverse(word.charAt(i)));
-               continue;
-           }
-           if (word.charAt(i)<97){
-               answer.append(word.charAt(i));
-               continue;
-           }
-           if(word.charAt(i)<=122){
-               answer.append(reverse(word.charAt(i)));
-               continue;
-           }
-           if(word.charAt(i)>122){
-               answer.append(word.charAt(i));
-           }
 
+            answer += specialSeparate(word.charAt(i));
 
-
-        }
-
-        return answer.toString();
-    }
-    public static char reverse(char word){
-        char answer = 0;
-        if( word <= 90 && word>=65){
-            for(int i = 0; i<=25 ; i++){
-                if (word == (65+i) ){
-                    answer += (char)(65+(25-i));
-                    break;
-                }
-            }
-        }
-        else if(word >=97 &&word <=122) {
-            for (int i = 0; i <= 25; i++) {
-                if (word == (97 + i)) {
-                    answer += (char) (97 + (25- i));
-                    break;
-                }
-            }
         }
 
         return answer;
+    }
+
+    public static String specialSeparate(char word){
+
+        char answer = 0;
+
+        if(word<65){
+
+            answer = word;
+
+        }
+
+        if((word>=65) && (word<=90) ){
+
+            answer =letterSeparate(word);
+
+        }
+        if ((word>90) &&(word<97)){
+
+            answer = word;
+
+        }
+        if((word>=97) && (word<=122)){
+
+            answer =letterSeparate(word);
+
+        }
+
+        if(word>122){
+
+            answer = word;
+
+        }
+
+        return String.valueOf(answer);
+
+    }
+    public static char letterSeparate(char word){
+
+        char answer=0;
+
+        int firstCode = 0;
+
+        if( (word <= 90) && (word>=65)){
+
+            firstCode = 65;
+
+        }
+
+        if((word >=97) &&(word <=122)) {
+
+            firstCode = 97;
+
+        }
+
+        answer = reverse(word, firstCode);
+
+
+        return answer;
+    }
+    public static char reverse(int word, int firstCode){
+
+            char answer =0;
+
+            for (int i = 0; i <= 25; i++) {
+
+                if (word == (firstCode + i)) {
+
+                    answer += (char) (firstCode + (25- i));
+
+                    break;
+
+                }
+            }
+
+            return answer;
+
     }
 }
