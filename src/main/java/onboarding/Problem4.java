d2e7a5bf2b1b639d84ea733dde2c8f77c397eace
Amenable-C <duswo0624@naver.com>
2022-11-01 00:39:08 +0900

    feat : [문제4] 청개구리 사전 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        HashMap<Character, Character> map = new HashMap<>();
+        for(int i = 0; i < 27; i++){
+            map.put((char) ('a' + i), (char) ('z' - i));
+            map.put((char) ('A' + i), (char) ('Z' - i));
+        }
+
         return answer;
     }
 }
