95f0e34f1527865830f9b7a98eaf027bc22b2fac
ys200209 <lys200209@naver.com>
2022-10-31 17:52:30 +0900

    refactor(문제1): 예외 타입 변경 IllegalStateException -> IllegalArgumentException

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,7 +8,7 @@ class Problem1 {
         try {
             validate(pobi);
             validate(crong);
-        } catch (IllegalStateException ex) {
+        } catch (IllegalArgumentException ex) {
             return -1;
         }
 
@@ -58,24 +58,24 @@ class Problem1 {
 
     private static void validate(List<Integer> pages) {
         if (pages.size() != 2) {
-            throw new IllegalStateException("페이지는 총 2쪽이어야 합니다. 현재 페이지 수 : " + pages.size());
+            throw new IllegalArgumentException("페이지는 총 2쪽이어야 합니다. 현재 페이지 수 : " + pages.size());
         }
 
         if (pages.get(0) % 2 != 1) {
-            throw new IllegalStateException("왼쪽 페이지가 홀수가 아닙니다. 왼쪽 페이지 : " + pages.get(0));
+            throw new IllegalArgumentException("왼쪽 페이지가 홀수가 아닙니다. 왼쪽 페이지 : " + pages.get(0));
         }
 
         if (pages.get(1) % 2 != 0) {
-            throw new IllegalStateException("오른쪽 페이지가 짝수가 아닙니다. 오른쪽 페이지 : " + pages.get(1));
+            throw new IllegalArgumentException("오른쪽 페이지가 짝수가 아닙니다. 오른쪽 페이지 : " + pages.get(1));
         }
 
         if (pages.get(1) <= pages.get(0) || pages.get(1) - pages.get(0) != 1) {
-            throw new IllegalStateException("페이지가 잘못되었습니다.");
+            throw new IllegalArgumentException("페이지가 잘못되었습니다.");
         }
 
         for (Integer page : pages) {
             if (page < 1 || page > 400) {
-                throw new IllegalStateException("페이지의 범위가 잘못되었습니다. 올바른 범위 : 1 ~ 400, 현재 페이지 : " + page);
+                throw new IllegalArgumentException("페이지의 범위가 잘못되었습니다. 올바른 범위 : 1 ~ 400, 현재 페이지 : " + page);
             }
         }
     }
