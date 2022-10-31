a3091aecc91f704966b549a0b05792b9f6aa5c54
lacram <chemin9898@gmail.com>
2022-11-01 00:03:20 +0900

    Problem 2 - 기능 목록 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/*
+ * 기능 목록
+ * 1. 연속하는 중복 문자의 마지막 인덱스 찾기
+ * 2. 중복 문자 삭제 및 반복
+ * */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
