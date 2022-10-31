ddd1c5140b3d7a94c834fb6a240aeb74eedf10a7
chanminmaxscaler <jcm04@naver.com>
2022-10-31 21:12:44 +0900

    feat(Main): makeCollectionCrews 메소드의 기능을 구현한다
    makeCollectionCrews 메소드는 이메일과 닉네임을 저장한 2차원 리스트를 입력받으면, Crew 객체를 생성하고 그를 저장한 Crews 일급 컬렉션을 반환한다.

--- src/main/java/onboarding/problem6/Main.java
+++ src/main/java/onboarding/problem6/Main.java
@@ -1,9 +1,25 @@
 package onboarding.problem6;
 
+import onboarding.problem6.collection.Crews;
 import onboarding.problem6.domain.Crew;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public Crew makeCrewByEmailAndNickname(String email, String nickname) {
         return new Crew(email, nickname);
+    }
+
+    public Crews makeCollectionCrews(List<List<String>> forms) {
+        List<Crew> crewList = new ArrayList<>();
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            Crew crew = makeCrewByEmailAndNickname(email, nickname);
+            crewList.add(crew);
+        }
+        Crews crews = new Crews(crewList);
+        return crews;
     }
 }
