c61ec4d24608a2204ab02b2e294ccb952461d111
heohyeonjink <hyeonjin4870@naver.com>
2022-11-01 00:01:44 +0900

    feat(PROBLEM4) : 알파벳 변환 함수 생성 및 결론 도출
    각 문자마다 대/소문자를 따로 구별하고, 아스키코드의 성질을 이용하여 구현하였다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-
+        StringBuffer sb = new StringBuffer();
+        for(Character x : word.toCharArray()){
+            if(x==' ') sb.append(" ");
+            else sb.append(AlphabetChange(x));
+        }
+        answer = sb.toString();
         return answer;
+    }
+
+    public static char AlphabetChange(char c){
+        if(Character.isUpperCase(c)) return (char)('Z'-(c-'A'));
+        else return (char)('z'-(c-'a'));
     }
 }
