c86610000386f966979a8274a22145dcf8d9d781
ohhyungsuh <ohhyungsuh@gmail.com>
2022-11-01 00:26:14 +0900

    Problem3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=2; i<=number; i++){
+            int tmp = i;
+            while(tmp > 0){
+                if((tmp % 10 == 3) || (tmp % 10 == 6) || (tmp % 10 == 9)){
+                    answer++;
+                }
+                tmp /= 10;
+            }
+        }
         return answer;
     }
 }
