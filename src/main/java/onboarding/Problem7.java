76512da1a3ef308dfdd6c3c230ae949bf0a1f683
Go-Jaecheol <gojaech@naver.com>
2022-10-31 23:53:57 +0900

    feat: Problem7 - 사용자 아이디를 key, 해당 사용자의 친구들을 value로 가지는 HashMap 생성 후 추가하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String, Set<String>> friendsMap;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        friendsMap = new HashMap<>();
+
+        friendsMap.put(user, new HashSet<>());
+        for (List<String> friend : friends) {
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+            addFriendsToMap(friend1, friend2);
+            addFriendsToMap(friend2, friend1);
+        }
+
         return answer;
+    }
+
+    private static void addFriendsToMap(String keyFriend, String valueFriend) {
+        if (!friendsMap.containsKey(keyFriend)) {
+            friendsMap.put(keyFriend, new HashSet<>());
+        }
+        friendsMap.get(keyFriend).add(valueFriend);
     }
 }
