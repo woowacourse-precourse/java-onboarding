d3d43c9d2aa1c16a4133b8d432e999267dc2abe4
akskflwn <akskflwn@gmail.com>
2022-10-31 19:51:16 +0900

    (refactor) Problem7 스트림 추가,리펙토링

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,17 +3,65 @@ package onboarding;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.util.Comparator.*;
-
 public class Problem7 {
     static Map<String, Integer> points;
-    static String userName;
-    static List<String> userFriendList;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        init(user, friends, visitors);
-        List<String> answer = new ArrayList<>(points.keySet());
+        points = new HashMap<>();
 
+        Map<String, List<String>> relationMap = getRelationships(friends, new HashMap<>());
+        addRelationPoint(relationMap, user);
+        addVisitorPoint(relationMap, visitors, user);
+
+        List<String> answer = new ArrayList<>(points.keySet());
+        sort(answer);
+
+        return answer.stream().limit(5).filter(s -> !s.equals(user)).collect(Collectors.toList());
+    }
+
+
+    private static Map<String, List<String>> getRelationships(List<List<String>> friends, HashMap<String, List<String>> relationMap) {
+        for (List<String> friend : friends) {
+            if (!relationMap.containsKey(friend.get(0))) {
+                relationMap.put(friend.get(0), new ArrayList<>());
+            }
+            if (!relationMap.containsKey(friend.get(1))) {
+                relationMap.put(friend.get(1), new ArrayList<>());
+            }
+            relationMap.get(friend.get(0)).add(friend.get(1));
+            relationMap.get(friend.get(1)).add(friend.get(0));
+        }
+        return relationMap;
+
+    }
+
+    private static void addRelationPoint(Map<String, List<String>> relationMap, String user) {
+        relationMap.get(user).stream().map(relationMap::get).forEach(friends -> friends.forEach(Problem7::addRelationPoint));
+    }
+
+    private static void addRelationPoint(String s) {
+        if (points.containsKey(s)) {
+            points.put(s, points.get(s) + 10);
+        } else {
+            points.put(s, 10);
+        }
+    }
+
+    private static void addVisitorPoint(Map<String, List<String>> relationMap, List<String> visitors, String user) {
+        for (String visitor : visitors) {
+            if (relationMap.get(user).contains(visitor)) {
+                continue;
+            }
+            if (points.containsKey(visitor)) {
+                points.put(visitor, points.get(visitor) + 1);
+            } else {
+                points.put(visitor, 1);
+            }
+
+        }
+    }
+
+    private static void sort(List<String> answer) {
         answer.sort((o1, o2) -> {
             if (points.get(o1) < points.get(o2)) return 1;
             else if (points.get(o1) > points.get(o2)) return -1;
@@ -24,74 +72,7 @@ public class Problem7 {
             }
             return 0;
         });
-
-        List<String> collect = answer.stream().limit(5).collect(Collectors.toList());
-        return collect;
-    }
-
-    private static void init(String user, List<List<String>> friends, List<String> visitors) {
-        points = new HashMap<>();
-        userName = user;
-        userFriendList = getFriends(userName, friends);
-        addPoints(getRecommendFriends(userFriendList, friends));
-        addVisitorPoint(visitors);
-    }
-
-    private static void addVisitorPoint(List<String> visitors) {
-        for (String s : visitors) {
-            if (userFriendList.contains(s)) {
-                continue;
-            }
-            if (points.containsKey(s)) {
-                points.put(s, points.get(s) + 1);
-            } else {
-                points.put(s, 1);
-            }
-
-        }
-    }
-
-    private static void addPoints(List<String> list) {
-        for (int i = 0; i < list.size(); i++) {
-            if (points.containsKey(list.get(i))) {
-                points.put(list.get(i), points.get(list.get(i)) + 10);
-            } else points.put(list.get(i), 10);
-        }
-    }
-
-    private static List<String> getRecommendFriends(List<String> userFriendList, List<List<String>> friends) {
-        List<String> list = new ArrayList<>();
-        for (String s : userFriendList) {
-            List<String> list2 = getFriends(s, friends);
-            for (int i = 0; i < list2.size(); i++) {
-                if (list2.get(i).equals(userName)) {
-                    continue;
-                }
-                if (!userFriendList.contains(list2.get(i))) {
-                    list.add(list2.get(i));
-                }
-            }
-
-        }
-        return list;
-
     }
 
 
-    private static List<String> getFriends(String user, List<List<String>> friends) {
-        List<String> friendList = new ArrayList<>();
-        for (List s : friends) {
-            if (s.contains(user)) {
-                friendList.add(OnlyFriend(s, user));
-            }
-        }
-        return friendList;
-    }
-
-    private static String OnlyFriend(List s, String user) {
-        if (s.get(0).equals(user)) {
-            return (String) s.get(1);
-        }
-        return (String) s.get(0);
-    }
 }
