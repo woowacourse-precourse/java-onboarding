2b3f980a5f5c0dc5d3a88597ed774a850b411fae
LeeJinHo <hosboy93@naver.com>
2022-10-31 09:18:48 +0900

    문제2. String -> Char로 쪼개 배열에 저장

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        ArrayList<Character> password = new ArrayList<>();
+        
+        for(int i=0; i<cryptogram.length(); i++) {
+        	password.add(cryptogram.charAt(i));
+        }
         return answer;
     }
 }
