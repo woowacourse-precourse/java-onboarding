e8fddd38cf45da29b976d5f12b3bfc04b1188ec4
YooDongSeon <inko51366@naver.com>
2022-10-31 19:48:25 +0900

    기능구현 후 커밋

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem4 {
     /*
         기능목록 :
@@ -11,6 +15,33 @@ public class Problem4 {
      */
     public static String solution(String word) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        List<String> upperList = new ArrayList<>(Arrays.asList("Z", "Y", "X", "W", "V", "U",
+                                                                "T", "S", "R", "Q", "P", "O",
+                                                                "N", "M", "L", "K", "J", "I",
+                                                                "H", "G", "F", "E", "D", "C",
+                                                                "B", "A"));
+
+        List<String> lowerList = new ArrayList<>(Arrays.asList("z", "y", "x", "w", "v", "u",
+                                                                "t", "s", "r", "q", "p", "o",
+                                                                "n", "m", "l", "k", "j", "i",
+                                                                "h", "g", "f", "e", "d", "c",
+                                                                "b", "a"));
+
+        for(int i : word.toCharArray()) {
+            if(i == 32) {
+                sb.append(" ");
+            }
+            else if(i >= 65 && i <= 90) {
+                sb.append(upperList.get(i - 65));
+            }
+            else if(i >= 97 && i <= 122) {
+                sb.append(lowerList.get(i - 97));
+            }
+        }
+
+        answer = sb.toString();
 
         return answer;
     }
