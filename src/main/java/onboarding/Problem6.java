c76b21acd6dafcbe53b81881eaa57664514cdc49
yun8565 <yunyun8565@naver.com>
2022-11-01 02:54:30 +0900

    feat(P6): 해시맵 변환 기능 추가
    최종 반환 값은 이메일 리스트이기 때문에
    닉네임을 key, 이메일을 value로 해시맵에 저장하면 중복 문자가 있는 닉네임만 알아도 이메일과 매칭할 수 있다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static final int EMAIL = 0;
+    public static final int NICKNAME = 1;
+
+    public static HashMap<String, String> crews;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        crews = toHashMap(forms);
+
         return answer;
+    }
+
+    public static HashMap<String, String> toHashMap(List<List<String>> forms) {
+        HashMap<String, String> crewInfo = new HashMap<>();
+        for(List<String> crew : forms) {
+            crewInfo.putIfAbsent(crew.get(NICKNAME), crew.get(EMAIL));
+        }
+        return crewInfo;
     }
 }
