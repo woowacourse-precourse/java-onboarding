f67eb14a676830eb341825767c87247eeb555d1e
lhyeonj120 <isk04387@gmail.com>
2022-11-01 04:00:52 +0900

    [problem3] feat: 손뼉 횟수 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,8 +6,25 @@ public class Problem3 {
         
         for(int i = 1; i <= number; i++) {
         	String numStr = Integer.toString(i);
+        	answer += getClapCnt(numStr);
         }
         
         return answer;
+    }
+    
+    private static int getClapCnt(String s) {
+    	int res = 0;
+    	
+    	for(int i = 0; i < s.length(); i++) {
+    		if(s.charAt(i) == '3') {
+    			res++;
+    		} else if(s.charAt(i) == '6') {
+    			res++;
+    		} else if(s.charAt(i) == '9') {
+    			res++;
+    		}
+    	}
+    	
+    	return res;
     }
 }
