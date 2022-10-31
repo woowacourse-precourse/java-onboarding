f81ab7628c1cb3d5658862337d9d6b5d69a9ebf7
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-10-31 14:57:14 +0900

    problem01 : page entity dev

---
+++ src/main/java/onboarding/pagenumbergame/entity/Page.java
@@ -0,0 +1,36 @@
+package onboarding.pagenumbergame.entity;
+
+public class Page {
+
+  private Integer page;
+
+  private Page(Integer page) {
+    this.page = page;
+  }
+
+  public static Page from(int page) {
+    if (page < 1 || page > 400) {
+      return new Page(-1);
+    }
+    return new Page(page);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Page)) {
+      return false;
+    }
+
+    Page page1 = (Page) o;
+
+    return page != null ? page.equals(page1.page) : page1.page == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return page != null ? page.hashCode() : 0;
+  }
+}
