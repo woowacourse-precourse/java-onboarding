44c64d96aebeac2c7d0b1e54e9d89ffa524373cc
twoosky <lhjae3@naver.com>
2022-11-01 02:07:01 +0900

    feat(problem3): 입력값이 1~10000 사이 값이 아닌 경우 예외 처리

--- src/main/java/problem3/Validator.java
+++ src/main/java/problem3/Validator.java
@@ -9,12 +9,19 @@ public class Validator {
 
     public void validate() {
         validateNaturalNumber();
+        validateRange();
     }
 
     private void validateNaturalNumber() {
         String number = String.valueOf(this.number);
         if(!number.matches("^[0-9]*$")) {
             throw new IllegalArgumentException("입력값은 자연수이어야 합니다.");
+        }
+    }
+
+    private void validateRange() {
+        if((number < 1) || (number > 10000)) {
+            throw new IllegalArgumentException("입력값은 1 이상 10000 이하이어야 합니다.");
         }
     }
 }
