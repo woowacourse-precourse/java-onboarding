77b30314c06a8e7f811bca6036204ac16d15387c
권민정 <22alswjd@naver.com>
2022-11-01 02:14:35 +0900

    feat: Problem5 기능 목록 주석 추가
    기능 목록을 작성한 주석 추가함.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        // 1. 화폐 금액 리스트 내림차순 선언
+        // 2. 각 금액이 반환될 수 있는 최대 갯수 구하기
+
         return answer;
     }
 }
