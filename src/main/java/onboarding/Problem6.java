40cd3da9f4586006dd5ca891153a70dfdfa05a88
desfox <desfox99@naver.com>
2022-10-31 22:33:24 +0900

    feat(Problem6): 각 크루원 닉네임 중 연속된 두 글자의 모든 조합을 구하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
+    public static final int EMAIL_IDX = 0;
+    public static final int NICKNAME_IDX = 1;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static void getSimilarNicknameUser(List<List<String>> forms) {
+        for (int i = 0; i <= forms.size() - 1; i++) {
+            String nickname = forms.get(i).get(NICKNAME_IDX);
+
+            for (int j = 0; j <= nickname.length() - 2; j++) {
+                char[] charArray = {nickname.charAt(j), nickname.charAt(j + 1)};
+                String continuous = String.valueOf(charArray);
+            }
+        }
     }
 }
