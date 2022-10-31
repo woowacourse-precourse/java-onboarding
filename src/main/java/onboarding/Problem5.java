92c709fa15250b0b8715292a5f5f07b4671ab109
LeeJinHo <hosboy93@naver.com>
2022-10-31 22:38:19 +0900

    문제5. 큰 단위부터 입력하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        
+        int[] wallet = new int[9];
+        int nowUnit = 50000;
+        int firstNum = 5;
+        
+        for(int i=0; i<wallet.length; i++) {
+        	wallet[i] = money/nowUnit;
+        	money = money%nowUnit;
+        }
+        
         return answer;
     }
 }
