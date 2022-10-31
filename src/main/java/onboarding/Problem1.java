76f43ef52bc192ae05e7a3a09a54ee12ecafc902
Gibeom Nam <rlqja0523@naver.com>
2022-11-01 03:17:32 +0900

    feat: Problem1 solved

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import onboarding.problem1.Page;
+import onboarding.problem1.PageException;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        boolean pobiException = PageException.checkException(pobi);
+        boolean crongException = PageException.checkException(crong);
 
+        if (pobiException || crongException) {
+            return result.EXCEPTION.getCode();
+        }
+
+        int pobiMaxPage = Page.getMyMaxPage(pobi);
+        int crongMaxPage = Page.getMyMaxPage(crong);
+
+        if (pobiMaxPage > crongMaxPage) {
+            return result.POBI_WIN.getCode();
+        } else if (pobiMaxPage < crongMaxPage) {
+            return result.CRONG_WIN.getCode();
+        } else {
+            return result.DRAW.getCode();
+        }
     }
 
     private enum result {
