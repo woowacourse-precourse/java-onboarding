fd44f78a7aad9e3704a739b07161203cf4604a67
hongjinwon <herenever@naver.com>
2022-10-31 22:56:30 +0900

    feat: Problem2 stackToString, getStringList, dostack 추가
    - Problem2 stackToString, getStringList, doStack 추가
    - doStack() 에서 조건마다
    push 하거나 pop 하는 로직 메소드 분리 필요

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
 
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static String stackToString(Stack<String> stack){
+        return stack.stream()
+                .reduce("",(a,b)->a+b);
+    }
+
+    public static List<String> getStringList(String cryptogram){
+        return Stream.of(cryptogram.split(""))
+                .collect(Collectors.toList());
+    }
+
+    public static Stack<String> doStack(List<String> stringList){
+        Stack<String> stack =new Stack<>();
+        for (String s : stringList) {
+            if (stack.isEmpty()){
+                stack.push(s);
+                continue;
+            }
+            if (stack.lastElement().equals(s)){
+                stack.pop();
+                continue;
+            }
+            stack.push(s);
+        }
+        return stack;
     }
 }
