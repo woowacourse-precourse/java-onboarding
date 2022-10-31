9b10e84971166e76810a4e1da4fd3a21d3e05a15
kksks3192 <kksks3192@gmail.com>
2022-10-31 15:32:25 +0900

    feat(problem1): 승자를 구하고 결과값을 반환하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,8 +6,11 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
+        answer = getWinner(pobi, crong);
         return answer;
     }
+
+
 
     public static int getScore(List<Integer> pages){
         int max_score=0;
@@ -26,6 +29,22 @@ class Problem1 {
             if(max_score<max) max_score = max;
         }
         return max_score;
+    }
+
+    public static int getWinner(List<Integer> pobi, List<Integer> crong){
+        int winner = Integer.MAX_VALUE;
+        int pobi_score = getScore(pobi);
+        System.out.println("pobi:"+ pobi_score);
+        int crong_score = getScore(crong);
+        System.out.println("crong:"+ crong_score);
+        if(pobi_score>crong_score){
+            winner = 1;
+        }else if(pobi_score<crong_score){
+            winner = 2;
+        }else if(pobi_score==crong_score){
+            winner = 0;
+        }
+        return winner;
     }
 
 }
