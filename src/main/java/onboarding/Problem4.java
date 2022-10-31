cca32f2f10f8f107bfec0dd00c8efd34461c325d
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-01 04:21:31 +0900

    [PROBLEM 4] 청개구리 사전 Map에 추가
    1. 알파벳 배열에 넣기
    2. HashMap 이용하여 key에서 알파벳순, value에는 알파벳 역순으로 넣기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] alphabetArr = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        Map<Character, Character> reverseMap = new HashMap();
+        for(int i=0; i<alphabetArr.length; i++) {
+            reverseMap.put(alphabetArr[i], alphabetArr[alphabetArr.length-1-i]);
+        }
+
         return answer;
     }
 }
