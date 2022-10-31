bdd6db804657bfbecd27ff7d2cb35bbf24e772c6
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-10-31 20:15:06 +0900

    [pb7] feat: case별로 score 계산

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,13 +5,21 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Set<String> userFriends = new HashSet<>();
-        friends.forEach(elem -> updateFriendsInSet(userFriends, user, elem.get(0), elem.get(1)));
+        friends.forEach(elem -> constructFriendsSet(userFriends, user, elem.get(0), elem.get(1)));
 
         List<String> friendsOfUserFriends = new ArrayList<>();
-        userFriends.forEach(elem -> updateFriendsInList(elem, friendsOfUserFriends, friends));
+        userFriends.forEach(elem -> constructFriendsList(elem, friendsOfUserFriends, friends));
+
+        Map<String, Integer> scoreOfFriends = new HashMap<>();
+        addScores(scoreOfFriends, userFriends, user, friendsOfUserFriends, visitors);
+        return getAnswer(scoreOfFriends);
+
     }
 
-    private static void updateFriendsInSet(Set<String> setFriends, String target, String friendA, String friendB) {
+    private static void constructFriendsSet(Set<String> setFriends,
+                                           String target,
+                                           String friendA,
+                                           String friendB) {
         if (friendA.equals(target)) {
             setFriends.add(friendB);
         }
@@ -20,10 +28,35 @@ public class Problem7 {
         }
     }
 
-    private static void updateFriendsInList(String target, List<String> listFriends, List<List<String>> friends) {
+    private static void constructFriendsList(String target,
+                                            List<String> listFriends,
+                                            List<List<String>> friends) {
         Set<String> targetFriends = new HashSet<>();
-        friends.forEach(elem -> updateFriendsInSet(targetFriends, target, elem.get(0), elem.get(1)));
+        friends.forEach(elem -> constructFriendsSet(targetFriends, target, elem.get(0), elem.get(1)));
         targetFriends.forEach(elem -> listFriends.add(elem));
+    }
+
+    private static Map<String, Integer> addScores(Map<String, Integer> scores,
+                                                  Set<String> userFriends,
+                                                  String user,
+                                                  List<String> commonFriends,
+                                                  List<String> visitors) {
+        commonFriends.forEach(item -> addCommonFriendsScores(scores, userFriends, item, user));
+        visitors.forEach(item -> scores.put(item, scores.containsKey(item) ? scores.get(item) + 1 : 1));
+        return scores;
+    }
+
+    private static void addCommonFriendsScores(Map<String, Integer> scores,
+                                               Set<String> userFriends,
+                                               String target,
+                                               String user) {
+        if (!userFriends.contains(target) && !target.equals(user)) {
+            scores.put(target, scores.containsKey(target) ? scores.get(target) + 10 : 0);
+        }
+    }
+
+    private static List<String> getAnswer(Map<String, Integer> scores) {
+
     }
 }
 
