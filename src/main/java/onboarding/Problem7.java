d5df19e4a7747c31d8a0ef450e5b885fb8bfb526
hojunking96 <junsong96@naver.com>
2022-11-01 01:15:38 +0900

    P7 feat (이름, 점수) 객체 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> alreadyFriends = new ArrayList<>();
+        List<ScoreList> scoreLists = new ArrayList<>();
+        HashMap<String, Integer> candidates = new HashMap<>();
+        
         return answer;
     }
+    public static class ScoreList {
+        String name;
+        private Integer score;
+        public ScoreList(String name, Integer score) {
+            this.name = name;
+            this.score = score;
+        }
+        public String getName() {
+            return name;
+        }
+        public Integer getScore() {
+            return score;
+        }
+    }
+
 }
