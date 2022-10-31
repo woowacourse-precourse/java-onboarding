ab319af278bc6ec3ac7af29634841e901214fa5c
hwanee__ <fkfkthth@naver.com>
2022-11-01 03:49:33 +0900

    2번문제_기능구현_완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,34 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        answer = check_duplication(cryptogram);
+
         return answer;
+    }
+
+    static String check_duplication(String str){
+        int i,j,check=0;
+        String result = "";
+
+        for (i = 0; i < str.length()-1; i++) {
+           for(j = i+1; j < str.length();j++){
+               if(str.charAt(i)!=str.charAt(j)) break;
+               else check=1;
+            }
+
+           if(check==1){
+               for(int z = 0; z < str.length();z++){
+                   if(z < i || z >= j) result += str.charAt(z);
+               }
+               if(!result.equals("")) result = check_duplication(result);
+               break;
+           }
+
+        }
+
+        if(check!=1) return str;
+        else return result;
     }
 }
