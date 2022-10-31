33255d41638058bd3d141c6ddab3cf09d923004b
이지혁 <olzlgur@naver.com>
2022-10-31 21:47:18 +0900

    중복 문자 체크 메서드, 중복 문자 삭제 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return checkDuplicateWord(cryptogram);
+    }
+
+    private static String checkDuplicateWord(String cryptogram) {
+        Stack<Integer> indexStack = new Stack<>();
+        while (true) {
+            for (int i = 0; i < cryptogram.length() - 1; i++) {
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    indexStack.push(i);
+                    indexStack.push(i + 1);
+                    i++;
+                }
+            }
+            if (indexStack.size() == 0) {
+                break;
+            }
+            cryptogram = removeDuplicateWord(cryptogram, indexStack);
+        }
+        return cryptogram;
+    }
+    private static String removeDuplicateWord(String cryptogram, Stack<Integer> indexStack) {
+        int index;
+        StringBuffer stringBuffer = new StringBuffer(cryptogram);
+        while (!indexStack.empty()) {
+            index = indexStack.pop();
+            stringBuffer.deleteCharAt(index);
+        }
+        return stringBuffer.toString();
     }
 }
