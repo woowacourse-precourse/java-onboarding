96adb2c4702b963f2c3995a3371392d1f9b83855
hyejungg <kimhyejung12@naver.com>
2022-10-31 20:56:33 +0900

    feat: [문제 7] 사용자와 함께 아는 친구 점수 구하는 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,20 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+	private static Map<String, Integer> getSameFriendScore(Map<String, Set<String>> friendMapSet, String user) {
+		Map<String, Integer> score = new HashMap<>();
+		for (String name : friendMapSet.keySet()) {
+			// 나 또는 이미 친구는 넘겨
+			if (name.equals(user) || friendMapSet.get(user).contains(name)) {
+				continue;
+			}
+			// 함께 아는 친구수 구하기
+			int sameFriend = getSameFriendCount(user, name, friendMapSet);
+			score.put(name, sameFriend * SAME_FRIEND_SCORE);
+		}
+		return score;
+	}
+
 	private static int getSameFriendCount(String user, String friend, Map<String, Set<String>> friendMapSet) {
 		int count = 0;
 		for (String name : friendMapSet.get(friend)) {
