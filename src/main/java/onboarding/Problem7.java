f74d98b371c18de868359c53e8f11b1f164980c6
baksohyeon <bori12370@gmail.com>
2022-10-31 16:19:00 +0900

    feat: add code which makes object to organize data

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        HashSet<String> friendList = new HashSet<>();
+        HashMap<String, Integer> suggestScoreList = new HashMap<>();
+        List<List<String>> notFriendList = new ArrayList<>();
+
+        friends.forEach((relationship) -> {
+            String person1 = relationship.get(0);
+            String person2 = relationship.get(1);
+
+            if (Objects.equals(person1, user)) {
+                friendList.add(person2);
+            } else if (Objects.equals(person2, user)) {
+                friendList.add(person1);
+            } else {
+                notFriendList.add(relationship);
+            }
+        });
+
+        System.out.println(friendList);
+        System.out.println(suggestScoreList);
+        System.out.println(notFriendList);
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
