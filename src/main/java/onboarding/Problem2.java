0f40f05ef8e84547f340687b4696b69da3f2f451
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 21:22:58 +0900

    P2 | 결과 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,5 +4,14 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+        StringBuilder answer = new StringBuilder();
+        List<String> answerList = removeDouble(cryptogram);
+
+        for (String st : answerList) {
+            answer.append(st);
+        }
+
+        return answer.toString();
     }
+
 }
