ad035194600d9e867b676ee8aafd4383d4031e19
yxnxx <jh4rgh0@gmail.com>
2022-11-01 00:38:04 +0900

    refactor: Fix Problem6 checking nickname func

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,28 +8,32 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        HashMap<String, String> crew_list = new HashMap<>();
+        HashMap<String, String> word_list = new HashMap<>();
         HashSet<String> email_list = new HashSet<>();
 //        for (List<String> crew : forms) {
 //            crew_list.put(crew.get(0), crew.get(1));
 //        }
         List<String> email;
         for(List<String> crew : forms) {
-            email = checkNickname(crew.get(1), crew_list);
+            email = checkNickname(crew, word_list);
             if(email != null) {
                 //중복임. 이메일 목록에 추가
+                email_list.add()
             }
             // 중복 확인 끝난 crew
-            crew_list.put(crew.get(0), crew.get(1));
+            word_list.put(crew.get(0), crew.get(1));
         }
         return answer;
     }
-    public static List<String> checkNickname(String Nickname, HashMap<String, String> crew_list) {
-        if(crew_list.isEmpty()) return null;
+    public static List<String> checkNickname(List<String> crew, HashMap<String, String> word_list) {
+        String email = crew.get(0);
+        String nickname = crew.get(1);
         List<String> nicklist = new ArrayList<>();
-        for(int i = 0; i < Nickname.length(); i++) {
-            String find_nick = Nickname.substring(i, i+2);
-            if(crew_list.containsValue(find_nick)) nicklist.add(crew_list.get(find_nick));
+        if(word_list.isEmpty()) nicklist = null;
+        for(int i = 0; i < nickname.length() -1; i++) {
+            String find_nick =  nickname.substring(i, i+2);
+            if(word_list.containsValue(find_nick)) nicklist.add(word_list.get(find_nick));
+            word_list.put(find_nick, email);
         }
         return nicklist;
     }
