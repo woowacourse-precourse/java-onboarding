543f22311e33577cb8e893e66bee28605960e9f3
hongjinwon <herenever@naver.com>
2022-11-01 04:08:09 +0900

    feat: problem5 moneyUnit ,withdraw 추가
    - problem5 moneyUnit ,withdraw 추가
    - for 문을 돌지 않고 처리할 수 있는 방법 떠올려보기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> moneyUnits = List.of(50_000,10_000,5_000,1_000,500,100,50,10,1);
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static List<Integer> withdraw(int money){
+        List<Integer> wallet = new ArrayList<>();
+        for (int i=0;i<moneyUnits.size();i++){
+            int share = money / moneyUnits.get(i);
+            wallet.add(i,share);
+            money = money % moneyUnits.get(i);
+        }
+        return wallet;
     }
 }
