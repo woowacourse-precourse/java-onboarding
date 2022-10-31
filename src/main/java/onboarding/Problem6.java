efa35489bad909b5085e7bea000818b541f5ff46
baexxbin <soo3886@gmail.com>
2022-10-31 13:29:34 +0900

    Feat(PROBLEM6): "중복문자검출 함수작성"
    문자중복비교를 위해, 닉네임에서 중복문자를 추출해 비교하는 함수작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,37 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	static List<String> answer = List.of("answer");
+	static HashMap<String, String> duplicateMap = new HashMap<>();
+
+	@SuppressWarnings("checkstyle:WhitespaceAround")
+	public static void checkDuplicate(String nickName, String email) {
+		String twoLetters;
+		for (int i = 0; i < nickName.length() - 1; i++) {
+			twoLetters = nickName.substring(i, i + 2);
+			if (duplicateMap.containsKey(twoLetters)) {
+				if (duplicateMap.get(twoLetters).equals(email)) {
+					continue;
+				}
+				answer.add(email);
+				continue;
+			}
+			duplicateMap.put(twoLetters, email);
+		}
+	}
+
+	public static List<String> solution(List<List<String>> forms) {
+		String email;
+		String nickName;
+		int index = 0;
+		for (List<String> member : forms) {
+			email = member.get(0);
+			nickName = member.get(1);
+			checkDuplicate(nickName, email);
+		}
+		return answer;
+	}
 }
