5e8df4189564f8426839d9a87237c6028d0f4701
minju412 <ln8847@ajou.ac.kr>
2022-10-31 17:31:24 +0900

    style: [p2] apply checkstyle

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,38 +3,38 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        Stack<Character> stack = new Stack<Character>();
-        char dup = ' '; // 중복되는 문자 -> 삭제할 문자
-        char top = ' '; // 스택의 top
-        char cur = ' '; // cur 은 cryptogram 을 순회할 문자
+	public static String solution(String cryptogram) {
+		Stack<Character> stack = new Stack<Character>();
+		char dup = ' '; // 중복되는 문자 -> 삭제할 문자
+		char top = ' '; // 스택의 top
+		char cur = ' '; // cur 은 cryptogram 을 순회할 문자
 
-        for(int i = 0; i < cryptogram.length(); i++) {
-            cur = cryptogram.charAt(i);
+		for (int i = 0; i < cryptogram.length(); i++) {
+			cur = cryptogram.charAt(i);
 
-            if(stack.isEmpty()) { // 스택이 비어있을 때는 무조건 push
-                stack.push(cur);
-            } else {
-                top = stack.peek();
-                if (cur != top && cur != dup) { // push 할 문자가 스택의 top 과 같지 않으면서 중복 문자도 아닐 때
-                    stack.push(cur);
-                    dup = ' '; // dup 초기화
-                } else if (cur == top) { // push 할 문자와 스택의 top 이 같을 때
-                    dup = top; // dup 업데이트
-                    stack.pop(); // 중복 문자 제거
-                }
-            }
-        }
+			if (stack.isEmpty()) { // 스택이 비어있을 때는 무조건 push
+				stack.push(cur);
+			} else {
+				top = stack.peek();
+				if (cur != top && cur != dup) { // push 할 문자가 스택의 top 과 같지 않으면서 중복 문자도 아닐 때
+					stack.push(cur);
+					dup = ' '; // dup 초기화
+				} else if (cur == top) { // push 할 문자와 스택의 top 이 같을 때
+					dup = top; // dup 업데이트
+					stack.pop(); // 중복 문자 제거
+				}
+			}
+		}
 
-        return stackToString(stack);
-    }
+		return stackToString(stack);
+	}
 
-    private static String stackToString(Stack<Character> stack) {
-        StringBuilder answer = new StringBuilder();
-        for (Character character : stack) {
-            answer.append(character);
-        }
-        return answer.toString();
-    }
+	private static String stackToString(Stack<Character> stack) {
+		StringBuilder answer = new StringBuilder();
+		for (Character character : stack) {
+			answer.append(character);
+		}
+		return answer.toString();
+	}
 
 }
