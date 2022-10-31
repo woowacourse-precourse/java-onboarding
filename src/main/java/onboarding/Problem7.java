a597ac3960958c161edd3edc89989ff4eddfb07e
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 01:23:50 +0900

    feat problem7 친구 관계도 그릴 map에 담는 메서드  getFriendsMap구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        Map<String,List<String>> friendsMap =  getFriendsMap(friends);
+
         return answer;
+    }
+    public static Map<String,List<String>> getFriendsMap(List<List<String>> friends){
+        Map<String,List<String>> map = new HashMap<>();
+
+        for(int i=0; i<friends.size();i++){
+            String leftName = friends.get(i).get(0);
+            String rightName = friends.get(i).get(1);
+            List<String> leftList= map.getOrDefault(leftName, new ArrayList<>());
+            leftList.add(rightName);
+            List<String> rightList = map.getOrDefault(rightName, new ArrayList<>());
+            rightList.add(leftName);
+            map.put(leftName,leftList);
+            map.put(rightName,rightList);
+        }
+        return map;
     }
 }
