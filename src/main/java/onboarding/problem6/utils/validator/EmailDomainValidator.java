9cab7683543a59f82d03fb21364157968ea58dae
song-wooseok <sws981202@gmail.com>
2022-11-01 00:50:36 +0900

    feat: [PROBLEM6 - 예외사항4] 이메일 도메인 검사

---
+++ src/main/java/onboarding/problem6/utils/validator/EmailDomainValidator.java
@@ -0,0 +1,32 @@
+package onboarding.problem6.utils.validator;
+
+import onboarding.problem6.utils.Constants;
+import onboarding.problem6.utils.validator.exception.EmailDomainException;
+
+import java.util.List;
+
+public class EmailDomainValidator {
+    private List<List<String>> form;
+
+    public EmailDomainValidator(List<List<String>> form) throws EmailDomainException{
+        this.form = form;
+        validateAllCrewEmailDomain();
+    }
+
+    private void validateAllCrewEmailDomain() throws EmailDomainException{
+        for(List<String> crew : form){
+            validateCrewEmailDomain(crew);
+        }
+    }
+
+    private void validateCrewEmailDomain(List<String> crew) throws EmailDomainException{
+        String crewEmail = crew.get(Constants.CREW_NICNAME);
+        if(!(getDomain(crewEmail)==Constants.DOMAIN)){
+            throw new EmailDomainException("이메일 도메인이 맞지 않는 크루가 있음");
+        }
+    }
+
+    private String getDomain(String email){
+        return email.substring(email.length()-Constants.DOMAIN_POSITION);
+    }
+}
---
+++ src/main/java/onboarding/problem6/utils/validator/exception/EmailDomainException.java
@@ -0,0 +1,7 @@
+package onboarding.problem6.utils.validator.exception;
+
+public class EmailDomainException extends Exception{
+    public EmailDomainException(String msg){
+        super(msg);
+    }
+}
