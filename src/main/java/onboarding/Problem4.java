3493cef29dd60f4c447e134db5651565d2ac9e53
glass <glass7312@gmail.com>
2022-10-31 20:33:44 +0900

    Problem4 refactor(줄바꿈,변수명 변경)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,24 +4,21 @@ public class Problem4 {
 
     public static String solution(String word) {
         String answer = "";
-        int A_Num=0;
-        int t=0;
+        int Ascii_Num=0;
+        int sub_num=0;
 
-        for(int i=0; i<word.length(); i++)
-        {
-            A_Num=word.charAt(i);
-            if(32==A_Num) answer+=" ";
+        for(int i=0; i<word.length(); i++) {
+            Ascii_Num=word.charAt(i);
+            if(32==Ascii_Num) answer+=" ";
 
-            if(A_Num>64 && A_Num<91)
-            {
-                t = 90 - word.charAt(i);
-                answer += (char) (65 + t);
+            if(Ascii_Num>64 && Ascii_Num<91) {
+                sub_num = 90 - word.charAt(i);
+                answer += (char) (65 + sub_num);
             }
 
-            if(A_Num>96 && A_Num<123)
-            {
-                t=122 - word.charAt(i);
-                answer += (char)(97+t);
+            if(Ascii_Num>96 && Ascii_Num<123) {
+                sub_num=122 - word.charAt(i);
+                answer += (char)(97+sub_num);
             }
         }
 
