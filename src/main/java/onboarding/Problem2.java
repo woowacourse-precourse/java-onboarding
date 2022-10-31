fbe2ed78ac82323864e28d7bc287976f672fc842
ckn1070 <ckn1070@naver.com>
2022-10-31 20:11:45 +0900

    Solve the problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+    static String removeDup(String cryptoString){
+        StringBuffer newString = new StringBuffer(cryptoString);
+        for (int i=0; i<newString.length(); i++){
+            if (newString.charAt(i) == newString.charAt(i+1)){
+                newString.deleteCharAt(i);
+                newString.deleteCharAt(i);
+                break;
+            }
+        }
+        return String.valueOf(newString);
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        for (int i=0; i<cryptogram.length(); i++){
+            answer = removeDup(answer);
+        }
         return answer;
     }
 }
