3eb36c33e3e28358c97c651b08827e2745d5177c
Sehoon Kim <shggm2000@gmail.com>
2022-10-31 23:38:38 +0900

    사용자와 친구관계인 friends id를 추출하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.security.KeyStore;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        ArrayList<String> userRelationShip = new ArrayList<String>();
+
+
+        for(int i = 0; i<friends.size(); i++){
+            for(int j =0; j<friends.get(i).size(); j++){
+                if(friends.get(i).get(j) == user){
+                    if(j == 0){
+                        userRelationShip.add(friends.get(i).get(1));
+                    } else userRelationShip.add(friends.get(i).get(0));
+                }
+            }
+        }
+
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
 }
