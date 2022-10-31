ffe18ac5304bfd41f322fc7565c5aa55faafb8ab
hongjinwon <herenever@naver.com>
2022-11-01 05:32:15 +0900

    feat: problem6 checkDuplication 추가
    - problem6 checkDuplication 추가
    - firstNickname 의 한 subString이
    secondNickname의 부분 집합인지 판단하여
    - 맞으면 true 아니면 false return

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
@@ -7,4 +10,14 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    public static boolean checkDuplication(String fistNickname, String SecondNickname){
+        for (int i =0 ; i<fistNickname.length()-2 ; i++){
+            if (SecondNickname.contains(fistNickname.substring(i,i+2))){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
