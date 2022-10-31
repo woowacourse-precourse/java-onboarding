85f1c6f50c2a579e7e52456fde98dd33fa7802a1
myungjunlee <dlaudwns789@gmail.com>
2022-10-31 17:12:14 +0900

    feat(Problem1): 예외사항을 확인하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,30 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    boolean checkException(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(1) - pobi.get(0) != 1)
+            return false;
+
+        if (crong.get(1) - crong.get(0) != 1)
+            return false;
+
+        if (pobi.get(0) == 1 || crong.get(0) == 1) {
+            return false;
+        }
+
+        if (pobi.get(1) == 400 || crong.get(1) == 400) {
+            return false;
+        }
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!checkException(pobi, crong))
+            return -1;
+
+
+
         return answer;
     }
 }
