a423efe740df9f0407e6599efffa6affd4153633
squareyun <squareyun@gmail.com>
2022-10-31 17:47:33 +0900

    refactor: 중복 문자 삭제 로직

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,48 +2,36 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = cryptogram;
-        int startIdx = getOverlabStartIdx(answer);
+        while (true) {
+            char[] cryptogramArray = cryptogram.toCharArray();
+            StringBuilder decoding = new StringBuilder();
 
-        while (startIdx != -1) {
-            int endIdx = getOverlabEndIdx(answer, startIdx);
-
-            String substr1 = answer.substring(0, startIdx);
-            String substr2 = answer.substring(endIdx);
-            answer = substr1 + substr2;
-
-            startIdx = getOverlabStartIdx(answer);
-        }
-
-        return answer;
-    }
-
-    static int getOverlabStartIdx(String s) {
-        if (s.length() <= 1) {
-            return -1;
-        }
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                return i;
+            boolean duplicateFlag = false;
+            char prevC = 0;
+            for (char c : cryptogramArray) {
+                if (c != prevC) {
+                    if (duplicateFlag) {
+                        decoding.deleteCharAt(decoding.length() - 1);
+                        duplicateFlag = false;
+                    }
+                    decoding.append(c);
+                    prevC = c;
+                } else {
+                    duplicateFlag = true;
+                }
             }
-        }
 
-        return -1;
-    }
-
-    static int getOverlabEndIdx(String s, int overlabIdx) {
-        char base = s.charAt(overlabIdx);
-        int endIdx = overlabIdx + 1;
-
-        for (int i = endIdx; i < s.length(); i++) {
-            if (base == s.charAt(i)) {
-                endIdx += 1;
-            } else {
-                return endIdx;
+            // "aa" 와 같은 예제 처리
+            if (duplicateFlag) {
+                decoding.deleteCharAt(decoding.length() - 1);
             }
-        }
 
-        return endIdx;
+            // 기존 문자열에서 바뀐 부분이 없으면 종료
+            if (decoding.length() == cryptogramArray.length) {
+                return cryptogram;
+            }
+
+            cryptogram = decoding.toString();
+        }
     }
 }
