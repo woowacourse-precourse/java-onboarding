2824c418c033f6ebde750c15290df6d8f7cee7fd
sosow0212 <sosow0212@naver.com>
2022-10-31 16:18:21 +0900

    feat: Problem5 - 유효한 돈의 범위인지 판단해주는 isValidRangeNumberException(int money) 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,13 +5,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int MIN_MONEY = 1;
+    private static final int MAX_MONEY = 1000000;
     private static final int[] moneyType = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
+        if(!isValidRangeNumberException(money)) {
+            throw new IllegalArgumentException("유효한 돈의 범위를 입력해주세요. (1 ~ 1000000");
+        }
         List<Integer> answer = getCount(money);
         return answer;
     }
 
+    public static boolean isValidRangeNumberException(int number) {
+        return number >= MIN_MONEY && number <= MAX_MONEY;
+    }
     public static List<Integer> getCount(int money) {
         List<Integer> countOfMoneySortedByType = new ArrayList<>();
         int mod = 0;
