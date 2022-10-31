7e68c19ce5f9633712948c700134dcbee742acbc
dclxxi <dclxxi@kakao.com>
2022-10-31 18:42:45 +0900

    refactor(문제5): 상수의 접근제한자를 public에서 private으로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static final int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static final int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         return withdraw(money);
