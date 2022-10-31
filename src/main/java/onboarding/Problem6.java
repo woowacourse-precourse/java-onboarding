e7c6cb7fbb11b32ff5d80131f08367b53431fc46
saehim <saeilee.vxv@gmail.com>
2022-11-01 00:11:45 +0900

    Feat(Problem6): 닉네임 분할 후 중복체크 함수 호출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,6 +7,19 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static void checkNicknames(List<List<String>> forms) {
+        for (int i=0; i<forms.size(); i++) {
+            String name = forms.get(i).get(1);      // 닉네임
+            String email = forms.get(i).get(0);     // 메일
+
+            for (int j=0; j<name.length()-1; j++){
+                String nameSlice = name.substring(j,j+2);       // 닉네임 2글자씩 자르기
+                checkNameMap(nameSlice, email);                  // 자른 이름 중복 체크 함수 호출
+            }
+        }
+    }
+
     private static void checkNameMap(String nameSlice, String email) {
         if (nameMap.containsKey(nameSlice)) {       // 이름 2글자가 중복된다면,
             emails.add(nameMap.get(nameSlice));         // answer 배열에 중복되는 이름 있는 메일 추가
