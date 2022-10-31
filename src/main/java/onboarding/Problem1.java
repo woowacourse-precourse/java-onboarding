43f4fe42ea6fe7f00ad74b7bed9c8d470c0b2d90
yubin <dbqls2821@gmail.com>
2022-10-31 21:49:18 +0900

    FEAT: [Problem1] 예외사항 -1로 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,11 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!checkException(pobi, crong)) return -1;
     }
+
+    private static boolean checkException(List<Integer> pobi, List<Integer> crong) {
+        return pobi.get(0) + 1 == pobi.get(1) && crong.get(0) + 1 == crong.get(1);
+    }
+
 }
