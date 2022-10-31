a9d159d150104653d711c011886c5778fcafe09f
ashlovesliitea <skyrider1811@gmail.com>
2022-10-31 23:39:02 +0900

    feature: 6번 - 두 문자열이 중복되는 구간이 있는지 체크한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,42 +1,39 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
 
-        HashSet<String> mails=new HashSet<String>();
+        HashSet<String> mails = new HashSet<>();
 
-        int formSize=forms.size();
-        int[] visited=new int[formSize];
-        Arrays.fill(visited,0);
-
-        for(int i=0;i<forms.size()-1;i++){
-            for(int j=i+1;j<forms.size();j++){
-                if(visited[i]==0 && visited[j]==0){
-                    String name1=forms.get(i).get(1);
-                    String name2=forms.get(j).get(1);
-                    if(isDuplicate(name1,name2)){
-                        visited[i]=0;
-                        visited[j]=0;
-                        mails.add(forms.get(i).get(0));
-                        mails.add(forms.get(j).get(0));
-                    }
+        for (int i = 0; i < forms.size() - 1; i++) {
+            for (int j = i + 1; j < forms.size(); j++) {
+                String name1 = forms.get(i).get(1);
+                String name2 = forms.get(j).get(1);
+                if (isDuplicate(name1, name2)) {
+                    mails.add(forms.get(i).get(0));
+                    mails.add(forms.get(j).get(0));
                 }
             }
         }
 
-        ArrayList<String> answer= new ArrayList<>(mails);
-
+        ArrayList<String> answer = new ArrayList<>(mails);
         return answer;
     }
 
-    public static boolean isDuplicate(String name1,String name2){
+    public static boolean isDuplicate(String name1, String name2) {
 
+        for (int s = 0; s < name1.length() - 1; s++) {
+            for (int e = s + 2; e <= name1.length(); e++) {
+                String subStr = name1.substring(s, e);
+                if (name2.contains(subStr)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
-    //두 닉네임이 중복되는 경우가 있으면 True를 리턴한다.
 }
