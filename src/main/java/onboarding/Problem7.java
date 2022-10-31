0d41ca1a12bf12c60ab814b8407b50c553bd54e9
kkm5291 <kkm23125291@gmail.com>
2022-11-01 00:36:59 +0900

    Problem7 Feat : 친구 관계 설정 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,29 +6,34 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
+
+
         return answer;
     }
 
 
-    // 유저 친구 목록
-    public static List<String> userFriendsList(String user, List<List<String>> friends) { // 유저와 직접적인 친구관계
-        List<String> userFriend = new ArrayList<>();
+    // sns관계설정
+    public static Map<String, List<String>> snsRelation (List<List<String>> friends) {
+
+        Map<String, List<String>> relation = new HashMap<>(); // K : 사람 V : K와의 관계에 있는 사람들
 
         for (List<String> friend : friends) {
-            if (friend.contains(user)) {
-                for (String name : friend) {
-                    if (name.equals(user)) {
-                        continue;
-                    }
-                    userFriend.add(name);
-                }
-            }
+            userFriends(relation, friend.get(0), friend.get(1)); //
+            userFriends(relation, friend.get(1), friend.get(0));
         }
 
-        return userFriend;
+        return relation;
     }
 
 
-
-
+    // user의 친구관계 설정
+    public static void userFriends(Map<String, List<String>> relation, String person, String personFriends) {
+        if (relation.containsKey(person)) {
+            relation.get(person).add(personFriends);
+        } else {
+            List<String> friends = new ArrayList<>();
+            friends.add(personFriends);
+            relation.put(person, friends);
+        }
+    }
 }
