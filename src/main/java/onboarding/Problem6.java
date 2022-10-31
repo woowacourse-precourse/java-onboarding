aeac677767bb3696415d15f609516730e471c5d7
wltks98 <wltks98@hanmail.net>
2022-10-31 09:27:12 +0900

    [문제1] refactor : getEmailList를 분리시켜 saveForms 메소드 생성, validateNickname 메소드명 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,8 +8,9 @@ public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
 
 		Woowacourse woowacourse= new Woowacourse();
+		woowacourse.saveForms(forms);
 
-		List<String> answer = woowacourse.getEmailList(forms);
+		List<String> answer = woowacourse.getEmailList();
 		return answer;
 	}
 }
