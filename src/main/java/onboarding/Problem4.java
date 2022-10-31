662dc9b8b7a809625afc4ba683e826a1498f17d3
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-10-31 19:00:47 +0900

    comment: add comments

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 public class Problem4 {
     public static ArrayList<Character> frog(String word) {
-
+        // 대문자 => 두 문자의 아스키코드를 더한 값이 항상 155로 같다.
+        // 소문자 => 두 문자의 아스키코드를 더한 값이 항상 219로 같다.
+        //참고: 공백 = 32 (대문자 A 97 - 소문자 a 65)
+        // 알파벳 외의 문자는 변환하지 않는다.
         char[] charArr = word.toCharArray();
         ArrayList<Character> result = new ArrayList<Character>();
         for (int i = 0; i < charArr.length; i++) {
