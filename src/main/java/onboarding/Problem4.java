5e768dea754386fe11fbf518a7fc19556faf92be
Nugur1i <duckhee092@naver.com>
2022-10-31 23:09:13 +0900

    feat<problem4> : Add exception

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        int wordLength = word.length();
+
+        if (wordLength<1 || wordLength >1000){
+            System.out.println("Please, Enter between 1 ~ 1000 words length");
+            return word;
+        }
+
+
+
+
+
+
+
         return answer;
     }
 }
