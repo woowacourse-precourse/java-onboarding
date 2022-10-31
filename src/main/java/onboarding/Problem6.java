670b1cb86332339918fdf9f0bef79f971e6207ad
seheonCho <whtpgjs94@gmail.com>
2022-10-31 23:33:20 +0900

    feat: [문제6] 중복된 email 들을 담기위한 Set 선언
    추후 중복은 없애고, 정렬해야 하기때문에 TreeSet 구현체 사용

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,15 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         Map<String, String> namesAlreadyApplied = new HashMap<>();
+        Set<String> emails = new TreeSet<>();
 
         List<String> answer = List.of("answer");
         return answer;
     }
+
 }
