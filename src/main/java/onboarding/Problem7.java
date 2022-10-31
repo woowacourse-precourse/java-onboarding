39f82b75f26c2acf3b78a47f4c11eafae8f4f565
hhiyeon <hhyyeon0214@gmail.com>
2022-10-31 20:33:54 +0900

    feat: p7-get limit 5 sorted list, descending score and ascending name

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,14 +1,31 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    private static final int FRIEND_SCORE = 10;
+    private static final int VISITOR_SCORE = 1;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, List<String>> friendsInfoMap = new HashMap<>();
+        List<String> userFriends = new ArrayList<>();
+        Map<String, Integer> scoreMap = new HashMap<>();
+
+        for (List<String> friendInfo : friends) {
+            String userA = friendInfo.get(0);
+            String userB = friendInfo.get(1);
+            createFriendMap(friendsInfoMap, user, userA, userB, userFriends);
+        }
+
+        for (String userFriend : userFriends) {
+            List<String> crossFriends = friendsInfoMap.get(userFriend);
+            addScoreToMap(FRIEND_SCORE, crossFriends, userFriends, scoreMap);
+        }
+
+        addScoreToMap(VISITOR_SCORE, visitors, userFriends, scoreMap);
+
+        return getSortedList(scoreMap);
     }
 
     private static void createFriendMap(Map<String, List<String>> friendsMap, String user, String userA, String userB, List<String> userFriends) {
@@ -25,6 +42,15 @@ public class Problem7 {
         friendOrVisitor.stream()
                 .filter(key -> !isAlreadyFriend(userFriends, key))
                 .forEach(key -> scoreMap.put(key, scoreMap.getOrDefault(key, 0) + plusScore));
+    }
+
+    private static List<String> getSortedList(Map<String, Integer> scoreMap) {
+        return scoreMap.entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry<String, Integer>::getValue)
+                        .reversed().thenComparing(Map.Entry::getKey))
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     private static void addFriendToMap(Map<String, List<String>> friendsMap, String userA, String userB) {
