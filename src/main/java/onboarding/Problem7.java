99145cdb684683a1ae83acceda9b9cb84ec9b9ca
chkim <cndghks0213@gmail.com>
2022-10-31 09:59:56 +0900

    feat(problem7): 점수를 바탕으로 친구를 추천해주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    private static final int MAX_RECOMMENDATION_COUNT = 5;
+    private static final int NOT_RECOMMENDATION_SCORE = 0;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Map<String, Friends> userFriends = new HashMap<>();
         Map<String, Integer> scores = new HashMap<>();
@@ -15,7 +20,7 @@ public class Problem7 {
 
         calculateVisitorScores(visitors, scores);
         calculateSameFriendsScore(userFriends, scores, myFriends);
-        return Collections.emptyList();
+        return recommend(user, scores, myFriends);
     }
 
     private static void registerFriends(List<List<String>> friends, Map<String, Friends> userFriends) {
@@ -46,5 +51,33 @@ public class Problem7 {
 
             scores.put(username, score + sameFriendsScore);
         }
+    }
+
+    private static List<String> recommend(String user, Map<String, Integer> scores, Friends myFriends) {
+        return scores.entrySet().stream()
+                .filter(predicate(user, myFriends))
+                .sorted(comparator())
+                .limit(MAX_RECOMMENDATION_COUNT)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    private static Predicate<Map.Entry<String, Integer>> predicate(String user, Friends myFriends) {
+        return score -> notMe(user, score)
+                && score.getValue() != NOT_RECOMMENDATION_SCORE
+                && myFriends.notContains(score.getKey());
+    }
+
+    private static boolean notMe(String user, Map.Entry<String, Integer> score) {
+        return !score.getKey().equals(user);
+    }
+
+    private static Comparator<Map.Entry<String, Integer>> comparator() {
+        return (score1, score2) -> {
+            if (score1.getValue().equals(score2.getValue())) {
+                return score1.getKey().compareTo(score2.getKey());
+            }
+            return score2.getValue().compareTo(score1.getValue());
+        };
     }
 }
