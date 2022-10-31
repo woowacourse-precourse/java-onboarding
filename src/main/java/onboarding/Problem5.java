0138fcee3aeb1fb10fbbbe931e3d11b3410eb7dc
Go-Jaecheol <gojaech@naver.com>
2022-10-31 16:33:42 +0900

    feat: Problem5 - 가장 큰 금액 단위부터 변환되는지 하나씩 확인하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,5 +9,13 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<Integer> getResultList(int money) {
+        List<Integer> result = new ArrayList<>();
+        for (int unit : moneyUnit) {
+
+        }
+        return result;
     }
 }
