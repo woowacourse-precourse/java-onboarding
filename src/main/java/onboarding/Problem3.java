c451129be683db9a83177ea8fc573baac0f629e8
Ridealist <cjsn92@gmail.com>
2022-10-31 14:33:06 +0900

    style/Problem3 : for문 Java 코딩컨벤션 적용

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,7 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        for (int i=1; i<=number; i++) {
+        for (int i = 1; i <= number; i++) {
             answer += countThreeSixNine(i);
         }
         return answer;
@@ -12,7 +12,7 @@ public class Problem3 {
     public static int countThreeSixNine(int n) {
         String n_str = String.valueOf(n);
         int cnt = 0;
-        for (int i=0; i<n_str.length(); i++) {
+        for (int i = 0; i < n_str.length(); i++) {
             char digit = n_str.charAt(i);
             if (digit == '3' | digit == '6' | digit == '9') {
                 cnt += 1;
