747a200407c3bf6db2356d08f02d7b98528516c2
kkyu0718 <net605@naver.com>
2022-10-31 23:51:06 +0900

    [1] add: 결과 반환 메소드

--- src/main/java/onboarding/problem1/Problem1.java
+++ src/main/java/onboarding/problem1/Problem1.java
@@ -3,25 +3,28 @@ package onboarding.problem1;
 import java.util.List;
 
 public class Problem1 {
-    private static final int START = 1;
-    private static final int END = 400;
+    public static final int START = 1;
+    public static final int END = 400;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         Page pobiPage = new Page(pobi.get(0), pobi.get(1));
         Page crongPage = new Page(crong.get(0), crong.get(1));
 
-        int answer = Integer.MAX_VALUE;
-
-        if (validate(pobiPage) & validate(crongPage)) {
-            answer = -1;
-        }
-
-
-
-        return answer;
+        return getAnswer(pobiPage, crongPage);
     }
 
-    public static boolean validate(Page page){
-        return page.isValidLeft(page.getLeft()) & page.isValidRight(page.getRight()) & page.isBetween(START, END );
+    public static int getAnswer(Page pobiPage, Page crongPage) {
+        if (pobiPage.validatePage() & crongPage.validatePage()) {
+            if (pobiPage.getScore() > crongPage.getScore()){
+                return 1;
+            }
+            if (pobiPage.getScore() < crongPage.getScore()){
+                return 2;
+            }
+            if (pobiPage.getScore() == crongPage.getScore()){
+                return 0;
+            }
+        }
+        return -1;
     }
 }
