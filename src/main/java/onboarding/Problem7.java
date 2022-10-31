1aadc836a50efa3fcc997e3c12d042b4bdb0046f
Dabeen Jeong <hello70825@gmail.com>
2022-10-31 22:12:47 +0900

    refactor: [Problem7] putIfAbsent를 사용하여 들여쓰기 깊이를 줄이다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -70,20 +70,16 @@ public class Problem7 {
     private static Map<String, List<String>> getAdjacencyList(List<List<String>> friends) {
         Map<String, List<String>> resultAdjacencyList = new TreeMap<>();
 
-        for(List<String> friend : friends) {
+        friends.forEach(friend -> {
             String friendFirst = friend.get(0);
             String friendSecond = friend.get(1);
 
-            if (!resultAdjacencyList.containsKey(friendFirst)) {
-                resultAdjacencyList.put(friendFirst, new ArrayList<>());
-            }
+            resultAdjacencyList.putIfAbsent(friendFirst, new ArrayList<>());
             resultAdjacencyList.get(friendFirst).add(friendSecond);
 
-            if (!resultAdjacencyList.containsKey(friendSecond)) {
-                resultAdjacencyList.put(friendSecond, new ArrayList<>());
-            }
+            resultAdjacencyList.putIfAbsent(friendSecond, new ArrayList<>());
             resultAdjacencyList.get(friendSecond).add(friendFirst);
-        }
+        });
 
         return resultAdjacencyList;
     }
