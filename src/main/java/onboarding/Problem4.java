87f582e0605c976fce2227960e97d0b8235f657f
yoonjuho <dbswn990@naver.com>
2022-10-31 19:36:34 +0900

    feat: PROBLEM4-(예외처리) 문자열 길이 예외처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        if(hasException(word)){
+            return null;
+        }
         return answer;
     }
+
+    public static boolean hasException(String word){
+        if(word.length()<1 || word.length()>10000)
+            return true;
+        return false;
+    }
+
+
 }
