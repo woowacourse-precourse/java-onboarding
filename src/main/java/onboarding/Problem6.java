5db2a7d67639a2fa836494e84c97254656b56dcf
Sangin <ksiisk99@gmail.com>
2022-11-01 00:57:27 +0900

    feat: problem6 - 닉네임 중 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록을 반환하는 solution 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,36 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-
+    private static Map<String,String> duplicateNames =new HashMap<>(); //<이름, 이메일>
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer=Collections.emptyList();
+        Set<String> answer = new HashSet<>();
 
-        return answer;
+        for (List<String> crew : forms) {
+            String nickName = crew.get(1);
+            if (nickName.length() < 2) {
+                continue;
+            }
+
+            String email = crew.get(0);
+            for (int i = 0; i < nickName.length() - 1; i++) {
+                String partialName = nickName.substring(i, i+2);
+                String userEmailWithDuplicatedName = duplicateNames.getOrDefault(partialName,"");
+
+                if(isDuplicatedName(userEmailWithDuplicatedName,email)){
+                    answer.add(email);
+                    answer.add(userEmailWithDuplicatedName);
+                }
+
+                duplicateNames.put(partialName, email);
+            }
+        }
+
+        return new ArrayList<>(answer).stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     private static boolean isDuplicatedName(String userEmailWithDuplicatedName, String email){
