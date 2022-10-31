20006c8fdb7ab19942f185dfb4763e0660140b77
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-10-31 20:10:43 +0900

    feat: Problem2 풀이
    단순 계산을 통해 문제를 해결하였음

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer;
+        /*
+        * 1. 0~9까지 박수는 3번 친다.
+        * 2. 10으로 나는 몫 * 3을 구한다.
+        * 3. 10으로 나눈 나머지를 3으로 나누면 몇번 박수를 치는지 알 수 있다.
+        * 4. 2번 + 3번을 한다.
+         */
+        int share = number / 10;
+        int remainder = number % 10;
+
+        answer = (share * 3) + (remainder / 3);
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10000));
+        System.out.println(solution(12));
     }
 }
