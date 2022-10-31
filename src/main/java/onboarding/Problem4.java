bb5dcee5c29ce3557ab36819c1774d142bfe30af
anneu <b.imnuj@gmail.com>
2022-10-31 15:54:49 +0900

    feat(problem4): 알파벳인지 확인하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        char[] alphabets = word.toCharArray();
+
+        for (char alphabet : alphabets) {
+            if (isAlphabet(alphabet)) {
+                // sb.append(변환된 알파벳);
+                continue;
+            }
+
+            sb.append(alphabet);
+        }
+
+        return sb.toString();
+    }
+
+    private static boolean isAlphabet(char alphabet) {
+        if ('A' <= alphabet && alphabet <= 'Z') {
+            return true;
+        }
+
+        if ('a' <= alphabet && alphabet <= 'z') {
+            return true;
+        }
+
+        return false;
     }
 }
