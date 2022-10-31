4c6cc77ea7e052c888fdbe871203692ad5e69b86
woosung1223 <dntjd991223@naver.com>
2022-10-31 17:41:00 +0900

    feature(Problem7): Validation 클래스 구현

--- src/main/java/onboarding/problem7/Controller.java
+++ src/main/java/onboarding/problem7/Controller.java
@@ -5,18 +5,21 @@ import java.util.List;
 public class Controller {
 
     public static List<String> getSolution(String user, List<List<String>> friends, List<String> visitors) {
-        Relations relation = new Relations(friends);
-        Visitors visitor = new Visitors(visitors);
-        init(relation, visitor);
-        List<String> myFriends = relation.getFriendsList(user);
-        for (int i = 0; i < myFriends.size(); i++) {
-            List<String> acquaintance = relation.getFriendsList(myFriends.get(i));
-            Friend.addWeight(acquaintance, Constant.FRIEND_WEIGHT);
+        if (Validation.check(user, friends, visitors)) {
+            Relations relation = new Relations(friends);
+            Visitors visitor = new Visitors(visitors);
+            init(relation, visitor);
+            List<String> myFriends = relation.getFriendsList(user);
+            for (int i = 0; i < myFriends.size(); i++) {
+                List<String> acquaintance = relation.getFriendsList(myFriends.get(i));
+                Friend.addWeight(acquaintance, Constant.FRIEND_WEIGHT);
+            }
+            Friend.addWeight(visitors, Constant.VISITOR_WEIGHT);
+            List<String> recommendedList = Friend.getRecommendedList(user);
+            recommendedList.removeAll(myFriends);
+            return recommendedList;
         }
-        Friend.addWeight(visitors, Constant.VISITOR_WEIGHT);
-        List<String> recommendedList = Friend.getRecommendedList(user);
-        recommendedList.removeAll(myFriends);
-        return recommendedList;
+        return Constant.EXCEPTION;
     }
 
     private static void init(Relations relation, Visitors visitor) {
