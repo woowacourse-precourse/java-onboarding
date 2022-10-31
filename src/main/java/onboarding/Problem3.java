edcaf374c3f29494826e267dca25bd1a0de7a7aa
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-10-31 18:54:49 +0900

    Comment(Problem3) : 기능 목록 comment 삭제 기능 목록 comment를 삭제하고 PROBLEM3.md에 기능 목록을 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,11 +4,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem3 {
-    /*
-    Game369 클래스는 다음과 같은 기능을 함.
-    생성자에서 number를 받아 calc 메서드를 통해 3, 6, 9 가 들어간 횟수를 계산함.
-    calc 메서드에서는 number를 String으로 치환하여 Pattern과 Matcher클래스를 이용하여 3, 6, 9의 개수를 카운트
-     */
     public static int solution(int number) {
         int answer = 0;
         Game369 game = new Game369(number);
