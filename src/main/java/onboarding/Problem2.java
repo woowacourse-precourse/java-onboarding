152cb7e7255dcbe2bb92d4f7c967dc3d1f2c0b11
nakalic <nakalic28@gmail.com>
2022-10-31 14:32:21 +0900

    fix(Problem2): 중복된 문자열을 잘못 제거하던 문제 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,17 +1,11 @@
 package onboarding;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
-
 public class Problem2 {
 	public static String solution(String cryptogram) {
 		String answer = "answer";
-
 		while (checkDuplication(cryptogram)) {
 			cryptogram = removeDuplication(cryptogram);
+
 		}
 		answer = cryptogram;
 		return answer;
@@ -31,31 +25,7 @@ public class Problem2 {
 	}
 
 	private static String removeDuplication(String str) {
-
-		Stack<Integer> storage = new Stack<>();
-		LinkedList<Character> lst = new LinkedList<>();
-
-		for (int i = 0; i < str.length(); i++)
-			lst.add(str.charAt(i));
-
-		for (int i = 0; i < str.length() - 1; i++)
-			if (str.charAt(i) == str.charAt(i + 1)) {
-				storage.push(i);
-				storage.push(i + 1);
-				i += 1;
-			}
-
-		for (int i = storage.size(); i >= 1; i--) {
-			lst.remove(storage.pop().intValue());
-		}
-
-		String temp = "";
-		
-		for (Character c : lst) {
-			temp += c;
-		}
-
-		return temp;
+		return str.replaceAll("(.)\\1+", "");
 	}
 
 }
