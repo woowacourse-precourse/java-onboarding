9d454eec08f1c1028c6dd182bb7472bf957d5ac3
myungjunlee <dlaudwns789@gmail.com>
2022-10-31 21:19:08 +0900

    feat(Problem4): 청개구리 사전에 따른 대문자 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+    private static String convertUpper(char letter) {
+        int difference = letter - 65;
+        char converted = 90 - difference;
+
+        return converted;
+
+    }
+
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
