8c5c3e51b92fc042fd7876d34df0e599420209fd
WhoIsWoony <WhoIsWoony@gmail.com>
2022-10-31 17:57:19 +0900

    problem4: TODO-'기능1' complete

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem4 {
+    private static char reverseTransferChar(char ascii){
+        if((int)'A' <= ascii && ascii <= (int)'Z')
+            ascii = (char)((int)'A'+ (int)'Z'- (int)ascii);
+        else if((int)'a' <= ascii && ascii <= (int)'z')
+            ascii = (char)((int)'a'+ (int)'z'- (int)ascii);
+        return ascii;
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return "";
     }
 }
