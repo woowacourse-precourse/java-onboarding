74826f2b81921a0fd95b3f2751d1de9ddcb5e2b7
소대훈 <theogns0681@naver.com>
2022-10-31 17:59:02 +0900

    [feat] (문제2) 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+    static String answer;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        answer = cryptogram;
+        deleteDuplicate(answer);
         return answer;
+    }
+
+    /*
+        duplicateWord가 비어있는 문자열("")이라면
+        연속적인 알파벳이 없다.
+         */
+    public static void deleteDuplicate(String input) {
+        String duplicateWord = findDuplicate(input);
+        if (!duplicateWord.equals("")) {
+            answer = answer.replaceAll(duplicateWord, "");
+            deleteDuplicate(answer);
+        }
     }
 }
