a49b2d4d9a1252ce65a5e5d8dd7063faf49b30c1
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:10:34 +0900

    [problem2] CryptogramGenerator 생성자 추가

--- src/main/java/onboarding/problem2/CryptogramGenerator.java
+++ src/main/java/onboarding/problem2/CryptogramGenerator.java
@@ -1,8 +1,15 @@
 package onboarding.problem2;
 
 public class CryptogramGenerator {
-    //Cryptogram
+    private Cryptogram cryptogram;
 
+    public CryptogramGenerator(String input){
+        this.cryptogram = new Cryptogram(input);
+    }
     //1. 생성자, 문자열 받고 Cryptogram 인스턴스 생성
+
+
     //2. 크립토그램 변환함수, Cryptogram의 중복 제거 반복호출 후 반환
+
+
 }
