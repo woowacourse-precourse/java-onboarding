14ad6bbffa340afa5e1df344cecbceaf2cb92696
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-01 02:28:54 +0900

    feat(Problem5): add Wallet class

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,21 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    // TODO: class Wallet
-        // TODO: Wallet(int money)
-
-        // TODO: countCurrency()
-
     public static List<Integer> solution(int money) {
         if (money < 1 || 1_000_000 < money) {
             throw new IllegalArgumentException();
         }
-
-        // Make wallet
-
-        // County currency in wallet
+        var myWallet = new Wallet(money);
 
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+}
+
+class Wallet {
+    private int money;
+    private final Integer[] COUNT;
+
+    Wallet(int money) {
+        this.money = money;
+        this.COUNT = new Integer[9];
     }
 }
