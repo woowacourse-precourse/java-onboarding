291d37334c1bc06ed3451836900dcde8108e275a
himjjong <jjijji5728@tukorea.ac.kr>
2022-10-31 14:37:27 +0900

    test : 1가지 Testcase 추가

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -168,6 +168,20 @@ class ApplicationTest {
             List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
             assertThat(Problem6.solution(forms)).isEqualTo(result);
         }
+        @Test
+        void case2() {
+            List<List<String>> forms = List.of(
+                    List.of("jm@email.com", "제이엠"),
+                    List.of("jason@email.com", "제이슨"),
+                    List.of("woniee@email.com", "워니"),
+                    List.of("mj@email.com", "엠제이"),
+                    List.of("nowm@email.com", "이제엠"),
+                    List.of("barojm@email.com", "바로제이엠"),
+                    List.of("codingejm@email.com", "코딩이제이엠")
+            );
+            List<String> result = List.of("barojm@email.com","codingejm@email.com","jason@email.com", "jm@email.com", "mj@email.com","nowm@email.com");
+            assertThat(Problem6.solution(forms)).isEqualTo(result);
+        }
     }
 
     @Nested
