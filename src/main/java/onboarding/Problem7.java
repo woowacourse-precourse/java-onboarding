31a50fe4172c8b0e50a9051ed6eb9dfe9dc8f22c
Go-Jaecheol <gojaech@naver.com>
2022-11-01 01:55:12 +0900

    feat: Problem7 - user의 함께 아는 친구들의 점수를 계산하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,12 +1,13 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     private static Map<String, Set<String>> friendsMap;
     private static Map<String, Integer> scoreMap;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
         friendsMap = new HashMap<>();
         scoreMap = new HashMap<>();
 
@@ -18,8 +19,9 @@ public class Problem7 {
             addFriendsToMap(friend2, friend1);
         }
         addVisitorScore(visitors);
+        calculateFriendsScore(user);
 
-        return answer;
+        return getSortedResult(user);
     }
 
     private static void addFriendsToMap(String keyFriend, String valueFriend) {
@@ -36,5 +38,28 @@ public class Problem7 {
             }
             scoreMap.put(visitor, scoreMap.get(visitor) + 1);
         }
+    }
+
+    private static void calculateFriendsScore(String user) {
+        Set<String> friends = friendsMap.get(user);
+        for (String friend : friends) {
+            Set<String> friendsOfFriends = friendsMap.get(friend);
+            calculateFriendsOfFriends(user, friendsOfFriends);
+        }
+    }
+
+    private static void calculateFriendsOfFriends(String user, Set<String> friendsOfFriends) {
+        for (String person : friendsOfFriends) {
+            if (!person.equals(user) && !friendsMap.get(person).contains(user)) {
+                scoreMap.put(person, friendsMap.get(person).size() * 10 + scoreMap.getOrDefault(person, 0));
+            }
+        }
+    }
+
+    private static List<String> getSortedResult(String user) {
+        List<String> result = new ArrayList<>(scoreMap.keySet());;
+        result = result.stream().filter(o -> !friendsMap.get(user).contains(o)).collect(Collectors.toList());
+        result.sort(((o1, o2) -> (scoreMap.get(o2).compareTo(scoreMap.get(o1)))));
+        return result;
     }
 }
