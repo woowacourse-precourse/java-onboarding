e2a3df7fc13a8a32659b15bc6a606ac6c227884d
Damgom <wisewish1@gmail.com>
2022-10-31 13:08:08 +0900

    Feat: count clap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        return countClap(number);
+    }
+
+    private static int countClap(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            String num = String.valueOf(i);
+            for(int j = 0; j < num.length(); j++) {
+                int charsNumericValue = num.charAt(j)-'0';
+                if((charsNumericValue) % 3 == 0 && (charsNumericValue != 0)) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
