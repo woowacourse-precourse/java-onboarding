dc8c192011065e14eadd828598aebf743b3a5d37
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-10-31 11:18:17 +0900

    문제 4(refactor) - 접근 제어자 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,9 +1,9 @@
 package onboarding;
 
 public class Problem4 {
-	static final String[] UPPER_CASE_ARR = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L",
+	private static final String[] UPPER_CASE_ARR = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L",
 		"K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
-	static final String[] LOWER_CASE_ARR = {"z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l",
+	private static final String[] LOWER_CASE_ARR = {"z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l",
 		"k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"};
 
 	public static String solution(String word) {
@@ -22,7 +22,7 @@ public class Problem4 {
 		return stringBuilder.toString();
 	}
 
-	static String changeWord(char j) {
+	private static String changeWord(char j) {
 		//대소문자 여부 판별
 		if (Character.isUpperCase(j)) {
 			//아스키코드를 반대문자로 변환
@@ -34,7 +34,7 @@ public class Problem4 {
 		return String.valueOf(j);
 	}
 
-	static int charToIndex(char j) {
+	private static int charToIndex(char j) {
 		//문자를 인덱스 숫자로 변환
 		return (int)j >= 97 ? j - 97 : j - 65;
 	}
