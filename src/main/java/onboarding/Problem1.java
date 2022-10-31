75c6bb1436ae7127f9c4ba7ba24045e555d173d8
DDing77 <devseo2021@gmail.com>
2022-10-31 21:29:25 +0900

    [Problem1] document : document 생성 및 기능 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,12 +2,6 @@ package onboarding;
 
 import java.util.List;
 
-/* 기능 목록
-  1. 입력값 예외처리
-  2. 포비와 크롱의 최대 점수를 비교
-  3. 각 자리 숫자를 모두 더하기
-  4. 각 자리 숫자를 모두 곱하기  */
-
 class Problem1 {
 
     // 입력 예외처리
