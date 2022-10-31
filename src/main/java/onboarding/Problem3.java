16ab617df477eedff3bf0265b1adb08aeb572f22
이지영 <wldudwldhd@naver.com>
2022-10-31 23:42:48 +0900

    docs:기능구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,14 @@
 package onboarding;
 
 public class Problem3 {
+    /* 기능구현
+    1. 조건 : number는 1 이상 10,000 이하인 자연수이다.
+    3 6 9의 개수만 알면 되기 때문에 %10, //10 연산을 반복하고 더이상 10으로 나누어지지 않는다면 while문을 탈출한다.
+    * */
+
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
     }
 }
