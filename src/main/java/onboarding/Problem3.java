caf136c8ffe4f9540bae4587b292b521ee3c033c
ChaCha3088 <cha3088@gmail.com>
2022-10-31 23:08:18 +0900

    feat: problem3 new function solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,9 @@ import java.util.stream.Stream;
 
 public class Problem3 {
     public static int solution(int number) {
-        int clapCount = 0;
-
+        int[] arrNum = splitEachNumbers(number);
+        int clapCount = countTargetNumber(arrNum);
+        return clapCount;
     }
 
     private static int[] splitEachNumbers(int number) {
@@ -13,12 +14,13 @@ public class Problem3 {
         return arrNum;
     }
 
-    private static void countTargetNumber(int[] arrNum) {
+    private static int countTargetNumber(int[] arrNum) {
+        int clapCount = 0;
         for (int i = 0; i<arrNum.length; i++) {
             if (i == 3 || i == 6 || i == 9) {
                 clapCount = clapCount + 1;
             }
+        return clapCount;
         }
-
     }
 }
