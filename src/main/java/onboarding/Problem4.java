dbde5d7c02a21d5385ba2704d8fb8bfd7455f87b
baekeunsun <eunsun5460@gmail.com>
2022-10-31 15:12:41 +0900

    [문제4] feats: input 예외확인

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        if(CheckInput(word)){
+
+        }
+        throw new IllegalArgumentException("잘못된 입력값");
+    }
+
+    /* 기능1 : input 확인 */
+    private static boolean CheckInput(String word){
+        if(word.length() > 0 && word.length() < 1001) return true;
+        return false;
     }
 }
