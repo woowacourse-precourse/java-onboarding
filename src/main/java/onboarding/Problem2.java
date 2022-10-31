e501aba745aa3a6cc3a71c92312c791945f762b9
alswn <alswn9938@gmail.com>
2022-10-31 15:23:01 +0900

    [Q2] feat : add "중복 문자 Index 저장" 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
+    public static List<Integer> duplicatedIndexList;
+    public static StringBuilder sb;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return cryptogram;
+    }
+
+    public static void addDuplicatedIndex(String cryptogram) {
+//       duplicatedIndexList.clear();
+        char first = ' ', second = ' ';
+        for (int index = 0; index < cryptogram.length() - 1; index++) {
+            first = cryptogram.charAt(index);
+            second = cryptogram.charAt(index + 1);
+            if (first == second) {
+                duplicatedIndexList.add(index);
+                duplicatedIndexList.add(index + 1);
+            }
+        }
     }
 }
