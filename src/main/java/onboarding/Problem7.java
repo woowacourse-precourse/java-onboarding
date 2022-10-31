69d1eb68da87b954fd6b862319dcbfbdecf3dd24
Koonayoung518 <koony000415@gmail.com>
2022-10-31 10:20:41 +0900

    feat(problem7) : 친구 관계 정보 기반 추천 목록 찾기 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,6 +7,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         List<String> userFriends = findUserFriends(user, friends);
+        List<Person> friendList = findRecommendationFromFriends(user, friends, userFriends);
         return answer;
     }
 
@@ -17,5 +18,25 @@ public class Problem7 {
                 list.add(item.get(1 - item.indexOf(user)));
         }
         return list.stream().distinct().collect(Collectors.toList());
+    }
+    private static List<Person> findRecommendationFromFriends(String user, List<List<String>> friends, List<String> userFriends){
+        List<String> list = new ArrayList<>();
+        for(List<String> item : friends){
+            if(userFriends.contains(item.get(0)) && !item.get(1).equals(user))
+                list.add(item.get(1));
+            else if(userFriends.contains(item.get(1)) && !item.get(0).equals(user))
+                list.add(item.get(0));
+        }
+        return new ArrayList<>();
+    }
+
+    static class Person{
+        String name;
+        int score;
+
+        public Person(String name, int score){
+            this.name = name;
+            this.score = score;
+        }
     }
 }
