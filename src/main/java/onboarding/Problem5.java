4d0ac1851a3fd9cea1fc46e2b58488315e9016fd
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-01 01:28:32 +0900

    feat: Problem 5, Add Solution of Problem 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        final List<Integer> moneyList =
+                new ArrayList<Integer>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
+        for(int i = 0; i < moneyList.size(); i++){
+            int moneyCount = money/moneyList.get(i);
+            answer.add(moneyCount);
+            money = money % moneyList.get(i);
+        }
         return answer;
     }
 }
