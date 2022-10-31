da3fc1f56598c46d81ff87b95472227406f111d9
¤knj <krkoknj@gmail.com>
2022-10-31 22:11:04 +0900

    List 안에 닉네임을 배열에 각각 채운다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,7 +7,6 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         String[] nicknameArr = fillNicknameArr(forms);
-        System.out.println("nicknameArr = " + Arrays.toString(nicknameArr));
         checkIndexAndNickname(nicknameArr);
         return answer;
     }
@@ -15,28 +14,22 @@ public class Problem6 {
     private static void checkIndexAndNickname(String[] nicknameArr) {
         String[] duplicateNicknames = new String[nicknameArr.length];
         int checkIndex = 0;
-        for (int i = 0; i < nicknameArr.length - 1; i++) {
+
+        for (int i = 0; i < nicknameArr.length; i++) {
             for (int j = 0; j < nicknameArr[i].length(); j++) {
-                if (nicknameArr[i].charAt(j) == nicknameArr[i + 1].charAt(j)) {
+                for (int k = 0; k < nicknameArr[j].length(); k++) {
+
                 }
             }
         }
-
-        System.out.println("duplicateNicknames = " + Arrays.toString(duplicateNicknames));
     }
 
     private static String[] fillNicknameArr(List<List<String>> forms) {
         String[] nicknames = new String[forms.size()];
         int idx = 0;
+
         for (List<String> i : forms) {
-            if (i.get(1).length() > 1 || i.get(1).length() < 20) {
-                int i1 = 19 - i.get(1).length();
-                StringBuilder builder = new StringBuilder();
-                for (int j = 0; j < i1; j++) {
-                    builder.append(j);
-                }
-                nicknames[idx] = i.get(1) + builder;
-            }
+            nicknames[idx] = i.get(1);
             idx++;
         }
 
