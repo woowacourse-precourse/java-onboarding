465f64df391c07ae514b5d581ee5610fed867397
eora21 <eora21@naver.com>
2022-10-31 23:39:12 +0900

    refactor: 💡 변경 크루 닉네임 객체 추가 및 닉네임 등록 메서드를 통한 결과 반환
    static 메서드를 모두 클래스 내로 변경 및 업로드를 통해 겹치는 이메일이 설정된 크루들을 반환받을 수 있도록 변경하였다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,17 +4,43 @@ import java.util.*;
 
 public class Problem6 {
 
-    private static void putTwoCharsAndEmailsByTraversingForms(Map<String, Set<String>> twoCharsAndEmails, List<List<String>> forms) {
+    /**
+     * 신청받은 닉네임 중 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록을 return 하도록 solution 메서드를 완성하라.
+     *
+     * @param forms 이메일, 닉네임
+     * @return 닉네임 연속 글자 포함 목록, 이메일 오름차순, 중복제거
+     */
+    public static List<String> solution(List<List<String>> forms) {
+        CrewNickname crewNickname = new CrewNickname();
+        return crewNickname.upload(forms);
+    }
 
+}
+
+class CrewNickname {
+    private Map<String, Set<String>> twoCharsAndEmails;
+    private Set<String> duplicateCrewEmail;
+
+    public List<String> upload(List<List<String>> forms) {
+        twoCharsAndEmails = new HashMap<>();
+        createTwoCharsAndEmails(forms);
+
+        duplicateCrewEmail = new HashSet<>();
+        updateDuplicateCrewEmail();
+
+        return createAnswer();
+    }
+
+    private void createTwoCharsAndEmails(List<List<String>> forms) {
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickname = form.get(1);
-            Set<String> twoCharsSet = makeTwoCharsKeyUsingNickname(nickname);
-            putTwoCharsAndEmails(twoCharsAndEmails, twoCharsSet, email);
+            Set<String> twoCharsKey = makeTwoCharsKey(nickname);
+            putTwoCharsAndEmails(twoCharsKey, email);
         }
     }
 
-    private static Set<String> makeTwoCharsKeyUsingNickname(String nickname) {
+    private Set<String> makeTwoCharsKey(String nickname) {
         Set<String> twoCharsFromNickname = new HashSet<>();
 
         for (int i = 0; i < nickname.length() - 1; i++) {
@@ -24,54 +50,38 @@ public class Problem6 {
         return twoCharsFromNickname;
     }
 
-    private static void putTwoCharsAndEmails(Map<String, Set<String>> twoCharsAndEmails, Set<String> twoCharsSet, String email) {
+    private void putTwoCharsAndEmails(Set<String> twoCharsKey, String email) {
 
-        for (String twoChars : twoCharsSet) {
-            Set<String> emails = getOrCreateEmailSet(twoCharsAndEmails, email, twoChars);
+        for (String twoChars : twoCharsKey) {
+            Set<String> emails = getOrCreateEmailSet(email, twoChars);
             twoCharsAndEmails.put(twoChars, emails);
         }
     }
 
-    private static Set<String> getOrCreateEmailSet(Map<String, Set<String>> twoCharsAndEmails, String email, String twoChars) {
+    private Set<String> getOrCreateEmailSet(String email, String twoChars) {
         Set<String> emails = twoCharsAndEmails.getOrDefault(twoChars, new HashSet<>());
         emails.add(email);
         return emails;
     }
 
-    private static void updateCrewEmailForDuplicateNicknames(Set<String> crewEmailForDuplicateNicknames, Map<String, Set<String>> twoCharsAndEmails) {
+    private void updateDuplicateCrewEmail() {
 
         for (Set<String> emails : twoCharsAndEmails.values()) {
-            updateCrewEmailIfNicknameDuplicate(crewEmailForDuplicateNicknames, emails);
+            updateCrewEmailIfNicknameDuplicate(emails);
         }
     }
 
-    private static void updateCrewEmailIfNicknameDuplicate(Set<String> crewEmailForDuplicateNicknames, Set<String> emails) {
+    private void updateCrewEmailIfNicknameDuplicate(Set<String> emails) {
 
         if (emails.size() > 1) {
-            crewEmailForDuplicateNicknames.addAll(emails);
+            duplicateCrewEmail.addAll(emails);
         }
     }
 
-    /**
-     * 신청받은 닉네임 중 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록을 return 하도록 solution 메서드를 완성하라.
-     *
-     * @param forms 이메일, 닉네임
-     * @return 닉네임 연속 글자 포함 목록, 이메일 오름차순, 중복제거
-     */
-    public static List<String> solution(List<List<String>> forms) {
-        Map<String, Set<String>> twoCharsAndEmails = new HashMap<>();
-        putTwoCharsAndEmailsByTraversingForms(twoCharsAndEmails, forms);
-
-        Set<String> crewEmailForDuplicateNicknames = new HashSet<>();
-        updateCrewEmailForDuplicateNicknames(crewEmailForDuplicateNicknames, twoCharsAndEmails);
-
-        List<String> answer = new ArrayList<>(crewEmailForDuplicateNicknames);
+    private List<String> createAnswer() {
+        List<String> answer = new ArrayList<>(duplicateCrewEmail);
         answer.sort(Comparator.naturalOrder());
 
         return answer;
     }
-
-
-
-
 }
