3f4a3488eb15d5eda926f0aaf27b0c553b800ea0
xi_jjun <rlawowns97@naver.com>
2022-11-01 00:38:01 +0900

    feat(Problem5): UNIT 화폐단위로 최대한 바꾸고 남은 돈 반환하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,6 +13,13 @@ public class Problem5 {
         return money / UNIT.getValue();
     }
 
+    public static int remainedMoney(final CURRENCY UNIT, int money) {
+        int count = countMaxNumberOfCurrencies(UNIT, money);
+        int transformedMoney = UNIT.getValue() * count;
+
+        return money - transformedMoney;
+    }
+
     enum CURRENCY {
         W_50000(50000), W_10000(10000), W_5000(5000),
         W_1000(1000), W_500(500), W_100(100), W_50(50), W_10(10), W_1(1);
