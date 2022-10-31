0250d5edfa1afdc53b419d1f7d66824e71574561
farmJun <junpreme1205@gmail.com>
2022-10-31 14:38:26 +0900

    Problem1. 예외처리 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,20 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //예외처리
+        if (!isPossible(pobi) || !isPossible(crong)) {
+            return answer = -1;
+        }
+
         return answer;
+    }
+
+    public static boolean isPossible(List<Integer> name) {
+        if (name.get(0) + 1 == name.get(1) && name.size() == 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
