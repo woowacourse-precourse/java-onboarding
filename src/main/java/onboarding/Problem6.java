a3302e626b6f8bc065050bc8f67f8c47752a6edf
baksohyeon <bori12370@gmail.com>
2022-10-31 13:52:52 +0900

    feat: add variable which get headcount

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,8 +7,7 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> emails = forms.stream().map(x -> x.get(0)).collect(Collectors.toList());
         List<String> usernames = forms.stream().map(x -> x.get(1)).collect(Collectors.toList());
-
-
+        int headCount = forms.size();
 
         List<String> answer = List.of("jason@email.com", "jm@email.com", "mj@email.com");
         return answer;
