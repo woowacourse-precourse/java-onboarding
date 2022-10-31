fe2f76f8d8a8f0bba649e37203115771520035f4
vcho1958 <gusckd57@gmail.com>
2022-10-31 22:02:44 +0900

    [problem1] 요구사항에 맞춰 메소드 선언

--- src/main/java/onboarding/problem1/PageGame.java
+++ src/main/java/onboarding/problem1/PageGame.java
@@ -1,9 +1,28 @@
 package onboarding.problem1;
 
+import java.util.List;
+
 public class PageGame {
+    private Pages pobi;
+    private Pages crong;
+
+    public void join(List<Integer> pobi, List<Integer> crong){
+        this.pobi = new Pages(pobi);
+        this.crong = new Pages(crong);
+        //1. 포비와 크롱의 배열을 인자로 받아 참가자 정하기
+
+    }
+
+    public Integer getWinner(){
+        //2. 필드를 통해 구한 최댓값끼리 비교후 결과 반환하는 메소드
+        if(pobi.getMaxValue() > crong.getMaxValue()) return 1;
+        if(pobi.getMaxValue() < crong.getMaxValue()) return 2;
+        if(pobi.getMaxValue() == crong.getMaxValue()) return 0;
+        return -1;
+    }
 
 
-    //4. 포비와 크롱의 배열을 인자로 받고 3번을 통해 구한 최댓값끼리 비교후 결과 반환하는 메소드
+
 
 
 }
--- src/main/java/onboarding/problem1/Pages.java
+++ src/main/java/onboarding/problem1/Pages.java
@@ -1,12 +1,38 @@
 package onboarding.problem1;
 
+import java.util.List;
+
 public class Pages {
     //페이지 리스트
+    private List<Integer> pages;
 
-    //1.각 자리 숫자를 모두 더하는 메소드
-    //2.각 자리 숫자를 모두 곱하는 메소드
-    //3. 1, 2에 왼쪽 오른쪽 페이지를 넣고 최대 값을 반환하는 메소드
+    public Pages(List<Integer> pobi) {
+        //5. 생성자, 6사용하여 예외 체크
+        if(isFaultSet())throw new RuntimeException();
+    }
 
-    //5. 생성자, 6사용하여 예외 체크
-    //6. 나올 수 없는 페이지인지, 연속된 페이지인지 체크는 메소드
+    public Integer getMaxValue(){
+        return 0;
+        //3. 1, 2에 왼쪽 오른쪽 페이지를 넣고 최대 값을 반환하는 메소드
+    }
+
+    private boolean isFaultSet(){
+        //6. 나올 수 없는 페이지인지, 연속된 페이지인지 체크는 메소드
+        return false;
+    }
+
+    private Integer sumPageNumber(Integer pageNumber){
+        return 0;
+        //1.각 자리 숫자를 모두 더하는 메소드
+    }
+
+    private Integer multiplyPageNumber(Integer pageNumber){
+        return 0;
+        //2.각 자리 숫자를 모두 곱하는 메소드
+    }
+
+
+
+
+
 }
