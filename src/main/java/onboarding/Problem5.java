709eb64bd112792d012cc7c2cfc4d5fca281b5bd
wjh2012 <wjh2012@naver.com>
2022-10-31 18:59:13 +0900

    (PROBLEM5)단일기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int [] won = {50000,10000,5000,1000,500,100,50,10,1};
+        int count=0;
+        for (int i = 0; i < 9; i++) {
+            count= money/won[i];
+            answer.add(count);
+            money -= (count*won[i]);
+        }
+
         return answer;
     }
 }
