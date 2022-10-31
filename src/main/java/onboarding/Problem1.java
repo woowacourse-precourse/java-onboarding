b97c6b1290b35df3b741d84031ba978937a4dbb4
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 14:50:19 +0900

    crong 점수 저장할 crongscore 라는 List 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,7 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         List<Integer> pobiScore = new ArrayList<>();
+        List<Integer> crongScore = new ArrayList<>();
 
         for (int i = 0; i < pobi.size(); i++) {
             int pobiPage = pobi.get(i);
@@ -27,6 +28,8 @@ class Problem1 {
             }
             pobiScore.add(pobiPageMul);
         }
+
+
 
 
 
