36b032f3c98b7ab8b27a6fb3bd2e1b47edee6c46
Yeongseon Park <yseon406@gmail.com>
2022-11-01 01:34:59 +0900

    user와 이미 친구인 list를 반환하는 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Problem7 P = new Problem7();
+        List<String> following = P.checkFriend(user, friends);
         return answer;
+    }
+
+    // user와 이미 친구인 list를 반환하는 메서드
+    public List<String> checkFriend (String user, List<List<String>> friends) {
+        List<String> following = new ArrayList<>();
+        for(List l : friends) {
+            if(l.contains(user)){
+                following.add(String.valueOf(l.get(0)));
+            }
+        }
+        return following;
     }
 }
