8deb88ab2244e584e867dea99978a46a849d524b
Umin <adorecamus@gmail.com>
2022-11-01 00:38:39 +0900

    Add: Problem3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        //입력한 숫자까지
+        for(int i=1; i<=number; i++){
+            String numStr = String.valueOf(i);
+
+            //손뼉을 치는 횟수 (숫자에 3, 6, 9가 들어갈 때마다)
+            for(int j=0; j<numStr.length(); j++) {
+                if(numStr.charAt(j) == 51 | numStr.charAt(j) == 54 | numStr.charAt(j) == 57 )
+                    answer += 1;
+            }
+        }
+
         return answer;
     }
 }
