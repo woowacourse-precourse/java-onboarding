89ffdaec4f3d305a44fd2d9da84bc7818f642e30
Soseung Soo <sss9073@naver.com>
2022-11-01 02:42:25 +0900

    feat 'addNumber' function

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,13 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int addNumber(int num){
+        int result = 0;
+        while(num != 0){
+            result += (num % 10);
+            num /= 10;
+        }
+        return result;
+    }
 }
