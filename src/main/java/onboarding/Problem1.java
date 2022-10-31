bf7a603d3c4d25b26384b7d75793a7303de65e4d
PPakSang <sanghyun-dev@naver.com>
2022-10-31 14:53:21 +0900

    Feat: Problem1 InputValidation 추가
    validateInput(int, int): Input Value Validation 수행 메소드
    요구사항
    - 왼쪽, 오른쪽 페이지는 각각 첫, 마지막 페이지가 될 수 없다.
    - 왼쪽 페이지와 오른쪽 페이지는 연속한다.(isContinuous 참고)
    - 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수(isOdd 참고)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,48 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int START_PAGE = 1;
+    private static final int END_PAGE = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiScore;
+        int crongScore;
+
+        int pobiLeft = pobi.get(0);
+        int pobiRight = pobi.get(1);
+        int crongLeft = crong.get(0);
+        int crongRight = crong.get(1);
+
+        if (!validateInput(pobiLeft, pobiRight) || !validateInput(crongLeft, crongRight)) {
+            return -1;
+        }
+
+    }
+
+    private static boolean validateInput(int leftPage, int rightPage) {
+        if (leftPage <= START_PAGE || rightPage >= END_PAGE) {
+            return false;
+        }
+        if (!isContinuous(leftPage, rightPage)) {
+            return false;
+        }
+        if (!isOdd(leftPage) || isOdd(rightPage)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isContinuous(int leftPageNum, int rightPageNum) {
+        if ((rightPageNum - leftPageNum) != 1) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isOdd(int num) {
+        if ((num % 2) == 0) {
+            return false;
+        }
+        return true;
     }
 }
