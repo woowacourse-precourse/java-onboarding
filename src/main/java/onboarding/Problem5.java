e47616a4cf5485703b8f6e5aae3de9f27e3502a6
ParangBird <seekerman@naver.com>
2022-10-31 23:41:49 +0900

    feat: 슈도 코드에 맞게 로직 짜기 + 테스트 확인

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -16,7 +16,8 @@ public class Problem5 {
     }
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int coinList[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = getChangeCoinList(money, coinList);
         return answer;
     }
 }
