fd3391e238bf059c6a8a7d87291d25e99e2abeee
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-10-31 23:09:28 +0900

    Feat(Problem3): 수에 369 있는지 판별

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,35 @@
 package onboarding;
 
 public class Problem3 {
+    static class Number{
+        int num;
+
+        Number(int num) throws Exception {
+            if(!isValidNumber(num))
+            {
+                throw new Exception("Wrong number");
+            }
+            this.num = num;
+        }
+
+        private static boolean isValidNumber(int num) {
+            return num >= 1 && num <= 10000;
+        }
+
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+
         return answer;
+    }
+
+
+
+    private static int isClap(int n) {
+        if(n == 3 || n == 6 || n == 9) return 1;
+
+        return 0;
     }
 }
