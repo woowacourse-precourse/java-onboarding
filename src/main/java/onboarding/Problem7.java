532a47030b3e3adadc79b89ad5e92e45fa5fa488
southoftheriver <anwjsrlrhwkd@naver.com>
2022-10-31 21:03:07 +0900

    feat: Problem_7 함께 아는 친구들의 점수를 더하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,26 +7,87 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+}
 
-    private static Map<String, List<String>>toMap(List<List<String>> linkedNodes){
-        Map<String, List<String>> linkedMap = new HashMap<>();
+class FriendRecommender{
+    private String user;
+    private Map<String, Integer> recommendationScores = new HashMap<>();
 
-        for (List<String> linkedNode: linkedNodes){
-            String nodeA =  linkedNode.get(0);
-            String nodeB =  linkedNode.get(1);
+    private FriendRecommender(Builder builder){
+        this.user = builder.user;
+        addFriendScore(builder.friends);
 
-            List<String> nodesLinkedToA= linkedMap.getOrDefault(
-                    nodeA, new ArrayList<>());
-            List<String> nodesLinkedToB = linkedMap.getOrDefault(
-                    nodeB, new ArrayList<>());
+    }
 
-            nodesLinkedToA.add(nodeB);
-            nodesLinkedToB.add(nodeA);
+    private void addFriendScore(Map<String, List<String>> friends){
+        List<String> friendsOfUser = friends.get(user);
+        final Integer recommendedFriendScore = 10;
 
-            linkedMap.put(nodeA, nodesLinkedToA);
-            linkedMap.put(nodeB, nodesLinkedToB);
+        for (String friend : friendsOfUser){
+            for(String recommendedFriend : friends.get(friend)){
+                recommendationScores.put(recommendedFriend,
+                        recommendationScores.getOrDefault(recommendedFriend, 0)
+                                + recommendedFriendScore);
+            }
+        }
+    }
+
+
+
+    public static class Builder {
+
+        private String user;
+        private Map<String, List<String>> friends;
+        private List<String> visitors;
+
+        public Builder(){
         }
 
-        return linkedMap;
+        public Builder user(String user){
+            this.user = user;
+            return this;
+        }
+
+        public Builder friends(List<List<String>> friends){
+            this.friends = toMap(friends);
+            return this;
+        }
+
+        public Builder visitors(List<String> visitors){
+            this.visitors = visitors;
+            return this;
+        }
+
+        public FriendRecommender build() {
+            return new FriendRecommender(this);
+        }
+
+        private static Map<String, List<String>>toMap(List<List<String>> linkedNodes){
+            Map<String, List<String>> linkedMap = new HashMap<>();
+
+            for (List<String> linkedNode: linkedNodes){
+                String nodeA =  linkedNode.get(0);
+                String nodeB =  linkedNode.get(1);
+
+                List<String> nodesLinkedToA= linkedMap.getOrDefault(
+                        nodeA, new ArrayList<>());
+                List<String> nodesLinkedToB = linkedMap.getOrDefault(
+                        nodeB, new ArrayList<>());
+
+                nodesLinkedToA.add(nodeB);
+                nodesLinkedToB.add(nodeA);
+
+                linkedMap.put(nodeA, nodesLinkedToA);
+                linkedMap.put(nodeB, nodesLinkedToB);
+            }
+
+            return linkedMap;
+        }
     }
+
+
+
+
+
+
 }
