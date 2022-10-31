617ff3787da2060465bb7feb8269dc66d8a66656
code-tree <dltjdqls1200@naver.com>
2022-10-31 21:16:17 +0900

    feat: 유효하지 않은 입력값 체크

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        if (!checkValidInput(user, friends, visitors)) {
+            return Collections.emptyList();
+        }
+
         return answer;
+    }
+
+    private static boolean checkValidInput(String user, List<List<String>> friends, List<String> visitors) {
+        if (user.length() < 1 || user.length() > 30) {
+            return false;
+        }
+        if (friends.size() == 0 || friends.size() > 10000) {
+            return false;
+        }
+        if (visitors.size() > 10000) {
+            return false;
+        }
+
+        return true;
     }
 }
