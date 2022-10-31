25c6a3bb63774822b116bd4e4529715786bf823d
wltks98 <wltks98@hanmail.net>
2022-10-31 10:37:37 +0900

    [문제7] style : 쓰지 않는 import 제거

--- src/main/java/onboarding/problemseven/domain/Sns.java
+++ src/main/java/onboarding/problemseven/domain/Sns.java
@@ -3,7 +3,6 @@ package onboarding.problemseven.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
