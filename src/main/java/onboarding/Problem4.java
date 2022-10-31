35ccbcfc13a75bf21602847ecb63f3f1b8187630
이지영 <jioung93@naver.com>
2022-10-31 17:23:19 +0900

    comment: 기능사항 정리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+    /*
+    1. 알파벳인지 검사 후
+    2. 알파벳이라면 reversAlphabet() 실행
+    3. 알파벳이 아니라면 그대로 StringBuffer에 append
+     */
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    // 알파벳을 반대쪽 알파벳으로 변환해주는 함수 reversAlphabet()
+
 }
