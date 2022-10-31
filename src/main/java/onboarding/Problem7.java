7a9215dfa8f20ad6361ca6721ea1601882bea6f8
doehy <dbehxks02@naver.com>
2022-10-31 23:05:41 +0900

    feat : 사용자와 친구인 사람을 찾아주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String,Integer> map = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> user_friend = new ArrayList<>();
+        user_friend = findFriends(user,friends);
         return answer;
+    }
+    static List<String> findFriends(String host,List<List<String>> friends){
+        List<String> friend_info = new ArrayList<>();
+        for(List<String> info : friends){
+            if(info.get(0).equals(host)){friend_info.add(info.get(1));}
+            if(info.get(1).equals(host)){friend_info.add(info.get(0));}
+        }
+        return friend_info;
     }
 }
