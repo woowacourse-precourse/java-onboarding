9f00787ef835c6766995513a9cbe59757a43ea9d
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-31 12:45:12 +0900

    메소드 이름 변경

--- src/main/java/onboarding/prob6/domain/Transformer.java
+++ src/main/java/onboarding/prob6/domain/Transformer.java
@@ -8,7 +8,7 @@ public class Transformer {
     private Transformer() {
     }
 
-    public static List<List<String>> getLists(List<List<String>> forms) {
+    public static List<List<String>> encodeDecodLists(List<List<String>> forms) {
         List<List<String>> processed = new ArrayList<>(new ArrayList<>());
         for (List<String> form : forms) {
             processed.add(List.of(form.get(0), Decoder.decode(Encoder.encode(form.get(1)))));
