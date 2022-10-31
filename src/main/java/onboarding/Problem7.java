cd074b4b8bedeff128f04aaf11701181d55de6e4
alstjr706 <alstjr706@naver.com>
2022-11-01 06:01:12 +0900

    fix : SNS 친구 추천 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,13 +1,22 @@
 package onboarding;
 
+import onboarding.problem7.FriendListScoreCalculation;
 import onboarding.problem7.TestProblem7;
+import onboarding.problem7.TopFiveCompare;
 
 import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> result = Collections.emptyList();
 
-        List<String> answer = Collections.emptyList();
-        return answer;
+        FriendListScoreCalculation friendListScoreCalculation = new FriendListScoreCalculation();
+        HashMap<String, Integer> scores = friendListScoreCalculation.scoreCalculations(user, friends, visitors);
+
+        TopFiveCompare topFiveCompare = new TopFiveCompare();
+        topFiveCompare.sortOrder(scores);
+
+
+        return result;
     }
 }
