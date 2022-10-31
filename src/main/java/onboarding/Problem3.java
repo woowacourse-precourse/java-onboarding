b3af974071befd711bab539ed4472dcc9b50a00b
Cyma-s <ssol0319@naver.com>
2022-11-01 00:47:05 +0900

    feat : Problem3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+/*
+기능 구현 사항
+1. 3, 6, 9가 들어있는지 확인
+2. 해당 숫자에 3, 6, 9가 몇 개 있는지 확인
+3. 1~number 까지 반복하고, 값 모두 더하기
+ */
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clapCount = 0;
+        for(int i = 1; i<=number; i++) {
+            clapCount += countThreeNumber(i);
+        }
+        return clapCount;
+    }
+
+    private static int countThreeNumber(int number) {
+        String stringNumber = String.valueOf(number);
+        char[] threeNumber = {'3', '6', '9'};
+        int clapCount = 0;
+        for(int i = 0; i<stringNumber.length(); i++) {
+            for (char c : threeNumber) {
+                if (stringNumber.charAt(i) == c) {
+                    clapCount++;
+                }
+            }
+        }
+        return clapCount;
     }
 }
