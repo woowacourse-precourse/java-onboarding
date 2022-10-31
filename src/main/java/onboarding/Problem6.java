35fc642d731b0f7303f0a98ce2dd6cced9d68021
unknown <illppang@naver.com>
2022-11-01 02:48:36 +0900

    Add(김현수/Problem6): 이메일 형식 확인하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,12 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        System.out.println(forms.get(0).get(1));
+        try {
+            List<String> answer = List.of("answer");
 
-        return answer;
+            verificationEmail(forms);
+
+            return answer;
+        } catch (Exception error){
+            return (List<String>) error;
+        }
     }
+
+    public  static void verificationEmail(List<List<String>> forms) {
+        String emailPattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@email.com";
+
+        for (int i = 0; i < forms.size(); i++) {
+            if (!(Pattern.matches(emailPattern, forms.get(i).get(0)))) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+
 }
