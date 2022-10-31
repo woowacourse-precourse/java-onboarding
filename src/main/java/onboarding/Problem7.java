f4f753cd5aa635a15e127b5bc8e36494aef984a6
skfkgla <narahim.lee@gmail.com>
2022-10-31 16:17:32 +0900

    feat: Problem7 - user와 함께 아는 친구 user리스트를 문자열 리스트형으로 반환

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -9,6 +10,23 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    /**
+     * @param user    : 문자열(기준이되는 user이름)
+     * @param friends : 문자열 리스트(친구 관계 정보)
+     * @return user와 친구인 사람과 친구인 user를 문자열 리스트형으로 반환
+     */
+    private static List<String> getFriendOfFriends(String user, List<List<String>> friends) {
+        List<String> result = new ArrayList<>();
+        List<String> userFriends = getFriends(user, friends);
+        for (String userFriend : userFriends) {
+            result.addAll(getFriends(userFriend, friends));
+            if (result.contains(user)) {
+                result.remove(user);
+            }
+        }
+        return result;
     }
 
     /**
