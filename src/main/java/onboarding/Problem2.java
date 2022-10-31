42f48ecc25174d62ca9fffe5d11963a358d089ec
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-31 22:27:18 +0900

    feat: implement check duplicate char

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while(hasDuplicateChar(cryptogram)){
+            break;
+        }
+
+        return "";
     }
+
+    private static boolean hasDuplicateChar(String cryptogram) {
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i - 1) == cryptogram.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
