196bdbee00329e16a8b37bc54ca13f7f8187d856
Yejin Shin <shinyejin0212@gmail.com>
2022-10-31 16:45:11 +0900

    feat : solve problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,42 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        return answer;
+        answer = new ArrayList<>();
+        List<String> split = new ArrayList<>();
+        Set<String> freq = new HashSet<>();
+
+
+        //2문자로 쪼개기
+        for (int i =0; i < forms.size(); i++){
+            String nickname = forms.get(i).get(1);
+            for(int j = 0; j < nickname.length()-1; j++){
+                split.add(nickname.substring(j,j+2));
+            }
+        }
+        // 중복 값 추출
+        freq = split.stream().filter(i -> Collections.frequency(split, i) > 1).collect(Collectors.toSet());
+        System.out.println(freq);
+        List<String> target = new ArrayList<>(freq);
+
+        // 중복값을 포함한 이메일 추출
+        Iterator iter = freq.iterator();
+        for (int i =0; i < forms.size(); i++){
+            for (String t : target){
+                String nickname = forms.get(i).get(1);
+                if(nickname.contains(t)){
+                    answer.add(forms.get(i).get(0));
+                }
+            }
+        }
+
+        // answer(이메일) 정렬
+        Collections.sort(answer);
+            return answer;
     }
 }
 
