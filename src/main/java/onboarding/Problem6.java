0650d8dc035a746565e73dfaa082a98b64ca4a2c
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-01 04:22:09 +0900

    P6: get emails and sort email list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Problem6 {
     public static List<String> delDuplicate(List<String> emails){
@@ -34,13 +35,13 @@ public class Problem6 {
         return available;
     }
 
-    public static List<String> checkOverlapped(List<List<String>> forms, List<String> target) {
+    public static List<String> checkDuplication(List<List<String>> forms, List<String> target) {
         List<String> result = new ArrayList<String>();
         String email = target.get(0);
         String nickname = target.get(1);
         int len = 2;
 
-        while (len < nickname.length()) {
+        while (len <= nickname.length()) {
             for (List<String> form : forms) {
                 for (int i = 0; i < nickname.length() - len + 1; i++) {
                     String part = nickname.substring(i, i + len);
@@ -60,7 +61,22 @@ public class Problem6 {
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+
+        if (forms.size() > 10_000) {
+            return answer;
+        }
+
+        forms = isAvailable(forms);
+
+        for (int i = 0; i < forms.size(); i++) {
+            List<String> form = forms.get(i);
+            List<String> result = checkDuplication(forms.subList(i + 1, forms.size()), form);
+            answer.addAll(result);
+        }
+
+        answer = delDuplicate(answer);
+        Collections.sort(answer);
         return answer;
     }
 }
