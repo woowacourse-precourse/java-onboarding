2d9562070d4aeb53d02f5481c44bd68013b1fdcf
song-wooseok <sws981202@gmail.com>
2022-11-01 00:58:26 +0900

    feat: [PROBLEM6 - 예외사항6] 닉네임의 길이 검사 및 모든 예외사항 검사

--- src/main/java/onboarding/problem6/utils/Constants.java
+++ src/main/java/onboarding/problem6/utils/Constants.java
@@ -10,13 +10,19 @@ public class Constants {
 
     public static final int DOMAIN_POSITION = 10;
 
-    public static final int MIN_NICKNAME_LENGTH = 1;
+    public static final int MIN_NICKNAME_LENGTH = 0;
 
-    public static final int MAX_NICKNAME_LENGTH = 19;
+    public static final int MAX_NICKNAME_LENGTH = 20;
+
+    public static final int MIN_EMAIL_LENGTH = 10;
+
+    public static final int MAX_EMAIL_LENGTH = 20;
 
     public static final int LEAST_DUPLICATED_WORD_LENGTH = 2;
 
     public static final String DOMAIN = "@email.com";
 
     public static final String HANGUL_REGEX = "^[ㄱ-ㅎ가-힣]*$";
+
+    public static final String EMAIL_REGEX = "[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\\\w+\\\\.)+\\\\w+$";
 }
