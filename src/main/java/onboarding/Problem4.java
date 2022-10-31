130c30ddf569dfa018623062613ee3d09be36e98
UngHeung <t01047377477@gmail.com>
2022-11-01 01:30:53 +0900

    feat : (problem 4) 문자를 매칭해주는 기능 추가
    문자의 대소문자 확인 및
    해당 문자와 매칭되는 문자로 변환해준 후
    빌더에 순서대로 저장해주는 메소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+
+	public static Queue<Character>queue = new LinkedList<>();
+
+	public static String solution(String word) {
+		String answer = "";
+
+		queue = wordCheck(word);
+
+		return answer;
+	}
+
+	public static Queue<Character> wordCheck(String word) {
+
+		for(int i = 0; i < word.length(); i++) {
+			char character = word.charAt(i);
+			int distance = 0;
+
+			if(65 <= character && character <= 90) {
+				distance = 90 - character;
+				character = (char) (65 + distance);
+			} else if(97 <= character && character <= 122) {
+				distance = 122 - character;
+				character = (char) (97 + distance);
+			}
+
+			queue.offer(character);
+		}
+
+		return queue;
+	}
+
 }
