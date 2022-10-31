1a4b078d16f0abb5ad41932a4a54104e776db98e
leesangjin <tkdwls0628@gmail.com>
2022-10-31 18:28:10 +0900

    P1, 각 리스트의 최대값 구하기 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -33,7 +33,21 @@ class Problem1 {
     }
 
     private static int calculateMaxValue(List<Integer> list) {
-        return 0;
+        int maxValue = 0;
+
+        for (Integer integer : list) {
+            int maxValueOfPlus = 0;
+            int maxValueOfMultiply = 1;
+
+            while(integer > 0){
+                maxValueOfPlus += integer % 10;
+                maxValueOfMultiply *= integer % 10;
+                integer /= 10;
+            }
+            maxValue = Math.max(maxValue, Math.max(maxValueOfPlus, maxValueOfMultiply));
+        }
+
+        return maxValue;
     }
 
     private static int calculateResult(int maxValueOfPobi, int maxValueOfCrong) {
