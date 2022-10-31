a332df2f5f769b149faae8e55293c70579e666db
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:35:04 +0900

    6번: 도메인 상수로 빼기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    private static final String DOMAIN = "@email.com";
+    
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
