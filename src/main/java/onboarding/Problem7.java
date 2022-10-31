1db865fb9bd998b1730b777276e58088608629ef
5nam <ohnam90@gmail.com>
2022-10-31 14:18:06 +0900

    feat: Freind 내부 클래스 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    private static class Friend {
+        private int score;
+        private String name;
+
+        public Friend(int score, String name) {
+            this.score = score;
+            this.name = name;
+        }
+
+        public int getScore() {
+            return score;
+        }
+        public String getName() {
+            return name;
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
