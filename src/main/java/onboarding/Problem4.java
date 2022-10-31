d0c12a3142f3b6ea9960e5879637e7f8154cccd3
CAEL0 <kjongmin26@gmail.com>
2022-10-31 15:51:54 +0900

    기능 추가: PROBLEM4 문자열을 규칙대로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,8 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            char transformedLetter = transform(letter);
+            sb.append(transformedLetter);
+        }
+        return sb.toString();
     }
     private static char transform(char c) {
         if ('a' <= c && c <= 'z')
