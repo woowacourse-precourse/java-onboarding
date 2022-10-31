eb9de022166c8a38e90a05956447782d68d5bcc3
es <mascoeskim@gmail.com>
2022-10-31 11:14:02 +0900

    Problem4/1.초기세팅 아스키코드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,10 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        ////////////아스키코드 A=65~Z=90, a=97~z=122///////////////
+        System.out.println(word);
+        char wordChar = word.charAt(0);
+        System.out.println(wordChar);
         String answer = "";
         return answer;
     }
