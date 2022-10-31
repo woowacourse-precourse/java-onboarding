dfb0c8a8c2649fc5d9b08ec488c43e9a37bbeac9
이규승 <rpg8988@naver.com>
2022-10-31 21:36:48 +0900

    feat : pobi의 책 페이지 및 가장 큰 수 계산 메소드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,45 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    public static int calpobi(int pobi1, int pobi2) {
+        int Respobi = 0;
+
+        int a1 = pobi1 / 100;
+        int b1 = (pobi1 % 100) / 10;
+        int c1 = pobi1 % 10;
+
+        int a2 = pobi2 / 100;
+        int b2 = (pobi2 % 100) / 10;
+        int c2 = pobi2 % 10;
+
+        if (a1 != 0) {
+            int multpobi1T = a1 * b1 * c1;
+            int pluspobi1T = a1 + b1 + c1;
+
+            int multpobi2T = a2 * b2 * c2;
+            int pluspobi2T = a2 + b2 + c2;
+
+            int max_mult = Math.max(multpobi1T, multpobi2T);
+            int max_plus = Math.max(pluspobi2T, pluspobi1T);
+
+            Respobi = Math.max(max_plus, max_mult);
+        }
+
+        else {
+            if (b1 != 0) {
+                int multpobi1D = b1 * c1;
+                int pluspobi1D = b1 + c1;
+
+                int multpobi2D = b2 * c2;
+                int pluspobi2D = b2 + c2;
+
+                int max_mult = Math.max(multpobi1D, multpobi2D);
+                int max_plus = Math.max(pluspobi2D, pluspobi1D);
+
+                Respobi = Math.max(max_plus, max_mult);
+            }
+        }
+        return Respobi;
+    }
 }
