110f4d8e6a4932bfb586c4f2edde1f2300937e7d
D <doxxx93@gmail.com>
2022-10-31 13:28:00 +0900

    Problem3 (#5)
    * docs: add requirement checklist
    
    * feat: solve problem3
    
    * refactor: replace for loop to stream
    
    * refactor: remove unnecessary constants

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
+
+    private static final int THREE = 3;
+    private static final int TEN = 10;
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return IntStream.rangeClosed(1, number)
+            .reduce(0,(total, value) -> total + getCount(value));
+    }
+
+    private static int getCount(int number) {
+        int count = 0;
+        while (number > 0) {
+            int placeNumber = number % TEN;
+            if (isThreeSixNine(placeNumber)) {
+                count++;
+            }
+            number /= TEN;
+        }
+        return count;
+    }
+
+    private static boolean isThreeSixNine(int placeNumber) {
+        return placeNumber % THREE == 0 && placeNumber != 0;
     }
 }
