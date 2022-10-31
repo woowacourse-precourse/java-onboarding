055d7c5792dee7fe0f704a28a312d9f8f40da5a6
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-10-31 23:04:37 +0900

    1번 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,51 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean hasError(List<Integer> list) {
+        int left = list.get(0);
+        int right = list.get(1);
+
+        // 페이지의 범위를 벗어나지 않는가
+        if(left < 1 || left > 400) return true;
+        if(right < 1 || right > 400) return true;
+
+        // 왼쪽 페이지가 홀수인가
+        if (left % 2 == 0) return true;
+
+        // 두 수가 연속적인가
+        return (right - left != 1);
+    }
+
+    public static int sumPage(List<Integer> list){
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            char[] chars = list.get(i).toString().toCharArray();
+            for(char x : chars){
+                sum += Integer.parseInt(String.valueOf(x));
+            }
+        }
+        return sum;
+    }
+    public static int multiplyPage(List<Integer> list){
+        int mul = 1;
+        for (int i = 0; i < list.size(); i++) {
+            char[] chars = list.get(i).toString().toCharArray();
+            for(char x : chars){
+                mul *= Integer.parseInt(String.valueOf(x));
+            }
+        }
+        return mul;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if(hasError(pobi) || hasError(crong)) return -1;
+
+        int scorePobi = Math.max(sumPage(pobi),multiplyPage(pobi));
+        int scoreCrong = Math.max(sumPage(crong),multiplyPage(crong));
+
+        if(scorePobi > scoreCrong) return 1;
+        if(scoreCrong > scorePobi) return 2;
+
+        return 0;
     }
 }
