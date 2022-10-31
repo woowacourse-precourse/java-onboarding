d2d3525f31a581d07e6b725d672926cd908d869c
sjkim <seoungjoo01@gmail.com>
2022-10-31 20:49:29 +0900

    [feat] 7번 문제 : 사용자와 함께 아는 친구 점수 부여 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 public class Problem7 {
 
+    /**
+     * 친구 관계 정보를 저장하는 map 생성 함수
+     * @param friends : 친구 정보
+     * @return 친구 관계 map
+     */
     public static Map<String, List<String>> makeFriendInfoMap(List<List<String>> friends){
 
         Map<String, List<String>> snsFriendMap = new HashMap<>();
@@ -32,10 +37,34 @@ public class Problem7 {
 
     }
 
+    /**
+     * 함께 아는 사람 수 점수 추가 함수
+     * @param snsFriendMap : 친구 관계 map
+     * @param user : 사용자 정보
+     * @return 점수 정보 map
+     */
+    public static Map<String, Integer> calcBothFriendPoint( Map<String, List<String>> snsFriendMap, String user){
+        Map<String,Integer> score = new HashMap<>();
+        for(String userFriend : snsFriendMap.getOrDefault(user, new ArrayList<>())){
+            for (String relatedFriend:snsFriendMap.getOrDefault(userFriend, new ArrayList<>())){
+                if (!relatedFriend.equals(user)){
+                    if(score.containsKey(relatedFriend)){
+                        score.put(relatedFriend, score.get(relatedFriend)+10);
+                    }else{
+                        score.put(relatedFriend, 10);
+                    }
+                }
+            }
+        }
+        return score;
+    }
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
 
         Map<String, List<String>> snsFriendMap = makeFriendInfoMap(friends);
+        Map<String,Integer> score = calcBothFriendPoint(snsFriendMap, user);
 
         return answer;
     }
