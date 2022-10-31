1fd73259a6e1401a2910e62266cfefafc0a051ff
codingvegeta <hong090992@gmail.com>
2022-10-31 19:48:26 +0900

    feat : soultion을 해결하기 위한 함수를 구현했습니다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number ; i++) {
+            String str = String.valueOf(i);
+            if (str.contains("3")) {
+                answer += str.length() - str.replaceAll("3", "").length();
+            } else if (str.contains("6")) {
+                answer += str.length() - str.replaceAll("6", "").length();
+            } else if (str.contains("9")) {
+                answer += str.length() - str.replaceAll("9", "").length();
+            }
+        }
         return answer;
     }
 }
