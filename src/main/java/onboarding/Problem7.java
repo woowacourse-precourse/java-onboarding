9d5e322a02039b89c8db177c40a39e64e9b7210d
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-31 21:26:30 +0900

    (etc: Problem7) 기능 요구사항 정리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * [ ] 친구 추천 규칙에 따라 점수가 가장 높은 순으로 5명을 return한다.
+ * [ ] 추천 점수가 0인 경우에는 추천하지 않는다.
+ * [ ] 추천 점수가 같은 경우에는 이름 순으로 정렬한다.
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
