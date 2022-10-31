12a7f645fab1ae08eb9e696cd21b35bba6a0dc1a
ouohoon <begreat@kakao.com>
2022-11-01 04:41:00 +0900

    리팩토링: 문제7 FriendRecommender 클래스 public 클래스로 변경, 사용하지 않는 import 삭제

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,8 @@
 package onboarding;
 
+import onboarding.problem7.FriendRecommender;
+
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Problem7 {
 
@@ -16,86 +17,5 @@ public class Problem7 {
         FriendRecommender friendRecommender = new FriendRecommender();
 
         return friendRecommender.recommend(user, friends, visitors);
-    }
-
-    private static class FriendRecommender {
-        private Map<String, Map<String, Boolean>> friendsRelation;
-        private Map<String, Integer> friendsScore;
-
-        public List<String> recommend(String user, List<List<String>> friends, List<String> visitors) {
-            setFriendsRelation(user, friends);
-            setFriendScore(user);
-            setVisitScore(user, visitors);
-
-            List<Map.Entry<String, Integer>> friendsScoreList = new ArrayList<>(friendsScore.entrySet());
-
-            friendsScoreList.sort(new StringIntPair());
-
-            return friendsScoreList.stream().map(Map.Entry::getKey).collect(Collectors.toList());
-        }
-
-        private void setVisitScore(String user, List<String> visitors) {
-            Set<String> userFriends = friendsRelation.get(user).keySet();
-            for (String visitor : visitors) {
-                if (friendsScore.containsKey(visitor)) {
-                    friendsScore.put(visitor, friendsScore.get(visitor) + 1);
-                } else if (!userFriends.contains(visitor) && !visitor.equals(user)){
-                    friendsScore.put(visitor, 1);
-                }
-            }
-        }
-
-        private void setFriendScore(String user) {
-            friendsScore = new HashMap<>();
-            Set<String> userFriends = friendsRelation.get(user).keySet();
-            for (String friend : friendsRelation.keySet()) {
-                if (friend.equals(user) || userFriends.contains(friend)) {
-                    continue;
-                }
-                int friendScore = 0;
-                for (String other : friendsRelation.get(friend).keySet()) {
-                    if (userFriends.contains(other)) {
-                        friendScore += 10;
-                    }
-                }
-                if (!friendsScore.containsKey(friend)) {
-                    friendsScore.put(friend, friendScore);
-                } else {
-                    friendsScore.put(friend, friendScore + friendsScore.get(friend));
-                }
-            }
-
-        }
-
-        private void setFriendsRelation(String user, List<List<String>> friends) {
-            friendsRelation = new HashMap<>();
-            for (List<String> friendRelation : friends) {
-                String friendA = friendRelation.get(0);
-                String friendB = friendRelation.get(1);
-
-                boolean isExistA = friendsRelation.containsKey(friendA);
-                boolean isExistB = friendsRelation.containsKey(friendB);
-
-                if (!isExistA) {
-                    friendsRelation.put(friendA, new HashMap<>());
-                }
-                Map<String, Boolean> aFriends = friendsRelation.get(friendA);
-                aFriends.put(friendB, true);
-
-                if (!isExistB) {
-                    friendsRelation.put(friendB, new HashMap<>());
-                }
-                Map<String, Boolean> bFriends = friendsRelation.get(friendB);
-                bFriends.put(friendA, true);
-            }
-        }
-    }
-
-    private static class StringIntPair implements Comparator<Map.Entry<String, Integer>> {
-        @Override
-        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-            return o1.getValue() - o2.getValue() == 0 ?
-                    o1.getKey().compareTo(o2.getKey()) : -(o1.getValue() - o2.getValue());
-        }
     }
 }
---
+++ src/main/java/onboarding/problem7/FriendRecommender.java
@@ -0,0 +1,95 @@
+package onboarding.problem7;
+
+
+import java.util.*;
+
+public class FriendRecommender {
+    private final int MAX_RECOMMEND_SIZE = 5;
+    private Map<String, Map<String, Boolean>> friendsRelation;
+    private Map<String, Integer> friendsScore;
+
+    public List<String> recommend(String user, List<List<String>> friends, List<String> visitors) {
+        setFriendsRelation(friends);
+        setFriendScore(user);
+        setVisitScore(user, visitors);
+
+        List<Map.Entry<String, Integer>> friendsScoreList = new ArrayList<>(friendsScore.entrySet());
+
+        friendsScoreList.sort(new StringIntPair());
+        List<String> result = new ArrayList<>();
+        int resultSize = 0;
+        for (Map.Entry<String, Integer> recommendFriend : friendsScoreList) {
+            if (resultSize >= MAX_RECOMMEND_SIZE) {
+                break;
+            }
+            result.add(recommendFriend.getKey());
+            resultSize++;
+        }
+
+        return result;
+    }
+
+    private void setVisitScore(String user, List<String> visitors) {
+        Set<String> userFriends = friendsRelation.get(user).keySet();
+        for (String visitor : visitors) {
+            if (friendsScore.containsKey(visitor)) {
+                friendsScore.put(visitor, friendsScore.get(visitor) + 1);
+            } else if (!userFriends.contains(visitor) && !visitor.equals(user)) {
+                friendsScore.put(visitor, 1);
+            }
+        }
+    }
+
+    private void setFriendScore(String user) {
+        friendsScore = new HashMap<>();
+        Set<String> userFriends = friendsRelation.get(user).keySet();
+        for (String friend : friendsRelation.keySet()) {
+            if (friend.equals(user) || userFriends.contains(friend)) {
+                continue;
+            }
+            int friendScore = 0;
+            for (String other : friendsRelation.get(friend).keySet()) {
+                if (userFriends.contains(other)) {
+                    friendScore += 10;
+                }
+            }
+            if (!friendsScore.containsKey(friend)) {
+                friendsScore.put(friend, friendScore);
+            } else {
+                friendsScore.put(friend, friendScore + friendsScore.get(friend));
+            }
+        }
+
+    }
+
+    private void setFriendsRelation(List<List<String>> friends) {
+        friendsRelation = new HashMap<>();
+        for (List<String> friendRelation : friends) {
+            String friendA = friendRelation.get(0);
+            String friendB = friendRelation.get(1);
+
+            boolean isExistA = friendsRelation.containsKey(friendA);
+            boolean isExistB = friendsRelation.containsKey(friendB);
+
+            if (!isExistA) {
+                friendsRelation.put(friendA, new HashMap<>());
+            }
+            Map<String, Boolean> aFriends = friendsRelation.get(friendA);
+            aFriends.put(friendB, true);
+
+            if (!isExistB) {
+                friendsRelation.put(friendB, new HashMap<>());
+            }
+            Map<String, Boolean> bFriends = friendsRelation.get(friendB);
+            bFriends.put(friendA, true);
+        }
+    }
+
+    private static class StringIntPair implements Comparator<Map.Entry<String, Integer>> {
+        @Override
+        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            return o1.getValue() - o2.getValue() == 0 ?
+                    o1.getKey().compareTo(o2.getKey()) : -(o1.getValue() - o2.getValue());
+        }
+    }
+}
