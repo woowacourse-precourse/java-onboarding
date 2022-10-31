1c42a80878056cd87697dfbb0c1f5b5a1f49ea9c
sami355-24 <ddung355@gmail.com>
2022-11-01 01:16:32 +0900

    [add] isInRange and TestisInRange

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,11 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static boolean inInRange(Integer number){
+        if(number >= 1 && number <= 10000)
+            return true;
+
+        return false;
+    }
 }
