4d8c37568c17137e12dd800fd9f8e0d1d6ddb268
saint6839 <saint6839@gmail.com>
2022-10-31 16:54:21 +0900

    feat(Problem5): 화폐 가치들의 값을 담은 배열 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,11 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        int[] moneyValues = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        return null;
     }
 }
