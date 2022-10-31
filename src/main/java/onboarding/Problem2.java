85806e8d6dc60bd5576f15adbdb02644b59287bf
sun007021 <sun007021@gmail.com>
2022-11-01 04:18:36 +0900

    feat: 연속되는 문자 제거한 스택 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeDuplicateChar(cryptogram);
         return answer;
     }
+
+    private static Stack<Character> makeStackNoDuplicateChar(String string) {// 연속하는 중복 문자들 제거한 스택
+        Stack<Character> resultStringStack = new Stack<>();
+        Character prevChar;
+        Character currentChar;
+        int i=0;
+        while (i < string.length()) {
+            currentChar = string.charAt(i);
+            if(resultStringStack.empty()){
+                resultStringStack.push(currentChar);
+                i++;
+            }
+            else{
+                prevChar = resultStringStack.peek();
+                if(prevChar.equals(currentChar)){//같은 문자면 제거
+                    while(i<string.length() && prevChar.equals(string.charAt(i))){//다른문자 나올때까지 한칸씩 이동
+                        i++;
+                    }
+                    resultStringStack.pop();
+                }
+                else{// 같은문자 아니면 스택에 push
+                    resultStringStack.push(currentChar);
+                    i++;
+                }
+            }
+        }
+        return resultStringStack;
+    }
+
 }
