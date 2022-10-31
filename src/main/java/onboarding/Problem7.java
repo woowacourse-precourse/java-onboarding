93b9f0ec743fa86b11e572c92308677550f6255d
tjddnr7760 <tjddnr7760@naver.com>
2022-10-31 10:33:19 +0900

    <7번 - 구현> 사용자 제외 맵 사람목록 자료구조 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,35 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Problem7 problem = new Problem7();
+        HashMap<String, Integer> listpeople;
+
+        listpeople = problem.extractPeopleMap(user, friends, visitors);
+
         return answer;
+    }
+
+    public HashMap<String, Integer> extractPeopleMap(String user, List<List<String>> friends, List<String> visitors) {
+        HashMap<String, Integer> result = new HashMap<>();
+
+        for(List e : friends) {
+            for(Object s : e) {
+                result.put((String) s, 0);
+            }
+        }
+
+        for(String e : visitors) {
+            result.put(e, 0);
+        }
+
+        result.remove(user);
+
+        return result;
     }
 }
