e16c904cdaa12f73587271de553e004117c1b159
paran <paran@hoon>
2022-10-31 13:48:34 +0900

    2번 : 오류 수정 및 refactoring

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,24 +8,22 @@ public class Problem2 {
     	
     	boolean isChange = true;
     	StringBuilder crypto = new StringBuilder(cryptogram);
-    	char cryptocheck ;
     	
     	while(isChange) {
     		int cnt = 0;
-	    	try {
-	    		for(int i = 0; i<cryptogram.length();i++) {
-	    			cryptocheck = crypto.charAt(i);
-	    			if(crypto.charAt(i) == crypto.charAt(i+1)) {
-	    				crypto.deleteCharAt(i+1);
-	    				crypto.deleteCharAt(i);
-	    				cnt ++;
-	    			}
-	    		}
-			} catch (Exception e) {
-				System.out.println("오류발생 " + e.getMessage());
-			}
-	    	cryptogram = crypto.toString();
-	    	
+    		for(int i = 0; i<cryptogram.length();i++) {
+    			System.out.println("cryptogrma length-> " + cryptogram.length());
+    			char cryptocheck = crypto.charAt(i);
+    			if(i < cryptogram.length()-1) {
+    				if(crypto.charAt(i) == crypto.charAt(i+1)) {
+    					crypto.deleteCharAt(i+1);
+    					crypto.deleteCharAt(i);
+    					cnt ++;	    		
+    					cryptogram = crypto.toString();
+    					break;
+    				}		
+    			}
+    		}
 	    	if(cnt == 0) {
 	    		isChange = false;
 	    	}
