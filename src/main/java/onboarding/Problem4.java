d76bdc347acdc56a04b796baa3bafeafb3a3a0ce
doehy <dbehxks02@naver.com>
2022-10-31 21:46:55 +0900

    feat : word를 index 0 부터 하나씩 알파벳인지 확인

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i<word.length(); i++){
+            char word_char = word.charAt(i);
+            if(checkChar(word_char)) {
+                answer += changeWord(word_char);
+            }
+            else{answer += word_char;}
+        }
         return answer;
+    }
+    static boolean checkChar(char ch){
+        if (ch >= 'a' && ch <= 'z'){return true;}
+        if (ch >= 'A' && ch <= 'Z'){return true;}
+        return false;
     }
 }
