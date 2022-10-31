95cd4036b3a7cd64f6eb3030ca3c6563bc7a9942
iamsseaa <homingsea@gmail.com>
2022-10-31 18:52:19 +0900

    feat(Problem7): 추천 친구 점수를 더해주는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,16 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
+        final int friends_to_friends_count = 10;
+        final int visitors_count = 1;
+        
         HashMap<String, Integer> friendRecommendationMap = new HashMap<>();
 
         List<String> usersFriends = new ArrayList<>(findUserFriend(friends, user));
         List<String> usersFriendsToFriends = new ArrayList<>(findUserFriendToFriend(friends, usersFriends, user));
+        countFriendsShip(friendRecommendationMap, usersFriendsToFriends, friends_to_friends_count);
+        countFriendsShip(friendRecommendationMap, visitors, visitors_count);
+
 
         List<String> answer = Collections.emptyList();
         return answer;
@@ -37,5 +43,15 @@ public class Problem7 {
 
         usersFriendToFriends.remove(user);
         return usersFriendToFriends;
+    }
+
+    public static void countFriendsShip(HashMap<String, Integer> friendRecommendationMap, List<String> friendsToFriends, int count) {
+        for (String name : friendsToFriends) {
+            if (friendRecommendationMap.containsKey(name)) {
+                friendRecommendationMap.put(name, friendRecommendationMap.get(name) + count);
+                continue;
+            }
+            friendRecommendationMap.put(name, count);
+        }
     }
 }
