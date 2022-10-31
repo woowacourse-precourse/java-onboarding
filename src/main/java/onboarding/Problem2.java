df7a936e062a095e2e9a7f57e4e16dfb812e9c9c
Nugur1i <duckhee092@naver.com>
2022-10-31 15:06:55 +0900

    feat: Add exception 2 (small letter)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,6 +5,22 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         if (cryptogram.length()>1000 || cryptogram.length()<1){
             System.out.println("Please, enter string length between 1 ~ 1000 !");
+            return cryptogram + "A"; //return error
+        }
+
+        int lengthCrypto = cryptogram.length();
+
+        while (lengthCrypto > 0 ){
+
+            int numASCII = cryptogram.charAt( lengthCrypto - 1 );
+
+            if ( numASCII < 97 || numASCII > 122 ) {
+                System.out.println("Please, enter small letter !");
+                return cryptogram + "A"; //return error
+            }
+
+            lengthCrypto -= 1;
+
         }
 
 
