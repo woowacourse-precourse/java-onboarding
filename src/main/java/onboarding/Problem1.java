4828e4efff032b869494459845d5d85343fc6524
WhoIsWoony <WhoIsWoony@gmail.com>
2022-10-31 17:16:01 +0900

    problem1: TODO-'예외사항2' complete

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,7 @@ import java.util.List;
 class Problem1 {
     private static boolean checkCorrectOf(List<Integer> player){
         if(player.get(1) - player.get(0) != 1) return false;
+        else if(player.get(0)==null || player.get(1) ==null) return false;
         return true;
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
