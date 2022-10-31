1cc4977d3f9ee9add686fdd8d79356558661f565
5onchangwoo <scw9410@gmail.com>
2022-10-31 09:21:54 +0900

    problem4/char문자열을 치환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -11,4 +11,23 @@ public class Problem4 {
         return word.toCharArray();
     }
 
+    // 치환 로직
+    public static char transChar(char c){
+        char result;
+        // 대문자인 경우 65 ~ 90 소문자인 경우 97 ~ 122
+        if (!Character.isLetter(c)) {
+            return c;
+        }
+        if (Character.isUpperCase(c)) {
+            int total = 155;
+            result = (char) (total - c);
+        }
+        else {
+            int total = 219;
+            result = (char) (total - c);
+        }
+        return result;
+    }
+
+
 }
