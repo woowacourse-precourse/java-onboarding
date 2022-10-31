936aaa591a74711ecc6323dd4e2159b2a684b649
saint6839 <saint6839@gmail.com>
2022-10-31 15:55:18 +0900

    feat(Problem3): 문자열로 변경된 숫자의 각 자리수를 반복문을 통해 검사하고 결과를 반환하도록 구현
    - 3, 6, 9에 일치한다면 result 값을 1씩 증가시킴

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,16 @@ public class Problem3 {
 
         for (int i = 1; i <= number; i++) {
             String value = String.valueOf(i);
+            result = validateHandclap(value, result);
+        }
+        return result;
+    }
+
+    private static int validateHandclap(String value, int result) {
+        for (int i = 0; i < value.length(); i++) {
+            if (value.charAt(i) == '3' || value.charAt(i) == '6' || value.charAt(i) == '9') {
+                result++;
+            }
         }
         return result;
     }
