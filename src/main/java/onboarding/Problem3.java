3b4a0856cbb14aeca70736e6d8888a40f76ca8fa
김선동 <mok03127@hanyang.ac.kr>
2022-11-01 00:50:20 +0900

    feat: 문제 3 숫자를 자릿수 별로 확인하여 횟수 구하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,12 @@ public class Problem3 {
          * 숫자를 자릿수 별로 확인하여 3,6,9인지 확인하여 횟수 구한다.
          */
         int answer = 0;
-        
+
+        for (int i = 1; i <= number; i++) {
+            String strNum = String.valueOf(i);
+            answer += countClaps(strNum);
+        }
+
         return answer;
     }
 
@@ -16,5 +21,19 @@ public class Problem3 {
         } else {
             return false;
         }
+    }
+
+    public static int countClaps(String strNum) {
+        int cnt = 0;
+
+        for (int i = 0; i < strNum.length(); i++) {
+            int num = strNum.charAt(i) - '0';
+
+            if (is369(num)) {
+                cnt += 1;
+            }
+        }
+
+        return cnt;
     }
 }
