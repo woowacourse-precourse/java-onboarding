50115c01092d727cbd77148b5da65e26c36ad721
TaeHwan <kimta2hwan@gmail.com>
2022-11-01 00:45:34 +0900

    feat(PROBLEM4): 알파벳 문자를 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
+
+    static final char[] UPPER = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    static final char[] LOWER = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    static int conversionLower(char c) {
+        return 25 - (c - 'a');
+    }
+
+    static int conversionUpper(char c) {
+        return 25 - (c - 'A');
     }
 }
