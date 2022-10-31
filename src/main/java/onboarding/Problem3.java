2119f27af32def9715b594d051216b03f5d72d25
소대훈 <theogns0681@naver.com>
2022-10-31 18:14:42 +0900

    [feat] (문제3) 숫자 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,9 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int input = 1; input <= number ;input++){
+            //answer+= countingNumber(input);
+        }
         return answer;
     }
 }
