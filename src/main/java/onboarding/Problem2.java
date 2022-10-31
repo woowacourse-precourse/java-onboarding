bdffe0b125e32dfb2bcc4888d217f95768bc34ab
watterricecake <fruturum@nate.com>
2022-10-31 16:14:41 +0900

    feat: Problem2 중복된 char 3개 이상일 경우 고려하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,19 +5,36 @@ import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "";
-        Stack<Character> stack = new Stack<Character>();
-        int count = cryptogram.length();
-        char chr;
-        stack.add(cryptogram.charAt(0));
-        for(int i = 1;i<count;i++){
-            chr = stack.pop();
-            if(cryptogram.charAt(i) != chr){
-                stack.add(chr);
-                stack.add(cryptogram.charAt(i));
+        Stack<Character> stack = new Stack<>();
+        boolean check = false;
+        int now = 0;
+        while(now<cryptogram.length()){
+            if(stack.size() !=0){
+                if(stack.size() !=0) {
+                    if (!stack.get(stack.size()-1).equals(cryptogram.charAt(now))) {
+                        if(check){
+                            stack.pop();
+                        }else{
+                            stack.add(cryptogram.charAt(now));
+                            now ++;
+                        }
+                        check =false;
+                    }else{
+                        check =true;
+                        now ++;
+                    }
+                }
+            }else{
+                stack.add(cryptogram.charAt(now));
+                now++;
             }
+            check = check;
         }
-        for(char i: stack){
-            answer += i;
+        if(check){
+            stack.pop();
+        }
+        for(char c: stack){
+            answer += c;
         }
         return answer;
     }
