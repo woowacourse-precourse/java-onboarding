3ff3faf401ce88cf01ba57c17df328f124ab866b
0417taehyun <0417taehyun@gmail.com>
2022-10-31 18:02:12 +0900

    feat: [문제1] 메서드 및 풀이 구조 구축

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,34 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean isValidParameter(List<Integer> pages) {
+
+    }
+
+    public static int getEachMaximumNumber(Integer page) {
+
+    }
+
+    public static int getMaximumNumber(List<Integer> pages) {
+
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = -1;
+
+        if (isValidParameter(pobi) && isValidParameter(crong)) {
+            int maximumPobi = getMaximumNumber(pobi);
+            int maximumCrong = getMaximumNumber(crong);
+
+            if (maximumPobi == maximumCrong) {
+                answer = 0;
+            } else if (maximumPobi > maximumCrong) {
+                answer = 1;
+            } else {
+                answer = 2;
+            }
+        }
+
         return answer;
     }
 }
