9404205852c0dbbcf01e1fa117fbfc2984cf2134
CMHyuk <alsgur9710@naver.com>
2022-10-31 20:34:32 +0900

    refactor: for문 -> lambda, 방문자 체크 로직 메서드로 추출, 변수명 변경

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,9 +9,9 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer;
 
-        ArrayList<String> withFriends = storeUserFriends(user, friends);
-        Map<String, Integer> friendAndScore = storeFriendAndScore(user, friends, withFriends);
-        visitScore(visitors, withFriends, friendAndScore);
+        ArrayList<String> userFriends = storeUserFriends(user, friends);
+        Map<String, Integer> friendAndScore = storeFriendAndScore(user, friends, userFriends);
+        visitScore(visitors, userFriends, friendAndScore);
         answer = mapToList(friendAndScore);
 
         return answer;
@@ -24,55 +24,72 @@ public class Problem7 {
     }
 
     private static void visitScore(List<String> visitors, ArrayList<String> withFriends, Map<String, Integer> friendScore) {
-        for (int i = 0; i < visitors.size(); i++) {
-            String visitor = visitors.get(i);
-            if (friendScore.containsKey(visitor)) {
-                Integer score = friendScore.get(visitor);
-                score++;
-                friendScore.replace(visitor, score);
-            }
-            if (!withFriends.contains(visitor)) {
-                if (!friendScore.containsKey(visitor)) {
-                    friendScore.put(visitor, VISITOR_SCORE);
-                }
+        visitors.forEach(visitor -> {
+            existVisitor(withFriends, friendScore, visitor);
+        });
+
+    }
+
+    private static void existVisitor(ArrayList<String> withFriends, Map<String, Integer> friendScore, String visitor) {
+        if (friendScore.containsKey(visitor)) {
+            Integer score = friendScore.get(visitor);
+            score++;
+            friendScore.replace(visitor, score);
+        }
+        if (!withFriends.contains(visitor)) {
+            if (!friendScore.containsKey(visitor)) {
+                friendScore.put(visitor, VISITOR_SCORE);
             }
         }
     }
 
     private static Map<String, Integer> storeFriendAndScore(String user, List<List<String>> friends, ArrayList<String> withFriends) {
         Map<String, Integer> friendScore = new HashMap<>();
-        for (int i = 0; i < withFriends.size(); i++) {
-            String withFriend = withFriends.get(i);
-            for (int j = 0; j < friends.size(); j++) {
-                List<String> knowFriendList = friends.get(j);
-                if (knowFriendList.contains(withFriend)) {
-                    for (int k = 0; k < knowFriendList.size(); k++) {
-                        String knowFriend = knowFriendList.get(k);
-                        if (!withFriend.equals(knowFriend) && !knowFriendList.contains(user)) {
-                            if (!friendScore.containsKey(knowFriend)) {
-                                friendScore.put(knowFriend, FRIEND_SCORE);
-                            }
-                        }
-                    }
-                }
+        withFriends.forEach(userFriend -> {
+            friends.forEach(knowFriendList -> {
+                calculateVisitPoint(user, friendScore, userFriend, knowFriendList);
+            });
+        });
+
+        return friendScore;
+    }
+
+    private static void calculateVisitPoint(String user, Map<String, Integer> friendScore, String userFriend, List<String> knowFriendList) {
+        if (knowFriendList.contains(userFriend)) {
+            knowFriendList.forEach(knowFriend -> {
+                calculateFriendPoint(user, friendScore, userFriend, knowFriendList, knowFriend);
+            });
+        }
+    }
+
+    private static void calculateFriendPoint(String user, Map<String, Integer> friendScore, String userFriend, List<String> knowFriendList, String knowFriend) {
+        if (!userFriend.equals(knowFriend) && !knowFriendList.contains(user)) {
+            if (!friendScore.containsKey(knowFriend)) {
+                friendScore.put(knowFriend, FRIEND_SCORE);
             }
         }
-        return friendScore;
     }
 
     private static ArrayList<String> storeUserFriends(String user, List<List<String>> friends) {
         ArrayList<String> withFriends = new ArrayList<>();
-        for (int i = 0; i < friends.size(); i++) {
-            List<String> friendList = friends.get(i);
-            if (friendList.contains(user)) {
-                for (int j = 0; j < friendList.size(); j++) {
-                    String friend = friendList.get(j);
-                    if (!user.equals(friend)) {
-                        withFriends.add(friend);
-                    }
-                }
-            }
-        }
+        friends.forEach(friendList -> {
+            storeUserFriend(user, withFriends, friendList);
+        });
+
         return withFriends;
+    }
+
+    private static void storeUserFriend(String user, ArrayList<String> withFriends, List<String> friendList) {
+        if (friendList.contains(user)) {
+            friendList.forEach(friend -> {
+                addFriend(user, withFriends, friend);
+            });
+        }
+    }
+
+    private static void addFriend(String user, ArrayList<String> withFriends, String friend) {
+        if (!user.equals(friend)) {
+            withFriends.add(friend);
+        }
     }
 }
