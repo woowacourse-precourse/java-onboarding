25f1725b94cbe6afc931d2c24cbb20f02e1d8e59
mardi2020 <mardi@khu.ac.kr>
2022-10-31 15:18:17 +0900

    feat: Problem4 청개구리 사전 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,17 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
+        HashMap<Character, String> map = new HashMap<>();
+        for(int i = 'A'; i <= 'Z'; i++) {
+            map.put((char) i, Character.toString(155 - (char) i));
+        }
+        for(int i = 'a'; i <= 'z'; i++) {
+            map.put((char) i, Character.toString(219 - (char) i));
+        }
+
         String answer = "";
         return answer;
     }
