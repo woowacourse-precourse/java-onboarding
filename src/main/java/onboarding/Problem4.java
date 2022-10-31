73635e31b9b3be6c1063b1def8442b9b9d3f7570
소대훈 <theogns0681@naver.com>
2022-10-31 18:17:08 +0900

    [feat] (문제4) 청개구리 사전 등록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String[][] convertMessage = new String[2][26];
+        StringBuilder sb = new StringBuilder();
+        HashMap<String,String> frogAlphabet = new HashMap<>();
+        convertMessage[0] = new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M",
+                "a","b","c","d","e","f","g","h","i","j","k","l","m"};
+        convertMessage[1] = new String[]{"Z","Y","X","W","V","U","T","S","R","Q","P","O","N",
+                "z","y","x","w","v","u","t","s","r","q","p","o","n"};
+        return sb.toString();
     }
 }
