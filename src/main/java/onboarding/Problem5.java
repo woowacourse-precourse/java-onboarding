119545cbdf10070d745b17aef0783f18d4cb9147
AhnJoonSung <tjdtna01@naver.com>
2022-11-01 08:47:49 +0900

    Problem5 refactoring

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,17 +6,16 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
         int[] units = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
-        List<Integer> result = new ArrayList<>(units.length);
+        List<Integer> answer = new ArrayList<>(units.length);
         int count;
 
         for (int i = 0; i < units.length; i++) {
             count = money / units[i];
-            result.add(count);
+            answer.add(count);
             money %= units[i];
         }
 
-        return result;
+        return answer;
     }
 }
