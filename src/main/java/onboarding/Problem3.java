f840fa4d78b2023136e75b97d1fcc98b6cd83c3b
Toa <ld980105@snu.ac.kr>
2022-11-01 00:33:21 +0900

    Feat : add countContain369 method to get the number of 3,6,9 in target number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
-
+import java.util.LinkedList;
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return 0;
+    }
+    public static int countContains369(int number){
+        int count369 = 0;
+        while (number != 0){
+            int digit = number % 10;
+            if (digit == 3 || digit == 6 || digit == 9)
+                count369++;
+            number /= 10;
+        }
+        return count369;
     }
 }
