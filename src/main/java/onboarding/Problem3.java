1c0ed34504d33a0e5525e8525bd00f0fcc32c3a2
유지웅 <poujis0@gmail.com>
2022-10-31 18:42:23 +0900

    feature(Problem3 ClapCountService) 작성
    주어진 숫자까지 쳐야하는 박수수를 구하는 메소드 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import onboarding.pkgProblem3.service.ClapCountService;
+
+/**
+ * 369 게임을 진행하며 주어진 숫자까지 쳐야하는 박수 수를 구하는 문제
+ * @author 유지웅
+ */
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        ClapCountService clapCountService = new ClapCountService();
+        return clapCountService.clapCount(number);
     }
+
 }
