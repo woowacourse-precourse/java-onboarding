8fcc4fed5ad0a8252acd3347c0bd2066d758498a
HyebinShin <hyeviin.dev@gmail.com>
2022-10-31 19:37:44 +0900

    대소문자 확인 메소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,7 +7,23 @@ public class Problem4 {
         char[] lower = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         char[] upper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
+        //청개구리 표현
+
         String answer = "";
         return answer;
+    }
+
+    public static int isAlphabet(char ch) {
+        if(String.valueOf(ch).matches("(^[a-z]$)")) {
+            //소문자면 리턴 1
+            System.out.println("ch: "+ch+", (int)ch: "+(int)ch);
+            return 1;
+        } else if(String.valueOf(ch).matches("(^[A-Z]$)")) {
+            //대문자면 리턴 2
+            System.out.println("ch: "+ch+", (int)ch: "+(int)ch);
+            return 2;
+        }
+        //알파벳이 아니면 -1 리턴
+        return -1;
     }
 }
