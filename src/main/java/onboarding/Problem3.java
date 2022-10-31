c5f8ae6d5cac65d8c04187d0bcf4e70b364282d6
wormjoo <wormjoo@naver.com>
2022-10-31 23:36:01 +0900

    feat: problem3 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String num = "";
+        char n = 0;
+
+        for (int i = 1; i <= number; i++) {
+            num = Integer.toString(i);
+            for (int j = 0; j < num.length(); j++) {
+                n = num.charAt(j);
+                if (n == '3' || n == '6' || n == '9') {
+                    answer += 1;
+                }
+            }
+        }
+
         return answer;
     }
 }
