f8751a3e3724915c0b628959c906268bac9da4b1
xi_jjun <rlawowns97@naver.com>
2022-11-01 03:45:17 +0900

    feat(Problem6): 한 닉네임에서 중복없이 토큰 추출하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem6 {
+    private static final int START_INDEX_OF_TOKENIZATION = 0;
+    private static final int TOKEN_LENGTH = 2;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static Set<String> extractTokensFromNicknameWithoutDuplication(String nickname) {
+        return IntStream.range(START_INDEX_OF_TOKENIZATION, nickname.length() - 1)
+                .mapToObj(start -> nickname.substring(start, start + TOKEN_LENGTH))
+                .collect(Collectors.toSet());
     }
 }
