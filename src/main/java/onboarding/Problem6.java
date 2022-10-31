8bb2f8ff43177b0044bf88877d5818b102ae0b10
KimSunU <kgw6147@naver.com>
2022-10-31 14:53:55 +0900

    pb6_second

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,12 +4,12 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         for(int i=0; i<forms.size(); i++){
             String name = new String();
             name = forms.get(i).get(1);
             for(int j=0; j<name.length()-1; j++){
-                String tmp = name.substring(j) + name.substring(j+1);
+                String tmp = name.substring(j,j+2);
                 for(int k=i+1; k<forms.size(); k++){
                     String tmp2 = forms.get(k).get(1);
                     if(tmp2.contains(tmp)) answer.add(forms.get(k).get(0));
@@ -22,3 +22,4 @@ public class Problem6 {
         return ans;
     }
 }
+
