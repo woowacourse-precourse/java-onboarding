9bcc44e80247f67637852d559d4888bb7a1a85c9
민병욱 <woook27@naver.com>
2022-11-01 02:25:42 +0900

    feat: isPossible 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -35,8 +35,33 @@ class Problem1 {
         return ans;
     }
 
+    public static boolean isPossible(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.size() != 2 || crong.size() != 2) {
+            return false;
+        }
+
+        int leftPage[] = {pobi.get(0), crong.get(0)};
+        int rightPage[] = {pobi.get(1), crong.get(1)};
+
+        for (int i = 0; i < 2 ; i++) {
+            if(leftPage[i] % 2 != 1 || rightPage[i] % 2 != 0) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            if(leftPage[i] + 1 != rightPage[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!isPossible(pobi, crong)) return -1;
 
         int pobiScore = getScore(pobi);
         int crongScore = getScore(crong);
