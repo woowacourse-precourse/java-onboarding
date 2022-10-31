4ee9ffd8366d287793a4d9105aea2da6d9ae6060
송민진 <m_6595@naver.com>
2022-11-01 01:19:00 +0900

    #2 feat: split given String to charList

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        List<String> charList = List.of(cryptogram.split(""));
         return answer;
     }
 }
