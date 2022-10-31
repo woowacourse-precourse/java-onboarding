bfb8e14d232db69e6fbebd36906150f8c23640d3
Amenable-C <duswo0624@naver.com>
2022-11-01 00:45:47 +0900

    feat : [문제6] 중복된 닉네임이 있는 경우 이메일의 중복을 허용하지 않고 이메일을 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,18 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-
         HashMap<String, Long> map = new HashMap<>();
         for(List<String> form : forms){
             String id = form.get(1);
             for(int j = 0; j < id.length() - 1; j++){
                 String s = id.substring(j, j + 2);
                 map.put(s, map.getOrDefault(s, 0L) + 1L);
+            }
+        }
+
+        List<String> answer = new ArrayList<>();
+        for(List<String> form : forms){
+            String email = form.get(0);
+            String id = form.get(1);
+            for(int j = 0; j < id.length() - 1; j++){
+                String s = id.substring(j, j + 2);
+                if(map.get(s) >= 2 && !answer.contains(email)){
+                    answer.add(email);
+                    break;
+                }
             }
         }
 
