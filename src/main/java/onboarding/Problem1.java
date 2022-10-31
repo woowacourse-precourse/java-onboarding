22f32561a85f9db20bdcd187ef56c52c03dda308
sora5421 <21800804@handong.edu>
2022-10-31 23:49:39 +0900

    problem2

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,43 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        
         int answer = Integer.MAX_VALUE;
+        answer = -1;
+        //compute two numbers gap
+        int p_gap = pobi.get(1) - pobi.get(0);
+        int c_gap = crong.get(1) - crong.get(0);
+
+        //check Exceptions
+        //two numbers gap is not 1
+        //pobi and crong does not open start page and end page
+        //pobi and crong does not start (null)
+        if(p_gap != 1 || c_gap != 1){
+            return answer;
+        }
+        else if(pobi.get(0) == 1 || pobi.get(0) == 399){
+            return answer;
+        }
+        else if(crong.get(0) == 1 || crong.get(0) == 399){
+            return answer;
+        }
+        else if(pobi.get(0) == null || pobi.get(1) == null || crong.get(0) == null || crong.get(1) == null){
+            return answer;
+        }
+        //compute numbers
+        computeNumber(pobi,crong);
+
         return answer;
+    }
+
+    static void computeNumber(List<Integer> pobi, List<Integer> crong){
+        //make max, plus number, multiple number
+        int p_plus = 0;
+        int p_multi = 0;
+        int c_plus = 0;
+        int c_multi = 0;
+        int p_max = 0;
+        int c_max = 0;
+
+        
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,61 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        Stack<Character> checklist = new Stack<>();
+
+        String answer = "";
+        //check the line strength 1
+        if(cryptogram.length() == 1){
+            answer = cryptogram;
+            return answer;
+        }
+        else{
+            check_duplicate(checklist, cryptogram);
+        }
+
+        while(!checklist.isEmpty()){
+            answer = Character.toString(checklist.pop()) + answer;
+        }
         return answer;
+    }
+
+    static void check_duplicate(Stack<Character> checklist, String str){
+        int start = 0; //start point
+        int end = 1; //end point
+
+        checklist.push(str.charAt(start));
+        char spoint = checklist.peek();
+
+        //check the same character in string
+        while(end < str.length()){
+            if(spoint != str.charAt(end)){
+                start = end;
+                end++;
+                checklist.push(str.charAt(start));
+                spoint = checklist.peek();
+            }
+
+            else {
+                end++;
+                //check the more same character in string
+                while(end < str.length() && spoint == str.charAt(end)){
+                    end++;
+                }
+                checklist.pop();  //remove same character
+                //check stack empty and input the new character
+                if(end < str.length() && checklist.empty()){
+                    start = end;
+                    end++;
+                    checklist.push(str.charAt(start));
+                    spoint = checklist.peek();
+                }
+                else if(end < str.length()){
+                    spoint = checklist.peek();
+                }
+            }
+        }
     }
 }
