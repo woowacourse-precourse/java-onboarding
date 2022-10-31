51c938606f39f6a62f6370793b14c57c2606f93c
gyumin159 <gyumin159@DESKTOP-6CKHGBO>
2022-10-31 20:55:03 +0900

    temp를 안나누고 money를 나누어서 에러발생됐음

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) { // 50000원 부터 나누어서 가장 작은 단위까지 몫을 구하자
-        List<Integer> answer = new ArrayList<Integer>(9);
+    	List<Integer> answer = new ArrayList<Integer>(9);
         int temp = money;
         int[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         for (int i : arr) {
-        	answer.add((int) money / i);
+        	answer.add((int) temp / i);
         	temp = temp % i;
         }
         return answer;
