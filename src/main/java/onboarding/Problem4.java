bb63248f8f19406a2b3183478ad18e7dec615058
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-10-31 18:17:07 +0900

    feat(Problem4): 문자를 청개구리 사전대로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,11 @@ public class Problem4 {
     private static final String UPPERCASE = "uppercase";
     private static final String LOWERCASE = "lowerCase";
     private static final String OTHER_LETTERS = "otherLetters";
-    
+    private static final int UPPERCASE_A_ASCIICODE = 65;
+    private static final int UPPERCASE_Z_ASCIICODE = 90;
+    private static final int LOWERCASE_A_ASCIICODE = 97;
+    private static final int LOWERCASE_Z_ASCIICODE = 122;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
@@ -29,5 +33,27 @@ public class Problem4 {
         }
 
         return OTHER_LETTERS;
+    }
+
+    public static int[] reverseLetter(int[] asciiCodes) {
+        int[] reverseAsciiCodes = new int[asciiCodes.length];
+
+        for (int i = 0; i < asciiCodes.length; i++) {
+            String letterType = classifyASCIIType(asciiCodes[i]);
+
+            switch (letterType) {
+                case UPPERCASE:
+                    reverseAsciiCodes[i] = UPPERCASE_Z_ASCIICODE - (asciiCodes[i] - UPPERCASE_A_ASCIICODE);
+                    break;
+                case LOWERCASE:
+                    reverseAsciiCodes[i] = LOWERCASE_Z_ASCIICODE - (asciiCodes[i] - LOWERCASE_A_ASCIICODE);
+                    break;
+                case OTHER_LETTERS:
+                    reverseAsciiCodes[i] = asciiCodes[i];
+                    break;
+            }
+        }
+
+        return reverseAsciiCodes;
     }
 }
