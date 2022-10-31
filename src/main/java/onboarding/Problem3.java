a68492121dad6c3b82ccd1fcf8a9603cc10c0933
tkdtn4657 <tkdtn101226@gmail.com>
2022-10-31 23:25:21 +0900

    problem3 100의자리 1000의자리 기능 구현 및 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,8 +12,19 @@ class tsn_game{
 		this.tsn = n;
 	}
 	
-	int tsn_calc() {
-		int i, temp;
+	int tsn_one() {
+		int i;
+	
+		for(i = 1; i <= tsn; i++) {
+			if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+				clap++;
+			}
+		}
+		return clap;
+	}
+	
+	int tsn_ten() {
+		int i;
 		
 		for(i = 1; i <= tsn; i++) {
 			if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
@@ -24,6 +35,59 @@ class tsn_game{
 			}	
 		}
 		return clap;
+	}
+	int tsn_hund() {
+		int i;
+		
+		for(i = 1; i <= tsn; i++) {
+			if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+				clap++;
+			}
+			if(i / 10 % 10 == 3 || i / 10 % 10 == 6 || i / 10 % 10 == 9) {
+				clap++;
+			}	
+			if(i / 100 == 3 || i / 100 == 6 || i / 100 == 9) {
+				clap++;
+			}
+		}
+		return clap;
+	}
+	
+	int tsn_thous(){
+		int i;
+		
+		for(i = 1; i <= tsn; i++) {
+			if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+				clap++;
+			}
+			if(i / 10 % 10 == 3 || i / 10 % 10 == 6 || i / 10 % 10 == 9) {
+				clap++;
+			}	
+			if(i / 100 % 10== 3 || i / 100 % 10== 6 || i / 100 % 10== 9) {
+				clap++;
+			}
+			if(i / 1000 == 3 || i / 1000 == 6 || i / 1000 == 9) {
+				clap++;
+			}
+		}
+		return clap;
+	}
+	
+	int tsn_calc() {
+		if(tsn >= 1 && tsn < 10) {
+			return tsn_one();
+		}
+		else if(tsn >= 10 && tsn < 100) {
+			return tsn_ten();
+		}
+		else if(tsn >= 100 && tsn < 1000) {
+			return tsn_hund();
+		}
+		else if(tsn >= 1000 && tsn < 10000) {
+			return tsn_thous();
+		}
+		else
+			return 0;
 	}
 }
 
