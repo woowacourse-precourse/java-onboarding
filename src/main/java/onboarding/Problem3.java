d2d29a46c2b8834e985eff6edf21548e5678b1df
ouohoon <begreat@kakao.com>
2022-11-01 04:10:27 +0900

    리팩토링: 문제3 기능목록 주석 추가, solution 반복 범위 수정
    for문이 1~number 범위가 아닌 0~number 범위였음

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,10 +3,14 @@ package onboarding;
 import java.util.List;
 
 public class Problem3 {
+    /*
+    기능목록
+    1. 수의 각 자릿수를 검사해서 손뼉의 횟수를 세는 기능
+     */
     private static List<Integer> CLAP_NUMBERS = List.of(3, 6, 9);
     public static int solution(int number) {
         int answer = 0;
-        for (int i = 0; i <= number; i++) {
+        for (int i = 1; i <= number; i++) {
             answer += countClap(i);
         }
         return answer;
