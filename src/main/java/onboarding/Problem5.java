dc034a82fdf6b0bebc0158541f16dad747610060
daeun <gain3436@gmail.com>
2022-11-01 00:29:21 +0900

    [feat](5) 돈을 단위 화폐로 나눈 몫 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int divide(int money, int paper) {
+        return money/paper;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
