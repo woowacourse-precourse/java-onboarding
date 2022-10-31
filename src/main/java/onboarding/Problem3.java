cba704e1a132e35c1e97a2e1a1d47f7482a5f18d
권민정 <22alswjd@naver.com>
2022-11-01 01:54:57 +0900

    feat: Problem3 기능 '2. 임시 숫자 temp에 3이 몇 개 포함되는지 세기' 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,10 +6,16 @@ public class Problem3 {
 
         // 1. 1~number까지 반복문을 돌기
         for (int i = 1; i <= number; i++) {
-
+            // 2. 임시 숫자 temp에 3이 몇 개 포함되는지 세기
+            int tmp = i;
+            while(tmp > 0) {
+                int tmp2 = tmp % 10;
+                if(tmp2 != 0 && (tmp2 % 3) == 0) {
+                    answer++;
+                }
+                tmp /= 10;
+            }
         }
-
-        // 2. 임시 숫자 temp에 3이 몇 개 포함되는지 세기
 
         return answer;
     }
