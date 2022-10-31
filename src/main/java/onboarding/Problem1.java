e2ca8bcf714288ad4db71d22488c82dddcb5b224
shinjaewon99 <tlswodnjs99@naver.com>
2022-10-31 18:01:17 +0900

    [Problem1] Sum함수 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+    // 함수 Sum을 만들어 자릿수의 합을 구현해준다
+    public static int Sum(int n){
+        int a = 0;
+        while(n > 0){
+            a += n%10;
+            n = n/10;
+
+        }
+        return a;
     }
+
+
 }
