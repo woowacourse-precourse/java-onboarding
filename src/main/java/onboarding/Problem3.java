4c81e202c7aa181c5fcca6681a7e0feb5b2cc892
JuSeong1130 <jsmoon1130@naver.com>
2022-11-01 00:37:31 +0900

    feat: 예외 체크 메서드 checkException(숫자 체크 checkNunber)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        if(checkException(number)){
+            throw new IllegalArgumentException();
+        }
         return answer;
+    }
+    public static boolean checkException(int number){
+        if(checkNumber(number)){
+            return true;
+        }
+
+        return false;
+    }
+    public static boolean checkNumber(int number){
+        return number < 1 || number > 1000;
     }
 }
