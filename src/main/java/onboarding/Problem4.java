bcebf3dda05f1a755c8d9b6c67737738ea6a46b9
noparamin <noparamin@naver.com>
2022-10-31 17:22:18 +0900

    Problem4 리팩토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,7 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem4 {
-    static boolean isAlphabet(char w) {
+
+    public static Map<Character, Character> getCharMap() {
+        Map<Character, Character> charMap = new HashMap<>();
+
+        for(int i = 0; i < 26; i++) {
+            int asciiKey = i + 97;
+            int asciiVal = 122 - i;
+            charMap.put((char)asciiKey, (char)asciiVal);
+        }
+
+        return charMap;
+    }
+
+    public static boolean isAlphabet(char w) {
         if(w >= 65 && w <= 90) {
             return true;
         } else if(w >= 97 && w <= 122) {
@@ -14,30 +27,30 @@ public class Problem4 {
         }
     }
 
-    public static String solution(String word) {
-
-        String answer = "";
-
-        Map<Character, Character> charMap = new HashMap<>();
-
-        for(int i = 0; i < 26; i++) {
-            int asciiKey = i + 97;
-            int asciiVal = 122 - i;
-            charMap.put((char)asciiKey, (char)asciiVal);
-        }
+    public static String convertWord(String word, Map<Character, Character> charMap) {
+        String result = "";
 
         for(char c : word.toCharArray()) {
             if(isAlphabet(c)) {
                 if(Character.isUpperCase(c)) {
                     char lowerAlpha = Character.toLowerCase(c);
-                    answer += Character.toUpperCase(charMap.get(lowerAlpha));
+                    result += Character.toUpperCase(charMap.get(lowerAlpha));
                 } else {
-                    answer += charMap.get(c);
+                    result += charMap.get(c);
                 }
             } else {
-                answer += c;
+                result += c;
             }
         }
+
+        return result;
+    }
+
+    public static String solution(String word) {
+
+        Map<Character, Character> charMap = getCharMap();
+
+        String answer = convertWord(word, charMap);
 
         return answer;
     }
