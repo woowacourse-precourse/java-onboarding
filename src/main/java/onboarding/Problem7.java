4ba709fb6473db9bd14de47b928df098b9280956
YuHayeon <youha0930@gmail.com>
2022-10-31 13:55:18 +0900

    feat(Problem7): user의 입력 예외처리
    길이가 범위에 맞지 않거나
    소문자가 아닌 경우 예외처리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,8 +4,38 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> answer = Collections.emptyList();
+		return answer;
+	}
+
+	static class InputValidator {
+
+		public static void checkRightInput(String user, List<List<String>> friends, List<String> visitors) {
+			isRightUserInput(user);
+		}
+
+		public static boolean isRightUserInput(String user) {
+			checkRightUserRange(user);
+			checkUserLowerCase(user);
+			return true;
+		}
+
+		public static void checkRightUserRange(String user) {
+			if (user.length() < 1 || user.length() > 30) {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		public static void checkUserLowerCase(String user) {
+			char[] charArray = user.toCharArray();
+
+			for (int i = 0; i < charArray.length; i++) {
+				if (!Character.isLowerCase(charArray[i])) {
+					throw new IllegalArgumentException();
+				}
+			}
+		}
+	}
 }
