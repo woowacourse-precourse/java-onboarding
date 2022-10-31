8566d12d5b6399bac6be060f3e5fdf162903fd51
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 22:44:10 +0900

    P6 | 이메일 집합 생성(연속 글자를 포함한 닉네임을 가진)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -18,6 +16,21 @@ public class Problem6 {
         sortEmailList(emailList);
 
         return emailList;
+    }
+    private static Set<String> getDuplicatedEmailSet(List<List<String>> forms) {
+        Set<String> result = new HashSet<>();
+        Map<Integer, Set<String>> charSetEmailMap = new HashMap<>();
+        for(List<String> form : forms) {
+            putCharSet(form, charSetEmailMap);
+        }
+
+        charSetEmailMap.forEach((strKey, setValue) -> {
+            if(setValue.size() >= 2) {
+                result.addAll(setValue);
+            }
+        });
+
+        return result;
     }
 
 
