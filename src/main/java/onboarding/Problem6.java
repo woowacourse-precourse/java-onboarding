27d6cf27239fd2a409d2996f54e47389e07aab51
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-10-31 21:08:12 +0900

    Problem6 기능 목록 1 : 각 크루가 제출한 닉네임들에서 두글자 이상의 단어들을 모두 추출해주는 함수 getAllWordList 를 만든다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
         List<String> answer = List.of("answer");
+
         return answer;
+    }
+
+    public static List<String> getAllWordList(String string) {
+
+        List<String> wordList = new ArrayList<>();
+
+        for (int j = 0; j < string.length() - 1; j++) {
+
+            for (int k = j + 2; k < string.length() + 1; k++) {
+
+                wordList.add(string.substring(j, k));
+
+            }
+        }
+        return wordList;
     }
 }
