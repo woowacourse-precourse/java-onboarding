e0bd2219dedc509e64e47107ba4be29c54a89a98
LeeJinHo <hosboy93@naver.com>
2022-10-31 10:00:24 +0900

    문제3. print 제거

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,17 +6,14 @@ public class Problem3 {
         
         for(int i=1; i<number+1; i++) {
         	int checkNum = i;
-        	System.out.println("checkNum 값 받았을 때 : " + checkNum);
+        	
         	while(checkNum!=0) {
         		if(checkNum%10==3 || checkNum%10==6 || checkNum%10==9) {
         			answer++;
         		}
         		checkNum = checkNum/10;
         	}
-        	System.out.println("박수 횟수 : " + answer);
         }
-        
-        System.out.println(answer);
         return answer;
     }
 }
