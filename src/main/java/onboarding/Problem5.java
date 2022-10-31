17060cb66e577b8b60abbf40e2dce1219616ca36
meunrud <y3a0522@gmail.com>
2022-10-31 13:28:42 +0900

    리팩토링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,20 +8,23 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
 
-        answer = test(money);
+        answer = splitMoney(won(),money);
 
 
         return answer;
     }
 
-    public static ArrayList<Integer> test(int money){
-        ArrayList<Integer> arrNum = new ArrayList<>();
+    public static int[] won(){
+        return new int[]{50000,10000,5000,1000,500,100,50,10,1};
+    }
 
-        int[]coin = {50000,10000,5000,1000,500,100,50,10,1};
-
-        for(int i = 0; i<coin.length; i++){
-            arrNum.add(money/coin[i]);
-            money %= coin[i];
-        }return arrNum;
+    public static ArrayList<Integer> splitMoney(int [] won, int money){
+        ArrayList<Integer> splitMoney = new ArrayList<>();
+        for(int i = 0; i<won.length; i++){
+            splitMoney.add(money/won[i]);
+            money %= won[i];
+        }return splitMoney;
     }
 }
+
+
