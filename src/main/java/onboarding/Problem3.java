62c390dbf320e81fa0bafc4f3355f3913d5c2f13
kingseungwooya <ksw_0505@nate.com>
2022-10-31 13:09:44 +0900

    feat: solve problem3
    3번문제 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+/**
+ * Programs should be written for people to read, and only incidectally for machine to execute
+ */
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        long countClap = 0L;
+        for(int i=1; i<=number; i ++){
+            String numberToString = String.valueOf(i);
+            countClap += getCountOfChar(numberToString, '3');
+            countClap += getCountOfChar(numberToString, '6');
+            countClap += getCountOfChar(numberToString, '9');
+        }
+        int answer = (int) countClap;
         return answer;
+    }
+
+    public static long getCountOfChar(String str, char ch){
+        return str.chars().filter(c -> c == ch).count();
     }
 }
