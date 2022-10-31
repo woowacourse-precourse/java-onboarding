218412fe7480b3bcf0e0055eca4d007a859eea8e
hayeongKo <polarpheno@icloud.com>
2022-11-01 00:18:41 +0900

    Feat: getFriendCountKnownTogether

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
-        
+
         return answer;
     }
 
@@ -21,7 +21,18 @@ public class Problem7 {
         }
         return alreadyFriend;
     }
-//    public static List<String> getFriendCountKnownTogether() {}
+    public static List<List<String>> getFriendCountKnownTogether(String user, List<String> alreadyFriend, List<List<String>> friends) {
+        List<List<String>> recommendationFriend = new ArrayList<>();
+
+        for (int i = 0; i < alreadyFriend.size(); i++) {
+            for (int j = 0; j < friends.size(); j++) {
+                if (friends.get(j).get(0).equals(alreadyFriend.get(i)) && !friends.get(j).contains(user)) {
+                    recommendationFriend.add(List.of(friends.get(j).get(1), "10"));
+                }
+            }
+        }
+        return recommendationFriend;
+    }
 //    public static List<String> getCountofVisitor() {}
 //    public static List<String> getRecommendedFriend() {}
 //    public static void sortByScore() {}
