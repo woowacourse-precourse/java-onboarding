ce1a8c7c3fc6969182526a14ffa53dc32acf8443
0chil <0@chll.it>
2022-11-01 02:41:10 +0900

    feat(problem6): 이메일 길이 검증

--- src/main/java/onboarding/problem6/Email.java
+++ src/main/java/onboarding/problem6/Email.java
@@ -5,6 +5,7 @@ public class Email {
 	private final String email;
 
 	public Email(String email) {
+		validateLength(email);
 		validateFormat(email);
 		this.email = email;
 	}
@@ -12,6 +13,15 @@ public class Email {
 	private void validateFormat(String email) {
 		if (!email.matches(".+@.+\\..+")) {
 			throw new IllegalArgumentException("이메일의 형식이 잘못되었습니다");
+		}
+	}
+
+	private void validateLength(String email) {
+		if (email.length() < 11) {
+			throw new IllegalArgumentException("이메일은 11글자 이상입니다");
+		}
+		if (email.length() >= 20) {
+			throw new IllegalArgumentException("이메일은 20글자 미만입니다");
 		}
 	}
 }
--- src/test/java/onboarding/problem6/EmailTest.java
+++ src/test/java/onboarding/problem6/EmailTest.java
@@ -7,10 +7,20 @@ import org.junit.jupiter.api.Test;
 
 public class EmailTest {
 
+	public static final String EMAIL_LENGTH_10 = "12345@7.91";
+	public static final String EMAIL_LENGTH_20 = "12345@7.911234512345";
+
 	@Test
 	@DisplayName("이메일은 이메일 형식을 가진다")
 	void email_has_format_of_email() {
 		assertThatNoException().isThrownBy(() -> new Email("test@email.com"));
 		assertThatIllegalArgumentException().isThrownBy(() -> new Email("test@."));
+	}
+
+	@Test
+	@DisplayName("이메일의 길이를 제한한다")
+	void email_is_longer_or_equal_than_11_and_shorter_than_20() {
+		assertThatIllegalArgumentException().isThrownBy(() -> new Email(EMAIL_LENGTH_10));
+		assertThatIllegalArgumentException().isThrownBy(() -> new Email(EMAIL_LENGTH_20));
 	}
 }
