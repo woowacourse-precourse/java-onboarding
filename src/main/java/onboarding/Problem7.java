6a99001f36a60fad5ee152b406370b229bb37da5
BHC-Chicken <tij05078@gmail.com>
2022-10-31 22:11:01 +0900

    P7) 1. 친구 목록 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,53 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    public static HashMap<String, ArrayList<String>> createFriendsMap(List<List<String>> friends) {
+        HashMap<String, ArrayList<String>> friendsList = new HashMap<>();
+        for (List<String> friend : friends) {
+            for (int i = 0; i < 2; i++) {
+                if (!friendsList.containsKey(friend.get(i))) {
+                    ArrayList<String> list = new ArrayList<>();
+                    list.add(friend.get((i + 1) % 2));
+                    friendsList.put(friend.get(i), list);
+                } else {
+                    friendsList.get(friend.get(i)).add(friend.get((i + 1) % 2));
+                }
+            }
+        }
+        return friendsList;
+    }
+
+    public static HashMap<String, Integer> createNotDupFriendsMap(String user, HashMap<String, ArrayList<String>> friendsMap) {
+        HashMap<String, Integer> result = new HashMap<>();
+
+        for (String friend : friendsMap.get(user)) {
+            for (String friendOfFriend : friendsMap.get(friend)) {
+                if (user.equals(friendOfFriend) || friendsMap.get(user).contains(friendOfFriend)) {
+                    continue;
+                }
+                if (!result.containsKey(friendOfFriend)) {
+                    result.put(friendOfFriend, 1);
+                } else {
+                    result.put(friendOfFriend, result.get(friendOfFriend) + 1);
+                }
+            }
+        }
+        return result;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        HashMap<String, Integer> totalPoint = new HashMap<>();
+        // 1. 친구 목록 생성
+        HashMap<String, ArrayList<String>> friendsMap = createFriendsMap(friends);
+        HashMap<String, Integer> notDupFriendsMap = createNotDupFriendsMap(user, friendsMap);
+        // 2. 친구의 친구의 경우 10점 증가
+        // 3. 타임라인 방문의 경우 1점 증가
+        // 4. 점수로 정렬(점수 동일한 경우 이름순) 후 5명 출력
+
+        return null;
     }
 }
