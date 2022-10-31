a7adcb52f6b343705def365cdc1d3c37fffa8ce0
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-10-31 17:58:41 +0900

    refactor : (Problem4) 관심사 분리
    관심사에 따라 클래스 및 이넘으로 역할을 분리한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,26 +3,60 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
 
-        /*
-        * 문장의 문자만큼 반복하여 문자를 변환한다.
-        * */
-        char[] wordToCharArr = word.toCharArray();
-        for (int i = 0; i < wordToCharArr.length; i++) {
-            if(wordToCharArr[i] ==' ') continue;
-            wordToCharArr[i] = charReverse(wordToCharArr[i]);
-        }
+        WordConversion wordConversion = new WordConversion(word);
 
-        return String.valueOf(wordToCharArr);
+        return wordConversion.convertString();
     }
 
-    /*
-    * 대소문자를 구분하여, 반대 문자로 바꾼다.
-    * */
-    private static char charReverse(char chr){
-        if(chr>='A' && chr<='Z'){
-            return (char)('A'+('Z'-chr));
-        }else{
-            return (char)('a'+('z'-chr));
+    public enum Alphabet{
+        LOWER_A('a'),
+        LOWER_Z('z'),
+        UPPER_A('A'),
+        UPPER_Z('Z'),
+        BLACK(' ');
+
+        private char alphabet;
+
+        Alphabet(char alphabet) {
+            this.alphabet = alphabet;
+        }
+
+        public char getAlphabet() {
+            return alphabet;
+        }
+    }
+
+    public static class WordConversion {
+        private String word;
+
+        WordConversion(String word){
+            this.word = word;
+        }
+
+        public String convertString(){
+            char[] wordToCharArr = word.toCharArray();
+            for (int i = 0; i < wordToCharArr.length; i++) {
+                wordToCharArr[i] = convertChar(wordToCharArr[i]);
+            }
+            return String.valueOf(wordToCharArr);
+        }
+
+        private static char convertChar(char chr){
+            /* ' ' */
+            if(chr == Alphabet.BLACK.getAlphabet()){
+                return Alphabet.BLACK.getAlphabet();
+            }
+
+            /* 'A'~'Z' */
+            if(chr>=Alphabet.UPPER_A.getAlphabet() &&
+                    chr<=Alphabet.UPPER_Z.getAlphabet()){
+                return (char)(Alphabet.UPPER_A.getAlphabet()+
+                        (Alphabet.UPPER_Z.getAlphabet()-chr));
+            }
+
+            /* 'a'~'z' */
+            return (char)(Alphabet.LOWER_A.getAlphabet()+
+                    (Alphabet.LOWER_Z.getAlphabet()-chr));
         }
     }
 }
