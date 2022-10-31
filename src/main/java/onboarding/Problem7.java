fd5b91b0346067738e6f2df4dfd7164b527a220e
BaeDongHyeon <sasd730@gmail.com>
2022-10-31 14:00:39 +0900

    feat(Problem7.java): 사용자 친구 리스트 반환 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> myFriendsList = myFriends(user, friends);
+        return myFriendsList;
+    }
+
+    private static List<String> myFriends(String user, List<List<String>> friends) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            if (user.equals(friends.get(i).get(0))) {
+                list.add(friends.get(i).get(1));
+            }
+            if (user.equals(friends.get(i).get(1))) {
+                list.add(friends.get(i).get(0));
+            }
+        }
+        return list;
     }
 }
