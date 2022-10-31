f9c074acdf7eed6c93149d9a1a96690476a2db2c
minhongsun <alsghdtjs7@gmail.com>
2022-10-31 21:26:54 +0900

    problem 1 clear

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,22 +8,45 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobi_max = 0;
         List<Integer> pobi_page = new ArrayList<>();
-        pobi_page.add(pobi.get(0)/100 + (pobi.get(0)%100)/10 + pobi.get(0)%10);
-        pobi_page.add(pobi.get(0)/100 * (pobi.get(0)%100)/10 * pobi.get(0)%10);
-        pobi_page.add(pobi.get(1)/100 + (pobi.get(1)%100)/10 + pobi.get(1)%10);
-        pobi_page.add(pobi.get(1)/100 * (pobi.get(1)%100)/10 * pobi.get(1)%10);
-
         int crong_max = 0;
         List<Integer> crong_page = new ArrayList<>();
-        crong_page.add(crong.get(0)/100 + (crong.get(0)%100)/10 + crong.get(0)%10);
-        crong_page.add(crong.get(0)/100 * (crong.get(0)%100)/10 * crong.get(0)%10);
-        crong_page.add(crong.get(1)/100 + (crong.get(1)%100)/10 + crong.get(1)%10);
-        crong_page.add(crong.get(1)/100 * (crong.get(1)%100)/10 * crong.get(1)%10);
 
-        for(int i = 0; i<4; i++){
-            if (pobi_max < pobi_page.get(i)){
+        for(int i=0 ; i<2 ; i++){
+            //포비
+            if (pobi.get(i) / 100 == 0 && (pobi.get(i) % 100) / 10 == 0) {
+                pobi_page.add(pobi.get(i) % 10);
+            }
+            else if (pobi.get(0) / 100 == 0 && (pobi.get(0) % 100) / 10 != 0) {
+                pobi_page.add((pobi.get(i) % 100) / 10 + pobi.get(i) % 10);
+                pobi_page.add(((pobi.get(i) % 100) / 10) * (pobi.get(i) % 10));
+            }
+            else {
+                pobi_page.add(pobi.get(i)/100 + (pobi.get(i)%100)/10 + pobi.get(i)%10);
+                pobi_page.add((pobi.get(i)/100) * ((pobi.get(i)%100)/10) * (pobi.get(i)%10));
+            }
+
+
+            //크롱
+            if (crong.get(i) / 100 == 0 && (crong.get(i) % 100) / 10 == 0) {
+                crong_page.add(crong.get(i) % 10);
+            }
+            else if (crong.get(0) / 100 == 0 && (crong.get(0) % 100) / 10 != 0) {
+                crong_page.add((crong.get(i) % 100) / 10 + crong.get(i) % 10);
+                crong_page.add(((crong.get(i) % 100) / 10) * (crong.get(i) % 10));
+            }
+            else {
+                crong_page.add(crong.get(i)/100 + (crong.get(i)%100)/10 + crong.get(i)%10);
+                crong_page.add((crong.get(i)/100) * ((crong.get(i)%100)/10) * (crong.get(i)%10));
+            }
+        }
+
+        for(int i = 0; i<pobi_page.size(); i++) {
+            if (pobi_max < pobi_page.get(i)) {
                 pobi_max = pobi_page.get(i);
             }
+        }
+
+        for(int i = 0; i<crong_page.size(); i++){
             if (crong_max < crong_page.get(i)){
                 crong_max = crong_page.get(i);
             }
@@ -31,7 +54,7 @@ class Problem1 {
 
         int answer = Integer.MAX_VALUE;
 
-        if (pobi.get(0) + 1 == pobi.get(1) || crong.get(0) + 1 == crong.get(1)){
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)){
             answer = -1;
         }
         else if(pobi_max > crong_max){
