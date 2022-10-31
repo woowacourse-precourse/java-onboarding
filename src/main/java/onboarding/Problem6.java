9055ebe07ffe06563fc0406e1b2e87c683da6dd9
moonn6pence <shin421179@gmail.com>
2022-11-01 02:31:22 +0900

    feat #6 : hashMap을 이용해 크루들의 닉네임 중복 확인 기능
    - checkHashMap : 특정 닉네임의 부분 문자열들이 특정 부분문자열 해쉬맵에 있는지 확인
    - isCrewDuplicated : 특정 크루의 닉네임을 다른 모든 크루와 비교해서 있다면 겹치는 크루 인덱스 반환
    - checkAllCrewDuplicated : 모든 크루의 닉네임 중복 검사 후 중복되면 기록
    - addAllDuplicatedCrewEmail : 중복 확인된 크루의 이메일을 리스트에 추가 후 정렬된 리스트 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,23 +1,27 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
 
     private static ArrayList<HashMap<Character,ArrayList<Character>>> crewSubstring;
+    private static boolean[] crewDuplication = new boolean[10000];
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+        initializeCrewSubstring(forms);
+        checkAllCrewDuplicated(forms);
+        return addAllDuplicatedCrewEmail(forms);
     }
 
-    private static HashMap<Character, ArrayList<Character>> addHashMap(String nickname){
+    private static HashMap<Character, ArrayList<Character>> addHashMap(String crewName){
         HashMap<Character, ArrayList<Character>> substringMap = new HashMap<Character, ArrayList<Character>>();
 
-        for (int i = 0; i < nickname.length() - 1; i++) {
-            Character key = nickname.charAt(i);
-            Character value = nickname.charAt(i + 1);
+        for (int i = 0; i < crewName.length() - 1; i++) {
+            Character key = crewName.charAt(i);
+            Character value = crewName.charAt(i + 1);
 
             ArrayList<Character> list = new ArrayList<Character>();
 
@@ -38,5 +42,60 @@ public class Problem6 {
         for (int i = 0; i < forms.size(); i++) {
             crewSubstring.add(addHashMap(forms.get(i).get(1)));
         }
+    }
+
+    // 닉네임이 부분문자열 해쉬맵에 있는지 확인
+    private static boolean checkHashMap(String crewName,HashMap<Character,ArrayList<Character>> substringMap){
+        for (int i = 0; i < crewName.length() - 1; i++) {
+            Character key = crewName.charAt(i); // 첫번째 글자
+            Character value = crewName.charAt(i + 1); // 두번째 글자
+
+            if (substringMap.containsKey(key)) {
+                if(substringMap.get(key).contains(value))
+                 return true;
+            }
+        }
+
+        return false;
+    }
+
+    // 특정 크루의 닉네임을 다른 모든 크루의 부분문자열 정보와 확인
+    // 다른 크루와 중복된다면 그 크루의 인덱스 반환
+    // 중복 없다면 -1 반환
+    private static int isCrewDuplicated(String crewName, int crewIndex){
+        for (int i = 0; i < crewSubstring.size(); i++) {
+            if(i == crewIndex) continue;
+
+            if (checkHashMap(crewName, crewSubstring.get(i))) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    // 모든 크루의 닉네임의 중복 검사
+    private static void checkAllCrewDuplicated(List<List<String>> forms){
+        for (int i = 0; i < forms.size(); i++) {
+            int duplicatedCrew = isCrewDuplicated(forms.get(i).get(1), i);
+            if (duplicatedCrew > 0) {
+                crewDuplication[i] = true;
+                crewDuplication[duplicatedCrew] = true;
+            }
+        }
+    }
+
+    // 중복 확인된 크루의 이메일을 리스트에 추가 후 정렬된 리스트 반환
+    private static List<String> addAllDuplicatedCrewEmail(List<List<String>> forms) {
+        List<String> emails = new ArrayList<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            if (crewDuplication[i]) {
+                emails.add(forms.get(i).get(0));
+            }
+        }
+
+        Collections.sort(emails);
+        return emails;
     }
 }
