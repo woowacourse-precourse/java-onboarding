474255b38aa2732fa6675c94837b523824ff66ba
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-01 02:22:29 +0900

    Problem6_Init

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,16 @@
 package onboarding;
 
+/*
+
+# 과제 요구 사항
+- 글자가 연속적으로 포함되는 닉네임 작성자의 이메일을 Return 하는 Method 제작
+- 이메일 형식 : email.com
+- 닉네임은 한글만 가는ㅇ하고, 전체 길이는 1자 이상 20자 미만
+- 결과는 이메일을 오름차순 정렬하고 중복 제거
+
+ */
+
+
 import java.util.List;
 
 public class Problem6 {
