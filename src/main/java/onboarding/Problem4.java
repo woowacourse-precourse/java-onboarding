09c2ec738bf2e3a96989b37a0b1a895225b62cb3
CAEL0 <kjongmin26@gmail.com>
2022-10-31 15:43:49 +0900

    기능 추가: PROBLEM4 각 문자에 대해 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,13 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    private static char transform(char c) {
+        if ('a' <= c && c <= 'z')
+            return (char)('a' + 'z' - c);
+
+        if ('A' <= c && c <= 'Z')
+            return (char)('A' + 'Z' - c);
+
+        return c;
+    }
 }
