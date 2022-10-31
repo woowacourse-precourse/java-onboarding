5a3ffad352bb22bc8c90b41fac69171bb1723eea
Sehoon Kim <shggm2000@gmail.com>
2022-10-31 23:14:54 +0900

    암호문에서 중복되는 문자열을 개수를 구하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -17,7 +17,26 @@ public class Problem2 {
             list.add(arr[l]);
         }
 
-        String answer = "";
+        int i = 0;
+
+
+        for(i=0; i<list.size()-1; i++) {
+
+            int l = 0;
+            int j = i + 1;
+
+            while (list.get(i) == list.get(j)) {
+                if (j + 1 == list.size()) {
+                    l++;
+                    break;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+
+            String answer = "";
         return answer;
     }
 }
