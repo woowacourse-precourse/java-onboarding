db5435c44d343199d10220c74271f5092f1f1508
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-31 22:06:37 +0900

    feat: 3번 예외처리 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,9 +5,27 @@ public class Problem3 {
     static final String THREE = "3";
     static final String SIX = "6";
     static final String NINE = "9";
+    static final int INPUT_MINIMUM_VALUE = 1;
+    static final int INPUT_MAXIMUM_VALUE = 10000;
+    static final String INPUT_TOO_SMALL_EXCEPTION = "입력한 값이 1보다 작습니다. ";
+    static final String INPUT_TOO_BIG_EXCEPTION = "입력한 값이 10000보다 큽니다. ";
 
     public static int solution(int number) {
+        checkInput(number);
         return calculateResult(number);
+    }
+
+    private static void checkInput(int number){
+        if(number < INPUT_MINIMUM_VALUE) inputTooSmallException();
+        if(number > INPUT_MAXIMUM_VALUE) inputTooBigException();
+    }
+
+    private static void inputTooSmallException(){
+        throw new IllegalArgumentException(INPUT_TOO_SMALL_EXCEPTION);
+    }
+
+    private static void inputTooBigException(){
+        throw new IllegalArgumentException(INPUT_TOO_BIG_EXCEPTION);
     }
 
     private static int calculateResult(int number){
