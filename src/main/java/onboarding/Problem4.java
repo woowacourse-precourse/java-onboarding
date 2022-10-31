8f1c5d75ffa4e5963ce717981447abe13ef0177f
zeunala <zeunala@naver.com>
2022-11-01 03:01:15 +0900

    feat: 문제 4 알파벳 하나 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+    public static char changeOneCharacter(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            return (char)('A' + 'Z' - ch);
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            return (char)('a' + 'z' - ch);
+        }
+        return ch;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
