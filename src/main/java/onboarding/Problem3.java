97b17f395115dcb59749c6fff0e726cf892db7d5
이정우 <acg6138@naver.com>
2022-10-31 22:25:21 +0900

    feat(Problem3): 3,6,9 개수의 총합을 구하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,9 +5,20 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
     public static long countChar(String str, char num) {
         return str.chars()
                 .filter(c -> c == num)
                 .count();
+    }
+
+    public static int countTotal(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            count += countChar(String.valueOf(i), '3');
+            count += countChar(String.valueOf(i), '6');
+            count += countChar(String.valueOf(i), '9');
+        }
+        return count;
     }
 }
