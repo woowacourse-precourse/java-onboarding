afc724cdfdcef57cf99e16986e5b270330d3128f
canbeyour <wuzucy@gmail.com>
2022-10-31 21:18:51 +0900

    feat: solve Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+    public static char changeChar(char c, String alphabets) {
+        int charIndex = alphabets.indexOf(c);
+        int changeCharIndex = alphabets.length() - 1 - charIndex;
+
+        return alphabets.charAt(changeCharIndex);
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int index = 0; index < word.length(); index++) {
+            char originChar = word.charAt(index);
+            char newChar = originChar;
+
+            if (Character.isUpperCase(originChar))
+                newChar = changeChar(originChar, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            else if (Character.isLowerCase(originChar))
+                newChar = changeChar(originChar, "abcdefghijklmnopqrstuvwxyz");
+            answer.append(newChar);
+        }
+        return answer.toString();
     }
 }
