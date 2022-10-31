f9bf7146bdecab19e9eb7d87ca18da9d246fd557
Amenable-C <duswo0624@naver.com>
2022-11-01 00:40:00 +0900

    feat : [문제4] 청개구리 사전을 이용하여 word 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,12 +4,17 @@ import java.util.HashMap;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
 
         HashMap<Character, Character> map = new HashMap<>();
         for(int i = 0; i < 27; i++){
             map.put((char) ('a' + i), (char) ('z' - i));
             map.put((char) ('A' + i), (char) ('Z' - i));
+        }
+
+        String answer = "";
+        for(int i = 0; i < word.length(); i++){
+            Character c = word.charAt(i);
+            answer += map.getOrDefault(c, c);
         }
 
         return answer;
