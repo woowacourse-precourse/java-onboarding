a4fa51ef03b17e5b366f00dbb1a61b4ebdb318ac
swonny <wonny921@gmail.com>
2022-10-31 16:24:57 +0900

    feat: 알파벳 배열 생성, 역순 알파벳 배열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        StringBuffer sb = new StringBuffer(alphabet);
+        String reversedAlphabet = sb.reverse().toString();
         String answer = "";
         return answer;
+    }
+
+    public static void main(String[] args) {
+        solution("I love you");
     }
 }
