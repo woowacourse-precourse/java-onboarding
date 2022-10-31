b0e97fd61d719e85b3d68c82c3dd40e184972e20
yhlee9098 <yhlee9098@gmail.com>
2022-11-01 00:07:22 +0900

    Problem 4 요약사항 * 구현 기능 - 알파벳 여부 확인 기능 - Pair 확인 및 변환 기능(Using ASCII)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
 public class Problem4 {
+    //Whether it is alphabet or not using ASCII value
+    public static boolean checkAlphabet(char c) {
+        boolean b = (64 < c) && (c < 91);
+        b = ( (96 < c) && (c < 123) ) || b;
+
+        return b;
+    }
+
+    //
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); ++i) {
+            char currentAlphabet = word.charAt(i);
+            final boolean b = checkAlphabet( currentAlphabet );
+            if( b ) {
+                //Using the sum of the ASCII values of an alphabet pair
+                int accumulatedASCII = (currentAlphabet < 97 ) ? 155 : 219;
+                char convertedAlphabet = (char) (accumulatedASCII - currentAlphabet);
+                answer += convertedAlphabet;
+            }
+            else {
+                answer += currentAlphabet;
+            }
+        }
         return answer;
     }
 }
