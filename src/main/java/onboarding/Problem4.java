f7cef6192f18c7af89911642a394e40d0e005954
alswn <alswn9938@gmail.com>
2022-10-31 16:46:17 +0900

    [Q4] feat : add "해당 char값 변환" 메소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,17 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
 
-    public static char changeUpperCase(char c){
-        return (char)(c - 32);
+    public static char changeCharacter(char c) {
+        return (char) (155 - c);
     }
 
-    public static char changeLowerCase(char c){
-        return (char)(c + 32);
+    public static char changeUpperCase(char c) {
+        return (char) (c - 32);
     }
+
+    public static char changeLowerCase(char c) {
+        return (char) (c + 32);
+    }
+
 
 }
