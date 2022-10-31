3db7122de7b38e03a3c584ceb3b4aed324fc4f7a
brotherstone97 <brotherstone97@gmail.com>
2022-10-31 22:36:34 +0900

    Feat: 자릿수별 3, 6, 9포함 검사 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+    static int AccumulateCount(int number) {
+        int divisor = 10000;
+        int count = 0;
+        while (divisor > 0) {
+            if ((int) (number / divisor) == 3 || (int) (number / divisor) == 6 || (int) (number / divisor) == 9) {
+                count += 1;
+            }
+            number %= divisor;
+            divisor /= 10;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
