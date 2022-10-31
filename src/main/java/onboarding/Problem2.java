e59cf5a16e233f698475dc72214d9880cc4f84d7
Somi Kim <rlathal009@gmail.com>
2022-10-31 23:27:38 +0900

    feat : Problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class Problem2 {
+    public static final char FIRST_CHARACTER = 'A';
+    public static char previousChar;
+
+    public static boolean isContinuous(char next) {
+        return next == previousChar;
+    }
+
+    public static String removeDuplication(String cipher) {
+        Stack<Character> stack = new Stack<>();
+        previousChar = FIRST_CHARACTER;
+
+        for (char nextChar : cipher.toCharArray()) {
+            if (isContinuous(nextChar))
+                continue;
+
+            if (stack.isEmpty() || stack.peek() != nextChar) {
+                stack.add(nextChar);
+                previousChar = FIRST_CHARACTER;
+                continue;
+            }
+
+            previousChar = stack.pop();
+        }
+
+        return stack.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(""));
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return removeDuplication(cryptogram);
     }
 }
