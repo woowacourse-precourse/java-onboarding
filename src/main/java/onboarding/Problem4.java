e0857eee2d96b16b865ab370e5c57f9e25186cdc
jasonoh22 <jasonoh22@naver.com>
2022-10-31 23:39:12 +0900

    Problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String convertedWord = "";
+        for(int i = 0 ; i< word.length(); i++){
+            char slicedWord = word.charAt(i);
+            if(slicedWord >= 65 && slicedWord <= 90){
+                slicedWord = (char)(155 - (int)(slicedWord));
+            }
+            else if(slicedWord >= 97 && slicedWord <= 122){
+                slicedWord = (char)(219 - (int)(slicedWord));
+            }
+            convertedWord += slicedWord;
+        }
+        return convertedWord;
     }
+    public static void main(String args[]){
+        String word = "I love you";
+        System.out.println(solution((word)));
+    }
+
+
 }
