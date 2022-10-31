ba9991f061152b6cdb5776f652f0e3eef584dbc7
twknds <twknds@naver.com>
2022-10-31 22:12:20 +0900

    [7번 feature] : plusfriend 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,25 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
+    private static Map<String,Integer> plusFriend(Map<String,List<String>> friendList,String user){
+        Map<String,Integer> friendScore = new HashMap<>();
+        Map<String,List<String>> newFriendList = new HashMap<>(friendList); //여기서 삭제하면 heap영역의 friendList에도 영향이 가기 때문
+        List<String> userFriends=newFriendList.get(user);
+        newFriendList.remove(user);
+        Iterator<String> keys = newFriendList.keySet().iterator();
+        for (String friend : newFriendList.keySet()) { //깊이가 너무 깊어지기 때문에 scorelist 초기화 시켜놓기
+            friendScore.put(friend,0);
+        }
+        while (keys.hasNext()){
+            String key = keys.next();
+            for (String userFriend : userFriends) {
+                if(newFriendList.get(key).indexOf(userFriend)!=-1){ //user을 제외한 친구리스트에서 user와 동일한 친구가 존재한다면 +10
+                    friendScore.replace(key,friendScore.get(key)+10);
+                }
+            }
+        }
+        return friendScore;
+    }
     private static Map<String,List<String>> friendmake(List<List<String>> friends){
         Map<String,List<String>> friendList = new HashMap<>(); //TreeMap과 HashMap중 고민했음 하지만 마지막에만 정렬해주면 되기에 전체적인 시간을 고려해 HashMap
         for (int i = 0 ;i<friends.size();i++){
@@ -27,5 +46,6 @@ public class Problem7 {
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Map<String,List<String>> friendMap =friendmake(friends);
+        Map<String,Integer> friendScore = plusFriend(friendMap,user);
     }
 }
