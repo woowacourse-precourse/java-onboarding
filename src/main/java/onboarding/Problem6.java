66dc10d10bfbba3fa4c77d83256c9a80c4898ab9
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-31 23:37:41 +0900

    feat: Add Problem6 Set Email Map
    - 닉네임 2개씩 분할 함수 추가
    - 분할한 닉네임 Map에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,12 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        if(isException(forms)) throw new RuntimeException("잘못된 인자입니다.");
+        if (isException(forms)) throw new RuntimeException("잘못된 인자입니다.");
         List<String> answer = List.of("answer");
         return answer;
     }
@@ -16,4 +18,22 @@ public class Problem6 {
                         (form.get(1).length() < 1 || form.get(1).length() > 20) ||
                                 (form.get(0).length() < 11 || form.get(0).length() > 20));
     }
+
+    private static void setEmailMap(String stringToken, Map<String, List<String>> emailMap, String email) {
+        if (emailMap.get(stringToken) == null) {
+            emailMap.put(stringToken, new ArrayList<>() {{
+                add(email);
+            }});
+            return;
+        }
+        emailMap.get(stringToken).add(email);
+    }
+
+    private static void nicknameSplitByTwo(String nickname, Map<String, List<String>> emailMap, String email) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            setEmailMap(nickname.substring(i, i + 2), emailMap, email);
+        }
+    }
+
+
 }
