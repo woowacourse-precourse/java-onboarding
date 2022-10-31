df08cb08804f100400c9cce5de1cea27c788d4cd
Yh0sun <hy770819@naver.com>
2022-11-01 01:53:46 +0900

    func list

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,13 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiNum, crongNum;
+
+        //pobi의 큰 수 구하기
+
+        //crong 큰 수 구하기
+
+        //포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0
         return answer;
     }
 }
