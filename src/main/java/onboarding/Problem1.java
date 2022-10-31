3a66a3c33eb7e4aa5fe7ac1bfa7c58a54211a45e
lhyeonj120 <isk04387@gmail.com>
2022-11-01 02:34:35 +0900

    [problem1] feat: 페이지 번호의 각 숫자 합하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+	private static int allAdd(int n) {
+		int res = 0;
+		
+		while(n != 0) {
+			res += n % 10;
+			n /= 10;
+		}
+		return res;
+	}
+	
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobiAddLeftPage = allAdd(pobi.get(0));
+        int pobiAddRightPage = allAdd(pobi.get(1));
+        int crongAddLeftPage = allAdd(crong.get(0));
+        int crongAddRightPage = allAdd(crong.get(1));
+        
         return answer;
     }
 }
