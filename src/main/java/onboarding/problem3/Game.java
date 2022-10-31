189d866f8f13deada838f460220868d5f0b843e1
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:17:05 +0900

    [problem3] Game 클래스 정의

--- src/main/java/onboarding/problem3/Game.java
+++ src/main/java/onboarding/problem3/Game.java
@@ -2,5 +2,20 @@ package onboarding.problem3;
 
 public class Game {
     //1.생성자
+    public Game(){};
     //2.countSnap
+    public int countSnap(int number){
+        int sum = 0;
+        for (int i = 3; i <= number; i++) sum += countThree(i);
+        return sum;
+    }
+    private int countThree(int number){
+        int result = 0;
+        while (number > 0) {
+            int slotNumber = number % 10;
+            if (slotNumber > 0 && slotNumber % 3 == 0) result++;
+            number /= 10;
+        }
+        return result;
+    }
 }
