c512b62e7e57e6a8d7a40dcd0a4359d23e492d8c
BHC-Chicken <tij05078@gmail.com>
2022-10-31 21:54:16 +0900

    P5) 5만원 부터 1원까지 순서대로 나눈 후 사용한 횟수 배열에 저장

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
+    public static Integer[] calc(int money) {
+        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        Integer[] totalMoneyUnit = new Integer[9];
+        Arrays.fill(totalMoneyUnit, 0);
+
+        for (int i = 0; i < 9; i++) {
+            if (money == 0) {
+                break;
+            } else if (money >= unit[i]) {
+                int div = money / unit[i];
+                money = money % unit[i];
+                totalMoneyUnit[i] = div;
+            }
+        }
+        return totalMoneyUnit;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        // 5만원 부터 1원까지 순서대로 나눈 후 사용한 횟수 배열에 저장
+        return Arrays.asList(calc(money));
     }
 }
