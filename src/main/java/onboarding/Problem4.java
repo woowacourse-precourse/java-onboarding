56d1eb063a43b7b7008a32964b9176a74887c59f
TaeMin <xoals3094@naver.com>
2022-10-31 21:35:10 +0900

    Problem4: add toGreenFrogLanguage method

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,17 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    public static String toGreenFrogLanguage(String word){
+        String greenFrogWord = "";
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            char greenFrogChar = translateGreenFrogChar(c);
+
+            greenFrogWord += greenFrogChar;
+
+        }
+        return greenFrogWord;
+    }
 
     public static char translateGreenFrogChar(char c){
         char greenFrogChar = c;
