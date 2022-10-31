a49e1526029c88906ccecb3df1b0e6812d3f7aef
bunsung92 <bunsung92@naver.com>
2022-10-31 13:48:36 +0900

    feat: add find continuous two char

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answerCandidate = new ArrayList<>();
+
+        findContinuousTwoChar(forms, answerCandidate);
+
+        return new ArrayList<>(answerCandidate);
+    }
+
+    private static void findContinuousTwoChar(List<List<String>> forms, List<String> answerCandidate) {
+        for (int i = 0; i < forms.size(); ++i) {
+            String nickName = forms.get(i).get(1);
+
+            for (int j = 0; j < nickName.length() - 1; ++j) {
+                String twoConsecutiveLetters = nickName.substring(j, j + 2);
+
+                for (int k = i + 1; k < forms.size(); ++k) {
+                    String nickNameComparator = forms.get(k).get(1);
+
+                    if (nickNameComparator.contains(twoConsecutiveLetters)) {
+                        answerCandidate.add(forms.get(i).get(0));
+                        answerCandidate.add(forms.get(k).get(0));
+
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
