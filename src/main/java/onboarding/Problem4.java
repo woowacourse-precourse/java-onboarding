40e6362f1bae7bc77f91e692a37869bc663be7f7
김선동 <mok03127@hanyang.ac.kr>
2022-11-01 01:15:49 +0900

    feat: 문제 4 청개구리 사전대로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,9 +5,22 @@ public class Problem4 {
         /*
          * 대문자 혹은 소문자라면 청개구리 사전대로 변환해서 결과를 구한다.
          */
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        int upperPivot = 'A' + 'Z'; // 대문자인 경우 변환을 위한 기준 값
+        int lowerPivot = 'a' + 'z'; // 소문자인 경우 변환을 위한 기준 값
 
-        return answer;
+        for (int i = 0; i < word.length(); i++) {
+            Character ch = word.charAt(i);
+            if (isUpperAlpha(ch)) {
+                answer.append(convertToFrogDict(ch, upperPivot));
+            } else if (isLowerAlpha(ch)) {
+                answer.append(convertToFrogDict(ch, lowerPivot));
+            } else {
+                answer.append(ch);
+            }
+        }
+
+        return answer.toString();
     }
 
     public static boolean isUpperAlpha(Character ch) {
@@ -18,4 +31,7 @@ public class Problem4 {
         return ch >= 'a' && ch <= 'z';
     }
 
+    public static char convertToFrogDict(Character ch, int standard) {
+        return (char) (standard - ch);
+    }
 }
