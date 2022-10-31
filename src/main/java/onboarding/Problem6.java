72fb6d1cf093eee2b2de1baff80b046ecc4fca1a
KimTaeKang57 <zizoen123@gmail.com>
2022-10-31 15:38:42 +0900

    문제 6 : 문제를 풀기위해 사용할 인스턴스 선언

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,51 +7,22 @@ import java.util.stream.Collectors;
  * 닉네임 중 같은 글자가 연속적으로 포함 될 경우 해당 닉네임 사용을 제한
  * 같은 글자가 연속적으로 포함되는 닉네임을 신청한 크루들에게 알려주는 시스템
  * "같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록을 리턴"
- *
+ * <p>
  * --> 수정 --> 제한사항에서 result 의 이메일 문자열의 중복을 제거한다를 발견 후 코드 수정
  * 1. stream 을 사용하여 result 를 오름차순 정렬 한 후 중복을 제거해줌.
  **/
 public class Problem6 {
+    private static boolean[] isContainWord;
+    private static HashMap<String, Integer> map;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
-        boolean[] visited = new boolean[forms.size()];
 
-        for (List<String> formList : forms) {
-            String str = formList.get(1);
-            Set<String> set = new HashSet<>();
-            for (int j = 0; j < str.length() - 1; j++) {
-                String sub = str.substring(j, j + 2);
-                if (set.contains(sub)) {
-                    continue;
-                } else {
-                    set.add(sub);
-                    map.put(sub, map.getOrDefault(sub, 0) + 1);
-                }
-            }
-        }
+        // 중복되는 단어를 가지고있는지 확인하는 boolean 배열
+        isContainWord = new boolean[forms.size()];
+        // 만들어질 수 있는 단어가 사용 된 횟수를 저장하기 위한 hashmap
+        map = new HashMap<>();
 
-        int k = 0;
-        for (List<String> formList : forms) {
-            String str = formList.get(1);
-            for (String key : map.keySet()) {
-                if (map.get(key) >= 2 && str.contains(key)) {
-                    visited[k] = true;
-                    break;
-                }
-            }
-            k++;
-        }
-
-        for (int i = 0; i < visited.length; i++) {
-            if (visited[i]) {
-                answer.add(forms.get(i).get(0));
-            }
-        }
-
-        // stream 을 사용하여 result 를 오름차순 정렬 한 후 중복을 제거해줌.
-        List<String> collect = answer.stream().sorted().distinct().collect(Collectors.toList());
-
-        return collect;
+        return answer;
     }
 }
