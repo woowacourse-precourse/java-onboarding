6793d379fc49be545c392712c77c07650ad383f0
zcae1020 <zcae1020@naver.com>
2022-10-31 10:52:04 +0900

    feat(Problem6): method frame

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
+
+    private static Map<String, List<Integer>> map = new HashMap<>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static boolean detectError(List<List<String>> forms){
+
+    }
+
+    private static void convertList2TwoCharCount(String str, int idx){
+
+    }
+
+    private static Set<Integer> getAnswerIndex(){
+
     }
 }
