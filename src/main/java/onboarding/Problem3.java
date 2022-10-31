f503d70bc051135e57aa3d9440f52b29805ae51a
CAEL0 <kjongmin26@gmail.com>
2022-10-31 15:23:23 +0900

    기능 추가: PROBLEM3 특정 숫자에서 손뼉을 치는 횟수 계산

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    private static int countClaps(int n) {
+        int clap = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit > 0 && digit % 3 == 0)
+                clap++;
+
+            n /= 10;
+        }
+        return clap;
+    }
 }
