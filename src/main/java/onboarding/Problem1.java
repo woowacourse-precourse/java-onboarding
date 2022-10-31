fd973dbc05131c109149ff431a8dc5a39b2fc48f
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2022-11-01 04:07:53 +0900

    [Feat] 예외사항 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,35 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        boolean validValuePobi = isValid(pobi);
+        boolean validValueCrong = isValid(crong);
+
+        if(!(validValuePobi && validValueCrong)) {
+            return -1;
+        }
+
+        return 0;
     }
+
+    /*
+    예외사항 확인
+    배열 값의 크기는 1 차이가 나야 유효한 값으로 인정한다.
+    */
+    static private boolean isValid(List<Integer> testingArr) {
+
+        if (Math.abs(testingArr.get(0) - testingArr.get(1)) != 1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+
+/*
+    int answer = Integer.MAX_VALUE;
+    return answer;
+ */
+
 }
