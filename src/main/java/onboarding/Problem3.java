eb75fd2c5b605a39ccb7317d0ba483d4fe93bce1
Miseong Kim <miseongk@naver.com>
2022-10-31 19:51:12 +0900

    refactor: change game369 function to countClap to fit the game logic

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,22 +2,24 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        return game369(number);
+        int answer = 0;
+
+        // number까지 순서대로 박수 치는 횟수 구하기
+        for (int i = 1; i <= number; i++) {
+            answer += countClap(i);
+        }
+        return answer;
     }
 
-    // 369 게임 중 손뼉 치는 횟수 반환 함수
-    static int game369(int number) {
+    // 숫자에 3, 6, 9가 몇 번 포함되는지 반환
+    static int countClap(int number) {
         int count = 0;
-        int j;
 
-        for (int i = 1; i <= number; i++) {
-            j = i;
-            while (j > 0) {
-                if (j % 10 == 3 || j % 10 == 6 || j % 10 == 9) {
-                    count++;
-                }
-                j /= 10;
+        while (number > 0) {
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9) {
+                count++;
             }
+            number /= 10;
         }
 
         return count;
