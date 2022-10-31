a616b8d7e16a55b23f959769da0a54c4974a69fe
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:29:20 +0900

    5번: 돈 액수 순회하며 나눗셈 이용하여 solution 메서드 구현 및 5번 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,19 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
     private static final List<Integer> moneySizeList = List.of(
-            50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1
+            50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
     );
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
 
         for (int i = 0; i < moneySizeList.size(); i++) {
-            
+            int count = getMoneyCount(money, moneySizeList.get(i));
+            answer.add(count);
+            money -= moneySizeList.get(i)*count;
         }
         return answer;
     }
