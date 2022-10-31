4a7967ecd949d9f704b03bb7bba2e6a073172193
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-10-31 21:06:36 +0900

    feat: Add Problem5 Exception Checker
    - 문제 5번 예외 사항 검사 함수 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        if (isException(money)) throw new RuntimeException("잘못된 인자입니다.");
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Boolean isException(int number) {
+        return number < 1 || number > 1000000;
     }
 }
