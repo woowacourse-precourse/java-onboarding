922a352e58f579acb58ac9a3d38fe61be8ae216e
5onchangwoo <scw9410@gmail.com>
2022-10-31 09:56:35 +0900

    problem5/메인 로직 추가 (탐욕법 사용)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] moneyArray = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+        for (int criteria : moneyArray) {
+            int[] result = transMoney(money, criteria);
+            money = result[1];
+            answer.add(result[0]);
+        }
         return answer;
     }
     // 50,000, 10,000, 5000, 1000, 500, 100, 50, 10, 1
