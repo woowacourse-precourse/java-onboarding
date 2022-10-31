e166384ca539e44ee3ec9dde0a5042b7f5ec2a8e
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-10-31 19:17:55 +0900

    feat(Problem6): 연속적으로 동일한 글자가 포함되는 크루의 이메일을 분리하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,46 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static HashSet<String> compareDuplicateName(List<List<String>> crewInfo) {
+        HashSet<String> emails = new HashSet<>();
+        Map<String, String> nameAndEmailPair = new HashMap<>();
+        String name = "";
+        String duplicateName = "";
+        String email = "";
+
+        for (int i = 0; i < crewInfo.size(); i++) {
+            name = crewInfo.get(i).get(1);
+
+            if (name.length() < 2) {
+                break;
+            }
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                duplicateName = name.substring(j, j + 2);
+
+                if (nameAndEmailPair.containsKey(duplicateName)) {
+                    email = nameAndEmailPair.get(duplicateName);
+
+                    if (!email.equals(crewInfo.get(i).get(0))) {
+                        emails.add(email);
+                        emails.add(crewInfo.get(i).get(0));
+                    }
+                }
+
+                nameAndEmailPair.put(duplicateName, crewInfo.get(i).get(0));
+            }
+        }
+
+        return emails;
     }
 }
