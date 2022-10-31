da103986ad62ffc8e87615dc8ec5c8f80362cb62
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 20:57:14 +0900

    ArrayList import, 친구 추천리스트인 recomList 생성, answer 변수 ArrayList로 재정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,15 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> recomList =new ArrayList<>();
+
+
+
         return answer;
     }
 }
