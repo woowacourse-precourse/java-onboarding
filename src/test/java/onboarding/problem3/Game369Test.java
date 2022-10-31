6172a71785f3d60a1915947e03b0abcb168cc111
hodomaroo <enghemdwkd@gmail.com>
2022-10-31 20:54:07 +0900

    [Problem3/Test] getTotalClapTimeOfNumbersInRange 메서드의 테스트 케이스를 생성하고, 검증을 완료하였습니다.

--- src/test/java/onboarding/problem3/Game369Test.java
+++ src/test/java/onboarding/problem3/Game369Test.java
@@ -31,4 +31,15 @@ public class Game369Test {
         Assertions.assertThat(Game369.getClapTimeOfNumber(145255)).isEqualTo(0);
         Assertions.assertThat(Game369.getClapTimeOfNumber(9999)).isEqualTo(4);
     }
+
+    @Test
+    @DisplayName("get total clap counts of numbers in range")
+    void validateGetTotalClapTimeOfNumbersInRange(){
+        Assertions.assertThat(Game369.getTotalClapTimeOfNumbersInRange(3,3)).isEqualTo(1);
+        Assertions.assertThat(Game369.getTotalClapTimeOfNumbersInRange(1,1)).isEqualTo(0);
+        Assertions.assertThat(Game369.getTotalClapTimeOfNumbersInRange(1,10)).isEqualTo(3);
+        Assertions.assertThat(Game369.getTotalClapTimeOfNumbersInRange(1,20)).isEqualTo(6);
+        Assertions.assertThat(Game369.getTotalClapTimeOfNumbersInRange(1,30)).isEqualTo(10);
+        Assertions.assertThat(Game369.getTotalClapTimeOfNumbersInRange(1,40)).isEqualTo(22);
+    }
 }
