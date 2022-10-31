171de812c4b136ceab8c989ee9a3c4ad22fbe54c
kki4504 <rhkddlf9876@gmail.com>
2022-11-01 00:29:05 +0900

    feat : 유저 중복 체크 기능 구현
    - Map을 이용해 중복되지 않은 유저추가
    - 기존 유저와 새로운 유저의 중복 발생시 두 명의 email반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+    static class Duplicate {
+        static Map<String, List<String>> passedMembers = new HashMap<>();
+
+        public static List<String> twoLettersDuplicateCheck (List<String> form) {
+            List<String> duplicateMember = new ArrayList<>();
+            String nickName = form.get(1);
+            for (int i = 0; i < nickName.length() - 1; i++) {
+                String twoLetter = nickName.substring(i, i + 2);
+
+                if (passedMembers.containsKey(twoLetter)) {
+                    duplicateMember.add(passedMembers.get(twoLetter).get(0));
+                    duplicateMember.add(form.get(0));
+                } else {
+                    passedMembers.put(twoLetter, form);
+                }
+            }
+            return duplicateMember;
+        }
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = List.of("answer");;
         return answer;
     }
 }
