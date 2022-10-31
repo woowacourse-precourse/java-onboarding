0006f953218054b0192159feae9f56657ead1845
Lee-Jiseung <ljss98@naver.com>
2022-10-31 16:56:06 +0900

    feat: problem4 변환 맵 초기화

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    private static Map<Character, Character> frogDict;
+
     public static String solution(String word) {
+        initFrogDict();
         String answer = "";
         return answer;
+    }
+
+    private static void initFrogDict() {
+        frogDict = new HashMap<>();
+        for (char c = 'A'; c <= 'Z'; c++) {
+            frogDict.put(c, (char) ('Z' - c + 'A'));
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            frogDict.put(c, (char) ('z' - c + 'a'));
+        }
     }
 }
