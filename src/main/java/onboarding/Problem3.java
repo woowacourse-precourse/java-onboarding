68d2b30236604323c9f5f9814ecada0a69f5cd84
LeeJejune <leejejune0625@gmail.com>
2022-10-31 14:39:54 +0900

    feat: 최종 메인 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,12 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 0; i <= number; i++) {
+            answer += countClap(i);
+        }
         return answer;
     }
 
