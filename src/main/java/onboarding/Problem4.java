97ea4ce5e9989546142014e5bf5616bc539a1dd1
조병우 <whquddn55@gmail.com>
2022-10-31 21:42:37 +0900

    Add: Problem4 Solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < word.length(); ++i) {
+            answer.append(convertToReverseOrder(word.charAt(i)));
+        }
+        return answer.toString();
+    }
+
+    private static char convertToReverseOrder(char ch) {
+        if (!Character.isAlphabetic(ch)) {
+            return ch;
+        }
+        else if (Character.isLowerCase(ch)) {
+            return (char)('z' - ch + 'a');
+        } else {
+            return (char)('Z' - ch + 'A');
+        }
     }
 }
