9b3137162030269d47dd0c6c7e6dda3384eda997
minaver <psmjoshua@gmail.com>
2022-10-31 16:07:27 +0900

    docs(problem6): 기능 설명 주석 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -14,11 +14,11 @@ import java.util.stream.Stream;
  *
  * 1. 접근 방식
  * - 모든 크루의 닉네임을 탐색하면서 닉네임을 2글자로 쪼갠다. 이를 'keyword'라고 부르겠다. (제이엠요 -> 제이 , 이엠 , 엠요)
- * - Map< keyword , keyword를 가지고 있는 크루의 parameter List indexs > 형식의 Map을 생성한다.
+ * - Map< keyword , keyword를 가지고 있는 크루의 parameter List indexes > 형식의 Map을 생성한다.
  * - 모든 크루의 닉네임을 탐색하면서
  *  _ 해당 키워드가 Map KEY에 존재시 VALUE에 add 해준다.
  *  _ 존재하지 않을 시 새로운 KEY - VALUE를 생성한다.
- * - Map이 완성되면 VALUE의 길이가 1이 아닌 아이들을 Set에 추가하고 중복제거, 오름차순 정렬한다.
+ * - Map이 완성되면 VALUE의 길이가 1이 아닌 VALUE들(List indexes)을 Set에 추가하고(중복 제거를 위해 Set으로 생성) 오름차순 정렬한다.
  *
  * */
 public class Problem6 {
@@ -35,13 +35,14 @@ public class Problem6 {
 
         Set<Integer> overlapCrewIdxSet = problem6.sortOverlapCrew();
 
+        // Set의 결과를 가지고 forms에서 중복에 해당하는 지원자 이메일 목록을 List화 한다.
         return overlapCrewIdxSet.stream()
                 .map(o -> forms.get(o).get(0))
                 .sorted()
                 .collect(Collectors.toList());
     }
 
-    // Map에서 VALUE의 size가 1이 아닌 아이들만의 Set 생성
+    // Map에서 VALUE의 size가 1이 아닌 VALUE들(List indexes)의 Set 생성(중복 제거를 위해 Set으로 생성)
     public Set<Integer> sortOverlapCrew(){
 
         return nickKeywordMap.entrySet()
@@ -56,11 +57,11 @@ public class Problem6 {
     // 닉네임 두글자씩으로 쪼개 keyword를 만든후 Map에 집어넣음
     public void makeKeyword(String nickname, int index){
         char[] nicknameCharArray = nickname.toCharArray();
+
         for(int i = 0; i < (nicknameCharArray.length-1); i++){
             // keyword 생성후 Map에 추가
             addToMap(new String(Arrays.copyOfRange(nicknameCharArray,i,i+2)),index);
         }
-
     }
 
     // keyword가 Key에 존재하는지 확인후 있다면 VALUE에 add, 없다면 새로운 KEY - VALUE를 생성
