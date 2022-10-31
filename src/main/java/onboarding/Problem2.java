37425922913c125673c21df47db23c71750823c2
daeun <gain3436@gmail.com>
2022-11-01 00:31:48 +0900

    [feat](2) 중복체크

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,15 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static boolean checkNotDuplicate(String string) {
+        for (int i=1; i<string.length(); i++)  {
+            if (string.charAt(i-1) == string.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
