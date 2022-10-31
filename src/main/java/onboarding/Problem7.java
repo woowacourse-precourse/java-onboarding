11d4f56254085349b518e44c171b96b2afcea4df
KimTaeKang57 <zizoen123@gmail.com>
2022-10-31 22:28:04 +0900

    문제 7 : 메소드 기능 분리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * [기능 구현]
@@ -14,15 +15,6 @@ import java.util.*;
  * 2. 함께아는 친구의 목록이 겹치더라도, 사용자와 친구관계 일 경우 추천 점수 +10 이 적용되지 않도록 코드 수정
  **/
 public class Problem7 {
-
-    // 각자의 친구 목록을 자료구조 hashmap 을 사용해 구현한다.
-    // 각 사용자 별로 친구 목록을 저장하는 메서드
-    // 함께 아는 친구 점수 추가해주는 메서드
-    // visitor 기반으로 방문한 친구 점수 추가해주는 메서드
-    // 각 사용자 별로 추천 점수를 저장해주는 메서드
-    // map_points 를 정렬하는 메서드
-    // 점수가 0점인 사용자를 제외하고 정렬한 순서에 따라 정답에 넣어주는 메서드
-
     private static Map<String, ArrayList> map_friends;
     private static Map<String, Integer> map_points;
 
@@ -32,54 +24,34 @@ public class Problem7 {
         map_friends = new HashMap<>();
         map_points = new TreeMap<>();
 
-        method1(friends);
-        method2(user);
-        // 사용자 즉 , user 로 주어진 사용자의 친구가 없을 경우 null 예외사항 처리를 위한 코드 수정
-        method3(user, visitors);
-
-        List<String> keySet = new ArrayList<>(map_points.keySet());
-        // Value 값으로 오름차순 정렬
-        keySet.sort((o1, o2) -> map_points.get(o2).compareTo(map_points.get(o1)));
-        int idx = 0;
-        for (String str : keySet) {
-            if (idx == 5) {
-                break;
-            }
-            answer.add(str);
-            idx++;
-        }
+        // 양방향 친구목록 만드는 메소드
+        createFriendsList(friends);
+        // 함께아는 친구 점수 추가하는 메소드
+        plusPointFriendWith(user);
+        // 방문자 점수 추가하는 메소드
+        plusPointVisitor(user, visitors);
+        // answer 만드는 메소드
+        createAnswer(answer);
 
         return answer;
     }
 
-    private static void method3(String user, List<String> visitors) {
-        for (String visitor : visitors) {
-            if(user.equals(visitor))    continue; // 방문자가 user 일 경우는 skip
-            if (map_friends.get(user) != null) {
-                if (!map_friends.get(user).contains(visitor)) {
-                    map_points.put(visitor, map_points.getOrDefault(visitor, 0) + 1);
-                }
-            } else {
-                map_points.put(visitor, map_points.getOrDefault(visitor, 0) + 1);
-            }
+    // answer 만드는 메소드
+    private static void createAnswer(List<String> answer) {
+        // stream() 을 사용해 map 을 value 기준으로 역정렬 후 상위 5개만 저장
+        List<Map.Entry<String, Integer>> collect = map_points.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(5)
+                .collect(Collectors.toList());
+
+        // collect 안의 5개의 요소들을 answer 에 add
+        for (Map.Entry<String, Integer> entry : collect) {
+            answer.add(entry.getKey());
         }
     }
 
-    private static void method2(String user) {
-        for (String str : map_friends.keySet()) {
-            if (!str.equals(user) && map_friends.get(user) != null) {
-                ArrayList friendList = map_friends.get(str); // jun {donut, shakevan}
-                for (int i = 0; i < map_friends.get(user).size(); i++) { // donut shakevan
-                    // 사용자와 친구관계 일 경우 추천 점수 +10 이 적용되지 않도록 코드 수정
-                    if (friendList.contains(map_friends.get(user).get(i)) && !friendList.contains(user)) {
-                        map_points.put(str, map_points.getOrDefault(str, 0) + 10);
-                    }
-                }
-            }
-        }
-    }
-
-    private static void method1(List<List<String>> friends) {
+    // 양방향 친구목록을 만들어주는 메소드 (Arraylist 사용)
+    private static void createFriendsList(List<List<String>> friends) {
         for (List<String> list : friends) {
             ArrayList A_List = map_friends.getOrDefault(list.get(0), new ArrayList());
             ArrayList B_List = map_friends.getOrDefault(list.get(1), new ArrayList());
@@ -89,6 +61,63 @@ public class Problem7 {
 
             map_friends.put(list.get(0), A_List);
             map_friends.put(list.get(1), B_List);
+        }
+    }
+
+    // 사용자와 함께아는 친구의 점수를 hashmap 에 저장하는 메소드
+    private static void plusPointFriendWith(String user) {
+        for (String str : map_friends.keySet()) {
+            plusPointFriendWithToMap(user, str);
+        }
+    }
+
+    /**
+     * 함께아는 친구의 점수를 더해주는 메소드
+     * <p>
+     * 1. user 가 아니면서 user 의 친구가 있는 경우
+     * -> if (!str.equals(user) && map_friends.get(user) != null)
+     * 2. 사용자와 함께아는 친구이고 사용자와 이미 친구관계가 아닐 경우
+     * -> if (friendList.contains(map_friends.get(user).get(i)) && !friendList.contains(user))
+     */
+    private static void plusPointFriendWithToMap(String user, String str) {
+        if (!str.equals(user) && map_friends.get(user) != null) {
+            ArrayList friendList = map_friends.get(str); // jun {donut, shakevan}
+            for (int i = 0; i < map_friends.get(user).size(); i++) { // donut shakevan
+                if (friendList.contains(map_friends.get(user).get(i)) && !friendList.contains(user)) {
+                    map_points.put(str, map_points.getOrDefault(str, 0) + 10);
+                }
+            }
+        }
+    }
+
+    /**
+     * 방문한 사용자의 점수를 저장하는 메소드
+     * <p>
+     * 1. 방문자가 user 가 아닐 경우만 진행
+     * -> if (user.equals(visitor)) continue;
+     */
+    private static void plusPointVisitor(String user, List<String> visitors) {
+        for (String visitor : visitors) {
+            if (user.equals(visitor)) continue; // 방문자가 user 일 경우는 skip
+            plusPointVisitorToMap(user, visitor);
+        }
+    }
+
+    /**
+     * 친구관계인지 아닌지 판단 후 방문자의 점수를 더해주는 메소드
+     * <p>
+     * 1. user 의 친구가 없을 경우와 있는 경우
+     * -> if (map_friends.get(user) != null) .. else
+     * 2. user 의 친구 목록에 visitor 가 없는 경우
+     * -> if (!map_friends.get(user).contains(visitor))
+     */
+    private static void plusPointVisitorToMap(String user, String visitor) {
+        if (map_friends.get(user) != null) {
+            if (!map_friends.get(user).contains(visitor)) {
+                map_points.put(visitor, map_points.getOrDefault(visitor, 0) + 1);
+            }
+        } else {
+            map_points.put(visitor, map_points.getOrDefault(visitor, 0) + 1);
         }
     }
 }
