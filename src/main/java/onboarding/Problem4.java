bfdba50c772cd7a32e83a8205b6174deba4f0b14
hanuel <ezzeezz@naver.com>
2022-11-01 05:47:06 +0900

    feat: [problem4] Add solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String result = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            char tar = word.charAt(i);
+            if ('A' <= tar && tar <= 'Z') {
+                result += Character.toString('A' + 'Z' - tar);
+                continue;
+            }
+            if ('a' <= tar && tar <= 'z') {
+                result += Character.toString('a' + 'z' - tar);
+                continue;
+            }
+            result += tar;
+        }
+
+        return result;
     }
 }
