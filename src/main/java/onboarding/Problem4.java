c4b26d5e5df654c333c600442f7819925904e063
Jeonsoyeon <itso22@naver.com>
2022-10-31 13:35:31 +0900

    feat(Problem4): solution메서드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,10 +3,13 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i <= word.length() - 1; i++) {
+            answer += convertToTreeFrogWord(word.charAt(i));
+        }
         return answer;
     }
 
-    public static char convert(char ch) {
+    public static char convertToTreeFrogWord(char ch) {
         if (ch >= 'A' && ch <= 'Z') {
             char alphabet = (char) (ch - 'A');
             return (char) ('Z' - alphabet);
