83768f5eb09e729abdea40814af1b96811f548a7
baekeunsun <eunsun5460@gmail.com>
2022-10-31 15:37:47 +0900

    [문제4] feats: 문자구분

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,16 +1,40 @@
 package onboarding;
 
 public class Problem4 {
+    public static final int SMALL = 1;
+    public static final int BIG = 2;
+    public static final int ETC = 0;
     public static String solution(String word) {
         if(CheckInput(word)){
+            char[] charArray = word.toCharArray();
 
-        }
-        throw new IllegalArgumentException("잘못된 입력값");
+            for(char c : charArray){
+                if( Classification(c) == BIG){
+                    System.out.println("BIG");
+                }
+                if( Classification(c) == SMALL){
+                    System.out.println("SMALL");
+                }
+                if( Classification(c) == ETC){
+                    System.out.println("ETC");
+                }
+            }
+
+        }throw new IllegalArgumentException("잘못된 입력값");
     }
 
     /* 기능1 : input 확인 */
     private static boolean CheckInput(String word){
-        if(word.length() > 0 && word.length() < 1001) return true;
-        return false;
+        if(word.length() > 0 && word.length() < 10001){
+            System.out.println(word.length());
+            return true;
+        }return false;
+    }
+
+    /* 기능2 : 문자 구분 */
+    private static int Classification(char charvalue){
+        if(charvalue >= 65 && charvalue <=90) return BIG;
+        if(charvalue >= 97 && charvalue <= 122) return SMALL;
+        return ETC;
     }
 }
