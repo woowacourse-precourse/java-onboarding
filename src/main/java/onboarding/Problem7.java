0f1a6169ffadf9064d63f931c204f3bdc8e3aa1c
Sangwoo Shin <sangwoonoel@naver.com>
2022-10-31 22:29:10 +0900

    feat(Problem7): friend  리스트에서 유저의 친구들 찾기(기능목록1)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> closeFriends = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).contains(user)) {
+                closeFriends.add(friends.get(i).get(0));
+            }
+        }
         return answer;
     }
 }
