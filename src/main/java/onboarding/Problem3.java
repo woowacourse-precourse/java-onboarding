02d43f630f965aea19cde51a050431c6690eba93
hyunkoes <201702083@o.cnu.ac.kr>
2022-10-31 21:45:17 +0900

    [Problem 3] Overview

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int total = 0;
+        /*
+        1. 1부터 number까지 반복
+        2. 각 차례의 박수 횟수를 구함
+        3. 누적
+        */
+        return total;
     }
 }
