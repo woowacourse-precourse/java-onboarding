6f310edecad49cf4052abf56496dcdbed1c379a9
Young37 <20171470@g.dongseo.ac.kr>
2022-10-31 20:45:54 +0900

    연속하는 중복 문자 삭제 작업

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
     	StringBuilder cryptogramSB = new StringBuilder(cryptogram);
+		Character preChar = null;
+    	for(int i = 0; i < cryptogramSB.length() && i != -1 ; i++) {
+    		if(preChar == null) {
+    			preChar = cryptogramSB.charAt(i);
+    			continue;
+    		}else {
+    			if(cryptogramSB.charAt(i) == preChar) {
+    				cryptogramSB.deleteCharAt(i);
+    				cryptogramSB.deleteCharAt(i-1);
+    				preChar = null;
+    				i = i - 3;
+    			}else {
+    				preChar = cryptogramSB.charAt(i);
+    			}
+    		}  		
+    	}
+    	
     	return cryptogramSB.toString();
     }
 }
