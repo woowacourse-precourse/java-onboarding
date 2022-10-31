c106f484a12f89f9bf9a101fe72c8ead0dd7a950
vcho1958 <gusckd57@gmail.com>
2022-10-31 23:32:08 +0900

    [problem5] Problem5 로직 완성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import onboarding.problem5.MoneyChanger;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        MoneyChanger changer = new MoneyChanger();
+        return changer.getChanges(money);
     }
 }
