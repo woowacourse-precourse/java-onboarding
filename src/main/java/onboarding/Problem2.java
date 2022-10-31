d837d9b55f91b69bd6a20d63111252ca2fd0fc3f
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-10-31 18:03:51 +0900

    Feat(Problem2): Password Class Completion

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,7 +6,19 @@ public class Problem2 {
     static class Password {
         String cryptogram;
 
+        public Password(String cryptogram) throws Exception {
+            if (!isValidGram(cryptogram)) throw new Exception("Wrong String");
+            this.cryptogram = cryptogram;
+        }
 
+        private boolean isValidGram(String cryptogram) {
+            if(!isValidLength(cryptogram.length())) return false;
+            for (int i = 0; i <cryptogram.length() ; i++) {
+                if(!isSmallLetter(cryptogram.charAt(i))) return false;
+            }
+            return true;
+
+        }
 
         private boolean isValidLength(int cryptogramLength) {
             return cryptogramLength>0 &&cryptogramLength<=1000;
