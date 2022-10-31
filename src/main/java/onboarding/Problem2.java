929fd07c089f341ae8e0beed33421d50e66c13fe
doxxx <doxxx93@gmail.com>
2022-11-01 06:11:44 +0900

    refactor(Problem2): extract method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,39 +3,59 @@ package onboarding;
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-        boolean[] isDeleted = new boolean[cryptogram.length()];
-        boolean isChanged = true;
+        boolean[] isDeleted;
+        boolean isDuplicated = true;
         StringBuilder stringBuilder = new StringBuilder(cryptogram);
-        while (isChanged) {
-            isChanged = false;
+        while (isDuplicated) {
             if (isShort(cryptogram)) {
                 return cryptogram;
             }
-            char previous = cryptogram.charAt(0);
-            for (int i = 1; i < cryptogram.length(); i++) {
-                char current = cryptogram.charAt(i);
-                if (previous == current) {
-                    isDeleted[i - 1] = true;
-                    isDeleted[i] = true;
-                    isChanged = true;
-                }
-                previous = current;
-            }
-            if (isChanged) {
-                stringBuilder.delete(0, stringBuilder.length());
-                for (int i = 0; i < cryptogram.length(); i++) {
-                    if (!isDeleted[i]) {
-                        stringBuilder.append(cryptogram.charAt(i));
-                    }
-                }
-                cryptogram = stringBuilder.toString();
-                isDeleted = new boolean[cryptogram.length()];
-            }
+            isDeleted = new boolean[cryptogram.length()];
+            isDuplicated = isDuplicated(cryptogram, isDeleted);
+            cryptogram = getCryptogram(cryptogram, isDeleted, isDuplicated, stringBuilder);
         }
         return cryptogram;
     }
 
+    private static String getCryptogram(String cryptogram, boolean[] isDeleted, boolean isDuplicated,
+        StringBuilder stringBuilder) {
+        if (isDuplicated) {
+            cryptogram = deleteDuplicates(cryptogram, isDeleted, stringBuilder);
+        }
+        return cryptogram;
+    }
+
+    private static String deleteDuplicates(String cryptogram, boolean[] isDeleted, StringBuilder stringBuilder) {
+        stringBuilder.setLength(0);
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!isDeleted[i]) {
+                stringBuilder.append(cryptogram.charAt(i));
+            }
+        }
+        cryptogram = stringBuilder.toString();
+        return cryptogram;
+    }
+
     private static boolean isShort(String cryptogram) {
-        return cryptogram.length() == 1 || cryptogram.length() == 0;
+        return cryptogram.length() <= 1;
+    }
+
+    private static boolean isDuplicated(String cryptogram, boolean[] isDeleted) {
+        boolean isDuplicated = false;
+        char previous = cryptogram.charAt(0);
+        for (int i = 1; i < cryptogram.length(); i++) {
+            char current = cryptogram.charAt(i);
+            if (isSame(previous, current)) {
+                isDeleted[i - 1] = true;
+                isDeleted[i] = true;
+                isDuplicated = true;
+            }
+            previous = current;
+        }
+        return isDuplicated;
+    }
+
+    private static boolean isSame(char previous, char current) {
+        return previous == current;
     }
 }
