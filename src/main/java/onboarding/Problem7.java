aa6b04000e59d7f5f910fba49208621bd38b1710
WonSeok-dd <cluejws@gmail.com>
2022-10-31 16:38:55 +0900

    Problem7 | feat: 기능 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 구현 목록
+ * 1. user의 친구들 얻기
+ * 2. user친구의 친구들 얻기.
+ *  2.1 Key: user친구, Value: user친구의 친구들
+ * 3. user친구의 친구들 순회 하면서 friendScore 합산
+ * 4. visitors 순회 하면서 visitorScore 합산
+ * 5. friendScore, visitorScore 병합
+ * 6. Score 내림차순 정렬
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
