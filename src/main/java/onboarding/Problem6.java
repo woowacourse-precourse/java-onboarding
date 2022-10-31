eb94595b92e9e94639b6d23b5b46a9b61894c657
rosesua318 <rosesua@duksung.ac.kr>
2022-10-31 21:49:02 +0900

    [Problem6] 기능목록 작성하기 (김민서)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
+    /**
+     [기능목록]
+     1. 닉네임을 연속 문자 단위로 나누어서 해쉬맵에 개수 저장
+     2. 연속 문자가 2명 이상 겹치는 경우 연속 문자가 포함된 닉네임의 이메일 answer에 추가하기
+     */
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
 }
