95670b501d3453933001b210b0a8c6f7ee0f0661
mrbonk97 <mrbonk97@gmail.com>
2022-10-31 14:51:47 +0900

    7번 해결

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,49 +1,62 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
-
-    static class friend{
-        String name;
-        int point;
-        friend(String name, int point) {this.name = name; this.point = point; }
-    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        HashMap<String, List<String>> friend_graph = new HashMap<>();
-        ArrayList<friend> sorted_friend = new ArrayList<>();
+        HashMap<String, Set<String>> hm = new HashMap<>();
+        HashMap<String, Integer> point = new HashMap<>();
+
+
 
         for(int i = 0; i < friends.size(); i++)
         {
             String f1 = friends.get(i).get(0);
             String f2 = friends.get(i).get(1);
 
-            friend_graph.getOrDefault(f1, new ArrayList<>()).add(f2);
-            friend_graph.getOrDefault(f2, new ArrayList<>()).add(f1);
+            if(!hm.containsKey(f1)) hm.put(f1, new HashSet<>());
+            if(!hm.containsKey(f2)) hm.put(f2, new HashSet<>());
+
+            hm.get(f1).add(f2);
+            hm.get(f2).add(f1);
         }
 
-        for(var e: friend_graph.keySet())
+        for(var person1: hm.keySet())
         {
-            if(e.equals(user)) continue;
+            if(person1.equals(user)) continue;
+            if(hm.get(user).contains(person1)) continue;
+
             int count = 0;
-            for(var name: friend_graph.get(e))
-                if(friend_graph.get(user).contains(name)) count++;
+            for(var person2: hm.get(person1)) if(hm.get(user).contains(person2)) count++;
 
-            sorted_friend.add(new friend(e, count*10));
+            point.put(person1, count*10);
         }
 
-        for(var e: visitors)
+        for(var person: visitors)
         {
-            sorted_friend.replace(e, sorted_friend.get(e)+1);
+            if(hm.get(user).contains(person)) continue;
+            if(!point.containsKey(person)) { point.put(person, 1); continue; }
+            point.replace(person, point.get(person) + 1);
         }
 
+        List<String> answer = new ArrayList<>(point.keySet());
+        Collections.sort(answer, (o1, o2) -> {
+            if(point.get(o2) > point.get(o1)) return 1;
+            if(point.get(o2) == point.get(o1)) return o1.compareTo(o2);
+            return -1; });
 
+        List<String> answer2 = new ArrayList<>();
+        int count = 0;
+        for(int i = 0; i < answer.size(); i++)
+        {
+            if(count == 5) break;
+            if(point.get(answer.get(i)) == 0) break;
 
+            answer2.add(answer.get(i));
+            count++;
+        }
 
-        List<String> answer = Collections.emptyList();
-        return answer;
+        return answer2;
     }
 }
