1fe0082cfa581b89fc42b87c325d1588cd167b1a
chkim <cndghks0213@gmail.com>
2022-10-31 09:51:08 +0900

    feat(problem7): 입력이 주어졌을 때 친구 목록을 등록하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, Friends> userFriends = new HashMap<>();
+
+        registerFriends(friends, userFriends);
+        return Collections.emptyList();
+    }
+
+    private static void registerFriends(List<List<String>> friends, Map<String, Friends> userFriends) {
+        friends.forEach(friend -> register(friend, userFriends));
+    }
+
+    private static void register(List<String> friend, Map<String, Friends> userFriends) {
+        String first = friend.get(0);
+        String second = friend.get(1);
+
+        userFriends.computeIfAbsent(first, user -> new Friends());
+        userFriends.computeIfAbsent(second, user -> new Friends());
+        userFriends.get(first).add(second);
+        userFriends.get(second).add(first);
     }
 }
