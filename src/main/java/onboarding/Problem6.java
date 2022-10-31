9213b156e9cac69cb904f73ee4b557620733b2a3
doehy <dbehxks02@naver.com>
2022-10-31 22:59:45 +0900

    feat : 하나의 닉네임 안에 중복되는 것을 방지 후 Map 채우기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,5 +12,16 @@ public class Problem6 {
     static void fillToMap(List<List<String>> forms){
         for(List<String> data : forms) {fillToSet(data.get(1));}
     }
-
+    static void fillToSet(String name){
+        Set<String> set = new HashSet<>();
+        for(int i=0; i< name.length()-1;i++){
+            String check_name = "" ;
+            check_name = check_name + name.charAt(i)+name.charAt(i+1);
+            set.add(check_name);
+        }
+        for(String string : set){
+            if(maps.get(string) == null){maps.put(string,"1");}
+            else if(maps.get(string) == "1"){maps.put(string,"duplicate");}
+        }
+    }
 }
