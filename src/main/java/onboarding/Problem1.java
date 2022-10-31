66a64084fd513e3695ed2996f8dda3ee22aeeeb2
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 03:09:56 +0900

    문제1 : 상수를 도입하여 입출력 결과를 변화의 용이하게 만듦

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,18 +5,21 @@ import java.util.List;
 import static onboarding.problem1.Operator.*;
 
 class Problem1 {
+    static final int DRAW = 0;
+    static final int POBI = 1;
+    static final int CRONG = 2;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
         if (!(Validator.validate(pobi) && Validator.validate(crong))) { return -1;}
         if (compare(pobi) == compare(crong)) {
-            answer = 0;
+            answer = DRAW;
         }
         else if (compare(pobi) > compare(crong)) {
-            answer = 1;
+            answer = POBI;
         }
         else {
-            answer = 2;
+            answer = CRONG;
         }
 
         return answer;
