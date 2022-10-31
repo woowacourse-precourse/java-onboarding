b7acdcab5114c41330c39416c261e6d03224f104
kkunh97@naver.com <kkunh97@naver.com>
2022-10-31 18:55:17 +0900

    10월 31일

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        boolean isChange = true;
+        String newCytogram = cryptogram;
+        // 더이상 중복되는 문자가 없을 때 까지 반복
+        while (isChange) {
+            isChange = false;
+            char[] charArray = newCytogram.toCharArray();
+            char overlapChar = ' ';
+            for(int i = 0; i < charArray.length; i++) {
+                // 이전 문자와 중복되면 '_'로 변환
+                if(charArray[i] == overlapChar) {
+                    charArray[i] = '_';
+                    charArray[i - 1] = '_';
+                    isChange = true;
+                }
+                // 중복되지 않으면 overlapChar에 새로운 문자를 넣음
+                else overlapChar = charArray[i];
+            }
+            // '_' 로 변환했던 문자들 제거
+            newCytogram = String.valueOf(charArray).replace("_", "");
+            System.out.println(newCytogram);
+        }
+        return newCytogram;
     }
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,12 +1,12 @@
 package onboarding;
 
-import java.util.Scanner;
+
 
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("숫자를 입력하세요.");
+
+
         number = scan.nextInt();
         for (int i = 1; i < number; i++) {
             String str1 = String.valueOf(i);
