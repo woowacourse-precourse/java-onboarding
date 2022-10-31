ae633abc62f0f0e1356b9246eccbea69061d4f31
ggazitbyul <ngusy123@gmail.com>
2022-10-31 21:08:13 +0900

    Problem4) 1.규칙 해석 추가 2. 주석 추가 3. transWords 메서드 구현   3-1 대문자, 소문자, 기타 해당조건에 맞게 문자 변환후 문자열 반환. 4. changeUpperWord, changeLowerWord 구현   4-1 대문자 끼리 변환, 소문자 끼리 변환 메서드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+
 public class Problem4 {
 //    기능 요구 사항
 //  어느 연못에 엄마 말씀을 좀처럼 듣지 않는 청개구리가 살고 있었다. 청개구리는 엄마가 하는 말은 무엇이든 반대로 말하였다.
@@ -22,10 +23,52 @@ public class Problem4 {
 //    1. 매개변수 word를 정해진 규칙대로 변환하여 return 할것
 //      1-1 A -> Z, C -> Y 이런식
 //      1-2 대문자는 대문자로, 소문자는 소문자로
-//    2. 알파벳만 규칙에 존재함
+//      1-3 A ~ Z , Z ~ A 역순임.변환할 대 참조할 것
+//    2. 알파벳외에 변환 금지
 //    3. 1~1000 자 문자열
+
     public static String solution(String word) {
         String answer = "";
+        answer = transWords(word);
+        System.out.println("Success");
         return answer;
+    }
+
+    public static String transWords(String word) {
+//        ASCII A : 65 ~ Z : 90 // a : 97 ~ z : 122
+//        UpperCase, LowerCase 메서드 적용
+//        대문자) 번환갑과 관계는 대칭이므로 변환전 값 + 변환후 값  = 155
+//        소문자) 번환갑과 관계는 대칭이므로 변환전 값 + 변환후 값  = 219
+
+
+        String result = "";
+        for (int index = 0; index < word.length(); index++) {
+            int wordCondition =  word.charAt(index);
+
+            if (97 <= wordCondition && wordCondition <= 122) {  //소문자 조건
+                result += (char) changeLowerWord(word.charAt(index));
+
+            }
+            else if (65 <= wordCondition && wordCondition <= 90) {  // 대문자 조건
+                result += (char) changeUpperWord(word.charAt(index));
+            }
+            else {                                                 // 기타 조건
+                result += word.charAt(index);
+            }
+    }
+        return result;
+}
+
+    public static int changeUpperWord(int word) {
+//        A , Z 합계는 65 + 90 = 155
+
+        int result = 0;
+        return result = 155 - word;
+    }
+
+    public static int changeLowerWord(int word) {
+//        a , z 합계는 97 + 122 = 219
+        int result = 0;
+        return result = 219 - word;
     }
 }
