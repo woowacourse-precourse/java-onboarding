c7b401c8cf4ed2a9ca01e19fbe2615463e1f76dd
EstelleYU <iamestelleyu@gmail.com>
2022-11-01 01:57:28 +0900

    (#8) Problem1 modified

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,7 +7,6 @@ class Problem1 {
 
         int answer = Integer.MAX_VALUE;
 
-        // 왼쪽 홀수 시작하지 않을 때, 오른쪽 연속된 숫자 아닐 때, 첫페이지 또는 마지막페이지 또는 벗어난 숫자일 때 예외 -1
         if (pobi.get(0)%2!=1
                 || crong.get(0)%2!=1
                 || pobi.get(1)!=pobi.get(0)+1
@@ -23,15 +22,15 @@ class Problem1 {
         int pobi_score = get_max(pobi);
         int crong_score = get_max(crong);
 
-        if (pobi_score > crong_score){ answer = 1; }  // 포비가 이긴다면 1
-        else if (pobi_score < crong_score){ answer = 2; }  // 크롱이 이긴다면 2
-        else if (pobi_score == crong_score){ answer = 0; }  // 무승부 0
+        if (pobi_score > crong_score){ answer = 1; }
+        else if (pobi_score < crong_score){ answer = 2; }
+        else if (pobi_score == crong_score){ answer = 0; }
         else answer = -1;  // 예외 -1
 
         return answer;
     }
 
-    public static int get_max(List<Integer> list){  // int 쪽수를 string으로 바꾸고 (최대계산) 하기
+    public static int get_max(List<Integer> list){
         int left_page = list.get(0);
         int right_page = list.get(1);
         left_page = calculate_max(Integer.toString(left_page));
@@ -39,7 +38,7 @@ class Problem1 {
         return Math.max(left_page, right_page);
     }
 
-    public static int calculate_max(String str) { // (최대계산) 각 문자의 합과 곱을 계산하여 최댓값을 리턴
+    public static int calculate_max(String str) {
         int sum = 0;
         int multiply = 1;
         for (int i=0; i<str.length(); i++){
