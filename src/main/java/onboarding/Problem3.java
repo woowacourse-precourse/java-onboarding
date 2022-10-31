92dd3b39d31bf101abd47517af470c46d689bd3c
이지혁 <olzlgur@naver.com>
2022-10-31 22:10:37 +0900

    3, 6, 9 검증 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return 1;
+    }
+    private static int checkNumber(int number){
+        int count = 0;
+        while (number > 0) {
+            if ((number % 10) % 3 == 0 && (number % 10) / 3 != 0) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
