9f65bfc94f52e4d4aa7632cf0de71831864b8f45
juhwang <juhong4930@naver.com>
2022-10-31 23:52:13 +0900

    refactor(Problem3):removing some methods

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,9 +3,13 @@ package onboarding;
 import java.util.List;
 
 public class Problem3 {
-    private static Integer[] GAME_369_BOARD;
+    private static int[] GAME_369_BOARD;
 
     public static int solution(int number) {
+        return numberOnGame369Board(number);
+    }
+
+    public static int numberOnGame369Board(int number) {
         return game369Board()[number];
     }
 
@@ -15,15 +19,15 @@ public class Problem3 {
                 .filter(value -> List.of("3", "6", "9").contains(value)).count();
     }
 
-    public static Integer[] game369Board() {
-        GAME_369_BOARD = new Integer[10001];
+    public static void markClapNumber(int num) {
+        GAME_369_BOARD[num] = GAME_369_BOARD[num - 1] + validCheck369(num);
+    }
+
+    public static int[] game369Board() {
+        GAME_369_BOARD = new int[10001];
         for (int i = 3; i < 10001; i++) {
             markClapNumber(i);
         }
         return GAME_369_BOARD;
-    }
-
-    public static void markClapNumber(int num) {
-        GAME_369_BOARD[num] = GAME_369_BOARD[num - 1] + validCheck369(num);
     }
 }
