e33f29c0eaff0f3f246c2623681809f761da6f86
kingseungwooya <ksw_0505@nate.com>
2022-10-31 14:35:11 +0900

    comment: add comment
    코드설명을 위한 주석 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -18,6 +18,11 @@ public class Problem4 {
         return convertedWord.toString();
     }
 
+    /**
+     * 아스키 코드로 변환된 Char 를 변환해주기 위해 시작점 'A' or 'a' 로부터
+     * 바꿔줄 char 의 값만큼 'Z' or 'z' 에서 빼준다. 
+     */
+
     private static char convertAlphabet(int alphabet) {
         char convertedAlphabet = ' ';
         if(Character.isLowerCase(alphabet)){
