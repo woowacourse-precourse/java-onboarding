a77f39cedcb3d17b5f5e6b3103453f47b7ad529c
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-01 01:43:13 +0900

    백엔드 4번문제 분석
    두 단계를 거쳐 원하는 답을 얻을 수 있어보임

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+//        1. 알파벳이 순서대로 들어있는 배열 rightAlpha와 역순으로 들어있는 reverseAlpha를 생성
+//        2. 기본적으로 소문자를 취급하며 대문자의 경우에 한해 대문자로 취급
+//        +알파벳을 수치화시켜서 챌린지해볼만 함.
         return answer;
     }
 }
