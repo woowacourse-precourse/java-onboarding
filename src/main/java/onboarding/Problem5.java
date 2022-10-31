6c1cf4cd65747bdbe8d5db298d79706287129022
cmw9957 <cmw9957@gmail.com>
2022-10-31 18:14:57 +0900

    add: 돈 단위 값 저장하는 배열 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,6 +11,8 @@ import java.util.List;
  */
 
 public class Problem5 {
+    int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
