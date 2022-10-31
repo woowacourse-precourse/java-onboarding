ed4fb23f1bb64878d275ffbef0c37024c3a08b23
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-10-31 22:57:01 +0900

    refactor: Problem7 - 문자열 삭제 로직 수정
    - 문제 요구 사항을 잘못 이해하여 문자열 삭제 로직 수정
    - 문자열 삭제 로직을 바꾸며 전체적인 구조도 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,36 +1,75 @@
 package onboarding;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class Problem2 {
     public static String solution(String cryptogram) {
-        List<String> splitCryptogram = new ArrayList<>(List.of(cryptogram.split("")));
-        int codeIndex = 1;
-        while (!decipherFinish(splitCryptogram, codeIndex)) {
-            codeIndex = decipherCode(splitCryptogram, codeIndex);
+        return decipherCode(cryptogram);
+    }
+
+    private static String decipherCode(String code) {
+        while (!isNotDuplicate(code)) {
+            code = decodingCode(code);
         }
-        return String.join("", splitCryptogram);
+        return code;
     }
 
-    private static int decipherCode(List<String> code, int idx) {
-        if (isDuplicate(code, idx)) {
-            removeDuplicate(code, idx);
-            idx = 0;
+    private static String decodingCode(String code) {
+        int startDuplicate = findDuplicateStart(code);
+        code = removeDuplicate(startDuplicate, code);
+        return code;
+    }
+    private static String removeDuplicate(int startDuplicate, String code) {
+        if (startDuplicate != -1) {
+            int endDuplicate = findDuplicateEnd(code, startDuplicate);
+            String decode = createDecode(code, startDuplicate, endDuplicate);
+            code = isEndOneCycle(code, decode);
         }
-        return idx + 1;
+        return code;
     }
 
-    private static boolean decipherFinish(List<String> str, int idx) {
-        return str.size() == idx || str.size() == 0;
+    private static String isEndOneCycle(String code, String decode) {
+        if (decode.equals(code)) {
+            return code;
+        }
+        return decode;
     }
 
-    private static boolean isDuplicate(List<String> str, int idx) {
-        return str.get(idx-1).equals(str.get(idx));
+
+    private static boolean isNotDuplicate(String str) {
+        for (int i = 0; i < str.length()-1; i++) {
+            if (str.charAt(i) == str.charAt(i+1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    private static void removeDuplicate(List<String> strList, int idx) {
-        strList.remove(idx);
-        strList.remove(idx-1);
+    private static int findDuplicateStart(String code) {
+        int startIndex = -1;
+        for (int i = 0; i < code.length()-1; i++) {
+            if (code.charAt(i) == code.charAt(i+1)) {
+                return i;
+            }
+        }
+        return startIndex;
+    }
+
+    private static int findDuplicateEnd(String code, int startIndex) {
+        if (code.length() == 2) {
+            return startIndex+1;
+        }
+        for (int i = startIndex; i < code.length()-1; i++) {
+            if (code.charAt(startIndex) != code.charAt(i+1)) {
+                return i;
+            }
+        }
+        return startIndex;
+    }
+
+    private static String createDecode(String code, int startIndex, int endIndex) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(code);
+        sb.replace(startIndex, endIndex+1, "");
+        return sb.toString();
+
     }
 }
