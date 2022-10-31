ed7f80ae6efef9dd0ef8d6dc06cd27b7f26b0762
baksohyeon <bori12370@gmail.com>
2022-10-31 11:18:08 +0900

    feat: solve a problem 3 to get clap count when number contains 3, 6, 9

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,12 +1,29 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        Problem3 problem = new Problem3();
+        int[] consecutiveNumbers = IntStream.range(1, number + 1).toArray();
+        Set<Integer> appointedNumber = new HashSet<>(Arrays.asList(3, 6, 9));
+        return Arrays.stream(consecutiveNumbers).reduce(0, (acc, cur) -> {
+            List<Integer> eachDigits = problem.getDigits(cur);
+            boolean hasAppointedNumber = eachDigits.stream().anyMatch(appointedNumber::contains);
+
+           if (hasAppointedNumber) {
+               int countClap =   eachDigits.stream().reduce(0, (count, digit) -> {
+                   if (appointedNumber.contains(digit)) {
+                       return count + 1;
+                   } else {
+                       return count;
+                   }
+               });
+               return acc + countClap;
+           }
+           return acc;
+        });
     }
 
     public List<Integer> getDigits(int num) {
