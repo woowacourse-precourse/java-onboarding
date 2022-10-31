d83d07c0e06705cc1c467ae036307d684bdeee62
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-01 00:50:35 +0900

    feat:Add a function that return userId's friends list

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,4 +9,21 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    //입력된 userId와 친구인 사람 list 반환하는 함수
+    public static List<String> getFriends(String userId, List<List<String>> friends){
+        List<String> userFriend = new ArrayList<>();
+        for(List<String> friend : friends){
+            if(friend.get(0).equals(userId)) {
+                userFriend.add(friend.get(1));
+                continue;
+            }
+            if(friend.get(1).equals(userId)){
+                userFriend.add(friend.get(0));
+            }
+        }
+
+        return userFriend;
+    }
+
 }
