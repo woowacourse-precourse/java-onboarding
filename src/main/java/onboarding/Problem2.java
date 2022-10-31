b8785d17783169d86178dd5a8e873598a6ba9126
nahyeon cho <whskgus12@nate.com>
2022-10-31 21:29:07 +0900

    Problem 2 - 예외사항 처리 기능
    Problem2의 제한사항에 따라 예외사항 처리 기능 구현하였습니다. 문자열 길이가 주어진 범위를 벗어났을 경우,문자열에 대문자가 포함된 경우에 False를 반환합니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
     }
+    static boolean isValid(String cryptogram){
+        int min_length = 1;
+        int max_length = 1000;
+        int crypto_length = cryptogram.length();
+        // 예외 1. 문자열 길이가 주어진 범위를 벗어났을때
+        if(crypto_length < min_length || crypto_length > max_length)
+            return false;
+        // 예외 2. 대문자가 포함되어있을때
+        else return cryptogram.toLowerCase().equals(cryptogram);
+    }
 }
+
+
+
