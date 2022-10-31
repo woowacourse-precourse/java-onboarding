89f8c1cc4e55896a60f33c7353657dd5cf338c5e
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-01 04:23:55 +0900

    Problem3 버전정보 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,7 @@
  * - 문제 3
  *
  * 버전정보
- * - openjdk version "11.0.15"
+ * - jdk-11.0.17
  *
  * 날짜
  * - 2022-10-31
@@ -13,17 +13,20 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
-        // 1 ~ number 까지 자연수 확인
+
         for (int i = 1; i <= number; i++) {
             int num = i;
+
             // 각 숫자의 박수치는 횟수 카운트
             answer += getClapCount(num);
         }
 
         return answer;
     }
+
     // 해당 숫자에 3, 6, 9가 몇개 들어가 있는지 확인하는 메소드
     private static int getClapCount(int num) {
         int count = 0;
