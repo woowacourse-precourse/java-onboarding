13162bcaf701c21ddb2cc50062e399534f415bc3
lacram <chemin9898@gmail.com>
2022-11-01 00:13:17 +0900

    Problem 3 - 기능 목록 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/*
+* 기능 목록
+* 1. 특정 숫자의 369 개수 구하기
+* 2. 1부터 해당 숫자까지 369개수 구하기
+* */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
