52407336f9b503791fe1854fd02590c1fbd27f21
TaeMin <xoals3094@naver.com>
2022-10-31 21:29:01 +0900

    Problem2: add solution method

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,7 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decode(cryptogram);
     }
 
     public static String decode(String cryptogram) {
