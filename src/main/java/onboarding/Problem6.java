4d7a47925f7c93c2ea74156d0bb3198eb2945796
bumwoo <gld55602@gmail.com>
2022-10-31 20:52:50 +0900

    feat(Problem6): 중복 닉네임 이메일 집합 구현
    두 배열을 비교 후 중복되는 원소 체크 함수 구현
    HashMap을 순회하며 원소가 중복될 경우 집합에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,8 +2,10 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
@@ -13,7 +15,20 @@ public class Problem6 {
             nicknamePartition.put(form.get(0), devideByTwo(form.get(1)));
         }
 
-        
+        Set<String> emails = new HashSet<>();
+        Set<String> nickname = nicknamePartition.keySet();
+        for (String n1 : nickname) {
+            for (String n2 : nickname) {
+                if (n1.equals(n2)) {
+                    continue;
+                }
+                if (isDuplicate(nicknamePartition.get(n1), nicknamePartition.get(n2))) {
+                    emails.add(n1);
+                    emails.add(n2);
+                }
+            }
+        }
+
         return answer;
     }
 
@@ -23,6 +38,10 @@ public class Problem6 {
             result.add(s.substring(i, i + 2));
         }
         return result;
+    }
+
+    private static boolean isDuplicate(List<String> s1, List<String> s2) {
+        return s1.stream().anyMatch(s2::contains);
     }
 
 }
