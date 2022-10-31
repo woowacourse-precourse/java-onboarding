d6f09ae7ce92855d0cb67e5d5640e2607dee8a50
eugene1204 <s1061001@naver.com>
2022-10-31 16:13:26 +0900

    p3, 1. number -> String으로 변경후 3,6,9가 있으면 count로 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,8 +3,15 @@ package onboarding;
 public class Problem3 {
     // 1. number -> String으로 변경후 3,6,9가 있으면 count로 세기
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        String num = "";
+        int count =0;
+        for(int i = 1; i<number+1; i++) num +=i+"";//1234567891011121314
+            for(int j =0; j<num.length();j++){
+                if(num.charAt(j) == '3') count++;
+                if(num.charAt(j) == '6') count++;
+                if(num.charAt(j) == '9') count++;
+            }
+        return count;
 
     }
 }
