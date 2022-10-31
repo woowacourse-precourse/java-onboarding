f29014ad6b866517247bae3aee0fba8b81270b51
sieun <sieun960513@naver.com>
2022-10-31 14:42:04 +0900

    [ApplicationTest] : problem2 예제 추가 [수정]

--- src/test/java/onboarding/ApplicationTest.java
+++ src/test/java/onboarding/ApplicationTest.java
@@ -50,6 +50,13 @@ class ApplicationTest {
             String result = "";
             assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
         }
+
+        @Test
+        void case3() {
+            String cryptogram = "ccabbbacdeef";
+            String result = "cdf";
+            assertThat(Problem2.solution(cryptogram)).isEqualTo(result);
+        }
     }
 
     @Nested
