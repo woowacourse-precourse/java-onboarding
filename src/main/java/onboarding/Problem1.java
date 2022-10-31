ae1899f72ccffc5d80a7edc53bb61df8b62a6235
miffyKing <whqudghk28@gmail.com>
2022-11-01 02:04:21 +0900

    style: follow google java coding convention for problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,129 +5,137 @@ import java.util.List;
 
 class Problem1 {
 
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        if (pageException(pobi, crong) == -1) {
-            return -1;
-        }
-        User Pobi = new User(pobi);
-        User Crong = new User(crong);
-        answer = numCompare(Pobi.maxVal, Crong.maxVal);
-        return answer;
+  public static int solution(List<Integer> pobi, List<Integer> crong) {
+    int answer = Integer.MAX_VALUE;
+    if (pageException(pobi, crong) == -1) {
+      // System.out.println(-1);
+      return -1;
     }
+    User Pobi = new User(pobi);
+    User Crong = new User(crong);
+    answer = numCompare(Pobi.maxVal, Crong.maxVal);
+    return answer;
+  }
 
-    public static int pageException(List<Integer> usr1, List<Integer> usr2) {
-        if (pageInRange(usr1, usr2) == -1) {
-            return -1;
-        }
-        if (evenOddCheck(usr1, usr2) == -1) {
-            return -1;
-        }
-        if (pagesInPair(usr1, usr2) == -1) {
-            return -1;
-        }
-        return 1;
+  public static int pageException(List<Integer> usr1, List<Integer> usr2) {
+    //pageException()
+    //예외 처리 3. 1~400 범위 안인가?
+    if (usr1.size() != 2 || usr2.size() != 2) {
+      return -1;
     }
-
-    public static int numCompare(int usr1Max, int usr2Max) {
-        if (usr1Max == usr2Max) {
-            return 0;
-        }
-        if (usr1Max > usr2Max) {
-            return 1;
-        } else {
-            return 2;
-        }
+    if (pageInRange(usr1, usr2) == -1) {
+      return -1;
     }
-
-    public static int pageInRange(List<Integer> usr1, List<Integer> usr2) {
-
-        for (int i = 0; i < 2; i++) {
-            if (usr1.get(i) <= 0 || usr1.get(i) > 400) {
-                return -1;
-            }
-        }
-        for (int i = 0; i < 2; i++) {
-            if (usr2.get(i) <= 0 || usr2.get(i) > 400) {
-                return -1;
-            }
-        }
-        return 1;
+    //예외 처리 1. 왼쪽 홀수, 오른쪽 짝수
+    if (evenOddCheck(usr1, usr2) == -1) {
+      return -1;
     }
-
-    public static int evenOddCheck(List<Integer> usr1, List<Integer> usr2) {
-        if (usr1.get(0) % 2 != 1 || usr1.get(1) % 2 != 0) {
-            return -1;
-        }
-        if (usr2.get(0) % 2 != 1 || usr2.get(1) % 2 != 0) {
-            return -1;
-        }
-        return 1;
+    //예외 처리 2.  오른쪽 = 왼쪽 + 1.
+    if (pagesInPair(usr1, usr2) == -1) {
+      return -1;
     }
+    return 1;
+  }
 
-    public static int pagesInPair(List<Integer> usr1, List<Integer> usr2) {
-        if (usr1.get(1) != usr1.get(0) + 1 || usr2.get(1) != usr2.get(0) + 1) {
-            return -1;
-        }
-        return 1;
+  public static int numCompare(int usr1Max, int usr2Max) {
+    if (usr1Max == usr2Max) {
+      return 0;
     }
+    if (usr1Max > usr2Max) {
+      return 1;
+    } else {
+      return 2;
+    }
+  }
+
+  public static int pageInRange(List<Integer> usr1, List<Integer> usr2) {
+
+    for (int i = 0; i < 2; i++) {
+      if (usr1.get(i) <= 1 || usr1.get(i) >= 400) {
+        return -1;
+      }
+    }
+    for (int i = 0; i < 2; i++) {
+      if (usr2.get(i) <= 1 || usr2.get(i) >= 400) {
+        return -1;
+      }
+    }
+    return 1;
+  }
+
+  public static int evenOddCheck(List<Integer> usr1, List<Integer> usr2) {
+    if (usr1.get(0) % 2 != 1 || usr1.get(1) % 2 != 0) {
+      return -1;
+    }
+    if (usr2.get(0) % 2 != 1 || usr2.get(1) % 2 != 0) {
+      return -1;
+    }
+    return 1;
+  }
+
+  public static int pagesInPair(List<Integer> usr1, List<Integer> usr2) {
+    if (usr1.get(1) != usr1.get(0) + 1 || usr2.get(1) != usr2.get(0) + 1) {
+      return -1;
+    }
+    return 1;
+  }
 
 }
 
 class User {
 
-    int lBookPage;
-    int rBookPage;
-    int maxVal;
+  int lBookPage;
+  int rBookPage;
+  int maxVal;
 
-    public User(List<Integer> user) {
-        this.lBookPage = user.get(0);
-        this.rBookPage = user.get(1);
-        this.maxVal = getAnswer(lBookPage, rBookPage);
-    }
+  public User(List<Integer> user) {
+    this.lBookPage = user.get(0);
+    this.rBookPage = user.get(1);
+    this.maxVal = getAnswer(lBookPage, rBookPage);
+  }
 
-    int getAnswer(int lPage, int rPage) {
-        int answer = compare2(getMax(lPage), getMax(rPage));
-        return answer;
-    }
+  int getAnswer(int lPage, int rPage) {
+    int answer = compare2(getMax(lPage), getMax(rPage));
+    return answer;
+  }
 
-    int getSum(int page) {
-        List<Integer> pageDigits = getDigit(page);
-        int sum = 0;
-        for (int i = 0; i < pageDigits.size(); i++) {
-            sum += pageDigits.get(i);
-        }
-        return sum;
+  int getSum(int page) {
+    List<Integer> pageDigits = getDigit(page);
+    int sum = 0;
+    for (int i = 0; i < pageDigits.size(); i++) {
+      sum += pageDigits.get(i);
     }
+    return sum;
+  }
 
-    int getMulti(int page) {
-        List<Integer> pageDigits = getDigit(page);
-        int multi = 1;
-        for (int i = 0; i < pageDigits.size(); i++) {
-            multi *= pageDigits.get(i);
-        }
-        return multi;
+  int getMulti(int page) {
+    List<Integer> pageDigits = getDigit(page);
+    int multi = 1;
+    for (int i = 0; i < pageDigits.size(); i++) {
+      multi *= pageDigits.get(i);
     }
+    return multi;
+  }
 
-    List<Integer> getDigit(int page) {
-        List<Integer> digitList = new ArrayList<Integer>();
-        while (page != 0) {
-            digitList.add(page % 10);
-            page /= 10;
-        }
-        return digitList;
+  List<Integer> getDigit(int page) {
+    List<Integer> digitList = new ArrayList<Integer>();
+    while (page != 0) {
+      digitList.add(page % 10);
+      page /= 10;
     }
+    return digitList;
+  }
 
-    int getMax(int bookPage) {
-        int midMax = compare2(getSum(bookPage), getMulti(bookPage));
-        return midMax;
-    }
+  int getMax(int bookPage) {
+    int midMax = compare2(getSum(bookPage), getMulti(bookPage));
+    return midMax;
+  }
 
-    int compare2(int a, int b) {
-        if (a > b) {
-            return a;
-        } else {
-            return b;
-        }
+  int compare2(int a, int b) {
+    if (a > b) {
+      return a;
+    } else {
+      return b;
     }
+  }
 }
