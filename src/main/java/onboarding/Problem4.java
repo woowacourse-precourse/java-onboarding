11b3a19d4ea4f8bef79d53e7abe83b639f92bea5
sodlfmag <sodlfmag@khu.ac.kr>
2022-11-01 04:25:36 +0900

    Problem 4 Solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] list = word.toCharArray();
+        for(int i=0;i<list.length;i++){
+            if(!Character.isLetter(list[i])){
+                continue;
+            }
+            if(Character.isUpperCase(list[i])){
+                list[i] = (char)(155 - (int)list[i]);
+            }
+            else{
+                list[i] = (char)(219 - (int)list[i]);
+            }
+        }
+        answer = new String(list);
         return answer;
     }
 }
