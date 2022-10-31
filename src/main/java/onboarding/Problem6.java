684e42a5eb387e1d8444534860ea1fc340ebe5fe
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-10-31 23:42:19 +0900

    feat : 닉네임 2글자씩 쪼개기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,17 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            String nickname = forms.get(i).get(1);
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                String tmp = String.valueOf(nickname.charAt(j)) + nickname.charAt(j + 1);
+            }
+        }
         List<String> answer = List.of("answer");
         return answer;
     }
+
 }
