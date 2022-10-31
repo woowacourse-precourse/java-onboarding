a5aa34c0b3c7fc96701f5700699ec2b7438a9e46
ingpyo <juninpyo72@gmail.com>
2022-10-31 16:41:19 +0900

    [problem2]연속된 문자가 없는 암호문의 문자를 합쳐서 리턴

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,23 +8,11 @@ import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidati
 public class Problem2 {
     public static String solution(String cryptogram) {
 
-
-
-        int size = cut.size();
-        while (count>0) {
-            count = 0;
-            for (int j = 0; j < size - 1; j++) {
-                if (cut.get(j) == cut.get(j + 1)) {
-                    cut.remove(j);
-                    cut.remove(j);
-                    size -= 2;
-                    j--;
-                    count++;
-                }
-            }
+        for (int i = 0; i < cut.size(); i++) {
+            answer += cut.get(i);
         }
 
-
+        return answer;
     }
 
 
