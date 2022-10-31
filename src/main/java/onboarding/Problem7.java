3c5c2fcd561abc073dfd621c28cf6c5892c8931a
KimYongJun <rladydwns129@naver.com>
2022-11-01 01:34:39 +0900

    feat(Problem7): Make set to save user's friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
+    private static HashSet<String> userFriend;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        userFriend = new HashSet<>();
+        userFriend.add(user);
+
+        for (List l : friends) {
+            String s1 = (String) l.get(0);
+            String s2 = (String) l.get(1);
+
+            addFriendNetwork(user, s1, s2);
+
+        }
+
         return answer;
     }
 }
