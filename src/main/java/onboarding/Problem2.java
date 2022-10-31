57ea7112a32974ed130ac11d8c25ba0a0e443ab9
p <gustjr2332@naver.com>
2022-11-01 00:49:50 +0900

    feat(Problem2): countDuplicateCharacter() 연속으로 중복된 문자의 수 세기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
         return answer;
+    }
+
+
+    public static int countDuplicateCharacter(String cryptogram, int startindex){
+        int count = 0;
+        int lastindex = startindex +1;
+
+        if(lastindex == cryptogram.length()-1){
+            return 1;
+        }
+        while (cryptogram.charAt(startindex) == cryptogram.charAt(lastindex)){
+            count++;
+            lastindex++;
+        }
+        return count;
     }
 }
