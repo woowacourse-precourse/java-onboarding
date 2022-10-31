4ec2654c9a3313cb6d77cc9ff494b875ee030b51
BHC-Chicken <tij05078@gmail.com>
2022-10-31 21:48:13 +0900

    P4) 각 알파벳에 대응하는 문자로 변환 후 stringBuilder에 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+    public static StringBuilder transform(String word) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char alphabet = word.charAt(i);
+            if ('A' <= alphabet && alphabet <= 'Z') {
+                int temp = alphabet - 'A';
+                stringBuilder.append((char) ('Z' - temp));
+            } else if ('a' <= alphabet && alphabet <= 'z') {
+                int temp = alphabet - 'a';
+                stringBuilder.append((char) ('z' - temp));
+            } else {
+                stringBuilder.append(alphabet);
+                continue;
+            }
+        }
+        return stringBuilder;
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        // 1. 각 알파벳
+        return String.valueOf(transform(word));
     }
 }
