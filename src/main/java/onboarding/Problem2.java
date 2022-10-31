28c286f96fbeb100d3744902a520e9f730fbdc81
ttaehee <ttaehee1105@gmail.com>
2022-11-01 02:50:18 +0900

    문제2) feat : 문자 삭제 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,12 +4,13 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         StringBuffer answer = new StringBuffer(cryptogram);
 
-        while(true) {
+        while (true) {
             int sameIdx = getSameIdx(answer);
 
-            if(sameIdx == -1) {
+            if (sameIdx == -1) {
                 break;
             }
+            answer = removeSame(answer, sameIdx);
         }
 
         return answer.toString();
@@ -18,12 +19,17 @@ public class Problem2 {
     private static int getSameIdx(StringBuffer string) {
         char[] charArray = string.toString().toCharArray();
 
-        for(int i=0; i<charArray.length-1; i++) {
-            if(charArray[i] == charArray[i+1]) {
+        for (int i = 0; i < charArray.length - 1; i++) {
+            if (charArray[i] == charArray[i + 1]) {
                 return i;
             }
         }
         return -1;
+    }
+
+    private static StringBuffer removeSame(StringBuffer origin, int idx) {
+        origin.delete(idx, idx + 2);
+        return origin;
     }
 
 }
