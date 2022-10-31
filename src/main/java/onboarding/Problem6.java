490863069fccd84d47362fd308d6c7a00c8349b7
lacram <chemin9898@gmail.com>
2022-11-01 00:38:07 +0900

    Problem 6 - 기능 목록 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,11 @@ package onboarding;
 
 import java.util.List;
 
+/*
+* 기능 목록
+* 1. 닉네임 중복 여부 확인
+* 2. 모든 크루들에 대해 닉네임 중복 검사
+* */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
