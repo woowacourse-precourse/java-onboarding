2ed8999a435b5b0c8fb0c8b06af6ab1c8583e37b
박상천 <pswlove38@naver.com>
2022-10-31 17:31:32 +0900

    수를 받았을 때 박수를 몇번 치는지 계산하는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,19 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int calcNum(int num){
+        int result = 0;
+        int temp = 0;
+        while (num != 0) {
+            temp = num % 10;
+            if(temp == 3 || temp == 6 || temp == 9){
+                result += 1;
+            }
+            num /= 10;
+        }
+        return result;
+    }
+    // 수를 받았을 때 박수를 몇번 치는지 계산
+
 }
