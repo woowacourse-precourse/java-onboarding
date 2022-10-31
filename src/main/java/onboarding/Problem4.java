d79424c2227e21449f0ba0997a0d08ea46195fc3
lacram <chemin9898@gmail.com>
2022-11-01 00:21:27 +0900

    Problem 4 - 알파벳 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,24 @@ package onboarding;
 * 2. 전체 문장 변환
 * */
 public class Problem4 {
+
+    // 알파벳 변환
+    public static char reverseChar(char c) {
+        // 알파벳인 경우에만 변환
+        if (!Character.isAlphabetic(c)) {
+            return c;
+        }
+
+        int capitalTotal = 'A' + 'Z';
+        int total = 'a' + 'z';
+
+        if (Character.isUpperCase(c)) {
+            return (char) (capitalTotal - c);
+        } else {
+            return (char) (total - c);
+        }
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
