1ade852b1193fe3f3c95d900af0b6fd54204ad69
tlsqj <rnjstlsqja@inha.edu>
2022-10-31 13:15:55 +0900

    문제 4 오류 수정 및 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,19 +2,19 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         for(int wordIterator = 0; wordIterator < word.length(); wordIterator++) {
             if (word.charAt(wordIterator) >= 'A' && word.charAt(wordIterator) <= 'Z') {
-                answer += (char)(90 - (int)word.charAt(wordIterator) - 65);
+                answer.append((char) (90 - ((int) word.charAt(wordIterator) - 65)));
             }
 
             else if (word.charAt(wordIterator) >= 'a' && word.charAt(wordIterator) <= 'z') {
-                answer += (char)(122 - (int)word.charAt(wordIterator) - 97);
+                answer.append((char) (122 - ((int) word.charAt(wordIterator) - 97)));
             }
 
-            else answer += word.charAt(wordIterator);
+            else answer.append(word.charAt(wordIterator));
         }
 
-        return answer;
+        return answer.toString();
     }
 }
