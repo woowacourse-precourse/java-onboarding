c605ca4741deeea73ff83652f0d12b8319656fb4
K-jun98 <alap_u@naver.com>
2022-10-31 23:33:07 +0900

    refactor - 전역 변수 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,15 +4,14 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Problem2 {
-    private static Deque<Character> deque = new ArrayDeque<>();
 
     public static String solution(String cryptogram) {
-        deque.clear();
-        return removeDuplicate(cryptogram);
+        Deque<Character> deque = new ArrayDeque<>();
+        return removeDuplicate(cryptogram,deque);
 
     }
 
-    private static String removeDuplicate(String cryptogram) {
+    private static String removeDuplicate(String cryptogram, Deque<Character> deque) {
         boolean changeFlag = false;
         char before = '@';
         for (int i = 0; i < cryptogram.length(); i++) {
@@ -21,7 +20,7 @@ public class Problem2 {
                 before = cryptogram.charAt(i);
             } else if (before != cryptogram.charAt(i) && deque.peekLast() != cryptogram.charAt(i)) {
                 deque.offerLast(cryptogram.charAt(i));
-                before=cryptogram.charAt(i);
+                before = cryptogram.charAt(i);
                 changeFlag = false;
             } else if (before == cryptogram.charAt(i) && !changeFlag) {
                 changeFlag = true;
@@ -34,10 +33,10 @@ public class Problem2 {
         return getString(deque);
     }
 
-    private static String getString(Deque<Character> deque) {
+    private static String getString(Deque<Character> letters) {
         StringBuilder stringBuilder = new StringBuilder();
-        while (!Problem2.deque.isEmpty()) {
-            stringBuilder.append(Problem2.deque.pollFirst());
+        while (!letters.isEmpty()) {
+            stringBuilder.append(letters.pollFirst());
         }
         return String.valueOf(stringBuilder);
     }
