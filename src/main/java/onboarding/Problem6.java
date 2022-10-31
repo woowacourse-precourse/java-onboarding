429e857d93a1498599c5794fb2e0e4a19bd2d9b3
Isu Shin <isu010614@naver.com>
2022-10-31 16:36:58 +0900

    feat : Add function to put all nicknames in list
    and if nickname is only one letter, delete it

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Problem6 {
+    static List<String> nicknameList = new ArrayList<>();
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    public static void setNicknameList(List<List<String>> forms) {
+        for (Iterator<List<String>> iter = forms.iterator(); iter.hasNext(); ) {
+            nicknameList.add(iter.next().get(1));
+        }
+    }
+
+    public static void removeOneLetterNickname() {
+        for (String nickname : nicknameList) {
+            if (nickname.length() == 1) {
+                nicknameList.remove(nickname);
+            }
+        }
     }
 }
