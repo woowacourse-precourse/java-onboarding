2c9eff33e9adc75e855836fe369c5c35f90bf9cf
hwanee__ <fkfkthth@naver.com>
2022-11-01 04:25:10 +0900

    3,6,9체크 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++){
+            if(i<10) answer += check_num(i);
+            else{
+
+            }
+        }
+
         return answer;
     }
+
+    static int check_num(int num) {
+
+        if ((num == 3) || (num == 6) || (num == 9)) return 1;
+        else return 0;
+
+    }
+
+
 }
