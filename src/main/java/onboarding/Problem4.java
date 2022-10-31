b2f56286ad9eb1238c6cfb34280849f53de9f974
sora5421 <21800804@handong.edu>
2022-11-01 08:42:05 +0900

    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,33 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        //save alphabet array
+        char [] save = new char[26];
+        int Z = 90;
+        int i = 0;
+        //make alphabet array
+        for(i =0; i < 26; i++){
+            save[i] = (char)Z;
+            Z--;
+        }
+        StringBuilder strBuilder = new StringBuilder();
+        int check = 0;
+        char small = ' ';
+        //change the letters
+        for(i = 0; i < word.length(); i++){
+            check = (int)(word.charAt(i));
+            if(check >= 65 && check <= 90){ //upper letter
+                strBuilder.append(save[check-65]);
+            }
+            else if(check >= 97 && check <= 122){ //lower letter
+                small = Character.toLowerCase(save[check-97]);
+                strBuilder.append(small);
+            }
+            else{
+                strBuilder.append(word.charAt(i));
+            }
+        }
+        answer = strBuilder.toString();
         return answer;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+
+        counting(answer,money);
         return answer;
+    }
+    static void counting(List<Integer> count, int money){
+        int[] lists = {50000,10000,5000,1000,500,100,50,10,1};
+        //compute count money
+        for(int i = 0; i < 9; i++){
+            if(money > lists[i]){
+                count.add(money/lists[i]);
+                money = money % lists[i];
+            }
+            else{
+                count.add(0);
+            }
+        }
     }
 }
