39f8a94b36519529ff46e0414c453b71cee3f1fd
소대훈 <theogns0681@naver.com>
2022-10-31 18:15:12 +0900

    [feat] (문제3) 숫자 카운팅

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,16 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (int input = 1; input <= number ;input++){
-            //answer+= countingNumber(input);
+            answer+= countingNumber(input);
         }
         return answer;
+    }
+    public static int countingNumber(int input){
+        int count = 0;
+        while(input!=0){
+            if (input%10==3 || input%10==6 || input%10==9) count++;
+            input/=10;
+        }
+        return count;
     }
 }
