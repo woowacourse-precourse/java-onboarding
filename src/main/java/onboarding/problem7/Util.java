ce3b28b11512a669cbbe31330a9cb59c868501db
woosung1223 <dntjd991223@naver.com>
2022-10-31 16:20:56 +0900

    feature(Problem7): 같은 점수에 대해서 이름순으로 정렬하는 함수 생성

--- src/main/java/onboarding/problem7/Util.java
+++ src/main/java/onboarding/problem7/Util.java
@@ -1,9 +1,6 @@
 package onboarding.problem7;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.HashMap;
+import java.util.*;
 
 public class Util {
     public static String getKeyFromValue(HashMap<String, Integer> hashMap, int value) {
@@ -19,5 +16,23 @@ public class Util {
         HashSet<String> removedSet = new HashSet<>(target);
         ArrayList<String> removedList = new ArrayList<>(removedSet);
         return removedList;
+    }
+    public static List<Map.Entry<String, Integer>> sameScoreSort(List<Map.Entry<String, Integer>> entryList) {
+        List<Map.Entry<String, Integer>> newEntryList = new LinkedList<>();
+        int value = entryList.get(0).getValue();
+        List<Map.Entry<String, Integer>> temp = new LinkedList<>();
+        for (int i = 0; i < entryList.size(); i++) {
+            if (entryList.get(i).getValue() != value) {
+                temp.sort(Map.Entry.comparingByKey());
+                newEntryList.addAll(temp);
+                temp.clear();
+            }
+            temp.add(entryList.get(i));
+            value = entryList.get(i).getValue();
+        }
+        temp.sort(Map.Entry.comparingByKey());
+        Collections.reverse(temp);
+        newEntryList.addAll(temp);
+        return newEntryList;
     }
 }
