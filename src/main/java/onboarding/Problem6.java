0a742b2c48fd5626156e1cb7e709209e1a015572
이지영 <jioung93@naver.com>
2022-11-01 00:22:34 +0900

    [Problem 6] comment: 기능 사항 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    /*
+    1. 닉네임 두개를 선택
+    2. 두 닉네임의 중복 여부 검사
+    3. 중복일 시 리스트에 이메일 push
+     */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    // 두 닉네임의 중복 여부를 검사하는 함수 isDuplicate()
+
 }
