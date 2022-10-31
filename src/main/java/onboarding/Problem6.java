a16bcae52ebb8243d66cb8c85dcd38b0322c198a
Jeonwoojae <dnwo0719@icloud.com>
2022-11-01 03:53:37 +0900

    문제 6 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,39 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer;
+        HashSet<String> emails = new HashSet<>();
+        Map<String, String> hashMap = new HashMap<>();
+
+        String name;
+        String email;
+        String keyword;
+
+        for (int i=0;i< forms.size();i++) {
+            name = forms.get(i)
+                    .get(1);
+            for(int j=0; j<name.length()-1;j++)
+            {
+                keyword = name.substring(j,j+2);
+                if(hashMap.containsKey(keyword)){
+                    email = hashMap.get(keyword);
+                    if(!email.equals(forms.get(i).get(0))){
+                        emails.add(email);
+                        emails.add(forms.get(i).get(0));
+                    }
+                }
+                hashMap.put(keyword,forms.get(i).get(0));
+            }
+        }
+        answer = emails.stream()
+                        .sorted()
+                        .collect(Collectors.toList());
+
+        System.out.print(answer);
         return answer;
     }
 }
