afe883f69c9207f7b1c3abf406f1a0d57a79d85b
kingseungwooya <ksw_0505@nate.com>
2022-10-31 14:25:14 +0900

    feat: solve  problem4
    4번문제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+/**
+ * Programs should be written for people to read, and only incidentally for machine to execute
+ */
+
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+        answer = getConvertedWord(word);
         return answer;
     }
+
+    private static String getConvertedWord(String word) {
+        StringBuilder convertedWord = new StringBuilder();
+        word.chars().forEach(ch -> convertedWord.append(convertAlphabet(ch)));
+        return convertedWord.toString();
+    }
+
+    private static char convertAlphabet(int alphabet) {
+        char convertedAlphabet = ' ';
+        if(Character.isLowerCase(alphabet)){
+            convertedAlphabet = (char) ('z' - (alphabet - 'a'));
+        }
+        if(Character.isUpperCase(alphabet)){
+            convertedAlphabet = (char) ('Z' - (alphabet - 'A'));
+        }
+        return convertedAlphabet;
+    }
+
 }
