7a6e5ead8a4359a769aa2e895334c64510202a13
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-31 22:46:40 +0900

    P6 | 2글자 이상 중복 닉네임 찾기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -33,5 +33,23 @@ public class Problem6 {
         return result;
     }
 
+    private static void putCharSet(List<String> form, Map<Integer, Set<String>> charSetEmailMap){
+        String nickname = form.get(1);
+        String email = form.get(0);
+
+        for(int node = 0; node < nickname.length() -1; node++){
+            String charSet = nickname.substring(node, node+2);
+            int charSetHashCode = charSet.hashCode();
+
+            if(charSetEmailMap.containsKey(charSetHashCode)){
+                charSetEmailMap.get(charSetHashCode).add(email);
+            } else {
+                Set<String> emailSet = new HashSet<>();
+                emailSet.add(email);
+                charSetEmailMap.put(charSetHashCode,emailSet);
+            }
+        }
+    }
+
 
 }
