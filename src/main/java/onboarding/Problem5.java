b87dbf7dd6ff116d8e1f0c8371c29fada63f0ef5
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 01:14:33 +0900

    [Add] Prob5 Function list
    + Function list

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,12 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    // list for cost of currency
+    // func : calculate amount of each currency, return List
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
