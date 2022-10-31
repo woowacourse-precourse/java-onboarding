d29b8ed16e1ea8d5fe9fc83da9938bf8b1550d18
cocomozila <joseyun94@naver.com>
2022-10-31 17:02:54 +0900

    [문제 1] 페이지의 각 자릿수의 덧셈을 반환하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,6 +10,14 @@ class Problem1 {
         }
         return -1;
     }
+    // 페이지의 각 자릿수의 덧셈을 반환하는 기능
+    private static int getAddition(String digit) {
+        int total = 0;
+        for (int i = 0; i < digit.length(); i++) {
+            total += Integer.parseInt(String.valueOf(digit.charAt(i)));
+        }
+        return total;
+    }
     // 페이지의 각 자릿수의 곱셈을 반환하는 기능
     private static int getMultiplication(String digit) {
         int total = 1;
