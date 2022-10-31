6a0ef067cb287f28470d5c6175ad2e49a72c15d6
vcho1958 <gusckd57@gmail.com>
2022-10-31 22:57:56 +0900

    [problem1] Problem1 로직 완성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,18 @@
 package onboarding;
 
+import onboarding.problem1.PageGame;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        try{
+            PageGame game = new PageGame();
+            game.join(pobi,crong);
+            return game.getWinner();
+        }catch (Exception e){
+            return -1;
+        }
+
     }
 }
