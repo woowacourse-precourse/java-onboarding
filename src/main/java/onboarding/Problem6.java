ed86d4f299de77bb67098fa2190f34f2f0414c84
Kijung Park <kjpark4321@naver.com>
2022-11-01 00:49:27 +0900

    6번: 서브 스트링 본인 제외하고 다른 사람의 닉네임 패턴 매칭되나 판별하는 메서드 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,8 +9,20 @@ public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-
+        
         return answer;
+    }
+
+    private static boolean isMatchWithOther(String myEmail, String targetSubNickName, List<List<String>> forms) {
+        for (List<String> member : forms){
+            if (member.get(0).equals(myEmail)){
+                continue;
+            }
+            if (checkDuplicate(targetSubNickName, member.get(1))){
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean checkDuplicate(String targetSubNickName, String otherNickName){
