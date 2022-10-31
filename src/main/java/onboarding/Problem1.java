40b4e2de63f3817e3e523711378fb02261288b85
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-10-31 14:45:35 +0900

    ArrayList 임포트, pobi 점수 담을 List 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,16 +1,23 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int pobiArray[] = new int[pobi.size()];
-        int crongArray[] = new int[crong.size()];
+        List<Integer> pobiScore = new ArrayList<>();
 
-        for (int i = 0; i < pobi.size(); i++) {
-            pobiArray[i] = pobi.get(i);
-        }
+
+
+
+
+
+
+
+
+
+
 
 
 
