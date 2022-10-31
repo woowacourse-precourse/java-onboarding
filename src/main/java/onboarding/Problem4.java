be5a2e0731f367fd9dee19b88bca0a30cfec8038
jojaeng2 <ds4ouj@naver.com>
2022-10-31 12:47:55 +0900

    feat : 반대의 알파벳을 구하는 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    private static Map<Character, Character> reverseAlpha() {
+        Map<Character, Character> alphabets = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            alphabets.put((char) ('A' + i), (char) ('Z' - i));
+            alphabets.put((char) ('a' + i), (char) ('z' - i));
+        }
+        return alphabets;
     }
 }
