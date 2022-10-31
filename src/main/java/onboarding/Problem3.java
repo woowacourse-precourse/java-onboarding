9c7a8dc00a04745f19601ec4f8b9d0cba9b136ba
kingseungwooya <ksw_0505@nate.com>
2022-10-31 13:18:26 +0900

    comment: add comment
    주석을 이용한 설명과 code convention에 맞게 공백 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,8 +6,8 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        long countClap = 0L;
-        for(int i=1; i<=number; i ++){
+        long countClap = 0L; // answer 이 될 박수 친 총 횟수
+        for(int i = 1; i <= number; i++){
             String numberToString = String.valueOf(i);
             countClap += getCountOfChar(numberToString, '3');
             countClap += getCountOfChar(numberToString, '6');
@@ -16,6 +16,10 @@ public class Problem3 {
         int answer = (int) countClap;
         return answer;
     }
+
+    /**
+     * stream을 이용해 해당 문자열에 특정 문자의 개수를 센다.
+     */
 
     public static long getCountOfChar(String str, char ch){
         return str.chars().filter(c -> c == ch).count();
