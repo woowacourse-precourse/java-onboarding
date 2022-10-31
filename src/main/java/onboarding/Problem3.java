d5bbed8539923cbb9e9df74825225866899b4177
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 00:14:53 +0900

    [Add] prob3 totalClap func
    + totlClap(int) : sum clap count 1 ~ n and return it

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,5 @@
 package onboarding;
 
-// func : count 1 to number, return total clap count
 public class Problem3 {
     // func : check this number contains how many 369s, return is number of 369s
     private static int checkNum(int n) {
@@ -11,14 +10,23 @@ public class Problem3 {
         for (int i=length-1;i>=0;i--){
             int tmp = (int)(num / Math.pow(10,i));
             // if tmp is 3 6 9, add clap
-            if(tmp % 3 == 0) clap += 1;
+            if((tmp != 0) && (tmp % 3 == 0)) clap += 1;
             // refresh num
             num = (int)(num % Math.pow(10,i));
         }
         return clap;
     }
+
+    // func : count 1 to number, return total clap count
+    private static int totalClap(int n) {
+        int total = 0;
+        for (int i=1;i<=n;i++)
+            total += checkNum(i);
+
+        return total;
+    }
     public static int solution(int number) {
-        int answer = 0;
+        int answer = totalClap(number);
         return answer;
     }
 }
