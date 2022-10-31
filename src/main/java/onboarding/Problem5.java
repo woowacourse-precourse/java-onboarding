07e6317d1c1b36151d2f85e78b7aa8a023b9b5bf
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-31 23:33:34 +0900

    feat: implement exchange money

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] moneyUnits = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return exchangeMoney(money);
+    }
+
+    private static List<Integer> exchangeMoney(int money) {
+        List<Integer> moneyList = new ArrayList<>();
+        for (int moneyUnit : moneyUnits) {
+            moneyList.add(money / moneyUnit);
+            money %= moneyUnit;
+        }
+
+        return moneyList;
     }
 }
