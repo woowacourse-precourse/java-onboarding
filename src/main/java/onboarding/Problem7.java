bcc1d59a8581c37c2ee5d00b2b58aaff70bd9018
은지 <midzy0228@gmail.com>
2022-10-31 23:24:29 +0900

    feat[Problem7]: 팔로우리스트 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,21 +16,38 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         visitorsCount(visitors);
-
         return answer;
     }
 
     public static HashMap visitorsCount(List<String> visitors) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        for (int i = 0; i < visitors.size(); i++) {
+        for(int i=0; i<visitors.size(); i++) {
             String visitor = visitors.get(i);
-            if (map.get(visitor) == null) {
-                System.out.println("널 나옴");
+            if(map.get(visitor) == null) {
+                //System.out.println("널 나옴");
                 map.put(visitor, 1);
                 continue;
             }
-            map.put(visitor, map.get(visitor) + 1);
+            map.put(visitor, map.get(visitor)+1);
         }
         return map;
+    }
+
+    public static List<String> flowedFriends(String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
+
+        for(int i=0; i<friends.size(); i++) {
+            String idA = new String(friends.get(i).get(0));
+            String idB = new String(friends.get(i).get(1));
+
+            //자신과 자신은 친구일까
+            if(user.equals(idA)) {
+                friendList.add(idB);
+            }
+            if(user.equals(idB)) {
+                friendList.add(idA);
+            }
+        }
+        return friendList;
     }
 }
