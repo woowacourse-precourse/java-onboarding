03757beae542a10a2c00089e79fc98557b09c069
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-10-31 13:31:10 +0900

    Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(solution(N));
+    }
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 2; i <= number; i++) {
+        String str = Integer.toString(i);
+        for(int j = 0; j < str.length(); j++){
+            if (str.charAt(j) == '3' || str.charAt(j) == '6' || str.charAt(j) == '9') {
+                answer++;
+            }
+            else{
+                continue;
+            }
+                }
+            }
         return answer;
     }
 }
