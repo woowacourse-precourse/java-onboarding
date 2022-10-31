7adf4cc9fefd24f22e5c539d84b9a2c1047d875f
5onchangwoo <scw9410@gmail.com>
2022-10-31 21:28:53 +0900

    problem7/친구 추천 점수 맵 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,14 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        // 1. 친구 추천 점수를 저장할 맵 생성
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
