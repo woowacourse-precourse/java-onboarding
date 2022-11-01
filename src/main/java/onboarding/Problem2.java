f39513d4fefd22ecfb2d504ea9af844dd639de79
WonSubSub <boxer1995@naver.com>
2022-11-01 15:58:53 +0900

    Problem2 Modification

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,19 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = cryptogram;
+        List<Character> answer_list = new ArrayList<>();
+        for (int i = 0; i < answer.length(); i++) {
+            answer_list.add(answer.charAt(i));
+        }
         int count = 0;
+
         do {
             count = 0;
-            for(int i = 0; i < answer.length()-1; i++){
-                if (answer.charAt(i) == answer.charAt(i+1)){
-                    answer = answer.substring(0,i) + answer.substring(i+2);
+            Set<Integer> index_set = new HashSet<>();
+            List<Integer> index_list = new ArrayList<>();
+            for(int i = 0; i < answer_list.size()-1; i++){
+                if (answer_list.get(i) == answer_list.get(i+1)){
+                    index_set.add(i);
+                    index_set.add(i+1);
                     count += 1;
-                    break;
-                }//if
+                } // if - 앞뒤문자비교
             }//for
-        } while(count != 0); //do
+            List<Character> answer_list2 = new ArrayList<>();
+            for (int i = 0; i < answer_list.size(); i++) {
+                if (index_set.contains(i)) {
+                    continue;
+                }
+                answer_list2.add(answer_list.get(i));
+            }
+            answer_list = new ArrayList<>(answer_list2);
+        } while(count != 0);
+
+        answer = "";
+        for (int i = 0; i < answer_list.size(); i++) {
+            answer += answer_list.get(i);
+        }//for
+
         return answer;
     }
 }
