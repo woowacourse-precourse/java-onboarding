38f593f56bea70957a88c69069fc9b3e83e0b8ee
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 18:42:55 +0900

    refactor : Problem4 클린코드를 위한 수정, else 문 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,14 +2,12 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = changeWordToFrogWord(word);
-        return answer;
+        return changeWordToFrogWord(word);
     }
 
     public static String changeWordToFrogWord(String word){
         String changedWord;
-        char[] preWord = word.toCharArray();
-        char[] postWord = new char[preWord.length];
+        char[] tempWord = word.toCharArray();
 
         /**
          *  ASCII Table
@@ -19,16 +17,14 @@ public class Problem4 {
          *  z = 122
          *  아스키 코드를 활용하여 문자변환
          */
-        for(int i=0; i<preWord.length; i++){
-            if((preWord[i] >= 'A') && (preWord[i] <= 'Z'))
-                postWord[i] = (char) (155 - preWord[i]);
-            else if((preWord[i] >= 'a') && (preWord[i] <= 'z'))
-                postWord[i] = (char) (219 - preWord[i]);
-            else
-                postWord[i] = preWord[i];
+        for(int i=0; i<tempWord.length; i++){
+            if((tempWord[i] >= 'A') && (tempWord[i] <= 'Z'))
+                tempWord[i] = (char) (155 - tempWord[i]);
+            if((tempWord[i] >= 'a') && (tempWord[i] <= 'z'))
+                tempWord[i] = (char) (219 - tempWord[i]);
         }
 
-        changedWord = new String(postWord);
+        changedWord = new String(tempWord);
         return changedWord;
     }
 }
