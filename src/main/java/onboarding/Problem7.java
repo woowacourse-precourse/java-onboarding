a0cdad39cafa3b787618264627409683f0ec9dd6
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-01 15:03:33 +0900

    221101 Feat: P7 - 기능3 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,12 +7,16 @@ public class Problem7 {
     public static ArrayList<Integer> graph[];
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
-        initData(friends);
+        List<String> users = initData(friends, visitors);
 
         //3. 주어진 사용자 아이디와 친구인 사용자를 조사하여 점수를 부여한다.
         List<String> answer = Collections.emptyList();
+        int[] score = new int[users.size()];
+        int userIdx = users.indexOf(user);
 
-
+        for(int w : graph[userIdx]){
+            score = calculateScore(score, w);
+        }
 
         //4. visitor 리스트를 조사하여, 방문한 사용자에게 1점씩 부여한다.
         //5. 모든 점수를 부여했다면 점수가 높은 5명을 추린다.
@@ -20,7 +24,15 @@ public class Problem7 {
         return answer;
     }
 
-    public static void initData(List<List<String>> friends){
+    public static int[] calculateScore(int[] score, int friendNumber){
+        for(int w : graph[friendNumber]){
+            score[w] += 10;
+        }
+
+        return score;
+    }
+
+    public static List<String> initData(List<List<String>> friends, List<String> visitors){
         //1. 친구 정보가 주어진 모든 사용자명을 추출한다.
         Set<String> userSet = new HashSet<>();
 
@@ -30,6 +42,9 @@ public class Problem7 {
             userSet.add(user1);
             userSet.add(user2);
         }
+
+        for(String data : visitors)
+            userSet.add(data);
 
         //2. 친구 정보를 담는 그래프(연결리스트)를 생성하여 데이터를 저장한다.
         graph = new ArrayList[userSet.size()];
@@ -45,5 +60,7 @@ public class Problem7 {
             graph[user1].add(user2);
             graph[user2].add(user1);
         }
+
+        return users;
     }
 }
