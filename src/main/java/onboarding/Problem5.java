b47762f0992aab0bed531ee8ab0b5eef4e8996c8
simjaeseo <delpho@naver.com>
2022-11-01 19:56:52 +0900

    feat: money를 큰 금액을 우선으로 변환하는 함수

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +9,23 @@ import java.util.List;
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int unit : units) {
+            money = conversionMoney(money, answer, unit);
+        }
+
         return answer;
+    }
+
+    public static int conversionMoney(int money, List<Integer> answer, int unit) {
+        if (money / unit >= 1) {
+            answer.add(money / unit);
+            return money % unit;
+        }
+        answer.add(0);
+        return money;
     }
 }
