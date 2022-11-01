76439afac2fc68d9d26034f4430e3e82d80c7b7a
boogahead <wherestuffgoes2@gmail.com>
2022-11-01 11:17:40 +0900

    prob2and3solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
 
 public class Problem2 {
+    public static boolean checker(String input)
+    {
+        int i;
+        for(i=0;i<input.length();i++)
+        {
+            if(i>0&&(Character.compare(input.charAt(i),input.charAt(i-1)))==0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
-        return answer;
+        int i;
+        while(checker(cryptogram))
+        {
+            for(i=0;i<cryptogram.length()-1;i++)
+            {
+                if(Character.compare(cryptogram.charAt(i),cryptogram.charAt(i+1))==0)
+                {
+                    cryptogram=cryptogram.substring(0,i+1)+cryptogram.substring(i+2);
+                    cryptogram=cryptogram.substring(0,i)+cryptogram.substring(i+1);
+                }
+            }
+        }
+        return cryptogram;
     }
 }
