cf001818c032acf574d7349b2fab19ec0e63f2b8
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 11:35:09 +0900

    문자 반전 수식

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -14,7 +14,9 @@ public class Problem4 {
                     ascii = 65;
                 else if (Character.isLowerCase(wordChar[i])) // 소문자일 경우, ascii 'a'의 코드번호
                     ascii = 97;
-
+                abs = Math.abs(ascii - wordChar[i]); // 아스키 코드값과 현재 알파벳 사이의 차이를 구한 후 소문자 / 대문자를 끝까지 밀고 절대값을 빼주는 방식
+                reverse = (ascii + 25) - abs;
+                result.append((char) reverse);
             }
             else
                 result.append(wordChar[i]); // 알파벳이 아닐 경우, 그대로
