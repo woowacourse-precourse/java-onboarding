d79fc9bf661ea151783ac0d3c9bb33039872095b
choiejae <ejae8319@gmail.com>
2022-11-01 17:50:23 +0900

    feat (isDuplicated): 모든 닉네임을 한번씩 검증하여 서로 중복되는 닉네임의 명단을 list로 받아오기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,11 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Problem6 {
@@ -68,5 +73,38 @@ public class Problem6 {
                 throw new IllegalArgumentException("올바른 도메인 형식이 아닙니다.");
             }
         }
+    }
+    private static List<String> getAnswer(List<List<String>> forms) {
+        Map<String, String> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+
+        makeMap(map, forms);
+        for (String nickName : map.keySet()) {
+            if (isDuplicated(nickName, map)) {
+                set.add(map.get(nickName));
+            }
+        }
+        return new ArrayList<>();
+//        return makeSort(set);
+    }
+    private static void makeMap(Map<String, String> map, List<List<String>> forms) {
+        for (List<String> form : forms) {
+            map.put(form.get(1), form.get(0));
+        }
+    }
+    private static boolean isDuplicated(String nickName, Map<String, String> map) {
+        if (nickName.length() < 2)
+            return false;
+        for (String nickName2 : map.keySet()) {
+            return isMatchTwoWord(nickName, nickName2);
+        }
+        return false;
+    }
+    private static boolean isMatchTwoWord(String str1, String str2) {
+        for (int i = 0; i < str2.length()-2; i++) {
+            if (str1.contains(str2.substring(i, i+2)))
+                return true;
+        }
+        return false;
     }
 }
