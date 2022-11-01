6e53173df9ee05569d5a18c89b844f985e51b749
ye1ne <yewon7036@naver.com>
2022-11-01 16:15:14 +0900

    feature : 메인 solution 함수 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,15 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int totalClap = 0;
+        for (int i = 1; i <= number; i++) {
+            totalClap += countClap(i);
+        }
+        return totalClap;
     }
+
+    private static int countClap(int number) {
+    }
+
+
 }
