879a88b81ea6275d37cbb329b8aa1cddc0b7b87a
이건회 <rg970604@naver.com>
2022-11-01 20:26:10 +0900

    feat(Problem3.java) : 특정 번호 박수 숫자 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,13 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int getClappingNum(String[] clapnums, int answer, String now) {
+        for (String clapnum : clapnums) {
+            answer += now.chars().filter(it -> it == clapnum.charAt(0)).count();
+        }
+        return answer;
+    }
     private static void numberRangeValidation(int number) {
         if (number < MIN_LENGTH || number > MAX_LENGTH) {
             throw new IllegalArgumentException("1 이상 10000 이하 숫자만 입력 가능합니다");
