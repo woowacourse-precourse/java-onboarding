caef9b7be1b594e0ed6883130166b9412fbc3736
xi_jjun <rlawowns97@naver.com>
2022-11-01 18:17:48 +0900

    feat(Problem7): 추천친구 점수를 모든 이름에 대해 초기화 하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,27 +7,34 @@ import java.util.List;
 import java.util.Map;
 
 public class Problem7 {
-    static final Map<String, List<String>> friendRelationGraph = new HashMap<>();
+	static final Map<String, List<String>> friendRelationGraph = new HashMap<>();
+	static final Map<String, Integer> recommendationScores = new HashMap<>();
 
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> answer = Collections.emptyList();
+		return answer;
+	}
 
-    public static void makeFriendRelationGraph(List<List<String>> friends) {
-        for (List<String> friend : friends) {
-            addFriendRelationGraph(friend);
-        }
-    }
+	public static void initRecommendationScores() {
+		friendRelationGraph.keySet()
+				.stream()
+				.forEach(key -> recommendationScores.putIfAbsent(key, 0));
+	}
 
-    private static void addFriendRelationGraph(List<String> friend) {
-        String friendFrom = friend.get(0);
-        String friendTo = friend.get(1);
+	public static void makeFriendRelationGraph(List<List<String>> friends) {
+		for (List<String> friend : friends) {
+			addFriendRelationGraph(friend);
+		}
+	}
+
+	private static void addFriendRelationGraph(List<String> friend) {
+		String friendFrom = friend.get(0);
+		String friendTo = friend.get(1);
 		friendRelationGraph.putIfAbsent(friendFrom, new ArrayList<>());
 		friendRelationGraph.putIfAbsent(friendTo, new ArrayList<>());
-        friendRelationGraph.getOrDefault(friendFrom, new ArrayList<>())
+		friendRelationGraph.getOrDefault(friendFrom, new ArrayList<>())
 				.add(friendTo);
 		friendRelationGraph.getOrDefault(friendTo, new ArrayList<>())
 				.add(friendFrom);
-    }
+	}
 }
