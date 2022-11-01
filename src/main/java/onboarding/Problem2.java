13b1aacb7c0099169cf0817c7b1ad5b866397ee8
Moveuk <v_donguk@naver.com>
2022-11-01 15:08:18 +0900

    feat: Problem2 연속하는 중복 문자들을 삭제하는 기능
    stack에 문자가 존재하면서 현재 문자와 stack 최상단의 문자가 같다면 pop하여 제거한다
    그외 다른 경우는 stack에 적재한다
    모든 문자열에 대한 검증이 끝나면 mutable한 StringBuilder 객체에 stack의 값을 append하고 반환타입인 String으로 변환시켜 return한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = cryptogram.toCharArray();
+
+        for (char curruntCharacter : charArray) {
+            if (!stack.empty() && stack.peek() == curruntCharacter) {
+                stack.pop();
+                continue;
+            }
+            stack.push(curruntCharacter);
+        }
+        stack.forEach(answer::append);
+
+        return answer.toString();
     }
 }
