94401eb8bb5eb5f41fcc1642e75755c96c896628
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 14:16:16 +0900

    [4번] feat: String을 Frog Rule에 따라 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,8 +5,21 @@ import onboarding.Problem4Package.UpperCase;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = translateStringByFrog(word);
         return answer;
+    }
+
+    // 1. String을 Frog Rule에 따라 변환
+    public static String translateStringByFrog(String word)
+    {
+        String newWord = "";
+
+        for (int i = 0 ; i < word.length() ; i ++) {
+            char x = word.charAt(i);
+            newWord += translateByFrog(x);
+        }
+
+        return newWord.toString();
     }
 
     // 2. 한 문자씩 Frog Rule에 따라 변환
