dabf11fc38651d80822161d03045c6361fdd484c
jungbum05114@naver.com <jungbum05114@gmail.com>
2022-11-02 00:04:39 +0900

    실수..

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -17,39 +17,38 @@ import java.util.stream.Stream;
  */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        try{
+        try {
 
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
         List<String> answer = Collections.emptyList();
-        List <String> friendList = new ArrayList<>(myFriendsSearch(user,friends));
+        List<String> friendList = new ArrayList<>(myFriendsSearch(user, friends));
         System.out.println(friendList);
-        List <String> friendCandidate= new ArrayList<>(friendOfFriend(friends,friendList));
+        List<String> friendCandidate = new ArrayList<>(friendOfFriend(friends, friendList));
         System.out.println(friendCandidate);
 
-        List <String> notFriendOfFriendCandidateBeforeScore = deleteMyAndFriend(friendCandidate,friendList,user);
-        List <String> notFriendOfVisitorBeforeScored = deleteMyAndFriendOfVisitors(user,friendList,visitors);
+        List<String> notFriendOfFriendCandidateBeforeScore = deleteMyAndFriend(friendCandidate, friendList, user);
+        List<String> notFriendOfVisitorBeforeScored = deleteMyAndFriendOfVisitors(user, friendList, visitors);
         System.out.println(notFriendOfFriendCandidateBeforeScore);
 
-        List <String> deduplicationNotFriendOfFriendCandidate = deduplication(notFriendOfFriendCandidateBeforeScore);
-        List <String> deduplicationNotFriendOfVisitor = deduplication(notFriendOfVisitorBeforeScored);
+        List<String> deduplicationNotFriendOfFriendCandidate = deduplication(notFriendOfFriendCandidateBeforeScore);
+        List<String> deduplicationNotFriendOfVisitor = deduplication(notFriendOfVisitorBeforeScored);
         System.out.println(deduplicationNotFriendOfFriendCandidate);
 
-        Map<String, Integer> notFriendOfFriendCandidate=scoreSet(notFriendOfFriendCandidateBeforeScore,deduplicationNotFriendOfFriendCandidate,10);
-        Map<String, Integer> notFriendOfVisitor=scoreSet(notFriendOfVisitorBeforeScored,deduplicationNotFriendOfVisitor,1);
+        Map<String, Integer> notFriendOfFriendCandidate = scoreSet(notFriendOfFriendCandidateBeforeScore, deduplicationNotFriendOfFriendCandidate, 10);
+        Map<String, Integer> notFriendOfVisitor = scoreSet(notFriendOfVisitorBeforeScored, deduplicationNotFriendOfVisitor, 1);
         System.out.println(notFriendOfVisitor);
         System.out.println(notFriendOfFriendCandidate);
 
 
         notFriendOfFriendCandidate.forEach((key, value) -> notFriendOfVisitor.merge(key, value, (v1, v2) -> v2));
-        Map <String,Integer> answerMap = notFriendOfVisitor;
+        Map<String, Integer> answerMap = notFriendOfVisitor;
 
 
         answer = new ArrayList<>();
         System.out.println(answerMap);
-        Map<String,Integer> topFive =
+        Map<String, Integer> topFive =
                 answerMap.entrySet().stream()
                         .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                         .limit(5)
@@ -64,29 +63,28 @@ public class Problem7 {
 
         return answer;
     }
-    public static List<String> myFriendsSearch(String user,List<List<String>> friends)
-    {
+
+    public static List<String> myFriendsSearch(String user, List<List<String>> friends) {
         List<List<String>> copyFriends = new ArrayList<>();
         copyFriends.addAll(friends);
         List<String> myFriends = new ArrayList<>();
-        for (int i=0;i<copyFriends.size();i++) {
-            if(copyFriends.get(i).contains(user))
-            {
+        for (int i = 0; i < copyFriends.size(); i++) {
+            if (copyFriends.get(i).contains(user)) {
                 myFriends.addAll(copyFriends.get(i));
                 continue;
             }
         }
 
 //        myFriends.addAll();
-        myFriends=new ArrayList<>(myFriends.stream().distinct().collect(Collectors.toList()));
+        myFriends = new ArrayList<>(myFriends.stream().distinct().collect(Collectors.toList()));
 
         myFriends.remove(user);
         return myFriends;
     }
-    public static List<String> friendOfFriend(List<List<String>> friend,List<String> myFriend)
-    {
+
+    public static List<String> friendOfFriend(List<List<String>> friend, List<String> myFriend) {
         List<String> friendCandidate = new ArrayList<>();
-        for(int j=0;j<myFriend.size();j++) {
+        for (int j = 0; j < myFriend.size(); j++) {
             for (int i = 0; i < friend.size(); i++) {
                 if (friend.get(i).contains(myFriend.get(j))) {
                     friendCandidate.addAll(friend.get(i));
@@ -95,41 +93,37 @@ public class Problem7 {
         }
         return friendCandidate;
     }
-    public static List<String> deleteMyAndFriend(List<String> friendCandidate,List<String> myFriend,String user)
-    {
+
+    public static List<String> deleteMyAndFriend(List<String> friendCandidate, List<String> myFriend, String user) {
         System.out.println(myFriend);
 
 
-        for(int i=0;i<friendCandidate.size()+2;i++){
-           friendCandidate.remove(user);
-           System.out.println(friendCandidate);
+        for (int i = 0; i < friendCandidate.size() + 2; i++) {
+            friendCandidate.remove(user);
+            System.out.println(friendCandidate);
         }
         System.out.println("test");
-        for(int i=0;i<myFriend.size();i++)
-        {
-            for(int j=0;j<friendCandidate.size()+2;j++)
-            {
+        for (int i = 0; i < myFriend.size(); i++) {
+            for (int j = 0; j < friendCandidate.size() + 2; j++) {
                 System.out.println(myFriend.get(i));
                 friendCandidate.remove(myFriend.get(i));
-                System.out.println(friendCandidate+""+friendCandidate.size());
+                System.out.println(friendCandidate + "" + friendCandidate.size());
             }
         }
         return friendCandidate;
     }
-    public static List<String> deleteMyAndFriendOfVisitors(String user,List<String> myFriend,List<String> visitors)
-    {
+
+    public static List<String> deleteMyAndFriendOfVisitors(String user, List<String> myFriend, List<String> visitors) {
         List<String> newVisitors = new ArrayList<>();
         newVisitors.addAll(visitors);
-        System.out.println(newVisitors+"시작");
-        for(int i=0;i<newVisitors.size();i++){
+        System.out.println(newVisitors + "시작");
+        for (int i = 0; i < newVisitors.size(); i++) {
             newVisitors.remove(user);
             System.out.println(newVisitors);
         }
         System.out.println("test2");
-        for(int i=0;i<myFriend.size();i++)
-        {
-            for(int j=0;j<newVisitors.size();j++)
-            {
+        for (int i = 0; i < myFriend.size(); i++) {
+            for (int j = 0; j < newVisitors.size(); j++) {
                 System.out.println(myFriend.get(i));
                 newVisitors.remove(myFriend.get(i));
                 System.out.println(newVisitors);
@@ -137,11 +131,12 @@ public class Problem7 {
         }
         return newVisitors;
     }
-    public static List<String> deduplication(List<String> list)
-    {
-       return list.stream().distinct().collect(Collectors.toList());
+
+    public static List<String> deduplication(List<String> list) {
+        return list.stream().distinct().collect(Collectors.toList());
     }
-    public static Map<String, Integer>scoreSet(List<String> list, List<String> deduplication, int score) {
+
+    public static Map<String, Integer> scoreSet(List<String> list, List<String> deduplication, int score) {
         int count;
         Map<String, Integer> scores = new HashMap<>();
         for (int i = 0; i < deduplication.size(); i++) {
@@ -150,4 +145,4 @@ public class Problem7 {
         }
         return scores;
     }
-
+}
