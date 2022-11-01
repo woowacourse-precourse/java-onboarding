43b3b80b438478ebb9959d20a0edb16626f8af9c
jxixeun <jxixeun@gmail.com>
2022-11-01 16:42:17 +0900

    Problem 4 코드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] chars = word.toCharArray();
+        char b = 219;
+        char d = 155;
+        for (int i =0;i<word.length();i++) {
+            char new_ch;
+            if (chars[i]>=65&&chars[i]<=90) {
+                new_ch = (char)(d-chars[i]);
+                chars[i] = new_ch;
+            }
+            else if (chars[i]>=97&&chars[i]<=122){
+                new_ch = (char)(b-chars[i]);
+                chars[i] = new_ch;
+            }
+        }
+        return new String(chars);
     }
 }
