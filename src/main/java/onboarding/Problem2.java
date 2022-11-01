a9334ca8a533e132b1fd60af6a6a7265dba052c1
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 13:54:20 +0900

    [2번] feat: 중복 문자열 남아있는 지 체크하기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,6 +6,15 @@ public class Problem2 {
         return answer;
     }
 
+    // 5. 중복 문자열 남아있는 지 체크하기
+    public static Boolean checkIfDuplicatesRemain(String s)
+    {
+        for(int i = 0; i < s.length()-1 ; i++){
+            if (s.charAt(i) == s.charAt(i+1)) return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+
     // 6. String 길이가 남았는지 체크하기
     public static Boolean checkIfStringRemain(String s)
     {
