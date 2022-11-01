7806bac26feb008af9f77c30aad160760bb34019
bnbnac <bnbnac@naver.com>
2022-11-01 22:36:33 +0900

    feat: (P4)문자열을 한 글자씩 아스키코드로 변환하여 ArrayList에 입력하는 메소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        ArrayList<Object> wordList = getAsciiList(word);
+
         return answer;
     }
+
+    private static ArrayList<Object> getAsciiList(String word) {
+        char[] ch = word.toCharArray();
+        ArrayList<Integer> asciiList = new ArrayList<>();
+        for (char c : ch) {
+            asciiList.add((int) c);
+        }
+        return asciiList;
+    }
+
 }
