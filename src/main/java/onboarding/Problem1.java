9c2e64372a90226cc167ba038ed03f4b591e6712
a1dotls <meeae9408@gmail.com>
2022-11-01 19:05:39 +0900

    4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다. 5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다. 6. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,70 +6,93 @@ import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
+	
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    	
+    	int a = 0;
+    	int b = 0;
+    	int c = 0;
+    	int sum = 0;
+    	int gop = 0;
+    	
+    	List<Integer> pobiCal = new ArrayList<Integer>();
+    	List<Integer> crongCal = new ArrayList<Integer>();
+    	
+    	int pPage = (int)((400)*Math.random( ) + 1);
+    	int cPage = (int)((400)*Math.random( ) + 1);
+    	
+    	
+    	if(pPage % 2 == 0) {
+    		pobi.add(pPage-1);
+    		pobi.add(pPage);
+    	} else {
+    		pobi.add(pPage);
+    		pobi.add(pPage+1);
+    	}
+    	
+    	if(cPage % 2 == 0) {
+    		crong.add(cPage-1);
+    		crong.add(cPage);
+    	} else {
+    		crong.add(cPage);
+    		crong.add(cPage+1);
+    	}
+    	
+    	for(int num : pobi) {
+    		
+    		a = num / 100;
+    		b = num / 10 - ( a * 10 );
+    		c = num % 10;
+    		sum = a + b + c;
+    		
+    		if(a == 0 && b == 0) {
+    			gop = c;
+    		} else if(a == 0) {
+    			gop = b * c;
+    		} else {
+    			gop = a * b * c; 
+    		}
+    		
+    		pobiCal.add(sum);
+    		pobiCal.add(gop);
+    	}
+    	
+    	for(int num : crong) {
+    		
+    		a = num / 100;
+    		b = num / 10 - ( a * 10 );
+    		c = num % 10;
+    		sum = a + b + c;
+    		
+    		if(a == 0 && b == 0) {
+    			gop = c;
+    		} else if(a == 0) {
+    			gop = b * c;
+    		} else {
+    			gop = a * b * c; 
+    		}
+    		
+    		crongCal.add(sum);
+    		crongCal.add(gop);
+    	}
+    	
+    	Collections.sort(pobiCal, Collections.reverseOrder());
+    	Collections.sort(crongCal, Collections.reverseOrder());
+    	
+    	int answer = Integer.MAX_VALUE;
 
-	public static int solution(List<Integer> pobi, List<Integer> crong) {
-
-		int a = 0;
-		int b = 0;
-		int c = 0;
-		int sum = 0;
-		int gop = 0;
-
-		int pPage = (int) ((400) * Math.random() + 1);
-		int cPage = (int) ((400) * Math.random() + 1);
-
-		if (pPage % 2 == 0) {
-			pobi.add(pPage - 1);
-			pobi.add(pPage);
-		} else {
-			pobi.add(pPage);
-			pobi.add(pPage + 1);
-		}
-
-		if (cPage % 2 == 0) {
-			crong.add(cPage - 1);
-			crong.add(cPage);
-		} else {
-			crong.add(cPage);
-			crong.add(cPage + 1);
-		}
-
-		for (int num : pobi) {
-
-			a = num / 100;
-			b = num / 10 - (a * 10);
-			c = num % 10;
-			sum = a + b + c;
-
-			if (a == 0 && b == 0) {
-				gop = c;
-			} else if (a == 0) {
-				gop = b * c;
-			} else {
-				gop = a * b * c;
-			}
-
-		}
-
-		for (int num : crong) {
-
-			a = num / 100;
-			b = num / 10 - (a * 10);
-			c = num % 10;
-			sum = a + b + c;
-
-			if (a == 0 && b == 0) {
-				gop = c;
-			} else if (a == 0) {
-				gop = b * c;
-			} else {
-				gop = a * b * c;
-			}
-
-		}
-
-		int answer = Integer.MAX_VALUE;
-
-		return answer;
-	}
+    	if(pobiCal.get(0) > crongCal.get(0)) {
+    		answer = 1;
+    	} else if(pobiCal.get(0) < crongCal.get(0)) {
+    		answer = 2;
+    	} else if(pobiCal.get(0) == crongCal.get(0)) {
+    		answer = 0;
+    	} else if(pPage == 400 || pPage == 400 || cPage == 1 || cPage == 1) {
+    		answer = -1;
+    	} else {
+    		answer = -1;
+    	}
+    		
+        return answer;
+    }
 }
