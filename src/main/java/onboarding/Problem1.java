20dd634c6b4721307df04896561355ec7ea6509e
ryuj-h <dbwlsgh57@naver.com>
2022-11-01 15:53:26 +0900

    문제 1번 테스트 커밋

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
+
 class Problem1 {
+    static int getMax(List<Integer> input) {
+        return 0;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobipoint;
+        int crongpoint;
+
+        if (abs(pobi.get(0) - pobi.get(1)) > 1 || abs(crong.get(0) - crong.get(1)) > 1)
+            return -1;
+
+        pobipoint = getMax(pobi);
+        crongpoint = getMax(crong);
+
+        if (pobipoint == crongpoint)
+            answer = 0;
+        else if (pobipoint > crongpoint)
+            answer = 1;
+        else
+            answer = 2;
 
         return answer;
     }
