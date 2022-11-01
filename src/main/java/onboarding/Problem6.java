cd7d17c75c18d57fe122989170ca797ebdf03007
이연재 <leeyj0304@gmail.com>
2022-11-01 23:11:45 +0900

    Problem6: Added features to split forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> email = new ArrayList<>();
+        List<String> name = new ArrayList<>();
+
         return answer;
+    }
+
+    public static void extractName(String name, List<String> output) {
+        for (int i = 0; i < name.length() - 1; i++) {
+            output.add(name.substring(i, (i + 2)));
+        }
     }
 }
