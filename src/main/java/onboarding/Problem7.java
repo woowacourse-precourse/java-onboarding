2a2176f284e5ea0d2cf3a091d2dbbfbf17114091
CAEL0 <kjongmin26@gmail.com>
2022-11-01 15:49:58 +0900

    기능 추가: PROBLEM7 그래프 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,6 +10,18 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+    private static HashMap<String, HashSet<String>> constructGraph(List<List<String>> friends) {
+        HashMap<String, HashSet<String>> graph = new HashMap<>();
+
+        for (List<String> friendRelationship: friends) {
+            String idA = friendRelationship.get(0);
+            String idB = friendRelationship.get(1);
+
+            addEdge(graph, idA, idB);
+            addEdge(graph, idB, idA);
+        }
+        return graph;
+    }
     private static void addEdge(HashMap<String, HashSet<String>> graph, String userFrom, String userTo) {
         if (graph.containsKey(userFrom))
             graph.get(userFrom).add(userTo);
