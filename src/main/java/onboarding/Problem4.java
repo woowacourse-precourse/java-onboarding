97ec1564fedab7e4dd49a25030954a4a5eed6358
khj <kim37171@cuk.edu>
2022-11-01 20:28:07 +0900

    [Problem4] 대소문자 구분

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
+    private static boolean isLowerCase(char now) {
+        return 'a' <= now && now <= 'z';
+    }
+
+    private static boolean isUpperCase(char now) {
+        return 'A' <= now && now <= 'Z';
     }
 }
