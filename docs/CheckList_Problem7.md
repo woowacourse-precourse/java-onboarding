e5099fc2fb5732615c37526a55652a11e611191c
ttaehee <ttaehee1105@gmail.com>
2022-11-01 17:08:32 +0900

    문제7) feat : 함께 아는 친구에 따른 추천점수 더하기 기능(+ 이미 친구인 경우 제외)

--- docs/CheckList_Problem7.md
+++ docs/CheckList_Problem7.md
@@ -5,7 +5,7 @@
 | 기능                       | function    | 구현여부 |
 |--------------------------|-------------|------|
 | 내 친구목록 기능                | getFriendsList     | O    |
-| 함께 아는 친구에 따른 추천점수 더하기 기능 | getFriendPoint     | X    |
+| 함께 아는 친구에 따른 추천점수 더하기 기능 | getFriendPoint     | O    |
 | 방문에 따른 추천점수 더하기 기능       | getVisitPoint     | X    |
-| 이미 친구인 경우 제외 기능          | isFriend     | X    |
+| 이미 친구인 경우 제외 기능          | isFriend     | O    |
 
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,14 +1,13 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         List<String> friendsList = getFriendsList(user, friends);
+        Map<String, Integer> friendPointMap = getFriendPoint(user, friendsList, friends, visitors);
 
         return answer;
     }
@@ -26,5 +25,28 @@ public class Problem7 {
             }
         }
         return friendsList;
+    }
+
+    private static Map<String, Integer> getFriendPoint(
+            String user, List<String> friendsList, List<List<String>> friends, List<String> visitors) {
+
+        Map<String, Integer> pointMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String friendName = friend.get(0);
+            String friendOfFriend = friend.get(1);
+            if (!user.equals(friendOfFriend) && isFriend(friendName, friendsList)) {
+                pointMap.put(friendOfFriend, pointMap.getOrDefault(friendOfFriend, 0) + 10);
+            }
+        }
+
+        return pointMap;
+    }
+
+    private static boolean isFriend(String name, List<String> friendsList) {
+        if (friendsList.contains(name)) {
+            return true;
+        }
+        return false;
     }
 }
