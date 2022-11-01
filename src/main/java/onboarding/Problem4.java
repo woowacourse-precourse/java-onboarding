2e9758c57a60d6ae1ab817de09b8e5c1f1fafd3a
jungle <minuet1215@jbnu.ac.kr>
2022-11-01 18:36:32 +0900

    Feat: Problem4 Solve

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] word_array = word.toCharArray();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (isChar(word_array[i])) {
+                word_array[i] = swap_char(word_array[i]);
+            }
+        }
+
+        return String.valueOf(word_array);
+    }
+
+    public static char swap_char(char c) {
+        return (c >= 65 && c <= 90) ? (char) (90 - (c - 65)) : (char) (122 - (c - 97));
+    }
+
+    public static boolean isChar(char w) {
+        return Character.isLetter(w) ? true : false;
     }
 }
