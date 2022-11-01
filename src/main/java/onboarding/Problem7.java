27ab21fafdc3d2bbbeb3479b2f96e3bfe4bd45e2
LeeJinHo <hosboy93@naver.com>
2022-11-01 11:08:31 +0900

    문제7. visitor가 방문 했을 때 점수 추가, 최종 점수 확인 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
@@ -9,6 +10,9 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         List<String> findFriends = new ArrayList<>();
         List<String> userFriends = new ArrayList<>();
+        HashMap<String, Integer> scoreMap = new HashMap<String, Integer>();
+        int knowFriend = 10;
+        int visit = 1;
         
         // 유저의 친구 목록
         for(int i=0; i<friends.size(); i++) { // 6
@@ -23,6 +27,18 @@ public class Problem7 {
         for(String friend : findFriends) {
         	if(!userFriends.contains(friend)) userFriends.add(friend);
         }
+        findFriends.clear();
+        
+        // 방문자 확인
+        for(int i=0; i<visitors.size(); i++) { // 5
+        	if(!scoreMap.containsKey(visitors.get(i))) scoreMap.put(visitors.get(i), visit);
+        	else scoreMap.put(visitors.get(i), scoreMap.get(visitors.get(i))+visit);
+        }
+        
+        // 점수
+        scoreMap.forEach((key, value) -> {
+        	System.out.println(key + " : " + value);
+        });
         
         return answer;
     }
