4f8bca4f73acdce7c591428cb94b0671fe04c21f
dydtmd6801 <dydtmd6801@naver.com>
2022-11-01 11:12:31 +0900

    feat : 정렬 및 list로 변환 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = new ArrayList<>();
+        List<String> answer;
         HashMap<String, Integer> friendsMap = new HashMap<>();
         List<String> oldFriendNames = checkOldFriends(friends, user, friendsMap, visitors);
         putNewFriends(friends, oldFriendNames, friendsMap, user);
         putVisitor(visitors, oldFriendNames, friendsMap);
+        answer = hashMapToList(friendsMap);
         return answer;
     }
 
@@ -42,8 +43,35 @@ public class Problem7 {
             if(friendsMap.containsKey(visitorName)) friendsMap.put(visitorName, (int)friendsMap.get(visitorName)+1);
             if(!friendsMap.containsKey(visitorName)) friendsMap.put(visitorName, 1);
         }
-        for(int i = 0; i < friendsMap.size() - 1; i++){
+        for(int i = 0; i < oldFriendName.size(); i++){
             friendsMap.remove(oldFriendName.get(i));
         }
+    }
+
+    private static List<String> hashMapToList(HashMap recommendFriends) {
+        List<String> keys = new ArrayList<>(recommendFriends.keySet());
+        int topRecommend = 0;
+
+        Collections.sort(keys);
+        keys.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Integer value1 = (Integer) recommendFriends.get(o1);
+                Integer value2 = (Integer) recommendFriends.get(o2);
+                return value2.compareTo(value1);
+            }
+        });
+        List<String> result = new ArrayList<>();
+
+        if(keys.size() < 5){
+            topRecommend = keys.size();
+        }
+        if(keys.size() >= 5){
+            topRecommend = 5;
+        }
+        for(int i = 0; i < topRecommend; i++){
+            result.add(String.valueOf(keys.get(i)));
+        }
+        return result;
     }
 }
