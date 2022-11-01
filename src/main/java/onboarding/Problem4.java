747b5eba02025621f61f22d3c186fe8f3ec3ced5
TeddyYeoni <po99027@naver.com>
2022-11-01 23:35:29 +0900

    PROBLEM4 기능 2,3,4 포함 solution 완성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,6 +8,30 @@ public class Problem4 {
 	
     public static String solution(String word) {
         String answer = "";
+        
+        int asciiCode;
+		int reverseAsciiCode;
+		
+		char[] wordToChar = word.toCharArray();
+		char[] changeWordArray = new char[wordToChar.length];
+		
+		for (int i = 0; i < wordToChar.length; i++) {
+			asciiCode = wordToChar[i];
+			
+			if(Character.isUpperCase(wordToChar[i])) {
+				reverseAsciiCode = UPPER_MAX_VALUE + UPPER_MIN_VALUE - asciiCode;
+			} else if (Character.isLowerCase(wordToChar[i])) {
+				reverseAsciiCode = LOWER_MAX_VALUE + LOWER_MIN_VALUE - asciiCode;
+			} else {
+				reverseAsciiCode = asciiCode;
+			}
+			
+			char reverseWord = (char) reverseAsciiCode;
+			changeWordArray[i] = reverseWord;
+		}
+		
+		answer = new String(changeWordArray);
+		
         return answer;
     }
 }
