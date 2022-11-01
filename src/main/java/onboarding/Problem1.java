ac482b433883763b97dc7ab72baabeb503fca682
SproutMJ <mjk54565@gmail.com>
2022-11-01 19:23:21 +0900

    feat: 문제 1 기능 목록과 페이지 개 를 받아 각 자리를 모두 더하거나 곱했을 때의 최댓값을 계산하는 함수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,20 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    //1. 펼친 페이지 번호 두 개를 받으면 계산해서 최댓값을 반환하는 기능
+    int maxScore(List<Integer> scores){
+        int ans = Integer.MIN_VALUE;
+        for (int i:scores){
+            int add = 0, multi = 1;
+            while(i>0){
+                add+=(i%10);
+                multi*=(i%10);
+                add/=10;
+                multi/=10;
+            }
+            ans = Integer.max(ans, Integer.max(add, multi);
+        }
+        return ans;
+    }
 }
