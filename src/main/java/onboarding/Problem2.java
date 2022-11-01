0f14313e3d3a07698877ed1128632619332305b8
sungsoo <tjdtn6725@naver.com>
2022-11-01 12:28:58 +0900

    Problem2.java 기능 구현 : 중복문자가 있는지 확인하는 isDuplicate 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem2 {
+    /*
+    * 1. 중복 문자들을 체크 후 제거
+    * 2. 더 이상 중복된 문자들이 없는지 확인
+    * 3. 더 이상 중복된 문자들이 없을때까지 반복
+    * */
     public static String solution(String cryptogram) {
         String answer = "answer";
+        CheckDuplicate(cryptogram);
         return answer;
+    }
+
+    private static void CheckDuplicate(String str) {
+        String res;
+        char[] arr = str.toCharArray();
+
+    }
+
+    private static boolean isDuplicate(char[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i] == arr[i+1]) return true; //중복문자가 있을경우 true 리턴
+        }
+        return false; //중복문자가 없을 경우 false 리턴턴
     }
 }
