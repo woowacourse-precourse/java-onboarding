b5b57055be5f75397a060991f8d21df64418c45c
박세원 <tpdnjs1825@naver.com>
2022-11-01 23:01:07 +0900

    Feat : 대문자 청개구리 사전 추가 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+
+import java.util.HashMap;
+
 public class Problem4 {
+    private static final int ALPHABET_LENGTH = 26;
+
+    private static void addUpperToDictionary(HashMap<Character, Character> greenFrogDictionary){
+        char startCharacter = 'A';
+        char endCharacter = 'Z';
+
+        for(int i = 0; i < ALPHABET_LENGTH; i++){
+            greenFrogDictionary.put(startCharacter++, endCharacter--);
+        }
+    }
+
     public static String solution(String word) {
         String answer = "";
+        HashMap<Character, Character> greenFrogDictionary = new HashMap<>();
+
         return answer;
     }
 }
