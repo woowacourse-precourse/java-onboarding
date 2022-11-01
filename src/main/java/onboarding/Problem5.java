cec2c6ca72349586916dd01239a06e7daca2fbb0
James <rlagurwo6209@naver.com>
2022-11-01 16:12:26 +0900

    거스름돈 갯수 반환 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = getChangeMoneyCountList(money);
+        return answer;
+    }
+
+    static List<Integer> getChangeMoneyCountList(int m){
+        List<Integer> ChangeMoneyList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> answer = new ArrayList<Integer>();
+        int money = m;
+        for(int i=0;i<ChangeMoneyList.size();i++){
+            int count = 0;
+            int change = ChangeMoneyList.get(i);
+            count = money/change;
+            answer.add(count);
+            money -= count*change;
+        }
+
         return answer;
     }
 }
