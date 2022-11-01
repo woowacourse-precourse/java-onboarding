fe934f4feee065642921ed37c15180c18d5cb8a8
ohhonggi <5ghdrl@naver.com>
2022-11-01 17:28:27 +0900

    chore: Divide Function List in Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+
     }
+
+    /*
+     * 한 닉네임에 대해 같은 글자가 연속적으로 포함되는 교육생을 찾아 이메일 목록에 추가
+     */
+    public static void detectCrewOfAll(String nickName) {
+
+    }
+
+
+    /*
+     * 주어진 두 닉네임에 대해서 같은 글자가 연속적으로 포함되는지를 검사하여 반환
+     */
+    public static boolean detectConsecutiveLetters(String text, String checkText) {
+        return false;
+    }
+
+    /*
+     * 주어진 닉네임에 대해, 생성될 수 있는 모든 문자열을 생성하여 반환
+     */
+    public static List<String> generateConsecutiveLetters(String nickName) {
+        return Collections.emptyList();
+    }
+
 }
