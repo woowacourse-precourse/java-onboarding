ad7809d932680cd09a5aeadd6a072a91224f4b6b
wxxhyeong <wxxhyeong@gmail.com>
2022-11-01 20:30:58 +0900

    [Problem4] feat : converAlpha() 추가 및 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,32 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        answer = converAlpha(word.toCharArray());
+        return answer;
+    }
+
+    static String converAlpha(char[] arr) {
+
+        String answer = "";
+        char temp;
+
+        for (char c : arr) {
+            if (c == 32) {
+                answer += " ";
+                continue;
+            }
+            if (Character.isUpperCase(c)) {
+                temp = (char)((int)c+25 -2*((int)c-65));
+                answer += Character.toString(temp);
+                continue;
+            }
+            if (Character.isLowerCase(c)){
+                temp = (char)((int)c+25 -2*((int)c-97));
+                answer += Character.toString(temp);
+            }
+        }
+
         return answer;
     }
 }
