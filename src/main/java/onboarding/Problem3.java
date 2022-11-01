a55c4e93df17300831e421da96630619bef8a55a
Woogie95 <choisw950928@gmail.com>
2022-11-01 21:55:24 +0900

    feat: Problem3 solved (3 6 9 게임)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < number; i++) {
+            sb.append(i + 1);
+        }
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '3' || sb.charAt(i) == '6' || sb.charAt(i) == '9') {
+                answer += 1;
+            }
+        }
         return answer;
     }
+
 }
