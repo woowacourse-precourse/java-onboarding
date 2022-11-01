5404bdc047f4dfda76f6137cc9cdd4921af0c2ea
cocomozila <joseyun94@naver.com>
2022-11-01 14:39:43 +0900

    [문제 2] i번째 문자와 i-1번째 문자가 같은지 비교

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,6 +12,13 @@ public class Problem2 {
         boolean[] isReduplication = new boolean[splitCryptogram.length];
         int reduplicationCount = 0;
 
+        // i번째 문자와 i-1번째 문자가 같은지 비교
+        for (int i = 1; i < splitCryptogram.length; i++) {
+            if (splitCryptogram[i] == splitCryptogram[i-1]) {
+                isReduplication[i] = true;
+                isReduplication[i-1] = true;
+            }
+        }
         return "";
     }
 }
