7b5ebabe35f4d31012df014bc14ff32dc8f893fb
tdmdgh <tdmdgh@gmail.com>
2022-11-01 18:59:23 +0900

    problem4_2_소문자를 규칙에 맞게 변환하는 매소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,9 @@ public class Problem4 {
     private static char changeUpper(char x){
         return (char) (x+25-(x-'A')*2);
     }
+    private static char changeLower(char x){
+        return (char) (x+25-(x-'a')*2);
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
