a3cf1a181df08b37722072d136c3c90eda0f7453
Jueun <jueun000101@gmail.com>
2022-11-01 16:08:22 +0900

    feat: 알파벳인지 확인하는 기능 추가
    Character.isAlphabetic()은 א 이런 문자도 true로 반환되기 때문

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,26 +2,27 @@ package onboarding;
 
 /**
  * 기능 목록
- * 1. 알파벳을 변환하는 함수
- * 2. 알파벳이면 변환, 아니면 변환하지 않고 이어붙여 정답 출력
+ * 1. 알파벳인지 확인하는 함수
+ * 2. 알파벳을 변환하는 함수
+ * 3. 알파벳이면 변환, 아니면 변환하지 않고 이어붙여 정답 출력
  */
 public class Problem4 {
     static final char LOWER = 'a';
     static final char UPPER = 'A';
 
+    static boolean isAlphabet(char ch) {
+        return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z';
+    }
+
     static char changeCase(char ch) {
-        char seq;
-        if (Character.isUpperCase(ch))
-            seq = UPPER;
-        else
-            seq = LOWER;
+        char seq = Character.isUpperCase(ch) ? UPPER : LOWER;
         return (char) (seq + Math.abs((ch - seq + 1) - 26));
     }
 
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
         for (char ch: word.toCharArray()) {
-            if (!Character.isAlphabetic(ch)) {
+            if (!isAlphabet(ch)) {
                 answer.append(ch);
                 continue;
             }
