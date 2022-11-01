1ecf5bf00741ee8ee21fb6fc39729b5322a9dda7
i960107 <i960107@naver.com>
2022-11-01 13:25:49 +0900

    feat(problem6): implement feature of get mapping of substring to containing substring
    implemented methods
    - getIdFromEmail
    - get2LengthSubstrings
    - getSubstringToIdMap

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,41 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static final String DOMAIN = "email.com";
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String, Set<String>> substringToId = getSubstringToIdMap(forms);
+
+        return null;
     }
+
+    public static Map<String, Set<String>> getSubstringToIdMap(List<List<String>> forms) {
+        Map<String, Set<String>> substringToId = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String id = form.get(0).substring(0, form.get(0).length() - 9);
+            String nickName = form.get(1);
+
+            get2LengthSubstrings(nickName)
+                    .forEach(s -> substringToId
+                            .computeIfAbsent(s, k -> new HashSet<>())
+                            .add(id));
+        }
+
+        return substringToId;
+    }
+
+
+    public static List<String> get2LengthSubstrings(String nickname) {
+        List<String> substrings = new ArrayList<>();
+
+        for (int i = 2; i < nickname.length() + 1; i++) {
+            substrings.add(nickname.substring(i - 2, i));
+        }
+        return substrings;
+    }
+
+
 }
