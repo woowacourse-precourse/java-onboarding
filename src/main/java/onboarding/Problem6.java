dcf0b1ff00e4c8267fbf66bc37b95155f8107e9d
eom-tae-in <eti0728@daum.net>
2022-11-01 21:15:11 +0900

    feat: static value

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+
+    static List<Boolean> checkDuplication; // LinkedList가 더 좋나?
+    static List<String> str = new ArrayList<>();
+    static List<String> saveEmailForDuplicatedName = new ArrayList<>();
+    static List<String> saveEmailCheckedByEmailRegex = new ArrayList<>();
+    private static final String NAME_REG = "^[가-힣]*$";
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        if (checkExceptionList(forms)) {
+            str.clear();
+            return str;
+        }
+        initializeDuplication(forms);
+        nameDeduplicationMethod(forms);
+        checkEmailRegex();
+        return saveEmailCheckedByEmailRegex;
     }
 }
