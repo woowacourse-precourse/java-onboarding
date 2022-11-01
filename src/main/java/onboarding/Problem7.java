0de021901fe0abb7c0e8bc9a033bf5be1a3effea
SeokHwan An <asok369258@naver.com>
2022-11-01 11:04:23 +0900

    feat [Problem7] : sns 사용자 아이디 모두 소문자 알파벳으로 구성되어 있는지 검증하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,6 +9,7 @@ public class Problem7 {
     private static final int USER_MAX_LENGTH = 30;
     private static final int FRIENDS_MIN_LENGTH = 1;
     private static final int FRIENDS_MAX_LENGTH = 10000;
+    private static final String LOWERCASE_ALPHABET = "^[a-z]*$";
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
@@ -30,6 +31,21 @@ public class Problem7 {
         for (List<String> friend : friends) {
             if (!(USER_MIN_LENGTH <= friend.get(0).length()
                 && friend.get(1).length() <= USER_MAX_LENGTH)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //sns 사용자 아이디 모두가 알파벳 소문자로 구성되어 있는지 검증하는 기능
+    public static boolean checkUserIdConsistOfLowercaseAlphabet(String user,
+        List<List<String>> friends) {
+        if (!user.matches(LOWERCASE_ALPHABET)) {
+            return false;
+        }
+        for (List<String> friend : friends) {
+            if (!(friend.get(0).matches(LOWERCASE_ALPHABET) || friend.get(1)
+                .matches(LOWERCASE_ALPHABET))) {
                 return false;
             }
         }
