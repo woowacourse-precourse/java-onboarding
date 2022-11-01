2a741acf03100cad97321639271d25773eaebccf
gwj0421 <gwj0421@gmail.com>
2022-11-01 17:03:51 +0900

    문제5 - 문제 요구사항 정리 및 해결방법

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    /*
+    - 문제 요구사항 정리 및 해결방법
+    1. 입력된 돈을 최대한 가볍도록 출금하기 위해서 큰 금액의 화폐부터 교환하여 다양한 화폐의 수 리턴
+        - 특정 화폐로 교환했을 때, 특정 화폐 수 얻음
+        - 특정 화폐로 교환했을 때, 나머지 금액을 얻음
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
