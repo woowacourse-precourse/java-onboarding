9abe2c5102bbc4e2d17f4075e5af1bbe581aef97
이경범 <tkdwk567@naver.com>
2022-11-01 16:57:21 +0900

    # 2 solution @@ refactoring -- refactoring

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,47 +15,15 @@ public class Problem2 {
             convertor = new CryptogramConverter(new ProblemTwoValidation(cryptogram));
             convertor.duplicatedWord(cryptogram);
 
-            return convertor.getConverterWord();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            String result = convertor.getConverterWord();
+            convertor.clear();
 
+            return result;
+        } catch (IllegalArgumentException e) {
+
+            System.out.println(e.getMessage());
             return "";
         }
     }
-
-    public static void main(String[] args) {
-        System.out.println(solution("zyelleyz"));
-    }
-
-
-    private static void duplicatedStringRemoveProcess(String validString) {
-        String[] validStringToStringSplit = validString.split("");
-
-        for (String splitData : validStringToStringSplit) {
-
-            if (isDuplicatedString(splitData)) {
-                removeDuplicatedString();
-                continue;
-            }
-
-            stack.add(splitData);
-        }
-    }
-
-    private static boolean isDuplicatedString(String splitData) {
-        return isNotEmpty() && isEquals(splitData);
-    }
-
-    // 너무 잘게 쪼게지는 않는 것이 좋을 거 같다
-    private static void removeDuplicatedString() {
-        stack.pop();
-    }
-
-    private static boolean isNotEmpty() {
-        return !stack.isEmpty();
-    }
-
-    private static boolean isEquals(String splitData) {
-        return stack.peek().equals(splitData);
-    }
 }
+
