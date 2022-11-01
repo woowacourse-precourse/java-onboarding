ff8b2760acd086f54814d800e1182e54d01610e3
wlwlwon <ryanwon@naver.com>
2022-11-01 20:28:58 +0900

    Problem5 메서드 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static int[] change = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    public static void main(String[] args) {
+        System.out.println(solution(15000));
+    }
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        for (int i =0;i<9;i++){
+            answer.add(money/change[i]);
+            money %= change[i];
+        }
         return answer;
     }
 }
