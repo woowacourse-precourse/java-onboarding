84a8b6c8fbdb8b6b9f2fc9159838b947ed953e27
KimSoo <ilppang@naver.com>
2022-11-01 18:12:42 +0900

    Feature(김현수/Problem6): 이름 중복처리 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,23 +1,30 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         try {
-            List<String> answer = List.of("answer");
             List<String> emailList = setInit(forms, 0);
             List<String> nameList = setInit(forms, 1);
 
             verificationEmail(emailList);
+            duplicateName(nameList);
 
-            return answer;
+
         } catch (Exception error) {
-            return (List<String>) error;
+            throw error;
         }
     }
+
+
 
     public static List<String> setInit(List<List<String>> forms, int index) {
         List<String> initlist = new ArrayList<>();
@@ -25,10 +32,10 @@ public class Problem6 {
             initlist.add(form.get(index));
         }
 
-        return  initlist;
+        return initlist;
     }
 
-    public  static void verificationEmail(List<String> forms) {
+    public static void verificationEmail(List<String> forms) {
         String emailPattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@email.com";
 
         for (String form : forms) {
@@ -36,5 +43,42 @@ public class Problem6 {
                 throw new IllegalArgumentException();
             }
         }
+    }
+
+    public static List<String> duplicateName(List<String> forms) {
+        String nameString = "";
+        List<String> duplicateNameList = new ArrayList<>();
+
+        for (String form : forms) {
+            nameString += form + " ";
+        }
+
+        for (String word : forms) {
+            for (int i = 0; i < word.length(); i++) {
+                if (i + 1 < word.length()) {
+                    String duplication = word.substring(i, i + 2);
+                    List<String> checkpattern = checkPattern(duplication, nameString, word);
+                    if (checkpattern.size() > 0) {
+                        duplicateNameList.addAll(checkpattern);
+                    }
+                }
+            }
+        }
+
+        return duplicateNameList.stream().distinct().collect(Collectors.toList());
+    }
+
+    public static List<String> checkPattern(String duplication, String nameString, String word) {
+        List<String> duplicationList = new ArrayList<>();
+        String combinedWord = "[가-힣]*" + duplication + "[가-힣]*\\s{1}";
+        Pattern pattern = Pattern.compile(combinedWord);
+        Matcher matcher = pattern.matcher(nameString);
+
+        while (matcher.find()) {
+            duplicationList.add(matcher.group().trim());
+        }
+        duplicationList.remove(word);
+
+        return duplicationList;
     }
 }
