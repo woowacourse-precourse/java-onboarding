a9294403b4de12af455315701699c80671aa9293
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 17:01:15 +0900

    FEAT mapFragmentedNNEmail

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,8 +4,12 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        HashMap<String, List<String>> map = mapFragmentedNNEmail(forms);
+
         List<String> answer = List.of("answer");
+
         return answer;
+
     }
 
     private static List<String> fragmentNN(String nickName) {
@@ -14,6 +18,21 @@ public class Problem6 {
         for (int i = 0; i < nickName.length() - 1; i++) {
             String subString = nickName.substring(i, i + 2);
             ret.add(subString);
+        }
+
+        return ret;
+    }
+
+    private static HashMap<String, List<String>> mapFragmentedNNEmail(List<List<String>> forms) {
+        HashMap<String, List<String>> ret = new HashMap<>();
+
+        for (List<String> list : forms) {
+            List<String> keys = fragmentNN(list.get(1));
+
+            for (String key : keys) {
+                ret.putIfAbsent(key, new ArrayList<>());
+                ret.get(key).add(list.get(0));
+            }
         }
 
         return ret;
