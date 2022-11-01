b154209fa2e4b9f1ac151133390eaa224457aa70
ckyeon <qsky8520@gmail.com>
2022-11-01 21:12:56 +0900

    Add Form enum to Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,15 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private enum Form {
+        EMAIL(0),
+        NICKNAME(1);
+
+        private final int index;
+
+        Form(int index) {
+            this.index = index;
+        }
+    }
 }
