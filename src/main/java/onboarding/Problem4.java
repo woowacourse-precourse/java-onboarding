8da52c97a93805f81ec7171298595ec19a47b6f9
JohnPrk <uo3641493@gmail.com>
2022-11-01 17:18:46 +0900

    feat : Problems4 프로덕션 코드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,10 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int REVERSE_SIZE= 16;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        ConvertUtils convertUtils = new ConvertUtils(word,REVERSE_SIZE);
+        return convertUtils.convertToReverseWord();
     }
 }
