699e4ee6073f9932bc1b685a76b4758c668c2616
yunjeong <uu0078@naver.com>
2022-11-01 22:15:56 +0900

    Problem1 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/* 기능 목록
+1. 페이지별로 가장 큰 수 구하기
+2. 본임 점수 정하기
+3. 승자 확인
+4. 예외사항 체크
+ */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
