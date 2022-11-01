2ba780ea30b754038780fbcc147caebb0bf501d2
JUHA <khabh@naver.com>
2022-11-01 21:55:51 +0900

    Feature(Problem3) : 전체 박수 횟수 구하는 기능 구현(getTotalClap)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,33 @@ public class Problem3 {
     }
 
 	private static class ThreeSixNineGame {
-		ThreeSixNineGame() {}
+		private final static int DEFAULT_DIGIT_UNIT = 1;
+		private final static int DEFAULT_MAXIMUM_CLAPPING = 0;
+		private final static int UNIT_CONVERT_CRITERIA = 10;
+		private int currentDigitUnit;
+		private int previousUnitMaxClap;
+		
+		ThreeSixNineGame() {
+			this.currentDigitUnit = DEFAULT_DIGIT_UNIT;
+			this.previousUnitMaxClap = DEFAULT_MAXIMUM_CLAPPING;
+		}
+
+		int getTotalClap(int number) {
+			int currentDigit;
+			int previousNumberSum = 0;
+			int totalClaps = 0;
+
+			while (number > 0) {
+				currentDigit = number % 10;
+				totalClaps += getCurrentTotal(currentDigit, currentDigitUnit, previousUnitMaxClap, previousNumberSum);
+				previousUnitMaxClap += getCurrentTotal(9, currentDigitUnit, previousUnitMaxClap, currentDigitUnit - 1);
+				previousNumberSum += currentDigit * currentDigitUnit;
+				number /= UNIT_CONVERT_CRITERIA;
+				currentDigitUnit *= UNIT_CONVERT_CRITERIA;
+			}
+
+			return totalClaps;
+		}
 
 		public static int getCurrentTotal(int currentNumber,int currentUnit, int beforeUnitTotal, int preDigit) {
 			return (currentNumber) * beforeUnitTotal + ((currentNumber - 1) / 3) * currentUnit + (currentNumber % 3 == 0 ? (preDigit + 1) : 0 );
