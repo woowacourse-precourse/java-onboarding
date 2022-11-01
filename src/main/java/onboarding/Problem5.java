c14930018cf37cc389a8cbb22c57d9ead9fbde3a
villya <sunwooye@gmail.com>
2022-11-01 23:24:12 +0900

    prob 1 ~ 5 done code prob 6 next

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,50 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
+        int count = 0;
+        int tmp = money;
+
+        count = tmp / 50000;
+        answer.add(count);
+        tmp -= tmp/50000 * 50000;
+
+        count = tmp / 10000;
+        answer.add(count);
+        tmp -= tmp/10000 * 10000;
+
+        count = tmp / 5000;
+        answer.add(count);
+        tmp -= tmp/5000 * 5000;
+
+        count = tmp / 1000;
+        answer.add(count);
+        tmp -= tmp/1000 * 1000;
+
+        count = tmp / 500;
+        answer.add(count);
+        tmp -= tmp/500 * 500;
+
+        count = tmp / 100;
+        answer.add(count);
+        tmp -= tmp/100 * 100;
+
+        count = tmp / 50;
+        answer.add(count);
+        tmp -= tmp/50 * 50;
+
+        count = tmp / 10;
+        answer.add(count);
+        tmp -= tmp/10 * 10;
+
+        count = tmp;
+        answer.add(count);
+
         return answer;
     }
 }
