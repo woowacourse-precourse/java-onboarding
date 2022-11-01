92ea599133b6057830327186f7097ddccd7d7045
Konahyeon <koo@inha.edu>
2022-11-01 22:14:51 +0900

    추천 점수 매기기 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,6 +16,12 @@ class User {
     public String getName() {
         return this.name;
     }
+
+    public int scoreAdd(int score) {
+        this.score += score;
+        return score;
+    }
+
     public List<String> getFriendList() {
         return this.friendList;
     }
@@ -26,6 +32,8 @@ class User {
 }
 
 public class Problem7 {
+    public static final int FRIEND_SCORE = 10;
+    public static final int VISITOR_SCORE = 1;
     public static final HashMap<String, User> classList = new HashMap<>(); //이름과 그 이름에 해당하는 클래스 보관
     public static void makeClass(String name) {
         if (!classList.containsKey(name)) { //class hashmap에 없을 경우 클래스 먼저 만듦
@@ -53,16 +61,38 @@ public class Problem7 {
         }
     }
 
-    public static void friendScore() {
-        
+    public static void friendScore(String name) {
+        User user = findClass(name); //user 클래스 가져오기
+        for (int i = 0; i < user.getFriendList().size(); i++) {
+            User friendUser = classList.get(user.getFriendList().get(i));
+            for (int j = 0; j < friendUser.getFriendList().size(); j++) {
+                if (!user.getFriendList().contains(friendUser.getFriendList().get(j))) //friendUser의 친구가 user 친구 목록에도 없는 친구라면
+                    classList.get(friendUser.getFriendList().get(j)).scoreAdd(FRIEND_SCORE);
+            }
+        }
     }
-    public static void visitorScore() {
-
+    public static void visitorScore(List<String> visitors) {
+        for (int i = 0; i < visitors.size(); i++) {
+            User user = classList.get(visitors.get(i));
+            user.scoreAdd(VISITOR_SCORE);
+        }
     }
-    public static List<Integer> scoreManage() {
+    public static void scoreManage(String user, List<String> visitors) {
+        friendScore(user);
+        visitorScore(visitors);
+    }
 
-        friendScore();
-        visitorScore();
+    public static List<String> selectFriend() {
+        List<String> answer = new ArrayList<>();
+
+/*        int scoreArr[]; //점수 집어넣기
+        for (int i = 0; i < classList.size(); i++) {
+            classList.
+        }*/
+
+
+
+        return answer;
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -72,8 +102,10 @@ public class Problem7 {
         makeFriend(friends);
 
         //2. scoreManage - 추천 점수 매기기
+        scoreManage(user, visitors);
 
         //3. 추천 점수 상위 5명 담긴 리스트 리턴받기
+        selectFriend();
 
         return answer;
     }
