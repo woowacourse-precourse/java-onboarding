5fe78108b36aac533237123061aba9a25edb53c2
rvlwldev <rvlwlasd77@gmail.com>
2022-11-01 14:38:51 +0900

    문제7 : 사용자의 친구목록을 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
@@ -13,6 +14,25 @@ public class Problem7 {
      * 추천 친구 목록을 구하기 위한 기능들을 구현한 클래스
      */
     public static class Problem7_Functions {
+        /**
+         * 사용자의 모든 친구목록을 반환한다.
+         * 
+         * @param user    친구를 추천받을 사용자 아이디
+         * @param friends 친구관계정보
+         * @return {@code List<String>}사용자의 친구목록
+         */
+        public List<String> getUsersFriendsList(String user, List<List<String>> friends) {
 
+            HashSet<String> res = new HashSet<>();
+
+            for (List<String> relation : friends) {
+                String friend = relation.get(0);
+                res.add(friend);
+            }
+
+            res.remove(user);
+
+            return List.copyOf(res);
+        }
     }
 }
