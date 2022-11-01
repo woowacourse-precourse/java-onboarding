6b1bc1de3b3c073d3426cb095a047dba60d31db0
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-01 23:28:34 +0900

    [feat] Problem1 포비, 크롱의 각자 최댓값 계산

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -21,6 +21,32 @@ class Problem1 {
                 pobi.get(0) % 2 != 1 ||
                 crong.get(0) % 2 != 1)
             answer = -1;
+        else {
+            int pobiMax = 0;
+            int crongMax = 0;
+            for (int i = 0; i < 2; i++) {
+                int pobiPlus = 0;
+                int pobiMulti = 1;
+                int pobiPage = pobi.get(i);
+                while (pobiPage != 0) {
+                    pobiPlus += pobiPage % 10;
+                    pobiMulti *= pobiPage % 10;
+                    pobiPage /= 10;
+                }
+                pobiMax = Math.max(pobiMax, Math.max(pobiPlus, pobiMulti));
+
+                int crongPlus = 0;
+                int crongMulti = 1;
+                int crongPage = crong.get(i);
+                while (crongPage != 0) {
+                    crongPlus += crongPage % 10;
+                    crongMulti *= crongPage % 10;
+                    crongPage /= 10;
+                }
+                crongMax = Math.max(crongMax, Math.max(crongPlus, crongMulti));
+            }
+        }
+
         return answer;
     }
 }
