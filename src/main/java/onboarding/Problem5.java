472c40beb8213b8b24aee78916389233625a06e4
xxeol2 <jhj01177@naver.com>
2022-11-01 14:52:48 +0900

    refactor(prob5): 정적 중첩 클래스 도입

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,17 +5,22 @@ import java.util.List;
 
 public class Problem5 {
 	public static List<Integer> solution(int money) {
-		List<Integer> answer = divideMoney(money);
+		MoneyDivider moneyDivider = new MoneyDivider();
+		List<Integer> answer = moneyDivider.divideMoney(money);
 		return answer;
 	}
 
-	public static List<Integer> divideMoney(int money) {
+	static class MoneyDivider {
 		List<Integer> units = List.of(50_000, 10_000, 5000, 1000, 500, 100, 50, 10, 1);
-		List<Integer> result = new ArrayList<Integer>();
-		for (int unit : units) {
-			result.add(money / unit);
-			money %= unit;
+
+		public List<Integer> divideMoney(int money) {
+			List<Integer> result = new ArrayList<>();
+			for (int unit : units) {
+				result.add(money / unit);
+				money %= unit;
+			}
+			return result;
 		}
-		return result;
 	}
+
 }
