0ac9224bda7a1eacc198947207a18170ad5c197a
ChaCha3088 <cha3088@gmail.com>
2022-11-01 21:32:43 +0900

    style: problem4 final

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,27 +10,43 @@ public class Problem4 {
     public static String solution(String word) {
         List<String> wordList = stringToWordList(word);
         List<String> resultList = new ArrayList<String>();
+
+        // 글자를 변환하고 resultList에 넣는다.
         for (String e : wordList) {
             resultList.add(convertLetter(e));
         }
-        String result = String.join("", resultList);
-        return result;
+
+        // 리스트를 문자열로 연결한다.
+        return String.join("", resultList);
     }
 
+    /**
+     * 문자열을 문자 리스트로 나눠 담는다.
+     * @param word 주어진 단어
+     * @return 문자 리스트
+     */
     private static List<String> stringToWordList(String word) {
         String[] items = word.split ("");
-        ArrayList<String> wordList = new ArrayList<> (Arrays.asList(items));
-        return wordList;
+        return new ArrayList<>(Arrays.asList(items));
     }
 
+    /**
+     * 알파벳 외의 문자는 변환하지 않는다.
+     * @param letter 글자
+     * @return 참 거짓
+     */
     private static Boolean isAlphabet(String letter) {
         if(Pattern.matches("^[a-zA-Z]*$", letter)) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
+    /**
+     * 알파벳 외의 문자가 아니라면 청개구리 사전을 이용해 문자를 변환한다.
+     * @param letter 글자
+     * @return 변환된 글자
+     */
     private static String convertLetter(String letter) {
         String result = letter;
         if (isAlphabet(letter)) {
@@ -39,6 +55,9 @@ public class Problem4 {
         return result;
     }
 
+    /**
+     * 청개구리 사전을 Map에 담았다.
+     */
     private static Map<String, String> frogsDictionary = Map.ofEntries(
             Map.entry("A", "Z"),
             Map.entry("B", "Y"),
