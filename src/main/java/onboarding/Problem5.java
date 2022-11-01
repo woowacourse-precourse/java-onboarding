c49243b62703096caa89de909ff2db3a045fb1aa
Timothy <toydiary@kakao.com>
2022-11-01 22:34:23 +0900

    feat(문제 5) 금액이 큰 순서대로 지폐와 동전으로 바꿔서 지갑을 가볍게 만들기
    1. 입력 값에 대한 예외처리
     - 입력된 금액의 범위 체크 : 1 이상, 1,000,000 이하인 자연수
    2. 돈의 액수 money가 매개변수로 주어질 때, 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 각 몇 개로 변환되는지 금액이 큰 순서대로 리스트/배열에 담아 return.
    - 지폐 단위 및 동전 단위를 담은 배열 생성
    - 입력받은 금액에서 지폐 및 동전 단위를 기준으로 Moth.floorDiv()로 몫을 구하고 List<Integer> answer에 추가하기
    - 입력받은 금액에서 구한 몫 X 현재 지폐 단위를 제외 하고 다시 나머지 Moth.floorDiv()로 몫을 구하기를 반복하기
    - 금액이 큰 순서대로 지폐와 동전으로 바꾼 결과를 반환하기
    
    Closes #9

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,15 @@
 package onboarding;
 
+import onboarding.problem5.SolveMoneyChange;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        SolveMoneyChange solveMoneyChange = new SolveMoneyChange();
         List<Integer> answer = Collections.emptyList();
+        answer = solveMoneyChange.changeMoney(money);
         return answer;
     }
 }
