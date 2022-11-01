4c8ca42a6415c6301b0c1a20c1d89b17f254ba13
kingseungwooya <ksw_0505@nate.com>
2022-11-01 17:27:17 +0900

    comment: add coment
    코드 설명 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,9 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
+/**
+ * 사용자를 정의하기 위한 Class
+ */
 class User {
     private String name;
     private Set<String> friends;
@@ -31,19 +34,23 @@ public class Problem7 {
     private static Map<String, Integer> recommendedFriend = new HashMap<>();
     private static User member;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-
+        // 사용자의 id
         member = new User(user);
+        // 친구관계 중 사용자가 포함되어 있는 리스트를 filter
         List<List<String>> userFriends = friends.stream()
                 .filter( s -> s.contains(user))
                 .collect(Collectors.toList());
+        // 사용자의 친구 목록에 추가
         setFriends(userFriends);
-
+        // 친구관계 중 사용자가 포함되어 있지 않은 리스트들 filter -> 사용자의 친구의 친구 즉 사용자와는 아직 친구가 아니다.
         List<List<String>> notUserFriends = friends.stream()
                 .filter( s -> !s.contains(user))
                 .collect(Collectors.toList());
+        // 친구의 친구들의 score을 매긴다.
         scoredFriendOfFriends(notUserFriends);
+        // 방문자들의 score을 매긴다.
         scoredVisitors(visitors);
-
+        // point순으로 정렬한 후 출력
         List<String> answer = recommendedFriend.entrySet().stream()
                 .sorted(Comparator.comparing(Entry::getValue , Comparator.reverseOrder()))
                 .map(s -> s.getKey())
@@ -51,6 +58,11 @@ public class Problem7 {
 
         return answer;
     }
+
+    /**
+     * 사용자의 친구목록을 기재한다. 이 때 친구관계중 [ 사용자, 친구1 ], [ 친구2, 사용자] 이런 경우를 방지하기 위해
+     * 조건문 처리를 해준 후 친구1, 친구2를 사용자의 친구 목록에 추가한다.
+     */
 
     private static void setFriends(List<List<String>> userFriends){
         for(List<String> friends : userFriends){
@@ -60,10 +72,12 @@ public class Problem7 {
             if(friends.get(1) != member.getName()){
                 member.addFriend(friends.get(1));
             }
-
         }
-
     }
+
+    /**
+     * 사용자 친구의 친구들의 점수를 매긴다.
+     */
 
     private static void scoredFriendOfFriends(List<List<String>> friends){
         for(List<String> friend : friends){
