51402670578a7d00ba4065d2540b441bad696dcb
20mini <dlqslek10@gmail.com>
2022-11-01 11:55:56 +0900

    Update Problem2~6

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        Stack<Character> stack=new Stack<>();
+        stack.push(cryptogram.charAt(0));
+        Boolean overlap=false; // 중복 되고 있는 상황인지 확인하는 변수
+        // cryptogram의 앞에서부터 한 글자씩 확인
+        for(int i=1; i<cryptogram.length(); i++){
+            if(!overlap){ // 중복되고 있는 상황이 아닐 경우
+                if(!stack.isEmpty() && cryptogram.charAt(i)==stack.peek()) // 확인하는 문자가 이전 문자랑 같을 경우 -> 중복 시작
+                {
+                    if(i==cryptogram.length()-1) // 확인하는 문자가 마지막일 경우
+                        stack.pop(); // 중복 시작 문자 스택에서 제거
+                    overlap=true; // 중복 시작
+                }
+                else { // 중복 시작되는 경우가 아닐 경우
+                    stack.push(cryptogram.charAt(i)); // 스택에 해당 문자를 넣어준다.
+                }
+            }
+            else{ // 중복되고 있는 상황일 경우
+                if(cryptogram.charAt(i)!=stack.peek()){ // 확인하는 문자가 중복 문자랑 다를 경우
+                    stack.pop(); // 중복 시작 문자 스택에서 제거
+                    overlap=false; // 중복 끝
+                    i--; // 해당 문자를 다시 확인하기 위해 i를 1 빼준다.
+                }
+                else if (i==cryptogram.length()-1) { // 마지막 문자까지 중복일 경우
+                    stack.pop(); // 중복 시작 문자 스택에서 제거
+                }
+            }
+        }
+        String answer = "";
+        while(!stack.isEmpty())
+            answer= stack.pop()+answer; // 스택에 있는 문자를 꺼내서 역순으로 합친 것이 정답
         return answer;
     }
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Set<String> answer_set = new HashSet<>(); // 정답 set
+        Map<String, Set<String>> nick_words=new HashMap<>(); // (이메일: 닉네임을 두 글자씩 쪼갠 단어들의 집합) 형태의 Map
+        for(List<String> list:forms){
+            Set<String> words=new HashSet<>();
+            for(int j=0; j<=list.get(1).length()-2; j++){
+                words.add(list.get(1).substring(j,j+2));
+            }
+            nick_words.put(list.get(0),words);
+        }
+        for(String i:nick_words.keySet()){
+            for(String j:nick_words.keySet()){
+                if(!i.equals(j)){
+                    HashSet<String> intersection = new HashSet<>(nick_words.get(i)); // intersection은 두 (닉네임을 두 글자씩 쪼갠)집합간의 교집합
+                    intersection.retainAll(nick_words.get(j));
+                    if(intersection.size()>0) // 교집합이 존재하면 두 닉네임간 같은 글자가 연속적으로 포함된다는 것이다.
+                        answer_set.add(i);
+                }
+            }
+        }
+        List<String> answer=new ArrayList<>(answer_set);
+        answer.sort(Comparator.naturalOrder());
         return answer;
     }
 }
