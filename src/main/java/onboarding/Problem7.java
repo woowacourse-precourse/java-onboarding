02dbe0450c89ae3adb1412a983585e17edf4a335
broden-horizon <rlghd1698@naver.com>
2022-11-01 23:13:13 +0900

    문제7. 친구 관계를 인접리스트로 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,4 +16,34 @@ public class Problem7 {
         //최종 제출할 추천 친구 이름을 기록할 List
         List<String> recommendedFriends = new ArrayList<>();
     }
+
+    private static int recordFriendNetwork(List<List<String>> friends, String[] userNameListWithIndex,
+                                           Map<String, Integer> userNameMap, List<List<Integer>> friendsNetwork) {
+        int index = 0;
+        for(List<String> relation: friends) {
+            String userA = relation.get(0);
+            String userB = relation.get(1);
+
+            //이미 유저 정보를 저장했는지 확인하여, 저장안했다면 생성, 생성했다면 index 하나 증가
+            index += createUser(userA, userNameListWithIndex, userNameMap, friendsNetwork, index);
+            index += createUser(userB, userNameListWithIndex, userNameMap, friendsNetwork, index);
+
+            //둘의 관계를 추가
+            int indexOfUserA = userNameMap.get(userA);
+            int indexOfUserB = userNameMap.get(userB);
+            friendsNetwork.get(indexOfUserA).add(indexOfUserB);
+            friendsNetwork.get(indexOfUserB).add(indexOfUserA);
+        }
+
+        return index;
+    }
+
+    private static int createUser(String user, String[] userNameListWithIndex, Map<String, Integer> userNameSet,
+                                  List<List<Integer>> friendsNetwork, int index) {
+        if(userNameSet.containsKey(user)) return 0;
+        userNameListWithIndex[index] = user;
+        userNameSet.put(user, index);
+        friendsNetwork.add(new ArrayList<Integer>());
+        return 1;
+    }
 }
