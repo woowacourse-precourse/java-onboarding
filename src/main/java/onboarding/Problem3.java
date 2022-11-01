950a0a3a37b84c36047141a928cb55e7c7ed8b0d
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-01 20:59:51 +0900

    Update Problem3.java

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+		int answer = 0;
+
+		int[] arr = new int[number + 1];
+		for (int i = 1; i <= number; i++) {
+			int num = i;
+			while (num > 0) {
+				if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+					arr[num] += 1;
+					num = (int) (num / 10);
+				} else {
+					num = (int) (num / 10);
+				}
+			}
+		}
+		for (int i = 1; i <= number; i++) {
+			answer += arr[i];
+		}
+		return answer;
+	}
 }
