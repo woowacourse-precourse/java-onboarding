7dd09abf214a1f9681f18bfd963a5741cf59bb3b
BoyCho <iijokv@naver.com>
2022-11-01 10:55:54 +0900

    feat : 문제1. 모두 더한 합, 모두 곱한 합 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,9 +10,40 @@ class Problem1 {
         if (isException(p1, p2)) return -1;
         if (isException(c1, c2)) return -1;
 
+        int p = Math.max(getAddedSum(p1, p2), getMultipliedSum(p1, p2));
+        int c = Math.max(getAddedSum(c1, c2), getMultipliedSum(c1, c2));
+
+
         return 0;
     }
     static boolean isException (int n, int m) {
         return m - n != 1;
+    }
+
+    static int getAddedSum (int n, int m){
+        int sum1 = 0, sum2 = 0;
+
+        while(n > 0 || m > 0){
+            sum1 += n % 10;
+            sum2 += m % 10;
+            n /= 10;
+            m /= 10;
+        }
+        return Math.max(sum1, sum2);
+    }
+
+    static int getMultipliedSum (int n, int m){
+        int mul1 = 1, mul2 = 1;
+
+        while(n > 0 || m > 0){
+            if(isOverZero(n)) mul1 *= n % 10;
+            if(isOverZero(m)) mul2 *= m % 10;
+            n /= 10;
+            m /= 10;
+        }
+        return Math.max(mul1, mul2);
+    }
+    static boolean isOverZero (int n) {
+        return n > 0;
     }
 }
