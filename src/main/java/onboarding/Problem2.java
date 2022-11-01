c4251669f64d76f22afc5a83abdf8fe8d4a288ba
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-01 15:24:37 +0900

    문제 2번 <1번 수정사항 추가>

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,36 +1,33 @@
 package onboarding;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "";
-        String[] answers = cryptogram.split(""); // 각 문자를 비교, 확인하기 위해 쪼개어 배열에 담아준다.
+        //매개변수를 담아줄 String과 철자를 쪼개어 담은 cryptogramList생성
+        String answer = cryptogram;
+        List<String> cryptogramList = new java.util.ArrayList<>(List.of(cryptogram.split("")));
 
-        //인접한 문자열이 서로 같다면 빈 값을 넣어주고, 다르다면 앞의 글자를 넣어준다.
-        for (int i = 1; i < cryptogram.length(); i++) {
-            if (answers[i-1].equals(answers[i])) {
-                answers[i-1] = "";
-                answers[i] = "";
-                answer += "";
-            }
-            if (!answers[i-1].equals(answers[i])) {
-                answer += answers[i-1];
-            }
-            //적어준 조건상, 맨 마지막 글자는 잘리기 때문에 이를 붙여준다.
-            if (i == cryptogram.length()-1) {
-                answer += answers[i];
-            }
-        }
-
-        // 위 과정을 걸쳐 나온 문자를 재귀적으로 함수를 호출해 반복하여 확인해본다.
-        String[] results = answer.split("");
-        if (answer.length() == 2 && results[0].equals(results[1])) {
-            answer = "";
-        }
-        for (int i = 1; i < answer.length()-1; i++) {
-            if (results[i-1].equals(results[i])) {
-                answer = solution(answer);
-            }
+        //Pattern과 Matcher를 이용하여 연속된 문자를 제거하는 메서드를 만들고 호출
+        for (int i = 0; i < cryptogram.length(); i++) {
+            String regex = cryptogramList.get(i);
+            answer = extracted(answer,regex);
         }
         return answer;
+    }
+
+    //연속된 문자를 제거하는 메서드
+    private static String extracted(String cryptogram,String regexPattern) {
+        String regex = regexPattern+"{2,}";
+        String word = cryptogram;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(word);
+        if (matcher.find()) {
+            word = word.replaceAll(regex, "").trim();
+        }
+        System.out.println("확인용 " + word);
+        return word;
     }
 }
