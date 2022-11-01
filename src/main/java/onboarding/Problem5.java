d59b02fa99d4b43b927d6b77b190c2ed8c7985fb
Kayoung <rkdkdudjd@gmail.com>
2022-11-01 14:57:54 +0900

    fix(problem5): 현금 단위를 멤버변수로 선언

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    public static final List<Integer> cashUnit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        List<Integer> unitList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
-        for (int unit: unitList) {
+        for (int unit : cashUnit) {
             answer.add(money / unit);
             money %= unit;
         }
