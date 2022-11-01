0f023442ce8c488bbc45f6b0942228138912dca8
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 17:23:41 +0900

    포비나 크롱의 페이지쌍을 입력받고 유효하다면 true 아니라면 false를 리턴한다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,6 +12,17 @@ class Problem1 {
     private static boolean isPair(int leftPage, int rightPage) {
         return rightPage - leftPage == 1;
     }
+    private static boolean isValidPage(List<Integer> pages) {
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+        if (!(isInRange(leftPage) && isInRange(rightPage))) {
+            return false;
+        }
+        if (isEven(leftPage) || !isEven(rightPage)) {
+            return false;
+        }
+        return isPair(leftPage, rightPage);
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
