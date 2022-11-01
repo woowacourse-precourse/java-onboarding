58a0b6c7bbd4cdd3e3b2b71c327414768753f947
ghchoi <ghchoi0427@gmail.com>
2022-11-01 15:18:29 +0900

    feat: 두글자씩 끊어서 리스트로 반환하는 메서드 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Problem6 p6 = new Problem6();
         return answer;
+    }
+
+    private List<String> stringTokens(String word) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < word.length() - 1; i++) {
+            String temp = Character.toString(word.charAt(i)) + Character.toString(word.charAt(i + 1));
+            result.add(temp);
+        }
+        return result;
     }
 }
