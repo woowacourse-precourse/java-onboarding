cf97649f0943e6b272d0308f96cabcfd8bd19a1c
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 17:43:50 +0900

    스택에 남은 문자들을 문자열로 재구성해 리턴한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,6 +10,13 @@ public class Problem2 {
         }
         decodeStack.push(letter);
     }
+    private static String stackToString(Stack<Character> decodeStack) {
+        String answer = "";
+        while (!decodeStack.isEmpty()) {
+            answer = decodeStack.pop() + answer;
+        }
+        return answer;
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
