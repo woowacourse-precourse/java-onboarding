ccb9f4196ad4e6196b8ef1d821b252d081b104fd
kwx4957 <kdy4957@naver.com>
2022-11-01 23:28:17 +0900

    1번 문제 기능 구현 및 풀이 4. 페이지 차이 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,7 @@ import java.util.List;
 1. 페이지 번호 더하기
 2. 페이지 번호 값 곱하기
 3. 더한 값 과 곱한 값 비교하기
+4. 페이지 차이 확인
  */
 class Problem1 {
     public static int add(List<Integer> value){
@@ -27,10 +28,11 @@ class Problem1 {
         for(int i:value){
             int tmp = 1;
 
-            for(int j=0;j<3;j++){
+            while (i!=0){
                 tmp *= (i % 10);
                 i = i / 10;
             }
+
             max = Math.max(max, tmp);
         }
 
@@ -38,12 +40,32 @@ class Problem1 {
     }
 
     public static int compareValue(List<Integer> com){
-        int answer = Math.max(add(com),multiple(com));
-        return answer;
+        return Math.max(add(com),multiple(com));
 
     }
+    public static boolean isVaild(List<Integer> value){
+        return value.get(0) + 1 == value.get(1);
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int pobipage = compareValue(pobi);
+        int crongpage = compareValue(crong);
         int answer = Integer.MAX_VALUE;
+
+        if(!isVaild(pobi) || !isVaild(crong)){
+            answer = -1;
+            return answer;
+        }
+
+        if (pobipage > crongpage){
+            answer = 1;
+        }
+        if (pobipage == crongpage) {
+            answer = 0;
+        }
+        if (pobipage < crongpage){
+            answer = 2;
+        }
+
         return answer;
     }
 }
