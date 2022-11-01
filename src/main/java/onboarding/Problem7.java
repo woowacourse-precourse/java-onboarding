c5fd6d426f45402f5d0be3ffdaac02482b5aecaa
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-01 21:32:20 +0900

    makeFriendsMap

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,25 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
-
+import java.util.*;
 public class Problem7 {
+
+    static Map<String, ArrayList> friendMap = new TreeMap<>();
+    static Map<String, Integer> mapPoints = new TreeMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void makeFriendsMap(List<String> friendsList) {
+
+        ArrayList list1 = friendMap.getOrDefault(friendsList.get(0), new ArrayList());
+        ArrayList list2 = friendMap.getOrDefault(friendsList.get(1), new ArrayList());
+
+        list1.add(friendsList.get(1));
+        list2.add(friendsList.get(0));
+
+        friendMap.put(friendsList.get(0), list1);
+        friendMap.put(friendsList.get(1), list2);
     }
 }
