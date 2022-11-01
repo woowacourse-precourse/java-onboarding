4cd191f2ca551d0fbf16e0ed8a47726fddd672be
kkh <kkho9654@naver.com>
2022-11-01 20:49:11 +0900

    Add feature list problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+/**
+ * 1. 문자열의 첫 문자 부터 끝 문자 까지 반복문 실행
+ * 2. 문자가 소문자이면 z부터 해당 문자의 차이를 'a'혹은 'A'문자에 더해서 원래 문자를 구한다.
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
