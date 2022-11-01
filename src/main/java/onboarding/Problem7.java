650fda8ea8a4657bb6134a82e9bd5842cde1bb5f
mihea1206 <mihea12@gmail.com>
2022-11-01 09:42:16 +0900

    problem7(map update function)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+    private static void appendMapValue(String key, String appendValue,
+        HashMap<String, List<String>> map) {
+        List<String> values = map.getOrDefault(key, new ArrayList<>());
+        values.add(appendValue);
+        map.put(key, values);
+    }
+
+    private static void updateMapValue(String key, int addScore, HashMap<String, Integer> map) {
+        map.put(key, map.getOrDefault(key, 0) + addScore);
+    }
+
+    private static void updateMapAll(List<String> keys, int addScore,
+        HashMap<String, Integer> map) {
+        keys.forEach(key -> updateMapValue(key, addScore, map));
+    }
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
