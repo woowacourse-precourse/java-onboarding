7b26dfcb3f2253fc75c70246604b9d69a9e9c1d6
ingpyo <juninpyo72@gmail.com>
2022-11-01 12:39:47 +0900

    [problem7]user의 친구 추출 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+		List<String> userfriends = new ArrayList<>();
+		Map<String, Integer> map = new HashMap<>();
+		List<String> answer = new ArrayList<>();
+		for (List<String> list : friends) {
+			String friend1 = list.get(0), friend2 = list.get(1);
+			if (friend1 == user) {
+				userfriends.add(friend2);
+			}
+			if (friend2 == user) {
+				userfriends.add(friend1);
+			}
+		}
+
+	}
+
+
 }
