a7ac2e0d4905ce0f9fb22978c81df57ca54e4761
gitFILO <x1xgudwls@naver.com>
2022-11-01 23:16:19 +0900

    [Problem 7] user와 친구인 사람의 친구에게 10점을 주는 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,6 +9,10 @@ public class Problem7 {
         List<String> answer = new ArrayList<>();
 
         findFriendsOfUser(friends, friendsOfUser, user); // user의 친구 찾기
+        scoreFriendsOfFriendsOfUser(friends, friendsOfUser, scoreMap);//찾은 user의 친구와 친구인 사람에게 10점 부여
+        scoreVisitors(visitors, scoreMap); // visitors에게 1점 부여
+        scoreFriendsOfUser(user,friendsOfUser,scoreMap);
+        answer = sortedList(scoreMap);
         return answer;
     }
 
@@ -24,4 +28,22 @@ public class Problem7 {
         }
     }
 
+    public static void scoreFriendsOfFriendsOfUser(List<List<String>> friends, ArrayList<String> friendsOfUser, HashMap<String, Integer> scoreMap) {
+        String firstPerson;
+        String secondPerson;
+        for (int i = 0; i < friends.size(); i++) {
+            firstPerson = friends.get(i).get(0);
+            secondPerson = friends.get(i).get(1);
+            friendsOrNot(friendsOfUser, firstPerson, secondPerson, scoreMap);
+        }
+    }
+    public static void friendsOrNot(ArrayList<String> friendsOfUser, String firstPerson, String secondPerson, HashMap<String, Integer> scoreMap) {
+        if (friendsOfUser.contains(firstPerson)) updateScore(scoreMap, secondPerson, 10); // firstPerson이 유저의 친구일 때
+        else if (friendsOfUser.contains(secondPerson)) updateScore(scoreMap, firstPerson, 10);
+    }
+
+    public static void updateScore(HashMap<String, Integer> scoreMap, String person, int addedScore) {
+        if (scoreMap.containsKey(person)) scoreMap.replace(person, scoreMap.get(person) + addedScore);
+        else scoreMap.put(person, addedScore);
+    }
 }
