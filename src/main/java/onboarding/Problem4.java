777b36760aff1e5a2792f4617df61199768d334e
core <jinjs6@gmail.com>
2022-11-01 18:24:53 +0900

    Problem4 소문자 문자열 및 대문자 문자열 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,8 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         return answer;
     }
 }
