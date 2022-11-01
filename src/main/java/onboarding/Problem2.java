2fff544e9754b95651e9289ccbae27f567786d16
jihoon-gh <jihoon806@gmail.com>
2022-11-01 15:46:30 +0900

    FEAT Problem2.java - Solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
-
+/*
+중복 문자 삭제
+1. String.charAt(i)을 이용해 i의 문자가 i+1과 같은지 검사
+2. 같다면 지워주고 ( substring 이용) i값 갱신
+3. 반복
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        int i = 0;
+        while(i<cryptogram.length()-1){
+            if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+                cryptogram=cryptogram.substring(0,i)+cryptogram.substring(i+2);
+                System.out.println("cryptogram = " + cryptogram);
+                i=i-1;
+                continue;
+            }
+            i++;
+        }
+        String answer = cryptogram;
         return answer;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, String> m = new HashMap<>();
+        List<List<String>> friendsOfUser= friends.stream().filter(f -> f.get(0)==user || f.get(1)==user).collect(Collectors.toList());
+        for(List<String> f :friendsOfUser){
+            if(f.get(0)==user) m.put(user,f.get(1));
+        }
         return answer;
+    }
+
+    public static boolean isUser(String s,String user){
+        if(user==s) return true;
+        return false;
+
     }
 }
