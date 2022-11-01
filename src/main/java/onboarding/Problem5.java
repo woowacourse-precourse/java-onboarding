66824f6088b9c874f77e40e81aa0205f4039b27b
WonJeongYeon <wonjungyoun@naver.com>
2022-11-01 10:12:06 +0900

    20221027 Problem5.java 예외처리(제한사항) 적용 및 기초공사 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,13 +8,24 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>(Collections.emptyList());
         int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        money = checkMoney(money);
         int currencyCount;
         int currentMoney = money;
+        //유지보수 측면에서 기존 money 변수는 갱신하지 않음
         for (int i = 0; i < currency.length; i++) {
             currencyCount = currentMoney / currency[i];
             answer.add(i, currencyCount);
             currentMoney = currentMoney % currency[i];
         }
         return answer;
+    }
+
+    public static Integer checkMoney(int money) {
+        if (money < 1 || money > 1_000_000) {
+            throw new IllegalStateException("돈은 1원 이상, 100만원 이하여야 합니다.");
+            //money는 1 이상 1,000,000 이하인 자연수이다.
+        } else {
+            return money;
+        }
     }
 }
