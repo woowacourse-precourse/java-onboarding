ff1097185d8ba280bf5e19d9b5ae1d40bf4fdfe5
lhyeonj120 <isk04387@gmail.com>
2022-11-01 14:19:31 +0900

    [problem6] feat: 닉네임에서 두 글자 문자열 생성하여 해당되는 크루들의 이메일 저장 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    
+    private static Map<String, List<String>> getOverlapMap(List<List<String>> list){
+    	Map<String, List<String>> overlapMap = new HashMap<>();
+    	
+    	for(int i = 0; i < list.size(); i++) {
+    		for(int j = 0; j < list.get(i).get(1).length() - 1; j++) {
+    			String s = list.get(i).get(1).substring(j, j + 2);
+    			if(!overlapMap.containsKey(s)) {
+    				overlapMap.put(s, new LinkedList<>());
+    			}
+    			overlapMap.get(s).add(list.get(i).get(0));
+    		}
+    	}
+    	
+    	return overlapMap;
     }
 }
