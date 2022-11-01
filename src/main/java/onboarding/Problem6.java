f40e45b8ed188d006a4b37893092839234087ef4
kmw2378 <rlarla677@gmail.com>
2022-11-01 19:10:22 +0900

    문제6 풀이 2 1에서 하나의 이름에서 중복된 단어가 포함될 경우 이것이 중복 단어로 판단될 수 있는 문제점을 해결하기 위해 메소드를 수정했다. 별도의 클래스를 만들어 solution 을 간단히했다. 각 단어마다 포함된 단어에 대해 중복 검사를 수행하기 위해 Set 에 담고 그것을 중복된 단어들을 모두 저장하는 컬렉션에 저장했다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,29 +1,49 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        Set<String> words = new HashSet<>();
 
-        for (List<String> form : forms) {
-
-            String name = form.get(1);
-
-            int currentIndex = 0;
-
-            while (currentIndex < name.length() - 1) {
-
-                words.add(name.substring(currentIndex, currentIndex + 2));
-                currentIndex++;
-            }
-
-        }
+        CheckDuplicateMemberNames checkDuplicateMemberNames = new CheckDuplicateMemberNames(forms);
+        checkDuplicateMemberNames.setDuplicatedWords();
 
         return answer;
+    }
+
+    private static class CheckDuplicateMemberNames {
+
+        List<String> duplicatedWords = new ArrayList<>();
+        List<List<String>> forms;
+
+        public CheckDuplicateMemberNames(List<List<String>> forms) {
+            this.forms = forms;
+        }
+
+        public void setDuplicatedWords() {
+
+            for (List<String> form : forms) {
+
+                Set<String> temp = new HashSet<>();
+
+                String name = form.get(1);
+
+                int currentIndex = 0;
+
+                while (currentIndex < name.length() - 1) {
+
+                    temp.add(name.substring(currentIndex, currentIndex + 2));
+                    currentIndex++;
+                }
+
+                duplicatedWords.addAll(temp);
+            }
+
+            duplicatedWords = duplicatedWords.stream()
+                    .filter(e -> Collections.frequency(duplicatedWords, e) > 1)
+                    .collect(Collectors.toList());
+        }
     }
 }
