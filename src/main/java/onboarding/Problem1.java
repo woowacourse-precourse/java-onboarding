c2ef6f5b7728aacd76075694e24784a568adc45b
gyumin159 <gyumin159@DESKTOP-6CKHGBO>
2022-11-01 17:25:23 +0900

    한 페이지를 넘겨받으면 String을 쪼개 각 자릿수를 더하기, 곱하기 하여 최댓값 뽑기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,24 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+	// 1개의 페이지에서 나오는 2가지 경우중 큰 값 구하기
+	// 2개의 페이지에서 각각 나온 값 비교하여 큰 값 구하기
+	// 한 사람이 가진 두개의 페이지가 연속적인가확인
+	
+	public static int all_poss(String num) { // 한 페이지 중 최대값
+		String[] arr = num.split("");
+		int curr, plus = 0, mult = 1;
+		for(String s : arr) {
+			curr = Integer.parseInt(s);
+			plus += curr;
+			mult *= curr;
+		}
+		if (plus >= mult) {
+			return plus;
+		}
+		return mult;
+	}
+	
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
