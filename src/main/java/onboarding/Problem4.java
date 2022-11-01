865b725de0f3e9649575fa115c13d83514d5c3fd
JungHyun Jo <wjstkwhwjdgus@gmail.com>
2022-11-01 12:21:43 +0900

    uppercase && lowercase && spacing, substitution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,7 +8,18 @@ public class Problem4 {
 		int ascNum = 0;
 		int abs = 0;
 
-
+		for (int i = 0; i < c.length; i++) {
+			if ((int) c[i] >= 65 && (int) c[i] <= 90) { // 대문자인 경우
+				ascNum = 65;
+			} else if ((int) c[i] >= 97 && (int) c[i] <= 122) { // 소문자인 경우
+				ascNum = 97;
+			} else { // 띄어쓰기인 경우 그냥 띄어쓰기를 직접 삽입하고 다음 루프 진행
+				answer += ' ';
+				continue;
+			}
+			abs = Math.abs(ascNum - (int) c[i]); // 반대문자 전환용 차이값
+			answer += (char) (ascNum + 25 - abs); // 반대문자 전환
+		}
 
 		return answer;
 	}
