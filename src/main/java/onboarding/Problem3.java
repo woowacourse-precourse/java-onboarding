b9f0566d3ff331edc8af57db22040190b70c7deb
hgo641 <hgo641@gmail.com>
2022-11-01 19:48:14 +0900

    feat: Problem3 숫자에 있는 3, 6, 9 개수 반환 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    /**
+     * number에 들어가는 3, 6, 9의 개수를 카운트
+     * @param number 3, 6, 9를 카운트할 숫자
+     * @return number에 들어가는 3, 6, 9의 개수
+     */
+    public static int getNumberOfClap(String number){
+        IntStream intStream = number.chars();
+        IntStream filteredStream = intStream.filter(digit -> digit == '3' || digit == '6' || digit == '9');
+        int numberOfClap = (int) filteredStream.count();
+        return numberOfClap;
     }
 }
