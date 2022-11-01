2c61e13d2ee7261cf0ebe7d005d793c6a6f15f85
eom-tae-in <eti0728@daum.net>
2022-11-01 21:56:03 +0900

    feat: updateScore() -> 친구의 친구와, 방문자의 추천 점수를 올려주는 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -91,4 +91,16 @@ public class Problem7 {
             }
         }
     }
+
+    public static void updateScore(List<List<String>> friends, List<String> visitors) {
+        for (List<String> friend : friends) {
+            if (myFriendsSet.contains(friend.get(0)) || myFriendsSet.contains(friend.get(1))) {
+                updateRecommendScore(friend.get(0), ACQUAINTANCE_POINT);
+                updateRecommendScore(friend.get(1), ACQUAINTANCE_POINT);
+            }
+        }
+        for (String visitor : visitors) {
+            updateRecommendScore(visitor, VISITOR_POINT);
+        }
+    }
 }
