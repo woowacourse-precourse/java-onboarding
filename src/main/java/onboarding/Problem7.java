97e07551ef41ec6d2b27e0a1cf5fbe4c1a22b80a
CAEL0 <kjongmin26@gmail.com>
2022-11-01 15:47:28 +0900

    기능 추가: PROBLEM7 그래프에 엣지 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,19 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static void addEdge(HashMap<String, HashSet<String>> graph, String userFrom, String userTo) {
+        if (graph.containsKey(userFrom))
+            graph.get(userFrom).add(userTo);
+        else
+            graph.put(userFrom, new HashSet<>(List.of(userTo)));
     }
 }
