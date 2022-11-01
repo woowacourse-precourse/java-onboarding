e192c77abfcf026cd54008bd3f663e0c6f2b6b7d
seungjun <seungjun123>
2022-11-01 13:47:18 +0900

    사용자와 친구 관계인 사람의 리스트를 만드는 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        List<String> userFriends = friendList(user, friends);
+
         return answer;
+    }
+
+    private static List<String> friendList(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (friend.get(0).equals(user)) {
+                userFriends.add(friend.get(1));
+            } else if (friend.get(1).equals(user)) {
+                userFriends.add(friend.get(0));
+            }
+        }
+        return userFriends;
     }
 }
