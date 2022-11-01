576130583a9cffc71d02e43991d19173acdfc7d4
ghchoi <ghchoi0427@gmail.com>
2022-11-01 14:58:44 +0900

    feat: invertCharacter 메서드 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        Problem4 p4 = new Problem4();
+        return p4.invertCharacter(word);
     }
+
+    private String invertCharacter(String str) {
+        StringBuilder sb = new StringBuilder();
+        final int upperMid = 77;
+        final int lowerMid = 109;
+
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                sb.append((char) (2 * lowerMid + 1 - c));
+                continue;
+            }
+            if (Character.isUpperCase(c)) {
+                sb.append((char) (2 * upperMid + 1 - c));
+                continue;
+            }
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
 }
