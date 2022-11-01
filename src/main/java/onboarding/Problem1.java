7036421db36e4e80a522df1aa108a3cc150a47a9
core <jinjs6@gmail.com>
2022-11-01 17:24:30 +0900

    Problem1 페이지 번호 게임 위한 사전 검증 기능 구현(페이지의 범위, 페이지 위치에 따른 짝수 또는 홀수 검증, 페이지 차이 1 확인)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,21 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static boolean check(List<Integer> pobi, List<Integer> crong) {
+        if (!(1 <= pobi.get(0) && pobi.get(0) <= 400) || !(1 <= pobi.get(1) && pobi.get(1) <= 400)
+                || !(1 <= crong.get(0) && crong.get(0) <= 400) || !(1 <= crong.get(1) && crong.get(1) <= 400)) {
+            return false;
+        }
+        if (!(pobi.get(0) % 2 == 1 && pobi.get(1) % 2 == 0) || !(crong.get(0) % 2 == 1 && crong.get(1) % 2 == 0)) {
+            return false;
+        }
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
+            return false;
+        }
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
