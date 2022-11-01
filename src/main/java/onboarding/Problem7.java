6cab77619d6cbd12a48712d21debbe07ea32a8fc
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 16:19:30 +0900

    [Add] Prob7 visitScore func
    + visitScore (nameList, visitors, scoreboard) : check nameList and friends, update scoreboard and return it

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    // func : save people from user, friends, visitors
+    // func : save people from user, friends, visitors and return it
     private static List<String> saveNames(String user, List<List<String>> friends, List<String> visitors){
        List<String> nameList = new ArrayList<>();
 
@@ -27,13 +27,25 @@ public class Problem7 {
         }
         return nameList;
     }
-    // func : calculate score by visit
+    // func : calculate score by visit, return scoreboard
+    private static List<Integer> visitScore(List<String> nameList, List<String> visitors, List<Integer> scoreboard){
+
+        // plus visit score
+        for (int i=0;i<visitors.size();i++){
+            int idx = nameList.indexOf(visitors.get(i));
+            scoreboard.set(idx, scoreboard.get(idx) + 1);
+        }
+        return scoreboard;
+    }
     // func : calculate score by checking friendship
     // func : calculate score and return 5 recommends
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        System.out.println(saveNames(user,friends,visitors));
+        List<String> answer = saveNames(user,friends,visitors);
+        List<Integer> scoreboard = new ArrayList<>();
+        for (int i=0;i<6;i++)
+            scoreboard.add(0);
+        System.out.println(visitScore(answer, visitors, scoreboard));
         return answer;
     }
 }
