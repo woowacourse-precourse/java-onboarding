db6051cb2ba54215c6c8897f1a352298a7ef2dd8
Frog-Slayer <Frog-Slayer@github.com>
2022-11-01 13:41:21 +0900

    Problem 4 Refactoring

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,26 +4,30 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         for (int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if (isLowerCase(c)){
-                c = (char)(-c + 'z' + 'a');
-            }
-            else if (isUpperCase(c)){
-                c = (char)(-c + 'Z' + 'A');
-            }
-            answer += c;
+            answer += switchChar(word.charAt(i));
         }
         return answer;
     }
 
-    public static boolean isLowerCase(char c){
+    private static boolean isLowerCase(char c){
         if (c >= 'a' && c <= 'z') return true;
         return false;
     }
 
-    public static boolean isUpperCase(char c){
+    private static boolean isUpperCase(char c){
         if (c >= 'A' && c <= 'Z') return true;
         return false;
+    }
+
+    private static char switchChar(char c){
+        char switchedChar = c;
+        if (isLowerCase(c)){
+            switchedChar = (char)(-c + 'z' + 'a');
+        }
+        else if (isUpperCase(c)) {
+            switchedChar = (char) (-c + 'Z' + 'A');
+        }
+        return switchedChar;
     }
 
 
