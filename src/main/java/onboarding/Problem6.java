5e0b403e5810b1d7333ea8bc233ad513e402aa17
LJW25 <dbjwl@g.skku.edu>
2022-11-01 21:38:43 +0900

    p6. complete functions

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,71 +1,74 @@
 package onboarding;
 
-import java.util.List;
+import java.util.Collections;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Problem6 {
+
+
+    private static HashMap<String, String> list2map(List<List<String>> forms){
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        for(List<String> f:forms) { map.put(f.get(1), f.get(0)); }
+
+        return map;
+    }
 
     private static List<String> splitNickname(String nickname){
         List<String> splitedList = new ArrayList<>();
 
+        int len = nickname.length();
+        for(int i=0;i<len-1;i++) splitedList.add(nickname.substring(i, i+2));
 
         return splitedList;
     }
 
+    private static Set<String> findSimilar(List<String> names, String nickname){
+        Set<String> checked = new HashSet<String>();
 
-    private static boolean checkDuplicateList(String names, List<String> splitedList){
-        boolean isDuplicate = false;
+        List<String> splitedList = splitNickname(nickname);
+        for(String name:names){
+            for(String s:splitedList){
+                if(name.contains(s) && !name.equals(nickname)){
+                    checked.add(name);
+                    break;
+                }
+            }
+        }
 
-        return isDuplicate;
+        if(checked.size()>0) checked.add(nickname);
+
+        return checked;
     }
 
-    private static List<String> checkUnduplicateList(List<List<String>> undupList, List<String> splitedList){
-        List<String> duplicated = new ArrayList<>();
+    private static List<String> getEmailList(HashMap<String, String> forms, List<String> checked){
+        List<String> emailList = new ArrayList<>();
 
-        return duplicated;
-    }
-
-    private static List<String> sortList(List<String> emailList){
-
+        checked.forEach(name -> emailList.add(forms.get(name)));
         return emailList;
     }
 
-
-
-
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = new ArrayList<>();
 
-        String dupNames = "";
-        List<List<String>> undupList = new ArrayList<>();
+        HashMap<String, String> mapForms = list2map(forms);
+        List<String> nameList = new ArrayList<>(mapForms.keySet());
 
-        String nickname, email;
-        List<String> splitedList = new ArrayList<>();
-        List<List<String>> dupList = new ArrayList<>();
+        Set<String> checked = new HashSet<String>();
 
-        for(List<String> f:forms){
-            email = f.get(0);
-            nickname = f.get(1);
-            splitedList = splitNickname(nickname);
-            if(checkDuplicate(names, splitedList){
-                answer.add(email);
-                dupNames = dupNames + "\n" + nickname;
-            }
-            else{
-                dupList = checkUnduplicateList(undupList, splitedList);
-                if(dupList.size()) > 0){
-                    for(List<String> d:dupList){
-                        answer.add(d.get(0));
-                        dupNames = dupNames + "\n" + d.get(1);
-                    }
-                }
-                else undupList.add(f);
-            }
-
-            splitedList.clear();
-            dupList.clear();
+        for(String name:nameList){
+            checked.addAll(findSimilar(nameList, name));
         }
 
+        List<String> colelctedName = new ArrayList<>(checked);
+
+        List<String> emailList = getEmailList(mapForms, colelctedName);
+        Collections.sort(emailList);
+
+        List<String> answer = emailList;
 
         return answer;
     }
