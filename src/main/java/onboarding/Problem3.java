e2d1ca50f0690ce99663eae2ffe8fa0fa2780b62
tdmdgh <tdmdgh@gmail.com>
2022-11-01 18:53:44 +0900

    problem3_1_하나의 정수가 필요로 하는 박수 횟수 구하는 매소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    private static int needClap(int n){
+        int ans = 0;
+        while (n != 0){
+            int unit_d = n % 10;
+            if (unit_d != 0 && unit_d % 3 == 0)
+                ans++;
+            n/=10;
+        }
+        return ans;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
