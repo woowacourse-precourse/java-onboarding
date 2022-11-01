b2fb444df1022f40bf064baa148685449ad9fed5
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 19:08:14 +0900

    문제4 / feat : 문제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,9 @@
 package onboarding;
 
+import onboarding.problem4.Convertor;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return Convertor.convertString(word);
     }
 }
