1fce4ad1eb40bc23a40d935c30a9684145f859ba
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-01 17:52:09 +0900

    docs: Problem2 - 기능 구현 목록 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/**
+ * isProperCryptogram 메서드 암호문이 적절한지 확인
+ * getDecodingCryptogram 메서드 해독된 암호문을 반환
+ * isLengthZero 암호문의 길이가 0인지 확인
+ * isDecodingCryptogram 암호해독이 완료됐는지 확인
+ * deleteOverlap 중복된 문자열을 제거 하여 반환
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
