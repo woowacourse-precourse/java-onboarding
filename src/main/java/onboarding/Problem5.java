a8017ed489ee160fa10a0efe29c140b762803c66
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-01 15:23:43 +0900

    feat: 그리디 알고리즘을 활용하여 화폐 단위별 개수를 도출함

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,7 +9,24 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
 
         int[] charges = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        
+
+        getCharges(money, answer, charges);
+
         return answer;
+    }
+
+    public static void getCharges(int money, List<Integer> answer, int[] charges) {
+        for(int i = 0; i < charges.length; i++){
+
+            int cnt = money / charges[i];
+            if(cnt == 0) {
+                answer.add(0);
+            } else {
+                answer.add(cnt);
+            }
+            int remainder = money % charges[i];
+
+            money = remainder;
+        }
     }
 }
