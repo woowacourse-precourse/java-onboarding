62074914dc0b3e598f272a1e0b66dadc31d9941d
Giho <poqw741@gmail.com>
2022-11-01 22:30:21 +0900

    multi method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -29,6 +29,28 @@ class Problem1 {
 
         return maxScore(value1, value2); // a or b 큰 수
     }
+
+    static int multi(List<Integer> value){
+
+        int value1 = 1;
+        int value2 = 1;
+        int value1tmp = value.get(0);
+        int value2tmp = value.get(1);
+
+        int tmp = 0; // 중간 저장
+
+
+        for (int i=0;i<(int)(Math.log10(value.get(0))+1);i++) {
+            value1 *= value1tmp%10;
+            value1tmp /= 10;
+        }
+
+        for (int i=0;i<(int)(Math.log10(value.get(0))+1);i++) {
+            value2 *= value2tmp%10;
+            value2tmp /= 10;
+        }
+        return maxScore(value1,value2);
+    }
     }
 
 
