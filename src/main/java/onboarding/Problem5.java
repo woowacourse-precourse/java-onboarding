25957060a7eb3e102e2d6c5fb46a9d9a16396b0b
eom-tae-in <eti0728@daum.net>
2022-11-01 20:43:45 +0900

    feat: static value

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    static final int[] monetaryUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    static Integer[] countPaperMoney = new Integer[9];
+    static List<Integer> countPaperMoneyList = new ArrayList<>();
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        if (moneyOutOfRangeException(money)) {
+            Arrays.fill(countPaperMoney, 0);
+            return countPaperMoneyList = Arrays.asList(countPaperMoney);
+        }
+        countMoney(money);
+        return countPaperMoneyList = Arrays.asList(countPaperMoney);
     }
 }
