9400113fef206d612769669810412bcdea945967
gitFILO <x1xgudwls@naver.com>
2022-11-01 23:02:55 +0900

    [Problem 6] 닉네임을 연속된 2글자로 잘라서 만든 모든 경우의 substring을 key로, 메일을 value로 HashMap에 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        HashSet<String> AnswerEmails = findAnswerEmailSet(forms);
+        List<String> answer = new ArrayList<>(AnswerEmails);
+        Collections.sort(answer);
+
         return answer;
     }
+
+    public static HashSet<String> findAnswerEmailSet(List<List<String>> forms) {
+        HashMap<String, String> allPartialNicknameWithEmail = new HashMap<>();
+        HashSet<String> answerEmails = new HashSet<>();
+        String nickname;
+        String email;
+
+        for (int i = 0; i < forms.size(); i++) {
+            nickname = forms.get(i).get(1);
+            email = forms.get(i).get(0);
+            putAllPartialNicknameWithEmail(allPartialNicknameWithEmail, answerEmails, nickname, email);
+        }
+
+        return answerEmails;
+    }
 }
+
