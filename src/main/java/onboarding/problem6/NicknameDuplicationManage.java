6bced875d8750b916f7c32be213ae78bc71406df
seominseok00 <seominseok4834@gmail.com>
2022-11-01 23:23:42 +0900

    feat(문제6): 닉네임 중복 관리 클래스 추가

---
+++ src/main/java/onboarding/problem6/NicknameDuplicationManage.java
@@ -0,0 +1,11 @@
+package onboarding.problem6;
+
+import java.util.List;
+
+public class NicknameDuplicationManage {
+    private List<List<String>> forms;
+
+    public NicknameDuplicationManage(List<List<String>> forms){
+        this.forms = forms;
+    }
+}
