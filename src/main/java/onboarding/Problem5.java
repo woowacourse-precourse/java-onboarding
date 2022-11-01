59c85815cc0633a0be92d3e896e63a0e0da53bd4
gitFILO <x1xgudwls@naver.com>
2022-11-01 22:55:38 +0900

    [Problem 5] unit의 5배와 1배 짜리의 지폐, 또는 동전의 개수를 구하는 메서드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,27 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+    }
+    public static int fiveTimesUnitCalculate(int money,int digit){
+        int currentMoney = money;
+        int fiveTimesUnit =0;
+
+        while(currentMoney / digit >= 5){
+            fiveTimesUnit++;
+            currentMoney -= 5*digit;
+        }
+        return fiveTimesUnit;
+    }
+    public static int unitCalculate(int money, int digit){
+        int currentMoney = money;
+        int unit=0;
+
+        while(currentMoney / digit > 0){
+            unit++;
+            currentMoney -= digit;
+        }
+
+        return unit;
     }
 }
