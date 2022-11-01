77caa8bdd35ba19b8b1a7029d726763da1bdc584
sa46lll <9812smh@naver.com>
2022-11-01 10:50:41 +0900

    feat: 문자열 분리, 대소문자 구분

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        List<String> arr = Stream.of(word.split("")).collect(Collectors.toList());
+
+        for (String a : arr) {
+            System.out.println(isUpper(a.charAt(0)));
+        }
+
         return answer;
+    }
+
+    private static boolean isUpper(char c) {
+        return Character.isUpperCase(c);
     }
 }
