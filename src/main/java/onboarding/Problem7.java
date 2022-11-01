b3ca57dc3cc5d465eda9b349b5f879e6cb47319b
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:41:34 +0900

    Feat: Add 친구 기반, 방문자 기반 추천 점수 계산 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -11,6 +11,7 @@ public class Problem7 {
         return answer;
     }
 
+    
     private static HashMap<String, ArrayList<String>> createRelation(List<List<String>> friends) {
         HashMap<String, ArrayList<String>> relationMap = new HashMap<>();
 
@@ -36,5 +37,46 @@ public class Problem7 {
         relationMap.put(userA, arrayList);
 
         return relationMap;
+    }
+
+
+    private static HashMap<String, Integer> calculateRecommendationScore(String user, HashMap<String, ArrayList<String>> relationMap, List<String> visitors) {
+        HashMap<String, Integer> recommendationScore = new HashMap<>();
+
+        calculateFriendRecommendationScore(user, relationMap, recommendationScore);
+
+        calculateVisitorRecommendationScore(user, relationMap, visitors, recommendationScore);
+
+        return recommendationScore;
+    }
+
+
+    private static void calculateFriendRecommendationScore(String user, HashMap<String, ArrayList<String>> relationMap, HashMap<String, Integer> recommendationScore) {
+        for (String isFriend : relationMap.get(user)) {
+            for (String friendOfFriend : relationMap.get(isFriend)) {
+                if (!(friendOfFriend == user) && !(relationMap.get(user).contains(friendOfFriend))) {
+                    Integer score = 10;
+                    if (recommendationScore.containsKey(friendOfFriend)) {
+                        score = recommendationScore.get(friendOfFriend);
+                        score += 10;
+                    }
+                    recommendationScore.put(friendOfFriend,score);
+                }
+            }
+        }
+    }
+
+
+    private static void calculateVisitorRecommendationScore(String user, HashMap<String, ArrayList<String>> relationMap, List<String> visitors, HashMap<String, Integer> recommendationScore) {
+        for (String visitor : visitors) {
+            Integer score = 1;
+            if (!(relationMap.get(user).contains(visitor))) {
+                if (recommendationScore.containsKey(visitor)) {
+                    score = recommendationScore.get(visitor);
+                    score ++;
+                }
+                recommendationScore.put(visitor, score);
+            }
+        }
     }
 }
