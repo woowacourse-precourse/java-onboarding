3d21144e051ff526d5cda09dc0f74352e98d9acc
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 18:13:56 +0900

    문자 하나를 입력받고 소문자면 true를 소문자가 아니면 false를 리턴한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+    private static boolean isLowerCase(char letter) {
+        return letter >= 'a' && letter <= 'z';
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
