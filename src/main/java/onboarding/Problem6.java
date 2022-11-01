61f383b8c1d8f96769dd1d3e9490dfcce8e02ed4
yw824 <yongwoo25@gmail.com>
2022-11-01 10:50:06 +0900

    feat: [Problem6] 기능 명세 / 프로토타이핑
    - 변수:
    	- static List<String> emails: 유효한 이메일 리스트
    	- static List<String> names: 유효한 닉네임 리스트
    	- static List<String> answer: 중복을 위한 정답 리스트
    	- static int len: 유효한 닉네임/이메일 리스트의 길이
    	- static boolean[] stringIncluded: 중복된 이메일이
    		- 이미 포함되었는지 기록하는 배열
    
    - 구현 기능 1 : 비교를 위한 이메일 배열과 닉네임 배열 설정
    - 구현 기능 2 : 각 닉네임 간 비교 후 중복되는 닉네임 처리
    - 구현 기능 3 : 중복 닉네임 간 결과 이메일 배열을 정렬
    
    - 구현 기능 1) SetNamesAndEmails(forms)
    	- 입력으로부터 유효한 이메일 및 닉네임만
    	- 비교를 위한 배열에 추가하는 기능
    	- 각 이메일과 닉네임이 유효한 지 판단하는 함수 호출
    - 구현 기능 1) isEmailValid(String email)
    	- 이메일 문자열의 형식 준수 여부 판단
    	- 11자 ~ 20자 이내여야 함
    	- 도메인 : @email.com 이여야 함
    - 구현 기능 1) isNameValid(String name)
    	- 닉네임 문자열의 형식 준수 여부 판단
    	- 길이 : 1 ~ 20자 내부인지 확인
    	- 닉네임이 한글인지 확인
    
    - 구현 기능 2) CheckDuplicatedNames()
    	- 유효한 닉네임만 가진 배열을 순회하면서
    	- 서로 비교하여 중복된 내용이 있는지 판단되면
    	- 결과 닉네임 리스트에 추가하기
    	- 중복되는 문자열 있는지 판단하는 함수 호출
    
    - 구현 기능 2) areNamesDuplicated(String name1, String name2)
    	- 각 문자열의 길이만큼(최대 len1 + len2 만큼)
    	- 첫 번째 문자가 겹치지 않으면 넘기기
    	- 두 번째 문자가 겹치지 않으면 넘기기
    	- 두 글자가 모두 겹치면 바로 true 반환
    
    - 구현 기능 3) 결과 리스트 정렬
    	- 이메일을 오름차순으로 정렬( a부터 , A부터 )

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,73 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    static List<String> emails;
+    static List<String> names;
+    static List<String> answer;
+    static int len;
+    static boolean[] stringIncluded;
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        answer = new ArrayList<String>();
+        names = new ArrayList<String>();
+        emails = new ArrayList<String>();
+
+        // implement 1 : 비교를 위한 이메일 배열과 닉네이 배열 설정
+
+        // implement 2 : 각 닉네임 간 비교 후 중복 닉네임 처리
+
+        // implement 3 : 중복 닉네임 가진 결과 이메일 배열을 정렬
+
         return answer;
     }
+
+    // 입력으로부터 유효한 이메일 및 닉네임만 비교 위한 배열에 추가
+    static void SetNamesAndEmails(List<List<String>> forms)
+    {
+
+    }
+    // 이메일 형식 준수 여부 확인
+    static boolean isEmailValid(String email)
+    {
+        //implement
+        // 11자 ~ 20자 이내여야 함
+        // 도메인 : @email.com 만 허용
+
+        return true;
+    }
+
+    // 닉네임 형식 준수 여부 확인
+    static boolean isNameValid(String name)
+    {
+        // implement
+        // 길이 : 1 ~ 20자 내부인지 확인
+        // 닉네임이 한글인지 확인
+        return true;
+    }
+
+    // 각 닉네임을 비교 : 겹치는 두 자 이상의 문자열이 있으면
+    // 겹치는 두 닉네임의 이메일들을 결과 리스트에 추가
+    static void CheckDuplicatedNames()
+    {
+        // implement
+        // O(n^2)
+        for(int i = 0; i < len; ++i)
+            for(int j = i + 1; j < len; ++j)
+            {
+
+            }
+    }
+
+    // 겹치는 두 자 이상의 문자가 있는지 비교 : 있으면 true , 없으면 false
+    static boolean areNamesDuplicated(String name1, String name2)
+    {
+        // implement : 문자열 비교
+        // 첫 번째 문자가 겹치지 않으면 넘기기
+        // 두 번째 문자가 겹치지 않으면 넘기기
+        // 두 글자가 모두 겹치면 바로 true 반환
+        return false;
+    }
+
 }
