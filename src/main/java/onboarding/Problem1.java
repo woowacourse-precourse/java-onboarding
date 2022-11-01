4c9ac41692146419fa235d1673c42bf15da7ec4a
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-01 19:19:43 +0900

    refactor: Page와 Calculator 객체로 포장 (#1)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,51 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    private static final int FIRST_PAGE_NUMBER = 1;
-    private static final int LAST_PAGE_NUMBER = 400;
     private static final int TIE = 0;
     private static final int POBI_WIN = 1;
     private static final int CRONG_WIN = 2;
     private static final int EXCEPTION = -1;
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (checkInvalidPage(pobi) || checkInvalidPage(crong))
+        Page pobiPage = Page.create(pobi.get(0), pobi.get(1));
+        Page crongPage = Page.create(crong.get(0), crong.get(1));
+        if (pobiPage.isInvalid() || crongPage.isInvalid())
             return EXCEPTION;
-        int pobiResult = getMaxFromLeftAndRightPage(pobi);
-        int crongResult = getMaxFromLeftAndRightPage(crong);
-        return getWinner(pobiResult, crongResult);
-    }
-
-    private static int getMaxFromLeftAndRightPage(List<Integer> pages) {
-        int leftResult = getMaxFromOnePage(pages.get(0));
-        int rightResult = getMaxFromOnePage(pages.get(1));
-        return Math.max(leftResult, rightResult);
-    }
-
-    private static int getMaxFromOnePage(Integer page) {
-        int addNum = getAddNum(page);
-        int multipleNum = getMultipleNum(page);
-        return Math.max(addNum, multipleNum);
-    }
-
-    private static int getAddNum(Integer page) {
-        int addNum = 0;
-        while ( page > 0 ) {
-            int i = page % 10;
-            addNum += i;
-            page /= 10;
-        }
-        return addNum;
-    }
-
-    private static int getMultipleNum(Integer page) {
-        int multipleNum = 1;
-        while ( page > 0 ) {
-            int i = page % 10;
-            multipleNum *= i;
-            page /= 10;
-        }
-        return multipleNum;
+        return getWinner(pobiPage.getMaxResult(), crongPage.getMaxResult());
     }
 
     private static int getWinner(Integer pobiMaxNum, Integer crongMaxNum) {
@@ -58,15 +24,80 @@ class Problem1 {
         return CRONG_WIN;
     }
 
-    private static boolean checkInvalidPage(List<Integer> pages) {
-        return !isRightPageRange(pages.get(0), pages.get(1)) || !isValidPageFormat(pages.get(0), pages.get(1));
-    }
+    public static class Page {
+        private final int leftPage;
+        private final int rightPage;
+        private static final int FIRST_PAGE_NUMBER = 1;
+        private static final int LAST_PAGE_NUMBER = 400;
+        private final Calculator calculator;
 
-    private static boolean isRightPageRange(Integer leftPage, Integer rightPage) {
-        return leftPage > FIRST_PAGE_NUMBER || rightPage < LAST_PAGE_NUMBER;
-    }
+        private Page(int leftPage, int rightPage) {
+            this.leftPage = leftPage;
+            this.rightPage = rightPage;
+            calculator = new Calculator();
+        }
 
-    private static boolean isValidPageFormat(Integer leftPage, Integer rightPage) {
-        return leftPage < rightPage && (rightPage - leftPage) == 1;
+        public static Page create(int leftPage, int rightPage) {
+            return new Page(leftPage, rightPage);
+        }
+
+        public boolean isInvalid() {
+            return !isRightPageRange() || !isValidPageFormat();
+        }
+
+        private boolean isRightPageRange() {
+            return this.leftPage > FIRST_PAGE_NUMBER || this.rightPage < LAST_PAGE_NUMBER;
+        }
+
+        private boolean isValidPageFormat() {
+            return this.leftPage < this. rightPage && (rightPage - leftPage) == 1;
+        }
+
+        public int getMaxResult() {
+            return getMaxFromLeftAndRightPage();
+        }
+
+        private int getMaxFromLeftAndRightPage() {
+            int leftResult = getMaxFromOnePage(leftPage);
+            int rightResult = getMaxFromOnePage(rightPage);
+            return Math.max(leftResult, rightResult);
+        }
+
+        private int getMaxFromOnePage(int page) {
+            return Math.max(calculator.getAddNum(page), calculator.getMulNum(page));
+        }
+
+        private static class Calculator {
+            private int addNum = 0;
+            private int mulNum = 1;
+
+            Calculator() {}
+
+            public int getAddNum(int page) {
+                add(page);
+                return addNum;
+            }
+
+            public int getMulNum(int page) {
+                multiple(page);
+                return mulNum;
+            }
+
+            private void add(int page) {
+                while ( page > 0 ) {
+                    int i = page % 10;
+                    addNum += i;
+                    page /= 10;
+                }
+            }
+
+            private void multiple(int page) {
+                while ( page > 0 ) {
+                    int i = page % 10;
+                    mulNum *= i;
+                    page /= 10;
+                }
+            }
+        }
     }
 }
