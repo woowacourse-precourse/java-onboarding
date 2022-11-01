c34077995e27df26172e7326f14bf78964f33c45
SeungEon <js112099@gmail.com>
2022-11-01 21:00:35 +0900

    문제4번 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = List.of(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        int[] price = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i = 0; i < 9; i++) {
+            answer.set(i, money/price[i]);
+
+            money = money % price[i];
+        }
+
+        System.out.println("money\tresult");
+
+        System.out.println(money + "\t" + answer);
+
         return answer;
     }
 }
