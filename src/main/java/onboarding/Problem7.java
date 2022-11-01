8e2047df3b27249dfecbbf6b6d7992d959e79ee0
simjaeseo <delpho@naver.com>
2022-11-01 23:32:10 +0900

    feat: 문제 7 기능목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 유저의 친구 찾아서 List에 저장하는 기능
+ * 2. 유저의 피드에 방문한 친구 찾아서 Map에 저장하는 기능
+ * 3. 유저의 친구와 친구인 사람 찾아서 Map에 저장하는 기능
+ * 4. 유저와 유저 친구는 Map에서 삭제하는 기능
+ * 5. 친구 이름과 추천 점수를 정렬하기 위해 class 구현 후 정렬하는 기능
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
