fc98996109e2a3bc9b1124aeb0f970e58990c921
gudals113 <gudals113@gmail.com>
2022-11-01 15:33:49 +0900

    feat : Problem2 Solved
    feat : Problem2 Solved
    
    스택 이용해서 반복 문자열 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i<cryptogram.length(); i++) {
+            char nowChar = cryptogram.charAt(i);
+            if (stack.isEmpty() == true) {
+                stack.push(nowChar);
+            } else {
+                if (stack.peek() == nowChar) {
+                    stack.pop();
+                } else {
+                    stack.push(nowChar);
+                }
+            }
+        }
+
+        for (char leftChar : stack) {
+            answer += leftChar;
+        }
+
         return answer;
     }
 }
