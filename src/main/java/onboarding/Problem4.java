c017b2f7ee22e4d10b26ab3c1c6cf714cd94ee33
pasly0920 <inwoo920621@gmail.com>
2022-11-01 15:49:26 +0900

    feat: 1 청개구리식 문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return "";
     }
+
+    private static char convertChar(char singleChr){
+        int charAsci = singleChr;
+        int convertedCharAsci = 0;
+
+        if(charAsci >= (int)'a')
+            convertedCharAsci = (int)'z' - (charAsci - (int)'a');
+        else
+            convertedCharAsci = (int)'Z' - (charAsci - (int)'A');
+
+        return (char)convertedCharAsci;
+    }
+
 }
