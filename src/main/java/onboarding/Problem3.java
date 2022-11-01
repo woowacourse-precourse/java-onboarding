0aed30a81ce92275441e29e7c9b577ac8ad12b8a
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-01 17:59:16 +0900

    feat(PROBLEM3): 한 자리의 정수가 3, 6, 9 인지 확인하고 true/false 를 반환하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,5 +9,20 @@ public class Problem3 {
         }
         return totalClap;
     }
+
+    //1. 한 자리의 정수가 3, 6, 9 인지 확인하고 true/false 를 반환하는 기능
+    private static boolean is369(int n){
+        String number = String.valueOf(n);
+        if (number.contains("3")){
+            return true;
+        }
+        if (number.contains("6")){
+            return true;
+        }
+        if (number.contains("9")){
+            return true;
+        }
+        return false;
+    }
 }
 
