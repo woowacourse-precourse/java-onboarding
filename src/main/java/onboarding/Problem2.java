4a8d3042b29a7818576efb4d72a6fd25618a7d38
yeop0740 <ssucgoo@gmail.com>
2022-11-01 19:18:17 +0900

    make method removeDup(Problem2)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,21 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    public static String removeDup(String cryptogram){
+        String result = "";
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if(cryptogram.charAt(i) != cryptogram.charAt(i + 1)){
+                result += String.valueOf(cryptogram.charAt(i));
+            } else {
+                i++;
+            }
+            if (i == cryptogram.length() - 2)
+                result += String.valueOf(cryptogram.charAt(i + 1));
+        }
+
+        if (cryptogram.length() != result.length())
+            result = removeDup(result);
+
+        return result;
+    }
 }
