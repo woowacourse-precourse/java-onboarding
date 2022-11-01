7eddd7305a083bdf73a2f006a06d7c34734fe1f9
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 14:44:59 +0900

    FEAT divideChar

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    //소문자 1 대문자 2 그외 -1로  문자 구분
+    private static int divideChar(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return 1;
+        }
+        if (c >= 'A' && c <= 'Z') {
+            return 2;
+        }
+
+        return -1;
+    }
 }
