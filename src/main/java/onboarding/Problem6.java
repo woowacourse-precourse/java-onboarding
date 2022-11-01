d4d9a4da3d6374101f55d8a31e10bf11b90c2805
ingpyo <juninpyo72@gmail.com>
2022-11-01 12:34:11 +0900

    [problem6]예외 처리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,15 +8,29 @@ import java.util.Map;
 
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
+		checkList(forms);
 
-		List<String> answer = new ArrayList<>();
-		Map<String, Integer> map = new HashMap<>();
-
-
-		Collections.sort(answer);
 		return answer;
 	}
 
-
+	public static void checkList(List<List<String>> forms) throws IllegalArgumentException {
+		if (forms.size() < 1 || forms.size() > 10000) {
+			throw new IllegalArgumentException("크루가 1명 미만 또는 100000명 초과되었습니다.");
+		}
+		for (List<String> list : forms) {
+			String email = list.get(0), name = list.get(1);
+			if (email.length() < 11 || email.length() > 20) {
+				throw new IllegalArgumentException("email의 길이가 맞지 않습니다.");
+			}
+			if (!email.substring(email.length() - 9).equals("email.com")) {
+				throw new IllegalArgumentException("\"email.com\"도메인이 아닙니다.");
+			}
+			if (!name.matches("^[가-힣]*$")) {
+				throw new IllegalArgumentException("닉네임은 한글만 가능합니다.");
+			}
+			if (name.length() < 1 || name.length() >= 20) {
+				throw new IllegalArgumentException("닉네임의 길이가 부합하지 않습니다.");
+			}
+		}
 	}
 }
