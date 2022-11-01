7ab116614541baef1e0a73f097a6c1cad3394c3e
cgk95 <kjkj5868@gmail.com>
2022-11-01 14:11:52 +0900

    P4 메서드 명 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,7 @@ public class Problem4 {
     public static String solution(String word) {
         HashMap<String,String> map=new HashMap<>();
         String answer = "";
-        myMapping(map); /*알파벳을 거꾸로 매핑*/
+        alphabetMapping(map);
         /*문자열을 순차적으로 순환*/
         for (int i=0;i<word.length();i++) {
             answer = addReversedStr(word, map, answer, i);
@@ -15,12 +15,12 @@ public class Problem4 {
     /**해시구조를 이용해 문자를 치환**/
     private static String addReversedStr(String word, HashMap<String, String> map, String answer, int i) {
         String s = String.valueOf(word.charAt(i));
-        answer += map.getOrDefault(s, s); /*해시맵을 가지고 문자를 치환하여 정답 문자열에 추가*/
+        answer += map.getOrDefault(s, s);               /*해시맵을 가지고 문자를 치환하여 정답 문자열에 추가*/
         return answer;
     }
 
     /**알파벳을 각기 매핑**/
-    private static void myMapping(HashMap<String, String> map) {
+    private static void alphabetMapping(HashMap<String, String> map) {
         String forward="ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
         String backward = "ZYXWVUTSRQPONMLKJIHGFEDCBA zyxwvutsrqponmlkjihgfedcba";
         for (int i = 0; i < forward.length(); i++) {
