2533bd1a0509b76f6cbe60dede2254c5375e15ef
jaewoo <wjdwodn0722@naver.com>
2022-11-01 14:18:38 +0900

    feat : problem7 add user friends list method
    - add makeFriendList method

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
+    public static List<String> makeFriendList(String user, List<List<String>> freinds){
+        List<String> userFriends = new ArrayList<>();
+        for (List<String> item :freinds){
+            if(item.get(0).equals(user)){
+                userFriends.add(item.get(1));
+            }
+            else if(item.get(1).equals(user)){
+                userFriends.add(item.get(0));
+            }
+        }
+        return userFriends;
+    }
+
+
 }
