f4898f9d92bfba84a542a13ce61e3acff5478dd8
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:21:54 +0900

    Problem1 numCalculator method
    Calculates the player's score from their page number

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -23,4 +23,18 @@ class Problem1 {
         return ((left + 1 != right) || (left == 1) || (right == 400));
     }
 
+    public static int numCalculator(List<Integer> pageList) {
+        int n = Integer.parseInt(pageList.get(0) + Integer.toString(pageList.get(1)));
+        int sum = 0;
+        int product = 1;
+        while (true) {
+            sum += (n % 10);
+            product *= (n % 10);
+            if (n < 10) {
+                break;
+            }
+            n /= 10;
+        }
+        return Math.max(sum, product);
+    }
 }
