a3e896dfe742882d84bac9109ef9779e5ba291f5
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-01 17:49:34 +0900

    feat(Problem6): 비교 대상이 자기 자신이 아니고 중복이 아니라면 해당 크루의 이메일을 answer List에 add(기능목록3)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,7 +10,8 @@ public class Problem6 {
             for (int j = 0; j < forms.get(i).get(1).length() - 1; j++) {
                 String checkName = forms.get(i).get(1).substring(j, j + 2);
                 for (int k = 0; k < forms.size(); k++) {
-                    if (forms.get(k).get(1).contains(checkName) {
+                    if (forms.get(k).get(1).contains(checkName) && !answer.contains(forms.get(k).get(0)) && forms.get(k).get(1) != forms.get(i).get(1)) {
+                        answer.add(forms.get(k).get(0));
                     }
                 }
             }
