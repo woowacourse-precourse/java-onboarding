827ea9b35c5f5fbfb3294e913cd63717822eae8c
Jaewoooong Kim <kimje1109@gmail.com>
2022-11-01 15:44:21 +0900

    feat: Add getCharacter

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,39 @@
 package onboarding;
 
 public class Problem4 {
+    public static Character getCharacter(Character alphabet) {
+        int gap = 25;
+        for (int ascii = (int)'a'; ascii <= (int)'m'; ascii++) {
+            if ((char)ascii == alphabet) {
+                return (char)((int)alphabet + gap);
+            }
+            gap -= 2;
+        }
+        gap = 25;
+        for (int ascii = (int)'A'; ascii <= (int)'Z'; ascii++) {
+            if ((char)ascii == alphabet) {
+                return (char)((int)alphabet + gap);
+            }
+            gap -= 2;
+        }
+        gap = 1;
+        for (int ascii = (int)'n'; ascii <= (int)'z'; ascii++) {
+            if ((char)ascii == alphabet) {
+                return (char)((int)alphabet - gap);
+            }
+            gap += 2;
+        }
+        gap = 1;
+        for (int ascii = (int)'N'; ascii <= (int)'Z'; ascii++) {
+            if ((char)ascii == alphabet) {
+                return (char)((int)alphabet - gap);
+            }
+            gap += 2;
+        }
+
+        return alphabet;
+    }
+
     public static Character conversionCase(Character alphabet) {
         if (alphabet >= 'a' && alphabet <= 'z') {
             return (char)((int)alphabet - 32);
