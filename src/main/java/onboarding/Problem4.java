daff479d88b07ee9aabd366b0608988520dad84d
kim1387 <kevink0@naver.com>
2022-11-01 17:06:06 +0900

    feat: 청개구리 변환 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import static java.lang.Character.*;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+  private static final char START_UPPER_ALPHABET = 'A';
+  private static final char START_LOWER_ALPHABET = 'a';
+  private static final int ALPHABET_NUMBER = 25;
+
+  public static String solution(String word) {
+    String answer = "";
+
+    return answer;
+  }
+
+  private static String convertByFrog(char alphabet) {
+    char reversedString;
+    if (Character.isLowerCase(alphabet)) {
+      reversedString = getReverseStringWhenStarter(alphabet, START_LOWER_ALPHABET);
+      return String.valueOf(reversedString);
     }
+    reversedString = getReverseStringWhenStarter(alphabet, START_UPPER_ALPHABET);
+    return String.valueOf(reversedString);
+  }
+
+  private static char getReverseStringWhenStarter(char alphabet, char starterAlphabet) {
+    return (char) (ALPHABET_NUMBER - (alphabet - starterAlphabet) + starterAlphabet);
+  }
 }
