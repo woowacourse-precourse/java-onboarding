f85d02360d2661af17aa843048ebea37b23d4972
왕준영 <wangjunyoung@wangjun-yeong-ui-MacBookPro.local>
2022-11-01 15:12:09 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,6 +4,22 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer="";
+        int last = 0;
+        int flag = 1;
+        StringBuilder str = new StringBuilder(cryptogram);
+
+        while (flag == 1) {
+            flag = 0;
+            for (int i = 0; i < str.length()-1; i++) {
+                if (str.charAt(i) == str.charAt(i + 1)) {
+                    str.delete(i, i + 2);
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        answer = str.toString();
+
         return answer;
     }
 }
