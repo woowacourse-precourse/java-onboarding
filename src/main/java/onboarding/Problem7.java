f33ebdfd98e5c1863b233f83e9a8aa931ac1de84
patronum98 <zkrhklkl98@gmail.com>
2022-11-01 21:14:25 +0900

    문제7 기능1, 기능2 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,12 +6,37 @@ package onboarding;
 //기능4. value 값이 높은 순으로 리스트에 정렬한다.
 //기능5. 만약 점수가 같으면 알파벳 순으로 출력한다.
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        HashMap<String,Integer> map = new HashMap<>();
+
+        for (int i=0;i<friends.size();i++) {
+            for (int j=0;j<2;j++) {
+                if (!(friends.get(i).contains(user))) { //user 의 이름을 포함하지 않으면
+                    if (map.containsKey(friends.get(i).get(j))) { //만약 이름이 이미 있으면
+                        map.put((friends.get(i).get(j)), map.get((friends.get(i).get(j)))+10); //10을 더한다.
+                    } else {
+                        map.put((friends.get(i).get(j)), 10); //추가
+                    }
+                }
+            }
+        }
+
+//        if (friends.get(2).contains("mrko")) {
+//            System.out.println(1);
+//        }
+//        System.out.println(friends);
+
+
+        System.out.println(map);
+
         return answer;
     }
 }
