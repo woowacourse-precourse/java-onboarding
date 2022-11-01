43b2d7b7038274e20cc36a24585338cd28eae596
moonchaewon <codnjs8989@gmail.com>
2022-11-01 17:53:05 +0900

    feat(Problem3): add function to get clap by number

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -29,8 +29,35 @@ public class Problem3 {
         return clap;
     }
 
+    private static int getClapByNumber(int number, int[] clap) {
+        System.out.printf("Target Number: %d\n", number);
+        int digit, digitNum, remainder; // ex) 582 -> 자릿수(3), 해당 자릿수에 해당하는 숫자(5), 나머지(82)
+        int clapNum = 0; // 손뼉횟수
+
+        if (number < 10) return number/3;
+        else {
+            digit = getDigit(number);
+            digitNum = number / getPowerByDigit(digit);
+            clapNum += (digitNum-1) * clap[digit-1-1];
+            clapNum += ((digitNum-1)/3) * getPowerByDigit(digit);
+            remainder = (number % getPowerByDigit(digit));
+            if (digitNum % 3 == 0) {
+                clapNum += remainder+1;
+            }
+            System.out.printf("clapNum: %d\n", clapNum);
+            return clapNum + getClapByNumber(remainder, clap);
+        }
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        System.out.println("==================");
+        int[] clap = getClapByDigit(number);
+        answer = getClapByNumber(number, clap);
+        int digit = getDigit(number);
+        for(int i=digit-1; i>0; i--) {
+            answer += clap[i-1];
+        }
         return answer;
     }
 }
