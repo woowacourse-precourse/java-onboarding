0cd71d4f408588727372a876aabd6f1e0c00719a
KIM-GO-EUN <rhdmstkdals@naver.com>
2022-11-01 23:52:04 +0900

    1. 받아온 문자열을 한글자씩 분리 2. 분리한 문자를 한글자씩 공간에 넣으며 중복 비교 3. 중복일 경우 삭제 4. 중복이 아닐 경우 저장 5. 최종 문자열 출력

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
+import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Problem2 prob = new Problem2();
+        answer = prob.chrsave(cryptogram);
         return answer;
+    }
+
+    public String chrval(String cryptogram){
+        char[] car = cryptogram.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0;i<car.length;i++) {
+            char c = car[i];
+            char[] chr;
+            if (stack.isEmpty()){
+                stack.push(c);
+            }
+            else{
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+
+        }
+        String stacksave = stack.toString();
+        return stacksave;
+    }
+
+    public String chrsave(String s){
+        String str = new String(chrval(s));
+        return str;
     }
 }
