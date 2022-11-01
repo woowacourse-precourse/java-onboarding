5f816cd1a6acdc4460237a0d7b90486247459dce
Jinhui <jda0526@naver.com>
2022-11-01 23:08:41 +0900

    Feat(Problem7): [F-01] 사용자의 친구와 사용자 친구의 친구 관계를 정리한 변수 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,54 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        System.out.println(friends);
+        List<String> userFriends = makeFriendsList(user, friends);
+        System.out.println(userFriends);
+        List<List<String>> recommendFriends = makeRecoFriends(userFriends, friends);
+        System.out.println(recommendFriends);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> makeFriendsList(String user, List<List<String>> friends) {
+        Set<String> userFriends = new HashSet<>();
+
+        for (List<String> friend : friends) {
+            if(friend.contains(user)){ // 친구 관계라면
+                userFriends.add(friend.get(0));
+                userFriends.add(friend.get(1));
+            }
+        }
+        userFriends.remove(user);
+        return new ArrayList<>(userFriends);
+    }
+
+    private static List<List<String>> makeRecoFriends(List<String> userFriends, List<List<String>> friends) {
+        List<List<String>> list = new ArrayList<>();
+
+        for(String friend : userFriends){
+            list.add(makeFriendsList(friend, friends));
+        }
+        return list;
+    }
+
+
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+
+        solution(user, friends, visitors);
     }
 }
