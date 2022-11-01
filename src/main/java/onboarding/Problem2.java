01909fe66ce6e313de70b05f1283843e2a9cf086
Yeongseon Park <yseon406@gmail.com>
2022-11-01 13:03:17 +0900

    problem2 init code

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        for(char c : cryptogram.toCharArray()){
+            if(stack.isEmpty()){
+                stack.push(c);
+            }else{
+                if(stack.peek()==c){
+                    stack.pop();
+                }else{
+                    stack.push(c);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.peek());
+            stack.pop();
+        }
+
+        answer = String.valueOf(sb.reverse());
         return answer;
     }
 }
