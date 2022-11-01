abfcf1cf4a2c9297d5e8922bf4478f63683accf6
MINJU <alswn9938@gmail.com>
2022-11-01 15:43:12 +0900

    [Q7] refactor : 메소드명 리팩토링

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,8 +5,8 @@ import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class Problem7 {
-    public static Map<String, List<String>> friendsMap;
-    public static Map<String, Integer> scoreMap;
+    public static Map<String, List<String>> userFriendsListMap;
+    public static Map<String, Integer> userScoreMap;
     public static int FIRST_FRIEND_INDEX = 0;
     public static int SECOND_FRIEND_INDEX  = 1;
     public static int INIT_SCORE = 0;
@@ -14,69 +14,69 @@ public class Problem7 {
     public static int VISITOR_SCORE = 1;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        initScoreMap();
-        initFriendsMap(friends);
+        initUserScoreMap();
+        initUserFriendsListMap(friends);
 
         plusAcquaintanceScore(user);
         plusVisitorScore(visitors);
 
-        return getTopThreeFriends(user);
+        return getTopFiveScoreFriends(user);
     }
 
-    public static void initScoreMap() {
-        scoreMap = new HashMap<>();
+    public static void initUserScoreMap() {
+        userScoreMap = new HashMap<>();
     }
 
-    public static void initFriendsMap(List<List<String>> friends) {
-        friendsMap = new HashMap<>();
+    public static void initUserFriendsListMap(List<List<String>> friends) {
+        userFriendsListMap = new HashMap<>();
         for (List<String> friend : friends) {
             String firstFriend = friend.get(FIRST_FRIEND_INDEX);
             String secondFriend = friend.get(SECOND_FRIEND_INDEX);
 
-            putFriendsMap(firstFriend, secondFriend);
-            putFriendsMap(secondFriend, firstFriend);
+            putUserFriendsMap(firstFriend, secondFriend);
+            putUserFriendsMap(secondFriend, firstFriend);
         }
     }
 
-    public static void putScoreMap(String key, int plusValue) {
-        if (!scoreMap.containsKey(key)) {
-            scoreMap.put(key, INIT_SCORE);
+    public static void plusUserScore(String key, int plusValue) {
+        if (!userScoreMap.containsKey(key)) {
+            userScoreMap.put(key, INIT_SCORE);
         }
-        scoreMap.put(key, scoreMap.get(key) + plusValue);
+        userScoreMap.put(key, userScoreMap.get(key) + plusValue);
     }
 
-    public static void putFriendsMap(String key, String value) {
-        if (friendsMap.containsKey(key)) {
-            friendsMap.get(key).add(value);
+    public static void putUserFriendsMap(String key, String value) {
+        if (userFriendsListMap.containsKey(key)) {
+            userFriendsListMap.get(key).add(value);
         } else {
             List<String> values = new ArrayList<>();
             values.add(value);
-            friendsMap.put(key, values);
+            userFriendsListMap.put(key, values);
         }
     }
 
     public static void plusAcquaintanceScore(String user) {
-        if(!isUserHasFriend(user)){
+        if(!isUserHasFriends(user)){
             return;
         }
-        List<String> userFriends = friendsMap.get(user);
+        List<String> userFriends = userFriendsListMap.get(user);
         for (String userFriend : userFriends) {
             List<String> acquaintanceList = getAcquaintanceList(user, userFriend);
-            acquaintanceList.forEach(ac -> putScoreMap(ac, ACQUAINTANCE_SCORE));
+            acquaintanceList.forEach(ac -> plusUserScore(ac, ACQUAINTANCE_SCORE));
         }
     }
 
     public static List<String> getAcquaintanceList(String user, String userFriend) {
-        return friendsMap.get(userFriend).stream().
+        return userFriendsListMap.get(userFriend).stream().
                 filter(acquaintance -> !acquaintance.equals(user)).collect(Collectors.toList());
     }
 
     public static void plusVisitorScore(List<String> visitors) {
-        visitors.forEach(visitor -> putScoreMap(visitor, VISITOR_SCORE));
+        visitors.forEach(visitor -> plusUserScore(visitor, VISITOR_SCORE));
     }
 
-    public static List<Entry<String, Integer>> getSortScoreMapEntryList() {
-        List<Entry<String, Integer>> entryList = new ArrayList<>(scoreMap.entrySet());
+    public static List<Entry<String, Integer>> getSortedScoreMapEntryList() {
+        List<Entry<String, Integer>> entryList = new ArrayList<>(userScoreMap.entrySet());
         entryList.sort((o1, o2) -> {
             if (o1.getValue().equals(o2.getValue())) return o1.getKey().compareTo(o2.getKey());
             return o2.getValue().compareTo(o1.getValue());
@@ -84,10 +84,10 @@ public class Problem7 {
         return entryList;
     }
 
-    public static List<String> getTopThreeFriends(String user) {
-        List<Entry<String, Integer>> sortScoreMapEntryList = getSortScoreMapEntryList();
-        if(isUserHasFriend(user)){
-            List<String> userFriendsList = friendsMap.get(user);
+    public static List<String> getTopFiveScoreFriends(String user) {
+        List<Entry<String, Integer>> sortScoreMapEntryList = getSortedScoreMapEntryList();
+        if(isUserHasFriends(user)){
+            List<String> userFriendsList = userFriendsListMap.get(user);
             return sortScoreMapEntryList.stream().filter(userScoreEntry -> userScoreEntry.getValue() > 0)
                     .filter(userScoreEntry -> !userFriendsList.contains(userScoreEntry.getKey()))
                     .limit(5).map(Entry::getKey).collect(Collectors.toList());
@@ -96,8 +96,8 @@ public class Problem7 {
                 .limit(5).map(Entry::getKey).collect(Collectors.toList());
     }
 
-    public static boolean isUserHasFriend(String user){
-        return friendsMap.containsKey(user);
+    public static boolean isUserHasFriends(String user){
+        return userFriendsListMap.containsKey(user);
     }
 
 }
