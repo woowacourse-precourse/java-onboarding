f7a5ebcc4c7be01eec0e4498da8c5a17d1ffb4b3
si hwan <pum005@naver.com>
2022-11-01 22:09:47 +0900

    feat (problem1) : 입력을 예외처리하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,16 @@
 package onboarding;
 
+import ExceptionValidation.P1ExceptionValidation;
+
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        if(!(P1ExceptionValidation.p1ExceptionValidation(pobi)
+        && P1ExceptionValidation.p1ExceptionValidation(crong))){
+            return -1;
+        }
     }
+
 }
