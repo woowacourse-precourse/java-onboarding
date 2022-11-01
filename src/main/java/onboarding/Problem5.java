ac3dd62f226ad57e0fdc12954ef2587bf9f10bec
yun8565 <yunyun8565@naver.com>
2022-11-01 14:45:22 +0900

    refactor(P5): 코드 간소화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,8 +7,7 @@ public class Problem5 {
     public static final int[] WON = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = getCash(money);
-        return answer;
+        return getCash(money);
     }
 
     public static List<Integer> getCash(int money) {
