cf7d486a4e1a003ea982c4b8db6752db178c2f3a
tjwjdgus12 <01048733438a@gmail.com>
2022-11-01 13:05:54 +0900

    Solve problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                answer.append((char) ('a' + ('z' - ch)));
+            } else if (Character.isUpperCase(ch)) {
+                answer.append((char) ('A' + ('Z' - ch)));
+            } else {
+                answer.append(ch);
+            }
+        }
+
+        return answer.toString();
     }
 }
