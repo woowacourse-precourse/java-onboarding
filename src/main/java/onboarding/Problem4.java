0f7a498139172fa20ab450eb0abcb4bf048e4854
wlwlwon <ryanwon@naver.com>
2022-11-01 20:28:50 +0900

    Problem4 메서드 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem4 {
+    public static void main(String[] args) {
+        System.out.println(solution("I love you"));
+    }
     public static String solution(String word) {
         String answer = "";
-        return answer;
+        StringBuilder result = new StringBuilder();
+        for(int i=0;i<word.length();i++){
+            char alphabet = word.charAt(i);
+
+            if(alphabet>=65 && alphabet<=90){
+                int change = 90 - (int)alphabet + 65;
+                alphabet = (char) change;
+            }else if(alphabet>=97 && alphabet<=122){
+                int change = 122 - (int)alphabet + 97;
+                alphabet = (char) change;
+            }
+            result.append(alphabet);
+        }
+        return result.toString();
     }
 }
