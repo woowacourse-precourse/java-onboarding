be44ace8891e86b6c7eb6549424d0f93a6955a36
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-01 22:37:02 +0900

    백엔드 7번문제
    미해결
    
    knowFriend를 ArrayList로 전환하여 중복되는 닉네임 없이 friend와 visitors의 닉네임들을 담음
    ->friends의 리스트에서 가져왔다면 개수만큼 count하여 10점씩, visitors 리스트에서 가져왔다면 1점씩 가산하여 총 점수를 계산, 동점일 경우 알파벳순으로 나열, 점수가 0인 친구는 result에서 제거

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,6 +6,40 @@ import java.util.List;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        int[] score = new int[friends.size()];
+        String[] knowFriend = new String[friends.size()];
+        String[] knowFriendSet = new String[friends.size()];
+
+
+        for(int i = 0; i < friends.size(); i++){
+            knowFriend[i] = String.valueOf(friends.get(i).get(1));
+        }
+        for(int i = 0; i < friends.size(); i++){
+            for(int j = 1+i ; j < friends.size(); j++){
+                if(knowFriend[i] == knowFriend[j]){
+                    knowFriend[j] = null;
+                }
+                continue;
+            }
+        }
+        System.out.println(knowFriend.length);
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        List<String> result = List.of("andole", "jun", "bedi");
+
+        System.out.println(solution(user, friends, visitors));
     }
 }
