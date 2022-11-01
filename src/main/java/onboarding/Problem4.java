23e622552f823ace7e8a2a59c561d5d0f4b577aa
yhkee0404 <yhkee0404@gmail.com>
2022-11-01 19:06:39 +0900

    Update Problem4.java

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.stream.Collector;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String chars = word.chars().parallel()
+                .mapToObj(d -> {
+                    char c = (char) d;
+                    if (Character.isLetter(c)) {
+                        if (Character.isUpperCase(c)) {
+                            d = 'Z' - d + 'A';
+                        } else {
+                            d = 'z' - d + 'a';
+                        }
+                        c = (char) d;
+                    }
+                    return c;
+                })
+                // https://www.digitalocean.com/community/tutorials/java-stream-collect-method-examples
+                .collect(StringBuilder::new,
+                    (builder, c) -> builder.append(c),
+                    // StringBuilder implements CharSequence
+                    StringBuilder::append
+                ).
+                // StringBuilder
+                toString();
+        String answer = chars.toString();
         return answer;
     }
 }
