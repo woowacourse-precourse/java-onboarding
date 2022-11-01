99df92c3150cc7b5ba0a95dbba5fadeb07de3a68
khj <kim37171@cuk.edu>
2022-11-01 21:46:26 +0900

    [Problem7] 아는 친구 리스트 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,12 @@ import java.util.stream.Collectors;
 
 public class Problem7 {
     private static Map<String,Integer> recommenedFreinds = new HashMap<>();
+    private static List<String> myFriends = new LinkedList<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new LinkedList<>();
         calcVisitorsPoints(visitors);
+        calcMyFriends(user, friends);
         return answer;
     }
 
@@ -19,6 +21,23 @@ public class Problem7 {
                 continue;
             }
             recommenedFreinds.put(visitor, 1);
+        }
+    }
+
+    private static void calcMyFriends(String user, List<List<String>> friends) {
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> friend =  friends.get(i);
+            addMyFriends(user, friend.get(0),friend.get(1));
+            addMyFriends(user, friend.get(1),friend.get(0));
+        }
+    }
+
+    private static void addMyFriends(String user, String id1, String id2) {
+        if(id1.equals(user)){
+            myFriends.add(id2);
+            if(recommenedFreinds.containsKey(id2)){
+                recommenedFreinds.remove(id2);
+            }
         }
     }
 }
