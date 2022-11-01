b9eb59ca0dcc32006dc833863c983da3b4c1e1d3
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 21:46:53 +0900

    feat:Problem5 값 구하여 리스트에 추가하는 기능 추가.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,7 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
