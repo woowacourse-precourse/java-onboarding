9791333b5237c2a44532684d26a226fac503fe42
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-01 14:20:14 +0900

    problem4 feat : get ascii of alphabet

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int ascii = 0;
+        char[] tempArr = word.toCharArray();
+        for (int i = 0; i < tempArr.length; i++) {
+            ascii = setAsciiOfAlphabet(tempArr[i]);
+        }
         return answer;
+    }
+    public static int setAsciiOfAlphabet(char c){
+        int ascii = 0;
+        if(Character.isUpperCase(c)) ascii = 65;
+        else if (Character.isLowerCase(c)) ascii = 97;
+        return ascii;
     }
 }
