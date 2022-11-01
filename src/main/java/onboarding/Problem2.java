e9ce0f43f8393b1d3841bd93d58d2d3c6203582e
rt3310 <btac3310@gmail.com>
2022-11-01 23:11:03 +0900

    refactor(Problem2): getAllRemovedDuplicatedString 및 관련 메서드 Decryptor 객체로 이동

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,51 +2,8 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        return getAllRemovedDuplicatedString(cryptogram);
+        Decryptor decryptor = new Decryptor(cryptogram);
+        return decryptor.getAllRemovedDuplicatedString();
     }
 
-    public static String getAllRemovedDuplicatedString(String cryptogram) {
-        while (cryptogram != "") {
-            String removedDuplicatedString = getRemovedDuplicatedString(cryptogram);
-            if (removedDuplicatedString.equals(cryptogram)) {
-                return removedDuplicatedString;
-            }
-            cryptogram = removedDuplicatedString;
-        }
-        return cryptogram;
-    }
-
-    public static String getRemovedDuplicatedString(String cryptogram) {
-        cryptogram = addBlankAtLastIndex(cryptogram);
-        boolean prevDuplicated = false;
-        boolean curDuplicated;
-        String result = "";
-
-        for (int i = 1; i < cryptogram.length(); i++) {
-            char previous = cryptogram.charAt(i - 1);
-            char current = cryptogram.charAt(i);
-            curDuplicated = isCurDuplicated(previous, current);
-            result += getNotDuplicatedChar(curDuplicated, prevDuplicated, previous);
-            prevDuplicated = curDuplicated;
-        }
-        return result;
-    }
-
-    private static String addBlankAtLastIndex(String cryptogram) {
-        return cryptogram + " ";
-    }
-
-    private static String getNotDuplicatedChar(Boolean curDuplication, Boolean prevDuplication, char previous) {
-        if (!curDuplication && !prevDuplication) {
-            return String.valueOf(previous);
-        }
-        return "";
-    }
-
-    private static boolean isCurDuplicated(char previous, char current) {
-        if (current == previous) {
-            return true;
-        }
-        return false;
-    }
 }
