c9a22fe41cc5c76d09732c39f5a368d707dc0929
kimsh2948 <shkim2948@gmail.com>
2022-11-01 23:06:08 +0900

    last commit

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+        Map<String, String> duplicateCheckMap = new HashMap<String, String>();
+        List<String> tmpList = new ArrayList<String>();
+
+        for (int i=0; i<forms.size(); i++) {
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
+
+            for (int j=0; j<nickname.length()-1; j++) {
+                if (duplicateCheckMap.containsKey(nickname.substring(j, j+2))) {
+                    answer.add(email);
+                    answer.add(duplicateCheckMap.get(nickname.substring(j, j+2)));
+                }
+                else {
+                    duplicateCheckMap.put(nickname.substring(j, j+2), email);
+                }
+            }
+        }
+
+        for (String item : answer) {
+            if (!tmpList.contains(item))
+                tmpList.add(item);
+        }
+        answer = tmpList;
+        Collections.sort(answer);
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,58 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<String>();
+        Map<String, Integer> pointMap = new HashMap<String, Integer>();
+        List<String> userFriendsList = new ArrayList<String>();
+
+        for (int i=0; i<friends.size(); i++) {
+            int index = friends.get(i).indexOf(user);
+            if (friends.get(i).contains(user)) {
+                userFriendsList.add(friends.get(i).get(1-index));
+            }
+        }
+
+        for (int i=0; i<friends.size(); i++) {
+            String[] tmpName = {friends.get(i).get(0), friends.get(i).get(1)};
+            if (tmpName[0].equals(user) || tmpName[1].equals(user)) continue;
+            for (int j=0; j<2; j++) {
+                if (userFriendsList.contains(tmpName[j]))
+                    pointMap.put(tmpName[1-j], pointMap.getOrDefault(tmpName[1-j], 0) + 10);
+            }
+        }
+
+        for (int i=0; i<visitors.size(); i++) {
+            String visitUser = visitors.get(i);
+            if (!pointMap.containsKey(visitUser))
+                pointMap.put(visitUser, pointMap.getOrDefault(visitUser, 0) + 1);
+        }
+
+        for (int i=0; i<userFriendsList.size(); i++) {
+            if (pointMap.containsKey(userFriendsList.get(i)))
+                pointMap.remove(userFriendsList.get(i));
+        }
+
+        List<Map.Entry<String, Integer>> sortList = new ArrayList<Map.Entry<String, Integer>>(pointMap.entrySet());
+        Collections.sort(sortList, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                int sol= o2.getValue().compareTo(o1.getValue());
+                return sol == 0 ? o1.getKey().compareTo(o2.getKey()) : sol;
+            }
+        });
+
+        int index = 0;
+        for (Map.Entry<String, Integer> ent : sortList) {
+            if (index == 5)
+                break;
+            if (ent.getValue() != 0)
+                answer.add(ent.getKey());
+            index += 1;
+        }
+
         return answer;
     }
 }
