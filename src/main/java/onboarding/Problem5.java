be7111cfb0bfcdb86211a31a5b11d96af0d58943
sa46lll <9812smh@naver.com>
2022-11-01 23:30:37 +0900

    refactor(PROBLEM5.java): 변수, 함수 이름 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,18 +6,18 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new LinkedList<>();
-        int[] moneyUnit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] moneyUnit = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
         for (int currentUnit : moneyUnit) {
-            answer.add(divideAB(money, currentUnit));
+            answer.add(divide(money, currentUnit));
             money %= currentUnit;
         }
 
         return answer;
     }
 
-    private static int divideAB(int a, int b) {
-        return a / b;
+    private static int divide(int money, int moneyUnit) {
+        return money / moneyUnit;
     }
 
 }
