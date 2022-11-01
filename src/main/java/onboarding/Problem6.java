a9962e1f02e6aa2a8985e5f197dcaf3940725c33
changeme4585 <benscience@naver.com>
2022-11-01 14:12:07 +0900

    good

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,29 +7,16 @@ public class Problem6 {
         Map<String,Integer> Id = new HashMap<>();
         List<String> answer  = new ArrayList<>();
         for (int idx = 0; idx<forms.size();idx++) {
-            for(int i=2 ;i<forms.get(idx).get(1).length();i++){
-                for (int q=0;q<forms.get(idx).get(1).length()-i+1;q++) {
-                    if(Id.containsKey(forms.get(idx).get(1).substring(q, q+i))==false){
-                        Id.put(forms.get(idx).get(1).substring(q, q+i),0);
-                    }
-                    else if(Id.containsKey(forms.get(idx).get(1).substring(q, q+i))==true){
-                        //해당 키가 이미 있다면
-                        for (int x= 0; x<forms.size();x++){
-                            if( forms.get(x).get(1).contains(forms.get(idx).get(1).substring(q, q+i))==true){
-                                answer.add(forms.get(x).get(0));
-                            }
-                        }
-                    }
+            for(int i=0 ;i<forms.get(idx).get(1).length()-1;i++){
+                if(Id.containsKey(forms.get(idx).get(1).substring(i,i+2))==false){
+                    Id.put(forms.get(idx).get(1).substring(i, i+2),0);
                 }
-            }
-            //풀넴임 넣는 코드
-            if(Id.containsKey(forms.get(idx).get(1))==false ) {
-                Id.put(forms.get(idx).get(1),0);
-            }
-            else if(Id.containsKey(forms.get(idx).get(1))==true ) {
-                for (int x= 0; x<forms.size();x++){
-                    if( forms.get(x).get(1).contains(forms.get(idx).get(1))==true){
-                        answer.add(forms.get(x).get(0));
+                else if(Id.containsKey(forms.get(idx).get(1).substring(i, i+2))==true){
+                    //해당 키가 이미 있다면
+                    for (int x= 0; x<forms.size();x++){
+                        if( forms.get(x).get(1).contains(forms.get(idx).get(1).substring(i, i+2))==true){
+                            answer.add(forms.get(x).get(0));
+                        }
                     }
                 }
             }
