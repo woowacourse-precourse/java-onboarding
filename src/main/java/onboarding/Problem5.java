66319c73b1a78196559343b8c870de3dc6fd8b48
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-01 21:24:29 +0900

    Problem5 Collections 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
 
         int[] arrays = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
