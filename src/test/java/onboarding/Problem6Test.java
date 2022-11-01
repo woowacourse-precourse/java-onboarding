cfe3a09b5de3f68891784783c849abf94108a435
이소영 <th42500@naver.com>
2022-11-01 23:49:11 +0900

    [Test] 유사한 닉네임을 갖고있는 크루들 리스트 요소와 오름차순 유무 체크하는 테스트
    ❓ searchNickname()의 결과가 null이 나오는 오류 발생

--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -3,10 +3,7 @@ package onboarding;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,4 +38,23 @@ class Problem6Test {
 
         assertEquals(testDuplicateEmail, problem6.findDuplicateNickname(crews));
     }
+
+    @Test
+    @DisplayName("유사한 닉네임을 갖고 있는 크루들의 이메일을 오름차순으로 정렬하여 출력")
+    void searchNickname() {
+        problem6 = new Problem6();
+        List<String> resultEmail = Arrays.asList("jm@email.com","jason@email.com", "mj@email.com");
+        Collections.sort(resultEmail);
+
+        List<String> answer = new ArrayList<>();
+        List<List<String>> forms = new ArrayList<>();
+        forms.add(new ArrayList<>(Arrays.asList("jm@email.com", "제이엠")));
+        forms.add(new ArrayList<>(Arrays.asList("jason@email.com", "제이슨")));
+        forms.add(new ArrayList<>(Arrays.asList("woniee@email.com", "워니")));
+        forms.add(new ArrayList<>(Arrays.asList("mj@email.com", "엠제이")));
+        forms.add(new ArrayList<>(Arrays.asList("nowm@email.com", "이제엠")));
+
+        assertEquals(resultEmail, problem6.searchNickname(answer, forms));
+    }
+
 }
