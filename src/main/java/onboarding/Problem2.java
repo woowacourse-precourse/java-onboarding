30c8f61e49cf2e82febd8e92fe4731532c8e095d
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-01 19:14:40 +0900

    221101 Style: P2 - 각주 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,18 +6,18 @@ public class Problem2 {
     public static String solution(String cryptogram) {
 
         boolean hasRepetition = true;
-        //3.문자열 내에 중복되는 문자가 없을 때까지 반복한다.
+        /*3.문자열 내에 중복되는 문자가 없을 때까지 반복한다.*/
         while(hasRepetition && !cryptogram.isBlank()) {
             hasRepetition = deleteRepetition(cryptogram, cryptogram.length());
             cryptogram = sb.toString();
         }
 
-        //4.중복되는 문자가 없다면 반복을 종료하고 결과 문자열을 출력한다.
+        /*4.중복되는 문자가 없다면 반복을 종료하고 결과 문자열을 출력한다.*/
         return sb.toString();
     }
 
     public static boolean deleteRepetition(String cryptogram, int length) {
-        //1.입력 문자열을 순회하며 중복 구간을 찾는다.
+        /*1.입력 문자열을 순회하며 중복 구간을 찾는다.*/
         sb = new StringBuilder();
         char prev = cryptogram.charAt(0);
         int sequenceCount = 0;
@@ -33,7 +33,7 @@ public class Problem2 {
 
             sb.append(prev);
 
-            //2.중복 구간이 끝나면 중복 구간을 StringBuilder 에서 삭제한다.
+            /*2.중복 구간이 끝나면 중복 구간을 StringBuilder 에서 삭제한다.*/
             if(prev != cryptogram.charAt(i) && isSequence) {
                 int currentLength = sb.length();
                 sb.delete((currentLength - sequenceCount - 1), currentLength);
