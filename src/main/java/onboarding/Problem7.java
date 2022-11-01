416c39637ec53c2cadf7c9fb6c83dfa05ff141a8
YooSuYeon <usuyn000@gmail.com>
2022-11-01 19:17:15 +0900

    feat(Problem7): 모든 친구 관계 정보 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Map<String, List<String>> friendsInfo = setFriendsInfo(friends);
+
         return answer;
+    }
+
+    private static Map<String, List<String>> setFriendsInfo(List<List<String>> friends) {
+        Map<String, List<String>> friendsInfo = new HashMap<>();
+
+        for (List<String> relationship : friends) {
+            String[] users = {relationship.get(0), relationship.get(1)};
+
+            for (int i = 0; i < users.length; i++) {
+                List<String> friendList = friendsInfo.getOrDefault(users[i], new ArrayList<>());
+                friendList.add(users[(i + 1) % 2]);
+
+                friendsInfo.put(users[i], friendList);
+            }
+        }
+
+        return friendsInfo;
     }
 }
