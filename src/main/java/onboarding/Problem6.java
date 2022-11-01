69c1a9e671f6737883f20103dbb7dcd5c78c7f3d
suhwan <popomarin28@gmail.com>
2022-11-01 16:23:47 +0900

    [problem6] 인자들에 대한 유효성 검사를 하여 remove 해주는 기능함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,21 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    public static List<List<String>> getValidList(List<List<String>> forms){
+        String domain = "@email.com";
+        for(int i = 0; i < forms.size(); i++ ) {
+            if(!forms.get(i).get(0).substring(forms.get(i).get(0).length() - 10).equals(domain))
+                forms.remove(i);
+            if(forms.get(i).get(0).length() < 11 || forms.get(i).get(0).length() > 19)
+                forms.remove(i);
+            boolean koeanWord = forms.get(i).get(1).matches("^[가-힣]*$");
+            if(!koeanWord)
+                forms.remove(i);
+            if(forms.get(i).get(1).length() < 1 || forms.get(i).get(1).length() > 19)
+                forms.remove(i);
+        }
+        return forms;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
