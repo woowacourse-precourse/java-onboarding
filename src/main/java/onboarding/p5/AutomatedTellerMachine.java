b7a9a1ca341141d56b9c04040874ead341761908
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-01 11:54:59 +0900

    [5번] comment : "화폐 변환 기능 주석 추가"

--- src/main/java/onboarding/p5/AutomatedTellerMachine.java
+++ src/main/java/onboarding/p5/AutomatedTellerMachine.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AutomatedTellerMachine {
+    // 화폐를 담고있는 배열
     public static final int[] bill = {50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+
+    // 가장 적은 화폐 조합을 반환해주는 메서드
     public static List<Integer> getBills(final int money){
         List<Integer> result = new ArrayList<>();
         int remain = money;
