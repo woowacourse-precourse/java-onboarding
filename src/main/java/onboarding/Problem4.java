f2cc15055a030fac71f0577be191bdaec985b583
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 11:13:59 +0900

    범위 설정, 알파벳 판별

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) { // 사전 반전순으로 일치하도록
-        String answer = "";
-        return answer;
+        char[] wordChar = word.toCharArray();
+        StringBuilder result = new StringBuilder(); // String 조작을 위해서
+        int ascii = 0, abs, reverse = 0;
+
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (Character.isUpperCase(wordChar[i]) || Character.isLowerCase(wordChar[i])) // 알파벳일 경우만
+            {
+
+            }
+        }
     }
 }
