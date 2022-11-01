106ae36674e05ef9ad782de0d5ecefaddcbd1cc3
unknown <dragontaek98@naver.com>
2022-11-01 21:17:29 +0900

    feat: problem7 친구인 사용자 목록과 친구가 아닌 사용자 목록 생성 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        HashSet<String> notFriend = new HashSet<>();
+        HashSet<String> friend = new HashSet<>();
+        HashMap<String,Integer> score = new HashMap<>();
+
+        //set friend / not friend list
+        for(int i = 0; i < friends.size(); i++){
+            String firstPerson = friends.get(i).get(0);
+            String secondPerson = friends.get(i).get(1);
+
+            //both are not user
+            if(!friends.get(i).contains(user)){
+                notFriend.add(firstPerson);
+                notFriend.add(secondPerson);
+            } else {
+                //if first is user
+                if(firstPerson.equals(user)){
+                    friend.add(secondPerson);
+                    notFriend.remove(secondPerson);
+                } else {
+                    friend.add(firstPerson);
+                    notFriend.remove(firstPerson);
+                }
+            }
+        }
+
         return answer;
     }
 }
