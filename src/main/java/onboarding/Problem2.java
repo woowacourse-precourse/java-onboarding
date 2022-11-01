a9f6861320f54c1febf3336629f2335a1530481b
ryuj-h <dbwlsgh57@naver.com>
2022-11-01 16:40:25 +0900

    Creating Problem2 class structure

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem2 {
+    static boolean isvalid(String cryptogram){
+
+        return true;
+    }
+    static String decrypt(String cryptogram){
+
+        return new String("failed");
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        if (!isvalid(cryptogram))
+            return new String("failed");
+        answer = decrypt(cryptogram);
         return answer;
     }
 }
