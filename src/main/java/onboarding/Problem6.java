c89d4b01292fbd057f2b8f8bb8acd297bf43837a
ParkJoungWoo <jug78@naver.com>
2022-11-01 19:16:40 +0900

    문제 6) 기능 3 : 유저 리스트를 입력 받고 기능 1, 기능 2를 통해 중복된 유저의 이메일 집합을 반환하는 기능 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,15 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>(makeRedunSet(forms));
+        Collections.sort(answer);
         return answer;
     }
     private static boolean EmailVaildCheck(String input){
@@ -20,6 +25,28 @@ public class Problem6 {
             }
         else {
             result = first.equals(second);
+        }
+        return result;
+    }
+    private static Set<String> makeRedunSet(List<List<String>> input){
+        Set<String> result = new HashSet<>();
+        for(int i = 0; i < input.size(); i++){
+            String standEmail = input.get(i).get(0);
+            String standNickName = input.get(i).get(1);
+            if(!EmailVaildCheck(standEmail))
+                continue;
+            for(int j = i + 1; j < input.size(); j++){
+                String diffEmail = input.get(j).get(0);
+                String diffNickName = input.get(j).get(1);
+                if(!EmailVaildCheck(diffEmail))
+                    continue;
+                if(redunCheck(standNickName, diffNickName)) {
+                    if(!result.contains(standEmail))
+                        result.add(standEmail);
+                    if(!result.contains(diffEmail))
+                        result.add(diffEmail);
+                }
+            }
         }
         return result;
     }
