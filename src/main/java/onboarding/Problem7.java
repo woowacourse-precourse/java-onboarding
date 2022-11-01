38616c720c283654d0870ef2b4fe310f9449919b
hyungkya <hyungkya@uci.edu>
2022-11-01 16:14:29 +0900

    feat(problem 7): add retrieval of users by friend status

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,11 +2,25 @@ package onboarding;
 
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static List<String> getUnfriended(Map<String, List<String>> friendsGraph, String user) {
+        List<String> friended = getFriended(friendsGraph, user);
+        return getAllUsers(friendsGraph)
+                .stream()
+                .filter(u -> !u.equals(user) & !friended.contains(u))
+                .collect(Collectors.toList());
+    }
+    private static List<String> getFriended(Map<String, List<String>> friendsGraph, String user) {
+        return friendsGraph.get(user);
+    }
+    private static Set<String> getAllUsers(Map<String, List<String>> friendsGraph) {
+        return friendsGraph.keySet();
     }
     private static Map<String, List<String>> buildFriendsGraph(List<List<String>> friends) {
         Map<String, List<String>> friendsGraph = new HashMap<>();
