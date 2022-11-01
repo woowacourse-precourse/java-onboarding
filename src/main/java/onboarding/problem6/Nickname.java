73935b367ff0765d90e7d63dd8aa98d8aebee11d
kkyu0718 <net605@naver.com>
2022-11-01 11:59:09 +0900

    [6] 기능 구현

---
+++ src/main/java/onboarding/problem6/Nickname.java
@@ -0,0 +1,30 @@
+package onboarding.problem6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nickname {
+    private String email;
+    private String name;
+
+    public Nickname(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getToken(){
+        List<String> result = new ArrayList<>(name.length() - 1);
+        for(int i = 0; i < name.length()-1; i++) {
+            result.add(name.substring(i, i+2));
+        }
+        return result;
+    }
+}
---
+++ src/main/java/onboarding/problem6/Problem6.java
@@ -0,0 +1,31 @@
+package onboarding.problem6;
+
+import java.util.*;
+
+public class Problem6 {
+    public static List<String> solution(List<List<String>> forms) {
+        List<String> answer = new ArrayList<>();
+        List<Nickname> nicknameList = new ArrayList<>(forms.size());
+        for (List<String> form: forms){
+            Nickname nickname = new Nickname(form.get(0), form.get(1));
+            nicknameList.add(nickname);
+        }
+        for (int i = 0; i < nicknameList.size(); i++){
+            List<String> token1 = nicknameList.get(i).getToken();
+            for (String token: token1) {
+                for (int j = i+1; j < nicknameList.size(); j++){
+                    List<String> token2 = nicknameList.get(j).getToken();
+                    if(token2.contains(token)){
+                        answer.add(nicknameList.get(i).getEmail());
+                        answer.add(nicknameList.get(j).getEmail());
+                    }
+                }
+            }
+        }
+        Set<String> set = new HashSet<String>(answer);
+        List<String> result = new ArrayList<>(set);
+        Collections.sort(result);
+        return result;
+    }
+
+}
