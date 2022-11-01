ce0f5abdd0a6e0369b554d486d0b77237b37a00e
taehyun <didls1643@gmail.com>
2022-11-01 15:55:14 +0900

    5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
+	public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+       
+		int[] m = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		for(int i = 0; i < m.length; i++) {
+			answer.add(money/m[i]);
+			money %= m[i];
+		}
         return answer;
     }
 }
