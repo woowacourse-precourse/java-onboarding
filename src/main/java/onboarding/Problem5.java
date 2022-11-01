da6586e48eb1fe22c461d354c2546ba96d4243a3
yw824 <yongwoo25@gmail.com>
2022-11-01 09:59:31 +0900

    feat: [Problem5] 화폐 분류 함수 기능 명세
    - 함수명: List<Integer> classifyMoney(int money)
    	- 50000 ~ 1원까지 값을 담은 배열을 만들고
    	- for 문을 순회하면서 높은 화폐 단위로 나눈 몫을
    	- answer 리스트에 추가한다.
    	- 나머지를 다음 순회에서 나눌 값으로 설정한다.
    	- 모든 화폐 단위로 나누면 answer 결과 리스트를 반환

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,19 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        // answer 변수에 각 화폐마다 분류를 완료한 List를 받는다.
+        // classifyMoney(money)
+        return answer;
+    }
+
+    static List<Integer> classifyMoney(int money) {
+        List<Integer> answer = Collections.emptyList();
+
+        // implement
+        // 화폐 단위의 배열을 생성한 후 for문을 돌면서
+        // 각 화폐 단위마다 나눠서 몫을 answer 배열에 저장한다.
+        // 총 저장된 answer 배열을 반환한다.
         return answer;
     }
 }
