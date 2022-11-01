b4a299ae56392291383d3bb9d2c88b8c155750d6
khj <kim37171@cuk.edu>
2022-11-01 20:04:46 +0900

    [Problem1] 페이지 번호로 가장 큰 수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -14,4 +14,15 @@ class Problem1 {
     private static boolean isNotValid(List<Integer> pobi, List<Integer> crong) {
         return Math.abs(pobi.get(0) - pobi.get(1)) > 1 || Math.abs(crong.get(0) - crong.get(1)) > 1;
     }
+
+    private static int getMax(String str) {
+        int sum = 0;
+        int multiply = 1;
+        for (int i = 0; i < str.length(); i++) {
+            int now = str.charAt(i)-'0';
+            sum += now;
+            multiply *= now;
+        }
+        return sum >= multiply ? sum : multiply;
+    }
 }
