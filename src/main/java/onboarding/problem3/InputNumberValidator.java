24a08d6e25b355bc14c8fcdd03f92c31183da6dc
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-01 14:29:18 +0900

    FIX : Q3 : 상수에 언더바 (_) 추가

--- src/main/java/onboarding/problem3/InputNumberValidator.java
+++ src/main/java/onboarding/problem3/InputNumberValidator.java
@@ -4,7 +4,7 @@ import onboarding.problem3.exception.NumberOutOfSizeException;
 
 public class InputNumberValidator {
     private static final int MIN_INPUT_NUMBER_SIZE = 1;
-    private static final int MAX_INPUT_NUMBER_SIZE = 10000;
+    private static final int MAX_INPUT_NUMBER_SIZE = 10_000;
 
     public void validate(int inputNumber) {
         if(!isSatisfiedInputNumSize(inputNumber)) {
