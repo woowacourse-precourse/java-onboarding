10a2f2972d995142dce260a14d9a14dd0ccc7b06
suhwan <popomarin28@gmail.com>
2022-11-01 16:08:52 +0900

    [problem1] 인자 유효성 검사 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    //유효한 인자 확인
+    public static boolean isVaildInput(List<Integer> list){
+        if (list.get(1) - list.get(0) != 1 || (list.get(0) % 2 != 1) || (list.get(1) % 2 != 0) || list.size() != 2)
+            return false;
+        else if(list.get(0) < 0 || list.get(0) > 400 || list.get(1) < 0 || list.get(1) > 400)
+            return false;
+        else
+            return true;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
