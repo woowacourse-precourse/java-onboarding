3c21c70ebf0988ab80177bf908eb7308655126cf
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 19:47:00 +0900

    refactor: extract method(countCurrency)
    - countCurrency 메소드로 분리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,21 +1,30 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = Collections.emptyList();
 
         int[] currencies = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        answer = countCurrency(money, currencies);
+
+        return answer;
+    }
+
+    private static List<Integer> countCurrency(int money, int[] currencies) {
+        List<Integer> currencyCounts = new ArrayList<>();
 
         for (int currency : currencies) {
             int count = (money < currency) ? 0 : money / currency;
 
-            answer.add(count);
+            currencyCounts.add(count);
             money %= currency;
         }
 
-        return answer;
+        return currencyCounts;
     }
 }
