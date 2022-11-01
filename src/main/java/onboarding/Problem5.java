888ff781a527e8c07d456298ef1883c247af7f85
d0ri123 <lsh81550@gmail.com>
2022-11-01 19:07:08 +0900

    Problem5 refactoring

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,25 +1,18 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) throws UnsupportedOperationException{
-        try {
-            List<Integer> answer = Collections.emptyList(); //TODO:Collections.emptyList가 무엇인가?
+    public static List<Integer> solution(int money){
+        List<Integer> answer = new ArrayList<>();
 
-            int[] cash = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-            answer = new ArrayList<>();
-            for (int i = 0; i < cash.length; i++) {
-                int ae = (money / cash[i]);
-                answer.add(ae);
-                money -= (cash[i] * ae);
-            }
-            return answer;
-        } catch (UnsupportedOperationException e){
-            System.out.println("Not implemented, yet");
+        int[] cash = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i = 0; i < cash.length; i++) {
+            int ae = (money / cash[i]);
+            answer.add(ae);
+            money -= (cash[i] * ae);
         }
-        return null;
+        return answer;
     }
 }
