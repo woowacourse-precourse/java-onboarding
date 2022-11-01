dad9ab975d0aae0851b174199eac177b62997c2e
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-01 14:55:34 +0900

    [Problem1] 승부에서 카운트 값 갱신하여 결과 도출 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,26 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiCount = 0;
+        int crongCount = 0;
+        int tieCount = 0;   // 무승부 카운트
+
+        for (int i = 0; i < 2; i++) {
+            if (maxNum(String.valueOf(pobi.get(i))) > maxNum(String.valueOf(crong.get(i)))) {
+                pobiCount++;
+            } else if (maxNum(String.valueOf(pobi.get(i))) < maxNum(String.valueOf(crong.get(i)))) {
+                crongCount++;
+            } else if (maxNum(String.valueOf(pobi.get(i))) == maxNum(String.valueOf(crong.get(i)))) {
+                tieCount++;
+            }
+        }
+
+        if(pobiCount == 2) answer = 1;
+        else if (crongCount == 2) answer = 2;
+        else if (tieCount == 2) answer = 0;   // 무승부
+        else answer = -1;
+
         return answer;
     }
     // 가장 큰 수 반환 메서드
