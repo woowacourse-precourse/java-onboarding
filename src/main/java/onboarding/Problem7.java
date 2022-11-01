0a20c2d52090c517e43fb39474d746d5a31669d3
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-01 19:18:51 +0900

    [P7] user의 친구 찾기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        HashMap<String, Integer> refArr = new HashMap<>();
+        List<String> friendArr = new ArrayList<>();
+        String userfriend;
+        int score =0;
+
+        // 1. user의 친구 찾기
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                friendArr.add(friend.get(0));
+            }
+        }
         return answer;
     }
 }
