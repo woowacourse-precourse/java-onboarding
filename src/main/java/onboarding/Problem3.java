c612566361c6240e3e205b9d00794f54148d9c9c
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-01 16:38:38 +0900

    fix: Problem3 결과값
    요구사항에 명시되어 있는 3, 6, 9을 세는 방법을 고려 하지 않아 수정함.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,18 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer;
-        /*
-        * 1. 0~9까지 박수는 3번 친다.
-        * 2. 10으로 나는 몫 * 3을 구한다.
-        * 3. 10으로 나눈 나머지를 3으로 나누면 몇번 박수를 치는지 알 수 있다.
-        * 4. 2번 + 3번을 한다.
-         */
-        int share = number / 10;
-        int remainder = number % 10;
-
-        answer = (share * 3) + (remainder / 3);
-
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            int k = i;
+            while (k > 0) {
+                int digit = k % 10;
+                if (digit == 3 || digit == 6 || digit == 9) {
+                    answer++;
+                }
+                k /= 10;
+            }
+        }
         return answer;
     }
 
