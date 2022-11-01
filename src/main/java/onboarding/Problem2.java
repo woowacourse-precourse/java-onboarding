a42b4f2fc292a113251751aade3a72473c4e3460
seonghbae <fullbloomh3ck@gmail.com>
2022-11-01 22:37:40 +0900

    문제 2번 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int prev = cryptogram.length() + 1, next = cryptogram.length();
+        String pattern = "aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz";
+        String tmp = cryptogram;
+        while(next > 0 && prev > next) {
+            tmp = tmp.replaceAll(pattern, "");
+            prev = next;
+            next = tmp.length();
+        }
+        return tmp;
     }
 }
