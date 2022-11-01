1eb691ef47139fe3d1549c4b62492a1c680c9106
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2022-11-01 21:25:23 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        char before = '@';
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            Character word = cryptogram.charAt(i);
+            if (!stack.empty()){
+                if (stack.peek() == word || before == word){
+                    stack.pop();
+                }
+                else {
+                    stack.push(word);
+                }
+                before = word;
+            }
+            else{
+                stack.push(word);
+            }
+        }
+
+        while (!stack.empty())
+            answer += stack.pop();
+
+        StringBuffer sb = new StringBuffer(answer);
+        String reversedStr = sb.reverse().toString();
+
+        return reversedStr;
     }
 }
