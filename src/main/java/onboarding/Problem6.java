618e965a2b05f690cf4c287fec92de86ee8dfb4a
박세원 <tpdnjs1825@naver.com>
2022-11-01 23:16:11 +0900

    Feat : 닉네임에 중복이 있는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,38 @@
 package onboarding;
 
-import java.util.List;
+import javax.swing.*;
+import java.util.*;
 
 public class Problem6 {
+    private static final int EMAIL = 0;
+    private static final int NICKNAME = 1;
+
+    private static void selectCrew(List<List<String>> forms, HashMap<String, String> nicknameEmailMap, HashSet<String> duplicateCrewEmailList) {
+        for (int i = 0; i < forms.size(); i++) {
+            String crewNickname = forms.get(i).get(NICKNAME);
+            String email = forms.get(i).get(EMAIL);
+            makeWordOfNickname(nicknameEmailMap, duplicateCrewEmailList, crewNickname, email);
+        }
+    }
+
+    private static void makeWordOfNickname(HashMap<String, String> nicknameEmailMap, HashSet<String> duplicateCrewEmailList, String nickname, String email) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            char firstCharacter = nickname.charAt(i);
+            char secondCharacter = nickname.charAt(i + 1);
+            String wordOfNickname = String.valueOf(firstCharacter) + String.valueOf(secondCharacter);
+//            checkDuplicate(nicknameEmailMap, duplicateCrewEmailList, wordOfNickname, email);
+        }
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> result = new ArrayList<>();
+        HashMap<String, String> nicknameEmailMap = new HashMap<>();
+        HashSet<String> duplicateCrewEmailList = new HashSet<>();
+
+        selectCrew(forms, nicknameEmailMap, duplicateCrewEmailList);
+
         return answer;
     }
 }
+
