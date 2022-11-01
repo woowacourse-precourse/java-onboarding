26935500ebe0f74e12b0f1a1de903d89869d15be
sylee <sy>
2022-11-01 23:34:48 +0900

    [Feat] : Problem5 변수, 함수 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static int[] exchageList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+
+        answer = getCountOfMoneyList(money);
+
         return answer;
+    }
+
+    public static List<Integer> getCountOfMoneyList(int money) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        return result;
     }
 }
