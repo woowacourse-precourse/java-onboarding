cbff47bd4d68a47455b730af02550a47794b7280
ineiw <75738349+ineiw@users.noreply.github.com>
2022-11-01 23:17:04 +0900

    a

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        StringBuffer sb = new StringBuffer();
+
+        sb.append(cryptogram);
+        String prevString = sb.toString();
+
+        int i=0;
+
+        while (true){
+            for(i=1;i<sb.length();i++){
+                if(sb.charAt(i-1) == sb.charAt(i)){
+                    sb.delete(i-1,i+1);
+                    break;
+                }
+            }
+            System.out.println(prevString);
+            if(prevString.equals(sb.toString()))
+                break;
+            prevString = sb.toString();
+        }
+
+        answer = sb.toString();
         return answer;
     }
 }
