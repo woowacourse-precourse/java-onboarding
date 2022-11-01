1e00f7c76be028704286fe1e0325df10f828a2db
hsh_519 <suk000519@naver.com>
2022-11-01 15:52:02 +0900

    Problem2: 기능 구현(removeDoubleString)

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,32 @@
 package onboarding;
 
+import java.util.*;
 public class Problem2 {
+    public static List<String> removeDoubleString(List<String> arr, int size) {
+        List<String> result = new ArrayList<>();
+        for (int k=0;k<size-1;k++) {
+            if(result.size() != 0) { break; }
+            if(arr.get(k).equals(arr.get(k+1))) {
+                for (int i=0;i<size;i++) {
+                    if(i == 0) {
+                        if (!(arr.get(i+1).equals(arr.get(i)))) {
+                            result.add(arr.get(i));
+                        }
+                    } else if(i == arr.size()-1) {
+                        if (!(arr.get(i).equals(arr.get(i-1)))) {
+                            result.add(arr.get(i));
+                        }
+                    } else {
+                        if (!(arr.get(i+1).equals(arr.get(i))) && !(arr.get(i).equals(arr.get(i-1)))) {
+                            result.add(arr.get(i));
+                        }
+                    }
+                }
+            }
+        }
+        if (result.size() == 0) { return arr; }
+        else { return removeDoubleString(result, result.size()); }
+    }
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
