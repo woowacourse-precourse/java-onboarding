6083db918b3b5ee9de02db334fb0a845d619258e
yuntaeLee <leeyt1201@gmail.com>
2022-11-01 16:13:28 +0900

    feat(Problem5): Money값의 범위 검증 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int MIN_MONEY = 1;
+    private static final int MAX_MONEY = 1000000;
+
     public static List<Integer> solution(int money) {
+        if (isOutOfNumber(money)) {
+            throw new IllegalArgumentException("유효한 돈의 범위를 입력해주세요. (1 ~ 1,000,000원)");
+        }
         return getChangeUnits(money);
     }
 
@@ -18,5 +25,9 @@ public class Problem5 {
         }
 
         return answer;
+    }
+
+    private static boolean isOutOfNumber(int money) {
+        return money < MIN_MONEY || money > MAX_MONEY;
     }
 }
