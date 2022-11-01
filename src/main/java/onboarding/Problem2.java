4d7666bde604d00e91af878c2e5e070f4150324f
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-01 14:47:41 +0900

    (문제2) feat : 연속되는 중복된 문자열을 찾아 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,20 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static String clearString(String answer) {
+
+        char[] chars = answer.toCharArray();
+        char previous = 0;
+        int k = 0;
+
+        for (char c : chars) {
+            if (previous != c) {
+                chars[k++] = c;
+                previous = c;
+            }
+        }
+
+        return new String(chars).substring(0, k);
+    }
 }
