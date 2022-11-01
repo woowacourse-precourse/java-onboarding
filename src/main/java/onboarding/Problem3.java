b6e4ce5d8517457b333c8a40bf633015364c6649
hsh_519 <suk000519@naver.com>
2022-11-01 15:57:05 +0900

    Problem3: 기능 구현(countThreeSixNine)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+    public  static int countThreeSixNine(int num) {
+        int count = 0;
+
+        for(int i=1;i<=num;i++) {
+            String sNum = Integer.toString(i);
+            count += sNum.length() - sNum.replace("3","").length();
+            count += sNum.length() - sNum.replace("6","").length();
+            count += sNum.length() - sNum.replace("9","").length();
+        }
+        return count;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
