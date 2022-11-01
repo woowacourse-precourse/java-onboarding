c90bfaae9c32a6f96b11f21ddbbc4e49bebfe08d
cmw9957 <cmw9957@gmail.com>
2022-11-01 21:48:19 +0900

    add: solution method 내용 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -142,7 +142,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-
+        answer = findRecommendFriends(user, friends, visitors);
 
         return answer;
     }
