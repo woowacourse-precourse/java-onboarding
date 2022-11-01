46ab18390f7ec4139f8225a128b07f8e7716540b
jhsseonn <hosu0125@gmail.com>
2022-11-01 22:33:45 +0900

    feat: (Problem7) 나와 친구인 유저의 아이디 set 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        HashMap<String, Integer> recommendList = new HashMap<>();
+
+        //나와 친구인 유저의 아이디 set 만들기
+        Set<String> friendSet = new HashSet<>();
+        for (List<String> list: friends) {
+            if(list.contains(user)) {
+                friendSet.add(list.get(0));
+                friendSet.add(list.get(1));
+            }
+        }
+        friendSet.remove(user);
+
         return answer;
     }
 }
