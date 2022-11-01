0d80ac06bbb064a7a376d5045b3a38db0ffb9be9
cuttlefish55 <funade7@gmail.com>
2022-11-01 12:32:10 +0900

    5번문제 구현완료.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,11 +2,16 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-
+        List<Integer> answer = new ArrayList();
+        int[] spliter =new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for(int i = 0; i < spliter.length; i ++ ){
+            answer.add(money/spliter[i]);
+            money = money%spliter[i];
+        }
         return answer;
     }
 }
