9661949ff730b14dde6aa5cfc260b25ec4b9c08f
SproutMJ <mjk54565@gmail.com>
2022-11-01 21:04:53 +0900

    feat: 치환용 hashMap 생성기 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    static HashMap<Character, Character> makeCharacterHash(){
+        HashMap<Character, Character> hashMap = new HashMap<>();
+        for (char ch = 'A'; ch<='Z'; ch++){
+            hashMap.put(ch, (char) ('Z' - ch + 1));
+        }
+        for (char ch = 'a'; ch<='z'; ch++){
+            hashMap.put(ch, (char) ('z' - ch + 1));
+        }
+        return hashMap;
     }
 }
