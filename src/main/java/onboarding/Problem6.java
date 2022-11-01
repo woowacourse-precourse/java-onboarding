44cb796c808fd2d4b8d3d30fa1e9c68172cdb391
limjimin <dkdldsla0000@naver.com>
2022-11-01 23:28:57 +0900

    Refactor(Problem6): 제한사항 제거 제한사항 제거 및 자료구조 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,14 +5,9 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
     private static final int DUPLICATED_COUNT = 2;
-    private static final int MIN_EMAIL_SIZE = 11;
-    private static final int MAX_EMAIL_SIZE = 20;
-    private static final int MIN_NICKNAME_SIZE = 1;
-    private static final int MAX_NICKNAME_SIZE = 20;
-    private static final String EMAIL_DOMAIN = "@email.com";
 
     public static List<String> solution(List<List<String>> forms) {
-        Map<String, Set<String>> nicknames = createNicknameMap(forms);
+        Map<String, List<String>> nicknames = createNicknameMap(forms);
         Set<String> duplicateEmails = getDuplicateEmails(nicknames);
         return sortAnswer(duplicateEmails);
     }
@@ -23,7 +18,7 @@ public class Problem6 {
                 .collect(Collectors.toList());
     }
 
-    private static Set<String> getDuplicateEmails(Map<String, Set<String>> nicknameDict) {
+    private static Set<String> getDuplicateEmails(Map<String, List<String>> nicknameDict) {
         Set<String> duplicatedEmails = new HashSet<>();
 
         nicknameDict.forEach((nickname, emails) -> {
@@ -35,25 +30,22 @@ public class Problem6 {
         return duplicatedEmails;
     }
 
-    private static Map<String, Set<String>> createNicknameMap(List<List<String>> forms) {
-        Map<String, Set<String>> nicknames = new HashMap<>();
+    private static Map<String, List<String>> createNicknameMap(List<List<String>> forms) {
+        Map<String, List<String>> nicknames = new HashMap<>();
 
         for (List<String> form : forms) {
             String email = form.get(0);
             String nickname = form.get(1);
-            if (!isValidEmail(email) || !isValidNickname(nickname)){
-                continue;
-            }
 
-            List<String> twoLetterNicknames = splitTwoLetter(nickname);
+            Set<String> twoLetterNicknames = splitTwoLetter(nickname);
             storeEmails(nicknames, email, twoLetterNicknames);
         }
 
         return nicknames;
     }
 
-    private static List<String> splitTwoLetter(String nickname) {
-        List<String> twoLetterNicknames = new ArrayList<>();
+    private static Set<String> splitTwoLetter(String nickname) {
+        Set<String> twoLetterNicknames = new HashSet<>();
 
         for (int i = 0; i < nickname.length() - 1; i++) {
             String twoLetterNickname = nickname.substring(i, i + 2);
@@ -63,40 +55,11 @@ public class Problem6 {
         return twoLetterNicknames;
     }
 
-    private static void storeEmails(Map<String, Set<String>> nicknames, String email, List<String> twoLetterNicknames) {
+    private static void storeEmails(Map<String, List<String>> nicknames, String email, Set<String> twoLetterNicknames) {
         for (String twoLetterNickname : twoLetterNicknames) {
-            Set<String> emails = nicknames.getOrDefault(twoLetterNickname, new HashSet<>());
+            List<String> emails = nicknames.getOrDefault(twoLetterNickname, new ArrayList<>());
             emails.add(email);
             nicknames.put(twoLetterNickname, emails);
         }
-    }
-
-    private static boolean isValidEmail(String email) {
-        int emailLength = email.length();
-        int domainLength = EMAIL_DOMAIN.length();
-
-        if (email.length() < MIN_EMAIL_SIZE || email.length() >= MAX_EMAIL_SIZE) {
-            return false;
-        }
-
-        if (!email.startsWith(EMAIL_DOMAIN, emailLength - domainLength)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private static boolean isValidNickname(String nickname) {
-        String nicknameExp = "^[ㄱ-ㅎ가-힣ㅏ-ㅣ]+$";
-
-        if (nickname.length() < MIN_NICKNAME_SIZE || nickname.length() >= MAX_NICKNAME_SIZE) {
-            return false;
-        }
-
-        if (!nickname.matches(nicknameExp)) {
-            return false;
-        }
-
-        return true;
     }
 }
