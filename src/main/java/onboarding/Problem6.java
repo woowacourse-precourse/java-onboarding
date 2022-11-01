97455da3ee0db71a8b03d6d81e74fff816972aeb
hyejuc <chjjudy@naver.com>
2022-11-01 17:38:15 +0900

    splitToTwoLetters - 닉네임을 2글자씩 쪼개서 List에 담음

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        HashMap<String, String> profile = new HashMap<>();
+        for (int i = 0; i < forms.size(); i++) {
+            profile.put(forms.get(i).get(1), forms.get(i).get(0));
+        }
+        List<String> nicknames = new ArrayList<>(profile.keySet());
+        List<List<String>> duplicateNicknames = new ArrayList<>();
+        for (int i = 0; i < nicknames.size(); i++) {
+            List<String> twoLetters = splitToTwoLetters(nicknames.get(i));
+        }
+
+        return emails;
     }
+    public static List<String> splitToTwoLetters(String nickname){
+        List<String> nicknameTwoLetters = new ArrayList<>();
+        for (int i = 0; i < nickname.length()-1; i++) {
+            String twoLetter = nickname.substring(i,i+2);
+            nicknameTwoLetters.add(twoLetter);
+        }
+        return nicknameTwoLetters;
+    }
+
 }
