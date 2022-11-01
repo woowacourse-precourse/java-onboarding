e6f516a9e3673a94887d171ad4c71868c4909a60
seungjun <seungjun123>
2022-11-01 13:39:10 +0900

    코인을 금액순으로 정렬해놓은 리스트와 코인의 갯수를 체크할 리스트 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        List<Integer> coin = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> cnt = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0));
+
         return answer;
     }
 }
