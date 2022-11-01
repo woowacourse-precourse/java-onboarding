e152171a8c93f6b1964f8f9951c197402c15f53b
rvlwldev <rvlwlasd77@gmail.com>
2022-11-01 14:40:17 +0900

    문제7 : 사용자 친구들의 관계정보를 Map으로 반환하는 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -33,6 +35,35 @@ public class Problem7 {
             res.remove(user);
 
             return List.copyOf(res);
+        }
+
+        /**
+         * 사용자의 친구목록에서 모든 친구에 대한 관계정보를 Map으로 반환한다.
+         * 
+         * @param user        친구를 추천받을 사용자 아이디
+         * @param userFriends 사용자의 모든 친구목록
+         * @param friends     친구관계정보
+         * @return {@code HashMap}user를 제외한 user의 1:N의 모든 친구관계정보
+         */
+        public HashMap<String, List<String>> getUsersFriendsRelationMap(String user,
+                List<String> userFriends,
+                List<List<String>> friends) {
+
+            HashMap<String, List<String>> res = new HashMap<>();
+
+            for (List<String> relation : friends) {
+                String friend = relation.get(0);
+                String withWho = relation.get(1);
+
+                List<String> friendsList = res.getOrDefault(friend, new ArrayList<>());
+
+                friendsList.add(withWho);
+                friendsList.remove(user);
+
+                res.put(friend, friendsList);
+            }
+
+            return res;
         }
     }
 }
