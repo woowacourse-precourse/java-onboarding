724058b131177372b70ac19430d2c600b388c52c
ahinkim <ahin98@naver.com>
2022-11-01 11:08:50 +0900

    refactor(problem3.java): Add function that searches from 1 to nubmer and counts the number of 3, 6, 9 in the number [feature2]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+    // 2. 1~nubmer까지 탐색하며 탐색하고 있는 숫자에 3, 6, 9가 몇 개 들어가는 지 센다.
+    public static int solution(int number) {
+        int answer = 0;
+        for(int i = 1; i <= number; i++){
+            String num = Integer.toString(i);
+            answer += countChar(num, '3');
+            answer += countChar(num, '6');
+            answer += countChar(num, '9');
+        }
+        return answer;
+    }
     // 1. 문자열에서 특정 문자 개수를 센다.
     public static int countChar(String str, char ch) {
         int count = 0;
