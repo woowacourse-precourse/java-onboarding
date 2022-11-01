3ea8c4b24b3c5fc0b7000f140c4ecd02917a7321
임원정 <LWJ@MacBookPro.local>
2022-11-01 18:21:41 +0900

    feature : problem2 동일한 문자가 연속되어 있는지 확인하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem2 {
+    private static int start, end;
+    private static boolean flag;
+    private static char character;
+    
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static boolean compareIndex(char firstCharacter, char secondCharacter, int index) {
+        if (firstCharacter == secondCharacter) {
+            if (!flag) {
+                flag = true;
+                start = index;
+                end = index + 1;
+                character = firstCharacter;
+                return false;
+            }
+            if (firstCharacter == character) {
+                end = index + 1;
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }
