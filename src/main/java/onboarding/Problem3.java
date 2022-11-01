6bbeb880070a0469d04051ba8318645e5b598bce
박세원 <tpdnjs1825@naver.com>
2022-11-01 22:57:47 +0900

    Feat : 1부터 number까지 필요한 총 손뼉 수를 출력하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -18,6 +18,11 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += countNumber369(i);
+        }
+
         return answer;
     }
 }
