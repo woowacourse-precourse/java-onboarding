cafec6b7e4a1328a50e158cc8d0c32a0e0c11ce5
kjg1514 <kjg1514@naver.com>
2022-11-01 21:27:21 +0900

    문제2번

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,32 @@
 package onboarding;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = decryption(cryptogram);
+
         return answer;
+    }
+
+    public static String decryption(String a) {
+        StringBuilder sb = new StringBuilder(a);
+
+        for(int i=1; i<sb.length();) {
+            char left = sb.charAt(i-1);
+            char right = sb.charAt(i);
+
+            if(left == right) {
+                sb.delete(i-1, i+1);
+                if(i!=1) {
+                    i=1;
+                }
+            }else {
+                i+=1;
+            }
+            System.out.println(sb.toString()+" " + i);
+        }
+
+        return sb.toString();
     }
 }
