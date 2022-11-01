a8f08ad4f74a1a5dd7aa76cdc0368287294b9835
이경범 <tkdwk567@naver.com>
2022-11-01 14:25:39 +0900

    # 1 solution @@ 코드 문제점 보안 예정 - TODO LIST 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,33 +1,35 @@
 package onboarding;
 
 import onboarding.problem1.ConditionValue;
-import onboarding.problem1.ProblemOneValidation;
+import onboarding.problem1.infra.PageNumberAndSizeValidation;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Problem1 {
 
-    private static final ProblemOneValidation VALIDATION = new ProblemOneValidation();
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        VALIDATION.settingFixedPageSize(ConditionValue.FIXED_LIST_SIZE.getValue());
-        if (!VALIDATION.isPageSize(pobi.size(), crong.size())) {
+        PageNumberAndSizeValidation validation = new PageNumberAndSizeValidation();
+        validation.settingFixedPageSize(ConditionValue.FIXED_LIST_SIZE.getValue());
+
+        // TODO 요청 데이터 검증 로직 예외 처리 하는 것이 낫다고 봄
+        if (!validation.isPageSize(pobi.size(), crong.size())) {
             return ConditionValue.EXCEPTION_VALUE.getValue();
         }
 
-        int pobiMaxValue = findMaxValue(pobi);
-        int crongMaxValue = findMaxValue(crong);
+        int pobiMaxValue = findMaxValue(pobi, validation);
+        int crongMaxValue = findMaxValue(crong, validation);
 
         return getResult(pobiMaxValue, crongMaxValue);
     }
 
-    protected static int findMaxValue(List<Integer> woowahanTeachers) {
+    // TODO 비지니스 로직이니 service 코드로 분리
+    protected static int findMaxValue(List<Integer> woowahanTeachers, PageNumberAndSizeValidation validation) {
         Integer leftPageNumber = woowahanTeachers.get(ConditionValue.LEFT_INDEX.getValue());
         Integer rightPageNumber = woowahanTeachers.get(ConditionValue.RIGHT_INDEX.getValue());
 
-        VALIDATION.settingPageNumber(leftPageNumber, rightPageNumber);
-        if (!VALIDATION.isPageNumberValidation()) {
+        validation.settingPageNumber(leftPageNumber, rightPageNumber);
+        if (!validation.isPageNumberValidation()) {
             return ConditionValue.EXCEPTION_VALUE.getValue();
         }
 
@@ -37,6 +39,7 @@ public class Problem1 {
         return Math.max(sumMaxValue, multiplicationMaxValue);
     }
 
+    // TODO 비지니스 로직이니 service 코드로 분리
     protected static int getSumMaxValue(Integer leftPageNumber, Integer rightPageNumber) {
         int leftSumValue = getSumValue(leftPageNumber);
         int rightSumValue = getSumValue(rightPageNumber);
