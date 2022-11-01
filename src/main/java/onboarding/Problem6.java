e9985572c1968f389ff847282e7957fe73e396ad
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-01 16:33:14 +0900

    comment: P6 기능 목록 주석 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,7 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 1: 이메일, 이름을 가져와 주는 함수 작성
+ * 기능 2: 중복 닉네임을 판단하여 이메일을 담는 함수 작성
+ * 기능 3: 중복 제거하고 정렬해주는 함수 작성
+ */
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
