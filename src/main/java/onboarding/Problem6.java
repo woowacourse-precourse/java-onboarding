6a1895a0ce19b5bae0821cc67b3dadaef07c93b6
judyi19997 <judyi1999@kookmin.ac.kr>
2022-11-01 20:59:44 +0900

    fix/ 문제 6) 요구 기능 목룍 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,12 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
     }
 }
