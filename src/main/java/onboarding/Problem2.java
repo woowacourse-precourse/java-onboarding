a89c7914e5e49385994c92c6e50c7b308462be6c
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-01 17:55:36 +0900

    feat(PROBLEM2): 연속된 중복 문자가 존재할 경우, 삭제한 결과를 반환하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -18,5 +18,20 @@ public class Problem2 {
         return false;
     }
 
+    //2. 연속된 중복 문자가 존재할 경우, 삭제한 결과를 반환하는 기능
+    static String removeRepetition(String str){
+        String result = "";
+        for (int i=0 ; i<str.length()-1 ; i++){
+            if (str.charAt(i) != str.charAt(i+1)){
+                result += str.charAt(i);
+            } else {
+                i++;
+            }
+        }
+        if (str.length() > 2){
+            result += str.charAt(str.length()-1);
+        }
+        return result;
+    }
 }
 
