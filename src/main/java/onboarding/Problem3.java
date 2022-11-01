f17843b49b91cd86031ed644cc55b6624fa67119
wxxhyeong <wxxhyeong@gmail.com>
2022-11-01 20:24:38 +0900

    [Problem3] feat : countNumber() 추가 및 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        answer = countNumber(number);
+        return answer;
+    }
+
+    static int countNumber(int number) {
+
+        String str;
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            str = Integer.toString(i);
+            String[] strArr = str.split("");
+            List<String> num = Arrays.asList(strArr);
+
+            answer += Collections.frequency(num, "3");
+            answer += Collections.frequency(num, "6");
+            answer += Collections.frequency(num, "9");
+
+        }
+
         return answer;
     }
 }
