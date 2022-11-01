7f126c7d490518a66b9f5dd44ae2b3f29dc72c5b
yenawee <yaena0319@naver.com>
2022-11-01 15:52:20 +0900

    [REFAC] 1. solution 메소드 제외하고 private 으로 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,13 +11,13 @@ class Problem1 {
             return 0;
         return (pobi_num > crong_num)? 1:2;
     }
-    public static int getbignumber(List<Integer> list){
+    private static int getbignumber(List<Integer> list){
         int leftpage = Math.max(addnumber(list.get(0)), multi_number((list.get(0))));
         int rightpage = Math.max(addnumber(list.get(1)), multi_number((list.get(1))));
         return Math.max(leftpage, rightpage);
     }
 
-    public static int addnumber(int n){
+    private static int addnumber(int n){
         int sum = 0;
         while (n > 0){
             sum += n % 10;
@@ -26,7 +26,7 @@ class Problem1 {
         return sum;
     }
 
-    public static boolean is_valid(List<Integer> list){
+    private static boolean is_valid(List<Integer> list){
         if (list.get(0) < 1 || list.get(0) > 400 || list.get(0) % 2 == 0
                 || list.get(1) < 1 || list.get(1) > 400 || list.get(1) % 2 == 1)
             return false;
@@ -35,7 +35,7 @@ class Problem1 {
         return true;
     }
 
-    public static int multi_number(int n){
+    private static int multi_number(int n){
         int sum = 1;
         while (n > 0){
             sum *= n % 10;
