79f1b3f5c6cdac1fd85b814b0ecab25b6c95b77b
임원정 <LWJ@MacBookPro.local>
2022-11-01 18:09:34 +0900

    feature : 예외 처리 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,30 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int EXCEPTION = -1;
+    private static final int DRAW = 0;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+
+        if (handlingExceptions(pobi) || handlingExceptions(crong))
+            return EXCEPTION;
+
         return answer;
+    }
+
+    private static boolean handlingExceptions(List<Integer> player) {
+        if (player.size() != 2)
+            return true;
+        if (player.get(0) % 2 != 1 || player.get(1) % 2 != 0)
+            return true;
+        if (player.get(1) - player.get(0) != 1)
+            return true;
+        if (player.get(0) < 2 || 399 < player.get(1))
+            return true;
+        return false;
     }
 }
