46af8302291403746d4bbc3b7fbeb89f60dc2178
juno-junho <ssmm0205@naver.com>
2022-11-01 19:08:34 +0900

    feat(Problem1): add GamePlayer class
    페이지를 GamePlayer로 변환하며 유효성 검사하는 메서드

---
+++ src/main/java/onboarding/problem1/GamePlayer.java
@@ -0,0 +1,19 @@
+package onboarding.problem1;
+
+import java.util.List;
+
+public class GamePlayer {
+
+    private final List<Integer> pages;
+    private final PageValidator pageValidator;
+
+    public GamePlayer(List<Integer> pages) {
+        pageValidator = new PageValidator();
+        pageValidator.validate(pages);
+        this.pages = pages;
+    }
+
+    public List<Integer> getPages() {
+        return pages;
+    }
+}
