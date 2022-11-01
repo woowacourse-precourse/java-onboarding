1b25f163c6a828bb0685b06d006515bfbfb1dc31
jeomxon <jeomxon@gmail.com>
2022-11-01 22:18:29 +0900

    feat(Problem7): get user friend list

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        //최대 5명까지 점수 높은 순으로 정렬하여 리턴
+        //점수가 0이면 추천하지 않음
+        //점수가 같으면 이름 순으로 정렬
+        List<String> userFriends = getUserFriends(user, friends);
+
         return answer;
     }
+
+    public static List<String> getUserFriends(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+
+        for (List<String> relation : friends) {
+
+            if (relation.get(0).equals(user)) {
+                userFriends.add(relation.get(1));
+            }
+
+            if (relation.get(1).equals(user)) {
+                userFriends.add((relation.get(0)));
+            }
+
+        }
+
+        return userFriends;
+    }
+
 }
