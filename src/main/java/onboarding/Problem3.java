c26a4f538a4066daacb8186593b12d72ef00d0ca
GeonTae Park <rssr96@naver.com>
2022-11-01 14:02:30 +0900

    [feat]3번 기능구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+
 public class Problem3 {
+    static int result;
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        result =0;
+        for(int i=1; i<=number; i++){
+            calculration(i);
+        }
+        return result;
+    }
+
+
+    private static void calculration(int i) {
+        String a = String.valueOf(i);
+
+        for(int s=0; s<a.length(); s++){
+            calculration2(a.charAt(s));
+        }
+    }
+
+    private static int calculration2(char charAt) {
+        if(charAt == '3' || charAt == '6' || charAt == '9'){
+            result++;
+        }
+        return result;
     }
 }
