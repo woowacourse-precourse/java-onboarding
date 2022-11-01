203c1b0900a8785105eb1067419abd9d8300aefb
idoburnish <doyun7433@naver.com>
2022-11-01 21:02:40 +0900

    feat(PROBLEM7): 전체 친구 목록 Map 생성
    전체적으로 각각의 사용자에 대한 친구 목록이 담겨 있는 Map 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        Map<String, ArrayList<String>> friendMap = makeFriendMap(friends);
+
+
         return answer;
+    }
+
+    public static Map<String, ArrayList<String>> makeFriendMap(List<List<String>> friends) {
+        HashMap<String, ArrayList<String>> friendMap = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String friendA = friend.get(0);
+            String friendB = friend.get(1);
+
+            ArrayList<String> AFriends = friendMap.getOrDefault(friendA, new ArrayList<>());
+            AFriends.add(friendB);
+            friendMap.put(friendA, AFriends);
+
+            ArrayList<String> BFriends = friendMap.getOrDefault(friendB, new ArrayList<>());
+            BFriends.add(friendA);
+            friendMap.put(friendB, BFriends);
+        }
+
+        return friendMap;
     }
 }
