ad9b05bd94508691bb9bd092c56da48158ead408
ddukddaki <rlfrkdms1@naver.com>
2022-11-01 16:36:37 +0900

    comment(Problem4): 기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 문자를 변환하는 기능
+ *      1. 대문자라면 155에서 아스키 코드를 빼 반환하는 기능
+ *      2. 소문자라면 219에서 아스키 코드를 빼 반환하는 기능
+ *      3. 문자가 아니라면 그냥 반환하는 기능
+ * 2. 변환된 문자를 합치는 기능
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
