8b6e00f49bce9e0d9df24158399fae93dde49abf
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-01 20:19:29 +0900

    feat: Problem 7, complete solution
    need to refactoring

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem7 {
     static Map<String, Integer> userIndexMappingMap = new HashMap<String, Integer>();
@@ -17,34 +14,70 @@ public class Problem7 {
             addMap(friends.get(i).get(0));
             addMap(friends.get(i).get(1));
         }
+        for(int i = 0; i < visitors.size(); i++){
+            addMap(visitors.get(i));
+        }
         int userNumber = userIndexMappingMap.size();
-        int[] visitor = new int[userNumber];
+        int[] visitorArray = new int[userNumber];
         int[][] friendMatrix = new int[userNumber][userNumber];
 
+
         for(int i = 0; i < visitors.size(); i++){
-            visitor[userIndexMappingMap.get(visitors.get(i))]++;
+            visitorArray[userIndexMappingMap.get(visitors.get(i))]++;
         }
         for(int i = 0; i < friends.size(); i++){
             friendMatrix[userIndexMappingMap.get(friends.get(i).get(0))][userIndexMappingMap.get(friends.get(i).get(1))] = 1;
             friendMatrix[userIndexMappingMap.get(friends.get(i).get(1))][userIndexMappingMap.get(friends.get(i).get(0))] = 1;
         }
 
-        
+        Map<Integer, Integer> recommendScore = new HashMap<Integer, Integer>();
+        for(int i = 0; i < userNumber; i++){
+            recommendScore.put(i,0);
+        }
+
+        for(int i = 1; i < userNumber; i++){
+            recommendScore.put(i,recommendScore.get(i)+visitorArray[i]);
+        }
+        for(int i = 1; i <userNumber; i++){
+            for(int j = 1; j < userNumber; j++){
+                if(i != j && friendMatrix[0][j] == 1 && friendMatrix[i][j] == 1){
+                    recommendScore.put(i,recommendScore.get(i)+5);
+                }
+            }
+        }
+        List<Integer> keySet = new ArrayList<>(recommendScore.keySet());
+        keySet.sort((o1, o2) -> recommendScore.get(o2).compareTo(recommendScore.get(o1)));
+        List<String> answer = new ArrayList<String>();
+        for(int i = 0; i < userNumber; i++){
+            if(friendMatrix[0][keySet.get(i)] != 1 && keySet.get(i) != 0) {
+                answer.add(getKey(keySet.get(i)));
+            }
+            if(answer.size() == 5){
+                break;
+            }
+
+        }
 
 
-
-
-
-
-
-        List<String> answer = Collections.emptyList();
         return answer;
     }
+
+
+
     public static void addMap(String userName){
         if(!userIndexMappingMap.containsKey(userName)){
             userIndexMappingMap.put(userName,index);
             index++;
         }
+    }
+    public static String getKey(int value) {
+        for (String key : userIndexMappingMap.keySet()) {
+            if (userIndexMappingMap.get(key).equals(value)) {
+                return key;
+
+            }
+        }
+        return "error";
     }
 
 }
