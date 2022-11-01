b8a2c295ccf3e0f065f70c5b143019131c0eb7f8
DongWooE <ehddn977@naver.com>
2022-11-01 18:43:36 +0900

    [p6][add] nickname을 탐색하며 2글자씩 나누는 메소드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,11 +1,14 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
 
     List<List<String>> forms = new ArrayList<>();
+    Map<String, String> map = new HashMap<String, String>();
     public static List<String> solution(List<List<String>> forms) {
         Problem6 p6 = new Problem6();
         p6.forms = forms;
@@ -15,7 +18,14 @@ public class Problem6 {
 
     private void iterate(){
         for(List<String> form : forms){
+            searchNickName(form.get(0), form.get(1));
+        }
+    }
 
+    private void searchNickName(String email, String nickName){
+        for(int i = 0; i < nickName.length()-1; i++){
+            String substring = nickName.substring(i, i + 1);
+            if(!map.containsKey(substring)) map.put(substring, email);
         }
     }
 }
