91c36af9406cf786be54180db7acc9b72c9b686a
eom-tae-in <eti0728@daum.net>
2022-11-01 20:25:00 +0900

    feat: countClapCase() -> 각 자리수 별로 나눠서 총 박수를 치는 횟수를 계산

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,4 +9,21 @@ public class Problem3 {
         }
         return countClapCase(number);
     }
+
+    public static int countClapCase(int number) {
+        int sum = 0;
+        int currentDigitValue;
+        for (int i = 0; i < saveByDigit.length; i++) {
+            currentDigitValue = number / (int) Math.pow(10, (4 - i));
+            saveByDigit[i] = (byte) currentDigitValue;
+            number %= (int) Math.pow(10, (4 - i));
+            if (saveByDigit[i] != 0 && saveByDigit[i] % 3 == 0 && i != 4) {
+                sum += number + 1;
+            }
+        }
+        sum += saveByDigit[0] * tenThousand() + calculateThousandsClapCase(saveByDigit[1]) +
+                calculateHundredsClapCase(saveByDigit[2]) + calculateTensClapCase(saveByDigit[3]) +
+                calculateOnesClapCase(saveByDigit[4]);
+        return sum;
+    }
 }
