70306cc3f89d0e0d53114398ffeafdfcf4e9c39f
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2022-11-01 21:30:19 +0900

    Problem3,4,5,6,7

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,24 @@
 package onboarding;
 
 public class Problem3 {
+    public static int getThreeCnt(int number) {
+        int cnt = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            if (digit == 3 || digit == 6 || digit == 9)
+                cnt += 1;
+            number /= 10;
+        }
+
+        return cnt;
+    }
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i < number + 1; i++)
+            answer += Problem3.getThreeCnt(i);
+
         return answer;
     }
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        List<Character> alphabets = Arrays.asList('a','b','c','d','e','f'
+                ,'g','h','i','j','k','l','m','n','o','p'
+                ,'q','r','s','t','u','v','w','x','y','z');
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+
+        for (int i = 0; i < alphabets.size(); i++){
+            map.put(alphabets.get(i), alphabets.get(25 - i));
+            map.put(alphabets.get(i).toString().toUpperCase().charAt(0), alphabets.get(25 - i).toString().toUpperCase().charAt(0));
+        }
+
+        for (int i = 0; i < word.length(); i++){
+            Character c = word.charAt(i);
+            if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
+                answer += map.get(c);
+            else
+                answer += c;
+        }
+
         return answer;
     }
 }
