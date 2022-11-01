ffc3d2465d50f755793d01c0a86c09104cccd8de
ttaehee <ttaehee1105@gmail.com>
2022-11-01 11:12:07 +0900

    문제5) feat : 1미만 1,000,000 초과인경우 유효성 체크

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,11 +9,22 @@ public class Problem5 {
         List<Integer> answer = new ArrayList<>();
         int[] moneyTypes = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+        if (isNotInRange(money)) {
+            return answer;
+        }
+
         for (int moneyType : moneyTypes) {
             answer.add(money / moneyType);
             money = money % moneyType;
         }
         return answer;
+    }
+
+    private static boolean isNotInRange(int number) {
+        if (number >= 1 && number <= 1000000) {
+            return false;
+        }
+        return true;
     }
 
 }
