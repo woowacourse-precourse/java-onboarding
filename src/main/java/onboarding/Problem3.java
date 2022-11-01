136dc8a7f451d6f58dd987c95b284b4a2afe8beb
Hiluxy <sunset.lhj@gmail.com>
2022-11-01 20:41:57 +0900

    Probelm3 (Update method)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,47 +1,32 @@
 package onboarding;
 
 /**
- * 기능
- * 1. 입력 문자열 길이 검증
- * String에 3,6,9가 들어가는지 확인 2. 3,6,9가 들어가는 숫자들의 개수 계산
+ * 기능 1. 해당 수의 3,6,9 개수 확인
  */
 public class Problem3 {
-    private static final int MIN_LENGTH = 1;
-    private static final int MAX_LENGTH = 1000;
 
     public static int solution(int number) {
-        return clac(number);
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += count(i);
+        }
+        return answer;
     }
 
     /**
-     * 1. 3,6,9가 들어가는지 확인하는 함수
-     *
-     * @param strNum
-     * @return 3, 6, 9가 들어가면 true 반환
+     * 1. 해당 수의 3,6,9 개수 확인
+     * @param num
+     * @return 3, 6, 9가 들어가는 개수 반환
      */
-    static boolean checkGame(String strNum) {
-        for (int i = 0; i < strNum.length(); i++) {
-            if (strNum.charAt(i) == '3' || strNum.charAt(i) == '6' || strNum.charAt(i) == '9') {
-                return true;
+    static int count(int num) {
+        int res = 0;
+        while (num != 0) {
+            int tmp = num % 10;
+            if (tmp == 3 || tmp == 6 || tmp == 9) {
+                res++;
             }
+            num /= 10;
         }
-        return false;
-    }
-
-    /**
-     * 2. 3,6,9가 들어가는 숫자들의 개수 계산 함수
-     *
-     * @param number
-     * @return 손뼉 쳐야하는 수 sum 반환
-     */
-    static int clac(int number) {
-        int sum = 0;
-        for (int num = 1; num < number + 1; num++) {
-            String strNum = Integer.toString(num);
-            if (checkGame(strNum) == true) {
-                sum += 1;
-            }
-        }
-        return sum;
+        return res;
     }
 }
