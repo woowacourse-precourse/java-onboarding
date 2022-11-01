6144ee48efa68d01bb58aca771c1f7714161618a
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:56:39 +0900

    배열 생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
     public static int solution(int number) { // 들어온 int를 범위로 생각하고 배열로 생성한 후, split으로 자른 후에 3, 6, 9가 매치될 때마다 횟수를 증가시키도록
+        int[] arr = new int[number];
         int answer = 0;
+
+        Arrays.setAll(arr, num -> (num + 1)); // scan받은 범위만큼 int형 배열 생성
         return answer;
     }
 }
