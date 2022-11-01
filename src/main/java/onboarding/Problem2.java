093937005af3d1eaeb80d0b93e69ce137d73f512
ParkJoungWoo <jug78@naver.com>
2022-11-01 18:59:25 +0900

    문제 2) 기능 1 : 문자열에서 중복을 제거해 수정된 문자열을 반환하는 기능 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,27 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeRedun(cryptogram);
         return answer;
+    }
+    private static String removeRedun(String right){
+        String left = "";
+        char stand = ' ';
+        for(int i = 0; i < right.length(); i++){
+            if(left.length() > 0) {
+                stand = left.charAt(left.length() - 1);
+                if (stand == right.charAt(i)) {
+                    for (int j = i + 1; j < right.length(); j++) {
+                        if (stand != right.charAt(j))
+                            break;
+                        i++;
+                    }
+                    left = left.substring(0, left.length() - 1);
+                    continue;
+                }
+            }
+            left += right.charAt(i);
+        }
+        return left;
     }
 }
