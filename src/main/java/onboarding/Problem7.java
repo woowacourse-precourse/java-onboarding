66e79fd1895934b0d0982b99fc0a037e673cd3a8
임원정 <LWJ@MacBookPro.local>
2022-11-01 19:54:15 +0900

    feature : problem7 친구의 친구 목록을 구하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,13 +2,32 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> usersFriends = getUsersFriends(user, friends);
+        HashMap<String, Integer> recommendMap = getFriendssFriends(user, friends, usersFriends);
 
         return answer;
+    }
+
+    private static HashMap<String, Integer> getFriendssFriends(String user, List<List<String>> friends, List<String> usersFriends) {
+        HashMap<String, Integer> recommandList = new HashMap<>();
+
+        for (String friendName : usersFriends) {
+            for (List<String> friend : friends) {
+                if (friend.contains(friendName) && !friend.contains(user)) {
+                    if (friend.get(0).equals(friendName)) {
+                        setHashMap(recommandList, friend.get(1));
+                    } else {
+                        setHashMap(recommandList, friend.get(0));
+                    }
+                }
+            }
+        }
+        return recommandList;
     }
 
     private static List<String> getUsersFriends(String user, List<List<String>> friends) {
