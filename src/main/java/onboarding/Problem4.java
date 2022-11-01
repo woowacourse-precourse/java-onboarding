628786af9b3d88283173c4051f161de348230eef
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-01 18:47:24 +0900

    feat: 문자 변환 기능 함수화

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,15 +1,16 @@
 package onboarding;
 
 public class Problem4 {
+    public static char convertWord(char word) {
+        if (Character.isUpperCase(word)) return (char) ('A'+'Z'-word);         //대문자인 경우 변경
+        else if (Character.isLowerCase(word)) return (char) ('a'+'z'-word);    //소문자인 경우 변경
+        return word;
+    }
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
         for(int i=0; i<word.length(); i++) {
             char current = word.charAt(i);  //i번째 index 배열 접근이 많으므로 변수로 선언
-            if (current>='A' & current<='Z' | current>='a' & current<='z') {    //알파벳인 경우
-                if (Character.isUpperCase(current)) current = (char) ('A'+'Z'-current);         //대문자 변경
-                else if (Character.isLowerCase(current)) current = (char) ('a'+'z'-current);    //소문자 변경
-            }
-            answer.append(current);
+            answer.append(convertWord(current));
         }
         return answer.toString();
     }
