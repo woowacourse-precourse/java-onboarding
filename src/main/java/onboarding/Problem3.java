7ed9cdeac043803c0e8aa22c766c6128cd77cf1a
Ellie <doris0707@naver.com>
2022-11-01 18:17:54 +0900

    [Problem 3] solve

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int chk_num = 1; chk_num <= number; chk_num++) {
+            String str = String.valueOf(chk_num);
+
+            for (int i = 0; i < str.length(); i++) {
+                char tmp = str.charAt(i);
+                if (tmp == '3' || tmp == '6' || tmp == '9') {
+                    answer +=1;
+                }
+            }
+        }
+
         return answer;
     }
 }
