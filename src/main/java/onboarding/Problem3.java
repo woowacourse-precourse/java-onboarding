a08355b175bfeda897e926c3b39da5a31e3a01dc
suhwan <popomarin28@gmail.com>
2022-11-01 16:14:50 +0900

    [problem3] 주어진 정수까지 각각 문자열로 만드는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        //1 부터 Number까지 3,6,9의 여부 확인
+        for(int i = 1; i <= number; i++) {
+            String numToStr = "" + i;
+        }
         return answer;
     }
 }
