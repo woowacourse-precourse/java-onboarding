9f03f4e0e0c408291c782f49defae6e9391dec0e
kim1387 <kevink0@naver.com>
2022-11-01 21:40:29 +0900

    feat: 문자열을 2문자씩 중복되는지 확인하는 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,22 +1,33 @@
 package onboarding;
 
+import onboarding.problem6.Crew;
 import onboarding.problem6.Validator;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-
   public static List<String> solution(List<List<String>> forms) {
     Validator.validate(forms);
+
     return new ArrayList<>();
   }
 
-  private static Set<String> subStringNicknameByTwo(String nickname) {
-    Set<String> subStringByTwoNicknameSet = new HashSet<>();
-    int nicknameLength = nickname.length() - 1;
-    for (int i = 0; i < nicknameLength; i++) {
-      subStringByTwoNicknameSet.add(nickname.substring(i, i + 2));
+  private static boolean isContinuousStringByTwo(String base, String target) {
+
+    int baseLength = base.length();
+    int targetLength = target.length();
+    for (int i = 0; i < baseLength - 1; i++) {
+      for (int j = 0; j < targetLength - 1; j++) {
+        if (isOverlappedSection(base, target, i, j)) {
+          return true;
+        }
+      }
     }
-    return subStringByTwoNicknameSet;
+    return false;
+  }
+
+  private static boolean isOverlappedSection(String base, String target, int i, int j) {
+    return base.charAt(i) == target.charAt(j) && (base.charAt(i + 1) == target.charAt(j + 1));
   }
 }
