14bc106160b4937c1a4434ac1ba4858b8888d4d3
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-01 23:48:57 +0900

    [feat] Problem3 각 자리수의 3의 배수 개수 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,7 +5,16 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-
+        for(int i = 1 ; i <= number ; i ++) {
+            int nowNum = i;
+            while (nowNum != 0) {
+                int thisNum = nowNum % 10;
+                if(thisNum % 3 == 0 && thisNum != 0) {
+                    answer++;
+                }
+                nowNum /= 10;
+            }
+        }
         return answer;
     }
 }
