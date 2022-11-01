d2f92f00ccc48e0cd3d1ef54429fb194fcfbdd0c
Plastic_hong <zakkslash18@naver.com>
2022-11-01 19:37:55 +0900

    problem7 1  mrko 의 친구와 친구인 사람 맵 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,6 +7,7 @@ public class Problem7 {
         List<String> answer = new ArrayList<>();
 
         List<String> userFriends = getUserFriends(user, friends);
+        Map<String, Integer> map = getCloseFriends(user, friends, userFriends);
 
         return answer;
     }
@@ -30,6 +31,41 @@ public class Problem7 {
         return userFriends;
     }
 
+    private static Map<String, Integer> getCloseFriends(String user, List<List<String>> friends, List<String> userFriends) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < userFriends.size(); i++) {
+
+            String userFriend = userFriends.get(i);
+
+            for (int j = 0; j < friends.size(); j++) {
+
+                String name1 = friends.get(j).get(0);
+                String name2 = friends.get(j).get(1);
+
+
+                if (name1.equals(userFriend) && !name2.equals(user)) {
+                    //mrko 의 친구의 친구 이면서 이름이 mrko 가 아니면?
+                    if (map.containsKey(name2)) {
+                        map.put(name2, map.get(name2) + 10);
+                    } else {
+                        map.put(name2, 10);
+                    }
+
+                } else if (name2.equals(userFriend) && !name1.equals(user)) {
+                    if (map.containsKey(name1)) {
+                        map.put(name1, map.get(name1) + 10);
+                    } else {
+                        map.put(name1, 10);
+                    }
+                }
+
+            }
+
+
+        }
+        return map;
+    }
 
 
 }
