8c333be0a00797c8dc6a933a264969d4243ee57c
Kayoung <rkdkdudjd@gmail.com>
2022-11-01 15:04:28 +0900

    refactor(problem3): 박수 개수 계산 메서드 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,20 +1,29 @@
 package onboarding;
 
 public class Problem3 {
+    public static final int THREE = 3;
+    public static final int SIX = 6;
+    public static final int NINE = 9;
+
     public static int solution(int number) {
         int answer = 0;
         for (int i = 3; i <= number; i++) {
-            int j = i;
-            do {
-                switch (j % 10) {
-                    case 3:
-                    case 6:
-                    case 9:
-                        answer++;
-                }
-                j /= 10;
-            } while (j != 0);
+            answer += getClapCount(i);
         }
         return answer;
+    }
+
+    public static int getClapCount(int number) {
+        int clap = 0;
+        do {
+            switch (number % 10) {
+                case THREE:
+                case SIX:
+                case NINE:
+                    clap++;
+            }
+            number /= 10;
+        } while (number != 0);
+        return clap;
     }
 }
