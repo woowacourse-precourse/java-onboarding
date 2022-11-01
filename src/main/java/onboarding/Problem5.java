55912d775ead1b5258e946259df8ef8837206685
Lee-SungMin <saevers26@gmail.com>
2022-11-01 20:50:32 +0900

    Add: 5번 문제 기능 구현 - 화폐 및 동전으로 변환하는 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    // 화폐 및 동전 계산
+    public static Integer isUnit(int number, int index){
+
+        int unit = number;
+
+        if(index % 2 == 0){ // 오만, 오천, 오백, 오십
+            unit /= 5;
+        }
+
+        else if(index % 2 == 1){ // 만, 천, 백, 십
+            if(unit == 10){ // 십 -> 일
+                unit /= 10;
+            }
+            else {
+                unit /= 2;
+            }
+        }
+
+        return unit;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
