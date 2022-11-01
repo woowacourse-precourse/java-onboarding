b45a8f68a2b5acdf0aaad99f709ae4e7ff24cb20
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-11-01 21:15:43 +0900

    feat: add main class

---
+++ src/main/java/onboarding/Problem5.java
@@ -0,0 +1,12 @@
+package onboarding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Problem5 {
+
+  public static List<Integer> solution(int money) {
+    return  new ExchangeMoneyPro5(money).divideMoney();
+  }
+}
