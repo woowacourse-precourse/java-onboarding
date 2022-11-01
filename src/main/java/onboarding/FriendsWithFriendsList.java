7bd3d81f05497a2b461167ddbf460dee0292c23a
JohnPrk <uo3641493@gmail.com>
2022-11-01 19:27:11 +0900

    refactor : FriendsWithFriends 클래스 수정

--- src/main/java/onboarding/FriendsWithFriendsList.java
+++ src/main/java/onboarding/FriendsWithFriendsList.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FriendsWithFriendsList {
-    private final UserWithFriendsList userFriendsList;
     private final List<String> friendsOfFriendsList;
 
     public FriendsWithFriendsList(UserWithFriendsList userFriendsList, List<List<String>> friendsList) {
-        this.userFriendsList = userFriendsList;
         this.friendsOfFriendsList = getFriendsOfFriendsList(userFriendsList, friendsList);
     }
 
@@ -24,6 +22,7 @@ public class FriendsWithFriendsList {
                         friendsOfFriendsList.add(friends.get(1));
                     }
                 });
+        System.out.println(friendsOfFriendsList);
         return friendsOfFriendsList;
     }
 }
