d6cb455528eb5f5fa4632975de7c2b8689617af4
youswim <youswim96@gmail.com>
2022-11-01 21:57:30 +0900

    Feat: solution 메서드 완성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,11 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            appendChar(output, word.charAt(i));
+        }
+        return output.toString();
     }
 
-    
+
     // 문자를 규칙에 따라 바꾸고, StringBuilder에 append하는 메서드
     private static void appendChar(StringBuilder output, char c) {
         if ('A' <= c && c <= 'Z') {
