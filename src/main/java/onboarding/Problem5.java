f6e90ae0efa5fcb8d336cadab9e8acd5d1057a15
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 15:05:46 +0900

    feat:static value

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static List<Integer> result = new ArrayList<>();
+    private static List<Integer> coins= List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        return answer;
+        return MoneyNumber(money);
     }
 }
