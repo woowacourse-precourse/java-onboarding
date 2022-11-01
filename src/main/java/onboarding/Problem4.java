83021bec6d322da35a1627163f199398d2788f7d
pyj <les85611@hanyang.ac.kr>
2022-11-01 21:07:43 +0900

    [p4] a와 z의 ascii 숫자를 지정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,18 @@ public class Problem4 {
         char[] wordstrip = word.toCharArray();
         int reverse, abstemp, ascii = 0;
         for (int i = 0; i<wordstrip.length; i++) {
+            if(Character.isUpperCase(wordstrip[i])) {
+                ascii = 65;
+            } else if (Character.isLowerCase(wordstrip[i])) {
+                ascii = 97;
+            } else {
+                answer = answer + ' ';
+                continue;
+            }
+            abstemp = Math.abs(ascii - wordstrip[i]);
+            reverse = (ascii + 25) - abstemp;
+            answer = answer + String.valueOf((char) reverse);
+        
         }
         return answer;
     }
