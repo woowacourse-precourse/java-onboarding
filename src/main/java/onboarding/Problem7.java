0db113c50bad75bfc57d2f0f8745bf39b3f024f9
yeop0740 <ssucgoo@gmail.com>
2022-11-01 19:44:53 +0900

    make method findFriend(Problem7)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,4 +9,20 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static List<String> findFriend(String user, List<List<String>> friends) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).get(0).equals(user)) {
+                result.add(friends.get(i).get(1));
+            }else if (friends.get(i).get(1).equals(user)) {
+                result.add(friends.get(i).get(0));
+            }
+        }
+
+        return result;
+    }
+
+
 }
