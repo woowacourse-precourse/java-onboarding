745ea62cee600ea23c391301ae8c9d5412606d16
splitube <voicesplit@daum.net>
2022-11-01 13:05:43 +0900

    5번 문제 - 기능 목록 수정, 메서드 이름 일부 수정, 오타 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,16 +1,17 @@
 package onboarding;
 
 /**
- * Money_divider
+ * [ Money_divider ]
+ *  - int order[] = 제일 큰 화폐부터 작은 화폐까지 순서대로 나열한 배열
+ *  - List<Integer> result = 변환할 수 있는 수를 나열할 리스트
  *
- * 1. divide_in_order(int money) = 화폐가 큰 순서대로 money를 나눈다.
- * 2. save_result_get_left(int money, int divide) = money 를 divide 크기의 화페로 나눠서 결과를 저장하고 나눈 나머지를 반환
- * 3. save_result(int amount) = 결과를 저장한다.
- * 4. show_result() = 저장된 결과를 반환한다.
+ *  - divide_in_order(int money) = 화폐가 큰 순서대로 money 를 나눈다.
+ *  - save_result_get_left(int money, int divide) = money 를 divide 크기의 화페로 나눠서 결과를 저장하고 나눈 나머지를 반환
+ *  - save_result(int amount) = 결과를 저장한다.
+ *  - show_result() = 저장된 결과를 반환한다.
  */
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
@@ -21,11 +22,11 @@ public class Problem5 {
     }
 }
 
-class Money_divider{
-    List<Integer> result;
+class Money_divider {
     int order[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    List<Integer> result;
 
-    public void divide_in_order(int money){
+    public void divide_in_order(int money) {
         result = new ArrayList<>();
         for (int divide : order) {
             money = save_result_get_left(money, divide);
@@ -37,11 +38,11 @@ class Money_divider{
         return money % divide;
     }
 
-    private void save_result(int amout) {
-        this.result.add(amout);
+    private void save_result(int amount) {
+        this.result.add(amount);
     }
 
-    public List<Integer> show_result(){
+    public List<Integer> show_result() {
         return this.result;
     }
 }
