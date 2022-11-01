941b34429311140cd5fc85fdec08b1b8b670df75
limzzum <chfhddl1234@naver.com>
2022-11-01 14:26:44 +0900

    [문제 7] 친구관계 정보가 담긴 friends를 돌며 friendList에 각자의 친구들 추가.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,10 +5,30 @@ import java.util.*;
 public class Problem7 {
     static Map<String, Integer> scoreCard = new HashMap<>();
     static HashMap<String, Set<String>> friendList = new HashMap<>();
-
+    
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        String friend1 = "";
+        String friend2 = "";
+        for(int i=0; i<friends.size(); i++){
+            friend1 = friends.get(i).get(0);
+            friend2 = friends.get(i).get(1);
+
+            if(friendList.get(friend1)==null)
+                friendList.put(friend1, new HashSet<>() {
+                });
+
+            friendList.get(friend1).add(friend2);
+
+            if(friendList.get(friend2)==null)
+                friendList.put(friend2, new HashSet<>());
+
+            friendList.get(friend2).add(friend1);
+
+        }
 
         List<String> answer = List.of();
         return answer;
     }
+
 }
