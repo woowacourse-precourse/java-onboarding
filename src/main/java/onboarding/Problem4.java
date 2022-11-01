b1fd7f07104cec3f546017fab7e4a704ff7d75bf
broden-horizon <rlghd1698@naver.com>
2022-11-01 21:44:47 +0900

    문제4. 대칭 문자를 찾는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,17 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    public static char getSymmetricChar(char c) {
+        int charIndex = 0;
+        if(c >= 'A' & c <= 'Z'){
+            charIndex = c - 'A';
+            return (char) ('A' + 25 - charIndex);
+        } else if(c >= 'a' && c <= 'z'){
+            charIndex = c - 'a';
+            return (char) ('a' + 25 - charIndex);
+        } else {
+            return c;
+        }
+    }
 }
