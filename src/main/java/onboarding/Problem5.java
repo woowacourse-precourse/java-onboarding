6718b02c5513a869abb397860b25710ae41b3a3d
2do1 <dowon3036@naver.com>
2022-11-01 17:36:46 +0900

    feat: 문제 5번 현재 화페로 받을 수 있는 최대 개수를 구하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,7 +12,9 @@ public class Problem5 {
 
         // 금액이 큰 화폐 순서대로 화폐를 받는 기능
         for (int cash: moneyArr) {
-
+            // 현재 화페로 받을 수 있는 최대 개수를 구하는 기능
+            int cashCount = money / cash;
+            answer.add(cashCount);
         }
         return answer;
     }
