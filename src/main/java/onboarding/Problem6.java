7b46cd814b965e85c2416c25008d1ba968c63a18
YusunPark <solarpy.kor@gmail.com>
2022-11-01 21:43:08 +0900

    Feat : 문제 6 구현 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Set<String> use_word = new HashSet<String>();
+        Set<String> result = new HashSet<String>(new ArrayList<>());
+
+        for (int i = 0; i < forms.size(); i++) {
+            String name = forms.get(i).get(1);
+
+            for (int j = 0; j < name.length() - 2; j++) {
+                String split_name = name.substring(j, j + 2);
+                if (use_word.contains(split_name)) continue;
+
+                for (int k = i; k < forms.size(); k++) {
+                    String compare_name = forms.get(k).get(1);
+
+                    if (i != k && compare_name.contains(split_name)) {
+                        result.add(forms.get(i).get(0));
+                        result.add(forms.get(k).get(0));
+                    }
+                }
+                use_word.add(split_name);
+            }
+        }
+        List<String> answer =new ArrayList<String>(result);
+        Collections.sort(answer);
         return answer;
     }
+
 }
