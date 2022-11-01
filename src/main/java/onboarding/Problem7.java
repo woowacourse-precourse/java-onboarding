8761fdff6c35bbaa52cadf2504759cc04734afae
hyunseo <goatsfish@gmail.com>
2022-11-01 15:14:17 +0900

    feat(Problem7) : 기존 친구 저장 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem7 {
+
+    private static final int FRIEND_A = 0;
+    private static final int FRIEND_B = 1;
+
+    private static List<String> userFriends = new ArrayList<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        userFriends.clear();
+
+        reflectAlreadyUserFriends(user, friends);
     }
+
+    private static void reflectAlreadyUserFriends(String user, List<List<String>> friends) {
+        friends.forEach(friend -> addUserFriend(user, friend.get(FRIEND_A), friend.get(FRIEND_B)));
+    }
+
+    private static void addUserFriend(String userName, String friendAName, String friendBName) {
+        if (friendAName.equals(userName)) {
+            userFriends.add(friendBName);
+        }
+        if (friendBName.equals(userName)) {
+            userFriends.add(friendAName);
+        }
+    }
+
 }
