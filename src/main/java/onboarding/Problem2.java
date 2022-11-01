9af14149fa81c3a62a1991d9b14d19eb3236bad9
lynne921 <dkfls921@gmail.com>
2022-11-01 15:09:44 +0900

    [feat][problem2] 기능 목록 작성 및 중복 판별 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem2 {
+    private static boolean isDuplicate(char first, char second) {
+        return (first == second);
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
