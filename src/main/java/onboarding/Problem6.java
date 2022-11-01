19f2ae7fa94f42a3461d14d478235ce06d101702
JeonghakLee <wjdgkr0527@naver.com>
2022-11-01 11:33:04 +0900

    feat: problem6 solve

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,48 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+
+  private static final Map<String, Set<Integer>> substringMap = new HashMap<>();
+  private static final Set<String> answerSet = new TreeSet<>();
+
+
+  public static List<String> solution(List<List<String>> forms) {
+    makeSubstringMap(forms);
+    getAnswerSet(forms);
+    return new ArrayList<>(answerSet);
+  }
+
+  private static void getAnswerSet(List<List<String>> forms) {
+    for (Entry<String, Set<Integer>> entries : substringMap.entrySet()) {
+      Set<Integer> formIndexes = entries.getValue();
+      if (formIndexes.size() > 1) {
+        for (Integer index : formIndexes) {
+          answerSet.add(forms.get(index).get(0));
+        }
+      }
     }
+  }
+
+  private static void makeSubstringMap(List<List<String>> forms) {
+    for (int formIndex = 0; formIndex < forms.size(); formIndex++) {
+      String nickName = forms.get(formIndex).get(1);
+      for (int i = 0; i < nickName.length() - 1; i++) {
+        String substring = nickName.substring(i, i + 2);
+        Set<Integer> indexes = substringMap.getOrDefault(substring, new HashSet<>());
+        indexes.add(formIndex);
+        substringMap.put(substring, indexes);
+      }
+    }
+  }
 }
