d77276a0f6a7a11d8719dc0e96f66a4ff8528ff4
sypark <soun997@gmail.com>
2022-11-01 14:36:41 +0900

    Problem3 369게임 로직 구현, 정수를 문자열로 변환하여 자릿값 판별

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String strNum;
+
+       for(int i = 1; i <= number; i++) {
+           strNum = Integer.toString(i);
+           for (int j = 0; j < strNum.length(); j++) {
+               if (strNum.charAt(j) == '3' || strNum.charAt(j) == '6' || strNum.charAt(j) == '9') {
+                   answer++;
+               }
+           }
+       }
+
         return answer;
     }
 }
