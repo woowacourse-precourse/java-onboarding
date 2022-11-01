e7ba8b269b4079b3b8aa44cf17d70f4ada6d31b1
jiwon1217 <koagjw@gmail.com>
2022-11-01 15:52:27 +0900

    [Feature] 비교할 이메일, 닉네임 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,6 +10,11 @@ public class Problem6 {
         for (int i = 0; i < forms.size(); i++) {
             String email = forms.get(i).get(0);
             String nickname = forms.get(i).get(1);
+
+            for (int j = i + 1; j < forms.size(); j++) {
+                String comparedEmail = forms.get(j).get(0);
+                String comparedNickname = forms.get(j).get(1);
+            }
         }
         return answer;
     }
