65be684be3d53c915aa62294471eef6cf7f68037
judyi19997 <judyi1999@kookmin.ac.kr>
2022-11-01 15:48:28 +0900

    add/ 문제 2) 반복되는 부분 삭제기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,29 +6,48 @@ import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        List<int[]> answer = check(cryptogram);
-        return "answer";
+        String answer = cryptogram;
+        while(true){
+            List<int[]> removeList = check(answer);
+            if (removeList.size() == 0){
+                break;
+            }
+            answer = remove(removeList,answer);
+            if (answer.length() ==0){
+                break;
+            }
+        }
+        return answer;
     }
 
+    public static String remove(List<int[]> removeList,String cryptogram){
+        StringBuffer stringBuffer = new StringBuffer(cryptogram);
+        for (int i=0; i<removeList.size();i++){ //마지막 수 까지 반복되는 글자인 경우는 전부 삭제
+            if (cryptogram.length() <= removeList.get(i)[1]){
+                return "";
+            }
+            stringBuffer.delete(removeList.get(i)[0], removeList.get(i)[1]);
+        }
+        return stringBuffer.toString();
+    }
 
     //반복되는 문자 위치 + 갯수 찾기
     public static List<int[]> check(String cryptogram){
-        int tmpCount = 0;
         List<int[]> result = new ArrayList<>();
         char tmp = cryptogram.charAt(cryptogram.length()-1);;
         int cnt = 0;
-        for (int i=cryptogram.length()-2; i>=0; i--){
-
-            if (tmp == cryptogram.charAt(i)){
+        for (int i=cryptogram.length()-1; i>0; i--){
+            if (tmp == cryptogram.charAt(i-1)){;
                 cnt += 1;
             }
-            if (cnt !=0 && tmp!= cryptogram.charAt(i)) {
-                result.add(new int[]{i+1, cnt + 1});
-                System.out.println("Arrays.toString(result.get(0)) = " + Arrays.toString(result.get(tmpCount)));
-                tmpCount++;
+            if (cnt !=0 && tmp!= cryptogram.charAt(i-1)) {
+                result.add(new int[]{i, i+cnt + 1});
                 cnt = 0;
+            } else if (cnt !=0 && i-1 ==0) { //중간에 문자가 끝나버린 경우
+                result.add(new int[]{0, cnt+1 });
+                break;
             }
-            tmp = cryptogram.charAt(i);
+            tmp = cryptogram.charAt(i-1);
 
         }
 
