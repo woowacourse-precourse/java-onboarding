3a7713ce20e6f6451a95742f748df608de8d719a
jungle <minuet1215@jbnu.ac.kr>
2022-11-01 18:36:45 +0900

    Feat: Problem5 Solve

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = Arrays.asList(new Integer[]{0, 0, 0, 0, 0, 0, 0, 0, 0});
+        List<Integer> change_list = Arrays.asList(new Integer[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1});
+
+        for (int i = 0; i < 9; i++) {
+            int change = change_list.get(i);
+            answer.set(i, money / change);
+            money %= change;
+        }
+
         return answer;
     }
 }
