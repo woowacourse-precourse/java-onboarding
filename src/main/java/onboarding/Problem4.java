efde99baa644d2caf258722ef3efba2387f7a91a
왕준영 <wangjunyoung@wangjun-yeong-ui-MacBookPro.local>
2022-11-01 15:14:02 +0900

    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-
+        int l=word.length();
+        char c;
+        int tmp;
+        for(int i=0; i<l; i++){
+            c=word.charAt(i);
+            if((int)c<=90&&(int)c>=65) c=(char)(155-(int)c);
+            else if((int)c<=122&&(int)c>=97) c=(char)(219-(int)c);
+            answer+=c;
+        }
         return answer;
     }
 }
