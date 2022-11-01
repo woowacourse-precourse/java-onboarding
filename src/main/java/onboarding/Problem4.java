c68e00066f5e549bb40d4a7098a1fb5e29a88a88
JeyunSong <jyunart@naver.com>
2022-11-01 15:46:40 +0900

    Problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return Convert(word);
+    }
+
+    private static String Convert(String word) {
+
+        StringBuilder s = new StringBuilder();
+
+        for (int k = 0; k < word.length(); k++) {
+            int a = word.charAt(k);
+            s.append(ConvertChar(a));
+        }
+        return s.toString();
+    }
+
+    private static char ConvertChar(int a) {
+
+        if (a >= 65 && a <= 90) {
+            return UpperCase(a);
+        }
+        if (a >= 97 && a <= 122) {
+            return LowerCase(a);
+        }
+        return (char)a;
+    }
+
+    private static char UpperCase(int a) {
+        return (char)(155 - a);
+    }
+
+    private static char LowerCase(int a) {
+        return (char)(219 - a);
     }
 }
