9efe26ce1a5bb047981294328b278f036d802e6f
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-01 11:46:27 +0900

    Refactor(Problem6) : 이메일 리스트 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,17 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         Map<String, String> infoMap = setInfoMap(forms);
+        Set<String> emailSet  = new HashSet<>();
+        String dupStr;
+        for(String s : infoMap.keySet()) {
+            dupStr = getDuplicateString(s);
+            if(!dupStr.equals("")) {
+                emailSet.add(infoMap.get(dupStr));
+                emailSet.add(infoMap.get(s));
+            }
+        }
+        answer = new ArrayList<>(emailSet);
+        Collections.sort(answer);
         return answer;
     }
 
@@ -16,5 +27,20 @@ public class Problem6 {
             m.put(l.get(1),l.get(0));
         }
         return m;
+    }
+
+    // k : twoLetters | v : nickname
+    private static Map<String, String> dupMap = new HashMap<>();
+    private static String getDuplicateString(String nickname) {
+        String twoLetters;
+        for(int i = 0; i < nickname.length() - 1; i++) {
+            twoLetters = nickname.substring(i,i+2);
+            if(dupMap.containsKey(twoLetters)) {
+                return dupMap.get(twoLetters);
+            } else {
+                dupMap.put(twoLetters, nickname);
+            }
+        }
+        return "";
     }
 }
