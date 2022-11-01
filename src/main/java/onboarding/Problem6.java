ae76e8caa394d2184c45e3528667f498a66bacd3
ahinkim <ahin98@naver.com>
2022-11-01 13:38:46 +0900

    refactor(problem6.md): Modify feature list, refactor(problem6.java): Add function to find emails that contain the same character consecutively

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        // 1. 같은 글자가 연속적으로 포함된 이메일을 구한다.
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        int N = forms.size();
+        for (int i = 0; i < N; i++){
+            String nick = forms.get(i).get(1);
+            String email = forms.get(i).get(0);
+            int M = nick.length();
+            for (int j = 0; j < M-1; j++){
+                String key = nick.substring(j, j+2);
+                ArrayList<String> valueList = new ArrayList<String>();
+                if (map.containsKey(key)) {
+                    valueList = map.get(key);
+                }
+                valueList.add(email);
+                map.put(key, valueList);
+            }
+        }
+        for (String key : map.keySet()) {
+            ArrayList<String> list = map.get(key);
+            if(list.size() >= 2){
+                for(String email: list){
+                    answer.add(email);
+                }
+            }
+        }
         return answer;
     }
 }
