ed10581807470d684b2217389b0f0f3763097ed1
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-01 17:54:46 +0900

    feat: Problem2 - 필요한 상수 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,9 +5,13 @@ package onboarding;
  * getDecodingCryptogram 메서드 해독된 암호문을 반환
  * isLengthZero 암호문의 길이가 0인지 확인
  * isDecodingCryptogram 암호해독이 완료됐는지 확인
- * deleteOverlap 중복된 문자열을 제거 하여 반환
+ * deleteOverlap 중복된 문자열을 제거 하여 반
  */
 public class Problem2 {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
+    private static final String LOWERCASE = "^[a-z]*$";
+
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
