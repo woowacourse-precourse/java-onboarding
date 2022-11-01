6ebea0bf6c77ec7967a9ac5c0a06fc962d8bd1ff
DavinAn <blueapple1030@gmail.com>
2022-11-01 23:07:23 +0900

    Problem7 import statements
    We use several data structures in the class, such as HashMap, List, ArrayList etc.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
