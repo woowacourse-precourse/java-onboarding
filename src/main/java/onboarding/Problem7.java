06b24bbc4ed87dc0593dcf8dff2a2da9295d4b55
cmw9957 <cmw9957@gmail.com>
2022-11-01 18:51:38 +0900

    comment: 기능 목록 주석 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/*
+========================================Problem7 기능 목록========================================
+- user에 따라 같은 list에 속해있는 문자열을 찾아 배열로 저장하는 기능
+- 배열을 리스트로 변환하는 기능
+- 리스트를 배열로 변환하는 기능 (1차원, 2차원)
+- map type 변수에 모든 user의 점수를 0으로 초기화하는 기능
+- user와 친구인 user의 점수는 0으로 만드는 기능
+ */
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
