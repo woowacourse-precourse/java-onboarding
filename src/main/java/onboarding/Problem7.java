367ccf5599774404eab1fabd6af6b626f6402897
youswim <youswim96@gmail.com>
2022-11-01 22:04:33 +0900

    Feat: 추천 친구와 점수를 기록하기 위한 클래스 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,23 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    // 추천 친구와 점수를 기록하기 위한 클래스
+    private static class FriendCount {
+        String name;
+        int count;
+
+        FriendCount(String name, int count) {
+            this.name = name;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "name='" + name + '\'' +
+                    ", count=" + count +
+                    '}';
+        }
+    }
 }
