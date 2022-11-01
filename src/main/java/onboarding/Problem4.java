7818dd094cfa3c14efbf8983029b11b0e79f387e
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-01 23:42:55 +0900

    Problem 4 : 한 단어를 청개구리 사전을 참고하여 반대로 변환하는 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,18 @@
 package onboarding;
 
 public class Problem4 {
+
+    // 기능 목록 1 : 한 단어를 청개구리 사전을 참고하여 반대로 변환하는 함수
+    public static Character transformReverse(Character ch){
+        int temp = (int)ch;
+        if(temp>=65&&temp<=90){
+            temp = 90-(temp-65);
+        }
+        if(temp>=97&&temp<=122){
+            temp = 122-(temp-97);
+        }
+        return (char)temp;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
