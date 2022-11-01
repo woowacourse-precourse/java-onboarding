6ab26db9fa1271e2d5a3de72c6264ad962578c63
ckyeon <qsky8520@gmail.com>
2022-11-01 15:47:49 +0900

    Add getClapPlaceValues() to Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,12 +2,19 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static List<Integer> getClapPlaceValues(List<Integer> placeValues) {
+        return placeValues.stream()
+            .filter(Problem3::isClap)
+            .collect(Collectors.toList());
     }
 
     private static boolean isClap(int number) {
