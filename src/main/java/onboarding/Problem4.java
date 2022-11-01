f3420ff92bfd46c86e8b66108388dffae1257cfb
judyi19997 <judyi1999@kookmin.ac.kr>
2022-11-01 18:18:13 +0900

    add/ 문제 4) 대응되는 알파벳으로 변경하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,12 +4,28 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         for(int i=0;i<word.length();i++){
-            System.out.println("isAlpha(word) = " + isAlpha(word.charAt(i)));
-
+            if (isAlpha(word.charAt(i))){
+                answer+=changeAlpha(word.charAt(i));
+            }else {
+                answer+=word.charAt(i);
+            }
         }
         return answer;
     }
 
+    //대응하는 알파벳으로 변환
+    public static char changeAlpha(char alpha){
+        int numAlpha = (int)alpha;
+        char result = 0;
+        if (65 <= numAlpha && numAlpha <= 90){//대문자
+            int minus = 90 - numAlpha;
+            return (char) (65 + minus);
+        }else{
+            int minus = 122 - numAlpha;
+            return (char) (97+minus);
+        }
+
+    }
     //알파벳인지 그 외 문자인지 판별하는 기능
     public static boolean isAlpha(char alpha){
         return (65 <= (int)alpha && (int)alpha <= 90) || (97 <= (int)alpha && (int)alpha <= 122);
