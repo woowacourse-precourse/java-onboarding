9a4950e2620f6f597bb573834d6e6880e4f59e77
SeokHwan An <asok369258@naver.com>
2022-11-01 22:13:22 +0900

    refactor [Problem3] : 반복되는 3가지 메소드를 하나로 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,9 +14,7 @@ public class Problem3 {
         }
         int answer = 0;
         for (int i = 0; i <= number; i++) {
-            answer += countOfThree(i);
-            answer += countOfSix(i);
-            answer += countOfNine(i);
+            answer += countOfClap(i);
         }
         return answer;
     }
@@ -29,33 +27,13 @@ public class Problem3 {
         return Integer.toString(number);
     }
 
-    public static int countOfThree(int number) {
-        int countOfThree = 0;
+    public static int countOfClap(int number) {
+        int clap = 0;
         for (char eachDigit : intToString(number).toCharArray()) {
-            if (eachDigit == THREE) {
-                countOfThree++;
+            if (eachDigit == THREE || eachDigit == SIX || eachDigit == NINE) {
+                clap++;
             }
         }
-        return countOfThree;
-    }
-
-    public static int countOfSix(int number) {
-        int countOfSix = 0;
-        for (char eachDigit : intToString(number).toCharArray()) {
-            if (eachDigit == SIX) {
-                countOfSix++;
-            }
-        }
-        return countOfSix;
-    }
-
-    public static int countOfNine(int number) {
-        int countOfNine = 0;
-        for (char eachDigit : intToString(number).toCharArray()) {
-            if (eachDigit == NINE) {
-                countOfNine++;
-            }
-        }
-        return countOfNine;
+        return clap;
     }
 }
