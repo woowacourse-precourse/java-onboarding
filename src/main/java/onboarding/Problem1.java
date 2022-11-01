d8f202679162e67b09c382d044c30ebd6e0e148e
eom-tae-in <eti0728@daum.net>
2022-11-01 19:28:40 +0900

    feat: static value

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static final int LEFT_PAGE = 0;
+    static final int RIGHT_PAGE = 1;
+    static final int POBI_WIN = 1;
+    static final int CRONG_WIN = 2;
+    static final int DRAW = 0;
+    static final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!(isCorrectPage(pobi) && isCorrectPage(crong))) {
+            return EXCEPTION;
+        }
+        return compareMaxValue(pobi, crong);
     }
 }
+
