31ebf183e7683bc4b1f16f1bc74cea8d13c198dd
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 17:45:48 +0900

    문제1 / refactor : 승자 비교 객체를 이용하여 문제 해결 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,27 +1,11 @@
 package onboarding;
 
-import onboarding.problem1.Validator;
 import java.util.List;
-import static onboarding.problem1.Operator.*;
+
+import static onboarding.problem1.Comparator.decideWinner;
 
 class Problem1 {
-    static final int DRAW = 0;
-    static final int POBI = 1;
-    static final int CRONG = 2;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-
-        if (!(Validator.validate(pobi) && Validator.validate(crong))) { return -1;}
-        if (compare(pobi) == compare(crong)) {
-            answer = DRAW;
-        }
-        else if (compare(pobi) > compare(crong)) {
-            answer = POBI;
-        }
-        else {
-            answer = CRONG;
-        }
-
-        return answer;
+        return decideWinner(pobi,crong);
     }
 }
