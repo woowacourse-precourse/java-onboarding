ebde0c56da45e3e5de8c4a20f064a94d00f802c1
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-01 15:11:52 +0900

    refactor : Problem4 알파벳 변수, stringBuilder 클래스로 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,27 +5,25 @@ import java.util.stream.Collectors;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        if (word.length() <= 1000) {
-            List<Character> wordList = word.chars()
-                    .mapToObj(e -> (char) e).collect(Collectors.toList());
-            char UPPER_CASE = (char) 155;
-            char LOWER_CASE = (char) 219;
-            for (int i = 0; i < wordList.size(); i++) {
-                char c = wordList.get(i);
-                if (c == 32) {
-                    answer += " ";
-                }
-                if (c >= 65 && c <= 90) {
-                    c = (char) (UPPER_CASE - c);
-                    answer += String.valueOf(c);
-                }
-                if (c >= 97 && c <= 122) {
-                    c = (char) (LOWER_CASE - c);
-                    answer += String.valueOf(c);
-                }
+        StringBuilder answer = new StringBuilder();
+        List<Character> wordList = word.chars()
+                .mapToObj(e -> (char) e).collect(Collectors.toList());
+        char UPPER_CASE = (char) 155;
+        char LOWER_CASE = (char) 219;
+        for (char alphabet : wordList) {
+            if (alphabet == 32) {
+                answer.append(" ");
+            }
+            if (alphabet >= 65 && alphabet <= 90) {
+                alphabet = (char) (UPPER_CASE - alphabet);
+                answer.append(String.valueOf(alphabet));
+            }
+            if (alphabet >= 97 && alphabet <= 122) {
+                alphabet = (char) (LOWER_CASE - alphabet);
+                answer.append(String.valueOf(alphabet));
             }
         }
-        return answer;
+
+        return answer.toString();
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,40 +5,29 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem7 {
-    private static final int FIRST_FRIEND = 0;
-    private static final int SECOND_FRIEND = 0;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         Map<String, Integer> userInfo = new HashMap<>();
         Set<String> myFriends = new HashSet<>();
 
-        for (List<String> friend : friends) {
-            userInfo.put(friend.get(0), FIRST_FRIEND);
-            userInfo.put(friend.get(1), SECOND_FRIEND);
-        }
         friends.stream()
                 .filter(friend -> friend.contains(user))
                 .forEach(myFriends::addAll);
-        List<String> myFriend = new ArrayList<>(myFriends);
 
         for (String visitor : visitors) {
             userInfo.put(visitor, userInfo.getOrDefault(visitor, 0) + 1);
         }
 
-        for (List<String> strings : friends) {
-            for (int j = 0; j < 2; j++) {
-                if (strings.contains(myFriend.get(0))) {
-                    userInfo.put(strings.get(1), userInfo.get(strings.get(1)) + 10);
-                }
-                if (strings.contains(myFriend.get(1))) {
-                    userInfo.put(strings.get(0), userInfo.get(strings.get(0)) + 10);
-                }
-            }
-        }
-        userInfo.remove(user);
-        for (String friend : myFriend) {
+        friends.stream()
+                .flatMap(Collection::stream)
+                .filter(username -> !myFriends.contains(username))
+                .forEach(username -> userInfo.put(username, userInfo.getOrDefault(username, 0) + 10));
+
+        for (String friend : myFriends) {
             userInfo.remove(friend);
         }
+        userInfo.remove(user);
+
         List<String> answer = new ArrayList<>(userInfo.keySet());
         answer.sort(((o1, o2) -> userInfo.get(o2).compareTo(userInfo.get(o1))));
         return answer;
