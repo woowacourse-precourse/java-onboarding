5ccece99d382c736b89167cb6f9ac829e9140fbb
stk346 <stk346@gamil.com>
2022-11-01 12:23:38 +0900

    refactor: 변수, 메서드 이름 조금 더 명확하게 수정하고 일부 기능 통합해 가독성을 높였음.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,47 @@ import java.util.List;
 
 class Problem1 {
 
-    private static int doSum(int num) {
+    private static boolean isCorrectPage(List<Integer> player) {
+        int leftPage = player.get(0);
+        int rightPage = player.get(1);
+
+        if (leftPage - rightPage != -1) {
+            return false;
+        }
+
+        if (leftPage % 2 == 0 || rightPage % 2 != 0) {
+            return false;
+        }
+
+        if (leftPage < 1 || rightPage > 400) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isFirstOrLastPage(List<Integer> player) {
+        for (int pageNum : player) {
+            if (pageNum == 1 || pageNum == 400) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int calculateMaxScore(List<Integer> person) {
+        int leftNum = calculateScore(person.get(0));
+        int rightNum = calculateScore(person.get(1));
+        return Math.max(leftNum, rightNum);
+    }
+
+    private static int calculateScore(int num) {
+        int summedNum = calculateBySumRule(num);
+        int multipliedNum = calculateByMultiplyRule(num);
+        return Math.max(summedNum, multipliedNum);
+    }
+
+    private static int calculateBySumRule(int num) {
         String stringInt = String.valueOf(num);
         int sum = 0;
         for (int i=0; i< stringInt.length(); i++) {
@@ -14,7 +54,7 @@ class Problem1 {
         return sum;
     }
 
-    private static int doMultiply(int num) {
+    private static int calculateByMultiplyRule(int num) {
         String stringInt = String.valueOf(num);
         int multiply = 1;
         for (int i=0; i< stringInt.length(); i++) {
@@ -24,79 +64,24 @@ class Problem1 {
         return multiply;
     }
 
-    private static int chooseLargeNumber(int num) {
-        int summedNum = doSum(num);
-        int multipliedNum = doMultiply(num);
-        return Math.max(summedNum, multipliedNum);
-    }
-
-    private static boolean isFirstOrLastPage(int num) {
-        if (num == 1 || num == 400) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isFirstOrLastPage(List<Integer> person1, List<Integer> person2) {
-        boolean flag = false;
-        for (int num : person1) {
-            flag = isFirstOrLastPage(num);
-        }
-        for (int num : person2) {
-            flag = isFirstOrLastPage(num);
-        }
-        return flag;
-    }
-
-    private static boolean isCorrectPage(List<Integer> person) {
-        int leftPage = person.get(0);
-        int rightPage = person.get(1);
-        if (leftPage - rightPage != -1) {
-            return false;
-        }
-        if (leftPage % 2 == 0 || rightPage % 2 != 0) {
-            return false;
-        }
-        if (leftPage < 1 || rightPage > 400) {
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean isCorrectPage(List<Integer> person1, List<Integer> person2) {
-        boolean flag = true;
-        if (!isCorrectPage(person1)) {
-            flag = false;
-        }
-        if (!isCorrectPage(person2)) {
-            flag = false;
-        }
-        return flag;
-    }
-
-    private static int chooseLageNumber(List<Integer> person) {
-        int leftNum = chooseLargeNumber(person.get(0));
-        int rightNum = chooseLargeNumber(person.get(1));
-        return Math.max(leftNum, rightNum);
-    }
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int pobiNum = chooseLageNumber(pobi);
-        int crongNum = chooseLageNumber(crong);
-        int answer = Math.max(pobiNum, crongNum);
+        if (!isCorrectPage(pobi) || !isCorrectPage(crong) || isFirstOrLastPage(pobi) || isFirstOrLastPage(crong)  ) {
+            return -1;
+        }
 
-        if (isFirstOrLastPage(pobi, crong)) {
-            return -1;
+        int pobiScore = calculateMaxScore(pobi);
+        int crongScore = calculateMaxScore(crong);
+
+        int result;
+
+        if (pobiScore > crongScore) {
+            result = 1;
+        } else if (pobiScore == crongScore) {
+            result = 0;
+        } else {
+            result = 2;
         }
-        if (!isCorrectPage(pobi, crong)) {
-            return -1;
-        }
-        if (pobiNum == crongNum) {
-            return 0;
-        }
-        if (answer == pobiNum) {
-            return 1;
-        }
-        return 2;
+
+        return result;
     }
 }
