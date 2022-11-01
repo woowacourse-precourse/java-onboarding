e4e567afbe8a79e78de84de708ef3d908d829ed6
taekwon-dev <taekwon_dev@naver.com>
2022-11-01 10:37:11 +0900

    problem 7 - feat : 사용자와 함께 아는 친구 수만큼 대상 10점 점수 부여

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,15 +1,17 @@
 package onboarding;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
+import java.util.*;
 
 public class Problem7 {
     private static Map<String, List<String>> map = new HashMap<>();
+    private static Set<String> userFriends = new HashSet<>();
+    private static Map<String, Integer> scores = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        makeRelationships(friends);
+        makeUserFriendSet(user);
+        add10PointsToKnowTogether(user);
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
@@ -32,4 +34,32 @@ public class Problem7 {
             map.get(from).add(to);
         }
     }
+
+    private static void makeUserFriendSet(String user) {
+        List<String> friends = map.get(user);
+        for (String friend : friends) {
+            userFriends.add(friend);
+        }
+    }
+
+    private static void add10PointsToKnowTogether(String user) {
+        List<String> friends = map.get(user);
+        for (String friend : friends) {
+            List<String> friendOfFriends = map.get(friend);
+            add10Points(user, friendOfFriends);
+        }
+    }
+
+    private static void add10Points(String user, List<String> friendOfFriends) {
+        for (String friend : friendOfFriends) {
+            if (user.equals(friend)) {
+                continue;
+            }
+            if (userFriends.contains(friend)) {
+                continue;
+            }
+            scores.put(friend, scores.getOrDefault(friend, 0) + 10);
+        }
+    }
+
 }
