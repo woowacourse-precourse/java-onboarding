e55a8b0ca350d17573aee61d2c25709cd056088e
1eejisoo <jisu991012@gmail.com>
2022-11-01 16:42:20 +0900

    [Problem 5] Style: 주석 삭제

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,7 +12,6 @@ public class Problem5 {
         return answer;
     }
 
-    // 큰 큼액의 화폐를 우선으로 출금하는 기능
     public static void withdraw (int money, List<Integer> answer) {
         int[] unitSet = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
