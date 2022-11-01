f65d94339c9e1f48935a0fa1ed4e6d0637628700
si hwan <pum005@naver.com>
2022-11-01 22:16:17 +0900

    feat (problem2) : 입력된 문자가 1 ~ 1000 이하의 길이 인지 판단 하는 예외처리 기능

---
+++ src/main/java/ExceptionValidation/P2ExceptionValidation.java
@@ -0,0 +1,22 @@
+package ExceptionValidation;
+
+//- 입력된 문자가 1 ~ 1000 이하의 길이 인지 판단
+//- 입력된 문자가 알파벳인지 판단
+//- 입력된 문자가 소문자로 이뤄져 있는지 판단
+
+public class P2ExceptionValidation {
+
+    public static boolean p2ExceptionValidation(String cryptogram){
+
+        return inputBetween1and1000(cryptogram);
+
+    }
+
+    private static boolean inputBetween1and1000(String cryptogram) {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            return false;
+        }
+        return true;
+    }
+
+}
