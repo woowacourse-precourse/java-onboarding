5207c0e5f6b98c5161dfab58d0ab0108622c52bb
patronum98 <zkrhklkl98@gmail.com>
2022-11-01 21:27:23 +0900

    문제7 기능목록 수정 및 추가, 기능3 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,8 +3,9 @@ package onboarding;
 //기능1. friends 리스트/배열에 있는 이름(key)과 점수10(value) 을 해시맵에 추가한다. 만약 friends 리스트/배열에 유저와 같은 이름이 있으면 해시맵에 넣지 않는다.
 //기능2. 만약 해쉬맵에있는 이름이 있을경우 추가하지 않고 value에 10을 더한다.
 //기능3. visitors 에 등장하는 친구들은 value에 +1 을 하고 key에 등록되지 않은 경우 등록하고 +1 한다.
-//기능4. value 값이 높은 순으로 리스트에 정렬한다.
-//기능5. 만약 점수가 같으면 알파벳 순으로 출력한다.
+//기능4. 맵에서 유저와 친구인 친구를 삭제한다.
+//기능5. value 값이 높은 순으로 리스트에 정렬한다.
+//기능6. 만약 점수가 같으면 알파벳 순으로 출력한다.
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,6 +29,15 @@ public class Problem7 {
                 }
             }
         }
+        for (int i=0;i<visitors.size();i++) {
+            if (map.containsKey(visitors.get(i))) { //맵에 비지터의 이름이 있으면
+                map.put((visitors.get(i)), map.get((visitors.get(i)))+1); //1을 더한다.
+            } else { // 없으면
+                map.put(visitors.get(i), 1);
+            }
+        }
+
+
 
 //        if (friends.get(2).contains("mrko")) {
 //            System.out.println(1);
