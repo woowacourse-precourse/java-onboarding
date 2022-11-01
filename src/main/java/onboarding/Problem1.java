f92b8ce46801ce9469e089a538d0a08a63c57f00
pasly0920 <inwoo920621@gmail.com>
2022-11-01 13:34:06 +0900

    feat: 2. 각 자리를 모두 더한 값 반환

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    private static int getPlusValue(Integer pageNum){
+        int result = 0;
+        String pageNumString = pageNum.toString();
+
+        for(int i=0; i<pageNumString.length(); i++){
+            result += Integer.parseInt(pageNumString.substring(i, i+1));
+        }
+        return result;
     }
 }
