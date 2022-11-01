4dceda446d70db9f1ddaa2ed21afef149c9edb36
hum02 <ehdgma02@gmail.com>
2022-11-01 10:27:35 +0900

    style:4번 코딩 컨벤션 준수,논리 연산시 괄호

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,17 +7,17 @@ public class Problem4 {
         char[] wordChar = word.toCharArray();
         char[] converted = new char[wordChar.length];
 
-        for(int i=0; i<wordChar.length; i++){
-            if('a'<= wordChar[i] && wordChar[i] <= 'z'){
-                converted[i] = (char)('a'+ ('z'- wordChar[i])) ;
-            } else if ('A'<= wordChar[i] && wordChar[i]<='Z') {
-                converted[i] = (char)('A'+ ('Z'- wordChar[i]));
+        for (int i=0; i<wordChar.length; i++) {
+            if (('a' <= wordChar[i]) && (wordChar[i] <= 'z')) {
+                converted[i] = (char)('a' + ('z' - wordChar[i])) ;
+            } else if (('A' <= wordChar[i]) && (wordChar[i] <='Z')) {
+                converted[i] = (char)('A' + ('Z' - wordChar[i]));
             } else {
                 converted[i] = wordChar[i];
             }
         }
 
-        for(char c:converted){
+        for (char c:converted) {
             sb.append(c);
         }
 
