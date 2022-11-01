5749c8e5f0f89591e9ea70bcb750da2c74ec340c
heon118 <ectmdgjs@gmail.com>
2022-11-01 21:39:14 +0900

    [문제6] 닉네임 연속 글자 사용 불가 알림
    반복문을 중복해서 사용하는 것은 연산속도가 느려 잘 사용하지 않으려고 노력한다.
    다른 방식이 생각이 나지 않았고 내가 사용한 코드는 중복되는 문자가 하나일 경우에만 적용할 수 있다.
    리스트 내 이름 추출 -> 이름을 두글자씩 잘라서 다른 이름과 비교 -> 중복 제거 및 정렬 순서이다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        // 리스트 내 1번 index를 추출을 위한 반복문
+        for (int i = 0; i < forms.size(); i++) {
+            // 이름의 글자수로 반복문
+            if ((forms.get(i).get(1)).length() > 1) {
+                for (int j = 0; j < (forms.get(i).get(1)).length() - 1; j++) {
+                    // 다른 이름과 비교를 위한 반복문
+                    for (int k = 0; k < forms.size(); k++) {
+                        // 이름별 두글자씩 잘라 다른 이름과 비교
+                        if ((forms.get(k).get(1)).contains((forms.get(i).get(1)).substring(j, j + 2)) && k != i) {
+                            // 배열에 이메일 추가
+                            answer.add(forms.get(k).get(0));
+                        }
+                    }
+                }
+            }
+        }
+        // 배열의 중복 제거 및 오름차순 정렬
+        answer = answer.stream().distinct().sorted().collect(Collectors.toList());
         return answer;
     }
 }
