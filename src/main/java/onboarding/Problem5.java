339dc48a8bbb688fad96aa11003c402f4aa1b258
Hongjun <ggaye159@naver.com>
2022-11-01 18:51:16 +0900

    Fifth problem solved

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int num=0;
+        int[] cost={50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<Integer>();
+        for(int i=0;i<9;i++){
+            num=money/cost[i];
+            money=money-(num*cost[i]);
+            answer.add(num);
+        }
         return answer;
     }
 }
