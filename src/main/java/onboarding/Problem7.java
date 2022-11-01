5f12705100d22bca2a640e26b5f9a9deee67233c
ChaCha3088 <cha3088@gmail.com>
2022-11-01 16:24:05 +0900

    feat: problem7 new function findFriends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,28 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        TreeSet<String> friendSet = new TreeSet<String>();
+        TreeSet<String> peopleSet = new TreeSet<String>();
+
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    private static void findFriends(String user, List<List<String>> friends, TreeSet<String> friendSet) {
+        for (List<String> e : friends) {
+            int indexOfMe = e.indexOf(user);
+            if (indexOfMe == 1) {
+                String friendName = e.get(0);
+                friendSet.add(friendName);
+            } else if (indexOfMe == 0) {
+                String friendName = e.get(1);
+                friendSet.add(friendName);
+            }
+        }
+    }
+
 }
