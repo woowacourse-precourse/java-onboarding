820680fb86f2b341c9c75c8407c1bd9dfe7a6b94
Choi Seongwon <vlflalektzja@naver.com>
2022-11-01 12:37:01 +0900

    Fix Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,41 +5,54 @@ import java.util.Stack;
 
 public class Problem2 {
 
-    public static String charArraytoString(Iterable<Character> target) {
-        Iterator itr = target.iterator();
+    public static String stackToString(Stack<Character> stack) {
+        Iterator itr = stack.iterator();
         String temp = "";
         while (itr.hasNext()) {
             temp += itr.next().toString();
         }
         return temp;
     }
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        Stack<Character> stack = new Stack<Character>();
-        int beforeLen, afterLen;
-        while (true) {
-            stack.clear();
-            beforeLen = cryptogram.length();
-            for (char ch : cryptogram.toCharArray()) {
-                if (stack.isEmpty()){
-                    stack.push(ch);
-                }
-                else {
-                    if (stack.peek() != ch) {
-                        stack.push(ch);
-                    } else {
-                        stack.pop();
-                    }
+
+    public static String delDuplicated(String target) {
+        Stack<Character> stack = new Stack<>();
+        boolean flag = false;
+        if (target.isEmpty()) return "";
+        else if (target.length() == 1) return target;
+
+        for (int i = 0; i < target.length(); i++) {
+            if (i == target.length() - 1) {
+                if (!flag) {
+                    stack.push(target.charAt(i));
                 }
             }
 
-            cryptogram = charArraytoString(stack);
-            afterLen = cryptogram.length();
-            if (beforeLen == afterLen) {
-                answer = cryptogram;
-                break;
+            else if (target.charAt(i) != target.charAt(i + 1)) {
+                if (!flag) stack.push(target.charAt(i));
+                else flag = false;
+            } else {
+                flag = true;
             }
         }
+
+        return stackToString(stack);
+    }
+    public static String solution(String cryptogram) {
+        String answer;
+
+        int i = 0, j = 1;
+        int beforeLen, afterLen;
+
+        while (true) {
+            beforeLen = cryptogram.length();
+
+            String after = delDuplicated(cryptogram);
+
+            afterLen = after.length();
+            if (beforeLen == afterLen) break;
+            cryptogram = after;
+        }
+        answer = cryptogram;
 
         return answer;
     }
