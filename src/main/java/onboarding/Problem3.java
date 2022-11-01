acc6b6461d7e6b10a6ed277ea6e6f9d5b7146357
otndus <xuyeon219@gmail.com>
2022-11-01 15:07:59 +0900

    [기능 구현] Problem3 count기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int countNum(int number){
+        int count = 0;
+        for(int i = 1; i < number+1; i++){
+            int temp = i;
+            while(temp > 0){
+                if((temp % 10) == 3 || (temp % 10) == 6 || (temp % 10) == 9)
+                    count++;
+                temp /= 10;
+            }
+        }
+        return count;
+    }
+
     public static int solution(int number) {
-        int answer = 0;
+        int answer = countNum(number);
         return answer;
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -51,7 +51,7 @@ public class Problem7 {
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List answer = friendsScore(user, friends, visitors);
         return answer;
     }
 }
