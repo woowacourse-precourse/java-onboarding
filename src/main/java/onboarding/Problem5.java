55dfa6f2f8645a8eadea1f12de838b121bf16bec
judyi19997 <judyi1999@kookmin.ac.kr>
2022-11-01 18:47:06 +0900

    add/ 문제 5) 단위 순으로 money 구하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,18 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new java.util.ArrayList<>(Collections.emptyList());
+        int [] unit = {50000,10000,5000,1000,500,100,50,10,1};
+        for(int i=0; i<9; i++){
+            int fullOfNowUnit = division(money,unit[i]);
+            money -=fullOfNowUnit;
+            answer.add(fullOfNowUnit/unit[i]);
+        }
         return answer;
+    }
+    //단위 순으로 money 구하는 기능
+    public static int division(int money, int unit){
+        int result = money / unit;
+        return result *unit;
     }
 }
