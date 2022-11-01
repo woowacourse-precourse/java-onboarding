3963dd4cca99c37596b36d3a7674ce696b4ebf5f
sami355-24 <ddung355@gmail.com>
2022-11-01 14:43:50 +0900

    [add] find50000

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,5 +8,16 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static boolean isInRange(Integer Money){
+        if(Money >= 1 && Money <= 50000)
+            return true;
+
+        return false;
+    }
+
+    public static Integer find50000(Integer money){
+        return money/50000;
     }
 }
