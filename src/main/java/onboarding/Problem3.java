8fdeb1108b040a214262c05ba04eb84d4443a7ab
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 13:39:22 +0900

    feat: Problem3 숫자를 문자열로 바꾼 후 해당 문자에 3,6,9가 포함되는 개수만큼 추가하는 기능 구현
    Problem3 숫자를 문자열로 바꾼 후 해당 문자에 3,6,9가 포함되는 개수만큼 answer에 추가하는 기능을 구현했습니다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,7 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-       
+        for (int cnt = 1; cnt < number + 1; cnt++) {
+            String s = Integer.toString(cnt);
+            for(int i=0;i<s.length();i++){
+                if (check(s.charAt(i))){
+                    answer+=1;
+                }
+            }
+        }
         return answer;
     }
     public static Boolean check(char c){
