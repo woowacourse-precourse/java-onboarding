80e16536907c450bbd3a2c8ee0a4d57b2b59847c
simjaeseo <delpho@naver.com>
2022-11-01 14:13:31 +0900

    feat: 문제 - 2 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+import java.util.Stack;
+
+/**
+ * 기능 목록
+ * 1. Stack을 활용하여 암호문의 연속 문자 제거
+ * 2. Stack -> String 변환하는 함수
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
