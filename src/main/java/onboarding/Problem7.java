590a01a4fe3daaf2c811f1b8df23fe76b8d00c5a
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-01 16:41:00 +0900

    [feat]Problem7: Re

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 /*
+
     Goal : 친구추천 알고리즘 구현하기
     Parameters: 사용자 아이디 user, 친구관계정보 friends, 사용자 타임라인 방문기록 visitors
 
@@ -10,78 +11,61 @@ package onboarding;
     - 점수가 0인 경우 추천 X, 점수가 같을 경우 이름순 정렬
 
     To dos
+    [ ] 친구 목록 구하기
+
     [ ] 점수 계산
     [ ] 0점 유저 제거 및 정렬
- */
-
-/*
-    Notes
-
-    1) User 친구 목록 확인
-    2) 그 친구가 알고있는 친구 +10, 리스트에 추가
-    3) 방문자 리스트 확인 +1, 리스트 추가
-
  */
 
 import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-
-        TreeMap<String, Integer> userList = new TreeMap<String, Integer>();
-        ArrayList<String> friendList = new ArrayList<String>();
-
-        int users = 0;
-        for (int i = 0; i < friends.size(); i++) {
-            for (int j = 0; j < 2; j++) {
-                String name = friends.get(i).get(j);
-
-                if (name != user) {
-                    userList.put(name, 0);
-                    if (friends.get(i).contains(user)) {
-                        friendList.add(name);
-                    }
-
-                }
-            }
-        }
-
-        for (int i = 0; i < visitors.size(); i++) {
-            userList.put(visitors.get(i),0);
-
-        }
-
+        Map<String, Integer> userList = new TreeMap<String, Integer>();
+        Set<String> friendList = new HashSet<String>();
 
         for (int i = 0; i < friends.size(); i++) {
             for (int j = 0; j < 2; j++) {
-                if (friends.get(i).contains(friendList.get(j))) {
-                    userList.put(friends.get(i).get(j), userList.get(friends.get(i).get(j)) + 10);
+                if (friends.get(i).contains(user)) {
+                    String name = friends.get(i).get(j);
+                    friendList.add(name);
                 }
             }
+        }
+        friendList.remove(user);
+
+        for (int i = 0; i < friends.size(); i++) {
+
+            if (!userList.containsKey(friends.get(i).get(0)) && !friendList.contains(friends.get(i).get(0))) {
+                userList.put(friends.get(i).get(0), 0);
+            }
+            if (!userList.containsKey(friends.get(i).get(1)) && !friendList.contains(friends.get(i).get(1))) {
+                userList.put(friends.get(i).get(1), 0);
+            }
+            if (friendList.contains(friends.get(i).get(0))) {
+                userList.put(friends.get(i).get(1), userList.get(friends.get(i).get(1)) + 10);
+            } else if (friendList.contains(friends.get(i).get(1))) {
+                userList.put(friends.get(i).get(0), userList.get(friends.get(i).get(0)) + 10);
+            }
+
         }
 
         for (int i = 0; i < visitors.size(); i++) {
-            for (int j = 0; j < friendList.size(); j++) {
-                if (visitors.get(i).contains(friendList.get(j))){
-                    userList.put(visitors.get(i), userList.get(visitors.get(i)) + 1);
-                }
+            if (!userList.containsKey(visitors.get(i)) && !(friendList.contains(visitors.get(i)))) {
+                userList.put(visitors.get(i), 0);
             }
-
+            if (userList.containsKey(visitors.get(i))) {
+                userList.put(visitors.get(i), userList.get(visitors.get(i)) + 1);
+            }
         }
 
-
-        userList.put(user, 0);
-
-        for(int i=0; i< friendList.size(); i++){
-            userList.put(friendList.get(i), 0);
-        }
+        userList.remove(user);
 
 
-        List<String> totalscores = new ArrayList<>(friendList);
 
-        return totalscores;
+        List<String> answer = new ArrayList<>(userList.keySet());
+
+
+        return answer;
     }
-
-
 }
