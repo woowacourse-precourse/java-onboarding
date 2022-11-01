f59896d406c87807692cc809be4a740afe4eafba
JunginPark4364 <wjddlsee@gmail.com>
2022-11-01 20:07:32 +0900

    refactor: update blank line
    - 라인 정리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-
         int[] currencies = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
         answer = countCurrency(money, currencies);
