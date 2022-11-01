20e96626ae9b48205f2b3959d0456513aa19108c
juno-junho <ssmm0205@naver.com>
2022-11-01 19:45:44 +0900

    feat(Problem3): add method in Game369 class
    validation을 위한 validate 메서드와 손뼉을 쳐야 하는 로직 생성

--- src/main/java/onboarding/problem3/Game369.java
+++ src/main/java/onboarding/problem3/Game369.java
@@ -1,4 +1,41 @@
 package onboarding.problem3;
 
+import java.util.stream.IntStream;
+
+import static onboarding.problem3.ValidNumber.*;
+
 public class Game369 {
+    private int clapCount;
+
+    public void playTheGame(int number) {
+        validate(number);
+        this.clapCount = IntStream.rangeClosed(1, number).map(this::countNumberThreeSixNine).sum();
+    }
+
+    private void validate(int number) {
+        if (isValidNumber(number))
+            throw new IllegalArgumentException("숫자 범위는 1 이상 10,000 이하 입니다.");
+    }
+
+    private boolean isValidNumber(int number) {
+        return number < MAX_NUMBER.getNumber() || number > MIN_NUMBER.getNumber();
+    }
+
+    private int countNumberThreeSixNine(int number) {
+        int countedNum = 0;
+        while (number > 0) {
+            if (isThreeSixNine(number))
+                countedNum++;
+            number /= 10;
+        }
+        return countedNum;
+    }
+
+    private boolean isThreeSixNine(int number) {
+        return (number % 10) == 3 || (number % 10) == 6 || (number % 10) == 9;
+    }
+
+    public int getClapCount() {
+        return this.clapCount;
+    }
 }
