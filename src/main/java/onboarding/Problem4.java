9b46a81554881e2e91543270698ec44a80b8b971
dingding-21 <phj8221@naver.com>
2022-11-01 22:52:50 +0900

    feat: Problem4 문자를 반대로 변환하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,18 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static String getWord(String word) {
+        char[] wordList = word.toCharArray();
+
+        int upperSum = 'A' + 'Z';
+        int lowerSum = 'a' + 'z';
+
+        for (int i = 0; i < wordList.length; i++) {
+            if (Character.isUpperCase(wordList[i])) wordList[i] = (char) (upperSum - wordList[i]);
+            if (Character.isLowerCase(wordList[i])) wordList[i] = (char) (lowerSum - wordList[i]);
+        }
+
+        return String.valueOf(wordList);
+    }
 }
