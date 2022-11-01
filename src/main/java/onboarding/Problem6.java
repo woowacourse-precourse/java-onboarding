97e4b570bc13fd036c884100a31157af23d804f9
nayooniverse <kangna99@naver.com>
2022-11-01 18:17:19 +0900

    [feat] 이메일 형식 검사

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,24 +7,24 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         for(List<String> crew : forms) {
-//            if(!check_email(crew.get(0)) || !check_nickname(crew.get(1))) { // 이메일, 닉네임 형식 검사
-//                // todo 에러처리
-//            }
+            if(!check_email(crew.get(0)) || !check_nickname(crew.get(1))) { // 이메일, 닉네임 형식 검사
+                // todo 에러처리
+            }
         }
         // todo answer 오름차순 정렬
         return answer;
     }
 
-//    /**
-//     * 이메일 형식 검사 함수
-//     * @param email
-//     * @return
-//     */
-//    public static boolean check_email(String email) {
-//        if(email.length() < 11 || email.length() >= 20) return false;
-//        // 이메일 정규식 검사
-//        return true;
-//    }
+    /**
+     * 이메일 형식 검사 함수
+     * @param email
+     * @return
+     */
+    public static boolean check_email(String email) {
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@email.com$";
+        if(email.length() < 11 || email.length() >= 20) return false; // 길이 예외처리
+        return Pattern.matches(regex, email); // 도메인 예외처리
+    }
 
     /**
      * 닉네임 형식 검사 함수
@@ -33,7 +33,7 @@ public class Problem6 {
      */
     public static boolean check_nickname(String nickname) {
         String regex = "^[가-힣]*$";
-        if(nickname.length() < 1 || nickname.length() >= 20) return false;
-        return Pattern.matches(regex, nickname);
+        if(nickname.length() < 1 || nickname.length() >= 20) return false; // 길이 예외처리
+        return Pattern.matches(regex, nickname); // 한글 예외처리
     }
 }
