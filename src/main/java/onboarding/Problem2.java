bd48736f71af9a5730fa6545e1e2f35d585866cc
bnbnac <bnbnac@naver.com>
2022-11-01 19:48:30 +0900

    refactor: (P2)인덴트를 줄이긴 줄였는데...

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,19 @@
 package onboarding;
 
 import java.util.Stack;
+import java.util.ArrayList;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         int prevSize = 0;
         int reversed = -1;
         Stack<Character> stack = makeStack(cryptogram);
-
+//        while (prevSize != stack.size()) {
+//            prevSize = stack.size();
+//            stack = removeDuplication(stack);
+//            reversed *= -1;
+//        }
+        String answer = removeDuplication(stack).toString();
         return answer;
     }
 
@@ -20,28 +26,16 @@ public class Problem2 {
         return stack;
     }
 
-    private static Stack<Character> removeDuplication(Stack<Character> stack) {
-        int i = 0;
+    public static Stack<Character> removeDuplication(Stack<Character> stack) {
         int isPrevSame = 0;
         Stack<Character> removed = new Stack<>();
-
+        ArrayList<Object> args = new ArrayList<>();
+        args = makeArgs(stack, removed, isPrevSame);
         while (!stack.empty()) {
-            char cur = stack.pop();
-            if (removed.empty()) {
-                removed.push(cur);
-                continue;
-            }
-            if (removed.peek() == cur) {
-                removed.pop();
-                isPrevSame = 1;
-                removed.push(cur);
-                continue;
-            }
-            if (isPrevSame == 1) {
-                removed.pop();
-            }
-            isPrevSame = 0;
-            removed.push(cur);
+            stack = (Stack<Character>)args.get(0);
+            removed = (Stack<Character>)args.get(1);
+            isPrevSame = (Integer)args.get(2);
+            args = remove(stack, removed, isPrevSame);
         }
         if (isPrevSame == 1) {
             removed.pop();
@@ -49,10 +43,33 @@ public class Problem2 {
         return removed;
     }
 
-    private static void checkPrev(Stack<Character> stack, Stack<Character> removed) {
+    public static ArrayList remove(Stack<Character> stack, Stack<Character> removed, int isPrevSame) {
+        ArrayList<Object> args = new ArrayList<>();
         char cur = stack.pop();
-        char prev = removed.peek();
+        if (removed.empty()) {
+            removed.push(cur);
+            return makeArgs(stack, removed, isPrevSame);
+        }
+        if (removed.peek() == cur) {
+            removed.pop();
+            isPrevSame = 1;
+            removed.push(cur);
+            return makeArgs(stack, removed, isPrevSame);
+        }
+        if (isPrevSame == 1) {
+            removed.pop();
+        }
+        isPrevSame = 0;
+        removed.push(cur);
+        return makeArgs(stack, removed, isPrevSame);
+    }
 
+    public static ArrayList makeArgs(Stack<Character> stack, Stack<Character> removed, int isPrevSame) {
+        ArrayList<Object> args = new ArrayList<>();
+        args.add(stack);
+        args.add(removed);
+        args.add(isPrevSame);
+        return args;
     }
 }
 
