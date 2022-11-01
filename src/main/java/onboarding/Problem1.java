951eeeba4c61c8967886d119f7a39b1f6d650e42
kksks3192 <kksks3192@gmail.com>
2022-11-01 11:24:29 +0900

    chore(problem1): 기능 주석 및 줄바꿈 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -29,16 +29,19 @@ class Problem1 {
     public static int getScore(List<Integer> pages){
         int max_score=0;
 
+        //덧셈과 곱셈 중 큰 수 점수로 반환
         for(int i=0;i<2;i++){
             int sum=0;
             int mlt=1;
             int page = pages.get(i);
             int max = 0;
+            
             while(page>0) {
                 sum += page%10;
                 mlt *= page%10;
                 page = page/10;
             }
+
             max = Math.max(sum, mlt);
             if(max_score<max) max_score = max;
         }
@@ -48,9 +51,8 @@ class Problem1 {
     public static int getWinner(List<Integer> pobi, List<Integer> crong){
         int winner = Integer.MAX_VALUE;
         int pobi_score = getScore(pobi);
-        System.out.println("pobi:"+ pobi_score);
         int crong_score = getScore(crong);
-        System.out.println("crong:"+ crong_score);
+
         if(pobi_score>crong_score){
             winner = 1;
         }else if(pobi_score<crong_score){
@@ -58,6 +60,7 @@ class Problem1 {
         }else if(pobi_score==crong_score){
             winner = 0;
         }
+
         return winner;
     }
 
