991a44e300e44aa87ad430d2d146e492dd253bdd
Sieun Park <psieun0606@gmail.com>
2022-11-01 23:51:56 +0900

    feat: problem7 친구리스트에서 공통된 사용자를 찾는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
@@ -26,10 +27,34 @@ public class Problem7 {
         return friend.get(0);
     }
 
+    static ArrayList<String> findCommonUser(String user, ArrayList<String> friendsOfUser, List<List<String>> friends) {
+        ArrayList<String> commonFriend = new ArrayList<>();
+        ArrayList<String> realCommonFriend = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            for (String myFriend : friendsOfUser) {
+                commonFriend.add(addFriends(myFriend, friend));
+            }
+        }
+
+        for (int i = 0; i < commonFriend.size(); i++) {
+            Collections.sort(commonFriend);
+            if (!commonFriend.get(i).equals(user)) {
+                if (!realCommonFriend.contains(commonFriend.get(i))) {
+                    realCommonFriend.add(commonFriend.get(i));
+                }
+            }
+        }
+
+        return realCommonFriend;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String, Integer> score = new HashMap<>();
 
         ArrayList<String> friendsOfUser = makeFriendList(user, friends);
+        ArrayList<String> commonFriend = findCommonUser(user, friendsOfUser, friends);
 
         return answer;
     }
