fac9185379a8c38b69239c99deb8e906035e928e
heohyeonjink <hyeonjin4870@naver.com>
2022-11-01 10:48:07 +0900

    feat(PROBLEM6) : formsMap 생성하기
    문자열을 2개씩 나누어 2자리 문자를 key, 해당 인덱스를 list로 하는 해쉬맵 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        HashMap<String, ArrayList<Integer>> formsMap = new HashMap<>();
+        List<String> answer = (Arrays.asList("answer"));
+
+        //formsMap만들기
+        for(int i=0;i<forms.size();i++){
+            List<String> tmp = forms.get(i);
+            makeFormsMap(tmp.get(1),i,formsMap); //tmp.get(1)은 닉네임
+        }
+
         return answer;
     }
+
+    public static void makeFormsMap(String nickName, int idx, HashMap<String, ArrayList<Integer>> formsMap){
+        if(nickName.length()<2) return;
+
+        for(int i=0;i<nickName.length()-1;i++){
+            String doubleChar = nickName.substring(i, i+2);
+
+            if(formsMap.containsKey(doubleChar)){
+                formsMap.get(doubleChar).add(idx);
+            }
+            else{
+                formsMap.put(doubleChar, new ArrayList<Integer>(Arrays.asList(idx)));
+            }
+        }
+
+    }
+
 }
