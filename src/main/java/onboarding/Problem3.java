e46e6bcd1a546c63535cfec10945f54363d5808f
minju412 <ln8847@ajou.ac.kr>
2022-11-01 13:37:29 +0900

    fix: [p3] number 가 자연수가 아닌 경우 예외 처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,10 +1,17 @@
 package onboarding;
 
 public class Problem3 {
+
+	public static final int EXCEPTION = -1;
+
 	public static int solution(int number) {
 		int cnt = 0;
 		String strNumber = "";
 		char oneLetter = ' ';
+
+		if (checkRestrictions(number) == EXCEPTION) {
+			return EXCEPTION;
+		}
 
 		for (int i = 1; i <= number; i++) {
 			strNumber = String.valueOf(i); // int -> string 변환
@@ -19,5 +26,25 @@ public class Problem3 {
 		}
 
 		return cnt;
+	}
+
+	/**
+	 * 제한사항을 위배했는지 체크하는 메서드
+	 * @param number
+	 * @return
+	 */
+	private static Integer checkRestrictions(int number) {
+		if (!isNaturalNumber(number)) {
+			return EXCEPTION;
+		}
+		return 0;
+	}
+
+	// number 가 자연수가 아닌 경우 예외
+	private static boolean isNaturalNumber(int number) {
+		if (number < 1) {
+			return false;
+		}
+		return true;
 	}
 }
