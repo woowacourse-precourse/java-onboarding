e1dfa57352ce13bc8ba1de5e40cdd1e71814882b
yhkee0404 <yhkee0404@gmail.com>
2022-11-01 19:20:07 +0900

    Update Problem3.java with camelcase

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,5 @@
 package onboarding;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Problem3 {
@@ -14,29 +12,29 @@ public class Problem3 {
                 .toArray();
 
         // The number of claps accumulated from 1 to all the previous numbers.
-        int total_num_claps = 0;
+        int totalNumClaps = 0;
 
         // The number of claps added from the very previous number.
-        int prev_num_claps = 0;
+        int prevNumClaps = 0;
 
         // The very previous number.
-        int prev_number = 0;
+        int prevNumber = 0;
 
         for (final int digit: digits) {
 
             // Count the number of claps resulting from the previous number
             // for when every 10 last digit is exhaustively considered for the ones place.
-            total_num_claps = (total_num_claps - prev_num_claps) * 10
-                    + prev_number * 3;
+            totalNumClaps = (totalNumClaps - prevNumClaps) * 10
+                    + prevNumber * 3;
             
             // Count the number of claps for when any of 10 digits may not be included in ones place.
-            total_num_claps += prev_num_claps * (digit + 1)
+            totalNumClaps += prevNumClaps * (digit + 1)
                     + digit / 3;
             
-            prev_num_claps += digit != 0 && digit % 3 == 0 ? 1 : 0;
-            prev_number = prev_number * 10 + digit;
+                    prevNumClaps += digit != 0 && digit % 3 == 0 ? 1 : 0;
+            prevNumber = prevNumber * 10 + digit;
         }
-        final int answer = total_num_claps;
+        final int answer = totalNumClaps;
         return answer;
     }
 }
