3e29682d4fa44de6613d153e3c8942b14387e876
seung-hun2 <jungsh2328@naver.com>
2022-11-01 20:53:56 +0900

    7번 문제 - userf 리스트에 user와 친구인 사람들 저장 후 friends 리스트에서 userf에 들어있으면서 user와는 다른 목록을 data 배열에 10점의 점수와 함께 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,6 +5,27 @@ import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
+        String[][] data = new String[friends.size()][2];
+        List<List<String>> result = new ArrayList<>();
+        List<String> value = new ArrayList<>();
+        List<String> userf = new ArrayList<>();
+        for(int i=0;i<friends.size();i++){
+            if(friends.get(i).get(1).equals(user)){
+                userf.add(friends.get(i).get(0));
+            }
+        }
+
+        int a=0;
+        for(int i=0;i<userf.size();i++){
+            for(int j=0;j<friends.size();j++) {
+                if (friends.get(j).get(0).equals(userf.get(i)) && !friends.get(j).get(1).equals(user)) {
+                    data[a][0] = friends.get(j).get(0);
+                    data[a][1] = "10";
+                    a++;
+                }
+            }
+        }
+
 
         return answer;
     }
