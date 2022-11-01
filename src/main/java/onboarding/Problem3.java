48eb56676a2e95cdff64ed9492c0688224fc4718
devEdok13 <dev.edok13@gmail.com>
2022-11-01 16:10:19 +0900

    feat: P03-countDigit

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,9 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    //  입력된 숫자의 자리수를 계산하는 메소드
+    private static int countDigit(int number){
+        return (int) Math.log10(number);
+    }
 }
