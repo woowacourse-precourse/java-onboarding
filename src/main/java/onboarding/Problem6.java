c160c594e7724a15895b6c1889ba1895dd2b8a9f
DongWooE <ehddn977@naver.com>
2022-11-01 18:54:36 +0900

    [p6][change] searchNickName, whereExisted 메소드 수정 for문 조건 수정 이미 존재 시에 해당 map의 value값도 result에 저장하도록 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,12 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem6 {
 
     List<List<String>> forms = new ArrayList<>();
     Map<String, String> map = new HashMap<String, String>();
+    Set<String> result = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
         Problem6 p6 = new Problem6();
         p6.forms = forms;
@@ -24,8 +22,16 @@ public class Problem6 {
 
     private void searchNickName(String email, String nickName){
         for(int i = 0; i < nickName.length()-1; i++){
-            String substring = nickName.substring(i, i + 1);
-            if(!map.containsKey(substring)) map.put(substring, email);
+            String substring = nickName.substring(i, i + 2);
+            whetherExisted(email, substring);
+        }
+    }
+
+    private void whetherExisted(String email, String subStr){
+        if(!map.containsKey(subStr)) map.put(subStr, email);
+        else{
+            result.add(email);
+            result.add(map.get(subStr));
         }
     }
 }
