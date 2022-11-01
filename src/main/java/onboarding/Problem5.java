c04655daae98c11d960370e9514f4c133582cfd7
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-01 20:54:52 +0900

    Problem5 1차 완성
    getList 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    private static final int UNIT[]={50000,10000,5000,1000,500,100,10};
+    private static final int UNIT[]={50000,10000,5000,1000,500,100,50,10,1};
 
     public static List<Integer> solution(int money) {
         if(input_exception(money)) return Collections.emptyList();
@@ -14,6 +14,15 @@ public class Problem5 {
     }
 
     private static List<Integer> getList(int money) {
+        List<Integer> result=new ArrayList<>();
+
+        for(int i=0;i<UNIT.length;i++){
+            int num=money/UNIT[i];
+            result.add(num);
+            money-=num*UNIT[i];
+        }
+
+        return result;
     }
 
     private static boolean input_exception(int input){
