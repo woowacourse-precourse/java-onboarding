b566fd65076f95fa5917bf4aaabdf5a093fb5950
hsh_519 <suk000519@naver.com>
2022-11-01 15:33:47 +0900

    Problem1: 기능 구현(pageMul)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,10 @@ class Problem1 {
         int res = (int)(page/100) + (int)(page/10) + ((page%100)%10);
         return res;
     }
+    public static int pageMul(int page) {
+        int res = (int)(page/100) * (int)(page/10) * ((page%100)%10);
+        return res;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
