cbd86a5a28ffeb1eb934445801b7d702b170a3a0
이건회 <rg970604@naver.com>
2022-11-01 20:16:17 +0900

    feat(Problem2.java) : 예외 검증 처리 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.regex.Pattern;
+
 public class Problem2 {
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 1000;
@@ -7,5 +9,16 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+    private static void cryptogramTypeValidation(String cryptogram) {
+        if (!Pattern.matches(ENGLISH_PATTERN, cryptogram)) {
+            throw new IllegalArgumentException("영소문자만 입력 가능합니다");
+        }
+    }
+
+    private static void cryptogramLengthValidation(int cryptogramLength) {
+        if (MIN_LENGTH > cryptogramLength || cryptogramLength > MAX_LENGTH) {
+            throw new IllegalArgumentException("문자열의 길이는 1 이상 1000 이하만 허용합니다");
+        }
     }
 }
