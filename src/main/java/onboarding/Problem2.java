75988e75954bd4602fedf0d007396f982c7a1fff
Yh0sun <hy770819@naver.com>
2022-11-01 14:43:14 +0900

    func1 add

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,7 +4,8 @@ public class Problem2 {
     public static String solution(String cryptogram) {
 
         //1. answer에  cryptogram 첫문자 넣기
-        String answer ="answer";
+        String answer = Character.toString(cryptogram.charAt(0));
+
 
         //2. cryptogram 하나씩 읽어서
         //      - answer 마지막 글자랑 같으면 answer 마지막 글자 삭제
