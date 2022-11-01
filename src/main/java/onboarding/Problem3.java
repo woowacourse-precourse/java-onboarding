8cb5d422a3cebdf110a6636739c0e9c2aea9d2ae
JasmineAir <gofoxfox@naver.com>
2022-11-01 17:00:45 +0900

    Problem3 Solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,30 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int temp = 0;
+        int current_num = 1;
+
+        for(int i=1; i<=number; i++){
+            current_num = i;
+            temp = current_num;
+            while(temp>0){
+                current_num = temp%10;
+                if(current_num == 3 || current_num == 6 || current_num == 9) {
+                    answer ++;
+                }
+                temp /= 10;
+            }
+        }
+
         return answer;
     }
+
+    public static void main (String [] args) {
+
+        int number = 33;
+
+        Problem3 prob = new Problem3();
+        System.out.println(prob.solution(number));
+    }
+
 }
