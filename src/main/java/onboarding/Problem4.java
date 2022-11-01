d2e6432e9c508d65378dd27d330f2f462a2add5b
masiljangajji <hjk172262@gmail.com>
2022-11-01 20:26:16 +0900

    feat: Problem5_알파벳 대문자 소문자 그 외의문자 판별

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
+        char ch;
+
+        for(int i=0;i<word.length();i++)
+        {
+            if(word.charAt(i)>='a'&&word.charAt(i)<='z')
+            {
+
+            }
+            else if(word.charAt(i)>='A'&&word.charAt(i)<='Z')
+            {
+
+            }
+            else
+
+
+        }
+
         return answer;
     }
 }
