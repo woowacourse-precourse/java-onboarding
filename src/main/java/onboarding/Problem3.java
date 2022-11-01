4858236b2a1ac5d3431c0ef208821ee4a888d519
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-01 22:16:22 +0900

    feat 369라는 게임의 구현을 통해 특정한 양의 정수에 장치를 넣는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int countClap(int a){
+
+        int checkClap = 0 ;
+        while(a > 0){
+            int num = a % 10;
+            if(num ==3 || num == 6 || num == 9){
+                checkClap++;
+            }
+            a = a/10;
+        }
+        return checkClap;
+    }
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i =1; i<=number; i++){
+            answer += countClap(i);
+        }
         return answer;
     }
 }
