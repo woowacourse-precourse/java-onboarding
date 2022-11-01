531fa273196abec02f0762e7b8749c4c0cba66e2
otndus <xuyeon219@gmail.com>
2022-11-01 13:19:21 +0900

    [기능 구현] Problem2 중복문자 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,20 @@ package onboarding;
 
 public class Problem2 {
 
+    public static StringBuilder deleteDuplicate(StringBuilder str){
+        int firstIndex = checkDuplicate(str);
+        int sum = 0;
+        if(firstIndex!=-1){
+            for(int i = firstIndex+1; i < str.length(); i++){
+                if (str.charAt(firstIndex) == str.charAt(i))
+                    sum+=1;
+                else break;
+            }
+            str.delete(firstIndex, firstIndex+sum+1);
+        }else return str;
+        return deleteDuplicate(str);
+    }
+
     public static int checkDuplicate(StringBuilder str){
         for(int i = 0; i < str.length()-1; i++){
             if(str.charAt(i) == str.charAt(i+1)) return i;
@@ -9,7 +23,8 @@ public class Problem2 {
     }
 
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        StringBuilder str = new StringBuilder(cryptogram);
+        String answer = String.valueOf(deleteDuplicate(str));
         return answer;
     }
 }
