3c15873074ce8ab52a466fbcbf588d16c9cd2db3
sschan99 <seungchan1219@gmail.com>
2022-11-01 17:07:07 +0900

    feat: solve problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem3 {
+    public static int clab(int num) {
+        int count = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            if (digit % 3 == 0 && digit != 0) {
+                count++;
+            }
+            num /= 10;
+        }
+        return count;
+    }
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int total = 0;
+
+        for (int i = 1; i <= number; i++) {
+            total += clab(i);
+        }
+        return total;
     }
 }
