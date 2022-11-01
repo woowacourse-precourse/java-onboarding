e7bb1875031dfdaab00d4457772c3f4c6f6043a6
eom-tae-in <eti0728@daum.net>
2022-11-01 19:59:25 +0900

    feat: static value

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+    static StringBuilder sb = new StringBuilder();
+    static boolean checkModifyText;
+    static boolean checkPre;
+    static boolean checkCurrent;
+    static boolean checkNotLowerCase;
+    public static String solution(String cyptogram) {
+        if (exceptionMethod(cyptogram)) {
+            return cyptogram;
+        }
+        do {
+            cyptogram = (cyptogram.length() >= 3 ? longTextDeduplication(cyptogram) : shortTextDuplication(cyptogram));
+        } while (checkModifyText);
+        return cyptogram;
     }
 }
