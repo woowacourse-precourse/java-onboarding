b8d494d701af5e0fe47502d81798960d6eaf97a1
jiwon1217 <koagjw@gmail.com>
2022-11-01 15:21:58 +0900

    [Fix] leftIndex, rightIndex 변수 선언

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,31 +1,12 @@
 package onboarding;
 
 public class Problem2 {
-    public static Integer findDuplicateIndex(String cryptogram) {
-        for (int index = 0; index < cryptogram.length() - 1; index++) {
-            if (cryptogram.charAt(index) == cryptogram.charAt(index + 1)) {
-                return index;
-            }
-        }
-        return -1;
-    }
-
-    public static String deleteDuplicateString(String cryptogram, Integer index) {
-        cryptogram = cryptogram.substring(0, index) + cryptogram.substring(index + 2);
-        return cryptogram;
-    }
 
     public static String solution(String cryptogram) {
         String answer;
 
-        while (true) {
-            Integer duplicateIndex = findDuplicateIndex(cryptogram);
-            if (duplicateIndex == -1) {
-                break;
-            }
-            cryptogram = deleteDuplicateString(cryptogram, duplicateIndex);
-        }
-        answer = cryptogram;
+        int leftIndex = 0;
+        int rightIndex = 1;
 
         return answer;
     }
