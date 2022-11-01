a26b974906d9ad39b9198cd5ad5beae12a03b896
Toa <ld980105@snu.ac.kr>
2022-11-01 10:39:18 +0900

    Feat : add getNamemap method
    getNamemap method is used to get a map of divided letters

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        return answer;
+        HashMap<String, Integer> nameMap = getNameMap(forms);
+        return null;
+    }
+    public static HashMap<String, Integer> getNameMap(List<List<String>> forms){
+        HashMap<String, Integer> nameMap = new HashMap<>();
+        for (List<String> form : forms){
+            String name = form.get(1);
+            for (int i = 0 ; i < name.length() - 1 ; i++){
+                String subsetOfName = name.substring(i, i + 2);
+                if (nameMap.containsKey(subsetOfName))
+                    nameMap.put(subsetOfName, nameMap.get(subsetOfName) + 1);
+                else {
+                    nameMap.put(subsetOfName, 1);
+                }
+            }
+        }
+        return nameMap;
     }
 }
