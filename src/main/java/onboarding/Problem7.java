67827f59e38a4c633bc6ac77b8469c896bede2e5
hyejuc <chjjudy@naver.com>
2022-11-01 18:20:10 +0900

    findUserFriend - user와 친구인 사람 list 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> userFriends = findUserFriend(user, friends);
         return answer;
+    }
+    public static List<String> findUserFriend(String user, List<List<String>> friends){
+        List<String> userFriends = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            if(friends.get(i).get(0).equals(user)){
+                userFriends.add(friends.get(i).get(1));
+            }
+            else if(friends.get(i).get(1).equals(user)){
+                userFriends.add(friends.get(i).get(0));
+            }
+        }
+        return userFriends;
     }
 }
