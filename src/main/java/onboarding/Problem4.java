b5462e7d4fe637334fc0de9bfcc3ada6c4d21430
이건회 <rg970604@naver.com>
2022-11-01 20:41:14 +0900

    feat(Problem4.java) : 상수 선언

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,14 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int MIN_WORD_SIZE = 1;
+    private static final int MAX_WORD_SIZE = 1000;
+
+    private static final int UPPERCASE_ASCII_START = 65;
+    private static final int UPPERCASE_ASCII_END = 90;
+
+    private static final int LOWERCASE_ASCII_START = 97;
+    private static final int LOWERCASE_ASCII_END = 122;
     public static String solution(String word) {
         String answer = "";
         return answer;
