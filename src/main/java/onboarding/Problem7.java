c2a4374025d067c2990289f3feedef616bdb3684
hgo641 <hgo641@gmail.com>
2022-11-01 20:10:39 +0900

    feat: Problem7 class UserManager 생성 & 모든 친구 관계를 저장하는 friendsMap 생성 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /** 추천 친구 목록을 정렬하기 위한 클래스 */
 class RecommendedFriendComparator implements Comparator<String> {
@@ -28,6 +25,46 @@ class RecommendedFriendComparator implements Comparator<String> {
     }
 }
 
+/** 모든 유저들의 친구 관계를 관리하는 클래스 */
+class UserManager{
+    static final int VISIT_POINT = 1;
+    static final int FRIEND_POINT = 10;
+    private static Map<String, List<String>> friendsMap = Collections.EMPTY_MAP;
+    private static UserManager userManager = new UserManager();
+    private UserManager(){}
+
+
+    public static UserManager getUserManager(){
+        friendsMap = new HashMap<>();
+        return userManager;
+    }
+
+    /**
+     * friendsMap에 데이터를 추가하는 함수
+     * @param sourceUser friendsMap에 들어갈 key가 된다
+     * @param destUser source를 key로 가지는 리스트에 add할 String
+     */
+    private void addFriend(String sourceUser, String destUser){
+        List<String> friends = friendsMap.getOrDefault(sourceUser, new ArrayList<>());
+        friends.add(destUser);
+        friendsMap.put(sourceUser, friends);
+    }
+
+    /**
+     * friends를 순회하며, 각 유저들의 친구 관계를 friendsMap에 저장
+     * @param friends 메소드 solution에서 인자로 받는 친구 관계 정보
+     */
+    public Map<String, List<String>> getFriendsMap(List<List<String>> friends){
+        for(List<String> friend : friends){
+            String firstUser = friend.get(0);
+            String secondUser = friend.get(1);
+
+            addFriend(firstUser, secondUser);
+            addFriend(secondUser, firstUser);
+        }
+        return friendsMap;
+    }
+}
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
