161c450b851f7b53a6596048459bbb53e022f570
June1010 <tjrwns1010@naver.com>
2022-11-01 23:23:34 +0900

    chore: 기능 목록 커밋 (Mock 함수만 생성)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuffer answer = new StringBuffer();
+        return answer.toString();
+    }
+
+    private static char convert(char chr) {
+        return ' ';
+    }
+
+    private static boolean isValidChar(char chr) {
+        return false;
     }
 }
