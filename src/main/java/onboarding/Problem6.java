9eb558a19e3227c237c3923140d3ca78dd9ce850
WhalesBob <ndw8200@naver.com>
2022-11-01 18:57:39 +0900

    Add restricted check code in Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,21 +1,25 @@
 package onboarding;
 
+import onboarding.newExceptions.InvalidRestrictionException;
+
 import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        Problem6 restrictions = new Problem6();
+        if(restrictions.isTeamSizeNotValid(forms)){
+            throw new InvalidRestrictionException("크루원들의 수가 1명 이상 10,000명 이하에 속하지 않습니다.");
+        }
 
         Set<String> emailSet = new HashSet<>();
 
-        for(int i = 0; i < forms.size(); i++){ // 애초에 n^2 로직이 아니었는데?
-            for(int j = i; j < forms.size(); j++){ // 다 보기는 해야함. 그래야 맞지.
-                // 정렬을 하더라도, 애초에 맨 뒤 두개만 겹치면 상관이 없어진다. 애초에 그냥 이걸로 가는게 맞을지도
+        for(int i = 0; i < forms.size(); i++){
+            for(int j = i; j < forms.size(); j++){
                 if((i != j) && isSimilar(forms.get(i).get(1),forms.get(j).get(1))){
-                    emailSet.add(forms.get(i).get(0));
-                    emailSet.add(forms.get(j).get(0));
+                    addOnEmailSet(emailSet,forms.get(i),forms.get(j));
                 }
             }
-        }// 여기서 n^2/2 인데, 여기서 더 줄여보자.
+        }
 
         List<String> answer = new ArrayList<>(emailSet);
         Collections.sort(answer);
@@ -29,5 +33,24 @@ public class Problem6 {
             }
         }
         return false;
+    }
+    static void addOnEmailSet(Set<String> emailSet,List<String> formA, List<String> formB){
+        if(isViolatedRestrictions(formA.get(0),formA.get(1)) || isViolatedRestrictions(formB.get(0),formB.get(1))){
+            throw new InvalidRestrictionException("제한사항에 맞지 않는 인원이 존재합니다.");
+        }
+
+        emailSet.add(formA.get(0));
+        emailSet.add(formB.get(0));
+    }
+    static boolean isViolatedRestrictions(String email, String nickname){
+        boolean isEmailLengthValid = (11 <= email.length() && email.length() < 20);
+        boolean isEmailFormatValid = email.endsWith("@email.com");
+        boolean isNickNameHaveOnlyKorean = nickname.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*");
+        boolean isNickNameSizeValid = (1 <= nickname.length() && nickname.length() <= 20);
+
+        return !(isEmailLengthValid && isEmailFormatValid && isNickNameSizeValid && isNickNameHaveOnlyKorean);
+    }
+    boolean isTeamSizeNotValid(List<List<String>> forms){
+        return !(1 <= forms.size() && forms.size() <= 10_000);
     }
 }
