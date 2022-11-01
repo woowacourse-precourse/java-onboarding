da058c1640b43864a25564ef4428852966537303
bnbnac <bnbnac@naver.com>
2022-11-01 22:45:07 +0900

    feat: (P4)알파벳에 해당하는 아스키코드를 청개구리 사전에 맞게 변환하는 메소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,18 +5,35 @@ import java.util.ArrayList;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        ArrayList<Object> wordList = getAsciiList(word);
-
+        ArrayList<Integer> wordList = getAsciiList(word);
+        ArrayList<Integer> reflectedList = reflectList(wordList);
         return answer;
     }
 
-    private static ArrayList<Object> getAsciiList(String word) {
+    private static ArrayList<Integer> getAsciiList(String word) {
         char[] ch = word.toCharArray();
         ArrayList<Integer> asciiList = new ArrayList<>();
         for (char c : ch) {
             asciiList.add((int) c);
         }
         return asciiList;
+    }
+
+    private static ArrayList<Integer> reflectList(ArrayList<Integer> asciiList) {
+        ArrayList<Integer> reflectedList = new ArrayList<>();
+        for (Integer i : asciiList) {
+            reflectedList.add(reflectAlphabet(i));
+        }
+    }
+
+    private static int reflectAlphabet(int i) {
+        if (65 <= i && 90 >= i) {
+            i = 65 + 90 - i;
+        }
+        if (97 <= i && 122 >= i) {
+            i = 97 + 122 - i;
+        }
+        return i;
     }
 
 }
