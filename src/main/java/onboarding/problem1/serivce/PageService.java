7de690bd040d2043f7a3946a7df64a9c6d80f12d
이경범 <tkdwk567@naver.com>
2022-11-01 15:54:34 +0900

    # 1 solution @@ refactoring - Service 코드 분리

---
+++ src/main/java/onboarding/problem1/serivce/PageService.java
@@ -0,0 +1,90 @@
+package onboarding.problem1.serivce;
+
+import onboarding.problem1.infra.PageNumberAndSizeValidation;
+import onboarding.problem1.type.ResultValue;
+import onboarding.problem1.type.ValidationValue;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PageService {
+
+    /**
+     * @param woowahanTeachers
+     * @param validation
+     *
+     * @throws IllegalArgumentException
+     * */
+    public int findMaxValue(List<Integer> woowahanTeachers, PageNumberAndSizeValidation validation) throws IllegalArgumentException {
+        Integer leftPageNumber = woowahanTeachers.get(ValidationValue.LEFT_INDEX.getValue());
+        Integer rightPageNumber = woowahanTeachers.get(ValidationValue.RIGHT_INDEX.getValue());
+
+        validation.checkedPageNumber(leftPageNumber, rightPageNumber);
+
+        // 왼쪽과 오른쪽 페이지의 각 자리 숫자의 합 중에 큰 값
+        int sumMaxValue = getSumMaxValue(leftPageNumber, rightPageNumber);
+
+        // 왼쪽과 오른쪽 페이지의 각 자리 숫자의 곱 중에 제일 큰 값
+        int multiplicationMaxValue = getMultiplicationMaxValue(leftPageNumber, rightPageNumber);
+
+        return Math.max(sumMaxValue, multiplicationMaxValue);
+    }
+
+    protected int getSumMaxValue(Integer leftPageNumber, Integer rightPageNumber) {
+        int leftSumValue = getSumValue(leftPageNumber);
+        int rightSumValue = getSumValue(rightPageNumber);
+
+        return Math.max(leftSumValue, rightSumValue);
+    }
+
+    protected int getSumValue(Integer pageNumber) {
+        String[] pageNumberStringSplit = String.valueOf(pageNumber).split("");
+        return Arrays.stream(pageNumberStringSplit)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    protected int getMultiplicationMaxValue(Integer leftPageNumber, Integer rightPageNumber) {
+        int leftValue = getMultiplicationValue(leftPageNumber);
+        int rightValue = getMultiplicationValue(rightPageNumber);
+
+        return Math.max(leftValue, rightValue);
+    }
+
+    protected int getMultiplicationValue(Integer pageNumber) throws NumberFormatException {
+        String[] PageNumberStringSplit = String.valueOf(pageNumber).split("");
+        int result = 1;
+
+        for (String data : PageNumberStringSplit) {
+            try {
+                result *= Integer.parseInt(data);
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+                return ResultValue.EXCEPTION_VALUE.getValue();
+            }
+        }
+
+        return result;
+    }
+
+    public int getResult(int pobiMaxValue, int crongMaxValue) {
+        Integer exceptionValue = ResultValue.EXCEPTION_VALUE.getValue();
+        if (pobiMaxValue == exceptionValue || crongMaxValue == exceptionValue) {
+            return exceptionValue;
+        }
+
+        if (pobiMaxValue > crongMaxValue) {
+            return ResultValue.POBI_WIN.getValue();
+        }
+
+        if (pobiMaxValue < crongMaxValue) {
+            return ResultValue.CRONG_WIN.getValue();
+        }
+
+        if (pobiMaxValue == crongMaxValue) {
+            return ResultValue.POBI_CRONG_DRAW.getValue();
+        }
+
+        return exceptionValue;
+    }
+}
--- src/main/java/onboarding/problem1/type/ValidationValue.java
+++ src/main/java/onboarding/problem1/type/ValidationValue.java
@@ -2,7 +2,6 @@ package onboarding.problem1.type;
 
 public enum ValidationValue {
 
-    FIXED_LIST_SIZE(2),
     LEFT_INDEX(0),
     RIGHT_INDEX(1);
 
