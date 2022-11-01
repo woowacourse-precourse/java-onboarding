e659bb8a47fa46b6b3f1b1b56e5bb936b32cda58
cpot5620 <=cpot5620@naver.com>
2022-11-01 17:27:27 +0900

    Add isValidMoney() method in 'Bank' class to handle exceptions

--- src/main/java/onboarding/Bank.java
+++ src/main/java/onboarding/Bank.java
@@ -6,6 +6,8 @@ public class Bank {
     private int money;
 
     public Bank(int money) {
+        isValidMoney(money);
+
         this.money = money;
     }
 
@@ -13,5 +15,10 @@ public class Bank {
         MoneyExchanger moneyExchanger = new MoneyExchanger();
 
         return moneyExchanger.toUnits(money);
+    }
+
+    private void isValidMoney(int money) {
+        if (money < 1 || money > 1000000)
+            throw new IllegalArgumentException("money는 1 이상 1,000,000 이하여야 합니다.");
     }
 }
