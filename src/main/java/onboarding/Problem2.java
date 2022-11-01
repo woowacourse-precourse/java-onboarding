9b9c4f5eb4a5e3008f574ea52790324f3127674f
0417taehyun <0417taehyun@gmail.com>
2022-11-01 14:50:07 +0900

    feat: [문제2] 중복된 문자열 발견 및 삭제 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem2 {
     public static String removeDuplicatedCharacters(String cryptogram) {
+        int index = 0;
+        while (index < cryptogram.length() - 1) {
+            char currentCharacter = cryptogram.charAt(index);
+            char nextCharacter = cryptogram.charAt(index + 1);
+            if (currentCharacter == nextCharacter) {
+                cryptogram = cryptogram.replaceFirst(cryptogram.substring(index, index + 2), "");
+                index -= 2;
+            }
+            index++;
+        }
 
+        return cryptogram;
     }
 
     public static String solution(String cryptogram) {
