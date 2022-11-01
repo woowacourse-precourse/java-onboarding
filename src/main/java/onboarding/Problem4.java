1dd39058cf07ca505ed2af748adc0ef3e1b904a2
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-01 19:06:14 +0900

    problem4: add comment

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem4 {
+    //대문자, 소문자 각 range에서 reverse character를 반환하는 함수
     private static char reverseTransferChar(char ascii){
         if((int)'A' <= ascii && ascii <= (int)'Z')
             ascii = (char)((int)'A'+ (int)'Z'- (int)ascii);
@@ -8,7 +9,7 @@ public class Problem4 {
             ascii = (char)((int)'a'+ (int)'z'- (int)ascii);
         return ascii;
     }
-
+    //단어를 규칙에 따라 전체 변환하는 함수
     private static String wordTransfer(String word){
         StringBuilder answer = new StringBuilder();
         for(int i = 0; i < word.length(); i++)
