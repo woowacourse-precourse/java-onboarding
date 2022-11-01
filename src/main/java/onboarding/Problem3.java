79f7fdeb7f4cd16a1f5bd8df19eec862add6c4b3
parking0 <p980710@gmail.com>
2022-11-01 14:41:26 +0100

    problem 3 is done

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
+import java.util.*;
 
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int length = (int)(Math.log10(number)+1);
+
+        for(int i=1; i<=number;i++){
+            int tmp = i;
+            for(int j=0;j<(int)(Math.log10(i)+1);j++) {
+                if(tmp%10==3||tmp%10==6||tmp%10==9){
+                    answer++;
+                }
+                tmp/=10;
+            }
+        }
         return answer;
     }
 }
