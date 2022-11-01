dc8979a78d1909dbf8aad45b91ed7be7d2eab13b
kkh <kkho9654@naver.com>
2022-11-01 20:49:02 +0900

    Add feature list problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+
+/**
+ * 1. 1부터 number 까지 i 반복문
+ * 2. i를 문자열로 변환 후 각 자리수에 3또는 6또는 9가 있는지 검사해서 각 자리수의 3,6,9개수 반환 : clapNum(int num)
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
