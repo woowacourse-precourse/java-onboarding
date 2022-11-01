16e8490fc5da3e08601937434e0bf15ab8c07fbe
jisoolee11 <jisu6414@likelion.org>
2022-11-01 22:03:58 +0900

    problem3 submit

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    public static long countChar(String str, char ch) {
+        return str.chars()
+                .filter(c -> c == ch)
+                .count();
+    }
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int cnt = 0;
+        for (int i = 1; i <= number; i++) {
+            String stringNum = String.valueOf(i);
+            cnt += countChar(stringNum, '3');
+            cnt += countChar(stringNum, '6');
+            cnt += countChar(stringNum, '9');
+
+        }
+        return cnt;
     }
 }
