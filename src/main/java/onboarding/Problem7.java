843d9741836f264ca245bc100791478e102ad4cf
Yejin Shin <shinyejin0212@gmail.com>
2022-11-01 18:58:43 +0900

    feat : solve problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,11 @@
 package onboarding;
 
 import java.util.*;
-
+import java.util.Map.Entry;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        answer = new ArrayList<>();
         Map<String, List<String>> frdList = new HashMap<>();
 
         for (int i = 0; i < friends.size(); i++) {
@@ -31,7 +32,6 @@ public class Problem7 {
                 frdList.put(second, temp);
             }
         }
-        System.out.println(frdList);
 
         Map<String, Integer> scoreMap = new HashMap<>();
 
@@ -41,34 +41,64 @@ public class Problem7 {
         userFriends = frdList.get(user);
 
         Iterator<String> iter = frdList.keySet().iterator();
-        while(iter.hasNext()) {
+        while (iter.hasNext()) {
             String key = iter.next();
-            if(!userFriends.contains(key) && (key != user)){
+            if (!userFriends.contains(key) && (key != user)) {
                 notFriends.put(key, frdList.get(key));
             }
         }
 
         Iterator<String> iter2 = notFriends.keySet().iterator();
-        while(iter2.hasNext()) {
-            String key2 = iter2.next();
-            int score = 0;
+        while (iter2.hasNext()) {
+            String key = iter2.next();
+            int fscore = 0;
 
-            for(String userFriend : userFriends){
-                if(notFriends.get(key2).contains(userFriend)){
-                    score +=10;
+            for (String userFriend : userFriends) {
+                if (notFriends.get(key).contains(userFriend)) {
+                    fscore += 10;
                 }
-                scoreMap.put(key2, score);
+                scoreMap.put(key, fscore);
             }
         }
 
-        System.out.println(userFriends);
-        System.out.println(notFriends);
-        System.out.println(scoreMap);
+        Iterator<String> iter3 = scoreMap.keySet().iterator();
+
+        for (String visitor : visitors) {
+            if (!userFriends.contains(visitor)) {
+                if (scoreMap.containsKey(visitor)) {
+                    int temp = scoreMap.get(visitor);
+                    temp = temp + 1;
+                    scoreMap.put(visitor, temp);
+                } else {
+                    scoreMap.put(visitor, 1);
+                }
+            }
+        }
+        List<Entry<String, Integer>> sortScoreMap = new ArrayList<Entry<String, Integer>>(scoreMap.entrySet());
+        Collections.sort(sortScoreMap, new Comparator<Entry<String, Integer>>() {
+            public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+                if (obj1.getValue().compareTo(obj2.getValue()) == 0) {
+                    return obj1.getKey().compareTo(obj2.getKey());
+                }
+                return obj1.getValue().compareTo(obj1.getValue());
+            }
+        });
+
+//        Iterator<String> iter4 = sortScoreMap.keySet().iterator();
+
+
+        for (Entry<String, Integer> entry : sortScoreMap) {
+            if (answer.size() == 5) {
+                break;
+            }
+            answer.add(entry.getKey());
+        }
 
 
 
         return answer;
     }
+
 }
 
 /**
