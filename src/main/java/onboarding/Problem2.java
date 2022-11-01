03a8f17b17d677552dab392ceff2aacba13b52f3
Yh0sun <hy770819@naver.com>
2022-11-01 14:42:18 +0900

    func list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        //1. answer에  cryptogram 첫문자 넣기
+        String answer ="answer";
+
+        //2. cryptogram 하나씩 읽어서
+        //      - answer 마지막 글자랑 같으면 answer 마지막 글자 삭제
+        //      - answer 마지막 글자랑 다르면 answer에 추가
+
         return answer;
     }
 }
