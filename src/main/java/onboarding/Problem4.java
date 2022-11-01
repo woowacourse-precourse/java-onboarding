2edeab59846d40e64fcfe93606b59f43681ad4a9
d0ri123 <lsh81550@gmail.com>
2022-11-01 09:23:18 +0900

    Problem4 기능구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,19 +7,20 @@ public class Problem4 {
         return answer;
     }
 
-    //TODO:fun함수 기능목록 정의(말 다듬기)
+    //TODO: 리팩토링 필요
     public static String fun(String word){
         char[] arr = word.toCharArray();
-        String str = "";
+        StringBuilder str = new StringBuilder();
         for(int i=0; i<arr.length; i++){
             if(arr[i]>=65 && arr[i]<=90){
-                str += (arr[i] = (char)(155-arr[i]));
+                str.append(arr[i] = (char) (155 - arr[i]));
             }else if(arr[i]>=97 && arr[i]<=122){
-                str += (arr[i] = (char)(219-arr[i]));
+                str.append(arr[i] = (char) (219 - arr[i]));
             }else if(arr[i]==32){
-                str+=" ";
-            }
+                str.append(" ");
+            }else
+                str.append(arr[i]);
         }
-        return str;
+        return str.toString();
     }
 }
