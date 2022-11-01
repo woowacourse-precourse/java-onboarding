2aa6ded2fd46ba1cffdc87b3816db86e107624bc
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 18:45:59 +0900

    refactor : Problem5 클린코드를 위한 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,17 +6,16 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = countAmountMoney(money);
-        return answer;
+        return countAmountMoney(money);
     }
 
     public static List<Integer> countAmountMoney(int money){
         int[] amount = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> countedMoney = new ArrayList<>();
 
-        for(int i=0; i<amount.length; i++){
-            countedMoney.add(money / amount[i]);
-            money %= amount[i];
+        for (int amt: amount) {
+            countedMoney.add(money / amt);
+            money %= amt;
         }
 
         return countedMoney;
