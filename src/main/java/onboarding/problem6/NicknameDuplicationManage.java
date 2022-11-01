c0c103c4f7289ea74ac70eebe5ca5d9797a41be8
seominseok00 <seominseok4834@gmail.com>
2022-11-01 23:24:44 +0900

    feat(문제6): 닉네임 서브 스트링 저장 메소드 구현

--- src/main/java/onboarding/problem6/NicknameDuplicationManage.java
+++ src/main/java/onboarding/problem6/NicknameDuplicationManage.java
@@ -1,11 +1,42 @@
 package onboarding.problem6;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NicknameDuplicationManage {
     private List<List<String>> forms;
+    private HashMap<String, Set<String>> nickNameMap = new HashMap<>();
 
     public NicknameDuplicationManage(List<List<String>> forms){
         this.forms = forms;
     }
+
+    private void saveSubstringNickname(){
+        for(List<String> form : forms){
+            String email = form.get(0);
+            String nickName = form.get(1);
+
+            if(nickName.length() >= 2){
+                for(int endIdx = 2; endIdx <= nickName.length(); endIdx++){
+                    int startIdx = 0;
+                    while(startIdx + endIdx <= nickName.length()){
+                        String nameSubstring = nickName.substring(startIdx, startIdx + endIdx);
+                        if(nickNameMap.containsKey(nameSubstring)){
+                            Set<String> emailSet = nickNameMap.get(nameSubstring);
+                            emailSet.add(email);
+                        }
+                        else{
+                            Set<String> emailSet = new HashSet<>();
+                            emailSet.add(email);
+                            nickNameMap.put(nameSubstring, emailSet);
+                        }
+                        startIdx++;
+                    }
+                }
+            }
+        }
+    }
+    
 }
