27381337f467aa6782f6c05c306584161de53cbc
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 14:04:51 +0900

    [3번] feat: 해당 숫자의 ClapNumber 갯수 찾기
    원래 숫자 길이와 ClapNumber가 제거된 숫자 길이의 차이 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,18 @@ public class Problem3 {
         return answer;
     }
 
+    // 2. 원래 숫자 길이와 ClapNumber가 제거된 숫자 길이의 차이를 찾기
+    // -> 해당 숫자의 ClapNumber 갯수를 찾음
+    public static int find369(String num)
+    {
+        String removedNum =removeClapNumber(num);
+
+        int totalNumLength = num.length();
+        int removedNumLength = removedNum.length();
+
+        return totalNumLength - removedNumLength;
+    }
+
     // 3. 현재 숫자에서 ClapNumber(3,6,9)만 제거하기
     public static String removeClapNumber(String num)
     {
