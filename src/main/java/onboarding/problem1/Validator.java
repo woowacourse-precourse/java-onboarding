a756a8f94ea3a530159faec1cecf315802e6b858
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 17:41:39 +0900

    문제1 / refactor : if문 내부의 로직을 따로 메소드화 하여 코드의 복잡도를 낮춤

--- src/main/java/onboarding/problem1/Validator.java
+++ src/main/java/onboarding/problem1/Validator.java
@@ -10,14 +10,25 @@ public class Validator {
         int leftPage = pages.get(0);
         int rightPage = pages.get(1);
 
-        if (!(isInRange(leftPage) && isInRange(rightPage))) {return false;}
+        if (checkRangeError(leftPage, rightPage)) {return false;}
 
-        if (!((leftPage % 2 == 1 ) && ( rightPage % 2 == 0))) {return false;}
+        if (checkEvenOddError(leftPage, rightPage)) {return false;}
 
-        if (leftPage - rightPage != -1) {return false;}
-
-        return true;
+        return checkContinuousError(leftPage, rightPage);
     }
+
+    private static boolean checkContinuousError(int leftPage, int rightPage) {
+        return leftPage - rightPage != -1;
+    }
+
+    private static boolean checkEvenOddError(int leftPage, int rightPage) {
+        return !((leftPage % 2 == 1) && (rightPage % 2 == 0));
+    }
+
+    private static boolean checkRangeError(int leftPage, int rightPage) {
+        return !(isInRange(leftPage) && isInRange(rightPage));
+    }
+
     public static boolean isInRange(int page) {
         return (page >= START_PAGE) && (page <= END_PAGE);
     }
