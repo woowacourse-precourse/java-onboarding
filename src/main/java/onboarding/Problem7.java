e270fe1aefde1af9cbd881eec7666700f3daba07
이소영 <th42500@naver.com>
2022-11-01 23:55:01 +0900

    [Add] 각 친구들의 점수 얻기 메서드 구현 중

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    private List<String> getScore(String user, List<List<String>> friends, List<String> visitors) {
+
+
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        Problem7 problem7 = new Problem7();
+        answer = problem7.getScore(user, friends, visitors);
+
+
         return answer;
     }
 }
