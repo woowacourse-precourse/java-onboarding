a97173f023d633456df04c532325853e06a84524
ECHO <eunchan1001@gmail.com>
2022-11-01 11:17:47 +0900

    feat(Problem6): result 변수 TreeSet으로 초기화 및 리스트로 변환하여 반환기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,13 @@
 package onboarding;
 
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> result = new TreeSet<String>();
+        return result.stream().collect(Collectors.toList());
     }
 }
