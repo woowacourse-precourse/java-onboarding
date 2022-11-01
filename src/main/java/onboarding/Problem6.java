8ff57b52fb08ddc3da108daf7df417adb78679d1
Jaewoooong Kim <kimje1109@gmail.com>
2022-11-01 17:29:15 +0900

    feat: Add getSlicedNicknames

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    public static List<String> getSlicedNicknames(List<List<String>> forms) {
+        List<String> slicedNicknames = new ArrayList<>();
+        String nickName = "";
+        String slicedNickname = "";
+
+        for (List<String> userInfo : forms) {
+            nickName = userInfo.get(1);
+            for (int index = 0; index < nickName.length() - 1; index++) {
+                slicedNickname = nickName.substring(index, index + 2);
+                if (!slicedNicknames.contains(slicedNickname)) {
+                    slicedNicknames.add(slicedNickname);
+                }
+            }
+        }
+        return slicedNicknames;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
