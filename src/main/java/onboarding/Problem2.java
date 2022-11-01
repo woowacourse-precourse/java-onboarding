a6c29e325119618f0202052f44e7e569419a3576
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-01 21:12:05 +0900

    Problem2 idx2를 1번째부터 시작하도록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,9 +5,9 @@ public class Problem2 {
         String Str = cryptogram;
 
         int idx1 = 0;
-        int idx2 = 0;
+        int idx2;
 
-        for (idx2 = 1; idx2 < cryptogram.length(); idx2++) {
+        for (idx2 = 1; idx2 < Str.length(); idx2++) {
             if(Str.charAt(idx1) == Str.charAt(idx2)) {
                 Str = Str.substring(0,idx1) + Str.substring(idx2+1);
                 idx1 = 0;
