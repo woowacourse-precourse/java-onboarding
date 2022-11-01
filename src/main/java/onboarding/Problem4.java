bd7e24dcfb7bf8642a646aefc9ad8472301c35b5
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 14:57:10 +0900

    feat : solution 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,15 @@ public class Problem4 {
     private static final int MIN = 1;
     private static final int MAX = 1000;
 
+    public static String solution(String word) {
+        if (!canWord(word)) throw new IllegalArgumentException("제한 사항을 위반하였습니다.");
+        String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            answer += changeAlphabet(word.charAt(i));
+        }
+        return answer;
+    }
+
     public static Character changeAlphabet(Character alpha) {
         if (alpha >= 'A' && alpha <= 'Z') return (char) ('A' + 'Z' - alpha);
         if (alpha >= 'a' && alpha <= 'z') return (char) ('a' + 'z' - alpha);
