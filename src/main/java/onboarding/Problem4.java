eaddb29546351c2eaab6d2d5cb662461aa717907
20191777 <97873629+20191777@users.noreply.github.com>
2022-11-01 21:16:53 +0900

    [문제4] : 문자의 아스키값을 계산하여 청개구리 사전 말로 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        char[] asc = new char[word.length()];
+
+        for(int i=0; i<word.length(); i++){
+            if('a'<= word.charAt(i) && 'z'>= word.charAt(i)){
+                asc[i] = (char)('z' - (word.charAt(i) -'a'));
+            }
+            else if('A'<= word.charAt(i) && 'Z'>= word.charAt(i)){
+                asc[i] = (char)('Z' - (word.charAt(i) -'A'));
+            }
+            else{
+                asc[i]=word.charAt(i);
+            }
+        }
+
         String answer = "";
+
         return answer;
     }
+
 }
