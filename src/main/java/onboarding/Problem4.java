33ecf37e9eed001e6ea806407a863ef57ad47a1f
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-01 17:22:41 +0900

    feat: [Prob 4] 청개구리 사전에 맞게 알파벳을 다른 알파벳으로 변환하는 기능 추가 변환되는 알파벳의 아스키코드와 기존 알파벳의 아스키코드의 합은 대문자/소문자 각각에서 일정함을 이용하여, 알파벳을 다른 알파벳으로 변환해 반환하는 convertAlphabet 함수를 구현합니다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,19 @@
 package onboarding;
 
 public class Problem4 {
+    public static char convertAlphabet(char originalAlphabet){
+        int originalAsciiCode=(int) originalAlphabet;
+        int convertedAsciiCode=0;
+        int sumOfAsciiCodeInUpperCase=(int) 'A'+(int) 'Z';
+        int sumOfAsciiCodeInLowerCase=(int) 'a'+(int) 'z';
+
+        if((int) 'Z' >= originalAsciiCode){ // 대문자일 경우
+            convertedAsciiCode=sumOfAsciiCodeInUpperCase-originalAsciiCode;
+        } else if((int) 'a' <= originalAsciiCode){ // 소문자일 경우
+            convertedAsciiCode=sumOfAsciiCodeInLowerCase-originalAsciiCode;
+        }
+        return (char)convertedAsciiCode;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
