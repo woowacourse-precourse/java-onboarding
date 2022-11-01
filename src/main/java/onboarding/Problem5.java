1d54758d2f86945bb46727a5107052fcc7e0069c
ChaCha3088 <cha3088@gmail.com>
2022-11-01 11:31:40 +0900

    feat: problem5 new function divideByBillAndCoin

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,15 +8,28 @@ import java.util.Map;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         ArrayList<Integer> billAndCoinList = new ArrayList<Integer> ();
-        return answer;
+        return billAndCoinList;
+    }
+
+    private static int divideByBillAndCoin(int money) {
+        int quotient = 0;
+        int remainder = money;
+        for ( Integer key : billAndCoin.keySet() ){
+            int divisor = billAndCoin.get(key);
+            quotient = (int)(remainder / divisor);
+            remainder = remainder % divisor;
+            checkQuotient(quotient);
+        }
+
     }
 
     private static void checkQuotient(int quotient) {
-        if (quetient >= 1) {
+        if (quotient >= 1) {
             billAndCoinList.add(quotient);
         } else if (quotient < 1) {
             billAndCoinList.add(quotient);
         }
+        System.out.println(billAndCoinList);
     }
 
     private static Map<Integer, Integer> billAndCoin = Map.ofEntries(
