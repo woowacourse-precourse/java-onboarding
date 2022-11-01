885732c134e0ff7a68058e8ba4940140a3279b07
ckyeon <qsky8520@gmail.com>
2022-11-01 15:46:39 +0900

    Add isClap() to Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static boolean isClap(int number) {
+        return number == 3 || number == 6 || number == 9;
     }
 
     private static List<Integer> getPlaceValues(int number) {
