6e0856cb33b869db52c4cab645a7ecfde15cf4cb
Woong99 <dndry1072@naver.com>
2022-11-01 13:49:26 +0900

    feat: 문제 7 - 사용자의 친구 목록을 구하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> getUserFriendsList(String user, List<List<String>> friends) {
+        List<String> userFriendsList = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (Objects.equals(friend.get(0), user)) {
+                userFriendsList.add(friend.get(1));
+            }
+            if (Objects.equals(friend.get(1), user)) {
+                userFriendsList.add(friend.get(0));
+            }
+        }
+        return userFriendsList;
     }
 }
