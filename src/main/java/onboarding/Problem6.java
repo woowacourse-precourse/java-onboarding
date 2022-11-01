9d6f87af1d08eff1d45ab9702f5fb92be5f93fb8
hyunseo <goatsfish@gmail.com>
2022-11-01 15:08:47 +0900

    feat(Problem6) : 사용자별 닉네임 중복자 저장 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -17,6 +17,7 @@ public class Problem6 {
         nicknamePiecesCount.clear();
 
         initNicknamePiecesCount(forms);
+        findAllBannedUser(forms);
 
         List<String> distinctAnswer = new ArrayList<>(answer);
         Collections.sort(distinctAnswer);
@@ -40,6 +41,18 @@ public class Problem6 {
         }
     }
 
+    private static void findAllBannedUser(List<List<String>> forms) {
+        for (List<String> userForm : forms) {
+            String email = userForm.get(FORM_EMAIL_INDEX);
+            String nickname = userForm.get(FORM_NICKNAME_INDEX);
+            boolean isBanned = getDistinctNicknamePieces(nickname).stream()
+                    .filter(nicknamePiece -> checkNicknamePieceExists(nicknamePiece))
+                    .anyMatch(nicknamePiece -> checkNicknamePieceBanned(nicknamePiece));
+
+            reflectBannedUserEmail(email, isBanned);
+        }
+    }
+
     private static Set<String> getDistinctNicknamePieces(String nickname) {
         Set<String> nicknamePieces = new HashSet<>();
         IntStream.range(0, nickname.length() - 1)
@@ -56,4 +69,17 @@ public class Problem6 {
                 .collect(Collectors.toSet());
     }
 
+    private static boolean checkNicknamePieceExists(String nicknamePiece) {
+        return nicknamePiecesCount.containsKey(nicknamePiece);
+    }
+
+    private static boolean checkNicknamePieceBanned(String nicknamePiece) {
+        return nicknamePiecesCount.get(nicknamePiece) >= 2;
+    }
+
+    private static void reflectBannedUserEmail(String email, boolean isBanned) {
+        if (isBanned) {
+            answer.add(email);
+        }
+    }
 }
