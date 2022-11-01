809fa1290b9a730a8ae5ba6ee41003bfcb32b516
이소영 <th42500@naver.com>
2022-11-01 22:05:15 +0900

    [Test] 사용하지 않는 리스트 제거

--- src/test/java/onboarding/Problem5Test.java
+++ src/test/java/onboarding/Problem5Test.java
@@ -18,12 +18,12 @@ class Problem5Test {
     void getCountAndExamineMoney() {
         problem5 = new Problem5();
         money = 50237;
-        List<Integer> result = new ArrayList<>();
 
         for (int i=0; i<moneyUnits.length; i++) {
             assertEquals(money/moneyUnits[i], problem5.getMoneyCount(money, moneyUnits[i]));
             assertEquals(money%moneyUnits[i], problem5.examineChange(money, moneyUnits[i]));
         }
     }
+    
 
 }
