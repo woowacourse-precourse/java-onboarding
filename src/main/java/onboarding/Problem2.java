93d2c7ee02f890f943bb2c04fcfef60fe0dcd32a
JangHyojoon <fdasfaj9@gmail.com>
2022-11-01 17:32:06 +0900

    문제 2번 기능 1 예외 처리 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,16 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+    private static boolean exception(String cryptogram) {
+        if(cryptogram.length() < 1 || cryptogram.length() > 1000){
+            return false;
+        }
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (Character.isUpperCase(cryptogram.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
