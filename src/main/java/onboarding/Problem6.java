7668eb950fa9d1b47000fb18120b1502f980ab2f
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-01 10:00:13 +0900

    refactoring : Problem6 (Nickname) - 중복문제 제거

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,18 +6,28 @@ import java.util.stream.Collectors;
 public class Problem6 {
     // 검증리스트 만드는 함수
     public static List<String> makeList(List<List<String>> listD){
-        // 1. 모든닉네임의 연속되는 2자리 문자들을 모아놓은 리스트 생성
+        //  모든닉네임의 연속되는 2자리 문자들을 모아놓은 리스트 생성
         List<String> list =  new ArrayList<String>();
+
         for(int i = 0; i < listD.size(); i++){
             String Name = listD.get(i).get(1);
+            // 이름별 2자리 문자들 (중복 없이)
+            List<String> sub_list = new ArrayList<String>();
             for (int j = 0; j < Name.length()-1; j++){
-                list.add(Name.substring(j,j+2));
+                sub_list.add(Name.substring(j,j+2));
             }
+            sub_list = uniqueSort(sub_list);
+
+            for (int k = 0; k < sub_list.size(); k++){
+                list.add(sub_list.get(k));
+            }
+
         }
         // 2. 중복되는 2자리 문자만 필터링
         Set<String> Set = list.stream().filter(i -> Collections.frequency(list, i) > 1)
                 .collect(Collectors.toSet());
         List<String> result = new ArrayList<String>(Set);
+
 
         return result;
     }
