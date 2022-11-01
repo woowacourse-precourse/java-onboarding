d2ee3ea7acf5d2b17d743d100fcb84829d81a924
chanminmaxscaler <jcm04@naver.com>
2022-11-01 20:32:38 +0900

    feat(VisitorList): VisitorList 클래스 getVisitorListExcluding 메소드를 구현한다.
    getVisitorListExcluding(List<User> friendListOfUser) 메소드는 VisitorList 중 입력된 friendListOfUser을 제외한 VisitorList를 List\<User>로 반환한다.

--- src/main/java/onboarding/problem7/collections/VisitorList.java
+++ src/main/java/onboarding/problem7/collections/VisitorList.java
@@ -3,6 +3,7 @@ package onboarding.problem7.collections;
 import onboarding.problem7.wrapper.User;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class VisitorList {
     private final List<User> visitorList;
@@ -17,5 +18,11 @@ public class VisitorList {
         if (friendsLength > 10000 || friendsLength < 1) {
             throw new IllegalArgumentException("visitors 길이는 1이상 10000이하여야 합니다.");
         }
+    }
+
+    public List<User> getVisitorListExcluding(List<User> friendListOfUser) {
+        return visitorList.stream()
+                .filter(x -> !friendListOfUser.contains(x))
+                .collect(Collectors.toList());
     }
 }
