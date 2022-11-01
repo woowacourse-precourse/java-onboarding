52dd1a873ef7c71b58e8028e38c45e3c899248f5
seongbeom <84623098+hi12123123@users.noreply.github.com>
2022-11-01 19:16:07 +0900

    feat : add function plusLogic

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+//pobi와 crong의 길이는 2이다.
+        if (pobi.size() != 2 && crong.size() != 2){
+            return -1;
+        }
+        return compareLogic(pobi, crong);
     }
+
+
+    private static int plusLogic(List<Integer> lst){
+        List<Integer> plus = new ArrayList<>();
+
+        for(int i = 0; i < lst.size(); i++) {
+            String s = Integer.toString(lst.get(i));
+
+            for (int j = 0; j < s.length(); j++) {
+                int sum = 0;
+                sum += Integer.parseInt(s.substring(i, i + 1));
+                plus.add(sum);
+            }
+        }
+        return Collections.max(plus);
+    }
+
+
 }
