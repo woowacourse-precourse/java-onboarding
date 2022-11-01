1070abf46c9912c9ed0c478d7502793453e2416b
JeonghakLee <wjdgkr0527@naver.com>
2022-11-01 13:05:10 +0900

    fix: 병행성 문제 해결

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,28 +11,33 @@ import java.util.TreeSet;
 
 public class Problem6 {
 
-  private static final Map<String, Set<Integer>> substringMap = new HashMap<>();
-  private static final Set<String> answerSet = new TreeSet<>();
-
-
   public static List<String> solution(List<List<String>> forms) {
-    makeSubstringMap(forms);
-    getAnswerSet(forms);
-    return new ArrayList<>(answerSet);
+    Map<String, Set<Integer>> substringMap = new HashMap<>();
+    makeSubstringMap(forms, substringMap);
+    return getAnswerSet(forms, substringMap);
   }
 
-  private static void getAnswerSet(List<List<String>> forms) {
+  private static ArrayList<String> getAnswerSet(List<List<String>> forms,
+      Map<String, Set<Integer>> substringMap) {
+    Set<String> result = new TreeSet<>();
     for (Entry<String, Set<Integer>> entries : substringMap.entrySet()) {
       Set<Integer> formIndexes = entries.getValue();
       if (formIndexes.size() > 1) {
-        for (Integer index : formIndexes) {
-          answerSet.add(forms.get(index).get(0));
-        }
+        addDuplicatedFormIndexes(forms, result, formIndexes);
       }
+    }
+    return new ArrayList<>(result);
+  }
+
+  private static void addDuplicatedFormIndexes(List<List<String>> forms, Set<String> result,
+      Set<Integer> formIndexes) {
+    for (Integer index : formIndexes) {
+      result.add(forms.get(index).get(0));
     }
   }
 
-  private static void makeSubstringMap(List<List<String>> forms) {
+  private static void makeSubstringMap(List<List<String>> forms,
+      Map<String, Set<Integer>> substringMap) {
     for (int formIndex = 0; formIndex < forms.size(); formIndex++) {
       String nickName = forms.get(formIndex).get(1);
       for (int i = 0; i < nickName.length() - 1; i++) {
