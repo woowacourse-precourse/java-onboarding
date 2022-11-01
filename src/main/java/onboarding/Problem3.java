eb57cf2a0b90109b2abe59f8b8881197b16e07ca
anso33 <hyeon131812@gmail.com>
2022-11-01 11:29:37 +0900

    [FEAT] add func that change int to string

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+// int -> String 으로 변경
+// 3, 6, 9 가 있는지 find
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String num;
+        for (int i = 0; i < number; i++) {
+            num = Integer.toString(i + 1);
+
+        }
         return answer;
     }
 }
