28f04f911325300edb2e85b6eb9384cdae2326f4
ECHO <eunchan1001@gmail.com>
2022-11-01 15:22:53 +0900

    feat(Problem7): 친구 목록 생성 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,26 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<String>();
+        Map<String, List<String>> friendMap = getFriendsMap(friends);
         return answer;
+    }
+
+    static Map<String, List<String>> getFriendsMap(List<List<String>> friends) {
+        Map<String, List<String>> friendsMap = new HashMap<String, List<String>>();
+        for (List<String> friend : friends) {
+            String friendA = friend.get(0);
+            String friendB = friend.get(1);
+            friendsMap = addFriends(friendsMap, friendA, friendB);
+            friendsMap = addFriends(friendsMap, friendB, friendA)
+        }
+        return friendsMap;
     }
 }
