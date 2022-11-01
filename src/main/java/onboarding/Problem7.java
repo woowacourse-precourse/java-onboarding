19d13cf73904ebf311b5c620769eec8a73d1d708
xi_jjun <rlawowns97@naver.com>
2022-11-01 19:13:45 +0900

    feat(Problem7): user친구들의 친구들에게 10점 점수 부과 하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,12 +10,31 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Problem7 {
+	static final int SCORE_FRIENDS_KNOW_WITH_USER = 10;
+	static final int SCORE_VISIT_USER_SNS = 1;
+
 	static final Map<String, List<String>> friendRelationGraph = new HashMap<>();
 	static final Map<String, Integer> recommendationScores = new HashMap<>();
 
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 		List<String> answer = Collections.emptyList();
 		return answer;
+	}
+
+	public static void giveScoresToFriendsKnowWithUser(String user) {
+		Set<String> friendsOfUser = findFriendsOfUser(user);
+		friendsOfUser.add(user);
+
+		for (String friendOfUser : friendsOfUser) {
+			Set<String> friendsKnowWithUser = findFriendsOfUser(friendOfUser);
+
+			for (String friendKnowWithUser : friendsKnowWithUser) {
+				if (!friendsOfUser.contains(friendKnowWithUser)) {
+					int previousScore = recommendationScores.get(friendKnowWithUser);
+					recommendationScores.put(friendKnowWithUser, previousScore + SCORE_FRIENDS_KNOW_WITH_USER);
+				}
+			}
+		}
 	}
 
 	public static Set<String> findFriendsOfUser(String user) {
