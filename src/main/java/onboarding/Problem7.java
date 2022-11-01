2d78385136ce96322a565f407a159622ad5c3252
dmdrk1414 <seungchan141414@gmail.com>
2022-11-01 21:27:59 +0900

    :recycle:[Refactor] Problem_7 공부후 수정
    List와 Map과 다른사람들의 깃을 보면서 이틀간 공부를 하고 와서 변경을했습니다.
    컬렉션프레임워크를 활용한 코드를 만들었습니다.
    
    1. 제가 변경하기 전에는 for문을 이용한 포함관계를 변수에 저장한후 map와 LIst에 저장하는 식이였습니다.
    2. 변경하기 전에는 for문을 이용한 Left Right의 친구들을 비교하였습니다.
    3. 지금은 Container을 이용한 포함관계를 통해 Map에 저장을 합니다.
    4. Container의 방식으로 인해 생각을 안해도 되는 점을 만들어 편리합니다.
    
    컬렉션 프레임 워크의 위대함을 또 느겼습니다.
    다른 사람의 깃을 보는 것을 싫어하여 안보았지만 남의 코드를 보는 것또한 공부라는 것을 느겼습니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,151 +9,75 @@ public class Problem7 {
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
+        Map<String, Integer> priorityPersonMap = new HashMap<>();
+        List<String> userFriendList = new ArrayList<>();
+        List<List<String>> friendExcetionUserList = new ArrayList<>();
 
-        // user와 친구가 아닌것을 걸러야된다.
-        ArrayList<String> notFriendUserList = new ArrayList<>();
-        ArrayList<String> friendUserList = new ArrayList<>();
-        ArrayList<String> personList = new ArrayList<>();
-        ArrayList<String> allFriendsVisitors = new ArrayList<>();
-
-
-        List<String> listAtNow = null;
-        String leftFriendsInList = "";
-        String rightFriendsInList = "";
-        String namePersonAtNow = "";
-
-        int endFistSize = friends.size();
-        int endSecondSize = friends.get(0).size();
-        for (int firstListIndex = 0; firstListIndex < endFistSize; firstListIndex++) {
-            // friends 리스트 안의 리스트 작업
-            listAtNow = friends.get(firstListIndex);
-
-            leftFriendsInList = listAtNow.get(LEFT_FRIEND);
-            rightFriendsInList = listAtNow.get(RIGHT_FRIEND);
-
-            // user의 친구면? FriendUserListdp 넣는다.
-            if (strAddressEquals(user, leftFriendsInList)) {
-                friendUserList.add(rightFriendsInList);
+        for (List<String> friendPairList : friends) {
+            if (friendPairList.contains(user)) {
+                String nameUserFriend = getAnotherPairName(friendPairList, user);
+                userFriendList.add(nameUserFriend);
+            } else {
+                friendExcetionUserList.add(friendPairList);
             }
-            if (strAddressEquals(user, rightFriendsInList)) {
-                friendUserList.add(leftFriendsInList);
-            }
-            personList.add(leftFriendsInList);
-            personList.add(rightFriendsInList);
         }
 
-        personList = (ArrayList<String>) deduplicationList(personList);
-
-        friendUserList = (ArrayList<String>) deduplicationList(friendUserList);
-
-        // 친구가 아닌 리스트를 만든다.
-        boolean isFriend = true;
-        int endPersonList = personList.size();
-        for (int personIndex = 0; personIndex < endPersonList; personIndex++) {
-            isFriend = false;
-            namePersonAtNow = personList.get(personIndex);
-
-            // 현재 사람이 친구인지 판별
-            for (int friendUserIndex = 0; friendUserIndex < friendUserList.size(); friendUserIndex++) {
-                String nameFriendUserAtNow = friendUserList.get(friendUserIndex);
-                if (namePersonAtNow.equals(nameFriendUserAtNow)) {
-                    isFriend = true;
+        for (String myFriend : userFriendList) {
+            for (List<String> friendPair : friendExcetionUserList) {
+                if (friendPair.contains(myFriend)) {
+                    String nameAnotherPair = getAnotherPairName(friendPair, myFriend);
+                    addMapPriority(nameAnotherPair, 10, priorityPersonMap);
                 }
             }
-            if (!isFriend && !user.equals(namePersonAtNow)) {
-                notFriendUserList.add(namePersonAtNow);
-            }
         }
 
-        notFriendUserList = (ArrayList<String>) deduplicationList(notFriendUserList);
-
-        // notFriendUserList의 우선순위를 가진 map을 구현
-        Map<String, Integer> priorityNotFriendMap = new HashMap<>();
-        int endNotFriendSize = notFriendUserList.size();
-        for (int notFriendUserIndex = 0; notFriendUserIndex < endNotFriendSize; notFriendUserIndex++) {
-            String nameNotFriend = notFriendUserList.get(notFriendUserIndex);
-            priorityNotFriendMap.put(nameNotFriend, 10);
-        }
-
-        // User의 친구와 User의 친구가 아닌것과 Visitor의 전체 배열 연관된 모든사람 List만듬
-        int endPersonListVisitors = personList.size() + visitors.size();
-        allFriendsVisitors.addAll(personList);
-
-        int endVisitor = visitors.size();
-        for (int visitorIndex = 0; visitorIndex < endVisitor; visitorIndex++) {
-            String nameVisitor = visitors.get(visitorIndex);
-            allFriendsVisitors.add(nameVisitor);
-        }
-
-        allFriendsVisitors = (ArrayList<String>) deduplicationList(allFriendsVisitors);
-
-        // visitor들의 방문횟수를 가져왔다.
-        Map<String, Integer> cntVisitorMap = new HashMap<>();
+//        visitors
+        // 친구들 리스트에 포함이 안되면
         for (String nameVisitor : visitors) {
-            cntVisitorMap.put(nameVisitor, 0);
-        }
-        for (String nameVisitor : visitors) {
-            if (cntVisitorMap.containsKey(nameVisitor)) {
-                int numBeforValue = cntVisitorMap.get(nameVisitor);
-                cntVisitorMap.put(nameVisitor, numBeforValue + 1);
+            if (!userFriendList.contains(nameVisitor)) {
+                addMapPriority(nameVisitor, 1, priorityPersonMap);
             }
-        }
-
-        // 친구가 아닌 visitor들의 방문횟수를 가져왔다.
-        Map<String, Integer> cntNotFrendsVisitorMap = new HashMap<>();
-        int endFriendUserList = friendUserList.size();
-        for (Map.Entry<String, Integer> entry : cntVisitorMap.entrySet()) {
-            isFriend = false;
-            String nameVisitor = entry.getKey();
-            int cntPriorityVisitor = entry.getValue();
-
-            for (int FriendIndex = 0; FriendIndex < endFriendUserList; FriendIndex++) {
-                String nameFriendUserAtNow = friendUserList.get(FriendIndex);
-                if (nameVisitor.equals(nameFriendUserAtNow)) {
-                    isFriend = true;
-                }
-            }
-            if (!isFriend) {
-                cntNotFrendsVisitorMap.put(nameVisitor, cntPriorityVisitor);
-            }
-        }
-
-        // 이제 전체적  priorityNotFriendMap과 cntVisitorMap의 값을 합쳐야합니다.
-        Map<String, Integer> priorityAllNotFriendMap = new HashMap<>();
-        priorityAllNotFriendMap.putAll(priorityNotFriendMap);
-        for (Map.Entry<String, Integer> entry : cntNotFrendsVisitorMap.entrySet()) {
-            String nameNotFrendsVisitor = entry.getKey();
-            int priorityNotFrendsVisitor = entry.getValue();
-            priorityAllNotFriendMap.put(nameNotFrendsVisitor, priorityNotFrendsVisitor);
         }
 
         // 우선순위로 정렬
-        List<String> keySet = new ArrayList<>(priorityAllNotFriendMap.keySet());
-
+        List<String> keySet = new ArrayList<>(priorityPersonMap.keySet());
         keySet.sort(new Comparator<String>() {
             @Override
             // 우선순위로 비교하는 것
-            public int compare(String o1, String o2) {
+            public int compare(String str_1, String str_2) {
                 // 1. 우선순위 가 같으면 이름으로 정렬
                 // 과연 문자열 비교는 주소로 비교를 하는 것이 맞는가?
-                if (priorityAllNotFriendMap.get(o2) == priorityAllNotFriendMap.get(o1)) {
-                    return (o1.compareTo(o2));
+                if (priorityPersonMap.get(str_2) == priorityPersonMap.get(str_1)) {
+                    return (str_1.compareTo(str_2));
                 }
                 // 2. 우선 순위  값으로 내림차순 정렬
                 else {
-                    return (priorityAllNotFriendMap.get(o2) - priorityAllNotFriendMap.get(o1));
+                    return (priorityPersonMap.get(str_2) - priorityPersonMap.get(str_1));
                 }
             }
         });
-
-
-        List<String> resultList = new ArrayList<>();
-        for (String key : keySet) {
-            resultList.add(key);
-        }
-
-        answer.addAll(resultList);
+        answer.addAll(keySet);
+        System.out.println("answer = " + answer);
         return answer;
+    }
+
+    // name이랑
+    private static void addMapPriority(String name, int priority, Map<String, Integer> map) {
+        Integer mapValue = 0;
+        if (map.containsKey(name)) {
+            mapValue = map.get(name);
+            map.put(name, mapValue + priority);
+        } else {
+            map.put(name, priority);
+        }
+    }
+
+    private static String getAnotherPairName(List<String> friendPairList, String user) {
+        if (friendPairList.get(0).equals(user)) {
+            return friendPairList.get(1);
+        } else {
+            return friendPairList.get(0);
+        }
     }
 
     private static List<String> deduplicationList(ArrayList<String> List) {
