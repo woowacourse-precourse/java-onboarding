3d62b0422dc60dfbc2a8ba2ee0e9b3a7d4df546c
wxxhyeong <wxxhyeong@gmail.com>
2022-11-01 21:12:38 +0900

    [Problem6] feat : getMail() 추가 및 정렬 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,18 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        List<String> partialName = new ArrayList<>();
+        Set<String> dupMail = new HashSet<>();
+        Set<String> duplName = new HashSet<>();
+
+        partialName = getPartialName(forms);
+        duplName = getDuplicateName(partialName);
+        dupMail = getMail(forms, duplName);
+
+        List<String> answer = new ArrayList<>(dupMail);
+        Collections.sort(answer);
+
         return answer;
     }
 
@@ -15,8 +26,8 @@ public class Problem6 {
 
         for (List<String> form : forms) {
             userName = form.get(1);
-            for (int i = 0; i < userName.length()-1; i++) {
-                partialName.add(userName.substring(i, i+2));
+            for (int i = 0; i < userName.length() - 1; i++) {
+                partialName.add(userName.substring(i, i + 2));
             }
         }
 
@@ -35,6 +46,21 @@ public class Problem6 {
         return duplName;
     }
 
+    static Set<String> getMail(List<List<String>> forms, Set<String> duplName) {
 
+        Set<String> dupMail = new HashSet<>();
+        String userName;
 
+        for (List<String> form : forms) {
+            userName = form.get(1);
+
+            for (String name : duplName) {
+                if (userName.contains(name)) {
+                    dupMail.add(form.get(0));
+                }
+            }
+        }
+
+        return dupMail;
+    }
 }
