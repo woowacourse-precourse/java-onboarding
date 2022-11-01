313527f5adf73d671809b15a82a1f7eb6549fc0a
ddukddaki <rlfrkdms1@naver.com>
2022-11-01 16:46:46 +0900

    feat(Problem5): 알맞은 화폐로 변환한 수를 구하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,7 +9,14 @@ import java.util.List;
  */
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] money_type = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int type : money_type) {
+            answer.add(money / type);
+            money %= type;
+        }
+
         return answer;
     }
 }
