38d6606053325b029a822d4c358492a694864a0c
euncheol-kim <klmeuncheol@kakao.com>
2022-11-01 22:34:56 +0900

    Problem7/Feat : 미구현 상태이며 아이디어를 주석으로 달아놓은 상태

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, List<String>> relationships = new HashMap<>();
+        Map<String, Integer> recommendScore = new HashMap<>();
+
+        // 친구 관계를 저장합니다.
+        for(List<String> e : friends) {
+            String key = e.get(0);
+            String value = e.get(1);
+
+            if(!relationships.containsKey(key)) relationships.put(key, new ArrayList<>());
+            relationships.get(key).add(value);
+        }
+
+        System.out.println();
+
+        for(String e : relationships.get(user)) {
+            int cnt = 0;
+            while(cnt == relationships.get(e).size()){
+
+                cnt++;
+            }
+        }
+
+        return null;
     }
 }
+/*
+*   그래프 형식으로 문제를 접근하여 풀어야할 것으로 사료 (미구현)
+*
+* */
