c53b9ab689b963d85432a85a53046275fcd2bd7f
kwx4957 <kdy4957@naver.com>
2022-11-01 13:54:08 +0900

    4번 문제 기능 목록 작성 1. 알파벳외 문자 확인 2. 대문자 to 대문자, 소문자 to 소문자 3. 문자열의 길이 1 ~ 1000

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,15 @@
 package onboarding;
 
+/*
+기능 목록
+1. 알파벳외 문자 확인
+2. 대문자 to 대문자, 소문자 to 소문자
+3. 문자열의 길이 1 ~ 1000
+ */
 public class Problem4 {
+    public static boolean isAlpah(char c){
+        return Character.isAlphabetic(c);
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
