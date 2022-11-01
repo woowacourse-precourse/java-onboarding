1fb327fc62060bf6893238d3266ad5618213070e
0chil <0@chll.it>
2022-11-01 18:46:13 +0900

    refactor(problem7): 클래스명 변경

--- src/main/java/onboarding/problem7/Network.java
+++ src/main/java/onboarding/problem7/Network.java
@@ -1,16 +1,43 @@
 package onboarding.problem7;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Network {
-	private final FriendsUpper friends;
+
+	private final Visitors visitors;
+	private final Friends friends;
 
 	public Network(List<List<String>> relationships, List<String> visitors) {
-		this.friends = new FriendsUpper(relationships, visitors);
+		this.friends = new Friends(relationships);
+		this.visitors = new Visitors(visitors);
 	}
 
-	public Collection<String> recommendFriends(String user) {
-		return friends.recommendFriendsFor(user);
+	public Collection<String> recommendFriendsFor(String user) {
+		Map<String, Integer> scores = new TreeMap<>();
+		for (String friend : getKnownUsers()) {
+			if (friends.isTwoFriends(user, friend)) {
+				continue;
+			}
+			scores.put(friend, calculateScoreOf(user, friend));
+		}
+		return scores.keySet();
+	}
+
+	private int calculateScoreOf(String user, String friend) {
+		int sharedFriendsCount = friends.countSharedFriends(user, friend);
+		int visitCount = visitors.count(friend);
+		return sharedFriendsCount * 10 + visitCount;
+	}
+
+	private Set<String> getKnownUsers() {
+		Set<String> knownUsers = new HashSet<>();
+		knownUsers.addAll(friends.getKnownUsers());
+		knownUsers.addAll(visitors.getKnownUsers());
+		return knownUsers;
 	}
 }
