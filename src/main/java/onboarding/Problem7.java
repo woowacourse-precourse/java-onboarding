c5d53787de7bcd8cb35a99af2c5c26c387b2691d
sun007021 <sun007021@gmail.com>
2022-11-01 23:38:24 +0900

    feat: friends 클래스 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,15 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class UsersInformation {
+class UsersInfo {
     private String user;
     private List<List<String>> friends;
     private List<String> usersFriends;
     private List<String> visitors;
-    private List<String> recomendFriends;
+    private  List<String> recomendFriends;
 
     public void setUser(String name) {
         user = name;
@@ -31,12 +33,44 @@ class UsersInformation {
         return friends;
     }
 
+    public List<String> getUsersFriends() {
+        return usersFriends;
+    }
+
     public List<String> getVisitors() {
         return visitors;
     }
 
     public List<String> getRecomendFriends() {
         return recomendFriends;
+    }
+}
+
+class Friends extends UsersInfo {
+    private List<String> friendsFriend = new ArrayList<>();
+
+    public void findUsersFriends(){
+        List<String> usersFriends = getFriends().stream()
+                .filter(friendList -> friendList.contains(getUser()))
+                .map(friendList -> friendList.get(findFriendsIndex(getUser(),friendList)))
+                .collect(Collectors.toList());
+
+    }
+
+    public void findFriendsFriend() {
+        Iterator<String> iterator = getUsersFriends().iterator();
+        while (iterator.hasNext()) {
+            String friend = iterator.next();
+            List<String> list = getFriends().stream()
+                    .filter(friends -> friends.contains(friend))
+                    .map(friends -> friends.get(findFriendsIndex(friend, friends)))
+                    .distinct()
+                    .collect(Collectors.toList());
+            friendsFriend = list;
+        }
+    }
+    private int findFriendsIndex(String user, List<String> friendList) {
+        return friendList.get(0).equals(user) ? 1 : 0;
     }
 }
 
