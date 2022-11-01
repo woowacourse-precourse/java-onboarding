7a5a27165e4edd48047d00699989a112326b0823
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 12:18:22 +0900

    [Add] Prob6 Funtion list
    + Funtion list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,9 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    // list for store forms + similar Boolean
+    // func : compare with prior nicknames, return Boolean
+    // func : found similar nicknames, return List<String> email
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
