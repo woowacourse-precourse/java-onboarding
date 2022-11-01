9e32c5c32260c1ce379fc7e2c7c19e6649151a5b
cuttlefish55 <funade7@gmail.com>
2022-11-01 22:30:40 +0900

    5번문제: 코드 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,10 +7,10 @@ import java.util.ArrayList;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList();
-        int[] spliter =new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        for(int i = 0; i < spliter.length; i ++ ){
-            answer.add(money/spliter[i]);
-            money = money%spliter[i];
+        int[] splitter = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1}; // 화폐 순서 저장
+        for (int i : splitter) {
+            answer.add(money / i);
+            money = money % i;
         }
         return answer;
     }
