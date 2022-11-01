eb86588ce6cb054d90de3a459ca5acd52c1e7ce7
tdmdgh <tdmdgh@gmail.com>
2022-11-01 19:13:57 +0900

    problem6_1_name_list 추출하는 매소드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    private static final int EMAIL = 0;
+    private static final int NAME = 1;
+    private static List<String> getNameList(List<List<String>> forms){
+        List<String> name_list = new ArrayList<>();
+        for (List<String> crew : forms){
+            name_list.add(crew.get(NAME));
+        }
+        return name_list;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
