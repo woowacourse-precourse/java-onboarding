3782b907d7e59b0852e76fa027e3efbc3b87cbae
Shingiwon <65998782+Shingiwon@users.noreply.github.com>
2022-11-01 22:51:06 +0900

    6번 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = new ArrayList<String>();
+
+		ArrayList<String> nicknames = new ArrayList<String>();
+		HashSet<String> dupl = new HashSet();
+		int nick_len = 0;
+		for (int i = 0; i < forms.size(); i++) {
+			String str = forms.get(i).get(1);
+
+			for (int j = 2; j <= str.length(); j++) {
+				if (nicknames.contains(str.substring(j - 2, j))) {
+					dupl.add(str.substring(j - 2, j));
+				}
+				nicknames.add(str.substring(j - 2, j));
+			}
+		}
+		Iterator<String> iter = dupl.iterator();
+		while (iter.hasNext()) {
+			String word = iter.next();
+			for (int i = 0; i < forms.size(); i++) {
+				if(forms.get(i).get(1).contains(word))answer.add(forms.get(i).get(0));
+			}
+		}
+		Collections.sort(answer);
+		return answer;
+	}
 }
