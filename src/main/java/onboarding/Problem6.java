ec6f1520b2e534f50242d92937c34a5e36ef9386
BoyCho <iijokv@naver.com>
2022-11-01 13:16:07 +0900

    feat : 문제6. 교육생들간 비교 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<List<String>> prevL = new ArrayList<>(forms);
+        List<String> answer = Collections.emptyList();
+
+        while (true) {
+            List<List<String>> nextL = new ArrayList<>();
+            String cmpe = prevL.get(0).get(0);  // prevL 리스트의 첫 번째 교육생의 닉네임
+            String cmpk = prevL.get(0).get(1);  // prevL 리스트의 첫 번째 교육생의 이메일
+            boolean isSameNick = false;
+
+            // 첫 번째 교육생의 닉네임을 나머지 교육생의 닉네임과 비교합니다.
+            for (int i = 1; i < prevL.size(); i++) {
+                String email = prevL.get(i).get(0);
+                String knick = prevL.get(i).get(1);
+            }
+        }
     }
 }
