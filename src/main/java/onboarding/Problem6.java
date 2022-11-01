51a1261de0e133bfd0be37f56ff67f81f2ced48a
xi_jjun <rlawowns97@naver.com>
2022-11-01 14:21:40 +0900

    feat(Problem6): 토큰의 개수를 저장하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,8 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,6 +10,9 @@ import java.util.stream.IntStream;
 public class Problem6 {
     private static final int START_INDEX_OF_TOKENIZATION = 0;
     private static final int TOKEN_LENGTH = 2;
+    private static final int DEFAULT_TOKEN_CNT = 0;
+
+    static final Map<String, Integer> tokenCounter = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
@@ -18,5 +23,12 @@ public class Problem6 {
         return IntStream.range(START_INDEX_OF_TOKENIZATION, nickname.length() - 1)
                 .mapToObj(start -> nickname.substring(start, start + TOKEN_LENGTH))
                 .collect(Collectors.toSet());
+    }
+
+    public static void countToken(Set<String> tokens) {
+        for (String token : tokens) {
+            Integer tokenCount = tokenCounter.getOrDefault(token, DEFAULT_TOKEN_CNT);
+            tokenCounter.put(token, tokenCount + 1);
+        }
     }
 }
