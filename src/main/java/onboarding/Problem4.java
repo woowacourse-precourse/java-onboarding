05940edfe95557298c7a1831b4b80feb34545c87
danneh-0427 <jc14972@gmail.com>
2022-11-01 23:09:51 +0900

    question 5,6

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,24 +1,27 @@
 package onboarding;
 
+//기능 목록
+//1. 문자열을 역순으로 반환하는 기능
+
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder(word);
 
         for (int i = 0; i < answer.length(); i++) {
-            answer.setCharAt(i, getFlipWord(answer.charAt(i)));
+            answer.setCharAt(i, getFrogWord(answer.charAt(i)));
         }
         return answer.toString();
     }
 
-    private static char getFlipWord(char letter) {
-        char result = letter;
+    private static char getFrogWord(char reverse) {
+        char result = reverse;
 
-        if (Character.isUpperCase(letter)) {
-            result = (char)('A' + 'Z' - letter);
+        if (Character.isUpperCase(reverse)) {
+            result = (char)('A' + 'Z' - reverse);
         }
 
-        if(Character.isLowerCase(letter)) {
-            result = (char)('a' + 'z' - letter);
+        if(Character.isLowerCase(reverse)) {
+            result = (char)('a' + 'z' - reverse);
         }
 
         return result;
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
+//기능 목록:
+//1. 각 단위별로 구분하는 기능
+
 public class Problem5 {
+    public static int count;
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        count = 0;
+
+        int[] cash = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> result = new ArrayList<>();
+
+        for (int bill : cash) {
+            count = 0;
+            money = calculateMoney(money, bill);
+            result.add(count);
+        }
+        return result;
+    }
+    public static int calculateMoney(int money, int bill) {
+        if (money >= bill) {
+            count = money / bill;
+            money %= bill;
+        }
+        return money;
     }
 }
