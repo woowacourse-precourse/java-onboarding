813f4c4041b5b0634fc0acb3eadef8b31ba0a8de
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 21:55:09 +0900

    feat:Problem6 닉네임 2글자로 쪼개는 기능 splitNickName()추가.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,7 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class Problem6 {
 
@@ -18,38 +16,8 @@ public class Problem6 {
         return nicknameSet;
     }
 
-    public static boolean compareElement (List<String> nicknameSet, String nickname) {
-        for (String nicknameElement: nicknameSet) {
-            if (nickname.contains(nicknameElement)) return true;
-        }
-        return false;
-    }
-
-    public static List<String> addEmail(List<List<String>> forms) {
-        List<String> correctEmail = new ArrayList<>();
-        for (int j = 0; j < forms.size()-1; j++) {
-            int prevention = 0;
-            for (int k = j + 1; k < forms.size(); k++) {
-                if (prevention < 1 && compareElement(splitNickName(forms.get(j).get(1)), forms.get(k).get(1))) {
-                    correctEmail.add(forms.get(j).get(0));
-                    prevention += 1;
-                }
-                if (compareElement(splitNickName(forms.get(j).get(1)), forms.get(k).get(1))) {
-                    correctEmail.add(forms.get(k).get(0));
-                }
-            }
-        }
-        return correctEmail;
-    }
-
-    public static List<String> removeDuplicate (List<String> correctEmail) {
-        List<String> answerEmail = correctEmail.stream().distinct().collect(Collectors.toList());
-        Collections.sort(answerEmail);
-        return answerEmail;
-    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answerEmail = addEmail(forms);
-        answerEmail = removeDuplicate(answerEmail);
-        return answerEmail;
+        List<String> answer = List.of("answer");
+        return answer;
     }
 }
