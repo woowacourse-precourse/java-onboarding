6c8f08d6501956f3ce758829d9925732bdb70ecf
jiwon1217 <koagjw@gmail.com>
2022-11-01 15:34:05 +0900

    [Refactor] 암호화 로직을 메서드로 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,18 +1,25 @@
 package onboarding;
 
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
 public class Problem4 {
+    public static char encryptCharacter(char alphabet) {
+        if (isUpperCase(alphabet)) {
+            alphabet = (char) ('A' + 'Z' - alphabet);
+        }
+        if (isLowerCase(alphabet)) {
+            alphabet = (char) ('a' + 'z' - alphabet);
+        }
+        return alphabet;
+    }
 
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < word.length(); i++) {
             char alphabet = word.charAt(i);
-            if (alphabet >= 'A' && alphabet <= 'Z') {
-                alphabet = (char) ('A' + 'Z' - alphabet);
-            } else if (alphabet >= 'a' && alphabet <= 'z') {
-                alphabet = (char) ('a' + 'z' - alphabet);
-            }
-            answer.append(alphabet);
+            answer.append(encryptCharacter(alphabet));
         }
         return answer.toString();
     }
