20adb2f00d9fd96517b08b18cad91b327771a0d6
sylee <sy>
2022-11-01 22:23:31 +0900

    [Feat] : Problem2 변수, 함수 정의

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,30 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        int index = 0, startIndex = -1, lastIndex = -1;
+        while(index < cryptogram.length() - 1) {
+            if(cryptogram.charAt(index) == cryptogram.charAt(index + 1)) {
+                startIndex = index;
+                lastIndex = getLastIndex(cryptogram, startIndex);
+                answer = removeRedundant(cryptogram, startIndex, lastIndex);
+                index = startIndex - 1;
+            }
+            else {
+                index++;
+            }
+        }
+
         return answer;
+    }
+
+    public static int getLastIndex(String str, int sIndex) {
+
+        return -1;
+    }
+
+    public static String removeRedundant(String str, int sIndex, int lIndex) {
+
+        return "";
     }
 }
