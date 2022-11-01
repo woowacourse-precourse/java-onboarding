da1b5123d9301ff141fa4e9592a90b7344d3459a
Yeongseon Park <yseon406@gmail.com>
2022-11-01 14:17:34 +0900

    MaxNum 메서드 수정 -> findPageMaxNum

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,32 +4,33 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(pobi.get(1)-pobi.get(0)==1 && crong.get(1)-crong.get(0)==1){
+        int pobiLpage = pobi.get(0);
+        int pobiRpage = pobi.get(1);
+        int crongLpage = crong.get(0);
+        int crongRpage = crong.get(1);
+
+        if(pobiRpage-pobiLpage==1 && crongRpage-crongLpage==1){
             int answer;
             Problem1 P = new Problem1();
-            int pobiMax = P.maxNum(pobi);
-            int crongMax = P.maxNum(crong);
+            int pobiMax = Math.max(P.findPageMaxNum(pobiLpage),P.findPageMaxNum(pobiRpage));
+            int crongMax = Math.max(P.findPageMaxNum(crongLpage),P.findPageMaxNum(crongRpage));
             answer = pobiMax!=crongMax?pobiMax>crongMax?1:2:0;
             return answer;
         }
         else return -1;
     }
 
-    public int maxNum(List<Integer> list){
-        int Max = 0;
-        for (int pageNum : list) {
-            int plus = 0;
-            int multi = 1;
-            while (pageNum > 0) {
-                plus += pageNum % 10;
-                multi *= pageNum % 10;
-                pageNum /= 10;
-            }
-            Max = Math.max(Max, plus);
-            Max = Math.max(Max, multi);
+    public int findPageMaxNum(int pageNum){
+        int plus = 0;
+        int multi = 1;
+        while(pageNum > 0) {
+            plus += pageNum % 10;
+            multi *= pageNum % 10;
+            pageNum /= 10;
         }
+        int maxNum = Math.max(plus, multi);
 
-        return Max;
-
+        return maxNum;
     }
+
 }
