c12f3783212853307e5cfa4a00f1ca1c51d84852
박세원 <tpdnjs1825@naver.com>
2022-11-01 23:11:30 +0900

    Feat : 계좌에 남아있는 돈을 갱신하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,22 +5,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    private static final int [] MONEY_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-    private static int getMoneyCount(int money, int index){
+    private static final int[] MONEY_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+    private static int getMoneyCount(int money, int index) {
         int count = 0;
-        if(money / MONEY_LIST[index] >= 1) {
+        if (money / MONEY_LIST[index] >= 1) {
             count = money / MONEY_LIST[index];
         }
         return count;
     }
+
+    private static int getChangeMoney(int money, int index, int count) {
+        int changeMoney = 0;
+        changeMoney = money - (MONEY_LIST[index] * count);
+        return changeMoney;
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
-        List<Integer> AnswerList = new ArrayList<>();
+        List<Integer> answerList = new ArrayList<>();
 
-        for(int i = 0; i < MONEY_LIST.length; i++){
-            AnswerList.add(getMoneyCount(money, i));
-//            money = getChangeMoney(money, i, list.get(i));
+        for (int i = 0; i < MONEY_LIST.length; i++) {
+            answerList.add(getMoneyCount(money, i));
+            money = getChangeMoney(money, i, answerList.get(i));
         }
+
+        answer = answerList;
 
         return answer;
     }
