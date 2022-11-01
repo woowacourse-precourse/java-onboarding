8831a35127fd65622d178370de63c75147098cde
unknown <dragontaek98@naver.com>
2022-11-01 17:31:19 +0900

    feat: problem4 string을 ë¬대문자 알파벳 변환 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,19 @@ public class Problem4 {
 
         //convert String to char Array
         char[] ch = word.toCharArray();
+
+        //convert characters
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] == ' '){
+                answer += ' ';
+            } else {
+                int parsed = (int) ch[i];
+                if(parsed < 97){
+                    answer += (char)(parsed + (90-parsed) - (parsed-65));
+                }
+            }
+        }
+
         return answer;
     }
 }
