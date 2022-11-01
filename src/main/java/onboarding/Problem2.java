470c22e749e6f38a72c9a220222b80cd8131265b
20191777 <97873629+20191777@users.noreply.github.com>
2022-11-01 20:59:01 +0900

    [문제2] : 연속하는 중복 문자 pop, 중복이 아닌 문자 push

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+        Stack<String> stack = new Stack<>();
+        String[] str = cryptogram.split("");
+        StringBuilder sb = new StringBuilder();
+
+        stack.push(str[0]);
+            for(int i = 1; i<sb.length(); i++){
+                if((stack.peek()).equals(String.valueOf(sb.charAt(i)))==false){
+                    stack.push(String.valueOf(sb.charAt(i)));
+                    System.out.println("push : " + stack.peek());
+                }else if((stack.peek()).equals(String.valueOf(sb.charAt(i)))){
+                    System.out.println("pop : " + stack.peek());
+                    stack.pop();
+                }
+            }
+
         String answer = "answer";
         return answer;
     }
