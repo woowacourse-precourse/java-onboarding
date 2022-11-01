b290649994cb437ada764728afbff2400b8dc8dc
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-01 18:56:39 +0900

    problem7: TODO-'기능1' complete

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem7 {
+    private static HashMap<String, HashSet<String>> getGraphOf(List<List<String>> friends){
+        HashMap<String, HashSet<String>> graph = new HashMap<>();
+        for(List<String> friend:friends){
+            String v1 = friend.get(0);
+            String v2 = friend.get(1);
+
+            if(graph.containsKey(v1))
+                graph.get(v1).add(v2);
+            else
+                graph.put(v1, new HashSet<>(Collections.singletonList(v2)));
+
+            if(graph.containsKey(v2))
+                graph.get(v2).add(v1);
+            else
+                graph.put(v2, new HashSet<>(Collections.singletonList(v1)));
+        }
+        return graph;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        HashMap<String, HashSet<String>> graph = getGraphOf(friends);
+
+
+        return null;
     }
 }
