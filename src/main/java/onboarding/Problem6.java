8e31c08a0b979619570e9cfd1db531729753814b
footdev <ekclstkfka5@gmail.com>
2022-11-01 20:38:58 +0900

    feat: 기능6 checkNickName 메소드 구현
    구현 및 메소드 명세 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = checkNickName(forms);
         return answer;
+    }
+
+    public static List<String> checkNickName(List<List<String>> forms) {
+        SortedSet<String> emails = new TreeSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            String crewEmail = forms.get(i).get(0);
+            String crewNickName = forms.get(i).get(1);
+            if (isOverLap(crewNickName)) {
+                emails.add(crewEmail);
+            }
+        }
+
+        return convertSetToList(emails);
+    }
+
+    public static List<String> convertSetToList(SortedSet<String> set) {
+        return Collections.emptyList();
+    }
+
+    public static boolean isOverLap(String nickName) {
+        return true;
     }
 }
