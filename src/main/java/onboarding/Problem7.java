0d19c987d3bd3d505f39692757a9fb8ea546abff
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 19:21:06 +0900

    user의 친구를 myFriends에 추가한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    private static void addMyFriend(List<String> myFriends, List<String> relation, String user) {
+        String user1 = relation.get(0);
+        String user2 = relation.get(1);
+        if (user1.equals(user)) {
+            myFriends.add(user2);
+        }
+        else if(user2.equals(user)) {
+            myFriends.add(user1);
+        }
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
