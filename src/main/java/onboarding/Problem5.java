6811f3299096a2217d011a49fb2dcfe9025eb328
hyunseo <goatsfish@gmail.com>
2022-11-01 14:48:38 +0900

    feat(Problem5) : 화폐 최소 개수 출금 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,48 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int[] CASH = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static final int[] wallet = new int[CASH.length];
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        initWallet();
+
+        convertToMoneyFrom(money);
+    }
+
+    private static void convertToMoneyFrom(int money) {
+        int currentCashIndex = 0;
+        while (money != 0) {
+            boolean isCashBiggerThanMoney = checkCashIsBiggerThanLeftMoney(money, currentCashIndex);
+            money = calculateLeftMoney(money, currentCashIndex, isCashBiggerThanMoney);
+            currentCashIndex = getNextCashIndex(currentCashIndex, isCashBiggerThanMoney);
+        }
+    }
+
+    private static boolean checkCashIsBiggerThanLeftMoney(int money, int currentMoneyIndex) {
+        return money < CASH[currentMoneyIndex];
+    }
+
+    private static int calculateLeftMoney(int money, int currentCashIndex, boolean isCashBiggerThanMoney) {
+        if (!isCashBiggerThanMoney) {
+            money -= CASH[currentCashIndex];
+            wallet[currentCashIndex]++;
+        }
+        return money;
+    }
+
+    private static int getNextCashIndex(int currentCashIndex, boolean isCashBiggerThanMoney) {
+        if (isCashBiggerThanMoney) {
+            currentCashIndex++;
+        }
+        return currentCashIndex;
+    }
+
+    private static void initWallet() {
+        Arrays.fill(wallet, 0);
     }
 }
