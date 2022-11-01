1bb3814b8547dae44252ee22291fe190158f7877
sangeun-b <baek0007@algonquinlive.com>
2022-11-01 20:13:38 +0900

    feat(PROBLEM5): Add feature for calculating the money type

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,12 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] coinType = {50000,10000,5000,1000,500,100,50,10,1};
+        int cnt = 0;
+        for(int i=0; i<coinType.length; i++){
+            answer.add(money/coinType[i]);
+            money %= coinType[i];
+        }
         return answer;
     }
 }
