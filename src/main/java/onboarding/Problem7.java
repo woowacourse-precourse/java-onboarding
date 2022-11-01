61e71884a7655c21be64145e3c3c34f679db0dc0
chj <bluedog129@gmail.com>
2022-11-01 22:28:02 +0900

    [P7] 구현한 그래프를 사용하여 함께아는 친구 +10, 방문자 +1 을 적용한 배열 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -51,6 +51,31 @@ public class Problem7 {
 
         return graph;
     }
+    private static int[] calculateScore(String user, List<List<Integer>> adj, List<String> visitors, Map<String, Integer> nameToNumber, List<String> allUserList) {
+        int[] score = new int[allUserList.size() + 1];
+        int userIndex = nameToNumber.get(user);
+
+        for (int i = 0; i < adj.get(userIndex).size(); i++) {
+            int node = adj.get(userIndex).get(i);
+            for (int j = 0; j < adj.get(node).size(); j++) {
+                int targetNode = adj.get(node).get(j);
+                if (targetNode == userIndex) continue;
+                score[targetNode] += 10;
+            }
+        }
+
+        for (int i = 0; i < visitors.size(); i++) {
+            int index = nameToNumber.get(visitors.get(i));
+            score[index]++;
+        }
+
+        for (int i = 0; i < adj.get(userIndex).size(); i++) {
+            int node = adj.get(userIndex).get(i);
+            score[node] = 0;
+        }
+
+        return score;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> allUserList = getUserList(user, friends, visitors);
 
@@ -58,6 +83,8 @@ public class Problem7 {
         Map<Integer, String> numberToName = mappingNumberToName(allUserList);
 
         List<List<Integer>> adj = makeUndirectedGraph(friends, nameToNumber, allUserList);
+
+        int[] score = calculateScore(user, adj, visitors, nameToNumber, allUserList);
 
         return allUserList;
     }
