d84f1e8595e8d85ce7d0052dec13fae2a91200e7
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-01 21:26:05 +0900

    feat : 관계있는 친구들을 HashMap 사용으로 재정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,12 +1,30 @@
 package onboarding;
-
-import java.util.Collections;
 import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        Map<String, Integer> pointboard = new HashMap<>();
+        Map<String, List<String>> userfriends = new HashMap<>();
+        List<String> answer = new ArrayList<>();
+        for (List<String> friend : friends) {
+            String leftfriend = friend.get(0);
+            String rightfriend = friend.get(1);
+
+            List<String> currentleft = new ArrayList<>();
+            List<String> currentright = new ArrayList<>();
+
+            if(userfriends.get(leftfriend) != null) currentleft = userfriends.get(leftfriend);
+            if(userfriends.get(rightfriend) != null) currentright = userfriends.get(rightfriend);
+
+            currentleft.add(rightfriend);
+            userfriends.put(leftfriend, currentleft);
+
+            currentright.add(leftfriend);
+            userfriends.put(rightfriend, currentright);
+        }
+
         return answer;
     }
-
 }
+
