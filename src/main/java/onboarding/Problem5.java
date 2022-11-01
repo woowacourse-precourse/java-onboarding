bbbae052c2c278d8d9f37beddf92918b08d169a7
Yeonji-Lim <view7186@naver.com>
2022-11-01 23:25:59 +0900

    Solved

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] monar = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int cnt = 0;
+        for(int mon : monar) {
+            while(money >= mon){
+                cnt++;
+                money -= mon;
+            }
+            answer.add(cnt);
+            cnt = 0;
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem6 {
+    public static boolean isDup (String n1, String n2) {
+        String target;
+        for(int i = 0; i < n1.length()-1; i++) {
+            target = n1.substring(i, i+2);
+            for(int j = 0; j < n2.length()-1; j++) {
+                if(target.equals(n2.substring(j, j+2))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        boolean[] dupcheck = new boolean[forms.size()];
+        for(int i = 0; i < forms.size(); i++) {
+            for(int j = 0; j < forms.size(); j++) {
+                if(i != j && !dupcheck[j] && isDup(forms.get(i).get(1), forms.get(j).get(1))) {
+                    dupcheck[i] = true;
+                    dupcheck[j] = true;
+                }
+            }
+        }
+        for(int i = 0; i < forms.size(); i++) {
+            if(dupcheck[i]) {
+                answer.add(forms.get(i).get(0));
+            }
+        }
+        answer.sort(Comparator.naturalOrder());
         return answer;
     }
 }
