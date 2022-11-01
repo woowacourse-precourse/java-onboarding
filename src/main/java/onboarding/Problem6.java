015fc864a9c88cc10c29b6234d267d7115c1300c
simba <shim9597@gmail.com>
2022-11-01 22:00:04 +0900

    Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,27 +1,52 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
 
 
-        List<String> answer = List.of("answer");
-        return answer;
+        return duplicateCheck(forms);
     }
 
-    public static List<String> getNickNameList(List<List<String >> forms) {
+    public static List<String> duplicateCheck(List<List<String>> forms) {
 
         List<String> nickNames = new ArrayList<>();
+        Set<String> duplicatedCrewEmail = new HashSet<>();
 
-        for (int i = 0; i <= forms.size(); i ++) {
-            nickNames.add(forms.get(i).get(1));
+        for (List<String> form : forms) {
+            nickNames.add(form.get(1));
         }
 
-        return nickNames;
 
+        for (int a = 0; a < nickNames.size() - 1; a++) {
+
+            for (int i = 1; a < i && i < nickNames.size(); i++) {
+
+
+                for (int k = 0; k <= nickNames.get(a).length() - 2; k++) {
+
+                    for (int j = 0; j <= nickNames.get(i).length() - 2; j++) {
+
+                        if (nickNames.get(a).substring(k, k + 2).equals(nickNames.get(i).substring(j, j + 2))) {
+
+                            duplicatedCrewEmail.add(forms.get(a).get(0));
+                            duplicatedCrewEmail.add(forms.get(i).get(0));
+
+                        }
+                    }
+                }
+            }
+        }
+
+        List<String> result = new ArrayList<>(duplicatedCrewEmail);
+        Collections.sort(result);
+
+        return result;
     }
+
+
+
 
 
 
