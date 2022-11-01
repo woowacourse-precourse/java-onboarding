531236f563d25b8107993d0a033221911d3d95b4
Gentlee <ekdma3259@gmail.com>
2022-11-01 19:24:01 +0900

    Problem1 - 각 자리의 숫자를 더하는 기능, 각자리의 숫자를 곱하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,24 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    // 각 자리의 숫자 더하는 메서드
+    public static int sumNum(int num){
+        int sum=0;
+        while(num > 0){
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+    // 각 자리의 숫자 곱하는 메서드
+    public static int mulNum(int num){
+        int mul=1;
+        while(num>0){
+            if(mul!=0){
+                mul *= num%10;
+            }
+            num /= 10;
+        }
+        return mul;
+    }
 }
