5b0b452ed0149a6864bf989c1484b08d1d493577
seungjun <seungjun123>
2022-11-01 13:37:42 +0900

    해시맵 형태로 청개구리 사전을 만드는 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    public static void setValue(HashMap<Character, Character> map){
+        map.put(' ', ' ');
+        for (int i = 0; i < 26; i++){
+            map.put((char)(65+i), (char)(90-i));
+        }
+        for (int i = 0; i < 26; i++){
+            map.put((char)(97+i), (char)(122-i));
+        }
     }
 }
