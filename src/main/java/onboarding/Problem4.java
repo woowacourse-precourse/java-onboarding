72ed62c0422e142d7504e853d629fed9fa9057aa
silver <silver36c@gmail.com>
2022-11-01 22:23:09 +0900

    feat: Problem4 글자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -16,4 +16,12 @@ public class Problem4 {
         return false;
     }
 
+    private static char changeCharacter(char c){
+        // 사전은 알파벳 순서 반대로 매칭되어 있음-> 사전에서 매칭되어 있는 숫자끼리 더하면 '일정'하다
+        final int UPPER = (int)'A' + (int)'Z';
+        final int LOWER = (int)'a' + (int)'z';
+        if (isUpper(c)) return (char) (UPPER - (int)c);
+        if (isLower(c)) return (char) (LOWER - (int)c);
+        return c;
+    }
 }
