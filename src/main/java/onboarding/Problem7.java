3b944767b9420c2c34a3d3deb9aaa5481d548d12
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2022-11-01 14:25:22 +0900

    feat(Problem7): 모든 친구, 이미 친구인 사람, 친구가 아닌 사람 리스트로 정리 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,51 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    private final static int LEFT_FRIEND = 0;
+    private final static int RIGHT_FRIEND = 1;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> answer = List.of("");
+        List<String> everyFriendList = makeEveryFriendsList(user,friends);
+        List<String> alreadyFriends = findAlreadyFriends(user,friends);
+        List<String> notFriends = findNotFriends(everyFriendList,alreadyFriends);
+        return answer = notFriends;
+    }
+
+    private static List<String> makeEveryFriendsList(String user, List<List<String>> friends){
+        List<String> FriendList = new ArrayList<>();
+        for(List<String> friend : friends){
+            for(String name:friend){
+                if(name!=user && !FriendList.contains(name))
+                    FriendList.add(name);
+            }
+        }
+        return  FriendList;
+    }
+
+    private  static List<String> findAlreadyFriends(String user,List<List<String>> friends){
+        List<String> alreadyFriend = new ArrayList<>();
+        for(List<String> friend : friends){
+            if(friend.get(LEFT_FRIEND) == user) {
+                alreadyFriend.add(friend.get(RIGHT_FRIEND));
+            }
+            if(friend.get(RIGHT_FRIEND) == user) {
+                alreadyFriend.add(friend.get(LEFT_FRIEND));
+            }
+        }
+        return alreadyFriend;
+    }
+
+    private static List<String> findNotFriends(List<String> everyFriendList,List<String> alreadyFriends){
+        List<String> notFriends = new ArrayList<>();
+        for(String name : alreadyFriends){
+            everyFriendList.remove(name);
+        }
+        notFriends = everyFriendList;
+        return notFriends;
     }
 }
