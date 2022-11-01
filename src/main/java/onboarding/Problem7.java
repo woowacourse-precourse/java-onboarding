aae471220ae0fd533fce5ca590e1bc2ad58411c3
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-11-01 18:58:27 +0900

    친구 및 방문자로 점수 계산

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,10 +9,38 @@ public class Problem7 {
         friendList.add(user);
         //모든 사람 리스트
         Set<String> peopleList = new HashSet<>();
+        //점수판
+        HashMap<String, Integer> friendPoint = new HashMap<>();
 
         collectAllPeopleAndUserFriends(user, friends, visitors, friendList, peopleList);
+
+        gradeScore(friends, visitors, friendList, peopleList, friendPoint);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void gradeScore(List<List<String>> friends, List<String> visitors, List<String> friendList, Set<String> peopleList, HashMap<String, Integer> friendPoint) {
+        for (String people : peopleList) {
+            friendPoint.put(people, 0);
+        }
+
+        //리스트로 10점 계산
+        for (List<String> friend : friends) {
+            if(friendList.indexOf(friend.get(0))!=-1 &&
+                    friendList.indexOf(friend.get(1))== -1 ){
+                friendPoint.put(friend.get(1), friendPoint.get(friend.get(1))+10);
+            }else if(friendList.indexOf(friend.get(1))!=-1 &&
+                    friendList.indexOf(friend.get(0))== -1){
+                friendPoint.put(friend.get(0), friendPoint.get(friend.get(0))+10);
+            }
+        }
+        //방문자로 1점 계산
+        for (String visitor : visitors) {
+            if(friendList.indexOf(visitor)==-1){
+                friendPoint.put(visitor, friendPoint.get(visitor)+1);
+            }
+        }
     }
 
     private static void collectAllPeopleAndUserFriends(String user, List<List<String>> friends, List<String> visitors, List<String> friendList, Set<String> peopleList) {
