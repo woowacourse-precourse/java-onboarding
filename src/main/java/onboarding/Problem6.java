0bc99443ca2217eec7955520f7f1b1f9a8024639
taehyun <didls1643@gmail.com>
2022-11-01 16:46:26 +0900

    6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
 
-public class Problem6 {
+public class email {
 	public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
         
