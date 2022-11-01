63d59e5542c3c77068287d451538c708616203e0
rt3310 <btac3310@gmail.com>
2022-11-01 13:23:07 +0900

    feat(Problem3): ClapNumber Enum 정의

---
+++ src/main/java/onboarding/problem3/ClapNumber.java
@@ -0,0 +1,13 @@
+package onboarding.problem3;
+
+public enum ClapNumber {
+    THREE(3),
+    SIX(6),
+    NINE(9);
+
+    private int number;
+
+    ClapNumber(int number) {
+        this.number = number;
+    }
+}
