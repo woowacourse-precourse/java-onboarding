d49bde7f3aa6a22a494e249869e2342c1ac9677a
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-01 18:39:07 +0900

    [Problem 6] chore: clean up

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,18 +5,7 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-class Main{
-    public static void main(String[] args){
-        Problem6 prob = new Problem6();
-        System.out.println(prob.solution(List.of(
-                    List.of("jm@email.com", "제이엠"),
-                    List.of("jason@email.com", "제이슨"),
-                    List.of("woniee@email.com", "워니"),
-                    List.of("mj@email.com", "엠제이"),
-                    List.of("nowm@email.com", "이제엠")
-            )));
-    }
-}
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
