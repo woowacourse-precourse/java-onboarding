4254a3140ace61f2375401748b74c6b530c1239e
송민진 <m_6595@naver.com>
2022-11-01 13:53:49 +0900

    #6 feat: make email-list of duplicated nicknames using indexes

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,12 +1,13 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
 
         List<String> emails = new ArrayList<>();
         List<String> nicknames = new ArrayList<>();
@@ -16,7 +17,11 @@ public class Problem6 {
         }
 
         List<Integer> duplicatedIndexes = checkNicknames(nicknames);
-
+        for (int index : duplicatedIndexes) {
+            answer.add(emails.get(index));
+        }
+        
+        Collections.sort(answer);
         return answer;
     }
 
@@ -24,17 +29,19 @@ public class Problem6 {
         List<Integer> indexes = new ArrayList<>();
 
         for (int i = 0; i < nicknames.size()-1; i++) {
-            String checkingNickname = nicknames.get(i);
-            List<String> checkingNicknameSlices = devideIntoTwo(checkingNickname);
+
+            String checkingNickname = nicknames.get(i);     // 이번에 검사할 닉네임
+            List<String> checkingNicknameSlices = devideIntoTwo(checkingNickname);  // 검사할 닉네임을 2글자씩 조각으로 분리
+
             List<String> others = new ArrayList<>(nicknames.subList(i+1,nicknames.size()));
             for (String other : others) {
                 for (String nowChecking : checkingNicknameSlices) {
-                    if (isDuplicated(nowChecking, other)) {
+                    if (isDuplicated(nowChecking, other)) {     // 중복일 경우
                         if (!indexes.contains(i)) {
-                            indexes.add(i);
+                            indexes.add(i);         // 두글자로 검사한 닉네임의 인덱스 추가
                         }
                         if (!indexes.contains(nicknames.indexOf(other))){
-                            indexes.add(nicknames.indexOf(other));
+                            indexes.add(nicknames.indexOf(other));      // 검사 대상이 된 닉네임의 인덱스 추가
                         }
                     }
                 }
@@ -50,8 +57,8 @@ public class Problem6 {
 
     private static List<String> devideIntoTwo(String nickname) {
         List<String> twoLetters = new ArrayList<>();
-            for (int i = 0; i < nickname.length() - 1; i++) {
-                String slicedStr = nickname.substring(i, i + 1);
+            for (int i = 0; i < nickname.length() - 2; i++) {
+                String slicedStr = nickname.substring(i, i + 2);
                 if (!twoLetters.contains(slicedStr)) {
                     twoLetters.add(slicedStr);
                 }
