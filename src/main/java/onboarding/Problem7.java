f550be81d6cf2b9ca2c9609c0f8ae0058f6a9cf5
cocomozila <joseyun94@naver.com>
2022-11-01 15:01:41 +0900

    [문제 7] 나오는 모든계정을 중복제거 후 리스트에 담아 리턴

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,11 +8,22 @@ public class Problem7 {
         List<String> answer = new ArrayList<>();
         HashMap<String, Integer> idToNumber = new HashMap<>();
         HashMap<Integer, String> numberToId = new HashMap<>();
-        HashSet<String> allUserList = new HashSet<>();
+        HashSet<String> allUserList = getAllUserList(user, friends, visitors);
         HashSet<String> myFriendList = new HashSet<>();
         List<Integer>[] friendGraph;
 
         return answer;
+    }
+
+    // 나오는 모든계정을 중복제거 후 리스트에 담아 리턴
+    private static HashSet<String> getAllUserList(String user, List<List<String>> friends, List<String> visitors) {
+        HashSet<String> allUserList = new HashSet<>();
+        for (int i = 0; i < friends.size(); i++) {
+            allUserList.add(user);
+            allUserList.addAll(friends.get(i));
+        }
+        allUserList.addAll(visitors);
+        return allUserList;
     }
 }
 
