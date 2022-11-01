f1b5852b64c4559d0011ac941d99c61dee617dbd
hyunseo <goatsfish@gmail.com>
2022-11-01 15:07:06 +0900

    feat(Problem6) : 분할된 닉네임 종류별 개수 저장 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,10 +6,38 @@ import java.util.stream.IntStream;
 
 public class Problem6 {
 
+    private static final int FORM_EMAIL_INDEX = 0;
+    private static final int FORM_NICKNAME_INDEX = 1;
+
+    private static Map<String, Integer> nicknamePiecesCount = new HashMap<>();
     private static Set<String> answer = new HashSet<>();
 
     public static List<String> solution(List<List<String>> forms) {
         answer.clear();
+        nicknamePiecesCount.clear();
+
+        initNicknamePiecesCount(forms);
+
+        List<String> distinctAnswer = new ArrayList<>(answer);
+        Collections.sort(distinctAnswer);
+
+        return distinctAnswer;
+    }
+
+    private static void initNicknamePiecesCount(List<List<String>> forms) {
+        for (List<String> userForm : forms) {
+            String nickname = userForm.get(FORM_NICKNAME_INDEX);
+            reflectNicknamePiecesCount(nickname);
+        }
+    }
+
+    private static void reflectNicknamePiecesCount(String nickname) {
+        for (String nicknamePiece : getDistinctNicknamePieces(nickname)) {
+            nicknamePiecesCount.put(
+                    nicknamePiece,
+                    nicknamePiecesCount.getOrDefault(nicknamePiece, 0) + 1
+            );
+        }
     }
 
     private static Set<String> getDistinctNicknamePieces(String nickname) {
