3014dfd684fe0f6cd69073a286e36bfab176e448
ahinkim <ahin98@naver.com>
2022-11-01 11:05:40 +0900

    refactor(problem3.md): Modify feature list, feat(problem3.java): Add function to count the number of specific characters in a string [feature1]

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
+    // 1. 문자열에서 특정 문자 개수를 센다.
+    public static int countChar(String str, char ch) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+            }
+        }
+        return count;
     }
+
 }
