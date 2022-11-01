2a0b8d8dd53e1da254af0f9e370c1a3c4bc9da00
Dasol Jung <greteljds0251@gmail.com>
2022-11-01 22:21:37 +0900

    fix: 7-1. Add a score to an ID that already exists

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,17 +9,26 @@ public class Problem7 {
         /*
         1. 친구의 친구 아이디 추가, 점수 + 10
         2. 방문자 아이디 추가, 점수 + 1
-        3. 만약 이미 아이디 있으면 점수만 추가
-        4. user, friends[][0]에 있는 아이디의 점수 0으로 만들기
-        5. 점수 높은 순으로 출력, 점수 0인 경우는 출력 안 함
+         + 만약 이미 아이디 있으면 점수만 추가
+        3. user, friends[][0]에 있는 아이디의 점수 0으로 만들기
+        4. 점수 높은 순으로 출력, 점수 0인 경우는 출력 안 함
          */
         List<String> answer = new ArrayList<>();
         List<String> name = new ArrayList<>();
         List<Integer> score = new ArrayList<>();
+        int a;
 
-        for (int i = 0; i < friends.size(); i++) {
-            name.add(friends.get(i).get(1));
-            score.add(10);
+        name.add(friends.get(0).get(1));
+        score.add(10);
+
+        for (int i = 1; i < friends.size(); i++) {
+            if (name.contains(friends.get(i).get(1))) {
+                a = score.get(name.indexOf(friends.get(i).get(1)));
+                score.add(name.indexOf(friends.get(i).get(1)), a + 10);
+            } else {
+                name.add(friends.get(i).get(1));
+                score.add(10);
+            }
         }
 
         return answer;
