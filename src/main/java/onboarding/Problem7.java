ebe25965f3505c3af4c83f9b1d00ff05cb50939c
ch200203 <ch200205@naver.com>
2022-11-01 20:46:52 +0900

    Problem7 기능 목록 1 작성 User의 친구 목록을 찾는 메소드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,48 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
+        List<String> usersFriends = getUsersfriends(friends, user);
+        System.out.println(usersFriends.toString());
+        Map<String, List<String>> friend_map = getfriends(friends);
+
         return answer;
+    }
+
+    /**
+     * User의 친구 목록을 찾는 메소드
+     * @param friends
+     * @return {List<String>} usersFriends
+     */
+    private static List<String> getUsersfriends(List<List<String>> friends, String user) {
+        List<String> usersFriends = new ArrayList<>();
+
+        for(List<String> fr_list : friends) {
+            if(fr_list.contains(user)) {
+                if(fr_list.get(0).equals(user)) usersFriends.add(fr_list.get(1));
+                else usersFriends.add(fr_list.get(0));
+            }
+        }
+
+        return usersFriends;
+    }
+
+    /**
+     * 친구 관계를 정리하여 리턴
+     * @param friends
+     * @return
+     */
+    private static Map<String, List<String>> getfriends(List<List<String>> friends) {
+        Map<String, List<String>> friend_map = new HashMap<>();
+
+
+        for (List<String> fr_list : friends) {
+            // fr_list.contains()
+        }
+        return null;
     }
 }
