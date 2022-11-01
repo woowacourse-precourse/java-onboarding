1eb8126d921fd2252c21984f9aa61cf2bd5c1755
jinuk <devuks0820@gmail.com>
2022-11-01 11:33:19 +0900

    feat: Problem3 Add method
    각 자리수가 3, 6, 9인지 비교해서 박수횟수를 카운팅하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,34 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        answer = countThreeSixNineNumber(number);
+
         return answer;
+    }
+
+    private static int countThreeSixNineNumber(int number) {
+        int count = 0;
+
+        for(int i = 1 ; i <= number ; ++i) {
+            count += findFactor(i);
+        }
+
+        return count;
+    }
+
+    private static int findFactor(int number) {
+        int count = 0;
+
+        while (number>0) {
+            int digit = number % 10;
+
+            if(digit == 3 || digit == 6 || digit == 9)
+                count++;
+
+            number /= 10;
+        }
+
+        return count;
     }
 }
