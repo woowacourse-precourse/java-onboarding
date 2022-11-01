aac9b6865fad3145763ee69090a5dca33041b667
신채원 <cw121@naver.com>
2022-11-01 16:35:09 +0900

    feat(problem5): 숫자 유효성 검사 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public class Problem5 {
 
+    private static final int minLength = 1;
+    private static final int maxLength = 1000000;
     private static final List<Integer> moneyList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
+        isValidate(money);
         List<Integer> answer = getCountMoney(money);
         return answer;
     }
@@ -19,5 +23,10 @@ public class Problem5 {
             money %= unit;
         }
         return answer;
+    }
+    private static void isValidate(int money) {
+        if (money < minLength || money > maxLength) {
+            throw new IllegalArgumentException("숫자가 1이상 1000000이하 이어야합니다.");
+        }
     }
 }
