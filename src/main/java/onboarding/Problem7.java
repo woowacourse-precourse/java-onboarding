4c5d28cf692b31a53d2664b81c6b33bb8a325383
hanjaegyeong <hjg000223@naver.com>
2022-11-01 19:52:41 +0900

    [Mission7] feat: setFriendOfFriendsList(), setScore1() 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -30,4 +30,27 @@ public class Problem7 {
             }
         }
     }
+    private static void setFriendOfFriendsList(String user, List<List<String>> friends, ArrayList<String> friendsList, HashMap<String, Integer> scoreMap, int score) {
+        for(int i = 0; i< friends.size(); i++){
+            for(int j=0; j<2; j++){
+                if(friendsList.contains(friends.get(i).get(1-j))){
+                    if(friends.get(i).get(j) != user){
+                        //score세팅
+                        setScore1(scoreMap, friends.get(i), j, 10);
+                    }
+                }
+            }
+        }
+    }
+    private static void setScore1(HashMap<String, Integer> scoreMap, List<String> friends, int j, int x) {
+        int score;
+        if(scoreMap.containsKey(friends.get(j))){
+            score = scoreMap.get(friends.get(j)) + x;
+            scoreMap.put(friends.get(j), score);
+        }
+        else{
+            score = x;
+            scoreMap.put(friends.get(j), score);
+        }
+    }
 }
