5d84e33bd48f078c833b78d5c74bf36f299b6e18
xi_jjun <rlawowns97@naver.com>
2022-11-01 14:22:05 +0900

    test(Problem6): 토큰의 개수를 저장하는 기능 테스트

--- src/test/java/onboarding/Problem6Test.java
+++ src/test/java/onboarding/Problem6Test.java
@@ -1,11 +1,15 @@
 package onboarding;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -33,4 +37,23 @@ class Problem6Test {
 		);
 	}
 
+	@DisplayName("tokenCounter 가 empty 할 때, 토큰이 모두 1개씩 저장되는지 확인")
+	@Test
+	void 토큰의_개수가_저장되는지_테스트() {
+		// given : nickname "김재준재" 에서 토큰화된 `Set` 이 주어졌을 때
+		final Set<String> tokensOfNickname = new HashSet<>(List.of("김재", "재준", "준재"));
+
+		// when : 토큰들의 개수를 'tokenCounter' 에 추가
+		Problem6.countToken(tokensOfNickname);
+
+		// then : 한 닉네임에 대해서는 모두 1개씩 추가되어야 한다.
+		Map<String, Integer> result = Problem6.tokenCounter;
+		final Map<String, Integer> expected = new HashMap<>() {{
+			put("김재", 1);
+			put("재준", 1);
+			put("준재", 1);
+		}};
+
+		assertThat(result).isEqualTo(expected);
+	}
 }
