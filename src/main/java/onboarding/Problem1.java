c470f992133e116a949dd8837d8a5c875d88dea1
sangmins930 <sangmins930@gmail.com>
2022-11-01 21:01:48 +0900

    check method upload

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,11 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean check(int left, int right) {
+        if ((1 <= left && left < 400) && (1 < right && right <= 400) && left + 1 == right) return false;
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
