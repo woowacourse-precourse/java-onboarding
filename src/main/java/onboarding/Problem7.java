2f610895dd757f72292b37075a3206614991d0e0
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-01 19:08:50 +0900

    feat: Problem 7, make array to save friend relation and visitors
    friends relation : adjacency matrix
    visitors : 1d array

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,6 +10,30 @@ public class Problem7 {
     static int index = 0;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        addMap(user);
+
+        //initailize( maybe scheduled to be refactorized )
+        for(int i = 0; i < friends.size(); i++){
+            addMap(friends.get(i).get(0));
+            addMap(friends.get(i).get(1));
+        }
+        int userNumber = userIndexMappingMap.size();
+        int[] visitor = new int[userNumber];
+        int[][] friendMatrix = new int[userNumber][userNumber];
+
+        for(int i = 0; i < visitors.size(); i++){
+            visitor[userIndexMappingMap.get(visitors.get(i))]++;
+        }
+        for(int i = 0; i < friends.size(); i++){
+            friendMatrix[userIndexMappingMap.get(friends.get(i).get(0))][userIndexMappingMap.get(friends.get(i).get(1))] = 1;
+            friendMatrix[userIndexMappingMap.get(friends.get(i).get(1))][userIndexMappingMap.get(friends.get(i).get(0))] = 1;
+        }
+
+        
+
+
+
+
 
 
 
