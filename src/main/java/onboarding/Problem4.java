e9866d722cee46eab961f42bd7930a5565019ecf
2do1 <dowon3036@naver.com>
2022-11-01 17:21:10 +0900

    feat: 문제 4번 문자열의 각 문자에 접근 후 아스키 값으로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            // 문자열의 각 문자에 접근 후 아스키 값으로 변환하는 기능
+            char ch = word.charAt(i);
+            int ascii = (int)ch;
+        }
         return answer;
     }
 }
