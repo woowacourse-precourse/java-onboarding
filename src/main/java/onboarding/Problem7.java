354a75ce291429f6ed07ec4dd1059ae5ddd489f5
ddogga <hayeonlee60327@gmail.com>
2022-11-01 17:08:37 +0900

    문제7 각 유저의 점수리스트 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,59 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String,Integer> graph =new HashMap<>();
+
+
+
+        for (List<String> f: friends){
+
+            graph = makeUserPointList(graph,f, user);
+        }
+
+        graph = makeUserPointList(graph,visitors,user);
+
+
+        Set<String> keySet = graph.keySet();
+        for (String key : keySet) {
+            System.out.println(key + " : " + graph.get(key));
+        }
+
+
         return answer;
+    }
+
+
+
+    public static HashMap<String,Integer> makeUserPointList(HashMap<String,Integer> graph,List<String> friend,String user){
+
+        for (int i =0; i < friend.size(); i++){
+            if (friend.get(i) != user && graph.get(friend.get(i)) == null) {
+                graph.put(friend.get(i),0);
+            }
+        }
+
+
+        return graph;
+    }
+
+
+    public static void main(String[] args) {
+        String user = "mrko";
+        List<List<String>> friends = List.of(
+                List.of("donut", "andole"),
+                List.of("donut", "jun"),
+                List.of("donut", "mrko"),
+                List.of("shakevan", "andole"),
+                List.of("shakevan", "jun"),
+                List.of("shakevan", "mrko")
+        );
+        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+        List<String> result = List.of("andole", "jun", "bedi");
+        System.out.println(solution(user, friends, visitors));
+
     }
 }
