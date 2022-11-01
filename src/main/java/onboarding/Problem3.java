0a56f6ba7e20aabfe27d3e7f276d988943a2fd44
ho2n <jungho2n@gmail.com>
2022-11-01 17:16:44 +0900

    1부터 인자로 전달된 숫자까지 369게임 시작

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = start(number);
+        return answer;
+    }
+
+    private static int start(int number) {
+        int answer = 0;
+        for (int i = 1; i <= number; ++i) {
+            answer += isContain369(i);
+        }
         return answer;
     }
 }
