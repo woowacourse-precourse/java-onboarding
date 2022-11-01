4cfbd3b52d35931395958ef8b5ce503cd2406c6b
jamangstangs <jamangsong@gmail.com>
2022-11-01 10:44:50 +0900

    feat: problem4(진행), changeCharacter() ->받은 문자를 알파벳 대문자, 소문자에 따라 청개구리 사전을 참고해 반대로 변환하는 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,34 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        // 하나씩 변환해서 answer로 만든다.
+        for (int i = 0; i < word.length(); i++) {
+            answer += changeCharacter(word.charAt(i));
+        }
+        
         return answer;
     }
+
+    private static String changeCharacter(char character) {
+        if (isCharacterUpperCase(character)) {
+            return getShiftedCharacterWithOffset('Z', getOffset('A', character));
+        } else if (isCharacterLowerCase(character)) {
+            return getShiftedCharacterWithOffset('z', getOffset('a', character));
+        }
+        return character;
+    }
+
+    private static char getShiftedCharacterWithOffset(char standardCharacter, Integer offset) {
+    }
+
+    private static Integer getOffset(char standardCharacter, char character) {
+    }
+
+    private static boolean isCharacterUpperCase(char character) {
+    }
+
+    private static boolean isCharacterLowerCase(char character) {
+    }
+
 }
