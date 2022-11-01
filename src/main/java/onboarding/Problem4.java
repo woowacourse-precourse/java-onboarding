b4f65741c403235fa51431c4c0f0e7cb5e4bc9c5
Jaewoooong Kim <kimje1109@gmail.com>
2022-11-01 15:23:37 +0900

    feat: Add conversionCase

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+    public static Character conversionCase(Character alphabet) {
+        if (alphabet >= 'a' && alphabet <= 'z') {
+            return (char)((int)alphabet - 32);
+        }
+        if (alphabet >= 'A' && alphabet <= 'Z') {
+            return (char)((int)alphabet + 32);
+        }
+
+        return alphabet;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
