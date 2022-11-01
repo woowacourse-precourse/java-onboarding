f0c61fd37ba4cb82f2b1d138a33c469c8acd5ccc
TeddyYeoni <po99027@naver.com>
2022-11-01 23:33:03 +0900

    PROBLEM3 기능 구현 및 solution 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        
+		int[] numberRange = new int[number];
+		
+		for (int i = 0; i < numberRange.length; i++) {
+			numberRange[i] = i+1;
+			
+			String numberToString = numberRange[i] + "";
+			String[] clap = numberToString.split("");
+			
+			for (int j = 0; j < clap.length; j++) {
+				int number369 = Integer.parseInt(clap[j]);
+				if (number369 == 3 || number369 == 6 || number369 == 9) {
+					answer++;
+				}
+			}
+		}
+        
         return answer;
     }
 }
