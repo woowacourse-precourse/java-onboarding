fb78c47543b737280b6e94771fc3892f1bc3ac6f
gksruf2848 <4455qqq@naver.com>
2022-11-01 22:13:00 +0900

    Problem4 기능1. 아스키 코드를 이용하여 문자를 바꾸는 changeWord() 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,18 @@ public class  Problem4 {
         String answer = "";
         return answer;
     }
+
+    static char changeWord(char c) {
+        // 대문자일 경우
+        if(c >= 65 && c <= 90) {
+            return (char)(155 - c);
+        }
+
+        // 소문자일 경우
+        if(c >= 97 && c <= 122) {
+            return (char) (219 - c);
+        }
+
+        return c;
+    }
 }
