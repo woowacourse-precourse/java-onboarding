6d4259c43fbe9f7f54f74ee7d88d00358d3ec55b
sdiaeyu6n <qdial214@dgu.ac.kr>
2022-11-01 20:00:15 +0900

    style: modify variable & function names following java code convention

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,30 +6,30 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        //nicknames = form에서 닉네임만 추출하고 한글자씩 분리하여 배열로 저장
-        List<List<String>> nicknames = new ArrayList<>();
+        //nickNames = form에서 닉네임만 추출하고 한글자씩 분리하여 배열로 저장
+        List<List<String>> nickNames = new ArrayList<>();
         for (int i=0;i<forms.size();i++){
-            nicknames.add(Arrays.asList(forms.get(i).get(1).split("")));
+            nickNames.add(Arrays.asList(forms.get(i).get(1).split("")));
         }
 
-        //splitnames = 이웃하는 두글자씩 묶어 set을 형성
-        List<List<String>> splitnames = new ArrayList<>();
-        for (int i=0;i<nicknames.size();i++) {
+        //splitNames = 이웃하는 두글자씩 묶어 set을 형성
+        List<List<String>> splitNames = new ArrayList<>();
+        for (int i=0;i<nickNames.size();i++) {
             List<String> temp = new ArrayList<>();
-            for (int j = 0; j < nicknames.get(i).size()-1; j++) {
-                temp.add(nicknames.get(i).get(j) + nicknames.get(i).get(j+1));
+            for (int j = 0; j < nickNames.get(i).size()-1; j++) {
+                temp.add(nickNames.get(i).get(j) + nickNames.get(i).get(j+1));
             }
-            splitnames.add(temp);
+            splitNames.add(temp);
         }
 
         //idx = 닉네임 중복이 발생하는 리스트의 인덱스를 저장
         HashSet<Integer> idx = new HashSet<>(); // 인덱스 값 중복 제거를 위해 HashSet으로 선언
-        for (int i=0;i<splitnames.size()-1;i++){
-            for(int j = 0; j < splitnames.get(i).size(); j++){
-                String target=splitnames.get(i).get(j);
-                for(int k=i+1; k<splitnames.size();k++) {
-                    for (int h = 0; h < splitnames.get(k).size(); h++) {
-                        if(target.equals(splitnames.get(k).get(h))){
+        for (int i=0;i<splitNames.size()-1;i++){
+            for(int j = 0; j < splitNames.get(i).size(); j++){
+                String target=splitNames.get(i).get(j);
+                for(int k=i+1; k<splitNames.size();k++) {
+                    for (int h = 0; h < splitNames.get(k).size(); h++) {
+                        if(target.equals(splitNames.get(k).get(h))){
                             idx.add(i);
                             idx.add(k);
                         }
