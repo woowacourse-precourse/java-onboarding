666bd0bd4657c1e26f99d119e62d8509cec8bf60
Umin <adorecamus@gmail.com>
2022-11-01 21:13:45 +0900

    changed how to change money using List collection

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,25 +7,12 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
-        int fiftyThousand = money / 50000;
-        int tenThousand = money % 50000 / 10000 ;
-        int fiveThousand= money % 50000 % 10000 / 5000;
-        int oneThousand = money % 50000 % 10000 % 5000 / 1000;
-        int fiveHundred = money % 50000 % 10000 % 5000 % 1000 / 500;
-        int oneHundred = money % 50000 % 10000 % 5000 % 1000 % 500 / 100;
-        int fifty = money % 50000 % 10000 % 5000 % 1000 % 500 % 100 / 50;
-        int ten = money % 50000 % 10000 % 5000 % 1000 % 500 % 100 % 50 / 10;
-        int one = money % 50000 % 10000 % 5000 % 1000 % 500 % 100 % 50 % 10;
+        List<Integer> moneyChange = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
-        answer.add(fiftyThousand);
-        answer.add(tenThousand);
-        answer.add(fiveThousand);
-        answer.add(oneThousand);
-        answer.add(fiveHundred);
-        answer.add(oneHundred);
-        answer.add(fifty);
-        answer.add(ten);
-        answer.add(one);
+        for(int i=0; i<moneyChange.size(); i++){
+            answer.add(money / moneyChange.get(i));
+            money = money % moneyChange.get(i);
+        }
 
         return answer;
     }
