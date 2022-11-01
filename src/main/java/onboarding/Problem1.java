9c5af1d9ba89f1acc0777ee29647974e67d438f2
tseop <77163842+tseop@users.noreply.github.com>
2022-11-01 23:44:26 +0900

    test

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         Problem1 mission = new Problem1();
