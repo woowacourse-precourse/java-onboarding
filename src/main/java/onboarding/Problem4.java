df7db0e88ff685a71d94b80c957970d5d33b32c5
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-01 13:14:26 +0900

    feat(Problem4): 소문자를 변환해서 리턴해주는 메소드 구현(기능목록2)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,4 +10,9 @@ public class Problem4 {
         int result = 65 - (int)c + 90;
         return (char)result;
     }
+
+    private static char changeLowerCase(char c) {
+        int result = 97 - (int)c + 122;
+        return (char)result;
+    }
 }
