ad853c2b9904d7007b0e7e39619542b046a8c6d4
sungjun kim <dd8873@naver.com>
2022-11-01 22:30:48 +0900

    feat: Problem2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,47 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (true) {
+            StringBuilder newCryptogram = new StringBuilder();
+            Stack<Character> temporaryStorage = new Stack<>();
+            int count = 0;
+            char removed = ' ';
+
+            for (int j = 0; j < cryptogram.length(); j++) {
+                char ch = cryptogram.charAt(j);
+                if (removed == ch) {
+                    removed = ' ';
+                    continue;
+                }
+                if (temporaryStorage.isEmpty()) {
+                    temporaryStorage.push(ch);
+                } else {
+                    if (temporaryStorage.peek() == ch) {
+                        removed = temporaryStorage.pop();
+                        count++;
+                    } else {
+                        temporaryStorage.push(ch);
+                    }
+                }
+            }
+
+            for (Character ch : temporaryStorage) {
+                newCryptogram.append(ch);
+            }
+
+            temporaryStorage.clear();
+            cryptogram = String.valueOf(newCryptogram);
+
+            if (count == 0) {
+                break;
+            }
+
+        }
+        return cryptogram;
     }
+
 }
