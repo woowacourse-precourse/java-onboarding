d28432dc4723dd2e561ed4adc27eddba4f12a7a3
kki4504 <rhkddlf9876@gmail.com>
2022-11-01 20:32:03 +0900

    feat : user의 친구를 아는 친구목록 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,18 +1,20 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     static class RecommendAlgorithm {
         List<String> userKnownMembers = new ArrayList<>();
         List<List<String>> userUnknownMembers = new ArrayList<>();
-
         String user;
 
         public void setUser(String user) {
             this.user = user;
         }
 
+
+        // 1번
         public void checkUserFriends (List<List<String>> friends){
             for (List<String> friend : friends) {
                 if (friend.contains(user)) {
@@ -24,8 +26,32 @@ public class Problem7 {
                     userUnknownMembers.add(friend);
                 }
             }
-
+            for (String friend : userKnownMembers) {
+                for (List<String> friendship : userUnknownMembers) {
+                    if (friendship.contains(friend)) {
+                        friendship.stream()
+                                .filter(f -> !f.equals(friend))
+                                .forEach(x -> setScore(x, "friends"));
+                    }
+                }
+            }
         }
+        public void setScore (String member, String category) {
+            if (category == "friends") {
+                if (membersScore.containsKey(member)) {
+                    membersScore.put(member, membersScore.get(member) + 10);
+                } else {
+                    membersScore.put(member, 10);
+                }
+            } else {
+                if (membersScore.containsKey(member)) {
+                    membersScore.put(member, membersScore.get(member) + 1);
+                } else {
+                    membersScore.put(member, 1);
+                }
+            }
+        }
+
 
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
