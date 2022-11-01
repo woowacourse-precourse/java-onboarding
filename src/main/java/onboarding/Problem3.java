54c66087737a0f33aca8988ea52013758d77116d
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-01 20:34:52 +0900

    problem3 executable statement

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String stringNum = Integer.toString(i);
+            answer += clapNumber(stringNum);
+        }
+
         return answer;
     }
 
