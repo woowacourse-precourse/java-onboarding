b7245fcef6a7a4a9ab04b652cb7718150f8c7210
0112leesy <0112leesy@naver.com>
2022-11-01 20:50:42 +0900

    [Problem7] feat: friend_map, score_map

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,12 +2,17 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Problem7 {
 
     static final int SCORE_OF_COMMON_FRIEND = 10;
     static final int SCORE_OF_TIMELINE_VISIT = 1;
     static final int MAX_LIST_SIZE = 5;
+
+    static Map<String, ArrayList<String>> friend_map;
+    static Map<String, Integer> score_map;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
