a90619b9558465612aafc58d7d49061d3ba9fac5
jsc1014 <wotlr5209@gmail.com>
2022-11-01 18:05:57 +0900

    2번문제 : 인접한 두 문자가 같은지 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+
         return answer;
+    }
+
+    public static boolean equalCheckNextLetter(String origin,String compare){
+        if(origin.equals(compare))
+            return true;
+        else
+            return false;
     }
 }
