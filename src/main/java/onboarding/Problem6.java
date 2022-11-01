55d181e064e34ad28c4a8a7f2ed756a08d13411a
Luka <meena2003@naver.com>
2022-11-01 12:24:44 +0900

    중복된 이름 비교하고 중복 글자 2개 이상 있으면 이메일로 저장 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -15,6 +15,8 @@ public class Problem6 {
     }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
+        HashSet<String> emailSet = new HashSet<>();
+        HashSet<String> duplicatedWordsSet = new HashSet<>();
         String[] arrayEmail = new String[forms.size()];
         String[] arrayName = new String[forms.size()];
 
@@ -26,18 +28,33 @@ public class Problem6 {
             }
         }
 
-        // 이름마다 두 글자씩 분리
+        // 이름마다 두 글자씩 분리해서 HashSet에 저장
         for (int i = 0; i < arrayName.length; i++) {
             String name = arrayName[i];
             for (int j = 0, x = 2; j < name.length() - 1; j++) {
-                String namePart = name.substring(j, x++);
-                System.out.println(namePart);
+                String duplicatedWord = name.substring(j, x++);
+                duplicatedWordsSet.add(duplicatedWord);
             }
-
         }
 
-        System.out.println(Arrays.toString(arrayEmail));
-        System.out.println(Arrays.toString(arrayName));
+        // 이름 하나씩 비교하면서 중복된 글자가 2개 이상 있으면 이메일을 저장
+        Iterator it = duplicatedWordsSet.iterator();
+        while (it.hasNext()) {
+            int i = 0;
+            String part = (String) it.next();
+            ArrayList<Integer> arrayListIdx = new ArrayList<>();
+            for (i = 0; i < arrayName.length; i++) {
+                if (arrayName[i].contains(part)) {
+                    arrayListIdx.add(i);
+                }
+            }
+            if (arrayListIdx.size() >= 2) {
+                for (int j = 0; j < arrayListIdx.size(); j++) {
+                    emailSet.add(arrayEmail[j]);
+                }
+            }
+            arrayListIdx.clear();
+        }
 
         return answer;
     }
