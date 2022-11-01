3471a8d274f695229e064be13779fea17314ecc1
Tae jeong Son <taejeongson@Taeui-MacBookAir.local>
2022-11-01 17:16:40 +0900

    [Feat] 연속된 페이지로 주어지지 않고 첫번째 값이 홀수 페이지가 아닐 때 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,7 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static boolean isException(List<Integer> pobi, List<Integer> crong)
+    public static boolean IsException(List<Integer> pobi, List<Integer> crong)
     {
         if (pobi.size() != 2 || crong.size() != 2)
             return true;
@@ -13,6 +13,10 @@ class Problem1 {
         for (Integer page : crong)
             if (page < 1 || page > 400)
                 return true;
+        if ((pobi.get(0) % 2 != 1) || (crong.get(0) % 2 != 1))
+            return true;
+        if ((pobi.get(0) + 1 != pobi.get(1)) || (crong.get(0) + 1 != crong.get(1)))
+            return true;
         return false;
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
