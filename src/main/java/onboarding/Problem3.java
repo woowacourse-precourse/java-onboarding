36019b3204e4850cbde0221eac958f750a078c25
DavinAn <blueapple1030@gmail.com>
2022-11-01 23:27:14 +0900

    Final Update

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+/**
+ * Problem 3
+ * @author Davin An
+ * @version 1.0
+ */
 public class Problem3 {
+
+    /**
+     * Counts the occurrences of 3, 6, and 9
+     * @param number Counts within the range of 1 through given number
+     * @return Number of occurrences
+     */
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        String temp;
+        for (int i = 1; i <= number; i++) {
+            temp = Integer.toString(i);
+            count += temp.length() - temp.replaceAll("[369]", "").length();
+        }
+        return count;
     }
+
+    public static void main(String[] args) {}
 }
