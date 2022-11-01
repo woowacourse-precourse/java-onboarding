5c347c0a43f3dec11b5acc4eae10c650a6b0774b
dener8 <8dener@gmail.com>
2022-11-01 15:40:21 +0900

    [Problem6] feat: 닉네임의 길이 2인 부분 문자열을 hashset에 넣기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static List<String> searchDuplicate(List<List<String>> forms) {
+        HashMap<String, Integer> allNicknameElements = new HashMap<String, Integer>();
+        String nickname;
+        for (int i = 0; i < forms.size(); i++) {
+            HashSet<String> nicknameElements = new HashSet<>();
+            nickname = forms.get(i).get(1);
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                nicknameElements.add(nickname.substring(i, i+2));
+            }
+        }
     }
 }
