d1e5ca3ef29b26e20d9b825366eb83d8f43fd817
alivejuicy <kkddhh1998@gmail.com>
2022-11-01 21:33:33 +0900

    feat 중복된 문자열은 모두 제거하고, 스택 적용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Problem2 {
+    public static Stack<String> cryptogramStack;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        stringToDecode(cryptogram);
+
+        cryptogramStack.stream().forEach(sb::append);
+
+        return sb.toString();
+    }
+    private static void stringToDecode(String cryptogram) {
+        cryptogramStack = new Stack<>();
+        for (String s : cryptogram.split("")) {
+            decode(s);
+        }
+    }
+    private static void decode(String s) {
+        String top = cryptogramStack.peek();
+
+        if (cryptogramStack.empty()) {
+            cryptogramStack.push(s);
+            return;
+        }
+        if (top == s) {
+            cryptogramStack.pop();
+            return;
+        }
+        cryptogramStack.push(s);
     }
 }
