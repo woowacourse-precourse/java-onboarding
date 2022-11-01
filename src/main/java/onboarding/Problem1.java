1a745d41dca7b50a48d6b044126d7b7d64066c96
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-01 18:35:10 +0900

    [P1] 점수를 비교함. (포비가 이기면 1, 크롱이 이기면 2, 무승부이면 0 반환)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,7 +15,7 @@ class Problem1 {
         totalScoreArr = new ArrayList<>();
         List<List<Integer>> userArr = List.of(pobi, crong);
 
-        // 1. 왼쪽과 오른쪽 페이지의 쪽수 차이가 2 이상일 때 예외 사항으로 간주 (-1 반환)
+        // 1. 왼쪽과 오른쪽 페이지의 쪽수 차이가 2 이상일 때 예외 사항으로 간주
         if ((Math.abs(pobi.get(0) - pobi.get(1)) > 1) || (Math.abs(crong.get(0) - crong.get(1)) > 1)) {
             answer = -1;
         } else {
@@ -36,6 +36,14 @@ class Problem1 {
                 // 3. 모두 더한 값과 모두 곱한 값 중 가장 큰 값을 본인의 점수로 함.
                 maxScore = Collections.max(userScoreArr);
                 totalScoreArr.add(maxScore);
+            }
+            // 4. 점수를 비교함. (포비가 이기면 1, 크롱이 이기면 2, 무승부이면 0 반환)
+            if (totalScoreArr.get(0) > totalScoreArr.get(1)) {
+                answer = 1;
+            } else if (totalScoreArr.get(0) < totalScoreArr.get(1)) {
+                answer = 2;
+            } else {
+                answer = 0;
             }
         }
         return answer;
