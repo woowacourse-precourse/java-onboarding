65209a331550cf1a5deaff8d4faf7a7306893c35
sangeun-b <baek0007@algonquinlive.com>
2022-11-01 20:05:50 +0900

    feat(PROBLEM4): Add feature for checking letter - check uppercase, lowercase, space, etc

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,12 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+    // 문자열 대문자,소문자,공백, 특수기호, 확인
+    public static String checkLetter(char letter){
+        if (Character.isUpperCase(letter)) return "A";
+        if (Character.isLowerCase(letter)) return "a";
+        else return "etc";
+
+    }
+
 }
