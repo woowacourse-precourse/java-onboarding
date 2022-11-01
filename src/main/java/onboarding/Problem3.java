03cd55643c28fef8613e79e547df72d2f073bfe6
SproutMJ <mjk54565@gmail.com>
2022-11-01 20:51:12 +0900

    feat: 숫자에서 3을 체크하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,13 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    static int checkThree(int n){
+        int result = 0;
+        while(n>0){
+            if(result%10 == 3)result++;
+            n/=10;
+        }
+        return result;
+    }
 }
