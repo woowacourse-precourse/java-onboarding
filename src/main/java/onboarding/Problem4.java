262d9e967bbae94dd0f74b9c7dddbf89642e02fc
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-01 18:58:22 +0900

    [P4] 알바벳 외의 문자는 빈칸으로 설정.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -13,6 +13,10 @@ public class Problem4 {
                 // 2. 문자가 소문자일 경우, 아스키코드값을 97로 설정.
             } else if (Character.isLowerCase(word.charAt(i))) {
                 ascii = 97;
+                // 3. 알바벳 외의 문자는 빈칸으로 설정.
+            } else {
+                answer += ' ';
+                continue;
             }
         }
         return answer;
