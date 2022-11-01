c78024c4680be616cde937b5359316df4148306f
이지영 <wldudwldhd@naver.com>
2022-11-01 14:49:51 +0900

    êdocs:(Problem6)기능구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    /*기능구현
+    예외처리
+    : 크루 1<=, <= 10000 이다.
+    : 이메일은 email.com만 가능
+    : 이메일 길이는 11<=, <20이다.
+    : 닉네임은 한글만 가능, 길이는 1<=, <20이다.
+    : result는 오름차순, 중복은 제거하여 출력
+
+    1. 같은 글자가 연속으로 포함된다 -> 2글자만 공통으로 포함되어도 중복으로 간주
+    2. 중복된 단어를 가진 닉네임을 저장 -> 해당 이메일을 정렬 후 반환
+    * */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
