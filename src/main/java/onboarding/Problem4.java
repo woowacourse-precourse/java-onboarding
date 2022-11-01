b5a64f56db080563e7fa55b6256fa0ccbdc9ca23
Lee-CntTheWins <Lee-CntTheWins>
2022-11-01 23:00:35 +0900

    week1 upload

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.Scanner;
+
 public class Problem4 {
+
+    /*
+    public static void main(String[] args) {
+        System.out.println(solution("I love you"));
+    }
+*/
     public static String solution(String word) {
         String answer = "";
+
+        char[] result = new char[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            char w = word.charAt(i);
+            if (w != ' ') {
+                if (w >= 'a' && w <= 'z') {
+                    w = (char) (w + 25 - ((w - 'a') * 2));
+                }
+
+                if (w >= 'A' && w <= 'Z') {
+                    w = (char) (w + 25 - ((w - 'A') * 2));
+                }
+            }
+            result[i] = w;
+        }
+        answer = String.valueOf(result);
         return answer;
     }
 }
+
+
+
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,13 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        char[][] arr = Stream.of(forms).map(String::toCharArray).toArray(Object[]::new);
+
         return answer;
     }
 }
