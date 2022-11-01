3e36d622f6d95fee10d69203a69889124135122b
hum02 <ehdgma02@gmail.com>
2022-11-01 12:20:28 +0900

    refactor:6번 로직 함수들로 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,36 +3,58 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
+    static final Set<String> twoChars = new HashSet<>(); //연속 글자들의 저장소
+    static final Set<String> duplicated = new HashSet<>(); //중복 글자들의 저장소
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
-        Set<String> twoChars = new HashSet<>();
-        Set<String> duplicated = new HashSet<>();
 
-        for(List<String> info:forms) {
+        for (List<String> info:forms) {
             String nickName = info.get(1);
-
-            for(String twoChar :twoChars) {
-                if(nickName.contains(twoChar)) {
-                    duplicated.add(twoChar);
-                }
-            }
-
-            for(int i=0; i<nickName.length()-1; i++) {
-                twoChars.add( nickName.substring(i,i+2) );
-            }
+            //닉네임에 연속글자가 이미 존재했으면 duplicated에 추가
+            checkDuplicated(nickName);
+            //닉네임들을 2글자씩 나눠 저장
+            saveTwoChars(nickName);
         }
 
-        for(List<String> info:forms) {
-            String nickName = info.get(1);
-            for(String dupString: duplicated) {
-                if (nickName.contains(dupString)) {
-                    String email = info.get(0);
-                    answer.add(email);
-                }
+        //같은 연속글자 포함하는 이메일들 얻기
+        for (List<String> info:forms) {
+            String email = getDuplicatedEmail(info);
+
+            if (email != null) {
+                answer.add(email);
             }
         }
 
         Collections.sort(answer);
         return answer;
+    }
+
+    private static void checkDuplicated(String nickName) {
+        for (String twoChar :twoChars) {
+            if (nickName.contains(twoChar)) {
+                duplicated.add(twoChar);
+            }
+        }
+    }
+
+    private static void saveTwoChars(String nickName) {
+        for (int i=0; i<nickName.length()-1; i++) {
+            String twoChar = nickName.substring(i,i+2);
+            twoChars.add(twoChar);
+        }
+    }
+
+    private static String getDuplicatedEmail(List<String> info) {
+        String nickName = info.get(1);
+
+        for (String dupString:duplicated) {
+            if (nickName.contains(dupString)) {
+                String Email = info.get(0);
+                return Email;
+            }
+        }
+
+        return null;
     }
 }
