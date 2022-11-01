0d413cd1d22ba6bba29a03237e2d78ee6d6b9687
kim1387 <kevink0@naver.com>
2022-11-01 17:37:27 +0900

    feat: 거스름돈 리스트 구하는 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+  private static final int[] CHANGE_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+  public static List<Integer> solution(int money) {
+    return solveChangeList(money);
+  }
+
+  private static List<Integer> solveChangeList(int money) {
+    List<Integer> answer = new ArrayList<>(10);
+    for (int change : CHANGE_LIST) {
+      answer.add(money / change);
+      money %= change;
     }
+    return answer;
+  }
 }
