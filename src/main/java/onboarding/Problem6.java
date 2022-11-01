fa475edab335d88ee0a05d6ea426c4c93d2adea6
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-01 14:45:53 +0900

    feat : 문제6 두글자 이상 겹치는지 체크

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,18 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+    private static List<String> answer;
+
+    //1) 같은 글자가 연속적으로 포함 되어있는지 체크
+    public static boolean SimilarCheck(String nickName1,String nickName2) {
+        for (int i = 0; i < nickName1.length() - 1; i++) {
+            if (nickName2.contains(nickName1.substring(i, i + 2)))
+                return true;
+        }
+        return false;
     }
+
+    
 }
