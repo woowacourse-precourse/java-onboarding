0e88e0917f3c5c0ceafdeb32f4866c2c9ef8e775
Nugur1i <duckhee092@naver.com>
2022-11-01 21:39:46 +0900

    refactor<problem5> : Refactoring the code shorter & faster

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,35 +1,22 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        int moneyUnit = 50000;
-        int numberOfKindOfMoney = 9;
+
+        List<Integer> moneyUnitArray = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
 
 
-        while (numberOfKindOfMoney > 0) {
-            if(numberOfKindOfMoney>2 && numberOfKindOfMoney%2 == 1){
-                answer.add(money/moneyUnit);
-                money %= moneyUnit;
-                moneyUnit /= 5;
+        int numberOfKindOfMoney = moneyUnitArray.size(); //화폐 단위 개수는 총 9개이다.
 
-            } else if (numberOfKindOfMoney>2 && numberOfKindOfMoney%2 == 0) {
-                answer.add(money/moneyUnit);
-                money %= moneyUnit;
-                moneyUnit /= 2;
-            } else if (numberOfKindOfMoney == 2){
-                answer.add(money/moneyUnit);
-                money %= moneyUnit;
-                moneyUnit /= 10;
-            } else if (numberOfKindOfMoney == 1){
-                answer.add(money/moneyUnit);
-            }
-            numberOfKindOfMoney -= 1;
-
+        for (int i = 0; i < numberOfKindOfMoney ; i++) {
+            answer.add(money/moneyUnitArray.get(i));
+            money %= moneyUnitArray.get(i);
         }
 
 
