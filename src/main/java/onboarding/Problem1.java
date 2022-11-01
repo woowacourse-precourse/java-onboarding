f5cde34c3df46ec9ebb2b5572b2599103056bfb8
yenawee <yaena0319@naver.com>
2022-11-01 15:46:45 +0900

    [FEAT] 1. addnumber 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,17 @@
 package onboarding;
-
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+    }
+
+    public static int addnumber(int n){
+        int sum = 0;
+        while (n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
