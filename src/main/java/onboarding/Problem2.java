d72c021111dbb5efbf7ce85de96656fa22276b7a
i960107 <i960107@naver.com>
2022-11-01 11:51:05 +0900

    feat(problem2): implement feature of converting string to character list

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        List<Character> charList = convertStringToCharList(cryptogram);
+        return null;
     }
+
+    public static List<Character> convertStringToCharList(String s) {
+        List<Character> charList = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            charList.add(s.charAt(i));
+        }
+        return charList;
+    }
+
+
 }
