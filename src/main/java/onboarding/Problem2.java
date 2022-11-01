9cd033d3ba8ff2d762781f11c95103707fbfac6a
Yebin <lyb2325@gmail.com>
2022-11-01 23:56:40 +0900

    feat: Problem2 스택에서 중복되는 문자열 제거 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Problem2 {
+
+    private static final char DEFAULT_VALUE = ' ';
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> cryptoStack = new Stack<>();
+        char recentChar = DEFAULT_VALUE;
+
+
+        for (char ch : cryptogram.toCharArray()) {
+            if (cryptoStack.isEmpty()) {
+                cryptoStack.push(ch);
+                recentChar = DEFAULT_VALUE;
+                continue;
+            }
+            if (ch == recentChar) {
+                continue;
+            }
+
+            if (cryptoStack.peek() == ch) {
+                recentChar = cryptoStack.pop();
+                continue;
+            }
+            cryptoStack.push(ch);
+        }
+        return cryptoStack.toString();
     }
 }
