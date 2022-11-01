06c8ca945b4ec89bac55af9ea40eac79cea06427
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 14:28:31 +0900

    feat: Map을 이용해 청개구리 사전 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
 
     private final static int THE_NUMBER_OF_ALPHABET = 26;
 
+    private final static HashMap<Character, Character> treeFrogDict = new HashMap<>();
+
+
     public static String solution(String word) {
         String answer = "";
+        createTreeFrogDictionary();
         return answer;
+    }
+
+    private static void createTreeFrogDictionary() {
+        for (int i = 0; i < THE_NUMBER_OF_ALPHABET; i++) {
+            treeFrogDict.put((char) ('a' + i), (char) ('z' - i));
+            treeFrogDict.put((char) ('A' + i), (char) ('Z' - i));
+        }
     }
 }
