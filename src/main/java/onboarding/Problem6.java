21c9179848d37bd1089a0d9dbf21c5bff5509748
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-01 17:28:09 +0900

    feat : P6 - 닉네임 받아 리스트 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<String> nicknames = collectNicknames(forms);
+
         return answer;
+    }
+
+    private static List collectNicknames(List<List<String>> name) {
+        List<String> collection = new ArrayList<>();
+        for (List target : name) {
+            collection.add((String)target.get(1));
+        }
+        return collection;
     }
 }
