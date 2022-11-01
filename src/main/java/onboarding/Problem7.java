abb9e935945d1d04ed29fd4b72ec9c6c8ff61285
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 19:06:00 +0900

    직접적으로 친구일 때

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,5 +7,15 @@ public class Problem7 {
         Set<String> userFriend = new HashSet<>(); // user와 직접 친구, 간접친구 구분 후 부여받을 점수 고려
         List<List<String>> knownFriend = new ArrayList<>();
         Map<String, Integer> friendScore = new HashMap<>();
+
+        for (List<String> sub : friends) {  // 전체 배열을 돌면서 직접친구 추출
+            if (sub.get(0).equals((user)))
+                userFriend.add(sub.get(1));
+            else if (sub.get(1).equals(user))
+                userFriend.add(sub.get(0));
+            else
+                knownFriend.add(sub);
+        }
+
     }
 }
