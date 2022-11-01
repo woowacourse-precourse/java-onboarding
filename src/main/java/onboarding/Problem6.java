3d98821c1c0363065432e0aaa093b3f90dd9349f
hgson <hgson.8@gmail.com>
2022-11-01 21:29:38 +0900

    [기능 6] (문제 6) 닉네임이 중복되는 이메일 목록 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,45 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class Problem6 {
+    /**key: 연속되는 문자 2개
+     * value: 연속되는 문자를 가진 크루의 이메일
+     * e.g. {
+     *     "제이": ["jason@email.com", "jm@email.com", "mj@email.com"]
+     * }
+     * value 길이가 2 이상이면 목록을 정렬해서 반환
+     * */
+    private static Map<String, Set<String>> nickNameMap = new HashMap<>();
+    
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> answer = new HashSet<>();
+
+        for (List<String> form : forms) {
+            setNicknameMap(form);
+        }
+
+        for (String sameChars : nickNameMap.keySet()) {
+            Set<String> emails = nickNameMap.get(sameChars);
+            if (emails.size() > 1) {
+                answer.addAll(emails);
+            }
+        }
+
+        return Arrays.asList(Arrays.stream(answer.toArray()).sorted().toArray(String[]::new));
+    }
+
+    private static void setNicknameMap(List<String> form) {
+        String email = form.get(0);
+        String name = form.get(1);
+        
+        for(int i = 0; i < name.length() - 1; i++) {
+            String chars = i == name.length() - 1 ? name.substring(i) : name.substring(i, i + 2);
+            Set<String> emailSet = nickNameMap.getOrDefault(chars, new HashSet<>());
+            emailSet.add(email);
+            nickNameMap.put(chars, emailSet);
+        }
     }
 }
