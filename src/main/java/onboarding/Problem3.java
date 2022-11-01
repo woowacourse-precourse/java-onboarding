43aeefa0df08e71b55692da46bd741d37076443c
sieun <supiya0908@naver.com>
2022-11-01 23:23:39 +0900

    Feat: Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i<=number; i++) {
+            String NumToString = Integer.toString(i);
+            for (String s : NumToString.split("")) {
+                if (s.equals("3") || s.equals("6") || s.equals("9")) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
