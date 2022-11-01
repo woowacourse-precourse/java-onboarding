30a2d72b6098c6721c5889f1beae666518f979fc
a1dotls <meeae9408@gmail.com>
2022-11-01 23:21:01 +0900

    problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,56 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+
+	public static String solution(String word) {
+
+		String compareWord = word.toUpperCase();
+		String answer = "";
+
+		String[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+				"S", "T", "U", "V", "W", "X", "Y", "Z" };
+		List<String> alphabetAry = Arrays.asList(alphabet);
+
+		List<String> wordAry = Arrays.asList(word.split(""));
+		List<String> compareWordAry = Arrays.asList(compareWord.split(""));
+		List<Integer> index = new ArrayList<Integer>();
+
+		for (String ch : compareWordAry) {
+
+			if (ch == " ") {
+				index.add(-1);
+			} else {
+				index.add(alphabetAry.indexOf(ch));
+			}
+
+		}
+
+		Collections.reverse(alphabetAry);
+		String[] alphabetRev = alphabetAry.toArray(alphabet);
+		List<String> alphabetRevAry = Arrays.asList(alphabetRev);
+		List<String> revWord = new ArrayList<String>();
+
+		for(int i = 0; i < wordAry.size(); i++) {
+			int indexNum = index.get(i);
+			String character = wordAry.get(i);
+			if (Character.isUpperCase(character.charAt(0))) {
+				revWord.add(alphabetRevAry.get(indexNum));
+			} else if (indexNum == -1) {
+				revWord.add(" ");
+			} else {
+				revWord.add(alphabetRevAry.get(indexNum).toLowerCase());
+			}
+		}
+
+		for(String str : revWord) {
+			answer += str;
+		}
+		System.out.println(answer);
+		return answer;
+	}
 }
