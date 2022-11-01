e3bcd131da6ab0b7f3859ba1f637151d6f4feba2
swa07016 <swa07016@khu.ac.kr>
2022-11-01 15:33:28 +0900

    Problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i < word.length(); i++) {
+            int ch = word.charAt(i);
+            if (ch >= 'A' && ch <='Z') ch = 'A' + 'Z' - ch;
+            if(ch >= 'a' && ch <='z') ch = 'a' + 'z' - ch;
+            answer += (char)ch;
+        }
         return answer;
     }
 }
