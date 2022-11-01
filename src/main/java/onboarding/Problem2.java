6838f6a07c5e229186f0545104b1221c645bd485
kjb95 <topmedia44@gmail.com>
2022-11-01 14:16:33 +0900

    refactor(Problem2): duplicateRemove() - 반복문 대신 stream 사용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,15 @@
 package onboarding;
 
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem2 {
 
+    private static final int ZERO = 0;
     private static final int ONE = 1;
-    private static final int INPUT_LEN_MIN = 1;
-    private static final int INPUT_LEN_MAX = 1000;
-    private static final String LOWER_ALPHABET_REGEX = "^[a-z]*$";
+    private static final int TWO = 2;
+    private static final String LOWER_ALPHABET_REGEX = "^[a-z]{1,1000}$";
 
     public static String solution(String cryptogram) {
         validateInput(cryptogram);
@@ -15,13 +17,9 @@ public class Problem2 {
     }
 
     private static void validateInput(String input) {
-        if (!isValidCryptogramForm(input) || !isValidCryptogramLen(input)) {
+        if (!isValidCryptogramForm(input)) {
             throw new IllegalArgumentException();
         }
-    }
-
-    private static boolean isValidCryptogramLen(String input) {
-        return input.length() >= INPUT_LEN_MIN && input.length() <= INPUT_LEN_MAX;
     }
 
     private static boolean isValidCryptogramForm(String input) {
@@ -29,31 +27,36 @@ public class Problem2 {
     }
 
     private static String computeDuplicateRemovedStr(String str) {
-        while (true) {
-            int beforeRemoveSize = str.length();
+        int beforeRemoveSize;
+
+        do
+        {
+            beforeRemoveSize = str.length();
             str = duplicateRemove(str);
-            if (beforeRemoveSize == str.length() || str.length() == ONE) {
-                break;
-            }
-        }
+        } while (beforeRemoveSize != str.length() && str.length() != ONE);
         return str;
     }
 
     static String duplicateRemove(String str) {
-        StringBuilder duplicateRemovedStr = new StringBuilder();
-        int i = 0;
+        return IntStream.range(ZERO, str.length())
+            .filter(i -> isNotDuplicatedWord(str, i))
+            .mapToObj(i -> String.valueOf(str.charAt(i)))
+            .collect(Collectors.joining());
+    }
 
-        while (++i < str.length()) {
-            if (str.charAt(i - 1) != str.charAt(i)) {
-                duplicateRemovedStr.append(str.charAt(i - 1));
-            }
-            while (i < str.length() && str.charAt(i - 1) == str.charAt(i)) {
-                i++;
-            }
-            if (i + 1 == str.length()) {
-                duplicateRemovedStr.append(str.charAt(i));
-            }
-        }
-        return duplicateRemovedStr.toString();
+    private static boolean isNotDuplicatedWord(String str, int i) {
+        return isNotDuplicatedFirstWord(str, i) || isNotDuplicatedMiddleWord(str, i) || isNotDuplicatedLastWord(str, i);
+    }
+
+    private static boolean isNotDuplicatedFirstWord(String str, int i) {
+        return i == ZERO && str.length() >= TWO && str.charAt(ZERO) != str.charAt(ONE);
+    }
+
+    private static boolean isNotDuplicatedLastWord(String str, int i) {
+        return i == str.length() - ONE && str.length() >= TWO && str.charAt(str.length() - ONE) != str.charAt(str.length() - TWO);
+    }
+
+    private static boolean isNotDuplicatedMiddleWord(String str, int i) {
+        return i != ZERO && i != str.length() - ONE && str.charAt(i) != str.charAt(i - ONE) && str.charAt(i) != str.charAt(i + ONE);
     }
 }
