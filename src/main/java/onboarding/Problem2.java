7556c196609ba7d4fac262ec0fa6b2838c6c1d27
이경범 <tkdwk567@naver.com>
2022-11-01 16:54:04 +0900

    # 2 solution @@ refactoring -- refactoring

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,42 +1,32 @@
 package onboarding;
 
+import onboarding.problem2.CryptogramConverter;
 import onboarding.problem2.ProblemTwoValidation;
 
 import java.util.*;
 
 public class Problem2 {
 
-    private static ProblemTwoValidation validation = new ProblemTwoValidation();
-
     private static Stack<String> stack = new Stack<>();
 
     public static String solution(String cryptogram) {
-        if (!checkStringLength(cryptogram)) {
+        CryptogramConverter convertor;
+        try {
+            convertor = new CryptogramConverter(new ProblemTwoValidation(cryptogram));
+            convertor.duplicatedWord(cryptogram);
+
+            return convertor.getConverterWord();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
             return "";
         }
-
-        // 검증 코드 분리
-        String validString = validation.getValidString();
-
-        // 중복제거 코드 분리
-        duplicatedStringRemoveProcess(validString);
-
-        String result = String.join("", stack);
-        stack.clear();
-        return result;
     }
 
     public static void main(String[] args) {
         System.out.println(solution("zyelleyz"));
     }
 
-    private static boolean checkStringLength(String cryptogram) {
-        validation.settingCryptogram(cryptogram);
-        if (validation.isLength()) {
-            return true;
-        }
-        return false;
-    }
 
     private static void duplicatedStringRemoveProcess(String validString) {
         String[] validStringToStringSplit = validString.split("");
