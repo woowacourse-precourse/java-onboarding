a000141d4b61464d629ae852745f53ec08d48b9b
chaos0103 <lyt1228@naver.com>
2022-11-01 11:18:02 +0900

    refactor(Problem4): 코드 리팩토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,34 +1,33 @@
 package onboarding;
 
+import static java.lang.Character.*;
+
 public class Problem4 {
     public static String solution(String word) {
         return stringConverter(word);
     }
 
     private static String stringConverter(String target) {
+        char[] targetArray = getCharArray(target);
+
         StringBuilder builder = new StringBuilder();
-        for (int index = 0; index < target.length(); index++) {
-            char result = charConverter(target.charAt(index));
-            builder.append(result);
+        for (char ch : targetArray) {
+            builder.append(charConverter(ch));
         }
         return String.valueOf(builder);
     }
 
+    private static char[] getCharArray(String target) {
+        return target.toCharArray();
+    }
+
     private static char charConverter(char target) {
-        if (isUppercase(target)) {
+        if (isUpperCase(target)) {
             return (char) ('Z' - (target - 'A'));
-        } else if (isLowercase(target)) {
+        } else if (isLowerCase(target)) {
             return (char) ('z' - (target - 'a'));
         } else {
             return target;
         }
-    }
-
-    private static boolean isUppercase(char target) {
-        return 'A' <= target && target <= 'Z';
-    }
-
-    private static boolean isLowercase(char target) {
-        return 'a' <= target && target <= 'z';
     }
 }
