4bcad426d9abbb90bfcc56051b1fb6f6159fb117
lhyeonj120 <isk04387@gmail.com>
2022-11-01 19:03:43 +0900

    [problem1] refactor: 페이지 번호 예외처리 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -38,6 +38,12 @@ class Problem1 {
 	}
 	
 	private static boolean isCorrect(List<Integer> list) {
+		if(list.get(0) < 1 || list.get(1) > 400) {
+			return false;
+		}
+		if(list.get(0) % 2 != 1 || list.get(1) % 2 != 0) {
+			return false;
+		}
 		if(list.get(0) + 1 != list.get(1)) {
 			return false;
 		}
