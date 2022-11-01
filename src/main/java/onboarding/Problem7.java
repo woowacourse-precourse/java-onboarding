f636e7934c4b3e148daf4ea31c0440166da5d246
jamangstangs <jamangsong@gmail.com>
2022-11-01 11:21:05 +0900

    feat: problem7(진행), setFriendScoreOfUser() -> 친구관계 점수를 계산하여 친구점수hashMap에 적용하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        HashMap<String, Integer> friendCandidateHashMap = new HashMap<>();
+        setFriendScoreOfUser(friendCandidateHashMap, friends, user);
+
+
         return answer;
+    }
+
+    private static void setFriendScoreOfUser(HashMap<String, Integer> friendCandidateHashMap, List<List<String>> friends, String user) {
+        List<String> friendsOfUser = getFriendOfUser(friends, user);
+        List<String> friendsOfFriendOfUser = getFriendOfFriendOfUser(friendsOfUser, friends, user);
+        for (String friendOfFriendOfUser : friendsOfFriendOfUser) {
+            friendCandidateHashMap.put(friendOfFriendOfUser, friendCandidateHashMap.containsKey("friendOfFriendOfUser") ? friendCandidateHashMap.get(friendOfFriendOfUser) + 10 : 10);
+        }
+    }
+
+    private static List<String> getFriendOfUser(List<List<String>> friends, String user) {
+    }
+
+    private static List<String> getFriendOfFriendOfUser(List<String> friendsOfUser, List<List<String>> friends, String user) {
     }
 }
