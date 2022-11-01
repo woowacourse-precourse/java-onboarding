7be50da8f068a2bc2889a15b4554415ec68e4a38
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 14:41:05 +0900

    feat: 돈의 최소 단위를 원소로 가지는 상수 배열 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,14 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private final static List<Integer> moneyUnitList = Arrays.asList(
+            50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
+    );
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
