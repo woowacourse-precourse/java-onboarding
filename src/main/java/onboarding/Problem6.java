e282dbc45057d1fceefe25ec9e67e0454301cdf2
YUSEONJUN <seonjun0906@gmail.com>
2022-11-01 12:45:04 +0900

    [Problem 6] 기능 요구 사항 1 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        // List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+
+        for (List<String> list : forms) {
+            String email = list.get(0);
+            String id = list.get(1);
+
+
+        }
+
         return answer;
     }
 }
