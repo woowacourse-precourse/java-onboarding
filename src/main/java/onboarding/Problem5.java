867efb820850fe2d09d46c55904b2293558b9a81
JangHyojoon <fdasfaj9@gmail.com>
2022-11-01 19:30:59 +0900

    문제 5번 기능 1 예외 처리 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
         return answer;
     }
+    private static boolean exception(int number) {
+        if(number < 1 || number > 1000000){
+            return false;
+        }
+
+        return true;
+    }
 }
+
