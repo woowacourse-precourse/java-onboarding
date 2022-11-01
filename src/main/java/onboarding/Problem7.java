534c49ad363279ec094cf1d08de9d1202a8d44fc
ParkJoungWoo <jug78@naver.com>
2022-11-01 19:18:53 +0900

    문제 7) 기능 1 : 전체 친구관계 리스트와 유저를 입력받고 유저의 친구 집합을 반환하는 기능 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,10 +2,22 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static Set<String> findUserFriends(String user, List<List<String>> friends) {
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i < friends.size(); i++) {
+            for (int j = 0; j < 2; j++)
+                if (friends.get(i).get(j) == user) {
+                    result.add(friends.get(i).get(1 - j));
+                }
+        }
+        return result;
     }
 }
