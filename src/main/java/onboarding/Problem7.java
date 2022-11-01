4863c148360b8745518849547315974a3e3f3673
gilyeon00 <12venus08@naver.com>
2022-11-01 22:01:08 +0900

    Feat : 문제7 - 사용자의 친구의 친구(건너 아는 친구)를 찾는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -18,11 +18,13 @@ import java.util.*;
 
 public class Problem7 {
     static List<String> userFriend;
+    static List<String> acrossFriends;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         userFriend = new ArrayList<>();
+        acrossFriends = new ArrayList<>();
 
         return answer;
     }
@@ -37,5 +39,27 @@ public class Problem7 {
             }
         }
     }
+
+    /** user의 친구의 친구(user와 함께 아는 친구) **/
+    private static void isAcrossFriend(String user, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            /* user일 경우 예외 */
+            if (friend.get(0).equals(user) || friend.get(1).equals(user)) {
+                continue;
+            }
+            /* user의 친구일 경우 예외 */
+            if (userFriend.contains(friend.get(0)) && userFriend.contains(friend.get(1))) {
+                continue;
+            }
+
+            /* 위 예외사항 제외하고 user친구 목록에 포함되는 사람이 있다면, 다른 한 명을 함께아는 친구목록(acrossFriends)에 추가 */
+            if (userFriend.contains(friend.get(0))) {
+                acrossFriends.add(friend.get(1));
+            } else if (userFriend.contains(friend.get(1))) {
+                acrossFriends.add(friend.get(0));
+            }
+        }
+    }
+
 
 }
