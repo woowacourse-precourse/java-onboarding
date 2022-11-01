4a82faf7b74e97428e64fd6251794d35db2e6d42
tenta <eunoia3802@gmail.com>
2022-11-01 16:18:50 +0900

    correct: 배열 초기화 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Problem5 {
-    static int[] unitOfMoney;
 
     public static List<Integer> solution(int money) {
         List answer = exchangeMoney(money);
@@ -13,7 +12,7 @@ public class Problem5 {
     }
 
     public static List exchangeMoney(int money) {
-        unitOfMoney = new int[9];
+        int[] unitOfMoney = new int[9];
 
         if (money >= 50000) {
             int moneyCount = money / 50000;
@@ -71,14 +70,12 @@ public class Problem5 {
                 unitOfMoney[7]++;
             }
         }
-        if (money < 10) { //1원짜리만 있을 때
+        if (money < 10) {
             for (int i = 0; i < money; i++) {
                 unitOfMoney[8]++;
             }
         }
         List MoneyTypeToList = Arrays.stream(unitOfMoney).boxed().collect(Collectors.toList());
-
         return MoneyTypeToList;
     }
-
 }
