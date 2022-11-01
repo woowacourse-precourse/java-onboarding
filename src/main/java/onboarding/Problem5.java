33fc134d41c4e2ec09ea5aa193956eddf3583c03
taehyun <didls1643@gmail.com>
2022-11-01 16:55:48 +0900

    5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,12 +7,7 @@ import java.util.List;
 public class Problem5 {
 	public static List<Integer> solution(int money) {
 		List<Integer> answer = new ArrayList<>();
-       
-		int[] m = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-		for(int i = 0; i < m.length; i++) {
-			answer.add(money/m[i]);
-			money %= m[i];
-		}
+
         return answer;
     }
 }
