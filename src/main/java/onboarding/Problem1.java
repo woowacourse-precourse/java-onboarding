1ee2c621dfa1a1cc1c4fa5a39fda885a49f108e2
hataerin <goil1113@likelion.org>
2022-11-01 20:21:18 +0900

    DOCS: Problem1 기능 목록 작성
    Signed-off-by: hataerin <goil1113@likelion.org>

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+
+        //포비-페이지 계산 기능
+
+        //포비-최댓값 저장 기능
+
+        //크롱-페이지 계산 기능
+
+        //크롱-최댓값 저장 기능
+
+        //승자 출력 기능
+
+
+       int answer = nteger.MAX_VALUE;
         return answer;
     }
 }
