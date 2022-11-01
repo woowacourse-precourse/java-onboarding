007953ab25b6e949401d13e7f65485ffeb666004
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-01 09:59:55 +0900

    refactor: rename text to docodingText

--- src/main/java/onboarding/problem2/Decoder.java
+++ src/main/java/onboarding/problem2/Decoder.java
@@ -5,19 +5,19 @@ import java.util.Queue;
 
 public class Decoder {
 
-    private final Queue<Character> text = new LinkedList<>();
+    private final Queue<Character> decodingText = new LinkedList<>();
 
     public Decoder(String cryptogram) {
         for (int i = 0; i < cryptogram.length(); i++)
-            text.add(cryptogram.charAt(i));
+            decodingText.add(cryptogram.charAt(i));
     }
 
     public String decode() {
         int size;
         do {
-            size = text.size();
+            size = decodingText.size();
             removeDuplication(size);
-        } while (size != text.size() && !text.isEmpty());
+        } while (size != decodingText.size() && !decodingText.isEmpty());
         return buildPlainText();
     }
 
@@ -25,25 +25,25 @@ public class Decoder {
         boolean duplicated = false;
 
         for (int i = 0; i < size - 1; i++) {
-            Character ch = text.poll();
-            if (text.peek() == ch)
+            Character ch = decodingText.poll();
+            if (decodingText.peek() == ch)
                 duplicated = true;
             else if (duplicated)
                 duplicated = false;
             else
-                text.add(ch);
+                decodingText.add(ch);
         }
 
         if (duplicated)
-            text.poll();
+            decodingText.poll();
         else
-            text.add(text.poll());
+            decodingText.add(decodingText.poll());
     }
 
     private String buildPlainText() {
         StringBuilder plainBuilder = new StringBuilder();
-        while (!text.isEmpty())
-            plainBuilder.append(text.poll());
+        while (!decodingText.isEmpty())
+            plainBuilder.append(decodingText.poll());
         return plainBuilder.toString();
     }
 }
