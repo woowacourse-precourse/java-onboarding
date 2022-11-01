83ec6fbd6b40c810515cbb755d8e579cc30e87d7
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-01 14:10:35 +0900

    feat : P3 - 3, 6, 9의 개수 세서 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = countClap(number);
         return answer;
+    }
+
+    private static int countClap(int num) {
+        int cnt = 0;
+        for (int i = 1; i < num + 1; i++) {
+            int temp = i;
+
+            while (temp != 0) {
+                if (temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9)
+                    cnt++;
+                temp /= 10;
+            }
+        }
+        return cnt;
     }
 }
