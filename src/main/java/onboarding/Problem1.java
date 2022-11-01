e6133ef84c7949adfbf6e18b9220c0ec701a436d
julie0005 <julie0005@ajou.ac.kr>
2022-11-01 21:12:58 +0900

    feat : problem 1 is solved

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,44 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        if (!validatePage(pobi) || !validatePage(crong)) return -1;
+        int pobiNum = calculatePageMaxNumber(pobi);
+        int crongNum = calculatePageMaxNumber(crong);
+        int answer = 0;
+        if (pobiNum - crongNum < 0) {
+            answer = 1;
+        }
+        else if (pobiNum - crongNum > 0) {
+            answer = 2;
+        }
         return answer;
     }
+
+    private static boolean validatePage(List<Integer> page) {
+        return page.get(1) - page.get(0) == 1;
+    }
+
+    private static int addEachDigit(Integer num) {
+        int divided = num;
+        int sum = 0;
+        while (divided != 0) {
+            sum += divided % 10;
+            divided /= 10;
+        }
+        return sum;
+    }
+
+    private static int multiplyDigit(Integer num) {
+        int divided = num;
+        int sum = 1;
+        while (divided != 0) {
+            sum *= divided % 10;
+            divided /= 10;
+        }
+        return sum;
+    }
+
+   private static int calculatePageMaxNumber(List<Integer> page) {
+        return Math.max(addEachDigit(page.get(0)), multiplyDigit(page.get(1)));
+   }
 }
