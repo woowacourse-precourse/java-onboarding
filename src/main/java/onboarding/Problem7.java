37e629b9dd2ba94dd409528524251e1d3a60636c
DESKTOP-994V1OK\김은선 <agseon9@gmail.com>
2022-11-01 20:03:58 +0900

    Problem7 기능 목록 1 : 본인의 친구가 누구인지 찾아 리스트에 저장하는 함수 findMyFriend를 구현한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
-
+import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+        List<String> answer = new ArrayList<>();
+
+        List<String> resultFineMyFriend = findMyFriend(user, friends);
+
         return answer;
+    }
+
+    public static List<String> findMyFriend(String user, List<List<String>> friends){
+
+        String newString;
+        List<String> friend = new ArrayList<>();
+
+        for(int i = 0; i < friends.size(); i++){
+            for (int j = 0; j < friends.get(i).size(); j++){
+                if(friends.get(i).get(j).equals(user)){
+                    List<String> newList = new ArrayList<>(friends.get(i));
+                    newList.remove(user);
+                    newString = String.join(",", newList);
+                    friend.add(newString);
+                }
+            }
+        }
+        return friend;
     }
 }
