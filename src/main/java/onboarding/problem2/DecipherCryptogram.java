b78b9db5e3a820e61990d0e8cfd13a2bb9eafce7
seominseok00 <seominseok4834@gmail.com>
2022-11-01 23:00:12 +0900

    feat(문제2): 중복 제거 메소드 구현

--- src/main/java/onboarding/problem2/DecipherCryptogram.java
+++ src/main/java/onboarding/problem2/DecipherCryptogram.java
@@ -1,9 +1,28 @@
 package onboarding.problem2;
 
+import java.util.Stack;
+
 public class DecipherCryptogram {
     private String cryptogram;
-    
+    private Stack<Character> stack = new Stack<>();
+
     public DecipherCryptogram(String cryptogram){
         this.cryptogram = cryptogram;
     }
+
+    private void decipherCryptogram() {
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (stack.size() == 0) {
+                stack.push(cryptogram.charAt(i));
+            } else {
+                if (stack.peek().equals(cryptogram.charAt(i))) {
+                    stack.pop();
+                } else {
+                    stack.push(cryptogram.charAt(i));
+                }
+            }
+        }
+    }
+
+    
 }
