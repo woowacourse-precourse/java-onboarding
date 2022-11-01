5647404d3f52275bc020d2212983773d465499bd
thddh <dhthdrud@naver.com>
2022-11-01 21:55:52 +0900

    feat: Problem4 finished

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (char c : word.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                answer += (char) ('Z' - (c - 'A'));
+            } else if (c >= 'a' && c <= 'z') {
+                answer += (char) ('z' - (c - 'a'));
+            } else {
+                answer += c;
+            }
+        }
         return answer;
     }
 }
