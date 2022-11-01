5102289388763d1e731b42303e1df3f6496982d3
song-wooseok <sws981202@gmail.com>
2022-11-01 18:53:38 +0900

    feat: [PROBLEM7 - 예외사항3] friend 리스트의 각 원소(리스트)의 길이 검사

---
+++ src/main/java/onboarding/problem7/utils/validator/exception/FriendRelationLengthException.java
@@ -0,0 +1,7 @@
+package onboarding.problem7.utils.validator.exception;
+
+public class FriendRelationLengthException extends Exception{
+    public FriendRelationLengthException(String msg){
+        super(msg);
+    }
+}
