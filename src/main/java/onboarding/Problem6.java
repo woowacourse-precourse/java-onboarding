a68edb260d2e93f9a19f552f74095a98c3342597
jinuk <devuks0820@gmail.com>
2022-11-01 11:35:28 +0900

    feat: Problem6 Add method
    닉네임중 두글자 이상 중복되는 경우를 검사하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,47 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = checkOverlapNickName(forms);
+
         return answer;
+    }
+
+    private static List<String> checkOverlapNickName(List<List<String>> forms) {
+
+        Set<String> uniqueNames = new HashSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+
+            String originName = forms.get(i).get(1);
+
+            for (int j = 0; j < originName.length()-1; j++) {
+
+                String partName = originName.substring(j,j+2);
+
+                if(checkOverlap(partName, i, forms))
+                    uniqueNames.add(forms.get(i).get(0));
+            }
+        }
+
+        List<String> overlapNickNames = new ArrayList<>(uniqueNames);
+        Collections.sort(overlapNickNames);
+        return overlapNickNames;
+    }
+
+    private static boolean checkOverlap(String str, int currentIndex, List<List<String>> forms) {
+
+        for (int i = 0 ; i < forms.size();++i) {
+
+            if(i == currentIndex)
+                continue;
+
+            if(forms.get(i).get(1).contains(str))
+                return true;
+        }
+
+        return false;
     }
 }
