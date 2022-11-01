545a879184b7ed3aec56b3e3baa6b6bd90011c9d
core <jinjs6@gmail.com>
2022-11-01 19:45:44 +0900

    Problem6 두 문자열에서 연속으로 중복된 글자 존재 여부 확인 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,22 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    public static boolean check(String s1, String s2) {
+        for (int i = 0; i < s1.length() - 1; ++i) {
+            for (int j = 0; j < s2.length() - 1; ++j) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    if (s1.charAt(i + 1) == s2.charAt(j + 1)) {
+                        return true;
+                    } else {
+                        ++j;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
