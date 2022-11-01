fc8dcb5cb4eb2e822b5ef8f279838ff4fd48b561
신채원 <cw121@naver.com>
2022-11-01 16:05:59 +0900

    feat(problem1): 페이지 유효성 검사 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,11 +3,37 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int length = 2;
+    private static final int minPage = 1;
+    private static final int maxPage = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobiScore = getScore(pobi);
         int crongScore = getScore(crong);
         int answer = getResult(pobiScore,crongScore);
         return answer;
+    }
+
+    private static boolean validation(List<Integer> page){
+        int leftPage = page.get(0);
+        int rightPage = page.get(1);
+        if(isNotPageSize(page)){
+            return false;
+        }
+        if(!isInRange(leftPage)||!isInRange(rightPage)){
+            return false;
+        }
+        if(!isOddNumber(leftPage)){
+            return false;
+        }
+        if(!isEvenNumber(rightPage)){
+            return false;
+        }
+        if(notCheckPageOrder(leftPage,rightPage)){
+            return false;
+        }
+        return true;
     }
 
     private static int addEachDigits(int page){
@@ -42,5 +68,25 @@ class Problem1 {
         }else{
             return 0;
         }
+    }
+
+    private static boolean isNotPageSize(List<Integer> pages) {
+        return pages.size()!=length;
+    }
+
+    private static boolean isInRange(int page) {
+        return minPage <= page && page <= maxPage;
+    }
+
+    private static boolean isOddNumber(int page) {
+        return page % 2 != 0;
+    }
+
+    private static boolean isEvenNumber(int page) {
+        return page % 2 == 0;
+    }
+
+    private static boolean notCheckPageOrder(int leftPage, int rightPage) {
+        return (rightPage - leftPage) != 1;
     }
 }
