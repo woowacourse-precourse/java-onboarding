114e0b9d1e5bc06dbe2b10f83276c626096b663f
Jeonsoyeon <itso22@naver.com>
2022-11-01 11:54:06 +0900

    feat(Problem7): 점수를 관리하는 Map 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
         return answer;
+    }
+    static HashMap<String, Integer> scoreMap = new HashMap<String, Integer>();
+
+    static void addScore(String name, int score) {
+        if(scoreMap.get(name)==null) {
+            scoreMap.put(name, score);
+        } else {
+            scoreMap.put(name, scoreMap.get(name)+score);
+        }
     }
 }
