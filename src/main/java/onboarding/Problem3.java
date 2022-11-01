71064612789a342854851bd0b4a8da4c97b99e64
herbssssst <zxx52539@kyonggi.ac.kr>
2022-11-01 17:38:35 +0900

    Changes

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
+
+        for(int i=1; i<=number; i++)
+        {
+            numberList.add(i);
+        }
+
+        for(int i=0; i<number; i++)
+        {
+            String numStr = numberList.get(i)+"";
+            for(int j=0; j<numStr.length(); j++)
+            {
+                if(numStr.charAt(j)=='3' || numStr.charAt(j)=='6' || numStr.charAt(j)=='9')
+                    count++;
+            }
+        }
+        return count;
     }
 }
