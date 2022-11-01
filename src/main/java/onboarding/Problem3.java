6faf700a1bb4b7c32b4bf1b3e7ecf12b3b2788ae
yewon <yewon.ux@gmail.com>
2022-11-01 19:56:08 +0900

    1부터 number까지 for문생성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,13 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
         /*
         기능 목록
         1. 1부터 숫자 number 까지 for문 생성
         2. 각각의 number에 3, 6, 9가 포함되어 있는 경우 해당 숫자 저장
         3. 저장된 숫자중 3, 6, 9 개수 계산
         */
+        int answer = 0;
+
+        for(int i=1; i<=number; i++) {
+            System.out.print(i + "\n");
+        }
         return answer;
     }
 }
