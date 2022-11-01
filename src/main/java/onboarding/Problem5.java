79e9a978e91331fa396c8e2f489cb109f47f2c7a
CJY-kor <vivo_reprise@naver.com>
2022-11-01 18:57:02 +0900

    FEAT(problem5) : i) getExchange 기능 구현 자세한 사항은 기능 목록 참고

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -13,6 +13,29 @@ import java.util.List;
 
 public class Problem5 {
 
+
+    ArrayList getExchange(int money){
+        ArrayList temp_array = new ArrayList();
+        int temp_int = money;
+        temp_array.add(temp_int/50000);
+        temp_int = money % 50000;
+        temp_array.add(temp_int/10000);
+        temp_int = money % 10000;
+        temp_array.add(temp_int/5000);
+        temp_int = money % 5000;
+        temp_array.add(temp_int/1000);
+        temp_int = money % 1000;
+        temp_array.add(temp_int/500);
+        temp_int = money % 500;
+        temp_array.add(temp_int/100);
+        temp_int = money % 100;
+        temp_array.add(temp_int/50);
+        temp_int = money % 50;
+        temp_array.add(temp_int/10);
+        return temp_array;
+    }
+
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
