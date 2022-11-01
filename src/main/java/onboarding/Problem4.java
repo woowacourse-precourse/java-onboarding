ca894edeb1a951882da361fcae97192891282dab
yunjeong <uu0078@naver.com>
2022-11-01 22:45:32 +0900

    Problem4 기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+/* 기능 목록
+1. word 의 각 문자 순회
+2. 소문자 변환
+3. 대문자 변환
+4. (알파벳 외의 문자는 변환 x 그대로) answer 에 추가
+ */
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
