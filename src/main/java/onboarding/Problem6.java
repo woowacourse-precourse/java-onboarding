add1cadb590b28d2eaa63d3110eed74c237c59ca
idoburnish <doyun7433@naver.com>
2022-11-01 17:21:52 +0900

    feat(PROBLEM6): 유효한 입력 형식인지 확인하는 기능
    1. 유효한 이메일 형식인지 확인하는 기능 구현
    2. 유효한 닉네임 형식인지 확인하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,26 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for (List<String> form : forms) {
+            System.out.println(isValidForm(form));
+        }
         return answer;
+    }
+
+    public static boolean isValidForm(List<String> form) {
+        if (!isValidEmailForm(form.get(0))) return false;
+        if (!isValidNicknameForm(form.get(1))) return false;
+        return true;
+    }
+
+    public static boolean isValidEmailForm(String email) {
+        String regex = "^[a-z_0-9]{1,9}(@email.com)$";
+        return email.matches(regex);
+    }
+
+    public static boolean isValidNicknameForm(String nickname) {
+        String regex = "^[ㄱ-ㅎㅏ-ㅣ가-힣]{1,19}$";
+        return nickname.matches(regex);
     }
 }
