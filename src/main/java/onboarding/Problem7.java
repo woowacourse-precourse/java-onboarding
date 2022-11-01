a5c835729c24a617e9b87b6766a1c6bcb8bf09c3
i960107 <i960107@naver.com>
2022-11-01 14:01:58 +0900

    feat(problem7): implement feature of get list of friends mapping to user
    implements methods
    - getUserToFriendMap
    - add Friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
 
 public class Problem7 {
     public static final int MAX_RECOMMEND_SIZE = 5;
@@ -11,6 +10,34 @@ public class Problem7 {
     public static final Comparator<Map.Entry<String, Integer>> byValDesc = Map.Entry.comparingByValue(Comparator.reverseOrder());
     public static final Comparator<Map.Entry<String, Integer>> byKeyAsc = Map.Entry.comparingByKey();
     public static final Comparator<Map.Entry<String, Integer>> sortConditions = byValDesc.thenComparing(byKeyAsc);
+
+    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        Map<String, Set<String>> userToFriends = getUserToFriendMap(friends);
+
+        return null;
+    }
+
+    public static Map<String, Set<String>> getUserToFriendMap(List<List<String>> friends) {
+        Map<String, Set<String>> userToFriends = new HashMap<>();
+
+        for (List<String> relation : friends) {
+            addFriend(userToFriends, relation.get(0), relation.get(1));
+        }
+
+
+        return userToFriends;
+    }
+
+    public static void addFriend(Map<String, Set<String>> userToFriends, String user1, String user2) {
+        userToFriends
+                .computeIfAbsent(user1, k -> new HashSet<>())
+                .add(user2);
+
+        userToFriends
+                .computeIfAbsent(user2, k -> new HashSet<>())
+                .add(user1);
+
+    }
 
 
 }
