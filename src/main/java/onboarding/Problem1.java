d337ccf584b3b55939472e0f31e9eb668b34f862
Heesom <heesom.hs@gmail.com>
2022-11-01 10:42:32 +0900

    Problem1 : getScore 수정
    곱셈시 0이란 예외 고려 못해 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,20 +3,21 @@ package onboarding;
 import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(exception(pobi, crong) == -1){
+        Problem1 q1 = new Problem1();
+        if(q1.exception(pobi, crong) == -1){
             return -1;
         }else{
-            return winnerLooser(getScore(pobi), getScore(crong));
+            return q1.winnerLooser(q1.getScore(pobi), q1.getScore(crong));
         }
     }
 
-    public static int winnerLooser(int pobi, int crong){
+    public int winnerLooser(int pobi, int crong){
         if(pobi>crong) { return 1;}
         else if(crong>pobi){ return 2;}
         else {return 0;}
     }
 
-    public static int getScore(List<Integer> one){
+    public int getScore(List<Integer> one){
         int[] leftrightArr = new int[2];
         for(int listIndex=0; listIndex<2; listIndex++){
             //각 숫자 더하기 빼기
@@ -25,6 +26,9 @@ class Problem1 {
                 int second = (one.get(listIndex)/10) %10;
                 int third = one.get(listIndex)%10;
                 int add = first+second+third;
+                if(first==0){first=1;}
+                else if(second==0){second=1;}
+                else if(third==0){third=1;}
                 int mul= first*second*third;
 
                 leftrightArr[listIndex] = Math.max(add, mul);
@@ -33,7 +37,7 @@ class Problem1 {
         return Math.max(leftrightArr[0], leftrightArr[1]);
     }
 
-    public static int exception(List<Integer> pobi, List<Integer> crong) {
+    public int exception(List<Integer> pobi, List<Integer> crong) {
         //페이지 연속
         int pageDiffpobi = Math.abs(pobi.get(0) - pobi.get(1));
         int pageDiffcrong = Math.abs(crong.get(0) - crong.get(1));
