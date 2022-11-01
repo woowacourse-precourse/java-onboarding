56d654386cf408bb95c02f63e234f2d330cbd0cf
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-01 22:28:00 +0900

    pr.7: 메소드 분리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,60 +4,66 @@ import java.util.*;
 import java.util.Comparator;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = new ArrayList<>();
-        HashMap<String, Integer> friendScore = new HashMap<>();
-        List<String> friendList = new ArrayList<>();
-        List<String> friendOfFriendList = new ArrayList<>();
+
+    private static HashSet<String> peopleList(String user, List<List<String>> friends, List<String> visitors) {
         HashSet<String> people = new HashSet<>();
-
-        //유저의 타임라인에 방문한 사람 목록을 전체 사람 목록에 put
+        //유저의 타임라인에 방문한 사람, 전체 친구목록 put
         people.addAll(visitors);
-
-        //유저의 친구 목록 계산, 전체 사람 목록에 중복되는 사람과 유저 본인 제거해서 계산
         for (List<String> list : friends) {
             people.addAll(list);
-            if (list.contains(user)) {
-                if (list.get(0) == user) {
-                    friendList.add(list.get(1));
-                } else friendList.add(list.get(0));
-            }
         }
-        people.remove(user);
+        return people;
+    }
 
-        //전체 사람 목록 set -> map
-        for (String name : people) {
-            friendScore.put(name, 0);
-        }
 
-        //다른 유저의 친구 관계에 유저의 친구가 포함되어 있다면 점수 +10
+    //유저의 친구 목록
+    private static List<String> makeFriendList(String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
         for (List<String> list : friends) {
-            if (list.contains(user))
-                continue;
-            //전체 친구 관계를 돌아보며 유저의 친구가 포함되어 있는지 확인
-            for (String name : friendList) {
-                if (list.contains(name)) {
-                    String friendOfFriend;
-                    if (friendList.contains(list.get(0))) {
-                        friendOfFriend = list.get(1);
-                    } else {
-                        friendOfFriend = list.get(0);
-                    }
-                    int score = friendScore.get(friendOfFriend);
-                    score += 10;
-                    friendScore.put(friendOfFriend, score);
-                }
+            if (list.contains(user)) {
+                String friend = anotherPerson(list, user);
+                friendList.add(friend);
             }
         }
+        return friendList;
+    }
 
-        //visitor 점수 계산
+    private static String anotherPerson(List<String> list, String person) {
+        String another;
+        if (list.get(0) == person) {
+            another = list.get(1);
+        } else {
+            another = list.get(0);
+        }
+        return another;
+    }
+
+    //친구들의 친구 목록에 있는 유저들의 점수 +10
+    private static HashMap<String, Integer> friendOfFriendScore(List<String> friendOfFriendList,
+                                                                HashMap<String, Integer> friendScore) {
+        for (String friendOfFriend : friendOfFriendList) {
+            int score = friendScore.get(friendOfFriend);
+            score += 10;
+            friendScore.put(friendOfFriend, score);
+        }
+        return friendScore;
+    }
+
+    //visitor 점수 +1
+    private static HashMap<String, Integer> visitorScore(List<String> visitors,
+                                                         HashMap<String, Integer> friendScore) {
         for (String visitor : visitors) {
             int score = friendScore.get(visitor);
             score += 1;
             friendScore.put(visitor, score);
         }
+        return friendScore;
+    }
 
-        //이름으로 정렬 -> 점수로 정렬 후 유저와 친구가 아닌 사람만 answer에 추가.
+    //이름으로 정렬 -> 점수로 정렬 후 유저와 친구가 아닌 사람만 answer에 추가.
+    private static List<String> sortAnswer(HashMap<String, Integer> friendScore,
+                                           List<String> friendList) {
+        List<String> answer = new ArrayList<>();
         List<Map.Entry<String, Integer>> entryList = new LinkedList<>(friendScore.entrySet());
         entryList.sort(Map.Entry.comparingByKey());
         entryList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
@@ -69,6 +75,33 @@ public class Problem7 {
         if (answer.size() > 5)
             answer = answer.subList(0, 4);
 
+        return answer;
+    }
+
+    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> friendScore = new HashMap<>();
+
+        HashSet<String> people = peopleList(user, friends, visitors);
+        List<String> friendList = makeFriendList(user, friends);
+
+        //전체 사람 목록 set -> map
+        for (String name : people) {
+            friendScore.put(name, 0);
+        }
+
+        //다른 유저의 친구 관계에 유저의 친구가 포함되어 있다면 점수 +10
+        for (String name : friendList) {
+            List<String> friendOfFriendList = makeFriendList(name, friends);
+            friendScore = friendOfFriendScore(friendOfFriendList, friendScore);
+        }
+
+        //visitor 점수 계산
+        friendScore = visitorScore(visitors, friendScore);
+
+        friendScore.remove(user);
+
+        answer = sortAnswer(friendScore, friendList);
         return answer;
     }
 }
