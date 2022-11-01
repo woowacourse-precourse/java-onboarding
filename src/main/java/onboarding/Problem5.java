c31cb6962f0f300677c67a145bf261cab83871d7
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-01 09:24:59 +0900

    feat: P5 가장 큰 금액부터 나눠서 개수 세어주는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,7 +11,13 @@ public class Problem5 {
     private static final int[] MONEY = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < MONEY.length; i++) {
+            int count = money / MONEY[i];
+            money %= MONEY[i];
+            answer.add(count);
+        }
 
         return answer;
     }
