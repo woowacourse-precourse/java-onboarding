9512c9004d2099ca70938b6fbcae88568380a05f
taehyun <didls1643@gmail.com>
2022-11-01 16:37:07 +0900

    6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,7 +7,7 @@ import java.util.TreeMap;
 
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         
         String s;
         String ss;
