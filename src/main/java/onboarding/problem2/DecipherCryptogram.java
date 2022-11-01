9ba70b5fed841331f16c70b099a08e141c9ce0aa
seominseok00 <seominseok4834@gmail.com>
2022-11-01 22:58:48 +0900

    feat(문제2): 암호 해독 클래스 추가

---
+++ src/main/java/onboarding/problem2/DecipherCryptogram.java
@@ -0,0 +1,9 @@
+package onboarding.problem2;
+
+public class DecipherCryptogram {
+    private String cryptogram;
+    
+    public DecipherCryptogram(String cryptogram){
+        this.cryptogram = cryptogram;
+    }
+}
