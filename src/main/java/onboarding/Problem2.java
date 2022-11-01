dd7b9a8428325742e451ce2c73fe1dab8953881c
SproutMJ <mjk54565@gmail.com>
2022-11-01 20:47:26 +0900

    feat: 문제 2 구현 완료 bug: 기능 1, 2 버그 해결 style: 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,28 +2,50 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        while (!isUnique(answer)) {
+            System.out.println(answer);
+            answer = forUnique(answer);
+        }
         return answer;
     }
 
+    //문자열에 중복이 포함되는지 확인하는 기능
     static boolean isUnique(String str){
-        if(str.length() == 1)return true;
+        if(str.length() == 1 || str.length() == 0)return true;
         char before = str.charAt(0);
         for (int i = 1; i<str.length(); i++){
             if(before == str.charAt(i)) return false;
+            before = str.charAt(i);
         }
         return true;
     }
 
+    //중복 문자열을 삭제하는 기능
     static String forUnique(String str) {
         StringBuffer strBuf = new StringBuffer();
         char before = str.charAt(0);
+        boolean flag = false;
+        strBuf.append(before);
         for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) != before) {
-                strBuf.append(before);
-                before = str.charAt(i);
+            if(before == str.charAt(i)){
+                flag = true;
+                continue;
+            }
+            if(before != str.charAt(i)){
+                if(flag){
+                    flag = false;
+                    before = str.charAt(i);
+                    strBuf.deleteCharAt(strBuf.length() - 1);
+                    strBuf.append(before);
+                }
+                else{
+                    before = str.charAt(i);
+                    strBuf.append(before);
+                }
             }
         }
+        if(flag) strBuf.deleteCharAt(strBuf.length() - 1);
         return strBuf.toString();
     }
 
