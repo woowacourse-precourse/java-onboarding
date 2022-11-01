49fde446474113fe9149faf1b890187de0642be1
gomljo <gomljo0307@naver.com>
2022-11-01 20:30:06 +0900

    feat(Problem7.java): getTotalScore 함수 구현
    추천 점수 총합 반환하는 getTotalScore 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -47,6 +47,9 @@ class User{
     void updateAcquaintanceScore(){
         totalScore+=ACQUAINTANCE_SCORE;
     }
+    int getTotalScore(){
+        return totalScore;
+    }
 }
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
