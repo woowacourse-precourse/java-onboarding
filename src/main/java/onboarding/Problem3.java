1fec42fc320673e1304649d3c3b6dd23182a5a22
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-01 15:03:57 +0900

    feat:for문을 이용하여 369게임의 박수 횟수 도출

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int a = i / 10;
+            int b = i % 10;
+
+            if (a == 3 || a == 6 || a == 9)
+                answer++;
+
+            if (b == 3 || b == 6 || b == 9)
+                answer++;
+        }
+        
         return answer;
     }
 }
