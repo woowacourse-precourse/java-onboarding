38144320a37cf75249c42b7e32b413b690cfb277
Lee-SungMin <saevers26@gmail.com>
2022-11-01 20:34:22 +0900

    Add: 4번 문제 기능 구현 - 대문자인 경우 변환하는 함수 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+
+    // 대문자일 경우
+    public static Integer changeUpper(Character word){
+        return 'Z' - word + 'A';
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
