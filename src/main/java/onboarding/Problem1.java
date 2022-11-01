781ce3be2ccffe4cbae50c07865cbf3faecc2e51
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-01 14:23:54 +0900

    test1 - 페이지 예외사항 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,11 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 페이지 예외사항 처리
+        if(pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1)
+            return answer = -1;
+
         return answer;
     }
 }
