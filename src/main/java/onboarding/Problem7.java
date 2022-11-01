0e57019b752d7775adea6d19c816506120a3b087
yun8565 <yunyun8565@naver.com>
2022-11-01 14:13:33 +0900

    feat(P7): 점수 기준 정렬 기능 추가
    - Comparator를 직접 만들어 정렬을 할 때 점수 기준 내림차순으로 정렬되도록 하고, 점수가 같을 때는 이름 순으로 정렬되도록 만들었다.
    - 정렬 후 점수가 0 이상인 아이디를 추천 목록에 추가했다.
    - 최대 5명의 추천 친구를 반환한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,8 +3,16 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
+    public static final int MAX_RECOMMENDATION = 5;
     public static HashMap<String, Integer> friendScore;
     public static List<String> answer;
+
+    public static Comparator<? super Map.Entry<String, Integer>> customComparator = (Comparator<Map.Entry<String, Integer>>) (a, b) -> {
+        if (a.getValue().equals(b.getValue()))
+            return a.getKey().compareTo(b.getKey());
+        else
+            return b.getValue().compareTo(a.getValue());
+    };
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         answer = new ArrayList<>();
@@ -12,8 +20,9 @@ public class Problem7 {
 
         countScoreByFriend(user, friends);
         countScoreByVisit(visitors);
+        sortByScore();
 
-        return answer;
+        return answer.size() > MAX_RECOMMENDATION ? answer.subList(0,MAX_RECOMMENDATION) : answer;
     }
 
     public static void countScoreByFriend(String user, List<List<String>> friends) {
@@ -34,6 +43,15 @@ public class Problem7 {
                 friendScore.put(visitor, friendScore.getOrDefault(visitor, 0) + 1);
             }
         }
+    }
+
+    public static void sortByScore() {
+        friendScore.entrySet().stream()
+                .sorted(customComparator)
+                .forEach(entry -> {
+                    if(entry.getValue() > 0)
+                        answer.add(entry.getKey());
+                });
     }
 
     public static boolean isNew(String id) {
