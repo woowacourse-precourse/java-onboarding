9a1c844e70f5aa253308b2f9bad953c81f49eb96
Dasol Jung <greteljds0251@gmail.com>
2022-11-01 19:20:04 +0900

    feat: 6-1. Divide a nickname into two letters

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        /*
+        1. 1번 지원자의 닉네임부터 2글자씩 분리
+        2. 2~n번 지원자의 닉네임에 포함되는지 검사
+        3. 포함 되는 지원자 이메일 list에 저장
+        4. 다음 번호 글자 분리하면서 1~3 반복
+        5. list email 오름차순으로 정렬, 중복은 제거
+         */
+
+        List<String> email = new ArrayList<>();
+        String[] name = new String[20];
+
+        for (int i = 0; i < forms.size(); i++) {
+            name = nameCut(forms.get(i).get(i));
+        }
+
+        return email;
+    }
+    // 글자 분리하는 메서드
+    public static String[] nameCut(String name) {
+        String[] cutname = new String[20];
+        for (int i = 0; i < name.length(); i++) {
+            cutname[i] = name.substring(i, (i + 2));
+        }
+        return cutname;
     }
 }
