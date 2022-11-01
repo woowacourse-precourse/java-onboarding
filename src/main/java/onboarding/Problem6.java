4cc468b898133d4a56be1a65890c2ca6efd72e0a
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-01 18:40:31 +0900

    problem6: TODO-'기능1' complete

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,15 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+    private static List<String> sortHashSetAsList(HashSet<String> hashSet){
+        return hashSet.stream().sorted().collect(Collectors.toList());
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        return null;
     }
 }
