886e0638abb84e7ffa6a010fe343d9cd9c59fd89
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-11-01 17:44:59 +0900

    solve: Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++){
+            int divider = 10;
+            int tmp = i;
+            while(tmp != 0){
+                int a = tmp % divider;
+                if(a == 3 || a == 6 || a == 9){
+                    answer++;
+                }
+                tmp /= divider;
+            }
+        }
+
         return answer;
     }
 }
