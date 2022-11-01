696f204c9b73340a3c4cf4e6271a4d8182bd229f
yejin130 <jinis2023@gmail.com>
2022-11-01 14:11:15 +0900

    Second commit : problem1, exception value -1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = -1;
 
 
         if(pobi.get(0) %2==1 && pobi.get(1) - pobi.get(0) ==1 && crong.get(0) %2==1 && crong.get(1) - crong.get(0) ==1 && pobi.get(0) !=1 && pobi.get(0) !=399 && crong.get(0) !=1 && crong.get(0) !=399) {
