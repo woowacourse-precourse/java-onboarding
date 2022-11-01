1b28f1b3b88331c8f81835d016155dfed4f26037
kimdoha <kkhhjj3393@gmail.com>
2022-11-01 21:37:22 +0900

    refactor: Cryptogram 클래스 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,49 +1,17 @@
 package onboarding;
 
+import onboarding.problem2.Cryptogram;
+
 public class Problem2 {
-
-    private static String removeDuplicateCharacters(String str) {
-        char[] chars = str.toCharArray();
-        char previousChar = chars[0];
-
-        StringBuilder results = new StringBuilder();
-        results.append(previousChar);
-
-        for(int i = 1; i < chars.length; i++) {
-            char currentChar = chars[i];
-            if(previousChar != currentChar) {
-
-                results.append(currentChar);
-                previousChar = currentChar;
-            } else {
-
-                results.deleteCharAt(results.length() - 1);
-            }
-        }
-
-        return results.toString();
-    }
-
-    private static boolean checkDuplicateCharacters(String str) {
-
-        char[] chars = str.toCharArray();
-        char previousChar = chars[0];
-
-        for(int i = 1; i < chars.length; i++) {
-            char currentChar = chars[i];
-
-            if(previousChar == currentChar) return true;
-            else previousChar = currentChar;
-        }
-
-        return false;
-    }
 
     public static String solution(String cryptogram) {
 
-        while(cryptogram.length() > 0 && checkDuplicateCharacters(cryptogram)){
+        while(cryptogram.length() > 0){
+            String decodeResult = Cryptogram.removeDuplicateCharacters(cryptogram);
+            if(decodeResult.equals(cryptogram))
+                break;
 
-            cryptogram = removeDuplicateCharacters(cryptogram);
+            cryptogram = decodeResult;
         }
 
         return cryptogram;
