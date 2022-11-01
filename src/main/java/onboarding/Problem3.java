6bcf3bbb48adf632249f2a2a39f1865c3ac5476b
aran <aran0317@naver.com>
2022-11-01 21:09:57 +0900

    문제 3번 기능 요구사항

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/*기능 요구 사항*/
+/* 1. 3,6,9가 나올 때 3,6,9의 개수만큼 손뼉을 쳐야한다.
+ * 2. 1부터 number 까지 손뼉을 몇번 쳐야하는지 Return
+ <solution>
+ *  - list에 3,6,9를 넣고, num%10이 포함되면 answer를 하나씩 더해준다.
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
