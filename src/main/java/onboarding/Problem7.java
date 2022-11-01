78ac07b2ee18877b77f6507808f888341648e075
= <kimje1109@gmail.com>
2022-11-01 19:35:52 +0900

    feat: Add findCandidates

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static Map<String, Integer> findCandidates(String user, List<List<String>> friendRelation, List<String> visitors) {
+        Map<String, Integer> candidates = new HashMap<>();
+
+        for (List<String> friends : friendRelation) {
+            for (String friend : friends) {
+                if (!candidates.containsKey(friend)) {
+                    candidates.put(friend, 0);
+                }
+            }
+        }
+        for (String visitor : visitors) {
+            if (!candidates.containsKey(visitor)) {
+                candidates.put(visitor, 0);
+            }
+        }
+        for (List<String> friends : friendRelation) {
+            for (String friend : friends) {
+                if (friends.contains(user) && !friend.equals(user)) {
+                    candidates.remove(friend);
+                }
+            }
+        }
+        candidates.remove(user);
+
+        return candidates;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
