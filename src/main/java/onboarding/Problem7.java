e47e18b8fb7c30da297f48bdb7bec69da2d860b0
Toa <ld980105@snu.ac.kr>
2022-11-01 15:25:53 +0900

    complete solution method of problem 7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,7 +4,50 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        Map<String, Set<String>> usersFriends = getUsersFriends(friends);
+        Set<String> targetUserFriends = usersFriends.get(user);
+        Map<String, Integer> userScore = new HashMap<>();
+        for (String userName : usersFriends.keySet()){
+            if (!Objects.equals(userName, user)){
+                Set<String> targetUserFriendsCloned = new HashSet<>(targetUserFriends);
+                Set<String> currUserFriends = usersFriends.get(userName);
+                targetUserFriendsCloned.retainAll(currUserFriends);
+                userScore.put(userName, 10 * targetUserFriendsCloned.size());
+            }
+        }
+        for (String visitor : visitors){
+            if (userScore.containsKey(visitor)){
+                userScore.put(visitor, userScore.get(visitor) + 1);
+            }
+            else {
+                userScore.put(visitor, 1);
+            }
+        }
+        List<Map.Entry<String, Integer>> userScoreEntries = new ArrayList<>(userScore.entrySet());
+        Collections.sort(userScoreEntries, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o2.getValue() > o1.getValue())
+                    return 1;
+                else if (o2.getValue() < o1.getValue())
+                    return -1;
+                else {
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+            }
+        });
+        List<String> answer = new ArrayList<>();
+        for (Map.Entry<String, Integer> userScoreEntry : userScoreEntries){
+            int score = userScoreEntry.getValue();
+            String userName = userScoreEntry.getKey();
+            if (answer.size() == 5)
+                break;
+            if (score > 0 && !targetUserFriends.contains(userName)){
+                answer.add(userName);
+            }
+            else if (score <= 0)
+                break ;
+        }
         return answer;
     }
 
