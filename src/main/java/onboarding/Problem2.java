eee2fdc212ca211450ddeddcdd4196e2e0005312
seheonCho <whtpgjs94@gmail.com>
2022-11-01 20:28:10 +0900

    feat: [문제2] 각 글자와, 이전 문자가 연속되는지 기록

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,9 +8,13 @@ public class Problem2 {
 
     private static String decrypt(String cryptogram) {
         char[] cryptogramChars = cryptogram.toCharArray();
+        boolean[] isConsecutive = new boolean[cryptogram.length()];
 
         for (int i = 1; i < cryptogramChars.length; i++) {
-
+            if (cryptogramChars[i] == cryptogramChars[i - 1]) {
+                isConsecutive[i] = true;
+                isConsecutive[i - 1] = true;
+            }
         }
 
         return cryptogram;
