53eeb4b9fef80998b8b0dc03847b8ccb21d026f8
la1av1a <zldqkrktqk@gmail.com>
2022-11-01 16:46:13 +0900

    feat : 각 유저의 덧셈 곱셈중 큰 값 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,6 +11,9 @@ class Problem1 {
             return -1;
         }
 
+        int pobiValue = compareSumWithMultiplyValue(pobi);
+        int crongSum = compareSumWithMultiplyValue(crong);
+
         return 0;
     }
 
@@ -26,4 +29,64 @@ class Problem1 {
         throw new IllegalArgumentException();
     }
 
+    public static int compareSumWithMultiplyValue(List<Integer> user){
+
+        int leftPage = user.get(0);
+        int rightPage = user.get(1);
+
+        int sumValue = sumAndCompareLeftRight(user,leftPage,rightPage);
+        int multiplyValue = multiplyAndCompareLeftRight(user,leftPage,rightPage);
+
+        return returnBiggerValue(sumValue,multiplyValue);
+    }
+
+    public static int sumAndCompareLeftRight(List<Integer> user, int leftPage, int rightPage){
+
+        int leftPageNumSum = sum(leftPage);
+        int rightPageNumSum = sum(rightPage);
+
+        return returnBiggerValue(leftPageNumSum,rightPageNumSum);
+    }
+
+    public static int multiplyAndCompareLeftRight(List<Integer> user, int leftPage, int rightPage){
+        int leftPageNumSum = multiply(leftPage);
+        int rightPageNumSum = multiply(rightPage);
+
+        return returnBiggerValue(leftPageNumSum,rightPageNumSum);
+    }
+
+    //각 자리 숫자를 더하기
+    public static int sum(int page) {
+
+        int result = 0;
+
+        while (page > 10) {
+            int mod = page % 10;
+            page /= 10;
+            result += mod;
+        }
+        result += page;
+
+        return result;
+    }
+
+    //각 자리 숫자를 곱하기
+    public static int multiply(int page){
+
+        int result = 1;
+
+        while (page > 10){
+            int mod = page % 10;
+            page /= 10;
+            result *= mod;
+        }
+        result *= page;
+
+        return result;
+    }
+
+    //둘 중 큰 수를 반환
+    public static int returnBiggerValue(int value1, int value2){
+        return Math.max(value1,value2);
+    }
 }
