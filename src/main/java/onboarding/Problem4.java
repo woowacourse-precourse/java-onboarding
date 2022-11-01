d9386bc1495c836a83608f425886a504f9fe7a09
jwj <real980312@gmail.com>
2022-11-01 23:03:49 +0900

    기능들 분할중

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        char[] rev = word.toCharArray();
+        int a, b, re =0;
         String answer = "";
+
+        for(int i = 0; i < rev.length; i++) {
+            if(Character.isUpperCase(rev[i])){
+                a =65;
+            } else if (Character.isLowerCase(rev[i])) {
+                a = 97;
+            }else{
+                answer=answer + ' ';
+                continue;
+            }
+            b = Math.abs(a - rev[i]);
+            re = (a + 25) - b;
+            answer = answer + String.valueOf((char) re);
+        }
+
         return answer;
     }
 }
