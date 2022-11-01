dbf2ba4d53ea777f8770b33b7ac62d2ecf65d81a
unknown <dragontaek98@naver.com>
2022-11-01 20:34:09 +0900

    feat: problem6 모든 닉네임의 두글자 조합 경우의 수 구하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> nameHashedSet = new ArrayList<>();
+        HashSet<String> nameDuplicated = new HashSet<>();
+        List<String> answer = new ArrayList<>();
+
+        //get all possible name set
+        for(int j = 0; j < forms.size(); j++){
+            String ch[] = forms.get(j).get(1).split("");
+            for(int l = 0; l < ch.length; l++){
+                if(l != ch.length-1){
+                    String combined = ch[l] + ch[l+1];
+                    nameHashedSet.add(combined);
+                }
+            }
+        }
+
         return answer;
     }
 }
