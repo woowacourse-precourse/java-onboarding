1f443385a2b443e3288543a12bb7eb8b1a528a02
wlwlwon <ryanwon@naver.com>
2022-11-01 20:28:32 +0900

    Problem2 메서드 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem2 {
+    public static void main(String[] args) {
+        System.out.println(solution("browoanoommnaon"));
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder stringBuilder = new StringBuilder(cryptogram);
+        boolean flag = true;
+        while(flag){
+            flag = false;
+            for (int i =1;i< stringBuilder.length();i++){
+                if(stringBuilder.charAt(i)== stringBuilder.charAt(i-1)){
+                    stringBuilder.replace(i-1,i+1,"");
+                    flag = true;
+                    i--;
+                }
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
