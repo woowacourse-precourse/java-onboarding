373a82effe307a63c7709d273d4fde7da7be3d4d
dener8 <8dener@gmail.com>
2022-11-01 15:59:14 +0900

    [Problem6] feat: hashmap에 이미 있는 부분 문자열인지 확인

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,12 +11,24 @@ public class Problem6 {
     static List<String> searchDuplicate(List<List<String>> forms) {
         HashMap<String, Integer> allNicknameElements = new HashMap<String, Integer>();
         String nickname;
+        HashSet<String> nicknameElements;
         for (int i = 0; i < forms.size(); i++) {
-            HashSet<String> nicknameElements = new HashSet<>();
+            nicknameElements = new HashSet<>();
             nickname = forms.get(i).get(1);
             for (int j = 0; j < nickname.length() - 1; j++) {
                 nicknameElements.add(nickname.substring(i, i+2));
             }
+            // hashmap에 이미 key값이 있으면 +=1, 없으면 추가하고 value값 1로 지정
+            for (String nicknameElement : nicknameElements) {
+                if (allNicknameElements.containsKey(nicknameElement)) {
+                    allNicknameElements.put(nicknameElement,allNicknameElements.get(nicknameElement) + 1);
+                } else {
+                    allNicknameElements.put(nicknameElement, 1);
+                }
+            }
+
+
+
         }
     }
 }
