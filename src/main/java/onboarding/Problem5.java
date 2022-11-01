17f457a48980a7613704fb4800441e732a822c7a
ye1ne <yewon7036@naver.com>
2022-11-01 17:10:09 +0900

    feature : 문제를 풀기 위한 메인 메소드작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -11,7 +11,10 @@ public class Problem5 {
     static final int[] MONEY_UNIT = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getMoneyList(money);
+    }
+
+    private static List<Integer> getMoneyList(int money) {
+        return null;
     }
 }
