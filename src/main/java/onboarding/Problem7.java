3ed3ff70848244956d3cf79626d85f08400458b1
kimJaeHyeon <kmr2644@gmail.com>
2022-11-01 19:00:55 +0900

    feat: implement method for createPointMap

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        Map<String, List<String>> relationMap = createRelationMap(getUserFriendList(user, friends), friends);
+        Map<String, List<String>> relationMap = createRelationMap(friends);
+        Map<String, Integer> pointMap = createPointMap(user, relationMap);
+        System.out.println(relationMap);
+        System.out.println(pointMap);
         return answer;
     }
 
@@ -23,12 +26,35 @@ public class Problem7 {
         for (List<String> userRelations : friends) {
             String user1 = userRelations.get(0);
             String user2 = userRelations.get(1);
-            addRelation(relationMap,user1);
+            addRelation(relationMap, user1);
             addRelation(relationMap, user2);
             setRelation(relationMap, user1, user2);
         }
-        System.out.println(relationMap);
         return relationMap;
+    }
+
+    public static Map<String, Integer> createPointMap(String user, Map<String, List<String>> relationMap) {
+        List<String> userFriendList = relationMap.get(user);
+        return relationMap.entrySet()
+                          .stream()
+                          .filter(entry -> !entry.getKey()
+                                                 .equals(user) && hasAnyUserFriend(userFriendList, entry.getValue()))
+                          .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+                              List<String> relationList = entry.getValue();
+                              return countMatchesFriend(userFriendList, relationList) * 10;
+                          }, Integer::sum));
+    }
+
+    private static boolean hasAnyUserFriend(List<String> userFriendList, List<String> userRelationList) {
+        return userRelationList.stream()
+                               .anyMatch(userFriendList::contains);
+    }
+
+    private static Integer countMatchesFriend(List<String> userFriendList, List<String> userRelationList) {
+        long count = userRelationList.stream()
+                                     .filter(userFriendList::contains)
+                                     .count();
+        return (int) count;
     }
 
     private static void setRelation(Map<String, List<String>> relationMap, String user1, String user2) {
@@ -37,6 +63,7 @@ public class Problem7 {
         relationMap.get(user2)
                    .add(user1);
     }
+
     private static void addRelation(Map<String, List<String>> relationMap, String user) {
         if (relationMap.containsKey(user)) {
             return;
