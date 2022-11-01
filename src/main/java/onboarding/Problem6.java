1d7f67c1c567705d20a29f482bd910811b789e19
la1av1a <zldqkrktqk@gmail.com>
2022-11-01 20:14:29 +0900

    feat : 유저들을 관리할 배열 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    public static boolean[] isOverlap;
+    public static List<String>[] userForms;
+    public static List<String> answer;
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        answer = new ArrayList<>();
+        isOverlap = new boolean[forms.size()];
+        userForms = new List[forms.size()];
+        setUserForms(forms);
+
         return answer;
+    }
+    public static void setUserForms(List<List<String>> forms){
+        for(int i=0;i<forms.size();i++){
+            userForms[i] = forms.get(i);
+        }
     }
 }
