0e8bc57b78d1e3fb6c71c672d273ed9b25d12bdd
younghoon211@naver.com <younghoon211@naver.com>
2022-11-01 21:06:31 +0900

    feat(Problem3): 3, 6, 9 포함 시 카운트 로직 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,4 +14,16 @@ public class Problem3 {
 
         return numCount(numList, count);
     }
+
+    private static int numCount(List<Integer> numList, int count) {
+        for (int i = 0; i < numList.size(); i++) {
+            for (int j = 0; j < String.valueOf(numList.get(i)).length(); j++) {
+                if (String.valueOf(numList.get(i)).charAt(j) == '3') count++;
+                if (String.valueOf(numList.get(i)).charAt(j) == '6') count++;
+                if (String.valueOf(numList.get(i)).charAt(j) == '9') count++;
+            }
+        }
+
+        return count;
+    }
 }
