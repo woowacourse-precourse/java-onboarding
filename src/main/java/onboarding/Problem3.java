d2398fe4a525564c52781943ec32d940d2a054cb
devEdok13 <dev.edok13@gmail.com>
2022-11-01 16:11:02 +0900

    feat: P03-checkClap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,12 @@ public class Problem3 {
         return answer;
     }
 
+    //  입력된 한자리 숫자가 3,6,9에 해당하는 확인하는 메소드
+    private static int checkClap(int number){
+        if(number % 3 == 0) return 1;
+        return 0;
+    }
+
     //  입력된 자리수의 10의 거듭제곱을 구하는 메소드
     private static int tenPowDigit(int number){
         return (int) Math.pow(10, countDigit(number));
