b956cecc0f8142750871555897962152f45aea1c
glaehfdl1654 <glaehfdl1654@naver.com>
2022-11-01 21:19:27 +0900

    [Problem7] declare Friend class

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,3 +9,33 @@ public class Problem7 {
         return answer;
     }
 }
+class Friend implements Comparable<Friend> {
+    private String name;
+    private int score;
+
+    public Friend (String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getScore() {
+        return this.score;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    @Override
+    public int compareTo(Friend f) {
+        if (this.score < f.getScore()) {
+            return 1;
+        } else if (this.score > f.getScore()) {
+            return -1;
+        }
+        return this.name.compareTo(f.getName());
+    }
+}
