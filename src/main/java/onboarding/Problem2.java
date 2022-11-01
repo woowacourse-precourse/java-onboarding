7ea6c3049fd2d46f3fa35253a11abd0777606b69
gitFILO <x1xgudwls@naver.com>
2022-11-01 22:43:55 +0900

    [Problem 2] 같은 문자를 삭제하는 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String answer = "";
+        int[] removeFlag = new int[cryptogram.length()];
+        int before = 0;
+
+        for (int cur = 1; cur < cryptogram.length(); cur++) {
+            before = cur - 1;
+
+            while (cryptogram.charAt(before) == cryptogram.charAt(cur) && removeFlag[before] == 0 && removeFlag[cur] == 0) {
+                removeFlag[before] = 1;
+                removeFlag[cur] = 1;
+                while (before > 0 && removeFlag[before] == 1) before--;
+                while (cur < cryptogram.length() - 1 && removeFlag[cur] == 1) cur++;
+            }
+        }
+
     }
 }
+
