ee2a6c78d9ca815baffb40b8eb8e6b025d290709
taekwon-dev <taekwon_dev@naver.com>
2022-11-01 13:43:38 +0900

    problem 7 - refactor: 사용 패키지만 import 하도록 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,12 @@
 package onboarding;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
 
 public class Problem7 {
     private static Map<String, List<String>> map = new HashMap<>();
