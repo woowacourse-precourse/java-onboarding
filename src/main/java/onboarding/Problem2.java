b08b4f7be1831f61c71ef4cd646c231457224e86
dingding-21 <phj8221@naver.com>
2022-11-01 22:44:42 +0900

    feat: Problem2 암호문의 연속하는 중복 문자들을 제거하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,21 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static String getDecryptedWord(String cryptogram) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (i != 0 && cryptogram.charAt(i - 1) == cryptogram.charAt(i)) {
+                continue;
+            }
+            else if (i != cryptogram.length() - 1 && cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                continue;
+            }
+
+            result.append(cryptogram.charAt(i));
+        }
+
+        return result.toString();
+    }
 }
