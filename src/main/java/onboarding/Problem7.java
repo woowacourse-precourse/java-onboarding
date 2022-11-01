2c525c694fc721a37720d1801cff6a46edd7d881
Lee-SungMin <saevers26@gmail.com>
2022-11-01 21:17:41 +0900

    Add: 7번 문제 기능 구현 - user와 친구이면서, 사용자와 함께 아는 친구 수(10점 점수) 구하는 기능 구현 - user와 이미 친구 사이는 포함하면 안되도록 하는 기능 - 이미 저장되어 있을 경우, 기존 점수에 더하도록 하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -28,6 +28,46 @@ public class Problem7 {
             }
         }
 
+        // user 와 친구인 사람들 중에서,
+        // 사용자와 함께 아는 친구 수 점수 계산
+        for(int i = 0; i < user_friend_list.size(); i++){
+
+            String name = user_friend_list.get(i);
+            // System.out.println(name);
+
+            // 추천해 줄 친구가 있는 지 확인
+            for(int j = 0; j < friends.size(); j++){
+                List<String> temp = friends.get(j);
+
+                String A = temp.get(0);
+                String B = temp.get(1);
+
+                // 단, user 및 이미 친구사이는 포함해서는 안된다.
+                if(A.equals(name) && !B.equals(user) && !user_friend_list.contains(B)){
+
+                    // 이미 map 에 있다면, 기존 점수에 + 10
+                    if(map.containsKey(B)){
+                        map.put(B, map.get(B) + 10);
+                    }
+
+                    else{
+                        map.put(B, 10);
+                    }
+
+                }
+                else if(B.equals(name) && !A.equals(user) && !user_friend_list.contains(A)){
+
+                    if(map.containsKey(A)){
+                        map.put(A, map.get(A) + 10);
+                    }
+                    else{
+                        map.put(A, 10);
+                    }
+                }
+            }
+
+        }
+
         return answer;
     }
 }
