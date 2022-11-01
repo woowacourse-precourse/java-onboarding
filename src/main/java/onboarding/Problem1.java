e7705db9792cc2bea5039d84eee5a7f45a6cf4a4
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 17:16:26 +0900

    주어진 페이지가 짝수이면 true를 홀수이면 false를 리턴한다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,9 @@ class Problem1 {
     private static boolean isInRange(int page) {
         return page >= 3 && page <= 398;
     }
+    private static boolean isEven(int page) {
+        return page % 2 == 0;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
