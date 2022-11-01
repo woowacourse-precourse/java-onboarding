63ccdf04a3b87e6ca120cb3ab2ea5723c2098fed
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:30:37 +0900

    Problem3 solution method
    Iterates through each number in given range and counts the occurrences of 3, 6, and 9.
    temp.length() is the original length of the number and after temp.replaceAll("[369]", "").length() is the length after 3, 6, and 9 is removed.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,12 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        String temp;
+        for (int i = 1; i <= number; i++) {
+            temp = Integer.toString(i);
+            count += temp.length() - temp.replaceAll("[369]", "").length();
+        }
+        return count;
     }
 }
