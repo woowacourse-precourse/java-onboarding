f5ed0c9d55bb9366f732d0c9029febad8e6063a4
Luna,J <syt06119@naver.com>
2022-11-01 17:58:35 +0900

    Feat: Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+    public static int BaeminClap(int clap) {
+        int count = 0;
+        while(clap !=0){
+            if(clap%10%3 ==0 && clap%10 != 0) {
+                count++;
+            }
+            clap /= 10;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            answer += BaeminClap(i);
+        }
         return answer;
     }
 }
