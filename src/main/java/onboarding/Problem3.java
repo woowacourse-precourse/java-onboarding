634ce9cf34f9336cb7d9b8571ec64d0a5431686d
gardenee <snnjjang@naver.com>
2022-11-01 15:43:08 +0900

    fix - 배열 범위 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,7 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int[] samYookGoo = new int[10001];
+        int[] samYookGoo = new int[number+1];
 
         for (int i = 1; i < samYookGoo.length; i++) {
             samYookGoo[i] = samYookGoo[i-1];
@@ -10,9 +10,7 @@ public class Problem3 {
 
             while (num > 0) {
                 int mod = num % 10;
-                if (mod != 0 && mod % 3 == 0) {
-                    samYookGoo[i] += 1;
-                }
+                if (mod != 0 && mod % 3 == 0) samYookGoo[i] += 1;
                 num /= 10;
             }
         }
