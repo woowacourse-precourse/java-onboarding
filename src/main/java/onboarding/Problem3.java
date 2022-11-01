793489358fe2a37716cd811d0d1b381e7fbdf45c
LeeMyeongJin <dlaudwls1203@naver.com>
2022-11-01 20:13:36 +0900

    sol 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,7 +7,7 @@ public class Problem3 {
         for (int i = 1; i <= number; i++) {
             int current = i;
             while (current != 0) {
-                if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9){
+                if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
                     count++;
                 }
                 current /= 10;
