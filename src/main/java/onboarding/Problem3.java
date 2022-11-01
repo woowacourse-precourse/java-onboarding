bfd56663984d7ff77aea09c98351a73c881a35e3
박동주 <cxz541@gmail.com>
2022-11-01 14:38:24 +0900

    Add: Solve Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    private static boolean is369Number(int num) {
+        return num == 3 || num == 6 || num == 9;
+    }
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; ++i) {
+            int currNum = i;
+            while(currNum > 0) {
+                if(is369Number(currNum % 10)) ++answer;
+                currNum = currNum / 10;
+            }
+        }
         return answer;
     }
 }
