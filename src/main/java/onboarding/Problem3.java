5e87a8f6acb94cd27889aa2be657711f3715086b
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:52:58 +0900

    3번 문제 풀이

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,7 +1,7 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
+    public static int solution(int number) { // 들어온 int를 범위로 생각하고 배열로 생성한 후, split으로 자른 후에 3, 6, 9가 매치될 때마다 횟수를 증가시키도록
         int answer = 0;
         return answer;
     }
