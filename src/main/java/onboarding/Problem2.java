225c2d0bb8f28dd001942c5a89ba1e26ca2fb79a
sxxgxxnz <tmddms2292@gmail.com>
2022-11-01 13:27:12 +0900

    fix Problem2 refactoring

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,28 +3,27 @@ package onboarding;
 import java.util.Stack;
 
 public class Problem2 {
-
-    public static String solution(String cryptogram) {
-        String answer = "";
+    public static String dupCheck(String[] arr){
+        String result = "";
         Stack<String> stk = new Stack<String>();
-        String[] arr = cryptogram.split("");
-
         stk.push(arr[0]);
         for(int i=1;i<arr.length;i++){
             String str = arr[i];
-
             if(!stk.peek().equals(str)){
                 stk.push(str);
             }else if(stk.peek().equals(str)){
                 stk.pop();
             }
-
         }
-
         for(String s : stk){
-            answer+=s;
+            result+=s;
         }
-
+        return result;
+    }
+    public static String solution(String cryptogram) {
+        String answer = "";
+        String[] arr = cryptogram.split("");
+        answer = dupCheck(arr);
         return answer;
     }
 
