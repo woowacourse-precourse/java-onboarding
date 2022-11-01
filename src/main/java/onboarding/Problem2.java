70fd8d02266bd970ac0707322fa6e9cfa958bd64
박정현 <ros28081@naver.com>
2022-11-01 22:06:37 +0900

    [FEAT] Problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        List<String> word = new ArrayList<>(Arrays.asList(cryptogram.split("")));
+        List<String> stack = new ArrayList<>();
+        while(!word.isEmpty()) {
+            if(stack.isEmpty()) {
+                stack.add(word.get(0));
+            }
+            else { // 스택에 문자가 있을 때
+                if(stack.get(stack.size() - 1).equals(word.get(0))) {
+                    stack.remove(stack.size() - 1);
+                }
+                else {
+                    stack.add(word.get(0));
+                }
+            }
+            word.remove(0);
+        }
+        return String.join("", stack);
     }
 }
