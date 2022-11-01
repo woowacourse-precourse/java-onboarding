37e0eeb5843bfb13a0e98a4630407c6048bcb237
aran <aran0317@naver.com>
2022-11-01 21:14:14 +0900

    문제 4번 기능 요구사항

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,11 @@
 package onboarding;
 
+/*기능 요구 사항*/
+/* 1. word가 매개변수로 주어질 때 반대로 변환함.
+ * 2. 알파벳 외의 문자는 변환하지 않는다.
+ <solution>
+ *  - 알파벳 범위를 파악한 후, 반대로 변환하여 answer에 더해준다.
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
