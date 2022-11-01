5c21b2b019f32e14cd160749f8a4ab16fad8f0f7
민병욱 <woook27@naver.com>
2022-11-01 13:35:12 +0900

    fix: emptyList 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,7 +10,7 @@ public class Problem5 {
     }
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
 
         int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
