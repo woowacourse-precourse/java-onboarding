3d29770681f4e858a88e475f4797e3af5fc95d46
glass <glass7312@gmail.com>
2022-11-01 18:07:03 +0900

    Problem6 기능 구현 get:하나의 리스트의 이름데이터을 고정한뒤 나머지 리스트들의 이름을 비교하여 연속된 2개의 글자가 같을 시 정답 리스트의 담는다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,7 +7,19 @@ import java.util.stream.Collectors;
 public class Problem6 {
     public static List<String> get(List<String> ls1,List<String> ls2) {
 
+        List<String> ls= new ArrayList<>();
+        for(int i=0; i<ls1.get(1).length()-1; i++) {
+            for(int j=0; j<ls2.get(1).length()-1; j++) {
+                if (ls1.get(1).substring(i, i + 2).equals(ls2.get(1).substring(j, j + 2))) {
+                    ls.add(ls2.get(0));
+                    ls.add(ls1.get(0));
+                    return ls;
+                }
+            }
+        }
+        return null;
     }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
         for(int i=0; i<forms.size(); i++) {
