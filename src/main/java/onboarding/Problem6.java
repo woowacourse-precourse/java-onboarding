1fe38c2dbe081629114cf785436a8e8598b7a22b
hanjaegyeong <hjg000223@naver.com>
2022-11-01 13:44:15 +0900

    [Mission6] feat: 전체 작동 구조 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        ArrayList<String> answer = new ArrayList<>();
+        Set<Integer> overlapIndexSet = new HashSet<>();
+        HashMap<String,Integer> twoNameMap = new HashMap<>();
+        for(int formIndex=0; formIndex < forms.size(); formIndex++) {
+            setTwoName(overlapIndexSet, formIndex, forms, twoNameMap);
+        }
+        List<Integer> overlapIndexList = new ArrayList<>(overlapIndexSet);
+        for(int i=0; i < overlapIndexList.size(); i++){
+            answer.add(forms.get(overlapIndexList.get(i)).get(0));
+        }
+        Collections.sort(answer);
         return answer;
     }
 }
