c1724b77a68201dcfc616a42db4f0c1c54287689
JohnPrk <uo3641493@gmail.com>
2022-11-01 18:28:15 +0900

    feat : Problems6 프로덕션 코드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,14 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Users users = new Users(forms.stream()
+                .map(user -> new User(user))
+                .collect(Collectors.toList()));
+        UserMatchingList userMatchingList = users.getUserMatchingList();
+        return userMatchingList.mapToUserEmail();
     }
 }
