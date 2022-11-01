0f07fa266140677cba3c810c291d06169b034503
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-01 17:53:33 +0900

    feat(PROBLEM2): 연속된 중복 문자 존재 여부 판별 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,5 +7,16 @@ public class Problem2 {
         }
         return cryptogram;
     }
+
+    //1. 연속된 중복 문자 존재 여부 판별 기능
+    static boolean checkRepetition(String str){
+        for (int i=0 ; i<str.length()-1 ; i++){
+            if (str.charAt(i) == str.charAt(i+1)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
 
