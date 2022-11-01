f93093ef1e8597795e0c3040373d028b28e5b348
cgk95 <kjkj5868@gmail.com>
2022-11-01 14:01:55 +0900

    P2 변수명 수정,세부 메서드 분리,조건문 일부를 캡슐화

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,50 +3,50 @@ package onboarding;
 import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
-        myfunc02 mf=new myfunc02();
-
-        while(!mf.chk_cond(cryptogram)){ /*중복이 있다면*/
-            cryptogram=mf.remove_rpt(cryptogram,mf);
+        while(!checkCondition(cryptogram)){                                 /*중복문자열이 있는 경우 반복*/
+            cryptogram=removeRepeated(cryptogram);
         }
         return cryptogram;
     }
-    public static class myfunc02{
-        /**종료조건을 체크하는 메소드**/
-        boolean chk_cond(String str){
-            if (str.length()<=1) return true;
-            for (int i = 1; i < str.length(); i++) {
-                if(str.charAt(i-1)==str.charAt(i)) return false; /*중복을 발견하면*/
-            }
-            return true;
+    /**조건에 맞게 중복된 문자열이 있다면 거짓을 리턴**/
+    private static boolean checkCondition(String str){
+        if (str.length()<=1) return true;
+        for (int i = 1; i < str.length(); i++) {
+            if(str.charAt(i-1)==str.charAt(i)) return false; 
         }
-        /**다음 문자열을 만들어 리턴해주는 메소드*/
-        String nxtCrypto(Stack<String> stk){
-            StringBuilder nxt= new StringBuilder();
-            if (stk.size()<1) return "";
-            for (String s : stk) {
-                nxt.append(s);
-            }
-            return nxt.toString();
+        return true;
+    }
+    /**다음 문자열을 만들어 리턴*/
+    private static String nxtCrypto(Stack<String> stk){
+        StringBuilder nxt= new StringBuilder();
+        if (stk.size()<1) return "";
+        for (String s : stk) {
+            nxt.append(s);
         }
-        /** 중복을 제거하는 메인 기능 **/
-        String remove_rpt(String cryptogram,myfunc02 mf){
-            Stack<String> stk = new Stack<>();       /*스택 선언*/
-            stk.push(String.valueOf(cryptogram.charAt(0)));/*초기화*/
+        return nxt.toString();
+    }
+    /** 중복을 제거하는 메인 기능 **/
+    private static String removeRepeated(String cryptogram){
+        Stack<String> stk = new Stack<>();       
+        stk.push(String.valueOf(cryptogram.charAt(0)));                     /*초기화*/
 
-            boolean flag=false; /*찌꺼기 확인용*/
-            for (int i = 1; i < cryptogram.length(); i++) {
-                if (!String.valueOf(cryptogram.charAt(i)).equals(stk.peek())){ /* 이전의 원소와 같지않다면 */
-                    if (flag){ /*찌꺼기가 있었다면 제거해준다*/
-                        stk.pop();
-                        flag=false;
-                    }
-                    stk.push(String.valueOf(cryptogram.charAt(i)));
-                }else { /*같은 원소가 있다면*/
-                    flag=true; /*현재 원소를 스택에 추가하지 않고 넘어간다*/
+        boolean flag=false; /*찌꺼기 확인용 변수*/
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (!isEqualsToPrev(cryptogram, stk, i)){
+                if (flag){                                                  /*찌꺼기가 있다면*/
+                    stk.pop();                                              /*제거*/
+                    flag=false;
                 }
+                stk.push(String.valueOf(cryptogram.charAt(i)));
+            }else { 
+                flag=true;
             }
-            if (flag) stk.clear();
-            return mf.nxtCrypto(stk); /*일순한 문자열을 다음 다시 재전달*/
         }
+        if (flag) stk.clear();
+        return nxtCrypto(stk);                                              /*일순한 문자열을 다음 다시 재전달*/
+    }
+    /** 이전의 문자열과 현재 인덱스의 문자열이 같으면 참, 다르면 거짓을 반환 **/
+    private static boolean isEqualsToPrev(String cryptogram, Stack<String> stk, int i) {
+        return String.valueOf(cryptogram.charAt(i)).equals(stk.peek());
     }
 }
