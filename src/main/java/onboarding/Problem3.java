f4c25bed71d42008d3c6af1eed396b157a6578e1
ckn1070 <ckn1070@naver.com>
2022-11-01 23:05:44 +0900

    Problem 2: Modified remove duplication function for repeated characters and added comments for understanding

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,8 @@ public class Problem3 {
     static int numCounter(int curNum){
         int count = 0;
         while(curNum > 1){
-            int quotient = curNum % 10;
-            if(quotient == 3 || quotient == 6 || quotient == 9){
+            int remainder = curNum % 10;
+            if(remainder == 3 || remainder == 6 || remainder == 9){
                 count += 1;
             }
             curNum /= 10;
