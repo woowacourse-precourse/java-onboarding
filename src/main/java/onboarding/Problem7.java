a33c49f7cdfb4c0ada0eeaff45a6a3e2c74b7e0d
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-01 22:35:21 +0900

    problem7 : getUser, getFreindList
    getUser, getFreindList 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,56 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static final int USER_NUM=100;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        if(input_exception(user,friends,visitors)) return Collections.emptyList();
+        List<String> answer = getRanking(user,friends,visitors);
         return answer;
+    }
+
+    private static List<String> getRanking(String user, List<List<String>> friends, List<String> visitors){
+        List<String> result= new ArrayList<>();
+        List<String> userList=getUser(friends);
+        ArrayList<String>[] freindList=new ArrayList[USER_NUM];
+
+        for(int i=0;i<freindList.length;i++){
+            freindList[i]=getFriendList(userList.get(i),friends);
+        }
+
+        
+
+        Collections.sort(result);
+        return result;
+    }
+
+    private static List<String> getUser(List<List<String>> friends){
+        List<String> result=new ArrayList<>();
+        HashSet<String> userList=new HashSet<>();
+        for(int i=0;i<friends.size();i++){
+            userList.add(friends.get(i).get(0));
+            userList.add(friends.get(i).get(1));
+        }
+        Iterator<String> iter=userList.iterator();
+        while(iter.hasNext()){
+            result.add(iter.next());
+        }
+        return result;
+    }
+
+    private static ArrayList<String> getFriendList(String user, List<List<String>> friends) {
+        ArrayList result=new ArrayList<>();
+        for(int i=0;i<friends.size();i++){
+            if(user==friends.get(i).get(0)) result.add(friends.get(i).get(1));
+            else if(user==friends.get(i).get(1)) result.add(friends.get(i).get(0));
+        }
+        return result;
+    }
+
+    private static boolean input_exception(String user, List<List<String>> friends, List<String> visitors){
+        //true is 'exception excution'
+        return false;
     }
 }
