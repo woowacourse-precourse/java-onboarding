1003c7b5932b4fe1adda5863127c7eef2a5188bd
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-11-01 21:22:33 +0900

    Problem 6

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,3 +4,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
+public class Problem5 {
+    public static List<Integer> solution(int money) {
+        List<Integer> answer = new ArrayList<>();
+
+        int[] moneyArr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int div : moneyArr){
+            answer.add(money / div);
+            money %= div;
+        }
+        return answer;
+    }
+}
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,3 +2,31 @@ package onboarding;
 
 import java.util.*;
 
+public class Problem6 {
+    public static List<String> solution(List<List<String>> forms) {
+        List<String> crew = new ArrayList<>();
+        for (int i = 0; i < forms.size() - 1; i++){
+            String str = forms.get(i).get(1);
+            for (int j = 0; j < str.length() - 1; j++){
+                String sub_str = str.substring(j, j+2);
+                for (int k = i + 1; k < forms.size(); k++){
+                    if (forms.get(j).get(1).contains(sub_str))
+                        crew.add(forms.get(i).get(0));
+                    crew.add(forms.get(j).get(0));
+                }
+            }
+        }
+        crew = sortEmail(crew);
+        return crew;
+    }
+
+    private static List<String> sortEmail(List<String> str){
+        List<String> list = new ArrayList<String>();
+        for(int i=0; i< str.size(); i++)
+            if(!list.contains(str.get(i)))
+                list.add(str.get(i));
+        Collections.sort(list);
+        return list;
+    }
+
+}
