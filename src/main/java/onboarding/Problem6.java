8748e6f002a2927c8ecee35a64cc242c3f6f1c57
jsc1014 <wotlr5209@gmail.com>
2022-11-01 20:39:06 +0900

    6번 문제 // 닉네임 리스트 만들기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = new ArrayList<>();
+        List<String> nicknameList = new ArrayList<>();
+
+
+        makeNickNameList(forms,nicknameList);
+
+
         return answer;
+    }
+    public static void makeNickNameList(List<List<String>> originList,List <String> nickname){
+        for(int i = 0 ; i < originList.size(); i++){
+            nickname.add(originList.get(i).get(1));
+        }
     }
 }
