43e4684c148b8260652f95b9210262eb952dc247
WonSeok-dd <cluejws@gmail.com>
2022-11-01 13:48:43 +0900

    Problem1 | docs: 기능 정의

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,16 +2,6 @@ package onboarding;
 
 import java.util.List;
 
-/**
- * 기능 정의
- * 1. 페이지로 Score얻기
- * 2. Score 비교
- * 3. 리스트/배열 메소드 오버로딩(isErrorPages, solution)
- * 4. 예외처리 구현
- *  4-1 왼쪽 페이지가 홀수? 오른쪽 페이지가 짝수?
- *  4-2 왼쪽 페이지와 오른쪽 페이지가 범위 내의 페이지인가?
- *  4-3 왼쪽 페이지와 오른쪽 페이지가 연속된 페이지인가?
- */
 class Problem1 {
     private static boolean isErrorPages(List<Integer> pages){
 
