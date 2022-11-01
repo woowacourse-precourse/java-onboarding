9001249dded9febbc29102323cdf9aaf3e45fd65
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2022-11-01 16:07:17 +0900

    Feat: [문제4] 문자 변환하기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,21 @@
 package onboarding;
 
 public class Problem4 {
+    public static char[] stringtochar;
+
+    public static char changeWord(char word){
+        int newword=(int)word;
+        if(newword>64&&newword<97){
+            newword=155-(int)word;
+        }
+        if(newword>96&&newword<123){
+            newword=219-(int)word;
+        }
+        return (char)newword;
+    }
+
     public static String solution(String word) {
+        stringtochar=word.toCharArray();
         String answer = "";
         return answer;
     }
