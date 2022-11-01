9ba9924e3e5e1f581365eb08c06d1d82efc904d8
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-01 21:09:59 +0900

    problem6 : getList
    기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,30 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        if(input_exception(forms)) return List.of("input error");
+//        List<String> answer = List.of("");
+//        System.out.println(forms.get(0).get(0));
+        List<String> answer=getList(forms);
+        return List.of("true");
+    }
+
+    private static List<String> getList(List<List<String>> forms){
+        List<String> find_overlap=getOverlap(forms);
+        List<String> result=sort_find_overlap(find_overlap);
+        return result;
+    }
+
+    private static List<String> getOverlap(List<List<String>> forms){
+
+    }
+
+    private static List<String> sort_find_overlap(List<String> list){
+
+    }
+
+
+    private static boolean input_exception(List<List<String>> forms){
+        //true is 'exception excution'
+        return false;
     }
 }
