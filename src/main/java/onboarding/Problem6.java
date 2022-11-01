5d0ee0a7d6153e36ef47f7b00c45f3a29ca80a49
임원정 <LWJ@MacBookPro.local>
2022-11-01 19:47:53 +0900

    feature : problem6 각각의 케이스가 닉네임마다 들어있는지 확인하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,10 +8,26 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> nameList = makeNameList(forms);
         List<String> allCaseName = getAllCaseName(nameList);
+        List<List<Integer>> caseIndexList = checkSame(nameList, allCaseName);
 
 
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static List<List<Integer>> checkSame(List<String> nameList, List<String> nameCaseList) {
+        List<List<Integer>> index = new ArrayList<>();
+
+        for (String nameCase : nameCaseList) {
+            List<Integer> tmpList = new ArrayList<>();
+            for (int j = 0; j < nameList.size(); j++) {
+                if (nameList.get(j).contains(nameCase)) {
+                    tmpList.add(j);
+                }
+            }
+            index.add(tmpList);
+        }
+        return index;
     }
 
     private static List<String> getAllCaseName(List<String> nameList) {
