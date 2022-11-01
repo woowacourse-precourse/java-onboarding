83e5e30e102b7ae1181acb62e5292cbdad3fad75
2do1 <dowon3036@naver.com>
2022-11-01 22:37:59 +0900

    feat: 문제 7번 사용자와 함께 아는 친구의 수를 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -22,6 +22,42 @@ public class Problem7 {
             }
         }
 
+        // <아이디, 추천 점수>를 저장하는 해시맵
+        HashMap<String, Integer> score = new HashMap<>();
+
+        // 사용자와 함께 아는 친구의 수를 구하는 기능
+        for (int i = 0; i < userFriends.size(); i++) {
+            String friend = userFriends.get(i); // 사용자와 친구
+
+            for (int j = 0; j < friends.size(); j++) {
+                if (friends.get(j).contains(friend)) { // 사용자와의 친구의 친구일 경우
+                    String user1 = friends.get(j).get(0);
+                    String user2 = friends.get(j).get(1);
+
+                    if (user1.equals(friend)) {
+                        if (!user2.equals(user)) { // 사용자가 아닐 경우
+                            if (!userFriends.contains(user2)) // 사용자와 친구가 아닐 경우
+                                if (score.containsKey(user2)) { // 아이디가 이미 존재할 경우
+                                    score.put(user2, score.get(user2) + 10);
+                                } else { // 아이디가 존재하지 않을 경우
+                                    score.put(user2, 10);
+                                }
+                        }
+                    } else if (user2.equals(friend)) {
+                        if (!user1.equals(user)) { // 사용자가 아닐 경우
+                            if (!userFriends.contains(user1)) { // 사용자와 친구가 아닐 경우
+                                if (score.containsKey(user1)) { // 아이디가 이미 존재할 경우
+                                    score.put(user1, score.get(user1) + 10);
+                                } else { // 아이디가 존재하지 않을 경우
+                                    score.put(user1, 10);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 }
