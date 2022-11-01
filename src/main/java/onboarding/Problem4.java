ffb3095fafc14d696876888cb5fc39e4f0ad9b9d
gudals113 <gudals113@gmail.com>
2022-11-01 16:09:55 +0900

    feat : Problem4 Solved
    feat : Problem4 Solved
    
    isUpperCase 이용해서 대소문자 구분 후 계산

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (char w : word.toCharArray()) {
+            if (w == ' ') {
+                answer+=' ';
+            } else {
+                if (Character.isUpperCase(w)) {
+                    answer+=(char) (155 - (int) w);
+                } else {
+                    answer+=(char) (219 - (int) w);
+                }
+            }
+        }
         return answer;
     }
 }
