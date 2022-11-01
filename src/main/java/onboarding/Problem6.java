221da2cd3a40bfc534302d3eab64b0b2639e27a5
KimSoo <ilppang@naver.com>
2022-11-01 11:48:10 +0900

    Remove/Feature(김현수/Problem6): 다중 for문을 줄이기 위한 기존 코드 삭제 및 이메일, 이름 배열 나눠주는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,49 +1,29 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         try {
             List<String> answer = List.of("answer");
-
-            verificationEmail(forms);
-            duplicateName(forms);
+            List<String> emailList = setInit(forms, 0);
+            List<String> nameList = setInit(forms, 1);
 
             return answer;
         } catch (Exception error){
             return (List<String>) error;
         }
     }
-
-    public  static void verificationEmail(List<List<String>> forms) {
-        String emailPattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@email.com";
-
-        for (int i = 0; i < forms.size(); i++) {
-            if (!(Pattern.matches(emailPattern, forms.get(i).get(0)))) {
-                throw new IllegalArgumentException();
-            }
+    public static List<String> setInit(List<List<String>> forms, int index){
+        List<String> initlist = new ArrayList<>();
+        for (List<String> form : forms) {
+            initlist.add(form.get(index));
         }
+
+        return  initlist;
     }
 
-    public static void duplicateName(List<List<String>> forms){
-        List<String> nameArray = new ArrayList<>();
 
-        for (int i = 0; i < forms.size(); i++) {
-            nameArray.add(forms.get(i).get(1));
-        }
 
-        for (int i = 0; i < forms.size(); i++){
-            System.out.println(nameArray.get(i).length());
-            for (int j = 0; j < nameArray.get(i).length(); j++) {
-                if (j + 1 < nameArray.get(i).length()) {
-                    String duplication = nameArray.get(i).substring(j,j + 2);
-                    System.out.println(nameArray.contains(duplication));
-                }
-            }
-        }
-    }
 }
