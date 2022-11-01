1fa12e07d9ff47fff8ca7c90e5c69306355d4163
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 10:31:40 +0900

    좌, 우 중에 더 큰수 판별

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,4 +16,12 @@ class Problem1 {
             return (0);
         return (pobiMax > crongMax ? 1 : 2);
     }
+
+    public static int findMax(List<Integer> num) // 자리수를 이용한 계산 후에 좌, 우 중에 더 큰 수 판별
+    {
+        int left = num.get(0);
+        int right = num.get(1);
+
+        return (Math.max(calSumMul(left), calSumMul(right)));
+    }
 }
