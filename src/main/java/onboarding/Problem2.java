f077ce9849f740cb1eb420c7846719e517ae3ad9
i960107 <i960107@naver.com>
2022-11-01 11:52:32 +0900

    feat(problem2): implement feature of decode charList

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,10 +2,12 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
         List<Character> charList = convertStringToCharList(cryptogram);
+        List<Character> decoded = decode(charList);
         return null;
     }
 
@@ -16,6 +18,55 @@ public class Problem2 {
             charList.add(s.charAt(i));
         }
         return charList;
+    }
+
+    public static List<Character> decode(List<Character> charList) {
+        while (hasConsecutiveChars(charList)) {
+            charList = popConsecutiveChars(charList);
+        }
+        return charList;
+    }
+
+    public static boolean hasConsecutiveChars(List<Character> charList) {
+
+        if (charList.size() <= 1) {
+            return false;
+        }
+
+        for (int i = 1; i < charList.size(); i++) {
+            if (charList.get(i).equals(charList.get(i - 1))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static List<Character> popConsecutiveChars(List<Character> charList) {
+        List<Character> newCharList = new ArrayList<>();
+
+        Character previous = null;
+        int count = 0;
+
+        for (Character current : charList) {
+            if (Objects.equals(previous, current)) {
+                count += 1;
+                continue;
+            }
+
+            if (count == 1) {
+                newCharList.add(previous);
+            }
+
+            previous = current;
+            count = 1;
+        }
+
+        if (count == 1) {
+            newCharList.add(previous);
+        }
+
+        return newCharList;
     }
 
 
