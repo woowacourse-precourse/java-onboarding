03978a2ce0b2e9f54b2b933ced64c28ae21a051c
Park Sungchul <try615@naver.com>
2022-11-01 21:51:38 +0900

    [feat] Problem4.java : 주석 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+
+    /**
+     * solution 메소드
+     * StringBuilder를 이용하여 결과 문자열을 저장, 도출
+     */
     public static String solution(String word) {
         StringBuilder sb = new StringBuilder();
         int strLen = word.length();
@@ -12,6 +17,9 @@ public class Problem4 {
         return sb.toString();
     }
 
+    /**
+     * 대소문자, 혹은 그 이외 문자 판별 메소드
+     */
     private static void verifyChar(String word, StringBuilder sb, int i) {
         Character c = word.charAt(i);
 
@@ -24,6 +32,9 @@ public class Problem4 {
         }
     }
 
+    /**
+     * 대문자 판별 메소드
+     */
     private static boolean isUpper(char word) {
         if ((int) word >= 65 && (int) word <= 90) {
             return true;
@@ -32,6 +43,9 @@ public class Problem4 {
         }
     }
 
+    /**
+     * 소문자 판별 메소드
+     */
     private static boolean isLower(char word) {
         if ((int) word >= 97 && (int) word <= 122) {
             return true;
@@ -40,10 +54,16 @@ public class Problem4 {
         }
     }
 
+    /**
+     * 대문자에서 소문자 변환 메소드
+     */
     private static char upperToLower(char word) {
         return (char)(90 - ((int) word - 65));
     }
 
+    /**
+     * 소문자에서 대문자 변환 메소드
+     */
     private static char lowerToUpper(char word) {
         return (char)(122 - ((int) word - 97));
     }
