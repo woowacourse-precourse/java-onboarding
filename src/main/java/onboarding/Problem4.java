8f27c2a515cbfa701cc5411b5237296806946870
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-01 20:27:53 +0900

    problem4 : CONVERSION

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int CONVERSION(int x){
+        return 25-x;
+    }
+
     public static String solution(String word) {
         if(input_exception(word)) return "input error";
+
+        return getWord(word);
+    }
+
+    private static String getWord(String word){
 
     }
 
