74b95a8f19b7236aab94a2a41be1e9aa74adc245
DonghyunShin <shindh0429@gmail.com>
2022-11-01 23:18:19 +0900

    [CREATE] 중복 문자 체크 함수 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        while(true) {
+            String code = checkDuplicated(cryptogram);
+            if (cryptogram.equals(code)) {
+                break;
+            }
+            cryptogram = code;
+        }
+        return cryptogram;
     }
+
+    private static String checkDuplicated(String code) {
+        for (int i =0; i < code.length() - 1; i++) {
+            if (code.charAt(i) == code.charAt(i+1)) {
+                //중복문자 제거 함수 호출
+            }
+        }
+        return code;
+    }
+
 }
