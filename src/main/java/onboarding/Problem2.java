404a94c0d6e5179d0da312d2deab8aaf90d26426
0417taehyun <0417taehyun@gmail.com>
2022-11-01 14:53:59 +0900

    fix: [문제2] 중복된 문자열 여부 확인 및 제거 캡슐화를 위한 메서드 개별 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,19 @@
 package onboarding;
 
 public class Problem2 {
+    public static String removeEachCharacter(String cryptogram, int startIndex, int endIndex) {
+        return cryptogram.replaceFirst(cryptogram.substring(startIndex, endIndex + 1), "");
+    }
+
+    public static boolean isDuplicated(String cryptogram, int currentIndex, int compareIndex) {
+        return cryptogram.charAt(currentIndex) == cryptogram.charAt(compareIndex);
+    }
+
     public static String removeDuplicatedCharacters(String cryptogram) {
         int index = 0;
         while (index < cryptogram.length() - 1) {
-            char currentCharacter = cryptogram.charAt(index);
-            char nextCharacter = cryptogram.charAt(index + 1);
-            if (currentCharacter == nextCharacter) {
-                cryptogram = cryptogram.replaceFirst(cryptogram.substring(index, index + 2), "");
+            if (isDuplicated(cryptogram, index, index + 1)) {
+                cryptogram = removeEachCharacter(cryptogram, index, index + 1);
                 index -= 2;
             }
             index++;
