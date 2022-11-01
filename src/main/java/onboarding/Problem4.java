e4a022247be214a087af081a1c4516288170d510
yunseoooo <jeoungys0114@naver.com>
2022-11-01 12:29:00 +0900

    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+    public static String change_Word(String str){
+        String result = "";
+        for (int i=0; i< str.length(); i++){
+            Character ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch)){
+                result = result + ((char)('Z'-ch +'A'));
+            }
+
+            else if (Character.isLowerCase(ch)){
+                result = result + ((char)('z' - ch + 'a'));
+            }
+            else {
+                result = result + (ch);
+            }
+        }
+        return result;
+    }
+
     public static String solution(String word) {
         String answer = "";
+        answer = change_Word(word);
         return answer;
     }
 }
