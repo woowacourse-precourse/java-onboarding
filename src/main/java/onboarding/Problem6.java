f59630c39673567cfd14c76c7be1c66ddfe7602a
yunjeong <uu0078@naver.com>
2022-11-01 22:58:18 +0900

    Problem6 기능 목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,12 @@ package onboarding;
 
 import java.util.List;
 
+/* 기능 목록
+1. 모든 닉네임을 앞에서부터 2글자씩 끊어서 HashMap 에 저장
+2. HashMap 순회하며 둘 이상 담겨있는 이메일들 answer set 에 담기
+3. 이메일 오름차순 정렬, 중복 제거
+*/
+
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
