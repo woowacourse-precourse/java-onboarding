be814360be73464a1ff62173ba379f8b3047d217
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-01 14:43:00 +0900

    feat: Problem2 solution 메서드 구현
    - Problem2의 solution 함수를 구현함.
    - solution 함수가 디버그 빌드시 assert에 사용하는 도우미 함수
      isComposedOnnlyOfLowercaseAlphabets 함수를 구현함.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,52 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        assert (cryptogram != null);
+        assert (cryptogram.length() >= 1 && cryptogram.length() <= 1000);
+        assert (isComposedOnlyOfLowercaseAlphabets(cryptogram));
+
+        int cryptogramLength = cryptogram.length();
+        char[] nonDuplicateCharStack = new char[cryptogramLength];
+
+        int pointer = 0;
+        int top = 0;
+
+        while (pointer < cryptogramLength) {
+            if (top == 0) {
+                nonDuplicateCharStack[top] = cryptogram.charAt(pointer);
+                top++;
+                pointer++;
+                continue;
+            }
+
+            if (nonDuplicateCharStack[top - 1] != cryptogram.charAt(pointer)) {
+                nonDuplicateCharStack[top] = cryptogram.charAt(pointer);
+                top++;
+                pointer++;
+            } else {
+                pointer++;
+                while (pointer < cryptogramLength && nonDuplicateCharStack[top - 1] == cryptogram.charAt(pointer)) {
+                    pointer++;
+                }
+                top--;
+            }
+        }
+
+        char[] truncatedCryptogram = new char[top];
+        System.arraycopy(nonDuplicateCharStack, 0, truncatedCryptogram, 0, truncatedCryptogram.length);
+
+        return new String(truncatedCryptogram);
+    }
+
+    private static boolean isComposedOnlyOfLowercaseAlphabets(String string) {
+        int stringLength = string.length();
+
+        for (int i = 0; i < stringLength; i++) {
+            if (!Character.isLowerCase(string.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
