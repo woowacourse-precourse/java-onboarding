3799d814262e5d1f581c94f124e16b43552e705e
choiejae <ejae8319@gmail.com>
2022-11-01 17:39:16 +0900

    style (problem5): Google java convention

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,30 +3,36 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 public class Problem5 {
-    private static final List<Integer> MONETARY_UNITS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 1000000;
-    public static List<Integer> solution(int money) {
-        validityChecker(money);
 
-        return getAnswer(money);
-    }
+  private static final List<Integer> MONETARY_UNITS = List.of(50000, 10000, 5000, 1000, 500, 100,
+      50, 10, 1);
+  private static final int MIN_NUMBER = 1;
+  private static final int MAX_NUMBER = 1000000;
 
-    private static void validityChecker(int money) {
-        if (!isValidateMoney(money)) {
-            throw new IllegalArgumentException("Not valid money");
-        }
+  public static List<Integer> solution(int money) {
+    validityChecker(money);
+
+    return getAnswer(money);
+  }
+
+  private static void validityChecker(int money) {
+    if (!isValidateMoney(money)) {
+      throw new IllegalArgumentException("Not valid money");
     }
-    private static boolean isValidateMoney(int money) {
-        return MIN_NUMBER <= money && money <= MAX_NUMBER;
+  }
+
+  private static boolean isValidateMoney(int money) {
+    return MIN_NUMBER <= money && money <= MAX_NUMBER;
+  }
+
+  private static List<Integer> getAnswer(int money) {
+    List<Integer> answer = new ArrayList<>();
+    for (int monetaryUnit : MONETARY_UNITS) {
+      answer.add(money / monetaryUnit);
+      money %= monetaryUnit;
     }
-    private static List<Integer> getAnswer(int money) {
-        List<Integer> answer = new ArrayList<>();
-        for (int monetaryUnit : MONETARY_UNITS) {
-            answer.add(money / monetaryUnit);
-            money %= monetaryUnit;
-        }
-        return answer;
-    }
+    return answer;
+  }
 }
