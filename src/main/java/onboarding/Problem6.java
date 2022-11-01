6cc228230f1733cc165557a696be38f7413d8cd1
ggazitbyul <ngusy123@gmail.com>
2022-11-01 13:55:52 +0900

    Problem6) 1. 규칙 해석 및 주석 추가 2. returnDuplicatedNickname 메서드 구현   2-1 아래 메서드를 포함한 최종 메서드   2-2 중복 email 검출, 연산결과 중복값 제거, 정렬 3. isDuplicated 메서드 구현   3-1 조건식에 필요한 중복여부 메서드 4. deduplicateAndSortEmail 메서드 구현   4-1 연산결과의 중복값 제거 및 오름차순 정렬

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,11 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Collections;
+
 
 public class Problem6 {
 //    기능 요구 사항
@@ -27,18 +32,55 @@ public class Problem6 {
 //      ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"],
 //      ["nowm@email.com", "이제엠"] ]
 
-//      -- 요약 --
+    //      -- 요약 --
 //      1. 같은 글자가 연속적으로 포함 되는 닉네임 지원자 이메일 return.
 //        1-1 무슨뜻이고 하니, "제이엠", "제이슨" 이면 "제이"가 중복된다 이말이구만.
 //        1-2 연속되고, 두글자 이상 중복 걸러낼 것
+//          1-2-1 어차피 두글자 중복되면 세글자, 네글자도 조건에 포함되므로 무시.
 //        1-3 연속되지 않으면 중복 해당 아님
 //      2. 이메일이 email.com 인지 체크할 것
+//        2-1 email.com 형식이 아닐때 반환할 양식이 따로 없는데?
+//          2-1-1 아.. 어차피 email.com 로 정해진 폼이 온다는 얘기지..
 //      3. 닉네임은 한글
 //      4. return 값 오른차순 정렬하고, 중복은 제거할 것
-//      5. set 인터페이스 사용해서 중복 제거해보자.
-//      6. List 인터페이스 사용해서 오름차순 정렬할 것
+//        4-1 set 인터페이스 사용해서 중복 제거해보자.
+//        4-2 List 인터페이스 사용해서 오름차순 정렬할 것
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = returnDuplicatedNickname(forms);   // 닉네임 중복 반환 메서드 실행
+        System.out.println(answer); // 결과 확인 (임시)
         return answer;
+    }
+
+    public static List<String> returnDuplicatedNickname(List<List<String>> forms) {
+        List<String> temp = new ArrayList<>();  // 임시 저장소 생성
+        for (int targetIndex = 0; targetIndex < forms.size(); targetIndex++) {    // form size만큼 반복
+            for (int standIndex = 0; standIndex < targetIndex; standIndex++) {
+                if (isDuplicated(forms.get(targetIndex).get(1), forms.get(standIndex).get(1))) { // 닉네임 중복비교 isDuplicated(비교대상, 비교기준)
+                    temp.add(forms.get(targetIndex).get(0));    // 비교대상 리스트 업
+                    temp.add(forms.get(standIndex).get(0));     // 비교기준 리스트 업 나중에 중복리스트는 set으로 없애자.
+                }
+            }
+        }
+        List<String> result = deduplicateAndSortEmail(temp);     // 중복제거, 오름차순 정렬
+        return result;
+    }
+
+    public static boolean isDuplicated(String target, String standard) {  // 중복 포함 체크
+
+        for (int subStrIndex = 0; subStrIndex < target.length() - 1; subStrIndex++) {
+            if (standard.contains(target.substring(subStrIndex, subStrIndex + 2))) { // 왼쪽부터 순차적으로 두글자 짝짓고 연속 중복 확인
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<String> deduplicateAndSortEmail(List<String> email) {    // 중복제거, 오름차순 정렬
+        Set<String> setEmail = new HashSet<>(email);        // 중복 제거
+        List<String> sortEmail = new ArrayList<>(setEmail);
+        Collections.sort(sortEmail);        // 오름차순 정렬
+        List<String> result = sortEmail;
+        return result;
     }
 }
