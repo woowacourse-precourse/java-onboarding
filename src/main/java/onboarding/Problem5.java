b51c52a3a4963c655095a6dd1bda5ce793ed5a0b
gwj0421 <gwj0421@gmail.com>
2022-11-01 17:05:37 +0900

    문제5 - 입력된 돈을 최대한 가볍도록 출금하기 위해서 큰 금액의 화폐부터 교환하여 다양한 화폐의 수 리턴

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,7 +12,24 @@ public class Problem5 {
         - 특정 화폐로 교환했을 때, 나머지 금액을 얻음
      */
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        answer.add(money / 50000); // 50000원짜리 개수 반환
+        money %= 50000; // 50000원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money/ 10000);   // 10000원짜리 개수 반환
+        money %= 10000; // 10000원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money/ 5000);    // 5000원짜리 개수 반환
+        money %= 5000; // 5000원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money/ 1000);    // 1000원짜리 개수 반환
+        money %= 1000; // 1000원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money/ 500); // 500원짜리 개수 반환
+        money %= 500; // 500원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money/ 100); // 100원짜리 개수 반환
+        money %= 100; // 100원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money/ 50);  // 50원짜리 개수 반환
+        money %= 50; // 50원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money/ 10);  // 10원짜리 개수 반환
+        money %= 10; // 10원짜리 화폐로 교환시 나머지 금액 반환
+        answer.add(money);  // 1원짜리 개수 반환
         return answer;
     }
 }
