63fe6d337114dbd2982154f234ec59ecb32521fc
ho2n <jungho2n@gmail.com>
2022-11-01 17:17:09 +0900

    각 숫자의 자릿수에서 3, 6, 9 개수 세기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,4 +14,16 @@ public class Problem3 {
         }
         return answer;
     }
+
+    public static int isContain369(int number) {
+        int count = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            number /= 10;
+            if ((digit == 3) || (digit == 6) || (digit == 9)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
