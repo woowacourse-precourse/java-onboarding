f57aed0f9613d0cd18dc6b03387516f2b6e04ff5
ye1ne <yewon7036@naver.com>
2022-11-01 14:56:13 +0900

    docs : 문제 1번에 대한 풀이 및 구현 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,10 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 문제 풀이 방식 & 기능 목록서는
+ * docs/PROBLEM1.md 의 하단에 기술되어 있습니다
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
