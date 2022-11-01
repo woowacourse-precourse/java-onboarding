2ece445936e5442783b96d48cc8ce8c8ea0d6a22
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-01 15:31:30 +0900

    Problem_7_Restriction 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,17 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+class Problem_7_Restriction extends Exception {
+    public Problem_7_Restriction() {
+    }
+
+    ;
+
+    public Problem_7_Restriction(String message) {
+        super(message);
+    }
+}
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
