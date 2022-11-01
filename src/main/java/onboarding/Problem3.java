a4b7c951cad28ad59d799227907f7d1b8c2e2c31
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-01 13:31:04 +0900

    Clap 클래스 생성자 구현, Line Formatting 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
+
+class Clap {
+    ArrayList<Integer> numberList = new ArrayList<>();
+    ArrayList<String> stringList = new ArrayList<>();
+
+    Clap(int number) throws RestrictException {
+        if (number > 10000 || number < 1) {
+            throw new RestrictException("OutOfRange");
+        }
+        for (int i = 1; i <= number; i++) {
+            numberList.add(i);
+        }
+        for (Integer value : numberList) {
+            int radix = 1;
+            for (int i = 1; radix <= value; i++) {
+                stringList.add(value.toString().split("")[i - 1]);
+                radix *= 10;
+            }
+        }
+    }
+}
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
