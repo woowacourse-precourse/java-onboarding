6fb83508e39cc5b3b365c15c61eaae9d9b426dd5
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-01 22:15:02 +0900

    style(문제 5): 주석 추가 및 LF로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,13 +10,15 @@ public class Problem5 {
         if (violation(money)) return ERROR;
         List<Integer> answer = new ArrayList<>(9);
         List<Integer> coins = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        // 비싼 화폐부터 채우는 것이 최적(뒤에 나오는 값이 앞에 나오는 값의 약수이기 때문)
         for (int coin : coins) {
             answer.add(money / coin);
             money %= coin;
         }
         return answer;
     }
-
+    
+    // 돈의 액수가 올바른 입력인지 체크
     static boolean violation(int money) {
         return money <= 0 || money > 1000000;
     }
