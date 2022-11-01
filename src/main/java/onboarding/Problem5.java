519528f385a409d829bca34b36aea302fef2ee5b
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-01 12:13:49 +0900

    [Feature]Problem5 - solution

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> MONETARY_UNIT = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return withdrawMoney(money);
+    }
+
+    private static List<Integer> withdrawMoney(int money) {
+        List<Integer> wallet = new ArrayList<>();
+
+        for (Integer unit : MONETARY_UNIT) {
+            int cnt = getCount(money, unit);
+            wallet.add(cnt);
+            money %= unit;
+        }
+
+        return wallet;
+    }
+
+    private static int getCount(int money, Integer unit) {
+        return money / unit;
     }
 }
