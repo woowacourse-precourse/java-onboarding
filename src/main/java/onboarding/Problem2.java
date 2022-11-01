54cffeee83bb92448c83bc09d2cbd13a7f226dc5
Taekwon Lee <dev.taekwonlee@gmail.com>
2022-11-01 20:32:33 +0900

    feat: Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        answer = removeDuplicate(cryptogram);
+
         return answer;
+    }
+
+    // 중복 문자 제거
+    private static String removeDuplicate(String str) {
+
+        char lastChar = 0;
+        boolean isRemoved = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i != 0 && lastChar == str.charAt(i)) {
+                str = str.substring(0, i - 1) + str.substring(i + 1);
+                isRemoved = true;
+                break;
+            }
+            lastChar = str.charAt(i);
+        }
+
+        if (isRemoved)
+            str = removeDuplicate(str);
+
+        return str;
     }
 }
