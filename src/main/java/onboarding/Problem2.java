636a5ccd9f55f32684dabe20d7a091e7e07e99aa
youswim <youswim96@gmail.com>
2022-11-01 21:53:17 +0900

    Feat: 문자열에서 중복 확인 & 중복 삭제 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,17 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static boolean checkDuplicatedAndDelete(StringBuilder input) {
+        boolean chk = false;
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == input.charAt(i + 1)) {
+                input.deleteCharAt(i);
+                input.deleteCharAt(i);
+                i--;
+                chk = true;
+            }
+        }
+        return chk;
+    }
 }
