0919395125ab4cb2bc682dcb9907ff9a275c9506
yujeongkwon <yoojeongkwon@naver.com>
2022-11-01 10:20:11 +0900

    feat(PROBLEM4): isUpperAlpha() 대문자 알파벳을 청개구리 문자로 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,22 +3,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        char ch; int tmp;
-
-        for(int i=0; i<word.length(); i++){
-            ch = word.charAt(i);
-            if(ch >='A' && ch <= 'Z'){
-                tmp = ch - 65;
-                answer += (char)(90 -tmp);
-            }
-            else if(ch >= 'a' && ch <= 'z'){
-                tmp = ch - 97;
-                answer += (char)(122 -tmp);
-            }
-            else
-                answer += ch;
-        }
-
         return answer;
+    }
+
+    public static char isUpperAlpha(char ch){
+        return (char)(90 -(ch - 65));
     }
 }
