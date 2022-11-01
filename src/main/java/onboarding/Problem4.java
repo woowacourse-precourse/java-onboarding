a20a057d8c444c4afe9a385bc18cdb68c68521aa
yewon <yewon.ux@gmail.com>
2022-11-01 20:33:41 +0900

    대문자 소문자 배열 생성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
     public static String solution(String word) {
         /*
@@ -11,6 +13,24 @@ public class Problem4 {
         5. 예외 case 처리 (A-Z, a-z 외의 경우)
         */
         String answer = "";
+
+        //A-Z, a-z 배열 생성
+        ArrayList<Character> upperCase = new ArrayList<Character>(26);
+        ArrayList<Character> lowerCase = new ArrayList<Character>(26);
+
+        for(int i=0; i<26; i++) {
+            //ASCII
+            upperCase.add((char)(65+i));
+        }
+
+        for (int j=0; j<26; j++) {
+            //ASCII
+            lowerCase.add((char)(97+j));
+        }
+
+        System.out.print(upperCase);
+        System.out.print(lowerCase);
+
         return answer;
     }
 }
