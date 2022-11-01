01ead5f369d49919ff42a53bfa0c6d2ebf0a2d5a
sungjun kim <dd8873@naver.com>
2022-11-01 22:31:13 +0900

    feat: Problem6 solved

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Set<String> results = new HashSet<>();
+
+        for (int i = 0; i < forms.size(); i++) {
+            List<StringBuilder> list = new ArrayList<>();
+
+            for (int j = 1; j < forms.get(i).get(1).length(); j++) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(forms.get(i).get(1).charAt(j - 1)).append(forms.get(i).get(1).charAt(j));
+                list.add(stringBuilder);
+            }
+
+            for (int j = i + 1; j < forms.size(); j++) {
+                for (StringBuilder stringBuilder : list) {
+                    if (forms.get(j).get(1).contains(stringBuilder)) {
+                        results.add(forms.get(j).get(0));
+                        results.add(forms.get(i).get(0));
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        List<String> answer = new ArrayList<>(results);
+        Collections.sort(answer);
         return answer;
     }
+
 }
