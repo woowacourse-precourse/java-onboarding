cd60fd006984d46bcb54f70c7e4c6f0c4731f6f0
rvlwldev <rvlwlasd77@gmail.com>
2022-11-01 14:15:39 +0900

    문제3 : 박수의 갯수를 세는 메소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,31 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    /**
+     * 숫자를 입력받아 문자열로 변환 후 3,6,9의 갯수를 반환한다.
+     * 
+     * @param nowNumber
+     * @return {@code} int} 3,6,9 갯수의 합
+     */
+    private static int getClaps(int nowNumber) {
+        String strNumber = String.valueOf(nowNumber);
+        String removed = removeClaps(strNumber);
+
+        return strNumber.length() - removed.length();
+    }
+
+    /**
+     * 문자열을 입력받아 3,6,9 문자를 지운다.
+     * 
+     * @param strNum
+     * @return {@code String} 3,6,9를 없앤 문자열
+     */
+    private static String removeClaps(String strNum) {
+        strNum = strNum.replace("3", "");
+        strNum = strNum.replace("6", "");
+        strNum = strNum.replace("9", "");
+
+        return strNum;
+    }
 }
