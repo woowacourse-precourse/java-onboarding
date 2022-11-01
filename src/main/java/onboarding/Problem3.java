012a3801f597679ec954f5c083305d193e36c392
unknown <dragontaek98@naver.com>
2022-11-01 17:23:24 +0900

    feat: problem3 숫자 각 자리수의 3,6,9의 개수를 세는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,6 +5,12 @@ public class Problem3 {
         int answer = 0;
         for(int i = 1; i <= number; i++){
             String numberString = Integer.toString(i);
+            int length = numberString.length();
+            for(int j = 0; j < length; j++){
+                if(numberString.charAt(j) == '3' || numberString.charAt(j) == '6' || numberString.charAt(j) == '9'){
+                    answer++;
+                }
+            }
         }
         return answer;
     }
