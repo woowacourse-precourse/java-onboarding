2382922dc863ad0984ccd762fd096692aad6dc01
민병욱 <woook27@naver.com>
2022-11-01 21:16:28 +0900

    feat: calculateScore 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -7,11 +8,39 @@ import java.util.List;
 public class Problem7 {
 
     public static HashMap<String, HashSet<String>> friendList = new HashMap<>();
-    public static HashMap<String, Integer> vistorList;
+    public static HashMap<String, Integer> vistorList = new HashMap<>();
     public static HashMap<String, Integer> scoreboard = new HashMap<>();
 
     private static List<String> getRecommendedFriends(String user) {
+        calculateScore(user);
 
+    }
+
+    private static void calculateScore(String user) {
+        List<String> userFriends = new ArrayList<>(friendList.get(user));
+
+        for (int i = 0; i < userFriends.size(); i++) {
+            String userFriend = userFriends.get(i);
+            List<String> acquaintance = new ArrayList<>(friendList.get(userFriend));
+            for (int j = 0; j < acquaintance.size(); j++) {
+                String name = acquaintance.get(i);
+                int score = 0;
+                if(scoreboard.containsKey(name)) {
+                    score = scoreboard.get(name);
+                }
+                scoreboard.put(name, score + 10);
+            }
+        }
+
+        List<String> vistors = new ArrayList<>(vistorList.keySet());
+        for (int i = 0; i < vistors.size(); i++) {
+            String name = vistors.get(i);
+            int score = 0;
+            if(scoreboard.containsKey(name)) {
+                score = scoreboard.get(name);
+            }
+            scoreboard.put(name, score + 1);
+        }
     }
 
     private static void setFriendList(List<List<String>> friends) {
