6988c33e9011ee206491412904d97e273eb115be
ttaehee <ttaehee1105@gmail.com>
2022-11-01 14:00:28 +0900

    문제6) feat : Crew 객체 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,3 +8,26 @@ public class Problem6 {
         return answer;
     }
 }
+
+class Crew {
+
+    private String email;
+    private String nickname;
+
+    public Crew(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public static Crew toCrew(List<String> form) {
+        return new Crew(form.get(0), form.get(1));
+    }
+}
