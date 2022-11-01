26a8ff99a04661651dc948bc30a9820d644ed020
KyungKyu Park <rudrb0505@gmail.com>
2022-11-01 15:12:51 +0900

    refactor(Problem7): 구현한 기능들을 각각의 메소드로 정리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,9 +8,36 @@ import java.util.Map;
 import java.util.Set;
 
 public class Problem7 {
+
+	public static final int POINT_FRIEND_OF_FRIEND = 10;
+	public static final int POINT_VISITOR = 1;
+
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 		HashMap<String, ArrayList<String>> friendshipChart = new HashMap<>();
 
+		inputFriendshipChart(friends, friendshipChart);
+
+		Map<String, Boolean> memberUpdated = initMemberAccess(friendshipChart);
+
+		List<Set<String>> groupCollection = createGroupCollection(friendshipChart, memberUpdated);
+
+		Set<String> recommendedFriends = getRecommendedFriends(user, groupCollection);
+		recommendedFriends.remove(user);
+		recommendedFriends.addAll(visitors);
+		deleteFriend(user, friendshipChart, recommendedFriends);
+
+		HashMap<String, Integer> scoreOfRecommendation = initRecommendationScore(recommendedFriends);
+		addPointToFriendOfFriend(user, friendshipChart, scoreOfRecommendation);
+		addPointToVisitors(visitors, scoreOfRecommendation);
+		deleteZeroPoint(scoreOfRecommendation);
+
+		List<String> sortedRecommendation = sortRecommendation(scoreOfRecommendation);
+		sortedRecommendation = getFiveRecommendations(sortedRecommendation);
+
+		return sortedRecommendation;
+	}
+
+	private static void inputFriendshipChart(List<List<String>> friends, HashMap<String, ArrayList<String>> friendshipChart) {
 		for (List<String> friend : friends) {
 			String member = friend.get(0);
 			String friendOfMember = friend.get(1);
@@ -24,12 +51,18 @@ public class Problem7 {
 			friendshipChart.get(member).add(friendOfMember);
 			friendshipChart.get(friendOfMember).add(member);
 		}
+	}
 
+	private static Map<String, Boolean> initMemberAccess(HashMap<String, ArrayList<String>> friendshipChart) {
 		Map<String, Boolean> memberUpdated = new HashMap<>();
 		for (String member : friendshipChart.keySet()) {
 			memberUpdated.put(member, false);
 		}
+		return memberUpdated;
+	}
 
+	private static List<Set<String>> createGroupCollection(HashMap<String, ArrayList<String>> friendshipChart,
+		Map<String, Boolean> memberUpdated) {
 		List<Set<String>> groupCollection = new ArrayList<>();
 		for (String member : friendshipChart.keySet()) {
 			if (!memberUpdated.get(member)) {
@@ -38,7 +71,21 @@ public class Problem7 {
 				groupCollection.add(connectedMembers);
 			}
 		}
+		return groupCollection;
+	}
 
+	private static void setConnectedMembers(String member, HashMap<String, ArrayList<String>> friendshipChart,
+		Map<String, Boolean> memberUpdated, Set<String> connectedMembers) {
+		memberUpdated.put(member, true);
+		connectedMembers.add(member);
+		for (String friendOfMember : friendshipChart.get(member)) {
+			if (!memberUpdated.get(friendOfMember)) {
+				setConnectedMembers(friendOfMember, friendshipChart, memberUpdated, connectedMembers);
+			}
+		}
+	}
+
+	private static Set<String> getRecommendedFriends(String user, List<Set<String>> groupCollection) {
 		Set<String> recommendedFriends = new HashSet<>();
 		for (Set<String> group : groupCollection) {
 			if (group.contains(user)) {
@@ -46,34 +93,50 @@ public class Problem7 {
 				break;
 			}
 		}
-		recommendedFriends.remove(user);
-		recommendedFriends.addAll(visitors);
+		return recommendedFriends;
+	}
 
+	private static void deleteFriend(String user, HashMap<String, ArrayList<String>> friendshipChart,
+		Set<String> recommendedFriends) {
 		for (String friend : friendshipChart.get(user)) {
 			recommendedFriends.remove(friend);
 		}
+	}
 
+
+	private static HashMap<String, Integer> initRecommendationScore(Set<String> recommendedFriends) {
 		HashMap<String, Integer> scoreOfRecommendation = new HashMap<>();
 		for (String member : recommendedFriends) {
 			scoreOfRecommendation.put(member, 0);
 		}
+		return scoreOfRecommendation;
+	}
 
+	private static void addPointToFriendOfFriend(String user, HashMap<String, ArrayList<String>> friendshipChart,
+		HashMap<String, Integer> scoreOfRecommendation) {
 		for (String friend : friendshipChart.get(user)) {
 			for (String friendOfFriend : friendshipChart.get(friend)) {
 				if (scoreOfRecommendation.containsKey(friendOfFriend)) {
-					scoreOfRecommendation.put(friendOfFriend, scoreOfRecommendation.get(friendOfFriend) + 10);
+					scoreOfRecommendation.put(friendOfFriend, scoreOfRecommendation.get(friendOfFriend) + POINT_FRIEND_OF_FRIEND);
 				}
 			}
 		}
+	}
 
+	private static void addPointToVisitors(List<String> visitors, HashMap<String, Integer> scoreOfRecommendation) {
 		for (String visitor : visitors) {
 			if (scoreOfRecommendation.containsKey(visitor)) {
-				scoreOfRecommendation.put(visitor, scoreOfRecommendation.get(visitor) + 1);
+				scoreOfRecommendation.put(visitor, scoreOfRecommendation.get(visitor) + POINT_VISITOR);
 			}
 		}
+	}
 
+	private static void deleteZeroPoint(HashMap<String, Integer> scoreOfRecommendation) {
 		scoreOfRecommendation.entrySet().removeIf(entry -> entry.getValue() == 0);
+	}
 
+
+	private static List<String> sortRecommendation(HashMap<String, Integer> scoreOfRecommendation) {
 		List<String> sortedRecommendation = new ArrayList<>(scoreOfRecommendation.keySet());
 		sortedRecommendation.sort((a, b) -> {
 			if (scoreOfRecommendation.get(a).equals(scoreOfRecommendation.get(b))) {
@@ -81,22 +144,14 @@ public class Problem7 {
 			}
 			return scoreOfRecommendation.get(b) - scoreOfRecommendation.get(a);
 		});
-
-		if (sortedRecommendation.size() > 5) {
-			sortedRecommendation = sortedRecommendation.subList(0, 5);
-		}
-
 		return sortedRecommendation;
 	}
 
-	private static void setConnectedMembers(String member, HashMap<String, ArrayList<String>> friendshipChart,
-		Map<String, Boolean> memberUpdated, Set<String> connectedMembers) {
-		memberUpdated.put(member, true);
-		connectedMembers.add(member);
-		for (String otherMember : friendshipChart.get(member)) {
-			if (!memberUpdated.get(otherMember)) {
-				setConnectedMembers(otherMember, friendshipChart, memberUpdated, connectedMembers);
-			}
+	private static List<String> getFiveRecommendations(List<String> recommendations) {
+		if (recommendations.size() > 5) {
+			recommendations = recommendations.subList(0, 5);
 		}
+		return recommendations;
 	}
+
 }
