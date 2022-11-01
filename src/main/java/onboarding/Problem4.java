a1dd758e6e3e6685decf2d937e336546e8dcacf0
Gentlee <ekdma3259@gmail.com>
2022-11-01 19:31:44 +0900

    Problem4 - solve(아스키코드로 변환)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
+	public static String solution(String word) {
+    	if(word.length() < 1 || word.length() >1000) return null;
+    	int intChar=0;
+    	char charWord=' ';
+    	String answer = "";
+    	// 아스키코드로 변환하여 기능 구현
+        for(int num : word.toCharArray()) {
+        	if(num >= 97 && num <= 122) {
+        		intChar=122-(num-97);
+        		charWord=(char)intChar;
+        		answer+=charWord;
+        	}else if(num >= 65 && num <= 90) {
+        		intChar=90-(num-65);
+        		charWord=(char)intChar;
+        		answer+=charWord;
+        	}else if(num == 32){
+        		answer+=' ';
+        	}else {
+        		return null;
+        	}
+        }
         return answer;
     }
 }
