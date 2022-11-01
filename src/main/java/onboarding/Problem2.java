0091d41dea38222923bef1a5b543964e7e1af1eb
JeyunSong <jyunart@naver.com>
2022-11-01 15:44:41 +0900

    Problem 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
+
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (!answer.equals(cryptogram)) {
+            answer = cryptogram;
+            cryptogram = deduplicate(cryptogram);
+        }
         return answer;
+    }
+
+    static boolean isDuplicate(String str, int idx) {
+        if (idx > 0 && str.charAt(idx) == str.charAt(idx - 1)) {
+            return true;
+        } else return idx < str.length() - 1 && str.charAt(idx) == str.charAt(idx + 1);
+    }
+
+    static String deduplicate(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!isDuplicate(str, i)) {
+                result.append(str.charAt(i));
+            }
+        }
+
+        return result.toString();
     }
 }
