749e220dea8e4280e8cc85bedc2aaad1a975ccc2
Yh0sun <hy770819@naver.com>
2022-11-01 15:12:00 +0900

    func1 ããadd

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -7,6 +7,9 @@ public class Problem3 {
         //1. 1~number loop
         //  2. 숫자를 10으로 나눈 나머지가 3,6,9 중 하나이면 answere+=1
         //     숫자를 10으로 나눈 몫으로 계속 반복
+        for(int i=1;i<=number;i++){
+            int num=i;
+        }
         return answer;
     }
 }
