596019c5f3696a600aed5a07db207f05dd61db1d
otndus <xuyeon219@gmail.com>
2022-11-01 13:47:37 +0900

    [기능 구현] Problem4 문자를 반대로 변환해주는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
+
+    public static int change(int charIndex){
+        if(Character.isUpperCase(charIndex)){
+            charIndex = 155-charIndex;
+        }else charIndex = 219-charIndex;
+        return charIndex;
+    }
+    public static String charToString(ArrayList cArr){
+        String result = "";
+        for(int i = 0; i < cArr.size(); i++){
+            result += cArr.get(i).toString();
+        }
+        return result;
+    }
+
     public static String solution(String word) {
         String answer = "";
         return answer;
