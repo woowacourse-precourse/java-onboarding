5d460e2ae83d16676c3aea965a84d089e60288c5
WonSubSub <boxer1995@naver.com>
2022-11-01 20:37:59 +0900

    Problem7 Modification

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,6 +10,7 @@ public class Problem7 {
         Set<String> friends_list1 = new HashSet<>();
         Set<String> friends_list = new HashSet<>();
         Set<String> final_friends_set = new HashSet<>();
+        List<String> friends_list10_count = new ArrayList<>();
         List<Integer> score = new ArrayList<>();
         List<String> answer1 = new ArrayList<>();
 
@@ -35,9 +36,11 @@ public class Problem7 {
                                 if (friend_index == 1){
                                     int new_friend_index = 0;
                                     friends_list10.add(friends.get(b).get(new_friend_index));
+                                    friends_list10_count.add(friends.get(b).get(new_friend_index));
                                 } else {
                                     int new_friend_index = 1;
                                     friends_list10.add(friends.get(b).get(new_friend_index));
+                                    friends_list10_count.add(friends.get(b).get(new_friend_index));
                                 }//if4
                             }//if3
                         }//for3
@@ -55,7 +58,7 @@ public class Problem7 {
         for (int i = 0; i < final_friends_list.size(); i++) {
             int count = 0;
             if (friends_list10.contains(final_friends_list.get(i))) {
-                count += 10;
+                count += Collections.frequency(friends_list10_count, final_friends_list.get(i)) * 10;
             }//if
             if (friends_list1.contains(final_friends_list.get(i))) {
                 count += Collections.frequency(visitors, final_friends_list.get(i));
@@ -63,14 +66,39 @@ public class Problem7 {
             score.add(count);
         }//for
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; answer1.size() < 5;) {
+            System.out.println(final_friends_list);
+            System.out.println(score);
+            System.out.println(score.size());
             if (score.size() == 0 || Collections.max(score) == 0){
+                System.out.println(1);
                 break;
             }//if
-            int max_index = score.indexOf(Collections.max(score));
-            answer1.add(final_friends_list.get(max_index));
-            final_friends_list.remove(max_index);
-            score.remove(max_index);
+            List<String> same_score = new ArrayList<>();
+            int b = Collections.frequency(score, score.get(i));
+            if (b > 1){
+                for (; b != 0; b--) {
+                    int max_index = score.indexOf(Collections.max(score));
+                    same_score.add(final_friends_list.get(max_index));
+                    final_friends_list.remove(max_index);
+                    score.remove(max_index);
+                }//for
+                Collections.sort(same_score);
+                System.out.println(same_score);
+                System.out.println(1);
+                for (int a = 0; a < same_score.size(); a++) {
+                    answer1.add(same_score.get(a));
+                    if (answer1.size() == 5) {
+                        break;
+                    }
+                }
+            } else {
+                int max_index = score.indexOf(Collections.max(score));
+                answer1.add(final_friends_list.get(max_index));
+                final_friends_list.remove(max_index);
+                score.remove(max_index);
+                System.out.println(max_index);
+            }//if
         }//for
         answer = new ArrayList<>(answer1);
         return answer;
