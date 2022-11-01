73526a00bc10105aee948c1a2176b25f93b0b24c
yujindonut <hyujin0825@gmail.com>
2022-11-01 21:51:59 +0900

    [CHORE] 코드 리팩토링 #4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,28 +1,22 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
 
         Map<Character, Character> map = new HashMap<>();
         for (int i = 0; i < 25; i++) {
-            map.put((char)('A' + i), (char)('Z' - i));
-            map.put((char)('a' + i), (char)('z' - i));
+            map.put((char) ('A' + i), (char)('Z' - i));
+            map.put((char) ('a' + i), (char)('z' - i));
         }
 
         for (int i = 0; i < word.length(); i++) {
             Character c = word.charAt(i);
-            answer += map.getOrDefault(c,c);
+            answer.append(map.getOrDefault(c, c));
         }
-        return answer;
+        return answer.toString();
     }
-
-//    public static void main(String[] args) {
-//        System.out.println(solution("I love you"));
-//    }
 }
