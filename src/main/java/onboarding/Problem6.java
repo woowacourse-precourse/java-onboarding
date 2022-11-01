0678a0f4178fbf2274b6650947520a2b618a880a
tdmdgh <tdmdgh@gmail.com>
2022-11-01 19:14:37 +0900

    problem6_2_pattern_list 추출하는 매소드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Problem6 {
     private static final int EMAIL = 0;
     private static final int NAME = 1;
+    private static List<String> getPatternList(List<String> name_list) {
+        List<String> pattern_list = new ArrayList<>();
+        for(String name: name_list){
+            for(int i = 0 ; i < name.length()-1; i ++){
+                String pattern = name.substring(i,i+2);
+                if(!pattern_list.contains(pattern)) pattern_list.add(pattern);
+            }
+        }
+        return pattern_list;
+    }
     private static List<String> getNameList(List<List<String>> forms){
         List<String> name_list = new ArrayList<>();
         for (List<String> crew : forms){
