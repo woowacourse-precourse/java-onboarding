5b18fc974288d0ea0d27d799b0065f0d2fd623ff
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2022-11-01 17:30:53 +0900

    refactor(Problem5): 변수명 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,14 +8,13 @@ public class Problem5 {
             {50000,10000,5000,1000,500,100,50,10,1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = withdrawalMoney(money);
-
         return answer;
     }
     private static List<Integer> withdrawalMoney(int money){
         List<Integer> counting_Unit = new ArrayList<>();
-        for(int won_Amount : MONEY_UNIT_ARRAY){
-            counting_Unit.add(money / won_Amount);
-            money = money % won_Amount;
+        for(int wonAmount : MONEY_UNIT_ARRAY){
+            counting_Unit.add(money / wonAmount);
+            money = money % wonAmount;
         }
         return counting_Unit;
     }
