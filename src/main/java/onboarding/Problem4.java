cc59e3ae3b417e57517ec7649c313aa902a359db
2do1 <dowon3036@naver.com>
2022-11-01 17:23:50 +0900

    feat: 문제 4번 각 문자를 청개구리 사전대로 반대로 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,11 +9,15 @@ public class Problem4 {
             int ascii = (int)ch;
 
             // 알파벳 외의 문자는 변환하지 않는다
+            // 각 문자를 청개구리 사전대로 반대로 변환하는 기능
             if (65 <= ascii && ascii <= 90) { // 문자가 대문자일 경우
-
+                ascii = 155 - ascii; // 반대로 변환해준다
+                ch = (char)ascii; // 아스키 값 -> 문자로 변환
             } else if (97 <= ascii && ascii <= 122) { // 문자가 소문자일 경우
-
+                ascii = 219 - ascii; // 반대로 변환해준다
+                ch = (char)ascii; // 아스키 값 -> 문자로 변환
             }
+            answer += ch;
         }
         return answer;
     }
