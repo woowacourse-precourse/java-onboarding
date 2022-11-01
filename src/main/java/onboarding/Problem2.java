553842a4a2cc024633d4de9849346eddaeca2f0c
khj <kim37171@cuk.edu>
2022-11-01 20:19:59 +0900

    [Problem2] 중복 문자열 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,7 +7,21 @@ public class Problem2 {
         }
         StringBuilder sb = new StringBuilder();
         sb.append(cryptogram.charAt(0));
+        boolean isDuplicated = false;
 
+        for (int i = 1; i < cryptogram.length(); i++) {
+            char now = cryptogram.charAt(i);
+            char prev = cryptogram.charAt(i - 1);
+            if (now == prev) {
+                sb.deleteCharAt(sb.length()-1);
+                isDuplicated = true;
+                continue;
+            }
+            sb.append(now);
+        }
+        if(!isDuplicated){
+            return cryptogram;
+        }
         return solution(sb.toString());
     }
 }
