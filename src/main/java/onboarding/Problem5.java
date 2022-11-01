c126efc4617e29c89251b490d313c2353b16f7e8
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 15:14:43 +0900

    FEAT computeAnswer

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] cashUnits = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        List<Integer> answer = computeAnswer(cashUnits, money);
         return answer;
+    }
+
+    private static List<Integer> computeAnswer(int[] cashUnits, int money) {
+        List<Integer> ret = new ArrayList<>();
+        int rest = money;
+
+        for (int unit : cashUnits) {
+            ret.add(rest / unit);
+            rest = rest % unit;
+        }
+
+        return ret;
     }
 }
