a2bc0bc47862da1af9db78ac1ddc958aa396bc16
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-01 15:43:26 +0900

    Refactor: Problem2 code refactoring - rename function & variable
    기존 solution 함수에 전부 구현해두었던 기능을
    findLastDuplicatedIndex, checkIfStringTypeAA 함수로 구분하였음.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,58 +4,45 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
-        int i=0;
-        while (i != cryptogram.length()-1) {
-            if (cryptogram.length() == 0) break;
+        int i = 0;
+        while (i != cryptogram.length()-1) { // until we check final character of cryptogram
+            if (cryptogram.length() == 0) break; // if the cryptogram = '' than stop checking
 
-            for (i = 0; i < cryptogram.length()-1; i++) {
-                char start = cryptogram.charAt(i);
-                int last = i;
-                while(start == cryptogram.charAt(last)){
-                    last += 1;
-                    if (last>=cryptogram.length()) break;
-                }
-                if (i+1==last) cryptogram = cryptogram.substring(0, i+1) + cryptogram.substring(last);
+            for (i = 0; i < cryptogram.length()-1; i++) { // check string from first to last
+
+                int lastIndex = findLastDuplicatedIndex(cryptogram, i);
+
+                // remove duplication from string
+                if (i+1 == lastIndex)
+                    cryptogram = cryptogram.substring(0, i+1) + cryptogram.substring(lastIndex);
                 else {
-                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(last);
+                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(lastIndex);
                     i = 0;
                 }
-
-                /**
-                 *  abcsss 0,0 0
-                 * a bcsss 0,1 1
-                 * ab csss 0,2 2
-                 * abc sss 0,3
-                 */
-
-
-//                if (target == cryptogram.charAt(i+1)){
-//                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i+2);
-//                    i = 0;
-//                    break;
-//                }
             }
         }
 
-        if (cryptogram.length()==2 && cryptogram.charAt(0)==cryptogram.charAt(1)) {
-            cryptogram = "";
+        answer = checkIfStringTypeAA(cryptogram);
+        return answer;
+    }
+
+    public static int findLastDuplicatedIndex(String testString, int startIndex){
+
+        char standardChar = testString.charAt(startIndex);
+        int lastIndex = startIndex;
+
+        while(standardChar == testString.charAt(lastIndex)){
+            lastIndex += 1;
+            if (lastIndex>=testString.length()) break;
         }
 
-        return cryptogram;
+        return lastIndex;
+    }
+    public static String checkIfStringTypeAA(String testString){
+        // type AA means length==2 && same character
+        if (testString.length()==2 && testString.charAt(0)==testString.charAt(1))
+            testString = "";
+        return testString;
     }
 
 }
-
-
-//    int i=0;
-//        while (i != cryptogram.length()-1) {
-//                if (cryptogram.length() == 0) break;
-//                for (i = 0; i < cryptogram.length()-1; i++) {
-//        char target = cryptogram.charAt(i);
-//        if (target == cryptogram.charAt(i+1)){
-//        cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i+2);
-//        i = 0;
-//        break;
-//        }
-//        }
-//        }
