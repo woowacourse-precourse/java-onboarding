c5ca3c88ba11369cadebe4f4243f3082990b92fc
June1010 <tjrwns1010@naver.com>
2022-11-01 23:27:06 +0900

    chore: 기능 목록 커밋 (코드 내 주석 참고)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,14 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    // 액수 단위를 저장하는 리스트 추가하기
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        // 정답으로 반환할 단위 액수만큼의 리스트 선언하기
+        // 그리디하게 액수 단위를 순회하면서 각 단위 동전의 갯수를 구해주기
+        // 반환
+        return new ArrayList<>();
     }
 }
