ef9effc4f1d7f26fbdc048650297308801353b17
hyee0715 <hyee0715@naver.com>
2022-11-01 17:01:46 +0900

    feat(Problem6.java): 검사할 닉네임을 두 글자로 분리하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static HashMap<String, ArrayList<String>> findDuplicateNickname(String nickname, List<List<String>> forms, HashMap<String, ArrayList<String>> map) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String partialNickname = getPartialNicknameToValidate(i, nickname);
+        }
+
+        return map;
+    }
+
+    private static String getPartialNicknameToValidate(int idx, String nickname) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = idx; i < idx + 2; i++) {
+            sb.append(nickname.charAt(i));
+        }
+
+        String partialNicknameToValidate = sb.toString();
+        return partialNicknameToValidate;
     }
 }
