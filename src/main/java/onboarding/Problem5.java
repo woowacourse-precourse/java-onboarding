585aa7469ff71bc52c4733cb8f14dfbba35a8aa4
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 18:26:09 +0900

    돈과 지폐를 입력받고 돈을 지폐로 나눈 몫을 리턴한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static int calculateQuotient(int money, int bill) {
+        return money / bill;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
