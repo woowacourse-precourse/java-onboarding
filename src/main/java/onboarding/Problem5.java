80ee6778d2b16007af4eff5d7bdf5eabdf433e8c
taejeong <sontaewjd@nate.com>
2022-11-01 19:42:09 +0900

    [Feat] 각각의 단위가 몇개 필요한지 배열에 값 대입

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,11 @@ public class Problem5 {
     static int[] moneyUnit = {50000,10000,5000,1000,500,100,50,10,1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Arrays.asList(0,0,0,0,0,0,0,0,0);
+        for (int i = 0; i < 9; i++)
+        {
+            answer.set(i, money / moneyUnit[i]);
+            money %= moneyUnit[i];
+        }
         return answer;
     }
 }
