56c9f85e2bfa75321d58cbed5fe7d325001213f7
seungjun <seungjun123>
2022-11-01 13:39:33 +0900

    금액이 큰 금액의 화폐 위주로 돈을 받는 함수 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,6 +12,17 @@ public class Problem5 {
         List<Integer> coin = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
         List<Integer> cnt = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0));
 
+        answer = withdraw(coin, cnt, money);
         return answer;
+    }
+
+    private static List<Integer> withdraw(List<Integer> coin, List<Integer> cnt, int money) {
+        for (int i=0; i<coin.size(); i++) {
+            if (money >= coin.get(i)) {
+                cnt.set(i, money/coin.get(i));
+                money %= coin.get(i);
+            }
+        }
+        return cnt;
     }
 }
