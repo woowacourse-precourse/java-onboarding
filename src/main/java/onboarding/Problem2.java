91d301cd1abd060c8c796ea854685ce5df098478
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-11-01 14:57:24 +0900

    같은 문자를 삭제하는 deleteChar 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,6 +11,28 @@ public class Problem2 {
         }
         return false;
     }
+    //같은 문자 삭제
+    public static String deleteChar(String cryptogram) {
+        int n = cryptogram.length();
+        char[] chars = cryptogram.toCharArray();
+        for (int i = 1; i < n; i++) {
+            if (chars[i] == chars[i - 1]) {
+                chars[i - 1] = 'X';
+                for (int j = i+1; j < n; j++) {
+                    if (chars[i] != chars[j]) break;
+                    if (chars[i] == chars[j]) chars[j] = 'X';
+                }
+                chars[i] = 'X';
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (chars[i] != 'X') {
+                sb.append(chars[i]);
+            }
+        }
+        return sb.toString();
+    }
 
 }
 
