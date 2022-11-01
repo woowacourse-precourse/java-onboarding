0716ce90b0efa080449cee78dd31b17f7cf996e7
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-11-01 20:46:41 +0900

    add function : get nicknames from forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+
+    public static List<String> getName(List<List<String>> forms) {
+        List<String> name = new ArrayList<>();
+        for (List<String> form : forms) {
+            name.add(form.get(1));
+        }
+        return name;
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<String> name = getName(forms);
+
         return answer;
     }
 }
