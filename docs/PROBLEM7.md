56f904c8c66cc893d3135f588981594fa899a4f1
WonSeok-dd <cluejws@gmail.com>
2022-11-01 14:11:22 +0900

    Problem7 | docs: 기능 정의

--- docs/PROBLEM7.md
+++ docs/PROBLEM7.md
@@ -1,3 +1,16 @@
+## ❗기능 정의
+
+1. user의 친구들 얻기
+2. user친구의 친구들 얻기
+   + Key: user친구, Value: user친구의 친구들
+3. user친구의 친구들 순회 하면서 friendScore 합산
+   + 단 user친구는 friendScore에 포함하지 않는다.
+4. visitors 순회 하면서 visitorScore 합산
+   + 단 user친구는 visitorScore에 포함하지 않는다.
+5. friendScore, visitorScore 병합
+6. Score 내림차순 정렬 후 이름 오름차순 정렬
+   + 최대 5명 return
+
 ## 🚀 기능 요구 사항
 
 레벨 2의 팀 프로젝트 미션으로 SNS(Social Networking Service)를 만들고자 하는 팀이 있다. 팀에 속한 크루 중 평소 알고리즘에 관심이 많은 미스터코는 친구 추천 알고리즘을 구현하고자 아래와 같은 규칙을 세웠다.
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,22 +1,8 @@
 package onboarding;
 
-
 import java.util.*;
 import java.util.Map.Entry;
 
-/**
- * 기능 구현 목록
- * 1. user의 친구들 얻기
- * 2. user친구의 친구들 얻기.
- *  2.1 Key: user친구, Value: user친구의 친구들
- * 3. user친구의 친구들 순회 하면서 friendScore 합산
- *  3.1 단 user친구는 friendScore에 포함하지 않는다.
- * 4. visitors 순회 하면서 visitorScore 합산
- *  4.1 단 user친구는 visitorScore에 포함하지 않는다.
- * 5. friendScore, visitorScore 병합
- * 6. Score 내림차순 정렬 후 이름 오름차순 정렬
- *  6.1 최대 5명 return
- */
 public class Problem7 {
 
     private static List<String> getFriends(String user, List<List<String>> friends){
