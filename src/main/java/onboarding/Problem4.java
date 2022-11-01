979bd86af1b985e47d35b9f1e88d6918d927dbda
Yh0sun <hy770819@naver.com>
2022-11-01 15:29:40 +0900

    problem4:func list

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        //1. word 한 글자씩 읽음.
+        //  2. 알파벳이 소문자이면(아스키 코드 97~121) 122-(알파벳-97)
+        //  3. 알파벳이 대문자이면(아스키 코드 65~90) 90-(알파벳-65)
+        //  4. 공백일 때는 answer에 공백 넣어주고 continue
+
         return answer;
     }
 }
