0be476222534047ba0f7fffec44732efbbc5915d
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 22:18:10 +0900

    feat:Problem7 사용자 제외한 모든 친구들 HashMap에 값 0으로 올리는 기능 inputHash()추가.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,10 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.ArrayList;
 
 public class Problem7 {
 
@@ -24,63 +24,8 @@ public class Problem7 {
         }
         return friendsRelation;
     }
-
-    public static HashMap<String, Integer> scoreVisitFriend (HashMap<String, Integer> friendsRelation, List<String> visitors) {
-        for (String visitor : visitors) {
-            if (!friendsRelation.containsKey(visitor)){
-                friendsRelation.put(visitor, 1);
-            }else {
-                friendsRelation.put(visitor, friendsRelation.get(visitor)+1);
-            }
-        }
-        return friendsRelation;
-    }
-
-    public static List<String> sortScore (HashMap<String, Integer> friendsRelation, List<String> yesFriends) {
-        List<String> keySetList = new ArrayList<>(friendsRelation.keySet());
-        Collections.sort(keySetList);
-        Collections.sort(keySetList, ((o1, o2) -> friendsRelation.get(o2).compareTo(friendsRelation.get(o1))));
-
-        List<String> answerList = new ArrayList<>();
-        for (String key: keySetList) {
-            if (friendsRelation.get(key) == 0) {
-                break;
-            }else if (yesFriends.contains(key)) {
-                continue;
-            }else {
-                answerList.add(key);
-            }
-        }
-
-        return answerList;
-    }
-
-
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        HashMap<String, Integer> friendsRelation = inputHash(friends);
-        HashMap<String, Integer> friendsScore = scoreVisitFriend(friendsRelation, visitors);
-        List<String> yesFriends = new ArrayList<>();
-        List<List<String>> noFriends = new ArrayList<>();
-
-        for (List<String> twoFriends : friends) {
-            if (twoFriends.get(0).equals(user)) {
-                yesFriends.add(twoFriends.get(1));
-            } else if (twoFriends.get(1).equals(user)) {
-                yesFriends.add(twoFriends.get(0));
-            } else {
-                noFriends.add(twoFriends);
-            }
-        }
-
-        for (List<String> twoNoFriends : noFriends) {
-            if (yesFriends.contains(twoNoFriends.get(0))) {
-                friendsScore.put(twoNoFriends.get(1), friendsScore.get(twoNoFriends.get(1))+10);
-            }else if (yesFriends.contains(twoNoFriends.get(1))) {
-                friendsScore.put(twoNoFriends.get(0), friendsScore.get(twoNoFriends.get(0))+10);
-            }
-        }
-
-        List<String> answerList = sortScore(friendsScore, yesFriends);
-        return answerList;
+        List<String> answer = Collections.emptyList();
+        return answer;
     }
 }
