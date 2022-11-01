dfb50c2f2edf4044cc2d6aca801c2a6f2c39bacf
SproutMJ <mjk54565@gmail.com>
2022-11-01 21:20:55 +0900

    feat: 문제 5 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,20 +1,23 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = makeCharge(money);
         return answer;
     }
 
-    static int[] makeCharge(int money){
+    //금액을 입력받으면 거스름돈을 계산하는 기능
+    static List<Integer> makeCharge(int money){
         int[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        int[] result = new int[9];
+        List<Integer> result = new LinkedList<>();
 
         for (int i = 0; i<9; i++){
-            result[i] = money/arr[i];
+            result.add(money/arr[i]);
             money %= arr[i];
         }
 
