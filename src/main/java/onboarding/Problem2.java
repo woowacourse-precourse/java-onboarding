b41440d6af0168fade3229a02078170679bd9435
JasmineAir <gofoxfox@naver.com>
2022-11-01 15:58:08 +0900

    Prob2 Solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        boolean flag = true;
+        int count = 0;
+
+        while(flag==true){
+            int num[] = new int [cryptogram.length()];
+            flag = false;
+
+            for(int i=0; i<cryptogram.length()-1; i++){
+                if((cryptogram.charAt(i)==cryptogram.charAt(i+1))){
+                    num[i]++;
+                    num[i+1]++;
+                    flag = true;
+                }
+            }
+            for(int i=0; i<cryptogram.length(); i++){
+                if(num[i]==0) answer+=cryptogram.charAt(i);
+            }
+
+            cryptogram = answer.substring(0,answer.length());
+            answer = "";
+            System.out.println(cryptogram);
+        }
+
         return answer;
     }
+
+    public static void main (String [] args) {
+
+        String cryptogram = "browoanoommnaon";
+
+        Problem2 prob = new Problem2();
+        System.out.println(prob.solution(cryptogram));
+    }
+
 }
