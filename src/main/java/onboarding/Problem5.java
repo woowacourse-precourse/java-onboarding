4bb841e0db3e8df37d9ced60dd13edc38c1bd4f0
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-11-01 23:43:09 +0900

    Add: problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,56 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer =new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0));
+
+        if(money / 50000 > 0){
+            answer.set(0, money / 50000);
+            money %= 50000;
+        }
+
+        if(money / 10000 > 0){
+            answer.set(1, money / 10000);
+            money %= 10000;
+        }
+
+        if(money / 5000 > 0){
+            answer.set(2, money / 5000);
+            money %= 5000;
+        }
+
+        if(money / 1000 > 0){
+            answer.set(3, money / 1000);
+            money %= 1000;
+        }
+
+        if(money / 500 > 0){
+            answer.set(4, money / 500);
+            money %= 500;
+        }
+
+        if(money / 100 > 0){
+            answer.set(5, money / 100);
+            money %= 100;
+        }
+
+        if(money / 50 > 0){
+            answer.set(6, money / 50);
+            money %= 50;
+        }
+
+        if(money / 10 > 0){
+            answer.set(7, money / 10);
+            money %= 10;
+        }
+
+        answer.set(8, money);
         return answer;
     }
 }
+
