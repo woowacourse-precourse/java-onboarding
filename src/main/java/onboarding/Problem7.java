6df416ec3c69329cb9c944a21bfe1a4bb41f8fcf
choiejae <ejae8319@gmail.com>
2022-11-01 18:00:37 +0900

    feat (validityChecker): 예외 처리 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 class Person implements Comparable<Person> {
 
@@ -63,8 +64,76 @@ class Person implements Comparable<Person> {
 
 }
 public class Problem7 {
+    private static final int VISITORS_MIN_SIZE = 0;
+    private static final int VISITORS_MAX_SIZE = 10000;
+    private static final int FRIENDS_RELATION_MIN_SIZE = 1;
+    private static final int FRIENDS_RELATION_MAX_SIZE = 10000;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        vaildityChecker(user, friends, visitors);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static void vaildityChecker(String user, List<List<String>> friends,
+        List<String> visitors) {
+        userChecker(user);
+        friendsChecker(friends);
+        visitorsChecker(visitors);
+    }
+    private static void userChecker(String user) {
+        checkLength(user, user.length());
+        checkConsistOfLowerCase(user);
+    }
+    private static void checkLength(String type, int length) {
+        if (length < 1 || 30 < length) {
+            throw new IllegalArgumentException(type+"는 길이가 1 이상 30 이하인 문자열이어야 합니다.");
+        }
+    }
+
+    private static void friendsChecker(List<List<String>> friends) {
+        checkSize("friendRelation", friends.size(), FRIENDS_RELATION_MIN_SIZE, FRIENDS_RELATION_MAX_SIZE);
+        checkFriendRelation(friends);
+        for (List<String> friendRelation : friends) {
+            checkElements(friendRelation);
+            checkOverlapFriendRelation(friendRelation);
+        }
+    }
+    private static void checkOverlapFriendRelation(List<String> friends) {
+        String leftUser = friends.get(0);
+        String rightUser = friends.get(1);
+
+        if (Pattern.matches(leftUser, rightUser)) {
+            throw new IllegalArgumentException("동일한 친구 관계가 중복해서 주어지면 안됩니다.");
+        }
+
+    }
+    private static void checkSize(String type, int size, int minSize, int maxSize) {
+        if (size < minSize || maxSize < size) {
+            throw new IllegalArgumentException(type+"는 길이가 1 이상 10,000 이하인 리스트/배열이어야 합니다.");
+        }
+    }
+    private static void checkFriendRelation(List<List<String>> friends) {
+        for (List<String> friendRelation : friends) {
+            String leftUser = friendRelation.get(0);
+            String rightUser = friendRelation.get(1);
+
+            checkLength("friendsElement", leftUser.length());
+            checkLength("friendsElement", rightUser.length());
+        }
+    }
+    private static void visitorsChecker(List<String> visitors) {
+        checkSize("visitors", visitors.size(), VISITORS_MIN_SIZE, VISITORS_MAX_SIZE);
+        checkElements(visitors);
+    }
+    private static void checkElements(List<String> names) {
+        for (String name : names) {
+            checkConsistOfLowerCase(name);
+        }
+
+    }
+    private static void checkConsistOfLowerCase(String name) {
+        if (!Pattern.matches("^[a-z]*$", name)) {
+            throw new IllegalArgumentException("사용자 아이디는 알파벳 소문자로만 이루어져 있어야 합니다.");
+        }
     }
 }
