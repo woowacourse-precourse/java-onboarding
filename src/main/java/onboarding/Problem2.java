9bdc184e8537ec0636e2b6f841cf5ef51f3fcfd1
seheonCho <whtpgjs94@gmail.com>
2022-11-01 20:27:33 +0900

    feat: [문제2] 암호화 된 문자 문자 배열로 변환및 한 문자씩 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decrypt(cryptogram);
     }
+
+    private static String decrypt(String cryptogram) {
+        char[] cryptogramChars = cryptogram.toCharArray();
+
+        for (int i = 1; i < cryptogramChars.length; i++) {
+
+        }
+
+        return cryptogram;
+    }
+
 }
