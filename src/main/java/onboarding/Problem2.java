876cca921101ac1d498605fccbcd291da6671aff
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-01 17:09:52 +0900

    [Problem2] 연속문자 제거횟수 최악 횟수 가정 최소 len/2까지 횟수 조정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,6 +7,11 @@ public class Problem2 {
         int len = cryptogram.length();
         // 정규표현식으로 연속된 문자 제거
         answer = cryptogram.replaceAll("(.)\\1+", "");
+
+        // 최악을 가정하고 최소 횟수는 절반까지
+        for (int i = 0; i < len/2; i++)  {
+            answer = answer.replaceAll("(.)\\1+", "");
+        }
         return answer;
     }
 }
