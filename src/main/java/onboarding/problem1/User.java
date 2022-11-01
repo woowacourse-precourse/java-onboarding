c505771b1db4b9e3d8f4e8394daa61c468227983
seominseok00 <seominseok4834@gmail.com>
2022-11-01 22:46:15 +0900

    feat(문제1): 예외처리 메소드 구현

--- src/main/java/onboarding/problem1/User.java
+++ src/main/java/onboarding/problem1/User.java
@@ -2,10 +2,24 @@ package onboarding.problem1;
 
 import java.util.List;
 
+class PageException extends Exception{
+
+}
+
 public class User {
     private List<Integer> pages;
 
     public User(List<Integer> pages){
         this.pages = pages;
+    }
+
+    public void checkPageValidation() throws PageException{
+        if(pages.size() !=2){
+            throw new PageException();
+        }
+
+        if(pages.get(0) != pages.get(1)-1){
+            throw new PageException();
+        }
     }
 }
