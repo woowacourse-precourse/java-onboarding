b97c5f12c36f6078607224f8f741ec9ce48ea9da
chanminmaxscaler <jcm04@naver.com>
2022-11-01 21:06:37 +0900

    feat(UserAndScore): UserAndScore 클래스의 멤버변수와 생성자를 구현한다.
    UserAndScore 클래스는 User newFriendUser와 int score를 멤버변수로 갖는 클래스이다.
    다중조건 정렬 시 활용된다.

---
+++ src/main/java/onboarding/problem7/wrapper/UserAndScore.java
@@ -0,0 +1,12 @@
+package onboarding.problem7.wrapper;
+
+public class UserAndScore {
+
+    private final User newFriendUser;
+    private final int score;
+
+    public UserAndScore(User newFriendUser, int score) {
+        this.newFriendUser = newFriendUser;
+        this.score = score;
+    }
+}
