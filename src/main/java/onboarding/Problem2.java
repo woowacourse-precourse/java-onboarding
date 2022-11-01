75cf7699e94632be72eb333054a75ff5475af41a
MAYY27 <117031840+MAYY27@users.noreply.github.com>
2022-11-01 15:24:52 +0900

    Update Problem2.java

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        boolean isChange = true;
+        String newCytogram = cryptogram;
+
+        while (isChange) {
+            isChange = false;
+            char[] charArray = newCytogram.toCharArray();
+            char overlapChar = ' ';
+            for(int i = 0; i < charArray.length; i++) {
+
+                if(charArray[i] == overlapChar) {
+                    charArray[i] = '_';
+                    charArray[i - 1] = '_';
+                    isChange = true;
+                }
+
+                else overlapChar = charArray[i];
+            }
+
+            newCytogram = String.valueOf(charArray).replace("_", "");
+            System.out.println(newCytogram);
+        }
+        return newCytogram;
     }
 }
