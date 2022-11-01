4e4ae4318dfb65d47e4df294fa2c16bb4c513d9d
Jinhui <jda0526@naver.com>
2022-11-01 21:48:34 +0900

    Feat(Problem6): [F-01], [F-02]  닉네임 중 같은 글자가 연속적으로 포함되는 닉네임을 찾고, result 변수에 이메일을 오름차순 정렬, 중복 제거하여 담는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,38 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Map<String, String> seperatedNicknameMap = new HashMap<>();
+        Set<String> emails = new HashSet<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            makeMap(seperatedNicknameMap, emails, email, nickname);
+        }
+
+        List<String> emailsList = new ArrayList<>(emails);
+        return emailsList;
+    }
+
+    private static void makeMap(Map<String, String> map, Set<String> emails, String email, String nickname) {
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            for (int j = 1; j < nickname.length(); j++) {
+                String seperatedNickname = nickname.substring(i, j+1); // 닉네임 분리
+
+                if(!map.containsKey(seperatedNickname) ){ // 분리된 닉네임이 같은게 없고,
+                    if(seperatedNickname.length() > 1){ // 분리된 닉네임이 2글자 이상이면
+                        map.put(seperatedNickname, email); // map에 분리된 닉네임과 이메일 넣기
+                    }
+                }
+                else{
+                    emails.add(map.get(seperatedNickname)); // map에 있는 닉네임의 사람의 이메일 추가
+                    emails.add(email); // 새로 중복된 닉네임을 가진 사람의 이메일 추가
+//                    System.out.println(seperatedNickname + " : " + emails);
+                }
+            }
+        }
     }
 }
