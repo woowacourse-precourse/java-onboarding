3f5eeaec362df0018f1510d74013c08a9de15bb6
Eungyu-code <Agyu625@gmail.com>
2022-11-01 13:14:35 +0900

    user의 친구와 친구인지 체크하는 함수 findUserFriend() 함수 + 점수가 0점 이상이 아닌 추천 친구와 그 점수를 recommendList에 추가 및 수정해주는 함수 putRecommendList() 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -11,6 +11,22 @@ public class Problem7 {
         List<String> userFriends = createUserFriend(user, friends);
         Map<String, Integer> recommendList = new HashMap<>();
 
+        for (int i = 0; i < friends.size(); i++) {
+            String friendA = friends.get(i).get(0);
+            String friendB = friends.get(i).get(1);
+
+            if (findUserFriend(friendA, userFriends)) {
+                putRecommendList(friendB, recommendList, 10);
+            }
+            if (findUserFriend(friendB, userFriends)) {
+                putRecommendList(friendA, recommendList, 10);
+            }
+        }
+
+        for (int i = 0; i < visitors.size(); i++) {
+            putRecommendList(visitors.get(i), recommendList, 1);
+        }
+
         List<String> answer = new ArrayList<>();
         return answer;
     }
@@ -29,4 +45,31 @@ public class Problem7 {
 
     }
 
+    public static boolean findUserFriend(String friend, List<String> userFriends) {
+
+        for (int i = 0; i < userFriends.size(); i++) {
+            if (userFriends.get(i).equals(friend)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void putRecommendList(String friend, Map<String, Integer> recommendList, int point) {
+
+        Iterator<String> iterator = recommendList.keySet().iterator();
+        boolean isExist = false;
+
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(friend)) {
+                int recommendPoint = recommendList.get(friend);
+                recommendList.replace(friend, recommendPoint + point);
+                isExist = true;
+                break;
+            }
+        }
+
+        if (!isExist) recommendList.put(friend, point);
+    }
 }
