f47dd27e21f64ef91c21f52f644aa690a3fd8708
ingpyo <juninpyo72@gmail.com>
2022-11-01 12:46:03 +0900

    [problem7]예외 처리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,25 +9,45 @@ import java.util.Map;
 
 public class Problem7 {
 	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		checkList(user, friends, visitors);
 
-		List<String> userfriends = new ArrayList<>();
-		Map<String, Integer> map = new HashMap<>();
-		List<String> answer = new ArrayList<>();
-
-		List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
-		entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
-			@Override
-			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-				return o2.getValue() - o1.getValue();
-			}
-		});
-		for (String name : map.keySet()) {
-			if (answer.size() <= 5) {
-				answer.add(name);
-			}
-		}
-		return answer;
 	}
 
-
+	public static void checkList(String user, List<List<String>> friends, List<String> visitors) {
+		if (user.length() < 1 || user.length() > 30) {
+			throw new IllegalArgumentException("아이디 길이가 부합하지 않습니다.");
+		}
+		if (!user.matches("^[a-z]*$")) {
+			throw new IllegalArgumentException("아이디가 알파벳 소문자로 이뤄지지 않았습니다.");
+		}
+		if (friends.size() < 1 || friends.size() > 10000) {
+			throw new IllegalArgumentException("friends는 길이가 부합하지 않습니다.");
+		}
+		for (List<String> list : friends) {
+			String a = list.get(0), b = list.get(1);
+			if (list.size() != 2) {
+				throw new IllegalArgumentException("friends의 길이가 부합하지 않습니다.");
+			}
+			if (a.length() < 1 || a.length() > 30 && b.length() < 1 || b.length() > 30) {
+				throw new IllegalArgumentException("아이디 길이가 부합하지 않습니다.");
+			}
+			if (!a.matches("^[a-z]*$") && !b.matches("^[a-z]*$")) {
+				throw new IllegalArgumentException("아이디가 알파벳 소문자로 이뤄지지 않았습니다.");
+			}
+			if (a == b) {
+				throw new IllegalArgumentException("동일한 친구 관계입니다.");
+			}
+		}
+		if (visitors.size() < 1 || visitors.size() > 10000) {
+			throw new IllegalArgumentException("visitors는 길이의 부합하지 않습니다.");
+		}
+		for (String list : visitors) {
+			if (list.length() < 1 || list.length() > 30) {
+				throw new IllegalArgumentException("아이디 길이가 부합하지 않습니다.");
+			}
+			if (!list.matches("^[a-z]*$")) {
+				throw new IllegalArgumentException("아이디가 알파벳 소문자로 이뤄지지 않았습니다.");
+			}
+		}
+	}
 }
