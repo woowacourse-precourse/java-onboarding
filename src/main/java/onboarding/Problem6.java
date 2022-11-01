4800895d4c60b7e3d525c9b24c77dd2ba12c3de6
김승규 <rlatmdrb1996@gmail.com>
2022-11-01 21:29:11 +0900

    feat(problem6): 주어진 단어들을 닉네임에 포함하고 있는 회원의 이메일을 중복을 제거하여 반환하는 메소드 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,8 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 문제 6.
@@ -12,9 +14,13 @@ import java.util.List;
  */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        Set<String> answer = new java.util.HashSet<>();
 
-        return answer;
+        for (int i = 0; i < forms.size(); ++i) {
+            answer.addAll(getEmailList(forms, i));
+        }
+
+        return new ArrayList<>(answer);
     }
 
     /**
@@ -27,5 +33,32 @@ public class Problem6 {
             twoConsecutiveWords.add(nickname.substring(i, i + 2));
         }
         return twoConsecutiveWords;
+    }
+
+    /**
+     * 주어진 단어들을 닉네임에 포함하고 있는 회원의 이메일을 중복을 제거하여 반환하는 메소드
+     *
+     * @param forms 모든 회원의 닉네임과 이메일이 담긴 리스트
+     * @param index 현재 회원의 인덱스
+     * @return 중복이 제거된 이메일 목록
+     */
+    private static Set<String> getEmailList(List<List<String>> forms, int index) {
+        Set<String> emailList = new java.util.HashSet<>();
+        String nickname = forms.get(index).get(1);
+        List<String> twoConsecutiveWords = getTwoConsecutiveWords(nickname);
+
+        for (int i = 0; i < forms.size(); ++i) {
+            if (i == index) {
+                continue;
+            }
+            for (String word : twoConsecutiveWords) {
+                if (forms.get(i).get(1).contains(word)) {
+                    emailList.add(forms.get(i).get(0));
+                    break;
+                }
+            }
+        }
+
+        return emailList;
     }
 }
