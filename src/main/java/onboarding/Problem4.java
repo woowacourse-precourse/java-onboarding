77c4e448b02dd64a9c144f6bb280e9c0969dc2f0
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-01 18:59:09 +0900

    test4 - 대소문자 구별하여 변환하기

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,10 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int temp = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<word.length(); i++) {
+            temp = ascii(word.charAt(i));
+
+            if(temp!=0)
+                sb.append(String.valueOf((char)temp));
+            else
+                sb.append(word.charAt(i));
+        }
+
+        answer = sb.toString();
+
         return answer;
     }
 
-    // 대소문자 구별
+    // 대소문자 구별하여 변환
     public static int ascii(char ch) {
         int asc = 0;
 
@@ -14,7 +28,12 @@ public class Problem4 {
             asc = 65;
         else if(Character.isLowerCase(ch))
             asc = 97;
+        else
+            return 0;
 
-        return asc;
+        int abs = Math.abs(asc - ch);
+        int reverse = (asc + 25) - abs;
+
+        return reverse;
     }
 }
