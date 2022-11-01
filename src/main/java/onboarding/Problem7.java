91a160034f98d26b5d595e3c26b9be4e60b9d255
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-01 10:55:55 +0900

    problem7 - addFriend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String, List> map = new HashMap<>();
+    static Map <String, Integer> point = new HashMap<>();
+    public static void addFriend(String friend1, String friend2) {
+        if(map.get(friend1)==null) {
+            List <String> list = new ArrayList<>();
+            list.add(friend2);
+            map.put(friend1, list);
+        }
+        else {
+            List <String> list = map.get(friend1);
+            list.add(friend2);
+            map.put(friend1, list);
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        for(int i=0; i<friends.size(); i++) {
+            String friend1 = friends.get(i).get(0);
+            String friend2 = friends.get(i).get(1);
+            addFriend(friend1, friend2);
+            addFriend(friend2, friend1);
+        }
+        addFriendPoint(user);
+        addVisitorPoint(visitors);
+        excludeFriend(user);
+        List<Map.Entry<String, Integer>> resultmap = sortByKeyAndValue();
+        List <String> answer = convertResultMapToList(resultmap);
+
         return answer;
     }
 }
+
