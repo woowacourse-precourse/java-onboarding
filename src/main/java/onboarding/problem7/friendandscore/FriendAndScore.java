f3c1195cb0e1b3ae2b6f040b45a4524fe090608a
chanminmaxscaler <jcm04@naver.com>
2022-11-01 21:15:06 +0900

    feat(FriendAndScore): FriendAndScore 클래스의 transformMapIntoList 메소드를 구현한다.
    transformMapIntoList(Map<User, Integer> newFriendAndScore) 메소드는 Map<User, Integer> newFriendAndScore를 입력하면, List<UserAndScore> newFriendAndScoreList로 변환해 반환한다.

--- src/main/java/onboarding/problem7/friendandscore/FriendAndScore.java
+++ src/main/java/onboarding/problem7/friendandscore/FriendAndScore.java
@@ -3,11 +3,9 @@ package onboarding.problem7.friendandscore;
 import onboarding.problem7.collections.FriendList;
 import onboarding.problem7.collections.VisitorList;
 import onboarding.problem7.wrapper.User;
+import onboarding.problem7.wrapper.UserAndScore;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class FriendAndScore {
 
@@ -39,11 +37,20 @@ public class FriendAndScore {
         set.addAll(visitorsOfNotFriendOfUser);
         return List.copyOf(set);
     }
+
     public Map<User, Integer> mapNewFriendAndScore(List<User> friendsOfUserFriends, List<User> visitorsOfNotFriendOfUser) {
         List<User> newFriend = addTwoListsWithoutDuplicate(friendsOfUserFriends, visitorsOfNotFriendOfUser);
         FriendAndScoreMapper newFriendAndScoreMapper = new FriendAndScoreMapper(newFriend);
         newFriendAndScoreMapper.calculateScoreOfNewFriendWith(friendsOfUserFriends, FRIENDS_OF_FRIEND_POINT);
         newFriendAndScoreMapper.calculateScoreOfNewFriendWith(visitorsOfNotFriendOfUser, VISITORS_POINT);
         return newFriendAndScoreMapper.getFriendAndScore();
+    }
+
+    public List<UserAndScore> transformMapIntoList(Map<User, Integer> newFriendAndScore) {
+        List<UserAndScore> newFriendAndScoreList = new ArrayList<>();
+        for (Map.Entry<User, Integer> entry : newFriendAndScore.entrySet()) {
+            newFriendAndScoreList.add(new UserAndScore(entry.getKey(), entry.getValue()));
+        }
+        return newFriendAndScoreList;
     }
 }
