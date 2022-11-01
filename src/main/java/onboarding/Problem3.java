adfb6d3fa905def42fc1921c5554170d08a67290
Frog-Slayer <Frog-Slayer@github.com>
2022-11-01 13:37:41 +0900

    Problem 3 Refactoring

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,12 +4,17 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         for (Integer i = 1; i <= number; i++){
-            String iToString = i.toString();
-            for (int j = 0; j < iToString.length(); j++){
-                if (iToString.charAt(j) - '0' != 0 && iToString.charAt(j) % 3 == 0) answer += 1;
-            }
+            answer += get369Number(i.toString());
         }
-
         return answer;
     }
+    private static int get369Number(String numberString){
+        int numberOf369 = 0;
+        for (int i = 0; i < numberString.length(); i++){
+            int digit = numberString.charAt(i) - '0';
+            if (digit != 0 && digit % 3 == 0) numberOf369++;
+        }
+        return numberOf369;
+    }
+
 }
