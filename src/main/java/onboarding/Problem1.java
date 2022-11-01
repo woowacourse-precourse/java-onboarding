e568cd934070e770fe19ad391504578f8867c264
seungjun <seungjun123>
2022-11-01 12:26:46 +0900

    3가지 예외 처리 함수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -25,4 +25,16 @@ class Problem1 {
         }
         return sum;
     }
+
+    private static boolean check(List<Integer> list) {
+        if (list.get(0)%2 != 1 || list.get(1)%2 != 0) {
+            return false;
+        } else if (list.get(0) == 1 || list.get(1) == 400) {
+            return false;
+        } else if (list.get(0)+1 != list.get(1)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
