64d2baa1db95a9d0dc48a85697de76090f39f23e
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-01 20:46:39 +0900

    moneyConversion function

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,27 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem5 {
+
+    static List<Integer> answer = new ArrayList<Integer>();
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void moneyConversion(int money) {
+        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < moneyList.length; i++) {
+            int moneyNum = (money / moneyList[i]);
+
+            answer.add(moneyNum);
+
+            money -= moneyList[i] * moneyNum;
+        }
+
     }
 }
