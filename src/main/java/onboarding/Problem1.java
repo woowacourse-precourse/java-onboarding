8da6013db441607706ca9d26709f9680abec63b4
TeddyYeoni <po99027@naver.com>
2022-11-01 23:26:04 +0900

    PROBLEM1 기능2 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -19,4 +19,15 @@ class Problem1 {
 		}
 		return odd;
 	}
+	
+	public static int pageEven(int pageNumber) {
+		int even = 1;
+		String pageToString = Integer.toString(pageNumber);
+		int[] evenArray = new int[pageToString.length()];
+		for (int i = 0; i < evenArray.length; i++) {
+			evenArray[i] = pageToString.charAt(i) - '0';
+			even *= evenArray[i];
+		}
+		return even;
+	}
 }
