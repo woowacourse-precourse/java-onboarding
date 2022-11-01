ab066748782cc0a14012b01657d3f2a8515c5fd0
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 14:11:45 +0900

    feat:static value

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,14 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
+    private static int sum;
+    private static int Biggest;
+    private static final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        return answer;
+        return whoseBiggest(pobi, crong);
     }
 }
