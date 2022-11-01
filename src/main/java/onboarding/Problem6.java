9aa25716e748cb1dda562cace657f77103a20d8d
bini59 <myuni0234@gmail.com>
2022-11-01 14:17:54 +0900

    feat : Check duplicate
    check duplicate, add duplicated nickname to answer

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -14,11 +14,20 @@ public class Problem6 {
             for(int j = i+1; j < forms.size(); j++){
                 if (temp.get(j) == 1) continue;
                 for(int k = 0; k < forms.get(i).get(1).length()-1; k++){
-
+                    for(int l = 0; l < forms.get(j).get(1).length()-1; l++){
+                        if(forms.get(i).get(1).substring(k, k+2).compareTo(forms.get(j).get(1).substring(l, l+2)) == 0){
+                            temp.set(i, 1);
+                            temp.set(j, 1);
+                        }
+                    }
                 }
             }
         }
 
+
+        for(int i = 0; i < temp.size(); i++){
+            if (temp.get(i) == 1)  answer.add(forms.get(i).get(0));
+        }
 
 
         return answer;
