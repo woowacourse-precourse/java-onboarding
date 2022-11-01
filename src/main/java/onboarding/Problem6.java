40a181305c010061eb36b23423424e781d676373
SeongbinPark <binni0515@gmail.com>
2022-11-01 09:46:41 +0900

    [Problem6] 1. 입력 된 forms에서 이메일, 닉네임을 추출하는 메서드(getEmailFromCrew, getNicknameFromCrew)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        Map<String, Integer> tokenAndCountMap = new HashMap<>(); // K : token, V : count
+        List<Set<String>> allTokens = new ArrayList<>();
+
+        for (int i = 0; i < allTokens.size(); i++) {
+            Set<String> tokens = allTokens.get(i);
+            for (String token : tokens) {
+                if (tokenAndCountMap.get(token) > 1) {
+                    List<String> eachCrew = forms.get(i);
+                    String email = getEmailFromCrew(eachCrew);
+                    answer.add(email);
+                    break;
+                }
+            }
+        }
+
+        Collections.sort(answer);
         return answer;
     }
+
+    private static String getEmailFromCrew(List<String> eachCrew) {
+        return eachCrew.get(0);
+    }
+
+    private static String getNicknameFromCrew(List<String> eachCrew) {
+        return eachCrew.get(1);
+    }
+
 }
