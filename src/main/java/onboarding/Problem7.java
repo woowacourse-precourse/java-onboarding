9e53f498dacdcf7d3a5939d98f915750025aceb5
Gentlee <ekdma3259@gmail.com>
2022-11-01 19:39:31 +0900

    Problem7 - 사용자와 친구관계를 가진 친구들을 LinkedList에 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+    	List <String> friendList = new LinkedList<>();
+
+        for(List <String> friend : friends){
+            if(friend.get(0).equals(user)) {    
+            	friendList.add(friend.get(1));
+            }else if(friend.get(1).equals(user))  {  
+            	friendList.add(friend.get(0));
+            }
+        }
 }
