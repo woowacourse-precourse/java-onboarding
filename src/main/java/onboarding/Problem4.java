dcce9b0d3c6c6a893f5626729cf3b94f90d8705c
BoyCho <iijokv@naver.com>
2022-11-01 12:46:10 +0900

    feat : 문제4. 문자 변환 기능 및 풀이 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < word.length(); i++)
+            answer.append(change(word.charAt(i)));
+        return answer.toString();
     }
+    static char change(char c){
+        if (isSmallLetter(c)) return (char)('a' + 'z' - c);
+        if (isCapitalLetter(c)) return (char)('A' + 'Z' - c);
+        return c;
+    }
+    static boolean isSmallLetter (char c) { return 'a' <= c && c <= 'z'; }
+    static boolean isCapitalLetter (char c) { return 'A' <= c && c <= 'Z'; }
 }
