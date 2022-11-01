b7755b3b2c2f4f330a967360255a7d04f09009a1
heejeong <hjx66@naver.com>
2022-11-01 13:36:01 +0900

    refactor: result 변수명 변경 >> score로

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -24,7 +24,7 @@ public class Problem7 {
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
-        Map<String, Integer> result = new TreeMap<>(); //점수 계산용
+        Map<String, Integer> score = new TreeMap<>(); //점수 계산용
 
         //1. 사용자와 함께 아는 친구
         Map<String, Set<String>> relation = makeFriendRelation(friends);
@@ -33,19 +33,18 @@ public class Problem7 {
             Set<String> userfriends = relation.get(user);
             Set<String> target = relation.get(key);
             userfriends.retainAll(target);
-            result.put(key, userfriends.size() * 10);
+            score.put(key, userfriends.size() * 10);
         }
         //2. 사용자의 타임 라인에 방문한 횟수
         for(String visitor : visitors){
-            if(!result.containsKey(visitor)){
-                result.put(visitor,0);
+            if(!score.containsKey(visitor)){
+                score.put(visitor,0);
             }
-            result.put(visitor,result.get(visitor)+1);
+            score.put(visitor,score.get(visitor)+1);
         }
 
         //3. 점수가 가장 높은 순으로 정렬하여 최대 5명 return, 0점은 추천x
-        List<String> keySet = new ArrayList<>(result.keySet());
-
+        List<String> keySet = new ArrayList<>(score.keySet());
         //예외처리
         // - user와 이미 친구인 경우 제외
         // - user 제외
@@ -56,13 +55,13 @@ public class Problem7 {
             }
         }
         // Value 값으로 내림차순 정렬
-        keySet.sort((k1, k2) -> result.get(k2).compareTo(result.get(k1)));
+        keySet.sort((k1, k2) -> score.get(k2).compareTo(score.get(k1)));
 
         //최대 5명만 추출, 0점인 경우 제외
         int answerLength = Math.min(keySet.size(), 5);
         for(int i = 0; answer.size() < answerLength; i++){
             String key = keySet.get(i);
-            if( result.get(key)!=0){
+            if( score.get(key)!=0){
                 answer.add(key);
             }
         }
