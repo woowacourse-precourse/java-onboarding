840f082bff5ec484dafa32ca039aaaada41ee2a4
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:48:28 +0900

    Problem6 solution method
    Cross-checks each username and adds to a list if they have an overlap.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = i + 1; j < forms.size(); j++) {
+                if (compareString(forms.get(i).get(1), forms.get(j).get(1))) {
+                    answer.add(forms.get(i).get(0));
+                    answer.add(forms.get(j).get(0));
+                }
+            }
+        }
+        return removeDuplicates(answer);
     }
 }
