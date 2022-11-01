d925ebada0b6f7ea42638429ffb760845a692dc3
lynne921 <dkfls921@gmail.com>
2022-11-01 15:55:05 +0900

    [feat][problem2] 문장 내 중복 확인하는 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem2 {
-    private static boolean isDuplicate(char first, char second) {
-        return (first == second);
+    private static int isDuplicate(char first, char second) {
+        if (first == second)
+            return 1;
+        return 0;
+    }
+
+    private static boolean checkSentence(String sentence) {
+        int sum = 0;
+        for (int i = 0; i < sentence.length() - 1; i++) {
+            sum += isDuplicate(sentence.charAt(i), sentence.charAt(i+1));
+        }
+        return sum <= 0;
     }
 
     public static String solution(String cryptogram) {
