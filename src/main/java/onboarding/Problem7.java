16a1c681f37472a338a46d49a89006c8a3036956
kkh <kkho9654@naver.com>
2022-11-01 20:49:53 +0900

    Add feature list problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,28 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 변수 선언
+ *      - allUser     : 모든 user set
+ *      - friendSet   : user의 friend set
+ *      - recommScore : 친구 추천목록의 점수를 저장할 hash map
+ *      - noFriend    : 모든 user목록 에서 user의 친구가 아닌 사람들 set
+ *      - frFrineds   : 친구들의 친구들을 저장하는 List
+ * 변수 초기화
+ *      - friendSet 변수 초기화 : getFriendSet() 함수 사용
+ *      - allUser 변수 초기화 : 입력받은 friends List 를 순회하며 set에 추가
+ *      - noFriend 변수 초기화 : allUser set 에서 user 본인과 friendSet 을 remove 함수와 removeAll 함수로 제거
+ *      - frFriends 변수 초기화 : friendSet 을 순회하며 친구들의 친구들을 frFriends에 모두 저장
+ * 기능 목록
+ * 1. getFriendSet(List<List<String>> friends, String user)
+ *      - @param String user 에 해당하는 사람의 친구들 set 을 반환
+ * 2. addRecommScore(HashMap<String,Integer> recommScore,int addNum, String name)
+ *      - recommScore 에 name 에 해당하는 사람을 addNum 만큼 더한다.
+ * 3. 친구의 친구들 frFriends 를 순회하며 이 중 noFriend set 에 있는 사람의 score 를 recommScore 에 10 더한다
+ * 4. visitors 들을 순회하며 visitor 가 친구가 아닌경우 recommScore 에 1점을 더한다
+ * 5. ArrayList<String> sortingSet(HashMap<java.lang.String,Integer> recommScore)
+ *      - 입력받은 hash map 의 key 를 value 기준 내림차순으로 5개까지 list로 반환한다.
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
