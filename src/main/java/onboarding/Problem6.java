d56e97f113d1403c083190334bcca3a635165c53
ye1ne <yewon7036@naver.com>
2022-11-01 17:43:22 +0900

    feature :  문제 6번에 대한 풀이 및 구현 기능 목록 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
         List<String> answer = List.of("answer");
         return answer;
     }
