d63e8c7cd6a315550fbc67eaf3605168e0a89eec
core <jinjs6@gmail.com>
2022-11-01 21:27:46 +0900

    Problem7 HashMap 선언 및 friends에서 user를 제외한 이름들에 대한 값을 0으로 초기화

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -13,6 +11,14 @@ public class Problem7 {
                 userFriends.add(friends.get(i).get(1));
             } else if (friends.get(i).get(1).equals(user)) {
                 userFriends.add(friends.get(i).get(0));
+            }
+        }
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < friends.size(); ++i) {
+            for (int j = 0; j < 2; ++j) {
+                if (!friends.get(i).get(j).equals(user) && !map.containsKey(friends.get(i).get(j))) {
+                    map.put(friends.get(i).get(j), 0);
+                }
             }
         }
         return answer;
