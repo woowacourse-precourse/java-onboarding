2a57561689858715a7ebe65c8334b3683c20df93
hsh_519 <suk000519@naver.com>
2022-11-01 16:15:59 +0900

    Problem4: 기능 구현(changeAlphabet)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,22 @@
 package onboarding;
 
 public class Problem4 {
+    public static String changeAlphabet(String word) {
+        char[] word_arr = word.toCharArray();
+        String res = "";
+        for(int i=0;i<word_arr.length;i++) {
+            if ((int) ('A') <= (int) word_arr[i] && (int) ('Z') >= (int) word_arr[i]) {
+                Character w = (char) ((int) ('A') + (int) ('Z') - (int) word_arr[i]);
+                res += w.toString();
+            } else if ((int) ('a') <= (int) word_arr[i] && (int) ('z') >= (int) word_arr[i]) {
+                Character w = (char) ((int) ('a') + (int) ('z') - (int) word_arr[i]);
+                res += w.toString();
+            } else {
+                res += word_arr[i];
+            }
+        }
+        return res;
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
