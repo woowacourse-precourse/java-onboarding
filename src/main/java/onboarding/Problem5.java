eedb4bf1f990e1c7ca0d10b450a11eb7f2a0036d
오승윤 <>
2022-11-01 15:42:58 +0900

    추가 : Problem5 입력받은 금액을 현재 사용할 화폐가 몇장 쓰이는지 기록하는 메서드, 재귀 사용
    DFS 수행
    - 깊이는 화폐 단위가 저장된 배열의 인덱스를 의미
    - 다음 깊이의 경우 입력 금액에 화폐를 사용한 만큼 뺀 나머지를 금액으로 전달
    - 금액이 0이되면 탐색 중지

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,25 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int[] monetary_unit_array = {50000,10000,5000,1000,500,100,50,10,1};
+    private int[] number_of_monetary_units_used_array = new int[9];
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+
+    private void countUsesPerMonetaryUnit(int monetary_unit_idx, int money){
+
+        if(money==0){
+            return;
+        }
+
+        if(money<monetary_unit_array[monetary_unit_idx]) {
+            countUsesPerMonetaryUnit(monetary_unit_idx+1,money);
+        }else {
+            number_of_monetary_units_used_array[monetary_unit_idx] = money / monetary_unit_array[monetary_unit_idx];
+            countUsesPerMonetaryUnit(monetary_unit_idx+1,money % monetary_unit_array[monetary_unit_idx]);
+        }
     }
 }
