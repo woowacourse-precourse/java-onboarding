83d7352eb70ec209d433240c3ffe2efdd8da8e48
masiljangajji <hjk172262@gmail.com>
2022-11-01 20:36:50 +0900

    feat: Problem3_최종 solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,12 +4,18 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-
-        if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
-            answer++;
+        for(int j=3;j<=number;j++)
+        {
+            int i=j;
+            while(i>0)
+            {
+                if(i%10==3||i%10==6||i%10==9)
+                {
+                    answer++;
+                }
+                i=i/10;
+            }
         }
-
-
 
 
         return answer;
