03f324274b9dac94a7bd24f53ba45cfa71ca8e21
kmw2378 <rlarla677@gmail.com>
2022-11-01 18:29:27 +0900

    feat(Problem1) - 입력받은 forms 에서 이름에 포함된 단어를 별도의 List에 담는 로직 추가 - 이 때, 중복을 허용하지 않으므로 Set 사용

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        Set<String> words = new HashSet<>();
+
+        for (List<String> form : forms) {
+
+            String name = form.get(1);
+
+            int currentIndex = 0;
+
+            while (currentIndex < name.length() - 1) {
+
+                words.add(name.substring(currentIndex, currentIndex + 2));
+                currentIndex++;
+            }
+
+        }
+
         return answer;
     }
 }
