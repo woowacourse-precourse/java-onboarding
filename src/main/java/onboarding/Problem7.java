915e943600d5d174b782b10d86c0b428c5a0aee5
Luka <meena2003@naver.com>
2022-11-01 17:50:03 +0900

    친구의 친구를 구해서 HashMap에 점수 저장 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -11,13 +11,15 @@ public class Problem7 {
                 List.of("donut", "mrko"),
                 List.of("shakevan", "andole"),
                 List.of("shakevan", "jun"),
-                List.of("shakevan", "mrko")
+                List.of("shakevan", "mrko"),
+                List.of("Kim", "mrko"),
+                List.of("andole", "Kim")
         );
         List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
         solution(user, friends, visitors);
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        Map<String, Integer> mapListOfScores = new HashMap<>();
+        Map<String, Integer> pointsMap = new HashMap<>();
         int score = 0;
         Set<String> friendsSet = new HashSet<>();
 
@@ -31,12 +33,22 @@ public class Problem7 {
 
         System.out.println(friendsSet);
 
-        // [shakevan, donut]
-        for (int i = 0; i < friendsSet.size(); i++) {
-            for (int j = 0; j < friends.size(); j++) {
-
+        // 친구의 친구를 구해서 HashMap에 점수와 함께 저장
+        Iterator it = friendsSet.iterator();
+        while (it.hasNext()) {
+            String friendName = (String) it.next();
+            for (int i = 0; i < friends.size(); i++) {
+                if (friends.get(i).contains(friendName)) {
+                    String friendOfFriendName = IsLeftOrRight(friends.get(i), friendName);
+                    if (user != friendOfFriendName) {
+                        int point = pointsMap.get(friendOfFriendName) != null ? pointsMap.get(friendOfFriendName) + 10 : 10;
+                        pointsMap.put(friendOfFriendName, point);
+                    }
+                }
             }
         }
+
+        System.out.println(pointsMap);
 
         List<String> answer = Collections.emptyList();
         return answer;
