d592eb1572fd9ce30ba8cfdea3d8b7634b14206e
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-01 17:48:57 +0900

    feat(Problem6): 이 2글자가 다른 크루들의 이름에 포함되어 있나 확인(기능목록2)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,6 +9,10 @@ public class Problem6 {
         for (int i = 0; i < forms.size(); i++) {
             for (int j = 0; j < forms.get(i).get(1).length() - 1; j++) {
                 String checkName = forms.get(i).get(1).substring(j, j + 2);
+                for (int k = 0; k < forms.size(); k++) {
+                    if (forms.get(k).get(1).contains(checkName) {
+                    }
+                }
             }
         }
         return answer;
