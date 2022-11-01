b14a8f26c3ab8f7f2c8eede4d1842b3680b94063
이건회 <rg970604@naver.com>
2022-11-01 20:03:38 +0900

    feat(Problem1.java) : 상수 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int MIN_PAGE_NUM = 1;
+    private static final int MAX_PAGE_NUM = 400;
+    private static final int FIXED_SIZE = 2;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
