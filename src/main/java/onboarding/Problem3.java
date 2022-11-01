8f5223cd52efca9dfec500fd5a57cd766748e5b6
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-11-01 21:08:18 +0900

    Problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,2 +1,24 @@
 package onboarding;
 
+public class Problem3 {
+    static int count(int n) {
+        int cnt = 0;
+        while (n > 0) {
+            int first = n % 10;
+            if (first == 3 || first == 6 || first == 9) {
+                cnt++;
+            }
+            n /= 10;
+        }
+        return cnt;
+    }
+
+    public static int solution(int number) {
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            answer += count(i);
+        }
+        return answer;
+    }
+}
