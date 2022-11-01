bb9fb6fd74b5706bf6e58884cfd80dc0b90debde
SeokyungKim <kskyung0624@gmail.com>
2022-11-01 15:49:25 +0900

    문제 5 - 조금 더 직관적이도록 answer가 아닌 wallet으로 변수명 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,15 +8,15 @@ public class Problem5 {
     private static final int[] bills = { 50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1 };
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> wallet = new ArrayList<>();
 
         for (int bill : bills) {
             int billCount = countBill(money, bill);
-            answer.add(billCount);
             money = exchangeBill(money, billCount, bill);
+            wallet.add(billCount);
         }
 
-        return answer;
+        return wallet;
     }
 
     private static int countBill(int money, int bill) {
