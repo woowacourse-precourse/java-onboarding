8fdc6240e9bb83840f6117ed48bb84dd5c26aa20
jongmyeongbak <withpleasure@gmail.com>
2022-11-01 20:53:38 +0900

    solve problem 6

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Problem5 {
 
     public static List<Integer> solution(int money) {
-        List<Integer> moneyUnit = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+        List<Integer> moneyUnits = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
         List<Integer> answer = new ArrayList<>(9);
-        for (int change: moneyUnit) {
+        for (int change: moneyUnits) {
             answer.add(money / change);
             money %= change;
         }
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    static HashMap<String, String> twoWords = new HashMap<>(10000);
+    static Set<String> emailSet = new HashSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        for (List<String> crew : forms) {
+            dontname(crew);
+        }
+        return new ArrayList<>(emailSet);
+    }
+
+    private static void dontname(List<String> crew) {
+        String email = crew.get(0);
+        String nickname = crew.get(1);
+        HashMap<String, String> prevWords = twoWords;
+        String vmail;
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            String twoLetter = nickname.substring(i, i +2);
+            if ((vmail = prevWords.get(twoLetter)) != null){
+                emailSet.add(vmail);
+                emailSet.add(email);
+            } else {
+                twoWords.put(twoLetter, email);
+            }
+        }
     }
 }
