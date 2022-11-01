8f4b78f957f91b21be5da9e1101ca6bc70d75875
dydtmd6801 <dydtmd6801@naver.com>
2022-11-01 09:20:04 +0900

    fix : 반복문 안 함수 호출로 인한 버그 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,33 +1,37 @@
 package onboarding;
 
+import javax.swing.*;
 import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         HashMap<String, Integer> friendsMap = new HashMap<>();
-        checkOldFriends(friends, user, friendsMap, visitors);
-        System.out.println(friendsMap);
+        List<String> oldFriendNames = checkOldFriends(friends, user, friendsMap, visitors);
+        putNewFriends(friends, oldFriendNames, friendsMap, user);
         return answer;
     }
 
-    private static void checkOldFriends(List<List<String>> friendsList, String userName, HashMap friendsMap, List<String> visitor){
+    private static List<String> checkOldFriends(List<List<String>> friendsList, String userName, HashMap friendsMap, List<String> visitor){
+        List<String> oldFriendNames = new ArrayList<>();
+
         for(int i = 0; i < friendsList.size(); i++){
             int oldFriends = friendsList.get(i).indexOf(userName);
-            if(oldFriends > -1){
-                String oldFriendName = friendsList.get(i).get(1-oldFriends);
-                putNewFriends(friendsList, oldFriendName, friendsMap, userName);
-            }
+            if(oldFriends > -1) oldFriendNames.add(friendsList.get(i).get(1-oldFriends));
         }
+
+        return oldFriendNames;
     }
 
-    private static void putNewFriends(List<List<String>> friendsList, String oldFriendName, HashMap friendsMap, String userName){
+    private static void putNewFriends(List<List<String>> friendsList, List<String> oldFriendName, HashMap friendsMap, String userName){
         for(int i = 0; i < friendsList.size(); i++){
-            int newFriends = friendsList.get(i).indexOf(oldFriendName);
-            if(newFriends > -1 && !friendsList.get(i).get(1 - newFriends).equals(userName)){
-                String newFriendName = friendsList.get(i).get(1 - newFriends);
-                if(friendsMap.containsKey(newFriendName)) friendsMap.put(newFriendName, (int)friendsMap.get(newFriendName)+10);
-                if(!friendsMap.containsKey(newFriendName)) friendsMap.put(newFriendName, 10);
+            for(int j = 0; j < oldFriendName.size(); j++){
+                int newFriends = friendsList.get(i).indexOf(oldFriendName.get(j));
+                if(newFriends > -1 && !friendsList.get(i).get(1 - newFriends).equals(userName)){
+                    String newFriendName = friendsList.get(i).get(1 - newFriends);
+                    if(friendsMap.containsKey(newFriendName)) friendsMap.put(newFriendName, (int)friendsMap.get(newFriendName)+10);
+                    if(!friendsMap.containsKey(newFriendName)) friendsMap.put(newFriendName, 10);
+                }
             }
         }
     }
