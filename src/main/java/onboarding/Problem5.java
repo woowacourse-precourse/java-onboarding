b8abd1962fbb708be16ee5bf1d6d0643279a6650
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-01 14:50:58 +0900

    style : [problem5] 코드 정렬

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,10 +10,10 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         // 계산해야하는 화폐 단위는 변하지 않으므로 final로 생성
-        final int CURRENTUNITS[] = new int[]{50000,10000,5000,1000,500,100,50,10,1};
+        final int CURRENTUNITS[] = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
         // 모든 화폐 단위에 대해 money를 나눈 몫을 리스트에 순서대로 저장
-        for(int currentUnit : CURRENTUNITS){
+        for (int currentUnit : CURRENTUNITS) {
             answer.add(money / currentUnit);
             money %= currentUnit;
         }
