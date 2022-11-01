d8c676849733528dbef78e149cee30f3d0b1849f
hwanee__ <fkfkthth@naver.com>
2022-11-01 20:53:40 +0900

    6번문제 구현 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+
+        HashSet<String> email_list = new HashSet<>();
+        HashMap<String,String> map = new HashMap<>();
+
+        for(int i=0;i<forms.size();i++){
+            String name = forms.get(i).get(1);
+
+            for(int j=0;j<name.length()-1;j++){
+                String sub_name = name.substring(j,j+2);
+                if(map.containsKey(sub_name)){
+                    email_list.add(map.get(sub_name));
+                    email_list.add(forms.get(i).get(0));
+                }else{
+                    map.put(sub_name,forms.get(i).get(0));
+                }
+            }
+        }
+
+        List<String> answer = new ArrayList<>(email_list);
+        Collections.sort(answer);
+
         return answer;
     }
 }
