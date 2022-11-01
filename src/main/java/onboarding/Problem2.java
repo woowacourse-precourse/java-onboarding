00bd7de28f36b9015a472ad28ef8f3da51a11ad7
kwx4957 <kdy4957@naver.com>
2022-11-01 17:54:21 +0900

    2번 문제 기능 구현 3,4번 알파벳 및 소문자 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,13 +8,18 @@ package onboarding;
 5. 문자열 길이가 1 ~ 1000자 이하인지 확인
  */
 public class Problem2 {
-    public static boolean isAlpha(String str){
-        return str != null && str.chars().allMatch(Character::isAlphabetic);
+    public static boolean isAlphaAndLowerCase(String s){
+        for(char c:s.toCharArray()){
+            if(!('a'<=c && c <='z')){
+                return false;
+            }
+        }
+        return true;
     }
     public static String solution(String cryptogram) {
         String answer = "wrong";
 
-        if(isAlpha(cryptogram)){
+        if(isAlphaAndLowerCase(cryptogram)){
             return answer;
         }
 
