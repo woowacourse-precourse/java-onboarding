d0ec3d4c91d7cb6afe35cdf43071480601d558a9
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 14:05:41 +0900

    [3번] feat: 1부터 현재 숫자까지의 모든 ClapNumber 갯수를 찾기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,8 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer =sumClaps(number);
         return answer;
+    }
+
+    // 1. 1부터 현재 숫자까지의 ClapNumber의 총 갯수를 찾는 기능
+    public static int sumClaps(int number)
+    {
+        int sumClaps = 0;
+
+        for (int i = 1; i <= number; i++) {
+            String num =changeIntToString(i);
+            sumClaps +=find369(num);
+        }
+
+        return sumClaps;
     }
 
     // 2. 원래 숫자 길이와 ClapNumber가 제거된 숫자 길이의 차이를 찾기
