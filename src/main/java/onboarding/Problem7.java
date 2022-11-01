9a4c90476e774c2b7aea0d5df1a4309c6d1f376a
chj <bluedog129@gmail.com>
2022-11-01 21:02:50 +0900

    [P7] 파라미터로 받은 모든 유저를 리스트에 저장 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,19 +1,28 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = new ArrayList<>();
-        for (int i = 0; i < friends.size(); i++) {
-            for (int j = 0; j < friends.get(i).size(); j++) {
+    private static List<String> getUserList(String user, List<List<String>> friends, List<String> visitors) {
+        Set<String> allUserSet = new TreeSet<>();
+        allUserSet.add(user);
 
-            }
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> friend = friends.get(i);
+            allUserSet.add(friend.get(0));
+            allUserSet.add(friend.get(1));
+        }
+        for (int i = 0; i < visitors.size(); i++) {
+            allUserSet.add(visitors.get(i));
         }
 
+        List<String> allUserList = new ArrayList<>(allUserSet);
 
-        return answer;
+        return allUserList;
+    }
+    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> allUserList = getUserList(user, friends, visitors);
+
+        return allUserList;
     }
 }
