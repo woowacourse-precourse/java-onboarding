85cc930e340024516f67c9563c04ffb911ec406c
seominseok00 <seominseok4834@gmail.com>
2022-11-01 23:25:39 +0900

    feat(문제6): 중복 닉네임을 찾는 메소드 구현

--- src/main/java/onboarding/problem6/NicknameDuplicationManage.java
+++ src/main/java/onboarding/problem6/NicknameDuplicationManage.java
@@ -1,14 +1,12 @@
 package onboarding.problem6;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class NicknameDuplicationManage {
     private List<List<String>> forms;
     private HashMap<String, Set<String>> nickNameMap = new HashMap<>();
-
+    private List<String> duplicateEmailList = new ArrayList<>();
+    
     public NicknameDuplicationManage(List<List<String>> forms){
         this.forms = forms;
     }
@@ -38,5 +36,17 @@ public class NicknameDuplicationManage {
             }
         }
     }
-    
+
+    private void findDuplicateNickname(){
+        Set<String> duplicateEmailSet = new HashSet<>();
+
+        for(String key : nickNameMap.keySet()) {  // key 값을 가져옴
+            Set<String> value = nickNameMap.get(key);
+            if (value.size() > 1) {
+                duplicateEmailSet.addAll(value);
+            }
+        }
+
+        duplicateEmailList = new ArrayList<>(duplicateEmailSet);
+    }
 }
