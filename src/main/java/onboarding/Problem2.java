c6c8557e3c335d53cbf1fb1df18b6c52a80530f9
seheonCho <whtpgjs94@gmail.com>
2022-11-01 18:38:42 +0900

    feat: [문제2] 주어질수 있는 문자열을 암호문의 가능한 최대의 길이 횟수만큼 반복한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,12 +4,18 @@ public class Problem2 {
 
     private static final String REGEX_LOWER_ALPHABET = "([a-z])\\1";
     private static final String SPACE = "";
+    private static final int MAX_CRYPTOGRAM_LENGTH = 1000;
 
     public static String solution(String cryptogram) {
         String decrypting = SPACE;
         String beforeTryedDecrypting = SPACE;
 
         decrypting = cryptogram.replaceAll(REGEX_LOWER_ALPHABET, SPACE);
+
+        for (int i = 0; i < MAX_CRYPTOGRAM_LENGTH; i++) {
+
+        }
+
         return decrypting;
     }
 }
