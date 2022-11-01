7cdd3adea6639a3f6b6ad7a05e7b826bb007dd69
rt3310 <btac3310@gmail.com>
2022-11-01 10:20:40 +0900

    feat(Problem7): 친구 목록 맵 생성 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static Map<String, List<String>> getFriendMap(List<List<String>> friends) {
+        Map<String, List<String>> friendMap = new HashMap<>();
+        for (List<String> friendPair : friends) {
+            List<String> friend1 = friendMap.getOrDefault(friendPair.get(0), new ArrayList<>());
+            List<String> friend2 = friendMap.getOrDefault(friendPair.get(1), new ArrayList<>());
+            friend1.add(friendPair.get(1));
+            friend2.add(friendPair.get(0));
+            friendMap.put(friendPair.get(0), friend1);
+            friendMap.put(friendPair.get(1), friend2);
+        }
+        return friendMap;
     }
 }
