a1d79ce8fb45a1dc24d75ee1f3d764f0313d777f
MAYY27 <117031840+MAYY27@users.noreply.github.com>
2022-11-01 15:28:16 +0900

    Update Problem5.java

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] moneyArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        int leftMoney = money;
+        for (int i = 0; i < moneyArray.length; i++) {
+
+            if(leftMoney >= moneyArray[i]) {
+
+                answer.add(leftMoney/moneyArray[i]);
+
+                leftMoney = leftMoney % moneyArray[i];
+            }
+
+            else answer.add(0);
+        }
         return answer;
     }
 }
