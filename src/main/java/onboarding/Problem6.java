2a2f167f991a83514bfc58bd7800d69537f44944
Donguk <95donguk.dev@gmail.com>
2022-11-01 17:46:23 +0900

    feat: 문제6의 실행 결과 출력 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,6 +12,21 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
 
+    public static void main(String[] args) {
+        List<List<String>> forms = List.of(
+            List.of("jm@email.com", "제이엠"),
+            List.of("jason@email.com", "제이슨"),
+            List.of("woniee@email.com", "워니"),
+            List.of("mj@email.com", "엠제이"),
+            List.of("nowm@email.com", "이제엠")
+        );
+
+        System.out.printf("%-30s|%-30s|", "forms", "result");
+        System.out.println("\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.printf("%-30s|%-30s|", forms, solution(forms));
+        System.out.println("\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         FormsValidator.validate(forms);
