45ea0b1395bf901ace0fda2a96c01ec9029b79e9
sylee <sy>
2022-11-01 23:00:45 +0900

    [Feat] : Problem2 중복 문자열 인덱스 찾기, 중복 제거 함수 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,14 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
 
         int index = 0, startIndex = -1, lastIndex = -1;
-        while(index < cryptogram.length() - 1) {
-            if(cryptogram.charAt(index) == cryptogram.charAt(index + 1)) {
+        while(index < answer.length() - 1) {
+            if(answer == "") {
+                break;
+            }
+            if(answer.charAt(index) == answer.charAt(index + 1)) {
                 startIndex = index;
-                lastIndex = getLastIndex(cryptogram, startIndex);
-                answer = removeRedundant(cryptogram, startIndex, lastIndex);
+                lastIndex = getLastIndex(answer, startIndex);
+                answer = removeRedundant(answer, startIndex, lastIndex);
                 index = startIndex - 1;
             }
             else {
@@ -21,12 +24,14 @@ public class Problem2 {
     }
 
     public static int getLastIndex(String str, int sIndex) {
-
-        return -1;
+        int i = sIndex;
+        while(i < str.length() - 1 && str.charAt(i) == str.charAt(i+1)) {
+            i++;
+        }
+        return i;
     }
 
     public static String removeRedundant(String str, int sIndex, int lIndex) {
-
-        return "";
+        return str.substring(0, sIndex) + str.substring(lIndex + 1);
     }
 }
