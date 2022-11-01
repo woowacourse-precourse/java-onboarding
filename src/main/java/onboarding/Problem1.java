d3154ade87d816bbfbc0d396a2856619638d70ca
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:22:58 +0900

    Problem1 javadocs
    Method and class descriptions

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,7 +2,19 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * Problem 1
+ * @author Davin An
+ * @version 1.0
+ */
 class Problem1 {
+
+    /**
+     * Calculates the winner from a random page game
+     * @param pobi Player 1
+     * @param crong Player 2
+     * @return 1 if Pobi wins, 2 if Crong wins, 0 if there is a draw, -1 for exception cases
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobiNum = numCalculator(pobi);
         int crongNum = numCalculator(crong);
@@ -17,12 +29,23 @@ class Problem1 {
         }
     }
 
+    /**
+     * Checks if the page number results in an exception
+     * @param pageList List with the page numbers
+     * @return True if there is an exception, false otherwise
+     */
     public static boolean exceptionHandler(List<Integer> pageList) {
         int left = pageList.get(0);
         int right = pageList.get(1);
         return ((left + 1 != right) || (left == 1) || (right == 400));
     }
 
+
+    /**
+     * Calculates the score for a given player
+     * @param pageList List with the page numbers
+     * @return Player's score
+     */
     public static int numCalculator(List<Integer> pageList) {
         int n = Integer.parseInt(pageList.get(0) + Integer.toString(pageList.get(1)));
         int sum = 0;
