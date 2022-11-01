5814a3a478754d97995f1ef5b59d8a8dc6d85a7f
SeokHwan An <asok369258@naver.com>
2022-11-01 09:25:45 +0900

    feat [Problem7] : friends의 길이가 1이상 10,000 이하인지 검증하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,6 +7,8 @@ public class Problem7 {
 
     private static final int USER_MIN_LENGTH = 1;
     private static final int USER_MAX_LENGTH = 30;
+    private static final int FRIENDS_MIN_LENGTH = 1;
+    private static final int FRIENDS_MAX_LENGTH = 10000;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
@@ -16,6 +18,11 @@ public class Problem7 {
     //user의 길이가 1이상 30이하인지 검증하는 기능
     public static boolean checkUserLengthValidation(String user) {
         return USER_MIN_LENGTH <= user.length() && user.length() <= USER_MAX_LENGTH;
+    }
+
+    //friends의 길이가 1이상 10,000이하인지 검증하는 기능
+    public static boolean checkFriendsLengthValidation(List<List<String>> friends) {
+        return FRIENDS_MIN_LENGTH <= friends.size() && friends.size() <= FRIENDS_MAX_LENGTH;
     }
 
 }
