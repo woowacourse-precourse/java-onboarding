04adf37971f82ff57b5b8ad2d1ca9046f9697910
lynne921 <dkfls921@gmail.com>
2022-11-01 18:19:34 +0900

    [feat][problem2] 중복 삭제 및 전체를 확인하며 중복을 제거하는 메서드 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,8 +15,28 @@ public class Problem2 {
         return sum <= 0;
     }
 
+    private static String makeNonDuplicate(String sentence) {
+        for (int i = 0; i < sentence.length() - 1; i++) {
+            if (isDuplicate(sentence.charAt(i), sentence.charAt(i+1)) == 1)
+                sentence = deleteDuplicate(sentence, i);
+        }
+        return sentence;
+    }
+
+    private static String deleteDuplicate(String sentence, int deleteIdx) {
+        String sentenceFirst = sentence.substring(0, deleteIdx);
+        String sentenceSecond = sentence.substring(deleteIdx+2);
+        sentence = sentenceFirst + sentenceSecond;
+        return sentence;
+    }
+
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (!checkSentence(cryptogram)) {
+            cryptogram = makeNonDuplicate(cryptogram);
+        }
+        answer = cryptogram;
         return answer;
     }
 }
