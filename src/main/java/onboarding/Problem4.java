28852f6419edd0c1cf97ad89c3c6500bce37a37b
we4sley <galaxy138742@gmail.com>
2022-11-01 23:41:31 +0900

    feat: problem4 solution 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,47 @@
 package onboarding;
 
 public class Problem4 {
+    private static final String MESSAGE = "잘못된 입력입니다.";
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        if (!isInputValid(word)) {
+            throw new IllegalArgumentException(MESSAGE);
+        }
+
+        return convertWord(word);
+    }
+
+    private static String convertWord(String word) {
+        char[] charArray = word.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char c : charArray) {
+            c = convertCharacter(c);
+            stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
+    }
+
+    private static char convertCharacter(char c) {
+        int valueDifference;
+
+        if (isAlphabet(c)) {
+            if (Character.isUpperCase(c)) {
+                valueDifference = 'Z' - c + 'A';
+            } else {
+                valueDifference = 'z' - c + 'a';
+            }
+            c = (char) valueDifference;
+        }
+
+        return c;
+    }
+
+    private static boolean isInputValid(String word) {
+        return word.length() > 1 && word.length() < 1000;
+    }
+
+    private static boolean isAlphabet(char c) {
+        return (int) c >= 65 && (int) c <= 122;
     }
 }
