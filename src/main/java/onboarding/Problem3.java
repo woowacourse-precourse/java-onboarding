c2c584519fb3163b2b4b58a9ae22e3f264eaf919
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:43:57 +0900

    feat(problem3): 입력된 number 까지 게임 반복

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,13 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         validate(number);
-        return 0;
+        return playGame(number);
     }
 
     private static void validate(int number) {
         if (number < 1 || number > 10000) {
             throw new IllegalArgumentException("number는 1 이상 10,000 이하인 자연수이어야 합니다.");
         }
+    }
+
+    private static int playGame(int number) {
+        int clapCount = 0;
+        for (int turn = 1; turn <= number; turn++) {
+            clapCount = playTurn(clapCount, turn);
+        }
+        return clapCount;
     }
 
     private static int playTurn(int clapCount, int speakNumber) {
