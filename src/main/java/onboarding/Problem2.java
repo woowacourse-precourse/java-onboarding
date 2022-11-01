dc2c2001a89aed575dae9e455e349637b82a4680
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-01 10:31:03 +0900

    fix : 하나의 중복만 차근차근 제거하는 것에서 모든 중복 제거하도록 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String result = "";
@@ -7,22 +12,43 @@ public class Problem2 {
         return result;
     }
     static String checkString(String str) {
-        String answer = "";
-        for(int i=0; i<str.length()-1; i++) {
-            if(str.charAt(i)==str.charAt(i+1)) {
-                str = splitString(str, i);
-                i = -1;
-            }
+        while(validCheck(str)){
+            str = catchSameIndex(str);
         }
+
         return str;
     }
-    static String splitString(String str, int index) {
-        String newstring = "";
+    static String catchSameIndex(String str){
+        List<Integer> index = new ArrayList<>();
         for(int i=0; i<str.length()-1; i++) {
-            if(i==index || i==index+1) continue;
+            if(str.charAt(i) == str.charAt(i+1)) {
+                index.add(i);
+                index.add(i+1);
+            }
+        }
+        return splitString(str, index);
+    }
+    static String splitString(String str, List<Integer> index) {
+        String newstring = "";
+        for(int i=0; i<str.length(); i++) {
+            if(checkIndex(i,index)) continue;
             newstring += str.charAt(i);
         }
-        if(index!=str.length()-2) newstring += str.charAt(str.length()-1);
+        index.clear();
+        //if(index!=str.length()-2) newstring += str.charAt(str.length()-1);
         return newstring;
+    }
+    static boolean checkIndex(int num, List<Integer> index){
+        for(int i=0; i<index.size(); i++){
+            if(num==index.get(i)) return true;
+        }
+        return false;
+    }
+
+    static boolean validCheck(String str){
+        for(int i=0; i<str.length()-1; i++){
+            if(str.charAt(i)==str.charAt(i+1)) return true;
+        }
+        return false;
     }
 }
