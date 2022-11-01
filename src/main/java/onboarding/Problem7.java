efbd1a9d406cd4381d959d65c380b1dc49f831dc
DongWooE <ehddn977@naver.com>
2022-11-01 19:46:52 +0900

    [p7][add] friendGraph를 만드는 메소드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+
+    HashMap<String, ArrayList<String>> friendGraph = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Problem7 p7 = new Problem7();
+        p7.makeFriendGraph(friends);
+        return null;
+    }
+
+    private void makeFriendGraph(List<List<String>> friends){
+        for(List<String> list : friends){
+            String friend1 = list.get(0);
+            String friend2 = list.get(1);
+        }
     }
 }
