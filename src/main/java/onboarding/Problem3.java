64faa122abb423a0c49b9c0dca0be9ce1e84a7d5
dain-lee <dain9934@gmail.com>
2022-11-01 23:13:40 +0900

    feat : 문제 3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int checkNum(int num) {
+        int clap = 0;
+        String numString = String.valueOf(num);
+
+        for (int i = 0; i < numString.length(); i++) {
+            if (numString.charAt(i) == '3' || numString.charAt(i) == '6' || numString.charAt(i) == '9') {
+                clap ++;
+            }
+        }
+        return clap;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i ++) {
+            answer += checkNum(i);
+        }
+
         return answer;
     }
 }
