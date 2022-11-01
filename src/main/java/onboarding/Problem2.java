1eb79ca3a79eb6abbae5b4699de894a75f725b2b
kim1387 <kevink0@naver.com>
2022-11-01 15:22:21 +0900

    feat: String을 stack으로 바꾸는 함수
    Stack은 Deque를 이용하여 구현하였음

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.*;
+
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+  public static String solution(String cryptogram) {
+    Deque<Character> cryptogramStack = convertStringToStack(cryptogram);
+
+    return "";
+  }
+
+  public static Deque<Character> convertStringToStack(String string) {
+    Deque<Character> stack = new ArrayDeque<>();
+    for (char letter : string.toCharArray()) {
+      stack.add(letter);
     }
+    return stack;
+  }
 }
