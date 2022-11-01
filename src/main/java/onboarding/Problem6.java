405e7d3185550b714ac8859840f35ce312928779
patronum98 <zkrhklkl98@gmail.com>
2022-11-01 11:14:16 +0900

    문제6 기능목록 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+//기능1. 닉네임과 닉네임을 두글자씩 검사한다.
+//기능2. 겹치는 닉네임글자가 있으면 이메일만 리스트에 추가한다.
+//기능3. 기능1,2 를 2중포문으로 만들어 모든 닉네임끼리 비교하게 한다.
+//기능4 . answer 을 오름차순으로 정렬한뒤 리턴한다.
+
 import java.util.List;
 
 public class Problem6 {
