ce1064282c59b5ea1bc4817cc0b5a9e29e8903d7
saehim <saeilee.vxv@gmail.com>
2022-11-01 17:57:20 +0900

    Feat(Problem7): 새친구 찾아 점수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,17 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+    // 새친구 점수 추가
+    private static void NewFriends(String user, List<List<String>> friends, String oldFriend) {
+        // user의 새 친구 찾기
+        for (List<String> relation : friends) {
+            if(relation.contains(oldFriend) && !relation.contains(user)){
+                String newFriend = relation.get(0)==oldFriend ? relation.get(1) : relation.get(0);
+                addPoint(newFriend, 10);
+            }
+        }
+    }
+
     // user 친구 찾기
     private static List<String> getOldFriends(String user, List<List<String>> friends) {
         List<String> result = new ArrayList<>();
