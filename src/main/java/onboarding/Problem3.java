65a479f72b18f979d24ea021ceb1a734c9810e06
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:29:36 +0900

    Feat: Change 솔루션 함수 로직

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,8 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = countClap(number);
+
         return answer;
     }
 
