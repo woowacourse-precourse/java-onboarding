1f3e321c8355297c4b94703134d9155fe9ddb099
gitFILO <x1xgudwls@naver.com>
2022-11-01 22:46:41 +0900

    [Problem 3] 박수 치는 횟수를 리턴

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clap = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int currentNumber = i;
+            int remainderOfCurrentNumber;
+
+            while (currentNumber != 0) {
+                remainderOfCurrentNumber = currentNumber % 10;
+                if (remainderOfCurrentNumber == 3 || remainderOfCurrentNumber == 6 || remainderOfCurrentNumber == 9)
+                    clap++;
+                currentNumber /= 10;
+            }
+        }
+        return clap;
     }
 }
