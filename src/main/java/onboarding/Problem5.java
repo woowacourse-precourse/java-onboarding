933dcfcae896734a9b2e56c20baffa7966688015
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:38:42 +0900

    Problem5 javadocs
    Method and class description

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,7 +3,18 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Problem 5
+ * @author Davin An
+ * @version 1.0
+ */
 public class Problem5 {
+
+    /**
+     * Converts amount of money into currency units
+     * @param money Amount of money to convert
+     * @return List containing the amount for each unit
+     */
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
