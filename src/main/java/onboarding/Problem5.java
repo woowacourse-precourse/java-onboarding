835059d86890232c539ccabe15392068e0059f4e
June1010 <tjrwns1010@naver.com>
2022-11-01 23:27:52 +0900

    feat: 액수 단위를 순회하면서 그리디하게 동전 갯수를 구하는 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,10 +5,21 @@ import java.util.List;
 
 public class Problem5 {
     // 액수 단위를 저장하는 리스트 추가하기
+    private static List<Integer> amounts = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     public static List<Integer> solution(int money) {
-        // 정답으로 반환할 단위 액수만큼의 리스트 선언하기
-        // 그리디하게 액수 단위를 순회하면서 각 단위 동전의 갯수를 구해주기
-        // 반환
-        return new ArrayList<>();
+        List<Integer> answer = new ArrayList<Integer>(9);
+
+        int amount;
+        for (int i = 0; i < amounts.size(); i++) {
+            amount = amounts.get(i);
+            if (money / amount > 0) {
+                answer.add(money / amount);
+                money %= amount;
+            } else {
+                answer.add(0);
+            }
+        }
+
+        return answer;
     }
 }
