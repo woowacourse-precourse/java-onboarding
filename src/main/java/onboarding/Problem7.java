cec039aff1073a92616cdedca72b9246f6602c4b
김선동 <mok03127@hanyang.ac.kr>
2022-11-01 19:52:44 +0900

    feat: 문제 7 전체 사용자들을 나타내는 social network와 친구의 친구 정보를 설정하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> recommendedFriendList = new ArrayList<>();
+        HashSet<String> socialNetwork = new HashSet<>(); // 전체 사용자들
+        HashMap<String, HashSet<String>> friendsOfFriendOfUserMap = new HashMap<>(); // 친구의 친구들 저장
+
+        // 전체 사용자들을 나타내는 social network와 친구의 친구 정보를 설정
+        generateSocialNetworkInfo(user, friends, socialNetwork, friendsOfFriendOfUserMap);
+
+        return recommendedFriendList;
+    }
+
+    public static void generateSocialNetworkInfo(String user, List<List<String>> friends, HashSet<String> socialNetwork, HashMap<String, HashSet<String>> friendsOfFriendOfUserMap) {
+        socialNetwork.add(user);
+        friendsOfFriendOfUserMap.put(user, new HashSet<String>());
+
+        for (List<String> friend : friends) {
+            String friendA = friend.get(0);
+            String friendB = friend.get(1);
+
+            if (socialNetwork.contains(friendA) == false) {
+                socialNetwork.add(friendA);
+                friendsOfFriendOfUserMap.put(friendA, new HashSet<String>());
+            }
+            if (socialNetwork.contains(friendB) == false) {
+                socialNetwork.add(friendB);
+                friendsOfFriendOfUserMap.put(friendB, new HashSet<String>());
+            }
+
+            // 서로 친구로 추가
+            friendsOfFriendOfUserMap.get(friendA).add(friendB);
+            friendsOfFriendOfUserMap.get(friendB).add(friendA);
+        }
     }
 }
