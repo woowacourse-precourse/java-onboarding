f3c35192b080a3dca811dd9ca9e208df667436a9
hongjinwon <herenever@naver.com>
2022-11-01 22:18:31 +0900

    refactor: Problem7 리팩토링
    - Problem7 리팩토링
    - freindScoring을 시에 친구의 친구가 user의 친구인 경우를 제외하도록 수정
    - 이에
    따른 매개변수 추가 및 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -16,22 +16,23 @@ public class Problem7 {
 
         List<String> userFriends = new ArrayList<>();
         for (List<String> friend : friends) {
-            if(friend.get(0).equals(user)){
-                userFriends.add(friend.get(0));
-                continue;
+            if(friend.get(0).equals(user) && !userFriends.contains(friend.get(1))){
+                userFriends.add(friend.get(1));
             }
-            userFriends.add(friend.get(1));
+            if(friend.get(1).equals(user) && !userFriends.contains(friend.get(0))) {
+                userFriends.add(friend.get(0));
+            }
         }
         return userFriends;
     }
 
-    public static Map<String,List<String>> makeFriendsMap(String user, List<String> userFriends, List<List<String>> friends){
+    public static Map<String,List<String>> makeFriendsMap(String user, List<List<String>> friends){
 
         Map<String,List<String>> friendsMap = new HashMap<>();
 
         for (List<String> friend : friends){
 
-            if(userFriends.contains(friend.get(0)) || userFriends.contains(friend.get(1))) continue;
+            if(friend.get(0).equals(user) || friend.get(1).equals(user)) continue;
 
             makeRelation(friend,friendsMap);
 
@@ -40,17 +41,21 @@ public class Problem7 {
         return friendsMap;
     }
 
-    public static Map<String,Integer> makeScoreMap(Map<String,List<String>> friendsMap,List<String> visitors){
+    public static Map<String,Integer> makeScoreMap(Map<String,List<String>> friendsMap,List<String> visitors,List<String> userFriends){
 
         Map<String,Integer> scoreMap = new HashMap<>();
         Set<String> keySet = friendsMap.keySet();
 
-        for(String user : keySet){
-            scoreMap.put(user,0);
+        for(String key : keySet){
+            if (!userFriends.contains(key)) {
+                scoreMap.put(key, 0);
+            }
         }
 
-        for(String user: visitors){
-            scoreMap.put(user,0);
+        for(String visitor: visitors){
+            if (!userFriends.contains(visitor)) {
+                scoreMap.put(visitor, 0);
+            }
         }
 
         return scoreMap;
@@ -91,16 +96,17 @@ public class Problem7 {
 
     public static void scoring(Map<String,Integer> scoreMap,Map<String,List<String>> friendsMap ,List<String> userFriends,List<String> visitors){
 
-        visitScoring(scoreMap,visitors);
+        visitScoring(scoreMap,userFriends,visitors);
         friendScoring(scoreMap,userFriends,friendsMap);
-
     }
 
-    public static void visitScoring(Map<String,Integer> scoreMap,List<String> visitors){
+    public static void visitScoring(Map<String,Integer> scoreMap,List<String> userFriends,List<String> visitors){
 
         for (String visitor : visitors){
-            Integer score = scoreMap.get(visitor);
-            scoreMap.put(visitor,score+1);
+            if(!userFriends.contains(visitor)) {
+                Integer score = scoreMap.get(visitor);
+                scoreMap.put(visitor, score + 1);
+            }
         }
 
     }
@@ -109,9 +115,12 @@ public class Problem7 {
 
         List<String> scoreList = new ArrayList<>();
         for(String userFriend : userFriends){
-            scoreList.addAll(friendsMap.get(userFriend));
+            for (String person :friendsMap.get(userFriend)){
+                if(!userFriends.contains(person)){
+                    scoreList.add(person);
+                }
+            }
         }
-
         for(String user : scoreList){
             Integer score = scoreMap.get(user);
             scoreMap.put(user,score+10);
