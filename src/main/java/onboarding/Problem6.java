fd09c0324f7adfd756ae6497d25083d5ef84fdea
Giho <poqw741@gmail.com>
2022-11-01 22:53:50 +0900

    add duplication index save

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> name = new ArrayList<>();
+        List<Integer> emailnumber = new ArrayList<>();
+        List<String> email = new ArrayList<>();
+        String tmp = "";
+
+        for (int i = 0; i < forms.size(); i++) {
+            name.add(forms.get(i).get(1));
+        } // 네임 리스트 생성
+
+
+        for (int i = name.size() - 1; i > 0; i--) {
+            for (int j = 0; j < name.get(i).length() - 1; j++) {
+                tmp = name.get(i).substring(j, j + 2);//pattern
+                for (int z = 0; z < i; z++) {
+                    if (name.get(z).contains(tmp)) {
+                        emailnumber.add(i);
+                        emailnumber.add(z);
+                    }
+                }
+
+            }
+        }
+        return email;
     }
 }
