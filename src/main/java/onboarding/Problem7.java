ee391e0c62f92fd43c84db87065f32dde444d713
Sieun Park <psieun0606@gmail.com>
2022-11-01 23:12:54 +0900

    feat: problem7 사용자의 친구리스트 만드는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    static ArrayList<String> makeFriendList(String user, List<List<String>> friends) {
+        ArrayList<String> friendsOfUser = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                String friendName = addFriends(user, friend);
+                friendsOfUser.add(friendName);
+            }
+        }
+
+        return friendsOfUser;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        ArrayList<String> friendsOfUser = makeFriendList(user, friends);
+
         return answer;
     }
 }
