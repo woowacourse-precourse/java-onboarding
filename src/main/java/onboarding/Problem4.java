6a87f088e9e97d7d998ce91578fd4333d2597c2b
Anthologia <pyoungjy@gmail.com>
2022-11-01 18:53:02 +0900

    feat: Problem 4 알파벳인 경우에만 청개구리 알파벳 변환 처리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,10 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (byte w : word.getBytes()) {
+            answer += funnyConverter((char) w);
+        }
+
         return answer;
     }
 
     private static char funnyConverter(char ch) {
+        if (!Character.isAlphabetic(ch)) {
+            return (char) ch;
+        }
+
         if (Character.isLowerCase(ch)) {
             return (char) ('z' - (ch - 'a'));
         }
