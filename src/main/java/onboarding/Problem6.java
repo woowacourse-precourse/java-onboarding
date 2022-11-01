edd21e338ce9e633b205876fab7ebed2e32ce6c1
ChaCha3088 <cha3088@gmail.com>
2022-11-01 13:36:34 +0900

    fix: problem6 function isThereMatch

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,16 +1,26 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        for (List e : forms) {
+
+        }
+
         return answer;
     }
 
-    private static Boolean isThereMatch(String string, keyword) {
+    private static Boolean isThereMatch(List<List<String>> forms, String keyword) {
         String regex = ".*("+keyword+")+.*";
-        Boolean result = string.matches(regex);
-        return result;
+        for (List<String> e : forms) {
+            System.out.println(e);
+            String nickname = e.get(1);
+            Boolean result = nickname.matches(regex);
+            if (result == true) {
+                return true;
+            }
+        }
+        return false;
     }
 }
