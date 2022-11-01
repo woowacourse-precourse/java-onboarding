9c49c8640795ec6a06aacfd8256ac32f85c41f34
seominseok00 <seominseok4834@gmail.com>
2022-11-01 23:33:02 +0900

    feat(문제6) Solve

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,12 @@
 package onboarding;
 
+import onboarding.problem6.NicknameDuplicationManage;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        NicknameDuplicationManage nicknameDuplicationManage = new NicknameDuplicationManage(forms);
+        List<String> answer = nicknameDuplicationManage.start();
         return answer;
     }
 }
