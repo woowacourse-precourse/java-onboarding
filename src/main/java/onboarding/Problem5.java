a27946612a7569d7e7ee339fcbe397d8f69ed535
SEONMl <sm000117@naver.com>
2022-11-01 14:45:17 +0900

    refactor: Problem5 화폐 변수 재사용성 고려해 static화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static int[] korCurrency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     static class Wallet {
-        static final int MONEY_MAX_COUNT = 9;
+        private static final int MONEY_MAX_COUNT = 9;
 
         private List<Integer> money = new ArrayList<>(MONEY_MAX_COUNT);
-        private int[] currency = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         private int target;
 
         public Wallet(int target) {
             this.target = target;
         }
 
-        List<Integer> getResult() {
+        List<Integer> getTotalMoney() {
             calculate();
             return money;
         }
@@ -24,14 +24,14 @@ public class Problem5 {
         private void calculate() {
             int tmp = target;
             for(int i=0 ; i<MONEY_MAX_COUNT ; i++){
-                money.add(tmp / currency[i]);
-                tmp%=currency[i];
+                money.add(tmp / korCurrency[i]);
+                tmp%= korCurrency[i];
             }
         }
 
     }
     public static List<Integer> solution(int money) {
         Wallet wallet = new Wallet(money);
-        return wallet.getResult();
+        return wallet.getTotalMoney();
     }
 }
