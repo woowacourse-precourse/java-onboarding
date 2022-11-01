4edff931b8d61a8efba0fb4709845cb315cb3532
slbin-park <smpts00@naver.com>
2022-11-01 15:09:26 +0900

    refactor(PROBLEM4) : words 에서 alpha로 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 
 public class Problem4 {
     public static String solution(String word) {
-        HashMap<String,String> words = changeAlpha();
-        return changeWords(word,words);
+        HashMap<String,String> alpha = changeAlpha();
+        return changeWords(word,alpha);
     }
     public static String changeWords(String word,HashMap<String,String> words){
         String answer = "";
@@ -19,17 +19,17 @@ public class Problem4 {
         return answer;
     }
     public static HashMap<String,String> changeAlpha(){
-        HashMap<String,String> words = new HashMap<String,String>();
+        HashMap<String,String> alpha = new HashMap<>();
         int ascii = 90;
         for(int i=65;i<=90;i++){
-            words.put(String.valueOf((char)i),String.valueOf((char)ascii));
+            alpha.put(String.valueOf((char)i),String.valueOf((char)ascii));
             ascii-=1;
         }
         ascii = 122;
         for(int i=97;i<=122;i++){
-            words.put(String.valueOf((char)i),String.valueOf((char)ascii));
+            alpha.put(String.valueOf((char)i),String.valueOf((char)ascii));
             ascii-=1;
         }
-        return words;
+        return alpha;
     }
 }
