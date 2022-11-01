8fb789f87a32551c44383e2ced730a2669aa9130
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 16:52:30 +0900

    FEAT fragmentNN

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static List<String> fragmentNN(String nickName) {
+        List<String> ret = new ArrayList<>();
+
+        for (int i = 0; i < nickName.length() - 1; i++) {
+            String subString = nickName.substring(i, i + 2);
+            ret.add(subString);
+        }
+
+        return ret;
+    }
+
 }
