5d6b067ad8b761ec6105dc3095bcbeb3dcfe765b
WooJin Kim <dnwls813@ajou.ac.kr>
2022-11-01 09:34:07 +0900

    [Modify] Q1 : exception을 throw하지 않도록 수정
    - 기존엔 사용자 정의 exception(InvalidPageException)을 throw 하도록
      했지만 채점 사이트 내에서 채점이 안되어, 코드 상에서 예외를 처리하도록
      수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,28 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) throws InvalidPageNumberException {
+    public static int solution(List<Integer> pobi, List<Integer> crong){
         int answer = Integer.MAX_VALUE;
 
-        try {
-            int pobiMax = pageMaxValue(pobi); // pobi의 가장 큰 page
-            int crongMax = pageMaxValue(crong);// crong의 가장 큰 page
+        int pobiMax = pageMaxValue(pobi); // pobi의 가장 큰 page
+        int crongMax = pageMaxValue(crong);// crong의 가장 큰 page
 
-            if (pobiMax > crongMax)
-                answer = 1;
-            else if (pobiMax < crongMax)
-                answer = 2;
-            else
-                answer = 0;
-
-            return answer;
-        }
-        catch (InvalidPageNumberException e){
-            System.out.println(e.getMessage()+"\n");
-            e.printStackTrace();
-
+        if(pobiMax == -1 || crongMax == -1) // 올바르지 않은 페이지에 대한 예외처리
             return -1;
-        }
+
+        if (pobiMax > crongMax)
+            answer = 1;
+        else if (pobiMax < crongMax)
+            answer = 2;
+        else
+            answer = 0;
+        return answer;
     }
 
     /**
@@ -39,13 +33,13 @@ class Problem1 {
      * RETURN
      *   두 page 중 더 큰 값
      */
-    public static int pageMaxValue(List<Integer> pages) throws InvalidPageNumberException {
+    public static int pageMaxValue(List<Integer> pages){
 
         int leftValue = pages.get(0); // 왼쪽 페이지 번호
         int rightValue = pages.get(1); // 오른쪽 페이지 번호
 
         if(rightValue <= leftValue || (rightValue -1) != leftValue)
-            throw new InvalidPageNumberException("올바르지 않은 페이지 리스트입니다.");
+            return -1;
 
         return Math.max(splitAndCalc(leftValue), splitAndCalc(rightValue));
     }
@@ -83,13 +77,4 @@ class Problem1 {
         // 3. (2)에서 구한 값 중 큰 값을 반환
         return Math.max(multiple, sum);
     }
-
-    public static class InvalidPageNumberException extends Exception{
-        public InvalidPageNumberException(){}
-        public InvalidPageNumberException(String message){
-            super(message);
-        }
-
-    }
-
 }
