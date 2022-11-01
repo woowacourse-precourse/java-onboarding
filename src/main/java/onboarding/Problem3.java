d17a47a3897fcfed6b17758ec8ff0f0e28d211c1
Vector1331 <tpgus4796@gmail.com>
2022-11-01 17:17:47 +0900

    [Feat] problem 3 -solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int temp, i = 1;
+        while (i <= number) {
+            temp = i;
+            String str = String.valueOf(i);
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                if (ch == '3' || ch == '6' || ch == '9') {
+                    answer++;
+                }
+            }
+            i++;
+
+        }
         return answer;
     }
 }
