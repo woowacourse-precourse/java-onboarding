1f33b8e5f276fbf218ef1dd820965dce81a22d94
Moveuk <v_donguk@naver.com>
2022-11-01 13:38:23 +0900

    feat: Problem1 알맞은 페이지 값이 들어왔는지 확인하는 기능 구현
    1. 왼쪽과 오른쪽 페이지의 번호가 연속된 숫자가 나오지 않을 경우 ex) 1,4 - (2,3) 쪽이 찢어져 버렸다
    2. 페이지 번호가 1~400의 숫자 안에 존재하지 않는 경우
        - 페이지 번호가 존재하지 않는 경우 포함 ex) 첫 시작 면을 골랐거나 한 쪽 페이지의 번호가 없는 경우
    3. 왼쪽 페이지의 번호가 짝수로 시작할 경우 - 페이지 번호 프린트가 잘못되었다

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,30 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (isCorrectCondition(pobi) && isCorrectCondition(crong)) {
+            answer = 1;
+        } else {
+            answer = -1;
+        }
+
         return answer;
+    }
+
+    public static boolean isCorrectCondition(List<Integer> pageNumbers) {
+        int leftPageNumber = pageNumbers.get(0);
+        int rightPageNumber = pageNumbers.get(1);
+
+        if (leftPageNumber < 1 || 400 < leftPageNumber || rightPageNumber < 1 || 400 < rightPageNumber) {
+            return false;
+        }
+        if ((leftPageNumber + 1) != rightPageNumber) {
+            return false;
+        }
+        if (leftPageNumber % 2 != 1) {
+            return false;
+        }
+
+        return true;
     }
 }
