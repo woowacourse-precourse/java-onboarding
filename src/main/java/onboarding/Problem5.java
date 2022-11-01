f812f44278d72376ee7990884e4062a8d1825a04
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-01 19:07:12 +0900

    problem5: add comment

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,10 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    //currency에 따른 반환할 지폐,화폐 수를 리스트로 반환하는 함수
     private static List<Integer> numOfCurrencyBy(List<Integer> currency, int money){
         List<Integer> answer = new ArrayList<>();
         for(int cur:currency){
