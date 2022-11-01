2b334753431836c05b232428c12a0775c5cfae86
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 11:37:47 +0900

    결과값, 지갑생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) { // 하나의 새로운 지갑을 만들고 몫과 나머지를 이용해서 풀이
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> wallet = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1); // 하나의 리스트에 모두 담아두기
+        List<Integer> result = new ArrayList<>(); // 반환할 결과값
     }
 }
