be8e7107f0a7e1974a3533526a62a0ad9899678c
taehyun <didls1643@gmail.com>
2022-11-01 15:48:12 +0900

    3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+		for(int i = 1; i <= number; i++) {
+			int j = i;
+			while(j != 0) {
+				int n = j%10;
+				if(n == 3 || n == 6 || n == 9) {
+					answer++;
+				}
+				else {}
+				j /= 10;
+			}
+		}
         return answer;
     }
 }
