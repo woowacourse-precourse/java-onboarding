1ccfa815954b29b77bb355e898f0f4fca5ae907d
WonJeongYeon <wonjungyoun@naver.com>
2022-11-01 11:49:56 +0900

    20221101 Problem7.java 예외처리(제한사항) 적용 및 불필요한 코드 삭제

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,5 @@
 package onboarding;
 
-import net.bytebuddy.build.HashCodeAndEqualsPlugin;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -9,11 +7,16 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         List<String> alreadyFriendList = new ArrayList<>();
-        Map<String, Integer> relatedUser = new HashMap<>();
-        for (int i = 0; i<friends.size(); i++) {
-            if (friends.get(i).contains(user)) {
-                alreadyFriendList.add(friends.get(i).get(0));
-                alreadyFriendList.add(friends.get(i).get(1));
+        Map<String, Integer> relatedUser;
+
+        checkUser(user);
+        checkFriends(friends);
+        checkVisitors(visitors);
+
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                alreadyFriendList.add(friend.get(0));
+                alreadyFriendList.add(friend.get(1));
                 alreadyFriendList.remove(user);
             }
         }
@@ -21,48 +24,33 @@ public class Problem7 {
         Map<String, Integer> visitUser = checkWhoVisit(visitors, alreadyFriendList);
         togetherKnowUser.forEach((key, value) -> visitUser.merge(key, value, Integer::sum));
         relatedUser = visitUser;
-        List<Map.Entry<String, Integer>> entries =
-                relatedUser.entrySet().stream()
-                        .sorted(Map.Entry.comparingByKey())
-                        .collect(Collectors.toList());
-        System.out.println(relatedUser + "가나다");
 
-        entries = relatedUser.entrySet().stream()
+        List<Map.Entry<String, Integer>> entries = relatedUser.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toList());
 
-
-        System.out.println(relatedUser);
-
-
         int top5Counter = 5;
-        for (int i = entries.size()-1; i >= 0; i--) {
-            if (top5Counter > 0 && entries.get(i) != null)
-            {
+        for (int i = entries.size() - 1; i >= 0; i--) {
+            if (top5Counter > 0 && entries.get(i) != null) {
                 answer.add(entries.get(i).getKey());
-        }
+            }
             top5Counter--;
         }
-       // answer = new ArrayList<>(relatedUser.keySet());
 
-
-        System.out.println(answer);
         return answer;
     }
 
     public static Map<String, Integer> checkTogetherKnow(String user, List<List<String>> friends, List<String> alreadyFriendList) {
         Map<String, Integer> togetherKnowUser = new HashMap<>();
-        for (int i = 0; i<friends.size(); i++) {
-            for (int j = 0; j<alreadyFriendList.size(); j++) {
-                if (friends.get(i).contains(alreadyFriendList.get(j))) {
+        for (List<String> friend : friends) {
+            for (String s : alreadyFriendList) {
+                if (friend.contains(s)) {
                     for (int k = 0; k < 2; k++) {
-                        if (!Objects.equals(friends.get(i).get(k), alreadyFriendList.get(j)) && !Objects.equals(friends.get(i).get(k), user)) {
-                            if (!togetherKnowUser.containsKey(friends.get(i).get(k))) {
-                                System.out.println("새로운" + friends.get(i).get(k));
-                                togetherKnowUser.put(friends.get(i).get(k), 10);
-                            } else if (togetherKnowUser.containsKey(friends.get(i).get(k))) {
-                                System.out.println("안새로운" + friends.get(i).get(k));
-                                togetherKnowUser.put(friends.get(i).get(k), togetherKnowUser.get(friends.get(i).get(k)) + 10);
+                        if (!Objects.equals(friend.get(k), s) && !Objects.equals(friend.get(k), user)) {
+                            if (!togetherKnowUser.containsKey(friend.get(k))) {
+                                togetherKnowUser.put(friend.get(k), 10);
+                            } else {
+                                togetherKnowUser.put(friend.get(k), togetherKnowUser.get(friend.get(k)) + 10);
                             }
                         }
                     }
@@ -74,39 +62,56 @@ public class Problem7 {
 
     public static Map<String, Integer> checkWhoVisit(List<String> visitors, List<String> alreadyFriendList) {
         Map<String, Integer> visitUser = new HashMap<>();
-        for (int i = 0; i<visitors.size(); i++) {
-            if (!alreadyFriendList.contains(visitors.get(i))) {
-                if (!visitUser.containsKey(visitors.get(i))) {
-                    System.out.println("새로운방문자" + visitors.get(i));
-                    visitUser.put(visitors.get(i), 1);
-                } else if (visitUser.containsKey(visitors.get(i))) {
-                    visitUser.put(visitors.get(i), visitUser.get(visitors.get(i)) + 1);
-                    System.out.println("안새로운방문자" + visitors.get(i));
+        for (String visitor : visitors) {
+            if (!alreadyFriendList.contains(visitor)) {
+                if (!visitUser.containsKey(visitor)) {
+                    visitUser.put(visitor, 1);
+                } else {
+                    visitUser.put(visitor, visitUser.get(visitor) + 1);
                 }
-                System.out.println(visitUser);
             }
         }
         return visitUser;
     }
 
-    public static void main(String[] args) {
-        List<List<String>> friends = List.of(
-                List.of("donut", "andole"),
-                List.of("donut", "jun"),
-                List.of("donut", "mrko"),
-                List.of("donut", "newKing"),
-                List.of("shakevan", "andole"),
-                List.of("shakevan", "jun"),
-                List.of("shakevan", "mrko"),
-                List.of("donut", "newKing"),
-                List.of("king", "andole"),
-                List.of("king", "jun"),
-                List.of("king", "mrko"),
-                List.of("king", "newKing")
-        );
-        List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan", "newKing1", "newKing1", "newKing1");
-        solution("mrko", friends, visitors );
+    public static void checkUser(String user) {
+        if (user.length() < 1 || user.length() > 30) {
+            throw new IllegalStateException("사용자 아이디는 1자 이상, 30자 이하여야 합니다.");
+        }
+        if (!user.toLowerCase().equals(user)) {
+            throw new IllegalStateException("사용자 아이디는 소문자로만 이루어져 있어야 합니다.");
+        }
     }
 
+    public static void checkFriends(List<List<String>> friends) {
+        if (friends.size() < 1 || friends.size() > 10_000) {
+            throw new IllegalStateException("친구 관계 정보의 개수는 1 이상, 10,000 이하여야 합니다.");
+        }
+        for (List<String> friend : friends) {
+            if (friend.size() != 2) {
+                throw new IllegalStateException("친구 관계 정보가 잘못되었습니다.");
+            } else {
+                for (String user : friend) {
+                    if (!user.toLowerCase().equals(user)) {
+                        throw new IllegalStateException("사용자 아이디는 소문자로만 이루어져 있어야 합니다.");
+                    } else if (user.length() < 1 || user.length() > 30) {
+                        throw new IllegalStateException("사용자 아이디는 1자 이상, 30자 이하여야 합니다.");
+                    }
+                }
+            }
+        }
+    }
 
+    public static void checkVisitors(List<String> visitors) {
+        if (visitors.size() > 10_000) {
+            //visitors는 길이가 0 이상 10,000 이하인 리스트/배열이다. 이 때, visitors는 리스트이므로 0 미만이 될 수 없다.
+            throw new IllegalStateException("방문자 수는 0명 이상, 10,000명 이하여야 합니다.");
+        } else {
+            for (String visitor : visitors) {
+                if (!visitor.toLowerCase().equals(visitor)) {
+                    throw new IllegalStateException("사용자 아이디는 소문자로만 이루어져 있어야 합니다.");
+                }
+            }
+        }
+    }
 }
