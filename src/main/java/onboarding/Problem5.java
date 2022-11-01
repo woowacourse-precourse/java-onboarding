86f3ba3f978fcac24ba78c169a5605fa188a1a9e
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 23:13:22 +0900

    style:Problem5 비효율적인 구조 개선.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,23 +6,13 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        answer.add(money/50000);
-        if (money >= 50000) money %= 50000;
-        answer.add(money/10000);
-        if (money >= 10000) money %= 10000;
-        answer.add(money/5000);
-        if (money >= 5000) money %= 5000;
-        answer.add(money/1000);
-        if (money >= 1000) money %= 1000;
-        answer.add(money/500);
-        if (money >= 500) money %= 500;
-        answer.add(money/100);
-        if (money >= 100) money %= 100;
-        answer.add(money/50);
-        if (money >= 50) money %= 50;
-        answer.add(money/10);
-        if (money >= 10)money %= 10;
-        answer.add(money);
+
+        int[] standardMoneyArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int standardMoney: standardMoneyArray) {
+            answer.add(money/standardMoney);
+            money %= standardMoney;
+        }
 
         return answer;
     }
