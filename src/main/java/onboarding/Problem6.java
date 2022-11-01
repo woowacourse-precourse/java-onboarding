3c90c481574ec1ad584f00ec638cc29860d2502f
ChaCha3088 <cha3088@gmail.com>
2022-11-01 12:31:26 +0900

    feat: problem6 new function isThereMatch

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,9 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static Boolean isThereMatch(String string) {
+        Boolean result = string.matches(".*([a-zA-Z])\\1+.*");
+        return result;
+    }
 }
