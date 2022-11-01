8d2e3d8015c0ee060f648e9a514f95fcf3d57e72
donghyun <9703l8@AL02038907.local>
2022-11-01 23:00:35 +0900

    six-zero

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,7 +6,35 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+
         List<String> answer = new ArrayList<>();
+        List<String> email = new ArrayList<>();
+
+        for (int i = 0; i < forms.size()-1; i++) {
+            String check = forms.get(i).get(1);
+
+            for (int j = i + 1; j < forms.size(); j++) {
+
+                for (int k = 0; k < forms.get(i).get(1).length() - 1; k++) {
+
+                    for (int l = 0; l < forms.get(j).get(1).length() - 1; l++) {
+
+                        if (check.charAt(k) == forms.get(j).get(1).charAt(l)
+                                && check.charAt(k + 1) == forms.get(j).get(1).charAt(l + 1)) {
+
+                            email.add(forms.get(i).get(0));
+                            email.add(forms.get(j).get(0));
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0;i<email.size();i++){
+            if(!answer.contains(email.get(i))){
+                answer.add(email.get(i));
+            }
+        }
+        answer.sort(Comparator.naturalOrder());
         return answer;
     }
 }
