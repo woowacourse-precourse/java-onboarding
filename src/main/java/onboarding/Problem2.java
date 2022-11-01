a8082b979caca394a87627e12e03c02e9b164c4b
geonho <soo_1992@naver.com>
2022-11-01 17:46:25 +0900

    Problem2 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,39 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String temp = "";
+        boolean isChanged = true;
+
+        while (isChanged) {
+
+            isChanged = false;
+            temp = "";
+
+            for (int i = 0; i < cryptogram.length(); i++) {
+                if (i == 0) {
+                    if (cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
+                        temp += cryptogram.charAt(i);
+                    } else {
+                        isChanged = true;
+                    }
+                } else if (i == cryptogram.length() - 1) {
+                    if (cryptogram.charAt(i) != cryptogram.charAt(i - 1)) {
+                        temp += cryptogram.charAt(i);
+                    } else {
+                        isChanged = true;
+                    }
+                } else {
+                    if (cryptogram.charAt(i) != cryptogram.charAt(i + 1) && cryptogram.charAt(i) != cryptogram.charAt(i - 1)) {
+                        temp += cryptogram.charAt(i);
+                    } else {
+                        isChanged = true;
+                    }
+                }
+            }
+
+            cryptogram = temp;
+        }
+
+        return cryptogram;
     }
 }
