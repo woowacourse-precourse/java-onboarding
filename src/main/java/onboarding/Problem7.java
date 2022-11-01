130f20fb5712b9b807af0e41bea57ddc4b0f059d
KimSoo <ilppang@naver.com>
2022-11-01 19:18:52 +0900

    Add(김현수/Problem7): 유저 친구목록 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+    public static List<String>  solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> answer = Collections.singletonList("aa");
+        List<String> userFriendList = getUserFriendList(user, friends);
+
         return answer;
+    }
+    public static List<String> getUserFriendList(String user, List<List<String>> friends){
+        List<String> userFriendList = new ArrayList<>();
+        for (List<String> friend : friends){
+            if (friend.indexOf(user) == 1){
+                userFriendList.add(friend.get(0));
+            }
+            if (friend.indexOf(user) == 0){
+                userFriendList.add(friend.get(1));
+            }
+        }
+        return userFriendList;
     }
 }
