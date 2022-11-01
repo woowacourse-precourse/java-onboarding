d68a345a78cac22d089fa971aebf8459b15b93db
Jaewoong Kim <kimje1109@gmail.com>
2022-11-01 11:08:44 +0900

    feat: Add find369

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static int find369(int number) {
+        String numberString = Integer.toString(number);
+        int count = 0;
+
+        for (int index = 0; index < numberString.length(); index++) {
+            if (numberString.charAt(index) == '3' || numberString.charAt(index) == '6' || numberString.charAt(index) == '9') {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
