b418b6f79e3c559e516cf21578a1f553d200f50e
yeeun <dpdms9621@naver.com>
2022-11-01 16:43:39 +0900

    [Problem3] 자리수마다 3,6,9가 해당하면 count += 1 해주는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -9,6 +9,15 @@ public class Problem3 {
             //number는 변하면 안되기 때문에 copy생성
             int copy = i;
 
+            //자리수마다 3,6,9가 해당하면 count += 1 해주는 기능
+            while(copy > 0) {
+                //10으로 나눈 나머지
+                int cal = copy % 10;
+                //계산
+                if(cal == 3 || cal == 6 || cal == 9 ) count += 1;
+                //계속해서 10으로 나눈 몫 저장
+                copy /= 10;
+            }
         }
 
         int answer = count;
