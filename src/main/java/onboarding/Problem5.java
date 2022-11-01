401b5d0761e827f122f85696c64bbffbdc76078d
ckyeon <qsky8520@gmail.com>
2022-11-01 20:46:43 +0900

    Add MoneyUnit enum to Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,4 +8,22 @@ public class Problem5 {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    private enum MoneyUnit {
+        FIFTY_THOUSAND_WON(50000),
+        TEN_THOUSAND_WON(10000),
+        FIVE_THOUSAND_WON(5000),
+        THOUSAND_WON(1000),
+        FIVE_HUNDRED_WON(500),
+        HUNDRED_WON(100),
+        FIFTY_WON(50),
+        TEN_WON(10),
+        ONE_WON(1);
+
+        private final int value;
+
+        MoneyUnit(int value) {
+            this.value = value;
+        }
+    }
 }
