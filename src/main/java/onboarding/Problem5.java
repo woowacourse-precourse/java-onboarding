89042f1989829b064a2f5b945939a88964476fdc
Park Sungchul <try615@naver.com>
2022-11-01 22:05:03 +0900

    [feat] Problem5.java : 주석 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -3,20 +3,19 @@ package onboarding;
 import java.util.*;
 
 public class Problem5 {
-    public static void main(String[] args) {
-        int money = 15000;
-        System.out.println(solution(money));
-    }
-
+    /**
+     * 화폐 단위를 저장한 전역 정수 배열
+     */
     private static int[] moneys = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
         List<Integer> result = new ArrayList<>();
 
+        // 전체 화폐 단위 순회
         for (int i = 0; i < moneys.length; i++) {
             int m = money / moneys[i];
-
             money -= moneys[i] * m;
+
             result.add(m);
         }
 
