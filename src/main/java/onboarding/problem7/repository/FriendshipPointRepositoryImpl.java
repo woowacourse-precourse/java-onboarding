7b70926a71e8ca14ab9cd9e4441b181dffa8fd28
이경범 <tkdwk567@naver.com>
2022-11-01 13:22:18 +0900

    # 7 solution @@ refactoring -- Point 저장소 생성 및 로직 분리

--- src/main/java/onboarding/problem7/repository/FriendshipPointRepositoryImpl.java
+++ src/main/java/onboarding/problem7/repository/FriendshipPointRepositoryImpl.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class FriendshipPointRepositoryImpl implements FriendshipPointRepository{
+public class FriendshipPointRepositoryImpl implements PointRepository {
 
     private static Map<String, Integer> friendPoints = new LinkedHashMap<>();
+    private static UserRepository userRepository = new UserRepositoryImpl();
 
     @Override
     public void save(String userName, int point) {
@@ -20,10 +21,11 @@ public class FriendshipPointRepositoryImpl implements FriendshipPointRepository{
     }
 
     @Override
-    public List<String> friendshipOrderByPointAndLimitFive() {
+    public List<String> friendshipOrderByPointAndLimitFive(String userName) {
         return friendPoints.entrySet().stream()
                 .sorted(((o1, o2) -> o2.getValue().compareTo(o1.getValue())))
                 .map(Map.Entry::getKey)
+                .filter(s -> !userRepository.findFriends(userName).contains(s))
                 .limit(5)
                 .collect(Collectors.toList());
     }
--- src/main/java/onboarding/problem7/repository/PointRepository.java
+++ src/main/java/onboarding/problem7/repository/PointRepository.java
@@ -2,9 +2,9 @@ package onboarding.problem7.repository;
 
 import java.util.List;
 
-public interface FriendshipPointRepository {
+public interface PointRepository {
 
     void save(String userName, int point);
     int findFriendshipPoint(String userName);
-    List<String> friendshipOrderByPointAndLimitFive();
+    List<String> friendshipOrderByPointAndLimitFive(String userName);
 }
