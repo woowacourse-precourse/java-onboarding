f0a39e3438c4b4a2df4c9fa72647419c56f543fc
seungjun <seungjun123>
2022-11-01 13:44:23 +0900

    2글자 단위로 쪼개서 나온 횟수를 기록해두는 해쉬맵 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        HashMap<String, Integer> letters = makeLetter(forms);
+
         return answer;
+    }
+
+    private static HashMap<String, Integer> makeLetter(List<List<String>> forms) {
+        HashMap<String, Integer> letters = new HashMap<>();
+        for (List<String> crew : forms) {
+            String nickname = crew.get(1);
+            for (int j=0; j<nickname.length()-1; j++) {
+                String nick = nickname.substring(j, j+2);
+                letters.put(nick, letters.getOrDefault(nick, 0)+1);
+            }
+        }
+        return letters;
     }
 }
