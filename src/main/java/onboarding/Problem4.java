eebb7e8dc89a4176efe583d5e4bc7820afe99549
Lee-CntTheWins <Lee-CntTheWins>
2022-11-01 23:26:58 +0900

    week1 problem 1-6

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class Problem4 {
 
-    /*
     public static void main(String[] args) {
         System.out.println(solution("I love you"));
     }
-*/
+
     public static String solution(String word) {
         String answer = "";
 
