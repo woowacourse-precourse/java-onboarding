4f013cdc8f2c615afc32d6add1adc57df2c0ecb6
이건회 <rg970604@naver.com>
2022-11-01 20:05:20 +0900

    feat(Problem1.java) : 예외 검증 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -11,4 +11,23 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static boolean validateWrongPageOrder(List<Integer> pobi, List<Integer> crong) {
+        return pobi.get(0).intValue() + 1 != pobi.get(1).intValue()
+            || crong.get(0).intValue() + 1 != crong.get(1).intValue();
+    }
+
+    private static void validateMemberSize(int POBISIZE, int CRONGSIZE) {
+        if (POBISIZE != FIXED_SIZE || CRONGSIZE != FIXED_SIZE) {
+            throw new IllegalArgumentException("pobi와 crong의 길이는 2여야 합니다");
+        }
+    }
+    private static boolean validatePageNum(List<Integer> member) {
+        for (Integer pageNum : member) {
+            if (pageNum < MIN_PAGE_NUM || pageNum > MAX_PAGE_NUM) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
