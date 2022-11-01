4ebf23c56932da957e678ae0cb24724d08d3f30b
dydckd <toofriend09@naver.com>
2022-11-01 21:30:36 +0900

    problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return slove(cryptogram);
+    }
+    static String slove(String n) {
+        String result1= "";
+        String result = "";
+        for (int i = 0; i < n.length(); i++) {
+            if (n.charAt(i) == (n.charAt(i + 1))) {
+                for (int j = 0; j < i; j++) {
+                    result += n.charAt(j);
+                }
+                for (int k = i + 2; k < n.length(); k++) {
+                    result += n.charAt(k);
+                }
+                break;
+
+            }
+
+        }
+        for (int i = 0; i < result.length()-1; i++) {
+            if (result.charAt(i) == (result.charAt(i + 1))) {
+                return slove(result);
+            }
+
+        }
+        return result;
     }
 }
