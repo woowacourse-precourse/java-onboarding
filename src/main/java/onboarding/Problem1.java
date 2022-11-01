6525b3fefd8ddc1d90f24a031ad68ec01f47806c
ddukddaki <rlfrkdms1@naver.com>
2022-11-01 13:20:59 +0900

    feat(Problem1): 예외사항 - 페이지가 연속적이지 않을 경우

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,9 +13,16 @@ import java.util.List;
  * 4. max 값을 도출하는 기능
  * 5. pobi와 crong의 승부를 판정하는 기능
  */
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        //예외 사항 - 페이지가 연속적이지 않을 경우
+        if (pobi.get(1) - pobi.get(0) != 1 || crong.get(1) - crong.get(0) != 1) {
+            return -1;
+        }
+
         return answer;
     }
 }
