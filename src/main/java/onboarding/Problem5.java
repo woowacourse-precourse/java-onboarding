a69c411707d57f54a4b369e4ad73cffd77335ba1
JJongmen <pjy90123@gmail.com>
2022-11-01 16:21:58 +0900

    refactor(Problem5): 필요없는 if문 리팩토링

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,17 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    private static final int[] moneys = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+    private static final List<Integer> UNITS_OF_KRW = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
 
     public static List<Integer> solution(int money) {
         List<Integer> wallet = new ArrayList<>();
-        for (int i = 0; i < 9; i++) {
-            if (money < moneys[i]) {
-                wallet.add(0);
-                continue;
-            }
-            wallet.add(money / moneys[i]);
-            money %= moneys[i];
+        for (int kindOfKRW : UNITS_OF_KRW) {
+            wallet.add(money / kindOfKRW);
+            money %= kindOfKRW;
         }
         return wallet;
     }
