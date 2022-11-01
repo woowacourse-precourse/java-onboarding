33c32bf0198be48ac789084bd189b5ffd493f89f
younghoon211@naver.com <younghoon211@naver.com>
2022-11-01 21:01:29 +0900

    feat(Problem2): solution

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<String> stack = new Stack<>();
+        StringBuffer sb = new StringBuffer();
+        String[] strings = cryptogram.split("");
+
+        stringStack(stack, strings);
+
+        for (String a : stack) {
+            sb.append(a);
+        }
+
+        return String.valueOf(sb);
     }
 }
