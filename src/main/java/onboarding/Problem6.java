21f51f3c835bb3c5855887c85b931a21bbb0e62e
glass <glass7312@gmail.com>
2022-11-01 17:58:54 +0900

    Problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,8 +5,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Problem6 {
+    public static List<String> get(List<String> ls1,List<String> ls2) {
+
+    }
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        for(int i=0; i<forms.size(); i++) {
+            for (int j =i+1; j < forms.size(); j++) {
+                if(null==get(forms.get(i),forms.get(j))) continue;
+
+                answer.add(get(forms.get(i),forms.get(j)).get(0));
+                answer.add(get(forms.get(i),forms.get(j)).get(1));
+
+            }
+        }answer=answer.stream().distinct().collect(Collectors.toList());
+
         return answer;
 
     }
