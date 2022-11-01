5f941cdc73b76941496a4eadadaf8bc6de2a57c6
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-01 23:51:33 +0900

    [feat] Problem4 대문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,7 +9,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        
+        for(char letter : word.toCharArray()){
+            if((65 <= letter && letter <= 90) ||
+                    (97 <= letter && letter <122)){
+                char c = 0;
+                if (letter <= 90)
+                    c = (char) (65 + 90 - letter);
+                answer += Character.toString(c);
+            }
+        }
         return answer;
     }
 }
