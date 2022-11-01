3821e132730da98dd25706cb9d421f0844a47714
mckim123 <minchan06@gmail.com>
2022-11-01 12:22:48 +0900

    문제 2 기능 1 수정(문제 2 완료)
    문제 2의 풀이 방식을 수정하였습니다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,16 +6,15 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int pobi_score = max(pobi);
         int crong_score = max(crong);
-
+        int answer = 0;
         if (pobi_score == -1 || crong_score == -1) {
-            return -1;
+            answer = -1;
         } else if (pobi_score > crong_score){
-            return 1;
+            answer = 1;
         } else if (pobi_score < crong_score){
-            return 2;
-        } else {
-            return 0;
+            answer = 2;
         }
+        return answer;
     }
     static int max(List<Integer> pages) {
         int page1 = pages.get(0);
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,30 +1,42 @@
 package onboarding;
 
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        Stack<String> stack = new Stack<>();
-        int i = 0;
-        int N = cryptogram.length();
-
-        while (i < N){
-            String next = Character.toString(cryptogram.charAt(i));
-            if (stack.empty()) {
-                stack.push(next);
-                i += 1;
-            } else if (stack.peek().equals(next)){
-                stack.pop();
-                i += 1;
-            } else {
-                stack.push(next);
-                i += 1;
+        String[] cryptoArr = cryptogram.split("");
+        ArrayList<String> answerList = new ArrayList<>(Arrays.asList(cryptoArr));
+        ArrayList<String> tempList = new ArrayList<>();
+        boolean removed = true;         // 반복시 중복 제거 여부(재실시할지 판단)
+        boolean duplicated = false;     // 수정한지 여부
+        while(removed){
+            removed = false;
+            if (answerList.size() == 0){
+                return "";
             }
-        }
-        String answer = "";
-        for (String s:stack){
-            answer = answer + s;
-        }
-        return answer;
+            String curr = answerList.get(0);
+            tempList.clear();
+            for(int i=1;i<answerList.size();i++) {
+                String next = answerList.get(i);
+                if  (curr.equals(next)) {
+                    removed = true;
+                    duplicated = true;
+                } else if(duplicated){
+                    curr = next;
+                    duplicated = false;
+                } else {
+                    tempList.add(curr);
+                    curr = next;
+                }
+            }
+            if (!duplicated) {
+                tempList.add(curr);
+            }
+            answerList.clear();
+            answerList.addAll(tempList);
+            }
+
+        return String.join("", answerList);
     }
 }
