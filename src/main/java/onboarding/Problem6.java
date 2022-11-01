f5121eb2ebd3c1156ac6ac7e925cb0c4493e76e5
Yh0sun <hy770819@naver.com>
2022-11-01 17:05:20 +0900

    problem6:func3 add

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -14,15 +14,18 @@ public class Problem6 {
         //3. 글자 포함된 닉네임이 있었다면 1.닉네임도 ans에 이메일 넣음
         for (int i = 0; i < forms.size(); i++) {
             String nickName = forms.get(i).get(1);
+            boolean flag = false;
             for (int j = 0; j < nickName.length() - 1; j++) {
                 String nameStr = nickName.substring(j, j + 2);
                 for (int k = 0; k < forms.size(); k++) {
                     if (i == k) continue;
                     if (forms.get(k).get(1).contains(nameStr)) {
                         ans.add(forms.get(k).get(0));
+                        flag = true;
                     }
                 }
             }
+            if (flag) ans.add(forms.get(i).get(0));
         }
         List<String> answer = new ArrayList<>(ans);
         return answer;
