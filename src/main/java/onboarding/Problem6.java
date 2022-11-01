61191f1418a65cfea8a52cc6c70a9aff80367776
Identityless <rlandwnd555@naver.com>
2022-11-01 21:18:47 +0900

    FEAT : 닉네임 리스트를 받아 각 닉네임을 2글자 크기의 substring으로 만들어주는 기능의 getNameToken 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,7 @@ import java.util.*;
 /*
 *   기능 목록
 *   1. 조건을 만족하는 닉네임을 가진 크루만 모아놓은 리스트를 만드는 기능
-*   2. 모든 이름에서 패턴과 일치하는 부분이 있는지 탐색하는 기능
+*   2. 크루들의 닉네임을 2글자 크기의 토큰으로 나누는 기능
 *   3. 패턴이 나타난 이름을 forms 리스트에서 제외하고 result 리스트에 넣는 기능
 */
 public class Problem6 {
@@ -26,5 +26,17 @@ public class Problem6 {
             }
         }
         return list;
+    }
+
+    static List<List<String>> getNameToken(List<String> names) {
+        List<List<String>> nameToken = new ArrayList<>();
+        for (String name : names) {
+            List<String> tokenList = new ArrayList<>();
+            for (int j = 2; j <= name.length(); j++) {
+                tokenList.add(name.substring(j - 2, j));
+            }
+            nameToken.add(tokenList);
+        }
+        return nameToken;
     }
 }
