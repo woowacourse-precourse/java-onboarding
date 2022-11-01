88c24d600bde61bab80cf64d800cf20efd4a5912
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 18:33:25 +0900

    각 지폐별로 돈을 나누고 몫은 answer에 추가하고 나머지는 돈을 업데이트하고 answer를 리턴한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -12,6 +12,13 @@ public class Problem5 {
     }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] bills = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int bill : bills) {
+            int numberOfBill = calculateQuotient(money, bill);
+            int leftMoney = calculateRemainder(money, bill);
+            answer.add(numberOfBill);
+            money = leftMoney;
+        }ㄴ으
         return answer;
     }
 }
