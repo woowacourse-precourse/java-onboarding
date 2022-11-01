f56cae98311b28fc1356c0addd086d007ebd98a8
wlwlwon <ryanwon@naver.com>
2022-11-01 20:28:42 +0900

    Problem3 메서드 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem3 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(13));
+    }
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 3; i <= number; i++) {
+            String next = String.valueOf(i);
+            String[] num = next.split("");
+
+            for (String str : num) {
+                int tmp = Integer.parseInt(str);
+                if(tmp == 0)
+                    continue;
+                if (tmp % 3 == 0) {
+                    answer++;
+                }
+            }
+        }
+
         return answer;
     }
 }
