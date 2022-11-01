99f1feb8b104d2f5f7a260f5063a9acbeeb9483c
ChaCha3088 <cha3088@gmail.com>
2022-11-01 11:15:23 +0900

    feat: problem5 new function billAndCoin

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,10 +2,23 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    private static Map<Integer, Integer> billAndCoin = Map.ofEntries(
+            Map.entry(50000, 50000),
+            Map.entry(10000, 10000),
+            Map.entry(5000, 5000),
+            Map.entry(1000, 1000),
+            Map.entry(500, 500),
+            Map.entry(100, 100),
+            Map.entry(50, 50),
+            Map.entry(10, 10),
+            Map.entry(1, 1)
+    );
 }
