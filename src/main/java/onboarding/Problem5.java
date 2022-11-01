271778c62862a3cded18056f2121812a0d391345
yewon <yewon.ux@gmail.com>
2022-11-01 20:53:48 +0900

    기능 목록 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        /*
+        기능 목록
+        1. 값을 저장할 빈 배열 생성
+        2. 금액 별 count 변수 생성 및 초기화
+        3. 금액을 높은 단위(50000)부터 나눈 후 몫은 count 변수에, 나머지 값은 money에 전달
+        4. 빈 배열에 금액 별 count 값 전달
+         */
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
