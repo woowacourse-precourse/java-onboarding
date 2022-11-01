25cb1a674e81b5dbc9f35950cb057d48846e4e54
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 14:01:43 +0900

    FEAT deduplicate

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
         return answer;
+    }
+
+    public static String deduplicate(String s) {
+        return s.replaceAll("(([a-zA-Z\\d])\\2+)", "");
     }
 }
