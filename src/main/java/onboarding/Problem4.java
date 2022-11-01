b0891bb9b546cbfeccfea14a609319fe4c292ad7
JUNBEOM_LEE <bom1215@naver.com>
2022-11-01 16:45:13 +0900

    alphabet change function of mission4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,38 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+
+        for (int i = 0; i < word.length(); i++) {
+            char a = word.charAt(i);
+            byte num = (byte) a;
+
+
+//                    대문자 알파벳일때
+            if (num >= 65 && num <=90){
+                int num2 = 155 -num  ;
+
+                char num_to_char =  (char) num2;
+                sb.append(num_to_char);
+
+//                    소문자 알파벳일때
+            }else if (num >= 97 && num <= 122){
+                int num2 = 219 -num  ;
+
+                char num_to_char =  (char) num2;
+                sb.append(num_to_char);
+            }else if (num ==32){
+                int num2 = num;
+                char num_to_char =  (char) num2;
+                sb.append(num_to_char);
+            }else{
+                continue;
+            }
+
+        }
+        System.out.println(sb.getClass().getName());
+        String answer = sb.toString();
         return answer;
     }
 }
