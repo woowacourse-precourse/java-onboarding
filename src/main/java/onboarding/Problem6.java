364be92ea4514d38e584e1b0af899d95c36f6644
ECHO <eunchan1001@gmail.com>
2022-11-01 11:55:42 +0900

    feat(Problem6): 중복 단어 저장한 변수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Set<String> result = new TreeSet<String>();
+        Set<String> duplicatedWords = new HashSet<String>();
         return result.stream().collect(Collectors.toList());
     }
 
