edc478cb3bf56d9cee76bfde31eef5a329505afc
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-01 16:13:22 +0900

    백엔드 5번문제 분석
    for문과 if문을 활용하여 원하는 값을 도출

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        /*
+        * 5만원권부터 1원 동전까지의 값이 들어있는 배열 생성
+        * money를 위의 배열을 통해 for문으로 나눈뒤 0이 아닌 값이 나왔을 경우 money에서 값을 감산
+        * money를 나눈 몫이 0인 경우 answer의 배열에 0을 추가하며 다음 루프로 이동
+        * */
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int money = 15_000;
+        System.out.println(solution(money));
     }
 }
