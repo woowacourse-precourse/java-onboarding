d8448af3c3c0c90de521cebbdfae545822a4b682
MuSeung <peter0757@nvaer.com>
2022-11-01 18:43:26 +0900

    refactor : Problem5의 unitCounting메소드 삭제하고 unitizing 메소드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,15 +9,17 @@ public class Problem5 {
 			{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 	
 	public static List<Integer> solution(int money) {
-		List<Integer> answer = new ArrayList<>();
-		int balance = money;
-		for (int i = 0; i < BANKNOTE_UNIT.length; i++) {
-			answer.add(unitCounting(balance, BANKNOTE_UNIT[i]));
-			balance -= answer.get(i) * BANKNOTE_UNIT[i];
-		}
+		List<Integer> answer = Collections.emptyList();
+		answer = unitizing(money);
 		return answer;
 	}
-	public static int unitCounting(int money, int unit) {
-		return money / unit;
+	private static List<Integer> unitizing(int money) {
+		List<Integer> unitizedMoney = new ArrayList<>();
+		int balance = money;
+		for(int i=0; i < BANKNOTE_UNIT.length; i++) {
+			unitizedMoney.add(balance/BANKNOTE_UNIT[i]);
+			balance -= unitizedMoney.get(i) * BANKNOTE_UNIT[i];
+		}
+		return unitizedMoney;
 	}
 }
