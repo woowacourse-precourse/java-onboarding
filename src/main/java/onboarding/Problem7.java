e93fb5e7c812169e48f55f6e14c720ee6540321f
송민진 <m_6595@naver.com>
2022-11-01 15:12:46 +0900

    #7 feat: make user's friendList using given friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    private List<String> makeFriendList(String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
+        for (List<String> relation : friends) {
+            if (relation.contains(user)) {
+                relation.remove(user);
+                friendList.add(relation.get(0));
+            }
+        }
+        return friendList;
     }
 }
