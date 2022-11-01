d6f188015891c0a79c405b76d7cf4d4dcbe66617
TeddyYeoni <po99027@naver.com>
2022-11-01 23:34:10 +0900

    PROBLEM4 기능1 상수 지정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+	public static final int UPPER_MIN_VALUE = 'A';
+	public static final int UPPER_MAX_VALUE = 'Z';
+	public static final int LOWER_MIN_VALUE = 'a';
+	public static final int LOWER_MAX_VALUE = 'z';
+	
     public static String solution(String word) {
         String answer = "";
         return answer;
