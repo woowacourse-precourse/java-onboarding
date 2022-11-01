5aa3f4ed331d0db8a46eb694073e6d03a1afd8bb
gitFILO <x1xgudwls@naver.com>
2022-11-01 23:11:21 +0900

    [Problem 7] user의 친구를 찾는 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        ArrayList<String> friendsOfUser = new ArrayList<>();
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+        List<String> answer = new ArrayList<>();
+
+        findFriendsOfUser(friends, friendsOfUser, user); // user의 친구 찾기
         return answer;
     }
+
+    public static void findFriendsOfUser(List<List<String>> friends, ArrayList<String> friendsOfUser, String user) {
+        String firstPerson;
+        String secondPerson;
+        for (int i = 0; i < friends.size(); i++) {
+            firstPerson = friends.get(i).get(0);
+            secondPerson = friends.get(i).get(1);
+
+            if (firstPerson == user) friendsOfUser.add(secondPerson);
+            else if (secondPerson == user) friendsOfUser.add(firstPerson);
+        }
+    }
+
 }
