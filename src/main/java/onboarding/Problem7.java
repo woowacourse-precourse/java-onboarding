a20c682776f781d9c5dedd3924d3a5e93bf2135f
이건회 <rg970604@naver.com>
2022-11-01 21:11:00 +0900

    feat(Problem7.java) : 상수 선언

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    private static final int MIN_USER_SIZE = 1;
+    private static final int MAX_USER_SIZE = 30;
+    private static final int MIN_FRIENDS_SIZE = 1;
+    private static final int MAX_FRIENDS_SIZE = 10000;
+    private static final int REQULAR_FRIEND_RELATIONSHIP_SIZE = 2;
+    private static final int MIN_ID_SIZE = 1;
+    private static final int MAX_ID_SIZE = 30;
+    private static final String ENGLISH_PATTERN = "^[a-z]*$";
+    private static final int MAX_RECOMMENDATION_NUM = 5;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
