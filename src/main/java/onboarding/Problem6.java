b5e90796bafd0a145eb729dd938a7b908de4eec6
chanwonlee <clw8679@naver.com>
2022-11-01 10:51:41 +0900

    REFACTOR : [Problem6] 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,17 +4,16 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-
         //중복을 제거하기 위해 set 사용
         Set<String> set = new HashSet<>();
         int n = forms.size();
         for (int i = 0; i < n-1; i++) {
             for (int j = i + 1; j < n; j++) {
-                String name1 = forms.get(i).get(1);
-                String name2 = forms.get(j).get(1);
-                if (hasDuplication(name1, name2)) {
-                    addEmail(forms.get(i), set);
-                    addEmail(forms.get(j), set);
+                List<String> form1 = forms.get(i);
+                List<String> form2 = forms.get(j);
+                if (hasDuplication(form1.get(1), form2.get(1))) {
+                    set.add(form1.get(0));
+                    set.add(form2.get(0));
                 }
             }
         }
@@ -24,26 +23,12 @@ public class Problem6 {
         return list;
     }
 
-    public static void addEmail(List<String> form, Set<String> emailList) {
-        emailList.add(form.get(0));
-
-    }
-
     public static boolean hasDuplication(String name1, String name2) {
-        int n1 = name1.length();
-        int n2 = name2.length();
-
-        for (int i = 0; i < n1 - 1; i++) {
-            for (int j = 0; j < n2 - 1; j++) {
-                if (findDuplication(name1, name2, i, j)) return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean findDuplication(String name1, String name2, int i, int j) {
-        if (name1.charAt(i) == name2.charAt(j)) {
-            return name1.charAt(i + 1) == name2.charAt(j + 1);
+        int n = name1.length();
+        for (int i = 0; i < n - 1; i++) {
+            //연속된 2글자씩 끊어서 중복여부 확인
+            String twoWords = name1.substring(i, i + 2);
+            if(name2.contains(twoWords)) return true;
         }
         return false;
     }
