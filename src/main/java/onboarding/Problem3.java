926866690ab82b5c623e2773e4df5cb9578a887c
0417taehyun <0417taehyun@gmail.com>
2022-11-01 14:54:32 +0900

    feat: [문제3] 메서드 및 풀이 구조 구축

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem3 {
+    public static int countTargetNumber(int targetNumber) {
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        for (int targetNumber = 1; targetNumber <= number; targetNumber++) {
+            answer += countTargetNumber(targetNumber);
+        }
+
         return answer;
     }
 }
