cec63baa4b3a3f144309404d921188303736a4b3
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-11-01 20:42:18 +0900

    feat: Problem5 풀이

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final List<Integer> VALUES = Arrays.asList(
+            50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
+    );
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
+        for (Integer value : VALUES) {
+            answer.add(money / value);
+            money -= (money / value) * value;
+        }
         return answer;
     }
 }
