d67e6acb2734fba2a8b8e546455e65f4ace4adf6
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-01 20:30:27 +0900

    (문제5) feat : 남은 돈이 없을 때까지 사용한 지폐 개수를 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
+    private static final int[] moneyItem = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        List<Integer> answer = calculateMoney(money);
+
         return answer;
     }
+
+    private static List<Integer> calculateMoney(int money) {
+        List<Integer> moneyCount = new ArrayList<>();
+
+        for (int item : moneyItem) {
+            int share = money / item;
+            moneyCount.add(share);
+            money %= item;
+        }
+
+        return moneyCount;
+    }
+
 }
