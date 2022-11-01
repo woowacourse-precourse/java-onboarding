1a776a3bb864dd7e40d1d0d5fc2bb0758746c0b3
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 13:41:16 +0900

    [1번] feat: 한 숫자씩 나눠서 하나의 배열로 만들기

--- src/main/java/onboarding/Problem1Package/Calc.java
+++ src/main/java/onboarding/Problem1Package/Calc.java
@@ -1,5 +1,16 @@
 package onboarding.Problem1Package;
 
-public interface Calc {
+import java.util.ArrayList;
+import java.util.List;
 
+public interface Calc {
+    // 1. 한 숫자씩 나눠서 하나의 배열로 만들기
+    default List<Integer> getListByInteger(Integer num) {
+        ArrayList<Integer> arrNum = new ArrayList<>();
+        while(num > 0) {
+            arrNum.add(num % 10);
+            num /= 10;
+        }
+        return arrNum;
+    }
 }
