9fab3ee15f6571a2d728606905e3f82157f4c962
김형기 <khk211113@naver.com>
2022-11-01 23:23:55 +0900

    [FEAT] Problem4 - 알파벳 위치로 청개구리 적용 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,14 @@ public class Problem4 {
         return answer;
     }
 
+    // 한 문자에 대해 청개구리 사전을 적용
+    public static char transChar(char ch) {
+        int vector = getVector(ch);
+        if (vector == -1) return ch;
+
+        return (char) (ch + (25 - 2 * vector));
+    }
+
     // 대소문자 상관없이 몇 번째 알파벳인지 반환
     private static int getVector(char ch) {
         if ('a' <= ch && ch <= 'z')
