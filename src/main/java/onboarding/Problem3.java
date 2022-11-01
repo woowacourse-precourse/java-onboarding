4d25eee435471941a509d1d5effafd00ae0909e6
yujindonut <hyujin0825@gmail.com>
2022-11-01 22:53:43 +0900

    [CHORE] 코드 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,18 +1,29 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
 
-        if (number < 1 || number > 10000) {
-            return -1;
+    static final int EXCEPTION = -1;
+    public static int solution(int number) {
+
+        if (!inRange(number)){
+            return EXCEPTION;
         }
 
-        for (int i = 1; i <= number; i++) {
+
+        return addNum(number);
+    }
+    public static boolean inRange(int number) {
+        return number >= 1 && number <= 10000;
+    }
+
+    public static int addNum(int num) {
+        int answer = 0;
+        for (int i = 1; i <= num; i++) {
             answer += getNum(i);
         }
         return answer;
     }
+
     public static int getNum(int num){
         String[] numStr = String.valueOf(num).split("");
         int flag = 0;
