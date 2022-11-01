9ee0292f00e9e4854073dd43968fa0d58c9c6325
dingding-21 <phj8221@naver.com>
2022-11-01 22:57:32 +0900

    feat: Problem5 solution함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,11 @@
 package onboarding;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getCoinNum(money);
     }
 
     private static List<Integer> getCoinNum(int money) {
