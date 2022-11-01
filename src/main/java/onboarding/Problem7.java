efff7ea1d0fd702773f0a4f79cd86bf1b243e318
JangHyojoon <fdasfaj9@gmail.com>
2022-11-01 20:09:56 +0900

    문제 7번 기능 1 각 유저의 친구 리스트를 도출하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,24 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    public static Map<String, List<String>> getFriendsList(List<List<String>> friends) {
+        Map<String, List<String>> friendsList = new HashMap<>();
+        for (int i = 0; i < friends.size(); i++) {
+            if (!friendsList.containsKey(friends.get(i).get(0))) {
+                friendsList.put(friends.get(i).get(0), new ArrayList<>());
+            }
+            friendsList.get(friends.get(i).get(0)).add(friends.get(i).get(1));
+            if (!friendsList.containsKey(friends.get(i).get(1))) {
+                friendsList.put(friends.get(i).get(1), new ArrayList<>());
+            }
+            friendsList.get(friends.get(i).get(1)).add(friends.get(i).get(0));
+        }
+        return friendsList;
     }
 }
