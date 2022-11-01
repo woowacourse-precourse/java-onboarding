092f27f53950c94e54729b154cef09b4b644c105
박세원 <tpdnjs1825@naver.com>
2022-11-01 22:52:16 +0900

    Feat : 중복되는 문자를 제거한 복호문을 구하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,27 +1,26 @@
 package onboarding;
 
 public class Problem2 {
-    private static String removeDuplicate(String cryptogram){
+    private static String getDecryption(String cryptogram) {
         String decryption = "";
-        for(int i = 0; i < cryptogram.length() - 1; i++){
-            char firstCharacter = cryptogram.charAt(i);
-            char secondCharacter = cryptogram.charAt(i + 1);
-            if(firstCharacter == secondCharacter) {
-                i++;
-                continue;
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (i < cryptogram.length() - 1) {
+                char firstCharacter = cryptogram.charAt(i);
+                char secondCharacter = cryptogram.charAt(i + 1);
+                if (firstCharacter == secondCharacter) {
+                    i++;
+                    continue;
+                }
+                decryption += String.valueOf(firstCharacter);
             }
-            decryption += String.valueOf(firstCharacter);
-            if(i == cryptogram.length() - 2) {
-                decryption += String.valueOf(secondCharacter);
+            if (i == cryptogram.length() - 1) {
+                decryption += String.valueOf(cryptogram.charAt(cryptogram.length() - 1));
             }
         }
         return decryption;
     }
     public static String solution(String cryptogram) {
         String answer = "answer";
-
-        removeDuplicate(cryptogram);
-
         return answer;
     }
 }
