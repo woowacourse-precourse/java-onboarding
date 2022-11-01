76ecf0ca46766d4fd4e71c7d41807a57c091136c
kkh <kkho9654@naver.com>
2022-11-01 20:48:42 +0900

    Add feature list problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,12 @@
 package onboarding;
 
+
+/**
+ * 1. 정규식 초기화
+ * 2. 정규식으로 pattern 객체 생성
+ * 3. cryptogram 으로 matcher 변수 생성
+ * 4. 정규식과 matches 되는 동안 연속된 문자열 삭제
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
