99eac86582fd191aca09ad0f97654d675849e19e
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-01 18:41:38 +0900

    feat: 박수 횟수 계산 기능 함수화

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,12 +1,14 @@
 package onboarding;
 
 public class Problem3 {
+    public static int getClappingCount(int i) { //369 갯수만큼 박수 Count
+        return Integer.toString(i).length() - Integer.toString(i).replaceAll("[369]","").length();
+    }
+
     public static int solution(int number) {
         int answer = 0;
         for (int i=3; i<=number; i++) { //3 이전은, 박수를 치지 않으므로 3부터 시작
-            for (String p : new String[]{"3","6","9"}) {    //3,6,9가 포함된 만큼 박수
-                answer+=Integer.toString(i).length() - Integer.toString(i).replace(p,"").length();
-            }
+            answer+=getClappingCount(i);
         }
         return answer;
     }
