28a374354263efb0f4604c088bae8ef36e794e70
cpot5620 <=cpot5620@naver.com>
2022-11-01 17:37:42 +0900

    Add handling exception in solution method

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,9 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Members members = new Members();
 
+        if (forms.size() < 1 || forms.size() > 10000)
+            throw new IllegalArgumentException("크루는 1명 이상 10,000명 이하여야 합니다.");
+
         for (int i = 0; i < forms.size(); i++) {
             String email = forms.get(i).get(0);
             String nickName = forms.get(i).get(1);
