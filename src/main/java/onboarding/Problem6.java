85556446e2886a57a7c8017b3d446385a86004ef
yenawee <yaena0319@naver.com>
2022-11-01 18:16:40 +0900

    [FEAT] 6. 문자열 중복 이메일 확인 후 ret 리스트에 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> ret = new ArrayList<>();
+        for (int i = 0; i < forms.size() - 1; i++){
+            String str = forms.get(i).get(1);
+            for (int j = 0; j < str.length() - 1; j++){
+                String sub_str = str.substring(i, i+2);
+                for (int k = i + 1; k < forms.size(); k++){
+                    if (forms.get(j).get(1).contains(sub_str))
+                        ret.add(str);
+                        ret.add(forms.get(j).get(0));
+                }
+            }
+        }
+        return ret;
     }
+
 }
