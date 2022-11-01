179100a7b3432fdb3453f8ddbb79ca3d492a64c8
masiljangajji <hjk172262@gmail.com>
2022-11-01 20:28:13 +0900

    feat: Problem4_최종 solution

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,19 +6,23 @@ public class Problem4 {
 
 
         char ch;
+        char p='Z';
+        char q='z';
 
         for(int i=0;i<word.length();i++)
         {
             if(word.charAt(i)>='a'&&word.charAt(i)<='z')
             {
-
+                ch=(char) ('a'+('z'-word.charAt(i)));
+                answer+=ch;
             }
             else if(word.charAt(i)>='A'&&word.charAt(i)<='Z')
             {
-
+                ch=(char) ('A'+('Z'-word.charAt(i)));
+                answer+=ch;
             }
             else
-
+                answer+=word.charAt(i);
 
         }
 
