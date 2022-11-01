892579293d36a8d0c0fd564042b301fb7b270029
kkh <kkho9654@naver.com>
2022-11-01 20:49:44 +0900

    Add feature list problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 1. for loop 두개로 모든 서로 다른 두개의 이름을 비교한다.
+ * 2. 하나의 이름의 두개의 연속되 문자를 앞에서부터 순서대로 검새해서 그 문자열이 나머지 이름의 문자열에 포함되어 있는지 검사
+ *    있다면, answer에 해당하는 두 이름의 이메일들을 저장
+ * 3. treeset으로 이메일 정렬 및 중복 제거
+ * 4. list에 treeset 복사 및 list반환
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
