5d272a2d486fda6e785a163f60aa4d2d89287067
sxxgxxnz <tmddms2292@gmail.com>
2022-11-01 13:21:13 +0900

    fix Problem1 refactoring

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,37 +5,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        int pMax=0;
-        int cMax=0;
-        List<Integer> pList=new ArrayList<Integer>();
-        List<Integer> cList=new ArrayList<Integer>();
-
+    public static int pobiCalc(List<Integer> pobi){
+        List<Integer> pList = new ArrayList<>();
+        int pMax = 0;
         for(int i=0;i<pobi.size();i++){
-
-            if(pobi.get(i) == 1 || pobi.get(i)==400){
-                answer = -1;
-                break;
-            }
-
-            int tmp = pobi.get(i);
+            int num = pobi.get(i);
             int sum=0,multi=1;
-            while(tmp>0){
-                sum += tmp%10;
-                multi *=tmp%10;
-                tmp/=10;
+            while(num>0){
+                sum += num%10;
+                multi *=num%10;
+                num/=10;
             }
             pList.add(sum);
             pList.add(multi);
 
         }
-        for(int i=0;i<crong.size();i++){
+        pList.sort(Comparator.reverseOrder());
+        pMax = pList.get(0);
+        return pMax;
+    }
 
-            if(crong.get(i) == 1 || crong.get(i)==400){
-                answer = -1;
-                break;
-            }
+    public static int crongCalc(List<Integer> crong){
+        List<Integer> cList = new ArrayList<>();
+        int cMax = 0;
+        for(int i=0;i<crong.size();i++){
             int tmp = crong.get(i);
             int sum = 0, multi = 1;
             while(tmp > 0){
@@ -46,23 +39,46 @@ class Problem1 {
             cList.add(sum);
             cList.add(multi);
         }
-        pList.sort(Comparator.reverseOrder());
         cList.sort(Comparator.reverseOrder());
-        pMax = pList.get(0);
         cMax = cList.get(0);
+        return cMax;
+    }
+    public static int checkArg(List<Integer> pobi,List<Integer> crong){
+        int check = 0;
 
-        if(pMax == cMax){
-            answer = 0;
-        }else if(pMax > cMax){
-            answer = 1;
-        }else if(pMax < cMax){
-            answer = 2;
-        }
         if(pobi.get(1)-pobi.get(0)!=1){
-            answer = -1;
+            check = -1;
+        }
+        for(int i=0;i<pobi.size();i++) {
+
+            if (pobi.get(i) == 1 || pobi.get(i) == 400) {
+                check = -1;
+                break;
+            }
         }
         if(crong.get(1)-crong.get(0)!=1){
-            answer = -1;
+            check = -1;
+        }
+        for(int i=0;i<crong.size();i++) {
+            if (crong.get(i) == 1 || crong.get(i) == 400) {
+                check = -1;
+                break;
+            }
+        }
+        return check;
+    }
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer = checkArg(pobi,crong);
+        if(answer != -1){
+            int pMax=pobiCalc(pobi);;
+            int cMax=crongCalc(crong);
+            if(pMax == cMax){
+                answer = 0;
+            }else if(pMax > cMax){
+                answer = 1;
+            }else if(pMax < cMax){
+                answer = 2;
+            }
         }
         return answer;
     }
