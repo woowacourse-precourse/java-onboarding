cbbc24246a46ce2e3fa29055f397fedd32e31156
minju412 <ln8847@ajou.ac.kr>
2022-11-01 20:09:11 +0900

    comment: [p3] 불필요한 주석 제거

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,9 +14,7 @@ public class Problem3 {
 		}
 
 		for (int i = 1; i <= number; i++) {
-			letter = String.valueOf(i); // int -> string 변환
-
-			// 예를 들어, 36 이라는 숫자에는 박수 두 번
+			letter = String.valueOf(i);
 			for (int j = 0; j < letter.length(); j++) {
 				oneLetter = letter.charAt(j);
 				if (oneLetter == '3' || oneLetter == '6' || oneLetter == '9') {
@@ -28,11 +26,6 @@ public class Problem3 {
 		return cnt;
 	}
 
-	/**
-	 * 제한사항을 위배했는지 체크하는 메서드
-	 * @param number
-	 * @return
-	 */
 	private static Integer checkRestrictions(int number) {
 		if (!isNumberRangeValid(number)) {
 			return EXCEPTION;
@@ -40,7 +33,6 @@ public class Problem3 {
 		return 0;
 	}
 
-	// number 의 범위가 1 이상 10,000 이하가 아닌 경우 예외
 	private static boolean isNumberRangeValid(int number) {
 		if (number < 1 || number > 10000) {
 			return false;
