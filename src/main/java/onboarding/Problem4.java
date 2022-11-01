2d25b2a4b6fe7e720d91bf56b8c13bf9ea517477
tunkcalb <wjdgusdndi@gmail.com>
2022-11-01 16:48:49 +0900

    problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem4 {
+//    아스키 a~z : 97~122 ; 97 + 122 = 219
+//    아스키 A~Z : 65~90  ; 65 + 90 = 155
+    public static char change(char before){
+        if (before == ' '){
+            return ' ';
+        }
+        int asc = before;
+        char after = ' ';
+        if (asc >= 97) {
+            after = (char)(219 - asc);
+        }
+        else{
+            after = (char)(155 - asc);
+            }
+        return after;
+    }
     public static String solution(String word) {
         String answer = "";
-        return answer;
+        for (int i = 0; i < word.length(); i++) {
+            answer += change(word.charAt(i));
+        }
+
+            return answer;
     }
 }
