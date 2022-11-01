43caea01ecc985334b84d2f021877b9229ccbc93
BEMELON <hgdkim2@naver.com>
2022-11-01 21:39:03 +0900

    Solve Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        boolean check;
+        StringBuilder answer;
+
+        do {
+            check = false;
+            answer = new StringBuilder();
+            for(int i = 0; i < cryptogram.length();) {
+                char target = cryptogram.charAt(i);
+                int next;
+                for (next = i + 1; next < cryptogram.length() && cryptogram.charAt(next) == target; next++);
+
+                if (next - i == 1)
+                    answer.append(target);
+                else
+                    check = true;
+
+                i = next;
+            }
+            cryptogram = answer.toString();
+        } while (check);
+
+        return answer.toString();
     }
 }
