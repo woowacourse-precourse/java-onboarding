67aa872770c2f2619dacfe9f27ab121b49c6799d
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 13:48:23 +0900

    [1번] feat: 페이지 번호 예외 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        // 예외 처리
+        if (pobi.get(1) != pobi.get(0) + 1 || crong.get(1) != crong.get(0) + 1) {
+            return -1;
+        }
+
         int answer = Integer.MAX_VALUE;
         return answer;
     }
