90208c33526e2da06fd0e1d42c314d1577d14cbb
ChaCha3088 <cha3088@gmail.com>
2022-11-01 21:13:32 +0900

    style: problem1 final

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,18 +4,23 @@ import java.util.*;
 import java.util.stream.Stream;
 
 class Problem1 {
+    /**
+     * 페이지가 유효하지 않을 때 발생하는 예외
+     */
     static class PageValidationException extends RuntimeException {
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         try {
-            if ( checkPageValidation(pobi) == false || checkPageValidation(crong) == false ) {
+            // 페이지가 유효하지 않을 때 예외 발생
+            if (!checkPageValidation(pobi) || !checkPageValidation(crong)) {
                 throw new PageValidationException();
             }
 
             int pobiResult = getResult(pobi);
             int crongResult = getResult(crong);
 
+            // 점수 비교
             if (pobiResult > crongResult) {
                 return 1;
             } else if (crongResult > pobiResult) {
@@ -30,23 +35,42 @@ class Problem1 {
         }
     }
 
+    /**
+     * 숫자를 각 자리수별 숫자로 나눈다.
+     * @param number 입력된 숫자
+     * @return 배열
+     */
     private static int[] splitEachNumbers(int number) {
         int[] arrNum = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
         return arrNum;
     }
 
+    /**
+     * 배열 내 숫자의 합을 구한다.
+     * @param numbers 분리된 숫자의 배열
+     * @return 배열 내 숫자의 총합
+     */
     private static int sumNumbers(int[] numbers) {
-        int sum = Arrays.stream(numbers).sum();
-        return sum;
+        return Arrays.stream(numbers).sum();
     }
 
+    /**
+     * 배열 내 숫자의 곱을 구한다.
+     * @param numbers 분리된 숫자의 배열
+     * @return 배열 내 숫자의 곱
+     */
     private static int multiplyNumbers(int[] numbers) {
-        int result = Arrays.stream(numbers).reduce(1, (a, b) -> a * b);
-        return result;
+        return Arrays.stream(numbers).reduce(1, (a, b) -> a * b);
     }
 
+    /**
+     * 문제의 결과를 계산한다.
+     * @param inputList 입력된 페이지 숫자 리스트
+     * @return 점수
+     */
     private static int getResult(List<Integer> inputList) {
         ArrayList<Integer> resultList = new ArrayList<>();
+
         for (Integer e : inputList) {
             int[] splittedArray = splitEachNumbers(e);
             int sumResult = sumNumbers(splittedArray);
@@ -54,17 +78,21 @@ class Problem1 {
             int result = Math.max(sumResult, multiplyResult);
             resultList.add(result);
         }
-        int finalResult = Collections.max(resultList);
-        return finalResult;
+        return Collections.max(resultList);
     }
 
+    /**
+     * 페이지 숫자를 검증한다.
+     * @param inputList 입력된 페이지 숫자 리스트
+     * @return 참 거짓
+     */
     private static Boolean checkPageValidation(List<Integer> inputList) {
         int firstPage = inputList.get(0);
         int secondPage = inputList.get(1);
+
         if (secondPage == firstPage + 1) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
