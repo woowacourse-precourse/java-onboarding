4c660658afcebe76324d5a86b39b313cb7242981
glass <glass7312@gmail.com>
2022-11-01 17:33:07 +0900

    Problem1 기능 구현 compare_max: 각 자릿수들의 합과 곱을 비교하여 큰 것을 반환하는 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,10 +15,29 @@ class Problem1 {
     }
     public static int compare_max(List<Integer> digit_ls) {
 
+        int num=0,max=0;
+        for(int i=0; i<2; i++){
+            num=get_max(get_sum(digit_ls.get(i)),get_prod(digit_ls.get(i)));
+            if(num>max){
+                max=num;
+            }
+        }
 
+        return max;
     }
     public static List<Integer> get_digitList(int num){
+        List<Integer> digitList=new ArrayList<>();
+        int hund_d=0;
+        int ten_d=0;
+        int one_d=0;
+        hund_d=num/100;
+        ten_d=(num%100)/10;
+        one_d=num%10;
 
+
+        digitList=Arrays.asList(hund_d,ten_d,one_d);
+
+        return digitList;
 
 
 
