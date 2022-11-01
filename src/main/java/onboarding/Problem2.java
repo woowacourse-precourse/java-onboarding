e2fcc4a3e379ef6e72bb1bdaad46286a2c1d212f
Juuuic <sn990908a@gmail.com>
2022-11-01 15:23:01 +0900

    2번문제 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,31 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static String decoding(String s) {
+        char c = 0;
+        boolean isRemoved = false;
+
+        for(int i=0; i<s.length(); i++) {
+            if(i != 0 && c == s.charAt(i)) {
+                s = s.substring(0, i-1) + s.substring(i+1);
+                isRemoved = true;
+                break;
+            }
+            c = s.charAt(i);
+        }
+
+        if(isRemoved) {
+            s = decoding(s);
+        }
+
+        return s;
+    }
     public static String solution(String cryptogram) {
         String answer = "";
 
-
-
-
-        return answer.toString();
+        answer = decoding(cryptogram);
+        return answer;
 
     }
 }
