f94f0982d4f2c201dbf73a99074e33c979286666
ddubidubap <park62000@gmail.com>
2022-11-01 09:39:43 +0900

    fix: Problem 2 modify logic error

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,9 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Problem2 {
     /*
         기능 요구 사항 목록
@@ -8,27 +12,45 @@ public class Problem2 {
         3. solution 함수
     */
 
-    // 반복되는 문자의 인덱스를 찾는 함수
-    private static Integer findRepetition(String cryptogram) {
-        for (int i = 1; i < cryptogram.length(); i++) {
-            char firstLetter = cryptogram.charAt(i-1);
-            char secondLetter = cryptogram.charAt(i);
-            if (firstLetter == secondLetter) return i-1;
+    // 리스트에 원소 추가
+    private static HashSet<Integer> addToBasket(
+            String cryptogram,
+            HashSet<Integer> basket,
+            Integer idx
+    ) {
+        char firstLetter = cryptogram.charAt(idx - 1);
+        char secondLetter = cryptogram.charAt(idx);
+        if (firstLetter == secondLetter) {
+            basket.add(idx - 1);
+            basket.add(idx);
         }
-        return null;
+        return basket;
+    }
+
+    // 반복되는 문자의 인덱스를 찾는 함수
+    private static HashSet<Integer> findRepetition(String cryptogram) {
+        HashSet<Integer> basket = new HashSet<>();
+        for (int i = 1; i < cryptogram.length(); i++)
+            addToBasket(cryptogram, basket, i);
+        return basket;
     }
 
     // 반복되는 문자를 제거하는 함수
-    private static String removeRepetition(String cryptogram, int index) {
-        String first = cryptogram.substring(0, index);
-        String second = cryptogram.substring(index+2);
-        return first + second;
+    private static String removeRepetition(String cryptogram, HashSet<Integer> basket) {
+        String result = "";
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char letter = cryptogram.charAt(i);
+            if (!basket.contains(i)) result += letter;
+        }
+        return result;
     }
 
     public static String solution(String cryptogram) {
-        Integer index;
-        while ((index = findRepetition(cryptogram)) != null)
-            cryptogram = removeRepetition(cryptogram, index);
+        HashSet<Integer> basket = findRepetition(cryptogram);
+        while (basket.size() > 0) {
+            cryptogram = removeRepetition(cryptogram, basket);
+            basket = findRepetition(cryptogram);
+        }
 
         return cryptogram;
     }
