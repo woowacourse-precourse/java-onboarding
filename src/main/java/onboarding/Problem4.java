9d16a52fafc2e927239b6e3266b793b27c62476d
Yongwoo Choi <cyw7515@naver.com>
2022-11-01 11:44:21 +0900

    fix(problem4): 알파벳이 아닌 한글도 문자로 인식해 발생하는 오류 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,12 +1,23 @@
 package onboarding;
 
+import java.nio.charset.StandardCharsets;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = decrypt(word);
+        if(!validate(word)) return "Invalid Input";
+
+        String answer = new String(decrypt(word).getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+
         return answer;
     }
 
+    private static boolean validate(String word) {
+        if (word.length() < 1 || word.length() > 1000) return false;
+        return true;
+    }
+
     private static String decrypt(String word) {
+        //한글입력꼬임
         StringBuilder outputBuilder = new StringBuilder();
 
         for (int i = 0; i < word.length(); i++){
@@ -18,12 +29,19 @@ public class Problem4 {
     }
 
     private static char getMatchingCharacter(char input) {
-        if (!Character.isLetter(input)) return input;
+        if (!isAlphabet(input)) return input;
 
         boolean isUpperCase = 'a' > input;
         char standard = isUpperCase ? 'A' : 'a';
 
         int index = input - standard;
         return (char) (standard + 25 - index);
+    }
+
+    private static boolean isAlphabet(char input) {
+        if (65 <= input && input <= 90) return true;
+        else if (97 <= input && input <= 122) return true;
+
+        return false;
     }
 }
