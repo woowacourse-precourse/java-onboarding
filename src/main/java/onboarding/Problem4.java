08b57f199c2742a76f8187e02a5b19c74aa307d9
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 11:11:03 +0900

    4번 문제 풀이

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,7 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
+    public static String solution(String word) { // 사전 반전순으로 일치하도록
         String answer = "";
         return answer;
     }
