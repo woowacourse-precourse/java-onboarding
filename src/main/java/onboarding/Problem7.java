c589dea6ed31a5fc238e87c2339ff6c24c1cb976
Moveuk <v_donguk@naver.com>
2022-11-01 22:00:40 +0900

    feat: Problem7 user의 친구 목록에서 사용자와 함께 아는 친구(user 제외)에게 10점을 주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,12 +3,19 @@ package onboarding;
 import java.util.*;
 
 public class Problem7 {
+    public static final int DEFAULT_SCORE = 0;
+    public static final int FRIEND_TOGETHER_SCORE = 10;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, Integer> scoreBoard = new HashMap<>();
         Set<String> friendList = new HashSet<>();
 
         // 친구목록 찾아오기(초기화)
         initializeFriendList(user, friendList, friends);
+
+        // 친구의 친구 목록 확인하고 점수 주기
+        giveFriendOfFriendScore(user, friendList, friends, scoreBoard);
+
 
         return answer;
     }
@@ -25,4 +32,15 @@ public class Problem7 {
         }
     }
 
+    public static void giveFriendOfFriendScore(String user, Set<String> friendList, List<List<String>> friends, Map<String, Integer> scoreBoard) {
+        friendList.forEach(friendID -> friends.forEach(friendBetweenList -> {
+            if (friendBetweenList.contains(friendID) && !friendBetweenList.contains(user)) {
+                if (friendBetweenList.get(0).equals(friendID)) {
+                    scoreBoard.put(friendBetweenList.get(1), scoreBoard.getOrDefault(friendBetweenList.get(1), DEFAULT_SCORE) + FRIEND_TOGETHER_SCORE);
+                } else {
+                    scoreBoard.put(friendBetweenList.get(0), scoreBoard.getOrDefault(friendBetweenList.get(0), DEFAULT_SCORE) + FRIEND_TOGETHER_SCORE);
+                }
+            }
+        }));
+    }
 }
