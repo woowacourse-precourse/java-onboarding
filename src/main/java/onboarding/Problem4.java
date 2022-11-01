ac07f0ef445ecda0b5a79159019f3c9164c69204
ingpyo <juninpyo72@gmail.com>
2022-11-01 11:45:43 +0900

    [problem4]전체 코드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,19 @@ public class Problem4 {
     public static String solution(String word) {
         checkList(word);
 
+        String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            if ((int)word.charAt(i)>=65 &&(int)word.charAt(i)<=90){
+                answer += (char)(155 - (int)word.charAt(i));
+            } else if ((int)word.charAt(i) >= 97 && (int)word.charAt(i) <= 122) {
+                answer += (char)(219 - (int)word.charAt(i));
+            } else {
+                answer += word.charAt(i);
+            }
+        }
+
+        return answer;
     }
 
     public static void checkList(String word) throws IllegalArgumentException{
