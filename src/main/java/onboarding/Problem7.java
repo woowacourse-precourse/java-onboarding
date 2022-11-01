a4e0054683e21c02d7801d2c473839f6efc45c6d
박세원 <tpdnjs1825@naver.com>
2022-11-01 23:42:32 +0900

    Feat : 예외 처리 사항 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,10 +1,19 @@
+// 7번 문제
+
 package onboarding;
 
 import java.util.*;
 import java.util.Map.Entry;
 
 public class Problem7 {
-    private static final int ANSWER_LENGTH = 3;
+    private static final int ANSWER_LENGTH = 5;
+
+    private static void getMyFriendsList(HashMap<String, List<String>> friendsList, List<String> userFriends, String user) {
+        userFriends.add(user);
+        for (int i = 0; i < friendsList.get(user).size(); i++) {
+            userFriends.add(friendsList.get(user).get(i));
+        }
+    }
 
     private static void getFriendsList(HashMap<String, List<String>> friendsList, List<List<String>> friends) {
         for (int i = 0; i < friends.size(); i++) {
@@ -27,11 +36,11 @@ public class Problem7 {
     private static void shareFriends(HashMap<String, List<String>> friendsList, HashMap<String, Integer> recommendScore, String user) {
         for (int i = 0; i < friendsList.get(user).size(); i++) {
             String frinedName = friendsList.get(user).get(i);
-            getShareFriendScore(friendsList, recommendScore, frinedName, user);
+            addFriendScore(friendsList, recommendScore, frinedName, user);
         }
     }
 
-    private static void getShareFriendScore(HashMap<String, List<String>> friendsList, HashMap<String, Integer> recommendScore, String member, String user) {
+    private static void addFriendScore(HashMap<String, List<String>> friendsList, HashMap<String, Integer> recommendScore, String member, String user) {
         for (int i = 0; i < friendsList.get(member).size(); i++) {
             String friendName = friendsList.get(member).get(i);
             if (friendName.equals(user)) {
@@ -44,17 +53,17 @@ public class Problem7 {
         }
     }
 
-    private static void getVisitFriendScore(List<String> visitors, HashMap<String, Integer> recommendScore){
-        for(int i = 0; i < visitors.size(); i++){
+    private static void getVisitFriendScore(List<String> visitors, HashMap<String, Integer> recommendScore) {
+        for (int i = 0; i < visitors.size(); i++) {
             String visitorName = visitors.get(i);
-            if(!recommendScore.containsKey(visitorName)) {
+            if (!recommendScore.containsKey(visitorName)) {
                 recommendScore.put(visitorName, 0);
             }
             recommendScore.put(visitorName, recommendScore.get(visitorName) + 1);
         }
     }
 
-    private static List<String> sortByScoreAndName(HashMap<String, Integer> recommendScore){
+    private static List<String> sortByScoreAndName(HashMap<String, Integer> recommendScore, List<String> userFriends) {
         List<String> recommendFriendsList = new ArrayList<>();
         List<Entry<String, Integer>> sortRecommendList = new ArrayList<Entry<String, Integer>>(recommendScore.entrySet());
         Collections.sort(sortRecommendList, new Comparator<Entry<String, Integer>>() {
@@ -62,11 +71,21 @@ public class Problem7 {
                 return obj2.getValue().compareTo(obj1.getValue());
             }
         });
-        for(int i = 0; i < ANSWER_LENGTH; i++){
-            recommendFriendsList.add(sortRecommendList.get(i).getKey());
+        for (int i = 0; i < recommendScore.size(); i++) {
+            if (recommendFriendsList.size() == ANSWER_LENGTH) {
+                break;
+            }
+            if (!isNotRecommend(userFriends, sortRecommendList.get(i).getKey())) {
+                recommendFriendsList.add(sortRecommendList.get(i).getKey());
+            }
         }
         return recommendFriendsList;
     }
+
+    private static boolean isNotRecommend(List<String> userFriends, String name) {
+        return userFriends.contains(name);
+    }
+
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
@@ -80,9 +99,10 @@ public class Problem7 {
 
         getVisitFriendScore(visitors, recommendScore);
 
-        answer = sortByScoreAndName(recommendScore);
+        getMyFriendsList(friendsList, userFriends, user);
+
+        answer = sortByScoreAndName(recommendScore, userFriends);
 
         return answer;
     }
 }
-
