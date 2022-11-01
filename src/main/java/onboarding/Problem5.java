7d6d48928e310972b1299cd49b5b99be74411158
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 13:48:20 +0900

    feat: Problem5 화폐의 가치가 큰값부터 필요한 화폐의 최대 개수를 저장하는 기능 구현
    Problem5 화폐의 가치가 큰값부터 필요한 화폐의 최대 개수를 리스트에 저장하는 기능을 구현했습니다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        Integer[] pocket = {50000,10000,5000,1000,500,100,50,10,1};
+        for (int item : pocket){
+            if (money/item >0) {
+                answer.add(money/item);
+            }
+            else{
+                answer.add(0);
+            }
+            
+        }
         return answer;
     }
+
 }
