2d72d61083b67ba47e6f494f63fb9cda2da250e1
hgo641 <hgo641@gmail.com>
2022-11-01 19:35:07 +0900

    feat: Problem1 페이지 MaxNumber 반환 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,8 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Math.max;
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
@@ -30,6 +32,21 @@ class Problem1 {
             pageNumber/=10;
         }
         return ret;
+    }
+
+    /**
+     * 왼쪽 페이지와 오른쪽 페이지의 각 자리 숫자들의 합과 곱들을 얻고, 그 중 가장 큰 값을 반환
+     * @param pages 왼쪽 페이지와 오른쪽 페이지의 숫자가 담긴 리스트
+     */
+    public static int getMaxNumber(List<Integer> pages){
+        int MaxNumber = 0;
+        for(Integer pageNumber : pages){
+            int sum = getPageNumberSum(pageNumber);
+            int mul = getPageNumberMul(pageNumber);
+            MaxNumber = max(MaxNumber, sum);
+            MaxNumber = max(MaxNumber, mul);
+        }
+        return MaxNumber;
     }
 
 }
