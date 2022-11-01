120764f01207e34e18df264e5184641a8def9ff1
core <jinjs6@gmail.com>
2022-11-01 21:23:57 +0900

    Problem7 userFriends리스트 선언 및 userFriends에 user의 친구 이름 담기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> userFriends = new ArrayList<>();
+        for (int i = 0; i < friends.size(); ++i) {
+            if (friends.get(i).get(0).equals(user)) {
+                userFriends.add(friends.get(i).get(1));
+            } else if (friends.get(i).get(1).equals(user)) {
+                userFriends.add(friends.get(i).get(0));
+            }
+        }
         return answer;
     }
 }
