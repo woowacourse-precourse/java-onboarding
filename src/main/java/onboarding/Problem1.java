7fe918be6cbf96f670aa86f3e970a106128de00b
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-01 16:36:03 +0900

    Problem1 예외처리 조건 변경 및 추가  - 연속되지 않는 경우 조건 수정  - 왼쪽페이지가 홀수가 아닌 경우 추가 (연속된페이지 조건이 있기 때문에 오른쪽 짝수조건은 안넣음)  - 번호 범위 조건 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -37,17 +37,19 @@ class Problem1 {
 
     private static void processException(List<Integer> pobi, List<Integer> crong) throws Exception {
         // 책 페이지가 연속되지 않은 경우
-        if (pobi.get(0) + 1 != pobi.get(1) && pobi.get(1) + 1 != pobi.get(0)) {
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
             throw new Exception();
         }
-        if (crong.get(0) + 1 != crong.get(1) && pobi.get(1) + 1 != pobi.get(0)) {
+        // 왼쪽페이지가 홀수가 아닌 경우
+        if (pobi.get(0) % 2 != 1 || crong.get(0) % 2 != 1) {
             throw new Exception();
         }
-        // 시작 면이나 마지막 면이 나오는 경우
-        if (pobi.get(0) == 0 || pobi.get(1) == 0) {
+        // 번호가 범위안에 안들어갈 경우
+        if (pobi.get(0) < 1 || pobi.get(1) > 400) {
             throw new Exception();
         }
-        if (crong.get(0) == 0 || crong.get(1) == 0) {
+        // 번호가 범위안에 안들어갈 경우
+        if (crong.get(0) < 1 || crong.get(1) > 400) {
             throw new Exception();
         }
     }
