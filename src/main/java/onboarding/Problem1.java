dda73b7621b6dbb060238bcdabece87b948809c5
2do1 <dowon3036@naver.com>
2022-11-01 16:48:40 +0900

    feat: 문제 1번 예외사항 확인 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,24 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
+        // 예외사항 1: 왼쪽 오른쪽 페이지가 차이가 1이 아닐 경우
+        // 예외사항 2: 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수가 아닐 경우
+        // 예외사항 3: 첫번째 페이지나, 마지막 페이지가 나올 경우
+
+        if (pobiLeftPage % 2 != 1 || pobiRightPage - pobiLeftPage != 1 || crongLeftPage % 2 != 1 || crongRightPage - crongLeftPage != 1) { // 예외사항 1, 2
+            answer = -1;
+        } else if(pobiLeftPage == 1 || pobiRightPage == 400 || crongLeftPage == 1 || crongRightPage == 400) { // 예외사항 2
+            answer = -1;
+        } else { // 예외사항이 없을 경우
+
+        }
         return answer;
     }
 }
