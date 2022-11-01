933a9f81034ce40ee0438d9173ba2f4f5c84baec
ghchoi <ghchoi0427@gmail.com>
2022-11-01 15:47:21 +0900

    feat: 이름에서 겹치는 부분이 있는지 판별하는 메서드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,9 +6,45 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
         Problem6 p6 = new Problem6();
-        return answer;
+        return p6.getEmailOfOverlap(forms);
+    }
+
+    private List<String> getEmailOfOverlap(List<List<String>> forms) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < forms.size() - 1; i++) {
+            for (int j = i + 1; j < forms.size(); j++) {
+                String name1 = forms.get(i).get(1);
+                String name2 = forms.get(j).get(1);
+
+                List<String> stringTokens1 = getStringTokens(name1);
+                List<String> stringTokens2 = getStringTokens(name2);
+
+                if (isOverlapped(stringTokens1, stringTokens2)) {
+                    if (!isAlreadyInList(result, forms.get(i).get(0))) {
+                        result.add(forms.get(i).get(0));
+                    }
+
+                    if (!isAlreadyInList(result, forms.get(j).get(0))) {
+                        result.add(forms.get(j).get(0));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean isAlreadyInList(List<String> list, String target) {
+        return list.contains(target);
+    }
+
+    private boolean isOverlapped(List<String> l1, List<String> l2) {
+        for (String element : l1) {
+            if (l2.contains(element)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private HashMap<String, String> getHashMappedForm(List<List<String>> forms) {
@@ -19,7 +55,7 @@ public class Problem6 {
         return map;
     }
 
-    private List<String> stringTokens(String word) {
+    private List<String> getStringTokens(String word) {
         List<String> result = new ArrayList<>();
         for (int i = 0; i < word.length() - 1; i++) {
             String temp = Character.toString(word.charAt(i)) + Character.toString(word.charAt(i + 1));
