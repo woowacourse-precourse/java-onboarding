2042a17a1a15d3cfd619f97a837399cc9ec791d0
hunko <rhwlgns4386@naver.com>
2022-11-01 11:13:32 +0900

    약간의 리펙토링

--- src/main/java/onboarding/problem7/Problem7Service.java
+++ src/main/java/onboarding/problem7/Problem7Service.java
@@ -4,7 +4,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem7Service {
-    static Problem7SnsRepository repository=RepositoryFactory.makeProblem7SnsRepository();
+    private static Problem7SnsRepository repository=RepositoryFactory.makeProblem7SnsRepository();
+
 
     public void save(String userName) {
         User user = new User(userName);
@@ -17,59 +18,86 @@ public class Problem7Service {
     }
 
     public void addFriend(String userName, String friendName) {
-        Optional<User> userOptional = repository.findByUserName(userName);
-        Optional<User> friendOptional = repository.findByUserName(friendName);
-        userOptional.orElseThrow(()->new IllegalArgumentException());
-        friendOptional.orElseThrow(()->new IllegalArgumentException());
-        User findUser = (User)userOptional.get();
-        User findFriend = (User)friendOptional.get();
+        User findUser = getUser(userName);
+        User findFriend = getUser(friendName);
         findUser.addFriend(findFriend);
     }
 
     public List<User> suggestUser(String user, List<String> visitors){
-        Optional<User> userOptional = repository.findByUserName(user);
-        userOptional.orElseThrow();
-        return suggestUser(userOptional.get(),visitors);
+        User findUser = getUser(user);
+        return suggestUser(findUser,visitors);
     }
 
-    public List<User> suggestUser(User user, List<String> visitors) {
-        int[] visit=new int[repository.lastIdNumber()];
-        Arrays.fill(visit,4);
-        Map<User,Integer> cnt=new HashMap<>();
-        for(int i=0;i<=2;i++){
-            dls(cnt,visit,user,i,0);
-        }
-        for(String visitor:visitors){
-            Optional<User> visitorOptional = repository.findByUserName(visitor);
-            visitorOptional.orElseThrow(()->new RuntimeException());
-            User user1 = visitorOptional.get();
-            if(visit[user1.getId()]>1){
-                if(!cnt.containsKey(user1)){
-                    cnt.put(user1,0);
-                }
-                cnt.put(user1,cnt.get(user1)+1);
-            }
-        }
-        List<User> collect = cnt.entrySet().stream().sorted((e1, e2) -> {
-            if(e2.getValue()<e1.getValue()){
-                return -1;
-            }else if(e2.getValue()==e1.getValue()){
-                return e1.getKey().getUserName().compareTo(e2.getKey().getUserName());
-            }else{
-                return 1;
-            }
+    private static Map<User,Integer> scoreMap;
+    private static int[] depth;
+    private static final int MAX_DEPTH=2;
+    private static final int FRIEND_DEPTH=1;
 
-        }).map(e -> e.getKey()).collect(Collectors.toList());
+    public List<User> suggestUser(User user, List<String> visitors) {
+        depth =new int[repository.lastIdNumber()];
+        Arrays.fill(depth,4);
+        scoreMap =new HashMap<>();
+
+        findSuggestUserAndCalcScore(user);
+
+        visitorCalc(visitors);
+
+        List<User> collect = scoreMap.entrySet().stream().sorted(scoreSort()).map(e -> e.getKey()).collect(Collectors.toList());
         return collect.subList(0,collect.size()<5?collect.size():5);
     }
 
-    private void dls(Map<User,Integer> cnt,int[] visit, User user, int deep,int deepCount) {
-        visit[user.getId()]=deepCount;
-        if(2==deepCount){
-            if(!cnt.containsKey(user)){
-                cnt.put(user,0);
+    private void visitorCalc(List<String> visitors) {
+        for(String visitor: visitors){
+            User user = getUser(visitor);
+            if(isNotFriend(depth[user.getId()])){
+                if(!scoreMap.containsKey(user)){
+                    scoreMap.put(user,0);
+                }
+                scoreMap.put(user, scoreMap.get(user)+1);
             }
-            cnt.put(user,cnt.get(user)+10);
+        }
+    }
+
+    private boolean isNotFriend(int userId) {
+        return userId >FRIEND_DEPTH;
+    }
+
+    private User getUser(String userName) {
+        Optional<User> visitorOptional = repository.findByUserName(userName);
+        visitorOptional.orElseThrow(()->new IllegalArgumentException());
+        User user = visitorOptional.get();
+        return user;
+    }
+
+    private void findSuggestUserAndCalcScore(User user) {
+        ids(user);
+    }
+
+    private void ids(User user) {
+        for(int i=0;i<=2;i++){
+            dls(user,i,0);
+        }
+    }
+
+    private Comparator<Map.Entry<User, Integer>> scoreSort() {
+        return (e1, e2) -> {
+            if (e2.getValue() < e1.getValue()) {
+                return -1;
+            } else if (e2.getValue() == e1.getValue()) {
+                return e1.getKey().getUserName().compareTo(e2.getKey().getUserName());
+            } else {
+                return 1;
+            }
+        };
+    }
+
+    private void dls(User user, int deep,int deepCount) {
+        depth[user.getId()]=deepCount;
+        if(MAX_DEPTH==deepCount){
+            if(!scoreMap.containsKey(user)){
+                scoreMap.put(user,0);
+            }
+            scoreMap.put(user, scoreMap.get(user)+10);
             return;
         }
         if(deep==deepCount) return;
@@ -77,8 +105,8 @@ public class Problem7Service {
         Iterator<User> iterator = user.getFriends();
         while (iterator.hasNext()){
             User next = iterator.next();
-            if(visit[next.getId()]>visit[user.getId()]){
-                dls(cnt,visit,next,deep,deepCount+1);
+            if(depth[next.getId()]> depth[user.getId()]){
+                dls(next,deep,deepCount+1);
             }
         }
     }
