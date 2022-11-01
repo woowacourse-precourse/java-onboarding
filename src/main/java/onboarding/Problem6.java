d9245c45061472c82d36137e4aae3c8f7721d2dd
dvlpr0 <rladjswkd@naver.com>
2022-11-01 21:37:39 +0900

    fix/Problem6: 한 줄에 온점이 두 개 이상 포함되지 않도록 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,6 +9,7 @@ public class Problem6 {
 		String nickname = form.get(1);
 		String word;
 		int last;
+		int newValue;
 
 		last = nickname.length();
 		if (last == 1)
@@ -16,7 +17,8 @@ public class Problem6 {
 		last--;
 		for (int i = 0; i < last; i++) {
 			word = nickname.substring(i, i + 2);
-			words.put(word, words.getOrDefault(word, 0) + 1);
+			newValue = words.getOrDefault(word, 0) + 1;
+			words.put(word, newValue);
 		}
 	}
 	private static void fillSetAll(List<List<String>> forms) {
@@ -28,11 +30,24 @@ public class Problem6 {
 	}
 	private static void checkIfContainKey(String email, String nickname) {
 		Set<String> keySet = words.keySet();
+		boolean isContained;
+		boolean isOver1;
 
-		keySet.forEach(x->addEmailIfContainKey(email, nickname.contains(x) && words.get(x) > 1));
+		for (String key : keySet) {
+			isContained = nickname.contains(key);
+			isOver1 = words.get(key) > 1;
+			addEmailIfContainKey(email, isContained && isOver1);
+		}
 	}
 	private static void findDuplicated(List<List<String>> forms) {
-		forms.forEach(x->checkIfContainKey(x.get(0), x.get(1)));
+		String email;
+		String nickName;
+
+		for (List<String> form : forms) {
+			email = form.get(0);
+			nickName = form.get(1);
+			checkIfContainKey(email, nickName);
+		}
 	}
 	public static List<String> solution(List<List<String>> forms) {
 		emails = new HashSet<>();
