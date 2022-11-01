f23fb23c4cc063af45223ecc7f6dd4c61e0d9ace
rt3310 <btac3310@gmail.com>
2022-11-01 15:02:33 +0900

    feat(Problem5): Cash Enum 생성

---
+++ src/main/java/onboarding/problem5/Cash.java
@@ -0,0 +1,19 @@
+package onboarding.problem5;
+
+public enum Cash {
+    FIFTY_THOUSAND(50000),
+    TEN_THOUSAND(10000),
+    FIVE_THOUSAND(5000),
+    THOUSAND(1000),
+    FIVE_HUNDRED(500),
+    HUNDRED(100),
+    FIFTY(50),
+    TEN(10),
+    ONE(1);
+
+    private int price;
+
+    Cash(int price) {
+        this.price = price;
+    }
+}
