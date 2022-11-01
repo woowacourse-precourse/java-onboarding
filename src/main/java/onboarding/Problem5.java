f29d30a226ea7ee59474f660fe7ad4d8344471f1
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-11-01 15:53:24 +0900

    화폐의 수를 확인하는 change 구현 및 solution 구현 / solve Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,17 +5,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
+    static int[] coin = { 50000, 10000, 5000, 1000, 500, 100 , 50, 10, 1};
+
+    public static List<Integer> change(int money){
         List<Integer> list = new ArrayList<>();
-        int[] coin = { 50000, 10000, 5000, 1000, 500, 100 , 50, 10, 1};
         for(int i=0; i<coin.length; i++){
             list.add(i, money/coin[i]);
             money %= coin[i];
         }
         return list;
     }
-}
 
-/*
-[ 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 ]
- */
+    public static List<Integer> solution(int money) {
+        return change(money);
+    }
+}
