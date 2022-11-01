dc2565a8a95b8536348282608e61ddd86499a145
minji <heunhanireum@gmail.com>
2022-11-01 11:02:46 +0900

    Problem6 : 닉네임을 두글자씩 끊어주기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
+
+    private static List<String> myTwoNames(String name) {
+        List<String> twoNames = new ArrayList<>();
+        for (int idx = 0 ; idx < name.length()-1 ; idx++) {
+            twoNames.add(name.substring(idx, idx+2));
+        }
+        return twoNames;
+    }
+
 }
