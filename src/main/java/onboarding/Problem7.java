148b68901c2b7dd4222ae6186a8ee4d1f9adaa15
ye1ne <yewon7036@naver.com>
2022-11-01 20:28:32 +0900

    feature : 친구관계를 저장하는 메소드 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,47 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    static HashMap<String, List<String>> friendMap;
+    static HashMap<String, Integer> scoreCountMap;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        init();
+        makeFriendsMap(friends);
         List<String> answer = Collections.emptyList();
+
         return answer;
+    }
+
+    private static void init(){
+        friendMap = new HashMap<>();
+        scoreCountMap = new HashMap<>();
+    }
+    private static void makeFriendsMap(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+
+            if (friendMap.containsKey(user1)) {
+                friendMap.get(user1).add(user2);
+            } else {
+                scoreCountMap.put(user1, 0);
+                List<String> list = new ArrayList<>();
+                list.add(user2);
+                friendMap.put(user1, list);
+            }
+
+            if (friendMap.containsKey(user2)) {
+                friendMap.get(user2).add(user1);
+            } else {
+                scoreCountMap.put(user2, 0);
+                List<String> list = new ArrayList<>();
+                list.add(user1);
+                friendMap.put(user2, list);
+            }
+        }
     }
 }
