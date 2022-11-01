354a505460ecfa614a1243b8e9b69e134f5727ac
KyungKyu Park <rudrb0505@gmail.com>
2022-11-01 14:25:36 +0900

    refactor(Problem5): 변수명과 메소드명을 좀더 알아보기 쉽게 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,17 +7,17 @@ public class Problem5 {
 	public static List<Integer> solution(int money) {
 		int withdrawalMoney = money;
 		int[] monetaryUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-		List<Integer> unitCount = new ArrayList<>(monetaryUnit.length);
+		List<Integer> eachUnitNumberToGive = new ArrayList<>(monetaryUnit.length);
 
-		for (int i : monetaryUnit) {
-			unitCount.add(minUnitNumber(withdrawalMoney, i));
-			withdrawalMoney = getNextMoney(withdrawalMoney, i);
+		for (int unit : monetaryUnit) {
+			eachUnitNumberToGive.add(getMinimumNumberOfUnit(withdrawalMoney, unit));
+			withdrawalMoney = getNextMoney(withdrawalMoney, unit);
 		}
 
-		return unitCount;
+		return eachUnitNumberToGive;
 	}
 
-	private static int minUnitNumber(int withdrawalMoney, int unit) {
+	private static int getMinimumNumberOfUnit(int withdrawalMoney, int unit) {
 		return withdrawalMoney / unit;
 	}
 
