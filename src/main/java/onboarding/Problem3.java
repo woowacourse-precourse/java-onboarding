b245ae4b8a5dae6bb54185a52589686aee7185e9
junyong <jymoon1121@gmail.com>
2022-11-01 14:48:12 +0900

    [3번] countNumber(손뼉 치는 횟수) 기능 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = countNumber(number);
         return answer;
+    }
+    public static int countNumber(int number) {
+        int count = 0; // clap's total count
+
+        for(int n = 1; n < number; n++) {
+            String stringNum = Integer.toString(n);
+            String[] numArr = stringNum.split("");
+
+            for(int i = 1; i < numArr.length; i++) {
+                if(numArr[i].contains("3") || numArr[i].contains("6") || numArr[i].contains("9")) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
     }
 }
