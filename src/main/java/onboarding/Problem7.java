8e85eabf8e50f9bc99d1b43063241efd198d9af6
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 23:22:57 +0900

    문제7 / feat : 사용자의 친구 목록 생성 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,18 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static List<String> userFriends = new ArrayList<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        makeUserFriends(user, friends);
         return answer;
+    }
+    private static void makeUserFriends(String user, List<List<String>> friends) {
+        for (List<String> relation : friends) {
+            if(Objects.equals(relation.get(0), user)) userFriends.add(relation.get(1));
+            else if(Objects.equals(relation.get(1), user)) userFriends.add(relation.get(0));
+        }
     }
 }
