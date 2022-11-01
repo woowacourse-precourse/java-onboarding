0b49a93f463df60bd9f9af6c5b195b729eab64cb
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-01 23:41:27 +0900

    feat: Add Problem5 solution

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Problem5 {
+
     //지폐 수 계산
     public static List<Integer> returnNumberOfBills(int money){
         List<Integer> koreanWonList = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1); //한화 단위 리스트
