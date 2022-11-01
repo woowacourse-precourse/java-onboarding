7af2b43ef7777fbc68a283f93223bd7f130ee09a
sschan99 <seungchan1219@gmail.com>
2022-11-01 18:23:35 +0900

    refactor: integrate branching when the number of digits is different

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,44 +10,36 @@ class Problem1 {
         return left + 1 == right && left >= 1 && right <= 400 && right % 2 == 0;
     }
 
-    public static int addEachNum(int...num) {
+    public static int addEachNum(int num) {
         int result = 0;
 
-        for (int n : num) {
-            result += n;
+        while (num != 0) {
+            result += num % 10;
+            num /= 10;
         }
         return result;
     }
 
-    public static int mulEachNum(int...num) {
+    public static int mulEachNum(int num) {
         int result = 1;
 
-        for (int n : num) {
-            result *= n;
+        while (num != 0) {
+            result *= num % 10;
+            num /= 10;
         }
         return result;
     }
 
     public static int calMaxNum(int num) {
-        int addResult, mulResult;
-
-        if (num < 10) { // single digit
-            return num;
-        }
-        if (num < 100) { // two-digit number
-            addResult = addEachNum(num / 10, num % 10);
-            mulResult = mulEachNum(num / 10, num % 10);
-        } else { // three-digit number
-            addResult = addEachNum(num / 100, num % 100 / 10, num % 10);
-            mulResult = mulEachNum(num / 100, num % 100 / 10, num % 10);
-        }
-        return Math.max(addResult, mulResult);
+        return Math.max(addEachNum(num), mulEachNum(num));
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
         if (!isValid(pobi) || !isValid(crong)) {
             return -1;
         }
+
         int pobiScore = Math.max(calMaxNum(pobi.get(0)), calMaxNum(pobi.get(1)));
         int crongScore = Math.max(calMaxNum(crong.get(0)), calMaxNum(crong.get(1)));
 
