a0d2079b413206570538d40816b91771791ee531
JohnPrk <uo3641493@gmail.com>
2022-11-01 17:51:31 +0900

    feat : Users 클래스 및 테스트 작성(Users 객체를 두 개씩 묶은 리스트의 리스트를 변수로 가지는 UserMatchingList 객체 반환)

--- src/main/java/onboarding/Users.java
+++ src/main/java/onboarding/Users.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Users {
@@ -15,5 +17,16 @@ public class Users {
 
     public boolean compareUsers() {
         return users.get(0).compareSplitList(users.get(1));
+    }
+
+    public UserMatchingList getUserMatchingList() {
+        List<Users> list = new ArrayList<>();
+
+        for (int i = 0; i < this.users.size(); i++) {
+            for (int j = i + 1; j < this.users.size(); j++) {
+                list.add(new Users(Arrays.asList(this.users.get(i), this.users.get(j))));
+            }
+        }
+        return new UserMatchingList(list);
     }
 }
