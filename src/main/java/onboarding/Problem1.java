57cda57ef7c9457016c7370423353fe10731b8b1
ahinkim <ahin98@naver.com>
2022-11-01 09:07:26 +0900

    refactor(problem1.md): Add function to find the maximum value of Crong

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -28,6 +28,29 @@ class Problem1 {
             System.out.println(total);
             pmax_total = Math.max(pmax_total, total);
         }
+        // 2. 크롱도 1과 마찬가지로 가장 큰 수를 구한다.
+        int cmax_total = 0;
+        for (int i = 0; i < 2; i++) {
+            String page = Integer.toString(crong.get(i));
+            int total = 0;
+            int N = page.length();
+            for (int j = 0; j < N; j++) {
+                char num = page.charAt(j);
+                // 더해야 할 때
+                if (total <= 1 || num == '0' || num == '1') {
+                    total += num - '0';
+                }
+                // 곱해야 할 때
+                else {
+                    if (total == 0) {
+                        total = 1;
+                    }
+                    total *= num - '0';
+                }
+            }
+            System.out.println(total);
+            cmax_total = Math.max(cmax_total, total);
+        }
         return answer;
     }
 }
