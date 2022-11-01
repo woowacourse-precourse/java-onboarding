1466adde7c7729727b1326a16c3f3cdec111013f
WonSubSub <boxer1995@naver.com>
2022-11-01 18:27:05 +0900

    Problem4 Modification

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,14 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = "answer";
+        answer = "";
         String abclist = "abcdefghijklmnopqrstuvwxyz";
 
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             int char_index = abclist.indexOf(Character.toLowerCase(c));
-            if (c == ' '){
-                answer += ' ';
+            if (char_index == -1){
+                answer += c;
             } else if (c != Character.toLowerCase(c)){
                 answer += Character.toUpperCase(abclist.charAt(25 - char_index));
             } else {
