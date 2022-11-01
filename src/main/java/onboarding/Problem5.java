6dc3ab7dab8bd6e2138d5cf3837cd68228c2e757
c1ce <chalicenuclear@gmail.com>
2022-11-01 23:41:38 +0900

    Problem4 clear

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) { 
+    public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         answer =changeMoney(money);
         return answer;
