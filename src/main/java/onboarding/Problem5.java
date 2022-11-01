4a657b0fe121c850a3c8a1c4fc5abcf81fbfa283
silver <silver36c@gmail.com>
2022-11-01 22:36:37 +0900

    feat: Problem5 메인 로직 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        final int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int left = money;
+        for (int unit: units) {
+            answer.add(left / unit);
+            left = left%unit;
+        }
         return answer;
     }
 }
