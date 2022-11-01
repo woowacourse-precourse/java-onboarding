cb04697ee2aa639646449fddb53585f2cd01318c
ahinkim <ahin98@naver.com>
2022-11-01 09:32:26 +0900

    refactor(problem1.md): Modify function 1, 2

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -13,45 +13,37 @@ class Problem1 {
         int pmax_total = 0;
         for (int i = 0; i < 2; i++) {
             String page = Integer.toString(pobi.get(i));
-            int total = 0;
+            int addTotal = 0;
+            int multiplyTotal = 1;
             int N = page.length();
+
             for (int j = 0; j < N; j++) {
                 char num = page.charAt(j);
                 // 더해야 할 때
-                if (total <= 1 || num == '0' || num == '1') {
-                    total += num - '0';
-                }
+                addTotal += num - '0';
                 // 곱해야 할 때
-                else {
-                    if (total == 0) {
-                        total = 1;
-                    }
-                    total *= num - '0';
-                }
+                multiplyTotal *= num - '0';
             }
-            pmax_total = Math.max(pmax_total, total);
+            pmax_total = Math.max(pmax_total, addTotal);
+            pmax_total = Math.max(pmax_total, multiplyTotal);
         }
         // 2. 크롱도 1과 마찬가지로 가장 큰 수를 구한다.
         int cmax_total = 0;
         for (int i = 0; i < 2; i++) {
             String page = Integer.toString(crong.get(i));
-            int total = 0;
+            int addTotal = 0;
+            int multiplyTotal = 1;
             int N = page.length();
+
             for (int j = 0; j < N; j++) {
                 char num = page.charAt(j);
                 // 더해야 할 때
-                if (total <= 1 || num == '0' || num == '1') {
-                    total += num - '0';
-                }
+                addTotal += num - '0';
                 // 곱해야 할 때
-                else {
-                    if (total == 0) {
-                        total = 1;
-                    }
-                    total *= num - '0';
-                }
+                multiplyTotal *= num - '0';
             }
-            cmax_total = Math.max(cmax_total, total);
+            cmax_total = Math.max(cmax_total, addTotal);
+            cmax_total = Math.max(cmax_total, multiplyTotal);
         }
         // 3. 포비와 크롱의 점수를 비교해 승, 패, 무승부를 정한다.
         if(pmax_total == cmax_total){
