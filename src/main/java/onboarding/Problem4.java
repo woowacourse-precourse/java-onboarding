2dcf822b208757e5892c75ba9948ac1170d00d4d
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 14:15:41 +0900

    [4번] feat: 한 문자씩 Frog Rule에 따라 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,29 @@
 package onboarding;
 
+import onboarding.Problem4Package.LowerCase;
+import onboarding.Problem4Package.UpperCase;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    // 2. 한 문자씩 Frog Rule에 따라 변환
+    public static char translateByFrog(char inputChar)
+    {
+        UpperCase upperCase = new UpperCase();
+        LowerCase lowerCase = new LowerCase();
+
+        if (upperCase.isTrue(inputChar)) {
+            return upperCase.translateAlphabetByFrog(inputChar);
+        }
+
+        if (lowerCase.isTrue(inputChar)) {
+            return lowerCase.translateAlphabetByFrog(inputChar);
+        }
+
+        // 대소문자가 아닌 경우 (" " 일 경우 등등)
+        return inputChar;
     }
 }
