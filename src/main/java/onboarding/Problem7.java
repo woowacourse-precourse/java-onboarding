e8ddc2174d502dc5e94a2cf3dab49db3b81ae113
SeokHwan An <asok369258@naver.com>
2022-11-01 09:22:16 +0900

    feat [Problem7] : user의 길이가 1이상 30이하인지 검증하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,8 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    private static final int USER_MIN_LENGTH = 1;
+    private static final int USER_MAX_LENGTH = 30;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    //user의 길이가 1이상 30이하인지 검증하는 기능
+    public static boolean checkUserLengthValidation(String user) {
+        return USER_MIN_LENGTH <= user.length() && user.length() <= USER_MAX_LENGTH;
+    }
+
 }
