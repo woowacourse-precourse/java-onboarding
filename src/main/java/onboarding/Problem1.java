df9d678488a65520a1b5aa67e8f74e3e114f59e0
ParkJoungWoo <jug78@naver.com>
2022-11-01 18:55:02 +0900

    문제 1) 기능 1 : 유저의 점수를 계산해주는 기능 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,20 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+    private static int getAddMulMax(List<Integer> first_list){
+        int result = 0;
+        int temp_list, temp_add_cal, temp_mul_cal;
+        for(int i = 0; i < first_list.size(); i++){
+            temp_list = first_list.get(i);
+            temp_add_cal = 0;
+            temp_mul_cal = 1;
+            while(temp_list > 0){
+                temp_add_cal += temp_list % 10;
+                temp_mul_cal *= temp_list % 10;
+                temp_list /= 10;
+            }
+            result = (temp_add_cal > temp_mul_cal) ? temp_add_cal : temp_mul_cal;
+        }
+        return result;
+    }
 }
