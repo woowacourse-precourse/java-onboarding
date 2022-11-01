4f88d5641e960e9256edafc54b83cb57799b2a49
yewon <yewon.ux@gmail.com>
2022-11-01 18:08:26 +0900

    기능목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        /*
+        기능 목록
+        1. 각 페이지 번호가 담긴 리스트를 만든다.
+        2. 리스트를 이용해 가장 큰 수를 구한다.
+        3. 두 점수를 비교한다.
+        4. 예외 case를 처리한다.
+        */
         int answer = Integer.MAX_VALUE;
         return answer;
     }
