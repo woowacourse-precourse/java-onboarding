c33ee08ba79e0bfb66178087bd78664746959857
이연재 <leeyj0304@gmail.com>
2022-11-01 19:37:14 +0900

    Created loop to remove redundant adjacent elements in a String

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,21 @@
 package onboarding;
 
-import java.util.*;
-
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        while (compareAdjacent(cryptogram)) {
+            for (int i = 0; i < cryptogram.length() - 1; i++) {
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    String begin = cryptogram.substring(0, i);
+                    String end = cryptogram.substring(i + 2);
+                    cryptogram = begin + end;
+                }
+            }
+        }
+
+        answer = cryptogram;
+
         return answer;
     }
 
