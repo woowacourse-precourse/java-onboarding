30b997c0cbd610a32f8fc4112e921961d4e76ddf
villya <sunwooye@gmail.com>
2022-11-01 23:04:53 +0900

    prob 1 & 2 & 3 & 4 done code prob 5 next

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,14 +2,19 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = word;
+        StringBuilder answer = new StringBuilder(word);
         char alph;
         int k;
         for(k=0;k<answer.length();k++){
             alph = answer.charAt(k);
-
+            if(alph >= 'A' && alph <= 'Z'){
+                alph = (char)( (int)'A' + ( 25 - ((int)alph - (int)'A')));
+            }if(alph >= 'a' && alph <= 'z'){
+                alph = (char)( (int)'a' + ( 25 - ((int)alph - (int)'a')));
+            }
+            answer.setCharAt(k, alph);
         }
 
-        return answer;
+        return answer.toString();
     }
 }
