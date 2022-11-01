1b289edbcb451450b624aafb73c7056cc5a56621
Park Sungchul <try615@naver.com>
2022-11-01 20:18:34 +0900

    [feat] Problem1.java : 메소드 주석 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,8 +5,15 @@ import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
+    /**
+     * 페이지 최대 자리수 final 상수
+     */
     static final int LEN_NUMBER_OF_DIGITS = 3;
 
+    /**
+     * solution 메소드.
+     * 예외처리 후 게임 진행.
+     */
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         if (!verifyGame(pobi) || !verifyGame(crong)) {
             return -1;
@@ -15,6 +22,10 @@ class Problem1 {
         }
     }
 
+    /**
+     * 게임 진행 메소드.
+     * 페이지를 점수로 변환 후 결과 도출.
+     */
     private static int playGame(List<Integer> pobi, List<Integer> crong) {
         int pobiScore = calcScore(pobi);
         int crongScore = calcScore(crong);
@@ -22,6 +33,10 @@ class Problem1 {
         return resultGame(pobiScore, crongScore);
     }
 
+    /**
+     * 게임 결과 메소드.
+     * 점수 확인 후 승자 도출.
+     */
     private static int resultGame(int pobiScore, int crongScore) {
         if (pobiScore > crongScore) {
             return 1;
@@ -32,6 +47,10 @@ class Problem1 {
         }
     }
 
+    /**
+     * 예외 처리 메소드.
+     * 세부 검증 처리 후 boolean 리턴.
+     */
     private static boolean verifyGame(List<Integer> list) {
         int leftPage = list.get(0);
         int rightPage = list.get(1);
@@ -42,6 +61,9 @@ class Problem1 {
         return true;
     }
 
+    /**
+     * 짝수 홀수 페이지 검증 메소드.
+     */
     private static boolean verifyOddEven(int leftPage, int rightPage) {
         if (leftPage % 2 != 1 || rightPage % 2 != 0) {
             return false;
@@ -49,6 +71,9 @@ class Problem1 {
         return true;
     }
 
+    /**
+     * 연속된 페이지 검증 메소드.
+     */
     private static boolean verifyContinuePage(int leftPage, int rightPage) {
         if (rightPage - leftPage != 1) {
             return false;
@@ -56,6 +81,9 @@ class Problem1 {
         return true;
     }
 
+    /**
+     * 처음과 끝 페이지 검증 메소드.
+     */
     private static boolean verifyEdgePage(int leftPage, int rightPage) {
         if (leftPage == 1 || rightPage == 400) {
             return false;
@@ -63,6 +91,9 @@ class Problem1 {
         return true;
     }
 
+    /**
+     * 페이지 범위 검증 메소드.
+     */
     private static boolean verifyPageBoundary(int leftPage, int rightPage) {
         if (leftPage < 1 || leftPage > 400 || rightPage < 1 || rightPage > 400) {
             return false;
@@ -70,6 +101,9 @@ class Problem1 {
         return true;
     }
 
+    /**
+     * 페이지 자리수 곱셈 메소드.
+     */
     private static int operMulti(int[] arr) {
         int result = 1;
         boolean numberStart = false;
@@ -84,6 +118,9 @@ class Problem1 {
         return result;
     }
 
+    /**
+     * 페이지 자리수 덧셈 메소드.
+     */
     private static int operPlus(int[] arr) {
         int result = 0;
         for (int num : arr) {
@@ -92,10 +129,16 @@ class Problem1 {
         return result;
     }
 
+    /**
+     * 페이지 자리수 덧셈 곱셈 비교 메소드.
+     */
     private static int compareScore(int[] arr) {
         return Math.max(operMulti(arr), operPlus(arr));
     }
 
+    /**
+     * 정수값을 자리수 별로 배열에 저장 메소드.
+     */
     private static int[] intToArr(int page) {
         int[] arr = new int[LEN_NUMBER_OF_DIGITS];
         for (int i =0; i < arr.length; i++) {
@@ -104,6 +147,9 @@ class Problem1 {
         return arr;
     }
 
+    /**
+     * 플레이어 페이지 점수 계산 메소드.
+     */
     private static int calcScore(List<Integer> pages) {
         int score = Integer.MIN_VALUE;
         for (Integer page : pages) {
