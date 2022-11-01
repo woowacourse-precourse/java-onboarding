b13c249fbf3ff20ee9f5c9464fb041cf64ec6ad4
SproutMJ <mjk54565@gmail.com>
2022-11-01 20:55:20 +0900

    refactor: 3을 체크하는 기능을 6, 9도 체크하게 변경

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,13 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i<=number; i++){
+            answer += checkThree(i);
+        }
         return answer;
     }
 
     static int checkThree(int n){
         int result = 0;
         while(n>0){
-            if(result%10 == 3)result++;
+            int tmp = n%10;
+            if(tmp == 3 || tmp ==6 || tmp == 9)result++;
             n/=10;
         }
         return result;
