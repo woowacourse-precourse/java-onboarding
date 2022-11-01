2dfdbcd83805c8a20f5bc1f85ccd81706f021bd9
kmw2378 <rlarla677@gmail.com>
2022-11-01 19:15:00 +0900

    feat(Problem7) - 함께아는 친구 점수 계산 - 입력받은 user와 친구인 사람의 이름을 별도의 컬렉션에 저장 (이미 user와 친구인 사람들은 친구 추천에 뜨면 안되기 때문)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,75 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        FriendRecommendationAlgorithm friendRecommendationAlgorithm
+                = new FriendRecommendationAlgorithm(user, friends, visitors);
+
+        answer = friendRecommendationAlgorithm.result();
         return answer;
+    }
+
+    static class FriendRecommendationAlgorithm {
+
+        private final String user;
+        private final List<List<String>> friends;
+        private final List<String> visitors;
+
+        public FriendRecommendationAlgorithm(String user, List<List<String>> friends, List<String> visitors) {
+            this.user = user;
+            this.friends = friends;
+            this.visitors = visitors;
+        }
+
+        public List<String> result() {
+
+            List<String> acquaintance = getAcquaintance();
+            Map<String, Integer> result = new HashMap<>();
+
+            return getNameList(result);
+        }
+
+        private List<String> getNameList(Map<String, Integer> result) {
+
+            List<Map.Entry<String, Integer>> collect = result.entrySet().stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .collect(Collectors.toList());
+
+            int count = 1;
+            List<String> answer = new ArrayList<>();
+
+            for (Map.Entry<String, Integer> nameScoreEntity : collect) {
+
+                if (count > 5 || nameScoreEntity.getValue() == null) {
+                    break;
+                }
+
+                answer.add(nameScoreEntity.getKey());
+                count++;
+            }
+            return answer;
+        }
+
+        private List<String> getAcquaintance() {
+
+            List<String> acquaintance = new ArrayList<>();
+
+            for (List<String> friend : friends) {
+
+                if (friend.get(0).equals(user)) {
+                    acquaintance.add(friend.get(1));
+                } else if (friend.get(1).equals(user)) {
+                    acquaintance.add(friend.get(0));
+                }
+            }
+
+            return acquaintance;
+        }
     }
 }
