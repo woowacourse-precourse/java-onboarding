1156b08d1721aa8a2957b75569b316bb0d562fe2
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:29:07 +0900

    Feat: Add 박수 개수 반환 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,23 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+
+    private static int countClap(int endNumber) {
+        int result = 0;
+
+        for (int number = 1; number <= endNumber; number++) {
+            String strNumber = String.valueOf(number);
+
+            for (int digit = 0; digit < strNumber.length(); digit++) {
+                int digitNumber = Integer.parseInt(String.valueOf(strNumber.charAt(digit)));
+                if (digitNumber % 3 == 0 && digitNumber != 0) {
+                    result++;
+                }
+            }
+
+        }
+
+        return result;
+    }
 }
