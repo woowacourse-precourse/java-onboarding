bdd46b815cab0085199f1b314c1a0d5d46883f12
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-01 21:53:54 +0900

    [Problem 7] Check visitors already friend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map
+import java.util.Map;
 class Main{
     public static void main(String[] args){
         Problem7 prob = new Problem7();
@@ -44,15 +44,21 @@ public class Problem7 {
             String friendA = friends.get(i).get(0);
             String friendB = friends.get(i).get(1);
             for ( int j = 0 ; j < directFriends.size() ; j ++ ){
-                if ( directFriends.get(j).equals(friendA) ) rank.put(friendA, rank.getOrDefault(friendA, 0) + 10 );
-                if ( directFriends.get(j).equals(friendB) ) rank.put(friendB, rank.getOrDefault(friendB, 0) + 10 );
+                if ( directFriends.get(j).equals(friendA) && ! friendB.equals(user)) 
+                    rank.put(friendB, rank.getOrDefault(friendB, 0) + 10 );
+                if ( directFriends.get(j).equals(friendB) && ! friendA.equals(user) ) 
+                    rank.put(friendA, rank.getOrDefault(friendA, 0) + 10 );
             }
         }
 
         // 3. visitor를 순회하며 +1 씩
         for ( int i = 0 ; i < visitors.size() ; i ++ )
-            rank.put(visitors.get(i), visitors.getOrDefault(visitors.get(i), 0) + 1);
-            
+            if (! directFriends.contains(visitors.get(i)))
+                rank.put(visitors.get(i), rank.getOrDefault(visitors.get(i), 0) + 1);
+        
+        for( String key : rank.keySet() ){
+            System.out.println(key + " " + rank.get(key));
+        }            
         /*
         4. Comparator 사용하여 HashMap 정렬
         5. 정렬된 HashMap의 key 값 5개 추출
