4cd306b4b3a51fc2e4b1d3aff7e31f01c774f328
khj <bbb6254177@gmail.com>
2022-11-01 13:34:14 +0900

    refactor

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 public class Problem3 {
+    //주어진 숫자에 포함된 3, 6, 9의 개수를 반환해주는 함수
     private static int count_369(int num){
         int count = 0;
         while(num >= 1){
@@ -14,6 +15,8 @@ public class Problem3 {
 
     public static int solution(int number) {
         int count = 0;
+
+        //1부터 주어진 수 까지 369게임을 진행했을 때 손뼉을 친 횟수를 구한다.
         for(int i = 1; i<=number; i++){
             count += count_369(i);
         }
