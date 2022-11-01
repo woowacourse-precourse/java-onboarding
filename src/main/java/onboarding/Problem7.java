5d7e68856d7b160e011b1a2cc5ad07c9a7c0d269
yun8565 <yunyun8565@naver.com>
2022-11-01 14:05:28 +0900

    feat(P7): 함께 아는 친구의 수에 따른 점수 계산 기능 추가
    - 친구 점수를 저장하기 위해 해시맵 사용
    - 사용자와 함께 아는 친구의 수 만큼 10점 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static HashMap<String, Integer> friendScore;
+    public static List<String> answer;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        answer = new ArrayList<>();
+        friendScore = new HashMap<>();
+
+        countScoreByFriend(user, friends);
+
         return answer;
+    }
+
+    public static void countScoreByFriend(String user, List<List<String>> friends) {
+        for(List<String> friendInfo : friends) {
+            String friend = friendInfo.get(0);
+            String candidate = friendInfo.get(1);
+
+            friendScore.putIfAbsent(friend, 0);
+            if(!candidate.equals(user)){
+                friendScore.put(candidate, friendScore.getOrDefault(candidate, 0) + 10);
+            }
+        }
     }
 }
