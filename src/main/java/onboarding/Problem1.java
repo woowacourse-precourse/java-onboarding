749d11a2e740177b571c34ddf809a4491788faf6
silver <silver36c@gmail.com>
2022-11-01 20:49:37 +0900

    feat: Problem1 자리수별로 숫자 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+
+    // 자리수별로 숫자 분리
+    private static List<Integer> separateNumber(int num){
+        List<Integer> separatedNumbers = new ArrayList<>();
+        final int NOTATION = 10;
+
+        while (num > NOTATION){
+            separatedNumbers.add(num % NOTATION);
+            num /= NOTATION;
+        }
+        separatedNumbers.add(num % NOTATION);
+        return separatedNumbers;
+    }
     }
 }
