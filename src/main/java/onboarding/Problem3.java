27796c5faf358f1173536cca385c1c2db3838fe3
kyliecamila <kyliecamila@naver.com>
2022-11-01 20:48:39 +0900

    [feat]problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer ;
+        int count=0;
+        for(int i=1;i<number+1;i++){
+            String numstr = Integer.toString(i);
+            for(int j=0; j<numstr.length(); j++) {
+                int temp_num=Integer.parseInt(numstr.substring(j, j + 1));
+                if(temp_num==3||temp_num==6||temp_num==9){
+                    count++;
+                }
+            }
+        }
+
+       answer=count;
         return answer;
     }
 }
