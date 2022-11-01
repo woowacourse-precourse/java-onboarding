cdf46383209d5d1da8d4f260d502c162982fcf60
오승윤 <>
2022-11-01 15:16:46 +0900

    추가 : Problem2 찾은 구간을 복호화 하는 기능
    암호문에 X 로 표시된 구간을 제거함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem2 {
+
+    private StringBuffer cryptograph = new StringBuffer();
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private void deleteFoundSection(){
+        String decryptedCryptograph = cryptograph.toString().replace("X","");
+        cryptograph = new StringBuffer(decryptedCryptograph);
     }
 }
