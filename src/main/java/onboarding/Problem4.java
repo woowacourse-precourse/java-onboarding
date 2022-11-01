a5e98c46eb15e1b146a5505e122bd32b5b774c71
simjaeseo <delpho@naver.com>
2022-11-01 19:04:23 +0900

    feat: 청개구리 사전 초기화 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.Arrays;
+
 /**
  * 기능 목록
  * 1. 청개구리 사전 초기화 함수
@@ -8,6 +10,22 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        char[] reverseDictionary = new char[26];
+
+        initReverseDictionary(reverseDictionary);
+
         return answer;
+    }
+
+    public static void initReverseDictionary(char[] reverseDictionary) {
+        char reverseAlphabet = 'Z';
+        for (char alphabet = 'A'; alphabet <= 'Z'; alphabet++) {
+            int index = alphabet - 'A';
+
+            reverseDictionary[index] = reverseAlphabet;
+
+            reverseAlphabet--;
+        }
     }
 }
