4cbb8735e46c26eef867ad5160ae7c5dfd696193
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-01 17:45:27 +0900

    feat(problem2): make variable

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,29 +1,21 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem2 {
 
-	static String answer;
+	static char[] chars;
+	static Boolean[] removeIndex;
+	static int lengthOfString;
 
 	public static String solution(String cryptogram) {
-		answer = "";
-		char[] chars = cryptogram.toCharArray();
-
-		for (Character c : chars) {
-			answer += String.valueOf(c);
-
-			if (answer.length() > 1) {
-				checkEndChars();
-			}
-		}
-
+		Boolean isChanged = false;
+		chars = cryptogram.toCharArray();
+		lengthOfString = chars.length;
+		removeIndex = new Boolean[lengthOfString];
+		Arrays.fill(removeIndex, Boolean.FALSE);
+		int nowRemove = 0;
+		String answer = "";
 		return answer;
-	}
-
-	private static void checkEndChars() {
-		char c1 = answer.charAt(answer.length() - 2);
-		char c2 = answer.charAt(answer.length() - 1);
-		if (c1 == c2) {
-			answer = answer.substring(0, answer.length() - 2);
-		}
 	}
 }
