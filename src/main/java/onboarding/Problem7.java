cd1d08f1588cb55c27b44e65d610a0028ac9e5dd
hanuel <ezzeezz@naver.com>
2022-11-01 16:44:56 +0900

    feat: [problem7] Add suggestion

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -47,6 +47,40 @@ public class Problem7 {
         }
     }
 
+    private static List<String> suggestion(Map<String, Integer> newFriend) {
+        List<String> keySet = new ArrayList<>(newFriend.keySet());
+        Collections.sort(keySet, (o1, o2) -> (newFriend.get(o2).compareTo(newFriend.get(o1))));
+
+        List<String> result = new ArrayList<>();
+        int i;
+        for (i = 0; i < 5 && i < keySet.size(); i++) {
+            String key, nextKey;
+            int key_idx, nextKey_idx;
+
+            if (i == keySet.size() - 1) {
+                key_idx = i - 1;
+                nextKey_idx = i;
+                key = keySet.get(key_idx);
+                nextKey = keySet.get(nextKey_idx);
+            } else {
+                key_idx = i;
+                nextKey_idx = i + 1;
+                key = keySet.get(key_idx);
+                nextKey = keySet.get(nextKey_idx);
+            }
+
+            if (newFriend.get(key) == newFriend.get(nextKey)) {
+                if (key.compareTo(nextKey) > 0) {
+                    String tmp = key;
+                    keySet.set(key_idx, nextKey);
+                    keySet.set(nextKey_idx, tmp);
+                }
+            }
+            result.add(keySet.get(i));
+        }
+        return result;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> alreadyFriend = new ArrayList<>();
         List<List<String>> candidate = new ArrayList<>();
