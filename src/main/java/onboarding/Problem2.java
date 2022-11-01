d0621ca040cb227f9400bd165bcb00c039af0372
ahinkim <ahin98@naver.com>
2022-11-01 10:16:14 +0900

    refactor(problem2.md): Modify feature list, feat(problem2.java): Add funtion to delete consecutive duplicate characters

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        int N = cryptogram.length();
+        // 1. 연속하는 중복 문자를 삭제한다.
+        char prev = cryptogram.charAt(0);
+        String res = cryptogram.substring(0, 1);
+        for (int i = 1; i < N; i++){
+            char now = cryptogram.charAt(i);
+            if(prev != now){
+                res += now;
+            }
+            else{
+                res = res.substring(0, res.length()-1);
+            }
+            prev = now;
+        }
         return answer;
     }
 }
