6b32ebb50fea683937df19d894774f187d977ab6
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 15:25:01 +0900

    feat: 문제 해결에 필요한 상수 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    private final static int TIMELINE_VISIT_SCORE = 1;
+    private final static int KNOW_TOGETHER_SCORE = 10;
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
