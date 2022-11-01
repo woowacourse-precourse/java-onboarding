df2303fe221f95b2a2621338ffc8345e70add76a
JinNitt <js020315@gmail.com>
2022-11-01 22:39:48 +0900

    Changes

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,25 +3,25 @@ package onboarding;
 import java.util.Scanner;
 
 public class Problem3 {
+    static int answer = 0;
     public static int solution() { //ex)33
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt(); //33
-        int answer = 0;
-        int temp = 0;
 
         for (int i = number; i > 0; i--) {
             String numStr = Integer.toString(i);
             int numLen = numStr.length();
 
             while (numLen > 0) {
+                int temp;
                 if (numLen == 1) { //한 자리 숫자
                     temp = i % 10;
                     if (temp == 3 || temp == 6 || temp == 9) {
                         answer++;
                     }
                 }
-                else { //두 자리 이상
-                    temp = (int) (i / Math.pow(10, numLen - 1)); //10^2
+                else { //두 자리 이상 i333 len3
+                    temp = (int) (i / Math.pow(10, numLen - 1)); //temp3
                     if (temp == 3 || temp == 6 || temp == 9)
                         answer++;
                     i = (int) (i - (temp * (Math.pow(10, numLen - 1))));
