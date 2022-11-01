f7eaf0f337797fd574959a38e44e599ef64aade9
ChaCha3088 <cha3088@gmail.com>
2022-11-01 11:54:12 +0900

    final: problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,25 +4,19 @@ import java.util.*;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        makeBillAndCoin();
         ArrayList<Integer> resultList = divideByBillAndCoin(money);
         return resultList;
     }
 
     private static ArrayList<Integer> divideByBillAndCoin(int money) {
         ArrayList<Integer> billAndCoinList = new ArrayList<> ();
+        Map<Integer, Integer> billAndCoin = makeBillAndCoin();
         int quotient = 0;
         int remainder = money;
         for ( Integer key : billAndCoin.keySet() ){
-            System.out.println("key is");
-            System.out.println(key);
             int divisor = billAndCoin.get(key);
             quotient = (int)(remainder / divisor);
             remainder = remainder % divisor;
-            System.out.println("quotient is");
-            System.out.println(quotient);
-            System.out.println("remainder is");
-            System.out.println(remainder);
             checkQuotient(quotient, billAndCoinList);
         }
         return billAndCoinList;
@@ -34,11 +28,9 @@ public class Problem5 {
         } else if (quotient < 1) {
             billAndCoinList.add(quotient);
         }
-        System.out.println("billAndCoinList is");
-        System.out.println(billAndCoinList);
     }
 
-    public static void makeBillAndCoin() {
+    public static Map<Integer, Integer> makeBillAndCoin() {
         Map<Integer, Integer> billAndCoin = new LinkedHashMap<>();
         billAndCoin.put(50000, 50000);
         billAndCoin.put(10000, 10000);
@@ -49,5 +41,6 @@ public class Problem5 {
         billAndCoin.put(50, 50);
         billAndCoin.put(10, 10);
         billAndCoin.put(1, 1);
+        return billAndCoin;
     }
 }
