dcf805bef154cb6bba94de8e1b226837ed3ec921
YUSEONJUN <seonjun0906@gmail.com>
2022-11-01 13:25:46 +0900

    [Problem 5] 예외 사항 조건 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        check1(money);
         List<Integer> answer = new ArrayList<>();
         int[] divide = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
@@ -15,5 +16,11 @@ public class Problem5 {
         }
 
         return answer;
+    }
+    public static void check1(int money) throws IllegalArgumentException{
+        if (money < 1 || money > 1000000) {
+            throw new IllegalArgumentException("money가 1보다 작거나 1000000을 초과했다.");
+        }
+
     }
 }
