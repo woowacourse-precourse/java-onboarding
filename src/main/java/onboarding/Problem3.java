3d81590949937a6fb8b1bdc34e0bfb6c24221607
Eungyu-code <Agyu625@gmail.com>
2022-11-01 13:04:02 +0900

    1부터 number까지 각 수의 자리수를 확인해 3, 6, 9 중 하나일 경우 answer += 1, 정답 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        // 각 자리 숫자를 확인해 3, 6, 9 중 하나일 경우는 answer += 1
+        for (int i = 1; i <= number; i++) {
+            int res = i;
+            while (res != 0) {
+                if (res % 10 == 3 || res % 10 == 6 || res % 10 == 9) answer += 1;
+                res /= 10;
+            }
+        }
         return answer;
     }
 }
