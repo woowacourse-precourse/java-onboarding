8c71cc8f0559b3090b1f4aecf433c1020a23ce1a
김형기 <khk211113@naver.com>
2022-11-01 23:25:37 +0900

    [FEAT] Problem5 - 내림차순 돈 빼기 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+
+    private static final int[] MONEY_SIZE = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+        Integer[] result = new Integer[9];
+        int idx = 0;
+
+        // idx가 더 이상 움직일 수 없거나, money가 0이 될 때까지 반복
+        while (money > 0 && idx < MONEY_SIZE.length) {
+
+            if (money >= MONEY_SIZE[idx]) {
+                result[idx]++;
+                money -= MONEY_SIZE[idx];
+            } else {
+                idx++;
+            }
+        }
+
     }
 }
