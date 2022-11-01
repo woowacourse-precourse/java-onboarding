7f0d5850de59396a0b85e1fe5114993913a36acc
sun007021 <sun007021@gmail.com>
2022-11-01 13:31:40 +0900

    fix: 알파벳 확인 조건문 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,16 +2,15 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = reverseAlphabetString(word);
         return answer;
     }
 
     private static int whatKindOfChar(char letter){//소문자 알파벳 =1, 대문자 알파벳 = 2, 그외 -1
-        int ascii = (int)letter; // 문자 아스키코드로 변환
-        if(ascii >=97 && ascii <=122){// 소문자 알파벳인 경우
+        if(letter >='a' && letter <='z'){// 소문자 알파벳인 경우
             return 1;
         }
-        else if(ascii >=41 && ascii <=90){// 대문자 알파벳인 경우
+        else if(letter >='A' && letter <='Z'){// 대문자 알파벳인 경우
             return 2;
         }
         else{// 그외 다른 문자
