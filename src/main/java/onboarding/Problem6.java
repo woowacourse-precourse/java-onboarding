372867c8773d58752181ea1a0fdb09614a54ca8c
ckn1070 <ckn1070@naver.com>
2022-11-01 16:29:52 +0900

    Problem 7: make a dictionary of friends list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    /*
+    static List<String> subName(List<List<String>> forms){
+        for(int i=0; i<forms.get(i).get(1).length(); i++){
+            String substring = forms.get(i).get(1).substring(i, i+3);
+        }
+    }
+    static findDup(List<List<String>> forms){
+        for(int i=0; i<forms.size(); i++){
+            for(int j=0; j<forms.get(i).get(1).length()-1; j++){
+                String substring = forms.get(i).get(1).substring(j, j+2);
+                if()
+            }
+        }
+    } */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+    public static void main(String[] args) {
+        List<List<String>> forms = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "워니"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
+        );
+        List<String> a = forms.get(1);
+        int b = forms.size();
+        List<String> c = new ArrayList<>();
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,46 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String, List<String>> friendsDic(List<List<String>> friends){
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> emptyList = new ArrayList<>();
+        for(int i=0; i<friends.size(); i++){
+            if(!map.containsKey(friends.get(i).get(0))){
+                map.put(friends.get(i).get(0), emptyList);
+            } else {
+                List<String> friendsList = map.get(friends.get(i).get(0));
+                friendsList.add(friends.get(i).get(1));
+                map.put(friends.get(i).get(0), friendsList);
+            }
+            if(!map.containsKey(friends.get(i).get(1))){
+                map.put(friends.get(i).get(1), emptyList);
+            } else {
+                List<String> friendsList = map.get(friends.get(i).get(1));
+                friendsList.add(friends.get(i).get(0));
+                map.put(friends.get(i).get(1), friendsList);
+            }
+        }
+        return map;
+    }
+    static Map<String, Integer> peopleDic(List<List<String>> friends, List<String> visitors){
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<friends.size(); i++){
+            if(!map.containsKey(friends.get(i).get(0))){
+                map.put(friends.get(i).get(0), 0);
+            }
+            if(!map.containsKey(friends.get(i).get(1))){
+                map.put(friends.get(i).get(1), 0);
+            }
+        }
+        for(int i=0; i<visitors.size(); i++){
+            if(!map.containsKey(visitors.get(i))){
+                map.put(visitors.get(i), 0);
+            }
+        }
+        return map;
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
