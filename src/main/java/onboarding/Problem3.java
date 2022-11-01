48244d0a27072ac67f89b44ad4f6851a1e77de5b
minju412 <ln8847@ajou.ac.kr>
2022-11-01 20:08:48 +0900

    refactor: [p3] 변수명 변경 strNum -> letter

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,7 @@ public class Problem3 {
 
 	public static int solution(int number) {
 		int cnt = 0;
-		String strNumber = "";
+		String letter = "";
 		char oneLetter = ' ';
 
 		if (checkRestrictions(number) == EXCEPTION) {
@@ -14,11 +14,11 @@ public class Problem3 {
 		}
 
 		for (int i = 1; i <= number; i++) {
-			strNumber = String.valueOf(i); // int -> string 변환
+			letter = String.valueOf(i); // int -> string 변환
 
 			// 예를 들어, 36 이라는 숫자에는 박수 두 번
-			for (int j = 0; j < strNumber.length(); j++) {
-				oneLetter = strNumber.charAt(j);
+			for (int j = 0; j < letter.length(); j++) {
+				oneLetter = letter.charAt(j);
 				if (oneLetter == '3' || oneLetter == '6' || oneLetter == '9') {
 					cnt++;
 				}
