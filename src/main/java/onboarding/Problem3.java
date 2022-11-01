e692068931f9befcc8b6cd6edeea8661f3b59a57
ckn1070 <ckn1070@naver.com>
2022-11-01 23:08:27 +0900

    Problem 3: Added comments for understanding

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+    // 손뼉 치는 횟수 Count
     static int numCounter(int curNum){
         int count = 0;
         while(curNum > 1){
+            // 숫자의 1의자리부터 높은 자릿수 순서대로 3, 6, 9가 들어가는지 확인
             int remainder = curNum % 10;
             if(remainder == 3 || remainder == 6 || remainder == 9){
                 count += 1;
@@ -12,6 +14,7 @@ public class Problem3 {
         }
         return count;
     }
+    // 1부터 제시된 숫자까지 누적된 손뼉 횟수 Count
     static int clapCounter(int clapNum){
         int count = 0;
         while(clapNum>0){
