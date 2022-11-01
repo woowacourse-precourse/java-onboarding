96f03b6f695009d4e634cfd2aeec559554e1ae0a
jiwon1217 <koagjw@gmail.com>
2022-11-01 15:48:54 +0900

    [Feature] 이메일, 닉네임 추출

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,29 +7,10 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        for (int i = 0; i < forms.size(); i++) {
-            String nickname = forms.get(i).get(1);
-            for (int j = 0; j < nickname.length() - 1; j++) {
-                String splitNickname = nickname.substring(j, j + 2);
-
-                for (int k = i + 1; k < forms.size(); k++) {
-                    String comparedNickname = forms.get(k).get(1);
-
-                    for (int x = 0; x < comparedNickname.length() - 1; x++) {
-                        String splitComparedNickname = comparedNickname.substring(x, x + 2);
-
-                        if (splitNickname.equals(splitComparedNickname)) {
-                            if (!answer.contains(forms.get(i).get(0))) {
-                                answer.add(forms.get(i).get(0));
-                            } else if (!answer.contains(forms.get(k).get(0))) {
-                                answer.add(forms.get(k).get(0));
-                            }
-                        }
-                    }
-                }
-            }
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
         }
-        Collections.sort(answer);
         return answer;
     }
 }
