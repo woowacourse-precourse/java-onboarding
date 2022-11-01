4a5a6148c745be0fa8a8270a939bce1565e71e37
jinuk <devuks0820@gmail.com>
2022-11-01 11:32:48 +0900

    feat: Problem2 Add method
    중복된 단어들의 인덱스를 확인해서 제거해주는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,53 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem2 {
+    private static StringBuffer cryptoBuffer = new StringBuffer();
     public static String solution(String cryptogram) {
+        cryptoBuffer = new StringBuffer(cryptogram);
+
         String answer = "answer";
+
+        while(checkOverlapString()) {
+            int index = findOverlapIndex();
+
+            deleteOverlapWord(index);
+        }
+
+        answer = cryptoBuffer.toString().trim();
+
         return answer;
     }
+    private static boolean checkOverlapString() {
+        boolean checkOverlap = false;
+
+        for(int i = 0 ; i < cryptoBuffer.length()-1; i++) {
+            if(cryptoBuffer.charAt(i) == cryptoBuffer.charAt(i+1)) {
+                checkOverlap = true;
+                break;
+            }
+        }
+
+        return checkOverlap;
+    }
+
+    private static int findOverlapIndex() {
+        int index = -1;
+
+        for(int i = 0 ; i < cryptoBuffer.length()-1; i++) {
+            if(cryptoBuffer.charAt(i) == cryptoBuffer.charAt(i+1)) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    private static void deleteOverlapWord(int index) {
+        cryptoBuffer.delete(index,index+2);
+        return;
+    }
+
 }
