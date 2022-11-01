79f4c15dd662a2ae6d7b82c8e40def01537fa3a1
gun1kim <obkh31@naver.com>
2022-11-01 11:25:00 +0900

    feat: 4번 문제 풀이 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i=0;i<word.length();i++){
+            if (word.charAt(i) >= 'a' && word.charAt(i) <='Z'){
+                answer += (char)('A' + 'Z' - word.charAt(i));
+            }
+            else {
+                answer += (char)word.charAt(i);
+            }
+        }
         return answer;
     }
 }
