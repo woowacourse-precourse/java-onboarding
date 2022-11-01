daef1616ab8ed0003671d26c1c45f3e92f24d8ff
wooseokchoe <chldntjr0425@gmail.com>
2022-11-01 23:26:27 +0900

    문제7 / feat : 겹치는 친구를 체크하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class Problem7 {
     static List<String> userFriends = new ArrayList<>();
+    static Map<String, Integer> scoreDict = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         makeUserFriends(user, friends);
+        checkFriends(user, friends);
         return answer;
     }
     private static void makeUserFriends(String user, List<List<String>> friends) {
@@ -14,5 +16,30 @@ public class Problem7 {
             if(Objects.equals(relation.get(0), user)) userFriends.add(relation.get(1));
             else if(Objects.equals(relation.get(1), user)) userFriends.add(relation.get(0));
         }
+    }
+    private static void checkFriends(String user, List<List<String>> friends) {
+        for (List<String> relation : friends) {
+            String userA = relation.get(0);
+            String userB = relation.get(1);
+            if(isAlreadyFriend(user, userA, userB)) continue;
+            if(isNewFriend(userA, userB)) {
+                scorePerson(userB, 10);
+            }
+            else if(isNewFriend(userB, userA)) {
+                scorePerson(userA, 10);
+            }
+        }
+    }
+    private static boolean isNewFriend(String userA, String userB) {
+        return userFriends.contains(userA) && !userFriends.contains(userB);
+    }
+
+    private static boolean isAlreadyFriend(String user, String userA, String userB) {
+        return Objects.equals(userA, user) || Objects.equals(userB, user);
+    }
+    static void scorePerson(String name, int score) {
+        if(scoreDict.containsKey(name))
+            score += scoreDict.get(name);
+        scoreDict.put(name, score);
     }
 }
