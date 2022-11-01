6841272aeee1c9f15d6ae69c697a6a4601018e40
CJY-kor <vivo_reprise@naver.com>
2022-11-01 16:49:54 +0900

    FEAT(problem2) : i) checkRedupl 구현 자세한 사항은 기능목록 참고

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,40 @@
+/*
+기능목록
+i) checkRedupl -> 중복 문자가 있는지 확인하며 중복 문자가 있는 index를 반환한다.
+ii) reduceRedupl -> 입력받은 문자열에서 같이 입력받은 index에 해당하는 문자룰 없애준다
+iii) solution -> i) ii) 기능을 적절히 이용하여 답을 반환한다.
+ */
+
+
 package onboarding;
 
 public class Problem2 {
+
+    int[] checkRedupl(String cryptogram) {
+        int[] temp_array = new int[cryptogram.length()];
+
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                temp_array[i] = 1;
+                temp_array[i+1] = 1;
+            }
+        }
+
+        return temp_array;
+    }
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        Problem2 agent = new Problem2();
+        String answer = cryptogram;
+
+
+        for (int i = 0; i < 10; i++){
+            int[] indexarray = agent.checkRedupl(answer);
+            answer = agent.reduceRedupl(answer, indexarray);
+        }
+
         return answer;
     }
 }
