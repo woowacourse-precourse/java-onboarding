a234e4ea87f617e6a8e2344a4ce053f34ae867fb
kim1387 <kevink0@naver.com>
2022-11-01 15:31:50 +0900

    feat: 검증 로직 추가 하기
    정상적인 문자열이 입력되지 않은 경우 - 문제에서 요구하는 별도의 요구사항이 없음으로 예외 발생
    1. 문자열 길이가 제한 사항에 속하지 않은 경우 (1이상 1000이하)
    2. 알파벳 소문자로 구성하지 않은 경우

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,9 +5,34 @@ import java.util.*;
 public class Problem2 {
 
   public static String solution(String cryptogram) {
+    validate(cryptogram);
     Deque<Character> cryptogramStack = convertStringToStack(cryptogram);
     Deque<Character> resultStack = eliminateDuplicatedChar(cryptogramStack);
     return convertStackToString(resultStack);
+  }
+
+  public static void validate(String string) {
+    /*
+     정상적인 문자열이 입력되지 않은 경우 - 문제에서 요구하는 별도의 요구사항이 없음으로 예외 발생
+     1. 문자열 길이가 제한 사항에 속하지 않은 경우 (1이상 1000이하)
+     2. 알파벳 소문자로 구성하지 않은 경우
+    */
+    isInRange(string);
+    isStringConsistOfLowerCase(string);
+  }
+
+  public static void isInRange(String string) {
+    if (string.length() < 1 || string.length() > 1000) {
+      throw new IllegalArgumentException("범위 초과");
+    }
+  }
+
+  public static void isStringConsistOfLowerCase(String string) {
+    for (char i : string.toCharArray()) {
+      if (!Character.isLowerCase(i)) {
+        throw new IllegalArgumentException("소문자로만 구성이 되어야합니다.");
+      }
+    }
   }
 
   public static String convertStackToString(Deque<Character> stack) {
