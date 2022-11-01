df1f4d3b9a34aa979d7ee76af32f09ae20db09bb
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 14:27:28 +0900

    feat : 상수 변수 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,18 @@ package onboarding;
 
 import java.util.List;
 
+
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+
+    private static final int EXCEPTION = -1;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 2;
+    private static final int DRAW = 0;
+
+    private static final int MIN = 1;
+    private static final int MAX = 400;
+    private static final int LEFT = 0;
+    private static final int RIGHT = 1;
+
+
 }
