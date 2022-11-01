03cbea525c9479fdfc6c2c54d7842d26c8e925bf
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-01 19:30:34 +0900

    problem6 feat : find rep and add to emailList

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
 import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashMap<String, String> hashList = new HashMap<>();
+        HashSet<String> emailList = new HashSet<>();
+        for(int i=0;i<forms.size();i++){
+            String name = forms.get(i).get(1);
+            for (int j = 0; j < name.length() - 1; j++) {
+                String key = name.substring(j, j+2);
+                if(hashList.containsKey(key)){
+                    String email = hashList.get(key);
+                    if(!email.equals(forms.get(i).get(0))){
+                        emailList.add(email);
+                        emailList.add(forms.get(i).get(0));
+                    }
+                }
+                hashList.put(key, forms.get(i).get(0));
+            }
+        }
         return answer;
     }
 }
