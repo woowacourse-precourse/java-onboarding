c01a7442d2af72691bd628ef0e186655cae2ab65
bnbnac <bnbnac@naver.com>
2022-11-01 21:28:56 +0900

    feat: (P3)자연수의 자리수를 ArrayList에 저장하는 메소드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    public ArrayList<Integer> digitList(int num) {
+        ArrayList<Integer> digits = new ArrayList<>();
+        while (num > 0) {
+            digits.add(num % 10);
+            num /= 10;
+        }
+        return digits;
     }
 }
