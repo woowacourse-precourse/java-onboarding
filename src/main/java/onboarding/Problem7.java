25c84e120ac1062d9ae0a17af36fb029842f5d07
Cho-El <syzzang21c@gmail.com>
2022-11-01 15:35:12 +0900

    점수 계산하는 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,13 +1,10 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     // 친구 관계 만들어주는 함수
-    public static HashMap<String, ArrayList<String>> friendRelation(List<List<String>> friends){
+    static HashMap<String, ArrayList<String>> friendRelation(List<List<String>> friends){
         HashMap<String, ArrayList<String>> relation = new HashMap<>();
         for(int i = 0 ; i < friends.size(); i++){
             ArrayList<String> list = new ArrayList<>();
@@ -33,9 +30,44 @@ public class Problem7 {
         }
         return relation;
     }
+    // 점수 계산
+    static HashMap<String, Integer> calScore(HashMap<String, ArrayList<String>> relation, String user, List<String> visitors){
+        HashMap<String, Integer> totalScore = new HashMap<>();
+        // 점수 저장 Map 초기화
+        for(String friend : relation.keySet()){
+            totalScore.put(friend, 0);
+        }
+        for(String visitor : visitors){
+            totalScore.put(visitor, 0);
+        }
+        Set<String> userFriends = new HashSet<>(relation.get(user)); // user의 친구인지 검색을 O(1)의 시간복잡도를 위해 Set으로 구현
+        // 함께 아는 친구를 통한 점수 계산
+        for(String userFriend : userFriends){
+            for(String userFriendF : relation.get(userFriend)){
+                if (!userFriends.contains(userFriendF)){// 사용자와 친구가 아니라면
+                    totalScore.put(userFriendF, totalScore.get(userFriendF) + 10);
+                }
+            }
+        }
+
+        // 방문한 횟수를 통한 점수 계산
+        for(String visitor : visitors){
+            if (!userFriends.contains(visitor)){
+                totalScore.put(visitor, totalScore.get(visitor) + 1);
+            }
+        }
+
+        // 점수가 0인 값들과 key값이 user인 값 제거해서 반환
+        totalScore.keySet().removeIf(n -> n.equals(user));
+        totalScore.values().removeIf(n -> n == 0);
+        System.out.println(totalScore);
+        return totalScore;
+    }
+    
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         friendRelation(friends);
+        calScore(friendRelation(friends),user,visitors);
         return answer;
     }
 }
