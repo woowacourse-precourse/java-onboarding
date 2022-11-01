7fcb8e46a177c723a2db9432f44456abfb23f95d
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 14:18:51 +0900

    feat: 숫자에 3,6,9 가 나타난 횟수를 반환하는 함수 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,6 +6,17 @@ public class Problem3 {
         return answer;
     }
 
+    private static int getTheNumberOf369(int number) {
+        int ret = 0;
+        while (number != 0) {
+            if (is369Number(number % 10)) {
+                ret++;
+            }
+            number /= 10;
+        }
+        return ret;
+    }
+
     private static boolean is369Number(int number) {
         return number == 3 || number == 6 || number == 9;
     }
