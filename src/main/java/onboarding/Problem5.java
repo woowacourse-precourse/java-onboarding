e997690be39f2305eafe66fcdbb18351f28818ae
박소현 <parksohyun@bagsohyeons-MacBook-Air.local>
2022-11-01 11:35:59 +0900

    5번 문제 풀이

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
+    public static List<Integer> solution(int money) { // 하나의 새로운 지갑을 만들고 몫과 나머지를 이용해서 풀이
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
