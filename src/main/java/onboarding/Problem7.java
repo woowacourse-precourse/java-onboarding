bd24e81594247ccd5a7ac07c8a40b86b1f3d49c4
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-01 11:00:09 +0900

    feat : [problem7] makeUserFriends()메소드작성 makeUserFriends() : 유저의 친구들 목록을 만들어 주는 함수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> userFriends = makeUserFriends(user, friends);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    //user의 친구들의 리스트를 반환
+    public static List<String> makeUserFriends(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+
+        for (List<String> friend : friends) {
+            //user와 친구인 경우
+            if (friend.get(0).equals(user)) {
+                userFriends.add(friend.get(1));
+            } else if (friend.get(1).equals(user)) {
+                userFriends.add(friend.get(0));
+            }
+        }
+
+        return userFriends;
     }
 }
