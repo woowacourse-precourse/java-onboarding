09619cba00cf69afa0e6fdfdbf7157bf37932be3
silver <silver36c@gmail.com>
2022-11-01 23:57:27 +0900

    feat: Problem7 그래프 구축

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        HashMap<String, List<String>> userGraph = new HashMap<String, List<String>>();
+
+        for (List<String> friend:friends) {
+            getGraph(friend, userGraph);
+        }
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    private static void getGraph(List<String> friend, HashMap<String, List<String>> userGraph){
+        if(userGraph.containsKey(friend.get(0))){
+            userGraph.get(friend.get(0)).add(friend.get(1));
+            return;
+        }
+        userGraph.put(friend.get(0), new ArrayList<>());
+        userGraph.get(friend.get(0)).add(friend.get(1));
+    }
 }
+
