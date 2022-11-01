3d6e697e70199d8e89ff8764671b47ce5fe0a762
yenawee <yaena0319@naver.com>
2022-11-01 15:56:51 +0900

    [FEAT] 2. 인접한 문자 삭제 함수 구현 : 재귀

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,6 +4,27 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         return deleteAdjacentOverlap(cryptogram);
     }
+
+    private static String deleteAdjacentOverlap(String str){
+        if (!ExistAdjacentOverlap(str))
+            return str;
+        char prev = '\0';
+        String new_str = "";
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == prev || str.charAt(i) == str.charAt(i + 1)){
+
+                prev = str.charAt(i);
+                i += 1;
+                continue;
+            }
+            prev = str.charAt(i);
+            new_str += prev;
+        }
+        if (str.charAt(str.length() - 1) != prev)
+            new_str += str.charAt(str.length() - 1);
+        return (deleteAdjacentOverlap(new_str));
+    }
+
     private static boolean ExistAdjacentOverlap(String str){
         char prev = '\0';
         for (int i = 0; i < str.length(); i++){
