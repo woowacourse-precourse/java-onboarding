9cd0d68e1e786f69774a4986c88eee56d5e8d76b
tunkcalb <wjdgusdndi@gmail.com>
2022-11-01 17:33:44 +0900

    problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> cash = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
+
+        for (int i =0;i<cash.size();i++){
+            answer.add(money/cash.get(i));
+            money %= cash.get(i);
+        }
         return answer;
+
     }
 }
