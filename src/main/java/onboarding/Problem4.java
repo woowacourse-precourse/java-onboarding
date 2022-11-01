dc883f7dcadfd5a2b77ee414b4603b76dbc2db75
younghoon211@naver.com <younghoon211@naver.com>
2022-11-01 21:08:04 +0900

    feat(Problem4): 알파벳 초기화 로직 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,4 +10,16 @@ public class Problem4 {
 
         return replaceWord(word);
     }
+
+    private static void initialWord() {
+        for (char i = 'a'; i <= 'z'; i++) {
+            char j = (char) ('z' - i + 'a');
+            initial.put(i, j);
+        }
+
+        for (char i = 'A'; i <= 'Z'; i++) {
+            char j = (char) ('Z' - i + 'A');
+            initial.put(i, j);
+        }
+    }
 }
