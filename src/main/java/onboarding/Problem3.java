db5d72cae190cdf7b8f2ad623253ef80e71d5748
Damsul <gyoung9707@naver.com>
2022-11-01 22:09:03 +0900

    문제 3번 기능 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer;
+        int current;
+        int count = 0;
+
+
+        for (int i = 1; i <= number; i++) {
+            current = i;
+            while (current != 0) {
+                if (current % 10 == 3 || current % 10 == 6 || current % 10 == 9) {
+                    count++;
+                }
+
+                current /= 10;
+            }
+        }
+
+        answer = count;
         return answer;
     }
 }
