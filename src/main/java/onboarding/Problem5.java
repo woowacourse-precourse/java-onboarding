99dd13c255e7f0260744e4fa51b9852089de38a5
minju412 <ln8847@ajou.ac.kr>
2022-11-01 14:34:03 +0900

    style: [p5] 변수명 변경 amountOfMoney -> units

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,13 +10,13 @@ public class Problem5 {
 
 	public static List<Integer> solution(int money) {
 		List<Integer> answer = new ArrayList<>(Collections.emptyList());
-		int[] amountOfMoney = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		int[] units = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
 		if (checkRestrictions(money) == EXCEPTION) {
 			return answer;
 		}
 
-		for (int unit : amountOfMoney) {
+		for (int unit : units) {
 			answer.add(money / unit);
 			money %= unit;
 		}
