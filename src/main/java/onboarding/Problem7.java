2a422b292c2bbc94a53831d847d42565fcc9b6f2
ParkRootSeok <g_e_un@naver.com>
2022-11-01 23:36:27 +0900

    feat : Problem7 - 관련 친구 리스트 만들기 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -13,6 +13,46 @@ public class Problem7 {
 
     private static List<String> makeFriendList(String user, List<List<String>> friends, List<String> visitors) {
 
+        List<String> friendlist = new ArrayList<>();
+        String friend;
+
+        /* 친구 리스트 생성 */
+        for (int i = 0; i < friends.size(); i++) {  // 친구인 유저들과 친구인 유저
+
+            friend = friends.get(i).get(1);
+
+            // 중복 제거 및 자신은 포함시키지 않는다.
+            if (!friendlist.contains(friend) && friend != user) {
+                friendlist.add(friend);
+            }
+
+        }
+
+        for (int i = 0; i < visitors.size(); i++) {  // 내 피드를 조회한 유저
+
+            friend = visitors.get(i);
+
+            // 중복 제거 및 자신은 포함시키지 않는다.
+            if (!friendlist.contains(friend) && friend != user) {
+                friendlist.add(friend);
+            }
+
+        }
+
+        for (int i = 0; i < friends.size(); i++) {  // 친구 관계 목록에서 A에 있는 유저는 이미 친구이므로 제외
+
+            friend = friends.get(i).get(0);
+
+            for (int j = 0; j < friendlist.size(); j++) {
+                if (friendlist.get(j) == friend) {
+                    friendlist.remove(j);
+                }
+            }
+
+        }
+
+        return friendlist;
+
     }
 
     public static HashMap<String, Integer> calculateScore(List<List<String>> friends, List<String> visitors, List<String> friendList) {
