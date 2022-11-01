e0a3badad133b27419b1a01bf0e86015f194c862
pasly0920 <inwoo920621@gmail.com>
2022-11-01 20:36:11 +0900

    feat: 1 전체 사용자 추출

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> friendList = new ArrayList<>();
+
+        createFriendList(user, friends, friendList);
+
         return answer;
+    }
+
+    private static void createFriendList(String user, List<List<String>> friends, List<String> friendList) {
+        for (List<String> friend : friends)
+            if (friend.contains(user))
+                for (String name : friend)
+                    if (!name.equals(user))
+                        friendList.add(name);
     }
 }
