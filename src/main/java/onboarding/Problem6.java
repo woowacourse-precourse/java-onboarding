65bf9a0df0389580be3fa59ed649aa10e6d8ad84
ChaCha3088 <cha3088@gmail.com>
2022-11-01 14:00:47 +0900

    feat: problem6 new function solution

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,20 +4,26 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        List<String> answer = new ArrayList<>();
         for (List<String> e : forms) {
-            Boolean matchResult = isThereMatch(e.get(1));
+            ArrayList<String> cutNameList = cutNickname(e.get(1));
+            for (String cutName : cutNameList) {
+                Boolean matchResult = isThereMatch(cutName);
+                if (matchResult) {
+                    answer.add(e.get(0));
+                }
+            }
         }
-
         return answer;
     }
 
-    private static Boolean isThereMatch(String keyword) {
-        String regex = ".*("+keyword+")+.*";
+    private static Boolean isThereMatch(String cutName) {
+        String regex = ".*("+cutName+")+.*";
         for (List<String> e : forms) {
             System.out.println(e);
             String nickname = e.get(1);
             Boolean result = nickname.matches(regex);
-            if (result == true) {
+            if (result) {
                 return true;
             }
         }
@@ -27,7 +33,8 @@ public class Problem6 {
     private static ArrayList<String> cutNickname(String nickname) {
         ArrayList<String> cutNameList = new ArrayList<> ();
         for (int i = 0; i < nickname.length() - 1; i++) {
-            cutNameList.add(nickname.substring(i, i+2));
+            cutNameList.add(nickname.substring(i, i + 2));
+        }
         return cutNameList;
     }
 }
