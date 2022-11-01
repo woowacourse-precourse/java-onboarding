df4d82de803cafe4cc01d46663300098bd7da0cf
sangeun-b <baek0007@algonquinlive.com>
2022-11-01 20:30:26 +0900

    feat(PROBLEM7): Add feature for finding user's friend's friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -21,6 +21,21 @@ public class Problem7 {
         }
         return friendList;
     }
+    // user의 친구의 친구찾기
+    public static List<String> findLinkedFriends(String user, List<List<String>> friends, List<String> friendList) {
+        List<String> linkedFriends = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            if (friendList.contains(friends.get(i).get(0)) || friendList.contains(friends.get(i).get(1))) {
+                for (String name : friends.get(i)) {
+                    if (!name.equals(user) && !friendList.contains(name)) {
+                        linkedFriends.add(name);
+                    }
+                }
+            }
+        }
+        return linkedFriends;
+    }
+
 
 
 }
