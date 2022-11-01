c6634012d6508aa2742c2df70830fe25dd205918
jw0293 <k38836919@gmail.com>
2022-11-01 09:17:32 +0900

    feature : Trie Algorithm Init [트라이 초기화 기능]

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,24 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+
+class Node{
+    Map<Character, Node> childNode = new HashMap<Character, Node>();
+    boolean endOfWord;
+}
+
+class Trie{
+    Node rootNode = new Node();
+
+    public void insert(String str){
+        Node node = this.rootNode;
+
+        for(int i=0;i<str.length();i++){
+            node = node.childNode.computeIfAbsent(str.charAt(i), key-> new Node());
+        }
+        node.endOfWord = true;
+    }
+}
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
