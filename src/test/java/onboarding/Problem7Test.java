bcdb1323336574828a30c9c64abe33856f485e34
xi_jjun <rlawowns97@naver.com>
2022-11-01 18:18:45 +0900

    test(Problem7): 추천친구 점수를 모든 이름에 대해 초기화 하는 기능 테스트

--- src/test/java/onboarding/Problem7Test.java
+++ src/test/java/onboarding/Problem7Test.java
@@ -1,9 +1,12 @@
 package onboarding;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,6 +16,12 @@ import java.util.Set;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class Problem7Test {
+	@BeforeEach
+	void resetStaticVariables() {
+		Problem7.friendRelationGraph.clear();
+		Problem7.recommendationScores.clear();
+	}
+
 	@DisplayName("친구관계 그래프 생성 테스트")
 	@Test
 	void makeFriendRelationGraphTest() {
@@ -41,4 +50,28 @@ class Problem7Test {
 		assertThat(result).isEqualTo(expected);
 	}
 
+	@DisplayName("모든 친구 이름에 대해 '추천친구 점수' 0으로 초기화")
+	@Test
+	void initializeRecommendationScoresTest() {
+		// given : 친구관계 그래프가 주어졌을 때
+		List<List<String>> friends = List.of(
+				List.of("a", "b"),
+				List.of("b", "c"),
+				List.of("heap", "c"),
+				List.of("pork", "fork")
+		);
+		Problem7.makeFriendRelationGraph(friends);
+
+		// when : 모든 친구에 대해 추천점수를 0점으로 초기화
+		Problem7.initRecommendationScores();
+
+		// then : 모든 value 값이 0 인지 확인하고 map size 가 친구관계 그래프 size 와 같은지 확인
+		Map<String, Integer> result = Problem7.recommendationScores;
+		final int expectedSize = Problem7.friendRelationGraph.size();
+		final int expectedValue = 0;
+
+		assertThat(result.size()).isEqualTo(expectedSize);
+		result.values()
+				.forEach(score -> assertThat(score).isEqualTo(expectedValue));
+	}
 }
