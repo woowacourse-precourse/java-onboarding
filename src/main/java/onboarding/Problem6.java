70693688ec7b4d61f7049f6d50970df95a5bb51c
sujin0607 <goweg0607@gmail.com>
2022-11-01 22:00:28 +0900

    Refact: 수정 문제6
    주석 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,7 +10,6 @@ public class Problem6 {
 
         //주어진 forms 이차원 배열 반복문
         for (List<String> form : forms) {
-
             //첫번째값은 이메일, 두번재값은 인덱스로 지정
             String email = form.get(0);
             String nickname = form.get(1);
@@ -20,23 +19,31 @@ public class Problem6 {
                 String s = nickname.substring(i,i+2);
                 //value값에 들어갈 리스트 선언
                 List<String> values = new ArrayList<>();
-                //
+                //자른 닉네임 대로 HashMap에 맵핑
+                //HashMap에 key값이 있을경우
                 if(words.containsKey(s)){
+                    //기존 values 리스트에 현재값을 추가
                     values = words.get(s);
                     values.add(email);
                     words.put(s,values);
 
+                    //만약 values리스트의 개수가 2개 이상이라면 중복 리스트에 기존 이메일 추가
                     if(words.get(s).size() == 2){
                         answer.add(words.get(s).get(0));
                     }
+                    //현재 이메일 추가
                     answer.add(form.get(0));
+                //HashMap에 key값이 없을경우
                 }else{
+                    //현재 이메일을 value에 넣고 HashMap 업데이트
                     values.add(email);
                     words.put(s,values);
                 }
             }
         }
+        //오름차순으로 정렬
         Collections.sort(answer);
+        //중복 제거
         Set<String> set = new HashSet<String>(answer);
         return answer;
     }
