b0e89bf2a2a85d697f925e9695b075c8aed5285b
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-01 17:21:24 +0900

    comment: P7 기능 목록 주석 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 기능 1: 사용자의 친구 목록을 뽑는 함수 작성
+ * 기능 2: 사용자의 친구 목록을 사용하여 그 친구를 같이 아는 사람들에게 점수를 주는 함수 작성
+ * 기능 3: 방문자에게 점수를 주는 함수 작성
+ * 기능 4-1: 점수를 첫번째 기준으로, 이름을 두번째 기준으로 하여 내림차순으로 정렬하는 함수 작성
+ * 기능 4-2: 이미 친구인 경우와 0점인 경우를 제외하는 함수 작성
+ */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
