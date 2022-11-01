77cf6870f3ad59cec69b526af4f6c11d1dd3ac59
KimSangHyeok <tkdgur0906@naver.com>
2022-11-01 15:50:07 +0900

    Feat(PROBLEM2): 문자를 비교해 연속된 문자 삭제 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,11 +7,21 @@ public class Problem2 {
         String answer = "";
         Stack<Character> st = new Stack<Character>();
         boolean isSame = false;
-
+        
         st.push(cryptogram.charAt(0));
         for(int i = 1;i< cryptogram.length();i++){
+            if(isSame == true && st.peek()!= cryptogram.charAt(i)){
+                st.pop();
+                isSame = false;
+            }
+            if(!st.empty() && st.peek() == cryptogram.charAt(i)){
+                st.pop();
+                isSame = true;
+            }
             st.push(cryptogram.charAt(i));
         }
+        if(isSame == true) st.pop();
+
         return answer;
     }
 }
