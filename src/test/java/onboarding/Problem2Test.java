2a35ee1bcf166ce4d318d1bb00a60b0889751688
chanminmaxscaler <jcm04@naver.com>
2022-11-01 23:24:52 +0900

    test(Problem2Test): Problem2 클래스를 리팩토링하기 위해 테스트 코드를 수정한다.
    Problem2 클래스의 메소드들을 Decoder라는 새로운 클래스에 옮길 예정이다.
    기존에 Problem2 의 인스턴스를 참조해 실행했던 메소드들이 Decoder의 인스턴스를 참조하도로 수정한다.
    
    decodeCryptogramTest() 테스트 코드를 추가한다. decodeCryptogram 메소드는 암호를 해독하는 기능을 수행한다.

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import onboarding.problem2.Decoder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,12 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Problem2Test {
 
-    private Problem2 problem2;
+    private Decoder decoder;
     private String RegularExpressionOfConsecutiveDuplicateCharacters;
 
     @BeforeEach
     public void setUp(){
-        problem2 = new Problem2();
+        decoder = new Decoder();
     }
 
     @Test
@@ -20,22 +21,22 @@ class Problem2Test {
     void isStringContainingRegularExpressionTest() {
         RegularExpressionOfConsecutiveDuplicateCharacters = "(.*)(\\w)\\2{1,}(.*)";
         String falseCase1 = "brown";
-        assertThat(problem2.isStringContainingRegularExpression(falseCase1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
+        assertThat(decoder.isStringContainingRegularExpression(falseCase1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
 
         String falseCase2 = "";
-        assertThat(problem2.isStringContainingRegularExpression(falseCase2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
+        assertThat(decoder.isStringContainingRegularExpression(falseCase2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
 
         String falseCase3 = "ab";
-        assertThat(problem2.isStringContainingRegularExpression(falseCase3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
+        assertThat(decoder.isStringContainingRegularExpression(falseCase3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(false);
 
         String trueCase1 = "browoanoommnaon";
-        assertThat(problem2.isStringContainingRegularExpression(trueCase1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
+        assertThat(decoder.isStringContainingRegularExpression(trueCase1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
 
         String trueCase2 = "ababb";
-        assertThat(problem2.isStringContainingRegularExpression(trueCase2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
+        assertThat(decoder.isStringContainingRegularExpression(trueCase2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
 
         String trueCase3 = "bbabbbbaooo";
-        assertThat(problem2.isStringContainingRegularExpression(trueCase3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
+        assertThat(decoder.isStringContainingRegularExpression(trueCase3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(true);
     }
 
     @Test
@@ -44,14 +45,23 @@ class Problem2Test {
         RegularExpressionOfConsecutiveDuplicateCharacters = "(\\w)\\1{1,}";
         String case1 = "browoanoommnaon";
         String result1 = "browoannaon";
-        assertThat(problem2.deleteStringByRegularExpression(case1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(result1);
+        assertThat(decoder.deleteStringByRegularExpression(case1, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(result1);
 
         String case2 = "aabbb";
         String result2 = "";
-        assertThat(problem2.deleteStringByRegularExpression(case2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(result2);
+        assertThat(decoder.deleteStringByRegularExpression(case2, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(result2);
 
         String case3 = "brown";
         String result3 = "brown";
-        assertThat(problem2.deleteStringByRegularExpression(case3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(result3);
+        assertThat(decoder.deleteStringByRegularExpression(case3, RegularExpressionOfConsecutiveDuplicateCharacters)).isEqualTo(result3);
+    }
+
+    @Test
+    @DisplayName("암호를 잘 해독하는지")
+    void decodeCryptogramTest() {
+        String cryptogram = "browoanoommnaon";
+        String result = "brown";
+
+        assertThat(decoder.decodeCryptogram(cryptogram)).isEqualTo(result);
     }
 }
