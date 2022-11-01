706a6ab3c2ec47a082643668c553f3fdcf81e821
BoyCho <iijokv@naver.com>
2022-11-01 10:53:34 +0900

    feat : 문제1. 예외 처리 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int p1 = pobi.get(0), p2 = pobi.get(1);
+        int c1 = crong.get(0), c2 = crong.get(1);
+
+        if (isException(p1, p2)) return -1;
+        if (isException(c1, c2)) return -1;
+
+        return 0;
+    }
+    static boolean isException (int n, int m) {
+        return m - n != 1;
     }
 }
