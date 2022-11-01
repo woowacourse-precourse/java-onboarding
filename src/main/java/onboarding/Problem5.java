e0892bafba2bb5c73d530e3bbe37f41e81694ef7
장한울 <swingjhw@naver.com>
2022-11-01 19:16:10 +0900

    feat(#문제5) : 화폐단위 별로 출금 가능한지 확인

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        int[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i = 0; i < moneyList.length; i++) {
+            if (moneyList[i] <= money) {
+
+            }
+        }
+
+
         return answer;
     }
+
+
 }
