77f4dddd170c0805559c0693d6098bcd12c76f49
seominseok00 <seominseok4834@gmail.com>
2022-11-01 22:45:15 +0900

    feat(문제1): 유저 클래스 추가

---
+++ src/main/java/onboarding/problem1/User.java
@@ -0,0 +1,11 @@
+package onboarding.problem1;
+
+import java.util.List;
+
+public class User {
+    private List<Integer> pages;
+
+    public User(List<Integer> pages){
+        this.pages = pages;
+    }
+}
