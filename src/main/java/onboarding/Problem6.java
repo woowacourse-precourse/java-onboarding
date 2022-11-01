7aad149cfb04a5a25be9d7164760164bc3b67484
ingpyo <juninpyo72@gmail.com>
2022-11-01 12:34:47 +0900

    [problem6]전체 코드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,7 +9,26 @@ import java.util.Map;
 public class Problem6 {
 	public static List<String> solution(List<List<String>> forms) {
 		checkList(forms);
-
+		List<String> answer = new ArrayList<>();
+		Map<String, Integer> map = new HashMap<>();
+		for (List<String> list : forms) {
+			String id = list.get(1);
+			for (int i = 0; i < id.length() - 1; i++) {
+				String piece = id.substring(i, i + 2);
+				map.put(piece, map.getOrDefault(piece, 0) + 1);
+			}
+		}
+		for (List<String> list : forms) {
+			String email = list.get(0), id = list.get(1);
+			for (int i = 0; i < id.length() - 1; i++) {
+				String piece = id.substring(i, i + 2);
+				if (map.get(piece) >= 2 && !answer.contains(email)) {
+					answer.add(email);
+					break;
+				}
+			}
+		}
+		Collections.sort(answer);
 		return answer;
 	}
 
