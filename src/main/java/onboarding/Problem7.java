f00e91f1e30c35909dd34290fe796be58a19f331
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 18:07:55 +0900

    feat:organize()->user를 제외한 방문자와 친구 목록들을 중복없이 List에 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     private static List<String> memberList = new ArrayList<>();
@@ -13,5 +11,22 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return rankMemberList(user, friends, visitors);
+    }
+
+    public static List<String> orgainze(String user, List<List<String>> friends, List<String> visitors) {
+        memberList.clear();
+        for (int i = 0; i < friends.size(); i++) {
+            for (int j = 0; j < friends.get(i).size(); j++) {
+                memberList.add(friends.get(i).get(j));
+            }
+        }
+        for(int i=0; i<visitors.size(); i++){
+            memberList.add(visitors.get(i));
+        }
+        Set<String> set = new HashSet<String>(memberList);
+        List<String> newlist = new ArrayList<String>(set);
+        newlist.remove(user);
+        Collections.sort(newlist);
+        return newlist;
     }
 }
