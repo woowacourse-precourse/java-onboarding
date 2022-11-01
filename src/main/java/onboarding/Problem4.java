c96229580b4105845e95ef50ce832e4577e4374f
taehyun <didls1643@gmail.com>
2022-11-01 15:50:11 +0900

    4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+	public class reverse {
+		public static String solution(String word) {
+	        String answer = "";
+	        char[] arr = word.toCharArray();
+			for(int i = 0; i < arr.length; i++) {
+				int n = (int) arr[i];
+				if(n > 64 && n < 90) {
+					arr[i] = (char) (26 - (n - 65) + 64);
+				}
+				else if (n > 96 && n < 122){
+					arr[i] = (char) (26 - (n - 97) + 96);
+				}
+			}
+			String result = String.valueOf(arr);
+			
+			answer = result;
+	        return answer;
+	    }
 }
