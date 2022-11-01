9648ad8c92d3423daf76a504b3298cf08f272261
juno-junho <ssmm0205@naver.com>
2022-11-01 20:03:55 +0900

    feat(Problem4): create convertWithDictionary method
    convertWithDictionary 메소드 생성과 problem4 리펙토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,20 +1,10 @@
 package onboarding;
 
+import onboarding.problem4.WordConverter;
+
 public class Problem4 {
     public static String solution(String word) {
-        StringBuilder convertedWord = new StringBuilder(word.length());
-        for (char letter : word.toCharArray()) convertedWord.append(convertWithDictionary(letter));
-        return convertedWord.toString();
-    }
-
-    static char convertWithDictionary(char letter) {
-        final char ALPHABET_UPPERCASE_START = 'A';
-        final char ALPHABET_UPPERCASE_END = 'Z';
-        final char ALPHABET_LOWERCASE_START = 'a';
-        final char ALPHABET_LOWERCASE_END = 'z';
-
-        if (Character.isUpperCase(letter)) return (char) (ALPHABET_UPPERCASE_END - (letter - ALPHABET_UPPERCASE_START));
-        if (Character.isLowerCase(letter)) return (char) (ALPHABET_LOWERCASE_END - (letter - ALPHABET_LOWERCASE_START));
-        return letter;
+        WordConverter wordConverter = new WordConverter();
+        return wordConverter.convertWord(word);
     }
 }
--- src/main/java/onboarding/problem4/WordConverter.java
+++ src/main/java/onboarding/problem4/WordConverter.java
@@ -1,5 +1,8 @@
 package onboarding.problem4;
 
+import static java.lang.Character.*;
+import static onboarding.problem4.Alphabet.*;
+
 public class WordConverter {
 
     public String convertWord(String word) {
@@ -12,7 +15,10 @@ public class WordConverter {
     }
 
     private char convertWithDictionary(char letter) {
-
-
+        if (isUpperCase(letter))
+            return (char) (ALPHABET_UPPERCASE_END.getLetter()- (letter - ALPHABET_UPPERCASE_START.getLetter()));
+        if (isLowerCase(letter))
+            return (char) (ALPHABET_LOWERCASE_END.getLetter() - (letter - ALPHABET_LOWERCASE_START.getLetter()));
+        return letter;
     }
 }
