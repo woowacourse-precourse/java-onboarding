4a02d36e7ce044490443223ea24a522544b570d6
youswim <youswim96@gmail.com>
2022-11-01 21:56:58 +0900

    Feat: 문자를 규칙에 따라 바꾸고, StringBuilder에 append하는 메서드 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,16 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    
+    // 문자를 규칙에 따라 바꾸고, StringBuilder에 append하는 메서드
+    private static void appendChar(StringBuilder output, char c) {
+        if ('A' <= c && c <= 'Z') {
+            output.append((char)('Z' - c + 'A'));
+        } else if ('a' <= c && c <= 'z') {
+            output.append((char)('z' - c + 'a'));
+        } else {
+            output.append(c);
+        }
+    }
 }
