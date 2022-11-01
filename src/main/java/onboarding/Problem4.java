5669a326efb6fce2525018c1ec068c92d1de9614
Eungyu-code <Agyu625@gmail.com>
2022-11-01 13:06:37 +0900

    주어진 소문자의 아스키코드 값을 'z'의 아스키코드 값에서 뺀 후 그 차를 'a'의 아스키코드 값에 더하고, 그에 해당하는 값에 대한 소문자를 리턴하는 changeLowerCase() 함수 구 현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        // 알파벳이 소문자일 경우 반대로 바꿔주는 함수 changeLowerCase()
+        // 알파벳이 대문자일 경우 반대로 바꿔주는 함수 changeUpperCase()
+
+        // answer를 가변적인 클래스인 StringBuffer로 변경
+        StringBuffer answer = new StringBuffer();
+
+        for (int i = 0; i < word.length(); i++) {
+            // 현재 index의 character가 소문자일 경우, changeLowerCase()에서 리턴된 character를 answer에 추가
+            if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') answer.append(changeLowerCase(word.charAt(i)));
+        }
+
+        return answer.toString();
     }
+
+    public static char changeLowerCase(char alphabet) {
+        int start = 97;
+        int end = 122;
+        return (char)(end - (int)alphabet + start);
+    }
+
 }
