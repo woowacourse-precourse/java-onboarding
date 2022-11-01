fcc826c5dc59cdd7cc343baa2e2b2fe0957ffb6f
hongjinwon <herenever@naver.com>
2022-11-01 20:28:44 +0900

    feat: Problem7 getUserFriends, makeFreindsMap, makeRelation 추가
    - Problem7 getUserFriends, makeFreindsMap, makeRelation 추가
    -
    getUserFriends 를 통해서 freinds 를 순회하며 user의 친구 리스트를 반환
    - makeFriendsMap 을
    통해서 user의 friend가 아닌 사람들의 친구 관계 맵 반환
    - makeRelation을 통해서 두 사람의 관계를
    freindsMap에 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,70 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static List<String> getUserFriends(String user, List<List<String>> friends){
+        List<String> userFriends = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if(friend.get(0).equals(user)){
+                userFriends.add(friend.get(0));
+                continue;
+            }
+            userFriends.add(friend.get(1));
+        }
+        return userFriends;
+    }
+
+    public static Map<String,List<String>> makeFriendsMap(String user, List<String> userFriends, List<List<String>> friends){
+        Map<String,List<String>> friendsMap = new HashMap<>();
+
+        for (List<String> friend : friends){
+
+            if(userFriends.contains(friend.get(0)) || userFriends.contains(friend.get(1))) continue;
+
+            makeRelation(friend,friendsMap);
+
+            }
+
+        return friendsMap;
+    }
+
+
+    public static void makeRelation(List<String> friend, Map<String, List<String>> friendsMap){
+
+        List<String> friends;
+
+        if(friendsMap.containsKey(friend.get(0))){
+
+            friends = friendsMap.get(friend.get(0));
+
+        }else{
+
+            friends = new ArrayList<>();
+
+        }
+
+        friends.add(friend.get(1));
+        friendsMap.put(friend.get(0),friends);
+
+        if(friendsMap.containsKey(friend.get(1))){
+
+            friends = friendsMap.get(friend.get(1));
+
+        }else{
+
+            friends = new ArrayList<>();
+
+        }
+
+        friends.add(friend.get(0));
+        friendsMap.put(friend.get(1),friends);
+
+    }
+
 }
