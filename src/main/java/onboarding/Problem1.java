26fd1214dc35a6df08962de92e01de7cf9f1d6bb
la1av1a <zldqkrktqk@gmail.com>
2022-11-01 16:26:21 +0900

    feat : 유효한 좌우 페이지인지 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        try{
+            validate(pobi);
+        } catch(IllegalArgumentException e){
+            return -1;
+        }
+
+        return 0;
     }
+
+    //유효한 좌우 페이지인지 확인
+    public static void validate(List<Integer> pages){
+
+        int leftPage = pages.get(0);
+        int rightPage = pages.get(1);
+
+        if(leftPage+1 == rightPage)
+            return;
+
+        throw new IllegalArgumentException();
+    }
+
 }
