5e487453e3ddb97f413e6e947d51f098a5f78c5f
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-01 19:35:36 +0900

    docs(Problem7): 구현할 기능 목록 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 
+// <기능 목록>
+// 0. class person (String name, int score)
+// 1. 추천 목록 만들기
+//  a. 유저 친구 목록
+//  b. 추천 점수 목록
+//  c. 추천 점수 목록에서 user와 userFreinds 삭제
+//  d 추천 점수 목록 PersonArray로 변환
+// 2. 추천 목록 정렬하기
+
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
