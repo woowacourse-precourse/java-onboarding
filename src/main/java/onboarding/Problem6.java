a45f983e2a6b5f7d4aa761299671a24b6199f3e8
youswim <youswim96@gmail.com>
2022-11-01 21:59:45 +0900

    Feat: KMP 알고리즘을 위한 테이블 생성 메서드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,24 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    // KMP를 위한 테이블 생성 메서드
+    static int[] makeTable(String pattern) {
+        int n = pattern.length();
+        int[] table = new int[n];
+
+        int idx=0;
+        for (int i = 1; i < n; i++) {
+
+            while (idx > 0 && pattern.charAt(i) != pattern.charAt(idx)) {
+                idx = table[idx - 1];
+            }
+
+            if (pattern.charAt(i) == pattern.charAt(idx)) {
+                idx += 1;
+                table[i] = idx;
+            }
+        }
+        return table;
+    }
 }
