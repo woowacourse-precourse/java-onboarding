9c3f98a6863f3d397e34f983fbffa7658f3683cc
we4sley <galaxy138742@gmail.com>
2022-11-01 23:42:10 +0900

    feat: problem5 solution 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static final String MESSAGE = "잘못된 입력입니다.";
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        if (!isInputValid(money)) {
+            throw new IllegalArgumentException(MESSAGE);
+        }
+
+        return getMoneyBag(money);
+    }
+
+    private static boolean isInputValid(int money) {
+        return money > 1 && money < 1000000;
+    }
+
+    private static List<Integer> getMoneyBag(int money) {
+        List<Integer> answer = new ArrayList<>();
+        int[] bank = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < bank.length; i++) {
+            answer.add(money / bank[i]);
+            money %= bank[i];
+        }
         return answer;
     }
 }
