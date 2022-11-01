d702a9e0519b15f1ccaf28bdee8f964e1ccb3808
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 13:37:14 +0900

    feat: Problem3 해당 문자가 3,6,9라는 문자인지 확인하는 기능 구현
    해당 문자가 3,6,9라는 문자인지 확인하고 맞으면 true 아니면 false를 반환하는 check함수를 만들었습니다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+       
         return answer;
+    }
+    public static Boolean check(char c){
+        if (c=='3'){
+            return true;
+        }
+        else if (c=='6'){
+            return true;
+        }
+        else if (c=='9'){
+            return true;
+        }
+        return false;
     }
 }
