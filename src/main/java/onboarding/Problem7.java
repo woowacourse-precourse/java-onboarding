a327f8aaac84fd546f651b9e49b50b9cdd47ed07
Basak <chgy2131@naver.com>
2022-11-01 12:56:51 +0900

    [feat] 7번 - 유저 정보(이름 및 친구목록)를 저장하는 클래스

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,38 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+
+    // 유저의 정보(이름 및 친구목록)를 저장하는 클래스
+    public static class User {
+        private String username;
+        private List<String> friendList;  // 친구 목록
+
+        public User(String username, List<String> friendList) {
+            this.username = username;
+            this.friendList = friendList;
+        }
+
+        // 친구 추가
+        public void addFriend(String friendName) {
+            if(!findFriend(friendName))  // 친구가 아닌경우에만
+                friendList.add(friendName);
+        }
+
+        // 특정 사용자와 친구인지 확인
+        public boolean findFriend(String friendName) {
+            return friendList.contains(friendName);
+        }
+
+        // 친구목록을 반환
+        public List<String> getFriendList() {
+            return friendList;
+        }
+
+        public void printFriendList() {
+            for(String s : friendList) {
+                System.out.println(this.username + ": " + s);
+            }
+        }
+    }
 }
