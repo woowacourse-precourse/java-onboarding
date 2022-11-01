07ca6b97aae2d1bda47e0b906d56d605a0ea0a8f
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 14:43:18 +0900

    feat : 중복 문자를 제거해주는 메서드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,6 +7,23 @@ public class Problem2 {
     private static final int MIN = 1;
     private static Character SameBeforeCharacter;
 
+    public static String deleteOverlapCharacter(String cryptogram) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (!s.isEmpty() && s.peek() == cryptogram.charAt(i)) {
+                s.pop();
+                SameBeforeCharacter = cryptogram.charAt(i);
+                continue;
+            }
 
+            s.push(cryptogram.charAt(i));
+        }
+        if (!s.isEmpty() && s.peek() == SameBeforeCharacter) {
+            s.pop();
+        }
+
+
+        return getChangeCryptogram(s);
+    }
 
 }
