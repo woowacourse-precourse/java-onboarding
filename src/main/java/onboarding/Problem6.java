54ee6b32347792aae0f842606cfed9d9c4496021
오승윤 <>
2022-11-01 15:58:04 +0900

    추가 : Problem6 입력 받은 유저들의 정보를 Map에 저장하는 메서드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    private static final int NAME_IDX = 1;
+    private static final int EMAIL_IDX = 0;
+
+    Map<String,String> usersInfo = new HashMap<>();
+    Map<String, ArrayList<String>> namesMappedToEachSubstringInfo = new HashMap<>();
+
+    Set<String> substringsOfLength2 = new HashSet<>();
+    Set<String> AlerterEmails = new TreeSet<>();
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private void putUsersInfoInMap(List<List<String>> forms){
+        for(List<String> form : forms){
+            String UserName = form.get(NAME_IDX);
+            String UserEmail = form.get(EMAIL_IDX);
+            usersInfo.put(UserName, UserEmail);
+        }
     }
 }
