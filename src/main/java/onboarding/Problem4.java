d8c89077b471dea118eab0524a0fab81250287ee
jsc1014 <wotlr5209@gmail.com>
2022-11-01 18:37:41 +0900

    4번 문제 //  대소문자 구분

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
+
         return answer;
     }
+    public static int checkUpperLower(char letter){
+        if('A' <= letter && letter <= 'Z')
+            return 1;
+        else if('a' <= letter && letter <= 'z')
+            return 2;
+        else
+            return 0;
+    }
+
 }
