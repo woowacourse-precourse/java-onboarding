8f0ed9cc26008e2528228efd1492b838d9b8f4ff
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:34:38 +0900

    Feat: Add 이메일 도메인, 길이 검증 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,13 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static boolean validateEmailCheck(String email) {
+        if (email.endsWith("email.com") &&
+                11 <= email.length() && email.length() < 20) {
+            return true;
+        }
+
+        return false;
+    }
 }
