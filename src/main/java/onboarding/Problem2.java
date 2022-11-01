f551f4371f7ce300b9c5e853ddf9034b12e6f57a
김형기 <khk211113@naver.com>
2022-11-01 23:20:34 +0900

    [FEAT] Problem2 - 중복되는 문자의 위치 정보 찾기 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,13 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    // 중복되는 문자의 위치 정보를 반환 i와 i+1이 중복됨, 없으면 -1
+    public static int getDuplicateIdx(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1))
+                return i;
+        }
+        return -1;
+    }
 }
