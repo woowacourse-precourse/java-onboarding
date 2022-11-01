6d5f3de91a4f3e1d07ca974d13fc2fa8590ceaf7
yeeun <dpdms9621@naver.com>
2022-11-01 16:46:54 +0900

    [Problem4] char배열에 한글자씩 넣는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,13 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        //글자 저장할 stringBuilder
+        StringBuilder sb = new StringBuilder();
+        //char배열에 한글자씩 넣는 기능
+        char[] arr = word.toCharArray();
+
+        String answer = sb.toString();
         return answer;
     }
+
 }
