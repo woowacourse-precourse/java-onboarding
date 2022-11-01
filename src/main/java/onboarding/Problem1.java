852d6b893804b688ce55488c47fe42a91fcacdcc
Tae jeong Son <taejeongson@Taeui-MacBookAir.local>
2022-11-01 17:07:04 +0900

    [Feat] 페이지 범위를 벗어난 값이 주어졌을 때 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,15 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean isException(List<Integer> pobi, List<Integer> crong)
+    {
+        for (Integer page : pobi)
+            if (page < 1 || page > 400)
+                return true;
+        for (Integer page : crong)
+            if (page < 1 || page > 400)
+                return true;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
