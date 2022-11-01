470e3518fb99ea4163dc61eb02ab22ce3eeb19d5
lms0806 <godmlzkf1@naver.com>
2022-11-01 16:51:09 +0900

    기능2 : problem 2 solve

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,37 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return solve(cryptogram);
+    }
+    public static String solve(String s) {
+		if(s == null || s.length() == 0) {
+			return s;
+		}
+		
+		int idx = 0, i;
+		char[] c = s.toCharArray();
+		for(i = 1; i < c.length; i++) {
+			if(c[i] == c[i - 1]) {
+				while(i < c.length && c[i - 1] == c[i]) {
+					i++;
+				}
+			}
+			else {
+				c[idx++] = c[i - 1];
+			}
+		}
+		
+		if(c.length < i) {
+			return "";
+		}
+		
+		c[idx++] = c[i - 1];
+		
+		s = new String(c).substring(0, idx);
+		
+		if(idx != c.length) {
+			return solve(s);
+		}
+    	return s;
     }
 }
