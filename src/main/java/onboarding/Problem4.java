2700404dedb53fc82d84e24b24fe356cd7494f7f
janu01ary <janu01ary@gmail.com>
2022-11-01 16:51:12 +0900

    [Problem4] 대소문자 판별을 함수로 뺌

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,14 +1,26 @@
 package onboarding;
 
 public class Problem4 {
+    private static boolean isUppercase(char c) {
+        if (c >= 'A' && c <= 'Z')
+            return true;
+        return false;
+    }
+
+    private static boolean isLowercase(char c) {
+        if (c >= 'a' && c <= 'z')
+            return true;
+        return false;
+    }
+
     public static String solution(String word) {
         String result = "";
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if (c >= 'A' && c <= 'Z') {
+            if (isUppercase(c)) {
                 result += (char)('Z' - c + 'A');
             }
-            else if (c >= 'a' && c <= 'z') {
+            else if (isLowercase(c)) {
                 result += (char)('z' - c + 'a');
             }
             else {
