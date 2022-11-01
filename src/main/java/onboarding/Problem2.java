bb946a4d2500c64193e9668f0c0819e14898c439
hyunseo <goatsfish@gmail.com>
2022-11-01 14:15:04 +0900

    feat(Problem2) : 암호 문자 삭제 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decodeCryptogram(cryptogram);
     }
+
+    private static String decodeCryptogram(String cryptogram) {
+        StringBuilder cryptogramBuilder = new StringBuilder(cryptogram);
+        int cryptogramLength = decodeCharacters(cryptogramBuilder);
+
+
+        return cryptogramBuilder.toString();
+    }
+
+    private static int decodeCharacters(StringBuilder cryptogramBuilder) {
+        int deletedCount = 0;
+        int cryptogramLength = cryptogramBuilder.length();
+        for (int currIndex = 0; currIndex < findCryptogramLength(cryptogramLength, deletedCount); currIndex++) {
+            deletedCount = deleteRepeatedCharacters();
+        }
+        return cryptogramLength;
+    }
+
+    private static int findCryptogramLength(int cryptogramLength, int deletedCount) {
+        return cryptogramLength - 1 - deletedCount;
+    }
+
 }
