9fba611fa1e6c767d6daa0d98773d362d1f31710
KimSoo <ilppang@naver.com>
2022-11-02 00:34:18 +0900

    Feature(김현수/Problemm7): 친구목록에 따른 점수 증가 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,35 +8,66 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Problem7 {
-    public static List<String>  solution(String user, List<List<String>> friends, List<String> visitors) {
+
+    public static List<String> solution(String user, List<List<String>> friends,
+        List<String> visitors) {
         List<String> answer = Collections.singletonList("aa");
         List<String> userFriendList = getUserList(user, friends);
-        List<String> userList =  getUserList(friends, visitors);
+        List<String> userList = getUserList(friends, visitors);
         Map<String, Integer> userObject = getUserObject(userList);
-
+        Map<String, Integer> userFriendSocreObject = countScore(user, userFriendList, friends, userObject);
 
         return answer;
     }
 
-    public static  Map<String, Integer> getUserObject(List<String> userList){
+    public static Map<String, Integer> getUserObject(List<String> userList) {
         Map<String, Integer> userObject = new HashMap<>();
 
-        for (String mapUser : userList){
+        for (String mapUser : userList) {
             userObject.put(mapUser, 0);
         }
 
         return userObject;
     }
-    public static List<String> getUserList(String user, List<List<String>> friends){
+
+    public static Map<String, Integer> countScore(String user, List<String> userFriendList,
+        List<List<String>> friends, Map<String, Integer> userObject) {
+        List<String> friendsFirendList = new ArrayList<>();
+
+        for (String userFriend : userFriendList) {
+            for (int i = 0; i < friends.size(); i++) {
+                if (friends.get(i).indexOf(userFriend) == 1 && friends.get(i).get(0) != user) {
+                    friendsFirendList.add(friends.get(i).get(0));
+                }
+                if (friends.get(i).indexOf(userFriend) == 0 && friends.get(i).get(1) != user) {
+                    friendsFirendList.add(friends.get(i).get(1));
+                }
+            }
+        }
+        return countFriendScore(userObject, friendsFirendList);
+    }
+
+    public static Map<String, Integer> countFriendScore(Map<String, Integer> userObject, List<String> friendsFriendList) {
+        for (String friendsFriend : friendsFriendList) {
+            if (userObject.containsKey(friendsFriend)) {
+                userObject.put(friendsFriend, userObject.get(friendsFriend) + 10);
+            }
+        }
+
+        return userObject;
+    }
+
+    public static List<String> getUserList(String user, List<List<String>> friends) {
         List<String> userFriendList = new ArrayList<>();
-        for (List<String> friend : friends){
-            if (friend.indexOf(user) == 1){
+        for (List<String> friend : friends) {
+            if (friend.indexOf(user) == 1) {
                 userFriendList.add(friend.get(0));
             }
-            if (friend.indexOf(user) == 0){
+            if (friend.indexOf(user) == 0) {
                 userFriendList.add(friend.get(1));
             }
         }
+
         return userFriendList;
     }
 
@@ -50,6 +81,4 @@ public class Problem7 {
 
         return flattenUserList.stream().distinct().collect(Collectors.toList());
     }
-
-//    public static List<String>
 }
