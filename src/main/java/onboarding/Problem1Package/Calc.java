d0443da7865e7b673f07dea5e0e78f067d97b94c
sooyoungh <sooyoung.h8@gmail.com>
2022-11-01 13:42:06 +0900

    [1번] feat: 두 배열을 하나의 배열로 합치기

--- src/main/java/onboarding/Problem1Package/Calc.java
+++ src/main/java/onboarding/Problem1Package/Calc.java
@@ -2,6 +2,8 @@ package onboarding.Problem1Package;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface Calc {
     // 1. 한 숫자씩 나눠서 하나의 배열로 만들기
@@ -12,5 +14,15 @@ public interface Calc {
             num /= 10;
         }
         return arrNum;
+    }
+
+    // 2. 두 배열(왼쪽/오른쪽 페이지 배열)을 하나의 배열로 합치기
+    default List<Integer> mergeTwoArray(List<Integer> list) {
+        List<Integer> first = getListByInteger(list.get(0));
+        List<Integer> second = getListByInteger(list.get(1));
+
+        List<Integer> result = Stream.concat(first.stream(), second.stream()).collect(Collectors.toList());
+
+        return result;
     }
 }
