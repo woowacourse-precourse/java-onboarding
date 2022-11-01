07c57734ff51c4c8ddb3a225536c05e8ed37fc05
otndus <xuyeon219@gmail.com>
2022-11-01 12:51:02 +0900

    [기능 구현] Problem1 더하기, 곱하기 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,19 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static int Sum(int page){
+        int sum = 0;
+
+        while(page > 0) {sum += page%10; page /= 10;}
+        return sum;
+    }
+    public static int Mul(int page){
+        int sum = 0;
+
+        while(page > 0) {sum *= page%10; page /= 10;}
+        return sum;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
