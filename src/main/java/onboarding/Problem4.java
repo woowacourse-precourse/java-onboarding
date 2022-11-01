b82182b70a3fca10b25af4e3155f5b13258ffc76
idoburnish <doyun7433@naver.com>
2022-11-01 16:46:30 +0900

    feat(PROBLEM4): 문자 변환 기능
    청개구리 사전을  참고하여 문자를 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return reverse(word);
+    }
+
+    public static String reverse(String word) {
+        StringBuilder reverseString = new StringBuilder();
+
+        for (char curChar : word.toCharArray()) {
+            if (curChar >= 65 && curChar <= 90) {
+                reverseString.append((char)(155 - curChar));
+            }
+            if (curChar >= 97 && curChar <= 122) {
+                reverseString.append((char)(219 - curChar));
+            }
+            if (curChar == ' ') reverseString.append(' ');
+        }
+        return reverseString.toString();
     }
 }
