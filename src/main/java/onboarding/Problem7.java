45e06f14d43ce240b1955fd775474a3c0b9d03bf
democracyKim <107744617+juspian@users.noreply.github.com>
2022-11-01 23:39:32 +0900

    pr7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -14,15 +14,12 @@ public class Problem7 {
         String userfriend;
         int score =0;
 
-        // 1. user의 친구 찾기
         for (List<String> friend : friends) {
             if (friend.contains(user)) {
                 friendArr.add(friend.get(0));
             }
         }
 
-
-        // 2. user의 친구의 친구인 경우, 10점을 추가.
         for (int i=0; i < friends.size(); i++) {
             for (String userFriend : friendArr) {
                 if (friends.get(i).contains(userFriend)) {
@@ -40,8 +37,6 @@ public class Problem7 {
             }
         }
 
-
-        // 3. user의 방문자는 1점 추가.
         for (String visitor : visitors) {
             if (!friendArr.contains(visitor)) {
                 if (refArr.containsKey(visitor)) {
@@ -54,8 +49,6 @@ public class Problem7 {
             }
         }
 
-
-        // 4. 추천 점수대로 정렬하되 추천 점수가 같은 경우, 이름순으로 정렬.
         final List<String> valueArr = new ArrayList<>();
         refArr.entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue()
@@ -66,7 +59,6 @@ public class Problem7 {
                 });
         answer = valueArr;
 
-        // 5. 친구 추천은 최대 5명으로 제한.
         if (answer.size() > 5) {
             answer = answer.subList(0,5);
         }
