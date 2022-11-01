6d015d1032d3c7d87b0ea962f95c0ede0f676168
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-01 15:00:51 +0900

    feat: 중복을 체크하는 반복문 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +12,23 @@ public class Problem2 {
 
         String[] array = cryptogram.split("");
         List<String> list = Arrays.asList(array);
+
+        while(check){
+            check = false;
+            List<String> cur_string = new ArrayList<>();
+            cur_string.addAll(list);
+
+            for(int i = 0; list.size() -1 > i; i++){
+                if(list.get(i).equals(list.get(i+1))){
+                    cur_string.remove(i+1);
+                    cur_string.remove(i);
+                    list = cur_string;
+                    check=true;
+                    continue;
+                }
+            }
+            list = cur_string;
+        }
 
         return answer;
     }
