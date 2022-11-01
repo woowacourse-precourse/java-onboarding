de8216e6193543f0f8f88918c4ed1bbb8d25deca
kkyu0718 <net605@naver.com>
2022-11-01 11:59:19 +0900

    [7] 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,58 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Problem7 {
+    public static final int FRIEND_SCORE = 10;
+    public static final int VISIT_SCORE = 1;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        HashMap<String, Integer> score = new HashMap<>();
+        HashMap<String, List<String>> friendsMap = getFriendsMap(friends);
+        HashMap<String, Integer> visitorCount = getVisitorCount(visitors);
+        List<String> userFriends = friendsMap.get(user);
+        for (String userFriend: userFriends){
+            for (String friend: friendsMap.get(userFriend)){
+                if (friend == user){
+                    continue;
+                }
+                score.put(friend, score.getOrDefault(friend, 0) + FRIEND_SCORE);
+            }
+        }
+
+        for(String visitor: visitorCount.keySet()) {
+            if (userFriends.contains(visitor)){
+                continue;
+            }
+            score.put(visitor, score.getOrDefault(visitor, 0) + visitorCount.get(visitor) * VISIT_SCORE);
+        }
+
+        List<String> answer = new ArrayList<>(score.keySet());
+        Collections.sort(answer);
+        Collections.sort(answer, (o1, o2) -> (score.get(o2).compareTo(score.get(o1))));
+
+        if (answer.size() > 5) {
+            answer = answer.subList(0, 5);
+        }
         return answer;
     }
+
+    public static HashMap<String, List<String>> getFriendsMap(List<List<String>> friends) {
+        HashMap<String, List<String>> hashMap = new HashMap<>();
+        for (List<String> f: friends ) {
+            hashMap.computeIfAbsent(f.get(0), s -> new ArrayList<>()).add(f.get(1));
+            hashMap.computeIfAbsent(f.get(1), s -> new ArrayList<>()).add(f.get(0));
+        }
+        return hashMap;
+    }
+
+    public static HashMap<String, Integer> getVisitorCount(List<String> visitors) {
+        HashMap <String, Integer> hashMap = new HashMap<>();
+        for (String v : visitors) {
+            hashMap.computeIfAbsent(v, s -> 0);
+            hashMap.put(v, hashMap.get(v) + 1);
+        }
+        return hashMap;
+    }
+
 }
