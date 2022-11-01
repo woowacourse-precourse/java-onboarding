766c25a6a89a76103cce7f73beb89c38b0a5eb1e
Yh0sun <hy770819@naver.com>
2022-11-01 15:30:20 +0900

    problem4:func1 add

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -8,7 +8,9 @@ public class Problem4 {
         //  2. 알파벳이 소문자이면(아스키 코드 97~121) 122-(알파벳-97)
         //  3. 알파벳이 대문자이면(아스키 코드 65~90) 90-(알파벳-65)
         //  4. 공백일 때는 answer에 공백 넣어주고 continue
+        for(int i=0;i<word.length();i++) {
 
+        }
         return answer;
     }
 }
