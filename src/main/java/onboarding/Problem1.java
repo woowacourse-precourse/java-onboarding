b960a151f48341f9508a55dba1dd50d2a6eec24c
Tae jeong Son <taejeongson@Taeui-MacBookAir.local>
2022-11-01 17:11:17 +0900

    [Feat] 페이지 값으로 2개가 들어오지 않았을 때 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,12 +5,15 @@ import java.util.List;
 class Problem1 {
     public static boolean isException(List<Integer> pobi, List<Integer> crong)
     {
+        if (pobi.size() != 2 || crong.size() != 2)
+            return true;
         for (Integer page : pobi)
             if (page < 1 || page > 400)
                 return true;
         for (Integer page : crong)
             if (page < 1 || page > 400)
                 return true;
+        return false;
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
