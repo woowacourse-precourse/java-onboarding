211fc2564d937c2bae3dd683dea4a598ec14eab9
kmw2378 <rlarla677@gmail.com>
2022-11-01 21:49:32 +0900

    문제2 수정 연속적으로 중복된 문자가 3번 이상 있는 경우를 고려했습니다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,7 +12,19 @@ public class Problem2 {
 
                 if (cryptogram.substring(i, i + 1).equals(cryptogram.substring(i + 1, i + 2))) {
 
-                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + 2);
+                    String substring = cryptogram.substring(i, i + 1);
+                    int j = i + 1;
+
+                    while (j < cryptogram.length() - 1) {
+
+                        if (cryptogram.substring(j + 1, j + 2).equals(substring)) {
+                            j++;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(j + 1);
                     isOverlap = true;
                     break;
                 }
