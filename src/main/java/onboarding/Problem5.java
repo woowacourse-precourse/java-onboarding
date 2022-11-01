b4a47a0740b22dc6bd104649480568c2dbda4900
Choi-JY <Choi-JY1107@github.com>
2022-11-01 15:16:34 +0900

    refactor: 돈 계산하는 메소드 분리

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] div = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>();
-        int[] div = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-
-        for(int i: div){
-            answer.add(money/i);
-            money%=i;
-        }
-
+        List<Integer> answer = caculateBills(money);
         return answer;
+    }
+
+    private static List<Integer> caculateBills(int money) {
+        List<Integer> wallet = new ArrayList<>();
+        for(int i: div){
+            wallet.add(money / i);
+            money %= i;
+        }
+        return wallet;
     }
 }
