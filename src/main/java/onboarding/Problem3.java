b568a3ca95ffb501f79041897f3a4bdbf0cfc4c9
JungYoonshin <sju08227@naver.com>
2022-11-01 11:25:00 +0900

    feat: #problem3 -> 1~number를 하나씩 증가시키고 총 횟수를 저장하는 함수 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,18 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+        answer = play365Game(number);
 
         return answer;
+    }
+
+    private static int play365Game(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            String str = "" + i;
+            sum += checkNumberin365(str);
+        }
+        return sum;
     }
 
     private static int checkNumberin365(String str) {
