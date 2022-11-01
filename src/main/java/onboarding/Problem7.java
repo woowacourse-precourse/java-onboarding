03284294940edb9ea30f73d40d15986012288b64
장한울 <swingjhw@naver.com>
2022-11-01 20:46:46 +0900

    feat(#문제7) : 사람 별로 점수 정리(hashMap)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,11 +9,16 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
         HashMap<String, List<String>> relation = new HashMap<>();
-
+        HashMap<String, Integer> scoreMap = new HashMap<String, Integer>();
         for (int i = 0; i < friends.size(); i++) {
 
             List<String> friend = friends.get(i);
+            // 친구 관계 정리
             connectfriend(friend, relation);
+
+            // 사람 별로 점수 정리 Map 생성
+            makescoremap(friend, scoreMap);
+
         }
 
         List<String> answer = Collections.emptyList();
@@ -37,6 +42,21 @@ public class Problem7 {
             relation.put(friend2, new ArrayList<>());
             relation.get(friend2).add(friend1);
         }
+
+    }
+
+    // 사람 별로 점수 정리해줄 hashMap 생성
+    public static void makescoremap(List<String> friend, HashMap<String, Integer> scoreMap) {
+        String friend1 = friend.get(0);
+        String friend2 = friend.get(1);
+
+        if (!scoreMap.containsKey(friend1)) {
+            scoreMap.put(friend1, 0);
+        }
+        if (!scoreMap.containsKey(friend2)) {
+            scoreMap.put(friend2, 0);
+        }
+
 
     }
 
