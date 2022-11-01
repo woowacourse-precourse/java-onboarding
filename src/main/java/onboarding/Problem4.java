6dd4c77d1743e339262408ff7f96d569b59b23ec
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 14:55:49 +0900

    feat : 규칙에 맞게 알파벳을 변환해주는 메서드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,5 +4,9 @@ public class Problem4 {
     private static final int MIN = 1;
     private static final int MAX = 1000;
 
-
+    public static Character changeAlphabet(Character alpha) {
+        if (alpha >= 'A' && alpha <= 'Z') return (char) ('A' + 'Z' - alpha);
+        if (alpha >= 'a' && alpha <= 'z') return (char) ('a' + 'z' - alpha);
+        return (char) (alpha);
+    }
 }
