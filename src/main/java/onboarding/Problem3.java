10bc10d77b7b75d6ff29bde67a86e2a9571eb4d4
JUHA <khabh@naver.com>
2022-11-01 21:48:24 +0900

    Feature(Problem3) : ThreeSixNineGame 생성, 현재 자릿수에 해당되는 박수 계산하는 기능 구현(getCurrentTotal)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+	private static class ThreeSixNineGame {
+		ThreeSixNineGame() {}
+
+		public static int getCurrentTotal(int currentNumber,int currentUnit, int beforeUnitTotal, int preDigit) {
+			return (currentNumber) * beforeUnitTotal + ((currentNumber - 1) / 3) * currentUnit + (currentNumber % 3 == 0 ? (preDigit + 1) : 0 );
+		}
+	}
 }
