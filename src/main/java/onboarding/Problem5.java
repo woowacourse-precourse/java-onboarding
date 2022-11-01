91416a96bf7db271af9227110d4541ca37ea8443
junyong <jymoon1121@gmail.com>
2022-11-01 16:07:24 +0900

    [5번]기능 요구사항 1,2번 기능 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+/**
+ * 기능 요구 사항
+ * 1. 큰 금액의 화폐 위주로 받는다.
+ * 2. 돈의 액수 money 각 몇개로 반환되는지 solution 메서드 완성
+ */
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        int[] result = calculateMoney(money);
         return answer;
+    }
+    public static int[] calculateMoney(int money) {
+        int[] wont = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] result = new int[9];
+        while(money != 0) {
+            for(int i = 0; i < wont.length; i++) {
+                if(money < wont[i])
+                    continue;
+                result[i] += (money / wont[i]);
+                money = money % wont[i];
+            }
+        }
+        return result;
     }
 }
