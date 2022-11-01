656dcc7b021c262cd472e000fca07d726ee62de0
silver <silver36c@gmail.com>
2022-11-01 22:07:12 +0900

    feat: Problem3 자리수별로 숫자 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    // 자리수별로 숫자 분리
+    private static List<Integer> separateNumber(int num){
+        List<Integer> separatedNumbers = new ArrayList<>();
+        final int NOTATION = 10;
+
+        while (num >= NOTATION){
+            separatedNumbers.add(num % NOTATION);
+            num /= NOTATION;
+        }
+        separatedNumbers.add(num % NOTATION);
+        return separatedNumbers;
+    }
+
 }
