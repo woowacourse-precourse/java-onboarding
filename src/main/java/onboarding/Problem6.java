a167c059e99879bfbfe9ec233f1e07bf49155a8e
권민정 <22alswjd@naver.com>
2022-11-01 12:02:49 +0900

    feat: Problem6 기능 '2. 연속된 같은 글자가 있는 크루들을 찾아 이메일을 answerSet에 넣음' 완료
    - answer에 들어갈 이메일들을 중복없이 담기 위해 answerSet을 만드는 함수 makeAnswerSet 구현
    - 두 사람이 2개 이상의 연속되는 공통 문자를 가졌는지 토큰을 비교하여 검사하는 hasCommonToken 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,6 +6,7 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         Map<String, Set<String>> words;
+        Set<String> answerSet; // 알림이 필요한 크루들의 이메일을 저장
 
         /*
         1. 크루들 닉네임을 2글자씩 분리
@@ -19,6 +20,7 @@ public class Problem6 {
         - 두 사람 모두 이미 answerSet에 있다면 재비교 하지 않음
         - 두 크루의 닉네임에 연속된 같은 글자가 있는지 확인
          */
+        answerSet = makeAnswerSet(forms, words);
 
         /*
         3. answer에 담아 오름차순 정렬
@@ -39,5 +41,37 @@ public class Problem6 {
                 words.get(email).add(name.substring(i, i + 2));
         }
         return words;
+    }
+
+    private static Set<String> makeAnswerSet(List<List<String>> forms,
+                                                 Map<String, Set<String>> words) {
+        Set<String> answerSet = new HashSet<>();
+
+        for(int i = 0; i < forms.size() - 1; i++) {
+            for(int j = i + 1; j < forms.size(); j++) {
+                String firstPersonEmail = forms.get(i).get(0);
+                String secondPersonEmail = forms.get(j).get(0);
+
+                if(answerSet.contains(firstPersonEmail) && answerSet.contains(secondPersonEmail))
+                    continue;
+
+                if(hasCommonToken(words.get(firstPersonEmail), words.get(secondPersonEmail))) {
+                    answerSet.add(firstPersonEmail);
+                    answerSet.add(secondPersonEmail);
+                }
+            }
+        }
+
+        return answerSet;
+    }
+
+    private static boolean hasCommonToken(Set<String> set1, Set<String> set2) {
+        for(String token1 : set1) {
+            for(String token2 : set2) {
+                if(token1.equals(token2))
+                    return true;
+            }
+        }
+        return false;
     }
 }
