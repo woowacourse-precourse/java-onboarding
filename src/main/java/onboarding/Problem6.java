9f3c7fceae784fe9df570217beda819230903795
Sieun Park <psieun0606@gmail.com>
2022-11-01 16:58:13 +0900

    feat: problem6 이메일을 찾는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,23 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    static String getEmail(List<List<String>> forms, int i) {
+
+        List<String> formOne = forms.get(i);
+        String email = formOne.get(0);
+
+        return email;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        for (int i = 0; i < forms.size(); i++) {
+            String email = getEmail(forms, i);
+        }
+
         return answer;
     }
+
 }
