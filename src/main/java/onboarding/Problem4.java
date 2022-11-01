f7fe5af874c65494d346947e673b353e2843a2c7
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:48:58 +0900

    feat(problem4): word 길이 검증

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        validate(word);
+        return "";
+    }
+
+    private static void validate(String word) {
+        int length = word.length();
+        if (length < 1 || length > 1000) {
+            throw new IllegalArgumentException("word는 길이가 1 이상 1,000 이하인 문자열이어야 합니다.");
+        }
     }
 }
