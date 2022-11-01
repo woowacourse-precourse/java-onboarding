89a2a3e8fc5649665df2d42b59e2552e6cff2944
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-01 13:16:13 +0900

    feat(Problem6): 이메일 정렬 및 중복 제거 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 // <기능 목록>
 // <기능 목록>
@@ -28,14 +29,15 @@ public class Problem6 {
         // 2. 중복 닉네임을 가진 이메일 찾기
         answer = findOverlapNickname(forms);
 
+        // 3. 오름차순으로 이메일 정렬 & 중복 제거
+        answer = emailSortAndDeduplication(answer);
+
         return answer;
     }
 
     // 1. 닉네임 두글자씩 잘라서 Treeset에 저장
     public static void addAllTwoLettersInTreeSet(List<List<String>> forms) {
         for (int i = 0; i < forms.size(); i++) {
-            System.out.println(i + ": " + forms.get(i).get(1));
-
             // 1-a. 닉네임 두글자씩 자름 String[] cutNickname
             String[] cutNickname = cutNicknameByTwoLetters(forms.get(i).get(1)); // nickname
 
@@ -66,13 +68,12 @@ public class Problem6 {
             }
 
             // 1-c. 저장되어 있는게 맞다면 twoLetter.overlap=true로 설정
-            twoLetter tmp = twoLetterTreeSet.ceiling(new twoLetter(cutNickname[i], false));
-            System.out.println(tmp);
-            if (tmp == null) {
+            twoLetter ceilingLetter = twoLetterTreeSet.ceiling(new twoLetter(cutNickname[i], false));
+            if (ceilingLetter == null) {
                 twoLetterTreeSet.add(new twoLetter(cutNickname[i], false));
-            } else if (cutNickname[i].equals(tmp.letter)) {
+            } else if (cutNickname[i].equals(ceilingLetter.letter)) {
                 // overlap==true일때는 다시 저장할 필요 없음
-                if (!tmp.overlap) {
+                if (!ceilingLetter.overlap) {
                     twoLetterTreeSet.remove(new twoLetter(cutNickname[i], false));
                     twoLetterTreeSet.add(new twoLetter(cutNickname[i], true));
                 }
@@ -80,12 +81,11 @@ public class Problem6 {
                 twoLetterTreeSet.add(new twoLetter(cutNickname[i], false));
             }
         }
-        System.out.println("end");
     }
 
     // 2. 중복 닉네임을 가진 이메일 찾기
     public static List<String> findOverlapNickname(List<List<String>> forms) {
-        List<String> email = new LinkedList<>();
+        List<String> email = new ArrayList<>();
         for (int i = 0; i < forms.size(); i++) {
             // 2-a. 닉네임 두글자씩 자름 String[] cutNickname
             String[] cutNickname = cutNicknameByTwoLetters(forms.get(i).get(1)); // nickname
@@ -101,12 +101,33 @@ public class Problem6 {
     // 2-b. cutNickname의 overlap이 true인지 확인
     public static boolean isOverlapNickname(String[] cutNickname) {
         for (int i = 0; i < cutNickname.length; i++) {
-            twoLetter tmp = twoLetterTreeSet.ceiling(new twoLetter(cutNickname[i], true));
-            if (tmp.overlap) {
+            twoLetter ceilingLetter = twoLetterTreeSet.ceiling(new twoLetter(cutNickname[i], true));
+            if (ceilingLetter.overlap) {
                 return true;
             }
         }
         return false;
+    }
+
+    // 3. 오름차순으로 이메일 정렬 & 중복 제거
+    public static List<String> emailSortAndDeduplication(List<String> email) {
+        TreeSet<String> emailTreeSet = new TreeSet<String>(email);
+        List<String> resultEmail = new ArrayList<String>(emailTreeSet);
+        return resultEmail;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> forms = List.of(
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "차이차이차이문"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠"));
+        List<String> answer = solution(forms);
+
+        for (int i = 0; i < answer.size(); i++) {
+            System.out.println(answer.get(i));
+        }
     }
 
     // 0. class twoLetter(string letter, boolean overlap)
