df9de4172762bbabe42aa6fe128363b4f7b505a5
simjaeseo <delpho@naver.com>
2022-11-01 22:46:14 +0900

    feat: 문제 6 기능 목록

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,6 +2,11 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 중복 비교를 위한 form과 해당 닉네임에 포함된 두글자 모음 리스트 구현
+ * 2. 다른 form을 탐색하면서 중복된 닉네임에 해당하는 이메일 추가
+ */
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
