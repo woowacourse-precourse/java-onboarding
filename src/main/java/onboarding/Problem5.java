bb36cdb6927921375b99e8caba02f6d54c8d0300
Nugur1i <duckhee092@naver.com>
2022-11-01 21:44:16 +0900

    docs<problem5> : Add Remark

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,12 +9,11 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
-        List<Integer> moneyUnitArray = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
+        List<Integer> moneyUnitArray = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1)); //화폐 단위 배열 만들기
 
+        int numberOfKindOfMoney = moneyUnitArray.size(); //화폐 단위 개수구하기
 
-        int numberOfKindOfMoney = moneyUnitArray.size(); //화폐 단위 개수는 총 9개이다.
-
-        for (int i = 0; i < numberOfKindOfMoney ; i++) {
+        for (int i = 0; i < numberOfKindOfMoney ; i++) { // 50000원권 부터 순서대로 나눠주면서 answer 배열에 저장하기
             answer.add(money/moneyUnitArray.get(i));
             money %= moneyUnitArray.get(i);
         }
