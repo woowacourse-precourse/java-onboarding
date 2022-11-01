55def1b17c5eadce693bc31628e4511d79cdd542
metorg <metrog950@gmail.com>
2022-11-01 22:13:54 +0900

    feat: [Problem3] 숫자 순회 메서드 생성 1부터 주어진 숫자까지 순차적으로 순회

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        numberRound(number);
         return answer;
+    }
+
+    public static int numberRound(int number){
+        int result =0;
+        for(int i=1;i<=number;i++){
+            
+        }
+        return result;
     }
 }
