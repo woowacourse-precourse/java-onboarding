509318804ce6a9f04c54f29d8f0138e06fc7c91d
Lee-SungMin <saevers26@gmail.com>
2022-11-01 20:38:19 +0900

    Add: 4번 문제 기능 구현 - 전체적인 로직 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -12,6 +12,35 @@ public class Problem4 {
     }
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); i++){
+
+            // 공백일 경우
+            if(word.charAt(i) == ' '){
+                answer += ' ';
+            }
+
+            // 공백 아닌 경우(알파벳인 경우)
+            else if(word.charAt(i) != ' '){
+
+                // 아스키 값 저장
+                int temp = 0;
+
+                // 대문자일 경우
+                if(word.charAt(i) <= 'Z'){
+                    temp = changeUpper(word.charAt(i));
+                }
+
+                // 소문자일 경우
+                else if(word.charAt(i) <= 'z'){
+                    temp = changeLower(word.charAt(i));
+                }
+
+                answer += (char)temp;
+            }
+        }
+
+        // 정답
         return answer;
     }
 }
