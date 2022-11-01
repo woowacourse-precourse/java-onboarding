635f5a514c112ea7745ee4140cfa0ec2958f0b44
mydreamis-18 <hitoryok@naver.com>
2022-11-01 17:17:17 +0900

    3, 4, 5번 문제 테스트 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,27 +1,31 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-
+	
 	public static List<Integer> solution(int money) {
 
-		List<Integer> answer = Collections.emptyList();
-
 		int[] monetaryUnits = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1 };
+		
+		// ㅜ 불변 객체인 듯...
+		List<Integer> answer = Collections.emptyList();
+		List<Integer> countList = new ArrayList<Integer> (monetaryUnits.length);
 
-		for (int won : monetaryUnits) {
+		for (int i = 0; i < monetaryUnits.length; i++) {
 
-			int quotient = getQuotient(money, won);
+			int quotient = getQuotient(money, monetaryUnits[i]);
 			if (quotient >= 1) {
 
-				money = money % won;
+				money = money % monetaryUnits[i];
 			}
 
-			answer.add(quotient);
+			countList.add(i, quotient);
 		}
 
+		answer = countList;
 		return answer;
 	}
 
