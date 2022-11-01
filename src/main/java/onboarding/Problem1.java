6c0249a6b1916c2847e1fab623ca2b45dc7b79ba
tdmdgh <tdmdgh@gmail.com>
2022-11-01 18:27:53 +0900

    problem1_1_getMaxScores_매소드_구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static int getMaxScores(int n){
+        int tmp_add=0;
+        int tmp_mul=1;
+        while(n!=0){
+            tmp_add += n%10;
+            tmp_mul *= n%10;
+            n /= 10;
+        }
+        return Math.max(tmp_add,tmp_mul);
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
