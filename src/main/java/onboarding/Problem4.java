cdfbbf61dea631d4ad79cdeef3c111efca3dd219
Shingiwon <65998782+Shingiwon@users.noreply.github.com>
2022-11-01 22:50:50 +0900

    4번 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        
+        for(int i = 0; i < word.length(); i++) {
+        	char alpa = word.charAt(i);
+        	if(alpa >= 65 && alpa <= 90) {
+        		answer +=(char)(90-(alpa-65));
+        	}else if(alpa >= 97 && alpa <= 122) {
+        		answer += (char)(122-(alpa-97));
+        	}else {
+        		answer +=" ";
+        	}
+        	
+        }
         return answer;
     }
 }
