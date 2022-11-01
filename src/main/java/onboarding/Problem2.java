109022a28b6f601d535d6c205acbffca93742c91
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-01 10:39:11 +0900

    chore : Problem 2 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,7 +4,7 @@ import java.util.Stack; //import
 public class Problem2 {
     public static String stackReverse (Stack<Character> stack)
     {
-
+        // 스택을 뒤집어 String 으로 반환하는 함수.
         String  answer ="";
         while (!stack.isEmpty()){
             answer += stack.pop();
@@ -18,11 +18,13 @@ public class Problem2 {
         Stack<Character> stack = new Stack<>();
         for (int i = 0 ; i< cryptogram.length(); i++)
         {
+            // stack 이 비어있을 때 알파벳 삽입
             if (stack.isEmpty())
                 stack.push(cryptogram.charAt(i));
             else {
                 if (stack.peek() == cryptogram.charAt(i))
                 {
+                    // 알파벳 중복되면 삽입하지않는다.
                     while (i < cryptogram.length() && stack.peek() == cryptogram.charAt(i))
                         i++;
                     stack.pop();
