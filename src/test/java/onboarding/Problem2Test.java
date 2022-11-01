8baac049922ab0a0e6a51e55a62ca6317eb6a7ee
saehim <saeilee.vxv@gmail.com>
2022-11-01 17:40:49 +0900

    Test(Problem2): 중복 체크 알고리즘 변경 테스트

--- src/test/java/onboarding/Problem2Test.java
+++ src/test/java/onboarding/Problem2Test.java
@@ -26,27 +26,21 @@ class Problem2Test {
     }
 
     @Test
-    void 중복문자_제거() {
-        assertThat(replace(input)).isEqualTo("browoannaon");
+    void 중복된반복문자체크() {
+        boolean[] result = {false,false,true,true,true,true,false,false};
+        char[] input = {'b','a','n','n','a','a','n','a'};
+        assertThat(checkJungbok(input)).isEqualTo(result);
     }
 
-    private String replace(String input) {
-        List<Integer> delete = deleteIdx(input);
-        char[] chars = input.toCharArray();
-        for (int i : delete) {
-            String temp = Character.toString(chars[i])+Character.toString(chars[i]);
-            input = input.replace(temp,"");
+    private boolean[] checkJungbok(char[] input) {
+        boolean[] result = new boolean[input.length];
+        for (int i=1; i<input.length; i++){
+            if (input[i-1]==input[i]) {
+                result[i-1] = true;
+                result[i] = true;
+            }
         }
-        return input;
+        return result;
     }
 
-    // 삭제할 문자 인덱스 반환
-    private List<Integer> deleteIdx(String input){
-        char[] chars = input.toCharArray();
-        List<Integer> delete = new ArrayList<>();
-        for (int i=1; i<chars.length; i++){
-            if (chars[i-1]==chars[i]) delete.add(i);
-        }
-        return delete;
-    }
 }
