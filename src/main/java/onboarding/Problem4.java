d110abb3c9e1c0a3690e87835aab4d80198cd99c
ye1ne <yewon7036@naver.com>
2022-11-01 16:52:06 +0900

    feature : 문제를 풀기 위한 메인 메소드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,13 +1,33 @@
 package onboarding;
 
+import static java.lang.Character.isLowerCase;
+
 /**
  * 문제 풀이 방식 & 기능 목록서는
  * docs/PROBLEM4.md 의 하단에 기술되어 있습니다
  */
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<word.length(); i++){
+            sb.append(convertChar(word.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    private static char convertChar(char character) {
+        if(isLowerCase(character)){
+            return convertLowerCase(character);
+        }
+        return convertUpperCase(character);
+    }
+
+    private static char convertUpperCase(char character) {
+        return 0;
+    }
+
+    private static char convertLowerCase(char character) {
+        return 0;
     }
 
 
