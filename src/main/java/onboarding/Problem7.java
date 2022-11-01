a452bbd72440ba5e13ba47978ed1bfbc54bb7d61
rhqudco <rhqudco1204@naver.com>
2022-11-01 12:53:23 +0900

    DOCS : Requirements analysis

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
+/*
+* 1. user의 친구 목록을 구해야 한다. (이미 친구면 추천할 이유가 없기 때문)
+* 2. 목록을 통해 친구의 친구인 원소를 찾아 가중치를 부여한다.
+* 3. 방문자 리스트를 통해 가중치를 부여한다.
+* 4. 사이즈가 5인 친구 추천 리스트를 반환한다.
+* */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
