b64d6b7f3d99dbd0e71a346432787535db6e487c
bnbnac <bnbnac@naver.com>
2022-11-01 21:43:54 +0900

    feat: (P3)ArrayList에 포함된 3, 6, 9의 개수를 반환하는 메소드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,17 +3,37 @@ package onboarding;
 import java.util.ArrayList;
 
 public class Problem3 {
+    static ArrayList<Integer> CLAP = new ArrayList<>();
+    CLAP.add(3);
+    CLAP.add(6);
+    CLAP.add(9);
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
     }
 
-    public ArrayList<Integer> digitList(int num) {
-        ArrayList<Integer> digits = new ArrayList<>();
+    private static ArrayList<Integer> digitList(int num) {
+        ArrayList<Integer> digitList = new ArrayList<>();
         while (num > 0) {
-            digits.add(num % 10);
+            digitList.add(num % 10);
             num /= 10;
         }
-        return digits;
+        return digitList;
+    }
+
+    private static int findNumOfClap(ArrayList<Integer> digitList) {
+        int clap = 0;
+        for (Integer num : digitList) {
+            clap += isClap(num);
+        }
+        return clap;
+    }
+
+    private static Integer isClap(Integer num) {
+        if (CLAP.contains(num)) {
+            return 1;
+        }
+        return 0;
     }
 }
