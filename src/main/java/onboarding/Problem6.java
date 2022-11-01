5de289835d1465213c27f8291658e28af675814b
DonghyunShin <shindh0429@gmail.com>
2022-11-01 23:34:54 +0900

    [CREATE] Crew의 크기 유효성 검사 기능 구현 완료

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        if (!isValidCrewSize(forms.size())) {
+            return Collections.emptyList();
+        }
+
         return answer;
     }
+
+    private static boolean isValidCrewSize(int size) {
+        return size >= 1 && size <= 10000;
+    }
+
 }
