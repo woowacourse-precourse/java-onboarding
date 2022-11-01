0da309fffcb1d20f2181c48d2a6cb4c3cc93b121
Woong99 <dndry1072@naver.com>
2022-11-01 13:51:41 +0900

    feat: 문제 7 - 최종 점수를 정렬하는 메소드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -59,5 +60,23 @@ public class Problem7 {
         getFriendsCount(friends, userFriendsList, user, score).forEach((key, value) -> score.merge(key, value, Integer::sum));
         getVisitedCount(visitors, userFriendsList, score).forEach((key, value) -> score.merge(key, value, Integer::sum));
         return score;
+    }
+
+    private static List<String> getSortedScore(HashMap<String, Integer> score) {
+        List<String> sortedScoreList = new ArrayList<>();
+
+        List<Map.Entry<String, Integer>> entries =
+                score.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .collect(Collectors.toList());
+
+        for (Map.Entry<String, Integer> entry : entries) {
+            sortedScoreList.add(entry.getKey());
+        }
+
+        if (sortedScoreList.size() > 5) {
+            sortedScoreList = sortedScoreList.subList(0, 5);
+        }
+        return sortedScoreList;
     }
 }
