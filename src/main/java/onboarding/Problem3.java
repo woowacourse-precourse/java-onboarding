6990d861ba1b9aafe35d5d53935f4151240c9b4a
dener8 <8dener@gmail.com>
2022-11-01 23:09:28 +0900

    [Problem3] feat: 박수 세는 메서드 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+
+import static java.util.Arrays.*;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        answer = countClap(number);
         return answer;
+    }
+
+    static int countClap(int number) {
+        String threeSixNine = "369";
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            String num = Integer.toString(i);
+            for (int j = 0; j < num.length(); j++) {
+                if (threeSixNine.contains(Character.toString(num.charAt(j)))) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
