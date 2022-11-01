eb4afd91148fd5a0a6597cb29e9d4b2db94d460d
jay <jaewhan232@naver.com>
2022-11-01 21:29:14 +0900

    Implement : key=유저명, value = 친구의 리스트로 구성된 relationMap을 반환하는 메소드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static HashMap<String, List<String>> getRelationMap(List<List<String>> friends) {
+        HashMap<String, List<String>> relationMap = new HashMap<String, List<String>>();
+
+        for (List<String> relation : friends) {
+            String user1 = relation.get(0);
+            String user2 = relation.get(1);
+
+            if (!relationMap.containsKey(user1)) relationMap.put(user1, new ArrayList<>());
+            if (!relationMap.containsKey(user2)) relationMap.put(user2, new ArrayList<>());
+
+            relationMap.get(user1).add(user2);
+            relationMap.get(user2).add(user1);
+        }
+        return relationMap;
     }
 }
