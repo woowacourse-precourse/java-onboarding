9c93f6fc9a9ca6c4e4988d559bf8319ec9f8733b
gksruf2848 <4455qqq@naver.com>
2022-11-01 21:40:24 +0900

    Problem2 기능1. 각 자리 수의 3, 6, 9 갯수를 반환하는 countClap() 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    // 1. 각 자리 수의 3, 6, 9 갯수를 반환하는 함수
+    static int countClap(int num) {
+        int cnt = 0;
+
+        while(num > 0) {
+            int digit = num % 10;
+            if(digit % 3 == 0 && digit != 0) {
+                cnt++;
+            }
+            num /= 10;
+        }
+
+        return cnt;
     }
 }
