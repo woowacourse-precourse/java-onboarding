32170daf5fbb2d4ff53ed603baf7d6f2ee28c844
CMHyuk <alsgur9710@naver.com>
2022-11-01 19:06:37 +0900

    refactor: calculate 메서드 List 반환, 정답 메서드 반환으로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,15 +8,15 @@ public class Problem5 {
     public static final int[] moneyArray = {50000, 10000, 5000, 1000, 500, 100 ,50 ,10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
-        calculate(money, answer);
-        return answer;
+        return calculate(money, answer);
     }
 
-    private static void calculate(int calculatedMoney, List<Integer> answer) {
+    private static List<Integer> calculate(int calculatedMoney, List<Integer> answer) {
         for (int money : moneyArray) {
             int countMoney = calculatedMoney / money;
             answer.add(countMoney);
             calculatedMoney %= money;
         }
+        return answer;
     }
 }
