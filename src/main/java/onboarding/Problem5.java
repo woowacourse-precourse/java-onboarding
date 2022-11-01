85c34462448ce82cf3f1bb3b80b151d558f9455f
ghchoi <ghchoi0427@gmail.com>
2022-11-01 15:06:18 +0900

    feat: 화폐 개수 반환 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        Problem5 p5 = new Problem5();
+        return p5.convert(money);
+    }
+
+    private List<Integer> convert(int money) {
+        int[] bill = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < bill.length; i++) {
+            if (money >= bill[i]) {
+                result.add(money / bill[i]);
+                money %= bill[i];
+            } else {
+                result.add(0);
+            }
+        }
+        return result;
     }
 }
