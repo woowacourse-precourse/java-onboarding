5a367194aef4c31952772ae7eb20c840a614ef6f
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:19:44 +0900

    Problem1 solution method
    Basic structure for how the game is played

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiNum = numCalculator(pobi);
+        int crongNum = numCalculator(crong);
+        if (exceptionHandler(pobi) || exceptionHandler(crong)) {
+            return -1;
+        } else if (pobiNum > crongNum) {
+            return 1;
+        } else if (crongNum > pobiNum) {
+            return 2;
+        } else {
+            return 0;
+        }
     }
 }
