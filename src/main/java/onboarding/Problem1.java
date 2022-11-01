33eb9e7b425f757fe44e0c9e379bf30922f2b492
김형기 <khk211113@naver.com>
2022-11-01 23:19:16 +0900

    [FEAT] Problem1 - 페이지별 점수 반환 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,19 @@ class Problem1 {
         return answer;
     }
 
+    // 펼친 왼쪽(홀수) / 오른쪽(짝수) 페이지를 이용해 점수를 반환
+    private static int getScore(final List<Integer> pages) throws PageNumException {
+        int oddPage = pages.get(0);
+        int evenPage = pages.get(1);
+        if (oddPage + 1 != evenPage || oddPage < 2 || evenPage > 399)
+            throw new PageNumException();
+
+        int oddScore = new DigitDecoder(oddPage).getMax();
+        int evenScore = new DigitDecoder(evenPage).getMax();
+
+        return Math.max(oddScore, evenScore);
+    }
+
     // 페이지 숫자를 이용하여 자릿수 연산 결과를 반환
     static class DigitDecoder {
         int product = 1;
@@ -26,4 +39,6 @@ class Problem1 {
             return Math.max(this.product, this.sum);
         }
     }
+
+    static class PageNumException extends Exception { }
 }
