89d9e04dba99c63d14bf48615615001a1fc7bb7e
leorb159 <ldgsocial159@naver.com>
2022-11-01 19:48:01 +0900

    우테코 프리코스 1주차

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,40 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        
+        /*
+         * 아스키코드 
+         * 영어 대문자 65~90
+         * 영어 소문자 97~122
+         * 
+         * */
+        
+        char result;
+        
+        for(int i=0; i<word.length(); i++) {
+        	
+        	//문자 하나를 아스키값으로 변환
+        	int asciiNum = (int)word.charAt(i);
+        	
+        	//변환된 값이 알파벳 대문자
+        	if(asciiNum >64 && asciiNum <91) {
+        		//나머지연산을 통해 몇번째 알파벳인지 확인하고 Z에서 값을 뺀 것이 반대 알파벳이된다.
+        		//A가 0번째 알파벳 (A % 65 = 0)
+        		//Z(90)- 0 = Z가 되므로 A -> Z가 된다.
+        		result = (char)(90-(asciiNum%65));        		
+        		
+        	}
+        	//변환된 값이 알파벳 소문자
+        	else if(asciiNum >96 && asciiNum <123) {
+        		result = (char)(122-(asciiNum%97));  
+        	}
+        	else {
+        		//알파벳이 아닌 경우 변환없음
+				result = word.charAt(i);
+			}
+        	//변환된 값을 answer에 한개씩 추가
+        	answer =answer.concat(String.valueOf(result));
+        }
         return answer;
     }
 }
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,118 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+    	
+        List<Integer> answer = new ArrayList<Integer>();
+        
+         /*
+         * 5만원
+         * 1만원
+         * 5000원
+         * 1000원
+         * 500원
+         * 100원
+         * 50원
+         * 10원
+         * 1원
+         * 
+         * */
+        //50000원으로 나누어떨어지면
+        if(money / 50000 > 0) {
+        	//몫 = 50000원권 개수 answer에 추가
+        	answer.add(money / 50000);
+        	//나머지 = 50000원권으로 바꾸고 남은 돈
+        	money = money % 50000;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 50000);
+		}
+        
+        
+        //10000원으로 나누어떨어지면
+        if(money / 10000 > 0) {
+        	//몫 = 10000원권 개수 answer에 추가
+        	answer.add(money / 10000);
+        	//나머지 = 10000원권으로 바꾸고 남은 돈
+        	money = money % 10000;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 10000);
+		}
+        
+        //5000원으로 나누어떨어지면
+        if(money /5000 > 0) {
+        	//몫 = 5000원권 개수 answer에 추가
+        	answer.add(money / 5000);
+        	//나머지 = 5000원권으로 바꾸고 남은 돈
+        	money = money % 5000;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 5000);
+		}
+        
+        //1000원으로 나누어떨어지면
+        if(money / 1000 > 0) {
+        	//몫 = 1000원권 개수 answer에 추가
+        	answer.add(money / 1000);
+        	//나머지 = 1000원권으로 바꾸고 남은 돈
+        	money = money % 1000;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 1000);
+		}
+        
+        //500원으로 나누어떨어지면
+        if(money / 500 > 0) {
+        	//몫 = 500원권 개수 answer에 추가
+        	answer.add(money / 500);
+        	//나머지 = 500원권으로 바꾸고 남은 돈
+        	money = money % 500;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 500);
+		}
+        
+        //100원으로 나누어떨어지면
+        if(money / 100 > 0) {
+        	//몫 = 100원권 개수 answer에 추가
+        	answer.add(money / 100);
+        	//나머지 = 100원권으로 바꾸고 남은 돈
+        	money = money % 100;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 100);
+		}
+        
+        //50원으로 나누어떨어지면
+        if(money / 50 > 0) {
+        	//몫 = 50원권 개수 answer에 추가
+        	answer.add(money / 50);
+        	//나머지 = 50원권으로 바꾸고 남은 돈
+        	money = money % 50;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 50);
+		}
+        
+      //10원으로 나누어떨어지면
+        if(money / 10 > 0) {
+        	//몫 = 10원권 개수 answer에 추가
+        	answer.add(money / 10);
+        	//나머지 = 10원권으로 바꾸고 남은 돈
+        	money = money % 10;
+        }else {
+        	//나누어 떨어지지 않으므로 0개 answer에 추가
+        	answer.add(money / 10);
+		}
+        
+        //남은값 1원 개수 answer에 추가
+        answer.add(money / 1);
+        
+        
         return answer;
     }
 }
