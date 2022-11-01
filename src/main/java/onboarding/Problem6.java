2b385e705e52035209e16d256d52842a9b8374ea
gwj0421 <gwj0421@gmail.com>
2022-11-01 17:06:25 +0900

    문제6 - 문제 요구사항 정리 및 해결방법

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,13 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+    /*
+    - 문제 요구사항 정리 및 해결방법
+    1. 크루들간의 닉네임 중 연속적으로 포함된 닉네임을 갖고있는 크루들의 이메일 리턴
+        - 각각의 크루의 닉네임을 2단어씩 나눠 HashSet에 저장(메모리 효율위해)
+        - 각각의 크루들 중 나눠진 닉네임이 겹치는 크루들의 이메일을 List에 저장
+        - 저장된 이메일 List의 문자열 원소들을 오름차순으로 정렬하고 중복 제거
+     */
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
