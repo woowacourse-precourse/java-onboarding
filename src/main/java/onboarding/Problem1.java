a600f5ba1ce7dd053f1631ee0e7f3a5f4c39186c
simjaeseo <delpho@naver.com>
2022-11-01 11:45:39 +0900

    feat: 문제 1 기능 목록 작성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,13 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능 목록
+ * 1. 각 자리 숫자를 모두 더하는 함수, 각 자리를 모두 곱하는 함수
+ * 2. (1)를 이용하여 왼쪽, 오른쪽 페이지 중 가장 큰 수 구하는 함수
+ * 3. 입력 예외사항 처리
+ * 4. 포비, 크롱의 수를 구한 후 비교하여 승부 결과 값 반환
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
