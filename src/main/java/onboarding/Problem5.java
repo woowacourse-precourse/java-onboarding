20e0bbfefc55d6e0b52265a041186c695049b1f9
이소영 <th42500@naver.com>
2022-11-01 22:02:15 +0900

    [Add] 각 화폐단위에 맞는 개수와 거스름돈 구하기
    1. calculateMoney() 👉  각 화폐단위에 맞는 돈을 계산하기 위한 메서드
    2. getMoneyCount() 👉 현재 갖고 있는 돈을 계산하고자 하는 화폐단위 몇개로 계산할 수 있는지 구하기위한 베서드
    3. examineChange() 👉 현재 화폐단위로 계산할 수 없는 돈(나머지 돈) 계산하는 메서드

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,13 +1,38 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     static final int[] moneyUnits = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
+    public List<Integer> calculateMoney(List<Integer> answer, int money) {
+        for (int unit : moneyUnits) {
+            answer.add(getMoneyCount(money, unit));
+            money = examineChange(money, unit);
+        }
+
+        return answer;
+    }
+
+    public Integer getMoneyCount(int money, int unit) {
+        return money / unit;
+    }
+
+    public int examineChange(int money, int unit) {
+        if(money <= 0) {
+            return 0;
+        }
+        return money % unit;
+    }
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        Problem5 problem5 = new Problem5();
+        answer = problem5.calculateMoney(answer, money);
+
         return answer;
     }
 }
