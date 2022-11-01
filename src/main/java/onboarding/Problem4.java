890c607bc1afb32a37f6968c89a3afca4214d25f
hataerin <goil1113@likelion.org>
2022-11-01 20:01:17 +0900

    DOCS: Problem4 기능 목록 작성
    Signed-off-by: hataerin <goil1113@likelion.org>

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,8 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+
+        //엄마말씀 반대로 변환 기능
         String answer = "";
         return answer;
     }
