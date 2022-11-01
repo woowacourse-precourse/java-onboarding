ecf096bd419b545fee031eee7c8f975ea327c249
kksks3192 <kksks3192@gmail.com>
2022-11-01 16:55:18 +0900

    feat(problem6): list를 Map으로 변환하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+
+
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+
+
+    public static Map<String, String> convertToMap(List<List<String>> forms){
+
+        Map<String, String> formsMap = forms.stream()
+                .collect(Collectors.toMap(k->k.get(0), v->v.get(1)));
+
+        return formsMap;
+    }
+
+
 }
