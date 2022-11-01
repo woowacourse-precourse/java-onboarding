9810ea3f0881bae0b154dd3c785f83a816557700
gomljo <gomljo0307@naver.com>
2022-11-01 20:21:12 +0900

    feat(Problem7.java): findLocation 함수 구현
    친구 관계 정보에서 사용자 정보의 위치를 반환하는findLocation 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,6 +3,8 @@ package onboarding;
 import java.util.Collections;
 import java.util.List;
 class User{
+    final int FRIEND_A=0;
+    final int FRIEND_B=1;
     String user;
     int totalScore;
     User(String userName){
@@ -11,6 +13,15 @@ class User{
     }
     boolean isMyFriend(List<String> relation){
         return relation.contains(user);
+    }
+    int findLocation(List<String> relation){
+        int index=relation.indexOf(user);
+        if(index==FRIEND_A){
+            return FRIEND_B;
+        }
+        else{
+            return FRIEND_A;
+        }
     }
 }
 public class Problem7 {
