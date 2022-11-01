e232920f4f6bb97b9233d5f67a26c66a72d99c89
juno-junho <ssmm0205@naver.com>
2022-11-01 19:15:50 +0900

    feat(Problem1): add findGreaterOfSumAndProduct method
    ScoreCalculator 클래스 내 getGameScore에서 사용하는 직접적인 로직 메서드 생성
    페이지를 한자리씩 끊어 더한 값과 곱한 값 중 큰 값을 반환하는 findGreaterOfSumAndProduct 메서드 생성

--- src/main/java/onboarding/problem1/ScoreCalculator.java
+++ src/main/java/onboarding/problem1/ScoreCalculator.java
@@ -7,4 +7,16 @@ public class ScoreCalculator {
         List<Integer> pages = gamePlayer1.getPages();
         return pages.stream().mapToInt(ScoreCalculator::findGreaterOfSumAndProduct).max().orElseThrow(IllegalArgumentException::new);
     }
+
+    private static int findGreaterOfSumAndProduct(int page) {
+        int sumOfPage = 0;
+        int multipliedPage = 1;
+        while (page != 0) {
+            int units = page % 10;
+            sumOfPage += units;
+            multipliedPage *= units;
+            page /= 10;
+        }
+        return Math.max(sumOfPage, multipliedPage);
+    }
 }
