5f4b6798d214dff4c47df2a9d5e499a87e59477a
bini59 <myuni0234@gmail.com>
2022-11-01 14:17:04 +0900

    feat : Search word
    Search word 2characters

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+        List<Integer> temp = new ArrayList<Integer>();
+        for(int i = 0; i < forms.size(); i++) temp.add(0);
+        for(int i = 0; i < forms.size()-1; i++){
+            if (temp.get(i) == 1) continue;
+            for(int j = i+1; j < forms.size(); j++){
+                if (temp.get(j) == 1) continue;
+                for(int k = 0; k < forms.get(i).get(1).length()-1; k++){
+
+                }
+            }
+        }
+
+
+
         return answer;
     }
 }
