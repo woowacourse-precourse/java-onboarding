66c4e088e9484bca312ffacddcb909f55b184bd9
WonJeongYeon <wonjungyoun@naver.com>
2022-11-01 11:18:45 +0900

    20221101 Problem3.java 불필요한 코드 삭제

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,26 +4,27 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        if (number < 1 || number > 10000) {
-            throw new IllegalStateException("숫자는 1 이상, 10,000 이하여야 합니다.");
-        } else {
-            int[] gameNumbers = {3, 6, 9};
-            for (int i = 1; i <= number; i++) {
-                String numStr = Integer.toString(i);
-                int currentNum;
-                for (int j = 0; j < numStr.length(); j++) {
-                    currentNum = numStr.charAt(j) - '0';
-                    for (int k = 0; k < 3; k++) {
-                        if (currentNum == gameNumbers[k]) {
-                            answer++;
-                        }
+        checkNumber(number);
+        int[] gameNumbers = {3, 6, 9};
+        for (int i = 1; i <= number; i++) {
+            String numStr = Integer.toString(i);
+            int currentNum;
+            for (int j = 0; j < numStr.length(); j++) {
+                currentNum = numStr.charAt(j) - '0';
+                for (int k = 0; k < 3; k++) {
+                    if (currentNum == gameNumbers[k]) {
+                        answer++;
                     }
                 }
             }
-            System.out.println("number result");
-            System.out.println(number + "  " + answer);
+        }
+        return answer;
+    }
 
-            return answer;
+    public static void checkNumber(int number) {
+        //number는 1 이상 10,000 이하인 자연수이다.
+        if (number < 1 || number > 10000) {
+            throw new IllegalStateException("숫자는 1 이상, 10,000 이하여야 합니다.");
         }
     }
 }
