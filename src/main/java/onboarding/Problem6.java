27c8e0d7e2838e6d0c4d63d0edaefbd3aa275dda
tunkcalb <wjdgusdndi@gmail.com>
2022-11-01 19:03:22 +0900

    problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Problem6 {
+//    해쉬맵에 닉네임을 2글자씩 key값으로 이메일을 value값으로 넣어 key가 없을 경우 추가 있을 경우 이메일 비교 후 중복 이메일에 추가
+//    중복 이메일 목록에서 중복항목 제거하기 위해 HashSet 이용
     public static List<String> solution(List<List<String>> forms) {
         HashSet<String> dup_mail = new HashSet<>();
         HashMap<String,String> dup_word = new HashMap<>();
@@ -20,6 +22,7 @@ public class Problem6 {
                 if (dup_word.get(word) == null){
                     dup_word.put(word, Email);
                 }
+//                제이제이 등 자신의 닉네임에서 겹치는 경우를 제외하기 위해 해쉬맵의 value값과 Email이 다를 경우에만 추가
                 else if (dup_word.get(word) != Email){
                     dup_mail.add(Email);
                     dup_mail.add(dup_word.get(word));
