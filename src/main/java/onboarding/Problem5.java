1498075546c7f655f9645ba080774ccba3bfd8c9
sschan99 <seungchan1219@gmail.com>
2022-11-01 18:46:02 +0900

    feat: solve problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] divisors = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < divisors.length; i++) {
+            int divisor = divisors[i];
+            answer.add(money / divisor);
+            money %= divisor;
+        }
+
         return answer;
     }
 }
