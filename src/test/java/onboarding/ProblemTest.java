62aec9e41fa9968de11a44c2e8e8ab70666d5df9
hong-sile <gurwns9325@naver.com>
2022-11-01 23:40:17 +0900

    test : problem6,7 테스트 케이스 추가

--- src/test/java/onboarding/ProblemTest.java
+++ src/test/java/onboarding/ProblemTest.java
@@ -1,13 +1,17 @@
 package onboarding;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ProblemTest {
+	//개인적으로 자바 문법을 테스트 해보기 위한 test클래스
 	@Nested
 	class JavaGrammarTest {
 		@Test
@@ -26,6 +30,11 @@ public class ProblemTest {
 			int[] emptyArray = new int[6];
 			System.out.println(emptyArray[0]);
 			//int는 primitive이기에 0으로 값이 초기화 되어 있음.
+		}
+
+		@Test
+		public void testMap(){
+			Map<String,Integer> map;
 		}
 
 	}
@@ -172,8 +181,8 @@ public class ProblemTest {
 
 		@Test
 		public void testCase(){
-			String[] cryptograms = {"abbbd", "nhhhj","nhdddjjh", "woowacourse", "test"};
-			String[] result = {"ad", "nj","n", "acourse", "test"};
+			String[] cryptograms = {"abbbd", "nhhhj","nhdddjjh", "woowacourse", "test", "ccabbbacdeef", "saaddas"};
+			String[] result = {"ad", "nj","n", "acourse", "test", "cdf", "sas"};
 			for(int i=0;i<cryptograms.length;i++)
 				Assertions.assertThat(Problem2.solution(cryptograms[i]))
 					.isEqualTo(result[i]);
@@ -200,8 +209,8 @@ public class ProblemTest {
 	class TestProblem4{
 		@Test
 		public void translateTest(){
-			String te_st = Problem4.solution("Te st");
-			Assertions.assertThat(te_st).isEqualTo("Gv hg");
+			String te_st = Problem4.solution("Te st!!");
+			Assertions.assertThat(te_st).isEqualTo("Gv hg!!");
 		}
 		@Test
 		public void checkWordLength0(){
@@ -242,6 +251,129 @@ public class ProblemTest {
 				.isInstanceOf(IllegalArgumentException.class);
 			Assertions.assertThatThrownBy(()->Problem5.solution(overRangeNumber))
 				.isInstanceOf(IllegalArgumentException.class);
+		}
+	}
+
+	@Nested
+	class TestProblem6{
+		@Test
+		void testCase1(){
+			List<List<String>> forms = List.of(
+				List.of("jm@email.com", "제이엠"),
+				List.of("jason@email.com", "제이슨"),
+				List.of("woniee@email.com", "워니"),
+				List.of("javajigi@email.com", "이슨제"),
+				List.of("mj@email.com", "엠제이"),
+				List.of("nowm@email.com", "이제엠")
+			);
+			List<String> result = List.of("jason@email.com", "javajigi@email.com","jm@email.com", "mj@email.com");
+			assertThat(Problem6.solution(forms)).isEqualTo(result);
+		}
+
+		@Test
+		void testCase2() {
+			List<List<String>> forms = List.of(
+				List.of("kim@email.com", "김소이"),
+				List.of("nam@email.com", "소이야"),
+				List.of("choi@email.com", "최강"),
+				List.of("lee@email.com", "강합니다"),
+				List.of("jun@email.com", "왜아소이야")
+			);
+			List<String> result = List.of("jun@email.com", "kim@email.com", "nam@email.com");
+			assertThat(Problem6.solution(forms)).isEqualTo(result);
+		}
+
+		@Test
+		void testCase3(){
+			List<List<String>> forms = List.of(
+				List.of("onepiece@email.com", "원피스"),
+				List.of("jason@email.com", "제이슨"),
+				List.of("onepisu@email.com", "원피수"),
+				List.of("mj@email.com", "엠제이"),
+				List.of("suonepi@email.com", "수원피")
+			);
+			List<String> result = List.of("jason@email.com","mj@email.com","onepiece@email.com"
+				,"onepisu@email.com","suonepi@email.com");
+			assertThat(Problem6.solution(forms)).isEqualTo(result);
+		}
+
+		@Test
+		void testCase4(){
+			List<List<String>> forms = List.of(
+				List.of("jm@email.com", "제이엠"),
+        List.of("jason@email.com", "제이슨"),
+        List.of("woniee@email.com", "워니"),
+        List.of("mj@email.com", "엠제이"),
+        List.of("nowm@email.com", "이제엠"),
+        List.of("jasp@email.com", "박이김"),
+        List.of("jp@email.com", "김이박"),
+        List.of("qpw@email.com", "김이순"),
+        List.of("teemo@email.com", "티모"),
+        List.of("banana@email.com", "대위티모"),
+        List.of("secondJm@email.com", "제이엠"),
+        List.of("apple@email.com", "땷횱봵"),
+        List.of("stran@email.com", "횱봵으"),
+        List.of("zebra@email.com", "두글자이상의문자가연속적으로이제맞티모")
+			);
+			List<String> result = List.of("apple@email.com", "banana@email.com", "jason@email.com",
+				"jm@email.com", "jp@email.com", "mj@email.com", "nowm@email.com", "qpw@email.com",
+				"secondJm@email.com", "stran@email.com", "teemo@email.com", "zebra@email.com");
+			assertThat(Problem6.solution(forms)).isEqualTo(result);
+		}
+	}
+
+	@Nested
+	class TestProblem7{
+		@Test
+		public void testCase1(){
+			String user = "mrko";
+			List<List<String>> friends = List.of(
+				List.of("donut", "andole"),
+				List.of("donut", "jun"),
+				List.of("donut", "mrko"),
+				List.of("shakevan", "andole"),
+				List.of("shakevan", "jun"),
+				List.of("shakevan", "mrko")
+			);
+			List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
+			List<String> result = List.of("andole", "jun", "bedi");
+			assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+		}
+
+		@Test
+		void case4() {
+			String user = "andole";
+			List<List<String>> friends = List.of(
+				List.of("andole", "jun"),
+				List.of("donut", "jun"),
+				List.of("donut", "shakevan"),
+				List.of("shakevan", "andole"),
+				List.of("shakevan", "jun"),
+				List.of("shakevan", "bedi"),
+				List.of("anne", "jun")
+			);
+			List<String> visitors = List.of("donut", "mrko", "peter", "sam");
+			List<String> result = List.of("donut", "anne", "bedi", "mrko", "peter");
+			assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
+		}
+
+		@Test
+		void case5() {
+			String user = "andole";
+			List<List<String>> friends = List.of(
+				List.of("andole", "jun"),
+				List.of("andole", "bedi"),
+				List.of("jun", "shakevan"),
+				List.of("jun", "kane"),
+				List.of("jun", "sam"),
+				List.of("bedi", "shakevan"),
+				List.of("bedi", "anne"),
+				List.of("bedi", "sam"),
+				List.of("anne", "mrko")
+			);
+			List<String> visitors = List.of("donut", "anne", "mrko", "mrko", "sam");
+			List<String> result = List.of("sam", "shakevan", "anne", "kane", "mrko");
+			assertThat(Problem7.solution(user, friends, visitors)).isEqualTo(result);
 		}
 	}
 }
