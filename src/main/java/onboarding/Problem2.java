c86e83acbad9e337bbc9e1fd2893a34bc204b83f
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2022-11-01 16:06:09 +0900

    refactor(Ploblem2): 변수명 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,24 +4,23 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        answer = deleteRepeat(cryptogram);
-        return answer;
+
+        return deleteRepeat(cryptogram);
     }
 
     private static String deleteRepeat(String cryptogram) {
-        String Result = "";
+        String result = "";
         Stack<Character> deleteStack = new Stack<>();
-        for (Character x : cryptogram.toCharArray()) {
-            if (!deleteStack.isEmpty() && deleteStack.peek() == x) {
+        for (Character word : cryptogram.toCharArray()) {
+            if (!deleteStack.isEmpty() && deleteStack.peek() == word) {
                 deleteStack.pop();
             } else
-                deleteStack.push(x);
+                deleteStack.push(word);
         }
 
         for (int i = 0; i < deleteStack.size(); i++)
-            Result = Result + deleteStack.get(i);
+            result = result + deleteStack.get(i);
 
-        return Result;
+        return result;
     }
 }
