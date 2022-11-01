09e6f339297157adb6521b0c40be84b816259926
seominseok00 <seominseok4834@gmail.com>
2022-11-01 23:09:05 +0900

    feat(문제2): Solve

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,11 @@
 package onboarding;
 
+import onboarding.problem2.DecipherCryptogram;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        DecipherCryptogram decipherCryptogram = new DecipherCryptogram(cryptogram);
+        String answer = decipherCryptogram.start();
         return answer;
     }
 }
