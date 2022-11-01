f66e6560a7eefdad3e188ca13a8a597e62aa3ed8
ChaCha3088 <cha3088@gmail.com>
2022-11-01 13:46:58 +0900

    feat: problem6 new function cutNickname

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        for (List e : forms) {
-
+        for (List<String> e : forms) {
+            Boolean matchResult = isThereMatch(e.get(1));
         }
 
         return answer;
     }
 
-    private static Boolean isThereMatch(List<List<String>> forms, String keyword) {
+    private static Boolean isThereMatch(String keyword) {
         String regex = ".*("+keyword+")+.*";
         for (List<String> e : forms) {
             System.out.println(e);
@@ -22,5 +22,12 @@ public class Problem6 {
             }
         }
         return false;
+    }
+
+    private static ArrayList<String> cutNickname(String nickname) {
+        ArrayList<String> cutNameList = new ArrayList<> ();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            cutNameList.add(nickname.substring(i, i+2));
+        return cutNameList;
     }
 }
