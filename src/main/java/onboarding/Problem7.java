6152b4be55b1d2ceac33aac40e8d3a7ba8c2bf2e
sxxgxxnz <tmddms2292@gmail.com>
2022-11-01 12:36:57 +0900

    feat : Problem7[1] user와 친구인 친구리스트를 만든다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static void makeFriendList(String user,List<List<String>> friends,List<String> myFriend){
+
+        for(int i=0;i<friends.size();i++){
+            List<String> friend = friends.get(i);
+            if(friend.contains(user)){
+                String myF = friend.indexOf(user) == 0 ? friend.get(1) : friend.get(0);
+                myFriend.add(myF);
+
+            }
+        }
+
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> myFriend = new ArrayList<>();
+
+        makeFriendList(user,friends,myFriend);
         return answer;
     }
 }
