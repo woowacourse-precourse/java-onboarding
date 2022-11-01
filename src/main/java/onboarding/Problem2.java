320fcee084bcdb3cd92d6b2426b0776acdb230be
이건회 <rg970604@naver.com>
2022-11-01 20:15:06 +0900

    feat(Problem2.java) : 상수 선언

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem2 {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
+    private static final String ENGLISH_PATTERN =  "^[a-z]*$";
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
