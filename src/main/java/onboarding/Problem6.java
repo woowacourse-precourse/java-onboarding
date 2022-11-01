162a94bb19071b8e8e5c28d0af4dfe4f85fcc637
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:49:21 +0900

    Problem6 compareString method
    Checks if there is an overlap between two usernames.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -15,4 +15,13 @@ public class Problem6 {
         }
         return removeDuplicates(answer);
     }
+
+    public static boolean compareString(String s1, String s2) {
+        for (int i = 0; i < s1.length() - 1; i++) {
+            if (s2.contains(s1.substring(i, i + 2))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
