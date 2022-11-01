3a92b9ecdd4d7b51e0a77c7f16c025ea8a084818
splitube <voicesplit@daum.net>
2022-11-01 12:56:43 +0900

    3번 문제 - 기능 목록 수정, 메서드 이름 일부 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,16 +2,15 @@ package onboarding;
 
 
 /**
- * Game369
- *
- * 1. is_369(int num) = 0이 아니고 3으로 나눈 나머지가 0이면 1을 반환 나머지는 0을 반환
- * 2. find_all_369(int num) = num 의 각 자리 수에 3,6,9가 총 몇개 있는지 반환
- * 3. get_total_count(int limit) = 1부터 limit까지의 숫자에 포함된 총 369의 갯수 반환
+ * [ Game369 ]
+ *  - is_369(int num) = 0이 아니고 3으로 나눈 나머지가 0이면 1을 반환 나머지는 0을 반환
+ *  - find_all_369(int num) = num 의 각 자리 수에 3,6,9가 총 몇개 있는지 반환
+ *  - get_total_369_one_to_limit(int limit) = 1부터 limit 까지의 숫자에 포함된 총 369의 갯수 반환
  */
 public class Problem3 {
     public static int solution(int number) {
         Game369 game369 = new Game369();
-        return game369.get_total_count(number);
+        return game369.get_total_369_one_to_limit(number);
     }
 }
 
@@ -31,7 +30,7 @@ class Game369 {
         return count;
     }
 
-    public int get_total_count(int limit) {
+    public int get_total_369_one_to_limit(int limit) {
         int total = 0;
         for (int i = 1; i <= limit; i++) {
             total += find_all_369(i);
