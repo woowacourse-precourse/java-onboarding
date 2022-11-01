4764f1aedcce9f49f87eed71a78225db183e97e1
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-01 23:02:42 +0900

    [Add] Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(char letter : word.toCharArray()){
+            if((65 <= letter && letter <= 90) ||
+                    (97 <= letter && letter <122)){
+                char c = 0;
+                if (letter <= 90)
+                    c = (char) (65 + 90 - letter);
+                else
+                    c = (char) (97 + 122 - letter);
+                answer += Character.toString(c);
+            }
+            else
+                answer += Character.toString(letter);
+        }
         return answer;
     }
 }
