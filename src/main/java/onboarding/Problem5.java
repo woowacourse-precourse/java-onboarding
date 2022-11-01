5789f298fa79bb9537412a3c91352f75f4ec527d
sun007021 <sun007021@gmail.com>
2022-11-01 15:34:26 +0900

    feat: Money 클래스 생성 및 돈 단위 지정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+class Money{
+    private List<Integer> moneyUnits = Arrays.asList(50000, 10000, 1000, 500, 100, 50, 10, 1); // default 값 한국기준
+
+    public void setMoneyUnits(List<Integer> moneyUnits) {// 단위 변경
+        this.moneyUnits = moneyUnits;
+    }
+}
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
