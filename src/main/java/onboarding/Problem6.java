f38eb8bc97dd3c8ec283764749bfc640b65fe2bb
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2022-11-01 10:09:12 +0900

    feat(Problem6): 닉네임의 두 글자씩 HashMap에 추가하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
         return answer;
+    }
+    private static HashMap<String,Integer> checkNicknameDuplication(List<List<String>> forms){
+        HashMap<String,Integer> nicknameDuplication = new HashMap<>();
+        for(List<String> form: forms){
+            for(int i=0; i<form.get(1).length();i++ ){
+                String twoWords = form.get(1).substring(i,i+2);
+                nicknameDuplication.put(twoWords,
+                        nicknameDuplication.getOrDefault(twoWords,0)+1);
+            }
+        }
+        return nicknameDuplication;
     }
 }
