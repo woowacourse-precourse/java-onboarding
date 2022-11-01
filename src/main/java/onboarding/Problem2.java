98e81c5f18ee6534ed6bec942a8fd31f773f719f
1eejisoo <jisu991012@gmail.com>
2022-11-01 16:19:23 +0900

    [Problem 2] Style: 메소드 위치 변경, 주석 삭제

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,14 +7,20 @@ public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = removeDuplicate(cryptogram);
 
-        while (isDuplicate(cryptogram)) {   // 중복 문자가 없을 때까지 반복
+        while (isDuplicate(cryptogram)) {
             cryptogram = removeDuplicate(cryptogram);
         }
 
         return cryptogram;
     }
 
-    //중복 문자를 제거하는 기능
+    public static boolean isDuplicate(String cryptogram) {
+        for (int i = 0; i<cryptogram.length()-1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) return true;
+        }
+        return false;
+    }
+
     public static String removeDuplicate(String cryptogram) {
 
         List<String> duplicateCharacters = new ArrayList<>();
@@ -27,7 +33,6 @@ public class Problem2 {
         }
 
         for (String duplicateCharacter : duplicateCharacters) {
-            System.out.println(duplicateCharacter);
             if (cryptogram.contains(duplicateCharacter))
                 cryptogram = cryptogram.replaceAll(duplicateCharacter, "");
         }
@@ -35,15 +40,6 @@ public class Problem2 {
         return cryptogram;
     }
 
-    // 중복 문자가 존재하는지 체크하는 기능
-    public static boolean isDuplicate(String cryptogram) {
-        for (int i = 0; i<cryptogram.length()-1; i++) {
-            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) return true;
-        }
-        return false;
-    }
-
-    // 몇 번째 인덱스까지 연속된 문자가 나오는지 확인하는 기능
     public static int getCutIndex(String str) {
         int cutIndex = 0;
 
