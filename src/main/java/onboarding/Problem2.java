fd6effca504aba139d46b67b8d56c8acccb2d206
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-01 14:01:21 +0900

    feat : P2 - 중복 문자 제거하여 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,15 @@
 package onboarding;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         while (checkDuplication(cryptogram)) {
+            cryptogram = removeDuplication(cryptogram);
         }
+        answer = cryptogram;
         return answer;
     }
 
@@ -15,5 +20,32 @@ public class Problem2 {
                 return true;
             }
         return false;
+    }
+
+    private static String removeDuplication(String str) {
+
+        Stack<Integer> storage = new Stack<>();
+        LinkedList<Character> list = new LinkedList<>();
+
+        for(int i = 0; i < str.length(); i++)
+            list.add(str.charAt(i));
+
+        for(int i = 0; i < str.length() - 1; i++)
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                storage.push(i);
+                storage.push(i + 1);
+                i += 1;
+            }
+
+        for (int i = storage.size(); i >= 1; i--) {
+            list.remove(storage.pop().intValue());
+        }
+
+        String temp = "";
+        for (Character character : list) {
+            temp += character;
+        }
+
+        return temp;
     }
 }
