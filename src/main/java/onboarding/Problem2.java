8dd3330d81655997fadb249c8543f2c927c4e787
thddh <dhthdrud@naver.com>
2022-11-01 21:35:22 +0900

    feat: Problem2 finished

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> charStack = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            char c = cryptogram.charAt(i);
+            if (charStack.isEmpty()) {
+                charStack.push(c);
+            } else if (charStack.peek() == c) {
+                charStack.pop();
+            } else {
+                charStack.push(c);
+            }
+
+        }
+        while (!charStack.isEmpty()) {
+            answer = charStack.pop() + answer;
+        }
+
         return answer;
     }
 }
