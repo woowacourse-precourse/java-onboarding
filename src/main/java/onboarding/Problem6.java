7bd20398fd58b308f08d953206356e6486ca8207
0417taehyun <0417taehyun@gmail.com>
2022-11-01 20:20:45 +0900

    feat: [문제6] 메서드 및 풀이 구조 구축

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    public static String parseNicknameWithIndex(
+            String nickname, int startIndex, int endIndex, int destinationIndex
+    ) {
+
+    }
+
+    public static boolean isDuplicated(String nickname, String targetNickname) {
+
+    }
+
+
+    public static List<String> getDuplicatedMemberEmails(List<List<String>> forms) {
+        HashSet<String> emails = new HashSet<>();
+
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = getDuplicatedMemberEmails(forms);
+
         return answer;
     }
 }
