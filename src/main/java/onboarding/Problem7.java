aed32fd6230e4b7a04a8261fc0e4566bd3bfcccc
lhyeonj120 <isk04387@gmail.com>
2022-11-01 14:49:02 +0900

    [problem7] feat: user와 이미 친구인 리스트 생성 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> userFriends = getUserFriends(user, friends);
+        
         return answer;
+    }
+    
+    private static List<String> getUserFriends(String user, List<List<String>> list){
+    	List<String> userFriends = new ArrayList<>();
+    	
+    	for(int i = 0; i < list.size(); i++) {
+    		if(list.get(i).get(0).equals(user)) {
+    			userFriends.add(list.get(i).get(1));
+    		} else if(list.get(i).get(1).equals(user)) {
+    			userFriends.add(list.get(i).get(0));
+    		}
+    	}
+    	
+    	return userFriends;
     }
 }
