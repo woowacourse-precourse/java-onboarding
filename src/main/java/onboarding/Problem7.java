ea006ef2223265b3d04e5edb2272e728d4496e6c
jeongjihun <wlgns2234@naver.com>
2022-11-01 20:23:53 +0900

    Feat: 방문자에게 1점씩 주는 keepScoreToVisitors 함수 완성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,8 +9,44 @@ public class Problem7 {
 
         //친구들 점수를 나타내는 해시테이블 생성
         Map<String, Integer> scoreTable = makeHashTable(user, friends);
+
+        // 친구들 중 사용자와 연결된 사람에게 10점을 추가해 주는 기능
+        keepScore(user, scoreTable, friends);
+
+        // visitors에게 각각 1점씩 주는 기능
+        keepScoreToVisitors(scoreTable, visitors);
+
         System.out.println(scoreTable);
+
         return answer;
+    }
+
+    public static void keepScoreToVisitors(Map<String, Integer> scoreTable, List<String> visitors) {
+        for (int i = 0; i < visitors.size(); i++) {
+            String name = visitors.get(i);
+            System.out.println(name);
+            if (scoreTable.get(name) == null) {
+                scoreTable.put(name, 1);
+                continue;
+            }
+            Integer score = scoreTable.get(name);
+            scoreTable.put(name, score + 1);
+        }
+    }
+
+    public static void keepScore(String user, Map<String, Integer> scoreTable, List<List<String>> friends) {
+        for (int i = 0; i < friends.size(); i++) {
+            String person1 = friends.get(i).get(0);
+            String person2 = friends.get(i).get(1);
+            if (person1.equals(user)){
+                Integer nextScore = scoreTable.get(person2) + 10;
+                scoreTable.put(person2, nextScore);
+            }
+            if (person2.equals(user)){
+                Integer nextScore = scoreTable.get(person1) + 10;
+                scoreTable.put(person1, nextScore);
+            }
+        }
     }
 
     public static Map<String, Integer> makeHashTable(String user, List<List<String>> friends) {
