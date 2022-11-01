58d616903d74b6001cddb585b63a96276cd0d50b
ParkRootSeok <g_e_un@naver.com>
2022-11-01 23:26:26 +0900

    feat : Problem6 - 닉네임 중복 유무 확인 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,7 +8,24 @@ public class Problem6 {
     public static List<Integer> hasReduplication(String[] nickNames, StringBuilder nickName) {
         List<Integer> result = new ArrayList<>();
 
+        for (int i = 0; i < nickName.length() - 1;i++) {
+
+            // 닉네임 부분 추출
+            String tmp = nickName.substring(i, i+2);
+            System.out.println(tmp);
+
+            for (int j = 0;j < nickNames.length;j++) {
+
+                // 부분 추출한 닉네임 포함 확인
+                if (nickNames[j].contains(tmp)) {
+                    result.add(j);
+                }
+
+            }
+        }
+
         return result;
+
     }
 
     public static List<String> compareNickname(List<List<String>> forms) {
