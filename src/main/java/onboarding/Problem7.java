ced2d92ad0e8b9c8e926dd756a396a1bea432a71
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 18:07:09 +0900

    feat:static value

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    private static List<String> memberList = new ArrayList<>();
+    private static List<Integer> scoreList = new ArrayList<>();
+    private static List<String> friendsList = new ArrayList<>();
+    private static String member =new String();
+    private static int memberScore =0;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        return answer;
+        return rankMemberList(user, friends, visitors);
     }
 }
