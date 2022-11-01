274a252887b0cd9ed6854c1cefd51d24fc150cc6
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-01 12:13:42 +0900

    Add: Problem2 "aabba" 테스트케이스에 대한 처리 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,6 +6,10 @@ public class Problem2 {
         String[] wordArr = cryptogram.split("");
 
         for(int i=1;i<wordArr.length;i++){
+            if(answer.length() == 0){
+                answer.append(wordArr[i]);
+                continue;
+            }
             String lastword = String.valueOf(answer.charAt(answer.length()-1));
             if(lastword.equals(wordArr[i])){
                 answer = new StringBuilder(answer.substring(0, answer.length() - 1));
@@ -17,8 +21,9 @@ public class Problem2 {
     }
 
     public static void main(String[] args){
-        System.out.println(solution("browoanoommnaon"));// "brown"
-        System.out.println(solution("zyelleyz"));// ""
-        System.out.println(solution("abcdef"));// "abcdef"
+        //System.out.println(solution("browoanoommnaon"));// "brown"
+        //System.out.println(solution("zyelleyz"));// ""
+        //System.out.println(solution("abcdef"));// "abcdef"
+        System.out.println(solution("aabba"));// "abcdef"
     }
 }
