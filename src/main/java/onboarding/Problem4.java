631551176dd6843abc5044647cc82ed044a6aa8d
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-01 20:53:51 +0900

    refactor(문제 4): validation을 확인하는 대신 반대인 위반을 확인하게 변경, 함수 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,22 +2,26 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        if (!validation(word)) return "ERROR";
+        if (violation(word)) return "ERROR";
         StringBuilder answer = new StringBuilder();
         for (char c : word.toCharArray()) {
-            char frogC = c;
-            if (Character.isLowerCase(c)) {
-                frogC = (char) ('z' - c + 'a');
-            } else if (Character.isUpperCase(c)) {
-                frogC = (char) ('Z' - c + 'A');
-            }
-            answer.append(frogC);
+            answer.append(frog(c));
         }
         return answer.toString();
     }
 
-    static boolean validation(String word) {
+    static boolean violation(String word) {
         int length = word.length();
-        return length > 0 && length <= 1000;
+        return length == 0 || length > 1000;
+    }
+
+    static char frog(char c) {
+        char frogC = c;
+        if (Character.isLowerCase(c)) {
+            frogC = (char) ('z' - c + 'a');
+        } else if (Character.isUpperCase(c)) {
+            frogC = (char) ('Z' - c + 'A');
+        }
+        return frogC;
     }
 }
