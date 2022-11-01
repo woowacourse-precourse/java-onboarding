743c11901ae186a0c781dd6ce6f3004f9d743261
jiwon1217 <koagjw@gmail.com>
2022-11-01 15:24:06 +0900

    [Fix] 중복 글자인지 확인 후 처리하는 로직 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,17 @@ public class Problem2 {
         int leftIndex = 0;
         int rightIndex = 1;
 
+        while (rightIndex != cryptogram.length()) {
+            if (cryptogram.charAt(leftIndex) == cryptogram.charAt(rightIndex)) {
+                cryptogram = cryptogram.substring(0, leftIndex) + cryptogram.substring(rightIndex + 1);
+                leftIndex = 0;
+                rightIndex = 1;
+            }
+            leftIndex++;
+            rightIndex++;
+        }
+
+        answer = cryptogram;
         return answer;
     }
 }
