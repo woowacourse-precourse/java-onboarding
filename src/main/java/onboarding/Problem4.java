98e0f06ed3fb89dd12163d03d297885d8933c1f5
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:35:21 +0900

    Problem4 javadocs
    Method and class description

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,18 @@
 package onboarding;
 
+/**
+ * Problem 4
+ * @author Davin An
+ * @version 1.0
+ */
 public class Problem4 {
+
+    /**
+     * Replaces letters in a String according to dictionary.
+     * Method performs all replacements within one traversal by manipulating the UNICODE values of each character.
+     * @param word String to perform replacement on
+     * @return String after replacement
+     */
     public static String solution(String word) {
         char[] wordArray = word.toCharArray();
         for (int i = 0; i < wordArray.length; i++) {
