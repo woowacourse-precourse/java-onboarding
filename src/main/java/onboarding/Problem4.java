8275c6b9533bdbab29636431005a0df30cc35c6c
김형기 <khk211113@naver.com>
2022-11-01 23:23:00 +0900

    [FEAT] Problem4 - 알파벳 위치 반환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,13 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    // 대소문자 상관없이 몇 번째 알파벳인지 반환
+    private static int getVector(char ch) {
+        if ('a' <= ch && ch <= 'z')
+            return ch - 'a';
+        else if ('A' <= ch && ch <= 'Z')
+            return ch - 'A';
+        return -1;   // 알파벳이 아님
+    }
 }
