860f2ba10f038295ce40d6e04b89a459da27abdc
seongbeom <84623098+hi12123123@users.noreply.github.com>
2022-11-01 16:29:55 +0900

    feat : add function moneyList and boundary exception

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = moneyList(money);
         return answer;
+    }
+
+    private static List<Integer> moneyList(int money){
+        List<Integer> result = new ArrayList<>();
+
+        // money는 1 이상 1,000,000 이하인 자연수이다.
+        if (1 > money || 1000000 < money) {
+            return null;
+        }
+
+        return result;
     }
 }
