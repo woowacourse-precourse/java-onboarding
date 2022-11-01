e17301ed55964a0d4e79a193058a857dfc075544
a1dotls <meeae9408@gmail.com>
2022-11-01 18:53:22 +0900

    1. 책을 임의로 펼친다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
+	
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+    	
+    	int pPage = (int)((400)*Math.random( ) + 1);
+    	int cPage = (int)((400)*Math.random( ) + 1);
+    	
+    	if(pPage % 2 == 0) {
+    		pobi.add(pPage-1);
+    		pobi.add(pPage);
+    	} else {
+    		pobi.add(pPage);
+    		pobi.add(pPage+1);
+    	}
+    	
+    	if(cPage % 2 == 0) {
+    		crong.add(cPage-1);
+    		crong.add(cPage);
+    	} else {
+    		crong.add(cPage);
+    		crong.add(cPage+1);
+    	}
+    	
         int answer = Integer.MAX_VALUE;
+    		
         return answer;
     }
 }
