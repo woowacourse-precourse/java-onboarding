527c6f58a0125b8b212f0f308ddafb06ee136b5a
임원정 <LWJ@MacBookPro.local>
2022-11-01 19:52:46 +0900

    feature : problem7 user의 friend를 구하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> usersFriends = getUsersFriends(user, friends);
+
         return answer;
+    }
+
+    private static List<String> getUsersFriends(String user, List<List<String>> friends) {
+        List<String> friendList = new ArrayList<>();
+
+        for (List<String> name : friends) {
+            if (name.get(0).equals(user)) {
+                friendList.add(name.get(1));
+            }
+            if (name.get(1).equals(user)) {
+                friendList.add(name.get(0));
+            }
+        }
+        return friendList;
     }
 }
