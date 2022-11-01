f11ee751200afc33a0041cbd013ffaf019fde230
ahinkim <ahin98@naver.com>
2022-11-01 11:56:07 +0900

    refactor(problem3.java): Modify to distinguish between lowercase and uppercase letters [feature1], Add function to turn word into tree frog language [feature2]

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,12 +1,30 @@
 package onboarding;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Problem4 {
+    // 2. word를 청개구리 언어로 바꾼다.
     public static String solution(String word) {
         String answer = "";
+        int N = word.length();
+        for(int i = 0; i < N; i++){
+            char ch = word.charAt(i);
+            if(ch == ' '){
+                answer += ' ';
+            }
+            else{
+                answer += changeChar(ch);
+            }
+        }
         return answer;
     }
     // 1. 청개구리 사전에 따라 문자를 return 해준다.
     public static char changeChar(char ch){
-        return (char)('A' + (25 - (ch - 'A')));
+        if('a' <= ch && 'z' >= ch){
+            return (char) ('a' + (25 - (ch - 'a')));
+        }
+        else {
+            return (char) ('A' + (25 - (ch - 'A')));
+        }
     }
 }
