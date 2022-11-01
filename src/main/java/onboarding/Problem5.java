7a274cbbc65da5f5966a7cf02612b6de02cd35d2
JungYoonshin <sju08227@naver.com>
2022-11-01 20:10:04 +0900

    feat: #problem5-> 돈을 큰 금액의 화폐순으로 변환하여 출력하여 출금하는 함수 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = withdrawMoney(money);
+
+        return answer;
+    }
+
+    private static List<Integer> withdrawMoney(int money){
+        List<Integer> answer = new ArrayList<>();
+        int[] sortMoney = new int[] {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i=0; i<sortMoney.length; i++){
+            answer.add(money / sortMoney[i]);
+            money %= sortMoney[i];
+        }
         return answer;
     }
 }
