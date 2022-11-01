9ed7e0b7a35fc861daef4b8ef5715c70e91ff1ca
jamangstangs <jamangsong@gmail.com>
2022-11-01 10:32:48 +0900

    feat: problem2(진행), findContinuousChar() -> 연속된 문자의 index정보를 반환하는 메소드 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem2 {
@@ -19,6 +20,28 @@ public class Problem2 {
     }
 
     private static List<Integer> findContinuousChar(String cryptogram) {
+        List<Integer> indices = new ArrayList<Integer>();
+
+        cryptogram = cryptogram + ".";
+        Integer firstWindow=0;
+        Integer secondWindow=1;
+
+        // 문자열 해독 과정
+        while(secondWindow != cryptogram.length()) {
+            if (cryptogram.charAt(firstWindow) == cryptogram.charAt(secondWindow)) {
+                secondWindow += 1;
+                continue;
+            } else if (secondWindow - firstWindow == 1) {
+                firstWindow = secondWindow;
+            } else {
+                for (int j = firstWindow; j < secondWindow; j++) {
+                    indices.add(j);
+                }
+                firstWindow = secondWindow;
+            }
+            secondWindow += 1;
+        }
+        return indices;
     }
 
     private static String eraseCharByIndex(List<Integer> continuousCharIndices, String cryptogram) {
