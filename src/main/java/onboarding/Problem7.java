2b1a69e52d72f078121c7d60620fbcf763391a4c
Lee-Jiseung <ljss98@naver.com>
2022-11-01 16:47:01 +0900

    feat: problem7 사용자 친구 리스트 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    private static List<String> userFriends;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        initUserFriends(user, friends);
         return answer;
+    }
+
+    private static void initUserFriends(String user, List<List<String>> friends) {
+        userFriends = new ArrayList<>();
+        for (List<String> friend : friends) {
+            if (friend.get(0).equals(user)) {
+                userFriends.add(friend.get(1));
+            } else if (friend.get(1).equals(user)) {
+                userFriends.add(friend.get(0));
+            }
+        }
     }
 }
