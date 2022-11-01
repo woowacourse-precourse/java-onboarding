8b0c32c6b07a4dc83871d1816e6b0f29e43f8d30
seominseok00 <seominseok4834@gmail.com>
2022-11-01 22:55:12 +0900

    feat(문제1): 승자 판단 메소드 구현

--- src/main/java/onboarding/problem1/BookGame.java
+++ src/main/java/onboarding/problem1/BookGame.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class BookGame {
     private User user1, user2;
     private int user1Score, user2Score;
-
+    private int result = -1;
+    
     public BookGame(List<Integer> page1, List<Integer> page2){
         this.user1 = new User(page1);
         this.user2 = new User(page2);
@@ -24,5 +25,17 @@ public class BookGame {
     private void getUsersScore(){
         user1Score = user1.getUserScore();
         user2Score = user2.getUserScore();
+    }
+
+    private void judgeWinner(){
+        if(user1Score > user2Score) {
+            result = 1;
+        }
+        else if(user1Score < user2Score) {
+            result = 2;
+        }
+        else if(user1Score == user2Score){
+            result = 0;
+        }
     }
 }
