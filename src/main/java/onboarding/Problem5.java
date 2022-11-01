ead55a6de9ca621ce00753fb5b60d76ff64922d6
kim1387 <kevink0@naver.com>
2022-11-01 17:38:12 +0900

    feat: 검증 로직 추가
    범위를 초과하지 않았는지 검증

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 public class Problem5 {
   private static final int[] CHANGE_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+  private static final int MIN_RANGE_NUMBER = 1;
+  private static final int MAX_RANGE_NUMBER = 1000000;
 
   public static List<Integer> solution(int money) {
+    validate(money);
     return solveChangeList(money);
   }
 
@@ -17,5 +20,15 @@ public class Problem5 {
       money %= change;
     }
     return answer;
+  }
+
+  public static void validate(int number) {
+    if (isInRange(number)) {
+      throw new IllegalArgumentException("범위 초과");
+    }
+  }
+
+  private static boolean isInRange(int number) {
+    return (number < MIN_RANGE_NUMBER) || (MAX_RANGE_NUMBER < number);
   }
 }
