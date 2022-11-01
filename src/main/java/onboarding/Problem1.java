c9bdd7554579d291d721f7816638508baa412a51
c1ce <chalicenuclear@gmail.com>
2022-11-01 22:05:06 +0900

    Problem1 clear

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-class Problem1 { 
+class Problem1 {
 //    public static void main(String[] args) {
 //        List<Integer> pobi = new ArrayList<>(Arrays.asList(123,124));
 //        solution()
