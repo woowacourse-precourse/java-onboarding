3ce34b04e9350dfc8b3a87eb489dbd1379cbdf4b
jamangstangs <jamangsong@gmail.com>
2022-11-01 11:05:31 +0900

    feat: problem6(진행), Trainee class -> Trainee class 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        List<String> duplicatedTraineeEmail = new ArrayList<>();
+        List<Trainee> validTrainees = validateEmailAndNickname(forms);
+
+
         return answer;
+    }
+
+    private static class Trainee {
+        private String email;
+        private String nickname;
+
+        public Trainee(String email, String nickname) {
+            this.email = email;
+            this.nickname = nickname;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
     }
 }
