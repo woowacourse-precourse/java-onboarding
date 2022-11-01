eef9c4ba370b94542bbeb0faefbb624f133538d3
Park Sungchul <try615@naver.com>
2022-11-01 21:06:35 +0900

    [feat] Problem2.java : 주석 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,16 +3,26 @@ package onboarding;
 import java.util.*;
 
 public class Problem2 {
+
+    /**
+     * 암호 해독기 클래스
+     */
     static class Decrypt {
         Stack<Character> stack;
         StringBuilder sb;
 
+        /**
+         * 해독기 클래스 생성자.
+         */
         public Decrypt() {
             this.stack = new Stack<>();
             this.stack.push('/');
             this.sb = new StringBuilder();
         }
 
+        /**
+         * 암호 해독 메소드
+         */
         public String decrypt(String cryptogram) {
             int strLen = cryptogram.length();
 
@@ -29,6 +39,9 @@ public class Problem2 {
             return result();
         }
 
+        /**
+         * 암호 해독 결과 메소드
+         */
         private String result() {
             int stackLen = this.stack.size();
             for (int i = 1; i < stackLen; i++) {
