2bdc10615d3d842a16a42d8e04cfcc10b0d48dec
Yohan <sprae114@naver.com>
2022-11-01 16:43:03 +0900

    problem5 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        List<Integer> changeMoney = Arrays.asList(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
+
+        for(int moneyType : changeMoney){
+            answer.add(money / moneyType);
+            money %= moneyType;
+        }
         return answer;
     }
 }
