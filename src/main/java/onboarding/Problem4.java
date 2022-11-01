408a96f67404ff8a2a90c26ebaf1e3398fa0fa7f
2tsumo <sherlockid8@naver.com>
2022-11-01 19:35:05 +0900

    [problrm4] 청개구리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        List<String> alphabetLowerList = new ArrayList<>();
+        List<String> alphabetUpperList = new ArrayList<>();
+        List<String> alphabetList = new ArrayList<>();
+        for(char i='a'; i<='z'; i++) {
+            alphabetLowerList.add(String.valueOf(i));
+        }
+        for(char i='A'; i<='Z'; i++){
+            alphabetUpperList.add(String.valueOf(i));
+        }
+
+        alphabetList.addAll(alphabetUpperList);
+        alphabetList.add(" ");
+        alphabetList.addAll(alphabetLowerList);
+
+        List<Integer> pointList = new ArrayList<>();
+        char[] words = word.toCharArray();
+        for (char alphabet: words) {
+            pointList.add(alphabetList.indexOf(String.valueOf(alphabet)));
+        }
+        List<String> reverseAlphabetList = new ArrayList<>();
+        Collections.reverse(alphabetLowerList);
+        Collections.reverse(alphabetUpperList);
+        reverseAlphabetList.addAll(alphabetUpperList);
+        reverseAlphabetList.add(" ");
+        reverseAlphabetList.addAll(alphabetLowerList);
+
+        String answer="";
+
+        for (int point : pointList) {
+            answer += reverseAlphabetList.get(point);
+        }
         return answer;
     }
+
 }
+
