9420dca5d641aea7d27970c4fe43211865b3ad02
소대훈 <theogns0681@naver.com>
2022-11-01 09:47:43 +0900

    [feat] (문제2) 연속적인 문자 찾기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,10 +2,24 @@ package onboarding;
 
 public class Problem2 {
     static char[] answer;
-
+    static String temp;
     public static String solution(String cryptogram) {
-        answer = cryptogram.toCharArray();
-        return "";
+        temp = cryptogram;
+        findWord(false);
+        return temp;
     }
-
+    public static void findWord(boolean check){
+        answer = temp.toCharArray();
+        for (int i = 0 ; i < answer.length-1 ; i++){
+            char ch = answer[i];
+            if (ch==answer[i+1] && ch!='0'){
+                //convert(i,findLength(i));
+                check =true;
+            }
+        }
+        if (check) {
+            //generateString();
+            findWord(false);
+        }
+    }
 }
