683a82bcd2fc689a1db4aaf9700d3b172826a58a
JJongmen <pjy90123@gmail.com>
2022-11-01 18:58:21 +0900

    refactor(Problem2): 의도에 맞는 메서드명으로 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,13 @@ package onboarding;
 
 public class Problem2 {
     private static final String REGEX = "([a-z])\\1+";
+    private static final String EMPTY_STRING = "";
 
     public static String solution(String cryptogram) {
+        return decodeCryptogram(cryptogram);
+    }
+
+    private static String decodeCryptogram(String cryptogram) {
         String decodedCryptogram = cryptogram;
         do {
             cryptogram = decodedCryptogram;
@@ -12,11 +17,11 @@ public class Problem2 {
         return decodedCryptogram;
     }
 
-    private static boolean isDecoded(String cryptogram, String decodedCryptogram) {
-        return cryptogram.equals(decodedCryptogram);
+    private static String removeConsecutiveDuplicates(String cryptogram) {
+        return cryptogram.replaceAll(REGEX, EMPTY_STRING);
     }
 
-    private static String removeConsecutiveDuplicates(String cryptogram) {
-        return cryptogram.replaceAll(REGEX, "");
+    private static boolean isDecoded(String cryptogram, String decodedCryptogram) {
+        return cryptogram.equals(decodedCryptogram);
     }
 }
