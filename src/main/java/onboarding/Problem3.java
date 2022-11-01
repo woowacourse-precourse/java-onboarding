219028be5bc391d9b0855f984cca68c2bdbf08d5
Yh0sun <hy770819@naver.com>
2022-11-01 15:11:01 +0900

    ëfunc list

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,10 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        //1. 1~number loop
+        //  2. 숫자를 10으로 나눈 나머지가 3,6,9 중 하나이면 answere+=1
+        //     숫자를 10으로 나눈 몫으로 계속 반복
         return answer;
     }
 }
