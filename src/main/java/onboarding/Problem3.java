5390db702d280f9e7ad818af3e5a35413e4fb6a3
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-01 21:56:52 +0900

    refactor : 불필요한 코드및 네임 변경, 접근제어자 수정, 알맞은 메서드로 정정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,19 +6,19 @@ public class Problem3 {
         answer = splitNumber(number);
         return answer;
     }
-    static int splitNumber(int number){
+    public static int splitNumber(int number){
         int result = 0;
         for(int i=1; i<=number; i++){
-            String str = Integer.toString(i);
-            String[] strarr = str.split("");
-            if(clapCount(strarr) > 0) result += clapCount(strarr);
+            String str = String.valueOf(i);
+            String[] stringarr = str.split("");
+            if(clapCount(stringarr) > 0) result += clapCount(stringarr);
         }
         return result;
     }
-    static int clapCount(String[] strarr){
+    private static int clapCount(String[] stringarr){
         int count = 0;
-        for(int i=0 ; i<strarr.length; i++){
-            if(strarr[i].equals("3") || strarr[i].equals("6") || strarr[i].equals("9")) count++;
+        for(int i=0 ; i<stringarr.length; i++){
+            if(stringarr[i].equals("3") || stringarr[i].equals("6") || stringarr[i].equals("9")) count++;
         }
         return count;
     }
