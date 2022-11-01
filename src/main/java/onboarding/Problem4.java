55db4fa4e6f34864e55c2210a03d40414fefac50
aran <aran0317@naver.com>
2022-11-01 21:16:29 +0900

    문제 4번 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,10 +5,22 @@ package onboarding;
  * 2. 알파벳 외의 문자는 변환하지 않는다.
  <solution>
  *  - 알파벳 범위를 파악한 후, 반대로 변환하여 answer에 더해준다.
+ *  - 범위가 아닌경우, answer에 그대로 더해준다.
  */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for(int i=0; i<word.length(); i++){
+            if(word.charAt(i)>=65 && word.charAt(i) <=90){
+                int dis = word.charAt(i) - 65;
+                answer += (char)  ('A' +25 -dis);
+
+            }else if (word.charAt(i)>=97 && word.charAt(i) <=122){
+                int dis = word.charAt(i) - 97;
+                answer += (char) ('a'+25 -dis);
+            }
+            else answer+=word.charAt(i);
+        }
         return answer;
     }
 }
