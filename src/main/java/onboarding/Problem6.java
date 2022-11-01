bfcba1e6917917bded9fc764bd878854c19c7aac
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-01 18:40:56 +0900

    problem6: TODO-'기능2' complete

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,7 +9,30 @@ public class Problem6 {
     private static List<String> sortHashSetAsList(HashSet<String> hashSet){
         return hashSet.stream().sorted().collect(Collectors.toList());
     }
+    private static List<String> getEmailsToRecommend(List<List<String>> forms){
+
+        HashSet<String> emails = new HashSet<>();
+        HashMap<String, String> dic = new HashMap<>();
+
+        for(List<String> form:forms){
+            String email = form.get(0);
+            String nickname = form.get(1);
+            for(int i = 0; i < nickname.length()-1; i++)
+            {
+                String twoOfNick = nickname.substring(i,i+2);
+                if(dic.containsKey(twoOfNick) && !dic.get(twoOfNick).equals(email))
+                {
+                    emails.add(email);
+                    emails.add(dic.get(twoOfNick));
+                }
+                dic.put(twoOfNick, email);
+            }
+        }
+
+        return sortHashSetAsList(emails);
+
+    }
     public static List<String> solution(List<List<String>> forms) {
-        return null;
+        return getEmailsToRecommend(forms);
     }
 }
