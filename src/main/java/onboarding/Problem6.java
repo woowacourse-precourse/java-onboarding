eeaa2b7bb176c3513353fc8001caa3ffe45373b6
patronum98 <zkrhklkl98@gmail.com>
2022-11-01 13:23:16 +0900

    문제6 기능목록 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,9 +1,10 @@
 package onboarding;
 
-//기능1. 닉네임과 닉네임을 두글자씩 검사한다.
-//기능2. 겹치는 닉네임글자가 있으면 이메일만 리스트에 추가한다.
-//기능3. 기능1,2 를 2중포문으로 만들어 모든 닉네임끼리 비교하게 한다.
-//기능4 . answer 을 오름차순으로 정렬한뒤 리턴한다.
+//기능1. 비교할 닉네임의 두글자를 문자열에 저장한다. ex)"제이엠" 에서 "제이"만 저장
+//기능2. 겹치는 닉네임글자가 있으면 이메일만 리스트에 추가한다. ex)"제이" 가 다른 닉네임에 포함되는지 검사
+//기능3. 문자열을 초기화 하고 비교할 닉네임의 다음 두 글자를 문자열에 저장한뒤 기능2 실행. ex)"제이엠" 에서 "이엠" 만 저장
+//기능4. 반복문을 통해 모든 닉네임을 비교한다.
+//기능5. answer 을 오름차순으로 정렬한뒤 리턴한다.
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +19,26 @@ public class Problem6 {
 
         String a = (forms.get(0)).get(1); //제이엠
         String b = (forms.get(1)).get(1); //제이슨
+        String c = (forms.get(2)).get(1); //워니
+        String d = (forms.get(3)).get(1); //엠제이
+        String e = (forms.get(4)).get(1); //이제엠
 
-        if ((a.charAt(0) == b.charAt(0)) && (a.charAt(1) == a.charAt(1))) {
-            answer.add((forms.get(0)).get(0));
-        }
-        System.out.println(answer);
+        String str = "제이엠";
+
+        System.out.println(str.contains("제이슨"));
+
+
+
+//        for (int i=0;i<forms.toArray().length;i++) {
+//            String com1 = (forms.get(i)).get(1); // 제이엠
+//            String com2 = (forms.get(i+1)).get(1); // 제이슨
+//            for (int j=0;j<forms.toArray().length-i;j++) {
+//                if (com1.charAt(0) == (forms.get(j)).get(1).charAt(0)) && ((com2.charAt(1) == (forms.get(j+1)).get(1).charAt(1))) {
+//                    answer.add((forms.get(0)).get(0));
+//            }
+//        }
+//        }
+//        System.out.println(answer);
 
 
         return answer;
