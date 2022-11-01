caf7cba677322f05abff4119d589f1e0440b78d4
gichan-song <thdrlcks97@gmail.com>
2022-11-01 13:19:34 +0900

    problem2 finished

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
-
+import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        Stack<Character> stack1 = new Stack<Character>();
+        for(int i=cryptogram.length()-1; i>=0; i--){
+            try{
+                if(stack1.peek() == cryptogram.charAt(i)){
+                    stack1.pop();
+                }else{
+                    stack1.push(cryptogram.charAt(i));
+                }
+            }catch(Exception e){
+                stack1.push(cryptogram.charAt(i));
+            }
+
+        }
+        StringBuilder strb = new StringBuilder();
+        while (!stack1.empty()) {
+            answer += stack1.pop();
+        }
         return answer;
     }
 }
