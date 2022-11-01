c3d26fb03e14bd19c9742417bc67f4be3579a249
saehim <saeilee.vxv@gmail.com>
2022-11-01 17:57:05 +0900

    Feat(Problem7): 유저 친구 찾기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,18 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+    // user 친구 찾기
+    private static List<String> getOldFriends(String user, List<List<String>> friends) {
+        List<String> result = new ArrayList<>();
+        // user 친구 찾기
+        for (List<String> relation : friends){
+            if(relation.contains(user)) {
+                result.add(relation.get(0)==user ? relation.get(1) : relation.get(0));
+            }
+        }
+        return result;
+    }
+
     // 추천 점수 추가
     static void addPoint(String user, int point) {
         if(friendPoint.containsKey(user)) {
