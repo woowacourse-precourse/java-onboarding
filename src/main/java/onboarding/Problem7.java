0eae8be1d9bf7608b9420ccbeacbbd5d5e7baa04
sun007021 <sun007021@gmail.com>
2022-11-01 23:14:20 +0900

    feat: UsersInformation class 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,6 +2,43 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+class UsersInformation {
+    private String user;
+    private List<List<String>> friends;
+    private List<String> usersFriends;
+    private List<String> visitors;
+    private List<String> recomendFriends;
+
+    public void setUser(String name) {
+        user = name;
+    }
+
+    public void setFriends(List<List<String>> list) {
+        friends = list;
+    }
+
+    public void setVisitors(List<String> list) {
+        visitors = list;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public List<List<String>> getFriends() {
+        return friends;
+    }
+
+    public List<String> getVisitors() {
+        return visitors;
+    }
+
+    public List<String> getRecomendFriends() {
+        return recomendFriends;
+    }
+}
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
