bc6fe6ac679ecf21a6b974a6e098bb7dbd19b91d
jiwon1217 <koagjw@gmail.com>
2022-11-01 15:50:02 +0900

    [Fix] for 문 로직 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,9 +7,9 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        for (List<String> form : forms) {
-            String email = form.get(0);
-            String nickname = form.get(1);
+        for (int i = 0; i < forms.size(); i++) {
+            String email = forms.get(i).get(0);
+            String nickname = forms.get(i).get(1);
         }
         return answer;
     }
