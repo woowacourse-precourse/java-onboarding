eed12d23d6d0f008a7001e069917104d45363aa8
solesy <tkddbs9801@gmail.com>
2022-11-01 11:36:20 +0900

    refactor: Modify a problem2 variable name and add a remark

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,21 +5,21 @@ import java.util.Stack;
 public class Problem2 {
     private String removeDuplicate(final String N){
         Stack<Character> stk = new Stack<Character>();
-        Character prevPoped = null;
+        Character prevPopedChar = null;
         for(int i = 0; i < N.length(); ++i){
             if(stk.empty()) {
-                if(prevPoped == null || prevPoped != N.charAt(i))
+                if(prevPopedChar == null || prevPopedChar != N.charAt(i))
                     stk.push(N.charAt(i));
             } else{
-                //String[i]와 stk.top 을 비교해서, 같다면 stk.pop 다르면 stk.push(String[i]) 실행
                 if(N.charAt(i) == stk.peek()){
-                    prevPoped = stk.pop();
+                    prevPopedChar = stk.pop();
                 } else{
-                    if(prevPoped == null || prevPoped != N.charAt(i))
+                    if(prevPopedChar == null || prevPopedChar != N.charAt(i))
                         stk.push(N.charAt(i));
                 }
             }
         }
+        //스택에 남아있는 문자열이 정답이다.
         String ret = "";
         for(int i = 0; i < stk.size(); ++i){
             Character c = stk.get(i);
