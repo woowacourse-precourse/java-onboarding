7f09b68b47e21e2f5f288549bfd42c90ae2a3b6f
Identityless <rlandwnd555@naver.com>
2022-11-01 21:52:00 +0900

    FEAT : resultset에 조건에 부합하는 닉네임을 가진 사용자의 email 넣는 기능 getDup() 메서드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 /*
 *   기능 목록
-*   1. 조건을 만족하는 닉네임을 가진 크루만 모아놓은 리스트를 만드는 기능
+*   1. 조건을 만족하지 않는 닉네임을 resultset에 추가하는 기능
 *   2. 크루들의 닉네임을 2글자 크기의 토큰으로 나누는 기능
 *   3. 패턴이 나타난 이름을 forms 리스트에서 제외하고 result 리스트에 넣는 기능
 */
@@ -15,28 +15,33 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
-    static List<String> getNameList(List<List<String>> forms) {
-        List<String> list = new ArrayList<>();
+    static void getNameList(List<List<String>> forms) {
         for (List<String> splitedlist : forms) {
-            if (splitedlist.get(1).length() > 1 && splitedlist.get(1).length() < 20) {
-                list.add(splitedlist.get(1));
-            }
-            else {
-                resultset.add(splitedlist.get(2));
+            if (splitedlist.get(0).length() < 1 || splitedlist.get(0).length() > 20) {
+                resultset.add(splitedlist.get(1));
             }
         }
-        return list;
     }
 
-    static List<List<String>> getNameToken(List<String> names) {
-        List<List<String>> nameToken = new ArrayList<>();
+    static List<String> getNameToken(List<String> names) {
+        List<String> nameToken = new ArrayList<>();
         for (String name : names) {
-            List<String> tokenList = new ArrayList<>();
             for (int j = 2; j <= name.length(); j++) {
-                tokenList.add(name.substring(j - 2, j));
+                nameToken.add(name.substring(j - 2, j));
             }
-            nameToken.add(tokenList);
         }
         return nameToken;
+    }
+
+    static void findDup(List<String> tokens, List<List<String>> forms, int idx) {
+        for(int i = 0 ; i < forms.size() ; i++) {
+            if(i != idx) {
+                for (String token : tokens) {
+                    if (forms.get(i).get(0).contains(token)) {
+                        resultset.add(forms.get(i).get(1));
+                    }
+                }
+            }
+        }
     }
 }
