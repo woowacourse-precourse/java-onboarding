fa549772f9effb19a046ff7ad85a9a15c0816bc7
ryuj-h <dbwlsgh57@naver.com>
2022-11-01 21:55:37 +0900

    Solve Problem 6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,25 +2,37 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Problem6 {
-    private static List<String> GetPosibleExample(String nickname){
+    private static List<String> GetPossibleExample(String nickname){
         List<String> ret_variable = new ArrayList<>();
 
         for (int i = 0; i < nickname.length() - 1; i++){
-            String truncated = nickname.substring(i,i+2);
+            String truncated = nickname.substring(i, i+2);
             ret_variable.add(truncated);
         }
         return ret_variable;
     }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
 
-        for(List<String> obj:forms) {
-            List<String> truncated = GetPosibleExample(obj.get(1));
+        for (List<String> obj:forms) {
+            List<String> truncates = GetPossibleExample(obj.get(1));
 
-            System.out.println(truncated);
+            for (List<String> otherobj:forms) {
+                if (obj.get(0) == otherobj.get(0))// 다른사람과만 검사하기
+                    continue;
+
+                for (String  mytruncated : truncates){// 닉네임 조합을 포함하는 닉네임 찾기
+                    if (otherobj.get(1).contains(mytruncated))
+                        answer.add(otherobj.get(0));
+                }
+            }
         }
-        return answer;
+        Set<String> duplicateDelete = new TreeSet<>(answer);
+        return new ArrayList<>(duplicateDelete);
     }
 }
