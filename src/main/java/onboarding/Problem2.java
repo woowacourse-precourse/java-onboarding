49c03462592804e527128348d50014f5b5d7e7c4
areyouhun <areyouhun@gmail.com>
2022-11-01 22:19:49 +0900

    refactor(prob2): move lines of code to a new class

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,46 +1,10 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import onboarding.feature2.CryptogramSolver;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        if (cryptogram.length() < 2) {
-            return cryptogram;
-        }
-
-        char[] previousString = cryptogram.toCharArray();
-
-        // Make an empty list that will have the letters of previousString meeting the if statement below🔻
-        List<Character> laterString = new ArrayList<>();
-
-        // Start with the second letter of previousString
-        for (int i = 1; i < previousString.length; i++) {
-            if (previousString[i] != previousString[i - 1]) {
-                laterString.add(previousString[i - 1]);
-                continue;
-            }
-            // When adjacent elements are duplicate, increase the index
-            while (i < previousString.length && previousString[i] == previousString[i - 1]) {
-                i++;
-            }
-        }
-
-        /*
-        * Check the last letter of previousString because the index could exceed
-        * the length of previousString before the if statement above🔺 inspect the last letter
-        */
-        if (previousString[previousString.length - 1] != previousString[previousString.length - 2]) {
-            laterString.add(previousString[previousString.length - 1]);
-        }
-
-        String newCryptogram = laterString.stream()
-            .map(letter -> String.valueOf(letter))
-            .collect(Collectors.joining());
-        if (!cryptogram.equals(newCryptogram)) {
-            return solution(newCryptogram);
-        }
-        return newCryptogram;
+        String answer = CryptogramSolver.analyzeCryptogram(cryptogram);
+        return answer;
     }
 }
