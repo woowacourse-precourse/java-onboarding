267b2532a4cb940ae6898a2de2f8950a2199470c
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-01 18:40:41 +0900

    [P2] 연속 중복 문자 찾기

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        answer = "";
+        int indexCnt = 1;
+        String oneChar, twoChar;
+
+        while (cryptogram.length() != indexCnt) {
+            oneChar = String.valueOf(cryptogram.charAt(indexCnt-1));
+            twoChar = String.valueOf(cryptogram.charAt(indexCnt));
+            // 1. 연속 중복 문자 찾기
+            if (oneChar.equals(twoChar)) {
+
+            }
+        }
         return answer;
     }
 }
