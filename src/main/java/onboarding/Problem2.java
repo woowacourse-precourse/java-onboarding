a40fee6b3b177d06220f6f642fcaa5fa87a44013
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-01 13:31:33 +0900

    Update Problem2.java
    스택 형식으로 구현 변경, 오류 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,27 +12,15 @@ public class Problem2 {
         // 스택을 이용해서 가장 위의 값과 삽입하려는 값이 같으면 삭제
         StringBuffer sb = new StringBuffer();
 
-        // 연속해서 지우는 중인가?
-        boolean isChainDelete = false;
-        char checkAlphabet = cryptogram.charAt(0);
+        sb.append(cryptogram.charAt(0));
 
         for (int i = 1; i < cryptogram.length(); i++) {
-            // 만약 가장 뒤의 값과 삽입하려는 값이 같으면 -> 삭제
-            if (cryptogram.charAt(i) == checkAlphabet) {
-                isChainDelete = true;
+            char thisChar = cryptogram.charAt(i);
+            if (sb.charAt(sb.length() - 1) == thisChar) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(thisChar);
             }
-
-            // 아니라면 삽입
-            else {
-                if (!isChainDelete) {
-                    sb.append(checkAlphabet);
-                }
-                isChainDelete = false;
-                checkAlphabet = cryptogram.charAt(i);
-            }
-        }
-        if (!isChainDelete) {
-            sb.append(checkAlphabet);
         }
 
         answer = sb.toString();
@@ -41,6 +29,8 @@ public class Problem2 {
     }
 
     public static void main(String[] args) {
-        System.out.println(solution("bcdaaaa"));
+        System.out.println(solution("browoanoommnaon"));
+
+
     }
 }
