7f3dc80fbf722402c7bec8512a824813a6109810
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 11:38:44 +0900

    feat: 문제에서 요구하는 상수(Const) 정의

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,16 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private final static int POBI_WIN = 1;
+    private final static int CRONG_WIN = 2;
+    private final static int DRAW = 0;
+    private final static int EXCEPTION = -1;
+    private final static int START_PAGE_NUMBER = 1;
+    private final static int END_PAGE_NUMBER = 400;
+    private final static int PAGE_GAP = 1;
+    private final static int PAGE_LIST_SIZE = 2;
+    
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
