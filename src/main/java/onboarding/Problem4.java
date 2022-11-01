6b0d5b6caa8b51d415e65d62f38009e92f44408b
dlgosla <dlgoslaek1@gmail.com>
2022-11-01 13:23:55 +0900

    feature: [Problem4] 청개구리 사전 만들기 함수 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,44 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
         return answer;
     }
+
+    public static Map<Character, Character> make_convert_dict(){
+        Map<Character, Character> convert_dict;
+
+        Map<Character, Character> uppercase_convert_dict = match_ascii_between(65, 90);
+        Map<Character, Character> lowercase_convert_dict = match_ascii_between(97, 122);
+
+        uppercase_convert_dict.putAll(lowercase_convert_dict);
+        convert_dict = uppercase_convert_dict;
+
+        return convert_dict;
+    }
+
+    public static Map<Character, Character> match_ascii_between(int start_ascii, int end_ascii){
+        Map<Character, Character> convert_dict = new HashMap<>();
+
+        for (int i = 0; i < 26; i++) {
+            Character start_alpha = (char)start_ascii;
+            Character end_alpha = (char)end_ascii;
+
+            convert_dict.put(start_alpha, end_alpha);
+            convert_dict.put(end_alpha, start_alpha);
+
+            start_ascii += 1;
+            end_ascii -= 1;
+        }
+
+        return convert_dict;
+    }
+
+
 }
