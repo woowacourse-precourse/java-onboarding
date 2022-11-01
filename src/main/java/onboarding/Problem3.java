e4f9e4e07a1e5d99d75cf843064813f9e9e6671d
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 22:49:49 +0900

    chore:Problem3 주석 추가.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class Problem3 {
 
+    // 반복되는 숫자에서 369가 포함되는지 확인하는 메서드
     static int judgeNumber(String element) {
         int answernumber = 0;
         Pattern pattern = Pattern.compile("^([369]*)$");
