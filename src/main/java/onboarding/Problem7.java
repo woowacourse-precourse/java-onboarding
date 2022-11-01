e6f0f95dd7b1a7bf9ff886ce79166224507a087e
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-01 16:10:59 +0900

    feature : 함께 아는 친구 점수 추가 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,9 +2,11 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    private static final Integer FRIEND_SCORE = 10;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
@@ -19,6 +21,29 @@ public class Problem7 {
             }
         });
         return myFriends;
+    }
+
+    private static HashMap<String, Integer> getScoreByFriends(String user, List<String> myFriends, List<List<String>> AllFriends) {
+        HashMap<String, Integer> ranking = new HashMap<>();
+        if (myFriends.isEmpty()) {
+            return ranking;
+        }
+        AllFriends.forEach(friend -> {
+            String name = friend.get(1);
+            String friendName = friend.get(0);
+            // name의 친구가 나의 친구와 친구 관계이고, name이 내가 아닐 때
+            if (myFriends.contains(friendName) && !name.equals(user)) {
+                // 추천 랭킹에 이미 이름이 있으면
+                if (ranking.containsKey(name)) {
+                    // 함께아는 친구 점수 추가
+                    ranking.put(name, ranking.get(name) + FRIEND_SCORE);
+                } else if(!ranking.containsKey(name)) {
+                    // 그렇지 않으면 name에게 친구 추천 점수를 주고 랭킹에 등록
+                    ranking.put(name, FRIEND_SCORE);
+                }
+            }
+        });
+        return ranking;
     }
 
 }
