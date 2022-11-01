b9811e6b4276cbb21acf87b4d84c6abd0637add0
0417taehyun <0417taehyun@gmail.com>
2022-11-01 14:56:32 +0900

    feat: [문제3] 개별 숫자에 대한 자리수별 3, 6, 9 숫자 포함 여부 및 개수 카운팅 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int countTargetNumber(int targetNumber) {
+        int cumulativeCount = 0;
+        int exponent = (Integer.toString(targetNumber).length() - 1);
+
+        while (exponent >= 0) {
+            int digit = (int) Math.pow(10, exponent);
+            int target = targetNumber / digit;
+            if (target == 3 || target == 6 || target == 9) {
+                cumulativeCount++;
+            }
+
+            targetNumber %= digit;
+            exponent--;
+        }
+
+        return cumulativeCount;
     }
 
     public static int solution(int number) {
