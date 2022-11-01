b09c6f4465057f3c86987a0e19a89b929ea68dd4
can019 <jys01012@gmail.com>
2022-11-01 20:33:41 +0900

    :sparkles: 369 완성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,28 +3,29 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i = 2; i <= number; i ++){
+            int units = i % 10;
+            int tens = (i % 100 - units) / 10;
+            int hundreds = (i % 1000 - tens - units)/100;
+            int thousands = (i % 10000 - hundreds - tens - units)/1000;
+
+            if(isInRule(units))
+                answer+=1;
+            if(isInRule(tens))
+                answer+=1;
+            if(isInRule(hundreds))
+                answer+=1;
+            if(isInRule(thousands))
+                answer+=1;
+        }
+
         return answer;
     }
 
-    private static boolean inputExceptionChecker(int number){
-        // 1. Input이 1 이상 10000이하가 아닌 경우
-        if(!isBetween(1,10000, number))
+    private static boolean isInRule(int x){
+        if( x == 3 || x == 6 || x == 9)
             return true;
         return false;
-    }
-
-    /**
-     *
-     * Target이 min 이상 max 이하에 있는 값인지 검사
-     * @param min 검사하고 싶은 범위 중 작은 값
-     * @param max 검사하고 싶은 범위 중 큰 값
-     * @param target 검사하고 싶은 값
-     * @return 사이에 존재한다면 true, 아니라면 false를 반환
-     */
-    private static boolean isBetween(int min, int max, int target){
-        if(min<=target && target<=max)
-            return true;
-        else
-            return false;
     }
 }
