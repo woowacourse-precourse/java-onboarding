7c94c1d408aee44529a25f765e744b54e52fb61c
jihun-24k <gi020477@gmail.com>
2022-11-01 14:35:21 +0900

    Solve: 문제 2번

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,54 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        if (!validCryptogram(cryptogram.toCharArray())) {
+            return "error";
+        }
+
+        String answer = cryptogram;
+        String after = "";
+        while (true) {
+            after = removeStr(answer.toCharArray());
+            if (after.equals(answer)) {
+                answer = after;
+                break;
+            }
+            answer = after;
+        }
         return answer;
+    }
+
+    public static boolean validCryptogram(char [] cryptogram) {
+        for (char c : cryptogram) {
+            if (!Character.isLowerCase(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String removeStr(char [] str) {
+        if (str.length < 2) {
+            return new String(str);
+        }
+        char pre = 0;
+        int j = 0;
+        boolean isOverlap = false;
+        for (char s : str) {
+            if (pre != s) {
+                if (isOverlap) {
+                    isOverlap = false;
+                    j--;
+                }
+                str[j++] = s;
+                pre = s;
+            } else {
+                isOverlap = true;
+            }
+        }
+        if (isOverlap) {
+            j--;
+        }
+        return new String(str).substring(0,j);
     }
 }
