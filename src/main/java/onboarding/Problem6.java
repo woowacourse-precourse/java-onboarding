b3ece3f1ff173e87724c3631984c3f604a30436c
dingding-21 <phj8221@naver.com>
2022-11-01 23:02:18 +0900

    feat: Problem6 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록 구하는 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static Set<String> getDuplicatedCrewEmail(List<List<String>> forms) {
+        String nickname;
+        String divNickname;
+
+        Map<String, String> crew = new HashMap<>();
+        Set<String> email = new HashSet<>();
+
+        for (List<String> form : forms) {
+            nickname = form.get(1);
+            for (int j = 0; j < nickname.length() - 1; j++) {
+                divNickname = nickname.substring(j, j + 2);
+
+                if (crew.containsKey(divNickname)) {
+                    email.add(crew.get(divNickname));
+                    email.add(form.get(0));
+                }
+
+                crew.put(divNickname, form.get(0));
+            }
+        }
+        return email;
     }
 }
