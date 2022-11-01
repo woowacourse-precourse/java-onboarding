3a7351cfc773bd3d31e41e443e8e0c81ed6c2fd2
younghoon211@naver.com <younghoon211@naver.com>
2022-11-01 21:05:22 +0900

    feat(Problem3): solution

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        List<Integer> numList = new ArrayList<>(number);
+
+        for (int i = 1; i <= number; i++) {
+            numList.add(i);
+        }
+
+        return numCount(numList, count);
     }
 }
