df0accad641baac4348499905fe3c7b566f50977
YusunPark <solarpy.kor@gmail.com>
2022-11-01 21:41:13 +0900

    Feat : 문제 3 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i < number+1; i++){
+            String num_str = String.valueOf(i);
+            answer += num_str.length() - num_str.replace(String.valueOf("3"), "").length();
+            answer += num_str.length() - num_str.replace(String.valueOf("6"), "").length();
+            answer += num_str.length() - num_str.replace(String.valueOf("9"), "").length();
+        }
         return answer;
     }
 }
