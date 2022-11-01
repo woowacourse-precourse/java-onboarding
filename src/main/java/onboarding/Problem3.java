4992144d8cc050dcf7000a1245158bb8721fe270
simjaeseo <delpho@naver.com>
2022-11-01 15:12:35 +0900

    feat: 검증할 수를 char[]로 변환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,12 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            char[] numbers = String.valueOf(i).toCharArray();
+
+        }
+
         return answer;
     }
 }
