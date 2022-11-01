d6d0c03835af7ca9b609d23bd0548a48ea117943
이규승 <rpg8988@naver.com>
2022-11-01 19:22:24 +0900

    update: number를 받아와 모든 자리수가 3 6 9에 해당하는지 체크

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,16 @@
 package onboarding;
 
 public class Problem3 {
+    public static int solution1(int number) {
+        int cnt = 0;
+
+        while (number > 0) {
+            if (number % 10 == 3 || number % 10 == 6 || number % 10 == 9)
+                cnt++;
+            number /= 10;
+        }
+        return cnt;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
