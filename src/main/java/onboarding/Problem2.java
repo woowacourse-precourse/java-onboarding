c291eed54d29a42142c9a5282b475c15dfa396de
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-01 17:08:45 +0900

    [Problem2] 연속문자 제거 정규표현식으로 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        int len = cryptogram.length();
+        // 정규표현식으로 연속된 문자 제거
+        answer = cryptogram.replaceAll("(.)\\1+", "");
         return answer;
     }
 }
