11db264a3717779827ada084f65fcee36a3b7cf7
MinSungKim <timkim01@naver.com>
2022-11-01 11:54:18 +0900

    feat : Problem7 getUserFriends 메서드 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static Set<String> getUserFriends(String user, List<List<String>> friends){
+        Set<String> userFriends = new HashSet<>();
+
+        for (List<String> friendList : friends){
+            if(friendList.contains(user)){
+                for (String friend : friendList){
+                    if(!friend.equals(user)){
+                        userFriends.add(friend);
+                    }
+                }
+            }
+        }
+
+        return userFriends;
+    }
+
+
 }
