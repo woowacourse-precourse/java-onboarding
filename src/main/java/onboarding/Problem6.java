e5edc746403437dca76c6fab0205ad690d22727b
mihea1206 <mihea12@gmail.com>
2022-11-01 09:28:57 +0900

    problem6(tokenizing by two characters)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem6 {
+
+    private static List<String> tokenizer(String str) {
+        return IntStream.rangeClosed(0, str.length() - 2)
+            .mapToObj(i -> String.join("", Arrays.copyOfRange(str.split(""), i, i + 2)))
+            .collect(Collectors.toList());
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
