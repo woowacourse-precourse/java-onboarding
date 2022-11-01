35cdf49ca5b9154f1eefec0b3edb8f7669cbd5d5
khj <kim37171@cuk.edu>
2022-11-01 20:06:31 +0900

    [Problem1] 점수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,11 +8,20 @@ class Problem1 {
             return  -1;
         }
 
+        int pobi_max = getLarger(pobi.get(0),pobi.get(1));
+        int crong_max = getLarger(crong.get(0),crong.get(1));
+
         return -1;
     }
 
     private static boolean isNotValid(List<Integer> pobi, List<Integer> crong) {
         return Math.abs(pobi.get(0) - pobi.get(1)) > 1 || Math.abs(crong.get(0) - crong.get(1)) > 1;
+    }
+
+    private static int getLarger(int left, int right) {
+        int l = getMax(String.valueOf(left));
+        int r = getMax(String.valueOf(right));
+        return Math.max(l,r);
     }
 
     private static int getMax(String str) {
