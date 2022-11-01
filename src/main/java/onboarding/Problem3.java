fbb56c32b838d7c7ee7043e54a793edadd929b0a
judyi19997 <judyi1999@kookmin.ac.kr>
2022-11-01 17:38:41 +0900

    add/ 문제 3) 문자에서 3/6/9 찾는기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,8 +6,28 @@ import java.util.List;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int countingNum = 1;
+        while(countingNum <=number){
+            String stringCountingNum = numToString(countingNum);
+            answer+=countClap(stringCountingNum);
+            countingNum ++;
+
+        }
         return answer;
     }
+
+    //문자에서 3/6/9 갯수 찾는 기능
+    public static int countClap(String stringCountingNum){
+        int clapCount = 0;
+        for (int i=0; i<stringCountingNum.length();i++){
+            if ("3,6,9".contains(Character.toString(stringCountingNum.charAt(i)))){
+                clapCount++;
+            }
+        }
+
+        return clapCount;
+    }
+
     //숫자를 문자로 바꾸는 기능
     public static String numToString(int number){
         return Integer.toString(number);
