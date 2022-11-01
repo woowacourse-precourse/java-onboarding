ebe0465574d0699676d7b82a2cef3c045e753424
nahyeon cho <whskgus12@nate.com>
2022-11-01 19:48:59 +0900

    Problem 6 - 유효한 닉네임과 모든 경우의 수
    닉네임에 한글만 있는지 파악하는 기능, 닉네임의 길이가 주어진 범위 내 인지 파악하는 기능을 통해 유효한 닉네임을 판단하였습니다. 닉네임에서 가능한 두 글자 이상의 연속된 문자의  경우의 수 또한 allCaseNick함수를 통해 반환합니다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
@@ -7,4 +8,55 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static class Nickname {
+        private final String crewNick;
+        public Nickname(String nick) {
+            this.crewNick = nick;
+        }
+        public List<List<String>> allCaseNickEmail(List<List<String>> forms) {
+            List<List<String>> result_nicklist = new ArrayList<>();
+
+            for (List<String> form : forms) {
+                List<String> nicklist = allCaseNick(form.get(1)); // forms의 닉네임 각각에 대한 모든 경우의 수
+                for (String nick : nicklist) {
+                    List<String> sub = new ArrayList<>();
+                    sub.add(form.get(0)); // forms에서 이메일 가져오기
+                    sub.add(nick); // nicklist에서 닉네임 가져오기
+                    result_nicklist.add(sub);
+                }
+            }
+            return result_nicklist; // 비교하기 편하도록 이메일과 모든 경우의 수를 합친 array를 반환
+        }
+
+        public List<String> allCaseNick(String nick) { // 닉네임당 나올 수 있는 모든 경우의 수
+            int length = nick.length();
+            List<String> nicklist = new ArrayList<>();
+
+            for (int step = 2; step <= length; step++) { // 춘식이 -> 춘식,식이,춘식이로 만드는 과정
+                for (int index = 0; index < length; index++) {
+                    if (index + step >= length) {
+                        nicklist.add(nick.substring(index));
+                        break;
+                    } else {
+                        nicklist.add(nick.substring(index, index + step));
+                    }
+                }
+            }
+            return nicklist;
+        }
+
+        public boolean isValidNick() {
+            String regex_nick = ".*[가-힣].*";
+            int nick_length = crewNick.length();
+
+            if (crewNick.matches(regex_nick)) { // 닉네임에 한글만 허용
+                return nick_length >= 1 && nick_length < 20; // 한글만 있을때 길이는 유효한가
+            } else {
+                return false;
+            }
+
+        }
+    }
+
 }
