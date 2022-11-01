04df859c403d07f10ac8e7edd88e1844d963615f
jay <jaewhan232@naver.com>
2022-11-01 16:49:52 +0900

    Implement : 문자열 반환 규칙 hashmap 반환하는 메소드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,21 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    private static Map<Character, Character> getConversionRules() {
+        Map<Character, Character> conversionRules = new HashMap<Character, Character>();
+        for (int i = 0; i < 26; i++) {
+            conversionRules.put((char) (65+i), (char) (90-i));
+            conversionRules.put((char) (97+i),(char) (122-i));
+        }
+        return conversionRules;
+    }
+
 }
