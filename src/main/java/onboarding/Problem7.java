4b0f9614472c181e142bfcf8cab000b1318ead24
endlessmomo <soohyuk96@naver.com>
2022-11-01 16:00:00 +0900

    feat : Problem7 교우 관계를 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map <String,Set <String>> friendsRelationMap;
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        createFriendRelation(user, friends);
+
+        return null;
     }
+
+    private static void createFriendRelation(String user, List <List <String>> friends) {
+        friendsRelationMap = new HashMap <>();
+
+        for (List <String> relation : friends) {
+            if (!friendsRelationMap.containsKey(relation.get(0))) {
+                friendsRelationMap.put(relation.get(0), new HashSet <>());
+            }
+
+            if (!friendsRelationMap.containsKey(relation.get(1))) {
+                friendsRelationMap.put(relation.get(1), new HashSet <>());
+            }
+
+            friendsRelationMap.get(relation.get(0)).add(relation.get(1));
+            friendsRelationMap.get(relation.get(1)).add(relation.get(0));
+        }
+    }
+
 }
