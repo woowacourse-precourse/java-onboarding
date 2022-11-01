2cf243000530d4e44464216c64857463a9ee09e1
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-01 21:02:19 +0900

    refactor(문제 6): validation을 확인하는 대신 반대인 위반을 확인하게 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        if (!validation(forms)) return Collections.emptyList();
+        if (violation(forms)) return Collections.emptyList();
         boolean[] duplicate = new boolean[forms.size()];
         Set<String> totalSubNickNameSet = new HashSet<>();
         Set<String> subNickNameSetFrom2 = new HashSet<>();
@@ -27,33 +27,33 @@ public class Problem6 {
         return answer;
     }
 
-    static boolean validation(List<List<String>> forms) {
-        if (forms.size() == 0 || forms.size() > 10000) return false;
+    static boolean violation(List<List<String>> forms) {
+        if (forms.size() == 0 || forms.size() > 10000) return true;
         for (List<String> form : forms) {
-            if (!validationForm(form)) return false;
+            if (violationForm(form)) return true;
         }
-        return true;
+        return false;
     }
 
-    static boolean validationForm(List<String> form) {
-        if (form.size() != 2) return false;
+    static boolean violationForm(List<String> form) {
+        if (form.size() != 2) return true;
         String email = form.get(0);
         String nickname = form.get(1);
-        return validationEmail(email) && validationNickname(nickname);
+        return violationEmail(email) || violationNickname(nickname);
     }
 
-    static boolean validationEmail(String email) {
-        if (email.length() <= 10 || email.length() >= 20) return false;
-        if (!email.endsWith("@email.com")) return false;
+    static boolean violationEmail(String email) {
+        if (email.length() <= 10 || email.length() >= 20) return true;
+        if (!email.endsWith("@email.com")) return true;
         String id = email.substring(0, email.length() - 10);
         String regex = "^[a-zA-Z0-9]([-_.]?[0-9a-zA-Z])*$";
-        return Pattern.matches(regex, id);
+        return !Pattern.matches(regex, id);
     }
 
-    static boolean validationNickname(String nickname) {
-        if (nickname.length() == 0 || nickname.length() >= 20) return false;
-        String regex = "^[ㄱ-ㅎ가-힣]*$";
-        return Pattern.matches(regex, nickname);
+    static boolean violationNickname(String nickname) {
+        if (nickname.length() == 0 || nickname.length() >= 20) return true;
+        String regex = "^[ㄱ-ㅎㅏ-ㅣ가-힣]*$";
+        return !Pattern.matches(regex, nickname);
     }
 
     static Set<String> subNicknameSet(String nickname) {
