c915f8eaf7fa479ceaa6e70db046bde92663e52b
DonghyunShin <shindh0429@gmail.com>
2022-11-01 23:13:57 +0900

    [CREATE] 변수로 들어오는 책의 페이지 유효 검사 기능 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,36 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 0;
+
+        List<Integer> maxValue = new ArrayList<>();
+        if(checkValid(pobi) == 1){
+            return -1;
+        }
+
+        List<Integer> maxValue2 = new ArrayList<>();
+        if(checkValid(pobi) == 1){
+            return -1;
+        }
+
+
         return answer;
     }
+
+    private static int checkValid(List<Integer> pages){
+        int res = 0;
+        if(pages.get(0)+1 == pages.get(1)){
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+
 }
