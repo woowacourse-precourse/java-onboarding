5b5327c2f9e082d655252cab8eb3f84963c0f306
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-01 22:27:12 +0900

    docs: Problem1 주석 제거

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -25,7 +25,6 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        //0. 예외처리에 걸릴 경우 -1 리턴
         if (exceptionCheck(pobi) == -1 || exceptionCheck(crong) == -1) {
             return -1;
         }
@@ -33,8 +32,6 @@ class Problem1 {
         int player1Number = biggestNumberCheck(pobi);
         int player2Number = biggestNumberCheck(crong);
 
-        //3. 1,2 중 큰값 구해서 도출
-        //4. pobi와 crong 중 누가더 큰지 확인 후 포비승(1) , 크롱승(2), 무승부(0) 리턴
         if (player1Number > player2Number) {
             return 1;
         } else if (player1Number < player2Number) {
@@ -56,13 +53,11 @@ class Problem1 {
         int plusNumber = 0;
         int multiplyNumber = 1;
 
-        //1. 왼쪽 페이지 번호를 다 더한값과 곱한값 구해서 큰값 도출
         for (int leftNumber : leftNumbers) {
             plusNumber += leftNumber;
             multiplyNumber *= leftNumber;
         }
 
-        //2. 오른쪽 페이지 번호를 다 더한값과 곱한값 구해서 큰값 도출
         if (plusNumber > multiplyNumber) {
             return plusNumber;
         } else {
@@ -87,17 +82,14 @@ class Problem1 {
         int leftPageNumber = player.get(0);
         int rightPageNumber = player.get(1);
 
-        //2.1) 왼쪽페이지 번호가 2~ 399 사이의 값인가?
         if (leftPageNumber <= 1 || leftPageNumber >= 399) {
             return -1;
         }
 
-        //2.2) 왼쪽 페이지의 번호가 홀 수 인가?
         if (leftPageNumber % 2 == 0) {
             return -1;
         }
 
-        //2.3) 오른쪽페이지 번호가 왼쪽페이지 + 1 이 맞는가
         if (rightPageNumber != leftPageNumber + 1) {
             return -1;
         }
