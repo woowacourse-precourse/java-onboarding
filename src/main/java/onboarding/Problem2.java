956ab137fbdbea5b24204e2539c3ba7d2d3792e5
hyunseo <goatsfish@gmail.com>
2022-11-01 14:20:03 +0900

    refactor(Problem2) : 두 문자 이상 연속 암호 해독 기능 리팩터링

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -10,7 +10,6 @@ public class Problem2 {
         StringBuilder cryptogramBuilder = new StringBuilder(cryptogram);
         int cryptogramLength = decodeCharacters(cryptogramBuilder);
 
-
         return cryptogramBuilder.toString();
     }
 
@@ -18,13 +17,57 @@ public class Problem2 {
         int deletedCount = 0;
         int cryptogramLength = cryptogramBuilder.length();
         for (int currIndex = 0; currIndex < findCryptogramLength(cryptogramLength, deletedCount); currIndex++) {
-            deletedCount = deleteRepeatedCharacters();
+            deletedCount = deleteRepeatedCharacters(cryptogramBuilder, deletedCount, currIndex);
         }
         return cryptogramLength;
     }
 
     private static int findCryptogramLength(int cryptogramLength, int deletedCount) {
         return cryptogramLength - 1 - deletedCount;
+    }
+
+    private static int deleteRepeatedCharacters(StringBuilder cryptogramBuilder,
+                                                int deletedCount,
+                                                int currIndex) {
+
+        int endIndex = currIndex;
+        while (checkRepeatedCharacter(cryptogramBuilder, currIndex, endIndex)) {
+            endIndex++;
+            deletedCount++;
+        }
+
+        if (checkDeletedMoreThanTwo(currIndex, endIndex)) {
+            deleteString(cryptogramBuilder, currIndex, endIndex);
+        }
+
+        return deletedCount - 1;
+    }
+
+    private static boolean checkRepeatedCharacter(StringBuilder cryptogramBuilder,
+                                                  int currIndex,
+                                                  int endIndex) {
+
+        return checkCryptogramRange(cryptogramBuilder, endIndex) &&
+                checkSameCharacter(cryptogramBuilder, currIndex, endIndex);
+    }
+
+    private static boolean checkCryptogramRange(StringBuilder cryptogramBuilder, int endIndex) {
+        return endIndex < cryptogramBuilder.length();
+    }
+
+    private static boolean checkSameCharacter(StringBuilder cryptogramBuilder,
+                                              int currIndex,
+                                              int endIndex) {
+
+        return cryptogramBuilder.charAt(currIndex) == cryptogramBuilder.charAt(endIndex);
+    }
+
+    private static boolean checkDeletedMoreThanTwo(int currIndex, int endIndex) {
+        return endIndex - currIndex >= 2;
+    }
+
+    private static void deleteString(StringBuilder cryptogramBuilder, int currIndex, int endIndex) {
+        cryptogramBuilder.delete(currIndex, endIndex);
     }
 
 }
