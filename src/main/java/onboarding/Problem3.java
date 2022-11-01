cc63de34620deb6b2281769b244abcc976f4d7b4
Basak <chgy2131@naver.com>
2022-11-01 11:57:02 +0900

    [feat] 3번 - 입력된 숫자에 박수를 몇번 처야하는지 확인하는 함수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,24 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    // 입력된 숫자에 박수를 몇 번 처야하는지 확인하는 함수
+    private static int isIt369(int number) {
+        int clap = 0;
+        // 각 자릿수를 하나씩 불러온다.
+        while(number > 0){
+            switch (number % 10) {
+                // 박수를 칠지 (3,6,9 인지) 판단한다.
+                case 3:
+                case 6:
+                case 9:
+                    clap++;
+                    number /= 10;
+                    break;
+                default:
+                    number /= 10;
+            }
+        }
+        return clap;
+    }
 }
