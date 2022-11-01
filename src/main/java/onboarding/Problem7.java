66eacb22da60c45706654cfebdd0449fdaef4564
yesjuhee <juhee0924@g.skku.edu>
2022-11-01 19:59:33 +0900

    [PROBLEM7] 방문자 목록을 정리해주는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,14 +1,19 @@
 package onboarding;
 
+import org.mockito.internal.stubbing.answers.CallsRealMethods;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
         List<String> friendsOfUser = findFriendsOfUser(user, friends);
         List<String> commonFriends = findCommonFriends(friends, friendsOfUser, user);
+        visitors = changVisitors(visitors, friendsOfUser);
+        System.out.println(visitors);
 
         List<String> answer = Collections.emptyList();
         return answer;
@@ -44,6 +49,35 @@ public class Problem7 {
         }
 
         return commonFriends;
+    }
+
+    public static List<String> changVisitors(List<String> visitors,List<String> friendsOfUser){
+
+
+        System.out.println("1");
+//        for(int i=0; i<friendsOfUser.size(); i++){
+//            String oldFriend = friendsOfUser.get(i);
+//
+//        }
+
+//        Iterator iter = visitors.iterator();
+//        while(iter.hasNext()){
+//            if("donut".equals(iter.next())){
+//                iter.remove();
+//            }
+//        }
+//        visitors.remove(visitors.indexOf("donut"));
+//        visitors.remove(visitors.indexOf("shakevan"));
+//        visitors.remove(2);
+//        visitors.remove(3);
+        System.out.println("2");
+        ArrayList<String> visitors2 = new ArrayList<>();
+        System.out.println("3");
+        visitors2.add("bedi");
+        visitors2.add("bedi");
+        visitors2.add("bedi");
+        System.out.println("4");
+        return visitors2;
     }
 
 
