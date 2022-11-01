316330df6665cbeb5c7c0f1f5fbc4cc4bb93c6b2
ECHO <eunchan1001@gmail.com>
2022-11-01 11:29:38 +0900

    feat(Problem6): 이름 문자열을 두 글자씩 쪼개어 반환하는 메서드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -9,5 +10,13 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Set<String> result = new TreeSet<String>();
         return result.stream().collect(Collectors.toList());
+    }
+
+    static List<String> getTwoLetterWords(String s) {
+        List<String> words = new ArrayList<String>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            words.add(s.substring(i, i + 2));
+        }
+        return words;
     }
 }
