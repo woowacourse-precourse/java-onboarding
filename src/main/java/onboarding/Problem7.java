63278232ce95ea4a91601f1dc8d4e4ca575c5012
tlqkrus012345 <tlqkrus012345@naver.com>
2022-11-01 15:27:50 +0900

    Problem7 existFriend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,50 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
+/**
+ * 
+ * 기능 목록 1) frinedPoint 2) visitPoint 3) existFriend 4) suggestFriend
+ * 
+ * @version java 11 2022년 11월 1일
+ * @author 이규호(tlqkrus012345)
+ *
+ */
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+
+	static HashMap<String, Integer> point = new HashMap<>();
+	static List<String> userFriend = new ArrayList<>();
+
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		List<String> answer = new ArrayList<>();
+
+		existFriend(user, friends);
+		friendPoint(visitors);
+
+		return answer;
+	}
+
+	/**
+	 * user와 기존에 친구인 userFriend 배열을 만드는 기능이다
+	 * 
+	 * @param user
+	 * @param friends
+	 */
+	public static void existFriend(String user, List<List<String>> friends) {
+		for (List<String> list : friends) {
+			if (list.get(0).equals(user)) {
+				userFriend.add(list.get(1));
+			}
+			if (list.get(1).equals(user)) {
+				userFriend.add(list.get(0));
+			}
+		}
+	}
+
+	public static void friendPoint(List<String> visitors) {
+
+	}
 }
