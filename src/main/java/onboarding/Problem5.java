e5bcb9079fbb769b351f8d78f868f57e08007dd0
sami355-24 <ddung355@gmail.com>
2022-11-01 19:38:48 +0900

    [add] solution

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,27 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+
+        answer.add(0,find50000(money));
+        money%=50000;
+        answer.add(1, find10000(money));
+        money%=10000;
+        answer.add(2, find5000(money));
+        money%=5000;
+        answer.add(3, find1000(money));
+        money%=1000;
+        answer.add(4, find500(money));
+        money%=500;
+        answer.add(5, find100(money));
+        money%=100;
+        answer.add(6, find50(money));
+        money%=50;
+        answer.add(7, find10(money));
+        money%=10;
+        answer.add(8, find1(money));
+        money%=1;
+
         return answer;
     }
 
