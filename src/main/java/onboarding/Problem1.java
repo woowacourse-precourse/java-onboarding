cfab11faaca975f79a02f90cd01d18d83a08a067
yewon <yewon.ux@gmail.com>
2022-11-01 18:15:53 +0900

    페이지 배열 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -12,6 +13,30 @@ class Problem1 {
         4. 예외 case를 처리한다.
         */
         int answer = Integer.MAX_VALUE;
+
+        //CASE pobi
+        for(int j=0; j<pobi.size(); j++) {
+            //각 페이지 번호를 담을 배열 생성
+            ArrayList<Integer>tempList = new ArrayList<Integer>();
+
+            //페이지 번호를 자리수 별로 분리하여 tempList에 add
+            String tempStr = pobi.get(j).toString();
+            for(int i=0; i<tempStr.length(); i++) {
+                tempList.add((tempStr.charAt(i) - '0'));
+            }
+        }
+
+        //CASE crong
+        for(int j=0; j<crong.size(); j++) {
+            //각 페이지 번호를 담을 배열 생성
+            ArrayList<Integer>tempList = new ArrayList<Integer>();
+
+            //페이지 번호를 자리수 별로 분리하여 tempList에 add
+            String tempStr = crong.get(j).toString();
+            for(int i=0; i<tempStr.length(); i++) {
+                tempList.add((tempStr.charAt(i) - '0'));
+            }
+        }
         return answer;
     }
 }
