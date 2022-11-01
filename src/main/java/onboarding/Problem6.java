3b0abe15abee86d6d4b4a4881c2f7648e8b95993
이지영 <wldudwldhd@naver.com>
2022-11-01 23:08:53 +0900

    feat:중복인 이메일 넣은 후 중복 제거하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,10 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     /*기능구현
@@ -12,10 +16,32 @@ public class Problem6 {
     : result는 오름차순, 중복은 제거하여 출력
 
     1. 같은 글자가 연속으로 포함된다 -> 2글자만 공통으로 포함되어도 중복으로 간주
-    2. 중복된 단어를 가진 닉네임을 저장 -> 해당 이메일을 정렬 후 반환
+    2. 중복된 단어를 가진 닉네임을 저장 -> 해당 이메일을 정렬 후 반환(set이용)
     * */
+
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> answer = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String subname = nickname.substring(i, i + 2);
+
+                /* 중복이 있는 이메일을 중복을 가진 자신의 이메일과, 다른 이메일은 모두 result에 저장  */
+                if (map.containsKey(subname)) {
+                    answer.add(email);
+                    answer.add(map.get(subname));
+
+                    break;
+                }
+                map.put(subname, email);
+            }
+        }
+
+
+        return answer.stream().distinct().collect(Collectors.toList());
     }
 }
