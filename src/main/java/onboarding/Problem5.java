d37f5f1eee0e8943af85fe3e352baa63bb9644a4
민병욱 <woook27@naver.com>
2022-11-01 13:28:51 +0900

    feat: solution 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,6 +10,13 @@ public class Problem5 {
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
+        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < unit.length; i++) {
+            answer.add(exchange(money, unit[i]));
+            money = money % unit[i];
+        }
         return answer;
     }
 }
