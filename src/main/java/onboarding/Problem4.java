f4f3a8ff088eac4f8540b5cd7c48b6550f89dd24
rt3310 <btac3310@gmail.com>
2022-11-01 15:00:38 +0900

    refactor(Problem4): 알파벳 뒤집는 로직 메서드로 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,6 +5,33 @@ public class Problem4 {
         return reverseString(word);
     }
 
+    public static String reverseString(String word) {
+        String result = "";
+        for (char character : word.toCharArray()) {
+            result += reverseChar(character);
+        }
+        return result;
+    }
+
+    public static char reverseChar(char character) {
+        if (!isAlphabet(character)) {
+            return character;
+        }
+
+        if (Character.isUpperCase(character)) {
+            return reverseUpperCase(character);
+        }
+        return reverseLowerCase(character);
+    }
+
+    private static char reverseLowerCase(char character) {
+        return (char) ('z' + 'a' - character);
+    }
+
+    private static char reverseUpperCase(char character) {
+        return (char) ('Z' + 'A' - character);
+    }
+
     public static boolean isAlphabet(char character) {
         if (character >= 'A' && character <= 'Z') {
             return true;
@@ -15,24 +42,5 @@ public class Problem4 {
         }
 
         return false;
-    }
-
-    public static char reverseChar(char character) {
-        if (!isAlphabet(character)) {
-            return character;
-        }
-
-        if (Character.isUpperCase(character)) {
-            return (char) ('Z' + 'A' - character);
-        }
-        return (char) ('z' + 'a' - character);
-    }
-
-    public static String reverseString(String word) {
-        String result = "";
-        for (char character : word.toCharArray()) {
-            result += reverseChar(character);
-        }
-        return result;
     }
 }
