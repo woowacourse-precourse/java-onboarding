40db22d9b02c91c315c12aa960e24fc704361fcb
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-11-01 23:43:01 +0900

    Add: problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); i++){
+            char alphabet = word.charAt(i);
+
+            if(alphabet >=65 && alphabet <= 90){
+                answer += (char)(155 - alphabet);
+            }
+            else if(alphabet <= 122 && alphabet >= 97){
+                answer += (char)(219 - alphabet);
+            }
+            else answer += alphabet;
+        }
         return answer;
     }
 }
+
