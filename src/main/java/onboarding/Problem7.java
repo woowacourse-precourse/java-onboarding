f59435c646f65fc51c0e274045b1f714b0f7c6a4
이규승 <rpg8988@naver.com>
2022-11-01 22:04:02 +0900

    feat: 크루 멤버 친구 목록 초기화

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,4 +8,27 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static void friendsReset(String user, List<List<String>> friends, Map<String, Set<String>> relations, Set<String> newFriends) {
+        for (List<String> friend : friends) {
+            String from = friend.get(0);
+            String to = friend.get(1);
+
+            if (relations.containsKey(from)) {
+                relations.get(from).add(to);
+            } else {
+                relations.put(from, new HashSet<>(Arrays.asList(to)));
+            }
+            if (relations.containsKey(to)) {
+                relations.get(to).add(from);
+            } else {
+                relations.put(to, new HashSet<>(Arrays.asList(from)));
+            }
+
+            if (!from.equals(user) && !to.equals(user)) {
+                newFriends.add(from);
+                newFriends.add(to);
+            }
+        }
+    }
 }
