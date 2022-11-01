2aa393181fafe61aaf00fe2b2dd8d4afbaecf5bf
si hwan <pum005@naver.com>
2022-11-01 22:25:02 +0900

    feat (problem4) : 입력 문자를 예외처리 하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,16 @@
 package onboarding;
 
+import ExceptionValidation.P4ExceptionValidation;
+
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        if(!(P4ExceptionValidation.p4ExceptionValidation(word))){
+            return "";
+        }
     }
+
 }
