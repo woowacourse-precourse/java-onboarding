019293a59c3e4e583c8ca1493b149604aabae932
SeokyungKim <kskyung0624@gmail.com>
2022-11-01 15:46:46 +0900

    문제 5 - bill을 상수로 보낸다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,8 +5,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] bills = { 50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1 };
+
     public static List<Integer> solution(int money) {
-        int[] bills = { 50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1 };
         List<Integer> answer = new ArrayList<>();
 
         for (int bill : bills) {
