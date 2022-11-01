3e1fade4c3fd0eeea2f16a1edb62dc761b733e23
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 14:27:28 +0900

    feat: 문제 해결에 필요한 상수(Alphabet size) 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem4 {
+
+    private final static int THE_NUMBER_OF_ALPHABET = 26;
+
     public static String solution(String word) {
         String answer = "";
         return answer;
