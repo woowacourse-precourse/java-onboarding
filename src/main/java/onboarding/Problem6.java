5af05ba2ec1a0eed054f6a628fe2d0384a66849c
TaeHwan <kimta2hwan@gmail.com>
2022-11-01 16:39:00 +0900

    feat(PROBLEM6): 주어진 forms를 객체 Student로 초기화할 수 있는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+
+class Student {
+
+    String Email;
+    String NickName;
+
+    Student(String Email, String NickName) {
+        this.Email = Email;
+        this.NickName = NickName;
+    }
+}
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        List<Student> students = initStudents(forms);
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static List<Student> initStudents(List<List<String>> forms) {
+        List<Student> students = new ArrayList<>();
+
+        for (List<String> list : forms) {
+            students.add(new Student(list.get(0), list.get(1)));
+        }
+
+        return students;
     }
 }
