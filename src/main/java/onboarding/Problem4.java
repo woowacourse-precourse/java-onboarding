45b68591e1c1179755c051229435d63b1edc6c1c
ho2n <jungho2n@gmail.com>
2022-11-01 17:20:37 +0900

    참조할 알파벳 문자열등 상수로 정리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+    private static final String LOWER_ORDER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String LOWER_UNORDER = "zyxwvutsrqponmlkjihgfedcba";
+    private static final String UPPER_ORDER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String UPPER_UNORDER = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
     public static String solution(String word) {
         String answer = "";
         return answer;
