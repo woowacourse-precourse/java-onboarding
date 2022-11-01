a12d974bae8d94beac0783fcb0721165b44d67ff
kkh <kkho9654@naver.com>
2022-11-01 20:56:33 +0900

    Problem3 feature2

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -16,4 +16,16 @@ public class Problem3 {
 
         return answer;
     }
+
+    // i를 문자열로 변환 후 각 자리수에 3또는 6또는 9가 있는지 검사해서 각 자리수의 3,6,9개수 반환 : clapNum(int num)
+    public static int clapNum(int num){
+        String str = ""+num;
+        int ret = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '3' || str.charAt(i) == '6' || str.charAt(i) == '9'){
+                ret++;
+            }
+        }
+        return ret;
+    }
 }
