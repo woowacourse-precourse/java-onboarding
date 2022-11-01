8bc98efd04a51b378654cf54dc5191cfba7965b3
ghchoi <ghchoi0427@gmail.com>
2022-11-01 16:35:37 +0900

    feat: 유저의 친구를 리스트로 반환하는 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private List<String> getOnesFriends(List<List<String>> friends, String user) {
+        List<String> result = new ArrayList<>();
+        friends.stream().filter(e -> e.contains(user)).forEach(result::addAll);
+        return result.stream().filter(element -> !element.equals(user)).collect(Collectors.toList());
     }
 }
