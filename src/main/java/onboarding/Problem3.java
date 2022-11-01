5e934877d10949b5fae9f17ae4b8e682106fa010
gwj0421 <gwj0421@gmail.com>
2022-11-01 16:49:53 +0900

    문제3 - 문제 요구사항 정리 및 해결방법

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+    /*
+    - 문제 요구사항 정리 및 해결방법
+    1. 1부터 주어진 수까지 의 숫자 중 3, 6, 9가 몇번 사용 되는지 확인하고, 해당 횟수 리턴
+        - Stream을 활용하여 3, 6, 9의 빈도수를 구함
+     */
     public static int solution(int number) {
         int answer = 0;
         return answer;
