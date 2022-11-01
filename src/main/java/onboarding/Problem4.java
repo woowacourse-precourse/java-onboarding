325971308b65fdc6bfe639dce0b66a32ce54bdb9
rhoeunbin <rhoeb21@gmail.com>
2022-11-01 23:50:05 +0900

    solved problem 3,4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        
+        for (int n = 0; n < word.length(); n++) {
+            char c = word.charAt(n);
+            if (Character.isUpperCase(c)) {
+                c = (char) ('Z' - (c - 'A'));
+            }
+
+            if (Character.isLowerCase(c)) {
+                c = (char) ('z' - (c - 'a'));
+            }
+
+            answer += c;
+        }
         return answer;
     }
 }
