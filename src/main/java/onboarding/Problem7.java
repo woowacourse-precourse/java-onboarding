de0174a4108681a54eff8032324c630af77bb936
footdev <ekclstkfka5@gmail.com>
2022-11-01 22:55:22 +0900

    feat: 기능7 scoreBasedOnFriends 구현
    친구 관계 정보 friends에 대해서 추천 점수를 매기는 기능을 구현함.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,10 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    public static Map<String, Integer> score = new TreeMap<>();
+    public static Set<String> alreadyFriends = new HashSet<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         scoreBasedOnFriends(user, friends);
@@ -13,7 +14,23 @@ public class Problem7 {
     }
 
     public static void scoreBasedOnFriends(String user, List<List<String>> friends) {
+        for (int i = 0; i < friends.size(); i++) {
+            String friendA = friends.get(i).get(0);
+            String friendB = friends.get(i).get(1);
 
+            //a나b가 user 라면 그냥 넘어가야 한다.
+            if (friendA.equals(user) || friendB.equals(user)) {
+                continue;
+            }
+            //a나 b가 user와 친구라면
+            if (alreadyFriends.contains(friendA)) {
+                score.put(friendB, score.getOrDefault(friendB, 10) + 10);
+                continue;
+            }
+            if (alreadyFriends.contains(friendB)) {
+                score.put(friendA, score.getOrDefault(friendA, 10) + 10);
+            }
+        }
     }
 
     public static void scoreBasedOnVisitors(String user, List<String> visitors) {
