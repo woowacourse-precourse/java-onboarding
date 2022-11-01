3cc52947f7385bb8dec097e88a103ac0a95e7aed
yunjeong <uu0078@naver.com>
2022-11-01 22:48:55 +0900

    Problem4 1. word 의 각 문자 순회

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,6 +10,10 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+
+        }
         return answer;
     }
 }
