4127858a288433b022ff6ed651c3951483961a51
broden-horizon <rlghd1698@naver.com>
2022-11-01 21:26:16 +0900

    문제2. 스택을 사용해서 중복을 제거하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,43 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static Stack<Character> deleteDuplicateUsingStack(String str) {
+        final int LEN = str.length();
+        Stack<Character> stack = new Stack<>();
+        stack.push(str.charAt(0));
+        int index = 1;
+        char top = str.charAt(0);
+        boolean isDuplicate = false;
+        while(index < LEN) {
+            char current = str.charAt(index);
+            if(isDuplicate) {
+                if(current == top) {
+                    index++;
+                } else {
+                    stack.pop();
+                    top = stack.peek();
+                    isDuplicate = false;
+                }
+            } else {
+                if(current == top) {
+                    isDuplicate = true;
+                    index++;
+                    continue;
+                } else {
+                    stack.push(current);
+                    top = current;
+                    index++;
+                }
+            }
+
+        }
+        return stack;
     }
 }
