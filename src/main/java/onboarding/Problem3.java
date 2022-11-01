2dc554110562cdb217a9531e3146cb40912abda8
sun007021 <sun007021@gmail.com>
2022-11-01 11:18:15 +0900

    feat: 1부터 number까지 3,6,9 개수 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,11 +1,18 @@
 package onboarding;
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = Count369InAllNumber(number);
         return answer;
     }
-
-    private int CountContainDigit369(int number) {// 숫자에 3,6,9 몇개 있는지 확인
+    private static int Count369InAllNumber(int number){// 1부터 number까지 모든 숫자중 3,6,9 개수 확인
+        int count;
+        count = 0;
+        for(int i=1; i<=number; i++){
+            count+=CountContainDigit369(i);
+        }
+        return count;
+    }
+    private static int CountContainDigit369(int number) {// 숫자에 3,6,9 몇개 있는지 확인
         int lastnum;
         int temp;
         int count=0;
