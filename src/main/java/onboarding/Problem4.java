36201b1883660140e4656baa5f2fdcacf4a1c1de
Tae jeong Son <taejeongson@Taeui-MacBookAir.local>
2022-11-01 18:48:45 +0900

    [Feat] Problem4 알파벳 변환후 리턴

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,7 @@
 package onboarding;
 
 public class Problem4 {
-    int[] convert = {25,23,21,19,17,15,13,11,9,7,5,3,1,-1,-3,-5,-7,-9,-11,-13,-15,-17,-19,-21,-23,-25};
+    static int[] convert = {25,23,21,19,17,15,13,11,9,7,5,3,1,-1,-3,-5,-7,-9,-11,-13,-15,-17,-19,-21,-23,-25};
     public static boolean IsUpper(char c)
     {
         if (c >= 'A' && c <= 'Z')
@@ -23,7 +23,15 @@ public class Problem4 {
     public static String solution(String word) {
         String answer;
         char[] chars = word.toCharArray();
-
+        for (int i = 0; i < chars.length; i++)
+            if (IsAlpha(chars[i]))
+            {
+                if (IsUpper(chars[i]))
+                    chars[i] += convert[chars[i] - 'A'];
+                else
+                    chars[i] += convert[chars[i] - 'a'];
+            }
+        answer = String.valueOf(chars);
         return answer;
     }
 }
