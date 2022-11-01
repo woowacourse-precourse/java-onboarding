d303a3227e293435731158dc5b714f183f5b5845
sora_lee <chalrysr@naver.com>
2022-11-01 20:49:04 +0900

    [feat] Problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem3 {
+    /*
+        1부터 number까지 for문을 돌려 i라는 숫자에 3,6,9의 숫자가 포함되는지 while 반복문을 통해 자리수 별로 확인
+     */
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            int num = i;
+            while (num!=0) {
+                if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) {
+                    answer++;
+                }
+                num /= 10;
+            }
+        }
         return answer;
     }
 }
