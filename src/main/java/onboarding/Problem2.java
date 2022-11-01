00b6444bf01460554c99ec8fefe22f390b070cd6
WonJeongYeon <wonjungyoun@naver.com>
2022-11-01 11:16:43 +0900

    20221101 Problem2.java 불필요한 코드 삭제 및 중복 문자가 3자 이상일 때 발생하는 문제 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,17 +3,14 @@ package onboarding;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        String[] charList = answer.split("");
+        String answer;
+        String[] charList = cryptogram.split("");
         List<String> charArrayList = new ArrayList<>(Arrays.asList(charList));
-        if (!cryptogram.equals(cryptogram.toLowerCase())) {
-            throw new IllegalStateException("문자열은 소문자로만 이루어져 있어야 합니다.");
-        } else if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
-            throw new IllegalStateException("문자열의 길이는 1 이상, 1000 이하여야 합니다.");
-        } else {
+        checkStr(cryptogram);
             List<Integer> indexList = new ArrayList<>();
             do {
                 indexList.clear();
@@ -25,6 +22,7 @@ public class Problem2 {
                         }
                     }
                 }
+                indexList = indexList.stream().distinct().sorted().collect(Collectors.toList());
                 for (int i = indexList.size() - 1; i >= 0; i--) {
                     charArrayList.remove(Integer.parseInt(indexList.get(i).toString()));
                 }
@@ -32,10 +30,16 @@ public class Problem2 {
 
             answer = String.join("", charArrayList);
 
-            System.out.println("     cryptogram          result     ");
-            System.out.println("\"" + cryptogram + "\"" + "    " + "\"" + answer + "\"");
-        }
-
         return answer;
     }
+
+    public static void checkStr(String cryptogram) {
+        if (!cryptogram.equals(cryptogram.toLowerCase())) {
+            throw new IllegalStateException("문자열은 소문자로만 이루어져 있어야 합니다.");
+        } else if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            throw new IllegalStateException("문자열의 길이는 1 이상, 1000 이하여야 합니다.");
+        }
+    }
+
+
 }
