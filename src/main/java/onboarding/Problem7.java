2e739eeebe807d712e5c73d3a66b1f4d876238f9
heohyeonjink <hyeonjin4870@naver.com>
2022-11-01 15:54:14 +0900

    feat(PROBLEM7) : 친구 관계 정의하기
    해쉬맵을 이용하여 특정 사용자의 친구 리스트를 구현하였다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
 import java.util.Collections;
-import java.util.List;
-
+import java.util.*;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // 친구 관계를 표시할 HashMap
+        HashMap<String, ArrayList<String>> friendsMap = new HashMap<>();
+
+        for(List<String> tmp : friends){
+            makeFriendsMap(tmp, friendsMap);
+        }
+
         return answer;
     }
+
+    public static void makeFriendsMap(List<String> tmp, HashMap<String, ArrayList<String>> friendsMap){
+
+        String friend1 = tmp.get(0);
+        String friend2 = tmp.get(1);
+
+        if(friendsMap.containsKey(friend1)){
+            friendsMap.get(friend1).add(friend2);
+        }
+        else{
+            friendsMap.put(friend1, new ArrayList<>(Arrays.asList(friend2)));
+        }
+
+        if (friendsMap.containsKey(friend2)) {
+            friendsMap.get(friend2).add(friend1);
+        }
+        else{
+            friendsMap.put(friend2, new ArrayList<>(Arrays.asList(friend1)));
+        }
+
+    }
+
 }
