fec9a94d570739054c7461b2e04f0ca55d64165e
ymkim97 <rladudaud97@naver.com>
2022-11-01 13:09:42 +0900

    feat: add function count visited friends

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,17 +2,19 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
 
     static ArrayList<String> userFriend = new ArrayList<>();
+    static HashMap<String, Integer> recommendList = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
         getFriends(user, friends);
-
+        countVisited(visitors);
 
         return answer;
     }
@@ -32,6 +34,27 @@ public class Problem7 {
             if (friendsList.get(1).equals(user)) {
                 userFriend.add(friendsList.get(0));
             }
+        }
+
+    }
+
+    public static void countVisited(List<String> visitors) {
+        int index;
+        int score;
+
+        for(index = 0; index < visitors.size(); index++) {
+            if (userFriend.contains(visitors.get(index))){
+                continue;
+            }
+
+            if (recommendList.containsKey(visitors.get(index))) {
+                score = recommendList.get(visitors.get(index));
+                score += 1;
+                recommendList.put(visitors.get(index), score);
+                continue;
+            }
+
+            recommendList.put(visitors.get(index), 1);
         }
 
     }
