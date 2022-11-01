3f1e6333f76b5b4d8a3d705d7501fa27b8a2f118
MuHyeonSon <smuhyeon84@gmail.com>
2022-11-01 21:32:14 +0900

    Problem2 예외처리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,17 @@
 package onboarding;
 
 public class Problem2 {
+    // 기능 구현 목록
+    // 1. 예외처리 (cryptogram의 길이가 1이상 1000 이하인 문자열이 아닌 경우)
+    // 2. 문자열 해독 및 해독한 문자열 반환
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        //예외처리
+        // 1. cryptogram의 길이가 1이상 1000 이하인 문자열이 아닌 경우
+        if (1 > cryptogram.length() || 1000 < cryptogram.length()){
+            String e = "wrong input";
+            return e;
+        }
         return answer;
     }
 }
