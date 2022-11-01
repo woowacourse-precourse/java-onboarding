15d936f8ef9bdb33286ad289727ae2d44f0a4d27
areyouhun <areyouhun@gmail.com>
2022-11-01 16:36:20 +0900

    refactor(prob6): change the way of searching nicknames for duplicate letters

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,23 +12,27 @@ import java.util.Set;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         UserInfo userInfo = new UserInfo(forms);
-        DuplicateLettersFinder duplicateLettersFinder = new DuplicateLettersFinder(userInfo);
-        Set<String> duplicateLettersList = duplicateLettersFinder.duplicateLettersList;
+        List<String> userEmails = userInfo.getEmails();
+        List<String> userNicknames = userInfo.getNicknames();
 
-        ResultView resultView = new ResultView(duplicateLettersFinder);
-        Map<String, List<String>> answers = resultView.getSortedResults();
+        DuplicateLettersFinder duplicateLettersFinder = new DuplicateLettersFinder(userNicknames);
+        Set<String> nicknamesWithDuplicateLetters = duplicateLettersFinder.getNicknamesWithDuplicateLetters();
 
-        /*
-         * I think there could be several cases of adjacent duplicate letters depending on given nicknames.
-         * So if DuplicateLettersFinder found more than two cases, and you returned ANSWERS variable above🔺,
-         * you might get all the cases needed.
-         * But the given test case has only 'one case' (제이).
-         * So it has to return only one list from ANSWERS as below.
-         * [jason@email.com, jm@email.com, mj@email.com]
-         */
-        Iterator<String> iterator = duplicateLettersList.iterator();
-        String duplicateLetters = iterator.next(); // You can get only one case of duplicate letters from the given nicknames.
-        List<String> answer = answers.get(duplicateLetters);
-        return answer;
+
+//        ResultView resultView = new ResultView(duplicateLettersFinder);
+//        Map<String, List<String>> answers = resultView.getSortedResults();
+//
+//        /*
+//         * I think there could be several cases of adjacent duplicate letters depending on given nicknames.
+//         * So if DuplicateLettersFinder found more than two cases, and you returned ANSWERS variable above🔺,
+//         * you might get all the cases needed.
+//         * But the given test case has only 'one case' (제이).
+//         * So it has to return only one list from ANSWERS as below.
+//         * [jason@email.com, jm@email.com, mj@email.com]
+//         */
+//        Iterator<String> iterator = duplicateLettersList.iterator();
+//        String duplicateLetters = iterator.next(); // You can get only one case of duplicate letters from the given nicknames.
+//        List<String> answer = answers.get(duplicateLetters);
+//        return answer;
     }
 }
--- src/main/java/onboarding/feature6/DuplicateLettersFinder.java
+++ src/main/java/onboarding/feature6/DuplicateLettersFinder.java
@@ -2,69 +2,41 @@ package onboarding.feature6;
 
 import static onboarding.feature6.Constants.*;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class DuplicateLettersFinder {
-    private String duplicateLetters;
-    private String currentEmail;
-    private String nextEmail;
+    private List<String> nicknames;
+    private Set<String> nicknamesWithDuplicateLetters;
 
-    private List<String> userEmails;
-    private List<String> userNicknames;
-    private Map<String, Set<String>> result = new HashMap<>(); // {key: duplicate letters & value: filtered emails}
-    public Set<String> duplicateLettersList = new HashSet<>();
-
-    public DuplicateLettersFinder(UserInfo userInfo) {
-        userEmails = userInfo.getEmails();
-        userNicknames = userInfo.getNicknames();
+    public DuplicateLettersFinder(List<String> nicknames) {
+        this.nicknames = nicknames;
     }
 
-    public Map<String, Set<String>> findDuplicateLetters() {
-        for (int i = 0; i < userNicknames.size() - 1; i++) {
-            String currentNickname = userNicknames.get(i);
-            currentEmail = userEmails.get(i);
-            for (int k = i + 1; k < userNicknames.size(); k++) {
-                String nextNickname = userNicknames.get(k);
-                nextEmail = userEmails.get(k);
-                chooseNicknames(currentNickname, nextNickname);
+    public Set<String> getNicknamesWithDuplicateLetters() {
+        nicknamesWithDuplicateLetters = new HashSet<>();
+        for (int i = 0; i < nicknames.size() - 1; i++) {
+            String currentNickname = nicknames.get(i);
+            for (int j = i + 1; j < nicknames.size(); j++) {
+                String nextNickname = nicknames.get(j);
+                compareNicknames(currentNickname, nextNickname);
             }
         }
-        return result;
+        return nicknamesWithDuplicateLetters;
     }
 
-    public void chooseNicknames(String currentNickname, String nextNickname) {
-        for (int m = 0; m < currentNickname.length() - 1; m++) {
-            String lettersOfCurrentNickname = currentNickname.substring(m, m + INCREMENT);
-            for (int n = 0; n < nextNickname.length() - 1; n++) {
-                String lettersOfNextNickname = nextNickname.substring(n, n + INCREMENT);
-                compareLetters(lettersOfCurrentNickname, lettersOfNextNickname);
-            }
+    public void compareNicknames(String currentNickname, String nextNickname) {
+        for (int i = 0; i < currentNickname.length() - 1; i++) {
+            String lettersOfCurrentNickname = currentNickname.substring(i, i + INCREMENT);
+            checkExistenceOfLetters(currentNickname, nextNickname, lettersOfCurrentNickname);
         }
     }
 
-    public void compareLetters(String lettersOfCurrentNickname, String lettersOfNextNickname) {
-        if (lettersOfCurrentNickname.equals(lettersOfNextNickname)) {
-            duplicateLetters = lettersOfCurrentNickname;
-            duplicateLettersList.add(duplicateLetters);
-            updateResult();
+    public void checkExistenceOfLetters(String currentNickname, String nextNickname, String lettersOfCurrentNickname) {
+        if (nextNickname.contains(lettersOfCurrentNickname)) {
+            nicknamesWithDuplicateLetters.add(currentNickname);
+            nicknamesWithDuplicateLetters.add(nextNickname);
         }
-    }
-
-    public void updateResult() {
-        if (result.containsKey(duplicateLetters)) {
-            Set<String> existingFilteredEmails = result.get(duplicateLetters);
-            existingFilteredEmails.add(currentEmail);
-            existingFilteredEmails.add(nextEmail);
-            result.replace(duplicateLetters, existingFilteredEmails);
-            return;
-        }
-        Set<String> newFilteredEmails = new HashSet<>();
-        newFilteredEmails.add(currentEmail);
-        newFilteredEmails.add(nextEmail);
-        result.put(duplicateLetters, newFilteredEmails);
     }
 }
