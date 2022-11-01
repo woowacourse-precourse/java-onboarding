c1359f04bd0a60c82a9afb69c9f2a297c6354f50
yeeun <dpdms9621@naver.com>
2022-11-01 16:23:38 +0900

    [Problem2] 인접문자가 같으면 제거하는 기능메서드 호출

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        //제거 메서드 호출
+        String answer = removeString(cryptogram);
         return answer;
+    }
+
+    //인접문자가 같으면 제거하는 기능
+    public static String removeString(String cryptogram){
+        //추출할 StringBuilder 생성
+        StringBuilder sb = new StringBuilder();
+        //정답 변수
+        String word = "";
+        //인스턴스 한글자씩 쪼개기
+        String[] arr = cryptogram.split("");
+        //처음 글자수
+        int initSize = arr.length;
+        //sb에 저장되는 횟수
+        int count = 0;
+
+        return word;
     }
 }
