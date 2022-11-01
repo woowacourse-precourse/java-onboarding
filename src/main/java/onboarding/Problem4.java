b850ed00ea0fa82afe718484587a3631d7aece59
khj <bbb6254177@gmail.com>
2022-11-01 13:40:24 +0900

    refactor

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,12 +3,14 @@ package onboarding;
 import java.util.HashMap;
 
 public class Problem4 {
+    //엄마의 알파벳을 청개구리의 알파벳으로 매핑해주는 hashmap
     private static HashMap<Character,Character> map = new HashMap<Character,Character>(){{
         for(int i = 97; i<=122; i++){
             put((char)i, (char)(219-i));
         }
     }};
-
+    
+    //주어진 엄마의 알파벳을 청개구리의 알파벳으로 변환해 반환해주는 함수
     private static Character mapping(Character c){
         if (Character.isAlphabetic(c)){
             char mom = Character.toLowerCase(c);
@@ -21,6 +23,7 @@ public class Problem4 {
     public static String solution(String word) {
         StringBuffer ans = new StringBuffer();
 
+        //엄마 말씀 word를 알파벳 단위로 청개구리의 word로 변환한다.
         for(int i = 0; i<word.length(); i++){
             ans.append(mapping(word.charAt(i)));
         }
