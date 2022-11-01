865bd2f95273793821d12d8db9cc9b291c26637d
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 15:06:30 +0900

    feat : solution 메서드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -8,6 +8,12 @@ public class Problem5 {
     private static final int MIN = 1;
     private static final int MAX = 1000000;
 
+    public static List<Integer> solution(int money) {
+        if (!canMoney(money)) throw new IllegalArgumentException("제한 사항을 위반하였습니다.");
+        int divMoney[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        return countMoney(money, divMoney);
+
+    }
 
     public static List<Integer> countMoney(int money, int divMoney[]) {
         List<Integer> count = new ArrayList<>();
