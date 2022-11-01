b4a8d354dbe346bee04c3b2dd0e54d7576c3553e
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-01 14:26:53 +0900

    FIX : Q1 : Exception message 변경

--- src/main/java/onboarding/problem1/exception/InputOutOfSizeException.java
+++ src/main/java/onboarding/problem1/exception/InputOutOfSizeException.java
@@ -1,7 +1,7 @@
 package onboarding.problem1.exception;
 
 public class InputOutOfSizeException extends GameRuntimeException {
-    private static final String MESSAGE = "입력값이 초과되었습니다. 입력값은 2 이하여야 합니다.";
+    private static final String MESSAGE = "메시지의 길이가 기준에 부합하지 않습니다.";
 
     public InputOutOfSizeException() {
         super(MESSAGE);
