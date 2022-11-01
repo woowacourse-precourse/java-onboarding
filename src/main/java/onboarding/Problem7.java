718b6cf0790162fb02dd8b19de05db1e3ab2ba01
HASEUNGHEEE <ariul123@gmail.com>
2022-11-01 11:43:40 +0900

    feat: Problem7 사용자의 친구를 구하는 기능 구현
    사용자와 같은 리스트 내에 있으면서 다른 인덱스를 가지고 잇으면 사용자의 친구이다. 사용자의 친구를 찾아서 HashSet에
    담는다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
+    static Set<String> userFriends = new HashSet<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        getFriends(user, friends);
         return answer;
+    }
+
+    public static void getFriends(String user, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            if (friend.get(0) == user) {
+                userFriends.add(friend.get(1));
+            } else if (friend.get(1) == user) {
+                userFriends.add(friend.get(0));
+            }
+        }
     }
 }
