fa39d74e411d4a64f376894c44fd8a9d93b3effd
hyeon <vikim1210@naver.com>
2022-11-01 11:51:46 +0900

    문제 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            int a=i/10;
+            int b=i%10;
+            if (a==3||a==6||a==9) answer++;
+            if (b==3||b==6||b==9) answer++;
+        }
         return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(33));
     }
 }
