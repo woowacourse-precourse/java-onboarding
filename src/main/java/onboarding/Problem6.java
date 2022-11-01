807a762a411c488769c45cc66579ac5b92ee28c7
minju412 <ln8847@ajou.ac.kr>
2022-11-01 20:17:46 +0900

    comment: [p6] 불필요한 주석 제거

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -35,10 +35,6 @@ public class Problem6 {
 		return answer;
 	}
 
-	/**
-	 * 크루들의 이메일과 닉네임으로 map 을 초기화하는 메서드
-	 * @param forms
-	 */
 	private static Map<String, String> initMapWithEmailAndNicknames(List<List<String>> forms) {
 		Map<String, String> emailAndNicknames = new HashMap<>();
 		for (List<String> form : forms) {
@@ -47,12 +43,6 @@ public class Problem6 {
 		return emailAndNicknames;
 	}
 
-	/**
-	 * 사용이 제한되는 닉네임을 사용하는 크루들을 반환하는 메서드
-	 * (크루의 닉네임 안에 duplicateNicknames 의 요소가 포함되어 있으면 사용이 제한되는 닉네임이다.)
-	 * @param emailAndNicknames
-	 * @param duplicateNicknames
-	 */
 	private static List<String> getCrewUsingInvalidNickname(Map<String, String> emailAndNicknames,
 		Set<String> duplicateNicknames) {
 		Set<String> answer = new HashSet<>();
@@ -70,10 +60,6 @@ public class Problem6 {
 		return new ArrayList<>(answer);
 	}
 
-	/**
-	 * splitNicknames 트에서 중복을 찾아 duplicateNicknames 에 추가하는 메서드
-	 * @param splitNicknames
-	 */
 	private static Set<String> initDuplicateNicknames(List<String> splitNicknamesIntoTwoDigit) {
 		Set<String> duplicateNicknames = new HashSet<>();
 		for (String splitNickname : splitNicknamesIntoTwoDigit) {
@@ -85,10 +71,6 @@ public class Problem6 {
 		return duplicateNicknames;
 	}
 
-	/**
-	 * 닉네임을 두 글자씩 쪼개어 splitNicknames 에 추가하는 메서드
-	 * @param emailAndNicknames
-	 */
 	private static List<String> initSplitNicknamesIntoTwoDigit(Map<String, String> emailAndNicknames) {
 		List<String> splitNicknamesIntoTwoDigit = new ArrayList<>();
 		for (String nickname : emailAndNicknames.values()) {
@@ -99,11 +81,6 @@ public class Problem6 {
 		return splitNicknamesIntoTwoDigit;
 	}
 
-	/**
-	 * 제한사항을 위배했는지 체크하는 메서드
-	 * @param forms
-	 * @return
-	 */
 	private static Integer checkRestrictions(List<List<String>> forms) {
 		if (!isCrewRangeValid(forms)) {
 			return EXCEPTION;
@@ -117,7 +94,6 @@ public class Problem6 {
 		return 0;
 	}
 
-	// 크루의 수가 1명 이상 10,000명 이하가 아닌 경우 예외
 	private static boolean isCrewRangeValid(List<List<String>> forms) {
 		if (forms.size() < 1 || forms.size() > 10000) {
 			return false;
@@ -125,7 +101,6 @@ public class Problem6 {
 		return true;
 	}
 
-	// 이메일의 전체 길이가 11자 이상 20자 미만이 아닌 경우 예외
 	private static boolean isEmailRangeValid(List<List<String>> forms) {
 		for (List<String> form : forms) {
 			if (form.get(EMAIL).length() < 11 || form.get(EMAIL).length() >= 20) {
@@ -135,7 +110,6 @@ public class Problem6 {
 		return true;
 	}
 
-	// 닉네임의 전체 길이가 1자 이상 20자 미만이 아닌 경우 예외
 	private static boolean isNicknameRangeValid(List<List<String>> forms) {
 		for (List<String> form : forms) {
 			if (form.get(NICKNAME).length() < 1 || form.get(NICKNAME).length() >= 20) {
