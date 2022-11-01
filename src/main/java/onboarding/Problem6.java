c1533ac7e4675237c71a21482c492b9c450e7770
hyunseo <goatsfish@gmail.com>
2022-11-01 15:04:49 +0900

    feat(Problem6) : 두 글자 이상으로 닉네임 분할 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem6 {
+
+    private static Set<String> answer = new HashSet<>();
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        answer.clear();
     }
+
+    private static Set<String> getDistinctNicknamePieces(String nickname) {
+        Set<String> nicknamePieces = new HashSet<>();
+        IntStream.range(0, nickname.length() - 1)
+                .forEach(startPieceIndex ->
+                        nicknamePieces.addAll(createSubNicknamePieces(nickname, startPieceIndex))
+                );
+
+        return nicknamePieces;
+    }
+
+    private static Set<String> createSubNicknamePieces(String nickname, int startPieceIndex) {
+        return IntStream.range(startPieceIndex + 2, nickname.length() + 1)
+                .mapToObj(endPieceIndex -> nickname.substring(startPieceIndex, endPieceIndex))
+                .collect(Collectors.toSet());
+    }
+
 }
