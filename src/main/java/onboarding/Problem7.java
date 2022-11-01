5681b924aa21ae0011093e88a8f1ff28c7b9651c
seheonCho <whtpgjs94@gmail.com>
2022-11-01 17:43:03 +0900

    feat: [문제7] 매개변수로 제공되는 등장하는 모든 이름 수집 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        Set<String> allNames = getAllNames(friends, visitors, user);
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Set<String> getAllNames(List<List<String>> friends, List<String> visitor, String user) {
+        Set<String> allNames = new HashSet<>();
+
+        for (List<String> friend : friends) {
+            for (String name : friend) {
+                allNames.add(name);
+            }
+        }
+
+        for (String name : visitor) {
+            allNames.add(name);
+        }
+
+        allNames.add(user);
+
+        return allNames;
     }
 }
