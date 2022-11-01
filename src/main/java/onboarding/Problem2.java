5e298fb66cd0ef5f30c772012009b3ad5408e4b9
seung-hun2 <jungsh2328@naver.com>
2022-11-01 20:41:06 +0900

    2번 문제 - 연속된 문자가 같을 때 그 문자의 인덱스를 저장하는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,7 +8,27 @@ import java.util.List;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        StringBuilder sb = new StringBuilder(cryptogram);
+        while(true){
+            List<Integer> s = new ArrayList<>();
+            int[] arr = new int[sb.length()];
+            String str="";
+            int k=0;
+            if(sb.length() == 0){
+                break;
+            }
+            for(int i=1;i<sb.length();i++){
+                if(sb.charAt(i-1) == sb.charAt(i)){
+                    s.add(i);
+                    arr[i-1] = i;
+                    k++;
+                }
+            }
+            if(s.size()==0 || k==0){
+                break;
+            }
 
+        }
         return answer;
     }
 }
