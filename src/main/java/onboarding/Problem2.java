24bc198be29c0bfd62b08018c9262dd6ca335f36
민병욱 <woook27@naver.com>
2022-11-01 16:58:47 +0900

    feat: isDuplicate 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem2 {
+    public static boolean isDuplicate(String cryptogram) {
+        char prev = cryptogram.charAt(0);
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if(prev == cryptogram.charAt(i)) {
+                return true;
+            }
+            prev = cryptogram.charAt(i);
+        }
+        return false;
+    }
+
+
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
         return answer;
     }
 }
