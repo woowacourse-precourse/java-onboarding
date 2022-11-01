9029e06eb23d25a85fd26c3f9748508367c8f894
solesy <tkddbs9801@gmail.com>
2022-11-01 11:45:25 +0900

    refactor: Modify problem3 variable name

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,20 +3,20 @@ package onboarding;
 import java.util.Hashtable;
 
 public class Problem4 {
-    private Hashtable<Character,Character> ht;
+    private Hashtable<Character,Character> frogDictionaryHashtable;
     public Problem4(){
-        ht = new Hashtable<Character,Character>();
+        frogDictionaryHashtable = new Hashtable<Character,Character>();
         for(int i = 65, j = 25; i <= 90; ++i, j-=2)
-            ht.put((char)i, (char)(i+j));
+            frogDictionaryHashtable.put((char)i, (char)(i+j));
         for(int i = 97, j = 25; i <= 122; ++i, j-=2)
-            ht.put((char)i, (char)(i+j));
+            frogDictionaryHashtable.put((char)i, (char)(i+j));
     }
     private String frogChangeWord(final String word){
         String ret = "";
         for(int i = 0; i < word.length(); ++i) {
-            if(ht.get(word.charAt(i)) != null) {
-                ret += ht.get(word.charAt(i));
-            } else{
+            if(frogDictionaryHashtable.get(word.charAt(i)) != null) {
+                ret += frogDictionaryHashtable.get(word.charAt(i));
+            } else{ //알파벳 이외의 문자는 변환하지 않는다.
                 ret += word.charAt(i);
             }
         }
