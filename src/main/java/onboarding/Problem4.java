4edbd1de67efc22d88184bffa3a3c24c34497f47
kim <kajinori@gachon.ac.kr>
2022-11-01 23:49:02 +0900

    feat : Problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        StringBuilder answer = new StringBuilder();
+
+        for (char alphabet : word.toCharArray()) {
+            answer.append(reverseAlphabet(alphabet));
+        }
+        return answer.toString();
+    }
+    public static char reverseAlphabet(char alphabet) {
+        if ('A' <= alphabet && alphabet <= 'Z') {
+            return (char) ('Z' - alphabet + 'A');
+        } else if ('a' <= alphabet && alphabet <= 'z') {
+            return (char) ('z' - alphabet + 'a');
+        }
+        return alphabet;
     }
 }
