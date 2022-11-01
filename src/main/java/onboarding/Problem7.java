1be724273c4b489038023235c6cb568b2d927120
cocomozila <joseyun94@naver.com>
2022-11-01 15:01:03 +0900

    [문제 7] 전체 기능 호출

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    // 전체 기능 호출
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        HashMap<String, Integer> idToNumber = new HashMap<>();
+        HashMap<Integer, String> numberToId = new HashMap<>();
+        HashSet<String> allUserList = new HashSet<>();
+        HashSet<String> myFriendList = new HashSet<>();
+        List<Integer>[] friendGraph;
+
         return answer;
     }
 }
+
+
