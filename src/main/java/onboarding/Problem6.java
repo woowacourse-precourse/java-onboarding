7e293067dd2f320e99fb506a8ce05387e5ef4b87
BoyCho <iijokv@naver.com>
2022-11-01 13:18:57 +0900

    feat : 문제6. 닉네임 2글자 이상 같은 여부 확인 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<List<String>> prevL = new ArrayList<>(forms);
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
 
         while (true) {
             List<List<String>> nextL = new ArrayList<>();
@@ -17,7 +17,28 @@ public class Problem6 {
             for (int i = 1; i < prevL.size(); i++) {
                 String email = prevL.get(i).get(0);
                 String knick = prevL.get(i).get(1);
+
+                // 비교 대상 교육생의 넥네임이 2글자 이상 같은 경우, 결과 리스트에 삽입합니다.
+                if (findSameNick(cmpk, knick)) {
+                    isSameNick = true;
+                    answer.add(email);
+                }
+                else // 반대의 경우, nextL 리스트에 삽입합니다.
+                    nextL.add(new ArrayList<>(Arrays.asList(email, knick)));
             }
+            if (isSameNick) answer.add(cmpe);
+            if (nextL.size() <= 1) break;
+            prevL = nextL;
         }
+        return answer;
+    }
+    static boolean findSameNick (String a, String b) {
+        int len = a.length();
+
+        for (int i = 0; i < len - 1; i++) {
+            String s = a.substring(i, i + 2);
+            if (b.contains(s)) return true;
+        }
+        return false;
     }
 }
