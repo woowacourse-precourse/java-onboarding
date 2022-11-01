9f4fa10670b91364e723ce85ffc400b3530cb053
alswn <alswn9938@gmail.com>
2022-11-01 11:56:34 +0900

    [Q6] refactor : 상수 추가 및 함수 리팩토링

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,60 +4,68 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem6 {
-    public static Map<Integer, String> userMap;
-    public static Map<String, Integer> subStrIndexMap;
+    public static Map<Integer, String> userEmailMap;
+    public static Map<String, Integer> subStrUserIndexMap;
     public static List<Integer> duplicatedIndex;
+    public static int EMAIL_INDEX = 0;
+    public static int NICK_NAME_INDEX = 1;
 
 
     public static List<String> solution(List<List<String>> forms) {
-        initUserMap(forms);
-        initSubStrIndexMap();
-        initDuplicatedIndex();
-        for(int index = 0; index<forms.size(); index++){
-            findDuplicated(index, forms.get(index).get(1));
+        initUserEmailMap(forms);
+        initSubStrUserIndexMap();
+        initDuplicatedIndexList();
+
+        List<String> userInfo;
+        for (int userIndex = 0; userIndex < forms.size(); userIndex++) {
+            userInfo = forms.get(userIndex);
+            findDuplicated(userIndex, userInfo);
         }
+
         List<Integer> indexList = getDistinctIndexList();
         List<String> answer = getAnswerList(indexList);
         Collections.sort(answer);
         return answer;
     }
 
-    public static void initUserMap(List<List<String>> forms){
-        userMap = new HashMap<>();
-        for(int index = 0; index<forms.size(); index++){
-            userMap.put(index, forms.get(index).get(0));
-        }
+    public static void initSubStrUserIndexMap() {
+        subStrUserIndexMap = new HashMap<>();
     }
 
-    public static void initSubStrIndexMap(){
-        subStrIndexMap = new HashMap<>();
-    }
-
-    public static void initDuplicatedIndex(){
+    public static void initDuplicatedIndexList() {
         duplicatedIndex = new ArrayList<>();
     }
 
-    public static void findDuplicated(int index, String nickname){
-        for(int i=0; i<nickname.length() -1; i++){
-            String subStr = nickname.substring(i, i+2);
-            if(subStrIndexMap.containsKey(subStr)){
-                duplicatedIndex.add(index);
-                duplicatedIndex.add(subStrIndexMap.get(subStr));
-            }
-            else{
-                subStrIndexMap.put(subStr, index);
+    public static void initUserEmailMap(List<List<String>> forms) {
+        userEmailMap = new HashMap<>();
+        for (int userIndex = 0; userIndex < forms.size(); userIndex++) {
+            userEmailMap.put(userIndex, forms.get(userIndex).get(EMAIL_INDEX));
+        }
+    }
+
+    public static void findDuplicated(int userIndex, List<String> userInfo) {
+        String nickname = userInfo.get(NICK_NAME_INDEX);
+        String subStrKey;
+
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            subStrKey = nickname.substring(i, i + 2);
+            if (subStrUserIndexMap.containsKey(subStrKey)) {
+                Integer initUserIndex = subStrUserIndexMap.get(subStrKey);
+                duplicatedIndex.add(userIndex);
+                duplicatedIndex.add(initUserIndex);
+            } else {
+                subStrUserIndexMap.put(subStrKey, userIndex);
             }
         }
     }
 
-    public static List<Integer> getDistinctIndexList(){
+    public static List<Integer> getDistinctIndexList() {
         return duplicatedIndex.stream().distinct().collect(Collectors.toList());
     }
 
-    public static List<String> getAnswerList(List<Integer> indexList){
-        List<String> answer = new ArrayList<>();
-        indexList.stream().forEach(i->answer.add(userMap.get(i)));
-        return answer;
+    public static List<String> getAnswerList(List<Integer> indexList) {
+        List<String> emailsList = new ArrayList<>();
+        indexList.forEach(userIndex -> emailsList.add(userEmailMap.get(userIndex)));
+        return emailsList;
     }
-
 }
