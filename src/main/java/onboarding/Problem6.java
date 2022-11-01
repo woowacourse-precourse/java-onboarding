88d8cbe90584e125d1c263d00b15b45954b71aff
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-01 15:01:11 +0900

    CompareName 생성자 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,19 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+class CompareName {
+    List<String> splittedNames = new ArrayList<>();
+
+    CompareName(List<List<String>> nameList) {
+        String currentName = "";
+        for (int nameIndex = 0; nameIndex < nameList.size(); nameIndex++) {
+            currentName = nameList.get(nameIndex).get(1);
+            for (int subStringIndex = 0; subStringIndex < currentName.length() - 1; subStringIndex++) {
+                this.splittedNames.add(currentName.substring(subStringIndex, subStringIndex + 2));
+            }
+        }
+    }
+}
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
