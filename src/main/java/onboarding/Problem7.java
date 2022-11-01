9168e21da054bbbe3168232794ed69423fc2f495
chener <49267211+chanosong@users.noreply.github.com>
2022-11-01 13:58:19 +0900

    [Add] Prob7 function list
    + function list

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    // list for scoreboard
+    // func : calculate score by visit
+    // func : calculate score by checking friendship
+    // func : calculate score and return 5 recommends
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
