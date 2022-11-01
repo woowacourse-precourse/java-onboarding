6323029088e1b97e0d7a5f30610633a9b31c7050
jamangstangs <jamangsong@gmail.com>
2022-11-01 10:18:23 +0900

    feat: problem1(진행), isNotValid() -> 페이지의 값을 검증하는 메소드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        if(isNotValid(pobi, crong)) return -1;
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean isNotValid(List<Integer> pobi, List<Integer> crong) {
+        if(pobi.size() != 2 || crong.size() != 2) return true;
+        if(pobi.get(0) < 2 || pobi.get(0) > 400 || crong.get(0) < 2 && crong.get(1) > 399) return true;
+        if((pobi.get(1) - pobi.get(0)) != 1 || (crong.get(1) - crong.get(0)) != 1) return true;
+        return false;
     }
 }
