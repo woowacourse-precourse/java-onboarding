4ce3fecf70f937f3282652fe61e7b821937c044b
gomljo <gomljo0307@naver.com>
2022-11-01 14:21:16 +0900

    feat(Problem6.java): divideToken 함수 구현
    주어진 닉네임을 두 글자씩 나누어 집합으로 반환하는 divideToken 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+class TokenGroup{
+    List<String> divideToken(String nickName){
+        final int WINDOW_SIZE=2;
+        int nickNameLength=nickName.length()-1;
 
+        HashSet<String> token=new HashSet<>();
+        for(int position=0;position<nickNameLength;position++){
+            token.add(nickName.substring(position,position+WINDOW_SIZE));
+        }
+        return new ArrayList<>(token);
+    }
+}
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
