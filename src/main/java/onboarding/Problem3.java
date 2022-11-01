b6d93e1ebf5302933dc7a2ac24beee164d77e7e8
gudals113 <gudals113@gmail.com>
2022-11-01 16:00:04 +0900

    feat : Problem3 Solved
    feat : Problem3 Solved
    
    나머지 계산 이용해서 풀이

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int nowNum=1; nowNum<=number; nowNum++){
+            answer += countClap(nowNum);
+        }
         return answer;
+    }
+    public static int countClap(int number) {
+        int cnt = 0;
+        String strNum = Integer.toString(number);
+        for (int i=0; i<strNum.length(); i++) {
+            int ithNum = Integer.parseInt(strNum.substring(i,i+1));
+            if (ithNum != 0 && ithNum % 3 == 0 ) {
+                cnt += 1;
+            }
+        }
+        return cnt;
     }
 }
