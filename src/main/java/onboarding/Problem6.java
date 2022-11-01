164b85a1f6a53ef9627ee55033da456bd7a70aed
p <gustjr2332@naver.com>
2022-11-01 22:21:41 +0900

    feat(Problem6): cutTwoLettersEach() 신청한 닉네임을 2글자마다 자르기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
+
+    public static List<String> cutTwoLettersEach(String nickname){
+        List<String> cuttedletters = new ArrayList<>();
+
+        for(int i=0; i<nickname.length()-1; i++){
+            cuttedletters.add(nickname.substring(i,i+2));
+        }
+        return cuttedletters;
+    }
+
+
 }
