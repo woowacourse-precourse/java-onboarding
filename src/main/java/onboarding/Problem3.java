428b4a5711b6de8e829c9dea43e942ad14c8d2be
sylee <sy>
2022-11-01 22:49:38 +0900

    [Feat] : Problem3 변수, 함수 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        answer = getClapCount(number);
         return answer;
+    }
+
+    public static int getClapCount(int number) {
+
+        return -1;
+    }
+
+    public static int getNumberCount(String numberStr) {
+
+        return -1;
     }
 }
