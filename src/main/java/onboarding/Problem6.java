2e751ce753d4f230cf20ec1e2378594e6745e6e8
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 23:00:07 +0900

    chore:Problem6 주석 추가.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 
 public class Problem6 {
 
+    // 비교할 닉네임 하나의 문자열을 2글자로 쪼개는 메서드
     public static List<String> splitNickName(String nickname) {
         List<String> nicknameSet = new ArrayList<String>();
         for(int i = 0; i < nickname.length()-1; i++) {
@@ -18,6 +19,7 @@ public class Problem6 {
         return nicknameSet;
     }
 
+    // 쪼갠 문자열들과 하나의 닉네임을 비교하여 공통된 문자열이 있는지 확인하는 메서드
     public static boolean compareElement (List<String> nicknameSet, String nickname) {
         for (String nicknameElement: nicknameSet) {
             if (nickname.contains(nicknameElement)) return true;
@@ -25,10 +27,13 @@ public class Problem6 {
         return false;
     }
 
+    // 하나의 문자열을 쪼개서 모든 닉네임 문자열과 비교하는 메서드
     public static HashSet<String> addEmail(List<List<String>> forms) {
         HashSet<String> correctEmail = new HashSet<>();
         for (int j = 0; j < forms.size()-1; j++) {
+
             int prevention = 0;
+
             for (int k = j + 1; k < forms.size(); k++) {
                 if (prevention < 1 && compareElement(splitNickName(forms.get(j).get(1)), forms.get(k).get(1))) {
                     correctEmail.add(forms.get(j).get(0));
@@ -42,6 +47,7 @@ public class Problem6 {
         return correctEmail;
     }
 
+    // 중복된 닉네임을 제거하는 메서드
     public static List<String> removeDuplicate (HashSet<String> correctEmail) {
         List<String> answerEmail = new ArrayList<>(correctEmail);
         Collections.sort(answerEmail);
