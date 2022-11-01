8d7f07c86c8f562e7e9958707912f3ec15e3ccd0
Vector1331 <tpgus4796@gmail.com>
2022-11-01 17:17:59 +0900

    [Feat] problem 5 -solved

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        //List<Integer> answer = Collections.emptyList(); // unsupport operation 예외-> Empty list 가 빈 list 사용할 목적임
+        List<Integer> answer = new ArrayList<>();
+        int change[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i = 0; i < change.length; i++) {
+            Integer a = money/change[i];
+            answer.add(a);
+            money %= change[i];
+        }
         return answer;
     }
 }
