77a4a71de5c9a443f3c0c71881c59e81b569f19e
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:34:38 +0900

    Problem4 solution method
    Goes through string and replaces each letter according to the provided dictionary. The replacement is case-sensitive.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,16 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] wordArray = word.toCharArray();
+        for (int i = 0; i < wordArray.length; i++) {
+            if (Character.isLetter(wordArray[i])) {
+                if (Character.isUpperCase(wordArray[i])) {
+                    wordArray[i] = (char)(155 - (int)wordArray[i]);
+                } else {
+                    wordArray[i] = (char)(219 - (int)wordArray[i]);
+                }
+            }
+        }
+        return String.valueOf(wordArray);
     }
 }
