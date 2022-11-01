a482f9fb28de955ce56fc57434874edf79ea1de6
myungjunlee <dlaudwns789@gmail.com>
2022-11-01 22:23:01 +0900

    feat(Problem7): 사용자와 함께 아는 친구 점수 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -34,12 +34,30 @@ public class Problem7 {
         return friendMap;
     }
 
+    private static HashMap<String, Integer> scoreFriendsOfFriends(String user, HashMap<String, List<String>> friendMap) {
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+        List<String> userFriendList = friendMap.get(user);
+        List<String> friendOfFriendList;
+
+        for (String userFriend : userFriendList) {
+            friendOfFriendList = friendMap.get(userFriend);
+
+            for (String friendOfFriend : friendOfFriendList) {
+                scoreMap.put(friendOfFriend, scoreMap.getOrDefault(friendOfFriend, 0) + 10);
+            }
+        }
+
+        return scoreMap;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         HashMap<String, List<String>> friendMap;
         HashMap<String, Integer> scoreMap;
 
         friendMap = createFriendMap(friends);
+
+        scoreMap = scoreFriendsOfFriends(user, friendMap);
 
         return answer;
     }
