30e2dc15f9adf157d04a8887ca261baa7656cca5
이건회 <rg970604@naver.com>
2022-11-01 20:24:57 +0900

    feat(Problem3.java) : 상수 선언

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,9 @@
 package onboarding;
 
 public class Problem3 {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 10000;
+    private static final String[] CLAPNUMS = {"3", "6", "9"};
     public static int solution(int number) {
         int answer = 0;
         return answer;
