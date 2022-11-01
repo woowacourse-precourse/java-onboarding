33eb930e1e3a22ca5130982bbf501f031ce17d16
imsehwan <dla612@naver.com>
2022-11-01 14:07:21 +0900

    fix : 문제 7번 주석처리

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -28,40 +28,41 @@ public class Problem7 {
     }
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        init(friends);
+//        init(friends);
         List<String> answer = new ArrayList<>();
-
-        // 아는 친구 점수 ++
-        makeGraph(friends);
-        List<String> list = graph.get(user);
-        for (String s : list) {
-            List<String> list1 = graph.get(s);
-            for (String s1 : list1) {
-                hashmap.put(s1, hashmap.getOrDefault(s1, 0) + 10);
-            }
-        }
-
-        // 방문자 점수 ++
-        for (String visitor : visitors) {
-            visit(visitor);
-        }
-
-        for (Map.Entry<String, Integer> entry : hashmap.entrySet()) {
-            answer.add(entry.getKey());
-        }
-
-        Collections.sort(answer, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (hashmap.get(o1) == hashmap.get(o2)) {
-                    return o1.compareTo(o2);
-                } else {
-                    return hashmap.get(o2) - hashmap.get(o1);
-                }
-            }
-        });
-
-
-        return answer.stream().filter(i->i!=user).collect(Collectors.toList()).subList(0,3);
+//
+//        // 아는 친구 점수 ++
+//        makeGraph(friends);
+//        List<String> list = graph.get(user);
+//        for (String s : list) {
+//            List<String> list1 = graph.get(s);
+//            for (String s1 : list1) {
+//                hashmap.put(s1, hashmap.getOrDefault(s1, 0) + 10);
+//            }
+//        }
+//
+//        // 방문자 점수 ++
+//        for (String visitor : visitors) {
+//            visit(visitor);
+//        }
+//
+//        for (Map.Entry<String, Integer> entry : hashmap.entrySet()) {
+//            answer.add(entry.getKey());
+//        }
+//
+//        Collections.sort(answer, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                if (hashmap.get(o1) == hashmap.get(o2)) {
+//                    return o1.compareTo(o2);
+//                } else {
+//                    return hashmap.get(o2) - hashmap.get(o1);
+//                }
+//            }
+//        });
+//
+//
+//        return answer.stream().filter(i->i!=user).collect(Collectors.toList()).subList(0,3);
+        return answer;
     }
 }
