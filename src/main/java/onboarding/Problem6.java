73dab6edcac18ce1da3784cd6652a6cc06bfd059
gomdol <oioifor@naver.com>
2022-11-01 22:22:17 +0900

    feat(Problem6): 중복된 닉네임 찾기 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> result = new ArrayList<>();
+        Map<String, String> userMap = new HashMap<>();
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String name = form.get(1);
+
+            for (int i = 0; i < name.length() - 1; i++) {
+                String subName = name.substring(i, i+2);
+                if(userMap.containsKey(subName)){
+                    if(!((userMap.get(subName)) == email)){
+                        result.add((userMap.get(subName)));
+                        result.add(email);
+                    }
+                }
+                userMap.put(subName,email);
+            }
+        }
+        List<String> answer = new ArrayList<>(result);
+        Collections.sort(answer);
         return answer;
     }
 }
