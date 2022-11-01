f805782c736e4ffc2c27f43aa43cfde54af93ffc
p <gustjr2332@naver.com>
2022-11-01 22:36:32 +0900

    feat(Problem7): findMyFriend() 나의 친구를 찾기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
+
+    public static String findMyFriend( String myname, List<String> friends){
+        String userfriends = "";
+
+        if(friends.contains(myname)){
+            if(friends.get(0).equals(myname)) {
+                userfriends= friends.get(1);
+            }
+            else if(friends.get(1).equals(myname)){
+                userfriends = (friends.get(0));
+            }
+        }
+
+        return userfriends;
+    }
+
 }
+
