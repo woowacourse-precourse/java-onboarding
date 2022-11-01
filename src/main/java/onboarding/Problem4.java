abe35b547fbb71939d59f76d8d8e4337e296b445
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-11-01 21:13:22 +0900

    feat: add util class for string and list

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+class StrAndLstPro4 {
+
+  public static List<Character> stringToList(String string) {
+    List<Character> list = new ArrayList<>();
+    for (int i = 0; i < string.length(); i++) {
+      list.add(string.charAt(i));
+    }
+    return list;
+  }
+
+  public static String listToString(List<Character> list) {
+    StringBuilder build = new StringBuilder();
+    for (Character character : list) {
+      build.append(character);
+    }
+    return (build.toString());
+  }
+}
 
 public class Problem4 {
 
