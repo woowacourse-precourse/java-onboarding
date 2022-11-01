33e953284b9e9ba1f3580c0de8b867b7a957af24
choiejae <ejae8319@gmail.com>
2022-11-01 17:37:19 +0900

    feat (getAnswer): 최소한의 화폐 단위로 money를 구성하는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 public class Problem5 {
     private static final List<Integer> MONETARY_UNITS = List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1);
     private static final int MIN_NUMBER = 1;
@@ -10,9 +10,9 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         validityChecker(money);
 
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getAnswer(money);
     }
+
     private static void validityChecker(int money) {
         if (!isValidateMoney(money)) {
             throw new IllegalArgumentException("Not valid money");
@@ -20,5 +20,13 @@ public class Problem5 {
     }
     private static boolean isValidateMoney(int money) {
         return MIN_NUMBER <= money && money <= MAX_NUMBER;
+    }
+    private static List<Integer> getAnswer(int money) {
+        List<Integer> answer = new ArrayList<>();
+        for (int monetaryUnit : MONETARY_UNITS) {
+            answer.add(money / monetaryUnit);
+            money %= monetaryUnit;
+        }
+        return answer;
     }
 }
