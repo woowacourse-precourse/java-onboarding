fed05185413a2830389ac8183cf18d706f6f17e7
pyj <les85611@hanyang.ac.kr>
2022-11-01 20:50:45 +0900

    [p1] pobi와 crong의 더하기, 곱하기, 점수가 될 가장 큰 값의 변수를 선언

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,43 +15,7 @@ class Problem1 {
         int crong_max = 0;
 
 
-        List<Integer> scoreArr;
-        scoreArr = new ArrayList<>();
 
-        // pobi와 crong의 왼쪽과 오른쪽 페이지의 차이가 1이상이면 -1
-        if((Math.abs(pobi.get(0) - pobi.get(1))>1 ||(Math.abs(crong.get(0) - crong.get(1)) > 1))) {
-            answer = -1;
-        } else {
-            for (Integer score1 :pobi) {
-                while (score1 != 0) {
-                    pobi_sum += score1 % 10;
-                    pobi_mul *= score1 % 10;
-                    pobi_sum /= 10;
-                    pobi_mul /= 10;
-                }
-                pobi_max = Math.max(pobi_sum, pobi_mul);
-                scoreArr.add(pobi_max);
-            }
-            for (Integer score2:crong) {
-                while (score2 != 0) {
-                    crong_sum += score2 % 10;
-                    crong_mul *= score2 % 10;
-                    crong_sum /= 10;
-                    crong_mul /= 10;
-                }
-                crong_max = Math.max(crong_sum, crong_mul);
-                scoreArr.add(crong_max);
-            }
-
-            if (scoreArr.get(0) > scoreArr.get(1)) {
-                answer = 1;
-            } else if (scoreArr.get(0) < scoreArr.get(1)) {
-                answer = 2;
-            } else {
-                answer = 0;
-            }
-
-        }
 
 
         return answer;
