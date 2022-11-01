54f5cc827e182ed7b6a4061c1f61f1baf3ce8439
sangmins930 <sangmins930@gmail.com>
2022-11-01 21:23:01 +0900

    Problem4 solution upload

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char upper = 'A' + 'Z';
+        char lower = 'a' + 'z';
+        int length = word.length();
+        for (int i = 0; i < length; i++) {
+            char ch = word.charAt(i);
+            if (Character.isUpperCase(ch)) answer += (char) (upper - ch);
+            else if (Character.isLowerCase(ch)) answer += (char) (lower - ch);
+            else answer += ch;
+        }
         return answer;
     }
 }
