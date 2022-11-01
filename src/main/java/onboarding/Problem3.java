33175247680012e1385256c9a808655d6fa088db
hyee0715 <hyee0715@naver.com>
2022-11-01 16:46:18 +0900

    feat(Problem3.java): 조건 검증 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,17 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int countNumber(int num) {
+        String strNum = Integer.toString(num);
+        int count = 0;
+
+        for (int i = 0; i < strNum.length(); i++) {
+            if (strNum.charAt(i) == '3' || strNum.charAt(i) == '6' || strNum.charAt(i) == '9') {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
