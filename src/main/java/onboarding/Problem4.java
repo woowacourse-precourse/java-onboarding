675bbc58efcfcd096f93f68228f764a672a8b9ad
donghyun <9703l8@AL02038907.local>
2022-11-01 22:55:01 +0900

    four-one

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,11 +5,18 @@ import java.util.Stack;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] chArr = new char[word.length()];
+
         for (int i = 0; i < word.length(); i++) {
             int c = word.charAt(i);
+            if ('A' <= c && c <= 'Z') {
+                chArr[i] = (char) ('Z' - (c - 'A'));
+            } else if ('a' <= c && c <= 'z') {
+                chArr[i] = (char) ('z' - (c - 'a'));
+            } else {
+                chArr[i] = (char) c;
+            }
         }
-
-
         return answer;
     }
 }
