3c4baf469a4d35550c424b4f672fdbbbefab94c6
CJY-kor <vivo_reprise@naver.com>
2022-11-01 22:40:48 +0900

    DOC(problem5) : 기능 목록 i), ii), iii), iv) 작성함

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,3 +1,11 @@
+/*
+기능 목록
+i) checkRedupli -> 두 String을 입력 받아 서로 중복인지 체크하여 중복이라면 true, 아니라면 false 반환한다.
+ii) makeIndexList -> forms를 입력 받아 i)를 이용하여 중복이 아닌 index와 중복인 index를 기록한 indexlist를 반환한다.
+iii) makeEmailList -> forms를 입력 받고 ii)를 이용하여 최종적으로 출력할 email 목록들을 list 형태로 반환한다.
+iv) solution -> iii)을 이용하여 정답을 반환한다.
+ */
+
 package onboarding;
 
 import java.util.List;
