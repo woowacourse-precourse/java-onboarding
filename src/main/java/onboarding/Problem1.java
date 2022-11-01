f5273c39a42917ed7c7f3f66f93be993b2322052
dingding-21 <phj8221@naver.com>
2022-11-01 22:39:11 +0900

    feat: Problem1 유효한 페이지 번호인지 확인하는 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,24 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int maxPageNum = 400;
+    private static final int minPageNum = 1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean isExceptionCase(List<Integer> list) {
+        int leftPage = list.get(0);
+        int rightPage = list.get(1);
+
+        if(leftPage > rightPage) return false;
+        if(leftPage < minPageNum || leftPage > maxPageNum) return false;
+        if(rightPage > maxPageNum) return false;
+        if(rightPage - leftPage != 1) return false;
+        if(leftPage % 2 != 1) return false;
+
+        return true;
     }
 }
