7f172f18cad787d20fbe1e94eae2f3046fc1e06c
ParkRootSeok <g_e_un@naver.com>
2022-11-01 23:34:50 +0900

    docs : Problem7 Function List 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -7,11 +7,24 @@ import java.util.List;
 
 public class Problem7 {
 
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+    public static List<String> recommendFriends(String user, List<List<String>> friends, List<String> visitors) {
+
     }
 
+    private static List<String> makeFriendList(String user, List<List<String>> friends, List<String> visitors) {
+
+    }
+
+    public static HashMap<String, Integer> calculateScore(List<List<String>> friends, List<String> visitors, List<String> friendList) {
+
+    }
+
+    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+        List<String> answer = Collections.emptyList();
+        return answer;
+
+    }
 
 
 }
