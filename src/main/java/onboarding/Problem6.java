8bdb58122f8971b6ae2b4358589cba6a2240c71b
LJW25 <dbjwl@g.skku.edu>
2022-11-01 17:15:21 +0900

    p6. define functions

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,34 @@
 package onboarding;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Problem6 {
+
+    private static List<String> splitNickname(String nickname){
+        List<String> splitedList = new ArrayList<>();
+
+        return splitedList;
+    }
+
+
+
+    private static boolean checkDuplicate(String names, List<String> splitedList){
+        boolean isDuplicate = false;
+
+        return isDuplicate;
+    }
+
+
+
+
     public static List<String> solution(List<List<String>> forms) {
+
+        String names = "";
+
+
         List<String> answer = List.of("answer");
+
         return answer;
     }
 }
