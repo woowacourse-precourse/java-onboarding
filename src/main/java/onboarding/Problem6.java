aef1b4f08e2ac2e286ad5b9bf801c518b2c154ec
ingpyo <juninpyo72@gmail.com>
2022-11-01 12:29:56 +0900

    [problem6]중복 사전 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = new ArrayList<>();
+		Map<String, Integer> map = new HashMap<>();
+		for (List<String> list : forms) {
+			String id = list.get(1);
+			for (int i = 0; i < id.length() - 1; i++) {
+				String piece = id.substring(i, i + 2);
+				map.put(piece, map.getOrDefault(piece, 0) + 1);
+			}
+		}
+
+	}
+
+
 }
