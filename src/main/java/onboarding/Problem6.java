9915d087e73542cf3e82b81cc9d05b17c03d8dda
yejin130 <jinis2023@gmail.com>
2022-11-01 21:17:37 +0900

    rename: problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,24 +6,25 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        HashMap<String, String> map = new HashMap<>();
-        HashSet<String> set = new HashSet<String>();
+        HashMap<String, String> map = new HashMap<>(); //similarName 을 가진 지원자의 map (key: 닉네임, value: 이메일)
+        HashSet<String> emailSet = new HashSet<String>(); //similarName 을 가진 지원자의 set (이메일)
 
-        for (int i = 0; i < forms.size(); i++) { // forms[i][1].substring(j,j+2)를 전체랑 비교.
-            for (int j = 0; j < forms.size(); j++) {
-                for (int k = 0; k < forms.get(i).get(1).length() - 1; k++) {
-                    for (int l = 0; l < forms.get(j).get(1).length() - 1; l++) {
+        //forms[i][1].substring(j,j+2)를 전체랑 비교.
+        for (int i = 0; i < forms.size(); i++) { // 현재 닉네임
+            for (int j = 0; j < forms.size(); j++) { // 현재 닉네임과 비교될 전체 닉네임
+                for (int k = 0; k < forms.get(i).get(1).length() - 1; k++) { //현재 닉네임 - 한글자씩
+                    for (int l = 0; l < forms.get(j).get(1).length() - 1; l++) { //비교될 닉네임 - 한글자씩
                         if (i != j) {
 
-                            boolean a = forms.get(i).get(1).substring(k, k + 2).charAt(0) == forms.get(j).get(1).substring(l, l + 2)
+                            boolean firstEquals = forms.get(i).get(1).substring(k, k + 2).charAt(0) == forms.get(j).get(1).substring(l, l + 2)
                                     .charAt(0);
-                            boolean b = forms.get(i).get(1).substring(k, k + 2).charAt(1) == forms.get(j).get(1).substring(l, l + 2)
+                            boolean secondEquals = forms.get(i).get(1).substring(k, k + 2).charAt(1) == forms.get(j).get(1).substring(l, l + 2)
                                     .charAt(1);
-                            if (a && b) {
+                            if (firstEquals && secondEquals) { // 글자 두개가 같을 때
                                 map.put(forms.get(i).get(1), forms.get(i).get(0));
                                 map.put(forms.get(j).get(1), forms.get(j).get(0));
-                                set.add(forms.get(i).get(1));
-                                set.add(forms.get(j).get(1));
+                                emailSet.add(forms.get(i).get(1));
+                                emailSet.add(forms.get(j).get(1));
                             }
                         }
                     }
@@ -31,7 +32,7 @@ public class Problem6 {
             }
         }
 
-        Iterator itr = set.iterator();
+        Iterator itr = emailSet.iterator();
         while(itr.hasNext()) {
             answer.add(map.get((String) itr.next()));
         }
