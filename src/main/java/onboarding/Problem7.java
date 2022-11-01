8dcaa3f5ad6ae132126af24f0653af4be48ab2ee
limzzum <chfhddl1234@naver.com>
2022-11-01 14:10:32 +0900

    [문제 7] 친구별로 추천점수를 저장할 scoreCard, 각 친구리스트를 저장할 friendList 생성.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,14 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static Map<String, Integer> scoreCard = new HashMap<>();
+    static HashMap<String, Set<String>> friendList = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+        List<String> answer = List.of();
         return answer;
     }
 }
