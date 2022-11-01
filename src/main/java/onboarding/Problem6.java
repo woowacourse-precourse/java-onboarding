ca01124beb4ff130de9f030f895aa811b09237f7
c1ce <chalicenuclear@gmail.com>
2022-11-01 09:31:01 +0900

    1101_2 success

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = nickCheck(forms);
+        Collections.sort(answer);
         return answer;
     }
     public static List<String> nickCheck(List<List<String>> forms){
