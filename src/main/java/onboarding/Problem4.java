43d378887a3764c1d1d21e4a62e495761f5268a4
moonchaewon <codnjs8989@gmail.com>
2022-11-01 18:28:42 +0900

    feat(Problem4): add function to reverse word

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,8 +3,21 @@ package onboarding;
 public class Problem4 {
     /* 기능 단위
      * 1. 소문자인지, 대문자인지 확인 (isLowerCase, isUpperCase)
-     * 2. 문자를 변환하는 기능
+     * 2. 문자를 변환하는 기능 (reverseWord)
      * */
+
+    private static String reverseWord(char word) {
+        int n = (int) word;
+        if(isLowerCase(n)) {
+            n = 90 - (n - 65);
+            return String.valueOf((char) n);
+        } else if (isUpperCase(n)) {
+            n = 122 - (n - 97);
+            return String.valueOf((char) n);
+        }
+        return Character.toString(word);
+    }
+
     private static boolean isLowerCase(int n) {
         return n >= 65 && n <= 90;
     }
@@ -17,6 +30,7 @@ public class Problem4 {
         String[] wordArray = word.split("");
         for(int i=0; i < word.length(); i++) {
             char ch = word.charAt(i);
+            wordArray[i] = reverseWord(ch);
         }
         answer = String.join("", wordArray);
         System.out.println("answer: " + answer);
