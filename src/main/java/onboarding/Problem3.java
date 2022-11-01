00c163c6256195b612a1dc7cc72f5be20fc14279
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 14:17:40 +0900

    feat: 숫자가 3 or 6 or 9 인지 확인

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,8 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static boolean is369Number(int number) {
+        return number == 3 || number == 6 || number == 9;
+    }
 }
