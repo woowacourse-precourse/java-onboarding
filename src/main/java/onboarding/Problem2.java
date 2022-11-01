09057302c3cec008fa427ea96227e2044c4381f5
csb9427 <csb9427@naver.com>
2022-11-01 11:30:09 +0900

    feat: Problem2 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
-
 public class Problem2 {
+    public static int index(String cryptogramSubstring) {
+        char cryptogramSubchar = cryptogramSubstring.charAt(0);
+        for (int i = 1; i < cryptogramSubstring.length(); i++) {
+            if (cryptogramSubstring.charAt(i) == cryptogramSubchar) {
+                return (i);
+            }
+        }
+        return 0;
+    }
+    public static String deleteChar(String cryptogram){
+        String result = "";
+        int lastIndex = cryptogram.length() - 1;
+        for (int i = 0; i < lastIndex + 1; i++){
+            if (i != lastIndex && cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                i += index(cryptogram.substring(i));
+                continue;
+            }
+            result += cryptogram.charAt(i);
+        }
+        return result;
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        String deleteString = "";
+        while (true) {
+            deleteString = deleteChar(answer);
+            if (!deleteString.equals(answer)){
+                answer = deleteString;
+            } else {
+                break;
+            }
+        }
         return answer;
     }
 }
