be8e26c2ebad6d3e34113b0d8cd9db3a017c9ffa
eunseo <qnrqjrm03@naver.com>
2022-11-01 19:20:48 +0900

    docs : problem1 기능목록 정리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,16 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * 기능목록
+ * 1. 리스트의 값들이 1초과 400미만 값이 아니거나
+ * 첫번째 값이 홀수가 아니고, 두번째 값이 짝수가 아니거나,
+ * 서로 하나 이상 차이나면 예외를 터트리는 기능
+ * 2. 리스트의 첫번째, 두번째의 각 자릿수들를 더하는 기능
+ * 3. 리스트의 첫번째, 두번째의 각 자릿수들를 곱하는 기능
+ * 4. 2번과 3번의 값들 중 큰 값을 구하는 기능
+ * 5. pobi의 리턴값이 크면 1, crong의 리턴값이 크면 2, 같으면 0을 리턴하는 기능
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
