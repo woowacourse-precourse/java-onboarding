28702c38e730b665613300312ce0307ddad12bc7
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 21:07:28 +0900

    feat(problem1): 페이지 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,28 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!isValidePages(pobi) || !isValidePages(crong)) {
+            return -1;
+        };
+        return 0;
     }
+
+    private static boolean isValidePages(List<Integer> pages) {
+        if (pages.size() != 2) {
+            return false;
+        }
+        if (!(isLeftPage(pages) && isNextPage(pages))) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isLeftPage(List<Integer> pages) {
+        return pages.get(0) % 2 == 1;
+    }
+
+    private static boolean isNextPage(List<Integer> pages) {
+        return pages.get(0) + 1 == pages.get(1);
+    }
+
 }
