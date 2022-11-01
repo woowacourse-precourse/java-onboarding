7197de25d21de1cd1169d9da891d581eea080913
juno-junho <ssmm0205@naver.com>
2022-11-01 20:34:27 +0900

    feat(problem6): create crew class
    리펙토링을 위해 email과 nickname을 갖는 crew 클래스 생성

---
+++ src/main/java/onboarding/problem6/Crew.java
@@ -0,0 +1,20 @@
+package onboarding.problem6;
+
+public class Crew {
+
+    private final String email;
+    private final String nickname;
+
+    public Crew(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
