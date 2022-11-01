85dc10fffa6d9c585e83ac254af658d0037943d1
JeyunSong <jyunart@naver.com>
2022-11-01 15:47:18 +0900

    Problem 5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        int[] array = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>(9);
+
+
+        for(int i=0 ; i<9 ; i++) {
+            if(money/array[i] > 0) {
+                answer.add(money / array[i]);
+                money %= array[i];
+            }
+            else answer.add(0);
+        }
+
         return answer;
     }
 }
