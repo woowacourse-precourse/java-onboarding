4a4c2391451abfd8c073871bc468903a047a3580
yoonsj970407 <yoonsj970407@gmail.com>
2022-11-01 12:26:42 +0900

    ✨Feat: Add exception()

--- src/main/java/onboarding/Recommendation.java
+++ src/main/java/onboarding/Recommendation.java
@@ -15,4 +15,23 @@ class Recommendation {
         this.users = new HashMap<>();
     }
 
+    int exception() {
+        if((user.length() < 1) || (user.length() > 30)) {
+            return -1;
+        } else if((friends.size() < 1) || (friends.size() > 10000)) {
+            return -1;
+        } else if((visitors.size() > 10000)) {
+            return  -1;
+        } else {
+            for(int i = 0; i < friends.size(); i++) {
+                if((friends.get(i).get(0).length() < 1) || (friends.get(i).get(0).length() > 30)) {
+                    return -1;
+                } else if((friends.get(i).get(1).length() < 1) || (friends.get(i).get(1).length() > 30)) {
+                    return -1;
+                }
+            }
+        }
+
+        return 0;
+    }
 }
