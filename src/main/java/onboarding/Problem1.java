12e9d3e6879a9586a9bd1578ce6b7e1c33e3eab1
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-01 17:49:11 +0900

    Problem1:refactor:input예외 메소드로 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if(pobi.size() != 2 || crong.size() != 2) return -1;
-        if(pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) return -1;
+        if(isWrongInput(pobi, crong)) return -1;
 
         int pobi_high_score0 = Math.max(plusScore(pobi.get(0)), multiplyScore(pobi.get(0)));
         int pobi_high_score1 = Math.max(plusScore(pobi.get(1)), multiplyScore(pobi.get(1)));
@@ -37,4 +36,10 @@ class Problem1 {
         return total_score;
     }
 
+    private static boolean isWrongInput(List<Integer> pobi, List<Integer> crong) {
+        if(pobi.size() != 2 || crong.size() != 2) return true;
+        if(pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) return true;
+
+        return false;
+    }
 }
