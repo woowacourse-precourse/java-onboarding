8b6a5232e40843ba5a502607d265aa909869dfb0
suhwan <popomarin28@gmail.com>
2022-11-01 16:13:27 +0900

    [problem2] 예외처리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,7 +5,11 @@ public class Problem2 {
         char [] arrOfCryptogram = cryptogram.toCharArray();
         int i;
         int j = 0;
-       
+        //예외 처리 길이가 2이면서 둘이 같으면 오류가 나기 때문에 빈문자열로 삭제해서 리턴 한다
+        if(arrOfCryptogram.length == 2) {
+            if(arrOfCryptogram[0] == arrOfCryptogram[1])
+                return "";
+        }
         for(i = 1; i < arrOfCryptogram.length; i++) {
             if(arrOfCryptogram[i] != arrOfCryptogram[i - 1]) {
                 arrOfCryptogram[j++] = arrOfCryptogram[i - 1];
