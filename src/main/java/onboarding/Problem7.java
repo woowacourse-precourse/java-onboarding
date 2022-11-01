8ba18b77b77d326990e6a8a58635aac3dc95f1e9
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-01 15:34:00 +0900

    Feat(Problem7) : getFriendNumber 메서드 추가 사용자와 함께 아는 친구의 숫자를 구하는 메서드 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,6 +6,7 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         Set<String> peopleSet = getPeopleSet(friends, visitors);
+        Map<String, Set<String>> relationMap = getRelationMap(friends, peopleSet);
         return answer;
     }
 
@@ -28,4 +29,27 @@ public class Problem7 {
         friendSet.remove(name);
         return friendSet;
     }
+    public static Map<String, Set<String>> getRelationMap(List<List<String>> friends, Set<String> peopleSet) {
+        Map<String, Set<String>> relationMap = new HashMap<>();
+        Set<String> friendSet;
+        for(String s : peopleSet) {
+            friendSet = getFriendSet(friends, s);
+            relationMap.put(s, friendSet);
+        }
+        return relationMap;
+    }
+    public static int getFriendNumber(Map<String, Set<String>> relationMap, String friend, String user) {
+        int number = 0;
+        Set<String> friendSet = relationMap.get(friend);
+        Set<String> usersf = relationMap.get(user);
+
+        for(String s : usersf) {
+            if(friendSet.contains(s)) {
+                number =  number + 1;
+            }
+        }
+        return number;
+    }
+
+
 }
