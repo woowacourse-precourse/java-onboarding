871f37d26069857328836615797f39b9b392c611
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-01 22:16:27 +0900

    week1...

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        String result = "";
+        int len = word.length();
+        for(int i=0;i<len;i++){
+            char w = word.charAt(i);
+            int ascii = (int)w;
+            if(65<=ascii&&ascii<=90){
+                result += (char)(155-ascii);
+            }
+            else if(97<=ascii&&ascii<=122){
+                result += (char)(219-ascii);
+            }
+            else{
+                result = result + w;
+            }
+        }
+        return result;
     }
 }
+//65~90 97~122
+//155   219
--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+        int size = forms.size();
+        for(int i=0;i<size;i++){
+            String name = forms.get(i).get(1);
+            for(int j=0;j<name.length()-1;j++){
+                String cat = name.substring(j,j+2);
+                int overlap = 0;
+                for(int k=i+1;k<size;k++){
+                    if(forms.get(k).get(1).contains(cat)){
+                        String email = forms.get(k).get(0);
+                        if(!answer.contains(email)) answer.add(email);
+                        overlap += 1;
+                    }
+                }
+                if(overlap > 0){
+                    String email = forms.get(i).get(0);
+                    if(!answer.contains(email)) answer.add(email);
+                }
+            }
+        }
+        Collections.sort(answer);
         return answer;
     }
 }
