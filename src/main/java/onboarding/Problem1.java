4fe7fe356c19770e23b16a7045f8fd904b5dc2f6
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-01 14:15:19 +0900

    feat:myBiggest()->List의 값들 중 가장 큰 것을 출력

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -17,4 +17,11 @@ class Problem1 {
                 (myBiggest(pobi)<myBiggest(crong)) ? 2: (myBiggest(pobi)>myBiggest(crong)) ? 1:0;
         return Biggest;
     }
+
+    private static int myBiggest(List<Integer> paper){
+        int paper_left=Math.max(add(paper.get(0)), multi(paper.get(0)));
+        int paper_right=Math.max(add(paper.get(1)), multi(paper.get(1)));
+        Biggest=Math.max(paper_left, paper_right);
+        return Biggest;
+    }
 }
