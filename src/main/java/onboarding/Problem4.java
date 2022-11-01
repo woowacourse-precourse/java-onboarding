f17444f066cc011e07e3d6d67f3d1e75c2e54617
simjaeseo <delpho@naver.com>
2022-11-01 19:02:22 +0900

    feat: 문제 4 기능 목록

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 기능 목록
+ * 1. 청개구리 사전 초기화 함수
+ * 2. 청개구리 사전 기반 단어 변환 함수
+ */
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
