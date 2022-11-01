8782f696b4b6aadadcede62b42de2a13fa6dc041
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-01 22:28:16 +0900

    feat 배열안의 정보를 가지고 중복성을 판별해 필요한 정보를 도출하는 문제
    Map을 가지고 필요한 정보를 분리하고 도출하는 방법을 배움

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,43 @@
 package onboarding;
 
 import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+
+    public static HashSet getEmail(List<List<String>> forms){
+        Map<String, String> hashMap = new HashMap<>();
+        HashSet<String> emails = new HashSet<>();
+        String key ="";
+        String nickName;
+        String source;
+
+        for(int i =0; i < forms.size();i++) {
+            nickName = forms.get(i).get(1);
+            source = forms.get(i).get(0);
+
+            for (int j = 0; j < nickName.length() - 1; j++) {
+                key = nickName.substring(j, j + 2);
+                    if(hashMap.containsKey(key)){
+                        String name = hashMap.get(key);
+                        if(!name.equals(source)){
+                            emails.add(name);
+                            emails.add(source);
+                        }
+                    }
+
+                hashMap.put(key, source);
+            }
+        }return emails;
+    }
+
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = (List<String>) getEmail(forms).stream().sorted()
+                .collect(Collectors.toList());
+
         return answer;
     }
 }
