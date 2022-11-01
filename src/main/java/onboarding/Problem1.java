483d5d0cddefdcc4b7dff24d45078c185f94f7b7
kksks3192 <kksks3192@gmail.com>
2022-11-01 14:53:03 +0900

    chore(problem1): 기능 주석 수

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -29,13 +29,13 @@ class Problem1 {
     public static int getScore(List<Integer> pages){
         int max_score=0;
 
-        //덧셈과 곱셈 중 큰 수 점수로 반환
+        //덧셈과 곱셈 중 큰 수를 점수로 반환
         for(int i=0;i<2;i++){
             int sum=0;
             int mlt=1;
             int page = pages.get(i);
             int max = 0;
-            
+
             while(page>0) {
                 sum += page%10;
                 mlt *= page%10;
