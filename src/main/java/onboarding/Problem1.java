d9b82c88ade5c73806080e7da1ddf8b1e3fe414d
broden-horizon <rlghd1698@naver.com>
2022-11-01 20:59:40 +0900

    문제1. 각 자릿수의 곱을 구하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,4 +16,14 @@ class Problem1 {
         }
         return sum;
     }
+
+    //각 자리의 곱셈의 합계를 구하는 메서드
+    private static int multiplicationOfEachDigit(int num) {
+        int multi = 1;
+        while(num > 0) {
+            multi *= num % 10;
+            num /= 10;
+        }
+        return multi;
+    }
 }
