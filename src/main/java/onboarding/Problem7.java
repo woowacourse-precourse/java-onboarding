fadbbc1c99826effe6236e022b657ca082ce38b6
cksdud8028 <cksdud8028@naver.com>
2022-11-01 16:43:18 +0900

    FEAT scoreInitial

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,11 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String, Integer> recommendScore = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        scoreInitial(user, friends, visitors);
         List<String> answer = Collections.emptyList();
         return answer;
     }
@@ -25,5 +25,41 @@ public class Problem7 {
 
         return result;
     }
+
+    private static void scoreInitial(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> initialList = new ArrayList<>();
+        List<String> friendsList = usersFriendsList(user, friends);
+        for (String friend : friendsList) {
+            for (List<String> names : friends) {
+                if (names.contains(friend)) {
+                    int index = names.indexOf(friend);
+                    if (index == 0 && !friendsList.contains(names.get(1))) {
+                        initialList.add(names.get(1));
+                    } else if (!friendsList.contains(names.get(0))) {
+                        initialList.add(names.get(0));
+                    }
+                }
+            }
+        }
+
+        for (String s : initialList) {
+            if (recommendScore.containsKey(s)) {
+                recommendScore.put(s, recommendScore.get(s) + 10);
+                continue;
+            }
+            recommendScore.put(s, 10);
+        }
+
+        for (String visitor : visitors) {
+            if (recommendScore.containsKey(visitor)) {
+                recommendScore.put(visitor, recommendScore.get(visitor) + 1);
+                continue;
+            }
+            recommendScore.put(visitor, 1);
+        }
+
+    }
+
+    
 
 }
