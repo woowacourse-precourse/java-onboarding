a91c995c106a3b4dfce8af827a4de672da63bb06
eyeofsol <kingjihong@naver.com>
2022-11-01 10:39:30 +0900

    feat(문제6): 닉네임 목록 추출하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,16 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    static List<String> getNickNameList(List<List<String>> forms) {
+        List<String> nicknames = new ArrayList<>();
+        List<String> form;
+
+        for (int i = 0; i < forms.size(); i++) {
+            form = forms.get(i);
+            nicknames.add(form.get(1));
+        }
+
+        return nicknames;
+    }
 }
