a3e0fccf5a20997089f5042ebf62f9a4aabd5081
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 18:27:49 +0900

    돈과 지폐를 입력으로 받고 돈을 지폐로 나눈 나머지를 리턴한다

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,9 @@ public class Problem5 {
     private static int calculateQuotient(int money, int bill) {
         return money / bill;
     }
+    private static int calculateRemainder(int money, int bill) {
+        return money % bill;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
