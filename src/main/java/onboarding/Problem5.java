dbf927261bac51b0e8fe6395426e8719f4f974eb
isieun0908 <supiya0908@naver.com>
2022-11-01 23:33:04 +0900

    Revert "Feat: Problem5"
    This reverts commit 9e0c4e6886287712313da3bc1cca9d0590f10cd1.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,20 +1,11 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> moneyList = new ArrayList<>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
-
-        int money_temp = money;
-        List<Integer> answer = new ArrayList<Integer>();
-        for (Integer m : moneyList) {
-            answer.add(money_temp / m);
-            money_temp %= m;
-        }
-
+        List<Integer> answer = Collections.emptyList();
         return answer;
     }
 }
