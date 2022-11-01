675b29fcfb9a9d114e7719100d7ab67b1648fe17
이수진 <slrkajsep337@naver.com>
2022-11-01 23:06:54 +0900

    기능요구사항) 문제 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,23 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        //65~90, 32:공백
+        //97~122
+
+        char[] babyfrogB = {'Z','Y', 'X' ,'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N',
+                'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+        char[] babyfrogS = {'z','y', 'x' ,'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n',
+                'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+
+        for (int i =0; i<word.length(); i++) {
+            if(word.charAt(i)==' ') answer += ' ';
+            else if ('A'<=word.charAt(i) && word.charAt(i)<='Z') answer += babyfrogB[(int)word.charAt(i)-65];
+            else answer += babyfrogS[(int)word.charAt(i)-97];
+        }
+
+
         return answer;
     }
+
 }
