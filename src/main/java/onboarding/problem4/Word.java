d061d90dc50b682bae44a30820d870c29be6aad7
Daeeun Ko <7273322@naver.com>
2022-11-01 14:09:10 +0900

    feat: convert each characters of word

--- src/main/java/onboarding/problem4/Word.java
+++ src/main/java/onboarding/problem4/Word.java
@@ -2,6 +2,9 @@ package onboarding.problem4;
 
 public class Word {
 
+    private static final int UPPER_TYPE = 1;
+    private static final int LOWER_TYPE = 2;
+
     private final int MIN_WORD_LENGTH = 1;
     private final int MAX_WORD_LENGTH = 1_000;
 
@@ -18,5 +21,29 @@ public class Word {
 
     private boolean isOutOfBounds(String word) {
         return word.length() < MIN_WORD_LENGTH || word.length() > MAX_WORD_LENGTH;
+    }
+
+    private static String convert(int ch) {
+        if (!isAlphabet(ch)) return String.valueOf((char) ch);
+        return Character.isUpperCase(ch) ? uppercaseConvert(ch) : lowercaseConvert(ch);
+    }
+
+    private static boolean isAlphabet(int ch) {
+        int type = Character.getType(ch);
+        return type == UPPER_TYPE || type == LOWER_TYPE;
+    }
+
+    private static String lowercaseConvert(int ch) {
+
+        int offset = 'z' - ch;
+
+        return String.valueOf((char) ('a' + offset));
+    }
+
+    private static String uppercaseConvert(int ch) {
+
+        int offset = 'Z' - ch;
+
+        return String.valueOf((char) ('A' + offset));
     }
 }
