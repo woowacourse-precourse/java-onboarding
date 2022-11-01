db572798b8d7fde9542504f90729c9effad232f8
minje <alswp0910@naver.com>
2022-11-01 12:33:48 +0900

    문제7_기능3

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
@@ -47,5 +44,42 @@ public class Problem7 {
 
         List<String> notFriends = new ArrayList<>(notFri);
         return notFriends;
+    }
+
+    //기능3. 점수 계산
+    public static HashMap<String, Integer> score (List<String> notFriends, List<List<String>> friends, List<String> visitors){
+        HashMap<String, Integer> score = new HashMap<>();
+
+        for(String fri : notFriends){
+            score.put(fri, 0);
+        }
+
+        for(String fri : visitors){
+            if(score.containsKey(fri)){
+                score.put(fri, score.get(fri));
+            }
+        }
+        for(List<String> fri : friends){
+            if(score.containsKey(fri.get(0))){
+                score.put(fri.get(0), score.get(fri.get(0)) + 10);
+            }else if(score.containsKey(fri.get(1))){
+                score.put(fri.get(1), score.get(fri.get(1)) + 10);
+            }
+        }
+
+
+        Iterator<String> it = score.keySet().iterator();
+        ArrayList<String> arr = new ArrayList<>();
+        while (it.hasNext()) {
+            String key = it.next();
+            if(score.get(key) == 0){
+                arr.add(key);
+            }
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            score.remove(arr.get(i));
+        }
+
+        return score;
     }
 }
