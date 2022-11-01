67fc87ff0297166de41c2bec0436d22a08f7d281
alswn <alswn9938@gmail.com>
2022-11-01 11:25:35 +0900

    [Q3] refactor : 초기화 경고 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -12,7 +12,7 @@ public class Problem3 {
 
     public static int threeSixNineCount(int number) {
         int count = 0;
-        int nowDigit = 0;
+        int nowDigit;
         while (number > 0) {
             nowDigit = number % 10;
             if (nowDigit == 3 || nowDigit == 6 || nowDigit == 9) {
