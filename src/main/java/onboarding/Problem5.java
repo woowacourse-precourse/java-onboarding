3a5e5d56bca390b50877f56bb323999dac6c231e
hwanee__ <fkfkthth@naver.com>
2022-11-01 17:02:52 +0900

    불필요_코드_제거_

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -75,40 +75,4 @@ public class Problem5 {
         return money/1;
     }
 
-    static List<Integer> change_money(int money){
-        List<Integer> result = new ArrayList<>();
-
-        while(money>0){
-            if(money>=50000){
-                result.add(money/50000);
-                money %= 50000;
-            }else if(money>=10000){
-                result.add(money/10000);
-                money %= 10000;
-            }else if(money>=5000){
-                result.add(money/5000);
-                money %= 5000;
-            }else if(money>=1000){
-                result.add(money/1000);
-                money %= 1000;
-            }else if(money>=500){
-                result.add(money/500);
-                money %= 500;
-            }else if(money>=100){
-                result.add(money/100);
-                money %= 100;
-            }else if(money>=50){
-                result.add(money/50);
-                money %= 50;
-            }else if(money>=10){
-                result.add(money/10);
-                money %= 10;
-            }else if(money>=1){
-                result.add(money/1);
-                money %= 1;
-            }else System.out.println("자연수가 아닙니다.");
-        }
-
-        return result;
-    }
 }
