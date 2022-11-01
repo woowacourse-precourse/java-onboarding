0e53420ba5b6d6eab4174bab16cd676ba9008837
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 14:52:34 +0900

    feat : solution 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,6 +4,16 @@ public class Problem3 {
     private static final int MIN = 1;
     private static final int MAX = 10000;
 
+    public static int solution(int number) {
+        if (!canNumber(number)) {
+            throw new IllegalArgumentException("제한 사항을 위반하였습니다.");
+        }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += count369(Integer.toString(i));
+        }
+        return answer;
+    }
     public static int count369(String num) {
         int cnt = 0;
         for (int i = 0; i < num.length(); i++) {
