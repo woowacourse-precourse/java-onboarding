45800b3355941e9765ffe3620998f901b3458372
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-01 10:48:18 +0900

    problem2 - deleteChar

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
 public class Problem2 {
+    public static String deleteChar(String cur, int index){
+        return cur.substring(0,index) + cur.substring(index+1);
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int cnt=0;
+        int _cnt=0;
+        while(true){
+            cnt=0;
+            if(cryptogram.length()==0) break;
+            char pre = cryptogram.charAt(0);
+            for(int i=1; i<cryptogram.length(); i++){
+                if(pre==cryptogram.charAt(i)){
+                    cryptogram = deleteChar(cryptogram, i);
+                    i--;
+                    if(_cnt==0) {
+                        cryptogram = deleteChar(cryptogram, i);
+                        i--;
+                    }
+                    cnt++;
+                    _cnt++;
+                    continue;
+                }
+                pre = cryptogram.charAt(i);
+                _cnt=0;
+            }
+            if(cnt==0) {
+                break;
+            }
+        }
+        return cryptogram;
     }
 }
+
