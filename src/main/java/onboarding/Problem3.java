b78c693d653ab6f0a4ed9fef6d4833ef3a0353b4
judyi19997 <judyi1999@kookmin.ac.kr>
2022-11-01 17:37:03 +0900

    add/ 문제 3) 숫자 문자로 변환하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,17 +6,10 @@ import java.util.List;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        List<Integer> numberList = slice(number);
         return answer;
     }
-    //숫자 자릿수별로 나누는 기능
-    public static List<Integer> slice(int number){
-        List<Integer> numberList = new ArrayList<>();
-        while(number>0){
-            System.out.println(number);
-            numberList.add(number%10);
-            number /=10;
-        }
-        return numberList;
+    //숫자를 문자로 바꾸는 기능
+    public static String numToString(int number){
+        return Integer.toString(number);
     }
 }
