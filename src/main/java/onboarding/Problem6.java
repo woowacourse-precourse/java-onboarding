cbb8bca683be1b00ce0874b5b22fc7d235ea6ae4
otndus <xuyeon219@gmail.com>
2022-11-01 14:20:10 +0900

    [기능 구현] Problem6 닉네임으로 만들 수 있는 두 글자 조합 생성 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -10,6 +10,20 @@ public class Problem6 {
     static ArrayList<String> caseOfNickName = new ArrayList<>();
     static HashMap<Integer, Integer> duplicateIndex = new HashMap<>();
 
+    public static List<String> caseArray (List<List<String>> forms){
+        ArrayList<String> nickName = new ArrayList<>();
+        for(List<String> name : forms)
+            nickName.add(name.get(1));
+
+        for(int i = 0; i < nickName.size(); i++){
+            for(int j = 0; j <= nickName.get(i).length()-2; j++){
+                if(nickName.get(i).length() == 1) continue;
+                caseOfNickName.add(nickName.get(i).substring(j, j+2));
+            }
+        }
+        return containsNickName(nickName, forms);
+    }
+
     public static List<String> containsNickName(ArrayList nickName, List<List<String>> forms){
         for(int i = 0; i < nickName.size(); i++){
             for(String s : caseOfNickName){
@@ -31,7 +45,7 @@ public class Problem6 {
     }
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = caseArray(forms);
         return answer;
     }
 }
