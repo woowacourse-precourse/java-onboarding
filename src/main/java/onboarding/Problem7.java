e4939835ae302bb90757a999d200ab48c5742bf3
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-01 21:17:12 +0900

    feat : 문제7 user를 제외한 전체 회원 목록

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        HashSet<String> userlist = new HashSet<>();
+        HashSet<String> friendlist = new HashSet<>();
+        HashMap<String, Integer> recommendlist = new HashMap<>();
+
+        //1) user를 제외한 나머지 회원들의 이름 목록(HashSet)
+        for(int i=0; i<friends.size(); i++){
+            String name1 = friends.get(i).get(0);
+            String name2 = friends.get(i).get(1);
+            userlist.add(name1);
+            userlist.add(name2);
+        }
+        for(String name : visitors) userlist.add(name);
+        userlist.remove(user);
+
+
         return answer;
     }
 }
