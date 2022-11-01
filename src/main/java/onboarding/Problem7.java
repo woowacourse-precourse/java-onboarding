e6ddf65024c1869318a5f56957b450f8d22adb40
mon0mon <mon0mon@naver.com>
2022-11-01 11:58:43 +0900

    나머지 문제들 테스트 해보기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,69 +1,101 @@
 package onboarding;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Problem7 {
     static Map<String, List<String>> friendList = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = getRecommendFriendList(user, friends, visitors);
         return answer;
     }
 
     public static List<String> getRecommendFriendList(String user,
-                                                      List<List<String>> friends, List<String> visitiors) {
+                                                      List<List<String>> friends, List<String> visitors) {
         Map<String, Integer> score = new HashMap<>();
         List<String> resList = new ArrayList<>();
 
-        for (int i = 0; i < friends.size(); i++) {
+        initFriendList(friends);
 
+        // 사용자의 친구들 목록
+        for (String userFriend : friendList.get(user)) {
+            List<String> userFriendFriend = friendList.get(userFriend);
+            for (int i = 0; i < userFriendFriend.size(); i++) {
+                if (isFriend(user, userFriendFriend.get(i))) {
+                    score.put(userFriendFriend.get(i), 0);
+                    continue;
+                }
+
+                if (!userFriendFriend.get(i).equals(user)) {
+                    score.put(userFriendFriend.get(i), !score.containsKey(userFriendFriend.get(i))
+                            ? 10 : score.get(userFriendFriend.get(i)) + 10);
+                }
+            }
         }
 
-        for (int i = 0; i < visitiors.size(); i++) {
+        for (int i = 0; i < visitors.size(); i++) {
+            //  사용자의 친구 중에서 방문자가 있을 경우 스킵
+            if (friendList.get(user).contains(visitors.get(i))) {
+                continue;
+            }
 
+            score.put(visitors.get(i), !score.containsKey(visitors.get(i))
+                    ? 1 : score.get(visitors.get(i)) + 1);
         }
 
-        return null;
+//        return sortResult(score);
+
+        return new ArrayList<>(score.keySet());
     }
 
     public static void initFriendList(List<List<String>> friends) {
         Set<String> userSet = new HashSet<>();
         List<String> userList;
+        List<List<String>> userFriendList = new ArrayList<>();
 
         for (int i = 0; i < friends.size(); i++) {
-//            for (int j = 0; j < friends.get(i).size(); j++) {
-//                userSet.add(friends.get(i).get(0));
-//                userSet.add(friends.get(i).get(1));
-//            }
-            userSet.add(friends.get(i).toArray())
+            userSet.addAll(friends.get(i));
         }
 
         userList = new ArrayList<>(userSet);
 
         for (int i = 0; i < userList.size(); i++) {
-            List<String> friendsList = new ArrayList<>();
+            Set<String> set = new HashSet<>();
+            for (int j = 0; j < friends.size(); j++) {
+                if (friends.get(j).contains(userList.get(i))) {
+                    set.addAll(friends.get(j));
+                }
+            }
+            set.remove(userList.get(i));
+            userFriendList.add(new ArrayList<>(set));
+        }
 
-            friendsList = friends.contain
+        for (int i = 0; i < userList.size(); i++) {
+            friendList.put(userList.get(i), userFriendList.get(i));
         }
     }
 
-    public static int visitScore(String user, List<String> visitors) {
-
-    }
-
-    public static int knownFriendScore(String user, List<List<String>> friends) {
-
-    }
+//    public static int visitScore(String user, List<String> visitors) {
+//
+//    }
+//
+//    public static int knownFriendScore(String user, List<List<String>> friends) {
+//
+//    }
 
     public static boolean isFriend(String user, String target) {
-        if (friendList.containsKey(user)) {
-
-        }
-        else if (friendList.values().contains(user)) {
-
+        if (friendList.get(user).contains(target)) {
+            return true;
         }
 
+        return false;
+    }
 
+    public static List<String> sortResult(Map<String, Integer> score) {
+        List<String> res = new ArrayList<>(score.keySet());
+
+//        res.sort();
+
+        return null;
     }
 }
