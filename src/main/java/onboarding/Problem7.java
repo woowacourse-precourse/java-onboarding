8467c865a029faed75418ec8185555c7771cce18
ckyeon <qsky8520@gmail.com>
2022-11-01 23:41:44 +0900

    Add getRecommendScores() to Problem7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,16 +2,63 @@ package onboarding;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Problem7 {
 
-    public static List<String> solution(String user, List<List<String>> friends,
-        List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+    public static List<String> solution(
+        String user,
+        List<List<String>> friends,
+        List<String> visitors
+    ) {
+        HashMap<String, Integer> recommendScores = getRecommendScores(user, friends, visitors);
         return answer;
+    }
+
+    private static HashMap<String, Integer> getRecommendScores(
+        String user,
+        List<List<String>> friends,
+        List<String> visitors
+    ) {
+        List<String> userFriends = getUserFriends(user, friends);
+
+        HashMap<String, Integer> recommendScores = new HashMap<>();
+
+        friends.forEach(friendPair -> {
+                String friendA = friendPair.get(0);
+                String friendB = friendPair.get(1);
+
+                if (friendA.equals(user) || friendB.equals(user)) {
+                    return;
+                }
+
+                if (userFriends.contains(friendA) && !userFriends.contains(friendB)) {
+                    Integer recommendScore = recommendScores.getOrDefault(friendB, 0);
+                    recommendScores.put(
+                        friendB,
+                        recommendScore + Score.ACQUAINTANCE_WITH_USER.value
+                    );
+                } else if (!userFriends.contains(friendA) && userFriends.contains(friendB)) {
+                    Integer recommendScore = recommendScores.getOrDefault(friendA, 0);
+                    recommendScores.put(
+                        friendA,
+                        recommendScore + Score.ACQUAINTANCE_WITH_USER.value
+                    );
+                }
+            }
+        );
+
+        visitors.forEach(visitor -> {
+            if (!userFriends.contains(visitor)) {
+                Integer recommendScore = recommendScores.getOrDefault(visitor, 0);
+                recommendScores.put(visitor, recommendScore + Score.VISIT_TIMELINE.value);
+            }
+        });
+
+        return recommendScores;
     }
 
     private static List<String> getUserFriends(String user, List<List<String>> friends) {
