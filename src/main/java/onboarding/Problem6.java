2c3b383224dbf38aa0fecbe26497ff3cb363bb7a
ParkRootSeok <g_e_un@naver.com>
2022-11-01 23:25:21 +0900

    docs : Problem6 Function List 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+
+    public static List<Integer> hasReduplication(String[] nickNames, StringBuilder nickName) {
+        List<Integer> result = new ArrayList<>();
+
+        return result;
+    }
+
+    public static List<String> compareNickname(List<List<String>> forms) {
+        List<String> result = new ArrayList<>();
+
+        return result;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        answer = compareNickname(forms);
         return answer;
     }
+
+
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,17 @@
 package onboarding;
 
-import java.util.Collections;
+import java.awt.*;
+import java.awt.image.ImageProducer;
+import java.util.*;
 import java.util.List;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+
+
 }
