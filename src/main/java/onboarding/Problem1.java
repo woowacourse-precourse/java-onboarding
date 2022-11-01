fd93f317145e87008b4c04c07100295c9a3e40f9
aran <aran0317@naver.com>
2022-11-01 20:46:48 +0900

    문제 1번 기능 요구사항

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,14 @@ package onboarding;
 
 import java.util.List;
 
+
+/*기능 요구 사항*/
+/* 1. 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수 번호이다.
+*  2. 페이지 번호의 각 숫자를 모두 더하거나 모두 곱해 큰 수를 구한다
+*  3. 가장 큰 수를 본인의 점수로 하고, 점수를 비교해 높은 사람이 승자가 된다.
+*  4. 시작 면(1)이나 마지막 면(400)이 나오도록 책을 펼치지 않는다.
+*  5. 포비가 이기면 1, 크롱이 이기면 2, 무승부는 0
+*  6. 예외 사항은 -1로 retrun 한다. */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
