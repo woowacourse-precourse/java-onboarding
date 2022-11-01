4094156d116419d04b364654896cac8ccda4ee11
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-01 22:31:50 +0900

    refactor : 코드 구조 및 함수 재정의, 변수명 변경

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,7 +3,7 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    static int rest;
+    private static int section = 0;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = 0;
 
@@ -15,48 +15,59 @@ class Problem1 {
         }
         return answer;
     }
-    static int cal(List scoreData){
+    public static int cal(List scoreData){
         int result = 0;
-        int leftScore = 0;
-        int rightScore = 0;
+        int leftscore = 0;
+        int rightscore = 0;
 
-        if(Math.abs((int)scoreData.get(1)-(int)scoreData.get(0))!=1){
+        if(Math.abs((int)scoreData.get(1)-(int)scoreData.get(0)) != 1){
             result = -1;
             return result;
         }
-        if((int)scoreData.get(0)==1 || (int)scoreData.get(1)==400){
+        if((int)scoreData.get(0) == 1 || (int)scoreData.get(1) == 400){
             result = -1;
             return result;
         }
 
-        while(rest++ <=2){
-            int addScore = 0;
-            int mulScore = 1;
-            if(rest==1){
+        while(section++ <=2){
+            if(section==1){
                 String[] leftPage = scoreData.get(0).toString().split("");
-
-                for(int i=0; i<leftPage.length; i++) {
-                    addScore += Integer.parseInt(leftPage[i]);
-                    mulScore *= Integer.parseInt(leftPage[i]);
-                }
-                leftScore = Math.max(addScore, mulScore);
+                leftscore = calLeftPage(leftPage);
             }
-            else if(rest==2){
+            if(section==2){
                 String[] rightPage = scoreData.get(1).toString().split("");
-
-                for(int i=0; i<rightPage.length; i++) {
-                    addScore += Integer.parseInt(rightPage[i]);
-                    mulScore *= Integer.parseInt(rightPage[i]);
-                }
-                rightScore = Math.max(addScore, mulScore);
+                rightscore = calRightPage(rightPage);
             }
         }
-        rest = 0;
-        result = Math.max(leftScore, rightScore);
+        section = 0;
+        result = Math.max(leftscore, rightscore);
         return result;
     }
+    private static int calLeftPage(String[] leftpage){
+        int addScore = 0;
+        int mulScore = 1;
+        int leftscore = 0;
 
-    static int compareScore(int pobi, int crong){
+        for(int i=0; i<leftpage.length; i++) {
+            addScore += Integer.parseInt(leftpage[i]);
+            mulScore *= Integer.parseInt(leftpage[i]);
+        }
+        leftscore = Math.max(addScore, mulScore);
+        return leftscore;
+    }
+    private static int calRightPage(String[] rightpage){
+        int addScore = 0;
+        int mulScore = 1;
+        int rightscore = 0;
+
+        for(int i=0; i<rightpage.length; i++) {
+            addScore += Integer.parseInt(rightpage[i]);
+            mulScore *= Integer.parseInt(rightpage[i]);
+        }
+        rightscore = Math.max(addScore, mulScore);
+        return rightscore;
+    }
+    private static int compareScore(int pobi, int crong){
         int result = 0;
         if(pobi>crong) result = 1;
         if(crong>pobi) result = 2;
