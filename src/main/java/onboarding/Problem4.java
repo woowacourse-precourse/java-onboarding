ecda896d45d69cc0f8c9bdf9ca266d4844938922
khj <kim37171@cuk.edu>
2022-11-01 20:29:51 +0900

    [Problem4] 문자열 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,7 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder sb = new StringBuilder();
-
+        for (int i = 0; i < word.length(); i++) {
+            char now = word.charAt(i);
+            if(isLowerCase(now)){
+                sb.append((char)('z'-(now-'a')));
+                continue;
+            }
+            if(isUpperCase(now)){
+                sb.append((char)('Z'-(now-'A')));
+                continue;
+            }
+            sb.append(now);
+        }
         return sb.toString();
     }
 
