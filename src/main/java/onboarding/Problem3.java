a4606f9583ef532987eb65db9385f0a6dc8e64ff
Hyunyeop Ahn <hyeop9@gmail.com>
2022-11-01 17:34:53 +0900

    problem 3 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        int i = 1;
+
+        // 1부터 number 까지 반복하여 각 자릿수 별 3,6,9 확인
+        while (i <= number) {
+            // 십의 자리 3,6,9 확인
+            if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+                answer++;
+            }
+            // 일의 자리 3,6,9 확인
+            if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+                answer++;
+            }
+            i++;
+        }
+
         return answer;
     }
 }
