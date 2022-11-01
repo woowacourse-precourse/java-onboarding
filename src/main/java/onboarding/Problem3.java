7d7ec2632f0dd12552d99e15fd2e3bef13e983a0
June1010 <tjrwns1010@naver.com>
2022-11-01 23:21:24 +0900

    chore: 기능 목록 커밋 (Mock 함수만 생성)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int increaseCount(int count, int index) {
+        return 0;
+    }
+
+    private static int getTargetNumCount(int number) {
+        return 0;
     }
 }
