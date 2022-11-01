213d0a153f49c0cbc5c40617af2da03efdea4e75
yenawee <yaena0319@naver.com>
2022-11-01 16:01:29 +0900

    [FEAT] 5. 각각의 금액으로 나눈 몫을 배열에 할당하여 ret

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,5 +9,12 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         if (money < 1 || money > 1000000)
             return null;
+        int[] ret = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i = 0; i < 9; i++){
+            int tmp = ret[i];
+            ret[i] = money / ret[i];
+            money = money % tmp;
+        }
+        return ((List<Integer>) Arrays.stream(ret).boxed().collect(Collectors.toList()));
     }
 }
