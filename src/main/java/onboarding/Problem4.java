b823d3cd0fd520e4287068ebaeb51025fc4cc607
jinuk <devuks0820@gmail.com>
2022-11-01 11:34:12 +0900

    feat: Problem4 Add method
    대소문자별 시작과 끝 값을 더한후 현재 문자의 값을 빼주면 뒤집어진 값이 출력되도록 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,45 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
+     public static String solution(String word) {
+        String answer = convertWord(word);
         return answer;
+    }
+
+    private static String convertWord(String string) {
+        StringBuffer buffer = new StringBuffer(string);
+        StringBuffer convertBuffer = new StringBuffer();
+
+        for(int i = 0 ; i < buffer.length(); ++i) {
+
+            if((int)buffer.charAt(i) >= 'A' && (int)buffer.charAt(i) <= 'Z') {
+                convertBuffer.append(convertUppercase(buffer.charAt(i)));
+                continue;
+            }
+
+            if((int)buffer.charAt(i) >= 'a' && (int)buffer.charAt(i) <= 'z') {
+                convertBuffer.append(convertLowercase(buffer.charAt(i)));
+                continue;
+            }
+
+            convertBuffer.append(buffer.charAt(i));
+        }
+        return  convertBuffer.toString().trim();
+    }
+
+    private static char convertUppercase(char upper) {
+        int convertStandard = 'A' + 'Z';
+
+        char converChar = (char)(convertStandard - (int)upper);
+
+        return converChar;
+
+    }
+    private static char convertLowercase(char lower) {
+        int convertStandard = 'a' + 'z';
+
+        char converChar = (char)(convertStandard - (int)lower);
+
+        return converChar;
     }
 }
