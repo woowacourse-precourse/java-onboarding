298302d097eb672c8aacc8f1e12c0032bf53ca68
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-01 09:20:03 +0900

    feat: 누적 기능, 규칙 기능으로 테스트
    Signed-off-by: joohyeongKim <wnguddl96@knou.ac.kr>

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -14,13 +14,16 @@ public class Problem3 {
             int digitOne = i % 10;
             int digitTen = i / 10;
 
+        // 규칙 기능
             if (i==33 || i==66 || i==99) { answer += 2; break;}
+            if (i==333 || i==666 || i==999) { answer += 3; break;}
+            if (i==3333 || i==6666 || i==9999) { answer += 4; break;}   //  1~10000이하 자연수 범위 속 3,6,9 중첩 규칙
             if(digitOne == 3 || digitOne == 6 || digitOne == 9){    answer += 1;    } // 1의 자리 3,6,9 규칙
-            if(digitTen == 3 || digitTen == 6 || digitTen == 9) {   answer += 1;    } // 10의 자리 규칙
+            if(digitTen == 3 || digitTen == 6 || digitTen == 9) {   answer += 1;    } // 10의 자리 3,6,9 규칙
 
         }
 
-        // 원래 구현하고 싶었던 코드
+        // refactor : 캡슐화
 //        int repository = save(number);
 //        int count = getCount(repository);
 
