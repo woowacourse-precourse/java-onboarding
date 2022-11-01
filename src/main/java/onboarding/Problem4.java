ebd5db6125cc58535e3bbbe0be026052c504b1b8
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-11-01 15:47:26 +0900

    알파벳을 변환하는 change 구현 및 solution 구현 / solve Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,25 +1,29 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
+
+    public static String change(char[] chars) {
         String answer = "";
-        char[] arr = word.toCharArray();
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]==32){
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 32) {
                 answer += " ";
             }
-            else if(Character.isUpperCase(arr[i])){
-                int temp = arr[i] - 65;
+            if (Character.isUpperCase(chars[i])) {
+                int temp = chars[i] - 65;
                 int reverse = (65 + 25) - temp;
-                answer += String.valueOf((char)reverse);
+                answer += String.valueOf((char) reverse);
             }
-            else{
-                int temp = arr[i] - 97;
+            if(Character.isLowerCase(chars[i])){
+                int temp = chars[i] - 97;
                 int reverse = (97 + 25) - temp;
-                answer += String.valueOf((char)reverse);
+                answer += String.valueOf((char) reverse);
             }
         }
-        System.out.println(answer);
         return answer;
+    }
+
+    public static String solution(String word) {
+        char[] arr = word.toCharArray();
+        return change(arr);
     }
 }
