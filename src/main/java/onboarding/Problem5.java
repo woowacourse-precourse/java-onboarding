2205537a9ec410beafefb427f1da2463f48a9244
kksks3192 <kksks3192@gmail.com>
2022-11-01 14:54:59 +0900

    feat(problem5): 큰 화폐부터 내림차순으로 변환하는 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    public static int[] makeChange(int money){
+        int[] cashArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] changeArray = new int[9];
+        int index = 0;
+        int count = 0;
+
+        for(int i:cashArray){
+            count = money/i;
+            changeArray[index] = count;
+            money -= (count*i);
+            count = 0;
+            index++;
+        }
+
+        return changeArray;
+    }
+
 }
