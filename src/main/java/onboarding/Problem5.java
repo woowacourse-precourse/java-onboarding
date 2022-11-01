fe5701edd19e0884bd0861e9a54e9e4cb79c1f52
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-11-01 12:36:33 +0900

    refactor: Problem5 - MoneyUnit 구현 (상황마다 다른 단위를 사용할 수 있도록 함)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import onboarding.problem5.Bank;
+import onboarding.problem5.MoneyUnit;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,6 +9,6 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        return new Bank().withdraw(money);
+        return new Bank(MoneyUnit.KOREA).withdraw(money);
     }
 }
--- src/main/java/onboarding/problem5/Bank.java
+++ src/main/java/onboarding/problem5/Bank.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 public class Bank {
 
-    private final int[] MONEY_UNITS = new int[]{50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1};
+    private MoneyUnit moneyUnit;
+
+    public Bank(MoneyUnit moneyUnit){
+        this.moneyUnit = moneyUnit;
+    }
 
     public List<Integer> withdraw(int money){
         List<Integer> answer = new ArrayList<>();
-
         int remainingMoney = money;
-        for (int moneyUnit : MONEY_UNITS){
+
+        for (int moneyUnit : moneyUnit.getUnit()){
             int unitCount = remainingMoney / moneyUnit;
             answer.add(unitCount);
             remainingMoney -= moneyUnit * unitCount;
