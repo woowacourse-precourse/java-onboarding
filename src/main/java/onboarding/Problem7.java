d653f478a16c2a7b602207b4fb4355a088f2ee3d
dener8 <8dener@gmail.com>
2022-11-01 20:42:19 +0900

    [Problem7] feat: value가 0으로 초기화된 score 만들기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -10,7 +10,7 @@ public class Problem7 {
 
     static List<String> recommendFriend(String user, List<List<String>> friends, List<String> visitors) {
         Map<String, List<String>> relationship = new HashMap<>();
-        // 사람 목록 만들기
+        // 사람 목록 만들기 (relationship)
         String name;
         for (int i = 0; i < friends.size(); i++) {
             for (int j = 0; j < 2; j++) {
@@ -20,6 +20,12 @@ public class Problem7 {
                 }
             }
         }
+        // 점수 목록 만들기 (score)
+        Map<String, Integer> score = new HashMap<>();
+        for (int i = 0; i < relationship.size(); i++) {
+            score.put(relationship.get(i).get(0), 0);
+        }
+
         // 사람마다 친구인 사람을 나타내는 map
         for (int i = 0; i < friends.size(); i++) {
             String firstName = friends.get(i).get(0);
@@ -27,6 +33,10 @@ public class Problem7 {
             relationship.get(firstName).add(secondName);
             relationship.get(secondName).add(firstName);
         }
+
+
+
+
 
 
     }
