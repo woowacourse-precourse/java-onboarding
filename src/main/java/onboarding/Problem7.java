5cc381f7db1d6a57a9739e493f3d257879043b7f
Yejin Shin <shinyejin0212@gmail.com>
2022-11-01 19:32:13 +0900

    docs(problem7) : add 주석

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -3,14 +3,18 @@ package onboarding;
 import java.util.*;
 import java.util.Map.Entry;
 public class Problem7 {
+    
+    // 모든 사람에 대한 친구 목록을 받는다.
     public static Map<String, List<String>> getFriends(List<List<String>> friends){
         Map<String, List<String>> map = new HashMap<>();
 
         for (int i = 0; i < friends.size(); i++) {
             String first = friends.get(i).get(0);
             String second = friends.get(i).get(1);
-
+            
+            // 각 사람에 대한 원래 목록이 존재하는 경우와 존재하지 않는 경우를 나누어 받는다.
             if (map.containsKey(first)) {
+                // 원래 친구 목록에 새로 들어온 친구를 추가한다. 동일한 친구관계는 중복해서 들어오지 않기 때문에 단순히 추가하면 된다.
                 List<String> temp = map.get(first);
                 temp.add(second);
                 map.put(first, temp);
@@ -32,6 +36,8 @@ public class Problem7 {
         }
         return map;
     }
+    
+    // user와 친구가 아닌 사람 목록을 구별하기 위해 notFriends Map을 생성하고, 해당 사람의 친구 관계까지 받는 메소드
     public static Map<String, List<String>> getNotFriends(String user, Map<String, List<String>> frdList, List<String> userFriends){
         Map<String, List<String>> notFMap = new HashMap<>();
 
@@ -45,6 +51,7 @@ public class Problem7 {
         return notFMap;
     }
 
+    // user와 친구가 아닌 사람들 중 함께 아는 친구가 몇명인지 파악하고 1명당 10점을 부여하는 메소드
     public static Map<String, Integer> togetherFriendScore(Map<String, List<String>> notFriends, List<String> userFriends ){
         Map<String, Integer> map = new HashMap<>();
         Iterator<String> iter = notFriends.keySet().iterator();
@@ -62,14 +69,18 @@ public class Problem7 {
         return map;
     }
 
+    // 빙문자 점수를 부여하는 메소드
     public static Map<String, Integer> visitorScore(Map<String, Integer> scoreMap, List<String> visitors, List<String> userFriends){
         for (String visitor : visitors) {
+            // visitor가 user친구 목록에 없을 경우만 다룬다.
             if (!userFriends.contains(visitor)) {
+                // 원래 점수가 부여되어있을 경우와 점수가 0점인 경우를 나누어 점수를 부여한다.
                 if (scoreMap.containsKey(visitor)) {
                     int temp = scoreMap.get(visitor);
                     temp = temp + 1;
                     scoreMap.put(visitor, temp);
-                } else {
+                } else{
+                    // 즉, 처음 등장한 visitor이라면 1점 부여
                     scoreMap.put(visitor, 1);
                 }
             }
@@ -77,6 +88,7 @@ public class Problem7 {
         return scoreMap;
     }
 
+    // Score의 기준으로 추천할 사람들을 정렬하고, 점수가 같을 경우 이름을 기준으로 정렬하는 메소드
     public static List<Entry<String, Integer>> sortingScore(Map<String, Integer> scoreMap) {
         List<Entry<String, Integer>> sortScoreMap = new ArrayList<Entry<String, Integer>>(scoreMap.entrySet());
         Collections.sort(sortScoreMap, new Comparator<Entry<String, Integer>>() {
@@ -90,6 +102,8 @@ public class Problem7 {
         return sortScoreMap;
     }
 
+    // 추천할 수 있는 사람의 목록이 5명 이상일 경우 5명까지만 받아오는 메소드
+    // 이미 정렬되어있기 때문에 단순히 앞에서부터 짜르면 된다.
     public static List<String> getFiveAns (List<Entry<String, Integer>> sortScoreMap){
         List<String> temp = new ArrayList<>();
         for (Entry<String, Integer> entry : sortScoreMap) {
@@ -102,16 +116,24 @@ public class Problem7 {
     }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        // 모든 사람에 대한 친구 관계 리스트를 생성한다.
         Map<String, List<String>> frdList = new HashMap<>(getFriends(friends));
 
+        // user의 친구 목록을 받는다.
         List<String> userFriends = new ArrayList<>(frdList.get(user));
+        // user와 친구가 아닌 사람들에 대한 친구 관계 리스트를 생성한다.
         Map<String, List<String>> notFriends = new HashMap<>(getNotFriends(user, frdList, userFriends));
 
+        // 친구 관계에 대한 score을 부여한다.
         Map<String, Integer> scoreMap = new HashMap<>(togetherFriendScore(notFriends, userFriends));
+        // visitor에 대한 score을 부여한다.
         scoreMap = visitorScore(scoreMap, visitors, userFriends);
 
+        // 추천 가능한 사람의 목록을 점수와 이름을 기준으로 정렬한다.
         List<Entry<String, Integer>> sortScoreMap = new ArrayList<Entry<String, Integer>>(sortingScore(scoreMap));
 
+        // 추천할 사람을 5명 받아온다.
         answer = getFiveAns(sortScoreMap);
 
         return answer;
