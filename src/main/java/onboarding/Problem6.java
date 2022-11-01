ee4399c44e32d1820442906235bbc2d35b1abc85
khj <kim37171@cuk.edu>
2022-11-01 21:07:13 +0900

    [Problem6] 중복 이메일 제거

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,8 +6,20 @@ import java.util.stream.Collectors;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> emails = new LinkedList<>();
+        for (int i = 0; i < forms.size()-1; i++) {
+            for (int j = i+1 ; j < forms.size(); j++) {
+                emails.addAll(getCrewsEmails(forms.get(i),forms.get(j)));
+            }
+        }
+        Set<String> test = removeDuplicatedEmails(emails);
         return emails;
     }
+
+    private static Set<String> removeDuplicatedEmails(List<String> emails) {
+        Set<String> test = new HashSet<>(emails);
+        return test;
+    }
+
     // Function that returns a list of crew's emails if the crew's nickname contain same characters in series.
     private static List<String> getCrewsEmails(List<String> nowCrew, List<String> nextCrew) {
         List<String> crewsEmails = new LinkedList<>();
