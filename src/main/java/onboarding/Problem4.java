8f1720c5c1de01482e2074c547e06dbb5fad7d35
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 18:18:04 +0900

    문자 하나를 입력받고 대문자면 true를 대문자가 아니면 false를 리턴한다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,9 @@ public class Problem4 {
     private static boolean isLowerCase(char letter) {
         return letter >= 'a' && letter <= 'z';
     }
+    private static boolean isUpperCase(char letter) {
+        return letter >= 'A' && letter <= 'Z';
+    }
     public static String solution(String word) {
         String answer = "";
         return answer;
