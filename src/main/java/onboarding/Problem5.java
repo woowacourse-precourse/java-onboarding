52ba4792f3c37ad3990b2ad5e51b8bcfcd5de766
ddingmin <ddingminyt@gmail.com>
2022-11-01 22:33:14 +0900

    refactor(Problem5): 정답 출력 메소드 생성 및 bill 화폐 상수화

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    final static int[] bill = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        int[] bill = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
         List<Integer> answer = new ArrayList<>();
+        exchangeAllMoney(answer, money);
+        return answer;
+    }
+
+    public static void exchangeAllMoney (List<Integer> answer, int money){
         for (int j : bill) {
             answer.add(exchangeMoney(money, j));
             money = getChange(money, j);
         }
-        return answer;
     }
 
     private static int getChange(int money, int bill){
