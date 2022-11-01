a37ac5e4e2476c083667e67745113b64dff2f1f9
Nugur1i <duckhee092@naver.com>
2022-11-01 15:41:51 +0900

    feat<problem6> : Add Exception 2

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,17 +7,15 @@ import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        //- 크루는 1명 이상 10,000명 이하이다.
-        //- 이메일은 이메일 형식에 부합하며, 전체 길이는 11자 이상 20자 미만이다.
-        //- 신청할 수 있는 이메일은 `email.com` 도메인으로만 제한한다.
-        //- 닉네임은 한글만 가능하고 전체 길이는 1자 이상 20자 미만이다.
+
+
 
 
 
 
         int formsFirstRowLength = forms.toArray().length;
 
-        //예외사항 1
+        //예외사항 1 : 크루는 1명 이상 10,000명 이하이다.
         if (formsFirstRowLength < 1 || formsFirstRowLength > 10000){
             System.out.println("Please, Enter number of member between 1 ~ 10000");
             return new ArrayList<>();
@@ -35,18 +33,22 @@ public class Problem6 {
         while(formsFirstRowLength > 0){
             emailAddress = forms.get(formsFirstRowLength - 1).get(0);
             int emailAddressLength = emailAddress.length();
-            //예외사항2,3
+            //예외사항2 : 이메일은 이메일 형식에 부합하며, 전체 길이는 11자 이상 20자 미만이다.
             if (emailAddressLength<11 || emailAddressLength>20){
                 System.out.println("Please, Enter email address length between 11~20");
                 return new ArrayList<>();
-            } else if (emailAddress.substring(emailAddressLength-8,emailAddressLength+1) != "email.com"){
+            } else if (emailAddress.substring(emailAddressLength-8,emailAddressLength+1) != "email.com"){ // 예외사항 3 :신청할 수 있는 이메일은 `email.com` 도메인으로만 제한한다.
                 System.out.println("Please, Enter email domain only 'email.com'");
                 return new ArrayList<>();
             }
 
             nickName = forms.get(formsFirstRowLength - 1).get(1);
             int nickNameLength = nickName.length();
-
+            //예외사항4 : 닉네임은 한글만 가능하고 전체 길이는 1자 이상 20자 미만이다.
+            if (nickNameLength<1 || nickNameLength>20){
+                System.out.println("Please, Enter Nickname length between 1~20");
+                return new ArrayList<>();
+            }
             for (int i = 0; i < nickNameLength - 1; i++){
                 String twoWords = nickName.substring(i, i+2);
 
