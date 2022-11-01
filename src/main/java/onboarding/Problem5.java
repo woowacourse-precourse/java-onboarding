f32e2ea4312de13262aeedbb222bdc21fd810315
JoonHyug <hyug0819@gmail.com>
2022-11-01 20:58:31 +0900

    5번 문제 해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] moneycount = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int i = 0;
+        int count = 0;
+        while(money >= 0 && i < moneycount.length){
+            count = 0;
+            while(money-moneycount[i] >= 0){
+                if(money - moneycount[i] >= 0) {
+                    money -= moneycount[i];
+                    count++;
+                }
+            }
+            answer.add(count);
+            i++;
+        }
         return answer;
     }
 }
