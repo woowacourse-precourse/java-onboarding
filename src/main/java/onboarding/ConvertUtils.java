435eaa80e264aa4a96f739df288b068772aa234a
JohnPrk <uo3641493@gmail.com>
2022-11-01 17:10:25 +0900

    feat : ConvertUtils 클래스 및 테스트 작성(사전의 반전 크기를 기준으로 알파벳을 반전시키는 Map 생성)

---
+++ src/main/java/onboarding/ConvertUtils.java
@@ -0,0 +1,32 @@
+package onboarding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConvertUtils {
+
+    private static final int LARGE_A = 65;
+    private static final int LARGE_Z = 90;
+    private static final int SMALL_A = 97;
+    private static final int SMALL_Z = 122;
+
+    private final String word;
+    private final Map<Character, Character> convertReverseCharacterMap;
+
+    public ConvertUtils(String word, int reverseSize) {
+        this.word = word;
+        this.convertReverseCharacterMap = getConvertReverseCharacterMap(reverseSize);
+    }
+
+    public Map<Character, Character> getConvertReverseCharacterMap(int reverseSize) {
+        Map<Character, Character> convertReverseCharacterMap = new HashMap<>();
+        for (int i = 0; i < reverseSize; i++) {
+            convertReverseCharacterMap.put((char) (LARGE_A + i), (char) (LARGE_Z - i));
+            convertReverseCharacterMap.put((char) (LARGE_Z - i), (char) (LARGE_A + i));
+            convertReverseCharacterMap.put((char) (SMALL_A + i), (char) (SMALL_Z - i));
+            convertReverseCharacterMap.put((char) (SMALL_Z - i), (char) (SMALL_A + i));
+        }
+        convertReverseCharacterMap.put(' ', ' ');
+        return convertReverseCharacterMap;
+    }
+}
