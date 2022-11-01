67eceb51ac031e81403b5de5209dad2d2aeba302
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-01 19:16:32 +0900

    221101 Style: P4 - 기능 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -7,7 +7,7 @@ public class Problem4 {
         int capitalConstant = 'A' + 'Z';
         int smallConstant = 'a' + 'z';
 
-        //1.입력 문자열을 맨 앞부터 순회하며 대소문자 여부를 판별한다.
+        /*1.입력 문자열을 맨 앞부터 순회하며 대소문자 여부를 판별한다.*/
         for(int i = 0; i < word.length(); i++) {
             char curr = word.charAt(i);
             if(curr >= 'A' && curr <= 'Z'){
@@ -17,11 +17,11 @@ public class Problem4 {
             if(curr >= 'a' && curr <= 'z'){
                 curr = (char)(smallConstant - curr);
             }
-            //2.변환한 암호를 결과 String에 이어붙인다.
+            /*2.변환한 암호를 결과 String에 이어붙인다.*/
             sb.append(curr);
         }
 
-        //3.입력 문자열을 끝까지 순회했다면 결과 String을 반환한다.
+        /*3.입력 문자열을 끝까지 순회했다면 결과 String을 반환한다.*/
         answer = sb.toString();
 
         return answer;
