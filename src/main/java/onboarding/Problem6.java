8abe3a82a95962f3df3331597127f068f16338db
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-11-01 19:43:59 +0900

    feat: Problem6 두 글자로 분할한 닉네임이 2개 이상인 것을 찾기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -14,6 +14,25 @@ public class Problem6 {
     }
 
     /**
+     * 두 글자로 분할한 닉네임이 2개 이상인 것을 찾기
+     * @param nameList
+     * @return List<String>
+     */
+    private static List<String> reduplicatedNames(List<String> nameList) {
+        Set<String> resultSet = new HashSet<>();
+        for (int i = 0; i < nameList.size() - 1; i++) {
+
+            for (int j = i + 1; j < nameList.size(); j++) {
+                if (nameList.get(i).equals(nameList.get(j))) {
+                    resultSet.add(nameList.get(i));
+                }
+            }
+        }
+        List<String> reduNameList = new ArrayList<>(resultSet);
+        return reduNameList;
+    }
+
+    /**
      * 닉네임을 두 글자씩 변환
      * @param forms
      * @return List<String>
