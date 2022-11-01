edd12a84c4883d83ba05a4ad59cdcc6dcfbda493
Dabeen Jeong <hello70825@gmail.com>
2022-11-01 18:50:54 +0900

    refactor: [Problem2] 정규표현식을 적용하다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,62 +1,23 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem2 {
-    private static final int FIND_DUPLICATION = -1;
+    private static final String REGEX_FIND_DUPLICATION = "([a-z])\\1+";
 
     public static String solution(String cryptogram) {
         String answer = "answer";
+        Pattern pattern = Pattern.compile(REGEX_FIND_DUPLICATION);
+        Matcher matcher = pattern.matcher(cryptogram);
 
-        while(checkDuplicateString(cryptogram)) {
-            cryptogram = getAmendString(cryptogram);
+        while(matcher.find()) {
+            cryptogram = matcher.replaceAll("");
+            matcher = pattern.matcher(cryptogram);
         }
 
         answer = cryptogram;
 
         return answer;
-    }
-
-    private static boolean checkDuplicateString(String inputString) {
-        IntStream inputIntStream = inputString.chars();
-        int result = inputIntStream
-                .reduce(0, (a, b) -> a == FIND_DUPLICATION ? a : (a == b) ? FIND_DUPLICATION : b);
-
-        return result == -1;
-    }
-
-    private static String getAmendString(String inputString) {
-        List<Character> resultList = new ArrayList<>();
-        String resultString = null;
-        Character nowChar = null;
-        Character deleteChar = '.';
-        Integer nowSize = null;
-
-        for(int i = 0; i < inputString.length(); i++) {
-            nowSize = resultList.size();
-            nowChar = inputString.charAt(i);
-
-            try {
-                if (resultList.get(nowSize - 1).equals(nowChar)) {
-                    deleteChar = nowChar;
-                    resultList.remove(nowSize - 1);
-                } else if (deleteChar.equals(nowChar)) {
-                    continue;
-                } else {
-                    resultList.add(nowChar);
-                    deleteChar = '.';
-                }
-            } catch (Exception e) {
-                resultList.add(nowChar);
-            }
-        }
-
-        resultString = resultList.stream()
-                        .map(String::valueOf)
-                        .reduce("", String::concat);
-
-        return resultString;
     }
 }
