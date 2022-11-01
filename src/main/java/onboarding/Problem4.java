7432a2157629b11a6edee0c30ab4407332c7a1c8
sun007021 <sun007021@gmail.com>
2022-11-01 11:55:18 +0900

    feat: 해당 문자가 소문자, 대문자, 그외 문자인지 확인

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,17 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    private static int whatKindOfChar(char letter){//소문자 알파벳 =1, 대문자 알파벳 = 2, 그외 -1
+        int ascii = (int)letter; // 문자 아스키코드로 변환
+        if(ascii >=97 && ascii <=122){// 소문자 알파벳인 경우
+            return 1;
+        }
+        else if(ascii >=41 && ascii <=90){// 대문자 알파벳인 경우
+            return 2;
+        }
+        else{// 그외 다른 문자
+            return -1;
+        }
+    }
 }
