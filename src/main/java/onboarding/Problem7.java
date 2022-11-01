b949c59b8007c59a84349bc03b92aca283b0f5cf
Longstick0 <789456jang@naver.com>
2022-11-01 21:23:57 +0900

    refactor: Problem7 코드 리펙토링(변수명, 메소드명 변경)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -13,11 +13,11 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
         for (List<String> friend : friends) {
-            putFriend(friend, user);
+            putFriendToMap(friend, user);
         }
         calcFriendPoints(user);
-        getVisitorsCalc(visitors, user);
-        delFriendsFromPoints(user);
+        calcVisitorPoints(visitors, user);
+        delDuplicate(user);
 
         friendPointMap.entrySet().stream().sorted(Map.Entry.comparingByKey());
         friendPointMap.entrySet().stream()
@@ -28,15 +28,15 @@ public class Problem7 {
         return answer;
     }
 
-    private static void putFriend(List<String> friend, String user) {
+    private static void putFriendToMap(List<String> friend, String user) {
         if (!friend.get(FRIEND_A).equals(user)){
             friendPointMap.put(friend.get(FRIEND_A), 0);
         }
-        if (!friend.get(FRIEND_B).equals(user)) {
-            friendPointMap.put(friend.get(FRIEND_B), 0);
-        }
         if (!friendsMap.containsKey(friend.get(FRIEND_A))) {
             friendsMap.put(friend.get(FRIEND_A), new HashSet<>());
+        }
+        if (!friend.get(FRIEND_B).equals(user)) {
+            friendPointMap.put(friend.get(FRIEND_B), 0);
         }
         if (!friendsMap.containsKey(friend.get(FRIEND_B))) {
             friendsMap.put(friend.get(FRIEND_B), new HashSet<>());
@@ -48,16 +48,15 @@ public class Problem7 {
 
     private static void calcFriendPoints(String user) {
         Set<String> userFriends = friendsMap.get(user);
-        Set<String> chainFriends;
         for (String userFriend : userFriends) {
-            chainFriends = friendsMap.get(userFriend);
-            for (String chainFriend : chainFriends) {
-                if (friendPointMap.containsKey(chainFriend)) friendPointMap.put(chainFriend, friendPointMap.get(chainFriend) + 10);
+            for (String chainFriend : friendsMap.get(userFriend)) {
+                if (friendPointMap.containsKey(chainFriend))
+                    friendPointMap.put(chainFriend, friendPointMap.get(chainFriend) + 10);
             }
         }
     }
 
-    private static void getVisitorsCalc(List<String> visitors, String user) {
+    private static void calcVisitorPoints(List<String> visitors, String user) {
         for (String visitor : visitors) {
             if (friendsMap.get(user).contains(visitor)) continue;
             if (!friendPointMap.containsKey(visitor)) friendPointMap.put(visitor, 0);
@@ -65,7 +64,7 @@ public class Problem7 {
         }
     }
 
-    private static void delFriendsFromPoints(String user) {
+    private static void delDuplicate(String user) {
         for (String friend : friendsMap.get(user)) {
             friendPointMap.remove(friend);
         }
