8bfc88d8ecefb1c8729f918da2fdcc0fb3d4f21d
SeongbinPark <binni0515@gmail.com>
2022-11-01 21:00:16 +0900

    [Problem2] 1. 입력된 cryptogram을 한 문자씩 eachWords 라는 Queue에 저장

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,39 +1,32 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        int point1 = 0;
-        int point2 = 1;
+        while (isDuplicated(cryptogram)) {
+            Queue<Character> eachWords = new ArrayDeque<>();
 
-        // point2 가 cryptogram의 길이보다 작을 때 까지 반복
-        while (point2 < cryptogram.length()) {
-            char first = cryptogram.charAt(point1);
-            char second = cryptogram.charAt(point2);
-
-            if (isSameWord(first, second)) {
-                cryptogram = getNewCryptogram(cryptogram, point1, point2);
-
-                // 다시 처음부터 탐색
-                point1 = 0;
-                point2 = 1;
-                continue;
+            for (int i = 0; i < cryptogram.length(); i++) {
+                eachWords.offer(cryptogram.charAt(i));
             }
 
-            // 가리키는 문자가 같은 문자가 아닐 경우 포인터 이동
-            point1++;
-            point2++;
+            StringBuilder answer = new StringBuilder();
+
+            cryptogram = answer.toString();
         }
+
         return cryptogram;
     }
 
-    private static String getNewCryptogram(String cryptogram, int point1, int point2) {
-        return cryptogram.substring(0, point1) + cryptogram.substring(point2 + 1);
-    }
-
-    private static boolean isSameWord(char first, char second) {
-        if (first == second) {
-            return true;
+    private static boolean isDuplicated(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                return true;
+            }
         }
         return false;
     }
+
 }
