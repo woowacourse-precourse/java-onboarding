98eb3c56358e8ef6074e6306972c0431893fc775
김형기 <khk211113@naver.com>
2022-11-01 23:24:12 +0900

    [FEAT] Problem4 - 전체 문장 변환 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,12 @@
 package onboarding;
 
 public class Problem4 {
+
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            answer = answer + transChar(word.charAt(i));
+        }
         return answer;
     }
 
