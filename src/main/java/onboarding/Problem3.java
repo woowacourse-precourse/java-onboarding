199c26fed195f54ad037e2082c3fea8328890d1f
장한울 <swingjhw@naver.com>
2022-11-01 09:28:11 +0900

    feat(문제4) : 알파벳 변환해주는 hashMap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,30 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+//        int num = 1; // 자릿수 구분
+//        int answer = 0;
+//        int tmpNumber = number;
+//        while (tmpNumber/10 != 0) {
+//
+//            num *= 10;
+//            int tmpAnswer = 0;
+//            int tmp = number / num;
+//            int remaider = tmpNumber % 10;
+//            tmpAnswer += 3 * tmp;
+//            tmpAnswer += remaider / 3;
+//
+//            answer += tmpAnswer * num / 10;
+//            tmpNumber /= 10;
+//            System.out.println(answer);
+
+
+        int answer = 1;
 
         return answer;
     }
+
+
+
+
+
 }
--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,40 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
     public static String solution(String word) {
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        int A = 'A';
+        int Z = 'Z';
+        int a = 'a';
+        int z = 'z';
+
+        for (int i = 65; i < 91; i++) {
+            char frCh = (char) i;
+            int tmp = 65+(Z-i);
+            char toCh = (char) tmp;
+            map.put(frCh, toCh);
+        }
+        for (int i = 97; i < 123; i++) {
+            char frCh = (char) i;
+            int tmp = 97+(z-i);
+            char toCh = (char) tmp;
+            map.put(frCh, toCh);
+        }
+
+        
+
+
+
+
         String answer = "";
         return answer;
+    }
+
+    public static boolean isAlpha(char ch) {
+        return true;
     }
 }
