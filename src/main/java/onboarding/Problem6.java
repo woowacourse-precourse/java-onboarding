5f42cbcc6503063fb7a73f5b74cd5016c06f6171
khj <kim37171@cuk.edu>
2022-11-01 21:04:42 +0900

    [Problem6] 닉네임 중 두 글자 이상 연속적으로 중복되는 지원자들의 이메일 리스트 구하기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,23 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> emails = new LinkedList<>();
+        return emails;
+    }
+    // Function that returns a list of crew's emails if the crew's nickname contain same characters in series.
+    private static List<String> getCrewsEmails(List<String> nowCrew, List<String> nextCrew) {
+        List<String> crewsEmails = new LinkedList<>();
+        for (int k = 0; k < nowCrew.get(1).length()-1; k++) {
+            if(nextCrew.get(1).contains(nowCrew.get(1).substring(k,k+2))){
+                crewsEmails.add(nowCrew.get(0));
+                crewsEmails.add(nextCrew.get(0));
+                break;
+            }
+        }
+        return crewsEmails;
     }
 }
