d8208dec5fb9c827cd1d680ff38279b42fdcf52a
UngHeung <t01047377477@gmail.com>
2022-11-01 22:55:31 +0900

    feat : (problem 6) 중복 여부를 확인하는 기능 추가
    목표 닉네임과 비교 닉네임을 주었을 때
    목표 닉네임 안의 두글자 이상의 단어가 비교 닉네임에 포함되었을 경우
    true를 리턴해주는 메소드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,20 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+	public static List<String> solution(List<List<String>> forms) {
+		List<String> answer = List.of("answer");
+		return answer;
+	}
+
+	public static boolean dupleCheck(String target, String compare) {
+		char[] targetWord = target.toCharArray();
+
+		for(int i = 0; i <targetWord.length - 1; i++) {
+			if(compare.contains("" + targetWord[i] + targetWord[i + 1])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
