fd47907abe401c9a4ee6551bdf7867dac543ebdc
ryuj-h <dbwlsgh57@naver.com>
2022-11-01 16:31:29 +0900

    Solve Problem 1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,13 +10,34 @@ import static java.lang.Math.max;
 
 class Problem1 {
     static int getMax(List<Integer> input) {
-        return 0;
+        int leftSum = 0,leftMulti = 1,rightSum = 0,rightMulti = 1;
+        int leftPage = input.get(0);
+        int rightPage = input.get(1);
+        ArrayList <Integer> list = new ArrayList<Integer>();
+
+        while (leftPage > 0) {
+            int rest = leftPage % 10;
+            leftSum += rest;
+            leftMulti *= rest;
+            leftPage /= 10;
+        }
+        while (rightPage > 0) {
+            int rest = rightPage % 10;
+            rightSum += rest;
+            rightMulti *= rest;
+            rightPage /= 10;
+        }
+        list.add(leftSum);
+        list.add(leftMulti);
+        list.add(rightSum);
+        list.add(rightMulti);
+
+        return Collections.max(list);
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        int pobipoint;
-        int crongpoint;
+        int pobipoint, crongpoint;
 
         if (abs(pobi.get(0) - pobi.get(1)) > 1 || abs(crong.get(0) - crong.get(1)) > 1)
             return -1;
