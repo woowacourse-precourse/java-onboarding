dcf04fad6b0d3674ccb58fe0366fde5fe9b6338d
HiiWee <wpdlzhf159@gmail.com>
2022-11-01 18:35:12 +0900

    Feat problem7: 각 크루에 대해 친구 목록 리스트 맵으로 저장

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static final int FIRST_ID = 0;
+    private static final int SECOND_ID = 1;
+    private static Map<String, List<String>> friendsMap = new TreeMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        makeListOfFriends(friends);
+
+        return new ArrayList<>();
     }
+
+    private static void makeListOfFriends(List<List<String>> friends) {
+        for (List<String> twoFriends : friends) {
+            putToFriendsMap(twoFriends.get(FIRST_ID), twoFriends.get(SECOND_ID));
+            putToFriendsMap(twoFriends.get(SECOND_ID), twoFriends.get(FIRST_ID));
+        }
+    }
+
+    private static void putToFriendsMap(String key, String valueOfList) {
+        if (friendsMap.containsKey(key)) {
+            friendsMap.get(key).add(valueOfList);
+            return;
+        }
+        ArrayList<String> valueList = new ArrayList<>();
+        valueList.add(valueOfList);
+        friendsMap.put(key, valueList);
+    }
+
 }
