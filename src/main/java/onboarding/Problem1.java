2cd800bd972cc1570a0403f38da7813e271bc540
이경범 <tkdwk567@naver.com>
2022-11-01 15:54:46 +0900

    # 1 solution @@ refactoring - refactoring

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,107 +1,27 @@
 package onboarding;
 
-import onboarding.problem1.ConditionValue;
 import onboarding.problem1.infra.PageNumberAndSizeValidation;
+import onboarding.problem1.serivce.PageService;
+import onboarding.problem1.type.ResultValue;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Problem1 {
 
+    private static final PageService pageService = new PageService();
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        PageNumberAndSizeValidation validation = new PageNumberAndSizeValidation();
-        validation.settingFixedPageSize(ConditionValue.FIXED_LIST_SIZE.getValue());
+        try {
+            PageNumberAndSizeValidation validation = new PageNumberAndSizeValidation();
 
-        // TODO 요청 데이터 검증 로직 예외 처리 하는 것이 낫다고 봄
-        if (!validation.isPageSize(pobi.size(), crong.size())) {
-            return ConditionValue.EXCEPTION_VALUE.getValue();
+            int pobiMaxValue = pageService.findMaxValue(pobi, validation);
+            int crongMaxValue = pageService.findMaxValue(crong, validation);
+
+            return pageService.getResult(pobiMaxValue, crongMaxValue);
+        } catch (IllegalArgumentException e) {
+
+            System.out.println(e.getMessage());
+            return ResultValue.EXCEPTION_VALUE.getValue();
         }
-
-        int pobiMaxValue = findMaxValue(pobi, validation);
-        int crongMaxValue = findMaxValue(crong, validation);
-
-        return getResult(pobiMaxValue, crongMaxValue);
-    }
-
-    // TODO 비지니스 로직이니 service 코드로 분리
-    protected static int findMaxValue(List<Integer> woowahanTeachers, PageNumberAndSizeValidation validation) {
-        Integer leftPageNumber = woowahanTeachers.get(ConditionValue.LEFT_INDEX.getValue());
-        Integer rightPageNumber = woowahanTeachers.get(ConditionValue.RIGHT_INDEX.getValue());
-
-        validation.settingPageNumber(leftPageNumber, rightPageNumber);
-        if (!validation.isPageNumberValidation()) {
-            return ConditionValue.EXCEPTION_VALUE.getValue();
-        }
-
-        int sumMaxValue = getSumMaxValue(leftPageNumber, rightPageNumber);
-        int multiplicationMaxValue = getMultiplicationMaxValue(leftPageNumber, rightPageNumber);
-
-        return Math.max(sumMaxValue, multiplicationMaxValue);
-    }
-
-    // TODO 비지니스 로직이니 service 코드로 분리
-    protected static int getSumMaxValue(Integer leftPageNumber, Integer rightPageNumber) {
-        int leftSumValue = getSumValue(leftPageNumber);
-        int rightSumValue = getSumValue(rightPageNumber);
-
-        return Math.max(leftSumValue, rightSumValue);
-    }
-
-    protected static int getSumValue(Integer pageNumber) {
-        String[] leftPageNumberStringSplit = pageNumberToStringSplitArray(pageNumber);
-        return getSum(leftPageNumberStringSplit);
-    }
-
-    protected static int getSum(String[] pageNumberStringSplit) {
-        return Arrays.stream(pageNumberStringSplit)
-                .mapToInt(Integer::parseInt)
-                .sum();
-    }
-
-    protected static int getMultiplicationMaxValue(Integer leftPageNumber, Integer rightPageNumber) {
-        int leftValue = getMultiplicationValue(leftPageNumber);
-        int rightValue = getMultiplicationValue(rightPageNumber);
-
-        return Math.max(leftValue, rightValue);
-    }
-
-    protected static int getMultiplicationValue(Integer pageNumber) {
-        String[] PageNumberStringSplit = pageNumberToStringSplitArray(pageNumber);
-        int leftValue = 1;
-
-        for (String data : PageNumberStringSplit) {
-            try {
-                leftValue *= Integer.parseInt(data);
-            } catch (NumberFormatException e) {
-                return ConditionValue.EXCEPTION_VALUE.getValue();
-            }
-        }
-
-        return leftValue;
-    }
-
-    protected static String[] pageNumberToStringSplitArray(Integer pageNumber) {
-        return String.valueOf(pageNumber).split("");
-    }
-
-    protected static int getResult(int pobiMaxValue, int crongMaxValue) {
-        Integer exceptionValue = ConditionValue.EXCEPTION_VALUE.getValue();
-        if (pobiMaxValue == exceptionValue || crongMaxValue == exceptionValue) {
-            return exceptionValue;
-        }
-
-        if (pobiMaxValue > crongMaxValue) {
-            return ConditionValue.POBI_WIN.getValue();
-        }
-
-        if (pobiMaxValue < crongMaxValue) {
-            return ConditionValue.CRONG_WIN.getValue();
-        }
-
-        if (pobiMaxValue == crongMaxValue) {
-            return ConditionValue.POBI_CRONG_DRAW.getValue();
-        }
-
-        return exceptionValue;
     }
 }
