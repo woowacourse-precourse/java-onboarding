f439e84f04fc556324d05a3b2b62fcf46a1a1b6a
ho2n <jungho2n@gmail.com>
2022-11-01 17:47:10 +0900

    친구맵과 점수맵 생성하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,47 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        HashMap<String, HashSet<String>> friendMap = new HashMap<>();
+        HashMap<String, Integer> scoreMap = new HashMap<>();
+        List<String> result = new ArrayList<>();
+
+        fillFriendAndScoreMap(friends, friendMap, scoreMap);
+
         return answer;
+    }
+
+    private static void fillFriendAndScoreMap(List<List<String>> friends, HashMap<String, HashSet<String>> friendMap, HashMap<String, Integer> scoreMap) {
+        for(List<String> friend : friends) {
+            if (friendMap.containsKey(friend.get(0))) {
+                HashSet<String> values = friendMap.get(friend.get(0));
+                values.add(friend.get(1));
+                friendMap.put(friend.get(0), values);
+            } else {
+                HashSet<String> values = new HashSet<>();
+                values.add(friend.get(1));
+                friendMap.put(friend.get(0), values);
+                scoreMap.put(friend.get(0), 0);
+            }
+
+            if (friendMap.containsKey(friend.get(1))) {
+                HashSet<String> values = friendMap.get(friend.get(1));
+                values.add(friend.get(0));
+                friendMap.put(friend.get(1), values);
+            } else {
+                HashSet<String> values = new HashSet<>();
+                values.add(friend.get(0));
+                friendMap.put(friend.get(1), values);
+                scoreMap.put(friend.get(1), 0);
+            }
+        }
     }
 }
