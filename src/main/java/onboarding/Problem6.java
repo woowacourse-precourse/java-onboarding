2288e10f0fdb3e46da8d14b7660a846c6ef6cab0
Yebin <lyb2325@gmail.com>
2022-11-01 21:23:40 +0900

    feat: Problem6 연속된 두글자 맵 생성 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.String.*;
+
 public class Problem6 {
 
     private static final String EMAIL_DOMAIN = "@email.com";
@@ -16,12 +18,14 @@ public class Problem6 {
             String email = form.get(0);
             String nickname = form.get(1);
 
-            if (isValidForm(email, nickname)){
+            if (isValidForm(email, nickname)){ // email, nicknamee 유효성 검사
+                makeConsecutiveLettersMap(consecutiveLettersMap, form); // 닉네임의 모든 연속적인 두 글자에 대한 맵 만들기
             }
         }
 
         return answer;
     }
+
 
     private static boolean isValidForm(String email, String nickname) {
         boolean isValidEmail = email.endsWith(EMAIL_DOMAIN);
@@ -29,9 +33,37 @@ public class Problem6 {
         return (isValidEmail && isValidNickname);
     }
 
+    private static String getConsecutiveLetters(char firstLetter, char secondLetter){
+        return append(firstLetter)
+                .append(secondLetter)
+                .toString();
+    }
+
+    private static void makeConsecutiveLettersMap(Map<String, ArrayList<String>> lettersMap, List<String> info){
+        String email = info.get(0);
+        String nickname = info.get(1);
+        char[] nickNameArray = nickname.toCharArray();
+
+        for(int i=0;i<nickNameArray.length-1;i++){
+            String letters = getConsecutiveLetters(nickNameArray[i],nickNameArray[i+1]);
+            addToLettersMap(lettersMap, letters, email);
+        }
+
+    }
+    private static void addToLettersMap(Map<String, ArrayList<String>> lettersMap, String letters, String email){
+        ArrayList<String> arr;
+        if(lettersMap.containsKey(letters)){
+            arr = lettersMap.get(letters);
+        }else{
+            arr = new ArrayList<>();
+        }
+        arr.add(email);
+        lettersMap.put(letters,arr);
+    }
+
     private static boolean isHangulNickname(String nickname){
         for(char letter : nickname.toCharArray()){
-            if(!String.valueOf(letter).matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")){
+            if(!valueOf(letter).matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")){
                 return false;
             }
         }
