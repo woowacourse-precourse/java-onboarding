5ca10c34b5b52a208b7221ad55fb96109b20f39c
Giho <poqw741@gmail.com>
2022-11-01 23:01:23 +0900

    add make userfriendlist

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Problem7 {
+class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> userfriendList = new ArrayList<>();
+        List<String> withfriendList = new ArrayList<>();
+        List<String> visitcount = new ArrayList<>(); // 이미 친구인 사람과 user는 뺌
+
+
+        HashMap<String, Integer> map1 = new HashMap<>();
+
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).contains(user)) {
+                if (friends.get(i).get(0) == user) {
+                    userfriendList.add(friends.get(i).get(1));
+                } else if (friends.get(i).get(1) == user) {
+                    userfriendList.add(friends.get(i).get(0));
+                }
+            }
+        }
+
         return answer;
     }
 }
