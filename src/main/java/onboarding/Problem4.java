2c1916b3c2e26554c0031096020773ba5372526c
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-01 23:52:29 +0900

    [feat] Problem4 알파벳 외 문자 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -19,6 +19,8 @@ public class Problem4 {
                     c = (char) (97 + 122 - letter);
                 answer += Character.toString(c);
             }
+            else
+                answer += Character.toString(letter);
         }
         return answer;
     }
