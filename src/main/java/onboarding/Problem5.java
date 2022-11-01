0486054bc052f591d1293e9073386b3a60c535ac
JangHyojoon <fdasfaj9@gmail.com>
2022-11-01 19:32:24 +0900

    문제 5번 기능 2 화폐별 갯수 계산 기능 & 문제 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -6,7 +6,17 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        ArrayList<Integer> wallet = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
         List<Integer> answer = new ArrayList<>();
+        int cash;
+        if (exception(money)) {
+            for(int index=0; index<wallet.size(); index++) {
+                cash = wallet.get(index);
+
+                answer.add(money/cash);
+                money %= cash;
+            }
+        }
         return answer;
     }
     private static boolean exception(int number) {
