c55cedc642d1a9b5c9d87de271d7692269fec4f6
sangho-jeon <jeonsangho1587@gmail.com>
2022-11-01 19:05:38 +0900

    refactor  : problem7 delete legacy -> use SNSUser class

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,24 +1,51 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+
 /*
-* 기능 구현 사항
-* 1. visitor 점수 추가.
-* */
+ * 기능 구현 사항
+ * 1. visitor 점수 추가.
+ * 2. 각 유저별 친구 HashMap<String, List<String>> 으로 구현.
+ * */
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        HashMap<String, Integer> freindScore = visitorScore(visitors);
+
+
+
         return answer;
     }
-    public static HashMap<String, Integer> visitorScore(List<String> visitor){
-        HashMap<String, Integer> friendScore = new HashMap<>();
-        for (String friend:
-             visitor) {
-            friendScore.put(friend, friendScore.getOrDefault(friend, 0));
+
+
+    static class SNSUser implements Comparable<SNSUser>{
+        private int score;
+        private List<String> friendList;
+
+        public SNSUser(){
+            this.score = 0;
+            this.friendList = new ArrayList<>();
         }
-        return friendScore;
+
+        public void addFriend(String friend){
+            this.friendList.add(friend);
+        }
+
+        @Override
+        public int compareTo(SNSUser o) {
+            return this.score - o.score;
+        }
+
+        public void compareFriend(SNSUser user){
+            // todo
+        }
+
+        public void addVisitor(){
+            this.score++;
+        }
     }
+
 }
+
