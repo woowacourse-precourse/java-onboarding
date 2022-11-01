51a6b032ff363fa529093e11df8bda632c83222e
Anthologia <pyoungjy@gmail.com>
2022-11-01 19:27:19 +0900

    feat: Problem 5 사용 가능한 금액권 리스트 생성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,18 +1,25 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
 
     private static final int MIN_MONEY = 1;
     private static final int MAX_MONEY = 1000000;
+    private static final int[] availMoneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = makeAvailMoneyList();
 
         validateMoney(money);
         return answer;
+    }
+
+    private static List<Integer> makeAvailMoneyList() {
+        return Arrays.stream(availMoneyList).boxed().collect(Collectors.toList());
     }
 
     private static void validateMoney(int money) throws IllegalArgumentException {
