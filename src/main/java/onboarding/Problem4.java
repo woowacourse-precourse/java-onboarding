2204f861e81dcb92e21cd950fde7123af4b35a5b
MuHyeonSon <smuhyeon84@gmail.com>
2022-11-01 21:36:54 +0900

    Problem4 청개구리 사전에따라 입력된 문자열 변환 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -14,7 +14,37 @@ public class Problem4 {
             return result;
         }
 
+        // 청개구리 사전에따라 입력된 문자열을 변환
+        for (int i = 0; i < word.length(); i++) {
 
+            if (word.charAt(i) >='A' && word.charAt(i) <= 'Z'){ // 해당 문자가 알파벳 대문자라면
+
+                String replace_string = String.valueOf((char) (90 - ((int) word.charAt(i) - 65))); // 변경 문자 계산
+                //System.out.printf("변경할 문자 : %s, %c \n", replace_string, word.charAt(i));
+
+                StringBuffer temp_word = new StringBuffer(word); // 문자를 바꾸기 위한 replace메서드를 사용하기 위한 StringBuffer 변수 선언
+                temp_word = temp_word.replace(i,i+1, replace_string); // 문자 변경
+
+                //System.out.printf("변경된 문자로 업데이트된 문자열 : %s \n", temp_word);
+
+                word = temp_word.toString(); // 변경된 문자열로 업데이트
+
+            }
+            else if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') { // 해당 문자가 알파벳 소문자라면
+
+                String replace_string = String.valueOf((char) (122 - ((int) word.charAt(i) - 97))); // 변경 문자 계산
+                //System.out.printf("변경할 문자 : %s, %c \n", replace_string, word.charAt(i));
+
+                StringBuffer temp_word = new StringBuffer(word); // 문자를 바꾸기 위한 replace 메서드를 사용하기 위한 StringBuffer 변수 선언
+                temp_word = temp_word.replace(i,i+1, replace_string); // 문자 변경
+                //System.out.printf("변경된 문자로 업데이트된 문자열 : %s \n", temp_word);
+
+                word = temp_word.toString(); // 변경된 문자열로 업데이트
+
+
+            }
+        }
+        String answer = word;
         return answer;
     }
 }
