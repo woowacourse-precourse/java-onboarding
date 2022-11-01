21dbcfef0beebc619a436679ef9a32fad3356376
yenawee <yaena0319@naver.com>
2022-11-01 16:02:54 +0900

    [FEAT] 4. 알파벳인지 확인하는 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,11 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+    }
+    private static boolean is_Alpha(char c){
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+            return true;
+        return false;
     }
 }
