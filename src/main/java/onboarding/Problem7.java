bca657954d2fdeccf953c8c6f0c6190ffda1f000
doongu <must1080@naver.com>
2022-11-01 16:32:45 +0900

    friends로 그래프를 만드는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem7 {
 
@@ -13,8 +10,39 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
 
         makePeopleMap(user, friends, visitors);
-
+        makeGraph(friends);
         return answer;
+    }
+
+    public static HashMap<String, ArrayList<String>> makeGraph(List<List<String>> friends){
+        Map<String, ArrayList<String>> graph = new HashMap<>();
+        String firstFriend;
+        String secondFriend;
+        for (int index=0; index<friends.size(); index++) {
+
+            ArrayList<String> tempList = new ArrayList<>();
+            firstFriend = friends.get(index).get(0);
+            secondFriend = friends.get(index).get(1);
+
+            if (graph.containsKey(firstFriend)){
+                tempList = graph.get(firstFriend);
+                tempList.add(secondFriend);
+            } else if(!graph.containsKey(firstFriend)){
+                tempList.add(secondFriend);
+            }
+            graph.put(firstFriend, tempList);
+
+            if (graph.containsKey(secondFriend)){
+                tempList = graph.get(secondFriend);
+                tempList.add(firstFriend);
+            } else if(!graph.containsKey(secondFriend)){
+                tempList.add(firstFriend);
+            }
+            graph.put(secondFriend, tempList);
+        }
+
+
+        return (HashMap<String, ArrayList<String>>) graph;
     }
 
     public static void makePeopleMap(String user, List<List<String>> friends, List<String> visitors){
