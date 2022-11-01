602bf30181ae35e125d1f28094e7501e872f838f
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-01 20:51:43 +0900

    problem5 : input_exception

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int UNIT[]={50000,10000,5000,1000,500,100,10};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        if(input_exception(money)) return Collections.emptyList();
+        List<Integer> answer = getList(money);
         return answer;
+    }
+
+    private static List<Integer> getList(int money) {
+    }
+
+    private static boolean input_exception(int input){
+        //true is 'exception excution'
+        if(input<1||input>1000000) return true;
+        return false;
     }
 }
