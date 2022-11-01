585ebc0b80a2b43b0513d84cc19a2d5bd0327973
kiarakim <kiarakim413@gmail.com>
2022-11-01 21:33:04 +0900

    feat(Problem7): add method about giving recommended score

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,18 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem7 {
+    private static final Map<String, Integer> scoreMap = new HashMap<>();
     private static final Set<String> friendsOfUserSet = new HashSet<>();
+    private static final int SHARE_POINT = 10;
+    private static final int VISIT_POINT = 1;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         setFriends(friends, user);
 
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void updateScore(List<List<String>> friends, List<String> visitors) {
+        for (List<String> friend : friends) {
+            if (friendsOfUserSet.contains(friend.get(0)) || friendsOfUserSet.contains(friend.get(1))) {
+                giveScore(friend.get(0), SHARE_POINT);
+                giveScore(friend.get(1), SHARE_POINT);
+            }
+        }
+        for (String visitor : visitors) {
+            giveScore(visitor, VISIT_POINT);
+        }
+    }
+
+    public static void giveScore(String user, int score) {
+        scoreMap.put(user, scoreMap.getOrDefault(user, 0) + score);
     }
 
     public static void setFriends(List<List<String>> friends, String user) {
