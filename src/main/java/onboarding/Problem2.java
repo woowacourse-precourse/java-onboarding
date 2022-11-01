5ae5d12d44f139184a301c365defe3b2c7e80b04
parking0 <p980710@gmail.com>
2022-11-01 14:41:18 +0100

    problem 2 is done

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,30 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+        int duplicate=1;
+        int i=0;
+
+        while(duplicate!=0){
+            String tmp="";
+            duplicate=0;
+            int last_dup_index=-1;
+
+            for(i=0;i<cryptogram.length()-1;i++){
+                if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+                    duplicate++;
+                    i++;
+                    last_dup_index=i;
+                }
+                else{
+                    tmp+=cryptogram.charAt(i);
+                }
+            }
+            if(last_dup_index!=cryptogram.length()-1)
+                tmp+=cryptogram.charAt(cryptogram.length()-1);    //insert last index
+            cryptogram=new String(tmp);
+        }
+        answer=cryptogram;
         return answer;
     }
 }
