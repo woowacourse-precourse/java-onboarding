944a143d891e0fe58d4a36f68967c76b3c47c9ad
yeeun <dpdms9621@naver.com>
2022-11-01 17:12:36 +0900

    [Problem7] 모든사람 리스트 중 친구가 아닌 notFriendsList 추출하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -33,6 +33,18 @@ public class Problem7 {
             allList.add(visitor);
         }
 
+        /* 모든 사람 리스트를 반복하면서 친구리스트에 넣는 것이 가능하면(친구가 아님)
+        친구가 아닌 리스트에 추가, 친구리스트에 넣어진 항목 다시 삭제 */
+        for (String all : allList) {
+            if(friendsList.add(all)) {
+                notFriendsList.add(all);
+                friendsList.remove(all);
+            }
+        }
+
+        //친구리스트에 들어있는 유저 삭제
+        friendsList.remove(user);
+
 
         //5개이하만 출력하기 위한 정답List
         List<String> answer = new ArrayList<>();
