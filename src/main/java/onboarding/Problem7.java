690bd3136ffa8456b2c79f973d19cdf590d42fb8
taejeong <sontaewjd@nate.com>
2022-11-01 21:11:56 +0900

    [Feat] Problem7 사용자와 친구인 목록 세팅

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    
+
+    public static void SetFriendList(List<List<String>> friends, HashSet<String> friendList, String user)
+    {
+        for (int i = 0; i < friends.size(); i++)
+        {
+            String name1 = friends.get(i).get(0);
+            String name2 = friends.get(i).get(1);
+            if (name1 == user && !friendList.contains(name2))
+                friendList.add(name2);
+            else if (name2 == user && !friendList.contains(name1))
+                friendList.add(name1);
+        }
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashSet<String> friendList = new HashSet<String>();
+        SetFriendList(friends, friendList, user);
         return answer;
     }
 }
