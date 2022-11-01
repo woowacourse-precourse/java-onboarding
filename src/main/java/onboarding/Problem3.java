480bc1af886d755c1640d3670c8f4d5989b2ba95
Heesom <heesom.hs@gmail.com>
2022-11-01 11:37:57 +0900

    Problem3 : static을 동적으로 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,26 +2,29 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        return totalNum(number);
+        Problem3 p3 = new Problem3();
+        return p3.totalNum(number);
     }
 
-    public static int totalNum(int number) {
+    public int totalNum(int number) {
+        Problem3 p3 = new Problem3();
         int countNum=0;
         for(int i=1; i<=number; i++){
             System.out.print(i + " ");
-            countNum += oneNumToCharArr(i);
+            countNum += p3.oneNumToCharArr(i);
         }
         return countNum;
     }
 
-    public static int oneNumToCharArr(int number){
+    public int oneNumToCharArr(int number){
+        Problem3 p3 = new Problem3();
         String str = String.valueOf(number);
         char[] charArr = str.toCharArray();
 
-        return countNum(charArr);
+        return p3. countNum(charArr);
     }
 
-    public static int countNum (char[] charArr){
+    public int countNum (char[] charArr){
         int countNum=0;
         for(char ch : charArr){
             if(ch == '3' || ch == '6' || ch == '9'){
