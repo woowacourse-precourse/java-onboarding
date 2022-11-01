b059db745a0556de87b73aafc45a1922cf14d8d0
hataerin <goil1113@likelion.org>
2022-11-01 19:49:48 +0900

    DOCS: Problem2 기능 목록 작성
    Signed-off-by: hataerin <goil1113@likelion.org>

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,10 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        //중복 문자 제거 기능
+
+
+        //결과값 출력 기능
         String answer = "answer";
         return answer;
     }
