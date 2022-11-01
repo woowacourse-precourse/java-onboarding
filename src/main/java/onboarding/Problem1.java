4216db55998f45707a084ceab7f014cfc2298ed4
a1dotls <meeae9408@gmail.com>
2022-11-01 19:00:46 +0900

    2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다. 3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,30 +6,70 @@ import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
-	
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-    	
-    	int pPage = (int)((400)*Math.random( ) + 1);
-    	int cPage = (int)((400)*Math.random( ) + 1);
-    	
-    	if(pPage % 2 == 0) {
-    		pobi.add(pPage-1);
-    		pobi.add(pPage);
-    	} else {
-    		pobi.add(pPage);
-    		pobi.add(pPage+1);
-    	}
-    	
-    	if(cPage % 2 == 0) {
-    		crong.add(cPage-1);
-    		crong.add(cPage);
-    	} else {
-    		crong.add(cPage);
-    		crong.add(cPage+1);
-    	}
-    	
-        int answer = Integer.MAX_VALUE;
-    		
-        return answer;
-    }
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int sum = 0;
+		int gop = 0;
+
+		int pPage = (int) ((400) * Math.random() + 1);
+		int cPage = (int) ((400) * Math.random() + 1);
+
+		if (pPage % 2 == 0) {
+			pobi.add(pPage - 1);
+			pobi.add(pPage);
+		} else {
+			pobi.add(pPage);
+			pobi.add(pPage + 1);
+		}
+
+		if (cPage % 2 == 0) {
+			crong.add(cPage - 1);
+			crong.add(cPage);
+		} else {
+			crong.add(cPage);
+			crong.add(cPage + 1);
+		}
+
+		for (int num : pobi) {
+
+			a = num / 100;
+			b = num / 10 - (a * 10);
+			c = num % 10;
+			sum = a + b + c;
+
+			if (a == 0 && b == 0) {
+				gop = c;
+			} else if (a == 0) {
+				gop = b * c;
+			} else {
+				gop = a * b * c;
+			}
+
+		}
+
+		for (int num : crong) {
+
+			a = num / 100;
+			b = num / 10 - (a * 10);
+			c = num % 10;
+			sum = a + b + c;
+
+			if (a == 0 && b == 0) {
+				gop = c;
+			} else if (a == 0) {
+				gop = b * c;
+			} else {
+				gop = a * b * c;
+			}
+
+		}
+
+		int answer = Integer.MAX_VALUE;
+
+		return answer;
+	}
 }
