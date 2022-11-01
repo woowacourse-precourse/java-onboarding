068fe0eece0b6cdc58577386fc32eea9e6c321f4
xi_jjun <rlawowns97@naver.com>
2022-11-01 17:52:17 +0900

    feat(Problem7): 친구관계 그래프 생성 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,33 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
+    static final Map<String, List<String>> friendRelationGraph = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    public static void makeFriendRelationGraph(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            addFriendRelationGraph(friend);
+        }
+    }
+
+    private static void addFriendRelationGraph(List<String> friend) {
+        String friendFrom = friend.get(0);
+        String friendTo = friend.get(1);
+		friendRelationGraph.putIfAbsent(friendFrom, new ArrayList<>());
+		friendRelationGraph.putIfAbsent(friendTo, new ArrayList<>());
+        friendRelationGraph.getOrDefault(friendFrom, new ArrayList<>())
+				.add(friendTo);
+		friendRelationGraph.getOrDefault(friendTo, new ArrayList<>())
+				.add(friendFrom);
     }
 }
