a02dcedd95eed145c58ea01dd4306039224977aa
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-11-01 23:08:05 +0900

    faiil

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,40 @@ package onboarding;
 
 import java.util.List;
 
+import static java.lang.Integer.max;
+
 class Problem1 {
+
+    public static int calculateScore(int page){
+        int multi = 1;
+        int add = 0;
+
+        int divider = 10;
+        while(page != 0){
+            int tmp = page % divider;
+            multi *= tmp;
+            add += tmp;
+            page /= divider;
+        }
+
+        return max(multi, add);
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+
+        int pobiScore = max(calculateScore(pobi.get(0)), calculateScore(pobi.get(1)));
+        int crongScore = max(calculateScore(crong.get(0)), calculateScore(crong.get(1)));
+
+        if (pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
+            return -1;
+        }
+
+        if (pobiScore > crongScore) {
+            return 1;
+        } else if (pobiScore < crongScore) {
+            return 2;
+        } else{
+            return 0;
+        }
     }
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        char[] arr = new char[cryptogram.length()];
+
+        for(int i = 0; i < cryptogram.length(); i++){
+            arr[i] = cryptogram.charAt(i);
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            
+        }
+
         return answer;
     }
 }
