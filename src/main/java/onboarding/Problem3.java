97163eb05ed6356d5d0c676799899366811db608
이수진 <slrkajsep337@naver.com>
2022-11-01 23:06:45 +0900

    기능요구사항) 문제 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        String num;
+
+        for(int i=1; i<number+1; i++) {
+            num = Integer.toString(i);
+            for(char c: num.toCharArray()) {
+                if (c=='3' || c=='6' || c=='9') {
+                    answer ++;
+                }
+            }
+        }
+
         return answer;
     }
 }
