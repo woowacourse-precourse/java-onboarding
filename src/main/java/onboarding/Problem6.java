dcd0c55c000d35323fc247857b405d36a4076237
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-01 11:43:54 +0900

    Refactor(Problem6) : setInfoMap 메서드 분리 forms 데이터를 저장하는 map을 만드는 메서드 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,36 +5,16 @@ import java.util.*;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        Map<String,String> m = new HashMap<>();
-        Set<String> s = new HashSet<>();
-        String nickname;
-        String email;
-        String twoLetters;
-        for(List<String> l: forms) {
-            email = l.get(0);
-            nickname = l.get(1);
-            for(int j = 0;j < nickname.length()-1; j++) {
-                twoLetters = nickname.substring(j,j+2);
-                if(m.containsKey(twoLetters)) {
-                    s.add(m.get(twoLetters));
-                    s.add(email);
-                } else {
-                    m.put(twoLetters, email);
-                }
-            }
-        }
-
-        List<String> emailList = new ArrayList<>();
-        emailList.addAll(s);
-        String[] emailArray = new String[emailList.size()];
-        emailList.toArray(emailArray);
-        Arrays.sort(emailArray);
-        answer = List.of(emailArray);
-
-
+        Map<String, String> infoMap = setInfoMap(forms);
         return answer;
     }
 
-
-
+    private static Map<String, String> setInfoMap(List<List<String>> form) {
+        // k : nickname | v : email
+        Map<String, String> m = new HashMap<>();
+        for(List<String> l : form) {
+            m.put(l.get(1),l.get(0));
+        }
+        return m;
+    }
 }
