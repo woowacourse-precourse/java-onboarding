2fe4f92ccfe1d0d948b175afa5412074d8dca903
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:28:24 +0900

    Feat: Change 솔루션 함수 로직

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,11 +2,12 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = removeDuplicate(cryptogram);
+
         return answer;
     }
 
-    
+
     private static String removeDuplicate(String word) {
 
         char recentChar = 0;
