aa62e7f1e5cc76aad5b7db32620db76814502dfa
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-01 23:44:14 +0900

    Problem 6 : 닉네임을 두 글자씩 나누는 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,22 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+    // 기능 목록 1 : 닉네임을 두 글자씩 나누는 함수
+    public static List<String> makeNewnickname(String nickname){
+        List<String> new_nickname = new ArrayList<>();
+
+        for(int i=0;i<nickname.length()-1;i++){
+            String temp = "";
+            temp+=nickname.charAt(i);
+            temp+=nickname.charAt((i+1));
+            new_nickname.add(temp);
+        }
+
+        return new_nickname;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
