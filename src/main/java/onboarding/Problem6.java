1bf5d8d669ed363b748bfbda0f1fef3b9dc71dfe
DongWooE <ehddn977@naver.com>
2022-11-01 18:33:47 +0900

    [p6][add] forms를 순차적으로 조회하는 메소드 iterate 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+
+    List<List<String>> forms = new ArrayList<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Problem6 p6 = new Problem6();
+        p6.forms = forms;
+        p6.iterate();
+        return null;
+    }
+
+    private void iterate(){
+        for(List<String> form : forms){
+
+        }
     }
 }
