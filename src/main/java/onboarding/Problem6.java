2f48b918b3fbfa9933161b6b94f2488e35e001c2
이지영 <jioung93@naver.com>
2022-11-01 21:39:56 +0900

    [Problem 6] style: 코드 정렬 및 불필요한 변수 제거

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -14,33 +14,28 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
 
-        for (int i=0; i<forms.size(); i++){
+        for (int i = 0; i < forms.size(); i++) {
             String nickname1 = forms.get(i).get(1);
-
-            for (int j=i+1; j<forms.size(); j++) {
+            for (int j = i + 1; j < forms.size(); j++) {
                 String nickname2 = forms.get(j).get(1);
                 boolean isDuplicate = isDuplicate(nickname1, nickname2);
-
-                if(isDuplicate) {
-                    String email1 = forms.get(i).get(0);
-                    String email2 = forms.get(j).get(0);
-                    addEmail(answer, email1);
-                    addEmail(answer, email2);
+                if (isDuplicate) {
+                    addEmail(answer, forms.get(i).get(0));
+                    addEmail(answer, forms.get(j).get(0));
                 }
             }
         }
-
         Collections.sort(answer);
         return answer;
     }
 
     // 두 닉네임의 중복 여부를 검사하는 함수 isDuplicate()
     private static boolean isDuplicate(String nickname1, String nickname2) {
-        for (int i=0; i<nickname1.length()-1; i++){
-            String substr1 = nickname1.substring(i, i+2);
-            for (int j=0; j<nickname2.length()-1; j++){
-                String substr2 = nickname2.substring(j, j+2);
-                if(substr1.equals(substr2)) {
+        for (int i = 0; i < nickname1.length() - 1; i++) {
+            String substr1 = nickname1.substring(i, i + 2);
+            for (int j = 0; j < nickname2.length() - 1; j++) {
+                String substr2 = nickname2.substring(j, j + 2);
+                if (substr1.equals(substr2)) {
                     return true;
                 }
             }
@@ -49,8 +44,8 @@ public class Problem6 {
     }
 
     // 리스트에 없는 이메일만 추가하는 함수 addEmail()
-    private static void addEmail(List<String> answer, String email){
-        if(!answer.contains(email)){
+    private static void addEmail(List<String> answer, String email) {
+        if (!answer.contains(email)) {
             answer.add(email);
         }
     }
