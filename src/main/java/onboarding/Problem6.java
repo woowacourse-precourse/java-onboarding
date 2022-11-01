211094cc404f303d62d91956bd210fcc9c9f4cbc
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:35:38 +0900

    Feat: Add 이메일, 닉네임 검증 함수

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -8,6 +8,17 @@ public class Problem6 {
         return answer;
     }
 
+    private static void validationCheck(String email, String name) {
+        if (!validateEmailCheck(email)) {
+            throw new IllegalStateException("이메일 형식에 부합하지 않음");
+        }
+
+        if (!validateNicknameCheck(name)) {
+            throw new IllegalStateException("닉네임 형식에 부합하지 않음");
+        }
+    }
+
+
     private static boolean validateEmailCheck(String email) {
         if (email.endsWith("email.com") &&
                 11 <= email.length() && email.length() < 20) {
@@ -17,7 +28,7 @@ public class Problem6 {
         return false;
     }
 
-    
+
     private static boolean validateNicknameCheck(String nickname) {
         if (nickname.matches("^[가-힣]*$") &&
                 0 < nickname.length() && nickname.length() < 20) {
