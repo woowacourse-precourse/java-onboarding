b6a86925828abb353d0752f330ecb711f80fe8c2
taekwon-dev <taekwon_dev@naver.com>
2022-11-01 10:15:09 +0900

    problem 7 - feat : 친구 관계 정보를 활용하여 각 사용자의 친구 관계 정의

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 public class Problem7 {
+    private static Map<String, List<String>> map = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void makeRelationships(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String from = friend.get(0);
+            String to = friend.get(1);
+            makeFriendBetweenFromAndTo(from, to);
+            makeFriendBetweenFromAndTo(to, from);
+        }
+    }
+
+    private static void makeFriendBetweenFromAndTo(String from, String to) {
+        if (!map.containsKey(from)) {
+            List<String> friends = new ArrayList<>();
+            friends.add(to);
+            map.put(from, friends);
+        } else {
+            map.get(from).add(to);
+        }
     }
 }
