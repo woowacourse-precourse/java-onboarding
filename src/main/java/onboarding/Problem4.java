6c996b68369c62037ed1bf75f6c68c4ff0e7dd75
HangSub999 <gkdtjq999@gmail.com>
2022-11-01 23:31:21 +0900

    4번문제 실패..

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        char[] wordCha = new char[word.length()];
+        int[] rist = {25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1};
+        for (int i = 0; i < wordCha.length; i++) {
+            wordCha[i] = word.charAt(i);
+        }
+        for (int i = 0; i < wordCha.length; i++) {
+            if (wordCha[i] > 64 && wordCha[i] < 78) {
+
+            }
+        }
+        25, 23, 21, 19
+
+        return String.valueOf(upperCase);
+    }
+
+    public static char test(char chars) {
+        int num = 25;
+        chars
     }
 }
