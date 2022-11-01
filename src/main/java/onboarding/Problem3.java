b55b30a993c7e2f6ff1affe6fd1c7f16245b8a91
broden-horizon <rlghd1698@naver.com>
2022-11-01 21:42:59 +0900

    문제3. 1~N 중 일의 자리에 369가 포함된 숫자의 개수를 구하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,16 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    //일의 자리에 369가 들어간 숫자가 총 몇 개인가?
+    // ex) 333
+    // -> howMany10s = 33
+    // ->  units = 3
+    // -> 10번
+    private static int get3or6or9(int num) {
+        int howMany10s = num / 10;
+        int units = num % 10;
+
+        return howMany10s * 3 + units / 3;
+    }
 }
