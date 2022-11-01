2e5fce5632df06e5a2e3bf4790d1a23aece59979
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-01 14:50:14 +0900

    Problem7: 사용자와 친구인 사람 목록 만드는 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem7 {
+    /* checkUserFriend: 사용자와 친구인 사람 목록 만드는 함수 */
+    public static List<String> checkUserFriend(List<List<String>> friends, String user){
+        List<String> userFriends = new ArrayList<>();
+
+        for(int i=0; i<friends.size(); i++){
+            List<String> relation = friends.get(i);
+            if(relation.get(0).equals(user)){ //사용자와 친구인 사람이 있다면 친구인 사람 저장
+                userFriends.add(relation.get(1));
+            } else if (relation.get(1).equals(user)) {
+                userFriends.add(relation.get(0));
+            }
+        }
+
+        return userFriends;
+    }
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> userFriends = new ArrayList<>();
+
+        userFriends = checkUserFriend(friends, user);
+
         return answer;
     }
 }
