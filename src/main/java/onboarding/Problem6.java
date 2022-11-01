db9c0c4934b575c7ee9197efc46f389557a00665
taejeong <sontaewjd@nate.com>
2022-11-01 20:09:24 +0900

    [Feat] 겹치는 닉네임 있으면 리스트에 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < forms.size(); i++)
+            for (int j = 0; j + 1 < forms.get(i).size(); j++)
+                for (int k = i + 1; k < forms.size(); k++)
+                    if (forms.get(k).get(1).contains(forms.get(i).get(1).substring(j, j + 2)))
+                    {
+                        list.add(forms.get(i).get(0));
+                        list.add(forms.get(k).get(0));
+                    }
     }
 }
