489a7ca28f8680e37b9efa1ea472c92769e6a551
kksks3192 <kksks3192@gmail.com>
2022-11-01 11:11:54 +0900

    feat(problem3): 하나의 수에서 369의 개수를 세는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,18 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int countNum(int cur_number){
+        int count = 0;
+        String num = String.valueOf(cur_number);
+        if (num.contains("3") || num.contains("6") || num.contains("9")) {
+            for (int i = 0; i < num.length(); i++) {
+                if (num.charAt(i) == '3' || num.charAt(i) == '6' || num.charAt(i) == '9') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
