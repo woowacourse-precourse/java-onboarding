81c1a75ee4c1bfa185e4907e6258ca341441236c
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-01 21:35:42 +0900

    feat :: 화폐 저장소 기능
    Signed-off-by: joohyeongKim <wnguddl96@knou.ac.kr>

---
+++ src/main/java/problem5/moneyRepository.java
@@ -0,0 +1,22 @@
+package problem5;
+
+public class moneyRepository {
+
+    // 화폐 단위 저장소 기능
+    public static int[] array(int money){
+
+        int[] moneyRepository = new int[9];
+
+        moneyRepository[0] = 50000;
+        moneyRepository[1] = 10000;
+        moneyRepository[2] = 5000;
+        moneyRepository[3] = 1000;
+        moneyRepository[4] = 500;
+        moneyRepository[5] = 100;
+        moneyRepository[6] = 50;
+        moneyRepository[7] = 10;
+        moneyRepository[8] = 1;
+
+        return moneyRepository;
+    }
+}
