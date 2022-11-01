3ed109c3e162b1f74f4d9f7478ab2df4fbf1b23c
HyeonjungYun <guswnd9971@gmail.com>
2022-11-01 21:57:01 +0900

    feat:Problem6 splitNickName() 쪼개진 글자들 다른 닉네임과 비교하는 기능 comapareElement()추가.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -16,6 +16,13 @@ public class Problem6 {
         return nicknameSet;
     }
 
+    public static boolean compareElement (List<String> nicknameSet, String nickname) {
+        for (String nicknameElement: nicknameSet) {
+            if (nickname.contains(nicknameElement)) return true;
+        }
+        return false;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
