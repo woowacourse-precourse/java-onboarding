b030d09b8cc153062c8ed72746dce07f2408d042
박세원 <tpdnjs1825@naver.com>
2022-11-01 23:23:14 +0900

    Feat : 사용자의 친구 목록을 구하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Problem7 {
+    private static void getFriendsList(HashMap<String, List<String>> friendsList, List<List<String>> friends){
+        for(int i = 0; i < friends.size(); i++){
+            String member1 = friends.get(i).get(0);
+            String member2 = friends.get(i).get(1);
+
+            connectFrined(friendsList, member1, member2);
+            connectFrined(friendsList, member2, member1);
+        }
+    }
+
+    private static void connectFrined(HashMap<String, List<String>> friendsList, String member, String friend){
+        if(!friendsList.containsKey(member)) {
+            List<String> memberFriendsList = new ArrayList<>();
+            friendsList.put(member, memberFriendsList);
+        }
+        friendsList.get(member).add(friend);
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<String> userFriends = new ArrayList<>();
+        HashMap<String, List<String>> friendsList = new HashMap<>();
+        HashMap<String, Integer> recommendScore = new HashMap<>();
+
+        getFriendsList(friendsList, friends);
+
         return answer;
     }
 }
+
