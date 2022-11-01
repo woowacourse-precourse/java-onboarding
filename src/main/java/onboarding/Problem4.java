3cb09e0174dad6d843f0c63d43825f9288ecf229
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 13:42:34 +0900

    feat: Problem4 문자를 표에 나와있는것처럼 바꾸기 위해 아스키 코드를 이용해 소문자인 경우와 대문자인 경우에 따른 문자 변환 기능 구현
    Problem4 문자를 표에 나와있는것처럼 바꾸기 위해 아스키 코드를 이용해 소문자인 경우와 대문자인 경우에 따라 문자를 변환해서 반환하도록 구현했습니다

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        
         return answer;
     }
+    public static char change(char c){
+        char result = c;
+        if (c>=65 && c<=90){
+            int temp = c - 65;
+            result = (char) (90-temp);
+        } else if (c >= 97 && c<=122) {
+            int temp = c - 97;
+            result = (char) (122-temp);
+        }
+        return result;
+    }
+
 }
