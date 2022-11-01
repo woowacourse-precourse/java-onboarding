f786aa81abc7ce9b6469fb0e129cf369e6bdb06a
DavinAn <blueapple1030@gmail.com>
2022-11-01 21:37:33 +0900

    Problem5 solution method
    Calculates how many of each currency bill is required to equal the given amount of money.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int j : unit) {
+            answer.add(money / j);
+            money %= j;
+        }
         return answer;
     }
 }
