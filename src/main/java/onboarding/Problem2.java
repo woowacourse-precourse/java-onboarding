2c0307855e49be13b2964f72a83e7685c25ee09a
yewon <yewon.ux@gmail.com>
2022-11-01 21:09:35 +0900

    기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,6 +2,13 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        /*
+        기능 목록
+        1. 각 문자별로 배열에 저장
+        2. 문자 별로 index 'i'와 'i+1'이 같은 문자인 경우 탐색
+        3. 2번 케이스 발생 시 index 'i', 'i+1' 문자 삭제 및 처음부터 재 탐색
+        4. 2번 케이스 미 발생 시 탐색 종료, 문자 출력
+        */
         String answer = "answer";
         return answer;
     }
