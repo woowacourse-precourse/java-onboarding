ccd8a4691d75abdbe1ea4a9827a649281a5e4fdb
yewon <yewon.ux@gmail.com>
2022-11-01 20:30:52 +0900

    기능 목록 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,6 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        /*
+        기능 목록
+        1. A-Z, a-z 배열 생성
+        2. 주어진 문자를 문자별로 저장
+        3. 문자 별로 A-Z, a-z 배열 비교
+        4. 포함되어 있다면 25-index 값을 새 index로 가지는 문자로 출력
+        5. 예외 case 처리 (A-Z, a-z 외의 경우)
+        */
         String answer = "";
         return answer;
     }
