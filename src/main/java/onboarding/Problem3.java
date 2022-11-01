e0d7c7da1aba084146b71bdf590311b9ec3da265
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-01 19:05:13 +0900

    problem3: add comment

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,11 @@
 package onboarding;
 
 public class Problem3 {
+    //digit 숫자가 3,6,9인지 판단하는 함수
     private static boolean isDigit369(int digit){
         return digit == 3 || digit == 6 || digit == 9;
     }
+    //숫자에 대하여 박수 수를 반환하는 함수
     private static int countClap(int num){
         int clap = 0;
         while(num != 0){
