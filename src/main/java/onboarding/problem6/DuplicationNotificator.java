e9e60f13240663a41ff43dbe9ccfc3c4585e08a5
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-01 10:21:37 +0900

    refactor: rename parts to nicknameParts

--- src/main/java/onboarding/problem6/DuplicationNotificator.java
+++ src/main/java/onboarding/problem6/DuplicationNotificator.java
@@ -5,13 +5,13 @@ import java.util.stream.Collectors;
 
 public class DuplicationNotificator {
 
-    private final Map<String, List<String>> parts = new HashMap<>();
+    private final Map<String, List<String>> nicknameParts = new HashMap<>();
 
     public List<String> notifyEmails(List<List<String>> forms) {
         checkDuplication(forms);
         List<String> emails = new ArrayList<>();
-        for (String key : parts.keySet()) {
-            List<String> emailsOfParts = parts.get(key);
+        for (String key : nicknameParts.keySet()) {
+            List<String> emailsOfParts = nicknameParts.get(key);
             if (emailsOfParts.size() > 1)
                 emails.addAll(emailsOfParts);
         }
@@ -25,12 +25,12 @@ public class DuplicationNotificator {
             String nickname = form.get(1);
             for (int i = 0; i < nickname.length() - 1; i++) {
                 String partOfNickname = nickname.substring(i, i + 2);
-                if (parts.containsKey(partOfNickname)) {
-                    List<String> emails = parts.get(partOfNickname);
+                if (nicknameParts.containsKey(partOfNickname)) {
+                    List<String> emails = nicknameParts.get(partOfNickname);
                     if (!emails.contains(email))
                         emails.add(email);
                 } else {
-                    parts.put(partOfNickname, new ArrayList<>(Collections.singletonList(email)));
+                    nicknameParts.put(partOfNickname, new ArrayList<>(Collections.singletonList(email)));
                 }
             }
         }
