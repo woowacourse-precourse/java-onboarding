be2f95824fccd5a1e37d6f58e2d83b22a5c1813e
kki4504 <rhkddlf9876@gmail.com>
2022-11-01 20:29:31 +0900

    feat : user친구 목록 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,38 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    static class RecommendAlgorithm {
+        List<String> userKnownMembers = new ArrayList<>();
+        List<List<String>> userUnknownMembers = new ArrayList<>();
+
+        String user;
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public void checkUserFriends (List<List<String>> friends){
+            for (List<String> friend : friends) {
+                if (friend.contains(user)) {
+                    friend.stream()
+                            .filter(f -> !f.equals(user))
+                            .forEach(x -> userKnownMembers.add(x));
+                }
+                else {
+                    userUnknownMembers.add(friend);
+                }
+            }
+
+        }
+
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
+
 }
