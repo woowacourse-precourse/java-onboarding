01d9527b248d3caac10a435dacead1d8f5bb6481
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-01 17:46:57 +0900

    feat(PROBLEM2): solution 함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,10 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (checkRepetition(cryptogram)){
+            cryptogram = removeRepetition(cryptogram);
+        }
+        return cryptogram;
     }
 }
+
