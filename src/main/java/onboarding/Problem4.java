ff83903dfd65f8415db02cff93c9bc73d384ab7e
KimRiun <kimriun27@gmail.com>
2022-11-01 18:25:49 +0900

    FEAT: Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            answer += convertTo(word.charAt(i));
+        }
         return answer;
+    }
+
+    static char convertTo(char c) {
+        final int LOWER_SUM = 'a' + 'z';
+        final int UPPER_SUM = 'A' + 'Z';
+
+        if (Character.isLowerCase(c)) {
+            return (char)(LOWER_SUM - c);
+        }
+        else if(Character.isUpperCase(c)) {
+            return (char)(UPPER_SUM - c);
+        }
+        else {
+            return c;
+        }
     }
 }
