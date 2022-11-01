7ed932df999d34403283578259fcbf9b65ea843a
jay <jaewhan232@naver.com>
2022-11-01 18:51:08 +0900

    Implement : 화폐 단위별 장수 계산 메소드 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,6 +9,18 @@ public class Problem5 {
 
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        answer = getNumberOfCurrencies(money);
         return answer;
+    }
+
+    private static List<Integer> getNumberOfCurrencies(int money) {
+        List<Integer> numberOfCurrencies = new ArrayList<Integer>();
+
+        for (int i = 0; i< currencies.length; i++) {
+            numberOfCurrencies.add(money/=currencies[i]);
+            money = money%currencies[i];
+        }
+
+        return numberOfCurrencies;
     }
 }
