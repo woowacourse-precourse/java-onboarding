30ca1c8eed47c4aece4cc94fe92325b5bd0119c9
yunjeong <uu0078@naver.com>
2022-11-01 22:54:11 +0900

    Problem5 기능 목록 작성, 구현 1. 화폐 단위 배열에 저장 2. 배열 순회하며 개수 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/* 기능 목록
+1. 화폐 단위 배열에 저장
+2. 배열 순회하며 개수 계산
+ */
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        //List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<Integer>();
+        int[] units = {50_000, 10_000, 5000, 1000, 500, 100, 50, 10, 1};
+        for(int unit : units) {
+            answer.add(money/unit);
+            money %= unit;
+        }
         return answer;
     }
 }
