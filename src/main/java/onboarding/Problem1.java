dc8daeac297442a26330643d4b93e96b814a5a1a
minji0426 <minjee0426@naver.com>
2022-11-01 23:03:13 +0900

    feat: 각 자리의 숫자를 더하는 함수 구현, README.md 파일 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,17 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int pobiPlusScore = plus(pobi.get(0));
+
+
         return answer;
+    }
+
+    // 각 자리의 숫자들 더한 값을 구하는 함수
+    public static int plus(int num){
+        int plus = (num / 100) + ((num / 10) % 10) + (num % 10);
+
+        return plus;
     }
 }
