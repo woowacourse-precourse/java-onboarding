fb26a50d76786cecff0986808f2ef5d79d0e7265
Choi-JY <Choi-JY1107@github.com>
2022-11-01 14:59:33 +0900

    refactor: 기능 별 메소드 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,16 +1,26 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        int upper = (int)'A' + (int)'Z';
-        int lower = (int)'a' + (int)'z';
-        String answer = "";
+    private static final int UPPER_SUM = (int) 'A' + (int)'Z';
+    private static final int LOWER_SUM = (int) 'a' + (int)'z';
 
-        for(char c: word.toCharArray()){
-            if(Character.isUpperCase(c)) answer += (char)(upper-(int)c);
-            else if(Character.isLowerCase(c)) answer += (char)(lower-(int)c);
-            else answer += c;
-        }
+    public static String solution(String word) {
+        String answer = convertWord(word);
         return answer;
+    }
+
+    private static String convertWord(String word) {
+        StringBuilder convertedWord = new StringBuilder();
+
+        for (char c: word.toCharArray()) {
+            convertedWord.append(convertWord(c));
+        }
+        return convertedWord.toString();
+    }
+
+    private static char convertWord(char c) {
+        if (Character.isUpperCase(c)) return (char)(UPPER_SUM - (int)c);
+        if (Character.isLowerCase(c)) return (char)(LOWER_SUM - (int)c);
+        return c;
     }
 }
