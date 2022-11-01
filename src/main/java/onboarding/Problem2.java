25bf289fa856eb91bcbe30a4c9a054ee37569a3c
core <jinjs6@gmail.com>
2022-11-01 17:50:45 +0900

    중복 문자열 범위 확인 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,6 +6,25 @@ public class Problem2 {
         while (true) {
             int start = -1;
             int end = -1;
+            for (int i = 0; i < answer.length() - 1; ++i) {
+                if (answer.charAt(i) == answer.charAt(i + 1)) {
+                    start = i;
+                    end = start + 1;
+                    for (int j = start + 1; j < answer.length(); ++j) {
+                        if (j + 1 < answer.length() && answer.charAt(j) == answer.charAt(j + 1)) {
+                            end = j + 1;
+                        } else {
+                            if (j + 2 < answer.length() && answer.charAt(j + 1) == answer.charAt(j + 2)) {
+                                end = j + 2;
+                                ++j;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
             if (start == -1 && end == -1) {
                 break;
             }
