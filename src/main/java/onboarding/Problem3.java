92345b694e5094ed929dfc539f8f12fdbc00d17b
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-01 12:07:30 +0900

    feat: Problem3 add

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,26 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int cnt = 1; cnt < number + 1; cnt++) {
+            String s = Integer.toString(cnt);
+            for(int i=0;i<s.length();i++){
+                if (check(s.charAt(i))){
+                    answer+=1;
+                }
+            }
+        }
         return answer;
+    }
+    public static Boolean check(char c){
+        if (c=='3'){
+            return true;
+        }
+        else if (c=='6'){
+            return true;
+        }
+        else if (c=='9'){
+            return true;
+        }
+        return false;
     }
 }
