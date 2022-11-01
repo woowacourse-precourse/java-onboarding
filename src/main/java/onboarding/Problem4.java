3da9fec8ecfa5d878020cb04650d9cee591dfbaa
Basak <chgy2131@naver.com>
2022-11-01 12:00:23 +0900

    [feat] 4번 - 입력된 문자열을 한글자씩 확인하는 로직

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,12 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        // 입력된 문자열을 한글자씩 확인하며 청개구리 문자로 바꾼다.
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+        }
+
         return answer;
     }
 }
