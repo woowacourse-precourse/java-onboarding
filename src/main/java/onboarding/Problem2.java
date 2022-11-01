06fcf34cd23fd7d1e2b50ced5a39926bc82e8bd6
kwx4957 <kdy4957@naver.com>
2022-11-01 17:36:54 +0900

    2번 문제 기능 목록 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,16 @@
 package onboarding;
-
+/*
+기능 목록
+1. 연속된 중복 문자 확인
+2. 연속된 문자열 제거
+3. 알파벳인지 확인
+4. 알파벳 소문자인지 확인
+5. 문자열 길이가 1 ~ 1000자 이하인지 확인
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String answer;
+
+        return cryptogram;
     }
 }
