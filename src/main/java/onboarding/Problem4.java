c07b5d236287ac5ac36a1e3b23235169cc8a8eb8
Taekwon Lee <dev.taekwonlee@gmail.com>
2022-11-01 21:10:23 +0900

    feat: Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,28 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        // StringBuilder를 통하여 변경 가능한 문자열 생성
+        StringBuilder answer = new StringBuilder(word);
+
+        for (int i = 0; i < answer.length(); i++) {
+            answer.setCharAt(i, flipWord(answer.charAt(i)));
+        }
+
+        return answer.toString();
+    }
+
+    // isUpperCase, isLowerCase로 대소문자 판별
+    private static char flipWord(char letter) {
+        
+        char result = letter;
+
+        if (Character.isUpperCase(letter))
+            result = (char)('A' + ('Z' - letter));
+
+        if (Character.isLowerCase(letter)) 
+            result = (char)('a' + ('z' - letter));
+
+        return result;
     }
 }
