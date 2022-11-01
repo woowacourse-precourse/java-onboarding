30349f9c14ad19af255cad363af1a7cd1e10b19c
ddukddaki <rlfrkdms1@naver.com>
2022-11-01 17:49:33 +0900

    feat(Problem6): answer(이메일)의 중복을 제거하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,12 +2,13 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 기능 목록
  * 1. forms를 순회하는 기능
  * 2. 닉네임을 2개씩 자르는 기능
- * 3. 자른 닉네임이 다른 크루의 닉네임에 포함되면 anwer에 추가하는 기능
+ * 3. 자른 닉네임이 다른 크루의 닉네임에 포함되면 answer에 추가하는 기능
  * 4. answer(이메일)의 중복을 제거하는 기능
  * 5. answer(이메일)을 정렬하는 기능
  */
@@ -20,7 +21,7 @@ public class Problem6 {
             String nickname = form.get(1);
             for (int j = 0; j < nickname.length() - 1; j++) {
                 String subNickname = nickname.substring(j, j + 2);
-                for (int k = i + 1; k < forms.size(); k++) { 
+                for (int k = i + 1; k < forms.size(); k++) {
                     if (forms.get(k).get(1).contains(subNickname)) {
                         answer.add(forms.get(k).get(0)); //이메일 저장
                         answer.add(form.get(0));
@@ -28,6 +29,9 @@ public class Problem6 {
                 }
             }
         }
+
+        answer = answer.stream().distinct().collect(Collectors.toList());
+
         return answer;
     }
 }
