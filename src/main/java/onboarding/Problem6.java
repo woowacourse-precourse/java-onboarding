19cf4a4178c1b24be34383477b084ec6bb6d9b57
seung-hun2 <jungsh2328@naver.com>
2022-11-01 20:50:30 +0900

    6번 문제 - str에 연속되는 두 글자를 저장하고 다른 닉네임이 연속하는지 if문을 써서 확인 후 연속하면 answer에 같은 글자가 연속적으로 포함되는 닉네임을 저장

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,7 +5,17 @@ import java.util.*;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
-
+        for(int i=0;i< forms.size()-1;i++){
+            for(int k=0;k<forms.get(i).get(1).length()-1;k++){
+                String str = String.valueOf(forms.get(i).get(1).charAt(k))+ forms.get(i).get(1).charAt(k+1);
+                for(int j=i+1;j<forms.size()-1;j++){
+                    if(forms.get(j).get(1).contains(str)){
+                        answer.add(forms.get(i).get(0));
+                        answer.add(forms.get(j).get(0));
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
