b32a8723a114c67ee3c17c02c2409f591cb7bf97
yw824 <yongwoo25@gmail.com>
2022-11-01 11:09:11 +0900

    feat: [Problem6] 기능 1) 입력된 이메일/닉네임 유효성 검사 및 추출
    - SetNamesAndEmails(forms)
    	- 입력된 form의 길이만큼 순회
    	- 해당 인덱스의 데이터 쌍이 유효하면 비교할 배열에 추가
    	- 이메일 배열 : List<String> emails
    	- 닉네임 배열 : List<String> names
    	- 이메일 유효성 검사 함수 호출 : isEmailValid(String email)
    	- 닉네임 유효성 검사 함수 호출 : isNameValid(String name)
    
    - isEmailValid(String email)
    	- 이메일 유효성 검사 함수
    	- 길이가 11 ~ 20자 내부인지 확인 : 아니면 false 반환
    	- @email.com 패턴에 맞는지 확인 : 맞으면 true, 아니면 false
    
    - isNameValid(String name)
    	- 길이 : 1 ~ 20자 이내인지 확인 : 아니면 false
    	- 닉네임이 한글인지 확인 : 맞으면 true, 아니면 false
    		- 한글 정규식 regex 라이브러리 사용

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,9 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Problem6 {
     static List<String> emails;
@@ -15,6 +17,7 @@ public class Problem6 {
         emails = new ArrayList<String>();
 
         // implement 1 : 비교를 위한 이메일 배열과 닉네이 배열 설정
+        SetNamesAndEmails(forms);
 
         // implement 2 : 각 닉네임 간 비교 후 중복 닉네임 처리
 
@@ -26,25 +29,44 @@ public class Problem6 {
     // 입력으로부터 유효한 이메일 및 닉네임만 비교 위한 배열에 추가
     static void SetNamesAndEmails(List<List<String>> forms)
     {
-
+        len = forms.size();
+        boolean valid;
+        for(int i = 0; i < len; ++i)
+        {
+            valid = isEmailValid(forms.get(i).get(0));
+            if(valid == true)
+                valid = isNameValid(forms.get(i).get(1));
+            if(valid == true)
+            {
+                emails.add(forms.get(i).get(0));
+                names.add(forms.get(i).get(1));
+            }
+        }
+        len = emails.size();
+        stringIncluded = new boolean[len];
     }
     // 이메일 형식 준수 여부 확인
     static boolean isEmailValid(String email)
     {
-        //implement
-        // 11자 ~ 20자 이내여야 함
-        // 도메인 : @email.com 만 허용
+        int alpha = email.indexOf('@');
 
-        return true;
+        if(alpha <= 0) return false; // 11자 미만이면 false
+        if(alpha > 10) return false; // 20자 이상이면 false
+
+        // email 형식 틀리면 false, 형식 맞으면 true
+        String substr = email.substring(alpha, email.length());
+        return substr.equals("@email.com");
     }
 
     // 닉네임 형식 준수 여부 확인
     static boolean isNameValid(String name)
     {
-        // implement
         // 길이 : 1 ~ 20자 내부인지 확인
+        int alpha = name.length();
+        if(alpha < 1 || alpha >= 20) return false;
+
         // 닉네임이 한글인지 확인
-        return true;
+        return Pattern.matches("^[ㄱ-ㅎ가-힣]*$", name);
     }
 
     // 각 닉네임을 비교 : 겹치는 두 자 이상의 문자열이 있으면
