a3ec0d225345467cb019ba4326fc078bc4afe048
hyee0715 <hyee0715@naver.com>
2022-11-01 16:38:45 +0900

    feat(Problem2.java): 중복 문자 검증 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,52 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        decode(cryptogram);
+
         return answer;
+    }
+
+    private static void decode(String cryptogram) {
+        Stack<Character> stack = checkDuplicateCharacter(cryptogram);
+    }
+
+    private static Stack<Character> checkDuplicateCharacter(String cryptogram) {
+        Stack<Character> stack = new Stack<>();
+        char prevRemoved = '.';
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) == prevRemoved && prevRemoved != '.')
+                continue;
+
+            prevRemoved = removeDuplicateCharacter(stack, cryptogram, i);
+        }
+
+        return stack;
+    }
+
+    private static char removeDuplicateCharacter(Stack<Character> stack, String cryptogram, int idx) {
+        char prevRemoved = '.';
+        if (stack.empty()) {
+            stack.push(cryptogram.charAt(idx));
+            return prevRemoved;
+        }
+
+        if (stack.peek() != cryptogram.charAt(idx)) {
+            stack.push(cryptogram.charAt(idx));
+            return prevRemoved;
+        }
+
+        if (stack.peek() == cryptogram.charAt(idx)) {
+            while (!stack.empty() && stack.peek() == cryptogram.charAt(idx)) {
+                prevRemoved = stack.pop();
+            }
+        }
+
+        return prevRemoved;
     }
 }
