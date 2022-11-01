b4066908aebd8737f6355efc201ecb7244915bf4
kimminsu <xjfcnfw3@naver.com>
2022-11-01 12:53:55 +0900

    refactor(문제1): Math.max의 중복 함수 제거와 자바 컨벤션 적용

--- src/main/java/onboarding/problem1/Comparator.java
+++ src/main/java/onboarding/problem1/Comparator.java
@@ -3,19 +3,21 @@ package onboarding.problem1;
 import java.util.List;
 
 public class Comparator {
-    private static int compareSumAndMultiplication(int number){
-        return Operator.max(Operator.sum(number), Operator.multiply(number));
+    private static int compareSumAndMultiplication(int number) {
+        return Math.max(Operator.sum(number), Operator.multiply(number));
     }
-    private static int comparePages(int leftPage, int rightPage){
-        return Operator.max(compareSumAndMultiplication(leftPage), compareSumAndMultiplication(rightPage));
+
+    private static int comparePages(int leftPage, int rightPage) {
+        return Math.max(compareSumAndMultiplication(leftPage), compareSumAndMultiplication(rightPage));
     }
-    public static int compareUsers(List<Integer> pobi, List<Integer> crong){
+
+    public static int compareUsers(List<Integer> pobi, List<Integer> crong) {
         int result = 0;
         int pobiNumber = comparePages(pobi.get(0), pobi.get(1));
         int crongNumber = comparePages(crong.get(0), crong.get(1));
-        if (pobiNumber > crongNumber){
+        if (pobiNumber > crongNumber) {
             result = 1;
-        } else if (pobiNumber < crongNumber){
+        } else if (pobiNumber < crongNumber) {
             result = 2;
         }
         return result;
--- src/main/java/onboarding/problem1/Operator.java
+++ src/main/java/onboarding/problem1/Operator.java
@@ -1,22 +1,15 @@
 package onboarding.problem1;
 
 public class Operator {
-    public static int sum(int number){
-        return number / 100 + (number % 100) / 10 + number % 10;
+    public static int sum(int number) {
+        return (number / 100) + ((number % 100) / 10) + (number % 10);
     }
-    public static int multiply(int number){
-        if (number < 10){
+    public static int multiply(int number) {
+        if (number < 10) {
             return number;
-        } else if (number < 100){
+        } else if (number < 100) {
             return (number / 10) * (number % 10);
         }
         return (number / 100) * ((number % 100) / 10) * (number % 10);
-    }
-    public static int max(int number1, int number2){
-        int maxNumber = number1;
-        if (number1 < number2){
-            maxNumber = number2;
-        }
-        return maxNumber;
     }
 }
