946f9d1d42aa6595f231ff2bc226b574de93b932
hataerin <goil1113@likelion.org>
2022-11-01 19:50:57 +0900

    ADD: Problem2 중복문자 제거 기능 구현
    Signed-off-by: hataerin <goil1113@likelion.org>

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,9 +1,22 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        //중복 문자 제거 기능
+        Stack<String> stack = new Stack<>();
+        String[] arr = cryptogram.split("");
 
+        //중복 문자 제거 기능
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.empty()) {
+                stack.push(arr[i]);
+            } else if (stack.peek().equals(arr[i])) {
+                stack.pop();
+            } else {
+                stack.push(arr[i]);
+            }
+        }
 
         //결과값 출력 기능
         String answer = "answer";
