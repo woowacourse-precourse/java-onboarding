9fc24016ef89d011a741e305f2714f3d8c242e53
imsehwan <dla612@naver.com>
2022-11-01 15:37:27 +0900

    feat : 문제 7 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,67 +2,103 @@ package onboarding;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem7 {
-    public static HashMap<String, List<String>> graph;
     public static Map<String, Integer> hashmap;
+    public static Map<String, List<String>> graph;
+    public static List<String> userFriend;
+    public static String userName;
 
-    public static void init(List<List<String>> friends) {
-        graph = new HashMap<>();
+    public static void init(String user, List<List<String>> friends) {
         hashmap = new HashMap<>();
+        graph = new HashMap<>();
+        graph.put(user, new ArrayList<>());
         for (List<String> friend : friends) {
-            graph.put(friend.get(0), new ArrayList<>());
-            graph.put(friend.get(1), new ArrayList<>());
+            graph.put(friend.get(0), graph.getOrDefault(friend.get(0), new ArrayList<>()));
+            graph.put(friend.get(1), graph.getOrDefault(friend.get(1), new ArrayList<>()));
         }
     }
 
     public static void makeGraph(List<List<String>> friends) {
+        userFriend = new ArrayList<>();
         for (List<String> friend : friends) {
+            if(friend.get(0).equals(userName))
+                userFriend.add(friend.get(1));
+            if (friend.get(1).equals(userName)) {
+                userFriend.add(friend.get(0));
+            }
             graph.get(friend.get(0)).add(friend.get(1));
             graph.get(friend.get(1)).add(friend.get(0));
         }
     }
 
-    public static void visit(String userName) {
-        hashmap.put(userName, hashmap.getOrDefault(userName, 0) + 1);
+    // 방문자는 점수 1점
+    public static void visit(List<String> visitors) {
+        for (String visitor : visitors) {
+            hashmap.put(visitor, hashmap.getOrDefault(visitor, 0) + 1);
+        }
     }
 
+    public static class User {
+        private String name;
+        private Integer score;
+
+        public User(String name, Integer score) {
+            this.name = name;
+            this.score = score;
+        }
+    }
+
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-//        init(friends);
+        userName = user;
         List<String> answer = new ArrayList<>();
-//
-//        // 아는 친구 점수 ++
-//        makeGraph(friends);
-//        List<String> list = graph.get(user);
-//        for (String s : list) {
-//            List<String> list1 = graph.get(s);
-//            for (String s1 : list1) {
-//                hashmap.put(s1, hashmap.getOrDefault(s1, 0) + 10);
-//            }
-//        }
-//
-//        // 방문자 점수 ++
-//        for (String visitor : visitors) {
-//            visit(visitor);
-//        }
-//
-//        for (Map.Entry<String, Integer> entry : hashmap.entrySet()) {
-//            answer.add(entry.getKey());
-//        }
-//
-//        Collections.sort(answer, new Comparator<String>() {
-//            @Override
-//            public int compare(String o1, String o2) {
-//                if (hashmap.get(o1) == hashmap.get(o2)) {
-//                    return o1.compareTo(o2);
-//                } else {
-//                    return hashmap.get(o2) - hashmap.get(o1);
-//                }
-//            }
-//        });
-//
-//
-//        return answer.stream().filter(i->i!=user).collect(Collectors.toList()).subList(0,3);
+        init(user, friends);
+        makeGraph(friends);
+        List<String> friend = graph.get(user);
+
+        // user 의 친구들
+        for (String s : friend) {
+            List<String> strings = graph.get(s);
+            // user 의 친구들의 친구들은 플러스 10점
+            for (String string : strings) {
+                hashmap.put(string, hashmap.getOrDefault(string, 0) + 10);
+            }
+        }
+
+        // 방문자는 플러스 1점
+        visit(visitors);
+
+        PriorityQueue<User> priorityQueue = new PriorityQueue<>(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                if (o1.score == o2.score) {
+                    return o1.name.compareTo(o2.name);
+                } else {
+                    return o2.score - o1.score;
+                }
+            }
+        });
+
+        for (Map.Entry<String, Integer> entry : hashmap.entrySet()) {
+            priorityQueue.add(new User(entry.getKey(), entry.getValue()));
+        }
+
+        int reps = 5;
+        while (!priorityQueue.isEmpty()) {
+            if(reps==0)
+                break;
+
+            if (priorityQueue.peek().name.equals(user)||priorityQueue.peek().score==0) {
+                priorityQueue.poll();
+                continue;
+            }
+
+            answer.add(priorityQueue.poll().name);
+            reps--;
+        }
+        answer.removeAll(userFriend);
         return answer;
     }
 }
