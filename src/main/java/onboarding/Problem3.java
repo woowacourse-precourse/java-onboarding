24f4720ff3e1e530e3bd3e8adf2c69e328f2b270
a1dotls <meeae9408@gmail.com>
2022-11-01 23:41:15 +0900

    problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+	public static int solution(int number) {
+
+		int answer = 0;
+
+		for (int i = 1; i <= number; i++) {
+			int a = i / 1000;
+			int b = (i - a * 1000) / 100; 
+			int c = (i - a * 1000 - b * 100) / 10; 
+			int d = i - a * 1000 - b * 100 - c * 10; 
+
+			if (d == 3 || d == 6 || d == 9)
+				answer++;
+			if (c == 3 || c == 6 || c == 9)
+				answer++;
+			if (b == 3 || b == 6 || b == 9)
+				answer++;
+			if (a == 3 || a == 6 || a == 9)
+				answer++;
+		}
+
+		return answer;
+	}
+
 }
