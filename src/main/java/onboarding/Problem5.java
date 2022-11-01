3e5478cc9725a78ec8eeb5563f143461905f0fa3
swandevson <shai4956@gmail.com>
2022-11-01 16:48:35 +0900

    style: P5 컨벤션 수정
    - 제어문 키워드와 여는 소괄호 사이 공백 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,13 +4,15 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Arrays;
 
+
+
 public class Problem5 {
     public static List<Integer> calcMoneyUnit(int money) {
         // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
         List<Integer> moneyUnitList = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
         List<Integer> moneyUnitNumList = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-        for(int i = 0; i < moneyUnitList.size() && 0 < money; i++) {
+        for (int i = 0; i < moneyUnitList.size() && 0 < money; i++) {
             int moneyUnit = moneyUnitList.get(i);
             int moneyUnitNum = money / moneyUnit;
 
