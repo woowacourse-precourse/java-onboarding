8c3ccb352d314156db4a82b69bda3db1642c5c7a
ahinkim <ahin98@naver.com>
2022-11-01 16:47:43 +0900

    refactor(problem7.java): Add function to score points based on the number of friends you know with the user [feature2]

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -9,17 +9,47 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         // 1. 사용자가 아는 친구 구하기
-        ArrayList<String> userFriends = new ArrayList<>();
+        HashMap<String, Integer> userFriends = new HashMap<>();
         int N = friends.size();
         for(int i = 0; i < N; i++){
             List<String> list = friends.get(i);
             if (list.get(0).equals(user)){
-                userFriends.add(list.get(1));
+                userFriends.put(list.get(1), 0);
             }
             else if(list.get(1).equals(user)){
-                userFriends.add(list.get(0));
+                userFriends.put(list.get(0), 0);
             }
         }
+        // 2. 사용자와 함께 아는 친구의 수에 따라 점수 부여하기 +10
+        HashMap<String, Integer> score = new HashMap<>();
+        // friends 돌면서 freind에 사용자가 아는 친구가 있다면 점수 부여 user가 아닐 경우에만
+        for (int i = 0; i < N; i++){
+            List<String> list = friends.get(i);
+            // 사용자와 아는 친구일 때
+            if (userFriends.containsKey(list.get(0))){
+                String key = list.get(1);
+                int value = 10;
+                // user(사용자)&원래 사용자와 친구인 경우는 무시
+                if(!(key.equals(user) || userFriends.containsKey(key))){
+                    if (score.containsKey(key)){
+                        value = score.get(key) + 10;
+                    }
+                    score.put(key, value);
+                }
+            }
+            else if (userFriends.containsKey(list.get(1))){
+                String key = list.get(0);
+                int value = 10;
+                // user(사용자)&원래 사용자와 친구인 경우는 무시
+                if(!(key.equals(user) || userFriends.containsKey(key))){
+                    if (score.containsKey(key)){
+                        value = score.get(key) + 10;
+                    }
+                    score.put(key, value);
+                }
+            }
+        }
+
         return answer;
     }
 }
