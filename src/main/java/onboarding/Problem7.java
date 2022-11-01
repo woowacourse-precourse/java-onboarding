2624d6221dfe651ac1539ea2df9c6ba90fa32ee5
yenawee <yaena0319@naver.com>
2022-11-01 18:48:59 +0900

    [FEAT] 7. friends 이중배열 중에 현재 친구가 아닌 목록 구하기

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,12 +2,30 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> CurrentFriends = CurrentFriends(user, friends);
+        HashMap<String, Integer> PossibleFriends = PossibleFreind(user, friends, CurrentFriends);
+
+
+
         return answer;
+    }
+
+    HashMap<String, Integer> PossibleFriend(String usr, List<List<String>> friends, List<String> CurrentFriends){
+        HashMap<String, Integer> PossibleFriends = new HashMap<>();
+        for (int i = 0; i < friends.size(); i++){
+            for (int j = 0; j < 2; j++){
+                if (!CurrentFriends.contains(friends.get(i).get(j)) && friends.get(i).get(j) != usr){
+                    PossibleFriends.put(friends.get(i).get(j), 0);
+                }
+            }
+        }
+        return PossibleFriends;
     }
 
     private static List<String> CurrentFriends(String usr, List<List<String>> friends){
