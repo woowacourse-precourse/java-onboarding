71715eb5ce4c501355d541f0996c23af56b511e8
VogJog <hackwaffle@gmail.com>
2022-11-01 17:44:51 +0900

    problem 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int cnt = 0;
+        for (int i = 1; i <= number; i++){
+            int current = i;
+            //int temp = cnt;
+
+            while (current !=0){
+                if(current %10 == 3 || current%10 == 6 || current%10 ==9){
+                    cnt++;
+                }
+                current /= 10;
+            }
+        }
+        return cnt;
     }
 }
