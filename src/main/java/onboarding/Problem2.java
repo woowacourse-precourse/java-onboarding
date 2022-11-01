ad96c7742395147424844a5f9172cc83c014aaf6
la1av1a <zldqkrktqk@gmail.com>
2022-11-01 16:58:26 +0900

    feat : 문자열이 주어지면 문자열의 길이만큼 반복하며 중복되는 문자가 있는지 찾는다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem2 {
+
+    public static String answer;
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        answer = cryptogram;
+
+        searchDuplicateWords();
         return answer;
+    }
+
+    //문자열이 주어지면 문자열의 길이만큼 반복하며 중복되는 문자가 있는지 찾는다
+    public static void searchDuplicateWords(){
+
+        for(int i=0;i<answer.length();i++){
+            char c = answer.charAt(i);
+            String tmp = answer;
+            answer = answer.replaceAll("["+c+"]{2,}","");
+
+            if(tmp.length() > answer.length()) {
+                searchDuplicateWords();
+                return;
+            }
+        }
     }
 }
