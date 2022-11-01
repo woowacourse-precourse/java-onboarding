292d08c81829ef0abe32f099c2df271dd2fa9510
kwx4957 <kdy4957@naver.com>
2022-11-01 13:28:00 +0900

    5번 문제 기능 구현 2. money 최대 최소값 설정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.TreeMap;
 
 /*
 1. 돈의 액수 초기화 설정
@@ -11,7 +12,9 @@ public class Problem5 {
     public static int[] initCoin(){
         return new int[]{50000,10000,5000,1000,500,100,50,10,1};
     }
-
+    public static boolean ckCoin(int money){
+        return money <= 1000000 && 1 <= money;
+    }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
