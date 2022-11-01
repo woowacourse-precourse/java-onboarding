f0a130a0c25854780d0c6b86815824e53d832a52
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-01 17:47:30 +0900

    feat(Problem6): forms에 있는 크루들의 이름들을 하나씩 보면서 2글자 단위로 자르기(기능목록1)

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = 0; j < forms.get(i).get(1).length() - 1; j++) {
+                String checkName = forms.get(i).get(1).substring(j, j + 2);
+            }
+        }
         return answer;
     }
 }
