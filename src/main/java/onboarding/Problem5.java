5c0ee07e2d6c6c888b305419712568992d855df9
yujindonut <hyujin0825@gmail.com>
2022-11-01 21:52:03 +0900

    [CHORE] 코드 리팩토링 #5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,29 +1,24 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<Integer>();
+        List<Integer> answer = new ArrayList<>();
 
         Integer[] moneyList = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
-        if (money >= 1 || money < 1000000) {
-            for (int i = 0; i < moneyList.length; i++){
-                Integer c = money / moneyList[i];
-                if (c > 0){
+        if (money >= 1 && money < 1000000) {
+            for (Integer integer : moneyList) {
+                int c = money / integer;
+                if (c > 0) {
                     answer.add(c);
                 } else {
                     answer.add(0);
                 }
-                money %= moneyList[i];
+                money %= integer;
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(solution(15000));
     }
 }
