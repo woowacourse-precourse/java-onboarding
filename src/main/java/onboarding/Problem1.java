e359c0eab7b0d6e6e4067404b992b9d14545bc9b
오승윤 <>
2022-11-01 15:05:00 +0900

    추가 :  Problem1 승부 결과를 계산하는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,9 +6,25 @@ class Problem1 {
 
     private final int LEFT_IDX = 0;
     private final int RIGHT_IDX = 1;
+
+    private static final int POBI_WIN =1;
+    private static final int CRONG_WIN =2;
+    private static final int DRAW =0;
+    private static final int ERROR = -1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Problem1 problem1 = new Problem1();
+        try {
+            int answer = Integer.MAX_VALUE;
+
+            if (problem1.calculateScore(pobi) > problem1.calculateScore(crong)) answer = POBI_WIN;
+            else if (problem1.calculateScore(pobi) < problem1.calculateScore(crong)) answer = CRONG_WIN;
+            else if (problem1.calculateScore(pobi) == problem1.calculateScore(crong)) answer = DRAW;
+
+            return answer;
+
+        }catch(Exception e){
+            return ERROR;
+        }
     }
 
     private Integer calculateScore(List<Integer> bothPageNums) throws Exception {
