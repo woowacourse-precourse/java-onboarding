9f9a9904ac74899392e17211a7d44f4dffc3c69e
im-shung <seoyoung1113@gmail.com>
2022-11-01 22:31:54 +0900

    :art: 전역 변수로 변환 #6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -13,34 +13,30 @@ public class Problem6 {
     private static final int EMAIL = 0;
     private static final int NICK_NAME = 1;
 
+    private static Map<String, String> emailToNameMap; // 이메일을 KEY, 닉네임을 VALUE로 갖는 해시맵
+    private static Set<String> emailSet; // 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록
+
     public static List<String> solution(List<List<String>> forms) {
-        // 이메일을 KEY, 닉네임을 VALUE로 갖는 해시맵 생성
-        Map<String, String> emailToNameMap = makeEmailToNameMap(forms);
+        emailToNameMap = new HashMap<>();
+        makeEmailToNameMap(forms);
 
-        // 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록
-        Set<String> emailSet = getEmailSetWithDuplicatedName(emailToNameMap);
+        emailSet = new HashSet<>();
+        makeEmailSetByDuplicatedName();
 
-        // 오름차순 정렬한 리스트 반환
-        List<String> answer = sortListByAsc(emailSet);
-
-        return answer;
+        return sortListByAsc(emailSet);
     }
 
-    // 이메일을 KEY, 닉네임을 VALUE로 갖는 해시맵을 생성하는 메서드
-    public static Map<String, String> makeEmailToNameMap(List<List<String>> forms) {
-        Map<String, String> map = new HashMap<>();
+    // emailToNameMap을 초기화하는 메서드
+    public static void makeEmailToNameMap(List<List<String>> forms) {
         for (List<String> form : forms) {
             String email = form.get(EMAIL);
             String nickName = form.get(NICK_NAME);
-            map.put(email, nickName);
+            emailToNameMap.put(email, nickName);
         }
-        return map;
     }
 
     // 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록 리턴하는 메서드
-    public static Set<String> getEmailSetWithDuplicatedName(Map<String, String> emailToNameMap) {
-        // Set 자료형: 중복 제거 위함
-        Set<String> emailSet = new HashSet<>();
+    public static void makeEmailSetByDuplicatedName() {
         emailToNameMap.forEach(
                 (email, name) -> {
                     for (int i = 0; i <= name.length() - 2; i++) {
@@ -48,12 +44,9 @@ public class Problem6 {
                         emailSet.addAll(emailToNameMap.entrySet().stream()
                                 .filter(e -> !Objects.equals(e.getKey(), email))
                                 .filter(e -> e.getValue().contains(word))
-                                .map(Map.Entry::getKey)
-                                .collect(Collectors.toSet()));
+                                .map(Map.Entry::getKey).collect(Collectors.toSet()));
                     }
                 });
-
-        return emailSet;
     }
 
     // 리스트를 오름차순 정렬하는 메서드
