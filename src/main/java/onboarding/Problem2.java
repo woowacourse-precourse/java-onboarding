de2362572fbddca9bfc2b1332bc0e8ba63442ad9
Hyunyeop Ahn <hyeop9@gmail.com>
2022-11-01 17:34:37 +0900

    problem 2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,31 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        /* 스택 생성 */
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (stack.size() == 0) {
+                stack.push(cryptogram.charAt(i));   // 첫 값 삽입
+            } else {
+                if (stack.peek().equals(cryptogram.charAt(i))) {
+                    stack.pop();    // 중복 제거
+                } else {
+                    stack.push(cryptogram.charAt(i));   // 값 삽입
+                }
+            }
+        }
+
+        // 문자열로 반환
+        for (int i = 0; i < stack.size(); i++) {
+            answer += stack.get(i);
+        }
         return answer;
     }
 }
+
+
