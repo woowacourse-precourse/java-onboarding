071af935a21e8f2979cab4771b45aa7afd57f702
juno-junho <ssmm0205@naver.com>
2022-11-01 19:13:04 +0900

    feat(Problem1): add case in validate method
    PageValidator 클래스 내 페이지의 유효성 검사 해주는 validate 메서드에서 세부 검사 메서드 생성

--- src/main/java/onboarding/problem1/PageValidator.java
+++ src/main/java/onboarding/problem1/PageValidator.java
@@ -20,4 +20,24 @@ public class PageValidator {
         validateIfFirstOrLastPage(firstPage, lastPage);
     }
 
+    private List<Integer> validatePageSize(List<Integer> pages) {
+        if (DEFAULT_PAGE_SIZE != pages.size())
+            throw new IllegalArgumentException("페이지 크기는 2입니다.");
+        return pages;
+    }
+
+    private void validateContinuousPage(Integer firstPage, Integer lastPage) {
+        if (lastPage - firstPage != 1)
+            throw new IllegalArgumentException("첫 페이지와 마지막 페이지는 연속되어야 합니다.");
+    }
+
+    private void validateIfFirstOrLastPage(Integer firstPage, Integer lastPage) {
+        if (checkPageRange(firstPage, lastPage))
+            throw new IllegalArgumentException("해당 페이지는 펼칠 수 없습니다.");
+    }
+
+    private boolean checkPageRange(Integer firstPage, Integer lastPage) {
+        return firstPage <= 1 || lastPage >= 400;
+    }
+
 }
