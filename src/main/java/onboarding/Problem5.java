e45177c512d4d9a96a5d1feaaa391235396b42eb
gitFILO <x1xgudwls@naver.com>
2022-11-01 22:57:35 +0900

    [Problem 5] 구한 지폐와 동전의 개수만큼 총 money를 빼주며 다음 unit의 지폐와 동전의 개수를 구하여 리턴

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,7 +5,38 @@ import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
+        int fiftyThousandWonBill = fiveTimesUnitCalculate(money,10000);
+        while(money / 10000 >= 5) money -= 50000;
 
+        int tenThousandWonBill = unitCalculate(money,10000);
+        while(money / 10000 != 0) money -= 10000;
+
+        int fiveThousandWonBill = fiveTimesUnitCalculate(money,1000);
+        while(money / 1000 >= 5) money -= 5000;
+
+        int oneThousandWonBill = unitCalculate(money,1000);
+        while(money / 1000 != 0) money -= 1000;
+
+        int fiveHundredWonCoin = fiveTimesUnitCalculate(money,100);
+        while(money / 100 >= 5) money -= 500;
+
+        int hundredWonCoin = unitCalculate(money,100);
+        while(money / 100 != 0) money -= 100;
+
+        int fiftyWonCoin = fiveTimesUnitCalculate(money,10);
+        while(money / 10 >= 5) money -= 50;
+
+        int tenWonCoin = unitCalculate(money,10);
+        while(money / 10 != 0) money -= 10;
+
+        int oneWonCoin = 0;
+        while(money != 0){
+            money -= 1;
+            oneWonCoin++;
+        }
+        List<Integer> answer = List.of(fiftyThousandWonBill,tenThousandWonBill,fiveThousandWonBill, oneThousandWonBill,
+                fiveHundredWonCoin, hundredWonCoin,fiftyWonCoin,tenWonCoin,oneWonCoin);
+        return answer;
     }
     public static int fiveTimesUnitCalculate(int money,int digit){
         int currentMoney = money;
