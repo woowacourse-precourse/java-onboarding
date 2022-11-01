c833ac7d9613e3fb2644f103115d767d4b642ebb
JasonSong97 <jaegeunsong97@gmail.com>
2022-11-01 19:58:55 +0900

    problem1

--- src/main/java/onboarding/Problem6/Problem6.java
+++ src/main/java/onboarding/Problem6/Problem6.java
@@ -1,7 +1,7 @@
 package onboarding;
 
 import java.util.List;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 // {이메일, 닉네임} 리스트 형식으로 구성된 리스트가 forms 파라미터로 주어짐
 // >> { {'asd', 'fgh'}, {'zxc', 'qwe'}, {'cvb', 'qwe'}, ... }
@@ -10,20 +10,13 @@ public class Problem6 {
         // return 값인 answer 초기화
         List<String> answer = List.of("answer");
 
-        // nickname 빈 어레이 리스트 생성
-        ArrayList<String> nickname = new ArrayList<String>();
-
-        // out리스트의 index번호를 가져옴 >> in리스트의 index( string > char[] )로 만들기
-        for (int i = 0; i < forms.size(); i++) {
-            List<String> lst = forms.get(i);
-
-
-
+        // map 사용해서 서로 연결
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < forms.size(); i ++) {
+            map.put(forms.get(i).get(0), forms.get(i).get(1));
         }
 
-        // answer 오름차순 정렬하기
-
-
+        System.out.println(map.get("jm@email.com"));
         return answer;
     }
     }
--- src/main/java/onboarding/Problem7/Problem7.java
+++ src/main/java/onboarding/Problem7/Problem7.java
@@ -7,20 +7,6 @@ public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
 
-
-
-
-
-
-        // 최종 출력
-        if () {
-
-        } else if () {
-
-        } else if () {
-
-        }
-
         return answer;
     }
 }
