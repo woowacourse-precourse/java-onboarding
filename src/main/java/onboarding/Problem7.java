d97aa9f73c23a6c2a7c3deb05f71822184b10773
김선동 <mok03127@hanyang.ac.kr>
2022-11-01 19:53:42 +0900

    feat: 문제 7 사용자와 함께 아는 친구의 수만큼 대상에게 10점을 반복해서 더하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,9 +8,13 @@ public class Problem7 {
         List<String> recommendedFriendList = new ArrayList<>();
         HashSet<String> socialNetwork = new HashSet<>(); // 전체 사용자들
         HashMap<String, HashSet<String>> friendsOfFriendOfUserMap = new HashMap<>(); // 친구의 친구들 저장
-
+        HashMap<String, Integer> scoreInfo = new HashMap<>(); // 사용자들의 추천 점수 저장
+        
         // 전체 사용자들을 나타내는 social network와 친구의 친구 정보를 설정
         generateSocialNetworkInfo(user, friends, socialNetwork, friendsOfFriendOfUserMap);
+
+        // 사용자와 함께 아는 친구의 수만큼 10점을 반복해서 더해줌
+        plusAcquaintanceScore(user, socialNetwork, friendsOfFriendOfUserMap, scoreInfo);
 
         return recommendedFriendList;
     }
@@ -35,6 +39,22 @@ public class Problem7 {
             // 서로 친구로 추가
             friendsOfFriendOfUserMap.get(friendA).add(friendB);
             friendsOfFriendOfUserMap.get(friendB).add(friendA);
+        }
+    }
+
+    public static void plusAcquaintanceScore(String user, HashSet<String> socialNetwork, HashMap<String, HashSet<String>> friendsOfFriendOfUserMap, HashMap<String, Integer> scoreInfo) {
+        for (String name : socialNetwork) {
+            if (name.equals(user)) {
+                continue; // 자기 자신은 스킵
+            }
+            if (friendsOfFriendOfUserMap.get(user).contains(name)) {
+                continue; // 이미 친구라면 스킵
+            }
+
+            friendsOfFriendOfUserMap.get(name).retainAll(friendsOfFriendOfUserMap.get(user));
+            if (friendsOfFriendOfUserMap.get(name).size() > 0) {
+                scoreInfo.put(name, friendsOfFriendOfUserMap.get(name).size() * 10);
+            }
         }
     }
 }
