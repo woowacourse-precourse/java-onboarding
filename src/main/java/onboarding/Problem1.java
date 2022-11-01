d51af2844b27982dd3c3224f6694396b285b15d1
LeeHaiLim <hailim1997@gmail.com>
2022-11-01 13:12:16 +0900

    FEAT checkValidity

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,35 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(!checkValidity(pobi)||!checkValidity(crong)){
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean checkValidity(List<Integer> list) {
+        if (list.size() != 2) {
+            return false;
+        }
+
+        for (Integer integer : list) {
+            if (integer == null) {
+                return false;
+            }
+            if (integer <= 1 || integer >= 400) {
+                return false;
+            }
+        }
+
+        if (list.get(1) - list.get(0) != 1) {
+            return false;
+        }
+
+        if (list.get(0) % 2 != 1 || list.get(1) % 2 != 0) {
+            return false;
+        }
+
+        return true;
     }
 }
