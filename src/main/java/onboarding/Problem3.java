ec40244bee2554f60c69d38e32de7f8718d0eee1
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-11-01 15:34:27 +0900

    3,6,9를 확인하는 checkNumber 구현 및 solution 구현 / solve Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,16 +1,21 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
+
+    public static int checkNumber(int number) {
         int answer = 0;
-        for(int i=1; i<=number; i++){
+        for (int i = 1; i <= number; i++) {
             int num = i;
-            while(num>0){
-                int mod = num%10;
-                if(mod==3||mod==6||mod==9)answer++;
+            while (num > 0) {
+                int mod = num % 10;
+                if (mod == 3 || mod == 6 || mod == 9) answer++;
                 num /= 10;
             }
         }
         return answer;
+    }
+
+    public static int solution(int number) {
+        return checkNumber(number);
     }
 }
