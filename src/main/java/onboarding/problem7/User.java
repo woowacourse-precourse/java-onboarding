5d5e5266bc49c798e7ba1671fdba1b78efc553b4
Daeeun Ko <7273322@naver.com>
2022-11-01 19:48:50 +0900

    feat: validate if user id is out of bounds

--- src/main/java/onboarding/problem7/User.java
+++ src/main/java/onboarding/problem7/User.java
@@ -1,3 +1,27 @@
 package onboarding.problem7;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
+
+    private final int MIN_ID_LENGTH = 1;
+    private final int MAX_ID_LENGTH = 30;
+
+    private String id;
+    private Set<User> friends;
+
+    public User(String id) {
+        validateId(id);
+        this.id = id;
+        this.friends = new HashSet<>();
+    }
+
+    private void validateId(String id) {
+        if (isOutOfBounds(id)) throw new IllegalArgumentException("user id is out of bounds");
+    }
+
+    private boolean isOutOfBounds(String id) {
+        return id.length() < MIN_ID_LENGTH || id.length() > MAX_ID_LENGTH;
+    }
 }
