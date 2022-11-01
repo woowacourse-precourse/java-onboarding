22460ffc65405f08dd12b9e06c0e3226001f81fb
yenawee <yaena0319@naver.com>
2022-11-01 16:04:38 +0900

    [FEAT] 4. solution 메소드 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-
+        if (!checkInput(word))
+            return null;
+        String ret = "";
+        for (int i = 0; i < word.length(); i++){
+            if (!is_Alpha(word.charAt(i))){
+                ret += word.charAt(i);
+                continue;
+            }
+            ret += translateChar(word.charAt(i));
+        }
+        return ret;
     }
 
     private static boolean checkInput(String word){
