85b17112eae500f5ceb0ea35478edebff28bd759
eom-tae-in <eti0728@daum.net>
2022-11-01 21:51:47 +0900

    feat: static value

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,35 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+
+    private static final int MIN_USER_LENGTH = 1;
+    private static final int MAX_USER_LENGTH = 30;
+    private static final int MIN_FRIENDS_LENGTH = 1;
+    private static final int MAX_FRIENDS_LENGTH = 10000;
+    private static final int MIN_FRIEND_ID_LENGTH = 1;
+    private static final int MAX_FRIEND_ID_LENGTH = 30;
+    private static final int MIN_VISITORS_LENGTH = 0;
+    private static final int MAX_VISITORS_LENGTH = 10000;
+    private static final String LOWERCASE_REG = "^[a-z]*$";
+    private static final Map<String, Integer> userAndScoreMap = new HashMap<>();
+    private static final Set<String> myFriendsSet = new HashSet<>();
+    private static final int ACQUAINTANCE_POINT = 10;
+    private static final int VISITOR_POINT = 1;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        checkException(user, friends, visitors);
+        friendsSetup(friends, user);
+        updateScore(friends, visitors);
+        removeUserFriendsAndZeroScore();
+        List<String> answer = userAndScoreMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(Map.Entry::getKey)
+                .limit(5)
+                .collect(Collectors.toList());
         return answer;
     }
 }
