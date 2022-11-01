9d729aef5be828108fbfcaab084d9027f1a52ace
JUNBEOM_LEE <bom1215@naver.com>
2022-11-01 16:53:33 +0900

    Friend_find method of mission7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,9 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.TreeMap;
 
 public class Problem7 {
+
+    static TreeMap<String, Integer> treemap = new TreeMap<>();
+    static ArrayList<String> friend_lis = new ArrayList<>();
+
+    static ArrayList<String> common_friend_lis = new ArrayList<>();
+    public static void Friend_find(String user, String friend, List<List<String>> friends){
+        for (List i : friends) {
+            if (i.get(0) == friend && i.get(1) != user) {
+                String common_friend = i.get(1).toString();
+                common_friend_lis.add(common_friend);
+            } else if (i.get(1) == friend && i.get(0) != user) {
+                String common_friend = i.get(0).toString();
+                common_friend_lis.add(common_friend);
+            }
+        }
+
+
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
