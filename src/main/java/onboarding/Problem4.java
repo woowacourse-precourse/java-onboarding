aafd2e823899cc2cd8a7c13f8e8409ac69e051ae
rlaalstn0107 <minsu980107@naver.com>
2022-11-01 21:40:16 +0900

    4번문제 풀이

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder wordB = new StringBuilder(word);
+
+        for (int i = 0; i < word.length(); i++) {
+            int currChar = (char) word.charAt(i);
+
+            if (currChar >= 'A' && currChar <= 'Z') {
+                currChar = 'A' + 'Z' - currChar;
+                wordB.setCharAt(i, (char) currChar);
+            } else if (currChar >= 'a' && currChar <= 'z') {
+                currChar = 'a' + 'z' - currChar;
+                wordB.setCharAt(i, (char) currChar);
+            }
+        }
+
+        answer = wordB.toString();
         return answer;
     }
 }
