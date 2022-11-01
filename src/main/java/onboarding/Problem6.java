d891ff91dce1d7e337acb05a7dcde91a45236a91
ho2n <jungho2n@gmail.com>
2022-11-01 17:36:18 +0900

    중복되는 닉네임 찾기

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,18 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        getRepeatedNicknamesInForms(forms, answer);
+
         return answer;
+    }
+
+    private static void getRepeatedNicknamesInForms(List<List<String>> forms, List<String> answer) {
+        for (List<String> form : forms) {
+            String nickName = form.get(1);
+            if (nickName.length() >= 2) {
+                getRepeatedSubnicknamesInNickname(forms, nickName, answer);
+            }
+        }
     }
 }
