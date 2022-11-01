187b6cbd37362d16546535829b322ea69bc273d5
Anthologia <pyoungjy@gmail.com>
2022-11-01 17:08:57 +0900

    feat: Problem 3 재귀를 통한 문자열 분리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,14 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static void game369(int number) {
+        if (number <= 0) {
+            return ;
+        }
+
+        int seperatedNumber = number % 10;
+
+        game369(number / 10);
+    }
 }
