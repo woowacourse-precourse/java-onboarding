26f04761eb649540715d31ffd7550ae322b30370
jay <jaewhan232@naver.com>
2022-11-01 15:42:25 +0900

    Implement : 페이지 번호들의 유효성을 검사하는 메소드. 페이지 개수, 범위, 두 값의 차이, 홀/짝, 증가 여부 확인

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,4 +7,22 @@ class Problem1 {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+
+    private static boolean isValidPageNumbers(List<Integer> pageNumbers) {
+        if (pageNumbers.size() != 2){
+            return false;
+        }
+        if ((1 <= pageNumbers.get(0) && pageNumbers.get(0) <= 400)
+                && (1 <= pageNumbers.get(1) && pageNumbers.get(1) <= 400)){
+            return false;
+        }
+        if (pageNumbers.get(0)%2 != 1){
+            return false;
+        }
+        if (pageNumbers.get(0)+1 != pageNumbers.get(1)){
+            return false;
+        }
+        return true;
+    }
+
 }
