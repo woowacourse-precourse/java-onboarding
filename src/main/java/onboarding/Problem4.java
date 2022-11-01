9738ae872733c74b5b49b1ee7dc6ae5435b182a0
ckn1070 <ckn1070@naver.com>
2022-11-01 23:10:10 +0900

    Problem 4: Added comments for understanding

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,21 +1,24 @@
 package onboarding;
 
 public class Problem4 {
+    // ascii 표를 이용하여 청개구리 사전 구현
     static char convertReverse(char character){
         int ascii = (int)character;
         int startPoint = 0;
         int endPoint = 0;
         int displacement;
         int reverseAscii;
-
+        // 대문자일 경우 변환
         if(65 <= ascii && ascii <= 90){
             startPoint = 65;
             endPoint = 90;
         }
+        // 소문자일 경우 변환
         if(97<= ascii && ascii <= 122){
             startPoint = 97;
             endPoint = 122;
         }
+        // 띄어쓰기의 경우 변환 없이 반환
         if(ascii == 32){
             return (char)ascii;
         }
