98d50b2a1520c1e3fdba3713c4b8649764040681
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-01 18:24:39 +0900

    problem5: TODO-'기능1' complete

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,21 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+    private static List<Integer> numOfCurrencyBy(List<Integer> currency, int money){
+        List<Integer> answer = new ArrayList<>();
+        for(int cur:currency){
+            answer.add(money / cur);
+            money %= cur;
+        }
         return answer;
+    }
+    public static List<Integer> solution(int money) {
+        List<Integer> currency = Arrays.asList(50000,10000,5000,1000,500,100,50,10,1);
+        return numOfCurrencyBy(currency, money);
     }
 }
