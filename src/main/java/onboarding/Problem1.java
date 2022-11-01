08f8cbd01e2f946adf3ec4bc7f96d3377ac884ac
Eungyu-code <Agyu625@gmail.com>
2022-11-01 12:57:15 +0900

    각 페이지별 최대값을 구하는 함수 maxNumberByPage 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,12 +10,30 @@ class Problem1 {
 
         if (! except(pobi) || ! except(crong)) return -1;
 
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int pobiNumber = Math.max(maxNumberByPage(pobi.get(0)), maxNumberByPage(pobi.get(1)));
+        int crongNumber = Math.max(maxNumberByPage(crong.get(0)), maxNumberByPage(crong.get(1)));
+
+        if (pobiNumber > crongNumber) return 1;
+        else if (crongNumber > pobiNumber) return 2;
+        else return 0;
     }
 
     public static boolean except(List<Integer> list) {
 
         return list.get(0).equals(list.get(1) - 1);
+    }
+
+    public static Integer maxNumberByPage( int page) {
+        int mul = 1;
+        int add = 0;
+
+        while (page > 0) {
+            int n = page % 10;
+            mul *= n;
+            add += n;
+            page /= 10;
+        }
+
+        return Math.max(mul, add);
     }
 }
