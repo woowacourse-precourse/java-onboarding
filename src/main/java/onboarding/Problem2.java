3d18a9c15f4051c0382a2a5d4b67a4d7096d856d
dingding-21 <phj8221@naver.com>
2022-11-01 22:45:51 +0900

    feat: Problem2 solution함수 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,7 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return checkEnd(cryptogram);
     }
 
     private static String getDecryptedWord(String cryptogram) {
