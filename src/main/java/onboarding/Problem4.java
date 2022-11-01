1b4b8a4fbaf8ab96ec99dccb2b888f2ab2638fb9
core <jinjs6@gmail.com>
2022-11-01 18:27:26 +0900

    Problem4 소문자, 대문자에 따른 청개구리 문자열 제작 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,8 +3,18 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         String lower = "abcdefghijklmnopqrstuvwxyz";
         String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < word.length(); ++i) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                sb.append(upper.charAt(25 - upper.indexOf(word.charAt(i))));
+            } else if (Character.isLowerCase(word.charAt(i))) {
+                sb.append(lower.charAt(25 - lower.indexOf(word.charAt(i))));
+            } else {
+                sb.append(word.charAt(i));
+            }
+        }
         return answer;
     }
 }
