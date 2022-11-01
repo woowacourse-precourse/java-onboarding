8e17f6271fc9f1b8c564c16ffb180a3bb011dce7
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-01 19:35:04 +0900

    problem7 feat : get userFiend list

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    static HashMap<String,Integer> scoreList = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        List<String> userFriends = new ArrayList<>();
+        for(int i=0;i<friends.size();i++){
+            if(user == friends.get(i).get(0)) userFriends.add(friends.get(i).get(1));
+            else if(user == friends.get(i).get(1)) userFriends.add(friends.get(i).get(0));
+        }
+
         return answer;
     }
+
 }
