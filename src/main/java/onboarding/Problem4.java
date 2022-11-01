95ea38d316c6bd63159301ce1e9891e6c044f641
Sehoon Kim <shggm2000@gmail.com>
2022-11-01 14:21:51 +0900

    문자열 각각을 하나의 인덱스로 arraylist에 담는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Problem4 {
     public static String solution(String word) {
+
+        char[] arr = word.toCharArray();
+        ArrayList<Character> list = new ArrayList<Character>();
+
+        for (int k = 0; k < arr.length; k++) {
+            list.add(arr[k]);
+        }
+
+
+
+
         String answer = "";
+
+
         return answer;
     }
 }
+
