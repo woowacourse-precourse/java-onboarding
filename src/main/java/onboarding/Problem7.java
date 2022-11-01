5b5e881b979cf8f11734eb4c9aa4b94e2703983d
Donguk <95donguk.dev@gmail.com>
2022-11-01 20:07:19 +0900

    feat: 문제7의 사용자의 친구 목록을 구하는 기능 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+    public static List<String> solution(String user, List<List<String>> friends,
+                                        List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Set<String> userFriends = new HashSet<>();
+        findUserFriends(user, friends, userFriends);
         return answer;
+    }
+
+    private static Set<String> findUserFriends(String user, List<List<String>> friends,
+                                               Set<String> userFriends) {
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                userFriends.add(friend.get(0));
+                userFriends.add(friend.get(1));
+            }
+        }
+        userFriends.remove(user);
+        return userFriends;
     }
 }
