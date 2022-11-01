f6a49428aef5602be90f63f59c95861b69c6a654
JeonghakLee <wjdgkr0527@naver.com>
2022-11-01 11:32:56 +0900

    feat: problem5 solve

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import static java.util.Collections.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+
+  private static final List<Integer> banknotesAndCoins = List.of(
+      50000, 10000, 5000, 1000, 500, 100, 50, 10, 1
+  );
+
+
+  public static List<Integer> solution(int money) {
+    List<Integer> answer = new ArrayList<>();
+    for (Integer banknoteOrCoin : banknotesAndCoins) {
+      answer.add(money / banknoteOrCoin);
+      money %= banknoteOrCoin;
     }
+    return answer;
+  }
 }
