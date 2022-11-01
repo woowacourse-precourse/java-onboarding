e33001a4810a89797af62e8cfb456ff2e0f05c6b
Lee-Jiseung <ljss98@naver.com>
2022-11-01 18:39:10 +0900

    fix: problem2 중복 문자 제거 로직 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,24 +2,31 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
         return decode(cryptogram);
     }
 
     private static String decode(String c) {
-        int count = 0;
+        boolean flag = true;
 
-        for (int i = 1; i < c.length(); i++) {
-            if (i >= 0 && c.charAt(i) == c.charAt(i - 1)) {
-                count++;
-            } else if (count > 0) {
-                c = c.substring(0, i - count - 1) + c.substring(i);
-                i -= count + 2;
-                count = 0;
+        while (flag) {
+            flag = false;
+            int count = 0;
+
+            for (int i = 1; i < c.length(); i++) {
+                if (c.charAt(i) == c.charAt(i - 1)) {
+                    count++;
+                } else if (count > 0) {
+                    c = c.substring(0, i - count - 1) + c.substring(i);
+                    i -= count + 1;
+                    count = 0;
+                    flag = true;
+                }
             }
-        }
 
-        if (count > 0) {
-            c = c.substring(0, c.length() - count - 1);
+            if (count > 0) {
+                c = c.substring(0, c.length() - count - 1);
+            }
         }
 
         return c;
