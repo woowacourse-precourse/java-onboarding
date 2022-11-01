9a51f85e8e392efa13cc658b9d9e6e4879349b1f
pasly0920 <inwoo920621@gmail.com>
2022-11-01 15:27:14 +0900

    feat: 1 각 숫자의 3,6,9 개수 판별

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return 0;
     }
+
+    private static int needClapCount(int number){
+        int result = 0;
+        String numberStr = String.valueOf(number);
+
+        for(int i=0; i<numberStr.length(); i++){
+            String nowStr = numberStr.substring(i, i+1);
+            if(nowStr.equals("3") || nowStr.equals("6") || nowStr.equals("9"))
+                result++;
+        }
+
+        return result;
+    }
+
 }
