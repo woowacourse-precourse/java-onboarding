cc93e5b6bf9cefd9aff7e3e4b8e68b9ecd50a6ed
brotherstone97 <brotherstone97@gmail.com>
2022-11-01 13:46:06 +0900

    Feat: 모든 크루의 닉네임을 비교해 겹치는 연속문자가 있는지 확인하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    static boolean isOverlap(String nickname1, String nickname2) {
+        List<String> listChar = new ArrayList<String>();
+        for (int i = 0; i < nickname1.length() - 1; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(nickname1.charAt(i));
+            sb.append(nickname1.charAt(i + 1));
+            listChar.add(sb.toString());
+        }
+        for (String ch : listChar) {
+            if (nickname2.contains(ch)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        for (int i = 0; i < forms.size(); i++) {
+            for (int j = i + 1; j < forms.size(); j++) {
+                List<String> info1 = forms.get(i);
+                List<String> info2 = forms.get(j);
+                if (isOverlap(info1.get(1), info2.get(1))) {
+
+                }
+            }
+        }
         return answer;
     }
 }
