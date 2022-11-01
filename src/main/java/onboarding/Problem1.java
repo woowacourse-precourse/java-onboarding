7e4b170e33b662a24191587e123feb058ef01716
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-01 14:12:03 +0900

    feat:페이지별 배열에 담고 이를 이용하여 계산한 값을 새로운 배열에 담음

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,23 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        int sum = 0;
+        int pobi_left = pobi.get(0), pobi_right = pobi.get(1), crong_left = crong.get(0), crong_right = crong.get(1);
+
+        int[] pobi_scores = new int[4];
+        int[] crong_scores = new int[4];
+
+        pobi_scores[0] = sum(pobi_left);
+        pobi_scores[1] = sum(pobi_right);
+        pobi_scores[2] = multiple(pobi_left);
+        pobi_scores[3] = multiple(pobi_right);
+
+        crong_scores[0] = sum(crong_left);
+        crong_scores[1] = sum(crong_right);
+        crong_scores[2] = multiple(crong_left);
+        crong_scores[3] = multiple(crong_right);
+
         return answer;
     }
 
