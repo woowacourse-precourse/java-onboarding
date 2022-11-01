6e026fe740eca005c1a3c45f54a73b17e676d198
ryuj-h <dbwlsgh57@naver.com>
2022-11-01 18:31:40 +0900

    Create Problem 4 class structure

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,13 @@
 package onboarding;
 
 public class Problem4 {
+    private static boolean isCap(char c){
+        return (c >= 0x41 && c <= 0x5A);//A to Z
+    }
+    private static char change(char c){
+
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
     }
 }
