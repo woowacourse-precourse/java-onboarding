58bcc5af27168578cba253dc0ab8923959f5988e
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-01 19:57:49 +0900

    feat: Add Problem7 Exception Checker
    - 문제 7번 예외 검사 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,7 +5,29 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        if (isUserException(user) || isFriendsException(friends) || isVisitorsException(visitors)) {
+            throw new RuntimeException("올바르지 않은 인자입니다.");
+        }
+
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Boolean isUserException(String user) {
+        for (int i = 0; i < user.length(); i++) {
+            if (user.charAt(i) < 'A' || user.charAt(1) > 'Z') return true;
+        }
+        return user.length() < 1 || user.length() > 30;
+    }
+
+    private static Boolean isFriendsException(List<List<String>> friends) {
+        if (friends.size() < 1 || friends.size() > 10000) return true;
+        return friends.stream()
+                .anyMatch(friend -> (friend.size() != 2)
+                        || (friend.stream().anyMatch(f -> (f.length() < 1 || f.length() > 30))));
+    }
+
+    private static Boolean isVisitorsException(List<String> visitors) {
+        return visitors.size() < 0 || visitors.size() > 10000;
     }
 }
