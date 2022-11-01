a0bb275c03582196b4060a575bc5aef28f0fe7f3
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:27:22 +0900

    Feat: Add 연속하는 중복 문자 삭제 함수

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,29 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    
+    private static String removeDuplicate(String word) {
+
+        char recentChar = 0;
+        boolean isRemoved = false;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (i != 0 && recentChar == word.charAt(i)) {
+                int start = i - 1;
+                int end = findEndPoint(word, start);
+                word = word.substring(0, start) + word.substring(end);
+                isRemoved = true;
+                break;
+            }
+            recentChar = word.charAt(i);
+        }
+
+        if (isRemoved) {
+            word = removeDuplicate(word);
+        }
+
+        return word;
+    }
+
 }
