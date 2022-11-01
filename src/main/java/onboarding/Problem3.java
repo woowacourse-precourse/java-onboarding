050f8b130d1a0a8c90c60d4d3850dc7eea165b18
문민혁 <odo27@munminhyeog-ui-MacBookAir.local>
2022-11-01 17:48:19 +0900

    한자리 수를 입력받고 3이나 6이나 9면 1을 리턴하고 아니면 0을 리턴한다

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,12 @@
 package onboarding;
 
 public class Problem3 {
+    private static int isClap(int digit) {
+        if (digit != 0 && digit % 3 == 0) {
+            return 1;
+        }
+        return 0;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
