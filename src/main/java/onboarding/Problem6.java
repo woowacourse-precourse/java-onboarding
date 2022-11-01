031f225d0f9165841d735a77c36cc2b3efdeb8ba
ye1ne <yewon7036@naver.com>
2022-11-01 18:41:02 +0900

    feature : 배열에 자료를 저장하여 중복을 판별하는 메소드

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,22 +1,42 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
+    static List<String> nameList;
     public static List<String> solution(List<List<String>> forms) {
+        nameList = new ArrayList<>();
         Set<String> overlapSet = findOverlapCrew(forms);
         return sortSet(overlapSet);
     }
 
+
     private static List<String> sortSet(Set<String> overlapSet) {
+
         return null;
     }
 
     private static Set<String> findOverlapCrew(List<List<String>> forms){
-        return null;
+        HashMap<String, String> map = new HashMap<>();
+        Set<String> overlapSet = new HashSet<>();
+        OuterLoop:
+        for(List<String> form : forms){
+            String crewEmail = form.get(0);
+            String crewName = form.get(1);
+            for(String name: nameList){
+                if(impossibleName(crewName, name)){
+                    overlapSet.add(crewEmail);
+                    overlapSet.add(map.get(name));
+                    continue OuterLoop;
+                }
+            }
+            map.put(crewName, crewEmail);
+        }
+        return overlapSet;
 
+    }
+
+    private static boolean impossibleName(String crewName, String name) {
+        return true;
     }
 }
