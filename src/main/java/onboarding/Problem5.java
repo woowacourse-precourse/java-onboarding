0baed2b7724db1d547f654c3a70cfff982666798
moonstal1506 <moonstal1506@gmail.com>
2022-11-01 22:00:08 +0900

    feat(problem5): money 단위에 따라 화폐 개수 계산

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,17 +1,28 @@
 package onboarding;
 
+import onboarding.problem5.MoneyType;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
         validate(money);
-        return new ArrayList<>();
+        List<Integer> wallet = new ArrayList<>();
+        calculateChanges(money, wallet);
+        return wallet;
     }
 
     private static void validate(int money) {
         if (money < 1 || money > 1_000_000) {
             throw new IllegalArgumentException("유효하지 않은 금액입니다.");
+        }
+    }
+
+    private static void calculateChanges(int money, List<Integer> wallet) {
+        for (MoneyType value : MoneyType.VALUES) {
+            wallet.add(money / value.getType());
+            money %= value.getType();
         }
     }
 }
