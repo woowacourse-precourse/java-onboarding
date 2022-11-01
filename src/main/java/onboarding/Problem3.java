c5570c3c0bb40c2b749d5fd5f1c7df5db0446bb9
choiejae <ejae8319@gmail.com>
2022-11-01 17:13:26 +0900

    feat (validityChecker): 예외 처리 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
 public class Problem3 {
+    private static final int NUMBER_THREE = 3;
+    private static final int NUMBER_SIX = 6;
+    private static final int NUMBER_NINE = 9;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 10000;
     public static int solution(int number) {
+        validityChecker(number);
+
         int answer = 0;
         return answer;
+    }
+    private static void validityChecker(int number) {
+        if (isNotValidNumber(number))
+            throw new IllegalArgumentException("Not valid number");
+    }
+    private static boolean isNotValidNumber(int number) {
+        return number < MIN_NUMBER || MAX_NUMBER < number;
     }
 }
