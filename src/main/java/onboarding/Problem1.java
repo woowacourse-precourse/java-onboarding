281217d931403f235810c412db0ee55057b11730
Lee-KyuHwun <107871734+Lee-Kyuhwun@users.noreply.github.com>
2022-11-01 21:17:59 +0900

    Update Problem1.java

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int[] a;
         int answer = Integer.MAX_VALUE;
         return answer;
     }
