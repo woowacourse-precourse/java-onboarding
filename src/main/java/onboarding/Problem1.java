3818d06b2581e5e26d362c99c74c16f6564d5ce0
hyee0715 <hyee0715@naver.com>
2022-11-01 16:19:35 +0900

    feat(Problem1.java): 예외 상황 체크 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,52 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (!exceptionCheck(pobi, crong)) {
+            answer = -1;
+            return answer;
+        }
+
         return answer;
+    }
+
+    private static boolean exceptionCheck(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(0) == null || pobi.get(1) == null ||
+                crong.get(0) == null || crong.get(1) == null) {
+            return false;
+        }
+
+        int pobiLeftPage = pobi.get(0);
+        int pobiRightPage = pobi.get(1);
+
+        int crongLeftPage = crong.get(0);
+        int crongRightPage = crong.get(1);
+
+        if (pobiLeftPage >= pobiRightPage || crongLeftPage >= crongRightPage) {
+            return false;
+        }
+
+        if (pobiRightPage - pobiLeftPage != 1 || crongRightPage - crongLeftPage != 1) {
+            return false;
+        }
+
+        if (pobiLeftPage == 1 || pobiRightPage == 1 ||
+                pobiLeftPage == 400 || pobiRightPage == 400 ||
+                crongLeftPage == 1 || crongRightPage == 1 ||
+                crongLeftPage == 400 || crongRightPage == 400) {
+            return false;
+        }
+
+        if (pobiLeftPage <= 1 || pobiRightPage >= 400 ||
+                crongLeftPage <= 1 || crongRightPage >= 400) {
+            return false;
+        }
+
+        if (pobiLeftPage % 2 == 0 || pobiRightPage % 2 != 0 ||
+                crongLeftPage % 2 == 0 || crongRightPage % 2 != 0) {
+            return false;
+        }
+
+        return true;
     }
 }
