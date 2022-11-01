e3ca854e1bb6d8abfa5c35c196bf72be68566dc1
RadSparkle <wwwartg1@naver.com>
2022-11-01 23:18:47 +0900

    FEAT : 중복닉네임 추출메소드 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,48 +1,58 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> IsFalse= new ArrayList<>();
-        IsFalse.add("false");
+        String[][] form = new String[forms.size()][forms.size()];
 
-        boolean checkDomain = checkDomain(forms);
-        if(!checkDomain) {
-            return IsFalse;
-        }
-
-        boolean checkNickName = checkNickName(forms);
-        if(!checkNickName) {
-            return IsFalse;
-        }
-
-
-        List<String> answer = List.of("answer");
-        return answer;
-    }
-
-    public static boolean checkDomain(List<List<String>> forms) {
-        for(int i = 0, j=0; i<forms.size(); i++) {
-            String checkForms = forms.get(i).get(j);
-
-            if(!checkForms.contains("email.com")) {
-                return false;
+        for(int i=0;i<forms.size();i++) {
+            for(int j=0;j<forms.get(0).size();j++) {
+                form[i][j] = forms.get(i).get(j);
             }
         }
-        return true;
+
+        String[] result = checkDuplicatation(form);
+        List testList = new ArrayList<>();
+        for(int i=0;i<result.length;i++) {
+            testList.add(result[i]);
+        }
+
+        return testList;
     }
 
-    public static boolean checkNickName(List<List<String>> forms) {
-        String pattern = "^[a-zA-Z]*$";
-        for(int i = 0,j=1; i<forms.size(); i++) {
-            String checkForms = forms.get(i).get(j);
+    public static String[] checkDuplicatation(String[][] forms) {
+        HashSet<String> emails = new HashSet<>();
 
-            if(!checkForms.matches(pattern)) {
-                return false;
+        final Map<String, String > hashMap = new HashMap<>();
+
+        for (int i = 0; i < forms.length; i++) {
+
+            final String name = forms[i][1];
+
+            if(name.length() < 2){
+                break;
+            }
+
+            for (int j = 0; j < name.length() - 1; j++) {
+                final String key = name.substring(j, j+2);
+                if(hashMap.containsKey(key)){
+                    final String email = hashMap.get(key);
+                    if(!email.equals(forms[i][0])){
+                        emails.add(email);
+                        emails.add(forms[i][0]);
+                    }
+                }
+                hashMap.put(key, forms[i][0]);
             }
         }
-        return true;
+        final List<String> collect = emails.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        final String[] results = collect.toArray(new String[0]);
+
+        return results;
+
     }
 }
