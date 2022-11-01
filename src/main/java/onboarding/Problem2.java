403ecb01cc08fd6d9252f7910b420f0bdc28473a
eunseo <qnrqjrm03@naver.com>
2022-11-01 19:54:26 +0900

    docs : 'problem2' 기능목록 정리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/**
+ * 기능목록
+ * 1. 중복된 문자를 찾는 기능
+ * 2. 1에서 찾은 문자를 삭제하는 기능
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
