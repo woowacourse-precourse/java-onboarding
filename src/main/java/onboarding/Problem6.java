b69d7d8fd8b1cfea12fe804463e0b8e9a18ac068
임원정 <LWJ@MacBookPro.local>
2022-11-01 19:43:28 +0900

    feature : problem6 닉네임만 저장되어있는 List를 구하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        List<String> nameList = makeNameList(forms);
+
+
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static List<String> makeNameList(List<List<String>> forms) {
+        List<String> nameList = new ArrayList<>();
+        for (List<String> list : forms) {
+            nameList.add(list.get(1));
+        }
+        return nameList;
     }
 }
