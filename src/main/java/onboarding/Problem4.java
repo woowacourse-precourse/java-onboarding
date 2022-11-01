c04dbe2668a0f57ed32b3da1b34be41d48c46acc
anso33 <hyeon131812@gmail.com>
2022-11-01 11:50:27 +0900

    [FEAT] add init 청개구리 dictionary

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,7 +1,20 @@
 package onboarding;
 
+/*
+1. 청개구리 사전인덱스 순서대로 return 값만 배열에 저장한다.
+2. 검색할 때 소문자인지 대문자인지 아스키값을 기준으로 검색하고 플래그에 저장된다.
+3. 리턴받은 값을
+ */
+
 public class Problem4 {
+
+
     public static String solution(String word) {
+        int dic[] = new int[26];
+        for (int i = 0; i < 26; i++) {
+            dic[i] = 'Z' - i;
+        }
+
         String answer = "";
         return answer;
     }
