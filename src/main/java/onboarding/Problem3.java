5dc1eee44a94b0913d8f458191fca2dd3023b47b
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-01 14:40:21 +0900

    style : [problem3] 코드 정렬

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,7 @@ public class Problem3 {
         int clap = 0;
 
         // 1부터 number까지 반복문
-        for (int num = 1 ; num <= number;num++ ){
+        for (int num = 1; num <= number; num++) {
             clap += countClap(Integer.toString(num));
         }
 
@@ -14,14 +14,14 @@ public class Problem3 {
     }
 
     // String을 주면 몇 번의 박수를 쳐야 하는지 알려주는 함수
-    public static int countClap(String clapString){
+    public static int countClap(String clapString) {
         int clap = 0;
 
         // forEach문을 사용해 각 숫자별로 판단
-        for (String check : clapString.split("")){
+        for (String check : clapString.split("")) {
 
             // 3,6,9가 있으면 clap의 수를 1 증가시킨다.
-            if (checkClap(check) == true ){
+            if (checkClap(check) == true) {
                 clap++;
             }
         }
@@ -30,11 +30,10 @@ public class Problem3 {
     }
 
     // 3,6,9 이면 true를 반환하는 함수
-    public static boolean checkClap(String check){
-        if (check.equals("3") || check.equals("6") || check.equals("9") ){
+    public static boolean checkClap(String check) {
+        if (check.equals("3") || check.equals("6") || check.equals("9")) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
