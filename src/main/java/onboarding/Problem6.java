93607233fd6585fda2ed2396261d1d679797df7c
hyun0404woo <hyun0404woo@gmail.com>
2022-11-01 15:08:43 +0900

    feat: 닉네임 중복을 관리하는 HashMap 생성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,13 +10,27 @@ import static java.util.regex.Pattern.compile;
 public class Problem6 {
 
     private final static Pattern emailRegexPattern = compile("^(.+)@email.com");
+
+    private static Map<String, List<Integer>> nicknameDuplicateMap;
     
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-
+        nicknameDuplicateMap = new HashMap<>();
         assert validateEmail(forms);
         
         return answer;
+    }
+
+    private static void createNicknameDuplicateMap(List<List<String>> forms) {
+        for (int i = 0; i < forms.size(); i++) {
+            final int index = i;
+            final String nickname = forms.get(index).get(1);
+            getPartialNicknameSet(nickname)
+                    .forEach(nick -> nicknameDuplicateMap
+                            .computeIfAbsent(nick, list -> new ArrayList<>())
+                            .add(index)
+                    );
+        }
     }
 
     private static Set<String> getPartialNicknameSet(String nickname) {
