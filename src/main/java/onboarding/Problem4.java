263fb92e25c9f4ef34746bc80814e14d2ef561c7
hum02 <ehdgma02@gmail.com>
2022-11-01 10:22:58 +0900

    refactor:4번 선언부 final추가,변수명 변경

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,21 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         char[] wordChar = word.toCharArray();
-        char[] result = new char[wordChar.length];
+        char[] converted = new char[wordChar.length];
 
-        for(int i=0; i<result.length; i++){
+        for(int i=0; i<wordChar.length; i++){
             if('a'<= wordChar[i] && wordChar[i] <= 'z'){
-                result[i] = (char)('a'+ ('z'- wordChar[i])) ;
+                converted[i] = (char)('a'+ ('z'- wordChar[i])) ;
             } else if ('A'<= wordChar[i] && wordChar[i]<='Z') {
-                result[i] = (char)('A'+ ('Z'- wordChar[i]));
+                converted[i] = (char)('A'+ ('Z'- wordChar[i]));
             } else {
-                result[i] = wordChar[i];
+                converted[i] = wordChar[i];
             }
         }
 
-        for(char c:result){
+        for(char c:converted){
             sb.append(c);
         }
 
