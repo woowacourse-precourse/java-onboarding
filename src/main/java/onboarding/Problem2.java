692da2f43548ac06ba1f11e99e15a56d179c3a51
can019 <jys01012@gmail.com>
2022-11-01 21:00:39 +0900

    :bulb: method 주석 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,19 @@ public class Problem2 {
         return answer;
     }
 
+    /**
+     * 재귀적으로 돌며 연속된 문자열 삭제<br/><br/>
+     * - 오른쪽 방향으로 읽어가며 문자열 처음부터 끝까지 탐색 <br/>
+     * - 해당 절차에서 연속된 문자열이 생겼다면 해당 글자를 자르고 그 위치부터 다시 탐색 <br/>
+     * - 위 절차들을 진행하며 문자열 끝에 도달하는 것을 하나의 sequence라고 표현 <br/>
+     * - 해당 sequence에서 문자열 수정이 있었다면 다시 처음부터 위 절차들을 진행 <br/>
+     * - 아니라면 종료
+     * @param cryptogram 검사할 암호문
+     * @param index 검사해야 할 string index
+     * @param before 이전에 검사했던 character
+     * @param duplicateFoundInSequence 이번 sequence에서 중복이 발생했는지
+     * @return 재귀호출 또는 해독된 암호문
+     */
     private static String eliminateDuplicateChars(String cryptogram, int index, char before, boolean duplicateFoundInSequence){
         if(index + 1 > cryptogram.length()){
             if(duplicateFoundInSequence==true) {
