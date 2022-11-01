0fa17341deabdb9f3c0c231b4c5b5066ade6b84b
tdmdgh <tdmdgh@gmail.com>
2022-11-01 19:04:47 +0900

    problem5_1_Define moneyUnit

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private final int[] moneyUnit = {50000,10000,5000,1000,500,100,50,10,1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
