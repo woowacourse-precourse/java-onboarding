9134a66c31ef25a7aafd758a82ee210611db4845
hwanee__ <fkfkthth@naver.com>
2022-11-01 12:50:13 +0900

    4번문제 기능목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -14,18 +14,18 @@ public class Problem2 {
         String result = "";
 
         for (i = 0; i < str.length()-1; i++) {
-           for(j = i+1; j < str.length();j++){
-               if(str.charAt(i)!=str.charAt(j)) break;
-               else check=1;
+            for(j = i+1; j < str.length();j++){
+                if(str.charAt(i)!=str.charAt(j)) break;
+                else check=1;
             }
 
-           if(check==1){
-               for(int z = 0; z < str.length();z++){
-                   if(z < i || z >= j) result += str.charAt(z);
-               }
-               if(!result.equals("")) result = check_duplication(result);
-               break;
-           }
+            if(check==1){
+                for(int z = 0; z < str.length();z++){
+                    if(z < i || z >= j) result += str.charAt(z);
+                }
+                if(!result.equals("")) result = check_duplication(result);
+                break;
+            }
 
         }
 
