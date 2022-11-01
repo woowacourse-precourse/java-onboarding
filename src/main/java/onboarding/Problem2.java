7ea0ba1bf811cb800e450126a79f7e1a9a67ad47
swonny <wonny921@gmail.com>
2022-11-01 15:26:35 +0900

    refactor: Problem2 refactor
    중복되는 문자의 index를 기존 String 자료형에서 ArrayList로 변경:
    - 자바 기본 라이브러리의 추가가 불가능하다고 생각해서 index를 String으로 저장했었던 것을 ArrayList로 변경하였다.
    - ArrayList는 동적인 추가/삭제가 가능하다는 점에서 중복되는 문자의 index를 저장하기에 적합하다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,10 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+    /* 공백으로 대체된 문자 제거 */
     public static String removeSpace(char[] chars) {
         String result = "";
         for(int i = 0; i < chars.length; i++) {
@@ -12,34 +16,31 @@ public class Problem2 {
         return result;
     }
 
-    public static String deleteDuplication(String cryptogram, String indexes) {
+    /* 2개 이상 중복되는 문자 제거
+    *  중복 문자 즉시 제거 시 index가 달라질 수 있으므로 공백으로 대입 후 removeSpace를 통해 공백 제거
+    */
+    public static String deleteDuplication(String cryptogram, List<Integer> indexes) {
         char[] chars = cryptogram.toCharArray();
-        String tmp = "";
-        for(int i = 0; i < indexes.length(); i++) {
-            if(indexes.charAt(i) != ' ') {
-                tmp += Character.toString(indexes.charAt(i));
-                continue;
-            }
-            chars[Integer.parseInt(tmp)] = ' ';
-            tmp = "";
+        for(Integer i : indexes) {
+            chars[i] = ' ';
         }
         cryptogram = removeSpace(chars);
         return cryptogram;
     }
 
+    /* 중복 문자에 해당하는 인덱스 찾기 */
     public static String findDuplication(String cryptogram) {
         char prevChar = cryptogram.charAt(0);
-        String indexes;
         while(true) {
-            indexes = "";
+            List<Integer> indexes = new ArrayList<Integer>();
             for(int i = 1; i < cryptogram.length(); i++) {
                 if(prevChar == cryptogram.charAt(i)) {
-                    indexes += Integer.toString(i - 1) + " ";
-                    indexes += Integer.toString(i) + " ";
+                    indexes.add(i - 1);
+                    indexes.add(i);
                 }
                 prevChar = cryptogram.charAt(i);
             }
-            if(indexes.length() == 0) {
+            if(indexes.size() == 0) {
                 break;
             }
             cryptogram = deleteDuplication(cryptogram, indexes);
@@ -52,9 +53,5 @@ public class Problem2 {
         answer = findDuplication(cryptogram);
         System.out.println(answer);
         return answer;
-    }
-
-    public static void main(String[] args) {
-        solution("abaaa");
     }
 }
