f0156fa4a183eddf601419c34e75959584e9b495
poter0501 <poter0501@gmail.com>
2022-11-01 13:46:18 +0900

    feat: createUserInfo 함수 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,40 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        HashMap<String, HashSet<String>> userInfo = createUserInfo(friends);
+
         return answer;
+    }
+
+    private static HashMap<String, HashSet<String>> createUserInfo(List<List<String>> friends) {
+        HashMap<String, HashSet<String>> userInfo = new HashMap<String, HashSet<String>>();
+        for (List<String> pair : friends) {
+            String people0 = pair.get(0);
+            String people1 = pair.get(1);
+            if (userInfo.containsKey(people0)){
+                userInfo.get(people0).add(people1);
+            }
+            else {
+                HashSet<String> temp = new HashSet<String>();
+                temp.add(people1);
+                userInfo.put(people0,temp);
+            }
+            if (userInfo.containsKey(people1)){
+                userInfo.get(people1).add(people0);
+            }
+            else {
+                HashSet<String> temp = new HashSet<String>();
+                temp.add(people0);
+                userInfo.put(people1,temp);
+            }
+        }
+        return userInfo;
     }
 }
