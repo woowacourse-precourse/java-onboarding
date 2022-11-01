1c1e661237d268ca5d9a265d06cb0664b8147758
villya <sunwooye@gmail.com>
2022-11-01 22:22:53 +0900

    prob 1 & 2 & 3 done coding prob 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = word;
+        char alph;
+        int k;
+        for(k=0;k<answer.length();k++){
+            alph = answer.charAt(k);
+
+        }
+
         return answer;
     }
 }
