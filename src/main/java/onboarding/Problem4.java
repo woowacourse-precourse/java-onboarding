02da3988cdafac9df5206f986aa8b521a4b7ebf2
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2022-11-01 10:08:44 +0900

    feat : 문자 변환 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder(word);
+
+        for(int i = 0; i<answer.length();i++){
+            char temp1 = ' ';
+            char temp2 = answer.charAt(i);
+
+            if(Character.isUpperCase(temp2))
+                temp1 = (char) ('A' + 'Z' - temp2);
+            else if (Character.isLowerCase(temp2)) {
+                temp1 = (char) ('a' + 'z' - temp2);
+            }
+            answer.setCharAt(i, temp1);
+        }
+
+        return answer.toString();
     }
 }
