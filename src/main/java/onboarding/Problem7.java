52f72e5ac3cb75f638f0ff0c168545ed2059c585
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-01 15:32:09 +0900

    RecommandAlgorithm 생성자 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,7 +1,6 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Problem_7_Restriction extends Exception {
     public Problem_7_Restriction() {
@@ -11,6 +10,35 @@ class Problem_7_Restriction extends Exception {
 
     public Problem_7_Restriction(String message) {
         super(message);
+    }
+}
+
+class RecommandAlgorithm {
+    private Set<String> unknownUsers = new TreeSet<>();
+    private List<String> userFriends = new ArrayList<>();
+    private List<String> friendsOfUserFriends = new ArrayList<>();
+    private List<String> visitors = new ArrayList<>();
+    private Map<String, Integer> scoresOfFriends = new TreeMap<>();
+    private LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+    private String user = "";
+
+    RecommandAlgorithm(String user, List<List<String>> friends, List<String> visitors) {
+        this.user = user;
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).get(0) == user) {
+                this.userFriends.add(friends.get(i).get(1));
+            } else if (friends.get(i).get(1) == user) {
+                this.userFriends.add(friends.get(i).get(0));
+            }
+        }
+        for (String visitor : visitors) {
+            if (this.userFriends.contains(visitor)) {
+                continue;
+            } else {
+                this.visitors.add(visitor);
+                this.unknownUsers.add(visitor);
+            }
+        }
     }
 }
 
