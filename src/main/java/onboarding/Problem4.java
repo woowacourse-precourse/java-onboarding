464556edfff96733b9774583cf65038998e87081
hyunseo <goatsfish@gmail.com>
2022-11-01 14:32:31 +0900

    feat(Problem4) : 대소문자 알파벳 반대 변환 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,49 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static final char NOTHING = ' ';
+    private static final int LETTER_CHARACTER_INDEX = 0;
+    private static final int ALPHABET_UPPER_CASE_MAX_ASCII_CODE = 'Z';
+    private static final int ALPHABET_DOWN_CASE_MAX_ASCII_CODE = 'z';
+    private static final int ALPHABET_TOTAL_SIZE = 'Z' - 'A';
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
     }
+
+    private static char reverseLetter(char letter) {
+        char reversedLetter = NOTHING;
+        if (checkUpperCase(letter)) {
+            reversedLetter = reverseUpperCaseLetter(letter);
+        }
+        if (checkDownCase(letter)) {
+            reversedLetter = reverseDownCaseLetter(letter);
+        }
+        return reversedLetter;
+    }
+
+    private static char reverseUpperCaseLetter(char letter) {
+        return (char) (ALPHABET_UPPER_CASE_MAX_ASCII_CODE * 2 - letter - ALPHABET_TOTAL_SIZE);
+    }
+
+    private static char reverseDownCaseLetter(char letter) {
+        return (char) (ALPHABET_DOWN_CASE_MAX_ASCII_CODE * 2 - letter - ALPHABET_TOTAL_SIZE);
+    }
+
+    private static boolean checkUpperCase(char letter) {
+        return Character.isUpperCase(letter);
+    }
+
+    private static boolean checkDownCase(char letter) {
+        return !Character.isUpperCase(letter);
+    }
+
+    private static boolean checkNotAlphabetic(char letter) {
+        return !Character.isAlphabetic(letter);
+    }
+
+    private static char convertToCheckableLetter(String letter) {
+        return letter.charAt(LETTER_CHARACTER_INDEX);
+    }
+
 }
