3cb7d1818f492f5324204aa77a510c2551381088
zszszszs <so4823@gmail.com>
2022-11-01 19:05:59 +0900

    Mission 6, 7 clear & README.md Update

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,38 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<String>();
+        HashSet<String> set = overlapEmail(forms);
+        answer = hashSort(set);
+
         return answer;
     }
+    public static HashSet<String> overlapEmail(List<List<String>> form){ // 중복되는 닉네임을 가지는 크루들의 이메일을 저장하는 함수
+        HashSet<String> set = new HashSet<String>();
+        for(int i = 0; i < form.size(); i++){
+            for(int j = 0; j < form.get(i).get(1).length()-1; j++){
+                String s = "";
+                s += form.get(i).get(1).charAt(j);
+                s += form.get(i).get(1).charAt(j+1);
+                for(int k = 0; k < form.size(); k++){
+                    if(i == k) continue;
+                    if(form.get(k).get(1).contains(s)){
+                        set.add(form.get(i).get(0));
+                    }
+                }
+            }
+        }
+        return set;
+    }
+
+    public static ArrayList<String> hashSort(HashSet<String> set){ // Set을 받아서 정렬해주는 기능
+        ArrayList<String> ret = new ArrayList<String>(set);
+        Collections.sort(ret);
+        return ret;
+    }
+
+
 }
---
+++ src/main/java/onboarding/README.md
@@ -0,0 +1,27 @@
+## Problem 1
+1. eachNum(Integer a): 각 자리수를 ArrayList로 추출하는 함수
+2. Mul(ArrayList<Integer> arr): 구해진 각 자리 수 들을 곱하는 함수
+3. Add(ArrayList arr): 구해진 각 자리 수 들을 더하는 함수
+
+##  Problem 2
+1. overlapChar(String crptogram): 중복된 문자를 추출하는 함수
+2. sol(String cryptogram, ArrayList arr): 중복된 문자를 제거하는 함수
+
+## Problem 3
+1. count(int num) : 손뼉 치는 횟수를 구하는 함수
+
+## Problem 4
+1. changeWord(Character c): 글자 변환 하는 함수
+
+## Problem 5
+1. Coins(int coin): 돈이 주어지면 각 동전으로 변환하는 함수
+
+## Problem 6
+1. overlapEmail(List<List<String>> form) : 중복되는 닉네임을 가지는 크루들의 이메일을 정하는 함수
+2. hashSort(HashSet<String> set): Set을 받아서 정렬해주는 기능
+
+## Problem 7
+1. userFriends(String user, List<List<String>> friends): user의 친구를 추출하는 함수
+2. member(String user, List<List>> Friends, List visitors): 유저를 제외한 모든 사람들을 저장하는 함수
+3. pointNemail(List<List<String>> friends, List<String>visirots, HashSet<String>set, List<String> ufriends) : 유저의 친구를 제외한 사람들의 포인트를 구하고 그 포인트를 내림차 순으로 정렬하는 함수(단 포인트가 같다면 이름을 오름차순으로 정렬)
+4. resultName(String[][] str): 포인트가 0이아닌 사람들 중 가장 큰 사람 순을 최대 5명까지 추출하는 함수
