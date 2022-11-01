95b2cfa5b78de34b022ba9678bfaf876c417af4f
이지영 <jioung93@naver.com>
2022-11-01 21:03:57 +0900

    [Problem 4] refactor: else문 제거 및 함수 통합

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -9,35 +9,24 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         StringBuffer sb = new StringBuffer();
-        for (int i=0; i<word.length(); i++){
+        for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if('a'<=c && c<='z'){
-                sb.append(reverseLowerAlphabet(c));
-            } else if ('A'<=c && c<='Z'){
-                sb.append(reverseUpperAlphabet(c));
-            } else {
-                sb.append(c);
-            }
+            c = reverseAlphabet(c);
+            sb.append(c);
         }
         answer = sb.toString();
         return answer;
     }
 
-    // 알파벳(대문자)을 반대쪽 알파벳으로 변환해주는 함수 reversUpperAlphabet()
-    private static char reverseUpperAlphabet(char c){
-        char result = c;
-        if ('A'<=c && c<='Z'){
-            result = (char) ('Z'-c+'A');
+    // 알파벳을 반대쪽 알파벳으로 변환해주는 함수 reverseAlphabet()
+    private static char reverseAlphabet(char c) {
+        if (Character.isLowerCase(c)) {
+            return (char) ('z' - c + 'a');
         }
-        return result;
+        if (Character.isUpperCase(c)) {
+            return (char) ('Z' - c + 'A');
+        }
+        return c;
     }
 
-    // 알파벳(소문자)을 반대쪽 알파벳으로 알파벳(대문자)을 반대쪽 알파벳으로 변환해주는 함수 reversLowerAlphabet()
-    private static char reverseLowerAlphabet(char c){
-        char result = c;
-        if ('a'<=c && c<='z'){
-            result = (char) ('z'-c+'a');
-        }
-        return result;
-    }
 }
