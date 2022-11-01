ffc4ecac2541023f29e55641c174d3112270368d
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-01 21:28:44 +0900

    fix(countCoinByCoinUnit): [Prob 5] 반환 타입을 ArrayList로 변경

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,22 +2,25 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Problem5 {
     static int countOfCoinUnit=9;
     public static Integer[] coinValueByCoinUnit=new Integer[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
-    public static int[] countCoinByCoinUnit(int money){
-        int[] coinCountByCoinUnit=new int[]{0,0,0,0,0,0,0,0,0};
+    public static List<Integer> countCoinByCoinUnit(int money){
+        List<Integer> coinCountByCoinUnit=new ArrayList<>();
         for(int coinUnitIndex=0;coinUnitIndex<countOfCoinUnit;coinUnitIndex++){
             int coinCountByUnit=money/coinValueByCoinUnit[coinUnitIndex];
             money-=coinCountByUnit*coinValueByCoinUnit[coinUnitIndex];
-            coinCountByCoinUnit[coinUnitIndex]=coinCountByUnit;
+            coinCountByCoinUnit.set(coinUnitIndex, coinCountByUnit);
         }
         return coinCountByCoinUnit;
     }
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+
         return answer;
     }
 }
