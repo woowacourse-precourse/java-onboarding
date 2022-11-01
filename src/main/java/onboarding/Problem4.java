2782c934f5860c6a18012da9fc89b5a4241c1d89
ParkJoungWoo <jug78@naver.com>
2022-11-01 19:03:45 +0900

    문제 4) 기능 1 : 문자열을 입력받으면 각 자리의 문자를 대소문자 변환, 알파벳 역순 변환을 수행한 문자열을 반환하는 기능 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = changeWord(word);
         return answer;
+    }
+    private static String changeWord(String input){
+        String result = "";
+        char temp = ' ';
+        for(int i = 0; i < input.length(); i++){
+            temp = input.charAt(i);
+            if(((int)input.charAt(i) > 96) && (int)input.charAt(i) < 123)
+                temp = (char)(122 - (int)input.charAt(i) + 'a');
+            if(((int)input.charAt(i) > 64) && (int)input.charAt(i) < 91)
+                temp = (char)(90 - (int)input.charAt(i) + 'A');
+            result += temp;
+        }
+        return result;
     }
 }
