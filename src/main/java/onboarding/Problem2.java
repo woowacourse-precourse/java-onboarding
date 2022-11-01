6b4326a1a36bb75a6453bbceeb4bc4df7530cba8
Jaewoooong Kim <kimje1109@gmail.com>
2022-11-01 15:15:35 +0900

    fix: Change removeDuplicateChars to handle exceptional cases

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -22,20 +22,30 @@ public class Problem2 {
     }
 
     public static String removeDuplicateChars(String string) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result =new StringBuilder();
+        int index = 0;
 
-        for (int index = 0; index < (string.length() - 1) ; index++) {
-            if (string.charAt(index) != string.charAt(index + 1)) {
+        while(index < string.length()) {
+            if(0 ==findNextIndex(string, index)) {
+                result.append("");
+                return result.toString();
+            }
+            if(index + 1 ==findNextIndex(string, index)) {
                 result.append(string.charAt(index));
+                index =findNextIndex(string, index);
+                continue;
             }
-            if (string.charAt(index) == string.charAt(index + 1)) {
-                index++;
+            if(string.length() - 1 ==findNextIndex(string, index)) {
+                index =findNextIndex(string, index);
+                result.append(string.charAt(index));
+                return result.toString();
             }
+            if(index ==findNextIndex(string, index)) {
+                return result.toString();
+            }
+            index =findNextIndex(string, index);
         }
-        if (string.charAt(string.length() - 2) != string.charAt(string.length() - 1)) {
-            result.append(string.charAt(string.length() - 1));
-        }
-        
+
         return result.toString();
     }
 
