ef41068407ada974455cd2b669be0edef8d460a6
i960107 <i960107@naver.com>
2022-11-01 12:23:40 +0900

    feat(problem5): implement feature of get count of each unit to get money in lightest way

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,33 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    static int[] units = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        List<Integer> answer = new ArrayList<>();
+
+        int moneyLeft = money;
+
+        for (int unit : units) {
+            int count = getMaxCountByUnit(moneyLeft, unit);
+            answer.add(count);
+
+            moneyLeft -= getTotalAmount(unit, count);
+        }
+
         return answer;
     }
+
+    public static int getTotalAmount(int unit, int count) {
+        return unit * count;
+    }
+
+    public static int getMaxCountByUnit(int money, int unit) {
+        return money / unit;
+    }
+
 }
