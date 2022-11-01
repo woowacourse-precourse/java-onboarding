ad186e7b4334fa20dbee2b117c51aa5e5717970c
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-01 19:42:00 +0900

    first solution

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,51 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<String> nicknames = new ArrayList<>();
+        List<String> emails = new ArrayList<>();
+        List<String> nicknamesExceptThis;
+        int index;
+        String nickname;
+        boolean exists;
+
+        for (List<String> user : forms) {
+            emails.add(user.get(0));
+            nicknames.add(user.get(1));
+        }
+
+        for (int j = 0; j < nicknames.size(); j++) {
+            nickname = nicknames.get(j);
+            nicknamesExceptThis = new ArrayList<>(nicknames);
+            nicknamesExceptThis.remove(j);
+            exists = false;
+            for (int k = 0; k < nickname.length(); k++) {
+                index = (toString(nicknamesExceptThis)).indexOf(nickname.charAt(k));
+                if ((index != -1) && (index + 1 < nicknamesExceptThis.size()) && (k + 1 < nickname.length())) {
+                    if (toString(nicknamesExceptThis).charAt(index + 1) == nickname.charAt(k + 1)) {
+                        exists = true;
+                    }
+                }
+            }
+            if (exists) {
+                answer.add(emails.get(j));
+            }
+        }
+
+        Collections.sort(answer);
+        answer = new ArrayList<>(new LinkedHashSet<>(answer));
         return answer;
+    }
+
+    static String toString(List<String> arr) {
+        StringBuilder str = new StringBuilder();
+        for (String item : arr) {
+            str.append(item);
+            str.append(" ");
+        }
+        return str.toString();
     }
 }
--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,100 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Set<String> uniqueFriends = new HashSet<>();
+        List<String> listOfUniqueFriends;
+        ArrayList<String>[] friendsOfUsers;
+        ArrayList<String> friendsOfThisUser;
+        Map<String, Integer> scoresWithNames = new HashMap<>();
+
+        int[] scores;
+        int idx1, idx2, userIdx, numOfUsers;
+
+        for (List<String> friendship : friends) {
+            uniqueFriends.addAll(friendship);
+        }
+        uniqueFriends.addAll(visitors);
+        numOfUsers = uniqueFriends.size();
+
+        listOfUniqueFriends = new ArrayList<>(uniqueFriends);
+        friendsOfUsers = new ArrayList[numOfUsers];
+        for (int i = 0; i < numOfUsers; i ++) {
+            friendsOfUsers[i] = new ArrayList<>();
+        }
+
+        for (List<String> friendship : friends) {
+            idx1 = listOfUniqueFriends.indexOf(friendship.get(0));
+            idx2 = listOfUniqueFriends.indexOf(friendship.get(1));
+            friendsOfUsers[idx1].add(friendship.get(1));
+            friendsOfUsers[idx2].add(friendship.get(0));
+        }
+
+        userIdx = listOfUniqueFriends.indexOf(user);
+        friendsOfThisUser = friendsOfUsers[userIdx];
+        friendsOfUsers[userIdx] = new ArrayList<>();
+        scores = new int[numOfUsers];
+        Arrays.fill(scores, 0);
+
+        // 함께 아는 친구 수 당 10점 추가
+        for (int j = 0; j < numOfUsers; j++) {
+            for (String friend : friendsOfUsers[j]) {
+                if (friendsOfThisUser.contains(friend)) {
+                    scores[j] += 10;
+                }
+            }
+        }
+
+        // 방문 횟수 당 1점 추가
+        for (String visitor : visitors) {
+            idx1 = listOfUniqueFriends.indexOf(visitor);
+            scores[idx1] ++;
+        }
+
+        //현재 유저와 이미 친구면 추천 친구에서 제거 (스코어 0점 처리)
+        for (String friendOfThisUser : friendsOfThisUser) {
+            idx1 = listOfUniqueFriends.indexOf(friendOfThisUser);
+            scores[idx1] = 0;
+        }
+
+        for (int k = 0; k < numOfUsers; k++) {
+            scoresWithNames.put(listOfUniqueFriends.get(k), scores[k]);
+        }
+
+        return top_five(sortByValueThenKey(scoresWithNames));
+
+    }
+
+    public static <K extends Comparable<? super K>, V extends Comparable<? super V>> Map<K, V> sortByValueThenKey(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            @Override
+            public int compare(Map.Entry<K, V> user1, Map.Entry<K, V> user2) {
+                if (user1.getValue().equals(user2.getValue()))
+                    return user1.getKey().compareTo(user2.getKey());
+                else
+                    return user2.getValue().compareTo(user1.getValue());
+            }
+        });
+
+        Map<K, V> sorted = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            sorted.put(entry.getKey(), entry.getValue());
+        }
+        return sorted;
+    }
+
+    static ArrayList<String> top_five(Map<String, Integer> hm) {
+        int count = 0;
+        ArrayList<String> recommended = new ArrayList<>();
+        for (Map.Entry<String, Integer> user : hm.entrySet()) {
+            if (user.getValue() > 0 && count < 6) {
+                recommended.add(user.getKey());
+                count++;
+            }
+        }
+        return recommended;
     }
 }
