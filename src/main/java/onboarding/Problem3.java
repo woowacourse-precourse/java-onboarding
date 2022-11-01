58305f636b7741d977f9833de8f4e175027f2625
bnbnac <bnbnac@naver.com>
2022-11-01 21:44:31 +0900

    feat: (P3)1부터 입력받은 숫자까지 위 메소드를 실행하여 더하는 메소드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,6 +10,9 @@ public class Problem3 {
 
     public static int solution(int number) {
         int answer = 0;
+        for (int i=1; i<=number; i++) {
+            answer += findNumOfClap(digitList(i));
+        }
         return answer;
     }
 
