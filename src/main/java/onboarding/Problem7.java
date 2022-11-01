2c71ef57dd383629f0608bcbb18ea176638929e2
minje <alswp0910@naver.com>
2022-11-01 12:31:38 +0900

    문제7_기능1

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    //기능1. 사용자의 친구 목록
+    public static List<String> userFriends(String user, List<List<String>> friends){
+        HashSet<String> userFri = new HashSet<>();
+
+        for (List<String> fri: friends) {
+
+            if(fri.get(0).equals(user)){
+                userFri.add(fri.get(1));
+            } else if (fri.get(1).equals(user)) {
+                userFri.add(fri.get(0));
+            }
+        }
+
+        List<String> userFriends = new ArrayList<>(userFri);
+
+        return userFriends;
     }
 }
