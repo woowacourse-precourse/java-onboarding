2a6d7f4a2cc80beaa469f7b72ebbb1eea64d5bd2
jeonyuneo <piq2255@gmail.com>
2022-11-01 22:10:28 +0900

    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,39 @@
 package onboarding;
 
 public class Problem4 {
+    private static final char LOWER_A = 'a';
+    private static final char LOWER_Z = 'z';
+    private static final char UPPER_A = 'A';
+    private static final char UPPER_Z = 'Z';
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            if (!isAlphabet(letter)) {
+                answer.append(letter);
+                continue;
+            }
+            if (isLowerCase(letter)) {
+                answer.append(Character.toString(LOWER_Z - (letter - LOWER_A)));
+                continue;
+            }
+            if (isUpperCase(letter)) {
+                answer.append(Character.toString(UPPER_Z - (letter - UPPER_A)));
+            }
+        }
+        return answer.toString();
+    }
+
+    private static boolean isAlphabet(char letter) {
+        return isLowerCase(letter) || isUpperCase(letter);
+    }
+
+    private static boolean isLowerCase(char letter) {
+        return letter >= 97 && letter <= 122;
+    }
+
+    private static boolean isUpperCase(char letter) {
+        return letter >= 65 && letter <= 90;
     }
 }
