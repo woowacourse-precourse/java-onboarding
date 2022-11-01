8fdd4786f3f17c29e4a2cd7de830fe35a79f2faa
jinuk <devuks0820@gmail.com>
2022-11-01 11:34:39 +0900

    feat: Problem5 Add method
    각 단위별 연산을 통한 결과값 출력

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = changeMoney(money);
         return answer;
+    }
+
+    private static List<Integer> changeMoney(int money) {
+        List<Integer> moneys = new ArrayList<Integer>();
+
+        int[] digitMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int digit : digitMoney) {
+            moneys.add(money / digit);
+            money%=digit;
+        }
+
+        return moneys;
     }
 }
