0b99df22451b3a6f32ec3599c9b136cc980b364e
silver <silver36c@gmail.com>
2022-11-01 21:54:29 +0900

    feat: Problem2 메인 로직 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,25 @@
 package onboarding;
+import java.util.Stack;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        char[] chars = cryptogram.toCharArray();
+
+        // 스택에 넣기
+        for (char c: chars) {
+            insertStack(stack, c);
+        }
+
+        // 스택에서 빼기
+        while(!stack.empty()){
+            sb.append(stack.peek());
+            stack.pop();
+        }
+
+        String answer = sb.reverse().toString();
         return answer;
     }
 
