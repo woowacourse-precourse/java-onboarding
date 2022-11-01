7d9aa6e8924d8d5ae02d20fcf3996f5b217d8b2a
kimJaeHyeon <kmr2644@gmail.com>
2022-11-01 18:25:58 +0900

    feat: implement method for createRelationMap

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,29 +1,53 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Map<String, List<String>> relationMap = createRelationMap(getUserFriendList(user, friends), friends);
         return answer;
     }
 
-    public static List<String> getUserFriends(String user , List<List<String>> friends) {
+    public static List<String> getUserFriendList(String user, List<List<String>> friends) {
         return friends.stream()
                       .filter(userAndFriends -> userAndFriends.contains(user))
-                      .map(userAndFriends -> getFriends(user, userAndFriends))
+                      .map(userAndFriend -> getFriend(user, userAndFriend))
                       .collect(Collectors.toList());
-
     }
 
-    private static String getFriends(String user,List<String> userAndFriends) {
-        String result = userAndFriends.get(0);
-        if (result.equals(user)) {
-            return userAndFriends.get(1);
+    public static Map<String, List<String>> createRelationMap(List<List<String>> friends) {
+
+        Map<String, List<String>> relationMap = new HashMap<>();
+        for (List<String> userRelations : friends) {
+            String user1 = userRelations.get(0);
+            String user2 = userRelations.get(1);
+            addRelation(relationMap,user1);
+            addRelation(relationMap, user2);
+            setRelation(relationMap, user1, user2);
         }
-        return result;
+        System.out.println(relationMap);
+        return relationMap;
+    }
+
+    private static void setRelation(Map<String, List<String>> relationMap, String user1, String user2) {
+        relationMap.get(user1)
+                   .add(user2);
+        relationMap.get(user2)
+                   .add(user1);
+    }
+    private static void addRelation(Map<String, List<String>> relationMap, String user) {
+        if (relationMap.containsKey(user)) {
+            return;
+        }
+        relationMap.put(user, new ArrayList<>());
+    }
+
+    private static String getFriend(String user, List<String> userAndFriends) {
+        return userAndFriends.stream()
+                             .filter(friend -> !friend.equals(user))
+                             .collect(Collectors.joining());
     }
 
 }
