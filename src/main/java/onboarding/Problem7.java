3c4767413898fe95d7addb2fa77841fe333d85a8
metorg <metrog950@gmail.com>
2022-11-01 22:06:57 +0900

    feat: [Problem7] 스코어 정렬 메서드 생성 스코어는 내림차순 정렬 같을시에 이름 오름차순 정렬

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,19 +1,25 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
 
     public static HashMap<String,Boolean> user_friend;
     public static HashMap<String,Integer> friend_friend;
+    public static List<Map.Entry<String, Integer>> result ;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
 
         user_friend = new HashMap<String,Boolean>();
         friend_friend = new HashMap<String,Integer>();
+
+        userFriendCheck(user,friends);
+        friendOfFriend(user,friends);
+        friendVisitCheck(visitors);
+
+        result = new LinkedList<>(friend_friend.entrySet());
+        friendScoreSort();
+
         List<String> answer = Collections.emptyList();
 
         return answer;
@@ -61,15 +67,37 @@ public class Problem7 {
 
     public static void friendVisitCheck(List<String> visitors){
         for(int i=0;i<visitors.size();i++){
-
+            friendVisitException(visitors.get(i));
         }
     }
 
     public static void friendVisitException(String name){
         if(user_friend.containsKey(name)==true)return;
         else{
-            friend_friend.put(name,friend_friend.get(name)+1);
+            if(friend_friend.containsKey(name)==false){
+                friend_friend.put(name,1);
+            }
+            else {
+                friend_friend.put(name, friend_friend.get(name) + 1);
+            }
         }
     }
+
+    public static void friendScoreSort(){
+        Collections.sort(result, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> score1, Map.Entry<String, Integer> score2) {
+                if (score1.getValue() > score2.getValue()) {
+                    return -1;
+                }
+                else if (score1.getValue() < score2.getValue()) {
+                    return 1;
+                }
+
+                return score1.getKey().compareTo(score2.getKey());
+            }
+        });
+    }
+    
 
 }
