94ad870ca46108f2ab5cc1cbee5f2f386ec8eb3d
김태용 <amico741@naver.com>
2022-11-01 09:54:28 +0900

    refactor: DEPTH 1을 위한 검증 메서드 따로 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,6 +3,7 @@ package onboarding;
 import java.util.*;
 
 public class Problem6 {
+    private static final int MIN_SIZE = 2;
     public static List<String> solution(List<List<String>> forms) {
         HashSet<String> letters = makeLetterSet(forms);
         TreeSet<String> emails = getDuplicateSet(letters, forms);
@@ -22,20 +23,25 @@ public class Problem6 {
 
     public static TreeSet<String> getDuplicateSet(HashSet<String> letters, List<List<String>> forms){
         Iterator<String> iterator = letters.iterator();
-        ArrayList<String> temp = new ArrayList<>();
         TreeSet<String> answer = new TreeSet<>();
 
         while(iterator.hasNext()){ // 연속된 문자열 순회하면서 nickname이 포함하고 있는지 체크
             String letter = iterator.next();
-            for(List<String> crew : forms){
-                String nickname = crew.get(1);
-                if(nickname.contains(letter)){
-                    temp.add(crew.get(0));
-                }
-            }
-            if(temp.size() >= 2) answer.addAll(temp); // 사이즈 2개 이상이라면 TreeSet에 추가 -> 이메일 오름차순 정렬 및 중복 제거
-            temp.clear();
+            answer.addAll(validation(letter, forms)); // 사이즈 2개 이상이라면 TreeSet에 추가 -> 이메일 오름차순 정렬 및 중복 제거
         }
         return answer;
+    }
+
+    public static ArrayList<String> validation(String letter, List<List<String>> forms){
+        ArrayList<String> temp = new ArrayList<>();
+        for(List<String> crew : forms){
+            String nickname = crew.get(1);
+            if(nickname.contains(letter)){ // letter를 포함하고 있다면
+                temp.add(crew.get(0));
+            }
+        }
+        if(temp.size() >= MIN_SIZE) return temp;
+        temp.clear();
+        return temp;
     }
 }
