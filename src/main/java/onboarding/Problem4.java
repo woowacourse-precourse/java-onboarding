4cb9f0bead531087942853e80cd957e541188dc0
hyungkya <hyungkya@uci.edu>
2022-11-01 19:25:26 +0900

    fix(problem 4): fixed character reversal other than English
    isAlphabetic() method returns true regardless of language if it is alphabetic. By adding custom static method, only the character in range of English ascii-code will be reversed.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -13,11 +13,14 @@ public class Problem4 {
         return frogWord.toString();
     }
     private static char toFrogChar(char c) {
-        if (Character.isAlphabetic(c)) {
+        if (isEnglish(c)) {
             // "155, 219" are each sum of uppercase, lowercase alphabet ascii code's start/end.
             // Subtracting original ascii code from it will result in a reversed alphabet.
             c = (char) (Character.isUpperCase(c) ? 155 - c : 219 - c);
         }
         return c;
+    }
+    private static boolean isEnglish(char c) {
+        return ((65 <= c & c <= 90) | (97 <= c & c <= 122));
     }
 }
