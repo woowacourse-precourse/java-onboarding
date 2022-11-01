3adf1739922cd39fa6f9c661e45218d7a0bb0f04
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-01 15:15:29 +0900

    Refactor: Problem1 code refactoring - rename function & variable

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,57 +7,57 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        if (error_handling(pobi) == false) return -1;
-        if (error_handling(crong) == false) return -1;
+        if (errorHandling(pobi) == false) return -1;
+        if (errorHandling(crong) == false) return -1;
 
-        int pobi_result = compare_left_right(pobi);
-        int crong_result = compare_left_right(crong);
+        int pobiResult = compareLeftRightPage(pobi);
+        int crongResult = compareLeftRightPage(crong);
 
-        if (pobi_result>crong_result) answer=1;
-        else if (pobi_result<crong_result) answer=2;
-        else answer=0;
+        if (pobiResult > crongResult) answer = 1; // pobi is winner
+        else if (pobiResult < crongResult) answer = 2; // crong is winner
+        else answer = 0; // tie
 
         return answer;
     }
 
-    public static boolean error_handling(List<Integer> who){
+    public static boolean errorHandling(List<Integer> who){
         int left = who.get(0);
         int right = who.get(1);
 
         if (left%2==1 && left+1==right && 0<left && right<401) return true;
         else return false;
     }
-    public static String[] int2intList(int page_number){
-        String strNum = Integer.toString(page_number);
+    public static String[] integerToStringList(int num){
+        String strNum = Integer.toString(num);
         return strNum.split("");
     }
-    public static int page_summation(int page_number){
+    public static int summationPage(int pageNumber){
         int result = 0;
-        String[] arrNum = int2intList(page_number);
-        for (String str : arrNum){
-            result += Integer.valueOf(str);
+        String[] arrNumOfPage = integerToStringList(pageNumber);
+        for (String strNum : arrNumOfPage){
+            result += Integer.valueOf(strNum);
         }
         return result;
     }
-    public static int page_muliplication(int page_number){
+    public static int multiplicationPage(int pageNumber){
         int result = 1;
-        String[] arrNum = int2intList(page_number);
-        for (String str : arrNum){
-            result *= Integer.valueOf(str);
+        String[] arrNumOfPage = integerToStringList(pageNumber);
+        for (String strNum : arrNumOfPage){
+            result *= Integer.valueOf(strNum);
         }
         return result;
     }
-    public static int compare_left_right(List<Integer> who){
+    public static int compareLeftRightPage(List<Integer> who){
         int result = Integer.MIN_VALUE;
 
-        int left_page = who.get(0);
-        int right_page = who.get(1);
+        int leftPage = who.get(0);
+        int rightPage = who.get(1);
 
         int[] candidate = new int[4];
-        candidate[0] = page_summation(left_page);
-        candidate[1] = page_muliplication(left_page);
-        candidate[2] = page_summation(right_page);
-        candidate[3] = page_muliplication(right_page);
+        candidate[0] = summationPage(leftPage);
+        candidate[1] = multiplicationPage(leftPage);
+        candidate[2] = summationPage(rightPage);
+        candidate[3] = multiplicationPage(rightPage);
 
         for (int candy : candidate) result = (candy > result) ? candy : result;
 
