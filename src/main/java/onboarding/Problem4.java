fc823ca06ccb85c36f6b83136a2bdbb1fe8cc689
ckyeon <qsky8520@gmail.com>
2022-11-01 19:11:51 +0900

    Add createGreenFrogDictionary() to Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
     public static String solution(String word) {
-        String answer = "";
+        HashMap<Character, Character> greenFrogDictionary = createGreenFrogDictionary();
         return answer;
+    }
+
+    private static HashMap<Character, Character> createGreenFrogDictionary() {
+        HashMap<Character, Character> greenFrogDictionary = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            greenFrogDictionary.put((char) ('a' + i), (char) ('z' - i));
+            greenFrogDictionary.put((char) ('A' + i), (char) ('Z' - i));
+        }
+        return greenFrogDictionary;
     }
 }
