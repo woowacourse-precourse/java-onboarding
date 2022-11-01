1ddfc6c012d9bf10d63e7256f685d1b9b06868e3
Sangin <ksiisk99@gmail.com>
2022-11-01 16:10:58 +0900

    feat: problem7 - user와 친구이거나 친구가 아닌 사람을 찾는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static Map<String,Integer> recommendedFriendScores =new HashMap<>();
+    private static List<String> userFriendList =new ArrayList<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
         return answer;
     }
+
+    private static String findUserFriendId(List<String> friend, String userId) {
+        String friendId1=friend.get(0);
+        String friendId2=friend.get(1);
+
+        if(friendId1.equals(userId)){
+            return friendId2;
+        }
+        if(friendId2.equals(userId)){
+            return friendId1;
+        }
+        return "";
+    }
+
+
+
 }
