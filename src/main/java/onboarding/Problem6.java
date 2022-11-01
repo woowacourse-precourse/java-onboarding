bf17be21480837447dffd31a744dc4a056555ebc
jeomxon <jeomxon@gmail.com>
2022-11-01 22:09:07 +0900

    feat(Problem6): check 2 letter is repeated

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,22 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+
+
         return answer;
+    }
+
+    public static boolean checkRepeated(String longerWord, String shorterWord) {
+
+        for (int i = 0; i < shorterWord.length()-1; i++) {
+
+            if (longerWord.contains(shorterWord.substring(i, i + 2))) {
+                return true;
+            }
+
+        }
+
+        return false;
     }
 }
