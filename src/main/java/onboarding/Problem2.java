142fcbe35ffebc9c022b70abf21774a9514a6865
qkrdmswl <eunji303@naver.com>
2022-11-01 17:27:42 +0900

    [fix] 오류 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,7 +3,6 @@ package onboarding;
 import java.util.ArrayList;
 
 public class Problem2 {
-
     /** 기능 명세
      * 1. 중복 문자 찾아 삭제하기
      *    - 입력받은 문자열을 문자 배열(cryptoToArr)로 변환
@@ -12,7 +11,6 @@ public class Problem2 {
      * 2. 결과 배열을 다시 문자열로 만들기
      *    - stringConverter
      */
-
     public static String decryptor(String cryptogram) {
         char[] cryptoToArr = cryptogram.toCharArray();
         ArrayList<Character> resultList = new ArrayList<Character>();
@@ -20,9 +18,10 @@ public class Problem2 {
 
         for (char current : cryptoToArr) {
             if (previous == current) {
-                resultList.remove(resultList.size()-1);
-                if (resultList.size() == 0) { break; }  // Index error를 위한 예외처리
-                previous = resultList.get(resultList.size()-1);
+                if (resultList.size() == 0) { resultList.add(current); }
+                else resultList.remove(resultList.size()-1);
+
+                if(resultList.size() > 0 ) { previous = resultList.get(resultList.size()-1); }
             }
             else {
                 resultList.add(current);
