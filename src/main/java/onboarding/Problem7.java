bc772c804defa5b6e71c1b76bb11baf50aa11381
Moveuk <v_donguk@naver.com>
2022-11-01 21:58:58 +0900

    feat: Problem7 user의 친구 목록을 만드는 기능 구현
    배열 [A, B] 중 어디에 user가 들어가던지 user의 친구를 찾아 추가한다

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        Set<String> friendList = new HashSet<>();
+
+        // 친구목록 찾아오기(초기화)
+        initializeFriendList(user, friendList, friends);
+
         return answer;
     }
+
+    public static void initializeFriendList(String user, Set<String> friendList, List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            if (friend.contains(user)) {
+                if (friend.get(0).equals(user)) {
+                    friendList.add(friend.get(1));
+                } else {
+                    friendList.add(friend.get(0));
+                }
+            }
+        }
+    }
+
 }
