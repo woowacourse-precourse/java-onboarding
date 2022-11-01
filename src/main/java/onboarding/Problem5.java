55e8827338eebf325af16ed131bf4498e6c46a52
Nugur1i <duckhee092@naver.com>
2022-11-01 21:48:52 +0900

    feat<problem5> : Add Exception

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,6 +9,13 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
 
+        //예외사항 : money의 범위가 1~1000,000을 벗어나는 경우
+
+        if (money > 1000000 || money < 1) {
+            System.out.println("Please, Enter money range 1~1000,000");
+            return Collections.emptyList();
+        }
+
         List<Integer> moneyUnitArray = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1)); //화폐 단위 배열 만들기
 
         int numberOfKindOfMoney = moneyUnitArray.size(); //화폐 단위 개수구하기
