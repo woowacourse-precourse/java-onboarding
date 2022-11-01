8dbb3d253d59b6e93dcd56d7e0ac4c149bf155fc
송민진 <m_6595@naver.com>
2022-11-01 11:29:46 +0900

    #6 feat: separate emails and nicknames from forms

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+
+        List<String> emails = new ArrayList<>();
+        List<String> nicknames = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            emails.add(form.get(0));
+            nicknames.add(form.get(1));
+        }
+
         return answer;
     }
 }
