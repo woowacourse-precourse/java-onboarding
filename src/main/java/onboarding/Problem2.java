2c56ad3935d6ae3f3e489c16ab63a8dda27a0ed2
sschan99 <seungchan1219@gmail.com>
2022-11-01 16:37:25 +0900

    feat: solve problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
 public class Problem2 {
+    public static String decryptionStep(String crypto) {
+        for (int i = 0; i < crypto.length() - 1; i++) {
+            if (crypto.charAt(i) == crypto.charAt(i + 1)) {
+                String str = Character.toString(crypto.charAt(i));
+                crypto = crypto.replace(str + str, "");
+                i--;
+            }
+        }
+        return crypto;
+    }
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String currentString = cryptogram;
+
+        while (true) {
+            String nextString = decryptionStep(currentString);
+
+            if (nextString.equals(currentString)) {
+                break;
+            }
+            currentString = nextString;
+        }
+        return currentString;
     }
 }
