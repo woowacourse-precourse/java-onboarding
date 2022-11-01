220361dbb19aec6cec556d2615c4f89e249b3254
import023 <80230155+import023@users.noreply.github.com>
2022-11-01 20:18:18 +0900

    feat : problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
+
 public class Problem2 {
+    private static Stack<Character> decodedCryptogram;
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        decodedCryptogram = new Stack<>();
+        for (char arg : cryptogram.toCharArray()) {
+            decoding(arg);
+        }
+        StringBuilder answer = new StringBuilder();
+        decodedCryptogram.forEach(answer::append);
+        return answer.toString();
     }
+
+    private static void decoding(char arg) {
+        if (decodedCryptogram.empty()) {
+            decodedCryptogram.push(arg);
+            return;
+        }
+        Character peekBefore = decodedCryptogram.peek();
+        if (peekBefore == arg) {
+            decodedCryptogram.pop();
+            return;
+        } else if (peekBefore != arg) {
+            decodedCryptogram.push(arg);
+            return;
+        }
+
+    }
+
 }
