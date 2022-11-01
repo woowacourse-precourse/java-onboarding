f4bbd3caac398ed43b8c755e1387702317eea91a
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-01 21:46:23 +0900

    Problem6 중복문제 해결

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -11,8 +11,12 @@ public class Problem6 {
         for (int i = 0; i < forms.size(); i++) {
             for(int j = 0; j < forms.size(); j++) {
                 if ( isDuplicated(forms.get(i).get(1), forms.get(j).get(1))) {
-                    emails.add(forms.get(i).get(0));
-                    emails.add(forms.get(j).get(0));
+                    if (!emails.contains(forms.get(i).get(0))) {
+                        emails.add(forms.get(i).get(0));
+                    }
+                    if (!emails.contains(forms.get(j).get(0))) {
+                        emails.add(forms.get(j).get(0));
+                    }
                 }
             }
         }
@@ -23,8 +27,8 @@ public class Problem6 {
     }
 
     private static boolean isDuplicated(String email1, String email2) {
-        for (int i = 0; i < email1.length(); i++) {
-            for (int j = 0; j < email2.length(); j++) {
+        for (int i = 0; i < email1.length() -1 ; i++) {
+            for (int j = 0; j < email2.length() -1 ; j++) {
                 if (email1.substring( i, i+2 ).equals(email2.substring( j, j+2 ))) {
                     return true;
                 }
