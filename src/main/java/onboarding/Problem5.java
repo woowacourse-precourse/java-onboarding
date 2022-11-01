c89786871812e5896b72ccd24aded63a90015fba
SproutMJ <mjk54565@gmail.com>
2022-11-01 21:18:22 +0900

    feat: 거스름돈을 계산하는 기능 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -9,5 +9,15 @@ public class Problem5 {
         return answer;
     }
 
+    static int[] makeCharge(int money){
+        int[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        int[] result = new int[9];
 
+        for (int i = 0; i<9; i++){
+            result[i] = money/arr[i];
+            money %= arr[i];
+        }
+
+        return result;
+    }
 }
