a5b7b34e32023286a4cee73c8d9cd7817b6a5ba6
hyungkya <hyungkya@uci.edu>
2022-11-01 16:01:55 +0900

    feat(problem 7): transform flat friend list to graph (map)

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    private static Map<String, List<String>> buildFriendsGraph(List<List<String>> friends) {
+        Map<String, List<String>> friendsGraph = new HashMap<>();
+        for (List<String> friend : friends) {
+            addFriend(friendsGraph, friend);
+        }
+        return friendsGraph;
+    }
+    private static void addFriend(Map<String, List<String>> friendsGraph, List<String> friend) {
+        if (friendsGraph.putIfAbsent(friend.get(0), new ArrayList<>(Arrays.asList(friend.get(1)))) != null) {
+            friendsGraph.get(friend.get(0)).add(friend.get(1));
+        }
+        if (friendsGraph.putIfAbsent(friend.get(1), new ArrayList<>(Arrays.asList(friend.get(0)))) != null) {
+            friendsGraph.get(friend.get(1)).add(friend.get(0));
+        }
     }
 }
