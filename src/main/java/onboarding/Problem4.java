32fd2b229b6abc17fd0f77de4140e58b6856242f
Eingerjar <cksrb1526@naver.com>
2022-11-01 23:20:46 +0900

    feat:problem 4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+
 public class Problem4 {
+    private static char convertToReverse(char chr){
+        if ('a' <= chr && chr <= 'z')
+            return (char)('z' - chr + 'a');
+        if ('A' <= chr && chr <= 'Z')
+            return (char)('Z' - chr + 'A');
+        return chr;
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder   answer = new StringBuilder();
+        char[] wordToArray = word.toCharArray();
+        int             i;
+        char            chr;
+
+        for(i = 0; i < word.length(); i++){
+            chr = convertToReverse(wordToArray[i]);
+            answer.append(chr);
+        }
+        return answer.toString();
     }
 }
