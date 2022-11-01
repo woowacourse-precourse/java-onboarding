87b292cfb816d2fb476531bb806904ea8b9a930e
0112leesy <0112leesy@naver.com>
2022-11-01 20:50:01 +0900

    [Problem7] feat: SCORE_OF_COMMON_FRIEND, SCORE_OF_TIMELINE_VISIT, MAX_LIST_SIZE

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
+    static final int SCORE_OF_COMMON_FRIEND = 10;
+    static final int SCORE_OF_TIMELINE_VISIT = 1;
+    static final int MAX_LIST_SIZE = 5;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
