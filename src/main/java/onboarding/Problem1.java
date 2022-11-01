55ed4f380ab87e708b44fe224a0f35366dc118b8
DonghyunShin <shindh0429@gmail.com>
2022-11-01 23:16:15 +0900

    [CREATE] 각각 두명의 가장 높은 숫자 조합 계산 및 비교하여 최고값 반환 함수 구현 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,20 +7,37 @@ import java.util.List;
 class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = 0;
 
         List<Integer> maxValue = new ArrayList<>();
         if(checkValid(pobi) == 1){
             return -1;
         }
+        int pMax = calculateMax(pobi, maxValue);
 
         List<Integer> maxValue2 = new ArrayList<>();
         if(checkValid(pobi) == 1){
             return -1;
         }
+        int cMax = calculateMax(crong, maxValue2);
 
-
+        int answer = (pMax > cMax) ? 1 : ((cMax > pMax) ? 2 : 0);
         return answer;
+    }
+
+    private static int calculateMax(List<Integer> pages, List<Integer> maxValue) {
+        for(int data : pages){
+            int hundreds = data/100;
+            int tens = data%100/10;
+            int units = data%10;
+            int plus = hundreds+tens+units;
+            int multiple = ((hundreds > 0) ? hundreds*tens*units : tens*units);
+
+            Integer max = (plus > multiple) ? plus : ((multiple > plus) ? multiple : plus);
+            maxValue.add(max);
+        }
+        int resultMax = Collections.max(maxValue);
+
+        return resultMax;
     }
 
     private static int checkValid(List<Integer> pages){
