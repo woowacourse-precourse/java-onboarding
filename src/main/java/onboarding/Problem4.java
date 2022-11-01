a0d55c40d23bcfc75809843d5763302ff8a68560
WonSeok-dd <cluejws@gmail.com>
2022-11-01 14:05:01 +0900

    Problem4 | docs: 기능 정의

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,12 +1,5 @@
 package onboarding;
 
-/**
- * 기능 구현 사항
- * 1. 문자 반대로 변환
- *  1-1 알파벳 외의 문자는 변환 x
- *  1-2 대문자는 대문자, 소문자는 소문자로 변환 o
- * 2. 문자 합쳐서 문자열로 합치기
- */
 public class Problem4 {
 
     private static boolean isUpperCase(char alp){
