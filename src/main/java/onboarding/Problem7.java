e32771053145f43530d14cd783d52a36410ddd01
Seiyon Park <seiyonpdev@gmail.com>
2022-11-01 18:34:38 +0900

    feat: ADD method recommendFriend

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -33,4 +33,44 @@ public class Problem7 {
         return friendOfFriend;
     }
 
+    public static Map<String, Integer> recommendFriend(Map<String, ArrayList<String>> friendOfFriend, List<String> visitors, String user) {
+
+        Map<String, Integer> recommendScore = new HashMap<>();
+
+        for(String alreadyFriend : friendOfFriend.keySet()) {
+
+            for(String potentialFriend : friendOfFriend.get(alreadyFriend)) {
+
+                if(potentialFriend.equals(user)) {
+                    continue;
+                }
+                else if(!recommendScore.containsKey(potentialFriend)) {
+
+                    recommendScore.put(potentialFriend, 10);
+                }
+                else {
+
+                    recommendScore.put(potentialFriend, recommendScore.get(potentialFriend) + 10);
+                }
+            }
+        }
+
+        for(String visitor : visitors) {
+
+            if(friendOfFriend.containsKey(visitor)) {
+                continue;
+            }
+            else if (!recommendScore.containsKey(visitor)) {
+
+                recommendScore.put(visitor, 1);
+            }
+            else {
+
+                recommendScore.put(visitor, recommendScore.get(visitor) + 1);
+            }
+        }
+
+        return recommendScore;
+    }
+
 }
