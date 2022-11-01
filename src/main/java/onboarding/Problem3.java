7e3595ff2ad46c380e1ef525865db6ce6284c3b2
JungYoonshin <sju08227@naver.com>
2022-11-01 11:21:46 +0900

    feat: #problem3 -> 각 자리수를 분리하고 3, 6, 9를 포함하는지 확인하는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         int answer = 0;
+
         return answer;
+    }
+
+    private static int checkNumberin365(String str) {
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char num = str.charAt(i);
+            if (num == '3' || num == '6' || num == '9') {
+                cnt += 1;
+            }
+        }
+        return cnt;
     }
 }
