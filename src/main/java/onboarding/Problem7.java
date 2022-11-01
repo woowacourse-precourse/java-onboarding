1c8dd3a88bc255e3916a089e6de3162db4696a05
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-01 18:38:26 +0900

    feat : 상수 변수 및 전역 변수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+    private static final int USER_MIN = 1;
+    private static final int USER_MAX = 30;
+    private static final int FRIENDS_ID_MIN = 1;
+    private static final int FRIENDS_ID_MAX = 30;
+    private static final int FRIENDS_MIN = 1;
+    private static final int FRIENDS_MAX = 10000;
+    private static final int VISITOR_MIN = 0;
+    private static final int VISITOR_MAX = 10000;
+    private static final Map<String, Integer> scoreForUser = new HashMap<>();
+    private static final Set<String> userFriends = new HashSet<>();
+
+
+
+
+
 }
