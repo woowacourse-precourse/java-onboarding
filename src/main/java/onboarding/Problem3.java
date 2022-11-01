9349296eac4d5c6d45288a25d5e35c50d94b87ab
VogJog <hackwaffle@gmail.com>
2022-11-01 21:35:44 +0900

    problem 2,3,4,5

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,10 +5,9 @@ public class Problem3 {
         int cnt = 0;
         for (int i = 1; i <= number; i++){
             int current = i;
-            //int temp = cnt;
 
             while (current !=0){
-                if(current %10 == 3 || current%10 == 6 || current%10 ==9){
+                if(current %10 == 3 || current %10 == 6 || current %10 ==9){
                     cnt++;
                 }
                 current /= 10;
--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        int[] moneyUnit = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int i = 0; i < moneyUnit.length; i++){
+            if(i == 0){
+                answer.add(i, (money/ moneyUnit[i]));
+            } else {
+                answer.add(i, (money % moneyUnit[i-1]) / moneyUnit[i]);
+            }
+        }
         return answer;
     }
 }
