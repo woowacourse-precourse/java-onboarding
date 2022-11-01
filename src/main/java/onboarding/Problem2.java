2f1dcb1ad80449070d7194c371dd65236cf9e815
1eejisoo <jisu991012@gmail.com>
2022-11-01 13:51:30 +0900

    [Problem 2] Feat: 중복 문자가 존재하는지 체크하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    // 중복 문자가 존재하는지 체크하는 기능
+    public static boolean isDuplicate(String cryptogram) {
+        for (int i = 0; i<cryptogram.length()-1; i++) {
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) return true;
+        }
+        return false;
     }
 }
