f06a2dbfe6ec2b9f91727a55f5112816435160da
Seiyon Park <seiyonpdev@gmail.com>
2022-11-01 18:33:07 +0900

    feat: ADD method overOneBridge

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,36 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+
+        List<String> answer = new ArrayList<>();
+
         return answer;
     }
+
+    public static Map<String, ArrayList<String>> overOneBridge(List<List<String>> friends) {
+
+        Map<String, ArrayList<String>> friendOfFriend = new HashMap<>();
+
+        for(List<String> edge : friends) {
+
+            String name = edge.get(0);
+            String friendWith = edge.get(1);
+
+            if(!friendOfFriend.containsKey(name)) {
+
+                friendOfFriend.put(name, new ArrayList<>());
+                friendOfFriend.get(name).add(friendWith);
+            }
+            else {
+
+                friendOfFriend.get(name).add(friendWith);
+            }
+        }
+
+        return friendOfFriend;
+    }
+
 }
