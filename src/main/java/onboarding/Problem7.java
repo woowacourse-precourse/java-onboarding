24d0dc84ae582077526dc5794e98091ed6eb9c54
0chil <0@chll.it>
2022-11-01 21:04:08 +0900

    feat(problem7): 문제 해결

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
+import onboarding.problem7.Network;
+
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+	public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+		Network network = new Network(friends, visitors);
+		return new ArrayList<>(network.recommendFriendsFor(user));
+	}
 }
