53820fabd2ab71461a5fafb8870f704a1eb70ed8
DonghyunShin <shindh0429@gmail.com>
2022-11-01 23:22:48 +0900

    [CREATE] 1부터 들어오는 변수까지 모든 수를 리스트에 추가하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        List<Integer> stream = IntStream.range(1, number+1)
+                .boxed()
+                .collect(Collectors.toList());
+
         return answer;
     }
+
 }
