56bb9301de068dbcc7a1da6fdba1d93b17820555
ghchoi <ghchoi0427@gmail.com>
2022-11-01 22:23:10 +0900

    feat: 곱셈, 덧셈 메서드 생성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,35 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        Problem1 p1 = new Problem1();
+        if (!p1.validatePage(pobi) || !p1.validatePage(crong)) {
+            return -1;
+        }
+
+
         return answer;
+    }
+
+    private int multiply(int number) {
+        int result = 1;
+        while (number > 0) {
+            result *= (number % 10);
+            number /= 10;
+        }
+        return result;
+    }
+
+    private int addition(int number) {
+        int result = 0;
+        while (number > 0) {
+            result += (number % 10);
+            number /= 10;
+        }
+        return result;
+    }
+
+    private int getMax(int i1, int i2) {
+        return Math.max(i1, i2);
     }
 
     private boolean validatePage(List<Integer> pages) {
