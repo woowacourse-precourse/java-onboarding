6b199834cbfa542da76e0d029d87696c72dfd608
swandevson <shai4956@gmail.com>
2022-11-01 14:33:20 +0900

    refactor: P4 대문자/소문자 문자 변환 기능 분리
    - 대/소문자에 따른 변환 기능 분리

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,14 +3,26 @@ package onboarding;
 
 
 public class Problem4 {
+    public static char convertUpperAlphabet(char ch) {
+        char converted = (char)('Z' - (ch - 'A'));
+
+        return converted;
+    }
+
+    public static char convertLowerAlphabet(char ch) {
+        char converted = (char)('z' - (ch - 'a'));
+
+        return converted;
+    }
+
     public static char convertAlphabet(char ch) {   // 해당 문자를 변환
         char converted = ch;
 
         if (Character.isUpperCase(ch)) {
-            converted = (char)('Z' - (ch - 'A'));
+            converted = convertUpperAlphabet(ch);
         }
         else {
-            converted = (char)('z' - (ch - 'a'));
+            converted = convertLowerAlphabet(ch);
         }
 
         return converted;
