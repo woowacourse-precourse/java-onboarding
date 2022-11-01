ce0b919a21b0396e4f3c8010b96da8f9f1392d02
Yh0sun <hy770819@naver.com>
2022-11-01 17:02:27 +0900

    problem6:func list

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,19 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        //중복제거
+        TreeSet<String> ans= new TreeSet<>();
+
+        //1. 닉네임에서 연속된 2글자 추출
+        //2. 다른 닉네임에 추출한 글자가 포함되면 ans에 이메일 넣음
+        //3. 글자 포함된 닉네임이 있었다면 1.닉네임도 ans에 이메일 넣음
+
+        List<String> answer = new ArrayList<>(ans);
         return answer;
     }
 }
