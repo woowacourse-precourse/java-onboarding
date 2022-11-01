f5ab6513f043013fc51b83d39923f68c918ef7eb
lhyeonj120 <isk04387@gmail.com>
2022-11-01 19:44:57 +0900

    [problem6] refactor: 중복 이메일 생성 기능 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,54 +1,51 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        List<String> emailList = createOverlapEmailList(forms);
+        Map<String, List<String>> overlapMap = createOverlapMap(forms);
         
-        answer = getListMaximumSizeOnMap(overlapMap);
+        answer = removeOverlapEmail(createListOverlapOnMap(overlapMap));
         answer.sort(Comparator.naturalOrder());
         
         return answer;
     }
     
-    private static List<String> createOverlapEmailList(List<List<String>> list){
-    	Set<String> unitOfNickname = new HashSet<>();
-    	List<String> res = List.of();
+    private static Map<String, List<String>> createOverlapMap(List<List<String>> list){
+    	Map<String, List<String>> overlapMap = new HashMap<>();
     	
     	for(int i = 0; i < list.size(); i++) {
     		for(int j = 0; j < list.get(i).get(1).length() - 1; j++) {
     			String s = list.get(i).get(1).substring(j, j + 2);
-    			if(!unitOfNickname.contains(s)) {
-    				unitOfNickname.add(s);
-    				continue;
+    			if(!overlapMap.containsKey(s)) {
+    				overlapMap.put(s, new LinkedList<>());
     			}
-    			res.add(list.get(i).get(0));
+    			overlapMap.get(s).add(list.get(i).get(0));
     		}
     	}
     	
-    	return res;
+    	return overlapMap;
     }
     
     private static List<String> removeOverlapEmail(List<String> list){
     	return list.stream().distinct().collect(Collectors.toList());
     }
     
-    private static List<String> getListMaximumSizeOnMap(Map<String, List<String>> map){
-    	int maxSize = 0;
-    	List<String> list = List.of();
+    private static List<String> createListOverlapOnMap(Map<String, List<String>> map){
+    	int size = 2;
+    	List<String> list = new ArrayList<>();
     	
     	for(String s : map.keySet()) {
-    		if(maxSize < map.get(s).size()) {
-    			maxSize = map.get(s).size();
-    			list = map.get(s);
+    		if(size <= map.get(s).size()) {
+    			list.addAll(map.get(s));
     		}
     	}
     	
