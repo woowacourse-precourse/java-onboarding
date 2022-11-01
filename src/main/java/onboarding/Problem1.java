114906733250bc40cbc2cf3b9c63b5f42b58c7a6
hsh_519 <suk000519@naver.com>
2022-11-01 15:30:57 +0900

    Problem1: 기능 구현(pageSum)

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,12 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 class Problem1 {
+    public static int pageSum(int page) {
+        int res = (int)(page/100) + (int)(page/10) + ((page%100)%10);
+        return res;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
