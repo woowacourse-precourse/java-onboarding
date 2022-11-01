68095f53398e3697188b3485467bc8738e6f1d2a
jay <jaewhan232@naver.com>
2022-11-01 19:41:30 +0900

    Implement : 숫자의 박수 횟수를 구하는 메소드

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,15 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    private static int getNumberOfClap(int number) {
+        int clap = 0;
+
+        while (number > 0) {
+            if ((number%10%3 == 0) && (number%10 != 0)) clap += 1;
+            number /= 10;
+        }
+
+        return clap;
+    }
 }
