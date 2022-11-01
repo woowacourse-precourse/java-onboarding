63c2edc74a370bc9c6f8c669aac9e0bb7d97b76f
seongbeom <84623098+hi12123123@users.noreply.github.com>
2022-11-01 20:54:33 +0900

    feat : add function isLowerAlha, isUpperAlpha, transferLogic

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,23 +1,49 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+
+        // word는 길이가 1 이상 1,000 이하인 문자열이다.
+        if (word.length() < 1 || word.length() > 1000){
+            return "";
+        }
+        return transferLogic(word);
     }
 
-    public static boolean isLowerAlpha(char word){
+    public static boolean isLowerAlpha(int word){
         if (word >= 97 && word <= 122){
-            return ture;
+            return true;
         }
         return false;
     }
 
-    public static boolean isNotAlpha(char word){
-        if (word < 65 || word > 122 || (91 <= word < 96){
+    public static boolean isUpperAlpha(int word){
+        if (word >= 65 && word <= 90){
             return true;
         }
 
         return false;
+    }
+
+    public static String transferLogic(String word){
+        String answer ="";
+        for (int i = 0; i < word.length(); i++){
+            int index = word.charAt(i);
+
+            if (isUpperAlpha(index)){
+                index = 90 - (index - 65);
+            }
+
+            else if (isLowerAlpha(index)){
+                index = 122 - (index - 97);
+            }
+
+
+            answer += (char)index;
+        }
+        return answer;
     }
 }
