cf9cc535133e0d16a8bbd8ec1c298b1c396e1f34
yunjeong <uu0078@naver.com>
2022-11-01 22:29:36 +0900

    Problem2 기능 목록 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,5 +1,10 @@
 package onboarding;
 
+/* 기능 목록
+1. 문자열 순회해 앞, 뒤문자와 중복되지 않는 것만 남기기
+2. 변경된 문자열에 새로운 중복 있는지 체크
+ */
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
