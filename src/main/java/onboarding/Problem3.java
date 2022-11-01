e7a72a6db5a71c86a18c9eb2952e80f86ae24ec8
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-01 20:19:18 +0900

    problem3 : input_exception

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        if(input_exception(number)) return -1;
+
+        return getTotalClap(number);
+    }
+
+    private static int getTotalClap(int num){
+        
+    }
+
+    private static int getClap(int num) {
+
+    }
+
+    private static boolean input_exception(int input){
+        //true is 'exception excution'
+        if(input<1||input>1000) return true;
+        return false;
     }
 }
