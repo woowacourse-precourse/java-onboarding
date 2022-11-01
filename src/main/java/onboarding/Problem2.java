f66c0c84f1527fbb1c19eebee5e631b193715542
eom-tae-in <eti0728@daum.net>
2022-11-01 20:00:59 +0900

    feat: static value

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,4 +15,23 @@ public class Problem2 {
         } while (checkModifyText);
         return cyptogram;
     }
+
+    public static String longTextDeduplication(String cryptogram) {
+        sb.setLength(0);
+        checkPre = false;
+        checkCurrent = false;
+        checkModifyText = false;
+        initailValue(cryptogram);
+        checkPre = checkCurrent;
+        for (int i = 1; i < cryptogram.length() - 1; i++) {
+            checkCurrent = checkDuplicate(cryptogram.charAt(i), cryptogram.charAt(i + 1));
+            if (checkPre == checkCurrent && !checkCurrent) {
+                sb.append(cryptogram.charAt(i));
+            }
+            verifyStringChange();
+            checkPre = checkCurrent;
+        }
+        lastValue(cryptogram);
+        return String.valueOf(sb);
+    }
 }
