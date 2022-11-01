b5fc63420e67d0d690e2777552d2be02c2355b3a
kwx4957 <kdy4957@naver.com>
2022-11-01 18:46:22 +0900

    1번 문제 기능 목록 및 구현 1. 페이지 번호 값 더하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,7 +2,26 @@ package onboarding;
 
 import java.util.List;
 
+/*
+1. 페이지 번호 더하기
+2. 페이지 번호 값 곱하기
+3. 더한 값 과 곱한 값 비교하기
+ */
 class Problem1 {
+    public static int add(List<Integer> value){
+        int max = 0;
+        for(int i:value){
+            int tmp = 0;
+
+            for(int j=0;j<3;j++){
+                tmp += i % 10;
+                i = i % 10;
+            }
+            max = Math.max(max, tmp);
+        }
+
+        return max;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
