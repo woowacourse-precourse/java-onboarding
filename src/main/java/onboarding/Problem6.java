f97e7a77a6b414f7b0f9364bab06b343625593c0
sangmins930 <sangmins930@gmail.com>
2022-11-01 21:25:58 +0900

    splitString method upload

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,8 +1,15 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
+    public static List<String> splitString(String nickname) {
+        List<String> fragment = new ArrayList<>();
+        for (int i = 0; i < nickname.length() - 1; i++)
+            fragment.add(nickname.substring(i, i + 2));
+        return fragment;
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
