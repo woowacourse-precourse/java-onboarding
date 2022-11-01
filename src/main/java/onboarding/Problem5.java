31dbc6f171ad55832a545ad81370f57f132c7597
seongbeom <84623098+hi12123123@users.noreply.github.com>
2022-11-01 16:36:06 +0900

    feat : add function moneyList and boundary exception

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,6 +12,7 @@ public class Problem5 {
     }
 
     private static List<Integer> moneyList(int money){
+        List<Integer> lst = new ArrayList<>(Arrays.asList(50000,10000,5000,1000,500,100,50,10,1));
         List<Integer> result = new ArrayList<>();
 
         // money는 1 이상 1,000,000 이하인 자연수이다.
@@ -18,6 +20,16 @@ public class Problem5 {
             return null;
         }
 
+        // 각 지폐와 동전을 확인하여 최대한 큰 금액의 화폐가 주어지도록 한다.
+        for(int i = 0; i < lst.size(); i++){
+            if(money >= lst.get(i)) {
+                result.add(money / lst.get(i));
+                money = money % lst.get(i);
+            }
+            else if(money < lst.get(i)){
+                result.add(0);
+            }
+        }
         return result;
     }
 }
