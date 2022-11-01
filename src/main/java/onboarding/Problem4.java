7f3623c2afe436cb226de86acfc97eda13570124
fefe2200 <dlcjf2200@naver.com>
2022-11-01 23:51:01 +0900

    problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            char tmep = ch;
+
+            if ('A' <= ch && ch <= 'Z')
+                tmep = (char) ('Z' - ch + 'A');
+            else if ('a' <= ch && ch <= 'z')
+                tmep = (char) ('z' - ch + 'a');
+
+            answer += tmep;
+        }
         return answer;
     }
 }
+
