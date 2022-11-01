dc9a19642d7b29faef37501f060e7aacffa4f3b9
masiljangajji <hjk172262@gmail.com>
2022-11-01 20:35:40 +0900

    feat: Problem3_숫자 3,6,9체크

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+
+        if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            answer++;
+        }
+
+
+
+
         return answer;
     }
 }
