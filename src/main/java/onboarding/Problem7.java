2f5de3927ced0916910e03a48faa5d8e63f50544
sypark <soun997@gmail.com>
2022-11-01 18:42:41 +0900

    Problem7 friendsMap, pointsMap 정의, friends와 visitors를 순회하며 friendsMap, pointsMap 초기화

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,47 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>();
+        HashMap<String, List<String>> friendsMap = new HashMap<>();  // 친구1  : [친구1의 친구들], ... 형태의 자료구조
+        HashMap<String, Integer> pointsMap = new HashMap<>();   // 친구추천점수 산출을 위한 자료구조
+
+        for (int i = 0; i < friends.size(); i++){
+            /*
+            if - 사용자의 친구들을 userFriends에 추가
+            else - 사용자는 제외한 친구들을 pointsMap, friendsMap에 추가
+            */
+            if (friends.get(i).get(0).equals(user)) {
+                friendsMap.put(user, new ArrayList<>());  // 사용자 : 사용자 친구 목록
+            }
+            else{
+                pointsMap.put(friends.get(i).get(0), 0);
+                friendsMap.put(friends.get(i).get(0), new ArrayList<>());
+            }
+            if (friends.get(i).get(1).equals(user)) {
+                friendsMap.put(user, new ArrayList<>());
+            }
+            else{
+                pointsMap.put(friends.get(i).get(1), 0);
+                friendsMap.put(friends.get(i).get(1), new ArrayList<>());
+            }
+        }
+
+        System.out.println(friendsMap);
+
+        // visitors들을 pointsMap에 추가
+        for (int i = 0; i < visitors.size(); i++){
+
+            pointsMap.put(visitors.get(i), 0);
+        }
+        System.out.println(pointsMap);
+
+
+
+
+
         return answer;
     }
 }
