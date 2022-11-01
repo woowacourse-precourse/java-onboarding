e319f540fa8098b4cbad1450ebbf16cdca3e0db5
lqsml <dvlprsml@gmail.com>
2022-11-01 23:24:23 +0900

    2~3. 펼친 책의  양쪽 페이지   번호의 각 자리 숫자를 모두 더하거나 모두 곱하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -28,7 +28,74 @@ class Problem1 {
 			crong.add(0, cRandom - 1);
 			crong.add(1, cRandom);
 		}
-
+		
+		// 2~3. 왼쪽/오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+    	List<Integer> pResults = new ArrayList<Integer>();
+    	List<Integer> cResults = new ArrayList<Integer>();
+    	
+    	//// 포비 왼쪽 페이지 
+    	int pNum1 = pobi.get(0)/100;
+    	int pNum2 = (pobi.get(0) - pNum1*100) / 10;
+    	int pNum3 = (pobi.get(0) - pNum1*100 - pNum2*10);
+    	
+    	pResults.add(pNum1 + pNum2 + pNum3);
+    	if(pNum1 == 0) {    		
+    		if(pNum2 == 0) {
+    			pResults.add(pNum3);
+    		} else {
+    			pResults.add(pNum2 * pNum3);
+    		}
+    	} else {
+        	pResults.add(pNum1 * pNum2 * pNum3);
+    	}
+    	
+    	//// 포비 오른쪽 페이지 
+    	pNum1 = pobi.get(1)/100;
+    	pNum2 = (pobi.get(1) - pNum1*100) / 10;
+    	pNum3 = (pobi.get(1) - pNum1*100 - pNum2*10);
+    	
+    	pResults.add(pNum1 + pNum2 + pNum3);
+    	if(pNum1 == 0) {    		
+    		if(pNum2 == 0) {
+    			pResults.add(pNum3);
+    		} else {
+    			pResults.add(pNum2 * pNum3);
+    		}
+    	} else {
+        	pResults.add(pNum1 * pNum2 * pNum3);
+    	}
+    	
+    	//// 크롱 왼쪽 페이지 
+    	int cNum1 = crong.get(0)/100;
+    	int cNum2 = (crong.get(0) - cNum1*100) / 10;
+    	int cNum3 = (crong.get(0) - cNum1*100 - cNum2*10);
+    	
+    	cResults.add(cNum1 + cNum2 + cNum3);
+    	if(cNum1 == 0) {    		
+    		if(cNum2 == 0) {
+    			cResults.add(cNum3);
+    		} else {
+    			cResults.add(cNum2 * cNum3);
+    		}
+    	} else {
+        	cResults.add(cNum1 * cNum2 * cNum3);
+    	}
+    	
+    	//// 크롱 오른쪽 페이지 
+    	cNum1 = crong.get(1)/100;
+    	cNum2 = (crong.get(1) - cNum1*100) / 10;
+    	cNum3 = (crong.get(1) - cNum1*100 - cNum2*10);
+    	
+    	cResults.add(cNum1 + cNum2 + cNum3);
+    	if(cNum1 == 0) {    		
+    		if(cNum2 == 0) {
+    			cResults.add(cNum3);
+    		} else {
+    			cResults.add(cNum2 * cNum3);
+    		}
+    	} else {
+        	cResults.add(cNum1 * cNum2 * cNum3);
+    	}
 
 
 		return answer;
