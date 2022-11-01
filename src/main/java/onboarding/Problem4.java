959740a7c2024b0c3a8eb40a4a8a81b34bdf09ef
inwoo <korean5078@gmail.com>
2022-11-01 23:16:44 +0900

    1주차 프리코스 문제 풀이(4번 수정)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,7 @@ public class Problem4 {
         String answer = "";
 
         // word의 길이가 1보다 작거나 1000보다 클 때 예외 처리
-        if (word.length() == 0 || word.length() > 1000)
+        if (word.length() < 1 || word.length() > 1000)
             return answer;
 
         // word를 char 배열로 나누고 한 char마다 아스키코드표로 변환
@@ -18,9 +18,6 @@ public class Problem4 {
             else if (chars[i] >= 97 && chars[i] <= 122) {
                 chars[i] += 27 - (chars[i] - 96) * 2;
             }
-            // 알파벳 이외의 문자는 예외 처리
-            else
-                return answer;
         }
         answer = new String(chars);
 
