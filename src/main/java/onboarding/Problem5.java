cd6b5b7b3c21b77f491d684a39c7c9d5080f61da
lynne921 <dkfls921@gmail.com>
2022-11-01 19:13:13 +0900

    화폐 단위를 저장하는 메서드 추가

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static void moneyUnitAdd (List<Integer> moneyUnit) {
+        moneyUnit.add(50000);
+        moneyUnit.add(10000);
+        moneyUnit.add(5000);
+        moneyUnit.add(1000);
+        moneyUnit.add(500);
+        moneyUnit.add(100);
+        moneyUnit.add(50);
+        moneyUnit.add(10);
+        moneyUnit.add(1);
+    }
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
+        List<Integer> moneyUnit = Collections.emptyList();
+        moneyUnitAdd(moneyUnit);
+
         return answer;
     }
 }
