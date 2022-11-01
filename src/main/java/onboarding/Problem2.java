793f0759bd44d3cec90da854dc910aac71bc78b2
Basak <chgy2131@naver.com>
2022-11-01 11:50:37 +0900

    [feat] 2번 - 문자열의 시작과 끝 인덱스를 저장하기 위한 데이터 클래스

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,27 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    // 문자열의 시작과 끝 인덱스를 저장하기 위한 데이터 클래스
+    public static class IndexData {
+        private int start;
+        private int end;
+
+        IndexData(int start, int end) {
+            setStart(start);
+            setEnd(end);
+        }
+        public int getStart() {
+            return start;
+        }
+        public int getEnd() {
+            return end;
+        }
+        public void setStart(int start) {
+            this.start = start;
+        }
+        public void setEnd(int end) {
+            this.end = end;
+        }
+    }
 }
