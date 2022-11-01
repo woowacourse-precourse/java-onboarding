e47b6e20c707a4e11e30ab8d0d78416bbafd252c
고범석 <kobums23@gmail.com>
2022-11-01 14:17:59 +0900

    problem6 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -2,7 +2,9 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,21 +17,27 @@ public class Problem6 {
             String name = forms.get(i).get(1);
             if (name.length() > 1) {
                 for (int k = 0; k < name.length() - 1; k++) {
-                    name = name.substring(k, k + 2);
-                    Pattern pattern = Pattern.compile(".*" + name + ".*");
+                    String newName = name.substring(k, k + 2);
+                    Pattern pattern = Pattern.compile(".*" + newName + ".*");
 
                     for (int j = 0; j < forms.size(); j++) {
                         if (i != j) {
                             Matcher matcher = pattern.matcher(forms.get(j).get(1));
                             if (matcher.find()) {
                                 answer.add(forms.get(j).get(0));
-                                break;
+                                // break;
                             }
                         }
                     }
                 }
             }
         }
-        return answer;
+
+        // List를 Set으로 변경
+        Set<String> set = new HashSet<String>(answer);
+        // Set을 List로 변경        
+        List<String> newList = new ArrayList<String>(set);
+        
+        return newList;
     }
 }
