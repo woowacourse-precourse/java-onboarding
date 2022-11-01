591408d0fa3041b1529fea00b11a4281dcf4a17a
jamangstangs <jamangsong@gmail.com>
2022-11-01 10:39:37 +0900

    feat: problem3(진행), getTotalClapNumber() -> 숫자 x까지 박수를 치는 횟수를 구하는 메소드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return getTotalClapNumber(number);
+    }
+
+    private static int getTotalClapNumber(int number) {
+        Integer result = 0;
+        for (int i = 1; i <= number; i++) {
+            result += getSingleClapNum(i);
+        }
+        return result;
+    }
+
+    private static Integer getSingleClapNum(int i) {
     }
 }
