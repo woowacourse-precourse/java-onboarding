b80dd8002af7f3b530dd39dd2e5ff9cf207899a5
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-11-01 21:12:02 +0900

    feat: solve transfer alphabet(알파벳 변환)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
-
+/*
+A->Z
+B->Y
+알파벳 변환 : 'A' + 'Z' - '현재 알파벳'
+ */
 public class Problem4 {
     public static String solution(String word) {
+        String str = word;
         String answer = "";
+        int strLen = str.length();
+        for (int i = 0; i < strLen; ++i) {
+            char c = str.charAt(i);
+            answer += transformAlpha(c);
+        }
         return answer;
+    }
+    private static char transformAlpha(char c)
+    {
+        if (c >= 'A' && c <= 'Z')
+            return (char)('A' + 'Z' - c);
+        else if (c >= 'a' && c <= 'z')
+            return (char)('a' + 'z' - c);
+        else
+            return c;
     }
 }
