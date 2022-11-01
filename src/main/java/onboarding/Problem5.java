490d2228166e24c859a6f9112d7a03c1ee99d000
Daeeun Ko <7273322@naver.com>
2022-11-01 14:14:30 +0900

    feat: get cash

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,13 @@
 package onboarding;
 
+import onboarding.problem5.Account;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        Account account = new Account(money);
+        return account.withdraw();
     }
 }
--- src/main/java/onboarding/problem5/Account.java
+++ src/main/java/onboarding/problem5/Account.java
@@ -1,9 +1,13 @@
 package onboarding.problem5;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Account {
 
     private final int MIN_MONEY = 1;
     private final int MAX_MONEY = 1_000_000;
+    private final int[] UNIT = {50_000, 10_000, 5_000, 1_000, 500, 100, 50 , 10, 1};
 
     private int money;
 
@@ -20,4 +24,16 @@ public class Account {
         return money < MIN_MONEY || money > MAX_MONEY;
     }
 
+    public List<Integer> withdraw() {
+
+        List<Integer> list = new LinkedList<>();
+
+        for (int unit : UNIT) {
+            int count = money / unit;
+            money -= unit * count;
+            list.add(count);
+        }
+
+        return list;
+    }
 }
