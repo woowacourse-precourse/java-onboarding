c04aa9df57615df2885701535f4708e7db948a82
임원정 <LWJ@MacBookPro.local>
2022-11-01 18:30:31 +0900

    feature : problem3 각 자리 숫자를 검사하는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem3 {
+    private static int count;
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static void checkNumber(int num) {
+        StringBuilder number = new StringBuilder(Integer.toString(num));
+
+        for (int i = 0; i < number.length(); i++) {
+            check369(number.charAt(i));
+        }
     }
 }
