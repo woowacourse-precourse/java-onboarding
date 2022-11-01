a02270bd72efd1a0264dc5076b361862b54e4fd7
footdev <ekclstkfka5@gmail.com>
2022-11-01 22:35:55 +0900

    refactor: 기능7 메소드 수정
    매개변수 수정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,17 +6,17 @@ import java.util.List;
 public class Problem7 {
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        scoreBasedOnFriends(friends);
-        scoreBasedOnVisitors(visitors);
+        scoreBasedOnFriends(user, friends);
+        scoreBasedOnVisitors(user, visitors);
         List<String> answer = returnAnswer();
         return answer;
     }
 
-    public static void scoreBasedOnFriends(List<List<String>> friends) {
+    public static void scoreBasedOnFriends(String user, List<List<String>> friends) {
 
     }
 
-    public static void scoreBasedOnVisitors(List<String> visitors) {
+    public static void scoreBasedOnVisitors(String user, List<String> visitors) {
 
     }
 
