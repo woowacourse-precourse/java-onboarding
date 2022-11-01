ed201db25967c5b1b862bc429c8265852273947b
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2022-11-01 16:30:21 +0900

    Feat: Add 청개구리 사전 함수

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,4 +5,15 @@ public class Problem4 {
         String answer = "";
         return answer;
     }
+
+    
+    private static char convertToFrogDictionary(char word) {
+        if (65 <= (byte)word && (byte)word <= 90) {
+            return (char)(155 - (byte)word);
+        } else if (97 <= (byte) word && (byte) word <= 122) {
+            return (char) (219 - (byte)word);
+        }
+
+        return word;
+    }
 }
