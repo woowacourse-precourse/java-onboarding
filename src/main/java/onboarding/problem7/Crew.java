951b702dc87be93399fa6bf2c9269fc992bd902d
rt3310 <btac3310@gmail.com>
2022-11-01 10:54:22 +0900

    feat(Problem7): Crew 객체 생성

---
+++ src/main/java/onboarding/problem7/Crew.java
@@ -0,0 +1,19 @@
+package onboarding.problem7;
+
+public class Crew {
+    private String name;
+    private Integer point;
+
+    public Crew(String name, Integer point) {
+        this.name = name;
+        this.point = point;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+}
